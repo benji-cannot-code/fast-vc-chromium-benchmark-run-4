22466fb11372f57818df866dc6fa04f0321a6c19
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 22:36:52 +0000

    Take out additional dependency from http_util as a short-term fix to get the pdf build compiling. Review URL: http://codereview.chromium.org/3450024
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60367 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_util.cc
+++ net/http/http_util.cc
@@ -15,10 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "base/string_piece.h"
 #include "base/string_util.h"
-#include "net/base/net_errors.h"
 #include "net/base/net_util.h"
-#include "net/http/http_response_body_drainer.h"
-#include "net/http/http_stream.h"
 
 using std::string;
 
@@ -698,12 +695,6 @@ bool HttpUtil::ValuesIterator::GetNext() {
       return true;
   }
   return false;
-}
-
-void HttpUtil::DrainStreamBodyAndClose(HttpStream* stream) {
-  HttpResponseBodyDrainer* drainer = new HttpResponseBodyDrainer(stream);
-  drainer->Start();
-  // |drainer| will delete itself.
 }
 
 }  // namespace net
