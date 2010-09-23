4154b77771d03acac8ddd1b027b2c93e00324ce6
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 21:18:47 +0000

    2010-09-23  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Roben.
    
            Set synchronous handle in ResourceHandleWin
            https://bugs.webkit.org/show_bug.cgi?id=46395
    
            When loading synchronous network resources, we need to use a synchronous internet handle.
    
            * platform/network/ResourceHandle.h:
            * platform/network/win/ResourceHandleWin.cpp:
            (WebCore::ResourceHandle::loadResourceSynchronously):
            (WebCore::ResourceHandle::setSynchronousInternetHandle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandle.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandle.h
@@ -169,6 +169,7 @@ public:
     static void forceContentSniffing();
 
 #if USE(WININET)
+    void setSynchronousInternetHandle(HINTERNET);
     void fileLoadTimer(Timer<ResourceHandle>*);
     void onHandleCreated(LPARAM);
     void onRedirect();
