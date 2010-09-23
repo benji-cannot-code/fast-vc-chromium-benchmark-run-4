97717e44b083c62701a9820270acfcefb76f93a7
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 18:42:01 +0000

    Fix a typo that crashes oobe.
    This breaks owning process as well.
    
    BUG=chromium-os:6854
    TEST=Verify fix for chromeium-os:6854 and make sure you see the account image screen in oobe.
    
    Review URL: http://codereview.chromium.org/3393021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60319 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -435,7 +435,7 @@ void WizardController::ShowEulaScreen() {
 }
 
 void WizardController::ShowRegistrationScreen() {
-  if (!GetCustomization() &&
+  if (!GetCustomization() ||
       !GURL(GetCustomization()->registration_url()).is_valid()) {
     LOG(INFO) <<
         "Skipping registration screen: manifest not defined or invalid URL.";
