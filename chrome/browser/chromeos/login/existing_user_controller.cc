79fb4b7929a7e47568f06da99b401c540e20e8b5
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 06:46:35 +0000

    Enable html background for login screen. Fix background of unlock view to match login screen.
    BUG=chromium-os:5763
    TEST=none
    
    Review URL: http://codereview.chromium.org/3380021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/wm_ipc.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/profile_manager.h"
+#include "chrome/common/chrome_switches.h"
 #include "gfx/native_widget_types.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
@@ -136,8 +137,13 @@ ExistingUserController::ExistingUserController(
 
 void ExistingUserController::Init() {
   if (!background_window_) {
+    std::string url_string =
+        CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+            switches::kScreenSaverUrl);
+
     background_window_ = BackgroundView::CreateWindowContainingView(
         background_bounds_,
+        GURL(url_string),
         &background_view_);
 
     if (!WizardController::IsDeviceRegistered()) {
