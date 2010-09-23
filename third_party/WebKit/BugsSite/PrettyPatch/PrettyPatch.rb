8d414a041526f8bef471efbc9a8c591ba6cffe12
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 03:42:05 +0000

    2010-09-22  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add link to bug to review page
            https://bugs.webkit.org/show_bug.cgi?id=46192
    
            To make room for the link, I moved the help text to the top of the page
            and tweaked the language to help folks discover that you can select
            context using the line numbers.
    
            Also, move more text to sans-serif since that fits in better with the
            pretty-patch fonts.
    
            * PrettyPatch/PrettyPatch.rb:
            * code-review.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/BugsSite/PrettyPatch/PrettyPatch.rb
+++ third_party/WebKit/BugsSite/PrettyPatch/PrettyPatch.rb
@@ -214,18 +214,15 @@ body {
   right: 0;
   border-top: 1px solid #ddd;
   background-color: #eee;
+  font-family: sans-serif;
 }
 
 #toolbar .actions {
   float: left;
 }
 
-#toolbar .message {
+#toolbar .links {
   float: right;
-}
-
-#toolbar .commentStatus {
-  font-style:italic
 }
 
 .winter {
@@ -272,6 +269,15 @@ body {
  font-style: italic;
 }
 
+.message {
+  font-size: small;
+  font-family: sans-serif;
+}
+
+.commentStatus {
+  font-style: italic;
+}
+
 .description {
   font-style: italic;
 }
@@ -307,7 +313,7 @@ body {
 }
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
-<script src="code-review.js?version=11"></script> 
+<script src="code-review.js?version=12"></script> 
 EOF
 
     def self.revisionOrDescription(string)
