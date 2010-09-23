28db5994f24321fac45f829f6186833551741bc7
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 23:48:05 +0000

    2010-09-23  Tony Chang  <tony@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.sampleSVGAnimationForElementAtTime
            https://bugs.webkit.org/show_bug.cgi?id=46426
    
            * platform/chromium/test_expectations.txt: Mark 4 tests as passing.
    2010-09-23  Tony Chang  <tony@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.sampleSVGAnimationForElementAtTime
            https://bugs.webkit.org/show_bug.cgi?id=46426
    
            * public/WebFrame.h:
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::pauseSVGAnimation):
            * src/WebFrameImpl.h:
    2010-09-23  Tony Chang  <tony@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [chromium] implement layoutTestController.sampleSVGAnimationForElementAtTime
            https://bugs.webkit.org/show_bug.cgi?id=46426
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::sampleSVGAnimationForElementAtTime):
            * DumpRenderTree/chromium/LayoutTestController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebFrameImpl.cpp
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ReplaceSelectionCommand.h"
 #include "ResourceHandle.h"
 #include "ResourceRequest.h"
+#include "SVGSMILElement.h"
 #include "ScriptController.h"
 #include "ScriptSourceCode.h"
 #include "ScriptValue.h"
@@ -1721,6 +1722,26 @@ bool WebFrameImpl::selectionStartHasSpellingMarkerFor(int from, int length) cons
     if (!m_frame)
         return false;
     return m_frame->editor()->selectionStartHasSpellingMarkerFor(from, length);
+}
+
+bool WebFrameImpl::pauseSVGAnimation(const WebString& animationId, double time, const WebString& elementId)
+{
+#if !ENABLE(SVG)
+    return false;
+#else
+    if (!m_frame)
+        return false;
+
+    Document* document = m_frame->document();
+    if (!document || !document->svgExtensions())
+        return false;
+
+    Node* coreNode = document->getElementById(animationId);
+    if (!coreNode || !SVGSMILElement::isSMILElement(coreNode))
+        return false;
+
+    return document->accessSVGExtensions()->sampleAnimationAtTime(elementId, static_cast<SVGSMILElement*>(coreNode), time);
+#endif
 }
 
 // WebFrameImpl public ---------------------------------------------------------
