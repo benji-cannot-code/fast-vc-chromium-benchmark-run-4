422ac4b8fb1579e0e260c3b599325ff8ed3dde21
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 20:45:48 +0000

    2010-09-23  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Roben.
    
            Remove (has|set)ReceivedResponse() from public ResourceHandleWin API
            https://bugs.webkit.org/show_bug.cgi?id=46391
    
            There is no need for this functions to be public, so remove them.
    
            * platform/network/ResourceHandle.h:
            * platform/network/win/ResourceHandleWin.cpp:
            (WebCore::ResourceHandle::onRequestComplete):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/ResourceHandle.h
+++ third_party/WebKit/WebCore/platform/network/ResourceHandle.h
@@ -169,8 +169,6 @@ public:
     static void forceContentSniffing();
 
 #if USE(WININET)
-    void setHasReceivedResponse(bool = true);
-    bool hasReceivedResponse() const;
     void fileLoadTimer(Timer<ResourceHandle>*);
     void onHandleCreated(LPARAM);
     void onRedirect();
