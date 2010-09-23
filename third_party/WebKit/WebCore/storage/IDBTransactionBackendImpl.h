0868c5c8c46bd8b8fb7a3c846250cfb9ff7f7b77
andreip@google.com <andreip@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 15:13:18 +0000

    2010-09-23  Andrei Popescu  <andreip@google.com>
            Reviewed by Jeremy Orlow.
    
            [Chromium] Build broken due to missing include in IDBTransactionBackendImpl.h
            https://bugs.webkit.org/show_bug.cgi?id=46352
    
            No new tests needed, build fix.
    
            * storage/IDBTransaction.cpp:
            (WebCore::IDBTransaction::objectStore):
            * storage/IDBTransactionBackendImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBTransactionBackendImpl.h
+++ third_party/WebKit/WebCore/storage/IDBTransactionBackendImpl.h
@@ -32,7 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMStringList.h"
 #include "IDBTransactionBackendInterface.h"
 #include "IDBTransactionCallbacks.h"
-#include <SQLiteTransaction.h>
+#include "SQLiteTransaction.h"
+#include "Timer.h"
 #include <wtf/Deque.h>
 #include <wtf/RefPtr.h>
 
