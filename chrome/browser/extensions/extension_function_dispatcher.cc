61f14ebdbaaef0ffe2101737164ee77f25bdf35f
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 20:19:56 +0000

    Add a launchApp method to extension management API.
    This makes it possible for new tab replacement extensions to recreate the same
    app launch experience as the built-in NTP (pinned tabs, launch containers,
    etc.)
    
    Also add the extension version to data we report about extensions via
    the management API.
    
    BUG=56325,55474
    TEST=An extension using chrome.experimental.management.launchApp(<id>) should
    work to launch an app the same way that app gets launched from the built-in
    new tab page.
    
    
    
    Review URL: http://codereview.chromium.org/3381019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60334 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -274,13 +274,14 @@ void FactoryRegistry::ResetFunctions() {
 
   // Management.
   RegisterFunction<GetAllExtensionsFunction>();
+  RegisterFunction<LaunchAppFunction>();
   RegisterFunction<SetEnabledFunction>();
-  RegisterFunction<InstallFunction>();
   RegisterFunction<UninstallFunction>();
 
   // WebstorePrivate.
   RegisterFunction<GetSyncLoginFunction>();
   RegisterFunction<GetStoreLoginFunction>();
+  RegisterFunction<InstallFunction>();
   RegisterFunction<SetStoreLoginFunction>();
 }
 
