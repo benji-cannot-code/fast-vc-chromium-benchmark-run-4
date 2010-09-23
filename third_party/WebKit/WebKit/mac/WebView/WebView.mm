8688acddbeddec981dea2201c8db904cf50c6a93
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-23 20:35:30 +0000

    2010-09-23  Matthew Delaney  <mdelaney@apple.com>
            Reviewed by Simon Fraser.
    
            Reduce minimum DOMTimer interval
            https://bugs.webkit.org/show_bug.cgi?id=45362
    
            Since the setInterval call is meant to reflect a global change for
            all pages' DOMTimers, it's misleading to have it be a method on a
            given page's settings' object. Thus, this patch changes the call to be static.
    
            This simple change to using a static call instead of the page's setting's
            method doesn't warrant a test.
    
            * page/Settings.h: Changed the setInterval call to be static.
    2010-09-23  Matthew Delaney  <mdelaney@apple.com>
    
            Reviewed by Simon Fraser.
    
            Reduce minimum DOMTimer interval
            https://bugs.webkit.org/show_bug.cgi?id=45362
    
            * WebView/WebView.mm:
            Updating set interval call to use Settings' static version inside
            one time init block.
    2010-09-23  Matthew Delaney  <mdelaney@apple.com>
    
            Reviewed by Simon Fraser.
    
            Reduce minimum DOMTimer interval
            https://bugs.webkit.org/show_bug.cgi?id=45362
    
            * WebView.cpp:
            Updating set interval call to use Settings' static version inside
            one time init block.
    2010-09-23  Matthew Delaney  <mdelaney@apple.com>
    
            Reviewed by Simon Fraser.
    
            Reduce minimum DOMTimer interval
            https://bugs.webkit.org/show_bug.cgi?id=45362
    
            * WebProcess/WebPage/WebPage.cpp:
            Updating set interval call to use Settings' static version.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebView.mm
@@ -696,6 +696,7 @@ static bool shouldEnableLoadDeferring()
         
         // Initialize our platform strategies.
         WebPlatformStrategies::initialize();
+        Settings::setMinDOMTimerInterval(0.004);
 
         didOneTimeInitialization = true;
     }
@@ -717,7 +718,6 @@ static bool shouldEnableLoadDeferring()
 
     _private->page->setCanStartMedia([self window]);
     _private->page->settings()->setLocalStorageDatabasePath([[self preferences] _localStorageDatabasePath]);
-    _private->page->settings()->setMinDOMTimerInterval(0.004);
 
     [WebFrame _createMainFrameWithPage:_private->page frameName:frameName frameView:frameView];
 
