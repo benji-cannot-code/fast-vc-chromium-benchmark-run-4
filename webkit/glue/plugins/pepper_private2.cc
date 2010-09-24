461b71be9adaaeb5aae115e6cb49c6fd73234ccf
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 05:39:15 +0000

    Add some optimizations to plugin painting.
    The simplest one is to disable blending when the plugin is opaque. The more
    complicated one is to bypass webkit painting the background of plugins when we
    know the plugin to be always on top and also opaque. The always on top flag is
    currently set by a new "Private2" API. Bypassing WebKit makes animations
    faster.
    
    This is a re-land of the previous patch with a trivial compilation fix. This
    also adds a clip rect to the GetBitmap... function so we can properly handle
    plugins in nested iframes with proper clipping.
    
    BUG=none
    TEST=none
    Original review URL: http://codereview.chromium.org/3421030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60426 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/plugins/pepper_private2.cc
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "webkit/glue/plugins/pepper_private2.h"
+
+#include "webkit/glue/plugins/pepper_plugin_instance.h"
+#include "webkit/glue/plugins/ppb_private2.h"
+
+namespace pepper {
+
+namespace {
+
+void SetInstanceAlwaysOnTop(PP_Instance pp_instance, bool on_top) {
+  PluginInstance* instance = PluginInstance::FromPPInstance(pp_instance);
+  if (!instance)
+    return;
+  instance->set_always_on_top(on_top);
+}
+
+const PPB_Private2 ppb_private2 = {
+  &SetInstanceAlwaysOnTop
+};
+
+}  // namespace
+
+// static
+const PPB_Private2* Private2::GetInterface() {
+  return &ppb_private2;
+}
+
+}  // namespace pepper
---
+++ webkit/glue/plugins/pepper_private2.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_GLUE_PLUGINS_PEPPER_PRIVATE2_H_
+#define WEBKIT_GLUE_PLUGINS_PEPPER_PRIVATE2_H_
+
+#include "webkit/glue/plugins/pepper_resource.h"
+
+struct PPB_Private2;
+
+namespace pepper {
+
+class Private2 {
+ public:
+  // Returns a pointer to the interface implementing PPB_Private2 that is
+  // exposed to the plugin.
+  static const PPB_Private2* GetInterface();
+};
+
+}  // namespace pepper
+
+#endif  // WEBKIT_GLUE_PLUGINS_PEPPER_PRIVATE2_H_
