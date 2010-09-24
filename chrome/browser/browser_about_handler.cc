8301606e60af3a25d198bd8623fb3e1791f2332b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 04:46:58 +0000

    Revert 60373, trying to track down a perf regression (see bug 56752)- Add about:gpuhang
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3447023
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/3398027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -272,7 +272,7 @@ std::string AboutAbout() {
     html.append(kAllAboutPaths[i]);
     html.append("</a>\n");
   }
-  const char *debug[] = { "crash", "hang", "shorthang", "gpucrash", "gpuhang" };
+  const char *debug[] = { "crash", "hang", "shorthang", "gpucrash" };
   html.append("</ul><h2>For Debug</h2>");
   html.append("</ul><p>The following pages are for debugging purposes only. "
               "Because they crash or hang the renderer, they're not linked "
@@ -1161,13 +1161,9 @@ bool WillHandleBrowserAboutURL(GURL* url, Profile* profile) {
     return true;
   }
 
-  // Handle URLs to wreck the gpu process.
+  // Handle URL to crash the gpu process.
   if (LowerCaseEqualsASCII(url->spec(), chrome::kAboutGpuCrashURL)) {
     GpuProcessHost::SendAboutGpuCrash();
-    return true;
-  }
-  if (LowerCaseEqualsASCII(url->spec(), chrome::kAboutGpuHangURL)) {
-    GpuProcessHost::SendAboutGpuHang();
     return true;
   }
 
