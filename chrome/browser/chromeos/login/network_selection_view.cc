da61e82f0a8e59d40e21e4dc07175742f32b66b3
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 14:37:21 +0000

    Hide "Change proxy settings link" during connection in progress. Replace chrome://options with chrome://settings because it was changed recently.
    BUG=None
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/3397028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_selection_view.cc
+++ chrome/browser/chromeos/login/network_selection_view.cc
@@ -361,6 +361,7 @@ void NetworkSelectionView::ShowConnectingStatus(bool connecting,
   select_network_label_->SetVisible(!connecting);
   network_dropdown_->SetVisible(!connecting);
   continue_button_->SetVisible(!connecting);
+  proxy_settings_link_->SetVisible(!connecting);
   connecting_network_label_->SetVisible(connecting);
   InitLayout();
   Layout();
@@ -391,7 +392,7 @@ bool NetworkSelectionView::IsContinueEnabled() const {
 void NetworkSelectionView::LinkActivated(views::Link* source, int) {
   if (source == proxy_settings_link_) {
     if (!proxy_settings_dialog_.get()) {
-      static const char kProxySettingsURL[] = "chrome://options/proxy";
+      static const char kProxySettingsURL[] = "chrome://settings/proxy";
       proxy_settings_dialog_.reset(new LoginHtmlDialog(
           this,
           GetNativeWindow(),
