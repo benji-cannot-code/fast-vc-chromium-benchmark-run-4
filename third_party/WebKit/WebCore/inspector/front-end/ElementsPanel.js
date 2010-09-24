edfcd48fd7d8a6af640a1477de91ac39b3eb05a8
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-24 15:45:22 +0000

    2010-09-24  Ilya Tikhonovsky  <loislo@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: move searchingForNode flag in InspectorController under control of InspectorCookies.
    
            Four functions from Inspector IDL go away.
            Searching for node feature of Inspector keeps alive even after navigation.
    
            https://bugs.webkit.org/show_bug.cgi?id=46462
    
            * inspector/Inspector.idl:
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::updateInspectorStateCookie):
            (WebCore::InspectorController::restoreInspectorStateFromCookie):
            (WebCore::InspectorController::setSearchingForNode):
            (WebCore::InspectorController::populateScriptObjects):
            * inspector/InspectorController.h:
            * inspector/front-end/ElementsPanel.js:
            (WebInspector.ElementsPanel.prototype.hide):
            (WebInspector.ElementsPanel.prototype.updateFocusedNode):
            (WebInspector.ElementsPanel.prototype._setSearchingForNode):
            (WebInspector.ElementsPanel.prototype.setSearchingForNode):
            (WebInspector.ElementsPanel.prototype.toggleSearchingForNode):
            * inspector/front-end/inspector.js:
            (WebInspector._updateFocusedNode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ElementsPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ElementsPanel.js
@@ -149,7 +149,7 @@ WebInspector.ElementsPanel.prototype = {
         WebInspector.Panel.prototype.hide.call(this);
 
         WebInspector.highlightDOMNode(0);
-        InspectorBackend.disableSearchingForNode();
+        this.setSearchingForNode(false);
     },
 
     resize: function()
@@ -247,16 +247,6 @@ WebInspector.ElementsPanel.prototype = {
         this._searchQuery = query;
 
         InspectorBackend.performSearch(whitespaceTrimmedQuery, false);
-    },
-
-    searchingForNodeWasEnabled: function()
-    {
-        this._nodeSearchButton.toggled = true;
-    },
-
-    searchingForNodeWasDisabled: function()
-    {
-        this._nodeSearchButton.toggled = false;
     },
 
     populateHrefContextMenu: function(contextMenu, event, anchorElement)
@@ -1161,12 +1151,29 @@ WebInspector.ElementsPanel.prototype = {
         this.treeOutline.updateSelection();
     },
 
+    updateFocusedNode: function(nodeId)
+    {
+        var node = WebInspector.domAgent.nodeForId(nodeId);
+        if (!node)
+            return;
+
+        this.focusedDOMNode = node;
+        this._nodeSearchButton.toggled = false;
+    },
+
+    _setSearchingForNode: function(enabled)
+    {
+        this._nodeSearchButton.toggled = enabled;
+    },
+
+    setSearchingForNode: function(enabled)
+    {
+        InspectorBackend.setSearchingForNode(enabled, this._setSearchingForNode.bind(this));
+    },
+
     toggleSearchingForNode: function()
     {
-        if (!this._nodeSearchButton.toggled)
-            InspectorBackend.enableSearchingForNode();
-        else
-            InspectorBackend.disableSearchingForNode();
+        this.setSearchingForNode(!this._nodeSearchButton.toggled);
     },
 
     elementsToRestoreScrollPositionsFor: function()
