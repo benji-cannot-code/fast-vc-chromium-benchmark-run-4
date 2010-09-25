f09ba106d4e2619112c2302249b8656ca9263fca
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-25 20:42:46 +0000

    2010-09-25  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Tony Chang.
    
            FormatBlockCommand's modifyRange and doApply should be merged
            https://bugs.webkit.org/show_bug.cgi?id=46504
    
            Isolated the code in doApply to insert and remove lists for single paragraph into doApplyForSingleParagraph.
            Merged the code in modifyRange into doApply and cleaned up.
    
            No new tests are added since this is a cleanup.
    
            * editing/FormatBlockCommand.cpp:
            (WebCore::FormatBlockCommand::doApply): Merged with modifyRange; calls doApplyForSingleParagraph.
            (WebCore::FormatBlockCommand::doApplyForSingleParagraph): Added.
            * editing/FormatBlockCommand.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/FormatBlockCommand.h
+++ third_party/WebKit/WebCore/editing/FormatBlockCommand.h
@@ -42,9 +42,9 @@ private:
     FormatBlockCommand(Document*, const AtomicString& tagName);
 
     virtual void doApply();
+    void doApplyForSingleParagraph();
     virtual EditAction editingAction() const { return EditActionFormatBlock; }
 
-    bool modifyRange();
     AtomicString m_tagName;
 };
 
