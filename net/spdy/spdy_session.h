a26ae2d393ce0fc77d35d3071172c0954fb8f407
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-25 21:20:27 +0000

    Remove the references to HttpNetworkSession in SpdySession and replace it with a reference to the SpdySessionPool and SpdySettingStorage, which are actually required.
    Modified SpdySessionPool to take a pointer to 
    SpdySettingsStorage instead of to HttpNetworkSession 
    in both GetSpdySessionFromSocket() Get().
    
    Modified HttpStreamRequest (and various unit tests) accordingly
    when calling these methods.
    
    Review URL: http://codereview.chromium.org/3400024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60590 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.h
+++ net/spdy/spdy_session.h
@@ -40,9 +40,9 @@ namespace net {
 const int kMss = 1430;
 const int kMaxSpdyFrameChunkSize = (2 * kMss) - spdy::SpdyFrame::size();
 
-class SpdyStream;
-class HttpNetworkSession;
 class BoundNetLog;
+class SpdySettingsStorage;
+class SpdyStream;
 class SSLInfo;
 
 class SpdySession : public base::RefCounted<SpdySession>,
@@ -54,7 +54,8 @@ class SpdySession : public base::RefCounted<SpdySession>,
   // |session| is the HttpNetworkSession.  |net_log| is the NetLog that we log
   // network events to.
   SpdySession(const HostPortProxyPair& host_port_proxy_pair,
-              HttpNetworkSession* session,
+              SpdySessionPool* spdy_session_pool,
+              SpdySettingsStorage* spdy_settings,
               NetLog* net_log);
 
   const HostPortPair& host_port_pair() const {
@@ -287,7 +288,8 @@ class SpdySession : public base::RefCounted<SpdySession>,
   // The domain this session is connected to.
   const HostPortProxyPair host_port_proxy_pair_;
 
-  scoped_refptr<HttpNetworkSession> session_;
+  scoped_refptr<SpdySessionPool> spdy_session_pool_;
+  SpdySettingsStorage* spdy_settings_;
 
   // The socket handle for this session.
   scoped_ptr<ClientSocketHandle> connection_;
--- net/spdy/spdy_session_pool.h
+++ net/spdy/spdy_session_pool.h
@@ -31,6 +31,7 @@ class BoundNetLog;
 class ClientSocketHandle;
 class HttpNetworkSession;
 class SpdySession;
+class SpdySettingsStorage;
 
 // This is a very simple pool for open SpdySessions.
 // TODO(mbelshe): Make this production ready.
@@ -45,7 +46,7 @@ class SpdySessionPool
   // use.
   scoped_refptr<SpdySession> Get(
       const HostPortProxyPair& host_port_proxy_pair,
-      HttpNetworkSession* session,
+      SpdySettingsStorage* spdy_settings,
       const BoundNetLog& net_log);
 
   // Set the maximum concurrent sessions per domain.
@@ -66,7 +67,7 @@ class SpdySessionPool
   // Returns an error on failure, and |spdy_session| will be NULL.
   net::Error GetSpdySessionFromSocket(
       const HostPortProxyPair& host_port_proxy_pair,
-      HttpNetworkSession* session,
+      SpdySettingsStorage* spdy_settings,
       ClientSocketHandle* connection,
       const BoundNetLog& net_log,
       int certificate_error_code,
