be5f3928cb0eb7248e592a9459c48d1edbc90bd8
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-25 03:05:59 +0000

    One more build fix.
    * UIProcess/API/qt/qwkpage.cpp:
    (QWKPagePrivate::setEditCommandState):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68324 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -167,6 +167,10 @@ void QWKPagePrivate::wheelEvent(QGraphicsSceneWheelEvent* ev)
     page->wheelEvent(wheelEvent);
 }
 
+void QWKPagePrivate::setEditCommandState(const WTF::String&, bool, int)
+{
+}
+
 void QWKPagePrivate::updateAction(QWKPage::WebAction action)
 {
 #ifdef QT_NO_ACTION
@@ -413,10 +417,6 @@ void QWKPage::setViewportSize(const QSize& size)
 {
     if (d->page->drawingArea())
         d->page->drawingArea()->setSize(IntSize(size));
-}
-
-void QWKPage::setEditCommandState(const WTF::String&, bool, int)
-{
 }
 
 #ifndef QT_NO_ACTION
