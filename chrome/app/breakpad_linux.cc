3b97d709ff5aa99a5ce6e60a1d326d790a4399c6
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-26 05:27:51 +0000

    Fix Linux Official build breakage after Breakpad r700 roll.
    BUG=none
    TEST=none
    TBR=mark
    Review URL: http://codereview.chromium.org/3475022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60598 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_linux.cc
+++ chrome/app/breakpad_linux.cc
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "breakpad/src/client/linux/handler/exception_handler.h"
 #include "breakpad/src/client/linux/minidump_writer/directory_reader.h"
 #include "breakpad/src/common/linux/linux_libc_support.h"
-#include "breakpad/src/common/linux/memory.h"
+#include "breakpad/src/common/memory.h"
 #include "chrome/common/child_process_logging.h"
 #include "chrome/common/chrome_descriptors.h"
 #include "chrome/common/chrome_paths.h"
