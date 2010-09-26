a952950872d120aa70dc2585ef6d6ad36bd47af8
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-26 17:39:34 +0000

    2010-09-26  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Dan Bernstein.
    
            Canvas: Crash when setting a font with size in 'ex' units
            https://bugs.webkit.org/show_bug.cgi?id=46538
    
            update() the style's font after setting the style's font description.
            Needed because CSSPrimitiveValue::computeLengthDouble() later assumes
            that the style's font is properly initialized (for xHeight().)
    
            Fixes crash on IE test center's canvas-text-font-002 test.
    
            * html/canvas/CanvasRenderingContext2D.cpp:
            (WebCore::CanvasRenderingContext2D::setFont):
    2010-09-26  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Dan Bernstein.
    
            Canvas: Crash when setting a font with size in 'ex' units
            https://bugs.webkit.org/show_bug.cgi?id=46538
    
            * fast/canvas/canvas-font-ex-units-crash-expected.txt: Added.
            * fast/canvas/canvas-font-ex-units-crash.html: Added.
            * fast/canvas/script-tests/canvas-font-ex-units-crash.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-font-ex-units-crash.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test that setting a font with size in 'ex' units doesn't crash.");
+
+ctx = document.createElement('canvas').getContext('2d');
+
+ctx.font = "5ex sans-serif";
+shouldBe("ctx.font = '5ex sans-serif'; ctx.font", "'5ex sans-serif'");
+
+var successfullyParsed = true;
