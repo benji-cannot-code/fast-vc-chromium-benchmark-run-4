92688067e8ec22c5f726934930dab847c601c2b9
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-27 22:12:32 +0000

    Fix nits
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3381027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60715 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/SkThread_chrome.cc
+++ skia/ext/SkThread_chrome.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,7 +19,7 @@ int32_t sk_atomic_inc(int32_t* addr) {
 }
 
 int32_t sk_atomic_dec(int32_t* addr) {
-  // sk_atomic_inc is expected to return the old value, Barrier_AtomicIncrement
+  // sk_atomic_dec is expected to return the old value, Barrier_AtomicIncrement
   // returns the new value.
   return base::subtle::Barrier_AtomicIncrement(addr, -1) + 1;
 }
