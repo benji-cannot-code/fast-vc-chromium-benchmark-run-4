d2586078115770833051c86f71328efade7bb9ff
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-27 23:25:40 +0000

    Fix typo in a function name.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3467017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60727 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/url_fetcher_unittest.cc
+++ chrome/common/net/url_fetcher_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -337,7 +337,7 @@ void URLFetcherProtectTestPassedThrough::CreateFetcher(const GURL& url) {
   fetcher_ = new URLFetcher(url, URLFetcher::GET, this);
   fetcher_->set_request_context(new TestURLRequestContextGetter(
       io_message_loop_proxy()));
-  fetcher_->set_automatcally_retry_on_5xx(false);
+  fetcher_->set_automatically_retry_on_5xx(false);
   start_time_ = Time::Now();
   fetcher_->Start();
 }
