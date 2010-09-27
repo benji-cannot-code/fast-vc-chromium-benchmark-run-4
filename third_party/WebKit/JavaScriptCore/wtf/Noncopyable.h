24b431bad75f33b544ad176bbd4b90a1d22d1976
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-27 20:02:23 +0000

    Try to fix the Windows build.
    * wtf/Noncopyable.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Noncopyable.h
+++ third_party/WebKit/JavaScriptCore/wtf/Noncopyable.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     #define WTF_MAKE_NONCOPYABLE(ClassName) \
         private: \
             ClassName(const ClassName&); \
-            ClassName& operator=(const ClassName&);
+            ClassName& operator=(const ClassName&)
 #endif
 
 // We don't want argument-dependent lookup to pull in everything from the WTF
