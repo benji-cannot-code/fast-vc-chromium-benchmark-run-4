f30c65be4d54d6f207636b8a3741a4238e2f7489
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-27 17:53:51 +0000

    2010-09-27  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] use ENABLE(GLIB_SUPPORT)
            https://bugs.webkit.org/show_bug.cgi?id=46630
    
            Enabling GLIB_SUPPORT on all ports that use GLib to simplify
            checks.
    
            * GNUmakefile.am: Enabled the GLIB_SUPPORT define.
    
    JavaScriptCore:
    
            Reviewed by Martin Robinson.
    
            [GTK] use ENABLE(GLIB_SUPPORT)
            https://bugs.webkit.org/show_bug.cgi?id=46630
    
            * wtf/Platform.h: Include GTypedefs.h only if glib support
            is explicitly enabled.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -1128,7 +1128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_BRANCH_COMPACTION 1
 #endif
 
-#if PLATFORM(GTK) || (PLATFORM(EFL) && ENABLE(GLIB_SUPPORT))
+#if ENABLE(GLIB_SUPPORT)
 #include "GTypedefs.h"
 #endif
 
