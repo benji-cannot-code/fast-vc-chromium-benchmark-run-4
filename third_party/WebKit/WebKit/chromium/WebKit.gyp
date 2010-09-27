d1a77cf2c4a880ffba8bfc109efc68d72da7f739
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-27 22:51:47 +0000

    2010-09-24  Zhenyao Mo  <zmo@google.com>
            Reviewed by Kenneth Russell.
    
            Hook up ANGLE with chromium --in-process-webgl port
            https://bugs.webkit.org/show_bug.cgi?id=44309
    
            * WebKit.gyp: Add angle dependency.
            * src/WebGraphicsContext3DDefaultImpl.cpp: Using angle for shader validation and translation.
            (WebKit::WebGraphicsContext3DDefaultImpl::WebGraphicsContext3DDefaultImpl):
            (WebKit::WebGraphicsContext3DDefaultImpl::~WebGraphicsContext3DDefaultImpl):
            (WebKit::WebGraphicsContext3DDefaultImpl::initialize):
            (WebKit::WebGraphicsContext3DDefaultImpl::compileShader):
            (WebKit::WebGraphicsContext3DDefaultImpl::getIntegerv): Using defined const instead.
            (WebKit::WebGraphicsContext3DDefaultImpl::getShaderiv):
            (WebKit::WebGraphicsContext3DDefaultImpl::getShaderInfoLog):
            (WebKit::WebGraphicsContext3DDefaultImpl::getShaderSource):
            (WebKit::WebGraphicsContext3DDefaultImpl::shaderSource):
            (WebKit::WebGraphicsContext3DDefaultImpl::createShader):
            (WebKit::WebGraphicsContext3DDefaultImpl::deleteShader):
            (WebKit::WebGraphicsContext3DDefaultImpl::angleCreateCompilers):
            (WebKit::WebGraphicsContext3DDefaultImpl::angleDestroyCompilers):
            (WebKit::WebGraphicsContext3DDefaultImpl::angleValidateShaderSource):
            * src/WebGraphicsContext3DDefaultImpl.h: Add necessary data and functions for angle stuff.
            (WebKit::WebGraphicsContext3DDefaultImpl::ShaderSourceEntry::ShaderSourceEntry):
            (WebKit::WebGraphicsContext3DDefaultImpl::ShaderSourceEntry::~ShaderSourceEntry):
    2010-09-24  Zhenyao Mo  <zmo@google.com>
    
            Reviewed by Kenneth Russell.
    
            Hook up ANGLE with chromium --in-process-webgl port
            https://bugs.webkit.org/show_bug.cgi?id=44309
    
            * platform/graphics/GraphicsContext3D.h: Move Angle stuff inside MAC only section.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/WebKit.gyp
+++ third_party/WebKit/WebKit/chromium/WebKit.gyp
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 '<(chromium_src_dir)/app/app.gyp:app_base', # For GLContext
                 '<(chromium_src_dir)/skia/skia.gyp:skia',
                 '<(chromium_src_dir)/third_party/npapi/npapi.gyp:npapi',
+                '<(chromium_src_dir)/third_party/angle/src/build_angle.gyp:translator_glsl',
             ],
             'export_dependent_settings': [
                 '<(chromium_src_dir)/skia/skia.gyp:skia',
@@ -85,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'include_dirs': [
                 'public',
                 'src',
+                '<(chromium_src_dir)/third_party/angle/include',
             ],
             'defines': [
                 'WEBKIT_IMPLEMENTATION=1',
--- third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.h
@@ -34,8 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(3D_CANVAS)
 
+#include "GLSLANG/ShaderLang.h"
 #include "WebGraphicsContext3D.h"
 
+#include <wtf/HashMap.h>
 #include <wtf/ListHashSet.h>
 #include <wtf/OwnPtr.h>
 
@@ -301,8 +303,7 @@ private:
     // need to in order to verify that all enabled vertex arrays have
     // a valid buffer bound -- to avoid crashes on certain cards.
     unsigned int m_boundArrayBuffer;
-    class VertexAttribPointerState {
-    public:
+    struct VertexAttribPointerState {
         VertexAttribPointerState();
 
         bool enabled;
@@ -324,6 +325,44 @@ private:
     ListHashSet<unsigned long> m_syntheticErrors;
 
     OwnPtr<gfx::GLContext> m_glContext;
+
+    // ANGLE related.
+    struct ShaderSourceEntry {
+        ShaderSourceEntry()
+                : type(0)
+                , source(0)
+                , log(0)
+                , translatedSource(0)
+                , isValid(false)
+        {
+        }
+
+        ~ShaderSourceEntry()
+        {
+            if (source)
+                fastFree(source);
+            if (log)
+                fastFree(log);
+            if (translatedSource)
+                fastFree(translatedSource);
+        }
+
+        unsigned long type;
+        char* source;
+        char* log;
+        char* translatedSource;
+        bool isValid;
+    };
+
+    bool angleCreateCompilers();
+    void angleDestroyCompilers();
+    bool angleValidateShaderSource(ShaderSourceEntry& entry);
+
+    typedef HashMap<WebGLId, ShaderSourceEntry> ShaderSourceMap;
+    ShaderSourceMap m_shaderSourceMap;
+
+    ShHandle m_fragmentCompiler;
+    ShHandle m_vertexCompiler;
 };
 
 } // namespace WebKit
