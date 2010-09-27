8f824d91e907d89ccea479870cb4c36cbdb9d999
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-27 12:31:35 +0000

    2010-09-27  Andrey Kosyakov  <caseq@chromium.org>
            Unreviewed build fix (win; broken in r68371)
    
            * WebFrame.cpp: remove include <WebCore/ResourceHandleWin.h>
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebFrame.cpp
+++ third_party/WebKit/WebKit/win/WebFrame.cpp
@@ -90,7 +90,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/PluginView.h>
 #include <WebCore/PrintContext.h>
 #include <WebCore/ResourceHandle.h>
-#include <WebCore/ResourceHandleWin.h>
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/RenderView.h>
 #include <WebCore/RenderTreeAsText.h>
