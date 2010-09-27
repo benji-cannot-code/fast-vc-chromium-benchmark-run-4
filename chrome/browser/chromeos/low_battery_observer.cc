3868eecdd704f752d9ed0a3a57d58117a569f762
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-27 20:06:39 +0000

    Make feedback failure notification non sticky
    BUG=chromium-os:6368
    TEST=none
    
    Review URL: http://codereview.chromium.org/3412033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60696 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/low_battery_observer.cc
+++ chrome/browser/chromeos/low_battery_observer.cc
@@ -73,7 +73,7 @@ void LowBatteryObserver::PowerChanged(PowerLibrary* object) {
 
 void LowBatteryObserver::Show(base::TimeDelta remaining, bool urgent) {
   notification_.Show(l10n_util::GetStringFUTF16(IDS_LOW_BATTERY_MESSAGE,
-      TimeFormat::TimeRemaining(remaining)), urgent);
+      TimeFormat::TimeRemaining(remaining)), urgent, true);
   remaining_ = remaining.InMinutes();
 }
 
@@ -82,4 +82,3 @@ void LowBatteryObserver::Hide() {
 }
 
 }  // namespace chromeos
-
