33b1eb35c3d400724f597662501d75723663bf55
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-28 14:23:04 +0000

    2010-09-28  Benjamin Poulain  <benjamin.poulain@nokia.com>
            Reviewed by Andreas Kling.
    
            [Qt] Remove support for Qt 4.5
            https://bugs.webkit.org/show_bug.cgi?id=46718
    
            Remove the code for versions of Qt prior to 4.6.
    
            * UIProcess/API/qt/qgraphicswkview.cpp:
            (QGraphicsWKView::event):
            * UIProcess/API/qt/qwkpage.cpp:
            * UIProcess/API/qt/qwkpage_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -36,13 +36,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QApplication>
 #include <QGraphicsSceneMouseEvent>
 #include <QStyle>
+#include <QTouchEvent>
 #include <QtDebug>
 #include <WebKit2/WKFrame.h>
 #include <WebKit2/WKRetainPtr.h>
 
-#if QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
-#include <QTouchEvent>
-#endif
 
 using namespace WebKit;
 using namespace WebCore;
@@ -230,15 +228,11 @@ void QWKPagePrivate::_q_webActionTriggered(bool checked)
 }
 #endif // QT_NO_ACTION
 
-#if QT_VERSION >= QT_VERSION_CHECK(4, 6, 0)
-
 void QWKPagePrivate::touchEvent(QTouchEvent* event)
 {
     WebTouchEvent touchEvent = WebEventFactory::createWebTouchEvent(event);
     page->handleTouchEvent(touchEvent);
 }
-
-#endif
 
 QWKPage::QWKPage(WKPageNamespaceRef namespaceRef)
     : d(new QWKPagePrivate(this, namespaceRef))
