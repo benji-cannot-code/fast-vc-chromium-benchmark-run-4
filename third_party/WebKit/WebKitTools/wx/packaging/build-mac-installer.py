5ad4dc877f9b70a73d732e2470e1af267a014a2e
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-28 16:17:17 +0000

    [wx] Build fixes for recent changes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@68524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/wx/packaging/build-mac-installer.py
+++ third_party/WebKit/WebKitTools/wx/packaging/build-mac-installer.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Script for building Mac .pkg installer
 
 import commands
+import datetime
 import distutils.sysconfig
 import glob
 import optparse
@@ -49,11 +50,11 @@ wxwebkit_dir = os.path.abspath(os.path.join(wxwk_root, "WebKitBuild", get_config
 wx_version = wx.__version__[:5]
 py_version = sys.version[:3]
 
-wxwk_version = svn_revision()
+date = str(datetime.date.today())
 
 platform = "osx"
     
-pkgname = "wxWebKit-%s-wx%s-py%s" % (platform, wx_version[:3], py_version)
+pkgname = "wxWebKit-%s-wx%s-py%s-%s" % (platform, wx_version[:3], py_version, date)
 
 tempdir = "/tmp/%s" % (pkgname)
 
@@ -142,7 +143,7 @@ try:
     
         pkg_args = ['--title ' + pkgname,
                     '--out %s.pkg' % pkgname,
-                    '--version ' + wxwk_version.strip(),
+                    '--version ' + date.strip(),
                     '--id org.wxwebkit.wxwebkit',
                     '--domain system',
                     '--root-volume-only',
