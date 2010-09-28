85648ccdb537513b0276282411b4f3575b8157a3
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-28 00:31:09 +0000

    [Qt] Remove setDeviceSize methods https://bugs.webkit.org/show_bug.cgi?id=46347
    Reviewed by Antonio Gomes.
    
    Remove our setDeviceHeight() API and obtain it automatically from the
    system. Also for testing purposes, make it possible to override the
    values via two newly introduced environment variables.
    
    * Api/qgraphicswebview.cpp:
    * Api/qgraphicswebview.h:
    * Api/qwebpage.cpp:
    (getintenv):
    (queryDeviceSizeForScreenContainingWidget):
    (QWebPage::viewportConfigurationForSize):
    * WebCoreSupport/PageClientQt.cpp:
    (WebCore::PageClientQGraphicsWidget::windowRect):
    * symbian/eabi/QtWebKitu.def:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68457 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
+++ third_party/WebKit/WebKit/qt/Api/qgraphicswebview.h
@@ -42,7 +42,6 @@ class QWEBKIT_EXPORT QGraphicsWebView : public QGraphicsWidget {
     Q_PROPERTY(QString title READ title NOTIFY titleChanged)
     Q_PROPERTY(QIcon icon READ icon NOTIFY iconChanged)
     Q_PROPERTY(qreal zoomFactor READ zoomFactor WRITE setZoomFactor)
-    Q_PROPERTY(QSize deviceSize READ deviceSize WRITE setDeviceSize)
 
     Q_PROPERTY(QUrl url READ url WRITE setUrl NOTIFY urlChanged)
 
@@ -65,9 +64,6 @@ public:
 
     qreal zoomFactor() const;
     void setZoomFactor(qreal);
-
-    QSize deviceSize() const;
-    void setDeviceSize(const QSize&);
 
     bool isModified() const;
 
