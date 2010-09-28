8e664024046fc0de197ba7a9f6811a9dd3379e8c
allanwoj@chromium.org <allanwoj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-28 08:47:19 +0000

    Change name of PollingPolicyInterface.
    Renamed PollingPolicyInterface to GatewayPollingPolicyInterface as there was a name conflict with one in wifi_data_provider_common.h
    
    BUG=None
    TEST=Chrome compiles.
    
    Review URL: http://codereview.chromium.org/3453026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/gateway_data_provider_common.cc
+++ chrome/browser/geolocation/gateway_data_provider_common.cc
@@ -86,6 +86,6 @@ void GatewayDataProviderCommon::ScheduleNextScan(int interval) {
           &GatewayDataProviderCommon::DoRouterScanTask), interval);
 }
 
-PollingPolicyInterface* GatewayDataProviderCommon::NewPollingPolicy() {
-  return new GenericPollingPolicy;
+GatewayPollingPolicyInterface* GatewayDataProviderCommon::NewPollingPolicy() {
+  return new GenericGatewayPollingPolicy;
 }
--- chrome/browser/geolocation/gateway_data_provider_common_unittest.cc
+++ chrome/browser/geolocation/gateway_data_provider_common_unittest.cc
@@ -58,7 +58,7 @@ class MessageLoopQuitListener
   DeviceDataProvider<GatewayData>* provider_;
 };
 
-class MockGatewayPollingPolicy : public PollingPolicyInterface {
+class MockGatewayPollingPolicy : public GatewayPollingPolicyInterface {
  public:
   MockGatewayPollingPolicy() {
     ON_CALL(*this, PollingInterval())
@@ -67,7 +67,7 @@ class MockGatewayPollingPolicy : public PollingPolicyInterface {
         .WillByDefault(Return(1));
   }
 
-  // PollingPolicyInterface
+  // GatewayPollingPolicyInterface
   MOCK_METHOD0(PollingInterval, int());
   MOCK_METHOD0(NoRouterInterval, int());
  };
@@ -84,7 +84,7 @@ class GatewayDataProviderCommonWithMock : public GatewayDataProviderCommon {
     CHECK(new_gateway_api_ != NULL);
     return new_gateway_api_.release();
   }
-  virtual PollingPolicyInterface* NewPollingPolicy() {
+  virtual GatewayPollingPolicyInterface* NewPollingPolicy() {
     CHECK(new_polling_policy_ != NULL);
     return new_polling_policy_.release();
   }
