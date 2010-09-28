9d2a4ef582901a8eba371b9858b0762ef2b71568
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-28 00:25:06 +0000

    Bug 46680 - Inlining string concatenation can regress interpreter performance <rdar://problem/8362752> REGRESSION: ~6.4% sunspider regression in interpreter Do not inline calls to string concatenation in the interpret loop.
    Reviewed by Darin Adler.
    
    * interpreter/Interpreter.cpp:
    (JSC::concatenateStrings):
    (JSC::Interpreter::privateExecute):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/JavaScriptCore/interpreter/Interpreter.cpp
@@ -83,6 +83,11 @@ static int depth(CodeBlock* codeBlock, ScopeChain& sc)
 }
 
 #if ENABLE(INTERPRETER) 
+static NEVER_INLINE JSValue concatenateStrings(ExecState* exec, Register* strings, unsigned count)
+{
+    return jsString(exec, strings, count);
+}
+
 NEVER_INLINE bool Interpreter::resolve(CallFrame* callFrame, Instruction* vPC, JSValue& exceptionValue)
 {
     int dst = vPC[1].u.operand;
@@ -4302,7 +4307,7 @@ skip_id_custom_self:
         int src = vPC[2].u.operand;
         int count = vPC[3].u.operand;
 
-        callFrame->r(dst) = jsString(callFrame, &callFrame->registers()[src], count);
+        callFrame->r(dst) = concatenateStrings(callFrame, &callFrame->registers()[src], count);
         CHECK_FOR_EXCEPTION();
         vPC += OPCODE_LENGTH(op_strcat);
 
