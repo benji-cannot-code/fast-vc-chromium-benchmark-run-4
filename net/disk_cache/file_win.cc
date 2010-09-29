c14e5bb1f11300aae125524a3edf77a4225af8f8
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 21:00:35 +0000

    Disk cache: Change the way we delete files.
    Now we always share delete and request delete access when we
    open the files, and don't close the handle before deleting a
    file. The result is that the delete operation should not be
    prevented by someone opening the file right after we close
    our handle.
    
    The downside is that anybody attempting to open one of our
    files while the browser is running will have to also share
    delete or their request will fail. On the other hand, we
    don't really want other people modifying the files at the
    same time, so the change may be for good.
    
    The caveat is that I think most of the failures are result
    of improper AV behavior, so we have to see what happens.
    
    BUG=16723
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/3475025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60989 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/file_win.cc
+++ net/disk_cache/file_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2006-2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -73,9 +73,9 @@ bool File::Init(const FilePath& name) {
   if (init_)
     return false;
 
-  platform_file_ = CreateFile(name.value().c_str(),
-                              GENERIC_READ | GENERIC_WRITE,
-                              FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+  DWORD sharing = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE;
+  DWORD access = GENERIC_READ | GENERIC_WRITE | DELETE;
+  platform_file_ = CreateFile(name.value().c_str(), access, sharing, NULL,
                               OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 
   if (INVALID_HANDLE_VALUE == platform_file_)
@@ -85,9 +85,7 @@ bool File::Init(const FilePath& name) {
       platform_file_, Singleton<CompletionHandler>::get());
 
   init_ = true;
-  sync_platform_file_  = CreateFile(name.value().c_str(),
-                                    GENERIC_READ | GENERIC_WRITE,
-                                    FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+  sync_platform_file_  = CreateFile(name.value().c_str(), access, sharing, NULL,
                                     OPEN_EXISTING, 0, NULL);
 
   if (INVALID_HANDLE_VALUE == sync_platform_file_)
