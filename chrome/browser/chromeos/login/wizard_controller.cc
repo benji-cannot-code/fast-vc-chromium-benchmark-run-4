df048a4db671aad45fd9ec12ec5a29faddfbddb5
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 10:48:19 +0000

    Disable wizard screen test accelerators in official builds.
    BUG=chromium-os:7166
    TEST=Wizard screen accelerators are not working in official build.
    
    Review URL: http://codereview.chromium.org/3455030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -79,22 +79,24 @@ const int kWaitForRebootTimeSec = 3;
 class ContentView : public views::View {
  public:
   ContentView()
-      : accel_account_screen_(views::Accelerator(app::VKEY_A,
-                                                 false, true, true)),
-        accel_login_screen_(views::Accelerator(app::VKEY_L,
-                                               false, true, true)),
-        accel_network_screen_(views::Accelerator(app::VKEY_N,
-                                                 false, true, true)),
-        accel_update_screen_(views::Accelerator(app::VKEY_U,
-                                                false, true, true)),
-        accel_image_screen_(views::Accelerator(app::VKEY_I,
-                                               false, true, true)),
-        accel_eula_screen_(views::Accelerator(app::VKEY_E,
-                                              false, true, true)),
-        accel_register_screen_(views::Accelerator(app::VKEY_R,
-                                                  false, true, true)),
-        accel_enable_accessibility_(
+      : accel_enable_accessibility_(
             WizardAccessibilityHelper::GetAccelerator()) {
+    AddAccelerator(accel_enable_accessibility_);
+#if !defined(OFFICIAL_BUILD)
+    accel_account_screen_ = views::Accelerator(app::VKEY_A,
+                                               false, true, true);
+    accel_login_screen_ = views::Accelerator(app::VKEY_L,
+                                             false, true, true);
+    accel_network_screen_ = views::Accelerator(app::VKEY_N,
+                                               false, true, true);
+    accel_update_screen_ = views::Accelerator(app::VKEY_U,
+                                              false, true, true);
+    accel_image_screen_ = views::Accelerator(app::VKEY_I,
+                                             false, true, true);
+    accel_eula_screen_ = views::Accelerator(app::VKEY_E,
+                                            false, true, true);
+    accel_register_screen_ = views::Accelerator(app::VKEY_R,
+                                                false, true, true);
     AddAccelerator(accel_account_screen_);
     AddAccelerator(accel_login_screen_);
     AddAccelerator(accel_network_screen_);
@@ -102,7 +104,7 @@ class ContentView : public views::View {
     AddAccelerator(accel_image_screen_);
     AddAccelerator(accel_eula_screen_);
     AddAccelerator(accel_register_screen_);
-    AddAccelerator(accel_enable_accessibility_);
+#endif
   }
 
   ~ContentView() {
@@ -117,7 +119,11 @@ class ContentView : public views::View {
     if (!controller)
       return false;
 
-    if (accel == accel_account_screen_) {
+    if (accel == accel_enable_accessibility_) {
+      WizardAccessibilityHelper::GetInstance()->EnableAccessibility(
+          controller->contents());
+#if !defined(OFFICIAL_BUILD)
+    } else if (accel == accel_account_screen_) {
       controller->ShowAccountScreen();
     } else if (accel == accel_login_screen_) {
       controller->ShowLoginScreen();
@@ -131,9 +137,7 @@ class ContentView : public views::View {
       controller->ShowEulaScreen();
     } else if (accel == accel_register_screen_) {
       controller->ShowRegistrationScreen();
-    } else if (accel == accel_enable_accessibility_) {
-      WizardAccessibilityHelper::GetInstance()->EnableAccessibility(
-          controller->contents());
+#endif
     } else {
       return false;
     }
@@ -152,6 +156,7 @@ class ContentView : public views::View {
  private:
   scoped_ptr<views::Painter> painter_;
 
+#if !defined(OFFICIAL_BUILD)
   views::Accelerator accel_account_screen_;
   views::Accelerator accel_login_screen_;
   views::Accelerator accel_network_screen_;
@@ -159,6 +164,7 @@ class ContentView : public views::View {
   views::Accelerator accel_image_screen_;
   views::Accelerator accel_eula_screen_;
   views::Accelerator accel_register_screen_;
+#endif
   views::Accelerator accel_enable_accessibility_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentView);
