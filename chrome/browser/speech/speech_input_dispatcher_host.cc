e36b5bdef80ee14b830bfc9844eae573fa888941
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 08:53:39 +0000

    Get the speech input browser test working again.
    This test was having issues in Mac and was disabled. After that we also added a webkit change
    to not allow programmatic events from Javascript to trigger speech input, so this test was not
    working anymore.
    
    In this CL I have rewritten the test to send the mouse click via the RenderView and this
    should fix the flakiness and get it working again.
    
    BUG=51337
    TEST=SpeechInputBrowserTest.TestBasicRecognition on all platforms.
    
    Review URL: http://codereview.chromium.org/3446029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60916 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_input_dispatcher_host.cc
+++ chrome/browser/speech/speech_input_dispatcher_host.cc
@@ -138,8 +138,6 @@ void SpeechInputDispatcherHost::OnStartRecognition(
     int render_view_id,
     int request_id,
     const gfx::Rect& element_rect) {
-  LOG(INFO) << "SpeechInputDispatcherHost: start recognition"
-            << render_view_id;
   int caller_id = callers_->CreateId(resource_message_filter_process_id_,
                                      render_view_id, request_id);
   manager()->StartRecognition(this, caller_id,
@@ -149,8 +147,6 @@ void SpeechInputDispatcherHost::OnStartRecognition(
 
 void SpeechInputDispatcherHost::OnCancelRecognition(int render_view_id,
                                                     int request_id) {
-  LOG(INFO) << "SpeechInputDispatcherHost: cancel recognition"
-            << render_view_id;
   int caller_id = callers_->GetId(resource_message_filter_process_id_,
                                  render_view_id, request_id);
   manager()->CancelRecognition(caller_id);
@@ -159,8 +155,6 @@ void SpeechInputDispatcherHost::OnCancelRecognition(int render_view_id,
 
 void SpeechInputDispatcherHost::OnStopRecording(int render_view_id,
                                                 int request_id) {
-  LOG(INFO) << "SpeechInputDispatcherHost: stop recording"
-            << render_view_id;
   int caller_id = callers_->GetId(resource_message_filter_process_id_,
                                   render_view_id, request_id);
   manager()->StopRecording(caller_id);
