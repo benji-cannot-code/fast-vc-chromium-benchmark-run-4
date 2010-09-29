b203b45c9fb466c0f595dc1389b09dce42d8739e
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 01:00:50 +0000

    Allow chrome to be customized for screen reader users.
    Add Widget::IsAccessibleWidget() that will return true when Chrome can be customized screen reader users.
    
    BUG=37357,55902
    TEST=JAWS or NVDA users have "(Incognito)" in their window title. Tablet users are unaffected.
    
    Review URL: http://codereview.chromium.org/3427026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60888 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/widget_gtk.cc
+++ views/widget/widget_gtk.cc
@@ -778,6 +778,10 @@ bool WidgetGtk::IsActive() const {
   return is_active_;
 }
 
+bool WidgetGtk::IsAccessibleWidget() const {
+  return false;
+}
+
 void WidgetGtk::GenerateMousePressedForView(View* view,
                                             const gfx::Point& point) {
   NOTIMPLEMENTED();
