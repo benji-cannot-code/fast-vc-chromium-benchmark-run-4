8e1f619d93e8dda908898644033ad9f1561ea881
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 09:53:58 +0000

    GTTF: remove flaky mark from following tests:
    - FilePathWatcherTest.DeleteAndRecreate (bug 54822)
    - FilePathWatcherTest.DirectoryChain (bug 54822)
    - FilePathWatcherTest.MoveChild (bug 54822)
    - FilePathWatcherTest.MoveParent (bug 54822)
    - FilePathWatcherTest.NonExistentDirectory (bug 54822)
    - AccountScreenTest.TestSchemeInspector (bug 46274)
    - SavePageBrowserTest.SaveCompleteHTML (bug 52292)
    - DownloadRequestLimiterTest.Allow (bug 39753)
    - PPAPITest.Graphics2D (bug 54150)
    - ExtensionApiTest.Storage (bug 42943)
    - BrowserEncodingTest.TestEncodingAliasMapping (bug 51716)
    - BrowserCrApplicationAppleScriptTest.InsertWindow (bug 53584)
    - ExtensionInstallUIBrowserTest.TestThemeInstallUndoResetsToDefault (bug 43441)
    
    TBR=mal
    
    BUG=54822, 46274, 52292, 39753, 54150, 42943, 51716, 53584, 43441
    TEST=none
    
    Review URL: http://codereview.chromium.org/3545003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_storage_apitest.cc
+++ chrome/browser/extensions/extension_storage_apitest.cc
@@ -5,13 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-#if defined(OS_LINUX) || defined(OS_MACOSX)
-// See http://crbug.com/42943.
-#define MAYBE_Storage FLAKY_Storage
-#else
-#define MAYBE_Storage Storage
-#endif
-
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Storage) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Storage) {
   ASSERT_TRUE(RunExtensionTest("storage")) << message_;
 }
