d234461e56f0f5d3c543328e28fad4c5bd6baf98
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-29 21:46:04 +0000

    2010-09-29  Mihai Parparita  <mihaip@chromium.org>
            Unreviewed removal of unused #define from WebHistory.h.
            Chromium usage of WEBKIT_BUG_40451_IS_FIXED was removed with
            http://crrev.com/50642.
    
            * public/WebHistoryItem.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebHistoryItem.h
+++ third_party/WebKit/WebKit/chromium/public/WebHistoryItem.h
@@ -35,9 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCommon.h"
 #include "WebPrivatePtr.h"
 
-// FIXME: Remove this once Chromium starts using itemSequenceNumber.
-#define WEBKIT_BUG_40451_IS_FIXED
-
 namespace WebCore { class HistoryItem; }
 
 namespace WebKit {
