4d0a5ccb576ce5de890d1dfe51fb1991719203c6
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 17:34:32 +0000

    VP8 decoder for chromoting
    Added DecoderVp8 and unit test for chromoting.
    
    TEST=remoting_unittests
    BUG=50235
    
    Review URL: http://codereview.chromium.org/3032047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60960 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/base/decoder_vp8_unittest.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/video_frame.h"
+#include "remoting/base/codec_test.h"
+#include "remoting/base/decoder_vp8.h"
+#include "remoting/base/encoder_vp8.h"
+#include "remoting/client/mock_objects.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+
+namespace remoting {
+
+TEST(DecoderVp8Test, EncodeAndDecode) {
+  EncoderVp8 encoder;
+  DecoderVp8 decoder;
+  TestEncoderDecoder(&encoder, &decoder, false);
+}
+
+}  // namespace remoting
