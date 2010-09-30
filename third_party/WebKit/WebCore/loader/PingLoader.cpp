45488fa76adf44c290eeb2a041ac5e6d90fc318c
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-30 12:47:55 +0000

    2010-09-30  Andreas Kling  <andreas.kling@nokia.com>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] V8 port: Unbreak build
    
            Add missing <wtf/text/CString.h> include.
    
            * WebCoreSupport/InspectorServerQt.cpp:
    2010-09-30  Andreas Kling  <andreas.kling@nokia.com>
    
            Reviewed by Tor Arne Vestbø.
    
            [Qt] V8 port: Unbreak build
    
            Add missing <wtf/text/CString.h> include.
    
            * loader/PingLoader.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/PingLoader.cpp
+++ third_party/WebKit/WebCore/loader/PingLoader.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SecurityOrigin.h"
 #include <wtf/OwnPtr.h>
 #include <wtf/UnusedParam.h>
+#include <wtf/text/CString.h>
 
 namespace WebCore {
 
