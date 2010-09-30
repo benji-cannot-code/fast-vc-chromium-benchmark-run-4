9fd2462eb306dd84956080801ea1fa14cdda1ab2
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-30 11:50:51 +0000

    Minor cleanup of MediaPlayerPrivateQt
    Reviewed by Simon Hausmann.
    
    * platform/graphics/qt/MediaPlayerPrivateQt.cpp:
    (WebCore::MediaPlayerPrivateQt::MediaPlayerPrivateQt):
    (WebCore::MediaPlayerPrivateQt::currentTime):
    (WebCore::MediaPlayerPrivateQt::positionChanged):
    (WebCore::MediaPlayerPrivateQt::paint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/MediaPlayerPrivateQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/MediaPlayerPrivateQt.cpp
@@ -126,8 +126,7 @@ MediaPlayerPrivateQt::MediaPlayerPrivateQt(MediaPlayer* player)
            this, SLOT(nativeSizeChanged(QSizeF)));
 
     // Grab the player control
-    QMediaService* service = m_mediaPlayer->service();
-    if (service) {
+    if (QMediaService* service = m_mediaPlayer->service()) {
         m_mediaPlayerControl = qobject_cast<QMediaPlayerControl *>(
                 service->requestControl(QMediaPlayerControl_iid));
     }
@@ -331,8 +330,7 @@ float MediaPlayerPrivateQt::duration() const
 
 float MediaPlayerPrivateQt::currentTime() const
 {
-    float currentTime = m_mediaPlayer->position() / 1000.0f;
-    return currentTime;
+    return m_mediaPlayer->position() / 1000.0f;
 }
 
 PassRefPtr<TimeRanges> MediaPlayerPrivateQt::buffered() const
@@ -467,7 +465,7 @@ void MediaPlayerPrivateQt::seekTimeout()
 
 void MediaPlayerPrivateQt::positionChanged(qint64)
 {
-    // Only propogate this event if we are seeking
+    // Only propagate this event if we are seeking
     if (m_isSeeking && m_queuedSeek == -1) {
         m_webCorePlayer->timeChanged();
         m_isSeeking = false;
@@ -574,10 +572,7 @@ void MediaPlayerPrivateQt::paint(GraphicsContext* context, const IntRect& rect)
     if (!m_isVisible)
         return;
 
-    // Grab the painter and widget
     QPainter* painter = context->platformContext();
-
-    // Render the video
     m_videoScene->render(painter, QRectF(QRect(rect)), m_videoItem->sceneBoundingRect());
 }
 
