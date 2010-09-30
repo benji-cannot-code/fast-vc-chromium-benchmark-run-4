ab96dce6c812fb47692049ff38bef6ad532641e2
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-09-30 18:32:42 +0000

    2010-09-30  Nico Weber  <thakis@chromium.org>
            Reviewed by Adam Barth.
    
            [chromium] Fix clang error in KURLTest.cpp
            https://bugs.webkit.org/show_bug.cgi?id=46861
    
            * tests/KURLTest.cpp:
            (WTF::operator<<):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/tests/KURLTest.cpp
+++ third_party/WebKit/WebKit/chromium/tests/KURLTest.cpp
@@ -38,13 +38,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "KURL.h"
 
-namespace {
-
+namespace WTF {
 // Output stream operator so gTest's macros work with WebCore strings.
-std::ostream& operator<<(std::ostream& out, const WTF::String& str)
+std::ostream& operator<<(std::ostream& out, const String& str)
 {
     return str.isEmpty() ? out : out << str.utf8().data();
 }
+} // namespace WTF
+
+namespace {
+
 
 struct ComponentCase {
     const char* url;
