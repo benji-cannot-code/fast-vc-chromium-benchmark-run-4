4f3d8e02d80a9c58593c9f670990e9f60eb66084
erikkay@chromium.org <erikkay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-30 18:05:29 +0000

    Revert 61074 - changed to flaky
    BUG=54332
    TBR=sky
    
    TBR=erikkay@chromium.org
    Review URL: http://codereview.chromium.org/3590008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_unittest.cc
+++ chrome/common/extensions/extension_unittest.cc
@@ -833,8 +833,7 @@ TEST(ExtensionTest, EffectiveHostPermissions) {
   EXPECT_TRUE(extension->HasEffectiveAccessToAllHosts());
 }
 
-// crbug.com/54332
-TEST(ExtensionTest, FLAKY_IsPrivilegeIncrease) {
+TEST(ExtensionTest, IsPrivilegeIncrease) {
   const struct {
     const char* base_name;
     bool expect_success;
