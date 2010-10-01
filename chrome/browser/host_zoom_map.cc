a6ec28a66414608d0ad048d3cefa90f50ae011ad
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 08:28:03 +0000

    Fix zoom unit test and add missing break in zoom map code. Review URL: http://codereview.chromium.org/3524006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/host_zoom_map.cc
+++ chrome/browser/host_zoom_map.cc
@@ -251,6 +251,7 @@ void HostZoomMap::Observe(
         if (prefs::kPerHostZoomLevels == *name)
           Load();
       }
+      break;
     }
     default:
       NOTREACHED() << "Unexpected preference observed.";
