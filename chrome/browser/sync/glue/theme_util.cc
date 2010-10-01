7482fbfaa5ea5892247ea03ada2e189192bb3c97
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 19:09:16 +0000

    Fix crash in ExtensionInstallUI::OnInstallSuccess when the active browser was not a tabbed browser. Also cleanup this area a bit.
    BUG=53476
    TEST=See bug
    
    Review URL: http://codereview.chromium.org/3602004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61212 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/theme_util.cc
+++ chrome/browser/sync/glue/theme_util.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/scoped_ptr.h"
+#include "chrome/browser/browser.h"
+#include "chrome/browser/browser_list.h"
 #include "chrome/browser/extensions/extension_install_ui.h"
 #include "chrome/browser/extensions/extension_updater.h"
 #include "chrome/browser/extensions/extensions_service.h"
@@ -120,9 +122,12 @@ void SetCurrentThemeFromThemeSpecifics(
       // just set the current theme to it.
       profile->SetTheme(extension);
       // Pretend the theme was just installed.
-      ExtensionInstallUI::ShowThemeInfoBar(
-          previous_theme_id, previous_use_system_theme,
-          extension, profile);
+      Browser* browser = BrowserList::GetLastActiveWithProfile(profile);
+      if (browser) {
+        ExtensionInstallUI::ShowThemeInfoBar(
+            browser, previous_theme_id, previous_use_system_theme,
+            extension, profile);
+      }
     } else {
       // No extension with this id exists -- we must install it; we do
       // so by adding it as a pending extension and then triggering an
