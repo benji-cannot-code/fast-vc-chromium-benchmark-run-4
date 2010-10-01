8a8cd852ce9d52f14a7d6653a15b74b84d9f86dd
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-01 16:16:03 +0000

    Implement NPN_GetValue(NPNVnetscapeWindow)
    Fixes <http://webkit.org/b/46726> <rdar://problem/8486319>
    Right-clicking on windowless Flash plugin in WebKit2 makes a context
    menu appear in the bottom-right corner of the screen
    
    Test: platform/win/plugins/get-value-netscape-window.html
    
    Reviewed by Anders Carlsson.
    
    WebKit2:
    
    * WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp:
    (WebKit::NPN_GetValue): Added case for NPNVnetscapeWindow that calls
    through to NetscapePlugin::containingWindow.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.h: Added containingWindow
    on Windows.
    
    * WebProcess/Plugins/Netscape/win/NetscapePluginWin.cpp:
    (WebKit::NetscapePlugin::containingWindow): Added. Calls through to
    the PluginController.
    (WebKit::NetscapePlugin::platformPostInitialize): Changed to use
    containingWindow.
    
    WebKitTools:
    
    Test NPN_GetValue(NPNVnetscapeWindow)
    
    * DumpRenderTree/TestNetscapePlugIn/PluginTest.cpp:
    (PluginTest::NPN_GetValue): Added. Calls through to the browser.
    
    * DumpRenderTree/TestNetscapePlugIn/PluginTest.h: Added NPN_GetValue.
    
    * DumpRenderTree/TestNetscapePlugIn/Tests/win/GetValueNetscapeWindow.cpp: Added.
    (GetValueNetscapeWindow::GetValueNetscapeWindow): Initialize members.
    (GetValueNetscapeWindow::NPP_SetWindow): Test that
    NPN_GetValue(NPNVnetscapeWindow) returns a valid HWND and that it
    isn't our HWND.
    
    * DumpRenderTree/TestNetscapePlugIn/win/TestNetscapePlugin.vcproj:
    Added GetValueNetscapeWindow.
    
    LayoutTests:
    
    Support for testing NPN_GetValue(NPNVnetscapeWindow)
    
    * platform/win/plugins/get-value-netscape-window-expected.txt: Added.
    * platform/win/plugins/get-value-netscape-window.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
+++ third_party/WebKit/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
@@ -56,6 +56,8 @@ public:
 #if PLATFORM(MAC)
     NPError setDrawingModel(NPDrawingModel);
     NPError setEventModel(NPEventModel);
+#elif PLATFORM(WIN)
+    HWND containingWindow() const;
 #endif
 
     void invalidate(const NPRect*);
