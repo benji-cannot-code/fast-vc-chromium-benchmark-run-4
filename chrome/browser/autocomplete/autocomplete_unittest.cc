d648631fbf55629c8cbea0a39f3c31551dae7ca8
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 10:12:25 +0000

    Fix autocomplete_unittest two-phase lookup problems.
    The operator<< for AutocompleteResult::const_iterator needs to be declared
    in the same namespace as its parameter type, in this case the global namespace.
    
    Otherwise Clang fails to compile this because it is more strict on the Standard.
    
    See http://clang.llvm.org/compatibility.html#dep_lookup for more info.
    
    BUG=none
    TEST=unit_tests
    
    Review URL: http://codereview.chromium.org/3591001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_unittest.cc
+++ chrome/browser/autocomplete/autocomplete_unittest.cc
@@ -18,6 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HISTORY_IDENTIFIER L"Chrome:History"
 #define SEARCH_IDENTIFIER L"google.com/websearch/en"
 
+static std::ostream& operator<<(std::ostream& os,
+                                const AutocompleteResult::const_iterator& it) {
+  return os << static_cast<const AutocompleteMatch*>(&(*it));
+}
+
 namespace {
 
 const size_t num_results_per_provider = 3;
@@ -167,11 +172,6 @@ void AutocompleteProviderTest::Observe(NotificationType type,
     result_.CopyFrom(*(Details<const AutocompleteResult>(details).ptr()));
     MessageLoop::current()->Quit();
   }
-}
-
-std::ostream& operator<<(std::ostream& os,
-                         const AutocompleteResult::const_iterator& iter) {
-  return os << static_cast<const AutocompleteMatch*>(&(*iter));
 }
 
 // Tests that the default selection is set properly when updating results.
