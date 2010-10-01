4ea24be721ed565102ca22923aa69fc4b692d344
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 13:15:50 +0000

    Aligns the network menu width with combo box width.
    BUG=chromium-os:7100
    TEST=Boot device. On "Select network" screen click on the network
    selection combo box. The networks menu that will appear has the same
    width as combo box itself.
    
    Review URL: http://codereview.chromium.org/3586005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61173 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_selection_view.cc
+++ chrome/browser/chromeos/login/network_selection_view.cc
@@ -67,6 +67,10 @@ const int kSelectionBoxHeight = 29;
 // horizontal size, eaten by those icons.
 const int kMenuHorizontalOffset = -1;
 
+// Constants were taken from 'views/controls/menu_button.cc'.
+const int kDefaultMenuOffsetX = -2;
+const int kDefaultMenuOffsetY = -4;
+
 // Vertical addition to the menu window to make it appear exactly below
 // MenuButton.
 const int kMenuVerticalOffset = 3;
@@ -211,6 +215,7 @@ void NetworkSelectionView::InitLayout() {
       2 * kPaddingColumnWidth - kMediumPaddingColumnWidth - widest_label;
   delegate_->language_switch_menu()->SetFirstLevelMenuWidth(
       dropdown_width - kMenuWidthOffset);
+  network_dropdown_->SetFirstLevelMenuWidth(dropdown_width - kMenuWidthOffset);
 
   // Define layout and column set for entire screen (welcome + screen).
   views::GridLayout* screen_layout = new views::GridLayout(this);
@@ -289,6 +294,7 @@ void NetworkSelectionView::Init() {
   languages_menubutton_->SetFocusable(true);
   languages_menubutton_->SetNormalHasBorder(true);
   // Menu is positioned by bottom right corner of the MenuButton.
+  // TODO(altimofeev): switch to the absolute offsets (button related)
   delegate_->language_switch_menu()->set_menu_offset(kMenuHorizontalOffset,
                                                      kMenuVerticalOffset);
 
@@ -298,6 +304,12 @@ void NetworkSelectionView::Init() {
   network_dropdown_ = new NetworkControlWithAccelerators(false,
                                                          GetNativeWindow(),
                                                          delegate_);
+
+  // TODO(altimofeev): switch to the absolute offsets (button related)
+  network_dropdown_->set_menu_offset(
+      kDefaultMenuOffsetX + kMenuHorizontalOffset,
+      kDefaultMenuOffsetY + kMenuVerticalOffset);
+
   network_dropdown_->SetNormalHasBorder(true);
   network_dropdown_->SetFocusable(true);
 
