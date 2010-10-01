8cf4e93ec8c92022fb2ff69a39f555fe551d6f9f
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 12:42:16 +0000

    Disable ExtensionApiTest.Popup. It's been timing out for the last 750+ runs.
    TBR=michaeln
    
    BUG=46601
    TEST=ExtensionApiTest.FLAKY_Popup no longer runs
    Review URL: http://codereview.chromium.org/3603003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_popup_apitest.cc
+++ chrome/browser/extensions/extension_popup_apitest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-// Flaky, http://crbug.com/46601.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_Popup) {
+// Times out. See http://crbug.com/46601.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Popup) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
 
