ac2aad1fd61ed775f3daf44c9cffd6cf6cc7a685
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-02 07:47:39 +0000

    Disable ExtensionApiTest.WebSocket
    Disabling this test so that we can make another WebKit roll to fix layout tests.
    This test can be enabled after we rolled and make changes to the test that
    points to a new directory.
    
    TBR=thakis
    
    BUG=57662
    TEST=None
    
    Review URL: http://codereview.chromium.org/3590013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #define MAYBE_WebSocket WebSocket
 #endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_WebSocket) {
+// This test is disabled because of http://crbug.com/57662.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WebSocket) {
   FilePath websocket_root_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &websocket_root_dir);
   websocket_root_dir = websocket_root_dir.AppendASCII("layout_tests")
