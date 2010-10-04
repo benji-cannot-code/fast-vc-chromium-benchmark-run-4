545580d2f9401f0d8366cb2038cf45be1df5e0f3
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-04 13:42:42 +0000

    2010-10-04  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: add total bar to the network panel.
            https://bugs.webkit.org/show_bug.cgi?id=47081
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleView.createDividerElement):
            * inspector/front-end/DataGrid.js:
            (WebInspector.DataGrid.prototype.removeChild):
            (WebInspector.DataGrid.prototype.sortNodes):
            (WebInspector.DataGrid.prototype._clickInHeaderCell):
            (WebInspector.DataGrid.prototype.markColumnAsSortedBy):
            * inspector/front-end/NetworkPanel.js:
            (WebInspector.NetworkPanel):
            (WebInspector.NetworkPanel.prototype.resize):
            (WebInspector.NetworkPanel.prototype._positionSummaryBar):
            (WebInspector.NetworkPanel.prototype._createTimelineGrid):
            (WebInspector.NetworkPanel.prototype._createSortingFunctions):
            (WebInspector.NetworkPanel.prototype._sortItems):
            (WebInspector.NetworkPanel.prototype._sortByTimeline):
            (WebInspector.NetworkPanel.prototype._createFilterStatusBarItems):
            (WebInspector.NetworkPanel.prototype._createSummaryBar):
            (WebInspector.NetworkPanel.prototype._updateSummaryBar):
            (WebInspector.NetworkPanel.prototype._updateFilter):
            (WebInspector.NetworkPanel.prototype.show):
            (WebInspector.NetworkPanel.prototype.refresh):
            (WebInspector.NetworkPanel.prototype.reset):
            (WebInspector.NetworkDataGridNode.SizeComparator):
            (WebInspector.NetworkDataGridNode.ResourcePropertyComparator):
            (WebInspector.NetworkTotalGridNode):
            (WebInspector.NetworkTotalGridNode.prototype.createCells):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype.createFilterPanel):
            * inspector/front-end/inspector.css:
            (.scope-bar-divider):
            * inspector/front-end/networkPanel.css:
            (.network-timeline-grid):
            (.network-summary-bar):
            (.network-summary-bar-bottom):
            (.data-grid td .network-summary-bar):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ConsoleView.js
+++ third_party/WebKit/WebCore/inspector/front-end/ConsoleView.js
@@ -65,7 +65,7 @@ WebInspector.ConsoleView = function(drawer)
 
     function createDividerElement() {
         var dividerElement = document.createElement("div");
-        dividerElement.addStyleClass("divider");
+        dividerElement.addStyleClass("scope-bar-divider");
         this.filterBarElement.appendChild(dividerElement);
     }
 
