5aa8d55dd1014fa149c4af9600f6a741135ed05d
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 15:42:34 +0000

    Fix a couple of warnings.
    This patch on behalf of Jon Leighton (leighton@cis.udel.edu)
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3550005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61376 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/flip_in_mem_edsm_server.cc
+++ net/tools/flip_server/flip_in_mem_edsm_server.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <dirent.h>
-#include <linux/tcp.h>  // For TCP_NODELAY
+#include <netinet/tcp.h>  // For TCP_NODELAY
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
