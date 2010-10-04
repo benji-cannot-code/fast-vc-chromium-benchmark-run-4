d7ac73e19450edaf4d902169dcc873cc5d8c39f7
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 18:50:01 +0000

    Remove cryptohome dir when removing user
    BUG=chromium-os:5860
    TEST=Remove user from home screen. Ensure that directory in /home/.shadow
    is also removed. Can also check if non-synced data no longer appears when
    same user is recreated. Downloads are an example of this.
    
    Review URL: http://codereview.chromium.org/3290023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61396 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/cros/cryptohome_library.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
 #include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/chromeos/cros_settings_provider_user.h"
@@ -51,6 +52,30 @@ const size_t kNotSelected = -1;
 // Offset of cursor in first position from edit left side. It's used to position
 // info bubble arrow to cursor.
 const int kCursorOffset = 5;
+
+// Used to handle the asynchronous response of deleting a cryptohome directory.
+class RemoveAttempt : public CryptohomeLibrary::Delegate {
+ public:
+  explicit RemoveAttempt(const std::string& user_email)
+      : user_email_(user_email) {
+    if (CrosLibrary::Get()->EnsureLoaded()) {
+      CrosLibrary::Get()->GetCryptohomeLibrary()->AsyncRemove(
+          user_email_, this);
+    }
+  }
+
+  void OnComplete(bool success, int return_code) {
+    // Log the error, but there's not much we can do.
+    if (!success) {
+      LOG(INFO) << "Removal of cryptohome for " << user_email_
+                << " failed, return code: " << return_code;
+    }
+    delete this;
+  }
+
+ private:
+  std::string user_email_;
+};
 
 // Checks if display names are unique. If there are duplicates, enables
 // tooltips with full emails to let users distinguish their accounts.
@@ -332,6 +357,8 @@ void ExistingUserController::RemoveUser(UserController* source) {
   for (int i = 0; i < new_size; ++i)
     controllers_[i]->UpdateUserCount(i, new_size);
 
+  // Delete the encrypted user directory.
+  new RemoveAttempt(source->user().email());
   // We need to unmap entry windows, the windows will be unmapped in destructor.
   delete source;
 }
