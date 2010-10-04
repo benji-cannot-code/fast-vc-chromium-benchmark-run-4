ca1fed4dfb0085288c25e5ffd81c9be0a02023d3
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 22:49:45 +0000

    chromium-os:5166 Panel/popup cleanup for ChromeOS * Move size based disposition override for ChromeOS from tab_container to browser. * TYPE_APP_PANEL |= TYPE_POPUP so that special popup behavior applies to panels. * Removed confusing BrowserFrameChromeos::IsPanel(), replaced with BrowserView::IsBrowserTypePopup() calls. * Replaced now redundant checks against panel or popup types with 'browser->type() & Browser::TYPE_POPUP'. * Added size limiting code to PanelBrowserView. * Modified CreateWindowFunction::RunImpl() to create TYPE_APP_POPUP windows instead of TYPE_POPUP.
    BUG=http://code.google.com/p/chromium-os/issues/detail?id=5166
    TEST=Test spawning popups/panels/tabs from web pages (e.g. gmail), extensions (e.g. chat manager), and apps
    
    More info: https://docs.google.com/document/edit?id=14nBjW6gHJW0kk44o8Z8vfDSnDi0ZT1sUU74Zc5wmwIQ&hl=en&authkey=CKT6-O8I
    
    !!! This commit didn't go through !!! Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=29fe99e
    
    Review URL: http://codereview.chromium.org/3449007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61427 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/frame/panel_browser_view.h
+++ chrome/browser/chromeos/frame/panel_browser_view.h
@@ -19,7 +19,9 @@ namespace chromeos {
 class PanelController;
 
 // A browser view that implements Panel specific behavior.
-class PanelBrowserView : public BrowserView,
+// NOTE: This inherits from ::BrowserView in chrome/browser/views/frame/,
+// not chromeos::BrowserView in chrome/browser/chromeos/frame/.
+class PanelBrowserView : public ::BrowserView,
                          public PanelController::Delegate {
  public:
   explicit PanelBrowserView(Browser* browser);
@@ -27,11 +29,12 @@ class PanelBrowserView : public BrowserView,
   // BrowserView overrides.
   virtual void Init();
   virtual void Show();
+  virtual void SetBounds(const gfx::Rect& bounds);
   virtual void Close();
   virtual void UpdateTitleBar();
   virtual void ActivationChanged(bool activated);
-
   virtual void SetCreatorView(PanelBrowserView* creator);
+  virtual bool GetSavedWindowBounds(gfx::Rect* bounds) const;
 
   // PanelController::Delegate overrides
   virtual string16 GetPanelTitle();
@@ -40,6 +43,9 @@ class PanelBrowserView : public BrowserView,
   virtual void OnPanelStateChanged(PanelController::State state) {}
 
  private:
+  // Enforces the min, max, and default bounds.
+  void LimitBounds(gfx::Rect* bounds) const;
+
   // Controls interactions with the window manager for popup panels.
   scoped_ptr<chromeos::PanelController> panel_controller_;
 
