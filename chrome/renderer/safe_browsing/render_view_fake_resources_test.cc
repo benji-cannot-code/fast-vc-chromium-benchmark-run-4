aa26bc62cc57de29f971bc1ce75e347c97bc6ff4
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 18:47:50 +0000

    Only run the phishing classifier for GET requests.
    BUG=none
    TEST=PhishingClassifierTest
    
    Review URL: http://codereview.chromium.org/3572008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/render_view_fake_resources_test.cc
+++ chrome/renderer/safe_browsing/render_view_fake_resources_test.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/url_request/url_request_status.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURLRequest.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebView.h"
 #include "webkit/glue/webkit_glue.h"
@@ -111,7 +112,16 @@ WebKit::WebFrame* RenderViewFakeResourcesTest::GetMainFrame() {
 }
 
 void RenderViewFakeResourcesTest::LoadURL(const std::string& url) {
-  GetMainFrame()->loadRequest(WebKit::WebURLRequest(GURL(url)));
+  GURL g_url(url);
+  GetMainFrame()->loadRequest(WebKit::WebURLRequest(g_url));
+  message_loop_.Run();
+}
+
+void RenderViewFakeResourcesTest::LoadURLWithPost(const std::string& url) {
+  GURL g_url(url);
+  WebKit::WebURLRequest request(g_url);
+  request.setHTTPMethod(WebKit::WebString::fromUTF8("POST"));
+  GetMainFrame()->loadRequest(request);
   message_loop_.Run();
 }
 
