b725d110ab9ca6e82bfb61874ca0458f4edec572
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-04 06:14:41 +0000

    2010-10-03  Kent Tamura  <tkent@chromium.org>
            Unreviewed, build fix for r68996.
    
            * html/BaseDateAndTimeInputType.cpp: Includes <wtf/MathExtras.h> for isfinite().
            * html/MonthInputType.cpp: ditto.
            * html/NumberInputType.cpp: ditto.
            * html/RangeInputType.cpp: ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@68997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/BaseDateAndTimeInputType.cpp
+++ third_party/WebKit/WebCore/html/BaseDateAndTimeInputType.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BaseDateAndTimeInputType.h"
 
 #include "DateComponents.h"
+#include <wtf/MathExtras.h>
 #include <wtf/PassOwnPtr.h>
 #include <wtf/text/WTFString.h>
 
--- third_party/WebKit/WebCore/html/NumberInputType.cpp
+++ third_party/WebKit/WebCore/html/NumberInputType.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NumberInputType.h"
 
 #include "HTMLParserIdioms.h"
+#include <wtf/MathExtras.h>
 #include <wtf/PassOwnPtr.h>
 
 namespace WebCore {
