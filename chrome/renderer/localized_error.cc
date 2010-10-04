da0c2efc4d06e4a961832a9f97246827f10c2397
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-04 19:51:55 +0000

    Initial pass for proxy server error message change.
    CL began its life under rdsmith (http://codereview.chromium.org/3548001/show).
    
    BUG=55728
    TEST=Run chrome with invalid proxy server and examine navigation error message.
    
    Review URL: http://codereview.chromium.org/3521010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61403 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/localized_error.cc
+++ chrome/renderer/localized_error.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/renderer/extensions/extension_renderer_info.h"
 #include "googleurl/src/gurl.h"
+#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "net/base/escape.h"
 #include "net/base/net_errors.h"
@@ -122,6 +123,13 @@ const LocalizedErrorMap net_error_options[] = {
    IDS_ERRORPAGES_SUMMARY_SSL_PROTOCOL_ERROR,
    IDS_ERRORPAGES_DETAILS_SSL_PROTOCOL_ERROR,
    SUGGEST_LEARNMORE,
+  },
+  {net::ERR_PROXY_CONNECTION_FAILED,
+   IDS_ERRORPAGES_TITLE_PROXY_CONNECTION_FAILED,
+   IDS_ERRORPAGES_HEADING_PROXY_CONNECTION_FAILED,
+   IDS_ERRORPAGES_SUMMARY_PROXY_CONNECTION_FAILED,
+   IDS_ERRORPAGES_DETAILS_PROXY_CONNECTION_FAILED,
+   SUGGEST_NONE,
   },
 };
 
@@ -322,11 +330,24 @@ void LocalizedError::GetStrings(const WebKit::WebURLError& error,
       l10n_util::GetStringUTF16(options.summary_resource_id));
   // TODO(tc): we want the unicode url here since it's being displayed
   summary->SetString("failedUrl", failed_url);
+  summary->SetString("productName",
+                     l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
   error_strings->Set("summary", summary);
 
   string16 details = l10n_util::GetStringUTF16(options.details_resource_id);
   error_strings->SetString("details",
       GetErrorDetailsString(error_domain, error_code, details));
+
+  // Any special case processing that needs to happen for particular
+  // errors (hack).
+  if (error_domain == net::kErrorDomain &&
+      error_code == net::ERR_PROXY_CONNECTION_FAILED) {
+    // Suffix the platform dependent portion of the summary section.
+    summary->SetString("msg",
+        l10n_util::GetStringFUTF16(options.summary_resource_id,
+            l10n_util::GetStringUTF16(
+                IDS_ERRORPAGES_SUMMARY_PROXY_CONNECTION_FAILED_PLATFORM)));
+  }
 
   if (options.suggestions & SUGGEST_RELOAD) {
     DictionaryValue* suggest_reload = new DictionaryValue;
