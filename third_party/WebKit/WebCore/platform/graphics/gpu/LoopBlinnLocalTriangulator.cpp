8e764c2b77e981351ff26e9dc2957f1d377849d7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 11:16:41 +0000

    2010-10-05  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Csaba Osztrogonác.
    
            Add ENABLE(ACCELERATED_2D_CANVAS) guard to LoopBlinnLocalTriangulator.cpp
            https://bugs.webkit.org/show_bug.cgi?id=47115
    
            LoopBlinnLocalTriangulator.cpp depends on LoopBlinnMathUtils.cpp which is guarded by
            ENABLE(ACCELERATED_2D_CANVAS).
    
            * platform/graphics/gpu/LoopBlinnLocalTriangulator.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69101 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/gpu/LoopBlinnLocalTriangulator.cpp
+++ third_party/WebKit/WebCore/platform/graphics/gpu/LoopBlinnLocalTriangulator.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#if ENABLE(ACCELERATED_2D_CANVAS)
+
 #include "LoopBlinnLocalTriangulator.h"
 
 #include "LoopBlinnMathUtils.h"
@@ -274,3 +276,5 @@ bool LoopBlinnLocalTriangulator::isSharedEdge(Vertex* v0, Vertex* v1)
 }
 
 } // namespace WebCore
+
+#endif
