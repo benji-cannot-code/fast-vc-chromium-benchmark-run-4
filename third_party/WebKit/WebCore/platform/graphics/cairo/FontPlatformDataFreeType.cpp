a655fe69216057da24b41295230a5ce796f68973
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 20:09:25 +0000

    2010-10-05  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Support FontPlatformData::isFixedPitch for custom fonts
            https://bugs.webkit.org/show_bug.cgi?id=47124
    
            Instead of determining whether or not a font is a fixed-width font
            lazily, do it up front. For fonts not backed by Fontconfig patterns,
            fetch information about whether or not the font is fixed-width from
            the FreeType face.
    
            No new tests as this should not change functionality.
    
            * platform/graphics/cairo/FontPlatformDataFreeType.cpp:
            (WebCore::FontPlatformData::FontPlatformData): Initialize the m_fixedWidth member
            from the Fontconfig pattern or the FreeType face.
            (WebCore::FontPlatformData::operator=): Copy over the m_fixedWidth member.
            (WebCore::FontPlatformData::isFixedPitch): Just return the value of the m_fixedWidth member.
            * platform/graphics/cairo/FontPlatformDataFreeType.h: Added an m_fixedWidth member.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/FontPlatformDataFreeType.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cairo/FontPlatformDataFreeType.cpp
@@ -99,6 +99,7 @@ FontPlatformData::FontPlatformData(FcPattern* pattern, const FontDescription& fo
     , m_size(fontDescription.computedPixelSize())
     , m_syntheticBold(false)
     , m_syntheticOblique(false)
+    , m_fixedWidth(false)
 {
     cairo_font_options_t* options = cairo_font_options_create();
     setCairoFontOptionsFromFontConfigPattern(options, pattern);
@@ -110,6 +111,10 @@ FontPlatformData::FontPlatformData(FcPattern* pattern, const FontDescription& fo
 
     PlatformRefPtr<cairo_font_face_t> fontFace = adoptPlatformRef(cairo_ft_font_face_create_for_pattern(m_pattern.get()));
     m_scaledFont = adoptPlatformRef(cairo_scaled_font_create(fontFace.get(), &fontMatrix, &ctm, options));
+
+    int spacing;
+    if (FcPatternGetInteger(pattern, FC_SPACING, 0, &spacing) == FcResultMatch && spacing == FC_MONO)
+        m_fixedWidth = true;
 }
 
 FontPlatformData::FontPlatformData(float size, bool bold, bool italic)
@@ -117,6 +122,7 @@ FontPlatformData::FontPlatformData(float size, bool bold, bool italic)
     , m_size(size)
     , m_syntheticBold(bold)
     , m_syntheticOblique(italic)
+    , m_fixedWidth(false)
 {
 }
 
@@ -144,6 +150,12 @@ FontPlatformData::FontPlatformData(cairo_font_face_t* fontFace, float size, bool
         options = defaultOptions;
 
     m_scaledFont = adoptPlatformRef(cairo_scaled_font_create(fontFace, &fontMatrix, &ctm, options));
+
+    FT_Face fontConfigFace = cairo_ft_scaled_font_lock_face(m_scaledFont.get());
+    if (fontConfigFace) {
+        m_fixedWidth = fontConfigFace->face_flags && FT_FACE_FLAG_FIXED_WIDTH;
+        cairo_ft_scaled_font_unlock_face(m_scaledFont.get());
+    }
 }
 
 FontPlatformData& FontPlatformData::operator=(const FontPlatformData& other)
@@ -155,6 +167,7 @@ FontPlatformData& FontPlatformData::operator=(const FontPlatformData& other)
     m_size = other.m_size;
     m_syntheticBold = other.m_syntheticBold;
     m_syntheticOblique = other.m_syntheticOblique;
+    m_fixedWidth = other.m_fixedWidth;
     m_scaledFont = other.m_scaledFont;
     m_pattern = other.m_pattern;
 
@@ -183,14 +196,7 @@ FontPlatformData::~FontPlatformData()
 
 bool FontPlatformData::isFixedPitch()
 {
-    // TODO: Support isFixedPitch() for custom fonts.
-    if (!m_pattern)
-        return false;
-
-    int spacing;
-    if (FcPatternGetInteger(m_pattern.get(), FC_SPACING, 0, &spacing) == FcResultMatch)
-        return spacing == FC_MONO;
-    return false;
+    return m_fixedWidth;
 }
 
 bool FontPlatformData::operator==(const FontPlatformData& other) const
--- third_party/WebKit/WebCore/platform/graphics/cairo/FontPlatformDataFreeType.h
+++ third_party/WebKit/WebCore/platform/graphics/cairo/FontPlatformDataFreeType.h
@@ -90,6 +90,7 @@ public:
     float m_size;
     bool m_syntheticBold;
     bool m_syntheticOblique;
+    bool m_fixedWidth;
     PlatformRefPtr<cairo_scaled_font_t> m_scaledFont;
 };
 
