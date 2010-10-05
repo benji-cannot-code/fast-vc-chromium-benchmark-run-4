c1188cd4a1f007795c94cc6da1f2717970132e51
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 04:58:54 +0000

    2010-10-04  Ryuan Choi  <bunhere@gmail.com>
            Unreviewed build fix.
    
            [WML] Build fix for r68854
            https://bugs.webkit.org/show_bug.cgi?id=47043
    
            Include HTMLParserIdioms.h.
    
            No features added, so no new tests.
    
            * wml/WMLAElement.cpp:
            * wml/WMLImageLoader.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/wml/WMLAElement.cpp
+++ third_party/WebKit/WebCore/wml/WMLAElement.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoader.h"
 #include "HTMLAnchorElement.h"
 #include "HTMLNames.h"
+#include "HTMLParserIdioms.h"
 #include "KeyboardEvent.h"
 #include "MouseEvent.h"
 #include "RenderBox.h"
