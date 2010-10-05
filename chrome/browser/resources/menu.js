7271bf3689ea13ddea52cb6e67c5562b76ee6fb0
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-05 22:23:58 +0000

    * Allow a user to select the last selected item when moving back to submenu. * Ignore false mouseover when switching to submenu, which was closing submenu. * Fixed a bug that was setting selection when moving a mouse. (in native_menu_domui.cc) BUG=none TEST=none
    Review URL: http://codereview.chromium.org/3615010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61575 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/menu.js
+++ chrome/browser/resources/menu.js
@@ -73,7 +73,8 @@ MenuItem.prototype = {
   activate: function() {
     if (this.attrs.type == 'submenu') {
       this.menu_.openSubmenu(this);
-    } else if (this.attrs.type != 'separator') {
+    } else if (this.attrs.type != 'separator' &&
+               this.div.className.indexOf('selected') >= 0) {
       sendClick(this.id);
     }
   },
@@ -357,7 +358,10 @@ Menu.prototype = {
 
   onMouseover_: function(event, item) {
     this.cancelSubmenuTimer_();
-    if (this.current_ != item && item.attrs.enabled) {
+    // Ignore false mouseover event at (0,0) which is
+    // emitted when opening submenu.
+    if (item.attrs.enabled &&
+        (event.x != 0 && event.y != 0)) {
       item.select();
     }
   },
@@ -365,7 +369,6 @@ Menu.prototype = {
   onMouseout_: function(event) {
     if (this.current_) {
       this.current_.unselect();
-      this.current_ = null;
     }
   },
 
@@ -389,7 +392,6 @@ Menu.prototype = {
     if (!this.is_root) {
       if (this.current_) {
         this.current_.unselect();
-        this.current_ = null;
       }
       chrome.send('move_to_parent', []);
     }
