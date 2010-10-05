556823fda4060336e0498ccc73ebd6089e06ed08
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 03:47:54 +0000

    2010-10-04  Simon Fraser  <simon.fraser@apple.com>
            No review.
    
            Touch cf/SocketStreamHandle.h and add an #ifdef to qt/SocketStreamHandle.h
            to fail at compile time if Mac includes that header.
    
            * platform/network/cf/SocketStreamHandle.h:
            * platform/network/qt/SocketStreamHandle.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/qt/SocketStreamHandle.h
+++ third_party/WebKit/WebCore/platform/network/qt/SocketStreamHandle.h
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
 
+#if !PLATFORM(QT)
+#error This should only be built on Qt
+#endif
+
 namespace WebCore {
 
     class AuthenticationChallenge;
