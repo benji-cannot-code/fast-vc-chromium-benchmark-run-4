0fbf780f7c9147d673f9ee7589bd5180519b262f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 15:02:45 +0000

    2010-10-05  Kimmo Kinnunen  <kimmo.t.kinnunen@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Implement API call for setting custom user agent
            (QWKPage::setCustomUserAgent)
    
            [Qt] Ability to set custom userAgent for WebKit2/WebKit
            https://bugs.webkit.org/show_bug.cgi?id=44265
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPage::setCustomUserAgent): Added.
            * UIProcess/API/qt/qwkpage.h: Add setCustomUserAgent.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.h
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.h
@@ -88,6 +88,8 @@ public:
     typedef QWKPage* (*CreateNewPageFn)(QWKPage*);
     void setCreateNewPageFunction(CreateNewPageFn function);
 
+    void setCustomUserAgent(const QString&);
+
 public:
     Q_SIGNAL void statusBarMessage(const QString&);
     Q_SIGNAL void titleChanged(const QString&);
