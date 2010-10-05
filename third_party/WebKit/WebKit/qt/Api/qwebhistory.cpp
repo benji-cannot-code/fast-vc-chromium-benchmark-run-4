7653a1551b1309ec8c5f26e59c27a07990d34868
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 19:01:15 +0000

    2010-10-05  Robert Hogan  <robert@webkit.org>
            Reviewed by Andreas Kling.
    
            [Qt] Fix url conversion in QWebHistory
    
            Converting from KURL to WFT::String to QUrl does not
            permit proper percent encoding later.
    
            Unskp tests that now pass:
             http/tests/navigation/postredirect-goback2.html
             http/tests/navigation/error404-frames.html
             http/tests/navigation/post-basic.html
             http/tests/navigation/post-frames.html
             http/tests/navigation/post-goback1.html
             http/tests/navigation/anchor-frames.html
    
            These produce only rendertree differences with '--platform mac'.
            Unskipping so we can get Qt-specific results from bot:
             http/tests/navigation/error404-basic.html
             http/tests/navigation/error404-goback.html
             http/tests/navigation/error404-subframeload.html
             http/tests/navigation/javascriptlink-frames.html
             http/tests/navigation/postredirect-basic.html
             http/tests/navigation/postredirect-frames.html
             http/tests/navigation/postredirect-goback1.html
    
            https://bugs.webkit.org/show_bug.cgi?id=47048
    
            * platform/qt/Skipped:
    2010-10-05  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Andreas Kling.
    
            [Qt] Fix url conversion in QWebHistory
    
            Converting from KURL to WFT::String to QUrl does not
            permit proper percent encoding later.
    
            https://bugs.webkit.org/show_bug.cgi?id=47048
    
            * Api/qwebhistory.cpp:
            (QWebHistoryItem::originalUrl):
            (QWebHistoryItem::url):
    2010-10-05  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Andreas Kling.
    
            [Qt] Fix url conversion in QWebHistory
    
            Converting from KURL to WFT::String to QUrl does not
            permit proper percent encoding later.
    
            https://bugs.webkit.org/show_bug.cgi?id=47048
    
            * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
            (WebCore::dumpHistoryItem):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebhistory.cpp
@@ -104,7 +104,7 @@ QWebHistoryItem::~QWebHistoryItem()
 QUrl QWebHistoryItem::originalUrl() const
 {
     if (d->item)
-        return QUrl(d->item->originalURL().string());
+        return d->item->originalURL();
     return QUrl();
 }
 
@@ -117,7 +117,7 @@ QUrl QWebHistoryItem::originalUrl() const
 QUrl QWebHistoryItem::url() const
 {
     if (d->item)
-        return QUrl(d->item->url().string());
+        return d->item->url();
     return QUrl();
 }
 
