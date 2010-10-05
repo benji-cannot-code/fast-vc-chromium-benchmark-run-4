474f8e5a7a224d2bc63469e4dcf410f8b28baf56
luiz@webkit.org <luiz@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 13:18:15 +0000

    2010-10-05  Luiz Agostini  <luiz.agostini@openbossa.org>
            Reviewed by Antti Koivisto.
    
            [Qt] Webkit2 MacOS build fix
            https://bugs.webkit.org/show_bug.cgi?id=47167
    
            Qt WebKit2 MacOS build fix.
    
            * UIProcess/API/C/WKNativeEvent.h:
            * UIProcess/API/qt/qwkpage.cpp:
            * UIProcess/API/qt/qwkpage.h:
            * WebKit2.pri:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WKNativeEvent.h
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WKNativeEvent.h
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern "C" {
 #endif
 
+#if PLATFORM(QT)
+class QEvent;
+typedef const QEvent* WKNativeEventPtr;
+#else
 #ifdef __APPLE__
 #ifdef __OBJC__
 @class NSEvent;
@@ -42,6 +46,7 @@ typedef NSEvent *WKNativeEventPtr;
 typedef const struct tagMSG* WKNativeEventPtr;
 #else
 typedef const void* WKNativeEventPtr;
+#endif
 #endif
 
 #ifdef __cplusplus
