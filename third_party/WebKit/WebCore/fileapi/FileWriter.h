6b5882c34af28bfa96e106bccd38b812542875a9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-05 20:28:59 +0000

    2010-10-05  Eric Uhrhane  <ericu@chromium.org>
            Reviewed by David Levin.
    
            Relax restrictions on FileWriter::didWrite calls
            https://bugs.webkit.org/show_bug.cgi?id=47139
    
            No new tests; they're still waiting on the first implementation.
    
            * fileapi/FileWriter.cpp:
            (WebCore::FileWriter::FileWriter):
            (WebCore::FileWriter::write):
            (WebCore::FileWriter::didWrite):
            * fileapi/FileWriter.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/fileapi/FileWriter.h
+++ third_party/WebKit/WebCore/fileapi/FileWriter.h
@@ -120,6 +120,7 @@ private:
     ReadyState m_readyState;
     long long m_position;
     long long m_length;
+    bool m_startedWriting;
     long long m_bytesWritten;
     long long m_bytesToWrite;
     long long m_truncateLength;
