ee61f046c970933339b421df09213f0f3b3ba29b
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-05 23:45:03 +0000

    Remove JS emulation of HTMLElement classList now that WebKit supports it natively.
    BUG=None
    TEST=NTP, Bookmarks manager, settings should all continue to work.
    
    Review URL: http://codereview.chromium.org/3521015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61586 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp/most_visited.js
+++ chrome/browser/resources/ntp/most_visited.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Dependencies that we should remove/formalize:
-// ../shared/js/class_list.js
 // util.js
 //
 // afterTransition
