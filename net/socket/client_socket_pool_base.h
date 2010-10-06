127ae3bfdfb32c62aad4742b008101a10b5ee3d6
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-06 21:27:22 +0000

    Prefer used idle sockets over unused idle sockets.
    * Pick used idle sockets LIFO.
    * In absence of used idle sockets, pick unused idle sockets FIFO.
    
    BUG=57491
    TEST=ClientSocketPoolBaseTest.PreferUsedSocketToUnusedSocket
    
    Review URL: http://codereview.chromium.org/3539013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61710 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_pool_base.h
+++ net/socket/client_socket_pool_base.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <deque>
+#include <list>
 #include <map>
 #include <set>
 #include <string>
@@ -324,14 +325,14 @@ class ClientSocketPoolBaseHelper
     void DecrementActiveSocketCount() { active_socket_count_--; }
 
     const std::set<const ConnectJob*>& jobs() const { return jobs_; }
-    const std::deque<IdleSocket>& idle_sockets() const { return idle_sockets_; }
+    const std::list<IdleSocket>& idle_sockets() const { return idle_sockets_; }
     const RequestQueue& pending_requests() const { return pending_requests_; }
     int active_socket_count() const { return active_socket_count_; }
     RequestQueue* mutable_pending_requests() { return &pending_requests_; }
-    std::deque<IdleSocket>* mutable_idle_sockets() { return &idle_sockets_; }
+    std::list<IdleSocket>* mutable_idle_sockets() { return &idle_sockets_; }
 
    private:
-    std::deque<IdleSocket> idle_sockets_;
+    std::list<IdleSocket> idle_sockets_;
     std::set<const ConnectJob*> jobs_;
     RequestQueue pending_requests_;
     int active_socket_count_;  // number of active sockets used by clients
