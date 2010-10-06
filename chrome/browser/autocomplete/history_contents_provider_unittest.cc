04315e852e789b2fb07d23560706cceacb536639
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-06 03:57:24 +0000

    Rename ChromeThread to BrowserThread Part 2:
    - Rename the entries under appcache, autocomplete and automation.
    
    BUG=56926
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/3524012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_contents_provider_unittest.cc
+++ chrome/browser/autocomplete/history_contents_provider_unittest.cc
@@ -34,8 +34,8 @@ class HistoryContentsProviderTest : public testing::Test,
                                     public ACProviderListener {
  public:
   HistoryContentsProviderTest()
-      : ui_thread_(ChromeThread::UI, &message_loop_),
-        file_thread_(ChromeThread::FILE, &message_loop_) {}
+      : ui_thread_(BrowserThread::UI, &message_loop_),
+        file_thread_(BrowserThread::FILE, &message_loop_) {}
 
   void RunQuery(const AutocompleteInput& input,
                 bool minimal_changes) {
@@ -95,8 +95,8 @@ class HistoryContentsProviderTest : public testing::Test,
   }
 
   MessageLoopForUI message_loop_;
-  ChromeThread ui_thread_;
-  ChromeThread file_thread_;
+  BrowserThread ui_thread_;
+  BrowserThread file_thread_;
 
   std::wstring history_dir_;
 
