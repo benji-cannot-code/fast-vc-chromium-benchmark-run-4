e0851ddcb05abf0768ef246cbcc25422009fa6c7
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-06 04:21:50 +0000

    Second try for support removeRecursively and new copy/move behaviors added to the spec recently.
    http://lists.w3.org/Archives/Public/public-webapps/2010JulSep/1101.html
    > For a move/copy of a file on top of existing file, or a directory on
    > top of an existing empty directory, you get an automatic overwrite.
    > A move/copy of a file on top of an existing directory, or of a
    > directory on top of an existing file, will always fail.
    > A move/copy of a file or directory on top of an existing non-empty
    > directory will always fail.
    
    original issue: http://codereview.chromium.org/3567012
    
    BUG=none
    TEST=FileSystemOperationTest.*
    TBR=ericu
    
    Review URL: http://codereview.chromium.org/3531012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61613 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/simple_file_system.h
+++ webkit/tools/test_shell/simple_file_system.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef WEBKIT_TOOLS_TEST_SHELL_SIMPLE_FILE_SYSTEM_H_
 #define WEBKIT_TOOLS_TEST_SHELL_SIMPLE_FILE_SYSTEM_H_
@@ -29,6 +29,8 @@ class SimpleFileSystem : public WebKit::WebFileSystem {
                     WebKit::WebFileSystemCallbacks* callbacks);
   virtual void remove(const WebKit::WebString& path,
                       WebKit::WebFileSystemCallbacks* callbacks);
+  virtual void removeRecursively(const WebKit::WebString& path,
+                                 WebKit::WebFileSystemCallbacks* callbacks);
   virtual void readMetadata(const WebKit::WebString& path,
                             WebKit::WebFileSystemCallbacks* callbacks);
   virtual void createFile(const WebKit::WebString& path,
