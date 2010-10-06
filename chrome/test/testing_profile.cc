96c44ad753f51d658f3325c37742e222a4b724b2
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-06 23:17:59 +0000

    A large part of this change is adding the 1st unit test for TemplateURLFetcher.
    Also did some refactoring of TemplateURLFetcher to allow for a future
    modification in which I make it able to start the load of TemplateURLModel and
    wait for that to finish before processing the fetched url.
    
    BUG=38475
    TEST=unit_test --gtest_filter=TemplateURLFetcher*
    
    Review URL: http://codereview.chromium.org/3548017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61727 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_profile.cc
+++ chrome/test/testing_profile.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/gaia/token_service.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/prefs/browser_prefs.h"
+#include "chrome/browser/search_engines/template_url_fetcher.h"
 #include "chrome/browser/search_engines/template_url_model.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sync/profile_sync_service_mock.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/testing_pref_service.h"
 #include "net/base/cookie_monster.h"
 #include "net/url_request/url_request_context.h"
+#include "net/url_request/url_request_unittest.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "webkit/database/database_tracker.h"
 
@@ -107,16 +109,6 @@ class BookmarkLoadObserver : public BookmarkModelObserver {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(BookmarkLoadObserver);
-};
-
-// This context is used to assist testing the CookieMonster by providing a
-// valid CookieStore. This can probably be expanded to test other aspects of
-// the context as well.
-class TestURLRequestContext : public URLRequestContext {
- public:
-  TestURLRequestContext() {
-    cookie_store_ = new net::CookieMonster(NULL, NULL);
-  }
 };
 
 // Used to return a dummy context (normally the context is on the IO thread).
@@ -312,8 +304,16 @@ void TestingProfile::BlockUntilBookmarkModelLoaded() {
   DCHECK(bookmark_bar_model_->IsLoaded());
 }
 
+void TestingProfile::CreateTemplateURLFetcher() {
+  template_url_fetcher_.reset(new TemplateURLFetcher(this));
+}
+
 void TestingProfile::CreateTemplateURLModel() {
-  template_url_model_.reset(new TemplateURLModel(this));
+  SetTemplateURLModel(new TemplateURLModel(this));
+}
+
+void TestingProfile::SetTemplateURLModel(TemplateURLModel* model) {
+  template_url_model_.reset(model);
 }
 
 void TestingProfile::UseThemeProvider(BrowserThemeProvider* theme_provider) {
