00012480171fc29338f6deddbe5931faecef97f0
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-07 00:34:56 +0000

    Try to fix Windows build.
    * platform/graphics/cg/ColorCG.cpp:
    (WebCore::cachedCGColor): Put in braces to make the Windows compiler
    happy with this file.
    
    * platform/graphics/cg/ImageCG.cpp: Added include of RetainPtr.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/ImageCG.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PDFDocumentImage.h"
 #include "PlatformString.h"
 #include <ApplicationServices/ApplicationServices.h>
+#include <wtf/RetainPtr.h>
 
 #if PLATFORM(MAC) || PLATFORM(CHROMIUM)
 #include "WebCoreSystemInterface.h"
