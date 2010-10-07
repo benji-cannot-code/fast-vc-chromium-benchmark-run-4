141a99dfed8875b4d6bef0ca44fa32cb82d327c2
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 00:00:54 +0000

    ExtensionApiTest.Tabs: disable failing test
    Fails on all platforms.  See bug.
    
    BUG=58229
    
    Review URL: http://codereview.chromium.org/3520020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/tabs/basics/test.js
+++ chrome/test/data/extensions/api_test/tabs/basics/test.js
@@ -441,6 +441,7 @@ chrome.test.runTests([
     chrome.windows.create({"url": pageUrl("a")}, pass(function(tab) {}));
   },
 
+  /* Disabled -- see http://bugs.chromium.org/58229.
   function windowSetFocused() {
     chrome.windows.getCurrent(function(oldWin) {
       chrome.windows.create({}, function(newWin) {
@@ -452,6 +453,7 @@ chrome.test.runTests([
       });
     });
   },
+  */
 
   /* TODO: Enable this test when crbug.com/28055 is fixed. This bug causes a
      newly created window not to be set as the current window, if
