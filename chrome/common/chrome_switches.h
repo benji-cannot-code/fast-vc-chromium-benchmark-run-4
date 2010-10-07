f52960bb7b9543fa42252c8bcea09d5bbdfe4643
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 17:28:57 +0000

    Let menu scroll when the menu content exceeds screen height. Menu locator limits the size of window, and scrolling is handled in DOMUI.  This does not implement mouse wheel yet as the current implementation doesn't support it. I also haven't enable this yet for dropdown as I need to figure out how this should work for OOBE. I'll address it in separate CL.
     Added command line parameter to specify DOMUI html page for testing.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3608006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -335,6 +335,9 @@ extern const char kCandidateWindowLang[];
 extern const char kBWSI[];
 extern const char kStubCros[];
 extern const char kScreenSaverUrl[];
+#ifndef NDEBUG
+extern const char kDOMUIMenuUrl[];
+#endif
 #endif
 
 #if defined(OS_LINUX)
