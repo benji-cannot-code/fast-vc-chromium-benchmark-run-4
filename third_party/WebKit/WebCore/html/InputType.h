b338b26febc3807f7561af3a35a88ab784499738
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-07 05:23:09 +0000

    2010-10-06  Kent Tamura  <tkent@chromium.org>
            Unreviewed, build fix.
    
            Windows build fix for r69272.
    
            * html/BaseDateAndTimeInputType.cpp:
            (WebCore::BaseDateAndTimeInputType::stepBase):
             Use defaultStepBase() instead of defaultStepBase.
            * html/InputType.h:
            (WebCore::InputType::defaultStepBase):
             Make defaultStepBase static function.
            * html/NumberInputType.cpp:
            (WebCore::NumberInputType::stepBase):
             Use defaultStepBase() instead of defaultStepBase.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/InputType.h
+++ third_party/WebKit/WebCore/html/InputType.h
@@ -42,8 +42,6 @@ class HTMLInputElement;
 
 class InputType : public Noncopyable {
 public:
-    static const double defaultStepBase = 0.0;
-
     static PassOwnPtr<InputType> create(HTMLInputElement*, const AtomicString&);
     static PassOwnPtr<InputType> createText(HTMLInputElement*);
     virtual ~InputType();
@@ -80,6 +78,8 @@ public:
 protected:
     InputType(HTMLInputElement* element) : m_element(element) { }
     HTMLInputElement* element() const { return m_element; }
+    // We can't make this a static const data member because VC++ doesn't like it.
+    static double defaultStepBase() { return 0.0; }
 
 private:
     // Raw pointer because the HTMLInputElement object owns this InputType object.
