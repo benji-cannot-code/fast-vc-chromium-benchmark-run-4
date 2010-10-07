31e972fbf8f4dcee11e5aa5bc8de7c8b81dbd71e
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 11:09:52 +0000

    Revert 61718 - GPU process terminates on hang.
    I added a watchdog thread that intermitently checks the main thread can respond to tasks posted on its message queue.
    
    I fixed some bugs that preventede GGL from failing when the GPU channel was lost.
    
    Added a command line swith to disable the watchdog thread for debugging purposes.
    
    TEST=try, check WebGL works, check about:gpuhang terminates process.
    BUG=38739,53871
    
    Review URL: http://codereview.chromium.org/3528012
    
    TBR=apatrick@chromium.org
    Review URL: http://codereview.chromium.org/3616011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_process_host.cc
+++ chrome/browser/gpu_process_host.cc
@@ -92,7 +92,6 @@ bool GpuProcessHost::Init() {
   // Propagate relevant command line switches.
   static const char* const kSwitchNames[] = {
     switches::kUseGL,
-    switches::kDisableGpuWatchdog,
     switches::kDisableLogging,
     switches::kEnableLogging,
     switches::kGpuStartupDialog,
@@ -124,18 +123,14 @@ GpuProcessHost* GpuProcessHost::Get() {
   return sole_instance_;
 }
 
+// static
 void GpuProcessHost::SendAboutGpuCrash() {
-    ChromeThread::PostTask(
-        ChromeThread::IO,
-        FROM_HERE,
-        NewRunnableFunction(&GpuProcessHost::SendAboutGpuCrashInternal));
+  Get()->Send(new GpuMsg_Crash());
 }
 
+// static
 void GpuProcessHost::SendAboutGpuHang() {
-    ChromeThread::PostTask(
-        ChromeThread::IO,
-        FROM_HERE,
-        NewRunnableFunction(&GpuProcessHost::SendAboutGpuHangInternal));
+  Get()->Send(new GpuMsg_Hang());
 }
 
 bool GpuProcessHost::Send(IPC::Message* msg) {
@@ -353,16 +348,4 @@ URLRequestContext* GpuProcessHost::GetRequestContext(
 
 bool GpuProcessHost::CanShutdown() {
   return true;
-}
-
-// static
-void GpuProcessHost::SendAboutGpuCrashInternal() {
-  DCHECK(Get());
-  Get()->Send(new GpuMsg_Crash());
-}
-
-// static
-void GpuProcessHost::SendAboutGpuHangInternal() {
-  DCHECK(Get());
-  Get()->Send(new GpuMsg_Hang());
 }
--- chrome/renderer/webgraphicscontext3d_command_buffer_impl.cc
+++ chrome/renderer/webgraphicscontext3d_command_buffer_impl.cc
@@ -57,7 +57,7 @@ bool WebGraphicsContext3DCommandBufferImpl::initialize(
   GpuChannelHost* host = render_thread->EstablishGpuChannelSync();
   if (!host)
     return false;
-  DCHECK(host->state() == GpuChannelHost::kConnected);
+  DCHECK(host->state() == GpuChannelHost::CONNECTED);
 
   // Convert WebGL context creation attributes into GGL/EGL size requests.
   const int alpha_size = attributes.alpha ? 8 : 0;
@@ -188,8 +188,7 @@ bool WebGraphicsContext3DCommandBufferImpl::isGLES2NPOTStrict() {
   return false;
 }
 
-bool WebGraphicsContext3DCommandBufferImpl::
-    isErrorGeneratedOnOutOfBoundsAccesses() {
+bool WebGraphicsContext3DCommandBufferImpl::isErrorGeneratedOnOutOfBoundsAccesses() {
   return false;
 }
 
