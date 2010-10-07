53ca0d1bb14a2b0bbf7c994ed3426d6294524a2b
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 09:32:36 +0000

    Mark ExtensionApiTest.Tabs flaky on win and linux.
    BUG=58269
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/3526023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // http://crbug.com/53779
 #define MAYBE_Tabs FAILS_Tabs
 #else
-#define MAYBE_Tabs Tabs
+// It's flaky on win and linux.
+// http://crbug.com/58269
+#define MAYBE_Tabs FLAKY_Tabs
 #endif
 
 // TabOnRemoved is flaky on chromeos and linux views debug build.
