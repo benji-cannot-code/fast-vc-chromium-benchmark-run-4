95d3eb2637345d59150c462b07f29c25a422b00b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-07 05:31:49 +0000

    2010-10-06  Chris Guillory  <chris.guillory@google.com>
            Reviewed by Chris Fleizach.
    
            Remove unused accessibility functions from webkit.
            https://bugs.webkit.org/attachment.cgi?bugid=46707
    
            * public/WebAccessibilityCache.h:
            * public/WebViewClient.h:
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::focusedNodeChanged):
            * src/WebAccessibilityCacheImpl.cpp:
            * src/WebAccessibilityCacheImpl.h:
    2010-10-06  Chris Guillory  <chris.guillory@google.com>
    
            Reviewed by Chris Fleizach.
    
            Remove unused accessibility functions from webkit.
            https://bugs.webkit.org/attachment.cgi?bugid=46707
    
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (WebViewHost::postAccessibilityNotification):
            * DumpRenderTree/chromium/WebViewHost.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebAccessibilityCacheImpl.h
+++ third_party/WebKit/WebKit/chromium/src/WebAccessibilityCacheImpl.h
@@ -46,7 +46,6 @@ public:
     virtual bool isInitialized() const { return m_initialized; }
 
     virtual WebAccessibilityObject getObjectById(int);
-    virtual bool isValidId(int) const;
     virtual int addOrGetId(const WebKit::WebAccessibilityObject&);
     virtual bool isCached(const WebAccessibilityObject&);
 
