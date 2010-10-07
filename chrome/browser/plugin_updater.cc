675e8bfe16420353b05db7d3bc4b82eb8f0f1e98
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 22:01:13 +0000

    Fix bug with enabling/disabling the pdf plugin, related to plugin grouping work.
    BUG=58329
    Review URL: http://codereview.chromium.org/3618015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_updater.cc
+++ chrome/browser/plugin_updater.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/path_service.h"
 #include "base/scoped_ptr.h"
+#include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "base/version.h"
 #include "chrome/browser/prefs/pref_service.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/notification_service.h"
+#include "chrome/common/pepper_plugin_registry.h"
 #include "chrome/common/pref_names.h"
 #include "webkit/glue/plugins/plugin_group.h"
 #include "webkit/glue/plugins/plugin_list.h"
@@ -139,7 +141,7 @@ void PluginUpdater::DisablePluginGroupsFromPrefs(Profile* profile) {
 
   bool found_internal_pdf = false;
   bool force_enable_internal_pdf = false;
-  string16 pdf_group_name;
+  string16 pdf_group_name = ASCIIToUTF16(PepperPluginRegistry::kPDFPluginName);
   FilePath pdf_path;
   PathService::Get(chrome::FILE_PDF_PLUGIN, &pdf_path);
   FilePath::StringType pdf_path_str = pdf_path.value();
@@ -185,7 +187,6 @@ void PluginUpdater::DisablePluginGroupsFromPrefs(Profile* profile) {
 
         if (FilePath::CompareIgnoreCase(path, pdf_path_str) == 0) {
           found_internal_pdf = true;
-          plugin->GetString("name", &pdf_group_name);
           if (!enabled && force_enable_internal_pdf) {
             enabled = true;
             plugin->SetBoolean("enabled", true);
@@ -216,6 +217,7 @@ void PluginUpdater::DisablePluginGroupsFromPrefs(Profile* profile) {
     // The internal PDF plugin is disabled by default, and the user hasn't
     // overridden the default.
     NPAPI::PluginList::Singleton()->DisablePlugin(pdf_path);
+    NPAPI::PluginList::Singleton()->EnableGroup(false, pdf_group_name);
   }
 
   if (update_preferences)
