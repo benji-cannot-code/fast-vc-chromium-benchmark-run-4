bd9a36c00e765ac90f7eb0b8ce16ecd81bfb1764
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 21:59:40 +0000

    Removed unneeded header from utility_messages_internal.h,
    BUG=None
    TEST=Build.
    
    Review URL: http://codereview.chromium.org/3596011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61862 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/utility_messages_internal.h
+++ chrome/common/utility_messages_internal.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/platform_file.h"
 #include "gfx/rect.h"
-#include "printing/native_metafile.h"
 #include "printing/page_range.h"
 
 class FilePath;
