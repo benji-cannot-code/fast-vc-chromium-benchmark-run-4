419a55a5ddb003802d55a106952676df0b070e2d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-08 11:42:27 +0000

    2010-10-08  Kimmo Kinnunen  <kimmo.t.kinnunen@nokia.com>
            Reviewed by Andreas Kling.
    
            Notify WebPage when QGraphicsWKView gains focus. Set both
            active flag and focused flag, similar to QWebPage behavior.
    
            [Qt] WebKit2 should support focusing
            https://bugs.webkit.org/show_bug.cgi?id=47168
    
            * UIProcess/API/qt/qgraphicswkview.cpp:
            (QGraphicsWKView::focusInEvent): Added.
            (QGraphicsWKView::focusOutEvent): Added.
            * UIProcess/API/qt/qgraphicswkview.h: Added declarations.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qgraphicswkview.h
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qgraphicswkview.h
@@ -77,6 +77,9 @@ protected:
 
     Q_SLOT void updateCursor(const QCursor&);
 
+    virtual void focusInEvent(QFocusEvent*);
+    virtual void focusOutEvent(QFocusEvent*);
+
 private:
     QGraphicsWKViewPrivate* d;
     friend class QGraphicsWKViewPrivate;
