0ce3e223baeee0c77d5a5b5b573b2d9b3e987e85
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-08 18:19:51 +0000

    Adds a list of kernel extensions to the net-internals screen on Macs
    BUG=56697
    TEST=manual
    
    Review URL: http://codereview.chromium.org/3611003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61986 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/net_internals_ui.cc
+++ chrome/browser/dom_ui/net_internals_ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
+#include "base/process_util.h"
 #include "base/singleton.h"
 #include "base/string_number_conversions.h"
 #include "base/string_piece.h"
@@ -52,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/ssl_client_socket_pool.h"
 #include "net/socket/tcp_client_socket_pool.h"
 #include "net/url_request/url_request_context.h"
-#ifdef OS_WIN
+#if defined(OS_WIN)
 #include "chrome/browser/net/service_providers_win.h"
 #endif
 
@@ -215,8 +216,10 @@ class NetInternalsMessageHandler::IOThreadImpl
   void OnStartConnectionTests(const ListValue* list);
   void OnGetHttpCacheInfo(const ListValue* list);
   void OnGetSocketPoolInfo(const ListValue* list);
-#ifdef OS_WIN
+#if defined(OS_WIN)
   void OnGetServiceProviders(const ListValue* list);
+#elif defined(OS_MACOSX)
+  void OnGetKernelExtensions(const ListValue* list);
 #endif
 
   // ChromeNetLog::Observer implementation:
@@ -404,10 +407,14 @@ void NetInternalsMessageHandler::RegisterMessages() {
   dom_ui_->RegisterMessageCallback(
       "getSocketPoolInfo",
       proxy_->CreateCallback(&IOThreadImpl::OnGetSocketPoolInfo));
-#ifdef OS_WIN
+#if defined(OS_WIN)
   dom_ui_->RegisterMessageCallback(
       "getServiceProviders",
       proxy_->CreateCallback(&IOThreadImpl::OnGetServiceProviders));
+#elif defined(OS_MACOSX)
+  dom_ui_->RegisterMessageCallback(
+      "getKernelExtensions",
+      proxy_->CreateCallback(&IOThreadImpl::OnGetKernelExtensions));
 #endif
 }
 
@@ -611,8 +618,10 @@ void NetInternalsMessageHandler::IOThreadImpl::OnRendererReady(
   OnGetHostResolverInfo(NULL);
   OnGetHttpCacheInfo(NULL);
   OnGetSocketPoolInfo(NULL);
-#ifdef OS_WIN
+#if defined(OS_WIN)
   OnGetServiceProviders(NULL);
+#elif defined(OS_MACOSX)
+  OnGetKernelExtensions(NULL);
 #endif
 }
 
@@ -838,10 +847,9 @@ void NetInternalsMessageHandler::IOThreadImpl::OnGetSocketPoolInfo(
   CallJavascriptFunction(L"g_browser.receivedSocketPoolInfo", socket_pool_info);
 }
 
-#ifdef OS_WIN
+#if defined(OS_WIN)
 void NetInternalsMessageHandler::IOThreadImpl::OnGetServiceProviders(
     const ListValue* list) {
-
   DictionaryValue* service_providers = new DictionaryValue();
 
   WinsockLayeredServiceProviderList layered_providers;
@@ -877,6 +885,19 @@ void NetInternalsMessageHandler::IOThreadImpl::OnGetServiceProviders(
 
   CallJavascriptFunction(L"g_browser.receivedServiceProviders",
                          service_providers);
+}
+
+#elif defined(OS_MACOSX)
+void NetInternalsMessageHandler::IOThreadImpl::OnGetKernelExtensions(
+    const ListValue* list) {
+  std::string kextstat_output;
+  Value* string_value;
+
+  CommandLine command_line(FilePath("kextstat"));
+  if (base::GetAppOutput(command_line, &kextstat_output))
+    string_value = new StringValue(kextstat_output);
+
+  CallJavascriptFunction(L"g_browser.receivedKernelExtensions", string_value);
 }
 #endif
 
