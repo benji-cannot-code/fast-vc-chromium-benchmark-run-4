6da8dbdb33b46d2fe87415b19b11238085e251dd
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-08 08:16:32 +0000

    Add in a test script I missed in r61898
    Originally reviewed at http://codereview.chromium.org/3402021
    r61898 which I submitted on behalf of amolk missed this file.
    
    BUG=36177
    TEST=find_in_page.py
    
    TBR=amolk
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61928 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/functional/find_in_page.py
@@ -0,0 +1,146 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import unittest
+
+import pyauto_functional
+import pyauto
+
+
+class FindMatchTests(pyauto.PyUITest):
+
+  def testCanFindMatchCount(self):
+    """Verify Find match count for valid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(1, self.FindInPage('title')['match_count'])
+
+  def testCanFindMatchCountFail(self):
+    """Verify Find match count for invalid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(0, self.FindInPage('blah')['match_count'])
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import unittest
+
+import pyauto_functional
+import pyauto
+
+
+class FindMatchTests(pyauto.PyUITest):
+
+  def testCanFindMatchCount(self):
+    """Verify Find match count for valid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(1, self.FindInPage('title')['match_count'])
+
+  def testCanFindMatchCountFail(self):
+    """Verify Find match count for invalid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(0, self.FindInPage('blah')['match_count'])
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import unittest
+
+import pyauto_functional
+import pyauto
+
+
+class FindMatchTests(pyauto.PyUITest):
+
+  def testCanFindMatchCount(self):
+    """Verify Find match count for valid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(1, self.GetMatchCountForSearch('title')['match_count'])
+
+  def testCanFindMatchCountFail(self):
+    """Verify Find match count for invalid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(0, self.FindInPage('blah')['match_count'])
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import unittest
+
+import pyauto_functional
+import pyauto
+
+
+class FindMatchTests(pyauto.PyUITest):
+
+  def testCanFindMatchCount(self):
+    """Verify Find match count for valid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(1, self.GetMatchCountForSearch('title')['match_count'])
+
+  def testCanFindMatchCountFail(self):
+    """Verify Find match count for invalid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(0, self.FindInPage('blah')['match_count'])
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import unittest
+
+import pyauto_functional
+import pyauto
+
+
+class FindMatchTests(pyauto.PyUITest):
+
+  def testCanFindMatchCount(self):
+    """Verify Find match count for valid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(1, self.GetMatchCountForSearch('title')['match_count'])
+
+  def testCanFindMatchCountFail(self):
+    """Verify Find match count for invalid search"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title1.html'))
+    self.NavigateToURL(url)
+    self.assertEqual(0, self.FindInPage('blah')['match_count'])
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
