476e22471b55ab58345afe3ce0eba6b11d4997e2
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-08 22:31:37 +0000

    Remove include for string16 which can cause problems for external projects using this interface.
    TEST=none
    BUG=none
    Original review=http://codereview.chromium.org/3521022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62034 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/ppb_private.h
+++ webkit/glue/plugins/ppb_private.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_PLUGINS_PPB_PRIVATE_H_
 #define WEBKIT_GLUE_PLUGINS_PPB_PRIVATE_H_
 
-#include "base/string16.h"
 #include "third_party/ppapi/c/dev/ppb_font_dev.h"
 #include "third_party/ppapi/c/pp_module.h"
 #include "third_party/ppapi/c/pp_var.h"
@@ -107,8 +106,8 @@ struct PPB_Private {
   // done.
   void (*SearchString)(
      PP_Module module,
-     const char16* string,
-     const char16* term,
+     const unsigned short* string,
+     const unsigned short* term,
      bool case_sensitive,
      PP_PrivateFindResult** results,
      int* count);
