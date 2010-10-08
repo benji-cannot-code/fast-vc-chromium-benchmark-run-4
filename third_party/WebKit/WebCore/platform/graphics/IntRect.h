1295f2ad47a1e899627dc6046f4dbcb64be95a8a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-08 13:24:13 +0000

    2010-10-08  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Implement IntRectEfl.cpp
            https://bugs.webkit.org/show_bug.cgi?id=47403
    
            Implement IntRect for Efl Port using Eina_Rectangle.
    
            * platform/graphics/IntRect.h:
            * platform/graphics/efl/IntRectEfl.cpp:
            (WebCore::IntRect::IntRect):
            (WebCore::IntRect::operator Eina_Rectangle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/IntRect.h
+++ third_party/WebKit/WebCore/platform/graphics/IntRect.h
@@ -57,6 +57,8 @@ typedef cairo_rectangle_int_t GdkRectangle;
 #endif
 #elif PLATFORM(HAIKU)
 class BRect;
+#elif PLATFORM(EFL)
+typedef struct _Eina_Rectangle Eina_Rectangle;
 #endif
 
 #if PLATFORM(WX)
@@ -159,6 +161,9 @@ public:
 #elif PLATFORM(HAIKU)
     explicit IntRect(const BRect&);
     operator BRect() const;
+#elif PLATFORM(EFL)
+    explicit IntRect(const Eina_Rectangle&);
+    operator Eina_Rectangle() const;
 #endif
 
 #if PLATFORM(CG)
