ac256d468bbefa81c00e52562d29866642320639
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-08 09:22:16 +0000

    WTR first compile: fix compile issues. https://bugs.webkit.org/show_bug.cgi?id=47343
    Patch by Jocelyn Turcotte <jocelyn.turcotte@nokia.com> on 2010-10-07
    Reviewed by Kenneth Rohde Christiansen.
    
    * WebKitTestRunner/InjectedBundle/Bindings/JSWrappable.h:
    * WebKitTestRunner/InjectedBundle/InjectedBundle.cpp:
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    * WebKitTestRunner/InjectedBundle/LayoutTestController.h:
    * WebKitTestRunner/TestController.cpp:
    * WebKitTestRunner/TestInvocation.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/Bindings/JSWrappable.h
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/InjectedBundle/Bindings/JSWrappable.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JSWrappable_h
 #define JSWrappable_h
 
-#include <JavaScriptCore/JavaScriptCore.h>
+#include <JavaScriptCore/JavaScript.h>
 #include <wtf/RefCounted.h>
 
 namespace WTR {
--- third_party/WebKit/WebKitTools/WebKitTestRunner/TestInvocation.cpp
+++ third_party/WebKit/WebKitTools/WebKitTestRunner/TestInvocation.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformWebView.h"
 #include "StringFunctions.h"
 #include "TestController.h"
+#include <cstdio>
 #include <WebKit2/WKContextPrivate.h>
 #include <WebKit2/WKRetainPtr.h>
 #include <wtf/OwnArrayPtr.h>
