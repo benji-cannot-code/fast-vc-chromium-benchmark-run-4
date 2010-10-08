d3fc7ea6885730731b5a5f10b2a04f8bc82099a9
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-08 00:38:19 +0000

    Do not use Alternate-Protocol for establishing new SpdySessions.
    Only use Alternate-Protocol for latching onto existing SpdySessions.
    This is a temporary change until SSL improvements are made, so we
    can begin sending http over spdy when available.
    
    Note that we have to do proxy resolution one time to figure out how to check for the appropriate existing SpdySession.  If no SpdySession exists, we fallback again and have to redo proxy resolution (since the alternate protocol of npn-spdy requires rewriting the URI from http:// to https://).  So we may end up doing two proxy resolutions in the fallback case.
    
    I had to disable all the tests that test establishing a new
    SpdySession for http:// URIs, since we don't do that for now.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3573013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61897 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory.cc
+++ net/http/http_stream_factory.cc
@@ -28,6 +28,8 @@ bool HttpStreamFactory::force_spdy_over_ssl_ = true;
 bool HttpStreamFactory::force_spdy_always_ = false;
 // static
 bool HttpStreamFactory::ignore_certificate_errors_ = false;
+// static
+bool HttpStreamFactory::g_create_new_spdy_session_for_http_ = false;
 
 // static
 void HttpStreamFactory::SetHostMappingRules(const std::string& rules) {
@@ -139,4 +141,3 @@ GURL HttpStreamFactory::ApplyHostMappingRules(const GURL& url,
 }
 
 }  // namespace net
-
