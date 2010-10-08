b9656897963715a6cbc5e1fac036e5223194578a
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-08 00:48:39 +0000

    2010-10-07  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Adam Barth.
    
            rebaseline-chromium-webkit-tests relied on the filename_to_uri()
            hook in the Port infrastructure to generate URIs for the files
            in its summary HTML report; however, that method is supposed to only
            be used for test files (and should really be renamed), so this would
            crash.
    
            This change adds a new "path" module to the system package with a
            routine called abspath_to_uri() that handles converting paths to
            file: URIs independently of anything in the layout_tests package,
            and changes the code to use this. At some point in the near future
            the layout_tests/port/* code should use this as well.
    
            This change also deletes a bunch of unused code and fixes some
            comments in rebaseline_chromium_webkit_tests.py.
    
            https://bugs.webkit.org/show_bug.cgi?id=47319
    
            * Scripts/webkitpy/common/system/path.py: Added.
            * Scripts/webkitpy/common/system/path_unittest.py: Added.
            * Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests_unittest.py
@@ -31,10 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Unit tests for rebaseline_chromium_webkit_tests.py."""
 
 import os
+import sys
 import unittest
 
 from webkitpy.layout_tests import port
 from webkitpy.layout_tests import rebaseline_chromium_webkit_tests
+from webkitpy.common.system.executive import Executive, ScriptError
 
 
 class MockPort(object):
@@ -46,8 +48,9 @@ class MockPort(object):
 
 
 class MockOptions(object):
-    def __init__(self):
-        self.configuration = None
+    def __init__(self, configuration=None, html_directory=None):
+        self.configuration = configuration
+        self.html_directory = html_directory
 
 
 def get_mock_get(config_expectations):
@@ -118,6 +121,32 @@ class TestRebaseliner(unittest.TestCase):
                          'passes/image.html'))
         self.assertFalse(rebaseliner._diff_baselines(image, image,
                                                      is_image=True))
+
+
+class TestHtmlGenerator(unittest.TestCase):
+    def make_generator(self, tests):
+        return rebaseline_chromium_webkit_tests.HtmlGenerator(
+            target_port=None,
+            options=MockOptions(html_directory="/tmp"),
+            platforms=['mac'],
+            rebaselining_tests=tests,
+            executive=Executive())
+
+    def test_generate_baseline_links(self):
+        orig_platform = sys.platform
+        orig_exists = os.path.exists
+
+        try:
+            sys.platform = 'darwin'
+            os.path.exists = lambda x: True
+            generator = self.make_generator(["foo.txt"])
+            links = generator._generate_baseline_links("foo", ".txt", "mac")
+            expected_links = '<td align=center><a href="file:///tmp/foo-expected-mac-old.txt">foo-expected.txt</a></td><td align=center><a href="file:///tmp/foo-expected-mac-new.txt">foo-expected.txt</a></td><td align=center><a href="file:///tmp/foo-expected-mac-diff.txt">Diff</a></td>'
+            self.assertEqual(links, expected_links)
+        finally:
+            sys.platform = orig_platform
+            os.path.exists = orig_exists
+
 
 if __name__ == '__main__':
     unittest.main()
