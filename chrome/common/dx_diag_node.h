fa9f37855871f96d6a586ccd266e3d94e17106f5
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-08 18:03:21 +0000

    Relanding 61904 with missing file this time.
    Original description:
    
    Added DirectX Diagnostics information to about:gpu on Windows.
    
    This includes the name of the GPU hardware and the driver version and release
    date and some other information that might potentially also be valuable in the
    GPU stats.
    
    TEST=try
    BUG=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61980 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/dx_diag_node.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// A tree of name value pairs that report contain DirectX diagnostic
+// information.
+
+#ifndef CHROME_COMMON_DX_DIAG_NODE_H_
+#define CHROME_COMMON_DX_DIAG_NODE_H_
+
+#include <map>
+#include <string>
+
+struct DxDiagNode {
+  std::map<std::string, std::string> values;
+  std::map<std::string, DxDiagNode> children;
+};
+
+#endif  // CHROME_COMMON_DX_DIAG_NODE_H_
--- chrome/gpu/gpu_info_collector.h
+++ chrome/gpu/gpu_info_collector.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
+#include "build/build_config.h"
+#include "chrome/common/dx_diag_node.h"
 #include "chrome/common/gpu_info.h"
 
 struct IDirect3D9;
@@ -28,6 +30,9 @@ bool CollectGraphicsInfoD3D(IDirect3D9* d3d, GPUInfo* gpu_info);
 
 // The GL version of collecting information
 bool CollectGraphicsInfoGL(GPUInfo* gpu_info);
+
+// Collect the DirectX Disagnostics information about the attached displays.
+bool GetDxDiagnostics(DxDiagNode* output);
 #endif
 
 }  // namespace gpu_info_collector
