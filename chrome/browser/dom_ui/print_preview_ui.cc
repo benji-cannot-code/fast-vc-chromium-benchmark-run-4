97979b28df17377f182953c69d24e8669578d9fd
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-09 00:53:09 +0000

    Fix nits in print preview CSS code.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3569021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62054 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/print_preview_ui.cc
+++ chrome/browser/dom_ui/print_preview_ui.cc
@@ -27,7 +27,7 @@ namespace {
 
 void SetLocalizedStrings(DictionaryValue* localized_strings) {
   localized_strings->SetString(std::string("title"),
-      l10n_util::GetStringUTF8(IDS_PRINTPREVIEW_TITLE));
+      l10n_util::GetStringUTF8(IDS_PRINT_PREVIEW_TITLE));
 }
 
 }  // namespace
@@ -53,7 +53,7 @@ void PrintPreviewUIHTMLSource::StartDataRequest(const std::string& path,
 
   static const base::StringPiece print_html(
       ResourceBundle::GetSharedInstance().GetRawDataResource(
-          IDR_PRINTPREVIEW_HTML));
+          IDR_PRINT_PREVIEW_HTML));
   const std::string full_html = jstemplate_builder::GetI18nTemplateHtml(
       print_html, &localized_strings);
 
