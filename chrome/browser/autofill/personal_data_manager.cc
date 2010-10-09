48fa582b12ebb070cf90db50b123adab4ef32393
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-09 00:30:18 +0000

    Release unique_ids_lock_ in Autofill PDM in case observers need to acquire it.
    BUG=58426
    TEST=none
    
    Review URL: http://codereview.chromium.org/3549028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/personal_data_manager.cc
+++ chrome/browser/autofill/personal_data_manager.cc
@@ -321,7 +321,15 @@ void PersonalDataManager::SetProfiles(std::vector<AutoFillProfile>* profiles) {
   // Read our writes to ensure consistency with the database.
   Refresh();
 
-  FOR_EACH_OBSERVER(Observer, observers_, OnPersonalDataChanged());
+  {
+    // We're now done with the unique IDs, and observers might call into a
+    // method that needs the lock, so release it.  For example, observers on Mac
+    // might call profiles() which calls LoadAuxiliaryProfiles(), which needs
+    // the lock.
+    AutoUnlock unlock(unique_ids_lock_);
+
+    FOR_EACH_OBSERVER(Observer, observers_, OnPersonalDataChanged());
+  }
 }
 
 void PersonalDataManager::SetCreditCards(
@@ -396,7 +404,15 @@ void PersonalDataManager::SetCreditCards(
     credit_cards_.push_back(new CreditCard(*iter));
   }
 
-  FOR_EACH_OBSERVER(Observer, observers_, OnPersonalDataChanged());
+  {
+    // We're now done with the unique IDs, and observers might call into a
+    // method that needs the lock, so release it.  For example, observers on Mac
+    // might call profiles() which calls LoadAuxiliaryProfiles(), which needs
+    // the lock.
+    AutoUnlock unlock(unique_ids_lock_);
+
+    FOR_EACH_OBSERVER(Observer, observers_, OnPersonalDataChanged());
+  }
 }
 
 // TODO(jhawkins): Refactor SetProfiles so this isn't so hacky.
