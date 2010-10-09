bebc82e0d712932f690c0c74d8126d849e723fe3
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-09 21:30:26 +0000

    Enable IndexedDB by default.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/3676001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -574,7 +574,7 @@ void BrowserRenderProcessHost::PropagateBrowserCommandLineToRenderer(
     switches::kDisableSharedWorkers,
     switches::kDisableApplicationCache,
     switches::kDisableDeviceOrientation,
-    switches::kEnableIndexedDatabase,
+    switches::kDisableIndexedDatabase,
     switches::kDisableSpeechInput,
     switches::kDisableGeolocation,
     switches::kShowPaintRects,
