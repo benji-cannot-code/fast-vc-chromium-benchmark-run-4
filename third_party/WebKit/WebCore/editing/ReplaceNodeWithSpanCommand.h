097c44aa07d8a3e0d12b5094ea8dbbbe21e8448d
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-09 19:49:48 +0000

    2010-10-08  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            Redo in ReplaceNodeWithSpanCommand is broken
            https://bugs.webkit.org/show_bug.cgi?id=47428
    
            The bug was caused by ReplaceNodeWithSpanCommand's inheriting from CompositeEditCommand,
            and ReplaceNodeWithSpanCommand's not implementing doReapply. Because ReplaceNodeWithSpanCommand's doApply
            directly modifies DOM and does not use simple edit commands while CompositeEditCommand's doReapply
            only calls reapply of compositing simple edit commands, ReplaceNodeWithSpanCommand's doReapply was no-op.
    
            Fixed the bug by changing the base class of ReplaceNodeWithSpanCommand to SimpleEditCommand.
            This allows ReplaceNodeWithSpanCommand's doReapply to call its doApply, which already supports redo operation.
    
            Test: editing/undo/replace-by-span-then-remove.html
    
            * editing/ApplyStyleCommand.h:
            (WebCore::ApplyStyleCommand::printStyle):
            * editing/ReplaceNodeWithSpanCommand.cpp:
            (WebCore::ReplaceNodeWithSpanCommand::ReplaceNodeWithSpanCommand):
            * editing/ReplaceNodeWithSpanCommand.h:
    2010-10-08  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Darin Adler.
    
            Redo in ReplaceNodeWithSpanCommand is broken
            https://bugs.webkit.org/show_bug.cgi?id=47428
    
            Added a test to ensure replacing a node with span can be redone,
            and doing so does not disrupt subsequent redo's.
    
            * editing/undo/replace-by-span-then-remove-expected.txt: Added.
            * editing/undo/replace-by-span-then-remove.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/ReplaceNodeWithSpanCommand.h
+++ third_party/WebKit/WebCore/editing/ReplaceNodeWithSpanCommand.h
@@ -39,7 +39,7 @@ namespace WebCore {
 class HTMLElement;
 
 // More accurately, this is ReplaceNodeWithSpanPreservingChildrenAndAttributesCommand
-class ReplaceNodeWithSpanCommand : public CompositeEditCommand {
+class ReplaceNodeWithSpanCommand : public SimpleEditCommand {
 public:
     static PassRefPtr<ReplaceNodeWithSpanCommand> create(PassRefPtr<Node> node)
     {
