dd0238675188abc655a938c887f8147e23e9a69f
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-10 00:49:33 +0000

    Add limits include for arm compiler to fix build break. TBR: piman BUG=None TEST=None
    Review URL: http://codereview.chromium.org/3679003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/native_theme_chromeos.cc
+++ chrome/browser/chromeos/native_theme_chromeos.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/native_theme_chromeos.h"
 
+#include <limits>
+
 #include "app/resource_bundle.h"
 #include "base/logging.h"
 #include "gfx/insets.h"
