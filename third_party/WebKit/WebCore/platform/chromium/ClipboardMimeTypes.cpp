61d2f4e754860c31a2dc74c09befe1b5e14c16cb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-10 19:48:58 +0000

    2010-10-10  Daniel Cheng  <dcheng@chromium.org>
            Reviewed by Adam Barth.
    
            [chromium] Clipboard no longer accepts 'text' as a synonym for 'text/plain'
            https://bugs.webkit.org/show_bug.cgi?id=47419
    
            This was a regression introduced in r68807. Updated the layout test to
            check for this case.
    
            * editing/pasteboard/dataTransfer-setData-getData-expected.txt:
            * editing/pasteboard/script-tests/dataTransfer-setData-getData.js:
            (runTest):
    2010-10-10  Daniel Cheng  <dcheng@chromium.org>
    
            Reviewed by Adam Barth.
    
            [chromium] Clipboard no longer accepts 'text' as a synonym for 'text/plain'
            https://bugs.webkit.org/show_bug.cgi?id=47419
    
            This was a regression introduced in r68807. mimeTypeText had the incorrect
            value.
    
            Test: editing/pasteboard/dataTransfer-setData-getData.html
    
            * platform/chromium/ClipboardMimeTypes.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/chromium/ClipboardMimeTypes.cpp
+++ third_party/WebKit/WebCore/platform/chromium/ClipboardMimeTypes.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-const char mimeTypeText[] = "text/plain";
+const char mimeTypeText[] = "text";
 const char mimeTypeTextPlain[] = "text/plain";
 const char mimeTypeTextPlainEtc[] = "text/plain;";
 const char mimeTypeTextHTML[] = "text/html";
