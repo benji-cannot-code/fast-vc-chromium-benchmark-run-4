a79d9a04d8883ccc2a27942382d6f48b305a5dd7
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-10 06:10:51 +0000

    Revert 62105 - Test change to fix a leak.  Will revert after getting in a run on ChromiumOS valgrind bots. Revert "Revert 60739 (still leaks on ChromiumOS!) - Reland r59972: Eagerly set the IO loop used for OCSP."
    Review URL: http://codereview.chromium.org/3681003
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/3669003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62107 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_script_fetcher.cc
+++ net/proxy/proxy_script_fetcher.cc
@@ -5,15 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/proxy/proxy_script_fetcher.h"
 
-#include <set>
-
 #include "base/compiler_specific.h"
 #include "base/i18n/icu_string_conversions.h"
-#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/ref_counted.h"
-#include "base/stl_util-inl.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "net/base/io_buffer.h"
@@ -74,43 +70,7 @@ void ConvertResponseToUTF16(const std::string& charset,
                         utf16);
 }
 
-class ProxyScriptFetcherTracker {
- public:
-  ProxyScriptFetcherTracker();
-  ~ProxyScriptFetcherTracker();
-
-  void AddFetcher(ProxyScriptFetcher* fetcher) {
-    DCHECK(!ContainsKey(fetchers_, fetcher));
-    fetchers_.insert(fetcher);
-  }
-
-  void RemoveFetcher(ProxyScriptFetcher* fetcher) {
-    DCHECK(ContainsKey(fetchers_, fetcher));
-    fetchers_.erase(fetcher);
-  }
-
-  void CancelAllFetches() {
-    for (std::set<ProxyScriptFetcher*>::const_iterator it = fetchers_.begin();
-         it != fetchers_.end(); ++it) {
-      (*it)->Cancel();
-    }
-  }
-
- private:
-  std::set<ProxyScriptFetcher*> fetchers_;
-};
-
-ProxyScriptFetcherTracker::ProxyScriptFetcherTracker() {}
-ProxyScriptFetcherTracker::~ProxyScriptFetcherTracker() {}
-
-base::LazyInstance<ProxyScriptFetcherTracker>
-    g_fetcher_tracker(base::LINKER_INITIALIZED);
-
 }  // namespace
-
-void EnsureNoProxyScriptFetches() {
-  g_fetcher_tracker.Get().CancelAllFetches();
-}
 
 class ProxyScriptFetcherImpl : public ProxyScriptFetcher,
                                public URLRequest::Delegate {
@@ -201,11 +161,9 @@ ProxyScriptFetcherImpl::ProxyScriptFetcherImpl(
       result_code_(OK),
       result_text_(NULL) {
   DCHECK(url_request_context);
-  g_fetcher_tracker.Get().AddFetcher(this);
 }
 
 ProxyScriptFetcherImpl::~ProxyScriptFetcherImpl() {
-  g_fetcher_tracker.Get().RemoveFetcher(this);
   // The URLRequest's destructor will cancel the outstanding request, and
   // ensure that the delegate (this) is not called again.
 }
@@ -408,6 +366,5 @@ size_t ProxyScriptFetcher::SetSizeConstraintForUnittest(size_t size_bytes) {
   max_response_bytes = size_bytes;
   return prev;
 }
-
 
 }  // namespace net
