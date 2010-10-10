08c1693c7ce29f00c57496e6317914e96e02946e
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-10 22:06:46 +0000

    Removes the #ifdef around Instant-related AutocompleteEditController methods.  Adds empty implementations on Mac and Linux.
    BUG=56385
    TEST=No visible impact.
    Review URL: http://codereview.chromium.org/3625004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62125 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
@@ -514,10 +514,8 @@ void AutocompleteEditViewGtk::UpdatePopup() {
 }
 
 void AutocompleteEditViewGtk::ClosePopup() {
-#if defined(TOOLKIT_VIEWS)
   if (popup_view_->GetModel()->IsOpen())
     controller_->OnAutocompleteWillClosePopup();
-#endif
 
   popup_view_->GetModel()->StopAutocomplete();
 }
@@ -949,11 +947,9 @@ gboolean AutocompleteEditViewGtk::HandleViewFocusIn(GtkWidget* sender,
 
 gboolean AutocompleteEditViewGtk::HandleViewFocusOut(GtkWidget* sender,
                                                      GdkEventFocus* event) {
-#if defined(TOOLKIT_VIEWS)
   // This must be invoked before ClosePopup.
   // TODO: figure out who is getting focus.
   controller_->OnAutocompleteLosingFocus(NULL);
-#endif
 
   // Close the popup.
   ClosePopup();
