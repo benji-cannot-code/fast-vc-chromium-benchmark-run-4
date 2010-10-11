895da286eb78d5339880cc29a7b4089c8bd00198
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 21:44:35 +0000

    Load COMPONENT extensions even when extensions are disabled This fixes issue 55475 (the Bookmarks manager being blocked with --disable-extensions)
    BUG=55475
    TEST=ExtensionsServiceTest
    
    Review URL: http://codereview.chromium.org/3711002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -1449,6 +1449,7 @@ void ExtensionsService::OnExtensionLoaded(Extension* extension,
   if (extensions_enabled() ||
       extension->is_theme() ||
       extension->location() == Extension::LOAD ||
+      extension->location() == Extension::COMPONENT ||
       Extension::IsExternalLocation(extension->location())) {
     Extension* old = GetExtensionByIdInternal(extension->id(), true, true);
     if (old) {
