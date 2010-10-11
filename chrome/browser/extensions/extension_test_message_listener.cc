c5839034151705738b65a66b36b231a29beda099
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 15:08:54 +0000

    Add more temporartraces to track down test failure on the bots.
    BUG=57694
    TEST=AllUrlsApiTest.WhitelistedExtension
    
    Review URL: http://codereview.chromium.org/3687004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62145 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_test_message_listener.cc
+++ chrome/browser/extensions/extension_test_message_listener.cc
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_type.h"
 #include "chrome/test/ui_test_utils.h"
 
+// TODO(finnur): Remove after capturing debug info.
+#include <iostream>
+#include "chrome/common/extensions/extension.h"
+
 ExtensionTestMessageListener::ExtensionTestMessageListener(
     const std::string& expected_message,
     bool will_reply)
@@ -45,6 +49,12 @@ void ExtensionTestMessageListener::Observe(
     const NotificationDetails& details) {
   const std::string& content = *Details<std::string>(details).ptr();
   function_ = Source<ExtensionTestSendMessageFunction>(source).ptr();
+  // TODO(finnur): Remove after capturing debug info.
+  if (Extension::emit_traces_for_whitelist_extension_test_) {
+    std::cout << "-*-*- Got     : " << content.c_str() << "\n" << std::flush;
+    std::cout << "-*-*- Expected: " << expected_message_.c_str() << "\n" << std::flush;
+  }
+
   if (!satisfied_ && content == expected_message_) {
     satisfied_ = true;
     registrar_.RemoveAll();  // Stop listening for more messages.
