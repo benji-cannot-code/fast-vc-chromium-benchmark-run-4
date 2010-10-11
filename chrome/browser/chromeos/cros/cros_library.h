0ec50f66d8eccbb49fc32c90c1e7140a6218c5da
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 18:58:22 +0000

    Added cellular device details to settings page. Added a new chrome switch --force-stub-libcros to force stub libcros use (even on the device). This should help us isolate chrome from the rest of OS for testing purposes (VM autotest runs, testing change like this one on the device).
    BUG=chromium-os:7569
    TEST=none
    
    Review URL: http://codereview.chromium.org/3655003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cros_library.h
+++ chrome/browser/chromeos/cros/cros_library.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include "base/basictypes.h"
+#include "base/command_line.h"
 #include "base/scoped_ptr.h"
 #include "base/singleton.h"
+#include "chrome/common/chrome_switches.h"
 namespace chromeos {
 
 class BurnLibrary;
@@ -167,6 +169,9 @@ class CrosLibrary {
     L* GetDefaultImpl(bool use_stub_impl) {
       if (!library_) {
         own_ = true;
+        if (CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kForceStubLibcros))
+          use_stub_impl = true;
         library_ = L::GetImpl(use_stub_impl);
       }
       return library_;
