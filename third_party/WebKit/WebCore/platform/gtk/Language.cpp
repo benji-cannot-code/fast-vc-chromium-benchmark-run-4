a46abc8ed4d5a7c614fb06b2b4afa42966d1c2ec
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 15:02:00 +0000

    2010-10-11  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Unreviewed. Remove unnecessary GTK+ and Pango includes.
    
            * platform/gtk/FileChooserGtk.cpp:
            * platform/gtk/Language.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69492 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/Language.cpp
+++ third_party/WebKit/WebCore/platform/gtk/Language.cpp
@@ -25,9 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 #include <wtf/text/CString.h>
 
-#include <gtk/gtk.h>
+#include <glib.h>
 #include <locale.h>
-#include <pango/pango.h>
 
 namespace WebCore {
 
