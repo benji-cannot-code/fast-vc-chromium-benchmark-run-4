61a03a762e26d9383fb55e045beea6cb3fdd6f16
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 04:18:19 +0000

    Autofill - "Clear form" failed to clear the select popup values
    Enhances Autofill "Clear form" functionality to reset select-one input elements to their original values.  The initial values are captured during form load and saved in the FormManager cache.
    
    BUG=50412
    TEST=FormManagerTest.ClearFormWithNodeContainingSelectOne
    
    Review URL: http://codereview.chromium.org/3707001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62133 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/form_field.h
+++ webkit/glue/form_field.h
@@ -68,7 +68,7 @@ class FormField {
 };
 
 // So we can compare FormFields with EXPECT_EQ().
-std::ostream& operator<<(std::ostream& os, const FormField& profile);
+std::ostream& operator<<(std::ostream& os, const FormField& field);
 
 }  // namespace webkit_glue
 
