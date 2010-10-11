1c19cc8dbdc817c4116eb0d1dc7e2a9043a0fd51
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 22:15:21 +0000

    Implement SharedMemory on Windows
    This makes visited links be colored correctly.
    
    Fixes <http://webkit.org/b/47499> <rdar://problem/8422725>.
    
    Reviewed by Anders Carlsson.
    
    * Platform/SharedMemory.h: Add Windows-specific members to
    SharedMemory and SharedMemory::Handle.
    
    * Platform/win/SharedMemoryWin.cpp:
    (WebKit::SharedMemory::Handle::Handle): Initialize our members.
    (WebKit::SharedMemory::Handle::~Handle): Close our HANDLE if we have
    one.
    (WebKit::SharedMemory::Handle::encode): Encode our size, HANDLE, and
    PID. Null out our HANDLE member, as it is now the receiving process's
    responsibility to close the HANDLE.
    (WebKit::SharedMemory::Handle::decode): Copy the handle from the
    sending process into the receiving process and close the HANDLE the
    sending process gave us.
    (WebKit::SharedMemory::create): Map some memory and store it in a new
    SharedMemory object.
    (WebKit::accessRights): Helper function to convert a
    SharedMemory::Protection to a file-mapping access right.
    (WebKit::SharedMemory::create): Map the memory represented by the
    Handle, and adopt the HANDLE from it.
    (WebKit::SharedMemory::~SharedMemory): Clean up our memory mapping.
    (WebKit::SharedMemory::createHandle): Give the Handle a copy of our
    HANDLE with the specified protection.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/SharedMemory.h
+++ third_party/WebKit/WebKit2/Platform/SharedMemory.h
@@ -59,6 +59,8 @@ public:
         friend class SharedMemory;
 #if PLATFORM(MAC)
         mutable mach_port_t m_port;
+#elif PLATFORM(WIN)
+        mutable HANDLE m_handle;
 #endif
         size_t m_size;
     };
@@ -82,6 +84,9 @@ public:
 private:
     size_t m_size;
     void* m_data;
+#if PLATFORM(WIN)
+    HANDLE m_handle;
+#endif
 };
 
 };
