8040964d6d744b5fbfbf8d37003569645290c3cc
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 22:55:04 +0000

    Revert 62193 - Final phase of the URL History Quick autocomplete provider integration.  This phase hooks up the InMemoryURLIndex to the HistoryQuickProvider which is itself made active during the autocomplete process. A small amount of refactoring was done to liberate some common functionality from the history_url_provider.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3375002
    
    TBR=mrossetti@chromium.org
    Review URL: http://codereview.chromium.org/3660007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history.cc
+++ chrome/browser/history/history.cc
@@ -43,12 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/in_memory_database.h"
 #include "chrome/browser/history/in_memory_history_backend.h"
 #include "chrome/browser/history/top_sites.h"
-#include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/visitedlink_master.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/notification_service.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/common/thumbnail_score.h"
 #include "chrome/common/url_constants.h"
 #include "grit/chromium_strings.h"
@@ -231,7 +229,7 @@ history::InMemoryURLIndex* HistoryService::InMemoryIndex() {
   // for this call.
   LoadBackendIfNecessary();
   if (in_memory_backend_.get())
-    return in_memory_backend_->InMemoryIndex();
+    return in_memory_backend_->index();
   return NULL;
 }
 
@@ -756,13 +754,7 @@ void HistoryService::LoadBackendIfNecessary() {
                          bookmark_service_));
   history_backend_.swap(backend);
 
-  // There may not be a profile when unit testing.
-  std::string languages;
-  if (profile_) {
-    PrefService* prefs = profile_->GetPrefs();
-    languages = prefs->GetString(prefs::kAcceptLanguages);
-  }
-  ScheduleAndForget(PRIORITY_UI, &HistoryBackend::Init, languages, no_db_);
+  ScheduleAndForget(PRIORITY_UI, &HistoryBackend::Init, no_db_);
 }
 
 void HistoryService::OnDBLoaded() {
--- chrome/browser/history/in_memory_history_backend.h
+++ chrome/browser/history/in_memory_history_backend.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_HISTORY_IN_MEMORY_HISTORY_BACKEND_H_
 #pragma once
 
-#include <string>
-
 #include "base/basictypes.h"
 #include "base/gtest_prod_util.h"
 #include "base/scoped_ptr.h"
@@ -41,9 +39,7 @@ class InMemoryHistoryBackend : public NotificationObserver {
   ~InMemoryHistoryBackend();
 
   // Initializes with data from the given history database.
-  bool Init(const FilePath& history_filename,
-            URLDatabase* db,
-            const std::string& languages);
+  bool Init(const FilePath& history_filename, URLDatabase* db);
 
   // Does initialization work when this object is attached to the history
   // system on the main thread. The argument is the profile with which the
@@ -57,13 +53,16 @@ class InMemoryHistoryBackend : public NotificationObserver {
     return db_.get();
   }
 
+  // Returns the in memory index owned by this backend.  This index is only
+  // loaded when the --enable-in-memory-url-index flag is used.
+  InMemoryURLIndex* index() const {
+    return index_.get();
+  }
+
   // Notification callback.
   virtual void Observe(NotificationType type,
                        const NotificationSource& source,
                        const NotificationDetails& details);
-
-  // Return the quick history index.
-  history::InMemoryURLIndex* InMemoryIndex() const { return index_.get(); }
 
  private:
   FRIEND_TEST_ALL_PREFIXES(HistoryBackendTest, DeleteAll);
@@ -78,12 +77,11 @@ class InMemoryHistoryBackend : public NotificationObserver {
 
   scoped_ptr<InMemoryDatabase> db_;
 
+  scoped_ptr<InMemoryURLIndex> index_;
+
   // The profile that this object is attached. May be NULL before
   // initialization.
   Profile* profile_;
-
-  // The index used for quick history lookups.
-  scoped_ptr<history::InMemoryURLIndex> index_;
 
   DISALLOW_COPY_AND_ASSIGN(InMemoryHistoryBackend);
 };
