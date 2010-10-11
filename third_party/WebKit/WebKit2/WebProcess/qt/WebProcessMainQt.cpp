e853c69d35e6278645f160257b726d30958ee41a
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 16:08:17 +0000

    Build fix when using MeeGo Touch
    Rubberstamped by Andreas Kling.
    
    * WebProcess/qt/WebProcessMainQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/qt/WebProcessMainQt.cpp
+++ third_party/WebKit/WebKit2/WebProcess/qt/WebProcessMainQt.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtGlobal>
 
 #if USE(MEEGOTOUCH)
-#include <meegotouch/MComponentData>
+#include <MComponentData>
 #endif
 
 #if !defined(QWEBKIT_EXPORT)
