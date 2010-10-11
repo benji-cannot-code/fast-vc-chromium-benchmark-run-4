28127124a24383578f70085b13b17dce786f87a4
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 14:43:16 +0000

    Unreviewed win build fix.
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "DrawingArea.h"
 #include "InjectedBundleUserMessageCoders.h"
-#include "ViewportArguments.h"
 #include "WebCoreArgumentCoders.h"
 #include "WebFrame.h"
 #include "WebFrameLoaderClient.h"
