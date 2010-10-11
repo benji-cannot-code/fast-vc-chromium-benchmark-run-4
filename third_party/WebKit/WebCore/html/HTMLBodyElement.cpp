74e23fdad59c1a227312f27c3707017464a7991b
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 18:12:33 +0000

    2010-10-11  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            spellcheck="false" not respected in designMode
            https://bugs.webkit.org/show_bug.cgi?id=39978
    
            The bug was caused by WebKit's setting focus to html element rather than body element in design mode.
            Because we disable spell-check only if ancestor nodes of the focused element had spellcheck=false,
            this caused us to ignore body element's spellcheck attribute.
    
            Fixed the bug by overriding supportsFocus in HTMLBodyElement to make body element focusable
            when the parent node of body elements (namely html element) is editable.
    
            Test: editing/spelling/design-mode-spellcheck-off.html
    
            * html/HTMLBodyElement.cpp:
            (WebCore::HTMLBodyElement::supportsFocus): Added.
            * html/HTMLBodyElement.h:
    2010-10-11  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Darin Adler.
    
            spellcheck="false" not respected in designMode
            https://bugs.webkit.org/show_bug.cgi?id=39978
    
            Added a test to ensure Webkit respects spellcheck=off on body element.
            Rebaselined are due to our setting the focus to body element rather than html element in design mode.
    
            * editing/spelling/design-mode-spellcheck-off-expected.txt: Added.
            * editing/spelling/design-mode-spellcheck-off.html: Added.
            * editing/inserting/space-after-removeformat-expected.txt: Editing delegates change due to focus moving to body.
            * platform/chromium-win/editing/inserting/editable-html-element-expected.txt: Ditto.
            * platform/chromium-win/editing/selection/editable-html-element-expected.txt: Ditto.
            * platform/chromium-win/editing/selection/editable-links-expected.txt: Ditto.
            * platform/chromium-win/editing/selection/focus_editable_html-expected.txt: Ditto.
            * platform/chromium-win/editing/style/designmode-expected.txt: Ditto.
            * platform/mac/editing/inserting/editable-html-element-expected.txt: Ditto.
            * platform/mac/editing/selection/designmode-no-caret-expected.txt: Ditto.
            * platform/mac/editing/selection/editable-html-element-expected.txt: Ditto.
            * platform/mac/editing/selection/editable-links-expected.txt: Ditto.
            * platform/mac/editing/selection/focus_editable_html-expected.txt: Ditto.
            * platform/mac/editing/style/designmode-expected.txt: Ditto.
            * platform/qt/editing/inserting/editable-html-element-expected.txt: Ditto.
            * platform/qt/editing/selection/editable-html-element-expected.txt: Ditto.
            * platform/qt/editing/selection/focus_editable_html-expected.txt: Ditto.
            * platform/qt/editing/style/designmode-expected.txt: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLBodyElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLBodyElement.cpp
@@ -207,6 +207,11 @@ bool HTMLBodyElement::isURLAttribute(Attribute *attr) const
     return attr->name() == backgroundAttr;
 }
 
+bool HTMLBodyElement::supportsFocus() const
+{
+    return isContentEditable() || HTMLElement::supportsFocus();
+}
+
 String HTMLBodyElement::aLink() const
 {
     return getAttribute(alinkAttr);
--- third_party/WebKit/WebCore/html/HTMLBodyElement.h
+++ third_party/WebKit/WebCore/html/HTMLBodyElement.h
@@ -78,6 +78,8 @@ private:
     void createLinkDecl();
     
     virtual bool isURLAttribute(Attribute*) const;
+    
+    virtual bool supportsFocus() const;
 
     virtual int scrollLeft() const;
     virtual void setScrollLeft(int scrollLeft);
