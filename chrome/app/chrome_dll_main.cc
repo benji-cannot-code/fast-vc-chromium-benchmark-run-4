b271d6659b541639731e7a77815f20a729a8f04d
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-11 22:59:48 +0000

    Make writing to stdout and stderr work in Chrome processes when inherited handles are available.
    Don't create a console, just attach to any existing console.
    
    R=cpu
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3574008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62205 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_dll_main.cc
+++ chrome/app/chrome_dll_main.cc
@@ -14,8 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <atlbase.h>
 #include <atlapp.h>
+#include <fcntl.h>
+#include <io.h>
 #include <malloc.h>
 #include <new.h>
+#include <process.h>
 #elif defined(OS_POSIX)
 #include <locale.h>
 #include <signal.h>
@@ -320,6 +323,19 @@ void RegisterInvalidParamHandler() {
 #endif
 }
 
+#if defined(OS_WIN)
+void SetupStdHandle(DWORD name, int target_fd, FILE* target_fp) {
+  HANDLE handle = GetStdHandle(name);
+  if (handle && handle != INVALID_HANDLE_VALUE) {
+    int fd = _open_osfhandle(reinterpret_cast<intptr_t>(handle), _O_TEXT);
+    if (fd != -1) {
+      _dup2(fd, target_fd);
+      target_fp->_file = target_fd;
+    }
+  }
+}
+#endif
+
 void SetupCRT(const CommandLine& parsed_command_line) {
 #if defined(OS_WIN)
 #ifdef _CRTDBG_MAP_ALLOC
@@ -342,6 +358,12 @@ void SetupCRT(const CommandLine& parsed_command_line) {
     HeapSetInformation(crt_heap, HeapCompatibilityInformation,
                        &enable_lfh, sizeof(enable_lfh));
   }
+
+  // Attach to an existing console if one exists, and connect up the CRT
+  // handles for stdout/stderr.
+  AttachConsole(ATTACH_PARENT_PROCESS);
+  SetupStdHandle(STD_OUTPUT_HANDLE, 1, stdout);
+  SetupStdHandle(STD_ERROR_HANDLE, 2, stderr);
 #endif
 }
 
