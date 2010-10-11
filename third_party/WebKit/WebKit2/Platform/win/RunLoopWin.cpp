bb8ae24ae6fd1b50168641064f22ab0de67070f2
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-11 22:14:36 +0000

    Make it possible to restart a RunLoop::Timer on Windows
    Fixes <http://webkit.org/b/47505> RunLoop::Timer only ever fires once
    on Windows
    
    Reviewed by Anders Carlsson.
    
    * Platform/win/RunLoopWin.cpp:
    (RunLoop::TimerBase::timerFired): Remove non-repeating timers from the
    set of active timers when they fire, just like we do when stopping
    them manually.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/win/RunLoopWin.cpp
+++ third_party/WebKit/WebKit2/Platform/win/RunLoopWin.cpp
@@ -124,7 +124,9 @@ void RunLoop::TimerBase::timerFired(RunLoop* runLoop, uint64_t ID)
 
     // FIMXE: Support repeating timers.
 
+    runLoop->m_activeTimers.remove(it);
     ::KillTimer(runLoop->m_runLoopMessageWindow, ID);
+
     timer->fired();
 }
 
