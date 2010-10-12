d46b4624e68d34d124d31a2c234d89c64147e97a
andybons@chromium.org <andybons@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 17:00:59 +0000

    Enable ExtensionApiTest.Tabs on Mac and remove FLAKY prefix from Win.
    BUG=53779,58269
    TEST=ExtensionApiTest.Tabs
    
    Review URL: http://codereview.chromium.org/3627003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -10,19 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profile.h"
 #include "chrome/common/pref_names.h"
 
-#if defined(OS_MACOSX)
-// Tabs appears to timeout, or maybe crash on mac.
-// http://crbug.com/53779
-#define MAYBE_Tabs FAILS_Tabs
-#elif defined(OS_WIN)
-// It's flaky on win.
-// http://crbug.com/58269
-#define MAYBE_Tabs FLAKY_Tabs
-#else
-#define MAYBE_Tabs Tabs
-#endif
-
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Tabs) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
   ASSERT_TRUE(test_server()->Start());
 
   // The test creates a tab and checks that the URL of the new tab
