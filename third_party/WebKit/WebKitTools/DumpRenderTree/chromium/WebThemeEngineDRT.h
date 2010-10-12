2a22e8ad04ea60311abbf788fb20c776460de77f
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-12 18:37:38 +0000

    2010-10-12  Tony Chang  <tony@chromium.org>
            Unreviewed, fixing DRT compile on Windows after r69586.
    
            * DumpRenderTree/chromium/WebThemeEngineDRT.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebThemeEngineDRT.h
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebThemeEngineDRT.h
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebThemeEngineDRT_h
 #define WebThemeEngineDRT_h
 
-#include "public/WebThemeEngine.h"
+#include "public/win/WebThemeEngine.h"
 #include <wtf/Noncopyable.h>
 
 class WebThemeEngineDRT : public WebKit::WebThemeEngine, public Noncopyable {
