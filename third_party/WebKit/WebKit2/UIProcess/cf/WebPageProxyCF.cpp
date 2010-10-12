de399c7aa332dfbd2ad25cee96bc9787f2367279
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-12 00:34:08 +0000

    Windows build fix. Add a needed include.
    * UIProcess/cf/WebPageProxyCF.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/cf/WebPageProxyCF.cpp
+++ third_party/WebKit/WebKit2/UIProcess/cf/WebPageProxyCF.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebBackForwardList.h"
 #include "WebData.h"
 
+#include <CoreFoundation/CFPropertyList.h>
+
 using namespace WebCore;
 
 namespace WebKit {
