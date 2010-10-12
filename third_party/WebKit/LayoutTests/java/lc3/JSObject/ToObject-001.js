af0716d1dc53cb390006306e96b99463ffdcdd05
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-12 02:12:51 +0000

    2010-10-11  Michael Saboff  <msaboff@apple.com>
            Reviewed by Darin Adler.
    
            Added and changed tests to verify that DOMWindow objects are
            create with appropriate constructors and that those constructors
            are not callable directly.
            https://bugs.webkit.org/show_bug.cgi?id=47422
    
            * fast/dom/Window/window-constructor-expected.txt: Added.
            * fast/dom/Window/window-constructor.html: Added.
            * fast/dom/Window/window-properties-expected.txt:
            * fast/dom/Window/window-property-descriptors-expected.txt:
            * fast/dom/prototype-inheritance-2-expected.txt:
            * fast/dom/script-tests/constructors-cached.js:
            * fast/dom/wrapper-classes-expected.txt:
            * http/tests/security/cross-frame-access-get-expected.txt:
            * http/tests/security/cross-frame-access-get.html:
            * http/tests/security/cross-frame-access-put.html:
            * inspector/console-dir-global-expected.txt:
            * java/lc3/JSObject/ToJSObject-001-expected.txt:
            * java/lc3/JSObject/ToObject-001-expected.txt:
    2010-10-11  Michael Saboff  <msaboff@apple.com>
    
            Reviewed by Darin Adler.
    
            Changed DOMWindow to have a constructor.  Updated the code generator
            to add security checks to the constructors if CheckDomainSecurity is
            set.  Also changed the constructor generation code to use
            globalObject->prototype() for DOMWindow object prototypes instead
            of "self".
            https://bugs.webkit.org/show_bug.cgi?id=47422
    
            Test: fast/dom/Window/window-constructor.html
    
            * bindings/scripts/CodeGeneratorJS.pm:
            * page/DOMWindow.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/java/lc3/JSObject/ToObject-001.js
+++ third_party/WebKit/LayoutTests/java/lc3/JSObject/ToObject-001.js
@@ -176,7 +176,7 @@ a[i++] = new TestObject(
   "dt.getObject()",
   "dt.getObject().constructor",
   'this',
-  'Object');
+  'this.constructor');
 
 a[i++] = new TestObject(
   "dt.setObject( Math )",
