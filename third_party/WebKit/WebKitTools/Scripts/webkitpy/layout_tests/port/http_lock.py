01d360d5f0017f95aab67913899a3c430062c944
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-12 16:32:10 +0000

    2010-10-12  Tony Chang  <tony@chromium.org>
            Unreviewed, trying to fix NRWT on Windows.
    
            * Scripts/webkitpy/layout_tests/port/http_lock.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/http_lock.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/http_lock.py
@@ -29,10 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """This class helps to block NRWT threads when more NRWTs run
 http and websocket tests in a same time."""
 
-import fcntl
 import glob
 import os
-import signal
 import sys
 import tempfile
 import time
