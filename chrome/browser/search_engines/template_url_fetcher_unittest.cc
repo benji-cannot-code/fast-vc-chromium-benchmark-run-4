8e0171f40fca278afc6150f322ed54f3a054742a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 19:46:21 +0000

    Fix clang compile error introduced by recent change to template_url_fetcher_unittest
    BUG=None
    TEST=None
    TBR=levin
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_fetcher_unittest.cc
+++ chrome/browser/search_engines/template_url_fetcher_unittest.cc
@@ -206,7 +206,7 @@ TEST_F(TemplateURLFetcherTest, BasicAutodetectedTest) {
   ASSERT_TRUE(t_url);
   EXPECT_STREQ(L"http://example.com/%s/other_stuff",
                t_url->url()->DisplayURL().c_str());
-  EXPECT_EQ(true, t_url->safe_for_autoreplace());
+  EXPECT_TRUE(t_url->safe_for_autoreplace());
 }
 
 TEST_F(TemplateURLFetcherTest, DuplicatesThrownAway) {
@@ -276,7 +276,7 @@ TEST_F(TemplateURLFetcherTest, BasicExplicitTest) {
   ASSERT_TRUE(last_callback_template_url_.get());
   EXPECT_STREQ(L"http://example.com/%s/other_stuff",
                last_callback_template_url_->url()->DisplayURL().c_str());
-  EXPECT_EQ(false, last_callback_template_url_->safe_for_autoreplace());
+  EXPECT_FALSE(last_callback_template_url_->safe_for_autoreplace());
 }
 
 TEST_F(TemplateURLFetcherTest, BasicExplicitDefaultTest) {
@@ -300,7 +300,7 @@ TEST_F(TemplateURLFetcherTest, BasicExplicitDefaultTest) {
   ASSERT_TRUE(last_callback_template_url_.get());
   EXPECT_STREQ(L"http://example.com/%s/other_stuff",
                last_callback_template_url_->url()->DisplayURL().c_str());
-  EXPECT_EQ(false, last_callback_template_url_->safe_for_autoreplace());
+  EXPECT_FALSE(last_callback_template_url_->safe_for_autoreplace());
 }
 
 TEST_F(TemplateURLFetcherTest, AutodetectedBeforeLoadTest) {
@@ -346,7 +346,7 @@ TEST_F(TemplateURLFetcherTest, ExplicitDefaultBeforeLoadTest) {
   ASSERT_TRUE(last_callback_template_url_.get());
   EXPECT_STREQ(L"http://example.com/%s/other_stuff",
                last_callback_template_url_->url()->DisplayURL().c_str());
-  EXPECT_EQ(false, last_callback_template_url_->safe_for_autoreplace());
+  EXPECT_FALSE(last_callback_template_url_->safe_for_autoreplace());
 }
 
 TEST_F(TemplateURLFetcherTest, DuplicateKeywordsTest) {
