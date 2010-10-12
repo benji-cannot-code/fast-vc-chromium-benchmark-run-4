5ec15ff55a6340e3a6681e93947a6054c094c0f5
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 06:41:38 +0000

    Does not seem to be logging the gpu stats for any but the browser process. This line should actually set it for the gpu process.
    BUG=58836
    TEST=need to see the crash reports that come back
    
    Review URL: http://codereview.chromium.org/3653004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/gpu/gpu_thread.cc
+++ chrome/gpu/gpu_thread.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "chrome/common/child_process.h"
+#include "chrome/common/child_process_logging.h"
 #include "chrome/common/gpu_info.h"
 #include "chrome/common/gpu_messages.h"
 #include "chrome/gpu/gpu_info_collector.h"
@@ -107,6 +108,7 @@ void GpuThread::OnEstablishChannel(int renderer_id) {
   if (gfx::GLContext::InitializeOneOff()) {
     // Fail to establish channel if GPU stats cannot be retreived.
     if (gpu_info_collector::CollectGraphicsInfo(&gpu_info)) {
+      child_process_logging::SetGpuInfo(gpu_info);
       GpuChannelMap::const_iterator iter = gpu_channels_.find(renderer_id);
       if (iter == gpu_channels_.end()) {
         channel = new GpuChannel(renderer_id);
