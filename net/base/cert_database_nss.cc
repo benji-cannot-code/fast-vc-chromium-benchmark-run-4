3756091858d35681b4f3822cfaba67b9bfa7390b
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 02:17:08 +0000

    CertDatabase: Add ImportServerCerts method.
    BUG=19991
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/3576016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62228 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cert_database_nss.cc
+++ net/base/cert_database_nss.cc
@@ -149,6 +149,11 @@ bool CertDatabase::ImportCACerts(const CertificateList& certificates,
   return psm::ImportCACerts(certificates, root, trust_bits, not_imported);
 }
 
+bool CertDatabase::ImportServerCert(const CertificateList& certificates,
+                                    ImportCertFailureList* not_imported) {
+  return psm::ImportServerCert(certificates, not_imported);
+}
+
 unsigned int CertDatabase::GetCertTrust(
     const X509Certificate* cert, CertType type) const {
   CERTCertTrust nsstrust;
