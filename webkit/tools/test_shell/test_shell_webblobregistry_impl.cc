c0a046228d5e93e23932e7fa8debbd38b14648ac
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 22:54:46 +0000

    Remove unneeded using directives. This will fix Canary build break caused by removing unneeded files in WebKit API.
    BUG=none
    TEST=none
    TBR=darin
    
    Review URL: http://codereview.chromium.org/3727004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62349 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_webblobregistry_impl.cc
+++ webkit/tools/test_shell/test_shell_webblobregistry_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/blob/blob_storage_controller.h"
 
 using WebKit::WebBlobData;
-using WebKit::WebBlobStorageData;
 using WebKit::WebString;
 using WebKit::WebURL;
 
