2b3f1a4a0bd519c54c31a89ddd394f82fd6080bc
podivilov@chromium.org <podivilov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-12 16:30:05 +0000

    Inspector settings not updated in profile when inspecting extensions.
    BUG=56815
    TEST=
    
    Review URL: http://codereview.chromium.org/3582021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -2554,18 +2554,11 @@ void TabContents::UpdateThumbnail(const GURL& url,
 
 void TabContents::UpdateInspectorSetting(const std::string& key,
                                          const std::string& value) {
-  DictionaryValue* inspector_settings =
-      profile()->GetPrefs()->GetMutableDictionary(
-          prefs::kWebKitInspectorSettings);
-  inspector_settings->SetWithoutPathExpansion(key,
-                                              Value::CreateStringValue(value));
+  RenderViewHostDelegateHelper::UpdateInspectorSetting(profile(), key, value);
 }
 
 void TabContents::ClearInspectorSettings() {
-  DictionaryValue* inspector_settings =
-      profile()->GetPrefs()->GetMutableDictionary(
-          prefs::kWebKitInspectorSettings);
-  inspector_settings->Clear();
+  RenderViewHostDelegateHelper::ClearInspectorSettings(profile());
 }
 
 void TabContents::Close(RenderViewHost* rvh) {
