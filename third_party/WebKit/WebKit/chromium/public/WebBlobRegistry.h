da80da250e923591fcb121145834f014b5455b0f
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-12 22:47:30 +0000

    Unreview. Build fix for chromium.
    * public/WebBlobRegistry.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebBlobRegistry.h
+++ third_party/WebKit/WebKit/chromium/public/WebBlobRegistry.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebBlobRegistry_h
 #define WebBlobRegistry_h
 
+#include "WebCommon.h"
+
 namespace WebKit {
 
 class WebBlobData;
