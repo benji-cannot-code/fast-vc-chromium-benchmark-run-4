1cbc0e10bfe2a1132920c5af8685d0f7b8115ec7
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 19:15:44 +0000

    Unreviewed build fixes after r69538.
    * UIProcess/WebBackForwardList.h: Add missing include directive
    for CoreFoundation.
    * UIProcess/cf/WebBackForwardListCF.cpp: Add missing include
    directives for RetainPtr.h and CoreFoundation.
    * UIProcess/cf/WebPageProxyCF.cpp: Add missing include directive
    for RetainPtr.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebBackForwardList.h
+++ third_party/WebKit/WebKit2/UIProcess/WebBackForwardList.h
@@ -34,6 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
+#if PLATFORM(CF)
+#include <CoreFoundation/CFDictionary.h>
+#endif
 
 namespace WebKit {
 
