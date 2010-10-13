e5f78b99a2e30448c00b4bec6df4f77a8354e648
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 21:29:17 +0000

    Unreviewed WinCairo build fix. New ContextShadow class not built for Windows builds.
    * WebCore.vcproj/WebCore.vcproj: Add missing files for
    building ContextShadow on WinCairo.
    * platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h:
    Add missing include for ContextShadow definition.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69699 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h
+++ third_party/WebKit/WebCore/platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 
 #include "CairoPath.h"
+#include "ContextShadow.h"
 #include <cairo.h>
 #include <math.h>
 #include <stdio.h>
