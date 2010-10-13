9377e772ca6e129a99fcc3f226091523c3e73924
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 00:57:11 +0000

    2010-10-12  James Robinson  <jamesr@chromium.org>
            Unreviewed chromium compile fixes for http://trac.webkit.org/changeset/69619
    
            * platform/graphics/chromium/LayerRendererChromium.cpp:
            (WebCore::LayerRendererChromium::create):
            (WebCore::LayerRendererChromium::LayerRendererChromium):
            * platform/graphics/chromium/LayerRendererChromium.h:
    2010-10-12  James Robinson  <jamesr@chromium.org>
    
            Unreviewed chromium compile fixes for http://trac.webkit.org/changeset/69619.
    
            * src/GraphicsContext3DChromium.cpp:
            (WebCore::GraphicsContext3D::create):
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::setIsAcceleratedCompositingActive):
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/chromium/LayerRendererChromium.cpp
+++ third_party/WebKit/WebCore/platform/graphics/chromium/LayerRendererChromium.cpp
@@ -74,7 +74,7 @@ static inline bool compareLayerZ(const LayerChromium* a, const LayerChromium* b)
     return transformA.m43() < transformB.m43();
 }
 
-PassRefPtr<LayerRendererChromium> LayerRendererChromium::create(PassOwnPtr<GraphicsContext3D> context)
+PassRefPtr<LayerRendererChromium> LayerRendererChromium::create(PassRefPtr<GraphicsContext3D> context)
 {
     if (!context)
         return 0;
@@ -86,7 +86,7 @@ PassRefPtr<LayerRendererChromium> LayerRendererChromium::create(PassOwnPtr<Graph
     return layerRenderer.release();
 }
 
-LayerRendererChromium::LayerRendererChromium(PassOwnPtr<GraphicsContext3D> context)
+LayerRendererChromium::LayerRendererChromium(PassRefPtr<GraphicsContext3D> context)
     : m_rootLayerTextureId(0)
     , m_rootLayerTextureWidth(0)
     , m_rootLayerTextureHeight(0)
