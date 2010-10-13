6b0574a45654f83a773ce73a2e6002f0192a4b0c
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 22:53:32 +0000

    AX: accessibilityIsIgnored not respected on ARIA tables https://bugs.webkit.org/show_bug.cgi?id=47564
    Reviewed by Beth Dakin.
    
    WebCore: 
    
    For platforms that ignore column headers and header container objects (everything
    except Mac) on accessibility tables, that choice needs to be respected in 
    ARIA tables as well.
    
    GTK is the only open platform doing this, so I'll have to collect the result of this 
    test. It was copied from table-hierarchy.html, but changed to use ARIA tables.
    
    Test: platform/gtk/accessibility/aria-table-hierarchy.html
    
    * accessibility/AccessibilityARIAGrid.cpp:
    (WebCore::AccessibilityARIAGrid::addChildren):
    
    LayoutTests: 
    
    Will gather the results from GTK after this runs.
    
    * platform/gtk/accessibility/aria-table-hierarchy.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69707 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityARIAGrid.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityARIAGrid.cpp
@@ -119,11 +119,12 @@ void AccessibilityARIAGrid::addChildren()
         column->setColumnIndex((int)i);
         column->setParentTable(this);
         m_columns.append(column);
-        m_children.append(column);
+        if (!column->accessibilityIsIgnored())
+            m_children.append(column);
     }
     
     AccessibilityObject* headerContainerObject = headerContainer();
-    if (headerContainerObject)
+    if (headerContainerObject && !headerContainerObject->accessibilityIsIgnored())
         m_children.append(headerContainerObject);
 }
     
