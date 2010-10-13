2698687aecfea3622cff435600816695035e791f
dumi@chromium.org <dumi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 01:47:55 +0000

    Test that workers can be successfully interrupted in the middle of DB operations.
    Reviewed by David Levin.
    
    * fast/workers/storage/interrupt-database-expected.txt: Added.
    * fast/workers/storage/interrupt-database-sync-expected.txt: Added.
    * fast/workers/storage/interrupt-database-sync.html: Added.
    * fast/workers/storage/interrupt-database.html: Added.
    * fast/workers/storage/resources/interrupt-database-sync.js: Added.
    * fast/workers/storage/resources/interrupt-database.js: Added.
    (runTransaction):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/workers/storage/resources/interrupt-database-sync.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var db = openDatabaseSync("InterruptDatabaseSyncTest", "1", "", 1);
+db.transaction(function(tx) {
+    tx.executeSql("CREATE TABLE IF NOT EXISTS Test (Foo INT)");
+    var counter = 0;
+    while (true) {
+        // Put both statements on the same line, to make sure the exception is always reported on the same line.
+        tx.executeSql("INSERT INTO Test VALUES (1)"); tx.executeSql("DELETE FROM Test WHERE Foo = 1");
+        if (++counter == 100)
+            postMessage("terminate");
+    }
+});
