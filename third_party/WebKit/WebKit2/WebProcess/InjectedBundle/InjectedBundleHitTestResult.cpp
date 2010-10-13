a73ef984997bfaeb614dc4326874b4e5c9ef59c4
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 01:46:30 +0000

    Add API to get the target frame from a HitTestResult.
    Reviewed by Gavin "Sometimes" Barraclough.
    
    * WebProcess/InjectedBundle/API/c/WKBundleHitTestResult.cpp:
    (WKBundleHitTestResultGetFrame):
    (WKBundleHitTestResultGetTargetFrame):
    * WebProcess/InjectedBundle/API/c/WKBundleHitTestResult.h:
    * WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp:
    (WebKit::InjectedBundleHitTestResult::frame): Rename from webFrame.
    (WebKit::InjectedBundleHitTestResult::targetFrame):
    * WebProcess/InjectedBundle/InjectedBundleHitTestResult.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp
@@ -49,7 +49,7 @@ PassRefPtr<InjectedBundleNodeHandle> InjectedBundleHitTestResult::nodeHandle() c
     return InjectedBundleNodeHandle::getOrCreate(m_hitTestResult.innerNonSharedNode());
 }
 
-WebFrame* InjectedBundleHitTestResult::webFrame() const
+WebFrame* InjectedBundleHitTestResult::frame() const
 {
     Node* node = m_hitTestResult.innerNonSharedNode();
     if (!node)
@@ -60,6 +60,15 @@ WebFrame* InjectedBundleHitTestResult::webFrame() const
         return 0;
 
     Frame* frame = document->frame();
+    if (!frame)
+        return 0;
+
+    return static_cast<WebFrameLoaderClient*>(frame->loader()->client())->webFrame();
+}
+
+WebFrame* InjectedBundleHitTestResult::targetFrame() const
+{
+    Frame* frame = m_hitTestResult.targetFrame();
     if (!frame)
         return 0;
 
