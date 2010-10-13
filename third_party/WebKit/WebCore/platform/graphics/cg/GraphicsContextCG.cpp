c07a7f3007d8bba3aa4985bac01d397359210fc1
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-13 21:44:08 +0000

    Don't set the fill/stroke colorspace before filling/stroking https://bugs.webkit.org/show_bug.cgi?id=47619
    Reviewed by Beth Dakin.
    
    Setting the colorspace has the side-effect of setting the color to black, which is
    not what we want. When fill/stroke is called, a fill/stroke color should already have been set.
    
    Pixel tests show no regressions from this change.
    
    * platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContext::fillPath):
    (WebCore::GraphicsContext::strokePath):
    (WebCore::GraphicsContext::fillRect):
    (WebCore::GraphicsContext::strokeRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -80,34 +80,6 @@ static void setCGStrokeColor(CGContextRef context, const Color& color, ColorSpac
     CGContextSetStrokeColorWithColor(context, cachedCGColor(color, colorSpace));
 }
 
-static void setCGFillColorSpace(CGContextRef context, ColorSpace colorSpace)
-{
-    switch (colorSpace) {
-    case DeviceColorSpace:
-        break;
-    case sRGBColorSpace:
-        CGContextSetFillColorSpace(context, sRGBColorSpaceRef());
-        break;
-    default:
-        ASSERT_NOT_REACHED();
-        break;
-    }
-}
-
-static void setCGStrokeColorSpace(CGContextRef context, ColorSpace colorSpace)
-{
-    switch (colorSpace) {
-    case DeviceColorSpace:
-        break;
-    case sRGBColorSpace:
-        CGContextSetStrokeColorSpace(context, sRGBColorSpaceRef());
-        break;
-    default:
-        ASSERT_NOT_REACHED();
-        break;
-    }
-}
-
 CGColorSpaceRef deviceRGBColorSpaceRef()
 {
     static CGColorSpaceRef deviceSpace = CGColorSpaceCreateDeviceRGB();
@@ -569,9 +541,6 @@ void GraphicsContext::fillPath()
 
     CGContextRef context = platformContext();
 
-    // FIXME: Is this helpful and correct in the fillPattern and fillGradient cases?
-    setCGFillColorSpace(context, m_common->state.fillColorSpace);
-
     if (m_common->state.fillGradient) {
         CGContextSaveGState(context);
         if (fillRule() == RULE_EVENODD)
@@ -596,9 +565,6 @@ void GraphicsContext::strokePath()
 
     CGContextRef context = platformContext();
 
-    // FIXME: Is this helpful and correct in the strokePattern and strokeGradient cases?
-    setCGStrokeColorSpace(context, m_common->state.strokeColorSpace);
-
     if (m_common->state.strokeGradient) {
         CGContextSaveGState(context);
         CGContextReplacePathWithStrokedPath(context);
@@ -620,9 +586,6 @@ void GraphicsContext::fillRect(const FloatRect& rect)
         return;
 
     CGContextRef context = platformContext();
-
-    // FIXME: Is this helpful and correct in the fillPattern and fillGradient cases?
-    setCGFillColorSpace(context, m_common->state.fillColorSpace);
 
     if (m_common->state.fillGradient) {
         CGContextSaveGState(context);
@@ -845,9 +808,6 @@ void GraphicsContext::strokeRect(const FloatRect& r, float lineWidth)
         return;
 
     CGContextRef context = platformContext();
-
-    // FIXME: Is this helpful and correct in the strokePattern and strokeGradient cases?
-    setCGStrokeColorSpace(context, m_common->state.strokeColorSpace);
 
     if (m_common->state.strokeGradient) {
         CGContextSaveGState(context);
