57656d95cd15759eff95bc618a5e368601402ee8
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 23:35:36 +0000

    This patch lays the groundwork for https://bugs.webkit.org/show_bug.cgi?id=47514 CSS transforms should affect scrolling by adding topmostPosition().
    Reviewed by Dave Hyatt.
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::topmostPosition):
    * rendering/RenderBlock.h:
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::topmostPosition):
    * rendering/RenderBox.h:
    * rendering/RenderMedia.cpp:
    (WebCore::RenderMedia::topmostPosition):
    * rendering/RenderMedia.h:
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::topmostPosition):
    * rendering/RenderTableSection.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTableSection.h
+++ third_party/WebKit/WebCore/rendering/RenderTableSection.h
@@ -137,6 +137,7 @@ private:
 
     virtual void removeChild(RenderObject* oldChild);
 
+    virtual int topmostPosition(bool includeOverflowInterior, bool includeSelf) const;
     virtual int lowestPosition(bool includeOverflowInterior, bool includeSelf) const;
     virtual int rightmostPosition(bool includeOverflowInterior, bool includeSelf) const;
     virtual int leftmostPosition(bool includeOverflowInterior, bool includeSelf) const;
