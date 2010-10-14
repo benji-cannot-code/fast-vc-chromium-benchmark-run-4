a8efa680b5f67adc48d2544dfc10e1e929721889
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 22:56:16 +0000

    2010-10-14  Simon Fraser  <simon.fraser@apple.com>
            Fix the build after r69808.
    
            * WebCore.xcodeproj/project.pbxproj:
            * platform/network/cf/ProxyServerCFNet.cpp:
            (WebCore::proxyServersForURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/cf/ProxyServerCFNet.cpp
+++ third_party/WebKit/WebCore/platform/network/cf/ProxyServerCFNet.cpp
@@ -89,7 +89,7 @@ Vector<ProxyServer> proxyServersForURL(const KURL& url, const NetworkingContext*
     
 }
 #else
-Vector<ProxyServer> proxyServersForURL(const KURL&)
+Vector<ProxyServer> proxyServersForURL(const KURL&, const NetworkingContext*)
 {
     // FIXME: Implement.
     return Vector<ProxyServer>();
