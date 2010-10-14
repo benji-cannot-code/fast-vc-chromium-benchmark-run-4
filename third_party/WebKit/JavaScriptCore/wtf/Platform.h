28de3e8f59302a6492242fd96115c346b44c593b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 09:13:10 +0000

    2010-10-14  Anton Faern  <anton@bladehawke.com>
            Reviewed by Csaba Osztrogonác.
    
            https://bugs.webkit.org/show_bug.cgi?id=47658
            NetBSD was not included in the WTF_PLATFORM_FOO to WTF_OS_FOO
            change.  This means that OS(NETBSD) is also undefined.
    
            * wtf/Platform.h: s/_PLATFORM_/_OS_/ for NetBSD
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -399,7 +399,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* OS(NETBSD) - NetBSD */
 #if defined(__NetBSD__)
-#define WTF_PLATFORM_NETBSD 1
+#define WTF_OS_NETBSD 1
 #endif
 
 /* OS(OPENBSD) - OpenBSD */
