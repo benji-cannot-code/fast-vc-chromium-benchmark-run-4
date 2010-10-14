8ec13630950611b5429c2ba71bde70e470998992
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-14 01:58:50 +0000

    Add a pp_resource include to the two private interfaces, which depend on this.
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/3769003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62496 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/ppb_private2.h
+++ webkit/glue/plugins/ppb_private2.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/ppapi/c/pp_module.h"
 #include "third_party/ppapi/c/pp_point.h"
 #include "third_party/ppapi/c/pp_rect.h"
+#include "third_party/ppapi/c/pp_resource.h"
 #include "third_party/ppapi/c/pp_var.h"
 
 #define PPB_PRIVATE2_INTERFACE "PPB_Private2;2"
