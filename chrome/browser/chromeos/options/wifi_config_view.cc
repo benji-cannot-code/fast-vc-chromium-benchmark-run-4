128e2654fbab94e5b43dc67f3ef01548726e1247
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-14 16:35:55 +0000

    [cros] Disable existing WiFi password view on OOBE/login.
    BUG=chromium-os:6801
    TEST=Manual. Have WiFi network with password that's not set for auto connect. There's no way to see it's existing password on OOBE/login.
    
    Review URL: http://codereview.chromium.org/3750007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/wifi_config_view.cc
+++ chrome/browser/chromeos/options/wifi_config_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/options/network_config_view.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -83,9 +84,18 @@ void WifiConfigView::UpdateCanLogin(void) {
   }
 }
 
+void WifiConfigView::UpdateCanViewPassword() {
+  if (!passphrase_visible_button_->IsVisible() &&
+      passphrase_textfield_->text().empty()) {
+    // Once initial password has been deleted, it's safe to show field content.
+    passphrase_visible_button_->SetVisible(true);
+  }
+}
+
 void WifiConfigView::ContentsChanged(views::Textfield* sender,
                                      const string16& new_contents) {
   UpdateCanLogin();
+  UpdateCanViewPassword();
 }
 
 bool WifiConfigView::HandleKeystroke(
@@ -312,6 +322,11 @@ void WifiConfigView::Init() {
         GetBitmapNamed(IDR_STATUSBAR_NETWORK_SECURE));
     passphrase_visible_button_->SetImageAlignment(
         views::ImageButton::ALIGN_CENTER, views::ImageButton::ALIGN_MIDDLE);
+    // Disable viewing password by unauthenticated user.
+    if (!wifi_.passphrase().empty() &&
+        chromeos::UserManager::Get()->logged_in_user().email().empty()) {
+      passphrase_visible_button_->SetVisible(false);
+    }
     layout->AddView(passphrase_visible_button_);
     layout->AddPaddingRow(0, kRelatedControlVerticalSpacing);
   }
