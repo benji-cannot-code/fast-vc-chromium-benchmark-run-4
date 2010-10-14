97d60587f8988afbcafa45f87931ddc13535a45d
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 00:16:01 +0000

    Build fix - remove some redundant references to ScriptString.
    * WebCore.gypi:
    * WebCore.pro:
    * bindings/v8/ScriptProfiler.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/ScriptProfiler.cpp
+++ third_party/WebKit/WebCore/bindings/v8/ScriptProfiler.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptProfiler.h"
 
 #include "InspectorValues.h"
-#include "ScriptString.h"
 
 #include <v8-profiler.h>
 
