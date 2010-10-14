380d3061c5423402c161e7dbdc14063ee44f41f8
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 22:32:49 +0000

    2010-10-14  Tony Chang  <tony@chromium.org>
            Reviewed by Kent Tamura.
    
            [chromium] compile TestNetscapePlugIn on Chromium mac
            https://bugs.webkit.org/show_bug.cgi?id=47633
    
            * WebKit.gyp:  Add a mac specific target for compiling TestNetscapePlugIn
    2010-10-14  Tony Chang  <tony@chromium.org>
    
            Reviewed by Kent Tamura.
    
            [chromium] compile TestNetscapePlugIn on Chromium mac
            https://bugs.webkit.org/show_bug.cgi?id=47633
    
            * DumpRenderTree/DumpRenderTree.gypi: files to compile
            * DumpRenderTree/TestNetscapePlugIn/main.cpp: Use ifdef because gcc was complaining
            (NP_GetEntryPoints):
            (NPP_New):
            (NPP_Destroy):
            (NPP_HandleEvent):
            (NPP_GetValue):
            * DumpRenderTree/chromium/TestNetscapePlugIn/ForwardingHeaders/WebKit/npapi.h: Added.
            * DumpRenderTree/chromium/TestNetscapePlugIn/ForwardingHeaders/WebKit/npfunctions.h: Added.
            * DumpRenderTree/chromium/TestNetscapePlugIn/ForwardingHeaders/WebKit/npruntime.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/TestNetscapePlugIn/ForwardingHeaders/WebKit/npfunctions.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include "bindings/npfunctions.h"
+
+// Non-standard event types can be passed to HandleEvent.
+// npapi.h that comes with WebKit.framework adds these events.
+#define getFocusEvent       (osEvt + 16)
+#define loseFocusEvent      (osEvt + 17)
+#define adjustCursorEvent   (osEvt + 18)
