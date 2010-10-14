133f14d06c8d5fc3607c508b162d68dd5ab6e7c2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-14 19:22:38 +0000

    2010-10-14  Dawit Alemayehu  <adawit@kde.org>
            Reviewed by Andreas Kling.
    
            Fix compile error when bulding webkit's Qt port using the "--v8" option.
            https://bugs.webkit.org/show_bug.cgi?id=47455
    
            * platform/qt/PlatformBridge.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/PlatformBridge.h
+++ third_party/WebKit/WebCore/platform/qt/PlatformBridge.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PlatformBridge_h
 
 #include "KURL.h"
-#include "npapi.h"
 #include "PlatformString.h"
 
 #include <wtf/Vector.h>
@@ -78,7 +77,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
 
 
-class NPObject;
+typedef struct NPObject NPObject;
+typedef struct _NPP NPP_t;
+typedef NPP_t* NPP;
 
 namespace WebCore {
 
