f76c9ee8f0a7bc138454a9a4645430e8f28c83f1
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-14 12:21:32 +0000

    In the TabRestoreService, do not create Window entries that only have 1 Tab.
    BUG=56744
    TEST=Open a window and navigate 1 tab. Close window. Both NTP and the Mac History menu show the tab, without being in a window.
    
    Review URL: http://codereview.chromium.org/3397032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62537 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/tab_restore_service.h
+++ chrome/browser/sessions/tab_restore_service.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -203,10 +203,10 @@ class TabRestoreService : public BaseSessionService {
   // Notifies observers the tabs have changed.
   void NotifyTabsChanged();
 
-  // Adds |entry| to the list of entries. If |prune| is true |PruneAndNotify|
-  // is invoked. If |to_front| is true the entry is added to the front,
-  // otherwise the back. Normal closes go to the front, but tab/window closes
-  // from the previous session are added to the back.
+  // Adds |entry| to the list of entries and takes ownership. If |prune| is true
+  // |PruneAndNotify| is invoked. If |to_front| is true the entry is added to
+  // the front, otherwise the back. Normal closes go to the front, but
+  // tab/window closes from the previous session are added to the back.
   void AddEntry(Entry* entry, bool prune, bool to_front);
 
   // Prunes entries_ to contain only kMaxEntries and invokes NotifyTabsChanged.
