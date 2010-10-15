68328b8158a7f660febbf6a049a5b9efa09bd155
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-15 02:54:40 +0000

    2010-10-14  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by Tony Chang.
    
            [chromium] null check the widget before sending it data as calling didReceiveData might delete it
            https://bugs.webkit.org/show_bug.cgi?id=47708
    
            * src/FrameLoaderClientImpl.cpp:
            (WebKit::FrameLoaderClientImpl::committedLoad):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69834 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -1073,7 +1073,11 @@ void FrameLoaderClientImpl::committedLoad(DocumentLoader* loader, const char* da
             m_pluginWidget->didReceiveResponse(
                 m_webFrame->frame()->loader()->activeDocumentLoader()->response());
         }
-        m_pluginWidget->didReceiveData(data, length);
+
+        // It's possible that the above call removed the pointer to the plugin, so
+        // check before calling it.
+        if (m_pluginWidget.get())
+            m_pluginWidget->didReceiveData(data, length);
     }
 }
 
