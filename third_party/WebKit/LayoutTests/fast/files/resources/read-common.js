46484027947d5d05ffbd9c586969059981ee972b
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-15 21:46:43 +0000

    FileReader.readAsDataURL should take Blob object https://bugs.webkit.org/show_bug.cgi?id=47679
    Reviewed by Dmitry Titov.
    
    WebCore:
    
    Test cases have been added into read-blob-test-cases.js.
    
    * fileapi/FileReader.idl: Update per the latest File API spec.
    
    LayoutTests:
    
    * fast/files/read-blob-async-expected.txt:
    * fast/files/resources/read-blob-test-cases.js:
    (testReadingSingleTextBlobAsDataURL):
    (testReadingSingleTextBlobAsDataURL2):
    (testReadingTripleSlicedHybridBlob):
    * fast/files/resources/read-common.js:
    (buildBlob):
    * fast/files/workers/worker-read-blob-async-expected.txt:
    * fast/files/workers/worker-read-blob-sync-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/files/resources/read-common.js
+++ third_party/WebKit/LayoutTests/fast/files/resources/read-common.js
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Builds a blob from a list of items.
+// The 'contentType' argument is optional.
 // If the 'builder' argument is not provided, create a new one.
-function buildBlob(items, builder)
+function buildBlob(items, contentType, builder)
 {
     if (builder === undefined)
         builder = new BlobBuilder();
     for (var i = 0; i < items.length; i++)
         builder.append(items[i]);
-    return builder.getBlob();
+    return builder.getBlob(contentType);
 }
 
 // Reads a blob either asynchronously or synchronously.
