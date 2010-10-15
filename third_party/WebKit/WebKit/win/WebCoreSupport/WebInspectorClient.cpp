263311bbe15d8a0cb30b93c49f955baf66bf9396
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-15 15:11:12 +0000

    Windows build fix. Unreviewed.
    * WebCoreSupport/WebInspectorClient.cpp:
    Add a missing include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCoreSupport/WebInspectorClient.cpp
+++ third_party/WebKit/WebKit/win/WebCoreSupport/WebInspectorClient.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <tchar.h>
 #include <wtf/RetainPtr.h>
+#include <wtf/text/StringConcatenate.h>
 
 using namespace WebCore;
 
