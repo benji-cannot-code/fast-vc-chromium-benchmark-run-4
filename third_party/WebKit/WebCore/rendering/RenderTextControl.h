28bcb28fb4333ec998401143826a5a5aca7ad2b2
jschuh@chromium.org <jschuh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-15 01:50:40 +0000

    2010-10-14  Justin Schuh  <jschuh@chromium.org>
            Reviewed by James Robinson.
    
            Crash in HTMLTextFormControlElement::selection()
            https://bugs.webkit.org/show_bug.cgi?id=47522
    
            Patch by Ryosuke Niwa  <rniwa@webkit.org>
    
            The bug was caused by our triggering layout inside RenderTextControl::selection.
            Fixed it by changing the return type of RenderTextControl::selection to PassRefPtr<Range> from VisibleSelection.
    
            Test: fast/forms/render-text-crash.html
    
            * editing/Editor.cpp:
            (WebCore::Editor::selectionForCommand): Calls HTMLTextFormControlElement::selection.
            * html/HTMLFormControlElement.cpp:
            (WebCore::HTMLTextFormControlElement::selection): Calls RenderTextControl::selection.
            * html/HTMLFormControlElement.h:
            * rendering/RenderTextControl.cpp:
            (WebCore::RenderTextControl::selection): Changed the return type.
            * rendering/RenderTextControl.h:
    2010-10-14  Justin Schuh  <jschuh@chromium.org>
    
            Reviewed by James Robinson.
    
            Crash in HTMLTextFormControlElement::selection()
            https://bugs.webkit.org/show_bug.cgi?id=47522
    
            Patch by Ryosuke Niwa  <rniwa@webkit.org>
    
            Added a test to ensure WebKit does not crash when moving selection between input elements.
    
            * fast/forms/render-text-crash-expected.txt: Added.
            * fast/forms/render-text-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTextControl.h
+++ third_party/WebKit/WebCore/rendering/RenderTextControl.h
@@ -47,7 +47,7 @@ public:
     void setSelectionEnd(int);
     void select();
     void setSelectionRange(int start, int end);
-    VisibleSelection selection(int start, int end) const;
+    PassRefPtr<Range> selection(int start, int end) const;
 
     virtual void subtreeHasChanged();
     String text();
