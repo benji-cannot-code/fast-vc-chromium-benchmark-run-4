a9d61705e4e86f6519a954a176f4788cdaaa1d44
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-15 18:12:36 +0000

    [GTK] Fix introspection support https://bugs.webkit.org/show_bug.cgi?id=47723
    Patch by Sergio Villar Senin <svillar@igalia.com> on 2010-10-15
    Reviewed by Martin Robinson.
    
    Added HTTP cache directory to the WebkitGtk+ GIR sources.
    
    * GNUmakefile.am:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qwkpreferences_p.h"
 
 #include "ClientImpl.h"
+#include "FindIndicator.h"
 #include "LocalizedStrings.h"
 #include "NativeWebKeyboardEvent.h"
 #include "WebContext.h"
--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebView.h"
 
 #include "ChunkedUpdateDrawingAreaProxy.h"
+#include "FindIndicator.h"
 #include "RunLoop.h"
 #include "NativeWebKeyboardEvent.h"
 #include "WebEditCommandProxy.h"
