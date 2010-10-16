4476a081521cd46686bd191c6acae589be41b4c9
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-16 04:10:26 +0000

    2010-10-15  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            unlink removes inline style of anchor elements
            https://bugs.webkit.org/show_bug.cgi?id=47424
    
            The bug was caused by our not extracting styles when the anchor element is removed by removeInlineStyle.
            Because we removed the element without pushing its inline style down, we lost style information.
    
            Fixed the bug by pushing down styles in removeInlineStyle as done in pushDownInlineStyleAroundNode.
            Also fixed a bug in addInlineStyleIfNeeded where StyleChange incorrectly removed styles of the container
            node as supposed to that of startNode from the style to apply when startNode is not an element.
    
            Test: editing/execCommand/toggle-unlink.html
    
            * editing/ApplyStyleCommand.cpp:
            (WebCore::ApplyStyleCommand::removeInlineStyleFromElement): To prevent a similar bug in the future,
            added an assert that extractedStyle is specified whenever we're removing a styled element.
            (WebCore::ApplyStyleCommand::removeInlineStyle):
            (WebCore::ApplyStyleCommand::addInlineStyleIfNeeded):
    2010-10-15  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Darin Adler.
    
            unlink removes inline style of anchor elements
            https://bugs.webkit.org/show_bug.cgi?id=47424
    
            Added a test to ensure execCommand('unlink', false, null) preserves the inline style declaration.
    
            * editing/execCommand/script-tests/toggle-unlink.js: Added.
            (testSingleToggle):
            (selectAll):
            (selectFirstTwoWords):
            (selectLastTwoWords):
            (selectLastWord):
            * editing/execCommand/toggle-unlink-expected.txt: Added.
            * editing/execCommand/toggle-unlink.html: Added.
            * editing/style/script-tests/make-text-writing-direction-inline.js: Specifies both direction
            and unicode-bidi properties instead of just direction property.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/editing/style/script-tests/make-text-writing-direction-inline.js
+++ third_party/WebKit/LayoutTests/editing/style/script-tests/make-text-writing-direction-inline.js
@@ -98,7 +98,7 @@ modifyWritingDirection('<div dir="rtl"><b>هنا يكتب</b> النص العر�
 modifyWritingDirection('<div dir="rtl">هنا <span dir="ltr">يكتب النص العربي</span></div>', selectThirdWord, 'Natural',
                        '<div dir="rtl">هنا <span dir="ltr">يكتب</span> النص<span dir="ltr"> العربي</span></div>');
 modifyWritingDirection('<div dir="rtl">هنا <span dir="ltr">يكتب النص العربي</span></div>', selectThirdWord, 'LeftToRight',
-                       '<div dir="rtl"><span style="direction: ltr;">هنا يكتب النص العربي</span></div>');
+                       '<div dir="rtl"><span style="unicode-bidi: embed; direction: ltr;">هنا يكتب النص العربي</span></div>');
 modifyWritingDirection('<div dir="rtl">هنا <span dir="ltr">يكتب النص العربي</span></div>', selectThirdWord, 'RightToLeft',
                        '<div dir="rtl">هنا <span dir="ltr">يكتب</span><span style="unicode-bidi: embed;"> النص</span><span dir="ltr"> العربي</span></div>');
 
