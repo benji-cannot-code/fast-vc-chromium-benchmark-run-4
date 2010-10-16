ee0b0ca6d5b010ae57850fde8698fbd4f9f416ba
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-16 20:00:36 +0000

    2010-10-16  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed, build fix.
    
            WinCE build fix for r69798.
    
            * platform/text/wince/TextCodecWinCE.cpp: Added missing include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/wince/TextCodecWinCE.cpp
+++ third_party/WebKit/WebCore/platform/text/wince/TextCodecWinCE.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <winnls.h>
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
+#include <wtf/text/CString.h>
 #include <wtf/text/StringConcatenate.h>
 #include <wtf/text/StringHash.h>
 #include <wtf/unicode/UTF8.h>
