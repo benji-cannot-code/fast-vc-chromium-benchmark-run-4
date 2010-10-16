133a73064f07514ebe48300ad67e8b54f5df70f0
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-16 00:14:50 +0000

    Fix build.
    * UIProcess/API/mac/FindIndicatorWindow.mm:
    (-[WebFindIndicatorWindowAnimation setCurrentProgress:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/mac/FindIndicatorWindow.mm
+++ third_party/WebKit/WebKit2/UIProcess/API/mac/FindIndicatorWindow.mm
@@ -85,7 +85,7 @@ using namespace WebCore;
 {
     if ((self = [super initWithDuration:fadeOutAnimationDuration animationCurve:NSAnimationEaseInOut])) {
         _findIndicatorWindow = findIndicatorWindow;
-        _animationCallback = animationCallback;
+        _animationProgressCallback = animationProgressCallback;
         _animationDidEndCallback = animationDidEndCallback;
         [self setDelegate:self];
         [self setAnimationBlockingMode:NSAnimationNonblocking];
@@ -95,7 +95,7 @@ using namespace WebCore;
 
 - (void)setCurrentProgress:(NSAnimationProgress)progress
 {
-    (_findIndicatorWindow->*_animationCallback)(progress);
+    (_findIndicatorWindow->*_animationProgressCallback)(progress);
 }
 
 - (void)animationDidEnd:(NSAnimation *)animation
