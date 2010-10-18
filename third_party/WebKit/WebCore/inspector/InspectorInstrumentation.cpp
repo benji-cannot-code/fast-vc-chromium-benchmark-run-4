e5cc99b903ac651effa1248e56803f1d8990be52
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 15:39:27 +0000

    2010-10-18  Andreas Kling  <kling@webkit.org>
            Unreviewed buildfix for Qt + V8.
            Add missing <wtf/text/CString.h> include.
    
            * inspector/InspectorInstrumentation.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorInstrumentation.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorInstrumentation.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorDebuggerAgent.h"
 #include "InspectorTimelineAgent.h"
 #include "XMLHttpRequest.h"
+#include <wtf/text/CString.h>
 
 namespace WebCore {
 
