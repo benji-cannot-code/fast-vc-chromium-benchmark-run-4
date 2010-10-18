101a83cb223f46c9b1a17dd1336181c412172ece
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 11:57:36 +0000

    2010-10-18  David Levin  <levin@chromium.org>
            Reviewed by Oliver Hunt.
    
            check-webkit-style needs to ignore underscores in opcode names and vm_throw
            https://bugs.webkit.org/show_bug.cgi?id=47789
    
            * Scripts/webkitpy/style/checker.py: Added the exception for the assembler directory.
            * Scripts/webkitpy/style/checkers/cpp.py: Added special cased names.
            * Scripts/webkitpy/style/checkers/cpp_unittest.py: Added unit tests for the special cases.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp_unittest.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp_unittest.py
@@ -3691,6 +3691,7 @@ class WebKitStyleTest(CppStyleTestBase):
 
         # There is an exception for op code functions but only in the JavaScriptCore directory.
         self.assert_lint('void this_op_code(int var1, int var2)', '', 'JavaScriptCore/foo.cpp')
+        self.assert_lint('void op_code(int var1, int var2)', '', 'JavaScriptCore/foo.cpp')
         self.assert_lint('void this_op_code(int var1, int var2)', 'this_op_code' + name_underscore_error_message)
 
         # GObject requires certain magical names in class declarations.
@@ -3716,6 +3717,9 @@ class WebKitStyleTest(CppStyleTestBase):
 
         # const_iterator is allowed as well.
         self.assert_lint('typedef VectorType::const_iterator const_iterator;', '')
+
+        # vm_throw is allowed as well.
+        self.assert_lint('int vm_throw;', '')
 
         # Bitfields.
         self.assert_lint('unsigned _fillRule : 1;',
