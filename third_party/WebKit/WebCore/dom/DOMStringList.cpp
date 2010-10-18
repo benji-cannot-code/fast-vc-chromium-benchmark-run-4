e0b4eeb9497ba0bda1a1032a6b3f4f52735dad76
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 19:15:34 +0000

    2010-10-18  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Adler.
    
            DOMStringList should return null if the index is out of range
            https://bugs.webkit.org/show_bug.cgi?id=47829
    
            Add tests for out of range behavior here since IndexedDB is the only user
            of DOMStringList.
    
            * storage/indexeddb/database-basics-expected.txt:
            * storage/indexeddb/database-basics.html:
            * storage/indexeddb/objectstore-basics-expected.txt:
            * storage/indexeddb/objectstore-basics.html:
    2010-10-18  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Darin Adler.
    
            DOMStringList should return null if the index is out of range
            https://bugs.webkit.org/show_bug.cgi?id=47829
    
            When item() returns a null string, it needs to be passed to JS as
            null. The indexed getter will then magically do the right thing.
            So all that's left is having DOMStringList::item() return null when
            the index is out of bounds.
    
            * dom/DOMStringList.cpp:
            (WebCore::DOMStringList::item):
            * dom/DOMStringList.h:
            * dom/DOMStringList.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/DOMStringList.cpp
+++ third_party/WebKit/WebCore/dom/DOMStringList.cpp
@@ -29,6 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+String DOMStringList::item(unsigned index) const
+{
+    if (index >= m_strings.size())
+        return String();
+    return m_strings[index];
+}
+
 bool DOMStringList::contains(const String& string) const
 {
     // FIXME: Currently, all consumers of DOMStringList store fairly small lists and thus an O(n)
--- third_party/WebKit/WebCore/dom/DOMStringList.h
+++ third_party/WebKit/WebCore/dom/DOMStringList.h
@@ -49,7 +49,7 @@ public:
 
     // Implements the IDL.
     size_t length() const { return m_strings.size(); }
-    String item(unsigned index) const { return m_strings[index]; }
+    String item(unsigned index) const;
     bool contains(const String& str) const;
 
 private:
