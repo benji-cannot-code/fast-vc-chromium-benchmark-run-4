521702662664782f796093da65109c8321f598a1
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 18:32:39 +0000

    2010-10-18  Oliver Hunt  <oliver@apple.com>
            Reviewed by Darin Adler.
    
            Strict mode: |this| should be undefined if it is not explicitly provided
            https://bugs.webkit.org/show_bug.cgi?id=47833
    
            To make strict mode behave correctly we want to pass undefined instead of null
            as the default this value.  This has no impact on behaviour outside of strict
            mode as both values are replaced with the global object if necessary.
    
            * bytecompiler/NodesCodegen.cpp:
            (JSC::FunctionCallValueNode::emitBytecode):
            (JSC::FunctionCallResolveNode::emitBytecode):
            (JSC::CallFunctionCallDotNode::emitBytecode):
            (JSC::ApplyFunctionCallDotNode::emitBytecode):
    
    2010-10-18  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Darin Adler.
    
            Strict mode: |this| should be undefined if it is not explicitly provided
            https://bugs.webkit.org/show_bug.cgi?id=47833
    
            Add tests to ensure that |this| is undefined rather than null when it has
            not been explicitly provided.
    
            * fast/js/basic-strict-mode-expected.txt:
            * fast/js/script-tests/basic-strict-mode.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/basic-strict-mode.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/basic-strict-mode.js
@@ -37,7 +37,11 @@ shouldBeUndefined("testThisBracketAccess.call(false, 'length')");
 shouldBeUndefined("testThisBracketAccess.call(1, 'length')");
 
 
-shouldBe("testGlobalAccess()", "null");
+shouldBe("testGlobalAccess()", "undefined");
+shouldBe("testThis.call()", "undefined");
+shouldBe("testThis.apply()", "undefined");
+shouldBe("testThis.call(undefined)", "undefined");
+shouldBe("testThis.apply(undefined)", "undefined");
 
 shouldThrow("(function eval(){'use strict';})");
 shouldThrow("(function (eval){'use strict';})");
