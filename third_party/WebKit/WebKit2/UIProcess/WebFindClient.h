98381012cbceb35a2dcf9d0d350c931d647107bc
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 18:46:51 +0000

    Add matchCountDidChange callback https://bugs.webkit.org/show_bug.cgi?id=47840
    Reviewed by Darin Adler.
    
    * UIProcess/API/C/WKPage.h:
    Add matchCountDidChange WKPageFindClient callback.
    
    * UIProcess/WebFindClient.cpp:
    (WebKit::WebFindClient::matchCountDidChange):
    Call the WKPageFindClient callback.
    
    * UIProcess/WebPageProxy.messages.in:
    Add MatchCountDidChange message.
    
    * WebProcess/WebPage/FindController.cpp:
    (WebKit::FindController::countStringMatches):
    Move code here from WebPage.
    
    (WebKit::FindController::findString):
    Send the MatchCountDidChange message.
    
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::countStringMatches):
    Move this code over to FindController.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebFindClient.h
+++ third_party/WebKit/WebKit2/UIProcess/WebFindClient.h
@@ -37,7 +37,8 @@ class WebPageProxy;
 
 class WebFindClient : public APIClient<WKPageFindClient> {
 public:
-    void didCountStringMatches(WebPageProxy*, const String&, uint32_t numMatches);
+    void matchCountDidChange(WebPageProxy*, const String&, uint32_t matchCount);
+    void didCountStringMatches(WebPageProxy*, const String&, uint32_t matchCount);
 };
 
 } // namespace WebKit
