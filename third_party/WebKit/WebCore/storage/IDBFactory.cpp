bd845d9958917233705594979c00f61f84f54d19
andreip@google.com <andreip@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 18:04:52 +0000

    2010-10-18  Andrei Popescu  <andreip@google.com>
            Reviewed by Jeremy Orlow.
    
            The description parameter of IDBFactory::open() should be mandatory.
            https://bugs.webkit.org/show_bug.cgi?id=47835
    
            * storage/indexeddb/basics-expected.txt:
            * storage/indexeddb/basics.html:
    2010-10-18  Andrei Popescu  <andreip@google.com>
    
            Reviewed by Jeremy Orlow.
    
            The description parameter of IDBFactory::open() should be mandatory.
            https://bugs.webkit.org/show_bug.cgi?id=47835
    
            Makes the description parameter mandatory, as per the IndexedDatabase specification.
            Modified existing tests to cover this change.
    
            * storage/IDBDatabaseBackendImpl.cpp:
            (WebCore::extractMetaData):
            (WebCore::setMetaData):
            (WebCore::IDBDatabaseBackendImpl::IDBDatabaseBackendImpl):
            * storage/IDBFactory.cpp:
            (WebCore::IDBFactory::open):
            * storage/IDBFactory.h:
            * storage/IDBFactory.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBFactory.cpp
+++ third_party/WebKit/WebCore/storage/IDBFactory.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "GroupSettings.h"
 #include "IDBDatabase.h"
+#include "IDBDatabaseException.h"
 #include "IDBFactoryBackendInterface.h"
 #include "IDBKeyRange.h"
 #include "IDBRequest.h"
@@ -70,6 +71,11 @@ PassRefPtr<IDBRequest> IDBFactory::open(ScriptExecutionContext* context, const S
 
 
     // FIXME: Raise a NON_TRANSIENT_ERR if the name is invalid.
+
+    if (description.isNull()) {
+        ec = IDBDatabaseException::NON_TRANSIENT_ERR;
+        return 0;
+    }
 
     RefPtr<IDBRequest> request = IDBRequest::create(document, IDBAny::create(this), 0);
     GroupSettings* groupSettings = document->page()->group().groupSettings();
