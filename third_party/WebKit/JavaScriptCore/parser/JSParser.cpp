c46ab5a90a16a17eabdf272ac020f9f74fb25117
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 16:30:12 +0000

    2010-10-18  Zoltan Herczeg  <zherczeg@webkit.org>
            Reviewed by Oliver Hunt.
    
            if (0) throw "x" ; else { } throws parse error after r69906
            https://bugs.webkit.org/show_bug.cgi?id=47807
    
            r69906 introduced a bug: the semicolon is not parsed after a throw
            expression anymore. Thus, the semicolon terminates the "if" parsing
            in the example above, and the else token results a parse error.
    
            * parser/JSParser.cpp:
            (JSC::JSParser::parseThrowStatement):
    2010-10-18  Zoltan Herczeg  <zherczeg@webkit.org>
    
            Reviewed by Oliver Hunt.
    
            if (0) throw "x" ; else { } throws parse error after r69906
            https://bugs.webkit.org/show_bug.cgi?id=47807
    
            Adding a new test case to break-ASI.js
    
            * fast/js/break-ASI-expected.txt:
            * fast/js/script-tests/break-ASI.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/parser/JSParser.cpp
+++ third_party/WebKit/JavaScriptCore/parser/JSParser.cpp
@@ -901,6 +901,7 @@ template <class TreeBuilder> TreeStatement JSParser::parseThrowStatement(TreeBui
     failIfFalse(expr);
     int eEnd = lastTokenEnd();
     int endLine = tokenLine();
+    failIfFalse(autoSemiColon());
 
     return context.createThrowStatement(expr, eStart, eEnd, startLine, endLine);
 }
