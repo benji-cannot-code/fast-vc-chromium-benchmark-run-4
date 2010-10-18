6d1d77d6c5abeff8f7578b4b59a3a52ed00f0dcd
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 18:02:24 +0000

    Use nullptr instead of 0.
    Reviewed by Darin Adler.
    
    * UIProcess/API/mac/FindIndicatorWindow.mm:
    (WebKit::FindIndicatorWindow::closeWindow):
    * UIProcess/API/mac/WKView.mm:
    (-[WKView renewGState]):
    * WebProcess/Plugins/PluginProcessConnection.cpp:
    (WebKit::PluginProcessConnection::removePluginProxy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/WebKit2/UIProcess/API/mac/WKView.mm
@@ -182,7 +182,7 @@ struct EditCommandState {
 - (void)renewGState
 {
     // Hide the find indicator.
-    _data->_findIndicatorWindow = 0;
+    _data->_findIndicatorWindow = nullptr;
 
     [super renewGState];
 }
