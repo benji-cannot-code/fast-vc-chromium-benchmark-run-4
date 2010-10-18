d0ae45717e8f84a76d5602ae2644e8edf5f1e290
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 16:48:07 +0000

    2010-10-18  No'am Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Andreas Kling.
    
            [Qt] AC rendering bugs
    
            This fixes a regression that was introduced by making scrolling in accelerated compositing automatic,
            which made our explicit function to scroll the compositing layers superfluous, making the scrolling behavior
            when graphics layers are present incorrect. The fix simply removes those redundant functions.
    
            https://bugs.webkit.org/show_bug.cgi?id=47571
    
            * WebCoreSupport/PageClientQt.cpp:
            (WebCore::PageClientQGraphicsWidget::scroll):
            (WebCore::PageClientQGraphicsWidget::update):
            (WebCore::PageClientQGraphicsWidget::setRootGraphicsLayer):
            * WebCoreSupport/PageClientQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/PageClientQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/PageClientQt.h
@@ -171,7 +171,6 @@ public:
 #if USE(ACCELERATED_COMPOSITING)
     virtual void setRootGraphicsLayer(QGraphicsItem* layer);
     virtual void markForSync(bool scheduleSync);
-    void updateCompositingScrollPosition();
     void syncLayers();
 
     // QGraphicsWebView can render composited layers
