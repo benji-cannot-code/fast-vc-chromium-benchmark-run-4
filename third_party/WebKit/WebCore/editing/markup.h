17316166be07a7a0495afd19126d5a26a90018f2
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-18 21:42:37 +0000

    2010-10-18  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Adam Barth.
    
            elementCannotHaveEndTag should be a member function of MarkupAccumulator
            https://bugs.webkit.org/show_bug.cgi?id=47846
    
            Moved elementCannotHaveEndTag into MarkupAccumulator.
    
            No new tests are added since this is a cleanup.
    
            * editing/MarkupAccumulator.cpp:
            (WebCore::MarkupAccumulator::elementCannotHaveEndTag): Added.
            * editing/MarkupAccumulator.h: Added MarkupAccumulator::elementCannotHaveEndTag.
            * editing/markup.cpp: Removed elementCannotHaveEndTag.
            * editing/markup.h: Removed elementCannotHaveEndTag.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@69994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/markup.h
+++ third_party/WebKit/WebCore/editing/markup.h
@@ -57,9 +57,6 @@ namespace WebCore {
     String createFullMarkup(const Range*);
 
     String urlToMarkup(const KURL&, const String& title);
-
-    // FIXME: Should this be better encapulated somewhere?
-    bool elementCannotHaveEndTag(const Node*);
 }
 
 #endif // markup_h
