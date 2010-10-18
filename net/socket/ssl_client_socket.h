17079d1d6df4f7978c8b88a99cbf37384bd8efa2
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-18 14:45:10 +0000

    non-virtual-dtor cleanup: Make DNSSecProvider destructor private.
    Interface classes are an example where the -Wnon-virtual-dtor warning can be a bit annoying, but this still seems reasonable and makes it clear that users should not delete through the interface.
    
    BUG=45135
    TEST=Builds
    Review URL: http://codereview.chromium.org/3781010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62926 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket.h
+++ net/socket/ssl_client_socket.h
@@ -32,6 +32,9 @@ class DNSSECProvider {
   //      function will return OK if called again.
   virtual int GetDNSSECRecords(RRResponse** out,
                                CompletionCallback* callback) = 0;
+
+ private:
+  ~DNSSECProvider() {}
 };
 
 // A client socket that uses SSL as the transport layer.
