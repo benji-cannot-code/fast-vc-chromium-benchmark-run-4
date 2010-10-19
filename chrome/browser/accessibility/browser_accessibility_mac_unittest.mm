5699595ac895071beb1295f0edebcec9cf4a3380
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 23:47:08 +0000

    Fix clang build.
    BUG=None
    TEST=clang build goes green
    TBR=dtseng
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/browser_accessibility_mac_unittest.mm
+++ chrome/browser/accessibility/browser_accessibility_mac_unittest.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
-#include "base/scoped_ptr.h";
+#include "base/scoped_ptr.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/accessibility/browser_accessibility_cocoa.h"
