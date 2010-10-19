f2d9fb6185ac37ab4d5e8074261d8407bd43790b
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 19:11:15 +0000

    ipc_channel: warn if someone forgets to check the result of Connect()
    I found a test that was doing this.
    
    Review URL: http://codereview.chromium.org/3759004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63083 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel.h
+++ ipc/ipc_channel.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IPC_IPC_CHANNEL_H_
 #pragma once
 
+#include "base/compiler_specific.h"
 #include "ipc/ipc_message.h"
 
 namespace IPC {
@@ -69,7 +70,7 @@ class Channel : public Message::Sender {
   // connect to a pre-existing pipe.  Note, calling Connect()
   // will not block the calling thread and may complete
   // asynchronously.
-  bool Connect();
+  bool Connect() WARN_UNUSED_RESULT;
 
   // Close this Channel explicitly.  May be called multiple times.
   void Close();
--- ipc/ipc_send_fds_test.cc
+++ ipc/ipc_send_fds_test.cc
@@ -111,7 +111,7 @@ int TestDescriptorClient(ino_t expected_inode_num) {
   // Setup IPC channel.
   IPC::Channel chan(kTestClientChannel, IPC::Channel::MODE_CLIENT,
                     &listener);
-  chan.Connect();
+  CHECK(chan.Connect());
   MessageLoop::current()->Run();
 
   return 0;
@@ -158,7 +158,7 @@ TEST_F(IPCChannelTest, DescriptorTestSandboxed) {
 
   IPC::Channel chan(kTestClientChannel, IPC::Channel::MODE_SERVER,
                     &listener);
-  chan.Connect();
+  ASSERT_TRUE(chan.Connect());
 
   base::ProcessHandle process_handle = SpawnChild(
       TEST_DESCRIPTOR_CLIENT_SANDBOXED,
@@ -182,7 +182,7 @@ TEST_F(IPCChannelTest, DescriptorTest) {
 
   IPC::Channel chan(kTestClientChannel, IPC::Channel::MODE_SERVER,
                     &listener);
-  chan.Connect();
+  ASSERT_TRUE(chan.Connect());
 
   base::ProcessHandle process_handle = SpawnChild(TEST_DESCRIPTOR_CLIENT,
                                                   &chan);
