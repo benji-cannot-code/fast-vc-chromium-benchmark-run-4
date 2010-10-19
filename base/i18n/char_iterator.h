2cd9f68da0dbec85acee625917503eb12cb17aac
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 02:31:03 +0000

    Reland r62959 without the changes to histogram.cc, in hopes it won't break tab_switching_test, and without the changes to tools_sanity_unittest.cc, which have already been relanded separately.
    TBR=timsteele
    
    Original changelog message:
    
    Convert LOG(INFO) to VLOG(1) - base/.
    
    Also removes an "else" after "return" and aligns "<<" per style guide.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3850007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63009 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/char_iterator.h
+++ base/i18n/char_iterator.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //   UTF8CharIterator iter(&str);
 //   while (!iter.End()) {
-//     LOG(INFO) << iter.get();
+//     VLOG(1) << iter.get();
 //     iter.Advance();
 //   }
 
--- base/i18n/word_iterator.h
+++ base/i18n/word_iterator.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   while (iter.Advance()) {
 //     if (iter.IsWord()) {
 //       // region [iter.prev(),iter.pos()) contains a word.
-//       LOG(INFO) << "word: " << iter.GetWord();
+//       VLOG(1) << "word: " << iter.GetWord();
 //     }
 //   }
 
