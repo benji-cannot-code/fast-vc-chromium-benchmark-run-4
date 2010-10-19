4f340e5352a02af40b9bb0b07227ec0108608ec9
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-19 02:33:33 +0000

    2010-10-18  Oliver Hunt  <oliver@apple.com>
            Reviewed by Sam Weinig.
    
            REGRESSION: Feedly extension crashes Webkit
            https://bugs.webkit.org/show_bug.cgi?id=45811
    
            Make test cover large number of properties/string pairs, at two offsets to get the
            new allocation to occur at different locations.
    
            * fast/dom/Window/window-postmessage-clone-expected.txt:
            * fast/dom/Window/window-postmessage-clone.html:
    2010-10-18  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Sam Weinig.
    
            REGRESSION: Feedly extension crashes Webkit
            https://bugs.webkit.org/show_bug.cgi?id=45811
    
            The basic problem was the deserializer was holding a pointer into
            the constant pool, but if you were sufficiently unlucky then the
            constant pool would be moved while still relying on the pointer,
            which leads to badness.
    
            I looked at just making all the sites this could happen extract the
            right string/jsstring before any possible allocations, but it seemed
            too fragile so i've gone for a forwarding object as the solution.
    
            * bindings/js/SerializedScriptValue.cpp:
            (WebCore::CloneDeserializer::CachedStringRef::CachedStringRef):
            (WebCore::CloneDeserializer::CachedStringRef::operator->):
            (WebCore::CloneDeserializer::readStringData):
            (WebCore::CloneDeserializer::readFile):
            (WebCore::CloneDeserializer::readTerminal):
            (WebCore::CloneDeserializer::deserialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70018 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
+++ third_party/WebKit/WebCore/bindings/js/SerializedScriptValue.cpp
@@ -782,6 +782,25 @@ private:
         JSValue m_jsString;
     };
 
+    struct CachedStringRef {
+        CachedStringRef()
+            : m_base(0)
+            , m_index(0)
+        {
+        }
+        CachedStringRef(Vector<CachedString>* base, size_t index)
+            : m_base(base)
+            , m_index(index)
+        {
+        }
+        
+        CachedString* operator->() { ASSERT(m_base); return &m_base->at(m_index); }
+        
+    private:
+        Vector<CachedString>* m_base;
+        size_t m_index;
+    };
+
     CloneDeserializer(ExecState* exec, JSGlobalObject* globalObject, const Vector<uint8_t>& buffer)
         : CloneBase(exec)
         , m_globalObject(globalObject)
@@ -935,13 +954,13 @@ private:
         return true;
     }
 
-    bool readStringData(CachedString*& cachedString)
+    bool readStringData(CachedStringRef& cachedString)
     {
         bool scratch;
         return readStringData(cachedString, scratch);
     }
 
-    bool readStringData(CachedString*& cachedString, bool& wasTerminator)
+    bool readStringData(CachedStringRef& cachedString, bool& wasTerminator)
     {
         if (m_failed)
             return false;
@@ -962,7 +981,7 @@ private:
                 fail();
                 return false;
             }
-            cachedString = &m_constantPool[index];
+            cachedString = CachedStringRef(&m_constantPool, index);
             return true;
         }
         UString str;
@@ -971,7 +990,7 @@ private:
             return false;
         }
         m_constantPool.append(str);
-        cachedString = &m_constantPool.last();
+        cachedString = CachedStringRef(&m_constantPool, m_constantPool.size() - 1);
         return true;
     }
 
@@ -997,13 +1016,13 @@ private:
 
     bool readFile(RefPtr<File>& file)
     {
-        CachedString* path = 0;
+        CachedStringRef path;
         if (!readStringData(path))
             return 0;
-        CachedString* url = 0;
+        CachedStringRef url;
         if (!readStringData(url))
             return 0;
-        CachedString* type = 0;
+        CachedStringRef type;
         if (!readStringData(type))
             return 0;
         if (m_isDOMGlobalObject)
@@ -1093,10 +1112,10 @@ private:
             return toJS(m_exec, static_cast<JSDOMGlobalObject*>(m_globalObject), result.get());
         }
         case BlobTag: {
-            CachedString* url = 0;
+            CachedStringRef url;
             if (!readStringData(url))
                 return JSValue();
-            CachedString* type = 0;
+            CachedStringRef type;
             if (!readStringData(type))
                 return JSValue();
             unsigned long long size = 0;
@@ -1107,7 +1126,7 @@ private:
             return toJS(m_exec, static_cast<JSDOMGlobalObject*>(m_globalObject), Blob::create(KURL(KURL(), url->ustring().impl()), String(type->ustring().impl()), size));
         }
         case StringTag: {
-            CachedString* cachedString = 0;
+            CachedStringRef cachedString;
             if (!readStringData(cachedString))
                 return JSValue();
             return cachedString->jsString(m_exec);
@@ -1115,10 +1134,10 @@ private:
         case EmptyStringTag:
             return jsEmptyString(&m_exec->globalData());
         case RegExpTag: {
-            CachedString* pattern = 0;
+            CachedStringRef pattern;
             if (!readStringData(pattern))
                 return JSValue();
-            CachedString* flags = 0;
+            CachedStringRef flags;
             if (!readStringData(flags))
                 return JSValue();
             RefPtr<RegExp> regExp = RegExp::create(&m_exec->globalData(), pattern->ustring(), flags->ustring());
@@ -1224,7 +1243,7 @@ JSValue CloneDeserializer::deserialize()
                 tickCount = ticksUntilNextCheck();
             }
 
-            CachedString* cachedString = 0;
+            CachedStringRef cachedString;
             bool wasTerminator = false;
             if (!readStringData(cachedString, wasTerminator)) {
                 if (!wasTerminator)
