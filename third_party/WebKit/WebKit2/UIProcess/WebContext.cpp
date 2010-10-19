4e8e3eb6037901be042d1da9673fc9aa4c49aa24
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-19 23:40:58 +0000

    Fix MiniBrowser crashes during startup and shutdown https://bugs.webkit.org/show_bug.cgi?id=47954
    Reviewed by Sam Weinig.
    
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::sharedProcessContext):
    Initialize the main thread.
    
    (WebKit::WebContext::create):
    Ditto.
    
    (WebKit::WebContext::WebContext):
    Add the language observer here so we know that it will always be set up.
    
    (WebKit::WebContext::ensureWebProcess):
    Remove code.
    
    * UIProcess/WebContext.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebContext.cpp
@@ -62,6 +62,7 @@ static WTF::RefCountedLeakCounter webContextCounter("WebContext");
 
 WebContext* WebContext::sharedProcessContext()
 {
+    WTF::initializeMainThread();
     RunLoop::initializeMainRunLoop();
     static WebContext* context = adoptRef(new WebContext(ProcessModelSharedSecondaryProcess, String())).leakRef();
     return context;
@@ -76,6 +77,7 @@ WebContext* WebContext::sharedThreadContext()
 
 PassRefPtr<WebContext> WebContext::create(const String& injectedBundlePath)
 {
+    WTF::initializeMainThread();
     RunLoop::initializeMainRunLoop();
     return adoptRef(new WebContext(ProcessModelSecondaryProcess, injectedBundlePath));
 }
@@ -89,7 +91,7 @@ WebContext::WebContext(ProcessModel processModel, const String& injectedBundlePa
     , m_shouldPaintNativeControls(true)
 #endif
 {
-    RunLoop::initializeMainRunLoop();
+    addLanguageChangeObserver(this, languageChanged);
 
     m_preferences = WebPreferences::shared();
     m_preferences->addContext(this);
@@ -125,11 +127,17 @@ void WebContext::initializeHistoryClient(const WKContextHistoryClient* client)
     m_process->send(Messages::WebProcess::SetShouldTrackVisitedLinks(m_historyClient.shouldTrackVisitedLinks()), 0);
 }
 
-static void languageChanged(void* context)
+void WebContext::languageChanged(void* context)
 {
-    WebProcessProxy* process = static_cast<WebContext*>(context)->process();
-    if (process)
-        process->send(Messages::WebProcess::LanguageChanged(defaultLanguage()), 0);
+    static_cast<WebContext*>(context)->languageChanged();
+}
+
+void WebContext::languageChanged()
+{
+    if (!hasValidProcess())
+        return;
+
+    m_process->send(Messages::WebProcess::LanguageChanged(defaultLanguage()), 0);
 }
 
 void WebContext::ensureWebProcess()
@@ -160,10 +168,7 @@ void WebContext::ensureWebProcess()
 
     parameters.shouldTrackVisitedLinks = m_historyClient.shouldTrackVisitedLinks();
     parameters.cacheModel = m_cacheModel;
-
     parameters.languageCode = defaultLanguage();
-    addLanguageChangeObserver(this, languageChanged);
-
     parameters.applicationCacheDirectory = applicationCacheDirectory();
 
     copyToVector(m_schemesToRegisterAsEmptyDocument, parameters.urlSchemesRegistererdAsEmptyDocument);
