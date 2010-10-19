86c90fd14c447fbc9caad689710e5007d69b99fb
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-19 15:21:01 +0000

    2010-10-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            webkit-patch stats the filesystem too many times
            https://bugs.webkit.org/show_bug.cgi?id=47883
    
            This patch attempts to cache the list of changed files more agressively
            and to use that list to compute the diff instead of stating the file
            system again.
    
            * Scripts/webkitpy/common/checkout/api.py:
            * Scripts/webkitpy/common/checkout/scm.py:
            * Scripts/webkitpy/tool/mocktool.py:
            * Scripts/webkitpy/tool/steps/abstractstep.py:
            * Scripts/webkitpy/tool/steps/editchangelog.py:
            * Scripts/webkitpy/tool/steps/preparechangelog.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/editchangelog.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/steps/editchangelog.py
@@ -36,3 +36,4 @@ class EditChangeLog(AbstractStep):
     def run(self, state):
         os.chdir(self._tool.scm().checkout_root)
         self._tool.user.edit_changelog(self.cached_lookup(state, "changelogs"))
+        self.did_modify_checkout(state)
