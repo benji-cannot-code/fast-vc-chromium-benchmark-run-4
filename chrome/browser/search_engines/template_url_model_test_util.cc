f3e4c7ddfe71974de87736d0263eb461aa32ac04
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 00:58:48 +0000

    Make the TemplateURL test code less likley to crash if various failures happen.
    For example, if the test server fails to start, it would crash before these
    changes.
    
    BUG=None
    TEST=unit_tests -- gtest_filter=TemplateURL* (will run the affected tests).
    
    Review URL: http://codereview.chromium.org/3816008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63004 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_model_test_util.cc
+++ chrome/browser/search_engines/template_url_model_test_util.cc
@@ -113,7 +113,7 @@ void TemplateURLModelTestingProfile::SetUp() {
 
   FilePath path = temp_dir_.path().AppendASCII("TestDataService.db");
   service_ = new WebDataService;
-  EXPECT_TRUE(service_->InitWithPath(path));
+  ASSERT_TRUE(service_->InitWithPath(path));
 }
 
 void TemplateURLModelTestingProfile::TearDown() {
@@ -129,7 +129,8 @@ void TemplateURLModelTestingProfile::TearDown() {
   io_thread_.Stop();
 
   // Clean up the test directory.
-  service_->Shutdown();
+  if (service_.get())
+    service_->Shutdown();
   // Note that we must ensure the DB thread is stopped after WDS
   // shutdown (so it can commit pending transactions) but before
   // deleting the test profile directory, otherwise we may not be
@@ -153,8 +154,10 @@ void TemplateURLModelTestUtil::SetUp() {
 }
 
 void TemplateURLModelTestUtil::TearDown() {
-  profile_->TearDown();
-  profile_.reset();
+  if (profile_.get()) {
+    profile_->TearDown();
+    profile_.reset();
+  }
   TemplateURLRef::SetGoogleBaseURL(NULL);
 
   // Flush the message loop to make Purify happy.
