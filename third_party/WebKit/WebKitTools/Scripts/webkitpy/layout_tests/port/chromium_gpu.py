b231ac7a9807e79ec8d07b743572633402565931
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-19 17:56:44 +0000

    2010-10-19  Kenneth Russell  <kbr@google.com>
            Reviewed by David Levin.
    
            chromium_gpu port of new-run-webkit-tests must search chromium-gpu directory for expectations
            https://bugs.webkit.org/show_bug.cgi?id=47874
    
            * Scripts/webkitpy/layout_tests/port/chromium_gpu.py:
            * Scripts/webkitpy/layout_tests/port/chromium_gpu_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
@@ -91,7 +91,7 @@ class ChromiumGpuLinuxPort(chromium_linux.ChromiumLinuxPort):
         chromium_linux.ChromiumLinuxPort.__init__(self, **kwargs)
 
     def baseline_search_path(self):
-        return ([self._webkit_baseline_path('chromium-gpu-linux')] +
+        return (map(self._webkit_baseline_path, ['chromium-gpu-linux', 'chromium-gpu']) +
                 chromium_linux.ChromiumLinuxPort.baseline_search_path(self))
 
     def path_to_test_expectations_file(self):
@@ -109,7 +109,7 @@ class ChromiumGpuMacPort(chromium_mac.ChromiumMacPort):
         chromium_mac.ChromiumMacPort.__init__(self, **kwargs)
 
     def baseline_search_path(self):
-        return ([self._webkit_baseline_path('chromium-gpu-mac')] +
+        return (map(self._webkit_baseline_path, ['chromium-gpu-mac', 'chromium-gpu']) +
                 chromium_mac.ChromiumMacPort.baseline_search_path(self))
 
     def path_to_test_expectations_file(self):
@@ -127,7 +127,7 @@ class ChromiumGpuWinPort(chromium_win.ChromiumWinPort):
         chromium_win.ChromiumWinPort.__init__(self, **kwargs)
 
     def baseline_search_path(self):
-        return ([self._webkit_baseline_path('chromium-gpu-win')] +
+        return (map(self._webkit_baseline_path, ['chromium-gpu-win', 'chromium-gpu']) +
                 chromium_win.ChromiumWinPort.baseline_search_path(self))
 
     def path_to_test_expectations_file(self):
