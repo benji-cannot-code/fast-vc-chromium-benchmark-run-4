0384db195b388af6394b70c511a00cd63f172e63
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-20 15:49:15 +0000

    Use scoped_refptr for refcounted param in SafeBrowsingService.
    BUG=28083
    TEST=none
    
    Review URL: http://codereview.chromium.org/3951001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -387,9 +387,6 @@ void SafeBrowsingService::OnIOInitialize(
                                                       mackey_url_prefix,
                                                       disable_auto_update);
 
-  // Balance the reference added by Start().
-  request_context_getter->Release();
-
   protocol_manager_->Initialize();
 }
 
@@ -649,9 +646,8 @@ void SafeBrowsingService::Start() {
   }
 
   // We will issue network fetches using the default profile's request context.
-  URLRequestContextGetter* request_context_getter =
+  scoped_refptr<URLRequestContextGetter> request_context_getter =
       GetDefaultProfile()->GetRequestContext();
-  request_context_getter->AddRef();  // Balanced in OnIOInitialize.
 
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
