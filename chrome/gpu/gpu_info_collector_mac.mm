351bc3b7e64e3a3d05444d09a07ac446763f4dc8
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-20 18:11:13 +0000

    Fix the mesa gl issue in chrome. BUG=58343 TEST=none
    Review URL: http://codereview.chromium.org/3860004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/gpu/gpu_info_collector_mac.mm
+++ chrome/gpu/gpu_info_collector_mac.mm
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "base/string_piece.h"
 #include "base/sys_string_conversions.h"
+#include "app/gfx/gl/gl_bindings.h"
 #include "app/gfx/gl/gl_context.h"
+#include "app/gfx/gl/gl_implementation.h"
+#include "app/gfx/gl/gl_interface.h"
 
 #import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
 #import <IOKit/IOKitLib.h>
-#import <OpenGL/gl.h>
 
 namespace gpu_info_collector {
 
@@ -129,6 +131,8 @@ static std::wstring CollectGLInfo(int *out_gl_version,
 
 
 bool CollectGraphicsInfo(GPUInfo* gpu_info) {
+  if (gfx::GetGLImplementation() != gfx::kGLImplementationDesktopGL)
+    return false;
   // Video Card data.
   int vendor_id = 0, device_id = 0;
   // OpenGL data.
