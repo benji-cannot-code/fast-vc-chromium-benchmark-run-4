f78d13a33137b4efcfa7fe046a8c5f6eb6a4eca9
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-20 09:14:09 +0000

    2010-10-19  Andrey Kosyakov  <caseq@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: expose request/response cookies in HAR
            https://bugs.webkit.org/show_bug.cgi?id=47894
    
            Test: inspector/cookie-parser.html
    
            * WebCore.gypi:
            * WebCore.vcproj/WebCore.vcproj:
            * inspector/front-end/CookieParser.js: Added.
            * inspector/front-end/HAREntry.js:
            (WebInspector.HAREntry.prototype._buildRequest):
            (WebInspector.HAREntry.prototype._buildResponse):
            (WebInspector.HAREntry.prototype._buildCookies):
            (WebInspector.HAREntry.prototype._buildCookie):
            * inspector/front-end/Resource.js: Parse Cookie/Set-Cookie headers and expose cookies.
            (WebInspector.Resource.prototype.set requestHeaders):
            (WebInspector.Resource.prototype.get requestCookies):
            (WebInspector.Resource.prototype.set responseHeaders):
            (WebInspector.Resource.prototype.get responseCookies):
            (WebInspector.Resource.prototype._parseRequestCookies):
            (WebInspector.Resource.prototype._parseResponseCookies):
            * inspector/front-end/WebKit.qrc:
            * inspector/front-end/inspector.html:
    
    2010-10-19  Andrey Kosyakov  <caseq@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: expose request/response cookies in HAR
            https://bugs.webkit.org/show_bug.cgi?id=47894
    
            * src/WebResourceRawHeaders.cpp:
            (WebKit::addHeader): paste coalescent headers using "\n: as a separator instead of ", "
    
    2010-10-19  Andrey Kosyakov  <caseq@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: expose request/response cookies in HAR
            https://bugs.webkit.org/show_bug.cgi?id=47894
    
            * http/tests/inspector/inspector-test.js:
            (dumpObject): Handle null objects as a special case (display null instead of empty object)
            * http/tests/inspector/inspector-test2.js:
            (initialize_InspectorTest.InspectorTest.addObject): Ditto.
            * http/tests/inspector/resource-har-conversion-expected.txt:
            * http/tests/inspector/resource-har-conversion.html:
            * inspector/cookie-parser-expected.txt: Added.
            * inspector/cookie-parser.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/Resource.js
+++ third_party/WebKit/WebCore/inspector/front-end/Resource.js
@@ -351,6 +351,7 @@ WebInspector.Resource.prototype = {
     {
         this._requestHeaders = x;
         delete this._sortedRequestHeaders;
+        delete this._requestCookies;
 
         this.dispatchEventToListeners("requestHeaders changed");
     },
@@ -373,6 +374,13 @@ WebInspector.Resource.prototype = {
         return this._headerValue(this.requestHeaders, headerName);
     },
 
+    get requestCookies()
+    {
+        if (!this._requestCookies)
+            this._requestCookies = WebInspector.CookieParser.parseCookie(this.requestHeaderValue("Cookie"));
+        return this._requestCookies;
+    },
+
     get requestFormData()
     {
         return this._requestFormData;
@@ -393,6 +401,7 @@ WebInspector.Resource.prototype = {
     {
         this._responseHeaders = x;
         delete this._sortedResponseHeaders;
+        delete this._responseCookies;
 
         this.dispatchEventToListeners("responseHeaders changed");
     },
@@ -413,6 +422,13 @@ WebInspector.Resource.prototype = {
     responseHeaderValue: function(headerName)
     {
         return this._headerValue(this.responseHeaders, headerName);
+    },
+
+    get responseCookies()
+    {
+        if (!this._responseCookies)
+            this._responseCookies = WebInspector.CookieParser.parseSetCookie(this.responseHeaderValue("Set-Cookie"));
+        return this._responseCookies;
     },
 
     get queryParameters()
