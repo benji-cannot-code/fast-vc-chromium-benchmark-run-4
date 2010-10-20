cdac30c1dec7c5801122fe873972391e5d8cf4e2
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-20 00:05:54 +0000

    Revert 63135 - Update icons to show lowdata and very lowdata for 3G data.
    Seems to be repsonsible for a compile failure.
    
    BUG=chromium-os:5608
    TEST=manual
    Review URL: http://codereview.chromium.org/3748005
    
    TBR=chocobo@chromium.org
    Review URL: http://codereview.chromium.org/3923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63145 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/network_menu_button.h
+++ chrome/browser/chromeos/status/network_menu_button.h
@@ -57,7 +57,6 @@ class NetworkMenuButton : public StatusAreaButton,
 
   // NetworkLibrary::Observer implementation.
   virtual void NetworkChanged(NetworkLibrary* obj);
-  virtual void CellularDataPlanChanged(NetworkLibrary* obj);
 
   // Sets the badge icon.
   void SetBadge(const SkBitmap& badge);
