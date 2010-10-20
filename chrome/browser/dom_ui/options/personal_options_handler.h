04880e791fe45ab09f0ee600d5eb1f6524982f4d
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-20 21:48:29 +0000

    DOMUI: Major fixes to the sync settings group of the Personal Stuff page.
    BUG=57101
    TEST=none
    
    Review URL: http://codereview.chromium.org/3907002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options/personal_options_handler.h
+++ chrome/browser/dom_ui/options/personal_options_handler.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class OptionsManagedBannerHandler;
 
 // Chrome personal options page UI handler.
-class PersonalOptionsHandler : public OptionsPageUIHandler {
+class PersonalOptionsHandler : public OptionsPageUIHandler,
+                               public ProfileSyncServiceObserver {
  public:
   PersonalOptionsHandler();
   virtual ~PersonalOptionsHandler();
@@ -30,9 +31,13 @@ class PersonalOptionsHandler : public OptionsPageUIHandler {
                        const NotificationSource& source,
                        const NotificationDetails& details);
 
+  // ProfileSyncServiceObserver implementation.
+  virtual void OnStateChanged();
+
  private:
   void ObserveThemeChanged();
-  void SetSyncStatusUIString(const ListValue* args);
+  void ShowSyncLoginDialog(const ListValue* args);
+  void OpenPrivacyDashboardTabAndActivate(const ListValue* args);
   void ThemesReset(const ListValue* args);
   void ThemesGallery(const ListValue* args);
 #if defined(TOOLKIT_GTK)
--- chrome/browser/dom_ui/options/stop_syncing_handler.cc
+++ chrome/browser/dom_ui/options/stop_syncing_handler.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/dom_ui/stop_syncing_handler.h"
+#include "chrome/browser/dom_ui/options/stop_syncing_handler.h"
 
 #include "app/l10n_util.h"
 #include "base/basictypes.h"
@@ -40,9 +40,8 @@ void StopSyncingHandler::RegisterMessages() {
 }
 
 void StopSyncingHandler::StopSyncing(const ListValue* args){
-  DCHECK(dom_ui_);
   ProfileSyncService* service = dom_ui_->GetProfile()->GetProfileSyncService();
-  if(service != NULL && ProfileSyncService::IsSyncEnabled()) {
+  if (service != NULL && ProfileSyncService::IsSyncEnabled()) {
     service->DisableForUser();
     ProfileSyncService::SyncEvent(ProfileSyncService::STOP_FROM_OPTIONS);
   }
