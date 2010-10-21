f18525f3af1bffcc401daf9c63ce7e070bba8aef
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 19:11:39 +0000

    Makes RLZ listen for changes to instant so that it can update at the right time.
    BUG=56259
    TEST=none
    
    Review URL: http://codereview.chromium.org/3969004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_model.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 
@@ -85,6 +86,11 @@ void InstantController::Update(TabContents* tab_contents,
   } else {
     ScheduleUpdate(match.destination_url);
   }
+
+  NotificationService::current()->Notify(
+      NotificationType::INSTANT_CONTROLLER_UPDATED,
+      Source<InstantController>(this),
+      NotificationService::NoDetails());
 }
 
 void InstantController::SetOmniboxBounds(const gfx::Rect& bounds) {
