6dbaf9c3eea4b6418e912e90faf354318dcdca51
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 08:44:58 +0000

    Disable hangy TabDraggingTest.Tab1Tab3Escape
    TBR=sidchat
    
    BUG=57687
    TEST=interactive_ui_tests
    Review URL: http://codereview.chromium.org/4001004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63343 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tabs/tab_dragging_test.cc
+++ chrome/browser/views/tabs/tab_dragging_test.cc
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Disabled on Toolkit views bot. See http://crbug.com/42614
 #define MAYBE_Tab1Tab3Escape DISABLED_Tab1Tab3Escape
 #elif defined(OS_WIN)
-// Fails on Windows. See http://crbug.com/57687
-#define MAYBE_Tab1Tab3Escape FLAKY_Tab1Tab3Escape
+// Disabled on Windows. See http://crbug.com/57687
+#define MAYBE_Tab1Tab3Escape DISABLED_Tab1Tab3Escape
 #else
 #define MAYBE_Tab1Tab3Escape Tab1Tab3Escape
 #endif
