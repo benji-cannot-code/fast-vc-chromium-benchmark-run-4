6620e68baa447b7feba4b6aec8cf1464bfe193c8
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 02:23:27 +0000

    Fix a failing test for the extension webstorePrivate API.
    The test has been failing very frequently, always hitting the expectation
    in FakeProfileSyncService::RemoveObserver that the observer being removed isn't
    the currently registered one.
    
    I think the problem is that I was waiting until the PromptBrowserLoginFunction
    destructor (which is called by the ref count going to 0) to have the function
    remove itself as a listener, when really once the OnStateChanged function 
    fires and sync setup is complete we don't want to be listening anymore.
    
    BUG=59419
    TEST=The ExtensionWebstorePrivateBrowserTest.BrowserLogin browser test should
    stop failling
    
    Review URL: http://codereview.chromium.org/3974002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webstore_private_api.cc
+++ chrome/browser/extensions/extension_webstore_private_api.cc
@@ -128,10 +128,7 @@ bool SetStoreLoginFunction::RunImpl() {
   return true;
 }
 
-PromptBrowserLoginFunction::~PromptBrowserLoginFunction() {
-  if (observing_sync_state_)
-    GetSyncService(profile_)->RemoveObserver(this);
-}
+PromptBrowserLoginFunction::~PromptBrowserLoginFunction() {}
 
 bool PromptBrowserLoginFunction::RunImpl() {
   if (!IsWebStoreURL(profile_, source_url()))
@@ -151,7 +148,6 @@ bool PromptBrowserLoginFunction::RunImpl() {
   // Matched with a Release in OnStateChanged().
   AddRef();
 
-  observing_sync_state_ = true;
   sync_service->AddObserver(this);
   // TODO(mirandac/estade) - make use of |preferred_email| to pre-populate the
   // browser login dialog if it was set to non-empty above.
@@ -165,6 +161,7 @@ void PromptBrowserLoginFunction::OnStateChanged() {
   ProfileSyncService* sync_service = GetSyncService(profile_);
   // If the setup is finished, we'll report back what happened.
   if (!sync_service->SetupInProgress()) {
+    sync_service->RemoveObserver(this);
     DictionaryValue* dictionary = new DictionaryValue();
 
     // TODO(asargent) - send the browser login token here too if available.
