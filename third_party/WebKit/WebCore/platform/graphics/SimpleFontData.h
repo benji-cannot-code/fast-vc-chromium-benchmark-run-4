4e7d6bd28d7f2d7076c0c9e00b75790b5171b404
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-21 22:37:31 +0000

    2010-10-21  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by James Robinson.
    
            [Chromium] Fix Chromium Mac build
            https://bugs.webkit.org/show_bug.cgi?id=48096
    
            Fix Chromium Mac build after r70225 (Chromium uses Core Text but is not
            PLATFORM(MAC)).
    
            * platform/graphics/SimpleFontData.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
+++ third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef struct OpaqueATSUStyle* ATSUStyle;
 #endif
 
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || USE(CORE_TEXT)
 #include <wtf/RetainPtr.h>
 #endif
 
@@ -131,7 +131,7 @@ public:
     NSFont* getNSFont() const { return m_platformData.nsFont(); }
 #endif
 
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || USE(CORE_TEXT)
     CFDictionaryRef getCFStringAttributes(TypesettingFeatures) const;
 #endif
 
@@ -235,7 +235,7 @@ public:
 private:
 #endif
 
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || USE(CORE_TEXT)
     mutable HashMap<unsigned, RetainPtr<CFDictionaryRef> > m_CFStringAttributes;
 #endif
 
