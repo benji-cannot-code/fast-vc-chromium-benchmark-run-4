0d74b0fd7fda34381ff9f349641fdc814e5b778e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-21 11:26:01 +0000

    2010-10-21  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Tests LayoutTests/fast/encoding/utf-32* fail when using glib unicode
            https://bugs.webkit.org/show_bug.cgi?id=47805
    
            Register UTF-32 encodings in TextCodecGtk.
    
            * platform/text/gtk/TextCodecGtk.cpp:
            (WebCore::TextCodecGtk::registerBaseEncodingNames):
            (WebCore::TextCodecGtk::registerBaseCodecs):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/gtk/TextCodecGtk.cpp
+++ third_party/WebKit/WebCore/platform/text/gtk/TextCodecGtk.cpp
@@ -106,6 +106,9 @@ void TextCodecGtk::registerBaseEncodingNames(EncodingNameRegistrar registrar)
 {
     // Unicode
     registerEncodingNameIfAvailable(registrar, "UTF-8");
+    registerEncodingNameIfAvailable(registrar, "UTF-32");
+    registerEncodingNameIfAvailable(registrar, "UTF-32BE");
+    registerEncodingNameIfAvailable(registrar, "UTF-32LE");
 
     // Western
     if (registerEncodingNameIfAvailable(registrar, "ISO-8859-1")) {
@@ -125,6 +128,9 @@ void TextCodecGtk::registerBaseCodecs(TextCodecRegistrar registrar)
 {
     // Unicode
     registerCodecIfAvailable(registrar, "UTF-8");
+    registerCodecIfAvailable(registrar, "UTF-32");
+    registerCodecIfAvailable(registrar, "UTF-32BE");
+    registerCodecIfAvailable(registrar, "UTF-32LE");
 
     // Western
     registerCodecIfAvailable(registrar, "ISO-8859-1");
