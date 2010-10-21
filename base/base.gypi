bf59bc0fc3f6191bd6767c07b1f69a4a26ffd312
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 19:35:50 +0000

    Add a message pump for touchui=1
    The message pump reads events directly from X. For most events, it passes them
    on to GDK for normal processing. It consumes some events (e.g. keypress events)
    to demonstrate how it's intended to work. This, of course, makes chrome mostly
    unusable since you can only use the mouse to do things. But this is a small
    first step towards capturing events through MPX (e.g. touch etc.) and processing
    them as chrome pleases.
    
    glib message pump: Slightly change architecture
    
    This changeset breaks down the glib message pump a little so that it can be
    easily subclassed. The next set of commits will introduce a subclass that still
    uses GTK and GDK widgets, but reads events directly from X instead of through
    GTK/GDK.
    
    Review URL: http://codereview.chromium.org/3748002
    Patch from Sadrul Chowdhury <sadrul@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63397 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base.gypi
+++ base/base.gypi
@@ -334,6 +334,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
               'sources!': [
                 'atomicops_internals_x86_gcc.cc',
                 'message_pump_glib.cc',
+                'message_pump_glib_x.cc',
               ],
           }],
           [ 'OS != "linux"', {
@@ -442,6 +443,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'md5.h',
           'message_pump_glib.cc',
           'message_pump_glib.h',
+          'message_pump_glib_x.cc',
+          'message_pump_glib_x.h',
           'message_pump_libevent.cc',
           'message_pump_libevent.h',
           'message_pump_mac.h',
--- base/message_loop.cc
+++ base/message_loop.cc
@@ -24,6 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
 #include "base/message_pump_glib.h"
 #endif
+#if defined(TOUCH_UI)
+#include "base/message_pump_glib_x.h"
+#endif
 
 using base::Time;
 using base::TimeDelta;
@@ -134,6 +137,9 @@ MessageLoop::MessageLoop(Type type)
 #define MESSAGE_PUMP_IO new base::MessagePumpForIO()
 #elif defined(OS_MACOSX)
 #define MESSAGE_PUMP_UI base::MessagePumpMac::Create()
+#define MESSAGE_PUMP_IO new base::MessagePumpLibevent()
+#elif defined(TOUCH_UI)
+#define MESSAGE_PUMP_UI new base::MessagePumpGlibX()
 #define MESSAGE_PUMP_IO new base::MessagePumpLibevent()
 #elif defined(OS_POSIX)  // POSIX but not MACOSX.
 #define MESSAGE_PUMP_UI new base::MessagePumpForUI()
@@ -534,9 +540,9 @@ bool MessageLoop::DoWork() {
   return false;
 }
 
-bool MessageLoop::DoDelayedWork(Time* next_delayed_work_time) {
+bool MessageLoop::DoDelayedWork(base::Time* next_delayed_work_time) {
   if (!nestable_tasks_allowed_ || delayed_work_queue_.empty()) {
-    *next_delayed_work_time = Time();
+    *next_delayed_work_time = base::Time();
     return false;
   }
 
