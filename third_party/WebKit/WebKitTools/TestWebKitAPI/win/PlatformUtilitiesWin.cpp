fe0a6b932aa674c93b8b421fbdb2ac1efce7dbe8
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-21 00:39:50 +0000

    Windows build fix
    * TestWebKitAPI/win/PlatformUtilitiesWin.cpp: Added missing #include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/TestWebKitAPI/win/PlatformUtilitiesWin.cpp
+++ third_party/WebKit/WebKitTools/TestWebKitAPI/win/PlatformUtilitiesWin.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PlatformUtilities.h"
 
+#include <WebKit2/WKStringCF.h>
 #include <WebKit2/WKURLCF.h>
 #include <wtf/RetainPtr.h>
 
