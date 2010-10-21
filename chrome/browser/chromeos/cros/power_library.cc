4f1e1f67171f73567d85548c03e8bf17798f959e
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 08:43:42 +0000

    Add RequestRestart and RequestShutdown in power_library
    We'll use these from the update confirmation dialog and the screen locker
    on Chrome OS.
    
    BUG=chromium-os:6526
    TEST=Confirmed that restart worked with another change I'm working on
    
    Review URL: http://codereview.chromium.org/4022002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/power_library.cc
+++ chrome/browser/chromeos/cros/power_library.cc
@@ -76,6 +76,18 @@ class PowerLibraryImpl : public PowerLibrary {
     chromeos::EnableScreenLock(enable);
   }
 
+  virtual void RequestRestart() {
+    if (!CrosLibrary::Get()->EnsureLoaded())
+      return;
+    chromeos::RequestRestart();
+  }
+
+  virtual void RequestShutdown() {
+    if (!CrosLibrary::Get()->EnsureLoaded())
+      return;
+    chromeos::RequestShutdown();
+  }
+
  private:
   static void PowerStatusChangedHandler(void* object,
       const chromeos::PowerStatus& status) {
@@ -137,6 +149,8 @@ class PowerLibraryStubImpl : public PowerLibrary {
     return base::TimeDelta::FromSeconds(0);
   }
   virtual void EnableScreenLock(bool enable) {}
+  virtual void RequestRestart() {}
+  virtual void RequestShutdown() {}
 };
 
 // static
@@ -152,4 +166,3 @@ PowerLibrary* PowerLibrary::GetImpl(bool stub) {
 // Allows InvokeLater without adding refcounting. This class is a Singleton and
 // won't be deleted until it's last InvokeLater is run.
 DISABLE_RUNNABLE_METHOD_REFCOUNT(chromeos::PowerLibraryImpl);
-
