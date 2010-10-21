4e99744afb2772c0347fca5199db40ced84d1145
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-21 17:04:16 +0000

    Ask the UI client to close the page when the WKView receives a WM_CLOSE message
    Fixes <http://webkit.org/b/48044> <rdar://problem/8488446> Pressing
    Ctrl+W when viewing a full-page PDF destroys the WKView but doesn't
    close its parent window
    
    Reviewed by Jon Honeycutt.
    
    WebKit2:
    
    * UIProcess/win/WebView.cpp:
    (WebKit::WebView::wndProc): Call through to WebPageProxy::tryClose when
    we receive a WM_CLOSE message, rather than passing the message along to
    ::DefWindowProcW. tryClose will end up asking the UI client to close
    the page, while ::DefWindowProcW would destroy us synchronously without
    notifying the UI client at all. One case where we'll get sent a
    WM_CLOSE message is when the user presses Ctrl+W when using the Adobe
    Reader plugin to view a PDF.
    
    WebKitTools:
    
    Test that the UI client gets notified when WKView receives a WM_CLOSE message
    
    * TestWebKitAPI/Tests/WebKit2/win/WMCloseCallsUIClientClose.cpp: Added.
    (TestWebKitAPI::close): Record that this function was called.
    (TestWebKitAPI::WebKit2_WMCloseCallsUIClientClose): Create a WKView,
    send it a WM_CLOSE message, and test that the UI client's close
    callback got called. Note that this will hang if the bug is
    reintroduced; <http://webkit.org/b/48043> covers making TestWebKitAPI
    able to handle hangs.
    
    * TestWebKitAPI/win/TestWebKitAPI.vcproj: Added the new test.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -82,6 +82,9 @@ LRESULT WebView::wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     bool handled = true;
 
     switch (message) {
+        case WM_CLOSE:
+            m_page->tryClose();
+            break;
         case WM_DESTROY:
             m_isBeingDestroyed = true;
             close();
