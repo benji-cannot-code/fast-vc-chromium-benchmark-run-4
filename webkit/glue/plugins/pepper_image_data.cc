644606f29bff0236e93da3aa6f1be8b1d9049f14
klobag@chromium.org <klobag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 22:07:01 +0000

    Add the support of PP_IMAGEDATAFORMAT_RGBA_PREMUL in the browser.
    If the plugin format and the browser format do not match, the browser will handle the proper conversion to ensure it uses the correct format.
    
    This is re-sbumit of 63375. Added couple of cast to make win happy.
    
    Review URL: http://codereview.chromium.org/4000004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63436 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_image_data.cc
+++ webkit/glue/plugins/pepper_image_data.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/ppapi/c/pp_resource.h"
 #include "third_party/ppapi/c/ppb_image_data.h"
 #include "third_party/ppapi/c/trusted/ppb_image_data_trusted.h"
+#include "third_party/skia/include/core/SkColorPriv.h"
 #include "webkit/glue/plugins/pepper_plugin_instance.h"
 #include "webkit/glue/plugins/pepper_plugin_module.h"
 
@@ -24,11 +25,11 @@ namespace pepper {
 namespace {
 
 PP_ImageDataFormat GetNativeImageDataFormat() {
-  return PP_IMAGEDATAFORMAT_BGRA_PREMUL;
+  return ImageData::GetNativeImageDataFormat();
 }
 
 bool IsImageDataFormatSupported(PP_ImageDataFormat format) {
-  return format == PP_IMAGEDATAFORMAT_BGRA_PREMUL;
+  return ImageData::IsImageDataFormatSupported(format);
 }
 
 PP_Resource Create(PP_Module module_id,
@@ -99,6 +100,7 @@ const PPB_ImageDataTrusted ppb_imagedata_trusted = {
 
 ImageData::ImageData(PluginModule* module)
     : Resource(module),
+      format_(PP_IMAGEDATAFORMAT_BGRA_PREMUL),
       width_(0),
       height_(0) {
 }
@@ -116,12 +118,28 @@ const PPB_ImageDataTrusted* ImageData::GetTrustedInterface() {
   return &ppb_imagedata_trusted;
 }
 
+// static
+PP_ImageDataFormat ImageData::GetNativeImageDataFormat() {
+  if (SK_B32_SHIFT == 0)
+    return PP_IMAGEDATAFORMAT_BGRA_PREMUL;
+  else if (SK_R32_SHIFT == 0)
+    return PP_IMAGEDATAFORMAT_RGBA_PREMUL;
+  else
+    return PP_IMAGEDATAFORMAT_BGRA_PREMUL;  // Default to something on failure.
+}
+
+// static
+bool ImageData::IsImageDataFormatSupported(PP_ImageDataFormat format) {
+  return format == PP_IMAGEDATAFORMAT_BGRA_PREMUL ||
+      format == PP_IMAGEDATAFORMAT_RGBA_PREMUL;
+}
+
 bool ImageData::Init(PP_ImageDataFormat format,
                      int width, int height,
                      bool init_to_zero) {
   // TODO(brettw) this should be called only on the main thread!
   // TODO(brettw) use init_to_zero when we implement caching.
-  if (format != PP_IMAGEDATAFORMAT_BGRA_PREMUL)
+  if (!IsImageDataFormatSupported(format))
     return false;  // Only support this one format for now.
   if (width <= 0 || height <= 0)
     return false;
@@ -131,13 +149,14 @@ bool ImageData::Init(PP_ImageDataFormat format,
 
   platform_image_.reset(
       module()->GetSomeInstance()->delegate()->CreateImage2D(width, height));
+  format_ = format;
   width_ = width;
   height_ = height;
   return !!platform_image_.get();
 }
 
 void ImageData::Describe(PP_ImageDataDesc* desc) const {
-  desc->format = PP_IMAGEDATAFORMAT_BGRA_PREMUL;
+  desc->format = format_;
   desc->size.width = width_;
   desc->size.height = height_;
   desc->stride = width_ * 4;
@@ -179,6 +198,7 @@ const SkBitmap* ImageData::GetMappedBitmap() const {
 void ImageData::Swap(ImageData* other) {
   swap(other->platform_image_, platform_image_);
   swap(other->mapped_canvas_, mapped_canvas_);
+  std::swap(other->format_, format_);
   std::swap(other->width_, width_);
   std::swap(other->height_, height_);
 }
