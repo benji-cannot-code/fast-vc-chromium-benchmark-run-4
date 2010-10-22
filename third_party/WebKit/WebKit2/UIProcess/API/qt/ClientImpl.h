99bd5a783ebe4646ecabbd6c48a316e748391a46
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-22 21:08:18 +0000

    Fix Qt build.
    * UIProcess/API/qt/ClientImpl.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/ClientImpl.h
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/ClientImpl.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DefaultClientCallbacksQt_h
 #define DefaultClientCallbacksQt_h
 
-#include <WebKit2/WKBase.h>
+#include <WebKit2/WKPage.h>
 
 #ifdef __cplusplus
 extern "C" {
