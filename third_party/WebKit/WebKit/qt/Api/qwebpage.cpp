317536ba0c05db3f9b075ff23b60792f67305c5e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-22 15:35:38 +0000

    2010-10-22  Yi Shen  <yi.4.shen@nokia.com>
            Reviewed by Andreas Kling.
    
            [Qt] InspectorClientQt crashes when deleting a qwebpage which has an inspector
            https://bugs.webkit.org/show_bug.cgi?id=48079
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::~QWebPagePrivate):
            (QWebPage::~QWebPage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70308 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -324,6 +324,11 @@ QWebPagePrivate::QWebPagePrivate(QWebPage *qq)
 
 QWebPagePrivate::~QWebPagePrivate()
 {
+    if (inspector && inspectorIsInternalOnly) {
+        // Since we have to delete an internal inspector,
+        // call setInspector(0) directly to prevent potential crashes
+        setInspector(0);
+    }
 #ifndef QT_NO_CONTEXTMENU
     delete currentContextMenu;
 #endif
@@ -333,6 +338,9 @@ QWebPagePrivate::~QWebPagePrivate()
     delete settings;
     delete page;
     
+    if (inspector)
+        inspector->setPage(0);
+
 #if ENABLE(NOTIFICATIONS)
     NotificationPresenterClientQt::notificationPresenter()->removeClient();
 #endif
@@ -1760,14 +1768,6 @@ QWebPage::~QWebPage()
     FrameLoader *loader = d->mainFrame->d->frame->loader();
     if (loader)
         loader->detachFromParent();
-    if (d->inspector) {
-        // Since we have to delete an internal inspector,
-        // call setInspector(0) directly to prevent potential crashes
-        if (d->inspectorIsInternalOnly)
-            d->setInspector(0);
-        else
-            d->inspector->setPage(0);
-    }
     delete d;
 }
 
