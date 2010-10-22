82e5c05af35df19a3c2224a14a914fd493f7a580
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-22 20:33:50 +0000

    Mac: Delay setting window titles while the window menu is being tracked. Works around OS wackiness.
    BUG=56745
    TEST=repro as in bug 60165 (has better instructions)
    
    Review URL: http://codereview.chromium.org/4005005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63557 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/browser_window_cocoa.h
+++ chrome/browser/cocoa/browser_window_cocoa.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_COCOA_BROWSER_WINDOW_COCOA_H_
 #pragma once
 
+#include "base/scoped_nsobject.h"
 #include "base/task.h"
 #include "chrome/browser/browser_window.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
@@ -135,6 +136,7 @@ class BrowserWindowCocoa : public BrowserWindow,
   Browser* browser_;  // weak, owned by controller
   BrowserWindowController* controller_;  // weak, owns us
   ScopedRunnableMethodFactory<Browser> confirm_close_factory_;
+  scoped_nsobject<NSString> pending_window_title_;
 };
 
 #endif  // CHROME_BROWSER_COCOA_BROWSER_WINDOW_COCOA_H_
