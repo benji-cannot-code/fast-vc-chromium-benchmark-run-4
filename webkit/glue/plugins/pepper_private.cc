ec724f2360012927c71a29fe9602e632dea1fa00
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-22 06:34:00 +0000

    Fix compilation with system ICU.
    This is upstreaming a Gentoo Linux patch.
    
    BUG=none
    TEST=noe
    
    Review URL: http://codereview.chromium.org/3769008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63490 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_private.cc
+++ webkit/glue/plugins/pepper_private.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/glue/plugins/pepper_private.h"
 
+#include "unicode/usearch.h"
+
 #include "app/resource_bundle.h"
 #include "base/metrics/histogram.h"
 #include "base/utf_string_conversions.h"
@@ -15,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/platform_canvas.h"
 #include "third_party/ppapi/c/pp_resource.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/icu/public/i18n/unicode/usearch.h"
 #include "webkit/glue/webkit_glue.h"
 #include "webkit/glue/plugins/pepper_image_data.h"
 #include "webkit/glue/plugins/pepper_plugin_delegate.h"
