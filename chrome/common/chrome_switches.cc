54b3a94332a1bd811c1920429de13188442b35bb
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-22 19:50:12 +0000

    Revert 63535 - Disable GPU features AcceleratingCompositing and WebGL by default.
    (This change got checked into trunk only to get merged into M8.)
    
    Flags have been switched to reversed logic, and about_flags.cc modified to expose these to users in GUI.
    
    BUG=56053, 59092
    TEST=Use about:flags to toggle features, confirm state with e.g. http://webglsamples.googlecode.com/hg/aquarium/aquarium.html http://webkit.org/blog-files/3d-transforms/poster-circle.html Vince's change for about:flags.
    
    Review URL: http://codereview.chromium.org/3978002
    
    TBR=vangelis@chromium.org
    Review URL: http://codereview.chromium.org/3974004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -112,6 +112,10 @@ const char kDebugPrint[]                    = "debug-print";
 // Triggers a pletora of diagnostic modes.
 const char kDiagnostics[]                   = "diagnostics";
 
+// Disables accelerated compositing.
+const char kDisableAcceleratedCompositing[] =
+    "disable-accelerated-compositing";
+
 // Disables the alternate window station for the renderer.
 const char kDisableAltWinstation[]          = "disable-winsta";
 
@@ -170,6 +174,9 @@ const char kDisableDevTools[]               = "disable-dev-tools";
 
 // Disables device orientation events.
 const char kDisableDeviceOrientation[]      = "disable-device-orientation";
+
+// Disable experimental WebGL support.
+const char kDisableExperimentalWebGL[]      = "disable-webgl";
 
 // Disable extensions.
 const char kDisableExtensions[]             = "disable-extensions";
@@ -349,9 +356,6 @@ extern const char kLogNetLog[]              = "log-net-log";
 // Enable gpu-accelerated 2d canvas.
 const char kEnableAccelerated2dCanvas[]     = "enable-accelerated-2d-canvas";
 
-// Enable gpu-accelerated compositing.
-const char kEnableAcceleratedCompositing[]  = "enable-accelerated-compositing";
-
 // Enables WebKit accessibility within the renderer process.
 const char kEnableAccessibility[]           = "enable-accessibility";
 
@@ -405,9 +409,6 @@ const char kEnableDNSSECCerts[]             = "enable-dnssec-certs";
 // Enables extension APIs that are in development.
 const char kEnableExperimentalExtensionApis[] =
     "enable-experimental-extension-apis";
-
-// Enable experimental WebGL support.
-const char kEnableExperimentalWebGL[]       = "enable-webgl";
 
 // Enable experimental timeline API.
 const char kEnableExtensionTimelineApi[]    = "enable-extension-timeline-api";
