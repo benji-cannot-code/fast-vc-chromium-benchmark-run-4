5227770cc499ec13e0f4b590801d28a86c3cdb29
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-22 17:58:47 +0000

    Fix file comment to use python triple quotes on both sides of the docstring.
    TBR=jorlow
    
    Review URL: http://codereview.chromium.org/4043004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63531 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/setup_third_party.py
+++ webkit/support/setup_third_party.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"A helper script for setting up forwarding headers."""
+"""A helper script for setting up forwarding headers."""
 
 import errno
 import os
