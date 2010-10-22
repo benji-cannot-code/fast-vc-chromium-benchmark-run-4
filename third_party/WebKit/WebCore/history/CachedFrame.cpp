cb192796ece46232f3d34dbc6fe6760269ad2d03
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-22 18:44:59 +0000

    2010-10-21  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Adam Roben.
    
            Content in <iframe> doesn't appear when going back to http://webkit.org/blog/386/3d-transforms/
            https://bugs.webkit.org/show_bug.cgi?id=32447
    
            Added documentDidBecomeActive() when document comes back from cache to cause compositing
            layers in iframes to be reattached.
    
            There is no layout test, but I added a manual test. This test is ready to be a layout test
            but currently DRT disables the WebCore page cache, so the back/forward essentially do a 
            page load which does not exhibit the bug.
    
            * history/CachedFrame.cpp:
            (WebCore::CachedFrameBase::restore):
            * manual-tests/compositing/show-composited-iframe-on-back-button.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/history/CachedFrame.cpp
+++ third_party/WebKit/WebCore/history/CachedFrame.cpp
@@ -116,6 +116,8 @@ void CachedFrameBase::restore()
     if (m_document->hasListenerType(Document::TOUCH_LISTENER))
         m_document->page()->chrome()->client()->needTouchEvents(true);
 #endif
+
+    m_document->documentDidBecomeActive();
 }
 
 CachedFrame::CachedFrame(Frame* frame)
