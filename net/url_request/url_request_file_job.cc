697be6fb7989f9a6fa724e2bff339ca9cf5b4ffd
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-23 00:07:32 +0000

    Thread IO safety: annotate file_util, and block IO thread from doing IO
    - Mark functions in file_util_posix as requiring permission to perform
      disk actions.
    - Mark the IO thread as disallowed from performing disk actions.
    - Temporarily work around the protections in places where we currently
      have bugs.
    
    BUG=59847,59849,60207,60211
    TEST=no dchecks in debug builds
    
    Review URL: http://codereview.chromium.org/3872002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63600 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_file_job.cc
+++ net/url_request/url_request_file_job.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/platform_file.h"
 #include "base/string_util.h"
+#include "base/thread_restrictions.h"
 #include "build/build_config.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/io_buffer.h"
@@ -129,8 +130,15 @@ void URLRequestFileJob::Start() {
     return;
   }
 #endif
+
+  // URL requests should not block on the disk!
+  //   http://code.google.com/p/chromium/issues/detail?id=59849
+  bool exists;
   base::PlatformFileInfo file_info;
-  bool exists = file_util::GetFileInfo(file_path_, &file_info);
+  {
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    exists = file_util::GetFileInfo(file_path_, &file_info);
+  }
 
   // Continue asynchronously.
   MessageLoop::current()->PostTask(FROM_HERE, NewRunnableMethod(
