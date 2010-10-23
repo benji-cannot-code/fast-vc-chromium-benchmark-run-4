78a7ee74b1bb6a57e14933ce6bb8a8c349759200
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-23 23:21:19 +0000

    2010-10-23  David Kilzer  <ddkilzer@apple.com>
            <http://webkit.org/b/48186> Remove unneeded WebHTMLRepresentationInternal.h header
    
            Reviewed by Sam Weinig.
    
            The only method defined in WebHTMLRepresentationInternal.h is
            also defined in WebHTMLRepresentation.h, so use that instead.
    
            * WebKit.xcodeproj/project.pbxproj: Removed references to
            WebHTMLRepresentationInternal.h.
    2010-10-23  David Kilzer  <ddkilzer@apple.com>
    
            <http://webkit.org/b/48186> Remove unneeded WebHTMLRepresentationInternal.h header
    
            Reviewed by Sam Weinig.
    
            The only method defined in WebHTMLRepresentationInternal.h is
            also defined in WebHTMLRepresentation.h, so use that instead.
    
            * WebView/WebHTMLRepresentationInternal.h: Removed.
    2010-10-23  David Kilzer  <ddkilzer@apple.com>
    
            <http://webkit.org/b/48186> Remove unneeded WebHTMLRepresentationInternal.h header
    
            Reviewed by Sam Weinig.
    
            The only method defined in WebHTMLRepresentationInternal.h is
            also defined in WebHTMLRepresentation.h, so use that instead.
    
            * DumpRenderTree/mac/InternalHeaders/WebKit/WebHTMLRepresentationInternal.h: Removed.
            * DumpRenderTree/mac/WebArchiveDumpSupport.mm: Switched to use
            WebHTMLRepresentation.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/mac/WebArchiveDumpSupport.mm
+++ third_party/WebKit/WebKitTools/DumpRenderTree/mac/WebArchiveDumpSupport.mm
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 #import <WebKit/WebArchive.h>
-#import <WebKit/WebHTMLRepresentationInternal.h>
+#import <WebKit/WebHTMLRepresentation.h>
 
 static void convertMIMEType(NSMutableString *mimeType)
 {
