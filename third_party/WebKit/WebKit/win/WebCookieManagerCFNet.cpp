a8726a689a307b24e04a0e01935a54b641bfe0aa
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-23 00:59:24 +0000

    Fix the Windows build.
    * WebCookieManagerCFNet.cpp: Rename CookieStorageWin.h to CookieStorageCFNet.h.
    * WebView.cpp: Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70377 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebCookieManagerCFNet.cpp
+++ third_party/WebKit/WebKit/win/WebCookieManagerCFNet.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCookieManager.h"
 
 #include <CFNetwork/CFHTTPCookiesPriv.h>
-#include <WebCore/CookieStorageWin.h>
+#include <WebCore/CookieStorageCFNet.h>
 
 using namespace WebCore;
 
