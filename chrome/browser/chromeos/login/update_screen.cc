13001c930494aad374ea9b5a379fe24e234dd24e
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-25 18:19:43 +0000

    [cros] Mark OOBE as complete after successful update.
    BUG=chromium-os:7961
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/4030004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63751 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/update_screen.cc
+++ chrome/browser/chromeos/login/update_screen.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/login/screen_observer.h"
 #include "chrome/browser/chromeos/login/update_view.h"
+#include "chrome/browser/chromeos/login/wizard_controller.h"
 
 namespace {
 
@@ -79,6 +80,8 @@ void UpdateScreen::UpdateStatusChanged(UpdateLibrary* library) {
       view()->SetProgress(kBeforeFinalizingProgress);
       break;
     case UPDATE_STATUS_UPDATED_NEED_REBOOT:
+      // Make sure that first OOBE stage won't be shown after reboot.
+      WizardController::MarkOobeCompleted();
       view()->SetProgress(kProgressComplete);
       view()->ShowCurtain(false);
       CrosLibrary::Get()->GetUpdateLibrary()->RebootAfterUpdate();
