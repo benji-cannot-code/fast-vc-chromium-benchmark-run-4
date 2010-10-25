c319b28e857fe59bda6c3ac168a8cad8824160e0
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-25 00:38:23 +0000

    2010-10-24  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed, fix typo of last build fix.
    
            * wtf/DateMath.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/DateMath.cpp
@@ -77,7 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ASCIICType.h"
 #include "CurrentTime.h"
 #include "MathExtras.h"
-#include "StdLibExtras.h.h"
+#include "StdLibExtras.h"
 #include "StringExtras.h"
 
 #include <algorithm>
