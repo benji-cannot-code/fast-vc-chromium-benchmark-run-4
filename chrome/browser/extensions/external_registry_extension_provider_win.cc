48004e6fa0e5a2dc3ea5ef9d887a8f57469d5ad8
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-25 21:05:20 +0000

    Fix browser crash with External Extension having blank version.
    BUG=http://crbug.com/60191
    TEST=None (See http://crbug.com/58155)
    
    Review URL: http://codereview.chromium.org/4046007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63780 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_registry_extension_provider_win.cc
+++ chrome/browser/extensions/external_registry_extension_provider_win.cc
@@ -67,6 +67,7 @@ void ExternalRegistryExtensionProvider::VisitRegisteredExtension(
           if (!version.get()) {
             LOG(ERROR) << "Invalid version value " << extension_version
                        << " for key " << key_path;
+            ++iterator;
             continue;
           }
 
