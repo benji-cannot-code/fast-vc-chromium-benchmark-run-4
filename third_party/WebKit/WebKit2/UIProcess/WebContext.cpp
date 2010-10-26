71fe1a08471bc0b286ba5e1593668aa040b104fe
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-26 20:46:27 +0000

    Post any pending messages to the Injected Bundle in WebContext::processDidFinishLaunching instead of in WebContext::ensureWebProcess. https://bugs.webkit.org/show_bug.cgi?id=48367
    Reviewed by Darin Adler.
    
    Send pending injected bundle messages after the process finished launching instead of of
    directly after creating it.
    
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::ensureWebProcess):
    Move the logic to dispatch pending injected bundle messages from here ...
    (WebKit::WebContext::processDidFinishLaunching):
    ... to here.
    Also use a reference instead of a pointer when sending the pending messages.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebContext.cpp
@@ -179,12 +179,6 @@ void WebContext::ensureWebProcess()
     platformInitializeWebProcess(parameters);
 
     m_process->send(Messages::WebProcess::InitializeWebProcess(parameters), 0);
-
-    for (size_t i = 0; i != m_pendingMessagesToPostToInjectedBundle.size(); ++i) {
-        pair<String, RefPtr<APIObject> >* message = &m_pendingMessagesToPostToInjectedBundle[i];
-        m_process->send(InjectedBundleMessage::PostMessage, 0, CoreIPC::In(message->first, WebContextUserMessageEncoder(message->second.get())));
-    }
-    m_pendingMessagesToPostToInjectedBundle.clear();
 }
 
 void WebContext::processDidFinishLaunching(WebProcessProxy* process)
@@ -193,6 +187,12 @@ void WebContext::processDidFinishLaunching(WebProcessProxy* process)
     ASSERT(process == m_process);
 
     m_visitedLinkProvider.populateVisitedLinksIfNeeded();
+
+    for (size_t i = 0; i != m_pendingMessagesToPostToInjectedBundle.size(); ++i) {
+        pair<String, RefPtr<APIObject> >& message = m_pendingMessagesToPostToInjectedBundle[i];
+        m_process->send(InjectedBundleMessage::PostMessage, 0, CoreIPC::In(message.first, WebContextUserMessageEncoder(message.second.get())));
+    }
+    m_pendingMessagesToPostToInjectedBundle.clear();
 }
 
 void WebContext::processDidClose(WebProcessProxy* process)
