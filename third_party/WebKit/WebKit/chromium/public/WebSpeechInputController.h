7f954b278d2b9963f599532c30d0b488aa457c89
satish@chromium.org <satish@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-26 20:40:37 +0000

    2010-10-26  Satish Sampath  <satish@chromium.org>
            Reviewed by Jeremy Orlow.
    
            Add a 'grammar' attribute for speech input.
            https://bugs.webkit.org/show_bug.cgi?id=48339
    
            To differentiate between various types of speech input, we add a 'x-webkit-grammar' attribute to a
            speech enabled input element. This is passed without validation to the speech recognizer. Typical values
            could be "builtin:search", "builtin:dictation" and even an externally hosted SRGS grammar XML file URI.
            It is up to the recognizer to interpret the value and use it as an aid in recognition.
    
            * bindings/generic/RuntimeEnabledFeatures.h:
            (WebCore::RuntimeEnabledFeatures::webkitGrammarEnabled):
            * html/HTMLAttributeNames.in:
            * html/HTMLInputElement.idl:
            * page/SpeechInput.cpp:
            (WebCore::SpeechInput::startRecognition):
            * page/SpeechInput.h:
            * page/SpeechInputClient.h:
            * platform/mock/SpeechInputClientMock.cpp:
            (WebCore::SpeechInputClientMock::startRecognition):
            * platform/mock/SpeechInputClientMock.h:
            * rendering/TextControlInnerElements.cpp:
            (WebCore::InputFieldSpeechButtonElement::defaultEventHandler):
    2010-10-26  Satish Sampath  <satish@chromium.org>
    
            Reviewed by Jeremy Orlow.
    
            Add a 'grammar' attribute for speech input.
            https://bugs.webkit.org/show_bug.cgi?id=48339
    
            To differentiate between various types of speech input, we add a 'x-webkit-grammar' attribute to a
            speech enabled input element. This is passed without validation to the speech recognizer. Typical values
            could be "builtin:search", "builtin:dictation" and even an externally hosted SRGS grammar XML file URI.
            It is up to the recognizer to interpret the value and use it as an aid in recognition.
    
            * public/WebSpeechInputController.h:
            (WebKit::WebSpeechInputController::startRecognition):
            * src/SpeechInputClientImpl.cpp:
            (WebKit::SpeechInputClientImpl::startRecognition):
            * src/SpeechInputClientImpl.h:
            * src/WebSpeechInputControllerMockImpl.cpp:
            (WebKit::WebSpeechInputControllerMockImpl::startRecognition):
            * src/WebSpeechInputControllerMockImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/public/WebSpeechInputController.h
+++ third_party/WebKit/WebKit/chromium/public/WebSpeechInputController.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 struct WebRect;
+class WebString;
 
 // Provides an embedder API called by WebKit.
 class WebSpeechInputController {
@@ -44,6 +45,13 @@ public:
     // Starts speech recognition. Speech will get recorded until the endpointer detects silence,
     // runs to the limit or stopRecording is called. Progress indications and the recognized
     // text are returned via the listener interface.
+    virtual bool startRecognition(int requestId, const WebRect& elementRect, const WebString& grammar)
+    {
+        startRecognition(requestId, elementRect);
+        return false;
+    }
+
+    // FIXME: Remove this method once chromium implements the above method.
     virtual bool startRecognition(int requestId, const WebRect&)
     {
         WEBKIT_ASSERT_NOT_REACHED();
