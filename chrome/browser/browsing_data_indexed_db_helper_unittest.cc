709d9485ee857d38ac644436b3998a4e5ee0add5
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-26 17:19:22 +0000

    Temporarily disable tests failing after WebKit Roll 70513:70527.
    TBR=jorlow
    TEST=none
    BUG=60532
    
    Review URL: http://codereview.chromium.org/4128005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browsing_data_indexed_db_helper_unittest.cc
+++ chrome/browser/browsing_data_indexed_db_helper_unittest.cc
@@ -37,7 +37,7 @@ class TestCompletionCallback {
   DISALLOW_COPY_AND_ASSIGN(TestCompletionCallback);
 };
 
-TEST(CannedBrowsingDataIndexedDBHelperTest, AddIndexedDB) {
+TEST(CannedBrowsingDataIndexedDBHelperTest, DISABLED_AddIndexedDB) {
   TestingProfile profile;
 
   const GURL origin1("http://host1:1/");
@@ -67,7 +67,7 @@ TEST(CannedBrowsingDataIndexedDBHelperTest, AddIndexedDB) {
   EXPECT_EQ(FilePath(file2).value(), result[1].file_path.BaseName().value());
 }
 
-TEST(CannedBrowsingDataIndexedDBHelperTest, Unique) {
+TEST(CannedBrowsingDataIndexedDBHelperTest, DISABLED_Unique) {
   TestingProfile profile;
 
   const GURL origin("http://host1:1/");
