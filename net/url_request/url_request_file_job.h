c876767e69b1007be38dc52597b9ffa9cde6207e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-26 00:19:08 +0000

    Revert 63801 (broke chromeos bots) - Restrict file protocol on chromeos to certain whitelisted directories. BUG=cros-3412 TEST=Access file: directories on chromeos
    Review URL: http://codereview.chromium.org/3999005
    
    TBR=achuith@chromium.org
    Review URL: http://codereview.chromium.org/4140002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63821 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_file_job.h
+++ net/url_request/url_request_file_job.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
-#include <vector>
 
 #include "base/file_path.h"
 #include "net/base/completion_callback.h"
@@ -46,8 +45,6 @@ class URLRequestFileJob : public URLRequestJob {
  private:
   void DidResolve(bool exists, const base::PlatformFileInfo& file_info);
   void DidRead(int result);
-#if defined(OS_CHROMEOS)
-  static bool AccessDisabled(const FilePath& file_path);
 #endif
 
   net::CompletionCallbackImpl<URLRequestFileJob> io_callback_;
