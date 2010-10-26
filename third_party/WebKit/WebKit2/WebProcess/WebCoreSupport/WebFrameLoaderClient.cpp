811c06783a061ac2d310cc38363a793b0469eecd
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-26 23:00:49 +0000

    Reviewed by Sam Weinig.
            https://bugs.webkit.org/show_bug.cgi?id=48375
            <rdar://problem/8392724> Need delegate calls in PageLoaderClient to indicate if we have
            loaded insecure content
    
            Added the delegate. Just like the bundle version, it misses WebOrigin parameter that Mac
            delegate call used to have. It doesn't seem necessary for clients.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -780,8 +780,11 @@ void WebFrameLoaderClient::didDisplayInsecureContent()
     if (!webPage)
         return;
 
-    // Notify the bundle client.
-    webPage->injectedBundleLoaderClient().didDisplayInsecureContentForFrame(webPage, m_frame);
+    RefPtr<APIObject> userData;
+
+    webPage->injectedBundleLoaderClient().didDisplayInsecureContentForFrame(webPage, m_frame, userData);
+
+    WebProcess::shared().connection()->send(Messages::WebPageProxy::DidDisplayInsecureContentForFrame(m_frame->frameID(), InjectedBundleUserMessageEncoder(userData.get())), webPage->pageID());
 }
 
 void WebFrameLoaderClient::didRunInsecureContent(SecurityOrigin*)
@@ -790,8 +793,11 @@ void WebFrameLoaderClient::didRunInsecureContent(SecurityOrigin*)
     if (!webPage)
         return;
 
-    // Notify the bundle client.
-    webPage->injectedBundleLoaderClient().didRunInsecureContentForFrame(webPage, m_frame);
+    RefPtr<APIObject> userData;
+
+    webPage->injectedBundleLoaderClient().didRunInsecureContentForFrame(webPage, m_frame, userData);
+
+    WebProcess::shared().connection()->send(Messages::WebPageProxy::DidRunInsecureContentForFrame(m_frame->frameID(), InjectedBundleUserMessageEncoder(userData.get())), webPage->pageID());
 }
 
 ResourceError WebFrameLoaderClient::cancelledError(const ResourceRequest& request)
--- third_party/WebKit/WebKitTools/MiniBrowser/mac/BrowserWindowController.m
+++ third_party/WebKit/WebKitTools/MiniBrowser/mac/BrowserWindowController.m
@@ -306,6 +306,16 @@ static void didRemoveFrameFromHierarchy(WKPageRef page, WKFrameRef frame, WKType
     LOG(@"didRemoveFrameFromHierarchy");
 }
 
+static void didDisplayInsecureContentForFrame(WKPageRef page, WKFrameRef frame, WKTypeRef userData, const void *clientInfo)
+{
+    LOG(@"didDisplayInsecureContentForFrame");
+}
+
+static void didRunInsecureContentForFrame(WKPageRef page, WKFrameRef frame, WKTypeRef userData, const void *clientInfo)
+{
+    LOG(@"didRunInsecureContentForFrame");
+}
+
 static void didStartProgress(WKPageRef page, const void *clientInfo)
 {
     [(BrowserWindowController *)clientInfo didStartProgress];
@@ -548,6 +558,8 @@ static bool runBeforeUnloadConfirmPanel(WKPageRef page, WKStringRef message, WKF
         didFirstLayoutForFrame,
         didFirstVisuallyNonEmptyLayoutForFrame,
         didRemoveFrameFromHierarchy,
+        didDisplayInsecureContentForFrame,
+        didRunInsecureContentForFrame,
         didStartProgress,
         didChangeProgress,
         didFinishProgress,
