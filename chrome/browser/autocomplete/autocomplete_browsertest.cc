051b984b9e0f1c1b7ac168596607768c328a1c07
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-26 09:26:28 +0000

    Re-enable AutocompleteBrowserTest.Basic
    BUG=49324
    TEST=browser_tests --gtest_filter=AutocompleteBrowserTest.Basic --gtest_repeat=10000 --gtest_break_on_failure
    
    Review URL: http://codereview.chromium.org/3436027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63870 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_browsertest.cc
+++ chrome/browser/autocomplete/autocomplete_browsertest.cc
@@ -22,14 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/ui_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// Basic test is crashy on Mac
-// http://crbug.com/49324
-#if defined(OS_MAC)
-#define MAYBE_Basic DISABLED_Basic
-#else
-#define MAYBE_Basic Basic
-#endif
-
 // Autocomplete test is flaky on ChromeOS.
 // http://crbug.com/52928
 #if defined(OS_CHROMEOS)
@@ -67,7 +59,7 @@ class AutocompleteBrowserTest : public InProcessBrowserTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, MAYBE_Basic) {
+IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, Basic) {
   LocationBar* location_bar = GetLocationBar();
 
   EXPECT_EQ(std::wstring(), location_bar->GetInputString());
