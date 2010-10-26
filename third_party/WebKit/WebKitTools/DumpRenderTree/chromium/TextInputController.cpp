05e93c189bacea163191bdadbd1423b881be0242
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-26 19:25:36 +0000

    2010-10-26  Kenichi Ishibashi  <bashi@google.com>
            Reviewed by Kent Tamura.
    
            Input Method inserts conversion candidates unexpectedly
            https://bugs.webkit.org/show_bug.cgi?id=46868
    
            Adds a test.
    
            * platform/chromium/fast/text/chromium-mac-duplicate-ime-composition.html: Added.
    2010-10-26  Kenichi Ishibashi  <bashi@google.com>
    
            Reviewed by Kent Tamura.
    
            Input Method inserts conversion candidates unexpectedly
            https://bugs.webkit.org/show_bug.cgi?id=46868
    
            Calls updateStyleIfNeeded() before setting selection for the composition to
            avoid inserting the previous composition text into a content editable element
            which has an event handler that changes the style of the element.
    
            Test: platform/chromium/fast/text/chromium-mac-duplicate-ime-composition.html
    
            * editing/Editor.cpp:
            (WebCore::Editor::setComposition): calls updateStyleIfNeeded() before setting selection.
    2010-10-26  Kenichi Ishibashi  <bashi@google.com>
    
            Reviewed by Kent Tamura.
    
            Input Method inserts conversion candidates unexpectedly
            https://bugs.webkit.org/show_bug.cgi?id=46868
    
            Adds setComposition() to TextInputController to make DRT emulate
            an input method behavior.
    
            * DumpRenderTree/chromium/TextInputController.cpp:
            (TextInputController::TextInputController):
            (TextInputController::setComposition): Added.
            * DumpRenderTree/chromium/TextInputController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/TextInputController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/TextInputController.cpp
@@ -34,9 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TestShell.h"
 #include "WebBindings.h"
+#include "WebCompositionUnderline.h"
 #include "WebFrame.h"
 #include "WebRange.h"
 #include "WebString.h"
+#include "WebVector.h"
 #include "WebView.h"
 #include <wtf/StringExtras.h>
 #include <string>
@@ -67,6 +69,7 @@ TextInputController::TextInputController(TestShell* shell)
     bindMethod("substringFromRange", &TextInputController::substringFromRange);
     bindMethod("unmarkText", &TextInputController::unmarkText);
     bindMethod("validAttributesForMarkedText", &TextInputController::validAttributesForMarkedText);
+    bindMethod("setComposition", &TextInputController::setComposition);
 }
 
 WebFrame* TextInputController::getMainFrame()
@@ -232,4 +235,28 @@ void TextInputController::makeAttributedString(const CppArgumentList&, CppVarian
 {
     // FIXME: Implement this.
     result->setNull();
+}
+
+void TextInputController::setComposition(const CppArgumentList& arguments, CppVariant* result)
+{
+    result->setNull();
+
+    WebView* view = getMainFrame() ? getMainFrame()->view() : 0;
+    if (!view)
+        return;
+
+    if (arguments.size() < 1)
+        return;
+
+    // Sends a keydown event with key code = 0xE5 to emulate input method behavior.
+    WebKeyboardEvent keyDown;
+    keyDown.type = WebInputEvent::RawKeyDown;
+    keyDown.modifiers = 0;
+    keyDown.windowsKeyCode = 0xE5; // VKEY_PROCESSKEY
+    keyDown.setKeyIdentifierFromWindowsKeyCode();
+    view->handleInputEvent(keyDown);
+
+    WebVector<WebCompositionUnderline> underlines;
+    WebString text(WebString::fromUTF8(arguments[0].toString()));
+    view->setComposition(text, underlines, 0, text.length());
 }
