a47b6386fa47a52751c1c41493071207b3b99006
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-27 18:53:55 +0000

    2010-10-27  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: "Reload Page and Audit on Load" fails to run audits when resource tracking is disabled
            https://bugs.webkit.org/show_bug.cgi?id=48431
    
            * http/tests/inspector/inspector-test2.js:
            (initialize_InspectorTest.InspectorTest.enableResourceTracking):
            (initialize_InspectorTest.InspectorTest.disableResourceTracking):
            * http/tests/inspector/resource-parameters.html:
            * inspector/audits-panel-functional.html:
            * inspector/styles-source-offsets.html:
    2010-10-27  Alexander Pavlov  <apavlov@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: "Reload Page and Audit on Load" fails to run audits when resource tracking is disabled
            https://bugs.webkit.org/show_bug.cgi?id=48431
    
            * inspector/front-end/AuditsPanel.js:
            (WebInspector.AuditsPanel.prototype._reloadResources):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype._createPanelEnabler):
            (WebInspector.ResourcesPanel.prototype._enableResourceTracking):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70682 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test2.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test2.js
@@ -119,7 +119,7 @@ InspectorTest.enableResourceTracking = function(callback)
         callback();
     else {
         InspectorTest._reloadPageCallback = callback;
-        WebInspector.panels.resources._toggleResourceTracking();
+        WebInspector.panels.resources.toggleResourceTracking();
     }
 }
 
@@ -129,7 +129,7 @@ InspectorTest.disableResourceTracking = function()
         return;
 
     if (WebInspector.panels.resources.resourceTrackingEnabled)
-        WebInspector.panels.resources._toggleResourceTracking();
+        WebInspector.panels.resources.toggleResourceTracking();
 }
 
 InspectorTest.findDOMNode = function(root, filter, callback)
