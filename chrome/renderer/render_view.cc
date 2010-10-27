e87e6f9dff78cc513eb771445a6b11801fc15d01
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 20:45:51 +0000

    Forward textfield focus event to the browser.
    Add two messages in IPC channel: 1. The renderer needs to inform the browser if an element that can accept use input gains focus. This is useful when the browser may want to take some action against such events. For example, in case a textfield is focused, the browser may want to show an on-screen keyboard. 2. The browser needs some way to inform the renderer to scroll the currently focused element into the view frame. This is useful when, for instance, the browser resize the frame and the focused element goes out of visible frame.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3474007
    Patch from Varun Jain <varunjain@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -110,6 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/WebKit/chromium/public/WebDevToolsAgent.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebDragData.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebElement.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileChooserParams.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileSystem.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileSystemCallbacks.h"
@@ -204,6 +205,7 @@ using WebKit::WebDragData;
 using WebKit::WebDragOperation;
 using WebKit::WebDragOperationsMask;
 using WebKit::WebEditingAction;
+using WebKit::WebElement;
 using WebKit::WebExternalPopupMenu;
 using WebKit::WebExternalPopupMenuClient;
 using WebKit::WebFileChooserCompletion;
@@ -406,6 +408,21 @@ static std::string DetermineTextLanguage(const string16& text) {
     language = LanguageCodeWithDialects(cld_language);
   }
   return language;
+}
+
+// Returns true if the parameter node is a textfield, text area or a content
+// editable div.
+static bool IsEditableNode(const WebNode& node) {
+  bool is_editable_node = false;
+  if (!node.isNull()) {
+    if (node.isContentEditable()) {
+      is_editable_node = true;
+    } else if (node.isElementNode()) {
+      is_editable_node =
+          node.toConst<WebElement>().isTextFormControlElement();
+    }
+  }
+  return is_editable_node;
 }
 
 static bool WebAccessibilityNotificationToViewHostMsg(
@@ -786,6 +803,8 @@ void RenderView::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(ViewMsg_DragSourceSystemDragEnded,
                         OnDragSourceSystemDragEnded)
     IPC_MESSAGE_HANDLER(ViewMsg_SetInitialFocus, OnSetInitialFocus)
+    IPC_MESSAGE_HANDLER(ViewMsg_ScrollFocusedEditableNodeIntoView,
+                        OnScrollFocusedEditableNodeIntoView)
     IPC_MESSAGE_HANDLER(ViewMsg_UpdateTargetURL_ACK, OnUpdateTargetURLAck)
     IPC_MESSAGE_HANDLER(ViewMsg_UpdateWebPreferences, OnUpdateWebPreferences)
     IPC_MESSAGE_HANDLER(ViewMsg_SetAltErrorPageURL, OnSetAltErrorPageURL)
@@ -1357,6 +1376,22 @@ void RenderView::OnSetInitialFocus(bool reverse) {
   if (!webview())
     return;
   webview()->setInitialFocus(reverse);
+}
+
+void RenderView::OnScrollFocusedEditableNodeIntoView() {
+  if (!webview())
+    return;
+  WebFrame* focused_frame = webview()->focusedFrame();
+  if (focused_frame) {
+    WebDocument doc = focused_frame->document();
+    if (!doc.isNull()) {
+      WebNode node = doc.focusedNode();
+      if (IsEditableNode(node))
+        // TODO(varunjain): Change webkit API to scroll a particular node into
+        // view and use that API here instead.
+        webview()->scrollFocusedNodeIntoView();
+    }
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -2312,7 +2347,7 @@ void RenderView::focusPrevious() {
 }
 
 void RenderView::focusedNodeChanged(const WebNode& node) {
-  Send(new ViewHostMsg_FocusedNodeChanged(routing_id_));
+  Send(new ViewHostMsg_FocusedNodeChanged(routing_id_, IsEditableNode(node)));
 
   if (WebAccessibilityCache::accessibilityEnabled() && node.isNull()) {
     // TODO(ctguil): Make WebKit send this notification.
