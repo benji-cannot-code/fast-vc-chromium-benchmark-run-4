bfc1aec6fc387266c21c9970063c0b8e49b4da7c
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 21:08:38 +0000

    Fix some tiny javascript lint errors.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/4203002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64148 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extension_process_bindings.js
+++ chrome/renderer/resources/extension_process_bindings.js
@@ -71,7 +71,7 @@ var chrome = chrome || {};
         throw new Error("Parameter " + i + " is required.");
       }
     }
-  }
+  };
 
   // Callback handling.
   var requests = [];
@@ -83,7 +83,7 @@ var chrome = chrome || {};
         delete chrome.extension.lastError;
       } else {
         if (!error) {
-          error = "Unknown error."
+          error = "Unknown error.";
         }
         console.error("Error during " + name + ": " + error);
         chrome.extension.lastError = {
@@ -131,6 +131,8 @@ var chrome = chrome || {};
       delete requests[requestId];
       delete chrome.extension.lastError;
     }
+
+    return undefined;
   };
 
   chromeHidden.setViewType = function(type) {
@@ -289,7 +291,7 @@ var chrome = chrome || {};
         function(text, requestId) {
       var suggestCallback = function(suggestions) {
         chrome.experimental.omnibox.sendSuggestions(requestId, suggestions);
-      }
+      };
       chrome.Event.prototype.dispatch.apply(this, [text, suggestCallback]);
     };
   }
@@ -319,7 +321,7 @@ var chrome = chrome || {};
       for (var index = 0, name; name = namespaces[index]; index++) {
         module[name] = module[name] || {};
         module = module[name];
-      };
+      }
 
       // Add types to global validationTypes
       if (apiDef.types) {
@@ -413,7 +415,7 @@ var chrome = chrome || {};
 
       // getTabContentses is retained for backwards compatibility
       // See http://crbug.com/21433
-      chrome.extension.getTabContentses = chrome.extension.getExtensionTabs
+      chrome.extension.getTabContentses = chrome.extension.getExtensionTabs;
     });
 
     apiFunctions["tabs.connect"].handleRequest = function(tabId, connectInfo) {
@@ -565,7 +567,7 @@ var chrome = chrome || {};
         img.onerror = function() {
           console.error("Could not load " + actionType + " icon '" +
                         details.path + "'.");
-        }
+        };
         img.onload = function() {
           var canvas = document.createElement("canvas");
           canvas.width = img.width > iconSize ? iconSize : img.width;
@@ -578,7 +580,7 @@ var chrome = chrome || {};
           details.imageData = canvas_context.getImageData(0, 0, canvas.width,
                                                           canvas.height);
           sendCustomRequest(nativeFunction, name, [details], parameters);
-        }
+        };
         img.src = details.path;
       } else {
         throw new Error(
@@ -687,19 +689,19 @@ var chrome = chrome || {};
     apiFunctions["experimental.omnibox.styleNone"].handleRequest =
         function(offset) {
       return {type: "none", offset: offset};
-    }
+    };
     apiFunctions["experimental.omnibox.styleUrl"].handleRequest =
         function(offset) {
       return {type: "url", offset: offset};
-    }
+    };
     apiFunctions["experimental.omnibox.styleMatch"].handleRequest =
         function(offset) {
       return {type: "match", offset: offset};
-    }
+    };
     apiFunctions["experimental.omnibox.styleDim"].handleRequest =
         function(offset) {
       return {type: "dim", offset: offset};
-    }
+    };
 
     if (chrome.test) {
       chrome.test.getApiDefinitions = GetExtensionAPIDefinition;
