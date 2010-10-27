0b104b7d257655e630e58b072f27fb540255e2ab
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 16:41:18 +0000

    Disable scissor test during multisample resolve.
    If the scissor is enabled during a blit, it throws a GL error.
    
    BUG=60379
    TEST=open test in bug report, verify no crash
    
    Review URL: http://codereview.chromium.org/4202002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64081 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -1454,6 +1454,7 @@ ScopedResolvedFrameBufferBinder::ScopedResolvedFrameBufferBinder(
                        decoder_->offscreen_saved_frame_buffer_->id());
   const int width = decoder_->offscreen_size_.width();
   const int height = decoder_->offscreen_size_.height();
+  glDisable(GL_SCISSOR_TEST);
   if (IsAngle()) {
     glBlitFramebufferANGLE(0, 0, width, height, 0, 0, width, height,
                            GL_COLOR_BUFFER_BIT, GL_NEAREST);
@@ -1471,6 +1472,9 @@ ScopedResolvedFrameBufferBinder::~ScopedResolvedFrameBufferBinder() {
 
   ScopedGLErrorSuppressor suppressor(decoder_);
   decoder_->RestoreCurrentFramebufferBindings();
+  if (decoder_->enable_scissor_test_) {
+    glEnable(GL_SCISSOR_TEST);
+  }
 }
 
 Texture::Texture(GLES2DecoderImpl* decoder)
