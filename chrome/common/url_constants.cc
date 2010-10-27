ec32c1ea7e5e13aa0219f86231c95d7e21fec154
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 21:58:02 +0000

    First try at a DOMUI based textfield in chrome. This is a copy of the issue I first created (http://codereview.chromium.org/3582018). I deleted that issue accidentally :( So I will copy all the comments from that issue to here (I still have that browser window open fortunately)
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3977001
    Patch from Varun Jain <varunjain@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64158 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -89,6 +89,7 @@ const char kChromeUINewTabURL[] = "chrome://newtab";
 const char kChromeUIPluginsURL[] = "chrome://plugins/";
 const char kChromeUIPrintURL[] = "chrome://print/";
 const char kChromeUISettingsURL[] = "chrome://settings/";
+const char kChromeUITextfieldsURL[] = "chrome://textfields/";
 
 #if defined(OS_CHROMEOS)
 const char kChromeUIFileBrowseURL[] = "chrome://filebrowse/";
@@ -123,6 +124,7 @@ const char kChromeUIResourcesHost[] = "resources";
 const char kChromeUIScreenshotPath[] = "screenshots";
 const char kChromeUISettingsHost[] = "settings";
 const char kChromeUISyncResourcesHost[] = "syncresources";
+const char kChromeUITextfieldsHost[] = "textfields";
 const char kChromeUIThemePath[] = "theme";
 const char kChromeUIThumbnailPath[] = "thumb";
 
--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -80,6 +80,7 @@ extern const char kChromeUINewTabURL[];
 extern const char kChromeUIPluginsURL[];
 extern const char kChromeUIPrintURL[];
 extern const char kChromeUISettingsURL[];
+extern const char kChromeUITextfieldsURL[];
 
 #if defined(OS_CHROMEOS)
 extern const char kChromeUIFileBrowseURL[];
@@ -114,6 +115,7 @@ extern const char kChromeUIResourcesHost[];
 extern const char kChromeUIScreenshotPath[];
 extern const char kChromeUISettingsHost[];
 extern const char kChromeUISyncResourcesHost[];
+extern const char kChromeUITextfieldsHost[];
 extern const char kChromeUIThemePath[];
 extern const char kChromeUIThumbnailPath[];
 
