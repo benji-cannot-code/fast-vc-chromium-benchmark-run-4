7b6e31245811402c983c31235d7d5cd2a3a3f737
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-27 01:56:52 +0000

    2010-10-26  MORITA Hajime  <morrita@google.com>
            Reviewed by Tony Chang.
    
            Rolling in r70512 again.
    
            spellcheck='' should be the same as spellcheck="true"
            https://bugs.webkit.org/show_bug.cgi?id=25539
    
            * editing/spelling/spellcheck-attribute-expected.txt:
            * editing/spelling/spellcheck-attribute.html:
    2010-10-26  MORITA Hajime  <morrita@google.com>
    
            Reviewed by Tony Chang.
    
            Rolling in r70512 again, added missing null-check to
            Editor::isSpellCheckingEnabled().
            http://trac.webkit.org/changeset/70512
    
            spellcheck='' should be the same as spellcheck="true"
            https://bugs.webkit.org/show_bug.cgi?id=25539
    
            * dom/Element.cpp:
            (WebCore::Element::spellcheckAttributeState):
            (WebCore::Element::isSpellCheckingEnabled):
            * dom/Element.h:
            * editing/Editor.cpp:
            (WebCore::markMisspellingsOrBadGrammar):
            (WebCore::Editor::isSpellCheckingEnabledInFocusedNode):
            (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges):
            * editing/Editor.h:
            * platform/ContextMenu.cpp:
            (WebCore::ContextMenu::populate):
    2010-10-26  MORITA Hajime  <morrita@google.com>
    
            Reviewed by Tony Chang.
    
            Rolling in r70512 again.
    
            spellcheck='' should be the same as spellcheck="true"
            https://bugs.webkit.org/show_bug.cgi?id=25539
    
            * src/ContextMenuClientImpl.cpp:
            (WebKit::ContextMenuClientImpl::getCustomMenuFromDefaultItems):
            * src/EditorClientImpl.cpp:
            (WebKit::EditorClientImpl::shouldSpellcheckByDefault):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Element.h
+++ third_party/WebKit/WebCore/dom/Element.h
@@ -41,6 +41,12 @@ class DOMTokenList;
 class ElementRareData;
 class IntSize;
 
+enum SpellcheckAttributeState {
+    SpellcheckAttributeTrue,
+    SpellcheckAttributeFalse,
+    SpellcheckAttributeDefault
+};
+
 class Element : public ContainerNode {
 public:
     static PassRefPtr<Element> create(const QualifiedName&, Document*);
@@ -321,6 +327,8 @@ public:
     void webkitRequestFullScreen(unsigned short flags);
 #endif
 
+    bool isSpellCheckingEnabled() const;
+
 protected:
     Element(const QualifiedName& tagName, Document* document, ConstructionType type)
         : ContainerNode(document, type)
@@ -376,7 +384,9 @@ private:
 
     ElementRareData* rareData() const;
     ElementRareData* ensureRareData();
-    
+
+    SpellcheckAttributeState spellcheckAttributeState() const;
+
 private:
     mutable RefPtr<NamedNodeMap> m_attributeMap;
 };
