c5f7972062181b6cee78abbc346252ec0e3415bf
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-27 04:29:08 +0000

    2010-10-26  Eric Seidel  <eric@webkit.org>
            Reviewed by David Kilzer.
    
            build-webkit should collect Visual Studio Express logs and display them
            https://bugs.webkit.org/show_bug.cgi?id=39199
    
            * Scripts/build-webkit:
            * Scripts/print-vse-failure-logs: Added.
            * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
+++ third_party/WebKit/WebKitTools/Scripts/webkitdirs.pm
@@ -1187,6 +1187,12 @@ sub buildXCodeProject($$@)
     return system "xcodebuild", "-project", "$project.xcodeproj", @extraOptions;
 }
 
+sub usingVisualStudioExpress()
+{
+    determineConfigurationForVisualStudio();
+    return $willUseVCExpressWhenBuilding;
+}
+
 sub buildVisualStudioProject
 {
     my ($project, $clean) = @_;
