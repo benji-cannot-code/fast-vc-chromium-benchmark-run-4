37e44d14df9c37ccebb5daaa89f04b744af57bda
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-27 19:13:22 +0000

    Second attempt to land http://codereview.chromium.org/4003006 Added mock implementation of prewarm method TBR: nkostylev@chromium.org BUG=chromium-os:7971 TEST=Login and examine /tmp/login-times-sent
    Review URL: http://codereview.chromium.org/4133002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -192,6 +192,7 @@ void ExistingUserController::Init() {
 
   WmMessageListener::instance()->AddObserver(this);
 
+  LoginUtils::Get()->PrewarmAuthentication();
   if (CrosLibrary::Get()->EnsureLoaded())
     CrosLibrary::Get()->GetLoginLibrary()->EmitLoginPromptReady();
 }
