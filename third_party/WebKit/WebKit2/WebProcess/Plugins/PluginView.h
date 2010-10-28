e2953807eb2a9825c83e8c68311a6e46d19e4f71
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 16:14:34 +0000

    Make PluginView retain its HTMLPlugInElement
    This matches WebKit/mac's WebBaseNetscapePluginView. WebCore didn't do
    this, but had a very different mechanism for implementing NPRuntime
    that didn't require it.
    
    Fixes <http://webkit.org/b/46672> <rdar://problem/8484208> Crash
    (preceded by assertion) in Node::document when running
    plugins/document-open.html in WebKit2 on Windows
    
    Reviewed by Anders Carlsson.
    
    LayoutTests:
    
    Remove plugins/document-open.html from the win-wk2 Skipped file
    
    It passes now that <http://webkit.org/b/46672> is fixed.
    
    * platform/win-wk2/Skipped:
    
    WebKit2:
    
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::create): Moved here from the header file so that
    clients of the header file won't have to pull in HTMLPlugInElement.h.
    Changed to take a PassRefPtr<HTMLPlugInElement>.
    (WebKit::PluginView::PluginView): Changed to take a
    PassRefPtr<HTMLPlugInElement> and to use m_pluginElement later (because
    the parameter to the function will be nulled-out after assigning into
    m_pluginElement).
    (WebKit::PluginView::pluginElementNPObject): Updated for change to m_pluginElement.
    
    * WebProcess/Plugins/PluginView.h: Made m_pluginElement a RefPtr.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/Plugins/PluginView.h
+++ third_party/WebKit/WebKit2/WebProcess/Plugins/PluginView.h
@@ -48,10 +48,7 @@ namespace WebKit {
 
 class PluginView : public WebCore::PluginViewBase, WebCore::MediaCanStartListener, PluginController, WebFrame::LoadListener {
 public:
-    static PassRefPtr<PluginView> create(WebCore::HTMLPlugInElement* pluginElement, PassRefPtr<Plugin> plugin, const Plugin::Parameters& parameters)
-    {
-        return adoptRef(new PluginView(pluginElement, plugin, parameters));
-    }
+    static PassRefPtr<PluginView> create(PassRefPtr<WebCore::HTMLPlugInElement>, PassRefPtr<Plugin>, const Plugin::Parameters&);
 
     WebCore::Frame* frame();
 
@@ -69,7 +66,7 @@ public:
 #endif
 
 private:
-    PluginView(WebCore::HTMLPlugInElement*, PassRefPtr<Plugin>, const Plugin::Parameters& parameters);
+    PluginView(PassRefPtr<WebCore::HTMLPlugInElement>, PassRefPtr<Plugin>, const Plugin::Parameters& parameters);
     virtual ~PluginView();
 
     void initializePlugin();
@@ -138,7 +135,7 @@ private:
     virtual void didFinishLoad(WebFrame*);
     virtual void didFailLoad(WebFrame*, bool wasCancelled);
 
-    WebCore::HTMLPlugInElement* m_pluginElement;
+    RefPtr<WebCore::HTMLPlugInElement> m_pluginElement;
     RefPtr<Plugin> m_plugin;
     WebPage* m_webPage;
     Plugin::Parameters m_parameters;
