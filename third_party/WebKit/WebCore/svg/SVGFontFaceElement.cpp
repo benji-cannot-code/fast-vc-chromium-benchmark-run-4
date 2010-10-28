1a3aa5aefcf1c0e61d2556caad4b805aa3bf8d2c
yuzo@google.com <yuzo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 03:11:33 +0000

    2010-10-27  Yuzo Fujishima  <yuzo@google.com>
            Reviewed by Eric Seidel.
    
            Fix for Bug 48310 - SVG font-face-name without name attribute causes a crash
            https://bugs.webkit.org/show_bug.cgi?id=48310
    
            * svg/custom/font-face-name-without-name-attr-expected.txt: Added.
            * svg/custom/font-face-name-without-name-attr.svg: Added.
    2010-10-27  Yuzo Fujishima  <yuzo@google.com>
    
            Reviewed by Eric Seidel.
    
            Fix for Bug 48310 - SVG font-face-name without name attribute causes a crash
            https://bugs.webkit.org/show_bug.cgi?id=48310
    
            Test: svg/custom/font-face-name-without-name-attr.svg
    
            * svg/SVGFontFaceElement.cpp:
            (WebCore::SVGFontFaceElement::rebuildFontFace): Don't create src
            property if the src value list is empty.
            * svg/SVGFontFaceSrcElement.cpp:
            (WebCore::SVGFontFaceSrcElement::srcValue): Don't add empty src
            values to the src value list.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGFontFaceElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGFontFaceElement.cpp
@@ -289,7 +289,7 @@ void SVGFontFaceElement::rebuildFontFace()
             list = srcElement->srcValue();
     }
 
-    if (!list)
+    if (!list || !list->length())
         return;
 
     // Parse in-memory CSS rules
