37f3a2ed3718c3dd3de154267e7a4e8d24141320
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 16:34:48 +0000

    Make SigninManager tolerate SignOut() calls when not initialized.
    BUG=60713
    TEST=Configure policy to disable sync, start chrome. Observe that it doesn't crash right away!
    
    Review URL: http://codereview.chromium.org/4127008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_unittest.cc
+++ chrome/browser/sync/profile_sync_service_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/net/gaia/gaia_constants.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/testing_profile.h"
+#include "chrome/test/testing_pref_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 using std::vector;
@@ -469,6 +470,16 @@ TEST_F(ProfileSyncServiceTest, InitialState) {
   EXPECT_TRUE(bookmark_bar_id());
 
   ExpectModelMatch();
+}
+
+TEST_F(ProfileSyncServiceTest, DisabledByPolicy) {
+  profile_->GetTestingPrefService()->SetManagedPref(
+      prefs::kSyncManaged,
+      Value::CreateBooleanValue(true));
+  service_.reset(new TestProfileSyncService(&factory_, profile_.get(),
+                                            "", true, NULL));
+  service_->Initialize();
+  EXPECT_TRUE(service_->IsManaged());
 }
 
 TEST_F(ProfileSyncServiceTest, AbortedByShutdown) {
--- chrome/browser/sync/signin_manager.cc
+++ chrome/browser/sync/signin_manager.cc
@@ -84,6 +84,9 @@ void SigninManager::ProvideSecondFactorAccessCode(
 }
 
 void SigninManager::SignOut() {
+  if (!profile_)
+    return;
+
   client_login_.reset();
   last_result_ = ClientLoginResult();
   username_.clear();
