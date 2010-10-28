119660e81da38e33f752da5ba9aa0f8a637b8649
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 10:41:58 +0000

    2010-10-28  MORITA Hajime  <morrita@google.com>
            Reviewed by Kent Tamura.
    
            WebKit needs to expose an Element::spellcheck attribute to javascript.
            https://bugs.webkit.org/show_bug.cgi?id=25536
    
            * editing/spelling/script-tests/spelling-attribute-change.js: Added.
            * editing/spelling/spelling-attribute-change-expected.txt: Added.
            * editing/spelling/spelling-attribute-change.html: Added.
            * fast/dom/HTMLElement/script-tests/spellcheck.js: Added.
            * fast/dom/HTMLElement/spellcheck-expected.txt: Added.
            * fast/dom/HTMLElement/spellcheck.html: Added.
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/qt/Skipped:
    2010-10-27  MORITA Hajime  <morrita@google.com>
    
            Reviewed by Kent Tamura.
    
            WebKit needs to expose an Element::spellcheck attribute to javascript.
            https://bugs.webkit.org/show_bug.cgi?id=25536
    
            Published Element.spellcheck property on IDL, implemented it.
    
            Tests: editing/spelling/spelling-attribute-change.html
                   fast/dom/HTMLElement/spellcheck.html
    
            * html/HTMLElement.cpp:
            (WebCore::HTMLElement::spellcheck):
            (WebCore::HTMLElement::setSpellcheck):
            * html/HTMLElement.h:
            * html/HTMLElement.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLElement.h
+++ third_party/WebKit/WebCore/html/HTMLElement.h
@@ -67,6 +67,9 @@ public:
     virtual bool draggable() const;
     void setDraggable(bool);
 
+    bool spellcheck() const;
+    void setSpellcheck(bool);
+
     void click();
 
     virtual void accessKeyAction(bool sendToAnyElement);
