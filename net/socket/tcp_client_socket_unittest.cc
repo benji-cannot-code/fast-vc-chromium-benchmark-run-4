b6ad418bb6c8a221431252c450c1e06b8ee6d7ec
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 04:11:50 +0000

    Initial support for built-in DNS resolver/cache.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/3693001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64202 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_unittest.cc
+++ net/socket/tcp_client_socket_unittest.cc
@@ -94,7 +94,7 @@ void TCPClientSocketTest::SetUp() {
   AddressList addr;
   scoped_ptr<HostResolver> resolver(
       CreateSystemHostResolver(HostResolver::kDefaultParallelism,
-                               NULL));
+                               NULL, NULL));
   HostResolver::RequestInfo info(HostPortPair("localhost", listen_port_));
   int rv = resolver->Resolve(info, &addr, NULL, NULL, BoundNetLog());
   CHECK_EQ(rv, OK);
