3b8e48d715df172293665d7e028e7546ae1f1bd8
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 19:17:11 +0000

    2010-10-28  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by David Levin.
    
            [Chromium] Support FileSystem in chromium DRT
            https://bugs.webkit.org/show_bug.cgi?id=47643
            Update the chromium DEPS to pick up corresponding webkit_support change.
            Also add a temporary glue implementation with FIXME comment to
            WebFrameClient::openFileSystem.
    
            * DEPS:
            * public/WebFrameClient.h:
            (WebKit::WebFrameClient::openFileSystem):
    2010-10-28  Kinuko Yasuda  <kinuko@chromium.org>
    
            Reviewed by David Levin.
    
            [Chromium] Support FileSystem in chromium DRT
            https://bugs.webkit.org/show_bug.cgi?id=47643
    
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (WebViewHost::openFileSystem): Added.
            * DumpRenderTree/chromium/WebViewHost.h:
            (WebViewHost::openFileSystem): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -1054,6 +1054,11 @@ bool WebViewHost::allowScript(WebFrame*, bool enabledPerSettings)
     return enabledPerSettings;
 }
 
+void WebViewHost::openFileSystem(WebFrame* frame, WebFileSystem::Type type, long long size, bool create, WebFileSystemCallbacks* callbacks)
+{
+    webkit_support::OpenFileSystem(frame, type, size, create, callbacks);
+}
+
 // Public functions -----------------------------------------------------------
 
 WebViewHost::WebViewHost(TestShell* shell)
