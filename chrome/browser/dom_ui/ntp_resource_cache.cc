8a2b873e5179ebf8031199fbc6fc380f9df1c104
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 15:32:40 +0000

    Use special text for Guest Session instead of using standard text form Incognito mode.
    BUG=chromium-os:6907
    TEST=Enter Guest mode, open new tab. Check opened page.
    
    Review URL: http://codereview.chromium.org/4010004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/ntp_resource_cache.cc
+++ chrome/browser/dom_ui/ntp_resource_cache.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #include "app/theme_provider.h"
+#include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/ref_counted_memory.h"
 #include "base/string16.h"
@@ -197,8 +198,16 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
   DictionaryValue localized_strings;
   localized_strings.SetString("title",
       l10n_util::GetStringUTF16(IDS_NEW_TAB_TITLE));
+  int new_tab_message_ids = IDS_NEW_TAB_OTR_MESSAGE;
+  int new_tab_html_idr = IDR_INCOGNITO_TAB_HTML;
+#if defined(OS_CHROMEOS)
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kGuestSession)) {
+    new_tab_message_ids = IDS_NEW_TAB_GUEST_SESSION_MESSAGE;
+    new_tab_html_idr = IDR_GUEST_SESSION_TAB_HTML;
+  }
+#endif
   localized_strings.SetString("content",
-      l10n_util::GetStringFUTF16(IDS_NEW_TAB_OTR_MESSAGE,
+      l10n_util::GetStringFUTF16(new_tab_message_ids,
                                  GetUrlWithLang(GURL(kLearnMoreIncognitoUrl))));
   localized_strings.SetString("extensionsmessage",
       l10n_util::GetStringFUTF16(IDS_NEW_TAB_OTR_EXTENSIONS_MESSAGE,
@@ -213,7 +222,7 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
 
   static const base::StringPiece incognito_tab_html(
       ResourceBundle::GetSharedInstance().GetRawDataResource(
-          IDR_INCOGNITO_TAB_HTML));
+          new_tab_html_idr));
 
   std::string full_html = jstemplate_builder::GetI18nTemplateHtml(
       incognito_tab_html, &localized_strings);
