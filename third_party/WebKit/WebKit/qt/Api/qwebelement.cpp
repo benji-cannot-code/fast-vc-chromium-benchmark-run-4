cc4f96c1967c74d8f00d12672c33a09eb6860d54
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 16:05:51 +0000

    [Qt] QWebElement: Don't retrieve CSS rules through DOMWindow. https://bugs.webkit.org/show_bug.cgi?id=48519
    Reviewed by Andreas Kling.
    
    JavaScript is prevented from retrieving cross-domain CSS rules since r70335.
    This patch allow QWebElement to retrive style without this limitation
    by asking the Document directly instead of going through DOMWindow.
    
    Fixes:
    - tst_QWebFrame::setHtmlWithResource()
    - tst_QWebElement::style()
    
    * Api/qwebelement.cpp:
    (QWebElement::styleProperty):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebelement.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSRule.h"
 #include "CSSRuleList.h"
 #include "CSSStyleRule.h"
+#include "CSSStyleSelector.h"
 #include "Document.h"
 #include "DocumentFragment.h"
 #include "FrameView.h"
@@ -853,8 +854,8 @@ QString QWebElement::styleProperty(const QString &name, StyleResolveStrategy str
         // by importance and inheritance order. This include external CSS
         // declarations, as well as embedded and inline style declarations.
 
-        DOMWindow* domWindow = m_element->document()->frame()->domWindow();
-        if (RefPtr<CSSRuleList> rules = domWindow->getMatchedCSSRules(m_element, "")) {
+        Document* doc = m_element->document();
+        if (RefPtr<CSSRuleList> rules = doc->styleSelector()->styleRulesForElement(m_element, /*authorOnly*/ true)) {
             for (int i = rules->length(); i > 0; --i) {
                 CSSStyleRule* rule = static_cast<CSSStyleRule*>(rules->item(i - 1));
 
