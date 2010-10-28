4333a7216c6c1eaadca6a449f3dbbe40991ca47c
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 12:40:05 +0000

    Cleanup: Remove stale TODO
    The code in question is no longer needed since Skia has changed to remove the #define in question.
    
    BUG=None
    TEST=Compilation shouldn't break
    
    Review URL: http://codereview.chromium.org/4099004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64234 0039d316-1c4b-4281-b951-d872f2087c98

--- app/l10n_util.cc
+++ app/l10n_util.cc
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util_mac.h"
 #endif
 
-// TODO(playmobil): remove this undef once SkPostConfig.h is fixed.
-// skia/include/corecg/SkPostConfig.h #defines strcasecmp() so we can't use
-// base::strcasecmp() without #undefing it here.
-#undef strcasecmp
-
 namespace {
 
 #if defined(OS_WIN)
