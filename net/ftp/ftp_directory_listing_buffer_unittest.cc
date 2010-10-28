364e169715a6d5557f0bbe99f1f8ef2ee92a5f06
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 07:17:42 +0000

    FTP: fix directory listing parser for ftp://ftp.dd-wrt.com/others/juno6/
    Now the "ls -l" parser ignores lines with empty file names
    instead of failing.
    
    BUG=60065
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/4182002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64208 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_buffer_unittest.cc
+++ net/ftp/ftp_directory_listing_buffer_unittest.cc
@@ -42,6 +42,7 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
     "dir-listing-ls-16",
     "dir-listing-ls-17",
     "dir-listing-ls-18",
+    "dir-listing-ls-19",
     "dir-listing-mlsd-1",
     "dir-listing-mlsd-2",
     "dir-listing-netware-1",
