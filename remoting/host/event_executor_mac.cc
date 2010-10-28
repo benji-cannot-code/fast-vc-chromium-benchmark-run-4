5b7a65a7960b77c6cd161e027cc7e0abced13b89
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 22:00:09 +0000

    Fix mac compilation issue
    TBR=ncarter
    
    Review URL: http://codereview.chromium.org/4135010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64320 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/event_executor_mac.cc
+++ remoting/host/event_executor_mac.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/event_executor_mac.h"
 
-#include "remoting/protocol/messages_decoder.h"
+#include "remoting/proto/internal.pb.h"
+#include "remoting/protocol/message_decoder.h"
 
 namespace remoting {
 
