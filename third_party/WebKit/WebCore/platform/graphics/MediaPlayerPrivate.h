7e3ac3ccc088b71f6fed47a91075b9796bc3fad1
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 21:46:48 +0000

    2010-10-28  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Adam Roben.
    
            Seeking by very small increment doesn't generate 'seeked' event
            https://bugs.webkit.org/show_bug.cgi?id=48530
    
            Test: media/video-seek-by-small-increment.html
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::seek): Ask the media engine for its closest time value so we can
            avoid asking it to seek to the current time.
    
            * platform/graphics/MediaPlayer.cpp:
            (WebCore::MediaPlayer::mediaTimeForTimeValue): New.
            * platform/graphics/MediaPlayer.h:
            * platform/graphics/MediaPlayerPrivate.h:
            (WebCore::MediaPlayerPrivateInterface::mediaTimeForTimeValue): Ditto.
    
            * platform/graphics/mac/MediaPlayerPrivateQTKit.h:
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::MediaPlayerPrivate::mediaTimeForTimeValue): Return the closest value in the movie's time scale.
    
            * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
            (WebCore::MediaPlayerPrivateQuickTimeVisualContext::mediaTimeForTimeValue): Ditto
            * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.h:
    
            * platform/graphics/win/QTMovie.cpp:
            (QTMovie::timeScale): Return the movie's time scale.
            * platform/graphics/win/QTMovie.h:
    
    2010-10-28  Eric Carlson  <eric.carlson@apple.com>
    
            Reviewed by Adam Roben.
    
            Seeking by very small increment doesn't generate 'seeked' event
            https://bugs.webkit.org/show_bug.cgi?id=48530
    
            * media/video-seek-by-small-increment-expected.txt: Added.
            * media/video-seek-by-small-increment.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/MediaPlayerPrivate.h
+++ third_party/WebKit/WebCore/platform/graphics/MediaPlayerPrivate.h
@@ -126,6 +126,10 @@ public:
 
     virtual void prepareForRendering() { }
 
+    // Time value in the movie's time scale. It is only necessary to override this if the media
+    // engine uses rational numbers to represent media time.
+    virtual float mediaTimeForTimeValue(float timeValue) const { return timeValue; }
+
 };
 
 }
