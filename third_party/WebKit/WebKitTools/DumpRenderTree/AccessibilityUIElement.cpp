fa7b268a83f836e3d908b442603e4b4d65469567
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-28 18:54:13 +0000

    AX: multi select group option does not handle setting of AXSelectedChildren correctly https://bugs.webkit.org/show_bug.cgi?id=48464
    Reviewed by Adele Peterson.
    
    WebCore: 
    
    Ensure that when selecting an item from a <select> element, it accounts for <optgroups>.
    
    Test: platform/mac/accessibility/select-element-selection-with-optgroups.html
    
    * accessibility/AccessibilityListBoxOption.cpp:
    (WebCore::AccessibilityListBoxOption::setSelected):
    
    WebKitTools: 
    
    Add support for querying information about selected children to DRT, including:
       selectedChildAtIndex
       selectedChildrenCount
       setSelectedChild
    
    * DumpRenderTree/AccessibilityUIElement.cpp:
    (selectedChildAtIndexCallback):
    (setSelectedChildCallback):
    (selectedChildrenCountCallback):
    (AccessibilityUIElement::setSelectedChild):
    (AccessibilityUIElement::selectedChildrenCount):
    (AccessibilityUIElement::selectedChildAtIndex):
    (AccessibilityUIElement::getJSClass):
    * DumpRenderTree/AccessibilityUIElement.h:
    * DumpRenderTree/mac/AccessibilityUIElementMac.mm:
    (AccessibilityUIElement::selectedChildAtIndex):
    (AccessibilityUIElement::selectedChildrenCount):
    (AccessibilityUIElement::setSelectedChild):
    
    LayoutTests: 
    
    * platform/mac/accessibility/select-element-selection-with-optgroups-expected.txt: Added.
    * platform/mac/accessibility/select-element-selection-with-optgroups.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityUIElement.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/AccessibilityUIElement.cpp
@@ -205,6 +205,15 @@ static JSValueRef childAtIndexCallback(JSContextRef context, JSObjectRef functio
     return AccessibilityUIElement::makeJSAccessibilityUIElement(context, toAXElement(thisObject)->getChildAtIndex(indexNumber));
 }
 
+static JSValueRef selectedChildAtIndexCallback(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
+{
+    int indexNumber = -1;
+    if (argumentCount == 1)
+        indexNumber = JSValueToNumber(context, arguments[0], exception);
+    
+    return AccessibilityUIElement::makeJSAccessibilityUIElement(context, toAXElement(thisObject)->selectedChildAtIndex(indexNumber));
+}
+
 static JSValueRef linkedUIElementAtIndexCallback(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
 {
     int indexNumber = -1;
@@ -259,6 +268,17 @@ static JSValueRef isEqualCallback(JSContextRef context, JSObjectRef function, JS
         return JSValueMakeBoolean(context, false);
     
     return JSValueMakeBoolean(context, toAXElement(thisObject)->isEqual(toAXElement(otherElement)));
+}
+
+static JSValueRef setSelectedChildCallback(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
+{
+    JSObjectRef element = 0;
+    if (argumentCount == 1)
+        element = JSValueToObject(context, arguments[0], exception);
+
+    toAXElement(thisObject)->setSelectedChild(toAXElement(element));
+
+    return JSValueMakeUndefined(context);
 }
 
 static JSValueRef elementAtPointCallback(JSContextRef context, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception)
@@ -696,6 +716,11 @@ static JSValueRef speakCallback(JSContextRef context, JSObjectRef thisObject, JS
     return JSValueMakeString(context, speakString.get());
 }
 
+static JSValueRef selectedChildrenCountCallback(JSContextRef context, JSObjectRef thisObject, JSStringRef propertyName, JSValueRef* exception)
+{
+    return JSValueMakeNumber(context, toAXElement(thisObject)->selectedChildrenCount());
+}
+
 static JSValueRef getHasPopupCallback(JSContextRef context, JSObjectRef thisObject, JSStringRef, JSValueRef*)
 {
     return JSValueMakeBoolean(context, toAXElement(thisObject)->hasPopup());
@@ -758,6 +783,9 @@ static JSValueRef removeNotificationListenerCallback(JSContextRef context, JSObj
 #if !PLATFORM(MAC)
 JSStringRef AccessibilityUIElement::speak() { return 0; }
 JSStringRef AccessibilityUIElement::rangeForLine(int line) { return 0; }
+void AccessibilityUIElement::setSelectedChild(AccessibilityUIElement*) const { }
+unsigned AccessibilityUIElement::selectedChildrenCount() const { return 0; }
+AccessibilityUIElement AccessibilityUIElement::selectedChildAtIndex(unsigned) const { return 0; }
 #endif
 
 #if !SUPPORTS_AX_TEXTMARKERS
@@ -861,6 +889,7 @@ JSClassRef AccessibilityUIElement::getJSClass()
         { "ariaDropEffects", getARIADropEffectsCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { "isIgnored", isIgnoredCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { "speak", speakCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
+        { "selectedChildrenCount", selectedChildrenCountCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { 0, 0, 0, 0 }
     };
 
@@ -921,6 +950,8 @@ JSClassRef AccessibilityUIElement::getJSClass()
         { "accessibilityElementForTextMarker", accessibilityElementForTextMarkerCallback, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { "textMarkerRangeLength", textMarkerRangeLengthCallback, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { "textMarkerForPoint", textMarkerForPointCallback, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
+        { "setSelectedChild", setSelectedChildCallback, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
+        { "selectedChildAtIndex", selectedChildAtIndexCallback, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { 0, 0, 0 }
     };
 
