482c1d3594effa9811e77ad16b02331682e126f6
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-29 18:55:53 +0000

    2010-10-29  Oliver Hunt  <oliver@apple.com>
            Reviewed by Gavin Barraclough.
    
            REGRESSION: r69429-r69611: Crash in JSC::Interpreter::privateExecute
            https://bugs.webkit.org/show_bug.cgi?id=47573
    
            I think the interpreter portion of this was introduced by
            an incorrect but silent merge when I updated prior to committing.
            The JIT change is basically just a correctness fix, but it is
            needed to prevent the testcase from asserting in debug builds.
    
            The basic problem is incorrectly setting the activation object
            on an arguments object.  The crash was due to us setting a null
            activation in the interpreter, in the jit we were setting the
            activation of a strict mode arguments object.
    
            * interpreter/Interpreter.cpp:
            (JSC::Interpreter::privateExecute):
            * jit/JITStubs.cpp:
            (JSC::DEFINE_STUB_FUNCTION):
            * wtf/Platform.h:
    2010-10-29  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Gavin Barraclough.
    
            REGRESSION: r69429-r69611: Crash in JSC::Interpreter::privateExecute
            https://bugs.webkit.org/show_bug.cgi?id=47573
    
            Add a test to ensure that we don't incorrectly set a null activation
            as an argument object's activation.
    
            * fast/js/interpreter-no-activation-expected.txt: Added.
            * fast/js/interpreter-no-activation.html: Added.
            * fast/js/script-tests/interpreter-no-activation.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/interpreter-no-activation.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Make sure arguments tearoff works correctly when a functions activation has not been created.");
+
+function f1(a,b,c,d) {
+    if (0) (function (){ a; });
+    a = true;
+    return arguments[0];
+}
+shouldBeTrue("f1(false)");
+
+function f2(a,b,c,d) {
+    "use strict";
+    a = false;
+    if (0) (function (){ a; });
+    return arguments[0];
+}
+shouldBeTrue("f2(true)");
+testPassed("Didn't crash");
+
+var successfullyParsed = true;
