769e96f25eba3b8a80109771bba81b937bb3b6de
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-29 13:35:15 +0000

    2010-10-29  Zhenyao Mo  <zmo@google.com>
            Reviewed by Chris Marrin.
    
            drawArrays/drawElements with first/offset out of range but size==0 should generate no error
            https://bugs.webkit.org/show_bug.cgi?id=48601
    
            * fast/canvas/webgl/draw-arrays-out-of-bounds-expected.txt:
            * fast/canvas/webgl/draw-arrays-out-of-bounds.html:
            * fast/canvas/webgl/draw-elements-out-of-bounds-expected.txt:
            * fast/canvas/webgl/draw-elements-out-of-bounds.html:
    2010-10-29  Zhenyao Mo  <zmo@google.com>
    
            Reviewed by Chris Marrin.
    
            drawArrays/drawElements with first/offset out of range but size==0 should generate no error
            https://bugs.webkit.org/show_bug.cgi?id=48601
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::drawArrays):
            (WebCore::WebGLRenderingContext::drawElements):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.cpp
+++ third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -934,6 +934,9 @@ void WebGLRenderingContext::drawArrays(unsigned long mode, long first, long coun
         return;
     }
 
+    if (!count)
+        return;
+
     if (!isErrorGeneratedOnOutOfBoundsAccesses()) {
         // Ensure we have a valid rendering state
         CheckedInt<int32_t> checkedFirst(first);
@@ -983,6 +986,9 @@ void WebGLRenderingContext::drawElements(unsigned long mode, long count, unsigne
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
         return;
     }
+
+    if (!count)
+        return;
 
     long numElements = 0;
     if (!isErrorGeneratedOnOutOfBoundsAccesses()) {
