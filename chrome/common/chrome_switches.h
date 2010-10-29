199e40db8ff70863ea26694c0633d3fc9dea3ead
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-29 23:07:01 +0000

    [GTK] enable spoof proof infobars by default.
    BUG=60110
    TEST=manual
    
    Review URL: http://codereview.chromium.org/4223003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -147,7 +147,6 @@ extern const char kEnablePrintPreview[];
 extern const char kEnableRemoting[];
 extern const char kEnableResourceContentSettings[];
 extern const char kEnableSearchProviderApiV2[];
-extern const char kEnableSecureInfoBars[];
 extern const char kEnableSnapStart[];
 extern const char kEnableStatsTable[];
 extern const char kEnableSync[];
