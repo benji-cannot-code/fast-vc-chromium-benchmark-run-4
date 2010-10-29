e5886a7b24983d2f39f75cf1efdc578cbcf7af63
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-29 20:33:16 +0000

    2010-10-29  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Dirk Schulze.
    
            ASSERT when SVGAnimatedProperty instance is assigned to multiple elements.
            https://bugs.webkit.org/show_bug.cgi?id=48555
    
            Test: svg/custom/insertItemBefore-from-non-list-origin.svg
    
            * bindings/scripts/CodeGeneratorJS.pm: Include SVGAnimatedPropertyTearOff.h instead of SVGPropertyTearOff.h
            * bindings/scripts/CodeGeneratorObjC.pm: Ditto.
            * bindings/scripts/CodeGeneratorV8.pm: Ditto.
            * svg/properties/SVGAnimatedListPropertyTearOff.h:
            (WebCore::SVGAnimatedListPropertyTearOff::isAnimatedListTearOff): Return true.
            (WebCore::SVGAnimatedListPropertyTearOff::removeItemFromList): Devirtualize function.
            * svg/properties/SVGAnimatedProperty.h:
            (WebCore::SVGAnimatedProperty::isAnimatedListTearOff): Return false.
            * svg/properties/SVGListPropertyTearOff.h:
            (WebCore::SVGListPropertyTearOff::removeItemFromListIfNeeded): Only try to remove the passed item from its list, if its actually a list and not just a SVGAnimatedProperty.
    
    2010-10-29  Nikolas Zimmermann  <nzimmermann@rim.com>
    
            Reviewed by Dirk Schulze.
    
            ASSERT when SVGAnimatedProperty instance is assigned to multiple elements.
            https://bugs.webkit.org/show_bug.cgi?id=48555
    
            * svg/custom/insertItemBefore-from-non-list-origin-expected.txt: Added.
            * svg/custom/insertItemBefore-from-non-list-origin.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/properties/SVGAnimatedListPropertyTearOff.h
+++ third_party/WebKit/WebCore/svg/properties/SVGAnimatedListPropertyTearOff.h
@@ -47,7 +47,9 @@ public:
         return m_animVal.get();
     }
 
-    virtual int removeItemFromList(SVGProperty* property, bool shouldSynchronizeWrappers)
+    virtual bool isAnimatedListTearOff() const { return true; }
+
+    int removeItemFromList(SVGProperty* property, bool shouldSynchronizeWrappers)
     {
         // FIXME: No animVal support.
         if (!m_baseVal)
