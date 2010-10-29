581fd5b3b23215c3dce3c886887be2e730fbe10a
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-29 20:50:52 +0000

    Crash fix due to uninitialized pointers.
    BUG=chromium-os:7619
    TEST=none
    
    Review URL: http://codereview.chromium.org/4110009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/network_config_view.cc
+++ chrome/browser/chromeos/options/network_config_view.cc
@@ -60,9 +60,6 @@ NetworkConfigView::NetworkConfigView(const CellularNetwork* cellular)
 NetworkConfigView::NetworkConfigView()
     : browser_mode_(true),
       flags_(FLAG_WIFI | FLAG_LOGIN_ONLY | FLAG_OTHER_NETWORK),
-      ethernet_(NULL),
-      wifi_(NULL),
-      cellular_(NULL),
       cellularconfig_view_(NULL),
       wificonfig_view_(NULL),
       ipconfig_view_(NULL),
@@ -70,12 +67,6 @@ NetworkConfigView::NetworkConfigView()
 }
 
 NetworkConfigView::~NetworkConfigView() {
-  if (ethernet_)
-    delete ethernet_;
-  if (wifi_)
-    delete wifi_;
-  if (cellular_)
-    delete cellular_;
 }
 
 gfx::NativeWindow NetworkConfigView::GetNativeWindow() const {
@@ -190,7 +181,7 @@ void NetworkConfigView::Init() {
   AddChildView(tabs_);
 
   if (flags_ & FLAG_CELLULAR) {
-    cellularconfig_view_ = new CellularConfigView(this, cellular_);
+    cellularconfig_view_ = new CellularConfigView(this, cellular_.get());
     tabs_->AddTab(
         l10n_util::GetString(IDS_OPTIONS_SETTINGS_INTERNET_OPTIONS_USAGE),
         cellularconfig_view_);
@@ -199,7 +190,7 @@ void NetworkConfigView::Init() {
     if (flags_ & FLAG_OTHER_NETWORK)
       wificonfig_view_ = new WifiConfigView(this);
     else
-      wificonfig_view_ = new WifiConfigView(this, wifi_);
+      wificonfig_view_ = new WifiConfigView(this, wifi_.get());
     tabs_->AddTab(
         l10n_util::GetString(IDS_OPTIONS_SETTINGS_SECTION_TITLE_NETWORK_CONFIG),
         wificonfig_view_);
