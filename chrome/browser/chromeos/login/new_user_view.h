fd59516bc49dedca342a93da3afc0ee0c92fb345
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-29 09:10:47 +0000

    [cros] Hide Guest mode link based on account settings. Repsecting cros.accounts.allowBWSI.
    BUG=chromium-os:6789
    TEST=manual
    
    Review URL: http://codereview.chromium.org/4186007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/new_user_view.h
+++ chrome/browser/chromeos/login/new_user_view.h
@@ -60,7 +60,7 @@ class NewUserView : public views::View,
   // If |need_border| is true, RoundedRect border and background are required.
   NewUserView(Delegate* delegate,
               bool need_border,
-              bool need_browse_without_signin);
+              bool need_guest_link);
 
   virtual ~NewUserView();
 
@@ -151,7 +151,7 @@ class NewUserView : public views::View,
   views::View* splitter_;
   views::NativeButton* sign_in_button_;
   views::Link* create_account_link_;
-  views::Link* browse_without_signin_link_;
+  views::Link* guest_link_;
   views::MenuButton* languages_menubutton_;
   views::Throbber* throbber_;
 
@@ -177,8 +177,8 @@ class NewUserView : public views::View,
   // If true, this view needs RoundedRect border and background.
   bool need_border_;
 
-  // Whether browse without signin is needed.
-  bool need_browse_without_signin_;
+  // Whether Guest Mode link is needed.
+  bool need_guest_link_;
 
   // Whether create account link is needed. Set to false for now but we may
   // need it back in near future.
