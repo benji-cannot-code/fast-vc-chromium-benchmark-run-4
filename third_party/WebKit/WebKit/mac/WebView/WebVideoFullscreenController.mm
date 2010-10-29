a496ec8534a024406be75a1c25483f7d59f8021a
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-29 17:53:38 +0000

    2010-10-27  Jer Noble  <jer.noble@apple.com>
            Reviewed by Darin Adler.
    
            Full screen video in Safari (still) exits full-screen if you switch to another 
            app on a multi-display system.
            https://bugs.webkit.org/show_bug.cgi?id=47364
            <rdar://problem/8382299>
            
            The FullScreen window does not technically close when it deactivates, but the 
            two NSWindows that make up the FullScreen feature should not hide when deactivated.
            
            * WebView/WebVideoFullscreenController.mm:
            (createBackgroundFullscreenWindow): Do not set hidesOnDeactivate.
            (-[WebVideoFullscreenWindow initWithContentRect:styleMask:backing:defer:]): Ditto.
            * WebView/WebVideoFullscreenHUDWindowController.mm:
            (-[WebVideoFullscreenHUDWindow initWithContentRect:styleMask:backing:defer:]): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenController.mm
@@ -247,7 +247,6 @@ static NSWindow *createBackgroundFullscreenWindow(NSRect frame, int level)
     [window setOpaque:YES];
     [window setBackgroundColor:[NSColor blackColor]];
     [window setLevel:level];
-    [window setHidesOnDeactivate:YES];
     [window setReleasedWhenClosed:NO];
     return window;
 }
@@ -476,7 +475,6 @@ static NSWindow *createBackgroundFullscreenWindow(NSRect frame, int level)
         return nil;
     [self setOpaque:NO];
     [self setBackgroundColor:[NSColor clearColor]];
-    [self setHidesOnDeactivate:YES];
     [self setIgnoresMouseEvents:NO];
     [self setAcceptsMouseMovedEvents:YES];
     return self;
--- third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenHUDWindowController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebVideoFullscreenHUDWindowController.mm
@@ -92,7 +92,6 @@ static inline CGFloat webkit_CGFloor(CGFloat value)
     [self setAcceptsMouseMovedEvents:YES];
     [self setIgnoresMouseEvents:NO];
     [self setMovableByWindowBackground:YES];
-    [self setHidesOnDeactivate:YES];
 
     return self;
 }
