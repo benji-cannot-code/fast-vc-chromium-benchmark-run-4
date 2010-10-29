ff8eaf7b033fb693ba8691b281977ed9de2e0418
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-29 19:37:21 +0000

    Language tag for speech.
    This is the 3rd in a 4-sided patch to add language attribute support to speech input.
    
    The other patches can be found here:
      1st - http://codereview.chromium.org/3615005/show
      2nd - https://bugs.webkit.org/show_bug.cgi?id=47089
      4th - https://bugs.webkit.org/show_bug.cgi?id=47420
    
    This patch will be submitted once the webkit change has been rolled into chromium DEPS. Also, a last webkit cleanup patch will be submitted at any point after the 2nd patch.
    
    BUG=53598
    TEST=
    
    Review URL: http://codereview.chromium.org/3595018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_recognizer.cc
+++ chrome/browser/speech/speech_recognizer.cc
@@ -110,9 +110,11 @@ void SpeexEncoder::Encode(const short* samples,
 
 SpeechRecognizer::SpeechRecognizer(Delegate* delegate,
                                    int caller_id,
+                                   const std::string& language,
                                    const std::string& grammar)
     : delegate_(delegate),
       caller_id_(caller_id),
+      language_(language),
       grammar_(grammar),
       encoder_(new SpeexEncoder()),
       endpointer_(kAudioSampleRate),
@@ -211,10 +213,11 @@ void SpeechRecognizer::StopRecording() {
        it != audio_buffers_.end(); it++) {
     data.append(*(*it));
   }
+
   DCHECK(!request_.get());
   request_.reset(new SpeechRecognitionRequest(
       Profile::GetDefaultRequestContext(), this));
-  request_->Send(grammar_, kContentTypeSpeex, data);
+  request_->Send(language_, grammar_, kContentTypeSpeex, data);
   ReleaseAudioBuffers();  // No need to keep the audio anymore.
 }
 
