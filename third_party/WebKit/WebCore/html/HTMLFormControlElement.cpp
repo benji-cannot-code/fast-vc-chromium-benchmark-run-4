b0763dc6fc6070482ec776e78a2bb1120c48db19
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-29 23:15:57 +0000

    2010-10-29  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            Remove RenderTextControl::setSelectionRange
            https://bugs.webkit.org/show_bug.cgi?id=47870
    
            Converted RenderTextControll::setSelectionRange to a global function.
    
            * Api/qwebpage.cpp:
            (QWebPagePrivate::inputMethodEvent): Calls setSelectionRange.
    2010-10-29  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Darin Adler.
    
            Remove RenderTextControl::setSelectionRange
            https://bugs.webkit.org/show_bug.cgi?id=47870
    
            Made RenderTextControl::setSelectionRange a global function.
            Removed setSelectionStart, setSelectionEnd, and select from RenderTextControl.
    
            No new tests are added since there is no behavioral change.
    
            * accessibility/AccessibilityRenderObject.cpp:
            (WebCore::AccessibilityRenderObject::setSelectedTextRange): Calls setSelectedTextRange.
            * dom/InputElement.cpp:
            (WebCore::InputElement::updateSelectionRange): Ditto.
            * html/HTMLFormControlElement.cpp:
            (WebCore::HTMLTextFormControlElement::setSelectionStart): Ditto.
            (WebCore::HTMLTextFormControlElement::setSelectionEnd): Ditto.
            (WebCore::HTMLTextFormControlElement::select): Ditto.
            (WebCore::HTMLTextFormControlElement::setSelectionRange): Ditto.
            * rendering/RenderTextControl.cpp:
            (WebCore::RenderTextControl::hasVisibleTextArea): Added.
            (WebCore::setSelectionRange): See above.
            * rendering/RenderTextControl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLFormControlElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLFormControlElement.cpp
@@ -47,11 +47,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderTheme.h"
 #include "ScriptEventListener.h"
 #include "ValidityState.h"
+#include <limits>
 #include <wtf/Vector.h>
 
 namespace WebCore {
 
 using namespace HTMLNames;
+using namespace std;
 
 HTMLFormControlElement::HTMLFormControlElement(const QualifiedName& tagName, Document* document, HTMLFormElement* form)
     : HTMLElement(tagName, document)
@@ -549,26 +551,22 @@ RenderTextControl* HTMLTextFormControlElement::textRendererAfterUpdateLayout()
 
 void HTMLTextFormControlElement::setSelectionStart(int start)
 {
-    if (RenderTextControl* renderer = textRendererAfterUpdateLayout())
-        renderer->setSelectionStart(start);
+    setSelectionRange(start, max(start, selectionEnd()));
 }
 
 void HTMLTextFormControlElement::setSelectionEnd(int end)
 {
-    if (RenderTextControl* renderer = textRendererAfterUpdateLayout())
-        renderer->setSelectionEnd(end);
+    setSelectionRange(min(end, selectionStart()), end);
 }
 
 void HTMLTextFormControlElement::select()
 {
-    if (RenderTextControl* renderer = textRendererAfterUpdateLayout())
-        renderer->select();
+    setSelectionRange(0, numeric_limits<int>::max());
 }
 
 void HTMLTextFormControlElement::setSelectionRange(int start, int end)
 {
-    if (RenderTextControl* renderer = textRendererAfterUpdateLayout())
-        renderer->setSelectionRange(start, end);
+    WebCore::setSelectionRange(this, start, end);
 }
 
 int HTMLTextFormControlElement::selectionStart()
--- third_party/WebKit/WebCore/rendering/RenderTextControl.h
+++ third_party/WebKit/WebCore/rendering/RenderTextControl.h
@@ -43,10 +43,6 @@ public:
 
     int selectionStart();
     int selectionEnd();
-    void setSelectionStart(int);
-    void setSelectionEnd(int);
-    void select();
-    void setSelectionRange(int start, int end);
     PassRefPtr<Range> selection(int start, int end) const;
 
     virtual void subtreeHasChanged();
@@ -110,10 +106,15 @@ private:
 
     String finishText(Vector<UChar>&) const;
 
+    bool hasVisibleTextArea() const;
+    friend void setSelectionRange(Node*, int start, int end);
+
     bool m_wasChangedSinceLastChangeEvent;
     bool m_lastChangeWasUserEdit;
     RefPtr<TextControlInnerTextElement> m_innerText;
 };
+
+void setSelectionRange(Node*, int start, int end);
 
 inline RenderTextControl* toRenderTextControl(RenderObject* object)
 { 
