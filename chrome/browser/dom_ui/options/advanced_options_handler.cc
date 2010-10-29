cdbaf7e65cfe09dd49d3afc4fe8557e386ea23d0
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-29 09:07:58 +0000

    Don't show the stats/crash reporting setting in the guest mode.
    BUG=chromium-os:8239
    TEST=confirmed that the stats/crash reporting setting disappeared in the guest mode, and it worked as before in non guest mode.
    
    Review URL: http://codereview.chromium.org/4221001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64386 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options/advanced_options_handler.cc
+++ chrome/browser/dom_ui/options/advanced_options_handler.cc
@@ -189,6 +189,7 @@ void AdvancedOptionsHandler::GetLocalizedValues(
 void AdvancedOptionsHandler::Initialize() {
   DCHECK(dom_ui_);
   SetupMetricsReportingCheckbox(false);
+  SetupMetricsReportingSettingVisibility();
   SetupDefaultZoomLevel();
   SetupDownloadLocationPath();
   SetupAutoOpenFileTypesDisabledAttribute();
@@ -502,6 +503,18 @@ void AdvancedOptionsHandler::SetupMetricsReportingCheckbox(bool user_changed) {
   dom_ui_->CallJavascriptFunction(
       L"options.AdvancedOptions.SetMetricsReportingCheckboxState", checked,
       disabled, user_has_changed);
+#endif
+}
+
+void AdvancedOptionsHandler::SetupMetricsReportingSettingVisibility() {
+#if defined(GOOGLE_CHROME_BUILD) && defined(OS_CHROMEOS)
+  // Don't show the reporting setting if we are in the guest mode.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kGuestSession)) {
+    FundamentalValue visible(false);
+    dom_ui_->CallJavascriptFunction(
+        L"options.AdvancedOptions.SetMetricsReportingSettingVisibility",
+        visible);
+  }
 #endif
 }
 
--- chrome/browser/dom_ui/options/advanced_options_handler.h
+++ chrome/browser/dom_ui/options/advanced_options_handler.h
@@ -122,6 +122,9 @@ class AdvancedOptionsHandler
   // Setup the checked state for the metrics reporting checkbox.
   void SetupMetricsReportingCheckbox(bool user_changed);
 
+  // Setup the visibility for the metrics reporting setting.
+  void SetupMetricsReportingSettingVisibility();
+
   void SetupDefaultZoomLevel();
 
   // Setup the download path based on user preferences.
@@ -156,4 +159,3 @@ class AdvancedOptionsHandler
 };
 
 #endif  // CHROME_BROWSER_DOM_UI_OPTIONS_ADVANCED_OPTIONS_HANDLER_H_
-
