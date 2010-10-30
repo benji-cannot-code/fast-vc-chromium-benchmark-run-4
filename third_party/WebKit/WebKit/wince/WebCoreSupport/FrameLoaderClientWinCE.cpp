96065b69c2983d39125400d2f6d2d453c086e82e
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-30 19:31:52 +0000

    2010-10-30  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed, build fix after r70846.
    
            * platform/graphics/wince/ImageWinCE.cpp:
            (WebCore::RGBA32Buffer::asNewNativeImage):
    2010-10-30  Patrick Gansterer  <paroga@webkit.org>
    
            Unreviewed, build fix after r70574.
    
            * WebCoreSupport/FrameLoaderClientWinCE.cpp:
            (WebKit::FrameLoaderClientWinCE::transferLoadingResourceFromPage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/wince/WebCoreSupport/FrameLoaderClientWinCE.cpp
+++ third_party/WebKit/WebKit/wince/WebCoreSupport/FrameLoaderClientWinCE.cpp
@@ -166,7 +166,7 @@ void FrameLoaderClientWinCE::didTransferChildFrameToNewDocument(Page*)
 {
 }
 
-void FrameLoaderClientWinCE::transferLoadingResourceFromPage(unsigned long, DocumentLoader*, const ResourceRequest&, Page*)
+void FrameLoaderClientWinCE::transferLoadingResourceFromPage(unsigned long, DocumentLoader*, const WebCore::ResourceRequest&, Page*)
 {
 }
 
