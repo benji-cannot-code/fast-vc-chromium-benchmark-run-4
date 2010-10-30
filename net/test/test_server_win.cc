4728e49bad31f3f767e3a0e5f0acb05657b1b810
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-30 09:41:08 +0000

    GTTF: Implement a timeout for TestServer on Windows.
    This will prevent tests from hanging when the test server fails to start.
    
    BUG=60082
    TEST=none
    Review URL: http://codereview.chromium.org/4176001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64540 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/test_server_win.cc
+++ net/test/test_server_win.cc
@@ -11,9 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
+#include "base/test/test_timeouts.h"
+#include "base/thread.h"
 #include "base/utf_string_conversions.h"
 
 #pragma comment(lib, "crypt32.lib")
@@ -73,6 +76,17 @@ bool LaunchTestServerAsJob(const CommandLine& cmdline,
   return true;
 }
 
+void UnblockPipe(HANDLE handle, bool* unblocked) {
+  static const char kUnblock[] = "UNBLOCK";
+  // Unblock the ReadFile in TestServer::WaitToStart by writing to the pipe.
+  // Make sure the call succeeded, otherwise we are very likely to hang.
+  DWORD bytes_written = 0;
+  CHECK(WriteFile(handle, kUnblock, arraysize(kUnblock), &bytes_written,
+                  NULL));
+  CHECK_EQ(arraysize(kUnblock), bytes_written);
+  *unblocked = true;
+}
+
 }  // namespace
 
 namespace net {
@@ -97,8 +111,8 @@ bool TestServer::LaunchPython(const FilePath& testserver_path) {
     PLOG(ERROR) << "Failed to create pipe";
     return false;
   }
-  child_fd_.Set(child_read);
-  ScopedHandle scoped_child_write(child_write);
+  child_read_fd_.Set(child_read);
+  child_write_fd_.Set(child_write);
 
   // Have the child inherit the write half.
   if (!SetHandleInformation(child_write, HANDLE_FLAG_INHERIT,
@@ -133,10 +147,29 @@ bool TestServer::LaunchPython(const FilePath& testserver_path) {
 }
 
 bool TestServer::WaitToStart() {
+  base::Thread thread("test_server_watcher");
+  if (!thread.Start())
+    return false;
+
+  // Prepare a timeout in case the server fails to start.
+  bool unblocked = false;
+  thread.message_loop()->PostDelayedTask(FROM_HERE,
+      NewRunnableFunction(UnblockPipe, child_write_fd_.Get(), &unblocked),
+      TestTimeouts::action_max_timeout_ms());
+
   char buf[8];
   DWORD bytes_read;
-  BOOL result = ReadFile(child_fd_, buf, sizeof(buf), &bytes_read, NULL);
-  child_fd_.Close();
+  BOOL result = ReadFile(child_read_fd_.Get(), buf, sizeof(buf), &bytes_read,
+                         NULL);
+
+  thread.Stop();
+  child_read_fd_.Close();
+  child_write_fd_.Close();
+
+  // If we hit the timeout, fail.
+  if (unblocked)
+    return false;
+
   return result && bytes_read > 0;
 }
 
