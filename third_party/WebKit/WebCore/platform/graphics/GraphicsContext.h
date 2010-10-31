cbf7675f056ac89edace2d725ed5934a36e9a418
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-31 19:02:27 +0000

    2010-10-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Dirk Schulze.
    
            Remove the unused GraphicsContext::strokeRect(FloatRect)
            https://bugs.webkit.org/show_bug.cgi?id=48726
    
            This function was only implemented by GraphicsContextOpenVG and not
            called from anywhere.
    
            * platform/graphics/GraphicsContext.h:
            * platform/graphics/openvg/GraphicsContextOpenVG.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
+++ third_party/WebKit/WebCore/platform/graphics/GraphicsContext.h
@@ -226,7 +226,6 @@ namespace WebCore {
 
         void clearRect(const FloatRect&);
 
-        void strokeRect(const FloatRect&);
         void strokeRect(const FloatRect&, float lineWidth);
 
         void drawImage(Image*, ColorSpace styleColorSpace, const IntPoint&, CompositeOperator = CompositeSourceOver);
--- third_party/WebKit/WebCore/platform/graphics/openvg/GraphicsContextOpenVG.cpp
+++ third_party/WebKit/WebCore/platform/graphics/openvg/GraphicsContextOpenVG.cpp
@@ -341,14 +341,6 @@ void GraphicsContext::clearRect(const FloatRect& rect)
     m_data->setCompositeOperation(op);
 }
 
-void GraphicsContext::strokeRect(const FloatRect& rect)
-{
-    if (paintingDisabled())
-        return;
-
-    m_data->drawRect(rect, VG_STROKE_PATH);
-}
-
 void GraphicsContext::strokeRect(const FloatRect& rect, float lineWidth)
 {
     if (paintingDisabled())
