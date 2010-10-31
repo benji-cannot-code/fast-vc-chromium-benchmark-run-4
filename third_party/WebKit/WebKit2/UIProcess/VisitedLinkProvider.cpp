79e80f09e906f3aa916674cd43e48ba91ee26ab4
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-31 20:20:42 +0000

    2010-10-31  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            Visited links not populated correctly in new web processes after the first
            https://bugs.webkit.org/show_bug.cgi?id=48735
            rdar://problem/8442177
    
            * UIProcess/VisitedLinkProvider.cpp:
            (WebKit::VisitedLinkProvider::VisitedLinkProvider): Initialize
            m_webProcessHasVisitedLinkState.
            (WebKit::VisitedLinkProvider::processDidFinishLaunching): Renamed
            from populateVisitedLinksIfNeeded. Set m_webProcessHasVisitedLinkState
            to false, since this is a new process, and also start the timer
            so the visited links messages will be sent.
            (WebKit::VisitedLinkProvider::processDidClose): Renamed from
            stopVisitedLinksTimer.
            (WebKit::VisitedLinkProvider::pendingVisitedLinksTimerFired):
            Added logic so we send SetVisitedLinkTable and
            AllVisitedLinkStateChanged once to each new process and to set
            m_webProcessHasVisitedLinkState to true once that is done.
    
            * UIProcess/VisitedLinkProvider.h: Updated for above changes.
    
            * UIProcess/WebContext.cpp:
            (WebKit::WebContext::processDidFinishLaunching): Called
            VisitedLinkProvider function under its new name. Also used
            ASSERT_UNUSED rather than ASSERT since the argument is used
            only for the assertion.
            (WebKit::WebContext::processDidClose): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70997 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/VisitedLinkProvider.cpp
+++ third_party/WebKit/WebKit2/UIProcess/VisitedLinkProvider.cpp
@@ -40,14 +40,20 @@ static const int VisitedLinkTableMaxLoad = 2;
 VisitedLinkProvider::VisitedLinkProvider(WebContext* context)
     : m_context(context)
     , m_visitedLinksPopulated(false)
+    , m_webProcessHasVisitedLinkState(false)
     , m_keyCount(0)
     , m_tableSize(0)
     , m_pendingVisitedLinksTimer(RunLoop::main(), this, &VisitedLinkProvider::pendingVisitedLinksTimerFired)
 {
 }
 
-void VisitedLinkProvider::populateVisitedLinksIfNeeded()
+void VisitedLinkProvider::processDidFinishLaunching()
 {
+    m_webProcessHasVisitedLinkState = false;
+
+    if (m_keyCount)
+        m_pendingVisitedLinksTimer.startOneShot(0);
+
     if (m_visitedLinksPopulated)
         return;
 
@@ -64,7 +70,7 @@ void VisitedLinkProvider::addVisitedLink(LinkHash linkHash)
         m_pendingVisitedLinksTimer.startOneShot(0);
 }
 
-void VisitedLinkProvider::stopVisitedLinksTimer()
+void VisitedLinkProvider::processDidClose()
 {
     m_pendingVisitedLinksTimer.stop();
 }
@@ -150,7 +156,7 @@ void VisitedLinkProvider::pendingVisitedLinksTimerFired()
 
     m_keyCount += pendingVisitedLinks.size();
 
-    if (currentTableSize != newTableSize) {
+    if (!m_webProcessHasVisitedLinkState || currentTableSize != newTableSize) {
         // Send the new visited link table.
         
         SharedMemory::Handle handle;
@@ -161,13 +167,14 @@ void VisitedLinkProvider::pendingVisitedLinksTimerFired()
     }
     
     // We now need to let the web process know that we've added links.
-    if (addedVisitedLinks.size() <= 20) {
+    if (m_webProcessHasVisitedLinkState && addedVisitedLinks.size() <= 20) {
         m_context->process()->send(Messages::WebProcess::VisitedLinkStateChanged(addedVisitedLinks), 0);
         return;
     }
     
     // Just recalculate all the visited links.
     m_context->process()->send(Messages::WebProcess::AllVisitedLinkStateChanged(), 0);
+    m_webProcessHasVisitedLinkState = true;
 }
 
 } // namespace WebKit
