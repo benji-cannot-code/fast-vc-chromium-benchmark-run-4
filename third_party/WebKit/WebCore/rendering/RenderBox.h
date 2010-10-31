f875b36fc025758f5e8e14f30805db9a28c2b726
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-10-31 17:33:14 +0000

    2010-10-31  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r70959.
            http://trac.webkit.org/changeset/70959
            https://bugs.webkit.org/show_bug.cgi?id=48731
    
            It broke fast/events/spatial-navigation tests (Requested by
            Ossy on #webkit).
    
            * WebCore.xcodeproj/project.pbxproj:
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::adjustForFlippedBlocksWritingMode):
            * rendering/InlineFlowBox.h:
            (WebCore::InlineFlowBox::bottomLayoutOverflow):
            (WebCore::InlineFlowBox::rightLayoutOverflow):
            (WebCore::InlineFlowBox::bottomVisualOverflow):
            (WebCore::InlineFlowBox::rightVisualOverflow):
            * rendering/RenderBox.cpp:
            * rendering/RenderBox.h:
            * rendering/RenderInline.cpp:
            (WebCore::RenderInline::linesBoundingBox):
            (WebCore::RenderInline::linesVisibleOverflowBoundingBox):
            (WebCore::RenderInline::clippedOverflowRectForRepaint):
            * rendering/RenderText.cpp:
            (WebCore::RenderText::linesBoundingBox):
    2010-10-31  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r70959.
            http://trac.webkit.org/changeset/70959
            https://bugs.webkit.org/show_bug.cgi?id=48731
    
            It broke fast/events/spatial-navigation tests (Requested by
            Ossy on #webkit).
    
            * fast/repaint/inline-vertical-rl-overflow.html: Removed.
            * platform/mac/fast/blockflow/background-vertical-lr-expected.txt:
            * platform/mac/fast/blockflow/background-vertical-rl-expected.txt:
            * platform/mac/fast/blockflow/basic-vertical-line-expected.txt:
            * platform/mac/fast/blockflow/border-image-vertical-lr-expected.txt:
            * platform/mac/fast/blockflow/border-image-vertical-rl-expected.txt:
            * platform/mac/fast/blockflow/border-radius-clipping-vertical-lr-expected.txt:
            * platform/mac/fast/blockflow/border-vertical-lr-expected.checksum:
            * platform/mac/fast/blockflow/border-vertical-lr-expected.png:
            * platform/mac/fast/blockflow/border-vertical-lr-expected.txt:
            * platform/mac/fast/blockflow/box-shadow-vertical-lr-expected.txt:
            * platform/mac/fast/blockflow/box-shadow-vertical-rl-expected.txt:
            * platform/mac/fast/blockflow/english-lr-text-expected.txt:
            * platform/mac/fast/blockflow/english-rl-text-expected.txt:
            * platform/mac/fast/blockflow/japanese-lr-text-expected.txt:
            * platform/mac/fast/blockflow/japanese-rl-text-expected.txt:
            * platform/mac/fast/repaint/inline-vertical-rl-overflow-expected.checksum: Removed.
            * platform/mac/fast/repaint/inline-vertical-rl-overflow-expected.png: Removed.
            * platform/mac/fast/repaint/inline-vertical-rl-overflow-expected.txt: Removed.
            * platform/mac/fast/text/international/vertical-text-glyph-test-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@70991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/WebCore/rendering/RenderBox.h
@@ -380,7 +380,6 @@ public:
 
     enum FlippingAdjustment { ChildToParentFlippingAdjustment, ParentToChildFlippingAdjustment };
     void adjustForFlippedBlocksWritingMode(RenderBox* child, IntPoint&, FlippingAdjustment);
-    void adjustForFlippedBlocksWritingMode(IntRect&); // Unflips a rect in our coordinate space.
     int convertFromFlippedWritingMode(int position);
     IntSize locationOffsetIncludingFlipping();
 
