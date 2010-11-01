fc1dc5cc4764fb45bec91257bee89b1b29971287
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-01 21:48:09 +0000

    Marking InstallMiniInstallerSys as flaky.
    BUG=61497
    TEST=InstallMiniInstallerSys
    
    Review URL: http://codereview.chromium.org/4238001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/test.cc
+++ chrome/test/mini_installer_test/test.cc
@@ -172,7 +172,8 @@ TEST_F(MiniInstallTest,
 }
 #endif
 
-TEST_F(MiniInstallTest, InstallMiniInstallerSys) {
+// http://crbug.com/61497
+TEST_F(MiniInstallTest, FLAKY_InstallMiniInstallerSys) {
   sys_inst_->Install();
 }
 
