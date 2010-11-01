fd4f4e82366793382f4bd06450d142b8cc8f7d19
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-01 18:53:33 +0000

    Windows build fix.
    * UIProcess/win/WebView.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FindIndicator.h"
 #include "RunLoop.h"
 #include "NativeWebKeyboardEvent.h"
+#include "WebContextMenuProxy.h"
 #include "WebEditCommandProxy.h"
 #include "WebEventFactory.h"
 #include "WebPageNamespace.h"
