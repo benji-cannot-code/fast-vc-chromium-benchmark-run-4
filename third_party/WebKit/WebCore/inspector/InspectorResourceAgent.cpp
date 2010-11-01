29222ff4e211264ba325088dd8405376fcbab31a
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-01 18:14:28 +0000

    2010-11-01  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed: Chromium build fix. Adding missing transitive dependency.
    
            * inspector/InspectorResourceAgent.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/WebCore/inspector/InspectorResourceAgent.cpp
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebSocketHandshakeRequest.h"
 #include "WebSocketHandshakeResponse.h"
 
+#include <wtf/CurrentTime.h>
 #include <wtf/ListHashSet.h>
 #include <wtf/RefPtr.h>
 #include <wtf/text/StringBuffer.h>
