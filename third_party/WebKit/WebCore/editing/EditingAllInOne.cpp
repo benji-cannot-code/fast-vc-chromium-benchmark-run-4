7742fd6998c5cca8dbf6712b54ad5a80c43eb8ff
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-01 06:35:59 +0000

    2010-10-26  MORITA Hajime  <morrita@google.com>
            Reviewed by Kent Tamura.
    
            Refactoring: Spellchecking related static functions could form a class
            https://bugs.webkit.org/show_bug.cgi?id=48287
    
            Extracted spellcheck related static functions to TextCheckingHelper class,
            which has EditorClient and Range as its member.
    
            No new tests. Just a refactoring.
    
            * CMakeLists.txt:
            * GNUmakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * editing/EditingAllInOne.cpp
            * editing/Editor.cpp:
            (WebCore::Editor::advanceToNextMisspelling):
            (WebCore::Editor::isSelectionUngrammatical):
            (WebCore::Editor::guessesForUngrammaticalSelection):
            (WebCore::Editor::guessesForMisspelledOrUngrammaticalSelection):
            (WebCore::Editor::markMisspellingsAfterTypingToPosition):
            (WebCore::Editor::markMisspellingsOrBadGrammar):
            (WebCore::Editor::markMisspellings):
            (WebCore::Editor::markBadGrammar):
            (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges):
            (WebCore::Editor::changeBackToReplacedString):
            * editing/Editor.h:
            * editing/TextCheckingHelper.cpp: Added.
            (WebCore::TextCheckingHelper::TextCheckingHelper):
            (WebCore::TextCheckingHelper::~TextCheckingHelper):
            (WebCore::TextCheckingHelper::paragraphAlignedRange):
            (WebCore::TextCheckingHelper::findFirstMisspelling):
            (WebCore::TextCheckingHelper::findFirstMisspellingOrBadGrammar):
            (WebCore::TextCheckingHelper::findFirstGrammarDetail):
            (WebCore::TextCheckingHelper::findFirstBadGrammar):
            (WebCore::TextCheckingHelper::isUngrammatical):
            (WebCore::TextCheckingHelper::guessesForMisspelledOrUngrammaticalRange):
            (WebCore::TextCheckingHelper::markAllMisspellings):
            (WebCore::TextCheckingHelper::markAllBadGrammar):
            * editing/TextCheckingHelper.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/EditingAllInOne.cpp
+++ third_party/WebKit/WebCore/editing/EditingAllInOne.cpp
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <SplitElementCommand.cpp>
 #include <SplitTextNodeCommand.cpp>
 #include <SplitTextNodeContainingElementCommand.cpp>
+#include <TextCheckingHelper.cpp>
 #include <TextIterator.cpp>
 #include <TypingCommand.cpp>
 #include <UnlinkCommand.cpp>
