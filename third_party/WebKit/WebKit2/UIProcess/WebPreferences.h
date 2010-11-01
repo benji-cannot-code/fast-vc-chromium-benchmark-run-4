9ddfd14972b2350c2539ddfcd3c2dde4dc1be1df
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-01 20:37:20 +0000

    https://bugs.webkit.org/show_bug.cgi?id=48774 minimFontSize preference needs to be exposed in WebKit2
    Reviewed by Anders Carlsson.
    
    * UIProcess/WebPreferences.cpp:
    (WebKit::WebPreferences::setMinimumFontSize):
    New function, parallel to existing functions for other preferences.
    (WebKit::WebPreferences::minimumFontSize):
    Ditto.
    
    * UIProcess/WebPreferences.h:
    Declared new function.
    
    * UIProcess/API/C/WKPreferences.cpp:
    (WKPreferencesSetMinimumFontSize):
    New UI process wrapper for new function.
    (WKPreferencesGetMinimumFontSize):
    Ditto.
    
    * UIProcess/API/C/WKPreferences.h:
    Declared new functions.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebPreferences.h
+++ third_party/WebKit/WebKit2/UIProcess/WebPreferences.h
@@ -104,6 +104,9 @@ public:
 
     void setFantasyFontFamily(const String&);
     const String& fantasyFontFamily() const;
+    
+    void setMinimumFontSize(uint32_t);
+    uint32_t minimumFontSize() const;
 
     void setAcceleratedCompositingEnabled(bool);
     bool acceleratedCompositingEnabled() const;
