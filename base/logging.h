0f4cfbabf8c79cd2d2cc9909305b47a93caf32fb
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-01 20:50:55 +0000

    Wrote a custom pattern matcher for vmodule.
    The custom pattern matcher matches any slash (forward or backward) to any
    slash.  It also does not do escaping like MatchPattern().  Finally, '?'
    matches exactly one character, which matches google-glog behavior
    (MatchPattern() matches '?' to 0 or 1 character).
    
    BUG=61123
    TEST=New unittests
    
    Review URL: http://codereview.chromium.org/4205005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64661 0039d316-1c4b-4281-b951-d872f2087c98

--- base/logging.h
+++ base/logging.h
@@ -88,9 +88,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   b. VLOG(1) and lower messages to be printed from icon_loader.{h,cc}
 //   c. VLOG(3) and lower messages to be printed from files prefixed with
 //      "browser"
-//   c. VLOG(4) and lower messages to be printed from files under a
+//   d. VLOG(4) and lower messages to be printed from files under a
 //     "chromeos" directory.
-//   d. VLOG(0) and lower messages to be printed from elsewhere
+//   e. VLOG(0) and lower messages to be printed from elsewhere
 //
 // The wildcarding functionality shown by (c) supports both '*' (match
 // 0 or more characters) and '?' (match any single character)
