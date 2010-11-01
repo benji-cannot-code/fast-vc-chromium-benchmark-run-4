7c0a97324d02f43207a7ac90f102852ff667258b
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-01 21:21:15 +0000

    [mac] Add stub implementation of platform_util::GetParent.
    BUG=none
    TEST=compile
    TBR=joshia
    
    Review URL: http://codereview.chromium.org/4233001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/platform_util_mac.mm
+++ chrome/browser/platform_util_mac.mm
@@ -133,6 +133,10 @@ gfx::NativeWindow GetTopLevel(gfx::NativeView view) {
   return [view window];
 }
 
+gfx::NativeView GetParent(gfx::NativeView view) {
+  return nil;
+}
+
 bool IsWindowActive(gfx::NativeWindow window) {
   return [window isKeyWindow] || [window isMainWindow];
 }
