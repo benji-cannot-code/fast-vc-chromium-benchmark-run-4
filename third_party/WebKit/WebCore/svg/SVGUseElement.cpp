209fca2020743788239439a31ef803d2a3d535fd
jschuh@chromium.org <jschuh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-01 15:23:32 +0000

    2010-11-01  Justin Schuh  <jschuh@chromium.org>
            Reviewed by Dirk Schulze.
    
            Check for NULL node in SVGUseElement::associateInstancesWithShadowTreeElements loop
            https://bugs.webkit.org/show_bug.cgi?id=48741
    
            * svg/custom/use-on-use-with-child-and-empty-target-expected.txt: Added.
            * svg/custom/use-on-use-with-child-and-empty-target.svg: Added.
    2010-11-01  Justin Schuh  <jschuh@chromium.org>
    
            Reviewed by Dirk Schulze.
    
            Check for NULL node in SVGUseElement::associateInstancesWithShadowTreeElements loop
            https://bugs.webkit.org/show_bug.cgi?id=48741
    
            Test: svg/custom/use-on-use-with-child-and-empty-target.svg
    
            * svg/SVGUseElement.cpp:
            (WebCore::SVGUseElement::associateInstancesWithShadowTreeElements):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGUseElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGUseElement.cpp
@@ -949,6 +949,9 @@ void SVGUseElement::associateInstancesWithShadowTreeElements(Node* target, SVGEl
         while (node && !node->isSVGElement())
            node = node->nextSibling();
 
+        if (!node)
+            break;
+
         associateInstancesWithShadowTreeElements(node, instance);
         node = node->nextSibling();
     }
