ed4519e3ca676a4f5de5582f39158fe1636ca30f
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-02 14:32:44 +0000

    2010-11-02  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: make xhr breakpoints editable
            https://bugs.webkit.org/show_bug.cgi?id=48830
    
            * inspector/front-end/BreakpointManager.js:
            (WebInspector.Breakpoint.prototype.set sourceText):
            (WebInspector.Breakpoint.prototype.click):
            (WebInspector.Breakpoint.prototype.populateLabelElement):
            (WebInspector.XHRBreakpoint.prototype.populateEditElement):
            * inspector/front-end/BreakpointsSidebarPane.js:
            (WebInspector.BreakpointsSidebarPane.prototype.addBreakpointItem):
            (WebInspector.BreakpointsSidebarPane.prototype._breakpointItemClicked):
            (WebInspector.BreakpointsSidebarPane.prototype._contextMenuEventFired):
            (WebInspector.BreakpointsSidebarPane.prototype._addListElement):
            (WebInspector.BreakpointsSidebarPane.prototype._removeListElement):
            (WebInspector.XHRBreakpointsSidebarPane.addButtonClicked):
            (WebInspector.XHRBreakpointsSidebarPane):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._startEditingBreakpoint):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._hideEditBreakpointDialog):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._breakpointItemClicked):
            (WebInspector.BreakpointItem):
            (WebInspector.BreakpointItem.prototype.get element):
            (WebInspector.BreakpointItem.prototype.click):
            (WebInspector.BreakpointItem.prototype.populateEditElement):
            (WebInspector.BreakpointItem.prototype.remove):
            (WebInspector.BreakpointItem.prototype._hitStateChanged):
            (WebInspector.BreakpointItem.prototype._labelChanged):
            (WebInspector.BreakpointItem.prototype._createLabelElement):
            * inspector/front-end/inspector.js:
            (WebInspector.createJSBreakpointsSidebarPane.breakpointAdded):
            (WebInspector.createJSBreakpointsSidebarPane):
            (WebInspector.createDOMBreakpointsSidebarPane.breakpointAdded):
            (WebInspector.createDOMBreakpointsSidebarPane):
            (WebInspector.createXHRBreakpointsSidebarPane.breakpointAdded):
            (WebInspector.createXHRBreakpointsSidebarPane):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/WebCore/inspector/front-end/inspector.js
@@ -191,7 +191,7 @@ var WebInspector = {
         var pane = new WebInspector.BreakpointsSidebarPane(WebInspector.UIString("Breakpoints"));
         function breakpointAdded(event)
         {
-            pane.addBreakpoint(new WebInspector.JSBreakpointItem(event.data));
+            pane.addBreakpointItem(new WebInspector.BreakpointItem(event.data));
         }
         WebInspector.breakpointManager.addEventListener("breakpoint-added", breakpointAdded);
         return pane;
@@ -202,7 +202,7 @@ var WebInspector = {
         var pane = new WebInspector.BreakpointsSidebarPane(WebInspector.UIString("DOM Breakpoints"));
         function breakpointAdded(event)
         {
-            pane.addBreakpoint(new WebInspector.BreakpointItem(event.data));
+            pane.addBreakpointItem(new WebInspector.BreakpointItem(event.data));
         }
         WebInspector.breakpointManager.addEventListener("dom-breakpoint-added", breakpointAdded);
         return pane;
@@ -213,7 +213,7 @@ var WebInspector = {
         var pane = new WebInspector.XHRBreakpointsSidebarPane();
         function breakpointAdded(event)
         {
-            pane.addBreakpoint(new WebInspector.BreakpointItem(event.data));
+            pane.addBreakpointItem(new WebInspector.BreakpointItem(event.data));
         }
         WebInspector.breakpointManager.addEventListener("xhr-breakpoint-added", breakpointAdded);
         return pane;
