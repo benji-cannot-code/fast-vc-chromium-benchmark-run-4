e8ca3272357917eee27d67a202b65aff813b19bc
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 04:10:57 +0000

    Textfield draws the right part of its parent's background as its background. Set the right background for existing user controls view.
    BUG=chromium-os:8101
    TEST=Check username and password text fields on existing or new user pods for light gray borders around them. Borders should not be visible.
    
    Review URL: http://codereview.chromium.org/4127009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64715 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_view.cc
+++ chrome/browser/chromeos/login/existing_user_view.cc
@@ -11,10 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/textfield_with_margin.h"
 #include "chrome/browser/chromeos/login/wizard_accessibility_helper.h"
 #include "grit/generated_resources.h"
+#include "views/background.h"
 #include "views/focus/focus_manager.h"
 #include "views/fill_layout.h"
 
 namespace chromeos {
+
+// Colors for gradient background. These should be consistent with border
+// window background so textfield border is not visible to the user.
+// The background is needed for password textfield to imitate its borders
+// transparency correctly.
+const SkColor kBackgroundColorTop = SkColorSetRGB(209, 213, 216);
+const SkColor kBackgroundColorBottom = SkColorSetRGB(205, 210, 213);
 
 // Textfield with custom processing for Tab/Shift+Tab to select entries.
 class UserEntryTextfield : public TextfieldWithMargin {
@@ -65,6 +73,9 @@ void ExistingUserView::RecreateFields() {
     SetLayoutManager(new views::FillLayout);
     password_field_ = new UserEntryTextfield(user_controller_,
                                              views::Textfield::STYLE_PASSWORD);
+    password_field_->set_background(
+        views::Background::CreateVerticalGradientBackground(
+            kBackgroundColorTop, kBackgroundColorBottom));
     CorrectTextfieldFontSize(password_field_);
     password_field_->SetFocusable(true);
     password_field_->SetController(user_controller_);
