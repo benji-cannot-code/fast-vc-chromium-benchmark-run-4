eaaf370fb0d418f8b2b10a7f7ea6df7984a4548e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-02 10:11:36 +0000

    2010-11-02  Ryuan Choi  <ryuan.choi@samsung.com>
            Unreviewed build fix for EFL build with libcurl
    
            [EFL] remove multiple definition with libcurl
            https://bugs.webkit.org/show_bug.cgi?id=48499
    
            Add macro to isolate setCookieStoragePrivateBrowsingEnabled from EFL
            build because it was duplicated.
    
            * platform/network/curl/CookieJarCurl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/network/curl/CookieJarCurl.cpp
+++ third_party/WebKit/WebCore/platform/network/curl/CookieJarCurl.cpp
@@ -62,9 +62,11 @@ void deleteCookie(const Document*, const KURL&, const String&)
     // FIXME: Not yet implemented
 }
 
+#if !PLATFORM(EFL)
 void setCookieStoragePrivateBrowsingEnabled(bool enabled)
 {
     // FIXME: Not yet implemented
 }
+#endif
 
 }
