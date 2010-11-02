0468c508b800fd3f7a708c9d3538057f937beae1
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 16:25:41 +0000

    ExtensionApiTest.Tabs is flaky on linux views
    TBR=sky
    BUG=61592
    TEST=
    
    Review URL: http://codereview.chromium.org/4316001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -17,7 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAYBE_TabOnRemoved TabOnRemoved
 #endif
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
+// Flaky on linux views. http://crbug.com/61592
+#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+#define MAYBE_Tabs FLAKY_Tabs
+#else
+#define MAYBE_Tabs Tabs
+#endif
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Tabs) {
   ASSERT_TRUE(test_server()->Start());
 
   // The test creates a tab and checks that the URL of the new tab
