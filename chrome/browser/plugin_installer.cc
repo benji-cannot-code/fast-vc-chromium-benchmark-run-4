7e2168eaa9abb62a37060dcf5c821ff0aa5b2c1a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 21:45:24 +0000

    Move URLs out of *.pak files and put them into code.
    BUG=28174
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/4130012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64820 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_installer.cc
+++ chrome/browser/plugin_installer.cc
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 #include "webkit/glue/plugins/default_plugin_shared.h"
 
+// The URL for the "Problems installing" page for the Plugins infobar.
+static const char kLearnMorePluginInstallerUrl[] =
+    "http://www.google.com/support/chrome/bin/answer.py?answer=95697&amp;topic=14687";
+
 PluginInstaller::PluginInstaller(TabContents* tab_contents)
     : ConfirmInfoBarDelegate(tab_contents),
       tab_contents_(tab_contents) {
@@ -74,8 +78,7 @@ string16 PluginInstaller::GetLinkText() {
 
 bool PluginInstaller::LinkClicked(WindowOpenDisposition disposition) {
   // Ignore the click dispostion and always open in a new top level tab.
-  tab_contents_->OpenURL(
-      GURL(l10n_util::GetStringUTF8(IDS_LEARN_MORE_PLUGININSTALLER_URL)),
-      GURL(), NEW_FOREGROUND_TAB, PageTransition::LINK);
+  tab_contents_->OpenURL(GURL(kLearnMorePluginInstallerUrl), GURL(),
+                         NEW_FOREGROUND_TAB, PageTransition::LINK);
   return false;  // Do not dismiss the info bar.
 }
