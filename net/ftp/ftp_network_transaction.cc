ef6fb95efff052d3509287e96e6ac83bf6173dd0
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 14:37:14 +0000

    Adds a new error (ERR_NETWORK_ACCESS_DENIED) for when network operations return an access is denied error.
    This is in preparation for adding new error text suggesting
    a firewall may be causing the problem, which would be a little
    embarrassing to do when an SSPI logon fails, or when trying to
    open a local file one doesn't have permissions to.
    
    BUG=57108
    TEST=Install firewall, block Chrome, verify that you get
         ERR_NETWORK_ACCESS_DENIED.  Unblock it, fail an SSPI logon
         (Or any other event that should generate an ERR_ACCESS_DENIED)
         and verify that you still get an ERR_ACCESS_DENIED.
    
    Review URL: http://codereview.chromium.org/3976004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64744 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_network_transaction.cc
+++ net/ftp/ftp_network_transaction.cc
@@ -1305,6 +1305,7 @@ void FtpNetworkTransaction::RecordDataConnectionError(int result) {
       type = NET_ERROR_OK;
       break;
     case ERR_ACCESS_DENIED:
+    case ERR_NETWORK_ACCESS_DENIED:
       type = NET_ERROR_ACCESS_DENIED;
       break;
     case ERR_TIMED_OUT:
