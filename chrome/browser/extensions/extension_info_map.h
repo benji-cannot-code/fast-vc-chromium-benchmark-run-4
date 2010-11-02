597d1b6b3135658e3f4785829bb6fc5011bb78f9
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 00:18:03 +0000

    Revert r64676 "Part 4/4 of immutable Extension refactor: Kill Extension::StaticData and put"
    Original CL:
    Part 4/4 of immutable Extension refactor: Kill Extension::StaticData and put
    its guts into Extension.
    
    BUG=56558
    TEST=no functional change
    
    Review URL: http://codereview.chromium.org/4200005
    
    TBR=nirnimesh
    
    Review URL: http://codereview.chromium.org/4274001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_info_map.h
+++ chrome/browser/extensions/extension_info_map.h
@@ -13,10 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_path.h"
 #include "base/ref_counted.h"
-#include "chrome/common/extensions/extension_extent.h"
+#include "chrome/common/extensions/extension.h"
 #include "googleurl/src/gurl.h"
-
-class Extension;
 
 // Contains extension data that needs to be accessed on the IO thread. It can
 // be created/destroyed on any thread, but all other methods must be called on
@@ -30,7 +28,7 @@ class ExtensionInfoMap : public base::RefCountedThreadSafe<ExtensionInfoMap> {
   ~ExtensionInfoMap();
 
   // Callback for when new extensions are loaded.
-  void AddExtension(const Extension* extension);
+  void AddExtension(const Extension::StaticData* data);
 
   // Callback for when an extension is unloaded.
   void RemoveExtension(const std::string& id);
@@ -66,7 +64,8 @@ class ExtensionInfoMap : public base::RefCountedThreadSafe<ExtensionInfoMap> {
 
  private:
   // Map of extension info by extension id.
-  typedef std::map<std::string, scoped_refptr<const Extension> > Map;
+  typedef std::map<std::string, scoped_refptr<const Extension::StaticData> >
+      Map;
 
   Map extension_info_;
 };
