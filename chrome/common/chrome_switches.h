80bc36a001956ce72804ff7b2709e1f5d8275547
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 23:01:10 +0000

    Disable DOMUI menu by default.  a part of reverting 6497 feature.
    BUG=chromium-os:6497,chromium-os:8574
    TEST=all tests should still pass.
    
    Review URL: http://codereview.chromium.org/4273003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64841 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -334,7 +334,7 @@ extern const char kStubCros[];
 extern const char kScreenSaverUrl[];
 extern const char kCompressSystemFeedback[];
 extern const char kForceStubLibcros[];
-extern const char kDisableDOMUIMenu[];
+extern const char kEnableDOMUIMenu[];
 #endif
 
 #if defined(OS_LINUX)
