5d80398c945c23dc9346c84c1dc11e45e665d12c
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-02 20:34:28 +0000

    Fixed activation process.
    BUG=chromium-os:8400
    TEST=make sure cellular device can be activated from UI
    
    Review URL: http://codereview.chromium.org/4311002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/mobile_setup.js
+++ chrome/browser/resources/mobile_setup.js
@@ -14,7 +14,7 @@ cr.define('mobile', function() {
   MobileSetup.PLAN_ACTIVATION_LOADING = -1;
   MobileSetup.PLAN_ACTIVATION_START = 0;
   MobileSetup.PLAN_ACTIVATION_INITIATING_ACTIVATION = 1;
-  MobileSetup.PLAN_ACTIVATION_ACTIVATING = 2;
+  MobileSetup.PLAN_ACTIVATION_RECONNECTING = 2;
   MobileSetup.PLAN_ACTIVATION_SHOWING_PAYMENT = 3;
   MobileSetup.PLAN_ACTIVATION_DONE = 4;
   MobileSetup.PLAN_ACTIVATION_ERROR = 5;
@@ -41,6 +41,7 @@ cr.define('mobile', function() {
           self.onMessageReceived_(e);
       });
       $('cheat').addEventListener('click', function(e) {
+        $('paymentForm').classList.add('hidden');
         chrome.send('setTransactionStatus', ['OK']);
       });
       $(frame_name).addEventListener('load', function(e) {
@@ -76,6 +77,7 @@ cr.define('mobile', function() {
       if (e.data.type == 'requestDeviceInfoMsg') {
         this.sendDeviceInfo_();
       } else if (e.data.type == 'reportTransactionStatusMsg') {
+        $('paymentForm').classList.add('hidden');
         chrome.send('setTransactionStatus', [e.data.status]);
       }
     },
@@ -98,7 +100,7 @@ cr.define('mobile', function() {
           $('systemStatus').classList.remove('hidden');
           break;
         case MobileSetup.PLAN_ACTIVATION_INITIATING_ACTIVATION:
-        case MobileSetup.PLAN_ACTIVATION_ACTIVATING:
+        case MobileSetup.PLAN_ACTIVATION_RECONNECTING:
           $('statusHeader').textContent =
               MobileSetup.localStrings_.getString('activating_header');
           $('errorMessage').textContent = '';
@@ -112,7 +114,7 @@ cr.define('mobile', function() {
           break;
         case MobileSetup.PLAN_ACTIVATION_DONE:
           $('statusHeader').textContent = '';
-          $('paymentForm').classList.add('hidden');
+          $('paymentForm').classList.remove('hidden');
           $('finalMessage').classList.remove('hidden');
           $('errorMessage').classList.add('hidden');
           $('systemStatus').classList.add('hidden');
