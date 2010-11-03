74e16ee7b59e0160b611e60e6588c89fe17622e0
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-03 21:45:45 +0000

    Fix 2 accessibility events that were not being sent with the appropriate accessibility object. BUG=none TEST=none Review URL: http://codereview.chromium.org/4337001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/browser_accessibility_manager.cc
+++ chrome/browser/accessibility/browser_accessibility_manager.cc
@@ -177,7 +177,7 @@ void BrowserAccessibilityManager::OnAccessibilityObjectValueChange(
   NotifyAccessibilityEvent(
       ViewHostMsg_AccessibilityNotification_Params::
           NOTIFICATION_TYPE_VALUE_CHANGED,
-      root_);
+      new_browser_acc);
 }
 
 void BrowserAccessibilityManager::OnAccessibilityObjectTextChange(
@@ -189,7 +189,7 @@ void BrowserAccessibilityManager::OnAccessibilityObjectTextChange(
   NotifyAccessibilityEvent(
       ViewHostMsg_AccessibilityNotification_Params::
           NOTIFICATION_TYPE_SELECTED_TEXT_CHANGED,
-      root_);
+      new_browser_acc);
 }
 
 void BrowserAccessibilityManager::GotFocus() {
