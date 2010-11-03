c6f88d78ef66791be1cc7fe101f81f8cec8f23cb
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-03 23:03:53 +0000

    2010-11-03  Dimitri Glazkov  <dglazkov@chromium.org>
            Reverting r71244. Broke default event handling inside text fields.
            https://bugs.webkit.org/show_bug.cgi?id=46015
    
            * fast/events/shadow-boundary-crossing.html: Reverted changes.
            * media/audio-delete-while-slider-thumb-clicked.html: Reverted changes.
    2010-11-03  Dimitri Glazkov  <dglazkov@chromium.org>
    
            Reverting r71244, r71248 and r71250. Broke default event handling inside text fields.
            https://bugs.webkit.org/show_bug.cgi?id=46015
    
            * Android.mk:
            * CMakeLists.txt:
            * GNUmakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * dom/ContainerNode.cpp:
            (WebCore::notifyChildInserted):
            * dom/DOMAllInOne.cpp:
            * dom/EventContext.cpp: Removed.
            * dom/EventContext.h: Removed.
            * dom/Node.cpp:
            (WebCore::Node::markAncestorsWithChildNeedsStyleRecalc):
            (WebCore::Node::createRendererIfNeeded):
            (WebCore::Node::eventParentNode):
            (WebCore::Node::enclosingLinkEventParentOrSelf):
            (WebCore::eventTargetAsSVGElementInstance):
            (WebCore::eventTargetRespectingSVGTargetRules):
            (WebCore::Node::eventAncestors):
            (WebCore::Node::dispatchGenericEvent):
            * dom/Node.h:
            * dom/Text.cpp:
            (WebCore::Text::createRenderer):
            * dom/WindowEventContext.cpp: Removed.
            * dom/WindowEventContext.h: Removed.
            * inspector/InspectorDOMAgent.cpp:
            (WebCore::InspectorDOMAgent::getEventListenersForNode):
            * inspector/InspectorInstrumentation.cpp:
            (WebCore::eventHasListeners):
            (WebCore::InspectorInstrumentation::willDispatchEventImpl):
            * inspector/InspectorInstrumentation.h:
            (WebCore::InspectorInstrumentation::willDispatchEvent):
            * page/EventHandler.cpp:
            (WebCore::EventHandler::updateMouseEventTargetNode):
            * rendering/ShadowElement.h:
            (WebCore::ShadowElement::shadowParent):
            (WebCore::ShadowElement::shadowParentNode):
            * svg/SVGElement.cpp:
            (WebCore::SVGElement::eventParentNode):
            * svg/SVGElement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/InspectorInstrumentation.h
+++ third_party/WebKit/WebCore/inspector/InspectorInstrumentation.h
@@ -41,7 +41,6 @@ namespace WebCore {
 
 class CharacterData;
 class Element;
-class EventContext;
 class InspectorController;
 class InspectorTimelineAgent;
 class KURL;
@@ -75,7 +74,7 @@ public:
     static void didCallFunction(const InspectorInstrumentationCookie&);
     static InspectorInstrumentationCookie willChangeXHRReadyState(ScriptExecutionContext*, XMLHttpRequest* request);
     static void didChangeXHRReadyState(const InspectorInstrumentationCookie&);
-    static InspectorInstrumentationCookie willDispatchEvent(Document*, const Event& event, DOMWindow* window, Node* node, const Vector<EventContext>& ancestors);
+    static InspectorInstrumentationCookie willDispatchEvent(Document*, const Event& event, DOMWindow* window, Node* node, const Vector<RefPtr<ContainerNode> >& ancestors);
     static void didDispatchEvent(const InspectorInstrumentationCookie&);
     static InspectorInstrumentationCookie willDispatchEventOnWindow(Frame*, const Event& event, DOMWindow* window);
     static void didDispatchEventOnWindow(const InspectorInstrumentationCookie&);
@@ -132,7 +131,7 @@ private:
     static void didCallFunctionImpl(const InspectorInstrumentationCookie&);
     static InspectorInstrumentationCookie willChangeXHRReadyStateImpl(InspectorController*, XMLHttpRequest* request);
     static void didChangeXHRReadyStateImpl(const InspectorInstrumentationCookie&);
-    static InspectorInstrumentationCookie willDispatchEventImpl(InspectorController*, const Event& event, DOMWindow* window, Node* node, const Vector<EventContext>& ancestors);
+    static InspectorInstrumentationCookie willDispatchEventImpl(InspectorController*, const Event& event, DOMWindow* window, Node* node, const Vector<RefPtr<ContainerNode> >& ancestors);
     static void didDispatchEventImpl(const InspectorInstrumentationCookie&);
     static InspectorInstrumentationCookie willDispatchEventOnWindowImpl(InspectorController*, const Event& event, DOMWindow* window);
     static void didDispatchEventOnWindowImpl(const InspectorInstrumentationCookie&);
@@ -227,6 +226,7 @@ inline void InspectorInstrumentation::characterDataModified(Document* document, 
 #endif
 }
 
+
 inline void InspectorInstrumentation::willSendXMLHttpRequest(ScriptExecutionContext* context, const String& url)
 {
 #if ENABLE(INSPECTOR)
@@ -294,7 +294,7 @@ inline void InspectorInstrumentation::didChangeXHRReadyState(const InspectorInst
 #endif
 }
 
-inline InspectorInstrumentationCookie InspectorInstrumentation::willDispatchEvent(Document* document, const Event& event, DOMWindow* window, Node* node, const Vector<EventContext>& ancestors)
+inline InspectorInstrumentationCookie InspectorInstrumentation::willDispatchEvent(Document* document, const Event& event, DOMWindow* window, Node* node, const Vector<RefPtr<ContainerNode> >& ancestors)
 {
 #if ENABLE(INSPECTOR)
     if (InspectorController* inspectorController = inspectorControllerForDocument(document))
