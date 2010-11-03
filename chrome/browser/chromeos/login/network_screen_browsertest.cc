7516da599d680768098b309c207ac6fadc945902
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-03 06:33:45 +0000

    Push for ers:
    Added handling of network ("service" in flimflam parlance) property changes. 
    NetworkLibrary updates its Network objects in response to property changes, 
    then calls any NetworkObservers that have registered for the given network. 
    
    Use the is_active member of Network in various places to ensure that we are 
    dealing with the correct network. 
    
    Created separate observer classes for NetworkManager, Network, and 
    CellularDataPlan, so that monitors can be established independently for each 
    of them. Changed the callback method names to prepend "On", i.e., 
    OnNetworkManagerChanged, OnNetworkChanged, OnCellularDataPlanChanged. 
    
    Wired up signal strength update and network technology update in the status 
    bar, network dropdown menu, and internet settings page. 
    
    Added new icon badges for 1xRTT radio technology. These are used when EVDO 
    coverage switches to 1xRTT. Just as is the case for the 3G icons, there are 
    three 1x icons: normal, warn, and error. Please consider these icons to be 
    placeholders if the UI folks think they need to be redesigned. 
    
    Centralized choosing of the network technology badge in 
    NetworkMenu::BadgeForNetworkTechnology. 
    
    BUG=chromium-os:5612:chromium-os:7619,chromium-os:5917,chromium-os:4674 
    TEST=Extensive manual testing with different combinations of wi-fi and 
    cellular networks connected and disconnected. Used a modified version 
    of flimflam that randomly changes reported signal strength, network 
    technology, and roaming state, and verified that the UI correctly reflected 
    current values for the active network (roaming is not handled in the UI 
    at this time).
    Review URL: http://codereview.chromium.org/4235005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/network_screen_browsertest.cc
+++ chrome/browser/chromeos/login/network_screen_browsertest.cc
@@ -89,10 +89,13 @@ class NetworkScreenTest : public WizardInProcessBrowserTest {
     EXPECT_CALL(*mock_network_library_, Connecting())
         .Times(2)
         .WillRepeatedly(Return(false));
-    // Add an AddObserver for prewarming auth url check.
     EXPECT_CALL(*mock_network_library_, AddObserver(_))
         .Times(3);
     EXPECT_CALL(*mock_network_library_, RemoveObserver(_))
+    // Add an Observer for prewarming auth url check.
+    EXPECT_CALL(*mock_network_library_, AddNetworkManagerObserver(_))
+        .Times(3);
+    EXPECT_CALL(*mock_network_library_, RemoveNetworkManagerObserver(_))
         .Times(2);
 
     cros_mock_->SetStatusAreaMocksExpectations();
@@ -148,7 +151,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Ethernet) {
   EXPECT_CALL(*mock_network_library_, ethernet_connecting())
       .WillOnce((Return(true)));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_FALSE(network_view->IsContinueEnabled());
   EXPECT_FALSE(network_view->IsConnecting());
 
@@ -157,7 +160,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Ethernet) {
   EXPECT_CALL(*mock_network_library_, Connected())
       .WillOnce(Return(true));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_TRUE(network_view->IsContinueEnabled());
 
   EmulateContinueButtonExit(network_screen);
@@ -187,7 +190,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Wifi) {
   EXPECT_CALL(*mock_network_library_, wifi_network())
       .WillOnce(Return(wifi.get()));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_FALSE(network_view->IsContinueEnabled());
   EXPECT_FALSE(network_view->IsConnecting());
 
@@ -196,7 +199,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Wifi) {
   EXPECT_CALL(*mock_network_library_, Connected())
       .WillOnce(Return(true));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_TRUE(network_view->IsContinueEnabled());
 
   EmulateContinueButtonExit(network_screen);
@@ -228,7 +231,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Cellular) {
   EXPECT_CALL(*mock_network_library_, cellular_network())
       .WillOnce(Return(cellular.get()));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_FALSE(network_view->IsContinueEnabled());
   EXPECT_FALSE(network_view->IsConnecting());
 
@@ -237,7 +240,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Cellular) {
   EXPECT_CALL(*mock_network_library_, Connected())
       .WillOnce(Return(true));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_TRUE(network_view->IsContinueEnabled());
 
   EmulateContinueButtonExit(network_screen);
@@ -269,7 +272,7 @@ IN_PROC_BROWSER_TEST_F(NetworkScreenTest, Timeout) {
   EXPECT_CALL(*mock_network_library_, Connected())
       .WillOnce(Return(false));
 
-  network_screen->NetworkChanged(mock_network_library_);
+  network_screen->OnNetworkManagerChanged(mock_network_library_);
   EXPECT_FALSE(network_view->IsContinueEnabled());
   EXPECT_FALSE(network_view->IsConnecting());
 
