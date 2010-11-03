5cb2ae8c970d206522629b132ad6c197a3581ecc
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-03 08:36:22 +0000

    SVG FELighting performance issues https://bugs.webkit.org/show_bug.cgi?id=48212
    Reviewed by Dirk Schulze.
    
    This patch speeds-up FELighting filter painting by 40%
    It reduces the number of floating point operations and
    empolys faster pixel manipulation (both for read and
    write). Furthermore the length() member of FloatPoint3D
    is made inline to speed up vector length calculations.
    
    The lighting filter pixels tests are cover this patch.
    
    * platform/graphics/FloatPoint3D.cpp:
    * platform/graphics/FloatPoint3D.h:
    (WebCore::FloatPoint3D::length):
    * platform/graphics/filters/FELighting.cpp:
    (WebCore::FELighting::LightingData::topLeft):
    (WebCore::FELighting::LightingData::topRow):
    (WebCore::FELighting::LightingData::topRight):
    (WebCore::FELighting::LightingData::leftColumn):
    (WebCore::FELighting::LightingData::interior):
    (WebCore::FELighting::LightingData::rightColumn):
    (WebCore::FELighting::LightingData::bottomLeft):
    (WebCore::FELighting::LightingData::bottomRow):
    (WebCore::FELighting::LightingData::bottomRight):
    (WebCore::FELighting::inlineSetPixel):
    (WebCore::FELighting::setPixel):
    (WebCore::FELighting::drawLighting):
    (WebCore::FELighting::apply):
    * platform/graphics/filters/FELighting.h:
    * platform/graphics/filters/LightSource.cpp:
    (WebCore::PointLightSource::updatePaintingData):
    (WebCore::SpotLightSource::updatePaintingData):
    (WebCore::DistantLightSource::initPaintingData):
    * platform/graphics/filters/LightSource.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/filters/LightSource.cpp
+++ third_party/WebKit/WebCore/platform/graphics/filters/LightSource.cpp
@@ -43,7 +43,7 @@ void PointLightSource::updatePaintingData(PaintingData& paintingData, int x, int
     paintingData.lightVector.setX(m_position.x() - x);
     paintingData.lightVector.setY(m_position.y() - y);
     paintingData.lightVector.setZ(m_position.z() - z);
-    paintingData.lightVector.normalize();
+    paintingData.lightVectorLength = paintingData.lightVector.length();
 }
 
 // spot-light edge darkening depends on an absolute treshold
@@ -85,9 +85,9 @@ void SpotLightSource::updatePaintingData(PaintingData& paintingData, int x, int 
     paintingData.lightVector.setX(m_position.x() - x);
     paintingData.lightVector.setY(m_position.y() - y);
     paintingData.lightVector.setZ(m_position.z() - z);
-    paintingData.lightVector.normalize();
+    paintingData.lightVectorLength = paintingData.lightVector.length();
 
-    float cosineOfAngle = paintingData.lightVector * paintingData.directionVector;
+    float cosineOfAngle = (paintingData.lightVector * paintingData.directionVector) / paintingData.lightVectorLength;
     if (cosineOfAngle > paintingData.coneCutOffLimit) {
         // No light is produced, scanlines are not updated
         paintingData.colorVector.setX(0.0f);
@@ -128,6 +128,7 @@ void DistantLightSource::initPaintingData(PaintingData& paintingData)
     paintingData.lightVector.setX(cosf(azimuth) * cosf(elevation));
     paintingData.lightVector.setY(sinf(azimuth) * cosf(elevation));
     paintingData.lightVector.setZ(sinf(elevation));
+    paintingData.lightVectorLength = 1;
 }
 
 void DistantLightSource::updatePaintingData(PaintingData&, int, int, float)
