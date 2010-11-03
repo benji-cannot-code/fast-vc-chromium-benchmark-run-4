d220372a94d86f4559cceeb811d189a4e0482031
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-03 14:50:04 +0000

    2010-11-02  Stephen White  <senorblanco@chromium.org>
            Reviewed by Tony Chang.
    
            [chromium] Fix LayoutTestController UMRs.
            https://bugs.webkit.org/show_bug.cgi?id=48872
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -72,6 +72,8 @@ using namespace std;
 
 LayoutTestController::LayoutTestController(TestShell* shell)
     : m_shell(shell)
+    , m_closeRemainingWindows(false)
+    , m_deferMainResourceDataLoad(false)
     , m_workQueue(this)
 {
 
