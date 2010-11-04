4c1b13f2903f724fe6e2bc519493dbcddafcf7c7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-04 02:13:28 +0000

    2010-11-03  Jenn Braithwaite  <jennb@chromium.org>
            Reviewed by Dmitry Titov.
    
            Chromium: Update resource tracking when moving a frame between documents
            https://bugs.webkit.org/show_bug.cgi?id=48363
    
            * public/WebFrameClient.h:
            (WebKit::WebFrameClient::removeIdentifierForRequest):
            Added
            * src/FrameLoaderClientImpl.cpp:
            (WebKit::FrameLoaderClientImpl::transferLoadingResourceFromPage):
            Notify current WebFrameClient of the resource and remove from
            former WebFrameClient.
    2010-11-03  Jenn Braithwaite  <jennb@chromium.org>
    
            Reviewed by Dmitry Titov.
    
            Chromium: Update resource tracking when moving a frame between documents
            https://bugs.webkit.org/show_bug.cgi?id=48363
    
            * DumpRenderTree/chromium/WebViewHost.cpp:
            (WebViewHost::assignIdentifierToRequest):
            Always put resource id in map so we can make assumptions about its
            presence.
            (WebViewHost::removeIdentifierForRequest):
            Added.
            (WebViewHost::didFinishResourceLoad):
            (WebViewHost::didFailResourceLoad):
            Check existence of resource id before removing from map.
            * DumpRenderTree/chromium/WebViewHost.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -941,9 +941,14 @@ void WebViewHost::didChangeLocationWithinPage(WebFrame* frame)
 
 void WebViewHost::assignIdentifierToRequest(WebFrame*, unsigned identifier, const WebURLRequest& request)
 {
-    if (!m_shell->shouldDumpResourceLoadCallbacks())
-        return;
+    ASSERT(!m_resourceIdentifierMap.contains(identifier));
     m_resourceIdentifierMap.set(identifier, descriptionSuitableForTestResult(request.url().spec()));
+}
+
+void WebViewHost::removeIdentifierForRequest(unsigned identifier)
+{
+    ASSERT(m_resourceIdentifierMap.contains(identifier));
+    m_resourceIdentifierMap.remove(identifier);
 }
 
 void WebViewHost::willSendRequest(WebFrame*, unsigned identifier, WebURLRequest& request, const WebURLResponse& redirectResponse)
@@ -1023,7 +1028,7 @@ void WebViewHost::didFinishResourceLoad(WebFrame*, unsigned identifier)
         printResourceDescription(identifier);
         fputs(" - didFinishLoading\n", stdout);
     }
-    m_resourceIdentifierMap.remove(identifier);
+    removeIdentifierForRequest(identifier);
 }
 
 void WebViewHost::didFailResourceLoad(WebFrame*, unsigned identifier, const WebURLError& error)
@@ -1034,7 +1039,7 @@ void WebViewHost::didFailResourceLoad(WebFrame*, unsigned identifier, const WebU
         fputs(webkit_support::MakeURLErrorDescription(error).c_str(), stdout);
         fputs("\n", stdout);
     }
-    m_resourceIdentifierMap.remove(identifier);
+    removeIdentifierForRequest(identifier);
 }
 
 void WebViewHost::didDisplayInsecureContent(WebFrame*)
