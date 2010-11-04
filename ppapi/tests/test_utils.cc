8c59726b639d8999f6716d49172a6cde78328c10
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-04 04:01:31 +0000

    Including <stdin.h> to hopefully fix a build error on the ChromeOS bots.
    BUG=none
    TEST=none
    TBR=darin
    
    Review URL: http://codereview.chromium.org/4469001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65022 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_utils.cc
+++ ppapi/tests/test_utils.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/tests/test_utils.h"
 
+#include <stdin.h>
+
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/cpp/module.h"
 
