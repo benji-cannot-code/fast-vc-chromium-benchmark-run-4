e0637981f9573fe0555f3d88e332a54f3e1956d8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-04 08:49:37 +0000

    2010-11-04  Shane Stephens  <shanestephens@google.com>
            Reviewed by Tony Chang.
    
            SVGStyledTransformableElement supplemental transforms pre-multiplied
            but should be post-multiplied.
            https://bugs.webkit.org/show_bug.cgi?id=48026
    
            * svg/animations/animate-path-nested-transforms-expected.txt: Added.
            * svg/animations/animate-path-nested-transforms.svg: Added.
    2010-11-04  Shane Stephens  <shanestephens@google.com>
    
            Reviewed by Tony Chang.
    
            SVGStyledTransformableElement supplemental transforms pre-multiplied
            but should be post-multiplied.
            https://bugs.webkit.org/show_bug.cgi?id=48026
    
            Test: svg/animations/animate-path-nested-transforms.svg
    
            * svg/SVGStyledTransformableElement.cpp:
            (WebCore::SVGStyledTransformableElement::animatedLocalTransform):
    
            Switched order of multiplication.  Note that due to a bug in
            AffineTransform's operator* implementation the order of arguments is
            reversed (see http://bugs.webkit.org/show_bug.cgi?id=48031).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/svg/SVGStyledTransformableElement.cpp
+++ third_party/WebKit/WebCore/svg/SVGStyledTransformableElement.cpp
@@ -55,7 +55,7 @@ AffineTransform SVGStyledTransformableElement::getScreenCTM(StyleUpdateStrategy 
 
 AffineTransform SVGStyledTransformableElement::animatedLocalTransform() const
 {
-    return m_supplementalTransform ? transform()->concatenate().matrix() * *m_supplementalTransform : transform()->concatenate().matrix();
+    return m_supplementalTransform ? *m_supplementalTransform * transform()->concatenate().matrix() : transform()->concatenate().matrix();
 }
     
 AffineTransform* SVGStyledTransformableElement::supplementalTransform()
