be719235ad40eee811ad56c810b316a0cd0145b3
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-04 00:32:25 +0000

    linux: include linux/nfs_fs.h for NFS superblock magic
    The location of the definition we want has moved over time.
    See the bug for why this location is better.
    
    BUG=52269
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/4455001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65000 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nss_util.cc
+++ base/nss_util.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <secmod.h>
 
 #if defined(OS_LINUX)
-#include <linux/magic.h>
+#include <linux/nfs_fs.h>
 #include <sys/vfs.h>
 #endif
 
