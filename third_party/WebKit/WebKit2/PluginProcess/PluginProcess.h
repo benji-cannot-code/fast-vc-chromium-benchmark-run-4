4295ba7d6d662d0a595a5e0b0055fc5603c918c0
alice.liu@apple.com <alice.liu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-04 02:32:14 +0000

    Fix clang++ build
    * PluginProcess/PluginProcess.h:
    * UIProcess/Plugins/PluginProcessProxy.h:
    * UIProcess/WebContextMenuProxy.h:
    Change class to struct and vise versa where needed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/PluginProcess/PluginProcess.h
+++ third_party/WebKit/WebKit2/PluginProcess/PluginProcess.h
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class NetscapePluginModule;
-class PluginProcessCreationParameters;
 class WebProcessConnection;
+struct PluginProcessCreationParameters;
         
 class PluginProcess : Noncopyable, CoreIPC::Connection::Client {
 public:
--- third_party/WebKit/WebKit2/UIProcess/Plugins/PluginProcessProxy.h
+++ third_party/WebKit/WebKit2/UIProcess/Plugins/PluginProcessProxy.h
@@ -41,9 +41,9 @@ namespace CoreIPC {
 
 namespace WebKit {
 
-class PluginProcessCreationParameters;
 class PluginProcessManager;
 class WebProcessProxy;
+struct PluginProcessCreationParameters;
 
 class PluginProcessProxy : CoreIPC::Connection::Client, ProcessLauncher::Client {
 public:
