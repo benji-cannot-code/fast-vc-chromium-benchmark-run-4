648eb6ed6fa44470b2a0814849e6855c98706948
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-04 17:17:41 +0000

    2010-11-04  Andreas Kling  <kling@webkit.org>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] Add a stub WebContextMenuProxy
    
            This is the Qt equivalent of r71333.
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPagePrivate::createContextMenuProxy):
            * UIProcess/qt/WebContextMenuProxyQt.cpp: Added.
            (WebKit::WebContextMenuProxyQt::WebContextMenuProxyQt):
            (WebKit::WebContextMenuProxyQt::create):
            (WebKit::WebContextMenuProxyQt::showContextMenu):
            (WebKit::WebContextMenuProxyQt::hideContextMenu):
            * UIProcess/qt/WebContextMenuProxyQt.h: Added.
            * WebKit2.pro:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LocalizedStrings.h"
 #include "NativeWebKeyboardEvent.h"
 #include "WebContext.h"
-#include "WebContextMenuProxy.h"
+#include "WebContextMenuProxyQt.h"
 #include "WebEventFactoryQt.h"
 #include "WebPlatformStrategies.h"
 #include "WebPopupMenuProxyQt.h"
@@ -138,8 +138,7 @@ PassRefPtr<WebPopupMenuProxy> QWKPagePrivate::createPopupMenuProxy()
 
 PassRefPtr<WebContextMenuProxy> QWKPagePrivate::createContextMenuProxy(WebPageProxy*)
 {
-    // FIXME: Implement;
-    return 0;
+    return WebContextMenuProxyQt::create();
 }
 
 void QWKPagePrivate::setFindIndicator(PassRefPtr<FindIndicator>, bool fadeOut)
--- third_party/WebKit/WebKit2/WebKit2.pro
+++ third_party/WebKit/WebKit2/WebKit2.pro
@@ -292,6 +292,7 @@ HEADERS += \
     UIProcess/WebProcessManager.h \
     UIProcess/WebProcessProxy.h \
     UIProcess/WebUIClient.h \
+    UIProcess/qt/WebContextMenuProxyQt.h \
     UIProcess/qt/WebPopupMenuProxyQt.h \
     WebProcess/Downloads/Download.h \
     WebProcess/Downloads/DownloadManager.h \
@@ -516,6 +517,7 @@ SOURCES += \
     WebProcess/WebProcess.cpp \
     UIProcess/qt/ChunkedUpdateDrawingAreaProxyQt.cpp \
     UIProcess/qt/WebContextQt.cpp \
+    UIProcess/qt/WebContextMenuProxyQt.cpp \
     UIProcess/qt/WebPopupMenuProxyQt.cpp \
     WebProcess/qt/WebProcessMainQt.cpp \
     WebProcess/qt/WebProcessQt.cpp \
