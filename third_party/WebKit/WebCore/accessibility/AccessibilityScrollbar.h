7f50ad4757c1d17379355c9413b36723a6033a8e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-05 19:55:25 +0000

    2010-11-05  Chris Guillory  <chris.guillory@google.com>
            Reviewed by Chris Fleizach.
    
            Keep a reference to the scrollbar in accessibility scrollbar.
            https://bugs.webkit.org/show_bug.cgi?id=48896
    
            * accessibility/AccessibilityScrollbar.h:
            (WebCore::AccessibilityScrollbar::scrollbar):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityScrollbar.h
+++ third_party/WebKit/WebCore/accessibility/AccessibilityScrollbar.h
@@ -42,7 +42,7 @@ public:
 
     void setScrollbar(Scrollbar* scrollbar) { m_scrollbar = scrollbar; }
 
-    Scrollbar* scrollbar() const { return m_scrollbar; }
+    Scrollbar* scrollbar() const { return m_scrollbar.get(); }
 
     virtual bool isAccessibilityScrollbar() const { return true; }
 
@@ -61,7 +61,7 @@ private:
     virtual IntRect elementRect() const { ASSERT_NOT_REACHED(); return IntRect(); }
     virtual AccessibilityObject* parentObject() const { ASSERT_NOT_REACHED(); return 0; }
 
-    Scrollbar* m_scrollbar;
+    RefPtr<Scrollbar> m_scrollbar;
 };
 
 } // namespace WebCore
