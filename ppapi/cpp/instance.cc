5eda774612e47e5d92e29b636f08540fa3e6b5d8
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-05 01:54:06 +0000

    Revert 65152 - Integrated Pepper3D v2 with the accelerated compositor. - It now uses GGL instead of a child window. - Fixed a bug where GLES2 decoder set texture state without updating TextureInfo. - Fixed Pepper3D demos and added awesome 3D CSS programmer art. - Removed ggl::GetCurrentContext to prevent further abuse. Fixed said abuse :) - GGL exposes GLES2Implementation to allow issue of GL calls on a particular context without making that context globally current. - Removed redundant "this context" argument from GGL SwapBuffers completion callback. - Temporarily removed context lost notification. I need to figure out the best semantics. TEST=Run Pepper3D and WebGL demos and YouTube videos on Windows and Mac. BUG=none
    Review URL: http://codereview.chromium.org/3531008
    
    TBR=apatrick@chromium.org
    Review URL: http://codereview.chromium.org/4561001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65169 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/instance.cc
+++ ppapi/cpp/instance.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/c/dev/ppp_printing_dev.h"
 #include "ppapi/c/ppb_instance.h"
-#include "ppapi/cpp/dev/graphics_3d_dev.h"
 #include "ppapi/cpp/dev/scrollbar_dev.h"
 #include "ppapi/cpp/dev/widget_dev.h"
 #include "ppapi/cpp/graphics_2d.h"
@@ -83,12 +82,6 @@ Var Instance::GetOwnerElementObject() {
 }
 
 bool Instance::BindGraphics(const Graphics2D& graphics) {
-  if (!ppb_instance_f)
-    return false;
-  return ppb_instance_f->BindGraphics(pp_instance(), graphics.pp_resource());
-}
-
-bool Instance::BindGraphics(const Graphics3D_Dev& graphics) {
   if (!ppb_instance_f)
     return false;
   return ppb_instance_f->BindGraphics(pp_instance(), graphics.pp_resource());
