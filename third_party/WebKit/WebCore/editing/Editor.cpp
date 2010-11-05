301d6bae0599a92b946a11ddadfb671cb1aefc68
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-05 03:58:41 +0000

    Build fix.
    * editing/Editor.cpp:
    (WebCore::Editor::dismissCorrectionPanel):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/Editor.cpp
+++ third_party/WebKit/WebCore/editing/Editor.cpp
@@ -2516,6 +2516,8 @@ void Editor::dismissCorrectionPanel(CorrectionWasRejectedOrNot correctionWasReje
     m_correctionPanelInfo.m_rangeToBeReplaced.clear();
     if (client())
         client()->dismissCorrectionPanel(correctionWasRejectedOrNot);
+#else
+    UNUSED_PARAM(correctionWasRejectedOrNot);
 #endif
 }
 void Editor::removeSpellAndCorrectionMarkersFromWordsToBeEdited(bool doNotRemoveIfSelectionAtWordBoundary)
