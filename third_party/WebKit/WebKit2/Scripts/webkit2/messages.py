b559285f60605cdc33c6a95765c939a8e88bb856
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-05 16:55:18 +0000

    Fix failing python tests.
    * Scripts/webkit2/messages.py:
    * Scripts/webkit2/messages_unittest.py:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71425 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Scripts/webkit2/messages.py
+++ third_party/WebKit/WebKit2/Scripts/webkit2/messages.py
@@ -251,7 +251,7 @@ def message_to_struct_declaration(message):
 def struct_or_class(namespace, type):
     structs = frozenset([
         'WebCore::KeypressCommand',
-        'WebCore::PluginData',
+        'WebCore::PluginInfo',
         'WebCore::ViewportArguments',
         'WebCore::WindowFeatures',
         'WebKit::PluginProcessCreationParameters',
--- third_party/WebKit/WebKit2/Scripts/webkit2/messages_unittest.py
+++ third_party/WebKit/WebKit2/Scripts/webkit2/messages_unittest.py
@@ -256,7 +256,7 @@ _expected_header = """/*
 #include "MessageID.h"
 #include "Plugin.h"
 #include <WebCore/KeyboardEvent.h>
-#include <WebCore/PluginInfo.h>
+#include <WebCore/PluginData.h>
 #include <wtf/Vector.h>
 
 namespace CoreIPC {
