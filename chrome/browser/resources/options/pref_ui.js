1538aeed3b441e7148fea57730d2191e2b6a0cbd
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-05 22:51:22 +0000

    [tabbed options] add instant confirm dialog.
    When the user clicks enable for the first time, it shows the dialog. After the user accepts for the first time, it should never show again.
    
    BUG=61042
    TEST=manual
    
    Review URL: http://codereview.chromium.org/4524001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/pref_ui.js
+++ chrome/browser/resources/options/pref_ui.js
@@ -26,10 +26,10 @@ cr.define('options', function() {
       // Listen to pref changes.
       Preferences.getInstance().addEventListener(this.pref,
           function(event) {
-            var value = (event.value && event.value['value'] != undefined) ?
+            var value = event.value && event.value['value'] != undefined ?
                 event.value['value'] : event.value;
             self.checked = Boolean(value);
-            self.managed = (event.value && event.value['managed'] != undefined) ?
+            self.managed = event.value && event.value['managed'] != undefined ?
                 event.value['managed'] : false;
             self.disabled = self.managed;
           });
@@ -43,7 +43,7 @@ cr.define('options', function() {
                     Number(self.checked), self.metric);
                 break;
               case 'boolean':
-                Preferences.setBooleanPref(self.pref, self.checked,
+                Preferences.setBooleanPref(self.pref,
                     self.checked, self.metric);
                 break;
             }
@@ -97,9 +97,9 @@ cr.define('options', function() {
       // Listen to pref changes.
       Preferences.getInstance().addEventListener(this.pref,
           function(event) {
-            var value = (event.value && event.value['value'] != undefined) ?
+            var value = event.value && event.value['value'] != undefined ?
                 event.value['value'] : event.value;
-            self.managed = (event.value && event.value['managed'] != undefined) ?
+            self.managed = event.value && event.value['managed'] != undefined ?
                 event.value['managed'] : false;
             self.checked = String(value) == self.value;
             self.disabled = self.managed;
@@ -157,9 +157,9 @@ cr.define('options', function() {
       // Listen to pref changes.
       Preferences.getInstance().addEventListener(this.pref,
           function(event) {
-            self.value = (event.value && event.value['value'] != undefined) ?
+            self.value = event.value && event.value['value'] != undefined ?
                 event.value['value'] : event.value;
-            self.managed = (event.value && event.value['managed'] != undefined) ?
+            self.managed = event.value && event.value['managed'] != undefined ?
                 event.value['managed'] : false;
             self.disabled = self.managed;
           });
@@ -269,9 +269,9 @@ cr.define('options', function() {
       // Listen to pref changes.
       Preferences.getInstance().addEventListener(this.pref,
           function(event) {
-            var value = (event.value && event.value['value'] != undefined) ?
+            var value = event.value && event.value['value'] != undefined ?
                 event.value['value'] : event.value;
-            self.managed = (event.value && event.value['managed'] != undefined) ?
+            self.managed = event.value && event.value['managed'] != undefined ?
                 event.value['managed'] : false;
             self.disabled = self.managed;
             // Honor manually_disabled property, so options pages can
@@ -366,9 +366,9 @@ cr.define('options', function() {
       // Listen to pref changes.
       Preferences.getInstance().addEventListener(this.pref,
           function(event) {
-            self.value = (event.value && event.value['value'] != undefined) ?
+            self.value = event.value && event.value['value'] != undefined ?
                 event.value['value'] : event.value;
-            self.managed = (event.value && event.value['managed'] != undefined) ?
+            self.managed = event.value && event.value['managed'] != undefined ?
                 event.value['managed'] : false;
             self.disabled = self.managed;
           });
