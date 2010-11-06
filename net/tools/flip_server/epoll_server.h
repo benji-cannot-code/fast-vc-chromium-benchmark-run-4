ecfa65c073769b93290a7d9582afbb472b621589
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-06 17:38:23 +0000

    flip_in_mem_edsm_server builds again.
    BUG=None
    TEST=It builds.
    
    Review URL: http://codereview.chromium.org/4094002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65313 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/epoll_server.h
+++ net/tools/flip_server/epoll_server.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fcntl.h>
 #include <sys/queue.h>
-#include <ext/hash_map>  // it is annoying that gcc does this. oh well.
-#include <ext/hash_set>
 #include <map>
 #include <string>
 #include <utility>
@@ -37,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include "base/basictypes.h"
+#include "base/hash_tables.h"
 #include "base/scoped_ptr.h"
 #include <sys/epoll.h>
 
@@ -575,7 +574,7 @@ class EpollServer {
     }
   };
 
-  typedef __gnu_cxx::hash_set<CBAndEventMask, CBAndEventMaskHash> FDToCBMap;
+  typedef base::hash_set<CBAndEventMask, CBAndEventMaskHash> FDToCBMap;
 
   // the following four functions are OS-specific, and are likely
   // to be changed in a subclass if the poll/select method is changed
@@ -944,7 +943,7 @@ class EpollServer {
 
     std::vector<DebugOutput*> debug_events_;
     std::vector<Events> unregistered_fds_;
-    typedef __gnu_cxx::hash_map<int, Events> EventCountsMap;
+    typedef base::hash_map<int, Events> EventCountsMap;
     EventCountsMap event_counts_;
     int64 num_records_;
     int64 record_threshold_;
