e687c22d2dffe798e0e8309133a1f56819717750
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-06 05:23:23 +0000

    2010-11-05  Simon Fraser  <simon.fraser@apple.com>
            Undo most of the previous commit since the compiler seems to have trouble
            with an inlined operator++ in debug builds.
    
            * platform/DeprecatedPtrListImpl.cpp:
            (WebCore::DeprecatedPtrListImplIterator::operator++):
            * platform/DeprecatedPtrListImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/DeprecatedPtrListImpl.cpp
+++ third_party/WebKit/WebCore/platform/DeprecatedPtrListImpl.cpp
@@ -472,6 +472,14 @@ void *DeprecatedPtrListImplIterator::operator--()
     return current();
 }
 
+void *DeprecatedPtrListImplIterator::operator++()
+{
+    if (node)
+        node = node->next;
+
+    return current();
+}
+
 DeprecatedPtrListImplIterator &DeprecatedPtrListImplIterator::operator=(const DeprecatedPtrListImplIterator &impl)
 {
     if (list) {
