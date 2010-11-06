492c5df46ea3fe51a37f5ef2f129ceddb3418ea7
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-06 01:28:40 +0000

    Add --enable-nacl to about:flags
    Review URL: http://codereview.chromium.org/4620001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -145,6 +145,13 @@ const Experiment kExperiments[] = {
     switches::kEnablePrintPreview
   },
   {
+    "enable-nacl",   // Do not change; see above.
+    IDS_FLAGS_ENABLE_NACL_NAME,
+    IDS_FLAGS_ENABLE_NACL_DESCRIPTION,
+    kOsAll,
+    switches::kEnableNaCl
+  },
+  {
     "dns-server",
     IDS_FLAGS_DNS_SERVER_NAME,
     IDS_FLAGS_DNS_SERVER_DESCRIPTION,
