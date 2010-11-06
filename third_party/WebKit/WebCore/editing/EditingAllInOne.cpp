ed809dbb96541454c161042961a8baf98d5af9b1
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-06 22:56:01 +0000

    2010-11-06  Ryosuke Niwa  <rniwa@webkit.org>
            Unreviewed Windows build fix.
    
            * editing/EditingAllInOne.cpp: Added EditingStyle.cpp
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/editing/EditingAllInOne.cpp
+++ third_party/WebKit/WebCore/editing/EditingAllInOne.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <DeleteFromTextNodeCommand.cpp>
 #include <DeleteSelectionCommand.cpp>
 #include <EditCommand.cpp>
+#include <EditingStyle.cpp>
 #include <Editor.cpp>
 #include <EditorCommand.cpp>
 #include <FormatBlockCommand.cpp>
