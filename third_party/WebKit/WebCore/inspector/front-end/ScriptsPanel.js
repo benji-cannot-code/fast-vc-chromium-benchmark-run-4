c92c87a9ab6ddce540ad333ddd4827dd43ee743b
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-08 10:12:36 +0000

    2010-11-06  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: add more event listener breakpoint types, add support for regular breakpoint hit state, beautify hit rendering.
            https://bugs.webkit.org/show_bug.cgi?id=49130
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/BreakpointManager.js:
            (WebInspector.BreakpointManager.prototype.debuggerPaused):
            (WebInspector.Breakpoint.jsBreakpointId):
            (WebInspector.Breakpoint.prototype.get id):
            * inspector/front-end/BreakpointsSidebarPane.js:
            (WebInspector.EventListenerBreakpointsSidebarPane):
            (WebInspector.EventListenerBreakpointsSidebarPane.prototype._createCategory):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel):
            * inspector/front-end/inspector.css:
            (#elements-sidebar):
            (.pane > .body):
            (#scripts-sidebar):
            (li.breakpoint-hit .breakpoint-hit-marker):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
@@ -136,8 +136,8 @@ WebInspector.ScriptsPanel = function()
     if (Preferences.nativeInstrumentationEnabled) {
         this.sidebarPanes.domBreakpoints = WebInspector.createDOMBreakpointsSidebarPane();
         this.sidebarPanes.xhrBreakpoints = WebInspector.createXHRBreakpointsSidebarPane();
+        this.sidebarPanes.eventListenerBreakpoints = new WebInspector.EventListenerBreakpointsSidebarPane();
     }
-    this.sidebarPanes.eventListenerBreakpoints = new WebInspector.EventListenerBreakpointsSidebarPane();
 
     this.sidebarPanes.workers = new WebInspector.WorkersSidebarPane();
 
