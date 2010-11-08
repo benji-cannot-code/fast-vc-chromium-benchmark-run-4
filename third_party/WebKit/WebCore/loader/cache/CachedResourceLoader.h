066aa0fd0e671ed26134b2f8f272b45a2734e3db
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-08 08:59:24 +0000

    2010-11-08  Adam Barth  <abarth@webkit.org>
            Reviewed by Antti Koivisto.
    
            Rename CachedResourceLoader::m_doc to CachedResourceLoader::m_document
            https://bugs.webkit.org/show_bug.cgi?id=49163
    
            The usual convention is to use "document", not "doc" to refer to the
            document.
    
            * css/CSSCursorImageValue.cpp:
            (WebCore::CSSCursorImageValue::cachedImage):
            * dom/XMLDocumentParserLibxml2.cpp:
            (WebCore::shouldAllowExternalLoad):
            * loader/cache/CachedResourceLoader.cpp:
            (WebCore::CachedResourceLoader::CachedResourceLoader):
            (WebCore::CachedResourceLoader::frame):
            (WebCore::CachedResourceLoader::requestImage):
            (WebCore::CachedResourceLoader::canRequest):
            (WebCore::CachedResourceLoader::requestResource):
            (WebCore::CachedResourceLoader::printAccessDeniedMessage):
            (WebCore::CachedResourceLoader::preload):
            (WebCore::CachedResourceLoader::checkForPendingPreloads):
            (WebCore::CachedResourceLoader::requestPreload):
            * loader/cache/CachedResourceLoader.h:
            (WebCore::CachedResourceLoader::document):
            * loader/cache/MemoryCache.cpp:
            (WebCore::MemoryCache::requestResource):
            * loader/loader.cpp:
            (WebCore::Loader::load):
            (WebCore::Loader::Host::servePendingRequests):
            (WebCore::Loader::Host::didFinishLoading):
            (WebCore::Loader::Host::didFail):
            * xml/XSLTProcessorLibxslt.cpp:
            (WebCore::docLoaderFunc):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/cache/CachedResourceLoader.h
+++ third_party/WebKit/WebCore/loader/cache/CachedResourceLoader.h
@@ -83,7 +83,7 @@ public:
     CachePolicy cachePolicy() const;
     
     Frame* frame() const; // Can be NULL
-    Document* doc() const { return m_doc; }
+    Document* document() const { return m_document; }
 
     void removeCachedResource(CachedResource*) const;
 
@@ -113,7 +113,7 @@ private:
     MemoryCache* m_cache;
     HashSet<String> m_reloadedURLs;
     mutable DocumentResourceMap m_documentResources;
-    Document* m_doc;
+    Document* m_document;
     
     int m_requestCount;
     
