904e07986ea8942f2c8afadfa9adb5e401670133
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-08 22:18:05 +0000

    2010-11-08  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Restore normal meaning of double-click on patch review page?
            https://bugs.webkit.org/show_bug.cgi?id=47641
    
            After this patch, you need to click on the line numbers to add a
            comment.
    
            * PrettyPatch/PrettyPatch.rb:
            * code-review.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/BugsSite/code-review.js
+++ third_party/WebKit/BugsSite/code-review.js
@@ -254,7 +254,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function crawlDiff() {
-    $('.Line').each(idify).each(hoverify).dblclick(addCommentField);
+    $('.Line').each(idify).each(hoverify);
     $('.FileDiff').each(function() {
       var file_name = $(this).children('h1').text();
       files[file_name] = this;
