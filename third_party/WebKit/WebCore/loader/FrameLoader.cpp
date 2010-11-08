da5546c25401e18f334b1373a57d108a36bb094c
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-08 07:59:14 +0000

    2010-11-07  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Remove some unneeded includes.
    
            * loader/FrameLoader.cpp:
            * loader/PlaceholderDocument.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/WebCore/loader/FrameLoader.cpp
@@ -78,7 +78,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PageCache.h"
 #include "PageGroup.h"
 #include "PageTransitionEvent.h"
-#include "PlaceholderDocument.h"
 #include "PluginData.h"
 #include "PluginDatabase.h"
 #include "PluginDocument.h"
--- third_party/WebKit/WebCore/loader/PlaceholderDocument.cpp
+++ third_party/WebKit/WebCore/loader/PlaceholderDocument.cpp
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PlaceholderDocument.h"
 
-#include "CSSStyleSelector.h"
-#include "StyleSheetList.h"
-
 namespace WebCore {
 
 void PlaceholderDocument::attach()
