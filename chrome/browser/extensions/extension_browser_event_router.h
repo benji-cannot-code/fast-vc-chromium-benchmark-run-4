48d3062a62f6d7edd2f9c34af5ba52e10ad05773
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 16:06:05 +0000

    Add pinned property to tabs.update and create. Fire tabs.onUpdated event if a tab's pinned state changes.
    BUG=24781
    TEST=ExtensionApiTest
    
    Review URL: http://codereview.chromium.org/3816005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65379 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browser_event_router.h
+++ chrome/browser/extensions/extension_browser_event_router.h
@@ -75,6 +75,7 @@ class ExtensionBrowserEventRouter : public TabStripModelObserver,
   virtual void TabReplacedAt(TabContents* old_contents,
                              TabContents* new_contents,
                              int index);
+  virtual void TabPinnedStateChanged(TabContents* contents, int index);
   virtual void TabStripEmpty();
 
   // Page Action execute event.
@@ -100,6 +101,11 @@ class ExtensionBrowserEventRouter : public TabStripModelObserver,
   // Internal processing of tab updated events. Is called by both TabChangedAt
   // and Observe/NAV_ENTRY_COMMITTED.
   void TabUpdated(TabContents* contents, bool did_navigate);
+
+  // Packages |changed_properties| as a tab updated event for the tab |contents|
+  // and dispatches the event to the extension.
+  void DispatchTabUpdatedEvent(TabContents* contents,
+                               DictionaryValue* changed_properties);
 
   // Called to dispatch a deprecated style page action click event that was
   // registered like:
@@ -164,6 +170,10 @@ class ExtensionBrowserEventRouter : public TabStripModelObserver,
 
     GURL url_;
   };
+
+  // Gets the TabEntry for the given |contents|. Returns TabEntry* if
+  // found, NULL if not.
+  TabEntry* GetTabEntry(const TabContents* contents);
 
   std::map<int, TabEntry> tab_entries_;
 
--- chrome/browser/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/gtk/tabs/tab_strip_gtk.cc
@@ -1051,6 +1051,10 @@ void TabStripGtk::TabReplacedAt(TabContents* old_contents,
 }
 
 void TabStripGtk::TabMiniStateChanged(TabContents* contents, int index) {
+  // Don't do anything if we've already picked up the change from TabMoved.
+  if (GetTabAt(index)->mini() == model_->IsMiniTab(index))
+    return;
+
   GetTabAt(index)->set_mini(model_->IsMiniTab(index));
   // Don't animate if the window isn't visible yet. The window won't be visible
   // when dragging a mini-tab to a new window.
@@ -2008,7 +2012,8 @@ bool TabStripGtk::CanPaintOnlyFavIcons(const GdkRectangle* rects,
   for (int r = 0; r < num_rects; ++r) {
     while (t < GetTabCount()) {
       TabGtk* tab = GetTabAt(t);
-      if (GdkRectMatchesTabFavIconBounds(rects[r], tab)) {
+      if (GdkRectMatchesTabFavIconBounds(rects[r], tab) &&
+          tab->ShouldShowIcon()) {
         tabs_to_paint->push_back(t);
         ++t;
         break;
