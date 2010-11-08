96eb22679f24c14b222f730285a14782132c15a1
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 18:59:24 +0000

    Removed few unused lines.
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/4572002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65403 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_resource_tracker.h
+++ webkit/glue/plugins/pepper_resource_tracker.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_module.h"
 #include "ppapi/c/pp_resource.h"
 
-typedef struct NPObject NPObject;
-
 namespace pepper {
 
 class PluginInstance;
