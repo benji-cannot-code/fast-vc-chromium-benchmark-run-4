1c7f12027bacaa6e1d469a16f8d1e591bacc2a4a
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 21:15:27 +0000

    Fix compile failure on GCC
    BUG=54274
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/test_url_fetcher_factory.cc
+++ chrome/common/net/test_url_fetcher_factory.cc
@@ -30,7 +30,7 @@ TestURLFetcher* TestURLFetcherFactory::GetFetcherByID(int id) const {
 }
 
 void TestURLFetcherFactory::RemoveFetcherFromMap(int id) {
-  Fetchers::const_iterator i = fetchers_.find(id);
+  Fetchers::iterator i = fetchers_.find(id);
   DCHECK(i != fetchers_.end());
   fetchers_.erase(i);
 }
