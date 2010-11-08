a4e9a7b7cf551fd2b2c38ddbab6a62078c71643f
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-08 03:54:27 +0000

    2010-11-07  Fumitoshi Ukai  <ukai@chromium.org>
            Unreviewed, rolling out r71474.
            http://trac.webkit.org/changeset/71474
            https://bugs.webkit.org/show_bug.cgi?id=48280
    
            breaks chromium webkit tests
            https://bugs.webkit.org/show_bug.cgi?id=49151
    
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/base_unittest.py:
            * Scripts/webkitpy/layout_tests/port/config.py:
            * Scripts/webkitpy/layout_tests/port/config_unittest.py:
            * Scripts/webkitpy/layout_tests/port/google_chrome_unittest.py:
            * Scripts/webkitpy/layout_tests/port/webkit.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
            * Scripts/webkitpy/tool/mocktool.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/mocktool.py
@@ -572,7 +572,6 @@ class MockStatusServer(object):
 
 
 # FIXME: This should not inherit from Mock
-# FIXME: Unify with common.system.executive_mock.MockExecutive.
 class MockExecutive(Mock):
     def __init__(self, should_log):
         self._should_log = should_log
