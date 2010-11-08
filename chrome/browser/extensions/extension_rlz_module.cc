bea175c892acb2bc2d5dd054d311fb3b41e65928
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 16:02:56 +0000

    Allow RLZ API calls from extensions to access the registry. Temporary fix until these IO operations are moved to the File thread.
    TEST=none
    BUG=62098
    
    This patch accepted from skare, reviewed at: http://codereview.chromium.org/4528002/show
    
    Review URL: http://codereview.chromium.org/4680002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_rlz_module.cc
+++ chrome/browser/extensions/extension_rlz_module.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_rlz_module.h"
 
 #include "base/scoped_ptr.h"
+#include "base/thread_restrictions.h"
 #include "base/values.h"
 #include "chrome/common/extensions/extension.h"
 #include "rlz/win/lib/lib_values.h"
@@ -71,6 +72,11 @@ bool GetEventFromName(const std::string& event_name,
 }  // namespace
 
 bool RlzRecordProductEventFunction::RunImpl() {
+  // This can be slow if registry access goes to disk. Should preferably
+  // perform registry operations on the File thread.
+  //   http://code.google.com/p/chromium/issues/detail?id=62098
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   std::string product_name;
   EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &product_name));
   rlz_lib::Product product;
@@ -91,6 +97,11 @@ bool RlzRecordProductEventFunction::RunImpl() {
 }
 
 bool RlzGetAccessPointRlzFunction::RunImpl() {
+  // This can be slow if registry access goes to disk. Should preferably
+  // perform registry operations on the File thread.
+  //   http://code.google.com/p/chromium/issues/detail?id=62098
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   std::string ap_name;
   EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &ap_name));
   rlz_lib::AccessPoint access_point;
@@ -104,6 +115,11 @@ bool RlzGetAccessPointRlzFunction::RunImpl() {
 }
 
 bool RlzSendFinancialPingFunction::RunImpl() {
+  // This can be slow if registry access goes to disk. Should preferably
+  // perform registry operations on the File thread.
+  //   http://code.google.com/p/chromium/issues/detail?id=62098
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   std::string product_name;
   EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &product_name));
   rlz_lib::Product product;
@@ -160,6 +176,11 @@ bool RlzSendFinancialPingFunction::RunImpl() {
 }
 
 bool RlzClearProductStateFunction::RunImpl() {
+  // This can be slow if registry access goes to disk. Should preferably
+  // perform registry operations on the File thread.
+  //   http://code.google.com/p/chromium/issues/detail?id=62098
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   std::string product_name;
   EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &product_name));
   rlz_lib::Product product;
