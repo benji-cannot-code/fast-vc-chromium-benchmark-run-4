e2661133259de1c91df7b10c6f03ec4167c961a5
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 17:46:44 +0000

    Implement NPN_reloadPlugins.
    The one catch is that in multiprocess Chromium, if reloadPages
    is true, we only reload pages that are in the same process.
    This is probably what is wanted most of the time (to reload the
    page that triggered this) and avoids unnecessary reloads.
    
    BUG=29018
    TEST=Covered by existing layout tests.
    
    Review URL: http://codereview.chromium.org/4618001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65391 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/plugin_host.cc
+++ webkit/glue/plugins/plugin_host.cc
@@ -16,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_string_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "net/base/net_util.h"
+#include "third_party/npapi/bindings/npapi_extensions.h"
+#include "third_party/npapi/bindings/npruntime.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebBindings.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebKit.h"
 #include "webkit/glue/webkit_glue.h"
 #include "webkit/glue/plugins/default_plugin_shared.h"
 #include "webkit/glue/plugins/npapi_extension_thunk.h"
@@ -26,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/plugins/plugin_stream_url.h"
 #include "webkit/glue/plugins/webplugin_delegate.h"
 #include "webkit/glue/plugins/webplugininfo.h"
-#include "third_party/npapi/bindings/npapi_extensions.h"
-#include "third_party/npapi/bindings/npruntime.h"
 
 using WebKit::WebBindings;
 
@@ -308,9 +309,8 @@ uint32_t NPN_MemFlush(uint32_t size) {
 
 // This is for dynamic discovery of new plugins.
 // Should force a re-scan of the plugins directory to load new ones.
-void NPN_ReloadPlugins(NPBool reloadPages) {
-  // TODO: implement me
-  DVLOG(1) << "NPN_ReloadPlugin is not implemented yet.";
+void NPN_ReloadPlugins(NPBool reload_pages) {
+  WebKit::resetPluginCache(reload_pages);
 }
 
 // Requests a range of bytes for a seekable stream.
