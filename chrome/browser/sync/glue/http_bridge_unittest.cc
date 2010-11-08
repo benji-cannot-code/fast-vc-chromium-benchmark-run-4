b11f6284d5f3e3dc40a769817999303f25eb75f1
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 21:09:04 +0000

    Make TestURLFetcherFactory able to remove fetchers from its map, and add an ID accessor on TestURLFetcher, in preparation for more careful fetchre tracking in the GoogleURLTracker unittest.
    BUG=54274
    TEST=none
    Review URL: http://codereview.chromium.org/4509002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65424 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/http_bridge_unittest.cc
+++ chrome/browser/sync/glue/http_bridge_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -104,7 +104,7 @@ class HttpBridgeTest : public testing::Test {
 
 class DummyURLFetcher : public TestURLFetcher {
  public:
-  DummyURLFetcher() : TestURLFetcher(GURL(), POST, NULL) {}
+  DummyURLFetcher() : TestURLFetcher(0, GURL(), POST, NULL) {}
 
   net::HttpResponseHeaders* response_headers() const {
     return NULL;
--- chrome/common/net/test_url_fetcher_factory.h
+++ chrome/common/net/test_url_fetcher_factory.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,13 +40,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TestURLFetcher : public URLFetcher {
  public:
-  TestURLFetcher(const GURL& url, RequestType request_type, Delegate* d);
-
-  // Returns the delegate installed on the URLFetcher.
-  Delegate* delegate() const { return URLFetcher::delegate(); }
+  TestURLFetcher(int id,
+                 const GURL& url,
+                 RequestType request_type,
+                 Delegate* d);
 
   // Overriden to do nothing. It is assumed the caller will notify the delegate.
   virtual void Start() {}
+
+  // Unique ID in our factory.
+  int id() const { return id_; }
 
   // URL we were created with. Because of how we're using URLFetcher url()
   // always returns an empty URL. Chances are you'll want to use original_url()
@@ -56,7 +59,11 @@ class TestURLFetcher : public URLFetcher {
   // Returns the data uploaded on this URLFetcher.
   const std::string& upload_data() const { return URLFetcher::upload_data(); }
 
+  // Returns the delegate installed on the URLFetcher.
+  Delegate* delegate() const { return URLFetcher::delegate(); }
+
  private:
+  const int id_;
   const GURL original_url_;
 
   DISALLOW_COPY_AND_ASSIGN(TestURLFetcher);
@@ -72,8 +79,8 @@ class TestURLFetcherFactory : public URLFetcher::Factory {
                                        const GURL& url,
                                        URLFetcher::RequestType request_type,
                                        URLFetcher::Delegate* d);
-
   TestURLFetcher* GetFetcherByID(int id) const;
+  void RemoveFetcherFromMap(int id);
 
  private:
   // Maps from id passed to create to the returned URLFetcher.
