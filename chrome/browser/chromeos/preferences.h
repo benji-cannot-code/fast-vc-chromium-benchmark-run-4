c405037169ea8af9264ba3d7fdae8b69638e1ad5
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 17:54:09 +0000

    Remove chat manager component extension in favour of external extensions.
    I originally added this in r60123 but over time it turned out that being a regular installed app will work out better.
    
    BUG=none
    TEST=chat manager should no longer be a component extension on Chrome OS builds
    
    Review URL: http://codereview.chromium.org/4652002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences.h
+++ chrome/browser/chromeos/preferences.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_observer.h"
 
 class PrefService;
-class Profile;
 
 namespace chromeos {
 
@@ -25,8 +24,8 @@ namespace chromeos {
 // When the preferences change, we change the settings to reflect the new value.
 class Preferences : public NotificationObserver {
  public:
-  explicit Preferences(Profile* profile);
-  virtual ~Preferences() {}
+  Preferences();
+  virtual ~Preferences();
 
   // This method will register the prefs associated with Chrome OS settings.
   static void RegisterUserPrefs(PrefService* prefs);
@@ -84,11 +83,6 @@ class Preferences : public NotificationObserver {
   // underlying XKB API requires it.
   void UpdateAutoRepeatRate();
 
-  // Updates whether the Talk app is enabled.
-  void UpdateTalkApp();
-
-  Profile* profile_;
-
   BooleanPrefMember tap_to_click_enabled_;
   BooleanPrefMember vert_edge_scroll_enabled_;
   BooleanPrefMember accessibility_enabled_;
@@ -126,9 +120,6 @@ class Preferences : public NotificationObserver {
   BooleanPrefMember language_xkb_auto_repeat_enabled_;
   IntegerPrefMember language_xkb_auto_repeat_delay_pref_;
   IntegerPrefMember language_xkb_auto_repeat_interval_pref_;
-
-  // Labs preferences.
-  IntegerPrefMember labs_talk_enabled_;
 
   BooleanPrefMember enable_screen_lock_;
 
