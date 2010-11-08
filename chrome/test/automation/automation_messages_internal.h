9b457c33b94a28f6a448bcf38f5884225408ddd7
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-08 21:43:55 +0000

    Reapply part of r64637, which caused mysterious failures on mac waterfall.
    This applies only the rename of IPC::ContextMenuParams to
    IPC::MiniContextMenuParams so that it doesn't conflict with ::ContextMenuParams
    (defined in webkit). This is about half the original patch.
    
    BUG=51409
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/4669005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65429 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/automation_messages_internal.h
+++ chrome/test/automation/automation_messages_internal.h
@@ -954,7 +954,7 @@ IPC_BEGIN_MESSAGES(Automation)
                       int /* tab_handle */,
                       HANDLE /* source menu handle */,
                       int    /* align flags */,
-                      IPC::ContextMenuParams /* params */)
+                      IPC::MiniContextMenuParams /* params */)
 
   IPC_MESSAGE_ROUTED2(AutomationMsg_ForwardContextMenuCommandToChrome,
                       int /* tab_handle */,
