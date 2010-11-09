981759447990ce9f2ac49feac2e8427b75e859ac
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 03:41:14 +0000

    2010-11-08  Kenneth Rohde Christiansen  <kenneth@webkit.org>
            Reviewed by Andreas Kling.
    
            Send page scroll requests to the PageClient when scroll delegation is turned on
            https://bugs.webkit.org/show_bug.cgi?id=49171
    
            Implement the IPC and API required for making scroll delegation work
            with WebKit2. This is only used when you have tiling enabled.
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPagePrivate::pageDidRequestScroll):
            * UIProcess/API/qt/qwkpage.h:
            * UIProcess/API/qt/qwkpage_p.h:
            * UIProcess/PageClient.h:
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::pageDidRequestScroll):
            * UIProcess/WebPageProxy.h:
            * UIProcess/WebPageProxy.messages.in:
            * WebProcess/WebCoreSupport/WebChromeClient.cpp:
            (WebKit::WebChromeClient::delegatedScrollRequested):
            * WebProcess/WebPage/WebPage.cpp:
            (WebKit::WebPage::pageDidRequestScroll):
            * WebProcess/WebPage/WebPage.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
@@ -344,7 +344,7 @@ void WebChromeClient::scroll(const IntSize& scrollDelta, const IntRect& rectToSc
 #if ENABLE(TILED_BACKING_STORE)
 void WebChromeClient::delegatedScrollRequested(const IntSize& scrollDelta)
 {
-    notImplemented();
+    m_page->pageDidRequestScroll(scrollDelta);
 }
 #endif
 
