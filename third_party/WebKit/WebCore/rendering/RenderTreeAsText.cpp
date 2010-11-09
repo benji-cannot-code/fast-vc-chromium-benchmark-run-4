0c5a3c1e31997c246521823b6d4c16a766bc7938
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 21:59:47 +0000

    2010-11-09  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by James Robinson.
    
            Make showLayerTree() more useful for debugging
            https://bugs.webkit.org/show_bug.cgi?id=49273
    
            Three things to make showLayerTree() more useful when called from gdb:
            1. Add showLayerTree(RenderObject*)
            2. Make it not update layout, so you can call it when debugging layout code.
            3. Add output of the 'needs layout' flags for each renderer.
    
            * rendering/RenderLayer.cpp:
            (showLayerTree):
            * rendering/RenderLayer.h:
            * rendering/RenderTreeAsText.cpp:
            (WebCore::RenderTreeAsText::writeRenderObject):
            (WebCore::externalRepresentation):
            * rendering/RenderTreeAsText.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
+++ third_party/WebKit/WebCore/rendering/RenderTreeAsText.cpp
@@ -414,6 +414,41 @@ void RenderTreeAsText::writeRenderObject(TextStream& ts, const RenderObject& o, 
             }
         }
     }
+    
+    if (behavior & RenderAsTextShowLayoutState) {
+        bool needsLayout = o.selfNeedsLayout() || o.needsPositionedMovementLayout() || o.posChildNeedsLayout() || o.normalChildNeedsLayout();
+        if (needsLayout)
+            ts << " (needs layout:";
+        
+        bool havePrevious = false;
+        if (o.selfNeedsLayout()) {
+            ts << " self";
+            havePrevious = true;
+        }
+
+        if (o.needsPositionedMovementLayout()) {
+            if (havePrevious)
+                ts << ",";
+            havePrevious = true;
+            ts << " positioned movement";
+        }
+
+        if (o.normalChildNeedsLayout()) {
+            if (havePrevious)
+                ts << ",";
+            havePrevious = true;
+            ts << " child";
+        }
+
+        if (o.posChildNeedsLayout()) {
+            if (havePrevious)
+                ts << ",";
+            ts << " positioned child";
+        }
+
+        if (needsLayout)
+            ts << ")";
+    }
 
 #if PLATFORM(QT)
     // Print attributes of embedded QWidgets. E.g. when the WebCore::Widget
@@ -692,7 +727,8 @@ String externalRepresentation(Frame* frame, RenderAsTextBehavior behavior)
         printContext.begin(frame->contentRenderer()->width());
     }
 
-    frame->document()->updateLayout();
+    if (!(behavior & RenderAsTextDontUpdateLayout))
+        frame->document()->updateLayout();
 
     RenderObject* o = frame->contentRenderer();
     if (!o)
