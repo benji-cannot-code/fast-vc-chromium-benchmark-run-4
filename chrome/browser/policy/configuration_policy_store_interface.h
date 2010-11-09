08b8b76d019bac63e081e5d9e9529926d5bbc4f9
danno@chromium.org <danno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-09 16:15:48 +0000

    Revert 65535 - Group Policy support for HTTP authentication
    BUG=53625
    TEST=ConfigurationPolicyPrefStore*
    
    Review URL: http://codereview.chromium.org/3517018
    Patch from Jakob Kummerow <jkummerow@google.com>.
    
    TBR=danno@chromium.org
    Review URL: http://codereview.chromium.org/4643004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65537 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_store_interface.h
+++ chrome/browser/policy/configuration_policy_store_interface.h
@@ -54,12 +54,6 @@ enum ConfigurationPolicyType {
   kPolicyBlockThirdPartyCookies,
   kPolicyExtensionInstallForceList,
   kPolicyChromeOsLockOnIdleSuspend,
-  kPolicyAuthSchemes,
-  kPolicyDisableAuthNegotiateCnameLookup,
-  kPolicyEnableAuthNegotiatePort,
-  kPolicyAuthServerWhitelist,
-  kPolicyAuthNegotiateDelegateWhitelist,
-
 };
 
 static const int kPolicyNoProxyServerMode = 0;
