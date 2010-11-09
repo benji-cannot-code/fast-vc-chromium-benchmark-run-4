1c0c39827399abbe2812a4ca7e1e29a61a012626
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-09 21:47:56 +0000

    Add base::ThreadRestrictions::ScopedAllowIO allow_io;  to google_update_settings.cc until the bug is fixed BUG=62121 TEST=none Review URL: http://codereview.chromium.org/4655005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/google_update_settings.cc
+++ chrome/installer/util/google_update_settings.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
+#include "base/thread_restrictions.h"
 #include "base/time.h"
 #include "base/win/registry.h"
 #include "chrome/common/chrome_switches.h"
@@ -22,6 +23,9 @@ using base::win::RegKey;
 namespace {
 
 bool ReadGoogleUpdateStrKey(const wchar_t* const name, std::wstring* value) {
+  // The registry functions below will end up going to disk.  Do this on another
+  // thread to avoid slowing the IO thread.  http://crbug.com/62121
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
   BrowserDistribution* dist = BrowserDistribution::GetDistribution();
   std::wstring reg_path = dist->GetStateKey();
   RegKey key(HKEY_CURRENT_USER, reg_path.c_str(), KEY_READ);
