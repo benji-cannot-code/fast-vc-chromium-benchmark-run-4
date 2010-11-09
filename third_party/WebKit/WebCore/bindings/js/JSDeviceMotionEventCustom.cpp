efa9d81787f39ce7229d6420eda312ee5ffcc92f
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 15:46:52 +0000

    2010-11-09  Steve Block  <steveblock@google.com>
            Reviewed by Andreas Kling.
    
            Unused parameter warning causes JSC build failure with deviceorientation
            https://bugs.webkit.org/show_bug.cgi?id=49251
    
            No new tests, build fix only.
    
            * bindings/js/JSDeviceMotionEventCustom.cpp:
            (WebCore::JSDeviceMotionEvent::interval):
            * bindings/js/JSDeviceOrientationEventCustom.cpp:
            (WebCore::JSDeviceOrientationEvent::alpha):
            (WebCore::JSDeviceOrientationEvent::beta):
            (WebCore::JSDeviceOrientationEvent::gamma):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDeviceMotionEventCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDeviceMotionEventCustom.cpp
@@ -154,7 +154,7 @@ JSValue JSDeviceMotionEvent::rotationRate(ExecState* exec) const
     return createRotationRateObject(imp->deviceMotionData()->rotationRate(), exec);
 }
 
-JSValue JSDeviceMotionEvent::interval(ExecState* exec) const
+JSValue JSDeviceMotionEvent::interval(ExecState*) const
 {
     DeviceMotionEvent* imp = static_cast<DeviceMotionEvent*>(impl());
     if (!imp->deviceMotionData()->canProvideInterval())
