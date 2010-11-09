3dc37a43eb39a93f3022a7bcaa8837cbb1a3a6d2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 07:02:28 +0000

    2010-11-08  James Simonsen  <simonjam@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            [Web Timing] Split domContentLoaded into start/end
            https://bugs.webkit.org/show_bug.cgi?id=48920
    
            * fast/dom/script-tests/webtiming.js:
            (checkTimingBeforeLoad):
            (checkTimingWhileDeferred):
            (checkWebTimingOnDOMContentLoaded): Added.
            (checkWebTimingWhileAsync):
            (checkWebTimingOnLoad):
            (checkWebTimingAfterLoad):
            * fast/dom/webtiming-document-open-expected.txt:
            * fast/dom/webtiming-expected.txt:
    2010-11-08  James Simonsen  <simonjam@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            [Web Timing] Split domContentLoaded into start/end
            https://bugs.webkit.org/show_bug.cgi?id=48920
    
            * dom/Document.cpp:
            (WebCore::Document::finishedParsing):
            * dom/DocumentTiming.h:
            (WebCore::DocumentTiming::DocumentTiming):
            * page/Timing.cpp:
            (WebCore::Timing::domContentLoadedStart):
            (WebCore::Timing::domContentLoadedEnd):
            * page/Timing.h:
            * page/Timing.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
@@ -41,7 +41,8 @@ function checkTimingBeforeLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBe("timing.domInteractive", "0");
-    shouldBe("timing.domContentLoaded", "0");
+    shouldBe("timing.domContentLoadedStart", "0");
+    shouldBe("timing.domContentLoadedEnd", "0");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -73,7 +74,42 @@ function checkTimingWhileDeferred()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.domLoading");
-    shouldBe("timing.domContentLoaded", "0");
+    shouldBe("timing.domContentLoadedStart", "0");
+    shouldBe("timing.domContentLoadedEnd", "0");
+    shouldBe("timing.domComplete", "0");
+
+    shouldBe("timing.loadEventStart", "0");
+    shouldBe("timing.loadEventEnd", "0");
+
+    window.addEventListener("DOMContentLoaded", checkWebTimingOnDOMContentLoaded, false);
+}
+
+function checkWebTimingOnDOMContentLoaded() {
+    shouldBeGreaterThanOrEqual("timing.navigationStart", "oneHourAgoUTC");
+
+    shouldBeGreaterThanOrEqual("timing.unloadEventEnd", "timing.navigationStart");
+
+    shouldBe("timing.redirectStart", "0");
+    shouldBe("timing.redirectEnd", "0");
+    shouldBe("navigation.redirectCount", "0");
+
+    shouldBeGreaterThanOrEqual("timing.fetchStart", "timing.navigationStart");
+
+    shouldBeGreaterThanOrEqual("timing.domainLookupStart", "timing.fetchStart");
+    shouldBeGreaterThanOrEqual("timing.domainLookupEnd", "timing.domainLookupStart");
+
+    shouldBeGreaterThanOrEqual("timing.connectStart", "timing.domainLookupEnd");
+    shouldBeGreaterThanOrEqual("timing.connectEnd", "timing.connectStart");
+
+    shouldBeGreaterThanOrEqual("timing.requestStart", "timing.connectEnd");
+    shouldBeGreaterThanOrEqual("timing.requestEnd", "timing.requestStart");
+
+    shouldBe("timing.responseStart", "timing.requestEnd");
+
+    shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
+    shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.domLoading");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
+    shouldBe("timing.domContentLoadedEnd", "0");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -112,7 +148,8 @@ function checkWebTimingWhileAsync()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoaded", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -147,8 +184,9 @@ function checkWebTimingOnLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoaded", "timing.domInteractive");
-    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoaded");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
+    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEnd");
 
     shouldBeGreaterThanOrEqual("timing.loadEventStart", "timing.responseEnd");
     shouldBe("timing.loadEventEnd", "0");
@@ -182,8 +220,9 @@ function checkWebTimingAfterLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoaded", "timing.domInteractive");
-    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoaded");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
+    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEnd");
 
     shouldBeGreaterThanOrEqual("timing.loadEventStart", "timing.responseEnd");
     shouldBeGreaterThanOrEqual("timing.loadEventEnd", "timing.loadEventStart + 50");
