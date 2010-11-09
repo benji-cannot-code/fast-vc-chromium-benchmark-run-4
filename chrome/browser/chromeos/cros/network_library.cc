cf0bb5b064e43279a890c90f60841322c98b9cf5
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-09 02:51:13 +0000

    Do not send many requests to flimflam when network chagned.  Send sync request to flimflam only if new request didn't come in 50ms.  Network menu was updating menu twice when opening.
    BUG=chromium-os:8435
    TEST=manual: enable vlog (--vlog=1) and observe that chrome log reports
         one "UpdateNetworkStatus" when network changes (disable, enable ethernet etc)
         There may be two when wifi is enabled as different wifi source may be
         recognized at different timing.
    
    Review URL: http://codereview.chromium.org/4229005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -18,6 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "grit/generated_resources.h"
 
+namespace {
+
+// FlimFlam may send multiple notifications for single network change.
+// We wait small amount of time before retrieving the status to
+// avoid send multiple sync request to flim flam.
+const int kNetworkUpdateDelayMs = 50;
+
+}  // namespace
+
 namespace chromeos {
 
 namespace {
@@ -650,7 +659,8 @@ class NetworkLibraryImpl : public NetworkLibrary  {
         available_devices_(0),
         enabled_devices_(0),
         connected_devices_(0),
-        offline_mode_(false) {
+        offline_mode_(false),
+        update_task_(NULL) {
     if (EnsureCrosLoaded()) {
       Init();
       network_manager_monitor_ =
@@ -677,6 +687,19 @@ class NetworkLibraryImpl : public NetworkLibrary  {
   virtual void AddNetworkManagerObserver(NetworkManagerObserver* observer) {
     if (!network_manager_observers_.HasObserver(observer))
       network_manager_observers_.AddObserver(observer);
+  }
+
+  void NetworkStatusChanged() {
+    CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    if (update_task_) {
+      update_task_->Cancel();
+    }
+    update_task_ =
+        NewRunnableMethod(this,
+                          &NetworkLibraryImpl::UpdateNetworkManagerStatus);
+    BrowserThread::PostDelayedTask(
+        BrowserThread::UI, FROM_HERE, update_task_,
+        kNetworkUpdateDelayMs);
   }
 
   virtual void RemoveNetworkManagerObserver(NetworkManagerObserver* observer) {
@@ -1168,7 +1191,7 @@ class NetworkLibraryImpl : public NetworkLibrary  {
                                                  const Value* value) {
     NetworkLibraryImpl* networklib = static_cast<NetworkLibraryImpl*>(object);
     DCHECK(networklib);
-    networklib->UpdateNetworkManagerStatus();
+    networklib->NetworkStatusChanged();
   }
 
   static void DataPlanUpdateHandler(void* object,
@@ -1391,18 +1414,14 @@ class NetworkLibraryImpl : public NetworkLibrary  {
 
   void UpdateNetworkManagerStatus() {
     // Make sure we run on UI thread.
-    if (!BrowserThread::CurrentlyOn(BrowserThread::UI)) {
-      BrowserThread::PostTask(
-          BrowserThread::UI, FROM_HERE,
-          NewRunnableMethod(this,
-                            &NetworkLibraryImpl::UpdateNetworkManagerStatus));
-      return;
-    }
+    CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+    update_task_ = NULL;
+    VLOG(1) << "Updating Network Status";
 
     SystemInfo* system = GetSystemInfo();
     if (!system)
       return;
-
 
     std::string prev_cellular_service_path = cellular_ ?
         cellular_->service_path() : std::string();
@@ -1574,6 +1593,9 @@ class NetworkLibraryImpl : public NetworkLibrary  {
 
   bool offline_mode_;
 
+  // Delayed task to retrieve the network information.
+  CancelableTask* update_task_;
+
   DISALLOW_COPY_AND_ASSIGN(NetworkLibraryImpl);
 };
 
@@ -1679,7 +1701,6 @@ class NetworkLibraryStubImpl : public NetworkLibrary {
     return NetworkIPConfigVector();
   }
   virtual std::string GetHtmlInfo(int refresh) { return std::string(); }
-  virtual void UpdateSystemInfo() {}
 
  private:
   std::string ip_address_;
