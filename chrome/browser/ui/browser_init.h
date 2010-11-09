574c0b3092f95eb7ff0bc60d51a18132cd89df5c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-09 04:00:19 +0000

    Move more files to chrome/browser/ui
    TBR=brettw
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/4720002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_init.h
+++ chrome/browser/ui/browser_init.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_BROWSER_INIT_H_
-#define CHROME_BROWSER_BROWSER_INIT_H_
+#ifndef CHROME_BROWSER_UI_BROWSER_INIT_H_
+#define CHROME_BROWSER_UI_BROWSER_INIT_H_
 #pragma once
 
 #include <string>
@@ -197,4 +197,4 @@ class BrowserInit {
   DISALLOW_COPY_AND_ASSIGN(BrowserInit);
 };
 
-#endif  // CHROME_BROWSER_BROWSER_INIT_H_
+#endif  // CHROME_BROWSER_UI_BROWSER_INIT_H_
--- chrome/browser/ui/browser_list.h
+++ chrome/browser/ui/browser_list.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_BROWSER_LIST_H_
-#define CHROME_BROWSER_BROWSER_LIST_H_
+#ifndef CHROME_BROWSER_UI_BROWSER_LIST_H_
+#define CHROME_BROWSER_UI_BROWSER_LIST_H_
 #pragma once
 
 #include <vector>
@@ -233,4 +233,4 @@ class TabContentsIterator {
   TabContents* cur_;
 };
 
-#endif  // CHROME_BROWSER_BROWSER_LIST_H_
+#endif  // CHROME_BROWSER_UI_BROWSER_LIST_H_
