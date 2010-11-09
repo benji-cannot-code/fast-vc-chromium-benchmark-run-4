c516e5fbfa770e4fb15973895353208d27f4c39a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 12:26:45 +0000

    2010-11-09  Dai Mikurube  <dmikurube@google.com>
            Reviewed by Kent Tamura.
    
            Too precise serialization from floating point number to string for "number" input elements
            https://bugs.webkit.org/show_bug.cgi?id=48308
    
            * fast/forms/input-stepup-stepdown-expected.txt:
            * fast/forms/script-tests/input-stepup-stepdown.js:
    2010-11-09  Dai Mikurube  <dmikurube@google.com>
    
            Reviewed by Kent Tamura.
    
            Too precise serialization from floating point number to string for "number" input elements
            https://bugs.webkit.org/show_bug.cgi?id=48308
    
            Modified to consider decimal places when handling step and base in applyStep().
    
            * html/HTMLInputElement.cpp: Considering decimal places of the given "step" attribtue.
            (WebCore::HTMLInputElement::getAllowedValueStep):
            (WebCore::HTMLInputElement::getAllowedValueStepWithDecimalPlaces):
            (WebCore::HTMLInputElement::applyStep):
            * html/HTMLInputElement.h:
            * html/InputType.cpp: Added virtual functions for decimal places and an acceptable error.
            (WebCore::InputType::stepBaseWithDecimalPlaces):
            (WebCore::InputType::acceptableError):
            (WebCore::InputType::parseToDoubleWithDecimalPlaces):
            * html/InputType.h:
            * html/NumberInputType.cpp:
            (WebCore::NumberInputType::stepMismatch): Using the virtual function acceptableError().
            (WebCore::NumberInputType::stepBaseWithDecimalPlaces): Considering decimal places of the given "base" attribute.
            (WebCore::NumberInputType::parseToDoubleWithDecimalPlaces):
            (WebCore::NumberInputType::acceptableError): Concrete acceptableError() for the number type.
            * html/NumberInputType.h:
            * html/parser/HTMLParserIdioms.cpp:
            (WebCore::parseToDoubleForNumberTypeWithDecimalPlaces): Parsing numbers with decimal places.
            * html/parser/HTMLParserIdioms.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/InputType.cpp
+++ third_party/WebKit/WebCore/html/InputType.cpp
@@ -238,6 +238,13 @@ double InputType::stepBase() const
     return 0;
 }
 
+double InputType::stepBaseWithDecimalPlaces(unsigned* decimalPlaces) const
+{
+    if (decimalPlaces)
+        *decimalPlaces = 0;
+    return stepBase();
+}
+
 double InputType::defaultStep() const
 {
     return numeric_limits<double>::quiet_NaN();
@@ -258,6 +265,11 @@ bool InputType::scaledStepValeuShouldBeInteger() const
     return false;
 }
 
+double InputType::acceptableError(double) const
+{
+    return 0;
+}
+
 RenderObject* InputType::createRenderer(RenderArena*, RenderStyle* style) const
 {
     return RenderObject::createObject(element(), style);
@@ -266,6 +278,13 @@ RenderObject* InputType::createRenderer(RenderArena*, RenderStyle* style) const
 double InputType::parseToDouble(const String&, double defaultValue) const
 {
     return defaultValue;
+}
+
+double InputType::parseToDoubleWithDecimalPlaces(const String& src, double defaultValue, unsigned *decimalPlaces) const
+{
+    if (decimalPlaces)
+        *decimalPlaces = 0;
+    return parseToDouble(src, defaultValue);
 }
 
 bool InputType::parseToDateComponents(const String&, DateComponents*) const
