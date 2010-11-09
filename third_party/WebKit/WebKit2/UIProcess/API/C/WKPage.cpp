b30c0f896ad8233eccab145c3549dc1715f7d77c
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-09 22:33:27 +0000

    Add some stylistic flourish to the last patch as suggested by Lord Darin Adler.
    * UIProcess/API/C/WKPage.cpp:
    (WKPageGetSourceForFrame):
    (WKPageGetContentsAsString):
    * UIProcess/WebPageProxy.cpp:
    (WebKit::invalidateCallbackMap):
    * WebProcess/WebPage/WebFrame.cpp:
    (WebKit::WebFrame::contentsAsString):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/WKPage.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/C/WKPage.cpp
@@ -312,7 +312,7 @@ void WKPageRenderTreeExternalRepresentation_b(WKPageRef pageRef, WKPageRenderTre
 }
 #endif
 
-void WKPageGetSourceForFrame(WKPageRef pageRef, WKFrameRef frameRef, void *context, WKPageGetSourceForFrameFunction callback)
+void WKPageGetSourceForFrame(WKPageRef pageRef, WKFrameRef frameRef, void* context, WKPageGetSourceForFrameFunction callback)
 {
     toImpl(pageRef)->getSourceForFrame(toImpl(frameRef), FrameSourceCallback::create(context, callback));
 }
@@ -331,7 +331,7 @@ void WKPageGetSourceForFrame_b(WKPageRef pageRef, WKFrameRef frameRef, WKPageGet
 }
 #endif
 
-void WKPageGetContentsAsString(WKPageRef pageRef, void *context, WKPageGetContentsAsStringFunction callback)
+void WKPageGetContentsAsString(WKPageRef pageRef, void* context, WKPageGetContentsAsStringFunction callback)
 {
     toImpl(pageRef)->getContentsAsString(ContentsAsStringCallback::create(context, callback));
 }
