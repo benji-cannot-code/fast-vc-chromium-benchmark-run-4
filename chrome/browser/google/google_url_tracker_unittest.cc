8ffd6e21fd06e7b78d7b498245f82c2711d1b065
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-09 22:31:07 +0000

    Cleanup precursor patch #3: Convert factory class into a simple function pointer, move a few more bits around, remove a portion of a test that isn't important.
    BUG=54274
    TEST=none
    Review URL: http://codereview.chromium.org/4661005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65583 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker_unittest.cc
+++ chrome/browser/google/google_url_tracker_unittest.cc
@@ -60,10 +60,11 @@ class TestInfoBarDelegate : public InfoBarDelegate {
                       const GURL& new_google_url);
   virtual ~TestInfoBarDelegate();
 
+  // InfoBarDelegate
+  virtual InfoBar* CreateInfoBar();
+
   GoogleURLTracker* google_url_tracker() const { return google_url_tracker_; }
   const GURL& new_google_url() const { return new_google_url_; }
-
-  virtual InfoBar* CreateInfoBar();
 
  private:
   GoogleURLTracker* google_url_tracker_;
@@ -84,20 +85,14 @@ InfoBar* TestInfoBarDelegate::CreateInfoBar() {
   return NULL;
 }
 
+InfoBarDelegate* CreateTestInfobar(
+    TabContents* tab_contents,
+    GoogleURLTracker* google_url_tracker,
+    const GURL& new_google_url) {
+  return new TestInfoBarDelegate(google_url_tracker, new_google_url);
+}
+
 }  // namespace
-
-
-// TestInfoBarDelegateFactory -------------------------------------------------
-
-class TestInfoBarDelegateFactory
-    : public GoogleURLTracker::InfoBarDelegateFactory {
- public:
-  virtual InfoBarDelegate* CreateInfoBar(TabContents* tab_contents,
-                                         GoogleURLTracker* google_url_tracker,
-                                         const GURL& new_google_url) {
-    return new TestInfoBarDelegate(google_url_tracker, new_google_url);
-  }
-};
 
 
 // GoogleURLTrackerTest -------------------------------------------------------
@@ -168,8 +163,8 @@ void GoogleURLTrackerTest::SetUp() {
 
   URLFetcher::set_factory(&fetcher_factory_);
   observer_.reset(new TestNotificationObserver);
-  g_browser_process->google_url_tracker()->infobar_factory_.reset(
-      new TestInfoBarDelegateFactory);
+  g_browser_process->google_url_tracker()->infobar_creator_ =
+      &CreateTestInfobar;
 }
 
 void GoogleURLTrackerTest::TearDown() {
@@ -360,14 +355,6 @@ TEST_F(GoogleURLTrackerTest, UpdatePromptedURLWhenBack) {
   FinishSleep();
   MockSearchDomainCheckResponse(0, ".google.co.uk");
 
-  EXPECT_EQ(GURL("http://www.google.co.uk/"), GetFetchedGoogleURL());
-  EXPECT_EQ(GURL("http://www.google.co.uk/"), GoogleURLTracker::GoogleURL());
-  EXPECT_EQ(GURL("http://www.google.co.uk/"), GetLastPromptedGoogleURL());
-
-  SearchCommitted(GURL("http://www.google.co.uk/search?q=test"));
-  NavEntryCommitted();
-
-  EXPECT_FALSE(InfoBarIsShown());
   EXPECT_EQ(GURL("http://www.google.co.uk/"), GetFetchedGoogleURL());
   EXPECT_EQ(GURL("http://www.google.co.uk/"), GoogleURLTracker::GoogleURL());
   EXPECT_EQ(GURL("http://www.google.co.uk/"), GetLastPromptedGoogleURL());
