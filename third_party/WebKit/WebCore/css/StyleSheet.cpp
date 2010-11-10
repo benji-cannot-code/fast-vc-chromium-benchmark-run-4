ebe17ba8d5114005c36c0e32f4eac7b7c8cacf1d
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-10 21:38:28 +0000

    Release build fix.
            * css/StyleSheet.cpp: isAcceptableStyleSheetParent() is a debug-only function.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/StyleSheet.cpp
+++ third_party/WebKit/WebCore/css/StyleSheet.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+#if !ASSERT_DISABLED
 static bool isAcceptableStyleSheetParent(Node* parentNode)
 {
     // Only these nodes can be parents of StyleSheets, and they need to call clearOwnerNode() when moved out of document.
@@ -41,6 +42,7 @@ static bool isAcceptableStyleSheetParent(Node* parentNode)
 #endif    
     ;
 }
+#endif
 
 StyleSheet::StyleSheet(StyleSheet* parentSheet, const String& originalURL, const KURL& finalURL)
     : StyleList(parentSheet)
