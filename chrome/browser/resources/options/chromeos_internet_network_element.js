10c84645604c53821affc673f2804233ff2da6ba
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 01:49:08 +0000

    Do not allow network status updates while password field is active.
    BUG=chromium-os:8304
    TEST=manually show password field (click on password protected wireless network or "join other network...") and make some network change (disconnect ethernet cable etc.), and check if the status is unchanged, then close the password field by any possible means and check if status update is working.
    
    Review URL: http://codereview.chromium.org/4705003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65617 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos_internet_network_element.js
+++ chrome/browser/resources/options/chromeos_internet_network_element.js
@@ -71,6 +71,7 @@ cr.define('options.internet', function() {
               if (this.childNodes[i] != item)
                 this.childNodes[i].hidePassword();
             }
+            InternetOptions.unlockUpdates();
             // If clicked on other networks item.
             if (data && data.servicePath == '?') {
               item.showOtherLogin();
@@ -255,6 +256,9 @@ cr.define('options.internet', function() {
     showPassword: function() {
       if (this.connecting)
         return;
+
+      InternetOptions.lockUpdates();
+
       var passwordDiv = this.ownerDocument.createElement('div');
       passwordDiv.className = 'network-password';
       var passInput = this.ownerDocument.createElement('input');
@@ -317,6 +321,8 @@ cr.define('options.internet', function() {
     showOtherLogin: function() {
       if (this.connecting)
         return;
+
+      InternetOptions.lockUpdates();
 
       var ssidDiv = this.ownerDocument.createElement('div');
       ssidDiv.className = 'network-password';
