60cb051299f2653880ce0201154baead3d6b86ec
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-10 04:48:26 +0000

    2010-11-09  Chris Rogers  <crogers@google.com>
            Reviewed by James Robinson.
    
            AudioFileReaderMac.cpp must include <CoreServices/CoreServices.h>
            https://bugs.webkit.org/show_bug.cgi?id=49270
    
            * platform/audio/mac/AudioFileReaderMac.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/audio/mac/AudioFileReaderMac.cpp
+++ third_party/WebKit/WebCore/platform/audio/mac/AudioFileReaderMac.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AudioBus.h"
 #include "AudioFileReader.h"
 #include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
 
 namespace WebCore {
 
