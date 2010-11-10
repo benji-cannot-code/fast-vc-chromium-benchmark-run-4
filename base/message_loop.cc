831fff6ce459182d7e1a08efe32053dea3932505
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 20:28:06 +0000

    Convert some DCHECKs to DCHECK_EQ.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/4766002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65699 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -161,7 +161,7 @@ MessageLoop::MessageLoop(Type type)
 }
 
 MessageLoop::~MessageLoop() {
-  DCHECK(this == current());
+  DCHECK_EQ(this, current());
 
   // Let interested parties have one last shot at accessing this.
   FOR_EACH_OBSERVER(DestructionObserver, destruction_observers_,
@@ -192,13 +192,13 @@ MessageLoop::~MessageLoop() {
 
 void MessageLoop::AddDestructionObserver(
     DestructionObserver* destruction_observer) {
-  DCHECK(this == current());
+  DCHECK_EQ(this, current());
   destruction_observers_.AddObserver(destruction_observer);
 }
 
 void MessageLoop::RemoveDestructionObserver(
     DestructionObserver* destruction_observer) {
-  DCHECK(this == current());
+  DCHECK_EQ(this, current());
   destruction_observers_.RemoveObserver(destruction_observer);
 }
 
@@ -252,7 +252,7 @@ __declspec(noinline) void MessageLoop::RunInternalInSEHFrame() {
 //------------------------------------------------------------------------------
 
 void MessageLoop::RunInternal() {
-  DCHECK(this == current());
+  DCHECK_EQ(this, current());
 
   StartHistogrammer();
 
@@ -287,7 +287,7 @@ bool MessageLoop::ProcessNextDelayedNonNestableTask() {
 //------------------------------------------------------------------------------
 
 void MessageLoop::Quit() {
-  DCHECK(current() == this);
+  DCHECK_EQ(this, current());
   if (state_) {
     state_->quit_received = true;
   } else {
@@ -296,7 +296,7 @@ void MessageLoop::Quit() {
 }
 
 void MessageLoop::QuitNow() {
-  DCHECK(current() == this);
+  DCHECK_EQ(this, current());
   if (state_) {
     pump_->Quit();
   } else {
