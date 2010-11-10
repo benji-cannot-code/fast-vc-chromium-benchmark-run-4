benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 4c1b13f2903f724fe6e2bc519493dbcddafcf7c7

2010-11-03  Jenn Braithwaite  <jennb@chromium.org>
        Reviewed by Dmitry Titov.

        Chromium: Update resource tracking when moving a frame between documents
        https://bugs.webkit.org/show_bug.cgi?id=48363

        * public/WebFrameClient.h:
        (WebKit::WebFrameClient::removeIdentifierForRequest):
        Added
        * src/FrameLoaderClientImpl.cpp:
        (WebKit::FrameLoaderClientImpl::transferLoadingResourceFromPage):
        Notify current WebFrameClient of the resource and remove from
        former WebFrameClient.
2010-11-03  Jenn Braithwaite  <jennb@chromium.org>

        Reviewed by Dmitry Titov.

        Chromium: Update resource tracking when moving a frame between documents
        https://bugs.webkit.org/show_bug.cgi?id=48363

        * DumpRenderTree/chromium/WebViewHost.cpp:
        (WebViewHost::assignIdentifierToRequest):
        Always put resource id in map so we can make assumptions about its
        presence.
        (WebViewHost::removeIdentifierForRequest):
        Added.
        (WebViewHost::didFinishResourceLoad):
        (WebViewHost::didFailResourceLoad):
        Check existence of resource id before removing from map.
        * DumpRenderTree/chromium/WebViewHost.h:

git-svn-id: svn://svn.chromium.org/blink/trunk@71297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -941,9 +941,14 @@ void WebViewHost::didChangeLocationWithinPage(WebFrame* frame)
 
 void WebViewHost::assignIdentifierToRequest(WebFrame*, unsigned identifier, const WebURLRequest& request)
 {
-    if (!m_shell->shouldDumpResourceLoadCallbacks())
-        return;
+    ASSERT(!m_resourceIdentifierMap.contains(identifier));
     m_resourceIdentifierMap.set(identifier, descriptionSuitableForTestResult(request.url().spec()));
+}
+
+void WebViewHost::removeIdentifierForRequest(unsigned identifier)
+{
+    ASSERT(m_resourceIdentifierMap.contains(identifier));
+    m_resourceIdentifierMap.remove(identifier);
 }
 
 void WebViewHost::willSendRequest(WebFrame*, unsigned identifier, WebURLRequest& request, const WebURLResponse& redirectResponse)
@@ -1023,7 +1028,7 @@ void WebViewHost::didFinishResourceLoad(WebFrame*, unsigned identifier)
         printResourceDescription(identifier);
         fputs(" - didFinishLoading\n", stdout);
     }
-    m_resourceIdentifierMap.remove(identifier);
+    removeIdentifierForRequest(identifier);
 }
 
 void WebViewHost::didFailResourceLoad(WebFrame*, unsigned identifier, const WebURLError& error)
@@ -1034,7 +1039,7 @@ void WebViewHost::didFailResourceLoad(WebFrame*, unsigned identifier, const WebU
         fputs(webkit_support::MakeURLErrorDescription(error).c_str(), stdout);
         fputs("\n", stdout);
     }
-    m_resourceIdentifierMap.remove(identifier);
+    removeIdentifierForRequest(identifier);
 }
 
 void WebViewHost::didDisplayInsecureContent(WebFrame*)

third_party/WebKit/WebKit/chromium/public/WebFrameClient.h
@@ -241,6 +241,10 @@ public:
     virtual void assignIdentifierToRequest(
         WebFrame*, unsigned identifier, const WebURLRequest&) { }
 
+     // Remove the association between an identifier assigned to a request if
+     // the client keeps such an association.
+     virtual void removeIdentifierForRequest(unsigned identifier) { }
+
     // A request is about to be sent out, and the client may modify it.  Request
     // is writable, and changes to the URL, for example, will change the request
     // made.  If this request is the result of a redirect, then redirectResponse

third_party/WebKit/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -1390,9 +1390,13 @@ void FrameLoaderClientImpl::didTransferChildFrameToNewDocument(Page*)
     m_webFrame->setClient(newParent->client());
 }
 
-void FrameLoaderClientImpl::transferLoadingResourceFromPage(unsigned long, DocumentLoader*, const ResourceRequest&, Page*)
+void FrameLoaderClientImpl::transferLoadingResourceFromPage(unsigned long identifier, DocumentLoader* loader, const ResourceRequest& request, Page* oldPage)
 {
-    notImplemented();
+    assignIdentifierToInitialRequest(identifier, loader, request);
+
+    WebFrameImpl* oldWebFrame = WebFrameImpl::fromFrame(oldPage->mainFrame());
+    if (oldWebFrame && oldWebFrame->client())
+        oldWebFrame->client()->removeIdentifierForRequest(identifier);
 }
 
 PassRefPtr<Widget> FrameLoaderClientImpl::createPlugin(

third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.h
@@ -186,6 +186,7 @@ class WebViewHost : public WebKit::WebViewClient, public WebKit::WebFrameClient,
     virtual void didNavigateWithinPage(WebKit::WebFrame*, bool isNewNavigation);
     virtual void didChangeLocationWithinPage(WebKit::WebFrame*);
     virtual void assignIdentifierToRequest(WebKit::WebFrame*, unsigned identifier, const WebKit::WebURLRequest&);
+    virtual void removeIdentifierForRequest(unsigned identifier);
     virtual void willSendRequest(WebKit::WebFrame*, unsigned identifier, WebKit::WebURLRequest&, const WebKit::WebURLResponse&);
     virtual void didReceiveResponse(WebKit::WebFrame*, unsigned identifier, const WebKit::WebURLResponse&);
     virtual void didFinishResourceLoad(WebKit::WebFrame*, unsigned identifier);

commit 02cc63c2dc6111f5de5e7d565473157ca5b5526f

2010-11-09  Mihai Parparita  <mihaip@chromium.org>
        Reviewed by Tony Chang.

        [Chromium] http/tests/security/XFrameOptions fail with Chromium DRT
        https://bugs.webkit.org/show_bug.cgi?id=49286

        The tests in http/tests/security/XFrameOptions started to fail after
        r71297, since it changed the WebViewHost::assignIdentifierToRequest
        logic to always assign identifiers to requests, even if we're not going
        to be dumping resource load callbacks at that point. These tests
        only call LayoutTestController.dumpResourceLoadCallbacks after the
        request is started, thus their expectations have "<unknown>" as the
        identifier. Change WebViewHost to the previous behavior, which also
        match the other ports'.

        * DumpRenderTree/chromium/WebViewHost.cpp:
        (WebViewHost::assignIdentifierToRequest):
        (WebViewHost::removeIdentifierForRequest):

git-svn-id: svn://svn.chromium.org/blink/trunk@71699 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKitTools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -941,13 +941,14 @@ void WebViewHost::didChangeLocationWithinPage(WebFrame* frame)
 
 void WebViewHost::assignIdentifierToRequest(WebFrame*, unsigned identifier, const WebURLRequest& request)
 {
+     if (!m_shell->shouldDumpResourceLoadCallbacks())
+        return;
     ASSERT(!m_resourceIdentifierMap.contains(identifier));
     m_resourceIdentifierMap.set(identifier, descriptionSuitableForTestResult(request.url().spec()));
 }
 
 void WebViewHost::removeIdentifierForRequest(unsigned identifier)
 {
-    ASSERT(m_resourceIdentifierMap.contains(identifier));
     m_resourceIdentifierMap.remove(identifier);
 }
 