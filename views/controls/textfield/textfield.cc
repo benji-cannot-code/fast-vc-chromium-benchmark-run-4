352b7634002dc7a665002df0d2f4df5785606e82
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 18:55:49 +0000

    Implements vertical margins setter for textfield.
    After bug fix #r64550, changing horizontal margin doesn't change
    vertical margins. Unfortunately FindBar has used this bug, so explicit
    vertical setter was implemented to deal with this issue.
    
    BUG=chromium-os:8536
    TEST=In chrome use 'ctrl+f' to invoke find bar. Notice that the border is inplace.
    
    Review URL: http://codereview.chromium.org/4246002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65686 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/textfield.cc
+++ views/controls/textfield/textfield.cc
@@ -49,7 +49,9 @@ Textfield::Textfield()
       background_color_(SK_ColorWHITE),
       use_default_background_color_(true),
       num_lines_(1),
-      initialized_(false) {
+      initialized_(false),
+      horizontal_margins_were_set_(false),
+      vertical_margins_were_set_(false) {
   SetFocusable(true);
 }
 
@@ -65,7 +67,9 @@ Textfield::Textfield(StyleFlags style)
       background_color_(SK_ColorWHITE),
       use_default_background_color_(true),
       num_lines_(1),
-      initialized_(false) {
+      initialized_(false),
+      horizontal_margins_were_set_(false),
+      vertical_margins_were_set_(false) {
   SetFocusable(true);
 }
 
@@ -168,8 +172,18 @@ void Textfield::SetFont(const gfx::Font& font) {
 }
 
 void Textfield::SetHorizontalMargins(int left, int right) {
+  margins_.Set(margins_.top(), left, margins_.bottom(), right);
+  horizontal_margins_were_set_ = true;
   if (native_wrapper_)
-    native_wrapper_->SetHorizontalMargins(left, right);
+    native_wrapper_->UpdateHorizontalMargins();
+  PreferredSizeChanged();
+}
+
+void Textfield::SetVerticalMargins(int top, int bottom) {
+  margins_.Set(top, margins_.left(), bottom, margins_.right());
+  vertical_margins_were_set_ = true;
+  if (native_wrapper_)
+    native_wrapper_->UpdateVerticalMargins();
   PreferredSizeChanged();
 }
 
@@ -188,6 +202,22 @@ void Textfield::RemoveBorder() {
     native_wrapper_->UpdateBorder();
 }
 
+bool Textfield::GetHorizontalMargins(int* left, int* right) {
+  if (!horizontal_margins_were_set_)
+    return false;
+  *left = margins_.left();
+  *right = margins_.right();
+  return true;
+}
+
+bool Textfield::GetVerticalMargins(int* top, int* bottom) {
+  if (!vertical_margins_were_set_)
+    return false;
+  *top = margins_.top();
+  *bottom = margins_.bottom();
+  return true;
+}
+
 void Textfield::UpdateAllProperties() {
   if (native_wrapper_) {
     native_wrapper_->UpdateText();
@@ -198,6 +228,8 @@ void Textfield::UpdateAllProperties() {
     native_wrapper_->UpdateEnabled();
     native_wrapper_->UpdateBorder();
     native_wrapper_->UpdateIsPassword();
+    native_wrapper_->UpdateHorizontalMargins();
+    native_wrapper_->UpdateVerticalMargins();
   }
 }
 
