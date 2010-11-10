4a4a287a3b94a9dcd14579fd3c5c3c1c48d48fb2
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 21:44:11 +0000

    Don't allow exact keyword matches for most uses of the autocomplete classifier.  Patch by Naoki Takano (see http://codereview.chromium.org/4142006 ), r=me.
    BUG=60059
    TEST=Change a search engine keyword to "a", highlight text beginning with "a" in a webpage (e.g. "a movie"), right-click and select "Search Google for a movie".  The result should be a Google search page, not a result page for whatever search engine has the keyword "a".
    Review URL: http://codereview.chromium.org/4717001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65706 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_omnibox_apitest.cc
+++ chrome/browser/extensions/extension_omnibox_apitest.cc
@@ -94,7 +94,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxApiTest, MAYBE_Basic) {
   // it.
   {
     autocomplete_controller->Start(L"keywor", std::wstring(),
-                                   true, false, false);
+                                   true, false, true, false);
 
     WaitForAutocompleteDone(autocomplete_controller);
     EXPECT_TRUE(autocomplete_controller->done());
@@ -119,7 +119,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxApiTest, MAYBE_Basic) {
   // Test that our extension can send suggestions back to us.
   {
     autocomplete_controller->Start(L"keyword suggestio", std::wstring(),
-                                   true, false, false);
+                                   true, false, true, false);
 
     WaitForAutocompleteDone(autocomplete_controller);
     EXPECT_TRUE(autocomplete_controller->done());
@@ -168,7 +168,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxApiTest, MAYBE_Basic) {
   {
     ResultCatcher catcher;
     autocomplete_controller->Start(L"keyword command", std::wstring(),
-                                   true, false, false);
+                                   true, false, true, false);
     location_bar->AcceptInput();
     EXPECT_TRUE(catcher.GetNextResult()) << catcher.message();
   }
