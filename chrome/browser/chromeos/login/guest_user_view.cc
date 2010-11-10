2d2332af4c807cea9e13db5dc15c3cd45cbfa445
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 12:37:51 +0000

    Fixed fonts for buttons and labels on picture taking screen. Button with minimum width for guest user, new user and picture taking views.
    BUG=chromium-os:8930
    TEST=Check that font on buttons and labels on picture screen matches that for login screen. Buttons size should be the same too.
    
    Review URL: http://codereview.chromium.org/4684002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/guest_user_view.cc
+++ chrome/browser/chromeos/login/guest_user_view.cc
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 // Button with custom processing for Tab/Shift+Tab to select entries.
-class UserEntryButton : public views::NativeButton {
+class UserEntryButton : public login::WideButton {
  public:
   UserEntryButton(UserController* controller,
                   const std::wstring& label)
-      : NativeButton(controller, label),
+      : WideButton(controller, label),
         controller_(controller) {}
 
   // Overridden from views::View:
@@ -28,13 +28,13 @@ class UserEntryButton : public views::NativeButton {
       controller_->SelectUser(index);
       return true;
     }
-    return views::NativeButton::OnKeyPressed(e);
+    return WideButton::OnKeyPressed(e);
   }
 
   virtual bool SkipDefaultKeyEventProcessing(const views::KeyEvent& e) {
     if (e.GetKeyCode() == app::VKEY_TAB)
       return true;
-    return views::NativeButton::SkipDefaultKeyEventProcessing(e);
+    return WideButton::SkipDefaultKeyEventProcessing(e);
   }
 
  private:
@@ -66,7 +66,6 @@ void GuestUserView::RecreateFields() {
   submit_button_ = new UserEntryButton(
       user_controller_,
       l10n_util::GetString(IDS_ENTER_GUEST_SESSION_BUTTON));
-  CorrectNativeButtonFontSize(submit_button_);
   AddChildView(submit_button_);
   Layout();
   SchedulePaint();
@@ -105,13 +104,11 @@ void GuestUserView::OnLocaleChanged() {
 
 void GuestUserView::Layout() {
   gfx::Size submit_button_size = submit_button_->GetPreferredSize();
-  int submit_button_width = std::max(login::kButtonMinWidth,
-                                     submit_button_size.width());
-  int submit_button_x = (width() - submit_button_width) / 2;
+  int submit_button_x = (width() - submit_button_size.width()) / 2;
   int submit_button_y = (height() - submit_button_size.height()) / 2;
   submit_button_->SetBounds(submit_button_x,
                             submit_button_y,
-                            submit_button_width,
+                            submit_button_size.width(),
                             submit_button_size.height());
 }
 
--- chrome/browser/chromeos/login/helper.h
+++ chrome/browser/chromeos/login/helper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_HELPER_H_
 #pragma once
 
+#include "views/controls/button/native_button.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 class GURL;
@@ -19,7 +20,7 @@ class Size;
 }  // namespace gfx
 
 namespace views {
-class NativeButton;
+class Label;
 class Painter;
 class Textfield;
 class Throbber;
@@ -42,6 +43,9 @@ views::Painter* CreateBackgroundPainter();
 gfx::Rect CalculateScreenBounds(const gfx::Size& size);
 
 // Corrects font size for NativeButton control.
+void CorrectLabelFontSize(views::Label* label);
+
+// Corrects font size for NativeButton control.
 void CorrectNativeButtonFontSize(views::NativeButton* button);
 
 // Corrects font size for Textfield control.
@@ -58,9 +62,6 @@ namespace login {
 enum Command {
   SIGN_OUT,
 };
-
-// Minimal width for the button.
-const int kButtonMinWidth = 90;
 
 // Gap between edge and image view, and image view and controls.
 const int kBorderSize = 6;
@@ -84,6 +85,21 @@ const int kWizardScreenHeight = 450;
 
 const int kScreenCornerRadius = 10;
 const int kUserCornerRadius = 5;
+
+class WideButton : public views::NativeButton {
+ public:
+  WideButton(views::ButtonListener* listener, const std::wstring& text)
+      : NativeButton(listener, text) {
+    CorrectNativeButtonFontSize(this);
+  }
+
+  ~WideButton() {}
+
+ private:
+  virtual gfx::Size GetPreferredSize();
+
+  DISALLOW_COPY_AND_ASSIGN(WideButton);
+};
 
 }  // namespace login
 
