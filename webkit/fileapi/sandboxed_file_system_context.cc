1ecced880d6b1dd881e2c3b77e21a35c9dcde7bb
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 00:02:31 +0000

    FileSystem code cleanup 2nd cut - introduce SandboxedFileSystemOperation
    1. Introduced SandboxedFileSystemOperation.
    2. Factored out most of the PathManager/QuotaManager related code from
    FileSystemDispatcherHost to the SandboxedFileSystemOperation.
    
    BUG=60243
    TEST=none
    
    Review URL: http://codereview.chromium.org/4054003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65598 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/fileapi/sandboxed_file_system_context.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "webkit/fileapi/sandboxed_file_system_context.h"
+
+#include "base/file_path.h"
+#include "base/message_loop_proxy.h"
+#include "webkit/fileapi/file_system_path_manager.h"
+#include "webkit/fileapi/file_system_quota_manager.h"
+
+namespace fileapi {
+
+SandboxedFileSystemContext::SandboxedFileSystemContext(
+    scoped_refptr<base::MessageLoopProxy> file_message_loop,
+    const FilePath& profile_path,
+    bool is_incognito,
+    bool allow_file_access,
+    bool unlimited_quota)
+    : path_manager_(new FileSystemPathManager(
+          file_message_loop, profile_path, is_incognito, allow_file_access)),
+      quota_manager_(new FileSystemQuotaManager(allow_file_access,
+                                                unlimited_quota)) {
+}
+
+SandboxedFileSystemContext::~SandboxedFileSystemContext() {
+}
+
+void SandboxedFileSystemContext::Shutdown() {
+  path_manager_.reset();
+  quota_manager_.reset();
+}
+
+}  // namespace fileapi
