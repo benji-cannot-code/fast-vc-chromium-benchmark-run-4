3f9ed2dafd488095b9f71a823d5bbc5d35eeb708
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 22:20:32 +0000

    Fix a GTK critical exhibited by ExtensionApiTest.WindowOpen.
    The view already has a parent in some shutdown scenarios.
    gtk_box_pack_start: assertion `child->parent == NULL' failed
    
    	ExtensionInfoBarDelegate::CreateInfoBar() [0x7bdeea]
    	InfoBarContainerGtk::AddInfoBar() [0x7e67e8]
    	InfoBarContainerGtk::UpdateInfoBars() [0x7e68c5]
    	InfoBarContainerGtk::ChangeTabContents() [0x7e6b16]
    	BrowserWindowGtk::TabSelectedAt() [0x7a2241]
    	TabStripModel::ChangeSelectedContentsFrom() [0xae4523]
    	TabStripModel::DetachTabContentsAt() [0xae710a]
    	TabStripModel::Observe() [0xae71ec]
    	NotificationService::Notify() [0x14fe3bb]
    	TabContents::~TabContents() [0xacc83f]
    
    BUG=62606
    
    Review URL: http://codereview.chromium.org/4762001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/extension_infobar_gtk.h
+++ chrome/browser/gtk/extension_infobar_gtk.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_GTK_EXTENSION_INFOBAR_GTK_H_
 #pragma once
 
-#include <gtk/gtk.h>
-
 #include "chrome/browser/gtk/extension_view_gtk.h"
 #include "chrome/browser/gtk/infobar_gtk.h"
 #include "chrome/browser/extensions/extension_infobar_delegate.h"
