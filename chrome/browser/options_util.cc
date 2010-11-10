44d1331ba409ed92175f5b90e7910286b77578dd
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 21:53:29 +0000

    Temporarily allow I/O on the UI thread for OptionsUtil::ResolveMetricsReportingEnabled().
    BUG=62626
    TEST=No DCHECKs when toggling crash reporting in debug+official builds.
    Review URL: http://codereview.chromium.org/4771002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/options_util.cc
+++ chrome/browser/options_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/options_util.h"
 
+#include "base/thread_restrictions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_manager.h"
 #include "chrome/browser/download/download_prefs.h"
@@ -98,6 +99,10 @@ void OptionsUtil::ResetToDefaults(Profile* profile) {
 
 // static
 bool OptionsUtil::ResolveMetricsReportingEnabled(bool enabled) {
+  // GoogleUpdateSettings touches the disk from the UI thread. MetricsService
+  // also calls GoogleUpdateSettings below. http://crbug/62626
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   GoogleUpdateSettings::SetCollectStatsConsent(enabled);
   bool update_pref = GoogleUpdateSettings::GetCollectStatsConsent();
 
