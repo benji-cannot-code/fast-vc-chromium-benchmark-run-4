f6ec143bf3a831ec9357e7f48bde5d4e8ca64fdd
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-11 12:38:58 +0000

    2010-11-11  Kenneth Rohde Christiansen  <kenneth@webkit.org>
            Reviewed by Andreas Kling.
    
            Make it possible to override the visibleContentRect in the case the
            client is handling the scrolling. This is similar to what the iPhone does.
    
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::visibleContentRect):
            * platform/ScrollView.h:
            (WebCore::ScrollView::setActualVisibleContentRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ScrollView.h
+++ third_party/WebKit/WebCore/platform/ScrollView.h
@@ -127,8 +127,10 @@ public:
 
     // The visible content rect has a location that is the scrolled offset of the document. The width and height are the viewport width
     // and height. By default the scrollbars themselves are excluded from this rectangle, but an optional boolean argument allows them to be
-    // included.
+    // included. In the situation the client is responsible for the scrolling (ie. with a tiled backing store) it is possible to
+    // override the current visibleContentRect by using setActualVisibleContentRect, for instance when a pan ends.
     IntRect visibleContentRect(bool includeScrollbars = false) const;
+    void setActualVisibleContentRect(IntRect actualVisibleContentRect) { m_actualVisibleContentRect = actualVisibleContentRect; }
     int visibleWidth() const { return visibleContentRect().width(); }
     int visibleHeight() const { return visibleContentRect().height(); }
 
@@ -288,6 +290,7 @@ private:
     // whether it is safe to blit on scroll.
     bool m_canBlitOnScroll;
 
+    IntRect m_actualVisibleContentRect;
     IntSize m_scrollOffset; // FIXME: Would rather store this as a position, but we will wait to make this change until more code is shared.
     IntSize m_fixedLayoutSize;
     IntSize m_contentsSize;
