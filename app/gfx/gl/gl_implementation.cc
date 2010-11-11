09fb5947e4b643a9bf6fee245334ef5b5a8eaaad
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-11 21:27:50 +0000

    Prefer native library symbol lookup to the platform GetProcAddress.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/4791002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65854 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/gl/gl_implementation.cc
+++ app/gfx/gl/gl_implementation.cc
@@ -116,12 +116,6 @@ void SetGLGetProcAddressProc(GLGetProcAddressProc proc) {
 void* GetGLProcAddress(const char* name) {
   DCHECK(g_gl_implementation != kGLImplementationNone);
 
-  if (g_get_proc_address) {
-    void* proc = g_get_proc_address(name);
-    if (proc)
-      return proc;
-  }
-
   if (g_libraries) {
     for (size_t i = 0; i < g_libraries->size(); ++i) {
       void* proc = base::GetFunctionPointerFromNativeLibrary((*g_libraries)[i],
@@ -129,6 +123,12 @@ void* GetGLProcAddress(const char* name) {
       if (proc)
         return proc;
     }
+  }
+
+  if (g_get_proc_address) {
+    void* proc = g_get_proc_address(name);
+    if (proc)
+      return proc;
   }
 
   return NULL;
