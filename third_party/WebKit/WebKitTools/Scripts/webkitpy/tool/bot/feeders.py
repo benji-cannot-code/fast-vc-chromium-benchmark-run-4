21c792a59d0dd4c357f2a0057aefd8482beaf443
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-11 23:26:43 +0000

    2010-11-11  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Split out Bug, Attachment and CommitterValidator from bugzilla.py
            https://bugs.webkit.org/show_bug.cgi?id=49403
    
            CommitterValidator really had nothing to do with bugzilla.py.
            I've put it next to committers.py for now since its rather tied to that file.
    
            * Scripts/webkitpy/common/config/committervalidator.py: Added.
            * Scripts/webkitpy/common/config/committervalidator_unittest.py: Added.
            * Scripts/webkitpy/common/net/bugzilla/__init__.py:
            * Scripts/webkitpy/common/net/bugzilla/attachment.py: Added.
            * Scripts/webkitpy/common/net/bugzilla/bug.py: Added.
            * Scripts/webkitpy/common/net/bugzilla/bug_unittest.py: Added.
            * Scripts/webkitpy/common/net/bugzilla/bugzilla.py:
            * Scripts/webkitpy/common/net/bugzilla/bugzilla_unittest.py:
            * Scripts/webkitpy/tool/bot/feeders.py:
            * Scripts/webkitpy/tool/commands/queues.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/feeders.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/tool/bot/feeders.py
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from webkitpy.common.config.committervalidator import CommitterValidator
 from webkitpy.common.system.deprecated_logging import log
-from webkitpy.common.net.bugzilla import CommitterValidator
 from webkitpy.tool.grammar import pluralize
 
 
