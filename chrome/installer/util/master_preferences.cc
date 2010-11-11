44a07c3f31381b86bd64738016b670c7c2c6b07a
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-11 03:45:31 +0000

    Preferences: remove log spam.
    The common case is for this file to not be available; we don't need
    to report it as an error each time.
    
    I improved this in r59984, but it regressed in r64945.
    
    Review URL: http://codereview.chromium.org/4740003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65770 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/master_preferences.cc
+++ chrome/installer/util/master_preferences.cc
@@ -53,10 +53,9 @@ std::vector<GURL> GetNamedList(const char* name,
 DictionaryValue* ParseDistributionPreferences(
     const FilePath& master_prefs_path) {
   std::string json_data;
-  if (!file_util::ReadFileToString(master_prefs_path, &json_data)) {
-    LOG(WARNING) << "Failed to read master prefs file. ";
+  if (!file_util::ReadFileToString(master_prefs_path, &json_data))
     return NULL;
-  }
+
   JSONStringValueSerializer json(json_data);
   std::string error;
   scoped_ptr<Value> root(json.Deserialize(NULL, &error));
@@ -135,8 +134,7 @@ MasterPreferences::MasterPreferences(const CommandLine& cmd_line)
 MasterPreferences::MasterPreferences(const FilePath& prefs_path)
     : distribution_(NULL), preferences_read_from_file_(false) {
   master_dictionary_.reset(ParseDistributionPreferences(prefs_path));
-  LOG_IF(ERROR, !master_dictionary_.get()) << "Failed to parse "
-      << prefs_path.value();
+
   if (!master_dictionary_.get()) {
     master_dictionary_.reset(new DictionaryValue());
   } else {
