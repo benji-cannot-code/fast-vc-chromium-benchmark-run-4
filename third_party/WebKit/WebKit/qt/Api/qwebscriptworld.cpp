280f9fdd00ab3684657b0ba9482b9ca0834695f7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-11 14:50:51 +0000

    2010-11-11  Yi Shen  <yi.4.shen@nokia.com>
            Reviewed by Andreas Kling.
    
            [Qt] QWebScriptWorld::world() may crash
            https://bugs.webkit.org/show_bug.cgi?id=49342
    
            * Api/qwebscriptworld.cpp:
            (QWebScriptWorld::world):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/Api/qwebscriptworld.cpp
+++ third_party/WebKit/WebKit/qt/Api/qwebscriptworld.cpp
@@ -51,7 +51,7 @@ QWebScriptWorld &QWebScriptWorld::operator=(const QWebScriptWorld& other)
 
 DOMWrapperWorld* QWebScriptWorld::world() const
 {
-    return d->world.get();
+    return d ? d->world.get() : 0;
 }
 
 /*!
