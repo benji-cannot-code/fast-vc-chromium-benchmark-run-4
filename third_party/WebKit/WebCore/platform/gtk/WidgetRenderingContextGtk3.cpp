37d8e77662230e7735f5f7c3ef64a73246d39375
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-11 09:44:45 +0000

    2010-11-11  Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Nikolas Zimmermann.
    
            Fixed WebKitGTK gtk3 compilation.
    
            * platform/gtk/WidgetRenderingContextGtk3.cpp:
            (WebCore::WidgetRenderingContext::WidgetRenderingContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/gtk/WidgetRenderingContextGtk3.cpp
+++ third_party/WebKit/WebCore/platform/gtk/WidgetRenderingContextGtk3.cpp
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-WidgetRenderingContext::WidgetRenderingContext(GraphicsContext* graphicsContext, const IntRect& targetRect)
-    : m_graphicsContext(graphicsContext)
+WidgetRenderingContext::WidgetRenderingContext(GraphicsContext* context, const IntRect& targetRect)
+    : m_graphicsContext(context)
     , m_targetRect(targetRect)
     , m_paintRect(targetRect)
     , m_hadError(false)
