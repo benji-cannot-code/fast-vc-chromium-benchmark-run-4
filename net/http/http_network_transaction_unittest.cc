889dc4429233ea808ec0329c34d543f440100408
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-11 00:24:40 +0000

    Change the way request headers are logged to the NetLog to ensure that the complete request is logged, not just the extra_headers. Before I broke things, HttpNetworkTransaction was attempting to do the logging, but after some refactors it no longer has access to the final request.  SpdySession already logs correctly in the SPDY case, and I modified HttpBasicStream to log in the HTTP case.
    Added unit tests is both HttpNetworkTransactionTest and 
    SpdyNetworkTransactionTest.
    
    BUG=48962
    TEST=net_unittests --gtest_filter=HttpNetworkTransactionTest.SimpleGET\*
    
    Review URL: http://codereview.chromium.org/4644003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65736 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_auth_handler_mock.h"
 #include "net/http/http_auth_handler_ntlm.h"
 #include "net/http/http_basic_stream.h"
+#include "net/http/http_net_log_params.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_network_session_peer.h"
 #include "net/http/http_stream.h"
@@ -157,6 +158,7 @@ class HttpNetworkTransactionTest : public PlatformTest {
     TestCompletionCallback callback;
 
     CapturingBoundNetLog log(CapturingNetLog::kUnbounded);
+    EXPECT_TRUE(log.bound().IsLoggingAllEvents());
     int rv = trans->Start(&request, &callback, log.bound());
     EXPECT_EQ(ERR_IO_PENDING, rv);
 
@@ -179,6 +181,14 @@ class HttpNetworkTransactionTest : public PlatformTest {
         log.entries(), pos,
         NetLog::TYPE_HTTP_TRANSACTION_READ_RESPONSE_HEADERS,
         NetLog::PHASE_NONE);
+
+    CapturingNetLog::Entry entry = log.entries()[pos];
+    NetLogHttpRequestParameter* request_params =
+        static_cast<NetLogHttpRequestParameter*>(entry.extra_parameters.get());
+    EXPECT_EQ("GET / HTTP/1.1\r\n", request_params->GetLine());
+    EXPECT_EQ("Host: www.google.com\r\n"
+              "Connection: keep-alive\r\n\r\n",
+              request_params->GetHeaders().ToString());
 
     return out;
   }
