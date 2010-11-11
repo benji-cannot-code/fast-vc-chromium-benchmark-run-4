21cdae5ce720bb4ad451f0c3615fd222a309df72
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-11 12:32:29 +0000

    2010-11-11  Andreas Kling  <kling@webkit.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            FrameView: Don't clip to visibleContentRect in paintEntireContents mode
            https://bugs.webkit.org/show_bug.cgi?id=49375
    
            Use paintEntireContents in combination with tiling, allowing to
            actually set visibleContentRect to something different from
            the actual contents size.
    
            * Api/qgraphicswebview.cpp:
            (QGraphicsWebViewPrivate::updateResizesToContentsForPage):
            * WebCoreSupport/FrameLoaderClientQt.cpp:
            (WebCore::FrameLoaderClientQt::transitionToCommittedForNewPage):
    2010-11-11  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            FrameView: Don't clip to visibleContentRect in paintEntireContents mode
            https://bugs.webkit.org/show_bug.cgi?id=49375
    
            In paintEntireContents mode (used in combination with tiling) we
            cannot clip to the actual visibleContentRect as the clipped regions
            will not be updated again on scroll. If paintsEntireContents is
            enabled, return the actual content rect.
    
            * page/FrameView.cpp:
            (WebCore::FrameView::windowClipRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -281,6 +281,10 @@ void FrameLoaderClientQt::transitionToCommittedForNewPage()
                         preferredLayoutSize.isValid(),
                         hScrollbar, hLock,
                         vScrollbar, vLock);
+
+    bool isMainFrame = m_frame == m_frame->page()->mainFrame();
+    if (isMainFrame && page->d->client)
+        m_frame->view()->setPaintsEntireContents(page->d->client->viewResizesToContentsEnabled());
 }
 
 void FrameLoaderClientQt::dispatchDidBecomeFrameset(bool)
