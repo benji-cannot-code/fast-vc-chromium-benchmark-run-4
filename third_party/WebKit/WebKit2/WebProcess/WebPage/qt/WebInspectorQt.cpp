c7779219ccbcf5e42f227f7755066ef92027d2fc
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 13:45:45 +0000

    2010-11-12  Benjamin Poulain  <benjamin.poulain@nokia.com>
            Unreviewed fix after r71857.
    
            * UIProcess/qt/WebInspectorProxyQt.cpp:
            * WebProcess/WebPage/qt/WebInspectorQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/qt/WebInspectorQt.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/qt/WebInspectorQt.cpp
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "WebInspector.h"
+#include "WebInspector.h"
 
-#import <wtf/text/WTFString.h>
+#include <wtf/text/WTFString.h>
 
 #define DISABLE_NOT_IMPLEMENTED_WARNINGS 1
 #include "NotImplemented.h"
