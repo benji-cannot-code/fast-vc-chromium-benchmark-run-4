02743fe460ebe84a440abafa5b47edcb3ae16b65
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 13:34:18 +0000

    2010-11-12  Andrey Kosyakov  <caseq@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: [refactoring] do not use absolute positioning for tabbed panes
            https://bugs.webkit.org/show_bug.cgi?id=49439
    
            * inspector/front-end/TabbedPane.js:
            (WebInspector.TabbedPane):
            (WebInspector.TabbedPane.prototype.appendTab):
            * inspector/front-end/inspector.css:
            (.resource-view.visible):
            (.resource-view .tabbed-pane-header):
            (.resource-view-cookies):
            (.tabbed-pane):
            (.tabbed-pane-content):
            * inspector/front-end/networkPanel.css:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/TabbedPane.js
+++ third_party/WebKit/WebCore/inspector/front-end/TabbedPane.js
@@ -32,10 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.TabbedPane = function(element)
 {
     this.element = element || document.createElement("div");
-
-    this.tabsElement = document.createElement("div");
-    this.tabsElement.className = "tabbed-pane-header";
-    this.element.appendChild(this.tabsElement);
+    this.element.addStyleClass("tabbed-pane");
+    this.tabsElement = this.element.createChild("div", "tabbed-pane-header");
+    this.contentElement = this.element.createChild("div", "tabbed-pane-content");
 
     this._tabObjects = {};
 }
@@ -47,7 +46,7 @@ WebInspector.TabbedPane.prototype = {
         tabElement.textContent = tabTitle;
         tabElement.addEventListener("click", tabClickListener, false);
         this.tabsElement.appendChild(tabElement);
-        this.element.appendChild(contentElement);
+        this.contentElement.appendChild(contentElement);
         this._tabObjects[id] = {tab: tabElement, content: contentElement};
     },
     
