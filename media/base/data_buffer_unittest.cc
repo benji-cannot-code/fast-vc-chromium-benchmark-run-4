2070b2777a58040158dfa83217c6abea2efbdd26
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 20:44:55 +0000

    Remove WriteableBuffer interface as DataBuffer is the only implementation.
    Turns out we never needed multiple WritableBuffer implementations.
    
    BUG=54110
    TEST=media_unittests
    
    Review URL: http://codereview.chromium.org/4824002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65988 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/data_buffer_unittest.cc
+++ media/base/data_buffer_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,7 +36,7 @@ TEST(DataBufferTest, StreamSampleImpl) {
   EXPECT_FALSE(buffer->IsDiscontinuous());
 }
 
-TEST(DataBufferTest, Ctors) {
+TEST(DataBufferTest, Constructors) {
   const size_t kTestSize = 10;
 
   scoped_refptr<DataBuffer> buffer(new DataBuffer(0));
@@ -47,7 +47,7 @@ TEST(DataBufferTest, Ctors) {
   EXPECT_EQ(kTestSize, buffer2->GetBufferSize());
 }
 
-TEST(DataBufferTest, WritableBufferImpl) {
+TEST(DataBufferTest, ReadingWriting) {
   const char kData[] = "hello";
   const size_t kDataSize = arraysize(kData);
   const char kNewData[] = "chromium";
