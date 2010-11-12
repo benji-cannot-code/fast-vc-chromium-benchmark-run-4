7fe56aed07f6885e306fe1a1611b349c54cc9ac5
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 21:50:56 +0000

    Change the FF CEEE to create its chrome frame instance initially hidden, and make it visible only once the content is done loading.  This avoids any flicker at startup, and fixes a bug where the chrome frame instance is removed after a print preview.
    Also added support to chrome frame to send an uninitialized ready state
    when its being torn down.
    
    BUG=62979
    TEST=Make sure that there is no UI flicker on chrome frame startup, and make
    sure that the extenions works correct after showing print preview and closing
    it.
    
    Review URL: http://codereview.chromium.org/4882004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66002 0039d316-1c4b-4281-b951-d872f2087c98

--- ceee/firefox/content/userscript_api.js
+++ ceee/firefox/content/userscript_api.js
@@ -652,9 +652,6 @@ UserScriptManager.prototype.loadUserScripts = function(manifest) {
         pathPattern: new RegExp(pathPattern)
       };
       patterns.push(pattern);
-
-      ceee.logInfo('loadUserScripts: url=' + spec +
-                   ' pattern=' + CEEE_json.encode(pattern));
     }
 
     script.patterns = patterns;
