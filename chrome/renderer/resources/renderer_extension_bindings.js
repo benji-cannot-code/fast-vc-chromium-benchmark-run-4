0d4706ee3d2fffe6f3aa9a7683d4677042dec166
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 20:08:19 +0000

    Sets lastError when trying to connect or send a request to an invalid extension.
    BUG=61998
    TEST=ExtensionApiTest.Messaging
    
    Review URL: http://codereview.chromium.org/4708006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65977 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/renderer_extension_bindings.js
+++ chrome/renderer/resources/renderer_extension_bindings.js
@@ -56,7 +56,7 @@ var chrome = chrome || {};
     });
     PortAddRef(portId);
     return port;
-  }
+  };
 
   // Called by native code when a channel has been opened to this context.
   chromeHidden.Port.dispatchOnConnect = function(portId, channelName, tab,
@@ -152,7 +152,7 @@ var chrome = chrome || {};
   chrome.Port.prototype.disconnect = function() {
     delete ports[this.portId_];
     CloseChannel(this.portId_, true);
-  }
+  };
 
   // This function is called on context initialization for both content scripts
   // and extension contexts.
@@ -208,6 +208,11 @@ var chrome = chrome || {};
       var port = chrome.extension.connect(targetId,
                                           {name: chromeHidden.kRequestChannel});
       port.postMessage(request);
+      port.onDisconnect.addListener(function() {
+        // For onDisconnects, we only notify the callback if there was an error
+        if (chrome.extension.lastError && responseCallback)
+          responseCallback();
+      });
       port.onMessage.addListener(function(response) {
         if (responseCallback)
           responseCallback(response);
@@ -232,7 +237,7 @@ var chrome = chrome || {};
     if (warnOnPrivilegedApiAccess) {
       setupApiStubs();
     }
-  }
+  };
 
   var notSupportedSuffix = " is not supported in content scripts. " +
       "See the content scripts documentation for more details.";
@@ -300,7 +305,6 @@ var chrome = chrome || {};
       "extension.getExtensionTabs",
       "extension.getToolstrips",
       "extension.getViews",
-      "extension.lastError",
       "extension.onConnectExternal",
       "extension.onRequestExternal",
       "extension.setUpdateUrlData",
--- chrome/test/data/extensions/api_test/messaging/connect/page.js
+++ chrome/test/data/extensions/api_test/messaging/connect/page.js
@@ -1,19 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 JSON.parse = function() {
   return "JSON.parse clobbered by content script.";
-}
+};
 
 JSON.stringify = function() {
   return "JSON.stringify clobbered by content script.";
-}
+};
 
 Array.prototype.toJSON = function() {
   return "Array.prototype.toJSON clobbered by content script.";
-}
+};
 
 Object.prototype.toJSON = function() {
   return "Object.prototype.toJSON clobbered by content script.";
-}
+};
 
 // For complex connect tests.
 chrome.extension.onConnect.addListener(function(port) {
@@ -32,6 +32,10 @@ chrome.extension.onConnect.addListener(function(port) {
       window.location = "about:blank";
     } else if (msg.testPortName) {
       port.postMessage({portName:port.name});
+    } else if (msg.testSendRequestFromTabError) {
+      testSendRequestFromTabError();
+    } else if (msg.testConnectFromTabError) {
+      testConnectFromTabError();
     }
   });
 });
@@ -55,6 +59,24 @@ function testSendRequestFromTab() {
       console.log('testSendRequestFromTab sent');
       chrome.extension.sendRequest({step: 2});
     }
+  });
+}
+
+// Tests sendRequest to an invalid extension.
+function testSendRequestFromTabError() {
+  // try sending a request to a bad extension id
+  chrome.extension.sendRequest("bad-extension-id", {m: 1}, function(response) {
+    var success = (response === undefined && chrome.extension.lastError);
+    chrome.extension.sendRequest({success: success});
+  });
+}
+
+// Tests connecting to an invalid extension.
+function testConnectFromTabError() {
+  var port = chrome.extension.connect("bad-extension-id");
+  port.onDisconnect.addListener(function() {
+    var success = (chrome.extension.lastError ? true : false);
+    chrome.extension.sendRequest({success: success});
   });
 }
 
