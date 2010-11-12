benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a1f42c43f2c0fb8c71b3132fb07f448820bb6cfc

2010-11-10  Kenneth Russell  <kbr@google.com>
        Reviewed by James Robinson.

        [chromium] Clean up gfx::GetGLImplementation calls in WebGraphicsContext3DDefaultImpl
        https://bugs.webkit.org/show_bug.cgi?id=49336

        * src/WebGraphicsContext3DDefaultImpl.cpp:
        (WebKit::WebGraphicsContext3DDefaultImpl::initialize):
        (WebKit::WebGraphicsContext3DDefaultImpl::reshape):
        (WebKit::WebGraphicsContext3DDefaultImpl::readBackFramebuffer):
        (WebKit::WebGraphicsContext3DDefaultImpl::getIntegerv):


git-svn-id: svn://svn.chromium.org/blink/trunk@71793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.cpp
@@ -171,7 +171,9 @@ bool WebGraphicsContext3DDefaultImpl::initialize(WebGraphicsContext3D::Attribute
 
     validateAttributes();
 
-    if (gfx::GetGLImplementation() != gfx::kGLImplementationEGLGLES2) {
+    m_isGLES2 = gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2;
+
+    if (m_isGLES2) {
         glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
         glEnable(GL_POINT_SPRITE);
     }
@@ -184,7 +186,6 @@ bool WebGraphicsContext3DDefaultImpl::initialize(WebGraphicsContext3D::Attribute
     glGenFramebuffersEXT(1, &m_copyTextureToParentTextureFBO);
 
     m_initialized = true;
-    m_isGLES2 = gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2;
     return true;
 }
 
@@ -361,7 +362,7 @@ void WebGraphicsContext3DDefaultImpl::reshape(int width, int height)
         if (m_attributes.stencil && m_attributes.depth)
             internalDepthStencilFormat = GL_DEPTH24_STENCIL8_EXT;
         else {
-            if (gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2)
+            if (m_isGLES2)
                 internalDepthStencilFormat = GL_DEPTH_COMPONENT16;
             else
                 internalDepthStencilFormat = GL_DEPTH_COMPONENT;
@@ -541,7 +542,7 @@ bool WebGraphicsContext3DDefaultImpl::readBackFramebuffer(unsigned char* pixels,
         mustRestorePackAlignment = true;
     }
 
-    if (gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2) {
+    if (m_isGLES2) {
         // FIXME: consider testing for presence of GL_OES_read_format
         // and GL_EXT_read_format_bgra, and using GL_BGRA_EXT here
         // directly.
@@ -991,7 +992,7 @@ void WebGraphicsContext3DDefaultImpl::getFramebufferAttachmentParameteriv(unsign
 void WebGraphicsContext3DDefaultImpl::getIntegerv(unsigned long pname, int* value)
 {
     makeContextCurrent();
-    if (gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2) {
+    if (m_isGLES2) {
         glGetIntegerv(pname, value);
         return;
     }

commit 46836d51f612bf5879bc3f0579853f3f34055441

2010-11-12  Zhenyao Mo  <zmo@google.com>
        Reviewed by James Robinson.

        WebGraphicsContext3DDefaultImpl does not run on top of OpenGL ES 2.0 implementations
        https://bugs.webkit.org/show_bug.cgi?id=48282

        Simple fix for a regression introduced in r71793.

        * src/WebGraphicsContext3DDefaultImpl.cpp:
        (WebKit::WebGraphicsContext3DDefaultImpl::initialize):


git-svn-id: svn://svn.chromium.org/blink/trunk@71942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/WebKit/chromium/src/WebGraphicsContext3DDefaultImpl.cpp
@@ -173,7 +173,7 @@ bool WebGraphicsContext3DDefaultImpl::initialize(WebGraphicsContext3D::Attribute
 
     m_isGLES2 = gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2;
 
-    if (m_isGLES2) {
+    if (!m_isGLES2) {
         glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
         glEnable(GL_POINT_SPRITE);
     }