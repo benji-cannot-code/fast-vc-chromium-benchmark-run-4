ea1966a4449086d265866b0c8283200fc3e3439d
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 00:28:38 +0000

    More Windows build fixing. Change another #import to a #include, and remove an unneeded namespace.
    * UIProcess/win/WebInspectorProxyWin.cpp:
    * WebProcess/WebPage/win/WebInspectorWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebInspectorProxyWin.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebInspectorProxyWin.cpp
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DISABLE_NOT_IMPLEMENTED_WARNINGS 1
 #include "NotImplemented.h"
 
-using namespace WebCore;
-
 namespace WebKit {
 
 WebPageProxy* WebInspectorProxy::platformCreateInspectorPage()
