34cb9193cb84328be56f484dbcbf58137629a866
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 21:07:26 +0000

    [Qt] Need to implement WebKit::pluginWillHandleLoadError for WebKit2 https://bugs.webkit.org/show_bug.cgi?id=48764
    WebKit/qt:
    
    Reviewed by Andreas Kling.
    
    Add the missing error.
    
    * WebCoreSupport/FrameLoaderClientQt.cpp:
    (WebCore::FrameLoaderClientQt::pluginWillHandleLoadError):
    
    WebKit2:
    
    Reviewed by NOBODY (OOPS!).
    
    Add the missing error to Qt support.
    
    * WebProcess/WebCoreSupport/qt/WebErrorsQt.cpp:
    (WebKit::pluginWillHandleLoadError):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/qt/WebErrorsQt.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/qt/WebErrorsQt.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebErrors.h"
 
-#include "NotImplemented.h"
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/ResourceResponse.h>
 
@@ -47,6 +46,7 @@ enum {
     WebKitErrorCannotFindPlugIn =                               200,
     WebKitErrorCannotLoadPlugIn =                               201,
     WebKitErrorJavaUnavailable =                                202,
+    WebKitErrorPluginWillHandleLoad =                           203
 };
 
 ResourceError cancelledError(const ResourceRequest& request)
@@ -87,10 +87,10 @@ ResourceError fileDoesNotExistError(const ResourceResponse& response)
                          QCoreApplication::translate("QWebFrame", "File does not exist", 0, QCoreApplication::UnicodeUTF8));
 }
 
-ResourceError pluginWillHandleLoadError(const ResourceResponse&)
+ResourceError pluginWillHandleLoadError(const ResourceResponse& response)
 {
-    notImplemented();
-    return ResourceError();
+    return ResourceError("WebKit", WebKitErrorPluginWillHandleLoad, response.url().string(),
+                         QCoreApplication::translate("QWebFrame", "Loading is handled by the media engine", 0, QCoreApplication::UnicodeUTF8));
 }
 
 } // namespace WebKit
