0ebae7141d4b21e4ba5992b726a4ee4e91fcc07d
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 12:03:54 +0000

    implement openssl symmetric key add AES derived key test
    Also includes some build fixes.
    
    BUG=None
    TEST=./out/Debug/base_unittests --gtest_filter=SymmetricKey*
    
    Review URL: http://codereview.chromium.org/4691003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65936 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/openssl_util.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/openssl_util.h"
+
+#include <openssl/err.h>
+
+#include "base/logging.h"
+
+namespace base {
+
+void ClearOpenSSLERRStack() {
+  if (logging::DEBUG_MODE && VLOG_IS_ON(1)) {
+    int error_num = ERR_get_error();
+    if (error_num == 0)
+      return;
+
+    DVLOG(1) << "OpenSSL ERR_get_error stack:";
+    char buf[140];
+    do {
+      ERR_error_string_n(error_num, buf, arraysize(buf));
+      DVLOG(1) << "\t" << error_num << ": " << buf;
+      error_num = ERR_get_error();
+    } while (error_num != 0);
+  } else {
+    ERR_clear_error();
+  }
+}
+
+}  // namespace base
