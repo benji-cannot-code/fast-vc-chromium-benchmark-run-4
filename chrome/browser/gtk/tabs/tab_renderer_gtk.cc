0dd85ab5749aa6b0d10c0583830f7d8fa2cdaf37
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 19:40:52 +0000

    [gtk] make tab title font irrespective of display DPI.
    The font size is changed from 9 to 13 in order to offset the new correction. The font has the same same pixel size that was displayed before on my 101 DPI display.
    
    BUG=62536
    TEST=using Xephyr -dpi XXX
    
    Review URL: http://codereview.chromium.org/4837002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65971 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/tabs/tab_renderer_gtk.cc
+++ chrome/browser/gtk/tabs/tab_renderer_gtk.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_service.h"
 #include "gfx/canvas_skia_paint.h"
 #include "gfx/favicon_size.h"
+#include "gfx/platform_font_gtk.h"
 #include "gfx/skbitmap_operations.h"
 #include "grit/app_resources.h"
 #include "grit/generated_resources.h"
@@ -1071,10 +1072,11 @@ void TabRendererGtk::InitResources() {
   LoadTabImages();
 
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-  // Force the font size to 9pt, which matches Windows' default font size
-  // (taken from the system).
   const gfx::Font& base_font = rb.GetFont(ResourceBundle::BaseFont);
-  title_font_ = new gfx::Font(base_font.GetFontName(), 9);
+  // Dividing by the pango scale factor maintains an absolute pixel size across
+  // all DPIs.
+  int size = static_cast<int>(13 / gfx::PlatformFontGtk::GetPangoScaleFactor());
+  title_font_ = new gfx::Font(base_font.GetFontName(), size);
   title_font_height_ = title_font_->GetHeight();
 
   crashed_fav_icon = rb.GetBitmapNamed(IDR_SAD_FAVICON);
