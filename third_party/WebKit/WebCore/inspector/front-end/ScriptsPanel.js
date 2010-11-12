c3f0ce1d9008968f83ee2ea742655c634d56392a
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 16:41:50 +0000

    2010-11-12  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: add more event listener breakpoint types, add support for regular breakpoint hit state, beautify hit rendering.
            https://bugs.webkit.org/show_bug.cgi?id=49130
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/BreakpointManager.js:
            (WebInspector.BreakpointManager.prototype.debuggerPaused):
            (WebInspector.Breakpoint.jsBreakpointId):
            (WebInspector.Breakpoint.prototype.get id):
            * inspector/front-end/BreakpointsSidebarPane.js:
            (WebInspector.EventListenerBreakpointsSidebarPane):
            (WebInspector.EventListenerBreakpointsSidebarPane.prototype._createCategory):
            * inspector/front-end/CallStackSidebarPane.js:
            (WebInspector.CallStackSidebarPane.prototype._breakpointHit): don't call populateStatusMessage for JS breakpoints
            since there is no specific message in that case.
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel):
            (WebInspector.ScriptsPanel.prototype.reset):
            * inspector/front-end/inspector.css:
            (#elements-sidebar):
            (.pane > .body):
            (#scripts-sidebar):
            (li.breakpoint-hit .breakpoint-hit-marker):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
@@ -136,8 +136,8 @@ WebInspector.ScriptsPanel = function()
     if (Preferences.nativeInstrumentationEnabled) {
         this.sidebarPanes.domBreakpoints = WebInspector.createDOMBreakpointsSidebarPane();
         this.sidebarPanes.xhrBreakpoints = WebInspector.createXHRBreakpointsSidebarPane();
+        this.sidebarPanes.eventListenerBreakpoints = new WebInspector.EventListenerBreakpointsSidebarPane();
     }
-    this.sidebarPanes.eventListenerBreakpoints = new WebInspector.EventListenerBreakpointsSidebarPane();
 
     this.sidebarPanes.workers = new WebInspector.WorkersSidebarPane();
 
@@ -463,8 +463,8 @@ WebInspector.ScriptsPanel.prototype = {
             if (Preferences.nativeInstrumentationEnabled) {
                 this.sidebarPanes.domBreakpoints.reset();
                 this.sidebarPanes.xhrBreakpoints.reset();
+                this.sidebarPanes.eventListenerBreakpoints.reset();
             }
-            this.sidebarPanes.eventListenerBreakpoints.reset();
             this.sidebarPanes.workers.reset();
         }
     },
