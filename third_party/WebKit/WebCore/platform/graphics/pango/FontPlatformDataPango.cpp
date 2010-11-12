04899de862b29a944788dd9648d96b21e22d80d6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-12 19:37:14 +0000

    2010-11-12  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Martin Robinson.
    
            [GTK] Remove unnecessary header for FontPlatformDataPango.cpp
            https://bugs.webkit.org/show_bug.cgi?id=46029
    
            Remove unnecessary header includes.
            No new tests, as there's no functionality.
    
            * platform/graphics/pango/FontPlatformDataPango.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@71933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/pango/FontPlatformDataPango.cpp
+++ third_party/WebKit/WebCore/platform/graphics/pango/FontPlatformDataPango.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cairo-ft.h>
 #include <pango/pangofc-fontmap.h>
 #endif
-#include <gtk/gtk.h>
 
 namespace WebCore {
 
