d5841eb94996565102fe4a26de628811bc5e1d49
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 21:23:17 +0000

    Fix flakiness
    Fix expectations and check for locked workstation to make
    tests depending on user input non-flaky.
    
    BUG=47596,26549
    TEST=FullTabUITest.CtrlN,FullTabUITest.KeyboardBackForward
    
    Review URL: http://codereview.chromium.org/4866002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65994 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/ui_test.cc
+++ chrome_frame/test/ui_test.cc
@@ -68,9 +68,12 @@ TEST_P(FullTabUITest, KeyboardInput) {
 }
 
 // Tests keyboard shortcuts for back and forward.
-// Marking this test FLAKY as it fails at times on the buildbot.
-// http://code.google.com/p/chromium/issues/detail?id=26549
-TEST_P(FullTabUITest, FLAKY_KeyboardBackForward) {
+TEST_P(FullTabUITest, KeyboardBackForward) {
+  if (IsWorkstationLocked()) {
+    LOG(ERROR) << "This test cannot be run in a locked workstation.";
+    return;
+  }
+
   std::wstring page1 = GetSimplePageUrl();
   std::wstring page2 = GetLinkPageUrl();
   bool in_cf = GetParam().invokes_cf();
@@ -90,7 +93,7 @@ TEST_P(FullTabUITest, FLAKY_KeyboardBackForward) {
   EXPECT_CALL(ie_mock_, OnLoad(in_cf, StrEq(page2)))
       .WillOnce(testing::DoAll(
           SetFocusToRenderer(&ie_mock_),
-          DelaySendScanCode(&loop_, 500, bkspace, simulate_input::NONE)));
+          DelaySendScanCode(&loop_, 1000, bkspace, simulate_input::NONE)));
 
   EXPECT_CALL(ie_mock_, OnLoad(in_cf, StrEq(page1)))
       .WillOnce(testing::DoAll(
@@ -136,7 +139,9 @@ TEST_P(FullTabUITest, CtrlN) {
       .WillOnce(testing::Return());
 
   EXPECT_CALL(win_observer_mock, OnWindowClose(_))
-      .WillOnce(CloseBrowserMock(&ie_mock_));
+      .Times(testing::AtMost(2))
+      .WillOnce(CloseBrowserMock(&ie_mock_))
+      .WillOnce(testing::Return());
 
   LaunchIEAndNavigate(GetSimplePageUrl());
 }
