c9d6133e095f8b23e1c41a7005ec28247b5bd979
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-12 23:21:40 +0000

    Mark snapshot tests as FAILS on Linux. Review URL: http://codereview.chromium.org/4923001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/automation_proxy_uitest.cc
+++ chrome/test/automation/automation_proxy_uitest.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/net/url_request_slow_http_job.h"
 #include "chrome/browser/view_ids.h"
-#include "chrome/common/automation_messages.h"
+//#include "chrome/common/automation_messages.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/json_value_serializer.h"
@@ -1562,9 +1562,15 @@ class AutomationProxySnapshotTest : public UITest {
   ScopedTempDir snapshot_dir_;
 };
 
+// See http://crbug.com/63022.
+#if defined(OS_LINUX)
+#define MAYBE_ContentSmallerThanView FAILS_ContentSmallerThanView
+#else
+#define MAYBE_ContentSmallerThanView ContentSmallerThanView
+#endif
 // Tests that taking a snapshot when the content is smaller than the view
 // produces a snapshot equal to the view size.
-TEST_F(AutomationProxySnapshotTest, ContentSmallerThanView) {
+TEST_F(AutomationProxySnapshotTest, MAYBE_ContentSmallerThanView) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
 
@@ -1590,9 +1596,15 @@ TEST_F(AutomationProxySnapshotTest, ContentSmallerThanView) {
   ASSERT_EQ(view_bounds.height(), bitmap.height());
 }
 
+// See http://crbug.com/63022.
+#if defined(OS_LINUX)
+#define MAYBE_ContentLargerThanView FAILS_ContentLargerThanView
+#else
+#define MAYBE_ContentLargerThanView ContentLargerThanView
+#endif
 // Tests that taking a snapshot when the content is larger than the view
 // produces a snapshot equal to the content size.
-TEST_F(AutomationProxySnapshotTest, ContentLargerThanView) {
+TEST_F(AutomationProxySnapshotTest, MAYBE_ContentLargerThanView) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
 
@@ -1642,6 +1654,9 @@ TEST_F(AutomationProxySnapshotTest, LargeSnapshot) {
 #if defined(OS_MACOSX)
 // Most pixels on mac are slightly off.
 #define MAYBE_ContentsCorrect DISABLED_ContentsCorrect
+#elif defined(OS_LINUX)
+// See http://crbug.com/63022.
+#define MAYBE_ContentsCorrect FAILS_ContentsCorrect
 #else
 #define MAYBE_ContentsCorrect ContentsCorrect
 #endif
