88086c4f6f404baf3a9f195e50bb46799ba8385a
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-15 00:18:30 +0000

    Revert 66088 - Changing the installer switches from wchar_t[] to char[].
    Because of this I'm also refactoring some code that before
    was using wstring to build command lines by hand instead of
    using the CommandLine class.  Now we use CommandLine.
    
    To get this to work correctly, I also needed to fix CommandLine::AppendArguments so I added a little test for it.
    
    TEST=There should be no changes in functionality.  Run all installer tests.
    BUG=61609
    
    Review URL: http://codereview.chromium.org/4928002
    
    TBR=tommi@chromium.org
    Review URL: http://codereview.chromium.org/4988001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
-#include "base/command_line.h"
 #include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/util_constants.h"
 #include "chrome/installer/util/version.h"
@@ -35,7 +34,9 @@ class RegKey;
 class InstallUtil {
  public:
   // Launches given exe as admin on Vista.
-  static bool ExecuteExeAsAdmin(const CommandLine& cmd, DWORD* exit_code);
+  static bool ExecuteExeAsAdmin(const std::wstring& exe,
+                                const std::wstring& params,
+                                DWORD* exit_code);
 
   // Reads the uninstall command for Chromium from registry and returns it.
   // If system_install is true the command is read from HKLM, otherwise
