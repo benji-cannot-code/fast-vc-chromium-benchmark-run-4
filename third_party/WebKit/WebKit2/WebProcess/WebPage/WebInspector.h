de0cb89170b2deab8d142195bed6d85f7518132d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-16 21:30:32 +0000

    Fix clang++ build.
    * Scripts/webkit2/messages.py:
    * WebProcess/WebPage/WebInspector.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/WebInspector.h
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/WebInspector.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebPage;
-class WebPageCreationParameters;
+struct WebPageCreationParameters;
 
 class WebInspector {
     WTF_MAKE_NONCOPYABLE(WebInspector);
