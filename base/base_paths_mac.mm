6a7e27a1235d4ec412bbb532b92a1fa031c34038
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-16 22:25:47 +0000

    Only need to depend on Foundation here, not Cocoa.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5095001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66331 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_paths_mac.mm
+++ base/base_paths_mac.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_paths_mac.h"
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 #include <mach-o/dyld.h>
 
 #include "base/compiler_specific.h"
