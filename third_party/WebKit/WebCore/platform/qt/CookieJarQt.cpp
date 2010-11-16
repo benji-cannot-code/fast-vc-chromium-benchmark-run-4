bdc529fda8d7f75f9bd854d47552c789c6b733be
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-16 01:23:38 +0000

    2010-11-15  Andreas Kling  <kling@webkit.org>
            Unreviewed buildfix (Qt WebKit2 minimal)
    
            * platform/qt/CookieJarQt.cpp: Add missing <QStringList> include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/CookieJarQt.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClientQt.h"
 #include <QNetworkAccessManager>
 #include <QNetworkCookie>
+#include <QStringList>
 
 namespace WebCore {
 
