22a2b2741e93e22ffa5e7a6d542ee0fa244867de
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-16 04:45:01 +0000

    https://bugs.webkit.org/show_bug.cgi?id=49570
    Reviewed by Dan Bernstein.
    
    Put italics specialization check back in CSSFontSelector.  I had to back it out, since it broke
    a layout test, and the issue was that local font fallback fonts should not be considered by the
    specialization check (since you never want to pick a fallback over an actual @font-face rule if
    the @font-face rule can handle it).
     
    * css/CSSFontFace.h:
    (WebCore::CSSFontFace::create):
    (WebCore::CSSFontFace::isLocalFallback):
    (WebCore::CSSFontFace::CSSFontFace):
    * css/CSSFontSelector.cpp:
    (WebCore::CSSFontSelector::addFontFaceRule):
    (WebCore::compareFontFaces):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSFontFace.h
+++ third_party/WebKit/WebCore/css/CSSFontFace.h
@@ -43,7 +43,7 @@ class SimpleFontData;
 
 class CSSFontFace : public RefCounted<CSSFontFace> {
 public:
-    static PassRefPtr<CSSFontFace> create(FontTraitsMask traitsMask) { return adoptRef(new CSSFontFace(traitsMask)); }
+    static PassRefPtr<CSSFontFace> create(FontTraitsMask traitsMask, bool isLocalFallback = false) { return adoptRef(new CSSFontFace(traitsMask, isLocalFallback)); }
     ~CSSFontFace();
 
     FontTraitsMask traitsMask() const { return m_traitsMask; }
@@ -58,6 +58,8 @@ public:
 
     bool isLoaded() const;
     bool isValid() const;
+
+    bool isLocalFallback() const { return m_isLocalFallback; }
 
     void addSource(CSSFontFaceSource*);
 
@@ -85,9 +87,10 @@ public:
 #endif
 
 private:
-    CSSFontFace(FontTraitsMask traitsMask)
+    CSSFontFace(FontTraitsMask traitsMask, bool isLocalFallback)
         : m_traitsMask(traitsMask)
         , m_activeSource(0)
+        , m_isLocalFallback(isLocalFallback)
     {
     }
 
@@ -96,6 +99,7 @@ private:
     HashSet<CSSSegmentedFontFace*> m_segmentedFontFaces;
     Vector<CSSFontFaceSource*> m_sources;
     CSSFontFaceSource* m_activeSource;
+    bool m_isLocalFallback;
 };
 
 }
