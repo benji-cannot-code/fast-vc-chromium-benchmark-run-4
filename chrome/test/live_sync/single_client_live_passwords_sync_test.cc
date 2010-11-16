1f1fe323eac13b79df8a46d27817f3eabaf6d9fe
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-16 21:32:03 +0000

    Annotating all failing password integration tests on Mac.
    It looks like all the Passwords integration tests are failing on the mac
    builder. Marking them as FAILS until the associated bug is fixed.
    
    For logs, see
    http://build.chromium.org/p/chromium.fyi/builders/Chromium%20Mac%20Sync/builds/1238
    
    TBR=akalin@chromium.org
    BUG=59867
    TEST=sync_integration_tests on mac builder
    
    Review URL: http://codereview.chromium.org/4985005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/single_client_live_passwords_sync_test.cc
+++ chrome/test/live_sync/single_client_live_passwords_sync_test.cc
@@ -8,7 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using webkit_glue::PasswordForm;
 
+// TODO(sync): Remove FAILS_ annotation after http://crbug.com/59867 is fixed.
+#if defined(OS_MACOSX)
+IN_PROC_BROWSER_TEST_F(SingleClientLivePasswordsSyncTest, FAILS_Sanity) {
+#else
 IN_PROC_BROWSER_TEST_F(SingleClientLivePasswordsSyncTest, Sanity) {
+#endif
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   PasswordForm form;
