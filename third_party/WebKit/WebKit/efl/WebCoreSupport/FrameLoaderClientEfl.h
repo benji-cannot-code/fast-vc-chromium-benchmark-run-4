26b8928982c36b3c17e85ed63bda2c9de85a01f8
leandro@webkit.org <leandro@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-16 15:45:46 +0000

    2010-11-16  Leandro Pereira  <leandro@profusion.mobi>
            [EFL] Unreviewed. Build fix.
    
            * WebCoreSupport/FrameLoaderClientEfl.h: Also include
            ResourceError.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.h
+++ third_party/WebKit/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EWebKit.h"
 #include "FrameLoaderClient.h"
 #include "PluginView.h"
+#include "ResourceError.h"
 #include "ResourceResponse.h"
 
 namespace WebCore {
