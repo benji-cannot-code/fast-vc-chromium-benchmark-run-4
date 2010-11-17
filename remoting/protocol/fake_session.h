cbe7652d78f796b2c8ea043b199ab0fa402c669a
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 23:05:15 +0000

    Unittests for RTP packetizer and depacketizer.
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/4946001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66536 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/fake_session.h
+++ remoting/protocol/fake_session.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_PROTOCOL_FAKE_SESSION_H_
 #define REMOTING_PROTOCOL_FAKE_SESSION_H_
 
+#include <string>
 #include <vector>
 
 #include "base/scoped_ptr.h"
@@ -27,7 +28,7 @@ class FakeSocket : public net::Socket {
   FakeSocket();
   virtual ~FakeSocket();
 
-  const std::vector<char>& written_data() { return written_data_; }
+  const std::string& written_data() { return written_data_; }
 
   void AppendInputData(char* data, int data_size);
   int input_pos() { return input_pos_; }
@@ -47,8 +48,43 @@ class FakeSocket : public net::Socket {
   int read_buffer_size_;
   net::CompletionCallback* read_callback_;
 
-  std::vector<char> written_data_;
-  std::vector<char> input_data_;
+  std::string written_data_;
+  std::string input_data_;
+  int input_pos_;
+};
+
+// FakeUdpSocket is similar to FakeSocket but behaves as UDP socket. All written
+// packets are stored separetely in written_packets(). AppendInputPacket() adds
+// one packet that will be returned by Read().
+class FakeUdpSocket : public net::Socket {
+ public:
+  FakeUdpSocket();
+  virtual ~FakeUdpSocket();
+
+  const std::vector<std::string>& written_packets() {
+    return written_packets_;
+  }
+
+  void AppendInputPacket(char* data, int data_size);
+  int input_pos() { return input_pos_; }
+
+  // net::Socket interface.
+  virtual int Read(net::IOBuffer* buf, int buf_len,
+                   net::CompletionCallback* callback);
+  virtual int Write(net::IOBuffer* buf, int buf_len,
+                    net::CompletionCallback* callback);
+
+  virtual bool SetReceiveBufferSize(int32 size);
+  virtual bool SetSendBufferSize(int32 size);
+
+ private:
+  bool read_pending_;
+  scoped_refptr<net::IOBuffer> read_buffer_;
+  int read_buffer_size_;
+  net::CompletionCallback* read_callback_;
+
+  std::vector<std::string> written_packets_;
+  std::vector<std::string> input_packets_;
   int input_pos_;
 };
 
@@ -73,8 +109,8 @@ class FakeSession : public Session {
   virtual FakeSocket* event_channel();
   virtual FakeSocket* video_channel();
 
-  virtual FakeSocket* video_rtp_channel();
-  virtual FakeSocket* video_rtcp_channel();
+  virtual FakeUdpSocket* video_rtp_channel();
+  virtual FakeUdpSocket* video_rtcp_channel();
 
   virtual const std::string& jid();
 
@@ -93,8 +129,8 @@ class FakeSession : public Session {
   FakeSocket control_channel_;
   FakeSocket event_channel_;
   FakeSocket video_channel_;
-  FakeSocket video_rtp_channel_;
-  FakeSocket video_rtcp_channel_;
+  FakeUdpSocket video_rtp_channel_;
+  FakeUdpSocket video_rtcp_channel_;
   std::string jid_;
   bool closed_;
 };
