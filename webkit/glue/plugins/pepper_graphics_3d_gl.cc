c790792ff420c801f1908cdc5c7f9c68d00d9a5a
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 21:50:19 +0000

    Update cmd buffer script to reflect ppapi reorg.
    Someone moved the files in PPAPI around and hand-modified autogenerated files.
    This CL moves the change to the .py file used to generate them.
    
    BUG=none
    TEST=generated files identical to hand-modified version.
    
    Review URL: http://codereview.chromium.org/5147003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66520 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/pepper_graphics_3d_gl.cc
+++ webkit/glue/plugins/pepper_graphics_3d_gl.cc
@@ -551,8 +551,13 @@ void* MapTexSubImage2D(
 void UnmapTexSubImage2D(const void* mem) {
   Graphics3D::GetCurrent()->impl()->UnmapTexSubImage2D(mem);
 }
+void CopyTextureToParentTexture(
+    GLuint client_child_id, GLuint client_parent_id) {
+  Graphics3D::GetCurrent()->impl()->CopyTextureToParentTexture(
+      client_child_id, client_parent_id);
+}
 
-const PPB_OpenGLES_Dev ppb_opengles = {
+const struct PPB_OpenGLES_Dev ppb_opengles = {
   &ActiveTexture,
   &AttachShader,
   &BindAttribLocation,
@@ -704,7 +709,8 @@ const PPB_OpenGLES_Dev ppb_opengles = {
   &MapBufferSubData,
   &UnmapBufferSubData,
   &MapTexSubImage2D,
-  &UnmapTexSubImage2D
+  &UnmapTexSubImage2D,
+  &CopyTextureToParentTexture
 };
 
 }  // namespace
