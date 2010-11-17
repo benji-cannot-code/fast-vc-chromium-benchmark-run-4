52685e084271e782a65c475f4d2917ac4673db3b
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 02:44:27 +0000

    add missing header.
    Should fix build.
    
    BUG=none
    TEST=build
    
    Review URL: http://codereview.chromium.org/5142001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66374 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/multi_process_lock_linux.cc
+++ chrome/common/multi_process_lock_linux.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/multi_process_lock.h"
 
+#include <stdio.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
