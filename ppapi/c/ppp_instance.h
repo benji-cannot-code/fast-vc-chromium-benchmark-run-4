d81e24d2f4deb3525d66aee8ad6cc33fe0f0ae3b
dmichael@google.com <dmichael@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 16:14:53 +0000

    A second attempt at forcing 8-byte alignment of the time stamp.
    I used padding this time.  pragma pack didn't work on Mac x86-32.  It seems to be an issue that only some (very recent?) versions of GCC support it.  I could do it with macros, but it would be ugly.  See the updated bug for more details.
    
    Try was clean:
    http://build.chromium.org/p/tryserver.chromium/waterfall?committer=dmichael@google.com
    
    BUG=62983
    TEST=I use a compile assert to ensure the size is consistent.
    
    Assuming this goes well, I'll do a future CL to scan other structs and tweak them to get consistent alignment if I find any other problems.
    Review URL: http://codereview.chromium.org/5001002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66446 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppp_instance.h
+++ ppapi/c/ppp_instance.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct PP_InputEvent;
 struct PP_Var;
 
-#define PPP_INSTANCE_INTERFACE "PPP_Instance;0.3"
+#define PPP_INSTANCE_INTERFACE "PPP_Instance;0.4"
 
 /**
  * @file
