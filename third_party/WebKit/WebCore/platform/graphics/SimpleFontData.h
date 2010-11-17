6eaddfb9b44d2d03ec0bdd8d0fe78e25677614fe
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-17 03:24:39 +0000

    Add FontBaseline.h and make ascent and descent take a baseline parameter so that both an alphabetic and ideographic baseline can be requested.
    Reviewed by Sam Weinig.
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/graphics/Font.h:
    (WebCore::Font::ascent):
    (WebCore::Font::descent):
    * platform/graphics/FontBaseline.h: Added.
    * platform/graphics/SimpleFontData.h:
    (WebCore::SimpleFontData::ascent):
    (WebCore::SimpleFontData::descent):
    (WebCore::SimpleFontData::height):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
+++ third_party/WebKit/WebCore/platform/graphics/SimpleFontData.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SimpleFontData_h
 #define SimpleFontData_h
 
+#include "FontBaseline.h"
 #include "FontData.h"
 #include "FontPlatformData.h"
 #include "FloatRect.h"
@@ -81,8 +82,9 @@ public:
     SimpleFontData* brokenIdeographFontData() const;
     
     // vertical metrics
-    int ascent() const { return m_ascent; }
-    int descent() const { return m_descent; }
+    int ascent(FontBaseline baselineType = AlphabeticBaseline) const { return baselineType == AlphabeticBaseline ? m_ascent : height() - height() / 2; }
+    int descent(FontBaseline baselineType = AlphabeticBaseline) const { return baselineType == AlphabeticBaseline ? m_descent : height() / 2; }
+    int height() const { return m_ascent + m_descent; }
     int lineSpacing() const { return m_lineSpacing; }
     int lineGap() const { return m_lineGap; }
     float maxCharWidth() const { return m_maxCharWidth; }
