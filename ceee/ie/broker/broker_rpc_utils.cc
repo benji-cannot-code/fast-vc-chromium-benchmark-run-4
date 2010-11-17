3be51a5024c28b1f31764823d3b065f927a15f04
twiz@google.com <twiz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 23:23:09 +0000

    Submitting changes by Vitaly Buka, reviewed here:  http://codereview.chromium.org/5168002
    BUG=3205655
    TEST=none
    
    Review URL: http://codereview.chromium.org/5182003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66543 0039d316-1c4b-4281-b951-d872f2087c98

--- ceee/ie/broker/broker_rpc_utils.cc
+++ ceee/ie/broker/broker_rpc_utils.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ceee/ie/broker/broker_rpc_utils.h"
 
+#include <algorithm>
+
 #include "base/win_util.h"
 #include "ceee/common/process_utils_win.h"
 
@@ -26,6 +28,8 @@ std::wstring GetRpcEndPointAddress() {
   win_util::GetUserSidString(&sid);
   end_point += sid;
   end_point += L"-B4630D08-4621-41A1-A8D0-F1E98DA460D6";
+  // XP does not accept endpoints longer than 52.
+  end_point.resize(std::min(52u, end_point.size()));
   return end_point;
 }
 
