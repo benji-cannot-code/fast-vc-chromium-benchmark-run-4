a4a36d7285a7305cc927893a6b0e87827d53c7c9
senorblanco@chromium.org <senorblanco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 19:16:03 +0000

    Do an early return from GLES2DecoderImpl::Initialize if context group initialization fails.
    BUG=none
    TEST=run GPU process under valgrind on a machine w/no GPU
    
    
    Review URL: http://codereview.chromium.org/5181002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66489 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -1764,6 +1764,7 @@ bool GLES2DecoderImpl::Initialize(gfx::GLContext* context,
     LOG(ERROR) << "GPUProcessor::InitializeCommon failed because group "
                << "failed to initialize.";
     Destroy();
+    return false;
   }
 
   CHECK_GL_ERROR();
