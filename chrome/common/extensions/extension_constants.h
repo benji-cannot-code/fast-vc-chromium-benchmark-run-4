52220545e872778ab48a42ba112b3597570deac4
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 23:15:05 +0000

    Allow localization of omnibox.keyword.
    Also change omnibox_keyword -> omnibox.keyword.
    
    BUG=63448,63449
    TEST=unit tests
    
    Review URL: http://codereview.chromium.org/5172002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66538 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -39,6 +39,7 @@ namespace extension_manifest_keys {
   extern const char* kMatches;
   extern const char* kMinimumChromeVersion;
   extern const char* kName;
+  extern const char* kOmnibox;
   extern const char* kOmniboxKeyword;
   extern const char* kOptionsPage;
   extern const char* kPageAction;
