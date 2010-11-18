cd45dc3433bea099374d94ff61ef9a0904d0f8b5
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-18 12:57:04 +0000

    2010-11-18  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Chrome DevTools: minify front-end JavaScript.
            https://bugs.webkit.org/show_bug.cgi?id=49657
    
            * scripts/concatenate_js_files.py:
            * scripts/jsmin.py: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/scripts/concatenate_js_files.py
+++ third_party/WebKit/WebKit/chromium/scripts/concatenate_js_files.py
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from HTMLParser import HTMLParser
 from cStringIO import StringIO
+
+import jsmin
 import os.path
 import sys
 
@@ -102,7 +104,7 @@ def main(argv):
         input_file.close()
 
     output_file = open(output_file_name, 'w')
-    output_file.write(output.getvalue())
+    output_file.write(jsmin.jsmin(output.getvalue()))
     output_file.close()
     output.close()
 
