65b649c271fa79e145c813750d982ac5f2d8828f
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 21:30:05 +0000

    Revert 66513 - sync: remove event_sys from ServerConnectionManager and SyncerThread
    BUG=26339
    TEST=sync_unit_tests
    
    Review URL: http://codereview.chromium.org/5125001
    
    TBR=tim@chromium.org
    Review URL: http://codereview.chromium.org/5228002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66675 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncer_thread.h
+++ chrome/browser/sync/engine/syncer_thread.h
@@ -26,9 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/engine/idle_query_linux.h"
 #endif
 #include "chrome/browser/sync/engine/syncer_types.h"
-#include "chrome/browser/sync/engine/net/server_connection_manager.h"
 #include "chrome/browser/sync/sessions/sync_session.h"
 #include "chrome/browser/sync/syncable/model_type.h"
+#include "chrome/common/deprecated/event_sys-inl.h"
+
+class EventListenerHookup;
 
 namespace browser_sync {
 
@@ -39,8 +41,7 @@ class URLFactory;
 struct ServerConnectionEvent;
 
 class SyncerThread : public base::RefCountedThreadSafe<SyncerThread>,
-                     public sessions::SyncSession::Delegate,
-                     public ServerConnectionEventListener {
+                     public sessions::SyncSession::Delegate {
   FRIEND_TEST_ALL_PREFIXES(SyncerThreadTest, CalculateSyncWaitTime);
   FRIEND_TEST_ALL_PREFIXES(SyncerThreadTest, CalculatePollingWaitTime);
   FRIEND_TEST_ALL_PREFIXES(SyncerThreadWithSyncerTest, Polling);
@@ -102,6 +103,8 @@ class SyncerThread : public base::RefCountedThreadSafe<SyncerThread>,
 
   explicit SyncerThread(sessions::SyncSessionContext* context);
   virtual ~SyncerThread();
+
+  virtual void WatchConnectionManager(ServerConnectionManager* conn_mgr);
 
   // Starts a syncer thread.
   // Returns true if it creates a thread or if there's currently a thread
@@ -237,8 +240,7 @@ class SyncerThread : public base::RefCountedThreadSafe<SyncerThread>,
       const base::TimeDelta& new_interval);
   virtual void OnShouldStopSyncingPermanently();
 
-  // ServerConnectionEventListener implementation.
-  virtual void OnServerConnectionEvent(const ServerConnectionEvent& event);
+  void HandleServerConnectionEvent(const ServerConnectionEvent& event);
 
   void SyncMain(Syncer* syncer);
 
@@ -294,6 +296,8 @@ class SyncerThread : public base::RefCountedThreadSafe<SyncerThread>,
   void RequestSyncerExitAndSetThreadStopConditions();
 
   void Notify(SyncEngineEvent::EventCause cause);
+
+  scoped_ptr<EventListenerHookup> conn_mgr_hookup_;
 
   // Modifiable versions of kDefaultLongPollIntervalSeconds which can be
   // updated by the server.
