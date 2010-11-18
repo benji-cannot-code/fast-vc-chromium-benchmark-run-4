c8eeaba0b7e099f91204c310da9e7941b59993d0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 23:50:32 +0000

    [gtk] don't show custom frame min/max/close when theme is changed.
    BUG=63661
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5217002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_titlebar.cc
+++ chrome/browser/gtk/browser_titlebar.cc
@@ -469,8 +469,10 @@ void BrowserTitlebar::BuildButtons(const std::string& button_string) {
   }
 
   // Now show the correct widgets in the two hierarchies.
-  gtk_widget_show_all(titlebar_left_buttons_vbox_);
-  gtk_widget_show_all(titlebar_right_buttons_vbox_);
+  if (using_custom_frame_) {
+    gtk_widget_show_all(titlebar_left_buttons_vbox_);
+    gtk_widget_show_all(titlebar_right_buttons_vbox_);
+  }
   UpdateMaximizeRestoreVisibility();
 }
 
@@ -523,9 +525,9 @@ void BrowserTitlebar::UpdateCustomFrame(bool use_custom_frame) {
   using_custom_frame_ = use_custom_frame;
   if (use_custom_frame) {
     if (titlebar_left_buttons_vbox_)
-      gtk_widget_show(titlebar_left_buttons_vbox_);
+      gtk_widget_show_all(titlebar_left_buttons_vbox_);
     if (titlebar_right_buttons_vbox_)
-      gtk_widget_show(titlebar_right_buttons_vbox_);
+      gtk_widget_show_all(titlebar_right_buttons_vbox_);
   } else {
     if (titlebar_left_buttons_vbox_)
       gtk_widget_hide(titlebar_left_buttons_vbox_);
