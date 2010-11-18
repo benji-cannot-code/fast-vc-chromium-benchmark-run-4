1b9e0836bc255e4166dacef09160a874188f8a21
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 23:42:00 +0000

    Disabled flaky pause tests (again).
    BUG=39070
    TEST=none
    TBR=tim@chromium.org
    
    Review URL: http://codereview.chromium.org/5226002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncer_thread_unittest.cc
+++ chrome/browser/sync/engine/syncer_thread_unittest.cc
@@ -894,7 +894,9 @@ TEST_F(SyncerThreadWithSyncerTest, AuthInvalid) {
   EXPECT_TRUE(syncer_thread()->Stop(2000));
 }
 
-TEST_F(SyncerThreadWithSyncerTest, Pause) {
+// TODO(zea): Disabled, along with PauseWhenNotConnected, due to stalling on
+// windows, preventing further sync unit tests from running. See crbug/39070.
+TEST_F(SyncerThreadWithSyncerTest, DISABLED_Pause) {
   WaitableEvent sync_cycle_ended_event(false, false);
   WaitableEvent paused_event(false, false);
   WaitableEvent resumed_event(false, false);
@@ -999,7 +1001,8 @@ TEST_F(SyncerThreadWithSyncerTest, StartWhenNotConnected) {
   EXPECT_TRUE(syncer_thread()->Stop(2000));
 }
 
-TEST_F(SyncerThreadWithSyncerTest, PauseWhenNotConnected) {
+// See TODO comment on the "Pause" test above.
+TEST_F(SyncerThreadWithSyncerTest, DISABLED_PauseWhenNotConnected) {
   WaitableEvent sync_cycle_ended_event(false, false);
   WaitableEvent event(false, false);
   ListenerMock listener;
