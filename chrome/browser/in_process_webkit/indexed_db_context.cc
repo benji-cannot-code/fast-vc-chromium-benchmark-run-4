2ffc5da31706422052d6748b73bf2dca9458478c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 17:55:19 +0000

    Added a method to delete indexed databases on shutdown.
    BUG=56249
    TEST=IndexedDBBrowserTest.ClearLocalState
    
    Review URL: http://codereview.chromium.org/5112001
    Patch from Yulian Pastarmov <pastarmovj@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/indexed_db_context.cc
+++ chrome/browser/in_process_webkit/indexed_db_context.cc
@@ -5,17 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/in_process_webkit/indexed_db_context.h"
 
+#include "base/file_util.h"
 #include "base/logging.h"
+#include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/in_process_webkit/webkit_context.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebCString.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebIDBDatabase.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebIDBFactory.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebSecurityOrigin.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebString.h"
 #include "webkit/glue/webkit_glue.h"
 
 using WebKit::WebIDBDatabase;
 using WebKit::WebIDBFactory;
+using WebKit::WebSecurityOrigin;
 
 const FilePath::CharType IndexedDBContext::kIndexedDBDirectory[] =
     FILE_PATH_LITERAL("IndexedDB");
@@ -43,4 +47,23 @@ FilePath IndexedDBContext::GetIndexedDBFilePath(
       kIndexedDBDirectory);
   FilePath::StringType id = webkit_glue::WebStringToFilePathString(origin_id);
   return storage_dir.Append(id.append(kIndexedDBExtension));
+}
+
+// static
+void IndexedDBContext::ClearLocalState(const FilePath& profile_path,
+                                       const char* url_scheme_to_be_skipped) {
+  file_util::FileEnumerator file_enumerator(profile_path.Append(
+      kIndexedDBDirectory), false, file_util::FileEnumerator::FILES);
+  // TODO(pastarmovj): We might need to consider exchanging this loop for
+  // something more efficient in the future.
+  for (FilePath file_path = file_enumerator.Next(); !file_path.empty();
+       file_path = file_enumerator.Next()) {
+    if (file_path.Extension() != IndexedDBContext::kIndexedDBExtension)
+      continue;
+    WebSecurityOrigin origin =
+        WebSecurityOrigin::createFromDatabaseIdentifier(
+            webkit_glue::FilePathToWebString(file_path.BaseName()));
+    if (!EqualsASCII(origin.protocol(), url_scheme_to_be_skipped))
+      file_util::Delete(file_path, false);
+  }
 }
