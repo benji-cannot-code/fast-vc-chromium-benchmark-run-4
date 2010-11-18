5de6b6d76dab397af758ec4fce95aa86b56d409f
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 23:49:27 +0000

    Temporarily allow IO on UI thread when accessing Linux/GTK proxy settings.
    BUG=63690
    TEST=accessing proxy settings on a debug build doesn't crash
    
    Review URL: http://codereview.chromium.org/5131002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66704 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/options/advanced_contents_gtk.cc
+++ chrome/browser/gtk/options/advanced_contents_gtk.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/process_util.h"
 #include "base/string_tokenizer.h"
+#include "base/thread_restrictions.h"
 #include "base/nix/xdg_util.h"
 #include "chrome/browser/browser_list.h"
 #include "chrome/browser/browser_process.h"
@@ -424,6 +425,10 @@ void NetworkSection::NotifyPrefChanged(const std::string* pref_name) {
 // static
 void NetworkSection::OnChangeProxiesButtonClicked(GtkButton *button,
                                                   NetworkSection* section) {
+  // Changing proxy settings searches the disk for the proxy configuration
+  // binary.  Temporarily allow IO for now, see http://crbug.com/63690
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+
   section->UserMetricsRecordAction(UserMetricsAction("Options_ChangeProxies"),
                                    NULL);
 
