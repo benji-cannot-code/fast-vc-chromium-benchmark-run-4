dc4bd57640ff814537198b567c620def3877016c
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 18:31:50 +0000

    Add "open as window" menu item to NTP app menu.
    BUG=59697
    TEST=BrowserTest.OpenAppWindowLikeNtp,SessionRestoreUITest.RestoreAfterClosing*
    
    Review URL: http://codereview.chromium.org/5019005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -280,18 +280,15 @@ void ExtensionProcessManager::Observe(NotificationType type,
                                       const NotificationSource& source,
                                       const NotificationDetails& details) {
   switch (type.value) {
-    case NotificationType::EXTENSIONS_READY:
+    case NotificationType::EXTENSIONS_READY: {
       CreateBackgroundHosts(this,
           Source<Profile>(source).ptr()->GetExtensionsService()->extensions());
       break;
+    }
 
     case NotificationType::EXTENSION_LOADED: {
-      ExtensionsService* service =
-          Source<Profile>(source).ptr()->GetExtensionsService();
-      if (service->is_ready()) {
-        const Extension* extension = Details<const Extension>(details).ptr();
-        ::CreateBackgroundHost(this, extension);
-      }
+      const Extension* extension = Details<const Extension>(details).ptr();
+      ::CreateBackgroundHost(this, extension);
       break;
     }
 
@@ -455,8 +452,7 @@ void IncognitoExtensionProcessManager::Observe(
       if (browser->profile() == browsing_instance_->profile()) {
         ExtensionsService* service =
             browsing_instance_->profile()->GetExtensionsService();
-        if (service && service->is_ready())
-          CreateBackgroundHosts(this, service->extensions());
+        CreateBackgroundHosts(this, service->extensions());
       }
       break;
     }
