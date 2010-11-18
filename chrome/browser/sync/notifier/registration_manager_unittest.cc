73933ce3364fdd608084cd8336b09b0ee5750d54
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 20:08:49 +0000

    roll cacheinvalidation to @56 (adds Start() method to InvalidationClient)
    original patch from ghc@google.com ( http://codereview.chromium.org/5111003/ )
    
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/5143005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66667 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/registration_manager_unittest.cc
+++ chrome/browser/sync/notifier/registration_manager_unittest.cc
@@ -26,6 +26,8 @@ class FakeInvalidationClient : public invalidation::InvalidationClient {
 
   virtual ~FakeInvalidationClient() {}
 
+  virtual void Start(const std::string& state) {}
+
   virtual void Register(const invalidation::ObjectId& oid) {
     registered_oids.push_back(oid);
   }
