f465431620963b208ec8c51dd44054248a494375
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-18 20:10:27 +0000

    2010-11-18  James Robinson  <jamesr@chromium.org>
            Reviewed by Darin Fisher.
    
            [chromium] REGRESSION(72141): Chromium fails acid3 on windows and linux
            https://bugs.webkit.org/show_bug.cgi?id=49638
    
            Adds a test checking that the bounding client rect of a text run with font-size:0
            has zero height.  This is also covered by acid3, but an isolated test case is easier
            to work with.
    
            * fast/text/font-size-zero-expected.txt: Added.
            * fast/text/font-size-zero.html: Added.
            * fast/text/script-tests/font-size-zero.js: Added.
    2010-11-18  James Robinson  <jamesr@chromium.org>
    
            Reviewed by Darin Fisher.
    
            [chromium] REGRESSION(72141): Chromium fails acid3 on windows and linux
            https://bugs.webkit.org/show_bug.cgi?id=49638
    
            Avoid clamping zero text size to 12pt in FontPlatformData::setupPaint() in FontPlatformDataLinux.cpp
            Set all metrics to 0 if the font size is specified as zero in SimpleFontDataChromiumWin.cpp, just
            like Safari Win did in r72270.
    
            Test: fast/text/font-size-zero.html
    
            * platform/graphics/chromium/FontPlatformDataLinux.cpp:
            (WebCore::FontPlatformData::setupPaint):
            * platform/graphics/chromium/SimpleFontDataChromiumWin.cpp:
            (WebCore::SimpleFontData::platformInit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72324 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/text/script-tests/font-size-zero.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var s = document.createElement('span');
+s.style.fontSize = 0;
+s.innerHTML = 'Text';
+document.body.appendChild(s);
+
+shouldBe("s.getBoundingClientRect().height", "0");
+
+document.body.removeChild(s);
+
+var successfullyParsed = true;
