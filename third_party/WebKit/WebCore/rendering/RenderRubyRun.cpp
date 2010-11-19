584f94064fc06ead3e529a9b0433097db9763616
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-19 01:33:33 +0000

    https://bugs.webkit.org/show_bug.cgi?id=49772
    Reviewed by Dan Bernstein.
    
    Fix nested ruby.  My previous checkin (for 49717) broke it.  Push the ruby text and ruby base text
    together based off logicalTopOverflow and logicalBottomOverflow rather than lineTop and lineBottom.
    This way the overflow from nested rubies is accounted for.
            
    Added fast/ruby/nested-ruby.html
    
    WebCore: 
    
    * rendering/RenderRubyRun.cpp:
    (WebCore::RenderRubyRun::layout):
    
    LayoutTests: 
    
    * fast/ruby/nested-ruby.html: Added.
    * platform/mac/fast/ruby/nested-ruby-expected.checksum: Added.
    * platform/mac/fast/ruby/nested-ruby-expected.png: Added.
    * platform/mac/fast/ruby/nested-ruby-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderRubyRun.cpp
+++ third_party/WebKit/WebCore/rendering/RenderRubyRun.cpp
@@ -251,8 +251,8 @@ void RenderRubyRun::layout()
     RootInlineBox* rootBox = rt->lastRootBox();
     if (rootBox) {
         // In order to align, we have to ignore negative leading.
-        firstLineRubyTextTop = rt->firstRootBox()->lineTop();
-        lastLineRubyTextBottom = rootBox->lineBottom();
+        firstLineRubyTextTop = rt->firstRootBox()->logicalTopLayoutOverflow();
+        lastLineRubyTextBottom = rootBox->logicalBottomLayoutOverflow();
     }
 
     if (!style()->isFlippedLinesWritingMode()) {
@@ -260,7 +260,7 @@ void RenderRubyRun::layout()
         if (RenderRubyBase* rb = rubyBase()) {
             RootInlineBox* rootBox = rb->firstRootBox();
             if (rootBox)
-                firstLineTop = rootBox->lineTop();
+                firstLineTop = rootBox->logicalTopLayoutOverflow();
             firstLineTop += rb->logicalTop();
         }
         
@@ -270,7 +270,7 @@ void RenderRubyRun::layout()
         if (RenderRubyBase* rb = rubyBase()) {
             RootInlineBox* rootBox = rb->lastRootBox();
             if (rootBox)
-                lastLineBottom = rootBox->lineBottom();
+                lastLineBottom = rootBox->logicalBottomLayoutOverflow();
             lastLineBottom += rb->logicalTop();
         }
 
