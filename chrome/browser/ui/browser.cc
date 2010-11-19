b53b397cb56624a7c34c814f332b05517cf107e9
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-19 00:38:05 +0000

    Make a DCHECK in Browser::ExecuteCommandWithDisposition more informative.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5099001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2066,7 +2066,8 @@ void Browser::ExecuteCommandWithDisposition(
   if (!GetSelectedTabContentsWrapper())
     return;
 
-  DCHECK(command_updater_.IsCommandEnabled(id)) << "Invalid/disabled command";
+  DCHECK(command_updater_.IsCommandEnabled(id)) << "Invalid/disabled command "
+                                                << id;
 
   // If command execution is blocked then just record the command and return.
   if (block_command_execution_) {
