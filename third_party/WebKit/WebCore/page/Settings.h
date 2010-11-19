42788e65b7b9029f93aa5a302a5441c7d4d505f6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-19 22:37:06 +0000

    2010-11-19  Avi Drissman  <avi@google.com>
            Reviewed by Antonio Gomes.
    
            Complete support for Unix editing mode
            https://bugs.webkit.org/show_bug.cgi?id=49757
    
            * page/Settings.cpp:
            (WebCore::Settings::Settings):
            * page/Settings.h:
    2010-11-19  Avi Drissman  <avi@google.com>
    
            Reviewed by Antonio Gomes.
    
            Complete support for Unix editing mode
            https://bugs.webkit.org/show_bug.cgi?id=49757
    
            * DumpRenderTree/LayoutTestController.cpp:
            (setEditingBehaviorCallback):
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::setEditingBehavior):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/Settings.h
+++ third_party/WebKit/WebCore/page/Settings.h
@@ -363,6 +363,7 @@ namespace WebCore {
         unsigned m_sessionStorageQuota;
 #endif
         unsigned m_pluginAllowedRunTime;
+        unsigned m_editingBehaviorType;
         bool m_isSpatialNavigationEnabled : 1;
         bool m_isJavaEnabled : 1;
         bool m_loadsImagesAutomatically : 1;
@@ -407,7 +408,6 @@ namespace WebCore {
         bool m_enforceCSSMIMETypeInNoQuirksMode : 1;
         bool m_usesEncodingDetector : 1;
         bool m_allowScriptsToCloseWindows : 1;
-        unsigned m_editingBehaviorType : 1;
         bool m_downloadableBinaryFontsEnabled : 1;
         bool m_xssAuditorEnabled : 1;
         bool m_acceleratedCompositingEnabled : 1;
