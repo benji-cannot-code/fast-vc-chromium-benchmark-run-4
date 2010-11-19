048e3f1141f329a4e6f3ca5a7742c803f667aff7
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-19 00:54:26 +0000

    Bug 49635 - Profiler implementation is fragile
    Reviewed by Oliver Hunt.
    
    JavaScriptCore: 
    
    The profile presently requires the exception handling mechanism to explicitly
    remove all stack frames that are exited during the exception unwind mechanism.
    This is fragile in a number of ways:
      * We have to change bytecode register allocation when compiling code to run
        when profiling, to preserve the callee function (this is also required to
        call did_call after the call has returned).
      * In the JIT we have to maintain additional data structures
        (CodeBlock::RareData::m_functionRegisterInfos) to map back to the register
        containing the callee.
      * In the interpreter we use 'magic values' to offset into the instruction
        stream to rediscover the register containing the function.
    
    Instead, move profiling into the head and tail of functions.
      * This correctly accounts the cost of the call itself to the caller.
      * This allows us to access the callee function object from the callframe.
      * This means that at the point a call is made we can track the stack depth
        on the ProfileNode.
      * When unwinding we can simply report the depth at which the exception is
        being handled - all call frames above this level are freed.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::shrinkToFit):
    * bytecode/CodeBlock.h:
    (JSC::CodeBlock::bytecodeOffset):
    (JSC::CodeBlock::methodCallLinkInfo):
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::emitCall):
    (JSC::BytecodeGenerator::emitCallVarargs):
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::unwindCallFrame):
    (JSC::Interpreter::throwException):
    (JSC::Interpreter::execute):
    (JSC::Interpreter::executeCall):
    (JSC::Interpreter::executeConstruct):
    (JSC::Interpreter::privateExecute):
    * jit/JITStubs.cpp:
    (JSC::DEFINE_STUB_FUNCTION):
    * profiler/Profile.cpp:
    (JSC::Profile::Profile):
    * profiler/ProfileGenerator.cpp:
    (JSC::ProfileGenerator::addParentForConsoleStart):
    (JSC::ProfileGenerator::willExecute):
    (JSC::ProfileGenerator::didExecute):
    (JSC::ProfileGenerator::exceptionUnwind):
    (JSC::ProfileGenerator::stopProfiling):
    * profiler/ProfileGenerator.h:
    * profiler/ProfileNode.cpp:
    (JSC::ProfileNode::ProfileNode):
    (JSC::ProfileNode::willExecute):
    * profiler/ProfileNode.h:
    (JSC::ProfileNode::create):
    (JSC::ProfileNode::callerCallFrame):
    * profiler/Profiler.cpp:
    (JSC::dispatchFunctionToProfiles):
    (JSC::Profiler::_willExecute):
    (JSC::Profiler::_didExecute):
    (JSC::Profiler::exceptionUnwind):
    * profiler/Profiler.h:
    
    LayoutTests: 
    
    Fixes previously failing tests - output was incorrect, showing duplicate entries
    for '(program) (no file) (line 1)'.
    
    * fast/profiler/throw-exception-from-eval-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/profiler/ProfileNode.h
+++ third_party/WebKit/JavaScriptCore/profiler/ProfileNode.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
+    class ExecState;
     class ProfileNode;
 
     typedef Vector<RefPtr<ProfileNode> >::const_iterator StackIterator;
@@ -45,23 +46,24 @@ namespace JSC {
 
     class ProfileNode : public RefCounted<ProfileNode> {
     public:
-        static PassRefPtr<ProfileNode> create(const CallIdentifier& callIdentifier, ProfileNode* headNode, ProfileNode* parentNode)
+        static PassRefPtr<ProfileNode> create(ExecState* callerCallFrame, const CallIdentifier& callIdentifier, ProfileNode* headNode, ProfileNode* parentNode)
         {
-            return adoptRef(new ProfileNode(callIdentifier, headNode, parentNode));
+            return adoptRef(new ProfileNode(callerCallFrame, callIdentifier, headNode, parentNode));
         }
-        static PassRefPtr<ProfileNode> create(ProfileNode* headNode, ProfileNode* node)
+        static PassRefPtr<ProfileNode> create(ExecState* callerCallFrame, ProfileNode* headNode, ProfileNode* node)
         {
-            return adoptRef(new ProfileNode(headNode, node));
+            return adoptRef(new ProfileNode(callerCallFrame, headNode, node));
         }
 
         bool operator==(ProfileNode* node) { return m_callIdentifier == node->callIdentifier(); }
 
-        ProfileNode* willExecute(const CallIdentifier&);
+        ProfileNode* willExecute(ExecState* callerCallFrame, const CallIdentifier&);
         ProfileNode* didExecute();
 
         void stopProfiling();
 
         // CallIdentifier members
+        ExecState* callerCallFrame() const { return m_callerCallFrame; }
         const CallIdentifier& callIdentifier() const { return m_callIdentifier; }
         const UString& functionName() const { return m_callIdentifier.m_name; }
         const UString& url() const { return m_callIdentifier.m_url; }
@@ -129,8 +131,8 @@ namespace JSC {
 #endif
 
     private:
-        ProfileNode(const CallIdentifier&, ProfileNode* headNode, ProfileNode* parentNode);
-        ProfileNode(ProfileNode* headNode, ProfileNode* nodeToCopy);
+        ProfileNode(ExecState* callerCallFrame, const CallIdentifier&, ProfileNode* headNode, ProfileNode* parentNode);
+        ProfileNode(ExecState* callerCallFrame, ProfileNode* headNode, ProfileNode* nodeToCopy);
 
         void startTimer();
         void resetChildrensSiblings();
@@ -148,6 +150,7 @@ namespace JSC {
         static inline bool functionNameDescendingComparator(const RefPtr<ProfileNode>& a, const RefPtr<ProfileNode>& b) { return a->functionName() > b->functionName(); }
         static inline bool functionNameAscendingComparator(const RefPtr<ProfileNode>& a, const RefPtr<ProfileNode>& b) { return a->functionName() < b->functionName(); }
 
+        ExecState* m_callerCallFrame;
         CallIdentifier m_callIdentifier;
         ProfileNode* m_head;
         ProfileNode* m_parent;
