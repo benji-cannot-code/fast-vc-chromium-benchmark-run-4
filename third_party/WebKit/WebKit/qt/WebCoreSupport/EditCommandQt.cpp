7fc77903e68e5cde1d093449f46cd6d1978bc7e4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-19 16:51:52 +0000

    2010-11-19  Sam Magnuson  <smagnuso@gmail.com>
            Reviewed by Andreas Kling.
    
            [Qt] Compile with QT_NO_UNDOCOMMAND
            https://bugs.webkit.org/show_bug.cgi?id=49752
    
            * WebCoreSupport/EditCommandQt.cpp:
            (EditCommandQt::EditCommandQt):
            * WebCoreSupport/EditCommandQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/qt/WebCoreSupport/EditCommandQt.cpp
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/EditCommandQt.cpp
@@ -23,15 +23,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-EditCommandQt::EditCommandQt(WTF::RefPtr<EditCommand> cmd, QUndoCommand *parent)
-:
 #ifndef QT_NO_UNDOCOMMAND
-    QUndoCommand(parent),
-#endif
-    m_cmd(cmd), m_first(true)
+EditCommandQt::EditCommandQt(WTF::RefPtr<EditCommand> cmd, QUndoCommand *parent)
+    : QUndoCommand(parent)
+    , m_cmd(cmd)
+    , m_first(true)
 {
 }
-
+#else
+EditCommandQt::EditCommandQt(WTF::RefPtr<EditCommand> cmd)
+    : m_cmd(cmd)
+    , m_first(true)
+{
+}
+#endif
 
 EditCommandQt::~EditCommandQt()
 {
--- third_party/WebKit/WebKit/qt/WebCoreSupport/EditCommandQt.h
+++ third_party/WebKit/WebKit/qt/WebCoreSupport/EditCommandQt.h
@@ -31,7 +31,11 @@ class EditCommandQt
 #endif
 {
     public:
+#ifndef QT_NO_UNDOCOMMAND
         EditCommandQt(WTF::RefPtr<WebCore::EditCommand> cmd, QUndoCommand *parent = 0);
+#else
+        EditCommandQt(WTF::RefPtr<WebCore::EditCommand> cmd);
+#endif
         ~EditCommandQt();
 
         void redo();
