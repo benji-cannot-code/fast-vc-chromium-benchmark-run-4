0a7dbc06bc909ab4bedeea8778fa16a6a8caea32
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-19 21:12:18 +0000

    Add a PRESUBMIT.py for Chrome Frame. As many of the checks will be common across CF and CEEE, this just reuses the ceee_presubmit.py file with some special-casing to avoid checks written for CEEE that do not apply to CF.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/5162007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66815 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome_frame/PRESUBMIT.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+
+def CheckChange(input_api, output_api, committing):
+  # We need to change the path so that we can import ceee_presubmit
+  # which lies at the root of the ceee folder. And we do it here so that
+  # it doesn't pollute all the cases where we get imported yet not called.
+  sys.path.append(os.path.join(input_api.PresubmitLocalPath(), '../ceee'))
+  import ceee_presubmit
+  return ceee_presubmit.CheckChange(input_api,
+                                    output_api,
+                                    committing,
+                                    is_chrome_frame=True)
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api, False)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api, True)
