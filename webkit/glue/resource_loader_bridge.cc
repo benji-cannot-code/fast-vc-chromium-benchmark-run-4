ad89d9be8792d2c2e5603cbf696b29465a664593
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-19 11:33:16 +0000

    DevTools: provide response code and status text as a part of raw headers data.
    BUG=44867
    TEST=
    
    Review URL: http://codereview.chromium.org/5106002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66763 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/resource_loader_bridge.cc
+++ webkit/glue/resource_loader_bridge.cc
@@ -28,7 +28,9 @@ ResourceLoadTimingInfo::ResourceLoadTimingInfo()
 ResourceLoadTimingInfo::~ResourceLoadTimingInfo() {
 }
 
-ResourceDevToolsInfo::ResourceDevToolsInfo() {}
+ResourceDevToolsInfo::ResourceDevToolsInfo()
+    : http_status_code(0) {
+}
 
 ResourceDevToolsInfo::~ResourceDevToolsInfo() {}
 
--- webkit/glue/weburlloader_impl.cc
+++ webkit/glue/weburlloader_impl.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "net/http/http_response_headers.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebHTTPHeaderVisitor.h"
-#include "third_party/WebKit/WebKit/chromium/public/WebResourceRawHeaders.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebHTTPLoadInfo.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebSecurityPolicy.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURL.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebURLError.h"
@@ -38,7 +38,7 @@ using base::TimeDelta;
 using WebKit::WebData;
 using WebKit::WebHTTPBody;
 using WebKit::WebHTTPHeaderVisitor;
-using WebKit::WebResourceRawHeaders;
+using WebKit::WebHTTPLoadInfo;
 using WebKit::WebSecurityPolicy;
 using WebKit::WebString;
 using WebKit::WebURL;
@@ -209,22 +209,26 @@ void PopulateURLResponse(
   response->setLoadTiming(timing);
 
   if (info.devtools_info.get()) {
-    WebResourceRawHeaders rawHeaders;
+    WebHTTPLoadInfo load_info;
+
+    load_info.setHTTPStatusCode(info.devtools_info->http_status_code);
+    load_info.setHTTPStatusText(WebString::fromUTF8(
+        info.devtools_info->http_status_text));
 
     const HeadersVector& request_headers = info.devtools_info->request_headers;
-    for (HeadersVector::const_iterator it = request_headers .begin();
+    for (HeadersVector::const_iterator it = request_headers.begin();
          it != request_headers.end(); ++it) {
-      rawHeaders.addRequestHeader(WebString::fromUTF8(it->first),
+      load_info.addRequestHeader(WebString::fromUTF8(it->first),
           WebString::fromUTF8(it->second));
     }
     const HeadersVector& response_headers =
         info.devtools_info->response_headers;
     for (HeadersVector::const_iterator it = response_headers.begin();
          it != response_headers.end(); ++it) {
-      rawHeaders.addResponseHeader(WebString::fromUTF8(it->first),
+      load_info.addResponseHeader(WebString::fromUTF8(it->first),
           WebString::fromUTF8(it->second));
     }
-    response->setResourceRawHeaders(rawHeaders);
+    response->setHTTPLoadInfo(load_info);
   }
 
   const net::HttpResponseHeaders* headers = info.headers;
