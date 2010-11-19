05ec406dae477fe9ba4e0cb0e20c41f1044dd7dc
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-19 20:48:15 +0000

    Revert 66722 - This prevents crashes due to URLRequestHttpJobs hanging on past IOThread destruction.
    BUG=chromium-os:8179,63692,63796
    TEST=sync integration tests
    
    Review URL: http://codereview.chromium.org/5163004
    
    For revert info, see bug 63796.
    
    TBR=akalin@chromium.org
    Review URL: http://codereview.chromium.org/5144006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66813 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -78,6 +78,7 @@ URLRequestJob* URLRequestHttpJob::Factory(URLRequest* request,
 
 URLRequestHttpJob::URLRequestHttpJob(URLRequest* request)
     : URLRequestJob(request),
+      context_(request->context()),
       response_info_(NULL),
       response_cookies_save_index_(0),
       proxy_auth_state_(net::AUTH_STATE_DONT_NEED_AUTH),
@@ -609,7 +610,6 @@ void URLRequestHttpJob::DestroyTransaction() {
 
   transaction_.reset();
   response_info_ = NULL;
-  context_ = NULL;
 }
 
 void URLRequestHttpJob::StartTransaction() {
@@ -632,9 +632,6 @@ void URLRequestHttpJob::StartTransaction() {
     if (rv == net::OK) {
       rv = transaction_->Start(
           &request_info_, &start_callback_, request_->net_log());
-      // Make sure the context is alive for the duration of the
-      // transaction.
-      context_ = request_->context();
     }
   }
 
