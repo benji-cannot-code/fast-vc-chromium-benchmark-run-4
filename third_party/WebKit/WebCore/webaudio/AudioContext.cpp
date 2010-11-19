1e4033a0b1b48cdbac719433fa3e2046e17f15f2
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-19 20:27:05 +0000

    2010-11-19  Chris Rogers  <crogers@google.com>
            Reviewed by Darin Adler.
    
            AudioContext should not include "HTMLNames.h"
            https://bugs.webkit.org/show_bug.cgi?id=49680
    
            No new tests since audio API is not yet implemented.
    
            * webaudio/AudioContext.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/webaudio/AudioContext.cpp
+++ third_party/WebKit/WebCore/webaudio/AudioContext.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "HRTFDatabaseLoader.h"
 #include "HRTFPanner.h"
-#include "HTMLNames.h"
 #include "HighPass2FilterNode.h"
 #include "JavaScriptAudioNode.h"
 #include "LowPass2FilterNode.h"
