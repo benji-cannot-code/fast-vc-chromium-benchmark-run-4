2331a8483e8ec405645971ed9769eb75ffb228ec
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-20 04:40:32 +0000

    2010-11-19  William Chan  <willchan@chromium.org>
            Reviewed by David Levin.
    
            Add USE(CHROMIUM_NET)
    
            Indicates the use of Chromium's network stack.  Chromium's network
            stack performs better when it has full view of all resource requests,
            so USE(CHROMIUM_NET) can be used to bypass throttles.
    
            https://bugs.webkit.org/show_bug.cgi?id=49778
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -542,6 +542,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_ICCJPEG 1
 #else
 #define WTF_PLATFORM_SKIA 1
+#define WTF_USE_CHROMIUM_NET 1
 #endif
 #endif
 
@@ -1125,4 +1126,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #endif /* WTF_Platform_h */
-
