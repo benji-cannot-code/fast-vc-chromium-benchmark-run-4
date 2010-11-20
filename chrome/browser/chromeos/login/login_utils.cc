f425f3c9c5fcd8efbf58b66f6507c5942037c9e5
cmasone@chromium.org <cmasone@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-20 02:27:19 +0000

    [Chrome OS] Remove obsolete PipeReader, ExternalCookieFetcher classes
    One of the PipeReaderTest tests is flaky.  That code is obsolete anyway, so just remove all of it.
    
    BUG=63228
    TEST=chrome, test targets compile without this code
    
    Review URL: http://codereview.chromium.org/5203004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
 #include "chrome/browser/chromeos/cros/network_library.h"
-#include "chrome/browser/chromeos/external_cookie_handler.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 #include "chrome/browser/chromeos/login/cookie_fetcher.h"
 #include "chrome/browser/chromeos/login/google_authenticator.h"
