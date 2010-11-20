9c1365dd0e6611df7087217e7d3f8eaa44c6a30a
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-20 18:54:43 +0000

    <http://webkit.org/b/49848> Make it possible to display the last character of a secure text field unobscured
    Reviewed by Darin Adler.
    
    * JavaScriptCore.exp:
    * wtf/text/StringImpl.cpp:
    (WTF::StringImpl::secure): Added argument that controls whether
    the last character is obscured or not.  Implemented behavior.
    * wtf/text/StringImpl.h:
    (WTF::StringImpl::LastCharacterBehavior): Added enum.
    (WTF::StringImpl::secure): Updated method signature.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/text/StringImpl.h
+++ third_party/WebKit/JavaScriptCore/wtf/text/StringImpl.h
@@ -285,7 +285,10 @@ public:
 
     PassRefPtr<StringImpl> lower();
     PassRefPtr<StringImpl> upper();
-    PassRefPtr<StringImpl> secure(UChar aChar);
+
+    enum LastCharacterBehavior { ObscureLastCharacter, DisplayLastCharacter };
+
+    PassRefPtr<StringImpl> secure(UChar, LastCharacterBehavior = ObscureLastCharacter);
     PassRefPtr<StringImpl> foldCase();
 
     PassRefPtr<StringImpl> stripWhiteSpace();
