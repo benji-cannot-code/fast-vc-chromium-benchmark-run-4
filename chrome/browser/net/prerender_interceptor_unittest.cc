e75957060a372a21c070f10ebebf2c73a78e0db4
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-21 05:37:47 +0000

    Fix PrerenderInterceptor leaks.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/5109012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/prerender_interceptor_unittest.cc
+++ chrome/browser/net/prerender_interceptor_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/compiler_specific.h"
 #include "base/file_path.h"
 #include "base/message_loop_proxy.h"
 #include "base/scoped_ptr.h"
@@ -16,11 +17,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/test/test_server.h"
 #include "net/url_request/url_request_unittest.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace chrome_browser_net {
 
 class PrerenderInterceptorTest : public testing::Test {
- public:
+ protected:
   PrerenderInterceptorTest();
 
   void MakeTestUrl(const std::string& base);
@@ -30,6 +32,7 @@ class PrerenderInterceptorTest : public testing::Test {
   GURL gurl_;
   GURL last_intercepted_gurl_;
   scoped_ptr<URLRequest> req_;
+
  private:
   void SetLastInterceptedGurl(const GURL& url);
 
@@ -37,6 +40,7 @@ class PrerenderInterceptorTest : public testing::Test {
   MessageLoopForIO io_loop_;
   scoped_refptr<base::MessageLoopProxy> io_message_loop_proxy_;
   BrowserThread ui_thread_;
+  TestDelegate delegate_;
 };
 
 PrerenderInterceptorTest::PrerenderInterceptorTest()
@@ -60,12 +64,14 @@ void PrerenderInterceptorTest::SetUp() {
 
 void PrerenderInterceptorTest::MakeTestUrl(const std::string& base) {
   gurl_ = test_server_.GetURL(base);
-  req_.reset(new TestURLRequest(gurl_, new TestDelegate()));
+  req_.reset(new TestURLRequest(gurl_, &delegate_));
 }
 
 void PrerenderInterceptorTest::SetLastInterceptedGurl(const GURL& url) {
   last_intercepted_gurl_ = url;
 }
+
+namespace {
 
 TEST_F(PrerenderInterceptorTest, Interception) {
   MakeTestUrl("files/prerender/doc1.html");
@@ -97,5 +103,6 @@ TEST_F(PrerenderInterceptorTest, WrongMimeType) {
   EXPECT_NE(gurl_, last_intercepted_gurl_);
 }
 
-}  // namespace chrome_browser_net
+}  // namespace
 
+}  // namespace chrome_browser_net
