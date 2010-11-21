361f8e458e4cba97264ed61238ae8320f7acd07d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-21 00:27:23 +0000

    2010-11-20  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by David Kilzer.
    
            [BREWMP] Replace HTONL, HTONS, NTOHL and NTOHS with byte order conversion functions from AEEstd.h
            https://bugs.webkit.org/show_bug.cgi?id=49518
    
            HTONL, HTONS, NTOHL and NTOHS macros are prohibited in Mod1. Use byte order conversion functions provided by AEEstd.h.
    
            * platform/graphics/WOFFFileFormat.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
+++ third_party/WebKit/WebCore/platform/graphics/WOFFFileFormat.cpp
@@ -37,11 +37,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if PLATFORM(BREWMP)
-#include <AEEStdLib.h>
-#define htonl(x) HTONL(x)
-#define htons(x) HTONS(x)
-#define ntohl(x) NTOHL(x)
-#define ntohs(x) NTOHS(x)
+#include <AEEstd.h>
+#define htonl(x) std_htonl(x)
+#define htons(x) std_htons(x)
+#define ntohl(x) std_ntohl(x)
+#define ntohs(x) std_ntohs(x)
 #endif
 
 #if PLATFORM(WIN)
