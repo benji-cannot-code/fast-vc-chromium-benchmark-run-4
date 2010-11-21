b85c8774413a20781360f9daae25f7a0c1503fd8
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-21 17:09:31 +0000

    2010-11-21  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Csaba Osztrogonác.
    
            Use WTF_ARRAY_LENGTH in WebCore/platform directory
            https://bugs.webkit.org/show_bug.cgi?id=49881
    
            * platform/KURL.cpp:
            (WebCore::portAllowed):
            * platform/KURLGoogle.cpp:
            (WebCore::portAllowed):
            * platform/MIMETypeRegistry.cpp:
            (WebCore::initializeSupportedImageMIMETypes):
            (WebCore::initializeSupportedJavaScriptMIMETypes):
            (WebCore::initializeSupportedNonImageMimeTypes):
            * platform/UUID.cpp:
            (WebCore::createCanonicalUUIDString):
            * platform/efl/RenderThemeEfl.cpp:
            (WebCore::RenderThemeEfl::applyEdjeStateFromForm):
            * platform/graphics/GraphicsTypes.cpp:
            * platform/graphics/chromium/FontCacheChromiumWin.cpp:
            (WebCore::FontCodepage::if):
            * platform/graphics/chromium/FontUtilsChromiumWin.cpp:
            * platform/graphics/mac/ComplexTextControllerCoreText.cpp:
            (WebCore::ComplexTextController::collectComplexTextRunsForCharactersCoreText):
            * platform/graphics/mac/MediaPlayerPrivateQTKit.mm:
            (WebCore::disableComponentsOnce):
            * platform/graphics/mac/SimpleFontDataATSUI.mm:
            (WebCore::SimpleFontData::checkShapesArabic):
            * platform/graphics/mac/SimpleFontDataCoreText.cpp:
            (WebCore::SimpleFontData::getCFStringAttributes):
            * platform/graphics/win/GraphicsContextCGWin.cpp:
            (WebCore::GraphicsContext::drawLineForTextChecking):
            * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
            (WebCore::disableComponentsOnce):
            * platform/graphics/win/QTMovie.cpp:
            (QTMovie::load):
            * platform/network/cf/SocketStreamHandleCFNet.cpp:
            (WebCore::SocketStreamHandle::createStreams):
            * platform/text/TextEncodingRegistry.cpp:
            (WebCore::pruneBlacklistedCodecs):
            * platform/text/transcoder/FontTranscoder.cpp:
            (WebCore::FontTranscoder::FontTranscoder):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/MIMETypeRegistry.cpp
+++ third_party/WebKit/WebCore/platform/MIMETypeRegistry.cpp
@@ -120,7 +120,7 @@ static void initializeSupportedImageMIMETypes()
         "image/x-icon",    // ico
         "image/x-xbitmap"  // xbm
     };
-    for (size_t i = 0; i < sizeof(types) / sizeof(types[0]); ++i) {
+    for (size_t i = 0; i < WTF_ARRAY_LENGTH(types); ++i) {
         supportedImageMIMETypes->add(types[i]);
         supportedImageResourceMIMETypes->add(types[i]);
     }
@@ -188,7 +188,7 @@ static void initializeSupportedJavaScriptMIMETypes()
         "text/jscript",
         "text/livescript",
     };
-    for (size_t i = 0; i < sizeof(types) / sizeof(types[0]); ++i)
+    for (size_t i = 0; i < WTF_ARRAY_LENGTH(types); ++i)
       supportedJavaScriptMIMETypes->add(types[i]);
 }
 
@@ -223,7 +223,7 @@ static void initializeSupportedNonImageMimeTypes()
     COMPILE_ASSERT(sizeof(types) / sizeof(types[0]) <= 16,
                    nonimage_mime_types_must_be_less_than_or_equal_to_16);
 
-    for (size_t i = 0; i < sizeof(types)/sizeof(types[0]); ++i)
+    for (size_t i = 0; i < WTF_ARRAY_LENGTH(types); ++i)
         supportedNonImageMIMETypes->add(types[i]);
 
     ArchiveFactory::registerKnownArchiveMIMETypes();
