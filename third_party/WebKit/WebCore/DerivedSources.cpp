4b3a5cfa332f89e2314f9700450c0121b9385b47
luiz@webkit.org <luiz@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-22 22:01:01 +0000

    2010-11-22  Luiz Agostini  <luiz.agostini@openbossa.org>
            Unreviewed, Windows build fix.
    
            Adding JSMediaQueryList to DerivedSources.cpp.
    
            * DerivedSources.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/DerivedSources.cpp
+++ third_party/WebKit/WebCore/DerivedSources.cpp
@@ -225,6 +225,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSLocation.cpp"
 #include "JSMediaError.cpp"
 #include "JSMediaList.cpp"
+#include "JSMediaQueryList.cpp"
 #include "JSMemoryInfo.cpp"
 #include "JSMessageChannel.cpp"
 #include "JSMessageEvent.cpp"
