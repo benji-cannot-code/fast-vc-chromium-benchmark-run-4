99ab7bbb5357fdef54fb5d98850cf4d66523a37e
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-22 15:42:50 +0000

    Disable content settings UI for managed default content settings (View). BUG=63178 TEST=none
    Review URL: http://codereview.chromium.org/5137010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/options/content_filter_page_view.h
+++ chrome/browser/ui/views/options/content_filter_page_view.h
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_OPTIONS_CONTENT_FILTER_PAGE_VIEW_H_
 #pragma once
 
+#include "chrome/browser/host_content_settings_map.h"
 #include "chrome/browser/views/options/options_page_view.h"
 #include "chrome/common/content_settings_types.h"
+#include "chrome/common/notification_registrar.h"
 #include "views/controls/button/button.h"
 
 namespace views {
@@ -28,7 +30,18 @@ class ContentFilterPageView : public OptionsPageView,
   ContentFilterPageView(Profile* profile, ContentSettingsType content_type);
   virtual ~ContentFilterPageView();
 
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details);
  protected:
+  // Updates the state of the UI. UpdateView is called when the content filter
+  // page is initialized and after the content setting changed (either the
+  // value or the is managed state).
+  virtual void UpdateView();
+
+  virtual void NotifyContentSettingsChanged(
+      const HostContentSettingsMap::ContentSettingsDetails *details);
+
   // OptionsPageView implementation:
   virtual void InitControlLayout();
 
@@ -43,6 +56,8 @@ class ContentFilterPageView : public OptionsPageView,
   views::RadioButton* ask_radio_;
   views::RadioButton* block_radio_;
   views::NativeButton* exceptions_button_;
+
+  NotificationRegistrar registrar_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(ContentFilterPageView);
 };
