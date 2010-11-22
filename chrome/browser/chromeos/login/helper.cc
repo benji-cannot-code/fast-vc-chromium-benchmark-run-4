944442ae0e6e059e66fdaca23eb66998a42b7403
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-22 19:15:11 +0000

    [cros] Use WideButton on EULA, Sign In screens. Adjust "sign in" button height.
    BUG=chromium-os:8101
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/5192005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66982 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/helper.cc
+++ chrome/browser/chromeos/login/helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 #include "grit/theme_resources.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
+#include "views/controls/button/menu_button.h"
 #include "views/controls/button/native_button.h"
 #include "views/controls/label.h"
 #include "views/controls/textfield/textfield.h"
@@ -103,6 +104,11 @@ void CorrectLabelFontSize(views::Label* label) {
     label->SetFont(label->font().DeriveFont(kFontSizeCorrectionDelta));
 }
 
+void CorrectMenuButtonFontSize(views::MenuButton* button) {
+  if (button)
+    button->SetFont(button->font().DeriveFont(kFontSizeCorrectionDelta));
+}
+
 void CorrectNativeButtonFontSize(views::NativeButton* button) {
   if (button)
     button->set_font(button->font().DeriveFont(kFontSizeCorrectionDelta));
@@ -122,8 +128,14 @@ namespace login {
 // Minimal width for the button.
 const int kButtonMinWidth = 90;
 
+// Button should have the same height as textfield.
+const int kButtonHeightDelta = 2;
+
 gfx::Size WideButton::GetPreferredSize() {
   gfx::Size preferred_size = NativeButton::GetPreferredSize();
+  // Decrease vertical margins.
+  preferred_size.set_height(preferred_size.height() - kButtonHeightDelta);
+  // Set minimal width.
   if (preferred_size.width() < kButtonMinWidth)
     preferred_size.set_width(kButtonMinWidth);
   return preferred_size;
