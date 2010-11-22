fba69f9c85289b73a4e1ab4facd994736a6ce51c
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-22 23:41:23 +0000

    Remove Feedback button from the status bar and add it to the toolbar. Side effect: Changed all instances of IDC_REPORT_BUG to IDC_FEEDBACK to correctly indicate the message
    BUG=chromium-os:5361
    TEST=Verified visually that the status bar icon is gone and the toolbar icon
    is there; tested the toolbar icon to verify functionality.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=65582
    
    Review URL: http://codereview.chromium.org/4546002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67023 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/status_area_view.h
+++ chrome/browser/chromeos/status/status_area_view.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 class ClockMenuButton;
-class FeedbackMenuButton;
 class InputMethodMenuButton;
 class NetworkMenuButton;
 class PowerMenuButton;
@@ -36,7 +35,6 @@ class StatusAreaView : public AccessiblePaneView {
   virtual void ChildPreferredSizeChanged(View* child);
 
   ClockMenuButton* clock_view() { return clock_view_; }
-  FeedbackMenuButton* feedback_view() { return feedback_view_; }
   InputMethodMenuButton* input_method_view() { return input_method_view_; }
   NetworkMenuButton* network_view() { return network_view_; }
   PowerMenuButton* power_view() { return power_view_; }
@@ -45,7 +43,6 @@ class StatusAreaView : public AccessiblePaneView {
   StatusAreaHost* host_;
 
   ClockMenuButton* clock_view_;
-  FeedbackMenuButton* feedback_view_;
   InputMethodMenuButton* input_method_view_;
   NetworkMenuButton* network_view_;
   PowerMenuButton* power_view_;
--- chrome/browser/views/toolbar_view.h
+++ chrome/browser/views/toolbar_view.h
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_VIEWS_TOOLBAR_VIEW_H_
 #pragma once
 
-#include "chrome/browser/ui/views/toolbar_view.h"
+ #include "chrome/browser/ui/views/toolbar_view.h"
 // TODO(beng): remove this file once all includes have been updated.
 
 #endif  // CHROME_BROWSER_VIEWS_TOOLBAR_VIEW_H_
-
