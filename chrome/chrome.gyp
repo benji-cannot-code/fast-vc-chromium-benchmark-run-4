442b83e4673ef40eee9ba142221ad5cbd4c551c0
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-23 15:01:22 +0000

    Add infrastructure enabling tests of installer upgrade scenarios.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/5236002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome.gyp
+++ chrome/chrome.gyp
@@ -1626,6 +1626,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'type': 'none',
           'dependencies': [
             'installer/mini_installer.gyp:*',
+            'installer/upgrade_test.gyp:*',
             '../app/app.gyp:*',
             '../base/base.gyp:*',
             '../ceee/ceee.gyp:*',
---
+++ chrome/installer/test/alternate_version_generator.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This file exposes the public interface to the mini_installer re-versioner.
+
+#ifndef CHROME_INSTALLER_TEST_ALTERNATE_VERSION_GENERATOR_H_
+#define CHROME_INSTALLER_TEST_ALTERNATE_VERSION_GENERATOR_H_
+#pragma once
+
+#include <string>
+
+class FilePath;
+
+namespace upgrade_test {
+
+// Generates an alternate mini_installer.exe using the one indicated by
+// |original_installer_path|, giving the new one a higher version than the
+// original and placing it in |target_path|.  Any previous file at |target_path|
+// is clobbered.  Returns true on success.
+bool GenerateNextVersion(const FilePath& original_installer_path,
+                         const FilePath& target_path);
+
+}  // namespace upgrade_test
+
+#endif  // CHROME_INSTALLER_TEST_ALTERNATE_VERSION_GENERATOR_H_
