bcadbbf25310b07036a910a6a66959f0fa9195c3
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-23 03:09:56 +0000

    Fix tests. TBR=stevenjb@chromium.org Review URL: http://codereview.chromium.org/5310002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67053 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cros_mock.cc
+++ chrome/browser/chromeos/cros/cros_mock.cc
@@ -284,19 +284,19 @@ void CrosMock::SetNetworkLibraryStatusAreaExpectations() {
       .WillRepeatedly((Return((const Network*)(NULL))))
       .RetiresOnSaturation();
   EXPECT_CALL(*mock_network_library_, wifi_connecting())
-      .Times(1)
+      .Times(AnyNumber())
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
   EXPECT_CALL(*mock_network_library_, cellular_connecting())
-      .Times(1)
+      .Times(AnyNumber())
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
   EXPECT_CALL(*mock_network_library_, Connected())
-      .Times(2)
+      .Times(AnyNumber())
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
   EXPECT_CALL(*mock_network_library_, Connecting())
-      .Times(1)
+      .Times(AnyNumber())
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
 
@@ -330,10 +330,6 @@ void CrosMock::SetNetworkLibraryStatusAreaExpectations() {
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
   EXPECT_CALL(*mock_network_library_, cellular_enabled())
-      .Times(1)
-      .WillRepeatedly((Return(false)))
-      .RetiresOnSaturation();
-  EXPECT_CALL(*mock_network_library_, Connected())
       .Times(1)
       .WillRepeatedly((Return(false)))
       .RetiresOnSaturation();
