94416a7fe8ec2f259a780d8d9509479f0488e0b5
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-23 01:50:20 +0000

    2010-11-22  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Adam Barth.
    
            Fix bogus include guard.
    
            * loader/NavigationScheduler.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/NavigationScheduler.h
+++ third_party/WebKit/WebCore/loader/NavigationScheduler.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef RedirectScheduler_h
-#define RedirectScheduler_h
+#ifndef NavigationScheduler_h
+#define NavigationScheduler_h
 
 #include "Event.h"
 #include "Timer.h"
@@ -80,4 +80,4 @@ private:
 
 } // namespace WebCore
 
-#endif // RedirectScheduler_h
+#endif // NavigationScheduler_h
