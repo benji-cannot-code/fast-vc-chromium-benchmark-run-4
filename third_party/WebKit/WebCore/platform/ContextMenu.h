c32239afa6ce75b6e42f81a4ba0ac29d6102680f
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-23 06:26:26 +0000

    2010-11-22  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Roben.
    
            Port ContextMenuWin.cpp to WinCE
            https://bugs.webkit.org/show_bug.cgi?id=48408
    
            * platform/ContextMenu.h:
            * platform/win/ContextMenuWin.cpp:
            (WebCore::ContextMenu::ContextMenu):
            (WebCore::ContextMenu::itemCount):
            (WebCore::insertMenuItem):
            (WebCore::ContextMenu::insertItem):
            (WebCore::contextMenuItemByIdOrPosition):
            (WebCore::ContextMenu::setPlatformDescription):
            (WebCore::contextMenuItemVector):
            (WebCore::platformMenuDescription):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ContextMenu.h
+++ third_party/WebKit/WebCore/platform/ContextMenu.h
@@ -89,6 +89,9 @@ namespace WebCore {
         PlatformMenuDescription m_platformDescription;
 #else
         PlatformMenuDescription m_platformDescription;
+#if OS(WINCE)
+        unsigned m_itemCount;
+#endif
 #endif
     };
 
