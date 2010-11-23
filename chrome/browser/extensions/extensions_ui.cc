586d495632ee9eaf1ed1d076046ab3dbefd473f4
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-23 09:55:36 +0000

    Add Show Button to the Extensions page. Used for a Browser Action button that has been hidden.
    BUG=57824
    TEST=None
    
    Review URL: http://codereview.chromium.org/5158009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extensions_ui.cc
+++ chrome/browser/extensions/extensions_ui.cc
@@ -170,6 +170,8 @@ void ExtensionsUIHTMLSource::StartDataRequest(const std::string& path,
       l10n_util::GetStringUTF16(IDS_OK));
   localized_strings.SetString("cancelButton",
       l10n_util::GetStringUTF16(IDS_CANCEL));
+  localized_strings.SetString("showButton",
+      l10n_util::GetStringUTF16(IDS_EXTENSIONS_SHOW_BUTTON));
 
   SetFontAndTextDirection(&localized_strings);
 
@@ -312,7 +314,9 @@ void ExtensionsDOMHandler::RegisterMessages() {
   dom_ui_->RegisterMessageCallback("uninstall",
       NewCallback(this, &ExtensionsDOMHandler::HandleUninstallMessage));
   dom_ui_->RegisterMessageCallback("options",
-    NewCallback(this, &ExtensionsDOMHandler::HandleOptionsMessage));
+      NewCallback(this, &ExtensionsDOMHandler::HandleOptionsMessage));
+  dom_ui_->RegisterMessageCallback("showButton",
+      NewCallback(this, &ExtensionsDOMHandler::HandleShowButtonMessage));
   dom_ui_->RegisterMessageCallback("load",
       NewCallback(this, &ExtensionsDOMHandler::HandleLoadMessage));
   dom_ui_->RegisterMessageCallback("pack",
@@ -404,6 +408,9 @@ void ExtensionsDOMHandler::OnIconsLoaded(DictionaryValue* json) {
       NotificationService::AllSources());
   registrar_.Add(this,
       NotificationType::BACKGROUND_CONTENTS_DELETED,
+      NotificationService::AllSources());
+  registrar_.Add(this,
+      NotificationType::EXTENSION_BROWSER_ACTION_VISIBILITY_CHANGED,
       NotificationService::AllSources());
 }
 
@@ -548,6 +555,11 @@ void ExtensionsDOMHandler::HandleOptionsMessage(const ListValue* args) {
     return;
   dom_ui_->GetProfile()->GetExtensionProcessManager()->OpenOptionsPage(
       extension, NULL);
+}
+
+void ExtensionsDOMHandler::HandleShowButtonMessage(const ListValue* args) {
+  const Extension* extension = GetExtension(args);
+  extensions_service_->SetBrowserActionVisibility(extension, true);
 }
 
 void ExtensionsDOMHandler::HandleLoadMessage(const ListValue* args) {
@@ -697,6 +709,7 @@ void ExtensionsDOMHandler::Observe(NotificationType type,
     case NotificationType::EXTENSION_FUNCTION_DISPATCHER_DESTROYED:
     case NotificationType::NAV_ENTRY_COMMITTED:
     case NotificationType::BACKGROUND_CONTENTS_NAVIGATED:
+    case NotificationType::EXTENSION_BROWSER_ACTION_VISIBILITY_CHANGED:
       MaybeUpdateAfterNotification();
       break;
     default:
@@ -800,6 +813,9 @@ DictionaryValue* ExtensionsDOMHandler::CreateExtensionDetailValue(
 
   if (!extension->options_url().is_empty())
     extension_data->SetString("options_url", extension->options_url().spec());
+
+  if (service && !service->GetBrowserActionVisibility(extension))
+    extension_data->SetBoolean("enable_show_button", true);
 
   // Add list of content_script detail DictionaryValues.
   ListValue *content_script_list = new ListValue();
