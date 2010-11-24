eeea4fe73852ec5c488eec121160057c3652c2ac
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 04:38:03 +0000

    Fix in BrowserTest.PosixSessionEnd in ui_tests failure on dbg build for Chrome OS
    The test failed as use of Singleton is not allowed in non-joinable threads
    since http://src.chromium.org/viewvc/chrome?view=rev&revision=66719
    
    We could make BootTimesLoader a leaky singoeton to fix the failure, but
    instead, the change removes the use of BootTimesLoader in the shutdown
    detector thread. The time spent from the previous check point (LogoutStarted)
    is negligible as shown in http://codereview.chromium.org/4973002/ hence it
    should be fine.
    
    BUG=chromium-os:9592
    TEST=out/Debug/ui_tests --gtest_filter='BrowserTest.PosixSessionEnd'
    
    Review URL: http://codereview.chromium.org/5339007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main_posix.cc
+++ chrome/browser/browser_main_posix.cc
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_thread.h"
 #include "chrome/common/chrome_switches.h"
 
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/boot_times_loader.h"
-#endif
-
 namespace {
 
 // See comment in |PreEarlyInitialization()|, where sigaction is called.
@@ -114,10 +110,6 @@ void ShutdownDetector::ThreadMain() {
   } while (bytes_read < sizeof(signal));
 
   VLOG(1) << "Handling shutdown for signal " << signal << ".";
-#if defined(OS_CHROMEOS)
-  chromeos::BootTimesLoader::Get()->AddLogoutTimeMarker("ShutdownDetected",
-                                                        false);
-#endif
 
   if (!BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
