34af98f60e96e05c957c92524f4512728834b346
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 23:22:08 +0000

    Histograms added for GPU Process Crashed, Died, Launched
    BUG=62865
    TEST=none
    
    Review URL: http://codereview.chromium.org/5299003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67341 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_process_host.cc
+++ chrome/browser/gpu_process_host.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/app_switches.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram.h"
 #include "base/thread.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_thread.h"
@@ -28,6 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+
+enum GPUProcessLifetimeEvent {
+  kLaunched,
+  kCrashed,
+  kGPUProcessLifetimeEvent_Max
+  };
 
 // Tasks used by this file
 class RouteOnUIThreadTask : public Task {
@@ -344,6 +351,14 @@ bool GpuProcessHost::CanShutdown() {
   return true;
 }
 
+void GpuProcessHost::OnChildDied() {
+  // Located in OnChildDied because OnProcessCrashed suffers from a race
+  // condition on Linux. The GPU process will only die if it crashes.
+  UMA_HISTOGRAM_ENUMERATION("GPU.GPUProcessLifetimeEvents",
+                            kCrashed, kGPUProcessLifetimeEvent_Max);
+  BrowserChildProcessHost::OnChildDied();
+}
+
 void GpuProcessHost::OnProcessCrashed() {
   // TODO(alokp): Update gpu process crash rate.
   BrowserChildProcessHost::OnProcessCrashed();
@@ -394,6 +409,8 @@ bool GpuProcessHost::LaunchGpuProcess() {
 #endif
       cmd_line);
 
+  UMA_HISTOGRAM_ENUMERATION("GPU.GPUProcessLifetimeEvents",
+                            kLaunched, kGPUProcessLifetimeEvent_Max);
   return true;
 }
 
