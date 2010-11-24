7e2adb616e2062518f7918e2ff7a0ef9167995a6
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 23:33:11 +0000

    This is another try to commit OOM priority management.
    This changes nothing, except that I now initialize memory_used in oom_priority_manager.cc to placate the incorrect gcc warning about it being used when uninitialized.
    
    The original review is here: http://codereview.chromium.org/4498001/
    
    BUG=none
    TEST=Passed chromeos try-bot, built both debug and release locally.
    
    Review URL: http://codereview.chromium.org/5328003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model.cc
+++ chrome/browser/tabs/tab_strip_model.cc
@@ -478,6 +478,7 @@ void TabStripModel::SetTabPinned(int index, bool pinned) {
 }
 
 bool TabStripModel::IsTabPinned(int index) const {
+  DCHECK(ContainsIndex(index));
   return contents_data_[index]->pinned;
 }
 
@@ -962,7 +963,8 @@ void TabStripModel::ChangeSelectedContentsFrom(
   ObserverListBase<TabStripModelObserver>::Iterator it(observers_);
   TabStripModelObserver* obs;
   while ((obs = it.GetNext()) != NULL)
-    obs->TabSelectedAt(last_selected_contents, new_contents, selected_index_, user_gesture);
+    obs->TabSelectedAt(last_selected_contents, new_contents,
+                       selected_index_, user_gesture);
   /*
   FOR_EACH_OBSERVER(TabStripModelObserver, observers_,
       TabSelectedAt(last_selected_contents, new_contents, selected_index_,
