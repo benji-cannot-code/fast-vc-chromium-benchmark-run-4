59be710754eb763926b8c4def7e166c682d2ad9a
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 20:16:03 +0000

    Monitor sleep/wake on Mac. Should fix some networking issues arising after sleep.
    BUG=29669
    TEST=turn logging up; see logging about power being suspended/resumed when Mac sleeps/awakes
    
    Review URL: http://codereview.chromium.org/5310005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67301 0039d316-1c4b-4281-b951-d872f2087c98

--- app/system_monitor.h
+++ app/system_monitor.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,6 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(ENABLE_BATTERY_MONITORING)
 #include "base/timer.h"
 #endif  // defined(ENABLE_BATTERY_MONITORING)
+
+#if defined(OS_MACOSX)
+#ifdef __OBJC__
+@class SystemMonitorBridge;
+#else
+class SystemMonitorBridge;
+#endif
+#endif
 
 // Class for monitoring various system-related subsystems
 // such as power management, network status, etc.
@@ -95,6 +103,11 @@ class SystemMonitor {
   void ProcessPowerMessage(PowerEvent event_id);
 
  private:
+#if defined(OS_MACOSX)
+  void PlatformInit();
+  void PlatformDestroy();
+#endif
+
   // Platform-specific method to check whether the system is currently
   // running on battery power.  Returns true if running on batteries,
   // false otherwise.
@@ -115,6 +128,10 @@ class SystemMonitor {
 
 #if defined(ENABLE_BATTERY_MONITORING)
   base::OneShotTimer<SystemMonitor> delayed_battery_check_;
+#endif
+
+#if defined(OS_MACOSX)
+  SystemMonitorBridge* system_monitor_bridge_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(SystemMonitor);
