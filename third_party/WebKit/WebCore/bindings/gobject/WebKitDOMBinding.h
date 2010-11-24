cb04c291ac6cc80cbb8a212e9ac7a1f642702c99
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-24 19:23:27 +0000

    2010-11-24  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Martin Robinson.
    
            * bindings/gobject/WebKitDOMBinding.h: fix stlye nit.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/gobject/WebKitDOMBinding.h
+++ third_party/WebKit/WebCore/bindings/gobject/WebKitDOMBinding.h
@@ -25,8 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebKitDOMBinding_h
 #define WebKitDOMBinding_h
 
-#include <glib.h>
 #include "webkit/webkitdomdefines.h"
+#include <glib.h>
 
 namespace WebCore {
 class Node;
