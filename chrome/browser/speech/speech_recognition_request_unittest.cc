adecaf8063e183ba8dc88801b5e610c6ab6ea1eb
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 18:49:37 +0000

    Used a MessageLoopProxy to store the delegate's message loop in URLFetcher::Core. This allows the thread that created the URLFetcher object to go away without causing the IO thread to crash.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/5283001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_recognition_request_unittest.cc
+++ chrome/browser/speech/speech_recognition_request_unittest.cc
@@ -38,6 +38,7 @@ class SpeechRecognitionRequestTest : public SpeechRecognitionRequestDelegate,
   }
 
  protected:
+  MessageLoop message_loop_;
   TestURLFetcherFactory url_fetcher_factory_;
   bool error_;
   SpeechInputResultArray result_;
