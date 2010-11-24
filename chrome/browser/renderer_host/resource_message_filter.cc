f7d92794873c938a8682b30af02c0762d419f090
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 21:12:56 +0000

    Remove messageloop DCHECK in keygen which triggers thread restriction assert.
    BUG=none
    TEST=use site with keygen in debug build (ex http://foaf.me)
    
    Review URL: http://codereview.chromium.org/5317004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/resource_message_filter.cc
+++ chrome/browser/renderer_host/resource_message_filter.cc
@@ -1580,8 +1580,6 @@ void ResourceMessageFilter::OnKeygenOnWorkerThread(
     const GURL& url,
     IPC::Message* reply_msg) {
   DCHECK(reply_msg);
-  // Verify we are on a worker thread.
-  DCHECK(!MessageLoop::current());
 
   // Generate a signed public key and challenge, then send it back.
   net::KeygenHandler keygen_handler(key_size_in_bits, challenge_string, url);
