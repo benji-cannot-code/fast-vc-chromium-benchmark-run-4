159a7ea6f50b58fa85b97ade21e6fd3370b2609f
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 18:35:17 +0000

    The earlier TabContentsWrapper changes had incomplete support for password manager.
    This wires up Password Manager properly, making sure it observes all notifications that it needs to function.
    
    http://crbug.com/63664
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/5311006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents_wrapper.cc
+++ chrome/browser/tab_contents_wrapper.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager_delegate_impl.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 
+////////////////////////////////////////////////////////////////////////////////
+// TabContentsWrapper, public:
 
 TabContentsWrapper::TabContentsWrapper(TabContents* contents)
     : tab_contents_(contents) {
@@ -17,6 +19,9 @@ TabContentsWrapper::TabContentsWrapper(TabContents* contents)
   // Stash this in the property bag so it can be retrieved without having to
   // go to a Browser.
   property_accessor()->SetProperty(contents->property_bag(), this);
+
+  // Needed so that we initialize the password manager on first navigation.
+  tab_contents()->AddNavigationObserver(this);
 }
 
 TabContentsWrapper::~TabContentsWrapper() {
@@ -26,6 +31,15 @@ TabContentsWrapper::~TabContentsWrapper() {
 
 PropertyAccessor<TabContentsWrapper*>* TabContentsWrapper::property_accessor() {
   return Singleton< PropertyAccessor<TabContentsWrapper*> >::get();
+}
+
+TabContentsWrapper* TabContentsWrapper::Clone() {
+  TabContents* new_contents = tab_contents()->Clone();
+  TabContentsWrapper* new_wrapper = new TabContentsWrapper(new_contents);
+  // Instantiate the passowrd manager if it has been instantiated here.
+  if (password_manager_.get())
+    new_wrapper->GetPasswordManager();
+  return new_wrapper;
 }
 
 PasswordManager* TabContentsWrapper::GetPasswordManager() {
@@ -41,11 +55,10 @@ PasswordManager* TabContentsWrapper::GetPasswordManager() {
   return password_manager_.get();
 }
 
-TabContentsWrapper* TabContentsWrapper::Clone() {
-  TabContents* new_contents = tab_contents()->Clone();
-  TabContentsWrapper* new_wrapper = new TabContentsWrapper(new_contents);
-  // Instantiate the passowrd manager if it has been instantiated here.
-  if (password_manager_.get())
-    new_wrapper->GetPasswordManager();
-  return new_wrapper;
+////////////////////////////////////////////////////////////////////////////////
+// TabContentsWrapper, WebNavigationObserver implementation:
+
+void TabContentsWrapper::NavigateToPendingEntry() {
+  GetPasswordManager();
+  tab_contents()->RemoveNavigationObserver(this);
 }
