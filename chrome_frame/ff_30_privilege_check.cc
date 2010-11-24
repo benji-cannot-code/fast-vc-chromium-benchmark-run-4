2a19b4e07986d628dbe52db5d57e72d258cd37e1
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 21:13:16 +0000

    Export the new sessionid property of Chrome Frame form the NPAPI interface.
    BUG=0
    TEST=see included unit test
    
    Review URL: http://codereview.chromium.org/5331003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67311 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/ff_30_privilege_check.cc
+++ chrome_frame/ff_30_privilege_check.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_frame/scoped_ns_ptr_win.h"
 #include "chrome_frame/ns_associate_iid_win.h"
 #include "chrome_frame/np_utils.h"
+#include "chrome_frame/utils.h"
 #include "googleurl/src/gurl.h"
 
 ASSOCIATE_IID(NS_ISERVICEMANAGER_IID_STR, nsIServiceManager);
@@ -30,6 +31,21 @@ ASSOCIATE_IID(NS_ISERVICEMANAGER_IID_STR, nsIServiceManager);
 // Returns true iff we're being instantiated into a document
 // that has the system principal's privileges
 bool IsFireFoxPrivilegedInvocation(NPP instance) {
+  // For testing purposes, check the registry to see if the privilege mode
+  // is being forced to a certain value.  If this property does not exist, the
+  // mode should be verified normally.  If this property does exist, its value
+  // is interpreted as follows:
+  //
+  //  0: force privilege mode off
+  //  1: force privilege mode on
+  //  any other value: do normal verification
+  int privilege_mode = GetConfigInt(2, kEnableFirefoxPrivilegeMode);
+  if (privilege_mode == 0) {
+    return false;
+  } else if (privilege_mode == 1) {
+    return true;
+  }
+
   // Make sure that we are running in Firefox before checking for privilege.
   const char* user_agent = npapi::UserAgent(instance);
   if (strstr(user_agent, "Firefox") == NULL)
