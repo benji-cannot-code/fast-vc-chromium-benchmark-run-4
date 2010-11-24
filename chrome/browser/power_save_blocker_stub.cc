ae308336dd9283badeb517b82834586f3bed66a3
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 15:53:49 +0000

    Add sleep blocker for Mac.
    BUG=34081
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/5360001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/power_save_blocker_stub.cc
+++ chrome/browser/power_save_blocker_stub.cc
@@ -9,5 +9,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void PowerSaveBlocker::ApplyBlock(bool blocking) {
   // http://code.google.com/p/chromium/issues/detail?id=33605
-  // http://code.google.com/p/chromium/issues/detail?id=34081
 }
