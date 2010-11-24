762ed3a4410e9bbbb51e1068df2f90f2d116410b
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 22:49:19 +0000

    Ensure that the contents settings dialog is displayed for notifications displayed in chrome frame pages.
    BUG=Fixes bug http://b/issue?id=3200366
    TEST=None
    Review URL: http://codereview.chromium.org/5266002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/notification_options_menu_model.cc
+++ chrome/browser/notifications/notification_options_menu_model.cc
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "grit/generated_resources.h"
 
+#if defined(OS_WIN)
+#include "chrome/browser/ui/views/browser_dialogs.h"
+#include "chrome/installer/util/install_util.h"
+#endif  // OS_WIN
+
 // Menu commands
 const int kTogglePermissionCommand = 0;
 const int kToggleExtensionCommand = 1;
@@ -141,9 +146,19 @@ void NotificationOptionsMenuModel::ExecuteCommand(int command_id) {
     }
     case kOpenContentSettingsCommand: {
       Browser* browser = BrowserList::GetLastActive();
-      if (browser)
+      if (browser) {
         static_cast<TabContentsDelegate*>(browser)->ShowContentSettingsWindow(
             CONTENT_SETTINGS_TYPE_NOTIFICATIONS);
+      } else {
+#if defined(OS_WIN)
+        if (InstallUtil::IsChromeFrameProcess()) {
+          // We may not have a browser if this is a chrome frame process.
+          browser::ShowContentSettingsWindow(NULL,
+                                             CONTENT_SETTINGS_TYPE_DEFAULT,
+                                             balloon_->profile());
+        }
+#endif  // OS_WIN
+      }
       break;
     }
     default:
