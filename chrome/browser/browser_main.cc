497fe9d00212cec9ea3e8c8313a8851121c7fb7e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 14:41:41 +0000

    net: disable the Snap Start command line flag.
    We're not ready for external people to start playing with this yet.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -203,8 +203,9 @@ void BrowserMainParts::EarlyInitialization() {
     net::SSLConfigService::DisableFalseStart();
   if (parsed_command_line().HasSwitch(switches::kAllowSSLMITMProxies))
     net::SSLConfigService::AllowMITMProxies();
-  if (parsed_command_line().HasSwitch(switches::kEnableSnapStart))
-    net::SSLConfigService::EnableSnapStart();
+  // Disabled to stop people playing with it.
+  // if (parsed_command_line().HasSwitch(switches::kEnableSnapStart))
+  //   net::SSLConfigService::EnableSnapStart();
   if (parsed_command_line().HasSwitch(
           switches::kEnableDNSCertProvenanceChecking)) {
     net::SSLConfigService::EnableDNSCertProvenanceChecking();
