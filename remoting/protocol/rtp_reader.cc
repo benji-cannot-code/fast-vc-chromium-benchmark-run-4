b9b846793f12885a04a66649d6ebcc1380c6440e
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 04:20:48 +0000

    Refactoring in RTP reader code: sequence number wrapping logic moved to RtpReader
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/5110008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67212 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/rtp_reader.cc
+++ remoting/protocol/rtp_reader.cc
@@ -11,12 +11,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 namespace protocol {
 
-RtpPacket::RtpPacket() {}
-RtpPacket::~RtpPacket() {}
+namespace {
+// Recomended values from RTP spec.
+const int kMaxDropout = 3000;
+const int kMaxMisorder = 100;
+}  // namespace
+
+RtpPacket::RtpPacket() { }
+
+RtpPacket::~RtpPacket() { }
 
 // RtpReader class.
-RtpReader::RtpReader() {}
-RtpReader::~RtpReader() {}
+RtpReader::RtpReader()
+    : max_sequence_number_(0),
+      wrap_around_count_(0),
+      started_(false) {
+}
+
+RtpReader::~RtpReader() {
+}
 
 void RtpReader::Init(net::Socket* socket,
                      OnMessageCallback* on_message_callback) {
@@ -44,6 +57,32 @@ void RtpReader::OnDataReceived(net::IOBuffer* buffer, int data_size) {
   packet->mutable_payload()->Append(
       buffer, buffer->data() + header_size + descriptor_size,
       data_size - header_size - descriptor_size);
+
+  uint16 sequence_number = packet->header().sequence_number;
+
+  // Reset |max_sequence_number_| after we've received first packet.
+  if (!started_) {
+    started_ = true;
+    max_sequence_number_ = sequence_number;
+  }
+
+  int16 delta = sequence_number - max_sequence_number_;
+  if (delta <= -kMaxMisorder || delta > kMaxDropout) {
+    // TODO(sergeyu): Do we need to handle restarted trasmission?
+    LOG(WARNING) << "Received RTP packet with invalid sequence number.";
+    delete packet;
+    return;
+  }
+
+  packet->set_extended_sequence_number(
+      (wrap_around_count_ << 16) + max_sequence_number_ + delta);
+
+  if (delta > 0 && delta < kMaxDropout) {
+    if (sequence_number < max_sequence_number_) {
+      wrap_around_count_++;
+    }
+    max_sequence_number_ = sequence_number;
+  }
 
   on_message_callback_->Run(packet);
 }
