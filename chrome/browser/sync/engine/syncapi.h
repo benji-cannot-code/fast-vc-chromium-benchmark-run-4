cfeb167151a3c3c676e627cd2681f2a9eb2e418a
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-24 20:55:02 +0000

    Autofill work: 1. Added a command line swithc to facilitate incremental checking in of autofill code. 2. Autofill profile model associator. 3. Part of the upgrade code.(The part to take the latest changes from server on the legacy type)
    Review URL: http://codereview.chromium.org/5126001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncapi.h
+++ chrome/browser/sync/engine/syncapi.h
@@ -82,6 +82,7 @@ class WriteTransaction;
 namespace sync_pb {
 class AppSpecifics;
 class AutofillSpecifics;
+class AutofillProfileSpecifics;
 class BookmarkSpecifics;
 class EntitySpecifics;
 class ExtensionSpecifics;
@@ -150,7 +151,7 @@ class BaseNode {
   // metahandle).  These ids are strictly local handles.  They will persist
   // on this client, but the same object on a different client may have a
   // different ID value.
-  int64 GetId() const;
+  virtual int64 GetId() const;
 
   // Nodes are hierarchically arranged into a single-rooted tree.
   // InitByRootLookup on ReadNode allows access to the root. GetParentId is
@@ -196,6 +197,9 @@ class BaseNode {
   // data.  Can only be called if GetModelType() == AUTOFILL.
   const sync_pb::AutofillSpecifics& GetAutofillSpecifics() const;
 
+  virtual const sync_pb::AutofillProfileSpecifics&
+      GetAutofillProfileSpecifics() const;
+
   // Getter specific to the NIGORI datatype.  Returns protobuf
   // data.  Can only be called if GetModelType() == NIGORI.
   const sync_pb::NigoriSpecifics& GetNigoriSpecifics() const;
@@ -233,11 +237,11 @@ class BaseNode {
 
   // Return the ID of the node immediately after this in the sibling order.
   // For the last node in the ordering, return 0.
-  int64 GetSuccessorId() const;
+  virtual int64 GetSuccessorId() const;
 
   // Return the ID of the first child of this node.  If this node has no
   // children, return 0.
-  int64 GetFirstChildId() const;
+  virtual int64 GetFirstChildId() const;
 
   // These virtual accessors provide access to data members of derived classes.
   virtual const syncable::Entry* GetEntry() const = 0;
@@ -257,8 +261,7 @@ class BaseNode {
   bool DecryptIfNecessary(syncable::Entry* entry);
 
  private:
-  // Node is meant for stack use only.
-  void* operator new(size_t size);
+  void* operator new(size_t size);  // Node is meant for stack use only.
 
   // If this node represents a password, this field will hold the actual
   // decrypted password data.
@@ -458,6 +461,9 @@ class ReadNode : public BaseNode {
   virtual const syncable::Entry* GetEntry() const;
   virtual const BaseTransaction* GetTransaction() const;
 
+ protected:
+  ReadNode();
+
  private:
   void* operator new(size_t size);  // Node is meant for stack use only.
 
@@ -492,6 +498,8 @@ class BaseTransaction {
   // to fail.
   explicit BaseTransaction(UserShare* share);
   virtual ~BaseTransaction();
+
+  BaseTransaction() { lookup_= NULL; }
 
  private:
   // A syncable ScopedDirLookup, which is the parent of syncable transactions.
@@ -537,6 +545,12 @@ class WriteTransaction : public BaseTransaction {
   // Provide access to the syncable.h transaction from the API WriteNode.
   virtual syncable::BaseTransaction* GetWrappedTrans() const;
   syncable::WriteTransaction* GetWrappedWriteTrans() { return transaction_; }
+
+ protected:
+  WriteTransaction() {}
+
+  void SetTransaction(syncable::WriteTransaction* trans) {
+      transaction_ = trans;}
 
  private:
   void* operator new(size_t size);  // Transaction is meant for stack use only.
