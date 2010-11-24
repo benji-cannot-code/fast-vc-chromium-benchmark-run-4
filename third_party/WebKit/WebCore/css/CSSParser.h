525b627a2c75fd981d390dd56badf6feae30d7fb
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-24 18:45:53 +0000

    2010-11-24  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Crash when inspecting http://alphaarnhem.nl/beta/
            https://bugs.webkit.org/show_bug.cgi?id=50025
    
            @font-face rules were improperly handled by source data extractor.
    
            WebCore:
            * css/CSSParser.cpp:
            (WebCore::CSSParser::CSSParser):
            (WebCore::CSSParser::parseDeclaration):
            (WebCore::CSSParser::createStyleRule):
            (WebCore::CSSParser::markRuleBodyStart):
            (WebCore::CSSParser::markPropertyStart):
            (WebCore::CSSParser::markPropertyEnd):
            * css/CSSParser.h:
    
            LayoutTests:
            * inspector/resources/styles-new-API-1.css:
            (@font-face):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSParser.h
+++ third_party/WebKit/WebCore/css/CSSParser.h
@@ -231,6 +231,7 @@ namespace WebCore {
         AtomicString m_defaultNamespace;
 
         // tokenizer methods and data
+        bool m_inStyleRuleOrDeclaration;
         SourceRange m_selectorListRange;
         SourceRange m_ruleBodyRange;
         SourceRange m_propertyRange;
