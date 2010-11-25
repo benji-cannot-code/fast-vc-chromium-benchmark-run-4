3ff0209d8b8c7e7dd59b90f4615e13fa12af8c3e
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-25 20:33:26 +0000

    2010-11-25  Antonio Gomes  <agomes@rim.com>
            Unreviewed build fix for GTK+ when --release and -no-video are passed to build-webkit
    
            Build problem with --gtk --no-video
            https://bugs.webkit.org/show_bug.cgi?id=50003
    
            Patch suggested by Philippe Normand  <pnormand@igalia.com>.
    
            * WebCoreSupport/ChromeClientGtk.cpp:
            * webkit/webkitprivate.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitprivate.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitprivate.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ApplicationCacheStorage.h"
 #include "Chrome.h"
 #include "ChromeClientGtk.h"
+#include "Element.h"
 #include "Frame.h"
 #include "FrameLoader.h"
 #include "FrameLoaderClientGtk.h"
