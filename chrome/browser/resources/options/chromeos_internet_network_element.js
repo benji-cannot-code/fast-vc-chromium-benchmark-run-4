77174946089704a54f0779b0f40c639696e75722
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-25 07:31:47 +0000

    Added missing connect button to cellular network element.
    BUG=chromium-os:9641
    TEST=make sure that we show [Connect] button on disconnected cellular network element in chrome:settings/internet page
    
    Review URL: http://codereview.chromium.org/5379002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos_internet_network_element.js
+++ chrome/browser/resources/options/chromeos_internet_network_element.js
@@ -223,17 +223,8 @@ cr.define('options.internet', function() {
                              'activate']);
               }));
         }
-        if (this.data.connected ||
-                this.data.networkType == NetworkItem.TYPE_CELLULAR) {
-          buttonsDiv.appendChild(
-              this.createButton_('options_button', 'options',
-                                 function(e) {
-                chrome.send('buttonClickCallback',
-                            [String(self.data.networkType),
-                             self.data.servicePath,
-                             'options']);
-              }));
-        } else if (!this.data.connecting && this.data.connectable) {
+        if (!this.data.connected && !this.data.connecting &&
+            this.data.connectable) {
           // connect button (if not ethernet and not showing activate button
           // and connectable)
           if (this.data.networkType != NetworkItem.TYPE_ETHERNET &&
@@ -247,6 +238,17 @@ cr.define('options.internet', function() {
                                'connect']);
                 }));
           }
+        }
+        if (this.data.connected ||
+            this.data.networkType == NetworkItem.TYPE_CELLULAR) {
+          buttonsDiv.appendChild(
+              this.createButton_('options_button', 'options',
+                                 function(e) {
+                chrome.send('buttonClickCallback',
+                            [String(self.data.networkType),
+                             self.data.servicePath,
+                             'options']);
+              }));
         }
       } else {
         // Put "Forget this network" button.
