c47e056577592c79374dd1d5f9c631ac5b3783d9
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-25 20:54:07 +0000

    Remove unused BaseTabStrip methods
    Before creating a new kind of TabStrip, I'm first cleaning up the BaseTabStrip class to remove two unused methods: SetDraggedTabBounds, and AsTabStrip.
    AsTabStrip was particularly unwanted because the base class was referencing the derived class.
    
    Furthermore, as an additional cleanup effort, I observe that there is no original caller to TabStripModelDelegate::ContinueDraggingDetachedTab(...)
    Therefore:
    TabStripModelDelegate::ContinueDraggingDetachedTab(...) REMOVED
    DefaultTabHandler::ContinueDraggingDetachedTab(...)  REMOVED
    
    ...which leaves no callers of Browser::ContinueDraggingDetachedTab(...)
    Therefore:
    Browser::ContinueDraggingDetachedTab(...) REMOVED
    
    ...which leaves no callers of BrowserWindow::ContinueDraggingDetachedTab(bounds)
    Therefore:
    BrowserWindow::ContinueDraggingDetachedTab(bounds) REMOVED
    BrowserView::ContinueDraggingDetachedTab(bounds) REMOVED
    
    ...which leaves no callers of BrowserFrame::ContinueDraggingDetachedTab()
    Therefore:
    BrowserFrame::ContinueDraggingDetachedTab() REMOVED
    BrowserFrameGtk::ContinueDraggingDetachedTab() REMOVED
    BrowserFrameWin::ContinueDraggingDetachedTab() REMOVED
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/5253001
    Patch from Chad Faragher <wyck@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67418 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame_win.h
+++ chrome/browser/ui/views/frame/browser_frame_win.h
@@ -43,7 +43,6 @@ class BrowserFrameWin : public BrowserFrame, public views::WindowWin {
   virtual gfx::Rect GetBoundsForTabStrip(BaseTabStrip* tabstrip) const;
   virtual int GetHorizontalTabStripVerticalOffset(bool restored) const;
   virtual void UpdateThrobber(bool running);
-  virtual void ContinueDraggingDetachedTab();
   virtual ThemeProvider* GetThemeProviderForFrame() const;
   virtual bool AlwaysUseNativeFrame() const;
   virtual views::View* GetFrameView() const;
