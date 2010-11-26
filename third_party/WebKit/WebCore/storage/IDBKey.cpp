19ccd8bc7db81558a9f48ab371e64be700be1927
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-26 16:37:09 +0000

    2010-11-26  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Steve Block.
    
            Make IDBKeyRange match the spec
            https://bugs.webkit.org/show_bug.cgi?id=50105
    
            Remove flags and instead add two booleans for being open.
            Change left to lower and right to upper everywhere.
    
            * storage/indexeddb/constants-expected.txt:
            * storage/indexeddb/constants.html:
            * storage/indexeddb/index-cursor.html:
            * storage/indexeddb/keyrange-expected.txt:
            * storage/indexeddb/keyrange.html:
            * storage/indexeddb/objectstore-cursor.html:
            * storage/indexeddb/open-cursor.html:
            * storage/indexeddb/tutorial.html:
    2010-11-26  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Steve Block.
    
            Make IDBKeyRange match the spec
            https://bugs.webkit.org/show_bug.cgi?id=50105
    
            Remove flags and instead add two booleans for being open.
            Change left to lower and right to upper everywhere.
    
            * storage/IDBIndexBackendImpl.cpp:
            (WebCore::IDBIndexBackendImpl::openCursorInternal):
            * storage/IDBKey.cpp:
            (WebCore::IDBKey::lowerCursorWhereFragment):
            (WebCore::IDBKey::upperCursorWhereFragment):
            * storage/IDBKey.h:
            * storage/IDBKeyRange.cpp:
            (WebCore::IDBKeyRange::IDBKeyRange):
            (WebCore::IDBKeyRange::only):
            (WebCore::IDBKeyRange::lowerBound):
            (WebCore::IDBKeyRange::upperBound):
            (WebCore::IDBKeyRange::bound):
            (WebCore::IDBKeyRange::lowerWhereClauseComparisonOperator):
            (WebCore::IDBKeyRange::upperWhereClauseComparisonOperator):
            * storage/IDBKeyRange.h:
            (WebCore::IDBKeyRange::create):
            (WebCore::IDBKeyRange::lower):
            (WebCore::IDBKeyRange::upper):
            (WebCore::IDBKeyRange::lowerOpen):
            (WebCore::IDBKeyRange::upperOpen):
            * storage/IDBKeyRange.idl:
            * storage/IDBObjectStoreBackendImpl.cpp:
            (WebCore::IDBObjectStoreBackendImpl::openCursorInternal):
    2010-11-26  Jeremy Orlow  <jorlow@chromium.org>
    
            Reviewed by Steve Block.
    
            Make IDBKeyRange match the spec
            https://bugs.webkit.org/show_bug.cgi?id=50105
    
            Remove flags and instead add two booleans for being open.
            Change left to lower and right to upper everywhere.
    
            * public/WebIDBKeyRange.h:
            (WebKit::WebIDBKeyRange::WebIDBKeyRange):
            * src/WebIDBKeyRange.cpp:
            (WebKit::WebIDBKeyRange::assign):
            (WebKit::WebIDBKeyRange::left):
            (WebKit::WebIDBKeyRange::right):
            (WebKit::WebIDBKeyRange::lower):
            (WebKit::WebIDBKeyRange::upper):
            (WebKit::WebIDBKeyRange::lowerOpen):
            (WebKit::WebIDBKeyRange::upperOpen):
            (WebKit::WebIDBKeyRange::flags):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72767 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/storage/IDBKey.cpp
+++ third_party/WebKit/WebCore/storage/IDBKey.cpp
@@ -106,7 +106,7 @@ String IDBKey::whereSyntax(String qualifiedTableName) const
     return "";
 }
 
-String IDBKey::leftCursorWhereFragment(String comparisonOperator, String qualifiedTableName)
+String IDBKey::lowerCursorWhereFragment(String comparisonOperator, String qualifiedTableName)
 {
     switch (m_type) {
     case StringType:
@@ -123,7 +123,7 @@ String IDBKey::leftCursorWhereFragment(String comparisonOperator, String qualifi
     return "";
 }
 
-String IDBKey::rightCursorWhereFragment(String comparisonOperator, String qualifiedTableName)
+String IDBKey::upperCursorWhereFragment(String comparisonOperator, String qualifiedTableName)
 {
     switch (m_type) {
     case StringType:
