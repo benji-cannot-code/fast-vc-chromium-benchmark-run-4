02da9d5fd348c8574215dd1fd59bd7b7f9aff5f9
reni@webkit.org <reni@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-26 15:36:21 +0000

    [Qt] Remove empty ScrollView::platformInit/platformDestroy https://bugs.webkit.org/show_bug.cgi?id=50079
    Reviewed by Antonio Gomes.
    
    No new test is needed.
    
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::wheelEvent):
    * platform/qt/ScrollViewQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ScrollViewQt.cpp
@@ -35,14 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void ScrollView::platformInit()
-{
-}
-
-void ScrollView::platformDestroy()
-{
-}
-
 void ScrollView::platformAddChild(Widget*)
 {
 }
