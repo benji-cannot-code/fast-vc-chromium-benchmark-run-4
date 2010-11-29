fc69a955207d38f27635d6e270be00458efb3bb1
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-29 10:08:51 +0000

    IndexedDB: Remove IDBDatabase.description
    This depends on WebKit being rolled to r72753.
    
    BUG=59514
    TEST=browser_tests, layout tests
    
    Review URL: http://codereview.chromium.org/5349006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67525 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/indexeddb/object_store_test.js
+++ chrome/test/data/indexeddb/object_store_test.js
@@ -81,7 +81,7 @@ function setVersion()
 function test()
 {
   debug('Connecting to indexedDB');
-  var result = webkitIndexedDB.open('name', 'description');
+  var result = webkitIndexedDB.open('name');
   result.onsuccess = setVersion;
   result.onerror = unexpectedErrorCallback;
 }
