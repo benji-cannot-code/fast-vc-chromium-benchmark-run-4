b4afa1e64303a2082015dad673fc18ce8ecb8f81
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-29 02:57:08 +0000

    Disallow non-owners from modifying the timezone setting
    BUG=chromium-os:8176
    TEST=Log in as a non-owner.  Attempt to change the time zone.
    
    Review URL: http://codereview.chromium.org/5391001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/system_settings_provider.cc
+++ chrome/browser/chromeos/dom_ui/system_settings_provider.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros_settings.h"
 #include "chrome/browser/chromeos/cros_settings_names.h"
+#include "chrome/browser/chromeos/login/ownership_service.h"
 #include "grit/generated_resources.h"
 #include "unicode/calendar.h"
 #include "unicode/timezone.h"
@@ -195,6 +196,10 @@ SystemSettingsProvider::~SystemSettingsProvider() {
 }
 
 void SystemSettingsProvider::DoSet(const std::string& path, Value* in_value) {
+  // Only the owner can change the time zone.
+  if (!OwnershipService::GetSharedInstance()->CurrentUserIsOwner())
+    return;
+
   if (path == kSystemTimezone) {
     string16 value;
     if (!in_value || !in_value->IsType(Value::TYPE_STRING) ||
