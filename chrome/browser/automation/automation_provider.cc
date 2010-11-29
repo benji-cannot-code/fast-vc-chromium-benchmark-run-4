46c95fabc70c00bc79461f7bd29ac193050a5d97
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-29 17:22:49 +0000

    Track permissions granted to extensions in prefs
    Adds a section to the extension preferences to store the recognized permissions that the user has granted the extension.
    Ignores unknown permissions when installing extensions.
    Disables extension and prompts user to re-enable when unknown permissions become recognized (e.g., through browser upgrade).
    Allows extensions to remove a permission in one version, then add them back in the next without prompting the user.
    
    BUG=42742
    TEST=ExtensionsServiceTest, ExtensionPrefsGrantedPermissions, ExtensionTest
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=67500
    
    Review URL: http://codereview.chromium.org/4687005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -800,7 +800,6 @@ void AutomationProvider::InstallExtension(const FilePath& crx_path,
 
     scoped_refptr<CrxInstaller> installer(
         new CrxInstaller(service, NULL));  // silent install, no UI
-    installer->set_allow_privilege_increase(true);
     installer->InstallCrx(crx_path);
   } else {
     AutomationMsg_InstallExtension::WriteReplyParams(
@@ -869,7 +868,6 @@ void AutomationProvider::InstallExtensionAndGetHandle(
     ExtensionInstallUI* client =
         (with_ui ? new ExtensionInstallUI(profile_) : NULL);
     scoped_refptr<CrxInstaller> installer(new CrxInstaller(service, client));
-    installer->set_allow_privilege_increase(true);
     installer->InstallCrx(crx_path);
   } else {
     AutomationMsg_InstallExtensionAndGetHandle::WriteReplyParams(
