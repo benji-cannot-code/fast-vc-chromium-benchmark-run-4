9a2408ad83084bf4011c0f63abbeebb2fd73fb14
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 01:08:06 +0000

    Alphabetically sort font family names in the drop-down menu for the "Change font settings" screen.
    BUG=chromium-os:9048
    TEST=manually done.
    
    Review URL: http://codereview.chromium.org/4664010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67637 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options/font_settings_utils_gtk.cc
+++ chrome/browser/dom_ui/options/font_settings_utils_gtk.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/dom_ui/options/font_settings_utils.h"
 
+#include <set>
+#include <string>
+
 #include <pango/pango.h>
 #include <pango/pangocairo.h>
 
@@ -18,15 +21,20 @@ ListValue* FontSettingsUtilities::GetFontsList() {
   int num_families = 0;
   ::pango_font_map_list_families(font_map, &families, &num_families);
 
+  std::set<std::string> sorted_families;
   for (int i = 0; i < num_families; i++) {
+    sorted_families.insert(::pango_font_family_get_name(families[i]));
+  }
+  g_free(families);
+
+  for (std::set<std::string>::const_iterator iter = sorted_families.begin();
+       iter != sorted_families.end(); ++iter) {
     ListValue* font_item = new ListValue();
-    const char* family_name = ::pango_font_family_get_name(families[i]);
-    font_item->Append(Value::CreateStringValue(family_name));
-    font_item->Append(Value::CreateStringValue(family_name));
+    font_item->Append(Value::CreateStringValue(*iter));
+    font_item->Append(Value::CreateStringValue(*iter));  // localized name.
+    // TODO(yusukes): Support localized family names.
     font_list->Append(font_item);
   }
-
-  g_free(families);
 
   return font_list;
 }
@@ -34,4 +42,3 @@ ListValue* FontSettingsUtilities::GetFontsList() {
 void FontSettingsUtilities::ValidateSavedFonts(PrefService* prefs) {
   // nothing to do for GTK.
 }
-
