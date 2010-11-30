8646c1d9c7e397ff549ee33a84377a67dd0d7716
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 19:16:56 +0000

    2010-11-30  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: implement "Revert to this revision" in resources panel.
            https://bugs.webkit.org/show_bug.cgi?id=50107
    
            * inspector/Inspector.idl:
            * inspector/InspectorCSSAgent.cpp:
            (WebCore::InspectorCSSAgent::setStyleSheetText2):
            * inspector/InspectorCSSAgent.h:
            * inspector/InspectorStyleSheet.cpp:
            (WebCore::InspectorStyleSheet::reparseStyleSheet):
            * inspector/front-end/CSSStyleModel.js:
            (WebInspector.CSSStyleModel.prototype._styleSheetChanged.callback):
            (WebInspector.CSSStyleModel.prototype._styleSheetChanged):
            (WebInspector.CSSStyleModel.prototype._onRevert):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.prototype.get contentTimestamp):
            (WebInspector.Resource.prototype.setInitialContent):
            (WebInspector.Resource.prototype.isLocallyModified):
            (WebInspector.Resource.prototype.setContent.else.finished):
            (WebInspector.Resource.prototype.setContent):
            (WebInspector.Resource.prototype.revertToThis.callback):
            (WebInspector.Resource.prototype.revertToThis):
            (WebInspector.Resource.prototype.get baseRevision):
            (WebInspector.Resource.prototype._innerRequestContent):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype._showResourceView):
            (WebInspector.FrameResourceTreeElement.prototype._contentChanged):
            (WebInspector.ResourceRevisionTreeElement):
            (WebInspector.ResourceRevisionTreeElement.prototype.onattach):
            (WebInspector.ResourceRevisionTreeElement.prototype._ondragstart):
            (WebInspector.ResourceRevisionTreeElement.prototype._handleContextMenuEvent):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype.editScriptSource.mycallback):
            (WebInspector.ScriptsPanel.prototype.editScriptSource):
            * inspector/front-end/SourceView.js:
            (WebInspector.SourceView.prototype._editLine):
            (WebInspector.SourceView.prototype._editLineComplete):
            (WebInspector.SourceView.prototype._revertEditLine):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ScriptsPanel.js
@@ -307,7 +307,7 @@ WebInspector.ScriptsPanel.prototype = {
         return Preferences.canEditScriptSource;
     },
 
-    editScriptSource: function(sourceID, newContent, line, linesCountToShift, commitEditingCallback, cancelEditingCallback)
+    editScriptSource: function(editData, commitEditingCallback, cancelEditingCallback)
     {
         if (!this.canEditScripts())
             return;
@@ -324,18 +324,19 @@ WebInspector.ScriptsPanel.prototype = {
                 if (callFrames && callFrames.length)
                     this.debuggerPaused(callFrames);
             } else {
-                cancelEditingCallback();
+                if (cancelEditingCallback)
+                    cancelEditingCallback();
                 WebInspector.log(newBodyOrErrorMessage, WebInspector.ConsoleMessage.MessageLevel.Warning);
             }
             for (var i = 0; i < breakpoints.length; ++i) {
                 var breakpoint = breakpoints[i];
                 var newLine = breakpoint.line;
-                if (success && breakpoint.line >= line)
-                    newLine += linesCountToShift;
-                WebInspector.breakpointManager.setBreakpoint(sourceID, breakpoint.url, newLine, breakpoint.enabled, breakpoint.condition);
+                if (success && breakpoint.line >= editData.line)
+                    newLine += editData.linesCountToShift;
+                WebInspector.breakpointManager.setBreakpoint(editData.sourceID, breakpoint.url, newLine, breakpoint.enabled, breakpoint.condition);
             }
         };
-        InspectorBackend.editScriptSource(sourceID, newContent, mycallback.bind(this));
+        InspectorBackend.editScriptSource(editData.sourceID, editData.content, mycallback.bind(this));
     },
 
     selectedCallFrameId: function()
