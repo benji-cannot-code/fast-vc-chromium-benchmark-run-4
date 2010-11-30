f415484dcfa47dd15f6c73f91aea426c2dbf10a1
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 16:47:06 +0000

    2010-11-30  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r72914.
            http://trac.webkit.org/changeset/72914
            https://bugs.webkit.org/show_bug.cgi?id=50241
    
            "pfeldman has a revision management patch both for CSS and JS"
            (Requested by apavlov on #webkit).
    
            * English.lproj/localizedStrings.js:
            * inspector/InspectorStyleSheet.cpp:
            (WebCore::InspectorStyleSheet::reparseStyleSheet):
            * inspector/front-end/CSSStyleModel.js:
            (WebInspector.CSSStyleModel.prototype._styleSheetChanged.callback):
            (WebInspector.CSSStyleModel.prototype._styleSheetChanged):
            (WebInspector.CSSStyleSheet.prototype.getText):
            (WebInspector.CSSStyleSheet.prototype.setText):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourceRevisionTreeElement.prototype.onattach):
            (WebInspector.ResourceRevisionTreeElement.prototype._ondragstart):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/CSSStyleModel.js
+++ third_party/WebKit/WebCore/inspector/front-end/CSSStyleModel.js
@@ -156,11 +156,6 @@ WebInspector.CSSStyleModel.prototype = {
         InspectorBackend.addRule2(nodeId, selector, callback.bind(this, successCallback, failureCallback, selector));
     },
 
-    setStyleSheetText: function(styleSheetId, newText)
-    {
-        InspectorBackend.setStyleSheetText2(styleSheetId, newText, this._styleSheetChanged.bind(this, styleSheetId, true));
-    },
-
     _styleSheetChanged: function(styleSheetId, majorChange)
     {
         if (!majorChange || !styleSheetId)
@@ -169,10 +164,8 @@ WebInspector.CSSStyleModel.prototype = {
         function callback(href, content)
         {
             var resource = WebInspector.resourceManager.resourceForURL(href);
-            if (resource && resource.type === WebInspector.Resource.Type.Stylesheet) {
-                resource.styleSheetId = styleSheetId;
+            if (resource && resource.type === WebInspector.Resource.Type.Stylesheet)
                 resource.content = content;
-            }
         }
         InspectorBackend.getStyleSheetText2(styleSheetId, callback);
     }
@@ -539,5 +532,20 @@ WebInspector.CSSStyleSheet.prototype = {
     getText: function()
     {
         return this._text;
+    },
+
+    setText: function(newText, userCallback)
+    {
+        function callback(styleSheetPayload)
+        {
+            if (!styleSheetPayload)
+                userCallback(null);
+            else {
+                userCallback(new WebInspector.CSSStyleSheet(styleSheetPayload));
+                WebInspector.cssModel._styleSheetChanged(this.id, true);
+            }
+        }
+
+        InspectorBackend.setStyleSheetText2(this.id, newText, callback.bind(this));
     }
 }
