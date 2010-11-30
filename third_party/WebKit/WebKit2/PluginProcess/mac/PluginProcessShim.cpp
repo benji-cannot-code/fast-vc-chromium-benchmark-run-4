2381bab33eef781d9b6191ab726c002e343d5116
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 23:48:07 +0000

    Fix build.
    * PluginProcess/mac/PluginProcessShim.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/PluginProcess/mac/PluginProcessShim.cpp
+++ third_party/WebKit/WebKit2/PluginProcess/mac/PluginProcessShim.cpp
@@ -27,8 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginProcessShim.h"
 
 #include <Carbon/Carbon.h>
-#include <mach-o/dyld-interposing.h>
 #include <stdio.h>
+
+#define DYLD_INTERPOSE(_replacement,_replacee) \
+    __attribute__((used)) static struct{ const void* replacement; const void* replacee; } _interpose_##_replacee \
+    __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
 
 namespace WebKit {
 
