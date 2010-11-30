e9d0b342beddb110293aa95d9c2d5f6a28eeba89
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 19:33:50 +0000

    Update HelpApp URL
    BUG=http://code.google.com/p/chromium-os/issues/detail?id=9625
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5359008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67743 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -148,7 +148,7 @@ namespace {
 // The URL to be loaded to display Help.
 #if defined(OS_CHROMEOS)
 const char kHelpContentUrl[] =
-    "chrome-extension://nifaohjgppdbmalmmgkmfdlodaggnbpe/main.html";
+    "chrome-extension://oonnajkebngbfpmcpekfdeofeannbapp/main.html";
 const char kHelpContentOnlineUrl[] =
     "http://www.google.com/support/chromeos/";
 #else
