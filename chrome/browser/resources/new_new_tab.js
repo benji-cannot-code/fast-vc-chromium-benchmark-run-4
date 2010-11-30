5c2851bd6a7e4be617b43e8496d4cb22e6d5c446
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 19:22:47 +0000

    NTP update for ChromeOS.
    - Default to show expanded apps (i.e. most visited and recently closed
      secion is closed by default);
    - Move closed section bar up to underneath the expanded sections;
    - Add a seprate line between expanded and closed section bar;
    - Change the closed section menu footer text to "Expand"
    
    BUG=chromium-os:9737
    TEST=Verify fix for chromium-os:9737 and no change to non chrome os version.
    
    Review URL: http://codereview.chromium.org/5340007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/new_new_tab.js
+++ chrome/browser/resources/new_new_tab.js
@@ -74,8 +74,12 @@ function addClosedMenuFooter(menu, sectionId, mask, opt_section) {
   var span = document.createElement('span');
   var a = span.appendChild(document.createElement('a'));
   a.href = '';
-  a.textContent =
-      localStrings.getString(getSectionMenuButtonTextId(sectionId));
+  if (cr.isChromeOS) {
+    a.textContent = localStrings.getString('expandMenu');
+  } else {
+    a.textContent =
+        localStrings.getString(getSectionMenuButtonTextId(sectionId));
+  }
   a.className = 'item';
   a.addEventListener(
       'click',
@@ -434,6 +438,8 @@ function layoutSections() {
     if (section == expandedSection)
       y += expandedSectionHeight;
   }
+  if (cr.isChromeOS)
+    $('closed-sections-bar').style.top = y + 'px';
 
   updateAttributionDisplay(y);
 }
