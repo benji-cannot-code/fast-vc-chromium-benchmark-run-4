c07afb9e0662daec6561c1e640abfb4a336dc730
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 19:28:12 +0000

    Disable phishing tests that appear to fail because of log parsing bug.
    BUG=64792
    TEST=none
    TBR=scherkus@chromium.org
    
    Review URL: http://codereview.chromium.org/5308004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67738 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
@@ -178,7 +178,14 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, MalwareProceed) {
   EXPECT_EQ(url, browser()->GetSelectedTabContents()->GetURL());
 }
 
-IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingDontProceed) {
+// Disabled because of bug in parsing output: 64792.
+#if defined(OS_WIN)
+#define MAYBE_PhishingDontProceed DISABLED_PhishingDontProceed
+#else
+#define MAYBE_PhishingDontProceed PhishingDontProceed
+#endif
+IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest,
+                       MAYBE_PhishingDontProceed) {
   GURL url = test_server()->GetURL(kEmptyPage);
   AddURLResult(url, SafeBrowsingService::URL_PHISHING);
 
@@ -202,7 +209,14 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingProceed) {
   EXPECT_EQ(url, browser()->GetSelectedTabContents()->GetURL());
 }
 
-IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingReportError) {
+// Disabled because of bug in parsing output: 64792.
+#if defined(OS_WIN)
+#define MAYBE_PhishingReportError DISABLED_PhishingReportError
+#else
+#define MAYBE_PhishingReportError PhishingReportError
+#endif
+IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest,
+                       MAYBE_PhishingReportError) {
   GURL url = test_server()->GetURL(kEmptyPage);
   AddURLResult(url, SafeBrowsingService::URL_PHISHING);
 
@@ -217,7 +231,13 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingReportError) {
             browser()->GetSelectedTabContents()->GetURL().path());
 }
 
-IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingLearnMore) {
+// Disabled because of bug in parsing output: 64792.
+#if defined(OS_WIN)
+#define MAYBE_PhishingLearnMore DISABLED_PhishingLearnMore
+#else
+#define MAYBE_PhishingLearnMore PhishingLearnMore
+#endif
+IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, MAYBE_PhishingLearnMore) {
   GURL url = test_server()->GetURL(kEmptyPage);
   AddURLResult(url, SafeBrowsingService::URL_PHISHING);
 
