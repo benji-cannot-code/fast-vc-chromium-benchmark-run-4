be3030c1004e573c04c4ccd00406b4230db8a2f0
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 23:21:15 +0000

    2010-11-30  Chris Guillory  <chris.guillory@google.com>
            Reviewed by Chris Fleizach.
    
            Computing style on a stale node while sending pending accessibility notification.
            https://bugs.webkit.org/show_bug.cgi?id=50162
    
            Retain node pointer members of AccessibilityImageMapLink.
    
            Test: accessibility/image-map-title-causes-crash.html
    
            * accessibility/AccessibilityImageMapLink.cpp:
            (WebCore::AccessibilityImageMapLink::parentObject):
            (WebCore::AccessibilityImageMapLink::anchorElement):
            (WebCore::AccessibilityImageMapLink::url):
            (WebCore::AccessibilityImageMapLink::elementRect):
            * accessibility/AccessibilityImageMapLink.h:
            (WebCore::AccessibilityImageMapLink::areaElement):
            (WebCore::AccessibilityImageMapLink::mapElement):
            (WebCore::AccessibilityImageMapLink::node):
            * dom/Document.cpp:
            (WebCore::Document::clearAXObjectCache):
    2010-11-30  Chris Guillory  <chris.guillory@google.com>
    
            Reviewed by Chris Fleizach.
    
            Computing style on a stale node while sending pending accessibility notification.
            https://bugs.webkit.org/show_bug.cgi?id=50162
    
            * accessibility/image-map-title-causes-crash-expected.txt: Added.
            * accessibility/image-map-title-causes-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72969 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/AccessibilityImageMapLink.cpp
+++ third_party/WebKit/WebCore/accessibility/AccessibilityImageMapLink.cpp
@@ -60,7 +60,7 @@ AccessibilityObject* AccessibilityImageMapLink::parentObject() const
     if (m_parent)
         return m_parent;
     
-    if (!m_mapElement || !m_mapElement->renderer())
+    if (!m_mapElement.get() || !m_mapElement->renderer())
         return 0;
     
     return m_mapElement->document()->axObjectCache()->getOrCreate(m_mapElement->renderer());
@@ -85,12 +85,12 @@ Element* AccessibilityImageMapLink::actionElement() const
     
 Element* AccessibilityImageMapLink::anchorElement() const
 {
-    return m_areaElement;
+    return m_areaElement.get();
 }
 
 KURL AccessibilityImageMapLink::url() const
 {
-    if (!m_areaElement)
+    if (!m_areaElement.get())
         return KURL();
     
     return m_areaElement->href();
@@ -122,7 +122,7 @@ String AccessibilityImageMapLink::title() const
     
 IntRect AccessibilityImageMapLink::elementRect() const
 {
-    if (!m_mapElement || !m_areaElement)
+    if (!m_mapElement.get() || !m_areaElement.get())
         return IntRect();
 
     RenderObject* renderer;
