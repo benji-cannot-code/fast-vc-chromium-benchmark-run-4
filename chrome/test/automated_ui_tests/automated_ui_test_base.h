f811aa69294960a06aa8ec7b27d779ef340590bb
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 03:01:23 +0000

    Update a couple of out of date comments.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5319001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automated_ui_tests/automated_ui_test_base.h
+++ chrome/test/automated_ui_tests/automated_ui_test_base.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_TEST_AUTOMATED_UI_TESTS_AUTOMATED_UI_TEST_BASE_H_
 #pragma once
 
+#include <string>
+
 #include "chrome/test/ui/ui_test.h"
 
 class AutomatedUITestBase : public UITest {
@@ -118,7 +120,7 @@ class AutomatedUITestBase : public UITest {
   bool ShowHistory();
 
   // Runs the specified browser command in the current active browser.
-  // See browser_commands.cc for the list of commands.
+  // See Browser::ExecuteCommandWithDisposition() for the list of commands.
   // Returns true if the call is successfully dispatched.
   // Possible failures include the active window is not a browser window or
   // the message to apply the accelerator fails.
@@ -126,7 +128,7 @@ class AutomatedUITestBase : public UITest {
 
   // Runs the specified browser command in the current active browser, wait
   // and return until the command has finished executing.
-  // See browser_commands.cc for the list of commands.
+  // See Browser::ExecuteCommandWithDisposition() for the list of commands.
   // Returns true if the call is successfully dispatched and executed.
   // Possible failures include the active window is not a browser window, or
   // the message to apply the accelerator fails, or the command execution
