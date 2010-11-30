9d9fd979a0119fda2d611cbf0927fc6c603c0655
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 19:57:02 +0000

    2010-11-30  Andras Becsi  <abecsi@webkit.org>
            Reviewed by Csaba Osztrogonác.
    
            [Qt][V8] Make QtWebKit with V8 build on Linux.
            https://bugs.webkit.org/show_bug.cgi?id=50244
    
            No new tests needed.
    
            * WebCore.pri: add missing idl file
            * WebCore.pro: enable Linux build
            * bindings/scripts/CodeGeneratorV8.pm: add missing headers
            * bindings/v8/ScriptControllerQt.cpp: fix instantiation
            (WebCore::ScriptController::qtScriptEngine):
            * bindings/v8/V8GCController.cpp: fix guard
            (WebCore::V8GCController::checkMemoryUsage):
            * inspector/CodeGeneratorInspector.pm: add missing header
            * loader/ResourceLoadScheduler.cpp: ditto
            * loader/cache/MemoryCache.cpp: ditto
            * loader/loader.cpp: ditto
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/cache/MemoryCache.cpp
+++ third_party/WebKit/WebCore/loader/cache/MemoryCache.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SecurityOrigin.h"
 #include <stdio.h>
 #include <wtf/CurrentTime.h>
+#include <wtf/text/CString.h>
 
 using namespace std;
 
