5467520a27442bf5d0db494ca2099d1a16153cc4
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 00:36:59 +0000

    Fix for "New blocked popup UI" on Windows Changes: 1. new sets of strings indicating animation in chrome/browser/content_setting_image_model.cc - non-NULL id indicated that the situation is animated. 2. TabSpecificContentSettings has indicator that the new blockage occured, and was/was not messaged to the user. 3. ContentSettingImageView on Windows/ChromeOs is descended from LinearAnimation to allow animation of the popup indicator to the user. BUG=39260 TEST=Mocks Review URL: http://codereview.chromium.org/3120034
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67630 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/location_bar_view_gtk.cc
+++ chrome/browser/gtk/location_bar_view_gtk.cc
@@ -653,7 +653,7 @@ void LocationBarViewGtk::FocusSearch() {
 }
 
 void LocationBarViewGtk::UpdateContentSettingsIcons() {
-  const TabContents* tab_contents = GetTabContents();
+  TabContents* tab_contents = GetTabContents();
   bool any_visible = false;
   for (ScopedVector<ContentSettingImageViewGtk>::iterator i(
            content_setting_views_.begin());
@@ -1268,7 +1268,7 @@ LocationBarViewGtk::ContentSettingImageViewGtk::~ContentSettingImageViewGtk() {
 }
 
 void LocationBarViewGtk::ContentSettingImageViewGtk::UpdateFromTabContents(
-    const TabContents* tab_contents) {
+    TabContents* tab_contents) {
   content_setting_image_model_->UpdateFromTabContents(tab_contents);
   if (content_setting_image_model_->is_visible()) {
     gtk_image_set_from_pixbuf(GTK_IMAGE(image_.get()),
--- chrome/browser/gtk/location_bar_view_gtk.h
+++ chrome/browser/gtk/location_bar_view_gtk.h
@@ -157,7 +157,7 @@ class LocationBarViewGtk : public AutocompleteEditController,
     void set_profile(Profile* profile) { profile_ = profile; }
 
     bool IsVisible() { return GTK_WIDGET_VISIBLE(widget()); }
-    void UpdateFromTabContents(const TabContents* tab_contents);
+    void UpdateFromTabContents(TabContents* tab_contents);
 
    private:
     CHROMEGTK_CALLBACK_1(ContentSettingImageViewGtk, gboolean, OnButtonPressed,
