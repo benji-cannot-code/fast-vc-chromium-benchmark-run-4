ff3d0d268455eae0a2c6d839b109b90a82576520
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-11-30 19:31:15 +0000

    2010-11-30  Darin Adler  <darin@apple.com>
            Reviewed by Jon Honeycutt.
    
            DOM binding code generator scripts import unused File::stat Perl module
            https://bugs.webkit.org/show_bug.cgi?id=50250
    
            * bindings/scripts/CodeGeneratorCPP.pm: Removed "using File::stat".
            * bindings/scripts/CodeGeneratorJS.pm: Ditto.
            * bindings/scripts/CodeGeneratorObjC.pm: Ditto.
            * bindings/scripts/CodeGeneratorV8.pm: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package CodeGeneratorJS;
 
-use File::stat;
-
 my $module = "";
 my $outputDir = "";
 my $writeDependencies = 0;
