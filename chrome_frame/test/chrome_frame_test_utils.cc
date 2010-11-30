58aa34be491193471f01a377f8faa04fe6a9439f
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-30 17:37:19 +0000

    Add chrome frame context menu tests. Patch by mdu@chromium.org. BUG=none TEST=none Review at http://codereview.chromium.org/3656002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.cc
+++ chrome_frame/test/chrome_frame_test_utils.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iepmapi.h>
 #include <sddl.h>
 
+#include "app/clipboard/clipboard.h"
+#include "app/clipboard/scoped_clipboard_writer.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/file_version_info.h"
@@ -478,6 +480,21 @@ std::wstring GetPathAndQueryFromUrl(const std::wstring& url) {
   string16 url16 = WideToUTF16(url);
   GURL gurl = GURL(url16);
   return UTF8ToWide(gurl.PathForRequest());
+}
+
+std::wstring GetClipboardText() {
+  Clipboard clipboard;
+  string16 text16;
+  clipboard.ReadText(Clipboard::BUFFER_STANDARD, &text16);
+  return UTF16ToWide(text16);
+}
+
+void SetClipboardText(const std::wstring& text) {
+  Clipboard clipboard;
+  {
+    ScopedClipboardWriter clipboard_writer(&clipboard);
+    clipboard_writer.WriteText(WideToUTF16(text));
+  }
 }
 
 bool AddCFMetaTag(std::string* html_data) {
