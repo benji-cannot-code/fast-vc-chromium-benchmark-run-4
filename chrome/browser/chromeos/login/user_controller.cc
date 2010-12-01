5cb2cc5df116aa2b7d156404ed545a76457ded43
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 10:53:56 +0000

    Login items customization.
    Implemented round coreners for the user image. Changed border size of
    the login entries.
    
    BUG=chromium-os:8917
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5346006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67839 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_controller.cc
+++ chrome/browser/chromeos/login/user_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/guest_user_view.h"
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
+#include "chrome/browser/chromeos/login/rounded_view.h"
 #include "chrome/browser/chromeos/login/user_view.h"
 #include "chrome/browser/chromeos/login/username_view.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
@@ -54,6 +55,9 @@ const int kControlsHeight = 31;
 const int kControlsHeight = 28;
 #endif
 
+// Vertical interval between the image and the textfield.
+const int kVerticalIntervalSize = 10;
+
 // Widget that notifies window manager about clicking on itself.
 // Doesn't send anything if user is selected.
 class ClickNotifyingWidget : public views::WidgetGtk {
@@ -83,6 +87,35 @@ void CloseWindow(views::WidgetGtk* window) {
   window->SetWidgetDelegate(NULL);
   window->Close();
 }
+
+// Class that sets up half rounded rectangle (only the bottom corners are
+// rounded) as a clip region of the view.
+// For more info see the file "chrome/browser/chromeos/login/rounded_view.h".
+template<typename C>
+class HalfRoundedView : public chromeos::RoundedView<C> {
+ public:
+  explicit HalfRoundedView(const std::wstring &text): RoundedView<C>(text) {
+  }
+
+ protected:
+  // Overrides ViewFilter.
+  virtual void DrawFrame(gfx::Canvas* canvas) {
+    // No frame is needed.
+  }
+
+  virtual SkRect GetViewRect() const {
+    gfx::Rect bounds = C::GetLocalBounds(false);
+    SkRect view_rect;
+    // The rectangle will be intersected with the bounds, so the correct half
+    // of the round rectangle will be obtained.
+    view_rect.iset(bounds.x(), bounds.y() - bounds.width(),
+                   bounds.x() + bounds.width(),
+                   bounds.y() + bounds.height());
+    view_rect.inset(2 * rounded_view::kStrokeWidth,
+                    2 * rounded_view::kStrokeWidth);
+    return view_rect;
+  }
+};
 
 }  // namespace
 
@@ -410,7 +443,7 @@ void UserController::CreateBorderWindow(int index,
   int height = controls_height;
   if (!is_new_user_) {
     width += kBorderSize * 2;
-    height += 3 * kBorderSize + kUserImageSize;
+    height += 2 * kBorderSize + kUserImageSize + kVerticalIntervalSize;
   }
 
   border_window_ = new WidgetGtk(WidgetGtk::TYPE_WINDOW);
@@ -463,7 +496,7 @@ WidgetGtk* UserController::CreateLabelWindow(int index,
   }
 
   views::Label *label = is_new_user_ ?
-      new views::Label(text) : new UsernameView(text);
+      new views::Label(text) : new HalfRoundedView<UsernameView>(text);
 
   label->SetColor(kTextColor);
   label->SetFont(font);
--- chrome/browser/chromeos/login/user_view.cc
+++ chrome/browser/chromeos/login/user_view.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/resource_bundle.h"
 #include "chrome/browser/chromeos/login/helper.h"
 #include "chrome/browser/chromeos/login/rounded_rect_painter.h"
+#include "chrome/browser/chromeos/login/rounded_view.h"
+#include "gfx/canvas.h"
 #include "gfx/canvas_skia.h"
+#include "gfx/rect.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #include "views/background.h"
@@ -240,7 +243,7 @@ class PodImageView : public views::ImageView {
 UserView::UserView(Delegate* delegate, bool is_login, bool need_background)
     : delegate_(delegate),
       signout_view_(NULL),
-      image_view_(new PodImageView()),
+      image_view_(new RoundedView<PodImageView>),
       throbber_(CreateDefaultSmoothedThrobber()),
       remove_button_(NULL) {
   DCHECK(delegate);
@@ -269,6 +272,7 @@ void UserView::Init(bool need_background) {
   // UserView's layout never changes, so let's layout once here.
   image_view_->SetBounds(0, 0, kUserImageSize, kUserImageSize);
   AddChildView(image_view_);
+
   if (signout_view_) {
     signout_view_->SetBounds(0, kUserImageSize, kUserImageSize,
                              signout_view_->GetPreferredSize().height());
