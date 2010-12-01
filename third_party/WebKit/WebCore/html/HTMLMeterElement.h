e9e2cc396f552d3d7768bf655523fd8439d98fd8
yael.aharon@nokia.com <yael.aharon@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 00:13:33 +0000

    Progress and meter elements should take a form in their constructor like any other form control. https://bugs.webkit.org/show_bug.cgi?id=50195
    Reviewed by Darin Adler.
    
    WebCore: 
    
    Tests: fast/dom/HTMLMeterElement/meter-element-form.html
           fast/dom/HTMLProgressElement/progress-element-form.html
    
    * html/HTMLMeterElement.cpp:
    (WebCore::HTMLMeterElement::HTMLMeterElement):
    (WebCore::HTMLMeterElement::create):
    * html/HTMLMeterElement.h:
    * html/HTMLProgressElement.h:
    * html/HTMLTagNames.in:
    
    LayoutTests: 
    
    * fast/dom/HTMLMeterElement/meter-element-form-expected.txt: Added.
    * fast/dom/HTMLMeterElement/meter-element-form.html: Added.
    * fast/dom/HTMLProgressElement/progress-element-form-expected.txt: Added.
    * fast/dom/HTMLProgressElement/progress-element-form.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@72976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLMeterElement.h
+++ third_party/WebKit/WebCore/html/HTMLMeterElement.h
@@ -29,7 +29,7 @@ namespace WebCore {
 
 class HTMLMeterElement : public HTMLFormControlElement {
 public:
-    static PassRefPtr<HTMLMeterElement> create(const QualifiedName&, Document*);
+    static PassRefPtr<HTMLMeterElement> create(const QualifiedName&, Document*, HTMLFormElement*);
 
     enum GaugeRegion {
         GaugeRegionOptimum,
@@ -57,7 +57,7 @@ public:
 
     GaugeRegion gaugeRegion() const;
 private:
-    HTMLMeterElement(const QualifiedName&, Document*);
+    HTMLMeterElement(const QualifiedName&, Document*, HTMLFormElement*);
 
     virtual bool recalcWillValidate() const { return false; }
 
