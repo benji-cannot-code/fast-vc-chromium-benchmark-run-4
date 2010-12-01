c45f78c79ba14c2c00af9d9c86d735ee9d017cb8
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 03:19:32 +0000

    2010-11-30  Ryosuke Niwa  <rniwa@webkit.org>
            Unreviewed speculative build fix for Windows.
    
            * UIProcess/win/WebView.cpp:
            (WebKit::WebView::pageDidEnterAcceleratedCompositing):
            (WebKit::WebView::pageDidLeaveAcceleratedCompositing):
            (WebKit::WebView::switchToDrawingAreaTypeIfNecessary):
            * UIProcess/win/WebView.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.h
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.h
@@ -119,7 +119,7 @@ private:
 #if USE(ACCELERATED_COMPOSITING)
     virtual void pageDidEnterAcceleratedCompositing();
     virtual void pageDidLeaveAcceleratedCompositing();
-    void switchToDrawingAreaTypeIfNecessary(DrawingAreaProxy::Type);
+    void switchToDrawingAreaTypeIfNecessary(DrawingAreaBase::Type);
 #endif
 
     virtual HWND nativeWindow();
