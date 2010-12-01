cb69bc8fc47ee5bdedaa631063aa374bdc5706f0
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 11:41:04 +0000

    2010-12-01  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Martin Robinson.
    
            Volume control not correctly initialized
            https://bugs.webkit.org/show_bug.cgi?id=36299
    
            Replaced the mute/volume Timers with g_timeouts which are (for
            now, at least) more reliable than Timers for one-shot-fire-now
            actions.
    
            Test: media/video-volume.html
    
            * platform/graphics/MediaPlayer.cpp:
            (WebCore::MediaPlayer::~MediaPlayer): Reset the raw pointers to 0
            when destructing the player.
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mediaPlayerPrivateVolumeChangeTimeoutCallback):
            (WebCore::mediaPlayerPrivateMuteChangeTimeoutCallback):
            (WebCore::MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer):
            (WebCore::MediaPlayerPrivateGStreamer::notifyPlayerOfVolumeChange):
            (WebCore::MediaPlayerPrivateGStreamer::volumeChanged):
            (WebCore::MediaPlayerPrivateGStreamer::notifyPlayerOfMute):
            (WebCore::MediaPlayerPrivateGStreamer::muteChanged):
            (WebCore::MediaPlayerPrivateGStreamer::createGSTPlayBin): Set
            playbin2 volume/mute base on MediaPlayer related values.
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73014 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
+++ third_party/WebKit/WebCore/platform/graphics/MediaPlayer.cpp
@@ -269,6 +269,7 @@ MediaPlayer::MediaPlayer(MediaPlayerClient* client)
 
 MediaPlayer::~MediaPlayer()
 {
+    m_mediaPlayerClient = 0;
 }
 
 void MediaPlayer::load(const String& url, const ContentType& contentType)
--- third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -44,11 +44,14 @@ class GraphicsContext;
 class IntSize;
 class IntRect;
 class GStreamerGWorld;
+class MediaPlayerPrivateGStreamer;
 
 gboolean mediaPlayerPrivateMessageCallback(GstBus* bus, GstMessage* message, gpointer data);
 void mediaPlayerPrivateVolumeChangedCallback(GObject* element, GParamSpec* pspec, gpointer data);
 void mediaPlayerPrivateMuteChangedCallback(GObject* element, GParamSpec* pspec, gpointer data);
 void mediaPlayerPrivateSourceChangedCallback(GObject* element, GParamSpec* pspec, gpointer data);
+gboolean mediaPlayerPrivateVolumeChangeTimeoutCallback(MediaPlayerPrivateGStreamer*);
+gboolean mediaPlayerPrivateMuteChangeTimeoutCallback(MediaPlayerPrivateGStreamer*);
 
 class MediaPlayerPrivateGStreamer : public MediaPlayerPrivateInterface {
         friend gboolean mediaPlayerPrivateMessageCallback(GstBus* bus, GstMessage* message, gpointer data);
@@ -82,12 +85,12 @@ class MediaPlayerPrivateGStreamer : public MediaPlayerPrivateInterface {
 
             void setVolume(float);
             void volumeChanged();
-            void volumeChangedTimerFired(Timer<MediaPlayerPrivateGStreamer>*);
+            void notifyPlayerOfVolumeChange();
 
             bool supportsMuting() const;
             void setMuted(bool);
             void muteChanged();
-            void muteChangedTimerFired(Timer<MediaPlayerPrivateGStreamer>*);
+            void notifyPlayerOfMute();
 
             void setPreload(MediaPlayer::Preload);
             void fillTimerFired(Timer<MediaPlayerPrivateGStreamer>*);
@@ -177,6 +180,8 @@ class MediaPlayerPrivateGStreamer : public MediaPlayerPrivateInterface {
             bool m_delayingLoad;
             bool m_mediaDurationKnown;
             RefPtr<GStreamerGWorld> m_gstGWorld;
+            guint m_volumeTimerHandler;
+            guint m_muteTimerHandler;
     };
 }
 
