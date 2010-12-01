7449675b34a058cd5b900d454bb982d937921e91
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 08:54:42 +0000

    Refactor DOM storage context clean-up on shutdown.
    Changed from a call to DOMStorageContext::ClearLocalStorage
    from BrowserImpl to a call to that code from the destructor
    of DOMStorageContext.
    
    BUG=64627
    TEST=DOMStorageBrowserTest.ClearLocalState
    
    Review URL: http://codereview.chromium.org/5372008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/google/google_url_tracker.h"
 #include "chrome/browser/icon_manager.h"
-#include "chrome/browser/in_process_webkit/dom_storage_context.h"
 #include "chrome/browser/intranet_redirect_detector.h"
 #include "chrome/browser/io_thread.h"
 #include "chrome/browser/metrics/metrics_service.h"
@@ -505,7 +504,6 @@ bool BrowserProcessImpl::have_inspector_files() const {
 void BrowserProcessImpl::ClearLocalState(const FilePath& profile_path) {
   SQLitePersistentCookieStore::ClearLocalState(profile_path.Append(
       chrome::kCookieFilename));
-  DOMStorageContext::ClearLocalState(profile_path, chrome::kExtensionScheme);
   webkit_database::DatabaseTracker::ClearLocalState(profile_path);
   ChromeAppCacheService::ClearLocalState(profile_path);
 }
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -2067,6 +2067,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/gtk/view_id_util_browsertest.cc',
         'browser/history/history_browsertest.cc',
         'browser/idbbindingutilities_browsertest.cc',
+        'browser/in_process_webkit/dom_storage_browsertest.cc',
         'browser/in_process_webkit/indexed_db_browsertest.cc',
         'browser/net/cookie_policy_browsertest.cc',
         'browser/net/ftp_browsertest.cc',
