d67159bae4f11ada265a8f9d4cc32829f4d19b77
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 23:58:16 +0000

    2010-12-01  Alexey Marinichev  <amarinichev@chromium.org>
            Reviewed by James Robinson.
    
            [chromium] renderer does not realize hardware compositing is disabled and crashes
            https://bugs.webkit.org/show_bug.cgi?id=50264
    
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::setRootGraphicsLayer): repaint always
            (WebKit::WebViewImpl::setIsAcceleratedCompositingActive): null check
            (WebKit::WebViewImpl::reallocateRenderer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebViewImpl.cpp
@@ -2273,17 +2273,15 @@ bool WebViewImpl::allowsAcceleratedCompositing()
 
 void WebViewImpl::setRootGraphicsLayer(WebCore::PlatformLayer* layer)
 {
-    bool wasActive = m_isAcceleratedCompositingActive;
     setIsAcceleratedCompositingActive(layer ? true : false);
     if (m_layerRenderer)
         m_layerRenderer->setRootLayer(layer);
-    if (wasActive != m_isAcceleratedCompositingActive) {
-        IntRect damagedRect(0, 0, m_size.width, m_size.height);
-        if (m_isAcceleratedCompositingActive)
-            invalidateRootLayerRect(damagedRect);
-        else
-            m_client->didInvalidateRect(damagedRect);
-    }
+
+    IntRect damagedRect(0, 0, m_size.width, m_size.height);
+    if (m_isAcceleratedCompositingActive)
+        invalidateRootLayerRect(damagedRect);
+    else
+        m_client->didInvalidateRect(damagedRect);
 }
 
 void WebViewImpl::setRootLayerNeedsDisplay()
@@ -2382,7 +2380,8 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
 
     if (!active) {
         m_isAcceleratedCompositingActive = false;
-        m_layerRenderer->finish(); // finish all GL rendering before we hide the window?
+        if (m_layerRenderer)
+            m_layerRenderer->finish(); // finish all GL rendering before we hide the window?
         m_client->didActivateAcceleratedCompositing(false);
         return;
     }
@@ -2519,7 +2518,6 @@ void WebViewImpl::reallocateRenderer()
     m_layerRenderer = layerRenderer;
 
     // Enable or disable accelerated compositing and request a refresh.
-    m_isAcceleratedCompositingActive = false;
     setRootGraphicsLayer(m_layerRenderer ? m_layerRenderer->rootLayer() : 0);
 }
 #endif
