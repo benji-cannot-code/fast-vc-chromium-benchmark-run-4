2659d19b3a6b6d68518aa45302ab7108e828ba8b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 01:04:21 +0000

    Fix test-only valgrind leak.
    Also initialize cocoa explicitly; nobody happens to call |NSApplication sharedApplication| before cocoa calls are made. Also have an explicit autorelease pool, since we don't derive from PlatfromTest
    
    BUG=64797
    TEST=suppression bug_64797 goes away
    
    Review URL: http://codereview.chromium.org/5309008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_mac.mm
+++ chrome/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1713,8 +1713,6 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
     return;
   }
 
-  DCHECK(
-      renderWidgetHostView_->render_widget_host_->process()->HasConnection());
   DCHECK(!renderWidgetHostView_->about_to_validate_and_paint_);
 
   renderWidgetHostView_->about_to_validate_and_paint_ = true;
