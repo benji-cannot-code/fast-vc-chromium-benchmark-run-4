3e0470ea21224fc4d5a0aa5c8b94056d1f768f82
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 06:12:38 +0000

    Fix bug 64688: Expose typed arrays in Workers
    BUG=64688
    TEST=Manual test
    
    Review URL: http://codereview.chromium.org/5422001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67819 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/worker/worker_thread.cc
+++ chrome/worker/worker_thread.cc
@@ -59,6 +59,9 @@ WorkerThread::WorkerThread() {
 
   WebRuntimeFeatures::enableFileSystem(
       !command_line.HasSwitch(switches::kDisableFileSystem));
+
+  WebRuntimeFeatures::enableWebGL(
+      !command_line.HasSwitch(switches::kDisableExperimentalWebGL));
 }
 
 WorkerThread::~WorkerThread() {
