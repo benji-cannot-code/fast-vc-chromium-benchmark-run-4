b737a0d4525d4151b3bd85e4f4ac402c5b7eb964
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 23:04:01 +0000

    2010-12-01  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Anders Carlsson.
    
            Size changes on layer-backed drawing areas don't always end up with the correct value
            https://bugs.webkit.org/show_bug.cgi?id=50351
            <rdar://problem/8692966>
    
            When the UI process does not receive didSetSize messages promptly,
            LayerBackedDrawingAreaProxy::setSize() bails early based on the
            m_isWaitingForDidSetFrameNotification flag.
    
            In this situation, the final size may be incorrect. didSetSize()
            needs to compare the size that the web process has to the expected size,
            and send a final setSize if they differ.
    
            * UIProcess/LayerBackedDrawingAreaProxy.cpp:
            (WebKit::LayerBackedDrawingAreaProxy::didSetSize):
            (WebKit::LayerBackedDrawingAreaProxy::didReceiveMessage):
            * UIProcess/LayerBackedDrawingAreaProxy.h:
            * WebProcess/WebPage/LayerBackedDrawingArea.cpp:
            (WebKit::LayerBackedDrawingArea::setSize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/LayerBackedDrawingAreaProxy.cpp
+++ third_party/WebKit/WebKit2/UIProcess/LayerBackedDrawingAreaProxy.cpp
@@ -79,6 +79,7 @@ void LayerBackedDrawingAreaProxy::setSize(const IntSize& viewSize)
 
     if (m_isWaitingForDidSetFrameNotification)
         return;
+
     m_isWaitingForDidSetFrameNotification = true;
 
     page->process()->responsivenessTimer()->start();
@@ -114,9 +115,12 @@ void LayerBackedDrawingAreaProxy::setPageIsVisible(bool isVisible)
     // FIXME: We should request a full repaint here if needed.
 }
     
-void LayerBackedDrawingAreaProxy::didSetSize()
+void LayerBackedDrawingAreaProxy::didSetSize(const IntSize& size)
 {
     m_isWaitingForDidSetFrameNotification = false;
+
+    if (size != m_lastSetViewSize)
+        setSize(m_lastSetViewSize);
 
     WebPageProxy* page = this->page();
     page->process()->responsivenessTimer()->stop();
@@ -136,7 +140,10 @@ void LayerBackedDrawingAreaProxy::didReceiveMessage(CoreIPC::Connection*, CoreIP
             break;
         }
         case DrawingAreaProxyMessage::DidSetSize: {
-            didSetSize();
+            IntSize size;
+            if (!arguments->decode(CoreIPC::Out(size)))
+                return;
+            didSetSize(size);
             break;
         }
         default:
