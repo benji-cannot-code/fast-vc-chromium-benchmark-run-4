f2c8887fbfb1dbc56b5d17b9a00083e57e28106a
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 17:36:20 +0000

    Fix SSLUITest.TestUnsafeContentsInWorker*.
    These still expected fixed port testserver's, but were undetected as failing since marked FLAKY.
    
    BUG=64785
    TEST=browser_tests --gtest_filter="*TestUnsafeContentsInWorker*"
    
    Review URL: http://codereview.chromium.org/5472001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/ssl/unsafe_worker.js
+++ chrome/test/data/ssl/unsafe_worker.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var message = "failed";
 try {
-  importScripts("https://127.0.0.1:9666/files/ssl/imported.js");
+  importScripts("REPLACE_WITH_IMPORTED_JS_URL");
 } catch(ex) {
 }
 postMessage(message);
