9bb03313f36eef097bd0749fedf34a394afa343c
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-01 06:22:44 +0000

    Fix x64 builder.
    BUG=none
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67821 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/browser_distribution.h
+++ chrome/installer/util/browser_distribution.h
@@ -26,7 +26,7 @@ class BrowserDistribution {
  public:
   virtual ~BrowserDistribution() {}
 
-  typedef enum DistributionType {
+  enum DistributionType {
     CHROME_BROWSER,
     CHROME_FRAME,
     CEEE,
