054aa3ae354b415988a5840582cf6872d02bf3b3
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-01 22:15:37 +0000

    Another windows build fix.
    * UIProcess/API/C/win/WKView.cpp:
    (WKViewCreate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/C/win/WKView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/C/win/WKView.cpp
@@ -38,7 +38,7 @@ WKTypeID WKViewGetTypeID()
 
 WKViewRef WKViewCreate(RECT rect, WKPageNamespaceRef pageNamespaceRef, WKPageGroupRef pageGroupRef, HWND parentWindow)
 {
-    RefPtr<WebView> view = WebView::create(rect, toImpl(pageNamespaceRef), toImpl(pageGroupRef), parentWindow, VisibleToInjectedBundle);
+    RefPtr<WebView> view = WebView::create(rect, toImpl(pageNamespaceRef), toImpl(pageGroupRef), parentWindow);
     return toAPI(view.release().releaseRef());
 }
 
