8f97a2b6f9a2ae6280f6ff92869d9b75d3e515ee
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-02 02:22:18 +0000

    Build fix.
    * JavaScriptCore.xcodeproj/project.pbxproj: Export OSAllocator.h as private
    so other projects can see it.
    
    * wtf/OSAllocatorPosix.cpp: #include UnusedParam.h for UNUSED_PARAM.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73092 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/OSAllocatorPosix.cpp
+++ third_party/WebKit/JavaScriptCore/wtf/OSAllocatorPosix.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/errno.h>
 #include <sys/mman.h>
 #include <wtf/Assertions.h>
+#include <wtf/UnusedParam.h>
 
 namespace WTF {
 
