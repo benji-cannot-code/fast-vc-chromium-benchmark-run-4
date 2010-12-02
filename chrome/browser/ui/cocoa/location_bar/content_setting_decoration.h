eff9dddf3754f2ed7cc572006625072a3db56b58
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-02 20:27:56 +0000

    [Mac] Implement the new popup blocker UI. This is behind a flag.
    This CL is based on work by pinkerton@chromium.org.
    
    BUG=39260,53508
    TEST=With --enable-blocked-content-animation, visit http://popuptest.com and run a test. The omnibox should run an animation indicating a blocked popup.
    
    Review URL: http://codereview.chromium.org/5297008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68059 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/location_bar/content_setting_decoration.h
+++ chrome/browser/ui/cocoa/location_bar/content_setting_decoration.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // ContentSettingDecoration is used to display the content settings
 // images on the current page.
 
+@class ContentSettingAnimationState;
 class ContentSettingImageModel;
 class LocationBarViewMac;
 class Profile;
@@ -34,6 +35,12 @@ class ContentSettingDecoration : public ImageDecoration {
   virtual bool AcceptsMousePress() { return true; }
   virtual bool OnMousePressed(NSRect frame);
   virtual NSString* GetToolTip();
+  virtual CGFloat GetWidthForSpace(CGFloat width);
+  virtual void DrawInFrame(NSRect frame, NSView* control_view);
+
+  // Called from internal animator. Only public because ObjC objects can't
+  // be friends.
+  virtual void AnimationTimerFired();
 
  private:
   // Helper to get where the bubble point should land.  Similar to
@@ -43,12 +50,25 @@ class ContentSettingDecoration : public ImageDecoration {
 
   void SetToolTip(NSString* tooltip);
 
+  // Returns an attributed string with the animated text. Caller is responsible
+  // for releasing.
+  NSAttributedString* CreateAnimatedText();
+
+  // Measure the width of the animated text.
+  CGFloat MeasureTextWidth();
+
   scoped_ptr<ContentSettingImageModel> content_setting_image_model_;
 
   LocationBarViewMac* owner_;  // weak
   Profile* profile_;  // weak
 
   scoped_nsobject<NSString> tooltip_;
+
+  // Used when the decoration has animated text.
+  scoped_nsobject<ContentSettingAnimationState> animation_;
+  CGFloat text_width_;
+  scoped_nsobject<NSAttributedString> animated_text_;
+  scoped_nsobject<NSGradient> gradient_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentSettingDecoration);
 };
