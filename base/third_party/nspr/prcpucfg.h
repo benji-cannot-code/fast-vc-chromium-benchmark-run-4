6353ac28e4a88379c40786ad5738dd34041092b9
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-02 08:14:42 +0000

    Update prcpucfg.h to build in Native Client.
    Native Client is similar to Linux, but it does not define __linux__.
    
    http://codereview.chromium.org/5479001/
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67985 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/nspr/prcpucfg.h
+++ base/third_party/nspr/prcpucfg.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/third_party/nspr/prcpucfg_win.h"
 #elif defined(__APPLE__)
 #include "base/third_party/nspr/prcpucfg_mac.h"
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__native_client__)
 #include "base/third_party/nspr/prcpucfg_linux.h"
 #elif defined(__FreeBSD__)
 #include "base/third_party/nspr/prcpucfg_freebsd.h"
