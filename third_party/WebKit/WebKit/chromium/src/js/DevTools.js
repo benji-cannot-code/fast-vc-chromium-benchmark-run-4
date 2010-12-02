67062e1dc508db2c675d9ceee107ac9f7f407328
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-02 17:42:03 +0000

    2010-12-01  Andrey Kosyakov  <caseq@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: [Resources panel] [HAR] Need a way to save timing data.
            Re-implementing HAR export functionality that used to be in Resources
            tab in Network tab.
            https://bugs.webkit.org/show_bug.cgi?id=45663
    
            * inspector/front-end/NetworkPanel.js:
            (WebInspector.NetworkPanel.prototype._createTable):
            (WebInspector.NetworkPanel.prototype._toggleViewingResourceMode):
            (WebInspector.NetworkPanel.prototype._contextMenu):
            (WebInspector.NetworkPanel.prototype._exportAll):
            (WebInspector.NetworkPanel.prototype._exportResource):
            (WebInspector.NetworkDataGridNode.prototype.refreshResource):
            * inspector/front-end/networkPanel.css:
            (.data-grid table.data tr.revealed.network-item):
            (.data-grid.filter-other table.data tr.revealed.network-category-other):
    
    2010-12-01  Andrey Kosyakov  <caseq@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: [Resources panel] [HAR] Need a way to save timing data.
            Enable resource export to HAR for Chromium.
            https://bugs.webkit.org/show_bug.cgi?id=45663
    
            * src/js/DevTools.js: Preferences.resourceExportEnabled = true
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/js/DevTools.js
+++ third_party/WebKit/WebKit/chromium/src/js/DevTools.js
@@ -48,6 +48,7 @@ var context = {};  // Used by WebCore's inspector routines.
     Preferences.onlineDetectionEnabled = false;
     Preferences.nativeInstrumentationEnabled = true;
     Preferences.fileSystemEnabled = false;
+    Preferences.resourceExportEnabled = true;
     Preferences.showTimingTab = true;
     Preferences.showCookiesTab = true;
 })();
