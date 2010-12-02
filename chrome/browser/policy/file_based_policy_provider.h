b2b118d13acfd788b91e59ba4bbbc8faca5cd0bb
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-02 21:29:48 +0000

    fix build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68074 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/file_based_policy_provider.h
+++ chrome/browser/policy/file_based_policy_provider.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "base/time.h"
 #include "base/weak_ptr.h"
-#include "chrome/browser/file_path_watcher.h"
+#include "chrome/browser/file_path_watcher/file_path_watcher.h"
 #include "chrome/browser/policy/configuration_policy_provider.h"
 
 class CancelableTask;
