ea4ab1e397f75a4105bcb4b3ddc258808c0d715b
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-02 00:25:13 +0000

    histogram API of WebKitClient implemented in chromium/webkit/glue
    BUG=62865
    TEST=none
    
    Review URL: http://codereview.chromium.org/5406004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67920 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitclient_impl.cc
+++ webkit/glue/webkitclient_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lock.h"
 #include "base/message_loop.h"
 #include "base/metrics/stats_counters.h"
+#include "base/metrics/histogram.h"
 #include "base/process_util.h"
 #include "base/platform_file.h"
 #include "base/singleton.h"
@@ -253,6 +254,30 @@ void WebKitClientImpl::decrementStatsCounter(const char* name) {
 
 void WebKitClientImpl::incrementStatsCounter(const char* name) {
   base::StatsCounter(name).Increment();
+}
+
+void WebKitClientImpl::histogramCustomCounts(
+    const char* name, int sample, int min, int max, int bucket_count) {
+  // Copied from histogram macro, but without the static variable caching
+  // the histogram because name is dynamic.
+  scoped_refptr<base::Histogram> counter =
+      base::Histogram::FactoryGet(name, min, max, bucket_count,
+          base::Histogram::kUmaTargetedHistogramFlag);
+  DCHECK_EQ(name, counter->histogram_name());
+  if (counter.get())
+    counter->Add(sample);
+}
+
+void WebKitClientImpl::histogramEnumeration(
+    const char* name, int sample, int boundary_value) {
+  // Copied from histogram macro, but without the static variable caching
+  // the histogram because name is dynamic.
+  scoped_refptr<base::Histogram> counter =
+      base::LinearHistogram::FactoryGet(name, 1, boundary_value,
+          boundary_value + 1, base::Histogram::kUmaTargetedHistogramFlag);
+  DCHECK_EQ(name, counter->histogram_name());
+  if (counter.get())
+    counter->Add(sample);
 }
 
 void WebKitClientImpl::traceEventBegin(const char* name, void* id,
