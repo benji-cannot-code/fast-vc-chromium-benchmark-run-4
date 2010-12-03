38f4c5fe899bd5a0e0eaa1849f75b268f3357564
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 00:55:30 +0000

    DOMUI prefs: First stage of changing lists to match mocks.
    This does two things:
    - Adds a mode to the DOMUI List class where the list auto-sizes.
    - Overrides the default list styling to make it more like the mocks (just basic cosmetic styling at this point).
    
    BUG=None
    TEST=Lists in DOMUI prefs should look more like the current mocks.
    
    Review URL: http://codereview.chromium.org/5628001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/search_engine_manager.js
+++ chrome/browser/resources/options/search_engine_manager.js
@@ -32,6 +32,7 @@ cr.define('options', function() {
       options.search_engines.SearchEngineList.decorate(this.list_);
       var selectionModel = new ListSingleSelectionModel
       this.list_.selectionModel = selectionModel;
+      this.list_.autoExpands = true;
 
       selectionModel.addEventListener('change',
           this.selectionChanged_.bind(this));
@@ -59,10 +60,6 @@ cr.define('options', function() {
           localStrings.getStringWithoutAccelerator('addSearchEngineButton');
       $('removeSearchEngineButton').textContent =
           localStrings.getStringWithoutAccelerator('removeSearchEngineButton');
-
-      this.addEventListener('visibleChange', function(event) {
-          $('searchEngineList').redraw();
-      });
     },
 
     /**
