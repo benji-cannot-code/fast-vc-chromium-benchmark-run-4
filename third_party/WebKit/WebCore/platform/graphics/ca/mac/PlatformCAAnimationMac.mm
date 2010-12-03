5eca127bbe5df8564990e8a3e7aad7dcf66bd30f
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-03 02:24:27 +0000

    2010-12-02  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Simon Fraser.
    
            Fix Leopard build broken by http://trac.webkit.org/changeset/73217
    
            * platform/graphics/ca/GraphicsLayerCA.cpp:
            (WebCore::getTransformFunctionValue):
            (WebCore::GraphicsLayerCA::updateContentsTransform):
            * platform/graphics/ca/PlatformCALayer.h:
            * platform/graphics/ca/mac/PlatformCAAnimationMac.mm:
            (PlatformCAAnimation::valueFunction):
            (PlatformCAAnimation::setValueFunction):
            * platform/graphics/ca/mac/PlatformCALayerMac.mm:
            (PlatformCALayer::PlatformCALayer):
            (PlatformCALayer::anchorPoint):
            (PlatformCALayer::setAnchorPoint):
            (PlatformCALayer::contentsTransform):
            (PlatformCALayer::setContentsTransform):
            (PlatformCALayer::isGeometryFlipped):
            (PlatformCALayer::setGeometryFlipped):
            (PlatformCALayer::setMinificationFilter):
            (PlatformCALayer::setMagnificationFilter):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ca/mac/PlatformCAAnimationMac.mm
+++ third_party/WebKit/WebCore/platform/graphics/ca/mac/PlatformCAAnimationMac.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "PlatformString.h"
 #import "TimingFunction.h"
 #import <QuartzCore/QuartzCore.h>
+#import <wtf/UnusedParam.h>
 
 #define HAVE_MODERN_QUARTZCORE (!defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD))
 
@@ -84,6 +85,7 @@ static PlatformCAAnimation::FillModeType fromCAFillModeType(NSString* string)
     return PlatformCAAnimation::Forwards;
 }
 
+#if HAVE_MODERN_QUARTZCORE
 static NSString* toCAValueFunctionType(PlatformCAAnimation::ValueFunctionType type)
 {
     switch (type) {
@@ -140,6 +142,7 @@ static PlatformCAAnimation::ValueFunctionType fromCAValueFunctionType(NSString* 
 
     return PlatformCAAnimation::NoValueFunction;
 }
+#endif
 
 static CAMediaTimingFunction* toCAMediaTimingFunction(const TimingFunction* timingFunction)
 {
@@ -359,13 +362,21 @@ void PlatformCAAnimation::setAdditive(bool value)
 
 PlatformCAAnimation::ValueFunctionType PlatformCAAnimation::valueFunction() const
 {
+#if HAVE_MODERN_QUARTZCORE
     CAValueFunction* vf = [m_animation.get() valueFunction];
     return fromCAValueFunctionType([vf name]);
+#else
+    return NoValueFunction;
+#endif
 }
 
 void PlatformCAAnimation::setValueFunction(ValueFunctionType value)
 {
+#if HAVE_MODERN_QUARTZCORE
     [m_animation.get() setValueFunction:[CAValueFunction functionWithName:toCAValueFunctionType(value)]];
+#else
+    UNUSED_PARAM(value);
+#endif
 }
 
 void PlatformCAAnimation::setFromValue(float value)
