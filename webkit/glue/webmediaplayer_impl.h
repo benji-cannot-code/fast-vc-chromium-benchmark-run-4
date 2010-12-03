54f100ad5f3de621b5fa732bda37f6686998166a
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 01:49:16 +0000

    Revert 68094 - Refactoring BufferedDataSource to work with WebURLLoader instead of a MediaResourceLoaderBridge.
    One thing to notice is that both buffered_data_source_unittest and simple_data_source_unittest need to have a way to inject a MockWebURLLoader into the BufferedResourceLoader and the SimpleDataSource.  In order to make sure a new one is not created during a Start(), I introduced the function SetURLLoaderForTest and keep_test_loader flag.
    
    Patch by annacc@chromium.org:
    http://codereview.chromium.org/3863002/
    
    BUG=16751
    TEST=src/xcodebuild/Debug/test_shell_tests --gtest_filter=Buffered*
    src/xcodebuild/Debug/test_shell_tests --gtest_filter=Simple*
    src/webkit/tools/layout_tests/run_webkit_tests.sh --debug media
    webkit/tools/layout_tests/run_webkit_tests.sh --debug http/tests/media
    
    TBR=scherkus@chromium.org
    Review URL: http://codereview.chromium.org/5619002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68126 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webmediaplayer_impl.h
+++ webkit/glue/webmediaplayer_impl.h
@@ -70,10 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
-namespace WebKit {
-class WebFrame;
-}
-
 namespace webkit_glue {
 
 class MediaResourceLoaderBridgeFactory;
@@ -181,7 +177,8 @@ class WebMediaPlayerImpl : public WebKit::WebMediaPlayer,
 
   // Finalizes initialization of the object.
   bool Initialize(
-      WebKit::WebFrame* frame,
+      MediaResourceLoaderBridgeFactory* bridge_factory_simple,
+      MediaResourceLoaderBridgeFactory* bridge_factory_buffered,
       bool use_simple_data_source,
       scoped_refptr<WebVideoRenderer> web_video_renderer);
 
