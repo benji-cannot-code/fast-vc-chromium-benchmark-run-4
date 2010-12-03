0be55610bbac62d83d2f256054665fc81cc8be6c
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 16:00:52 +0000

    Move database handling code to the Backend class to clean up the interface.
    BUG=64920
    TEST=None
    
    Review URL: http://codereview.chromium.org/5610003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/sqlite_persistent_cookie_store.h
+++ chrome/browser/net/sqlite_persistent_cookie_store.h
@@ -12,14 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "app/sql/meta_table.h"
-#include "base/file_path.h"
 #include "base/ref_counted.h"
 #include "net/base/cookie_monster.h"
-
-namespace sql {
-class Connection;
-}
 
 class FilePath;
 
@@ -41,13 +35,7 @@ class SQLitePersistentCookieStore
  private:
   class Backend;
 
-  // Database upgrade statements.
-  bool EnsureDatabaseVersion(sql::Connection* db);
-
-  FilePath path_;
   scoped_refptr<Backend> backend_;
-
-  sql::MetaTable meta_table_;
 
   DISALLOW_COPY_AND_ASSIGN(SQLitePersistentCookieStore);
 };
