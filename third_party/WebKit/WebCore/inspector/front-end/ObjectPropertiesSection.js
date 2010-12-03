c60c2494bb14da83796936c00ee0ccbcd8df9325
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-03 20:11:23 +0000

    2010-12-03  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: reveal in elements panel does not switch the panel itself.
            https://bugs.webkit.org/show_bug.cgi?id=50408
    
            * inspector/front-end/ElementsTreeOutline.js:
            (WebInspector.ElementsTreeOutline.prototype._contextMenuEventFired.focusElement):
            (WebInspector.ElementsTreeOutline.prototype._contextMenuEventFired):
            * inspector/front-end/ObjectPropertiesSection.js:
            (WebInspector.ObjectPropertyTreeElement.prototype._contextMenuEventFired):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ObjectPropertiesSection.js
+++ third_party/WebKit/WebCore/inspector/front-end/ObjectPropertiesSection.js
@@ -203,8 +203,10 @@ WebInspector.ObjectPropertyTreeElement.prototype = {
     {
         function selectNode(nodeId)
         {
-            if (nodeId)
+            if (nodeId) {
+                WebInspector.currentPanel = WebInspector.panels.elements;
                 WebInspector.panels.elements.focusedDOMNode = WebInspector.domAgent.nodeForId(nodeId);
+            }
         }
 
         function revealElement()
