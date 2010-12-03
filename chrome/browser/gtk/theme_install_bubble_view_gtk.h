361979aa8eb7a6e61b43fed7f6913c6547c9c542
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 22:35:28 +0000

    gtk: Make use of the gtk signal macros in ThemeInstallBubbleViewGtk class.
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/5385005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/theme_install_bubble_view_gtk.h
+++ chrome/browser/gtk/theme_install_bubble_view_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
+#include "app/gtk_signal.h"
 #include "base/basictypes.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
@@ -36,23 +37,12 @@ class ThemeInstallBubbleViewGtk : public NotificationObserver {
   void MoveWindow();
 
   // Our parent is going down; self destruct.
-  static gboolean HandleParentUnmapThunk(GtkWidget* widget,
-                                         GdkEvent* event,
-                                         gpointer user_data) {
-    return reinterpret_cast<ThemeInstallBubbleViewGtk*>(user_data)->
-        HandleParentUnmap();
-  }
-  gboolean HandleParentUnmap();
+  CHROMEGTK_CALLBACK_0(ThemeInstallBubbleViewGtk, gboolean, OnUnmapEvent);
 
   // Draw the background. This is only signalled if we are using a compositing
   // window manager, otherwise we just use ActAsRoundedWindow().
-  static gboolean HandleExposeEventThunk(GtkWidget* widget,
-                                         GdkEventExpose* event,
-                                         gpointer user_data) {
-    return reinterpret_cast<ThemeInstallBubbleViewGtk*>(user_data)->
-        HandleExposeEvent(event);
-  }
-  gboolean HandleExposeEvent(GdkEventExpose* event);
+  CHROMEGTK_CALLBACK_1(ThemeInstallBubbleViewGtk, gboolean,
+                       OnExpose, GdkEventExpose*);
 
   GtkWidget* widget_;
 
