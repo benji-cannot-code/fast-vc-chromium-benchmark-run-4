813ac070d0e3726278cdca2461d69464de1c4381
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-03 19:47:38 +0000

    2010-12-03  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by David Levin.
    
            Use String::adopt in TextCodecWinCE::decode
            https://bugs.webkit.org/show_bug.cgi?id=50319
    
            * platform/text/wince/TextCodecWinCE.cpp:
            (WebCore::TextCodecWinCE::decode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/wince/TextCodecWinCE.cpp
+++ third_party/WebKit/WebCore/platform/text/wince/TextCodecWinCE.cpp
@@ -334,8 +334,7 @@ String TextCodecWinCE::decode(const char* bytes, size_t length, bool flush, bool
         result.append(L'?');
         sawError = true;
         if (stopOnError)
-            return String(result.data(), result.size());
-
+            return String::adopt(result);
 
         if (left == 1)
             break;
@@ -352,7 +351,8 @@ String TextCodecWinCE::decode(const char* bytes, size_t length, bool flush, bool
         }
     } else
         m_decodeBuffer.clear();
-    return String(result.data(), result.size());
+
+    return String::adopt(result);
 }
 
 CString TextCodecWinCE::encode(const UChar* characters, size_t length, UnencodableHandling)
