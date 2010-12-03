56f587a3938e0bc4ba83535217de0f32eaae0d00
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-03 19:42:46 +0000

    Make google-a identity field not editable. BUG=chromium-os:8422 TEST=manual Review URL: http://codereview.chromium.org/5513005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos_internet_options.js
+++ chrome/browser/resources/options/chromeos_internet_options.js
@@ -104,7 +104,7 @@ cr.define('options', function() {
     if (data.certInPkcs) {
       chrome.send('loginToCertNetwork',[String(servicePath),
                                         String(data.certPath),
-                                        String($('inetIdentPkcs').value)]);
+                                        String(data.ident)]);
     } else {
       chrome.send('loginToCertNetwork',[String(servicePath),
                                         String($('inetCert').value),
@@ -122,7 +122,7 @@ cr.define('options', function() {
       newinfo.push($('autoConnectNetwork').checked ? "true" : "false");
       if (data.encrypted && data.certNeeded) {
         if (data.certInPkcs) {
-          newinfo.push($('inetIdentPkcs').value);
+          newinfo.push(data.ident);
         } else {
           newinfo.push($('inetIdent').value);
           newinfo.push($('inetCert').value);
@@ -286,7 +286,7 @@ cr.define('options', function() {
         if (data.certNeeded) {
           if (data.certInPkcs) {
             page.setAttribute('certPkcs', true);
-            $('inetIdentPkcs').value = data.ident;
+            $('inetIdentPkcs').textContent = data.ident;
           } else {
             page.setAttribute('cert', true);
             $('inetIdent').value = data.ident;
