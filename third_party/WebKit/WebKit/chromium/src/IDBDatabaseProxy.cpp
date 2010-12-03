8a450ae0eadd15e7535171a8cae5a7f0faa41b6b
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-03 16:14:21 +0000

    2010-11-29  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Darin Fisher.
    
            [Chromium] Clean up IndexedDB 2 sided roll bits
            https://bugs.webkit.org/show_bug.cgi?id=50160
    
            * public/WebIDBDatabase.h:
            (WebKit::WebIDBDatabase::version):
            (WebKit::WebIDBDatabase::objectStoreNames):
            (WebKit::WebIDBDatabase::deleteObjectStore):
            * public/WebIDBFactory.h:
            (WebKit::WebIDBFactory::open):
            * public/WebIDBKeyRange.h:
            * public/WebIDBObjectStore.h:
            (WebKit::WebIDBObjectStore::deleteFunction):
            * src/IDBDatabaseProxy.cpp:
            * src/IDBDatabaseProxy.h:
            * src/WebIDBKeyRange.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/IDBDatabaseProxy.cpp
+++ third_party/WebKit/WebKit/chromium/src/IDBDatabaseProxy.cpp
@@ -62,11 +62,6 @@ String IDBDatabaseProxy::name() const
     return m_webIDBDatabase->name();
 }
 
-String IDBDatabaseProxy::description() const
-{
-    return m_webIDBDatabase->description();
-}
-
 String IDBDatabaseProxy::version() const
 {
     return m_webIDBDatabase->version();
