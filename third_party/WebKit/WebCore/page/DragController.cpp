0c352e0f0a320fc604588af6feda7f3087852275
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-03 19:18:09 +0000

    2010-12-03  Daniel Cheng  <dcheng@chromium.org>
            Reviewed by Tony Chang.
    
            Dragging and dropping into an empty document crashes WebKit.
            https://bugs.webkit.org/show_bug.cgi?id=48793
    
            Remove the assert and update callers to check for a null return value.
    
            Test: manual-tests/drop-in-empty-doc.html
    
            * manual-tests/drop-in-empty-doc.html: Added.
            * manual-tests/resources/drop-in-empty-doc.xhtml: Added.
            * page/DragController.cpp:
            (WebCore::elementUnderMouse):
            (WebCore::DragController::tryDocumentDrag):
            (WebCore::DragController::concludeEditDrag):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/DragController.cpp
+++ third_party/WebKit/WebCore/page/DragController.cpp
@@ -269,6 +269,7 @@ static HTMLInputElement* asFileInput(Node* node)
     return inputElement;
 }
 
+// This can return null if an empty document is loaded.
 static Element* elementUnderMouse(Document* documentUnderMouse, const IntPoint& p)
 {
     Frame* frame = documentUnderMouse->frame();
@@ -285,7 +286,6 @@ static Element* elementUnderMouse(Document* documentUnderMouse, const IntPoint& 
     if (n)
         n = n->shadowAncestorNode();
 
-    ASSERT(n);
     return static_cast<Element*>(n);
 }
 
@@ -328,6 +328,8 @@ bool DragController::tryDocumentDrag(DragData* dragData, DragDestinationAction a
 
         IntPoint point = frameView->windowToContents(dragData->clientPosition());
         Element* element = elementUnderMouse(m_documentUnderMouse.get(), point);
+        if (!element)
+            return false;
         if (!asFileInput(element)) {
             VisibleSelection dragCaret = m_documentUnderMouse->frame()->visiblePositionForPoint(point);
             m_page->dragCaretController()->setSelection(dragCaret);
@@ -387,6 +389,8 @@ bool DragController::concludeEditDrag(DragData* dragData)
 
     IntPoint point = m_documentUnderMouse->view()->windowToContents(dragData->clientPosition());
     Element* element = elementUnderMouse(m_documentUnderMouse.get(), point);
+    if (!element)
+        return false;
     Frame* innerFrame = element->ownerDocument()->frame();
     ASSERT(innerFrame);
 
