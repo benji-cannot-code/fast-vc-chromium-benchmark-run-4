aeb1467b2c4f757e5e2c39f63b498447fbc0f65b
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-04 05:27:44 +0000

    [GTK+] Populate DumpRenderTreeSupportGtk https://bugs.webkit.org/show_bug.cgi?id=48429
    Reviewed by Martin Robinson.
    Patch by Antonio Gomes <agomes@rim.com>
    
    WebKit/gtk:
    
    Moved most of the webkit_web_frame* methods defined as private APIs in webkitprivate.h
    to DumpRenderTreeSupportGtk, as static class methods. These methods had
    not gone throught the formal API review process and were only being used by DRT for now.
    
    The essence of each original method name were kept. For example
    webkit_web_frame_get_children(...) was renamed to DumpRenderTreeSupportGtk::getFrameChildren(...),
    and so on.
    
    Also some of the method bodies were changed to return non-glib types (gchar* -> CString).
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::getFrameChildren):
    (DumpRenderTreeSupportGtk::getInnerText):
    (DumpRenderTreeSupportGtk::dumpRenderTree):
    (DumpRenderTreeSupportGtk::counterValueForElementById):
    (DumpRenderTreeSupportGtk::pageNumberForElementById):
    (DumpRenderTreeSupportGtk::numberOfPagesForFrame):
    (DumpRenderTreeSupportGtk::getPendingUnloadEventCount):
    (DumpRenderTreeSupportGtk::pauseAnimation):
    (DumpRenderTreeSupportGtk::pauseTransition):
    (DumpRenderTreeSupportGtk::pauseSvgAnimation):
    (DumpRenderTreeSupportGtk::markerTextForListItem):
    (DumpRenderTreeSupportGtk::numberOfActiveAnimations):
    (DumpRenderTreeSupportGtk::suspendAnimations):
    (DumpRenderTreeSupportGtk::resumeAnimations):
    (DumpRenderTreeSupportGtk::clearMainFrameName):
    (DumpRenderTreeSupportGtk::getFocusedAccessibleElement):
    * WebCoreSupport/DumpRenderTreeSupportGtk.h:
    * webkit/webkitprivate.h:
    * webkit/webkitwebframe.cpp:
    
    WebKitTools:
    
    Made the previous calls to webkit_web_frame* functions defined as private
    APIs in webkitprivate.h go through DRTSupportGtk.
    
    * DumpRenderTree/gtk/AccessibilityControllerGtk.cpp:
    (AccessibilityController::focusedElement):
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (dumpFramesAsText):
    (resetDefaultsToConsistentValues):
    (dump):
    (webViewDocumentLoadFinished):
    * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    (LayoutTestController::counterValueForElementById):
    (LayoutTestController::pageNumberForElementById):
    (LayoutTestController::numberOfPages):
    (LayoutTestController::pauseAnimationAtTimeOnElementWithId):
    (LayoutTestController::pauseTransitionAtTimeOnElementWithId):
    (LayoutTestController::sampleSVGAnimationForElementAtTime):
    (LayoutTestController::numberOfActiveAnimations):
    (LayoutTestController::suspendAnimations):
    (LayoutTestController::resumeAnimations):
    (LayoutTestController::markerTextForListItem):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
+++ third_party/WebKit/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
@@ -20,7 +20,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DumpRenderTreeSupportGtk_h
 #define DumpRenderTreeSupportGtk_h
 
+
 #include "JSStringRef.h"
+
+#include <atk/atk.h>
+#include <glib.h>
+#include <webkit/webkitdefines.h>
+#include <webkit/webkitwebframe.h>
+#include <wtf/text/CString.h>
 
 class DumpRenderTreeSupportGtk {
 
@@ -34,6 +41,24 @@ public:
     static void setLinksIncludedInFocusChain(bool);
     static bool linksIncludedInFocusChain();
     static JSValueRef nodesFromRect(JSContextRef context, JSValueRef value, int x, int y, unsigned top, unsigned right, unsigned bottom, unsigned left, bool ignoreClipping);
+
+    // FIXME: Move these to webkitwebframe.h once their API has been discussed.
+    static GSList* getFrameChildren(WebKitWebFrame* frame);
+    static WTF::CString getInnerText(WebKitWebFrame* frame);
+    static WTF::CString dumpRenderTree(WebKitWebFrame* frame);
+    static WTF::CString counterValueForElementById(WebKitWebFrame* frame, const char* id);
+    static int pageNumberForElementById(WebKitWebFrame* frame, const char* id, float pageWidth, float pageHeight);
+    static int numberOfPagesForFrame(WebKitWebFrame* frame, float pageWidth, float pageHeight);
+    static guint getPendingUnloadEventCount(WebKitWebFrame* frame);
+    static bool pauseAnimation(WebKitWebFrame* frame, const char* name, double time, const char* element);
+    static bool pauseTransition(WebKitWebFrame* frame, const char* name, double time, const char* element);
+    static bool pauseSVGAnimation(WebKitWebFrame* frame, const char* animationId, double time, const char* elementId);
+    static WTF::CString markerTextForListItem(WebKitWebFrame* frame, JSContextRef context, JSValueRef nodeObject);
+    static unsigned int numberOfActiveAnimations(WebKitWebFrame* frame);
+    static void suspendAnimations(WebKitWebFrame* frame);
+    static void resumeAnimations(WebKitWebFrame* frame);
+    static void clearMainFrameName(WebKitWebFrame* frame);
+    static AtkObject* getFocusedAccessibleElement(WebKitWebFrame* frame);
 
 private:
     static bool s_drtRun;
--- third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
+++ third_party/WebKit/WebKitTools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
@@ -30,14 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AccessibilityUIElement.h"
 #include "DumpRenderTree.h"
+#include "WebCoreSupport/DumpRenderTreeSupportGtk.h"
 
 #include <atk/atk.h>
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
-
-extern "C" {
-extern AtkObject* webkit_web_frame_get_focused_accessible_element(WebKitWebFrame*);
-}
 
 AccessibilityController::AccessibilityController()
 {
@@ -55,7 +52,7 @@ AccessibilityUIElement AccessibilityController::elementAtPoint(int x, int y)
 
 AccessibilityUIElement AccessibilityController::focusedElement()
 {
-    AtkObject* accessible =  webkit_web_frame_get_focused_accessible_element(mainFrame);
+    AtkObject* accessible =  DumpRenderTreeSupportGtk::getFocusedAccessibleElement(mainFrame);
     if (!accessible)
         return 0;
 
