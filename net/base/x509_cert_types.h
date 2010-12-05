c0e2da0680bc9a7abf538b4657b79a13878d83e3
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-05 19:48:54 +0000

    Cleanup X509CertificateMac style nits and introduce a crypto library agnostic means of parsing certificate dates.
    In the process of addressing style issues, a bug was found in the date/time parsing routines used on Mac. Though not currently hit, it could lead to wild misrepresentation of the validity period for a certificate. Switch to using the routine used when using OpenSSL, which does things correctly.
    
    BUG=none
    TEST=existing
    
    Review URL: http://codereview.chromium.org/4653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68322 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_cert_types.h
+++ net/base/x509_cert_types.h
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Security/x509defs.h>
 #endif
 
+namespace base {
+class Time;
+class StringPiece;
+}  // namespace base
+
 namespace net {
 
 class X509Certificate;
@@ -128,6 +133,23 @@ inline bool CSSMOIDEqual(const CSSM_OID* oid1, const CSSM_OID* oid2) {
 }
 #endif
 
+// A list of ASN.1 date/time formats that ParseCertificateDate() supports,
+// encoded in the canonical forms specified in RFC 2459/3280/5280.
+enum CertDateFormat {
+  // UTCTime: Format is YYMMDDHHMMSSZ
+  CERT_DATE_FORMAT_UTC_TIME,
+
+  // GeneralizedTime: Format is YYYYMMDDHHMMSSZ
+  CERT_DATE_FORMAT_GENERALIZED_TIME,
+};
+
+// Attempts to parse |raw_date|, an ASN.1 date/time string encoded as
+// |format|, and writes the result into |*time|. If an invalid date is
+// specified, or if parsing fails, returns false, and |*time| will not be
+// updated.
+bool ParseCertificateDate(const base::StringPiece& raw_date,
+                          CertDateFormat format,
+                          base::Time* time);
 }  // namespace net
 
 #endif  // NET_BASE_X509_CERT_TYPES_H_
