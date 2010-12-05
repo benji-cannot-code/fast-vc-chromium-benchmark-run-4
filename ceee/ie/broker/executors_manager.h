1f7f692a9132707747b8f7359a5819d61eead73c
mad@google.com <mad@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-05 19:07:40 +0000

    Yet another attempt at landing http://src.chromium.org/viewvc/chrome?view=rev&revision=68298 : "Make sure we don't keep tab id - handles mapping when a dread^Wthread dies."
    And hopefully won't hit http://code.google.com/p/chromium/issues/detail?id=63479 this time...
    
    TBR=joi
    TEST=None
    BUG=65153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68319 0039d316-1c4b-4281-b951-d872f2087c98

--- ceee/ie/broker/executors_manager.h
+++ ceee/ie/broker/executors_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lock.h"
 #include "base/singleton.h"
+#include "base/task.h"
 #include "ceee/common/window_utils.h"
 #include "ceee/ie/broker/window_events_funnel.h"
 
@@ -120,6 +121,10 @@ class ExecutorsManager {
 
   // Unregister the HWND and its corresponding tab ID and tool band tab ID.
   virtual void DeleteTabHandle(HWND handle);
+
+  // Cleans up the maps from all handles that would be associated to the given
+  // thread id.
+  virtual void CleanupMapsForThread(DWORD thread_id);
 
   // Traits for Singleton<ExecutorsManager> so that we can pass an argument
   // to the constructor.
