67abcb2fc1462837160683948e8e447bc9646b21
mad@google.com <mad@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-05 18:12:25 +0000

    Relanding of http://src.chromium.org/viewvc/chrome?view=rev&revision=68298...
    Make sure we don't keep tab id - handles mapping when a dread^Wthread dies.
    
    TBR=joi
    BUG=65153
    TEST=Just make sure it builds...
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68315 0039d316-1c4b-4281-b951-d872f2087c98

--- ceee/ie/broker/chrome_postman.cc
+++ ceee/ie/broker/chrome_postman.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ceee/ie/broker/chrome_postman.h"
 
 #include "base/string_util.h"
+#include "ceee/common/com_utils.h"
 #include "ceee/ie/broker/api_dispatcher.h"
 #include "ceee/ie/common/api_registration.h"
 #include "ceee/ie/common/ceee_module_util.h"
 #include "chrome/browser/automation/extension_automation_constants.h"
 #include "chrome/common/url_constants.h"
-#include "ceee/common/com_utils.h"
 
 namespace ext = extension_automation_constants;
 
@@ -118,6 +118,16 @@ void ChromePostman::PostMessage(BSTR message, BSTR target) {
                                               message, target));
   } else {
     LOG(ERROR) << "Trying to post a message before the postman thread is"
+                  "completely initialized and ready.";
+  }
+}
+
+void ChromePostman::QueueApiInvocationThreadTask(Task* task) {
+  MessageLoop* message_loop = api_worker_thread_.message_loop();
+  if (message_loop) {
+    message_loop->PostTask(FROM_HERE, task);
+  } else {
+    LOG(ERROR) << "Trying to queue a task before the API worker thread is"
                   "completely initialized and ready.";
   }
 }
