f65ab7202abd298326e22e50a000fdd8bde16aa1
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-06 23:01:58 +0000

    [gtk] Add fade-commit to instant suggestion for predictive instant.
    BUG=65399
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5588004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68395 0039d316-1c4b-4281-b951-d872f2087c98

--- app/animation.cc
+++ app/animation.cc
@@ -66,7 +66,6 @@ double Animation::CurrentValueBetween(double start, double target) const {
 
 int Animation::CurrentValueBetween(int start, int target) const {
   return Tween::ValueBetween(GetCurrentValue(), start, target);
-
 }
 
 gfx::Rect Animation::CurrentValueBetween(const gfx::Rect& start_bounds,
--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string>
 
+#include "app/animation_delegate.h"
 #include "app/gtk_signal.h"
 #include "app/gtk_signal_registrar.h"
 #include "base/basictypes.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AutocompleteEditController;
 class AutocompleteEditModel;
 class AutocompletePopupView;
+class MultiAnimation;
 class Profile;
 class TabContents;
 
@@ -45,7 +47,8 @@ class GtkThemeProvider;
 #endif
 
 class AutocompleteEditViewGtk : public AutocompleteEditView,
-                                public NotificationObserver {
+                                public NotificationObserver,
+                                public AnimationDelegate {
  public:
   // Modeled like the Windows CHARRANGE.  Represent a pair of cursor position
   // offsets.  Since GtkTextIters are invalid after the buffer is changed, we
@@ -147,7 +150,16 @@ class AutocompleteEditViewGtk : public AutocompleteEditView,
                        const NotificationSource& source,
                        const NotificationDetails& details);
 
+  // Overridden from AnimationDelegate.
+  virtual void AnimationEnded(const Animation* animation);
+  virtual void AnimationProgressed(const Animation* animation);
+  virtual void AnimationCanceled(const Animation* animation);
+
+  // Sets the colors of the text view according to the theme.
   void SetBaseColor();
+  // Sets the colors of the instant suggestion view according to the theme and
+  // the animation state.
+  void UpdateInstantViewColors();
 
   void SetInstantSuggestion(const std::string& suggestion);
   bool CommitInstantSuggestion();
@@ -350,6 +362,9 @@ class AutocompleteEditViewGtk : public AutocompleteEditView,
   // A widget for displaying instant suggestion text. It'll be attached to a
   // child anchor in the |text_buffer_| object.
   GtkWidget* instant_view_;
+  // Animation from instant suggest (faded text) to autocomplete (selected
+  // text).
+  scoped_ptr<MultiAnimation> instant_animation_;
 
   // A mark to split the content and the instant anchor. Wherever the end
   // iterator of the text buffer is required, the iterator to this mark should
