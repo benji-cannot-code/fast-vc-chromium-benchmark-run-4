ead6a6c42c5ae23f7f6a487fcd9bf21c1d3c9bea
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-06 21:21:27 +0000

    Print preview: Display a message when the PDF viewer is missing.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5574003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 var localStrings = new LocalStrings();
+var hasPDFPlugin = true;
 
 /**
  * Window onload handler, sets up the page.
@@ -44,20 +45,30 @@ function onPDFLoad() {
  * Create the PDF plugin or reload the existing one.
  */
 function createPDFPlugin(url) {
+  if (!hasPDFPlugin) {
+    return;
+  }
+
   if ($('pdf-viewer')) {
-    pdfPlugin.reload();
+    $('pdf-viewer').reload();
     return;
   }
 
   var loadingElement = $('loading');
+  loadingElement.classList.add('hidden');
   var mainView = loadingElement.parentNode;
-  mainView.removeChild(loadingElement);
 
-  pdfPlugin = document.createElement('object');
+  var pdfPlugin = document.createElement('object');
   pdfPlugin.setAttribute('id', 'pdf-viewer');
   pdfPlugin.setAttribute('type', 'application/pdf');
   pdfPlugin.setAttribute('src', url);
   mainView.appendChild(pdfPlugin);
+  if (!pdfPlugin.onload) {
+    hasPDFPlugin = false;
+    mainView.removeChild(pdfPlugin);
+    $('no-plugin').classList.remove('hidden');
+    return;
+  }
   pdfPlugin.onload('onPDFLoad()');
 }
 
