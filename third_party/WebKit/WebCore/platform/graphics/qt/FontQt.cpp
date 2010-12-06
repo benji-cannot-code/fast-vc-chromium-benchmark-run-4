97bfdc3f373bf3c2e66f8efc00b0955f794b65c1
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-06 10:24:08 +0000

    [Qt] Report zero width for zero size fonts
    Patch by Robert Hogan <robert@webkit.org> on 2010-12-06
    Reviewed by Kenneth Rohde Christiansen.
    
    Report zero width when font size is zero.
    
    Fixes fast/text/font-size-zero.html
    
    https://bugs.webkit.org/show_bug.cgi?id=50539
    
    * platform/graphics/qt/FontQt.cpp:
    (WebCore::Font::floatWidthForSimpleText):
    (WebCore::Font::floatWidthForComplexText):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/FontQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/FontQt.cpp
@@ -263,6 +263,9 @@ void Font::drawComplexText(GraphicsContext* ctx, const TextRun& run, const Float
 
 float Font::floatWidthForSimpleText(const TextRun& run, GlyphBuffer* glyphBuffer, HashSet<const SimpleFontData*>* fallbackFonts, GlyphOverflow* glyphOverflow) const
 {
+    if (!primaryFont()->platformData().size())
+        return 0;
+
 #if QT_VERSION >= QT_VERSION_CHECK(4, 7, 0)
     if (!run.length())
         return 0;
@@ -279,12 +282,15 @@ float Font::floatWidthForSimpleText(const TextRun& run, GlyphBuffer* glyphBuffer
     return w + run.padding();
 #else
     Q_ASSERT(false);
-    return 0.0f;
+    return 0;
 #endif
 }
 
 float Font::floatWidthForComplexText(const TextRun& run, HashSet<const SimpleFontData*>*, GlyphOverflow*) const
 {
+    if (!primaryFont()->platformData().size())
+        return 0;
+
     if (!run.length())
         return 0;
 
