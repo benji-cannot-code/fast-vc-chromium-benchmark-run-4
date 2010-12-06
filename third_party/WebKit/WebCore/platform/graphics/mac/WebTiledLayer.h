be163ee84d2f35d0d7e0766256709b2f055423f9
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-06 19:35:13 +0000

    2010-12-06  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Simon Fraser.
    
            Share code between Mac (CA) and Windows (CACF) GraphicsLayer implementations
            https://bugs.webkit.org/show_bug.cgi?id=49388
    
            Replaced GraphicsLayerMac with GraphicsLayerCA. GraphicsLayerCA is essentially 
            identical to GraphicsLayerMac, but replaces all the CoreAnimation specific calls 
            with calls to PlatformCALayer and PlatformCAAnimation. This makes GraphicsLayerCA 
            platform neutral as long as implementations of those two classes are available. 
            Added implementations in PlatformCALayerMac and PlatformCAAnimationMac. Also got 
            rid of GraphicsLayer::nativeLayer() and replaced it with GraphicsLayer::platformLayer() 
            to avoid confusion since both names were used to refer to the same thing. 
    
            This patch is only for Mac. Windows implementation has not changed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/mac/WebTiledLayer.h
+++ third_party/WebKit/WebCore/platform/graphics/mac/WebTiledLayer.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface WebTiledLayer : CATiledLayer 
 {
-    WebCore::GraphicsLayer* m_layerOwner;
 }
 
 // implements WebLayerAdditions
