eeab6726577948f70b9eb1c95b7b0f5845b65e8e
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-07 17:35:45 +0000

    Revert 68482 - Make webkit/glue/plugins no longer depend on ppapi/proxy directly. This causes things that use webkit but otherwise don't need IPC to include the IPC directory.
    This patch moves the set-up of the proxy into the renderer. I also did a lot
    of clean-up of the initialization and it seems much nicer now.
    
    BUG=63684
    TEST=manual PPAPI proxy testing
    Review URL: http://codereview.chromium.org/5592005
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/5616004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68486 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_renderer.gypi
+++ chrome/chrome_renderer.gypi
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'chrome_resources',
         'chrome_strings',
         'safe_browsing_proto',
-        '../ppapi/ppapi.gyp:ppapi_proxy',
         '../printing/printing.gyp:printing',
         '../skia/skia.gyp:skia',
         '../third_party/hunspell/hunspell.gyp:hunspell',
--- chrome/renderer/pepper_plugin_delegate_impl.cc
+++ chrome/renderer/pepper_plugin_delegate_impl.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/renderer/pepper_plugin_delegate_impl.h"
-
 #include <cmath>
+
+#include "chrome/renderer/pepper_plugin_delegate_impl.h"
 
 #include "app/l10n_util.h"
 #include "app/surface/transport_dib.h"
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/common/child_thread.h"
 #include "chrome/common/file_system/file_system_dispatcher.h"
-#include "chrome/common/pepper_plugin_registry.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/common/render_messages_params.h"
 #include "chrome/renderer/audio_message_filter.h"
@@ -34,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/locale_settings.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ppapi/c/dev/pp_video_dev.h"
-#include "ppapi/proxy/host_dispatcher.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileChooserCompletion.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebFileChooserParams.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebPluginContainer.h"
@@ -48,10 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include "chrome/common/render_messages.h"
 #include "chrome/renderer/render_thread.h"
-#endif
-
-#if defined(OS_POSIX)
-#include "ipc/ipc_channel_posix.h"
 #endif
 
 using WebKit::WebView;
@@ -420,60 +414,6 @@ class PlatformVideoDecoderImpl
   DISALLOW_COPY_AND_ASSIGN(PlatformVideoDecoderImpl);
 };
 
-class DispatcherWrapper : public pepper::PluginDelegate::OutOfProcessProxy {
- public:
-  DispatcherWrapper() {}
-  virtual ~DispatcherWrapper() {}
-
-  bool Init(base::ProcessHandle plugin_process_handle,
-            IPC::ChannelHandle channel_handle,
-            PP_Module pp_module,
-            pp::proxy::Dispatcher::GetInterfaceFunc local_get_interface);
-
-  // OutOfProcessProxy implementation.
-  virtual const void* GetProxiedInterface(const char* name) {
-    return dispatcher_->GetProxiedInterface(name);
-  }
-  virtual void AddInstance(PP_Instance instance) {
-    pp::proxy::HostDispatcher::SetForInstance(instance, dispatcher_.get());
-  }
-  virtual void RemoveInstance(PP_Instance instance) {
-    pp::proxy::HostDispatcher::RemoveForInstance(instance);
-  }
-
- private:
-  scoped_ptr<pp::proxy::HostDispatcher> dispatcher_;
-};
-
-bool DispatcherWrapper::Init(
-    base::ProcessHandle plugin_process_handle,
-    IPC::ChannelHandle channel_handle,
-    PP_Module pp_module,
-    pp::proxy::Dispatcher::GetInterfaceFunc local_get_interface) {
-  dispatcher_.reset(new pp::proxy::HostDispatcher(
-      plugin_process_handle, pp_module, local_get_interface));
-
-#if defined(OS_POSIX)
-  // If we received a ChannelHandle, register it now.
-  if (channel_handle.socket.fd >= 0)
-    IPC::AddChannelSocket(channel_handle.name, channel_handle.socket.fd);
-#endif
-
-  if (!dispatcher_->InitWithChannel(
-          ChildProcess::current()->io_message_loop(), channel_handle.name,
-          true, ChildProcess::current()->GetShutDownEvent())) {
-    dispatcher_.reset();
-    return false;
-  }
-
-  if (!dispatcher_->InitializeModule()) {
-    // TODO(brettw) does the module get unloaded in this case?
-    dispatcher_.reset();
-    return false;
-  }
-  return true;
-}
-
 }  // namespace
 
 PepperPluginDelegateImpl::PepperPluginDelegateImpl(RenderView* render_view)
@@ -485,29 +425,19 @@ PepperPluginDelegateImpl::~PepperPluginDelegateImpl() {
 }
 
 scoped_refptr<pepper::PluginModule>
-PepperPluginDelegateImpl::CreatePepperPlugin(const FilePath& path) {
-  // Easy case is in-process plugins.
-  if (!PepperPluginRegistry::GetInstance()->RunOutOfProcessForPlugin(path))
-    return PepperPluginRegistry::GetInstance()->GetModule(path);
-
-  // Out of process: have the browser start the plugin process for us.
+PepperPluginDelegateImpl::CreateOutOfProcessPepperPlugin(
+    const FilePath& path) {
   base::ProcessHandle plugin_process_handle = NULL;
   IPC::ChannelHandle channel_handle;
   render_view_->Send(new ViewHostMsg_OpenChannelToPepperPlugin(
       path, &plugin_process_handle, &channel_handle));
   if (channel_handle.name.empty())
     return scoped_refptr<pepper::PluginModule>();  // Couldn't be initialized.
-
-  // Create a new HostDispatcher for the proxying, and hook it to a new
-  // PluginModule.
-  scoped_refptr<pepper::PluginModule> module(new pepper::PluginModule);
-  scoped_ptr<DispatcherWrapper> dispatcher(new DispatcherWrapper);
-  if (!dispatcher->Init(plugin_process_handle, channel_handle,
-                        module->pp_module(),
-                        pepper::PluginModule::GetLocalGetInterfaceFunc()))
-    return scoped_refptr<pepper::PluginModule>();
-  module->InitAsProxied(dispatcher.release());
-  return module;
+  return pepper::PluginModule::CreateOutOfProcessModule(
+      ChildProcess::current()->io_message_loop(),
+      plugin_process_handle,
+      channel_handle,
+      ChildProcess::current()->GetShutDownEvent());
 }
 
 void PepperPluginDelegateImpl::ViewInitiatedPaint() {
