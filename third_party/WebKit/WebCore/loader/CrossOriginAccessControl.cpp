03696f62f4aad10cec95b8416b1371813a7abd09
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-07 16:40:31 +0000

    Another build fix.
    * loader/CrossOriginAccessControl.cpp: Added a missing include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/loader/CrossOriginAccessControl.cpp
+++ third_party/WebKit/WebCore/loader/CrossOriginAccessControl.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTTPParsers.h"
 #include "ResourceResponse.h"
 #include "SecurityOrigin.h"
+#include <wtf/HashSet.h>
 #include <wtf/Threading.h>
 #include <wtf/text/AtomicString.h>
 
