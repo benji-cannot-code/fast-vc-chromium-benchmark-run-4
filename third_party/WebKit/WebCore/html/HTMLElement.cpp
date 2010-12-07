288a9d08006f5d578227725a4f37151cfe3d1c9b
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-07 22:01:58 +0000

    2010-12-07  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Kent Tamura.
    
            Implement HTML5 hidden attribute
            https://bugs.webkit.org/show_bug.cgi?id=40511
    
            * fast/forms/caret-rtl.html: Fixed a non-closed comment which added an "hidden" attribute.
            * fast/html/hidden-attr-expected.txt: Added.
            * fast/html/hidden-attr.html: Added. Test static and dynamic cases of hidden attribute.
    2010-12-07  Maciej Stachowiak  <mjs@apple.com>
    
            Reviewed by Kent Tamura.
    
            Implement HTML5 hidden attribute
            https://bugs.webkit.org/show_bug.cgi?id=40511
    
            Test: fast/html/hidden-attr.html
    
            * html/HTMLElement.cpp:
            (WebCore::HTMLElement::mapToEntry): Pick up hidden as a global mapped attribute.
            (WebCore::HTMLElement::parseMappedAttribute): Map hidden attribute to display: none.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/HTMLElement.cpp
+++ third_party/WebKit/WebCore/html/HTMLElement.cpp
@@ -117,8 +117,9 @@ bool HTMLElement::ieForbidsInsertHTML() const
 
 bool HTMLElement::mapToEntry(const QualifiedName& attrName, MappedAttributeEntry& result) const
 {
-    if (attrName == alignAttr ||
-        attrName == contenteditableAttr) {
+    if (attrName == alignAttr
+        || attrName == contenteditableAttr
+        || attrName == hiddenAttr) {
         result = eUniversal;
         return false;
     }
@@ -143,6 +144,8 @@ void HTMLElement::parseMappedAttribute(Attribute* attr)
             addCSSProperty(attr, CSSPropertyTextAlign, attr->value());
     } else if (attr->name() == contenteditableAttr) {
         setContentEditable(attr);
+    } else if (attr->name() == hiddenAttr) {
+        addCSSProperty(attr, CSSPropertyDisplay, CSSValueNone);
     } else if (attr->name() == tabindexAttr) {
         indexstring = getAttribute(tabindexAttr);
         int tabindex = 0;
