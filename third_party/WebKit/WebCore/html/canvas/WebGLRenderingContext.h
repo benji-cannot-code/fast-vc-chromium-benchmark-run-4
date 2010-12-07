cdceda777221842ddedab365118ccc2b367fdca5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-07 04:24:25 +0000

    2010-12-06  Alexey Marinichev  <amarinichev@chromium.org>
            Reviewed by Kenneth Russell.
    
            Throw webglcontextlost and webglcontextrestored events when a WebGL context is lost and restored.
            https://bugs.webkit.org/show_bug.cgi?id=50364
    
            No new tests since there is currently no way to force a context lost event.
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::WebGLRenderingContextRestoreTimer::fired):
            (WebCore::WebGLRenderingContext::create):
            (WebCore::WebGLRenderingContext::WebGLRenderingContext):
            (WebCore::WebGLRenderingContext::isContextLost):
            (WebCore::WebGLRenderingContext::loseContext):
            (WebCore::WebGLRenderingContext::restoreContext):
            * html/canvas/WebGLRenderingContext.h:
            (WebCore::WebGLRenderingContext::WebGLRenderingContextRestoreTimer::WebGLRenderingContextRestoreTimer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.h
+++ third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext3D.h"
 #include "Int32Array.h"
 #include "PlatformString.h"
+#include "Timer.h"
 #include "Uint8Array.h"
 #include "WebGLGetInfo.h"
 
@@ -176,7 +177,7 @@ public:
 
     void hint(unsigned long target, unsigned long mode);
     bool isBuffer(WebGLBuffer*);
-    bool isContextLost() const;
+    bool isContextLost();
     bool isEnabled(unsigned long cap);
     bool isFramebuffer(WebGLFramebuffer*);
     bool isProgram(WebGLProgram*);
@@ -295,7 +296,7 @@ public:
   private:
     friend class WebGLObject;
 
-    WebGLRenderingContext(HTMLCanvasElement*, PassRefPtr<GraphicsContext3D>);
+    WebGLRenderingContext(HTMLCanvasElement*, PassRefPtr<GraphicsContext3D>, GraphicsContext3D::Attributes);
     void initializeNewContext();
     void setupFlags();
 
@@ -344,6 +345,17 @@ public:
     PassRefPtr<Image> videoFrameToImage(HTMLVideoElement* video);
 
     RefPtr<GraphicsContext3D> m_context;
+
+    class WebGLRenderingContextRestoreTimer : public TimerBase {
+    public:
+        WebGLRenderingContextRestoreTimer(WebGLRenderingContext* context) : m_context(context) { }
+    private:
+        virtual void fired();
+        WebGLRenderingContext* m_context;
+    };
+
+    WebGLRenderingContextRestoreTimer m_restoreTimer;
+
     bool m_needsUpdate;
     bool m_markedCanvasDirty;
     HashSet<RefPtr<WebGLObject> > m_canvasObjects;
@@ -432,6 +444,7 @@ public:
     bool m_unpackPremultiplyAlpha;
     unsigned long m_unpackColorspaceConversion;
     bool m_contextLost;
+    GraphicsContext3D::Attributes m_attributes;
 
     long m_stencilBits;
     unsigned long m_stencilMask;
