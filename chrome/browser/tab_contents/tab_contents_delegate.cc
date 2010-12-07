a46f7ad5a158e2cb07c83ee8095cf9f08ecc4bf7
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-07 17:47:04 +0000

    Adds the ability for the browser to specify additional headers to use.
    BUG=59429
    TEST=none
    
    Review URL: http://codereview.chromium.org/5574004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_delegate.cc
+++ chrome/browser/tab_contents/tab_contents_delegate.cc
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url.h"
 #include "gfx/rect.h"
 
+std::string TabContentsDelegate::GetNavigationHeaders(const GURL& url) {
+  return std::string();
+}
+
 void TabContentsDelegate::DetachContents(TabContents* source) {
 }
 
--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -107,6 +107,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/data_url.h"
 #include "net/base/escape.h"
 #include "net/base/net_errors.h"
+#include "net/http/http_util.h"
 #include "skia/ext/bitmap_platform_device.h"
 #include "skia/ext/image_operations.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebAccessibilityCache.h"
@@ -1414,6 +1415,15 @@ void RenderView::OnNavigate(const ViewMsg_Navigate_Params& params) {
     if (params.referrer.is_valid()) {
       request.setHTTPHeaderField(WebString::fromUTF8("Referer"),
                                  WebString::fromUTF8(params.referrer.spec()));
+    }
+
+    if (!params.extra_headers.empty()) {
+      for (net::HttpUtil::HeadersIterator i(params.extra_headers.begin(),
+                                            params.extra_headers.end(), "\n");
+           i.GetNext(); ) {
+        request.addHTTPHeaderField(WebString::fromUTF8(i.name()),
+                                   WebString::fromUTF8(i.values()));
+      }
     }
 
     if (navigation_state)
