60c07f2eeb867f919ff2437a93531928c20e5302
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-08 20:43:43 +0000

    2010-12-08  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Simon Fraser.
    
            Share code between Mac (CA) and Windows (CACF) GraphicsLayer implementations
            https://bugs.webkit.org/show_bug.cgi?id=49388
    
            Added copy constructors and casting operators to go between 
            TransformationMatrix and CATransform3D. Also added a copy
            constructor to go from CGAffineTransform to TransformationMatrix.
            Used these new methods to clean up platform CA code. This will
            make it easier to port to Windows.
    
            I also fixed a couple of build issues found when trying out a 
            Windows build.
    
            This is a resubmission of changesets r73477 and r73483 with a 
            fix for the SL build. The failure was because TransformationMatrix
            included QuartzCore/CATransform3D.h which brought in some system
            libraries, including Quickdraw, which has a definition for
            'Cursor' which clashed with WebCore's 'Cursor' class. So I had
            to qualify its use in WebChromeClient.cpp in WebKit2.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ca/TransformationMatrixCA.cpp
+++ third_party/WebKit/WebCore/platform/graphics/ca/TransformationMatrixCA.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006, 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2010 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
@@ -25,46 +25,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "AffineTransform.h"
+
+#if PLATFORM(CA)
+
 #include "TransformationMatrix.h"
 
-#if PLATFORM(CG)
-
-#include <CoreGraphics/CGAffineTransform.h>
 #include "FloatConversion.h"
 
 namespace WebCore {
 
-TransformationMatrix::TransformationMatrix(const CGAffineTransform& t)
+TransformationMatrix::TransformationMatrix(const CATransform3D& t)
 {
-    setA(t.a);
-    setB(t.b);
-    setC(t.c);
-    setD(t.d);
-    setE(t.tx);
-    setF(t.ty);
+    setMatrix(
+        t.m11, t.m12, t.m13, t.m14,
+        t.m21, t.m22, t.m23, t.m24, 
+        t.m31, t.m32, t.m33, t.m34, 
+        t.m41, t.m42, t.m43, t.m44);
 }
 
-TransformationMatrix::operator CGAffineTransform() const
+TransformationMatrix::operator CATransform3D() const
 {
-    return CGAffineTransformMake(narrowPrecisionToCGFloat(a()),
-                                 narrowPrecisionToCGFloat(b()),
-                                 narrowPrecisionToCGFloat(c()),
-                                 narrowPrecisionToCGFloat(d()),
-                                 narrowPrecisionToCGFloat(e()),
-                                 narrowPrecisionToCGFloat(f()));
+    CATransform3D toT3D;
+    toT3D.m11 = narrowPrecisionToFloat(m11());
+    toT3D.m12 = narrowPrecisionToFloat(m12());
+    toT3D.m13 = narrowPrecisionToFloat(m13());
+    toT3D.m14 = narrowPrecisionToFloat(m14());
+    toT3D.m21 = narrowPrecisionToFloat(m21());
+    toT3D.m22 = narrowPrecisionToFloat(m22());
+    toT3D.m23 = narrowPrecisionToFloat(m23());
+    toT3D.m24 = narrowPrecisionToFloat(m24());
+    toT3D.m31 = narrowPrecisionToFloat(m31());
+    toT3D.m32 = narrowPrecisionToFloat(m32());
+    toT3D.m33 = narrowPrecisionToFloat(m33());
+    toT3D.m34 = narrowPrecisionToFloat(m34());
+    toT3D.m41 = narrowPrecisionToFloat(m41());
+    toT3D.m42 = narrowPrecisionToFloat(m42());
+    toT3D.m43 = narrowPrecisionToFloat(m43());
+    toT3D.m44 = narrowPrecisionToFloat(m44());
+    return toT3D;
 }
 
-AffineTransform::operator CGAffineTransform() const
-{
-    return CGAffineTransformMake(narrowPrecisionToCGFloat(a()),
-                                 narrowPrecisionToCGFloat(b()),
-                                 narrowPrecisionToCGFloat(c()),
-                                 narrowPrecisionToCGFloat(d()),
-                                 narrowPrecisionToCGFloat(e()),
-                                 narrowPrecisionToCGFloat(f()));
 }
 
-}
-
-#endif // PLATFORM(CG)
+#endif // PLATFORM(CA)
