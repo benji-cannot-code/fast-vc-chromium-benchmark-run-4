3bda67d56ef9308f95c123dc4212471eccb09cb0
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 20:07:48 +0000

    Fix pyauto tests launcher on chromeos.
    Review URL: http://codereview.chromium.org/5560011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_PyAutoFunctionalTests/desktopui_PyAutoFunctionalTests.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_PyAutoFunctionalTests/desktopui_PyAutoFunctionalTests.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
 from autotest_lib.client.bin import site_chrome_test
 
 class desktopui_PyAutoFunctionalTests(site_chrome_test.ChromeTestBase):
@@ -10,6 +11,8 @@ class desktopui_PyAutoFunctionalTests(site_chrome_test.ChromeTestBase):
     version = 1
 
     def run_once(self):
+        deps_dir = os.path.join(self.autodir, 'deps')
+        self.test_binary_dir = ''
         pyauto_script = '%s/test_src/chrome/test/functional/' \
-                        'pyauto_functional.py' % self.dep_dir
+                        'pyauto_functional.py' % deps_dir
         self.run_chrome_test(pyauto_script)
