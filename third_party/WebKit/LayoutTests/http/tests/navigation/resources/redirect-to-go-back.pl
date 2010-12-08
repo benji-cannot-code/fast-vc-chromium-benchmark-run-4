10e9f74543b0d706123d793392e64d4ddc71129c
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-08 02:32:41 +0000

    2010-11-29  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by Adam Barth.
    
            returning to a POST result within a frame does a GET instead of a POST
            https://bugs.webkit.org/show_bug.cgi?id=10199
    
            Re-enable postredirect-frames-goback1.html and post-frames-goback1.html
            now that the big that made them be checked in disabled was fixed.
    
            The tests were also re-written to not be pixel tests, since that just
            adds extra per-platform baseline pain.
    
            Add fast/history/history-back-initial-vs-final-url.html since there did
            not appear to be any tests for whether we used the initial or final URL
            of a frame's history item (and this patch changes it from the former to
            the latter).
    
            * fast/history/history-back-initial-vs-final-url-expected.txt: Added.
            * fast/history/history-back-initial-vs-final-url.html: Added.
            * fast/history/resources/frame-final-url.html: Added.
            * fast/history/resources/frame-initial-url.html: Added.
            * http/tests/navigation/post-frames-goback1-expected.txt: Added.
            * http/tests/navigation/post-frames-goback1.html: Added.
            * http/tests/navigation/post-frames-goback1.html-disabled: Removed.
            * http/tests/navigation/post-frames.html:
            * http/tests/navigation/postredirect-frames-goback1-expected.txt: Added.
            * http/tests/navigation/postredirect-frames-goback1.html: Added.
            * http/tests/navigation/postredirect-frames-goback1.html-disabled: Removed.
            * http/tests/navigation/resources/form-target.pl:
            * http/tests/navigation/resources/redirect-to-go-back.pl: Added.
            * http/tests/navigation/resources/top-go-back.html: Added.
    2010-11-29  Mihai Parparita  <mihaip@chromium.org>
    
            Reviewed by Adam Barth.
    
            returning to a POST result within a frame does a GET instead of a POST
            https://bugs.webkit.org/show_bug.cgi?id=10199
    
            FrameLoader::loadURLIntoChildFrame had a check for whether a frame
            had a HistoryItem, and if so used that item's URL. However, that did
            not handle the case where the HistoryItem had FormData and thus was the
            result of a POST request. FrameLoader::navigateToDifferentDocument (now
            renamed to loadDifferentDocumentItem) does all that, so just switch to
            using it instead.
    
            Besides the bug fix, the only difference in behavior is that we use the
            HistoryItem's URL instead of original URL when restoring it in a child
            frame. This seems desirable too (for consistency with other browsers, and
            so that we can avoid form double-submits for forms that that do
            POST-then-redirect), and is tested by
            fast/history/history-back-initial-vs-final-url.html.
    
            Tests: fast/history/history-back-initial-vs-final-url.html
                   http/tests/navigation/post-frames-goback1.html
                   http/tests/navigation/postredirect-frames-goback1.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::loadURLIntoChildFrame): Switch to using
                loadDifferentDocumentItem. Also cleaned up ordering slightly (if
                on the archive path, there's no need to do any of the other checks).
            (WebCore::FrameLoader::loadDifferentDocumentItem): Renamed from
                navigateToDifferentDocument so that calling it from loadItem and
                loadURLIntoChildFrame flows more easily.
            (WebCore::FrameLoader::loadSameDocumentItem): Similarly, renamed from
                navigateWithinDocument.
            (WebCore::FrameLoader::loadItem): Update callsites to the above methods.
            * loader/FrameLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/redirect-to-go-back.pl
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl
+# Simple script to generate a 302 HTTP redirect to a page that triggers a 
+# navigation back (only if POST is used).
+
+use CGI;
+$query = new CGI;
+
+$method = $query->request_method();
+
+if ($method eq "POST") {
+    print "Status: 302 Moved Temporarily\r\n";
+    print "Location: top-go-back.html\r\n";
+    print "\r\n";
+} else {
+    print "Status: 405 Method not allowed\r\n";
+    print "Content-type: text/plain\r\n";
+    print "\r\n";
+    print "This should only be requested via POST ($method was used)."
+}
--- third_party/WebKit/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/WebCore/loader/FrameLoader.h
@@ -337,8 +337,8 @@ public:
 private:
     void checkTimerFired(Timer<FrameLoader>*);
     
-    void navigateWithinDocument(HistoryItem*);
-    void navigateToDifferentDocument(HistoryItem*, FrameLoadType);
+    void loadSameDocumentItem(HistoryItem*);
+    void loadDifferentDocumentItem(HistoryItem*, FrameLoadType);
     
     void loadProvisionalItemFromCachedPage();
 
