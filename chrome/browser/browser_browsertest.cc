89d7964407551329613775d1b39d98ce631662b8
bryeung@google.com <bryeung@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 14:57:19 +0000

    Propagate is_editable_node in the FOCUS_CHANGED_IN_PAGE notification.
    Also fixes up types and a bug in WindowedNotificationObserverWithDetails.
    
    BUG=none
    TEST=updated BrowserFocusTest.FocusTraversal
    
    Review URL: http://codereview.chromium.org/5516005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -514,7 +514,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_PageLanguageDetection) {
   EXPECT_TRUE(current_tab->language_state().original_language().empty());
   en_language_detected_signal.Wait();
   std::string lang;
-  EXPECT_TRUE(en_language_detected_signal.GetDetailsFor(current_tab, &lang));
+  EXPECT_TRUE(en_language_detected_signal.GetDetailsFor(
+        source.map_key(), &lang));
   EXPECT_EQ("en", lang);
   EXPECT_EQ("en", current_tab->language_state().original_language());
 
@@ -527,7 +528,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_PageLanguageDetection) {
   EXPECT_TRUE(current_tab->language_state().original_language().empty());
   fr_language_detected_signal.Wait();
   lang.clear();
-  EXPECT_TRUE(fr_language_detected_signal.GetDetailsFor(current_tab, &lang));
+  EXPECT_TRUE(fr_language_detected_signal.GetDetailsFor(
+        source.map_key(), &lang));
   EXPECT_EQ("fr", lang);
   EXPECT_EQ("fr", current_tab->language_state().original_language());
 }
