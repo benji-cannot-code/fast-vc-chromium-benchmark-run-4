a277213c6b9fcf5265fdf85c4279d9572225c973
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 22:15:10 +0000

    Fix a typo in UrlmonUrlRequest to clear the map during shutdown
    In UrlmonUrlRequestManager::StopAll we meant to clear the map but
    used map.empty() instead of map.clear().
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/5656004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68649 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_url_request.cc
+++ chrome_frame/urlmon_url_request.cc
@@ -1148,7 +1148,7 @@ void UrlmonUrlRequestManager::StopAll() {
     it->second->Stop();
   }
 
-  request_map_.empty();
+  request_map_.clear();
 }
 
 void UrlmonUrlRequestManager::OnResponseStarted(int request_id,
