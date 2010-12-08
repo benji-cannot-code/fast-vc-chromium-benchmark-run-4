ca991393139591d96adbb0fe268bc2d34bf7b9b9
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 18:33:49 +0000

    Fix cpplint.py nits.
    R=joth
    BUG=none
    TEST=no compilation errors.
    Review URL: http://codereview.chromium.org/5511015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68615 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_openssl.h
+++ net/socket/ssl_client_socket_openssl.h
@@ -55,7 +55,7 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
   virtual void Disconnect();
   virtual bool IsConnected() const;
   virtual bool IsConnectedAndIdle() const;
-  virtual int GetPeerAddress(AddressList*) const;
+  virtual int GetPeerAddress(AddressList* address) const;
   virtual const BoundNetLog& NetLog() const;
   virtual void SetSubresourceSpeculation();
   virtual void SetOmniboxSpeculation();
@@ -152,5 +152,5 @@ class SSLClientSocketOpenSSL : public SSLClientSocket {
 
 }  // namespace net
 
-#endif // NET_SOCKET_SSL_CLIENT_SOCKET_OPENSSL_H_
+#endif  // NET_SOCKET_SSL_CLIENT_SOCKET_OPENSSL_H_
 
