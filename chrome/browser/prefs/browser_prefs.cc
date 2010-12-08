21f057b60fc52f92f7b5e9b7f964ded65ebd3279
gfeher@chromium.org <gfeher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 13:46:38 +0000

    Fix handling of 'Use system proxy settings' policy
    The problem is that using system proxy settings is triggered when the
    internal proxy prefs of Chrome are set to their default values.
    Unfortunately it is possible that these prefs are set to their defaults but
    they are not controlled by the default pref store. This CL fixes this by
    checking for the default values and not only for the origin of prefs. A longer term solution is described in
    http://crbug.com/65732
    
    BUG=65736
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5664001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/instant/instant_controller.h"
 #include "chrome/browser/metrics/metrics_log.h"
 #include "chrome/browser/metrics/metrics_service.h"
-#include "chrome/browser/net/chrome_url_request_context.h"
 #include "chrome/browser/net/predictor_api.h"
+#include "chrome/browser/net/pref_proxy_config_service.h"
 #include "chrome/browser/net/net_pref_observer.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/page_info_model.h"
@@ -136,7 +136,7 @@ void RegisterUserPrefs(PrefService* user_prefs) {
   GeolocationContentSettingsMap::RegisterUserPrefs(user_prefs);
   TranslatePrefs::RegisterUserPrefs(user_prefs);
   DesktopNotificationService::RegisterUserPrefs(user_prefs);
-  ChromeURLRequestContextGetter::RegisterUserPrefs(user_prefs);
+  PrefProxyConfigService::RegisterUserPrefs(user_prefs);
 #if defined(TOOLKIT_VIEWS)
   BrowserActionsContainer::RegisterUserPrefs(user_prefs);
 #elif defined(TOOLKIT_GTK)
