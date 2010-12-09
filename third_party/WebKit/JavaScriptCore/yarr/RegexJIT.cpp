4942e95ea595a021d677dc657217b1498ab4b0e2
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-09 17:14:47 +0000

    2010-06-28  Michael Saboff  <msaboff@apple.com>
            Reviewed by Darin Adler.
    
            Bug 41238 - RegExp performance slow on Dromaeo benchmark
    
            Other javascript engines appear to cache prior results of regular 
            expression operations.
    
            Suggest adding some sort of caching mechanism to regular expression 
            processing.
    
            Added a single entry cache of match() results to RegExp class.
    
            Also added performance improvements to UString == operator.
            First check the impls for equality.  Then get the length of
            each of the non-null impls.  Next check the sizes for equality.
            Then check the data for the case of different impls that point 
            to the same data (most likely due to substrings from the beginning of
            another string).  Lastly we check the underlying data for equality.
    
            Refactored Vector<>::operator=() method into a separate copy() method 
            that it calls.  The copy() method is explicitly used for deep copying.
    
            * runtime/RegExp.cpp:
            (JSC::RegExp::RegExp):
            (JSC::RegExp::match):
            * runtime/RegExp.h:
            * runtime/UString.h:
            (JSC::operator==):
            * wtf/Vector.h:
            (WTF::::operator):
            (WTF::::copy):
    
    2010-12-09  Michael Saboff  <msaboff@apple.com>
    
            Reviewed by Darin Adler.
    
            Added test to verify proper operation of backtrack labels.
            https://bugs.webkit.org/show_bug.cgi?id=50579
    
            * fast/regex/parentheses-expected.txt:
            * fast/regex/script-tests/parentheses.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/yarr/RegexJIT.cpp
+++ third_party/WebKit/JavaScriptCore/yarr/RegexJIT.cpp
@@ -562,7 +562,8 @@ class RegexGenerator : private MacroAssembler {
         
         void setBacktrackToLabel(Label* backtrackToLabel)
         {
-            m_backtrackToLabel = backtrackToLabel;
+            if (!m_backtrackToLabel)
+                m_backtrackToLabel = backtrackToLabel;
         }
         
         void setBacktrackJumpList(JumpList* jumpList)
