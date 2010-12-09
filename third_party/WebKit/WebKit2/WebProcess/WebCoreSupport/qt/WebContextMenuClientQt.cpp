84cf7110f403ff794083e4882c085753b5335b6b
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-09 22:30:54 +0000

    Qt build fix.
    * WebProcess/WebCoreSupport/qt/WebContextMenuClientQt.cpp: Include "NotImplemented.h".
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/qt/WebContextMenuClientQt.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/qt/WebContextMenuClientQt.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebContextMenuClient.h"
 
+#include "NotImplemented.h"
+
 using namespace WebCore;
 
 namespace WebKit {
