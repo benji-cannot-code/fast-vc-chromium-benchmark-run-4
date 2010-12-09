72ac295c658ecfb04d97e260370154b1f108a80e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-09 00:03:58 +0000

    Fix chromeos build errors
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/devtools_messages.h
+++ chrome/common/devtools_messages.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <map>
+#include <string>
 
 typedef std::map<std::string, std::string> DevToolsRuntimeProperties;
 
