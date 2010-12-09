11e8476512cff5b8caa71231b3acd4efe34213d5
suzhe@chromium.org <suzhe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-09 20:18:24 +0000

    Checks whether the autocomplete controller can accept keywords before the composition text of input methods sets in the omnibox. This will prevent committing the current composition text incorrectly.
    BUG=61204
    TEST=Enter the keyword of an extension and type space, and inserting text with an input method, then the composition text should not be committed.
    
    Review URL: http://codereview.chromium.org/4103011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_editor.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_editor.mm
@@ -265,6 +265,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setMarkedText:(id)aString selectedRange:(NSRange)selRange {
+  if (![self hasMarkedText]) {
+    // Before input methods set composition text in the omnibox, we need to
+    // examine whether the autocompletion controller accepts the keyword to
+    // avoid committing the current composition text wrongly.
+    AutocompleteTextFieldObserver* observer = [self observer];
+    if (observer)
+      observer->OnStartingIME();
+  }
+
   [super setMarkedText:aString selectedRange:selRange];
 
   // Because the AutocompleteEditViewMac class treats marked text as content,
