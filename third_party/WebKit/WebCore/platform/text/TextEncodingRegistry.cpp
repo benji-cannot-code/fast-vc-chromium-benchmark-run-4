abc42fdfb85f22fa8639616b62a75e4f438e5c75
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-09 00:54:23 +0000

    Yensign hack should work with Shift_JIS and ISO-2022-JP encodings. https://bugs.webkit.org/show_bug.cgi?id=49714
    Reviewed by Alexey Proskuryakov.
    
    WebCore:
    
    IE chooses a font which shows a yensign for 0x5c code point for a page
    encoded in x-mac-japanese, ISO-2022-JP, EUC-JP, Shift_JIS, Shift_JIS_X0213-2000,
    x-sjis, and Windows-31J.
    We have emulated this behavior by replacing 0x5c with 0xa5 for EUC-JP and
    Shift_JIS_X0213-2000. This change adds other encodings above.
    
    Also, we move the HashSet initialization for isJapanese() and
    backslashAsCurrencySymbol() to TextEncodingRegistry.cpp because of
    ease of making them multi-thread safe.
    
    * platform/text/TextEncoding.cpp:
    (WebCore::TextEncoding::isJapanese): Just calls isJapaneseEncoding().
    (WebCore::TextEncoding::backslashAsCurrencySymbol): Uses shouldShowBackslashAsCurrencySymbolIn().
    * platform/text/TextEncodingRegistry.cpp:
    (WebCore::addEncodingName): Moved from TextEncoding.cpp, and stop using atomicCanonicalTextEncodingName().
    (WebCore::buildQuirksSets): Added. Initializes HashSets for isJapaneseEncoding() and shouldShowBackslashAsCurrencySymbolIn().
    (WebCore::isJapaneseEncoding):
    (WebCore::shouldShowBackslashAsCurrencySymbolIn):
    (WebCore::extendTextCodecMaps): Add a call to buildQuirksSets().
    * platform/text/TextEncodingRegistry.h:
    
    LayoutTests:
    
    Use Shift_JIS instead of Shift_JIS_X0213-2000 because Shift_JIS_X0213-2000
    encoding is available only on Mac.
    Add a test for ISO-2022-JP.
    
    * editing/selection/find-yensign-and-backslash-expected.txt:
    * editing/selection/find-yensign-and-backslash.html:
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/text/TextEncodingRegistry.cpp
+++ third_party/WebKit/WebCore/platform/text/TextEncodingRegistry.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/HashFunctions.h>
 #include <wtf/HashMap.h>
+#include <wtf/HashSet.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/StringExtras.h>
 #include <wtf/Threading.h>
@@ -126,6 +127,8 @@ static Mutex& encodingRegistryMutex()
 static TextEncodingNameMap* textEncodingNameMap;
 static TextCodecMap* textCodecMap;
 static bool didExtendTextCodecMaps;
+static HashSet<const char*>* japaneseEncodings;
+static HashSet<const char*>* nonBackslashEncodings;
 
 static const char* const textEncodingNameBlacklist[] = {
     "UTF-7"
@@ -250,6 +253,59 @@ static void buildBaseTextCodecMaps()
 #endif
 }
 
+static void addEncodingName(HashSet<const char*>* set, const char* name)
+{
+    // We must not use atomicCanonicalTextEncodingName() because this function is called in it.
+    const char* atomicName = textEncodingNameMap->get(name);
+    if (atomicName)
+        set->add(atomicName);
+}
+
+static void buildQuirksSets()
+{
+    // FIXME: Having isJapaneseEncoding() and shouldShowBackslashAsCurrencySymbolIn()
+    // and initializing the sets for them in TextEncodingRegistry.cpp look strange.
+
+    ASSERT(!japaneseEncodings);
+    ASSERT(!nonBackslashEncodings);
+
+    japaneseEncodings = new HashSet<const char*>();
+    addEncodingName(japaneseEncodings, "EUC-JP");
+    addEncodingName(japaneseEncodings, "ISO-2022-JP");
+    addEncodingName(japaneseEncodings, "ISO-2022-JP-1");
+    addEncodingName(japaneseEncodings, "ISO-2022-JP-2");
+    addEncodingName(japaneseEncodings, "ISO-2022-JP-3");
+    addEncodingName(japaneseEncodings, "JIS_C6226-1978");
+    addEncodingName(japaneseEncodings, "JIS_X0201");
+    addEncodingName(japaneseEncodings, "JIS_X0208-1983");
+    addEncodingName(japaneseEncodings, "JIS_X0208-1990");
+    addEncodingName(japaneseEncodings, "JIS_X0212-1990");
+    addEncodingName(japaneseEncodings, "Shift_JIS");
+    addEncodingName(japaneseEncodings, "Shift_JIS_X0213-2000");
+    addEncodingName(japaneseEncodings, "cp932");
+    addEncodingName(japaneseEncodings, "x-mac-japanese");
+
+    nonBackslashEncodings = new HashSet<const char*>();
+    // The text encodings below treat backslash as a currency symbol for IE compatibility.
+    // See http://blogs.msdn.com/michkap/archive/2005/09/17/469941.aspx for more information.
+    addEncodingName(nonBackslashEncodings, "x-mac-japanese");
+    addEncodingName(nonBackslashEncodings, "ISO-2022-JP");
+    addEncodingName(nonBackslashEncodings, "EUC-JP");
+    // Shift_JIS_X0213-2000 is not the same encoding as Shift_JIS on Mac. We need to register both of them.
+    addEncodingName(nonBackslashEncodings, "Shift_JIS");
+    addEncodingName(nonBackslashEncodings, "Shift_JIS_X0213-2000");
+}
+
+bool isJapaneseEncoding(const char* canonicalEncodingName)
+{
+    return canonicalEncodingName && japaneseEncodings && japaneseEncodings->contains(canonicalEncodingName);
+}
+
+bool shouldShowBackslashAsCurrencySymbolIn(const char* canonicalEncodingName)
+{
+    return canonicalEncodingName && nonBackslashEncodings && nonBackslashEncodings->contains(canonicalEncodingName);
+}
+
 static void extendTextCodecMaps()
 {
 #if USE(ICU_UNICODE)
@@ -278,6 +334,7 @@ static void extendTextCodecMaps()
 #endif
 
     pruneBlacklistedCodecs();
+    buildQuirksSets();
 }
 
 PassOwnPtr<TextCodec> newTextCodec(const TextEncoding& encoding)
