7eabaa4c76d8e2a009ad32400da933f89c7ac92e
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-10 20:50:13 +0000

    Marking ExtensionApiTest, Infobars as DISABLED on everything but Windows.
    TEST=none
    BUG=66461
    TBR=asargent@chromium.org
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_infobar_apitest.cc
+++ chrome/browser/extensions/extension_infobar_apitest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-#if defined(TOOLKIT_VIEWS)
+#if defined(OS_WIN)
 #define MAYBE_Infobars Infobars
 #else
 // Need to finish port to Linux. See http://crbug.com/39916 for details.
