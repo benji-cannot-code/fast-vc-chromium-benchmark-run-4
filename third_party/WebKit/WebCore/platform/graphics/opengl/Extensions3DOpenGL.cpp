d8147d33ead8e9ce369c31641ac2c0bf3512aaf3
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-10 23:14:28 +0000

    2010-12-10  Kenneth Russell  <kbr@google.com>
            Unreviewed, build fix. Change #ifndef NDEBUG to #ifdef NDEBUG.
    
            * platform/graphics/opengl/Extensions3DOpenGL.cpp:
            (WebCore::Extensions3DOpenGL::ensureEnabled):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/opengl/Extensions3DOpenGL.cpp
+++ third_party/WebKit/WebCore/platform/graphics/opengl/Extensions3DOpenGL.cpp
@@ -88,7 +88,7 @@ bool Extensions3DOpenGL::supports(const String& name)
 
 void Extensions3DOpenGL::ensureEnabled(const String& name)
 {
-#ifndef NDEBUG
+#ifdef NDEBUG
     UNUSED_PARAM(name);
 #endif
     ASSERT(supports(name));
