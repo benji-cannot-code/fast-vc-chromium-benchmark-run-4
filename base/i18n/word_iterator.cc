cc87b8e3da42186b146053f4b5a1df7d8099015c
cdn@chromium.org <cdn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-10 18:12:02 +0000

    Remove ICU header dependencies outside of /i18n directory. Committing for tsepez. Original code review is here http://codereview.chromium.org/5682002/
    BUG=49747
    TEST=WordIteratorTest.*
    Review URL: http://codereview.chromium.org/5718003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68867 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/word_iterator.cc
+++ base/i18n/word_iterator.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "unicode/ubrk.h"
+#include "unicode/uchar.h"
 #include "unicode/ustring.h"
 
 const size_t npos = -1;
--- chrome/browser/history/in_memory_url_index.cc
+++ chrome/browser/history/in_memory_url_index.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/url_database.h"
 #include "net/base/escape.h"
 #include "net/base/net_util.h"
+#include "unicode/utypes.h"  // for int32_t
 
 using base::Time;
 using base::TimeDelta;
