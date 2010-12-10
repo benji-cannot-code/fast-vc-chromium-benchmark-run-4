7e8e95dbc103abbdb900bc1d9fb4809103da8d21
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-10 23:43:44 +0000

    WebKit2: Context menu support on Windows <https://bugs.webkit.org/show_bug.cgi?id=50514>
    Reviewed by Darin Adler.
    
    Implment showing of the context menu on Windows for WebKit2, and calling through to
    WebPageProxy::contextMenuItemSelected.
    
    * UIProcess/win/WebContextMenuProxyWin.cpp:
    (WebKit::WebContextMenuProxyWin::WebContextMenuProxyWin): Initialize the member variables.
    (WebKit::WebContextMenuProxyWin::populateMenu): Iterate over the vector of WebContextMenuItemData
        that was passed in, adding the menu items and recursively calling back into this function to populate
        any submenus that are needed. This function also sets up the map from action identifierss to WebContextMenuItemData,
        to use to find the WebContextMenuItemData from a selected menu item.
    (WebKit::WebContextMenuProxyWin::showContextMenu): Destroy any menu we have already created, populate the menu
        to show, and show the context menu. Once TrackPopupMenuEx returns, it returns identifier of the selected menu item.
        Look up that identifier in the map we created, and tell the WebPageProxy that a context menu item was selected.
    (WebKit::WebContextMenuProxyWin::hideContextMenu): Destroy the HMENU if it is non-null, and clear the identifier map. 
    * UIProcess/win/WebContextMenuProxyWin.h:
    (WebKit::WebContextMenuProxyWin::create): Call through to the constructor, and change the arguments that it takes.
    * UIProcess/win/WebView.cpp:
    (WebKit::WebView::createContextMenuProxy): Call with the correct arguments.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebContextMenuProxyWin.h
+++ third_party/WebKit/WebKit2/UIProcess/win/WebContextMenuProxyWin.h
@@ -27,19 +27,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebContextMenuProxyWin_h
 #define WebContextMenuProxyWin_h
 
+#include "WebContextMenuItemData.h"
 #include "WebContextMenuProxy.h"
+#include "WebPageProxy.h"
+#include <wtf/HashMap.h>
 
 namespace WebKit {
 
 class WebContextMenuProxyWin : public WebContextMenuProxy {
 public:
-    static PassRefPtr<WebContextMenuProxyWin> create();
+    static PassRefPtr<WebContextMenuProxyWin> create(HWND parentWindow, WebPageProxy* page)
+    {
+        return adoptRef(new WebContextMenuProxyWin(parentWindow, page));
+    }
 
 private:
-    WebContextMenuProxyWin();
+    WebContextMenuProxyWin(HWND parentWindow, WebPageProxy* page);
 
     virtual void showContextMenu(const WebCore::IntPoint&, const Vector<WebContextMenuItemData>&);
     virtual void hideContextMenu();
+
+    void populateMenu(HMENU, const Vector<WebContextMenuItemData>&);
+
+    HMENU m_menu;
+    HWND m_window;
+    WebPageProxy* m_page;
+
+    // Creates a map from the context menu item's action to the context menu item itself.
+    HashMap<int, WebContextMenuItemData> m_actionMap;
 };
 
 } // namespace WebKit
