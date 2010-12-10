dba96b07e86d92948e1b988bd8e00e0c4db2e182
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-10 10:31:15 +0000

    2010-12-10  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Simplify context-menu handling code
            https://bugs.webkit.org/show_bug.cgi?id=49658
    
            * wtf/PlatformRefPtr.h:
    2010-12-10  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Simplify context-menu handling code
            https://bugs.webkit.org/show_bug.cgi?id=49658
    
            * platform/ContextMenuItem.h:
            * platform/gtk/ContextMenuGtk.cpp:
            (WebCore::ContextMenu::appendItem):
            * platform/gtk/ContextMenuItemGtk.cpp:
            (WebCore::ContextMenuItem::ContextMenuItem):
            (WebCore::ContextMenuItem::~ContextMenuItem):
            (WebCore::ContextMenuItem::releasePlatformDescription):
            (WebCore::ContextMenuItem::type):
            (WebCore::ContextMenuItem::setType):
            (WebCore::ContextMenuItem::action):
            (WebCore::ContextMenuItem::setAction):
            (WebCore::ContextMenuItem::title):
            (WebCore::ContextMenuItem::setTitle):
            (WebCore::ContextMenuItem::platformSubMenu):
            (WebCore::ContextMenuItem::setSubMenu):
            (WebCore::ContextMenuItem::setChecked):
            (WebCore::ContextMenuItem::setEnabled):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/ContextMenuItem.h
+++ third_party/WebKit/WebCore/platform/ContextMenuItem.h
@@ -43,6 +43,7 @@ class NSMenuItem;
 #elif PLATFORM(WIN)
 typedef struct tagMENUITEMINFOW* LPMENUITEMINFO;
 #elif PLATFORM(GTK)
+#include <GRefPtr.h>
 typedef struct _GtkMenuItem GtkMenuItem;
 #elif PLATFORM(QT)
 #include <QAction>
@@ -186,22 +187,7 @@ namespace WebCore {
         bool enabled;
     };
 #elif PLATFORM(GTK)
-    struct PlatformMenuItemDescription {
-        PlatformMenuItemDescription()
-            : type(ActionType)
-            , action(ContextMenuItemTagNoAction)
-            , subMenu(0)
-            , checked(false)
-            , enabled(true)
-        {}
-
-        ContextMenuItemType type;
-        ContextMenuAction action;
-        String title;
-        GtkMenu* subMenu;
-        bool checked;
-        bool enabled;
-    };
+    typedef GtkMenuItem* PlatformMenuItemDescription;
 #elif PLATFORM(WX)
     struct PlatformMenuItemDescription {
         PlatformMenuItemDescription()
@@ -261,9 +247,7 @@ namespace WebCore {
 
         ContextMenuItem(ContextMenuItemType, ContextMenuAction, const String&, bool enabled, bool checked);
         ContextMenuItem(ContextMenuAction, const String&, bool enabled, bool checked, Vector<ContextMenuItem>& submenuItems);
-#if PLATFORM(GTK)
-        ContextMenuItem(GtkMenuItem*);
-#endif
+
         ~ContextMenuItem();
 
         PlatformMenuItemDescription releasePlatformDescription();
@@ -288,13 +272,12 @@ namespace WebCore {
         bool enabled() const;
 
         // FIXME: Do we need a keyboard accelerator here?
-#if PLATFORM(GTK)
-        static GtkMenuItem* createNativeMenuItem(const PlatformMenuItemDescription&);
-#endif
 
     private:
 #if PLATFORM(MAC)
         RetainPtr<NSMenuItem> m_platformDescription;
+#elif PLATFORM(GTK)
+        PlatformRefPtr<GtkMenuItem> m_platformDescription;
 #else
         PlatformMenuItemDescription m_platformDescription;
 #endif
