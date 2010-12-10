3cf541ad2f3f17d4ace00dc6c2fdaf2f95865fb8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-10 04:06:02 +0000

    2010-12-09  Jasmin Lapalme  <jlapalme@druide.com>
            Reviewed by Alexey Proskuryakov.
    
            Test for a faulty conversion from UTF-8 to UTF-16 in WebCore during an XSLT transformation.
            https://bugs.webkit.org/show_bug.cgi?id=50708
    
            * fast/xsl/utf8-chunks-expected.txt: Added.
            * fast/xsl/utf8-chunks.xml: Added.
            * fast/xsl/utf8-chunks.xsl: Added.
    2010-12-09  Jasmin Lapalme  <jlapalme@druide.com>
    
            Reviewed by Alexey Proskuryakov.
    
            Fix a faulty conversion from UTF-8 to UTF-16 in WebCore during an XSLT transformation.
            https://bugs.webkit.org/show_bug.cgi?id=50708
    
            Test: fast/xsl/utf8-chunks.xml
    
            * xml/XSLTProcessorLibxslt.cpp:
            (WebCore::writeToVector): now converts and returns the correct byte count when the end of the chunk is in the middle of a multibyte UTF-8 character.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XSLTProcessorLibxslt.cpp
+++ third_party/WebKit/WebCore/xml/XSLTProcessorLibxslt.cpp
@@ -46,8 +46,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <libxslt/variables.h>
 #include <libxslt/xsltutils.h>
 #include <wtf/Assertions.h>
-#include <wtf/text/CString.h>
 #include <wtf/Vector.h>
+#include <wtf/text/StringBuffer.h>
+#include <wtf/unicode/UTF8.h>
 
 #if PLATFORM(MAC)
 #include "SoftLinking.h"
@@ -162,9 +163,24 @@ static inline void setXSLTLoadCallBack(xsltDocLoaderFunc func, XSLTProcessor* pr
 static int writeToVector(void* context, const char* buffer, int len)
 {
     Vector<UChar>& resultOutput = *static_cast<Vector<UChar>*>(context);
-    String decodedChunk = String::fromUTF8(buffer, len);
-    resultOutput.append(decodedChunk.characters(), decodedChunk.length());
-    return len;
+
+    if (!len)
+        return 0;
+
+    StringBuffer stringBuffer(len);
+    UChar* bufferUChar = stringBuffer.characters();
+    UChar* bufferUCharEnd = bufferUChar + len;
+
+    const char* stringCurrent = buffer;
+    WTF::Unicode::ConversionResult result = WTF::Unicode::convertUTF8ToUTF16(&stringCurrent, buffer + len, &bufferUChar, bufferUCharEnd);
+    if (result != WTF::Unicode::conversionOK && result != WTF::Unicode::sourceExhausted) {
+        ASSERT_NOT_REACHED();
+        return -1;
+    }
+
+    int utf16Length = bufferUChar - stringBuffer.characters();
+    resultOutput.append(stringBuffer.characters(), utf16Length);
+    return stringCurrent - buffer;
 }
 
 static bool saveResultToString(xmlDocPtr resultDoc, xsltStylesheetPtr sheet, String& resultString)
