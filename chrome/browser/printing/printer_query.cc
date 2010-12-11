b3bbffedb3944e2acf035e573dac62f24101e926
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-11 00:12:09 +0000

    Revert 68893 - Consider PlatformThread::Join() to be blocking IO.
    Marks PlatformThread::Join() as blocking IO using ThreadRestrictions.
    Whitelists existing spots where we join on the UI/IO threads.
    
    Also noteworthy is I allow blocking IO on shutdown.
    
    BUG=65530,66077,66082
    TEST=none
    
    Review URL: http://codereview.chromium.org/5693003
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/5743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68915 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/printer_query.cc
+++ chrome/browser/printing/printer_query.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/printing/printer_query.h"
 
 #include "base/message_loop.h"
-#include "base/thread_restrictions.h"
 #include "chrome/browser/printing/print_job_worker.h"
 
 namespace printing {
@@ -98,10 +97,6 @@ void PrinterQuery::GetSettings(GetSettingsAskParam ask_user_for_settings,
 
 void PrinterQuery::StopWorker() {
   if (worker_.get()) {
-    // http://crbug.com/66082: We're blocking on the PrinterQuery's worker
-    // thread.  It's not clear to me if this may result in blocking the current
-    // thread for an unacceptable time.  We should probably fix it.
-    base::ThreadRestrictions::ScopedAllowIO allow_io;
     worker_->Stop();
     worker_.reset();
   }
