e61cc2a48c0d113f95e81d3020e49e7076c44a90
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-11 06:15:17 +0000

    2010-12-10  Alexey Proskuryakov  <ap@apple.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=46573
            <rdar://problem/8479389> REGRESSION: charset="utf-8" (with quotes) doesn't work
    
            * http/tests/mime/quoted-charset-expected.txt: Added.
            * http/tests/mime/quoted-charset.php: Added.
    2010-12-10  Alexey Proskuryakov  <ap@apple.com>
    
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=46573
            <rdar://problem/8479389> REGRESSION: charset="utf-8" (with quotes) doesn't work
    
            Test: http/tests/mime/quoted-charset.php
    
            * platform/network/cf/ResourceResponseCFNet.cpp: (WebCore::ResourceResponse::platformLazyInit):
            * platform/network/mac/ResourceResponseMac.mm: (WebCore::ResourceResponse::platformLazyInit):
            Work around a CFNetwork issue, strip quotes if they are present.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/mime/quoted-charset.php
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    header('Content-type: text/html; charset="koi8-r"');
+?>
+
+<p>Should say SUCCESS: SUÛÛÂSS</p>
+<p>The latter has some Cyrillic characters that look like Latin ones. This test verifies that decoding
+is correctly performed.</p>
+
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+</script>
