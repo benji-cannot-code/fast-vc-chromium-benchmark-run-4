ee0cc205d08a7ee06ed390e1b507ee19cf28941c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-12 01:47:54 +0000

    net: Add namespace net to URLRequestFtpJob.
    BUG=64263
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/5739005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68952 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_ftp_job.h
+++ net/url_request/url_request_ftp_job.h
@@ -19,36 +19,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class URLRequestContext;
 
-// A net::URLRequestJob subclass that is built on top of FtpTransaction. It
+namespace net {
+
+// A URLRequestJob subclass that is built on top of FtpTransaction. It
 // provides an implementation for FTP.
-class URLRequestFtpJob : public net::URLRequestJob {
+class URLRequestFtpJob : public URLRequestJob {
  public:
 
-  explicit URLRequestFtpJob(net::URLRequest* request);
+  explicit URLRequestFtpJob(URLRequest* request);
 
-  static net::URLRequestJob* Factory(net::URLRequest* request,
-                                     const std::string& scheme);
+  static URLRequestJob* Factory(URLRequest* request,
+                                const std::string& scheme);
 
-  // net::URLRequestJob methods:
+  // Overridden from URLRequestJob:
   virtual bool GetMimeType(std::string* mime_type) const;
 
  private:
   virtual ~URLRequestFtpJob();
 
-  // net::URLRequestJob methods:
+  // Overridden from URLRequestJob:
   virtual void Start();
   virtual void Kill();
-  virtual net::LoadState GetLoadState() const;
+  virtual LoadState GetLoadState() const;
   virtual bool NeedsAuth();
   virtual void GetAuthChallengeInfo(
-      scoped_refptr<net::AuthChallengeInfo>* auth_info);
+      scoped_refptr<AuthChallengeInfo>* auth_info);
   virtual void SetAuth(const string16& username,
                        const string16& password);
   virtual void CancelAuth();
 
   // TODO(ibrar):  Yet to give another look at this function.
   virtual uint64 GetUploadProgress() const { return 0; }
-  virtual bool ReadRawData(net::IOBuffer* buf, int buf_size, int *bytes_read);
+  virtual bool ReadRawData(IOBuffer* buf, int buf_size, int *bytes_read);
 
   void StartTransaction();
 
@@ -59,15 +61,15 @@ class URLRequestFtpJob : public net::URLRequestJob {
 
   void LogFtpServerType(char server_type);
 
-  net::FtpRequestInfo request_info_;
-  scoped_ptr<net::FtpTransaction> transaction_;
+  FtpRequestInfo request_info_;
+  scoped_ptr<FtpTransaction> transaction_;
 
-  net::CompletionCallbackImpl<URLRequestFtpJob> start_callback_;
-  net::CompletionCallbackImpl<URLRequestFtpJob> read_callback_;
+  CompletionCallbackImpl<URLRequestFtpJob> start_callback_;
+  CompletionCallbackImpl<URLRequestFtpJob> read_callback_;
 
   bool read_in_progress_;
 
-  scoped_refptr<net::AuthData> server_auth_;
+  scoped_refptr<AuthData> server_auth_;
 
   // Keep a reference to the url request context to be sure it's not deleted
   // before us.
@@ -77,5 +79,7 @@ class URLRequestFtpJob : public net::URLRequestJob {
 
   DISALLOW_COPY_AND_ASSIGN(URLRequestFtpJob);
 };
+
+}  // namespace net
 
 #endif  // NET_URL_REQUEST_URL_REQUEST_FTP_JOB_H_
