15bdcca55141b2ada0637c18382581ad7ae8cf2e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-13 09:11:59 +0000

    2010-12-13  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Eric Seidel.
    
            [chromium] Reduce canvas.toDataURL("image/jpeg") run-time cost by 10%
            https://bugs.webkit.org/show_bug.cgi?id=50804
    
            Follow on from r73173, unroll the SkUnPreMultiply::PMColorToColor() call
            viz., compute the unpremultiplatcion in-place. This reduces the run-time
            cost of jpeg encoding by 10-15% for my image test set.
    
            No new tests: canvas.toDataURL() is covered by existing tests.
    
            * platform/image-encoders/skia/JPEGImageEncoder.cpp:
            (WebCore::preMultipliedBGRAtoRGB):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/image-encoders/skia/JPEGImageEncoder.cpp
+++ third_party/WebKit/WebCore/platform/image-encoders/skia/JPEGImageEncoder.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntSize.h"
 #include "SkBitmap.h"
 #include "SkUnPreMultiply.h"
+#include "SkColorPriv.h"
 extern "C" {
 #include <stdio.h> // jpeglib.h needs stdio.h FILE
 #include "jpeglib.h"
@@ -83,11 +84,19 @@ static void handleError(j_common_ptr common)
 // be ignored? See bug http://webkit.org/b/40147.
 void preMultipliedBGRAtoRGB(const SkPMColor* input, unsigned int pixels, unsigned char* output)
 {
-    while (pixels-- > 0) {
-        SkColor unmultiplied = SkUnPreMultiply::PMColorToColor(*input++);
-        *output++ = SkColorGetR(unmultiplied);
-        *output++ = SkColorGetG(unmultiplied);
-        *output++ = SkColorGetB(unmultiplied);
+    static const SkUnPreMultiply::Scale* scale = SkUnPreMultiply::GetScaleTable();
+
+    for (; pixels-- > 0; ++input) {
+        const unsigned alpha = SkGetPackedA32(*input);
+        if ((alpha != 0) && (alpha != 255)) {
+            *output++ = SkUnPreMultiply::ApplyScale(scale[alpha], SkGetPackedR32(*input));
+            *output++ = SkUnPreMultiply::ApplyScale(scale[alpha], SkGetPackedG32(*input));
+            *output++ = SkUnPreMultiply::ApplyScale(scale[alpha], SkGetPackedB32(*input));
+        } else {
+            *output++ = SkGetPackedR32(*input);
+            *output++ = SkGetPackedG32(*input);
+            *output++ = SkGetPackedB32(*input);
+        }
     }
 }
 
