9cb90c374eb1ae423feef0b950465eef7d0d1c56
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-13 08:56:31 +0000

    2010-12-13  Helder Correia  <helder@sencha.com>
            Reviewed by Eric Seidel.
    
            [Qt] StillImage::draw() should use ContextShadow
            https://bugs.webkit.org/show_bug.cgi?id=50849
    
            The branch that checks for a shadow requirement in StillImage::draw()
            is executed when drawing a canvas onto a canvas with
            ctx1.drawImage(canvas2, x, y). The current implementation supports
            solid shadows only. And if transformations are present, the offset gets
            transformed as well. Thus, ContextShadow must be used instead in order
            to support blur and correct offset transformations.
    
            * fast/canvas/canvas-draw-canvas-on-canvas-shadow-expected.txt: Added.
            * fast/canvas/canvas-draw-canvas-on-canvas-shadow.html: Added.
            * fast/canvas/script-tests/canvas-draw-canvas-on-canvas-shadow.js: Added.
    2010-12-13  Helder Correia  <helder@sencha.com>
    
            Reviewed by Eric Seidel.
    
            [Qt] StillImage::draw() should use ContextShadow
            https://bugs.webkit.org/show_bug.cgi?id=50849
    
            The branch that checks for a shadow requirement in StillImage::draw()
            is executed when drawing a canvas onto a canvas with
            ctx1.drawImage(canvas2, x, y). The current implementation supports
            solid shadows only. And if transformations are present, the offset gets
            transformed as well. Thus, ContextShadow must be used instead in order
            to support blur and correct offset transformations.
    
            Test: fast/canvas/canvas-draw-canvas-on-canvas-shadow.html
    
            * platform/graphics/qt/StillImageQt.cpp:
            (WebCore::StillImage::draw):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/qt/StillImageQt.cpp
+++ third_party/WebKit/WebCore/platform/graphics/qt/StillImageQt.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "StillImageQt.h"
 
+#include "ContextShadow.h"
 #include "GraphicsContext.h"
 #include "IntSize.h"
 
@@ -68,7 +69,6 @@ void StillImage::draw(GraphicsContext* ctxt, const FloatRect& dst,
     if (m_pixmap->isNull())
         return;
 
-
     FloatRect normalizedSrc = src.normalized();
     FloatRect normalizedDst = dst.normalized();
 
@@ -77,21 +77,14 @@ void StillImage::draw(GraphicsContext* ctxt, const FloatRect& dst,
 
     ctxt->setCompositeOperation(op);
 
-    FloatSize shadowOffset;
-    float shadowBlur;
-    Color shadowColor;
-    if (ctxt->getShadow(shadowOffset, shadowBlur, shadowColor)) {
-        FloatRect shadowImageRect(normalizedDst);
-        shadowImageRect.move(shadowOffset.width(), shadowOffset.height());
-
-        QImage shadowImage(QSize(static_cast<int>(normalizedSrc.width()), static_cast<int>(normalizedSrc.height())), QImage::Format_ARGB32_Premultiplied);
-        QPainter p(&shadowImage);
-        p.setCompositionMode(QPainter::CompositionMode_Source);
-        p.fillRect(shadowImage.rect(), shadowColor);
-        p.setCompositionMode(QPainter::CompositionMode_DestinationIn);
-        p.drawPixmap(QRect(0, 0, normalizedDst.width(), normalizedDst.height()), *m_pixmap, normalizedSrc);
-        p.end();
-        painter->drawImage(shadowImageRect, shadowImage, normalizedSrc);
+    ContextShadow* shadow = ctxt->contextShadow();
+    if (shadow->m_type != ContextShadow::NoShadow) {
+        QPainter* shadowPainter = shadow->beginShadowLayer(painter, normalizedDst);
+        if (shadowPainter) {
+            shadowPainter->setOpacity(static_cast<qreal>(shadow->m_color.alpha()) / 255);
+            shadowPainter->drawPixmap(normalizedDst, *m_pixmap, normalizedSrc);
+            shadow->endShadowLayer(painter);
+        }
     }
 
     painter->drawPixmap(normalizedDst, *m_pixmap, normalizedSrc);
