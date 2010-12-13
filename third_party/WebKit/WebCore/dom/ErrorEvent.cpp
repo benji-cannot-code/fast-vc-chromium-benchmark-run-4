e651fc8337a84ea1c5e12bf4486ec89143985f46
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-13 15:33:58 +0000

    2010-12-13  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed. Qt build fix. 
    
            * dom/ErrorEvent.cpp:
            * dom/ErrorEvent.h: remove ENABLE(WORKERS) guard since ErrorEvent is now used not
            only in workers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/ErrorEvent.cpp
+++ third_party/WebKit/WebCore/dom/ErrorEvent.cpp
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#if ENABLE(WORKERS)
-
 #include "ErrorEvent.h"
 
 #include "EventNames.h"
@@ -73,5 +71,3 @@ bool ErrorEvent::isErrorEvent() const
 }
 
 } // namespace WebCore
-
-#endif // ENABLE(WORKERS)
