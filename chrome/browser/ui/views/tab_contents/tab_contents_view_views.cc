0c1b75106395de4204591c5b2904e68337752242
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-13 17:50:32 +0000

    touchui: Fix showing sad tabs.
    BUG=sad tab (e.g. about:crash) does not work.
    TEST=about:crash works
    
    Review URL: http://codereview.chromium.org/5698007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69015 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/tab_contents_view_views.cc
+++ chrome/browser/ui/views/tab_contents/tab_contents_view_views.cc
@@ -85,9 +85,8 @@ RenderWidgetHostView* TabContentsViewViews::CreateViewForWidget(
 
   // If we were showing sad tab, remove it now.
   if (sad_tab_ != NULL) {
-    RemoveChildView(sad_tab_);
-    AddChildView(new views::View());
-    sad_tab_ = NULL;
+    RemoveChildView(sad_tab_.get());
+    sad_tab_.reset();
   }
 
   RenderWidgetHostViewViews* view =
@@ -133,6 +132,13 @@ void TabContentsViewViews::SetPageTitle(const std::wstring& title) {
 }
 
 void TabContentsViewViews::OnTabCrashed() {
+  if (sad_tab_ != NULL)
+    return;
+
+  sad_tab_.reset(new SadTabView(tab_contents()));
+  RemoveAllChildViews(true);
+  AddChildView(sad_tab_.get());
+  Layout();
 }
 
 void TabContentsViewViews::SizeContents(const gfx::Size& size) {
