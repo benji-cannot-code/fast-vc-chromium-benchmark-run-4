4e6c87cb3369afdcc20b0ce1a77a487d595f02be
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-13 00:30:20 +0000

    Give LogTextInput a sensible flag value.
    Reviewed by Anders Carlsson.
    
    * Platform/Logging.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Platform/Logging.cpp
+++ third_party/WebKit/WebKit2/Platform/Logging.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WTFLogChannel LogSessionState = { 0x00000001, "WebKit2LogLevel", WTFLogChannelOff };
 WTFLogChannel LogContextMenu  = { 0x00000002, "WebKit2LogLevel", WTFLogChannelOff };
-WTFLogChannel LogTextInput    = { 0x00000003, "WebKit2LogLevel", WTFLogChannelOff };
+WTFLogChannel LogTextInput    = { 0x00000004, "WebKit2LogLevel", WTFLogChannelOff };
 
 static inline void initializeLogChannel(WTFLogChannel* channel)
 {
