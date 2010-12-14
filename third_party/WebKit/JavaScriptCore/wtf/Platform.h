9b9c1a42101cb33603e0be9a2dae13206129c25a
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 16:59:46 +0000

    Not reviewed.
    Revert accidental change disabling the JIT for most platforms.
    
    * wtf/Platform.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -974,7 +974,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(MIPS)) \
     && (OS(DARWIN) || !COMPILER(GCC) || GCC_VERSION_AT_LEAST(4,1,0)) \
     && !OS(WINCE)
-#define ENABLE_JIT 0
+#define ENABLE_JIT 1
 #endif
 
 /* Ensure that either the JIT or the interpreter has been enabled. */
