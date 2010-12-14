a6c2be4f32f51de3c9d9e82f71bfd3aad6f1a90d
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 01:08:04 +0000

    Windows Build Fix. Add a needed include.
    * UIProcess/win/WebView.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@73988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerBackedDrawingAreaProxy.h"
 #include "RunLoop.h"
 #include "NativeWebKeyboardEvent.h"
+#include "WebContext.h"
 #include "WebContextMenuProxyWin.h"
 #include "WebEditCommandProxy.h"
 #include "WebEventFactory.h"
