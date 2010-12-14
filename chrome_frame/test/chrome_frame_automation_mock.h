204b50d748736f181f89a3d33050b7f1fde07f1c
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-14 05:22:51 +0000

    Fix ChromeFrame test failures caused by the change to support about:plugins in full tab mode.
    BUG=66118
    TBR=none
    
    Review URL: http://codereview.chromium.org/5707009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69104 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_automation_mock.h
+++ chrome_frame/test/chrome_frame_automation_mock.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome_frame/chrome_frame_automation.h"
 #include "chrome_frame/chrome_frame_plugin.h"
+#include "chrome_frame/navigation_constraints.h"
 #include "chrome_frame/test/http_server.h"
 #include "chrome_frame/test/chrome_frame_test_utils.h"
 #include "chrome_frame/test/test_with_web_server.h"
@@ -59,10 +60,10 @@ class AutomationMockDelegate
 
   // Navigate external tab to the specified url through automation
   bool Navigate(const std::string& url) {
+    NavigationConstraintsImpl navigation_constraints;
     url_ = GURL(url);
-    bool result = automation_client_->InitiateNavigation(url,
-                                                         std::string(),
-                                                         false);
+    bool result = automation_client_->InitiateNavigation(
+        url, std::string(), &navigation_constraints);
     if (!result)
       OnLoadFailed(0, url);
     return result;
