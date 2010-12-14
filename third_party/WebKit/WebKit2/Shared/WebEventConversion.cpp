17da20cb4e27e7ad08240a897ad9064e2fb0af1e
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 12:10:45 +0000

    2010-12-14  Benjamin Poulain  <benjamin.poulain@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            WebKit 2 does not send touch cancel events correctly between processes
            https://bugs.webkit.org/show_bug.cgi?id=50235
    
            The classes WebKit2PlatformTouchPoint and WebKit2PlatformTouchEvent
            misses the conversion of TouchCancel events and points.
    
            This patch add the missing enums when converting from a WebTouchEvent
            to a PlatformTouchEvent.
    
            Testing is already covered by fast/events/touch/send-oncancel-event.html
    
            * Shared/WebEventConversion.cpp:
            (WebKit::WebKit2PlatformTouchPoint::WebKit2PlatformTouchPoint):
            (WebKit::WebKit2PlatformTouchEvent::WebKit2PlatformTouchEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/WebEventConversion.cpp
+++ third_party/WebKit/WebKit2/Shared/WebEventConversion.cpp
@@ -180,6 +180,9 @@ public:
         case WebPlatformTouchPoint::TouchStationary:
             m_state = PlatformTouchPoint::TouchStationary;
             break;
+        case WebPlatformTouchPoint::TouchCancelled:
+            m_state = PlatformTouchPoint::TouchCancelled;
+            break;
         default:
             ASSERT_NOT_REACHED();
         }
@@ -202,6 +205,9 @@ public:
             break;
         case WebEvent::TouchEnd: 
             m_type = WebCore::TouchEnd; 
+            break;
+        case WebEvent::TouchCancel:
+            m_type = WebCore::TouchCancel;
             break;
         default:
             ASSERT_NOT_REACHED();
