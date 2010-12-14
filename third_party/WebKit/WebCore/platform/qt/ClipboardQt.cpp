bd2081dfe037dc1355ea64e2d396cd545d077911
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 10:50:03 +0000

    2010-12-14  Jarred Nicholls  <jarred@sencha.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Qt's Clipboard::files() implementation for HTML5 Drag/Drop (DataTransfer)
    
            No new tests. Run tests manually until DRT is updated w/ beginDragWithFiles.
    
            * platform/qt/ClipboardQt.cpp:
            (WebCore::ClipboardQt::files):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/qt/ClipboardQt.cpp
+++ third_party/WebKit/WebCore/platform/qt/ClipboardQt.cpp
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2007 Apple Inc.  All rights reserved.
  * Copyright (C) 2006, 2007 Apple Inc.  All rights reserved.
  * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2010 Sencha, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -205,8 +206,20 @@ HashSet<String> ClipboardQt::types() const
 
 PassRefPtr<FileList> ClipboardQt::files() const
 {
-    notImplemented();
-    return 0;
+    if (policy() != ClipboardReadable || !m_readableData->hasUrls())
+        return FileList::create();
+
+    RefPtr<FileList> fileList = FileList::create();
+    QList<QUrl> urls = m_readableData->urls();
+
+    for (int i = 0; i < urls.size(); i++) {
+        QUrl url = urls[i];
+        if (url.scheme() != QLatin1String("file"))
+            continue;
+        fileList->append(File::create(url.toLocalFile()));
+    }
+
+    return fileList.release();
 }
 
 void ClipboardQt::setDragImage(CachedImage* image, const IntPoint& point)
