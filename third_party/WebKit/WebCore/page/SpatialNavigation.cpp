a2389ddfc71d0a926f48921ab16fd1658191c486
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 06:51:52 +0000

    2010-12-13  Antonio Gomes  <agomes@rim.com>
            Reviewed by Daniel Bates.
    
            Spatial Navigation: code clean up (part IV)
            https://bugs.webkit.org/show_bug.cgi?id=50666
    
            Avoid calling canScrollInDirection more than necessary.
    
            No new tests needed.
    
            * page/FocusController.cpp:
            (WebCore::FocusController::advanceFocusDirectionallyInContainer): Moved the call to canScrollInDirection()
            to within the scrollInDirection().
            * page/SpatialNavigation.cpp: Removed the assertion to canScrollingDirection function.
            (WebCore::scrollInDirection):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/SpatialNavigation.cpp
+++ third_party/WebKit/WebCore/page/SpatialNavigation.cpp
@@ -335,7 +335,7 @@ bool hasOffscreenRect(Node* node, FocusDirection direction)
 
 bool scrollInDirection(Frame* frame, FocusDirection direction)
 {
-    ASSERT(frame && canScrollInDirection(direction, frame->document()));
+    ASSERT(frame);
 
     if (frame && canScrollInDirection(direction, frame->document())) {
         int dx = 0;
@@ -366,13 +366,14 @@ bool scrollInDirection(Frame* frame, FocusDirection direction)
 
 bool scrollInDirection(Node* container, FocusDirection direction)
 {
+    ASSERT(container);
     if (container->isDocumentNode())
         return scrollInDirection(static_cast<Document*>(container)->frame(), direction);
 
     if (!container->renderBox())
         return false;
 
-    if (container && canScrollInDirection(direction, container)) {
+    if (canScrollInDirection(direction, container)) {
         int dx = 0;
         int dy = 0;
         switch (direction) {
@@ -398,6 +399,7 @@ bool scrollInDirection(Node* container, FocusDirection direction)
         container->renderBox()->enclosingLayer()->scrollByRecursively(dx, dy);
         return true;
     }
+
     return false;
 }
 
