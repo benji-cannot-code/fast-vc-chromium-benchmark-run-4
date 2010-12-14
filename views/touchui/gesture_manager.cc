82f9dbc4f686c8995cf0f5477cf85b3070b2c061
bryeung@google.com <bryeung@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-14 15:37:45 +0000

    Fix GestureManager Singleton implementation
    This fix makes GestureManager conform to the new way of doing Singletons.  As per satish@'s email Tue, Dec 14, 2010.  "Changes to Singleton<T> pattern usage"
    
    This affects the touchui build only.
    
    Original CL is http://codereview.chromium.org/3192002
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/5696005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69133 0039d316-1c4b-4281-b951-d872f2087c98

--- views/touchui/gesture_manager.cc
+++ views/touchui/gesture_manager.cc
@@ -17,7 +17,7 @@ namespace views {
 GestureManager::~GestureManager() {
 }
 
-GestureManager* GestureManager::Get() {
+GestureManager* GestureManager::GetInstance() {
   return Singleton<GestureManager>::get();
 }
 
