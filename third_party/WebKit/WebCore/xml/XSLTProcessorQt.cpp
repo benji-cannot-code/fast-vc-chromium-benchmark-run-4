3727e50842030a740e19c031ffdc9e9b93916fcf
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-14 20:43:32 +0000

    2010-12-14  Nate Chapin  <japhet@chromium.org>
            Unreviewed, fixing qt (hopefully for real this time).
    
            Remove a couple more references to loader.h.
    
            * xml/XSLStyleSheetQt.cpp:
            * xml/XSLTProcessorQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/xml/XSLTProcessorQt.cpp
+++ third_party/WebKit/WebCore/xml/XSLTProcessorQt.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMWindow.h"
 #include "Frame.h"
 #include "TransformSource.h"
-#include "loader.h"
 #include "markup.h"
 #include <wtf/Assertions.h>
 #include <wtf/Vector.h>
