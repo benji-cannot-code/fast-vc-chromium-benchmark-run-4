dee4125d4910a108dc42424817eb725a581021d6
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-14 20:56:01 +0000

    Implement a ReadyPromptContent that displays a prompt to accept, temporarily decline, or permanently decline ready mode. Implement a RegistryReadyModeState that manages the state (based on user's decision) in the registry. Interfaces to integrate those with the installer.
    BUG=None
    TEST=chrome_frame_unittests --gtest_filter='Ready*'
    Review URL: http://codereview.chromium.org/5747002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69175 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome_frame/ready_mode/internal/ready_mode_state.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_FRAME_READY_MODE_INTERNAL_READY_MODE_STATE_H_
+#define CHROME_FRAME_READY_MODE_INTERNAL_READY_MODE_STATE_H_
+#pragma once
+
+// Allows the UI element to signal the user's response to a Ready Mode prompt.
+class ReadyModeState {
+ public:
+  virtual ~ReadyModeState() {}
+
+  // Indicates that the user has temporarily declined the product.
+  virtual void TemporarilyDeclineChromeFrame() = 0;
+
+  // Indicates that the user has permanently declined the product.
+  virtual void PermanentlyDeclineChromeFrame() = 0;
+
+  // Indicates that the user has accepted the product.
+  virtual void AcceptChromeFrame() = 0;
+};  // class ReadyModeState
+
+#endif  // CHROME_FRAME_READY_MODE_INTERNAL_READY_MODE_STATE_H_
