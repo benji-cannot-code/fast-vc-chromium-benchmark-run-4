db555370ff6443d4bf47eb0e5836d0cd5e3419dd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-15 23:06:43 +0000

    2010-12-15  Yong Li  <yoli@rim.com>
            Reviewed by Darin Adler.
    
            Reproduce stack overflow when there are too many sibling inline boxes.
            https://bugs.webkit.org/show_bug.cgi?id=48255
    
            * fast/overflow/lots-of-sibling-inline-boxes.html: Added.
    2010-12-15  Yong Li  <yoli@rim.com>
    
            Reviewed by Darin Adler.
    
            Fix stack overflow when there are too many sibling inline boxes by using
            a loop to traverse children instead of calling each sibling from the first child.
            https://bugs.webkit.org/show_bug.cgi?id=48255
    
            Test: fast/overflow/lots-of-sibling-inline-boxes.html
    
            * rendering/InlineBox.h:
            (WebCore::InlineBox::setConstructed):
            (WebCore::InlineBox::next):
            * rendering/InlineFlowBox.h:
            (WebCore::InlineFlowBox::setConstructed):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/InlineBox.h
+++ third_party/WebKit/WebCore/rendering/InlineBox.h
@@ -132,6 +132,8 @@ public:
     virtual void paint(PaintInfo&, int tx, int ty);
     virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty);
 
+    InlineBox* next() const { return m_next; }
+
     // Overloaded new operator.
     void* operator new(size_t, RenderArena*) throw();
 
@@ -177,12 +179,7 @@ public:
     }
 
     bool isConstructed() { return m_constructed; }
-    virtual void setConstructed()
-    {
-        m_constructed = true;
-        if (m_next)
-            m_next->setConstructed();
-    }
+    virtual void setConstructed() { m_constructed = true; }
 
     void setExtracted(bool b = true) { m_extracted = b; }
     
--- third_party/WebKit/WebCore/rendering/InlineFlowBox.h
+++ third_party/WebKit/WebCore/rendering/InlineFlowBox.h
@@ -77,8 +77,8 @@ public:
     virtual void setConstructed()
     {
         InlineBox::setConstructed();
-        if (firstChild())
-            firstChild()->setConstructed();
+        for (InlineBox* child = firstChild(); child; child = child->next())
+            child->setConstructed();
     }
 
     void addToLine(InlineBox* child);
