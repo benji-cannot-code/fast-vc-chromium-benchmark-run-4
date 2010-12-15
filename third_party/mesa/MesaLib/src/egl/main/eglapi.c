5803c72c6d7b20ab666dbdff6580c09e0a4851ab
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 22:03:20 +0000

    Fixed warning on linux x64. eglapi.c:276: warning: cast to pointer from integer of different size
    TBR=apatrick@chromium.org
    Review URL: http://codereview.chromium.org/5921002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69325 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/mesa/MesaLib/src/egl/main/eglapi.c
+++ third_party/mesa/MesaLib/src/egl/main/eglapi.c
@@ -274,7 +274,7 @@ EGLDisplay EGLAPIENTRY
 eglGetDisplay(EGLNativeDisplayType nativeDisplay)
 {
    _EGLPlatformType plat = _eglGetNativePlatform();
-   _EGLDisplay *dpy = _eglFindDisplay(plat, (void *) nativeDisplay);
+   _EGLDisplay *dpy = _eglFindDisplay(plat, nativeDisplay);
    return _eglGetDisplayHandle(dpy);
 }
 
