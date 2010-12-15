aa8f2dccd11b3e15c72143d513e9d3f283ff2ee3
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 16:57:28 +0000

    Compile fix?
    TBR=mirandac,ahendrickson
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69271 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_data_remover_helper.cc
+++ chrome/browser/plugin_data_remover_helper.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_service.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/browser/prefs/pref_service.h"
-#include "chrome/browser/profile.h"
+#include "chrome/browser/profiles/profile.h"
 
 class PluginDataRemoverHelper::Internal
     : public base::RefCountedThreadSafe<PluginDataRemoverHelper::Internal> {
