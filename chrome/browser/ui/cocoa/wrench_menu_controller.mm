906e605eaf4c86f3ef08f7d2ada81efe23ef038b
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 23:21:48 +0000

    Reset bg page badge when wrench menu closes.
    BUG=65926
    TEST=Install extension with background page, click on wrench menu, see badge, click again, don't see badge
    
    Review URL: http://codereview.chromium.org/5906002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/wrench_menu_controller.mm
+++ chrome/browser/ui/cocoa/wrench_menu_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/menus/menu_model.h"
 #include "base/sys_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/background_page_tracker.h"
 #import "chrome/browser/ui/cocoa/menu_tracked_root_view.h"
 #import "chrome/browser/ui/cocoa/toolbar_controller.h"
 #include "chrome/browser/ui/browser.h"
@@ -120,6 +121,12 @@ class ZoomLevelObserver : public NotificationObserver {
       [NSImage imageNamed:NSImageNameExitFullScreenTemplate] :
           [NSImage imageNamed:NSImageNameEnterFullScreenTemplate];
   [zoomFullScreen_ setImage:icon];
+}
+
+- (void)menuDidClose:(NSMenu*)menu {
+  // When the menu is closed, acknowledge the background pages so the badges go
+  // away.
+  BackgroundPageTracker::GetInstance()->AcknowledgeBackgroundPages();
 }
 
 // Used to dispatch commands from the Wrench menu. The custom items within the
