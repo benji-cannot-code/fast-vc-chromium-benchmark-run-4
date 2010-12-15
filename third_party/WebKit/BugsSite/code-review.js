97dd4855f914d8a5104c029e321e6f134b7eb14e
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-15 19:33:05 +0000

    2010-12-08  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            [reviewtool] should always show overall comments text box
            https://bugs.webkit.org/show_bug.cgi?id=45870
    
            * PrettyPatch/PrettyPatch.rb:
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/BugsSite/code-review.js
+++ third_party/WebKit/BugsSite/code-review.js
@@ -214,7 +214,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function addFlagsForAttachment(details) {
     var flag_control = "<select><option></option><option>?</option><option>+</option><option>-</option></select>";
-    $('#toolbar .actions').append(
+    $('#flagContainer').append(
       $('<span class="review"> r: ' + flag_control + '</span>')).append(
       $('<span class="commitQueue"> cq: ' + flag_control + '</span>'));
 
@@ -248,7 +248,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
       var details = $(data);
       addFlagsForAttachment(details);
-      $('#toolbar .actions').append($('<iframe class="statusBubble" src="https://webkit-commit-queue.appspot.com/status-bubble/' + attachment_id + '" scrolling="no"></iframe>'));
+      $('#statusBubbleContainer').append($('<iframe style="margin-top:2px;" class="statusBubble" src="https://webkit-commit-queue.appspot.com/status-bubble/' + attachment_id + '" scrolling="no"></iframe>'));
       $('#toolbar .bugLink').html('<a href="/show_bug.cgi?id=' + bug_id + '" target="_blank">Bug ' + bug_id + '</a>');
     });
   }
@@ -261,13 +261,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     });
   }
 
+  function openOverallComments(e) {
+    $('.overallComments textarea').addClass('open');
+    $('#statusBubbleContainer').addClass('wrap');
+  }
+
   $(document).ready(function() {
     crawlDiff();
     fetchHistory();
     $(document.body).prepend('<div id="message"><div class="help">Select line numbers to add a comment.</div><div class="commentStatus"></div></div>');
-    $(document.body).prepend('<div id="toolbar"><div class="actions"><button id="preview_comments">Preview</button><button id="post_comments">Publish</button></div><div class="links"><span class="bugLink"></span></div>');
+    $(document.body).prepend('<div id="toolbar">' +
+        '<div class="overallComments">' +
+            '<textarea placeholder="Overall comments"></textarea>' +
+        '</div>' +
+        '<div>' +
+          '<span id="statusBubbleContainer"></span>' +
+          '<span class="actions">' +
+              '<span class="links"><span class="bugLink"></span></span>' +
+              '<span id="flagContainer"></span>' +
+              '<button id="preview_comments">Preview</button>' +
+              '<button id="post_comments">Publish</button> ' +
+          '</span></div>' +
+        '</div>' +
+        '</div>');
+
+    $('.overallComments textarea').bind('click', openOverallComments);
+
     $(document.body).prepend('<div id="comment_form" class="inactive"><div class="winter"></div><div class="lightbox"><iframe id="reviewform" src="attachment.cgi?id=' + attachment_id + '&action=reviewform"></iframe></div></div>');
-    $(document.body).append('<div class="overallComments"><div class="description">Overall comments:</div><textarea></textarea></div>');
   });
 
   function discardComment() {
