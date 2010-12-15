d0204747366bf0f90ae8318e952cfbc608d51458
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 21:18:47 +0000

    Add a preference to clear plugin data on browser shutdown.
    BUG=58235
    TEST=none
    
    Review URL: http://codereview.chromium.org/5579002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.h
+++ chrome/browser/browser_process_impl.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_status_updater.h"
 #include "chrome/browser/prefs/pref_change_registrar.h"
 #include "chrome/browser/tab_contents/thumbnail_generator.h"
+#include "chrome/common/notification_observer.h"
+#include "chrome/common/notification_registrar.h"
 #include "ipc/ipc_message.h"
 
 class ChromeNetLog;
@@ -30,16 +32,20 @@ class CommandLine;
 class DebuggerWrapper;
 class FilePath;
 class NotificationService;
+class PluginDataRemover;
 class TabCloseableStateWatcher;
 
 // Real implementation of BrowserProcess that creates and returns the services.
-class BrowserProcessImpl : public BrowserProcess, public NonThreadSafe {
+class BrowserProcessImpl : public BrowserProcess,
+                           public NonThreadSafe,
+                           public NotificationObserver {
  public:
   explicit BrowserProcessImpl(const CommandLine& command_line);
   virtual ~BrowserProcessImpl();
 
   virtual void EndSession();
 
+  // BrowserProcess methods
   virtual ResourceDispatcherHost* resource_dispatcher_host();
   virtual MetricsService* metrics_service();
   virtual IOThread* io_thread();
@@ -76,6 +82,11 @@ class BrowserProcessImpl : public BrowserProcess, public NonThreadSafe {
       safe_browsing_detection_service();
   virtual void CheckForInspectorFiles();
 
+  // NotificationObserver methods
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details);
+
 #if (defined(OS_WIN) || defined(OS_LINUX)) && !defined(OS_CHROMEOS)
   virtual void StartAutoupdateTimer();
 #endif
@@ -96,6 +107,8 @@ class BrowserProcessImpl : public BrowserProcess, public NonThreadSafe {
 
   void CreateIOThread();
   static void CleanupOnIOThread();
+
+  void WaitForPluginDataRemoverToFinish();
 
   void CreateFileThread();
   void CreateDBThread();
@@ -224,6 +237,9 @@ class BrowserProcessImpl : public BrowserProcess, public NonThreadSafe {
 
   // Lives here so can safely log events on shutdown.
   scoped_ptr<ChromeNetLog> net_log_;
+
+  NotificationRegistrar notification_registrar_;
+  scoped_refptr<PluginDataRemover> plugin_data_remover_;
 
 #if (defined(OS_WIN) || defined(OS_LINUX)) && !defined(OS_CHROMEOS)
   base::RepeatingTimer<BrowserProcessImpl> autoupdate_timer_;
