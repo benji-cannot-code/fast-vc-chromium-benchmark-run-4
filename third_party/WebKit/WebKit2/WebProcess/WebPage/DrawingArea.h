41430b237151865cda2aefa48817cdb4631a8f4d
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-15 23:35:49 +0000

    2010-12-15  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Adele Peterson.
    
            WebKit2 in compositing mode no longer has font smoothing
            https://bugs.webkit.org/show_bug.cgi?id=50733
    
            Only turn off font smoothing for layers whose contents are
            not opaque. This allows the root GraphicsLayer in WebKit2 to
            set the opaque flag, and get smoothed text.
    
            * platform/graphics/mac/WebLayer.mm:
            (drawLayerContents):
    
    2010-12-15  Simon Fraser  <simon.fraser@apple.com>
    
            Reviewed by Adele Peterson.
    
            WebKit2 in compositing mode no longer has font smoothing
            https://bugs.webkit.org/show_bug.cgi?id=50733
    
            Tell the root GraphicsLayer that its contents are opaque
            if the WebPage draws its background, and that background
            is not transparent.
    
            The GraphicsLayer then uses the 'contentsOpaque' setting to
            determine whether to use font smoothing.
    
            Add pageBackgroundTransparencyChanged() to DrawingArea
            so that the WebPage can inform the DrawingArea when the
            background transparency changes.
    
            * WebProcess/WebPage/DrawingArea.h:
            (WebKit::DrawingArea::pageBackgroundTransparencyChanged):
            (WebKit::DrawingArea::onPageClose):
            * WebProcess/WebPage/LayerBackedDrawingArea.cpp:
            (WebKit::LayerBackedDrawingArea::LayerBackedDrawingArea):
            (WebKit::LayerBackedDrawingArea::pageBackgroundTransparencyChanged):
            * WebProcess/WebPage/LayerBackedDrawingArea.h:
            * WebProcess/WebPage/WebPage.cpp:
            (WebKit::WebPage::setDrawsBackground):
            (WebKit::WebPage::setDrawsTransparentBackground):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/DrawingArea.h
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/DrawingArea.h
@@ -57,8 +57,10 @@ public:
     virtual void setNeedsDisplay(const WebCore::IntRect&) = 0;
     virtual void display() = 0;
 
-    virtual void onPageClose() { }
+    virtual void pageBackgroundTransparencyChanged() { }
 
+    virtual void onPageClose() { }
+    
 #if USE(ACCELERATED_COMPOSITING)
     virtual void attachCompositingContext() = 0;
     virtual void detachCompositingContext() = 0;
