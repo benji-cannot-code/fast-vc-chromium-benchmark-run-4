6b389bdab4e797cc1b31213a4f5095051677f4a6
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 16:04:38 +0000

    Add support for sockets that can listen and accept a connection.
    These sockets allow one connection at a time, however clients can
    connect and disconnect repeatedly.
    
    These are going to be used by Cloud Print, Remoting and
    Automation.
    
    BUG=NONE
    TEST=BUILD
    
    Review URL: http://codereview.chromium.org/5749001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69264 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_unittest.cc
+++ ipc/ipc_message_unittest.cc
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ipc/ipc_message.h"
+
 #include <string.h>
 
 #include "base/scoped_ptr.h"
 #include "base/values.h"
-#include "ipc/ipc_message.h"
 #include "ipc/ipc_message_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
