15748e0caef647636bdb010d332957bdba947b1e
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-16 18:45:43 +0000

    2010-12-16  David Levin  <levin@chromium.org>
            Reviewed by Shinichiro Hamaji.
    
            check-webkit-style unit tests has some duplicate boilerplate code.
            https://bugs.webkit.org/show_bug.cgi?id=49519
    
            * Scripts/webkitpy/style/checkers/cpp.py:
            (update_include_state): Replaced the "io" parameter with the global
            configuration _unit_test_config. This allowed not calling into
            functions at a low level and also not plumbing through the injection
            information through many levels of code.
            (check_for_include_what_you_use): Ditto.
            (process_file_data): Added the ability to set up the unit test config
            to allow for injection.
            * Scripts/webkitpy/style/checkers/cpp_unittest.py:
            (ErrorCollector.__init__): Added support for having a filter for errors.
            (ErrorCollector.__call__): Ditto.
            (CppStyleTestBase.process_file_data): Added the ability to set unit_test_config.
            (CppStyleTestBase.perform_lint): Consolidated logic for the perform functions.
            (CppStyleTestBase.perform_single_line_lint): Replace specific calls to
            functions in the cpp.py with generic processing and a filter that
            indicates what errors should be kept.
            (CppStyleTestBase.perform_multi_line_lint): Ditto.
            (CppStyleTestBase.perform_language_rules_check): Ditto.
            (CppStyleTestBase.perform_function_lengths_check): Ditto.
            (CppStyleTestBase.perform_pass_ptr_check): Ditto.
            (CppStyleTestBase.perform_include_what_you_use): Ditto.
            (CppStyleTest.test_multi_line_comments): Added another
            error message which applies to the test case.
            (CppStyleTest.test_spacing_for_binary_ops): Fixed test
            to not have config.h, since it is processed as a header file.
            (CppStyleTest.test_static_or_global_stlstrings): Fixed variable name
            style and indentation in checked code.
            (OrderOfIncludesTest.test_check_preprocessor_in_include_section):
            Fixed line number.
            (NoNonVirtualDestructorsTest.test_multi_line_declaration_with_error):
            Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp.py
+++ third_party/WebKit/WebKitTools/Scripts/webkitpy/style/checkers/cpp.py
@@ -48,6 +48,8 @@ import string
 import sys
 import unicodedata
 
+# The key to use to provide a class to fake loading a header file.
+INCLUDE_IO_INJECTION_KEY = 'include_header_io'
 
 # Headers that we consider STL headers.
 _STL_HEADERS = frozenset([
@@ -117,6 +119,12 @@ _CONFIG_HEADER = 0
 _PRIMARY_HEADER = 1
 _OTHER_HEADER = 2
 _MOC_HEADER = 3
+
+
+# A dictionary of items customize behavior for unit test. For example,
+# INCLUDE_IO_INJECTION_KEY allows providing a custom io class which allows
+# for faking a header file.
+_unit_test_config = {}
 
 
 # The regexp compilation caching is inlined in all regexp functions for
@@ -2825,6 +2833,7 @@ def update_include_state(filename, include_state, io=codecs):
     Returns:
       True if a header was succesfully added. False otherwise.
     """
+    io = _unit_test_config.get(INCLUDE_IO_INJECTION_KEY, codecs)
     header_file = None
     try:
         header_file = io.open(filename, 'r', 'utf8', 'replace')
@@ -2843,8 +2852,7 @@ def update_include_state(filename, include_state, io=codecs):
     return True
 
 
-def check_for_include_what_you_use(filename, clean_lines, include_state, error,
-                                   io=codecs):
+def check_for_include_what_you_use(filename, clean_lines, include_state, error):
     """Reports for missing stl includes.
 
     This function will output warnings to make sure you are including the headers
@@ -2858,8 +2866,6 @@ def check_for_include_what_you_use(filename, clean_lines, include_state, error,
       clean_lines: A CleansedLines instance containing the file.
       include_state: An _IncludeState instance.
       error: The function to call with any errors found.
-      io: The IO factory to use to read the header file. Provided for unittest
-          injection.
     """
     required = {}  # A map of header name to line_number and the template entity.
         # Example of required: { '<functional>': (1219, 'less<>') }
@@ -2910,7 +2916,7 @@ def check_for_include_what_you_use(filename, clean_lines, include_state, error,
     for header in include_state.keys():  #NOLINT
         (same_module, common_path) = files_belong_to_same_module(abs_filename, header)
         fullpath = common_path + header
-        if same_module and update_include_state(fullpath, include_state, io):
+        if same_module and update_include_state(fullpath, include_state):
             header_found = True
 
     # If we can't find the header file for a .cpp, assume it's because we don't
@@ -3122,6 +3128,9 @@ class CppChecker(object):
 
 
 # FIXME: Remove this function (requires refactoring unit tests).
-def process_file_data(filename, file_extension, lines, error, min_confidence):
+def process_file_data(filename, file_extension, lines, error, min_confidence, unit_test_config):
+    global _unit_test_config
+    _unit_test_config = unit_test_config
     checker = CppChecker(filename, file_extension, error, min_confidence)
     checker.check(lines)
+    _unit_test_config = {}
