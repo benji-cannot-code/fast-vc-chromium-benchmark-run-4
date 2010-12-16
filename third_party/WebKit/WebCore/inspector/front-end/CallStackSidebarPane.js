580891b76f702a145b449a061d135c88c63b7be9
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-16 15:57:47 +0000

    2010-12-16  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: update CallStackSidebarPane placards and status message in the same event handler.
            https://bugs.webkit.org/show_bug.cgi?id=51174
    
            * inspector/front-end/BreakpointManager.js:
            (WebInspector.BreakpointManager.prototype.breakpointViewForEventData):
            (WebInspector.BreakpointManager.prototype._debuggerPaused):
            (WebInspector.BreakpointManager.prototype._debuggerResumed):
            * inspector/front-end/CallStackSidebarPane.js:
            (WebInspector.CallStackSidebarPane):
            (WebInspector.CallStackSidebarPane.prototype.update):
            (WebInspector.CallStackSidebarPane.prototype.registerShortcuts):
            * inspector/front-end/DebuggerModel.js:
            (WebInspector.DebuggerModel.prototype.pausedScript):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype._debuggerPaused):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/CallStackSidebarPane.js
+++ third_party/WebKit/WebCore/inspector/front-end/CallStackSidebarPane.js
@@ -27,12 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.CallStackSidebarPane = function()
 {
     WebInspector.SidebarPane.call(this, WebInspector.UIString("Call Stack"));
-    WebInspector.breakpointManager.addEventListener(WebInspector.BreakpointManager.Events.NativeBreakpointHit, this._nativeBreakpointHit, this);
-    WebInspector.debuggerModel.addEventListener(WebInspector.DebuggerModel.Events.BreakpointHit, this._scriptBreakpointHit, this);
 }
 
 WebInspector.CallStackSidebarPane.prototype = {
-    update: function(callFrames)
+    update: function(callFrames, eventType, eventData)
     {
         this.bodyElement.removeChildren();
 
@@ -83,6 +81,19 @@ WebInspector.CallStackSidebarPane.prototype = {
             this.placards.push(placard);
             this.bodyElement.appendChild(placard.element);
         }
+
+        if (eventType === WebInspector.DebuggerEventTypes.JavaScriptPause)
+            return;
+
+        var statusMessageElement = document.createElement("div");
+        statusMessageElement.className = "info";
+        if (eventType === WebInspector.DebuggerEventTypes.NativeBreakpoint) {
+            var breakpoint = WebInspector.breakpointManager.breakpointViewForEventData(eventData);
+            if (breakpoint)
+                breakpoint.populateStatusMessageElement(statusMessageElement, eventData);
+        } else
+            statusMessageElement.appendChild(document.createTextNode(WebInspector.UIString("Paused on a JavaScript breakpoint.")));
+        this.bodyElement.appendChild(statusMessageElement);
     },
 
     get selectedCallFrame()
@@ -170,23 +181,6 @@ WebInspector.CallStackSidebarPane.prototype = {
         this._shortcuts[prevCallFrame.key] = this._selectPreviousCallFrameOnStack.bind(this);
 
         section.addRelatedKeys([ nextCallFrame.name, prevCallFrame.name ], WebInspector.UIString("Next/previous call frame"));
-    },
-
-    _nativeBreakpointHit:  function(event)
-    {
-        var breakpoint = event.data.breakpoint;
-        var statusMessageElement = document.createElement("div");
-        statusMessageElement.className = "info";
-        breakpoint.populateStatusMessageElement(statusMessageElement, event.data.eventData);
-        this.bodyElement.appendChild(statusMessageElement);
-    },
-
-    _scriptBreakpointHit:  function(event)
-    {
-        var statusMessageElement = document.createElement("div");
-        statusMessageElement.className = "info";
-        statusMessageElement.appendChild(document.createTextNode(WebInspector.UIString("Paused on a JavaScript breakpoint.")));
-        this.bodyElement.appendChild(statusMessageElement);
     }
 }
 
