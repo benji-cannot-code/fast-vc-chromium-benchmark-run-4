45a7082358031c6b9ee45bad5611d3c30dfe93f6
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-17 18:11:24 +0000

    [cros] Fix left/right arrow pod navigation on login. Don't initiate sign in process if password is empty.
    BUG=chromium-os:10362
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/5918004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/new_user_view.cc
+++ chrome/browser/chromeos/login/new_user_view.cc
@@ -59,7 +59,8 @@ const char kDefaultDomain[] = "@gmail.com";
 // username doesn't have full domain.
 class UsernameField : public chromeos::TextfieldWithMargin {
  public:
-  UsernameField() {}
+  explicit UsernameField(chromeos::NewUserView* controller)
+      : controller_(controller) {}
 
   // views::Textfield overrides:
   virtual void WillLoseFocus() {
@@ -73,6 +74,16 @@ class UsernameField : public chromeos::TextfieldWithMargin {
     }
   }
 
+  // Overridden from views::View:
+  virtual bool OnKeyPressed(const views::KeyEvent& e) {
+    if (e.GetKeyCode() == app::VKEY_LEFT) {
+      return controller_->NavigateAway();
+    }
+    return false;
+  }
+
+ private:
+  chromeos::NewUserView* controller_;
   DISALLOW_COPY_AND_ASSIGN(UsernameField);
 };
 
@@ -149,7 +160,7 @@ void NewUserView::Init() {
   splitter_down1_ = CreateSplitter(kSplitterDown1Color);
   splitter_down2_ = CreateSplitter(kSplitterDown2Color);
 
-  username_field_ = new UsernameField();
+  username_field_ = new UsernameField(this);
   username_field_->set_background(new CopyBackground(this));
   username_field_->SetAccessibleName(
       ASCIIToWide(l10n_util::GetStringUTF8(IDS_CHROMEOS_ACC_USERNAME_LABEL)));
@@ -511,16 +522,10 @@ bool NewUserView::HandleKeystroke(views::Textfield* s,
     return false;
 
   if (keystroke.GetKeyboardCode() == app::VKEY_RETURN) {
-    Login();
+    if (!username_field_->text().empty() && !password_field_->text().empty())
+      Login();
     // Return true so that processing ends
     return true;
-  } else if (keystroke.GetKeyboardCode() == app::VKEY_LEFT) {
-    if (s == username_field_ &&
-        username_field_->text().empty() &&
-        password_field_->text().empty()) {
-      delegate_->NavigateAway();
-      return true;
-    }
   }
   delegate_->ClearErrors();
   // Return false so that processing does not end
@@ -542,6 +547,16 @@ void NewUserView::EnableInputControls(bool enabled) {
   }
   if (need_guest_link_) {
     guest_link_->SetEnabled(enabled);
+  }
+}
+
+bool NewUserView::NavigateAway() {
+  if (username_field_->text().empty() &&
+      password_field_->text().empty()) {
+    delegate_->NavigateAway();
+    return true;
+  } else {
+    return false;
   }
 }
 
