231d8f2be07ef6680f9b67e9c30927f9c9a21c3d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-17 23:28:01 +0000

    Merge ViewHostMsg_AllocatePDFTransport with ViewHostMsg_AllocateSharedMemoryBuffer.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/5961002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69589 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_message_filter.cc
+++ chrome/browser/renderer_host/render_message_filter.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/renderer_host/render_message_filter.h"
 
+#include <map>
+
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/metrics/histogram.h"
@@ -392,10 +394,6 @@ bool RenderMessageFilter::OnMessageReceived(const IPC::Message& message,
 #if defined(OS_WIN)
       IPC_MESSAGE_HANDLER(ViewHostMsg_DuplicateSection, OnDuplicateSection)
 #endif
-#if defined(OS_MACOSX)
-      IPC_MESSAGE_HANDLER(ViewHostMsg_AllocatePDFTransport,
-                          OnAllocateSharedMemoryBuffer)
-#endif
 #if defined(OS_POSIX)
       IPC_MESSAGE_HANDLER(ViewHostMsg_AllocateSharedMemoryBuffer,
                           OnAllocateSharedMemoryBuffer)
@@ -625,7 +623,7 @@ void RenderMessageFilter::OnCookiesEnabled(
         url, first_party_for_cookies, callback);
     if (policy == net::ERR_IO_PENDING) {
       Send(new ViewMsg_SignalCookiePromptEvent());
-      return; // CanGetCookies will take care to call our callback in this case.
+      return;  // CanGetCookies will call our callback in this case.
     }
   }
   callback->Run(policy);
--- chrome/common/render_messages_internal.h
+++ chrome/common/render_messages_internal.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -1941,14 +1942,6 @@ IPC_SYNC_MESSAGE_CONTROL0_2(ViewHostMsg_AllocateTempFileForPrinting,
                             int /* fd in browser*/)
 IPC_MESSAGE_CONTROL1(ViewHostMsg_TempFileForPrintingWritten,
                      int /* fd in browser */)
-#endif
-
-#if defined(OS_MACOSX)
-// Asks the browser to create a block of shared memory for the renderer to
-// pass NativeMetafile data to the browser.
-IPC_SYNC_MESSAGE_ROUTED1_1(ViewHostMsg_AllocatePDFTransport,
-                           uint32 /* buffer size */,
-                           base::SharedMemoryHandle /* browser handle */)
 #endif
 
 #if defined(OS_POSIX)
