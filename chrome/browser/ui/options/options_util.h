81fc6d54be7d8615b96ea7c25fb4f786f9d3c39d
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-17 22:17:57 +0000

    DOMUI Settings: UTH: Remove the 'Reset to Defaults' button.
    BUG=66115
    TEST=none
    
    Review URL: http://codereview.chromium.org/6017002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/options/options_util.h
+++ chrome/browser/ui/options/options_util.h
@@ -13,6 +13,8 @@ class Profile;
 
 class OptionsUtil {
  public:
+  // TODO(kmadhusu): Remove "ResetToDefaults" function after platform-specific
+  // dialogs are removed.
   // Resets all prefs to their default values.
   static void ResetToDefaults(Profile* profile);
 
