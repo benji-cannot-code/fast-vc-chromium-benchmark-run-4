bb0fc448af2d34e81784ba42b67f255b76749075
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-17 15:54:44 +0000

    2010-12-17  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r74201.
            http://trac.webkit.org/changeset/74201
            https://bugs.webkit.org/show_bug.cgi?id=51248
    
            It broke ~6000 layout tests (Requested by Ossy_ on #webkit).
    
            * UIProcess/API/qt/qgraphicswkview.cpp:
            (QGraphicsWKView::setGeometry):
            (QGraphicsWKView::itemChange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/qt/qgraphicswkview.cpp
+++ third_party/WebKit/WebKit2/UIProcess/API/qt/qgraphicswkview.cpp
@@ -48,7 +48,6 @@ struct QGraphicsWKViewPrivate {
     QGraphicsWKViewPrivate(QGraphicsWKView* view);
     WKPageRef pageRef() const { return page->pageRef(); }
 
-    void updateViewportSize();
     void onScaleChanged();
     void commitScale();
 
@@ -57,13 +56,6 @@ struct QGraphicsWKViewPrivate {
     RunLoop::Timer<QGraphicsWKViewPrivate> m_scaleCommitTimer;
     bool m_isChangingScale;
 };
-
-void QGraphicsWKViewPrivate::updateViewportSize()
-{
-    // NOTE: call geometry() as setGeometry ensures that
-    // the geometry is within legal bounds (minimumSize, maximumSize)
-    page->setViewportSize(q->geometry().size().toSize());
-}
 
 QGraphicsWKView::QGraphicsWKView(QWKContext* context, BackingStoreType backingStoreType, QGraphicsItem* parent)
     : QGraphicsWidget(parent)
@@ -122,12 +114,9 @@ void QGraphicsWKView::setGeometry(const QRectF& rect)
     if (geometry().size() == oldSize)
         return;
 
-    // Return early if not visible, since setting size on drawing
-    // areas when not visible is not supported.
-    if (!isVisible())
-        return;
-
-    d->updateViewportSize();
+    // NOTE: call geometry() as setGeometry ensures that
+    // the geometry is within legal bounds (minimumSize, maximumSize)
+    page()->setViewportSize(geometry().size().toSize());
 }
 
 void QGraphicsWKView::load(const QUrl& url)
@@ -221,19 +210,7 @@ bool QGraphicsWKView::focusNextPrevChild(bool next)
 */
 QVariant QGraphicsWKView::itemChange(GraphicsItemChange change, const QVariant& value)
 {
-    if (change == ItemVisibleChange) {
-        if (value.canConvert<bool>()) {
-            DrawingAreaProxy* drawingArea = page()->d->page->drawingArea();
-            bool shouldBeVisible = value.toBool();
-            drawingArea->setPageIsVisible(shouldBeVisible);
-
-            // This item might have been resized during being
-            // invisible. Update the size in any case.
-            if (shouldBeVisible)
-                d->updateViewportSize();
-        }
-        return value;
-    }
+    // Here so that it can be reimplemented without breaking ABI.
     return QGraphicsWidget::itemChange(change, value);
 }
 
