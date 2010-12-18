03b409655a76c66cbf7db5832314a3e16b13562b
suzhe@google.com <suzhe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-18 02:42:07 +0000

    Accepts current instant preview with Tab key.
    This CL adds a new test InstantTest.TabKey and also fixes (hopefully) InstantTest.OnSubmitEvent on Windows and InstantTest.OnCancelEvent on Mac.
    
    BUG=62940
    BUG=64696
    BUG=66768
    TEST=See bug report.
    
    Review URL: http://codereview.chromium.org/5811002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_edit_view_mac.mm
@@ -811,6 +811,9 @@ bool AutocompleteEditViewMac::OnDoCommandBySelector(SEL cmd) {
       controller_->OnCommitSuggestedText(GetText());
       return true;
     }
+
+    if (controller_->AcceptCurrentInstantPreview())
+      return true;
   }
 
   // |-noop:| is sent when the user presses Cmd+Return. Override the no-op
