09cf91d28e4956f49f912233612c4c67c74d9a32
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-18 18:44:48 +0000

    2010-12-18  Helder Correia  <helder@sencha.com> and Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Ariya Hidayat.
    
            [Qt] Canvas shadow offset should not be affected by any transformation
            https://bugs.webkit.org/show_bug.cgi?id=50422
    
            On a canvas context, shadows are currently affected by all
            transformations except scaling. According to the spec:
            http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#shadows
    
            "The shadowOffsetX and shadowOffsetY attributes specify the distance
            that the shadow will be offset in the positive horizontal and positive
            vertical distance respectively. Their values are in coordinate space
            units. They are not affected by the current transformation matrix."
    
            NOTE: this applies only to canvas, not to box shadows.
    
            Add new test to ensure that shadows are correctly transformed keeping
            the relative offset to the shape.
    
            * fast/canvas/canvas-scale-strokePath-shadow-expected.txt: See strokePath below.
            * fast/canvas/canvas-transforms-fillRect-shadow-expected.txt: Added.
            * fast/canvas/canvas-transforms-fillRect-shadow.html: Added.
            * fast/canvas/script-tests/canvas-scale-fillPath-shadow.js:
            * fast/canvas/script-tests/canvas-scale-fillRect-shadow.js:
            * fast/canvas/script-tests/canvas-scale-strokePath-shadow.js: Now using
              a lineWidth > 1 to make it easier to test and more fair among all
              ports, since there can be different transformation smoothness or
              aliasing settings.
            * fast/canvas/script-tests/canvas-transforms-fillRect-shadow.js: Added.
            * platform/gtk/Skipped: GTK port needs to implement this feature.
            * platform/gtk/fast/canvas/canvas-scale-fillPath-shadow-expected.txt: Removed.
            * platform/gtk/fast/canvas/canvas-scale-fillRect-shadow-expected.txt: Removed.
            * platform/gtk/fast/canvas/canvas-scale-strokePath-shadow-expected.txt: Removed.
    2010-12-18  Helder Correia  <helder@sencha.com> and Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Ariya Hidayat.
    
            [Qt] Canvas shadow offset should not be affected by any transformation
            https://bugs.webkit.org/show_bug.cgi?id=50422
    
            On a canvas context, shadows are currently affected by all
            transformations except scaling. According to the spec:
            http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#shadows
    
            "The shadowOffsetX and shadowOffsetY attributes specify the distance
            that the shadow will be offset in the positive horizontal and positive
            vertical distance respectively. Their values are in coordinate space
            units. They are not affected by the current transformation matrix."
    
            NOTE: this applies only to canvas, not to box shadows.
    
            Test: fast/canvas/canvas-transforms-fillRect-shadow.html
    
            * platform/graphics/ContextShadow.cpp:
            (WebCore::ContextShadow::ContextShadow):
            (WebCore::ContextShadow::calculateLayerBoundingRect):
            * platform/graphics/ContextShadow.h:
            (WebCore::ContextShadow::setShadowsIgnoreTransforms):
            (WebCore::ContextShadow::shadowsIgnoreTransforms):
            (WebCore::ContextShadow::offset):
            * platform/graphics/qt/ContextShadowQt.cpp:
            (WebCore::ContextShadow::beginShadowLayer):
            (WebCore::ContextShadow::endShadowLayer):
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::mustUseContextShadow):
            (WebCore::GraphicsContext::fillPath):
            (WebCore::GraphicsContext::strokePath):
            (WebCore::GraphicsContext::fillRect):
            (WebCore::GraphicsContext::fillRoundedRect):
            (WebCore::GraphicsContext::setPlatformShadow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-transforms-fillRect-shadow.js
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-transforms-fillRect-shadow.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Ensure correct behavior of canvas with path stroke + shadow after scaling. A blue and red checkered pattern should be displayed.");
+description("Ensure correct behavior of canvas with fillRect+shadow after translation+rotation+scaling. A blue and red checkered pattern should be displayed.");
 
 function print(message, color)
 {
@@ -20,7 +20,7 @@ function shouldBeAround(a, b)
         evalA = e;
     }
 
-    if (Math.abs(evalA - b) < 20)
+    if (Math.abs(evalA - b) < 10)
         print("PASS " + a + " is around " + b , "green")
     else
         print("FAIL " + a + " is not around " + b + " (actual: " + evalA + ")", "red");
@@ -28,129 +28,107 @@ function shouldBeAround(a, b)
 
 var canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
-canvas.setAttribute('width', '1000');
-canvas.setAttribute('height', '1000');
+canvas.setAttribute('width', '600');
+canvas.setAttribute('height', '600');
 var ctx = canvas.getContext('2d');
 
-ctx.scale(2, 2);
+ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
 ctx.shadowOffsetX = 100;
 ctx.shadowOffsetY = 100;
-ctx.strokeStyle = 'rgba(0, 0, 255, 1)';
-ctx.lineWidth = 5;
+
+ctx.translate(-100, -100);
+ctx.rotate(Math.PI/2);
+ctx.scale(2, 2);
 
 ctx.shadowColor = 'rgba(255, 0, 0, 1.0)';
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(50, 100);
-ctx.lineTo(50, 50);
-ctx.stroke();
+ctx.fillRect(100, -150, 50, 50);
 
-ctx.shadowColor = 'rgba(255, 0, 0, 0.3)';
-ctx.beginPath();
-ctx.moveTo(50, 150);
-ctx.lineTo(100, 150);
-ctx.lineTo(100, 200);
-ctx.lineTo(50, 200);
-ctx.lineTo(50, 150);
-ctx.stroke();
+ctx.shadowColor = 'rgba(255, 0, 0, 0.5)';
+ctx.fillRect(200, -150, 50, 50);
 
+ctx.shadowBlur = 5;
 ctx.shadowColor = 'rgba(255, 0, 0, 1.0)';
-ctx.shadowBlur = 10;
-ctx.beginPath();
-ctx.moveTo(150, 50);
-ctx.lineTo(200, 50);
-ctx.lineTo(200, 100);
-ctx.lineTo(150, 100);
-ctx.lineTo(150, 50);
-ctx.stroke();
+ctx.fillRect(100, -250, 50, 50);
 
-ctx.shadowColor = 'rgba(255, 0, 0, 0.6)';
-ctx.beginPath();
-ctx.moveTo(150, 150);
-ctx.lineTo(200, 150);
-ctx.lineTo(200, 200);
-ctx.lineTo(150, 200);
-ctx.lineTo(150, 150);
-ctx.stroke();
+ctx.shadowColor = 'rgba(255, 0, 0, 0.5)';
+ctx.fillRect(200, -250, 50, 50);
 
 var d; // imageData.data
 
 // Verify solid shadow.
-d = ctx.getImageData(250, 200, 1, 1).data;
+d = ctx.getImageData(201, 205, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
 shouldBe('d[3]', '255');
 
-d = ctx.getImageData(300, 290, 1, 1).data;
+d = ctx.getImageData(298, 298, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
 shouldBe('d[3]', '255');
 
-d = ctx.getImageData(200, 250, 1, 1).data;
+d = ctx.getImageData(201, 298, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
 shouldBe('d[3]', '255');
 
 // Verify solid alpha shadow.
-d = ctx.getImageData(201, 405, 1, 1).data;
+d = ctx.getImageData(201, 401, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '76');
+shouldBeAround('d[3]', '127');
 
-d = ctx.getImageData(201, 500, 1, 1).data;
+d = ctx.getImageData(298, 450, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '76');
+shouldBeAround('d[3]', '127');
 
-d = ctx.getImageData(300, 499, 1, 1).data;
+d = ctx.getImageData(205, 498, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '76');
+shouldBeAround('d[3]', '127');
 
 // Verify blurry shadow.
-d = ctx.getImageData(404, 210, 1, 1).data;
+d = ctx.getImageData(399, 205, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '149');
+shouldBeAround('d[3]', '106');
 
-d = ctx.getImageData(505, 250, 1, 1).data;
+d = ctx.getImageData(500, 205, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '116');
+shouldBeAround('d[3]', '106');
 
-d = ctx.getImageData(450, 205, 1, 1).data;
+d = ctx.getImageData(499, 299, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '115');
+shouldBeAround('d[3]', '83');
 
 // Verify blurry alpha shadow.
-d = ctx.getImageData(505, 450, 1, 1).data;
+d = ctx.getImageData(398, 405, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '70');
+shouldBeAround('d[3]', '36');
 
-d = ctx.getImageData(505, 450, 1, 1).data;
+d = ctx.getImageData(405, 501, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '70');
+shouldBeAround('d[3]', '36');
 
-d = ctx.getImageData(450, 405, 1, 1).data;
+d = ctx.getImageData(405, 501, 1, 1).data;
 shouldBe('d[0]', '255');
 shouldBe('d[1]', '0');
 shouldBe('d[2]', '0');
-shouldBeAround('d[3]', '69');
+shouldBeAround('d[3]', '36');
 
 var successfullyParsed = true;
