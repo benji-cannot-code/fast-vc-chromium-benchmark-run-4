08ffcbe2513a2ddb95e35a80f36b03377033da28
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-18 09:23:56 +0000

    2010-12-18  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Laszlo Gombos.
    
            [Web Timing] Rename domContentLoaded{Start,End}->domContentLoadedEvent{Start,End}
            https://bugs.webkit.org/show_bug.cgi?id=50943
    
            * fast/dom/Window/window-properties-performance-expected.txt:
            * fast/dom/script-tests/webtiming.js:
            (checkTimingBeforeLoad):
            (checkTimingWhileDeferred):
            (checkWebTimingOnDOMContentLoaded):
            (checkWebTimingWhileAsync):
            (checkWebTimingOnLoad):
            (checkWebTimingAfterLoad):
            * fast/dom/webtiming-document-open-expected.txt:
            * fast/dom/webtiming-expected.txt:
            * fast/dom/webtiming-navigate-within-document-expected.txt:
    2010-12-18  Tony Gentilcore  <tonyg@chromium.org>
    
            Reviewed by Laszlo Gombos.
    
            [Web Timing] Rename domContentLoaded{Start,End}->domContentLoadedEvent{Start,End}
            https://bugs.webkit.org/show_bug.cgi?id=50943
    
            See: http://test.w3.org/webperf/specs/NavigationTiming/#nt-dom-content-event-start
    
            * dom/Document.cpp:
            (WebCore::Document::finishedParsing):
            * dom/DocumentTiming.h:
            (WebCore::DocumentTiming::DocumentTiming):
            * page/PerformanceTiming.cpp:
            (WebCore::PerformanceTiming::domContentLoadedEventStart):
            (WebCore::PerformanceTiming::domContentLoadedEventEnd):
            * page/PerformanceTiming.h:
            * page/PerformanceTiming.idl:
    2010-12-18  Tony Gentilcore  <tonyg@chromium.org>
    
            Reviewed by Laszlo Gombos.
    
            [Web Timing] Rename domContentLoaded{Start,End}->domContentLoadedEvent{Start,End}
            https://bugs.webkit.org/show_bug.cgi?id=50943
    
            Exposes all dom* times to the chromium port. I'm particularly interested in
            domContentLoadedEventEnd as it compares to the FinishDoc metric.
    
            * public/WebPerformance.h:
            * src/WebPerformance.cpp:
            (WebKit::WebPerformance::domLoading):
            (WebKit::WebPerformance::domInteractive):
            (WebKit::WebPerformance::domContentLoadedEventStart):
            (WebKit::WebPerformance::domContentLoadedEventEnd):
            (WebKit::WebPerformance::domComplete):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/webtiming.js
@@ -43,8 +43,8 @@ function checkTimingBeforeLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBe("timing.domInteractive", "0");
-    shouldBe("timing.domContentLoadedStart", "0");
-    shouldBe("timing.domContentLoadedEnd", "0");
+    shouldBe("timing.domContentLoadedEventStart", "0");
+    shouldBe("timing.domContentLoadedEventEnd", "0");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -78,8 +78,8 @@ function checkTimingWhileDeferred()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.domLoading");
-    shouldBe("timing.domContentLoadedStart", "0");
-    shouldBe("timing.domContentLoadedEnd", "0");
+    shouldBe("timing.domContentLoadedEventStart", "0");
+    shouldBe("timing.domContentLoadedEventEnd", "0");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -114,8 +114,8 @@ function checkWebTimingOnDOMContentLoaded() {
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.domLoading");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
-    shouldBe("timing.domContentLoadedEnd", "0");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventStart", "timing.domInteractive");
+    shouldBe("timing.domContentLoadedEventEnd", "0");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -156,8 +156,8 @@ function checkWebTimingWhileAsync()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventEnd", "timing.domContentLoadedEventStart");
     shouldBe("timing.domComplete", "0");
 
     shouldBe("timing.loadEventStart", "0");
@@ -194,9 +194,9 @@ function checkWebTimingOnLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
-    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEnd");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventEnd", "timing.domContentLoadedEventStart");
+    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEventEnd");
 
     shouldBeGreaterThanOrEqual("timing.loadEventStart", "timing.responseEnd");
     shouldBe("timing.loadEventEnd", "0");
@@ -232,9 +232,9 @@ function checkWebTimingAfterLoad()
 
     shouldBeGreaterThanOrEqual("timing.domLoading", "timing.fetchStart");
     shouldBeGreaterThanOrEqual("timing.domInteractive", "timing.responseEnd");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedStart", "timing.domInteractive");
-    shouldBeGreaterThanOrEqual("timing.domContentLoadedEnd", "timing.domContentLoadedStart");
-    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEnd");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventStart", "timing.domInteractive");
+    shouldBeGreaterThanOrEqual("timing.domContentLoadedEventEnd", "timing.domContentLoadedEventStart");
+    shouldBeGreaterThanOrEqual("timing.domComplete", "timing.domContentLoadedEventEnd");
 
     shouldBeGreaterThanOrEqual("timing.loadEventStart", "timing.responseEnd");
     shouldBeGreaterThanOrEqual("timing.loadEventEnd", "timing.loadEventStart + 50");
