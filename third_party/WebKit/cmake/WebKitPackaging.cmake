1b9670d2a550320a77f59e2070653a97f0b1961c
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-18 00:02:12 +0000

    Rename WebKitTools to Tools https://bugs.webkit.org/show_bug.cgi?id=49861
    Reviewed by Simon Fraser.
    
    .: 
    
    * DerivedSources.pro:
    * GNUmakefile.am:
    * MakeFile:
    * MakeFile.shared:
    * Tools: Renamed from WebKitTools.
    * WebKit.pro:
    * cmake/WebKitPackaging.cmake:
    * wscript:
    
    BugsSite: 
    
    * PrettyPatch/PrettyPatch.rb:
    * committers-autocomplete.js:
    
    Tools: 
    
    * BuildSlaveSupport/build.webkit.org-config/master.cfg:
    * CodeCoverage/README:
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::DumpRenderTree::initializeFonts):
    * EWSTools/start-commit-queue.sh:
    * EWSTools/start-queue.sh:
    * GNUmakefile.am:
    * MIDLWrapper/MIDLWrapper.cpp:
    (wmain):
    * MiniBrowser/qt/MiniBrowser.pro:
    * Scripts/build-api-tests:
    * Scripts/build-dumprendertree:
    * Scripts/build-webkit:
    * Scripts/build-webkittestrunner:
    * Scripts/generate-coverage-data:
    * Scripts/old-run-webkit-tests:
    * Scripts/run-api-tests:
    * Scripts/run-iexploder-tests:
    * Scripts/run-javascriptcore-tests:
    * Scripts/run-mangleme-tests:
    * Scripts/run-sunspider:
    * Scripts/run-webkit-websocketserver:
    * Scripts/sunspider-compare-results:
    * Scripts/test-webkitperl:
    * Scripts/test-webkitpy:
    * Scripts/update-iexploder-cssproperties:
    * Scripts/update-webkit:
    * Scripts/update-webkit-localizable-strings:
    * Scripts/webkitdirs.pm:
    * Scripts/webkitpy/common/checkout/scm.py:
    * Scripts/webkitpy/common/checkout/scm_unittest.py:
    * Scripts/webkitpy/common/config/committervalidator.py:
    * Scripts/webkitpy/common/config/committervalidator_unittest.py:
    * Scripts/webkitpy/common/config/ports.py:
    * Scripts/webkitpy/common/config/ports_unittest.py:
    * Scripts/webkitpy/common/system/logutils_unittest.py:
    * Scripts/webkitpy/common/system/ospath_unittest.py:
    * Scripts/webkitpy/layout_tests/port/chromium.py:
    * Scripts/webkitpy/layout_tests/port/config.py:
    * Scripts/webkitpy/layout_tests/port/config_unittest.py:
    * Scripts/webkitpy/layout_tests/port/http_server.py:
    * Scripts/webkitpy/layout_tests/port/test.py:
    * Scripts/webkitpy/style/checker.py:
    * Scripts/webkitpy/style/checker_unittest.py:
    * Scripts/webkitpy/tool/bot/feeders_unittest.py:
    * Scripts/webkitpy/tool/commands/queues_unittest.py:
    * Scripts/webkitpy/tool/steps/steps_unittest.py:
    * WebKitTestRunner/DerivedSources.pro:
    * WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp:
    (WTR::activateFonts):
    * WebKitTestRunner/InjectedBundle/qt/InjectedBundle.pro:
    * wx/build/build_utils.py:
    
    WebKitSite: 
    
    * asking_questions.html:
    * building/tools.html:
    * coding/scripts.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/cmake/WebKitPackaging.cmake
+++ third_party/WebKit/cmake/WebKitPackaging.cmake
@@ -75,7 +75,7 @@ IF(NOT DEFINED CPACK_SOURCE_IGNORE_FILES)
     "/cmake-build/"
     "/build/"
     "/WebKitBuild/"
-    "/WebKitTools/Scripts/webkitpy/thirdparty/autoinstalled/"
+    "/Tools/Scripts/webkitpy/thirdparty/autoinstalled/"
     )
 
   IF(NOT WEBKIT_CPACK_ADD_TESTS)
@@ -89,7 +89,7 @@ IF(NOT DEFINED CPACK_SOURCE_IGNORE_FILES)
 
   IF(NOT WEBKIT_CPACK_ADD_TOOLS)
     LIST(APPEND CPACK_SOURCE_IGNORE_FILES
-      "/WebKitTools/"
+      "/Tools/"
       "/manual-tools/"
       "/tools/"
       "/PageLoadTools/"
