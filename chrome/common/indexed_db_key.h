2df5abbe011d66d8dfb2b65408544c7ccaa74d32
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-18 10:33:59 +0000

    IndexedDB: Support for Date object keys.
    Depends on WebKit r74266.
    
    BUG=64060
    TEST=browser_tests
    
    Review URL: http://codereview.chromium.org/5992002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69641 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/indexed_db_key.h
+++ chrome/common/indexed_db_key.h
@@ -19,12 +19,14 @@ class IndexedDBKey {
 
   void SetNull();
   void SetInvalid();
-  void Set(const string16& string);
-  void Set(double number);
+  void SetString(const string16& string);
+  void SetDate(double date);
+  void SetNumber(double number);
   void Set(const WebKit::WebIDBKey& key);
 
   WebKit::WebIDBKey::Type type() const { return type_; }
   const string16& string() const { return string_; }
+  double date() const { return date_; }
   double number() const { return number_; }
 
   operator WebKit::WebIDBKey() const;
@@ -32,6 +34,7 @@ class IndexedDBKey {
  private:
   WebKit::WebIDBKey::Type type_;
   string16 string_;
+  double date_;
   double number_;
 };
 
