33b2814ab0d0ec5deaf72ca277c725fdb08a91d5
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-19 20:18:03 +0000

    Attempt to fix the chrome frame net test failures on the try server. A DCHECK indicating that the RegisterDragDrop API which registers the drop target as part of the chrome widget initialization fails with E_OUTOFMEMORY.
    Reason the chrome frame net tests were using CoInitializeEx to initialize the COM apartment.
    As per docs we need to call OleInitialize.
    
    BUG=none
    TEST=chrome frame net tests should pass on the try server.
    TBR=amit
    
    Review URL: http://codereview.chromium.org/6026003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -344,7 +344,6 @@ void CFUrlRequestUnittestRunner::OnInitialTabLoaded() {
 void CFUrlRequestUnittestRunner::RunMainUIThread() {
   DCHECK(MessageLoop::current());
   DCHECK(MessageLoop::current()->type() == MessageLoop::TYPE_UI);
-  OleInitialize(NULL);
   MessageLoop::current()->Run();
 }
 
@@ -468,6 +467,14 @@ void FilterDisabledTests() {
 // We need a module since some of the accessibility code that gets pulled
 // in here uses ATL.
 class ObligatoryModule: public CAtlExeModuleT<ObligatoryModule> {
+ public:
+  static HRESULT InitializeCom() {
+    return OleInitialize(NULL);
+  }
+
+  static void UninitializeCom() {
+    OleUninitialize();
+  }
 };
 
 ObligatoryModule g_obligatory_atl_module;
