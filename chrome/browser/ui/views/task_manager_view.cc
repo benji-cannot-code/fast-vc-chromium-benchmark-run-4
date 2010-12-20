ef79c1ad6fda9eb3e1182941b25ebc364ba2c01c
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 20:59:16 +0000

    Turn off groups in task manager, as we're rolling out that functionality in favor of a larger change in a future release.
    BUG=63222
    TEST=Bring up task manager, see that it has no groups.
    
    Review URL: http://codereview.chromium.org/5865005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/task_manager_view.cc
+++ chrome/browser/ui/views/task_manager_view.cc
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static const int kDefaultWidth = 460;
 static const int kDefaultHeight = 270;
 
-// The group IDs used to separate background pages from foreground tabs.
-static const int kBackgroundGroupId = 0;
-static const int kForegroundGroupId = 1;
-
 namespace {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -65,13 +61,9 @@ class TaskManagerTableModel : public views::GroupTableModel,
   int RowCount();
   std::wstring GetText(int row, int column);
   SkBitmap GetIcon(int row);
-  bool ShouldIndent(int row);
   void GetGroupRangeForItem(int item, views::GroupRange* range);
   void SetObserver(TableModelObserver* observer);
   virtual int CompareValues(int row1, int row2, int column_id);
-  virtual Groups GetGroups();
-  virtual bool HasGroups();
-  virtual int GetGroupID(int row);
 
   // TaskManagerModelObserver.
   virtual void OnModelChanged();
@@ -159,9 +151,6 @@ SkBitmap TaskManagerTableModel::GetIcon(int row) {
   return model_->GetResourceIcon(row);
 }
 
-bool TaskManagerTableModel::ShouldIndent(int row) {
-  return !model_->IsResourceFirstInGroup(row);
-}
 
 void TaskManagerTableModel::GetGroupRangeForItem(int item,
                                                  views::GroupRange* range) {
@@ -176,41 +165,6 @@ void TaskManagerTableModel::SetObserver(TableModelObserver* observer) {
 
 int TaskManagerTableModel::CompareValues(int row1, int row2, int column_id) {
   return model_->CompareValues(row1, row2, column_id);
-}
-
-bool TaskManagerTableModel::HasGroups() {
-  return true;
-}
-
-int TaskManagerTableModel::GetGroupID(int row) {
-  // If there are any background resources in the group range, put the whole
-  // range in the background group.
-  std::pair<int, int> range_pair = model_->GetGroupRangeForResource(row);
-  for (int i = range_pair.first;
-       i < range_pair.first + range_pair.second;
-       ++i) {
-    if (model_->IsBackgroundResource(i))
-      return kBackgroundGroupId;
-  }
-  return kForegroundGroupId;
-}
-
-TableModel::Groups TaskManagerTableModel::GetGroups() {
-  Groups groups;
-
-  Group background_group;
-  background_group.title =
-      l10n_util::GetString(IDS_TASK_MANAGER_BACKGROUND_SEPARATOR);
-  background_group.id = kBackgroundGroupId;
-  groups.push_back(background_group);
-
-  Group foreground_group;
-  foreground_group.title =
-      l10n_util::GetString(IDS_TASK_MANAGER_FOREGROUND_SEPARATOR);
-  foreground_group.id = kForegroundGroupId;
-  groups.push_back(foreground_group);
-
-  return groups;
 }
 
 void TaskManagerTableModel::OnModelChanged() {
@@ -359,7 +313,7 @@ void TaskManagerView::Init() {
   table_model_.reset(new TaskManagerTableModel(model_));
 
   // Page column has no header label.
-  columns_.push_back(TableColumn(IDS_TASK_MANAGER_PAGE_COLUMN, L"",
+  columns_.push_back(TableColumn(IDS_TASK_MANAGER_PAGE_COLUMN,
                                  TableColumn::LEFT, -1, 1));
   columns_.back().sortable = true;
   columns_.push_back(TableColumn(IDS_TASK_MANAGER_PHYSICAL_MEM_COLUMN,
@@ -399,7 +353,7 @@ void TaskManagerView::Init() {
 
   tab_table_ = new views::GroupTableView(table_model_.get(), columns_,
                                          views::ICON_AND_TEXT, false, true,
-                                         true, false);
+                                         true, true);
 
   // Hide some columns by default
   tab_table_->SetColumnVisibility(IDS_TASK_MANAGER_PROCESS_ID_COLUMN, false);
