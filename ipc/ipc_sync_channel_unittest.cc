dfb58466b1cc6f6ec15dc7c749998a75fd5b9f23
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 06:39:44 +0000

    Revert 69694 - Add support for sockets that can listen and accept a connection.
    These sockets allow one connection at a time, however clients can
    connect and disconnect repeatedly.
    
    These are going to be used by Cloud Print, Remoting and
    Automation.
    
    BUG=NONE
    TEST=BUILD
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=69660
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=69690
    
    Review URL: http://codereview.chromium.org/5749001
    
    TBR=dmaclach@chromium.org
    Review URL: http://codereview.chromium.org/6060002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69695 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_channel_unittest.cc
+++ ipc/ipc_sync_channel_unittest.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Unit test for SyncChannel.
 
-#include "ipc/ipc_sync_channel.h"
-
 #include <string>
 #include <vector>
 
@@ -21,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/thread.h"
 #include "base/waitable_event.h"
 #include "ipc/ipc_message.h"
+#include "ipc/ipc_sync_channel.h"
 #include "ipc/ipc_sync_message_filter.h"
 #include "ipc/ipc_sync_message_unittest.h"
 #include "testing/gtest/include/gtest/gtest.h"
