287fbea17d8c23a10f6647c0001bc173772fa89d
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 03:08:07 +0000

    Revert 69690 - Add support for sockets that can listen and accept a connection.
    These sockets allow one connection at a time, however clients can
    connect and disconnect repeatedly.
    
    These are going to be used by Cloud Print, Remoting and
    Automation.
    
    BUG=NONE
    TEST=BUILD
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=69660
    
    Review URL: http://codereview.chromium.org/5749001
    
    TBR=dmaclach@chromium.org
    Review URL: http://codereview.chromium.org/5972002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69691 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/file_descriptor_set_posix_unittest.cc
+++ ipc/file_descriptor_set_posix_unittest.cc
@@ -5,13 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This test is POSIX only.
 
-#include "ipc/file_descriptor_set_posix.h"
-
 #include <unistd.h>
 #include <fcntl.h>
 
 #include "base/basictypes.h"
 #include "base/eintr_wrapper.h"
+#include "ipc/file_descriptor_set_posix.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
--- ipc/sync_socket_unittest.cc
+++ ipc/sync_socket_unittest.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/sync_socket.h"
-
 #include <stdio.h>
 #include <string>
 #include <sstream>
@@ -15,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_LINUX) || defined(OS_MACOSX)
 #include "base/platform_thread.h"
 #include "base/process_util.h"
+#include "base/sync_socket.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_message_utils.h"
