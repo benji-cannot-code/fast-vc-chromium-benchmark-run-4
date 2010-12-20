d496f8b6704e584b2bdc6a1c5b9197a22f2e6fbc
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 21:15:42 +0000

    [Sync] Added missing include
    This was causing problems when merging to 597.
    
    BUG=64652
    TEST=None
    
    Review URL: http://codereview.chromium.org/5723003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/chrome_invalidation_client_unittest.cc
+++ chrome/browser/sync/notifier/chrome_invalidation_client_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/message_loop.h"
 #include "chrome/browser/sync/notifier/chrome_invalidation_client.h"
 #include "chrome/browser/sync/notifier/state_writer.h"
 #include "jingle/notifier/base/fake_base_task.h"
