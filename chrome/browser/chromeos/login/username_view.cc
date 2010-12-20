97560d88a3be66b416b6505c583dc67eb4e518cb
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 12:29:15 +0000

    Set correct bounds for the RoundedView class.
    BUG=chromium-os:10399
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5848003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/username_view.cc
+++ chrome/browser/chromeos/login/username_view.cc
@@ -42,13 +42,13 @@ class HalfRoundedView : public RoundedView<C> {
       return RoundedView<C>::GetClipPath();
     } else {
       SkPath path;
-      gfx::Rect bounds = C::GetLocalBounds(false);
-      bounds.Inset(kSmallShapeFrameWidth, kSmallShapeFrameWidth,
+      gfx::Rect frame_bounds = this->bounds();
+      frame_bounds.Inset(kSmallShapeFrameWidth, kSmallShapeFrameWidth,
                    kSmallShapeFrameWidth, kSmallShapeFrameWidth);
-      path.addRect(SkIntToScalar(bounds.x()),
-                   SkIntToScalar(bounds.y()),
-                   SkIntToScalar(bounds.x() + bounds.width()),
-                   SkIntToScalar(bounds.y() + bounds.height()));
+      path.addRect(SkIntToScalar(frame_bounds.x()),
+                   SkIntToScalar(frame_bounds.y()),
+                   SkIntToScalar(frame_bounds.x() + frame_bounds.width()),
+                   SkIntToScalar(frame_bounds.y() + frame_bounds.height()));
       return path;
     }
   }
@@ -58,13 +58,13 @@ class HalfRoundedView : public RoundedView<C> {
   }
 
   virtual SkRect GetViewRect() const {
-    gfx::Rect bounds = C::GetLocalBounds(false);
     SkRect view_rect;
     // The rectangle will be intersected with the bounds, so the correct half
     // of the round rectangle will be obtained.
-    view_rect.iset(bounds.x(), bounds.y() - bounds.width(),
-                   bounds.x() + bounds.width(),
-                   bounds.y() + bounds.height());
+    view_rect.iset(this->x(),
+                   this->y() - this->height(),
+                   this->x() + this->width(),
+                   this->y() + this->height());
     return view_rect;
   }
 
@@ -82,7 +82,6 @@ void UsernameView::Paint(gfx::Canvas* canvas) {
   gfx::Rect bounds = GetLocalBounds(false);
   if (!text_image_.get())
     PaintUsername(bounds);
-
   DCHECK(bounds.size() ==
          gfx::Size(text_image_->width(), text_image_->height()));
 
