c8155b4ee793bc935e24c0e8c4df3956b1b20a61
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-20 21:24:45 +0000

    Added group policy for disabling all client-side 3D APIs in Chromium (in particular, WebGL and Pepper 3D). This has been hooked up through a new command-line argument (--disable-3d-apis) orthogonal to the existing ones, so that further changes to those command line arguments will not accidentally regress the group policy support.
    Tested in the following ways:
     - Verified that --disable-3d-apis disables WebGL and Pepper 3D
       support on Mac OS X.
     - Verified that specifying the Disable3DAPIs policy via a JSON file
       disables WebGL on Linux.
     - Ran unit_tests and verified that there were no failures introduced.
    
    BUG=64806
    TEST=ConfigurationPolicyPrefStoreBooleanTest
    
    Review URL: http://codereview.chromium.org/5991003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_store_interface.h
+++ chrome/browser/policy/configuration_policy_store_interface.h
@@ -67,6 +67,7 @@ enum ConfigurationPolicyType {
   kPolicyAuthServerWhitelist,
   kPolicyAuthNegotiateDelegateWhitelist,
   kPolicyGSSAPILibraryName,
+  kPolicyDisable3DAPIs
 };
 
 static const int kPolicyNoProxyServerMode = 0;
