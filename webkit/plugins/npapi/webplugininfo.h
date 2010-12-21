ef392f31108efd4438ce68d50ee2f4a34a66a9ff
jkummerow@chromium.org <jkummerow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 08:27:25 +0000

    Bugfixes for recent PluginGroup refactoring.
    BUG=66505
    TEST=test_shell_tests: PluginGroupTest.*; manual: check that issues reported and screenshotted in bug 66505 no longer occur
    
    Review URL: http://codereview.chromium.org/5918003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69813 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugininfo.h
+++ webkit/plugins/npapi/webplugininfo.h
@@ -39,6 +39,7 @@ struct WebPluginInfo {
 
   // Special constructor only used during unit testing:
   WebPluginInfo(const string16& fake_name,
+                const FilePath& fake_path,
                 const string16& fake_version,
                 const string16& fake_desc);
 
