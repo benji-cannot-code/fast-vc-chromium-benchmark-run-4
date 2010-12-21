e32909c1642a01d26360873c8d4f5181bd875ce9
rickcam@chromium.org <rickcam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 00:52:17 +0000

    Moved forward declare of BackgroundModeManager from profile.h to profile_impl.h
    BUG=67097
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5893003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.h
+++ chrome/browser/profiles/profile.h
@@ -46,7 +46,6 @@ class SandboxedFileSystemContext;
 
 class AutocompleteClassifier;
 class BackgroundContentsService;
-class BackgroundModeManager;
 class BookmarkModel;
 class BrowserSignin;
 class BrowserThemeProvider;
--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
 
+class BackgroundModeManager;
 class ExtensionPrefs;
 class PrefService;
 
