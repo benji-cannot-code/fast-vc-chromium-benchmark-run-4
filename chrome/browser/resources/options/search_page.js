8e15ae9a87e3f793a7c19878e91164d139ef3460
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 01:36:44 +0000

    DOMUI: Set 'margin: 0' for the search input field.
    BUG=63808
    TEST=none
    
    Review URL: http://codereview.chromium.org/6021003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69787 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/search_page.js
+++ chrome/browser/resources/options/search_page.js
@@ -32,7 +32,7 @@ cr.define('options', function() {
 
       // Create a search field element.
       var searchField = document.createElement('input');
-      searchField.id = 'searchField';
+      searchField.id = 'search-field';
       searchField.type = 'search';
       searchField.setAttribute('autosave', 'org.chromium.options.search');
       searchField.setAttribute('results', '10');
@@ -88,7 +88,7 @@ cr.define('options', function() {
           this.setSearchText_('');
         } else {
           // Just wipe out any active search text since it's no longer relevant.
-          $('searchField').value = '';
+          $('search-field').value = '';
         }
       }
 
