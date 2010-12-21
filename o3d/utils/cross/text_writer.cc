ee306ad62ad221a84437910ed1fb751be978d165
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 02:35:52 +0000

    o3d: Fix o3d build.
    This reverts the changes did on http://src.chromium.org/viewvc/chrome?view=rev&revision=69785
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/5970006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69793 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/utils/cross/text_writer.cc
+++ o3d/utils/cross/text_writer.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "utils/cross/text_writer.h"
 #include "base/string_util.h"
-#include "base/stringprintf.h"
 
 using std::string;
 
@@ -80,7 +79,7 @@ void TextWriter::WriteFormatted(const char* format, ...) {
   va_list args;
   va_start(args, format);
   string formatted;
-  base::StringAppendV(&formatted, format, args);
+  StringAppendV(&formatted, format, args);
   va_end(args);
 
   WriteString(formatted);
