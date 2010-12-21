fd2475065366533e2caf19265ac00b2ff5aefff8
rickcam@chromium.org <rickcam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 20:00:47 +0000

    Added Background Application entries to Dock menu
    BUG=67076
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5908003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file lists all the command IDs understood by e.g. the browser.
 // It is used by Windows RC files, Mac NIB files, and other platforms too.
 
+// Values below IDC_MinimumLabelValue are reserved for dynamic menu items.
+#define IDC_MinimumLabelValue           4000
+
 #define IDC_Messages                    4000
 #define IDC_MessagesAll                 4007
 #define IDC_MessagesNone                4008
