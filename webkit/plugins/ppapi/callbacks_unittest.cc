93462c90678950d8009913608cb71944fe2935e2
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 04:57:00 +0000

    A build fix for the "WebKit Mac (valgrind)" bot. This change just moves the CallbackRunInfo class (used from CallbackShutdownTest) out of the anonymous namespace. I'm not sure why this build break only happens on this bot, though.
    TBR=viettrungluu
    BUG=none
    TEST=fix build on the "WebKit Mac (valgrind)" bot
    Review URL: http://codereview.chromium.org/6000004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69803 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/callbacks_unittest.cc
+++ webkit/plugins/ppapi/callbacks_unittest.cc
@@ -17,14 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webkit {
 namespace ppapi {
 
-namespace {
-
 struct CallbackRunInfo {
   // All valid results (PP_OK, PP_ERROR_...) are nonpositive.
   CallbackRunInfo() : run_count(0), result(1) {}
   unsigned run_count;
   int32_t result;
 };
+
+namespace {
 
 void TestCallback(void* user_data, int32_t result) {
   CallbackRunInfo* info = reinterpret_cast<CallbackRunInfo*>(user_data);
