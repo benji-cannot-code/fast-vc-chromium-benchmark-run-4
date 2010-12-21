7a8b8db2bf53b0544571b6bc06aad457bc4a4518
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 02:58:55 +0000

    Don't scroll up when the user clicks on the 'disable plugins' tab.
    BUG=67056
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6015004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/content_settings.js
+++ chrome/browser/resources/options/content_settings.js
@@ -55,6 +55,7 @@ cr.define('options', function() {
 
       $('plugins-tab').onclick = function(event) {
         chrome.send('openPluginsTab');
+        return false;
       };
 
       if (!templateData.enable_click_to_play)
