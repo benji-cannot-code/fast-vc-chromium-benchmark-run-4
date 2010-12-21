a606da1e3b9853ee3b03fb6ae66b6af890205bc2
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 18:21:37 +0000

    DOMUI Prefs: Switch search engines to inline delete buttons
    BUG=63825
    TEST=Search engines in DOMUI prefs should be removable with inline delete buttons.
    
    Review URL: http://codereview.chromium.org/6013005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/search_engine_manager.js
+++ chrome/browser/resources/options/search_engine_manager.js
@@ -42,9 +42,6 @@ cr.define('options', function() {
         chrome.send('editSearchEngine', ["-1"]);
         OptionsPage.showOverlay('editSearchEngineOverlay');
       };
-      $('removeSearchEngineButton').onclick = function(event) {
-        chrome.send('removeSearchEngine', [self.selectedModelIndex_]);
-      };
       $('editSearchEngineButton').onclick = function(event) {
         chrome.send('editSearchEngine', [self.selectedModelIndex_]);
         OptionsPage.showOverlay('editSearchEngineOverlay');
@@ -58,8 +55,6 @@ cr.define('options', function() {
       // TODO(stuartmorgan): Remove this once the strings are updated.
       $('addSearchEngineButton').textContent =
           localStrings.getStringWithoutAccelerator('addSearchEngineButton');
-      $('removeSearchEngineButton').textContent =
-          localStrings.getStringWithoutAccelerator('removeSearchEngineButton');
     },
 
     /**
@@ -90,8 +85,6 @@ cr.define('options', function() {
       var engine = selectedIndex != -1 ?
           this.list_.dataModel.item(selectedIndex) : null;
 
-      $('removeSearchEngineButton').disabled =
-          !(engine && engine['canBeRemoved']);
       $('editSearchEngineButton').disabled = engine == null;
       $('makeDefaultSearchEngineButton').disabled =
           !(engine && engine['canBeDefault']);
--- chrome/browser/resources/options/search_engine_manager_engine_list.js
+++ chrome/browser/resources/options/search_engine_manager_engine_list.js
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('options.search_engines', function() {
-  const List = cr.ui.List;
+  const DeletableItem = options.DeletableItem;
+  const DeletableItemList = options.DeletableItemList;
   const ListInlineHeaderSelectionController =
       options.ListInlineHeaderSelectionController;
-  const ListItem = cr.ui.ListItem;
 
   /**
    * Creates a new search engine list item.
@@ -32,11 +32,11 @@ cr.define('options.search_engines', function() {
   };
 
   SearchEngineListItem.prototype = {
-    __proto__: ListItem.prototype,
+    __proto__: DeletableItem.prototype,
 
     /** @inheritDoc */
     decorate: function() {
-      ListItem.prototype.decorate.call(this);
+      DeletableItem.prototype.decorate.call(this);
 
       var engine = this.searchEngine_;
 
@@ -44,6 +44,8 @@ cr.define('options.search_engines', function() {
         this.classList.add('heading');
       else if (engine['default'])
         this.classList.add('default');
+
+      this.deletable = engine['canBeRemoved'];
 
       var nameEl = this.ownerDocument.createElement('div');
       nameEl.className = 'name';
@@ -55,21 +57,21 @@ cr.define('options.search_engines', function() {
         nameEl.style.backgroundImage = url('chrome://favicon/iconurl/' +
                                            engine['iconURL']);
       }
-      this.appendChild(nameEl);
+      this.contentElement.appendChild(nameEl);
 
       var keywordEl = this.ownerDocument.createElement('div');
       keywordEl.className = 'keyword';
       keywordEl.textContent = engine['heading'] ?
           localStrings.getString('searchEngineTableKeywordHeader') :
           engine['keyword'];
-      this.appendChild(keywordEl);
+      this.contentElement.appendChild(keywordEl);
     },
   };
 
   var SearchEngineList = cr.ui.define('list');
 
   SearchEngineList.prototype = {
-    __proto__: List.prototype,
+    __proto__: DeletableItemList.prototype,
 
     /** @inheritDoc */
     createItem: function(searchEngine) {
@@ -81,13 +83,19 @@ cr.define('options.search_engines', function() {
       return new ListInlineHeaderSelectionController(sm, this);
     },
 
+    /** @inheritDoc */
+    deleteItemAtIndex: function(index) {
+      var modelIndex = this.dataModel.item(index)['modelIndex']
+      chrome.send('removeSearchEngine', [String(modelIndex)]);
+    },
+
     /**
      * Returns true if the given item is selectable.
      * @param {number} index The index to check.
      */
     canSelectIndex: function(index) {
       return !this.dataModel.item(index).hasOwnProperty('heading');
-    }
+    },
   };
 
   // Export
