70450ef63e546aec8e778202aedc420bf48e5117
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-21 20:31:59 +0000

    Content settings lists moved to sub-sub pages.
    BUG=64153
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5964003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options.js
+++ chrome/browser/resources/options/options.js
@@ -12,6 +12,8 @@ var AutoFillOptions = options.AutoFillOptions;
 var BrowserOptions = options.BrowserOptions;
 var ClearBrowserDataOverlay = options.ClearBrowserDataOverlay;
 var ContentSettings = options.ContentSettings;
+var ContentSettingsExceptionsArea =
+    options.contentSettings.ContentSettingsExceptionsArea;
 var CookiesView = options.CookiesView;
 var EditSearchEngineOverlay = options.EditSearchEngineOverlay;
 var FontSettings = options.FontSettings;
@@ -92,6 +94,8 @@ function load() {
   OptionsPage.register(AdvancedOptions.getInstance());
   OptionsPage.registerSubPage(ContentSettings.getInstance(),
                               AdvancedOptions.getInstance());
+  OptionsPage.registerSubPage(ContentSettingsExceptionsArea.getInstance(),
+                              ContentSettings.getInstance());
   OptionsPage.registerSubPage(CookiesView.getInstance(),
                               ContentSettings.getInstance());
   OptionsPage.registerSubPage(FontSettings.getInstance(),
