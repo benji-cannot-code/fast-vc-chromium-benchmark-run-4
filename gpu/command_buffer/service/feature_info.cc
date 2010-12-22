c3a4df239c7984f8fce9616cb89ea0a288e3fc43
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 00:41:20 +0000

    Fixed requesting of GL_EXT_texture_format_BGRA8888 and GL_EXT_read_format_bgra on desktop systems where the underlying extension is GL_EXT_bgra. Fixed glDrawArrays to synthesize INVALID_VALUE instead of INVALID_ENUM when first < 0. Tested these changes with --enable-accelerated-2d-canvas.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6015006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69907 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -178,7 +178,8 @@ void FeatureInfo::AddFeatures(const char* desired_features) {
   // Check if we should allow GL_EXT_texture_format_BGRA8888
   if (ext.Desire("GL_EXT_texture_format_BGRA8888") &&
       (ext.Have("GL_EXT_texture_format_BGRA8888") ||
-       ext.Have("GL_APPLE_texture_format_BGRA8888"))) {
+       ext.Have("GL_APPLE_texture_format_BGRA8888") ||
+       ext.Have("GL_EXT_bgra"))) {
     enable_texture_format_bgra8888 = true;
   }
 
@@ -187,7 +188,9 @@ void FeatureInfo::AddFeatures(const char* desired_features) {
     enable_read_format_bgra = true;
   }
 
-  if (ext.HaveAndDesire("GL_EXT_read_format_bgra")) {
+  if (ext.Desire("GL_EXT_read_format_bgra") &&
+      (ext.Have("GL_EXT_read_format_bgra") ||
+       ext.Have("GL_EXT_bgra"))) {
     enable_read_format_bgra = true;
   }
 
