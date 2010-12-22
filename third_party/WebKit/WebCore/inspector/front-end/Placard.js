91271194dcf6b53fdb2813d7907ec6f2228830f4
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-22 22:39:08 +0000

    2010-12-22  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Joseph Pecoraro.
    
            Web Inspector: script name gets parsed as HTML in Call stack panel
            https://bugs.webkit.org/show_bug.cgi?id=51468
    
            * inspector/front-end/Placard.js:
            (WebInspector.Placard.prototype.set subtitle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/Placard.js
+++ third_party/WebKit/WebCore/inspector/front-end/Placard.js
@@ -68,7 +68,7 @@ WebInspector.Placard.prototype = {
         if (this._subtitle === x)
             return;
         this._subtitle = x;
-        this.subtitleElement.innerHTML = x;
+        this.subtitleElement.textContent = x;
     },
 
     get selected()
