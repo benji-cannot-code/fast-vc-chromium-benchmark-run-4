a4e5b10d5195767834591654b44c4db61fdcba5d
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 22:40:58 +0000

    Set scoped IO access when closing the audio input thread to allow joining.
    This is a fix for the crash problem described by issue 67806. The problem was caused by a new included IO access assertion that prevented the IO thread to join any other thread. This raised an error when trying to stop the audio recording thread.
    
    BUG=67806
    TEST=none
    
    Review URL: http://codereview.chromium.org/6063004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69994 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_input_controller.cc
+++ media/audio/audio_input_controller.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "media/audio/audio_input_controller.h"
+
+#include "base/thread_restrictions.h"
 #include "media/base/limits.h"
 
 namespace {
@@ -69,6 +71,15 @@ void AudioInputController::Close() {
   thread_.message_loop()->PostTask(
       FROM_HERE,
       NewRunnableMethod(this, &AudioInputController::DoClose));
+
+  // A ScopedAllowIO object is required to join the thread when calling Stop.
+  // This is because as joining threads may be a long operation it's now
+  // not allowed in threads without IO access, which is the case of the IO
+  // thread (it is missnamed) being used here. This object overrides
+  // temporarily this restriction and should be used only in specific
+  // infrequent cases where joining is guaranteed to be fast.
+  // Bug: http://code.google.com/p/chromium/issues/detail?id=67806
+  base::ThreadRestrictions::ScopedAllowIO allow_io_for_thread_join;
   thread_.Stop();
 }
 
