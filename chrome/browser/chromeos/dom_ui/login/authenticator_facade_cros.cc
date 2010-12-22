d0e21b97318e13dcd8e2827da4f4d94e1b9d28cd
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 18:17:36 +0000

    These files were missed in 69849.
    Here is the original review URL:
    http://codereview.chromium.org/5708002
    
    The original patch was from Ryan Harrison <rharrision@chromium.org>
    
    BUG=see original issue
    TEST=see original issue
    
    Review URL: http://codereview.chromium.org/6011007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69964 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/dom_ui/login/authenticator_facade_cros.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/dom_ui/login/authenticator_facade_cros.h"
+#include "chrome/browser/chromeos/dom_ui/login/authenticator_facade_cros_helpers.h"
+
+#include <string>
+
+namespace chromeos {
+
+AuthenticatorFacadeCros::AuthenticatorFacadeCros(
+    LoginStatusConsumer* consumer)
+    : AuthenticatorFacade(consumer),
+      authenticator_(NULL),
+      helpers_(new AuthenticatorFacadeCrosHelpers()) {
+}
+
+void AuthenticatorFacadeCros::Setup() {
+  authenticator_ = helpers_->CreateAuthenticator(consumer_);
+}
+
+void AuthenticatorFacadeCros::AuthenticateToLogin(
+    Profile* profile,
+    const std::string& username,
+    const std::string& password,
+    const std::string& login_token,
+    const std::string& login_captcha) {
+  helpers_->PostAuthenticateToLogin(authenticator_.get(),
+                                    profile,
+                                    username,
+                                    password);
+}
+
+}  // namespace chromeos
