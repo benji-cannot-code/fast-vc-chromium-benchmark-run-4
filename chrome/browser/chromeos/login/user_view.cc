564db6f08dd8aeb94ea502ba86079f78d4b4165b
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 16:40:59 +0000

    User pods show hand cursor on hover Re-applying from http://codereview.chromium.org/5986005/, LGTM is there.
    BUG=chromium-os:9296
    TEST=manual
    
    Review URL: http://codereview.chromium.org/5990006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_view.cc
+++ chrome/browser/chromeos/login/user_view.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/rounded_view.h"
 #include "gfx/canvas.h"
 #include "gfx/canvas_skia.h"
+#include "gfx/gtk_util.h"
 #include "gfx/rect.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
@@ -230,6 +231,12 @@ class PodImageView : public views::ImageView {
 
   virtual void OnMouseExited(const views::MouseEvent& event) {
     views::ImageView::SetImage(image_);
+  }
+
+  gfx::NativeCursor GetCursorForPoint(
+      views::Event::EventType event_type,
+      const gfx::Point& p) {
+    return gfx::GetCursor(GDK_HAND2);
   }
 
  private:
