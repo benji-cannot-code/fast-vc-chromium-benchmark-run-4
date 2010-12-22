c9af961accf063635bf46d9eb51d44b4c031f1aa
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 00:47:18 +0000

    Include scoped_gdi_object.h where necessary.
    Add base::win:: prefix to the calls to ScopedGDIObject and remove the using
    'using' declaration.
    
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/6018004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_win.cc
+++ chrome/browser/renderer_host/render_widget_host_view_win.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_comptr_win.h"
 #include "base/thread.h"
 #include "base/win_util.h"
+#include "base/win/scoped_gdi_object.h"
 #include "chrome/browser/accessibility/browser_accessibility_win.h"
 #include "chrome/browser/accessibility/browser_accessibility_manager.h"
 #include "chrome/browser/accessibility/browser_accessibility_state.h"
@@ -675,7 +676,7 @@ void RenderWidgetHostViewWin::Redraw() {
   RECT damage_bounds;
   GetUpdateRect(&damage_bounds, FALSE);
 
-  ScopedGDIObject<HRGN> damage_region(CreateRectRgn(0, 0, 0, 0));
+  base::win::ScopedGDIObject<HRGN> damage_region(CreateRectRgn(0, 0, 0, 0));
   GetUpdateRgn(damage_region, FALSE);
 
   // Paint the invalid region synchronously.  Our caller will not paint again
@@ -902,7 +903,7 @@ void RenderWidgetHostViewWin::OnPaint(HDC unused_dc) {
 
   // Grab the region to paint before creation of paint_dc since it clears the
   // damage region.
-  ScopedGDIObject<HRGN> damage_region(CreateRectRgn(0, 0, 0, 0));
+  base::win::ScopedGDIObject<HRGN> damage_region(CreateRectRgn(0, 0, 0, 0));
   GetUpdateRgn(damage_region, FALSE);
 
   CPaintDC paint_dc(m_hWnd);
