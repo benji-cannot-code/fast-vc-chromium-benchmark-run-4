e0c1c9750e8b30c1e165e0b828e9fed7dba91179
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 22:52:41 +0000

    DOMUI Settings: UTH: Fix up the 'Web Content' section.
    Added "Font Size Label" combo box with a list of font sizes.
    
    BUG=63838
    TEST=none
    
    Review URL: http://codereview.chromium.org/6057003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69995 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/advanced_options.js
+++ chrome/browser/resources/options/advanced_options.js
@@ -60,7 +60,11 @@ var OptionsPage = options.OptionsPage;
       $('defaultZoomLevel').onchange = function(event) {
         chrome.send('defaultZoomLevelAction',
             [String(event.target.options[event.target.selectedIndex].value)]);
-      }
+      };
+      $('defaultFontSize').onchange = function(event) {
+        chrome.send('defaultFontSizeAction',
+            [String(event.target.options[event.target.selectedIndex].value)]);
+      };
 
       if (cr.isWindows || cr.isMac) {
         $('certificatesManageButton').onclick = function(event) {
@@ -168,6 +172,31 @@ var OptionsPage = options.OptionsPage;
       }
     }
     selectCtl.selectedIndex = 4;  // 100%
+  };
+
+  // Set the font size selected item.
+  AdvancedOptions.SetFontSize = function(fixed_font_size_value,
+      font_size_value) {
+    var selectCtl = $('defaultFontSize');
+    if (fixed_font_size_value == font_size_value) {
+      for (var i = 0; i < selectCtl.options.length; i++) {
+        if (selectCtl.options[i].value == font_size_value) {
+          selectCtl.selectedIndex = i;
+          if ($('Custom'))
+            selectCtl.remove($('Custom').index);
+          return;
+        }
+      }
+    }
+
+    // Add/Select Custom Option in the font size label list.
+    if (!$('Custom')) {
+      var option = new Option(localStrings.getString('fontSizeLabelCustom'),
+                              -1, false, true);
+      option.setAttribute("id", "Custom");
+      selectCtl.add(option);
+    }
+    $('Custom').selected = true;
   };
 
   // Set the download path.
