948b5b47a92f635997c21a22b6a22a52721b2138
suzhe@google.com <suzhe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 00:07:38 +0000

    Add a CHECK() to verify the existence of necessary data file.
    BUG=60363
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/4212002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69902 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/gtk_key_bindings_handler_unittest.cc
+++ chrome/browser/renderer_host/gtk_key_bindings_handler_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/file_util.h"
+#include "base/logging.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
 #include "chrome/common/chrome_paths.h"
@@ -31,6 +32,8 @@ class GtkKeyBindingsHandlerTest : public testing::Test {
     FilePath gtkrc;
     PathService::Get(chrome::DIR_TEST_DATA, &gtkrc);
     gtkrc = gtkrc.AppendASCII("gtk_key_bindings_test_gtkrc");
+    CHECK(file_util::PathExists(gtkrc)) << gtkrc.value();
+
     gtk_rc_parse(gtkrc.value().c_str());
 
     GtkWidget* fixed = gtk_fixed_new();
