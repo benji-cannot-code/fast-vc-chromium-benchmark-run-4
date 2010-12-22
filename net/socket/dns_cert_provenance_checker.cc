aff820a08a4d370cf01be584a2ccb1f8c1c4db89
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 18:01:15 +0000

    net: dump certificate chain to LOG(ERROR) when a provenance check fails.
    BUG=none
    TEST=none
    
    http://codereview.chromium.org/6051006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69961 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/dns_cert_provenance_checker.cc
+++ net/socket/dns_cert_provenance_checker.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/base64.h"
 #include "base/basictypes.h"
 #include "base/crypto/encryptor.h"
 #include "base/crypto/symmetric_key.h"
@@ -151,6 +152,7 @@ class DnsCertProvenanceCheck : public NonThreadSafe {
                  << " hostname:" << hostname_
                  << " domain:" << domain_;
       g_dns_cert_limits.Get().DidUpload(hostname_);
+      LogCertificates(der_certs_);
       delegate_->OnDnsCertLookupFailed(hostname_, der_certs_);
     } else if (status == OK) {
       LOG(ERROR) << "GOOD"
@@ -161,6 +163,35 @@ class DnsCertProvenanceCheck : public NonThreadSafe {
     }
 
     delete this;
+  }
+
+  // LogCertificates writes a certificate chain, in PEM format, to LOG(ERROR).
+  static void LogCertificates(
+      const std::vector<std::string>& der_certs) {
+    std::string dump;
+    bool first = true;
+
+    for (std::vector<std::string>::const_iterator
+         i = der_certs.begin(); i != der_certs.end(); i++) {
+      if (!first)
+        dump += "\n";
+      first = false;
+
+      dump += "-----BEGIN CERTIFICATE-----\n";
+      std::string b64_encoded;
+      base::Base64Encode(*i, &b64_encoded);
+      for (size_t i = 0; i < b64_encoded.size();) {
+        size_t todo = b64_encoded.size() - i;
+        if (todo > 64)
+          todo = 64;
+        dump += b64_encoded.substr(i, todo);
+        dump += "\n";
+        i += todo;
+      }
+      dump += "-----END CERTIFICATE-----";
+    }
+
+    LOG(ERROR) << "Offending certificates:\n" << dump;
   }
 
   const std::string hostname_;
