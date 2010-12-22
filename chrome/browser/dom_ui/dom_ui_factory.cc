1d2788b7d45f99a20d37fc40301c89795c255bee
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-22 22:24:48 +0000

    touch: Login code necessary only for chromeos.
    BUG=none
    TEST=builds successfully for all combinations of touchui and chromeos
    
    Review URL: http://codereview.chromium.org/6017006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69990 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_factory.cc
+++ chrome/browser/dom_ui/dom_ui_factory.cc
@@ -158,8 +158,6 @@ static DOMUIFactoryFunction GetDOMUIFactoryFunction(Profile* profile,
 #if defined(TOUCH_UI)
   if (url.host() == chrome::kChromeUIKeyboardHost)
     return &NewDOMUI<KeyboardUI>;
-  if (url.host() == chrome::kChromeUILoginHost)
-    return &NewDOMUI<chromeos::LoginUI>;
 #endif
   if (url.host() == chrome::kChromeUIGpuInternalsHost)
     return &NewDOMUI<GpuInternalsUI>;
@@ -214,6 +212,11 @@ static DOMUIFactoryFunction GetDOMUIFactoryFunction(Profile* profile,
       return &NewDOMUI<PrintPreviewUI>;
     }
   }
+#endif  // defined(OS_CHROMEOS)
+
+#if defined(OS_CHROMEOS) && defined(TOUCH_UI)
+  if (url.host() == chrome::kChromeUILoginHost)
+    return &NewDOMUI<chromeos::LoginUI>;
 #endif
 
   if (url.spec() == chrome::kChromeUIConstrainedHTMLTestURL)
