1d984c7fc72c33e3e867887c60c4e3a38b8b94dd
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-22 21:26:42 +0000

    Move more of WebPageProxy::didCommitLoadForFrame into WebFrameProxy::didCommitLoad.
    Reviewed by Sam Weinig.
    
    * UIProcess/WebFrameProxy.cpp:
    (WebKit::WebFrameProxy::didCommitLoad): Added type and certificate info arguments,
    and set m_MIMEType, m_isFrameSet, and m_certificateInfo.
    * UIProcess/WebFrameProxy.h: Removed setMIMEType and setCertificateInfo and added
    arguments to didCommitLoad.
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didCommitLoadForFrame): Removed code that is now inside the
    WebFrameProxy::didCommitLoad function.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebFrameProxy.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebFrameProxy.cpp
@@ -70,11 +70,6 @@ bool WebFrameProxy::isMainFrame() const
     return this == m_page->mainFrame();
 }
 
-void WebFrameProxy::setCertificateInfo(PassRefPtr<WebCertificateInfo> certificateInfo)
-{
-    m_certificateInfo = certificateInfo;
-}
-
 bool WebFrameProxy::canProvideSource() const
 {
     return isDisplayingMarkupDocument();
@@ -133,7 +128,7 @@ void WebFrameProxy::didFailProvisionalLoad()
     m_provisionalURL = String();
 }
 
-void WebFrameProxy::didCommitLoad()
+void WebFrameProxy::didCommitLoad(const String& mimeType, const PlatformCertificateInfo& certificateInfo)
 {
     ASSERT(m_loadState == LoadStateProvisional);
     ASSERT(!m_provisionalURL.isEmpty());
@@ -141,6 +136,9 @@ void WebFrameProxy::didCommitLoad()
     m_url = m_provisionalURL;
     m_provisionalURL = String();
     m_title = String();
+    m_MIMEType = mimeType;
+    m_isFrameSet = false;
+    m_certificateInfo = WebCertificateInfo::create(certificateInfo);
 }
 
 void WebFrameProxy::didFinishLoad()
