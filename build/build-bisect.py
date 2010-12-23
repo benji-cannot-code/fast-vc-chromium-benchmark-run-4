1f3aea6b203fcc42b0001a1574d561d6ca379081
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-23 23:10:06 +0000

    Add forwarding script for build-bisect.py.
    TODO(evan): remove this in a few months.
    
    Review URL: http://codereview.chromium.org/6096001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70112 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/build-bisect.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+print "This script has been moved to tools/bisect-builds.py."
+print "Please update any docs you're working from!"
+
+sys.exit(1)
