f1a195d0b7e93c90c356146e003aea732ae9bdf8
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-23 02:00:26 +0000

    Make CopyTexImage2D and CopyTexSubImage2D fail if formats are incompatible.
    TEST=unit tests, more WebGL conformance tests pass.
    BUG=none
    
    Review URL: http://codereview.chromium.org/6028005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70021 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/framebuffer_manager.h
+++ gpu/command_buffer/service/framebuffer_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "gpu/command_buffer/service/gl_utils.h"
 #include "gpu/command_buffer/service/renderbuffer_manager.h"
+#include "gpu/command_buffer/service/texture_manager.h"
 
 namespace gpu {
 namespace gles2 {
@@ -24,6 +25,19 @@ class FramebufferManager {
   class FramebufferInfo : public base::RefCounted<FramebufferInfo> {
    public:
     typedef scoped_refptr<FramebufferInfo> Ref;
+
+    class Attachment : public base::RefCounted<Attachment> {
+     public:
+      typedef scoped_refptr<Attachment> Ref;
+
+      virtual ~Attachment() { }
+      virtual GLsizei width() const = 0;
+      virtual GLsizei height() const = 0;
+      virtual GLenum internal_format() const = 0;
+      virtual GLsizei samples() const = 0;
+      virtual bool cleared() const = 0;
+      virtual void set_cleared() = 0;
+    };
 
     explicit FramebufferInfo(GLuint service_id);
 
@@ -38,7 +52,14 @@ class FramebufferManager {
     void AttachRenderbuffer(
         GLenum attachment, RenderbufferManager::RenderbufferInfo* renderbuffer);
 
+    // Attaches a texture to a particlar attachment. Pass null to detach.
+    void AttachTexture(
+        GLenum attachment, TextureManager::TextureInfo* texture, GLenum target,
+        GLint level);
+
     void MarkAttachedRenderbuffersAsCleared();
+
+    const Attachment* GetAttachment(GLenum attachment) const;
 
     bool IsDeleted() {
       return service_id_ == 0;
@@ -52,16 +73,17 @@ class FramebufferManager {
 
     void MarkAsDeleted() {
       service_id_ = 0;
-      renderbuffers_.clear();
+      attachments_.clear();
     }
 
     // Service side framebuffer id.
     GLuint service_id_;
 
-    // A map of attachments to renderbuffers.
-    typedef std::map<GLenum, RenderbufferManager::RenderbufferInfo::Ref>
-        AttachmentToRenderbufferMap;
-    AttachmentToRenderbufferMap renderbuffers_;
+    // A map of attachments.
+    typedef std::map<GLenum, Attachment::Ref> AttachmentMap;
+    AttachmentMap attachments_;
+
+    DISALLOW_COPY_AND_ASSIGN(FramebufferInfo);
   };
 
   FramebufferManager();
