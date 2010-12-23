736e8ba04994bb514867c8b20e50252f08f5f9fc
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-23 17:45:08 +0000

    2010-12-23  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            Move V8 to WebCore's implementation of showModalDialog
            https://bugs.webkit.org/show_bug.cgi?id=51527
    
            This code is basically verbatim translation from the JavaScriptCore
            bindings.  The only intentional difference is in the world selection
            for the dialog's frame.  I suspect JavaScriptCore's bindings have a
            subtle bug there.
    
            In this patch, I also remove a bunch of now-unneeded code in the
            generic bindings.
    
            * bindings/generic/BindingDOMWindow.h:
            * bindings/js/JSDOMWindowCustom.cpp:
            (WebCore::DialogHandler::dialogCreated):
            (WebCore::setUpDialog):
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            (WebCore::DialogHandler::DialogHandler):
            (WebCore::DialogHandler::dialogCreated):
            (WebCore::DialogHandler::returnValue):
            (WebCore::setUpDialog):
            (WebCore::V8DOMWindow::showModalDialogCallback):
            (WebCore::V8DOMWindow::openCallback):
            * bindings/v8/specialization/V8BindingDOMWindow.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/specialization/V8BindingDOMWindow.h
+++ third_party/WebKit/WebCore/bindings/v8/specialization/V8BindingDOMWindow.h
@@ -32,28 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8BindingDOMWindow_h
 #define V8BindingDOMWindow_h
 
-#include "BindingDOMWindow.h"
-#include "GenericBinding.h"
-#include "V8Proxy.h"
-
 namespace WebCore {
 
-class V8Binding;
-
-class V8BindingDOMWindow : public BindingDOMWindow<V8Binding> {
-public:
-    static void storeDialogArgs(State<V8Binding>*, Frame* newFrame, v8::Handle<v8::Value> dialogArgs)
-    {
-        // Set dialog arguments on the global object of the new frame.
-        if (!dialogArgs.IsEmpty()) {
-            v8::Local<v8::Context> context = V8Proxy::context(newFrame);
-            if (!context.IsEmpty()) {
-                v8::Context::Scope scope(context);
-                context->Global()->Set(v8::String::New("dialogArguments"), dialogArgs);
-            }
-        }
-    }
-};
+// FIXME: Remove this file.
 
 } // namespace WebCore
 
