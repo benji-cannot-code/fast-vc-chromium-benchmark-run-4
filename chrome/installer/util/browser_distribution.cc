66d8832a3a310e0001b9a06fdfea6be6a01a5391
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-23 19:54:02 +0000

    Support for GCF ready-mode.
    The installer can now be run in a way that installs both Chrome and Chrome Frame
    at the same time, into a shared directory.  The command line to do this is:
    
    mini_installer.exe --multi-install --chrome --chrome-frame --ready-mode
    --system-level --verbose-logging
    
    This installs both products although only Chrome will have an entry in the
    add/remove programs dialog that serves as an uninstallation command for both
    products.  Chrome Frame will be installed in ready-mode, which means that the
    user will have to opt-in or opt-out of using it.  The installer will create a
    REG_DWORD value to indicate this mode, here:
    
    
    HKEY_LOCAL_MACHINE\SOFTWARE\Google\Update\ClientState\{4DC8B4CA-1BDA-483e-B5FA-D3C12E15B62D}
     Value name: ChromeFrameReadyMode
     Value: 1
    
    If the user opts-in to use GCF, setup will be run again to remove this value,
    update Chrome's uninstallation commands to only uninstall Chrome, and add an
    entry to the Add/Remove Programs dialog for GCF.  To do this, the installer
    needs to be run with this switch:
    
     --multi-install --chrome-frame --system-level --ready-mode-opt-in --verbose-logging
    
    If the user opts-out, Chrome Frame will be uninstalled by running the installer
    with these arguments:
    
     --uninstall --chrome-frame --ready-mode --system-level --multi-install --verbose-logging
    
    In addition to uninstalling GCF, this updates Chrome's uninstallation commands
    accordingly and sets the ChromeFrameReadyMode value to 0 to avoid enabling ready
    mode again.
    
    Requirements that must currently be met in order to install Chrome and Chrome
    Frame into the same shared location:
    - Chrome Frame must not already be installed
    OR
    - Chrome Frame must be already installed as multi, into the shared location
    
    **** other changes ****
    
    The installer does no use the ap value for detecting other multi-install
    products anymore.  Instead we inspect the uninstallation switches to see if
    --multi-install is present.  Updated unit tests accordingly.
    
    When uninstalling Chrome Frame along with Chrome, the Chrome Frame
    uninstallation prompt is not shown.
    
    Added safeguards:
    If we attempt to install Chrome Frame multi over a single, install aborts and returns an error.
    If we attempt to install Chrome Frame single over a multi, install aborts and returns an error.
    
    TEST=See description above.
    BUG=61609
    
    Review URL: http://codereview.chromium.org/5989007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/browser_distribution.cc
+++ chrome/installer/util/browser_distribution.cc
@@ -233,3 +233,7 @@ void BrowserDistribution::AppendUninstallCommandLineFlags(
   DCHECK(cmd_line);
   cmd_line->AppendSwitch(installer::switches::kChrome);
 }
+
+bool BrowserDistribution::ShouldCreateUninstallEntry() {
+  return true;
+}
--- chrome/installer/util/package.cc
+++ chrome/installer/util/package.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/channel_info.h"
 #include "chrome/installer/util/delete_tree_work_item.h"
 #include "chrome/installer/util/google_update_constants.h"
+#include "chrome/installer/util/helper.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/l10n_string_util.h"
 #include "chrome/installer/util/master_preferences.h"
@@ -201,19 +202,9 @@ size_t Package::GetMultiInstallDependencyCount() const {
     if (!version_key.Valid()) {
       VLOG(1) << "Product not installed: " << dist->GetApplicationName();
     } else {
-      RegKey key(root_key, dist->GetStateKey().c_str(), KEY_READ);
-      ChannelInfo channel_info;
-      if (channel_info.Initialize(key)) {
-        if (channel_info.IsMultiInstall()) {
-          VLOG(1) << "Product dependency: " << dist->GetApplicationName();
-          ret++;
-        } else {
-          VLOG(1) << "Product is installed, but not multi: "
-                  << dist->GetApplicationName();
-        }
-      } else {
-        LOG(INFO) << "Product missing 'ap' value: "
-                  << dist->GetApplicationName();
+      if (installer::IsInstalledAsMulti(system_level_, dist)) {
+        VLOG(1) << "Product dependency: " << dist->GetApplicationName();
+        ++ret;
       }
     }
   }
