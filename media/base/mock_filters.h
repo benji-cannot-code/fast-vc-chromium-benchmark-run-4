5b69448f1df97a8409111accc7b76794bb261426
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-23 16:59:52 +0000

    Refactor PipelineImpl to use CompositeFilter to manage Filter state transitions.
    BUG=54110
    TEST=media_unittests CompositeFilterTest.*
    
    Review URL: http://codereview.chromium.org/5744002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70063 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/mock_filters.h
+++ media/base/mock_filters.h
@@ -51,7 +51,12 @@ class Destroyable : public MockClass {
 // gmock will track the number of times the methods are executed.
 class MockFilterCallback {
  public:
-  MockFilterCallback() {}
+  MockFilterCallback() : run_destroy_callback_(true) {
+  }
+
+  MockFilterCallback(bool run_destroy_callback) :
+    run_destroy_callback_(run_destroy_callback) {
+  }
   virtual ~MockFilterCallback() {}
 
   MOCK_METHOD0(OnCallbackDestroyed, void());
@@ -62,7 +67,7 @@ class MockFilterCallback {
   // destroyed.  Clients should use NiceMock<> or StrictMock<> depending on the
   // test.
   FilterCallback* NewCallback() {
-    return new CallbackImpl(this);
+    return new CallbackImpl(this, run_destroy_callback_);
   }
 
  private:
@@ -70,12 +75,15 @@ class MockFilterCallback {
   // MockFilterCallback.
   class CallbackImpl : public CallbackRunner<Tuple0> {
    public:
-    explicit CallbackImpl(MockFilterCallback* mock_callback)
-        : mock_callback_(mock_callback) {
+    explicit CallbackImpl(MockFilterCallback* mock_callback,
+                          bool run_destroy_callback)
+        : mock_callback_(mock_callback),
+          run_destroy_callback_(run_destroy_callback) {
     }
 
     virtual ~CallbackImpl() {
-      mock_callback_->OnCallbackDestroyed();
+      if (run_destroy_callback_)
+        mock_callback_->OnCallbackDestroyed();
     }
 
     virtual void RunWithParams(const Tuple0& params) {
@@ -84,11 +92,39 @@ class MockFilterCallback {
 
    private:
     MockFilterCallback* mock_callback_;
+    bool run_destroy_callback_;
 
     DISALLOW_COPY_AND_ASSIGN(CallbackImpl);
   };
 
+  bool run_destroy_callback_;
   DISALLOW_COPY_AND_ASSIGN(MockFilterCallback);
+};
+
+class MockFilter : public Filter {
+ public:
+  MockFilter();
+  MockFilter(bool requires_message_loop);
+
+  // Filter implementation.
+  virtual bool requires_message_loop() const;
+  virtual const char* message_loop_name() const;
+
+  MOCK_METHOD1(Play, void(FilterCallback* callback));
+  MOCK_METHOD1(Pause, void(FilterCallback* callback));
+  MOCK_METHOD1(Flush, void(FilterCallback* callback));
+  MOCK_METHOD1(Stop, void(FilterCallback* callback));
+  MOCK_METHOD1(SetPlaybackRate, void(float playback_rate));
+  MOCK_METHOD2(Seek, void(base::TimeDelta time, FilterCallback* callback));
+  MOCK_METHOD0(OnAudioRendererDisabled, void());
+
+ protected:
+  virtual ~MockFilter();
+
+ private:
+  bool requires_message_loop_;
+
+  DISALLOW_COPY_AND_ASSIGN(MockFilter);
 };
 
 class MockDataSource : public DataSource {
