b22d94ec8d7f0430d9eb1fe2c06bb0f187360d0d
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-23 18:49:18 +0000

    Send certificate in content description in jingle for Chromoting
    Transmit the server certificate to the client via content description. This
    allow secure encryption between Chromoting host and client.
    
    BUG=None
    TEST=remoting_unittests
    
    Review URL: http://codereview.chromium.org/5804001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70075 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/jingle_session_manager.h
+++ remoting/protocol/jingle_session_manager.h
@@ -38,7 +38,8 @@ namespace protocol {
 class ContentDescription : public cricket::ContentDescription {
  public:
   explicit ContentDescription(const CandidateSessionConfig* config,
-                              const std::string& auth_token);
+                              const std::string& auth_token,
+                              scoped_refptr<net::X509Certificate> certificate);
   ~ContentDescription();
 
   const CandidateSessionConfig* config() const {
@@ -47,12 +48,18 @@ class ContentDescription : public cricket::ContentDescription {
 
   const std::string& auth_token() const { return auth_token_; }
 
+  scoped_refptr<net::X509Certificate> certificate() const {
+    return certificate_;
+  }
+
  private:
   scoped_ptr<const CandidateSessionConfig> candidate_config_;
 
   // This may contain the initiating, or the accepting token depending on
   // context.
   std::string auth_token_;
+
+  scoped_refptr<net::X509Certificate> certificate_;
 };
 
 // This class implements SessionClient for Chromoting sessions. It acts as a
@@ -95,15 +102,6 @@ class JingleSessionManager
                             buzz::XmlElement** elem,
                             cricket::WriteError* error);
 
-  // The following two methods are used in unit tests only.
-  void set_server_cert(scoped_refptr<net::X509Certificate> server_cert) {
-    server_cert_ = server_cert;
-  }
-
-  void set_key(base::RSAPrivateKey* key) {
-    key_.reset(key);
-  }
-
  protected:
   virtual ~JingleSessionManager();
 
@@ -129,7 +127,8 @@ class JingleSessionManager
   // Creates outgoing session description for an incoming session.
   cricket::SessionDescription* CreateSessionDescription(
       const CandidateSessionConfig* candidate_config,
-      const std::string& auth_token);
+      const std::string& auth_token,
+      scoped_refptr<net::X509Certificate> certificate);
 
   std::string local_jid_;  // Full jid for the local side of the session.
   JingleThread* jingle_thread_;
@@ -138,9 +137,6 @@ class JingleSessionManager
   bool allow_local_ips_;
 
   bool closed_;
-
-  scoped_refptr<net::X509Certificate> server_cert_;
-  scoped_ptr<base::RSAPrivateKey> key_;
 
   std::list<scoped_refptr<JingleSession> > sessions_;
 
