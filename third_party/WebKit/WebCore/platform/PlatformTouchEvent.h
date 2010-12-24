1bc2882b9588222c732531c0b7e139296a19bf52
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-24 11:27:55 +0000

    2010-12-24  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Add option to enable Touch Events.
            https://bugs.webkit.org/show_bug.cgi?id=49125
    
            Add ENABLE_TOUCH_EVENTS option.
    
            * cmake/OptionsEfl.cmake:
            * cmakeconfig.h.cmake:
    2010-12-24  Ryuan Choi  <ryuan.choi@samsung.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Add option to enable Touch Events.
            https://bugs.webkit.org/show_bug.cgi?id=49125
    
            Add files for EFL to support TOUCH_EVENTS.
    
            * CMakeLists.txt:
            * CMakeListsEfl.txt:
            * platform/PlatformTouchEvent.h: Add EFL related constructor.
            * platform/PlatformTouchPoint.h: ditto.
            * platform/efl/PlatformTouchEventEfl.cpp: Added.
            (WebCore::PlatformTouchEvent::PlatformTouchEvent):
            * platform/efl/PlatformTouchPointEfl.cpp: Added.
            (WebCore::PlatformTouchPoint::PlatformTouchPoint):
    2010-12-24  Ryuan Choi  <ryuan.choi@samsung.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Add option to enable Touch Events.
            https://bugs.webkit.org/show_bug.cgi?id=49125
    
            Implement ewk_frame_feed_touch_event to feed touch events. Because EFL
            doesn't have touch events, Application should generate and pass it.
    
            * WebCoreSupport/ChromeClientEfl.h:
            (WebCore::ChromeClientEfl::needTouchEvents):
            * ewk/ewk_frame.cpp:
            (ewk_frame_feed_touch_event):
            * ewk/ewk_frame.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/PlatformTouchEvent.h
+++ third_party/WebKit/WebCore/platform/PlatformTouchEvent.h
@@ -42,6 +42,10 @@ typedef unsigned long int uint32;
 #define AEEEvent uint16
 #endif
 
+#if PLATFORM(EFL)
+typedef struct _Eina_List Eina_List;
+#endif
+
 namespace WebCore {
 
 enum TouchEventType {
@@ -66,6 +70,8 @@ public:
     PlatformTouchEvent(const Vector<IntPoint>&, TouchEventType, PlatformTouchPoint::State, int metaState);
 #elif PLATFORM(BREWMP)
     PlatformTouchEvent(AEEEvent, uint16 wParam, uint32 dwParam);
+#elif PLATFORM(EFL)
+    PlatformTouchEvent(Eina_List*, const IntPoint, TouchEventType, int metaState);
 #endif
 
     TouchEventType type() const { return m_type; }
