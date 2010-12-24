a8796f817052e0135358e971eef5966aa67cae50
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-24 03:11:42 +0000

    gtk: Cleanup JSModalDialogGtk implementation.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/5999008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70131 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/js_modal_dialog_gtk.h
+++ chrome/browser/gtk/js_modal_dialog_gtk.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_GTK_JS_MODAL_DIALOG_GTK_H_
 #pragma once
 
-#include <gtk/gtk.h>
-
 #include "app/gtk_signal.h"
-#include "base/logging.h"
+#include "base/basictypes.h"
 #include "base/scoped_ptr.h"
 #include "chrome/browser/ui/app_modal_dialogs/native_app_modal_dialog.h"
 #include "gfx/native_widget_types.h"
+
+typedef struct _GtkWidget GtkWidget;
 
 class JavaScriptAppModalDialog;
 
@@ -32,9 +32,7 @@ class JSModalDialogGtk : public NativeAppModalDialog {
   virtual void CancelAppModalDialog();
 
  private:
-  void HandleDialogResponse(GtkDialog* dialog, gint response_id);
-  static void OnDialogResponse(GtkDialog* gtk_dialog, gint response_id,
-                               JSModalDialogGtk* dialog);
+  CHROMEGTK_CALLBACK_1(JSModalDialogGtk, void, OnDialogResponse, int);
 
   scoped_ptr<JavaScriptAppModalDialog> dialog_;
   GtkWidget* gtk_dialog_;
@@ -43,4 +41,3 @@ class JSModalDialogGtk : public NativeAppModalDialog {
 };
 
 #endif  // CHROME_BROWSER_GTK_JS_MODAL_DIALOG_GTK_H_
-
