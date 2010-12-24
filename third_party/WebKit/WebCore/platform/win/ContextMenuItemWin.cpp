065e3794d407e61e476b6af805ffb68bfb051569
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-24 01:11:04 +0000

    2010-12-23  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed build fix for WinCE after r73802.
    
            Reapply r70812:
            Port ContextMenuItemWin.cpp to WinCE
            https://bugs.webkit.org/show_bug.cgi?id=48407
    
            * platform/win/ContextMenuItemWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/win/ContextMenuItemWin.cpp
+++ third_party/WebKit/WebCore/platform/win/ContextMenuItemWin.cpp
@@ -29,6 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ContextMenu.h"
 
+#if OS(WINCE)
+#ifndef MFS_DISABLED
+#define MFS_DISABLED MF_GRAYED
+#endif
+#ifndef MIIM_FTYPE
+#define MIIM_FTYPE MIIM_TYPE
+#endif
+#ifndef MIIM_STRING
+#define MIIM_STRING 0
+#endif
+#endif
+
 namespace WebCore {
 
 ContextMenuItem::ContextMenuItem(const MENUITEMINFO& info)
