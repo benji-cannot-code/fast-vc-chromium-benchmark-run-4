1fdfa86d6c0dea657ec3b78b5e360925f21fee44
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-24 20:55:32 +0000

    2010-12-24  Benjamin Poulain  <ikipou@gmail.com>
            Reviewed by Darin Adler.
    
            [Qt] [WK2] WebKit 2 does not build on mac, missing reference to RetainPtr
            https://bugs.webkit.org/show_bug.cgi?id=51598
    
            Fix the build, add the missing header.
    
            * WebProcess/WebCoreSupport/WebPlatformStrategies.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74650 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/Page.h>
 #include <WebCore/PageGroup.h>
 #include <wtf/MathExtras.h>
+#include <wtf/RetainPtr.h>
 #include <wtf/text/CString.h>
 
 // FIXME: Implement localization.
