626ad9c0a43f278f79ef57c123442c83fdea18a6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-24 00:55:48 +0000

    2010-12-23  Adam Barth  <abarth@webkit.org>
            Remove include of non-existant file.
    
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/v8/custom/V8DOMWindowCustom.cpp
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SharedWorkerRepository.h"
 #include "Storage.h"
 #include "V8Binding.h"
-#include "V8BindingDOMWindow.h"
 #include "V8BindingMacros.h"
 #include "V8BindingState.h"
 #include "V8CustomEventListener.h"
