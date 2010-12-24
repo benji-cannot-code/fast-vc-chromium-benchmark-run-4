97e4f261374a03d390e5ae4d62c61b150320ccae
gfeher@chromium.org <gfeher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-24 08:13:49 +0000

    Revert 70142 - Fix build error
    TBR=jam@chromium.org
    Review URL: http://codereview.chromium.org/5977008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/tab_proxy.h
+++ chrome/test/automation/tab_proxy.h
@@ -49,9 +49,7 @@ class TabProxy : public AutomationResourceProxy,
  public:
   class TabProxyDelegate {
    public:
-    virtual bool OnMessageReceived(TabProxy* tab, const IPC::Message& msg) {
-      return false;
-    }
+    virtual bool OnMessageReceived(TabProxy* tab, const IPC::Message& msg) {}
     virtual void OnChannelError(TabProxy* tab) {}
 
    protected:
