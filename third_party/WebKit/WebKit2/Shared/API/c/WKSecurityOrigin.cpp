3a8aae6275318e9d5cf20dd780d8d3e7161d7e47
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-24 01:16:19 +0000

    Need WebKit2 API to create a SecurityOrigin from protocol, host, and port https://bugs.webkit.org/show_bug.cgi?id=51563
    Reviewed by Sam Weinig.
    
    WebCore: 
    
    Add SecurityOrigin::create(const String& protocol, const String& host, int port).
    
    * WebCore.exp.in:
    Export the symbols for WebCore::SecurityOrigin::create(String, String, int).
    
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::createFromDatabaseIdentifier):
    Use the constant MaxAllowedPort.
    (WebCore::SecurityOrigin::create):
    Sanitizing based off of that done in createFromDatabaseIdentifier.
    * page/SecurityOrigin.h:
    
    WebKit2: 
    
    Add WKSecurityOriginCreate(WKStringRef protocol, WKStringRef host, int port).
    
    * Shared/API/c/WKSecurityOrigin.cpp:
    (WKSecurityOriginCreateFromIdentifier):
    Renamed so as not to clash with the new WKSecurityOriginCreate.
    (WKSecurityOriginCreate):
    * Shared/API/c/WKSecurityOrigin.h:
    * Shared/WebSecurityOrigin.h:
    (WebKit::WebSecurityOrigin::create):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/API/c/WKSecurityOrigin.cpp
+++ third_party/WebKit/WebKit2/Shared/API/c/WKSecurityOrigin.cpp
@@ -36,9 +36,15 @@ WKTypeID WKSecurityOriginGetTypeID()
     return toAPI(WebSecurityOrigin::APIType);
 }
 
-WKSecurityOriginRef WKSecurityOriginCreate(WKStringRef identifier)
+WKSecurityOriginRef WKSecurityOriginCreateFromIdentifier(WKStringRef identifier)
 {
     RefPtr<WebSecurityOrigin> securityOrigin = WebSecurityOrigin::create(toImpl(identifier)->string());
+    return toAPI(securityOrigin.release().leakRef());
+}
+
+WKSecurityOriginRef WKSecurityOriginCreate(WKStringRef protocol, WKStringRef host, int port)
+{
+    RefPtr<WebSecurityOrigin> securityOrigin = WebSecurityOrigin::create(toImpl(protocol)->string(), toImpl(host)->string(), port);
     return toAPI(securityOrigin.release().leakRef());
 }
 
