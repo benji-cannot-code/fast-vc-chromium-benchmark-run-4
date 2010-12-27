10f4b88355e610e014d1f56629c62cf86ea5de4b
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-27 19:21:08 +0000

    2010-12-27  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Martin Robinson.
    
            Remove unneeded includes from our headers.
    
            * WebCoreSupport/PasteboardHelperGtk.h: ditto.
            * webkit/webkitprivate.h: ditto.
            * webkit/webkitsoupauthdialog.h: ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitsoupauthdialog.h
+++ third_party/WebKit/WebKit/gtk/webkit/webkitsoupauthdialog.h
@@ -18,13 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
-#include <gtk/gtk.h>
-#include <libsoup/soup.h>
-
-#include <webkit/webkitdefines.h>
-
 #ifndef webkitsoupauthdialog_h
 #define webkitsoupauthdialog_h
+
+#include <gtk/gtk.h>
+#include <webkit/webkitdefines.h>
 
 G_BEGIN_DECLS
 
