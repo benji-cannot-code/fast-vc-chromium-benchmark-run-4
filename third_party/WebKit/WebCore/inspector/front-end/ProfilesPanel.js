5154d0846fce1f8127c13080bf72fb5079662ec7
mnaganov@chromium.org <mnaganov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-28 17:24:06 +0000

    2010-12-28  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Pavel Feldman.
    
            [Chromium] Fix memory leak in Profiles tab concerned with heap profiles views.
    
            https://bugs.webkit.org/show_bug.cgi?id=51680
    
            * inspector/front-end/ProfilesPanel.js:
            (WebInspector.ProfilesPanel.prototype._reset):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/ProfilesPanel.js
+++ third_party/WebKit/WebCore/inspector/front-end/ProfilesPanel.js
@@ -213,6 +213,8 @@ WebInspector.ProfilesPanel.prototype = {
 
         this.profileViewStatusBarItemsContainer.removeChildren();
 
+        this.removeAllListeners();
+
         this._updateInterface();
         this.welcomeView.show();
     },
