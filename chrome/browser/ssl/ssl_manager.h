20949c098465c17c087c3b8d8fb9ab32c63be452
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-29 22:37:45 +0000

    Revert 70281 - Revert 70271 - Remove wstring from l10n_util. Part 3.
    BUG=9911
    TEST=no visible changes; all tests pass
    
    Review URL: http://codereview.chromium.org/6044006
    
    TBR=avi@chromium.org
    Review URL: http://codereview.chromium.org/6051012
    
    TBR=avi@chromium.org
    Review URL: http://codereview.chromium.org/5989012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70283 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_manager.h
+++ chrome/browser/ssl/ssl_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/scoped_ptr.h"
+#include "base/string16.h"
 #include "chrome/browser/ssl/ssl_policy_backend.h"
 #include "chrome/common/notification_observer.h"
 #include "chrome/common/notification_registrar.h"
@@ -67,7 +68,7 @@ class SSLManager : public NotificationObserver {
                                       int* connection_status);
 
   // Returns "<organization_name> [<country>]".
-  static std::wstring GetEVCertName(const net::X509Certificate& cert);
+  static string16 GetEVCertName(const net::X509Certificate& cert);
 
   // Construct an SSLManager for the specified tab.
   // If |delegate| is NULL, SSLPolicy::GetDefaultPolicy() is used.
