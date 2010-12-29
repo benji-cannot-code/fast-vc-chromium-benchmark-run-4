0f65240278e0cc16d3eca67e01bd7d496d731720
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-29 19:13:20 +0000

    2010-12-29  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r74735.
            http://trac.webkit.org/changeset/74735
            https://bugs.webkit.org/show_bug.cgi?id=51715
    
            assert failures on fast/forms/input-maxlength-ime-
            completed.html (Requested by mihaip on #webkit).
    
            * platform/chromium/test_expectations.txt:
    2010-12-29  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r74735.
            http://trac.webkit.org/changeset/74735
            https://bugs.webkit.org/show_bug.cgi?id=51715
    
            assert failures on fast/forms/input-maxlength-ime-
            completed.html (Requested by mihaip on #webkit).
    
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::setMarkedText):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebFrameImpl.cpp
@@ -1082,6 +1082,8 @@ void WebFrameImpl::setMarkedText(
 {
     Editor* editor = frame()->editor();
 
+    editor->confirmComposition(text);
+
     Vector<CompositionUnderline> decorations;
     editor->setComposition(text, decorations, location, length);
 }
