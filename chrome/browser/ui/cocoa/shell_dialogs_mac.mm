331f6df9c84eefe52c1c402b7c0bf325454f6269
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-29 18:21:29 +0000

    Mac: Try to fix a crash related to save panels.
    All the stacks are always in the destructors. Wild guess: Maybe NSSavePanel takes the context object from the delegate instead of self. Pass in the delegate as described by the docs.
    
    BUG=58959
    TEST=maybe fewer crashes on the crash server
    
    Review URL: http://codereview.chromium.org/6031008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/shell_dialogs_mac.mm
+++ chrome/browser/ui/cocoa/shell_dialogs_mac.mm
@@ -37,7 +37,7 @@ class SelectFileDialogImpl;
 - (id)initWithSelectFileDialogImpl:(SelectFileDialogImpl*)s;
 - (void)endedPanel:(NSSavePanel*)panel
         withReturn:(int)returnCode
-           context:(void *)context;
+           context:(void*)context;
 
 // NSSavePanel delegate method
 - (BOOL)panel:(id)sender shouldShowFilename:(NSString *)filename;
@@ -127,7 +127,7 @@ SelectFileDialogImpl::~SelectFileDialogImpl() {
 
   for (std::vector<NSSavePanel*>::iterator it = panels.begin();
        it != panels.end(); ++it) {
-    [(*it) cancel:nil];
+    [*it cancel:*it];
   }
 }
 
@@ -217,8 +217,8 @@ void SelectFileDialogImpl::SelectFile(
   SheetContext* context = new SheetContext;
 
   // |context| should never be NULL, but we are seeing indications otherwise.
-  // |This CHECK is here to confirm if we are actually getting NULL
-  // ||context|s. http://crbug.com/58959
+  // This CHECK is here to confirm if we are actually getting NULL
+  // |context|s. http://crbug.com/58959
   CHECK(context);
   context->type = type;
   context->owning_window = owning_window;
@@ -362,7 +362,7 @@ bool SelectFileDialogImpl::ShouldEnableFilename(NSSavePanel* dialog,
 
 - (void)endedPanel:(NSSavePanel*)panel
         withReturn:(int)returnCode
-           context:(void *)context {
+           context:(void*)context {
   // |context| should never be NULL, but we are seeing indications otherwise.
   // |This CHECK is here to confirm if we are actually getting NULL
   // ||context|s. http://crbug.com/58959
