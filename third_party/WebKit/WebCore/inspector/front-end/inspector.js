6844c37ecf42023e63ab0222f9c13f65bd6834cd
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-30 17:26:30 +0000

    2010-12-30  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Pasting a style property with value should automatically split it into prop/value
            https://bugs.webkit.org/show_bug.cgi?id=51581
    
            The "paste" DOM event is handled for the CSS property name field to parse out the name and value parts
            of a CSS property being pasted (by the first ':' found). The property is committed (if not a new one),
            and the edit focus is transferred to the value field.
    
            * inspector/front-end/StylesSidebarPane.js:
            (WebInspector.StylePropertyTreeElement.prototype.selectElement):
            (WebInspector.StylePropertyTreeElement.prototype):
            * inspector/front-end/inspector.js:
            (WebInspector.completeURL): Drive-by: return full URLs as-is.
            (WebInspector.startEditing.cleanUpAfterEditing):
            (WebInspector.startEditing):
            (WebInspector.startEditing.pasteEventListener):
            (WebInspector.startEditing.keyDownEventListener):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74799 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/WebCore/inspector/front-end/inspector.js
@@ -1548,6 +1548,13 @@ WebInspector.resourceURLForRelatedNode = function(node, url)
 
 WebInspector.completeURL = function(baseURL, href)
 {
+    if (href) {
+        // Return absolute URLs as-is.
+        var parsedHref = href.asParsedURL();
+        if (parsedHref && parsedHref.scheme)
+            return href;
+    }
+
     var parsedURL = baseURL.asParsedURL();
     if (parsedURL) {
         var path = href;
@@ -1754,6 +1761,7 @@ WebInspector.isEditingAnyField = function()
 // commitHandler: Function - handles editing "commit" outcome
 // cancelHandler: Function - handles editing "cancel" outcome
 // customFinishHandler: Function - custom finish handler for the editing session (invoked on keydown)
+// pasteHandler: Function - handles the "paste" event, return values are the same as those for customFinishHandler
 // multiline: Boolean - whether the edited element is multiline
 WebInspector.startEditing = function(element, config)
 {
@@ -1765,6 +1773,7 @@ WebInspector.startEditing = function(element, config)
     config = config || {};
     var committedCallback = config.commitHandler;
     var cancelledCallback = config.cancelHandler;
+    var pasteCallback = config.pasteHandler;
     var context = config.context;
     var oldText = getContent(element);
     var moveDirection = "";
@@ -1797,6 +1806,8 @@ WebInspector.startEditing = function(element, config)
 
         element.removeEventListener("blur", blurEventListener, false);
         element.removeEventListener("keydown", keyDownEventListener, true);
+        if (pasteCallback)
+            element.removeEventListener("paste", pasteEventListener, true);
 
         if (element === WebInspector.currentFocusElement || element.isAncestor(WebInspector.currentFocusElement))
             WebInspector.currentFocusElement = WebInspector.previousFocusElement;
@@ -1834,10 +1845,8 @@ WebInspector.startEditing = function(element, config)
             return "move-" + (event.shiftKey ? "backward" : "forward");
     }
 
-    function keyDownEventListener(event)
+    function handleEditingResult(result, event)
     {
-        var handler = config.customFinishHandler || defaultFinishHandler;
-        var result = handler(event);
         if (result === "commit") {
             editingCommitted.call(element);
             event.preventDefault();
@@ -1853,8 +1862,23 @@ WebInspector.startEditing = function(element, config)
         }
     }
 
+    function pasteEventListener(event)
+    {
+        var result = pasteCallback(event);
+        handleEditingResult(result, event);
+    }
+
+    function keyDownEventListener(event)
+    {
+        var handler = config.customFinishHandler || defaultFinishHandler;
+        var result = handler(event);
+        handleEditingResult(result, event);
+    }
+
     element.addEventListener("blur", blurEventListener, false);
     element.addEventListener("keydown", keyDownEventListener, true);
+    if (pasteCallback)
+        element.addEventListener("paste", pasteEventListener, true);
 
     WebInspector.currentFocusElement = element;
     return {
