2888771ce6f0118cea40ab799df186677a4e357c
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-30 18:45:01 +0000

    Fix include to fix unit test.
    TEST=it compiles
    BUG=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70317 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_net_log_unittest.cc
+++ chrome/browser/net/chrome_net_log_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/chrome_net_log.h"
 
 #include "base/waitable_event.h"
-#include "base/simple_thread.h"
+#include "base/threading/simple_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
