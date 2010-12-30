4de96514ee828fc0ce1ff233d455c47217c7da65
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-30 03:16:05 +0000

    2010-12-29  Kenichi Ishibashi  <bashi@google.com>
            Reviewed by Darin Adler.
    
            Use a HashMap for m_continuation to save memory
            https://bugs.webkit.org/show_bug.cgi?id=43716
    
            Saving memory consumption by applying a HashMap convention to
            continuation pointers of RenderInline and RenderBlock classes.
    
            * rendering/RenderBlock.cpp: Removed m_continuation.
            (WebCore::RenderBlock::RenderBlock):
            (WebCore::RenderBlock::destroy):
            (WebCore::RenderBlock::inlineElementContinuation):
            (WebCore::RenderBlock::blockElementContinuation):
            * rendering/RenderBlock.h: Removed m_continuation.
            * rendering/RenderBoxModelObject.cpp: Added a hash map for continuations.
            (WebCore::RenderBoxModelObject::destroy): Added an assertion.
            (WebCore::RenderBoxModelObject::continuation): Added.
            (WebCore::RenderBoxModelObject::setContinuation): Added.
            * rendering/RenderBoxModelObject.h:
            * rendering/RenderInline.cpp: Removed m_continuation.
            (WebCore::RenderInline::RenderInline):
            (WebCore::RenderInline::destroy):
            (WebCore::RenderInline::inlineElementContinuation):
            * rendering/RenderInline.h: Removed m_continuation.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderInline.cpp
+++ third_party/WebKit/WebCore/rendering/RenderInline.cpp
@@ -47,7 +47,6 @@ namespace WebCore {
 
 RenderInline::RenderInline(Node* node)
     : RenderBoxModelObject(node)
-    , m_continuation(0)
     , m_lineHeight(-1)
 {
     setChildrenInline(true);
@@ -62,9 +61,10 @@ void RenderInline::destroy()
     // Destroy our continuation before anything other than anonymous children.
     // The reason we don't destroy it before anonymous children is that they may
     // have continuations of their own that are anonymous children of our continuation.
-    if (m_continuation) {
-        m_continuation->destroy();
-        m_continuation = 0;
+    RenderBoxModelObject* continuation = this->continuation();
+    if (continuation) {
+        continuation->destroy();
+        setContinuation(0);
     }
     
     if (!documentBeingDestroyed()) {
@@ -96,9 +96,10 @@ void RenderInline::destroy()
 
 RenderInline* RenderInline::inlineElementContinuation() const
 {
-    if (!m_continuation || m_continuation->isInline())
-        return toRenderInline(m_continuation);
-    return toRenderBlock(m_continuation)->inlineElementContinuation();
+    RenderBoxModelObject* continuation = this->continuation();
+    if (!continuation || continuation->isInline())
+        return toRenderInline(continuation);
+    return toRenderBlock(continuation)->inlineElementContinuation();
 }
 
 void RenderInline::updateBoxModelInfoFromStyle()
