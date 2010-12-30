dca7d7f1edf6528fa349cf6e97602504ce29a541
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-30 22:31:41 +0000

    Fix ChromeOS buil by making hwnd_util conditional on Windows.
    TEST=it compiles
    BUG=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70323 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/infobars/infobars.cc
+++ chrome/browser/ui/views/infobars/infobars.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
 #include "app/slide_animation.h"
-#include "app/win/hwnd_util.h"
 #include "base/message_loop.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/views/event_utils.h"
@@ -25,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/widget.h"
 
 #if defined(OS_WIN)
-#include "app/win_util.h"
+#include "app/win/hwnd_util.h"
 #endif
 
 // static
