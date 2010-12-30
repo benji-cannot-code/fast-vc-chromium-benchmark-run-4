972597d2f8eda79f6fcdd0daf6ac39cf0b3722a5
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-30 07:20:22 +0000

    2010-12-29  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed WinCE buildfix.
    
            * WinCELauncher/main.cpp: Add missing include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WinCELauncher/main.cpp
+++ third_party/WebKit/Tools/WinCELauncher/main.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebView.h"
 #include <windows.h>
+#include <wtf/PassOwnPtr.h>
 
 static const LPCWSTR kMainWindowTitle = L"WebKit for WinCE";
 static const LPCWSTR kMainWindowClassName = L"MainWindowClass";
