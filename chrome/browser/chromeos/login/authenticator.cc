e655ac641b10c8b9f3628715b783c06c5023e612
cmasone@chromium.org <cmasone@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-30 19:09:45 +0000

    [Chrome OS] Respect periods in google apps account names
    When logging in to @gmail.com accounts via the web, . characters are
    ignored -- which we do on chrome OS.  When logging into google apps
    accounts for OTHER domains, . characters should NOT be ignored.  We
    were canonicalizing them out on Chrome OS, breaking login for apps
    users who have . embedded in their usernames.
    
    BUG=chromium-os:10397
    TEST=unit tests, install on device and try cmasone@gmail.com, c.masone@gmail.com, why.you@mydomain.org and whyyou@mydomain.org.  The first three work, the last one does not, as expected.
    
    Review URL: http://codereview.chromium.org/6089004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70318 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/authenticator.cc
+++ chrome/browser/chromeos/login/authenticator.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 class LoginStatusConsumer;
 
+// static
+const char Authenticator::kSpecialCaseDomain[] = "gmail.com";
+
 Authenticator::Authenticator(LoginStatusConsumer* consumer)
     : consumer_(consumer) {
 }
@@ -27,7 +30,11 @@ std::string Authenticator::Canonicalize(const std::string& email_address) {
   char at = '@';
   base::SplitString(email_address, at, &parts);
   DCHECK_EQ(parts.size(), 2U) << "email_address should have only one @";
-  RemoveChars(parts[0], ".", &parts[0]);
+  if (parts[1] == kSpecialCaseDomain)  // only strip '.' for gmail accounts.
+    RemoveChars(parts[0], ".", &parts[0]);
+  // Technically the '+' handling here could be removed, as the google
+  // account servers do not tolerate them, so we don't need to either.
+  // TODO(cmasone): remove this, unless this code becomes obsolete altogether.
   if (parts[0].find('+') != std::string::npos)
     parts[0].erase(parts[0].find('+'));
   std::string new_email = StringToLowerASCII(JoinString(parts, at));
