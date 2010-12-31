d8f1e5934b50350d71b901d1d7079a43bf2dfaaa
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2010-12-31 11:59:08 +0000

    2010-12-31  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed build fix after r74800.
    
            * bindings/js/JSDOMWindowCustom.cpp: Add missing header.
            * bindings/js/JSLocationCustom.cpp: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/bindings/js/JSDOMWindowCustom.cpp
+++ third_party/WebKit/WebCore/bindings/js/JSDOMWindowCustom.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Settings.h"
 #include "SharedWorkerRepository.h"
 #include <runtime/JSFunction.h>
+#include <runtime/PrototypeFunction.h>
 
 #if ENABLE(3D_CANVAS) || ENABLE(BLOB)
 #include "JSArrayBuffer.h"
