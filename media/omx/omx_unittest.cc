fa344f9d804c8a93e800ffead2ea18feb1ad08d9
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-01 23:30:07 +0000

    Fix build by removing unused include.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70364 0039d316-1c4b-4281-b951-d872f2087c98

--- media/omx/omx_unittest.cc
+++ media/omx/omx_unittest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
-#include "base/condition_variable.h"
 #include "base/logging.h"
 #include "base/waitable_event.h"
 #include "testing/gtest/include/gtest/gtest.h"
