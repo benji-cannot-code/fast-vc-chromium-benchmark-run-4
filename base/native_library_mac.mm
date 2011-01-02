60bdb56a750b03cb7a6b358fc08d81e84cc6857b
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-02 20:10:05 +0000

    Fix the mac build by fixing some include paths
    TEST=none
    BUG=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70375 0039d316-1c4b-4281-b951-d872f2087c98

--- base/native_library_mac.mm
+++ base/native_library_mac.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/string_util.h"
-#include "base/thread_restrictions.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/utf_string_conversions.h"
 
 namespace base {
