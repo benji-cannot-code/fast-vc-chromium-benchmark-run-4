0968b39e3d2387df0ae91e96476c3bc541885b6b
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-03 21:40:58 +0000

    Shrink RootInlineBox https://bugs.webkit.org/show_bug.cgi?id=51847
    Reviewed by Darin Adler.
    
    * rendering/RootInlineBox.cpp:
    (WebCore::RootInlineBox::alignBoxesInBlockDirection): Changed to use the baselineType()
    accessor.
    * rendering/RootInlineBox.h: Rolled m_baselineType into a bitfield. Changed its type to
    unsigned because otherwise MSVC treats it as signed.
    (WebCore::RootInlineBox::baselineType): Added a cast.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RootInlineBox.cpp
+++ third_party/WebKit/WebCore/rendering/RootInlineBox.cpp
@@ -239,7 +239,7 @@ int RootInlineBox::alignBoxesInBlockDirection(int heightOfBlock, GlyphOverflowAn
     m_baselineType = requiresIdeographicBaseline(textBoxDataMap) ? IdeographicBaseline : AlphabeticBaseline;
 
     computeLogicalBoxHeights(maxPositionTop, maxPositionBottom, maxAscent, maxDescent, setMaxAscent, setMaxDescent, noQuirksMode,
-                             textBoxDataMap, m_baselineType, verticalPositionCache);
+                             textBoxDataMap, baselineType(), verticalPositionCache);
 
     if (maxAscent + maxDescent < max(maxPositionTop, maxPositionBottom))
         adjustMaxAscentAndDescent(maxAscent, maxDescent, maxPositionTop, maxPositionBottom);
@@ -253,7 +253,7 @@ int RootInlineBox::alignBoxesInBlockDirection(int heightOfBlock, GlyphOverflowAn
     bool hasAnnotationsBefore = false;
     bool hasAnnotationsAfter = false;
     placeBoxesInBlockDirection(heightOfBlock, maxHeight, maxAscent, noQuirksMode, lineTop, lineBottom, setLineTop,
-                               lineTopIncludingMargins, lineBottomIncludingMargins, hasAnnotationsBefore, hasAnnotationsAfter, m_baselineType);
+                               lineTopIncludingMargins, lineBottomIncludingMargins, hasAnnotationsBefore, hasAnnotationsAfter, baselineType());
     m_hasAnnotationsBefore = hasAnnotationsBefore;
     m_hasAnnotationsAfter = hasAnnotationsAfter;
     setLineTopBottomPositions(lineTop, lineBottom);
