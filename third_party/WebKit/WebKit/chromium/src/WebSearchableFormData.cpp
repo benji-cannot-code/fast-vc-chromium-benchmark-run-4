15b762c6d70f505f968e27d7672827092f7c8a86
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-03 17:59:48 +0000

    2011-01-03  Daniel Bates  <dbates@rim.com>
            Attempt to fix the Chromium Linux Release build after changeset 74895 <http://trac.webkit.org/changeset/74895>.
    
            The portion of the patch in <https://bugs.webkit.org/show_bug.cgi?id=51791> that modified the
            file src/WebSearchableFormData.cpp was not landed (why?). Although the change log entry
            associated with this change was landed.
    
            * src/WebSearchableFormData.cpp:
            (HTMLNames::IsInDefaultState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/chromium/src/WebSearchableFormData.cpp
+++ third_party/WebKit/WebKit/chromium/src/WebSearchableFormData.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFormElement.h"
 
 using namespace WebCore;
+using namespace HTMLNames;
 
 namespace {
 
@@ -129,13 +130,13 @@ bool IsSelectInDefaultState(const HTMLSelectElement* select)
 // Returns true if the form element is in its default state, false otherwise.
 // The default state is the state of the form element on initial load of the
 // page, and varies depending upon the form element. For example, a checkbox is
-// in its default state if the checked state matches the defaultChecked state.
+// in its default state if the checked state matches the state of the checked attribute.
 bool IsInDefaultState(const HTMLFormControlElement* formElement)
 {
     if (formElement->hasTagName(HTMLNames::inputTag)) {
         const HTMLInputElement* inputElement = static_cast<const HTMLInputElement*>(formElement);
         if (inputElement->isCheckbox() || inputElement->isRadioButton())
-            return inputElement->checked() == inputElement->defaultChecked();
+            return inputElement->checked() == inputElement->hasAttribute(checkedAttr);
     } else if (formElement->hasTagName(HTMLNames::selectTag))
         return IsSelectInDefaultState(static_cast<const HTMLSelectElement*>(formElement));
     return true;
