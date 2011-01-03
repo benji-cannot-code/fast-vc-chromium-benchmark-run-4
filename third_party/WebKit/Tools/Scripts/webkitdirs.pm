d0f5b3b8836a1615062c1d70eb449f47ad5a11d1
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-03 21:04:17 +0000

    2011-01-03  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by David Kilzer.
    
            Add WinCE support to build-webkit
            https://bugs.webkit.org/show_bug.cgi?id=51642
    
            * Scripts/build-webkit:
            * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -64,6 +64,7 @@ my $isQt;
 my $isSymbian;
 my %qtFeatureDefaults;
 my $isGtk;
+my $isWinCE;
 my $isWx;
 my $isEfl;
 my @wxArgs;
@@ -273,6 +274,7 @@ sub argumentsForConfiguration()
     push(@args, '--symbian') if isSymbian();
     push(@args, '--gtk') if isGtk();
     push(@args, '--efl') if isEfl();
+    push(@args, '--wince') if isWinCE();
     push(@args, '--wx') if isWx();
     push(@args, '--chromium') if isChromium();
     push(@args, '--inspector-frontend') if isInspectorFrontend();
@@ -610,6 +612,9 @@ sub builtDylibPathForName
     if (isEfl()) {
         return "$configurationProductDir/$libraryName/../.libs/libewebkit.so";
     }
+    if (isWinCE()) {
+        return "$configurationProductDir/$libraryName";
+    }
     if (isAppleMacWebKit()) {
         return "$configurationProductDir/$libraryName.framework/Versions/A/$libraryName";
     }
@@ -763,6 +768,18 @@ sub determineIsGtk()
     $isGtk = checkForArgumentAndRemoveFromARGV("--gtk");
 }
 
+sub isWinCE()
+{
+    determineIsWinCE();
+    return $isWinCE;
+}
+
+sub determineIsWinCE()
+{
+    return if defined($isWinCE);
+    $isWinCE = checkForArgumentAndRemoveFromARGV("--wince");
+}
+
 sub isWx()
 {
     determineIsWx();
@@ -877,7 +894,7 @@ sub isLinux()
 
 sub isAppleWebKit()
 {
-    return !(isQt() or isGtk() or isWx() or isChromium() or isEfl());
+    return !(isQt() or isGtk() or isWx() or isChromium() or isEfl() or isWinCE());
 }
 
 sub isAppleMacWebKit()
@@ -964,7 +981,7 @@ sub relativeScriptsDir()
 sub launcherPath()
 {
     my $relativeScriptsPath = relativeScriptsDir();
-    if (isGtk() || isQt() || isWx() || isEfl()) {
+    if (isGtk() || isQt() || isWx() || isEfl() || isWinCE()) {
         return "$relativeScriptsPath/run-launcher";
     } elsif (isAppleWebKit()) {
         return "$relativeScriptsPath/run-safari";
@@ -983,6 +1000,8 @@ sub launcherName()
         return "Safari";
     } elsif (isEfl()) {
         return "EWebLauncher";
+    } elsif (isWinCE()) {
+        return "WinCELauncher";
     }
 }
 
@@ -1440,10 +1459,19 @@ sub buildCMakeProject($@)
     my $dir = File::Spec->canonpath(baseProductDir());
     my $config = configuration();
     my $result;
+    my $cmakeBuildArgs = "";
     my $makeArgs = "";
     my @buildArgs;
-    
-    $makeArgs .= " -j" . numberOfCPUs() if ($makeArgs !~ m/-j\s*\d+/);
+
+    if ($port =~ m/wince/i) {
+        if ($config =~ m/debug/i) {
+            $cmakeBuildArgs .= " --config Debug";
+        } elsif ($config =~ m/release/i) {
+            $cmakeBuildArgs .= " --config Release";
+        }
+    } else {
+        $makeArgs .= " -j" . numberOfCPUs() if ($makeArgs !~ m/-j\s*\d+/);
+    }
 
     if ($clean) {
         print "Cleaning the build directory '$dir'\n";
@@ -1452,7 +1480,7 @@ sub buildCMakeProject($@)
         $result = 0;
     } else {
         my $cmakebin = "cmake";
-        my $make = "make";
+        my $cmakeBuildCommand = $cmakebin . " --build .";
 
         push @buildArgs, "-DPORT=$port";
 
@@ -1485,8 +1513,10 @@ sub buildCMakeProject($@)
             die "Failed while running $cmakebin to generate makefiles!\n";
         }
 
-        print "Calling '$make $makeArgs' in " . $dir . "\n\n";
-        $result = system "$make $makeArgs";
+        $cmakeBuildArgs .= " -- " . $makeArgs;
+
+        print "Calling '$cmakeBuildCommand $cmakeBuildArgs' in " . $dir . "\n\n";
+        $result = system "$cmakeBuildCommand $cmakeBuildArgs";
         if ($result ne 0) {
             die "Failed to build $port port\n";
         }
@@ -1501,6 +1531,12 @@ sub buildCMakeEflProject($@)
 {
     my ($clean, @buildArgs) = @_;
     return buildCMakeProject("Efl", $clean, @buildArgs);
+}
+
+sub buildCMakeWinCEProject($@)
+{
+    my ($sdk, $clean, @buildArgs) = @_;
+    return buildCMakeProject("WinCE -DCMAKE_WINCE_SDK=\"" . $sdk . "\"", $clean, @buildArgs);
 }
 
 sub buildQMakeProject($@)
