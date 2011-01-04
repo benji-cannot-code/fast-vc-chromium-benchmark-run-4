7beedaf2c55f545842cbeeed198680d6e98507dc
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-04 22:49:35 +0000

    Don't use macros for the key names.
    Reviewed by Darin Adler.
    
    * UIProcess/mac/TextCheckerMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/mac/TextCheckerMac.mm
+++ third_party/WebKit/WebKit2/UIProcess/mac/TextCheckerMac.mm
@@ -28,9 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TextCheckerState.h"
 
-#define WebAutomaticSpellingCorrectionEnabled @"WebAutomaticSpellingCorrectionEnabled"
-#define WebContinuousSpellCheckingEnabled @"WebContinuousSpellCheckingEnabled"
-#define WebGrammarCheckingEnabled @"WebGrammarCheckingEnabled"
+static const NSString * const WebAutomaticSpellingCorrectionEnabled = @"WebAutomaticSpellingCorrectionEnabled";
+static const NSString * const WebContinuousSpellCheckingEnabled = @"WebContinuousSpellCheckingEnabled";
+static const NSString * const WebGrammarCheckingEnabled = @"WebGrammarCheckingEnabled";
 
 namespace WebKit {
 
