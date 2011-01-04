44c85c1d92dfcfd3c1eb8b461fc1f73dbe55c68a
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-04 17:21:13 +0000

    2011-01-04  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Dan Bernstein.
    
            Clicking on the first or the last letter of LTR/RTL text in a RTL/LTR block puts caret on the opposite side.
            https://bugs.webkit.org/show_bug.cgi?id=50992
    
            Fixed the bug by interchanging the offset when the direction of inline text box and the containing block
            does not match. Reused the code added by http://trac.webkit.org/changeset/73553.
    
            Test: editing/selection/caret-bidi-first-and-last-letters.html
    
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::offsetForPosition):
    2011-01-04  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Dan Bernstein.
    
            Clicking on the first or the last letter of LTR/RTL text in a RTL/LTR block puts caret on the opposite side.
            https://bugs.webkit.org/show_bug.cgi?id=50992
    
            Added a test to ensure WebKit places the caret on the side user clicked even when text's natural direction
            and the containing block's direction do not match.
    
            * editing/selection/caret-bidi-first-and-last-letters-expected.txt: Added.
            * editing/selection/caret-bidi-first-and-last-letters.html: Added.
            * editing/selection/resources/caret-edge-shared.js: Avoid calling verify() outside of DRT.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@74971 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/editing/selection/resources/caret-edge-shared.js
+++ third_party/WebKit/LayoutTests/editing/selection/resources/caret-edge-shared.js
@@ -3,10 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function runTest() {
     var div = document.getElementsByTagName('div')[0];
 
-    if (!window.layoutTestController) {
-        div.addEventListener('mouseup', verify);
+    if (!window.layoutTestController)
         return;
-    }
 
     if (clickOn == 'left')
         x = 5;
