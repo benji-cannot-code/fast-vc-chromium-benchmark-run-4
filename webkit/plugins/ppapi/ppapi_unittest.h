eeb841272aa7f14e64bbe9c141c88bed245033c6
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-04 22:31:21 +0000

    Re-use existing out-of-process plugins if one is already loaded. Otherwise sites with multiple plugins will load that many child processes, which is bad.
    This works by having creators of out-of-process plugins add it to a list in the
    PepperPluginRegistry. The items are deleted from this list by a new delegate
    implementation for the module to tell the registry that it's being destroyed.
    
    It's a little awkward since the PluginModule can't know about the
    PepperPluginRegistry, and all other pepper "delegates" are associated with the
    Instance/RenderView, so by definition can't be used when the module is being
    torn down since there are no instances.
    
    It might have been nice to have an Add function on the lifetime delegate so
    that the module will add itself to the list. But the plugin path name is
    currently not available in all code paths, and it seemed messy to add.
    
    TEST=manual
    BUG=none
    Review URL: http://codereview.chromium.org/6085007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70457 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppapi_unittest.h
+++ webkit/plugins/ppapi/ppapi_unittest.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "webkit/plugins/ppapi/plugin_delegate.h"
 
 namespace webkit {
 namespace ppapi {
@@ -18,7 +19,8 @@ class MockPluginDelegate;
 class PluginInstance;
 class PluginModule;
 
-class PpapiUnittest : public testing::Test {
+class PpapiUnittest : public testing::Test,
+                      public webkit::ppapi::PluginDelegate::ModuleLifetime {
  public:
   PpapiUnittest();
   virtual ~PpapiUnittest();
@@ -42,6 +44,9 @@ class PpapiUnittest : public testing::Test {
   // Note: module must be declared first since we want it to get destroyed last.
   scoped_refptr<PluginModule> module_;
   scoped_refptr<PluginInstance> instance_;
+
+  // ModuleLifetime implementation.
+  virtual void PluginModuleDestroyed(PluginModule* destroyed_module);
 
   DISALLOW_COPY_AND_ASSIGN(PpapiUnittest);
 };
