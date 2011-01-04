1f105d4308a8e48099c4ce65bf51cfe0fb0ef863
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-04 23:35:06 +0000

    Store more data per history entry in the session state blob.
    Reviewed by Darin Adler.
    
    * UIProcess/WebBackForwardListItem.cpp:
    (WebKit::WebBackForwardListItem::WebBackForwardListItem):
    * UIProcess/WebBackForwardListItem.h:
    (WebKit::WebBackForwardListItem::create):
    
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::addBackForwardItem):
    
    * UIProcess/cf/WebBackForwardListCF.cpp:
    (WebKit::WebBackForwardList::createCFDictionaryRepresentation):
    
    * UIProcess/cf/WebPageProxyCF.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/WebBackForwardListItem.cpp
+++ third_party/WebKit/WebKit2/UIProcess/WebBackForwardListItem.cpp
@@ -28,13 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-WebBackForwardListItem::WebBackForwardListItem(const String& originalURL, const String& url, const String& title, const Vector<uint8_t>& backForwardData, uint64_t itemID)
+WebBackForwardListItem::WebBackForwardListItem(const String& originalURL, const String& url, const String& title, const uint8_t* backForwardData, size_t backForwardDataSize, uint64_t itemID)
     : m_originalURL(originalURL)
     , m_url(url)
     , m_title(title)
     , m_itemID(itemID)
-    , m_backForwardData(backForwardData)
 {
+    m_backForwardData.reserveCapacity(backForwardDataSize);
+    m_backForwardData.append(backForwardData, backForwardDataSize);
 }
 
 WebBackForwardListItem::~WebBackForwardListItem()
