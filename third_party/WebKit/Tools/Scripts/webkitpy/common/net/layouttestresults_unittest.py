1b3c05ad5ac1b07b9a0920bff7d52a71d7d6c93a
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-05 23:57:02 +0000

    2011-01-05  Eric Seidel  <eric@webkit.org>
            Unreviewed.
    
            Move LayoutTestResults over to new-run-webkit-tests TestResult architecture
            https://bugs.webkit.org/show_bug.cgi?id=51802
    
            Add another unit test to cover a previously missing import.
    
            * Scripts/webkitpy/common/net/layouttestresults.py:
            * Scripts/webkitpy/common/net/layouttestresults_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/layouttestresults_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/layouttestresults_unittest.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.common.net.layouttestresults import LayoutTestResults
+from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.layout_tests.layout_package import test_results
 from webkitpy.layout_tests.layout_package import test_failures
 from webkitpy.thirdparty.BeautifulSoup import BeautifulSoup
@@ -76,8 +77,11 @@ class LayoutTestResultsTest(unittest.TestCase):
         self.assertEqual(len(results.failing_tests()), 0)
 
     def test_failures_from_fail_row(self):
-        row_html = "<tr><td><a>25%</a></td></tr>"
-        row = BeautifulSoup(row_html)
+        row = BeautifulSoup("<tr><td><a>25%</a></td></tr>")
         failures = LayoutTestResults._failures_from_fail_row(row)
         self.assertEqual(len(failures), 1)
         self.assertEqual(type(sorted(failures)[0]), test_failures.FailureImageHashMismatch)
+
+        row = BeautifulSoup("<tr><td><a>foo</a></td></tr>")
+        expected_stderr = "Unhandled link text in results.html parsing: foo.  Please file a bug against webkitpy.\n"
+        OutputCapture().assert_outputs(self, LayoutTestResults._failures_from_fail_row, [row], expected_stderr=expected_stderr)
