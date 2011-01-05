5dd95b53f3764911e08f48152f772f287cbe5a9d
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-05 10:51:00 +0000

    Roll DEPS for speex to get gyp changes for use_system_speex: http://src.chromium.org/viewvc/chrome?view=rev&revision=70427
    Convert all users to the shim header for speex.
    
    BUG=68524
    TEST=none
    Review URL: http://codereview.chromium.org/5968013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_recognizer.cc
+++ chrome/browser/speech/speech_recognizer.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_thread.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/net/url_request_context_getter.h"
-#include "third_party/speex/include/speex/speex.h"
+#include "third_party/speex/speex.h"
 
 using media::AudioInputController;
 using std::list;
