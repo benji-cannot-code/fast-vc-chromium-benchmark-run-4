7b8b77b5040fd32b64f545e47cd8f5cd4fd5b420
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-05 10:58:28 +0000

    base: Remove unnecessary includes from scoped_handle_win.h
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/5988009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70500 0039d316-1c4b-4281-b951-d872f2087c98

--- base/scoped_handle_win.h
+++ base/scoped_handle_win.h
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(brettw) remove this file when all callers are converted to using the
 // new location/namespace
-#include "base/win/scoped_gdi_object.h"
 #include "base/win/scoped_handle.h"
-#include "base/win/scoped_handle.h"
-#include "base/win/scoped_hdc.h"
-#include "base/win/scoped_hglobal.h"
 
 using base::win::ScopedHandle;
