1ce1ddea2fc49a9541a7daba03c1588cb2f96904
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-05 21:34:28 +0000

    2011-01-05  Eric Seidel  <eric@webkit.org>
            Unreviewed.
    
            Move LayoutTestResults over to new-run-webkit-tests TestResult architecture
            https://bugs.webkit.org/show_bug.cgi?id=51802
    
            Fix two exceptions seen on the bots after my original commit.
    
            * Scripts/webkitpy/common/net/layouttestresults.py:
            * Scripts/webkitpy/common/net/layouttestresults_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/layouttestresults.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/layouttestresults.py
@@ -67,8 +67,8 @@ class LayoutTestResults(object):
         # of new-run-webkit-test failures they equate to.
         failures = set()
         for anchor in row.findAll("a"):
-            anchor_text = anchor.string
-            if anchor_text in ["expected image", "image diffs"] or anchor_text.contains('%'):
+            anchor_text = unicode(anchor.string)
+            if anchor_text in ["expected image", "image diffs"] or '%' in anchor_text:
                 failures.add(test_failures.FailureImageHashMismatch())
             elif anchor_text in ["expected", "actual", "diff", "pretty diff"]:
                 failures.add(test_failures.FailureTextMismatch())
@@ -76,6 +76,7 @@ class LayoutTestResults(object):
                 log("Unhandled link text in results.html parsing: %s.  Please file a bug against webkitpy." % anchor_text)
         # FIXME: Its possible the row contained no links due to ORWT brokeness.
         # We should probably assume some type of failure anyway.
+        return failures
 
     @classmethod
     def _failures_from_row(cls, row, table_title):
