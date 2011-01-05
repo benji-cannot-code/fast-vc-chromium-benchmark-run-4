4fe7ab07b054cab1c27a13f4a66212e765010590
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-05 22:46:32 +0000

    2011-01-05  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Unreviewed build fix.
    
            Include unistd.h for _exit() for non-Windows builds.
    
            Build break noticed on QtWebkit Symbian builds.
    
            * Shared/ChildProcess.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/ChildProcess.cpp
+++ third_party/WebKit/WebKit2/Shared/ChildProcess.cpp
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChildProcess.h"
 
+#if !OS(WINDOWS)
+#include <unistd.h>
+#endif
+
 namespace WebKit {
 
 ChildProcess::ChildProcess()
