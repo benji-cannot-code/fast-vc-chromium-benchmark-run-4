8124c5c713f282c4fd9cccf78af053c795f0d479
piman@google.com <piman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-05 01:18:59 +0000

    Make Graphics3D::SwapBuffers take a completion callback
    BUG=none
    TEST=with demo_simple_vertex_shader, rate control works
    
    Review URL: http://codereview.chromium.org/5944001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70474 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/dev/surface_3d_dev.h
+++ ppapi/cpp/dev/surface_3d_dev.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/c/dev/ppb_surface_3d_dev.h"
 
+#include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/resource.h"
 
@@ -24,8 +25,7 @@ class Surface3D_Dev : public Resource {
                 PP_Config3D_Dev config,
                 const int32_t* attrib_list);
 
-  // TODO(alokp): Add completion callback.
-  int32_t SwapBuffers() const;
+  int32_t SwapBuffers(const CompletionCallback& cc) const;
 
  protected:
   explicit Surface3D_Dev(PP_Resource resource_id) : Resource(resource_id) {}
