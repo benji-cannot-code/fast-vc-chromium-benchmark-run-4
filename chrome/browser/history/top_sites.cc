1a24a2ca60707df91e84e604b2047c7a9f989dc7
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 09:35:37 +0000

    No-op change to trigger a build (fix a typo in a random file)
    TBR=sky
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6109002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/top_sites.cc
+++ chrome/browser/history/top_sites.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -221,7 +221,7 @@ void TopSites::GetMostVisitedURLs(CancelableRequestConsumer* consumer,
   // WARNING: this may be invoked on any thread.
   scoped_refptr<CancelableRequest<GetTopSitesCallback> > request(
       new CancelableRequest<GetTopSitesCallback>(callback));
-  // This ensures cancelation of requests when either the consumer or the
+  // This ensures cancellation of requests when either the consumer or the
   // provider is deleted. Deletion of requests is also guaranteed.
   AddRequest(request, consumer);
   MostVisitedURLList filtered_urls;
