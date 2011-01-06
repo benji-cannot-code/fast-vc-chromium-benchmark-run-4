00044a378d6634d6061e0db70e6ad061f60e2a09
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 20:53:25 +0000

    2011-01-06  Joone Hur  <joone.hur@collabora.co.uk>
            Reviewed by Eric Seidel.
    
            WML Parser should treat line/column number in a consistent way
            https://bugs.webkit.org/show_bug.cgi?id=51601
    
            Add the equality operators to TextPosition class.
    
            * wtf/text/TextPosition.h:
            (WTF::TextPosition::operator==): Added.
            (WTF::TextPosition::operator!=): Added.
            (WTF::TextPosition::belowRangePosition): Use belowBase() instead of base().
            (WTF::ZeroBasedNumber::operator==): Added.
            (WTF::ZeroBasedNumber::operator!=): Added.
            (WTF::OneBasedNumber::operator==): Added.
            (WTF::OneBasedNumber::operator!=): Added.
    2011-01-06  Joone Hur  <joone.hur@collabora.co.uk>
    
            Reviewed by Eric Seidel.
    
            WML Parser should treat line/column number in a consistent way
            https://bugs.webkit.org/show_bug.cgi?id=51601
    
            XML Parser treats line/column number as 1-based values, but WML ErrorHandler treat them as 0-based.
            Therefore, this patch allows WML ErrorHandler to use 1-based values.
    
            * dom/XMLDocumentParser.cpp:
            (WebCore::XMLDocumentParser::handleError): Treat line/column number as 1 based values.
            * dom/XMLDocumentParser.h: Make textPositionOneBased public and Add TextPosition1(m_lastErrorPosition) to keep error line/column number.
            * dom/XMLDocumentParserLibxml2.cpp:
            (WebCore::XMLDocumentParser::XMLDocumentParser): Initialize m_lastErrorPosition.
            * dom/XMLDocumentParserQt.cpp:
            (WebCore::XMLDocumentParser::XMLDocumentParser): Initialize m_lastErrorPosition.
            * wml/WMLErrorHandling.cpp:
            (WebCore::reportWMLError): Use 1 based value instead of 0 based value to report error line/column number.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/XMLDocumentParser.h
+++ third_party/WebKit/WebCore/dom/XMLDocumentParser.h
@@ -87,6 +87,7 @@ namespace WebCore {
         // Exposed for callbacks:
         enum ErrorType { warning, nonFatal, fatal };
         void handleError(ErrorType, const char* message, int lineNumber, int columnNumber);
+        void handleError(ErrorType, const char* message, TextPosition1);
 
         void setIsXHTMLDocument(bool isXHTML) { m_isXHTMLDocument = isXHTML; }
         bool isXHTMLDocument() const { return m_isXHTMLDocument; }
@@ -102,7 +103,9 @@ namespace WebCore {
 
         // WMLErrorHandling uses these functions.
         virtual bool wellFormed() const { return !m_sawError; }
+
         TextPosition0 textPosition() const;
+        TextPosition1 textPositionOneBased() const;
 
         static bool supportsXMLVersion(const String&);
 
@@ -130,13 +133,6 @@ namespace WebCore {
         void resumeParsing();
 
         bool appendFragmentSource(const String&);
-
-
-        // This method is introduced to temporary legalize existing line/column
-        // coordinate bug: it is believed that numbers that originally were zero-based
-        // eventually becomes one-based.
-        // FIXME: Investigate and get rid of this method.
-        TextPosition1 textPositionOneBased() const;
 
 #if USE(QXMLSTREAM)
 private:
@@ -211,8 +207,7 @@ public:
         bool m_finishCalled;
 
         int m_errorCount;
-        int m_lastErrorLine;
-        int m_lastErrorColumn;
+        TextPosition1 m_lastErrorPosition;
         String m_errorMessages;
 
         CachedResourceHandle<CachedScript> m_pendingScript;
