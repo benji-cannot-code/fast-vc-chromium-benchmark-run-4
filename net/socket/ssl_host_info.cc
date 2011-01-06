27cd42c80a73cec001e1d474fad47bb22aa550c6
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 19:49:43 +0000

    net: perform a non-A DNS lookup for HTTPS hosts.
    Since SSLHostInfo is enabled by default now, this will cause a DNS
    lookup for a random RR type (13172) for each HTTPS host. (Although many
    of these lookups will hit the in-process cache. Also, this won't do
    anything on non-Linux machines because I haven't gotten the DNS lookup
    code working on them yet.)
    
    At some point in the future we'll probably want to do these lookups in
    order to get information for certificate validation. This change just
    exists so that we can measure how long these lookups take.
    
    BUG=none
    TEST=none
    
    http://codereview.chromium.org/6041004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70631 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_host_info.cc
+++ net/socket/ssl_host_info.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/pickle.h"
 #include "base/string_piece.h"
+#include "net/base/dns_util.h"
+#include "net/base/dnsrr_resolver.h"
 #include "net/base/ssl_config_service.h"
 #include "net/base/x509_certificate.h"
 #include "net/socket/ssl_client_socket.h"
@@ -41,11 +43,31 @@ SSLHostInfo::SSLHostInfo(
       verifier_(cert_verifier),
       callback_(new CancelableCompletionCallback<SSLHostInfo>(
                         ALLOW_THIS_IN_INITIALIZER_LIST(this),
-                        &SSLHostInfo::VerifyCallback)) {
+                        &SSLHostInfo::VerifyCallback)),
+      dnsrr_resolver_(NULL),
+      dns_callback_(NULL),
+      dns_handle_(DnsRRResolver::kInvalidHandle) {
   state_.npn_valid = false;
 }
 
-SSLHostInfo::~SSLHostInfo() {}
+SSLHostInfo::~SSLHostInfo() {
+  if (dns_handle_ != DnsRRResolver::kInvalidHandle) {
+    dnsrr_resolver_->CancelResolve(dns_handle_);
+    delete dns_callback_;
+  }
+}
+
+void SSLHostInfo::StartDnsLookup(DnsRRResolver* dnsrr_resolver) {
+#if defined(OS_LINUX)
+  dnsrr_resolver_ = dnsrr_resolver;
+  dns_callback_ = NewCallback(this, &SSLHostInfo::DnsComplete);
+  dns_lookup_start_time_ = base::TimeTicks::Now();
+
+  dns_handle_ = dnsrr_resolver->Resolve(
+      hostname_, kDNS_CAA, DnsRRResolver::FLAG_WANT_DNSSEC, dns_callback_,
+      &dns_response_, 0, BoundNetLog());
+#endif
+}
 
 const SSLHostInfo::State& SSLHostInfo::state() const {
   return state_;
@@ -195,6 +217,15 @@ void SSLHostInfo::VerifyCallback(int rv) {
     cert_verification_callback_ = NULL;
     callback->Run(rv);
   }
+}
+
+void SSLHostInfo::DnsComplete(int rv) {
+  dns_handle_ = DnsRRResolver::kInvalidHandle;
+  dns_callback_ = NULL;
+
+  const base::TimeTicks now = base::TimeTicks::Now();
+  const base::TimeDelta elapsed = now - dns_lookup_start_time_;
+  UMA_HISTOGRAM_TIMES("Net.SSLHostInfoDNSLookup", elapsed);
 }
 
 SSLHostInfoFactory::~SSLHostInfoFactory() {}
--- net/socket/ssl_host_info.h
+++ net/socket/ssl_host_info.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/cert_verifier.h"
 #include "net/base/cert_verify_result.h"
 #include "net/base/completion_callback.h"
+#include "net/base/dnsrr_resolver.h"
 #include "net/socket/ssl_client_socket.h"
 
 namespace net {
@@ -56,6 +57,9 @@ class SSLHostInfo {
   // only be called once WaitForDataReady has returned OK or called its
   // callback.
   virtual void Persist() = 0;
+
+  // StartDnsLookup triggers a DNS lookup for the host.
+  void StartDnsLookup(DnsRRResolver* dnsrr_resolver);
 
   struct State {
     State();
@@ -120,6 +124,10 @@ class SSLHostInfo {
   // ParseInner is a helper function for Parse.
   bool ParseInner(const std::string& data);
 
+  // DnsComplete is a callback function which is called when our DNS resolution
+  // completes.
+  void DnsComplete(int rv);
+
   // This is the hostname that we'll validate the certificates against.
   const std::string hostname_;
   bool cert_parsing_failed_;
@@ -133,6 +141,12 @@ class SSLHostInfo {
   SingleRequestCertVerifier verifier_;
   scoped_refptr<X509Certificate> cert_;
   scoped_refptr<CancelableCompletionCallback<SSLHostInfo> > callback_;
+
+  DnsRRResolver* dnsrr_resolver_;
+  CompletionCallback* dns_callback_;
+  DnsRRResolver::Handle dns_handle_;
+  RRResponse dns_response_;
+  base::TimeTicks dns_lookup_start_time_;
 };
 
 class SSLHostInfoFactory {
