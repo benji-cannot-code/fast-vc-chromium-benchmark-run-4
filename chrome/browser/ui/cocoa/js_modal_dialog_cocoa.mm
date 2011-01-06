aff4a2ef4d14658b0350fa9384a74a8d0059b6b5
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 14:35:18 +0000

    [Mac] In modal JS dialogs with text fields, disable line breaks and just overflow the text box.
    BUG=65232
    TEST=See reduced test case in bug.
    
    Review URL: http://codereview.chromium.org/6028013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70614 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/js_modal_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/js_modal_dialog_cocoa.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSTextField*)textField {
   textField_ = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 300, 22)];
+  [[textField_ cell] setLineBreakMode:NSLineBreakByTruncatingTail];
   [alert_ setAccessoryView:textField_];
   [textField_ release];
 
