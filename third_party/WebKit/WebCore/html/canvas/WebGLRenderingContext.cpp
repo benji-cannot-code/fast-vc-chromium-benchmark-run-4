8b80b97543dd9f5e049f0ee78c564cbb83bf8cdb
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 19:33:58 +0000

    2010-12-28  Zhenyao Mo  <zmo@google.com>
            Reviewed by Kenneth Russell.
    
            Must generate INVALID_VALUE errors for strings containing out-of-range characters
            https://bugs.webkit.org/show_bug.cgi?id=50929
    
            * html/canvas/WebGLRenderingContext.cpp: Validate characters according to GLSL ES 1.0 spec section 3.1.
            (WebCore::WebGLRenderingContext::bindAttribLocation):
            (WebCore::WebGLRenderingContext::getAttribLocation):
            (WebCore::WebGLRenderingContext::getUniformLocation):
            (WebCore::WebGLRenderingContext::shaderSource):
            (WebCore::WebGLRenderingContext::validateString): Helper function to perform the character validation.
            * html/canvas/WebGLRenderingContext.h: Declare validateString().
    2010-12-28  Zhenyao Mo  <zmo@google.com>
    
            Reviewed by Kenneth Russell.
    
            Must generate INVALID_VALUE errors for strings containing out-of-range characters
            https://bugs.webkit.org/show_bug.cgi?id=50929
    
            * fast/canvas/webgl/invalid-passed-params-expected.txt: Add test cases for invalid characters.
            * fast/canvas/webgl/invalid-passed-params.html: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.cpp
+++ third_party/WebKit/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -87,6 +87,21 @@ namespace {
         *clippedRange = range;
     }
 
+    // Return true if a character belongs to the ASCII subset as defined in
+    // GLSL ES 1.0 spec section 3.1.
+    bool validateCharacter(unsigned char c)
+    {
+        // Printing characters are valid except " $ ` @ \ ' DEL.
+        if (c >= 32 && c <= 126
+            && c != '"' && c != '$' && c != '`' && c != '@' && c != '\\' && c != '\'')
+            return true;
+        // Horizontal tab, line feed, vertical tab, form feed, carriage return
+        // are also valid.
+        if (c >= 9 && c <= 13)
+            return true;
+        return false;
+    }
+
 } // namespace anonymous
 
 class WebGLStateRestorer {
@@ -310,6 +325,8 @@ void WebGLRenderingContext::bindAttribLocation(WebGLProgram* program, unsigned l
 {
     UNUSED_PARAM(ec);
     if (isContextLost() || !validateWebGLObject(program))
+        return;
+    if (!validateString(name))
         return;
     m_context->bindAttribLocation(objectOrZero(program), index, name);
     cleanupAfterGraphicsCall(false);
@@ -1461,6 +1478,8 @@ int WebGLRenderingContext::getAttribLocation(WebGLProgram* program, const String
 {
     if (isContextLost())
         return -1;
+    if (!validateString(name))
+        return -1;
     return m_context->getAttribLocation(objectOrZero(program), name);
 }
 
@@ -2086,6 +2105,8 @@ PassRefPtr<WebGLUniformLocation> WebGLRenderingContext::getUniformLocation(WebGL
     UNUSED_PARAM(ec);
     if (isContextLost() || !validateWebGLObject(program))
         return 0;
+    if (!validateString(name))
+        return 0;
     WebGLStateRestorer(this, false);
     long uniformLocation = m_context->getUniformLocation(objectOrZero(program), name);
     if (uniformLocation == -1)
@@ -2469,6 +2490,8 @@ void WebGLRenderingContext::shaderSource(WebGLShader* shader, const String& stri
 {
     UNUSED_PARAM(ec);
     if (isContextLost() || !validateWebGLObject(shader))
+        return;
+    if (!validateString(string))
         return;
     m_context->shaderSource(objectOrZero(shader), string);
     cleanupAfterGraphicsCall(false);
@@ -3701,6 +3724,17 @@ bool WebGLRenderingContext::validateSize(long x, long y)
     if (x < 0 || y < 0) {
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
         return false;
+    }
+    return true;
+}
+
+bool WebGLRenderingContext::validateString(const String& string)
+{
+    for (size_t i = 0; i < string.length(); ++i) {
+        if (!validateCharacter(string[i])) {
+            m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
+            return false;
+        }
     }
     return true;
 }
