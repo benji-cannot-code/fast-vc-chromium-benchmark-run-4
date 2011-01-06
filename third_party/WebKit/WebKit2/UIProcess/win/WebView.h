1c646c74c82273525215a754b3d2e87768fd69ba
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 20:47:38 +0000

    Remove WebView::m_rect
    It was only used in the constructor, and wasn't even kept up-to-date as
    the WebView's size changed.
    
    Fixes <http://webkit.org/b/52000> WebView::m_rect is unnecessary
    
    Reviewed by Ada Chan.
    
    * UIProcess/win/WebView.cpp:
    * UIProcess/win/WebView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.h
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.h
@@ -47,8 +47,6 @@ public:
     }
     ~WebView();
 
-    RECT rect() const { return m_rect; }
-
     HWND window() const { return m_window; }
     void setParentWindow(HWND);
     void windowAncestryDidChange();
@@ -138,7 +136,6 @@ private:
     // WebCore::WindowMessageListener
     virtual void windowReceivedMessage(HWND, UINT message, WPARAM, LPARAM);
 
-    RECT m_rect;
     HWND m_window;
     HWND m_topLevelParentWindow;
     HWND m_toolTipWindow;
