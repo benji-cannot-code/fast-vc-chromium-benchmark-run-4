b37d5e249324701c4dd06729c9f5d93231c70f05
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 22:05:14 +0000

    Revert 70655 - With the Mac App Store, we see that window controls are no longer tied to the top of the window. Adjust ours downward so they have space to breathe.
    BUG=68746
    TEST=window widgets are vertically centered
    
    Review URL: http://codereview.chromium.org/6100004
    
    TBR=avi@chromium.org
    Review URL: http://codereview.chromium.org/5981013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/framed_browser_window_unittest.mm
+++ chrome/browser/ui/cocoa/framed_browser_window_unittest.mm
@@ -111,9 +111,8 @@ TEST_F(FramedBrowserWindowTest, WindowWidgetLocation) {
   windowBounds.origin = NSZeroPoint;
   EXPECT_EQ(NSMaxY(closeBoxFrame),
             NSMaxY(windowBounds) -
-                kFramedWindowButtonsWithoutTabStripOffsetFromTop);
-  EXPECT_EQ(NSMinX(closeBoxFrame),
-            kFramedWindowButtonsWithoutTabStripOffsetFromLeft);
+            kFramedWindowButtonsWithoutTabStripOffsetFromTop);
+  EXPECT_EQ(NSMinX(closeBoxFrame), kFramedWindowButtonsOffsetFromLeft);
 
   NSCell* miniaturizeCell = [window_ accessibilityAttributeValue:
                              NSAccessibilityMinimizeButtonAttribute];
@@ -122,7 +121,7 @@ TEST_F(FramedBrowserWindowTest, WindowWidgetLocation) {
   NSRect miniaturizeFrame = [miniaturizeControl frame];
   EXPECT_EQ(NSMaxY(miniaturizeFrame),
             NSMaxY(windowBounds) -
-                kFramedWindowButtonsWithoutTabStripOffsetFromTop);
+            kFramedWindowButtonsWithoutTabStripOffsetFromTop);
   EXPECT_EQ(NSMinX(miniaturizeFrame),
             NSMaxX(closeBoxFrame) + kFramedWindowButtonsInterButtonSpacing);
 
@@ -146,9 +145,8 @@ TEST_F(FramedBrowserWindowTest, WindowWidgetLocation) {
   windowBounds.origin = NSZeroPoint;
   EXPECT_EQ(NSMaxY(closeBoxFrame),
             NSMaxY(windowBounds) -
-                kFramedWindowButtonsWithTabStripOffsetFromTop);
-  EXPECT_EQ(NSMinX(closeBoxFrame),
-            kFramedWindowButtonsWithTabStripOffsetFromLeft);
+            kFramedWindowButtonsWithTabStripOffsetFromTop);
+  EXPECT_EQ(NSMinX(closeBoxFrame), kFramedWindowButtonsOffsetFromLeft);
 
   miniaturizeCell = [window_ accessibilityAttributeValue:
                      NSAccessibilityMinimizeButtonAttribute];
@@ -157,7 +155,7 @@ TEST_F(FramedBrowserWindowTest, WindowWidgetLocation) {
   miniaturizeFrame = [miniaturizeControl frame];
   EXPECT_EQ(NSMaxY(miniaturizeFrame),
             NSMaxY(windowBounds) -
-                kFramedWindowButtonsWithTabStripOffsetFromTop);
+            kFramedWindowButtonsWithTabStripOffsetFromTop);
   EXPECT_EQ(NSMinX(miniaturizeFrame),
             NSMaxX(closeBoxFrame) + kFramedWindowButtonsInterButtonSpacing);
   [window_ setWindowController:nil];
