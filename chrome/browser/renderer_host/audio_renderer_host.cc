0b406fd1aafe8193161003ddf530dceade779758
inferno@chromium.org <inferno@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 20:39:33 +0000

    Turn the volume range check into a bail out return to prevent browser crash.
    BUG=68244
    TEST=None
    
    Review URL: http://codereview.chromium.org/6133001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/audio_renderer_host.cc
+++ chrome/browser/renderer_host/audio_renderer_host.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -369,7 +369,8 @@ void AudioRendererHost::OnSetVolume(const IPC::Message& msg, int stream_id,
   }
 
   // Make sure the volume is valid.
-  CHECK(volume >= 0 && volume <= 1.0);
+  if (volume < 0 || volume > 1.0)
+      return;
   entry->controller->SetVolume(volume);
 }
 
--- media/audio/audio_util.cc
+++ media/audio/audio_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,7 +94,8 @@ bool AdjustVolume(void* buf,
                   int bytes_per_sample,
                   float volume) {
   DCHECK(buf);
-  DCHECK(volume >= 0.0f && volume <= 1.0f);
+  if (volume < 0.0f || volume > 1.0f)
+      return false;
   if (volume == 1.0f) {
     return true;
   } else if (volume == 0.0f) {
@@ -130,7 +131,8 @@ bool FoldChannels(void* buf,
                   int bytes_per_sample,
                   float volume) {
   DCHECK(buf);
-  DCHECK(volume >= 0.0f && volume <= 1.0f);
+  if (volume < 0.0f || volume > 1.0f)
+      return false;
   if (channels > 2 && channels <= 8 && bytes_per_sample > 0) {
     int sample_count = buflen / (channels * bytes_per_sample);
     if (bytes_per_sample == 1) {
