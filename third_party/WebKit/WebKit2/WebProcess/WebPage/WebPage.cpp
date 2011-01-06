5bbe77d69f91d0e2332e7f55cb1cf1dd1cc1381f
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 08:41:49 +0000

    Fix build for Darin's previous change w/ the obvious renaming.
    Rubber stamped by Dan Bernstein.
    
    * WebProcess/WebPage/WebBackForwardListProxy.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::didRemoveBackForwardItem):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -1521,7 +1521,7 @@ void WebPage::setCustomTextEncodingName(const String& encoding)
 
 void WebPage::didRemoveBackForwardItem(uint64_t itemID)
 {
-    WebBackForwardListProxy::removeItem(itemID);
+    WebBackForwardListProxy::removeBackForwardItem(itemID);
 }
 
 #if PLATFORM(MAC)
