f89905a648201216e1ee2ee433ec1d23053a36fb
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 20:39:27 +0000

    AX: "AXExpanded" not exposed on most ARIA roles that use aria-expanded https://bugs.webkit.org/show_bug.cgi?id=51995
    Reviewed by Beth Dakin.
    
    WebCore: 
    
    Test: platform/mac/accessibility/aria-expanded-standard-items.html
    
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::supportsARIAExpanded):
    (WebCore::AccessibilityObject::isExpanded):
    * accessibility/AccessibilityObject.h:
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::isVisited):
    * accessibility/AccessibilityRenderObject.h:
    * accessibility/mac/AccessibilityObjectWrapper.mm:
    (-[AccessibilityObjectWrapper additionalAccessibilityAttributeNames]):
    
    LayoutTests: 
    
    * platform/mac/accessibility/aria-expanded-standard-items-expected.txt: Added.
    * platform/mac/accessibility/aria-expanded-standard-items.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.mm
+++ third_party/WebKit/WebCore/accessibility/mac/AccessibilityObjectWrapper.mm
@@ -646,6 +646,9 @@ static WebCoreTextMarkerRange* textMarkerRangeFromVisiblePositions(AXObjectCache
     if (m_object->supportsARIAOwns())
         [additional addObject:NSAccessibilityOwnsAttribute];
 
+    if (m_object->supportsARIAExpanded())
+        [additional addObject:NSAccessibilityExpandedAttribute];
+    
     if (m_object->isScrollbar())
         [additional addObject:NSAccessibilityOrientationAttribute];
     
