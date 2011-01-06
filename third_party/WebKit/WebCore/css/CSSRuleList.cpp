eb3fcf566980a8275fa77991fff22a206d43511e
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 18:52:51 +0000

    2011-01-06  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Simon Fraser.
    
            Null out the parent stylesheet pointer when a css rule is removed.
            https://bugs.webkit.org/show_bug.cgi?id=51993
    
            Tests: fast/dom/StyleSheet/removed-media-rule-deleted-parent-crash.html
                   fast/dom/StyleSheet/removed-stylesheet-rule-deleted-parent-crash.html
    
            * css/CSSRuleList.cpp:
            (WebCore::CSSRuleList::deleteRule):
            * css/CSSStyleSheet.cpp:
            (WebCore::CSSStyleSheet::deleteRule):
    2011-01-06  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Simon Fraser.
    
            Tests that we do not crash when accessing a deleted parent stylesheet
            from a removed css rule.
            https://bugs.webkit.org/show_bug.cgi?id=51993
    
            * fast/dom/StyleSheet/removed-media-rule-deleted-parent-crash-expected.txt: Added.
            * fast/dom/StyleSheet/removed-media-rule-deleted-parent-crash.html: Added.
            * fast/dom/StyleSheet/removed-stylesheet-rule-deleted-parent-crash-expected.txt: Added.
            * fast/dom/StyleSheet/removed-stylesheet-rule-deleted-parent-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/css/CSSRuleList.cpp
+++ third_party/WebKit/WebCore/css/CSSRuleList.cpp
@@ -77,6 +77,7 @@ void CSSRuleList::deleteRule(unsigned index)
         return;
     }
 
+    m_lstCSSRules[index]->setParent(0);
     m_lstCSSRules.remove(index);
 }
 
--- third_party/WebKit/WebCore/css/CSSStyleSheet.cpp
+++ third_party/WebKit/WebCore/css/CSSStyleSheet.cpp
@@ -165,6 +165,7 @@ void CSSStyleSheet::deleteRule(unsigned index, ExceptionCode& ec)
     }
 
     ec = 0;
+    item(index)->setParent(0);
     remove(index);
     styleSheetChanged();
 }
