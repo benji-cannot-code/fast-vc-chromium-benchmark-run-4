e93dc629066033d01511e41d18e20700ad9b5f80
kbr@google.com <kbr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-06 23:25:50 +0000

    Revert http://codereview.chromium.org/6094009 (r70768) due to compilation error on Mac OS X.
    BUG=65369
    TBR=apatrick
    TEST=none
    
    Review URL: http://codereview.chromium.org/6150001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/gpu/gpu_thread.h
+++ chrome/gpu/gpu_thread.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
-#include "base/command_line.h"
 #include "base/scoped_ptr.h"
 #include "base/time.h"
 #include "build/build_config.h"
@@ -23,15 +22,12 @@ namespace IPC {
 struct ChannelHandle;
 }
 
-class GpuWatchdogThread;
-
 class GpuThread : public ChildThread {
  public:
-  explicit GpuThread(const CommandLine& command_line);
+  GpuThread();
   ~GpuThread();
 
   void Init(const base::Time& process_start_time);
-  void StopWatchdog();
 
   // Remove the channel for a particular renderer.
   void RemoveChannel(int renderer_id);
@@ -41,7 +37,6 @@ class GpuThread : public ChildThread {
   virtual bool OnControlMessageReceived(const IPC::Message& msg);
 
   // Message handlers.
-  void OnInitialize();
   void OnEstablishChannel(int renderer_id);
   void OnCloseChannel(const IPC::ChannelHandle& channel_handle);
   void OnSynchronize();
@@ -58,10 +53,6 @@ class GpuThread : public ChildThread {
   static void CollectDxDiagnostics(GpuThread* thread);
   static void SetDxDiagnostics(GpuThread* thread, const DxDiagNode& node);
 #endif
-
-  CommandLine command_line_;
-  base::Time process_start_time_;
-  scoped_refptr<GpuWatchdogThread> watchdog_thread_;
 
   typedef base::hash_map<int, scoped_refptr<GpuChannel> > GpuChannelMap;
   GpuChannelMap gpu_channels_;
