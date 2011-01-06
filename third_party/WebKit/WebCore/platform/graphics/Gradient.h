d92e6dec601fdf511ce5f1051f39c73de6ed9ed0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-06 06:11:02 +0000

    2011-01-05  Helder Correia  <helder@sencha.com>
            Reviewed by Simon Fraser.
    
            Shadows are not drawn when filling a rect with a gradient fillStyle on Mac and Chromium-Mac
            https://bugs.webkit.org/show_bug.cgi?id=51869
    
            GraphicsContext::fillRect(const FloatRect&) is clipping to the rect.
            The shadow is being drawn, but getting clipped to the rect being filled.
            The solution is to draw the gradient into a CGLayer (when a shadow is
            required), then render the layer to the GraphicsContext.
    
            This patch also fixes:
            canvas/philip/tests/2d.shadow.gradient.alpha.html
            canvas/philip/tests/2d.shadow.gradient.basic.html
            canvas/philip/tests/2d.shadow.gradient.transparent.2.html
    
            New test to ensure that shadows are drawn when fillStyle is a gradient,
            possibly with some transparency and blur, and specifically using
            fillRect.
    
            * fast/canvas/canvas-fillRect-gradient-shadow-expected.txt: Added.
            * fast/canvas/canvas-fillRect-gradient-shadow.html: Added.
            * fast/canvas/script-tests/canvas-fillRect-gradient-shadow.js: Added.
    2011-01-05  Helder Correia  <helder@sencha.com>
    
            Reviewed by Simon Fraser.
    
            Shadows are not drawn when filling a rect with a gradient fillStyle on Mac and Chromium-Mac
            https://bugs.webkit.org/show_bug.cgi?id=51869
    
            GraphicsContext::fillRect(const FloatRect&) is clipping to the rect.
            The shadow is being drawn, but getting clipped to the rect being filled.
            The solution is to draw the gradient into a CGLayer (when a shadow is
            required), then render the layer to the GraphicsContext.
    
            This patch also fixes:
            canvas/philip/tests/2d.shadow.gradient.alpha.html
            canvas/philip/tests/2d.shadow.gradient.basic.html
            canvas/philip/tests/2d.shadow.gradient.transparent.2.html
    
            Test: fast/canvas/canvas-fillRect-gradient-shadow.html
    
            * platform/graphics/Gradient.h:
            * platform/graphics/cg/GradientCG.cpp:
            (WebCore::Gradient::paint):
            * platform/graphics/cg/GraphicsContextCG.cpp:
            (WebCore::GraphicsContext::fillRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/Gradient.h
+++ third_party/WebKit/WebCore/platform/graphics/Gradient.h
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/PassRefPtr.h>
 #include <wtf/Vector.h>
 
+#if PLATFORM(WIN)
+#include <CoreGraphics/CoreGraphics.h>
+#endif
+
 #if PLATFORM(CG)
 
 #define USE_CG_SHADING defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
@@ -137,6 +141,7 @@ namespace WebCore {
         void setPlatformGradientSpaceTransform(const AffineTransform& gradientSpaceTransformation);
 
 #if PLATFORM(CG)
+        void paint(CGContextRef);
         void paint(GraphicsContext*);
 #endif
     private:
