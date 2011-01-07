144841522de20fb19e870e6767b928764a05d220
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 23:39:19 +0000

    Fix the Leopard build: Replace CoreAnimation SL-only functions with Leopard equivalents.
    * WebView/WebFullScreenController.mm:
    (+[CATransaction setDisableActions:]):
    (+[CATransaction setAnimationDuration:]):
    (-[WebFullScreenController _animationDuration]):
    (-[WebFullscreenWindow initWithContentRect:styleMask:backing:defer:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebFullScreenController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebFullScreenController.mm
@@ -59,6 +59,26 @@ static NSString* const isEnteringFullscreenKey = @"isEnteringFullscreen";
 
 using namespace WebCore;
 
+#if defined(BUILDING_ON_LEOPARD)
+@interface CATransaction(SnowLeopardConvenienceFunctions)
++ (void)setDisableActions:(BOOL)flag;
++ (void)setAnimationDuration:(CFTimeInterval)dur;
+@end
+
+@implementation CATransaction(SnowLeopardConvenienceFunctions)
++ (void)setDisableActions:(BOOL)flag
+{
+    [self setValue:[NSNumber numberWithBool:flag] forKey:kCATransactionDisableActions];
+}
+
++ (void)setAnimationDuration:(CFTimeInterval)dur
+{
+    [self setValue:[NSNumber numberWithDouble:dur] forKey:kCATransactionAnimationDuration];
+}
+@end
+
+#endif
+
 @interface WebFullscreenWindow : NSWindow
 #if !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_TIGER)
 <NSAnimationDelegate>
@@ -762,7 +782,7 @@ private:
 #if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
     NSUInteger modifierFlags = [NSEvent modifierFlags];
 #else
-    NSUInteger modifierFlags = [[NSEvent currentEvent] modifierFlags];
+    NSUInteger modifierFlags = [[NSApp currentEvent] modifierFlags];
 #endif
     if ((modifierFlags & NSControlKeyMask) == NSControlKeyMask)
         duration *= 2;
@@ -792,7 +812,11 @@ private:
     [self setAcceptsMouseMovedEvents:YES];
     [self setReleasedWhenClosed:NO];
     [self setHasShadow:YES];
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
     [self setMovable:NO];
+#else
+    [self setMovableByWindowBackground:NO];
+#endif
     
     NSView* contentView = [self contentView];
     _animationView = [[NSView alloc] initWithFrame:[contentView bounds]];
@@ -805,7 +829,11 @@ private:
     
     _backgroundLayer = [[CALayer alloc] init];
     [contentLayer addSublayer:_backgroundLayer];
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
     [contentLayer setGeometryFlipped:YES];
+#else
+    [contentLayer setSublayerTransform:CATransform3DMakeScale(1, -1, 1)];
+#endif
     [contentLayer setOpacity:0];
     
     [_backgroundLayer setBackgroundColor:CGColorGetConstantColor(kCGColorBlack)];
