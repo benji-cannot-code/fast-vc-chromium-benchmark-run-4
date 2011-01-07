28fc8784f1feb2572a241d389c18beb683e7396c
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 21:08:29 +0000

    2011-01-07  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Build fix.
    
            * platform/graphics/ca/win/PlatformCALayerWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75266 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
+++ third_party/WebKit/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
@@ -34,9 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "PlatformCALayerWinInternal.h"
 #include "WKCACFLayerRenderer.h"
-#include <QuartzCore/CACFAnimation.h>
-#include <QuartzCore/CACFContext.h>
-#include <QuartzCore/CACFLayer.h>
+#include <QuartzCore/CoreAnimationCF.h>
 #include <wtf/CurrentTime.h>
 #include <wtf/text/CString.h>
 
