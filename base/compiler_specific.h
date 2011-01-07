5a607f7ff576e2a039373dcfb6b73c1a0f75eae6
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-07 19:54:49 +0000

    Support OVERRIDE for clang.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6165001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70763 0039d316-1c4b-4281-b951-d872f2087c98

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -85,6 +85,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   virtual void foo() OVERRIDE;
 #if defined(COMPILER_MSVC)
 #define OVERRIDE override
+#elif defined(__clang__)
+#define OVERRIDE __attribute__((override))
 #else
 #define OVERRIDE
 #endif
