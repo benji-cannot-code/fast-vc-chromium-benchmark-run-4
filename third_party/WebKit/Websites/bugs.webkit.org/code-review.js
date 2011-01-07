8ea6429ce9e4ab1699251016b74a70aa78a81e6b
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 02:52:42 +0000

    2011-01-06  Ojan Vafai  <ojan@chromium.org>
            Fix line context when replying to comments.
    
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -146,10 +146,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function addPreviousComment(line, author, comment_text) {
-    var comment_block = $('<div data-comment-for="' + line.attr('id') + '" class="previousComment"></div>');
-    var author_block = $('<div class="author"></div>').text(author + ':');
+    var line_id = line.attr('id');
+    var comment_block = $('<div data-comment-for="' + line_id + '" class="previousComment"></div>');    var author_block = $('<div class="author"></div>').text(author + ':');
     var text_block = $('<div class="content"></div>').text(comment_text);
     comment_block.append(author_block).append(text_block).each(hoverify).click(addCommentField);
+    addDataCommentBaseLine(line, line_id);
     insertCommentFor(line, comment_block);
   }
 
