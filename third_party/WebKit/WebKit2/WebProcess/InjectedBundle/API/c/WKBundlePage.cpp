c449edb38ebe826978859f2f82ad46f2c1663515
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 18:43:36 +0000

    WebKit2: Need WKBundlePagePrivate equivalent of WebKit1's WebViewPrivate's setDefersCallbacks https://bugs.webkit.org/show_bug.cgi?id=52038
    Reviewed by Jon Honeycutt.
    
    The implementation of WebKit1's WebViewPrivate's setDefersCallbacks on both Windows and Mac
    calls WebCore::Page::setDefersLoading.
    
    * WebProcess/InjectedBundle/API/c/WKBundlePage.cpp:
    (WKBundlePageSetDefersLoading):
    * WebProcess/InjectedBundle/API/c/WKBundlePagePrivate.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::setDefersLoading):
    * WebProcess/WebPage/WebPage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.cpp
+++ third_party/WebKit/WebKit2/WebProcess/InjectedBundle/API/c/WKBundlePage.cpp
@@ -94,6 +94,11 @@ void WKBundlePageStopLoading(WKBundlePageRef pageRef)
     toImpl(pageRef)->stopLoading();
 }
 
+void WKBundlePageSetDefersLoading(WKBundlePageRef pageRef, bool defersLoading)
+{
+    toImpl(pageRef)->setDefersLoading(defersLoading);
+}
+
 WKStringRef WKBundlePageCopyRenderTreeExternalRepresentation(WKBundlePageRef pageRef)
 {
     return toCopiedAPI(toImpl(pageRef)->renderTreeExternalRepresentation());
