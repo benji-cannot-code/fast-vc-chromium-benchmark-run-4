b1f5b20fc1030a6ad9e99718560b723cddf0d97c
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 21:28:36 +0000

    2011-01-07  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Build fix for Chromium. Missing include.
    
            * page/animation/AnimationBase.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/animation/AnimationBase.cpp
+++ third_party/WebKit/WebCore/page/animation/AnimationBase.cpp
@@ -49,8 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderLayerBacking.h"
 #include "RenderStyle.h"
 #include "UnitBezier.h"
-
 #include <algorithm>
+#include <wtf/CurrentTime.h>
 
 using namespace std;
 
