628e2ec719814761061d5f1b8200dbf4e862d993
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 23:09:25 +0000

    No review, build fix only.  Removed parameter who was causing a warning in Release builds.
    * dom/Document.cpp:
    (WebCore::Document::webkitDidExitFullScreenForElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/dom/Document.cpp
+++ third_party/WebKit/WebCore/dom/Document.cpp
@@ -4862,9 +4862,8 @@ void Document::webkitWillExitFullScreenForElement(Element*)
     recalcStyle(Force);
 }
 
-void Document::webkitDidExitFullScreenForElement(Element* element)
+void Document::webkitDidExitFullScreenForElement(Element*)
 {
-    ASSERT(element);
     m_isFullScreen = false;
     m_areKeysEnabledInFullScreen = false;
 
