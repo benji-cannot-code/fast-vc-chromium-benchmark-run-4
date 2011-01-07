234bd49fdaf5f7e1ed5eaedd888d001046ace28c
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 21:20:30 +0000

    Don't ever call ::SetCursor(0)
    Doing so makes the cursor disappear from the screen.
    
    Fixes <http://webkit.org/b/52024> Mouse cursor flashes when moving it
    around a WKView before the web process has finished launching
    
    Reviewed by Jon Honeycutt.
    
    * UIProcess/win/WebView.cpp:
    (WebKit::WebView::onSetCursor): If we don't have a cursor to set, just
    let Windows do its default thing (which is to set the cursor to the
    window class's cursor, which in our case is the arrow cursor).
    (WebKit::WebView::cursorToShow): Just return 0 when the page has
    crashed so that we'll show the default cursor (which is the arrow
    cursor). This change has no visible effect, but seems slightly better
    conceptually.
    (WebKit::WebView::updateNativeCursor): Don't do anything if we don't
    have a cursor to set.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/WebKit2/UIProcess/win/WebView.cpp
@@ -483,7 +483,12 @@ LRESULT WebView::onShowWindowEvent(HWND hWnd, UINT message, WPARAM wParam, LPARA
 
 LRESULT WebView::onSetCursor(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam, bool& handled)
 {
-    handled = ::SetCursor(m_lastCursorSet);
+    if (!m_lastCursorSet) {
+        handled = false;
+        return 0;
+    }
+
+    ::SetCursor(m_lastCursorSet);
     return 0;
 }
 
@@ -626,12 +631,11 @@ void WebView::toolTipChanged(const String&, const String& newToolTip)
 
 HCURSOR WebView::cursorToShow() const
 {
-    static HCURSOR arrowCursor = ::LoadCursor(0, IDC_ARROW);
-
     if (!m_page->isValid())
-        return arrowCursor;
+        return 0;
 
     // We only show the override cursor if the default (arrow) cursor is showing.
+    static HCURSOR arrowCursor = ::LoadCursor(0, IDC_ARROW);
     if (m_overrideCursor && m_webCoreCursor == arrowCursor)
         return m_overrideCursor;
 
@@ -641,6 +645,8 @@ HCURSOR WebView::cursorToShow() const
 void WebView::updateNativeCursor()
 {
     m_lastCursorSet = cursorToShow();
+    if (!m_lastCursorSet)
+        return;
     ::SetCursor(m_lastCursorSet);
 }
 
