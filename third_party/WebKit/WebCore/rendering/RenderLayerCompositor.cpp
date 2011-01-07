a8bbc931446c8a3f55b1e0a90c3579613c3c7f74
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 23:18:50 +0000

    Further build fixes.  Add a ENABLE(FULLSCREEN_API) guard around the implementation of requiresCompositingfForFullScreen.
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::requiresCompositingForFullScreen):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/WebCore/rendering/RenderLayerCompositor.cpp
@@ -1326,7 +1326,11 @@ bool RenderLayerCompositor::requiresCompositingWhenDescendantsAreCompositing(Ren
     
 bool RenderLayerCompositor::requiresCompositingForFullScreen(RenderObject* renderer) const
 {
+#if ENABLE(FULLSCREEN_API)
     return renderer->isRenderFullScreen() && toRenderFullScreen(renderer)->isAnimating();
+#else
+    return false;
+#endif
 }
 
 // If an element has negative z-index children, those children render in front of the 
