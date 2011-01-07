859e86c0abd893910a9ebd909c08a2c8d9dc1f52
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-07 07:13:57 +0000

    2011-01-06  James Kozianski  <koz@chromium.org>
            Reviewed by Mihai Parparita.
    
            Add classes to provide a consistent interface to a set of files.
            https://bugs.webkit.org/show_bug.cgi?id=50901
    
            These classes allow us to write code that is agnostic to whether a
            particular set of files resides in a local directory or in a zip file
            on a remote machine.
    
            * Scripts/webkitpy/common/system/directoryfileset.py: Added.
            * Scripts/webkitpy/common/system/directoryfileset_unittest.py: Added.
            * Scripts/webkitpy/common/system/fileset.py: Added.
            * Scripts/webkitpy/common/system/filesystem.py:
            * Scripts/webkitpy/common/system/filesystem_mock.py:
            * Scripts/webkitpy/common/system/zipfileset.py: Added.
            * Scripts/webkitpy/common/system/zipfileset_unittest.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/filesystem_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/filesystem_mock.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import errno
 import os
 import path
+import re
 
 
 class MockFileSystem(object):
@@ -58,7 +59,7 @@ class MockFileSystem(object):
         return any(f.startswith(path) for f in self.files)
 
     def join(self, *comps):
-        return '/'.join(comps)
+        return re.sub(re.escape(os.path.sep), '/', os.path.join(*comps))
 
     def listdir(self, path):
         if not self.isdir(path):
@@ -108,3 +109,11 @@ class MockFileSystem(object):
             raise IOError(errno.EISDIR, destination, os.strerror(errno.ISDIR))
 
         self.files[destination] = self.files[source]
+
+    def files_under(self, path):
+        if not path.endswith('/'):
+            path += '/'
+        return [file for file in self.files if file.startswith(path)]
+
+    def remove(self, path):
+        del self.files[path]
