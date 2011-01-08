ff61325b732771e69dd7a8ab82c1f639989aefc3
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-08 00:21:41 +0000

    GTK Linux build fix: isDocumentRunningFullScreenAnimation should be protected both by ENABLE(FULLSCREEN_API) && USE(ACCELERATED_COMPOSITING)
    * page/FrameView.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebCore/page/FrameView.cpp
+++ third_party/WebKit/WebCore/page/FrameView.cpp
@@ -530,7 +530,7 @@ void FrameView::calculateScrollbarModesForLayout(ScrollbarMode& hMode, Scrollbar
     }    
 }
 
-#if ENABLE(FULLSCREEN_API)
+#if ENABLE(FULLSCREEN_API) && USE(ACCELERATED_COMPOSITING)
 static bool isDocumentRunningFullScreenAnimation(Document* document)
 {
     return document->webkitIsFullScreen() && document->fullScreenRenderer() && document->fullScreenRenderer()->isAnimating();
