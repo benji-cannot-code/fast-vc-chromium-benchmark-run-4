feca19e68c46f03c5e01cd491d27f12e9135914d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-08 10:10:15 +0000

    Try to fix the Leopard build.
    * WebView/WebFullScreenController.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/mac/WebView/WebFullScreenController.mm
+++ third_party/WebKit/WebKit/mac/WebView/WebFullScreenController.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2009, 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebWindowAnimation.h"
 #import "WebViewInternal.h"
 #import <IOKit/pwr_mgt/IOPMLib.h>
+#import <OSServices/Power.h>
 #import <WebCore/AnimationList.h>
 #import <WebCore/CSSPropertyNames.h>
 #import <WebCore/Color.h>
