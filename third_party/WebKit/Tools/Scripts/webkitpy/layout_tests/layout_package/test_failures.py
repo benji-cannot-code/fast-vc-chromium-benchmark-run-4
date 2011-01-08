1cc57e5d48a58351d826ade72a9b5877f12ce732
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-08 23:56:02 +0000

    2011-01-08  Eric Seidel  <eric@webkit.org>
            Reviewed by Ojan Vafai.
    
            test_failures.py should not mention TestShell (long live DumpRenderTree!)
            https://bugs.webkit.org/show_bug.cgi?id=52067
    
            I ran test-webkitpy, but didn't bother adding a unit test for this string change.
            https://bugs.webkit.org/show_bug.cgi?id=51138#c3 is an example of why this is needed.
    
            * Scripts/webkitpy/layout_tests/layout_package/test_failures.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures.py
@@ -188,11 +188,11 @@ class FailureTimeout(TestFailure):
 
 
 class FailureCrash(TestFailure):
-    """Test shell crashed."""
+    """DumpRenderTree crashed."""
 
     @staticmethod
     def message():
-        return "Test shell crashed"
+        return "DumpRenderTree crashed"
 
     def result_html_output(self, filename):
         # FIXME: create a link to the minidump file
