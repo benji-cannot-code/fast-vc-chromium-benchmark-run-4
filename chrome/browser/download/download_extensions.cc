a9e451f1bf172373dad1d772e8d6521ba8a831e2
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-08 22:13:56 +0000

    Enable "Always Open Files of This Type" even for dangerous file types. The advanced user knows what they were doing, and dangerous file types will still go through the warning dialog depending on user gesture level.
    BUG=65895
    TEST=download_manager_unittest.cc
    
    Review URL: http://codereview.chromium.org/6196001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extensions.cc
+++ chrome/browser/download/download_extensions.cc
@@ -214,10 +214,6 @@ DownloadDangerLevel GetFileExtensionDangerLevel(
   return NotDangerous;
 }
 
-bool IsFileExtensionSafe(const FilePath::StringType& extension) {
-  return GetFileExtensionDangerLevel(extension) == NotDangerous;
-}
-
 bool IsFileSafe(const FilePath& path) {
   return GetFileDangerLevel(path) == NotDangerous;
 }
