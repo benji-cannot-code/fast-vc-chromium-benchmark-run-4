c66212b9d936163875a481533ad85753e0c249bd
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-10 22:59:59 +0000

    Adding new sync data type AUTOFILL_PROFILE to python server.
    This patch updates the python sync server implementation with the newly
    added AUTOFILL_PROFILE sync data type.
    
    BUG=69127
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/6096010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70954 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/testserver/chromiumsync.py
+++ net/tools/testserver/chromiumsync.py
@@ -37,6 +37,7 @@ ALL_TYPES = (
     TOP_LEVEL,  # The type of the 'Google Chrome' folder.
     APPS,
     AUTOFILL,
+    AUTOFILL_PROFILE,
     BOOKMARK,
     EXTENSIONS,
     NIGORI,
@@ -44,7 +45,7 @@ ALL_TYPES = (
     PREFERENCE,
     SESSION,
     THEME,
-    TYPED_URL) = range(11)
+    TYPED_URL) = range(12)
 
 # Well-known server tag of the top level "Google Chrome" folder.
 TOP_LEVEL_FOLDER_TAG = 'google_chrome'
@@ -54,6 +55,7 @@ TOP_LEVEL_FOLDER_TAG = 'google_chrome'
 SYNC_TYPE_TO_EXTENSION = {
     APPS: app_specifics_pb2.app,
     AUTOFILL: autofill_specifics_pb2.autofill,
+    AUTOFILL_PROFILE: autofill_specifics_pb2.autofill_profile,
     BOOKMARK: bookmark_specifics_pb2.bookmark,
     EXTENSIONS: extension_specifics_pb2.extension,
     NIGORI: nigori_specifics_pb2.nigori,
@@ -249,6 +251,8 @@ class SyncDataModel(object):
                     parent_tag='google_chrome', sync_type=PREFERENCE),
       PermanentItem('google_chrome_autofill', name='Autofill',
                     parent_tag='google_chrome', sync_type=AUTOFILL),
+      PermanentItem('google_chrome_autofill_profiles', name='Autofill Profiles',
+                    parent_tag='google_chrome', sync_type=AUTOFILL_PROFILE),
       PermanentItem('google_chrome_extensions', name='Extensions',
                     parent_tag='google_chrome', sync_type=EXTENSIONS),
       PermanentItem('google_chrome_passwords', name='Passwords',
@@ -490,7 +494,7 @@ class SyncDataModel(object):
 
   def _CopyOverImmutableFields(self, entry):
     """Preserve immutable fields by copying pre-commit state.
-    
+
     Args:
       entry: A sync entity from the client.
     """
@@ -612,7 +616,7 @@ class SyncDataModel(object):
     if not self._CheckParentIdForCommit(entry):
       return None
 
-    self._CopyOverImmutableFields(entry);  
+    self._CopyOverImmutableFields(entry);
 
     # At this point, the commit is definitely going to happen.
 
