6d67d9af3be4d79fc64bcca8d5cb3a42e24a61d3
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-10 11:21:36 +0000

    2011-01-10  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Commit bot tried to land r? cq+ patch
            https://bugs.webkit.org/show_bug.cgi?id=36638
    
            It should now be possible to post a patch with r? and cq+ and the
            commit-queue will correctly wait for the r+ before attempting to land the patch.
            (This was a feature requested by several users.)
    
            One caveat: We don't actually reject r-, cq+ patches from the queue
            (we just ignore them), but I think that's OK for now.
    
            I also noticed that setting r- once a commit-queue node had grabbed
            a patch would not properly cancel the landing.  That's fixed and
            tested in this patch.
    
            * Scripts/webkitpy/tool/bot/commitqueuetask.py:
            * Scripts/webkitpy/tool/bot/commitqueuetask_unittest.py:
            * Scripts/webkitpy/tool/bot/feeders.py:
            * Scripts/webkitpy/tool/bot/feeders_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitqueuetask.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitqueuetask.py
@@ -67,6 +67,8 @@ class CommitQueueTask(object):
             return False
         if not self._patch.committer():
             return False
+        if not self._patch.review() != "-":
+            return False
         # Reviewer is not required. Missing reviewers will be caught during
         # the ChangeLog check during landing.
         return True
--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitqueuetask_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/commitqueuetask_unittest.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from datetime import datetime
 import unittest
 
+from webkitpy.common.net import bugzilla
 from webkitpy.common.system.deprecated_logging import error, log
 from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.layout_tests.layout_package import test_results
@@ -321,3 +322,24 @@ command_failed: failure_message='Unable to land patch' script_error='MOCK land f
 """
         # FIXME: This should really be expect_retry=True for a better user experiance.
         self._run_through_task(commit_queue, expected_stderr, ScriptError)
+
+    def _expect_validate(self, patch, is_valid):
+        class MockDelegate(object):
+            def refetch_patch(self, patch):
+                return patch
+
+        task = CommitQueueTask(MockDelegate(), patch)
+        self.assertEquals(task._validate(), is_valid)
+
+    def _mock_patch(self, attachment_dict={}, bug_dict={'bug_status': 'NEW'}, committer="fake"):
+        bug = bugzilla.Bug(bug_dict, None)
+        patch = bugzilla.Attachment(attachment_dict, bug)
+        patch._committer = committer
+        return patch
+
+    def test_validate(self):
+        self._expect_validate(self._mock_patch(), True)
+        self._expect_validate(self._mock_patch({'is_obsolete': True}), False)
+        self._expect_validate(self._mock_patch(bug_dict={'bug_status': 'CLOSED'}), False)
+        self._expect_validate(self._mock_patch(committer=None), False)
+        self._expect_validate(self._mock_patch({'review': '-'}), False)
