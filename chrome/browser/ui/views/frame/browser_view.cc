1b95c2e5170b688d02ace545e0c8f5adfb5c729d
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-10 22:52:42 +0000

    Fix build break
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "app/l10n_util.h"
 #include "app/resource_bundle.h"
-#include "app/win/win_util.h"
 #include "base/command_line.h"
 #include "base/i18n/rtl.h"
 #include "base/string_number_conversions.h"
