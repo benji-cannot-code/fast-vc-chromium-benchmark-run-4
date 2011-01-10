5c9f47598e6c6b64ca32228b27d265717702f541
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-10 20:43:36 +0000

    Add SettingUp page in the Remoting Host setup wizard
    BUG=67218
    TEST=None
    
    Review URL: http://codereview.chromium.org/6094013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/remoting/setup_flow_start_host_step.cc
+++ chrome/browser/remoting/setup_flow_start_host_step.cc
@@ -45,6 +45,8 @@ void SetupFlowStartHostStep::OnRemotingHostInfo(
 }
 
 void SetupFlowStartHostStep::DoStart() {
+  flow()->dom_ui()->CallJavascriptFunction(L"showSettingUp");
+
   process_control_ =
       ServiceProcessControlManager::GetInstance()->GetProcessControl(
           flow()->profile());
