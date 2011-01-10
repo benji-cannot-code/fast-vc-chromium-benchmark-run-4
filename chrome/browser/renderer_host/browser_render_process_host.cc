77cb38016c8c85d95d7dd907b246cfd3ce294fe9
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-10 16:39:29 +0000

    Enable speech input by default and remove unused code.
    BUG=61677
    TEST=browser_tests --gtest_filter="SpeechInputBrowserTest.*"
    
    Review URL: http://codereview.chromium.org/6133006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70896 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/client_side_detection_service.h"
 #include "chrome/browser/search_engines/search_provider_install_state_message_filter.h"
 #include "chrome/browser/speech/speech_input_dispatcher_host.h"
-#include "chrome/browser/speech/speech_input_manager.h"
 #include "chrome/browser/spellcheck_host.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/visitedlink/visitedlink_master.h"
@@ -791,11 +790,6 @@ void BrowserRenderProcessHost::InitExtensions() {
   Send(new ViewMsg_Extension_SetFunctionNames(function_names));
 }
 
-void BrowserRenderProcessHost::InitSpeechInput() {
-  Send(new ViewMsg_SpeechInput_SetFeatureEnabled(
-      speech_input::SpeechInputManager::IsFeatureEnabled()));
-}
-
 void BrowserRenderProcessHost::SendUserScriptsUpdate(
     base::SharedMemory *shared_memory) {
   // Process is being started asynchronously.  We'll end up calling
@@ -1170,7 +1164,6 @@ void BrowserRenderProcessHost::OnProcessLaunched() {
 
   Send(new ViewMsg_SetIsIncognitoProcess(profile()->IsOffTheRecord()));
 
-  InitSpeechInput();
   InitVisitedLinks();
   InitUserScripts();
   InitExtensions();
