e32b3897a1945339fbfd0ca00e9779fd6d2f2aa0
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 16:49:38 +0000

    Unreviewed buildfix.
    [Qt][V8] Add missing include for debug build.
    
    * loader/cache/CachedResourceLoader.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PingLoader.h"
 #include "SecurityOrigin.h"
 #include "Settings.h"
+#include <wtf/text/CString.h>
 #include <wtf/text/StringConcatenate.h>
 
 #define PRELOAD_DEBUG 0
