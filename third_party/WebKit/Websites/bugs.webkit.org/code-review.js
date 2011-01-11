c327c08e52d3f9b6b8db3f3c33cd6a2224446c20
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 23:56:50 +0000

    2011-01-11  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            remember diffstate for review tool
            https://bugs.webkit.org/show_bug.cgi?id=52253
    
            If you use the global sidebyside/unified links, store the diff type
            in localstorage. Then, onload, use that diff type. That way, people
            who prefer one diff type or the other can always get that by default.
    
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -397,12 +397,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function convertDiff(difftype, convert_link) {
     var file_diffs = $(convert_link).parents('.FileDiff');
-    if (!file_diffs.size())
+    if (!file_diffs.size()) {
+      localStorage.setItem('code-review-diffstate', difftype);
       file_diffs = $('.FileDiff');
+    }
 
-    file_diffs.each(function() {
-      convertFileDiff(difftype, this);
-    });
+    convertAllFileDiffs(difftype, file_diffs);
   }
 
   function getWebKitSourceFile(file_name, onLoad, expand_bar) {
@@ -742,7 +742,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     $(resize_iframe[0].contentWindow).bind('resize', onBodyResize);
 
     updateToolbarAnchorState();
+    loadDiffState();
   });
+
+  function loadDiffState() {
+    var diffstate = localStorage.getItem('code-review-diffstate');
+    if (diffstate != 'sidebyside' && diffstate != 'unified')
+      return;
+
+    convertAllFileDiffs(diffstate, $('.FileDiff'));
+  }
 
   function isDiffSideBySide(file_diff) {
     return diffState(file_diff) == 'sidebyside';
@@ -772,6 +781,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       $('.side-by-side-link', file_diff).hide();
       $('.unify-link', file_diff).show();
     }
+  }
+
+  function convertAllFileDiffs(diff_type, file_diffs) {
+    file_diffs.each(function() {
+      convertFileDiff(diff_type, this);
+    });
   }
 
   function convertFileDiff(diff_type, file_diff) {
