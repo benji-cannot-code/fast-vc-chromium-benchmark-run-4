25aebd7afa450e446354b489c0c0547598b8ee7e
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 21:03:44 +0000

    2011-01-11  Dean Jackson  <dino@apple.com>
            Reviewed by Simon Fraser.
    
            https://bugs.webkit.org/show_bug.cgi?id=52233
    
            WebKitCSSMatrix constructor should accept 'none'
            as a valid string parameter.
    
            * css/WebKitCSSMatrix.cpp:
            (WebCore::WebKitCSSMatrix::setMatrixValue):
    
    2011-01-11  Dean Jackson  <dino@apple.com>
    
            Reviewed by Simon Fraser.
    
            https://bugs.webkit.org/show_bug.cgi?id=52233
    
            Update WebKitCSSMatrix test to cover a constructor
            that accepts 'none' as input.
    
            * transforms/cssmatrix-2d-interface-expected.txt:
            * transforms/cssmatrix-2d-interface.xhtml:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/WebKitCSSMatrix.cpp
+++ third_party/WebKit/Source/WebCore/css/WebKitCSSMatrix.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSStyleSelector.h"
 #include "CSSMutableStyleDeclaration.h"
 #include "CSSPropertyNames.h"
+#include "CSSValueKeywords.h"
 #include "ExceptionCode.h"
 #include "RenderStyle.h"
 #include <wtf/MathExtras.h>
@@ -58,9 +59,14 @@ void WebKitCSSMatrix::setMatrixValue(const String& string, ExceptionCode& ec)
     if (p.parseValue(styleDeclaration.get(), CSSPropertyWebkitTransform, string, true)) {
         // Convert to TransformOperations. This can fail if a property 
         // requires style (i.e., param uses 'ems' or 'exs')
-        PassRefPtr<CSSValue> val =  styleDeclaration->getPropertyCSSValue(CSSPropertyWebkitTransform);
+        RefPtr<CSSValue> value = styleDeclaration->getPropertyCSSValue(CSSPropertyWebkitTransform);
+
+        // Check for a "none" or empty transform. In these cases we can use the default identity matrix.
+        if (!value || (value->isPrimitiveValue() && (static_cast<CSSPrimitiveValue*>(value.get()))->getIdent() == CSSValueNone))
+            return;
+
         TransformOperations operations;
-        if (!CSSStyleSelector::createTransformOperations(val.get(), 0, 0, operations)) {
+        if (!CSSStyleSelector::createTransformOperations(value.get(), 0, 0, operations)) {
             ec = SYNTAX_ERR;
             return;
         }
