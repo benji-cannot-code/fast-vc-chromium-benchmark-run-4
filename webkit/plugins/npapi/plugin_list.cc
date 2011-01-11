64f39b3a0d9a85085f782ca8e0aa20a18a25b661
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-11 05:25:56 +0000

    Always delete your pointers! Note that RUNNING_UNDER_VALGRIND may not be true for other memory leak-check technologies such as heapcheck/pprof!
    BUG=NONE
    TEST=PluginLibLoading.UnloadAllPlugins (on Linux)
    TBR=bauerb@chromium.org
    Review URL: http://codereview.chromium.org/6155005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71010 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_list.cc
+++ webkit/plugins/npapi/plugin_list.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
+#include "base/stl_util-inl.h"
 #include "base/string_split.h"
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
@@ -20,11 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/plugins/npapi/plugin_constants_win.h"
 #include "webkit/plugins/npapi/plugin_lib.h"
 #include "webkit/plugins/plugin_switches.h"
-
-#if defined(OS_POSIX)
-#include "base/stl_util-inl.h"
-#include "base/third_party/valgrind/valgrind.h"
-#endif  // defined(OS_POSIX)
 
 namespace webkit {
 namespace npapi {
@@ -794,18 +790,8 @@ PluginList::~PluginList() {
 }
 
 void PluginList::Shutdown() {
-  // TODO
-  // Note: plugin_groups_ contains simple pointers of type PluginGroup*, but
-  // since this singleton lives until the process is destroyed, no explicit
-  // cleanup is necessary.
-  // However, when running on Valgrind, we need to do the cleanup to keep the
-  // memory tree green.
-#if defined(OS_POSIX)
-  if (RUNNING_ON_VALGRIND) {
-    STLDeleteContainerPairSecondPointers(plugin_groups_.begin(),
-                                         plugin_groups_.end());
-  }
-#endif
+  STLDeleteContainerPairSecondPointers(plugin_groups_.begin(),
+                                       plugin_groups_.end());
 }
 
 }  // namespace npapi
