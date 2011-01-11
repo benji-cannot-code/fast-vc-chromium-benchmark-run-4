8eb28fbedcbe05895623614655798041213bf126
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-11 12:43:36 +0000

    Update our use of the NPAPI ClearSiteData API.
    BUG=58235
    TEST=none
    
    Review URL: http://codereview.chromium.org/6148006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71039 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/plugin_messages_internal.h
+++ chrome/common/plugin_messages_internal.h
@@ -306,8 +306,8 @@ IPC_MESSAGE_ROUTED1(PluginMsg_SetFakeAcceleratedSurfaceWindowHandle,
 #endif
 
 IPC_MESSAGE_CONTROL3(PluginMsg_ClearSiteData,
+                     std::string, /* site */
                      uint64, /* flags */
-                     std::string, /* domain */
                      base::Time /* begin_time */)
 
 
