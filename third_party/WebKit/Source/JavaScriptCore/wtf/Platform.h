25fda71177a594e68b39fc3e7ae192be80a08c58
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 00:51:17 +0000

    2011-01-10  Daniel Bates  <dbates@rim.com>
            Reviewed by Martin Robinson.
    
            Remove extraneous COMPILER(GCC) condition when checking GCC_VERSION_AT_LEAST()
            https://bugs.webkit.org/show_bug.cgi?id=52178
    
            It is sufficient to test GCC_VERSION_AT_LEAST() instead of both COMPILER(GCC) and
            GCC_VERSION_AT_LEAST(). Notice GCC_VERSION_AT_LEAST() is defined to be 0 when we
            are not compiling with GCC.
    
            Fixes style issues at the callsites (i.e. replace comma with comma-space in
            macro function argument list). Also, makes a spelling correction in a comment.
    
            * jit/ExecutableAllocator.h:
            (JSC::ExecutableAllocator::cacheFlush):
            * wtf/Platform.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -88,7 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #define GCC_VERSION_AT_LEAST(major, minor, patch) (GCC_VERSION >= (major * 10000 + minor * 100 + patch))
 #else
-/* define this for !GCC compilers, just so we can write things like COMPILER(GCC) && GCC_VERSION_AT_LEAST(4,1,0) */
+/* Define this for !GCC compilers, just so we can write things like GCC_VERSION_AT_LEAST(4, 1, 0). */
 #define GCC_VERSION_AT_LEAST(major, minor, patch) 0
 #endif
 
@@ -957,8 +957,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_REPAINT_THROTTLING 0
 #endif
 
-/* Disable the JIT on versiond of GCC prior to 4.1 */
-#if !defined(ENABLE_JIT) && COMPILER(GCC) && !GCC_VERSION_AT_LEAST(4,1,0)
+/* Disable the JIT on versions of GCC prior to 4.1 */
+#if !defined(ENABLE_JIT) && COMPILER(GCC) && !GCC_VERSION_AT_LEAST(4, 1, 0)
 #define ENABLE_JIT 0
 #endif
 
@@ -970,7 +970,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* The JIT is enabled by default on all x86, x64-64, ARM & MIPS platforms. */
 #if !defined(ENABLE_JIT) \
     && (CPU(X86) || CPU(X86_64) || CPU(ARM) || CPU(MIPS)) \
-    && (OS(DARWIN) || !COMPILER(GCC) || GCC_VERSION_AT_LEAST(4,1,0)) \
+    && (OS(DARWIN) || !COMPILER(GCC) || GCC_VERSION_AT_LEAST(4, 1, 0)) \
     && !OS(WINCE)
 #define ENABLE_JIT 1
 #endif
