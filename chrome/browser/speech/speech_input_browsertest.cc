7e037cea1fe2287e644744486fbc94a8eed93c08
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-11 01:26:13 +0000

    Fix build breakage.
    BUG=none
    TEST=bots go green?
    Review URL: http://codereview.chromium.org/6204003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_input_browsertest.cc
+++ chrome/browser/speech/speech_input_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/speech_input_manager.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/in_process_browser_test.h"
 #include "chrome/test/ui_test_utils.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebInputEvent.h"
