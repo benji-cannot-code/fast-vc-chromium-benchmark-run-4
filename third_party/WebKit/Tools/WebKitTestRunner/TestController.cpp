ab4b98be209b9ef0db4a7b89247ea98974e24fd9
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 04:50:08 +0000

    Unreviewed, rolling out r75464. http://trac.webkit.org/changeset/75464 https://bugs.webkit.org/show_bug.cgi?id=52193
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-01-10
    Followup rollout for 75474 (Requested by Ossy on #webkit).
    
    * MiniBrowser/mac/BrowserWindowController.m:
    (-[BrowserWindowController awakeFromNib]):
    * MiniBrowser/win/BrowserView.cpp:
    (BrowserView::create):
    * WebKitTestRunner/TestController.cpp:
    (WTR::createOtherPage):
    (WTR::TestController::initialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -139,8 +139,7 @@ static WKPageRef createOtherPage(WKPageRef oldPage, WKDictionaryRef, WKEventModi
         0, // didDraw
         0, // pageDidScroll
         0, // exceededDatabaseQuota
-        0, // runOpenPanel
-        0, // decidePolicyForGeolocationPermissionRequest
+        0  // runOpenPanel
     };
     WKPageSetPageUIClient(newPage, &otherPageUIClient);
 
@@ -244,8 +243,7 @@ void TestController::initialize(int argc, const char* argv[])
         0, // didDraw
         0, // pageDidScroll
         0, // exceededDatabaseQuota
-        0, // runOpenPanel
-        0, // decidePolicyForGeolocationPermissionRequest
+        0  // runOpenPanel
     };
     WKPageSetPageUIClient(m_mainWebView->page(), &pageUIClient);
 
