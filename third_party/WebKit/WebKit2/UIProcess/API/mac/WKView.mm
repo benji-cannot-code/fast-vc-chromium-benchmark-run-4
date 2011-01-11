d7fb24e53130c01436251d618d6e9b43f75c8537
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 20:05:32 +0000

    [WKView setFrameSize:] derefs _page->drawingArea() without null check <https://bugs.webkit.org/show_bug.cgi?id=52231>
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView setFrameSize:]):
    Null check drawingArea(). This is OK because if it doesn't exist then it will be
    created later with the correct initial size.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/WebKit2/UIProcess/API/mac/WKView.mm
@@ -253,6 +253,9 @@ typedef HashMap<String, ValidationVector> ValidationMap;
 {
     [super setFrameSize:size];
 
+    if (!_data->_page->drawingArea())
+        return;
+    
     _data->_page->drawingArea()->setSize(IntSize(size));
 }
 
