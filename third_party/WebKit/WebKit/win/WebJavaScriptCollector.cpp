ed6c8c0f805f52c7f5f368d73e5d7b1f3af98340
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 00:05:24 +0000

    Try to fix Windows build.
    * WebJavaScriptCollector.cpp: Updated for rename to Heap.*. (Didn't catch
    this one before because it was miscapitalized.)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/win/WebJavaScriptCollector.cpp
+++ third_party/WebKit/WebKit/win/WebJavaScriptCollector.cpp
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebJavaScriptCollector.h"
 
 #pragma warning(push, 0)
+#include <JavaScriptCore/Heap.h>
 #include <JavaScriptCore/JSGlobalData.h>
-#include <JavaScriptCore/collector.h>
 #include <WebCore/GCController.h>
 #include <WebCore/JSDOMWindow.h>
 #include <runtime/JSLock.h>
