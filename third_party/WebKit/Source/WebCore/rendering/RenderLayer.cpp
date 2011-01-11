860a85781354f17d495fc6e532c9d4c130de027a
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 04:14:51 +0000

    Source/WebCore: https://bugs.webkit.org/show_bug.cgi?id=51119, transformed elements not responding properly to :hover after r73385. Just use the entire document area as the hit test area, since there appears to be a rounding bug/issues with relying on float math when mapping the hit test area into transformed coordinates.
    Patch by David Hyatt <hyatt@apple.com> on 2010-10-10
    Reviewed by Simon Fraser.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::hitTest):
    
    LayoutTests: Test for https://bugs.webkit.org/show_bug.cgi?id=51119
    
    Patch by David Hyatt <hyatt@apple.com> on 2011-01-10
    Reviewed by Simon Fraser.
    
    * fast/transforms/transform-hit-test-flipped-expected.txt: Added.
    * fast/transforms/transform-hit-test-flipped.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -2680,7 +2680,7 @@ bool RenderLayer::hitTest(const HitTestRequest& request, HitTestResult& result)
 {
     renderer()->document()->updateLayout();
     
-    IntRect hitTestArea = result.rectForPoint(result.point());
+    IntRect hitTestArea = renderer()->view()->documentRect();
     if (!request.ignoreClipping())
         hitTestArea.intersect(frameVisibleRect(renderer()));
 
