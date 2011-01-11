bb249ee84d06d8ed889566f0acb1fe4a6948763e
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-11 00:41:02 +0000

    2011-01-10  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Oliver Hunt.
    
            Removed dead number allocation code.
            https://bugs.webkit.org/show_bug.cgi?id=52177
    
            * runtime/Heap.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Heap.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Heap.h
@@ -66,8 +66,6 @@ namespace JSC {
         size_t nextCell;
         PageAllocationAligned* blocks;
         
-        void* nextNumber;
-
         size_t numBlocks;
         size_t usedBlocks;
 
@@ -86,7 +84,6 @@ namespace JSC {
     public:
         void destroy();
 
-        void* allocateNumber(size_t);
         void* allocate(size_t);
 
         bool isBusy(); // true if an allocation or collection is in progress
@@ -296,19 +293,6 @@ namespace JSC {
             recordExtraCost(cost);
     }
     
-    inline void* Heap::allocateNumber(size_t s)
-    {
-        if (void* result = m_heap.nextNumber) {
-            m_heap.nextNumber = 0;
-            return result;
-        }
-
-        void* result = allocate(s);
-        m_heap.nextNumber = static_cast<char*>(result) + (CELL_SIZE / 2);
-        return result;
-    }
-
-
     inline WeakGCHandlePool* Heap::weakGCHandlePool(size_t index)
     {
         return static_cast<WeakGCHandlePool*>(m_weakGCHandlePools[index].base());
