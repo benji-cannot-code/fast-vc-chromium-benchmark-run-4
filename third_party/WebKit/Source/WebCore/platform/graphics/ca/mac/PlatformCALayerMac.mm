ac697f3354cc5c6fe07f0818c2e8367a6d1811eb
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-12 23:41:25 +0000

    2011-01-12  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Another build fix
    
            * platform/graphics/ca/GraphicsLayerCA.cpp:
            (WebCore::GraphicsLayerCA::clampedContentsScaleForScale):
            * platform/graphics/ca/mac/PlatformCALayerMac.mm:
            (PlatformCALayer::setContentsScale):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/PlatformCALayerMac.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/PlatformCALayerMac.mm
@@ -739,6 +739,8 @@ void PlatformCALayer::setContentsScale(float value)
     BEGIN_BLOCK_OBJC_EXCEPTIONS
     [m_layer.get() setContentsScale:value];
     END_BLOCK_OBJC_EXCEPTIONS
+#else
+    UNUSED_PARAM(value);
 #endif
 }
 
