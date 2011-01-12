23be129ba26e4dfe9d83e456c5e7583755e19ed0
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-12 21:19:58 +0000

    Trim whitespace from key identifiers in the Input API.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/5972007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_input_api.cc
+++ chrome/browser/extensions/extension_input_api.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "app/keyboard_code_conversion.h"
+#include "base/string_util.h"
 #include "base/values.h"
 #include "chrome/browser/browser_window.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
@@ -84,6 +85,8 @@ bool SendKeyboardEventInputFunction::RunImpl() {
 
   std::string identifier;
   EXTENSION_FUNCTION_VALIDATE(args->GetString(kKeyIdentifier, &identifier));
+  TrimWhitespaceASCII(identifier, TRIM_ALL, &identifier);
+
   app::KeyboardCode code = app::KeyCodeFromKeyIdentifier(identifier);
   if (code == app::VKEY_UNKNOWN) {
     error_ = kUnknownOrUnsupportedKeyIdentiferError;
