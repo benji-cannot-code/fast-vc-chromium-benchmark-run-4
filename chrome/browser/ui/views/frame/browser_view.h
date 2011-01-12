02f041c7e288e5f4ee9c823c9635d29109755a8c
alekseys@chromium.org <alekseys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-12 20:57:44 +0000

    Streamline the layout of the BrowserView's children TabContents views. Modify SingleSplitView to calculate its children view's bounds, but do not actually resize them and change BrowserViewLayout accordingly (BrowserViewLayout resizes all views now). Do all reserved contents rect calculations before resizing TabContents views.
    Rationale: to do all BrowserView layout related actions in the context of
    BrowserViewLayout::Layout call and to minimize actual contents re-layouts.
    
    BUG=51084
    TEST=All tests should pass
    
    Review URL: http://codereview.chromium.org/5606012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/unhandled_keyboard_event_handler.h"
 #include "chrome/common/notification_registrar.h"
 #include "gfx/native_widget_types.h"
+#include "views/controls/single_split_view.h"
 #include "views/window/client_view.h"
 #include "views/window/window_delegate.h"
 
@@ -54,6 +55,7 @@ class InfoBarContainer;
 class LocationBarView;
 class SideTabStrip;
 class StatusBubbleViews;
+class TabContentsContainer;
 class TabStripModel;
 class ToolbarView;
 class ZoomMenuModel;
@@ -67,7 +69,6 @@ class JumpList;
 namespace views {
 class ExternalFocusTracker;
 class Menu;
-class SingleSplitView;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -85,7 +86,7 @@ class BrowserView : public BrowserBubbleHost,
                     public views::WindowDelegate,
                     public views::ClientView,
                     public InfoBarContainer::Delegate,
-                    public TabContentsContainer::ReservedAreaDelegate {
+                    public views::SingleSplitView::Observer {
  public:
   // The browser view's class name.
   static const char kViewClassName[];
@@ -397,8 +398,8 @@ class BrowserView : public BrowserBubbleHost,
   // InfoBarContainer::Delegate overrides
   virtual void InfoBarSizeChanged(bool is_animating);
 
-  // TabContentsContainer::ReservedAreaDelegate overrides.
-  virtual void UpdateReservedContentsRect(const TabContentsContainer* source);
+  // views::SingleSplitView::Observer overrides:
+  virtual bool SplitHandleMoved(views::SingleSplitView* view);
 
  protected:
   // Appends to |toolbars| a pointer to each AccessiblePaneView that
@@ -522,6 +523,9 @@ class BrowserView : public BrowserBubbleHost,
   // has already been added to the view hierarchy.
   void ProcessTabSelected(TabContentsWrapper* new_contents,
                           bool change_tab_contents);
+
+  // Exposes resize corner size to BrowserViewLayout.
+  gfx::Size GetResizeCornerSize() const;
 
   // Last focused view that issued a tab traversal.
   int last_focused_view_storage_id_;
