aca343a7025bbcfa5b28573601a8865ad9797125
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-12 13:21:13 +0000

    Remove extraneous #include's from http_auth_controller.cc
    BUG=None
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/6109009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71164 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_handler.cc
+++ net/http/http_auth_handler.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_auth_handler.h"
 
 #include "base/logging.h"
-#include "base/string_util.h"
-#include "base/stringprintf.h"
 #include "net/base/net_errors.h"
 
 namespace net {
