7a869de45eb8518731297ab0bd443b6be597c7a9
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-12 03:19:12 +0000

    replace memcmp with MagicCmp that supports '.' for single character of anything and add webp BUG=67987 TEST=omnibox can show webp from filer or harddrive when mimetype or extension is wrong
    Review URL: http://codereview.chromium.org/6124007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71141 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_sniffer.cc
+++ net/base/mime_sniffer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -147,6 +147,8 @@ static const MagicNumber kMagicNumbers[] = {
   MAGIC_NUMBER("image/tiff", "II*")
   MAGIC_NUMBER("image/tiff", "MM\x00*")
   MAGIC_NUMBER("audio/mpeg", "ID3")
+  MAGIC_NUMBER("image/webp", "RIFF....WEBPVP8 ")
+  MAGIC_NUMBER("video/webm", "\x1A\x45\xDF\xA3")
   // TODO(abarth): we don't handle partial byte matches yet
   // MAGIC_NUMBER("video/mpeg", "\x00\x00\x01\xB")
   // MAGIC_NUMBER("audio/mpeg", "\xFF\xE")
@@ -216,6 +218,19 @@ static scoped_refptr<base::Histogram> UMASnifferHistogramGet(const char* name,
   return counter;
 }
 
+// Compare content header to a magic number where magic_entry can contain '.'
+// for single character of anything, allowing some bytes to be skipped.
+static bool MagicCmp(const char* magic_entry, const char* content, size_t len) {
+  while (len) {
+    if ((*magic_entry != '.') && (*magic_entry != *content))
+      return false;
+    ++magic_entry;
+    ++content;
+    --len;
+  }
+  return true;
+}
+
 static bool MatchMagicNumber(const char* content, size_t size,
                              const MagicNumber* magic_entry,
                              std::string* result) {
@@ -240,7 +255,7 @@ static bool MatchMagicNumber(const char* content, size_t size,
     }
   } else {
     if (size >= len)
-      match = (memcmp(magic_entry->magic, content, len) == 0);
+      match = MagicCmp(magic_entry->magic, content, len);
   }
 
   if (match) {
@@ -252,7 +267,8 @@ static bool MatchMagicNumber(const char* content, size_t size,
 
 static bool CheckForMagicNumbers(const char* content, size_t size,
                                  const MagicNumber* magic, size_t magic_len,
-                                 base::Histogram* counter, std::string* result) {
+                                 base::Histogram* counter,
+                                 std::string* result) {
   for (size_t i = 0; i < magic_len; ++i) {
     if (MatchMagicNumber(content, size, &(magic[i]), result)) {
       if (counter) counter->Add(static_cast<int>(i));
