c3ee47790ec64dcc90a7c0bb121ac5b30f618f96
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-12 21:04:05 +0000

    Fix for https://bugs.webkit.org/show_bug.cgi?id=52309 Expose fixed layout through WebKit SPI -and corresponding- <rdar://problem/8844464>
    Reviewed by Anders Carlsson.
    
    Source/WebCore: 
    
    Export symbols for ScrollView::setUseFixedLayout() and 
    ScrollView::setFixedLayoutSize()
    * WebCore.exp.in:
    
    WebKit/mac: 
    
    * WebView/WebView.mm:
    (-[WebView _setUseFixedLayout:]):
    (-[WebView _setFixedLayoutSize:]):
    * WebView/WebViewPrivate.h:
    
    WebKit2: 
    
    Store useFixedLayout and fixedLayoutSize in the 
    WebPageCreationParameters and the WebPageProxy.
    * Shared/WebPageCreationParameters.cpp:
    (WebKit::WebPageCreationParameters::encode):
    (WebKit::WebPageCreationParameters::decode):
    * Shared/WebPageCreationParameters.h:
    * UIProcess/API/C/WKPage.cpp:
    (WKPageSetUseFixedLayout):
    (WKPageSetFixedLayoutSize):
    * UIProcess/API/C/WKPage.h:
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::WebPageProxy):
    (WebKit::WebPageProxy::setUseFixedLayout):
    (WebKit::WebPageProxy::setFixedLayoutSize):
    (WebKit::WebPageProxy::creationParameters):
    * UIProcess/WebPageProxy.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::setUseFixedLayout):
    (WebKit::WebPage::setFixedLayoutSize):
    * WebProcess/WebPage/WebPage.h:
    * WebProcess/WebPage/WebPage.messages.in:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit2/Shared/WebPageCreationParameters.cpp
+++ third_party/WebKit/WebKit2/Shared/WebPageCreationParameters.cpp
@@ -43,6 +43,8 @@ void WebPageCreationParameters::encode(CoreIPC::ArgumentEncoder* encoder) const
     encoder->encode(pageGroupData);
     encoder->encode(drawsBackground);
     encoder->encode(drawsTransparentBackground);
+    encoder->encode(useFixedLayout);
+    encoder->encode(fixedLayoutSize);
     encoder->encode(userAgent);
     encoder->encode(sessionState);
     encoder->encode(highestUsedBackForwardItemID);
@@ -77,6 +79,10 @@ bool WebPageCreationParameters::decode(CoreIPC::ArgumentDecoder* decoder, WebPag
     if (!decoder->decode(parameters.drawsBackground))
         return false;
     if (!decoder->decode(parameters.drawsTransparentBackground))
+        return false;
+    if (!decoder->decode(parameters.useFixedLayout))
+        return false;
+    if (!decoder->decode(parameters.fixedLayoutSize))
         return false;
     if (!decoder->decode(parameters.userAgent))
         return false;
--- third_party/WebKit/WebKit2/Shared/WebPageCreationParameters.h
+++ third_party/WebKit/WebKit2/Shared/WebPageCreationParameters.h
@@ -59,6 +59,9 @@ struct WebPageCreationParameters {
     bool drawsBackground;
     bool drawsTransparentBackground;
 
+    bool useFixedLayout;
+    WebCore::IntSize fixedLayoutSize;
+
     String userAgent;
 
     SessionState sessionState;
