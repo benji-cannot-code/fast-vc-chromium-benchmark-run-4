5023565f2f5d276a2ec002dc875773888bdc6d30
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-13 23:59:03 +0000

    EGL contexts reference count the EGL surfaces they share.
    This fixes the case where a NativeViewEGLContext and a SecondaryEGL context are backed by the same EGL surface and  the native one, which previously owned the surface, is destroyed first.
    
    TEST=try
    BUG=none
    
    Review URL: http://codereview.chromium.org/6296004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71390 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/gl/gl_context_egl.cc
+++ app/gfx/gl/gl_context_egl.cc
@@ -59,6 +59,22 @@ const char* GetLastEGLErrorString() {
 }
 }  // namespace anonymous
 
+SharedEGLSurface::SharedEGLSurface(EGLSurface surface) : surface_(surface) {
+}
+
+SharedEGLSurface::~SharedEGLSurface() {
+  if (surface_) {
+    if (!eglDestroySurface(g_display, surface_)) {
+      LOG(ERROR) << "eglDestroySurface failed with error "
+                 << GetLastEGLErrorString();
+    }
+  }
+}
+
+EGLSurface SharedEGLSurface::egl_surface() const {
+  return surface_;
+}
+
 bool BaseEGLContext::InitializeOneOff() {
   static bool initialized = false;
   if (initialized)
@@ -144,7 +160,6 @@ std::string BaseEGLContext::GetExtensions() {
 
 NativeViewEGLContext::NativeViewEGLContext(void* window)
     : window_(window),
-      surface_(NULL),
       context_(NULL)
 {
 }
@@ -158,9 +173,12 @@ bool NativeViewEGLContext::Initialize() {
   // Create a surface for the native window.
   EGLNativeWindowType native_window =
       reinterpret_cast<EGLNativeWindowType>(window_);
-  surface_ = eglCreateWindowSurface(g_display, g_config, native_window, NULL);
+  surface_ = new SharedEGLSurface(eglCreateWindowSurface(g_display,
+                                                         g_config,
+                                                         native_window,
+                                                         NULL));
 
-  if (!surface_) {
+  if (!surface_->egl_surface()) {
     LOG(ERROR) << "eglCreateWindowSurface failed with error "
                << GetLastEGLErrorString();
     Destroy();
@@ -205,20 +223,14 @@ void NativeViewEGLContext::Destroy() {
     context_ = NULL;
   }
 
-  if (surface_) {
-    if (!eglDestroySurface(g_display, surface_)) {
-      LOG(ERROR) << "eglDestroySurface failed with error "
-                 << GetLastEGLErrorString();
-    }
-
-    surface_ = NULL;
-  }
+  surface_ = NULL;
 }
 
 bool NativeViewEGLContext::MakeCurrent() {
   DCHECK(context_);
   if (!eglMakeCurrent(g_display,
-                      surface_, surface_,
+                      surface_->egl_surface(),
+                      surface_->egl_surface(),
                       context_)) {
     VLOG(1) << "eglMakeCurrent failed with error "
             << GetLastEGLErrorString();
@@ -238,7 +250,7 @@ bool NativeViewEGLContext::IsOffscreen() {
 }
 
 bool NativeViewEGLContext::SwapBuffers() {
-  if (!eglSwapBuffers(g_display, surface_)) {
+  if (!eglSwapBuffers(g_display, surface_->egl_surface())) {
     VLOG(1) << "eglSwapBuffers failed with error "
             << GetLastEGLErrorString();
     return false;
@@ -261,8 +273,10 @@ gfx::Size NativeViewEGLContext::GetSize() {
   // get updated on resize. When it does, we can share the code.
   EGLint width;
   EGLint height;
-  if (!eglQuerySurface(g_display, surface_, EGL_WIDTH, &width) ||
-      !eglQuerySurface(g_display, surface_, EGL_HEIGHT, &height)) {
+  if (!eglQuerySurface(
+          g_display, surface_->egl_surface(), EGL_WIDTH, &width) ||
+      !eglQuerySurface(
+          g_display, surface_->egl_surface(), EGL_HEIGHT, &height)) {
     NOTREACHED() << "eglQuerySurface failed with error "
                  << GetLastEGLErrorString();
     return gfx::Size();
@@ -284,14 +298,12 @@ void NativeViewEGLContext::SetSwapInterval(int interval) {
   }
 }
 
-EGLSurface NativeViewEGLContext::GetSurface() {
+SharedEGLSurface* NativeViewEGLContext::GetSurface() {
   return surface_;
 }
 
 SecondaryEGLContext::SecondaryEGLContext()
-    : surface_(NULL),
-      own_surface_(false),
-      context_(NULL)
+    : context_(NULL)
 {
 }
 
@@ -308,7 +320,6 @@ bool SecondaryEGLContext::Initialize(GLContext* shared_context) {
 
   if (shared_context) {
     surface_ = static_cast<BaseEGLContext*>(shared_context)->GetSurface();
-    own_surface_ = false;
 
     // Create a context.
     context_ = eglCreateContext(g_display,
@@ -323,13 +334,15 @@ bool SecondaryEGLContext::Initialize(GLContext* shared_context) {
       EGL_NONE
     };
 
-    surface_ = eglCreatePbufferSurface(g_display, g_config, kPbufferAttribs);
-    if (!surface_) {
+    surface_ = new SharedEGLSurface(eglCreatePbufferSurface(g_display,
+                                                            g_config,
+                                                            kPbufferAttribs));
+    if (!surface_->egl_surface()) {
       LOG(ERROR) << "eglCreatePbufferSurface failed with error "
                  << GetLastEGLErrorString();
+      Destroy();
       return false;
     }
-    own_surface_ = true;
 
     context_ = eglCreateContext(g_display, g_config, NULL, kContextAttributes);
 #else
@@ -349,16 +362,6 @@ bool SecondaryEGLContext::Initialize(GLContext* shared_context) {
 }
 
 void SecondaryEGLContext::Destroy() {
-  if (own_surface_) {
-    if (!eglDestroySurface(g_display, surface_)) {
-      LOG(ERROR) << "eglDestroySurface failed with error "
-                 << GetLastEGLErrorString();
-    }
-
-    own_surface_ = false;
-  }
-  surface_ = NULL;
-
   if (context_) {
     if (!eglDestroyContext(g_display, context_)) {
       LOG(ERROR) << "eglDestroyContext failed with error "
@@ -367,12 +370,15 @@ void SecondaryEGLContext::Destroy() {
 
     context_ = NULL;
   }
+
+  surface_ = NULL;
 }
 
 bool SecondaryEGLContext::MakeCurrent() {
   DCHECK(context_);
   if (!eglMakeCurrent(g_display,
-                      surface_, surface_,
+                      surface_->egl_surface(),
+                      surface_->egl_surface(),
                       context_)) {
     VLOG(1) << "eglMakeCurrent failed with error "
             << GetLastEGLErrorString();
@@ -410,7 +416,7 @@ void SecondaryEGLContext::SetSwapInterval(int interval) {
   NOTREACHED() << "Attempt to call SetSwapInterval on a SecondaryEGLContext.";
 }
 
-EGLSurface SecondaryEGLContext::GetSurface() {
+SharedEGLSurface* SecondaryEGLContext::GetSurface() {
   return surface_;
 }
 
