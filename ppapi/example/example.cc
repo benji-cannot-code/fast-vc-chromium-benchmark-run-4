abecc8f336028310a0a7a53f50d55a6358c34449
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-13 22:10:36 +0000

    Added plugin size to error logging and a logging statement when the plugin size changes. I found these changes useful while debugging issues when modifying the plugin.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6214007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71366 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/example/example.cc
+++ ppapi/example/example.cc
@@ -204,7 +204,7 @@ class MyInstance : public pp::Instance, public MyFetcherClient {
     pp::ImageData image(this, PP_IMAGEDATAFORMAT_BGRA_PREMUL,
                         pp::Size(width, height), false);
     if (image.is_null()) {
-      printf("Couldn't allocate the image data\n");
+      printf("Couldn't allocate the image data: %d, %d\n", width, height);
       return image;
     }
 
@@ -219,6 +219,7 @@ class MyInstance : public pp::Instance, public MyFetcherClient {
       }
     }
 
+    // Draw the orbiting box.
     float radians = static_cast<float>(animation_counter_) / kStepsPerCircle *
         2 * 3.14159265358979F;
 
@@ -226,7 +227,8 @@ class MyInstance : public pp::Instance, public MyFetcherClient {
     int x = static_cast<int>(cos(radians) * radius + radius + 2);
     int y = static_cast<int>(sin(radians) * radius + radius + 2);
 
-    FillRect(&image, x - 3, y - 3, 7, 7, 0x80000000);
+    const uint32_t box_bgra = 0x80000000;  // Alpha 50%.
+    FillRect(&image, x - 3, y - 3, 7, 7, box_bgra);
     return image;
   }
 
@@ -235,6 +237,8 @@ class MyInstance : public pp::Instance, public MyFetcherClient {
     if (!image.is_null()) {
       device_context_.ReplaceContents(&image);
       device_context_.Flush(pp::CompletionCallback(&FlushCallback, this));
+    } else {
+      printf("NullImage: %d, %d\n", width_, height_);
     }
   }
 
@@ -245,6 +249,8 @@ class MyInstance : public pp::Instance, public MyFetcherClient {
 
     width_ = position.size().width();
     height_ = position.size().height();
+    printf("DidChangeView relevant change: width=%d height:%d\n",
+           width_, height_);
 
     device_context_ = pp::Graphics2D(this, pp::Size(width_, height_), false);
     if (!BindGraphics(device_context_)) {
