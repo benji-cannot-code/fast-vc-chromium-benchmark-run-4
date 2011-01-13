4d5d16e5bf492b8c9acfcf65d8d85ca91f24d686
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-13 00:35:59 +0000

    2011-01-12  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Getting rid of an assert that was causing build failures that turns out not to be needed.
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::paintsWithTransform):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -3484,7 +3484,6 @@ bool RenderLayer::hasCompositedMask() const
 bool RenderLayer::paintsWithTransform(PaintBehavior paintBehavior) const
 {
 #if USE(ACCELERATED_COMPOSITING)
-    ASSERT(backing());
     bool paintsToWindow = !isComposited() || backing()->paintingGoesToWindow();
 #else
     bool paintsToWindow = true;
