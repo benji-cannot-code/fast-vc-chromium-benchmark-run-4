a2bbf5bd081de2bd2091f720f7aeda53071f5dac
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-13 10:31:30 +0000

    Delete test view delegate at end of test
    BUG=none
    TEST=leak 000000002551055A goes away
    
    Review URL: http://codereview.chromium.org/6132009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71309 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/textfield_views_model_unittest.cc
+++ views/controls/textfield/textfield_views_model_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/auto_reset.h"
 #include "base/message_loop.h"
 #include "base/scoped_ptr.h"
 #include "base/utf_string_conversions.h"
@@ -294,7 +295,9 @@ TEST(TextfieldViewsModelTest, SetText) {
 #define MAYBE_Clipboard Clipboard
 #endif
 TEST(TextfieldViewsModelTest, MAYBE_Clipboard) {
-  views::ViewsDelegate::views_delegate = new TestViewsDelegate();
+  scoped_ptr<TestViewsDelegate> test_views_delegate(new TestViewsDelegate());
+  AutoReset<views::ViewsDelegate*> auto_reset(
+      &views::ViewsDelegate::views_delegate, test_views_delegate.get());
   ui::Clipboard* clipboard
       = views::ViewsDelegate::views_delegate->GetClipboard();
   string16 initial_clipboard_text;
