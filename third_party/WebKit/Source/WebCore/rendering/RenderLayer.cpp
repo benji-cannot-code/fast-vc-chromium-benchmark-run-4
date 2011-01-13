2ff6e2b3d75644c9430579dccad0261ca97427ac
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-13 00:14:09 +0000

    2011-01-12  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Another fix for Gtk
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::paintsWithTransform):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -3484,7 +3484,7 @@ bool RenderLayer::hasCompositedMask() const
 bool RenderLayer::paintsWithTransform(PaintBehavior paintBehavior) const
 {
 #if USE(ACCELERATED_COMPOSITING)
-    assert(backing());
+    ASSERT(backing());
     bool paintsToWindow = !isComposited() || backing()->paintingGoesToWindow();
 #else
     bool paintsToWindow = true;
