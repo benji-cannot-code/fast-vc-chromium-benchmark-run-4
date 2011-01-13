221788554ed4ff38d62e44db50b8e1c0d64ba417
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-13 00:22:53 +0000

    2011-01-12  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Sam Weinig.
    
            Detect, identify and recover from WebProcess crashes in run-webkit-tests
            https://bugs.webkit.org/show_bug.cgi?id=52330
    
            * Scripts/old-run-webkit-tests: Detect when the regression test tool reports
            a web process crash.
            * WebKitTestRunner/TestController.cpp:
            (WTR::TestController::initialize): Set up a processDidCrash
            callback.
            (WTR::TestController::processDidCrash): Report that the Web process
            crashed.
            * WebKitTestRunner/TestController.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -273,7 +273,7 @@ void TestController::initialize(int argc, const char* argv[])
         0, // didFinishProgress
         0, // didBecomeUnresponsive
         0, // didBecomeResponsive
-        0, // processDidExit
+        processDidCrash, // processDidCrash
         0 // didChangeBackForwardList
     };
     WKPageSetPageLoaderClient(m_mainWebView->page(), &pageLoaderClient);
@@ -396,6 +396,11 @@ void TestController::didFinishLoadForFrame(WKPageRef page, WKFrameRef frame, WKT
     static_cast<TestController*>(const_cast<void*>(clientInfo))->didFinishLoadForFrame(page, frame);
 }
 
+void TestController::processDidCrash(WKPageRef page, const void* clientInfo)
+{
+    static_cast<TestController*>(const_cast<void*>(clientInfo))->processDidCrash(page);
+}
+
 void TestController::didFinishLoadForFrame(WKPageRef page, WKFrameRef frame)
 {
     if (m_state != Resetting)
@@ -410,6 +415,12 @@ void TestController::didFinishLoadForFrame(WKPageRef page, WKFrameRef frame)
 
     m_doneResetting = true;
     shared().notifyDone();
+}
+
+void TestController::processDidCrash(WKPageRef page)
+{
+    fputs("#CRASHED - WebProcess\n", stderr);
+    fflush(stderr);
 }
 
 } // namespace WTR
