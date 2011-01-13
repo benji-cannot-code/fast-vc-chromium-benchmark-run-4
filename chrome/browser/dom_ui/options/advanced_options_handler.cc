90607905e3688dd8ed4aafa487eef801f8611db0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-13 02:03:54 +0000

    Tidy up <select>s in tabbed options.
    Use pref ui for <select> where possible. This simplifies the zoom select and fixes the sync select.
    
    Also add more error checking.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6174009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options/advanced_options_handler.cc
+++ chrome/browser/dom_ui/options/advanced_options_handler.cc
@@ -201,7 +201,6 @@ void AdvancedOptionsHandler::Initialize() {
   DCHECK(dom_ui_);
   SetupMetricsReportingCheckbox();
   SetupMetricsReportingSettingVisibility();
-  SetupDefaultZoomLevel();
   SetupFontSizeLabel();
   SetupDownloadLocationPath();
   SetupAutoOpenFileTypesDisabledAttribute();
@@ -245,7 +244,6 @@ DOMMessageHandler* AdvancedOptionsHandler::Attach(DOMUI* dom_ui) {
   default_download_location_.Init(prefs::kDownloadDefaultDirectory,
                                   prefs, this);
   auto_open_files_.Init(prefs::kDownloadExtensionsToOpen, prefs, this);
-  default_zoom_level_.Init(prefs::kDefaultZoomLevel, prefs, this);
   default_font_size_.Init(prefs::kWebKitDefaultFontSize, prefs, this);
   default_fixed_font_size_.Init(prefs::kWebKitDefaultFixedFontSize, prefs,
                                 this);
@@ -265,8 +263,6 @@ void AdvancedOptionsHandler::RegisterMessages() {
   dom_ui_->RegisterMessageCallback("autoOpenFileTypesAction",
       NewCallback(this,
                   &AdvancedOptionsHandler::HandleAutoOpenButton));
-  dom_ui_->RegisterMessageCallback("defaultZoomLevelAction",
-      NewCallback(this, &AdvancedOptionsHandler::HandleDefaultZoomLevel));
   dom_ui_->RegisterMessageCallback("defaultFontSizeAction",
       NewCallback(this, &AdvancedOptionsHandler::HandleDefaultFontSize));
 #if !defined(OS_CHROMEOS)
@@ -386,14 +382,6 @@ void AdvancedOptionsHandler::HandleMetricsReportingCheckbox(
   enable_metrics_recording_.SetValue(is_enabled);
   SetupMetricsReportingCheckbox();
 #endif
-}
-
-void AdvancedOptionsHandler::HandleDefaultZoomLevel(const ListValue* args) {
-  UserMetricsRecordAction(UserMetricsAction("Options_ChangeDefaultZoomLevel"));
-  int zoom_level;
-  if (ExtractIntegerValue(args, &zoom_level)) {
-    default_zoom_level_.SetValue(static_cast<double>(zoom_level));
-  }
 }
 
 void AdvancedOptionsHandler::HandleDefaultFontSize(const ListValue* args) {
@@ -553,13 +541,6 @@ void AdvancedOptionsHandler::SetupMetricsReportingSettingVisibility() {
         visible);
   }
 #endif
-}
-
-void AdvancedOptionsHandler::SetupDefaultZoomLevel() {
-  // We're only interested in integer values, so convert to int.
-  FundamentalValue value(static_cast<int>(default_zoom_level_.GetValue()));
-  dom_ui_->CallJavascriptFunction(
-      L"options.AdvancedOptions.SetDefaultZoomLevel", value);
 }
 
 void AdvancedOptionsHandler::SetupFontSizeLabel() {
--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -63,6 +63,7 @@ cr.define('options', function() {
           OptionsPage.showOverlay('instantConfirmOverlay');
         }
       };
+      $('defaultSearchEngine').onchange = this.setDefaultSearchEngine;
 
       var homepageField = $('homepageURL');
       $('homepageUseNTPButton').onchange =
