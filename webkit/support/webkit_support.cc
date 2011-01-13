46b2183e41e366776990f490c48c5687ea85b78e
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-13 18:08:24 +0000

    Add support for DumpRenderTree to use the GPU rather than software renderer
    BUG=68797
    TEST=previous buildbots test old path, new buildbots will test the GPU path
    
    Support for webkit side changes:
    https://bugs.webkit.org/show_bug.cgi?id=52333
    
    Review URL: http://codereview.chromium.org/6258001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71332 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/webkit_support.cc
+++ webkit/support/webkit_support.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/support/webkit_support.h"
 
+#include "app/gfx/gl/gl_context.h"
 #include "app/gfx/gl/gl_implementation.h"
 #include "base/at_exit.h"
 #include "base/base64.h"
@@ -289,6 +290,19 @@ WebKit::WebApplicationCacheHost* CreateApplicationCacheHost(
 WebKit::WebString GetWebKitRootDir() {
   FilePath path = GetWebKitRootDirFilePath();
   return WebKit::WebString::fromUTF8(WideToUTF8(path.ToWStringHack()).c_str());
+}
+
+void SetUpGLBindings(GLBindingPreferences bindingPref) {
+  switch(bindingPref) {
+    case GL_BINDING_DEFAULT:
+      gfx::GLContext::InitializeOneOff();
+      break;
+    case GL_BINDING_SOFTWARE_RENDERER:
+      gfx::InitializeGLBindings(gfx::kGLImplementationOSMesaGL);
+      break;
+    default:
+      NOTREACHED();
+  }
 }
 
 void RegisterMockedURL(const WebKit::WebURL& url,
