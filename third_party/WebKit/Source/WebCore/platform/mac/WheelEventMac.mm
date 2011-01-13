e10ddf3c6836067b1e9519a29c8c19c41daae439
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-13 01:38:26 +0000

    Build fix, unreviewed.
    git-svn-id: svn://svn.chromium.org/blink/trunk@75663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/WheelEventMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/WheelEventMac.mm
@@ -49,6 +49,7 @@ static PlatformWheelEventPhase phaseForEvent(NSEvent *event)
         phase |= PlatformWheelEventPhaseCancelled;
     return static_cast<PlatformWheelEventPhase>(phase);
 #else
+    (void)event;
     return PlatformWheelEventPhaseNone;
 #endif
 }
