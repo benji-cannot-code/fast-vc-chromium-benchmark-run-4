a413677df48020ace454ddbd4f470ef38691f907
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-13 23:52:42 +0000

    2011-01-13  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            add container divs for diff blocks
            https://bugs.webkit.org/show_bug.cgi?id=52400
    
            This will help simplify a lot of code in code-review.js
            and make side-by-side diffs better (i.e. put removed lines
            to the left of corresponding added lines).
    
            Also, allow for running the JS from a local file. Now you can modify code-review.js
            to point to a local file and then run:
            ruby prettify.rb < foo.diff > foo.html
    
            foo.html will load a dummy code review matching foo.diff.
    
            Before structure:
            Line
            Line remove
            Line add
            Line add
            Line
    
            After structure:
            DiffBlock
              DiffBlockPart shared
                Line shared
            DiffBlock
              DiffBlockPart remove
                Line remove
              DiffBlockPart add
                Line add
                Line add
            DiffBlock
              DiffBlockPart shared
                Line shared
    
            * PrettyPatch/PrettyPatch.rb:
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -54,11 +54,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Attempt to activate only in the "Review Patch" context.
   if (window.top != window)
     return;
-  if (!window.location.search.match(/action=review/))
+
+  if (!window.location.search.match(/action=review/)
+      && !window.location.toString().match(/bugs\.webkit\.org\/PrettyPatch/))
     return;
+
   var attachment_id = determineAttachmentID();
   if (!attachment_id)
-    return;
+    console.log('No attachment ID');
 
   var next_line_id = 0;
   var files = {};
@@ -83,10 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function idify() {
     this.id = nextLineID();
-  }
-
-  function containerify() {
-    $(this).addClass('LineContainer');
   }
 
   function hoverify() {
@@ -315,7 +314,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function crawlDiff() {
-    $('.Line').each(idify).each(hoverify).each(containerify);
+    $('.Line').each(idify).each(hoverify);
     $('.FileDiff').each(function() {
       var file_name = $(this).children('h1').text();
       files[file_name] = this;
@@ -1087,7 +1086,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   $('.lineNumber').live('click', function() {
-    var line = $(this).parent();
+    var line = $(this).parents('.Line');
     if (line.hasClass('commentContext'))
       trimCommentContextToBefore(previousLineFor(line));
   }).live('mousedown', function() {
