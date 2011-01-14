b293b564011ac0bf94ea585ae226eefa3b8ea1ef
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-14 09:59:55 +0000

    2011-01-14  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed: gtk build fix.
    
            * webkit/webkitwebinspector.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebinspector.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebinspector.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestRequest.h"
 #include "HitTestResult.h"
 #include "InspectorClientGtk.h"
+#include "InspectorController.h"
 #include "IntPoint.h"
 #include "Page.h"
 #include "RenderLayer.h"
