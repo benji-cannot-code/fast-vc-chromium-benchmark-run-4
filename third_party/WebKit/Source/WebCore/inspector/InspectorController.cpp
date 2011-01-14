3a96a80c377bd3d7d78769f0e13bf9699856dc83
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-14 09:21:05 +0000

    2011-01-14  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r75774.
            http://trac.webkit.org/changeset/75774
            https://bugs.webkit.org/show_bug.cgi?id=52431
    
            gtk builds were broken (Requested by loislo2 on #webkit).
    
            * CMakeLists.txt:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * inspector/CodeGeneratorInspector.pm:
            * inspector/Inspector.idl:
            * inspector/InspectorBrowserDebuggerAgent.cpp: Removed.
            * inspector/InspectorBrowserDebuggerAgent.h: Removed.
            * inspector/InspectorController.cpp:
            (WebCore::InspectorController::InspectorController):
            (WebCore::InspectorController::inspectedPageDestroyed):
            (WebCore::InspectorController::didCommitLoad):
            (WebCore::InspectorController::enableDebuggerFromFrontend):
            (WebCore::InspectorController::disableDebugger):
            (WebCore::InspectorController::restoreStickyBreakpoints):
            (WebCore::InspectorController::restoreStickyBreakpoint):
            (WebCore::InspectorController::setEventListenerBreakpoint):
            (WebCore::InspectorController::removeEventListenerBreakpoint):
            (WebCore::InspectorController::hasEventListenerBreakpoint):
            (WebCore::InspectorController::setXHRBreakpoint):
            (WebCore::InspectorController::removeXHRBreakpoint):
            (WebCore::InspectorController::hasXHRBreakpoint):
            * inspector/InspectorController.h:
            * inspector/InspectorDOMAgent.cpp:
            (WebCore::InspectorDOMAgent::discardBindings):
            (WebCore::InspectorDOMAgent::setDOMBreakpoint):
            (WebCore::InspectorDOMAgent::removeDOMBreakpoint):
            (WebCore::InspectorDOMAgent::shouldBreakOnNodeInsertion):
            (WebCore::InspectorDOMAgent::shouldBreakOnNodeRemoval):
            (WebCore::InspectorDOMAgent::shouldBreakOnAttributeModification):
            (WebCore::InspectorDOMAgent::descriptionForDOMEvent):
            (WebCore::InspectorDOMAgent::didInsertDOMNode):
            (WebCore::InspectorDOMAgent::didRemoveDOMNode):
            (WebCore::InspectorDOMAgent::hasBreakpoint):
            (WebCore::InspectorDOMAgent::updateSubtreeBreakpoints):
            * inspector/InspectorDOMAgent.h:
            * inspector/InspectorInstrumentation.cpp:
            (WebCore::InspectorInstrumentation::willInsertDOMNodeImpl):
            (WebCore::InspectorInstrumentation::didInsertDOMNodeImpl):
            (WebCore::InspectorInstrumentation::willRemoveDOMNodeImpl):
            (WebCore::InspectorInstrumentation::didRemoveDOMNodeImpl):
            (WebCore::InspectorInstrumentation::willModifyDOMAttrImpl):
            (WebCore::InspectorInstrumentation::willSendXMLHttpRequestImpl):
            (WebCore::InspectorInstrumentation::pauseOnNativeEventIfNeeded):
    
    2011-01-14  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r75774.
            http://trac.webkit.org/changeset/75774
            https://bugs.webkit.org/show_bug.cgi?id=52431
    
            gtk builds were broken (Requested by loislo2 on #webkit).
    
            * Scripts/webkitpy/common/checkout/scm.py:
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorController.cpp
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InjectedScript.h"
 #include "InjectedScriptHost.h"
 #include "InspectorBackendDispatcher.h"
-#include "InspectorBrowserDebuggerAgent.h"
 #include "InspectorCSSAgent.h"
 #include "InspectorClient.h"
 #include "InspectorDOMAgent.h"
@@ -149,6 +148,7 @@ InspectorController::InspectorController(Page* page, InspectorClient* client)
     , m_injectedScriptHost(InjectedScriptHost::create(this))
 #if ENABLE(JAVASCRIPT_DEBUGGER)
     , m_attachDebuggerWhenShown(false)
+    , m_hasXHRBreakpointWithEmptyURL(false)
     , m_profilerAgent(InspectorProfilerAgent::create(this))
 #endif
 {
@@ -178,7 +178,6 @@ void InspectorController::inspectedPageDestroyed()
 
 #if ENABLE(JAVASCRIPT_DEBUGGER)
     m_debuggerAgent.clear();
-    m_browserDebuggerAgent.clear();
 #endif
     ASSERT(m_inspectedPage);
     m_inspectedPage = 0;
@@ -727,8 +726,6 @@ void InspectorController::didCommitLoad(DocumentLoader* loader)
 #if ENABLE(JAVASCRIPT_DEBUGGER)
         if (m_debuggerAgent) {
             m_debuggerAgent->clearForPageNavigation();
-            if (m_browserDebuggerAgent)
-                m_browserDebuggerAgent->clearForPageNavigation();
             restoreStickyBreakpoints();
         }
 #endif
@@ -1183,7 +1180,6 @@ void InspectorController::enableDebuggerFromFrontend(bool always)
     ASSERT(m_inspectedPage);
 
     m_debuggerAgent = InspectorDebuggerAgent::create(this, m_frontend.get());
-    m_browserDebuggerAgent = InspectorBrowserDebuggerAgent::create(this);
     restoreStickyBreakpoints();
 
     m_frontend->debuggerWasEnabled();
@@ -1216,7 +1212,6 @@ void InspectorController::disableDebugger(bool always)
     ASSERT(m_inspectedPage);
 
     m_debuggerAgent.clear();
-    m_browserDebuggerAgent.clear();
 
     m_attachDebuggerWhenShown = false;
 
@@ -1237,6 +1232,10 @@ void InspectorController::setStickyBreakpoints(PassRefPtr<InspectorObject> break
 
 void InspectorController::restoreStickyBreakpoints()
 {
+    m_eventListenerBreakpoints.clear();
+    m_XHRBreakpoints.clear();
+    m_hasXHRBreakpointWithEmptyURL = false;
+
     RefPtr<InspectorObject> allBreakpoints = m_state->getObject(InspectorState::stickyBreakpoints);
     KURL url = m_inspectedPage->mainFrame()->loader()->url();
     url.removeFragmentIdentifier();
@@ -1265,14 +1264,13 @@ void InspectorController::restoreStickyBreakpoint(PassRefPtr<InspectorObject> br
     if (!condition)
         return;
 
-    if (type == eventListenerBreakpointType && m_browserDebuggerAgent) {
+    if (type == eventListenerBreakpointType) {
         if (!enabled)
             return;
         String eventName;
-        if (!condition->getString("eventName", &eventName))
-            return;
-        m_browserDebuggerAgent->setEventListenerBreakpoint(eventName);
-    } else if (type == javaScriptBreakpointType && m_debuggerAgent) {
+        if (condition->getString("eventName", &eventName))
+            setEventListenerBreakpoint(eventName);
+    } else if (type == javaScriptBreakpointType) {
         String url;
         if (!condition->getString("url", &url))
             return;
@@ -1282,16 +1280,63 @@ void InspectorController::restoreStickyBreakpoint(PassRefPtr<InspectorObject> br
         String javaScriptCondition;
         if (!condition->getString("condition", &javaScriptCondition))
             return;
-        m_debuggerAgent->setStickyBreakpoint(url, static_cast<unsigned>(lineNumber), javaScriptCondition, enabled);
-    } else if (type == xhrBreakpointType && m_browserDebuggerAgent) {
+        if (m_debuggerAgent)
+            m_debuggerAgent->setStickyBreakpoint(url, static_cast<unsigned>(lineNumber), javaScriptCondition, enabled);
+    } else if (type == xhrBreakpointType) {
         if (!enabled)
             return;
         String url;
-        if (!condition->getString("url", &url))
-            return;
-        m_browserDebuggerAgent->setXHRBreakpoint(url);
+        if (condition->getString("url", &url))
+            setXHRBreakpoint(url);
     }
 }
+
+void InspectorController::setEventListenerBreakpoint(const String& eventName)
+{
+    m_eventListenerBreakpoints.add(eventName);
+}
+
+void InspectorController::removeEventListenerBreakpoint(const String& eventName)
+{
+    m_eventListenerBreakpoints.remove(eventName);
+}
+
+bool InspectorController::hasEventListenerBreakpoint(const String& eventName)
+{
+    return m_eventListenerBreakpoints.contains(eventName);
+}
+
+void InspectorController::setXHRBreakpoint(const String& url)
+{
+    if (url.isEmpty())
+        m_hasXHRBreakpointWithEmptyURL = true;
+    else
+        m_XHRBreakpoints.add(url);
+}
+
+void InspectorController::removeXHRBreakpoint(const String& url)
+{
+    if (url.isEmpty())
+        m_hasXHRBreakpointWithEmptyURL = false;
+    else
+        m_XHRBreakpoints.remove(url);
+}
+
+bool InspectorController::hasXHRBreakpoint(const String& url, String* breakpointURL)
+{
+    if (m_hasXHRBreakpointWithEmptyURL) {
+        *breakpointURL = "";
+        return true;
+    }
+    for (HashSet<String>::iterator it = m_XHRBreakpoints.begin(); it != m_XHRBreakpoints.end(); ++it) {
+        if (url.contains(*it)) {
+            *breakpointURL = *it;
+            return true;
+        }
+    }
+    return false;
+}
+
 #endif
 
 void InspectorController::dispatchOnInjectedScript(long injectedScriptId, const String& methodName, const String& arguments, RefPtr<InspectorValue>* result, bool* hadException)
