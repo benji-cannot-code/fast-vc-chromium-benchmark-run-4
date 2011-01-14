c5dfa2c453c5a059545f694e9e9e3aee43785f11
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 00:37:58 +0000

    Revert 71334 - Fix Pepper URL Loader callbacks.
    [Not applying the "obvious" fix, since it might lead to callbacks being called
    incorrectly.]
    
    Add some tests (for aborting calls).
    
    BUG=none
    TEST=ppapi_tests
    
    Review URL: http://codereview.chromium.org/6220006
    
    TBR=viettrungluu@chromium.org,jam@chromium.org,brettw@chromium.org
    Review URL: http://codereview.chromium.org/6254003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71400 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_url_loader.h
+++ ppapi/tests/test_url_loader.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -43,7 +43,6 @@ class TestURLLoader : public TestCase {
   std::string TestStreamToFile();
   std::string TestSameOriginRestriction();
   std::string TestAuditURLRedirect();
-  std::string TestAbortCalls();
 
   const PPB_FileIOTrusted_Dev* file_io_trusted_interface_;
 };
