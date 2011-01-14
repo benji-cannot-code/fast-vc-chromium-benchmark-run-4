12d265578831797f8eb0a6b80054936fe38a4205
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 00:05:52 +0000

    Another attempt to fix the ChromeFrame InstallFlowTest failures. We need to refresh the user agent before invoking SetSite on the ChromeFrame BHO to ensure that the protocol sink patch is not disabled.
    BUG=none
    TEST=Covered by existing ChromeFrame InstallFlowTest.
    
    Review URL: http://codereview.chromium.org/6252004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71391 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_activex.cc
+++ chrome_frame/chrome_frame_activex.cc
@@ -683,6 +683,14 @@ HRESULT ChromeFrameActivex::registerBhoIfNeeded() {
     return hr;
   }
 
+  hr = UrlMkSetSessionOption(URLMON_OPTION_USERAGENT_REFRESH, NULL, 0, 0);
+  if (FAILED(hr)) {
+    DLOG(ERROR) << "Failed to refresh user agent string from registry. "
+                << "UrlMkSetSessionOption returned "
+                << base::StringPrintf("0x%08x", hr);
+    return hr;
+  }
+
   hr = bho->SetSite(web_browser2);
   if (FAILED(hr)) {
     NOTREACHED() << "ChromeFrame BHO SetSite failed. Error:"
@@ -692,12 +700,5 @@ HRESULT ChromeFrameActivex::registerBhoIfNeeded() {
 
   web_browser2->PutProperty(base::win::ScopedBstr(bho_class_id_as_string),
                             base::win::ScopedVariant(bho));
-
-  hr = UrlMkSetSessionOption(URLMON_OPTION_USERAGENT_REFRESH, NULL, 0, 0);
-  if (FAILED(hr)) {
-    DLOG(ERROR) << "Failed to refresh user agent string from registry. "
-                << "UrlMkSetSessionOption returned "
-                << base::StringPrintf("0x%08x", hr);
-  }
-  return hr;
+  return S_OK;
 }
