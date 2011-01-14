6462f8f448d0f4b79f7dc7688c8d30b00d12eaac
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-14 10:05:12 +0000

    2011-01-14  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed: gtk build fix (2).
    
            * webkit/webkitwebview.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/WebKit/gtk/webkit/webkitwebview.cpp
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HitTestResult.h"
 #include "IconDatabase.h"
 #include "InspectorClientGtk.h"
+#include "InspectorController.h"
 #include "MemoryCache.h"
 #include "MouseEventWithHitTestResults.h"
 #include "NotImplemented.h"
