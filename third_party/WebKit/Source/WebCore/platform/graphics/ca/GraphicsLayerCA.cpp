d3e43a9ce6185d85e4fb01f24dba53c972921ce3
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-14 20:01:24 +0000

    2011-01-14  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Adam Roben.
    
            Issues with contentsScale in GraphicsLayerCA
            https://bugs.webkit.org/show_bug.cgi?id=52463
    
            Fix various issues with the recent contentsScale changes:
    
            * platform/graphics/ca/GraphicsLayerCA.cpp:
            (WebCore::GraphicsLayerCA::GraphicsLayerCA): Initialize m_contentsScale to 1.
            (WebCore::GraphicsLayerCA::updateContentsScale): Only do a setNeedsDisplay()
            if the layer draws content, otherwise we'll create backing store for empty layers.
            (WebCore::GraphicsLayerCA::swapFromOrToTiledLayer): Be sure to set contentsScale
            on the new (tiled or untiled) layer.
            (WebCore::GraphicsLayerCA::cloneLayer): Clones need contentsScale too.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75807 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -254,6 +254,7 @@ GraphicsLayerCA::GraphicsLayerCA(GraphicsLayerClient* client)
     , m_contentsLayerPurpose(NoContentsLayer)
     , m_contentsLayerHasBackgroundColor(false)
     , m_uncommittedChanges(NoChange)
+    , m_contentsScale(1)
 {
     m_layer = PlatformCALayer::create(PlatformCALayer::LayerTypeWebLayer, this);
 
@@ -1935,7 +1936,8 @@ void GraphicsLayerCA::updateContentsScale()
         swapFromOrToTiledLayer(needTiledLayer);
 
     m_layer->setContentsScale(m_contentsScale);
-    m_layer->setNeedsDisplay();
+    if (drawsContent())
+        m_layer->setNeedsDisplay();
 }
 
 void GraphicsLayerCA::setDebugBackgroundColor(const Color& color)
@@ -1995,6 +1997,7 @@ void GraphicsLayerCA::swapFromOrToTiledLayer(bool useTiledLayer)
     RefPtr<PlatformCALayer> oldLayer = m_layer;
     
     m_layer = PlatformCALayer::create(useTiledLayer ? PlatformCALayer::LayerTypeWebTiledLayer : PlatformCALayer::LayerTypeWebLayer, this);
+    m_layer->setContentsScale(m_contentsScale);
 
     m_usingTiledLayer = useTiledLayer;
     
@@ -2269,6 +2272,7 @@ PassRefPtr<PlatformCALayer> GraphicsLayerCA::cloneLayer(PlatformCALayer *layer, 
     newLayer->setDoubleSided(layer->isDoubleSided());
     newLayer->setOpaque(layer->isOpaque());
     newLayer->setBackgroundColor(layer->backgroundColor());
+    newLayer->setContentsScale(layer->contentsScale());
 
     if (cloneLevel == IntermediateCloneLevel) {
         newLayer->setOpacity(layer->opacity());
