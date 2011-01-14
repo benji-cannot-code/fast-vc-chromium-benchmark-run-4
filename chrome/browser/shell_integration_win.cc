a75e8807aac6086a0f8cc2c66ef48a25f551cec0
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 14:58:14 +0000

    Set Browser::app_name_ in a reasonable way for extension apps.
    BUG=67702
    TEST=BrowserTest.OpenAppWindowLikeNtp,BrowserTest.AppIdSwitch,manually open a few apps and see that windows taskbar hilights the right window.
    
    Review URL: http://codereview.chromium.org/6086004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Helper function for ShellIntegration::GetAppId to generates profile id
-// from profile path. "profile_id"  is composed of sanitized basenames of
+// from profile path. "profile_id" is composed of sanitized basenames of
 // user data dir and profile dir joined by a ".".
 std::wstring GetProfileIdFromPath(const FilePath& profile_path) {
   // Return empty string if profile_path is empty
@@ -228,6 +228,9 @@ bool MigrateChromiumShortcutsTask::GetExpectedAppId(
   if (command_line.HasSwitch(switches::kApp)) {
     app_name = UTF8ToWide(web_app::GenerateApplicationNameFromURL(
         GURL(command_line.GetSwitchValueASCII(switches::kApp))));
+  } else if (command_line.HasSwitch(switches::kAppId)) {
+    app_name = UTF8ToWide(web_app::GenerateApplicationNameFromExtensionId(
+        command_line.GetSwitchValueASCII(switches::kAppId)));
   } else {
     app_name = BrowserDistribution::GetDistribution()->GetBrowserAppId();
   }
