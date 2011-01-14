b4efa62eaa3c48c1dbcfe78b9720604474cf7f03
cstefansen@google.com <cstefansen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 18:27:37 +0000

    Enable Breakpad for crashes in trusted code in Native Client on Chrome for Linux. This change ensures that the appropriate command line flag (SetCrashReporterCommandLine()) is set for the NaCl process and read by the NaCl main process when it starts.
    TEST=
    0. Ensure that Native Client is DEPS'ed in at revision 4097 or higher
    1. From src/ issue
        ./build/gyp_chromim -f make -dlinux_breakpad=1
    2. Build Chromium (debug build)
    3. With env. variable CHROME_HEADLESS=1 and NACL_CRASH_TEST=1 run
        out/Debug/chrome --enable-nacl --enable-crash-reporter
    4. Visit a page that attempts to load a NaCl module, e.g.,
        http://naclports.googlecode.com/svn/trunk/src/gallery/index.html
    5. Observe that a .dmp file is created in ~/.config/chromium/Crash\ Reports/
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=206
    
    Review URL: http://codereview.chromium.org/6193005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_linux.cc
+++ chrome/app/breakpad_linux.cc
@@ -647,7 +647,7 @@ void EnableCrashDumping(const bool unattended) {
   }
 }
 
-// Currently Non-Browser = Renderer and Plugins
+// Currently Non-Browser = Renderer, Plugins and Native Client
 static bool
 NonBrowserCrashHandler(const void* crash_context, size_t crash_context_size,
                        void* context) {
@@ -748,7 +748,8 @@ void InitCrashReporter() {
     EnableCrashDumping(getenv(env_vars::kHeadless) != NULL);
   } else if (process_type == switches::kRendererProcess ||
              process_type == switches::kPluginProcess ||
-             process_type == switches::kZygoteProcess) {
+             process_type == switches::kZygoteProcess ||
+             process_type == switches::kNaClLoaderProcess) {
     // We might be chrooted in a zygote or renderer process so we cannot call
     // GetCollectStatsConsent because that needs access the the user's home
     // dir. Instead, we set a command line flag for these processes.
--- chrome/nacl/nacl_main.cc
+++ chrome/nacl/nacl_main.cc
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/message_loop.h"
 #include "base/string_util.h"
+
+#if defined(USE_LINUX_BREAKPAD)
+#include "chrome/app/breakpad_linux.h"
+#endif
+
 #include "chrome/common/child_process.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
@@ -91,6 +96,10 @@ static void HandleNaClTestParameters(const CommandLine& command_line) {
 
 // main() routine for the NaCl loader process.
 int NaClMain(const MainFunctionParams& parameters) {
+#if defined(USE_LINUX_BREAKPAD)
+  // Needs to be called after we have chrome::DIR_USER_DATA.
+  InitCrashReporter();
+#endif
   const CommandLine& parsed_command_line = parameters.command_line_;
 
   // This function allows pausing execution using the --nacl-startup-dialog
