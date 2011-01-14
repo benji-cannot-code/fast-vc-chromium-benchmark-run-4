0502dc2f1dddb99b7434bd551659140a3fcd6c46
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 17:44:04 +0000

    Debugging updates for sync integration tests
    The sync tests were using verbose logging in the past in order to help
    debug flaky and failing tests. Now that the tests are mostly green, the
    verbosity can be dialed down. This patch reduces the verbosity of sync
    test logging.
    
    In addition, a couple of bookmark tests occasionally fail due to a
    mismatch, and the logs don't make it clear where the test fails. This
    patch adds more detailed logging during bookmark comparison.
    
    TBR=tim@chromium.org
    BUG=none
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/6351001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/live_sync_test.cc
+++ chrome/test/live_sync/live_sync_test.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/command_line.h"
-#include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
@@ -107,9 +106,6 @@ LiveSyncTest::LiveSyncTest(TestType test_type)
       num_clients_(-1),
       test_server_handle_(base::kNullProcessHandle) {
   InProcessBrowserTest::set_show_window(true);
-
-  // TODO(rsimha): Remove after investigating flaky and failing sync tests.
-  logging::SetMinLogLevel(logging::LOG_VERBOSE);
 
   switch (test_type_) {
     case SINGLE_CLIENT: {
