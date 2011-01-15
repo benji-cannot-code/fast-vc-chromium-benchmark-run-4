8d955478ccbf5acd3cbb1cba562ab1d425c0d9b4
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 00:55:15 +0000

    2011-01-14  Tony Gentilcore  <tonyg@chromium.org>
            Unreviewed build fix.
    
            Fix Qt build after r75837
            https://bugs.webkit.org/show_bug.cgi?id=52494
    
            * editing/ApplyStyleCommand.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/ApplyStyleCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/ApplyStyleCommand.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSPropertyNames.h"
 #include "CSSStyleSelector.h"
 #include "CSSValueKeywords.h"
+#include "CSSValueList.h"
 #include "Document.h"
 #include "EditingStyle.h"
 #include "Editor.h"
--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameView.h"
 #include "GraphicsContext.h"
 #include "NotImplemented.h"
+#include "PaintInfo.h"
 #include "Page.h"
 #include "RenderBox.h"
 #include "RenderObject.h"
