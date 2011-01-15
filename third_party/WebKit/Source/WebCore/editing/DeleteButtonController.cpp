206a0b5e17e7e51265603aa29ff9d6a98baf1adf
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 00:16:47 +0000

    2011-01-14  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Eric Seidel.
    
            Stop instantiating legacy editing positions in AccessibilityRenderObject.cpp, Element.cpp,
            BreakBlockquoteCommand.cpp, CompositeEditCommand.cpp, and DeleteButtonController.cpp
            https://bugs.webkit.org/show_bug.cgi?id=52481
    
            Removed instantiation of legacy editing positions.
            Calls to Position::Position are replaced by calls to Position's convenience functions.
            
            Also fixed firstPositionInOrBeforeNode and lastPositionInOrAfterNode so that
            they instantiate right positions for text nodes.
    
            * accessibility/AccessibilityRenderObject.cpp:
            (WebCore::AccessibilityRenderObject::setSelectedTextRange):
            * dom/Element.cpp:
            (WebCore::Element::updateFocusAppearance):
            * editing/BreakBlockquoteCommand.cpp:
            (WebCore::BreakBlockquoteCommand::doApply):
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::inputText):
            (WebCore::CompositeEditCommand::rebalanceWhitespaceAt):
            (WebCore::CompositeEditCommand::moveParagraphContentsToNewBlockIfNecessary):
            (WebCore::CompositeEditCommand::moveParagraphs):
            (WebCore::CompositeEditCommand::breakOutOfEmptyListItem):
            (WebCore::CompositeEditCommand::breakOutOfEmptyMailBlockquotedParagraph):
            (WebCore::CompositeEditCommand::splitTreeToNode):
            * editing/DeleteButtonController.cpp:
            (WebCore::enclosingDeletableElement):
            * editing/htmlediting.h:
            (WebCore::firstPositionInOrBeforeNode): Instantiates firstPositionInNode instead of
            positionBeforeNode for a text node.
            (WebCore::lastPositionInOrAfterNode): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/DeleteButtonController.cpp
+++ third_party/WebKit/Source/WebCore/editing/DeleteButtonController.cpp
@@ -160,7 +160,7 @@ static HTMLElement* enclosingDeletableElement(const VisibleSelection& selection)
     if (!container->isContentEditable())
         return 0;
 
-    Node* element = enclosingNodeOfType(Position(container, 0), &isDeletableElement);
+    Node* element = enclosingNodeOfType(firstPositionInNode(container), &isDeletableElement);
     if (!element)
         return 0;
 
