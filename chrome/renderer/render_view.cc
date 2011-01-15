cabecfcf55dcbb30b153a43b384e86ac06d30a37
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-15 20:47:25 +0000

    Remove obsolete WebViewClient::createView method.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6282003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -2055,14 +2055,6 @@ void RenderView::OnMissingPluginStatus(
 
 // WebKit::WebViewClient ------------------------------------------------------
 
-// TODO(jochen): remove once WebKit side is up to date.
-WebView* RenderView::createView(
-    WebFrame* creator,
-    const WebWindowFeatures& features,
-    const WebString& frame_name) {
-  return createView(creator, WebURLRequest(), features, frame_name);
-}
-
 WebView* RenderView::createView(
     WebFrame* creator,
     const WebURLRequest& request,
