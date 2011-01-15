4857417f19357faaf265c1ed513987b484c91035
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 01:02:18 +0000

    2011-01-14  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            put remove lines to the left of add lines in sidebyside view
            https://bugs.webkit.org/show_bug.cgi?id=52458
    
            Also, sneaking in a fix to make long lines with no whitespace wrap.
    
            * PrettyPatch/PrettyPatch.rb:
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/PrettyPatch/PrettyPatch.rb
+++ third_party/WebKit/Websites/bugs.webkit.org/PrettyPatch/PrettyPatch.rb
@@ -165,10 +165,21 @@ h1 :hover {
     border-width: 1px 0px;
 }
 
-.LineSide {
+.sidebyside .DiffBlockPart.add:first-child {
+    float: right;
+}
+
+.LineSide,
+.sidebyside .DiffBlockPart.remove,
+.sidebyside .DiffBlockPart.add {
     display:inline-block;
     width:50%;
     vertical-align: top;
+}
+
+.sidebyside .DiffBlockPart.remove .to,
+.sidebyside .DiffBlockPart.add .from {
+    display: none;
 }
 
 .lineNumber, .expansionLineNumber {
@@ -192,8 +203,8 @@ h1 :hover {
 
 .text {
     padding-left: 5px;
-    white-space: pre;
     white-space: pre-wrap;
+    word-wrap: break-word;
 }
 
 .image {
@@ -394,9 +405,13 @@ body {
   position: absolute;
   top: -11%;
 }
+
+.clear_float {
+    clear: both;
+}
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
-<script src="code-review.js?version=23"></script>
+<script src="code-review.js?version=24"></script>
 EOF
 
     def self.revisionOrDescription(string)
@@ -580,7 +595,7 @@ END
         def to_html
             str = "<div class='DiffBlock'>\n"
             str += @parts.collect{ |part| part.to_html }.join
-            str += "</div>\n"
+            str += "<div class='clear_float'></div></div>\n"
         end
     end
 
@@ -597,7 +612,8 @@ END
         def to_html
             str = "<div class='DiffBlockPart %s'>\n" % @className
             str += @lines.collect{ |line| line.to_html }.join
-            str += "</div>\n"
+            # Don't put white-space after this so adjacent inline-block DiffBlockParts will not wrap.
+            str += "</div>"
         end
     end
 
--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -799,10 +799,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (diffState(file_diff) == diff_type)
       return;
 
+    $(file_diff).removeClass('sidebyside unified');
+    $(file_diff).addClass(diff_type);
+
     $(file_diff).attr('data-diffstate', diff_type);
     updateDiffLinkVisibility(file_diff);
 
-    $('.Line', file_diff).each(function() {
+    $('.shared .Line', file_diff).each(function() {
       convertLine(diff_type, this);
     });
 
