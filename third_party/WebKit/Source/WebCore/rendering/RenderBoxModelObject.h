e2331354b2f68f36c0be92b9df5f4ff6055a6c91
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 00:35:26 +0000

    2011-01-14  Tony Gentilcore  <tonyg@chromium.org>
            Unreviewed build fix.
    
            Fix Qt build after r75837
            https://bugs.webkit.org/show_bug.cgi?id=52494
    
            * rendering/InlineFlowBox.h:
            * rendering/RenderBoxModelObject.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RenderBoxModelObject_h
 
 #include "RenderObject.h"
+#include "ShadowData.h"
 
 namespace WebCore {
 
