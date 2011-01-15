0536782632cf6e4b590fe8a611edc7c1454d4d53
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 01:17:16 +0000

    2011-01-14  Tony Gentilcore  <tonyg@chromium.org>
            Unreviewed build fix.
    
            Fix Qt build after r75837
            https://bugs.webkit.org/show_bug.cgi?id=52494
    
            * rendering/style/StyleRareInheritedData.cpp:
            * rendering/style/StyleRareNonInheritedData.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKit/efl/ewk/ewk_frame.cpp
+++ third_party/WebKit/WebKit/efl/ewk/ewk_frame.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLPlugInElement.h"
 #include "HistoryItem.h"
 #include "HitTestResult.h"
+#include "IntSize.h"
 #include "KURL.h"
 #include "PlatformKeyboardEvent.h"
 #include "PlatformMouseEvent.h"
