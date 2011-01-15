6ec31c5fdcc5ba0a6bc6df56097e170372fd8ff7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 19:26:32 +0000

    2011-01-15  Joone Hur  <joone.hur@collabora.co.uk>
            Reviewed by Martin Robinson.
    
            [GTK] Linux build with FileSystem API enabled fails
            https://bugs.webkit.org/show_bug.cgi?id=43878
    
            This patch allows WebKitGtk+ to build with FileSystem API option.
    
            No new tests because no new functionality.
    
            * GNUmakefile.am: Included AsyncFileSystem.h,cpp and Excluded duplicated JSFileException.h,cpp.
            * bindings/js/JSDirectoryEntryCustom.cpp: Included ExceptionCode.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDirectoryEntryCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDirectoryEntryCustom.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSDirectoryEntry.h"
 
+#include "ExceptionCode.h"
 #include "JSDOMBinding.h"
 #include "JSEntryCallback.h"
 #include "JSErrorCallback.h"
