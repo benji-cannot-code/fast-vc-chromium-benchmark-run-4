a9a5cfc79a5f7fc49516c87521d25ed621c54cb5
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 00:44:33 +0000

    2011-01-14  Tony Gentilcore  <tonyg@chromium.org>
            Unreviewed build fix.
    
            Fix Qt build after r75837
            https://bugs.webkit.org/show_bug.cgi?id=52494
    
            * css/CSSStyleSelector.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScaleTransformOperation.h"
 #include "SelectionController.h"
 #include "Settings.h"
+#include "ShadowData.h"
 #include "ShadowValue.h"
 #include "SkewTransformOperation.h"
 #include "StyleCachedImage.h"
