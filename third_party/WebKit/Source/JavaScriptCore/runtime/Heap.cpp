a877786053ab1ef3cad91ace0e1c88d792a796dc
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-15 01:52:28 +0000

    Try to fix non-Dtrace builds: #include Tracing.h instead of TracingDtrace.h.
    * runtime/Heap.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Heap.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSGlobalObject.h"
 #include "JSLock.h"
 #include "JSONObject.h"
-#include "TracingDtrace.h"
+#include "Tracing.h"
 
 #define COLLECT_ON_EVERY_ALLOCATION 0
 
