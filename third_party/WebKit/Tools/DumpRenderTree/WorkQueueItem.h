dd28bfda936a38fe7986faa780bfa1935d92a87e
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-17 20:22:17 +0000

    REGRESSION: Assertion failure in FrameLoader::continueLoadAfterWillSubmitForm() when navigating back to an unreachable URL https://bugs.webkit.org/show_bug.cgi?id=52388
    Reviewed by Anders Carlsson.
    
    Source/WebCore: 
    
    Test: http/tests/navigation/go-back-to-error-page.html
    
    * history/PageCache.cpp:
    (WebCore::PageCache::canCachePageContainingThisFrame):
    Do not cache any error pages (which we can recognize as having substitute data and/or an
    unreachableURL).
    
    Tools: 
    
    Add a third parameter (unreachableURL) to queueLoadHTMLString.
    
    * DumpRenderTree/LayoutTestController.cpp:
    (queueLoadHTMLStringCallback):
    (LayoutTestController::queueLoadHTMLString):
    (LayoutTestController::queueLoadAlternateHTMLString):
    * DumpRenderTree/LayoutTestController.h:
    
    * DumpRenderTree/WorkQueueItem.h:
    (LoadHTMLStringItem::LoadHTMLStringItem):
    
    * DumpRenderTree/chromium/LayoutTestController.cpp:
    (WorkItemLoadHTMLString::WorkItemLoadHTMLString):
    (WorkItemLoadHTMLString::run):
    (LayoutTestController::queueLoadHTMLString):
    
    * DumpRenderTree/gtk/WorkQueueItemGtk.cpp:
    (LoadHTMLStringItem::invoke):
    * DumpRenderTree/mac/WorkQueueItemMac.mm:
    (LoadHTMLStringItem::invoke):
    * DumpRenderTree/win/WorkQueueItemWin.cpp:
    (LoadHTMLStringItem::invoke):
    
    LayoutTests: 
    
    Add a test that loads an alternate HTML String for an "unreachable" URL, loads another
    page, and then goes back to the "unreachable" URL, which is now reachable.
    
    This tests two things:
    1) No assertion failure when going back to an unreachable URL.
    2) There is no page cached for the unreachable URL.
    
    * http/tests/navigation/go-back-to-error-page-expected.txt: Added.
    * http/tests/navigation/go-back-to-error-page.html: Added.
    * http/tests/navigation/resources/page-to-go-back-from.html: Added.
    * http/tests/navigation/resources/page-treated-as-unreachable.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/WorkQueueItem.h
+++ third_party/WebKit/Tools/DumpRenderTree/WorkQueueItem.h
@@ -62,11 +62,19 @@ public:
     {
     }
 
+    LoadHTMLStringItem(const JSStringRef content, const JSStringRef baseURL, const JSStringRef unreachableURL)
+        : m_content(content)
+        , m_baseURL(baseURL)
+        , m_unreachableURL(unreachableURL)
+    {
+    }
+
 private:
     virtual bool invoke() const;
 
     JSRetainPtr<JSStringRef> m_content;
     JSRetainPtr<JSStringRef> m_baseURL;
+    JSRetainPtr<JSStringRef> m_unreachableURL;
 };
 
 class ReloadItem : public WorkQueueItem {
