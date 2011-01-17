96f147beeaa584a0588b023c085ccad4142f0fec
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-17 07:37:01 +0000

    2011-01-16  Adam Barth  <abarth@webkit.org>
            Update more include paths to reflect WebKit move.
    
            * DumpRenderTree/chromium/EventSender.cpp:
            (EventSender::keyDown):
            (EventSender::needsShiftModifier):
            * DumpRenderTree/chromium/WebThemeEngineDRTMac.h:
            * DumpRenderTree/chromium/WebThemeEngineDRTMac.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeEngineDRTMac.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeEngineDRTMac.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebThemeEngineDRTMac_h
 #define WebThemeEngineDRTMac_h
 
-#include "third_party/WebKit/WebKit/chromium/public/mac/WebThemeEngine.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/mac/WebThemeEngine.h"
 
 class WebThemeEngineDRTMac : public WebKit::WebThemeEngine {
 public:
--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeEngineDRTMac.mm
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeEngineDRTMac.mm
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebThemeEngineDRTMac.h"
 
-#include "third_party/WebKit/WebKit/chromium/public/WebCanvas.h"
-#include "third_party/WebKit/WebKit/chromium/public/WebRect.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebCanvas.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebRect.h"
 #import <AppKit/NSAffineTransform.h>
 #import <AppKit/NSGraphicsContext.h>
 #import <AppKit/NSScroller.h>
