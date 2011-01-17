918120967a567d04da1b4f8a421a14b70fd7d0ff
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-17 08:26:24 +0000

    2011-01-17  Philippe Normand  <pnormand@igalia.com>
            Unreviewed, rolling out r75914.
            http://trac.webkit.org/changeset/75914
            https://bugs.webkit.org/show_bug.cgi?id=49177
    
            multiple crashes on GTK
    
            * platform/gtk/Skipped:
    2011-01-17  Philippe Normand  <pnormand@igalia.com>
    
            Unreviewed, rolling out r75914.
            http://trac.webkit.org/changeset/75914
            https://bugs.webkit.org/show_bug.cgi?id=49177
    
            multiple crashes on GTK
    
            * platform/ScrollView.cpp:
            (WebCore::ScrollView::removeChild):
            (WebCore::ScrollView::wheelEvent):
            * platform/gtk/MainFrameScrollbarGtk.cpp:
            (MainFrameScrollbarGtk::attachAdjustment):
            (MainFrameScrollbarGtk::gtkValueChanged):
            * platform/gtk/ScrollViewGtk.cpp:
            (WebCore::ScrollView::platformInit):
            (WebCore::ScrollView::platformAddChild):
            (WebCore::ScrollView::platformRemoveChild):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
@@ -76,7 +76,6 @@ void ScrollView::addChild(PassRefPtr<Widget> prpChild)
         platformAddChild(child);
 }
 
-#if !PLATFORM(GTK)
 void ScrollView::removeChild(Widget* child)
 {
     ASSERT(child->parent() == this);
@@ -85,7 +84,6 @@ void ScrollView::removeChild(Widget* child)
     if (child->platformWidget())
         platformRemoveChild(child);
 }
-#endif
 
 void ScrollView::setHasHorizontalScrollbar(bool hasBar)
 {
@@ -1069,11 +1067,11 @@ void ScrollView::setScrollOrigin(const IntPoint& origin, bool updatePosition)
         updateScrollbars(scrollOffset());
 }
 
+#if !PLATFORM(WX) && !PLATFORM(GTK) && !PLATFORM(EFL)
+
 void ScrollView::platformInit()
 {
 }
-
-#if !PLATFORM(WX) && !PLATFORM(GTK) && !PLATFORM(EFL)
 
 void ScrollView::platformDestroy()
 {
@@ -1081,7 +1079,7 @@ void ScrollView::platformDestroy()
 
 #endif
 
-#if !PLATFORM(WX) && !PLATFORM(QT) && !PLATFORM(MAC)
+#if !PLATFORM(WX) && !PLATFORM(GTK) && !PLATFORM(QT) && !PLATFORM(MAC)
 
 void ScrollView::platformAddChild(Widget*)
 {
--- third_party/WebKit/Source/WebCore/platform/gtk/MainFrameScrollbarGtk.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/MainFrameScrollbarGtk.cpp
@@ -60,9 +60,6 @@ MainFrameScrollbarGtk::~MainFrameScrollbarGtk()
 
 void MainFrameScrollbarGtk::attachAdjustment(GtkAdjustment* adjustment)
 {
-    if (m_adjustment.get() == adjustment)
-        return;
-
     if (m_adjustment)
         detachAdjustment();
 
@@ -70,10 +67,9 @@ void MainFrameScrollbarGtk::attachAdjustment(GtkAdjustment* adjustment)
     if (!m_adjustment)
         return;
 
+    g_signal_connect(m_adjustment.get(), "value-changed", G_CALLBACK(MainFrameScrollbarGtk::gtkValueChanged), this);
     updateThumbProportion();
     updateThumbPosition();
-    g_signal_connect(m_adjustment.get(), "value-changed", G_CALLBACK(MainFrameScrollbarGtk::gtkValueChanged), this);
-
 }
 
 void MainFrameScrollbarGtk::detachAdjustment()
@@ -113,9 +109,7 @@ void MainFrameScrollbarGtk::updateThumbProportion()
 
 void MainFrameScrollbarGtk::gtkValueChanged(GtkAdjustment*, MainFrameScrollbarGtk* that)
 {
-    int newValue = static_cast<int>(gtk_adjustment_get_value(that->m_adjustment.get()));
-    if (newValue != that->value())
-        that->setValue(newValue, NotFromScrollAnimator);
+    that->setValue(static_cast<int>(gtk_adjustment_get_value(that->m_adjustment.get())), NotFromScrollAnimator);
 }
 
 void MainFrameScrollbarGtk::paint(GraphicsContext* context, const IntRect& rect)
