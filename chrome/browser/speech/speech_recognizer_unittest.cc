fd5333ae28fd6c5176a907a2cca8ed8398d72111
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-17 16:18:21 +0000

    Add the option of compressing speech input audio using FLAC. In the process, added a generic AudioEncoder interface which could create the requested codec. Right now the codec is set to FLAC. In a future CL, we'll determine the codec to use dynamically based on bandwidth considerations.
    This CL depends on http://codereview.chromium.org/6205006/ going in first.
    
    BUG=61677
    TEST=none
    
    Review URL: http://codereview.chromium.org/6111009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_recognizer_unittest.cc
+++ chrome/browser/speech/speech_recognizer_unittest.cc
@@ -39,10 +39,6 @@ class SpeechRecognizerTest : public SpeechRecognizerDelegate,
     audio_packet_.resize(audio_packet_length_bytes);
   }
 
-  void StartTest() {
-    EXPECT_TRUE(recognizer_->StartRecording());
-  }
-
   // SpeechRecognizer::Delegate methods.
   virtual void SetRecognitionResult(int caller_id,
                                     bool error,
