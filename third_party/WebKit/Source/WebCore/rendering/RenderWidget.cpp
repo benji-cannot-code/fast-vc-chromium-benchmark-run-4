b167a1c7311b6c01230ed2d81e3f5637800ea68a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-17 01:07:24 +0000

    2011-01-16  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dan Bernstein.
    
            Issues with iframes and plugins when the WebView is scaled.
            <rdar://problem/6213380>
    
            When _scaleWebView has been called on a WebView, iframes
            in WebKit1 render and hit-test incorrectly, and plug-ins don't scale up.
            This is caused by AppKit NSViews not playing nicely with the scale
            applied through style.
    
            Work around most of these issues by adjusting the bounds size
            of widgets to allow iframe contents to paint with the correct scale,
            and fix various places in the code where we relied on coordinate
            transforms via NSViews (which ignore CSS transforms).
    
            * WebCore.exp.in:
            * platform/Widget.cpp:
            (WebCore::Widget::setBoundsSize):
            * platform/Widget.h:
            * platform/mac/WidgetMac.mm:
            (WebCore::Widget::setBoundsSize):
            (WebCore::Widget::paint):
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::shouldPropagateCompositingToEnclosingIFrame):
            * rendering/RenderWidget.cpp:
            (WebCore::RenderWidget::setWidgetGeometry):
            (WebCore::RenderWidget::setWidget):
            (WebCore::RenderWidget::updateWidgetPosition):
            * rendering/RenderWidget.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderWidget.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderWidget.cpp
@@ -158,7 +158,7 @@ RenderWidget::~RenderWidget()
     clearWidget();
 }
 
-bool RenderWidget::setWidgetGeometry(const IntRect& frame)
+bool RenderWidget::setWidgetGeometry(const IntRect& frame, const IntSize& boundsSize)
 {
     if (!node())
         return false;
@@ -175,6 +175,7 @@ bool RenderWidget::setWidgetGeometry(const IntRect& frame)
     RenderWidgetProtector protector(this);
     RefPtr<Node> protectedNode(node());
     m_widget->setFrameRect(frame);
+    m_widget->setBoundsSize(boundsSize);
     
 #if USE(ACCELERATED_COMPOSITING)
     if (hasLayer() && layer()->isComposited())
@@ -202,7 +203,7 @@ void RenderWidget::setWidget(PassRefPtr<Widget> widget)
         // style pointer).
         if (style()) {
             if (!needsLayout())
-                setWidgetGeometry(absoluteContentBox());
+                setWidgetGeometry(localToAbsoluteQuad(FloatQuad(contentBoxRect())).enclosingBoundingBox(), contentBoxRect().size());
             if (style()->visibility() != VISIBLE)
                 m_widget->hide();
             else
@@ -342,14 +343,9 @@ void RenderWidget::updateWidgetPosition()
     if (!m_widget || !node()) // Check the node in case destroy() has been called.
         return;
 
-    // FIXME: This doesn't work correctly with transforms.
-    FloatPoint absPos = localToAbsolute();
-    absPos.move(borderLeft() + paddingLeft(), borderTop() + paddingTop());
-
-    int w = width() - borderAndPaddingWidth();
-    int h = height() - borderAndPaddingHeight();
-
-    bool boundsChanged = setWidgetGeometry(IntRect(absPos.x(), absPos.y(), w, h));
+    IntRect contentBox = contentBoxRect();
+    IntRect absoluteContentBox = localToAbsoluteQuad(FloatQuad(contentBoxRect())).enclosingBoundingBox();
+    bool boundsChanged = setWidgetGeometry(absoluteContentBox, contentBoxRect().size());
 
     // if the frame bounds got changed, or if view needs layout (possibly indicating
     // content size is wrong) we have to do a layout to set the right widget size
