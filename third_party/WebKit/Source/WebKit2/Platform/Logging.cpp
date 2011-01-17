9ab6579d0c8db122c24be4c7957a80248d0b0738
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-17 20:53:33 +0000

    Build fix.
            * Platform/Logging.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@75968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/Logging.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/Logging.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WTFLogChannel LogSessionState = { 0x00000001, "WebKit2LogLevel", WTFLogChannelOff };
 WTFLogChannel LogContextMenu  = { 0x00000002, "WebKit2LogLevel", WTFLogChannelOff };
 WTFLogChannel LogTextInput    = { 0x00000004, "WebKit2LogLevel", WTFLogChannelOff };
-WTFLogChannel LogTextView     = { 0x00000008, "WebKit2LogLevel", WTFLogChannelOff };
+WTFLogChannel LogView         = { 0x00000008, "WebKit2LogLevel", WTFLogChannelOff };
 
 static inline void initializeLogChannel(WTFLogChannel* channel)
 {
