199a5d6accedefd6e019521813e818611afab68a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-17 22:18:07 +0000

    Roll skia r690:r699.
    r692 fixes a valgrind error. r691 brings in optional gradient dithering code for 32bit pixels. Disable this in chrome for now.
    
    BUG=41756,69531,69452
    TEST=none
    TBR=reed
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71607 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -130,6 +130,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SK_SUPPORT_UNITTEST
 #endif
 
+/* If this is not defined, skia dithers gradients. Turning this on will make
+   gradients look better, but might have a performance impact. When it's turned
+   on, several webkit pixel tests will need to be rebaselined, too.
+   http://crbug.com/41756
+ */
+#define SK_DISABLE_DITHER_32BIT_GRADIENT
+
 // ===== Begin Chrome-specific definitions =====
 
 #define SK_SCALAR_IS_FLOAT
