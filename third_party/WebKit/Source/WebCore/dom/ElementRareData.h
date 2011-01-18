287418004ee9e452e8caa1dbdde460dd544953dc
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-18 07:12:43 +0000

    2011-01-17  MORITA Hajime  <morrita@google.com>
            Unreviewed, rolling out r75995.
            http://trac.webkit.org/changeset/75995
            https://bugs.webkit.org/show_bug.cgi?id=51914
    
            Causes assertion failures.
    
            * dom/Element.cpp:
            (WebCore::Element::shadowRoot):
            (WebCore::Element::setShadowRoot):
            (WebCore::Element::removeShadowRoot):
            * dom/ElementRareData.h:
            (WebCore::ElementRareData::ElementRareData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ElementRareData.h
+++ third_party/WebKit/Source/WebCore/dom/ElementRareData.h
@@ -42,7 +42,7 @@ public:
 
     IntSize m_minimumSizeForResizing;
     RefPtr<RenderStyle> m_computedStyle;
-    Node* m_shadowRoot;
+    RefPtr<Node> m_shadowRoot;
 
     OwnPtr<DatasetDOMStringMap> m_datasetDOMStringMap;
     OwnPtr<ClassList> m_classList;
@@ -55,7 +55,6 @@ inline IntSize defaultMinimumSizeForResizing()
 
 inline ElementRareData::ElementRareData()
     : m_minimumSizeForResizing(defaultMinimumSizeForResizing())
-    , m_shadowRoot(0)
 {
 }
 
