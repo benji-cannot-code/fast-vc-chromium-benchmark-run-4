ef21c018833efdb320f87c6c867d092bf0d7d0a1
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-18 22:22:21 +0000

    2011-01-18  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            Ask the web page proxy to scroll the view when needed
            https://bugs.webkit.org/show_bug.cgi?id=52665
    
            * UIProcess/DrawingAreaProxyImpl.cpp:
            (WebKit::DrawingAreaProxyImpl::incorporateUpdate):
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::scrollView):
            * UIProcess/WebPageProxy.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76065 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -476,6 +476,11 @@ void WebPageProxy::displayView()
     m_pageClient->displayView();
 }
 
+void WebPageProxy::scrollView(const IntRect& scrollRect, const IntSize& scrollOffset)
+{
+    m_pageClient->scrollView(scrollRect, scrollOffset);
+}
+
 void WebPageProxy::viewStateDidChange(ViewStateFlags flags)
 {
     if (!isValid())
--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
@@ -174,6 +174,7 @@ public:
 
     void setViewNeedsDisplay(const WebCore::IntRect&);
     void displayView();
+    void scrollView(const WebCore::IntRect& scrollRect, const WebCore::IntSize& scrollOffset);
 
     enum {
         ViewWindowIsActive = 1 << 0,
