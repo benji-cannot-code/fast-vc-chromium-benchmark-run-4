2d9df667d088049045ac52bb65f7d59dec214e45
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-19 18:18:00 +0000

    2011-01-19  Anders Carlsson  <andersca@apple.com>
            Reviewed by Darin Adler.
    
            Pass WebPageCreationParameters to DrawingArea::create
            https://bugs.webkit.org/show_bug.cgi?id=52726
    
            * WebProcess/WebPage/DrawingArea.cpp:
            (WebKit::DrawingArea::create):
            * WebProcess/WebPage/DrawingArea.h:
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            (WebKit::DrawingAreaImpl::create):
            (WebKit::DrawingAreaImpl::DrawingAreaImpl):
            * WebProcess/WebPage/DrawingAreaImpl.h:
            * WebProcess/WebPage/WebPage.cpp:
            (WebKit::WebPage::WebPage):
            (WebKit::WebPage::changeAcceleratedCompositingMode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ShareableBitmap.h"
 #include "UpdateInfo.h"
 #include "WebPage.h"
+#include "WebPageCreationParameters.h"
 #include "WebProcess.h"
 #include <WebCore/GraphicsContext.h>
 
@@ -41,17 +42,17 @@ using namespace WebCore;
 
 namespace WebKit {
 
-PassRefPtr<DrawingAreaImpl> DrawingAreaImpl::create(DrawingAreaInfo::Identifier identifier, WebPage* webPage)
+PassRefPtr<DrawingAreaImpl> DrawingAreaImpl::create(WebPage* webPage, const WebPageCreationParameters& parameters)
 {
-    return adoptRef(new DrawingAreaImpl(identifier, webPage));
+    return adoptRef(new DrawingAreaImpl(webPage, parameters));
 }
 
 DrawingAreaImpl::~DrawingAreaImpl()
 {
 }
 
-DrawingAreaImpl::DrawingAreaImpl(DrawingAreaInfo::Identifier identifier, WebPage* webPage)
-    : DrawingArea(DrawingAreaInfo::Impl, identifier, webPage)
+DrawingAreaImpl::DrawingAreaImpl(WebPage* webPage, const WebPageCreationParameters& parameters)
+    : DrawingArea(DrawingAreaInfo::Impl, parameters.drawingAreaInfo.identifier, webPage)
     , m_isWaitingForDidUpdate(false)
     , m_displayTimer(WebProcess::shared().runLoop(), this, &DrawingAreaImpl::display)
 {
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.h
@@ -37,11 +37,11 @@ struct UpdateInfo;
 
 class DrawingAreaImpl : public DrawingArea {
 public:
-    static PassRefPtr<DrawingAreaImpl> create(DrawingAreaInfo::Identifier, WebPage*);
+    static PassRefPtr<DrawingAreaImpl> create(WebPage*, const WebPageCreationParameters&);
     virtual ~DrawingAreaImpl();
 
 private:
-    DrawingAreaImpl(DrawingAreaInfo::Identifier, WebPage*);
+    DrawingAreaImpl(WebPage*, const WebPageCreationParameters&);
 
     // DrawingArea
     virtual void setNeedsDisplay(const WebCore::IntRect&);
