f62ef19fac5542f043e5f5e2df618f27bf7ac315
ace@chromium.org <ace@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-19 22:20:00 +0000

    Modifying some downloads hooks to act per-window (so that incognito windows can be correctly targetted) and updating some surrounding test cases.
    BUG=59259
    
    Review URL: http://codereview.chromium.org/6159001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71856 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/pyautolib.cc
+++ chrome/test/pyautolib/pyautolib.cc
@@ -75,6 +75,11 @@ void PyUITestBase::NavigateToURL(const char* url_string) {
   UITestBase::NavigateToURL(url);
 }
 
+void PyUITestBase::NavigateToURL(const char* url_string, int window_index) {
+  GURL url(url_string);
+  UITestBase::NavigateToURL(url, window_index);
+}
+
 void PyUITestBase::NavigateToURL(
     const char* url_string, int window_index, int tab_index) {
   GURL url(url_string);
