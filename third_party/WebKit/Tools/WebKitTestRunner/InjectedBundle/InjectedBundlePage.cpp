3b8bd2c9fed996c8a9f9d69c0cf269cdbf5d2cd9
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-19 07:12:44 +0000

    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp: Removed unncessary inculde of WKStringCF.h
    Rubber stamped by Csaba Osztrogonac
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit2/WKBundleFramePrivate.h>
 #include <WebKit2/WKBundlePagePrivate.h>
 
-#include <WebKit2/WKStringCF.h>
-
 using namespace std;
 
 namespace WTR {
