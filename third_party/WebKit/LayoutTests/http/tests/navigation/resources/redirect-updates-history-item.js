2f069a7169c2330e546411d024a735511468f5ae
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-20 05:49:51 +0000

    2011-01-19  Darin Fisher  <darin@chromium.org>
            Reviewed by Mihai Parparita.
    
            Re-initialize the HistoryItem fully when navigating to a HistoryItem,
            or replacing a HistoryItem, results in a different URL.
    
            https://bugs.webkit.org/show_bug.cgi?id=49654
    
            This patch also forces all HistoryItems to record the post-redirect
            URL.  Previously, frames would remember the pre-redirect URL.  This is
            problematic since other members of the HistoryItem correspond to the
            post-redirect URL.
    
            Tests: fast/history/history-replace-updates-current-item.html
                   http/tests/navigation/redirect-on-back-updates-history-item.html
                   http/tests/navigation/redirect-on-reload-updates-history-item.html
    
            * history/HistoryItem.cpp:
            (WebCore::HistoryItem::HistoryItem):
            (WebCore::HistoryItem::reset):
            * history/HistoryItem.h:
            * loader/HistoryController.cpp:
            (WebCore::HistoryController::updateForBackForwardNavigation):
            (WebCore::HistoryController::updateForReload):
            (WebCore::HistoryController::updateForStandardLoad):
            (WebCore::HistoryController::updateForRedirectWithLockedBackForwardList):
            (WebCore::HistoryController::initializeItem):
            (WebCore::HistoryController::createItem):
            (WebCore::HistoryController::createItemTree):
            (WebCore::HistoryController::updateCurrentItem):
            * loader/HistoryController.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/navigation/resources/redirect-updates-history-item.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function clearLocationCookie(url) {
+    document.cookie = 'location=;path=/;expires=' + (new Date()).toGMTString() + ';';
+}
+
+function setLocationCookie(url) {
+    clearLocationCookie(url);
+    document.cookie = 'location=' + url + ';path=/';
+}
+
+function log(s) {
+    var console = document.getElementById("console");
+    if (!console) {
+        console = document.createElement("pre");
+        document.body.appendChild(console);
+    }
+    console.innerText += s + "\n";
+}
+
+onpopstate = function() {
+    log("popstate: " + event.state);
+}
--- third_party/WebKit/Source/WebCore/loader/HistoryController.h
+++ third_party/WebKit/Source/WebCore/loader/HistoryController.h
@@ -85,7 +85,8 @@ public:
     void replaceState(PassRefPtr<SerializedScriptValue>, const String& title, const String& url);
 
 private:
-    PassRefPtr<HistoryItem> createItem(bool useOriginal);
+    void initializeItem(HistoryItem*);
+    PassRefPtr<HistoryItem> createItem();
     PassRefPtr<HistoryItem> createItemTree(Frame* targetFrame, bool clipAtTarget);
 
     void recursiveSetProvisionalItem(HistoryItem*, HistoryItem*, FrameLoadType);
@@ -94,6 +95,7 @@ private:
     bool itemsAreClones(HistoryItem*, HistoryItem*) const;
     bool currentFramesMatchItem(HistoryItem*) const;
     void updateBackForwardListClippedAtTarget(bool doClip);
+    void updateCurrentItem();
 
     Frame* m_frame;
 
