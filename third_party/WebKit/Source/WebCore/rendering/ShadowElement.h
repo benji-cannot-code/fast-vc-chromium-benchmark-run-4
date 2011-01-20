f92198ae0d33a84ee8486d0a6a7d2e4eb2a8b63f
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-20 12:08:07 +0000

    2011-01-19  MORITA Hajime  <morrita@google.com>
            Reviewed by Eric Seidel.
    
            ShadowElement::m_shadowParent should be removed
            https://bugs.webkit.org/show_bug.cgi?id=52558
    
            No new tests. No behavioral change.
    
            * rendering/ShadowElement.h:
            (WebCore::ShadowElement::ShadowElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/ShadowElement.h
+++ third_party/WebKit/Source/WebCore/rendering/ShadowElement.h
@@ -40,16 +40,12 @@ class ShadowElement : public BaseElement {
 protected:
     ShadowElement(const QualifiedName& name, HTMLElement* shadowParent)
         : BaseElement(name, shadowParent->document())
-        , m_shadowParent(shadowParent)
     {
         BaseElement::setShadowHost(shadowParent);
     }
 
 public:
     virtual void detach();
-
-private:
-    RefPtr<HTMLElement> m_shadowParent;
 };
 
 template<class BaseElement>
