0b0368af26d3f4644124c6faf90ee28464950740
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 19:33:07 +0000

    Password is not filed for matching usernames
    The |WebViewClient::didAcceptAutocompleteSuggestion| method is no longer dispatched from the WebKit side, and had been replaced by |WebViewClient::didAcceptAutoFillSuggestion|.  The PasswordAutocompleteManager had not been updated to account for this.  Those updates are made in this patch.  Follow up is to remove the |WebViewClient::didAcceptAutocompleteSuggestion| method from WebKit.  Also, the |PasswordAutocompleteManager::FrameClosing| method was not getting properly dispatched to perform cleanup tasks.  This is fixed now.
    
    BUG=68681
    TEST=PasswordAutocompleteManagerTest.SuggestionSelect
    
    Review URL: http://codereview.chromium.org/6262008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/password_autocomplete_manager.h
+++ chrome/renderer/password_autocomplete_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,30 +29,17 @@ class PasswordAutocompleteManager : public RenderViewObserver,
   explicit PasswordAutocompleteManager(RenderView* render_view);
   virtual ~PasswordAutocompleteManager();
 
-  // Invoked when the passed frame is closing.  Gives us a chance to clear any
-  // reference we may have to elements in that frame.
-  void FrameClosing(const WebKit::WebFrame* frame);
-
-  // Fills the password associated with |user_input|, using its current value
-  // as the actual user name.  Returns true if the password field was filled,
-  // false otherwise, typically if there was no matching suggestions for the
-  // currently typed username.
-  bool FillPassword(const WebKit::WebInputElement& user_input);
-
-  // Fills |login_input| and |password| with the most relevant suggestion from
-  // |fill_data| and shows a popup with other suggestions.
-  void PerformInlineAutocomplete(
-      const WebKit::WebInputElement& username,
-      const WebKit::WebInputElement& password,
-      const webkit_glue::PasswordFormFillData& fill_data);
-
   // WebViewClient editor related calls forwarded by the RenderView.
   // If they return true, it indicates the event was consumed and should not
   // be used for any other autofill activity.
   bool TextFieldDidEndEditing(const WebKit::WebInputElement& element);
   bool TextDidChangeInTextField(const WebKit::WebInputElement& element);
-  void TextFieldHandlingKeyDown(const WebKit::WebInputElement& element,
+  bool TextFieldHandlingKeyDown(const WebKit::WebInputElement& element,
                                 const WebKit::WebKeyboardEvent& event);
+  // Fills the password associated with user name |value|. Returns true if the
+  // username and password fields were filled, false otherwise.
+  bool DidSelectAutoFillSuggestion(const WebKit::WebNode& node,
+                                   const WebKit::WebString& value);
 
  private:
   friend class PasswordAutocompleteManagerTest;
@@ -65,16 +52,19 @@ class PasswordAutocompleteManager : public RenderViewObserver,
   };
   typedef std::map<WebKit::WebElement, PasswordInfo> LoginToPasswordInfoMap;
 
-  // RenderView::Observer implementation.
+  // RenderViewObserver:
   virtual bool OnMessageReceived(const IPC::Message& message);
   virtual void DidFinishDocumentLoad(WebKit::WebFrame* frame);
   virtual void DidFinishLoad(WebKit::WebFrame* frame);
+  virtual void FrameDetached(WebKit::WebFrame* frame);
+  virtual void FrameWillClose(WebKit::WebFrame* frame);
 
-  // PageClickListener implementation:
+  // PageClickListener:
   virtual bool InputElementClicked(const WebKit::WebInputElement& element,
                                    bool was_focused,
                                    bool is_focused);
 
+  // RenderView IPC handlers:
   void OnFillPasswordForm(
       const webkit_glue::PasswordFormFillData& form_data);
 
@@ -97,6 +87,17 @@ class PasswordAutocompleteManager : public RenderViewObserver,
       const webkit_glue::PasswordFormFillData& fill_data,
       bool exact_username_match,
       bool set_selection);
+
+  // Fills |login_input| and |password| with the most relevant suggestion from
+  // |fill_data| and shows a popup with other suggestions.
+  void PerformInlineAutocomplete(
+      const WebKit::WebInputElement& username,
+      const WebKit::WebInputElement& password,
+      const webkit_glue::PasswordFormFillData& fill_data);
+
+  // Invoked when the passed frame is closing.  Gives us a chance to clear any
+  // reference we may have to elements in that frame.
+  void FrameClosing(const WebKit::WebFrame* frame);
 
   // The logins we have filled so far with their associated info.
   LoginToPasswordInfoMap login_to_password_info_;
--- chrome/renderer/password_autocomplete_manager_unittest.cc
+++ chrome/renderer/password_autocomplete_manager_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -326,9 +326,11 @@ TEST_F(PasswordAutocompleteManagerTest, SuggestionSelect) {
 
   // To simulate a selection in the suggestion drop-down we just mimick what the
   // WebView does: it sets the element value then calls
-  // didAcceptAutocompleteSuggestion on the renderer.
-  username_element_.setValue(ASCIIToUTF16(kAliceUsername));
-  autofill_helper_->didAcceptAutocompleteSuggestion(username_element_);
+  // didSelectAutoFillSuggestion on the renderer.
+  autofill_helper_->didSelectAutoFillSuggestion(username_element_,
+                                                ASCIIToUTF16(kAliceUsername),
+                                                WebKit::WebString(),
+                                                0);
 
   // Autocomplete should have kicked in.
   CheckTextFieldsState(kAliceUsername, true, kAlicePassword, true);
