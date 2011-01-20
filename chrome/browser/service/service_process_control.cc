15dc58da060bc8c6a02dbb2c1aa14c7273cb7b4c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 23:32:39 +0000

    Update another include path
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72042 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/service/service_process_control.cc
+++ chrome/browser/service/service_process_control.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/service/service_process_control.h"
 
-#include "app/app_switches.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/process_util.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/notification_service.h"
 #include "chrome/common/service_messages.h"
 #include "chrome/common/service_process_util.h"
+#include "ui/base/ui_base_switches.h"
 
 // ServiceProcessControl::Launcher implementation.
 // This class is responsible for launching the service process on the
