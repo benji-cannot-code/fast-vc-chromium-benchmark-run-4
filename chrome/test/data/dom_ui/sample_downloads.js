8abedcf5f082fbfd2298109028a45fcdb1d9fbb1
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 18:41:16 +0000

    Initial sketch of DOM UI test framework. BUG=65911 TEST=manually run tests. Review URL: http://codereview.chromium.org/6231004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71972 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/dom_ui/sample_downloads.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Sample tests that exercise the test JS library and show how this framework
+// could be used to test the downloads page.
+// Call this method to run the tests.
+// The method takes an array of functions (your tests).
+runTests([
+  function testAssertFalse() {
+    assertFalse(false);
+  },
+
+  function testInitialFocus() {
+    assertTrue(document.activeElement.id == 'term', '');
+  }
+]);
