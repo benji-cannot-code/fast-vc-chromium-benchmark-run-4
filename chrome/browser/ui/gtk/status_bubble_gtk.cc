37b590d443e9ed6a54a7dcd633274c7c192b77b7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 20:02:27 +0000

    [gtk] un-break status bubble.
    broken in r71858.
    
    BUG=70244
    TEST=manual (in and out of --kiosk)
    
    Review URL: http://codereview.chromium.org/6343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71988 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/status_bubble_gtk.cc
+++ chrome/browser/ui/gtk/status_bubble_gtk.cc
@@ -116,7 +116,7 @@ void StatusBubbleGtk::Show() {
   // If we were going to hide, stop.
   hide_timer_.Stop();
 
-  gtk_widget_show_all(container_.get());
+  gtk_widget_show(container_.get());
   if (container_->window)
     gdk_window_raise(container_->window);
 }
@@ -126,7 +126,7 @@ void StatusBubbleGtk::Hide() {
   expand_timer_.Stop();
   expand_animation_.reset();
 
-  gtk_widget_hide_all(container_.get());
+  gtk_widget_hide(container_.get());
 }
 
 void StatusBubbleGtk::SetStatusTextTo(const std::string& status_utf8) {
@@ -254,6 +254,7 @@ void StatusBubbleGtk::InitWidgets() {
       kInternalLeftRightPadding + (ltr ? 0 : kCornerSize),
       kInternalLeftRightPadding + (ltr ? kCornerSize : 0));
   gtk_container_add(GTK_CONTAINER(padding_), label_);
+  gtk_widget_show_all(padding_);
 
   container_.Own(gtk_event_box_new());
   gtk_widget_set_no_show_all(container_.get(), TRUE);
