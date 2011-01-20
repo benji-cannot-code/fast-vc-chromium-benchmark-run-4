d455fe48f6cb096f348d4bbe8b3b77293d99db21
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 01:58:28 +0000

    DOMUI Prefs: Fix the initial visibility of Search
    Don't start with Search visible, since it can cause the code that shows specific sub pages to be confused when prefs are opened to a specific URL, and leave search as the root page.
    
    Also fixes an exception during initialization on non-ChromeOS machines.
    
    BUG=69613
    TEST=Opening prefs to a subpage by URL should show the right parent page, not Search.
    
    Review URL: http://codereview.chromium.org/6374005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71900 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/advanced_options.js
+++ chrome/browser/resources/options/advanced_options.js
@@ -262,7 +262,8 @@ var OptionsPage = options.OptionsPage;
 
   AdvancedOptions.RemoveRemotingSection = function() {
     var proxySectionElm = $('remoting-section');
-    proxySectionElm.parentNode.removeChild(proxySectionElm);
+    if (proxySectionElm)
+      proxySectionElm.parentNode.removeChild(proxySectionElm);
   };
 
   // Export
