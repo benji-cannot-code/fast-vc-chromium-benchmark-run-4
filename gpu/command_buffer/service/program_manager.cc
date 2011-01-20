10a84f6b6c458c754ff3ff0719b68cce819d02ef
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 00:17:34 +0000

    Fix for gl programs. An unsuccessful link should report the link is unsuccessful but not replace the old program.
    TEST=webgl conformance tests pass
    BUG=none
    
    Review URL: http://codereview.chromium.org/6242006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71882 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/program_manager.cc
+++ gpu/command_buffer/service/program_manager.cc
@@ -49,11 +49,13 @@ ProgramManager::ProgramInfo::ProgramInfo(GLuint service_id)
       max_attrib_name_length_(0),
       max_uniform_name_length_(0),
       service_id_(service_id),
-      valid_(false) {
+      valid_(false),
+      link_status_(false) {
 }
 
 void ProgramManager::ProgramInfo::Reset() {
   valid_ = false;
+  link_status_ = false;
   max_uniform_name_length_ = 0;
   max_attrib_name_length_ = 0;
   attrib_infos_.clear();
@@ -77,6 +79,7 @@ void ProgramManager::ProgramInfo::UpdateLogInfo() {
 
 void ProgramManager::ProgramInfo::Update() {
   Reset();
+  link_status_ = true;
   GLint num_attribs = 0;
   GLint max_len = 0;
   GLint max_location = -1;
@@ -338,7 +341,7 @@ void ProgramManager::ProgramInfo::GetProgramiv(GLenum pname, GLint* params) {
       *params = max_uniform_name_length_ + 1;
       break;
     case GL_LINK_STATUS:
-      *params = valid_;
+      *params = link_status_;
       break;
     case GL_INFO_LOG_LENGTH:
       // Notice +1 to accomodate NULL terminator.
