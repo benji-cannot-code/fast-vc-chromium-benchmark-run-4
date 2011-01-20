07205387704fd4251e101bc3795e8b040559c304
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-20 00:35:26 +0000

    2011-01-19  Oliver Hunt  <oliver@apple.com>
            Reviewed by Gavin Barraclough.
    
            Remove non-spec support for callable RegExp
            https://bugs.webkit.org/show_bug.cgi?id=28285
    
            Update test results for new behaviour.
    
            * fast/js/sputnik/Conformance/11_Expressions/11.4_Unary_Operators/11.4.3_The_typeof_Operator/S11.4.3_A3.6-expected.txt:
            * fast/js/sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.7/S15.10.7_A1_T1-expected.txt:
            * fast/js/sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.7/S15.10.7_A1_T2-expected.txt:
            * fast/js/sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.7/S15.10.7_A3_T1-expected.txt:
            * fast/js/sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.7/S15.10.7_A3_T2-expected.txt:
            * fast/loader/stateobjects/pushstate-object-types-expected.txt:
            * fast/regex/cross-frame-callable-expected.txt:
            * fast/regex/script-tests/cross-frame-callable.js:
            (doTest):
    2011-01-19  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Gavin Barraclough.
    
            Remove non-spec support for callable RegExp
            https://bugs.webkit.org/show_bug.cgi?id=28285
    
            Remove support for callable regexps.  If it breaks sites we can
            just roll this out.
    
            * runtime/RegExpObject.cpp:
            * runtime/RegExpObject.h:
            * tests/mozilla/expected.html: update results.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76180 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/regex/script-tests/cross-frame-callable.js
+++ third_party/WebKit/LayoutTests/fast/regex/script-tests/cross-frame-callable.js
@@ -2,7 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function doTest(childRegExp)
 {
     re = childRegExp;
-    shouldBe("re('a')", "['a']");
+    shouldThrow("re('a')");
+    shouldBe("re.exec('a')", "['a']");
 }
 
 var iframe = document.createElement('iframe');
