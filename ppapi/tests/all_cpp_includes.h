e18ff2b421b7c56b84d047c3374f4044b70e5c54
nfullagar@google.com <nfullagar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-20 01:35:50 +0000

    Remove dev/audio, dev/audio_config, and dev/audio_trusted Also, bump NaCl in deps to pull in new version of NaCl, which has now migrated to non-dev audio.
    Review URL: http://codereview.chromium.org/6281007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71897 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/all_cpp_includes.h
+++ ppapi/tests/all_cpp_includes.h
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PPAPI_TESTS_ALL_CPP_INCLUDES_H_
 #define PPAPI_TESTS_ALL_CPP_INCLUDES_H_
 
+#include "ppapi/cpp/audio.h"
+#include "ppapi/cpp/audio_config.h"
 #include "ppapi/cpp/common.h"
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/core.h"
-#include "ppapi/cpp/dev/audio_config_dev.h"
-#include "ppapi/cpp/dev/audio_dev.h"
 #include "ppapi/cpp/dev/buffer_dev.h"
 #include "ppapi/cpp/dev/context_3d_dev.h"
 #include "ppapi/cpp/dev/directory_entry_dev.h"
