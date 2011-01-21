78e6ee0c440bbd3f3cf823ce414ab0ee35c5a088
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 21:45:54 +0000

    Incorect IPC_MESSAGE_START definition in utility_messages_internal.h
    BUG=70421
    TEST=none
    
    Review URL: http://codereview.chromium.org/6354013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/utility_messages_internal.h
+++ chrome/common/utility_messages_internal.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/page_range.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
-#define IPC_MESSAGE_START NaClMsgStart
+#define IPC_MESSAGE_START UtilityMsgStart
 
 class FilePath;
 class IndexedDBKey;
