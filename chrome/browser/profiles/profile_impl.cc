188bb64bde1948acbbfb99f409c319245604de77
bbudge@google.com <bbudge@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 23:14:16 +0000

    This patch moves the experiments settings from the user profile to the browser's local state, since these settings correspond to command line switches. This is a release blocker for NaCl, so we decided to push this through.
    TEST=manual
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1322
    Review URL: http://codereview.chromium.org/6267009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_ptr.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
-#include "chrome/browser/about_flags.h"
 #include "chrome/browser/appcache/chrome_appcache_service.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autofill/personal_data_manager.h"
@@ -278,9 +277,6 @@ ProfileImpl::ProfileImpl(const FilePath& path)
   pref_change_registrar_.Add(prefs::kEnableSpellCheck, this);
   pref_change_registrar_.Add(prefs::kEnableAutoSpellCorrect, this);
   pref_change_registrar_.Add(prefs::kClearSiteDataOnExit, this);
-
-  // Convert active labs into switches. Modifies the current command line.
-  about_flags::ConvertFlagsToSwitches(prefs, CommandLine::ForCurrentProcess());
 
   // It would be nice to use PathService for fetching this directory, but
   // the cache directory depends on the profile directory, which isn't available
