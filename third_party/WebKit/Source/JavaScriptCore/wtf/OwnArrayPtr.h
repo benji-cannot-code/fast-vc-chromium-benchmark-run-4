4b15c9fc84044d9c4df55b86066f1137b1ce8be1
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-21 06:10:42 +0000

    2011-01-20  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Maciej Stachowiak.
    
            Added OwnPtrCommon.h because OwnArrayPtr::set calls deleteOwnedPtr.
    
            * wtf/OwnArrayPtr.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76334 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/OwnArrayPtr.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/OwnArrayPtr.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Assertions.h"
 #include "Noncopyable.h"
 #include "NullPtr.h"
+#include "OwnPtrCommon.h"
 #include "OwnArrayPtrCommon.h"
 #include <algorithm>
 
