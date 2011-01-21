987de4aa4572a5d6bdb52db5367ccb25c65592e0
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 21:32:57 +0000

    Restore two lines in hopes of greening perf bots
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72195 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/automation_messages_internal.h
+++ chrome/common/automation_messages_internal.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       any other purpose in these message types.
 
 // NOTE: All the new IPC messages should go at the end.
-//       The IPC message IDs need to match the reference builds.  Since we know
+//       The IPC message IDs need to match the reference builds.  Since we now
 //       define the IDs based on __LINE__, to allow these IPC messages to be
 //       used to control an old version of Chrome we need the message IDs to
 //       remain the same.  This means that you should not change the line number
@@ -765,6 +765,7 @@ IPC_SYNC_MESSAGE_CONTROL3_1(AutomationMsg_ClickInfoBarAccept,
                             int /* tab_handle */,
                             int /* info bar index */,
                             bool /* wait for navigation */,
+// This line blank on purpose, see comment atop file about __LINE__.
                             /* navigation result */
                             AutomationMsg_NavigationResponseValues)
 
@@ -779,6 +780,7 @@ IPC_SYNC_MESSAGE_CONTROL1_1(AutomationMsg_GetLastNavigationTime,
 IPC_SYNC_MESSAGE_CONTROL2_1(AutomationMsg_WaitForNavigation,
                             int /* tab_handle */,
                             int64 /* last navigation time */,
+// This line blank on purpose, see comment atop file about __LINE__.
                             /* navigation result */
                             AutomationMsg_NavigationResponseValues)
 
