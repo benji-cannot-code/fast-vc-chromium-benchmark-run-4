3a1edcca66832f5a2dc322e8316c4428065a4b64
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-21 19:33:41 +0000

    2011-01-21  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Sam Weinig.
    
            Report the sized used by canvases and types arrays
            https://bugs.webkit.org/show_bug.cgi?id=52856
    
            Report the memory cost associated with canvas elements,
            and typed arrays to JavaScript as extraCost, so that it
            can figure this into its GC behavior.
    
            * bindings/js/JSArrayBufferViewHelper.h:
            (WebCore::toJSArrayBufferView): New templatized function, similar
            to getDOMObjectWrapper() but calls reportExtraMemoryCost() with
            the byteLength of the array.
    
            * bindings/js/JSFloat32ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSInt16ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSInt32ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSInt8ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSUint16ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSUint32ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
            * bindings/js/JSUint8ArrayCustom.cpp:
            (WebCore::toJS): Use toJSArrayBufferView.
    
            * html/HTMLCanvasElement.cpp:
            (WebCore::HTMLCanvasElement::createImageBuffer): Call heap.reportExtraMemoryCost()
            with the size of the canvas backing store.
    
            * html/canvas/TypedArrayBase.h:
            (WebCore::TypedArrayBase::byteLength): byteLength needs to
            be public.
    
            * platform/graphics/ImageBuffer.h: Add dataSize() method.
    
            * platform/graphics/cairo/ImageBufferCairo.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/cg/ImageBufferCG.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/haiku/ImageBufferHaiku.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/qt/ImageBufferQt.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/skia/ImageBufferSkia.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/wince/ImageBufferWinCE.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
            * platform/graphics/wx/ImageBufferWx.cpp:
            (WebCore::ImageBuffer::dataSize): Implement dataSize().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/wx/ImageBufferWx.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/wx/ImageBufferWx.cpp
@@ -49,6 +49,12 @@ ImageBuffer::~ImageBuffer()
 {
 }
 
+size_t ImageBuffer::dataSize() const
+{
+    notImplemented();
+    return 0;
+}
+
 GraphicsContext* ImageBuffer::context() const
 {
     notImplemented();
