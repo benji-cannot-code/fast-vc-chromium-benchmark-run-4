e53453a0daeb9e6a01f4753fd676ffcfc213e765
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-21 23:41:07 +0000

    2011-01-21  Chris Rogers  <crogers@google.com>
            Reviewed by Kenneth Russell.
    
            fix audio build: header file should be "Noncopyable.h" and not "NonCopyable.h"
            https://bugs.webkit.org/show_bug.cgi?id=52933
    
            No new tests since this just fixes the build
    
            * webaudio/RealtimeAnalyser.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/webaudio/RealtimeAnalyser.h
+++ third_party/WebKit/Source/WebCore/webaudio/RealtimeAnalyser.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RealtimeAnalyser_h
 
 #include "AudioArray.h"
-#include <wtf/NonCopyable.h>
+#include <wtf/Noncopyable.h>
 #include <wtf/OwnPtr.h>
 
 namespace WebCore {
