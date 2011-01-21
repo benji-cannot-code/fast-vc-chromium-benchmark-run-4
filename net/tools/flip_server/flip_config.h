9184a688bbd8f11d626c4c37685d3669fd6913e7
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 00:00:13 +0000

    Memory leak fix, log file reopen, remote ip fix, connection idle timeout, pidfile.
    SPDY frames were leaking when used with DataFrame and OutputList.
    HUP signal will now reopen the log file.
    Remote IP obtained for 'forward-ip-header' was incorrect.
    Added connection idle timeout.
    Fixed up pidfile handling.
    Flip server now creates and locks a pid file.
    
    Patch from Kevin Lindsay: kelindsay@gmail.com
    
    BUG=none
    TEST=self
    
    Review URL: http://codereview.chromium.org/6332010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72046 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/flip_config.h
+++ net/tools/flip_server/flip_config.h
@@ -137,6 +137,7 @@ public:
    bool wait_for_iface_;
    int ssl_session_expiry_;
    bool ssl_disable_compression_;
+   int idle_timeout_s_;
 
    FlipConfig() :
        server_think_time_in_s_(0),
@@ -144,7 +145,8 @@ public:
        forward_ip_header_enabled_(false),
        wait_for_iface_(false),
        ssl_session_expiry_(300),
-       ssl_disable_compression_(false)
+       ssl_disable_compression_(false),
+       idle_timeout_s_(300)
        {}
 
    ~FlipConfig() {}
