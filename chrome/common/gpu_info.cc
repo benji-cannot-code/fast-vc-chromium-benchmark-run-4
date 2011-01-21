a88d01ff1959e66f15cf09490ca541c854e67daf
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 19:59:56 +0000

    Collect GL_EXTENSIONS string in GPUInfo and display it in about:gpu page.
    BUG=none
    TEST=GL_EXTENSIONS showing in about:gpu
    
    Review URL: http://codereview.chromium.org/6306008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72180 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gpu_info.cc
+++ chrome/common/gpu_info.cc
@@ -17,6 +17,7 @@ GPUInfo::GPUInfo()
       gl_version_string_(""),
       gl_vendor_(""),
       gl_renderer_(""),
+      gl_extensions_(""),
       can_lose_context_(false) {
 }
 
@@ -68,6 +69,10 @@ std::string GPUInfo::gl_renderer() const {
   return gl_renderer_;
 }
 
+std::string GPUInfo::gl_extensions() const {
+  return gl_extensions_;
+}
+
 bool GPUInfo::can_lose_context() const {
   return can_lose_context_;
 }
@@ -112,6 +117,10 @@ void GPUInfo::SetGLVendor(const std::string& gl_vendor) {
 
 void GPUInfo::SetGLRenderer(const std::string& gl_renderer) {
   gl_renderer_ = gl_renderer;
+}
+
+void GPUInfo::SetGLExtensions(const std::string& gl_extensions) {
+  gl_extensions_ = gl_extensions;
 }
 
 void GPUInfo::SetCanLoseContext(bool can_lose_context) {
