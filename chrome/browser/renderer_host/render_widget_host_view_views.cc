6cfce7d5e54cad5f79015f29e5759c9376f74274
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 18:24:50 +0000

    Make a hole only as big as is necessary.
    Compute the rectangle for the paint, and create a hole inside it, instead of
    making a giant all-consuming hole.
    
    BUG=70410
    TEST=manually, see bug.
    
    Review URL: http://codereview.chromium.org/6261019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_views.cc
+++ chrome/browser/renderer_host/render_widget_host_view_views.cc
@@ -283,6 +283,7 @@ void RenderWidgetHostViewViews::DidUpdateBackingStore(
     else
       SchedulePaint(rect, false);
   }
+  invalid_rect_ = invalid_rect_.Intersect(bounds());
 }
 
 void RenderWidgetHostViewViews::RenderViewGone(base::TerminationStatus status,
@@ -359,7 +360,8 @@ void RenderWidgetHostViewViews::Paint(gfx::Canvas* canvas) {
   // Paint a "hole" in the canvas so that the render of the web page is on
   // top of whatever else has already been painted in the views hierarchy.
   // Later views might still get to paint on top.
-  canvas->FillRectInt(SK_ColorBLACK, 0, 0, kMaxWindowWidth, kMaxWindowHeight,
+  canvas->FillRectInt(SK_ColorBLACK, 0, 0,
+                      bounds().width(), bounds().height(),
                       SkXfermode::kClear_Mode);
 
   // Don't do any painting if the GPU process is rendering directly
