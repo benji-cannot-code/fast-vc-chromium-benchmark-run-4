34426f5fd516a5f5f739f13449f2a8707c215e93
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 21:12:55 +0000

    PPAPI: Add context menu events.
    BUG=50984
    TEST=none
    
    Review URL: http://codereview.chromium.org/6257011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72189 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/event_conversion.cc
+++ webkit/plugins/ppapi/event_conversion.cc
@@ -37,6 +37,8 @@ PP_InputEvent_Type ConvertEventTypes(WebInputEvent::Type wetype) {
       return PP_INPUTEVENT_TYPE_MOUSEENTER;
     case WebInputEvent::MouseLeave:
       return PP_INPUTEVENT_TYPE_MOUSELEAVE;
+    case WebInputEvent::ContextMenu:
+      return PP_INPUTEVENT_TYPE_CONTEXTMENU;
     case WebInputEvent::MouseWheel:
       return PP_INPUTEVENT_TYPE_MOUSEWHEEL;
     case WebInputEvent::RawKeyDown:
@@ -214,6 +216,9 @@ WebMouseEvent* BuildMouseEvent(const PP_InputEvent& event) {
     case PP_INPUTEVENT_TYPE_MOUSELEAVE:
       mouse_event->type = WebInputEvent::MouseLeave;
       break;
+    case PP_INPUTEVENT_TYPE_CONTEXTMENU:
+      mouse_event->type = WebInputEvent::ContextMenu;
+      break;
     default:
       NOTREACHED();
   }
@@ -252,6 +257,7 @@ void CreatePPEvent(const WebInputEvent& event,
     case WebInputEvent::MouseMove:
     case WebInputEvent::MouseEnter:
     case WebInputEvent::MouseLeave:
+    case WebInputEvent::ContextMenu:
       AppendMouseEvent(event, pp_events);
       break;
     case WebInputEvent::MouseWheel:
@@ -281,6 +287,7 @@ WebInputEvent* CreateWebInputEvent(const PP_InputEvent& event) {
     case PP_INPUTEVENT_TYPE_MOUSEMOVE:
     case PP_INPUTEVENT_TYPE_MOUSEENTER:
     case PP_INPUTEVENT_TYPE_MOUSELEAVE:
+    case PP_INPUTEVENT_TYPE_CONTEXTMENU:
       web_input_event.reset(BuildMouseEvent(event));
       break;
     case PP_INPUTEVENT_TYPE_MOUSEWHEEL:
@@ -298,6 +305,6 @@ WebInputEvent* CreateWebInputEvent(const PP_InputEvent& event) {
 
   return web_input_event.release();
 }
+
 }  // namespace ppapi
 }  // namespace webkit
-
