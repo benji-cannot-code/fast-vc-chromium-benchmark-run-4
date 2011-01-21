ce196a290fcada2a45756153826eb1f631f572c0
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-21 21:02:51 +0000

    Fix range comparison. Get space to accept keyword work.
     I overlooked this placeholder code to fix.
    
    BUG=none
    TEST=enabled AcceptKeywordBySpaceTest for views-implementation
    
    Review URL: http://codereview.chromium.org/6266015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72185 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_browsertest.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_browsertest.cc
@@ -985,4 +985,8 @@ IN_PROC_BROWSER_TEST_F(AutocompleteEditViewViewsTest, BasicTextOperations) {
   BasicTextOperationsTest();
 }
 
+IN_PROC_BROWSER_TEST_F(AutocompleteEditViewViewsTest, AcceptKeywordBySpace) {
+  AcceptKeywordBySpaceTest();
+}
+
 #endif
