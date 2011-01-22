b73e5f5da87d4fd748c75ad9c11c4bd0554b810c
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-22 07:11:28 +0000

    Reorganized MarkedSpace, making many of its functions private.
    Reviewed by Beth Dakin.
    
    * runtime/JSCell.h:
    (JSC::JSCell::Heap::heap):
    * runtime/MarkedSpace.h:
    (JSC::MarkedSpace::globalData):
    (JSC::MarkedSpace::heap):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
@@ -364,7 +364,7 @@ namespace JSC {
 
     inline Heap* Heap::heap(JSCell* c)
     {
-        return MarkedSpace::cellBlock(c)->heap;
+        return MarkedSpace::heap(c);
     }
     
 #if ENABLE(JSC_ZOMBIES)
