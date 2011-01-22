de505b5ef5d27b15fd8ff1e5569a7ebd93b8af4b
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-22 01:07:55 +0000

    2011-01-21  Chris Rogers  <crogers@google.com>
            Unreviewed
    
            Add HAS_WEBAUDIO_FEATURE_ENABLE define to synchronize with chromium-side patch
            https://bugs.webkit.org/show_bug.cgi?id=52939
    
            * public/WebSettings.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebSettings.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebSettings.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebCommon.h"
 
+#define HAS_WEBAUDIO_FEATURE_ENABLE 1
+
 namespace WebKit {
 
 class WebString;
