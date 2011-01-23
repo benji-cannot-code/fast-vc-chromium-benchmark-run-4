9a8f2ef60f8fe08ec4cb0350af56f46904ab1b1a
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-23 04:46:32 +0000

    Remove ChromeURLRequestContext::is_media_
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/6342016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72291 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_url_request_context.cc
+++ chrome/browser/net/chrome_url_request_context.cc
@@ -451,7 +451,6 @@ class FactoryForMedia : public ChromeURLRequestContextFactory {
                 profile->GetRequestContext())),
         disk_cache_path_(disk_cache_path),
         cache_size_(cache_size) {
-    is_media_ = true;
     is_off_the_record_ = off_the_record;
   }
 
@@ -768,8 +767,7 @@ void ChromeURLRequestContextGetter::GetCookieStoreAsyncHelper(
 // ----------------------------------------------------------------------------
 
 ChromeURLRequestContext::ChromeURLRequestContext()
-    : is_media_(false),
-      is_off_the_record_(false) {
+    : is_off_the_record_(false) {
   CheckCurrentlyOnIOThread();
 }
 
@@ -844,8 +842,7 @@ void ChromeURLRequestContext::OnDefaultCharsetChange(
 // ChromeURLRequestContext on the IO thread (see
 // ApplyProfileParametersToContext() which reverses this).
 ChromeURLRequestContextFactory::ChromeURLRequestContextFactory(Profile* profile)
-    : is_media_(false),
-      is_off_the_record_(profile->IsOffTheRecord()),
+    : is_off_the_record_(profile->IsOffTheRecord()),
       io_thread_(g_browser_process->io_thread()) {
   CheckCurrentlyOnMainThread();
   PrefService* prefs = profile->GetPrefs();
@@ -898,7 +895,6 @@ void ChromeURLRequestContextFactory::ApplyProfileParametersToContext(
     ChromeURLRequestContext* context) {
   // Apply all the parameters. NOTE: keep this in sync with
   // ChromeURLRequestContextFactory(Profile*).
-  context->set_is_media(is_media_);
   context->set_is_off_the_record(is_off_the_record_);
   context->set_accept_language(accept_language_);
   context->set_accept_charset(accept_charset_);
--- chrome/browser/net/chrome_url_request_context.h
+++ chrome/browser/net/chrome_url_request_context.h
@@ -80,9 +80,6 @@ class ChromeURLRequestContext : public net::URLRequestContext {
   bool is_off_the_record() const {
     return is_off_the_record_;
   }
-  bool is_media() const {
-    return is_media_;
-  }
 
   virtual const std::string& GetUserAgent(const GURL& url) const;
 
@@ -142,9 +139,6 @@ class ChromeURLRequestContext : public net::URLRequestContext {
   void set_is_off_the_record(bool is_off_the_record) {
     is_off_the_record_ = is_off_the_record;
   }
-  void set_is_media(bool is_media) {
-    is_media_ = is_media;
-  }
   void set_host_content_settings_map(
       HostContentSettingsMap* host_content_settings_map) {
     host_content_settings_map_ = host_content_settings_map;
@@ -196,7 +190,6 @@ class ChromeURLRequestContext : public net::URLRequestContext {
   scoped_refptr<ExtensionInfoMap> extension_info_map_;
   scoped_refptr<PrerenderManager> prerender_manager_;
 
-  bool is_media_;
   bool is_off_the_record_;
 
  private:
