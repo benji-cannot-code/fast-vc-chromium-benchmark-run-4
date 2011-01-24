2400d6d414637abdbf13c448c1b30e2e244155bb
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-24 23:16:26 +0000

    2011-01-24  Anders Carlsson  <andersca@apple.com>
            Reviewed by John Sullivan.
    
            Don't use the timeout checker for non-user-interaction messages
            https://bugs.webkit.org/show_bug.cgi?id=53046
    
            * UIProcess/ChunkedUpdateDrawingAreaProxy.cpp:
            (WebKit::ChunkedUpdateDrawingAreaProxy::didSetSize):
            (WebKit::ChunkedUpdateDrawingAreaProxy::sendSetSize):
            * UIProcess/LayerBackedDrawingAreaProxy.cpp:
            (WebKit::LayerBackedDrawingAreaProxy::sizeDidChange):
            (WebKit::LayerBackedDrawingAreaProxy::didSetSize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/ChunkedUpdateDrawingAreaProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/ChunkedUpdateDrawingAreaProxy.cpp
@@ -114,9 +114,6 @@ void ChunkedUpdateDrawingAreaProxy::didSetSize(UpdateChunk* updateChunk)
     invalidateBackingStore();
     if (!updateChunk->isEmpty())
         drawUpdateChunkIntoBackingStore(updateChunk);
-
-    WebPageProxy* page = this->page();
-    page->process()->responsivenessTimer()->stop();
 }
 
 void ChunkedUpdateDrawingAreaProxy::update(UpdateChunk* updateChunk)
@@ -144,7 +141,6 @@ void ChunkedUpdateDrawingAreaProxy::sendSetSize()
         return;
     m_isWaitingForDidSetFrameNotification = true;
     
-    m_webPageProxy->process()->responsivenessTimer()->start();
     m_webPageProxy->process()->send(DrawingAreaLegacyMessage::SetSize, m_webPageProxy->pageID(), CoreIPC::In(info().identifier, m_size));
 }
     
--- third_party/WebKit/Source/WebKit2/UIProcess/LayerBackedDrawingAreaProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/LayerBackedDrawingAreaProxy.cpp
@@ -81,7 +81,6 @@ void LayerBackedDrawingAreaProxy::sizeDidChange()
 
     m_isWaitingForDidSetFrameNotification = true;
 
-    page->process()->responsivenessTimer()->start();
     page->process()->send(DrawingAreaLegacyMessage::SetSize, page->pageID(), CoreIPC::In(info().identifier, m_size));
 }
 
@@ -120,9 +119,6 @@ void LayerBackedDrawingAreaProxy::didSetSize(const IntSize& size)
 
     if (size != m_lastSetViewSize)
         setSize(m_lastSetViewSize);
-
-    WebPageProxy* page = this->page();
-    page->process()->responsivenessTimer()->stop();
 }
 
 void LayerBackedDrawingAreaProxy::update()
