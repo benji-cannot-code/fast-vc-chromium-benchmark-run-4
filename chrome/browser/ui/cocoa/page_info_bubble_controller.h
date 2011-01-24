66a7d81d3eaa000058d3be0d7242416914d1d098
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-24 19:55:06 +0000

    Objective-C readability review for Robert Sesek <rsesek@google.com>.
    OCL=http://codereview.chromium.org/3461016/
    BUG=http://b/3372125
    TEST=none
    R=pinkerton
    
    Review URL: http://codereview.chromium.org/6360007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72373 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/page_info_bubble_controller.h
+++ chrome/browser/ui/cocoa/page_info_bubble_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,11 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   int certID_;
 }
 
-@property (nonatomic, assign) int certID;
+@property(nonatomic, assign) int certID;
 
 // Designated initializer. The new instance will take ownership of |model| and
 // |bridge|. There should be a 1:1 mapping of models to bridges. The
-// controller will release itself when the bubble is closed.
+// controller will release itself when the bubble is closed. |parentWindow|
+// cannot be nil.
 - (id)initWithPageInfoModel:(PageInfoModel*)model
               modelObserver:(PageInfoModel::PageInfoModelObserver*)bridge
                parentWindow:(NSWindow*)parentWindow;
