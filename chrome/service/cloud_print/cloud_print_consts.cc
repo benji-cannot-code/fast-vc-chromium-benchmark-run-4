8af709e2cf9cdd8668d79af5d65ad0373dd3b6f4
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-24 23:47:41 +0000

    Added a diagnostic user message when enumerating printers fails. Also tweaked some of the strings. BUG=None TEST=Test Cloud Print Proxy.
    Review URL: http://codereview.chromium.org/6356007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/cloud_print/cloud_print_consts.cc
+++ chrome/service/cloud_print/cloud_print_consts.cc
@@ -58,4 +58,4 @@ const char kJobFetchReasonQueryMore[] = "querymore";
 // Short message ids for diagnostic messages sent to the server.
 const char kPrintSystemFailedMessageId[] = "printsystemfail";
 const char kGetPrinterCapsFailedMessageId[] = "getprncapsfail";
-
+const char kEnumPrintersFailedMessageId[] = "enumfail";
