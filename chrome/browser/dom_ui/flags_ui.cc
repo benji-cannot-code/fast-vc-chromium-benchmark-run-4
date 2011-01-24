623b9df24d46836c658d15bc70748ecf3b7cc201
bbudge@google.com <bbudge@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-24 22:28:39 +0000

    This patch moves the experiments settings from the user profile to the browser's local state, since these settings correspond to command line switches. It restores 6267009 http://codereview.chromium.org/6267009/ which got reverted because it broke ChromeOS tests.  The patch also removes some migration code for ChromeOS experiments. Users' settings should be migrated by now. This is a release blocker for NaCl, so we are pushing this through. TEST=manual BUG= http://code.google.com/p/nativeclient/issues/detail?id=1322 Review URL: http://codereview.chromium.org/6389002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/flags_ui.cc
+++ chrome/browser/dom_ui/flags_ui.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_thread.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/prefs/pref_service.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -137,7 +136,7 @@ void FlagsDOMHandler::HandleRequestFlagsExperiments(const ListValue* args) {
   DictionaryValue results;
   results.Set("flagsExperiments",
               about_flags::GetFlagsExperimentsData(
-                  dom_ui_->GetProfile()->GetPrefs()));
+                  g_browser_process->local_state()));
   results.SetBoolean("needsRestart",
                      about_flags::IsRestartNeededToCommitChanges());
   dom_ui_->CallJavascriptFunction(L"returnFlagsExperiments", results);
@@ -156,7 +155,7 @@ void FlagsDOMHandler::HandleEnableFlagsExperimentMessage(
     return;
 
   about_flags::SetExperimentEnabled(
-      dom_ui_->GetProfile()->GetPrefs(),
+      g_browser_process->local_state(),
       experiment_internal_name,
       enable_str == "true");
 }
@@ -196,6 +195,6 @@ RefCountedMemory* FlagsUI::GetFaviconResourceBytes() {
 }
 
 // static
-void FlagsUI::RegisterUserPrefs(PrefService* prefs) {
+void FlagsUI::RegisterPrefs(PrefService* prefs) {
   prefs->RegisterListPref(prefs::kEnabledLabsExperiments);
 }
