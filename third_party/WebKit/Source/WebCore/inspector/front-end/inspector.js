7c3b6bab2f3b7b3f5858aa6f2326b437f5df5917
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-24 12:46:05 +0000

    2011-01-24  Peter Beverloo  <peter@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: recognize application/x-font-woff as a valid mime-type.
            https://bugs.webkit.org/show_bug.cgi?id=52977
    
            No new tests.
    
            * inspector/front-end/inspector.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
@@ -1874,6 +1874,7 @@ WebInspector.MIMETypes = {
     "font/opentype":               {3: true},
     "application/x-font-type1":    {3: true},
     "application/x-font-ttf":      {3: true},
+    "application/x-font-woff":     {3: true},
     "application/x-truetype-font": {3: true},
     "text/javascript":             {4: true},
     "text/ecmascript":             {4: true},
