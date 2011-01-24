4b5aa4002148a4062286b75582c27e115943a5a2
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-24 20:35:17 +0000

    * Set the color of all state of signout button white * Tab/alt-tab should cycle over to first/last focusable item.   This wasn't working because views framework cannot handle   views under TYPE_CHILD correctly. This is a hack to workaround this   issue until DOMUI  login is ready. * Fix callback names that were changed/removed in refactoring.
    BUG=chromium-os:9889, chromium-os:9845, chromium-os:10655
    TEST=manual.
    
    Review URL: http://codereview.chromium.org/6329016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72379 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/view_ids.h
+++ chrome/browser/chromeos/view_ids.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,8 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // View ID used in ChromeOS.
 enum ChromeOSViewIds {
   // Start with the offset that is big enough to avoid possible
-  // collison.
+  // collision.
   VIEW_ID_STATUS_AREA = VIEW_ID_PREDEFINED_COUNT + 10000,
+  VIEW_ID_SCREEN_LOCKER_SIGNOUT_LINK,
+  VIEW_ID_SCREEN_LOCKER_SHUTDOWN,
 };
 
 #endif  // CHROME_BROWSER_CHROMEOS_VIEW_IDS_H_
