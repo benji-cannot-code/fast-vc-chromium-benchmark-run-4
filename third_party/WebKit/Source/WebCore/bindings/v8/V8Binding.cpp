a09b9f54ca2ec06fd373568fac3d41cc258db977
andreip@google.com <andreip@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-24 19:01:45 +0000

    2011-01-24  Andrei Popescu  <andreip@google.com>
            Reviewed by Nate Chapin.
    
            IndexedDatabase methods should not take arguments of type OptionsObject
            https://bugs.webkit.org/show_bug.cgi?id=53012
    
            * storage/indexeddb/create-object-store-options-expected.txt:
            * storage/indexeddb/create-object-store-options.html:
            * storage/indexeddb/cursor-delete-expected.txt:
            * storage/indexeddb/cursor-delete.html:
            * storage/indexeddb/cursor-index-delete-expected.txt:
            * storage/indexeddb/cursor-index-delete.html:
            * storage/indexeddb/data-corruption-expected.txt:
            * storage/indexeddb/data-corruption.html:
            * storage/indexeddb/database-quota-expected.txt:
            * storage/indexeddb/database-quota.html:
            * storage/indexeddb/index-cursor.html:
            * storage/indexeddb/objectstore-basics-expected.txt:
            * storage/indexeddb/objectstore-basics.html:
            * storage/indexeddb/objectstore-cursor.html:
            * storage/indexeddb/open-cursor-expected.txt:
            * storage/indexeddb/open-cursor.html:
            * storage/indexeddb/transaction-and-objectstore-calls-expected.txt:
            * storage/indexeddb/transaction-and-objectstore-calls.html:
            * storage/indexeddb/tutorial.html:
    2011-01-24  Andrei Popescu  <andreip@google.com>
    
            Reviewed by Nate Chapin.
    
            IndexedDatabase methods should not take arguments of type OptionsObject
            https://bugs.webkit.org/show_bug.cgi?id=53012
    
            This patch reverts all IDB methods, except IDBDatabase::createObjectStore and
            IDBObjectStore::createIndex, to using a plain list of arguments instead of
            grouping the various parameters inside a single OptionsObject argument.
            This decision was made on public-webapps@w3.org mailing list.
    
            We also add support (v8 only for now) for passing DOMStringList objects as arguments to native
            methods. The code for obtaining a DOMStringList object from a JS array of strings existed already
            in OptionsObject.cpp, I just copied it to V8Bindings.cpp and taught the v8 code generator how to
            use it.
    
            * bindings/scripts/CodeGeneratorV8.pm:
            * bindings/v8/V8Binding.cpp:
            (WebCore::v8ValueToWebCoreDOMStringList):
            * bindings/v8/V8Binding.h:
            * storage/IDBDatabase.cpp:
            (WebCore::IDBDatabase::transaction):
            * storage/IDBDatabase.h:
            (WebCore::IDBDatabase::transaction):
            * storage/IDBDatabase.idl:
            * storage/IDBIndex.cpp:
            (WebCore::IDBIndex::openCursor):
            (WebCore::IDBIndex::openKeyCursor):
            * storage/IDBIndex.h:
            (WebCore::IDBIndex::openCursor):
            (WebCore::IDBIndex::openKeyCursor):
            * storage/IDBIndex.idl:
            * storage/IDBObjectStore.cpp:
            (WebCore::IDBObjectStore::openCursor):
            * storage/IDBObjectStore.h:
            (WebCore::IDBObjectStore::openCursor):
            * storage/IDBObjectStore.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8Binding.h"
 
+#include "DOMStringList.h"
 #include "Element.h"
 #include "MathExtras.h"
 #include "PlatformString.h"
@@ -577,6 +578,21 @@ void setElementStringAttr(const v8::AccessorInfo& info,
     Element* imp = V8Element::toNative(info.Holder());
     AtomicString v = toAtomicWebCoreStringWithNullCheck(value);
     imp->setAttribute(name, v);
+}
+
+PassRefPtr<DOMStringList> v8ValueToWebCoreDOMStringList(v8::Handle<v8::Value> value)
+{
+    v8::Local<v8::Value> v8Value(v8::Local<v8::Value>::New(value));
+    if (!v8Value->IsArray())
+        return 0;
+
+    RefPtr<DOMStringList> ret = DOMStringList::create();
+    v8::Local<v8::Array> v8Array = v8::Local<v8::Array>::Cast(v8Value);
+    for (size_t i = 0; i < v8Array->Length(); ++i) {
+        v8::Local<v8::Value> indexedValue = v8Array->Get(v8::Integer::New(i));
+        ret->append(v8ValueToWebCoreString(indexedValue));
+    }
+    return ret.release();
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+    class DOMStringList;
     class EventListener;
     class EventTarget;
 
@@ -206,6 +207,8 @@ namespace WebCore {
     v8::Persistent<v8::FunctionTemplate> getToStringTemplate();
 
     String int32ToWebCoreString(int value);
+
+    PassRefPtr<DOMStringList> v8ValueToWebCoreDOMStringList(v8::Handle<v8::Value>);
 
     class V8ParameterBase {
     public:
