47b6d92e864ec79274cd1ce045b1d5085bcd8aff
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 21:02:13 +0000

    Refactor win printing workflow.
    As a preparation for print preview workflow, refactored PrintPage() function.
    
    Created the following functions:
    1. RenderPage(): Renders the page for printing. Handles Alpha blend transparency.
    2. CopyMetafileDataToSharedMem(): Copy the metafile data to shared memory.
    
    BUG=none
    TEST=printing works after code changes.
    
    Review URL: http://codereview.chromium.org/6321017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.h
+++ chrome/renderer/print_web_view_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrameClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebViewClient.h"
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_WIN)
 #include "base/shared_memory.h"
-#endif  // defined(OS_MACOSX)
+#endif  // defined(OS_MACOSX) || defined(OS_WIN)
 
 namespace gfx {
 class Size;
@@ -182,6 +182,14 @@ class PrintWebViewHelper : public WebKit::WebViewClient,
   void RenderPage(const gfx::Size& page_size, const gfx::Point& content_origin,
                   const float& scale_factor, int page_number,
                   WebKit::WebFrame* frame, printing::NativeMetafile* metafile);
+#elif defined(OS_WIN)
+  void RenderPage(const gfx::Size& page_size, float* scale_factor,
+                  int page_number, WebKit::WebFrame* frame,
+                  scoped_ptr<printing::NativeMetafile>* metafile,
+                  bool supports_alpha_blend);
+#endif
+
+#if defined(OS_MACOSX) || defined(OS_WIN)
   bool CopyMetafileDataToSharedMem(printing::NativeMetafile* metafile,
       base::SharedMemoryHandle* shared_mem_handle);
 #endif
