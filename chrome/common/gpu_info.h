b85467f1b2abb5ce859eb45d749dcf3b715ce395
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 01:29:15 +0000

    Collect GL_EXTENSIONS string in GPUInfo and display it in about:gpu page. (A second try after being reverted.)
    Review URL: http://codereview.chromium.org/6279009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gpu_info.h
+++ chrome/common/gpu_info.h
@@ -80,6 +80,10 @@ class GPUInfo {
   // Return "" if we are not using OpenGL.
   std::string gl_renderer() const;
 
+  // Return the GL_EXTENSIONS string.
+  // Return "" if we are not using OpenGL.
+  std::string gl_extensions() const;
+
   // Return the device semantics, i.e. whether the Vista and Windows 7 specific
   // semantics are available.
   bool can_lose_context() const;
@@ -104,6 +108,8 @@ class GPUInfo {
 
   void SetGLRenderer(const std::string& gl_renderer);
 
+  void SetGLExtensions(const std::string& gl_extensions);
+
   void SetCanLoseContext(bool can_lose_context);
 
 #if defined(OS_WIN)
@@ -126,6 +132,7 @@ class GPUInfo {
   std::string gl_version_string_;
   std::string gl_vendor_;
   std::string gl_renderer_;
+  std::string gl_extensions_;
   bool can_lose_context_;
 
 #if defined(OS_WIN)
