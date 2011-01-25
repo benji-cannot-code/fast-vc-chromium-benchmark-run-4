d31bff03ae227515c012f78faebee8fb3e5dbfcc
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 23:52:02 +0000

    Disk cache: Prevent obscure file corruption and deal with the result of that corruption.
    1. Now we mark any open entry as dirty, even if we are
    supposed to delete the entry right away because if we
    crash before that, we may end up clearing the dirty
    flag of a dirty entry.
    
    2. When we look for a parent of a given entry we now
    double check that the entry is the one that we want (and
    not just another entry with the same key).
    
    3. If we have a loop on the hash collision list (result of
    failing to do 1 and 2 above), we figure that out.
    
    4. Now every time we open an entry from an LRU list we end
    up using the same code path (with the proper handling of
    dirty entries).
    
    BUG=69135
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/6292011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72563 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/dump_cache/dump_files.cc
+++ net/tools/dump_cache/dump_files.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // to the actual files (they still may change if an error is detected on the
 // files).
 
+#include <set>
 #include <stdio.h>
 #include <string>
 
@@ -68,6 +69,7 @@ void DumpIndexHeader(const std::wstring& name) {
   for (int i = 0; i < 5; i++) {
     printf("head %d: 0x%x\n", i, header.lru.heads[i]);
     printf("tail %d: 0x%x\n", i, header.lru.tails[i]);
+    printf("size %d: 0x%x\n", i, header.lru.sizes[i]);
   }
   printf("transaction: 0x%x\n", header.lru.transaction);
   printf("operation: %d\n", header.lru.operation);
@@ -132,6 +134,7 @@ class CacheDumper {
   disk_cache::Index* index_;
   int current_hash_;
   disk_cache::CacheAddr next_addr_;
+  std::set<disk_cache::CacheAddr> dumped_entries_;
   DISALLOW_COPY_AND_ASSIGN(CacheDumper);
 };
 
@@ -155,17 +158,19 @@ bool CacheDumper::Init() {
 }
 
 bool CacheDumper::GetEntry(disk_cache::EntryStore* entry) {
+  if (dumped_entries_.find(next_addr_) != dumped_entries_.end()) {
+    printf("Loop detected\n");
+    next_addr_ = 0;
+    current_hash_++;
+  }
+
   if (next_addr_) {
-    if (LoadEntry(next_addr_, entry)) {
-      next_addr_ = entry->next;
-      if (!next_addr_)
-        current_hash_++;
+    if (LoadEntry(next_addr_, entry))
       return true;
-    } else {
-      printf("Unable to load entry at address 0x%x\n", next_addr_);
-      next_addr_ = 0;
-      current_hash_++;
-    }
+
+    printf("Unable to load entry at address 0x%x\n", next_addr_);
+    next_addr_ = 0;
+    current_hash_++;
   }
 
   for (int i = current_hash_; i < index_->header.table_len; i++) {
@@ -173,14 +178,10 @@ bool CacheDumper::GetEntry(disk_cache::EntryStore* entry) {
     // dumping every entry that we can find.
     if (index_->table[i]) {
       current_hash_ = i;
-      if (LoadEntry(index_->table[i], entry)) {
-        next_addr_ = entry->next;
-        if (!next_addr_)
-          current_hash_++;
+      if (LoadEntry(index_->table[i], entry))
         return true;
-      } else {
-        printf("Unable to load entry at address 0x%x\n", index_->table[i]);
-      }
+
+      printf("Unable to load entry at address 0x%x\n", index_->table[i]);
     }
   }
   return false;
@@ -199,6 +200,15 @@ bool CacheDumper::LoadEntry(disk_cache::CacheAddr addr,
 
   memcpy(entry, entry_block.Data(), sizeof(*entry));
   printf("Entry at 0x%x\n", addr);
+
+  // Prepare for the next entry to load.
+  next_addr_ = entry->next;
+  if (next_addr_) {
+    dumped_entries_.insert(addr);
+  } else {
+    current_hash_++;
+    dumped_entries_.clear();
+  }
   return true;
 }
 
