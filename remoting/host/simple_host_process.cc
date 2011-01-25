bd43107f50810c7b316ba57259e680664ecdeb85
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 21:06:42 +0000

    Fix crashes in ChromotingHost
    Capturer should be owned by ScreenRecoder. This patch also addes test to test
    reconnection of ChromotingHost.
    
    BUG=69969
    TEST=ChromotingHostTest.*
    
    Review URL: http://codereview.chromium.org/6266010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72543 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/simple_host_process.cc
+++ remoting/host/simple_host_process.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/capturer_fake.h"
 #include "remoting/host/chromoting_host.h"
 #include "remoting/host/chromoting_host_context.h"
+#include "remoting/host/event_executor.h"
 #include "remoting/host/json_host_config.h"
 #include "remoting/proto/video.pb.h"
 
@@ -117,9 +118,12 @@ int main(int argc, char** argv) {
 
   bool fake = cmd_line->HasSwitch(kFakeSwitchName);
   if (fake) {
+    remoting::Capturer* capturer =
+        new remoting::CapturerFake(context.main_message_loop());
+    remoting::protocol::InputStub* input_stub =
+        CreateEventExecutor(context.main_message_loop(), capturer);
     host = ChromotingHost::Create(
-        &context, config,
-        new remoting::CapturerFake(context.main_message_loop()));
+        &context, config, capturer, input_stub);
   } else {
     host = ChromotingHost::Create(&context, config);
   }
