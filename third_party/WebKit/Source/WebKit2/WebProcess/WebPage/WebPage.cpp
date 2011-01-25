e1a53b14a22b94c2558b894ba6b61229390c381d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-25 21:11:32 +0000

    2011-01-25  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Don't inform the UI process about compositing changes when using the new drawing area
            https://bugs.webkit.org/show_bug.cgi?id=53120
    
            * WebProcess/WebPage/WebPage.cpp:
            (WebKit::WebPage::changeAcceleratedCompositingMode):
            Return early if we're using the new drawing area.
    
            (WebKit::WebPage::exitAcceleratedCompositingMode):
            Call setRootCompositingLayer(0) so the drawing area knows when we leave accelerated compositing.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -314,6 +314,11 @@ void WebPage::changeAcceleratedCompositingMode(WebCore::GraphicsLayer* layer)
     if (m_isClosed)
         return;
 
+    // With the new drawing area we don't need to inform the UI process when the accelerated
+    // compositing mode changes.
+    if (m_drawingArea->info().type == DrawingAreaInfo::Impl)
+        return;
+
     bool compositing = layer;
     
     // Tell the UI process that accelerated compositing changed. It may respond by changing
@@ -343,6 +348,7 @@ void WebPage::enterAcceleratedCompositingMode(GraphicsLayer* layer)
 void WebPage::exitAcceleratedCompositingMode()
 {
     changeAcceleratedCompositingMode(0);
+    m_drawingArea->setRootCompositingLayer(0);
 }
 #endif
 
