d99ccaea06366c79b1645f37984c9b6e6857e926
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-25 12:43:02 +0000

    2011-01-25  Andreas Kling  <kling@webkit.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt][WK2] Add QWKPage::processCrashed() signal
            https://bugs.webkit.org/show_bug.cgi?id=53076
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPagePrivate::processDidCrash):
            * UIProcess/API/qt/qwkpage.h:
            * UIProcess/API/qt/qwkpage_p.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -394,6 +394,11 @@ void QWKPagePrivate::didRelaunchProcess()
         q->setViewportSize(wkView->size().toSize());
 }
 
+void QWKPagePrivate::processDidCrash()
+{
+    emit q->processCrashed();
+}
+
 QWKPage::QWKPage(QWKContext* context)
     : d(new QWKPagePrivate(this, context))
 {
