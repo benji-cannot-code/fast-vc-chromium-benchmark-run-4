4a7b5a12a043f66abbc47aceff62e26f167ffdf5
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 05:35:38 +0000

    [Sync] Roll cache-invalidation to @70
    Pass user agent to InvalidationClientImpl as client_info.
    
    Pick up fix for bug that was causing increased server traffic.
    
    BUG=70674
    TEST=none
    
    Review URL: http://codereview.chromium.org/6310015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/server_notifier_thread.cc
+++ chrome/browser/sync/notifier/server_notifier_thread.cc
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/browser/sync/notifier/chrome_invalidation_client.h"
+#include "googleurl/src/gurl.h"
 #include "jingle/notifier/base/notifier_options.h"
 #include "jingle/notifier/listener/notification_defines.h"
 #include "talk/xmpp/xmppclient.h"
+#include "webkit/glue/webkit_glue.h"
 
 namespace sync_notifier {
 
@@ -114,8 +116,11 @@ void ServerNotifierThread::DoListenForUpdates() {
     // make it so that we won't receive any notifications that were
     // generated from our own changes.
     const std::string kClientId = "server_notifier_thread";
+    // Use user agent as |client_info| so we can use it for debugging
+    // server-side.
+    const std::string& client_info = webkit_glue::GetUserAgent(GURL());
     chrome_invalidation_client_->Start(
-        kClientId, state_, this, this, base_task_);
+        kClientId, client_info, state_, this, this, base_task_);
     state_.clear();
   }
 }
