6e2ad87cfe0ebd2bcaa7eee5ce0084ac6c5945c2
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-25 13:46:18 +0000

    2011-01-25  Dirk Schulze  <krit@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            SVG is missing to-animation support for Path
            https://bugs.webkit.org/show_bug.cgi?id=52982
    
            Added an animation test to check 'to' animations in combination with SVG paths.
    
            * svg/animations/animate-path-to-animation-expected.txt: Added.
            * svg/animations/animate-path-to-animation.html: Added.
            * svg/animations/script-tests/animate-path-to-animation.js: Added.
            (sample1):
            (sample2):
            (sample3):
            (executeTest):
    2011-01-25  Dirk Schulze  <krit@webkit.org>
    
            Reviewed by Nikolas Zimmermann.
    
            SVG is missing to-animation support for Path
            https://bugs.webkit.org/show_bug.cgi?id=52982
    
            SVG was missing 'to' animation support for SVG paths. Even the fallback to discrete
            animation did not work and an assert was thrown, because of the missing m_fromPath.
            This also influences a test of the W3C test suite. Subtest 2 of animate-elem-83-t.svg passes now.
    
            Test: svg/animations/animate-path-to-animation.html
    
            * svg/SVGAnimateElement.cpp:
            (WebCore::SVGAnimateElement::calculateAnimatedValue): Take the value of the last SVGAnimateElement for
            m_fromPath, since 'to' animations are accumulative.
            (WebCore::SVGAnimateElement::calculateFromAndToValues): Added support for 'to' animations.
            (WebCore::SVGAnimateElement::resetToBaseValue): Set m_animatedPath on the first animation element to baseVal.
            * svg/SVGPathByteStream.h:
            (WebCore::SVGPathByteStream::copySVGPathByteStream): Return copy of current byte stream.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGPathByteStream.h
+++ third_party/WebKit/Source/WebCore/svg/SVGPathByteStream.h
@@ -52,6 +52,11 @@ public:
         return adoptPtr(new SVGPathByteStream);
     }
 
+    PassOwnPtr<SVGPathByteStream> copy()
+    {
+        return adoptPtr(new SVGPathByteStream(m_data));
+    }
+
     typedef Vector<unsigned char> Data;
     typedef Data::const_iterator DataIterator;
 
@@ -63,6 +68,11 @@ public:
 
 private:
     SVGPathByteStream() { }
+    SVGPathByteStream(Data& data)
+        : m_data(data)
+    {
+    }
+
     Data m_data;
 };
 
