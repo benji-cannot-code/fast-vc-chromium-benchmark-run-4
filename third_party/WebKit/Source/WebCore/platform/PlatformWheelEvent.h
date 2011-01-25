abc5e4ff39dbfa2fc6302ceee7517cddee7e1f5b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-25 19:54:58 +0000

    2011-01-25  Sam Weinig  <sam@webkit.org>
            Reviewed by Anders Carlsson.
    
            Add hasPreciseScrollingDeltas bit to PlatformWheelEvent on the Mac.
            https://bugs.webkit.org/show_bug.cgi?id=53107
    
            * platform/PlatformWheelEvent.h:
            (WebCore::PlatformWheelEvent::PlatformWheelEvent):
            (WebCore::PlatformWheelEvent::hasPreciseScrollingDeltas):
            * platform/mac/WheelEventMac.mm:
            (WebCore::PlatformWheelEvent::PlatformWheelEvent):
    2011-01-25  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Anders Carlsson.
    
            Add hasPreciseScrollingDeltas bit to PlatformWheelEvent on the Mac.
            https://bugs.webkit.org/show_bug.cgi?id=53107
    
            * Shared/WebEvent.h:
            (WebKit::WebWheelEvent::hasPreciseScrollingDeltas):
            * Shared/WebEventConversion.cpp:
            (WebKit::WebKit2PlatformWheelEvent::WebKit2PlatformWheelEvent):
            * Shared/WebWheelEvent.cpp:
            (WebKit::WebWheelEvent::WebWheelEvent):
            * Shared/mac/WebEventFactory.mm:
            (WebKit::WebEventFactory::createWebWheelEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PlatformWheelEvent.h
+++ third_party/WebKit/Source/WebCore/platform/PlatformWheelEvent.h
@@ -99,6 +99,7 @@ namespace WebCore {
             , m_altKey(false)
             , m_metaKey(false)
 #if PLATFORM(MAC)
+            , m_hasPreciseScrollingDeltas(false)
             , m_phase(PlatformWheelEventPhaseNone)
 #endif
         {
@@ -152,6 +153,7 @@ namespace WebCore {
 #endif
 
         PlatformWheelEventPhase phase() const { return m_phase; }
+        bool hasPreciseScrollingDeltas() const { return m_hasPreciseScrollingDeltas; }
 #endif
 
 #if PLATFORM(QT)
@@ -187,6 +189,7 @@ namespace WebCore {
         bool m_altKey;
         bool m_metaKey;
 #if PLATFORM(MAC)
+        bool m_hasPreciseScrollingDeltas;
         PlatformWheelEventPhase m_phase;
 #endif
     };
--- third_party/WebKit/Source/WebKit2/Shared/WebEvent.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebEvent.h
@@ -167,7 +167,7 @@ public:
 
     WebWheelEvent(Type, const WebCore::IntPoint& position, const WebCore::IntPoint& globalPosition, const WebCore::FloatSize& delta, const WebCore::FloatSize& wheelTicks, Granularity, Modifiers, double timestamp);
 #if PLATFORM(MAC)
-    WebWheelEvent(Type, const WebCore::IntPoint& position, const WebCore::IntPoint& globalPosition, const WebCore::FloatSize& delta, const WebCore::FloatSize& wheelTicks, Granularity, Phase, Modifiers, double timestamp);
+    WebWheelEvent(Type, const WebCore::IntPoint& position, const WebCore::IntPoint& globalPosition, const WebCore::FloatSize& delta, const WebCore::FloatSize& wheelTicks, Granularity, Phase, bool hasPreciseScrollingDeltas, Modifiers, double timestamp);
 #endif
 
     const WebCore::IntPoint position() const { return m_position; }
@@ -177,6 +177,7 @@ public:
     Granularity granularity() const { return static_cast<Granularity>(m_granularity); }
 #if PLATFORM(MAC)
     Phase phase() const { return static_cast<Phase>(m_phase); }
+    bool hasPreciseScrollingDeltas() const { return m_hasPreciseScrollingDeltas; }
 #endif
 
     void encode(CoreIPC::ArgumentEncoder*) const;
@@ -192,6 +193,7 @@ private:
     uint32_t m_granularity; // Granularity
 #if PLATFORM(MAC)
     uint32_t m_phase; // Phase
+    bool m_hasPreciseScrollingDeltas;
 #endif
 };
 
