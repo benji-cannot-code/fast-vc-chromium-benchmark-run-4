70897f9940a5c7470d23c07c784698387c8ba34a
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 22:26:18 +0000

    Remove the bad code.
    TBE=lzhang
    BUG=none
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72554 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/fullscreen_win.cc
+++ chrome/browser/fullscreen_win.cc
@@ -9,16 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shellapi.h>
 
 static bool IsPlatformFullScreenMode() {
-#if 0
   return false;
-#else
-  // SHQueryUserNotificationState is only available for Vista and above.
-  QUERY_USER_NOTIFICATION_STATE state;
-  if (FAILED(::SHQueryUserNotificationState(&state)))
-    return false;
-  return state == QUNS_RUNNING_D3D_FULL_SCREEN ||
-         state == QUNS_PRESENTATION_MODE;
-#endif
 }
 
 static bool IsFullScreenWindowMode() {
