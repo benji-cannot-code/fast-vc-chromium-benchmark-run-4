55e0a9e0441f4b20a4448013efe4f6e60f144c15
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 01:50:46 +0000

    Fix compile
    BUG=none
    TEST=green tree
    TBR=tfarina
    
    Review URL: http://codereview.chromium.org/6264014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72449 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/combobox_example.cc
+++ views/examples/combobox_example.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "ui/base/models/combobox_model.h"
-#include "views/fill_layout.h"
 
 namespace {
 
--- views/examples/slider_example.cc
+++ views/examples/slider_example.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/examples/slider_example.h"
 
 #include "build/build_config.h"
-#include "views/fill_layout.h"
+#include "views/layout/fill_layout.h"
 
 namespace examples {
 
