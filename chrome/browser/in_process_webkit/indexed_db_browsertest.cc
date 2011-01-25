fee4522c3da9afe07ffd1701bcf143cea4087cb7
lzheng@chromium.org <lzheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 21:25:16 +0000

    Correct typo for "disabled". TEST=none BUG=none Review URL: http://codereview.chromium.org/6368017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72545 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/indexed_db_browsertest.cc
+++ chrome/browser/in_process_webkit/indexed_db_browsertest.cc
@@ -54,7 +54,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, CursorTest) {
 }
 
 // http://code.google.com/p/chromium/issues/detail?id=70773
-IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, DISALBED_IndexTest) {
+IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, DISABLED_IndexTest) {
   SimpleTest(testUrl(FilePath(FILE_PATH_LITERAL("index_test.html"))));
 }
 
