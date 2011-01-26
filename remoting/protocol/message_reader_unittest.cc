bac90b5bc731d7e39449f941bbf90d9dfdd83ad3
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-26 03:17:23 +0000

    Fixed memory leaks in remoting_unittests. BUG=70826 TEST=remoting_unittests under valgrind
    Review URL: http://codereview.chromium.org/6266020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72601 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/message_reader_unittest.cc
+++ remoting/protocol/message_reader_unittest.cc
@@ -60,6 +60,11 @@ class MessageReaderTest : public testing::Test {
     return result == expected;
   }
 
+  void RunAndDeleteTask(Task* task) {
+    task->Run();
+    delete task;
+  }
+
   // MessageLoop must be first here, so that is is destroyed the last.
   MessageLoop message_loop_;
 
@@ -91,7 +96,7 @@ TEST_F(MessageReaderTest, OneMessage_Delay) {
   // finished processing the previous message.
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task->Run();
+  RunAndDeleteTask(done_task);
 
   EXPECT_TRUE(socket_.read_pending());
 }
@@ -138,11 +143,11 @@ TEST_F(MessageReaderTest, TwoMessages_Together) {
   // finished processing the previous message.
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task1->Run();
+  RunAndDeleteTask(done_task1);
 
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task2->Run();
+  RunAndDeleteTask(done_task2);
 
   EXPECT_TRUE(socket_.read_pending());
 }
@@ -173,7 +178,7 @@ TEST_F(MessageReaderTest, TwoMessages_Instant) {
   // finished processing the second message.
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task2->Run();
+  RunAndDeleteTask(done_task2);
 
   EXPECT_TRUE(socket_.read_pending());
 }
@@ -217,7 +222,7 @@ TEST_F(MessageReaderTest, TwoMessages_Separately) {
   // finished processing the previous message.
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task->Run();
+  RunAndDeleteTask(done_task);
 
   EXPECT_TRUE(socket_.read_pending());
 
@@ -234,7 +239,7 @@ TEST_F(MessageReaderTest, TwoMessages_Separately) {
   // finished processing the previous message.
   EXPECT_FALSE(socket_.read_pending());
 
-  done_task->Run();
+  RunAndDeleteTask(done_task);
 
   EXPECT_TRUE(socket_.read_pending());
 }
