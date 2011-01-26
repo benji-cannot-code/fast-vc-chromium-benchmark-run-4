a55fae902b62bacfb9e472c3ff942c2862bc41af
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-26 01:09:10 +0000

    Allow CDSA/CSSM to be used on worker threads in debug builds by making both the CSSM init singleton and the Mac Security Services lock singleton leaky.
    This primarily affects the use of <keygen> in a debug build, as the RSA private key is generated on a worker thread and needs to hold the Mac Security Services lock.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6354017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72580 0039d316-1c4b-4281-b951-d872f2087c98

--- base/crypto/cssm_init.cc
+++ base/crypto/cssm_init.cc
@@ -24,10 +24,11 @@ namespace {
 class CSSMInitSingleton {
  public:
   static CSSMInitSingleton* GetInstance() {
-    return Singleton<CSSMInitSingleton>::get();
+    return Singleton<CSSMInitSingleton,
+                     LeakySingletonTraits<CSSMInitSingleton> >::get();
   }
 
-  CSSM_CSP_HANDLE csp_handle() const  {return csp_handle_;}
+  CSSM_CSP_HANDLE csp_handle() const  { return csp_handle_; }
 
  private:
   CSSMInitSingleton() : inited_(false), loaded_(false), csp_handle_(NULL) {
@@ -88,18 +89,17 @@ class CSSMInitSingleton {
 class SecurityServicesSingleton {
  public:
   static SecurityServicesSingleton* GetInstance() {
-    return Singleton<SecurityServicesSingleton>::get();
+    return Singleton<SecurityServicesSingleton,
+                     LeakySingletonTraits<SecurityServicesSingleton> >::get();
   }
-
-  ~SecurityServicesSingleton() {}
 
   base::Lock& lock() { return lock_; }
 
  private:
-  friend class Singleton<SecurityServicesSingleton>;
   friend struct DefaultSingletonTraits<SecurityServicesSingleton>;
 
   SecurityServicesSingleton() {}
+  ~SecurityServicesSingleton() {}
 
   base::Lock lock_;
 
