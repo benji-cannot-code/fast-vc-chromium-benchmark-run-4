0b3c6a43e711d1f2b0bd0a7e98022844429e5e93
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-26 00:18:06 +0000

    2011-01-25  Anders Carlsson  <andersca@apple.com>
            Reviewed by Adam Roben.
    
            Add and implement LayerTreeHost::scheduleLayerFlush
            https://bugs.webkit.org/show_bug.cgi?id=53135
    
            * WebKit2.xcodeproj/project.pbxproj:
            Add LayerTreeHostMac.mm.
    
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            (WebKit::DrawingAreaImpl::scheduleCompositingLayerSync):
            Call LayerTreeHost::scheduleLayerFlush.
    
            * WebProcess/WebPage/LayerTreeHost.cpp:
            (WebKit::LayerTreeHost::~LayerTreeHost):
            Call platformInvalidate().
    
            (WebKit::LayerTreeHost::flushPendingLayerChanges):
            Rename from syncCompositingLayers.
    
            * WebProcess/WebPage/mac/LayerTreeHostMac.mm: Added.
            (WebKit::LayerTreeHost::scheduleLayerFlush):
            Create a run loop observer if needed. This code is copied from LayerBackedDrawingAreaMac.mm
    
            (WebKit::LayerTreeHost::platformInvalidate):
            Invalidate the run loop observer.
    
            (WebKit::LayerTreeHost::flushPendingLayerChangesRunLoopObserverCallback):
            Call flushPendingChanges() and remove the run loop observer if it succeeded.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/LayerTreeHost.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/LayerTreeHost.cpp
@@ -46,6 +46,7 @@ LayerTreeHost::LayerTreeHost(WebPage* webPage)
 
 LayerTreeHost::~LayerTreeHost()
 {
+    platformInvalidate();
 }
 
 void LayerTreeHost::attachRootCompositingLayer(GraphicsLayer* graphicsLayer)
@@ -58,7 +59,7 @@ void LayerTreeHost::detachRootCompositingLayer()
     // FIXME: Implement.
 }
 
-bool LayerTreeHost::syncCompositingLayers()
+bool LayerTreeHost::flushPendingLayerChanges()
 {
     return m_webPage->corePage()->mainFrame()->view()->syncCompositingStateIncludingSubframes();
 }
