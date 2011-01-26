e726b5c3fb9d28542bb35d9cc3ad551a5cc62353
senorblanco@chromium.org <senorblanco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-26 23:14:01 +0000

    Fix for jankiness in some accelerated 2D canvas demos:  If there is already a pending call to CopyTextureToParentTextureCHROMIUM, the second will block until the first is consumed (same fix as was done to SwapBuffers for WebGL).  [Patch by gman; I just typed.  :)]
    BUG=68735
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6373013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72713 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -1583,6 +1583,7 @@ _FUNCTION_INFO = {
                   'GLsizei stride, GLuint offset',
   },
   'CopyTextureToParentTextureCHROMIUM': {
+      'impl_func': False,
       'decoder_func': 'DoCopyTextureToParentTextureCHROMIUM',
       'unit_test': False,
       'extension': True,
@@ -5325,7 +5326,7 @@ class GLGenerator(object):
       else:
         arg = context_arg
       file.Write("%s %s(%s) {\n" % (func.return_type, func.name, arg))
-      
+
       file.Write("""  scoped_refptr<PPB_Context3D_Impl> context =
       Resource::GetAs<PPB_Context3D_Impl>(context_id);
 """)
