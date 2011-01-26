6b085c113473c55182fee660cced4f5c1b673686
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-26 01:17:17 +0000

    2011-01-25  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Barth.
    
            Move main CMakeLists.txt into Source directory
            https://bugs.webkit.org/show_bug.cgi?id=52888
    
            * CMakeLists.txt: Removed.
            * Source/CMakeLists.txt: Copied from CMakeLists.txt.
            * Source/cmakeconfig.h.cmake: Copied from cmakeconfig.h.cmake.
            * cmakeconfig.h.cmake: Removed.
    2011-01-25  Patrick Gansterer  <paroga@webkit.org>
    
            Reviewed by Adam Barth.
    
            Move main CMakeLists.txt into Source directory
            https://bugs.webkit.org/show_bug.cgi?id=52888
    
            * Scripts/webkitdirs.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1502,7 +1502,7 @@ sub buildCMakeProject($@)
             push @buildArgs, "-DCMAKE_BUILD_TYPE=Release";
         }
 
-        push @buildArgs, sourceDir();
+        push @buildArgs, sourceDir() . "/Source";
 
         $dir = File::Spec->catfile($dir, $config);
         File::Path::mkpath($dir);
