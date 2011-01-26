db2f0c9e08f43f78e570f973062fcf94ac75f049
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-26 15:04:51 +0000

    Release all DataSources before the main thread goes away.
    BUG=70782
    TEST=Valgrind no longer needs suppressions for AddDataSource
    
    Review URL: http://codereview.chromium.org/6268014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72638 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -256,6 +256,11 @@ void Shutdown() {
     chromeos::CrosLibrary::Get()->GetLoginLibrary()->StopSession("");
   }
 #endif
+
+  // Clean up data sources before the UI thread is removed.
+  ChromeURLDataManager* data_manager = ChromeURLDataManager::GetInstance();
+  if (data_manager)
+    data_manager->RemoveAllDataSources();
 }
 
 void ReadLastShutdownFile(
