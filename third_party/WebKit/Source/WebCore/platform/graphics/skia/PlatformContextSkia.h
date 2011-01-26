5746e290ceb138a75530bfc08e9c287b16ae38ae
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-26 14:50:19 +0000

    2011-01-26  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Andreas Kling.
    
            [SKIA] Remove "current path" of GraphicsContext
            https://bugs.webkit.org/show_bug.cgi?id=53124
    
            * platform/graphics/GraphicsContext.h:
            * platform/graphics/skia/GraphicsContextSkia.cpp:
            (WebCore::GraphicsContext::clipPath):
            (WebCore::GraphicsContext::fillPath):
            (WebCore::GraphicsContext::strokePath):
            * platform/graphics/skia/PathSkia.cpp:
            (WebCore::Path::strokeBoundingRect):
            * platform/graphics/skia/PlatformContextSkia.cpp:
            * platform/graphics/skia/PlatformContextSkia.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.h
@@ -119,7 +119,6 @@ public:
     void setAlpha(float);
     void setLineCap(SkPaint::Cap);
     void setLineJoin(SkPaint::Join);
-    void setFillRule(SkPath::FillType);
     void setXfermodeMode(SkXfermode::Mode);
     void setFillColor(SkColor);
     void setFillShader(SkShader*);
@@ -137,10 +136,6 @@ public:
     TextDrawingModeFlags getTextDrawingMode() const;
     float getAlpha() const;
     int getNormalizedAlpha() const;
-
-    void beginPath();
-    void addPath(const SkPath&);
-    SkPath currentPathInLocalCoordinates() const;
 
     void canvasClipPath(const SkPath&);
 
@@ -220,9 +215,6 @@ private:
     // Pointer to the current drawing state. This is a cached value of
     // mStateStack.back().
     State* m_state;
-
-    // Current path in global coordinates.
-    SkPath m_path;
 
     // Stores image sizes for a hint to compute image resampling modes.
     // Values are used in ImageSkia.cpp
