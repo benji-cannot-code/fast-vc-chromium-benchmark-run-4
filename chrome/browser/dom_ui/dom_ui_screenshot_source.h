ccb5b99abd40707289a0f5c8fdb965f05745db05
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-26 03:00:55 +0000

    Cleanup dom_ui_screenshot_source.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6334011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/dom_ui_screenshot_source.h
+++ chrome/browser/dom_ui/dom_ui_screenshot_source.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,13 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_DOM_UI_DOM_UI_SCREENSHOT_SOURCE_H_
 
 #include <string>
-#include <utility>
 #include <vector>
 
 #include "base/basictypes.h"
-#include "base/scoped_ptr.h"
 #include "chrome/browser/dom_ui/chrome_url_data_manager.h"
-#include "chrome/browser/dom_ui/html_dialog_ui.h"
 
 // ScreenshotSource is the data source that serves screenshots (saved
 // or current) to the bug report html ui
@@ -35,7 +32,6 @@ class DOMUIScreenshotSource : public ChromeURLDataManager::DataSource {
  private:
   virtual ~DOMUIScreenshotSource();
 
-//  scoped_refptr<RefCountedBytes> current_screenshot_;
   std::vector<unsigned char> current_screenshot_;
   DISALLOW_COPY_AND_ASSIGN(DOMUIScreenshotSource);
 };
