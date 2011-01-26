ff4e574e80370f87b8d55e27e8a91ba77c098f44
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-26 04:00:14 +0000

    HTMLFormElement::checkValidity() returns incorrect result if 'invalid' events are canceled. https://bugs.webkit.org/show_bug.cgi?id=52565
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    * html/HTMLFormElement.cpp:
    (WebCore::HTMLFormElement::validateInteractively):
      Check checkInvalidControlsAndCollectUnhandled() result instead of
      checking emptiness of unhandled invalid controls list.
    (WebCore::HTMLFormElement::checkValidity): ditto.
    (WebCore::HTMLFormElement::checkInvalidControlsAndCollectUnhandled):
      Renamed from collectUnhandledInvalidControls().
      Returns true if there is any invalid control regardless of event canceling.
    * html/HTMLFormElement.h: Rename collectUnhandledInvalidControls() to
      checkInvalidControlsAndCollectUnhandled().
    
    LayoutTests:
    
    Fix tests and results for the behavior change.
    
    * fast/forms/checkValidity-cancel-expected.txt:
    * fast/forms/checkValidity-handler-updates-dom-expected.txt:
    * fast/forms/interactive-validation-cancel-expected.txt:
    * fast/forms/interactive-validation-cancel.html:
    * fast/forms/script-tests/checkValidity-cancel.js:
    * fast/forms/script-tests/checkValidity-handler-updates-dom.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLFormElement.cpp
@@ -222,8 +222,7 @@ bool HTMLFormElement::validateInteractively(Event* event)
     }
 
     Vector<RefPtr<FormAssociatedElement> > unhandledInvalidControls;
-    collectUnhandledInvalidControls(unhandledInvalidControls);
-    if (unhandledInvalidControls.isEmpty())
+    if (!checkInvalidControlsAndCollectUnhandled(unhandledInvalidControls))
         return true;
     // If the form has invalid controls, abort submission.
 
@@ -587,8 +586,7 @@ HTMLFormControlElement* HTMLFormElement::defaultButton() const
 bool HTMLFormElement::checkValidity()
 {
     Vector<RefPtr<FormAssociatedElement> > controls;
-    collectUnhandledInvalidControls(controls);
-    return controls.isEmpty();
+    return !checkInvalidControlsAndCollectUnhandled(controls);
 }
 
 void HTMLFormElement::broadcastFormEvent(const AtomicString& eventName)
@@ -624,7 +622,7 @@ void HTMLFormElement::dispatchFormChange()
     broadcastFormEvent(eventNames().formchangeEvent);
 }
 
-void HTMLFormElement::collectUnhandledInvalidControls(Vector<RefPtr<FormAssociatedElement> >& unhandledInvalidControls)
+bool HTMLFormElement::checkInvalidControlsAndCollectUnhandled(Vector<RefPtr<FormAssociatedElement> >& unhandledInvalidControls)
 {
     RefPtr<HTMLFormElement> protector(this);
     // Copy m_associatedElements because event handlers called from
@@ -633,10 +631,15 @@ void HTMLFormElement::collectUnhandledInvalidControls(Vector<RefPtr<FormAssociat
     elements.reserveCapacity(m_associatedElements.size());
     for (unsigned i = 0; i < m_associatedElements.size(); ++i)
         elements.append(m_associatedElements[i]);
+    bool hasInvalidControls = false;
     for (unsigned i = 0; i < elements.size(); ++i) {
-        if (elements[i]->form() == this && elements[i]->isFormControlElement())
-            static_cast<HTMLFormControlElement*>(elements[i].get())->checkValidity(&unhandledInvalidControls);
+        if (elements[i]->form() == this && elements[i]->isFormControlElement()) {
+            HTMLFormControlElement* control = static_cast<HTMLFormControlElement*>(elements[i].get());
+            if (!control->checkValidity(&unhandledInvalidControls) && control->form() == this)
+                hasInvalidControls = true;
+        }
     }
+    return hasInvalidControls;
 }
 
 HTMLFormControlElement* HTMLFormElement::elementForAlias(const AtomicString& alias)
