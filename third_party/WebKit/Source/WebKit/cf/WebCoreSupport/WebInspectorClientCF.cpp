8748493becc0878fe2b6930c59af6fb67ee8efbb
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-26 11:43:09 +0000

    2011-01-26  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: inspector should start in attached state by default
            https://bugs.webkit.org/show_bug.cgi?id=53165
    
            * WebCoreSupport/WebInspectorClientCF.cpp:
            (WebInspectorClient::inspectorStartsAttached):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/cf/WebCoreSupport/WebInspectorClientCF.cpp
+++ third_party/WebKit/Source/WebKit/cf/WebCoreSupport/WebInspectorClientCF.cpp
@@ -110,6 +110,8 @@ bool WebInspectorClient::inspectorStartsAttached()
 {
     String value;
     populateSetting(inspectorStartsAttachedSetting, &value);
+    if (value.isEmpty())
+        return true;
     return value == "true";
 }
 
