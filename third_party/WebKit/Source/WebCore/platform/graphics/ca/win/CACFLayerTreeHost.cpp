b532118724fdbc3a07f3ce205b4108c58f35a4f9
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-27 06:40:54 +0000

    Add assertions that CACFLayerTreeHost gains and loses an HWND only once
    CACFLayerTreeHost doesn't support any other use pattern.
    
    Reviewed by Sam Weinig.
    
    * platform/graphics/ca/win/CACFLayerTreeHost.cpp:
    (WebCore::CACFLayerTreeHost::CACFLayerTreeHost): Initialize new member.
    (WebCore::CACFLayerTreeHost::setWindow): Assert that we transition from not having a window,
    to having a window, to not having a window just once over the lifetime of this object.
    
    * platform/graphics/ca/win/CACFLayerTreeHost.h: Added m_state.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.cpp
@@ -191,6 +191,9 @@ CACFLayerTreeHost::CACFLayerTreeHost()
     , m_mustResetLostDeviceBeforeRendering(false)
     , m_shouldFlushPendingGraphicsLayerChanges(false)
     , m_isFlushingLayerChanges(false)
+#if !ASSERT_DISABLED
+    , m_state(WindowNotSet)
+#endif
 {
     // Point the CACFContext to this
     wkCACFContextSetUserData(m_context, this);
@@ -233,6 +236,24 @@ void CACFLayerTreeHost::setWindow(HWND window)
 {
     if (window == m_window)
         return;
+
+#if !ASSERT_DISABLED
+    switch (m_state) {
+    case WindowNotSet:
+        ASSERT_ARG(window, window);
+        ASSERT(!m_window);
+        m_state = WindowSet;
+        break;
+    case WindowSet:
+        ASSERT_ARG(window, !window);
+        ASSERT(m_window);
+        m_state = WindowCleared;
+        break;
+    case WindowCleared:
+        ASSERT_NOT_REACHED();
+        break;
+    }
+#endif
 
     if (m_window)
         destroyRenderer();
