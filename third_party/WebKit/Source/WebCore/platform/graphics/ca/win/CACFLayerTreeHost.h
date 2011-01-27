86d0b2098dc45060f72e326b2bfa55bd664c4a5a
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-27 06:40:14 +0000

    Notify layers that their animations have started when we flush the context, not when we render
    r76372 separated context flushing from rendering, but this bit of code got left behind.
    
    Reviewed by Sam Weinig.
    
    * platform/graphics/ca/win/CACFLayerTreeHost.cpp:
    (WebCore::CACFLayerTreeHost::render): Moved code to notify the layers from here to
    notifyAnimationsStarted.
    (WebCore::CACFLayerTreeHost::flushPendingLayerChangesNow): Added a call to
    notifyAnimationsStarted after we flush the context.
    (WebCore::CACFLayerTreeHost::notifyAnimationsStarted): Added. Code came from render. Changed
    to call PlatformCALayer::animationStarted rather than calling through to the client
    directly.
    
    * platform/graphics/ca/win/CACFLayerTreeHost.h: Added notifyAniamtionsStarted.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.h
@@ -89,6 +89,7 @@ private:
     void destroyRenderer();
     void renderSoon();
     void renderTimerFired(Timer<CACFLayerTreeHost>*);
+    void notifyAnimationsStarted();
 
     CGRect bounds() const;
 
