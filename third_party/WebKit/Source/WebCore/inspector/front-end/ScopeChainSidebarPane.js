ce0955fb8844c9d7a0b5107a43ccf3a367c2d6ba
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-27 14:14:49 +0000

    2011-01-27  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r76789.
            http://trac.webkit.org/changeset/76789
            https://bugs.webkit.org/show_bug.cgi?id=53238
    
            Broke GTK layout tests (Requested by podivilov on #webkit).
    
            * inspector/front-end/ScopeChainSidebarPane.js:
            (WebInspector.ScopeChainSidebarPane):
            (WebInspector.ScopeChainSidebarPane.prototype.update):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScopeChainSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScopeChainSidebarPane.js
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ScopeChainSidebarPane = function()
 {
     WebInspector.SidebarPane.call(this, WebInspector.UIString("Scope Variables"));
-    this._sections = [];
-    this._expandedSections = {};
     this._expandedProperties = [];
 }
 
@@ -37,6 +35,9 @@ WebInspector.ScopeChainSidebarPane.prototype = {
     {
         this.bodyElement.removeChildren();
 
+        this.sections = [];
+        this.callFrame = callFrame;
+
         if (!callFrame) {
             var infoElement = document.createElement("div");
             infoElement.className = "info";
@@ -44,18 +45,6 @@ WebInspector.ScopeChainSidebarPane.prototype = {
             this.bodyElement.appendChild(infoElement);
             return;
         }
-
-        for (var i = 0; i < this._sections.length; ++i) {
-            var section = this._sections[i];
-            if (!section.title)
-                continue;
-            if (section.expanded)
-                this._expandedSections[section.title] = true;
-            else
-                delete this._expandedSections[section.title];
-        }
-
-        this._sections = [];
 
         var foundLocalScope = false;
         var scopeChain = callFrame.scopeChain;
@@ -93,10 +82,10 @@ WebInspector.ScopeChainSidebarPane.prototype = {
             section.editInSelectedCallFrameWhenPaused = true;
             section.pane = this;
 
-            if (!foundLocalScope || scopeObjectProxy.isLocal || title in this._expandedSections)
+            if (!foundLocalScope || scopeObjectProxy.isLocal)
                 section.expanded = true;
 
-            this._sections.push(section);
+            this.sections.push(section);
             this.bodyElement.appendChild(section.element);
         }
     }
