d2c86d97075157c2c3c2fef96fd0324fe0756ef4
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-27 21:23:06 +0000

    Add ability to do an unconstrained scroll on a ScrollView https://bugs.webkit.org/show_bug.cgi?id=53249
    Reviewed by Dave Hyatt.
    
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::ScrollView):
    Initialize m_constrainsScrollingToContentEdge to true.
    
    (WebCore::ScrollView::setScrollOffset):
    Only constrain the offset if the m_constrainsScrollingToContentEdge is set.
    
    (WebCore::ScrollView::updateScrollbars):
    Simplify expression converting an IntSize to an IntPoint.
            
    (WebCore::ScrollView::paint):
    Paint the overhang if there is any.
            
    (WebCore::ScrollView::calculateOverhangAreasForPainting):
    Calculate the overhang in viewport coordinates for painting.
    
    * platform/ScrollView.h:
    (WebCore::ScrollView::constrainsScrollingToContentEdge):
    (WebCore::ScrollView::setConstrainsScrollingToContentEdge):
    Add bit to control whether the scroll position should be constrained
    to the content edge when set.
    
    * platform/ScrollbarThemeComposite.cpp:
    (WebCore::usedTotalSize):
    (WebCore::ScrollbarThemeComposite::thumbPosition):
    (WebCore::ScrollbarThemeComposite::thumbLength):
    * platform/mac/ScrollbarThemeMac.mm:
    (WebCore::ScrollbarThemeMac::paint):
    Improve calculations of thumb size and position to take overhang into account.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollView.h
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.h
@@ -163,7 +163,11 @@ public:
     int contentsWidth() const { return contentsSize().width(); }
     int contentsHeight() const { return contentsSize().height(); }
     virtual void setContentsSize(const IntSize&);
-   
+
+    // Functions for controlling if you can scroll past the end of the document.
+    bool constrainsScrollingToContentEdge() const { return m_constrainsScrollingToContentEdge; }
+    void setConstrainsScrollingToContentEdge(bool constrainsScrollingToContentEdge) { m_constrainsScrollingToContentEdge = constrainsScrollingToContentEdge; }
+
     // Functions for querying the current scrolled position (both as a point, a size, or as individual X and Y values).
     IntPoint scrollPosition() const { return visibleContentRect().location(); }
     IntSize scrollOffset() const { return visibleContentRect().location() - IntPoint(); } // Gets the scrolled position as an IntSize. Convenient for adding to other sizes.
@@ -276,7 +280,7 @@ protected:
 
     virtual void repaintContentRectangle(const IntRect&, bool now = false);
     virtual void paintContents(GraphicsContext*, const IntRect& damageRect) = 0;
-    
+
     virtual void contentsResized() = 0;
     virtual void visibleContentsResized() = 0;
 
@@ -336,6 +340,8 @@ private:
     bool m_clipsRepaints;
     bool m_delegatesScrolling;
 
+    bool m_constrainsScrollingToContentEdge;
+
     // There are 8 possible combinations of writing mode and direction.  Scroll origin will be non-zero in the x or y axis
     // if there is any reversed direction or writing-mode.  The combinations are:
     // writing-mode / direction     scrollOrigin.x() set    scrollOrigin.y() set
@@ -357,6 +363,9 @@ private:
 
     // Called when the scroll position within this view changes.  FrameView overrides this to generate repaint invalidations.
     virtual void repaintFixedElementsAfterScrolling() {}
+
+    void calculateOverhangAreasForPainting(IntRect& horizontalOverhangRect, IntRect& verticalOverhangRect);
+    void paintOverhangAreas(GraphicsContext*, const IntRect& horizontalOverhangArea, const IntRect& verticalOverhangArea);
 
     void platformInit();
     void platformDestroy();
