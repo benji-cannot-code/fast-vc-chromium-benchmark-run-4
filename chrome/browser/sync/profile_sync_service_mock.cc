3add224d52f2ddd8043e709abea935332d88c5c0
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 20:17:24 +0000

    Unit tests that use ProfileSyncServiceMock are leaking due to adding a DataSource (SyncResourcesSource) to ChromeURLDataManager.
    The basic problem is that the destructors need to be called on the UI thread, but at shutdown the UI thread (which is the only thread left) is not recognized so the destruction is thrown to the thread even though the message loop is no longer running.  This means that the messages are dropped and memory is leaked.
    
    I added cleanup code to the destructor.
    
    BUG=70782
    TEST=Valgrind
    
    Review URL: http://codereview.chromium.org/6317017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72876 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_mock.cc
+++ chrome/browser/sync/profile_sync_service_mock.cc
@@ -4,7 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/sync/profile_sync_service_mock.h"
+#include "chrome/browser/dom_ui/chrome_url_data_manager.h"
+#include "chrome/common/url_constants.h"
 
 ProfileSyncServiceMock::ProfileSyncServiceMock() {}
 
-ProfileSyncServiceMock::~ProfileSyncServiceMock() {}
+ProfileSyncServiceMock::~ProfileSyncServiceMock() {
+  ChromeURLDataManager::GetInstance()->RemoveDataSourceForTest(
+      chrome::kChromeUISyncResourcesHost);
+}
