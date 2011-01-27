fb1d9fe7151cb9b684a05a3fe0526fb86497a6e8
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-27 18:52:34 +0000

    Reverted an accidental change in r76561.
    Rubber-stamped by Anders Carlsson.
    
    * WebView/WebHTMLView.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
@@ -1734,14 +1734,6 @@ static void _updateMouseoverTimerCallback(CFRunLoopTimerRef timer, void *info)
     }
 }
 
-- (BOOL)mouseDownCanMoveWindow
-{
-    BOOL result = [super mouseDownCanMoveWindow];
-
-    NSLog(@"%@, %@ %d", self, NSStringFromSelector(_cmd), result);
-    return result;
-}
-
 - (void)_handleAutoscrollForMouseDragged:(NSEvent *)event 
 { 
     [self autoscroll:event]; 
