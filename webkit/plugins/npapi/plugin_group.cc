29f5a6b4cb693cd0e8652f82151e3987c20b458c
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 01:16:30 +0000

    If a user chooses to open a PDF with Reader, ask them if they want to do so always. Review URL: http://codereview.chromium.org/6278017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72735 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_group.cc
+++ webkit/plugins/npapi/plugin_group.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webkit {
 namespace npapi {
 
-const char* PluginGroup::kAdobeReaderGroupName = "Adobe Reader";
+const char* PluginGroup::kAdobeReaderGroupName = "Adobe Acrobat";
 const char* PluginGroup::kAdobeReaderUpdateURL = "http://get.adobe.com/reader/";
 
 /*static*/
