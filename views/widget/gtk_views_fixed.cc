48793a0a612d121f280308277ed1899c9f46266d
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 20:03:48 +0000

    widget-gtk: Make it possible to grab focus.
    For a touchui build, there is only one native-view shared for all the components
    in the browser window, and gtk_widget_grab_focus requires this flag to be set on
    the widget to work.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6400004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72872 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/gtk_views_fixed.cc
+++ views/widget/gtk_views_fixed.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -71,6 +71,7 @@ static void gtk_views_fixed_class_init(GtkViewsFixedClass* views_fixed_class) {
 }
 
 static void gtk_views_fixed_init(GtkViewsFixed* fixed) {
+  GTK_WIDGET_SET_FLAGS(GTK_WIDGET(fixed), GTK_CAN_FOCUS);
 }
 
 GtkWidget* gtk_views_fixed_new(void) {
