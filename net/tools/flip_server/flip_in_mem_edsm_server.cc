dfdc6973e0099d8940a697c4ed967e2b5153050e
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 19:31:30 +0000

    The server will happily propagate invalid SPDY headers, which Chrome will reject.  Strip headers with 0 length header names or 0 length header values.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6286012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72858 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/flip_in_mem_edsm_server.cc
+++ net/tools/flip_server/flip_in_mem_edsm_server.cc
@@ -2076,6 +2076,11 @@ class SpdySM : public SpdyFramerVisitorInterface, public SMInterface {
          headers.header_lines_begin();
          hi != headers.header_lines_end();
          ++hi) {
+      // It is illegal to send SPDY headers with empty value or header
+      // names.
+      if (!hi->first.length() || !hi->second.length())
+        continue;
+
       SpdyHeaderBlock::iterator fhi = dest.find(hi->first.as_string());
       if (fhi == dest.end()) {
         dest[hi->first.as_string()] = hi->second.as_string();
