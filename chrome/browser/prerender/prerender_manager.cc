f3a59a9abde54d92dcd8b0e542c6a6255a0537e8
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-27 15:47:08 +0000

    Add histogram to track prerender sessions
    BUG=70401
    TEST=nope
    
    Review URL: http://codereview.chromium.org/6340013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -16,6 +16,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/render_view_host_manager.h"
 #include "chrome/common/render_messages.h"
 
+// static
+PrerenderManager::PrerenderManagerMode PrerenderManager::mode_ =
+    PRERENDER_MODE_ENABLED;
+
+// static
+PrerenderManager::PrerenderManagerMode PrerenderManager::GetMode() {
+  return mode_;
+}
+
+// static
+void PrerenderManager::SetMode(PrerenderManagerMode mode) {
+  mode_ = mode;
+}
+
 struct PrerenderManager::PrerenderContentsData {
   PrerenderContents* contents_;
   base::Time start_time_;
@@ -34,8 +48,7 @@ PrerenderManager::PrerenderManager(Profile* profile)
       max_prerender_age_(base::TimeDelta::FromSeconds(
           kDefaultMaxPrerenderAgeSeconds)),
       max_elements_(kDefaultMaxPrerenderElements),
-      prerender_contents_factory_(PrerenderContents::CreateFactory()),
-      mode_(PRERENDER_MODE_ENABLED) {
+      prerender_contents_factory_(PrerenderContents::CreateFactory()) {
 }
 
 PrerenderManager::~PrerenderManager() {
@@ -155,7 +168,7 @@ PrerenderContents* PrerenderManager::CreatePrerenderContents(
 }
 
 void PrerenderManager::RecordPerceivedPageLoadTime(base::TimeDelta pplt) {
-  switch(mode_) {
+  switch (mode_) {
     case PRERENDER_MODE_EXPERIMENT_CONTROL_GROUP:
       UMA_HISTOGRAM_TIMES("PLT.PerceivedPageLoadTime_PrerenderControl", pplt);
       break;
