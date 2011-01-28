48aa3bb10497e2e76cd3a041165bab1b95b698f6
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 19:31:55 +0000

    - upgrade_test::GenerateNextVersion is now GenerateAlternateVersion and can bump up or down - upgrade_test::GenerateAlternateVersion returns the old and new versions - added alternate_version_generator.exe command-line tool.
    BUG=none
    TEST=run alternate_version_generator.exe --help and explore it
    Review URL: http://codereview.chromium.org/6338020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/test/upgrade_test.cc
+++ chrome/installer/test/upgrade_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,8 +25,10 @@ class UpgradeTest : public testing::Test {
     ASSERT_TRUE(PathService::Get(base::DIR_EXE, &dir_exe));
     ASSERT_TRUE(file_util::CreateTemporaryFile(&next_mini_installer_path_));
     ASSERT_TRUE(
-        upgrade_test::GenerateNextVersion(dir_exe.Append(&kMiniInstallerExe[0]),
-                                          next_mini_installer_path_));
+        upgrade_test::GenerateAlternateVersion(
+            dir_exe.Append(&kMiniInstallerExe[0]),
+            next_mini_installer_path_,
+            upgrade_test::NEXT_VERSION, NULL, NULL));
   }
 
   // Clean up by deleting the created newer version of mini_installer.exe.
