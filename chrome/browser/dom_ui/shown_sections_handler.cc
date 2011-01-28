09071dcc491d4eb2e72294e549654e3bf1b51805
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 19:35:03 +0000

    DOMUI: Allow chrome.send to pass number, boolean, null and arrays of those
    BUG=None
    TEST=cpp_variant_unittest covers most of the new functionality. I also verified that NTP works correctly.
    
    Review URL: http://codereview.chromium.org/6254018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72994 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/shown_sections_handler.cc
+++ chrome/browser/dom_ui/shown_sections_handler.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -92,8 +92,9 @@ void ShownSectionsHandler::HandleGetShownSections(const ListValue* args) {
 }
 
 void ShownSectionsHandler::HandleSetShownSections(const ListValue* args) {
-  int mode;
-  CHECK(ExtractIntegerValue(args, &mode));
+  double mode_double;
+  CHECK(args->GetReal(0, &mode_double));
+  int mode = static_cast<int>(mode_double);
   int old_mode = pref_service_->GetInteger(prefs::kNTPShownSections);
 
   if (old_mode != mode) {
