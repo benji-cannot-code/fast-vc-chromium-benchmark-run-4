2ac4848ce846dab1236fee5510004c8ad7cd61b4
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 20:51:34 +0000

    Chromium build fix after r76967
    * wtf/ThreadingPrimitives.h: Use OS(WINDOWS) instead of PLATFORM(WIN), to match other
    similar macros in this file.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76975 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/ThreadingPrimitives.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/ThreadingPrimitives.h
@@ -151,7 +151,7 @@ private:
     PlatformCondition m_condition;
 };
 
-#if PLATFORM(WIN)
+#if OS(WINDOWS)
 // The absoluteTime is in seconds, starting on January 1, 1970. The time is assumed to use the same time zone as WTF::currentTime().
 // Returns an interval in milliseconds suitable for passing to one of the Win32 wait functions (e.g., ::WaitForSingleObject).
 DWORD absoluteTimeToWaitTimeoutInterval(double absoluteTime);
@@ -163,7 +163,7 @@ using WTF::Mutex;
 using WTF::MutexLocker;
 using WTF::ThreadCondition;
 
-#if PLATFORM(WIN)
+#if OS(WINDOWS)
 using WTF::absoluteTimeToWaitTimeoutInterval;
 #endif
 
