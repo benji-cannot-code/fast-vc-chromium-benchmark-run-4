de7f0de0a289b37c24b2385aa851fdc9846fa76e
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 05:30:10 +0000

    2011-01-27  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Dan Bernstein.
    
            Recalc table sections if needed before calculating the first line
            box baseline.
            https://bugs.webkit.org/show_bug.cgi?id=53265
    
            When we try to calculate the baseline position of a table cell,
            we recurse through all the child sibling boxes (when children are
            non inline) and add their first linebox baseline values. If one of
            the children is a table with pending section recalc, we will access
            wrong table section values. We recalc table sections if it is needed.
    
            Test: fast/table/recalc-section-first-body-crash-main.html
    
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::firstLineBoxBaseline):
    2011-01-27  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Dan Bernstein.
    
            Tests that we do not crash when calculating the first line box
            baseline for the table.
            https://bugs.webkit.org/show_bug.cgi?id=53265
    
            * fast/table/recalc-section-first-body-crash-main-expected.txt: Added.
            * fast/table/recalc-section-first-body-crash-main.html: Added.
            * fast/table/resources/recalc-section-first-body-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
@@ -1127,6 +1127,8 @@ int RenderTable::firstLineBoxBaseline() const
     if (isWritingModeRoot())
         return -1;
 
+    recalcSectionsIfNeeded();
+
     RenderTableSection* firstNonEmptySection = m_head ? m_head : (m_firstBody ? m_firstBody : m_foot);
     if (firstNonEmptySection && !firstNonEmptySection->numRows())
         firstNonEmptySection = sectionBelow(firstNonEmptySection, true);
