9a4792a3175414975647dd1bb89b04fb122c3f5a
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 11:20:18 +0000

    Implements delay before update screen is shown.
    This fix uses delay that is used before update screen is shown. This
    delay allows not to show update screen if update is quick enough.
    
    BUG=chromium-os:6329
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6338017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/update_screen.cc
+++ chrome/browser/chromeos/login/update_screen.cc
@@ -67,6 +67,7 @@ void UpdateScreen::UpdateStatusChanged(UpdateLibrary* library) {
       // check unless there is an update.
       break;
     case UPDATE_STATUS_UPDATE_AVAILABLE:
+      MakeSureScreenIsShown();
       view()->SetProgress(kBeforeDownloadProgress);
       if (!HasCriticalUpdate()) {
         LOG(INFO) << "Noncritical update available: "
@@ -79,6 +80,7 @@ void UpdateScreen::UpdateStatusChanged(UpdateLibrary* library) {
       break;
     case UPDATE_STATUS_DOWNLOADING:
       {
+        MakeSureScreenIsShown();
         if (!is_downloading_update_) {
           // Because update engine doesn't send UPDATE_STATUS_UPDATE_AVAILABLE
           // we need to is update critical on first downloading notification.
@@ -99,12 +101,15 @@ void UpdateScreen::UpdateStatusChanged(UpdateLibrary* library) {
       }
       break;
     case UPDATE_STATUS_VERIFYING:
+      MakeSureScreenIsShown();
       view()->SetProgress(kBeforeVerifyingProgress);
       break;
     case UPDATE_STATUS_FINALIZING:
+      MakeSureScreenIsShown();
       view()->SetProgress(kBeforeFinalizingProgress);
       break;
     case UPDATE_STATUS_UPDATED_NEED_REBOOT:
+      MakeSureScreenIsShown();
       // Make sure that first OOBE stage won't be shown after reboot.
       WizardController::MarkOobeCompleted();
       view()->SetProgress(kProgressComplete);
@@ -131,11 +136,13 @@ void UpdateScreen::UpdateStatusChanged(UpdateLibrary* library) {
 }
 
 void UpdateScreen::StartUpdate() {
-  // Reset view.
-  view()->Reset();
-  view()->set_controller(this);
-  is_downloading_update_ = false;
-  view()->SetProgress(kBeforeUpdateCheckProgress);
+  // Reset view if view was created.
+  if (view()) {
+    view()->Reset();
+    view()->set_controller(this);
+    is_downloading_update_ = false;
+    view()->SetProgress(kBeforeUpdateCheckProgress);
+  }
 
   if (!CrosLibrary::Get()->EnsureLoaded()) {
     LOG(ERROR) << "Error loading CrosLibrary";
@@ -154,6 +161,13 @@ void UpdateScreen::CancelUpdate() {
   // View is deleted after wizard proceeds to the next screen.
   if (view())
     ExitUpdate(REASON_UPDATE_CANCELED);
+}
+
+void UpdateScreen::Show() {
+  DefaultViewScreen<UpdateView>::Show();
+  view()->set_controller(this);
+  is_downloading_update_ = false;
+  view()->SetProgress(kBeforeUpdateCheckProgress);
 }
 
 void UpdateScreen::ExitUpdate(UpdateScreen::ExitReason reason) {
@@ -202,7 +216,14 @@ void UpdateScreen::ExitUpdate(UpdateScreen::ExitReason reason) {
 
 void UpdateScreen::OnWaitForRebootTimeElapsed() {
   LOG(ERROR) << "Unable to reboot - asking user for a manual reboot.";
+  MakeSureScreenIsShown();
   view()->ShowManualRebootInfo();
+}
+
+void UpdateScreen::MakeSureScreenIsShown() {
+  if (!view()) {
+    delegate()->ShowCurrentScreen();
+  }
 }
 
 void UpdateScreen::SetRebootCheckDelay(int seconds) {
