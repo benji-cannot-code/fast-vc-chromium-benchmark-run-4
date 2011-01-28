474b5eb77be39f88368eee7cc603dccfc9f49238
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 19:59:02 +0000

    Include util.js in the NTP so we get file:// linking.
    This fixes the bug that a Most Visited file:// link quit working following r72701
    
    BUG=57485
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6320020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/util.js
+++ chrome/browser/resources/shared/js/util.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {!Object}
  */
 const global = this;
+
+// TODO(estade): This should be removed and calls replaced with cr.isMac
+const IS_MAC = /^Mac/.test(navigator.platform);
 
 /**
  * Alias for document.getElementById.
@@ -36,7 +39,6 @@ function chromeSend(name, params, callbackName, callback) {
   };
   chrome.send(name, params);
 }
-
 
 /**
  * Generates a CSS url string.
@@ -71,6 +73,40 @@ function parseQueryParams(location) {
     params[pair[0]] = pair[1];
   }
   return params;
+}
+
+function findAncestorByClass(el, className) {
+  return findAncestor(el, function(el) {
+    if (el.classList)
+      return el.classList.contains(className);
+    return null;
+  });
+}
+
+/**
+ * Return the first ancestor for which the {@code predicate} returns true.
+ * @param {Node} node The node to check.
+ * @param {function(Node) : boolean} predicate The function that tests the
+ *     nodes.
+ * @return {Node} The found ancestor or null if not found.
+ */
+function findAncestor(node, predicate) {
+  var last = false;
+  while (node != null && !(last = predicate(node))) {
+    node = node.parentNode;
+  }
+  return last ? node : null;
+}
+
+function swapDomNodes(a, b) {
+  var afterA = a.nextSibling;
+  if (afterA == b) {
+    swapDomNodes(b, a);
+    return;
+  }
+  var aParent = a.parentNode;
+  b.parentNode.replaceChild(a, b);
+  aParent.insertBefore(b, afterA);
 }
 
 /*
