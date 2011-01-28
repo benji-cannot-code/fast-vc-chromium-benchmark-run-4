bb6d0bf100ef2e858778373ac7e5792354b44c23
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 06:42:23 +0000

    2011-01-27  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Adam Barth.
    
            Update the expected results for test-webkitpy after r76916.
    
            Include "config.h" in the expected results as well.
    
            * Scripts/webkit2/messages_unittest.py:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76919 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages_unittest.py
+++ third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages_unittest.py
@@ -48,6 +48,8 @@ _messages_file_contents = """# Copyright (C) 2010 Apple Inc. All rights reserved
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include "config.h"
+
 #if ENABLE(WEBKIT2)
 
 messages -> WebPage {
@@ -245,6 +247,8 @@ _expected_header = """/*
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include "config.h"
 
 #ifndef WebPageMessages_h
 #define WebPageMessages_h
@@ -478,6 +482,8 @@ _expected_receiver_implementation = """/*
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include "config.h"
 
 #if ENABLE(WEBKIT2)
 
