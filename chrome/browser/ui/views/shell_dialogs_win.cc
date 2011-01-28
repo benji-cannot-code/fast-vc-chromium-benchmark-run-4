27c12eec9ac6eea9d260658ebf32904110d7e802
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 22:33:11 +0000

    Use FilePath::Extension instead of the deprecated file_util::GetFileExtensionFromPath. Part 2.
    Use it in one place in shell_dialogs_win.cc
    
    BUG=24672
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/6269023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73031 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/shell_dialogs_win.cc
+++ chrome/browser/ui/views/shell_dialogs_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <set>
 
+#include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/message_loop.h"
 #include "base/scoped_comptr_win.h"
@@ -349,12 +350,11 @@ bool SaveFileAsWithFilter(HWND owner,
 bool SaveFileAs(HWND owner,
                 const std::wstring& suggested_name,
                 std::wstring* final_name) {
-  std::wstring file_ext = file_util::GetFileExtensionFromPath(suggested_name);
-  file_ext.insert(0, L"*.");
+  std::wstring file_ext = FilePath(suggested_name).Extension().insert(0, L"*");
   std::wstring filter = FormatFilterForExtensions(
-    std::vector<std::wstring>(1, file_ext),
-    std::vector<std::wstring>(),
-    true);
+      std::vector<std::wstring>(1, file_ext),
+      std::vector<std::wstring>(),
+      true);
   unsigned index = 1;
   return SaveFileAsWithFilter(owner,
                               suggested_name,
