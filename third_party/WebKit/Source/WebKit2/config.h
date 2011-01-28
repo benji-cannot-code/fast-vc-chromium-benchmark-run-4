b80dccf02822f7500882a759d7b7ac9dffa18560
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 07:25:26 +0000

    2011-01-27  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Unreviewed build fix for Mac Intel Debug after r76916.
    
            Fix macro redefinition by guarding the redefined macros.
    
            Check if the macro is already defined before defininig it.
            c++config.h has the same macros with the same values.
    
            * config.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/config.h
+++ third_party/WebKit/Source/WebKit2/config.h
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/FastMalloc.h>
 #endif
 
-#ifdef __cplusplus
+#if defined(__cplusplus) && !defined(EXTERN_C_BEGIN) && !defined(EXTERN_C_END)
 #define EXTERN_C_BEGIN extern "C" {
 #define EXTERN_C_END }
 #else
