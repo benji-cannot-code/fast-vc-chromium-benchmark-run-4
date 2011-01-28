ca65ad6ec2040c5b43545c71fcf87e1a4720e708
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 20:16:02 +0000

    Change BinarySemaphore to wrap an auto-reset Win32 event on Windows
    Fixes <http://webkit.org/b/53208> <rdar://problem/8922490>.
    
    Reviewed by Dave Hyatt.
    
    Source/WebKit2:
    
    * Platform/CoreIPC/BinarySemaphore.cpp: Wrap this implementation in #if !PLATFORM(WIN).
    
    * Platform/CoreIPC/BinarySemaphore.h: Make the Windows implementation have a single HANDLE
    member that holds the event.
    
    * Platform/CoreIPC/win/BinarySemaphoreWin.cpp: Copied from Source/WebKit2/Platform/CoreIPC/BinarySemaphore.cpp.
    (CoreIPC::BinarySemaphore::BinarySemaphore): Create our event.
    (CoreIPC::BinarySemaphore::~BinarySemaphore): Destory our event.
    (CoreIPC::BinarySemaphore::signal): Signal the event.
    (CoreIPC::BinarySemaphore::wait): Convert the absolute time to a wait interval, then wait
    for the event to be signaled or for the interval to elapse.
    
    * win/WebKit2.vcproj: Added BinarySemaphoreWin.cpp. Also let VS have its way with the file.
    
    Source/JavaScriptCore:
    
    Extract code to convert a WTF absolute time to a Win32 wait interval into a separate
    function
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def: Export the new function.
    
    * wtf/ThreadingPrimitives.h: Declare the new function.
    
    * wtf/ThreadingWin.cpp:
    (WTF::ThreadCondition::timedWait): Moved code to convert the absolute time to a wait
    interval from here...
    (WTF::absoluteTimeToWaitTimeoutInterval): ...to here.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/CoreIPC/BinarySemaphore.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/CoreIPC/BinarySemaphore.cpp
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace CoreIPC {
 
+#if !PLATFORM(WIN)
+
 BinarySemaphore::BinarySemaphore()
     : m_isSet(false)
 {
@@ -62,5 +64,6 @@ bool BinarySemaphore::wait(double absoluteTime)
     return true;
 }
 
+#endif // !PLATFORM(WIN)
 
 } // namespace CoreIPC
