5e9d214aa4575da5b048d1b4ca47e187f1f2e944
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 22:48:35 +0000

    https://bugs.webkit.org/show_bug.cgi?id=53330 Need InjectedBundle API to get the response MIMEType for a URL
    Reviewed by John Sullivan.
    
    * WebProcess/InjectedBundle/API/c/WKBundleFrame.cpp:
    (WKBundleFrameCopyMIMETypeForResourceWithURL):
    * WebProcess/InjectedBundle/API/c/WKBundleFrame.h:
    
    Get the MIMEType from the in-memory cache, or cachedResponseMIMETypeForURL() if not available:
    * WebProcess/WebPage/WebFrame.cpp:
    (WebKit::WebFrame::mimeTypeForResourceWithURL):
    * WebProcess/WebPage/WebFrame.h:
    
    Get the MIMEType from the platform's disk cache if available:
    * WebProcess/WebPage/WebPage.h:
    * WebProcess/WebPage/mac/WebPageMac.mm:
    (WebKit::WebPage::cachedResponseMIMETypeForURL):
    * WebProcess/WebPage/qt/WebPageQt.cpp:
    (WebKit::WebPage::cachedResponseMIMETypeForURL):
    * WebProcess/WebPage/win/WebPageWin.cpp:
    (WebKit::WebPage::cachedResponseMIMETypeForURL):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76993 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
@@ -272,6 +272,12 @@ bool WebPage::platformHasLocalDataForURL(const WebCore::KURL&)
     return false;
 }
 
+String WebPage::cachedResponseMIMETypeForURL(const WebCore::KURL&)
+{
+    // FIXME: Implement
+    return String();
+}
+
 bool WebPage::canHandleRequest(const WebCore::ResourceRequest&)
 {
     // FIXME: Implement
