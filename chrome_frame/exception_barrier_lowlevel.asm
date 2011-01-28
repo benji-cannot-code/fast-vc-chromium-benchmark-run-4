5b8540b2d59793dd2361d6ea8215acdc6a9c7b82
jeanluc@chromium.org <jeanluc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 18:27:19 +0000

    MASM 10.0 fixes a problem introduced in 8.0 with extra underscore added.
    BUG=71145
    TEST=Compile & link cleanly
    
    Review URL: http://codereview.chromium.org/6352023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72981 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/exception_barrier_lowlevel.asm
+++ chrome_frame/exception_barrier_lowlevel.asm
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ;
 ; MASM 8.0 inserts an additional leading underscore in front of names
 ; and this is an attempted fix until we understand why.
-IF @version LT 800
+; MASM 10.0 fixed this.
+IF @version LT 800 OR @version GE 1000
 _ExceptionBarrierHandler PROTO
 .SAFESEH _ExceptionBarrierHandler
 _ExceptionBarrierReportOnlyModuleHandler PROTO
