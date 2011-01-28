2512caa8a6405bea0ea1ee88f4aeb6f9c05be883
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 15:39:29 +0000

    Fixes OpenSSL build: adds missing include file
    BUG=none
    TEST=compiles with use_openssl
    
    Review URL: http://codereview.chromium.org/6340020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72962 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_openssl_util.cc
+++ net/base/x509_openssl_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/x509_openssl_util.h"
 
+#include <algorithm>
+
 #include "base/logging.h"
 #include "base/string_piece.h"
 #include "base/string_util.h"
