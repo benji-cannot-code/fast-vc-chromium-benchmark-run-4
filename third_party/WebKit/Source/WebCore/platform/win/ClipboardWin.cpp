603ea5d78ef07aa31ca117f3f3ad8395a141ca79
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 19:46:19 +0000

    Some drag and drop tests fail since r76824 https://bugs.webkit.org/show_bug.cgi?id=53304
    Reviewed by Adam Roben.
    
    Source/WebCore: 
    
    There were '||' instead of '&&' in the checks for valid
    clipboard content.
            
    * platform/win/ClipboardWin.cpp:
    (WebCore::ClipboardWin::getData):
    (WebCore::ClipboardWin::types):
    (WebCore::ClipboardWin::files):
    
    LayoutTests: 
    
    Removing platform-specific tests after fixing the cause of the failure.
            
    * platform/win/editing/pasteboard/get-data-text-plain-paste-expected.txt: Removed.
    * platform/win/editing/pasteboard/onpaste-text-html-expected.txt: Removed.
    * platform/win/fast/events/drag-and-drop-dataTransfer-types-nocrash-expected.txt: Removed.
    * platform/win/fast/events/ondrop-text-html-expected.txt: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/ClipboardWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/win/ClipboardWin.cpp
@@ -454,7 +454,7 @@ void ClipboardWin::clearAllData()
 String ClipboardWin::getData(const String& type, bool& success) const
 {     
     success = false;
-    if (policy() != ClipboardReadable || !m_dataObject || m_dragDataMap.isEmpty())
+    if (policy() != ClipboardReadable || (!m_dataObject && m_dragDataMap.isEmpty()))
         return "";
 
     ClipboardDataType dataType = clipboardTypeFromMIMEType(type);
@@ -522,7 +522,7 @@ HashSet<String> ClipboardWin::types() const
     if (policy() != ClipboardReadable && policy() != ClipboardTypesReadable)
         return results;
 
-    if (!m_dataObject || m_dragDataMap.isEmpty())
+    if (!m_dataObject && m_dragDataMap.isEmpty())
         return results;
 
     if (m_dataObject) {
@@ -560,7 +560,7 @@ PassRefPtr<FileList> ClipboardWin::files() const
     if (policy() != ClipboardReadable && policy() != ClipboardTypesReadable)
         return files.release();
 
-    if (!m_dataObject || m_dragDataMap.isEmpty())
+    if (!m_dataObject && m_dragDataMap.isEmpty())
         return files.release();
 
     if (m_dataObject) {
