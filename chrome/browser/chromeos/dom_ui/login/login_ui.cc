a0d7b26135e0a42ccbed847fa754dd1873ea00f9
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-28 17:56:35 +0000

    Add system shutdown functionality to DOM Login.
    Added functionality to LoginUI to support ShutdownSystem and added link to call
    function from login page. This patch also reenables chrome://login on touchui
    non-chromeos builds, since that is used for interface testing development.
    
    Patch from Ryan Harrison <rharrison@chromium.org>
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6282010
    Patch from Ryan Harrison <rharrison@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72972 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/login/login_ui.cc
+++ chrome/browser/chromeos/dom_ui/login/login_ui.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/cros/power_library.h"
 #include "chrome/browser/chromeos/dom_ui/login/authenticator_facade_cros.h"
 #else
 #include "chrome/browser/chromeos/dom_ui/login/authenticator_facade_stub.h"
@@ -84,6 +86,10 @@ void LoginUIHandler::RegisterMessages() {
       "AuthenticateUser",
       NewCallback(this,
                   &LoginUIHandler::HandleAuthenticateUser));
+  dom_ui_->RegisterMessageCallback(
+      "ShutdownSystem",
+      NewCallback(this,
+                  &LoginUIHandler::HandleShutdownSystem));
 }
 
 void LoginUIHandler::HandleAuthenticateUser(const ListValue* args) {
@@ -114,6 +120,19 @@ void LoginUIHandler::HandleLaunchIncognito(const ListValue* args) {
   logged_in->NewTab();
   logged_in->window()->Show();
   login_browser->CloseWindow();
+}
+
+void LoginUIHandler::HandleShutdownSystem(const ListValue* args) {
+#if defined(OS_CHROMEOS)
+  DCHECK(CrosLibrary::Get()->EnsureLoaded());
+  CrosLibrary::Get()->GetPowerLibrary()->RequestShutdown();
+#else
+  // When were are not running in cros we are just shutting the browser instead
+  // of trying to shutdown the system
+  Profile* profile = profile_operations_->GetDefaultProfileByPath();
+  Browser* browser = browser_operations_->GetLoginBrowser(profile);
+  browser->CloseWindow();
+#endif
 }
 
 void LoginUIHandler::OnLoginFailure(const LoginFailure& failure) {
