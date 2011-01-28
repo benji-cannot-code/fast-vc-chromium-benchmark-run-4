92328cfad70891012a4e64757ac168577775212d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-28 02:03:58 +0000

    2011-01-27  Finnur Thorarinsson  <finnur.webkit@gmail.com>
            Reviewed by Ojan Vafai.
    
            This addresses a regression introduced by r75784, as described in:
            https://bugs.webkit.org/show_bug.cgi?id=53176
    
            The problem is that originalEndContainer and originalEndOffset was
            meant to capture the end of the document, but was being assigned after
            a call to searchRange->setStart() (when scoping is restarted), which can
            result in the Range being collapsed to start (thereby assigning the
            wrong value to the Container/Offset pair).
    
            This code is never triggered by layout tests, but I'm adding a regressiono
            test on the Chromium side for this.
    
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::scopeStringMatches):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@76889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -1545,6 +1545,9 @@ void WebFrameImpl::scopeStringMatches(int identifier,
 
     RefPtr<Range> searchRange(rangeOfContents(frame()->document()));
 
+    Node* originalEndContainer = searchRange->endContainer();
+    int originalEndOffset = searchRange->endOffset();
+
     ExceptionCode ec = 0, ec2 = 0;
     if (m_resumeScopingFromRange.get()) {
         // This is a continuation of a scoping operation that timed out and didn't
@@ -1558,9 +1561,6 @@ void WebFrameImpl::scopeStringMatches(int identifier,
             return;
         }
     }
-
-    Node* originalEndContainer = searchRange->endContainer();
-    int originalEndOffset = searchRange->endOffset();
 
     // This timeout controls how long we scope before releasing control.  This
     // value does not prevent us from running for longer than this, but it is
