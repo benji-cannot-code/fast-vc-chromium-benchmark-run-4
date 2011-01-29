ad2363880fe74546b2df3f34bfebe511cf869277
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-29 00:55:26 +0000

    views: Move the implementation of ButtonExample from the header to the source file.
    BUG=None
    TEST=run out/Debug/views_examples, everything should works as before.
    
    Review URL: http://codereview.chromium.org/6288017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73049 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/button_example.h
+++ views/examples/button_example.h
@@ -7,37 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VIEWS_EXAMPLES_BUTTON_EXAMPLE_H_
 #pragma once
 
-#include "base/string_util.h"
+#include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "views/controls/button/text_button.h"
 #include "views/examples/example_base.h"
-#include "views/layout/fill_layout.h"
-#include "views/view.h"
+
+namespace views {
+class View;
+}
 
 namespace examples {
 
 // ButtonExample simply counts the number of clicks.
 class ButtonExample : public ExampleBase, public views::ButtonListener {
  public:
-  explicit ButtonExample(ExamplesMain* main) : ExampleBase(main), count_(0) {
-    button_ = new views::TextButton(this, L"Button");
-  }
+  explicit ButtonExample(ExamplesMain* main);
+  virtual ~ButtonExample();
 
-  virtual ~ButtonExample() {}
-
-  virtual std::wstring GetExampleTitle() {
-    return L"Text Button";
-  }
-
-  virtual void CreateExampleView(views::View* container) {
-    container->SetLayoutManager(new views::FillLayout);
-    container->AddChildView(button_);
-  }
+  // Overridden from ExampleBase:
+  virtual std::wstring GetExampleTitle() OVERRIDE;
+  virtual void CreateExampleView(views::View* container) OVERRIDE;
 
  private:
-  // ButtonListner implementation.
-  virtual void ButtonPressed(views::Button* sender, const views::Event& event) {
-    PrintStatus(L"Pressed! count:%d", ++count_);
-  }
+  // Overridden from views::ButtonListener:
+  virtual void ButtonPressed(views::Button* sender,
+                             const views::Event& event) OVERRIDE;
 
   // The only control in this test.
   views::TextButton* button_;
--- views/views.gyp
+++ views/views.gyp
@@ -521,6 +521,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '..',
       ],
       'sources': [
+        'examples/button_example.cc',
         'examples/button_example.h',
         'examples/combobox_example.cc',
         'examples/combobox_example.h',
