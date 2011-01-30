bf4b4cd6843865b36c2a94bb4302265c9038c67b
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-30 19:54:56 +0000

    Unreviewed, rolling out r77025. http://trac.webkit.org/changeset/77025 https://bugs.webkit.org/show_bug.cgi?id=53401
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-01-30
    It made js1_5/Regress/regress-159334.js fail on 64 bit Linux
    (Requested by Ossy on #webkit).
    
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    (JSC::FreeListEntry::FreeListEntry):
    (JSC::AVLTreeAbstractorForFreeList::get_less):
    (JSC::AVLTreeAbstractorForFreeList::set_less):
    (JSC::AVLTreeAbstractorForFreeList::get_greater):
    (JSC::AVLTreeAbstractorForFreeList::set_greater):
    (JSC::AVLTreeAbstractorForFreeList::get_balance_factor):
    (JSC::AVLTreeAbstractorForFreeList::set_balance_factor):
    (JSC::AVLTreeAbstractorForFreeList::null):
    (JSC::AVLTreeAbstractorForFreeList::compare_key_key):
    (JSC::AVLTreeAbstractorForFreeList::compare_key_node):
    (JSC::AVLTreeAbstractorForFreeList::compare_node_node):
    (JSC::reverseSortFreeListEntriesByPointer):
    (JSC::reverseSortCommonSizedAllocations):
    (JSC::FixedVMPoolAllocator::release):
    (JSC::FixedVMPoolAllocator::reuse):
    (JSC::FixedVMPoolAllocator::addToFreeList):
    (JSC::FixedVMPoolAllocator::coalesceFreeSpace):
    (JSC::FixedVMPoolAllocator::FixedVMPoolAllocator):
    (JSC::FixedVMPoolAllocator::alloc):
    (JSC::FixedVMPoolAllocator::free):
    (JSC::FixedVMPoolAllocator::isValid):
    (JSC::FixedVMPoolAllocator::allocInternal):
    (JSC::FixedVMPoolAllocator::isWithinVMPool):
    (JSC::FixedVMPoolAllocator::addToCommittedByteCount):
    (JSC::ExecutableAllocator::committedByteCount):
    (JSC::maybeModifyVMPoolSize):
    (JSC::ExecutableAllocator::isValid):
    (JSC::ExecutableAllocator::underMemoryPressure):
    (JSC::ExecutablePool::systemAlloc):
    (JSC::ExecutablePool::systemRelease):
    * wtf/PageReservation.h:
    (WTF::PageReservation::PageReservation):
    (WTF::PageReservation::commit):
    (WTF::PageReservation::decommit):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77090 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/PageReservation.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/PageReservation.h
@@ -58,9 +58,11 @@ namespace WTF {
 class PageReservation : private PageBlock {
 public:
     PageReservation()
-        : m_committed(0)
-        , m_writable(false)
+        : m_writable(false)
         , m_executable(false)
+#ifndef NDEBUG
+        , m_committed(0)
+#endif
     {
     }
 
@@ -82,7 +84,9 @@ public:
         ASSERT(isPageAligned(size));
         ASSERT(contains(start, size));
 
+#ifndef NDEBUG
         m_committed += size;
+#endif
         OSAllocator::commit(start, size, m_writable, m_executable);
     }
 
@@ -93,13 +97,10 @@ public:
         ASSERT(isPageAligned(size));
         ASSERT(contains(start, size));
 
+#ifndef NDEBUG
         m_committed -= size;
+#endif
         OSAllocator::decommit(start, size);
-    }
-
-    size_t committed()
-    {
-        return m_committed;
     }
 
     static PageReservation reserve(size_t size, OSAllocator::Usage usage = OSAllocator::UnknownUsage, bool writable = true, bool executable = false)
@@ -126,15 +127,19 @@ public:
 private:
     PageReservation(void* base, size_t size, bool writable, bool executable)
         : PageBlock(base, size)
-        , m_committed(0)
         , m_writable(writable)
         , m_executable(executable)
+#ifndef NDEBUG
+        , m_committed(0)
+#endif
     {
     }
 
-    size_t m_committed;
     bool m_writable;
     bool m_executable;
+#ifndef NDEBUG
+    size_t m_committed;
+#endif
 };
 
 }
