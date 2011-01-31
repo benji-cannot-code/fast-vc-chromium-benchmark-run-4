f3581caaa21b413161ef2bab0326b8b3073f6ce5
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-31 11:10:30 +0000

    2011-01-31  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed. Fix Chromium compilation on Linux.
    
            * platform/graphics/ShadowBlur.cpp: added PLATFORM(CHROMIUM) guard
            * platform/graphics/ShadowBlur.h: added missing ColorSpace.h header include
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77119 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ShadowBlur.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ShadowBlur.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ShadowBlur_h
 
 #include "Color.h"
+#include "ColorSpace.h"
 #include "FloatRect.h"
 #include "RoundedIntRect.h"
 #include <wtf/Noncopyable.h>
