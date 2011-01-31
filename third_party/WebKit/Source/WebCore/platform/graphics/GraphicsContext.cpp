31f077be557f7008be301a60a8456324da3848d0
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-31 06:02:01 +0000

    2011-01-30  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Sam Weinig.
    
            Enhance ShadowBlur to render inset box shadows
            https://bugs.webkit.org/show_bug.cgi?id=51567
    
            Use ShadowBlur for inset box-shadows with CG. It
            currently lacks a tiled version, but is still much
            faster than CG shadows.
    
            Test: fast/box-shadow/inset-box-shadow-radius.html
    
            * platform/graphics/ShadowBlur.cpp:
            * platform/graphics/ShadowBlur.h: New method for inset
            shadows.
            (WebCore::ShadowBlur::drawInsetShadow):
    
            * platform/graphics/GraphicsContext.cpp: #ifdef out
            fillRectWithRoundedHole() for CG.
    
            * platform/graphics/cg/GraphicsContextCG.cpp:
            (WebCore::GraphicsContext::fillRectWithRoundedHole): If there's
            a shadow with a radius > 0, use ShadowBlur.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/GraphicsContext.cpp
@@ -611,6 +611,7 @@ void GraphicsContext::fillRoundedRect(const RoundedIntRect& rect, const Color& c
     fillRoundedRect(rect.rect(), rect.radii().topLeft(), rect.radii().topRight(), rect.radii().bottomLeft(), rect.radii().bottomRight(), color, colorSpace);
 }
 
+#if !PLATFORM(CG)
 void GraphicsContext::fillRectWithRoundedHole(const IntRect& rect, const RoundedIntRect& roundedHoleRect, const Color& color, ColorSpace colorSpace)
 {
     if (paintingDisabled())
@@ -636,6 +637,7 @@ void GraphicsContext::fillRectWithRoundedHole(const IntRect& rect, const Rounded
     setFillRule(oldFillRule);
     setFillColor(oldFillColor, oldFillColorSpace);
 }
+#endif
 
 void GraphicsContext::setCompositeOperation(CompositeOperator compositeOperation)
 {
