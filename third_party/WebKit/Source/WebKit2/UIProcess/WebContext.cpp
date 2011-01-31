273823ac83c5dfacf9084a1beb68dd488220100f
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-31 03:36:07 +0000

    2011-01-30  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Maciej Stachowiak.
    
            Fixed crash on window close (maybe crash on launch?) due to missing
            frame null checks
            https://bugs.webkit.org/show_bug.cgi?id=53408
    
            * UIProcess/WebContext.cpp:
            (WebKit::WebContext::didNavigateWithNavigationData):
            (WebKit::WebContext::didPerformClientRedirect):
            (WebKit::WebContext::didPerformServerRedirect):
            (WebKit::WebContext::didUpdateHistoryTitle): Check for NULL, since we're
            calling a function that can return it.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
@@ -300,7 +300,7 @@ void WebContext::didReceiveSynchronousMessageFromInjectedBundle(const String& me
 void WebContext::didNavigateWithNavigationData(uint64_t pageID, const WebNavigationDataStore& store, uint64_t frameID) 
 {
     WebFrameProxy* frame = m_process->webFrame(frameID);
-    if (!frame->page())
+    if (!frame || !frame->page())
         return;
     
     m_historyClient.didNavigateWithNavigationData(this, frame->page(), store, frame);
@@ -309,7 +309,7 @@ void WebContext::didNavigateWithNavigationData(uint64_t pageID, const WebNavigat
 void WebContext::didPerformClientRedirect(uint64_t pageID, const String& sourceURLString, const String& destinationURLString, uint64_t frameID)
 {
     WebFrameProxy* frame = m_process->webFrame(frameID);
-    if (!frame->page())
+    if (!frame || !frame->page())
         return;
     
     m_historyClient.didPerformClientRedirect(this, frame->page(), sourceURLString, destinationURLString, frame);
@@ -318,7 +318,7 @@ void WebContext::didPerformClientRedirect(uint64_t pageID, const String& sourceU
 void WebContext::didPerformServerRedirect(uint64_t pageID, const String& sourceURLString, const String& destinationURLString, uint64_t frameID)
 {
     WebFrameProxy* frame = m_process->webFrame(frameID);
-    if (!frame->page())
+    if (!frame || !frame->page())
         return;
     
     m_historyClient.didPerformServerRedirect(this, frame->page(), sourceURLString, destinationURLString, frame);
@@ -327,7 +327,7 @@ void WebContext::didPerformServerRedirect(uint64_t pageID, const String& sourceU
 void WebContext::didUpdateHistoryTitle(uint64_t pageID, const String& title, const String& url, uint64_t frameID)
 {
     WebFrameProxy* frame = m_process->webFrame(frameID);
-    if (!frame->page())
+    if (!frame || !frame->page())
         return;
 
     m_historyClient.didUpdateHistoryTitle(this, frame->page(), title, url, frame);
