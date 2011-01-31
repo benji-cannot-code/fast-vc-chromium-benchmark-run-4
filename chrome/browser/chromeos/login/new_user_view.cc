6b70bc1ec4813b7b9763424cb2e3d2662033c510
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-31 11:47:49 +0000

    [cros] Refactoring login controllers.
    1. Move trusted settings check from UC to EUC.
    2. Move SetStatusAreaEnabled calls from UC to EUC.
    3. Rename LoginOffTheRecord to GuestLogin in couple of places.
    4. Sort method implementations in EUC.
    5. Introduce LoginDisplay / ViewsLoginDisplay and move Views login UI specific code there (working with UserControllers).
    6. Cleanup some CreateAccount screen callbacks; AddStartUrl methods.
    
    existing_user_view.cc
    guest_user_view.cc
    new_user_view.*
    are just a few removal/renames of methods.
    
    TBD:
    1. Move common code out of UserController (generating user tooltip), tracking user picture load.
    2. Move BackgroundView to ViewsLoginDisplay.
    
    BUG=chromium-os:11268
    TEST=bots, manual
    
    Review URL: http://codereview.chromium.org/6394002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73138 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/new_user_view.cc
+++ chrome/browser/chromeos/login/new_user_view.cc
@@ -215,7 +215,7 @@ bool NewUserView::AcceleratorPressed(const views::Accelerator& accelerator) {
   } else if (accelerator == accel_focus_pass_) {
     password_field_->RequestFocus();
   } else if (accelerator == accel_login_off_the_record_) {
-    delegate_->OnLoginOffTheRecord();
+    delegate_->OnLoginAsGuest();
   } else if (accelerator == accel_toggle_accessibility_) {
     WizardAccessibilityHelper::GetInstance()->ToggleAccessibility(this);
   } else {
@@ -484,7 +484,7 @@ void NewUserView::LinkActivated(views::Link* source, int event_flags) {
   if (source == create_account_link_) {
     delegate_->OnCreateAccount();
   } else if (source == guest_link_) {
-    delegate_->OnLoginOffTheRecord();
+    delegate_->OnLoginAsGuest();
   }
 }
 
@@ -581,3 +581,4 @@ void NewUserView::InitLink(views::Link** link) {
 }
 
 }  // namespace chromeos
+
