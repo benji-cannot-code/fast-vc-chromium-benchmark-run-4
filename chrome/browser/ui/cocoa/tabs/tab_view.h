0128d867bb4d9c579fd68d68d296992417d4da3e
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-31 18:43:09 +0000

    [mac accessibility] Fix an uncaught exception that would occur if VoiceOver is enabled.
    BUG=71245
    TEST=Verify that mouse-over and mouse-down on tabs do not cause uncaught exception w/ VoiceOver enabled.
    Review URL: http://codereview.chromium.org/6312021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73166 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_view.h
+++ chrome/browser/ui/cocoa/tabs/tab_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -48,9 +48,6 @@ enum AlertState {
   // TODO(rohitrao): Add this button to a CoreAnimation layer so we can fade it
   // in and out on mouseovers.
   IBOutlet HoverCloseButton* closeButton_;
-
-  // See awakeFromNib for purpose.
-  scoped_nsobject<HoverCloseButton> closeButtonRetainer_;
 
   BOOL closing_;
 
