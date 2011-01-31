1f46a559d1936144bf5c6755065f6655490c221c
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-31 22:35:17 +0000

    Revert 73197 - Support Mozilla's GUID for ISimpleDOM and add a unit test for it.
    BUG=48185
    TEST=Adds new test.
    
    Review URL: http://codereview.chromium.org/6409016
    
    TBR=dmazzoni@chromium.org
    Review URL: http://codereview.chromium.org/6260045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/browser_accessibility_win.cc
+++ chrome/browser/accessibility/browser_accessibility_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,15 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/escape.h"
 
 using webkit_glue::WebAccessibility;
-
-// The GUID for the ISimpleDOM service is not defined in the IDL files.
-// This is taken directly from the Mozilla sources
-// (accessible/src/msaa/nsAccessNodeWrap.cpp) and it's also documented at:
-// http://developer.mozilla.org/en/Accessibility/AT-APIs/ImplementationFeatures/MSAA
-
-const GUID GUID_ISimpleDOM = {
-    0x0c539790, 0x12e4, 0x11cf,
-    0xb6, 0x61, 0x00, 0xaa, 0x00, 0x4c, 0xd6, 0xd8};
 
 // static
 BrowserAccessibility* BrowserAccessibility::Create() {
@@ -1080,8 +1071,7 @@ STDMETHODIMP BrowserAccessibilityWin::QueryService(
       guidService == IID_IAccessibleText ||
       guidService == IID_ISimpleDOMDocument ||
       guidService == IID_ISimpleDOMNode ||
-      guidService == IID_ISimpleDOMText ||
-      guidService == GUID_ISimpleDOM) {
+      guidService == IID_ISimpleDOMText) {
     return QueryInterface(riid, object);
   }
 
