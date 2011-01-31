829c844c01aa3c5286f2a9d34ba5ac8942eae512
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-01-31 23:11:21 +0000

    2011-01-31  Oliver Hunt  <oliver@apple.com>
            Reviewed by Sam Weinig.
    
            Bogus callframe during stack unwinding
            https://bugs.webkit.org/show_bug.cgi?id=53454
    
            Add test
    
            * fast/js/invalid-callframe-during-unwind-expected.txt: Added.
            * fast/js/invalid-callframe-during-unwind.html: Added.
            * fast/js/script-tests/invalid-callframe-during-unwind.js: Added.
            (testUnwind):
    2011-01-31  Oliver Hunt  <oliver@apple.com>
    
            Reviewed by Sam Weinig.
    
            Bogus callframe during stack unwinding
            https://bugs.webkit.org/show_bug.cgi?id=53454
    
            Trying to access a callframe's globalData after destroying its
            ScopeChain is not a good thing.  While we could access the
            globalData directly through the (known valid) scopechain we're
            holding on to, it feels fragile.  Instead we push the valid
            ScopeChain onto the callframe again to ensure that the callframe
            itself remains valid.
    
            * interpreter/Interpreter.cpp:
            (JSC::Interpreter::unwindCallFrame):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/invalid-callframe-during-unwind.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test to ensure we have a valid callframe midway through unwinding");
+
+function testUnwind(){with({}){ arguments; throw "threw successfully";}}
+
+shouldThrow("testUnwind()")
+
+
+var successfullyParsed = true;
