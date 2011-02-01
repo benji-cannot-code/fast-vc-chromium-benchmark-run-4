3a908ff8d81f980932ace457b14d2638d9d729fa
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-01 22:52:02 +0000

    Fix Qt bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@77316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/Api/qwebframe.cpp
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebframe.cpp
@@ -1306,7 +1306,7 @@ QPoint QWebFrame::pos() const
     if (!d->frame->view())
         return QPoint();
 
-    return d->frame->view()->frameRect().topLeft();
+    return d->frame->view()->frameRect().location();
 }
 
 /*!
