52a63932437331d9aceae2ec05322b936d1fde44
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 21:45:18 +0000

    Make the gtk download shelf autoclose on mouse out.
    BUG=27797
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6249022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/ui/gtk/tabs/tab_strip_gtk.cc
@@ -1229,6 +1229,9 @@ void TabStripGtk::DidProcessEvent(GdkEvent* event) {
   }
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// TabStripGtk, NotificationObserver implementation:
+
 void TabStripGtk::Observe(NotificationType type,
                           const NotificationSource& source,
                           const NotificationDetails& details) {
