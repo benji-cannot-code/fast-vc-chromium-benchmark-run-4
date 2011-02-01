e2be3c5645d8aaef283d40132158e18239829db5
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 18:35:08 +0000

    Reapply and fix r73019. BalloonViewImpl has a different implemementation on linux_gtk, linux_views, and chromeos, but everyone uses the same class name. sizeof(linux_views BVI) > sizeof(linux_gtk BVI). Previously, the GTK version allocated on object of linux_views BVI, and just had some uninitialized memory at the end. Switching these around by changing the includes, the linux_views implementation wrote past the sizeof the linux_gtk BVI. Fix by #ifdefing on TOOLKIT.
    Fix style problems in the Balloon implementation on linux in preparation for
    turning on chrome-style bot.
    
    - Rename file that had all BalloonCollectionImpl code from
      balloon_collection.cc to balloon_collection_impl.cc so I could add a real
      balloon_collection.cc for deinlining.
    - GTK implementation files shouldn't depend on views/ code.
    
    BUG=none
    TEST=compiles
    Original Review URL: http://codereview.chromium.org/6400006
    
    Review URL: http://codereview.chromium.org/6260040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/balloon_collection.h
+++ chrome/browser/notifications/balloon_collection.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,11 +49,9 @@ class BalloonCollection {
 
   static BalloonCollection* Create();
 
-  BalloonCollection()
-      : space_change_listener_(NULL) {
-  }
+  BalloonCollection();
 
-  virtual ~BalloonCollection() {}
+  virtual ~BalloonCollection();
 
   // Adds a new balloon for the specified notification.
   virtual void Add(const Notification& notification,
