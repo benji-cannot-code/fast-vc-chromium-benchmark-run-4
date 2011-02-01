f12e5cec0b27e2c240ad94cae88c7b321a5797c4
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-01 23:50:58 +0000

    Unreviewed Qt buildfix after r77286.
    https://bugs.webkit.org/show_bug.cgi?id=53520
    Remove the physical terminology from IntRect and FloatRect.
    
    Source/WebCore:
    
    * platform/graphics/TiledBackingStore.cpp:
    (WebCore::TiledBackingStore::createTiles):
    
    Source/WebKit/qt:
    
    * WebCoreSupport/PopupMenuQt.cpp:
    (WebCore::PopupMenuQt::show):
    
    Source/WebKit2:
    
    * UIProcess/qt/ChunkedUpdateDrawingAreaProxyQt.cpp:
    (WebKit::ChunkedUpdateDrawingAreaProxy::drawUpdateChunkIntoBackingStore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77328 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PopupMenuQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PopupMenuQt.cpp
@@ -110,7 +110,7 @@ void PopupMenuQt::show(const IntRect& rect, FrameView* view, int index)
 
     if (QtFallbackWebPopup* fallback = qobject_cast<QtFallbackWebPopup*>(m_popup)) {
         QRect geometry(rect);
-        geometry.moveTopLeft(view->contentsToWindow(rect.topLeft()));
+        geometry.moveTopLeft(view->contentsToWindow(rect.location()));
         fallback->setGeometry(geometry);
         fallback->setFont(m_popupClient->menuStyle().font().font());
     }
