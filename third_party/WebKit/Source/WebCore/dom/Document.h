4379b9515648cd1a16559deb25b680066dd926ae
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-01 10:41:19 +0000

    2011-02-01  Roland Steiner  <rolandsteiner@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Bug 53289 - DOM: Move DocumentOrderedMap from Document into separate files
            https://bugs.webkit.org/show_bug.cgi?id=53289
    
            Moving the nested class DocumentOrderedMap from Document into separate files,
            updating code where necessary.
    
            No new tests. (refactoring)
    
            * Android.mk:
            * CMakeLists.txt:
            * GNUMakefile.am:
            * WebCore.gypi:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * dom/Document.cpp:
            (WebCore::Document::getElementById):
            (WebCore::Document::getImageMap):
            * dom/Document.h:
            * dom/DocumentOrderedMap.cpp: Added.
            (WebCore::keyMatchesId):
            (WebCore::keyMatchesMapName):
            (WebCore::keyMatchesLowercasedMapName):
            (WebCore::DocumentOrderedMap::clear):
            (WebCore::DocumentOrderedMap::add):
            (WebCore::DocumentOrderedMap::remove):
            (WebCore::DocumentOrderedMap::get):
            (WebCore::DocumentOrderedMap::getElementById):
            (WebCore::DocumentOrderedMap::getElementByMapName):
            (WebCore::DocumentOrderedMap::getElementByLowercasedMapName):
            * dom/DocumentOrderedMap.h: Added.
            (WebCore::DocumentOrderedMap::contains):
            (WebCore::DocumentOrderedMap::containsMultiple):
            * dom/DOMAllInOne.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -34,15 +34,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Color.h"
 #include "ContainerNode.h"
 #include "DOMTimeStamp.h"
+#include "DocumentOrderedMap.h"
 #include "DocumentTiming.h"
 #include "QualifiedName.h"
 #include "ScriptExecutionContext.h"
 #include "Timer.h"
 #include "ViewportArguments.h"
 #include <wtf/FixedArray.h>
-#include <wtf/HashCountedSet.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
+#include <wtf/PassRefPtr.h>
 
 #if USE(JSC)
 #include <runtime/WeakGCMap.h>
@@ -1095,28 +1096,6 @@ protected:
     void clearXMLVersion() { m_xmlVersion = String(); }
 
 private:
-    class DocumentOrderedMap {
-    public:
-        void add(AtomicStringImpl*, Element*);
-        void remove(AtomicStringImpl*, Element*);
-        void clear();
-
-        bool contains(AtomicStringImpl*) const;
-        bool containsMultiple(AtomicStringImpl*) const;
-        template<bool keyMatches(AtomicStringImpl*, Element*)> Element* get(AtomicStringImpl*, const Document*) const;
-
-        void checkConsistency() const;
-
-    private:
-        typedef HashMap<AtomicStringImpl*, Element*> Map;
-
-        // We maintain the invariant that m_duplicateCounts is the count of all elements with a given key
-        // excluding the one referenced in m_map, if any. This means it one less than the total count
-        // when the first node with a given key is cached, otherwise the same as the total count.
-        mutable Map m_map;
-        mutable HashCountedSet<AtomicStringImpl*> m_duplicateCounts;
-    };
-
     friend class IgnoreDestructiveWriteCountIncrementer;
 
     void detachParser();
@@ -1411,16 +1390,6 @@ private:
     int m_nextRequestAnimationFrameCallbackId;
 #endif
 };
-
-inline bool Document::DocumentOrderedMap::contains(AtomicStringImpl* id) const
-{
-    return m_map.contains(id) || m_duplicateCounts.contains(id);
-}
-
-inline bool Document::DocumentOrderedMap::containsMultiple(AtomicStringImpl* id) const
-{
-    return m_duplicateCounts.contains(id);
-}
 
 inline bool Document::hasElementWithId(AtomicStringImpl* id) const
 {
