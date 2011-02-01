c5939b55bc783f0985bf4a99ab43f86c7c0b65a9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 11:40:04 +0000

    views: Move the implementation of more examples from header to source file.
    BUG=None
    TEST=run out/Debug/views_examples, everything should works as before.
    
    Review URL: http://codereview.chromium.org/6347030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73276 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/table_example.h
+++ views/examples/table_example.h
@@ -16,17 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/table/table_view_observer.h"
 #include "views/examples/example_base.h"
 #include "views/layout/fill_layout.h"
-
-using ui::TableModel;
-using ui::TableModelObserver; // TODO(beng): remove these
+#include "views/layout/grid_layout.h"
 
 namespace examples {
 
-class TableExample
-    : public ExampleBase,
-      public TableModel,
-      public views::ButtonListener,
-      public views::TableViewObserver {
+class TableExample : public ExampleBase,
+                     public ui::TableModel,
+                     public views::ButtonListener,
+                     public views::TableViewObserver {
  public:
   explicit TableExample(ExamplesMain* main) : ExampleBase(main) {
   }
@@ -96,7 +93,7 @@ class TableExample
     layout->AddView(column4_visible_checkbox_);
   }
 
-  // TableModel implementation:
+  // Overridden from ui::TableModel:
   virtual int RowCount() {
     return 10;
   }
@@ -116,10 +113,10 @@ class TableExample
     return row % 2 ? icon1 : icon2;
   }
 
-  virtual void SetObserver(TableModelObserver* observer) {
+  virtual void SetObserver(ui::TableModelObserver* observer) {
   }
 
-  // TableViewObserver implementation:
+  // Overridden from views::TableViewObserver:
   virtual void OnSelectionChanged() {
     PrintStatus(L"Selection changed");
   }
