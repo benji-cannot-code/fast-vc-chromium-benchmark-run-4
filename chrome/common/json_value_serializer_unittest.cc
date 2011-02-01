4d5b10a61619464e69d9d8fb5f8058495194fdcc
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 01:43:51 +0000

    Fix expectations. This fixes the failure from r73232.
    BUG=None
    TEST=json_value_serializer_unittest
    TBR=tony@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/json_value_serializer_unittest.cc
+++ chrome/common/json_value_serializer_unittest.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 TEST(JSONValueSerializerTest, Roundtrip) {
   const std::string original_serialization =
-    "{\"bool\":true,\"int\":42,\"list\":[1,2],\"null\":null,\"double\":3.14}";
+    "{\"bool\":true,\"double\":3.14,\"int\":42,\"list\":[1,2],\"null\":null}";
   JSONStringValueSerializer serializer(original_serialization);
   scoped_ptr<Value> root(serializer.Deserialize(NULL, NULL));
   ASSERT_TRUE(root.get());
@@ -64,10 +64,10 @@ TEST(JSONValueSerializerTest, Roundtrip) {
   const std::string pretty_serialization =
     "{" JSON_NEWLINE
     "   \"bool\": true," JSON_NEWLINE
+    "   \"double\": 3.14," JSON_NEWLINE
     "   \"int\": 42," JSON_NEWLINE
     "   \"list\": [ 1, 2 ]," JSON_NEWLINE
-    "   \"null\": null," JSON_NEWLINE
-    "   \"double\": 3.14" JSON_NEWLINE
+    "   \"null\": null" JSON_NEWLINE
     "}" JSON_NEWLINE;
 #undef JSON_NEWLINE
   ASSERT_EQ(pretty_serialization, test_serialization);
