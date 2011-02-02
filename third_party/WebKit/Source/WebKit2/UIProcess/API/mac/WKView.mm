fd0415fd9ac79ea60e717de0d41cd87b7456d0bc
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-02 01:09:34 +0000

    2011-02-01  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            Add a layer backed NSView for accelerated content
            https://bugs.webkit.org/show_bug.cgi?id=53555
    
            Rename the current _layerHostingView to _oldLayerHostingView and
            add a new _layerHostingView to be used with the new drawing area.
    
            * UIProcess/API/mac/WKView.mm:
            (-[WKView hitTest:]):
            (-[WKView _startAcceleratedCompositing:]):
            (-[WKView _stopAcceleratedCompositing]):
            (-[WKView _enterAcceleratedCompositingMode:]):
            (-[WKView _exitAcceleratedCompositingMode]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77338 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -108,8 +108,11 @@ typedef HashMap<String, ValidationVector> ValidationMap;
     id _trackingRectOwner;
     void* _trackingRectUserData;
 
+    RetainPtr<NSView> _layerHostingView;
+
+    // FIXME: Remove _oldLayerHostingView.
 #if USE(ACCELERATED_COMPOSITING)
-    NSView *_layerHostingView;
+    NSView *_oldLayerHostingView;
 #endif
 
     RetainPtr<id> _remoteAccessibilityChild;
@@ -1502,8 +1505,11 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
 - (NSView *)hitTest:(NSPoint)point
 {
     NSView *hitView = [super hitTest:point];
-#if USE(ACCELERATED_COMPOSITING)
     if (hitView && _data && hitView == _data->_layerHostingView)
+        hitView = self;
+
+#if USE(ACCELERATED_COMPOSITING)
+    if (hitView && _data && hitView == _data->_oldLayerHostingView)
         hitView = self;
 #endif
     return hitView;
@@ -1768,7 +1774,7 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
 #if USE(ACCELERATED_COMPOSITING)
 - (void)_startAcceleratedCompositing:(CALayer *)rootLayer
 {
-    if (!_data->_layerHostingView) {
+    if (!_data->_oldLayerHostingView) {
         NSView *hostingView = [[NSView alloc] initWithFrame:[self bounds]];
 #if !defined(BUILDING_ON_LEOPARD)
         [hostingView setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
@@ -1776,7 +1782,7 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
         
         [self addSubview:hostingView];
         [hostingView release];
-        _data->_layerHostingView = hostingView;
+        _data->_oldLayerHostingView = hostingView;
     }
 
     // Make a container layer, which will get sized/positioned by AppKit and CA.
@@ -1810,8 +1816,8 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
     [viewLayer setTransform:CATransform3DMakeScale(scaleFactor, scaleFactor, 1)];
 #endif
 
-    [_data->_layerHostingView setLayer:viewLayer];
-    [_data->_layerHostingView setWantsLayer:YES];
+    [_data->_oldLayerHostingView setLayer:viewLayer];
+    [_data->_oldLayerHostingView setWantsLayer:YES];
     
     // Parent our root layer in the container layer
     [viewLayer addSublayer:rootLayer];
@@ -1819,11 +1825,11 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
 
 - (void)_stopAcceleratedCompositing
 {
-    if (_data->_layerHostingView) {
-        [_data->_layerHostingView setLayer:nil];
-        [_data->_layerHostingView setWantsLayer:NO];
-        [_data->_layerHostingView removeFromSuperview];
-        _data->_layerHostingView = nil;
+    if (_data->_oldLayerHostingView) {
+        [_data->_oldLayerHostingView setLayer:nil];
+        [_data->_oldLayerHostingView setWantsLayer:NO];
+        [_data->_oldLayerHostingView removeFromSuperview];
+        _data->_oldLayerHostingView = nil;
     }
 }
 
@@ -1856,12 +1862,32 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
 
 - (void)_enterAcceleratedCompositingMode:(const LayerTreeContext&)layerTreeContext
 {
-    // FIXME: Implement.
+    ASSERT(!_data->_layerHostingView);
+
+    // Create an NSView that will host our layer tree.
+    _data->_layerHostingView.adoptNS([[NSView alloc] initWithFrame:[self bounds]]);
+    [_data->_layerHostingView.get() setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+    [self addSubview:_data->_layerHostingView.get()];
+
+    // Create a root layer that will back the NSView.
+    CALayer *rootLayer = [CALayer layer];
+#ifndef NDEBUG
+    [rootLayer setName:@"Hosting root layer"];
+#endif
+
+    [_data->_layerHostingView.get() setLayer:rootLayer];
+    [_data->_layerHostingView.get() setWantsLayer:YES];
 }
 
 - (void)_exitAcceleratedCompositingMode
 {
-    // FIXME: Implement.
+    ASSERT(_data->_layerHostingView);
+
+    [_data->_layerHostingView.get() setLayer:nil];
+    [_data->_layerHostingView.get() setWantsLayer:NO];
+    [_data->_layerHostingView.get() removeFromSuperview];
+    
+    _data->_layerHostingView = nullptr;
 }
 
 - (void)_pageDidEnterAcceleratedCompositing
