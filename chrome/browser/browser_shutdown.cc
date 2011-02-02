50b6a0d56175d4ee9a0fe6f45019fcb053172b80
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 01:09:02 +0000

    Revert 73361 - Restart instead of signout when there are pending updates.
    BUG=chromium-os:9782
    TEST=Install an update, signout. With this change, we will reboot (installing the update).
    
    Review URL: http://codereview.chromium.org/6350020
    
    TBR=achuith@chromium.org
    Review URL: http://codereview.chromium.org/6349038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_shutdown.h"
 
-#include <map>
 #include <string>
 
 #include "base/command_line.h"
@@ -34,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/renderer_host/render_widget_host.h"
 #include "chrome/browser/service/service_process_control_manager.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -254,7 +252,9 @@ void Shutdown() {
   UnregisterURLRequestChromeJob();
 
 #if defined(OS_CHROMEOS)
-  BrowserList::NotifyAndTerminate(false);
+  if (chromeos::CrosLibrary::Get()->EnsureLoaded()) {
+    chromeos::CrosLibrary::Get()->GetLoginLibrary()->StopSession("");
+  }
 #endif
 
   // Clean up data sources before the UI thread is removed.
