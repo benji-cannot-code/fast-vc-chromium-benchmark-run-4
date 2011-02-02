081a4229abb741cc22cac6cb93470afbd241a97d
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-02 21:18:44 +0000

    2011-02-02  Dimitri Glazkov  <dglazkov@chromium.org>
            Update even more references to right() and bottom() in Chromium. Sheesh.
    
            * platform/graphics/chromium/LayerRendererChromium.cpp:
            (WebCore::LayerRendererChromium::getFramebufferPixels): Replaced bottom/right with maxY/maxX.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerRendererChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerRendererChromium.cpp
@@ -351,8 +351,8 @@ void LayerRendererChromium::setRootLayer(PassRefPtr<LayerChromium> layer)
 
 void LayerRendererChromium::getFramebufferPixels(void *pixels, const IntRect& rect)
 {
-    ASSERT(rect.right() <= rootLayerTextureSize().width()
-           && rect.bottom() <= rootLayerTextureSize().height());
+    ASSERT(rect.maxX() <= rootLayerTextureSize().width()
+           && rect.maxY() <= rootLayerTextureSize().height());
 
     if (!pixels)
         return;
