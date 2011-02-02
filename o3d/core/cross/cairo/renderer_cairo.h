aac8e410c359850a76b79d8b1e05c9ed8124b5bf
tschmelcher@chromium.org <tschmelcher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 21:21:05 +0000

    O2D: Add an API to the Layer class to enable painting the entire screen area. JavaScript will use this to paint a custom background colour using a Pattern created by Pattern::CreateRgbPattern(). (In principle JavaScript could already do this by setting (x, y) = (0, 0) and (width, height) = resolution of the display area, but this is simpler.) Also delete the code implementing the hard-coded black background.
    TEST=loaded O2D and verified that a background can be created as described above
    BUG=none
    
    Review URL: http://codereview.chromium.org/6349032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73511 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/cairo/renderer_cairo.h
+++ o3d/core/cross/cairo/renderer_cairo.h
@@ -222,10 +222,6 @@ class RendererCairo : public Renderer {
   // Clip the area of the current layer that will collide with other images.
   void ClipArea(cairo_t* cr, LayerList::iterator it);
 
-  // Paint the background with black color.
-  // TODO(fransiskusx): Support changing the background color.
-  void PaintBackground(cairo_t* cr);
-
   // Linux Client Display
   Display* display_;
   // Linux Client Window
@@ -233,9 +229,6 @@ class RendererCairo : public Renderer {
 
   // Main surface to render cairo
   cairo_surface_t* main_surface_;
-
-  // Draw the background
-  cairo_t* bg_drawing_;
 
   // Array of Layer
   LayerList layer_list_;
