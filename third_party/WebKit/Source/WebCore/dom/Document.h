ddd9c9af6450865b707ece414e02651b95ebf988
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-02 00:22:52 +0000

    2011-02-01  chris reiss  <christopher.reiss@nokia.com>
            Reviewed by Adam Barth.
    
            Self-replicating code makes Safari hang and eventually crash
            https://bugs.webkit.org/show_bug.cgi?id=15123
    
            * fast/dom/Document/document-close-iframe-load-expected.txt: Added.
            * fast/dom/Document/document-close-iframe-load.html: Added.
            * fast/dom/Document/document-close-nested-iframe-load-expected.txt: Added.
            * fast/dom/Document/document-close-nested-iframe-load.html: Added.
            * fast/dom/Document/document-write-recursion-expected.txt: Added.
            * fast/dom/Document/document-write-recursion.html: Added.
    2011-02-01  chris reiss  <christopher.reiss@nokia.com>
    
            Reviewed by Adam Barth.
    
            Self-replicating code makes Safari hang and eventually crash
            https://bugs.webkit.org/show_bug.cgi?id=15123
    
            Here we are replicating the Firefox safeguard against
            recursive document.write( ) 's.
    
            See  https://bug197052.bugzilla.mozilla.org/attachment.cgi?id=293907 in bug
            https://bugzilla.mozilla.org/show_bug.cgi?id=197052 .   Firefox does two things -
                a) imposes a recursion limit of 20 on document.write( ) and
                b) once that limit is passed, panics all the way the call stack (rather than just returning one level.)
            To see why this is necessary, consider the script :
    
            <script>
               var t = document.body.innerHTML;
               document.write(t);
            </script>
    
            This will create a tree both broad and deep as the script keeps appending itself to the text.   If
            we just return one level after the recursion limit is reached, we still allow millions of copies to
            duplicate (and execute).
    
            The recursion is fortunately depth-first, so as soon as we cross this limit, we panic up the callstack
            to prevent this situation.    (IE apparently does the same thing, with a lower recursion limit.)
    
            Test: fast/dom/Document/document-write-recursion.html
            Test: fast/dom/Document/document-close-iframe-load.html
            Test: fast/dom/Document/document-close-nested-iframe-load.html
    
            * dom/Document.cpp:
            (WebCore::Document::Document):
            (WebCore::Document::write):
            * dom/Document.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -1383,6 +1383,8 @@ private:
 
     DocumentTiming m_documentTiming;
     RefPtr<MediaQueryMatcher> m_mediaQueryMatcher;
+    bool m_writeRecursionIsTooDeep;
+    unsigned m_writeRecursionDepth;
 
 #if ENABLE(REQUEST_ANIMATION_FRAME)
     typedef Vector<RefPtr<RequestAnimationFrameCallback> > RequestAnimationFrameCallbackList;
