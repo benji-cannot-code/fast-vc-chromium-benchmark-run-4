2247521b6f735d138d2a970c2052d0fb4ad8bed6
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 21:52:22 +0000

    Update AudioMixerAlsa to not use PrefMember.
    AudioMixerAlsa is a Singleton and PrefMember relies on the associated
    PrefService still existing in its Destructor, which can not be gauranteed
    for a Singleton.
    
    BUG=chromium-os:11407
    TEST=suite_Smoke
    
    Review URL: http://codereview.chromium.org/6410039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73518 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/audio_mixer_alsa.h
+++ chrome/browser/chromeos/audio_mixer_alsa.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/threading/thread.h"
 #include "chrome/browser/chromeos/audio_mixer.h"
-#include "chrome/browser/prefs/pref_member.h"
+
+class PrefService;
 
 struct _snd_mixer_elem;
 struct _snd_mixer;
@@ -97,8 +98,7 @@ class AudioMixerAlsa : public AudioMixer {
   _snd_mixer_elem* elem_master_;
   _snd_mixer_elem* elem_pcm_;
 
-  IntegerPrefMember mute_pref_;
-  DoublePrefMember volume_pref_;
+  PrefService* prefs_;
 
   scoped_ptr<base::Thread> thread_;
 
@@ -110,4 +110,3 @@ class AudioMixerAlsa : public AudioMixer {
 DISABLE_RUNNABLE_METHOD_REFCOUNT(chromeos::AudioMixerAlsa);
 
 #endif  // CHROME_BROWSER_CHROMEOS_AUDIO_MIXER_ALSA_H_
-
