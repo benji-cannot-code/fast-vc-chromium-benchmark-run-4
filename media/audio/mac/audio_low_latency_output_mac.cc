a7dfe44a1beafef9dd173762476976727c2d272c
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 20:35:08 +0000

    media/audio/mac/audio_low_latency_output_mac.cc is missing an #include.
    audio_low_latency_output_mac.cc, new in r73058, uses the Mac OS X Component
    Manager. This includes the Component and ComponentDescription types, and the
    FindNextComponent, OpenAComponent, and CloseComponent calls. To use the
    Component Manager, #include <CoreServices/CoreServices.h>
    
    http://developer.apple.com/library/mac/#documentation/Carbon/Reference/Component_Manager/Reference/reference.html
    
    CoreServices.h is probably #included by something else when using the 10.5
    SDK, but this caused a compilation failure with the 10.6 SDK.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6286056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73503 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_low_latency_output_mac.cc
+++ media/audio/mac/audio_low_latency_output_mac.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/audio/mac/audio_low_latency_output_mac.h"
 
+#include <CoreServices/CoreServices.h>
+
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "media/audio/audio_util.h"
