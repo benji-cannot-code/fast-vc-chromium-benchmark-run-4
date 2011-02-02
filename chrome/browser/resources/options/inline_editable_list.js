0ca26f82704b2ced90b35cefc41ac58c607e4ce4
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 18:00:33 +0000

    Refactor the exceptions lists to use the InlineEditableItemList class
    While I was removing code that's no longer needed, I also simplified the select element handling.
    
    I also abstracted some styling for inline editable list text fields so that CSS isn't replicated for each list.
    
    BUG=None
    TEST=DOMUI Settings lists, especially exceptions, should still work correctly and look right.
    
    Review URL: http://codereview.chromium.org/6334038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/inline_editable_list.js
+++ chrome/browser/resources/options/inline_editable_list.js
@@ -161,7 +161,7 @@ cr.define('options', function() {
         var textEl = this.ownerDocument.createElement('div');
         textEl.className = 'static-text';
         textEl.textContent = text;
-        textEl.setAttribute('editmode', false);
+        textEl.setAttribute('displaymode', 'static');
         container.appendChild(textEl);
       }
 
@@ -169,7 +169,7 @@ cr.define('options', function() {
       inputEl.type = 'text';
       inputEl.value = text;
       if (!opt_alwaysEditable) {
-        inputEl.setAttribute('editmode', true);
+        inputEl.setAttribute('displaymode', 'edit');
         inputEl.staticVersion = textEl;
       }
       container.appendChild(inputEl);
@@ -253,6 +253,7 @@ cr.define('options', function() {
     /** @inheritDoc */
     decorate: function() {
       DeletableItemList.prototype.decorate.call(this);
+      this.setAttribute('inlineeditable', '');
       this.addEventListener('blur', this.handleBlur_.bind(this), true);
     },
 
