d93d475ef0942d143bd08185d0d1e17d70c04449
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 20:03:02 +0000

    Split flip_in_mem_edsm_server into a gazillion pieces.
    For the most part, this is a straight refactor.  I'm sure I broke something.
    
    BUG=monolithic code
    TEST=none
    
    Review URL: http://codereview.chromium.org/6392011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73491 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/tools/flip_server/spdy_util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_TOOLS_FLIP_SERVER_SPDY_UTIL_H_
+#define NET_TOOLS_FLIP_SERVER_SPDY_UTIL_H_
+
+#include <string>
+
+namespace net {
+
+// Flag indicating if we need to encode urls into filenames (legacy).
+extern bool g_need_to_encode_url;
+
+// Encode the URL.
+std::string EncodeURL(std::string uri, std::string host, std::string method);
+
+}  // namespace net
+
+#endif  // NET_TOOLS_FLIP_SERVER_SPDY_UTIL_H_
+
