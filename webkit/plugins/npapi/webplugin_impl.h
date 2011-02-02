ff98b58ac22b60e5e00120bcd90bf54a5edfb3bf
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-02 03:14:26 +0000

    Propagate the user gesture state. It is necessary to do correct pop-up blocking if a plug-in tries to open an URL in a new frame. No behaviour changes yet -- those require a separate change to the WebKit chromium directory, that uses the propagated state.
    BUG=70538
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6392042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73417 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugin_impl.h
+++ webkit/plugins/npapi/webplugin_impl.h
@@ -164,6 +164,7 @@ class WebPluginImpl : public WebPlugin,
   // corresponding error codes otherwise.
   RoutingStatus RouteToFrame(const char* url,
                              bool is_javascript_url,
+                             bool popups_allowed,
                              const char* method,
                              const char* target,
                              const char* buf,
