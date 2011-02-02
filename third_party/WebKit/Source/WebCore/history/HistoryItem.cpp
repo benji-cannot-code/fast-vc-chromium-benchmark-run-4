31abb13e1d09baaf1342d20afd2f7628c473b549
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-02 20:33:51 +0000

    Encode/decode FormData and FormDataElement objects consistently
    Fixes <http://webkit.org/b/53615> <rdar://problem/8943346> WebKit2: Restoring session state
    that contains form data fails (asserts in Debug build)
    
    To prevent this from interfering with WebKit2 testing, it's useful to get this into a build
    now, even though we don't have an automated test for it yet. Writing a test is covered by
    <http://webkit.org/b/53616>.
    
    Reviewed by Darin Adler.
    
    * history/HistoryItem.cpp: Bump the encoding version, since this patch changes how we encode
    FormData objects.
    
    * platform/network/FormData.cpp:
    (WebCore::decode): Decode the type from the Decoder, rather than getting it from the
    default-constructed FormDataElement. Failing to do this meant that all future uses of the
    Decoder would be reading from an unexpected part of the buffer (i.e., the next decode would
    start by reading the uint32_t that we forgot to decode here, and so on). We already had code
    to correctly set the FormDataElement's type based on this decoded type later in the
    function.
    (WebCore::FormData::encodeForBackForward): Encode m_identifier as an int64_t, since that
    matches its type and how we decode it.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/history/HistoryItem.cpp
+++ third_party/WebKit/Source/WebCore/history/HistoryItem.cpp
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-const uint32_t backForwardTreeEncodingVersion = 1;
+const uint32_t backForwardTreeEncodingVersion = 2;
 
 static long long generateSequenceNumber()
 {
