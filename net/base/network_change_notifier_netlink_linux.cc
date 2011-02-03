7aad6e172dad9c7a98cf757ba27ce4ce06ca2a2a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 23:37:02 +0000

    Revert 73690 - net: Add namespace net to network_change_notifier_netlink_linux.
    BUG=64263
    TEST=compiled locally and trybots
    
    Review URL: http://codereview.chromium.org/6368066
    
    TBR=tfarina@chromium.org
    Review URL: http://codereview.chromium.org/6286093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73701 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_netlink_linux.cc
+++ net/base/network_change_notifier_netlink_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -57,8 +57,6 @@ bool IsDuplicateIPv6AddressUpdate(
 }
 
 }  // namespace
-
-namespace net {
 
 int InitializeNetlinkSocket() {
   int sock = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
@@ -130,5 +128,3 @@ bool HandleNetlinkMessage(char* buf, size_t len) {
 
   return false;
 }
-
-}  // namespace net
