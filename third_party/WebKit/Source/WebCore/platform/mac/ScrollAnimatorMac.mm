93d97c92e0c202c3366273cb699b128505064f5b
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-03 01:36:14 +0000

    <rdar://problem/8952012> Crash on launch inside scrollbar code.
    Reviewed by Beth Dakin.
    
    We need to ensure that we remove ourselves as the delegates of objects when we're going
    away as failing to do this can lead to crashes if the lifetime of the other objects
    is longer than ours.
    
    * platform/mac/ScrollAnimatorMac.mm:
    (WebCore::ScrollAnimatorMac::~ScrollAnimatorMac):
    * platform/mac/ScrollbarThemeMac.mm:
    (WebCore::ScrollbarThemeMac::unregisterScrollbar):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
@@ -397,6 +397,7 @@ ScrollAnimatorMac::ScrollAnimatorMac(ScrollableArea* scrollableArea)
 
 ScrollAnimatorMac::~ScrollAnimatorMac()
 {
+    [(id)m_scrollbarPainterController.get() setDelegate:nil];
 }
 
 id ScrollAnimatorMac::scrollbarPainterDelegate()
