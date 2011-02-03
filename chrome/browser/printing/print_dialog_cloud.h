b60d95a660bff972da38902f778315c40337398c
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 00:11:07 +0000

    Cloud print: Code cleanup for PrintDialogCloud and friends.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6368047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud.h
+++ chrome/browser/printing/print_dialog_cloud.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
-#include "base/gtest_prod_util.h"
 #include "base/string16.h"
 
 class Browser;
 class FilePath;
+
 namespace IPC {
 class Message;
 }
@@ -32,6 +32,10 @@ class PrintDialogCloud {
                             bool modal);
   ~PrintDialogCloud();
 
+  void Init(const FilePath& path_to_pdf,
+            const string16& print_job_title,
+            bool modal);
+
   // Called as a task from the UI thread, creates an object instance
   // to run the HTML/JS based print dialog for printing through the cloud.
   static void CreateDialogImpl(const FilePath& path_to_pdf,
@@ -39,8 +43,6 @@ class PrintDialogCloud {
                                bool modal);
 
   Browser* browser_;
-  string16 print_job_title_;
-  bool modal_;
 
   DISALLOW_COPY_AND_ASSIGN(PrintDialogCloud);
 };
