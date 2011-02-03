9fabdb5863ba52030d6bc3e6cb6a0cad097afba7
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 16:50:36 +0000

    [cros] Mobile activation - temporary whitelist disk I/O on UI thread.
    BUG=chromium-os:11535
    TEST=No DCHECKs during activation.
    
    Review URL: http://codereview.chromium.org/6368078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73629 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/mobile_setup_ui.cc
+++ chrome/browser/chromeos/dom_ui/mobile_setup_ui.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_piece.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/timer.h"
 #include "base/values.h"
 #include "base/weak_ptr.h"
@@ -346,8 +347,13 @@ bool CellularConfigDocument::LoadFromFile(const FilePath& config_path) {
   error_map_.clear();
 
   std::string config;
-  if (!file_util::ReadFileToString(config_path, &config))
-    return false;
+  {
+    // Reading config file causes us to do blocking IO on UI thread.
+    // Temporarily allow it until we fix http://crosbug.com/11535
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    if (!file_util::ReadFileToString(config_path, &config))
+      return false;
+  }
 
   scoped_ptr<Value> root(base::JSONReader::Read(config, true));
   DCHECK(root.get() != NULL);
@@ -1263,7 +1269,14 @@ void MobileSetupHandler::LoadCellularConfig() {
   config_loaded = true;
   // Load partner customization startup manifest if it is available.
   FilePath config_path(kCellularConfigPath);
-  if (file_util::PathExists(config_path)) {
+  bool config_exists = false;
+  {
+    // Reading config file causes us to do blocking IO on UI thread.
+    // Temporarily allow it until we fix http://crosbug.com/11535
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    config_exists = file_util::PathExists(config_path);
+  }
+  if (config_exists) {
     scoped_ptr<CellularConfigDocument> config(new CellularConfigDocument());
     bool config_loaded = config->LoadFromFile(config_path);
     if (config_loaded) {
