450bedd9c277244d65cbaff93e2232481602475c
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 19:51:29 +0000

    Unconditionally remove "-multifail" from "ap" values.  Previously, "-multifail" was added if a --multi-install run failed for any reason.  Now, setup.exe always removes it.  As a result, it is only left behind when setup.exe crashes (it is added by mini_installer.exe).
    BUG=70878
    TEST=run a mini_installer.exe with --multi-install (e.g., --multi-install --system-level --chrome --verbose-logging) and try two things: 1) kill setup.exe and make sure that -multifail is left in an "ap" value; 2) cause setup.exe to fail in a non-crash way (e.g., bump up "pv" in Chrome's Clients key so that setup.exe exits with HIGHER_VERSION_EXISTS) then make sure that -multifail is not left in any "ap" value.
    
    Review URL: http://codereview.chromium.org/6286044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73648 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/google_update_settings.h
+++ chrome/installer/util/google_update_settings.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "chrome/installer/util/util_constants.h"
 
 namespace installer {
 class ChannelInfo;
@@ -99,12 +100,10 @@ class GoogleUpdateSettings {
   // - If we are currently running full installer, we remove this magic
   // string (if it is present) regardless of whether installer failed or not.
   // There is no fall-back for full installer :)
-  // - If multi-install fails we append -multifail; otherwise, we remove it
-  // (i.e., success or single-install).
+  // - Unconditionally remove "-multifail" since we haven't crashed.
   // |state_key| should be obtained via InstallerState::state_key().
   static void UpdateInstallStatus(bool system_install,
-                                  bool incremental_install,
-                                  bool multi_install,
+                                  installer::ArchiveType archive_type,
                                   int install_return_code,
                                   const std::wstring& product_guid);
 
@@ -117,12 +116,11 @@ class GoogleUpdateSettings {
   // - If full installer failed, still remove this magic
   //   string (if it is present already).
   //
-  // diff_install: tells whether this is incremental install or not.
+  // archive_type: tells whether this is incremental install or not.
   // install_return_code: if 0, means installation was successful.
   // value: current value of Google Update "ap" key.
   // Returns true if |value| is modified.
-  static bool UpdateGoogleUpdateApKey(bool diff_install,
-                                      bool multi_install,
+  static bool UpdateGoogleUpdateApKey(installer::ArchiveType archive_type,
                                       int install_return_code,
                                       installer::ChannelInfo* value);
 
