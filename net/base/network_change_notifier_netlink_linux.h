4cb033cfa4b21045a1794598ed428bad33dd0e6e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 22:58:45 +0000

    net: Add namespace net to network_change_notifier_netlink_linux.
    BUG=64263
    TEST=compiled locally and trybots
    
    Review URL: http://codereview.chromium.org/6368066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73690 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_netlink_linux.h
+++ net/base/network_change_notifier_netlink_linux.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,10 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstddef>
 
+namespace net {
+
 // Returns the file descriptor if successful.  Otherwise, returns -1.
 int InitializeNetlinkSocket();
 
 // Returns true if a network change has been detected, otherwise returns false.
 bool HandleNetlinkMessage(char* buf, size_t len);
+
+}  // namespace net
 
 #endif  // NET_BASE_NETWORK_CHANGE_NOTIFIER_NETLINK_LINUX_H_
