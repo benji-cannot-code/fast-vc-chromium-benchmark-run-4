cd117808a5bd9de8bd37e3e5040c9c741184fd43
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-03 22:56:24 +0000

    DOMUI Settings: Update to use correct type in chrome.send
    Take 2. This time with updated chromeos settings.
    
    BUG=None
    TEST=The tabbed settings should still work.
    
    Review URL: http://codereview.chromium.org/6312130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/dom_ui/options/core_options_handler.cc
+++ chrome/browser/dom_ui/options/core_options_handler.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/dom_ui/options/core_options_handler.h"
 
+#include "base/json/json_reader.h"
+#include "base/scoped_ptr.h"
 #include "base/string16.h"
 #include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
@@ -137,8 +139,8 @@ void CoreOptionsHandler::RegisterMessages() {
       NewCallback(this, &CoreOptionsHandler::HandleSetDoublePref));
   dom_ui_->RegisterMessageCallback("setStringPref",
       NewCallback(this, &CoreOptionsHandler::HandleSetStringPref));
-  dom_ui_->RegisterMessageCallback("setObjectPref",
-      NewCallback(this, &CoreOptionsHandler::HandleSetObjectPref));
+  dom_ui_->RegisterMessageCallback("setListPref",
+      NewCallback(this, &CoreOptionsHandler::HandleSetListPref));
   dom_ui_->RegisterMessageCallback("clearPref",
       NewCallback(this, &CoreOptionsHandler::HandleClearPref));
   dom_ui_->RegisterMessageCallback("coreOptionsUserMetricsAction",
@@ -172,32 +174,16 @@ void CoreOptionsHandler::ObservePref(const std::string& pref_name) {
 }
 
 void CoreOptionsHandler::SetPref(const std::string& pref_name,
-                                 Value::ValueType pref_type,
-                                 const std::string& value_string,
+                                 const Value* value,
                                  const std::string& metric) {
   PrefService* pref_service = dom_ui_->GetProfile()->GetPrefs();
 
-  switch (pref_type) {
+  switch (value->GetType()) {
     case Value::TYPE_BOOLEAN:
-      pref_service->SetBoolean(pref_name.c_str(), value_string == "true");
-      break;
-
     case Value::TYPE_INTEGER:
-      int int_value;
-      CHECK(base::StringToInt(value_string, &int_value));
-      pref_service->SetInteger(pref_name.c_str(), int_value);
-
-      break;
-
     case Value::TYPE_DOUBLE:
-      double double_value;
-      CHECK(base::StringToDouble(value_string, &double_value));
-      pref_service->SetDouble(pref_name.c_str(), double_value);
-
-      break;
-
     case Value::TYPE_STRING:
-      pref_service->SetString(pref_name.c_str(), value_string);
+      pref_service->Set(pref_name.c_str(), *value);
       break;
 
     default:
@@ -206,7 +192,7 @@ void CoreOptionsHandler::SetPref(const std::string& pref_name,
   }
 
   pref_service->ScheduleSavePersistentPrefs();
-  ProcessUserMetric(pref_type, value_string, metric);
+  ProcessUserMetric(value, metric);
 }
 
 void CoreOptionsHandler::ClearPref(const std::string& pref_name,
@@ -219,15 +205,17 @@ void CoreOptionsHandler::ClearPref(const std::string& pref_name,
     UserMetricsRecordAction(UserMetricsAction(metric.c_str()));
 }
 
-void CoreOptionsHandler::ProcessUserMetric(Value::ValueType pref_type,
-                                           const std::string& value_string,
+void CoreOptionsHandler::ProcessUserMetric(const Value* value,
                                            const std::string& metric) {
   if (metric.empty())
     return;
 
   std::string metric_string = metric;
-  if (pref_type == Value::TYPE_BOOLEAN)
-    metric_string += (value_string == "true" ? "_Enable" : "_Disable");
+  if (value->IsType(Value::TYPE_BOOLEAN)) {
+    bool bool_value;
+    CHECK(value->GetAsBoolean(&bool_value));
+    metric_string += bool_value ? "_Enable" : "_Disable";
+  }
 
   UserMetricsRecordAction(UserMetricsAction(metric_string.c_str()));
 }
@@ -318,8 +306,8 @@ void CoreOptionsHandler::HandleSetStringPref(const ListValue* args) {
   HandleSetPref(args, Value::TYPE_STRING);
 }
 
-void CoreOptionsHandler::HandleSetObjectPref(const ListValue* args) {
-  HandleSetPref(args, Value::TYPE_NULL);
+void CoreOptionsHandler::HandleSetListPref(const ListValue* args) {
+  HandleSetPref(args, Value::TYPE_LIST);
 }
 
 void CoreOptionsHandler::HandleSetPref(const ListValue* args,
@@ -330,15 +318,37 @@ void CoreOptionsHandler::HandleSetPref(const ListValue* args,
   if (!args->GetString(0, &pref_name))
     return;
 
-  std::string value_string;
-  if (!args->GetString(1, &value_string))
+  Value* value;
+  if (!args->Get(1, &value))
     return;
+
+  scoped_ptr<Value> temp_value;
+
+  // In JS all numbers are doubles.
+  if (type == Value::TYPE_INTEGER) {
+    double double_value;
+    CHECK(value->GetAsDouble(&double_value));
+    temp_value.reset(Value::CreateIntegerValue(static_cast<int>(double_value)));
+    value = temp_value.get();
+
+  // In case we have a List pref we got a JSON string.
+  } else if (type == Value::TYPE_LIST) {
+    std::string json_string;
+    CHECK(value->GetAsString(&json_string));
+    temp_value.reset(
+        base::JSONReader().JsonToValue(json_string,
+                                       false,  // no check_root
+                                       false));  // no trailing comma
+    value = temp_value.get();
+  }
+
+  CHECK_EQ(type, value->GetType());
 
   std::string metric;
   if (args->GetSize() > 2)
     args->GetString(2, &metric);
 
-  SetPref(pref_name, type, value_string, metric);
+  SetPref(pref_name, value, metric);
 }
 
 void CoreOptionsHandler::HandleClearPref(const ListValue* args) {
--- chrome/browser/resources/options/chromeos_proxy_rules_list.js
+++ chrome/browser/resources/options/chromeos_proxy_rules_list.js
@@ -93,7 +93,7 @@ cr.define('options.proxyexceptions', function() {
      * Updates backend.
      */
     updateBackend_: function() {
-      Preferences.setObjectPref(this.pref, this.dataModel.slice());
+      Preferences.setListPref(this.pref, this.dataModel.slice());
     }
   };
 
