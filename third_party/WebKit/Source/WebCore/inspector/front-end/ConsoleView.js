fda2e5871b9256e3130fed84fbd9e1ace37162ff
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-03 17:15:15 +0000

    2011-02-02  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: do not share source frames between resources panel and scripts panel.
            https://bugs.webkit.org/show_bug.cgi?id=53584
    
            Currently, we show error messages only for resources. This change will allow showing error
            messages in source frame even when resource is not available (eval scripts, inlined scripts).
    
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleView.prototype.addMessage):
            (WebInspector.ConsoleView.prototype.clearMessages):
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView.recreateResourceView):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.FrameResourceTreeElement.prototype._setBubbleText):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype._scriptSourceChanged):
            (WebInspector.ScriptsPanel.prototype.addConsoleMessage):
            (WebInspector.ScriptsPanel.prototype.clearConsoleMessages):
            (WebInspector.ScriptsPanel.prototype.reset):
            (WebInspector.ScriptsPanel.prototype._sourceFrameForScriptOrResource):
            (WebInspector.ScriptsPanel.prototype._sourceFrameForResource):
            (WebInspector.ScriptsPanel.prototype._sourceFrameForScript):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
@@ -274,6 +274,7 @@ WebInspector.ConsoleView.prototype = {
         if (msg instanceof WebInspector.ConsoleMessage && !(msg instanceof WebInspector.ConsoleCommandResult)) {
             this._incrementErrorWarningCount(msg);
             WebInspector.resourceTreeModel.addConsoleMessage(msg);
+            WebInspector.panels.scripts.addConsoleMessage(msg);
             this.commandSincePreviousMessage = false;
             this.previousMessage = msg;
         } else if (msg instanceof WebInspector.ConsoleCommand) {
@@ -322,6 +323,7 @@ WebInspector.ConsoleView.prototype = {
     clearMessages: function()
     {
         WebInspector.resourceTreeModel.clearConsoleMessages();
+        WebInspector.panels.scripts.clearConsoleMessages();
 
         this.messages = [];
 
--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourceView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourceView.js
@@ -110,7 +110,6 @@ WebInspector.ResourceView.recreateResourceView = function(resource)
     if (scrollTop)
         newView.scrollTop = scrollTop;
 
-    WebInspector.panels.scripts.viewRecreated(oldView, newView);
     return newView;
 }
 
