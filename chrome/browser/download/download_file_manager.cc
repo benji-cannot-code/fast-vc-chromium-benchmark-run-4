37dd7d448a54b3dd12b3c2a553f6a277443edf64
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 23:49:55 +0000

    Carnitas: Remove unneeded include from download_file_manager.cc
    BUG=None
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6410095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73872 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_file_manager.cc
+++ chrome/browser/download/download_file_manager.cc
@@ -25,12 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 #include "net/base/io_buffer.h"
 
-#if defined(OS_WIN)
-#include "chrome/common/win_safe_util.h"
-#elif defined(OS_MACOSX)
-#include "chrome/browser/ui/cocoa/file_metadata.h"
-#endif
-
 namespace {
 
 // Throttle updates to the UI thread so that a fast moving download doesn't
