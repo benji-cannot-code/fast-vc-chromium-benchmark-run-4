53c0e69f33b707604adb3d5c02968529ba1cc9a0
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 20:50:13 +0000

    Set the focus to proper views when the login window gets focus.  * Set the initial focus using SetInitialFocus template method.  * Removed unused method from GuestUserView  * Changed NewUserView to use SetInitialFocus.
    BUG=chromium-os:11425
    TEST=manual. The password of 1st user pod should have focused on boot. Use key to navigate between user pod, guest and new user pod, and make sure the password field, "Enter" button and the "Emai" field gets focused when switched.
    
    Review URL: http://codereview.chromium.org/6286087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73844 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/guest_user_view.cc
+++ chrome/browser/chromeos/login/guest_user_view.cc
@@ -72,10 +72,6 @@ void GuestUserView::RecreateFields() {
   SchedulePaint();
 }
 
-void GuestUserView::FocusSignInButton() {
-  submit_button_->RequestFocus();
-}
-
 bool GuestUserView::AcceleratorPressed(
     const views::Accelerator& accelerator) {
   if (accelerator == accel_login_off_the_record_)
@@ -89,6 +85,10 @@ bool GuestUserView::AcceleratorPressed(
   else
     return false;
   return true;
+}
+
+void GuestUserView::RequestFocus() {
+  submit_button_->RequestFocus();
 }
 
 void GuestUserView::ButtonPressed(
