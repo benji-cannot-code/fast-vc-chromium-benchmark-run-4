b423331d299348ef2dfbc94348b36d337ae99713
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-04 04:05:49 +0000

    2011-02-03  Adam Barth  <abarth@webkit.org>
            Attempt to fix Chromium build.
    
            * html/parser/XSSFilter.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSFilter.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/XSSFilter.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "XSSFilter.h"
 
+#include "DOMWindow.h"
 #include "Document.h"
 #include "DocumentLoader.h"
 #include "Frame.h"
