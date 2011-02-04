fa94655b9f450095f81f14edfde70a1c65f927eb
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 17:29:54 +0000

    Update Guest label upon locale change.
    BUG=chromium-os:11585
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/6368085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_controller.cc
+++ chrome/browser/chromeos/login/user_controller.cc
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/user_view.h"
 #include "chrome/browser/chromeos/login/username_view.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
-#include "third_party/cros/chromeos_wm_ipc_enums.h"
 #include "gfx/canvas.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "third_party/cros/chromeos_wm_ipc_enums.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "views/background.h"
@@ -87,9 +87,7 @@ void CloseWindow(views::WidgetGtk* window) {
 
 }  // namespace
 
-using login::kBackgroundColor;
 using login::kBorderSize;
-using login::kTextColor;
 using login::kUserImageSize;
 
 // static
@@ -422,15 +420,9 @@ void UserController::CreateBorderWindow(int index,
 
 WidgetGtk* UserController::CreateLabelWindow(int index,
                                              WmIpcWindowType type) {
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-  const gfx::Font& font = (type == WM_IPC_WINDOW_LOGIN_LABEL) ?
-      rb.GetFont(ResourceBundle::MediumBoldFont).DeriveFont(
-          kSelectedUsernameFontDelta) :
-      rb.GetFont(ResourceBundle::BaseFont).DeriveFont(
-          kUnselectedUsernameFontDelta, gfx::Font::BOLD);
   std::wstring text;
   if (is_guest_) {
-    text = UTF16ToWide(l10n_util::GetStringUTF16(IDS_GUEST));
+    text = std::wstring();
   } else if (is_new_user_) {
     // Add user should have label only in activated state.
     // When new user is the only, label is not needed.
@@ -440,7 +432,7 @@ WidgetGtk* UserController::CreateLabelWindow(int index,
     text = UTF8ToWide(user_.GetDisplayName());
   }
 
-  views::Label *label = NULL;
+  views::Label* label = NULL;
 
   if (is_new_user_) {
     label = new views::Label(text);
@@ -452,8 +444,14 @@ WidgetGtk* UserController::CreateLabelWindow(int index,
     label = UsernameView::CreateShapedUsernameView(text, true);
   }
 
-  label->SetColor(kTextColor);
+  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+  const gfx::Font& font = (type == WM_IPC_WINDOW_LOGIN_LABEL) ?
+      rb.GetFont(ResourceBundle::MediumBoldFont).DeriveFont(
+          kSelectedUsernameFontDelta) :
+      rb.GetFont(ResourceBundle::BaseFont).DeriveFont(
+          kUnselectedUsernameFontDelta, gfx::Font::BOLD);
   label->SetFont(font);
+  label->SetColor(login::kTextColor);
 
   if (type == WM_IPC_WINDOW_LOGIN_LABEL)
     label_view_ = label;
