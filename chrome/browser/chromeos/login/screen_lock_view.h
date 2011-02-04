7c74360c4b1b84b6e0532e57cb22e1fb4246a3bd
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 08:59:37 +0000

    UI polish on lock screen
    - Fixed clipped message on signout link
    - Remove error bubble on new typing
    - Fixed rounded corners
    
    BUG=chromium-os:11611
    TEST=manual, see issue
    
    Review URL: http://codereview.chromium.org/6410061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73785 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_lock_view.h
+++ chrome/browser/chromeos/login/screen_lock_view.h
@@ -60,7 +60,7 @@ class ScreenLockView : public ThrobberHostView,
 
   // views::Textfield::Controller implementation:
   virtual void ContentsChanged(views::Textfield* sender,
-                               const string16& new_contents) {}
+                               const string16& new_contents);
   virtual bool HandleKeyEvent(views::Textfield* sender,
                               const views::KeyEvent& keystroke);
 
