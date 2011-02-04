d45a43637c6eb2795986018d0b86474d75c3183f
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 03:35:55 +0000

    Added XFlush calls. Fixed mouse button mapping.
    BUG=71663
    TEST=Connect to a Linux host and try to move the mouse and type text. Both should work as they would at the console.
    
    Review URL: http://codereview.chromium.org/6368052
    Patch from Jamie Walch <jamiewalch@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73755 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/event_executor_linux.cc
+++ remoting/host/event_executor_linux.cc
@@ -14,6 +14,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task.h"
 #include "remoting/proto/internal.pb.h"
 
+// TODO(jamiewalch): Class to ensure that XFlush is called regardless of what
+// fields are set in the MouseEvent. It's not worth refactoring HandleMouse to
+// avoid the early returns because calling XFlush is not really the correct
+// way of flushing the XTest requests; instead we should dispatch the requests
+// to a suitable UI thread.
+namespace {
+class ScopedXFlusher {
+ public:
+  explicit ScopedXFlusher(Display* display)
+      : display_(display), needs_flush_(false) {
+  }
+
+  ~ScopedXFlusher() {
+    if (needs_flush_)
+      XFlush(display_);
+  }
+
+  void SignalFlush() {
+    needs_flush_ = true;
+  }
+
+ private:
+  Display* display_;
+  bool needs_flush_;
+};
+}
+
 namespace remoting {
 
 using protocol::MouseEvent;
@@ -26,10 +53,10 @@ static int MouseButtonToX11ButtonNumber(
       return 1;
 
     case MouseEvent::BUTTON_RIGHT:
-      return 2;
+      return 3;
 
     case MouseEvent::BUTTON_MIDDLE:
-      return 3;
+      return 2;
 
     case MouseEvent::BUTTON_UNDEFINED:
     default:
@@ -303,9 +330,13 @@ void EventExecutorLinuxPimpl::HandleKey(const KeyEvent* key_event) {
           << " sending keysym: " << keysym
           << " to keycode: " << keycode;
   XTestFakeKeyEvent(display_, keycode, key_event->pressed(), CurrentTime);
+
+  // TODO(jamiewalch): Get rid of this once we're dispatching to the UI thread.
+  XFlush(display_);
 }
 
 void EventExecutorLinuxPimpl::HandleMouse(const MouseEvent* event) {
+  ScopedXFlusher flusher(display_);
   if (event->has_x() && event->has_y()) {
     if (event->x() < 0 || event->y() < 0 ||
         event->x() > width_ || event->y() > height_) {
@@ -317,6 +348,7 @@ void EventExecutorLinuxPimpl::HandleMouse(const MouseEvent* event) {
 
     VLOG(3) << "Moving mouse to " << event->x()
             << "," << event->y();
+    flusher.SignalFlush();
     XTestFakeMotionEvent(display_, DefaultScreen(display_),
                          event->x(), event->y(),
                          CurrentTime);
@@ -333,6 +365,7 @@ void EventExecutorLinuxPimpl::HandleMouse(const MouseEvent* event) {
 
     VLOG(3) << "Button " << event->button()
             << " received, sending down " << button_number;
+    flusher.SignalFlush();
     XTestFakeButtonEvent(display_, button_number, event->button_down(),
                          CurrentTime);
   }
