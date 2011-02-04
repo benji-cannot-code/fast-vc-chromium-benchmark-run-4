ed8e1cfbb38d073087c9d6efc0062d8abbbde47f
jorlow@chromium.org <jorlow@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-04 20:10:25 +0000

    2011-02-04  Jeremy Orlow  <jorlow@chromium.org>
            Build fix from merge mistake.
    
            * storage/IDBTransaction.cpp:
            (WebCore::IDBTransaction::ensureEventTargetData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBTransaction.cpp
+++ third_party/WebKit/Source/WebCore/storage/IDBTransaction.cpp
@@ -143,6 +143,11 @@ EventTargetData* IDBTransaction::eventTargetData()
     return &m_eventTargetData;
 }
 
+EventTargetData* IDBTransaction::ensureEventTargetData()
+{
+    return &m_eventTargetData;
+}
+
 }
 
 #endif // ENABLE(INDEXED_DATABASE)
