602e1ff460b3769d779983a8c50ae14287dc6c2e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-05 02:19:43 +0000

    2011-02-04  Xiyuan Xia  <xiyuan@chromium.org>
            Reviewed by Tony Chang.
    
            [Chromium] Option text in select popup does not align with menulist button text
            https://bugs.webkit.org/show_bug.cgi?id=53632
    
            This makes clientPaddingLeft and  clientPaddingRight return
            the additional m_innerBlock's padding so that the popup item text
            aligns with the menulist button text.
    
            * rendering/RenderMenuList.cpp:
            (WebCore::RenderMenuList::clientPaddingLeft):
            (WebCore::RenderMenuList::clientPaddingRight):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77716 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderMenuList.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderMenuList.cpp
@@ -487,7 +487,7 @@ int RenderMenuList::clientInsetRight() const
 
 int RenderMenuList::clientPaddingLeft() const
 {
-    return paddingLeft();
+    return paddingLeft() + m_innerBlock->paddingLeft();
 }
 
 const int endOfLinePadding = 2;
@@ -503,7 +503,7 @@ int RenderMenuList::clientPaddingRight() const
 
     // If the appearance isn't MenulistPart, then the select is styled (non-native), so
     // we want to return the user specified padding.
-    return paddingRight();
+    return paddingRight() + m_innerBlock->paddingRight();
 }
 
 int RenderMenuList::listSize() const
