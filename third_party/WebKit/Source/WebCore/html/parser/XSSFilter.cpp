benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 8d6af40ab52857f1a244505842f11a99b4ae4b11

2011-02-02  Adam Barth  <abarth@webkit.org>
        Reviewed by Daniel Bates.

        Teach XSSFilter about JavaScript URLs
        https://bugs.webkit.org/show_bug.cgi?id=53635

        This patch teaches the XSSFilter to check for JavaScript URLs in
        attribute values.  If this approach has too many false positives, we
        can restrict which attribute names we examine.

        Fixes these tests:
            http/tests/security/xssAuditor/anchor-url-dom-write-location-javascript-URL.html
            http/tests/security/xssAuditor/dom-write-location-javascript-URL.html
            http/tests/security/xssAuditor/iframe-javascript-url*

        * html/parser/XSSFilter.cpp:
        (WebCore::HTMLNames::containsJavaScriptURL):
        (WebCore::XSSFilter::filterTokenInitial):
        (WebCore::XSSFilter::eraseDangerousAttributesIfInjected):
        * html/parser/XSSFilter.h:

git-svn-id: svn://svn.chromium.org/blink/trunk@77454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/html/parser/XSSFilter.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "HTMLDocumentParser.h"
 #include "HTMLNames.h"
+#include "HTMLParserIdioms.h"
 #include "Settings.h"
 #include "TextEncoding.h"
 #include "TextResourceDecoder.h"
@@ -85,6 +86,23 @@ bool isNameOfInlineEventHandler(const Vector<UChar, 32>& name)
     if (name.size() < lengthOfShortestInlineEventHandlerName)
         return false;
     return name[0] == 'o' && name[1] == 'n';
+}
+
+bool containsJavaScriptURL(const Vector<UChar, 32>& value)
+{
+    static const char javaScriptScheme[] = "javascript:";
+    static const size_t lengthOfJavaScriptScheme = sizeof(javaScriptScheme) - 1;
+
+    size_t i = 0;
+    while (i < value.size()) {
+        if (!isHTMLSpace(value[i]))
+            break;
+    }
+
+    if (value.size() - i < lengthOfJavaScriptScheme)
+        return false;
+
+    return equalIgnoringCase(value.data() + i, javaScriptScheme, lengthOfJavaScriptScheme);
 }
 
 String decodeURL(const String& string, const TextEncoding& encoding)
@@ -177,7 +195,7 @@ bool XSSFilter::filterTokenInitial(HTMLToken& token)
     if (token.type() != HTMLToken::StartTag)
         return false;
 
-    bool didBlockScript = eraseInlineEventHandlersIfInjected(token);
+    bool didBlockScript = eraseDangerousAttributesIfInjected(token);
 
     if (hasName(token, scriptTag))
         didBlockScript |= filterScriptToken(token);
@@ -292,12 +310,12 @@ bool XSSFilter::filterBaseToken(HTMLToken& token)
     return eraseAttributeIfInjected(token, hrefAttr);
 }
 
-bool XSSFilter::eraseInlineEventHandlersIfInjected(HTMLToken& token)
+bool XSSFilter::eraseDangerousAttributesIfInjected(HTMLToken& token)
 {
     bool didBlockScript = false;
     for (size_t i = 0; i < token.attributes().size(); ++i) {
         const HTMLToken::Attribute& attribute = token.attributes().at(i);
-        if (!isNameOfInlineEventHandler(attribute.m_name))
+        if (!isNameOfInlineEventHandler(attribute.m_name) && !containsJavaScriptURL(attribute.m_value))
             continue;
         if (!isContainedInRequest(snippetForAttribute(token, attribute)))
             continue;

third_party/WebKit/Source/WebCore/html/parser/XSSFilter.h
@@ -58,7 +58,7 @@ private:
     bool filterMetaToken(HTMLToken&);
     bool filterBaseToken(HTMLToken&);
 
-    bool eraseInlineEventHandlersIfInjected(HTMLToken&);
+    bool eraseDangerousAttributesIfInjected(HTMLToken&);
     bool eraseAttributeIfInjected(HTMLToken&, const QualifiedName&, const String& replacementValue = String());
 
     String snippetForRange(const HTMLToken&, int start, int end);

commit 1cec220be233de9f4de18f4e6c9a5b428babbebb

Make an infinite loop introduced in r77454 finite.
Reviewed by Anders Carlsson.

* html/parser/XSSFilter.cpp:
(WebCore::HTMLNames::containsJavaScriptURL):



git-svn-id: svn://svn.chromium.org/blink/trunk@77697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/html/parser/XSSFilter.cpp
@@ -97,8 +97,8 @@ bool containsJavaScriptURL(const Vector<UChar, 32>& value)
     static const char javaScriptScheme[] = "javascript:";
     static const size_t lengthOfJavaScriptScheme = sizeof(javaScriptScheme) - 1;
 
-    size_t i = 0;
-    while (i < value.size()) {
+    size_t i;;
+    for (i = 0; i < value.size(); ++i) {
         if (!isHTMLSpace(value[i]))
             break;
     }