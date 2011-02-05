7dbec79c678c927e13bd2bc40f7051705cfaeabd
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-05 15:53:50 +0000

    Revert 73427 (set the name of the PPAPI plugin correcetly) and 73916 (minor bugfix) to see if it is responsible for the startup performance regression.
    TEST=startup perf XP
    Review URL: http://codereview.chromium.org/6368120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73931 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/plugin_module.h
+++ webkit/plugins/ppapi/plugin_module.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <set>
-#include <string>
 
 #include "base/basictypes.h"
 #include "base/native_library.h"
@@ -73,8 +72,7 @@ class PluginModule : public base::RefCounted<PluginModule>,
   // The module lifetime delegate is a non-owning pointer that must outlive
   // all plugin modules. In practice it will be a global singleton that
   // tracks which modules are alive.
-  PluginModule(const std::string& name,
-               PluginDelegate::ModuleLifetime* lifetime_delegate);
+  PluginModule(PluginDelegate::ModuleLifetime* lifetime_delegate);
 
   ~PluginModule();
 
@@ -101,6 +99,7 @@ class PluginModule : public base::RefCounted<PluginModule>,
   // proxy needs this information to set itself up properly).
   PP_Module pp_module() const { return pp_module_; }
 
+  void set_name(const std::string& name) { name_ = name; }
   const std::string& name() const { return name_; }
 
   PluginInstance* CreateInstance(PluginDelegate* delegate);
@@ -154,7 +153,7 @@ class PluginModule : public base::RefCounted<PluginModule>,
   EntryPoints entry_points_;
 
   // The name of the module.
-  const std::string name_;
+  std::string name_;
 
   // Non-owning pointers to all instances associated with this module. When
   // there are no more instances, this object should be deleted.
