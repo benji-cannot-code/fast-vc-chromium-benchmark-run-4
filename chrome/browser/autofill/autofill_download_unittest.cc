8e0f9e5b1da5b571a7d1f41473fa8d2368013de7
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-05 05:53:57 +0000

    Enable AutoFill crowdsourcing for Chromium builds as well.
    BUG=71869
    TEST=none
    
    Review URL: http://codereview.chromium.org/6246100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_download_unittest.cc
+++ chrome/browser/autofill/autofill_download_unittest.cc
@@ -47,8 +47,6 @@ class AutoFillDownloadTestHelper : public AutoFillDownloadManager::Observer {
   AutoFillDownloadTestHelper()
       : download_manager(&profile) {
     download_manager.SetObserver(this);
-    // For chromium builds forces Start*Request to actually execute.
-    download_manager.is_testing_ = true;
   }
   ~AutoFillDownloadTestHelper() {
     download_manager.SetObserver(NULL);
