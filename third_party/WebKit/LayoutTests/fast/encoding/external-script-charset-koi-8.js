ccd89643050579024275cc30f9d63a903a003556
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-05 20:30:15 +0000

    2011-02-05  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Alexey Proskuryakov.
    
            Add addition test for inherited encoding
            https://bugs.webkit.org/show_bug.cgi?id=53398
    
            Check if the right encoding is used, after document.charset was set.
    
            * fast/dom/Document/document-charset.html: Added doctype to make it pass in IE9 out of the box.
            * fast/encoding/css-charset-default-expected.txt:
            * fast/encoding/css-charset-default.xhtml:
            * fast/encoding/external-script-charset-expected.txt:
            * fast/encoding/external-script-charset-koi-8.js: Added.
            * fast/encoding/external-script-charset.js:
            * fast/encoding/external-script-charset.xhtml:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/encoding/external-script-charset-koi-8.js
@@ -0,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+document.getElementById("result2").innerHTML += ("ó" == "\u0421") ? "PASS" : "FAIL";
--- third_party/WebKit/LayoutTests/fast/encoding/external-script-charset.js
+++ third_party/WebKit/LayoutTests/fast/encoding/external-script-charset.js
@@ -2,4 +2,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (window.layoutTestController)
     layoutTestController.dumpAsText();
 
-document.getElementById("result").innerHTML = ("Я" == "\u042F") ? "PASS" : "FAIL";
+document.getElementById("result1").innerHTML += ("Я" == "\u042F") ? "PASS" : "FAIL";
