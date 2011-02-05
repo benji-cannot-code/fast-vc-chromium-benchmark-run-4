6f19e148b16bfb3b9c6ca30a007e7241d4f12b69
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-05 03:41:51 +0000

    Remove accidental comment in TabContents.
    BUG=none
    TEST=No silly presubmit checks for tab_contents.cc.
    TBR=estade
    Review URL: http://codereview.chromium.org/6368119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents.cc
+++ chrome/browser/tab_contents/tab_contents.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/tab_contents/tab_contents.h"
-// DO NOT SUBMIT
+
 #include <cmath>
 
 #include "base/auto_reset.h"
