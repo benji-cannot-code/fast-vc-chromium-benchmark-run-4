a049bd1949bdf9455fab1cce2302e66193a26f14
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-05 00:11:01 +0000

    2011-02-04  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            WebKit2 doesn't hide mouse pointer on keypress
            https://bugs.webkit.org/show_bug.cgi?id=53821
            rdar://problem/8630895
    
            * UIProcess/PageClient.h: Renamed didNotHandleKeyEvent to
            doneWithKeyEvent and added a bool wasEventHandled argument.
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::didReceiveEvent): Changed code to call
            doneWithKeyEvent and call even if event was handled.
    
            * UIProcess/API/mac/PageClientImpl.h: Updated for above change.
            * UIProcess/API/mac/PageClientImpl.mm:
            (WebKit::PageClientImpl::doneWithKeyEvent): Ditto. But also
            call [NSCursor setHiddenUntilMouseMoves:YES] when a key down
            event was handled. This matches what -[WebHTMLView keyDown:] does.
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPagePrivate::doneWithKeyEvent): Ditto.
            * UIProcess/API/qt/qwkpage_p.h: Ditto.
            * UIProcess/win/WebView.cpp:
            (WebKit::WebView::doneWithKeyEvent): Ditto.
            * UIProcess/win/WebView.h: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -233,7 +233,7 @@ void QWKPagePrivate::selectionChanged(bool, bool, bool, bool)
 {
 }
 
-void QWKPagePrivate::didNotHandleKeyEvent(const NativeWebKeyboardEvent&)
+void QWKPagePrivate::doneWithKeyEvent(const NativeWebKeyboardEvent&, bool)
 {
 }
 
--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -2238,10 +2238,11 @@ void WebPageProxy::didReceiveEvent(uint32_t opaqueType, bool handled)
 
         m_keyEventQueue.removeFirst();
 
+        m_pageClient->doneWithKeyEvent(event, handled);
+
         if (handled)
             break;
 
-        m_pageClient->didNotHandleKeyEvent(event);
         m_uiClient.didNotHandleKeyEvent(this, event);
         break;
     }
