0732936e6732a5bb3f44839e585ad4b86cbf5593
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-05 18:03:13 +0000

    Re-land part of r73427 where the plugin info and internal plugin info are unified (checking for perf regressions). Review URL: http://codereview.chromium.org/6413009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pepper_plugin_registry.cc
+++ chrome/common/pepper_plugin_registry.cc
@@ -49,15 +49,7 @@ PepperPluginRegistry* PepperPluginRegistry::GetInstance() {
 
 // static
 void PepperPluginRegistry::GetList(std::vector<PepperPluginInfo>* plugins) {
-  InternalPluginInfoList internal_plugin_info;
-  GetInternalPluginInfo(&internal_plugin_info);
-  for (InternalPluginInfoList::const_iterator it =
-         internal_plugin_info.begin();
-       it != internal_plugin_info.end();
-       ++it) {
-    plugins->push_back(*it);
-  }
-
+  GetInternalPluginInfo(plugins);
   GetPluginInfoFromSwitch(plugins);
   GetExtraPlugins(plugins);
 }
@@ -175,13 +167,9 @@ void PepperPluginRegistry::GetExtraPlugins(
   }
 }
 
-PepperPluginRegistry::InternalPluginInfo::InternalPluginInfo() {
-  is_internal = true;
-}
-
 // static
 void PepperPluginRegistry::GetInternalPluginInfo(
-    InternalPluginInfoList* plugin_info) {
+    std::vector<PepperPluginInfo>* plugin_info) {
   // Currently, to centralize the internal plugin registration logic, we
   // hardcode the list of plugins, mimetypes, and registration information
   // in this function.  This is gross, but because the GetList() function is
@@ -194,17 +182,17 @@ void PepperPluginRegistry::GetInternalPluginInfo(
   // plugin initializers that is built with static initializers?
 
 #if defined(ENABLE_REMOTING)
+  // Add the chromoting plugin.
   if (CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnableRemoting)) {
-    InternalPluginInfo info;
-    // Add the chromoting plugin.
-    DCHECK(info.is_internal);
-    info.path =
-        FilePath(FILE_PATH_LITERAL("internal-chromoting"));
+    PepperPluginInfo info;
+    info.is_internal;
+    info.path = FilePath(FILE_PATH_LITERAL("internal-chromoting"));
     info.mime_types.push_back("pepper-application/x-chromoting");
-    info.entry_points.get_interface = remoting::PPP_GetInterface;
-    info.entry_points.initialize_module = remoting::PPP_InitializeModule;
-    info.entry_points.shutdown_module = remoting::PPP_ShutdownModule;
+    info.internal_entry_points.get_interface = remoting::PPP_GetInterface;
+    info.internal_entry_points.initialize_module =
+        remoting::PPP_InitializeModule;
+    info.internal_entry_points.shutdown_module = remoting::PPP_ShutdownModule;
 
     plugin_info->push_back(info);
   }
@@ -262,18 +250,18 @@ PepperPluginRegistry::~PepperPluginRegistry() {
 }
 
 PepperPluginRegistry::PepperPluginRegistry() {
-  InternalPluginInfoList internal_plugin_info;
+  std::vector<PepperPluginInfo> internal_plugin_info;
   GetInternalPluginInfo(&internal_plugin_info);
 
   // Register modules for these suckers.
-  for (InternalPluginInfoList::const_iterator it =
+  for (std::vector<PepperPluginInfo>::const_iterator it =
          internal_plugin_info.begin();
        it != internal_plugin_info.end();
        ++it) {
     const FilePath& path = it->path;
     scoped_refptr<webkit::ppapi::PluginModule> module(
         new webkit::ppapi::PluginModule(this));
-    if (!module->InitAsInternalPlugin(it->entry_points)) {
+    if (!module->InitAsInternalPlugin(it->internal_entry_points)) {
       DLOG(ERROR) << "Failed to load pepper module: " << path.value();
       continue;
     }
