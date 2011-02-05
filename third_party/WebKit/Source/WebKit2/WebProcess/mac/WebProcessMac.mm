78b6878233bedf2933fcdd97a0905bca2a5c26bf
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-05 01:33:44 +0000

    Fix build.
    * WebProcess/mac/WebProcessMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
@@ -111,6 +111,7 @@ void WebProcess::platformClearResourceCaches()
     [[NSURLCache sharedURLCache] removeAllCachedResponses];
 }
 
+#if ENABLE(WEB_PROCESS_SANDBOX)
 static void appendSandboxParameterPath(Vector<const char*>& vector, const char* name, const char* path)
 {
     char normalizedPath[PATH_MAX];
@@ -129,6 +130,7 @@ static void appendSandboxParameterConfPath(Vector<const char*>& vector, const ch
 
     appendSandboxParameterPath(vector, name, path);
 }
+#endif
 
 static void initializeSandbox(const WebProcessCreationParameters& parameters)
 {
