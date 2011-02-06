58900b3316cdacb4515ade3b2e67f336eb2573cf
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-06 20:05:38 +0000

    2011-02-06  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Antti Koivisto.
    
            media/video-document-types.html sometimes crashes the WebProcess
            https://bugs.webkit.org/show_bug.cgi?id=53884
    
            No new tests. Already covered by media/video-document-types.html
    
            * html/HTMLMediaElement.cpp:
            (WebCore::HTMLMediaElement::userCancelledLoad): Stop the load timer
            when cancelling the load.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -2173,6 +2173,7 @@ void HTMLMediaElement::userCancelledLoad()
     m_player.clear();
 #endif
     stopPeriodicTimers();
+    m_loadTimer.stop();
     m_loadState = WaitingForSource;
 
     // 2 - Set the error attribute to a new MediaError object whose code attribute is set to MEDIA_ERR_ABORTED.
