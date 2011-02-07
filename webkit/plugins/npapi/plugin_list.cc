490029c7afca102bfe1285998d5c050c07b8c9c7
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-07 12:01:33 +0000

    Correctly match plugins to their definitions.
    BUG=71781
    TEST=PluginListTest.HardcodedGroups
    
    Review URL: http://codereview.chromium.org/6286081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73984 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_list.cc
+++ webkit/plugins/npapi/plugin_list.cc
@@ -328,7 +328,7 @@ PluginList::PluginList()
       plugins_need_refresh_(false),
       disable_outdated_plugins_(false) {
   PlatformInit();
-  AddHardcodedPluginGroups();
+  AddHardcodedPluginGroups(&plugin_groups_);
 }
 
 void PluginList::LoadPluginsInternal(ScopedVector<PluginGroup>* plugin_groups) {
@@ -396,6 +396,7 @@ void PluginList::LoadPlugins(bool refresh) {
   }
 
   ScopedVector<PluginGroup> new_plugin_groups;
+  AddHardcodedPluginGroups(&new_plugin_groups);
   // Do the actual loading of the plugins.
   LoadPluginsInternal(&new_plugin_groups);
 
@@ -692,14 +693,12 @@ std::string PluginList::GetPluginGroupIdentifier(
   return group->identifier();
 }
 
-void PluginList::AddHardcodedPluginGroups() {
+void PluginList::AddHardcodedPluginGroups(ScopedVector<PluginGroup>* groups) {
   base::AutoLock lock(lock_);
   const PluginGroupDefinition* definitions = GetPluginGroupDefinitions();
-  for (size_t i = 0; i < GetPluginGroupDefinitionsSize(); ++i) {
-    PluginGroup* definition_group = PluginGroup::FromPluginGroupDefinition(
-        definitions[i]);
-    plugin_groups_.push_back(definition_group);
-  }
+  size_t num_definitions = GetPluginGroupDefinitionsSize();
+  for (size_t i = 0; i < num_definitions; ++i)
+    groups->push_back(PluginGroup::FromPluginGroupDefinition(definitions[i]));
 }
 
 PluginGroup* PluginList::AddToPluginGroups(
@@ -732,11 +731,8 @@ PluginGroup* PluginList::AddToPluginGroups(
   group->AddPlugin(web_plugin_info);
   // If group is scheduled for disabling do that now and remove it from the
   // list.
-  if (groups_to_disable_.find(group->GetGroupName()) !=
-      groups_to_disable_.end()) {
-      group->EnableGroup(false);
-      groups_to_disable_.erase(group->GetGroupName());
-  }
+  if (groups_to_disable_.erase(group->GetGroupName()))
+    group->EnableGroup(false);
   return group;
 }
 
@@ -759,7 +755,6 @@ bool PluginList::DisablePlugin(const FilePath& filename) {
   }
   // Non existing plugin is being disabled. Queue the plugin so that on the next
   // load plugins call they will be disabled.
-  // Check if we already have this one to avoid double inclusion.
   plugins_to_disable_.insert(filename);
   return true;
 }
