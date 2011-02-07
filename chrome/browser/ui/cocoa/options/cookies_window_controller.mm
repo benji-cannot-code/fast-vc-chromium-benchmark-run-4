973e4cf9ed91236518b08746d0fa6ea533906013
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-07 20:06:55 +0000

    Fix a warning found by Clang:
    chrome/browser/cookies_tree_model.h:497:16: error:
        'CookiesTreeModel::AddObserver' hides overloaded virtual function [-Woverloaded-virtual]
    
    Same with RemoveObserver().
    
    BUG=72149
    TEST=Clang compiles
    
    Review URL: http://codereview.chromium.org/6368130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/options/cookies_window_controller.mm
+++ chrome/browser/ui/cocoa/options/cookies_window_controller.mm
@@ -423,7 +423,7 @@ bool CookiesTreeModelObserverBridge::HasCocoaModel() {
       appcacheHelper_,
       indexedDBHelper_));
   modelObserver_.reset(new CookiesTreeModelObserverBridge(self));
-  treeModel_->AddObserver(modelObserver_.get());
+  treeModel_->AddCookiesTreeObserver(modelObserver_.get());
 
   // Convert the model's icons from Skia to Cocoa.
   std::vector<SkBitmap> skiaIcons;
