cae35c47169190fd3f997cbd107e648a93c3067a
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-07 05:47:24 +0000

    2011-02-06  Ryosuke Niwa  <rniwa@webkit.org>
            Another unreviewed Chromium build fix.
    
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::pathToLocalResource):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -804,7 +804,7 @@ void LayoutTestController::pathToLocalResource(const CppArgumentList& arguments,
         DWORD tempLength = ::GetTempPathW(bufferSize, tempPath.get());
         if (tempLength + url.length() - tempPrefixLength + 1 > bufferSize) {
             bufferSize = tempLength + url.length() - tempPrefixLength + 1;
-            tempPath.set(new WCHAR[bufferSize]);
+            tempPath = adoptArrayPtr(new WCHAR[bufferSize]);
             tempLength = GetTempPathW(bufferSize, tempPath.get());
             ASSERT(tempLength < bufferSize);
         }
