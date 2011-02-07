a523f903b6612064739b7ec8b547180074a03504
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-07 18:26:54 +0000

    2011-02-07  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Add WebPage::bounds and use it
            https://bugs.webkit.org/show_bug.cgi?id=53926
    
            * WebProcess/WebPage/ChunkedUpdateDrawingArea.cpp:
            (WebKit::ChunkedUpdateDrawingArea::resumePainting):
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            (WebKit::DrawingAreaImpl::resumePainting):
            (WebKit::DrawingAreaImpl::exitAcceleratedCompositingMode):
            * WebProcess/WebPage/WebPage.h:
            (WebKit::WebPage::bounds):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -230,7 +230,7 @@ void DrawingAreaImpl::resumePainting()
     m_isPaintingSuspended = false;
 
     // FIXME: We shouldn't always repaint everything here.
-    setNeedsDisplay(IntRect(IntPoint(), m_webPage->size()));
+    setNeedsDisplay(m_webPage->bounds());
 }
 
 void DrawingAreaImpl::enterAcceleratedCompositingMode(GraphicsLayer* graphicsLayer)
@@ -265,7 +265,7 @@ void DrawingAreaImpl::exitAcceleratedCompositingMode()
     if (m_isPaintingSuspended)
         updateInfo.viewSize = m_webPage->size();
     else {
-        m_dirtyRegion = IntRect(IntPoint(), m_webPage->size());
+        m_dirtyRegion = m_webPage->bounds();
         display(updateInfo);
     }
 
