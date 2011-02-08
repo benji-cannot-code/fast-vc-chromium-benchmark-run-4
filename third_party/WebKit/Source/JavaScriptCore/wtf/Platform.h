69094680d11665b2f0d844708b0b93070a786b62
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-08 17:27:54 +0000

    2011-02-08  Balazs Kelemen  <kbalazs@webkit.org>
            Reviewed by Andreas Kling.
    
            [Qt] Should not always define USE_SYSTEM_MALLOC
            https://bugs.webkit.org/show_bug.cgi?id=54007
    
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -838,7 +838,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(QT)
 /* We must not customize the global operator new and delete for the Qt port. */
 #define ENABLE_GLOBAL_FASTMALLOC_NEW 0
-#if !PLATFORM(UNIX) || OS(SYMBIAN)
+#if !OS(UNIX) || OS(SYMBIAN)
 #define USE_SYSTEM_MALLOC 1
 #endif
 #endif
