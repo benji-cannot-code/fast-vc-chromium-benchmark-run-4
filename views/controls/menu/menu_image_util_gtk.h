d6e48ba4e216d335406ad7040755b79c84742898
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-08 20:58:04 +0000

    Fix submenu arrow position for RTL Flip the direction of submenu arrow for RTL
    BUG=chromium-os:5593
    TEST=none
    
    Review URL: http://codereview.chromium.org/6428001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74171 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/menu_image_util_gtk.h
+++ views/controls/menu/menu_image_util_gtk.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef VIEWS_CONTROLS_MENU_RADIO_BUTTON_IMAGE_H_
-#define VIEWS_CONTROLS_MENU_RADIO_BUTTON_IMAGE_H_
+#ifndef VIEWS_CONTROLS_MENU_MENU_IMAGE_UTIL_GTK_H_
+#define VIEWS_CONTROLS_MENU_MENU_IMAGE_UTIL_GTK_H_
 #pragma once
 
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -17,6 +17,9 @@ namespace views {
 // The returned image is global object and should not be freed.
 const SkBitmap* GetRadioButtonImage(bool selected);
 
+// Returns the image for submenu arrow for current RTL setting.
+const SkBitmap* GetSubmenuArrowImage();
+
 } // namespace views
 
-#endif  // VIEWS_CONTROLS_MENU_RADIO_BUTTON_IMAGE_H_
+#endif  // VIEWS_CONTROLS_MENU_MENU_IMAGE_UTIL_GTK_H_
