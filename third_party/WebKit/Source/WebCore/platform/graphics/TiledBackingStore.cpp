f61ebede899b9eef32573390d1c2ff96300e1a78
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-08 15:08:05 +0000

    2011-02-08  Andreas Kling  <kling@webkit.org>
            Reviewed by Tor Arne Vestbø.
    
            REGRESSION(r77312): Unbreak TiledBackingStore.
    
            Due to an off-by-one error, we were no longer rendering the
            rightmost column and bottom row of tiles.
    
            Covered by tst_QGraphicsWebView::widgetsRenderingThroughCache().
    
            * platform/graphics/TiledBackingStore.cpp:
            (WebCore::TiledBackingStore::invalidate):
            (WebCore::TiledBackingStore::updateTileBuffers):
            (WebCore::TiledBackingStore::paint):
            (WebCore::TiledBackingStore::createTiles):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/TiledBackingStore.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/TiledBackingStore.cpp
@@ -77,8 +77,8 @@ void TiledBackingStore::invalidate(const IntRect& contentsDirtyRect)
     Tile::Coordinate topLeft = tileCoordinateForPoint(dirtyRect.location());
     Tile::Coordinate bottomRight = tileCoordinateForPoint(IntPoint(dirtyRect.maxX(), dirtyRect.maxY()));
     
-    for (unsigned yCoordinate = topLeft.y(); yCoordinate < bottomRight.y(); ++yCoordinate) {
-        for (unsigned xCoordinate = topLeft.x(); xCoordinate < bottomRight.x(); ++xCoordinate) {
+    for (unsigned yCoordinate = topLeft.y(); yCoordinate <= bottomRight.y(); ++yCoordinate) {
+        for (unsigned xCoordinate = topLeft.x(); xCoordinate <= bottomRight.x(); ++xCoordinate) {
             RefPtr<Tile> currentTile = tileAt(Tile::Coordinate(xCoordinate, yCoordinate));
             if (!currentTile)
                 continue;
@@ -111,6 +111,7 @@ void TiledBackingStore::updateTileBuffers()
         m_client->tiledBackingStorePaintEnd(paintedArea);
         return;
     }
+
     // FIXME: In single threaded case, tile back buffers could be updated asynchronously 
     // one by one and then swapped to front in one go. This would minimize the time spent
     // blocking on tile updates.
@@ -137,8 +138,8 @@ void TiledBackingStore::paint(GraphicsContext* context, const IntRect& rect)
     Tile::Coordinate topLeft = tileCoordinateForPoint(dirtyRect.location());
     Tile::Coordinate bottomRight = tileCoordinateForPoint(IntPoint(dirtyRect.maxX(), dirtyRect.maxY()));
 
-    for (unsigned yCoordinate = topLeft.y(); yCoordinate < bottomRight.y(); ++yCoordinate) {
-        for (unsigned xCoordinate = topLeft.x(); xCoordinate < bottomRight.x(); ++xCoordinate) {
+    for (unsigned yCoordinate = topLeft.y(); yCoordinate <= bottomRight.y(); ++yCoordinate) {
+        for (unsigned xCoordinate = topLeft.x(); xCoordinate <= bottomRight.x(); ++xCoordinate) {
             Tile::Coordinate currentCoordinate(xCoordinate, yCoordinate);
             RefPtr<Tile> currentTile = tileAt(currentCoordinate);
             if (currentTile && currentTile->isReadyToPaint())
@@ -231,8 +232,8 @@ void TiledBackingStore::createTiles()
     unsigned requiredTileCount = 0;
     Tile::Coordinate topLeft = tileCoordinateForPoint(coverRect.location());
     Tile::Coordinate bottomRight = tileCoordinateForPoint(IntPoint(coverRect.maxX(), coverRect.maxY()));
-    for (unsigned yCoordinate = topLeft.y(); yCoordinate < bottomRight.y(); ++yCoordinate) {
-        for (unsigned xCoordinate = topLeft.x(); xCoordinate < bottomRight.x(); ++xCoordinate) {
+    for (unsigned yCoordinate = topLeft.y(); yCoordinate <= bottomRight.y(); ++yCoordinate) {
+        for (unsigned xCoordinate = topLeft.x(); xCoordinate <= bottomRight.x(); ++xCoordinate) {
             Tile::Coordinate currentCoordinate(xCoordinate, yCoordinate);
             if (tileAt(currentCoordinate))
                 continue;
