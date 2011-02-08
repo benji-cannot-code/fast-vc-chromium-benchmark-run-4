78cb43b9536525a293a7b77d77de73d0b2428bb3
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-08 03:08:59 +0000

    2011-02-07  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
    
            Removed some dead code from Heap
            https://bugs.webkit.org/show_bug.cgi?id=53969
            
            SunSpider reports no change.
    
            * runtime/MarkedSpace.cpp:
            (JSC::MarkedSpace::shrink):
            (JSC::MarkedSpace::sweep):
            * runtime/MarkedSpace.h: Removed resizeBlocks and growBlocks, and
            renamed shrinkBlocks to shrink, making it unconditionally shrink as
            much as possible.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/MarkedSpace.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/MarkedSpace.h
@@ -97,9 +97,7 @@ namespace JSC {
 
         NEVER_INLINE MarkedBlock* allocateBlock();
         NEVER_INLINE void freeBlock(size_t);
-        void resizeBlocks();
-        void growBlocks(size_t neededBlocks);
-        void shrinkBlocks(size_t neededBlocks);
+        void shrink();
 
         void clearMarkBits(MarkedBlock*);
 
