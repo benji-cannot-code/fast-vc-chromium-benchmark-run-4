47e6f96ae5dcedafdedb9604d82c45cca0dcd348
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-08 06:02:17 +0000

    2011-02-07  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Eric Seidel.
    
            Add missing PaintInfo include in RenderThemeWin.cpp
            https://bugs.webkit.org/show_bug.cgi?id=53885
    
            * rendering/RenderThemeWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77895 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeWin.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeWin.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "GraphicsContext.h"
 #include "LocalWindowsContext.h"
+#include "PaintInfo.h"
 #include "RenderSlider.h"
 #include "Settings.h"
 #include "SoftLinking.h"
