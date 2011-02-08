93a209674f4f2fc45a526612a18870bb356c6397
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-08 16:47:12 +0000

    2011-02-08  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Roben.
    
            [WIN] Add missing include in WKCAImageQueue
            https://bugs.webkit.org/show_bug.cgi?id=53890
    
            * platform/graphics/win/WKCAImageQueue.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@77943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/win/WKCAImageQueue.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/win/WKCAImageQueue.cpp
@@ -25,11 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "WKCAImageQueue.h"
 
 #if USE(ACCELERATED_COMPOSITING)
 
-#include "WKCAImageQueue.h"
-
+#include <CoreFoundation/CoreFoundation.h>
 #include <WebKitSystemInterface/WebKitSystemInterface.h>
 #include <wtf/RetainPtr.h>
 
