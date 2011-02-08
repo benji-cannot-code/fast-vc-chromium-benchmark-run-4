0fa2bd6dbdbfa9fa0ecfaf5e25d2dbe14dc43f31
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-08 09:26:20 +0000

    Add some more debugging info to the --diagnostics for when checking filepath.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6413016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74104 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/diagnostics/recon_diagnostics.cc
+++ chrome/browser/diagnostics/recon_diagnostics.cc
@@ -241,7 +241,8 @@ class PathTest : public DiagnosticTest {
       return false;
     }
     if (!file_util::PathExists(dir_or_file)) {
-      RecordFailure(ASCIIToUTF16("Path not found"));
+      RecordFailure(ASCIIToUTF16("Path not found: ") +
+                    dir_or_file.LossyDisplayName());
       return true;
     }
 
@@ -252,7 +253,8 @@ class PathTest : public DiagnosticTest {
       file_util::GetFileSize(dir_or_file, &dir_or_file_size);
     }
     if (!dir_or_file_size && !path_info_.is_optional) {
-      RecordFailure(ASCIIToUTF16("Cannot obtain size"));
+      RecordFailure(ASCIIToUTF16("Cannot obtain size for: ") +
+                    dir_or_file.LossyDisplayName());
       return true;
     }
     DataUnits units = GetByteDisplayUnits(dir_or_file_size);
@@ -260,7 +262,10 @@ class PathTest : public DiagnosticTest {
 
     if (path_info_.max_size > 0) {
       if (dir_or_file_size > path_info_.max_size) {
-        RecordFailure(ASCIIToUTF16("Path is too big: ") + printable_size);
+        RecordFailure(ASCIIToUTF16("Path contents too large (") +
+                      printable_size +
+                      ASCIIToUTF16(") for: ") +
+                      dir_or_file.LossyDisplayName());
         return true;
       }
     }
@@ -269,7 +274,8 @@ class PathTest : public DiagnosticTest {
       return true;
     }
     if (!file_util::PathIsWritable(dir_or_file)) {
-      RecordFailure(ASCIIToUTF16("Path is not writable"));
+      RecordFailure(ASCIIToUTF16("Path is not writable: ") +
+                    dir_or_file.LossyDisplayName());
       return true;
     }
     RecordSuccess(ASCIIToUTF16("Path exists and is writable: ")
