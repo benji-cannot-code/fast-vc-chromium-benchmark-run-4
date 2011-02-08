27c1780cf31c1d74ee9edd9829e3f2c23db67832
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-08 23:07:15 +0000

    Fix favicon display in prerendering BUG=71222 TEST=prerender & navigate to a URL, verify the favicon
    Review URL: http://codereview.chromium.org/6462002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_thread.h"
+#include "chrome/browser/fav_icon_helper.h"
 #include "chrome/browser/prerender/prerender_contents.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
@@ -141,6 +142,10 @@ bool PrerenderManager::MaybeUsePreloadedPage(TabContents* tc, const GURL& url) {
   string16 title = pc->title();
   if (!title.empty())
     tc->UpdateTitle(rvh, pc->page_id(), UTF16ToWideHack(title));
+
+  GURL icon_url = pc->icon_url();
+  if (!icon_url.is_empty())
+    tc->fav_icon_helper().OnUpdateFavIconURL(pc->page_id(), icon_url);
 
   if (pc->has_stopped_loading())
     tc->DidStopLoading();
