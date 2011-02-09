977e492ee460de4a0c64144be2e7ee7a721338cb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 22:36:22 +0000

    2011-02-09  Ilya Sherman  <isherman@chromium.org>
            Reviewed by Darin Fisher.
    
            Export WebInputElement::defaultMaxLength as a function rather than a constant.
            Needed because the order of initialization of static constants
            is not guaranteed across compilation units.
            https://bugs.webkit.org/show_bug.cgi?id=54056
    
            * public/WebInputElement.h:
            * src/WebInputElement.cpp:
            (WebKit::WebInputElement::defaultMaxLength): Now a static function.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebInputElement.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebInputElement.cpp
@@ -156,7 +156,10 @@ bool WebInputElement::isValidValue(const WebString& value) const
     return constUnwrap<HTMLInputElement>()->isValidValue(value);
 }
 
-const int WebInputElement::defaultMaxLength = HTMLInputElement::s_maximumLength;
+int WebInputElement::defaultMaxLength()
+{
+    return HTMLInputElement::s_maximumLength;
+}
 
 WebInputElement::WebInputElement(const PassRefPtr<HTMLInputElement>& elem)
     : WebFormControlElement(elem)
