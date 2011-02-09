1fa638d6ebe50788fd35ffded567e69ec5b9c75d
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 10:12:07 +0000

    2011-02-08  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: script formatter should not create a worker for each script.
            https://bugs.webkit.org/show_bug.cgi?id=54010
    
            * inspector/front-end/ScriptFormatter.js:
            (WebInspector.ScriptFormatter):
            (WebInspector.ScriptFormatter.prototype._formatScript):
            (WebInspector.ScriptFormatter.prototype._handleMessage):
            (WebInspector.ScriptFormatter.prototype._handleError):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptFormatter.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptFormatter.js
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebInspector.ScriptFormatter = function()
 {
+    this._worker = new Worker("ScriptFormatterWorker.js");
+    this._worker.onmessage = this._handleMessage.bind(this);
+    this._worker.onerror = this._handleError.bind(this);
+    this._tasks = [];
 }
 
 WebInspector.ScriptFormatter.prototype = {
@@ -121,12 +125,21 @@ WebInspector.ScriptFormatter.prototype = {
 
     _formatScript: function(source, callback)
     {
-        var worker = new Worker("ScriptFormatterWorker.js");
-        function messageHandler(event)
-        {
-            callback(event.data.formattedSource, event.data.mapping);
-        }
-        worker.onmessage = messageHandler.bind(this);
-        worker.postMessage(source);
+        this._tasks.push({ source: source, callback: callback });
+        this._worker.postMessage(source);
+    },
+
+    _handleMessage: function(event)
+    {
+        var task = this._tasks.shift();
+        task.callback(event.data.formattedSource, event.data.mapping);
+    },
+
+    _handleError: function(event)
+    {
+        console.warn("Error in script formatter worker:", event);
+        event.preventDefault()
+        var task = this._tasks.shift();
+        task.callback(task.source, { original: [], formatted: [] });
     }
 }
