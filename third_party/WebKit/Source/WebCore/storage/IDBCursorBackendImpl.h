5444f6601a2d9100d721b01e849c3a1f077d8351
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 10:33:31 +0000

    2011-02-09  Hans Wennborg  <hans@chromium.org>
            Reviewed by Jeremy Orlow.
    
            IndexedDB: Cursors should skip deleted entries
            https://bugs.webkit.org/show_bug.cgi?id=53690
    
            Cursors should skip over entries that have been deleted
            since the cursor was opened.
    
            * storage/indexeddb/cursor-skip-deleted-expected.txt: Added.
            * storage/indexeddb/cursor-skip-deleted.html: Added.
    2011-02-09  Hans Wennborg  <hans@chromium.org>
    
            Reviewed by Jeremy Orlow.
    
            IndexedDB: Cursors should skip deleted entries
            https://bugs.webkit.org/show_bug.cgi?id=53690
    
            Add test to check that the cursor skips deleted entries.
    
            Test: storage/indexeddb/cursor-skip-deleted.html
    
            * storage/IDBCursorBackendImpl.cpp:
            (WebCore::IDBCursorBackendImpl::currentRowExists):
            (WebCore::IDBCursorBackendImpl::continueFunctionInternal):
            * storage/IDBCursorBackendImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBCursorBackendImpl.h
+++ third_party/WebKit/Source/WebCore/storage/IDBCursorBackendImpl.h
@@ -66,6 +66,7 @@ public:
 private:
     IDBCursorBackendImpl(IDBSQLiteDatabase*, PassRefPtr<IDBKeyRange>, IDBCursor::Direction, PassOwnPtr<SQLiteStatement> query, bool isSerializedScriptValueCursor, IDBTransactionBackendInterface*, IDBObjectStoreBackendInterface*);
 
+    bool currentRowExists();
     void loadCurrentRow();
     SQLiteDatabase& database() const;
 
