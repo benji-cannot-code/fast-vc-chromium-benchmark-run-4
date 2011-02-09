7f36a6aa2d79dc518a75933e90c4fb6d0bce96b2
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-09 03:20:58 +0000

    Mac: Turn on tab overview mode by default.
    BUG=50307,72362
    TEST=three-finger-swipe down and Window->Tab Overview both open tab overview mode
    
    Review URL: http://codereview.chromium.org/6459009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1397,11 +1397,6 @@ void Browser::SelectPreviousTab() {
 
 void Browser::OpenTabpose() {
 #if defined(OS_MACOSX)
-  if (!CommandLine::ForCurrentProcess()->HasSwitch(
-        switches::kEnableExposeForTabs)) {
-    return;
-  }
-
   UserMetrics::RecordAction(UserMetricsAction("OpenTabpose"), profile_);
   window()->OpenTabpose();
 #else
