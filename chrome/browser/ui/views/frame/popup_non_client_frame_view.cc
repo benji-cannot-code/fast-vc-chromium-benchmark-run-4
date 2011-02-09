5f98917e29158f2eee63f4872ba620b99f4dc1df
rjkroege@google.com <rjkroege@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-09 22:11:54 +0000

    BackingStoreSkia: Use skia for painting in RenderWidgetHostViewViews.
    Instead of directly painting to X, paint to the skia canvas from RWHVV. This
    will make it easier to apply transformation to views in a uniform way, without
    requiring any special-casing for RWHVV.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6246129
    Patch from Chad Faragher <wyck@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/popup_non_client_frame_view.cc
+++ chrome/browser/ui/views/frame/popup_non_client_frame_view.cc
@@ -47,7 +47,7 @@ void PopupNonClientFrameView::ResetWindowControls() {
 
 gfx::Rect PopupNonClientFrameView::GetBoundsForTabStrip(
     BaseTabStrip* tabstrip) const {
-  return gfx::Rect(0, 0, width(), tabstrip->GetPreferredHeight());
+  return gfx::Rect(0, 0, width(), tabstrip->GetPreferredSize().height());
 }
 
 int PopupNonClientFrameView::GetHorizontalTabStripVerticalOffset(
