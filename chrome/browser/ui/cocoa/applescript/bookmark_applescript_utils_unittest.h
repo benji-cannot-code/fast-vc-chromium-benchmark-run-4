33d307e85d4ed1a5e528d65444f18d0606632621
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-09 20:11:20 +0000

    [Mac] Kill a zombie left by BookmarkAppleScriptTest.
    In the ctor, |-[NSApp delegate]| is set to a test-local object but is not nil-ed
    at destruction, so a zombie is left as the app's delegate. This can ultimately
    lead to a crash.
    
    BUG=72446
    TEST=Nondeterministic, but all unit_tests pass.
    
    Review URL: http://codereview.chromium.org/6458031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/applescript/bookmark_applescript_utils_unittest.h
+++ chrome/browser/ui/cocoa/applescript/bookmark_applescript_utils_unittest.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,7 @@ class BookmarkModel;
 class BookmarkAppleScriptTest : public CocoaTest {
  public:
   BookmarkAppleScriptTest();
+  virtual ~BookmarkAppleScriptTest();
  private:
   BrowserTestHelper helper_;
   scoped_nsobject<FakeAppDelegate> appDelegate_;
--- chrome/browser/ui/cocoa/applescript/bookmark_applescript_utils_unittest.mm
+++ chrome/browser/ui/cocoa/applescript/bookmark_applescript_utils_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -50,12 +50,17 @@ static FakeScriptCommand* kFakeCurrentCommand;
 BookmarkAppleScriptTest::BookmarkAppleScriptTest() {
   appDelegate_.reset([[FakeAppDelegate alloc] init]);
   [appDelegate_.get() setHelper:&helper_];
+  DCHECK([NSApp delegate] == nil);
   [NSApp setDelegate:appDelegate_];
   const BookmarkNode* root = model().GetBookmarkBarNode();
   const std::string modelString("a f1:[ b d c ] d f2:[ e f g ] h ");
   model_test_utils::AddNodesFromModelString(model(), root, modelString);
   bookmarkBar_.reset([[BookmarkFolderAppleScript alloc]
       initWithBookmarkNode:model().GetBookmarkBarNode()]);
+}
+
+BookmarkAppleScriptTest::~BookmarkAppleScriptTest() {
+  [NSApp setDelegate:nil];
 }
 
 BookmarkModel& BookmarkAppleScriptTest::model() {
