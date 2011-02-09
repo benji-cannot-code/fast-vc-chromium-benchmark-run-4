1f52588cbfcea291992b620e3c426e153b54d54f
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 17:45:08 +0000

    2011-02-09  Chandra Vallala  <chandra.vallala@motorola.com> and Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Implement WKBaseGtk API for Webkit2
            https://bugs.webkit.org/show_bug.cgi?id=54081
    
            * GNUmakefile.am: Added new files to compilation.
            * Shared/API/c/WKBase.h: Included WKBaseGtk.h in this file.
            * Shared/API/c/gtk: Added.
            * Shared/API/c/gtk/WKBaseGtk.h: Added. Declaration of WKViewRef
            for GTK port.
            * UIProcess/API/C/WKAPICast.h: Included WKAPICastGtk.h in this
            file.
            * UIProcess/API/C/gtk: Added.
            * UIProcess/API/C/gtk/WKAPICastGtk.h: Added. Type definition
            template for WKViewRef to WebView and vice-versa.
            * UIProcess/API/C/gtk/WKView.cpp: Added. Implementation of WKView
            API interface.
            (WKViewCreate):
            (WKViewGetWindow):
            (WKViewGetPage):
            (WKURLCreateWithURL):
            * UIProcess/API/C/gtk/WKView.h: Added. Declaration of WKView API
            interface.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/API/c/WKBase.h
+++ third_party/WebKit/Source/WebKit2/Shared/API/c/WKBase.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
+ * Portions Copyright (c) 2010 Motorola Mobility, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(WIN32) || defined(_WIN32)
 #include <WebKit2/WKBaseWin.h>
+#endif
+
+#if defined(BUILDING_GTK__)
+#include <WebKit2/WKBaseGtk.h>
 #endif
 
 /* WebKit2 shared types */
--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKAPICast.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKAPICast.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Portions Copyright (c) 2010 Motorola Mobility, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -231,4 +232,7 @@ inline WebCore::CredentialPersistence toCredentialPersistence(WKCredentialPersis
 #include "WKAPICastWin.h"
 #endif
 
+#if defined(BUILDING_GTK__)
+#include "WKAPICastGtk.h"
+#endif
 #endif // WKAPICast_h
