6695125232b5a56af05ddd19cad44378c3661438
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 20:22:37 +0000

    Try to fix the build.
    * wtf/Bitmap.h: Include string.h for memset. Not sure why this started
    failing now.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Bitmap.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Bitmap.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FixedArray.h"
 #include "StdLibExtras.h"
 #include <stdint.h>
+#include <string.h>
 
 namespace WTF {
 
