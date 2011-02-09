4a9cfa0d6d7f174082d9963f6541468733aaabee
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-09 05:42:37 +0000

    Do not pass CRYPT_NOHASHOID to CryptSignHash with CALG_SSL3_SHAMD5. This may fix bug 72041.
    R=wtc
    BUG=72041
    TEST=covered by existing net_unittests
    
    Review URL: http://codereview.chromium.org/6458023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74254 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/sslplatf.c
+++ net/third_party/nss/ssl/sslplatf.c
@@ -264,7 +264,7 @@ ssl3_PlatformSignHashes(SSL3Hashes *hash, PlatformKey key, SECItem *buf,
         PORT_SetError(SSL_ERROR_SIGN_HASHES_FAILURE);
         goto done;
     }
-    if (!CryptSignHash(hHash, key->dwKeySpec, NULL, CRYPT_NOHASHOID,
+    if (!CryptSignHash(hHash, key->dwKeySpec, NULL, 0,
                        NULL, &signatureLen) || signatureLen == 0) {
         PORT_SetError(SSL_ERROR_SIGN_HASHES_FAILURE);
         goto done;
@@ -273,7 +273,7 @@ ssl3_PlatformSignHashes(SSL3Hashes *hash, PlatformKey key, SECItem *buf,
     if (!buf->data)
         goto done;    /* error code was set. */
 
-    if (!CryptSignHash(hHash, key->dwKeySpec, NULL, CRYPT_NOHASHOID,
+    if (!CryptSignHash(hHash, key->dwKeySpec, NULL, 0,
                        (BYTE*)buf->data, &signatureLen)) {
         PORT_SetError(SSL_ERROR_SIGN_HASHES_FAILURE);
         goto done;
