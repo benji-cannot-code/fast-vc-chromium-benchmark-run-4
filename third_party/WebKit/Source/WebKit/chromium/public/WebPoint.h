becf9a768df0df94cf595ecbc0a4599f78fb9b74
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 20:15:07 +0000

    2011-02-09  Sailesh Agrawal  <sail@chromium.org>
            Reviewed by Tony Chang.
    
            Search/replace references to gfx/* with ui/gfx/*
    
            * public/WebPoint.h:
            * public/WebRect.h:
            * public/WebSize.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebPoint.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebPoint.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if WEBKIT_IMPLEMENTATION
 #include "IntPoint.h"
 #else
-#include <gfx/point.h>
+#include <ui/gfx/point.h>
 #endif
 
 namespace WebKit {
