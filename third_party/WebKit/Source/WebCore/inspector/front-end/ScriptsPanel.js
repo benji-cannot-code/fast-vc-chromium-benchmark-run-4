fcdab0de1243e20185c3a53ff1af1b1ef42abeef
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-09 15:28:57 +0000

    2011-02-09  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: source frame scroll position is not restored.
            https://bugs.webkit.org/show_bug.cgi?id=54101
    
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype._scriptSourceChanged):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype.show):
            (WebInspector.SourceFrame.prototype.hide):
            (WebInspector.SourceFrame.prototype.get scrollTop):
            (WebInspector.SourceFrame.prototype.set scrollTop):
            * inspector/front-end/TextViewer.js:
            (WebInspector.TextViewer.prototype.get scrollTop):
            (WebInspector.TextViewer.prototype.set scrollTop):
            (WebInspector.TextViewer.prototype.get scrollLeft):
            (WebInspector.TextViewer.prototype.set scrollLeft):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -269,6 +269,7 @@ WebInspector.ScriptsPanel.prototype = {
             delete script._sourceFrame;
             newView = this._sourceFrameForScript(script);
         }
+        newView.scrollTop = oldView.scrollTop;
 
         if (this.visibleView === oldView)
             this.visibleView = newView;
