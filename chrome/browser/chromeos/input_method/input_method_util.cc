d867015f4aa4769f6ae85cb19918d92294dcc766
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 09:21:52 +0000

    Set the hardware keyboard layout based on startup_manifest.json.
    For instance, if initial_locale in startup_manifest.json is set to "fr",
    we'll set up the hardware keyboard to French, and use it in the OOBE
    screen.
    
    BUG=chromium-os:10964
    TEST=manually on the netbook. Confirm that French keyboard layout is enabled by default by putting startup_manifest.json with initial_locale set to "fr".
    
    Review URL: http://codereview.chromium.org/6463018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_util.cc
+++ chrome/browser/chromeos/input_method/input_method_util.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/keyboard_library.h"
 #include "chrome/browser/chromeos/language_preferences.h"
+#include "chrome/browser/prefs/pref_service.h"
+#include "chrome/common/pref_names.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_collator.h"
@@ -600,8 +602,20 @@ void EnableInputMethods(const std::string& language_code, InputMethodType type,
 }
 
 std::string GetHardwareInputMethodId() {
-  // TODO(satorux): Rework this function. crosbug.com/11528.
-  return GetFallbackInputMethodDescriptor().id;
+  if (!(g_browser_process && g_browser_process->local_state())) {
+    // This shouldn't happen but just in case.
+    LOG(ERROR) << "Local state is not yet ready";
+    return GetFallbackInputMethodDescriptor().id;
+  }
+
+  const std::string input_method_id =
+      g_browser_process->local_state()->GetString(
+          prefs::kHardwareKeyboardLayout);
+  if (input_method_id.empty()) {
+    LOG(ERROR) << "Hardware keyboard is not found (not yet stored?)";
+    return GetFallbackInputMethodDescriptor().id;
+  }
+  return input_method_id;
 }
 
 InputMethodDescriptor GetFallbackInputMethodDescriptor() {
