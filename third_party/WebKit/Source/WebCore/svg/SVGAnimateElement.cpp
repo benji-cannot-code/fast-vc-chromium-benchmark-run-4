29362371583c5e543c12db1b6ecc7ccc25775079
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-10 22:45:04 +0000

    2011-02-10  Dirk Schulze  <krit@webkit.org>
            Reviewed by Andreas Kling.
    
            SVG animation avoid unnecessary adjust for currentColor
            https://bugs.webkit.org/show_bug.cgi?id=54243
    
            At the moment we try to adjust every animation value for currentColor - independent of the animation type.
            Since the value is a string and the target element may needs to get called by getElementById, this could
            be an expensive and unnecessary operation. Also after we adjust for currentColor, we save the result back
            as a string and parse it to Color afterwards again.
            With the patch we just adjust an animation value, if we use color animation. The color won't get saved and
            parsed as a string again.
    
            No change of functionality, no new tests.
    
            * svg/SVGAnimateElement.cpp:
            (WebCore::adjustForCurrentColor):
            (WebCore::SVGAnimateElement::calculateFromAndToValues):
            (WebCore::SVGAnimateElement::calculateFromAndByValues):
            * svg/SVGAnimationElement.cpp:
            (WebCore::SVGAnimationElement::currentValuesForValuesAnimation):
            (WebCore::SVGAnimationElement::startedActiveInterval):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimateElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimateElement.cpp
@@ -25,9 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(SVG) && ENABLE(SVG_ANIMATION)
 #include "SVGAnimateElement.h"
 
+#include "CSSPropertyNames.h"
 #include "ColorDistance.h"
 #include "FloatConversion.h"
 #include "QualifiedName.h"
+#include "RenderObject.h"
 #include "SVGColor.h"
 #include "SVGNames.h"
 #include "SVGParserUtilities.h"
@@ -82,6 +84,17 @@ static bool parseNumberValueAndUnit(const String& in, double& value, String& uni
     bool ok;
     value = number.toDouble(&ok);
     return ok;
+}
+
+static inline bool adjustForCurrentColor(Color& color, const String& value, SVGElement* target)
+{
+    if (!target || !target->isStyled() || value != "currentColor")
+        return false;
+    
+    if (RenderObject* targetRenderer = target->renderer())
+        color = targetRenderer->style()->visitedDependentColor(CSSPropertyColor);
+
+    return true;
 }
 
 SVGAnimateElement::PropertyType SVGAnimateElement::determinePropertyType(const String& attribute) const
@@ -217,8 +230,11 @@ bool SVGAnimateElement::calculateFromAndToValues(const String& fromString, const
     // FIXME: Needs more solid way determine target attribute type.
     m_propertyType = determinePropertyType(attributeName());
     if (m_propertyType == ColorProperty) {
-        m_fromColor = SVGColor::colorFromRGBColorString(fromString);
-        m_toColor = SVGColor::colorFromRGBColorString(toString);
+        SVGElement* targetElement = this->targetElement();
+        if (!adjustForCurrentColor(m_fromColor, fromString, targetElement))
+            m_fromColor = SVGColor::colorFromRGBColorString(fromString);
+        if (!adjustForCurrentColor(m_toColor, toString, targetElement))
+            m_toColor = SVGColor::colorFromRGBColorString(toString);
         if ((m_fromColor.isValid() && m_toColor.isValid()) || (m_toColor.isValid() && animationMode() == ToAnimation))
             return true;
     } else if (m_propertyType == NumberProperty) {
@@ -256,8 +272,12 @@ bool SVGAnimateElement::calculateFromAndByValues(const String& fromString, const
     ASSERT(!hasTagName(SVGNames::setTag));
     m_propertyType = determinePropertyType(attributeName());
     if (m_propertyType == ColorProperty) {
-        m_fromColor = fromString.isEmpty() ? Color() : SVGColor::colorFromRGBColorString(fromString);
-        m_toColor = ColorDistance::addColorsAndClamp(m_fromColor, SVGColor::colorFromRGBColorString(byString));
+        SVGElement* targetElement = this->targetElement();
+        if (!adjustForCurrentColor(m_fromColor, fromString, targetElement))
+            m_fromColor = fromString.isEmpty() ? Color() : SVGColor::colorFromRGBColorString(fromString);
+        if (!adjustForCurrentColor(m_toColor, byString, targetElement))
+            m_toColor = SVGColor::colorFromRGBColorString(byString);
+        m_toColor = ColorDistance::addColorsAndClamp(m_fromColor, m_toColor);
         if (!m_fromColor.isValid() || !m_toColor.isValid())
             return false;
     } else {
