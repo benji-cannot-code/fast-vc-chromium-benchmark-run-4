e96bb125ddb033c56e7070cb4f0de297215506d0
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 15:49:46 +0000

    Remove failed attempt at overriding a function
    BUG=72148
    TEST=none
    
    Review URL: http://codereview.chromium.org/6480024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_types.cc
+++ chrome/browser/history/history_types.cc
@@ -150,7 +150,7 @@ URLResult::URLResult(const GURL& url,
 URLResult::~URLResult() {
 }
 
-void URLResult::Swap(URLResult* other) {
+void URLResult::SwapResult(URLResult* other) {
   URLRow::Swap(other);
   std::swap(visit_time_, other->visit_time_);
   snippet_.Swap(&other->snippet_);
@@ -193,7 +193,7 @@ void QueryResults::Swap(QueryResults* other) {
 
 void QueryResults::AppendURLBySwapping(URLResult* result) {
   URLResult* new_result = new URLResult;
-  new_result->Swap(result);
+  new_result->SwapResult(result);
 
   results_.push_back(new_result);
   AddURLUsageAtIndex(new_result->url(), results_.size() - 1);
