a35600ebb435bae918a2baa21e4f6c30fb322023
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-10 23:26:37 +0000

    2011-02-10  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Andreas Kling.
    
            Fix compilation error on OpenBSD
            https://bugs.webkit.org/show_bug.cgi?id=53766
    
            Add a HAVE_STRNSTR in Platform.h and define it only on Darwin and FreeBSD.
    
            * wtf/Platform.h:
            * wtf/StringExtras.h: Use HAVE(STRNSTR) now.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -732,6 +732,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_SIGNAL_H 1
 #endif
 
+#if !defined(HAVE_STRNSTR)
+#if OS(DARWIN) || OS(FREEBSD)
+#define HAVE_STRNSTR 1
+#endif
+#endif
+
 #if !OS(WINDOWS) && !OS(SOLARIS) && !OS(QNX) \
     && !OS(SYMBIAN) && !OS(HAIKU) && !OS(RVCT) \
     && !OS(ANDROID) && !PLATFORM(BREWMP)
