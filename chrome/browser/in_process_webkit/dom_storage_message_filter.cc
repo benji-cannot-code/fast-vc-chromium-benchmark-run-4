dabbbd6afd98ff697a8bb54f1e45983afab2255d
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 17:38:46 +0000

    Declare DOMStorageMessageFilter::OnDestruct() const.
    BUG=72424
    TEST=no clange error
    TBR=thakis
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/in_process_webkit/dom_storage_message_filter.cc
+++ chrome/browser/in_process_webkit/dom_storage_message_filter.cc
@@ -103,7 +103,7 @@ bool DOMStorageMessageFilter::OnMessageReceived(const IPC::Message& message,
   return handled;
 }
 
-void DOMStorageMessageFilter::OnDestruct() {
+void DOMStorageMessageFilter::OnDestruct() const {
   BrowserThread::DeleteOnIOThread::Destruct(this);
 }
 
--- chrome/browser/in_process_webkit/dom_storage_message_filter.h
+++ chrome/browser/in_process_webkit/dom_storage_message_filter.h
@@ -36,7 +36,7 @@ class DOMStorageMessageFilter : public BrowserMessageFilter {
                                         BrowserThread::ID* thread);
   virtual bool OnMessageReceived(const IPC::Message& message,
                                  bool* message_was_ok);
-  virtual void OnDestruct();
+  virtual void OnDestruct() const;
 
   // Only call on the WebKit thread.
   static void DispatchStorageEvent(const NullableString16& key,
