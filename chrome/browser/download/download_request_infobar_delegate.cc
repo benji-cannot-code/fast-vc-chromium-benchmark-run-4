2c4c9eadf079c7f160f53f813a108a4f462c20ec
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 17:27:10 +0000

    Fix a leak.  Remove suppression for the leak and update bug number for unrelated suppression.
    BUG=72288,72547
    TEST=none
    Review URL: http://codereview.chromium.org/6478011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74447 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_request_infobar_delegate.cc
+++ chrome/browser/download/download_request_infobar_delegate.cc
@@ -24,8 +24,7 @@ DownloadRequestInfoBarDelegate::~DownloadRequestInfoBarDelegate() {
 void DownloadRequestInfoBarDelegate::InfoBarClosed() {
   if (host_)
     host_->Cancel();
-  // This will delete us.
-  ConfirmInfoBarDelegate::InfoBarClosed();
+  delete this;
 }
 
 SkBitmap* DownloadRequestInfoBarDelegate::GetIcon() const {
