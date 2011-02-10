01f1c9256f4d648385c8e72fe1f1395d5da35308
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 21:08:12 +0000

    Keep a track of which directories are being created when expanding the installation archive. This is mostly to keep the noise level in the log down (the "directory already exists" entries were over half of the verbose log).
    TEST=The only change should be that the verbose installer log should be smaller.
    BUG=none
    
    Review URL: http://codereview.chromium.org/6484009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74480 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/lzma_util.h
+++ chrome/installer/util/lzma_util.h
@@ -7,10 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_INSTALLER_UTIL_LZMA_UTIL_H_
 #pragma once
 
-#include <string>
 #include <windows.h>
 
+#include <set>
+#include <string>
+
 #include "base/basictypes.h"
+
+class FilePath;
 
 // This is a utility class that acts as a wrapper around LZMA SDK library
 class LzmaUtil {
@@ -37,8 +41,12 @@ class LzmaUtil {
 
   void CloseArchive();
 
+ protected:
+  bool CreateDirectory(const FilePath& dir);
+
  private:
   HANDLE archive_handle_;
+  std::set<std::wstring> directories_created_;
 
   DISALLOW_COPY_AND_ASSIGN(LzmaUtil);
 };
