708579e9a7c08ea5afffeeff322987c247f1e214
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-10 01:17:35 +0000

    File uploads do not work within the sandbox <rdar://problem/8950518> https://bugs.webkit.org/show_bug.cgi?id=54154
    Reviewed by Darin Adler.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didChooseFilesForOpenPanel):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::extendSandboxForFileFromOpenPanel):
    * WebProcess/WebPage/WebPage.h:
    * WebProcess/WebPage/WebPage.messages.in:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -433,6 +433,9 @@ private:
 
     void didChooseFilesForOpenPanel(const Vector<String>&);
     void didCancelForOpenPanel();
+#if ENABLE(WEB_PROCESS_SANDBOX)
+    void extendSandboxForFileFromOpenPanel(const SandboxExtension::Handle&);
+#endif
 
     void didReceiveGeolocationPermissionDecision(uint64_t geolocationID, bool allowed);
 
