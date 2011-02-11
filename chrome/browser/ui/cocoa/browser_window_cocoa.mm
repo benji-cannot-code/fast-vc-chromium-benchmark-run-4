35d56d0ccc97dc276b4297999a80700a7128319c
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 20:03:08 +0000

    Mac: Make Request Restart dialog window modal
    Currently users can interact with the browser window while the Request Restart dialog is open. This can lead to some weird cases where things don't work.
    
    There are two problems here. First, RequestRestart() calls -[NSAlert beginSheetModalForWindow:] even if the parent window is nil. This causes the dialog to be top most but not block input in the browser window. Fix is to use -[NSAlert runModal] instead.
    
    Second, BrowserWindowCocoa::ShowUpdateChromeDialog() calls RequestRestart() with a nil parent window. I'm changing this to pass a parent window instead.
    
    BUG=66532
    TEST=Forced the restart dialog to show. Verified that the dialog was window modal and didn't allow any interaction with tabs.
    Review URL: http://codereview.chromium.org/6503005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_cocoa.mm
+++ chrome/browser/ui/cocoa/browser_window_cocoa.mm
@@ -311,7 +311,7 @@ views::Window* BrowserWindowCocoa::ShowAboutChromeDialog() {
 }
 
 void BrowserWindowCocoa::ShowUpdateChromeDialog() {
-  restart_browser::RequestRestart(nil);
+  restart_browser::RequestRestart(window());
 }
 
 void BrowserWindowCocoa::ShowTaskManager() {
