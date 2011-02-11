c9a2f4c5bc0347b6847298ccc8cbb571aefafd17
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 05:59:47 +0000

    Refactor TOUCH_UI to match other refactor.
    A few files were missed in a previous refactor (http://codereview.chromium.org/6480001/).
    
    BUG=72040
    TEST=none
    
    Review URL: http://codereview.chromium.org/6488006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dom_ui/login/login_ui.cc
+++ chrome/browser/chromeos/dom_ui/login/login_ui.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dom_ui/login/authenticator_facade_cros.h"
 #include "chrome/browser/chromeos/dom_ui/login/login_ui.h"
 #include "chrome/browser/chromeos/dom_ui/login/login_ui_helpers.h"
+#include "chrome/browser/dom_ui/chrome_url_data_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/browser.h"
@@ -158,7 +159,7 @@ LoginUI::LoginUI(TabContents* contents)
   LoginUIHTMLSource* html_source =
       new LoginUIHTMLSource(MessageLoop::current());
 
-  contents->profile->GetChromeURLDataManager()->AddDataSource(html_source);
+  contents->profile()->GetChromeURLDataManager()->AddDataSource(html_source);
 }
 
 }  // namespace chromeos
