ace4c4df0581518d68516befd457bcf21f835238
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 06:13:17 +0000

    Use SSE2 block differ for chromoting
    We have the SSE2 lying around in the tree just never being used. This will
    allow us to use it.
    
    BUG=None
    TEST=Chromoting to a host machine and the diff will work correctly
    
    Review URL: http://codereview.chromium.org/6469022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74571 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/differ_block_internal.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This header file is used only differ_block.h. It defines the SSE2 rountines
+// for finding block difference.
+
+#ifndef REMOTING_HOST_DIFFER_BLOCK_INTERNAL_H_
+#define REMOTING_HOST_DIFFER_BLOCK_INTERNAL_H_
+
+#include "base/basictypes.h"
+
+namespace remoting {
+
+// Find block difference of dimension 16x16.
+extern int BlockDifference_SSE2_W16(const uint8* image1, const uint8* image2,
+                                    int stride);
+
+// Find block difference of dimension 32x32.
+extern int BlockDifference_SSE2_W32(const uint8* image1, const uint8* image2,
+                                    int stride);
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_DIFFER_BLOCK_INTERNAL_H_
