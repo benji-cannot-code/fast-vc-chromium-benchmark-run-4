e13225c9c8ae04594e17d39cd78e3581811d2207
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-11 02:57:44 +0000

    2011-02-10  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            Repro crash with Sony Google TV ad at Gizmodo
            https://bugs.webkit.org/show_bug.cgi?id=54150
            <rdar://problem/8782346>
    
            Re-landing this fix, bug without the test case, since it causes hangs on the bots.
            https://bugs.webkit.org/show_bug.cgi?id=54171 tracks adding back the test.
    
            Since PluginView::evaluate can cause the plug-in element to go away, we need to protect it.
    
            * WebProcess/Plugins/PluginView.cpp:
            (WebKit::PluginView::~PluginView):
            Null out m_pluginElement here so we'll catch crashes earlier.
    
            (WebKit::PluginView::evaluate):
            Add a plug-in protector.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78299 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
@@ -285,8 +285,11 @@ PluginView::~PluginView()
     // Invalidate the object map.
     m_npRuntimeObjectMap.invalidate();
 
-    // Cancel all streams.
     cancelAllStreams();
+
+    // Null out the plug-in element explicitly so we'll crash earlier if we try to use
+    // the plug-in view after it's been destroyed.
+    m_pluginElement = nullptr;
 }
 
 Frame* PluginView::frame()
@@ -903,6 +906,10 @@ bool PluginView::evaluate(NPObject* npObject, const String& scriptString, NPVari
 
     bool oldAllowPopups = frame()->script()->allowPopupsFromPlugin();
     frame()->script()->setAllowPopupsFromPlugin(allowPopups);
+
+    // Calling evaluate will run JavaScript that can potentially remove the plug-in element, so we need to
+    // protect the plug-in view from destruction.
+    NPRuntimeObjectMap::PluginProtector pluginProtector(&m_npRuntimeObjectMap);
 
     bool returnValue = m_npRuntimeObjectMap.evaluate(npObject, scriptString, result);
 
