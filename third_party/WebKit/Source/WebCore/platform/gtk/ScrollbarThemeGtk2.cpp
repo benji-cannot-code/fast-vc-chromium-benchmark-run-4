fd2449630d3478a4879476fa607d51d1d29d03e3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-11 08:26:03 +0000

    2011-02-11  Ryuan Choi  <ryuan.choi@samsung.com>
            Unreviewed build fix.
    
            [GTK] Build break with 2.18.3
            https://bugs.webkit.org/show_bug.cgi?id=54057
    
            * platform/gtk/ScrollbarThemeGtk2.cpp: Include GtkVersioning.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/ScrollbarThemeGtk2.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/ScrollbarThemeGtk2.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifdef GTK_API_VERSION_2
 
+#include "GtkVersioning.h"
 #include "PlatformMouseEvent.h"
 #include "RenderThemeGtk.h"
 #include "ScrollView.h"
