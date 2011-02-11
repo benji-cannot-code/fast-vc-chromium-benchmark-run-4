4d9b1f864b2ec6f6a7060a7609e8cec0d87f37c7
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-11 06:35:18 +0000

    2011-02-10  Andy Estes  <aestes@apple.com>
            Reviewed by Adam Barth.
    
            Remove an unneeded argument from HTMLDocument::create().
            https://bugs.webkit.org/show_bug.cgi?id=54268
    
            Now that HTMLTreeBuilder no longer uses a dummy HTMLDocument for
            fragment parsing, there isn't a need to explicitly specify a baseURL
            when constructing an HTMLDocument. We can remove the unneeded argument.
    
            Also fix some style issues in Document.h and HTMLDocument.h caught by
            check-webkit-style.
    
            No new tests. No change in behavior.
    
            * dom/Document.cpp:
            (WebCore::Document::Document):
            * dom/Document.h:
            * html/HTMLDocument.cpp:
            (WebCore::HTMLDocument::HTMLDocument):
            * html/HTMLDocument.h:
            (WebCore::HTMLDocument::create):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLDocument.h
+++ third_party/WebKit/Source/WebCore/html/HTMLDocument.h
@@ -36,9 +36,9 @@ class HTMLElement;
 
 class HTMLDocument : public Document, public CachedResourceClient {
 public:
-    static PassRefPtr<HTMLDocument> create(Frame* frame, const KURL& url, const KURL& baseURL = KURL())
+    static PassRefPtr<HTMLDocument> create(Frame* frame, const KURL& url)
     {
-        return adoptRef(new HTMLDocument(frame, url, baseURL));
+        return adoptRef(new HTMLDocument(frame, url));
     }
     virtual ~HTMLDocument();
 
@@ -81,7 +81,7 @@ public:
     bool hasExtraNamedItem(AtomicStringImpl* name);
 
 protected:
-    HTMLDocument(Frame* frame, const KURL& url, const KURL& baseURL = KURL());
+    HTMLDocument(Frame*, const KURL&);
 
 private:
     virtual PassRefPtr<Element> createElement(const AtomicString& tagName, ExceptionCode&);
