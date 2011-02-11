d02ebef936e3d96512209b8fa6f2536e7f09f9d5
neb@chromium.org <neb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 21:28:44 +0000

    Pepper GLES fixes to support 3D in NaCl.
    BUG=none
    TEST=NaCl:910
    
    Review URL: http://codereview.chromium.org/6505002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74674 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_opengles_dev.h
+++ ppapi/c/dev/ppb_opengles_dev.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PPAPI_C_DEV_PPB_OPENGLES_DEV_H_
 
 #include "ppapi/c/pp_resource.h"
+#include "ppapi/c/pp_stdint.h"
 
 #ifndef __gl2_h_
 typedef unsigned int GLenum;
@@ -24,9 +25,9 @@ typedef unsigned int GLbitfield;
 typedef short GLshort;
 typedef float GLfloat;
 typedef float GLclampf;
-typedef signed char GLbyte;
-typedef unsigned char GLubyte;
-typedef int GLfixed;
+typedef int8_t GLbyte;
+typedef uint8_t GLubyte;
+typedef int32_t GLfixed;
 typedef unsigned short GLushort;
 typedef int GLclampx;
 #endif  // __gl2_h_
