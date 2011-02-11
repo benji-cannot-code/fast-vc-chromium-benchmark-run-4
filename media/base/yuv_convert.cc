d3f5249a18bf95738180a1a9a5c46febcd80f662
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 18:00:57 +0000

    Revert 74571 - Use SSE2 block differ for chromoting
    We have the SSE2 lying around in the tree just never being used. This will
    allow us to use it.
    
    A number of Windows bots have gone red in media_tests on the waterfall:
    
    http://build.chromium.org/p/chromium/builders/XP%20Tests%20%281%29
    http://build.chromium.org/p/chromium/builders/XP%20Tests%20%281%29/builds/501/steps/media_unittests/logs/stdio
    
    This seems to be related to r74571.  So am reverting to see.
    
    BUG=None
    TEST=Chromoting to a host machine and the diff will work correctly
    
    Review URL: http://codereview.chromium.org/6469022
    
    TBR=hclam@chromium.org
    Review URL: http://codereview.chromium.org/6502002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74630 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/yuv_convert.cc
+++ media/base/yuv_convert.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/yuv_convert.h"
 
-#include "build/build_config.h"
 #include "media/base/cpu_features.h"
 #include "media/base/yuv_convert_internal.h"
 #include "media/base/yuv_row.h"
@@ -358,7 +357,7 @@ void ConvertRGB32ToYUV(const uint8* rgbframe,
   static void (*convert_proc)(const uint8*, uint8*, uint8*, uint8*,
                               int, int, int, int, int) = NULL;
   if (!convert_proc) {
-#if defined(ARCH_CPU_ARM_FAMILY)
+#ifdef __arm__
     // For ARM processors, always use C version.
     // TODO(hclam): Implement a NEON version.
     convert_proc = &ConvertRGB32ToYUV_C;
