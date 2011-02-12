f6fc8ddffc949fd954700ce75ff637d1acd74fa0
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-12 01:42:00 +0000

    2011-02-11  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Tony Chang.
    
            This patch adds to NRWT most of the support needed to run the new
            message-based workers in separate threads or processes. The code
            isn't fully complete yet because we don't support cancel() or
            is_alive().
    
            https://bugs.webkit.org/show_bug.cgi?id=54070
    
            * Scripts/webkitpy/layout_tests/layout_package/manager_worker_broker.py:
            * Scripts/webkitpy/layout_tests/layout_package/manager_worker_broker_unittest.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_runner2.py:
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/mock_drt.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/mock_drt.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/mock_drt.py
@@ -54,6 +54,10 @@ class MockDRTPort(object):
         if 'port_name' in kwargs:
             kwargs['port_name'] = kwargs['port_name'][len(prefix):]
         self.__delegate = factory.get(**kwargs)
+        self.__real_name = prefix + self.__delegate.name()
+
+    def real_name(self):
+        return self.__real_name
 
     def __getattr__(self, name):
         return getattr(self.__delegate, name)
@@ -270,7 +274,6 @@ class MockChromiumDRT(MockDRT):
             self._stdout.write('\n')
         self._stdout.write('#EOF\n')
         self._stdout.flush()
-
 
 
 if __name__ == '__main__':
