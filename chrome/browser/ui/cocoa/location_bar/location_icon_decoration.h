f9a7c91aae8884bb80139c3c8855dbc058bb285a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-12 19:37:17 +0000

    cocoa: Show a tooltip for the location icon in the omnibox.
    BUG=41073, 22925
    TEST=launch chrome, hover the location icon in the left, a tooltip with "Click to view site information" should appears.
    
    Review URL: http://codereview.chromium.org/6489033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/location_bar/location_icon_decoration.h
+++ chrome/browser/ui/cocoa/location_bar/location_icon_decoration.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,10 +36,14 @@ class LocationIconDecoration : public ImageDecoration {
   // Show the page info panel on click.
   virtual bool OnMousePressed(NSRect frame);
   virtual bool AcceptsMousePress() { return true; }
+  virtual NSString* GetToolTip();
 
  private:
   // The location bar view that owns us.
   LocationBarViewMac* owner_;
+
+  // The string to show for a tooltip.
+  scoped_nsobject<NSString> tooltip_;
 
   DISALLOW_COPY_AND_ASSIGN(LocationIconDecoration);
 };
