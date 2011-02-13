5b3d4e8248db23576933c4cf6b156f766201c700
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-13 21:39:03 +0000

    2011-02-13  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            The plug-in process heap should be executable by default
            https://bugs.webkit.org/show_bug.cgi?id=54364
            <rdar://problem/8990563>
    
            * UIProcess/Launcher/ProcessLauncher.h:
            Add executableHeap flag.
            
            * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
            (WebKit::ProcessLauncher::launchProcess):
            Set the right posix_spawn flag if executableHeap is true.
    
            * UIProcess/Plugins/PluginProcessProxy.cpp:
            (WebKit::PluginProcessProxy::PluginProcessProxy):
            Initialize executableHeap to true.
    
            * UIProcess/WebProcessProxy.cpp:
            (WebKit::WebProcessProxy::connect):
            Initialize executableHeap to false.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginProcessProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginProcessProxy.cpp
@@ -53,6 +53,9 @@ PluginProcessProxy::PluginProcessProxy(PluginProcessManager* PluginProcessManage
     launchOptions.processType = ProcessLauncher::PluginProcess;
 #if PLATFORM(MAC)
     launchOptions.architecture = pluginInfo.pluginArchitecture;
+
+    // FIXME: This shouldn't be true for all plug-ins.
+    launchOptions.executableHeap = true;
 #endif
 
     m_processLauncher = ProcessLauncher::create(this, launchOptions);
