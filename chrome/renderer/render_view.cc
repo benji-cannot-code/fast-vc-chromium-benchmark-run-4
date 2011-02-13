6ba16ad7fb4bdc531453262b363a331e4d1a6c1c
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-13 23:56:43 +0000

    Add histograms for prerendering to the renderer process, second try.
    BUG=71160
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=74034
    
    Reverted: http://src.chromium.org/viewvc/chrome?view=rev&revision=74040
    
    Review URL: http://codereview.chromium.org/6448002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -1425,6 +1425,7 @@ void RenderView::OnNavigate(const ViewMsg_Navigate_Params& params) {
         navigation_state->set_load_type(NavigationState::NORMAL_LOAD);
       } else {
         navigation_state->set_load_type(NavigationState::PRERENDER_LOAD);
+        navigation_state->set_was_started_as_prerender(true);
         navigation_state->set_is_prerendering(true);
       }
     }
@@ -3222,8 +3223,11 @@ void RenderView::didCreateDataSource(WebFrame* frame, WebDataSource* ds) {
     if (ds_old) {
       NavigationState* navigation_state =
           NavigationState::FromDataSource(ds_old);
-      if (navigation_state)
+      if (navigation_state) {
         state->set_is_prerendering(navigation_state->is_prerendering());
+        state->set_was_started_as_prerender(
+            navigation_state->was_started_as_prerender());
+      }
     }
   }
 
@@ -4717,6 +4721,7 @@ void RenderView::OnDisplayPrerenderedPage() {
 
   DCHECK(navigation_state->is_prerendering());
   navigation_state->set_is_prerendering(false);
+  navigation_state->set_prerendered_page_display_time(Time::Now());
 }
 
 void RenderView::OnFileChooserResponse(const std::vector<FilePath>& paths) {
