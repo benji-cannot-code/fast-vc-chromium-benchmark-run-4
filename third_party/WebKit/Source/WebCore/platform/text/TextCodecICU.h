287ffac5fd5224c9bbd47da4cb076d412a388b6f
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-14 12:31:42 +0000

    2011-02-14  Alejandro G. Castro  <alex@igalia.com>
            Unreviewed, rolling out r78451.
            http://trac.webkit.org/changeset/78451
            https://bugs.webkit.org/show_bug.cgi?id=53898
    
            It is causing crashes in some bots
    
            * platform/text/TextCodecICU.cpp:
            (WebCore::newTextCodecICU):
            (WebCore::TextCodecICU::registerBaseEncodingNames):
            (WebCore::TextCodecICU::registerBaseCodecs):
            (WebCore::TextCodecICU::registerExtendedEncodingNames):
            (WebCore::TextCodecICU::registerExtendedCodecs):
            (WebCore::ErrorCallbackSetter::~ErrorCallbackSetter):
            (WebCore::getGbkEscape):
            (WebCore::urlEscapedEntityCallback):
            (WebCore::gbkCallbackEscape):
            (WebCore::gbkUrlEscapedEntityCallack):
            (WebCore::gbkCallbackSubstitute):
            * platform/text/TextCodecICU.h:
            (WebCore::ICUConverterWrapper::ICUConverterWrapper):
            * platform/text/TextCodecUTF8.cpp:
            (WebCore::TextCodecUTF8::registerEncodingNames):
            (WebCore::nonASCIISequenceLength):
            (WebCore::decodeNonASCIISequence):
            (WebCore::TextCodecUTF8::decode):
            * platform/text/TextCodecUTF8.h:
            * platform/text/TextEncodingRegistry.cpp:
            (WebCore::buildBaseTextCodecMaps):
            (WebCore::extendTextCodecMaps):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/TextCodecICU.h
+++ third_party/WebKit/Source/WebCore/platform/text/TextCodecICU.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004, 2006, 2007, 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2006, 2007 Apple Inc. All rights reserved.
  * Copyright (C) 2006 Alexey Proskuryakov <ap@nypop.com>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TextCodec.h"
 #include "TextEncoding.h"
+
 #include <unicode/utypes.h>
 
 typedef struct UConverter UConverter;
@@ -38,18 +39,19 @@ namespace WebCore {
 
     class TextCodecICU : public TextCodec {
     public:
-        static void registerEncodingNames(EncodingNameRegistrar);
-        static void registerCodecs(TextCodecRegistrar);
+        static void registerBaseEncodingNames(EncodingNameRegistrar);
+        static void registerBaseCodecs(TextCodecRegistrar);
 
-        virtual ~TextCodecICU();
+        static void registerExtendedEncodingNames(EncodingNameRegistrar);
+        static void registerExtendedCodecs(TextCodecRegistrar);
 
-    private:
         TextCodecICU(const TextEncoding&);
-        static PassOwnPtr<TextCodec> create(const TextEncoding&, const void*);
+        virtual ~TextCodecICU();
 
         virtual String decode(const char*, size_t length, bool flush, bool stopOnError, bool& sawError);
         virtual CString encode(const UChar*, size_t length, UnencodableHandling);
 
+    private:
         void createICUConverter() const;
         void releaseICUConverter() const;
         bool needsGBKFallbacks() const { return m_needsGBKFallbacks; }
@@ -66,12 +68,13 @@ namespace WebCore {
     };
 
     struct ICUConverterWrapper {
-        ICUConverterWrapper() : converter(0) { }
+        ICUConverterWrapper()
+            : converter(0)
+        {
+        }
         ~ICUConverterWrapper();
 
         UConverter* converter;
-
-        WTF_MAKE_NONCOPYABLE(ICUConverterWrapper);
     };
 
 } // namespace WebCore
