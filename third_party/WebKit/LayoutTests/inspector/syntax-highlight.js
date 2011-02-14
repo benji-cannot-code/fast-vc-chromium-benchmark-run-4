2ab7e758d77b62fd6622537b2d3e5c625184a299
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-14 15:43:42 +0000

    2011-02-14  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: move timeline and highlighter tests to the new harness.
            https://bugs.webkit.org/show_bug.cgi?id=54390
    
            * inspector/cookie-resource-match.html:
            * inspector/elements-panel-search.html:
            * inspector/evaluate-in-frontend-expected.txt: Removed.
            * inspector/evaluate-in-frontend.html: Removed.
            * inspector/inspected-objects-not-overriden.html:
            * inspector/resources/timeline-iframe-data.html:
            * inspector/syntax-highlight-css.html:
            * inspector/syntax-highlight-html.html:
            * inspector/syntax-highlight-javascript-expected.txt:
            * inspector/syntax-highlight-javascript.html:
            * inspector/syntax-highlight.js:
            (initialize_SyntaxHighlight.InspectorTest.dumpSyntaxHighlight):
            (initialize_SyntaxHighlight):
            * inspector/timeline-enum-stability-expected.txt:
            * inspector/timeline-enum-stability.html:
            * inspector/timeline-event-dispatch.html:
            * inspector/timeline-layout-expected.txt:
            * inspector/timeline-layout.html:
            * inspector/timeline-mark-timeline.html:
            * inspector/timeline-network-resource-expected.txt:
            * inspector/timeline-network-resource.html:
            * inspector/timeline-network-resource.js:
            * inspector/timeline-paint.html:
            * inspector/timeline-parse-html-expected.txt:
            * inspector/timeline-parse-html.html:
            * inspector/timeline-recalculate-styles-expected.txt:
            * inspector/timeline-recalculate-styles.html:
            * inspector/timeline-script-tag-1-expected.txt:
            * inspector/timeline-script-tag-1.html:
            * inspector/timeline-script-tag-2-expected.txt:
            * inspector/timeline-script-tag-2.html:
            * inspector/timeline-script-tag-2.js:
            * inspector/timeline-test.js:
            (initialize_Timeline.InspectorTest.performActionsAndPrint.step1):
            (initialize_Timeline.InspectorTest.performActionsAndPrint.step2):
            (initialize_Timeline.InspectorTest.performActionsAndPrint.step3):
            (initialize_Timeline.InspectorTest.performActionsAndPrint):
            (initialize_Timeline.InspectorTest.printTimelineRecords):
            (initialize_Timeline.InspectorTest._timelineResults):
            (initialize_Timeline.InspectorTest.dumpTimelineRecord):
            (initialize_Timeline.InspectorTest.dumpTimelineRecords):
            (initialize_Timeline.InspectorTest.printTimelineRecordProperties):
            (initialize_Timeline.InspectorTest._timelineAgentTypeToString):
            (initialize_Timeline):
            * inspector/timeline-trivial.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/syntax-highlight.js
+++ third_party/WebKit/LayoutTests/inspector/syntax-highlight.js
@@ -1,20 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Inspected Page functions.
+var initialize_SyntaxHighlight = function() {
 
-function doit()
-{
-    function callback(result)
-    {
-        for (var i = 0; i < result.length; ++i)
-            output(result[i]);
-        notifyDone();
-    }
-    evaluateInWebInspector("frontend_doitAndDump", callback);
-}
-
-// Frontend functions.
-
-function frontend_dumpSyntaxHighlight(str, mimeType)
+InspectorTest.dumpSyntaxHighlight = function(str, mimeType)
 {
     var node = document.createElement("span");
     node.textContent = str;
@@ -28,5 +15,7 @@ function frontend_dumpSyntaxHighlight(str, mimeType)
             node_parts.push("*");
         }
     }
-    return str + ": " + node_parts.join(",");
-}
+    InspectorTest.addResult(str + ": " + node_parts.join(","));
+};
+
+};
