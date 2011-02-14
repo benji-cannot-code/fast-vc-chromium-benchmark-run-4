a3de9f0decde8b9c40a32c3cefa99c820c725d6e
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-14 14:06:10 +0000

    2011-02-10  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: refactoring, extract SourceMapping from SourceFrameContent.
            https://bugs.webkit.org/show_bug.cgi?id=54195
    
            Add SourceMapping interface with two implementations - IdenticalSourceMapping and SourceMappingForFormattedScript.
            Rewrite SourceFrameContent based on SourceMapping. Make SourceFrame unaware of source mapping details.
    
            * inspector/front-end/ResourceView.js:
            (WebInspector.SourceFrameContentProviderForResource.prototype.requestContent):
            * inspector/front-end/ScriptFormatter.js:
            (WebInspector.ScriptFormatter.locationToPosition):
            (WebInspector.ScriptFormatter.positionToLocation):
            (WebInspector.ScriptFormatter.prototype.formatContent.didFormatChunks):
            (WebInspector.ScriptFormatter.prototype.formatContent):
            (WebInspector.ScriptFormatter.prototype._splitContentIntoChunks):
            (WebInspector.SourceMappingForFormattedScript):
            (WebInspector.SourceMappingForFormattedScript.prototype.fromScriptLocation):
            (WebInspector.SourceMappingForFormattedScript.prototype.toScriptLocation):
            (WebInspector.SourceMappingForFormattedScript.prototype.originalPositionToFormattedLocation):
            (WebInspector.SourceMappingForFormattedScript.prototype._convertPosition):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.SourceFrameContentProviderForScript.prototype.requestContent):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype._createTextViewer):
            (WebInspector.SourceFrame.prototype._shouldDisplayBreakpoint):
            (WebInspector.SourceFrame.prototype.setExecutionLine):
            (WebInspector.SourceFrame.prototype.clearExecutionLine):
            (WebInspector.SourceFrame.prototype._breakpointAdded):
            (WebInspector.SourceFrame.prototype.formatSource.didFormat):
            (WebInspector.SourceFrame.prototype.formatSource):
            (WebInspector.SourceFrame.prototype._continueToLine):
            (WebInspector.SourceFrame.prototype._setBreakpoint):
            (WebInspector.SourceFrame.prototype._findBreakpoint):
            (WebInspector.SourceFrameContentProvider.prototype.requestContent):
            * inspector/front-end/SourceFrameContent.js:
            (WebInspector.SourceFrameContent):
            (WebInspector.SourceFrameContent.prototype.sourceFrameLineNumberToScriptLocation):
            (WebInspector.SourceFrameContent.prototype.scriptLocationToSourceFrameLineNumber):
            (WebInspector.SourceFrameContent.prototype._sourceIDForSourceFrameLineNumber):
            (WebInspector.SourceMapping):
            (WebInspector.SourceMapping.prototype.fromScriptLocation):
            (WebInspector.SourceMapping.prototype.toScriptLocation):
            (WebInspector.IdenticalSourceMapping):
            (WebInspector.IdenticalSourceMapping.prototype.fromScriptLocation):
            (WebInspector.IdenticalSourceMapping.prototype.toScriptLocation):
            * inspector/front-end/utilities.js:
            (String.prototype.lineEndings):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -172,10 +172,10 @@ WebInspector.SourceFrame.prototype = {
             delete this._lineToHighlight;
     },
 
-    _createTextViewer: function(mimeType, text)
+    _createTextViewer: function(mimeType, content)
     {
-        this._content = new WebInspector.SourceFrameContent(text, this._contentProvider.scripts());
-        this._textModel.setText(null, text);
+        this._content = content;
+        this._textModel.setText(null, content.text);
 
         this._textViewer = new WebInspector.TextViewer(this._textModel, WebInspector.platform, this._url);
         var element = this._textViewer.element;
@@ -241,7 +241,7 @@ WebInspector.SourceFrame.prototype = {
     {
         if (this._url)
             return this._url === breakpoint.url;
-        var scripts = this._contentProvider.scripts();
+        var scripts = this._content.scriptRanges;
         for (var i = 0; i < scripts.length; ++i) {
             if (breakpoint.sourceID === scripts[i].sourceID)
                 return true;
@@ -367,7 +367,7 @@ WebInspector.SourceFrame.prototype = {
         this._executionLine = lineNumber;
         if (!this._textViewer)
             return;
-        var textViewerLineNumber = this._originalLocationToTextViewerLineNumber(this._executionLine - 1, 0);
+        var textViewerLineNumber = this._content.actualLocationToSourceFrameLineNumber(this._executionLine - 1, 0);
         this._textViewer.addDecoration(textViewerLineNumber, "webkit-execution-line");
     },
 
@@ -375,7 +375,7 @@ WebInspector.SourceFrame.prototype = {
     {
         if (!this._textViewer)
             return;
-        var textViewerLineNumber = this._originalLocationToTextViewerLineNumber(this._executionLine - 1, 0);
+        var textViewerLineNumber = this._content.actualLocationToSourceFrameLineNumber(this._executionLine - 1, 0);
         this._textViewer.removeDecoration(textViewerLineNumber, "webkit-execution-line");
         delete this._executionLine;
     },
@@ -478,7 +478,7 @@ WebInspector.SourceFrame.prototype = {
         var resolved = breakpoint.locations.length;
         var location = resolved ? breakpoint.locations[0] : breakpoint;
 
-        var textViewerLineNumber = this._originalLocationToTextViewerLineNumber(location.lineNumber, location.columnNumber);
+        var textViewerLineNumber = this._content.actualLocationToSourceFrameLineNumber(location.lineNumber, location.columnNumber);
         if (textViewerLineNumber >= this._textModel.linesCount)
             return;
 
@@ -852,7 +852,7 @@ WebInspector.SourceFrame.prototype = {
 
         function didFormat(formattedContent)
         {
-            this._formattedContent = formattedContent;
+            this._content = formattedContent;
             this._textModel.setText(null, formattedContent.text);
             this._setTextViewerDecorations();
         }
@@ -862,7 +862,7 @@ WebInspector.SourceFrame.prototype = {
 
     _continueToLine: function(lineNumber)
     {
-        var location = this._textViewerLineNumberToScriptLocation(lineNumber);
+        var location = this._content.sourceFrameLineNumberToActualLocation(lineNumber);
         if (location.sourceID)
             WebInspector.debuggerModel.continueToLine(location.sourceID, location.lineNumber);
     },
@@ -877,7 +877,7 @@ WebInspector.SourceFrame.prototype = {
             return;  // Do not trigger editing from line numbers.
 
         var lineNumber = lineRow.lineNumber;
-        var location = this._textViewerLineNumberToScriptLocation(lineNumber);
+        var location = this._content.sourceFrameLineNumberToActualLocation(lineNumber);
         if (!location.sourceID)
             return;
 
@@ -898,7 +898,7 @@ WebInspector.SourceFrame.prototype = {
 
     _setBreakpoint: function(lineNumber, condition, enabled)
     {
-        var location = this._textViewerLineNumberToScriptLocation(lineNumber);
+        var location = this._content.sourceFrameLineNumberToActualLocation(lineNumber);
         if (this._url)
             WebInspector.debuggerModel.setBreakpoint(this._url, location.lineNumber, location.columnNumber, condition, enabled);
         else if (location.sourceID)
@@ -914,20 +914,6 @@ WebInspector.SourceFrame.prototype = {
     {
         var breakpointId = this._textViewerLineNumberToBreakpointId[textViewerLineNumber];
         return WebInspector.debuggerModel.breakpointForId(breakpointId);
-    },
-
-    _originalLocationToTextViewerLineNumber: function(lineNumber, columnNumber)
-    {
-        if (!this._formattedContent)
-            return lineNumber;
-        return this._formattedContent.originalLocationToFormattedLocation(lineNumber, columnNumber).lineNumber;
-    },
-
-    _textViewerLineNumberToScriptLocation: function(lineNumber)
-    {
-        if (!this._formattedContent)
-            return this._content.scriptLocationForLineNumber(lineNumber);
-        return this._formattedContent.scriptLocationForFormattedLineNumber(lineNumber);
     }
 }
 
@@ -940,11 +926,6 @@ WebInspector.SourceFrameContentProvider = function()
 
 WebInspector.SourceFrameContentProvider.prototype = {
     requestContent: function(callback)
-    {
-        // Should be implemented by subclasses.
-    },
-
-    scripts: function()
     {
         // Should be implemented by subclasses.
     }
