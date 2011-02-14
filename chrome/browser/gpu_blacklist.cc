1bba4741056a68605c280c567adf7b3e8105911e
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-14 10:36:59 +0000

    Initialize GpuBlacklist member.
    BUG=None
    TEST=None
    CID=14627
    
    Review URL: http://codereview.chromium.org/6478025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_blacklist.cc
+++ chrome/browser/gpu_blacklist.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -417,7 +417,8 @@ GpuFeatureFlags GpuBlacklist::GpuBlacklistEntry::GetGpuFeatureFlags() const {
   return *feature_flags_;
 }
 
-GpuBlacklist::GpuBlacklist() {
+GpuBlacklist::GpuBlacklist()
+    : max_entry_id_(0) {
 }
 
 GpuBlacklist::~GpuBlacklist() {
