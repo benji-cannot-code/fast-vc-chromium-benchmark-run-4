224fb1b685128768d75cdc0a06eb610e5d5ec885
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-14 14:09:36 +0000

    [cros] Temporary whitelist disk I/O on UIThread while store/retrieve signed setting.
    BUG=chromium:70097
    TEST=unit tests
    
    Review URL: http://codereview.chromium.org/6520005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/signed_settings.cc
+++ chrome/browser/chromeos/login/signed_settings.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/ref_counted.h"
 #include "base/stringprintf.h"
+#include "base/threading/thread_restrictions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_thread.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
@@ -242,7 +243,14 @@ StorePropertyOp::StorePropertyOp(const std::string& name,
 StorePropertyOp::~StorePropertyOp() {}
 
 void StorePropertyOp::Execute() {
-  if (!service_->IsAlreadyOwned()) {
+  bool is_owned = false;
+  {
+    // This should not do blocking IO from the UI thread.
+    // Temporarily allow it for now. http://crbug.com/70097
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    is_owned = service_->IsAlreadyOwned();
+  }
+  if (!is_owned) {
     if (g_browser_process &&
         g_browser_process->local_state() &&
         SignedSettingsTempStorage::Store(name_, value_,
@@ -306,7 +314,14 @@ void RetrievePropertyOp::Execute() {
   // device has been owned and before temp_storage settings are finally
   // persisted into signed settings.
   // In this lapse of time Retrieve loses access to those settings.
-  if (!service_->IsAlreadyOwned()) {
+  bool is_owned = false;
+  {
+    // This should not do blocking IO from the UI thread.
+    // Temporarily allow it for now. http://crbug.com/70097
+    base::ThreadRestrictions::ScopedAllowIO allow_io;
+    is_owned = service_->IsAlreadyOwned();
+  }
+  if (!is_owned) {
     if (g_browser_process &&
         g_browser_process->local_state() &&
         SignedSettingsTempStorage::Retrieve(
