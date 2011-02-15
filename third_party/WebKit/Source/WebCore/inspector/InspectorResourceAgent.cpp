a324fec4a3f89a974b4fe2aac967a8271bcca408
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 13:09:13 +0000

    2011-02-15  Gavin Peters  <gavinp@chromium.org>
            Reviewed by Antti Koivisto.
    
            SharedBuffer::buffer() does not do what you think.
            https://bugs.webkit.org/show_bug.cgi?id=54420
    
            No new tests.
    
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::resourceContentBase64):
            * platform/SharedBuffer.h:
            * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
            (WebCore::JPEGImageReader::decode):
            (WebCore::JPEGImageDecoder::decode):
            * platform/image-decoders/webp/WEBPImageDecoder.cpp:
            (WebCore::WEBPImageDecoder::decode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorResourceAgent.cpp
@@ -108,7 +108,7 @@ bool InspectorResourceAgent::resourceContentBase64(Frame* frame, const KURL& url
         return false;
     }
 
-    *result = base64Encode(data->buffer());
+    *result = base64Encode(data->data(), data->size());
     return true;
 }
 
