21b169246ace63976790281a04473f6e247c041c
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 23:20:09 +0000

    2011-02-15  Joseph Pecoraro  <joepeck@webkit.org>
            Reviewed by David Kilzer.
    
            Cleanup Some Potential ENABLE(INSPECTOR) Related Building Issues
            https://bugs.webkit.org/show_bug.cgi?id=54048
    
            * WebCore.exp.in: moved inspector only export into ENABLE(INSPECTOR) block.
            * bindings/js/ScriptHeapSnapshot.h: include no longer needed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/ScriptHeapSnapshot.h
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptHeapSnapshot.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ScriptHeapSnapshot_h
 #define ScriptHeapSnapshot_h
 
-#include "InspectorValues.h"
 #include "PlatformString.h"
 
 namespace WebCore {
