d715bfa73d423a4af014a693a2482c247d614886
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-15 20:08:44 +0000

    Make code around CopyMetafileDataToSharedMem consistent.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6525008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74997 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper_mac.mm
+++ chrome/renderer/print_web_view_helper_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -48,7 +48,7 @@ void PrintWebViewHelper::PrintPage(const ViewMsg_PrintPage_Params& params,
 
   // Get the size of the compiled metafile.
   ViewHostMsg_DidPrintPage_Params page_params;
-  page_params.data_size = 0;
+  page_params.data_size = metafile.GetDataSize();
   page_params.page_number = page_number;
   page_params.document_cookie = params.params.document_cookie;
   page_params.actual_shrink = scale_factor;
@@ -58,12 +58,10 @@ void PrintWebViewHelper::PrintPage(const ViewMsg_PrintPage_Params& params,
 
   // Ask the browser to create the shared memory for us.
   if (!CopyMetafileDataToSharedMem(&metafile,
-          &(page_params.metafile_data_handle))) {
-    NOTREACHED();
-    return;
+                                   &(page_params.metafile_data_handle))) {
+    page_params.data_size = 0;
   }
 
-  page_params.data_size = metafile.GetDataSize();
   Send(new ViewHostMsg_DidPrintPage(routing_id(), page_params));
 }
 
@@ -112,7 +110,6 @@ void PrintWebViewHelper::CreatePreviewDocument(
   if (!CopyMetafileDataToSharedMem(&metafile,
           &(preview_params.metafile_data_handle))) {
     preview_params.data_size = 0;
-    NOTREACHED();
   }
   Send(new ViewHostMsg_PagesReadyForPreview(routing_id(), preview_params));
 }
