74d90af95195290c9193de77cc00177636314057
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 17:36:38 +0000

    2011-02-15  Leo Yang  <leo.yang@torchmobile.com.cn>
            Reviewed by Kent Tamura.
    
            Code style issue in WebCore/dom/InputElement.cpp
            https://bugs.webkit.org/show_bug.cgi?id=54428
    
            Remove 2 extra spaces in InputElement.cpp line 221.
    
            Code style fix, No new tests.
    
            * dom/InputElement.cpp:
            (WebCore::InputElement::handleBeforeTextInsertedEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78575 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/InputElement.cpp
+++ third_party/WebKit/Source/WebCore/dom/InputElement.cpp
@@ -219,7 +219,7 @@ void InputElement::handleBeforeTextInsertedEvent(InputElementData& data, InputEl
     if (!isConformToInputMask(inputElement->data(), candidateString)) {
         textEvent->setText("");
         return;
-      }
+    }
 #endif
     textEvent->setText(sanitizeUserInputValue(inputElement, textEvent->text(), appendableLength));
 }
