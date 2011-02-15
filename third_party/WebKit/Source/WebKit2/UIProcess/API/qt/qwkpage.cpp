18bb45c256b67539e5a4f2af1935d84e432d28f1
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 19:17:14 +0000

    2011-02-15  Andreas Kling  <kling@webkit.org>
            Unreviewed Qt build fix after r78588.
    
            * UIProcess/API/qt/qwkpage.cpp:
            (QWKPage::QWKPage): Add WKPageLoaderClient::shouldGoToHistoryItem.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78593 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwkpage.cpp
@@ -449,7 +449,8 @@ QWKPage::QWKPage(QWKContext* context)
         qt_wk_didBecomeUnresponsive,
         qt_wk_didBecomeResponsive,
         0,  /* processDidCrash */
-        0   /* didChangeBackForwardList */
+        0,  /* didChangeBackForwardList */
+        0   /* shouldGoToBackForwardListItem */
     };
     WKPageSetPageLoaderClient(pageRef(), &loadClient);
 
