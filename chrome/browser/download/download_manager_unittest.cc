fc87cdc79a618f79e843e12c8e1399b0ac2f5731
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-15 15:51:44 +0000

    Refactoring DownloadFile.
    Removed dead code (DeleteCrDownload method), and unnecessary parameter.
    
    BUG=None
    TEST=DownloadFile unit tests still pass.
    
    Review URL: http://codereview.chromium.org/6485008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager_unittest.cc
+++ chrome/browser/download/download_manager_unittest.cc
@@ -145,7 +145,6 @@ const struct {
   FilePath::StringType suggested_path;
   bool is_dangerous;
   bool finish_before_rename;
-  bool will_delete_crdownload;
   int expected_rename_count;
 } kDownloadRenameCases[] = {
   // Safe download, download finishes BEFORE file name determined.
@@ -154,19 +153,16 @@ const struct {
   { FILE_PATH_LITERAL("foo.zip"),
     false,
     true,
-    false,
     2, },
   // Dangerous download, download finishes BEFORE file name determined.
   // Needs to be renamed only once.
   { FILE_PATH_LITERAL("Unconfirmed xxx.crdownload"),
     true,
     true,
-    false,
     1, },
   // Safe download, download finishes AFTER file name determined.
   // Needs to be renamed twice.
   { FILE_PATH_LITERAL("foo.zip"),
-    false,
     false,
     false,
     2, },
@@ -174,7 +170,6 @@ const struct {
   // Needs to be renamed only once.
   { FILE_PATH_LITERAL("Unconfirmed xxx.crdownload"),
     true,
-    false,
     false,
     1, },
 };
@@ -185,7 +180,6 @@ class MockDownloadFile : public DownloadFile {
       : DownloadFile(info, NULL), renamed_count_(0) { }
   virtual ~MockDownloadFile() { Destructed(); }
   MOCK_METHOD2(Rename, bool(const FilePath&, bool));
-  MOCK_METHOD0(DeleteCrDownload, bool());
   MOCK_METHOD0(Destructed, void());
 
   bool TestMultipleRename(
@@ -240,9 +234,6 @@ TEST_F(DownloadManagerTest, DownloadRenameTest) {
               download, &MockDownloadFile::TestMultipleRename,
               2, true, new_path))));
     }
-
-    if (kDownloadRenameCases[i].will_delete_crdownload)
-      EXPECT_CALL(*download, DeleteCrDownload()).Times(1);
 
     download_manager_->CreateDownloadItem(info);
 
