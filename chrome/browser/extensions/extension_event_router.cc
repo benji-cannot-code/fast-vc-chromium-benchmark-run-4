f8f22158b17980f65de00badccdea5613748b618
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-15 00:40:03 +0000

    Revert 74887 - Add plumbing to webRequest API to support event filtering.
    This introduces a new kind of event (WebRequestEvent) to handle the extra parameters to addListener. Behind the scenes, adding a listener creates a unique sub-event associated with that set of filter+extraInfo parameters. When we dispatch the event, we dispatch only those sub-events that match the required filters.
    
    BUG=60101
    TEST=no
    
    Review URL: http://codereview.chromium.org/6250152
    
    TBR=mpcomplete@chromium.org
    Review URL: http://codereview.chromium.org/6526007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74889 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_event_router.cc
+++ chrome/browser/extensions/extension_event_router.cc
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_devtools_manager.h"
 #include "chrome/browser/extensions/extension_processes_api.h"
 #include "chrome/browser/extensions/extension_processes_api_constants.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
-#include "chrome/browser/extensions/extension_webrequest_api.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_host/render_process_host.h"
 #include "chrome/common/extensions/extension.h"
@@ -110,8 +109,6 @@ void ExtensionEventRouter::RemoveEventListener(
       " PID=" << process->id() << " extension=" << extension_id <<
       " event=" << event_name;
   listeners_[event_name].erase(listener);
-  // Note: extension_id may point to data in the now-deleted listeners_ object.
-  // Do not use.
 
   if (extension_devtools_manager_.get())
     extension_devtools_manager_->RemoveEventListener(event_name, process->id());
@@ -120,9 +117,6 @@ void ExtensionEventRouter::RemoveEventListener(
   // exits), then we let the TaskManager know that it has one fewer listener.
   if (event_name.compare(extension_processes_api_constants::kOnUpdated) == 0)
     ExtensionProcessesEventRouter::GetInstance()->ListenerRemoved();
-
-  ExtensionWebRequestEventRouter::RemoveEventListenerOnUIThread(
-      listener.extension_id, event_name);
 }
 
 bool ExtensionEventRouter::HasEventListener(const std::string& event_name) {
