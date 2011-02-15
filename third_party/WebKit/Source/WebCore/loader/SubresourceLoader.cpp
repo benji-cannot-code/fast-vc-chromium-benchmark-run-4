6582c8ba4b957e23693d3d42a5e2b36e7dbaef15
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 15:28:58 +0000

    2011-02-15  Bill Budge  <bbudge@chromium.org>
            Reviewed by David Levin.
    
            SubresourceLoader should expose finish time
            https://bugs.webkit.org/show_bug.cgi?id=54287
    
            No tests needed, exposes no new functionality
    
            * loader/DocumentThreadableLoader.cpp:
            (WebCore::DocumentThreadableLoader::didFinishLoading):
            * loader/DocumentThreadableLoader.h:
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::didFinishLoading):
            * loader/SubresourceLoaderClient.h:
            (WebCore::SubresourceLoaderClient::didFinishLoading):
            * loader/cache/CachedResourceRequest.cpp:
            (WebCore::CachedResourceRequest::didFinishLoading):
            * loader/cache/CachedResourceRequest.h:
            * loader/icon/IconLoader.cpp:
            (WebCore::IconLoader::didFinishLoading):
            * loader/icon/IconLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/SubresourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/SubresourceLoader.cpp
@@ -179,7 +179,7 @@ void SubresourceLoader::didFinishLoading(double finishTime)
     RefPtr<SubresourceLoader> protect(this);
 
     if (m_client)
-        m_client->didFinishLoading(this);
+        m_client->didFinishLoading(this, finishTime);
     
     m_handle = 0;
 
