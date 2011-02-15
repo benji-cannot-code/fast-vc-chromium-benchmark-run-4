929bb2d765cb6279885b339d91faa1cab1f93ad0
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-15 22:37:18 +0000

    2011-02-15  Simon Fraser  <simon.fraser@apple.com>
            Fix Mac builds that don't use rubber-band scrolling
            by moving m_drawingIntoLayer out of the #ifdef.
    
            * platform/mac/ScrollAnimatorMac.h:
            * platform/mac/ScrollAnimatorMac.mm:
            (WebCore::ScrollAnimatorMac::ScrollAnimatorMac):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
@@ -118,7 +118,6 @@ private:
     bool m_inScrollGesture;
     bool m_momentumScrollInProgress;
     bool m_ignoreMomentumScrolls;
-    bool m_drawingIntoLayer;
     
     CFTimeInterval m_lastMomemtumScrollTimestamp;
     FloatSize m_overflowScrollDelta;
@@ -132,6 +131,7 @@ private:
     FloatSize m_origVelocity;
     Timer<ScrollAnimatorMac> m_snapRubberBandTimer;
 #endif
+    bool m_drawingIntoLayer;
 };
 
 } // namespace WebCore
