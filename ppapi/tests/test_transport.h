1b72a47c0016dd9c54cbff4f4ed047e4524e5352
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 00:17:22 +0000

    Basic implementation of Pepper Transport API.
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/6478018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75035 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_transport.h
+++ ppapi/tests/test_transport.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef PAPPI_TESTS_TEST_TRANSPORT_H_
-#define PAPPI_TESTS_TEST_TRANSPORT_H_
+#ifndef PPAPI_TESTS_TEST_TRANSPORT_H_
+#define PPAPI_TESTS_TEST_TRANSPORT_H_
 
 #include <string>
 
@@ -25,10 +25,10 @@ class TestTransport : public TestCase {
   virtual void RunTest();
 
  private:
-  std::string TestFirstTransport();
+  std::string TestBasics();
 
   // Used by the tests that access the C API directly.
   const PPB_Transport_Dev* transport_interface_;
 };
 
-#endif  // PAPPI_TESTS_TEST_TRANSPORT_H_
+#endif  // PPAPI_TESTS_TEST_TRANSPORT_H_
