0056abafc182894fb200c245d2202c6aa281fe84
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 15:42:34 +0000

    Update minimum Flash version implementing NPP_ClearSiteData.
    BUG=58235
    TEST=none
    
    Review URL: http://codereview.chromium.org/6480115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_data_remover.cc
+++ chrome/browser/plugin_data_remover.cc
@@ -22,9 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 const char* kFlashMimeType = "application/x-shockwave-flash";
-// TODO(bauerb): Update minimum required Flash version as soon as there is one
-// implementing the API.
-const char* kMinFlashVersion = "100";
+// The minimum Flash Player version that implements NPP_ClearSiteData.
+const char* kMinFlashVersion = "10.3";
 const int64 kRemovalTimeoutMs = 10000;
 const uint64 kClearAllData = 0;
 }  // namespace
