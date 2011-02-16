5f43ba5ce2be1ec206ac735c1201d28be0212a44
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 03:37:04 +0000

    Rename ProcessPaint to Paint() Make OnPaint* methods protected.
    http://crbug.com/72040
    TEST=none
    TBR=sky
    
    Review URL: http://codereview.chromium.org/6529037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75063 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/root_view.cc
+++ views/widget/root_view.cc
@@ -64,7 +64,7 @@ namespace {
 
 #ifndef NDEBUG
 // Sets the value of RootView's |is_processing_paint_| member to true as long
-// as ProcessPaint is being called. Sets it to |false| when it returns.
+// as Paint() is being called. Sets it to |false| when it returns.
 class ScopedProcessingPaint {
  public:
   explicit ScopedProcessingPaint(bool* is_processing_paint)
@@ -359,7 +359,7 @@ void RootView::SchedulePaintInRect(const gfx::Rect& r, bool urgent) {
   }
 }
 
-void RootView::ProcessPaint(gfx::Canvas* canvas) {
+void RootView::Paint(gfx::Canvas* canvas) {
 #ifndef NDEBUG
   ScopedProcessingPaint processing_paint(&is_processing_paint_);
 #endif
@@ -384,7 +384,7 @@ void RootView::ProcessPaint(gfx::Canvas* canvas) {
                       invalid_rect_.height());
 
   // Paint the tree
-  View::ProcessPaint(canvas);
+  View::Paint(canvas);
 
   // Restore the previous transform
   canvas->Restore();
--- views/window/window_win.cc
+++ views/window/window_win.cc
@@ -1082,7 +1082,7 @@ void WindowWin::OnNCPaint(HRGN rgn) {
     gfx::CanvasSkiaPaint canvas(dc, opaque(), dirty_region.left,
                                 dirty_region.top, dirty_region.Width(),
                                 dirty_region.Height());
-    root_view->ProcessPaint(&canvas);
+    root_view->Paint(&canvas);
   }
 
   ReleaseDC(GetNativeView(), dc);
