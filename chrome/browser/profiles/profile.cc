0f999fdff09c7a6e778199384af155b551d41ab9
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 04:27:46 +0000

    Fix a bug that caused Chrome OS system preferences not to work in the guest mode.
    Before this change, we were initializing Chrome OS system preferences
    in ProfileImpl's constructor, but this didn't work in the guest mode,
    since we'll switch the profile implementation (GuestSessionProfile)
    afterward in the guest mode.
    
    BUG=chromium-os:11912
    TEST=confirm that "Enable tap-to-click" works both in the guest and the normal mode.
    
    Review URL: http://codereview.chromium.org/6484033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75067 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/native_backend_gnome_x.h"
 #include "chrome/browser/password_manager/native_backend_kwallet_x.h"
 #include "chrome/browser/password_manager/password_store_x.h"
+#elif defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/preferences.h"
 #endif
 
 using base::Time;
@@ -592,6 +594,11 @@ class OffTheRecordProfileImpl : public Profile,
   virtual void SetupChromeOSEnterpriseExtensionObserver() {
     profile_->SetupChromeOSEnterpriseExtensionObserver();
   }
+
+  virtual void InitChromeOSPreferences() {
+    // The off-the-record profile shouldn't have Chrome OS's preferences.
+    // The preferences are associated with the regular user profile.
+  }
 #endif  // defined(OS_CHROMEOS)
 
   virtual void ExitedOffTheRecordMode() {
@@ -744,6 +751,15 @@ class GuestSessionProfile : public OffTheRecordProfileImpl {
   virtual PersonalDataManager* GetPersonalDataManager() {
     return GetOriginalProfile()->GetPersonalDataManager();
   }
+
+  virtual void InitChromeOSPreferences() {
+    chromeos_preferences_.reset(new chromeos::Preferences());
+    chromeos_preferences_->Init(GetPrefs());
+  }
+
+ private:
+  // The guest user should be able to customize Chrome OS preferences.
+  scoped_ptr<chromeos::Preferences> chromeos_preferences_;
 };
 #endif
 
