39e09bd2947ea8ac92e83d5fddfad066919094fc
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 10:04:36 +0000

    Correctly update description and version when changing a PluginGroup.
    PluginListTest:UpdatedPlugin tests that we don't regress on updating a disabled plugin.
    
    BUG=72946
    TEST=PluginGroupTest.MultipleVersions:PluginListTest:UpdatedPlugin
    
    Review URL: http://codereview.chromium.org/6525012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75088 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_group.h
+++ webkit/plugins/npapi/plugin_group.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -233,15 +233,17 @@ class PluginGroup {
   // enabled one, or if all plugins are disabled, simply the first one.
   void UpdateActivePlugin(const WebPluginInfo& plugin);
 
-  // Refreshes the enabled flag based on the state of its plugins.
-  void RefreshEnabledState();
+  // Resets the group state to its default value (as if the group was empty).
+  // After calling this method, calling |UpdateActivePlugin| with all plugins
+  // in a row will correctly set the group state.
+  void ResetGroupState();
 
   // Enables the plugin if not already enabled and if policy allows it to.
-  // Returns true on success.
+  // Returns true on success. Does not update the group state.
   static bool Enable(WebPluginInfo* plugin, int reason);
 
   // Disables the plugin if not already disabled and if policy allows it to.
-  // Returns true on success.
+  // Returns true on success. Does not update the group state.
   static bool Disable(WebPluginInfo* plugin, int reason);
 
   // Returns a non-const vector of all plugins in the group. This is only used
