benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit be5bb379467b168bceb5a90d25552101a1fa0539

Add "open as window" menu item to NTP app menu.
BUG=59697
TEST=BrowserTest.OpenAppWindowLikeNtp,SessionRestoreUITest.RestoreAfterClosing*

Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=66646

Review URL: http://codereview.chromium.org/5019005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66954 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/ntp/apps.js
@@ -22,6 +22,10 @@ function getAppsCallback(data) {
   var appsPromoPing = PING_WEBSTORE_LAUNCH_PREFIX + '+' + data.showPromo;
   var webStoreEntry;
 
+  // Hide the app window menu option on platforms that do not support it.
+  $('apps-launch-type-window-menu-item').style.display =
+      (data.disableAppWindowLaunch ? 'none' : 'inline');
+
   appsMiniview.textContent = '';
   appsSectionContent.textContent = '';
 
@@ -146,10 +150,11 @@ var apps = (function() {
   var LaunchType = {
     LAUNCH_PINNED: 0,
     LAUNCH_REGULAR: 1,
-    LAUNCH_FULLSCREEN: 2
+    LAUNCH_FULLSCREEN: 2,
+    LAUNCH_WINDOW: 3
   };
 
-  // Keep in sync with LaunchContainer in extension.h
+  // Keep in sync with LaunchContainer in extension_constants.h
   var LaunchContainer = {
     LAUNCH_WINDOW: 0,
     LAUNCH_PANEL: 1,
@@ -185,7 +190,8 @@ var apps = (function() {
         // Update the commands related to the launch type.
         var launchTypeIds = ['apps-launch-type-pinned',
                              'apps-launch-type-regular',
-                             'apps-launch-type-fullscreen'];
+                             'apps-launch-type-fullscreen',
+                             'apps-launch-type-window'];
         launchTypeIds.forEach(function(id) {
           var command = $(id);
           command.disabled = isPanel;
@@ -216,6 +222,7 @@ var apps = (function() {
       case 'apps-launch-type-pinned':
       case 'apps-launch-type-regular':
       case 'apps-launch-type-fullscreen':
+      case 'apps-launch-type-window':
         chrome.send('setLaunchType',
             [currentApp['id'], e.command.getAttribute('launch-type')]);
         break;

chrome/app/generated_resources.grd
@@ -7433,6 +7433,10 @@ Keep your key file in a safe place. You will need it to create new versions of y
                desc="Text for the button that opens the app in a regular tab.">
         Open as regular tab
       </message>
+      <message name="IDS_APP_CONTEXT_MENU_OPEN_WINDOW"
+               desc="Text for the button that opens the app in an app window.">
+        Open as window
+      </message>
       <message name="IDS_APP_CONTEXT_MENU_OPEN_FULLSCREEN"
                desc="Text for the button that opens the app full screen.">
         Open full screen

chrome/browser/background_application_list_model.cc
@@ -243,6 +243,7 @@ void BackgroundApplicationListModel::Observe(
   ExtensionsService* service = profile_->GetExtensionsService();
   if (!service || !service->is_ready())
     return;
+
   switch (type.value) {
     case NotificationType::EXTENSION_LOADED:
       OnExtensionLoaded(Details<Extension>(details).ptr());
@@ -287,7 +288,6 @@ void BackgroundApplicationListModel::RemoveObserver(Observer* observer) {
 // each observer.
 void BackgroundApplicationListModel::Update() {
   ExtensionsService* service = profile_->GetExtensionsService();
-  DCHECK(service->is_ready());
 
   // Discover current background applications, compare with previous list, which
   // is consistently sorted, and notify observers if they differ.

chrome/browser/browser_browsertest.cc
@@ -574,6 +574,42 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, CloseWithAppMenuOpen) {
                                    new RunCloseWithAppMenuTask(browser()));
 }
 
+#if !defined(OS_MACOSX)
+IN_PROC_BROWSER_TEST_F(BrowserTest, OpenAppWindowLikeNtp) {
+  ASSERT_TRUE(test_server()->Start());
+
+  // Load an app
+  host_resolver()->AddRule("www.example.com", "127.0.0.1");
+  ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("app/")));
+  const Extension* extension_app = GetExtension();
+
+  // Launch it in a window, as AppLauncherHandler::HandleLaunchApp() would.
+  TabContents* app_window = Browser::OpenApplication(
+      browser()->profile(), extension_app, extension_misc::LAUNCH_WINDOW, NULL);
+  ASSERT_TRUE(app_window);
+
+  // Apps launched in a window from the NTP do not have extension_app set in
+  // tab contents.
+  EXPECT_FALSE(app_window->extension_app());
+  EXPECT_EQ(extension_app->GetFullLaunchURL(), app_window->GetURL());
+
+  // The launch should have created a new browser.
+  ASSERT_EQ(2u, BrowserList::GetBrowserCount(browser()->profile()));
+
+  // Find the new browser.
+  Browser* new_browser = NULL;
+  for (BrowserList::const_iterator i = BrowserList::begin();
+       i != BrowserList::end() && !new_browser; ++i) {
+    if (*i != browser())
+      new_browser = *i;
+  }
+  ASSERT_TRUE(new_browser);
+  ASSERT_TRUE(new_browser != browser());
+
+  EXPECT_EQ(Browser::TYPE_APP, new_browser->type());
+}
+#endif  // !defined(OS_MACOSX)
+
 // TODO(ben): this test was never enabled. It has bit-rotted since being added.
 // It originally lived in browser_unittest.cc, but has been moved here to make
 // room for real browser unit tests.

chrome/browser/dom_ui/app_launcher_handler.cc
@@ -191,6 +191,14 @@ void AppLauncherHandler::FillAppDictionary(DictionaryValue* dictionary) {
   bool showLauncher =
       CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableAppLauncher);
   dictionary->SetBoolean("showLauncher", showLauncher);
+
+#if defined(OS_MACOSX)
+  // App windows are not yet implemented on mac.
+  bool disable_app_window_launch = true;
+#else
+  bool disable_app_window_launch = false;
+#endif
+  dictionary->SetBoolean("disableAppWindowLaunch", disable_app_window_launch);
 }
 
 void AppLauncherHandler::HandleGetApps(const ListValue* args) {
@@ -249,8 +257,18 @@ void AppLauncherHandler::HandleLaunchApp(const ListValue* args) {
     old_contents = browser->GetSelectedTabContents();
 
   AnimateAppIcon(extension, rect);
+
+  extension_misc::LaunchContainer launch_container =
+      extension->launch_container();
+  ExtensionPrefs::LaunchType prefs_launch_type =
+      extensions_service_->extension_prefs()->GetLaunchType(extension_id);
+
+  // If the user chose to open in a window, change the container type.
+  if (prefs_launch_type == ExtensionPrefs::LAUNCH_WINDOW)
+    launch_container = extension_misc::LAUNCH_WINDOW;
+
   TabContents* new_contents = Browser::OpenApplication(
-      profile, extension, extension->launch_container(), old_contents);
+      profile, extension, launch_container, old_contents);
 
   if (new_contents != old_contents && browser->tab_count() > 1)
     browser->CloseTabContents(old_contents);
@@ -311,7 +329,7 @@ void AppLauncherHandler::HandleHideAppsPromo(const ListValue* args) {
   extensions_service_->default_apps()->SetPromoHidden();
 }
 
-//static
+// static
 void AppLauncherHandler::RecordWebStoreLaunch(bool promo_active) {
   if (!promo_active) return;
 
@@ -320,7 +338,7 @@ void AppLauncherHandler::RecordWebStoreLaunch(bool promo_active) {
                             extension_misc::PROMO_BUCKET_BOUNDARY);
 }
 
-//static
+// static
 void AppLauncherHandler::RecordAppLaunch(bool promo_active) {
   // TODO(jstritar): record app launches that occur when the promo is not
   // active using a different histogram.

chrome/browser/dom_ui/ntp_resource_cache.cc
@@ -311,6 +311,8 @@ void NTPResourceCache::CreateNewTabHTML() {
       l10n_util::GetStringUTF16(IDS_APP_CONTEXT_MENU_OPEN_PINNED));
   localized_strings.SetString("applaunchtyperegular",
       l10n_util::GetStringUTF16(IDS_APP_CONTEXT_MENU_OPEN_REGULAR));
+  localized_strings.SetString("applaunchtypewindow",
+      l10n_util::GetStringUTF16(IDS_APP_CONTEXT_MENU_OPEN_WINDOW));
   localized_strings.SetString("applaunchtypefullscreen",
       l10n_util::GetStringUTF16(IDS_APP_CONTEXT_MENU_OPEN_FULLSCREEN));
   localized_strings.SetString("web_store_title",

chrome/browser/extensions/extension_dom_ui.cc
@@ -244,13 +244,6 @@ bool ExtensionDOMUI::HandleChromeURLOverride(GURL* url, Profile* profile) {
     return false;
 
   ExtensionsService* service = profile->GetExtensionsService();
-  if (!service->is_ready()) {
-    // TODO(erikkay) So far, it looks like extensions load before the new tab
-    // page.  I don't know if we have anything that enforces this, so add this
-    // check for safety.
-    NOTREACHED() << "Chrome URL override requested before extensions loaded";
-    return false;
-  }
 
   size_t i = 0;
   while (i < url_list->GetSize()) {

chrome/browser/extensions/extension_prefs.cc
@@ -457,12 +457,22 @@ void ExtensionPrefs::SetAllowFileAccess(const std::string& extension_id,
 ExtensionPrefs::LaunchType ExtensionPrefs::GetLaunchType(
     const std::string& extension_id) {
   int value;
-  if (ReadExtensionPrefInteger(extension_id, kPrefLaunchType, &value) && (
-      value == LAUNCH_PINNED ||
+  if (ReadExtensionPrefInteger(extension_id, kPrefLaunchType, &value) &&
+     (value == LAUNCH_PINNED ||
       value == LAUNCH_REGULAR ||
-      value == LAUNCH_FULLSCREEN)) {
+      value == LAUNCH_FULLSCREEN ||
+      value == LAUNCH_WINDOW)) {
+
+#if defined(OS_MACOSX)
+    // App windows are not yet supported on mac.  Pref sync could make
+    // the launch type LAUNCH_WINDOW, even if there is no UI to set it
+    // on mac.
+    if (value == LAUNCH_WINDOW)
+      return LAUNCH_REGULAR;
+#endif
     return static_cast<LaunchType>(value);
   }
+
   return LAUNCH_REGULAR;
 }
 

commit 07f06d2e7f66a8932f27d93a6584d90248b9f79c

NTP: Fix issue with keyboard navigation of the context menu
We need to use the hidden property/attribute to get keyboard navigation working.

This regressed in r66954

BUG=None
TEST=On NTP
     Right click one of the Apps
     Press down/up keys it should select the next/previous menu item.

Review URL: http://codereview.chromium.org/6528026

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75205 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/ntp/apps.js
@@ -25,13 +25,12 @@ function getAppsCallback(data) {
   // Hide menu options that are not supported on the OS or windowing system.
 
   // The "Launch as Window" menu option.
-  $('apps-launch-type-window-menu-item').style.display =
-      (data.disableAppWindowLaunch ? 'none' : 'inline');
+  $('apps-launch-type-window-menu-item').hidden = data.disableAppWindowLaunch;
 
   // The "Create App Shortcut" menu option.
-  $('apps-create-shortcut-command-menu-item').style.display =
-      $('apps-create-shortcut-command-separator').style.display =
-      (data.disableCreateAppShortcut ? 'none' : '');
+  $('apps-create-shortcut-command-menu-item').hidden =
+      $('apps-create-shortcut-command-separator').hidden =
+          data.disableCreateAppShortcut;
 
   appsMiniview.textContent = '';
   appsSectionContent.textContent = '';