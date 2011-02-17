390f717525dc0254d98db32394c7054233861ba4
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-17 09:35:59 +0000

    2011-02-17  Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Add WKPluginSiteDataManager.cpp compilation after r78647
            https://bugs.webkit.org/show_bug.cgi?id=54564
    
            * GNUmakefile.am: Added files to compilation.
            * Shared/Plugins/Netscape/NetscapePluginModule.h: used
            npruntime_internal.h in the include to avoid problems with X
            types.
            * WebProcess/WebProcess.cpp:
            (WebKit::WebProcess::setAlwaysUsesComplexTextCodePath): fixed
            ambiguous reference to Font caused by the X headers required by
            the plugins.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/Plugins/Netscape/NetscapePluginModule.h
+++ third_party/WebKit/Source/WebKit2/Shared/Plugins/Netscape/NetscapePluginModule.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Module.h"
 #include "PluginQuirks.h"
-#include <WebCore/npfunctions.h>
+#include <WebCore/npruntime_internal.h>
 #include <wtf/RefCounted.h>
 #include <wtf/text/WTFString.h>
 
