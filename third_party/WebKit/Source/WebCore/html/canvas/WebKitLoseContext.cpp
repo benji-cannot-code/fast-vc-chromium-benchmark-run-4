02f6fd7e018b6f2a93d032ea44f5c6c78b2f95c6
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-17 23:32:56 +0000

    2011-02-17  Sergey Glazunov  <serg.glazunov@gmail.com>
            Reviewed by Kenneth Russell.
    
            Null out the WEBKIT_lose_context WebGL extension's context pointer when
            the WebGL rendering context is removed.
            https://bugs.webkit.org/show_bug.cgi?id=54679
    
            * fast/canvas/webgl/context-destroyed-crash-expected.txt: Added.
            * fast/canvas/webgl/context-destroyed-crash.html: Added.
    2011-02-17  Sergey Glazunov  <serg.glazunov@gmail.com>
    
            Reviewed by Kenneth Russell.
    
            Null out the WEBKIT_lose_context WebGL extension's context pointer when
            the WebGL rendering context is removed.
            https://bugs.webkit.org/show_bug.cgi?id=54679
    
            Test: fast/canvas/webgl/context-destroyed-crash.html
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::~WebGLRenderingContext):
            * html/canvas/WebKitLoseContext.cpp:
            (WebCore::WebKitLoseContext::loseContext): Add null-check for m_context.
            * html/canvas/WebKitLoseContext.h:
            (WebCore::WebKitLoseContext::contextDestroyed):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78921 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/WebKitLoseContext.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/WebKitLoseContext.cpp
@@ -56,7 +56,8 @@ PassRefPtr<WebKitLoseContext> WebKitLoseContext::create(WebGLRenderingContext* c
 
 void WebKitLoseContext::loseContext()
 {
-    m_context->forceLostContext();
+    if (m_context)
+        m_context->forceLostContext();
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/html/canvas/WebKitLoseContext.h
+++ third_party/WebKit/Source/WebCore/html/canvas/WebKitLoseContext.h
@@ -43,6 +43,7 @@ public:
     virtual ExtensionName getName() const;
 
     void loseContext();
+    void contextDestroyed() { m_context = 0; }
 
 private:
     WebKitLoseContext(WebGLRenderingContext*);
