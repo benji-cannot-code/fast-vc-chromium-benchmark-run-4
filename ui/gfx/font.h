674d36ef88cf6b365454cf2f9fc14c70aa2b02f8
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-17 14:55:26 +0000

    Fix leaks found on Linux in ui/gfx/font_unittest.cc.
    Also update the comment in font.h that incorrectly described the Mac memory
    management for native font handles.
    
    BUG=73148
    TEST=Linux Valgrind
    
    Review URL: http://codereview.chromium.org/6524042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75266 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/font.h
+++ ui/gfx/font.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -95,7 +95,7 @@ class Font {
   // Lifetime lore:
   // Windows: This handle is owned by the Font object, and should not be
   //          destroyed by the caller.
-  // Mac:     Caller must release this object.
+  // Mac:     The object is owned by the system and should not be released.
   // Gtk:     This handle is created on demand, and must be freed by calling
   //          pango_font_description_free() when the caller is done using it.
   NativeFont GetNativeFont() const;
