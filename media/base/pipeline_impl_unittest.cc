47b4647bae0b36d0e11ac405e91b839fa1f41811
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-17 02:21:27 +0000

    Delete the StatisticsCallback passed to media filter mocks.
    Patch by sjl@chromium.org:
    http://codereview.chromium.org/6519024/
    
    BUG=73132
    TEST=media_unittests under heapcheck and valgrind to make sure the new leak has gone.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75222 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_impl_unittest.cc
+++ media/base/pipeline_impl_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::testing::_;
+using ::testing::DeleteArg;
 using ::testing::InSequence;
 using ::testing::Invoke;
 using ::testing::Mock;
@@ -151,7 +152,7 @@ class PipelineImplTest : public ::testing::Test {
   void InitializeVideoDecoder(MockDemuxerStream* stream) {
     EXPECT_CALL(*mocks_->video_decoder(),
                 Initialize(stream, NotNull(), NotNull()))
-        .WillOnce(Invoke(&RunFilterCallback3));
+        .WillOnce(DoAll(Invoke(&RunFilterCallback3), DeleteArg<2>()));
     EXPECT_CALL(*mocks_->video_decoder(), SetPlaybackRate(0.0f));
     EXPECT_CALL(*mocks_->video_decoder(), Seek(base::TimeDelta(), NotNull()))
         .WillOnce(Invoke(&RunFilterCallback));
@@ -163,7 +164,7 @@ class PipelineImplTest : public ::testing::Test {
   void InitializeAudioDecoder(MockDemuxerStream* stream) {
     EXPECT_CALL(*mocks_->audio_decoder(),
                 Initialize(stream, NotNull(), NotNull()))
-        .WillOnce(Invoke(&RunFilterCallback3));
+        .WillOnce(DoAll(Invoke(&RunFilterCallback3), DeleteArg<2>()));
     EXPECT_CALL(*mocks_->audio_decoder(), SetPlaybackRate(0.0f));
     EXPECT_CALL(*mocks_->audio_decoder(), Seek(base::TimeDelta(), NotNull()))
         .WillOnce(Invoke(&RunFilterCallback));
@@ -175,7 +176,7 @@ class PipelineImplTest : public ::testing::Test {
   void InitializeVideoRenderer() {
     EXPECT_CALL(*mocks_->video_renderer(),
                 Initialize(mocks_->video_decoder(), NotNull(), NotNull()))
-        .WillOnce(Invoke(&RunFilterCallback3));
+        .WillOnce(DoAll(Invoke(&RunFilterCallback3), DeleteArg<2>()));
     EXPECT_CALL(*mocks_->video_renderer(), SetPlaybackRate(0.0f));
     EXPECT_CALL(*mocks_->video_renderer(), Seek(base::TimeDelta(), NotNull()))
         .WillOnce(Invoke(&RunFilterCallback));
