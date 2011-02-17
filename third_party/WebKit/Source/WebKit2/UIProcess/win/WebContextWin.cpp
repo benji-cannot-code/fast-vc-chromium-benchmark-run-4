99c55274f7270ac1462bf2955d1e8287ad5b2df6
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-17 22:23:26 +0000

    WebKit2: The CFNetwork Cache should be shared between the UI Process and the Web Process on Windows. https://bugs.webkit.org/show_bug.cgi?id=54683
    Reviewed by Adam Roben.
    
    * Shared/WebProcessCreationParameters.cpp:
    (WebKit::WebProcessCreationParameters::encode):
    (WebKit::WebProcessCreationParameters::decode):
    * Shared/WebProcessCreationParameters.h:
     
    * UIProcess/win/WebContextWin.cpp:
    (WebKit::WebContext::platformInitializeWebProcess):
    Make sure to remove the ending slash, as CFNetwork does not recognize the directory with
    that slash.
    
    * WebProcess/win/WebProcessWin.cpp:
    (WebKit::WebProcess::platformInitializeWebProcess):
    Create a cache using the path, disk capacity, and memory capacity and set it as default.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/win/WebContextWin.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/win/WebContextWin.cpp
@@ -31,6 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebProcessMessages.h"
 #include <WebCore/FileSystem.h>
 
+#if USE(CFNETWORK)
+#include <CFNetwork/CFURLCachePriv.h>
+#include <WebKitSystemInterface/WebKitSystemInterface.h> 
+#endif
+
 using namespace WebCore;
 
 namespace WebKit {
@@ -52,6 +57,17 @@ void WebContext::setShouldPaintNativeControls(bool b)
 void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& parameters)
 {
     parameters.shouldPaintNativeControls = m_shouldPaintNativeControls;
+
+#if USE(CFNETWORK)
+    RetainPtr<CFURLCacheRef> cfurlCache(AdoptCF, CFURLCacheCopySharedURLCache());
+    parameters.cfURLCacheDiskCapacity = CFURLCacheDiskCapacity(cfurlCache.get());
+    parameters.cfURLCacheMemoryCapacity = CFURLCacheMemoryCapacity(cfurlCache.get());
+
+    RetainPtr<CFStringRef> cfURLCachePath(AdoptCF, wkCopyFoundationCacheDirectory());
+    parameters.cfURLCachePath = String(cfURLCachePath.get());
+    // Remove the ending '/' (necessary to have CFNetwork find the Cache file).
+    parameters.cfURLCachePath.remove(parameters.cfURLCachePath.length() - 1);
+#endif
 }
 
 String WebContext::platformDefaultDatabaseDirectory() const
