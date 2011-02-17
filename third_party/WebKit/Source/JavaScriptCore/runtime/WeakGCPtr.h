d38bfbf29571b681fb9c12808b4ca4b6690f96be
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-17 20:35:59 +0000

    2011-02-17  Oliver Hunt  <oliver@apple.com>
            Reviewed by Geoffrey Garen.
    
            Refactor WriteBarrier and DeprecatedPtr to have less code duplication.
            https://bugs.webkit.org/show_bug.cgi?id=54608
    
            Make use of the tricks used for Handle, et al to avoid duplicating all
            of the logic for DeprecatedPtr and WriteBarrier simply to support known
            vs. unknown types.
    
            * JavaScriptCore.xcodeproj/project.pbxproj:
            * collector/handles/Global.h:
            (JSC::Global::internalSet):
            * collector/handles/Handle.h:
            (JSC::Handle::Handle):
            (JSC::Handle::get):
            * runtime/JSArray.cpp:
            (JSC::JSArray::sortNumeric):
            * runtime/JSObject.h:
            (JSC::JSObject::inlineGetOwnPropertySlot):
            * runtime/SlotAccessor.h: Added.
            (JSC::SlotTypes::getFromBaseType):
            (JSC::SlotTypes::convertToBaseType):
            (JSC::SlotTypes::getFromSlot):
            (JSC::SlotTypes::toJSValue):
            (JSC::SlotTypes::validateUpcast):
            (JSC::SlotAccessor::operator->):
            (JSC::SlotAccessor::operator*):
            * runtime/WeakGCPtr.h:
            (JSC::WeakGCPtr::get):
            (JSC::WeakGCPtr::internalSet):
            * runtime/WriteBarrier.h:
            (JSC::DeprecatedPtr::DeprecatedPtr):
            (JSC::DeprecatedPtr::get):
            (JSC::DeprecatedPtr::slot):
            (JSC::DeprecatedPtr::operator=):
            (JSC::WriteBarrierTranslator::convertToStorage):
            (JSC::WriteBarrierTranslator::convertFromStorage):
            (JSC::WriteBarrierBase::set):
            (JSC::WriteBarrierBase::get):
            (JSC::WriteBarrierBase::clear):
            (JSC::WriteBarrierBase::slot):
            (JSC::WriteBarrierBase::operator UnspecifiedBoolType*):
            (JSC::WriteBarrierBase::operator!):
            (JSC::WriteBarrierBase::setWithoutWriteBarrier):
            (JSC::WriteBarrier::WriteBarrier):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@78856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/WeakGCPtr.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/WeakGCPtr.h
@@ -33,11 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace JSC {
 // A smart pointer whose get() function returns 0 for cells that have died
 
-template <typename T> class WeakGCPtr : public HandleConverter<WeakGCPtr<T>, T> {
+template <typename T> class WeakGCPtr : public SlotAccessor<WeakGCPtr<T>, T> {
     WTF_MAKE_NONCOPYABLE(WeakGCPtr);
 
 public:
-    typedef typename HandleTypes<T>::ExternalType ExternalType;
+    typedef typename SlotTypes<T>::ExternalType ExternalType;
     
     WeakGCPtr()
         : m_slot(0)
@@ -57,7 +57,7 @@ public:
         internalSet(value);
     }
 
-    ExternalType get() const { return  HandleTypes<T>::getFromSlot(m_slot); }
+    ExternalType get() const { return  SlotTypes<T>::getFromSlot(m_slot); }
     
     void clear()
     {
@@ -92,7 +92,7 @@ private:
     void internalSet(ExternalType value)
     {
         ASSERT(m_slot);
-        JSValue newValue(HandleTypes<T>::toJSValue(value));
+        JSValue newValue(SlotTypes<T>::toJSValue(value));
         HandleHeap::heapFor(m_slot)->writeBarrier(m_slot, newValue);
         *m_slot = newValue;
     }
