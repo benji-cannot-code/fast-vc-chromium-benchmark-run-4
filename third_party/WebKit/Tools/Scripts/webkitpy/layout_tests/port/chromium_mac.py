d83edc787d1850ed95d54e9bef1aa99957bfa777
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-18 23:53:38 +0000

    2011-02-18  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Ojan Vafai.
    
            new-run-webkit-tests: enable multiple processes by default on mac
            https://bugs.webkit.org/show_bug.cgi?id=54596
    
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
@@ -112,6 +112,9 @@ class ChromiumMacPort(chromium.ChromiumPort):
 
         return chromium.ChromiumPort.default_child_processes(self)
 
+    def default_worker_model(self):
+        return 'processes'
+
     def driver_name(self):
         return "DumpRenderTree"
 
