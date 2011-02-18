55cd745c8f30ec58235987ded19b1aa843802711
yuzo@chromium.org <yuzo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-18 07:06:16 +0000

    For webkit_glue::MockWebFrame, define setScrollOffset() that will be soon introduced.
    (by https://bugs.webkit.org/show_bug.cgi?id=54632).
    
    Otherwise, landing the WebKit patch results in Chromium build breakage.
    Example:
    .\glue\media\buffered_resource_loader_unittest.cc(87) : error C2259: 'testing::NiceMock<MockClass>' : cannot instantiate abstract class
        with
        [
          MockClass=webkit_glue::MockWebFrame
        ]
        due to following members:
        'void WebKit::WebFrame::setScrollOffset(const WebKit::WebSize &)' : is abstract
        c:\b\build\slave\webkit-win-latest-rel\build\src\third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h(146) : see declaration of 'WebKit::WebFrame::setScrollOffset'
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6538049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75367 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/mocks/mock_webframe.cc
+++ webkit/mocks/mock_webframe.cc
@@ -45,6 +45,8 @@ WebSize MockWebFrame::scrollOffset() const {
   return WebSize(0,0);
 }
 
+void MockWebFrame::setScrollOffset(const WebSize&) {}
+
 WebSize MockWebFrame::contentsSize() const {
   return WebSize();
 }
