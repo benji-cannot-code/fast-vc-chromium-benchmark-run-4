e0a0093de12602910c7a58813f4900540b0e0855
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-18 20:55:25 +0000

    2011-02-18  Anders Carlsson  <andersca@apple.com>
            Reviewed by Simon Fraser.
    
            WKView flashes when entering/exiting compositing mode
            https://bugs.webkit.org/show_bug.cgi?id=54695
            <rdar://problem/9011554>
    
            * UIProcess/API/mac/WKView.mm:
            (-[WKView _exitAcceleratedCompositingMode]):
            Remove the layer hosting view before clearing out its layer, otherwise we can get
            white flashes when exiting accelerated compositing mode.
    
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            When entering accelerated compositing mode, we want to defer sending the message
            until we've actually committed the layer tree and pushed all changes over to the
            UI process.
    
            (WebKit::DrawingAreaImpl::layerHostDidFlushLayers):
            Tell the layer tree host to force a repaint. This will ensure that all layer tree
            changes are pushed over to the UI process. When that is done, send the new layer tree
            context over to the UI process.
    
            (WebKit::DrawingAreaImpl::setRootCompositingLayer):
            When exiting compositing mode in response to a didSetSize, we want to exit accelerated 
            compositing mode right away to avoid flashes. This is safe since we've laid out the page
            already so we won't end up reentering setRootCompositingLayer.
    
            (WebKit::DrawingAreaImpl::enterAcceleratedCompositingMode):
            Tell the layer tree host to notify us when the next layer tree flush happened, so we can
            let the UI process know. If we're entering accelerated compositing mode in response to a
            SetSize message, the new layer tree context will be passed in the DidSetSize message.
    
            * WebProcess/WebPage/DrawingAreaImpl.h:
            Add layerHostDidFlushLayers.
    
            * WebProcess/WebPage/LayerTreeHost.h:
            Add setShouldNotifyAfterNextScheduledLayerFlush.
    
            * WebProcess/WebPage/mac/LayerTreeHostMac.h:
            Add a m_notifyAfterScheduledLayerFlush flag.
    
            * WebProcess/WebPage/mac/LayerTreeHostMac.mm:
            (WebKit::LayerTreeHostMac::LayerTreeHostMac):
            Initialize m_notifyAfterScheduledLayerFlush to false.
    
            (WebKit::LayerTreeHostMac::setShouldNotifyAfterNextScheduledLayerFlush):
            Set m_notifyAfterScheduledLayerFlush to true.
    
            (WebKit::LayerTreeHostMac::flushPendingLayerChangesRunLoopObserverCallback):
            If m_notifyAfterScheduledLayerFlush is true, call DrawingAreaImpl::layerHostDidFlushLayers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -186,6 +186,18 @@ void DrawingAreaImpl::setPageOverlayNeedsDisplay(const IntRect& rect)
     setNeedsDisplay(rect);
 }
 
+void DrawingAreaImpl::layerHostDidFlushLayers()
+{
+    ASSERT(m_layerTreeHost);
+
+    m_layerTreeHost->forceRepaint();
+
+    if (!m_layerTreeHost)
+        return;
+
+    m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(generateSequenceNumber(), m_layerTreeHost->layerTreeContext()));
+}
+
 void DrawingAreaImpl::attachCompositingContext()
 {
 }
@@ -211,7 +223,12 @@ void DrawingAreaImpl::setRootCompositingLayer(GraphicsLayer* graphicsLayer)
             if (!m_alwaysUseCompositing) {
                 // We'll exit accelerated compositing mode on a timer, to avoid re-entering
                 // compositing code via display() and layout.
-                exitAcceleratedCompositingModeSoon();
+                // If we're leaving compositing mode because of a setSize, it is safe to
+                // exit accelerated compositing mode right away.
+                if (m_inSetSize)
+                    exitAcceleratedCompositingMode();
+                else
+                    exitAcceleratedCompositingModeSoon();
             }
         }
     }
@@ -305,6 +322,9 @@ void DrawingAreaImpl::enterAcceleratedCompositingMode(GraphicsLayer* graphicsLay
     ASSERT(!m_layerTreeHost);
 
     m_layerTreeHost = LayerTreeHost::create(m_webPage);
+    if (!m_inSetSize)
+        m_layerTreeHost->setShouldNotifyAfterNextScheduledLayerFlush();
+
     m_layerTreeHost->setRootCompositingLayer(graphicsLayer);
     
     // Non-composited content will now be handled exclusively by the layer tree host.
@@ -313,9 +333,6 @@ void DrawingAreaImpl::enterAcceleratedCompositingMode(GraphicsLayer* graphicsLay
     m_scrollOffset = IntSize();
     m_displayTimer.stop();
     m_isWaitingForDidUpdate = false;
-
-    if (!m_inSetSize)
-        m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(generateSequenceNumber(), m_layerTreeHost->layerTreeContext()));
 }
 
 void DrawingAreaImpl::exitAcceleratedCompositingMode()
