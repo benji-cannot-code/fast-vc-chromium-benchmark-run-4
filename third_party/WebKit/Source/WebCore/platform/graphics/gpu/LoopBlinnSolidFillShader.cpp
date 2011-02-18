f08e04a11defabb8edd2313664ab5226f0a54ba1
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-18 21:53:00 +0000

    2011-02-18  Kenneth Russell  <kbr@google.com>
            Unreviewed, Chromium build fix on certain Linux platforms.
    
            * platform/graphics/gpu/LoopBlinnSolidFillShader.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gpu/LoopBlinnSolidFillShader.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gpu/LoopBlinnSolidFillShader.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "LoopBlinnSolidFillShader.h"
 
+#include "Color.h"
 #include "GraphicsContext3D.h"
 
 namespace WebCore {
