30d16cb635999af75cc674161cf0fbe3a7bad51f
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 01:05:07 +0000

    Fix for <rdar://problem/9018729> Horizontal scroller doesn't appear when loading a page with a Horizontal scrollbar from the back/forward cache.
    Reviewed by Sam Weinig.
    
    This patch adds a new  bool member variable to FrameView to 
    keep track of whether we are loading a page from the back/
    forward cache. If we are, don't suppress scrollbars on 
    first layout.
    * history/CachedFrame.cpp:
    (WebCore::CachedFrameBase::restore):
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    (WebCore::FrameView::reset):
    (WebCore::FrameView::layout):
    * page/FrameView.h:
    (WebCore::FrameView::setIsRestoringFromBackForward):
    (WebCore::FrameView::isRestoringFromBackForward):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameView.cpp
@@ -140,6 +140,7 @@ FrameView::FrameView(Frame* frame)
     , m_shouldUpdateWhileOffscreen(true)
     , m_deferSetNeedsLayouts(0)
     , m_setNeedsLayoutWasDeferred(false)
+    , m_isRestoringFromBackForward(false)
     , m_scrollCorner(0)
 {
     init();
@@ -225,6 +226,7 @@ void FrameView::reset()
     m_isPainting = false;
     m_isVisuallyNonEmpty = false;
     m_firstVisuallyNonEmptyLayoutCallbackPending = true;
+    m_isRestoringFromBackForward = false;
     m_maintainScrollPositionAnchor = 0;
 }
 
@@ -844,7 +846,12 @@ void FrameView::layout(bool allowSubtree)
 
         if (m_firstLayout || (hMode != currentHMode || vMode != currentVMode)) {
             if (m_firstLayout) {
-                setScrollbarsSuppressed(true);
+                if (!m_isRestoringFromBackForward)
+                    setScrollbarsSuppressed(true);
+                else {
+                    setScrollbarsSuppressed(false);
+                    m_isRestoringFromBackForward = false;
+                }
 
                 m_firstLayout = false;
                 m_firstLayoutCallbackPending = true;
