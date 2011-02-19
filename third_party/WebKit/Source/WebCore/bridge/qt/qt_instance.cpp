600f5bbc9fadba111cfb8b8c2df4d18f8bc65d03
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 22:32:28 +0000

    Qt build fix.
    * bridge/qt/qt_instance.cpp:
    * bridge/qt/qt_pixmapruntime.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.cpp
+++ third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qt_class.h"
 #include "qt_runtime.h"
 #include "runtime_object.h"
+#include "runtime/FunctionPrototype.h"
 
 #include <qdebug.h>
 #include <qhash.h>
