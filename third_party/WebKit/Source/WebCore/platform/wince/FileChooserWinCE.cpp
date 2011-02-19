30df8ece08e532255783216dc035553bc6aa61a1
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 01:00:19 +0000

    2011-02-18  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed WinCE build fix for r78846.
    
            * platform/graphics/wince/FontWinCE.cpp:
            (WebCore::TextRunComponent::TextRunComponent):
            * platform/graphics/wince/GraphicsContextWinCE.cpp:
            (WebCore::GraphicsContext::drawLineForText):
            (WebCore::GraphicsContext::drawLineForTextChecking):
            (WebCore::GraphicsContext::drawText):
            * platform/wince/FileChooserWinCE.cpp:
            (WebCore::FileChooser::basenameForWidth):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/wince/FileChooserWinCE.cpp
+++ third_party/WebKit/Source/WebCore/platform/wince/FileChooserWinCE.cpp
@@ -53,9 +53,9 @@ String FileChooser::basenameForWidth(const Font& font, int width) const
         String tmpFilename = m_filenames[0];
         string = pathGetFileName(tmpFilename);
     } else
-        return StringTruncator::rightTruncate(String::number(m_filenames.size()) + " files", width, font, false);
+        return StringTruncator::rightTruncate(String::number(m_filenames.size()) + " files", width, font);
 
-    return StringTruncator::centerTruncate(string, width, font, false);
+    return StringTruncator::centerTruncate(string, width, font);
 }
 
 } // namespace WebCore
