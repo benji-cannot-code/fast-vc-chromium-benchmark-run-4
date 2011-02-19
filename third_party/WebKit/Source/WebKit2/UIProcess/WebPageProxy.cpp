080a0863ba95c6c5df9c7e86f7d169535248f6f2
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 00:48:58 +0000

    <rdar://problem/9026169> https://bugs.webkit.org/show_bug.cgi?id=54780 pendingAPIRequestURL isn't updated for WKPageGoToBackForwardListItem
    Reviewed by Maciej Stachowiak.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::goToBackForwardItem):
    Update the pending API URL.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -475,6 +475,8 @@ void WebPageProxy::goToBackForwardItem(WebBackForwardListItem* item)
         reattachToWebProcessWithItem(item);
         return;
     }
+    
+    setPendingAPIRequestURL(item->url());
 
     SandboxExtension::Handle sandboxExtensionHandle;
     initializeSandboxExtensionHandle(KURL(KURL(), item->url()), sandboxExtensionHandle);
