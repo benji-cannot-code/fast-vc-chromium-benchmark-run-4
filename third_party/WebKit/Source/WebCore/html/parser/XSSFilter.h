5d6763358f758bc31fe8e882b0850099a4f804d6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 08:12:02 +0000

    2011-02-19  Adam Barth  <abarth@webkit.org>
            Reviewed by Daniel Bates.
    
            Fix xssAuditor/iframe-injection.html
            https://bugs.webkit.org/show_bug.cgi?id=54591
    
            Update expected results to show that we pass.
    
            * http/tests/security/xssAuditor/iframe-injection-expected.txt:
    2011-02-19  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Daniel Bates.
    
            Fix xssAuditor/iframe-injection.html
            https://bugs.webkit.org/show_bug.cgi?id=54591
    
            We should block the iframe src attribute.  Although this technically
            can't be used to run script, it's a pretty easy vector for stealing
            passwords.
    
            * html/parser/XSSFilter.cpp:
            (WebCore::XSSFilter::filterTokenInitial):
            (WebCore::XSSFilter::filterIframeToken):
            * html/parser/XSSFilter.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSFilter.h
+++ third_party/WebKit/Source/WebCore/html/parser/XSSFilter.h
@@ -59,6 +59,7 @@ private:
     bool filterParamToken(HTMLToken&);
     bool filterEmbedToken(HTMLToken&);
     bool filterAppletToken(HTMLToken&);
+    bool filterIframeToken(HTMLToken&);
     bool filterMetaToken(HTMLToken&);
     bool filterBaseToken(HTMLToken&);
     bool filterFormToken(HTMLToken&);
