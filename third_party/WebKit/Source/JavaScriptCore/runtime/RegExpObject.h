d4818becdf22bb8ac845183694f5e76cec0c0751
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 03:52:55 +0000

    2011-02-18  Kristian Amlie  <kristian.amlie@nokia.com>
            Reviewed by Andreas Kling.
    
            Added friend exception to Qt platform, which also compiles Windows.
    
            QtWebKit fails to compile on Windows XP with msvc-2008
            https://bugs.webkit.org/show_bug.cgi?id=54746
    
            * bytecode/CodeBlock.h:
            * runtime/RegExpObject.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79083 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/RegExpObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/RegExpObject.h
@@ -71,7 +71,7 @@ namespace JSC {
             RefPtr<RegExp> regExp;
             double lastIndex;
         };
-#if PLATFORM(WIN)
+#if PLATFORM(WIN) || PLATFORM(QT)
         friend void WTF::deleteOwnedPtr<RegExpObjectData>(RegExpObjectData*);
 #endif
         OwnPtr<RegExpObjectData> d;
