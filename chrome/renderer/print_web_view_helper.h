eaee87c56bb462a5a712633a01944b96ffb7e595
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-19 00:06:51 +0000

    Cleanup PrintWebViewHelper.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6538048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.h
+++ chrome/renderer/print_web_view_helper.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_PRINT_WEB_VIEW_HELPER_H_
 #pragma once
 
-#include <vector>
-
 #include "base/scoped_ptr.h"
 #include "base/time.h"
 #include "chrome/renderer/render_view_observer.h"
