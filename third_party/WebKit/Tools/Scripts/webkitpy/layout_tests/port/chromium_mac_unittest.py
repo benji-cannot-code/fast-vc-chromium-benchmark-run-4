9f8b8b8fcd6cb1866df1ca4b99e3f4bbb0d3d66e
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-19 02:18:40 +0000

    2011-02-18  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by James Robinson.
    
            new-run-webkit-tests: enable multiple processes by default on
            mac. This change removes the artificial restrictions we placed
            on the # of child processes we used with the old threading
            model, and switches to the new message-based model and multiple
            processes, where available. If multiple processes are not
            available (Leopard / Python 2.5), then we use the 'inline' model
            instead of the 'old-threads' model on the 'mac' port or one
            process on the 'chromium-mac' port. We need additional testing
            to see if the new 'threads' model works reliably and is worth
            supporting, or if we should just live with things being slightly
            slow.
    
            https://bugs.webkit.org/show_bug.cgi?id=54596
    
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
            * Scripts/webkitpy/layout_tests/port/chromium_mac_unittest.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
            * Scripts/webkitpy/layout_tests/port/mac_unittest.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac_unittest.py
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+# Handle Python < 2.6 where multiprocessing isn't available.
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
 from webkitpy.thirdparty.mock import Mock
 
 from webkitpy.layout_tests.port import chromium_mac
@@ -46,6 +52,13 @@ class ChromiumMacPortTest(port_testcase.PortTestCase):
 
         # Currently is always true, just logs if missing.
         self.assertTrue(port._check_wdiff_install())
+
+    def test_check_default_worker_model(self):
+        port = chromium_mac.ChromiumMacPort()
+        if multiprocessing:
+            self.assertEqual(port.default_worker_model(), 'processes')
+        else:
+            self.assertEqual(port.default_worker_model(), 'inline')
 
     def assert_name(self, port_name, os_version_string, expected):
         port = chromium_mac.ChromiumMacPort(port_name=port_name,
