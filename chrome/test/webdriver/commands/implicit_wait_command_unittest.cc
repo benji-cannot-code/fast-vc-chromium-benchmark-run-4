a9fca0ef5e91f88713424f87dade3442fb6e3ac0
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-20 00:20:31 +0000

    Fix build by updating implicit_wait_command_unittest to use new Response class interface. BUG=none TEST=none TBR=jleyba
    Review URL: http://codereview.chromium.org/6543038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75509 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/commands/implicit_wait_command_unittest.cc
+++ chrome/test/webdriver/commands/implicit_wait_command_unittest.cc
@@ -21,7 +21,7 @@ namespace {
 void AssertIsAbleToInitialize(ImplicitWaitCommand* const command) {
   Response response;
   EXPECT_TRUE(command->Init(&response));
-  ASSERT_EQ(kSuccess, response.status()) << response.ToJSON();
+  ASSERT_EQ(kSuccess, response.GetStatus()) << response.ToJSON();
 }
 
 void AssertError(ErrorCode expected_status,
@@ -29,9 +29,9 @@ void AssertError(ErrorCode expected_status,
                  ImplicitWaitCommand* const command) {
   Response response;
   command->ExecutePost(&response);
-  ASSERT_EQ(expected_status,  response.status()) << response.ToJSON();
+  ASSERT_EQ(expected_status,  response.GetStatus()) << response.ToJSON();
 
-  const Value* value = response.value();
+  const Value* value = response.GetValue();
   ASSERT_TRUE(value->IsType(Value::TYPE_DICTIONARY));
 
   const DictionaryValue* dict = static_cast<const DictionaryValue*>(value);
@@ -44,7 +44,7 @@ void AssertTimeoutSet(const Session& test_session, int expected_timeout,
                       ImplicitWaitCommand* const command) {
   Response response;
   command->ExecutePost(&response);
-  ASSERT_EQ(kSuccess, response.status()) << response.ToJSON();
+  ASSERT_EQ(kSuccess, response.GetStatus()) << response.ToJSON();
   ASSERT_EQ(expected_timeout, test_session.implicit_wait());
 }
 
@@ -89,4 +89,3 @@ TEST(ImplicitWaitCommandTest, SettingImplicitWaits) {
 }
 
 }  // namespace webdriver
-
