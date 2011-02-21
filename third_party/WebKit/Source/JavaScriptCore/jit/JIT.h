7c818c0b353ca5a8e13a5e51479bdcb250b7f20f
xan@webkit.org <xan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 21:14:57 +0000

    2011-02-21  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gavin Barraclough.
    
            Use ASSERT_JIT_OFFSET in JITPropertyAccess32_64.cpp
            https://bugs.webkit.org/show_bug.cgi?id=54901
    
            * jit/JIT.h: swap actual and expected values in message, they were
            reversed.
            * jit/JITCall32_64.cpp:
            (JSC::JIT::compileOpCall): use ASSERT_JIT_OFFSET instead of
            a simple ASSERT.
            * jit/JITPropertyAccess32_64.cpp:
            (JSC::JIT::emit_op_method_check): ditto.
            (JSC::JIT::compileGetByIdHotPath): ditto.
            (JSC::JIT::compileGetByIdSlowCase): ditto.
            (JSC::JIT::emit_op_put_by_id): ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
+++ third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JIT_CLASS_ALIGNMENT
 #endif
 
-#define ASSERT_JIT_OFFSET(actual, expected) ASSERT_WITH_MESSAGE(actual == expected, "JIT Offset \"%s\" should be %d, not %d.\n", #expected, static_cast<int>(actual), static_cast<int>(expected));
+#define ASSERT_JIT_OFFSET(actual, expected) ASSERT_WITH_MESSAGE(actual == expected, "JIT Offset \"%s\" should be %d, not %d.\n", #expected, static_cast<int>(expected), static_cast<int>(actual));
 
 #include "CodeBlock.h"
 #include "Interpreter.h"
