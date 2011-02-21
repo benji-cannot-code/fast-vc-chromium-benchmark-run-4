c3f322c8808e3b5dc03110376d034ba0af6866cf
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 05:07:52 +0000

    2011-02-20  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Ojan Vafai.
    
            Fix various bugs related to handling of dir separators and real
            vs. fake filesystems that were causing test-webkitpy to fail
            tests on win32. These bugs have a variety of causes but are
            lumped together because they should be easy to review.
    
            https://bugs.webkit.org/show_bug.cgi?id=54700
    
            * Scripts/webkitpy/common/net/testoutput.py:
              Here we were using os.path.sep but not escaping it prior to
              using in a regexp, which  caused bad things on win32.
            * Scripts/webkitpy/common/net/testoutput_unittest.py:
              Same here.
            * Scripts/webkitpy/layout_tests/port/chromium_unittest.py:
              Here we were using the native filesystem instead of the mock
              filesystem, making some tests unnecessarily platform dependent.
              Also we weren't setting the default configuration for the
              TestChromiumWinPort.
            * Scripts/webkitpy/layout_tests/update_webgl_conformance_tests.py:
              Here we *shouldn't* use os.path.join(), just so the code becomes
              less platform dependent.
            * Scripts/webkitpy/style/checker.py:
              Here we should be using os.path.sep instead of "/"; the tests
              require the native filesystem.
            * Scripts/webkitpy/style/checkers/cpp_unittest.py:
              Here the underlying code is using os.path.sep, so we probably
              should as well.
            * Scripts/webkitpy/style/test_expectations_unittest.py:
              Here we should be using the filesystem hanging of the port
              object (which is the mocked filesystem) rather than the native
              one.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/testoutput_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/testoutput_unittest.py
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
 import re
+import sys
 import testoutput
 import unittest
 
@@ -61,10 +63,19 @@ class FakeTestOutput(testoutput.TestOutput):
 
 class TestOutputTest(unittest.TestCase):
     def _check_name(self, filename, expected_test_name):
+        # FIXME: should consider using MockFileSystem here so as to not
+        # have to worry about platform-specific path separators.
+        if sys.platform == 'win32':
+            filename = filename.replace('/', os.path.sep)
+            expected_test_name = expected_test_name.replace('/', os.path.sep)
         r = testoutput.TextTestOutput(None, FakeFile(filename))
         self.assertEquals(expected_test_name, r.name())
 
     def _check_platform(self, filename, expected_platform):
+        # FIXME: should consider using MockFileSystem here so as to not
+        # have to worry about platform-specific path separators.
+        if sys.platform == 'win32':
+            filename = filename.replace('/', os.path.sep)
         r = testoutput.TextTestOutput(None, FakeFile(filename))
         self.assertEquals(expected_platform, r.platform())
 
