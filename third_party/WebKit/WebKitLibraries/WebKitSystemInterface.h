4a51f506f5b2a2a15874029661045eda1f3f0ac3
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 22:02:31 +0000

    Update WebKitSystemInterface.h on Mac. The header that was in the tree didn't match the current WebKitSystemInterface.h.
    Rubber-stamped by Dan Bernstein.
    
    * WebKitSystemInterface.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*      
     WebKitSystemInterface.h
-    Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+    Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Apple Inc. All rights reserved.
 
     Public header file.
 */
@@ -87,9 +87,11 @@ void WKUnregisterUniqueIdForElement(id element);
 // Remote Accessibility API.
 void WKAXRegisterRemoteApp(void);
 void WKAXInitializeElementWithPresenterPid(id, pid_t);
-CFDataRef WKAXRemoteTokenForElement(id);
-id WKAXRemoteElementForToken(CFDataRef);
-void WKAXInitializeRemoteElementWithWindow(id remoteElement, id window);
+NSData *WKAXRemoteTokenForElement(id);
+id WKAXRemoteElementForToken(NSData *);
+void WKAXSetWindowForRemoteElement(id remoteWindow, id remoteElement);
+void WKAXRegisterRemoteProcess(bool registerProcess, pid_t);
+pid_t WKAXRemoteProcessIdentifier(id remoteElement);
 #endif
 
 void WKSetUpFontCache(void);
@@ -387,10 +389,37 @@ WKSandboxExtensionRef WKSandboxExtensionCreateFromSerializedFormat(const char* s
 
 typedef struct __WKScrollbarPainter *WKScrollbarPainterRef;
 WKScrollbarPainterRef WKMakeScrollbarPainter(int controlSize, bool isHorizontal);
+WKScrollbarPainterRef WKMakeScrollbarReplacementPainter(WKScrollbarPainterRef oldPainter, int newStyle, int controlSize, bool isHorizontal);
+void WKScrollbarPainterSetDelegate(WKScrollbarPainterRef, id scrollbarPainterDelegate);
 void WKScrollbarPainterPaint(WKScrollbarPainterRef, bool enabled, double value, CGFloat proportion, CGRect frameRect);
 int WKScrollbarThickness(int controlSize);
 int WKScrollbarMinimumThumbLength(WKScrollbarPainterRef);
 int WKScrollbarMinimumTotalLengthNeededForThumb(WKScrollbarPainterRef);
+CGFloat WKScrollbarPainterKnobAlpha(WKScrollbarPainterRef);
+void WKSetScrollbarPainterKnobAlpha(WKScrollbarPainterRef, CGFloat);
+CGFloat WKScrollbarPainterTrackAlpha(WKScrollbarPainterRef);
+void WKSetScrollbarPainterTrackAlpha(WKScrollbarPainterRef, CGFloat);
+bool WKScrollbarPainterIsHorizontal(WKScrollbarPainterRef);
+void WKScrollbarPainterSetOverlayState(WKScrollbarPainterRef, int overlayScrollerState);
+
+typedef struct __WKScrollbarPainterController *WKScrollbarPainterControllerRef;
+WKScrollbarPainterControllerRef WKMakeScrollbarPainterController(id painterControllerDelegate);
+void WKSetPainterForPainterController(WKScrollbarPainterControllerRef, WKScrollbarPainterRef, bool isHorizontal);
+WKScrollbarPainterRef WKVerticalScrollbarPainterForController(WKScrollbarPainterControllerRef);
+WKScrollbarPainterRef WKHorizontalScrollbarPainterForController(WKScrollbarPainterControllerRef);
+void WKSetScrollbarPainterControllerStyle(WKScrollbarPainterControllerRef, int newStyle);
+void WKContentAreaScrolled(WKScrollbarPainterControllerRef);
+void WKContentAreaWillPaint(WKScrollbarPainterControllerRef);
+void WKMouseEnteredContentArea(WKScrollbarPainterControllerRef);
+void WKMouseExitedContentArea(WKScrollbarPainterControllerRef);
+void WKMouseMovedInContentArea(WKScrollbarPainterControllerRef);
+void WKWillStartLiveResize(WKScrollbarPainterControllerRef);
+void WKContentAreaResized(WKScrollbarPainterControllerRef);
+void WKWillEndLiveResize(WKScrollbarPainterControllerRef);
+void WKContentAreaDidShow(WKScrollbarPainterControllerRef);
+void WKContentAreaDidHide(WKScrollbarPainterControllerRef);
+
+bool WKScrollbarPainterUsesOverlayScrollers(void);
 
 #endif
 
