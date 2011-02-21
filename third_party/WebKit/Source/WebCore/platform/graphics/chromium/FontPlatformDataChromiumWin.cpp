598824b7dc733a80dac7576572fe69295ae74294
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 03:28:22 +0000

    2011-02-20  Chun-Lung Huang  <alvincl.huang@gmail.com>
            Reviewed by Kent Tamura.
    
            On Chromium Windows, glyphs in vertical text tests are rotated 90
            degrees clockwise. https://bugs.webkit.org/show_bug.cgi?id=51450
    
            This platform dependent patch makes Chromium Windows show the
            vertical writing text correctly. Job was done by adding a prefix '@'
            in front of the font family name (Windows Only). No new tests added.
    
            * platform/graphics/chromium/FontCacheChromiumWin.cpp:
            (WebCore::FontCache::createFontPlatformData):
            * platform/graphics/chromium/FontPlatformDataChromiumWin.cpp:
            (WebCore::FontPlatformData::FontPlatformData):
            (WebCore::FontPlatformData::operator=):
            * platform/graphics/chromium/FontPlatformDataChromiumWin.h:
            (WebCore::FontPlatformData::orientation):
            * platform/graphics/skia/FontCustomPlatformData.cpp:
            (WebCore::FontCustomPlatformData::fontPlatformData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/FontPlatformDataChromiumWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/FontPlatformDataChromiumWin.cpp
@@ -47,6 +47,7 @@ FontPlatformData::FontPlatformData(WTF::HashTableDeletedValueType)
     , m_size(-1)
     , m_scriptCache(0)
     , m_scriptFontProperties(0)
+    , m_orientation(Horizontal)
 {
 }
 
@@ -55,14 +56,16 @@ FontPlatformData::FontPlatformData()
     , m_size(0)
     , m_scriptCache(0)
     , m_scriptFontProperties(0)
+    , m_orientation(Horizontal)
 {
 }
 
-FontPlatformData::FontPlatformData(HFONT font, float size)
+FontPlatformData::FontPlatformData(HFONT font, float size, FontOrientation orientation)
     : m_font(RefCountedHFONT::create(font))
     , m_size(size)
     , m_scriptCache(0)
     , m_scriptFontProperties(0)
+    , m_orientation(orientation)
 {
 }
 
@@ -72,6 +75,7 @@ FontPlatformData::FontPlatformData(float size, bool bold, bool oblique)
     , m_size(size)
     , m_scriptCache(0)
     , m_scriptFontProperties(0)
+    , m_orientation(Horizontal)
 {
 }
 
@@ -80,6 +84,7 @@ FontPlatformData::FontPlatformData(const FontPlatformData& data)
     , m_size(data.m_size)
     , m_scriptCache(0)
     , m_scriptFontProperties(0)
+    , m_orientation(Horizontal)
 {
 }
 
@@ -88,6 +93,7 @@ FontPlatformData& FontPlatformData::operator=(const FontPlatformData& data)
     if (this != &data) {
         m_font = data.m_font;
         m_size = data.m_size;
+        m_orientation = data.m_orientation;
 
         // The following fields will get re-computed if necessary.
         ScriptFreeCache(&m_scriptCache);
