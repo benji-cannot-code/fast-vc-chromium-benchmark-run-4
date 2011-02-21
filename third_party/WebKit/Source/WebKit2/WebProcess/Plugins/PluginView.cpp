2060c12941c950505bd6397eb8094ea67f456dde
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 00:09:23 +0000

    2011-02-20  Anders Carlsson  <andersca@apple.com>
            Reviewed by Maciej Stachowiak.
    
            Crash when a plug-in requests a javascript: url that destroys the plug-in
            https://bugs.webkit.org/show_bug.cgi?id=54837
            <rdar://problem/9005475>
    
            Add new plug-in test.
    
            * DumpRenderTree/DumpRenderTree.gypi:
            * DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj:
            * DumpRenderTree/TestNetscapePlugIn/PluginTest.cpp:
            (PluginTest::NPN_GetURL):
            * DumpRenderTree/TestNetscapePlugIn/PluginTest.h:
            * DumpRenderTree/TestNetscapePlugIn/Tests/GetURLWithJavaScriptURLDestroyingPlugin.cpp: Added.
            (GetURLWithJavaScriptURLDestroyingPlugin::GetURLWithJavaScriptURLDestroyingPlugin):
            (GetURLWithJavaScriptURLDestroyingPlugin::NPP_New):
            * DumpRenderTree/TestNetscapePlugIn/win/TestNetscapePlugin.vcproj:
            * DumpRenderTree/qt/TestNetscapePlugin/TestNetscapePlugin.pro:
    2011-02-20  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Maciej Stachowiak.
    
            Crash when a plug-in requests a javascript: url that destroys the plug-in
            https://bugs.webkit.org/show_bug.cgi?id=54837
            <rdar://problem/9005475>
    
            Add test.
    
            * plugins/get-url-with-javascript-destroying-plugin-expected.txt: Added.
            * plugins/get-url-with-javascript-destroying-plugin.html: Added.
    2011-02-20  Anders Carlsson  <andersca@apple.com>
    
            Reviewed by Maciej Stachowiak.
    
            Crash when a plug-in requests a javascript: url that destroys the plug-in
            https://bugs.webkit.org/show_bug.cgi?id=54837
            <rdar://problem/9005475>
    
            * WebProcess/Plugins/PluginProxy.cpp:
            (WebKit::PluginProxy::destroy):
            Null out m_pluginController.
    
            * WebProcess/Plugins/PluginView.cpp:
            (WebKit::PluginView::performJavaScriptURLRequest):
            Don't access the frame through m_pluginElement since it will be nulled out
            when the plug-in is destroyed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
@@ -739,7 +739,7 @@ void PluginView::performJavaScriptURLRequest(URLRequest* request)
     bool oldAllowPopups = frame->script()->allowPopupsFromPlugin();
     frame->script()->setAllowPopupsFromPlugin(request->allowPopups());
     
-    ScriptValue result = m_pluginElement->document()->frame()->script()->executeScript(jsString);
+    ScriptValue result = frame->script()->executeScript(jsString);
 
     frame->script()->setAllowPopupsFromPlugin(oldAllowPopups);
 
@@ -747,7 +747,7 @@ void PluginView::performJavaScriptURLRequest(URLRequest* request)
     if (!plugin->controller())
         return;
 
-    ScriptState* scriptState = m_pluginElement->document()->frame()->script()->globalObject(pluginWorld())->globalExec();
+    ScriptState* scriptState = frame->script()->globalObject(pluginWorld())->globalExec();
     String resultString;
     result.getString(scriptState, resultString);
   
--- third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gypi
+++ third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gypi
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'TestNetscapePlugIn/TestObject.h',
             'TestNetscapePlugIn/Tests/DocumentOpenInDestroyStream.cpp',
             'TestNetscapePlugIn/Tests/EvaluateJSAfterRemovingPluginElement.cpp',
+            'TestNetscapePlugIn/Tests/GetURLWithJavaScriptURLDestroyingPlugin.cpp',
             'TestNetscapePlugIn/Tests/GetUserAgentWithNullNPPFromNPPNew.cpp',
             'TestNetscapePlugIn/Tests/NPRuntimeObjectFromDestroyedPlugin.cpp',
             'TestNetscapePlugIn/Tests/NPRuntimeRemoveProperty.cpp',
