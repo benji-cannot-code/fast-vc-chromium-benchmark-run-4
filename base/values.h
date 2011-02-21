580e4c4f09a1e8f59c3e39ae83cac4a224340c25
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-21 16:35:04 +0000

    Add MoveToThread method to PrefMember to make it safe to read pref values from other threads.
    BUG=73385
    TEST=PrefMemberTest.MoveToThread
    
    Review URL: http://codereview.chromium.org/6524041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75555 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.h
+++ base/values.h
@@ -32,12 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string16.h"
 #include "build/build_config.h"
 
-class Value;
-class FundamentalValue;
-class StringValue;
 class BinaryValue;
 class DictionaryValue;
+class FilePath;
+class FundamentalValue;
 class ListValue;
+class StringValue;
+class Value;
 
 typedef std::vector<Value*> ValueVector;
 typedef std::map<std::string, Value*> ValueMap;
@@ -69,6 +70,7 @@ class Value {
   static FundamentalValue* CreateDoubleValue(double in_value);
   static StringValue* CreateStringValue(const std::string& in_value);
   static StringValue* CreateStringValue(const string16& in_value);
+  static StringValue* CreateFilePathValue(const FilePath& in_value);
 
   // This one can return NULL if the input isn't valid.  If the return value
   // is non-null, the new object has taken ownership of the buffer pointer.
@@ -93,6 +95,7 @@ class Value {
   virtual bool GetAsDouble(double* out_value) const;
   virtual bool GetAsString(std::string* out_value) const;
   virtual bool GetAsString(string16* out_value) const;
+  virtual bool GetAsFilePath(FilePath* out_value) const;
   virtual bool GetAsList(ListValue** out_value);
 
   // This creates a deep copy of the entire Value tree, and returns a pointer
@@ -160,6 +163,7 @@ class StringValue : public Value {
   // Subclassed methods
   virtual bool GetAsString(std::string* out_value) const;
   virtual bool GetAsString(string16* out_value) const;
+  virtual bool GetAsFilePath(FilePath* out_value) const;
   virtual StringValue* DeepCopy() const;
   virtual bool Equals(const Value* other) const;
 
