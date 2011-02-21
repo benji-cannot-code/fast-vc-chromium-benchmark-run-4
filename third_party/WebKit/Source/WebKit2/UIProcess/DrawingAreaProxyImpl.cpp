5bf1cac942f5d06410c0cef16141daf8e4c99170
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 23:21:06 +0000

    Add some assertions about the state IDs we receive from the web process
    Rubber-stamped by Anders Carlsson.
    
    * UIProcess/DrawingAreaProxyImpl.cpp:
    (WebKit::DrawingAreaProxyImpl::update):
    (WebKit::DrawingAreaProxyImpl::enterAcceleratedCompositingMode):
    (WebKit::DrawingAreaProxyImpl::exitAcceleratedCompositingMode):
    Assert that the state ID the web process sent us is no newer than our own state ID.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxyImpl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxyImpl.cpp
@@ -140,6 +140,7 @@ void DrawingAreaProxyImpl::detachCompositingContext()
 
 void DrawingAreaProxyImpl::update(uint64_t stateID, const UpdateInfo& updateInfo)
 {
+    ASSERT_ARG(stateID, stateID <= m_currentStateID);
     if (stateID < m_currentStateID)
         return;
 
@@ -183,6 +184,7 @@ void DrawingAreaProxyImpl::didUpdateState(uint64_t stateID, const UpdateInfo& up
 
 void DrawingAreaProxyImpl::enterAcceleratedCompositingMode(uint64_t stateID, const LayerTreeContext& layerTreeContext)
 {
+    ASSERT_ARG(stateID, stateID <= m_currentStateID);
     if (stateID < m_currentStateID)
         return;
 
@@ -191,6 +193,7 @@ void DrawingAreaProxyImpl::enterAcceleratedCompositingMode(uint64_t stateID, con
 
 void DrawingAreaProxyImpl::exitAcceleratedCompositingMode(uint64_t stateID, const UpdateInfo& updateInfo)
 {
+    ASSERT_ARG(stateID, stateID <= m_currentStateID);
     if (stateID < m_currentStateID)
         return;
 
