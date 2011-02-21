b935f76e1654689746d04c604180470b764513fb
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 23:20:35 +0000

    Rename DrawingArea[Proxy]Impl's "sequence number" concept to "state ID".
    As explained in r79251, DrawingAreaProxyImpl has some state that, when it changes, causes
    all operations performed before that state change to become invalid. Currently, this state
    consists of a single piece of data: the view's size. Eventually it will encompass more data
    (e.g., the backing store we're drawing into; when we start throwing away the backing store
    to save memory, our state will have changed, and any operations that were intended for the
    old backing store will have become invalid). r79251 effectively transformed
    DrawingArea[Proxy]Impl's "sequence number," which incremented every time DrawingAreaImpl
    sent DrawingAreaProxyImpl a message, to a "state ID," which only increments when the view's
    size changes.
    
    This patch is just a set of simple renames to reflect the transformation that r79251
    effected. It should not introduce any changes in behavior. The renames are:
      - Messages::DrawingArea::SetSize -> UpdateState
      - Messages::DrawingAreaProxy::DidSetSize -> DidUpdateState
      - DrawingAreaProxyImpl::m_lastDidSetSizeSequenceNumber -> m_currentStateID
      - DrawingAreaProxyImpl::m_isWaitingForDidSetSize -> m_isWaitingForDidUpdateState
      - DrawingAreaProxyImpl::waitForAndDispatchDidSetSize -> waitForAndDispatchDidUpdateState
      - DrawingAreaImpl::m_inSetSize -> m_inUpdateState
      - generateSequenceNumber -> generateStateID
      - sequenceNumber -> stateID
    
    Fixes <http://webkit.org/b/54911> DrawingArea[Proxy]Impl's "sequence number" concept should
    be renamed to "state ID"
    
    Reviewed by Anders Carlsson.
    
    * UIProcess/DrawingAreaProxy.h:
    * UIProcess/DrawingAreaProxy.messages.in:
    * UIProcess/DrawingAreaProxyImpl.cpp:
    * UIProcess/DrawingAreaProxyImpl.h:
    * WebProcess/WebPage/DrawingArea.h:
    * WebProcess/WebPage/DrawingArea.messages.in:
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    * WebProcess/WebPage/DrawingAreaImpl.h:
    Performed the renames described above.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -59,8 +59,8 @@ DrawingAreaImpl::~DrawingAreaImpl()
 
 DrawingAreaImpl::DrawingAreaImpl(WebPage* webPage, const WebPageCreationParameters& parameters)
     : DrawingArea(DrawingAreaInfo::Impl, parameters.drawingAreaInfo.identifier, webPage)
-    , m_sequenceNumber(0)
-    , m_inSetSize(false)
+    , m_stateID(0)
+    , m_inUpdateState(false)
     , m_isWaitingForDidUpdate(false)
     , m_isPaintingSuspended(!parameters.isVisible)
     , m_alwaysUseCompositing(false)
@@ -190,7 +190,7 @@ void DrawingAreaImpl::layerHostDidFlushLayers()
     if (!m_layerTreeHost)
         return;
 
-    m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(m_sequenceNumber, m_layerTreeHost->layerTreeContext()));
+    m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(m_stateID, m_layerTreeHost->layerTreeContext()));
 }
 
 void DrawingAreaImpl::attachCompositingContext()
@@ -220,7 +220,7 @@ void DrawingAreaImpl::setRootCompositingLayer(GraphicsLayer* graphicsLayer)
                 // compositing code via display() and layout.
                 // If we're leaving compositing mode because of a setSize, it is safe to
                 // exit accelerated compositing mode right away.
-                if (m_inSetSize)
+                if (m_inUpdateState)
                     exitAcceleratedCompositingMode();
                 else
                     exitAcceleratedCompositingModeSoon();
@@ -244,13 +244,13 @@ void DrawingAreaImpl::didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageID
 {
 }
 
-void DrawingAreaImpl::setSize(uint64_t sequenceNumber, const WebCore::IntSize& size, const WebCore::IntSize& scrollOffset)
+void DrawingAreaImpl::updateState(uint64_t stateID, const WebCore::IntSize& size, const WebCore::IntSize& scrollOffset)
 {
-    ASSERT(!m_inSetSize);
-    m_inSetSize = true;
+    ASSERT(!m_inUpdateState);
+    m_inUpdateState = true;
 
-    ASSERT_ARG(sequenceNumber, sequenceNumber > m_sequenceNumber);
-    m_sequenceNumber = sequenceNumber;
+    ASSERT_ARG(stateID, stateID > m_stateID);
+    m_stateID = stateID;
 
     // Set this to false since we're about to call display().
     m_isWaitingForDidUpdate = false;
@@ -277,9 +277,9 @@ void DrawingAreaImpl::setSize(uint64_t sequenceNumber, const WebCore::IntSize& s
         ASSERT(!m_layerTreeHost);
     }
 
-    m_webPage->send(Messages::DrawingAreaProxy::DidSetSize(m_sequenceNumber, updateInfo, layerTreeContext));
+    m_webPage->send(Messages::DrawingAreaProxy::DidUpdateState(m_stateID, updateInfo, layerTreeContext));
 
-    m_inSetSize = false;
+    m_inUpdateState = false;
 }
 
 void DrawingAreaImpl::didUpdate()
@@ -320,7 +320,7 @@ void DrawingAreaImpl::enterAcceleratedCompositingMode(GraphicsLayer* graphicsLay
     ASSERT(!m_layerTreeHost);
 
     m_layerTreeHost = LayerTreeHost::create(m_webPage);
-    if (!m_inSetSize)
+    if (!m_inUpdateState)
         m_layerTreeHost->setShouldNotifyAfterNextScheduledLayerFlush();
 
     m_layerTreeHost->setRootCompositingLayer(graphicsLayer);
@@ -345,7 +345,7 @@ void DrawingAreaImpl::exitAcceleratedCompositingMode()
     m_layerTreeHost->invalidate();
     m_layerTreeHost = nullptr;
 
-    if (m_inSetSize)
+    if (m_inUpdateState)
         return;
 
     UpdateInfo updateInfo;
@@ -358,8 +358,8 @@ void DrawingAreaImpl::exitAcceleratedCompositingMode()
 
     // Send along a complete update of the page so we can paint the contents right after we exit the
     // accelerated compositing mode, eliminiating flicker.
-    if (!m_inSetSize)
-        m_webPage->send(Messages::DrawingAreaProxy::ExitAcceleratedCompositingMode(m_sequenceNumber, updateInfo));
+    if (!m_inUpdateState)
+        m_webPage->send(Messages::DrawingAreaProxy::ExitAcceleratedCompositingMode(m_stateID, updateInfo));
 }
 
 void DrawingAreaImpl::exitAcceleratedCompositingModeSoon()
@@ -407,7 +407,7 @@ void DrawingAreaImpl::display()
         return;
     }
 
-    m_webPage->send(Messages::DrawingAreaProxy::Update(m_sequenceNumber, updateInfo));
+    m_webPage->send(Messages::DrawingAreaProxy::Update(m_stateID, updateInfo));
     m_isWaitingForDidUpdate = true;
 }
 
