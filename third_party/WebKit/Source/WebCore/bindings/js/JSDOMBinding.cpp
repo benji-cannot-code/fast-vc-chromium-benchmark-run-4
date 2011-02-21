b96fd4b60fe5a22c19be756030e9e719f7084234
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-21 05:37:29 +0000

    Build fix (remove includes).
    * bindings/js/JSDOMBinding.cpp:
    * bindings/js/JSDOMWindowCustom.cpp:
    * bindings/js/JSHistoryCustom.cpp:
    * bindings/js/JSLocationCustom.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.cpp
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime/DateInstance.h>
 #include <runtime/Error.h>
 #include <runtime/JSFunction.h>
-#include <runtime/PrototypeFunction.h>
 #include <wtf/MathExtras.h>
 #include <wtf/StdLibExtras.h>
 
--- third_party/WebKit/Source/WebCore/bindings/js/JSLocationCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSLocationCustom.cpp
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Location.h"
 #include <runtime/JSFunction.h>
-#include <runtime/PrototypeFunction.h>
 
 using namespace JSC;
 
