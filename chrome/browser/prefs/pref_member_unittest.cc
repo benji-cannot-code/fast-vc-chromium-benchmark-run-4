2dc3567ef94cf2d94e4cf8fe28231cc1cc9468a9
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-21 17:10:20 +0000

    Fix clang build.
    BUG=73385
    TEST=none
    
    Review URL: http://codereview.chromium.org/6541062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75557 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/pref_member_unittest.cc
+++ chrome/browser/prefs/pref_member_unittest.cc
@@ -247,10 +247,10 @@ TEST(PrefMemberTest, MoveToThread) {
   callback->Init(kBoolPref, &prefs);
 
   ASSERT_TRUE(callback->FetchValue());
-  EXPECT_EQ(false, callback->value());
+  EXPECT_FALSE(callback->value());
 
   prefs.SetBoolean(kBoolPref, true);
 
   ASSERT_TRUE(callback->FetchValue());
-  EXPECT_EQ(true, callback->value());
+  EXPECT_TRUE(callback->value());
 }
