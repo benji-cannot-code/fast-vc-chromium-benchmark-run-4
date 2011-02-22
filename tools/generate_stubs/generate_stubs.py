c4512f3f3b47dc57927dd2efcfb1942f8fb91299
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-22 13:51:33 +0000

    Speed up encoding by using active map
    Using active map can greatly reduce the amount of macro blocks need to be
    encoded by vp8. This brings average encoding time from 35ms per frame to about
    8ms on the tested system.
    
    However this change depends on an updated version of libvpx.
    
    BUG=None
    TEST=chromoting still works & no visual problems
    
    Review URL: http://codereview.chromium.org/6518011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75586 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/generate_stubs/generate_stubs.py
+++ tools/generate_stubs/generate_stubs.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
 #
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -16,6 +16,19 @@ found via dlsym.
 This script takes a set of files, where each file is a list of C-style
 signatures (one signature per line).  The output is either a windows def file,
 or a header + implementation file of stubs suitable for use in a posix system.
+
+This script also handles varidiac functions, e.g.
+void printf(const char* s, ...);
+
+TODO(hclam): Fix the situation for varidiac functions.
+Stub for the above function will be generated and inside the stub function it
+is translated to:
+void printf(const char* s, ...) {
+  printf_ptr(s, (void*)arg1);
+}
+
+Only one argument from the varidiac arguments is used and it will be used as
+type void*.
 """
 
 __author__ = 'ajwong@chromium.org (Albert J. Wong)'
@@ -97,7 +110,7 @@ VARIADIC_STUB_FUNCTION_DEFINITION = (
 %(return_type)s %(name)s(%(params)s) {
   va_list args___;
   va_start(args___, %(last_named_arg)s);
-  %(return_type)s ret___ = %(name)s_ptr(%(arg_list)s, args___);
+  %(return_type)s ret___ = %(name)s_ptr(%(arg_list)s, va_arg(args___, void*));
   va_end(args___);
   return ret___;
 }""")
@@ -117,7 +130,7 @@ VOID_VARIADIC_STUB_FUNCTION_DEFINITION = (
 void %(name)s(%(params)s) {
   va_list args___;
   va_start(args___, %(last_named_arg)s);
-  %(name)s_ptr(%(arg_list)s, args___);
+  %(name)s_ptr(%(arg_list)s, va_arg(args___, void*));
   va_end(args___);
 }""")
 
