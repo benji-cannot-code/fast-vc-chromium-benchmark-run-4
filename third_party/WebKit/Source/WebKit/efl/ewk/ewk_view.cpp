59acd32d30fcb88099f2fb01635c58e170bac74b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-22 07:21:09 +0000

    2011-02-21  Lukasz Slachciak  <l.slachciak@samsung.com>
            Reviewed by Antonio Gomes.
    
            [EFL] Fixed missing spatial navigation setting initialization. Doxygen comments added.
            https://bugs.webkit.org/show_bug.cgi?id=54806
    
            * ewk/ewk_view.cpp:
            (_ewk_view_priv_new): spatial navigation setting initialization added
    2011-02-21  Lukasz Slachciak  <l.slachciak@samsung.com>
    
            Reviewed by Antonio Gomes.
    
            [EFL] Restored spatial navigation option in EWebLauncher since #18662 bug is closed
            https://bugs.webkit.org/show_bug.cgi?id=54806
    
            * EWebLauncher/main.c:
            (on_key_down): calling ewk_view_setting_spatial_navigation_set when "F12" pressed added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2009-2010 ProFUSION embedded systems
-    Copyright (C) 2009-2010 Samsung Electronics
+    Copyright (C) 2009-2011 Samsung Electronics
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -619,6 +619,7 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* sd)
     priv->settings.resizable_textareas = priv->page_settings->textAreasAreResizable();
     priv->settings.private_browsing = priv->page_settings->privateBrowsingEnabled();
     priv->settings.caret_browsing = priv->page_settings->caretBrowsingEnabled();
+    priv->settings.spatial_navigation = priv->page_settings->isSpatialNavigationEnabled();
     priv->settings.local_storage = priv->page_settings->localStorageEnabled();
     priv->settings.offline_app_cache = true; // XXX no function to read setting; this keeps the original setting
     priv->settings.page_cache = priv->page_settings->usesPageCache();
@@ -2919,6 +2920,12 @@ Eina_Bool ewk_view_setting_font_sans_serif_set(Evas_Object* o, const char* famil
     return EINA_TRUE;
 }
 
+/**
+ * Gets if the spatial naviagtion is enabled.
+ *
+ * @param o view object to get spatial navigation setting.
+ * @return @c EINA_TRUE if spatial navigation is enabled, @c EINA_FALSE if not or on errors.
+ */
 Eina_Bool ewk_view_setting_spatial_navigation_get(Evas_Object* o)
 {
     EWK_VIEW_SD_GET_OR_RETURN(o, sd, EINA_FALSE);
@@ -2926,6 +2933,12 @@ Eina_Bool ewk_view_setting_spatial_navigation_get(Evas_Object* o)
     return priv->settings.spatial_navigation;
 }
 
+/**
+ * Sets the spatial navigation.
+ *
+ * @param o view object to set spatial navigation setting.
+ * @return @c EINA_TRUE on success and @c EINA_FALSE on failure
+ */
 Eina_Bool ewk_view_setting_spatial_navigation_set(Evas_Object* o, Eina_Bool enable)
 {
     EWK_VIEW_SD_GET_OR_RETURN(o, sd, EINA_FALSE);
