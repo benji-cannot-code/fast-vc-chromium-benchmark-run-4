567173c14645fc414a3347ef80143a025011e369
piman@google.com <piman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-22 22:51:02 +0000

    Fix crashes when the ExtensionService isn't present (e.g. incognito, chrome os before login). This is a workaround until http://code.google.com/p/chromium/issues/detail?id=73768 is fixed.
    BUG=73658
    TEST=with chromeos=1 build, go to any site and check no crash.
    
    Review URL: http://codereview.chromium.org/6544023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1328,7 +1328,10 @@ void Browser::OpenCurrentURL() {
       TabStripModel::ADD_FORCE_INDEX | TabStripModel::ADD_INHERIT_OPENER;
   browser::Navigate(&params);
 
-  if (profile_->GetExtensionService()->IsInstalledApp(url)) {
+  // TODO: the ExtensionService should never be NULL, but in some cases it is,
+  // see bug 73768. After it is resolved, the explicit test can go away.
+  ExtensionService* service = profile_->GetExtensionService();
+  if (service && service->IsInstalledApp(url)) {
     UMA_HISTOGRAM_ENUMERATION(extension_misc::kAppLaunchHistogram,
                               extension_misc::APP_LAUNCH_OMNIBOX_LOCATION,
                               extension_misc::APP_LAUNCH_BUCKET_BOUNDARY);
@@ -3518,7 +3521,10 @@ void Browser::CommitInstant(TabContentsWrapper* preview_contents) {
   instant_unload_handler_->RunUnloadListenersOrDestroy(old_contents, index);
 
   GURL url = preview_contents->tab_contents()->GetURL();
-  if (profile_->GetExtensionService()->IsInstalledApp(url)) {
+  // TODO: the ExtensionService should never be NULL, but in some cases it is,
+  // see bug 73768. After it is resolved, the explicit test can go away.
+  ExtensionService* service = profile_->GetExtensionService();
+  if (service && service->IsInstalledApp(url)) {
     UMA_HISTOGRAM_ENUMERATION(extension_misc::kAppLaunchHistogram,
                               extension_misc::APP_LAUNCH_OMNIBOX_INSTANT,
                               extension_misc::APP_LAUNCH_BUCKET_BOUNDARY);
