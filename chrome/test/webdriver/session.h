1231f16e3535bd9663aaeb7f2fb0b77b7799c6a5
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-22 20:48:25 +0000

    Return the version of Chrome when getting the session capabilities instead of the version of ChromeDriver. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/6485027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75630 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/session.h
+++ chrome/test/webdriver/session.h
@@ -109,6 +109,9 @@ class Session {
   // session.
   bool CloseWindow();
 
+  // Gets the version of the running browser.
+  std::string GetVersion();
+
   inline const std::string& id() const { return id_; }
 
   inline int implicit_wait() const { return implicit_wait_; }
