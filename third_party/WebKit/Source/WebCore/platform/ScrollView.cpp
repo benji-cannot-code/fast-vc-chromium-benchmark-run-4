9f8769ea8a750ffe758aa56abf6ec0fdb133f8f1
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-22 21:01:43 +0000

    2011-01-17  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] fast/events/scroll-after-click-on-tab-index has been failing on the bots
            https://bugs.webkit.org/show_bug.cgi?id=49177
    
            * platform/gtk/Skipped: Unskip passing test.
    2011-01-17  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] fast/events/scroll-after-click-on-tab-index has been failing on the bots
            https://bugs.webkit.org/show_bug.cgi?id=49177
    
            * platform/ScrollView.cpp: Remove the GTK+ guards around the implementation
            of platformAddChild and platformRemoveChild. This code can be shared.
            * platform/gtk/MainFrameScrollbarGtk.cpp:
            (MainFrameScrollbarGtk::attachAdjustment): Prevent re-attaching an already attached
            adjustment. Connect the adjustment value-changed signal handler after resetting the
            adjustment. This prevents the rest from stomping on pre-existing WebCore values.
            (MainFrameScrollbarGtk::gtkValueChanged): If the scrollbar is no longer attached to
            a scrollview do not listing for value changes. These scrollbars are defunct.
            * platform/gtk/ScrollViewGtk.cpp: Remove duplicated empty methods.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
@@ -1175,7 +1175,7 @@ void ScrollView::platformDestroy()
 
 #endif
 
-#if !PLATFORM(WX) && !PLATFORM(GTK) && !PLATFORM(QT) && !PLATFORM(MAC)
+#if !PLATFORM(WX) && !PLATFORM(QT) && !PLATFORM(MAC)
 
 void ScrollView::platformAddChild(Widget*)
 {
