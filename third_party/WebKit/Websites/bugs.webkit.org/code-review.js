be07e4c4fcb97c8343aebde7c529dc3f07a00aed
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-22 03:38:28 +0000

    2011-02-20  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Adam Barth.
    
            [codereviewtool] avoid overriding ctrl/cmd + r
            https://bugs.webkit.org/show_bug.cgi?id=54852
    
            * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79280 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -1503,6 +1503,10 @@ var CODE_REVIEW_UNITTEST;
     if (e.target.nodeName == 'TEXTAREA')
       return;
 
+    // Don't want to override browser shortcuts like ctrl+r.
+    if (e.metaKey || e.ctrlKey)
+      return;
+
     if (handleModifyContextKey(e))
       return;
 
