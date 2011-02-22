b058c88255befd376333c2bbbaf1de23698d00f2
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-22 23:23:42 +0000

    2011-02-22  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Oliver Hunt.
    
            Manage MarkedBlocks in a linked list instead of a vector, so arbitrary removal is O(1)
            https://bugs.webkit.org/show_bug.cgi?id=54999
            
            SunSpider reports no change.
    
            * GNUmakefile.am:
            * JavaScriptCore.gypi:
            * JavaScriptCore.vcproj/WTF/WTF.vcproj:
            * JavaScriptCore.xcodeproj/project.pbxproj: So many build systems, so little time.
            * wtf/CMakeLists.txt:
    
            * runtime/MarkedBlock.cpp:
            (JSC::MarkedBlock::MarkedBlock):
            * runtime/MarkedBlock.h:
            (JSC::MarkedBlock::setPrev):
            (JSC::MarkedBlock::setNext):
            (JSC::MarkedBlock::prev):
            (JSC::MarkedBlock::next): Added linked list data members and accessors.
    
            * runtime/MarkedSpace.cpp:
            (JSC::MarkedSpace::destroy):
            (JSC::MarkedSpace::allocateBlock): Stop using vector, since it doesn't exist anymore.
    
            (JSC::MarkedSpace::freeBlocks): New helper function for updating relevant
            data structures when freeing blocks.
    
            (JSC::MarkedSpace::allocate): Updated for nextBlock being a pointer and
            not a vector index.
    
            (JSC::MarkedSpace::shrink): Construct a temporary list of empties and
            then free them, to avoid modifying our hash table while iterating it.
            This wasn't a concern before because we were using indirect array
            indexing, not direct pointer indexing.
    
            (JSC::MarkedSpace::reset): Updated for nextBlock being a pointer and
            not a vector index.
    
            * runtime/MarkedSpace.h:
            (JSC::CollectorHeap::CollectorHeap): Changed data type from vector to linked list.
    
            * wtf/DoublyLinkedList.h: Added. New linked list class.
            (WTF::::DoublyLinkedList):
            (WTF::::isEmpty):
            (WTF::::head):
            (WTF::::append):
            (WTF::::remove):
    2011-02-22  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Manage MarkedBlocks in a linked list instead of a vector, so arbitrary removal is O(1)
            https://bugs.webkit.org/show_bug.cgi?id=54999
            
            New WTF header.
    
            * ForwardingHeaders/wtf/DoublyLinkedList.h: Copied from ForwardingHeaders/wtf/FixedArray.h.
    2011-02-22  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Oliver Hunt.
    
            Manage MarkedBlocks in a linked list instead of a vector, so arbitrary removal is O(1)
            https://bugs.webkit.org/show_bug.cgi?id=54999
            
            New WTF header.
    
            * ForwardingHeaders/wtf/DoublyLinkedList.h: Copied from ForwardingHeaders/wtf/FixedArray.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/MarkedSpace.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/MarkedSpace.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MarkedBlock.h"
 #include "PageAllocationAligned.h"
 #include <wtf/Bitmap.h>
+#include <wtf/DoublyLinkedList.h>
 #include <wtf/FixedArray.h>
 #include <wtf/HashSet.h>
 #include <wtf/Noncopyable.h>
@@ -50,15 +51,10 @@ namespace JSC {
             , nextAtom(0)
         {
         }
-        
-        MarkedBlock* collectorBlock(size_t index) const
-        {
-            return blocks[index];
-        }
 
-        size_t nextBlock;
+        MarkedBlock* nextBlock;
         size_t nextAtom;
-        Vector<MarkedBlock*> blocks;
+        DoublyLinkedList<MarkedBlock> blockList;
     };
 
     class MarkedSpace {
@@ -101,7 +97,7 @@ namespace JSC {
         typedef HashSet<MarkedBlock*>::iterator BlockIterator;
 
         NEVER_INLINE MarkedBlock* allocateBlock();
-        NEVER_INLINE void freeBlock(size_t);
+        NEVER_INLINE void freeBlocks(DoublyLinkedList<MarkedBlock>&);
 
         void clearMarks(MarkedBlock*);
 
