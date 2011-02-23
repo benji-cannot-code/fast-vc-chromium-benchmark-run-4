a95d44664f6ef0e2501291855da830582a6e949a
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 21:51:20 +0000

    Use named automation interface to control the primary chrome to run pyauto-based chrome tests on ChromeOS.
    This CL includes:
      - enable_testing.py -- sends dbus call to session_manager to enable testing interface
      - pyauto.py -- integration of named automation interface with PyAuto
      - desktopui_PyAutoFunctionalTests.py -- setup to use named automation interface.  Uses a suid-root binary (as discussed with cmasone) to dbus mesg to session_manager
      - PYAUTO_TESTS -- a new suite called "PRIMARY_CHROME" which currently consists of a basic hello world test that controls the primary chrome
      - chromeos_basic.py -- a basic test
    
    All plumbing necessary to integrate the named automation interface with autotest and pyauto is included.
    It's designed such that it's easy to work directly on a chromeos device while adding/editing a pyauto test, without having to run via autotest every time.  Also, the existing pyauto tests (about 200 of them) will begin controlling the primary chrome.
    
    Can be run with run_remote_tests.sh as:
      $ ./run_remote_tests.sh --remote <IP> desktopui_PyAutoFunctionalTests
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6410134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75797 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/functional/chromeos_basic.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import pyauto_functional
+import pyauto
+
+
+class ChromeosBasic(pyauto.PyUITest):
+  """Basic tests for ChromeOS.
+
+  Requires ChromeOS to be logged in.
+  """
+
+  def testAppendTabs(self):
+    """Basic test for primary chrome on ChromeOS (named testing interface)."""
+    self.AppendTab(pyauto.GURL('about:version'))
+    self.assertEqual(self.GetTabCount(), 2, msg='Expected 2 tabs')
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
