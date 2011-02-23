726dcf5b3c0c57ce3528c8bbd2a67b8469d28bf0
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-23 11:58:52 +0000

    2011-02-23 Patrick Gansterer <paroga@webkit.org>
            Reviewed by Andreas Kling.
    
            [WINCE] Get rid of TemporaryLinkStubs.cpp
            https://bugs.webkit.org/show_bug.cgi?id=54825
    
            * CMakeListsWinCE.txt:
            * platform/network/win/CookieStorageWin.cpp: Copied from platform/wince/TemporaryLinkStubs.cpp.
            * platform/wince/TemporaryLinkStubs.cpp: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/win/CookieStorageWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/win/CookieStorageWin.cpp
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-
 #include "CookieStorage.h"
+
 #include "NotImplemented.h"
 
 namespace WebCore {
