433716e59e683d5e903edb7001c520cb306ae52f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-23 09:29:33 +0000

    2011-02-23  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Ojan Vafai.
    
            nrwt: turn on multiprocessing on chromium_linux, clean up unit tests for worker model defaults
            https://bugs.webkit.org/show_bug.cgi?id=55016
    
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/chromium_linux.py:
            * Scripts/webkitpy/layout_tests/port/chromium_linux_unittest.py: Added.
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
            * Scripts/webkitpy/layout_tests/port/chromium_mac_unittest.py:
            * Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
            * Scripts/webkitpy/layout_tests/port/mac_unittest.py:
            * Scripts/webkitpy/layout_tests/port/mock_drt_unittest.py:
            * Scripts/webkitpy/layout_tests/port/port_testcase.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -39,6 +39,12 @@ import shlex
 import sys
 import time
 
+# Handle Python < 2.6 where multiprocessing isn't available.
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
 import apache_http_server
 import config as port_config
 import http_lock
@@ -125,6 +131,7 @@ class Port(object):
         if not hasattr(self._options, 'configuration') or self._options.configuration is None:
             self._options.configuration = self.default_configuration()
         self._test_configuration = None
+        self._multiprocessing_is_available = (multiprocessing is not None)
 
     def default_child_processes(self):
         """Return the number of DumpRenderTree instances to use for this
--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/port_testcase.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/port_testcase.py
@@ -32,6 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import sys
 import unittest
 
+# Handle Python < 2.6 where multiprocessing isn't available.
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
 from webkitpy.tool import mocktool
 mock_options = mocktool.MockOptions(results_directory='layout-test-results',
                                     use_apache=True,
@@ -59,6 +65,16 @@ class PortTestCase(unittest.TestCase):
         if hasattr(options, "results_directory"):
             port._options.results_directory = port.results_directory()
         return port
+
+    def test_default_worker_model(self):
+        port = self.make_port()
+        if not port:
+            return
+
+        if multiprocessing:
+            self.assertEqual(port.default_worker_model(), 'processes')
+        else:
+            self.assertEqual(port.default_worker_model(), 'old-threads')
 
     def test_driver_cmd_line(self):
         port = self.make_port()
