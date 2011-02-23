1d5e45f3c79899d4310d6667cdcbf63840f5786c
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 04:17:04 +0000

    Use ResourceBundle::BaseFont for GTK tooltips.
    If no font is specified, the font used can differ from the base font.  This
    happens when we set the UI font manually using IDS_UI_FONT_FAMILY_CROS.
    
    BUG=chromium-os:7737
    TEST=manually tested on netbook and Linux
    
    Review URL: http://codereview.chromium.org/6476023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75719 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/tooltip_manager_gtk.h
+++ views/widget/tooltip_manager_gtk.h
@@ -43,7 +43,7 @@ class TooltipManagerGtk : public TooltipManager {
   // The view supplied to the last invocation of ShowKeyboardTooltip.
   View* keyboard_view_;
 
-  // Custimized tooltip window.
+  // Customized tooltip window.
   TooltipWindowGtk tooltip_window_;
 
   DISALLOW_COPY_AND_ASSIGN(TooltipManagerGtk);
