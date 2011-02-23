8f42afe3e4c77f1c43247dc144a5f976963bdf2d
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-23 09:25:00 +0000

    2011-02-23  Tony Chang  <tony@chromium.org>
            Reviewed by Kent Tamura.
    
            [chromium] DumpRenderTree --check-layout-test-sys-deps leaks 4 temp dirs
            https://bugs.webkit.org/show_bug.cgi?id=55004
    
            * DumpRenderTree/chromium/DumpRenderTree.cpp:
            (WebKitSupportTestEnvironment::WebKitSupportTestEnvironment):
            (WebKitSupportTestEnvironment::~WebKitSupportTestEnvironment):
            (main): Use a C++ object to ensure that TearDownTestEnvironment
                gets called when --check-layout-test-sys-deps is called
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79422 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/DumpRenderTree.cpp
@@ -61,6 +61,18 @@ static const char optionStressDeopt[] = "--stress-deopt";
 static const char optionJavaScriptFlags[] = "--js-flags=";
 static const char optionNoTimeout[] = "--no-timeout";
 
+class WebKitSupportTestEnvironment {
+public:
+    WebKitSupportTestEnvironment()
+    {
+        webkit_support::SetUpTestEnvironment();
+    }
+    ~WebKitSupportTestEnvironment()
+    {
+        webkit_support::TearDownTestEnvironment();
+    }
+};
+
 static void runTest(TestShell& shell, TestParams& params, const string& testName, bool testShellMode)
 {
     int oldTimeoutMsec = shell.layoutTestTimeout();
@@ -110,7 +122,7 @@ static void runTest(TestShell& shell, TestParams& params, const string& testName
 
 int main(int argc, char* argv[])
 {
-    webkit_support::SetUpTestEnvironment();
+    WebKitSupportTestEnvironment testEnvironment;
     platformInit(&argc, &argv);
 
     TestParams params;
@@ -145,7 +157,7 @@ int main(int argc, char* argv[])
         else if (argument == optionStartupDialog)
             startupDialog = true;
         else if (argument == optionCheckLayoutTestSystemDeps)
-            exit(checkLayoutTestSystemDependencies() ? EXIT_SUCCESS : EXIT_FAILURE);
+            return checkLayoutTestSystemDependencies() ? EXIT_SUCCESS : EXIT_FAILURE;
         else if (argument == optionHardwareAcceleratedGL)
             hardwareAcceleratedGL = true;
         else if (argument == optionEnableAcceleratedCompositing)
@@ -219,6 +231,5 @@ int main(int argc, char* argv[])
         shell.resetTestController();
     }
 
-    webkit_support::TearDownTestEnvironment();
     return EXIT_SUCCESS;
 }
