5dc7b9d410996725438918ff9297f32f0b4d2140
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 22:37:56 +0000

    Pepper: Increment the version numbers for PPB_{FileRef,FileSystem}_Dev.
    The PP_FileSystemType_Dev enum was changed in r75566, implicitly changing these
    interfaces (whose revision numbers weren't changed).
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6579018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75806 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_file_system_dev.h
+++ ppapi/c/dev/ppb_file_system_dev.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 struct PP_CompletionCallback;
 
-#define PPB_FILESYSTEM_DEV_INTERFACE "PPB_FileSystem(Dev);0.4"
+#define PPB_FILESYSTEM_DEV_INTERFACE "PPB_FileSystem(Dev);0.5"
 
 struct PPB_FileSystem_Dev {
   /** Creates a filesystem object of the given type. */
