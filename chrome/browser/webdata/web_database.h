f4248e91b6fa190f9c321bd4d23c5c128f4683c5
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 05:57:38 +0000

    Autofill extend profiles to include multi-valued fields.
    Changes to the underlying |autofill_profiles| schema.  These changes split out name, email, and phone information into separate relationaly-tied tables so that we can support multi-valued fields.  New tables are introduced: |autofill_profile_names|, |autofill_profile_emails|, and |autofill_profile_phones|.  Also, the |label| fields have been removed from both |credit_cards| and |autofill_profiles|.
    
    BUG=65625
    TEST=WebDatabaseMigrationTest.*:WebDatabaseTest.*
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=75710
    
    Review URL: http://codereview.chromium.org/6546034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_database.h
+++ chrome/browser/webdata/web_database.h
@@ -339,6 +339,9 @@ class WebDatabase {
   bool InitAutofillTable();
   bool InitAutofillDatesTable();
   bool InitAutoFillProfilesTable();
+  bool InitAutoFillProfileNamesTable();
+  bool InitAutoFillProfileEmailsTable();
+  bool InitAutoFillProfilePhonesTable();
   bool InitCreditCardsTable();
   bool InitTokenServiceTable();
   bool InitWebAppIconsTable();
