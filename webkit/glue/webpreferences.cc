5ece07adea852f03bff86fd813ffb2ece98a9437
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 02:45:51 +0000

    Initialize the minimum timer interval upon RenderView construction, not during WebPreferences application, and base the interval on the initial hidden state of the view.
    This fixes a bug introduced late in the development of the minimum
    timer interval for background tabs, and also will fix
    http://code.google.com/p/chromium/issues/detail?id=73726 once the
    minimum timer interval is increased again.
    
    BUG=66078
    TEST=manual tests
    
    Review URL: http://codereview.chromium.org/6546080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75700 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURL.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
-#include "webkit/glue/webkit_constants.h"
 #include "webkit/glue/webkit_glue.h"
 
 using WebKit::WebRuntimeFeatures;
@@ -207,7 +206,4 @@ void WebPreferences::Apply(WebView* web_view) const {
 
   settings->setInteractiveFormValidationEnabled(
       interactive_form_validation_enabled);
-
-  // Tabs start out hidden and are made visible.
-  settings->setMinimumTimerInterval(webkit_glue::kBackgroundTabTimerInterval);
 }
