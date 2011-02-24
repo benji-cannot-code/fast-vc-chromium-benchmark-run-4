2031500629a4e02a97db5c91116388aa5a18ebfd
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 14:48:47 +0000

    Windows Production build fix
    * platform/network/cf/AuthenticationCF.cpp: Add an extra #include as a workaround for
    <rdar://problem/9042114>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/AuthenticationCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/AuthenticationCF.cpp
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Credential.h"
 #include "ProtectionSpace.h"
 
+// This header must come before all other CFNetwork headers to work around a CFNetwork bug. It can
+// be removed entirely once <rdar://problem/9042114> is fixed.
+#include <CFNetwork/CFURLConnectionPriv.h>
+
 #include <CFNetwork/CFURLAuthChallengePriv.h>
 #include <CFNetwork/CFURLCredentialPriv.h>
 #include <CFNetwork/CFURLProtectionSpacePriv.h>
