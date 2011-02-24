4ef33f16a12b21fde14d02432679598af3cb31a6
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 22:58:48 +0000

    2011-02-24  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Darin Adler.
    
            Variable-sized allocation (but still capped at 64 bytes)
            https://bugs.webkit.org/show_bug.cgi?id=55159
            
            SunSpider reports no change.
    
            * JavaScriptCore.exp: Some day, I hope not to have to edit this file.
    
            * runtime/Heap.cpp:
            (JSC::Heap::allocateSlowCase): Split allocation into a fast and slow
            case, so the fast case can inline size class selection and turn it into
            a compile-time constant.
            
            Changed the collect-on-every allocation debugging switch to collect only
            on every slow allocation, so you can still flip the switch without
            recompiling the world. This may also be preferable for debugging purposes,
            since collecting after every single allocation can be unusably slow,
            and can mask problems by running destructors early.
    
            * runtime/Heap.h: Ditto.
    
            * runtime/JSCell.h:
            (JSC::JSCell::MarkedSpace::sizeClassFor):
            (JSC::JSCell::Heap::allocate):
            (JSC::JSCell::JSCell::operator new): The inlining mentioned above.
    
            * runtime/MarkedBlock.h: Dropped the block size from 256KB to 16KB. With
            multiple size classes, allocating a full 256KB for the first allocation
            in a given class can be pathologically wasteful. (8KB, or 4KB Mac and
            8KB Windows, would be even better, but that seems to be a peformance
            regression for now.)
            
            * runtime/MarkedSpace.cpp:
            (JSC::MarkedSpace::MarkedSpace):
            (JSC::MarkedSpace::allocateBlock):
            (JSC::MarkedSpace::reset): There's more than one size class now, and its
            cell size is not constant.
    
            * runtime/MarkedSpace.h:
            (JSC::MarkedSpace::SizeClass::SizeClass): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Heap.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Tracing.h"
 #include <algorithm>
 
-#define COLLECT_ON_EVERY_ALLOCATION 0
+#define COLLECT_ON_EVERY_SLOW_ALLOCATION 0
 
 using namespace std;
 
@@ -103,29 +103,24 @@ void Heap::reportExtraMemoryCostSlowCase(size_t cost)
     m_extraCost += cost;
 }
 
-void* Heap::allocate(size_t s)
+void* Heap::allocateSlowCase(size_t bytes)
 {
     ASSERT(globalData()->identifierTable == wtfThreadData().currentIdentifierTable());
     ASSERT(JSLock::lockCount() > 0);
     ASSERT(JSLock::currentThreadIsHoldingLock());
-    ASSERT_UNUSED(s, s <= MarkedSpace::cellSize);
+    ASSERT(bytes <= MarkedSpace::maxCellSize);
     ASSERT(m_operationInProgress == NoOperation);
 
-#if COLLECT_ON_EVERY_ALLOCATION
+#if COLLECT_ON_EVERY_SLOW_ALLOCATION
     collectAllGarbage();
     ASSERT(m_operationInProgress == NoOperation);
 #endif
 
-    m_operationInProgress = Allocation;
-    void* result = m_markedSpace.allocate(s);
-    m_operationInProgress = NoOperation;
-    if (!result) {
-        reset(DoNotSweep);
+    reset(DoNotSweep);
 
-        m_operationInProgress = Allocation;
-        result = m_markedSpace.allocate(s);
-        m_operationInProgress = NoOperation;
-    }
+    m_operationInProgress = Allocation;
+    void* result = m_markedSpace.allocate(bytes);
+    m_operationInProgress = NoOperation;
 
     ASSERT(result);
     return result;
