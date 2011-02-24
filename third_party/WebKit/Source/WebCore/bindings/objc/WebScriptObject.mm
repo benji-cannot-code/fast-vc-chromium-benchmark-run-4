4855685b06691371990155243df468267f325036
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 21:24:45 +0000

    Fix clang build.
    * bindings/objc/WebScriptObject.mm:
    (-[WebUndefined release]):
    Release should be "oneway void".
    
    (-[WebUndefined retainCount]):
    Return NSUIntegerMax instead of UINT_MAX.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/objc/WebScriptObject.mm
+++ third_party/WebKit/Source/WebCore/bindings/objc/WebScriptObject.mm
@@ -650,13 +650,13 @@ static void getListFromNSArray(ExecState *exec, NSArray *array, RootObject* root
     return self;
 }
 
-- (void)release
+- (oneway void)release
 {
 }
 
 - (NSUInteger)retainCount
 {
-    return UINT_MAX;
+    return NSUIntegerMax;
 }
 
 - (id)autorelease
