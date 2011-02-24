eab71458f56183a884c5403b8e49943722377cba
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 16:59:12 +0000

    2011-02-24  Emil A Eklund  <eae@chromium.org>
            Reviewed by Simon Fraser.
    
            Add support for missing properties to getComputedStyle
            https://bugs.webkit.org/show_bug.cgi?id=23668
    
            Test getComputedStyle for the content, counter and outline-offset
            properties.
    
            * fast/css/getComputedStyle/computed-style-properties-expected.txt: Added.
            * fast/css/getComputedStyle/computed-style-properties.html: Added.
    2011-02-24  Emil A Eklund  <eae@chromium.org>
    
            Reviewed by Simon Fraser.
    
            Add support for missing properties to getComputedStyle
            https://bugs.webkit.org/show_bug.cgi?id=23668
    
            Implement getComputedStyle for the content, counter and outline-offset
            properties.
    
            Test: fast/css/getComputedStyle/computed-style-properties.html
    
            * css/CSSComputedStyleDeclaration.cpp:
            (WebCore::contentToCSSValue):
            (WebCore::counterToCSSValue):
            (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
            * css/CSSPrimitiveValue.cpp:
            (WebCore::CSSPrimitiveValue::cssText):
            * css/CSSPrimitiveValue.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSComputedStyleDeclaration.h"
 
 #include "AnimationController.h"
+#include "ContentData.h"
+#include "CounterContent.h"
 #include "CursorList.h"
 #include "CSSBorderImageValue.h"
 #include "CSSMutableStyleDeclaration.h"
@@ -726,6 +728,36 @@ static PassRefPtr<CSSValue> fillSizeToCSSValue(const FillSize& fillSize)
     return list.release();
 }
 
+static PassRefPtr<CSSValue> contentToCSSValue(const RenderStyle* style)
+{
+    RefPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
+    for (const ContentData* contentData = style->contentData(); contentData; contentData = contentData->next()) {
+        if (contentData->isCounter()) {
+            const CounterContent* counter = contentData->counter();
+            ASSERT(counter);
+            list->append(CSSPrimitiveValue::create(counter->identifier(), CSSPrimitiveValue::CSS_COUNTER_NAME));
+        } else if (contentData->isImage()) {
+            const StyleImage* image = contentData->image();
+            ASSERT(image);
+            list->append(image->cssValue());
+        } else if (contentData->isText())
+            list->append(CSSPrimitiveValue::create(contentData->text(), CSSPrimitiveValue::CSS_STRING));
+    }
+    return list.release();
+}
+
+static PassRefPtr<CSSValue> counterToCSSValue(const RenderStyle* style, int propertyID)
+{
+    RefPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
+    const CounterDirectiveMap* map = style->counterDirectives();
+    for (CounterDirectiveMap::const_iterator it = map->begin(); it != map->end(); ++it) {
+        list->append(CSSPrimitiveValue::create(it->first.get(), CSSPrimitiveValue::CSS_STRING));
+        short number = propertyID == CSSPropertyCounterIncrement ? it->second.m_incrementValue : it->second.m_resetValue;
+        list->append(CSSPrimitiveValue::create((double)number, CSSPrimitiveValue::CSS_NUMBER));
+    }
+    return list.release();
+}
+
 static void logUnimplementedPropertyID(int propertyID)
 {
     DEFINE_STATIC_LOCAL(HashSet<int>, propertyIDSet, ());
@@ -1181,6 +1213,8 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
             return CSSPrimitiveValue::create(style->orphans(), CSSPrimitiveValue::CSS_NUMBER);
         case CSSPropertyOutlineColor:
             return m_allowVisitedStyle ? CSSPrimitiveValue::createColor(style->visitedDependentColor(CSSPropertyOutlineColor).rgb()) : currentColorOrValidColor(style.get(), style->outlineColor());
+        case CSSPropertyOutlineOffset:
+            return zoomAdjustedPixelValue(style->outlineOffset(), style.get());
         case CSSPropertyOutlineStyle:
             if (style->outlineStyleIsAuto())
                 return CSSPrimitiveValue::createIdentifier(CSSValueAuto);
@@ -1577,6 +1611,13 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
             return CSSPrimitiveValue::create(style->writingMode());
         case CSSPropertyWebkitTextCombine:
             return CSSPrimitiveValue::create(style->textCombine());
+
+        case CSSPropertyContent:
+            return contentToCSSValue(style.get());
+        case CSSPropertyCounterIncrement:
+            return counterToCSSValue(style.get(), propertyID);
+        case CSSPropertyCounterReset:
+            return counterToCSSValue(style.get(), propertyID);
         
         /* Shorthand properties, currently not supported see bug 13658*/
         case CSSPropertyBackground:
@@ -1592,7 +1633,13 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
         case CSSPropertyFont:
         case CSSPropertyListStyle:
         case CSSPropertyMargin:
+        case CSSPropertyOutline:
         case CSSPropertyPadding:
+            break;
+
+        /* Individual properties not part of the spec */
+        case CSSPropertyBackgroundRepeatX:
+        case CSSPropertyBackgroundRepeatY:
             break;
 
         /* Unimplemented CSS 3 properties (including CSS3 shorthand properties) */
@@ -1655,13 +1702,6 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
             break;
 
         /* Other unimplemented properties */
-        case CSSPropertyBackgroundRepeatX:
-        case CSSPropertyBackgroundRepeatY:
-        case CSSPropertyContent: // FIXME: needs implementation, bug 23668
-        case CSSPropertyCounterIncrement:
-        case CSSPropertyCounterReset:
-        case CSSPropertyOutline: // FIXME: needs implementation
-        case CSSPropertyOutlineOffset: // FIXME: needs implementation
         case CSSPropertyPage: // for @page
         case CSSPropertyQuotes: // FIXME: needs implementation
         case CSSPropertySize: // for @page
