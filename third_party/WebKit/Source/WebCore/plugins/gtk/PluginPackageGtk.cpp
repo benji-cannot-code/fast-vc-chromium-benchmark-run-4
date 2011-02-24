59682bb148c02558f1275a25c5c802ebf19fa595
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 17:38:09 +0000

    2011-02-24  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Remove the GFile GOwnPtr specialization
            https://bugs.webkit.org/show_bug.cgi?id=55154
    
            Remove the GFile specialization of GOwnPtr. It's sufficient to use GRefPtr
            to track GFiles since they are just regular reference-counted GObjects.
    
            * wtf/gobject/GOwnPtr.cpp: Remove GFile specialization.
            * wtf/gobject/GOwnPtr.h: Ditto.
    2011-02-24  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Remove the GFile GOwnPtr specialization
            https://bugs.webkit.org/show_bug.cgi?id=55154
    
            Convert uses of GOwnPtr<GFile> to GRefPtr<GFile>.
    
            No new tests. This should not change behavior.
    
            * plugins/gtk/PluginPackageGtk.cpp: Fix include order and remove unnecessary include.
            (WebCore::PluginPackage::load): Use GRefPtr for GFile instead of GOwnPtr.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/gtk/PluginPackageGtk.cpp
+++ third_party/WebKit/Source/WebCore/plugins/gtk/PluginPackageGtk.cpp
@@ -30,14 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PluginPackage.h"
 
-#include <gio/gio.h>
-#include <stdio.h>
-
-#include "GOwnPtr.h"
+#include "GOwnPtrGtk.h"
+#include "GRefPtrGtk.h"
 #include "MIMETypeRegistry.h"
 #include "NotImplemented.h"
 #include "npruntime_impl.h"
 #include "PluginDebug.h"
+#include <gio/gio.h>
 #include <wtf/text/CString.h>
 
 namespace WebCore {
@@ -130,10 +129,10 @@ bool PluginPackage::load()
 
     GOwnPtr<gchar> finalPath(g_strdup(m_path.utf8().data()));
     while (g_file_test(finalPath.get(), G_FILE_TEST_IS_SYMLINK)) {
-        GOwnPtr<GFile> file(g_file_new_for_path(finalPath.get()));
-        GOwnPtr<GFile> dir(g_file_get_parent(file.get()));
+        GRefPtr<GFile> file = adoptGRef(g_file_new_for_path(finalPath.get()));
+        GRefPtr<GFile> dir = adoptGRef(g_file_get_parent(file.get()));
         GOwnPtr<gchar> linkPath(g_file_read_link(finalPath.get(), 0));
-        GOwnPtr<GFile> resolvedFile(g_file_resolve_relative_path(dir.get(), linkPath.get()));
+        GRefPtr<GFile> resolvedFile = adoptGRef(g_file_resolve_relative_path(dir.get(), linkPath.get()));
         finalPath.set(g_file_get_path(resolvedFile.get()));
     }
 
