9f8f37ad8d2b64b7b6aa7b4d28813a52c2b6f4c5
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-24 19:19:13 +0000

    Pin the PPAPI custom cursor value. PPAPI is intended to be frozen, so make space now for expanding the number of named cursors.
    BUG=73356
    TEST=no visible change; PPAPI custom cursors are currently not implemented anyway
    
    Review URL: http://codereview.chromium.org/6576034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75928 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/pp_cursor_type_dev.h
+++ ppapi/c/dev/pp_cursor_type_dev.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/* Copyright (c) 2010 The Chromium Authors. All rights reserved.
+/* Copyright (c) 2011 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_macros.h"
 
 enum PP_CursorType_Dev {
+  PP_CURSORTYPE_CUSTOM = -1,
   PP_CURSORTYPE_POINTER = 0,
   PP_CURSORTYPE_CROSS = 1,
   PP_CURSORTYPE_HAND = 2,
@@ -49,8 +50,7 @@ enum PP_CursorType_Dev {
   PP_CURSORTYPE_NONE = 37,
   PP_CURSORTYPE_NOTALLOWED = 38,
   PP_CURSORTYPE_ZOOMIN = 39,
-  PP_CURSORTYPE_ZOOMOUT = 40,
-  PP_CURSORTYPE_CUSTOM = 41
+  PP_CURSORTYPE_ZOOMOUT = 40
 };
 PP_COMPILE_ASSERT_ENUM_SIZE_IN_BYTES(PP_CursorType_Dev, 4);
 
--- webkit/plugins/ppapi/ppb_cursor_control_impl.cc
+++ webkit/plugins/ppapi/ppb_cursor_control_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,6 +33,7 @@ PP_Bool SetCursor(PP_Instance instance_id,
       Resource::GetAs<PPB_ImageData_Impl>(custom_image_id));
   if (custom_image.get()) {
     // TODO(neb): implement custom cursors.
+    // (Remember that PP_CURSORTYPE_CUSTOM != WebCursorInfo::TypeCustom.)
     NOTIMPLEMENTED();
     return PP_FALSE;
   }
