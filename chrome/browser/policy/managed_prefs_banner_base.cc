945e8a0e501757d44dd829ba58b6ae4a0d128373
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-24 18:21:49 +0000

    Fix incorrect include.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6579040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/managed_prefs_banner_base.cc
+++ chrome/browser/policy/managed_prefs_banner_base.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/cros_settings_names.cc"
+#include "chrome/browser/chromeos/cros_settings_names.h"
 #endif
 
 namespace policy {
