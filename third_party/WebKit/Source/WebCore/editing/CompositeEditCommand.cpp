7217172fa1bc2c37a1634ff918e8b4cb3ac27594
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-24 11:09:14 +0000

    2011-02-24  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Ojan Vafai.
    
            Crash when deleting inside a blockquote with a large offset
            https://bugs.webkit.org/show_bug.cgi?id=55098
    
            The bug was caused by inconsistency in lineBreakExistsAtPosition and breakOutOfEmptyMailBlockquotedParagraph.
            While lineBreakExistsAtPosition was checking that a line break exists at the downstream of the given position,
            breakOutOfEmptyMailBlockquotedParagraph wasn't using the downstream for caretPos. Fixed the bug by using
            the downstream position to instantiate caretPos.
    
            Co-author: Abhishek Arya <inferno@chromium.org>.
    
            Test: editing/deleting/delete-blockquote-large-offsets.html
    
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::breakOutOfEmptyMailBlockquotedParagraph):
    2011-02-24  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Ojan Vafai.
    
            Crash when deleting inside a blockquote with a large offset
            https://bugs.webkit.org/show_bug.cgi?id=55098
    
            Added a test to ensure WebKit doesn't crash when deleting line inside a blockquote
            and the caret is programmatically set to have a large offset.
    
            Co-author: Abhishek Arya <inferno@chromium.org>.
    
            * editing/deleting/delete-blockquote-large-offsets-expected.txt: Added.
            * editing/deleting/delete-blockquote-large-offsets.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
@@ -1129,8 +1129,8 @@ bool CompositeEditCommand::breakOutOfEmptyMailBlockquotedParagraph()
     // If this is an empty paragraph there must be a line break here.
     if (!lineBreakExistsAtVisiblePosition(caret))
         return false;
-    
-    Position caretPos(caret.deepEquivalent());
+
+    Position caretPos(caret.deepEquivalent().downstream());
     // A line break is either a br or a preserved newline.
     ASSERT(caretPos.deprecatedNode()->hasTagName(brTag) || (caretPos.deprecatedNode()->isTextNode() && caretPos.deprecatedNode()->renderer()->style()->preserveNewline()));
     
@@ -1138,7 +1138,7 @@ bool CompositeEditCommand::breakOutOfEmptyMailBlockquotedParagraph()
         Position beforeBR(positionInParentBeforeNode(caretPos.deprecatedNode()));
         removeNode(caretPos.deprecatedNode());
         prune(beforeBR.deprecatedNode());
-    } else {
+    } else if (caretPos.deprecatedNode()->isTextNode()) {
         ASSERT(caretPos.deprecatedEditingOffset() == 0);
         Text* textNode = static_cast<Text*>(caretPos.deprecatedNode());
         ContainerNode* parentNode = textNode->parentNode();
@@ -1147,7 +1147,7 @@ bool CompositeEditCommand::breakOutOfEmptyMailBlockquotedParagraph()
         deleteTextFromNode(textNode, 0, 1);
         prune(parentNode);
     }
-    
+
     return true;
 }
 
