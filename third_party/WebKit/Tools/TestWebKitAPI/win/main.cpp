3304afa53e900449eeac9bd5bf6295a0a78e99d7
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-25 18:45:10 +0000

    Work around Cygwin's crash-suppression behavior
    Cygwin calls ::SetErrorMode(SEM_FAILCRITICALERRORS), which any processes it launches will
    inherit. This is bad for testing/debugging, as it causes the post-mortem debugger not to be
    invoked. (Cygwin does this because it makes crashes more UNIX-y.) We reset the error mode
    when our test apps launch to work around Cygwin's behavior.
    
    Fixes <http://webkit.org/b/55222> Test apps crash silently (without invoking post-mortem
    debugger) when launched from Cygwin 1.7
    
    Reviewed by Darin Adler.
    
    Source/JavaScriptCore:
    
    * API/tests/testapi.c: Added a now-needed #include.
    (main):
    * jsc.cpp:
    (main):
    Call ::SetErrorMode(0) to undo Cygwin's folly.
    
    * JavaScriptCore.vcproj/testapi/testapiCommon.vsprops: Define NOMINMAX like many of our
    other projects do so that windows.h won't define min/max macros that interfere with
    std::numeric_limits<T>::min/max.
    
    Tools:
    
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (main):
    * TestWebKitAPI/win/main.cpp:
    (main):
    * WebKitAPITest/main.cpp:
    (main):
    * WebKitTestRunner/win/TestControllerWin.cpp:
    (WTR::TestController::platformInitialize):
    Call ::SetErrorMode(0) to undo Cygwin's folly.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/win/main.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/win/main.cpp
@@ -28,6 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int main(int argc, const char* argv[])
 {
+    // Cygwin calls ::SetErrorMode(SEM_FAILCRITICALERRORS), which we will inherit. This is bad for
+    // testing/debugging, as it causes the post-mortem debugger not to be invoked. We reset the
+    // error mode here to work around Cygwin's behavior. See <http://webkit.org/b/55222>.
+    ::SetErrorMode(0);
+
     bool passed = true;
 
     std::string argument(argv[1]);
