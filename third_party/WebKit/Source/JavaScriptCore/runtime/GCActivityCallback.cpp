6c66321395ca772b2b9e8873235586bcdffdae3d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-25 00:55:19 +0000

    2011-02-24  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Remove unused parameter name in GCActivityCallback.cpp
            https://bugs.webkit.org/show_bug.cgi?id=55194
    
            This change is not strictly required for the GYP-based build system,
            but I noticed this error when working on the new build system.
    
            * runtime/GCActivityCallback.cpp:
            (JSC::DefaultGCActivityCallback::DefaultGCActivityCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/GCActivityCallback.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/GCActivityCallback.cpp
@@ -35,7 +35,7 @@ namespace JSC {
 struct DefaultGCActivityCallbackPlatformData {
 };
 
-DefaultGCActivityCallback::DefaultGCActivityCallback(Heap* heap)
+DefaultGCActivityCallback::DefaultGCActivityCallback(Heap*)
 {
 }
 
