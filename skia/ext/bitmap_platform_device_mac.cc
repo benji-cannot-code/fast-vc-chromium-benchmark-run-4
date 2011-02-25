e609c224a5860295decbc829e8f11664fccfa893
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-25 18:23:17 +0000

    Revert 76069 - roll to rev. 846, includes change to SkDevice setMatrixClip() interface
    Review URL: http://codereview.chromium.org/6549029
    
    TBR=reed@google.com
    Review URL: http://codereview.chromium.org/6596014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76070 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/bitmap_platform_device_mac.cc
+++ skia/ext/bitmap_platform_device_mac.cc
@@ -219,8 +219,7 @@ CGContextRef BitmapPlatformDevice::GetBitmapContext() {
 }
 
 void BitmapPlatformDevice::setMatrixClip(const SkMatrix& transform,
-                                         const SkRegion& region,
-                                         const SkClipStack&) {
+                                         const SkRegion& region) {
   data_->SetMatrixClip(transform, region);
 }
 
--- skia/ext/bitmap_platform_device_mac_unittest.cc
+++ skia/ext/bitmap_platform_device_mac_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkMatrix.h"
 #include "third_party/skia/include/core/SkRegion.h"
-#include "third_party/skia/include/core/SkClipStack.h"
 
 namespace skia {
 
@@ -31,12 +30,11 @@ TEST_F(BitmapPlatformDeviceMacTest, ClipRectTransformWithTranslate) {
   SkMatrix transform;
   transform.setTranslate(50, 140);
 
-  SkClipStack ignore;
   SkRegion clip_region;
   SkIRect rect;
   rect.set(0, 0, kWidth, kHeight);
   clip_region.setRect(rect);
-  bitmap_->setMatrixClip(transform, clip_region, ignore);
+  bitmap_->setMatrixClip(transform, clip_region);
 
   CGContextRef context = bitmap_->GetBitmapContext();
   SkRect clip_rect = gfx::CGRectToSkRect(CGContextGetClipBoundingBox(context));
@@ -51,12 +49,11 @@ TEST_F(BitmapPlatformDeviceMacTest, ClipRectTransformWithScale) {
   SkMatrix transform;
   transform.setScale(0.5, 0.5);
 
-  SkClipStack unused;
   SkRegion clip_region;
   SkIRect rect;
   rect.set(0, 0, kWidth, kHeight);
   clip_region.setRect(rect);
-  bitmap_->setMatrixClip(transform, clip_region, unused);
+  bitmap_->setMatrixClip(transform, clip_region);
 
   CGContextRef context = bitmap_->GetBitmapContext();
   SkRect clip_rect = gfx::CGRectToSkRect(CGContextGetClipBoundingBox(context));
