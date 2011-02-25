c60724e5cbeff682adcdd2415a632bacca81b98d
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-25 22:39:58 +0000

    Mac proxy auto-launch.
    Delta from http://codereview.chromium.org/6482016 that adds in auto-launching of the proxy on the Macintosh and turns on the proxy management UI in Under the Hood.
    
    BUG=none
    TEST=Bring up the UI, enable CP, log out and log in, check to see that the proxy is running.
    
    Review URL: http://codereview.chromium.org/6499019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/service/service_process_control_mac.mm
+++ chrome/browser/service/service_process_control_mac.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void ServiceProcessControl::Launcher::DoRun() {
   base::mac::ScopedCFTypeRef<CFDictionaryRef> launchd_plist(
-      CreateServiceProcessLaunchdPlist(cmd_line_.get()));
+      CreateServiceProcessLaunchdPlist(cmd_line_.get(), false));
   CFErrorRef error = NULL;
   if (!GTMSMJobSubmit(launchd_plist, &error)) {
     LOG(ERROR) << error;
