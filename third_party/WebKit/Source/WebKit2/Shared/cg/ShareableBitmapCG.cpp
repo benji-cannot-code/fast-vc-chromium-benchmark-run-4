699da183a5374f0ae6bf78e30f5238bdac31d25a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-25 02:10:15 +0000

    2011-02-24  Darin Adler  <darin@apple.com>
            Reviewed by Anders Carlsson.
    
            WebKit2: Image-based cursors do not work
            https://bugs.webkit.org/show_bug.cgi?id=55184
    
            * WebCore.exp.in: Exported new entry points now used by WebKit2.
    2011-02-24  Darin Adler  <darin@apple.com>
    
            Reviewed by Anders Carlsson.
    
            WebKit2: Image-based cursors do not work
            https://bugs.webkit.org/show_bug.cgi?id=55184
    
            * Shared/ShareableBitmap.h: Added releaseData function
            declaration. This is only defined for CG, but we can declare
            it without defining it on non-CG platforms without causing problems.
    
            * Shared/WebCoreArgumentCoders.cpp: Added.
            (CoreIPC::encodeImage): Added. For use in the Cursor argument coder.
            (CoreIPC::decodeImage): Ditto.
    
            * Shared/WebCoreArgumentCoders.h: Added code to the Cursor encoder
            and decoder to handle image-based cursors, using the new encodeImage
            and decodeImage functions.
    
            * Shared/cg/ShareableBitmapCG.cpp:
            (WebKit::ShareableBitmap::releaseData): Added.
            (WebKit::ShareableBitmap::createGraphicsContext): Use
            CGBitmapContextCreateWithData instead of CGBitmapContextCreate,
            to guarantee we outlast the CGBitmapContext we create.
            (WebKit::ShareableBitmap::paint): Got rid of unnneeded local variable.
    
            * Shared/cg/WebCoreArgumentCodersCG.cpp: Added.
            (CoreIPC::createImage): Added. For use by CoreIPC::decodeImage.
    
            * WebKit2.xcodeproj/project.pbxproj: Added WebCoreArgumentCoders.cpp
            and WebCoreArgumentCodersCG.cpp.
            * win/WebKit2.vcproj: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/cg/ShareableBitmapCG.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/cg/ShareableBitmapCG.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,10 +38,13 @@ namespace WebKit {
 PassOwnPtr<GraphicsContext> ShareableBitmap::createGraphicsContext()
 {
     RetainPtr<CGColorSpaceRef> colorSpace(AdoptCF, CGColorSpaceCreateDeviceRGB());
-    RetainPtr<CGContextRef> bitmapContext(AdoptCF, CGBitmapContextCreate(data(), m_size.width(), m_size.height(), 8,  m_size.width() * 4, colorSpace.get(), 
-                                                                         kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
 
-    // We want the origin to be in the top left corner so flip the backing store context.
+    ref(); // Balanced by deref in releaseData.
+    RetainPtr<CGContextRef> bitmapContext(AdoptCF, CGBitmapContextCreateWithData(data(),
+        m_size.width(), m_size.height(), 8, m_size.width() * 4, colorSpace.get(),
+        kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host, releaseData, this));
+
+    // We want the origin to be in the top left corner so we flip the backing store context.
     CGContextTranslateCTM(bitmapContext.get(), 0, m_size.height());
     CGContextScaleCTM(bitmapContext.get(), 1, -1);
 
@@ -50,9 +53,14 @@ PassOwnPtr<GraphicsContext> ShareableBitmap::createGraphicsContext()
 
 void ShareableBitmap::paint(WebCore::GraphicsContext& context, const IntPoint& dstPoint, const IntRect& srcRect)
 {
-    OwnPtr<GraphicsContext> sourceContext(createGraphicsContext());
-
-    paintBitmapContext(context.platformContext(), sourceContext->platformContext(), dstPoint, srcRect);
+    paintBitmapContext(context.platformContext(), createGraphicsContext()->platformContext(), dstPoint, srcRect);
 }
-        
+
+void ShareableBitmap::releaseData(void* typelessBitmap, void* typelessData)
+{
+    ShareableBitmap* bitmap = static_cast<ShareableBitmap*>(typelessBitmap);
+    ASSERT_UNUSED(typelessData, bitmap->data() == typelessData);
+    bitmap->deref(); // Balanced by ref in createGraphicsContext.
+}
+
 } // namespace WebKit
