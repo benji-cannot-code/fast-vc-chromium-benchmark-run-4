638f91a545d056d07ce69a9529f6071125abd925
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-25 14:40:20 +0000

    touchui: Fix building on a buildbot.
    TBR=msw@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76047 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/accelerator_handler_touch.cc
+++ views/focus/accelerator_handler_touch.cc
@@ -93,7 +93,8 @@ bool DispatchX2Event(RootView* root, XEvent* xev) {
       XIDeviceEvent* xievent = static_cast<XIDeviceEvent*>(cookie->data);
       if (xievent->detail == 4 || xievent->detail == 5) {
         Event::FromNativeEvent2 from_native;
-        return root->OnMouseWheel(MouseWheelEvent(xev, from_native));
+        MouseWheelEvent wheelev(xev, from_native);
+        return root->OnMouseWheel(wheelev);
       }
 
       MouseEvent mouseev(xev);
@@ -175,7 +176,8 @@ bool DispatchXEvent(XEvent* xev) {
         if (xev->xbutton.button == 4 || xev->xbutton.button == 5) {
           // Scrolling the wheel triggers button press/release events.
           Event::FromNativeEvent2 from_native;
-          return root->OnMouseWheel(MouseWheelEvent(xev, from_native));
+          MouseWheelEvent wheelev(xev, from_native);
+          return root->OnMouseWheel(wheelev);
         } else {
           MouseEvent mouseev(xev);
           if (xev->type == ButtonPress) {
