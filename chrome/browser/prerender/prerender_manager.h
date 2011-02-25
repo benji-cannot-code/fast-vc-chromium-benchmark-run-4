04aa25f8e4c30f5e9bc4f55fa62bf6f5fa5a938f
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-25 21:22:09 +0000

    In PrerenderManager, use RepeatingTimer for repeating cleanup tasks. BUG=None Test=None
    Review URL: http://codereview.chromium.org/6598008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.h
+++ chrome/browser/prerender/prerender_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "base/time.h"
+#include "base/timer.h"
 #include "chrome/browser/prerender/prerender_contents.h"
 #include "googleurl/src/gurl.h"
 
@@ -94,9 +95,6 @@ class PrerenderManager : public base::RefCounted<PrerenderManager> {
   void StartSchedulingPeriodicCleanups();
   void StopSchedulingPeriodicCleanups();
 
-  // Schedules a periodic cleanup.
-  void SchedulePeriodicCleanup();
-
   // Deletes stale prerendered PrerenderContents.
   // Also identifies and kills PrerenderContents that use too much
   // resources.
@@ -150,9 +148,9 @@ class PrerenderManager : public base::RefCounted<PrerenderManager> {
   // This static variable should only be modified on the UI thread.
   static base::TimeTicks last_prefetch_seen_time_;
 
-  // Indicates whether we are currently performing periodic cleanups
-  // of pending prerendered pages.
-  bool periodic_cleanups_active_;
+  // RepeatingTimer to perform periodic cleanups of pending prerendered
+  // pages.
+  base::RepeatingTimer<PrerenderManager> repeating_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(PrerenderManager);
 };
