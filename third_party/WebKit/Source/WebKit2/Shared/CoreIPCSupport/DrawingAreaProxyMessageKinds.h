5391a46d4cbcdd43958d92f5ce65c95818309236
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-25 19:08:08 +0000

    2011-02-25  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Get rid of more old accelerated compositing code
            https://bugs.webkit.org/show_bug.cgi?id=55235
    
            * Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h:
            * UIProcess/ChunkedUpdateDrawingAreaProxy.h:
            * UIProcess/DrawingAreaProxy.h:
            * UIProcess/DrawingAreaProxyImpl.cpp:
            * UIProcess/DrawingAreaProxyImpl.h:
            * WebProcess/WebPage/ChunkedUpdateDrawingArea.h:
            * WebProcess/WebPage/DrawingArea.h:
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            * WebProcess/WebPage/DrawingAreaImpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h
+++ third_party/WebKit/Source/WebKit2/Shared/CoreIPCSupport/DrawingAreaProxyMessageKinds.h
@@ -36,9 +36,6 @@ namespace DrawingAreaProxyLegacyMessage {
 enum Kind {
     Update,
     DidSetSize,
-#if USE(ACCELERATED_COMPOSITING)
-    AttachCompositingContext,
-#endif
 #if ENABLE(TILED_BACKING_STORE)
     Invalidate,
     TileUpdated,
