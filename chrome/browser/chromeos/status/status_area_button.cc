ea81aaf07cec986cb1a1223b4b1b091264b3417f
chocobo@chromium.org <chocobo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-25 19:41:31 +0000

    Fix bug where badge is not showing up on network menu dropdown. BUG=chromium-os:12239,chromium-os:12232 TEST=manual Review URL: http://codereview.chromium.org/6578040
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/status_area_button.cc
+++ chrome/browser/chromeos/status/status_area_button.cc
@@ -41,7 +41,7 @@ void StatusAreaButton::OnPaint(gfx::Canvas* canvas, bool for_drag) {
   if (use_menu_button_paint_) {
     views::MenuButton::Paint(canvas, for_drag);
   } else {
-    DrawIcon(canvas);
+    canvas->DrawBitmapInt(icon(), horizontal_padding(), 0);
     OnPaintFocusBorder(canvas);
   }
 }
@@ -85,10 +85,6 @@ void StatusAreaButton::SetText(const std::wstring& text) {
   views::MenuButton::SetText(text);
   ClearMaxTextSize();
   PreferredSizeChanged();
-}
-
-void StatusAreaButton::DrawIcon(gfx::Canvas* canvas) {
-  canvas->DrawBitmapInt(icon(), horizontal_padding(), 0);
 }
 
 bool StatusAreaButton::Activate() {
