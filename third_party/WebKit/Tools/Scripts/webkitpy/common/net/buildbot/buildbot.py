f2c9b13a1c3694e64f2220724386d39e8054fad4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-25 22:28:39 +0000

    2011-02-25  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r79324.
            http://trac.webkit.org/changeset/79324
            https://bugs.webkit.org/show_bug.cgi?id=55260
    
            Leopard Debug is once again running out of address space while
            linking (Requested by aroben on #webkit).
    
            * Scripts/webkitpy/common/net/buildbot/buildbot.py:
            * Scripts/webkitpy/common/net/buildbot/buildbot_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot.py
@@ -269,7 +269,7 @@ class BuildBot(object):
             "SnowLeopard.*Build",
             "SnowLeopard.*\(Test",
             "SnowLeopard.*\(WebKit2 Test",
-            "Leopard",
+            "Leopard.*Release",
             "Windows.*Build",
             "WinCE",
             "EFL",
