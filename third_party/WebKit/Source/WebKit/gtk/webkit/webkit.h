1cc7d103e4418592e9c9fb0f0355bd182e70a445
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-26 19:08:30 +0000

    2011-02-26  Lukasz Slachciak  <l.slachciak@samsung.com>
            Reviewed by Xan Lopez.
    
            [GTK] Implemented API to set/get web application cache directory path database.
    
            * GNUmakefile.am: Add webkitapplicationcache.h file.
            * webkit/webkit.h: Add webkitapplicationcache.h file.
            * webkit/webkitapplicationcache.cpp: definition of API setting and getting
            web application cache directory path database.
            (webkit_application_cache_get_database_directory_path):
            (webkit_application_cache_set_database_directory_path):
            * webkit/webkitapplicationcache.h: Added new API file for setting and getting
            web application cache directory path database.
            * webkit/webkitglobals.cpp:
            (webkitInit): use webkit_application_cache_set_database_directory_path
            to set default web application cache directory path database.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkit.h
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkit.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define __WEBKIT_H__
 
 #include <webkit/webkitversion.h>
+#include <webkit/webkitapplicationcache.h>
 #include <webkit/webkitdefines.h>
 #include <webkit/webkitdom.h>
 #include <webkit/webkitdownload.h>
--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitglobals.cpp
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitglobals.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandleClient.h"
 #include "ResourceHandleInternal.h"
 #include "ResourceResponse.h"
+#include "webkitapplicationcache.h"
 #include "webkitglobalsprivate.h"
 #include "webkitsoupauthdialog.h"
 #include "webkitwebdatabase.h"
@@ -249,11 +250,8 @@ void webkitInit()
 
     gchar* databaseDirectory = g_build_filename(g_get_user_data_dir(), "webkit", "databases", NULL);
     webkit_set_web_database_directory_path(databaseDirectory);
+    webkit_application_cache_set_database_directory_path(databaseDirectory);
 
-#if ENABLE(OFFLINE_WEB_APPLICATIONS)
-    // FIXME: It should be possible for client applications to override the default appcache location
-    WebCore::cacheStorage().setCacheDirectory(databaseDirectory);
-#endif
     g_free(databaseDirectory);
 
     PageGroup::setShouldTrackVisitedLinks(true);
