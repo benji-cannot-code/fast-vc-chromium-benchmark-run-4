117068392c08a9deae1a88b33971c3bcb3b2dd40
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-26 00:43:03 +0000

    Added a command-line option to disable adding the service process to autorun. This is for use by enterprise proxies that use their own custom autostart mechanism. Also honored the kNoErrorDialogs flag to not show the token expired notification.
    BUG=None
    TEST=Launching the service process with --no-service-autorun should not register the service process to autorun. Also running it with --noerrdialogs and an expired token should not show the token expired notification.
    
    Review URL: http://codereview.chromium.org/6592023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76128 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/service_process.cc
+++ chrome/service/service_process.cc
@@ -307,7 +307,9 @@ void ServiceProcess::OnChromotingHostDisabled() {
 
 void ServiceProcess::OnServiceEnabled() {
   enabled_services_++;
-  if (1 == enabled_services_) {
+  if ((1 == enabled_services_) &&
+      !CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kNoServiceAutorun)) {
     ServiceProcessState::GetInstance()->AddToAutoRun();
   }
 }
