48c88426a040d25b77b50a635d77142f9158fff4
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-26 12:42:22 +0000

    gtk: Document the ConfirmInfoBarGtk class.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6591025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/confirm_infobar_gtk.h
+++ chrome/browser/ui/gtk/infobars/confirm_infobar_gtk.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef struct _GtkWidget GtkWidget;
 
+// An infobar that shows a message, up to two optional buttons, and an optional,
+// right-aligned link. This is commonly used to do things like:
+// "Would you like to do X? [Yes] [No]                         _Learn More_ [x]"
 class ConfirmInfoBarGtk : public InfoBar {
  public:
   explicit ConfirmInfoBarGtk(ConfirmInfoBarDelegate* delegate);
