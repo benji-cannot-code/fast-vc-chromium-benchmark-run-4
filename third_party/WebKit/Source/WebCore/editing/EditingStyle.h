77365c4d37dac8e1a929680acf928f08f6b06206
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-26 05:26:30 +0000

    2011-02-25  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            Move HTMLEquivalent and its subclasses to EditingStyle
            https://bugs.webkit.org/show_bug.cgi?id=55207
    
            Moved HTMLEquivalent and its subclasses from ApplyStyleCommand to EditingStyle,
            and renamed HTMLEquivalent, HTMLEquivalentValueList, HTMLAttributeEquivalent,
            and HTMLEquivalentFontSizeAttribute to HTMLElementEquivalent, HTMLTextDecorationEquivalent,
            HTMLAttributeEquivalent, and HTMLFontSizeEquivalent respectively.
    
            Also extracted the logic to determine which element and attribute are removed
            as conflictsWithImplicitStyleOfElement, conflictsWithImplicitStyleOfAttributes,
            and extractConflictingImplicitStyleOfAttributes.
    
            * editing/ApplyStyleCommand.cpp:
            (WebCore::ApplyStyleCommand::removeInlineStyleFromElement):
            (WebCore::ApplyStyleCommand::removeImplicitlyStyledElement):
            * editing/ApplyStyleCommand.h:
            * editing/EditingStyle.cpp:
            (WebCore::HTMLElementEquivalent::create): Moved from ApplyStyleCommand.cpp.
            (WebCore::HTMLElementEquivalent::~HTMLElementEquivalent): Ditto.
            (WebCore::HTMLElementEquivalent::matches): Ditto.
            (WebCore::HTMLElementEquivalent::hasAttribute): Ditto.
            (WebCore::HTMLElementEquivalent::propertyExistsInStyle): Ditto.
            (WebCore::HTMLElementEquivalent::HTMLElementEquivalent): Ditto.
            (WebCore::HTMLElementEquivalent::valueIsPresentInStyle): Ditto.
            (WebCore::HTMLElementEquivalent::addToStyle): Ditto.
            (WebCore::HTMLTextDecorationEquivalent::create): Ditto.
            (WebCore::HTMLTextDecorationEquivalent::HTMLTextDecorationEquivalent): Ditto.
            (WebCore::HTMLTextDecorationEquivalent::valueIsPresentInStyle): Ditto.
            (WebCore::HTMLAttributeEquivalent::create): Ditto.
            (WebCore::HTMLAttributeEquivalent::matches): Ditto.
            (WebCore::HTMLAttributeEquivalent::hasAttribute): Ditto.
            (WebCore::HTMLAttributeEquivalent::attributeName): Ditto.
            (WebCore::HTMLAttributeEquivalent::HTMLAttributeEquivalent): Ditto.
            (WebCore::HTMLAttributeEquivalent::valueIsPresentInStyle): Ditto.
            (WebCore::HTMLAttributeEquivalent::addToStyle): Ditto.
            (WebCore::HTMLAttributeEquivalent::attributeValueAsCSSValue): Ditto.
            (WebCore::HTMLFontSizeEquivalent::create): Ditto.
            (WebCore::HTMLFontSizeEquivalent::HTMLFontSizeEquivalent): Ditto.
            (WebCore::HTMLFontSizeEquivalent::attributeValueAsCSSValue): Ditto.
            (WebCore::EditingStyle::conflictsWithImplicitStyleOfElement): Added.
            (WebCore::htmlAttributeEquivalents): Added.
            (WebCore::EditingStyle::conflictsWithImplicitStyleOfAttributes): Added.
            (WebCore::EditingStyle::extractConflictingImplicitStyleOfAttributes): Added.
            * editing/EditingStyle.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79758 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/EditingStyle.h
+++ third_party/WebKit/Source/WebCore/editing/EditingStyle.h
@@ -44,8 +44,10 @@ class CSSStyleDeclaration;
 class CSSComputedStyleDeclaration;
 class CSSMutableStyleDeclaration;
 class Document;
+class HTMLElement;
 class Node;
 class Position;
+class QualifiedName;
 class RenderStyle;
 class StyledElement;
 
@@ -54,6 +56,7 @@ public:
 
     enum PropertiesToInclude { AllProperties, OnlyInheritableProperties };
     enum ShouldPreserveWritingDirection { PreserveWritingDirection, DoNotPreserveWritingDirection };
+    enum ShouldExtractMatchingStyle { ExtractMatchingStyle, DoNotExtractMatchingStyle };
     static float NoFontDelta;
 
     static PassRefPtr<EditingStyle> create()
@@ -97,6 +100,10 @@ public:
     {
         return conflictsWithInlineStyleOfElement(element, &conflictingProperties);
     }
+    bool conflictsWithImplicitStyleOfElement(HTMLElement*, CSSMutableStyleDeclaration* extractedStyle = 0, ShouldExtractMatchingStyle = DoNotExtractMatchingStyle) const;
+    bool conflictsWithImplicitStyleOfAttributes(HTMLElement*) const;
+    bool extractConflictingImplicitStyleOfAttributes(HTMLElement*, ShouldPreserveWritingDirection, CSSMutableStyleDeclaration* extractedStyle,
+            Vector<QualifiedName>& conflictingAttributes, ShouldExtractMatchingStyle) const;
     void prepareToApplyAt(const Position&, ShouldPreserveWritingDirection = DoNotPreserveWritingDirection);
     void mergeTypingStyle(Document*);
 
