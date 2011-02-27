e27fedfbdf2d47a2b5310fa0e8b303029d4cdb09
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-27 18:05:12 +0000

    2011-02-27  Alejandro G. Castro  <alex@igalia.com>
            Unreviewed, fixed GTK compilation after r79806.
    
            * UIProcess/gtk/WebView.cpp:
            (WebKit::WebView::didFinishLoadingDataForCustomRepresentation):
            * UIProcess/gtk/WebView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebView.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebView.h
@@ -109,7 +109,7 @@ private:
 #endif
 
     virtual void didCommitLoadForMainFrame(bool useCustomRepresentation);
-    virtual void didFinishLoadingDataForCustomRepresentation(const CoreIPC::DataReference&);
+    virtual void didFinishLoadingDataForCustomRepresentation(const String& suggestedFilename, const CoreIPC::DataReference&);
     virtual double customRepresentationZoomFactor();
     virtual void setCustomRepresentationZoomFactor(double);
 
