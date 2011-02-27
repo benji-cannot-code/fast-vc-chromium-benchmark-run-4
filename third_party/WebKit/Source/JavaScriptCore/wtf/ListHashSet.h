ff55cb3df1c2620988e213b1c01ed89f2936293b
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-27 15:51:19 +0000

    Eliminate DeprecatedPtrList from RenderBlock https://bugs.webkit.org/show_bug.cgi?id=54972
    Patch by Benjamin Poulain <ikipou@gmail.com> on 2011-02-27
    Reviewed by Darin Adler.
    
    Source/JavaScriptCore:
    
    Add methods find() and contains() using an adaptor to ListHashSet.
    Those method are like the one of HashSet, they allow to find objects
    based on a different key than the one used to define the set.
    
    Add convenience methods for direct access to the head and tail of the list.
    Those methods are providing similar API/behavior as Vector.
    
    * wtf/ListHashSet.h:
    (WTF::::first):
    (WTF::::last):
    (WTF::::removeLast):
    (WTF::ListHashSetTranslatorAdapter::hash):
    (WTF::ListHashSetTranslatorAdapter::equal):
    (WTF::::find):
    (WTF::::contains):
    
    Source/WebCore:
    
    Refactor RenderBlock to get rid of the DeprecatedPtrList.
    The floating objects are stored in a ListHashSet.
    
    Refactoring covered by existing test.
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::~RenderBlock):
    (WebCore::RenderBlock::addOverflowFromFloats):
    (WebCore::RenderBlock::repaintOverhangingFloats):
    (WebCore::RenderBlock::paintFloats):
    (WebCore::RenderBlock::selectionGaps):
    (WebCore::RenderBlock::insertFloatingObject):
    (WebCore::RenderBlock::removeFloatingObject):
    (WebCore::RenderBlock::removeFloatingObjectsBelow):
    (WebCore::RenderBlock::positionNewFloats):
    (WebCore::RenderBlock::positionNewFloatOnLine):
    (WebCore::RenderBlock::logicalLeftOffsetForLine):
    (WebCore::RenderBlock::logicalRightOffsetForLine):
    (WebCore::RenderBlock::nextFloatLogicalBottomBelow):
    (WebCore::RenderBlock::lowestFloatLogicalBottom):
    (WebCore::RenderBlock::clearFloats):
    (WebCore::RenderBlock::addOverhangingFloats):
    (WebCore::RenderBlock::addIntrudingFloats):
    (WebCore::RenderBlock::containsFloat):
    (WebCore::RenderBlock::hitTestFloats):
    (WebCore::RenderBlock::adjustForBorderFit):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::FloatingObjectHashFunctions::hash):
    (WebCore::RenderBlock::FloatingObjectHashFunctions::equal):
    (WebCore::RenderBlock::FloatingObjectHashTranslator::hash):
    (WebCore::RenderBlock::FloatingObjectHashTranslator::equal):
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::layoutInlineChildren):
    (WebCore::RenderBlock::matchedEndLine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/ListHashSet.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/ListHashSet.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2005, 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, Benjamin Poulain <ikipou@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -91,9 +92,23 @@ namespace WTF {
         const_iterator begin() const;
         const_iterator end() const;
 
+        ValueType& first();
+        const ValueType& first() const;
+
+        ValueType& last();
+        const ValueType& last() const;
+        void removeLast();
+
         iterator find(const ValueType&);
         const_iterator find(const ValueType&) const;
         bool contains(const ValueType&) const;
+
+        // An alternate version of find() that finds the object by hashing and comparing
+        // with some other type, to avoid the cost of type conversion.
+        // The HashTranslator interface is defined in HashSet.
+        template<typename T, typename HashTranslator> iterator find(const T&);
+        template<typename T, typename HashTranslator> const_iterator find(const T&) const;
+        template<typename T, typename HashTranslator> bool contains(const T&) const;
 
         // the return value is a pair of an iterator to the new value's location, 
         // and a bool that is true if an new entry was added
@@ -443,6 +458,42 @@ namespace WTF {
     }
 
     template<typename T, size_t inlineCapacity, typename U>
+    inline T& ListHashSet<T, inlineCapacity, U>::first()
+    {
+        ASSERT(!isEmpty());
+        return m_head->m_value;
+    }
+
+    template<typename T, size_t inlineCapacity, typename U>
+    inline const T& ListHashSet<T, inlineCapacity, U>::first() const
+    {
+        ASSERT(!isEmpty());
+        return m_head->m_value;
+    }
+
+    template<typename T, size_t inlineCapacity, typename U>
+    inline T& ListHashSet<T, inlineCapacity, U>::last()
+    {
+        ASSERT(!isEmpty());
+        return m_tail->m_value;
+    }
+
+    template<typename T, size_t inlineCapacity, typename U>
+    inline const T& ListHashSet<T, inlineCapacity, U>::last() const
+    {
+        ASSERT(!isEmpty());
+        return m_tail->m_value;
+    }
+
+    template<typename T, size_t inlineCapacity, typename U>
+    inline void ListHashSet<T, inlineCapacity, U>::removeLast()
+    {
+        ASSERT(!isEmpty());
+        m_impl.remove(m_tail);
+        unlinkAndDelete(m_tail);
+    }
+
+    template<typename T, size_t inlineCapacity, typename U>
     inline typename ListHashSet<T, inlineCapacity, U>::iterator ListHashSet<T, inlineCapacity, U>::find(const ValueType& value)
     {
         typedef ListHashSetTranslator<ValueType, inlineCapacity, HashFunctions> Translator;
@@ -460,6 +511,45 @@ namespace WTF {
         if (it == m_impl.end())
             return end();
         return makeConstIterator(*it);
+    }
+
+    template<typename ValueType, size_t inlineCapacity, typename T, typename Translator>
+    struct ListHashSetTranslatorAdapter {
+    private:
+        typedef ListHashSetNode<ValueType, inlineCapacity> Node;
+    public:
+        static unsigned hash(const T& key) { return Translator::hash(key); }
+        static bool equal(Node* const& a, const T& b) { return Translator::equal(a->m_value, b); }
+    };
+
+    template<typename ValueType, size_t inlineCapacity, typename U>
+    template<typename T, typename HashTranslator>
+    inline typename ListHashSet<ValueType, inlineCapacity, U>::iterator ListHashSet<ValueType, inlineCapacity, U>::find(const T& value)
+    {
+        typedef ListHashSetTranslatorAdapter<ValueType, inlineCapacity, T, HashTranslator> Adapter;
+        ImplTypeConstIterator it = m_impl.template find<T, Adapter>(value);
+        if (it == m_impl.end())
+            return end();
+        return makeIterator(*it);
+    }
+
+    template<typename ValueType, size_t inlineCapacity, typename U>
+    template<typename T, typename HashTranslator>
+    inline typename ListHashSet<ValueType, inlineCapacity, U>::const_iterator ListHashSet<ValueType, inlineCapacity, U>::find(const T& value) const
+    {
+        typedef ListHashSetTranslatorAdapter<ValueType, inlineCapacity, T, HashTranslator> Adapter;
+        ImplTypeConstIterator it = m_impl.template find<T, Adapter>(value);
+        if (it == m_impl.end())
+            return end();
+        return makeConstIterator(*it);
+    }
+
+    template<typename ValueType, size_t inlineCapacity, typename U>
+    template<typename T, typename HashTranslator>
+    inline bool ListHashSet<ValueType, inlineCapacity, U>::contains(const T& value) const
+    {
+        typedef ListHashSetTranslatorAdapter<ValueType, inlineCapacity, T, HashTranslator> Adapter;
+        return m_impl.template contains<T, Adapter>(value);
     }
 
     template<typename T, size_t inlineCapacity, typename U>
