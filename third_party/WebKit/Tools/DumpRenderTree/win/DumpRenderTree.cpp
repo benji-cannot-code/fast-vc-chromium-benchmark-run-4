15fdf67d64c9932b1ee0ce3715df94783c2630d6
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-27 21:16:50 +0000

    Make ResourceLoadDelegate print URLs relative to the main resource on Windows
    This matches Mac. (In fact, the code was ported from Mac!)
    
    Fixes <http://webkit.org/b/55328> URLs printed by ResourceLoadDelegate on Windows don't
    match Mac
    
    Reviewed by Anders Carlsson.
    
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (substringFromIndex): Added. Emulates -[NSString substringFromIndex:].
    (urlSuitableForTestResult): Ported code from -[NSURL(DRTExtras)
    _drt_descriptionSuitableForTestResult].
    (cfStringRefToWString): Moved here from LayoutTestControllerWin.cpp.
    
    * DumpRenderTree/win/DumpRenderTreeWin.h: Added declaration of cfStringRefToWString.
    
    * DumpRenderTree/win/LayoutTestControllerWin.cpp: Moved cfStringRefToWString to
    DumpRenderTree.cpp.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/win/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkQueueItem.h"
 #include "WorkQueue.h"
 
+#include <comutil.h>
 #include <fcntl.h>
 #include <io.h>
 #include <math.h>
@@ -132,12 +133,40 @@ bool setAlwaysAcceptCookies(bool alwaysAcceptCookies)
 #endif
 }
 
-wstring urlSuitableForTestResult(const wstring& url)
+static RetainPtr<CFStringRef> substringFromIndex(CFStringRef string, CFIndex index)
 {
-    if (url.find(L"file://") == wstring::npos)
-        return url;
+    return RetainPtr<CFStringRef>(AdoptCF, CFStringCreateWithSubstring(kCFAllocatorDefault, string, CFRangeMake(index, CFStringGetLength(string) - index)));
+}
 
-    return lastPathComponent(url);
+wstring urlSuitableForTestResult(const wstring& urlString)
+{
+    RetainPtr<CFURLRef> url(AdoptCF, CFURLCreateWithBytes(kCFAllocatorDefault, reinterpret_cast<const UInt8*>(urlString.c_str()), urlString.length() * sizeof(wstring::value_type), kCFStringEncodingUTF16, 0));
+
+    RetainPtr<CFStringRef> scheme(AdoptCF, CFURLCopyScheme(url.get()));
+    if (scheme && CFStringCompare(scheme.get(), CFSTR("file"), kCFCompareCaseInsensitive) != kCFCompareEqualTo)
+        return urlString;
+
+    COMPtr<IWebDataSource> dataSource;
+    if (FAILED(frame->dataSource(&dataSource))) {
+        if (FAILED(frame->provisionalDataSource(&dataSource)))
+            return urlString;
+    }
+
+    COMPtr<IWebMutableURLRequest> request;
+    if (FAILED(dataSource->request(&request)))
+        return urlString;
+
+    _bstr_t requestURLString;
+    if (FAILED(request->URL(requestURLString.GetAddress())))
+        return urlString;
+
+    RetainPtr<CFURLRef> requestURL(AdoptCF, CFURLCreateWithBytes(kCFAllocatorDefault, reinterpret_cast<const UInt8*>(requestURLString.GetBSTR()), requestURLString.length() * sizeof(OLECHAR), kCFStringEncodingUTF16, 0));
+    RetainPtr<CFURLRef> baseURL(AdoptCF, CFURLCreateCopyDeletingLastPathComponent(kCFAllocatorDefault, requestURL.get()));
+
+    RetainPtr<CFStringRef> basePath(AdoptCF, CFURLCopyPath(baseURL.get()));
+    RetainPtr<CFStringRef> path(AdoptCF, CFURLCopyPath(url.get()));
+
+    return cfStringRefToWString(substringFromIndex(path.get(), CFStringGetLength(basePath.get())).get());
 }
 
 wstring lastPathComponent(const wstring& url)
@@ -167,6 +196,14 @@ string toUTF8(BSTR bstr)
 string toUTF8(const wstring& wideString)
 {
     return toUTF8(wideString.c_str(), wideString.length());
+}
+
+wstring cfStringRefToWString(CFStringRef cfStr)
+{
+    Vector<wchar_t> v(CFStringGetLength(cfStr));
+    CFStringGetCharacters(cfStr, CFRangeMake(0, CFStringGetLength(cfStr)), (UniChar *)v.data());
+
+    return wstring(v.data(), v.size());
 }
 
 static LRESULT CALLBACK DumpRenderTreeWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
