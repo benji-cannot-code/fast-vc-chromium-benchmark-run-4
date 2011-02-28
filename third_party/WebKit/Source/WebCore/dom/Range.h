76e255b5ee7572bc3006461939d4c745f476ac5b
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 13:24:25 +0000

    2011-02-28  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Kent Tamura.
    
            Range::processContents needs cleanup
            https://bugs.webkit.org/show_bug.cgi?id=51006
    
            Refactored Range::processContents.  Extracted childOfCommonRootBeforeOffset from processContents
            which is used to find processStart and processEnd respectively.  In the case of processStart,
            we use the next sibling of the node returned by childOfCommonRootBeforeOffset when m_start is not
            the common root because copying m_start's ancestors will result in processing too much contents.
    
            Also extracted processNodes and deleteCharacterData from processContents and processContentsBetweenOffsets.
    
            In addition, lengthOfContentsInNode was modified to return the correct length instead of
            numeric_limits<unsigned>::max() because the convention that processContentsBetweenOffsets automatically
            corrects the length when endOffset is numeric_limits<unsigned>::max() seemed more confusing than
            having two switch statements that need to be consistent.
    
            Historically, lengthOfContentsInNode was introduced in r78413 as a build fix because unsigned const
            LengthOfContentsInNode added in r78409 violated WebKit C++ rules and caused build failures on Mac and
            other ports.
    
            * dom/Range.cpp:
            (WebCore::childOfCommonRootBeforeOffset): Extracted from processContents.
            (WebCore::lengthOfContentsInNode): Added.
            (WebCore::Range::processContents): Calls childOfCommonRootBeforeOffset, lengthOfContentsInNode,
            and processNodes.
            (WebCore::deleteCharacterData): Added.
            (WebCore::Range::processContentsBetweenOffsets): Calls deleteCharacterData and processNodes.
            (WebCore::Range::processNodes): Extracted from processContents and processContentsBetweenOffsets.
            (WebCore::Range::processAncestorsAndTheirSiblings):
            * dom/Range.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Range.h
+++ third_party/WebKit/Source/WebCore/dom/Range.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RangeBoundaryPoint.h"
 #include <wtf/Forward.h>
 #include <wtf/RefCounted.h>
+#include <wtf/Vector.h>
 
 namespace WebCore {
 
@@ -149,6 +150,7 @@ private:
     enum ActionType { DELETE_CONTENTS, EXTRACT_CONTENTS, CLONE_CONTENTS };
     PassRefPtr<DocumentFragment> processContents(ActionType, ExceptionCode&);
     static PassRefPtr<Node> processContentsBetweenOffsets(ActionType, PassRefPtr<DocumentFragment>, Node*, unsigned startOffset, unsigned endOffset, ExceptionCode&);
+    static void processNodes(ActionType, Vector<RefPtr<Node> >&, PassRefPtr<Node> oldContainer, PassRefPtr<Node> newContainer, ExceptionCode&);
     enum ContentsProcessDirection { ProcessContentsForward, ProcessContentsBackward };
     static PassRefPtr<Node> processAncestorsAndTheirSiblings(ActionType, Node* container, ContentsProcessDirection, PassRefPtr<Node> clonedContainer, Node* commonRoot, ExceptionCode&);
 
