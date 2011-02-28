25d0cb7ef43c5b1cf73b44e11aea98addf48ebe3
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 17:51:03 +0000

    2011-02-28  Andreas Kling  <kling@webkit.org>
            Reviewed by Darin Adler.
    
            Use Frame::ownerElement() directly where appropriate.
            https://bugs.webkit.org/show_bug.cgi?id=55385
    
            Don't take the roundabout way through frame->document->ownerElement
            which just checks that the document->frame is non-null.
    
            No new test, refactoring only.
    
            * editing/SelectionController.cpp:
            (WebCore::SelectionController::selectFrameElementInParentIfFullySelected):
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::loadWithDocumentLoader):
            * page/DOMWindow.cpp:
            (WebCore::DOMWindow::dispatchLoadEvent):
            * page/EventHandler.cpp:
            (WebCore::EventHandler::scrollRecursively):
            (WebCore::EventHandler::logicalScrollRecursively):
            * page/FrameView.cpp:
            (WebCore::FrameView::init):
            (WebCore::FrameView::layout):
            (WebCore::FrameView::repaintContentRectangle):
            (WebCore::FrameView::windowClipRect):
            (WebCore::FrameView::paintContents):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -1039,7 +1039,7 @@ bool EventHandler::scrollRecursively(ScrollDirection direction, ScrollGranularit
     frame = frame->tree()->parent();
     if (!frame)
         return false;
-    return frame->eventHandler()->scrollRecursively(direction, granularity, m_frame->document()->ownerElement());
+    return frame->eventHandler()->scrollRecursively(direction, granularity, m_frame->ownerElement());
 }
 
 bool EventHandler::logicalScrollRecursively(ScrollLogicalDirection direction, ScrollGranularity granularity, Node* startingNode)
@@ -1068,7 +1068,7 @@ bool EventHandler::logicalScrollRecursively(ScrollLogicalDirection direction, Sc
     if (!frame)
         return false;
 
-    return frame->eventHandler()->logicalScrollRecursively(direction, granularity, m_frame->document()->ownerElement());
+    return frame->eventHandler()->logicalScrollRecursively(direction, granularity, m_frame->ownerElement());
 }
 
 IntPoint EventHandler::currentMousePosition() const
