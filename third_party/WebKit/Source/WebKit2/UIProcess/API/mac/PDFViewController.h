417ba33c60a99617b68e35f55bd0a316c7b47924
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 23:55:34 +0000

    2011-02-28  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein, Sam Weinig, Alexey Proskuryakov and Darin Adler.
    
            Open PDF in Preview doesn't work
            https://bugs.webkit.org/show_bug.cgi?id=55400
            <rdar://problem/8750353>
    
            * UIProcess/API/mac/PDFViewController.mm:
            (-[WKPDFView initWithFrame:PDFViewController:]):
            Set the WKPDFView as the delegate of itself.
    
            (-[WKPDFView PDFViewOpenPDFInNativeApplication:]):
            Call PDFViewController::openPDFInFinder.
    
            (WebKit::PDFViewController::PDFViewController):
            Initialize m_hasWrittenPDFToDisk to false.
    
            (WebKit::PDFViewController::openPDFInFinder):
            Write the PDF to disk if needed and then open it using -[NSWorkspace openFile:].
            
            (WebKit::temporaryPDFDirectoryPath):
            Create a directory to put PDFs in.
    
            (WebKit::PDFViewController::pathToPDFOnDisk):
            Return a path to a (non-existent) file in the temporary PDF directory.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/PDFViewController.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/PDFViewController.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -58,12 +58,15 @@ public:
     static Class pdfPreviewViewClass();
 
     NSPrintOperation *makePrintOperation(NSPrintInfo *);
-    
+    void openPDFInFinder();
+
 private:
     explicit PDFViewController(WKView *wkView);
 
     static Class pdfDocumentClass();
     static NSBundle* pdfKitBundle();
+
+    NSString *pathToPDFOnDisk();
 
     WKView* m_wkView;
 
@@ -72,6 +75,9 @@ private:
 
     RetainPtr<NSString> m_suggestedFilename;
     RetainPtr<CFDataRef> m_pdfData;
+
+    RetainPtr<NSString> m_pathToPDFOnDisk;
+    bool m_hasWrittenPDFToDisk;
 };
 
 } // namespace WebKit
