e238596329132b1fad74fa08cb1f4956d6e773e3
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 11:21:20 +0000

    2011-02-28  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: add first network test, improve harness.
            https://bugs.webkit.org/show_bug.cgi?id=55306
    
            * http/tests/inspector/inspector-test.js:
            (initialize_InspectorTest.InspectorTest.assertGreaterOrEqual):
            * http/tests/inspector/network/network-size-expected.txt: Added.
            * http/tests/inspector/network/network-size.html: Added.
            * http/tests/inspector/network/network-timing-expected.txt: Added.
            * http/tests/inspector/network/network-timing.html: Added.
            * http/tests/inspector/network/resources/resource.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79849 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
@@ -75,6 +75,8 @@ InspectorTest.addResult = function(text)
     }
 }
 
+console.error = InspectorTest.addResult;
+
 InspectorTest.addResults = function(textArray)
 {
     if (!textArray)
@@ -105,6 +107,12 @@ InspectorTest.addObject = function(object, nondeterministicProps, prefix, firstL
             InspectorTest.addResult(prefixWithName + propValue);
     }
     InspectorTest.addResult(prefix + "}");
+}
+
+InspectorTest.assertGreaterOrEqual = function(expected, actual, message)
+{
+    if (actual < expected)
+        InspectorTest.addResult("FAILED: " + (message ? message + ": " : "") + actual + " < " + expected);
 }
 
 InspectorTest.reloadPage = function(callback)
@@ -258,11 +266,17 @@ function runTest()
                 initializationFunctions[i]();
             } catch (e) {
                 console.error("Exception in test initialization: " + e);
+                InspectorTest.completeTest();
             }
         }
 
         WebInspector.showPanel("elements");
-        testFunction();
+        try {
+            testFunction();
+        } catch (e) {
+            console.error("Exception during test execution: " + e);
+            InspectorTest.completeTest();
+        }
     }
 
     var initializationFunctions = [];
@@ -273,12 +287,28 @@ function runTest()
     var parameters = ["[" + initializationFunctions + "]", test, completeTestCallId];
     var toEvaluate = "(" + runTestInFrontend + ")(" + parameters.join(", ") + ");";
     layoutTestController.evaluateInWebInspector(runTestCallId, toEvaluate);
+
+    function watchDog()
+    {
+        console.log("Internal watchdog triggered at 10 seconds. Test timed out.");
+        closeInspectorAndNotifyDone();
+    }
+    window._watchDogTimer = setTimeout(watchDog, 10000);
 }
 
 function didEvaluateForTestInFrontend(callId)
 {
     if (callId !== completeTestCallId)
         return;
+    delete window.completeTestCallId;
+    closeInspectorAndNotifyDone();
+}
+
+function closeInspectorAndNotifyDone()
+{
+    if (window._watchDogTimer)
+        clearTimeout(window._watchDogTimer);
+
     layoutTestController.closeWebInspector();
     setTimeout(function() {
         layoutTestController.notifyDone();
