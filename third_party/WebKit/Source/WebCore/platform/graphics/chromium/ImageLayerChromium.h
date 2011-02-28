e85d389cc36f53ad436b96514fe13846d79898dc
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 18:03:29 +0000

    2011-02-25  Adrienne Walker  <enne@google.com>
            Reviewed by James Robinson.
    
            [chromium] Abstract "pixels with a graphics context" into its own class
            https://bugs.webkit.org/show_bug.cgi?id=55259
    
            This creates new PlatformCanvas/PlatformImage classes which wrap
            all of the #ifdef Skia/Cg warts from the compositor.  All classes
            (LayerTilerChromium, ContentLayerChromium, and ImageLayerChromium) are
            modified to use these abstractions.
    
            Tests: LayoutTests/compositing
    
            * WebCore.gypi:
            * platform/graphics/chromium/ContentLayerChromium.cpp:
            (WebCore::ContentLayerChromium::updateContentsIfDirty):
            (WebCore::ContentLayerChromium::resizeUploadBuffer):
            (WebCore::ContentLayerChromium::updateTextureIfNeeded):
            (WebCore::ContentLayerChromium::updateTexture):
            (WebCore::ContentLayerChromium::draw):
            * platform/graphics/chromium/ContentLayerChromium.h:
            * platform/graphics/chromium/ImageLayerChromium.cpp:
            (WebCore::ImageLayerChromium::updateContentsIfDirty):
            (WebCore::ImageLayerChromium::updateTextureIfNeeded):
            * platform/graphics/chromium/ImageLayerChromium.h:
            * platform/graphics/chromium/LayerTilerChromium.cpp:
            (WebCore::LayerTilerChromium::contentRectToTileIndices):
            (WebCore::LayerTilerChromium::update):
            (WebCore::LayerTilerChromium::updateFromPixels):
            * platform/graphics/chromium/LayerTilerChromium.h:
            * platform/graphics/chromium/PlatformCanvas.cpp: Added.
            (WebCore::PlatformCanvas::PlatformCanvas):
            (WebCore::PlatformCanvas::~PlatformCanvas):
            (WebCore::PlatformCanvas::resize):
            (WebCore::PlatformCanvas::AutoLocker::AutoLocker):
            (WebCore::PlatformCanvas::AutoLocker::~AutoLocker):
            (WebCore::PlatformCanvas::Painter::Painter):
            (WebCore::PlatformCanvas::Painter::~Painter):
            * platform/graphics/chromium/PlatformCanvas.h: Added.
            (WebCore::PlatformCanvas::AutoLocker::pixels):
            (WebCore::PlatformCanvas::Painter::context):
            (WebCore::PlatformCanvas::size):
            * platform/graphics/chromium/PlatformImage.cpp: Copied from Source/WebCore/platform/graphics/chromium/ImageLayerChromium.cpp.
            (WebCore::PlatformImage::PlatformImage):
            (WebCore::PlatformImage::updateFromImage):
            * platform/graphics/chromium/PlatformImage.h: Added.
            (WebCore::PlatformImage::pixels):
            (WebCore::PlatformImage::size):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "ContentLayerChromium.h"
+#include "PlatformImage.h"
 
 #if PLATFORM(CG)
 #include <wtf/RetainPtr.h>
@@ -56,8 +57,11 @@ public:
     void setContents(Image* image);
 
 private:
+    virtual void updateTextureIfNeeded();
+
     ImageLayerChromium(GraphicsLayerChromium* owner);
 
+    PlatformImage m_decodedImage;
     RefPtr<Image> m_contents;
 };
 
