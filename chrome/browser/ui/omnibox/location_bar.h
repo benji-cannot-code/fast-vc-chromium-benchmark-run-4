31511887ad61669185681290619d7ee1c4f2b74d
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-28 22:02:47 +0000

    Makes instant not load the current url if you're already viewing. This situation happens if you focus the omnibox and press the down arrow. As part of this I consolidated the code for upading instant to the LocationBar. I didn't want to put this in the instant code as I'm trying to make instant know as little as possible about autocomplete.
    BUG=73407
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6597023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/location_bar.h
+++ chrome/browser/ui/omnibox/location_bar.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,8 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class AutocompleteEditView;
 class ExtensionAction;
+class InstantController;
 class LocationBarTesting;
 class TabContents;
+class TabContentsWrapper;
 
 class LocationBar {
  public:
@@ -81,6 +83,12 @@ class LocationBar {
 
  protected:
   virtual ~LocationBar() {}
+
+  // Updates instant in response to the text possibly changing in the edit.
+  static void UpdateInstant(InstantController* instant,
+                            TabContentsWrapper* tab,
+                            AutocompleteEditView* edit,
+                            string16* suggested_text);
 };
 
 class LocationBarTesting {
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2760,6 +2760,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/login/login_prompt_mac.h',
         'browser/ui/login/login_prompt_mac.mm',
         'browser/ui/login/login_prompt_win.cc',
+        'browser/ui/omnibox/location_bar.cc',
         'browser/ui/omnibox/location_bar.h',
         'browser/ui/omnibox/location_bar_util.cc',
         'browser/ui/omnibox/location_bar_util.h',
