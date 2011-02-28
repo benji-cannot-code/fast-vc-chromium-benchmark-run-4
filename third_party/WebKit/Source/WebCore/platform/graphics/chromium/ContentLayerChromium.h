45462386f3609701d2acc635c8edee2d16db15ed
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-02-28 18:30:18 +0000

    2011-02-28  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r79877.
            http://trac.webkit.org/changeset/79877
            https://bugs.webkit.org/show_bug.cgi?id=55388
    
            Breaks chromium build (Requested by antonm_ on #webkit).
    
            * WebCore.gypi:
            * platform/graphics/chromium/ContentLayerChromium.cpp:
            (WebCore::ContentLayerChromium::updateContentsIfDirty):
            (WebCore::ContentLayerChromium::resizeUploadBufferForImage):
            (WebCore::ContentLayerChromium::resizeUploadBuffer):
            (WebCore::SkBitmapConditionalAutoLockerPixels::SkBitmapConditionalAutoLockerPixels):
            (WebCore::SkBitmapConditionalAutoLockerPixels::~SkBitmapConditionalAutoLockerPixels):
            (WebCore::SkBitmapConditionalAutoLockerPixels::lockPixels):
            (WebCore::ContentLayerChromium::updateTextureIfNeeded):
            (WebCore::ContentLayerChromium::draw):
            * platform/graphics/chromium/ContentLayerChromium.h:
            * platform/graphics/chromium/ImageLayerChromium.cpp:
            (WebCore::ImageLayerChromium::updateContentsIfDirty):
            * platform/graphics/chromium/ImageLayerChromium.h:
            * platform/graphics/chromium/LayerTilerChromium.cpp:
            (WebCore::LayerTilerChromium::contentRectToTileIndices):
            (WebCore::LayerTilerChromium::update):
            * platform/graphics/chromium/LayerTilerChromium.h:
            * platform/graphics/chromium/PlatformCanvas.cpp: Removed.
            * platform/graphics/chromium/PlatformCanvas.h: Removed.
            * platform/graphics/chromium/PlatformImage.cpp: Removed.
            * platform/graphics/chromium/PlatformImage.h: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "LayerChromium.h"
-#include "PlatformCanvas.h"
 #include "TextureManager.h"
 
 namespace WebCore {
@@ -66,18 +65,25 @@ protected:
     virtual void cleanupResources();
     bool requiresClippedUpdateRect() const;
     void resizeUploadBuffer(const IntSize&);
+    void resizeUploadBufferForImage(const IntSize&);
 
     OwnPtr<LayerTexture> m_contentsTexture;
     bool m_skipsDraw;
 
+    // The size of the upload buffer (also the size of our backing texture).
+    IntSize m_uploadBufferSize;
     // The portion of the upload buffer that has a pending update, in the coordinates of the texture.
     IntRect m_uploadUpdateRect;
-
-    virtual void updateTextureIfNeeded();
-    void updateTexture(const uint8_t* pixels, const IntSize&);
+    // On platforms using Skia, we use the skia::PlatformCanvas for content layers
+    // and the m_uploadPixelData buffer for image layers.
+    // FIXME: We should be using memory we control for all uploads.
+#if USE(SKIA)
+    OwnPtr<skia::PlatformCanvas> m_uploadPixelCanvas;
+#endif
+    OwnPtr<Vector<uint8_t> > m_uploadPixelData;
 
 private:
-    PlatformCanvas m_canvas;
+    void updateTextureIfNeeded();
 
     IntRect m_visibleRectInLayerCoords;
     FloatPoint m_layerCenterInSurfaceCoords;
--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "ContentLayerChromium.h"
-#include "PlatformImage.h"
 
 #if PLATFORM(CG)
 #include <wtf/RetainPtr.h>
@@ -57,11 +56,8 @@ public:
     void setContents(Image* image);
 
 private:
-    virtual void updateTextureIfNeeded();
-
     ImageLayerChromium(GraphicsLayerChromium* owner);
 
-    PlatformImage m_decodedImage;
     RefPtr<Image> m_contents;
 };
 
