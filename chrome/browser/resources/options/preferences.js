63a93fb8e800702252b2c18553dfa7ccc4995d11
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-28 22:59:44 +0000

    web-ui settings: Remove the getPref() method.  It was not used and as implemented, kinda broken.
    BUG=none
    Review URL: http://codereview.chromium.org/6588065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/preferences.js
+++ chrome/browser/resources/options/preferences.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,33 +18,13 @@ cr.define('options', function() {
   cr.addSingletonGetter(Preferences);
 
   /**
-   * Extracts preference value.
-   * @param {Object} dict Map of preference values passed to fetchPrefs
-   *     callback.
-   * @param {string} name Preference name.
-   * @return preference value.
-   */
-  Preferences.getPref = function (dict, name) {
-    var parts = name.split('.');
-    var cur = dict;
-    for (var part; part = parts.shift(); ) {
-      if (cur[part]) {
-        cur = cur[part];
-      } else {
-        return null;
-      }
-    }
-    return cur;
-  };
-
-  /**
    * Sets value of a boolean preference.
    * and signals its changed value.
    * @param {string} name Preference name.
    * @param {boolean} value New preference value.
    * @param {string} metric User metrics identifier.
    */
-  Preferences.setBooleanPref = function (name, value, metric) {
+  Preferences.setBooleanPref = function(name, value, metric) {
     var argumentList = [name, Boolean(value)];
     if (metric != undefined) argumentList.push(metric);
     chrome.send('setBooleanPref', argumentList);
