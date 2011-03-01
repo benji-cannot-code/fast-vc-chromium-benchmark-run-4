508bb9bfab6d6f8faaecf13ca6b7d33a9f07847a
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-01 01:51:45 +0000

    Number type input cannot be set to empty string if it has an initial value https://bugs.webkit.org/show_bug.cgi?id=53744
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    We need to distinguish null strings and empty strings for
    InputElementData::value. So InputType::sanitizeValue() also needs to
    take care of it.
    
    * dom/InputElement.h: Add a comment to InputElementData::value and
      setValue about null strings.
    * html/InputType.h: Add a comment to sanitizeValue about null strings.
    * html/NumberInputType.cpp:
    (WebCore::NumberInputType::sanitizeValue): Returns a null string if the
      proposed value is a null string. Returning an empty string if the
      proposed value is not a number.
    
    LayoutTests:
    
    * fast/forms/input-value-sanitization-expected.txt:
    * fast/forms/script-tests/input-value-sanitization.js: Add some tests.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/forms/script-tests/input-value-sanitization.js
+++ third_party/WebKit/LayoutTests/fast/forms/script-tests/input-value-sanitization.js
@@ -1,11 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description('Tests for value sanitization algorithm.');
 
-var input = document.createElement('input');
+var input;
 
+debug('');
+debug('Number:');
+input = document.createElement('input');
+input.setAttribute('value', '65536');
+input.type = 'number';
+shouldBe('input.value', '"65536"');
+shouldBe('input.value = "256"; input.value', '"256"');
+shouldBe('input.value = ""; input.value', '""');
+
+
+debug('');
+debug('Range:');
+input = document.createElement('input');
 input.type = 'text';
 input.value = ':)';
-
 input.type = 'range';
 shouldBe('input.value', '"50"');
 
