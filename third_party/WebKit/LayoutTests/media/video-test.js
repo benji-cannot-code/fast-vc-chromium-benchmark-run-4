0904932dd09ff332630d46d2a340359957780a39
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-01 01:31:35 +0000

    2011-02-28  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Darin Adler.
    
            Fix race condition in 4 media tests
            https://bugs.webkit.org/show_bug.cgi?id=55414
    
            The parser may yield and give events a chance to dispatch between any two tokens. So
            it is possible that the loadstart event fires immediately after the video tag is
            inserted into the document but before the subsequent script is parsed. This is very
            rare in the layout test environment since these small files are loaded from disk.
    
            This patch eliminates that race condition by allowing waitForEvent to register for
            events that bubble up to the document from any element. Then the registration can
            happen before before the element is inserted into the document.
    
            * media/video-source-media.html:
            * media/video-source-none-supported.html:
            * media/video-source-type-params.html:
            * media/video-source-type.html:
            * media/video-test.js:
            (waitForEvent):
            (waitForEventAndTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@79949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/media/video-test.js
+++ third_party/WebKit/LayoutTests/media/video-test.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var video = null;
-var mediaElement = null;
+var mediaElement = document; // If not set, an event from any element will trigger a waitForEvent() callback.
 var console = null;
 var printFullTestDetails = true; // This is optionaly switched of by test whose tested values can differ. (see disableFullTestDetailsPrinting())
 var Failed = false;
@@ -141,7 +141,7 @@ function waitForEvent(eventName, func, endit)
             endTest();
     }
 
-    mediaElement.addEventListener(eventName, _eventCallback);
+    mediaElement.addEventListener(eventName, _eventCallback, true);
 }
 
 function waitForEventTestAndEnd(eventName, testFuncString)
@@ -163,7 +163,7 @@ function waitForEventAndTest(eventName, testFuncString, endit)
             endTest();
     }
 
-    mediaElement.addEventListener(eventName, _eventCallback);
+    mediaElement.addEventListener(eventName, _eventCallback, true);
 }
 
 function testException(testString, exceptionString)
