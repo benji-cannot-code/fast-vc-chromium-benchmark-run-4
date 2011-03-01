29960ef455035dca224167c7dda9a7be5a4c7ba2
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-01 22:26:48 +0000

    Don't repaint tab when TabRenderData hasn't changed.  We are always repainting when new TabData is set even if there is no change.  This CL reduces tab re-paints by 10~20% on cr48. With 4 tabs, its about 1~1.5seconds reduction.
    BUG=none
    TEST=This should not change any behavior except for better startup time. I also tested dynamic favicon (http://ajaxify.com/run/favicon).
    
    Review URL: http://codereview.chromium.org/6599017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/base_tab.cc
+++ chrome/browser/ui/views/tabs/base_tab.cc
@@ -177,6 +177,9 @@ BaseTab::~BaseTab() {
 }
 
 void BaseTab::SetData(const TabRendererData& data) {
+  if (data_.Equals(data))
+    return;
+
   TabRendererData old(data_);
   data_ = data;
 
@@ -216,6 +219,7 @@ void BaseTab::SetData(const TabRendererData& data) {
   DataChanged(old);
 
   Layout();
+  SchedulePaint();
 }
 
 void BaseTab::UpdateLoadingAnimation(TabRendererData::NetworkState state) {
