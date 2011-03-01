7270444e82f4e443a85315d635ce2b9aa2a95701
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-01 23:18:33 +0000

    Makes instant show SSL error page. Because these end up with a different RenderViewHost I wasn't seeing the paint, and wasn't showing the preview. I've plumbed through notification so that I can detect this case.
    I'm writing the test for this separately.
    
    BUG=74085
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6597056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76458 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/interstitial_page.cc
+++ content/browser/tab_contents/interstitial_page.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/bindings_policy.h"
 #include "chrome/common/dom_storage_common.h"
 #include "chrome/common/net/url_request_context_getter.h"
+#include "chrome/common/notification_service.h"
 #include "chrome/common/notification_source.h"
 #include "content/browser/browser_thread.h"
 #include "content/browser/renderer_host/render_process_host.h"
@@ -355,6 +356,11 @@ void InterstitialPage::DidNavigate(
   // The RenderViewHost has loaded its contents, we can show it now.
   render_view_host_->view()->Show();
   tab_->set_interstitial_page(this);
+
+  NotificationService::current()->Notify(
+      NotificationType::INTERSTITIAL_ATTACHED,
+      Source<TabContents>(tab_),
+      NotificationService::NoDetails());
 
   RenderWidgetHostView* rwh_view = tab_->render_view_host()->view();
 
