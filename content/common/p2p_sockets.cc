41c95072c42a4e6a13cb83db1ed56c500e8ccc68
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 21:47:40 +0000

    Fix build break on clang bot
    BUG=None
    TEST=no clang warnings
    TBR=erg@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76622 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/p2p_sockets.cc
+++ content/common/p2p_sockets.cc
@@ -7,3 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 P2PSocketAddress::P2PSocketAddress() : port(0) {
 }
+
+P2PSocketAddress::~P2PSocketAddress() {
+}
--- content/common/p2p_sockets.h
+++ content/common/p2p_sockets.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Pair of IP address and port number.
 struct P2PSocketAddress {
   P2PSocketAddress();
+  ~P2PSocketAddress();
   net::IPAddressNumber address;
   int port;
 };
