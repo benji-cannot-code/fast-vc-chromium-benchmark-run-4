548d9107466fe0210a6828a0e48c7f2caafd2e18
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 21:46:28 +0000

    Cleans up some of the appengine ui to make it prettier. Also fixes the "show offline servers" checkbox.
    BUG=NONE
    TEST=Bring up appengine UI and make sure the show offline servers checkbox works correctly.
    
    Review URL: http://codereview.chromium.org/6588107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76620 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/appengine/api.py
+++ remoting/client/appengine/api.py
@@ -30,9 +30,6 @@ class GetXmppTokenHandler(webapp.RequestHandler):
     except auth.NotAuthenticated:
       self.response.out.write('User has not authenticated')
       self.set_status(400)
-      return
-  pass
-
 
 class GetHostListHandler(webapp.RequestHandler):
   """Proxies the host-list handlers on the Chromoting directory."""
@@ -51,8 +48,9 @@ class GetHostListHandler(webapp.RequestHandler):
     except auth.NotAuthenticated:
       self.response.out.write('User has not authenticated')
       self.response.set_status(400)
-      return
-
+    except (gdata.client.Unauthorized, gdata.client.RequestError), inst:
+      self.response.out.write(inst.reason)
+      self.response.set_status(inst.status)
 
 def main():
   application = webapp.WSGIApplication(
