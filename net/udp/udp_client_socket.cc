93282e595985bbdf1e5fb9d5fe25e2d738066f19
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 23:18:28 +0000

    Fix clang build.
    BUG=none
    TEST=none
    TBR=erg
    
    Review URL: http://codereview.chromium.org/6588136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76650 0039d316-1c4b-4281-b951-d872f2087c98

--- net/udp/udp_client_socket.cc
+++ net/udp/udp_client_socket.cc
@@ -46,5 +46,12 @@ int UDPClientSocket::GetLocalAddress(AddressList* address) const {
   return socket_.GetLocalAddress(address);
 }
 
+bool UDPClientSocket::SetReceiveBufferSize(int32 size) {
+  return true;
+}
+
+bool UDPClientSocket::SetSendBufferSize(int32 size) {
+  return true;
+}
 
 }  // namespace net
