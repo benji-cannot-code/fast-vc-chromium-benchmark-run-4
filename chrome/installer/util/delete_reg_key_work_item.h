5e7b0726541ab03b2978e275111aea5871478444
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 01:37:37 +0000

    Add rollback support to DeleteRegKeyWorkItem.
    BUG=none
    TEST=new test added to installer_util_unittests.exe
    
    Review URL: http://codereview.chromium.org/6598065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/delete_reg_key_work_item.h
+++ chrome/installer/util/delete_reg_key_work_item.h
@@ -12,9 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/scoped_ptr.h"
 #include "chrome/installer/util/work_item.h"
 
-// A WorkItem subclass that deletes a registry key at the given path.
+// A WorkItem subclass that deletes a registry key at the given path.  Be aware
+// that in the event of rollback the key's values and subkeys are restored but
+// the key and its subkeys take on their default security descriptors.
 class DeleteRegKeyWorkItem : public WorkItem {
  public:
   virtual ~DeleteRegKeyWorkItem();
@@ -24,6 +27,7 @@ class DeleteRegKeyWorkItem : public WorkItem {
   virtual void Rollback();
 
  private:
+  class RegKeyBackup;
   friend class WorkItem;
 
   DeleteRegKeyWorkItem(HKEY predefined_root, const std::wstring& path);
@@ -35,10 +39,9 @@ class DeleteRegKeyWorkItem : public WorkItem {
   // Path of the key to be deleted.
   std::wstring path_;
 
-  // Path in the registry that the key is backed up to.
-  std::wstring backup_path_;
+  // Backup of the deleted key.
+  scoped_ptr<RegKeyBackup> backup_;
 
- private:
   DISALLOW_COPY_AND_ASSIGN(DeleteRegKeyWorkItem);
 };
 
