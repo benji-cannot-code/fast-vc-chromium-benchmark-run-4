0bfb12cf4ea98d4d7ce3ed0db3399db7e97bd03c
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 16:31:19 +0000

    Dont update title window for chromeos  Chromeos doesn't use them, but this takes 10ms to update the title string
    BUG=chromium-os:8632
    TEST=none
    
    Review URL: http://codereview.chromium.org/6574054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76548 0039d316-1c4b-4281-b951-d872f2087c98

--- views/window/window_gtk.cc
+++ views/window/window_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -209,6 +209,9 @@ void WindowGtk::EnableClose(bool enable) {
 }
 
 void WindowGtk::UpdateWindowTitle() {
+  // ChromeOS doesn't use a window title, so don't update them.
+#if !defined(OS_CHROMEOS)
+
   // If the non-client view is rendering its own title, it'll need to relayout
   // now.
   non_client_view_->Layout();
@@ -219,6 +222,7 @@ void WindowGtk::UpdateWindowTitle() {
   base::i18n::AdjustStringForLocaleDirection(&window_title);
 
   gtk_window_set_title(GetNativeWindow(), WideToUTF8(window_title).c_str());
+#endif
 }
 
 void WindowGtk::UpdateWindowIcon() {
