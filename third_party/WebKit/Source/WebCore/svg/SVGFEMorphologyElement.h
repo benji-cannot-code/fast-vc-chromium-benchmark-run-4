8aadc9429c037eca8667cc7d046dbebca9e62182
reni@webkit.org <reni@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-02 08:40:07 +0000

    FEMorphologyElement changes doesn't require relayout https://bugs.webkit.org/show_bug.cgi?id=55462
    Reviewed by Andreas Kling.
    
    When the FEMorphologyElement receives an update message but the given value remains the same we don't need
    to relayout the filter.
    
    No new tests are needed because this modification is covered by the dynamic update tests of FEMorphology.
    
    * platform/graphics/filters/FEMorphology.cpp:
    (WebCore::FEMorphology::setMorphologyOperator):
    (WebCore::FEMorphology::setRadiusX):
    (WebCore::FEMorphology::setRadiusY):
    * platform/graphics/filters/FEMorphology.h:
    * svg/SVGFEMorphologyElement.cpp:
    (WebCore::SVGFEMorphologyElement::setFilterEffectAttribute):
    (WebCore::SVGFEMorphologyElement::svgAttributeChanged):
    * svg/SVGFEMorphologyElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGFEMorphologyElement.h
+++ third_party/WebKit/Source/WebCore/svg/SVGFEMorphologyElement.h
@@ -39,6 +39,7 @@ private:
     SVGFEMorphologyElement(const QualifiedName&, Document*);
 
     virtual void parseMappedAttribute(Attribute*);
+    virtual bool setFilterEffectAttribute(FilterEffect*, const QualifiedName&);
     virtual void svgAttributeChanged(const QualifiedName&);
     virtual void synchronizeProperty(const QualifiedName&);
     virtual void fillAttributeToPropertyTypeMap();
