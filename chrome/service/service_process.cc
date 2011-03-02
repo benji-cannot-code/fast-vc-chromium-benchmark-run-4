c9f2a8569d555921451d18eb2efb43895aa5dc5e
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 21:23:13 +0000

    Service process fixes for Linux.
    Linux requires that the proxy_config_service be created on the I/O
    thread. Fortunately, ServiceURLRequestContextGetter was already thread safe ref
    counted, so we lift creation of that and the (also, fortunately) thread safe
    ref counted net::ProxyService up to service process initialization time. That
    created proxy service now gets passed in to the dynamic creation of the
    ServiceURLRequestContexts. Should have no effect on other platforms.
    
    BUG=74226
    TEST=Start Chromium on Linux with --enable-cloud-print-proxy, sign in, and examine ~/.config/chromium/Service State. With the fix, Service State properly contains the needed tokens to properly re-launch and be a proxy.
    
    Review URL: http://codereview.chromium.org/6602041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76612 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/service_process.cc
+++ chrome/service/service_process.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/service_process_util.h"
 #include "chrome/service/cloud_print/cloud_print_proxy.h"
+#include "chrome/service/net/service_url_request_context.h"
 #include "chrome/service/service_ipc_server.h"
 #include "chrome/service/service_process_prefs.h"
 #include "grit/chromium_strings.h"
@@ -141,6 +142,8 @@ bool ServiceProcess::Initialize(MessageLoopForUI* message_loop,
     Teardown();
     return false;
   }
+
+  request_context_getter_ = new ServiceURLRequestContextGetter();
 
   // See if we have been suppiled an LSID in the command line. This LSID will
   // override the credentials we use for Cloud Print.
@@ -305,6 +308,12 @@ void ServiceProcess::OnChromotingHostEnabled() {
 
 void ServiceProcess::OnChromotingHostDisabled() {
   OnServiceDisabled();
+}
+
+ServiceURLRequestContextGetter*
+ServiceProcess::GetServiceURLRequestContextGetter() {
+  DCHECK(request_context_getter_.get());
+  return request_context_getter_.get();
 }
 
 void ServiceProcess::OnServiceEnabled() {
