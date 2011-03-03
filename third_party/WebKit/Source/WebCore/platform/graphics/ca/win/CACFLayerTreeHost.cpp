9b7d2846e2b76bc835b7ebc5aaa3b1348e1235fb
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-03 19:51:57 +0000

    Yet another attempt at fixing the Windows build.
    * platform/graphics/ca/win/CACFLayerTreeHost.cpp:
    * platform/graphics/ca/win/LegacyCACFLayerTreeHost.cpp:
    * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.h:
    * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKCACFViewLayerTreeHost.h"
 #include "WebCoreInstanceHandle.h"
 #include <limits.h>
+#include <QuartzCore/CABase.h>
 #include <wtf/CurrentTime.h>
 #include <wtf/OwnArrayPtr.h>
 
