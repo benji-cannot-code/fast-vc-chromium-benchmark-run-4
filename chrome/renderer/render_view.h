cc9f38e9c07a67d92602ecf7d4a1760e01764b68
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 11:12:18 +0000

    P2P sockets IPC dispatcher for the renderer side.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6602039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76731 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.h
+++ chrome/renderer/render_view.h
@@ -72,6 +72,7 @@ class ListValue;
 class LoadProgressTracker;
 class NavigationState;
 class NotificationProvider;
+class P2PSocketDispatcher;
 class PepperDeviceTest;
 class PrintWebViewHelper;
 class RenderViewObserver;
@@ -1360,6 +1361,9 @@ class RenderView : public RenderWidget,
 
   // Responsible for sending page load related histograms.
   PageLoadHistograms page_load_histograms_;
+
+  // Dispatches all P2P socket used by the renderer.
+  P2PSocketDispatcher* p2p_socket_dispatcher_;
 
   // Misc ----------------------------------------------------------------------
 
