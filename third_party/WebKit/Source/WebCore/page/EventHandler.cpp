dc1ed43f8a9d18b1da82f69f995be623855b9b32
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-03 00:18:26 +0000

    Fix assertion failures on Gtk bot.
            * page/EventHandler.cpp: (WebCore::EventHandler::tabsToLinks): Removed an overzealous
            assertion. We can get here with non-Tab key events when spatial navigation is enabled.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -2748,7 +2748,7 @@ static bool eventInvertsTabsToLinksClientCallResult(KeyboardEvent* event)
 
 bool EventHandler::tabsToLinks(KeyboardEvent* event) const
 {
-    ASSERT(!event || event->keyIdentifier() == "U+0009");
+    // FIXME: This function needs a better name. It can be called for keypresses other than Tab when spatial navigation is enabled.
 
     Page* page = m_frame->page();
     if (!page)
