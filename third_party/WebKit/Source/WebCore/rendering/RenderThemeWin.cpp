bab7445bec011978dd4e9a392631d652bc9c3946
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-03 21:19:23 +0000

    2011-03-03  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Darin Adler.
    
            Use RenderBox in volumeSliderOffsetFromMuteButton, since that's what is actually being used.
            https://bugs.webkit.org/show_bug.cgi?id=55099
    
            Cleanup, no behavior change.
    
            * html/shadow/MediaControls.cpp:
            (WebCore::MediaControls::updateVolumeSliderContainer): Changed to pass RenderBox
                instead of Node.
            * rendering/RenderMediaControls.cpp:
            (WebCore::RenderMediaControls::volumeSliderOffsetFromMuteButton): Changed
                to use RenderBox as argument.
            * rendering/RenderMediaControls.h: Ditto.
            * rendering/RenderTheme.cpp:
            (WebCore::RenderTheme::volumeSliderOffsetFromMuteButton): Ditto.
            * rendering/RenderTheme.h: Ditto.
            * rendering/RenderThemeChromiumMac.h: Ditto.
            * rendering/RenderThemeChromiumMac.mm:
            (WebCore::RenderThemeChromiumMac::volumeSliderOffsetFromMuteButton): Ditto.
            * rendering/RenderThemeMac.h: Ditto.
            * rendering/RenderThemeMac.mm:
            (WebCore::RenderThemeMac::volumeSliderOffsetFromMuteButton): Ditto.
            * rendering/RenderThemeWin.cpp:
            (WebCore::RenderThemeWin::volumeSliderOffsetFromMuteButton): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeWin.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeWin.cpp
@@ -1109,9 +1109,9 @@ bool RenderThemeWin::paintMediaVolumeSliderThumb(RenderObject* o, const PaintInf
     return RenderMediaControls::paintMediaControlsPart(MediaVolumeSliderThumb, o, paintInfo, r);
 }
 
-IntPoint RenderThemeWin::volumeSliderOffsetFromMuteButton(Node* muteButton, const IntSize& size) const
+IntPoint RenderThemeWin::volumeSliderOffsetFromMuteButton(RenderBox* muteButtonBox, const IntSize& size) const
 {
-    return RenderMediaControls::volumeSliderOffsetFromMuteButton(muteButton, size);
+    return RenderMediaControls::volumeSliderOffsetFromMuteButton(muteButtonBox, size);
 }
 
 
