4ca37771bf05cbbbdd0c6d3121316b0b3c63ecc6
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-03 00:12:52 +0000

    2011-03-02  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r80139.
            http://trac.webkit.org/changeset/80139
            https://bugs.webkit.org/show_bug.cgi?id=55620
    
            failing and crashing tests on Snow Leopard bot (Requested by
            mihaip1 on #webkit).
    
            * fast/dom/Geolocation/iframe-reparent-expected.txt: Removed.
            * fast/dom/Geolocation/iframe-reparent.html: Removed.
            * fast/dom/Geolocation/page-reload-cancel-permission-requests-expected.txt: Removed.
            * fast/dom/Geolocation/page-reload-cancel-permission-requests.html: Removed.
            * fast/dom/Geolocation/resources/iframe-reparent-page-iframe.html: Removed.
            * fast/dom/Geolocation/resources/iframe-reparent-page.html: Removed.
            * fast/dom/Geolocation/resources/page-reload-cancel-permission-requests-inner.html: Removed.
            * fast/dom/Geolocation/script-tests/iframe-reparent.js: Removed.
            * fast/dom/Geolocation/script-tests/page-reload-cancel-permission-requests.js: Removed.
            * platform/gtk/Skipped:
            * platform/qt-wk2/Skipped:
            * platform/qt/Skipped:
    2011-03-02  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r80139.
            http://trac.webkit.org/changeset/80139
            https://bugs.webkit.org/show_bug.cgi?id=55620
    
            failing and crashing tests on Snow Leopard bot (Requested by
            mihaip1 on #webkit).
    
            * page/DOMWindow.cpp:
            * page/DOMWindow.h:
            * page/Frame.cpp:
            (WebCore::Frame::pageDestroyed):
            (WebCore::Frame::transferChildFrameToNewDocument):
            * page/Frame.h:
            (WebCore::Frame::detachFromPage):
            * page/Geolocation.cpp:
            (WebCore::Geolocation::~Geolocation):
            (WebCore::Geolocation::disconnectFrame):
            (WebCore::Geolocation::lastPosition):
            (WebCore::Geolocation::requestPermission):
            (WebCore::Geolocation::startUpdating):
            (WebCore::Geolocation::stopUpdating):
            * page/Geolocation.h:
            * page/GeolocationController.cpp:
            (WebCore::GeolocationController::~GeolocationController):
            * page/Navigator.cpp:
            * page/Navigator.h:
            * platform/mock/GeolocationClientMock.cpp:
            * platform/mock/GeolocationClientMock.h:
    2011-03-02  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r80139.
            http://trac.webkit.org/changeset/80139
            https://bugs.webkit.org/show_bug.cgi?id=55620
    
            failing and crashing tests on Snow Leopard bot (Requested by
            mihaip1 on #webkit).
    
            * public/WebGeolocationClientMock.h:
            * src/WebGeolocationClientMock.cpp:
    2011-03-02  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r80139.
            http://trac.webkit.org/changeset/80139
            https://bugs.webkit.org/show_bug.cgi?id=55620
    
            failing and crashing tests on Snow Leopard bot (Requested by
            mihaip1 on #webkit).
    
            * DumpRenderTree/LayoutTestController.cpp:
            (LayoutTestController::staticFunctions):
            * DumpRenderTree/LayoutTestController.h:
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            * DumpRenderTree/chromium/LayoutTestController.h:
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            * DumpRenderTree/mac/UIDelegate.h:
            * DumpRenderTree/mac/UIDelegate.mm:
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            * DumpRenderTree/qt/LayoutTestControllerQt.h:
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            * DumpRenderTree/wx/LayoutTestControllerWx.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Frame.h
+++ third_party/WebKit/Source/WebCore/page/Frame.h
@@ -357,6 +357,11 @@ namespace WebCore {
         return m_page;
     }
 
+    inline void Frame::detachFromPage()
+    {
+        m_page = 0;
+    }
+
     inline EventHandler* Frame::eventHandler() const
     {
         return &m_eventHandler;
