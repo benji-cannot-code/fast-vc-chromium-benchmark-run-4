b1a6517eb9d3f932576bc141ec85aac7e59169f3
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 00:39:19 +0000

    gdb: check in a gdb module for printing string16
    This just reuses the string-printing code from the WebKit module.
    
    Review URL: http://codereview.chromium.org/6596118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76680 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/gdb/gdb_chrome.py
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""GDB support for Chrome types.
+
+Add this to your gdb by amending your ~/.gdbinit as follows:
+  python
+  import sys
+  sys.path.insert(0, "/path/to/tools/gdb/")
+  import gdb_chrome
+
+This module relies on the WebKit gdb module already existing in
+your Python path.
+"""
+
+import gdb
+import webkit
+
+class String16Printer(webkit.StringPrinter):
+    def to_string(self):
+        return webkit.ustring_to_string(self.val['_M_dataplus']['_M_p'])
+
+
+def lookup_function(val):
+    if str(val.type) == 'string16':
+        return String16Printer(val)
+    return None
+
+gdb.pretty_printers.append(lookup_function)
