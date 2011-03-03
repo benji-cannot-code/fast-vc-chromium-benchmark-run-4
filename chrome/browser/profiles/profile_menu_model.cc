2a5c65b513b5698b9c537c40d758dd4e52b78f55
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 21:24:48 +0000

    Fix Clang errors in profile_menu_model and menu_2.
    BUG=none
    TEST=Clang bots are happy.
    
    Review URL: http://codereview.chromium.org/6613028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_menu_model.cc
+++ chrome/browser/profiles/profile_menu_model.cc
@@ -18,6 +18,9 @@ ProfileMenuModel::ProfileMenuModel()
   menu_.reset(new views::Menu2(this));
 }
 
+ProfileMenuModel::~ProfileMenuModel() {
+}
+
 void ProfileMenuModel::RunMenuAt(const gfx::Point& point) {
   menu_->RunMenuAt(point, views::Menu2::ALIGN_TOPRIGHT);
 }
--- chrome/browser/profiles/profile_menu_model.h
+++ chrome/browser/profiles/profile_menu_model.h
@@ -28,6 +28,7 @@ class ProfileMenuModel : public ui::SimpleMenuModel,
                          public ui::SimpleMenuModel::Delegate {
  public:
   ProfileMenuModel();
+  virtual ~ProfileMenuModel();
 
   void RunMenuAt(const gfx::Point& point);
 
