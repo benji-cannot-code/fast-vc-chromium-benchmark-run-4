61da3f9a6847138429dd67575a370aa4b070ea78
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-03 19:58:55 +0000

    Try to fix the Windows build again.
    * platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp:
    * platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeVisualContext.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <AssertMacros.h>
 #include <CoreGraphics/CGAffineTransform.h>
 #include <CoreGraphics/CGContext.h>
+#include <QuartzCore/CATransform3D.h>
 #include <Wininet.h>
 #include <wtf/CurrentTime.h>
 #include <wtf/HashSet.h>
