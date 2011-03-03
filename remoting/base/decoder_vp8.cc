84475a9b93ae6270aab23228e63e9f040e9f62a7
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 17:24:36 +0000

    Linux: roll ffmpeg and vpx so that we can build with system vpx
    Also change remoting files to use libvpx shim header.
    
    This is upstreaming Gentoo Linux changes.
    
    TBR=scherkus
    
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/6588123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76757 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/base/decoder_vp8.cc
+++ remoting/base/decoder_vp8.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern "C" {
 #define VPX_CODEC_DISABLE_COMPAT 1
-#include "third_party/libvpx/source/libvpx/vpx/vpx_codec.h"
-#include "third_party/libvpx/source/libvpx/vpx/vpx_decoder.h"
-#include "third_party/libvpx/source/libvpx/vpx/vp8dx.h"
+#include "third_party/libvpx/libvpx.h"
 }
 
 namespace remoting {
