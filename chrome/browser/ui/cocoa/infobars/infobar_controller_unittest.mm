26075f55402427bcafbda03fa1f0033d5ef2cfbc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 23:41:53 +0000

    Split infobar_test_helper.[h,mm] into separate pieces for the different classes defined within.
    BUG=None
    TEST=existing unit_tests.
    
    Review URL: http://codereview.chromium.org/6612008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76836 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/infobars/infobar_controller_unittest.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_controller_unittest.mm
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/cocoa_test_helper.h"
 #import "chrome/browser/ui/cocoa/infobars/infobar_container_controller.h"
 #import "chrome/browser/ui/cocoa/infobars/infobar_controller.h"
-#include "chrome/browser/ui/cocoa/infobars/infobar_test_helper.h"
+#include "chrome/browser/ui/cocoa/infobars/mock_confirm_infobar_delegate.h"
+#include "chrome/browser/ui/cocoa/infobars/mock_link_infobar_delegate.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
