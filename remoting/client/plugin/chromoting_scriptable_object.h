fb309a55484495c2908a34e565fd2feb707e37a5
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 15:56:57 +0000

    Refactor JingleClient to support thunking the xmpp signaling into Javascript.
    We do this so that we may make HTTP requests using the javascript libraries available to Google Talk.  This is required to allow us to sandbox.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6597092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76746 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_scriptable_object.h
+++ remoting/client/plugin/chromoting_scriptable_object.h
@@ -66,12 +66,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/weak_ptr.h"
+
 #include "ppapi/cpp/dev/scriptable_object_deprecated.h"
 #include "ppapi/cpp/var.h"
 
 namespace remoting {
 
 class ChromotingInstance;
+class XmppProxy;
 
 extern const char kStatusAttribute[];
 
@@ -92,7 +95,9 @@ enum ConnectionQuality {
   QUALITY_BAD,
 };
 
-class ChromotingScriptableObject : public pp::deprecated::ScriptableObject {
+class ChromotingScriptableObject
+    : public pp::deprecated::ScriptableObject,
+      public base::SupportsWeakPtr<ChromotingScriptableObject> {
  public:
   explicit ChromotingScriptableObject(ChromotingInstance* instance);
   virtual ~ChromotingScriptableObject();
@@ -119,6 +124,12 @@ class ChromotingScriptableObject : public pp::deprecated::ScriptableObject {
   // This should be called to signal JS code to provide login information.
   void SignalLoginChallenge();
 
+  // Handles a Request/Response for a Javascript IQ stanza used to initiate a
+  // jingle connection.
+  void AttachXmppProxy(XmppProxy* xmpp_proxy);
+  void SendIq(const std::string& iq_request_xml);
+  void ReceiveIq(const std::string& iq_response_xml);
+
  private:
   typedef std::map<std::string, int> PropertyNameMap;
   typedef pp::Var (ChromotingScriptableObject::*MethodHandler)(
@@ -142,7 +153,6 @@ class ChromotingScriptableObject : public pp::deprecated::ScriptableObject {
     MethodHandler method;
   };
 
-
   // Routines to add new attribute, method properties.
   void AddAttribute(const std::string& name, pp::Var attribute);
   void AddMethod(const std::string& name, MethodHandler handler);
@@ -159,6 +169,7 @@ class ChromotingScriptableObject : public pp::deprecated::ScriptableObject {
 
   PropertyNameMap property_names_;
   std::vector<PropertyDescriptor> properties_;
+  scoped_refptr<XmppProxy> xmpp_proxy_;
 
   ChromotingInstance* instance_;
 };
