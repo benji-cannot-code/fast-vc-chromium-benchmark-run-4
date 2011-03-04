benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit dd2316c9e051a3b75ce8b3cfcf2dcb846b5f3731

2011-02-25  Adrienne Walker  <enne@google.com>
        Reviewed by James Robinson.

        [chromium] Abstract "pixels with a graphics context" into its own class
        https://bugs.webkit.org/show_bug.cgi?id=55259

        This creates new PlatformCanvas/PlatformImage classes which wrap
        all of the #ifdef Skia/Cg warts from the compositor.  All classes
        (LayerTilerChromium, ContentLayerChromium, and ImageLayerChromium) are
        modified to use these abstractions.

        Tests: LayoutTests/compositing

        * WebCore.gypi:
        * platform/graphics/chromium/ContentLayerChromium.cpp:
        (WebCore::ContentLayerChromium::updateContentsIfDirty):
        (WebCore::ContentLayerChromium::resizeUploadBuffer):
        (WebCore::ContentLayerChromium::updateTextureIfNeeded):
        (WebCore::ContentLayerChromium::updateTexture):
        (WebCore::ContentLayerChromium::draw):
        * platform/graphics/chromium/ContentLayerChromium.h:
        * platform/graphics/chromium/ImageLayerChromium.cpp:
        (WebCore::ImageLayerChromium::updateContentsIfDirty):
        (WebCore::ImageLayerChromium::updateTextureIfNeeded):
        * platform/graphics/chromium/ImageLayerChromium.h:
        * platform/graphics/chromium/LayerTilerChromium.cpp:
        (WebCore::LayerTilerChromium::contentRectToTileIndices):
        (WebCore::LayerTilerChromium::update):
        (WebCore::LayerTilerChromium::updateFromPixels):
        * platform/graphics/chromium/LayerTilerChromium.h:
        * platform/graphics/chromium/PlatformCanvas.cpp: Added.
        (WebCore::PlatformCanvas::PlatformCanvas):
        (WebCore::PlatformCanvas::~PlatformCanvas):
        (WebCore::PlatformCanvas::resize):
        (WebCore::PlatformCanvas::AutoLocker::AutoLocker):
        (WebCore::PlatformCanvas::AutoLocker::~AutoLocker):
        (WebCore::PlatformCanvas::Painter::Painter):
        (WebCore::PlatformCanvas::Painter::~Painter):

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.cpp
@@ -41,15 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerTexture.h"
 #include "RenderLayerBacking.h"
 
-#if USE(SKIA)
-#include "NativeImageSkia.h"
-#include "PlatformContextSkia.h"
-#include "SkColorPriv.h"
-#include "skia/ext/platform_canvas.h"
-#elif PLATFORM(CG)
-#include <CoreGraphics/CGBitmapContext.h>
-#endif
-
 namespace WebCore {
 
 PassRefPtr<ContentLayerChromium> ContentLayerChromium::create(GraphicsLayerChromium* owner)
@@ -137,7 +128,7 @@ void ContentLayerChromium::updateContentsIfDirty()
         }
 
         // If we need to resize the upload buffer we have to repaint everything.
-        if (m_uploadBufferSize != visibleRectInLayerCoords.size()) {
+        if (m_canvas.size() != visibleRectInLayerCoords.size()) {
             resizeUploadBuffer(visibleRectInLayerCoords.size());
             m_dirtyRect = boundsRect;
         }
@@ -162,7 +153,7 @@ void ContentLayerChromium::updateContentsIfDirty()
         dirtyRect = IntRect(m_dirtyRect);
         // If the texture needs to be reallocated then we must redraw the entire
         // contents of the layer.
-        if (m_uploadBufferSize != bounds()) {
+        if (m_canvas.size() != bounds()) {
             resizeUploadBuffer(bounds());
             dirtyRect = boundsRect;
         } else {
@@ -176,106 +167,29 @@ void ContentLayerChromium::updateContentsIfDirty()
     if (dirtyRect.isEmpty())
         return;
 
-#if USE(SKIA)
-    OwnPtr<PlatformContextSkia> skiaContext;
+    PlatformCanvas::Painter painter(&m_canvas);
+    painter.context()->save();
+    painter.context()->translate(-paintingOffset.x(), -paintingOffset.y());
+    painter.context()->clearRect(dirtyRect);
+    painter.context()->clip(dirtyRect);
 
-    skiaContext.set(new PlatformContextSkia(m_uploadPixelCanvas.get()));
-
-    // This is needed to get text to show up correctly.
-    skiaContext->setDrawingToImageBuffer(true);
-
-    GraphicsContext graphicsContext(reinterpret_cast<PlatformGraphicsContext*>(skiaContext.get()));
-
-#elif PLATFORM(CG)
-    // FIXME: Do we need to clear the dirty rectangle in the upload buffer?
-    RetainPtr<CGColorSpaceRef> colorSpace(AdoptCF, CGColorSpaceCreateDeviceRGB());
-    size_t rowBytes = m_uploadBufferSize.width() * 4;
-    RetainPtr<CGContextRef> contextCG(AdoptCF, CGBitmapContextCreate(m_uploadPixelData->data(),
-                                                                     m_uploadBufferSize.width(), m_uploadBufferSize.height(), 8, rowBytes,
-                                                                     colorSpace.get(),
-                                                                     kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
-    CGContextTranslateCTM(contextCG.get(), 0, m_uploadBufferSize.height());
-    CGContextScaleCTM(contextCG.get(), 1, -1);
-
-    GraphicsContext graphicsContext(contextCG.get());
-#else
-#error "Need to implement for your platform."
-#endif
-
-    graphicsContext.save();
-    graphicsContext.translate(-paintingOffset.x(), -paintingOffset.y());
-    graphicsContext.clearRect(dirtyRect);
-    graphicsContext.clip(dirtyRect);
-
-    m_owner->paintGraphicsLayerContents(graphicsContext, dirtyRect);
-    graphicsContext.restore();
+    m_owner->paintGraphicsLayerContents(*painter.context(), dirtyRect);
+    painter.context()->restore();
 }
 
-void ContentLayerChromium::resizeUploadBufferForImage(const IntSize& size)
-{
-    size_t bufferSize = size.width() * size.height() * 4;
-    m_uploadPixelData = new Vector<uint8_t>(bufferSize);
-#if PLATFORM(CG)
-    memset(m_uploadPixelData->data(), 0, bufferSize);
-#endif
-    m_uploadBufferSize = size;
-}
 void ContentLayerChromium::resizeUploadBuffer(const IntSize& size)
 {
-#if USE(SKIA)
-    m_uploadPixelCanvas = new skia::PlatformCanvas(size.width(), size.height(), false);
-    m_uploadBufferSize = size;
-#else
-    resizeUploadBufferForImage(size);
-#endif
+    m_canvas.resize(size);
 }
-
-#if USE(SKIA)
-class SkBitmapConditionalAutoLockerPixels {
-    WTF_MAKE_NONCOPYABLE(SkBitmapConditionalAutoLockerPixels);
-public:
-    SkBitmapConditionalAutoLockerPixels()
-        : m_bitmap(0)
-    {
-    }
-
-    ~SkBitmapConditionalAutoLockerPixels()
-    {
-        if (m_bitmap)
-            m_bitmap->unlockPixels();
-    }
-
-    void lockPixels(const SkBitmap* bitmap)
-    {
-        bitmap->lockPixels();
-        m_bitmap = bitmap;
-    }
-
-private:
-    const SkBitmap* m_bitmap;
-};
-#endif
 
 void ContentLayerChromium::updateTextureIfNeeded()
 {
-    uint8_t* pixels = 0;
-#if USE(SKIA)
-    SkBitmapConditionalAutoLockerPixels locker;
-#endif
-    if (!m_uploadUpdateRect.isEmpty()) {
-#if USE(SKIA)
-        if (m_uploadPixelCanvas) {
-            const SkBitmap& bitmap = m_uploadPixelCanvas->getDevice()->accessBitmap(false);
-            locker.lockPixels(&bitmap);
-            // FIXME: do we need to support more image configurations?
-            if (bitmap.config() == SkBitmap::kARGB_8888_Config)
-                pixels = static_cast<uint8_t*>(bitmap.getPixels());
-        }
-#endif
-        if (m_uploadPixelData)
-            pixels = m_uploadPixelData->data();
-    }
+    PlatformCanvas::AutoLocker locker(&m_canvas);
+    updateTexture(locker.pixels(), m_canvas.size());
+}
 
+void ContentLayerChromium::updateTexture(const uint8_t* pixels, const IntSize& size)
+{
     if (!pixels)
         return;
 
@@ -284,28 +198,28 @@ void ContentLayerChromium::updateTextureIfNeeded()
         m_contentsTexture = LayerTexture::create(context, layerRenderer()->textureManager());
 
     // If we have to allocate a new texture we have to upload the full contents.
-    if (!m_contentsTexture->isValid(m_uploadBufferSize, GraphicsContext3D::RGBA))
-        m_uploadUpdateRect = IntRect(IntPoint(0, 0), m_uploadBufferSize);
+    if (!m_contentsTexture->isValid(size, GraphicsContext3D::RGBA))
+        m_uploadUpdateRect = IntRect(IntPoint(0, 0), size);
 
-    if (!m_contentsTexture->reserve(m_uploadBufferSize, GraphicsContext3D::RGBA)) {
+    if (!m_contentsTexture->reserve(size, GraphicsContext3D::RGBA)) {
         m_skipsDraw = true;
         return;
     }
 
-    IntRect srcRect = IntRect(IntPoint(0, 0), m_uploadBufferSize);
+    IntRect srcRect = IntRect(IntPoint(0, 0), size);
     if (requiresClippedUpdateRect())
         srcRect = m_visibleRectInLayerCoords;
 
-    const size_t destStride = m_uploadUpdateRect.width() * 4;
+    const size_t destStride = size.width() * 4;
     const size_t srcStride = srcRect.width() * 4;
 
-    uint8_t* uploadPixels = pixels + srcStride * m_uploadUpdateRect.x();
+    const uint8_t* uploadPixels = pixels + srcStride * m_uploadUpdateRect.x();
     Vector<uint8_t> uploadBuffer;
     if (srcStride != destStride) {
         uploadBuffer.resize(m_uploadUpdateRect.height() * destStride);
         for (int row = 0; row < m_uploadUpdateRect.height(); ++row) {
             size_t srcOffset = (m_uploadUpdateRect.y() + row) * srcStride + m_uploadUpdateRect.x() * 4;
-            ASSERT(srcOffset + destStride <= static_cast<size_t>(m_uploadBufferSize.width() * m_uploadBufferSize.height() * 4));
+            ASSERT(srcOffset + destStride <= static_cast<size_t>(size.width() * size.height() * 4));
             size_t destOffset = row * destStride;
             ASSERT(destOffset  + destStride <= uploadBuffer.size());
             memcpy(uploadBuffer.data() + destOffset, pixels + srcOffset, destStride);
@@ -327,8 +241,6 @@ void ContentLayerChromium::updateTextureIfNeeded()
 
 void ContentLayerChromium::draw()
 {
-    updateTextureIfNeeded();
-
     if (m_skipsDraw)
         return;
 

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.cpp
@@ -39,18 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerRendererChromium.h"
 #include "LayerTexture.h"
 
-#if USE(SKIA)
-#include "NativeImageSkia.h"
-#include "PlatformContextSkia.h"
-#endif
-
-#if PLATFORM(CG)
-#include <CoreGraphics/CGBitmapContext.h>
-#include <CoreGraphics/CGContext.h>
-#include <CoreGraphics/CGImage.h>
-#include <wtf/RetainPtr.h>
-#endif
-
 namespace WebCore {
 
 PassRefPtr<ImageLayerChromium> ImageLayerChromium::create(GraphicsLayerChromium* owner)
@@ -85,63 +73,12 @@ void ImageLayerChromium::updateContentsIfDirty()
         return;
     }
 
-    NativeImagePtr nativeImage = m_contents->nativeImageForCurrentFrame();
+    m_decodedImage.updateFromImage(m_contents->nativeImageForCurrentFrame());
+}
 
-#if USE(SKIA)
-    // The layer contains an Image.
-    NativeImageSkia* skiaImage = static_cast<NativeImageSkia*>(nativeImage);
-    const SkBitmap* skiaBitmap = skiaImage;
-    IntSize bitmapSize(skiaBitmap->width(), skiaBitmap->height());
-    ASSERT(skiaBitmap);
-#elif PLATFORM(CG)
-    // NativeImagePtr is a CGImageRef on Mac OS X.
-    int width = CGImageGetWidth(nativeImage);
-    int height = CGImageGetHeight(nativeImage);
-    IntSize bitmapSize(width, height);
-#endif
-
-    if (m_uploadBufferSize != bitmapSize)
-        resizeUploadBufferForImage(bitmapSize);
-    m_uploadUpdateRect = IntRect(IntPoint(0, 0), bitmapSize);
-
-#if USE(SKIA)
-    SkAutoLockPixels lock(*skiaBitmap);
-    // FIXME: do we need to support more image configurations?
-    ASSERT(skiaBitmap->config()== SkBitmap::kARGB_8888_Config);
-    skiaBitmap->copyPixelsTo(m_uploadPixelData->data(), m_uploadPixelData->size());
-#elif PLATFORM(CG)
-    // FIXME: we should get rid of this temporary copy where possible.
-    int tempRowBytes = width * 4;
-    // Note we do not zero this vector since we are going to
-    // completely overwrite its contents with the image below.
-    // Try to reuse the color space from the image to preserve its colors.
-    // Some images use a color space (such as indexed) unsupported by the bitmap context.
-    RetainPtr<CGColorSpaceRef> colorSpaceReleaser;
-    CGColorSpaceRef colorSpace = CGImageGetColorSpace(nativeImage);
-    CGColorSpaceModel colorSpaceModel = CGColorSpaceGetModel(colorSpace);
-    switch (colorSpaceModel) {
-    case kCGColorSpaceModelMonochrome:
-    case kCGColorSpaceModelRGB:
-    case kCGColorSpaceModelCMYK:
-    case kCGColorSpaceModelLab:
-    case kCGColorSpaceModelDeviceN:
-        break;
-    default:
-        colorSpaceReleaser.adoptCF(CGColorSpaceCreateDeviceRGB());
-        colorSpace = colorSpaceReleaser.get();
-        break;
-    }
-    RetainPtr<CGContextRef> tempContext(AdoptCF, CGBitmapContextCreate(m_uploadPixelData->data(),
-                                                                       width, height, 8, tempRowBytes,
-                                                                       colorSpace,
-                                                                       kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
-    CGContextSetBlendMode(tempContext.get(), kCGBlendModeCopy);
-    CGContextDrawImage(tempContext.get(),
-                       CGRectMake(0, 0, static_cast<CGFloat>(width), static_cast<CGFloat>(height)),
-                       nativeImage);
-#else
-#error "Need to implement for your platform."
-#endif
+void ImageLayerChromium::updateTextureIfNeeded()
+{
+    updateTexture(m_decodedImage.pixels(), m_decodedImage.size());
 }
 
 }

third_party/WebKit/Source/WebCore/WebCore.gypi
@@ -2642,6 +2642,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'platform/graphics/chromium/LayerTilerChromium.h',
             'platform/graphics/chromium/MediaPlayerPrivateChromium.h',
             'platform/graphics/chromium/PlatformIcon.h',
+            'platform/graphics/chromium/PlatformCanvas.cpp',
+            'platform/graphics/chromium/PlatformCanvas.h',
+            'platform/graphics/chromium/PlatformImage.cpp',
+            'platform/graphics/chromium/PlatformImage.h',
             'platform/graphics/chromium/PluginLayerChromium.cpp',
             'platform/graphics/chromium/PluginLayerChromium.h',
             'platform/graphics/chromium/ProgramBinding.cpp',

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "LayerChromium.h"
+#include "PlatformCanvas.h"
 #include "TextureManager.h"
 
 namespace WebCore {
@@ -65,25 +66,18 @@ protected:
     virtual void cleanupResources();
     bool requiresClippedUpdateRect() const;
     void resizeUploadBuffer(const IntSize&);
-    void resizeUploadBufferForImage(const IntSize&);
 
     OwnPtr<LayerTexture> m_contentsTexture;
     bool m_skipsDraw;
 
-    // The size of the upload buffer (also the size of our backing texture).
-    IntSize m_uploadBufferSize;
     // The portion of the upload buffer that has a pending update, in the coordinates of the texture.
     IntRect m_uploadUpdateRect;
-    // On platforms using Skia, we use the skia::PlatformCanvas for content layers
-    // and the m_uploadPixelData buffer for image layers.
-    // FIXME: We should be using memory we control for all uploads.
-#if USE(SKIA)
-    OwnPtr<skia::PlatformCanvas> m_uploadPixelCanvas;
-#endif
-    OwnPtr<Vector<uint8_t> > m_uploadPixelData;
+
+    virtual void updateTextureIfNeeded();
+    void updateTexture(const uint8_t* pixels, const IntSize&);
 
 private:
-    void updateTextureIfNeeded();
+    PlatformCanvas m_canvas;
 
     IntRect m_visibleRectInLayerCoords;
     FloatPoint m_layerCenterInSurfaceCoords;

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "ContentLayerChromium.h"
+#include "PlatformImage.h"
 
 #if PLATFORM(CG)
 #include <wtf/RetainPtr.h>
@@ -56,8 +57,11 @@ public:
     void setContents(Image* image);
 
 private:
+    virtual void updateTextureIfNeeded();
+
     ImageLayerChromium(GraphicsLayerChromium* owner);
 
+    PlatformImage m_decodedImage;
     RefPtr<Image> m_contents;
 };
 

third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.cpp
@@ -36,13 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerRendererChromium.h"
 #include "LayerTexture.h"
 
-#if USE(SKIA)
-#include "NativeImageSkia.h"
-#include "PlatformContextSkia.h"
-#elif PLATFORM(CG)
-#include <CoreGraphics/CGBitmapContext.h>
-#endif
-
 #include <wtf/PassOwnArrayPtr.h>
 
 using namespace std;
@@ -149,8 +142,8 @@ void LayerTilerChromium::contentRectToTileIndices(const IntRect& contentRect, in
 
     left = m_tilingData.tileXIndexFromSrcCoord(layerRect.x());
     top = m_tilingData.tileYIndexFromSrcCoord(layerRect.y());
-    right = m_tilingData.tileXIndexFromSrcCoord(layerRect.maxX());
-    bottom = m_tilingData.tileYIndexFromSrcCoord(layerRect.maxY());
+    right = m_tilingData.tileXIndexFromSrcCoord(layerRect.maxX() - 1);
+    bottom = m_tilingData.tileYIndexFromSrcCoord(layerRect.maxY() - 1);
 }
 
 IntRect LayerTilerChromium::contentRectToLayerRect(const IntRect& contentRect) const
@@ -271,55 +264,26 @@ void LayerTilerChromium::update(TilePaintInterface& painter, const IntRect& cont
         return;
 
     const IntRect paintRect = layerRectToContentRect(dirtyLayerRect);
-    GraphicsContext3D* context = layerRendererContext();
-#if USE(SKIA)
-    OwnPtr<skia::PlatformCanvas> canvas(new skia::PlatformCanvas(paintRect.width(), paintRect.height(), false));
-    OwnPtr<PlatformContextSkia> skiaContext(new PlatformContextSkia(canvas.get()));
-    OwnPtr<GraphicsContext> graphicsContext(new GraphicsContext(reinterpret_cast<PlatformGraphicsContext*>(skiaContext.get())));
 
-    // Bring the canvas into the coordinate system of the paint rect.
-    canvas->translate(static_cast<SkScalar>(-paintRect.x()), static_cast<SkScalar>(-paintRect.y()));
+    m_canvas.resize(paintRect.size());
+    PlatformCanvas::Painter canvasPainter(&m_canvas);
+    canvasPainter.context()->translate(-paintRect.x(), -paintRect.y());
+    painter.paint(*canvasPainter.context(), paintRect);
 
-    painter.paint(*graphicsContext, paintRect);
+    PlatformCanvas::AutoLocker locker(&m_canvas);
+    updateFromPixels(paintRect, locker.pixels());
+}
 
-    // Get the contents of the updated rect.
-    const SkBitmap& bitmap = canvas->getDevice()->accessBitmap(false);
-    ASSERT(bitmap.width() == paintRect.width() && bitmap.height() == paintRect.height());
-    if (bitmap.width() != paintRect.width() || bitmap.height() != paintRect.height())
-        CRASH();
-    uint8_t* paintPixels = static_cast<uint8_t*>(bitmap.getPixels());
-    if (!paintPixels)
-        CRASH();
-#elif PLATFORM(CG)
-    Vector<uint8_t> canvasPixels;
-    int rowBytes = 4 * paintRect.width();
-    canvasPixels.resize(rowBytes * paintRect.height());
-    memset(canvasPixels.data(), 0, canvasPixels.size());
-    RetainPtr<CGColorSpaceRef> colorSpace(AdoptCF, CGColorSpaceCreateDeviceRGB());
-    RetainPtr<CGContextRef> m_cgContext;
-    m_cgContext.adoptCF(CGBitmapContextCreate(canvasPixels.data(),
-                                                       paintRect.width(), paintRect.height(), 8, rowBytes,
-                                                       colorSpace.get(),
-                                                       kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host));
-    CGContextTranslateCTM(m_cgContext.get(), 0, paintRect.height());
-    CGContextScaleCTM(m_cgContext.get(), 1, -1);
-    OwnPtr<GraphicsContext> m_graphicsContext(new GraphicsContext(m_cgContext.get()));
-
-    // Bring the CoreGraphics context into the coordinate system of the paint rect.
-    CGContextTranslateCTM(m_cgContext.get(), -paintRect.x(), -paintRect.y());
-    painter.paint(*m_graphicsContext, paintRect);
-
-    // Get the contents of the updated rect.
-    ASSERT(static_cast<int>(CGBitmapContextGetWidth(m_cgContext.get())) == paintRect.width() && static_cast<int>(CGBitmapContextGetHeight(m_cgContext.get())) == paintRect.height());
-    uint8_t* paintPixels = static_cast<uint8_t*>(canvasPixels.data());
-#else
-#error "Need to implement for your platform."
-#endif
-
+void LayerTilerChromium::updateFromPixels(const IntRect& paintRect, const uint8_t* paintPixels)
+{
     // Painting could cause compositing to get turned off, which may cause the tiler to become invalidated mid-update.
     if (!m_tiles.size())
         return;
 
+    GraphicsContext3D* context = layerRendererContext();
+
+    int left, top, right, bottom;
+    contentRectToTileIndices(paintRect, left, top, right, bottom);
     for (int j = top; j <= bottom; ++j) {
         for (int i = left; i <= right; ++i) {
             Tile* tile = m_tiles[tileIndex(i, j)].get();
@@ -359,7 +323,7 @@ void LayerTilerChromium::update(TilePaintInterface& painter, const IntRect& cont
             if (paintOffset.y() + destRect.height() > paintRect.height())
                 CRASH();
 
-            uint8_t* pixelSource;
+            const uint8_t* pixelSource;
             if (paintRect.width() == sourceRect.width() && !paintOffset.x())
                 pixelSource = &paintPixels[4 * paintOffset.y() * paintRect.width()];
             else {

third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "LayerChromium.h"
 #include "LayerTexture.h"
+#include "PlatformCanvas.h"
 #include "TilingData.h"
 #include <wtf/OwnArrayPtr.h>
 
@@ -57,6 +58,7 @@ public:
     void invalidateRect(const IntRect& contentRect);
     void invalidateEntireLayer();
     void update(TilePaintInterface& painter, const IntRect& contentRect);
+    void updateFromPixels(const IntRect& paintRect, const uint8_t* pixels);
     void draw(const IntRect& contentRect);
 
     // Set position of this tiled layer in content space.
@@ -125,6 +127,8 @@ private:
     Vector<OwnPtr<Tile> > m_tiles;
     // Linear array of unused tiles.
     Vector<OwnPtr<Tile> > m_unusedTiles;
+
+    PlatformCanvas m_canvas;
 
     // Cache a tile-sized pixel buffer to draw into.
     OwnArrayPtr<uint8_t> m_tilePixels;

commit 08a0a5b0f4eb5c67d321008d4e3e1a93468a62b5

2011-03-04  Adrienne Walker  <enne@google.com>
        Reviewed by James Robinson.

        Turn on pixel tests for huge-layer-img.html for better test coverage.
        https://bugs.webkit.org/show_bug.cgi?id=55679

        Also, modify this test to be more pixel test friendly.

        * compositing/tiling/huge-layer-img-expected.checksum: Added.
        * compositing/tiling/huge-layer-img-expected.png: Added.
        * compositing/tiling/huge-layer-img-expected.txt: Added.
        * compositing/tiling/huge-layer-img.html:
        * platform/chromium-gpu-win/compositing/tiling/huge-layer-img-expected.txt: Removed.
        * platform/chromium-gpu/compositing/tiling/huge-layer-img-expected.txt: Added.
        * platform/mac/compositing/tiling/huge-layer-img-expected.txt: Removed.
2011-03-04  Adrienne Walker  <enne@google.com>

        Reviewed by James Robinson.

        [chromium] Fix texture stride issues on large content and image layers.
        https://bugs.webkit.org/show_bug.cgi?id=55679

        This was an error caused during the refactoring in r80081.

        Tests: LayoutTests/compositing/tiling/huge-layer-img.html

        * platform/graphics/chromium/ContentLayerChromium.cpp:
        (WebCore::ContentLayerChromium::updateTexture):
        * platform/graphics/chromium/ImageLayerChromium.cpp:
        (WebCore::ImageLayerChromium::updateTextureIfNeeded):

git-svn-id: svn://svn.chromium.org/blink/trunk@80384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.cpp
@@ -211,12 +211,12 @@ void ContentLayerChromium::updateTexture(const uint8_t* pixels, const IntSize& s
     if (requiresClippedUpdateRect())
         srcRect = m_visibleRectInLayerCoords;
 
-    const size_t destStride = size.width() * 4;
+    const size_t destStride = m_uploadUpdateRect.width() * 4;
     const size_t srcStride = srcRect.width() * 4;
 
-    const uint8_t* uploadPixels = pixels + srcStride * m_uploadUpdateRect.x();
+    const uint8_t* uploadPixels = pixels + srcStride * m_uploadUpdateRect.y();
     Vector<uint8_t> uploadBuffer;
-    if (srcStride != destStride) {
+    if (srcStride != destStride || m_uploadUpdateRect.x()) {
         uploadBuffer.resize(m_uploadUpdateRect.height() * destStride);
         for (int row = 0; row < m_uploadUpdateRect.height(); ++row) {
             size_t srcOffset = (m_uploadUpdateRect.y() + row) * srcStride + m_uploadUpdateRect.x() * 4;

third_party/WebKit/Source/WebCore/platform/graphics/chromium/ImageLayerChromium.cpp
@@ -78,6 +78,11 @@ void ImageLayerChromium::updateContentsIfDirty()
 
 void ImageLayerChromium::updateTextureIfNeeded()
 {
+    // FIXME: Remove this test when tiled layers are implemented.
+    if (requiresClippedUpdateRect()) {
+        ContentLayerChromium::updateTextureIfNeeded();
+        return;
+    }
     updateTexture(m_decodedImage.pixels(), m_decodedImage.size());
 }
 

third_party/WebKit/LayoutTests/compositing/tiling/huge-layer-img-expected.checksum
@@ -1,0 +1,2 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+d4077ae352a5a2f0f9c950db849f4be3

third_party/WebKit/LayoutTests/compositing/tiling/huge-layer-img-expected.txt
@@ -1,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+(GraphicsLayer
+  (bounds 800.00 20053.00)
+  (children 1
+    (GraphicsLayer
+      (bounds 800.00 20053.00)
+      (children 1
+        (GraphicsLayer
+          (position 8.00 8.00)
+          (bounds 500.00 20000.00)
+          (usingTiledLayer 1)
+          (drawsContent 1)
+        )
+      )
+    )
+  )
+)
+

third_party/WebKit/LayoutTests/compositing/tiling/huge-layer-img.html
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       height: 20000px;
       -webkit-transform:translateZ(0);
     }
+    body {
+      overflow:hidden;
+    }
     </style>
     <script type="text/javascript" charset="utf-8">
         if (window.layoutTestController) {
-            layoutTestController.dumpAsText();
+            layoutTestController.dumpAsText(true);
             layoutTestController.waitUntilDone();
         }
 
@@ -28,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             context.fillRect(0, 0, canvas.width, canvas.height);
             context.fillStyle = "red";
             context.fillRect(50, 50, 200, 200);
-            context.fillStyle = "blue";
-            context.fillRect(50, canvas.height - 250, 200, 200);
             img.src = canvas.toDataURL();
 
             window.setTimeout(function() {
@@ -44,12 +45,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </script>
   </head>
   <body>
-      <p>
+      <!--
         The yellow box should be large enough to scroll off the bottom.
-        There should be a red box on the first page and a blue box
-        near the bottom of the yellow box. This tests that we can support
-        very large composited image layers.
-      </p>
+        There should be a red box within the yellow box.
+        This tests that we can support very large composited image layers.
+      -->
         <img id="container">
         <pre id="layers">Layer tree appears here in DRT.</pre>
   </body>

third_party/WebKit/LayoutTests/platform/chromium-gpu-win/compositing/tiling/huge-layer-img-expected.txt
@@ -1,20 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-The yellow box should be large enough to scroll off the bottom. There should be a red box on the first page and a blue box near the bottom of the yellow box. This tests that we can support very large composited image layers.
-
-
-(GraphicsLayer
-  (bounds 785.00 20119.00)
-  (children 1
-    (GraphicsLayer
-      (bounds 785.00 20119.00)
-      (children 1
-        (GraphicsLayer
-          (position 8.00 72.00)
-          (bounds 500.00 20000.00)
-          (drawsContent 1)
-        )
-      )
-    )
-  )
-)
-

third_party/WebKit/LayoutTests/platform/chromium-gpu/compositing/tiling/huge-layer-img-expected.txt
@@ -1,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+(GraphicsLayer
+  (bounds 800.00 20055.00)
+  (children 1
+    (GraphicsLayer
+      (bounds 800.00 20055.00)
+      (children 1
+        (GraphicsLayer
+          (position 8.00 8.00)
+          (bounds 500.00 20000.00)
+          (drawsContent 1)
+        )
+      )
+    )
+  )
+)
+

third_party/WebKit/LayoutTests/platform/mac/compositing/tiling/huge-layer-img-expected.txt
@@ -1,21 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-The yellow box should be large enough to scroll off the bottom. There should be a red box on the first page and a blue box near the bottom of the yellow box. This tests that we can support very large composited image layers.
-
-
-(GraphicsLayer
-  (bounds 785.00 20113.00)
-  (children 1
-    (GraphicsLayer
-      (bounds 785.00 20113.00)
-      (children 1
-        (GraphicsLayer
-          (position 8.00 68.00)
-          (bounds 500.00 20000.00)
-          (usingTiledLayer 1)
-          (drawsContent 1)
-        )
-      )
-    )
-  )
-)
-