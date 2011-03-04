c95315aed73349aeff5fb78ba4ef5bd95f27269c
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-04 22:06:45 +0000

    Adopt VersionStamper tool for Windows WebKit DLLs https://bugs.webkit.org/show_bug.cgi?id=55784
    Reviewed by NOBODY Jon Honeycutt.
    
    We now use a tool to stamp the version number onto the Apple WebKit DLLs
    during the post-build step.
    
    * WebCore.vcproj/QTMovieWin.rc: Removed.
    * WebCore.vcproj/QTMovieWin.vcproj:
    * WebCore.vcproj/QTMovieWinPostBuild.cmd: Stamp version with VersionStamper.
    * WebCore.vcproj/QTMovieWinPreBuild.cmd: Don't run auto-version.sh. We don't use autoversion.h in this project.
    * WebCore.vcproj/WebCoreMediaQT.vsprops: Remove unnecessary include paths for resource files.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.vcproj/QTMovieWinPostBuild.cmd
+++ third_party/WebKit/Source/WebCore/WebCore.vcproj/QTMovieWinPostBuild.cmd
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\include\QTMovieWin"
 xcopy /y /d "%PROJECTDIR%..\platform\graphics\win\QTMovie.h" "%CONFIGURATIONBUILDDIR%\include\QTMovieWin"
+if exist "%WEBKITLIBRARIESDIR%\tools\VersionStamper\VersionStamper.exe" "%WEBKITLIBRARIESDIR%\tools\VersionStamper\VersionStamper.exe" --verbose "%TARGETPATH%"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
