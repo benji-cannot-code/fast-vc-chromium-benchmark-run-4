50c725a920c8bb39b2745b20940c15dfd9b1bc4a
jeffm@apple.com <jeffm@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-04 00:11:17 +0000

    2011-03-03  Jeff Miller  <jeffm@apple.com>
            Reviewed by Anders Carlsson.
    
            WebKit2: Add WKViewRef API for executing edit commands
            https://bugs.webkit.org/show_bug.cgi?id=49829
    
            Added WKViewExecuteCommand() and documented some of the WebCore EditorCommand strings.
    
            * UIProcess/API/C/win/WKView.cpp:
            (WKViewExecuteCommand): Added.
            * UIProcess/API/C/win/WKView.h: Added WKViewExecuteCommand() and comment block with WebCore EditorCommand strings.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.cpp
@@ -82,3 +82,8 @@ WKViewFindIndicatorCallback WKViewGetFindIndicatorCallback(WKViewRef viewRef, vo
 {    
     return toImpl(viewRef)->getFindIndicatorCallback(context);
 }
+
+void WKViewExecuteCommand(WKViewRef viewRef, WKStringRef command)
+{
+    toImpl(viewRef)->page()->executeEditCommand(toImpl(command)->string());
+}
