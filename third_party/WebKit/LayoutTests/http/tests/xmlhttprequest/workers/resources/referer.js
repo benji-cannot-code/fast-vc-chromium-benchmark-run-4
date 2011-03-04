def74eada77093676731d63bb758f5a3a494eb58
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-04 03:41:32 +0000

    2011-03-03  David Levin  <levin@chromium.org>
            Reviewed by Darin Adler.
    
            Need web worker referer layout test for xhr requests.
            https://bugs.webkit.org/show_bug.cgi?id=55590
    
            Verify that the referrer is set for sync and async XHR requests
            from both workers and shared workers.
    
            * http/tests/xmlhttprequest/workers/referer-expected.txt: Added.
            * http/tests/xmlhttprequest/workers/referer.html: Added.
            * http/tests/xmlhttprequest/workers/resources/referer-test.js: Added.
            * http/tests/xmlhttprequest/workers/resources/referer.js: Added.
            * http/tests/xmlhttprequest/workers/shared-worker-referer-expected.txt: Added.
            * http/tests/xmlhttprequest/workers/shared-worker-referer.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/workers/resources/referer.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var refererLocation = location.href.replace(/:\/\/.*\@/, "://").replace(/#.*$/, "");
+var xhr = new XMLHttpRequest;
+
+importScripts("worker-pre.js");
+
+function log(message)
+{
+    postMessage("log " + message);
+}
+
+function done()
+{
+    postMessage("DONE");
+}
+
+function verifyReferer(method, xhr)
+{
+    if (xhr.responseText == refererLocation)
+        log("PASS: " + method + " referer.");
+    else
+        log("FAIL: " + method + ". Expected referer: '" + refererLocation + "' Actual referer: '" + xhr.responseText + "'");
+}
+
+function processStateChange()
+{
+    if (xhr.readyState == 4) {
+	verifyReferer("Async", xhr);
+        done();
+    }
+}
+
+function init()
+{
+    xhr.open("GET", "../../resources/print-referer.cgi", false);
+    xhr.send(null);
+    verifyReferer("Sync", xhr);
+    xhr.open("GET", "../../resources/print-referer.cgi", true);
+    xhr.onreadystatechange = processStateChange;
+    xhr.send(null);
+}
+
