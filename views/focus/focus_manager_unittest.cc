e2a8a214dd901f3557e68ab885d2220e3787b630
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 08:10:24 +0000

    One more chromeos fix for the InitWindow rename.
    TBR=ben
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76895 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/focus_manager_unittest.cc
+++ views/focus/focus_manager_unittest.cc
@@ -1682,7 +1682,7 @@ class FocusManagerDtorTest : public FocusManagerTest {
       set_focus_manager(tracked_focus_manager_);
 
       GetNonClientView()->SetFrameView(CreateFrameViewForWindow());
-      Init(NULL, gfx::Rect(0, 0, 100, 100));
+      InitWindow(NULL, gfx::Rect(0, 0, 100, 100));
     }
 
     virtual ~WindowGtkDtorTracked() {
