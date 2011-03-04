a3029b415c45d86093af6686e2613797ab3df239
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 19:26:05 +0000

    Move GetWindowsArchitecture() to windows_version.h.
    BUG=none
    TEST=none
    TBR=brettw
    Review URL: http://codereview.chromium.org/6624025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_details_win.cc
+++ chrome/browser/memory_details_win.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/scoped_handle.h"
-#include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/url_constants.h"
