4a17d71c17ca49d3ef19088acc0df978011d88f0
steveblock@google.com <steveblock@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-04 09:45:13 +0000

    2011-03-04  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            V8 version of JavaString should obtain string from JNI in UTF-16 encoding
            https://bugs.webkit.org/show_bug.cgi?id=55566
    
            We obtain the string from JNI in UTF-16 encoding and convert
            to UTF-8 using our own routines as required. This matches the
            behaviour of the JSC version of JavaString.
    
            No new tests, no change in behaviour.
    
            * bridge/jni/v8/JavaStringV8.h:
            (JSC::Bindings::JavaStringImpl::init):
            (JSC::Bindings::JavaStringImpl::utf8):
            (JSC::Bindings::JavaStringImpl::impl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/jni/v8/JavaStringV8.h
+++ third_party/WebKit/Source/WebCore/bridge/jni/v8/JavaStringV8.h
@@ -29,12 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JNIUtility.h"
 
-#include <wtf/Assertions.h>
 #include <wtf/text/CString.h>
-
-namespace WTF {
-class StringImpl;
-}
+#include <wtf/text/WTFString.h>
 
 
 namespace JSC {
@@ -45,20 +41,26 @@ class JavaStringImpl {
 public:
     void init() {}
 
-    void init(JNIEnv* e, jstring s)
+    void init(JNIEnv* env, jstring string)
     {
-        int size = e->GetStringLength(s);
-        const char* cs = getCharactersFromJStringInEnv(e, s);
-        m_utf8String = WTF::CString(cs, size);
-        releaseCharactersForJStringInEnv(e, s, cs);
+        int size = env->GetStringLength(string);
+        const jchar* jChars = getUCharactersFromJStringInEnv(env, string);
+        m_impl = StringImpl::create(jChars, size);
+        releaseUCharactersForJStringInEnv(env, string, jChars);
     }
 
-    const char* utf8() const { return m_utf8String.data(); }
+    const char* utf8() const
+    {
+        if (!m_utf8String.data())
+            m_utf8String = String(m_impl).utf8();
+        return m_utf8String.data();
+    }
     int length() const { return m_utf8String.length(); }
-    StringImpl* impl() const { ASSERT_NOT_REACHED(); return 0; }
+    StringImpl* impl() const { return m_impl.get(); }
 
 private:
-    WTF::CString m_utf8String;
+    RefPtr<StringImpl> m_impl;
+    mutable CString m_utf8String;
 };
 
 } // namespace Bindings
