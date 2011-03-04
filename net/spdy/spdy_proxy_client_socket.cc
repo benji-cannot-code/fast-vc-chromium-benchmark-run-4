d909b2689ac6a6ce83efaa34f82b777974f03a0d
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 08:58:37 +0000

    Revert "Add chunked uploads support to SPDY"
    This reverts commit 8431a6e7be70b1b50b0d5b851bbe728b7fef220f.
    
    TBR=satish
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76896 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_proxy_client_socket.cc
+++ net/spdy/spdy_proxy_client_socket.cc
@@ -405,11 +405,11 @@ int SpdyProxyClientSocket::OnSendBody() {
   return ERR_UNEXPECTED;
 }
 
-int SpdyProxyClientSocket::OnSendBodyComplete(int /*status*/, bool* /*eof*/) {
+bool SpdyProxyClientSocket::OnSendBodyComplete(int status) {
   // Because we use |spdy_stream_| via STATE_OPEN (ala WebSockets)
   // OnSendBodyComplete() should never be called.
   NOTREACHED();
-  return ERR_UNEXPECTED;
+  return false;
 }
 
 int SpdyProxyClientSocket::OnResponseReceived(
@@ -497,9 +497,6 @@ void SpdyProxyClientSocket::OnClose(int status)  {
   }
   if (write_callback)
     write_callback->Run(ERR_CONNECTION_CLOSED);
-}
-
-void SpdyProxyClientSocket::set_chunk_callback(ChunkCallback* /*callback*/) {
 }
 
 }  // namespace net
