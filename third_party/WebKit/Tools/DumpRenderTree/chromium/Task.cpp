6b586b71f94f48e346a3861b565e29c742af7269
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-04 10:06:30 +0000

    2011-03-02  MORITA Hajime  <morrita@google.com>
            Reviewed by Kent Tamura.
    
            [Chromium][DRT] WebTask should use webkit_support::TaskAdaptor
            https://bugs.webkit.org/show_bug.cgi?id=55434
    
            Changed WebTask to be a subclass of webkit_support::TaskAdaptor
            and passed the object webkit_support::PostDelayedTask directly.
    
            Note that the change keeps postTask() as is because it has a
            semantics taht webkit_support::PostDelayedTask() has; It jumps to
            the main thread.
    
            * DumpRenderTree/chromium/Task.cpp:
            (postDelayedTask):
            * DumpRenderTree/chromium/Task.h:
            (WebTask::Run):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/Task.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/Task.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebKit.h"
 #include "WebKitClient.h"
-#include "webkit/support/webkit_support.h"
 
 WebTask::WebTask(TaskList* list): m_taskList(list) { m_taskList->registerTask(this); }
 WebTask::~WebTask()
@@ -70,7 +69,7 @@ void postTask(WebTask* task)
 
 void postDelayedTask(WebTask* task, int64_t ms)
 {
-    webkit_support::PostDelayedTask(invokeTask, static_cast<void*>(task), ms);
+    webkit_support::PostDelayedTask(task, ms);
 }
 
 
