d8f19e231c8c46a27eb1387b27cfe3e8387c1359
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 21:54:49 +0000

    Build WebKit Full Screen API. Support is disabled by default; enable with --enable-fullscreen.
    Commit after WK bug 44797 (r79774).
    
    Fullscreen tests are temporarily until expectations can be updated.
    (Deleted test video-play-suspend.html is removed from test_expectations to fix lint errors.)
    
    BUG=16735
    TEST=fullscreen layout tests
    
    Review URL: http://codereview.chromium.org/6627001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76979 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -73,7 +73,8 @@ WebPreferences::WebPreferences()
       accelerated_2d_canvas_enabled(false),
       accelerated_plugins_enabled(false),
       memory_info_enabled(false),
-      interactive_form_validation_enabled(true) {
+      interactive_form_validation_enabled(true),
+      fullscreen_enabled(false) {
 }
 
 WebPreferences::~WebPreferences() {
@@ -206,4 +207,6 @@ void WebPreferences::Apply(WebView* web_view) const {
 
   settings->setInteractiveFormValidationEnabled(
       interactive_form_validation_enabled);
+
+  settings->setFullScreenEnabled(fullscreen_enabled);
 }
