aeb61e991f88c373317141b03ba4b3b419a12814
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 18:12:34 +0000

    Convert worker, speech_input, and utility messages to use the new macros, eliminating the need for hand coded traits.
    Review URL: http://codereview.chromium.org/6610026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76931 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/common_message_generator.h
+++ chrome/common/common_message_generator.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/indexed_db_messages.h"
 #include "chrome/common/mime_registry_messages.h"
 #include "chrome/common/safebrowsing_messages.h"
+#include "chrome/common/speech_input_messages.h"
+#include "chrome/common/utility_messages.h"
+#include "chrome/common/worker_messages.h"
 
 #if 0  // This feature is not yet enabled for these files.
 
@@ -24,8 +27,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/plugin_messages.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/common/service_messages.h"
-#include "chrome/common/speech_input_messages.h"
-#include "chrome/common/utility_messages.h"
-#include "chrome/common/worker_messages.h"
 
 #endif
