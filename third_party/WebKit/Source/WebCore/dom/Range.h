03836f0a8da14d21308700e2434269b54009dfcc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-05 06:12:36 +0000

    2011-03-04  Jia Pu  <jpu@apple.com>
            Reviewed by Darin Adler.
    
            On Mac, the bounding box sent to EditorClient::showCorrectionPanel() is incorrect when the correction occurs in an iframe.
            https://bugs.webkit.org/show_bug.cgi?id=55717
            <rdar://problem/9018127>
    
            manual-test: manual-tests/platforms/mac/autocorrection/autocorrection-in-iframe.html
    
            Previously, the bounding box passed into EditorClient::showCorrectionPanel() is in the frame's
            coordinate. This is incorrect when the correction occurs in an iframe. This patch added code
            to convert the bounding box to window coordinate using ScrollView::contentToWindow().
    
            * dom/Range.cpp:
            (WebCore::Range::getBoundingClientRect):
            (WebCore::Range::boundingRect):
            * dom/Range.h:
            * editing/Editor.cpp:
            (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges):
            (WebCore::Editor::correctionPanelTimerFired):
            (WebCore::Editor::windowRectForRange):
            * editing/Editor.h:
            * manual-tests/autocorrection/autocorrection-in-iframe.html: Added.
            * manual-tests/autocorrection/document-for-iframe-test.html: Added.
    2011-03-04  Jia Pu  <jpu@apple.com>
    
            Reviewed by Darin Adler.
    
            On Mac, the bounding box sent to EditorClient::showCorrectionPanel() is incorrect when the correction occurs in an iframe.
            https://bugs.webkit.org/show_bug.cgi?id=55717
            <rdar://problem/9018127>
    
            With the change in WebCore, the bounding box passed into EditorClient::showCorrectionPanel()
            is already in window coordinate. We don't need to do further conversion anymore.
    
            * WebCoreSupport/WebEditorClient.mm:
            (WebEditorClient::showCorrectionPanel):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Range.h
+++ third_party/WebKit/Source/WebCore/dom/Range.h
@@ -112,6 +112,7 @@ public:
     // Transform-friendly
     void textQuads(Vector<FloatQuad>&, bool useSelectionHeight = false);
     void getBorderAndTextQuads(Vector<FloatQuad>&) const;
+    FloatRect boundingRect() const;
 
     void nodeChildrenChanged(ContainerNode*);
     void nodeChildrenWillBeRemoved(ContainerNode*);
