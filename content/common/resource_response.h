862d9cb34b5ad8f5a165aca479753c83bdc7d489
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-05 19:15:49 +0000

    Fix clang build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77049 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/resource_response.h
+++ content/common/resource_response.h
@@ -33,6 +33,9 @@ struct SyncLoadResult : ResourceResponseHead {
 
 // Simple wrapper that refcounts ResourceResponseHead.
 struct ResourceResponse : public base::RefCounted<ResourceResponse> {
+  ResourceResponse() {}
+  ~ResourceResponse() {}
+
   ResourceResponseHead response_head;
 
  private:
