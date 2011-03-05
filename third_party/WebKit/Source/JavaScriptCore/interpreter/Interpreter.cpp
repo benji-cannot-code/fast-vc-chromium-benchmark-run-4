55fbc6278a056551be0b69621cfd85b4c00a0de9
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-05 01:15:21 +0000

    Bug 54945 - The web page hangs towards the end of page load in Interpreter enabled javascript code in the latest webkit trunk.
    Rubber stamped by olliej.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::privateExecute):
        (1) don't infinite loop.
        (2) goto 1.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.cpp
@@ -2469,6 +2469,8 @@ JSValue Interpreter::privateExecute(ExecutionFlag flag, RegisterFile* registerFi
             exceptionValue = createErrorForInvalidGlobalAssignment(callFrame, ident.ustring());
             goto vm_throw;
         }
+
+        vPC += OPCODE_LENGTH(op_ensure_property_exists);
         NEXT_INSTRUCTION();
     }
     DEFINE_OPCODE(op_resolve_with_base) {
