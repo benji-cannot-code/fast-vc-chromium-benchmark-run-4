1b14805584a2ba51bad6911137cda5d00aac7c8a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-05 01:29:14 +0000

    Rename Paint() to PaintForDrag() in two cases.
    Both TextButton and MenuItemView called the method that implements
    their painting Paint(), which is also the name of the superclass's
    Paint() method. The painting stack currently looks:
    TextButton::Paint()
    TextButton::OnPaint()
    View::Paint()
    which is confusing.
    
    MenuButton overrides the method from TextButton, so rename it there, too.
    
    Also convert the 2nd parameter of this function from a bool to an enum.
    
    BUG=70465,72205
    TEST=none
    TBR=sky
    
    Review URL: http://codereview.chromium.org/6625023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77015 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -204,7 +204,7 @@ class BookmarkFolderButton : public views::MenuButton {
   }
 
   virtual void OnPaint(gfx::Canvas* canvas) {
-    views::MenuButton::Paint(canvas, false);
+    views::MenuButton::PaintButton(canvas, views::MenuButton::PB_NORMAL);
   }
 
  private:
@@ -1093,7 +1093,7 @@ void BookmarkBarView::WriteDragDataForView(View* sender,
     if (sender == GetBookmarkButton(i)) {
       views::TextButton* button = GetBookmarkButton(i);
       gfx::CanvasSkia canvas(button->width(), button->height(), false);
-      button->Paint(&canvas, true);
+      button->PaintButton(&canvas, views::TextButton::PB_FOR_DRAG);
       drag_utils::SetDragImageOnDataObject(canvas, button->size(), press_pt,
                                            data);
       WriteBookmarkDragData(model_->GetBookmarkBarNode()->GetChild(i), data);
