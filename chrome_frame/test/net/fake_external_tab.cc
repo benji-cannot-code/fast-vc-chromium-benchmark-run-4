f1984a8ed2f9ab4b3a345692b20358378ed3c08a
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-05 00:18:14 +0000

    Revert 77002 - Enable ChromeFrame net tests. These tests regressed with the change in the chunked upload code in net to move the formatting of individual chunks to the http network stack.
    This caused the URLRequestTestHTTP.TestPostChunkedDataBeforeStart test to hang in ChromeFrame.
    
    Fix is to ensure that the individual chunks are properly formatted while marshaling them over
    IPC.
    
    BUG=none
    TEST=chrome frame net tests should pass.
    
    Review URL: http://codereview.chromium.org/6626035
    
    TBR=ananta@chromium.org
    Review URL: http://codereview.chromium.org/6628029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77004 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -506,7 +506,10 @@ int main(int argc, char** argv) {
     return 0;
   }
 
+  return 0;
+
   // TODO(tommi): Stuff be broke. Needs a fixin'.
+#if 0
   // This is awkward: the TestSuite derived CFUrlRequestUnittestRunner contains
   // the instance of the AtExitManager that RegisterPathProvider() and others
   // below require. So we have to instantiate this first.
@@ -525,4 +528,5 @@ int main(int argc, char** argv) {
   PluginService::EnableChromePlugins(false);
   test_suite.RunMainUIThread();
   return 0;
+#endif
 }
