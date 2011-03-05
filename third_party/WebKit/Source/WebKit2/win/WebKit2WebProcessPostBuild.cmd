91196c7d5750979516cf8243d032d5dc909c3369
sfalken@apple.com <sfalken@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-05 01:07:24 +0000

    2011-03-04  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by Darin Adler.
    
            Adopt VersionStamper tool for Windows WebKit DLLs
            https://bugs.webkit.org/show_bug.cgi?id=55784
    
            Calculate the WebKit version for the useragent via autoversion.h
            instead of reading it back from the DLL.
            
            We now use a tool to stamp the version number onto the Apple WebKit DLLs
            during the post-build step.
    
            * WebKit.vcproj/WebKit.rc: Removed. This file wasn't used.
            * WebKit.vcproj/WebKitLibCommon.vsprops: Fetch version from a string resource.
            * WebView.cpp:
            (WebView::standardUserAgentWithApplicationName): Use __BUILD_NUMBER_SHORT__ for the version.
            (osVersion): Removed
    2011-03-04  Steve Falkenburg  <sfalken@apple.com>
    
            Reviewed by Darin Adler.
    
            Adopt VersionStamper tool for Windows WebKit DLLs
            https://bugs.webkit.org/show_bug.cgi?id=55784
            <rdar://problem/9021320>
    
            We now use a tool to stamp the version number onto the Apple WebKit DLLs
            during the post-build step.
    
            Fetch the WebKit version from a string resource instead of the version resource.
    
            * UIProcess/win/WebPageProxyWin.cpp:
            (WebKit::userVisibleWebKitVersionString): Fetch version from a string resource.
            * win/WebKit2.rc: Removed version, added stringtable.
            * win/WebKit2WebProcess.rc: Removed.
            * win/WebKit2WebProcess.vcproj:
            * win/WebKit2WebProcessPostBuild.cmd: Don't run autoversion script. It isn't used in this project.
            * win/WebKit2WebProcessPreBuild.cmd: Stamp version.
            * win/WebKitPostBuild.cmd: Stamp version.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/win/WebKit2WebProcessPostBuild.cmd
+++ third_party/WebKit/Source/WebKit2/win/WebKit2WebProcessPostBuild.cmd
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 mkdir 2>NUL "%OUTDIR%\..\bin\WebKit2WebProcess.resources"
 xcopy /y /d "%PROJECTDIR%\WebKit2WebProcess.resources\*" "%OUTDIR%\..\bin\WebKit2WebProcess.resources"
+if exist "%WEBKITLIBRARIESDIR%\tools\VersionStamper\VersionStamper.exe" "%WEBKITLIBRARIESDIR%\tools\VersionStamper\VersionStamper.exe" --verbose "%TARGETPATH%"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
