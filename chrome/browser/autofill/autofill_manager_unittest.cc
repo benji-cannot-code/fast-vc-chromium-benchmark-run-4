f6cbf03d0fe1f8ad9326917a87a4fd921fa4642a
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-06 23:45:25 +0000

    Autofill: Fix GetCreditCardType to return 'generic' if the number does not pass the Luhn test.
    BUG=70064
    TEST=none
    
    Review URL: http://codereview.chromium.org/6625004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager_unittest.cc
+++ chrome/browser/autofill/autofill_manager_unittest.cc
@@ -824,7 +824,7 @@ TEST_F(AutofillManagerTest, GetCreditCardSuggestionsEmptyValue) {
   string16 expected_labels[] = {ASCIIToUTF16("*3456"), ASCIIToUTF16("*8765")};
   string16 expected_icons[] = {
     ASCIIToUTF16("visaCC"),
-    ASCIIToUTF16("masterCardCC")
+    ASCIIToUTF16("genericCC")
   };
   int expected_unique_ids[] = {
     autofill_manager_->GetPackedCreditCardID(4),
@@ -903,7 +903,7 @@ TEST_F(AutofillManagerTest, GetCreditCardSuggestionsNonCCNumber) {
   string16 expected_labels[] = {ASCIIToUTF16("*3456"), ASCIIToUTF16("*8765")};
   string16 expected_icons[] = {
     ASCIIToUTF16("visaCC"),
-    ASCIIToUTF16("masterCardCC")
+    ASCIIToUTF16("genericCC")
   };
   int expected_unique_ids[] = {
     autofill_manager_->GetPackedCreditCardID(4),
@@ -1039,7 +1039,7 @@ TEST_F(AutofillManagerTest, GetAddressAndCreditCardSuggestions) {
   string16 expected_labels2[] = {ASCIIToUTF16("*3456"), ASCIIToUTF16("*8765")};
   string16 expected_icons2[] = {
     ASCIIToUTF16("visaCC"),
-    ASCIIToUTF16("masterCardCC")
+    ASCIIToUTF16("genericCC")
   };
   int expected_unique_ids2[] = {
     autofill_manager_->GetPackedCreditCardID(4),
