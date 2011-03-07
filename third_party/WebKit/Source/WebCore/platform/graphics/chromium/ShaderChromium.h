1bcd49182860b13751df46a5d072b6d65b28bab2
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-07 22:34:11 +0000

    2011-03-07  Adrienne Walker  <enne@google.com>
            Reviewed by James Robinson.
    
            [chromium] Add missing include to #define Skia parameter
            https://bugs.webkit.org/show_bug.cgi?id=55885
    
            * platform/graphics/chromium/ShaderChromium.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ShaderChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ShaderChromium.h
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PlatformString.h"
 
+#if USE(SKIA)
+#include "SkColorPriv.h"
+#endif
+
 namespace WebCore {
 
 class GraphicsContext3D;
