5b06e9bc42dd29571882b2d5b52fab9536d24fd3
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 16:52:48 +0000

    Fixes OpenSSL build: adds missing include following r77024.
    BUG=none
    TEST=builds with use_openssl=1
    
    Review URL: http://codereview.chromium.org/6626051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77144 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cert_database_openssl.cc
+++ net/base/cert_database_openssl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <openssl/x509.h>
 
 #include "base/logging.h"
+#include "net/base/crypto_module.h"
 #include "net/base/net_errors.h"
 #include "net/base/openssl_private_key_store.h"
 #include "net/base/x509_certificate.h"
