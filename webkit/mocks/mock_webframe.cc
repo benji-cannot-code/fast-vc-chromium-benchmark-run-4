5b63fcca06e45af4a4e6476420a594e9c61b11e0
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 03:36:28 +0000

    Definition for new API method in WebKit::WebFrame.
    The new method (pageDismissalEventBeingDispatched) has not yet been added to
    the API (see https://bugs.webkit.org/show_bug.cgi?id=55844). When it is, it
    will be a pure virtual method; clients will break unless they already have a
    concrete implementation for it.
    
    BUG=68780
    TEST=none
    
    Review URL: http://codereview.chromium.org/6626047
    Patch from Sreeram Ramachandran <sreeram@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77101 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/mocks/mock_webframe.cc
+++ webkit/mocks/mock_webframe.cc
@@ -236,6 +236,10 @@ bool MockWebFrame::willSuppressOpenerInNewFrame() const {
   return false;
 }
 
+bool MockWebFrame::pageDismissalEventBeingDispatched() const {
+  return false;
+}
+
 void MockWebFrame::replaceSelection(const WebString& text) {}
 
 void MockWebFrame::insertText(const WebString& text) {}
