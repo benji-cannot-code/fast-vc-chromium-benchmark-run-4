5978adbcc6bcc59603b75eaf6cbef126f289b9bc
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-07 22:31:29 +0000

    Source/WebCore: AX: WK1 needs to use ScrollView attachment for AXScrollArea, WK2 does not https://bugs.webkit.org/show_bug.cgi?id=55706
    Reviewed by Beth Dakin.
    
    * WebCore.exp.in:
    * accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::rootObjectForFrame):
    * accessibility/AXObjectCache.h:
    * accessibility/AccessibilityScrollView.cpp:
    (WebCore::AccessibilityScrollView::isAttachment):
    (WebCore::AccessibilityScrollView::widgetForAttachmentView):
    * accessibility/AccessibilityScrollView.h:
    (WebCore::AccessibilityScrollView::firstChild):
    * accessibility/mac/AccessibilityObjectWrapper.mm:
    (-[AccessibilityObjectWrapper accessibilityAttributeValue:]):
    
    Source/WebKit/mac: AX: WK1 needs to use ScrollView attachment for AXScrollArea, WK2 does not
    https://bugs.webkit.org/show_bug.cgi?id=55706
    
    Reviewed by Beth Dakin.
    
    Make sure WebDynamicScrollBarsView appear in the AX tree in WK1 (since this object
    returns the scroll bar objects).
    Make the accessibilityRoot object be the web area in WK1 because WebDynamicScrollBarsView
    will expect that as its child, instead of the WebCore ScrollView object.
    
    * WebView/WebDynamicScrollBarsView.mm:
    (-[WebDynamicScrollBarsView accessibilityIsIgnored]):
    * WebView/WebFrame.mm:
    (-[WebFrame accessibilityRoot]):
    
    Tools: AX: WK1 needs to use the ScrollView attachment for AXScrollArea, WK2 should not use this element.
    https://bugs.webkit.org/show_bug.cgi?id=55706
    
    Reviewed by Beth Dakin.
    
    The rootElement in DRT should point to the scroll view. In WK1, that will be the parent of the rootObject.
    
    * DumpRenderTree/mac/AccessibilityControllerMac.mm:
    (AccessibilityController::focusedElement):
       Remove FIXME comment about caching the focusedElement. In fact, we shouldn't cache this 
       because it could change at any time.
    (AccessibilityController::rootElement):
    
    LayoutTests: AX: WK1 needs to use ScrollView attachment for AXScrollArea, WK2 does not
    https://bugs.webkit.org/show_bug.cgi?id=55706
    
    Reviewed by Beth Dakin.
    
    * platform/mac/accessibility/webkit-scrollarea-expected.txt:
    * platform/mac/accessibility/webkit-scrollarea-position-expected.txt:
    * platform/mac/accessibility/webkit-scrollarea-position.html:
    * platform/mac/accessibility/webkit-scrollarea.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AXObjectCache.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AXObjectCache.cpp
@@ -291,6 +291,13 @@ AccessibilityObject* AXObjectCache::rootObject()
     return getOrCreate(m_document->view());
 }
 
+AccessibilityObject* AXObjectCache::rootObjectForFrame(Frame* frame)
+{
+    if (!frame)
+        return 0;
+    return getOrCreate(frame->view());
+}    
+    
 AccessibilityObject* AXObjectCache::getOrCreate(AccessibilityRole role)
 {
     RefPtr<AccessibilityObject> obj = 0;
