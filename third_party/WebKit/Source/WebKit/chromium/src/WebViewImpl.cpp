407be953bed27a54e947aba52958f3ba123e4b1f
bulach@chromium.org <bulach@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-07 18:20:24 +0000

    2011-03-07  Marcus Bulach  <bulach@chromium.org>
            Reviewed by Dan Bernstein.
    
            WebViewImpl::focusedWebCoreFrame() should be const.
            https://bugs.webkit.org/show_bug.cgi?id=55870
    
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::focusedWebCoreFrame):
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -911,7 +911,7 @@ void WebViewImpl::hideAutoFillPopup()
     }
 }
 
-Frame* WebViewImpl::focusedWebCoreFrame()
+Frame* WebViewImpl::focusedWebCoreFrame() const
 {
     return m_page.get() ? m_page->focusController()->focusedOrMainFrame() : 0;
 }
