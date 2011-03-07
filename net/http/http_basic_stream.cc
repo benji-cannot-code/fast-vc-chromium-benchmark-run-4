d1ce88ac235bf5bb0f20953a90fa7798eeff37a3
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 08:10:14 +0000

    Revert 77075 - Don't block on stream requests on Alternate-Protocol.
    Basically, after we get an HTTP header giving an Alternate-Protocol, we'll end up in a state where we're probably going to issue more HTTP requests. We have the already warm single HTTP connection. We probably don't have a SPDY session open. Previously, once we notice Alternate-Protocol, we'd block on trying to set up the new SPDY session, even though we still have the HTTP connection. Change this so we keep using HTTP until the SPDY session on the Alternate-Protocol becomes available, then we switch over to it.
    
    BUG=69688,75000
    TEST=Browse to belshe.com. Examine net-internals to make sure it works. Test over a variety of connections (direct / various proxy types).
    
    Review URL: http://codereview.chromium.org/6610034
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/6621040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77117 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_basic_stream.cc
+++ net/http/http_basic_stream.cc
@@ -112,8 +112,4 @@ void HttpBasicStream::GetSSLCertRequestInfo(
   parser_->GetSSLCertRequestInfo(cert_request_info);
 }
 
-bool HttpBasicStream::IsSpdyHttpStream() const {
-  return false;
-}
-
 }  // namespace net
