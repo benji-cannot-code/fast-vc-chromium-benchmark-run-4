26ef2b0a2d92d862726a8d674ba65e3f014405ca
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-07 22:54:55 +0000

    2011-03-07  Adam Barth  <abarth@webkit.org>
            Fix Chromium Mac build.  This header should only be included when the
            feature is enabled.
    
            * platform/cf/RunLoopTimerCF.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/cf/RunLoopTimerCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/cf/RunLoopTimerCF.cpp
@@ -28,9 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "RunLoopTimer.h"
 
 #if PLATFORM(MAC) && HAVE(RUNLOOP_TIMER)
+
+#include "RunLoopTimer.h"
 
 namespace WebCore {
 
