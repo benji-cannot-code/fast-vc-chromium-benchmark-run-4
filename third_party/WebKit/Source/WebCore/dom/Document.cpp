4ce124dc842677cdbb1bfa07ea8ce07dc7517810
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-07 19:19:43 +0000

    Unreviewed buildfix after r80478.
    * dom/Document.cpp: Add suggested parentheses to make GCC happy.
    (WebCore::Document::didReceiveTask):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -4730,7 +4730,7 @@ void Document::didReceiveTask(void* untypedContext)
         return;
 
     Page* page = document->page();
-    if (page && page->defersLoading() || !document->m_pendingTasks.isEmpty()) {
+    if ((page && page->defersLoading()) || !document->m_pendingTasks.isEmpty()) {
         document->m_pendingTasks.append(context->task.release());
         return;
     }
