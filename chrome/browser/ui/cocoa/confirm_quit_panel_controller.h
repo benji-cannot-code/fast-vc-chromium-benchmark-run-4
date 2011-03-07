a270096423ad8a15ec5322a30c170b40e813a623
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 18:15:26 +0000

    [Mac] Confirm-to-Quit: Wait for a KeyUp event if the user held Cmd+Q on the second tap.
    This also refactors the logic that was in AppController to be in the
    ConfirmQuitPanelController
    
    BUG=74976
    TEST=Press and hold Cmd+Q. Windows fade out and Chromium quits when you release the keys.
    TEST=Tap Cmd+Q and get the floaty window. Tap it again quickly and Chromium quits.
    TEST=Tap Cmd+Q to get the floaty window. Press and hold again and the windows hide and Chromium quits when the keys are released.
    
    Review URL: http://codereview.chromium.org/6625025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
+++ chrome/browser/ui/cocoa/confirm_quit_panel_controller.h
@@ -25,6 +25,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // does not currently exist.
 + (ConfirmQuitPanelController*)sharedController;
 
+// Checks whether the |event| should trigger the feature.
++ (BOOL)eventTriggersFeature:(NSEvent*)event;
+
+// Runs a modal loop that brings up the panel and handles the logic for if and
+// when to terminate. Returns NSApplicationTerminateReply for use in
+// -[NSApplicationDelegate applicationShouldTerminate:].
+- (NSApplicationTerminateReply)runModalLoopForApplication:(NSApplication*)app;
+
 // Shows the window.
 - (void)showWindow:(id)sender;
 
