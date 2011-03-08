5a87b676984916e708793baacad41e9f9cf822de
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 02:07:29 +0000

    IDMap derives from NonThreadSafe.
    During a code review I noticed that RenderViewHost::FromID was being called from the IO thread, but there were no automated assertions that fired.
    
    BUG=None
    TEST=DCHECK fires when I add bad code. Otherwise Chrome works without crashing in Debug build.
    
    Review URL: http://codereview.chromium.org/6591108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77232 0039d316-1c4b-4281-b951-d872f2087c98

--- base/id_map.h
+++ base/id_map.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/hash_tables.h"
 #include "base/logging.h"
+#include "base/threading/non_thread_safe.h"
 
 // Ownership semantics - own pointer means the pointer is deleted in Remove()
 // & during destruction
@@ -32,16 +33,23 @@ enum IDMapOwnershipSemantics {
 // This class does not have a virtual destructor, do not inherit from it when
 // ownership semantics are set to own because pointers will leak.
 template<typename T, IDMapOwnershipSemantics OS = IDMapExternalPointer>
-class IDMap {
+class IDMap : public base::NonThreadSafe {
  private:
   typedef int32 KeyType;
   typedef base::hash_map<KeyType, T*> HashTable;
 
  public:
   IDMap() : iteration_depth_(0), next_id_(1), check_on_null_data_(false) {
+    // A number of consumers of IDMap create it on one thread but always access
+    // it from a different, but consitent, thread post-construction.
+    DetachFromThread();
   }
 
   ~IDMap() {
+    // Many IDMap's are static, and hence will be destroyed on the main thread.
+    // However, all the accesses may take place on another thread, such as the
+    // IO thread. Detaching again to clean this up.
+    DetachFromThread();
     Releaser<OS, 0>::release_all(&data_);
   }
 
@@ -50,6 +58,7 @@ class IDMap {
 
   // Adds a view with an automatically generated unique ID. See AddWithID.
   KeyType Add(T* data) {
+    DCHECK(CalledOnValidThread());
     CHECK(!check_on_null_data_ || data);
     KeyType this_id = next_id_;
     DCHECK(data_.find(this_id) == data_.end()) << "Inserting duplicate item";
@@ -63,12 +72,14 @@ class IDMap {
   // this function, or allow this object to generate IDs and call Add. These
   // two methods may not be mixed, or duplicate IDs may be generated
   void AddWithID(T* data, KeyType id) {
+    DCHECK(CalledOnValidThread());
     CHECK(!check_on_null_data_ || data);
     DCHECK(data_.find(id) == data_.end()) << "Inserting duplicate item";
     data_[id] = data;
   }
 
   void Remove(KeyType id) {
+    DCHECK(CalledOnValidThread());
     typename HashTable::iterator i = data_.find(id);
     if (i == data_.end()) {
       NOTREACHED() << "Attempting to remove an item not in the list";
@@ -84,10 +95,12 @@ class IDMap {
   }
 
   bool IsEmpty() const {
+    DCHECK(CalledOnValidThread());
     return size() == 0u;
   }
 
   T* Lookup(KeyType id) const {
+    DCHECK(CalledOnValidThread());
     typename HashTable::const_iterator i = data_.find(id);
     if (i == data_.end())
       return NULL;
@@ -95,6 +108,7 @@ class IDMap {
   }
 
   size_t size() const {
+    DCHECK(CalledOnValidThread());
     return data_.size() - removed_ids_.size();
   }
 
@@ -106,28 +120,34 @@ class IDMap {
     Iterator(IDMap<T, OS>* map)
         : map_(map),
           iter_(map_->data_.begin()) {
+      DCHECK(map->CalledOnValidThread());
       ++map_->iteration_depth_;
       SkipRemovedEntries();
     }
 
     ~Iterator() {
+      DCHECK(map_->CalledOnValidThread());
       if (--map_->iteration_depth_ == 0)
         map_->Compact();
     }
 
     bool IsAtEnd() const {
+      DCHECK(map_->CalledOnValidThread());
       return iter_ == map_->data_.end();
     }
 
     KeyType GetCurrentKey() const {
+      DCHECK(map_->CalledOnValidThread());
       return iter_->first;
     }
 
     ReturnType* GetCurrentValue() const {
+      DCHECK(map_->CalledOnValidThread());
       return iter_->second;
     }
 
     void Advance() {
+      DCHECK(map_->CalledOnValidThread());
       ++iter_;
       SkipRemovedEntries();
     }
