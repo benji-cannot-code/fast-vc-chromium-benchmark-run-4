cdd95f7f74e8c6c83d35c649e8a552d91c3487d2
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 19:10:05 +0000

    safe_browsing::PrefixSet persistence code.
    Just the ability to read/write the data structure to a file.  File
    format is a simple header, the contents of the vectors, and a
    checksum.
    
    BUG=71832
    TEST=none
    
    Review URL: http://codereview.chromium.org/6625002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77314 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/prefix_set.h
+++ chrome/browser/safe_browsing/prefix_set.h
@@ -38,8 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // 2^16 apart, which would need 512k (versus 256k to store the raw
 // data).
 //
-// TODO(shess): Write serialization code.  Something like this should
-// work:
+// The on-disk format looks like:
 //         4 byte magic number
 //         4 byte version number
 //         4 byte |index_.size()|
@@ -56,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/safe_browsing/safe_browsing_util.h"
 
+class FilePath;
+
 namespace safe_browsing {
 
 class PrefixSet {
@@ -65,6 +66,10 @@ class PrefixSet {
 
   // |true| if |prefix| was in |prefixes| passed to the constructor.
   bool Exists(SBPrefix prefix) const;
+
+  // Persist the set on disk.
+  static PrefixSet* LoadFile(const FilePath& filter_name);
+  bool WriteFile(const FilePath& filter_name) const;
 
   // Regenerate the vector of prefixes passed to the constructor into
   // |prefixes|.  Prefixes will be added in sorted order.
@@ -78,6 +83,11 @@ class PrefixSet {
   // a new index entry.  This helps keep the worst-case performance
   // for |Exists()| under control.
   static const size_t kMaxRun = 100;
+
+  // Helper for |LoadFile()|.  Steals the contents of |index| and
+  // |deltas| using |swap()|.
+  PrefixSet(std::vector<std::pair<SBPrefix,size_t> > *index,
+            std::vector<uint16> *deltas);
 
   // Top-level index of prefix to offset in |deltas_|.  Each pair
   // indicates a base prefix and where the deltas from that prefix
