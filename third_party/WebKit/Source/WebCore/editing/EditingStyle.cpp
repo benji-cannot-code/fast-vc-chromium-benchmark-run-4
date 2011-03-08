b1d6a59260b8c6ab766527ac6d073ea3271d9837
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-08 20:12:09 +0000

    2011-03-08  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Darin Adler.
    
            addInlineStyleIfNeeded should take EditingStyle
            https://bugs.webkit.org/show_bug.cgi?id=55950
    
            Deployed EditingStyle in addInlineStyleIfNeeded, StyleChange::StyleChange, and StyleChange::init.
            Also extracted EditingStyle::styleIsPresentInComputedStyleOfNode from removeStyleFromRunBeforeApplyingStyle.
    
            * editing/ApplyStyleCommand.cpp:
            (WebCore::StyleChange::StyleChange): Takes EditingStyle instead of CSSStyleDeclaration.
            (WebCore::StyleChange::init): Ditto.
            (WebCore::ApplyStyleCommand::applyBlockStyle): Instantiates StyleChange.
            (WebCore::ApplyStyleCommand::applyInlineStyleToNodeRange): Calls addInlineStyleIfNeeded.
            (WebCore::ApplyStyleCommand::removeStyleFromRunBeforeApplyingStyle): Calls styleIsPresentInComputedStyleOfNode.
            (WebCore::ApplyStyleCommand::removeInlineStyleFromElement): Calls EditingStyle::mergeInlineStyleOfElement
            instead of manually merging styles.
            (WebCore::ApplyStyleCommand::applyInlineStyleToPushDown): Calls addInlineStyleIfNeeded.
            (WebCore::ApplyStyleCommand::addInlineStyleIfNeeded): Takes EditingStyle instead of CSSMutableStyleDeclaration.
            * editing/ApplyStyleCommand.h:
            * editing/CompositeEditCommand.h:
            * editing/EditingStyle.cpp:
            (WebCore::EditingStyle::styleIsPresentInComputedStyleOfNode): Extracted from removeStyleFromRunBeforeApplyingStyle.
            * editing/EditingStyle.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/EditingStyle.cpp
+++ third_party/WebKit/Source/WebCore/editing/EditingStyle.cpp
@@ -676,6 +676,11 @@ bool EditingStyle::extractConflictingImplicitStyleOfAttributes(HTMLElement* elem
     return removed;
 }
 
+bool EditingStyle::styleIsPresentInComputedStyleOfNode(Node* node) const
+{
+    return !m_mutableStyle || !getPropertiesNotIn(m_mutableStyle.get(), computedStyle(node).get())->length();
+}
+
 void EditingStyle::prepareToApplyAt(const Position& position, ShouldPreserveWritingDirection shouldPreserveWritingDirection)
 {
     if (!m_mutableStyle)
