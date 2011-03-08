5dc439038121fe2cd5ce5ff66df26355dfaba848
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 01:50:00 +0000

    mac: UI tweaks to tab overview mode
    BUG=50307
    TEST=tab overview mode looks different
    
    Review URL: http://codereview.chromium.org/6621056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabpose_window.h
+++ chrome/browser/ui/cocoa/tabpose_window.h
@@ -49,6 +49,9 @@ class TabStripModelObserverBridge;
   // The layer showing the background layer. Covers the whole visible area.
   CALayer* bgLayer_;  // weak
 
+  // Top gradient.
+  CALayer* topGradient_;  // weak
+
   // The layer drawn behind the currently selected tile.
   CALayer* selectionHighlight_;  // weak
 
