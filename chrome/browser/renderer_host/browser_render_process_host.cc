a6bc2b98e98f966e45e9e49474273ee425bcd549
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 22:45:39 +0000

    Add runtime enabled command-line flag for data transfer items
    BUG=75233
    TEST=none
    
    Review URL: http://codereview.chromium.org/6626067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -691,6 +691,7 @@ void BrowserRenderProcessHost::PropagateBrowserCommandLineToRenderer(
     switches::kEnableBenchmarking,
     switches::kEnableClickToPlay,
     switches::kEnableCrxlessWebApps,
+    switches::kEnableDataTransferItems,
     switches::kEnableDCHECK,
     switches::kEnableExperimentalExtensionApis,
     switches::kEnableInBrowserThumbnailing,
