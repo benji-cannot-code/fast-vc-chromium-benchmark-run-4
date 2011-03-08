a27b705a8a587316046ec0b744543d12b642c8ce
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 01:20:41 +0000

    Add new MouseEvent ctors; update WidgetWin & WindowWin usage.
    BUG=72040
    TEST=Manual mouse event testing.
    
    Review URL: http://codereview.chromium.org/6591120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77221 0039d316-1c4b-4281-b951-d872f2087c98

--- views/events/event.cc
+++ views/events/event.cc
@@ -39,6 +39,7 @@ Event::Event(NativeEvent2 native_event_2, ui::EventType type, int flags,
 ////////////////////////////////////////////////////////////////////////////////
 // LocatedEvent, protected:
 
+// TODO(msw): Kill this legacy constructor when we update uses.
 LocatedEvent::LocatedEvent(ui::EventType type, const gfx::Point& location,
                            int flags)
     : Event(type, flags),
@@ -52,9 +53,6 @@ LocatedEvent::LocatedEvent(const LocatedEvent& model, View* source,
   if (target)
     View::ConvertPointToView(source, target, &location_);
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// LocatedEvent, private:
 
 LocatedEvent::LocatedEvent(const LocatedEvent& model, RootView* root)
     : Event(model),
@@ -74,16 +72,17 @@ KeyEvent::KeyEvent(ui::EventType type, ui::KeyboardCode key_code,
 ////////////////////////////////////////////////////////////////////////////////
 // MouseEvent, public:
 
+// TODO(msw): Kill this legacy constructor when we update uses.
 MouseEvent::MouseEvent(ui::EventType type,
-                       View* from,
-                       View* to,
+                       View* source,
+                       View* target,
                        const gfx::Point &l,
                        int flags)
-    : LocatedEvent(MouseEvent(type, l.x(), l.y(), flags), from, to) {
+    : LocatedEvent(MouseEvent(type, l.x(), l.y(), flags), source, target) {
 }
 
-MouseEvent::MouseEvent(const MouseEvent& model, View* from, View* to)
-    : LocatedEvent(model, from, to) {
+MouseEvent::MouseEvent(const MouseEvent& model, View* source, View* target)
+    : LocatedEvent(model, source, target) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -98,17 +97,18 @@ TouchEvent::TouchEvent(ui::EventType type, int x, int y, int flags,
 
 
 TouchEvent::TouchEvent(ui::EventType type,
-                       View* from,
-                       View* to,
+                       View* source,
+                       View* target,
                        const gfx::Point& l,
                        int flags,
                        int touch_id)
-    : LocatedEvent(TouchEvent(type, l.x(), l.y(), flags, touch_id), from, to),
+    : LocatedEvent(TouchEvent(type, l.x(), l.y(), flags, touch_id), source,
+                              target),
       touch_id_(touch_id) {
 }
 
-TouchEvent::TouchEvent(const TouchEvent& model, View* from, View* to)
-    : LocatedEvent(model, from, to),
+TouchEvent::TouchEvent(const TouchEvent& model, View* source, View* target)
+    : LocatedEvent(model, source, target),
       touch_id_(model.touch_id_) {
 }
 #endif
