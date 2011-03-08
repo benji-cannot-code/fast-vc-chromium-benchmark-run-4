1478c6b83e1a3a0664797d7d6d7d753a6707d8ee
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 20:25:58 +0000

    SUMMARY: Create dropdown menu button on Windows Aero frame to be displayed when the user is synced. This is the first step in the multi-profile UI implementation on Windows. The displayed tag consists of two parts:
    1. A profile menu button with invisible border and background consisting of text and a dropdown arrow.
    2. A tag image displayed on the browser frame beneath the profile menu button.
    
    
    DETAILED CL DESCRIPTION:
    
    1. Changes to the frame and view
    
    * glass_browser_frame_view
    adds the profile_menu button to the frame, along with methods for laying it out, and watching for user name changes.
    
    2. New profile menu button
    
    * profile_menu_button
    sets up the actual menu button itself, and provides the method PaintProfileTag that draws the underlying tag image on the browser frame.  a better way to do this might be to create a new kind of menu button that combines methods from ImageButton as well as TextButton -- but maybe that's overkill?
    Review URL: http://codereview.chromium.org/6490022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/glass_browser_frame_view.h
+++ chrome/browser/ui/views/frame/glass_browser_frame_view.h
@@ -7,15 +7,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_FRAME_GLASS_BROWSER_FRAME_VIEW_H_
 #pragma once
 
+#include "base/scoped_ptr.h"
+#include "chrome/browser/prefs/pref_member.h"
 #include "chrome/browser/ui/views/frame/browser_frame_win.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "views/controls/button/button.h"
+#include "views/controls/menu/view_menu_delegate.h"
 #include "views/window/non_client_view.h"
 
 class BrowserView;
 class SkBitmap;
 
-class GlassBrowserFrameView : public BrowserNonClientFrameView {
+namespace views {
+class ProfileMenuButton;
+class ProfileMenuModel;
+class ProfileTagView;
+}
+
+class GlassBrowserFrameView : public BrowserNonClientFrameView,
+                              public NotificationObserver,
+                              public views::ViewMenuDelegate {
  public:
   // Constructs a non-client view for an BrowserFrame.
   GlassBrowserFrameView(BrowserFrame* frame, BrowserView* browser_view);
@@ -35,6 +46,9 @@ class GlassBrowserFrameView : public BrowserNonClientFrameView {
   virtual void GetWindowMask(const gfx::Size& size, gfx::Path* window_mask) { }
   virtual void EnableClose(bool enable) { }
   virtual void ResetWindowControls() { }
+
+  // views::ViewMenuDelegate implementation:
+  virtual void RunMenu(views::View* source, const gfx::Point& pt) OVERRIDE;
 
  protected:
   // Overridden from views::View:
@@ -65,6 +79,7 @@ class GlassBrowserFrameView : public BrowserNonClientFrameView {
   // Layout various sub-components of this view.
   void LayoutOTRAvatar();
   void LayoutClientView();
+  void LayoutProfileTag();
 
   // Returns the bounds of the client area for the specified view size.
   gfx::Rect CalculateClientAreaBounds(int width, int height) const;
@@ -75,6 +90,17 @@ class GlassBrowserFrameView : public BrowserNonClientFrameView {
 
   // Displays the next throbber frame.
   void DisplayNextThrobberFrame();
+
+  // NotificationObserver implementation:
+  virtual void Observe(NotificationType type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details) OVERRIDE;
+
+  // Receive notifications when the user's Google services user name changes.
+  void RegisterLoginNotifications();
+
+  // Returns true if the ProfileButton has been created.
+  bool show_profile_button() const { return profile_button_.get() != NULL; }
 
   // The layout rect of the OTR avatar icon, if visible.
   gfx::Rect otr_avatar_bounds_;
@@ -88,11 +114,23 @@ class GlassBrowserFrameView : public BrowserNonClientFrameView {
   // The bounds of the ClientView.
   gfx::Rect client_view_bounds_;
 
+  // Menu button that displays user's name and multi-profile menu.
+  scoped_ptr<views::ProfileMenuButton> profile_button_;
+
+  // Image tag displayed on frame beneath profile_button_.
+  scoped_ptr<views::ProfileTagView> profile_tag_;
+
+  // Multi-profile menu for profile_button_.
+  scoped_ptr<views::ProfileMenuModel> profile_menu_model_;
+
   // Whether or not the window throbber is currently animating.
   bool throbber_running_;
 
   // The index of the current frame of the throbber animation.
   int throbber_frame_;
+
+  // The Google services user name associated with this BrowserView's profile.
+  StringPrefMember username_pref_;
 
   static const int kThrobberIconCount = 24;
   static HICON throbber_icons_[kThrobberIconCount];
