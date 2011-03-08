ac99b88abdfef1044f984ffae6c59f5301babb0f
tschmelcher@google.com <tschmelcher@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 01:30:39 +0000

    Add support for Core Graphics with Carbon, allowing us to use Core Graphics in Firefox (since it does not support Cocoa). This lets us use O2D in FF without porting it to Quickdraw. Also tweak the drawing model behaviour so that we select the best possible model for the requested RenderMode.
    TEST=RenderMode=Auto and =2D in FF on an MBP6,2 and verified 2D avoids issues with the dynamic GPU switching feature of that hardware; RenderMode=2D in Chrome and Safari
    BUG=none
    
    Review URL: http://codereview.chromium.org/6626059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77223 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/features.h
+++ o3d/core/cross/features.h
@@ -83,8 +83,13 @@ class Features {
     return init_status_;
   }
 
-  Renderer::RenderModes render_mode() const {
+  Renderer::RenderMode render_mode() const {
     return render_mode_;
+  }
+
+  // For use by SuppressRenderModeAuto() in main_mac.mm.
+  void set_render_mode(Renderer::RenderMode render_mode) {
+    render_mode_ = render_mode;
   }
 
  private:
@@ -104,7 +109,7 @@ class Features {
   bool not_anti_aliased_;
   bool flip_textures_;
   Renderer::InitStatus init_status_;
-  Renderer::RenderModes render_mode_;
+  Renderer::RenderMode render_mode_;
 
   DISALLOW_COPY_AND_ASSIGN(Features);
 };
