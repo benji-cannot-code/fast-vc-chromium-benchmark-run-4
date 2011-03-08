26681d9c1c9bead77881ed55b24b7d91c737ffc6
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-08 03:17:48 +0000

    2011-03-07  Alexey Proskuryakov  <ap@apple.com>
            Reviewed by Darin Adler.
    
            REGRESSION (HTML5 tree builder): Text selection in a large text document is extremely slow
            https://bugs.webkit.org/show_bug.cgi?id=55898
    
            * platform/mac/fast/text/large-text-composed-char-expected.txt: The huge text run is split
            into multiple text nodes again.
    2011-03-07  Alexey Proskuryakov  <ap@apple.com>
    
            Reviewed by Darin Adler.
    
            REGRESSION (HTML5 tree builder): Text selection in a large text document is extremely slow
            https://bugs.webkit.org/show_bug.cgi?id=55898
    
            <rdar://problem/9095839> REGRESSION: Mail hangs with a certain large mail message when
            linkifying e-mail addresses in in -[DOMCharacterData setData:]
    
            Changes render tree of fast/text/large-text-composed-char.html, but not image results.
    
            * dom/CharacterData.cpp: (WebCore::CharacterData::parserAppendData):
            Changed parserAppendData() to accept a maximum length, and moved code from Text::createWithLengthLimit().
    
            * dom/CharacterData.h: Moved a constant for maximum length from Text.h.
    
            * dom/Text.cpp: (WebCore::Text::createWithLengthLimit):
            * dom/Text.h:
            This function was unused in ToT. Moved code around to share with CharacterData.
    
            * html/parser/HTMLConstructionSite.cpp: (WebCore::HTMLConstructionSite::insertTextNode):
            Chnaged to split large text nodes while parsing again.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/CharacterData.cpp
+++ third_party/WebKit/Source/WebCore/dom/CharacterData.cpp
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorInstrumentation.h"
 #include "MutationEvent.h"
 #include "RenderText.h"
+#include "TextBreakIterator.h"
+
+using namespace std;
 
 namespace WebCore {
 
@@ -52,12 +55,27 @@ String CharacterData::substringData(unsigned offset, unsigned count, ExceptionCo
     return m_data->substring(offset, count);
 }
 
-void CharacterData::parserAppendData(const String& data)
+unsigned CharacterData::parserAppendData(const UChar* data, unsigned dataLength, unsigned lengthLimit)
 {
-    String newStr = m_data;
-    newStr.append(data);
+    unsigned oldLength = m_data->length();
 
-    int oldLength = m_data->length();
+    unsigned end = min(dataLength, lengthLimit - oldLength);
+
+    // Check that we are not on an unbreakable boundary.
+    // Some text break iterator implementations work best if the passed buffer is as small as possible, 
+    // see <https://bugs.webkit.org/show_bug.cgi?id=29092>. 
+    // We need at least two characters look-ahead to account for UTF-16 surrogates.
+    if (end < dataLength) {
+        TextBreakIterator* it = characterBreakIterator(data, (end + 2 > dataLength) ? dataLength : end + 2);
+        if (!isTextBreak(it, end))
+            end = textBreakPreceding(it, end);
+    }
+    
+    if (!end)
+        return 0;
+
+    String newStr = m_data;
+    newStr.append(data, end);
     m_data = newStr.impl();
 
     updateRenderer(oldLength, 0);
@@ -65,6 +83,8 @@ void CharacterData::parserAppendData(const String& data)
     // parser to dispatch DOM mutation events.
     if (parentNode())
         parentNode()->childrenChanged();
+    
+    return end;
 }
 
 void CharacterData::appendData(const String& data, ExceptionCode&)
