a812126f7fe0df924c20980ade400e0c4088aba9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-08 01:51:39 +0000

    2011-03-07  Cosmin Truta  <ctruta@chromium.org>
            Reviewed by Adam Barth.
    
            SVG <image> referenced by <use> is displayed incorrectly
            https://bugs.webkit.org/show_bug.cgi?id=55750
    
            * svg/custom/resources/rgb.svg: Added.
            * svg/custom/use-image-in-g-expected.checksum: Added.
            * svg/custom/use-image-in-g-expected.png: Added.
            * svg/custom/use-image-in-g-expected.txt: Added.
            * svg/custom/use-image-in-g.svg: Added.
    2011-03-07  Cosmin Truta  <ctruta@chromium.org>
    
            Reviewed by Adam Barth.
    
            SVG <image> referenced by <use> is displayed incorrectly
            https://bugs.webkit.org/show_bug.cgi?id=55750
    
            The result of sourceURI must use the URL of the enclosing document as base.
            Otherwise, the base may sometimes be empty, and, as a consequence,
            the intermediate KURL object may be invalid.
    
            Test: svg/custom/use-image-in-g.svg
    
            * svg/SVGImageLoader.cpp:
            (WebCore::SVGImageLoader::sourceURI):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80516 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGImageLoader.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGImageLoader.cpp
@@ -50,7 +50,7 @@ void SVGImageLoader::dispatchLoadEvent()
 
 String SVGImageLoader::sourceURI(const AtomicString& attr) const
 {
-    return KURL(element()->baseURI(), stripLeadingAndTrailingHTMLSpaces(attr)).string();
+    return element()->document()->completeURL(stripLeadingAndTrailingHTMLSpaces(attr));
 }
 
 }
