932ccd3db4ec54b520ec61be9764581596cd67d0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 01:25:40 +0000

    Make OwnedWidgetGtk automatically destroy its widget.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/owned_widget_gtk.cc
+++ chrome/browser/ui/gtk/owned_widget_gtk.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 
 OwnedWidgetGtk::~OwnedWidgetGtk() {
-  DCHECK(!widget_) << "You must explicitly call OwnerWidgetGtk::Destroy().";
+  Destroy();
 }
 
 void OwnedWidgetGtk::Own(GtkWidget* widget) {
