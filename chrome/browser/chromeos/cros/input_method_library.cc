9cb46cd3d573f21591d59a92835688831167f88e
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 07:19:02 +0000

    Cleanup the candidate window earlier in the destruction process
    BUG=chromium-os:12715
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/6621064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/input_method_library.cc
+++ chrome/browser/chromeos/cros/input_method_library.cc
@@ -714,6 +714,7 @@ class InputMethodLibraryImpl : public InputMethodLibrary,
     if (type.value == NotificationType::APP_TERMINATING) {
       notification_registrar_.RemoveAll();
       StopInputMethodDaemon();
+      candidate_window_controller_.reset(NULL);
     }
   }
 
@@ -774,7 +775,8 @@ class InputMethodLibraryImpl : public InputMethodLibrary,
   // should be checked where we call libcros functions.
   bool initialized_successfully_;
 
-  // The candidate window.
+  // The candidate window.  This will be deleted when the APP_TERMINATING
+  // message is sent.
   scoped_ptr<CandidateWindowController> candidate_window_controller_;
 
   // The active input method ids cache.
