d62783a973afef7ea040b273990954e9ec788dee
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 00:42:07 +0000

    Pepper: Store file extensions and description on a per-mime-type basis.
    Also eliminate stupid and unnecessary conversion of data to a big string, only
    to be parsed back.
    
    BUG=none
    TEST=everything still works
    
    Review URL: http://codereview.chromium.org/6635009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pepper_plugin_registry.h
+++ chrome/common/pepper_plugin_registry.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/file_path.h"
+#include "webkit/plugins/npapi/webplugininfo.h"
 #include "webkit/plugins/ppapi/plugin_delegate.h"
 #include "webkit/plugins/ppapi/plugin_module.h"
 
@@ -29,12 +30,10 @@ struct PepperPluginInfo {
   bool is_out_of_process;
 
   FilePath path;  // Internal plugins have "internal-[name]" as path.
-  std::vector<std::string> mime_types;
   std::string name;
   std::string description;
   std::string version;
-  std::string file_extensions;
-  std::string type_descriptions;
+  std::vector<webkit::npapi::WebPluginMimeType> mime_types;
 
   // When is_internal is set, this contains the function pointers to the
   // entry points for the internal plugins.
--- webkit/plugins/npapi/webplugininfo.cc
+++ webkit/plugins/npapi/webplugininfo.cc
@@ -1,16 +1,26 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "webkit/plugins/npapi/webplugininfo.h"
 
 #include "base/logging.h"
+#include "base/utf_string_conversions.h"
 
 namespace webkit {
 namespace npapi {
 
 WebPluginMimeType::WebPluginMimeType() {}
+
+WebPluginMimeType::WebPluginMimeType(const std::string& m,
+                                     const std::string& f,
+                                     const std::string& d)
+    : mime_type(m),
+      file_extensions(),
+      description(ASCIIToUTF16(d)) {
+  file_extensions.push_back(f);
+}
 
 WebPluginMimeType::~WebPluginMimeType() {}
 
