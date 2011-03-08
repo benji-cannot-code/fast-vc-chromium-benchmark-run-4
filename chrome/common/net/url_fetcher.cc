553a7f1ab0eb39f97386208a4b33a05cd6de0367
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 22:27:10 +0000

    Fix URLFetcher::CancelAll().
    URLFetcher::CancelAll() has re-entrant behavior. We swap all the fetchers out of the global registry and then try to cancel them. This causes a problem because cancellation will try to update the global registry. This fixes the problem by not swapping the fetchers. Instead, we iterate until the registry is empty. This also has the property of working when killing the URLRequestJobs may recursively invoke behavior that creates/removes URLFetchers.
    
    Adds a new test for this.
    
    BUG=75212
    TEST=URLFetcherTest.CancelAll
    
    Review URL: http://codereview.chromium.org/6627072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/url_fetcher.cc
+++ chrome/common/net/url_fetcher.cc
@@ -78,6 +78,10 @@ class URLFetcher::Core
 
     void CancelAll();
 
+    int size() const {
+      return fetchers_.size();
+    }
+
    private:
     std::set<Core*> fetchers_;
 
@@ -182,12 +186,8 @@ void URLFetcher::Core::Registry::RemoveURLFetcherCore(Core* core) {
 }
 
 void URLFetcher::Core::Registry::CancelAll() {
-  std::set<Core*> fetchers;
-  fetchers.swap(fetchers_);
-
-  for (std::set<Core*>::iterator it = fetchers.begin();
-       it != fetchers.end(); ++it)
-    (*it)->CancelURLRequest();
+  while (!fetchers_.empty())
+    (*fetchers_.begin())->CancelURLRequest();
 }
 
 // static
@@ -578,6 +578,11 @@ void URLFetcher::ReceivedContentWasMalformed() {
 // static
 void URLFetcher::CancelAll() {
   Core::CancelAll();
+}
+
+// static
+int URLFetcher::GetNumFetcherCores() {
+  return Core::g_registry.Get().size();
 }
 
 URLFetcher::Delegate* URLFetcher::delegate() const {
