ec877437bfd3425dd0d4135c5c282f47a45137f5
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-09 08:45:37 +0000

    Do not update the input method menu when the browser window for the menu is not active.
    Before this CL, FOR_EACH_OBSERVER(InputMethodChanged) took ~80ms (when # of windows = 20). After this CL, it became ~50ms. About 40% reduction.
    
    BUG=chromium-os:9206
    TEST=see above.
    
    Review URL: http://codereview.chromium.org/6625082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/input_method_menu_button.cc
+++ chrome/browser/chromeos/status/input_method_menu_button.cc
@@ -14,7 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/status/status_area_host.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "views/window/window.h"
 
 namespace {
 
@@ -111,6 +115,21 @@ void InputMethodMenuButton::RunMenu(views::View* unused_source,
   menu_->RunMenu(unused_source, pt);
 }
 
+bool InputMethodMenuButton::WindowIsActive() {
+  Browser* active_browser = BrowserList::GetLastActive();
+  if (!active_browser) {
+    // Can't get an active browser. Just return true, which is safer.
+    return true;
+  }
+  BrowserWindow* active_window = active_browser->window();
+  const views::Window* current_window = GetWindow();
+  if (!active_window || !current_window) {
+    // Can't get an active or current window. Just return true as well.
+    return true;
+  }
+  return active_window->GetNativeHandle() == current_window->GetNativeWindow();
+}
+
 void InputMethodMenuButton::UpdateUI(const std::string& input_method_id,
                                      const std::wstring& name,
                                      const std::wstring& tooltip,
@@ -131,7 +150,19 @@ void InputMethodMenuButton::UpdateUI(const std::string& input_method_id,
     SetTooltipText(tooltip);
   }
   SetText(name);
-  SchedulePaint();
+
+  if (WindowIsActive()) {
+    // We don't call these functions if the |current_window| is not active since
+    // the calls are relatively expensive (crosbug.com/9206). Please note that
+    // PrepareMenu() is necessary for fixing crosbug.com/7522 when the window
+    // is active.
+    menu_->PrepareMenu();
+    SchedulePaint();
+  }
+
+  // TODO(yusukes): For a window which isn't on top, probably it's better to
+  // update the texts when the window gets activated because SetTooltipText()
+  // and SetText() are also expensive.
 }
 
 void InputMethodMenuButton::OpenConfigUI() {
