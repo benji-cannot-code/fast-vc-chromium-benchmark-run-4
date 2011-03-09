1143c64870daa6befe83306c33a0d8ebadc022d6
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-09 19:11:53 +0000

    GTK: Set WM_CLASS to webapp names on webapp windows.
    BUG=20587
    TEST=1) Open chrome --app=<somesite> 2) run xprop and click the window. WM_CLASS should be set to the webapp name instead of "Google Chrome"
    
    Review URL: http://codereview.chromium.org/6647008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77495 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -177,6 +177,7 @@ class Browser : public TabHandlerDelegate,
   // Accessors ////////////////////////////////////////////////////////////////
 
   Type type() const { return type_; }
+  const std::string& app_name() const { return app_name_; }
   Profile* profile() const { return profile_; }
   const std::vector<std::wstring>& user_data_dir_profiles() const;
 
