815d837ee8aeda6e20c2aa5ec293dfedfa51ae34
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-10 23:50:56 +0000

    2011-03-10  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Ryosuke Niwa.
    
            InsertUnorderedList over a non-editable region and multiple lines enters an infinite loop
            https://bugs.webkit.org/show_bug.cgi?id=53409
    
            Avoiding crashes and infinite loops when listifying content with mixed-editability
    
            * editing/execCommand/insert-list-with-noneditable-content-expected.txt: Added.
            * editing/execCommand/insert-list-with-noneditable-content.html: Added.
    2011-03-10  Levi Weintraub  <leviw@chromium.org>
    
            Reviewed by Ryosuke Niwa.
    
            InsertUnorderedList over a non-editable region and multiple lines enters an infinite loop
            https://bugs.webkit.org/show_bug.cgi?id=53409
    
            Fixing broken handling of mixed-editability content for InsertListCommand. Previously, if the selection
            spanned non-contenteditable regions, it would get stuck endlessly iterating the same region as the algorithm
            didn't skip the editable boundary.
    
            Test: editing/execCommand/insert-list-with-noneditable-content.html
    
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::cleanupAfterDeletion): Changed signature to take the destination
            position for the active editing command. Without this, there are cases when the destination happens
            to be a placeholder, and we remove it.
            (WebCore::CompositeEditCommand::moveParagraphs):
            * editing/CompositeEditCommand.h:
            * editing/InsertListCommand.cpp:
            (WebCore::InsertListCommand::doApply): Added logic to the paragraph iteration loop to handle pockets of
            non-editable content in an editable context. Previously, this could cause an infinite loop.
            * editing/visible_units.cpp:
            (WebCore::startOfParagraph): Added a mode of operation where we'll jump across non-editable
            content in the same paragraph to reach the actual editable paragraph start.
            (WebCore::endOfParagraph): Ditto.
            (WebCore::startOfNextParagraph): Now uses the aforementioned non-editable content skipping mode of
            endOfParagraph.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
@@ -831,10 +831,10 @@ void CompositeEditCommand::cloneParagraphUnderNewElement(Position& start, Positi
 // Deleting a paragraph will leave a placeholder. Remove it (and prune
 // empty or unrendered parents).
 
-void CompositeEditCommand::cleanupAfterDeletion()
+void CompositeEditCommand::cleanupAfterDeletion(VisiblePosition destination)
 {
     VisiblePosition caretAfterDelete = endingSelection().visibleStart();
-    if (isStartOfParagraph(caretAfterDelete) && isEndOfParagraph(caretAfterDelete)) {
+    if (caretAfterDelete != destination && isStartOfParagraph(caretAfterDelete) && isEndOfParagraph(caretAfterDelete)) {
         // Note: We want the rightmost candidate.
         Position position = caretAfterDelete.deepEquivalent().downstream();
         Node* node = position.deprecatedNode();
@@ -948,8 +948,8 @@ void CompositeEditCommand::moveParagraphs(const VisiblePosition& startOfParagrap
         }
     }
     
-    VisiblePosition beforeParagraph = startOfParagraphToMove.previous();
-    VisiblePosition afterParagraph(endOfParagraphToMove.next());
+    VisiblePosition beforeParagraph = startOfParagraphToMove.previous(CannotCrossEditingBoundary);
+    VisiblePosition afterParagraph(endOfParagraphToMove.next(CannotCrossEditingBoundary));
 
     // We upstream() the end and downstream() the start so that we don't include collapsed whitespace in the move.
     // When we paste a fragment, spaces after the end and before the start are treated as though they were rendered.
@@ -986,8 +986,7 @@ void CompositeEditCommand::moveParagraphs(const VisiblePosition& startOfParagrap
     deleteSelection(false, false, false, false);
 
     ASSERT(destination.deepEquivalent().anchorNode()->inDocument());
-
-    cleanupAfterDeletion();
+    cleanupAfterDeletion(destination);
     ASSERT(destination.deepEquivalent().anchorNode()->inDocument());
 
     // Add a br if pruning an empty block level element caused a collapse. For example:
