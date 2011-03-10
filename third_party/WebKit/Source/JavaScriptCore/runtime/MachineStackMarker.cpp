a5a1ef2e2985165029563733a704bb6900257076
luiz@webkit.org <luiz@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-10 20:40:07 +0000

    [Qt] fast/workers/stress-js-execution.html is crashing on Qt bot (intermittently) https://bugs.webkit.org/show_bug.cgi?id=33008
    Reviewed by Andreas Kling.
    
    Source/JavaScriptCore:
    
    Defining WTF_USE_PTHREAD_BASED_QT=1 for platforms where QThread uses pthread internally.
    Symbian is excluded because pthread_kill does not work on it. Mac is excluded because
    it has its own ways to do JSC threading.
    
    Defining WTF_USE_PTHREADS inside MachineStackMarker.cpp if USE(PTHREAD_BASED_QT) is true.
    
    * runtime/MachineStackMarker.cpp:
    * wtf/Platform.h:
    
    LayoutTests:
    
    Removing several tests from the Qt skip list. Those tests were flaky because of race conditions
    related to web workers.
    
    * platform/qt/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/MachineStackMarker.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/MachineStackMarker.cpp
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <setjmp.h>
 #include <stdlib.h>
 
+#if USE(PTHREAD_BASED_QT) && !defined(WTF_USE_PTHREADS)
+#define WTF_USE_PTHREADS 1
+#endif
+
 #if OS(DARWIN)
 
 #include <mach/mach_init.h>
