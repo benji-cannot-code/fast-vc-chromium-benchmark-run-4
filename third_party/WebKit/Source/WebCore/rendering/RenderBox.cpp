ce54d46d92ad8e8ef1043bd7ff704586ef8fe9ea
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-10 23:11:32 +0000

    https://bugs.webkit.org/show_bug.cgi?id=47143
    Reviewed by Beth Dakin.
    
    Static distance computation is wrong when an ancestor is also positioned.  We need to go up the container()
    chain (after initially starting with the parent()) when doing this computation, so that we properly skip
    intermediate boxes between two positioned blocks.
    
    Added fast/block/positioning/static-distance-with-positioned-ancestor.html.
    
    Source/WebCore: 
    
    * rendering/RenderBox.cpp:
    (WebCore::computeInlineStaticDistance):
    (WebCore::computeBlockStaticDistance):
    
    LayoutTests: 
    
    * fast/block/positioning/static-distance-with-positioned-ancestor.html: Added.
    * platform/mac/fast/block/positioning/static-distance-with-positioned-ancestor-expected.checksum: Added.
    * platform/mac/fast/block/positioning/static-distance-with-positioned-ancestor-expected.png: Added.
    * platform/mac/fast/block/positioning/static-distance-with-positioned-ancestor-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -2102,7 +2102,7 @@ static void computeInlineStaticDistance(Length& logicalLeft, Length& logicalRigh
     // FIXME: The static distance computation has not been patched for mixed writing modes yet.
     if (containerDirection == LTR) {
         int staticPosition = child->layer()->staticInlinePosition() - containerBlock->borderLogicalLeft();
-        for (RenderObject* curr = child->parent(); curr && curr != containerBlock; curr = curr->parent()) {
+        for (RenderObject* curr = child->parent(); curr && curr != containerBlock; curr = curr->container()) {
             if (curr->isBox())
                 staticPosition += toRenderBox(curr)->logicalLeft();
         }
@@ -2111,7 +2111,7 @@ static void computeInlineStaticDistance(Length& logicalLeft, Length& logicalRigh
         RenderBox* enclosingBox = child->parent()->enclosingBox();
         int staticPosition = child->layer()->staticInlinePosition() + containerLogicalWidth + containerBlock->borderLogicalRight();
         staticPosition -= enclosingBox->logicalWidth();
-        for (RenderObject* curr = enclosingBox; curr && curr != containerBlock; curr = curr->parent()) {
+        for (RenderObject* curr = enclosingBox; curr && curr != containerBlock; curr = curr->container()) {
             if (curr->isBox())
                 staticPosition -= toRenderBox(curr)->logicalLeft();
         }
@@ -2453,7 +2453,7 @@ static void computeBlockStaticDistance(Length& logicalTop, Length& logicalBottom
     
     // FIXME: The static distance computation has not been patched for mixed writing modes.
     int staticLogicalTop = child->layer()->staticBlockPosition() - containerBlock->borderBefore();
-    for (RenderObject* curr = child->parent(); curr && curr != containerBlock; curr = curr->parent()) {
+    for (RenderObject* curr = child->parent(); curr && curr != containerBlock; curr = curr->container()) {
         if (curr->isBox() && !curr->isTableRow())
             staticLogicalTop += toRenderBox(curr)->logicalTop();
     }
