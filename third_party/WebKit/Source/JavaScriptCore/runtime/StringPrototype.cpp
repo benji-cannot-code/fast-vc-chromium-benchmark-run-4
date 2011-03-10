b3df49838b45510dfabcb29e66ebcbfaf8c9beeb
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-10 20:31:21 +0000

    Bug 56077 - ES5 conformance issues with RegExp.prototype
    Reviewed by Oliver Hunt.
    
    There are three issues causing test failures in sputnik.
    
    (1) lastIndex should be converted at the point it is used, not the point it is set (this is visible if valueOf is overridden).
    (2) The 'length' property of the test/exec functions should be 1.
    (3) If no input is specified, the input to test()/exec() is "undefined" (i.e. ToString(undefined)) - not RegExp.input.
    
    Source/JavaScriptCore: 
    
    * runtime/RegExpObject.cpp:
    (JSC::RegExpObject::markChildren):
        - Added to mark lastIndex
    (JSC::regExpObjectLastIndex):
    (JSC::setRegExpObjectLastIndex):
        - lastIndex is now stored as a JSValue.
    (JSC::RegExpObject::match):
        - Use accessor methods to get/set lastIndex, add fast case for isUInt32 (don't convert to double).
    * runtime/RegExpObject.h:
    (JSC::RegExpObject::setLastIndex):
    (JSC::RegExpObject::setLastIndex):
        - Set lastIndex, either from a size_t or a JSValue.
    (JSC::RegExpObject::getLastIndex):
        - Get lastIndex.
    (JSC::RegExpObject::RegExpObjectData::RegExpObjectData):
        - Initialize as a JSValue.
    * runtime/RegExpPrototype.cpp:
    (JSC::RegExpPrototype::RegExpPrototype):
        - Add test/exec properties with length 1.
    * runtime/StringPrototype.cpp:
    (JSC::stringProtoFuncMatch):
    (JSC::stringProtoFuncSearch):
        - Do not read RegExp.input if none is provided.
    * tests/mozilla/js1_2/regexp/RegExp_input.js:
    * tests/mozilla/js1_2/regexp/RegExp_input_as_array.js:
        - Update these tests (they relied on non-ES5 behaviour).
    
    LayoutTests: 
    
    * fast/js/kde/function_length-expected.txt:
    * fast/js/kde/script-tests/function_length.js:
        - 'length' of RexExp.test/RexExp.exec is 1
    * fast/js/regexp-caching-expected.txt:
    * fast/js/regexp-test-null-string-expected.txt:
    * fast/js/regexp-test-null-string.html:
        - test() is equivalent to test("undefined")
    * sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.6/15.10.6.2_RegExp.prototype.exec/S15.10.6.2_A11-expected.txt:
    * sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.6/15.10.6.2_RegExp.prototype.exec/S15.10.6.2_A1_T16-expected.txt:
    * sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.6/15.10.6.2_RegExp.prototype.exec/S15.10.6.2_A4_T11-expected.txt:
    * sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.6/15.10.6.3_RegExp.prototype.test/S15.10.6.3_A11-expected.txt:
    * sputnik/Conformance/15_Native_Objects/15.10_RegExp/15.10.6/15.10.6.3_RegExp.prototype.test/S15.10.6.3_A1_T16-expected.txt:
        - These tests now pass!
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/StringPrototype.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/StringPrototype.cpp
@@ -605,9 +605,7 @@ EncodedJSValue JSC_HOST_CALL stringProtoFuncMatch(ExecState* exec)
 
     JSValue a0 = exec->argument(0);
 
-    UString u = s;
     RefPtr<RegExp> reg;
-    RegExpObject* imp = 0;
     if (a0.inherits(&RegExpObject::s_info))
         reg = asRegExpObject(a0)->regExp();
     else {
@@ -621,7 +619,7 @@ EncodedJSValue JSC_HOST_CALL stringProtoFuncMatch(ExecState* exec)
     RegExpConstructor* regExpConstructor = exec->lexicalGlobalObject()->regExpConstructor();
     int pos;
     int matchLength = 0;
-    regExpConstructor->performMatch(reg.get(), u, 0, pos, matchLength);
+    regExpConstructor->performMatch(reg.get(), s, 0, pos, matchLength);
     if (!(reg->global())) {
         // case without 'g' flag is handled like RegExp.prototype.exec
         if (pos < 0)
@@ -631,15 +629,13 @@ EncodedJSValue JSC_HOST_CALL stringProtoFuncMatch(ExecState* exec)
 
     // return array of matches
     MarkedArgumentBuffer list;
-    int lastIndex = 0;
+    unsigned lastIndex = 0;
     while (pos >= 0) {
-        list.append(jsSubstring(exec, u, pos, matchLength));
+        list.append(jsSubstring(exec, s, pos, matchLength));
         lastIndex = pos;
         pos += matchLength == 0 ? 1 : matchLength;
-        regExpConstructor->performMatch(reg.get(), u, pos, pos, matchLength);
+        regExpConstructor->performMatch(reg.get(), s, pos, pos, matchLength);
     }
-    if (imp)
-        imp->setLastIndex(lastIndex);
     if (list.isEmpty()) {
         // if there are no matches at all, it's important to return
         // Null instead of an empty array, because this matches
@@ -659,7 +655,6 @@ EncodedJSValue JSC_HOST_CALL stringProtoFuncSearch(ExecState* exec)
 
     JSValue a0 = exec->argument(0);
 
-    UString u = s;
     RefPtr<RegExp> reg;
     if (a0.inherits(&RegExpObject::s_info))
         reg = asRegExpObject(a0)->regExp();
@@ -674,7 +669,7 @@ EncodedJSValue JSC_HOST_CALL stringProtoFuncSearch(ExecState* exec)
     RegExpConstructor* regExpConstructor = exec->lexicalGlobalObject()->regExpConstructor();
     int pos;
     int matchLength = 0;
-    regExpConstructor->performMatch(reg.get(), u, 0, pos, matchLength);
+    regExpConstructor->performMatch(reg.get(), s, 0, pos, matchLength);
     return JSValue::encode(jsNumber(pos));
 }
 
