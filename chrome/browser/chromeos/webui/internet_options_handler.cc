0a9d87ec6cc0ea5c199e5c0921916cce097c4899
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 22:29:49 +0000

    Revert 77702 - Use async connect and set primitives from libcros.
    BUG=chromium-os:12821
    TEST=Manual, test that open wifi, PSK, Google-A, and cell network connections still work, and that wrong-key PSK still produces an error
    
    Review URL: http://codereview.chromium.org/6628023
    
    TBR=stevenjb@google.com
    Review URL: http://codereview.chromium.org/6665008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77713 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/webui/internet_options_handler.cc
+++ chrome/browser/chromeos/webui/internet_options_handler.cc
@@ -670,7 +670,8 @@ void InternetOptionsHandler::LoginCallback(const ListValue* args) {
 
   chromeos::NetworkLibrary* cros =
       chromeos::CrosLibrary::Get()->GetNetworkLibrary();
-  cros->ConnectToWifiNetwork(service_path);
+  cros->ConnectToWifiNetwork(
+      service_path, password, std::string(), std::string());
 }
 
 void InternetOptionsHandler::LoginCertCallback(const ListValue* args) {
@@ -694,7 +695,8 @@ void InternetOptionsHandler::LoginCertCallback(const ListValue* args) {
     if (network)
       password = network->passphrase();
   }
-  cros->ConnectToWifiNetwork(service_path);
+  cros->ConnectToWifiNetwork(
+      service_path, password, identity, certpath);
 }
 
 void InternetOptionsHandler::LoginToOtherCallback(const ListValue* args) {
@@ -799,7 +801,8 @@ void InternetOptionsHandler::HandleWifiButtonClick(
           CreateModalPopup(new chromeos::NetworkConfigView(network));
         }
       } else {
-        cros->ConnectToWifiNetwork(service_path);
+        cros->ConnectToWifiNetwork(
+            service_path, std::string(), std::string(), std::string());
       }
     } else if (command == "disconnect") {
       cros->DisconnectFromWirelessNetwork(network);
