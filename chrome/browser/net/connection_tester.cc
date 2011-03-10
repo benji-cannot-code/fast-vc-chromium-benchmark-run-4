c1bb5d687f88ad67e8fcc093dbce98e1f89cf503
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 22:36:15 +0000

    connection_tester.cc doesn't need to include net/disk_cache/disk_cache.h.
    R=rvargas
    BUG=none
    TEST=no compilation errors
    
    Review URL: http://codereview.chromium.org/6657014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/connection_tester.cc
+++ chrome/browser/net/connection_tester.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
 #include "net/base/ssl_config_service_defaults.h"
-#include "net/disk_cache/disk_cache.h"
 #include "net/ftp/ftp_network_layer.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_cache.h"
