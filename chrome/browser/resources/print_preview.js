41078fa3f5b02f8f972cbd7230c1ab6deaf575a0
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 21:31:27 +0000

    PrintPreview: Modified print preview WebUI to match the design mocks.
    (1) Added page range example text.
    (2) Added radio button to page range textbox.
    (3) Changed "All" checkbox to radio button.
    
    BUG=none
    TEST=Enable print preview. Press ctrl+p on any tab. In the print preview tab, new pages option is displayed.
    
    Review URL: http://codereview.chromium.org/6626017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77699 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -20,6 +20,7 @@ function load() {
     window.close();
   });
 
+  $('pages').addEventListener('input', selectPageRange);
   chrome.send('getPrinters');
   chrome.send('getPreview');
 };
@@ -153,6 +154,10 @@ function createPDFPlugin(url, pagesCount) {
     return;
   }
   pdfPlugin.onload('onPDFLoad()');
+}
+
+function selectPageRange() {
+  $('print-pages').checked = true;
 }
 
 window.addEventListener('DOMContentLoaded', load);
