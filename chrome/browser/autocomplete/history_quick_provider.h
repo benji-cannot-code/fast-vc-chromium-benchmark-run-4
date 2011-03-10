ea095067aaf57012970e5959196a4d5091676099
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 17:23:14 +0000

    Implemented substring matching within page titles. Fixed bug where URL was being lower-cased. Added unit tests for page title matching as well as unit tests for various static functions and other areas not previously unit tested.
    BUG=58958,57853,58559,60107,75245
    TEST=Many unit tests added.
    
    Review URL: http://codereview.chromium.org/6652008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_quick_provider.h
+++ chrome/browser/autocomplete/history_quick_provider.h
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/autocomplete/history_provider.h"
 #include "chrome/browser/history/history_types.h"
 #include "chrome/browser/history/in_memory_url_index.h"
 
 class Profile;
+class TermMatches;
 
 namespace history {
 class HistoryBackend;
@@ -41,16 +43,11 @@ class HistoryQuickProvider : public HistoryProvider {
 
  private:
   friend class HistoryQuickProviderTest;
+  FRIEND_TEST_ALL_PREFIXES(HistoryQuickProviderTest, Spans);
 
   AutocompleteMatch QuickMatchToACMatch(
       const history::ScoredHistoryMatch& history_match,
-      MatchType match_type,
       size_t match_number);
-
-  // Breaks a string down into individual words and return as a vector with
-  // the individual words in their original order.
-  static history::InMemoryURLIndex::String16Vector WordVectorFromString16(
-      const string16& uni_string);
 
   // Determines the relevance for some input, given its type and which match it
   // is.  If |match_type| is NORMAL, |match_number| is a number
@@ -64,10 +61,18 @@ class HistoryQuickProvider : public HistoryProvider {
   // Returns the index that should be used for history lookups.
   history::InMemoryURLIndex* GetIndex();
 
+  // Fill and return an ACMatchClassifications structure given the term
+  // matches (|matches|) to highlight where terms were found. |adjust| is
+  // subtracted form each offset and is used to account for any leading
+  // 'http://' in the potential result.
+  static ACMatchClassifications SpansFromTermMatch(
+      const history::TermMatches& matches,
+      size_t text_length,
+      size_t adjust);
+
   // Only for use in unittests.  Takes ownership of |index|.
   void SetIndexForTesting(history::InMemoryURLIndex* index);
   AutocompleteInput autocomplete_input_;
-  bool trim_http_;
   std::string languages_;
 
   // Only used for testing.
