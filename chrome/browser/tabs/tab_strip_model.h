7c96043d491852b0ad27dacf5661572d693102a2
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 01:41:15 +0000

    Remove the notification stub files.
    This is the last part of this series.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6658015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model.h
+++ chrome/browser/tabs/tab_strip_model.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "chrome/browser/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/tabs/tab_strip_selection_model.h"
-#include "chrome/common/notification_observer.h"
-#include "chrome/common/notification_registrar.h"
 #include "chrome/common/page_transition_types.h"
+#include "content/common/notification_observer.h"
+#include "content/common/notification_registrar.h"
 
 class NavigationController;
 class Profile;
