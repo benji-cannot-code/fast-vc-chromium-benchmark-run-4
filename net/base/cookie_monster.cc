dd48075fa5304326bb9613e727f50fd29a53931c
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 07:16:15 +0000

    Add in a checker to help narrow down 74585.
    Original author is rdsmith (http://codereview.chromium.org/6660014/) -- I am just checking this in so gets picked up in time for canary build tommorow.
    
    BUG=74585
    TEST=Try bots, Cookie monster unit tests that have checker added, tab_specific_contents unit tests.
    
    Review URL: http://codereview.chromium.org/6659029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77608 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/cookie_monster.cc
+++ net/base/cookie_monster.cc
@@ -800,6 +800,14 @@ CookieMonster* CookieMonster::GetCookieMonster() {
   return this;
 }
 
+void CookieMonster::ValidateMap() {
+  base::AutoLock autolock(lock_);
+  // Skipping InitIfNecessary() to allow validation before first use.
+
+  for (CookieMap::iterator it = cookies_.begin(); it != cookies_.end(); ++it)
+    CHECK(it->second);
+}
+
 CookieMonster::~CookieMonster() {
   DeleteAll(false);
 }
