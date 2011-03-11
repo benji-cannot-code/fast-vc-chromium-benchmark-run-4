cd1363a6ab631beb8a2928c6e41e995217bb09f9
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 17:45:05 +0000

    Coverity: Fix unnecessary call by values.
    CID 13152, 14252
    
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6665016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77826 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_redirect_job.cc
+++ net/url_request/url_request_redirect_job.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 URLRequestRedirectJob::URLRequestRedirectJob(URLRequest* request,
-                                             GURL redirect_destination)
+                                             const GURL& redirect_destination)
     : URLRequestJob(request), redirect_destination_(redirect_destination) {
 }
 
