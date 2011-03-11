4efd8b2d19805584c3f931ac53937cdd377f59ad
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 00:14:28 +0000

    importer: More cleanups to ImporterHost class.
    - Remove unused include.
    - Remove unnecessary forward declarations.
    - Move some overridden methods to the private section and while here add OVERRIDE to them.
    - Put more order to the methods in the source file.
    - And most important, make more data members of ImporterHost private to it.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6667006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_host.cc
+++ chrome/browser/importer/importer_host.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/message_box_win.h"
 #endif
 
+// ImporterHost ----------------------------------------------------------------
+
 ImporterHost::ImporterHost()
     : profile_(NULL),
       task_(NULL),
@@ -247,24 +249,13 @@ void ImporterHost::CheckForLoadedModels(uint16 items) {
   }
 }
 
+// ExternalProcessImporterHost -------------------------------------------------
+
 ExternalProcessImporterHost::ExternalProcessImporterHost()
     : items_(0),
       import_to_bookmark_bar_(false),
       cancelled_(false),
       import_process_launched_(false) {
-}
-
-void ExternalProcessImporterHost::Loaded(BookmarkModel* model) {
-  DCHECK(model->IsLoaded());
-  model->RemoveObserver(this);
-  waiting_for_bookmarkbar_model_ = false;
-  installed_bookmark_observer_ = false;
-
-  // Because the import process is running externally, the decision whether
-  // to import to the bookmark bar must be stored here so that it can be
-  // passed to the importer when the import task is invoked.
-  import_to_bookmark_bar_ = (!model->HasBookmarks());
-  InvokeTaskIfDone();
 }
 
 void ExternalProcessImporterHost::Cancel() {
@@ -312,6 +303,21 @@ void ExternalProcessImporterHost::InvokeTaskIfDone() {
   import_process_launched_ = true;
   client_->Start();
 }
+
+void ExternalProcessImporterHost::Loaded(BookmarkModel* model) {
+  DCHECK(model->IsLoaded());
+  model->RemoveObserver(this);
+  waiting_for_bookmarkbar_model_ = false;
+  installed_bookmark_observer_ = false;
+
+  // Because the import process is running externally, the decision whether
+  // to import to the bookmark bar must be stored here so that it can be
+  // passed to the importer when the import task is invoked.
+  import_to_bookmark_bar_ = (!model->HasBookmarks());
+  InvokeTaskIfDone();
+}
+
+// ExternalProcessImporterClient -----------------------------------------------
 
 ExternalProcessImporterClient::ExternalProcessImporterClient(
     ExternalProcessImporterHost* importer_host,
