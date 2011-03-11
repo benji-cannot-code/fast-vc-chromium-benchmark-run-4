e01e5fc8d39461992597d31d76f4625cdcf06900
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 09:30:43 +0000

    Revert 77714 - Make chrome://extension-icon resources available in incognito mode.
    BUG=74905
    TEST=ExtensionIconSourceTest
    
    Review URL: http://codereview.chromium.org/6623057
    
    TBR=jstritar@chromium.org
    Review URL: http://codereview.chromium.org/6672024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77799 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/browser_theme_provider.h"
 #include "chrome/browser/ui/find_bar/find_bar_state.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
-#include "chrome/browser/ui/webui/extension_icon_source.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -177,9 +176,6 @@ class OffTheRecordProfileImpl : public Profile,
 #if defined(OS_CHROMEOS)
     GetRequestContext();
 #endif  // defined(OS_CHROMEOS)
-
-    // Make the chrome//extension-icon/ resource available.
-    GetChromeURLDataManager()->AddDataSource(new ExtensionIconSource(this));
   }
 
   virtual ~OffTheRecordProfileImpl() {
