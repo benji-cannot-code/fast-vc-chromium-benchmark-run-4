8627886cd5c0e6374fa6066b7ee3ae4319c369fc
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-11 18:57:29 +0000

    Don't trigger a build when build.webkit.org's HTML files are modified
    Fixes <http://webkit.org/b/56190> Windows bots shouldn't have built r80848/r80851, but did
    
    Reviewed by Dan Bernstein.
    
    * Scripts/webkitpy/common/config/build.py:
    (_should_file_trigger_build): Added build.webkit.org's public_html directory to the list of
    directories that shouldn't trigger builds. Re-sorted the list.
    
    * Scripts/webkitpy/common/config/build_unittest.py:
    (ShouldBuildTest): Added a test to cover the above change.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/config/build.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/config/build.py
@@ -42,9 +42,10 @@ def _should_file_trigger_build(target_platform, file):
 
     directories = [
         # Directories that shouldn't trigger builds on any bots.
+        ("Examples", []),
         ("PerformanceTests", []),
         ("Source/WebCore/manual-tests", []),
-        ("Examples", []),
+        ("Tools/BuildSlaveSupport/build.webkit.org-config/public_html", []),
         ("Websites", []),
         ("android", []),
         ("brew", []),
@@ -54,8 +55,8 @@ def _should_file_trigger_build(target_platform, file):
         ("opengl", []),
         ("opentype", []),
         ("openvg", []),
-        ("wx", []),
         ("wince", []),
+        ("wx", []),
 
         # Directories that should trigger builds on only some bots.
         ("Source/JavaScriptGlue", ["mac"]),
