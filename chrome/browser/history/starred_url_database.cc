766c8a9759fdc51dcc36bd59b7cd3bfaa7223e7e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 02:00:41 +0000

    bookmarks: Rename entries with group_modified to folder_modified.
    This is the first part to get rid of references to group in the bookmarks API.
    All of them will be changed from group to folder to make it consistent, since
    right now we have a mix of them.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6667013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/starred_url_database.cc
+++ chrome/browser/history/starred_url_database.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   parent_id          Group ID of the parent this entry is contained in, if 0
 //                      entry is not in a group.
 //   date_modified      Time the group was last modified. See comments in
-//                      StarredEntry::date_group_modified
+//                      StarredEntry::date_folder_modified
 // NOTE: group_id and parent_id come from the UI, id is assigned by the
 // db.
 
@@ -78,7 +78,7 @@ void FillInStarredEntry(const sql::Statement& s, StarredEntry* entry) {
   entry->parent_group_id = s.ColumnInt64(5);
   entry->url_id = s.ColumnInt64(7);
   entry->group_id = s.ColumnInt64(8);
-  entry->date_group_modified = base::Time::FromInternalValue(s.ColumnInt64(9));
+  entry->date_folder_modified = base::Time::FromInternalValue(s.ColumnInt64(9));
 }
 
 }  // namespace
@@ -410,7 +410,7 @@ bool StarredURLDatabase::EnsureVisualOrder(
       entry.visual_order = i;
       LOG(WARNING) << "Bookmark visual order is wrong";
       if (!UpdateStarredEntryRow(entry.id, entry.title, entry.parent_group_id,
-                                 i, entry.date_group_modified)) {
+                                 i, entry.date_folder_modified)) {
         NOTREACHED() << "Unable to update visual order";
         return false;
       }
@@ -526,7 +526,7 @@ bool StarredURLDatabase::Move(StarredNode* source, StarredNode* new_parent) {
   entry.parent_group_id = new_parent->value.group_id;
   if (!UpdateStarredEntryRow(entry.id, entry.title,
                              entry.parent_group_id, entry.visual_order,
-                             entry.date_group_modified)) {
+                             entry.date_folder_modified)) {
     NOTREACHED() << "Unable to move folder";
     return false;
   }
