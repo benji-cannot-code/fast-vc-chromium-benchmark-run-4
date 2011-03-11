c0ff6143c189bf9fa6b3e79bfe011b74df589e2f
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 12:36:50 +0000

    Invalidate the entire screen after registering for refresh callbacks.
    BUG=
    TEST=Make a chromoting connection to a Mac host and verify that the full screen is initially visible. Connect sooner rather than later, otherwise it's likely that the full screen would have been visible even without this fix.
    
    Review URL: http://codereview.chromium.org/6651024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77808 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/capturer_mac.cc
+++ remoting/host/capturer_mac.cc
@@ -30,6 +30,7 @@ CapturerMac::CapturerMac(MessageLoop* message_loop)
       CapturerMac::DisplaysReconfiguredCallback, this);
   DCHECK_EQ(err, kCGErrorSuccess);
   ScreenConfigurationChanged();
+  InvalidateScreen(gfx::Size(width_, height_));
 }
 
 CapturerMac::~CapturerMac() {
