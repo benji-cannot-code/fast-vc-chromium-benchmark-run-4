e49207da9a7315f927fe7156d2f9039b7d1de20f
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 01:50:24 +0000

    Coverity Fixlets: CHECKED_RETURNs
    CID=15628,1968,3877
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6674006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_backend.cc
+++ chrome/browser/sessions/session_backend.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/scoped_vector.h"
 #include "net/base/file_stream.h"
+#include "net/base/net_errors.h"
 
 using base::TimeTicks;
 
@@ -366,10 +367,9 @@ void SessionBackend::ResetFile() {
 net::FileStream* SessionBackend::OpenAndWriteHeader(const FilePath& path) {
   DCHECK(!path.empty());
   scoped_ptr<net::FileStream> file(new net::FileStream());
-  file->Open(path, base::PLATFORM_FILE_CREATE_ALWAYS |
-             base::PLATFORM_FILE_WRITE | base::PLATFORM_FILE_EXCLUSIVE_WRITE |
-             base::PLATFORM_FILE_EXCLUSIVE_READ);
-  if (!file->IsOpen())
+  if (file->Open(path, base::PLATFORM_FILE_CREATE_ALWAYS |
+      base::PLATFORM_FILE_WRITE | base::PLATFORM_FILE_EXCLUSIVE_WRITE |
+      base::PLATFORM_FILE_EXCLUSIVE_READ) != net::OK)
     return NULL;
   FileHeader header;
   header.signature = kFileSignature;
--- chrome/renderer/pepper_platform_context_3d_impl.cc
+++ chrome/renderer/pepper_platform_context_3d_impl.cc
@@ -75,11 +75,10 @@ bool PlatformContext3DImpl::Init() {
       "*",
       attribs,
       parent_texture_id_);
-  command_buffer_->SetChannelErrorCallback(callback_factory_.NewCallback(
-      &PlatformContext3DImpl::OnContextLost));
-
   if (!command_buffer_)
     return false;
+  command_buffer_->SetChannelErrorCallback(callback_factory_.NewCallback(
+      &PlatformContext3DImpl::OnContextLost));
 
   return true;
 }
@@ -114,4 +113,3 @@ void PlatformContext3DImpl::OnContextLost() {
 }
 
 #endif  // ENABLE_GPU
-
