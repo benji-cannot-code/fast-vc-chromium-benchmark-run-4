bf3e61a9e477063c494d840ef389ad4c1797728a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 15:43:00 +0000

    importer: Eliminate the dependency on browser/ui/ from importer.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6672012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_dialogs.h
+++ chrome/browser/ui/browser_dialogs.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 class Extension;
-class ImporterHost;
 class HtmlDialogUIDelegate;
 class Profile;
 class SkBitmap;
@@ -37,13 +36,6 @@ void ShowAboutIPCDialog();
 // to do so, i.e. before OnDialogClosed() is called on the delegate.
 gfx::NativeWindow ShowHtmlDialog(gfx::NativeWindow parent, Profile* profile,
                                  HtmlDialogUIDelegate* delegate);
-
-// This function is called by an ImporterHost, and displays the Firefox profile
-// locked warning by creating a dialog.  On the closing of the dialog, the
-// ImportHost receives a callback with the message either to skip the import,
-// or to try again.
-void ShowImportLockDialog(gfx::NativeWindow parent,
-                          ImporterHost* importer_host);
 
 // Creates the ExtensionInstalledBubble and schedules it to be shown once
 // the extension has loaded. |extension| is the installed extension. |browser|
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1220,6 +1220,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/importer/importer_host.h',
         'browser/importer/importer_list.cc',
         'browser/importer/importer_list.h',
+        'browser/importer/importer_lock_dialog.h',
         'browser/importer/importer_messages.cc',
         'browser/importer/importer_messages.h',
         'browser/importer/importer_messages_internal.h',
