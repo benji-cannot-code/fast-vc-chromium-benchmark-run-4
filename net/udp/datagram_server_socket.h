79b8bd65ddc7adcb269959930a9de5108e4272dc
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 22:01:17 +0000

    UDP sockets implementation for windows.
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/6658027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77870 0039d316-1c4b-4281-b951-d872f2087c98

--- net/udp/datagram_server_socket.h
+++ net/udp/datagram_server_socket.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_UDP_DATAGRAM_SERVER_SOCKET_H_
 #pragma once
 
-#include <sys/socket.h>
-
 #include "net/base/completion_callback.h"
 #include "net/udp/datagram_socket.h"
 
