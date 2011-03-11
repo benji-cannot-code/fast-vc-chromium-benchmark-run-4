9129ca08f45b99b738a6348e938177c3d78a10c6
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-11 19:44:08 +0000

    Coverity: Fix various NULL_RETURNS defects.
    CID=8699, 13592, 14444, 14510, 14658, 14805, 14897
    BUG=none
    Review URL: http://codereview.chromium.org/6672016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77845 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/badge_util.cc
+++ chrome/common/badge_util.cc
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/common/badge_util.h"
 
+#include "base/logging.h"
 #include "base/utf_string_conversions.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkTypeface.h"
@@ -42,6 +43,7 @@ SkPaint* GetBadgeTextPaintSingleton() {
       const gfx::Font& base_font = rb.GetFont(ResourceBundle::BaseFont);
       typeface = SkTypeface::CreateFromName(
           UTF16ToUTF8(base_font.GetFontName()).c_str(), SkTypeface::kNormal);
+      DCHECK(typeface);
     }
 
     text_paint->setTypeface(typeface);
