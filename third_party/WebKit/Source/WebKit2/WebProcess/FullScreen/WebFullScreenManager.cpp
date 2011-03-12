4d9e819448197a6d24c8aac69c953a09f09d2a90
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-12 05:16:36 +0000

    2011-03-11  Jer Noble  <jer.noble@apple.com>
            Reviewed by Anders Carlsson.
    
            Add full screen animation code to WebFullScreenManager.
            https://bugs.webkit.org/show_bug.cgi?id=56220
    
            * WebProcess/FullScreen/WebFullScreenManager.cpp: .
            (WebKit::WebFullScreenManager::~WebFullScreenManager): Added.
            (WebKit::WebFullScreenManager::exitFullScreenForElement): Remove ASSERTs.
            (WebKit::WebFullScreenManager::willEnterFullScreen): Set the full screen renderer's background color.
            (WebKit::WebFullScreenManager::didEnterFullScreen): Ditto.
            (WebKit::WebFullScreenManager::willExitFullScreen): Ditto.
            (WebKit::WebFullScreenManager::didExitFullScreen): Ditto.
            * WebProcess/FullScreen/WebFullScreenManager.h: Make a few functions pure virtual to be implemented in a
                concrete subclass.
            * WebProcess/FullScreen/mac/WebFullScreenManagerMac.h: Added.
            * WebProcess/FullScreen/mac/WebFullScreenManagerMac.mm: Added.
            (-[WebFullScreenManagerAnimationListener initWithManager:WebKit::began:finished:]): Listener for CAAnimations.
            (-[WebFullScreenManagerAnimationListener animationDidStart:]): Added.
            (-[WebFullScreenManagerAnimationListener animationDidStop:finished:]): Added.
            (-[WebFullScreenManagerAnimationListener invalidate]): Added.
            (WebKit::WebFullScreenManager::create): Now creates a WebFullScreenManagerMac.
            (WebKit::WebFullScreenManagerMac::create): Added.
            (WebKit::WebFullScreenManagerMac::WebFullScreenManagerMac): Added.
            (WebKit::WebFullScreenManagerMac::~WebFullScreenManagerMac): Added.
            (WebKit::WebFullScreenManagerMac::setRootFullScreenLayer): Set up the remote layer host.
            (WebKit::WebFullScreenManagerMac::beginEnterFullScreenAnimation): Create a CATransform3D
                which will animate the full screen renderer from its initial position to its final one.
            (WebKit::WebFullScreenManagerMac::beginExitFullScreenAnimation): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/FullScreen/WebFullScreenManager.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/FullScreen/WebFullScreenManager.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFullScreenManagerProxyMessages.h"
 #include "WebPage.h"
 #include "WebProcess.h"
+#include <WebCore/Color.h>
 #include <WebCore/Page.h>
 #include <WebCore/Settings.h>
 
@@ -41,14 +42,14 @@ using namespace WebCore;
 
 namespace WebKit {
 
-PassRefPtr<WebFullScreenManager> WebFullScreenManager::create(WebPage* page)
-{
-    return adoptRef(new WebFullScreenManager(page));
-}
-
 WebFullScreenManager::WebFullScreenManager(WebPage* page)
     : m_page(page)
 {
+}
+    
+WebFullScreenManager::~WebFullScreenManager()
+{
+    
 }
 
 WebCore::Element* WebFullScreenManager::element() 
@@ -81,6 +82,8 @@ void WebFullScreenManager::enterFullScreenForElement(WebCore::Element* element)
 
 void WebFullScreenManager::exitFullScreenForElement(WebCore::Element* element)
 {
+    ASSERT(element);
+    ASSERT(m_element == element);
     m_page->send(Messages::WebFullScreenManagerProxy::ExitFullScreen());
 }
 
@@ -103,10 +106,6 @@ void WebFullScreenManager::finishedExitFullScreenAnimation(bool completed)
 {
     m_page->send(Messages::WebFullScreenManagerProxy::FinishedExitFullScreenAnimation(completed));
 }
-
-void WebFullScreenManager::setRootFullScreenLayer(WebCore::GraphicsLayer* layer)
-{
-}
     
 IntRect WebFullScreenManager::getFullScreenRect()
 {
@@ -119,33 +118,30 @@ void WebFullScreenManager::willEnterFullScreen()
 {
     ASSERT(m_element);
     m_element->document()->webkitWillEnterFullScreenForElement(m_element.get());
+    m_element->document()->setFullScreenRendererBackgroundColor(Color::transparent);
 }
 
 void WebFullScreenManager::didEnterFullScreen()
 {
     ASSERT(m_element);
     m_element->document()->webkitDidEnterFullScreenForElement(m_element.get());
+    m_element->document()->setFullScreenRendererBackgroundColor(Color::black);
 }
 
 void WebFullScreenManager::willExitFullScreen()
 {
     ASSERT(m_element);
     m_element->document()->webkitWillExitFullScreenForElement(m_element.get());
+    m_element->document()->setFullScreenRendererBackgroundColor(Color::transparent);
 }
 
 void WebFullScreenManager::didExitFullScreen()
 {
     ASSERT(m_element);
     m_element->document()->webkitDidExitFullScreenForElement(m_element.get());
+    m_element->document()->setFullScreenRendererBackgroundColor(Color::black);
 }
 
-void WebFullScreenManager::beginEnterFullScreenAnimation(float duration)
-{
-}
-
-void WebFullScreenManager::beginExitFullScreenAnimation(float duration)
-{
-}
 
 } // namespace WebKit
 
