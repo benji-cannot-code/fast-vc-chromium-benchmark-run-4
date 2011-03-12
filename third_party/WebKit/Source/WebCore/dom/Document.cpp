5ca45f2cee6087609468606b7bba32b7dc9a076e
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-12 08:24:53 +0000

    Unreviewed build fix.
    Fix GTK+ builds by wrapping sections of full screen code in USE(ACCELERATED_COMPOSITING)
    checks.
    
    * dom/Document.cpp:
    (WebCore::Document::webkitWillEnterFullScreenForElement):
    (WebCore::Document::webkitDidEnterFullScreenForElement):
    (WebCore::Document::webkitWillExitFullScreenForElement):
    (WebCore::Document::webkitDidExitFullScreenForElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -4888,9 +4888,11 @@ void Document::webkitWillEnterFullScreenForElement(Element* element)
     
     if (m_fullScreenRenderer) {
         m_fullScreenRenderer->setAnimating(true);
+#if USE(ACCELERATED_COMPOSITING)
         view()->updateCompositingLayers();
         ASSERT(m_fullScreenRenderer->layer()->backing());
         page()->chrome()->client()->setRootFullScreenLayer(m_fullScreenRenderer->layer()->backing()->graphicsLayer());
+#endif
     }
 }
     
@@ -4898,9 +4900,11 @@ void Document::webkitDidEnterFullScreenForElement(Element*)
 {
     if (m_fullScreenRenderer) {
         m_fullScreenRenderer->setAnimating(false);
+#if USE(ACCELERATED_COMPOSITING)
         view()->updateCompositingLayers();
         ASSERT(!m_fullScreenRenderer->layer()->backing());
         page()->chrome()->client()->setRootFullScreenLayer(0);
+#endif
     }
     m_fullScreenChangeDelayTimer.startOneShot(0);
 }
@@ -4910,9 +4914,11 @@ void Document::webkitWillExitFullScreenForElement(Element*)
     if (m_fullScreenRenderer) {
         m_fullScreenRenderer->setAnimating(true);
         m_fullScreenRenderer->setAnimating(true);
+#if USE(ACCELERATED_COMPOSITING)
         view()->updateCompositingLayers();
         ASSERT(m_fullScreenRenderer->layer()->backing());
         page()->chrome()->client()->setRootFullScreenLayer(m_fullScreenRenderer->layer()->backing()->graphicsLayer());
+#endif
     }
 }
 
@@ -4928,7 +4934,9 @@ void Document::webkitDidExitFullScreenForElement(Element*)
         m_fullScreenElement->detach();
     
     setFullScreenRenderer(0);
+#if USE(ACCELERATED_COMPOSITING)
     page()->chrome()->client()->setRootFullScreenLayer(0);
+#endif
     recalcStyle(Force);
     
     m_fullScreenChangeDelayTimer.startOneShot(0);
