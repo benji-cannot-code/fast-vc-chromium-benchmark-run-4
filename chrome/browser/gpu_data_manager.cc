3ff00acb8c38832fe3805b521908530d050730e5
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-12 01:00:41 +0000

    Move chrome\gpu to content\gpu.
    TBR=avi
    Review URL: http://codereview.chromium.org/6684015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_data_manager.cc
+++ chrome/browser/gpu_data_manager.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/child_process_logging.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/gpu/gpu_info_collector.h"
 #include "content/browser/gpu_blacklist.h"
+#include "content/gpu/gpu_info_collector.h"
 #include "grit/browser_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 
