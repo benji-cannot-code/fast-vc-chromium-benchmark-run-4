352c9a18ae31dd6ab1833a2d44edf5767ab1bcfc
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-13 23:50:32 +0000

    Add logs to understand failures that only happen on the waterfall. Will revert once browser tests start.
    TEST=ExtensionHistoryApiTest.*
    TBR=skerner
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_history_api.cc
+++ chrome/browser/extensions/extension_history_api.cc
@@ -121,6 +121,12 @@ void ExtensionHistoryEventRouter::HistoryUrlVisited(
 
   std::string json_args;
   base::JSONWriter::Write(&args, false, &json_args);
+
+  // Logging to discover if this happens after search on XP in
+  // ExtensionHistoryTest.*.
+  LOG(WARNING) << "ExtensionHistoryEventRouter::HistoryUrlVisited(): "
+               << json_args;
+
   DispatchEvent(profile, keys::kOnVisited, json_args);
 }
 
@@ -141,6 +147,12 @@ void ExtensionHistoryEventRouter::HistoryUrlsRemoved(
 
   std::string json_args;
   base::JSONWriter::Write(&args, false, &json_args);
+
+  // Logging to discover if this happens after search on XP in
+  // ExtensionHistoryTest.*.
+  LOG(WARNING) << "ExtensionHistoryEventRouter::HistoryUrlRemoved(): "
+               << json_args;
+
   DispatchEvent(profile, keys::kOnVisitRemoved, json_args);
 }
 
