07a31c382598727892a079c5c6d43e3e0576806c
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-14 17:20:24 +0000

    https://bugs.webkit.org/show_bug.cgi?id=56296 Clean up IconDatabaseBase header.
    Reviewed by Sam Weinig.
    
    -Get rid of the "PlatformString.h" include and replace it with a forward declaration.
    -Group methods by which are used in WebCore directly and which are used in WebKit ports.
    
    This'll make it easier to use in external frameworks (like WebKit2).
    
    * loader/icon/IconDatabase.h:
    * loader/icon/IconDatabaseBase.h:
    (WebCore::IconDatabaseBase::retainIconForPageURL):
    (WebCore::IconDatabaseBase::releaseIconForPageURL):
    (WebCore::IconDatabaseBase::iconForPageURL):
    (WebCore::IconDatabaseBase::setIconURLForPageURL):
    (WebCore::IconDatabaseBase::setIconDataForIconURL):
    (WebCore::IconDatabaseBase::iconDataKnownForIconURL):
    (WebCore::IconDatabaseBase::loadDecisionForIconURL):
    (WebCore::IconDatabaseBase::importIconURLForPageURL):
    (WebCore::IconDatabaseBase::importIconDataForIconURL):
    (WebCore::IconDatabaseBase::open):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/icon/IconDatabaseBase.h
+++ third_party/WebKit/Source/WebCore/loader/icon/IconDatabaseBase.h
@@ -27,9 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IconDatabaseBase_h
 #define IconDatabaseBase_h
 
-#include "PlatformString.h"
-
+#include <wtf/Forward.h>
 #include <wtf/Noncopyable.h>
+#include <wtf/PassRefPtr.h>
+
 
 namespace WebCore { 
 
@@ -37,6 +38,7 @@ class DocumentLoader;
 class IconDatabaseClient;
 class Image;
 class IntSize;
+class SharedBuffer;
 
 enum IconLoadDecision {
     IconLoadYes,
@@ -53,22 +55,27 @@ protected:
 public:
     virtual ~IconDatabaseBase() { }
 
-    virtual void setEnabled(bool) { }
+    // Used internally by WebCore
     virtual bool isEnabled() const { return false; }
+    
+    virtual Image* iconForPageURL(const String&, const IntSize&) { return 0; }
     virtual Image* defaultIcon(const IntSize&) { return 0; }
-
+    
     virtual void retainIconForPageURL(const String&) { }
     virtual void releaseIconForPageURL(const String&) { }
 
-    virtual Image* iconForPageURL(const String&, const IntSize&) { return 0; }
     virtual String iconURLForPageURL(const String&);
-    virtual void setIconURLForPageURL(const String&, const String&) { }
-
-    virtual void setIconDataForIconURL(PassRefPtr<SharedBuffer>, const String&) { }
     virtual bool iconDataKnownForIconURL(const String&) { return false; }
     virtual IconLoadDecision loadDecisionForIconURL(const String&, DocumentLoader*) { return IconLoadNo; }
+    
+    virtual void setIconURLForPageURL(const String&, const String&) { }
+    virtual void setIconDataForIconURL(PassRefPtr<SharedBuffer>, const String&) { }
 
-    // Support for WebCoreStatistics in WebKit
+    // Used within one or more WebKit ports.
+    // We should try to remove these dependencies from the IconDatabaseBase class.
+
+    virtual void setEnabled(bool) { }
+
     virtual size_t pageURLMappingCount() { return 0; }
     virtual size_t retainedPageURLCount() { return 0; }
     virtual size_t iconRecordCount() { return 0; }
