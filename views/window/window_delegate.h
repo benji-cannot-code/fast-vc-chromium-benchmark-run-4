6542c9d2aad3da3c68abede80f4dfa0041b0ab5b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 23:53:45 +0000

    Reland 78062 with some NULL checks.
    Revert 78062 - Migrate more of BrowserFrame's event handling down into WindowWin/WidgetWin and their delegate interfaces. BUG=72040 TEST=none Review URL: http://codereview.chromium.org/6683007 TBR=ben@chromium.org Review URL: http://codereview.chromium.org/6686059
    
    
    Review URL: http://codereview.chromium.org/6677029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78129 0039d316-1c4b-4281-b951-d872f2087c98

--- views/window/window_delegate.h
+++ views/window/window_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_ptr.h"
 #include "ui/base/accessibility/accessibility_types.h"
+#include "views/widget/widget_delegate.h"
 
 class SkBitmap;
 
@@ -34,7 +35,7 @@ class Window;
 //  it should be displayed and notify the delegate object of certain events.
 //
 ///////////////////////////////////////////////////////////////////////////////
-class WindowDelegate {
+class WindowDelegate : public WidgetDelegate {
  public:
   WindowDelegate();
   virtual ~WindowDelegate();
@@ -118,7 +119,11 @@ class WindowDelegate {
   virtual void DeleteDelegate() {}
 
   // Called when the window's activation state changes.
-  virtual void OnWindowActivate(bool active) {}
+  virtual void OnWindowActivationChanged(bool active) {}
+
+  // Called when the user begins/ends to change the bounds of the window.
+  virtual void OnWindowBeginUserBoundsChange() {}
+  virtual void OnWindowEndUserBoundsChange() {}
 
   // Returns the View that is contained within this Window.
   virtual View* GetContentsView();
