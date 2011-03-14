24d60afd07dbc548cfd8cc376dfafdfeada76a01
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-14 07:11:14 +0000

    2011-03-14  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Move cache_directory api
            https://bugs.webkit.org/show_bug.cgi?id=52139
    
            Move cache directory related apis from ewk_view to ewk_settings because
            those are not related to ewk_view.
    
            * ewk/ewk_main.cpp:
            (_ewk_init_body):
            * ewk/ewk_settings.cpp:
            (ewk_settings_cache_directory_path_set):
            (ewk_settings_cache_directory_path_get):
            * ewk/ewk_settings.h:
            * ewk/ewk_view.cpp:
            (_ewk_view_priv_new):
            (_ewk_view_priv_del):
            * ewk/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_main.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_main.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Logging.h"
 #include "PageCache.h"
 #include "PageGroup.h"
-#include "appcache/ApplicationCacheStorage.h"
 #include "ewk_private.h"
 #include "ewk_settings.h"
 #include "runtime/InitializeThreading.h"
@@ -176,7 +175,9 @@ Eina_Bool _ewk_init_body(void)
         ewk_settings_web_database_path_set(wkdir.utf8().data());
         ewk_settings_icon_database_path_set(wkdir.utf8().data());
 
-        WebCore::cacheStorage().setCacheDirectory(wkdir);
+#if ENABLE(OFFLINE_WEB_APPLICATIONS)
+        ewk_settings_cache_directory_path_set(wkdir.utf8().data());
+#endif
     }
 
     // TODO: this should move to WebCore, already reported to webkit-gtk folks:
