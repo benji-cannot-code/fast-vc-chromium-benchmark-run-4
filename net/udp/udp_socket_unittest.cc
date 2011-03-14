353303b008f0b4155e35c2676cda9a257f2d7484
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 18:33:31 +0000

    Don't fire callbacks on Close().  Caller should take care of this already, and this matches the way our tcp_client_socket works today.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6682023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78058 0039d316-1c4b-4281-b951-d872f2087c98

--- net/udp/udp_socket_unittest.cc
+++ net/udp/udp_socket_unittest.cc
@@ -312,8 +312,7 @@ TEST_F(UDPSocketTest, CloseWithPendingRead) {
 
   server.Close();
 
-  EXPECT_TRUE(callback.have_result());
-  EXPECT_EQ(callback.GetResult(rv), ERR_ABORTED);
+  EXPECT_FALSE(callback.have_result());
 }
 
 }  // namespace
