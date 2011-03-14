af8fb5b6095e235a8eb83c323342da395be14a35
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 15:45:42 +0000

    Fix clang compile error, invalid return
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6683043
    Patch from Dominic Battre <battre@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_proxy_api.cc
+++ chrome/browser/extensions/extension_proxy_api.cc
@@ -270,7 +270,7 @@ Value* ProxyPreferenceTransformer::BrowserToExtensionPref(
   ProxyPrefs::ProxyMode mode;
   if (!dict.GetMode(&mode)) {
     LOG(ERROR) << "Cannot determine proxy mode.";
-    return false;
+    return NULL;
   }
 
   scoped_ptr<DictionaryValue> extension_pref(new DictionaryValue);
