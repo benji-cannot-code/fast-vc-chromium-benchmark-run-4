cdaf8747bc2a575854863066ff069c11423a5397
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 16:07:27 +0000

    Fix Port and Event leaking.
    BUG=67344
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/6670001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78036 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/event_bindings.js
+++ chrome/renderer/resources/event_bindings.js
@@ -70,7 +70,7 @@ var chrome = chrome || {};
           return "Event validation error during " + opt_eventName + " -- " +
                  exception;
         }
-      }
+      };
     }
   };
 
@@ -107,7 +107,7 @@ var chrome = chrome || {};
   chromeHidden.Event.hasListener = function(name) {
     return (attachedNamedEvents[name] &&
             attachedNamedEvents[name].listeners_.length > 0);
-  }
+  };
 
   // Registers a callback to be called when this event is dispatched.
   chrome.Event.prototype.addListener = function(cb) {
@@ -205,6 +205,12 @@ var chrome = chrome || {};
     delete attachedNamedEvents[this.eventName_];
   };
 
+  chrome.Event.prototype.destroy_ = function() {
+    this.listeners_ = [];
+    this.validate_ = [];
+    this.detach_();
+  };
+
   // Special load events: we don't use the DOM unload because that slows
   // down tab shutdown.  On the other hand, onUnload might not always fire,
   // since Chrome will terminate renderers on shutdown (SuddenTermination).
@@ -213,7 +219,7 @@ var chrome = chrome || {};
 
   chromeHidden.dispatchOnLoad = function(extensionId) {
     chromeHidden.onLoad.dispatch(extensionId);
-  }
+  };
 
   chromeHidden.dispatchOnUnload = function() {
     chromeHidden.onUnload.dispatch();
@@ -222,9 +228,9 @@ var chrome = chrome || {};
       if (event)
         event.detach_();
     }
-  }
+  };
 
   chromeHidden.dispatchError = function(msg) {
     console.error(msg);
-  }
+  };
 })();
