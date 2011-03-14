03ad570d8755833a62351eca6668921c05c4ccc7
kristianm@google.com <kristianm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 16:02:33 +0000

    Removing a TODO and cleaning up code.
    This is done in preparation of a patch to
    let some request ignore the max sockets
    and max sockets to group limits.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6679001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78034 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -7076,7 +7076,7 @@ TEST_F(HttpNetworkTransactionTest,
   scoped_refptr<SpdySession> spdy_session =
       session->spdy_session_pool()->Get(pair, BoundNetLog());
   scoped_refptr<TCPSocketParams> tcp_params(
-      new TCPSocketParams("www.google.com", 443, MEDIUM, GURL(), false));
+      new TCPSocketParams(host_port_pair, MEDIUM, GURL(), false));
 
   scoped_ptr<ClientSocketHandle> connection(new ClientSocketHandle);
   EXPECT_EQ(ERR_IO_PENDING,
@@ -8325,7 +8325,7 @@ TEST_F(HttpNetworkTransactionTest, PreconnectWithExistingSpdySession) {
   scoped_refptr<SpdySession> spdy_session =
       session->spdy_session_pool()->Get(pair, BoundNetLog());
   scoped_refptr<TCPSocketParams> tcp_params(
-      new TCPSocketParams("www.google.com", 443, MEDIUM, GURL(), false));
+      new TCPSocketParams(host_port_pair, MEDIUM, GURL(), false));
   TestCompletionCallback callback;
 
   scoped_ptr<ClientSocketHandle> connection(new ClientSocketHandle);
