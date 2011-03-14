e6746c24aae373818c1498b8f4119d377afd10e1
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-14 01:27:37 +0000

    Fix windows build.
    * platform/network/cf/ResourceErrorCF.cpp:
    (WebCore::ResourceError::ResourceError):
    Add missing constructor.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@80986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/ResourceErrorCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/ResourceErrorCF.cpp
@@ -36,6 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+ResourceError::ResourceError(CFErrorRef cfError)
+    : m_dataIsUpToDate(false)
+    , m_platformError(cfError)
+{
+    m_isNull = !cfError;
+}
+
 const CFStringRef failingURLStringKey = CFSTR("NSErrorFailingURLStringKey");
 const CFStringRef failingURLKey = CFSTR("NSErrorFailingURLKey");
 
