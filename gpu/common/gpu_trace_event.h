7e1b65c8d6b9153ccccc58688d8ed60e17bf89e8
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 22:45:10 +0000

    Fix clang bots unhappiness withth gpu::TraceEvent.
    TBR=John Bates <jbates@chromium.org>
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78302 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/common/gpu_trace_event.h
+++ gpu/common/gpu_trace_event.h
@@ -188,6 +188,7 @@ enum TraceEventPhase {
 struct TraceEvent {
   static void AppendAsJSON(std::string* out,
       const std::vector<TraceEvent>& events);
+  TraceEvent() { }
   void AppendAsJSON(std::string* out) const;
 
 
