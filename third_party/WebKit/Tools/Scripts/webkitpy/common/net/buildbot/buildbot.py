131392227a888a9e2e70a915aa6f3a393d59fe0e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 07:28:19 +0000

    Make Windows 7 Release (Tests) a core builder
    We can probably make Windows XP Debug (Tests) a core builder, too, but we should probably
    give it a few more days of greenness first.
    
    Fixes <http://webkit.org/b/55665> Windows 7 Release (Tests) should be a core builder
    
    Reviewed by Sam Weinig.
    
    * Scripts/webkitpy/common/net/buildbot/buildbot.py:
    (BuildBot): Added a regexp to match Windows 7 Release (Tests).
    
    * Scripts/webkitpy/common/net/buildbot/buildbot_unittest.py:
    (BuildBotTest): Updated the list of builders to match the current list on build.webkit.org.
    Updated the core regexps to match the buildbot module. Updated the expected results to match
    the current set of core builders.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot.py
@@ -285,6 +285,7 @@ class BuildBot(object):
             "SnowLeopard.*\(WebKit2 Test",
             "Leopard.*Release",
             "Windows.*Build",
+            "Windows.*Release.*\(Test",
             "WinCE",
             "EFL",
             "GTK.*32",
