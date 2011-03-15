f27a54594360121803e7873dec3c8a07d2132d4b
tschmelcher@chromium.org <tschmelcher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 20:04:43 +0000

    Oops, fix Linux build failures in r78239 caused by a bad resolve with r77904 and by using an enum value not defined in the Linux cairo headers.
    TEST=built on Linux
    BUG=none
    
    Review URL: http://codereview.chromium.org/6673051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78269 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/core/cross/cairo/renderer_cairo.cc
+++ o3d/core/cross/cairo/renderer_cairo.cc
@@ -507,8 +507,8 @@ bool RendererCairo::GoFullscreen(const DisplayWindow& display,
     if (XGetWindowAttributes(display_, window_, &window_attributes)) {
       fullscreen_ = true;
       SetClientSize(window_attributes.width, window_attributes.height);
-      DestroyCairoSurface();
-      CreateCairoSurface();
+      DestroyDisplaySurface();
+      CreateDisplaySurface();
     }
 #elif defined(OS_WIN)
     DEVMODE dev_mode;
@@ -545,8 +545,8 @@ bool RendererCairo::CancelFullscreen(const DisplayWindow& display,
 
     fullscreen_ = false;
     SetClientSize(width, height);
-    DestroyCairoSurface();
-    CreateCairoSurface();
+    DestroyDisplaySurface();
+    CreateDisplaySurface();
 #elif defined(OS_WIN)
     if (hwnd_ == NULL) {
       // Not initialized.
