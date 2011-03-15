50941f2dc3048f1686aa62dbe892031afa78116e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 17:51:06 +0000

    2011-03-15  Adam Roben  <aroben@apple.com>
            Fix a leak in WebStorageTrackerClient::dispatchDidModifyOrigin
    
            Fixes <http://webkit.org/b/56385> SecurityOrigin leaks seen in
            WebStorageTrackerClient::dispatchDidModifyOrigin
    
            Reviewed by Antti Koivisto.
    
            * Storage/WebStorageTrackerClient.mm:
            (WebStorageTrackerClient::dispatchDidModifyOriginOnMainThread): Adopt the reference passed
            to us by dispatchDidModifyOrigin.
            (WebStorageTrackerClient::dispatchDidModifyOrigin): Added a comment and removed an
            unnecessary cast.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/Storage/WebStorageTrackerClient.mm
+++ third_party/WebKit/Source/WebKit/mac/Storage/WebStorageTrackerClient.mm
@@ -54,7 +54,9 @@ WebStorageTrackerClient::~WebStorageTrackerClient()
 void WebStorageTrackerClient::dispatchDidModifyOriginOnMainThread(void* context)
 {
     ASSERT(isMainThread());
-    WebStorageTrackerClient::sharedWebStorageTrackerClient()->dispatchDidModifyOrigin(static_cast<SecurityOrigin*>(context));
+    // adoptRef is balanced by leakRef in dispatchDidModifyOrigin.
+    RefPtr<SecurityOrigin> origin = adoptRef(static_cast<SecurityOrigin*>(context));
+    WebStorageTrackerClient::sharedWebStorageTrackerClient()->dispatchDidModifyOrigin(origin.get());
 }
 
 void WebStorageTrackerClient::dispatchDidModifyOrigin(PassRefPtr<SecurityOrigin> origin)
@@ -70,7 +72,8 @@ void WebStorageTrackerClient::dispatchDidModifyOrigin(const String& originIdenti
     PassRefPtr<SecurityOrigin> origin = SecurityOrigin::createFromDatabaseIdentifier(originIdentifier);
 
     if (!isMainThread()) {
-        callOnMainThread(dispatchDidModifyOriginOnMainThread, static_cast<void*>(origin.leakRef()));
+        // leakRef is balanced by adoptRef in dispatchDidModifyOriginOnMainThread.
+        callOnMainThread(dispatchDidModifyOriginOnMainThread, origin.leakRef());
         return;
     }
 
