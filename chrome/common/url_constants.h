33f5efb7d03ed5a3da2293b67fbd6c87eee0189a
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 19:40:05 +0000

    Move collected cookies url constants into ChromeOS specific part.
    Because they are only used in ChromeOS.
    
    BUG=none.
    TEST=Expect no change in build and behavior.
    
    Review URL: http://codereview.chromium.org/6673046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -72,7 +72,6 @@ extern const char kChromeUIAppLauncherURL[];
 extern const char kChromeUIBookmarksURL[];
 extern const char kChromeUIBugReportURL[];
 extern const char kChromeUIConflictsURL[];
-extern const char kChromeUICollectedCookiesURL[];
 extern const char kChromeUIConstrainedHTMLTestURL[];
 extern const char kChromeUICrashesURL[];
 extern const char kChromeUIDevToolsURL[];
@@ -94,6 +93,7 @@ extern const char kChromeUITextfieldsURL[];
 #if defined(OS_CHROMEOS)
 extern const char kChromeUIAboutOSCreditsURL[];
 extern const char kChromeUIActivationMessage[];
+extern const char kChromeUICollectedCookiesURL[];
 extern const char kChromeUIFileBrowseURL[];
 extern const char kChromeUIImageBurnerURL[];
 extern const char kChromeUIKeyboardOverlayURL[];
@@ -111,7 +111,6 @@ extern const char kChromeUIBookmarksHost[];
 extern const char kChromeUIBugReportHost[];
 extern const char kChromeUIConflictsHost[];
 extern const char kChromeUICrashesHost[];
-extern const char kChromeUICollectedCookiesHost[];
 extern const char kChromeUIDevToolsHost[];
 extern const char kChromeUIDialogHost[];
 extern const char kChromeUIDownloadsHost[];
@@ -140,6 +139,7 @@ extern const char kChromeUIThumbnailPath[];
 
 #if defined(OS_CHROMEOS)
 extern const char kChromeUIActivationMessageHost[];
+extern const char kChromeUICollectedCookiesHost[];
 extern const char kChromeUIFileBrowseHost[];
 extern const char kChromeUIImageBurnerHost[];
 extern const char kChromeUIKeyboardOverlayHost[];
