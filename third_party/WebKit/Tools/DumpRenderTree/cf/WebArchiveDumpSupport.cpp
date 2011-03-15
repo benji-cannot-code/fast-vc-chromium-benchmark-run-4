66212ebd6731cfe2f9f4c1c20110881da012a8dd
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 15:51:44 +0000

    Build fix after r81135.
    * DumpRenderTree/cf/WebArchiveDumpSupport.cpp:
    * DumpRenderTree/mac/WebArchiveDumpSupportMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/cf/WebArchiveDumpSupport.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/cf/WebArchiveDumpSupport.cpp
@@ -24,11 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "WebArchiveDumpSupport.h"
+#include "config.h"
+#include "WebArchiveDumpSupport.h"
 
-#import <CoreFoundation/CoreFoundation.h>
-#import <CFNetwork/CFNetwork.h>
-#import <wtf/RetainPtr.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <CFNetwork/CFNetwork.h>
+#include <wtf/RetainPtr.h>
 
 extern "C" {
 
--- third_party/WebKit/Tools/DumpRenderTree/mac/WebArchiveDumpSupportMac.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/WebArchiveDumpSupportMac.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import "config.h"
 #import "WebArchiveDumpSupport.h"
 
 #import <CFNetwork/CFHTTPMessage.h>
