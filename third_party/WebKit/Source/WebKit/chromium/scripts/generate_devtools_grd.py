4de527bc03c81faf674f60bbdef26326713ed8dd
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 17:49:50 +0000

    2011-03-15  Tony Chang  <tony@chromium.org>
            Fix chromium build using python 2.5 (with statement).
    
            * scripts/generate_devtools_grd.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81149 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/scripts/generate_devtools_grd.py
+++ third_party/WebKit/Source/WebKit/chromium/scripts/generate_devtools_grd.py
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Creates a grd file for packaging the inspector files."""
 
+from __future__ import with_statement
+
 import errno
 import os
 import shutil
