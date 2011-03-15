1819ccf21b0f76895c8df4c10da2d08d9e638f20
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 19:48:17 +0000

    Printing: MockPrinter should return valid default printer settings. Review URL: http://codereview.chromium.org/6673038
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78262 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/mock_printer.h
+++ chrome/renderer/mock_printer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ref_counted.h"
 #include "base/scoped_ptr.h"
 #include "printing/image.h"
+#include "ui/gfx/size.h"
 
 struct ViewMsg_Print_Params;
 struct ViewMsg_PrintPages_Params;
@@ -100,8 +101,10 @@ class MockPrinter {
 
  private:
   // In pixels according to dpi_x and dpi_y.
-  int printable_width_;
-  int printable_height_;
+  gfx::Size page_size_;
+  gfx::Size printable_size_;
+  int margin_left_;
+  int margin_top_;
 
   // Specifies dots per inch.
   double dpi_;
