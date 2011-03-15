17e00f5de59809647b8632e8c2d0f7f16d6eec7f
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 14:13:03 +0000

    2011-03-15  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: crash upon "//html//@id" search in elements panel.
            https://bugs.webkit.org/show_bug.cgi?id=56334
    
            * inspector/elements/elements-panel-search-expected.txt:
            * inspector/elements/elements-panel-search.html:
    2011-03-15  Pavel Feldman  <pfeldman@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: crash upon "//html//@id" search in elements panel.
            https://bugs.webkit.org/show_bug.cgi?id=56334
    
            * inspector/InspectorDOMAgent.cpp:
            * inspector/front-end/ElementsTreeOutline.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
@@ -1493,6 +1493,11 @@ WebInspector.ElementsTreeElement.prototype = {
             matchRanges.push({ offset: match.index, length: match[0].length });
             match = regexObject.exec(text);
         }
+
+        // Fall back for XPath, etc. matches.
+        if (!matchRanges.length)
+            matchRanges.push({ offset: 0, length: text.length });
+
         highlightSearchResults(this.listItemElement, matchRanges);
         this._searchHighlightedHTML = this.listItemElement.innerHTML;
     }
