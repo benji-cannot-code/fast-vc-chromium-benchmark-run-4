89d3196e710db8e71e96c9044428157a5f40c882
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 15:12:14 +0000

    2011-03-12  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: re-implement xhr breakpoints.
            https://bugs.webkit.org/show_bug.cgi?id=56252
    
            * inspector/debugger/xhr-breakpoints-expected.txt: Added.
            * inspector/debugger/xhr-breakpoints.html: Added.
            * platform/gtk/Skipped:
            * platform/mac/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
    2011-03-12  Pavel Podivilov  <podivilov@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: re-implement xhr breakpoints.
            https://bugs.webkit.org/show_bug.cgi?id=56252
    
            - restore xhr breakpoints one by one instead of using setAllBrowserBreakpoints
            - store xhr breakpoints in a separate setting
            - move presentation-related code from BreakpointManager to XHRBreakpointsSidebarPane
    
            Test: inspector/debugger/xhr-breakpoints.html
    
            * inspector/InspectorBrowserDebuggerAgent.cpp:
            (WebCore::InspectorBrowserDebuggerAgent::InspectorBrowserDebuggerAgent):
            (WebCore::InspectorBrowserDebuggerAgent::inspectedURLChanged):
            (WebCore::InspectorBrowserDebuggerAgent::restoreStickyBreakpoint):
            (WebCore::InspectorBrowserDebuggerAgent::setXHRBreakpoint):
            (WebCore::InspectorBrowserDebuggerAgent::removeXHRBreakpoint):
            (WebCore::InspectorBrowserDebuggerAgent::willSendXMLHttpRequest):
            (WebCore::InspectorBrowserDebuggerAgent::clear):
            * inspector/InspectorBrowserDebuggerAgent.h:
            * inspector/front-end/BreakpointManager.js:
            (WebInspector.BreakpointManager.prototype.setXHRBreakpoint):
            (WebInspector.BreakpointManager.prototype.removeXHRBreakpoint):
            (WebInspector.BreakpointManager.prototype.breakpointViewForEventData):
            (WebInspector.BreakpointManager.prototype._projectChanged):
            (WebInspector.BreakpointManager.prototype._validateBreakpoints):
            (WebInspector.BreakpointManager.prototype._createEventListenerBreakpointId):
            * inspector/front-end/BreakpointsSidebarPane.js:
            (WebInspector.XHRBreakpointsSidebarPane):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._addButtonClicked.finishEditing):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._addButtonClicked):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._setBreakpoint):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._removeBreakpoint):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._contextMenu.removeBreakpoint):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._contextMenu):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._checkboxClicked):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._labelClicked.finishEditing):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._labelClicked):
            (WebInspector.XHRBreakpointsSidebarPane.prototype.highlightBreakpoint):
            (WebInspector.XHRBreakpointsSidebarPane.prototype.clearBreakpointHighlight):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._saveBreakpoints):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._restoreBreakpoints):
            (WebInspector.XHRBreakpointsSidebarPane.prototype._projectChanged):
            * inspector/front-end/CallStackSidebarPane.js:
            (WebInspector.CallStackSidebarPane.prototype.update):
            (WebInspector.CallStackSidebarPane.prototype._xhrBreakpointHit):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel):
            (WebInspector.ScriptsPanel.prototype._debuggerPaused):
            (WebInspector.ScriptsPanel.prototype._clearInterface):
            * inspector/front-end/Settings.js:
            (WebInspector.Settings):
            * inspector/front-end/inspector.js:
            (WebInspector.resetFocusElement):
            (WebInspector.set attached):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
@@ -142,7 +142,7 @@ var WebInspector = {
     resetFocusElement: function()
     {
         this.currentFocusElement = null;
-        this._previousFocusElement = null;        
+        this._previousFocusElement = null;
     },
 
     get currentPanel()
@@ -180,17 +180,6 @@ var WebInspector = {
             pane.addBreakpointItem(new WebInspector.BreakpointItem(event.data));
         }
         WebInspector.breakpointManager.addEventListener(WebInspector.BreakpointManager.Events.DOMBreakpointAdded, breakpointAdded);
-        return pane;
-    },
-
-    createXHRBreakpointsSidebarPane: function()
-    {
-        var pane = new WebInspector.XHRBreakpointsSidebarPane();
-        function breakpointAdded(event)
-        {
-            pane.addBreakpointItem(new WebInspector.BreakpointItem(event.data));
-        }
-        WebInspector.breakpointManager.addEventListener(WebInspector.BreakpointManager.Events.XHRBreakpointAdded, breakpointAdded);
         return pane;
     },
 
@@ -240,7 +229,7 @@ var WebInspector = {
             dockToggleButton.title = WebInspector.UIString("Dock to main window.");
         }
 
-        // This may be called before onLoadedDone, hence the bulk of inspector objects may 
+        // This may be called before onLoadedDone, hence the bulk of inspector objects may
         // not be created yet.
         if (WebInspector.searchController)
             WebInspector.searchController.updateSearchLabel();
