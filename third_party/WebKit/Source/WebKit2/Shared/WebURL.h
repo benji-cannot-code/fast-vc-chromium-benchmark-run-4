763b435274d3486b1865072050bb542c6bd213ef
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 00:32:53 +0000

    2011-03-14  Balazs Kelemen  <kbalazs@webkit.org>
            Reviewed by Adam Roben.
    
            [Qt][WK2]Unbreak InjectedBundle on Qt
            https://bugs.webkit.org/show_bug.cgi?id=54109
    
            No code changes so no new tests.
    
            Revert the changes that were needed to use KURL
            in WebKitTestRunner.
    
            * Configurations/WebCore.xcconfig:
            * WebCore.exp.in:
    2011-03-14  Balazs Kelemen  <kbalazs@webkit.org>
    
            Reviewed by Adam Roben.
    
            [Qt][WK2]Unbreak InjectedBundle on Qt
            https://bugs.webkit.org/show_bug.cgi?id=54109
    
            Add API's to WKURL and WKString to satisfy
            WebKitTestRunner's needs.
    
            * Shared/API/c/WKString.cpp:
            (WKStringIsEqualToUTF8CStringIgnoringCase):
            * Shared/API/c/WKString.h:
            * Shared/API/c/WKURL.cpp:
            (WKURLCopyHostName):
            (WKURLCopyScheme):
            * Shared/API/c/WKURL.h:
            * Shared/WebString.h:
            (WebKit::WebString::equalToUTF8StringIgnoringCase):
            * Shared/WebURL.h: Added OwnPtr<KURL> member to be able
            to lazily parse the URL and store the result.
            (WebKit::WebURL::host):
            (WebKit::WebURL::protocol):
            (WebKit::WebURL::parseURLIfNecessary):
            * win/WebKit2.def: Revert the symbol exports that were
            needed to use KURL in WebKitTestRunner.
    2011-03-14  Balazs Kelemen  <kbalazs@webkit.org>
    
            Reviewed by Adam Roben.
    
            [Qt][WK2]Unbreak InjectedBundle on Qt
            https://bugs.webkit.org/show_bug.cgi?id=54109
    
            * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
            (WTR::InjectedBundlePage::willSendRequestForFrame): Use new WebKit2
            API's instead of KURL to analyze the URL.
            * WebKitTestRunner/StringFunctions.h:
            (WTR::adoptWK): Moved from InjectedBundlePage.cpp to make it available globally
            in the WebKitTestRunner project.
            (WTR::toWK): Use adoptWK.
            (WTR::operator<<): Added helper for printing a WKURL.
            * WebKitTestRunner/WebKitTestRunner.xcodeproj/project.pbxproj: Do not link
            against WebCore.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebURL.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebURL.h
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebURL_h
 
 #include "APIObject.h"
+#include <WebCore/KURL.h>
+#include <wtf/OwnPtr.h>
+#include <wtf/PassOwnPtr.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/text/WTFString.h>
 
@@ -49,15 +52,35 @@ public:
 
     const String& string() const { return m_string; }
 
+    String host() const
+    {
+        parseURLIfNecessary();
+        return m_parsedURL->isValid() ? m_parsedURL->host() : String();
+    }
+
+    String protocol() const
+    {
+        parseURLIfNecessary();
+        return m_parsedURL->isValid() ? m_parsedURL->protocol() : String();
+    }
+
 private:
     WebURL(const String& string)
         : m_string(string)
     {
     }
 
+    void parseURLIfNecessary() const
+    {
+        if (m_parsedURL)
+            return;
+        m_parsedURL = WTF::adoptPtr(new WebCore::KURL(WebCore::KURL(), m_string));
+    }
+
     virtual Type type() const { return APIType; }
 
     String m_string;
+    mutable OwnPtr<WebCore::KURL> m_parsedURL;
 };
 
 } // namespace WebKit
