3af3d6b49a8b3aa6c46b20f0371da2748e5bb6a9
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-15 19:49:43 +0000

    Adds some histograms from various omnibox queries. I'm doing this as with the quick provider enabled the omnibox feels a bit sluggish and I would like to quantify that. I'll update the xml file separately.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6679049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78263 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_quick_provider.cc
+++ chrome/browser/autocomplete/history_quick_provider.cc
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/i18n/break_iterator.h"
 #include "base/logging.h"
+#include "base/metrics/histogram.h"
+#include "base/string_number_conversions.h"
 #include "base/string_util.h"
+#include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/history/in_memory_url_index.h"
@@ -63,7 +66,16 @@ void HistoryQuickProvider::Start(const AutocompleteInput& input,
   // someone unloads the history backend, we'll get inconsistent inline
   // autocomplete behavior here.
   if (GetIndex()) {
+    base::TimeTicks start_time = base::TimeTicks::Now();
     DoAutocomplete();
+    if (input.text().size() < 6) {
+      base::TimeTicks end_time = base::TimeTicks::Now();
+      std::string name = "HistoryQuickProvider.QueryIndexTime." +
+          base::IntToString(input.text().size());
+      scoped_refptr<base::Histogram> counter = base::Histogram::FactoryGet(
+          name, 1, 1000, 50, base::Histogram::kUmaTargetedHistogramFlag);
+      counter->Add(static_cast<int>((end_time - start_time).InMilliseconds()));
+    }
     UpdateStarredStateOfMatches();
   }
 }
