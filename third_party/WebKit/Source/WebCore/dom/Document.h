bc0ba2c76a1837cadfc2f48e6228e0ca6cc938bc
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-15 22:37:47 +0000

    2011-03-15  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Tony Chang.
    
            Crash in ReplaceSelectionCommand::doApply when inserting a node under a document node
            https://bugs.webkit.org/show_bug.cgi?id=56372
    
            The bug was caused by insertNodeAfter's calling parentElement on document's child.
            Fixed this by changing the node that AppendNodeCommand takes.
    
            There was also a bug that document node always returned false for isContentEditable
            and isContentRichlyEditable because they never overrode Node's default implementation.
            Fixed this by overriding them in Document.
    
            Test: editing/execCommand/append-node-under-document.html
    
            * dom/Document.cpp:
            (WebCore::Document::isContentEditable): Added.
            (WebCore::Document::isContentRichlyEditable): Added.
            * dom/Document.h:
            * editing/AppendNodeCommand.cpp:
            (WebCore::AppendNodeCommand::AppendNodeCommand): Takes ContainerNode instead of Element.
            * editing/AppendNodeCommand.h:
            (WebCore::AppendNodeCommand::create): Ditto.
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::appendNode): Ditto.
            (WebCore::CompositeEditCommand::insertNodeAfter): Calls parentNode instead of parentElement.
            * editing/CompositeEditCommand.h:
    2011-03-15  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Tony Chang.
    
            Crash in ReplaceSelectionCommand::doApply when inserting a node under a document node
            https://bugs.webkit.org/show_bug.cgi?id=56372
    
            Added a test to ensure WebKit does not crash when appending a node to a document
            that has only two nested iframes.
    
            * editing/execCommand/append-node-under-document-expected.txt: Added.
            * editing/execCommand/append-node-under-document.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -895,6 +895,8 @@ public:
     void setDesignMode(InheritedBool value);
     InheritedBool getDesignMode() const;
     bool inDesignMode() const;
+    virtual bool isContentEditable() const;
+    virtual bool isContentRichlyEditable() const;
 
     Document* parentDocument() const;
     Document* topDocument() const;
--- third_party/WebKit/Source/WebCore/editing/AppendNodeCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/AppendNodeCommand.cpp
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-AppendNodeCommand::AppendNodeCommand(PassRefPtr<Element> parent, PassRefPtr<Node> node)
+AppendNodeCommand::AppendNodeCommand(PassRefPtr<ContainerNode> parent, PassRefPtr<Node> node)
     : SimpleEditCommand(parent->document())
     , m_parent(parent)
     , m_node(node)
