84eba58b5aa5e2fb042fbf6bf57ae86cfac118a0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 19:27:16 +0000

    Fix Mac build break
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78410 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_carbon_interpose_mac.cc
+++ chrome/browser/plugin_carbon_interpose_mac.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Carbon/Carbon.h>
 
-#include "chrome/plugin/plugin_interpose_util_mac.h"
+#include "content/plugin/plugin_interpose_util_mac.h"
 #include "ui/gfx/rect.h"
 #include "webkit/plugins/npapi/carbon_plugin_window_tracker_mac.h"
 
