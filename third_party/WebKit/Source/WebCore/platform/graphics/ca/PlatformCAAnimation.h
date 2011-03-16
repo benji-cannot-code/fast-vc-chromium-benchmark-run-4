af08a52e623273b87587993e26ed5d0b45effa72
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-16 15:00:33 +0000

    Minor clean-up after r81156, r81172
    Follow-up for:
    <http://webkit.org/b/56381> Objective-C classes should be typedef-ed as structs (not void*) in C++
    
    * platform/graphics/GraphicsLayer.h: Removed unused @class
    WebLayer declaration.
    * platform/graphics/ca/PlatformCAAnimation.h: Changed typedef
    struct CAPropertyAnimation to class declaration.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/PlatformCAAnimation.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/PlatformCAAnimation.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef __OBJC__
 @class CAPropertyAnimation;
 #else
-typedef struct CAPropertyAnimation CAPropertyAnimation;
+class CAPropertyAnimation;
 #endif
 typedef CAPropertyAnimation* PlatformAnimationRef;
 #elif PLATFORM(WIN)
