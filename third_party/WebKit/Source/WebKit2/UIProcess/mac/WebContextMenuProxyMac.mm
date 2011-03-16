2e8145571c9c0629a8b5017ab9d67be5e8733782
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-16 00:50:36 +0000

    2011-03-15  Sam Weinig  <sam@webkit.org>
            Reviewed by Anders Carlsson.
    
            WebKit2: False SPOD cursor when context menu is open
            <rdar://problem/9029154>
            https://bugs.webkit.org/show_bug.cgi?id=56433
    
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::showContextMenu):
            Update to match showPopupMenu idiomatically, and stop the responsivenessTimer
            since the act of showing the context menu could spin a nested runloop.
    
            * UIProcess/mac/WebContextMenuProxyMac.mm:
            (WebKit::WebContextMenuProxyMac::showContextMenu):
            * UIProcess/qt/WebContextMenuProxyQt.cpp:
            (WebKit::WebContextMenuProxyQt::showContextMenu):
            * UIProcess/win/WebContextMenuProxyWin.cpp:
            (WebKit::WebContextMenuProxyWin::showContextMenu):
            Move isEmpty() check to implementations, since we don't want to show
            this in any case, not just the one where we check it.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81205 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/mac/WebContextMenuProxyMac.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/mac/WebContextMenuProxyMac.mm
@@ -200,6 +200,9 @@ void WebContextMenuProxyMac::populate(const Vector<WebContextMenuItemData>& item
 
 void WebContextMenuProxyMac::showContextMenu(const IntPoint& menuLocation, const Vector<WebContextMenuItemData>& items)
 {
+    if (items.isEmpty())
+        return;
+    
     populate(items);
     [[WKMenuTarget sharedMenuTarget] setMenuProxy:this];
     
