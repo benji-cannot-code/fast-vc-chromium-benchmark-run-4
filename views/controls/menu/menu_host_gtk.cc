37dfbac014583d00ebdf636ab893367ba75f8c68
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 18:12:09 +0000

    Re-lands: Add native capture API to Widget, and fix a bug in Window where we wouldn't let the window be closed from the X.
    BUG=72040
    TEST=none
    R=sky
    
    Review URL: http://codereview.chromium.org/6670049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78394 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/menu_host_gtk.cc
+++ views/controls/menu/menu_host_gtk.cc
@@ -93,7 +93,7 @@ void MenuHostGtk::SetMenuHostBounds(const gfx::Rect& bounds) {
 }
 
 void MenuHostGtk::ReleaseMenuHostCapture() {
-  ReleaseGrab();
+  ReleaseNativeCapture();
 }
 
 gfx::NativeWindow MenuHostGtk::GetMenuHostWindow() {
@@ -108,8 +108,8 @@ bool MenuHostGtk::ReleaseCaptureOnMouseReleased() {
   return false;
 }
 
-void MenuHostGtk::ReleaseGrab() {
-  WidgetGtk::ReleaseGrab();
+void MenuHostGtk::ReleaseNativeCapture() {
+  WidgetGtk::ReleaseNativeCapture();
   if (did_input_grab_) {
     did_input_grab_ = false;
     gdk_pointer_ungrab(GDK_CURRENT_TIME);
@@ -151,7 +151,7 @@ void MenuHostGtk::DoCapture() {
     gtk_grab_remove(current_grab_window);
 
   // Make sure all app mouse/keyboard events are targetted at us only.
-  DoGrab();
+  SetNativeCapture();
 
   // And do a grab.  NOTE: we do this to ensure we get mouse/keyboard
   // events from other apps, a grab done with gtk_grab_add doesn't get
