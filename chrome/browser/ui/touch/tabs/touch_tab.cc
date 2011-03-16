315574a0050eca998be6ccba8239556a91522155
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 21:45:28 +0000

    Real touch build fix.
    BUG=none
    TEST=GYP_DEFINES="toolkit_views=1 touchui=1" gclient runhooks && make chrome -j 8
    TBR=sadrul
    
    Review URL: http://codereview.chromium.org/6676050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78439 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/touch/tabs/touch_tab.cc
+++ chrome/browser/ui/touch/tabs/touch_tab.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/touch/tabs/touch_tab.h"
 
+#include "base/logging.h"
 #include "chrome/browser/themes/browser_theme_provider.h"
 #include "grit/app_resources.h"
 #include "grit/theme_resources.h"
