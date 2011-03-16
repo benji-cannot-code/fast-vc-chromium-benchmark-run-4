10500053f6e73818a3547db7ba01570f55e7c048
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 19:48:02 +0000

    Mark WorkerTest.WorkerNavigator as flaky.
    TEST=none
    BUG=76426
    
    Review URL: http://codereview.chromium.org/6677084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78413 0039d316-1c4b-4281-b951-d872f2087c98

--- content/worker/worker_uitest.cc
+++ content/worker/worker_uitest.cc
@@ -312,7 +312,8 @@ TEST_F(WorkerTest, WorkerMultiPort) {
   RunWorkerFastLayoutTest("worker-multi-port.html");
 }
 
-TEST_F(WorkerTest, WorkerNavigator) {
+// Flaky, http://crbug.com/76426.
+TEST_F(WorkerTest, FLAKY_WorkerNavigator) {
   RunWorkerFastLayoutTest("worker-navigator.html");
 }
 
