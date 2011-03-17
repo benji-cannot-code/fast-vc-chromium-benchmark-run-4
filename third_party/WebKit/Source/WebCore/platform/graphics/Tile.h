2b2768cc89470899dbf3025fb0254caed979baf3
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-17 12:00:34 +0000

    2011-03-17  Gustavo Noronha Silva  <gustavo.noronha@collabora.co.uk>
            Reviewed by Andreas Kling.
    
            Tiled backing store should only request repaint for updated areas
            https://bugs.webkit.org/show_bug.cgi?id=56464
    
            Reuse updateBackBuffer's dirty rectangle calculations to only
            invalidate the necessary parts of the window.
    
            * platform/graphics/Tile.h:
            * platform/graphics/TiledBackingStore.cpp:
            (WebCore::TiledBackingStore::updateTileBuffers):
            * platform/graphics/qt/TileQt.cpp:
            (WebCore::Tile::updateBackBuffer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/Tile.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/Tile.h
@@ -50,7 +50,7 @@ public:
     
     bool isDirty() const;
     void invalidate(const IntRect&);
-    void updateBackBuffer();
+    Vector<IntRect> updateBackBuffer();
     void swapBackBufferToFront();
     bool isReadyToPaint() const;
     void paint(GraphicsContext*, const IntRect&);
