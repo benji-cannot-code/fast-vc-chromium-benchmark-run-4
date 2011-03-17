940844e48d4d301b4b9b19fac1da23743bb64bf6
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 02:05:26 +0000

    Fix build break on Windows
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78501 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/win7_location_api_unittest_win.cc
+++ content/browser/geolocation/win7_location_api_unittest_win.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/scoped_ptr.h"
 #include "base/time.h"
-#include "chrome/common/geoposition.h"
 #include "content/browser/geolocation/win7_location_api_win.h"
+#include "content/common/geoposition.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
