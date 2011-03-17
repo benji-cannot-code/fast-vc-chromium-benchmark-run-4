d141b627c3b1f0e0c6e6e48b736f4b9686216d0c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-17 06:39:19 +0000

    2011-03-16  Jeff Johnson  <github@lapcatsoftware.com>
            Reviewed by Alexey Proskuryakov.
    
            Logic error in -[WebHTMLView close]
            https://bugs.webkit.org/show_bug.cgi?id=56445
    
            The function setDraggingImageURL() is no longer called and can be deleted.
    
            No new tests. Deleting dead code.
    
            * page/DragController.h:
    2011-03-16  Jeff Johnson  <github@lapcatsoftware.com>
    
            Reviewed by Alexey Proskuryakov.
    
            Logic error in -[WebHTMLView close]
            https://bugs.webkit.org/show_bug.cgi?id=56445
    
            Page is always nil at this point, because [self _webView] is nil,
            so the conditional code is never run.
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView close]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
@@ -1986,10 +1986,6 @@ static void _updateMouseoverTimerCallback(CFRunLoopTimerRef timer, void *info)
     [self removeAllToolTips];
 
     [_private clear];
-
-    Page* page = core([self _webView]);
-    if (page)
-        page->dragController()->setDraggingImageURL(KURL());
 }
 
 - (BOOL)_hasHTMLDocument
