b12da39f8154a2befafaf6e8925773ae01111211
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 19:06:01 +0000

    Stop subclassing FilterContext in URLRequestJob.
    Create a wrapper for URLRequestHttpJob that subclasses FilterContext,
    and pass an instance of this facade to Filter::Factory().
    
    Reduce the scope of URLRequestJob's interface as much as possible, moving
    methods into URLRequestHttpJob or its facade as appropriate, and making
    methods that remain in URLRequestJob non-virtual where possible.
    
    BUG=none
    TEST=net_unittests,try bots
    
    Review URL: http://codereview.chromium.org/6677104
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78576 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_job.cc
+++ net/url_request/url_request_job.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/auth.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/io_buffer.h"
-#include "net/base/load_flags.h"
+#include "net/base/load_states.h"
 #include "net/base/mime_util.h"
 #include "net/base/net_errors.h"
 #include "net/base/network_delegate.h"
@@ -29,7 +29,6 @@ namespace net {
 URLRequestJob::URLRequestJob(URLRequest* request)
     : request_(request),
       done_(false),
-      load_flags_(request_->load_flags()),
       prefilter_bytes_read_(0),
       postfilter_bytes_read_(0),
       is_compressible_content_(false),
@@ -206,40 +205,16 @@ bool URLRequestJob::GetMimeType(std::string* mime_type) const {
   return false;
 }
 
-bool URLRequestJob::GetURL(GURL* gurl) const {
-  if (!request_)
-    return false;
-  *gurl = request_->url();
-  return true;
-}
-
-base::Time URLRequestJob::GetRequestTime() const {
-  if (!request_)
-    return base::Time();
-  return request_->request_time();
-}
-
-bool URLRequestJob::IsDownload() const {
-  return (load_flags_ & net::LOAD_IS_DOWNLOAD) != 0;
-}
-
-bool URLRequestJob::IsSdchResponse() const {
-  return false;
-}
-
 bool URLRequestJob::IsCachedContent() const {
   return false;
-}
-
-int64 URLRequestJob::GetByteReadCount() const {
-  return filter_input_byte_count_;
 }
 
 int URLRequestJob::GetResponseCode() const {
   return -1;
 }
 
-void URLRequestJob::RecordPacketStats(StatisticSelector statistic) const {
+void URLRequestJob::RecordPacketStats(
+    FilterContext::StatisticSelector statistic) const {
   if (!packet_timing_enabled_ || (final_packet_time_ == base::Time()))
     return;
 
@@ -251,7 +226,7 @@ void URLRequestJob::RecordPacketStats(StatisticSelector statistic) const {
 
   base::TimeDelta duration = final_packet_time_ - request_time_snapshot_;
   switch (statistic) {
-    case SDCH_DECODE: {
+    case FilterContext::SDCH_DECODE: {
       UMA_HISTOGRAM_CLIPPED_TIMES("Sdch3.Network_Decode_Latency_F_a", duration,
                                   base::TimeDelta::FromMilliseconds(20),
                                   base::TimeDelta::FromMinutes(10), 100);
@@ -288,7 +263,7 @@ void URLRequestJob::RecordPacketStats(StatisticSelector statistic) const {
                                   base::TimeDelta::FromSeconds(10), 100);
       return;
     }
-    case SDCH_PASSTHROUGH: {
+    case FilterContext::SDCH_PASSTHROUGH: {
       // Despite advertising a dictionary, we handled non-sdch compressed
       // content.
       UMA_HISTOGRAM_CLIPPED_TIMES("Sdch3.Network_Pass-through_Latency_F_a",
@@ -326,7 +301,7 @@ void URLRequestJob::RecordPacketStats(StatisticSelector statistic) const {
       return;
     }
 
-    case SDCH_EXPERIMENT_DECODE: {
+    case FilterContext::SDCH_EXPERIMENT_DECODE: {
       UMA_HISTOGRAM_CLIPPED_TIMES("Sdch3.Experiment_Decode",
                                   duration,
                                   base::TimeDelta::FromMilliseconds(20),
@@ -335,7 +310,7 @@ void URLRequestJob::RecordPacketStats(StatisticSelector statistic) const {
       // case, so we don't need them here.
       return;
     }
-    case SDCH_EXPERIMENT_HOLDBACK: {
+    case FilterContext::SDCH_EXPERIMENT_HOLDBACK: {
       UMA_HISTOGRAM_CLIPPED_TIMES("Sdch3.Experiment_Holdback",
                                   duration,
                                   base::TimeDelta::FromMilliseconds(20),
@@ -815,7 +790,7 @@ void URLRequestJob::UpdatePacketReadTimes() {
   }
 
   if (!bytes_observed_in_packets_)
-    request_time_snapshot_ = GetRequestTime();
+    request_time_snapshot_ = request_ ? request_->request_time() : base::Time();
 
   final_packet_time_ = base::Time::Now();
   const size_t kTypicalPacketSize = 1430;
