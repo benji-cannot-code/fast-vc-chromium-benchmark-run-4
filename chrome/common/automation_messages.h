a37c294ac4aab6677ebf67fb3b349dd2b63298b7
vipul.bhasin@gmail.com <vipul.bhasin@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 01:18:32 +0000

    Removed 'off the record' references from ExternalTabSettings and ProfileIOData::ProfileParams.
    BUG=3333
    
    Review URL: http://codereview.chromium.org/6700001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78485 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/automation_messages.h
+++ chrome/common/automation_messages.h
@@ -64,7 +64,7 @@ struct ExternalTabSettings {
   ExternalTabSettings(gfx::NativeWindow parent,
                       const gfx::Rect& dimensions,
                       unsigned int style,
-                      bool is_off_the_record,
+                      bool is_incognito,
                       bool load_requests_via_automation,
                       bool handle_top_level_requests,
                       const GURL& initial_url,
@@ -76,7 +76,7 @@ struct ExternalTabSettings {
   gfx::NativeWindow parent;
   gfx::Rect dimensions;
   unsigned int style;
-  bool is_off_the_record;
+  bool is_incognito;
   bool load_requests_via_automation;
   bool handle_top_level_requests;
   GURL initial_url;
