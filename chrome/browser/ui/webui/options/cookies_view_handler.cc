2067947617755a003cdcf65badb82f4fb12173d3
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 16:27:14 +0000

    Create the CookiesTreeModel on demand in the cookies view handler.
    BUG=76292
    TEST=none
    
    Review URL: http://codereview.chromium.org/6676034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78556 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/cookies_view_handler.cc
+++ chrome/browser/ui/webui/options/cookies_view_handler.cc
@@ -131,12 +131,7 @@ void CookiesViewHandler::TreeModelEndBatch(CookiesTreeModel* model) {
   SendChildren(cookies_tree_model_->GetRoot());
 }
 
-void CookiesViewHandler::UpdateSearchResults(const ListValue* args) {
-  std::string query;
-  if (!args->GetString(0, &query)){
-    return;
-  }
-
+void CookiesViewHandler::EnsureCookiesTreeModelCreated() {
   if (!cookies_tree_model_.get()) {
     Profile* profile = web_ui_->GetProfile();
     cookies_tree_model_.reset(new CookiesTreeModel(
@@ -149,11 +144,21 @@ void CookiesViewHandler::UpdateSearchResults(const ListValue* args) {
         false));
     cookies_tree_model_->AddCookiesTreeObserver(this);
   }
+}
+
+void CookiesViewHandler::UpdateSearchResults(const ListValue* args) {
+  std::string query;
+  if (!args->GetString(0, &query)){
+    return;
+  }
+
+  EnsureCookiesTreeModelCreated();
 
   cookies_tree_model_->UpdateSearchResults(UTF8ToWide(query));
 }
 
 void CookiesViewHandler::RemoveAll(const ListValue* args) {
+  EnsureCookiesTreeModelCreated();
   cookies_tree_model_->DeleteAllStoredObjects();
 }
 
@@ -162,6 +167,8 @@ void CookiesViewHandler::Remove(const ListValue* args) {
   if (!args->GetString(0, &node_path)){
     return;
   }
+
+  EnsureCookiesTreeModelCreated();
 
   CookieTreeNode* node = cookies_tree_model_util::GetTreeNodeFromPath(
       cookies_tree_model_->GetRoot(), node_path);
@@ -174,6 +181,8 @@ void CookiesViewHandler::LoadChildren(const ListValue* args) {
   if (!args->GetString(0, &node_path)){
     return;
   }
+
+  EnsureCookiesTreeModelCreated();
 
   CookieTreeNode* node = cookies_tree_model_util::GetTreeNodeFromPath(
       cookies_tree_model_->GetRoot(), node_path);
--- chrome/browser/ui/webui/options/cookies_view_handler.h
+++ chrome/browser/ui/webui/options/cookies_view_handler.h
@@ -35,6 +35,9 @@ class CookiesViewHandler : public OptionsPageUIHandler,
   virtual void TreeModelEndBatch(CookiesTreeModel* model);
 
  private:
+  // Creates the CookiesTreeModel if neccessary.
+  void EnsureCookiesTreeModelCreated();
+
   // Updates search filter for cookies tree model.
   void UpdateSearchResults(const ListValue* args);
 
