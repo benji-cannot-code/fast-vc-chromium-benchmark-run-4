cca1f2d11c6b66095c26839991e9ee4e735bfb8c
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-17 20:49:11 +0000

    2011-03-17  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Adele Peterson and Enrica Casucci.
    
            Assert that editing does not ignore position's anchorNode if position is an offset in anchor
            https://bugs.webkit.org/show_bug.cgi?id=56027
    
            Debug build fix.
    
            * dom/Element.cpp:
            (WebCore::Element::updateFocusAppearance): "this" can be an input element so can't always instantiate
            a position inside the node. Call firstPositionInOrBeforeNode instead.
            * editing/ReplaceSelectionCommand.cpp:
            (WebCore::positionAvoidingPrecedingNodes): Exit early when a node's content is ignored by editing instead
            of just when the node is br.
            * editing/htmlediting.cpp:
            (WebCore::lastEditablePositionBeforePositionInRoot): The shadow ancestor node is usually an input element
            so don't instantiate a position inside it. Call firstPositionInOrBeforeNode instead.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextIterator.h"
 #include "WebKitAnimationList.h"
 #include "XMLNames.h"
+#include "htmlediting.h"
 #include <wtf/text/CString.h>
 
 #if ENABLE(SVG)
@@ -1555,7 +1556,7 @@ void Element::updateFocusAppearance(bool /*restorePreviousSelection*/)
             return;
 
         // FIXME: We should restore the previous selection if there is one.
-        VisibleSelection newSelection = VisibleSelection(firstPositionInNode(this), DOWNSTREAM);
+        VisibleSelection newSelection = VisibleSelection(firstPositionInOrBeforeNode(this), DOWNSTREAM);
         
         if (frame->selection()->shouldChangeSelection(newSelection)) {
             frame->selection()->setSelection(newSelection);
