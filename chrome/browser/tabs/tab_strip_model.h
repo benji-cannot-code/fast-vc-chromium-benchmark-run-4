a3f07e8e66fe1120fa388b3cd570d8fcc8b28cc7
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 21:28:21 +0000

    Updates TabStripModel context menu commands to deal with multiple selected tabs. A right click on an selected tab makes the action apply to all tabs. A right click on an unselected tab only applies to that tab.
    BUG=30572
    TEST=covered by unit tests.
    
    Review URL: http://codereview.chromium.org/6673095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78604 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model.h
+++ chrome/browser/tabs/tab_strip_model.h
@@ -246,6 +246,7 @@ class TabStripModel : public NotificationObserver {
   void MoveSelectedTabsTo(int index);
 
   // Returns the currently selected TabContents, or NULL if there is none.
+  // TODO(sky): rename to GetActiveTabContents.
   TabContentsWrapper* GetSelectedTabContents() const;
 
   // Returns the TabContentsWrapper at the specified index, or NULL if there is
@@ -376,10 +377,14 @@ class TabStripModel : public NotificationObserver {
   void ToggleSelectionAt(int index);
 
   // Returns true if the tab at |index| is selected.
-  bool IsTabSelected(int index);
+  bool IsTabSelected(int index) const;
 
   // Sets the selection to match that of |source|.
   void SetSelectionFromModel(const TabStripSelectionModel& source);
+
+  const TabStripSelectionModel& selection_model() const {
+    return selection_model_;
+  }
 
   // Command level API /////////////////////////////////////////////////////////
 
@@ -392,8 +397,8 @@ class TabStripModel : public NotificationObserver {
                       PageTransition::Type transition,
                       int add_types);
 
-  // Closes the selected TabContents.
-  void CloseSelectedTab();
+  // Closes the selected tabs.
+  void CloseSelectedTabs();
 
   // Select adjacent tabs
   void SelectNextTab();
@@ -424,7 +429,8 @@ class TabStripModel : public NotificationObserver {
     CommandLast
   };
 
-  // Returns true if the specified command is enabled.
+  // Returns true if the specified command is enabled. If |context_index| is
+  // selected the response applies to all selected tabs.
   bool IsContextMenuCommandEnabled(int context_index,
                                    ContextMenuCommand command_id) const;
 
@@ -433,7 +439,8 @@ class TabStripModel : public NotificationObserver {
                                    ContextMenuCommand command_id) const;
 
   // Performs the action associated with the specified command for the given
-  // TabStripModel index |context_index|.
+  // TabStripModel index |context_index|.  If |context_index| is selected the
+  // command applies to all selected tabs.
   void ExecuteContextMenuCommand(int context_index,
                                  ContextMenuCommand command_id);
 
@@ -442,6 +449,10 @@ class TabStripModel : public NotificationObserver {
   // descending order.
   std::vector<int> GetIndicesClosedByCommand(int index,
                                              ContextMenuCommand id) const;
+
+  // Returns true if 'CommandTogglePinned' will pin. |index| is the index
+  // supplied to |ExecuteContextMenuCommand|.
+  bool WillContextMenuPin(int index);
 
   // Overridden from notificationObserver:
   virtual void Observe(NotificationType type,
@@ -455,6 +466,11 @@ class TabStripModel : public NotificationObserver {
  private:
   // We cannot be constructed without a delegate.
   TabStripModel();
+
+  // If |index| is selected all the selected indices are returned, otherwise a
+  // vector with |index| is returned. This is used when executing commands to
+  // determine which indices the command applies to.
+  std::vector<int> GetIndicesForCommand(int index) const;
 
   // Returns true if the specified TabContents is a New Tab at the end of the
   // TabStrip. We check for this because opener relationships are _not_
--- chrome/browser/tabs/tab_strip_selection_model.cc
+++ chrome/browser/tabs/tab_strip_selection_model.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -63,7 +63,7 @@ void TabStripSelectionModel::SetSelectedIndex(int index) {
   SetSelectionFromAnchorTo(index);
 }
 
-bool TabStripSelectionModel::IsSelected(int index) {
+bool TabStripSelectionModel::IsSelected(int index) const {
   return std::find(selected_indices_.begin(), selected_indices_.end(), index) !=
       selected_indices_.end();
 }
