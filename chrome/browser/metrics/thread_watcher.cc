e08fb26c1c449f26cc969a02ba7ce3225254c27e
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-17 22:17:52 +0000

    Remove a confusing obsolete comment from MetricsService's header
    This comment stopped being true almost two and a half years ago (the function doesn't even *have* a return value any more), but was never removed.
    
    Also fixes incorrect use of "setup" as a verb, to make the interface more readable.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6712011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78616 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/thread_watcher.cc
+++ chrome/browser/metrics/thread_watcher.cc
@@ -198,7 +198,7 @@ ThreadWatcherList::ThreadWatcherList()
   CHECK(!global_);
   global_ = this;
   // Register Notifications observer.
-  MetricsService::SetupNotifications(&registrar_, this);
+  MetricsService::SetUpNotifications(&registrar_, this);
 }
 
 ThreadWatcherList::~ThreadWatcherList() {
