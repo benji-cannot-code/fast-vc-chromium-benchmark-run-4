598853c8a832a054f56b7af5e009b1ddbffcde6c
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 10:48:42 +0000

    Add data for showing power button functions on Keyboard Overlay.
    BUG=chromium-os:9957
    TEST=manually on the netbook
    
    Review URL: http://codereview.chromium.org/6649017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard_overlay_data.js
+++ chrome/browser/resources/keyboard_overlay_data.js
@@ -23675,6 +23675,7 @@ var keyboardOverlayData = {
     "n CTRL SHIFT": "keyboardOverlayNewIncognitoWindow",
     "o CTRL": "keyboardOverlayContentBrowser",
     "p CTRL": "keyboardOverlayPrint",
+    "power": "keyboardOverlayLockScreenOrPowerOff",
     "q CTRL SHIFT": "keyboardOverlaySignOut",
     "r CTRL": "keyboardOverlayReloadCurrentPage",
     "r CTRL SHIFT": "keyboardOverlayReloadIgnoringCache",
