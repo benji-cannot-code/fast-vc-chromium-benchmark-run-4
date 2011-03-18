8e51b79e7ca0b09c28887360065637593034e3a8
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 01:41:05 +0000

    Fix build break
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78645 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main_posix.cc
+++ chrome/app/chrome_main_posix.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/global_descriptors_posix.h"
 #include "base/logging.h"
 #include "base/process_util.h"
-#include "chrome/common/chrome_descriptors.h"
+#include "content/common/chrome_descriptors.h"
 
 #if defined(OS_MACOSX)
 #include "chrome/app/breakpad_mac.h"
