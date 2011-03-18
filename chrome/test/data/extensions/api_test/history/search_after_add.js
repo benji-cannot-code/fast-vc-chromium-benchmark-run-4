f9eb0360007271905eab675b253c47976e6c1858
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 14:13:30 +0000

    History tests de-flaked.
    The history tests used to be flaky on all platforms.
    Several fixes were made, and now the flakiness dashboard shows that the only source of failures is due to adding a URL and searching right away on windows XP.  The test that fails has been isolated in a single subtest (ExtensionHistoryApiTest.SearchAfterAdd), and all other tests are no longer marked as flaky.
    
    BUG=26296
    TEST=ExtensionHistoryApiTest.*
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=78542
    
    Review URL: http://codereview.chromium.org/6698028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78687 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/history/search_after_add.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// History api test for Chrome.
+// browser_tests.exe --gtest_filter=ExtensionHistoryApiTest.SearchAfterAdd
+
+// runHistoryTestFns is defined in ./common.js .
+runHistoryTestFns([
+  function searchAfterAdd() {
+    chrome.history.deleteAll(function() {
+      var VALID_URL = 'http://www.google.com/';
+      chrome.history.addUrl({url: VALID_URL});
+      chrome.history.search({text: ''}, function(historyItems) {
+        assertEq(1, historyItems.length);
+        assertEq(VALID_URL, historyItems[0].url);
+        chrome.test.succeed();
+      });
+    });
+  }
+]);
