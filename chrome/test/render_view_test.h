5af58cdcf88f4bf486fbb183040aadae6502c496
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 21:09:41 +0000

    Fix browser test failures.
    TBR=bradnelson
    Review URL: http://codereview.chromium.org/6688044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78756 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/render_view_test.h
+++ chrome/test/render_view_test.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/main_function_params.h"
 #include "content/common/native_web_keyboard_event.h"
 #include "content/common/sandbox_init_wrapper.h"
+#include "content/renderer/content_renderer_client.h"
 #include "content/renderer/renderer_webkitclient_impl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
@@ -93,6 +94,7 @@ class RenderViewTest : public testing::Test {
   virtual void TearDown();
 
   MessageLoop msg_loop_;
+  content::ContentRendererClient content_renderer_client_;
   MockRenderThread render_thread_;
   scoped_ptr<MockRenderProcess> mock_process_;
   scoped_refptr<RenderView> view_;
