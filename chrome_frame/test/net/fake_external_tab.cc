47161aad63ab836967fc3207837dbdceacfff187
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 23:40:09 +0000

    Fix for the chrome frame net tests which crashes on startup due to a missing ContentRendererClient interface which is part of the ContentClient object.
    While fixing this I also fixed a memory leak occurring due to a heap allocated ChromeContentBrowserClient
    object. This object does not need to live on the heap.
    
    BUG=none
    TEST=chrome frame net tests should run.
    TBR=amit
    Review URL: http://codereview.chromium.org/6709044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #include "chrome/browser/automation/automation_provider_list.h"
 #include "chrome/browser/chrome_content_browser_client.h"
+#include "chrome/renderer/chrome_content_renderer_client.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/prefs/proxy_config_dictionary.h"
@@ -494,10 +495,14 @@ int main(int argc, char** argv) {
   // Initialize the content client which that code uses to talk to Chrome.
   chrome::ChromeContentClient chrome_content_client;
   content::SetContentClient(&chrome_content_client);
+
   // Override the default ContentBrowserClient to let Chrome participate in
   // content logic.  Must be done before any tabs are created.
-  content::GetContentClient()->set_browser(
-      new chrome::ChromeContentBrowserClient);
+  chrome::ChromeContentBrowserClient browser_client;
+  content::GetContentClient()->set_browser(&browser_client);
+
+  chrome::ChromeContentRendererClient renderer_client;
+  content::GetContentClient()->set_renderer(&renderer_client);
 
   // TODO(tommi): Stuff be broke. Needs a fixin'.
   // This is awkward: the TestSuite derived CFUrlRequestUnittestRunner contains
