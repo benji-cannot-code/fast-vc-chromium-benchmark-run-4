cdc04e4b3a681916424a4d91dd9d50a92df39d15
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 20:45:40 +0000

    Print Preview: Fix typo from r78639.
    BUG=76707
    TEST=page count initialized correctly in print preview.
    R=arv@chromium.org
    Review URL: http://codereview.chromium.org/6688041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview_ui.cc
@@ -32,5 +32,5 @@ PrintPreviewUIHTMLSource* PrintPreviewUI::html_source() {
 
 void PrintPreviewUI::PreviewDataIsAvailable(int expected_pages_count) {
   FundamentalValue pages_count(expected_pages_count);
-  CallJavascriptFunction("createPDFPlugin", pages_count);
+  CallJavascriptFunction("updatePrintPreview", pages_count);
 }
