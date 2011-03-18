57ec2aac53f8c06f3dbefab813e593facb5b966b
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-18 10:26:56 +0000

    2011-03-18  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: console doesn&apos;t scroll when multiline expression is being evaluated
            https://bugs.webkit.org/show_bug.cgi?id=56639
    
            Always scoll in the console when command result is received.
    
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleView.prototype.addMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
@@ -299,7 +299,8 @@ WebInspector.ConsoleView.prototype = {
             this.currentGroup.addMessage(msg);
         }
 
-        if (shouldScrollToLastMessage)
+        // Always scroll when command result arrives.
+        if (shouldScrollToLastMessage || (msg instanceof WebInspector.ConsoleCommandResult))
             this._scheduleScrollIntoView();
     },
 
