e013b95bec6e7e00cc96ded66c64e06eff6a40f9
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-19 10:26:24 +0000

    FTP: use ICU to parse month abbreviations.
    In theory we should be able to parse localized listings
    in more languages now.
    
    This also allows us to get rid of hardcoded special cases
    for German.
    
    BUG=65917
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/6697021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78826 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_vms.cc
+++ net/ftp/ftp_directory_listing_parser_vms.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -131,7 +131,7 @@ bool VmsDateListingToTime(const std::vector<string16>& columns,
     return false;
   if (!base::StringToInt(date_parts[0], &time_exploded.day_of_month))
     return false;
-  if (!net::FtpUtil::ThreeLetterMonthToNumber(date_parts[1],
+  if (!net::FtpUtil::AbbreviatedMonthToNumber(date_parts[1],
                                               &time_exploded.month))
     return false;
   if (!base::StringToInt(date_parts[2], &time_exploded.year))
--- net/ftp/ftp_util.h
+++ net/ftp/ftp_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,22 +19,23 @@ namespace net {
 
 class FtpUtil {
  public:
-  // Convert Unix file path to VMS path (must be a file, and not a directory).
+  // Converts Unix file path to VMS path (must be a file, and not a directory).
   static std::string UnixFilePathToVMS(const std::string& unix_path);
 
-  // Convert Unix directory path to VMS path (must be a directory).
+  // Converts Unix directory path to VMS path (must be a directory).
   static std::string UnixDirectoryPathToVMS(const std::string& unix_path);
 
-  // Convert VMS path to Unix-style path.
+  // Converts VMS path to Unix-style path.
   static std::string VMSPathToUnix(const std::string& vms_path);
 
-  // Convert three-letter month abbreviation (like Nov) to its number (in range
-  // 1-12).
-  static bool ThreeLetterMonthToNumber(const string16& text, int* number);
+  // Converts abbreviated month (like Nov) to its number (in range 1-12).
+  // Note: in some locales abbreviations are more than three letters long,
+  // and this function also handles them correctly.
+  static bool AbbreviatedMonthToNumber(const string16& text, int* number);
 
-  // Convert a "ls -l" date listing to time. The listing comes in three columns.
-  // The first one contains month, the second one contains day of month.
-  // The first one is either a time (and then we guess the year based
+  // Converts a "ls -l" date listing to time. The listing comes in three
+  // columns. The first one contains month, the second one contains day
+  // of month. The third one is either a time (and then we guess the year based
   // on |current_time|), or is a year (and then we don't know the time).
   static bool LsDateListingToTime(const string16& month,
                                   const string16& day,
@@ -42,7 +43,7 @@ class FtpUtil {
                                   const base::Time& current_time,
                                   base::Time* result);
 
-  // Skip |columns| columns from |text| (whitespace-delimited), and return the
+  // Skips |columns| columns from |text| (whitespace-delimited), and returns the
   // remaining part, without leading/trailing whitespace.
   static string16 GetStringPartAfterColumns(const string16& text, int columns);
 };
