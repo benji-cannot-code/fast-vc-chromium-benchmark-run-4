26cdc0e6a7bbd7248e0caf5cc1df8b4be3ce55ac
amarinichev@chromium.org <amarinichev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-19 00:06:53 +0000

    Do not put off destroying of the IOSurface in the GPU process.
    Evicting old graphics contexts will be done in the GPU process by removing
    routes from GpuChannel::router_.  Doing so on a mac results in an assert when
    handling GpuCommandBufferMsg_SetWindowSize.  The assert was a consequence of how
    we deal with closed windows.  When a window is closed, the browser process sends
    a message to the GPU process to request that the surface is removed.  Rather
    than doing that immediately, GpuThread object stores the view id of the renderer
    in destroyed_renderer_routes_.  GpuCommandBufferStub::OnFlush always checks if
    its view id is in that set, and if is, it asks the GPUProcessor to destroy its
    surface.
    
    This change gets rid of the destroyed_renderer_routes_.  Rather than deferring
    the request to destroy the surface, we ask the GPU process to remove the route
    which, in turn, will remove the GpuCommandBufferStub, GPUProcessor and its
    surface.
    
    See also bug 67170.
    
    BUG=none
    TEST=set the breakpoint in GpuChannel::OnMessageReceived, manually remove the route from router_ and stubs_.  No assert should be triggered and context should be silently recovered.
    
    Review URL: http://codereview.chromium.org/6688026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_process_host_ui_shim.h
+++ chrome/browser/gpu_process_host_ui_shim.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -110,7 +110,7 @@ class GpuProcessHostUIShim
 
 #if defined(OS_MACOSX)
   // Notify the GPU process that an accelerated surface was destroyed.
-  void DidDestroyAcceleratedSurface(int renderer_id, int32 renderer_route_id);
+  void DidDestroyAcceleratedSurface(int renderer_id, int32 render_view_id);
 #endif
 
   // Sends a message to the browser process to collect the information from the
