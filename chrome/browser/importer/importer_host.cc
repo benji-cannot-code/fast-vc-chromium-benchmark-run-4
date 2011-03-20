70472c1b2a0332ad3fad34fdfc4e1ef7a185c127
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-20 15:08:07 +0000

    importer: Move InProcessImporterBridge to its own header file.
    BUG=None
    TEST=None
    
    R=avi@chromium.org
    
    Review URL: http://codereview.chromium.org/6685109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78851 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_host.cc
+++ chrome/browser/importer/importer_host.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer_list.h"
 #include "chrome/browser/importer/importer_lock_dialog.h"
 #include "chrome/browser/importer/importer_progress_observer.h"
+#include "chrome/browser/importer/in_process_importer_bridge.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_model.h"
