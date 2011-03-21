73ea68eac6c26bc947b19d7a5de01a4603d737ed
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 21:48:59 +0000

    Change GL_EXT_texture_compression_s3tc to GL_CHROMIUM_texture_compression_dxt3 and GL_CHROMIUM_texture_compression_dxt5 because we don't support all the features of GL_EXT_texture_compression_s3tc.
    TEST=unit tests
    BUG=none
    
    Review URL: http://codereview.chromium.org/6715005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78926 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/feature_info_unittest.cc
+++ gpu/command_buffer/service/feature_info_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -78,6 +78,10 @@ TEST_F(FeatureInfoTest, InitializeNoExtensions) {
   EXPECT_THAT(info_.extensions(), Not(HasSubstr("GL_OES_texture_npot")));
   EXPECT_THAT(info_.extensions(),
               Not(HasSubstr("GL_EXT_texture_compression_dxt1")));
+  EXPECT_THAT(info_.extensions(),
+              Not(HasSubstr("GL_CHROMIUM_texture_compression_dxt3")));
+  EXPECT_THAT(info_.extensions(),
+              Not(HasSubstr("GL_CHROMIUM_texture_compression_dxt5")));
   EXPECT_FALSE(info_.feature_flags().npot_ok);
   EXPECT_FALSE(info_.feature_flags().chromium_webglsl);
   EXPECT_FALSE(info_.validators()->compressed_texture_format.IsValid(
@@ -154,7 +158,9 @@ TEST_F(FeatureInfoTest, InitializeDXTExtensionGL) {
   EXPECT_THAT(info_.extensions(),
               HasSubstr("GL_EXT_texture_compression_dxt1"));
   EXPECT_THAT(info_.extensions(),
-              HasSubstr("GL_EXT_texture_compression_s3tc"));
+              HasSubstr("GL_CHROMIUM_texture_compression_dxt3"));
+  EXPECT_THAT(info_.extensions(),
+              HasSubstr("GL_CHROMIUM_texture_compression_dxt5"));
   EXPECT_TRUE(info_.validators()->compressed_texture_format.IsValid(
       GL_COMPRESSED_RGB_S3TC_DXT1_EXT));
   EXPECT_TRUE(info_.validators()->compressed_texture_format.IsValid(
