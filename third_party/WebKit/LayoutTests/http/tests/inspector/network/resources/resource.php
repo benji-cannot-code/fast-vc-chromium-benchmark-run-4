94b240818c5e01d1cc4701cd4db0ca07b43bb5b5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-21 14:40:33 +0000

    2011-03-21  Vsevolod Vlasov  <vsevik@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Inspector does not always show correct transfer size (for compressed/chunked data)
            https://bugs.webkit.org/show_bug.cgi?id=56691
    
            Added a test for transfer size for chunked encoding type.
    
            * http/tests/inspector/network/network-size-chunked.html: Added.
            * http/tests/inspector/network/network-size-expected-chunked.txt: Added.
            * http/tests/inspector/network/resources/resource.php:
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/mac/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
    2011-03-21  Vsevolod Vlasov  <vsevik@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: Inspector does not always show correct transfer size (for compressed/chunked data)
            https://bugs.webkit.org/show_bug.cgi?id=56691
    
            Added transfer size support in inspector for compressed data.
    
            Test: http/tests/inspector/network/network-size-chunked.html
    
            * inspector/Inspector.idl:
            * inspector/InspectorInstrumentation.cpp:
            (WebCore::InspectorInstrumentation::didReceiveContentLengthImpl):
            * inspector/InspectorInstrumentation.h:
            (WebCore::InspectorInstrumentation::didReceiveContentLength):
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::didReceiveResponse):
            (WebCore::InspectorResourceAgent::didReceiveContentLength):
            * inspector/InspectorResourceAgent.h:
            * inspector/front-end/NetworkManager.js:
            (WebInspector.NetworkDispatcher.prototype.didReceiveContentLength):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.prototype.get transferSize):
            (WebInspector.Resource.prototype.increaseTransferSize):
            * loader/ResourceLoadNotifier.cpp:
            (WebCore::ResourceLoadNotifier::didReceiveData):
            (WebCore::ResourceLoadNotifier::dispatchDidReceiveContentLength):
            (WebCore::ResourceLoadNotifier::sendRemainingDelegateMessages):
            * loader/ResourceLoadNotifier.h:
            * loader/appcache/ApplicationCacheGroup.cpp:
            (WebCore::ApplicationCacheGroup::didReceiveData):
            * platform/network/mac/ResourceHandleMac.mm:
            (-[WebCoreResourceHandleAsDelegate connection:didReceiveData:lengthReceived:]):
            * platform/network/qt/QNetworkReplyHandler.cpp:
            (WebCore::QNetworkReplyHandler::forwardData):
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::gotChunkCallback):
            * platform/network/win/ResourceHandleWin.cpp:
            (WebCore::ResourceHandle::onRequestComplete):
            (WebCore::ResourceHandle::fileLoadTimer):
    2011-03-21  Vsevolod Vlasov  <vsevik@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: Inspector does not always show correct transfer size (for compressed/chunked data)
            https://bugs.webkit.org/show_bug.cgi?id=56691
    
            Added transfer size support in inspector for compressed data.
    
            * src/ResourceHandle.cpp:
            (WebCore::ResourceHandleInternal::didReceiveData):
            * src/WebDevToolsAgentImpl.cpp:
            (WebKit::WebDevToolsAgentImpl::didReceiveData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/network/resources/resource.php
+++ third_party/WebKit/LayoutTests/http/tests/inspector/network/resources/resource.php
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     $gzip = $_GET["gzip"];
     $jsdelay = $_GET["jsdelay"];
     $jscontent = $_GET["jscontent"];
+    $chunked = $_GET["chunked"];
 
     # Enable gzip compression if needed
     if ($gzip)
@@ -68,16 +69,30 @@ __foo(<?php echo($jsdelay)?>);
         $data_len = strlen($data);
         print($data);
         if ($size) {
+            if ($chunked) {
+                ob_flush();
+                flush();
+            }
             for ($i = 0; $size && $i < $size - $data_len; ++$i)
                 echo("=");
         }
     } else {
         # Generate dummy text/html.
         if ($size) {
-            for ($i = 0; $i < $size; ++$i)
+            for ($i = 0; $i < $size; ++$i) {
+                if ($chunked && (1 == $i)) {
+                    ob_flush();
+                    flush();
+                }
                 echo("*");
+            }
         } else {
-            echo("Hello world");
+            echo("Hello ");
+            if ($chunked) {
+                ob_flush();
+                flush();
+            }
+            echo("world");
         }
     }
 ?>
