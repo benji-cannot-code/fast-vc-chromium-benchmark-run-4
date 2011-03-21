9d8e48bf50ffd224dfa46f8337ece7e1316ac1f4
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 19:28:51 +0000

    Use UDPServerSocket for P2P sockets.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6672031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78906 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p_socket_host.cc
+++ content/browser/renderer_host/p2p_socket_host.cc
@@ -5,8 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/p2p_socket_host.h"
 
+#include "content/browser/renderer_host/p2p_socket_host_udp.h"
+
 P2PSocketHost::P2PSocketHost(P2PSocketsHost* host, int routing_id, int id)
     : host_(host), routing_id_(routing_id), id_(id) {
 }
 
 P2PSocketHost::~P2PSocketHost() { }
+
+// static
+P2PSocketHost* P2PSocketHost::Create(
+    P2PSocketsHost* host, int routing_id, int id, P2PSocketType type) {
+  switch (type) {
+    case P2P_SOCKET_UDP:
+      return new P2PSocketHostUdp(host, routing_id, id);
+
+    case P2P_SOCKET_TCP_SERVER:
+      // TODO(sergeyu): Implement TCP sockets support.
+      return NULL;
+
+    case P2P_SOCKET_TCP_CLIENT:
+      return NULL;
+  }
+
+  NOTREACHED();
+  return NULL;
+}
