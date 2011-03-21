c30fc74a53ccacfb0604d51823219d17724db5db
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 20:36:12 +0000

    Add Lion button, wire it up to enter our full-screen.
    BUG=74056
    TEST=On Lion, hit the new full-screen button
    
    Review URL: http://codereview.chromium.org/6715003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78918 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -148,6 +148,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
+// 10.7 adds public APIs for full-screen support. Provide the declaration so it
+// can be called below when building with the 10.5 SDK.
+#if !defined(MAC_OS_X_VERSION_10_7) || \
+    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+
+@interface NSWindow (LionSDKDeclarations)
+- (void)toggleFullScreen:(id)sender;
+@end
+
+enum {
+  NSWindowCollectionBehaviorFullScreenPrimary = 1 << 7,
+  NSWindowCollectionBehaviorFullScreenAuxiliary = 1 << 8
+};
+
+enum {
+  NSWindowFullScreenButton = 7
+};
+
+#endif  // MAC_OS_X_VERSION_10_7
 
 // IncognitoImageView subclasses NSView to allow mouse events to pass through it
 // so you can drag the window by dragging on the spy guy.
@@ -387,6 +406,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // on the window bounds to determine whether to show buttons or not.
     if ([self hasToolbar])  // Do not create the buttons in popups.
       [toolbarController_ createBrowserActionButtons];
+
+    // For versions of Mac OS that provide an "enter fullscreen" button, make
+    // one appear (in a rather hacky manner). http://crbug.com/74065 : When
+    // switching the fullscreen implementation to the new API, revisit how much
+    // of this hacky code is necessary.
+    if ([window respondsToSelector:@selector(toggleFullScreen:)]) {
+      NSWindowCollectionBehavior behavior = [window collectionBehavior];
+      behavior |= NSWindowCollectionBehaviorFullScreenPrimary;
+      [window setCollectionBehavior:behavior];
+
+      NSButton* fullscreenButton =
+          [window standardWindowButton:NSWindowFullScreenButton];
+      [fullscreenButton setAction:@selector(enterFullscreen:)];
+      [fullscreenButton setTarget:self];
+    }
 
     // We are done initializing now.
     initializing_ = NO;
@@ -1856,6 +1890,10 @@ willAnimateFromState:(bookmarks::VisualState)oldState
 
 
 @implementation BrowserWindowController(Fullscreen)
+
+- (IBAction)enterFullscreen:(id)sender {
+  browser_->ExecuteCommand(IDC_FULLSCREEN);
+}
 
 - (void)setFullscreen:(BOOL)fullscreen {
   // The logic in this function is a bit complicated and very carefully
--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -53,6 +53,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image.h"
 
 NSString* const kTabStripNumberOfTabsChanged = @"kTabStripNumberOfTabsChanged";
+
+// 10.7 adds public APIs for full-screen support. Provide the declaration so it
+// can be called below when building with the 10.5 SDK.
+#if !defined(MAC_OS_X_VERSION_10_7) || \
+MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+
+@interface NSWindow (LionSDKDeclarations)
+- (void)toggleFullScreen:(id)sender;
+@end
+
+enum {
+  NSWindowFullScreenButton = 7
+};
+
+#endif  // MAC_OS_X_VERSION_10_7
 
 namespace {
 
@@ -762,10 +777,19 @@ private:
       availableSpace = availableResizeWidth_;
     } else {
       availableSpace = NSWidth([tabStripView_ frame]);
-      // Account for the new tab button and the incognito badge.
+
+      // Account for the widths of the new tab button, the incognito badge, and
+      // the fullscreen button if any/all are present.
       availableSpace -= NSWidth([newTabButton_ frame]) + kNewTabButtonOffset;
       if (browser_->profile()->IsOffTheRecord())
         availableSpace -= kIncognitoBadgeTabStripShrink;
+      if ([[tabStripView_ window]
+          respondsToSelector:@selector(toggleFullScreen:)]) {
+        NSButton* fullscreenButton = [[tabStripView_ window]
+            standardWindowButton:NSWindowFullScreenButton];
+        if (fullscreenButton)
+          availableSpace -= [fullscreenButton frame].size.width;
+      }
     }
     availableSpace -= [self indentForControls];
   }
