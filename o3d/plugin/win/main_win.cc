e2a5dbaea2e582b9e77ab82345452d5319dd9075
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 20:25:33 +0000

    Raise process priority of O3d when running under Chrome for better response and smoothness under high cpu load. BUG=none TEST=do a video change with high cpu load: cpuload --threads 16 --priority 0 100 Review URL: http://codereview.chromium.org/6716003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78917 0039d316-1c4b-4281-b951-d872f2087c98

--- o3d/plugin/win/main_win.cc
+++ o3d/plugin/win/main_win.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(O3D_INTERNAL_PLUGIN)
 #include "breakpad/win/exception_handler_win32.h"
 #endif
+#include "plugin/cross/o3d_glue.h"
 
 using glue::_o3d::PluginObject;
 using glue::StreamManager;
@@ -721,7 +722,6 @@ NPError PlatformPreNPInitialize() {
 NPError PlatformPostNPInitialize() {
   if (!RegisterO3DWindowClass())
     return NPERR_MODULE_LOAD_FAILED_ERROR;
-
   return NPERR_NO_ERROR;
 }
 
@@ -748,6 +748,12 @@ NPError PlatformPostNPShutdown() {
 }
 
 NPError PlatformNPPNew(NPP instance, PluginObject *obj) {
+  if (obj->IsChrome()) {
+    // We want our O3D process to take priority over the browser and other apps.
+    HANDLE process = ::GetCurrentProcess();
+    DWORD priority = ABOVE_NORMAL_PRIORITY_CLASS;
+    ::SetPriorityClass(process, priority);
+  }
   return NPERR_NO_ERROR;
 }
 
