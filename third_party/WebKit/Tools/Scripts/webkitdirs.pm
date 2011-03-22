49da9efd0e01df642e79e11a4d11be62815f4b19
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-22 20:13:46 +0000

    <http://webkit.org/b/56781> Add --dsym switch to enable dsym generation when building with Xcode
    Reviewed by Joseph Pecoraro.
    
    * Scripts/build-webkit: Updated usage statement to include
    --dsym.
    * Scripts/webkitdirs.pm: Added $generateDsym variable.
    (generateDsym): Added. Call determineGenerateDsym() and
    return $generateDsym.
    (determineGenerateDsym): Added.  Parse @ARGV for --dsym and set
    $generateDsym.
    (argumentsForXcode): Added.  Returns additional arguments for
    xcodebuild based on command-line switches.
    (XcodeOptions): Updated to include argumentsForXcode() in the
    array returned.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -61,6 +61,7 @@ my $configurationProductDir;
 my $sourceDir;
 my $currentSVNRevision;
 my $osXVersion;
+my $generateDsym;
 my $isQt;
 my $qmakebin = "qmake"; # Allow override of the qmake binary from $PATH
 my $isSymbian;
@@ -390,12 +391,31 @@ sub currentSVNRevision
     return $currentSVNRevision;
 }
 
+sub generateDsym()
+{
+    determineGenerateDsym();
+    return $generateDsym;
+}
+
+sub determineGenerateDsym()
+{
+    return if defined($generateDsym);
+    $generateDsym = checkForArgumentAndRemoveFromARGV("--dsym");
+}
+
+sub argumentsForXcode()
+{
+    my @args = ();
+    push @args, "DEBUG_INFORMATION_FORMAT=dwarf-with-dsym" if generateDsym();
+    return @args;
+}
+
 sub XcodeOptions
 {
     determineBaseProductDir();
     determineConfiguration();
     determineArchitecture();
-    return (@baseProductDirOption, "-configuration", $configuration, "ARCHS=$architecture");
+    return (@baseProductDirOption, "-configuration", $configuration, "ARCHS=$architecture", argumentsForXcode());
 }
 
 sub XcodeOptionString
