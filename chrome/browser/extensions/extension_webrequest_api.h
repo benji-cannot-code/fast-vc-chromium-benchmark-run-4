9371eba599d91639c9d9a1a9bae56ae4d1f4a73d
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-22 23:18:50 +0000

    Add tabId, type, and timeStamp parameters to extension webRequest events.
    Also added support for filtering based on resource type, tabId, and windowId.
    
    BUG=60101
    TEST=covered by apitests
    
    Review URL: http://codereview.chromium.org/6685010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79066 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webrequest_api.h
+++ chrome/browser/extensions/extension_webrequest_api.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "ipc/ipc_message.h"
 #include "net/base/completion_callback.h"
+#include "webkit/glue/resource_type.h"
 
 class ExtensionEventRouterForwarder;
 class GURL;
@@ -84,7 +85,12 @@ class ExtensionWebRequestEventRouter {
   // Returns a list of event listeners that care about the given event, based
   // on their filter parameters.
   std::vector<const EventListener*> GetMatchingListeners(
-      ProfileId profile_id, const std::string& event_name, const GURL& url);
+      ProfileId profile_id,
+      const std::string& event_name,
+      const GURL& url,
+      int tab_id,
+      int window_id,
+      ResourceType::Type resource_type);
 
   // Decrements the count of event handlers blocking the given request. When the
   // count reaches 0 (or immediately if the request is being cancelled), we
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -1001,6 +1001,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/extension_sidebar_api.h',
         'browser/extensions/extension_special_storage_policy.cc',
         'browser/extensions/extension_special_storage_policy.h',
+        'browser/extensions/extension_tab_id_map.cc',
+        'browser/extensions/extension_tab_id_map.h',
         'browser/extensions/extension_tabs_module.cc',
         'browser/extensions/extension_tabs_module.h',
         'browser/extensions/extension_tabs_module_constants.cc',
