004294578e71f27b1dcffffa827522e5b64f3875
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-22 15:42:29 +0000

    gtk: Move extension_infobar_gtk.[cc,h] into the ui/gtk/infobars/ directory.
    BUG=None
    TEST=None
    
    R=aa@chromium.org
    
    Review URL: http://codereview.chromium.org/6685034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/extension_infobar_gtk.h
+++ chrome/browser/ui/gtk/infobars/extension_infobar_gtk.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_GTK_EXTENSION_INFOBAR_GTK_H_
-#define CHROME_BROWSER_UI_GTK_EXTENSION_INFOBAR_GTK_H_
+#ifndef CHROME_BROWSER_UI_GTK_INFOBARS_EXTENSION_INFOBAR_GTK_H_
+#define CHROME_BROWSER_UI_GTK_INFOBARS_EXTENSION_INFOBAR_GTK_H_
 #pragma once
 
 #include "chrome/browser/extensions/extension_infobar_delegate.h"
@@ -48,4 +48,4 @@ class ExtensionInfoBarGtk : public InfoBar,
   DISALLOW_COPY_AND_ASSIGN(ExtensionInfoBarGtk);
 };
 
-#endif  // CHROME_BROWSER_UI_GTK_EXTENSION_INFOBAR_GTK_H_
+#endif  // CHROME_BROWSER_UI_GTK_INFOBARS_EXTENSION_INFOBAR_GTK_H_
