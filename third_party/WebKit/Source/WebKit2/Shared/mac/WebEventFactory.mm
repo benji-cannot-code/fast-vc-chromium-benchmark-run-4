0eb7c65924e52fdc1eb989e3214445a9e73e654e
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-22 18:26:12 +0000

    WebKit2: Cannot make a selection past the end of the visible page (no autoscrolling) <rdar://problem/8823874> https://bugs.webkit.org/show_bug.cgi?id=56847
    Reviewed by Adam Roben.
    
    * Shared/mac/WebEventFactory.mm:
    (WebKit::currentMouseButton):
    (WebKit::mouseButtonForEvent):
    Add mouse button for mouseEnter/Exit events. [NSEvent buttonNumber] doesn't seem to give the right result
    for these events, but getting the currentMouseButton does work.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/mac/WebEventFactory.mm
+++ third_party/WebKit/Source/WebKit2/Shared/mac/WebEventFactory.mm
@@ -35,6 +35,18 @@ using namespace WebCore;
 
 namespace WebKit {
 
+static WebMouseEvent::Button currentMouseButton()
+{
+    NSUInteger pressedMouseButtons = [NSEvent pressedMouseButtons];
+    if (!pressedMouseButtons)
+        return WebMouseEvent::NoButton;
+    if (pressedMouseButtons == 1 << 0)
+        return WebMouseEvent::LeftButton;
+    if (pressedMouseButtons == 1 << 1)
+        return WebMouseEvent::RightButton;
+    return WebMouseEvent::MiddleButton;
+}
+
 static WebMouseEvent::Button mouseButtonForEvent(NSEvent *event)
 {
     switch ([event type]) {
@@ -50,6 +62,9 @@ static WebMouseEvent::Button mouseButtonForEvent(NSEvent *event)
         case NSOtherMouseUp:
         case NSOtherMouseDragged:
             return WebMouseEvent::MiddleButton;
+        case NSMouseEntered:
+        case NSMouseExited:
+            return currentMouseButton();
         default:
             return WebMouseEvent::NoButton;
     }
