7a358630bff0d587f49767000384ebb7a508b7a0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-23 01:45:23 +0000

    2011-03-22  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Tony Chang.
    
            [chromium] Remove identity from dragTargetDragEnter() function signature
            https://bugs.webkit.org/show_bug.cgi?id=56875
    
            First part of 3-sided patch.  Add new function signature sans identity.
    
            * public/WebView.h:
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::dragTargetDragEnter):
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
@@ -159,6 +159,11 @@ public:
         const WebPoint& clientPoint,
         const WebPoint& screenPoint,
         WebDragOperationsMask operationsAllowed);
+    virtual WebDragOperation dragTargetDragEnter(
+        const WebDragData&,
+        const WebPoint& clientPoint,
+        const WebPoint& screenPoint,
+        WebDragOperationsMask operationsAllowed);
     virtual WebDragOperation dragTargetDragOver(
         const WebPoint& clientPoint,
         const WebPoint& screenPoint,
