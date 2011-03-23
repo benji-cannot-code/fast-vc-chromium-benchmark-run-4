19bc4159514c1c09f438ebd0dbfd59862862cceb
sunandt@google.com <sunandt@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 21:37:38 +0000

    Adding a html page where we open popup using window.open Changing the pyauto popup tests to use this.
    BUG=70659
    TEST=Popup tests should not fail on pyauto bots
    Review URL: http://codereview.chromium.org/6724016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/popups.py
+++ chrome/test/functional/popups.py
@@ -30,7 +30,7 @@ class PopupsTest(pyauto.PyUITest):
     self.assertFalse(self.GetBlockedPopupsInfo(),
                      msg='Should have no blocked popups on startup')
     file_url = self.GetFileURLForPath(os.path.join(
-        self.DataDir(), 'popup_blocker', 'popup-blocked-to-post-blank.html'))
+        self.DataDir(), 'popup_blocker', 'popup-window-open.html'))
     self.NavigateToURL(file_url)
     blocked_popups = self.GetBlockedPopupsInfo()
     self.assertEqual(1, len(blocked_popups), msg='Popup not blocked')
@@ -40,7 +40,7 @@ class PopupsTest(pyauto.PyUITest):
   def testLaunchBlockedPopup(self):
     """Verify that a blocked popup can be unblocked."""
     file_url = self.GetFileURLForPath(os.path.join(
-        self.DataDir(), 'popup_blocker', 'popup-blocked-to-post-blank.html'))
+        self.DataDir(), 'popup_blocker', 'popup-window-open.html'))
     self.NavigateToURL(file_url)
     self.assertEqual(1, len(self.GetBlockedPopupsInfo()))
     self.UnblockAndLaunchBlockedPopup(0)
@@ -55,7 +55,7 @@ class PopupsTest(pyauto.PyUITest):
     """Verify popup blocking is enabled in incognito windows."""
     self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
     file_url = self.GetFileURLForPath(os.path.join(
-        self.DataDir(), 'popup_blocker', 'popup-blocked-to-post-blank.html'))
+        self.DataDir(), 'popup_blocker', 'popup-window-open.html'))
     self.NavigateToURL(file_url, 1, 0)
     blocked_popups = self.GetBlockedPopupsInfo(tab_index=0, windex=1)
     self.assertEqual(1, len(blocked_popups), msg='Popup not blocked')
@@ -64,7 +64,7 @@ class PopupsTest(pyauto.PyUITest):
     """Verify that a blocked popup can be unblocked in incognito."""
     self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
     file_url = self.GetFileURLForPath(os.path.join(
-        self.DataDir(), 'popup_blocker', 'popup-blocked-to-post-blank.html'))
+        self.DataDir(), 'popup_blocker', 'popup-window-open.html'))
     self.NavigateToURL(file_url, 1, 0)
     self.assertEqual(1, len(self.GetBlockedPopupsInfo(tab_index=0, windex=1)))
     self.UnblockAndLaunchBlockedPopup(0, tab_index=0, windex=1)
