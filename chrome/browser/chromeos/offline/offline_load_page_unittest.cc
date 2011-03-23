76859d55b9acf82bf2ff2a34bb24ab5fb3720c93
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 18:22:26 +0000

    Fix chromeos build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/offline/offline_load_page_unittest.cc
+++ chrome/browser/chromeos/offline/offline_load_page_unittest.cc
@@ -4,12 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/offline/offline_load_page.h"
-#include "chrome/common/render_messages.h"
-#include "chrome/common/render_messages_params.h"
 #include "content/browser/browser_thread.h"
 #include "content/browser/renderer_host/test_render_view_host.h"
 #include "content/browser/tab_contents/navigation_entry.h"
 #include "content/browser/tab_contents/test_tab_contents.h"
+#include "content/common/view_messages.h"
 
 static const char* kURL1 = "http://www.google.com/";
 static const char* kURL2 = "http://www.gmail.com/";
