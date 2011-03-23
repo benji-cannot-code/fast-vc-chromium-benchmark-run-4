60010d5d3c92e4dc973a0b3c461bc11a8ed8c141
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 02:51:09 +0000

    Makes instant reload page when enter is pressed if last url loaded was a URL that resulted in failing the provisional load. This typically indicates the user initiated a download. Since instant blocks downloads, we need to reload on enter.
    BUG=74556
    TEST=see bug, covered by browser test too.
    
    R=jcivelli@chromium.org
    
    Review URL: http://codereview.chromium.org/6676091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79093 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_loader.cc
+++ chrome/browser/instant/instant_loader.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_view.h"
 #include "content/browser/tab_contents/navigation_controller.h"
 #include "content/browser/tab_contents/navigation_entry.h"
+#include "content/browser/tab_contents/provisional_load_details.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/browser/tab_contents/tab_contents_delegate.h"
 #include "content/browser/tab_contents/tab_contents_view.h"
@@ -263,6 +264,8 @@ InstantLoader::TabContentsDelegateImpl::TabContentsDelegateImpl(
   DCHECK(loader->preview_contents());
   registrar_.Add(this, NotificationType::INTERSTITIAL_ATTACHED,
       Source<TabContents>(loader->preview_contents()->tab_contents()));
+  registrar_.Add(this, NotificationType::FAIL_PROVISIONAL_LOAD_WITH_ERROR,
+      Source<NavigationController>(&loader->preview_contents()->controller()));
 }
 
 void InstantLoader::TabContentsDelegateImpl::PrepareForNewLoad() {
@@ -365,6 +368,14 @@ void InstantLoader::TabContentsDelegateImpl::Observe(
     const NotificationSource& source,
     const NotificationDetails& details) {
   switch (type.value) {
+    case NotificationType::FAIL_PROVISIONAL_LOAD_WITH_ERROR:
+      if (Details<ProvisionalLoadDetails>(details)->url() == loader_->url_) {
+        // This typically happens with downloads (which are disabled with
+        // instant active). To ensure the download happens when the user presses
+        // enter we set needs_reload_ to true, which triggers a reload.
+        loader_->needs_reload_ = true;
+      }
+      break;
     case NotificationType::RENDER_WIDGET_HOST_DID_PAINT:
       UnregisterForPaintNotifications();
       PreviewPainted();
@@ -496,6 +507,7 @@ void InstantLoader::TabContentsDelegateImpl::DragEnded() {
 }
 
 bool InstantLoader::TabContentsDelegateImpl::CanDownload(int request_id) {
+  // Downloads are disabled.
   return false;
 }
 
@@ -581,7 +593,8 @@ InstantLoader::InstantLoader(InstantLoaderDelegate* delegate, TemplateURLID id)
       template_url_id_(id),
       ready_(false),
       last_transition_type_(PageTransition::LINK),
-      verbatim_(false) {
+      verbatim_(false),
+      needs_reload_(false) {
 }
 
 InstantLoader::~InstantLoader() {
@@ -633,6 +646,7 @@ void InstantLoader::Update(TabContentsWrapper* tab_contents,
   user_text_ = new_user_text;
   verbatim_ = verbatim;
   last_suggestion_.clear();
+  needs_reload_ = false;
 
   bool created_preview_contents = preview_contents_.get() == NULL;
   if (created_preview_contents)
