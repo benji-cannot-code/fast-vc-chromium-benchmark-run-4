35f8fc2bac0dfdcda79087e39b678b478b5c8601
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 19:36:23 +0000

    SEC_ERROR_UNTRUSTED_CERT means the user has marked the (leaf) certificate as not trusted.  We should not allow the user to accept the certificate. A simple way to accomplish that is to treat the certificate as revoked.
    R=agl,rsleevi
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6675030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79165 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate_nss.cc
+++ net/base/x509_certificate_nss.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -104,9 +104,9 @@ int MapSecurityError(int err) {
     case SEC_ERROR_UNKNOWN_ISSUER:
     case SEC_ERROR_UNTRUSTED_ISSUER:
     case SEC_ERROR_CA_CERT_INVALID:
-    case SEC_ERROR_UNTRUSTED_CERT:
       return ERR_CERT_AUTHORITY_INVALID;
     case SEC_ERROR_REVOKED_CERTIFICATE:
+    case SEC_ERROR_UNTRUSTED_CERT:  // Treat as revoked.
       return ERR_CERT_REVOKED;
     case SEC_ERROR_BAD_DER:
     case SEC_ERROR_BAD_SIGNATURE:
@@ -136,7 +136,6 @@ int MapCertErrorToCertStatus(int err) {
     case SEC_ERROR_EXPIRED_CERTIFICATE:
     case SEC_ERROR_EXPIRED_ISSUER_CERTIFICATE:
       return CERT_STATUS_DATE_INVALID;
-    case SEC_ERROR_UNTRUSTED_CERT:
     case SEC_ERROR_UNKNOWN_ISSUER:
     case SEC_ERROR_UNTRUSTED_ISSUER:
     case SEC_ERROR_CA_CERT_INVALID:
@@ -146,6 +145,7 @@ int MapCertErrorToCertStatus(int err) {
     case SEC_ERROR_OCSP_SERVER_ERROR:
       return CERT_STATUS_UNABLE_TO_CHECK_REVOCATION;
     case SEC_ERROR_REVOKED_CERTIFICATE:
+    case SEC_ERROR_UNTRUSTED_CERT:  // Treat as revoked.
       return CERT_STATUS_REVOKED;
     case SEC_ERROR_BAD_DER:
     case SEC_ERROR_BAD_SIGNATURE:
