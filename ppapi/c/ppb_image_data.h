12260dd921f92aa8ccda6c879d0dae114c079a51
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 18:23:57 +0000

    Minor changes to the comments of ResourceTracker and PP_ImageDataFormat.
    BUG=none
    TEST=none
    
    R=brettw@chromium.org
    
    Review URL: http://codereview.chromium.org/6727028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79156 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_image_data.h
+++ ppapi/c/ppb_image_data.h
@@ -28,6 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * PP_ImageDataFormat is an enumeration of the different types of
  * image data formats.
+ *
+ * The third part of each enumeration value describes the memory layout from
+ * the lowest address to the highest. For example, BGRA means the B component
+ * is stored in the lowest address, no matter what endianness the platform is
+ * using.
  */
 typedef enum {
   PP_IMAGEDATAFORMAT_BGRA_PREMUL,
@@ -171,4 +176,3 @@ struct PPB_ImageData {
  */
 
 #endif  /* PPAPI_C_PPB_IMAGE_DATA_H_ */
-
