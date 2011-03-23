a6a47780c5b2ba997dc12aaad46badfe1a235b81
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-23 00:24:47 +0000

    2011-03-22  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by Tony Chang.
    
            [chromium] Make WebTextCheckingResult&apos;s members be public in preparation for converting it to a struct
            https://bugs.webkit.org/show_bug.cgi?id=56857
    
            * DEPS:
            * public/WebTextCheckingResult.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebTextCheckingCompletion.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebTextCheckingCompletion.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-class WebTextCheckingResult;
+struct WebTextCheckingResult;
 template <typename T> class WebVector;
 
 // Gets called back when WebViewClient finished an asynchronous spell checking.
