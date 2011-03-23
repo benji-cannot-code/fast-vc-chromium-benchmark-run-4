8d26d51f63c222fd7b5fb824bfff542dab8c0d09
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-23 00:46:53 +0000

    Fix nits and comment typos.  Add 'virtual' to URLFetcher::Core destructor declaration.
    R=willchan
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6716013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/url_fetcher.cc
+++ chrome/common/net/url_fetcher.cc
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 static const int kBufferSize = 4096;
 
-bool URLFetcher::g_interception_enabled = false;
-
 class URLFetcher::Core
     : public base::RefCountedThreadSafe<URLFetcher::Core>,
       public net::URLRequest::Delegate {
@@ -36,7 +34,7 @@ class URLFetcher::Core
   // For POST requests, set |content_type| to the MIME type of the content
   // and set |content| to the data to upload.  |flags| are flags to apply to
   // the load operation--these should be one or more of the LOAD_* flags
-  // defined in url_request.h.
+  // defined in net/base/load_flags.h.
   Core(URLFetcher* fetcher,
        const GURL& original_url,
        RequestType request_type,
@@ -88,7 +86,7 @@ class URLFetcher::Core
     DISALLOW_COPY_AND_ASSIGN(Registry);
   };
 
-  ~Core();
+  virtual ~Core();
 
   // Wrapper functions that allow us to ensure actions happen on the right
   // thread.
@@ -196,6 +194,9 @@ base::LazyInstance<URLFetcher::Core::Registry>
 
 // static
 URLFetcher::Factory* URLFetcher::factory_ = NULL;
+
+// static
+bool URLFetcher::g_interception_enabled = false;
 
 URLFetcher::URLFetcher(const GURL& url,
                        RequestType request_type,
--- chrome/common/net/url_fetcher.h
+++ chrome/common/net/url_fetcher.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
+
 // This file contains URLFetcher, a wrapper around net::URLRequest that handles
 // low-level details like thread safety, ref counting, and incremental buffer
 // reading.  This is useful for callers who simply want to get the data from a
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 typedef std::vector<std::string> ResponseCookies;
-class URLFetcher;
 class URLRequestContextGetter;
 
 namespace net {
@@ -35,7 +34,8 @@ class URLRequestStatus;
 
 // To use this class, create an instance with the desired URL and a pointer to
 // the object to be notified when the URL has been loaded:
-//   URLFetcher* fetcher = new URLFetcher("http://www.google.com", this);
+//   URLFetcher* fetcher = new URLFetcher("http://www.google.com",
+//                                        URLFetcher::GET, this);
 //
 // Then, optionally set properties on this object, like the request context or
 // extra headers:
@@ -197,7 +197,7 @@ class URLFetcher {
   // Reports that the received content was malformed.
   void ReceivedContentWasMalformed();
 
-  // Cancels all existing URLRequests.  Will notify the URLFetcher::Delegates.
+  // Cancels all existing URLFetchers.  Will notify the URLFetcher::Delegates.
   // Note that any new URLFetchers created while this is running will not be
   // cancelled.  Typically, one would call this in the CleanUp() method of an IO
   // thread, so that no new URLRequests would be able to start on the IO thread
