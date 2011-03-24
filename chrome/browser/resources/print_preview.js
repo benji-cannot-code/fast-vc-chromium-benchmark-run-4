d5be9fcdd2b567be74c8bb1cdbef3a3f18223608
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 20:40:13 +0000

    Print Preview: Pass the default options on first preview generation.
    BUG=none
    TEST=no more DCHECK in print preview on Mac.
    R=arv@chromium.org
    Review URL: http://codereview.chromium.org/6724012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -29,7 +29,6 @@ function load() {
   $('copies').addEventListener('blur', handleCopiesFieldBlur);
 
   chrome.send('getPrinters');
-  chrome.send('getPreview');
 };
 
 /**
@@ -140,25 +139,45 @@ function parsePageRanges() {
   return true;
 }
 
-function printFile() {
+/**
+ * Creates a JSON string based on the values in the printer settings.
+ *
+ * @return {string} JSON string with print job settings.
+ */
+function getSettingsJSON() {
   var selectedPrinter = $('printer-list').selectedIndex;
-  var printerName = $('printer-list').options[selectedPrinter].textContent;
+  var printerName = '';
+  if (selectedPrinter >= 0)
+    printerName = $('printer-list').options[selectedPrinter].textContent;
   var printAll = $('all-pages').checked;
   var twoSided = $('two-sided').checked;
   var copies = $('copies').value;
   var collate = $('collate').checked;
-  var landscape = ($('layout').options[$('layout').selectedIndex].value == "1");
-  var color = ($('color').options[$('color').selectedIndex].value == "1");
+  var landscape = ($('layout').options[$('layout').selectedIndex].value == '1');
+  var color = ($('color').options[$('color').selectedIndex].value == '1');
 
-  var jobSettings = JSON.stringify({'printerName': printerName,
-                                    'pageRange': pageRangesInfo,
-                                    'printAll': printAll,
-                                    'twoSided': twoSided,
-                                    'copies': copies,
-                                    'collate': collate,
-                                    'landscape': landscape,
-                                    'color': color});
-  chrome.send('print', [jobSettings]);
+  return JSON.stringify({'printerName': printerName,
+                         'pageRange': pageRangesInfo,
+                         'printAll': printAll,
+                         'twoSided': twoSided,
+                         'copies': copies,
+                         'collate': collate,
+                         'landscape': landscape,
+                         'color': color});
+}
+
+/**
+ * Asks the browser to print the preview PDF based on current print settings.
+ */
+function printFile() {
+  chrome.send('print', [getSettingsJSON()]);
+}
+
+/**
+ * Asks the browser to generate a preview PDF based on current print settings.
+ */
+function getPreview() {
+  chrome.send('getPreview', [getSettingsJSON()]);
 }
 
 /**
@@ -178,6 +197,9 @@ function setPrinters(printers) {
     option.textContent = localStrings.getString('noPrinter');
     $('printer-list').add(option);
   }
+
+  // Once the printer list is populated, generate the initial preview.
+  getPreview();
 }
 
 function onPDFLoad() {
--- content/browser/renderer_host/render_view_host.h
+++ content/browser/renderer_host/render_view_host.h
@@ -212,8 +212,8 @@ class RenderViewHost : public RenderWidgetHost {
   // behalf.
   bool PrintPages();
 
-  // Asks the renderer to render pages for print preview.
-  bool PrintPreview();
+  // Asks the renderer to render pages for print preview with |settings|.
+  bool PrintPreview(const DictionaryValue& settings);
 
   // Notify renderer of success/failure of print job.
   void PrintingDone(int document_cookie, bool success);
