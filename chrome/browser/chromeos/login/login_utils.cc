6a003a384644257d8b09cc201b6791a5694e6b86
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 04:56:01 +0000

    Call ScheduleSavePersistentPrefs() after writing IME prefs.
    BUG=chromium-os:13411
    TEST=manual and try
    
    Review URL: http://codereview.chromium.org/6705026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79253 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -504,6 +504,7 @@ void LoginUtilsImpl::SetFirstLoginPrefs(PrefService* prefs) {
   language_preferred_languages.Init(prefs::kLanguagePreferredLanguages,
                                     prefs, NULL);
   language_preferred_languages.SetValue(JoinString(language_codes, ','));
+  prefs->ScheduleSavePersistentPrefs();
 }
 
 Authenticator* LoginUtilsImpl::CreateAuthenticator(
