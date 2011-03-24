d5cca1367049d86d4f1684a325010997cf77f09e
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 15:57:31 +0000

    Convert some WidgetWin construction to use CreateParams.
    BUG=72040
    TEST=none
    R=erg@chromium.org,estade@chromium.org
    Review URL: http://codereview.chromium.org/6719006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79279 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/info_bubble_gtk.cc
+++ chrome/browser/ui/gtk/info_bubble_gtk.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/gtk_util.h"
 #include "chrome/browser/ui/gtk/info_bubble_accelerators_gtk.h"
 #include "content/common/notification_service.h"
+#include "ui/base/gtk/gtk_windowing.h"
 #include "ui/gfx/gtk_util.h"
 #include "ui/gfx/path.h"
 #include "ui/gfx/rect.h"
@@ -340,7 +341,7 @@ void InfoBubbleGtk::MoveWindow() {
 void InfoBubbleGtk::StackWindow() {
   // Stack our window directly above the toplevel window.
   if (toplevel_window_)
-    gtk_util::StackPopupWindow(window_, GTK_WIDGET(toplevel_window_));
+    ui::StackPopupWindow(window_, GTK_WIDGET(toplevel_window_));
 }
 
 void InfoBubbleGtk::Observe(NotificationType type,
