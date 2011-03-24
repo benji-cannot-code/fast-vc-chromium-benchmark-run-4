a2b35bbaf4dc1a2b9fab1f537ddf0ada92f89cc0
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 17:57:31 +0000

    Correct the duplicate creation of desktop shortcuts that can occur when system-level chrome creates a shortcut in the user's current desktop.
    BUG=5073
    TEST=Install system-level Chrome, observe a Chrome shortcut is created in the All Users desktop. Run Chrome, observe that no duplicate shortcut is created.
    Review URL: http://codereview.chromium.org/6731018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/shell_util.h
+++ chrome/installer/util/shell_util.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/work_item_list.h"
 
 class BrowserDistribution;
+class FilePath;
 
 // This is a utility class that provides common shell integration methods
 // that can be used by installer as well as Chrome.
@@ -88,10 +89,12 @@ class ShellUtil {
   // If alternate is true, an alternate text for the shortcut is used.
   // create_new: If false, will only update the shortcut. If true, the function
   //             will create a new shortcut if it doesn't exist already.
+  // Returns true iff the method causes a shortcut to be created / updated.
   static bool CreateChromeDesktopShortcut(BrowserDistribution* dist,
                                           const std::wstring& chrome_exe,
                                           const std::wstring& description,
-                                          int shell_change, bool alternate,
+                                          ShellChange shell_change,
+                                          bool alternate,
                                           bool create_new);
 
   // Create Chrome shortcut on Quick Launch Bar.
@@ -129,7 +132,7 @@ class ShellUtil {
   // Gets the desktop path for the current user or all users (if system_level
   // is true) and returns it in 'path' argument. Return true if successful,
   // otherwise returns false.
-  static bool GetDesktopPath(bool system_level, std::wstring* path);
+  static bool GetDesktopPath(bool system_level, FilePath* path);
 
   // Gets the Quick Launch shortcuts path for the current user and
   // returns it in 'path' argument. Return true if successful, otherwise
