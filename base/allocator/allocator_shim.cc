fac7a300b362951b9d035b2d7e2de5165df6326b
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 00:30:02 +0000

    Switch to using WinHeap for the browser process
    Disabled the TCMalloc double-free detection,
    which should restore much of the performance loss
    caused by this experiment (to find double frees,
    if any).  This should resolve bug 76640
    
    As an alternate experiment to better illuminate our
    memory corruption problem, this CL also enables
    WinHeap (default Windows allocator) in the browser
    process.  There is a chance that this will cause a
    very tiny reduction in performance, but it will surely
    increase our crash rate (based on what we saw recently
    on the dev channel).
    
    BUG=76640
    r=rtenneti
    Review URL: http://codereview.chromium.org/6705027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79224 0039d316-1c4b-4281-b951-d872f2087c98

--- base/allocator/allocator_shim.cc
+++ base/allocator/allocator_shim.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,7 +42,7 @@ typedef enum {
 // See SetupSubprocessAllocator() to specify a default secondary (subprocess)
 // allocator.
 // TODO(jar): Switch to using TCMALLOC for the renderer as well.
-static Allocator allocator = TCMALLOC;
+static Allocator allocator = WINHEAP;
 
 // The names of the environment variables that can optionally control the
 // selection of the allocator.  The primary may be used to control overall
