f45fb47f320bb435d684dadd093e9039270a45c6
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 19:45:02 +0000

    Add support for the cgroups config we have in chromeos
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6713089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79308 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/child_process_launcher.cc
+++ content/browser/child_process_launcher.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -246,6 +246,11 @@ class ChildProcessLauncher::Context
     process_.set_handle(base::kNullProcessHandle);
   }
 
+  void SetProcessBackgrounded(bool background) {
+    DCHECK(!starting_);
+    process_.SetProcessBackgrounded(background);
+  }
+
   static void TerminateInternal(
 #if defined(OS_LINUX)
       bool zygote,
@@ -344,6 +349,10 @@ base::TerminationStatus ChildProcessLauncher::GetChildTerminationStatus(
 }
 
 void ChildProcessLauncher::SetProcessBackgrounded(bool background) {
-  DCHECK(!context_->starting_);
-  context_->process_.SetProcessBackgrounded(background);
+  BrowserThread::PostTask(
+      BrowserThread::PROCESS_LAUNCHER, FROM_HERE,
+      NewRunnableMethod(
+          context_.get(),
+          &ChildProcessLauncher::Context::SetProcessBackgrounded,
+          background));
 }
