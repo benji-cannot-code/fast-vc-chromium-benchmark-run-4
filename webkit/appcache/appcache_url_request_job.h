97fad308250953865983040fa2995dfbdfcd6f0b
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 00:03:44 +0000

    Move URLRequestJob's histogram logic to URLRequestHttpJob.
    While this slightly degeneralizes some stats-collection code,
    the svn history shows that this code has, over several years, only been used for SDCH,
    and thus it's worth some degeneralization in order to greatly simplify URLRequestJob.
    
    BUG=none
    TEST=try bots
    
    Review URL: http://codereview.chromium.org/6713019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79219 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_url_request_job.h
+++ webkit/appcache/appcache_url_request_job.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -119,7 +119,6 @@ class AppCacheURLRequestJob : public net::URLRequestJob,
   // FilterContext methods
   virtual bool GetMimeType(std::string* mime_type) const;
   virtual int GetResponseCode() const;
-  virtual bool IsCachedContent() const;
 
   AppCacheStorage* storage_;
   bool has_been_started_;
