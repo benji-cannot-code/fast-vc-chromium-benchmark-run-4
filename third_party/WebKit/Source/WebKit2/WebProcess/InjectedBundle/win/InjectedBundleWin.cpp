e0de718c0ddc58294dc73f49fd75b5232d9c5f6b
jeffm@apple.com <jeffm@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-24 17:51:41 +0000

    Fix typo - USE(CF_NETWORK) should be USE(CFNETWORK).
    * WebProcess/InjectedBundle/win/InjectedBundleWin.cpp:
    (WebKit::InjectedBundle::setHostAllowsAnyHTTPSCertificate): USE(CF_NETWORK) should be USE(CFNETWORK)
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/win/InjectedBundleWin.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/win/InjectedBundleWin.cpp
@@ -90,7 +90,7 @@ void InjectedBundle::activateMacFontAscentHack()
 
 void InjectedBundle::setHostAllowsAnyHTTPSCertificate(const String& host)
 {
-#if USE(CF_NETWORK)
+#if USE(CFNETWORK)
     ResourceHandle::setHostAllowsAnyHTTPSCertificate(host);
 #endif
 }
