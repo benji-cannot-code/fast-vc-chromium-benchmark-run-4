6b256803604bddd0c38eccd38d072921f22274a9
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 00:30:27 +0000

    Remove the sum of square tally from histograms
    This value is accrued without locking, and hence is too
    commonly corrupted.  Even if it is gathered
    perfectly, I've been informed that for very large
    samples the computed variance is "not a stable
    metric" and can diverge."  If we did want to 
    look at the variance for most distribution, then
    we need to look at the variance of the log
    normal distribution, which would mean we wanted
    to take the sum-of-squares of the log, which is
    computationally intense... and probably of little
    value.
    
    Bottom line: Remove the useless metric.
    
    I still need to change the protobufs so that
    this value is optional in uploads, but other than
    that tiny point, this CL removes almost all
    traces of this metric.
    
    
    r=rtenneti
    Review URL: http://codereview.chromium.org/6672071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/metrics_helpers.cc
+++ chrome/common/metrics_helpers.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -402,7 +402,8 @@ void MetricsLogBase::RecordHistogramDelta(
   WriteAttribute("name", CreateBase64Hash(histogram.histogram_name()));
 
   WriteInt64Attribute("sum", snapshot.sum());
-  WriteInt64Attribute("sumsquares", snapshot.square_sum());
+  // TODO(jar): Remove sumsquares when protobuffer accepts this as optional.
+  WriteInt64Attribute("sumsquares", 0);
 
   for (size_t i = 0; i < histogram.bucket_count(); i++) {
     if (snapshot.counts(i)) {
