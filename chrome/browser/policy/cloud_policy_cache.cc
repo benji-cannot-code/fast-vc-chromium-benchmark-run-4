dccc48a8e2a389efd37a3e0a3e5ebeefa0db4bf4
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 12:14:02 +0000

    Always set refresh timestamp when installing cloud policy.
    BUG=77232
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6730026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79273 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud_policy_cache.cc
+++ chrome/browser/policy/cloud_policy_cache.cc
@@ -11,11 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/task.h"
 #include "base/values.h"
-#include "content/browser/browser_thread.h"
 #include "chrome/browser/policy/configuration_policy_pref_store.h"
 #include "chrome/browser/policy/proto/cloud_policy.pb.h"
 #include "chrome/browser/policy/proto/device_management_constants.h"
 #include "chrome/browser/policy/proto/device_management_local.pb.h"
+#include "content/browser/browser_thread.h"
 
 using google::protobuf::RepeatedField;
 using google::protobuf::RepeatedPtrField;
@@ -216,6 +216,7 @@ void CloudPolicyCache::SetPolicy(const em::PolicyFetchResponse& policy) {
   DCHECK(CalledOnValidThread());
   bool initialization_was_not_complete = !initialization_complete_;
   is_unmanaged_ = false;
+  last_policy_refresh_time_ = base::Time::NowFromSystemTime();
   base::Time timestamp;
   PolicyMap mandatory_policy;
   PolicyMap recommended_policy;
@@ -230,7 +231,6 @@ void CloudPolicyCache::SetPolicy(const em::PolicyFetchResponse& policy) {
   mandatory_policy_.Swap(&mandatory_policy);
   recommended_policy_.Swap(&recommended_policy);
   initialization_complete_ = true;
-  last_policy_refresh_time_ = timestamp;
   has_device_policy_ = false;
 
   if (new_policy_differs || initialization_was_not_complete) {
