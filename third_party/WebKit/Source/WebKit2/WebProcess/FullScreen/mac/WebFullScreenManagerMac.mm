bbb3e6583b67278e2763e2543bb7d2daf9265a3d
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-24 22:42:52 +0000

    Unreviewed build fix.
    Remove reference to LayerTreeHostMac.h from WebFullScreenManagerMac.mm.
    
    * WebProcess/FullScreen/mac/WebFullScreenManagerMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/FullScreen/mac/WebFullScreenManagerMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/FullScreen/mac/WebFullScreenManagerMac.mm
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "Connection.h"
 #import "LayerTreeContext.h"
-#import "LayerTreeHostMac.h"
 #import "MessageID.h"
 #import "WebFullScreenManagerProxyMessages.h"
 #import "WebPage.h"
