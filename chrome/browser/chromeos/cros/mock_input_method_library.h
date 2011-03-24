24d02cd630d27b5f876d9f35577f12fd396b4c2f
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 05:07:42 +0000

    Switch to an appropriate engine when ibus-daemon is started.
    BUG=chromium-os:12244
    TEST=see the bug
    
    Review URL: http://codereview.chromium.org/6726021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/mock_input_method_library.h
+++ chrome/browser/chromeos/cros/mock_input_method_library.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,7 +34,7 @@ class MockInputMethodLibrary : public InputMethodLibrary {
   MOCK_CONST_METHOD0(current_input_method, const InputMethodDescriptor&(void));
   MOCK_CONST_METHOD0(current_ime_properties, const ImePropertyList&(void));
   MOCK_METHOD1(GetKeyboardOverlayId, std::string(const std::string&));
-  MOCK_METHOD0(StartInputMethodDaemon, void(void));
+  MOCK_METHOD0(StartInputMethodDaemon, bool(void));
   MOCK_METHOD0(StopInputMethodDaemon, void(void));
   MOCK_METHOD1(SetDeferImeStartup, void(bool));
   MOCK_METHOD1(SetEnableAutoImeShutdown, void(bool));
