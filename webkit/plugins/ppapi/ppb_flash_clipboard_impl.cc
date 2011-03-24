e55ab516a63e251d850159fcd6884d56f8739269
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-24 17:13:38 +0000

    Pepper/Flapper: Add IsFormatAvailable() to PPB_Flash_Clipboard.
    This requires a WebKit change (see
    https://bugs.webkit.org/show_bug.cgi?id=56868).
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6724010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79289 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppb_flash_clipboard_impl.cc
+++ webkit/plugins/ppapi/ppb_flash_clipboard_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebKitClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
+#include "webkit/plugins/ppapi/common.h"
 #include "webkit/plugins/ppapi/ppapi_plugin_instance.h"
 #include "webkit/plugins/ppapi/resource_tracker.h"
 #include "webkit/plugins/ppapi/var.h"
@@ -41,6 +42,39 @@ WebKit::WebClipboard::Buffer ConvertClipboardType(
       NOTREACHED();
       return WebKit::WebClipboard::BufferStandard;
   }
+}
+
+WebKit::WebClipboard::Format ConvertClipboardFormat(
+    PP_Flash_Clipboard_Format format) {
+  switch (format) {
+    case PP_FLASH_CLIPBOARD_FORMAT_PLAINTEXT:
+      return WebKit::WebClipboard::FormatPlainText;
+    case PP_FLASH_CLIPBOARD_FORMAT_HTML:
+      return WebKit::WebClipboard::FormatHTML;
+    case PP_FLASH_CLIPBOARD_FORMAT_INVALID:
+    default:
+      NOTREACHED();
+      return WebKit::WebClipboard::FormatPlainText;  // Gotta return something.
+  }
+}
+
+PP_Bool IsFormatAvailable(PP_Instance instance_id,
+                          PP_Flash_Clipboard_Type clipboard_type,
+                          PP_Flash_Clipboard_Format format) {
+  // If you don't give us an instance, we don't give you anything.
+  PluginInstance* instance = ResourceTracker::Get()->GetInstance(instance_id);
+  if (!instance)
+    return PP_FALSE;
+
+  WebKit::WebClipboard* web_clipboard = WebKit::webKitClient()->clipboard();
+  if (!web_clipboard) {
+    NOTREACHED();
+    return PP_FALSE;
+  }
+
+  return BoolToPPBool(
+      web_clipboard->isFormatAvailable(ConvertClipboardFormat(format),
+                                       ConvertClipboardType(clipboard_type)));
 }
 
 PP_Var ReadPlainText(PP_Instance instance_id,
@@ -87,6 +121,7 @@ int32_t WritePlainText(PP_Instance instance_id,
 }
 
 const PPB_Flash_Clipboard ppb_flash_clipboard = {
+  &IsFormatAvailable,
   &ReadPlainText,
   &WritePlainText,
 };
