d804c115c6bf80801f4cecdf2bdb22ce15ba8ee7
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-25 01:34:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=57069 Add WKIconDatabase api to the project files, and expose accessor on WKContext.
    Reviewed by Darin Adler.
    
    Project file stuffs:
    * GNUmakefile.am:
    * WebKit2.xcodeproj/project.pbxproj:
    * win/WebKit2.vcproj:
    
    * Shared/API/c/WKBase.h:
    * UIProcess/API/C/WKAPICast.h:
    
    * UIProcess/API/C/WKContext.cpp:
    (WKContextGetIconDatabase):
    * UIProcess/API/C/WKContext.h:
    
    * UIProcess/API/C/WKIconDatabase.cpp: Added.
    (WKIconDatabaseGetTypeID):
    * UIProcess/API/C/WKIconDatabase.h: Added.
    
    * UIProcess/WebContext.h:
    (WebKit::WebContext::iconDatabase):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKContext.cpp
@@ -179,6 +179,11 @@ WKGeolocationManagerRef WKContextGetGeolocationManager(WKContextRef contextRef)
     return toAPI(toImpl(contextRef)->geolocationManagerProxy());
 }
 
+WKIconDatabaseRef WKContextGetIconDatabase(WKContextRef contextRef)
+{
+    return toAPI(toImpl(contextRef)->iconDatabase());
+}
+
 WKKeyValueStorageManagerRef WKContextGetKeyValueStorageManager(WKContextRef contextRef)
 {
     return toAPI(toImpl(contextRef)->keyValueStorageManagerProxy());
