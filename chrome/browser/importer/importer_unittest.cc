c49d5483a2dfe523a89ea3aff1d207262c973fc8
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 01:27:16 +0000

    importer: Remove typedef from struct declaration.
    BUG=None
    TEST=out/Debug/unit_tests --gtest_filter=ImporterTest.*
    
    R=avi@chromium.org,jam@chromium.org
    
    Review URL: http://codereview.chromium.org/6713118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79359 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_unittest.cc
+++ chrome/browser/importer/importer_unittest.cc
@@ -127,24 +127,13 @@ class ImporterTest : public testing::Test {
 
 const int kMaxPathSize = 5;
 
-typedef struct {
+struct BookmarkList {
   const bool in_toolbar;
   const size_t path_size;
   const wchar_t* path[kMaxPathSize];
   const wchar_t* title;
   const char* url;
-} BookmarkList;
-
-typedef struct {
-  const char* origin;
-  const char* action;
-  const char* realm;
-  const wchar_t* username_element;
-  const wchar_t* username;
-  const wchar_t* password_element;
-  const wchar_t* password;
-  bool blacklisted;
-} PasswordList;
+};
 
 // Returns true if the |entry| is in the |list|.
 bool FindBookmarkEntry(const ProfileWriter::BookmarkEntry& entry,
@@ -505,6 +494,17 @@ static const BookmarkList kFirefox2Bookmarks[] = {
    "mailto:username@host"},
 };
 
+struct PasswordList {
+  const char* origin;
+  const char* action;
+  const char* realm;
+  const wchar_t* username_element;
+  const wchar_t* username;
+  const wchar_t* password_element;
+  const wchar_t* password;
+  bool blacklisted;
+};
+
 static const PasswordList kFirefox2Passwords[] = {
   {"https://www.google.com/", "", "https://www.google.com/",
     L"", L"", L"", L"", true},
@@ -518,10 +518,10 @@ static const PasswordList kFirefox2Passwords[] = {
     L"loginuser", L"hello", L"", L"world", false},
 };
 
-typedef struct {
+struct KeywordList {
   const wchar_t* keyword;
   const char* url;
-} KeywordList;
+};
 
 static const KeywordList kFirefox2Keywords[] = {
   // Searh plugins
