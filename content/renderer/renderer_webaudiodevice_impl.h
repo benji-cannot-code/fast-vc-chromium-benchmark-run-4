5997749c19a9736b75e7a391e76049ecb3357a15
crogers@google.com <crogers@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 23:58:10 +0000

    Land Henrik's patch: http://codereview.chromium.org/6721027/ BUG=none TEST=none TBR=crogers@google.com Review URL: http://codereview.chromium.org/6745026
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79473 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webaudiodevice_impl.h
+++ content/renderer/renderer_webaudiodevice_impl.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/scoped_ptr.h"
+#include "base/ref_counted.h"
 #include "content/renderer/audio_device.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebAudioDevice.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebVector.h"
@@ -29,10 +29,11 @@ class RendererWebAudioDeviceImpl : public WebKit::WebAudioDevice,
 
   // AudioDevice::RenderCallback implementation.
   virtual void Render(const std::vector<float*>& audio_data,
-                      size_t number_of_frames);
+                      size_t number_of_frames,
+                      size_t audio_delay_milliseconds);
 
  private:
-  scoped_ptr<AudioDevice> audio_device_;
+  scoped_refptr<AudioDevice> audio_device_;
 
   // Weak reference to the callback into WebKit code.
   WebKit::WebAudioDevice::RenderCallback* client_callback_;
