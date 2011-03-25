057e4daab566fb4fbe7401ce1c7187b21bcd3b48
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 13:19:08 +0000

    importer: Add UMA logging to Firefox warning dialog.
    BUG=51100
    TEST=None
    
    R=avi@chromium.org,mirandac@chromium.org
    
    Review URL: http://codereview.chromium.org/6676138
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79393 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/importer/import_lock_dialog_view.cc
+++ chrome/browser/ui/views/importer/import_lock_dialog_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/importer/importer_host.h"
 #include "chrome/browser/importer/importer_lock_dialog.h"
+#include "chrome/browser/metrics/user_metrics.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
@@ -26,6 +27,7 @@ namespace importer {
 void ShowImportLockDialog(gfx::NativeWindow parent,
                           ImporterHost* importer_host) {
   ImportLockDialogView::Show(parent, importer_host);
+  UserMetrics::RecordAction(UserMetricsAction("ImportLockDialogView_Shown"));
 }
 
 }  // namespace importer
