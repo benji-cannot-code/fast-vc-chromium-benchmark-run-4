f5e52fec4112168a20bd3cd8b4f7afbabd2c985f
benjamin.poulain@nokia.com <benjamin.poulain@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-25 17:21:41 +0000

    [Qt] QtWebKit will not compile with QT_ASCII_CAST_WARNINGS enabled https://bugs.webkit.org/show_bug.cgi?id=57087
    Reviewed by Andreas Kling.
    
    Use explicit conversion for string to avoid depending on the default codec
    installed by the user code.
    
    * bridge/qt/qt_pixmapruntime.cpp:
    (JSC::Bindings::QtPixmapToDataUrlMethod::invoke):
    (JSC::Bindings::QtPixmapInstance::valueOf):
    * platform/qt/LanguageQt.cpp:
    (WebCore::platformDefaultLanguage):
    * platform/qt/PasteboardQt.cpp:
    (WebCore::Pasteboard::writeSelection):
    * plugins/qt/PluginPackageQt.cpp:
    (WebCore::initializeGtk):
    * plugins/qt/PluginViewQt.cpp:
    (WebCore::getPluginDisplay):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/LanguageQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/LanguageQt.cpp
@@ -36,7 +36,7 @@ namespace WebCore {
 String platformDefaultLanguage()
 {
     QLocale locale;
-    return locale.name().replace("_", "-");
+    return locale.name().replace(QLatin1Char('_'), QLatin1Char('-'));
 }
 
 }
