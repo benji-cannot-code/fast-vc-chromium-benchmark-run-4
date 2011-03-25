d7d5cdfa8d788b20baf953026355fac5c6f11a6c
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 22:30:21 +0000

    Add an opt-out header for HTTP throttling. Never throttle for localhost.
    Added net::IsLocalhost() function to net/base/net_utils.h
    
    Unit tests for the above.  Also fix flakiness in the ReceivedContentMalformed test that was caused by non-zero jitter.
    
    Modify back-off policy to ignore first 4 errors to help avoid back-off from erroneously kicking in on flaky connections.  Make maximum back-off period 15 minutes instead of 60.  Added documentation of results of analyzing behavior this new policy will give.
    
    Add a simple server for manual testing of the throttling feature.
    
    BUG=66062
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/6711046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79464 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_throttler_manager.h
+++ net/url_request/url_request_throttler_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <map>
+#include <set>
 #include <string>
 
 #include "base/basictypes.h"
@@ -42,6 +43,11 @@ class URLRequestThrottlerManager : public base::NonThreadSafe {
   // informations.
   scoped_refptr<URLRequestThrottlerEntryInterface> RegisterRequestUrl(
       const GURL& url);
+
+  // Adds the given host to a list of sites for which exponential back-off
+  // throttling will be disabled.  Subdomains are not included, so they
+  // must be added separately.
+  void AddToOptOutList(const std::string& host);
 
   // Registers a new entry in this service and overrides the existing entry (if
   // any) for the URL. The service will hold a reference to the entry.
@@ -94,6 +100,10 @@ class URLRequestThrottlerManager : public base::NonThreadSafe {
   typedef std::map<std::string, scoped_refptr<URLRequestThrottlerEntry> >
       UrlEntryMap;
 
+  // We maintain a set of hosts that have opted out of exponential
+  // back-off throttling.
+  typedef std::set<std::string> OptOutHosts;
+
   // Maximum number of entries that we are willing to collect in our map.
   static const unsigned int kMaximumNumberOfEntries;
   // Number of requests that will be made between garbage collection.
@@ -102,6 +112,9 @@ class URLRequestThrottlerManager : public base::NonThreadSafe {
   // Map that contains a list of URL ID and their matching
   // URLRequestThrottlerEntry.
   UrlEntryMap url_entries_;
+
+  // Set of hosts that have opted out.
+  OptOutHosts opt_out_hosts_;
 
   // This keeps track of how many requests have been made. Used with
   // GarbageCollectEntries.
