eb6cad74393d3d6c80c609ef074ad3014b915de8
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 02:16:34 +0000

    Limit impact of large result sets in the HistoryQuickProvider by short-circuiting any scoring if a certain maximum number of results is exceeded. That limit is currently set to 500.
    BUG=None
    TEST=Using a large history database enter a term of 'h' in the omnibox. No HQP results should be presented since 'h' occurs in nearly all URLs. Continue typing a URL from the history and after getting to the domain portion the HQP should start returning results. Alternatively, type a word starting with 'h' and which matches a term in an historical page title. Added a unit test.
    Review URL: http://codereview.chromium.org/6676051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79365 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_url_index_unittest.cc
+++ chrome/browser/history/in_memory_url_index_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "base/scoped_ptr.h"
+#include "base/string_util.h"
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/history/in_memory_url_index.h"
@@ -141,6 +142,24 @@ class LimitedInMemoryURLIndexTest : public InMemoryURLIndexTest {
   }
 };
 
+class ExpandedInMemoryURLIndexTest : public InMemoryURLIndexTest {
+ protected:
+  virtual void SetUp() {
+    InMemoryURLIndexTest::SetUp();
+    // Add 600 more history items.
+    // NOTE: Keep the string length constant at least the length of the format
+    // string plus 5 to account for a 3 digit number and terminator.
+    char url_format[] = "http://www.google.com/%d";
+    const size_t kMaxLen = arraysize(url_format) + 5;
+    char url_string[kMaxLen + 1];
+    for (int i = 0; i < 600; ++i) {
+      base::snprintf(url_string, kMaxLen, url_format, i);
+      URLRow row(MakeURLRow(url_string, "Google Search", 20, 0, 20));
+      AddURL(row);
+    }
+  }
+};
+
 TEST_F(InMemoryURLIndexTest, Construction) {
   url_index_.reset(new InMemoryURLIndex(FilePath(FILE_PATH_LITERAL("/dummy"))));
   EXPECT_TRUE(url_index_.get());
@@ -184,7 +203,7 @@ TEST_F(InMemoryURLIndexTest, Retrieval) {
   terms.clear();
   terms.push_back(ASCIIToUTF16("drudge"));
   matches = url_index_->HistoryItemsForTerms(terms);
-  ASSERT_EQ(2U, url_index_->HistoryItemsForTerms(terms).size());
+  ASSERT_EQ(2U, matches.size());
   // The results should be in descending score order.
   EXPECT_GT(matches[0].raw_score, matches[1].raw_score);
 
@@ -216,6 +235,23 @@ TEST_F(InMemoryURLIndexTest, Retrieval) {
             matches[0].url_info.url().spec());  // Note: URL gets lowercased.
   EXPECT_EQ(ASCIIToUTF16("Practically Useless Search Result"),
             matches[0].url_info.title());
+}
+
+TEST_F(ExpandedInMemoryURLIndexTest, ShortCircuit) {
+  url_index_.reset(new InMemoryURLIndex(FilePath(FILE_PATH_LITERAL("/dummy"))));
+  url_index_->Init(this, "en,ja,hi,zh");
+  InMemoryURLIndex::String16Vector terms;
+
+  // A search for 'w' should short-circuit and not return any matches.
+  terms.push_back(ASCIIToUTF16("w"));
+  ScoredHistoryMatches matches = url_index_->HistoryItemsForTerms(terms);
+  EXPECT_TRUE(matches.empty());
+
+  // A search for 'working' should not short-circuit.
+  terms.clear();
+  terms.push_back(ASCIIToUTF16("working"));
+  matches = url_index_->HistoryItemsForTerms(terms);
+  EXPECT_EQ(1U, matches.size());
 }
 
 TEST_F(InMemoryURLIndexTest, TitleSearch) {
