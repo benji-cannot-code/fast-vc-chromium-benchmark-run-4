15b7d0bd61a0c5aa538aaad975fe42df648d5078
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 19:27:53 +0000

    Fix for "Mouseovers follow the cursor even when there's a find bar in the way"
    Perform a hit test in the render widget host view to check if the cursor is
    actually on top of a different view - such as the find bar. If it is and that
    view has a method -[(BOOL)nonWebContentView] that returns YES, then ignore
    the event in the render widget host.
    
    Change FindBarView to have the aforementioned method that returns YES.
    
    Patch by Alexei Svitkine <asvitkine@chromium.org>
    
    BUG=22191
    TEST=Leave the content area by mousing over a visible find bar. Mouseovers
         from the content area should extinguish, and no masked by the find bar
         should be triggered.
    R=mark@chromium.org
    Review URL: http://codereview.chromium.org/6676094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_mac.h
+++ chrome/browser/renderer_host/render_widget_host_view_mac.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -126,6 +126,9 @@ class RWHVMEditCommandHelper;
 
   // Whether or not plugin IME is currently enabled active.
   BOOL pluginImeActive_;
+
+  // Whether the previous mouse event was ignored due to hitTest check.
+  BOOL mouseEventWasIgnored_;
 }
 
 @property(assign, nonatomic) NSRect caretRect;
