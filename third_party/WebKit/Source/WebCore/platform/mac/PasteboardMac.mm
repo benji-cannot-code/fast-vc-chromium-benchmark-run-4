0d740e0b4dec46abe52eafb2b6cbfaf512bca9c9
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-25 18:47:45 +0000

    Pasteboard data's RTF data doesn't always include URLs via NSLinkAttributeName. https://bugs.webkit.org/show_bug.cgi?id=57107 <rdar://problem/9084267>
    Reviewed by Oliver Hunt.
    
    If the selection is at the beginning of content inside an anchor tag
    we move the selection start to include the anchor.
            
    * platform/mac/PasteboardMac.mm:
    (WebCore::Pasteboard::writeSelection): Modified to change the selection
    start according to the new rule.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@81974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/PasteboardMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/PasteboardMac.mm
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD)
 #import "HTMLConverter.h"
 #endif
+#import "htmlediting.h"
 #import "HTMLNames.h"
 #import "Image.h"
 #import "KURL.h"
@@ -148,6 +149,17 @@ void Pasteboard::writeSelection(NSPasteboard* pasteboard, NSArray* pasteboardTyp
         Pasteboard::generalPasteboard(); // Initializes pasteboard types.
     ASSERT(selectedRange);
     
+    // If the selection is at the beginning of content inside an anchor tag
+    // we move the selection start to include the anchor.
+    // This way the attributed string will contain the url attribute as well.
+    // See <rdar://problem/9084267>.
+    ExceptionCode ec;
+    Node* commonAncestor = selectedRange->commonAncestorContainer(ec);
+    ASSERT(commonAncestor);
+    Node* enclosingAnchor = enclosingNodeWithTag(firstPositionInNode(commonAncestor), HTMLNames::aTag);
+    if (enclosingAnchor && comparePositions(firstPositionInOrBeforeNode(selectedRange->startPosition().anchorNode()), selectedRange->startPosition()) >= 0)
+        selectedRange->setStart(enclosingAnchor, 0, ec);
+
     // Using different API for WebKit and WebKit2.
     NSAttributedString *attributedString = nil;
     if (frame->view()->platformWidget())
