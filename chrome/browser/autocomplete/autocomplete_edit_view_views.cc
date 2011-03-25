fed2165a01d539925fd52e44dd5e07fd93b7c5e1
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 20:26:46 +0000

    Increasing toolbar for touch case, assets are not final
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6730039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit_view_views.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_views.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accessibility/accessible_view_state.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/font.h"
 #include "views/border.h"
 #include "views/controls/textfield/textfield.h"
@@ -148,6 +149,11 @@ void AutocompleteEditViewViews::Init() {
   // TODO(oshima): make sure the above happens with views.
   textfield_ = new AutocompleteTextfield(this);
   textfield_->SetController(this);
+
+#if defined(TOUCH_UI)
+  textfield_->SetFont(ui::ResourceBundle::GetSharedInstance().GetFont(
+                      ResourceBundle::LargeFont));
+#endif
 
   if (popup_window_mode_)
     textfield_->SetReadOnly(true);
