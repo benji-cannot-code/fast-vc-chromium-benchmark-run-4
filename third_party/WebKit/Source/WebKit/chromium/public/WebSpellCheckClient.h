043962c2ab4d5baa301414736893ff38537a3eef
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-26 00:14:02 +0000

    [chromium] add missing include from previous checkin
    git-svn-id: svn://svn.chromium.org/blink/trunk@82008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebSpellCheckClient.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebSpellCheckClient.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebSpellCheckClient_h
 #define WebSpellCheckClient_h
 
+#include "WebString.h"
+
 namespace WebKit {
 
 class WebString;
