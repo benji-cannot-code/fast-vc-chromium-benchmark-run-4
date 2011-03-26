1f78d65357d234559a423f5a0bd0ea19ec8b8111
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-26 11:08:50 +0000

    FTP: Detect the character encoding only after the entire listing is received.
    Otherwise the parser could be confused extremely easily. Imagine a listing
    which has only ASCII characters at the beginning, but after we detect it
    as ASCII-encoded, some UTF-8 bytes appear. It can be made more complex though,
    for example the beginning might look like UTF-8, but the entire listing
    may use a different encoding incompatible with UTF-8 (that was the case here,
    see bug).
    
    This change also simplifies the directory listing parser. When we always
    have the entire listing when parsing, some complex code can be removed.
    
    BUG=76171
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6670085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79490 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_unittest.cc
+++ net/ftp/ftp_directory_listing_parser_unittest.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/ftp/ftp_directory_listing_buffer.h"
+#include "net/ftp/ftp_directory_listing_parser.h"
 
 #include "base/file_util.h"
 #include "base/format_macros.h"
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/ftp/ftp_directory_listing_parser.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+namespace net {
 
 namespace {
 
@@ -74,15 +76,14 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
   for (size_t i = 0; i < arraysize(test_files); i++) {
     SCOPED_TRACE(base::StringPrintf("Test[%" PRIuS "]: %s", i, test_files[i]));
 
-    net::FtpDirectoryListingBuffer buffer(mock_current_time);
-
     std::string test_listing;
     EXPECT_TRUE(file_util::ReadFileToString(test_dir.AppendASCII(test_files[i]),
                                             &test_listing));
 
-    EXPECT_EQ(net::OK, buffer.ConsumeData(test_listing.data(),
-                                          test_listing.length()));
-    EXPECT_EQ(net::OK, buffer.ProcessRemainingData());
+    std::vector<FtpDirectoryListingEntry> entries;
+    EXPECT_EQ(OK, ParseFtpDirectoryListing(test_listing,
+                                           mock_current_time,
+                                           &entries));
 
     std::string expected_listing;
     ASSERT_TRUE(file_util::ReadFileToString(
@@ -93,7 +94,8 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
     StringTokenizer tokenizer(expected_listing, "\r\n");
     while (tokenizer.GetNext())
       lines.push_back(tokenizer.token());
-    ASSERT_EQ(0U, lines.size() % 8);
+
+    ASSERT_EQ(8 * entries.size(), lines.size());
 
     for (size_t i = 0; i < lines.size() / 8; i++) {
       std::string type(lines[8 * i]);
@@ -110,15 +112,14 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
       base::StringToInt(lines[8 * i + 6], &hour);
       base::StringToInt(lines[8 * i + 7], &minute);
 
-      ASSERT_TRUE(buffer.EntryAvailable());
-      net::FtpDirectoryListingEntry entry = buffer.PopEntry();
+      const FtpDirectoryListingEntry& entry = entries[i];
 
       if (type == "d") {
-        EXPECT_EQ(net::FtpDirectoryListingEntry::DIRECTORY, entry.type);
+        EXPECT_EQ(FtpDirectoryListingEntry::DIRECTORY, entry.type);
       } else if (type == "-") {
-        EXPECT_EQ(net::FtpDirectoryListingEntry::FILE, entry.type);
+        EXPECT_EQ(FtpDirectoryListingEntry::FILE, entry.type);
       } else if (type == "l") {
-        EXPECT_EQ(net::FtpDirectoryListingEntry::SYMLINK, entry.type);
+        EXPECT_EQ(FtpDirectoryListingEntry::SYMLINK, entry.type);
       } else {
         ADD_FAILURE() << "invalid gold test data: " << type;
       }
@@ -134,8 +135,9 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
       EXPECT_EQ(hour, time_exploded.hour);
       EXPECT_EQ(minute, time_exploded.minute);
     }
-    EXPECT_FALSE(buffer.EntryAvailable());
   }
 }
 
 }  // namespace
+
+}  // namespace net
--- net/ftp/ftp_directory_listing_parser_unittest.h
+++ net/ftp/ftp_directory_listing_parser_unittest.h
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef NET_FTP_FTP_DIRECTORY_LISTING_PARSER_UNITTEST_H_
 #define NET_FTP_FTP_DIRECTORY_LISTING_PARSER_UNITTEST_H_
 #pragma once
+
+#include <vector>
 
 #include "base/utf_string_conversions.h"
 #include "net/ftp/ftp_directory_listing_parser.h"
@@ -30,11 +32,18 @@ class FtpDirectoryListingParserTest : public testing::Test {
  protected:
   FtpDirectoryListingParserTest() {}
 
-  void RunSingleLineTestCase(FtpDirectoryListingParser* parser,
-                             const SingleLineTestData& test_case) {
-    ASSERT_TRUE(parser->ConsumeLine(UTF8ToUTF16(test_case.input)));
-    ASSERT_TRUE(parser->EntryAvailable());
-    FtpDirectoryListingEntry entry = parser->PopEntry();
+  std::vector<string16> GetSingleLineTestCase(const std::string& text) {
+    std::vector<string16> lines;
+    lines.push_back(UTF8ToUTF16(text));
+    return lines;
+  }
+
+  void VerifySingleLineTestCase(
+      const SingleLineTestData& test_case,
+      const std::vector<FtpDirectoryListingEntry>& entries) {
+    ASSERT_FALSE(entries.empty());
+
+    FtpDirectoryListingEntry entry = entries[0];
     EXPECT_EQ(test_case.type, entry.type);
     EXPECT_EQ(UTF8ToUTF16(test_case.filename), entry.name);
     EXPECT_EQ(test_case.size, entry.size);
@@ -48,6 +57,8 @@ class FtpDirectoryListingParserTest : public testing::Test {
     EXPECT_EQ(test_case.day_of_month, time_exploded.day_of_month);
     EXPECT_EQ(test_case.hour, time_exploded.hour);
     EXPECT_EQ(test_case.minute, time_exploded.minute);
+
+    EXPECT_EQ(1U, entries.size());
   }
 
   base::Time GetMockCurrentTime() {
@@ -59,9 +70,6 @@ class FtpDirectoryListingParserTest : public testing::Test {
     mock_current_time_exploded.minute = 45;
     return base::Time::FromLocalExploded(mock_current_time_exploded);
   }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(FtpDirectoryListingParserTest);
 };
 
 }  // namespace net
