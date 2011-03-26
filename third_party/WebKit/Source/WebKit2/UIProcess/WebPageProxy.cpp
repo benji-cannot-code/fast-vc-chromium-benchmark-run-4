48cf102ca53138dd214ad70f4da86b65167cb36e
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-26 21:18:07 +0000

    2011-03-26  Sam Weinig  <sam@webkit.org>
            Reviewed by Anders Carlsson.
    
            Web process considered unresponsive (SPOD shown) when displaying a JavaScript alert() as a result of a click
            <rdar://problem/9067557>
            https://bugs.webkit.org/show_bug.cgi?id=57166
    
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::runJavaScriptAlert):
            (WebKit::WebPageProxy::runJavaScriptConfirm):
            (WebKit::WebPageProxy::runJavaScriptPrompt):
            Invalidate the responsiveness timer before calling out to the client.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -1767,6 +1767,9 @@ void WebPageProxy::runJavaScriptAlert(uint64_t frameID, const String& message)
     WebFrameProxy* frame = process()->webFrame(frameID);
     MESSAGE_CHECK(frame);
 
+    // Since runJavaScriptAlert() can spin a nested run loop we need to turn off the responsiveness timer.
+    process()->responsivenessTimer()->stop();
+
     m_uiClient.runJavaScriptAlert(this, message, frame);
 }
 
@@ -1775,6 +1778,9 @@ void WebPageProxy::runJavaScriptConfirm(uint64_t frameID, const String& message,
     WebFrameProxy* frame = process()->webFrame(frameID);
     MESSAGE_CHECK(frame);
 
+    // Since runJavaScriptConfirm() can spin a nested run loop we need to turn off the responsiveness timer.
+    process()->responsivenessTimer()->stop();
+
     result = m_uiClient.runJavaScriptConfirm(this, message, frame);
 }
 
@@ -1782,6 +1788,9 @@ void WebPageProxy::runJavaScriptPrompt(uint64_t frameID, const String& message, 
 {
     WebFrameProxy* frame = process()->webFrame(frameID);
     MESSAGE_CHECK(frame);
+
+    // Since runJavaScriptPrompt() can spin a nested run loop we need to turn off the responsiveness timer.
+    process()->responsivenessTimer()->stop();
 
     result = m_uiClient.runJavaScriptPrompt(this, message, defaultValue, frame);
 }
