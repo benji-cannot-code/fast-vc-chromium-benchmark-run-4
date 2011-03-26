471039b5217183e2d5384d870db047e758b52741
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-26 22:24:19 +0000

    Workground for glGenerateMipmap bug in some drivers.
    TEST=unit tests
    BUG=72601
    
    R=zmo@chromium.org
    
    Review URL: http://codereview.chromium.org/6749014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79499 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_1.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_1.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,11 +35,21 @@ class GLES2DecoderTest1 : public GLES2DecoderTestBase {
 
 template <>
 void GLES2DecoderTestBase::SpecializedSetup<GenerateMipmap, 0>(
-    bool /* valid */) {
+    bool valid) {
   DoBindTexture(GL_TEXTURE_2D, client_texture_id_, kServiceTextureId);
   DoTexImage2D(
       GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE,
       0, 0);
+  if (valid) {
+    EXPECT_CALL(*gl_, TexParameteri(
+        GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR))
+        .Times(1)
+        .RetiresOnSaturation();
+    EXPECT_CALL(*gl_, TexParameteri(
+        GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR))
+        .Times(1)
+        .RetiresOnSaturation();
+  }
 };
 
 template <>
