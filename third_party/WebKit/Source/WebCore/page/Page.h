6a559086b7b47ef37b9e3ccb207fdd0bd828658a
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 23:15:20 +0000

    Fix for https://bugs.webkit.org/show_bug.cgi?id=57124 When the scroller style is changed via delegate method, the page needs a full relayout and repaint -and corresponding- <rdar://problem/9059129>
    Reviewed by Darin Adler.
    
    Call into WebKitSystemInterface to associate the new painter with the existing 
    painter controller. Reset the scrollbar frame rects to the new thickness -- normally 
    this only happens when a scrollbar is created, so we have to reset the thickness 
    here to pick up the new theme thickness. Finally, force a full relayout and style 
    recall with setNeedsRecalcStyleInAllFrames()
    * platform/mac/ScrollAnimatorMac.mm:
    (-[ScrollbarPainterControllerDelegate scrollerImpPair:updateScrollerStyleForNewRecommendedScrollerStyle:]):
    
    setNeedsRecalcStyleInAllFrames() used to be a static method in Settings.cpp. This 
    patch moves it to be a member function on Page so that it can be called from 
    FrameView when the scrollbar style changes.
    * page/FrameView.cpp:
    (WebCore::FrameView::setNeedsRecalcStyleInAllFrames):
    * page/FrameView.h:
    * page/Page.cpp:
    (WebCore::Page::setNeedsRecalcStyleInAllFrames):
    * page/Page.h:
    * page/Settings.cpp:
    (WebCore::Settings::setStandardFontFamily):
    (WebCore::Settings::setFixedFontFamily):
    (WebCore::Settings::setSerifFontFamily):
    (WebCore::Settings::setSansSerifFontFamily):
    (WebCore::Settings::setCursiveFontFamily):
    (WebCore::Settings::setFantasyFontFamily):
    (WebCore::Settings::setMinimumFontSize):
    (WebCore::Settings::setMinimumLogicalFontSize):
    (WebCore::Settings::setDefaultFontSize):
    (WebCore::Settings::setDefaultFixedFontSize):
    (WebCore::Settings::setTextAreasAreResizable):
    (WebCore::Settings::setAuthorAndUserStylesEnabled):
    (WebCore::Settings::setFontRenderingMode):
    (WebCore::Settings::setAcceleratedCompositingEnabled):
    (WebCore::Settings::setShowDebugBorders):
    (WebCore::Settings::setShowRepaintCounter):
    * platform/ScrollableArea.h:
    (WebCore::ScrollableArea::setNeedsRecalcStyleInAllFrames):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Page.h
+++ third_party/WebKit/Source/WebCore/page/Page.h
@@ -120,6 +120,8 @@ namespace WebCore {
         Page(const PageClients&);
         ~Page();
 
+        void setNeedsRecalcStyleInAllFrames();
+
         RenderTheme* theme() const { return m_theme.get(); };
 
         ViewportArguments viewportArguments() const { return m_viewportArguments; }
