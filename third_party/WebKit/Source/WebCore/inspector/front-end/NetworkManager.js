07e70f1d8fa1dd65148eaf79717ecf89ed5295f2
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 13:44:10 +0000

    2011-03-28  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: rename network domain events from didHappenSomething to somethingHappened.
            https://bugs.webkit.org/show_bug.cgi?id=57226
    
            * inspector/Inspector.json:
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::willSendRequest):
            (WebCore::InspectorResourceAgent::markResourceAsCached):
            (WebCore::InspectorResourceAgent::didReceiveResponse):
            (WebCore::InspectorResourceAgent::didReceiveContentLength):
            (WebCore::InspectorResourceAgent::didFinishLoading):
            (WebCore::InspectorResourceAgent::didFailLoading):
            (WebCore::InspectorResourceAgent::didLoadResourceFromMemoryCache):
            (WebCore::InspectorResourceAgent::setInitialContent):
            (WebCore::InspectorResourceAgent::didCommitLoad):
            (WebCore::InspectorResourceAgent::frameDetachedFromParent):
            (WebCore::InspectorResourceAgent::didCreateWebSocket):
            (WebCore::InspectorResourceAgent::willSendWebSocketHandshakeRequest):
            (WebCore::InspectorResourceAgent::didReceiveWebSocketHandshakeResponse):
            (WebCore::InspectorResourceAgent::didCloseWebSocket):
            * inspector/front-end/NetworkManager.js:
            (WebInspector.NetworkDispatcher.prototype.requestWillBeSent):
            (WebInspector.NetworkDispatcher.prototype.resourceMarkedAsCached):
            (WebInspector.NetworkDispatcher.prototype.responseReceived):
            (WebInspector.NetworkDispatcher.prototype.dataReceived):
            (WebInspector.NetworkDispatcher.prototype.loadingFinished):
            (WebInspector.NetworkDispatcher.prototype.loadingFailed):
            (WebInspector.NetworkDispatcher.prototype.resourceLoadedFromMemoryCache):
            (WebInspector.NetworkDispatcher.prototype.frameDetached):
            (WebInspector.NetworkDispatcher.prototype.initialContentSet):
            (WebInspector.NetworkDispatcher.prototype.frameNavigated):
            (WebInspector.NetworkDispatcher.prototype.webSocketCreated):
            (WebInspector.NetworkDispatcher.prototype.webSocketWillSendHandshakeRequest):
            (WebInspector.NetworkDispatcher.prototype.webSocketHandshakeResponseReceived):
            (WebInspector.NetworkDispatcher.prototype.webSocketClosed):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
@@ -113,11 +113,11 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResourceWithResponse(resource, cachedResource.response);
     },
 
-    willSendRequest: function(identifier, frameId, loaderId, documentURL, request, redirectResponse, time, callStack)
+    requestWillBeSent: function(identifier, frameId, loaderId, documentURL, request, redirectResponse, time, callStack)
     {
         var resource = this._inflightResourcesById[identifier];
         if (resource) {
-            this.didReceiveResponse(identifier, time, "Other", redirectResponse);
+            this.responseReceived(identifier, time, "Other", redirectResponse);
             resource = this._appendRedirect(identifier, time, request.url);
         } else
             resource = this._createResource(identifier, frameId, loaderId, request.url, documentURL, callStack);
@@ -127,7 +127,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._startResource(resource);
     },
 
-    markResourceAsCached: function(identifier)
+    resourceMarkedAsCached: function(identifier)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -137,7 +137,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didReceiveResponse: function(identifier, time, resourceType, response)
+    responseReceived: function(identifier, time, resourceType, response)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -151,7 +151,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didReceiveContentLength: function(identifier, time, dataLength, lengthReceived)
+    dataReceived: function(identifier, time, dataLength, lengthReceived)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -165,7 +165,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didFinishLoading: function(identifier, finishTime)
+    loadingFinished: function(identifier, finishTime)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -174,7 +174,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._finishResource(resource, finishTime);
     },
 
-    didFailLoading: function(identifier, time, localizedDescription)
+    loadingFailed: function(identifier, time, localizedDescription)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -185,7 +185,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._finishResource(resource, time);
     },
 
-    didLoadResourceFromMemoryCache: function(frameId, loaderId, documentURL, time, cachedResource)
+    resourceLoadedFromMemoryCache: function(frameId, loaderId, documentURL, time, cachedResource)
     {
         var resource = this._createResource("cached:" + ++this._lastIdentifierForCachedResource, frameId, loaderId, cachedResource.url, documentURL);
         this._updateResourceWithCachedResource(resource, cachedResource);
@@ -196,12 +196,12 @@ WebInspector.NetworkDispatcher.prototype = {
         this._finishResource(resource, time);
     },
 
-    frameDetachedFromParent: function(frameId)
+    frameDetached: function(frameId)
     {
         this._dispatchEventToListeners(WebInspector.NetworkManager.EventTypes.FrameDetached, frameId);
     },
 
-    setInitialContent: function(identifier, sourceString, type)
+    initialContentSet: function(identifier, sourceString, type)
     {
         var resource = WebInspector.networkResourceById(identifier);
         if (!resource)
@@ -212,19 +212,19 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didCommitLoadForFrame: function(frame, loaderId)
+    frameNavigated: function(frame, loaderId)
     {
         this._dispatchEventToListeners(WebInspector.NetworkManager.EventTypes.FrameCommittedLoad, { frame: frame, loaderId: loaderId });
     },
 
-    didCreateWebSocket: function(identifier, requestURL)
+    webSocketCreated: function(identifier, requestURL)
     {
         var resource = this._createResource(identifier, null, null, requestURL);
         resource.type = WebInspector.Resource.Type.WebSocket;
         this._startResource(resource);
     },
 
-    willSendWebSocketHandshakeRequest: function(identifier, time, request)
+    webSocketWillSendHandshakeRequest: function(identifier, time, request)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -238,7 +238,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didReceiveWebSocketHandshakeResponse: function(identifier, time, response)
+    webSocketHandshakeResponseReceived: function(identifier, time, response)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
@@ -253,7 +253,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResource(resource);
     },
 
-    didCloseWebSocket: function(identifier, time)
+    webSocketClosed: function(identifier, time)
     {
         var resource = this._inflightResourcesById[identifier];
         if (!resource)
