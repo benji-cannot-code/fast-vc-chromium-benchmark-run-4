2329f7334326d3004b7405d2012c006d0fde3765
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 10:45:50 +0000

    Fix a few uninitialized members in remoting classes.
    BUG=None
    TEST=None
    CID=15507, 13722, 13617, 13913, 13614.
    Review URL: http://codereview.chromium.org/6696106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79545 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/rtp_utils.cc
+++ remoting/protocol/rtp_utils.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -37,6 +37,7 @@ RtpHeader::RtpHeader()
       sequence_number(0),
       timestamp(0),
       sync_source_id(0) {
+  memset(source_id, 0, sizeof(source_id));
 }
 
 RtcpReceiverReport::RtcpReceiverReport()
--- remoting/protocol/rtp_video_reader.cc
+++ remoting/protocol/rtp_video_reader.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,7 +23,8 @@ RtpVideoReader::PacketsQueueEntry::PacketsQueueEntry()
 }
 
 RtpVideoReader::RtpVideoReader()
-    : last_sequence_number_(0) {
+    : last_sequence_number_(0),
+      video_stub_(NULL) {
 }
 
 RtpVideoReader::~RtpVideoReader() {
