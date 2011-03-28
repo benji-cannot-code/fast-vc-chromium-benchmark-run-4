e10b1bb6850ccb834dc6ca962333dd77a3cff0d5
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 15:11:26 +0000

    Switch from using Speex to FLAC for speech input requests.
    BUG=61677
    TEST=manual, verify that the audio received by server is in FLAC format.
    
    Review URL: http://codereview.chromium.org/6719033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79557 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/speech/speech_recognizer.cc
+++ content/browser/speech/speech_recognizer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -67,7 +67,7 @@ SpeechRecognizer::SpeechRecognizer(Delegate* delegate,
       grammar_(grammar),
       hardware_info_(hardware_info),
       origin_url_(origin_url),
-      codec_(AudioEncoder::CODEC_SPEEX),
+      codec_(AudioEncoder::CODEC_FLAC),
       encoder_(NULL),
       endpointer_(kAudioSampleRate),
       num_samples_recorded_(0),
