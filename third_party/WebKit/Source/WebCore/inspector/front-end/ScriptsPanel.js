f6ba462ca61915405119042799e11ec0397a0e75
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 17:16:52 +0000

    2011-03-28  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: brush up and rename debugger domain functions.
            https://bugs.webkit.org/show_bug.cgi?id=57240
    
            * inspector/Inspector.json:
            * inspector/InspectorAgent.cpp:
            (WebCore::InspectorAgent::setFrontend):
            (WebCore::InspectorAgent::postWorkerNotificationToFrontend):
            * inspector/InspectorDebuggerAgent.cpp:
            (WebCore::InspectorDebuggerAgent::setBreakpointsActive):
            (WebCore::InspectorDebuggerAgent::setBreakpointByUrl):
            (WebCore::InspectorDebuggerAgent::setBreakpoint):
            (WebCore::InspectorDebuggerAgent::removeBreakpoint):
            (WebCore::InspectorDebuggerAgent::editScriptSource):
            (WebCore::InspectorDebuggerAgent::setPauseOnExceptionsState):
            (WebCore::InspectorDebuggerAgent::didParseSource):
            (WebCore::InspectorDebuggerAgent::failedToParseSource):
            (WebCore::InspectorDebuggerAgent::didPause):
            (WebCore::InspectorDebuggerAgent::didContinue):
            * inspector/InspectorDebuggerAgent.h:
            * inspector/front-end/DebuggerModel.js:
            (WebInspector.DebuggerModel.prototype.setBreakpoint):
            (WebInspector.DebuggerModel.prototype.setBreakpointBySourceId):
            (WebInspector.DebuggerModel.prototype.removeBreakpoint):
            (WebInspector.DebuggerModel.prototype._didEditScriptSource):
            (WebInspector.DebuggerDispatcher.prototype.paused):
            (WebInspector.DebuggerDispatcher.prototype.resumed):
            (WebInspector.DebuggerDispatcher.prototype.scriptParsed):
            (WebInspector.DebuggerDispatcher.prototype.scriptFailedToParse):
            (WebInspector.DebuggerDispatcher.prototype.breakpointResolved):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype.toggleBreakpointsClicked):
            * inspector/front-end/inspector.js:
            (WebInspector.didCreateWorker):
            (WebInspector.didDestroyWorker):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -684,7 +684,7 @@ WebInspector.ScriptsPanel.prototype = {
 
     _setPauseOnExceptions: function(pauseOnExceptionsState)
     {
-        function callback(error, pauseOnExceptionsState)
+        function callback(error)
         {
             if (error)
                 return;
@@ -865,11 +865,11 @@ WebInspector.ScriptsPanel.prototype = {
     {
         this.toggleBreakpointsButton.toggled = !this.toggleBreakpointsButton.toggled;
         if (this.toggleBreakpointsButton.toggled) {
-            DebuggerAgent.activateBreakpoints();
+            DebuggerAgent.setBreakpointsActive(true);
             this.toggleBreakpointsButton.title = WebInspector.UIString("Deactivate all breakpoints.");
             document.getElementById("main-panels").removeStyleClass("breakpoints-deactivated");
         } else {
-            DebuggerAgent.deactivateBreakpoints();
+            DebuggerAgent.setBreakpointsActive(false);
             this.toggleBreakpointsButton.title = WebInspector.UIString("Activate all breakpoints.");
             document.getElementById("main-panels").addStyleClass("breakpoints-deactivated");
         }
