6dd1837d1d2c5c1cc1300c1ffe5f43a52f4e2fc0
adele@apple.com <adele@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 22:22:33 +0000

    Build fix.
    Source/WebKit/gtk: 
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::webkitWebFrameSelectionHasSpellingMarker):
    
    Tools: 
    
    * DumpRenderTree/win/LayoutTestControllerWin.cpp:
    (LayoutTestController::hasGrammarMarker):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
@@ -650,7 +650,7 @@ bool DumpRenderTreeSupportGtk::webkitWebFrameSelectionHasSpellingMarker(WebKitWe
 {
     g_return_val_if_fail(WEBKIT_IS_WEB_FRAME(frame), FALSE);
 
-    return core(frame)->editor()->selectionStartHasSpellingMarkerFor(from, length);
+    return core(frame)->editor()->selectionStartHasMarkerFor(DocumentMarker::Spelling, from, length);
 }
 
 bool DumpRenderTreeSupportGtk::findString(WebKitWebView* webView, const gchar* targetString, WebKitFindOptions findOptions)
