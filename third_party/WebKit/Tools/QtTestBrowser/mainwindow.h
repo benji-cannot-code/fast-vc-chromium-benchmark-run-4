9ff542ac37bf46449b19138103acd901a4065685
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 12:41:10 +0000

    2011-03-28  Andreas Kling  <kling@webkit.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] QtTestBrowser doesn't reset its window title when opening about:blank
            https://bugs.webkit.org/show_bug.cgi?id=57224
    
            * QtTestBrowser/mainwindow.cpp:
            (MainWindow::buildUI):
            (MainWindow::onTitleChanged):
            * QtTestBrowser/mainwindow.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/QtTestBrowser/mainwindow.h
+++ third_party/WebKit/Tools/QtTestBrowser/mainwindow.h
@@ -61,6 +61,7 @@ protected slots:
     void changeLocation();
     void onIconChanged();
     void onLoadStarted();
+    void onTitleChanged(const QString&);
 
 protected:
     QString addressUrl() const;
