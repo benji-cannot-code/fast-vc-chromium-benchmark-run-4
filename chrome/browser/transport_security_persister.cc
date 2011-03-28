e83842d1c0fe8c659b3ad19396d9e0dbe73b61eb
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 20:04:00 +0000

    Honor pre-existing HSTS metadata in Incognito mode, but obviously continue to never persist any metadata to disk. See the bug for a detailed discussion of the tradeoffs.
    BUG=77436
    TEST=see bug
    Review URL: http://codereview.chromium.org/6746003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/transport_security_persister.cc
+++ chrome/browser/transport_security_persister.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_thread.h"
 #include "net/base/transport_security_state.h"
 
-TransportSecurityPersister::TransportSecurityPersister()
-  : ALLOW_THIS_IN_INITIALIZER_LIST(save_coalescer_(this)) {
+TransportSecurityPersister::TransportSecurityPersister(bool readonly)
+  : ALLOW_THIS_IN_INITIALIZER_LIST(save_coalescer_(this)),
+    readonly_(readonly) {
 }
 
 TransportSecurityPersister::~TransportSecurityPersister() {
@@ -64,6 +65,9 @@ void TransportSecurityPersister::StateIsDirty(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   DCHECK(state == transport_security_state_);
 
+  if (readonly_)
+    return;
+
   if (!save_coalescer_.empty())
     return;
 
@@ -87,6 +91,7 @@ void TransportSecurityPersister::Save() {
 
 void TransportSecurityPersister::CompleteSave(const std::string& state) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
+  DCHECK(!readonly_);
 
   file_util::WriteFile(state_file_, state.data(), state.size());
 }
--- chrome/browser/transport_security_persister.h
+++ chrome/browser/transport_security_persister.h
@@ -44,7 +44,7 @@ class TransportSecurityPersister
     : public base::RefCountedThreadSafe<TransportSecurityPersister>,
       public net::TransportSecurityState::Delegate {
  public:
-  TransportSecurityPersister();
+  explicit TransportSecurityPersister(bool readonly);
   void Initialize(net::TransportSecurityState* state,
                   const FilePath& profile_path);
 
@@ -70,6 +70,9 @@ class TransportSecurityPersister
 
   // The path to the file in which we store the serialised state.
   FilePath state_file_;
+
+  // Whether or not we're in read-only mode.
+  bool readonly_;
 };
 
 #endif  // CHROME_BROWSER_TRANSPORT_SECURITY_PERSISTER_H_
