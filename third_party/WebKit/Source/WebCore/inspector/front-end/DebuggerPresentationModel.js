6871e6662579b5ee5fb4985c57e2612f0a773b33
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-28 12:23:15 +0000

    2011-03-24  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: fix console messages positions in formatted scripts.
            https://bugs.webkit.org/show_bug.cgi?id=57010
    
            * inspector/front-end/DebuggerPresentationModel.js:
            (WebInspector.DebuggerPresentationModel):
            (WebInspector.DebuggerPresentationModel.prototype.toggleFormatSourceFiles):
            (WebInspector.DebuggerPresentationModel.prototype.addConsoleMessage.didRequestMapping):
            (WebInspector.DebuggerPresentationModel.prototype.addConsoleMessage):
            (WebInspector.DebuggerPresentationModel.prototype.clearConsoleMessages):
            (WebInspector.DebuggerPresentationModel.prototype._reset):
            * inspector/front-end/ResourceTreeModel.js:
            (WebInspector.ResourceTreeModel.prototype.addConsoleMessage):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel):
            (WebInspector.ScriptsPanel.prototype.addConsoleMessage):
            (WebInspector.ScriptsPanel.prototype.clearConsoleMessages):
            (WebInspector.ScriptsPanel.prototype._consoleMessageAdded):
            (WebInspector.ScriptsPanel.prototype.reset):
            (WebInspector.ScriptsPanel.prototype._sourceFrameLoaded):
            * inspector/front-end/SourceFile.js:
            (WebInspector.SourceFile):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype.addMessage):
            (WebInspector.SourceFrame.prototype._addExistingMessagesToSource):
            (WebInspector.SourceFrame.prototype.addMessageToSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerPresentationModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerPresentationModel.js
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.DebuggerPresentationModel = function()
 {
     this._sourceFiles = {};
+    this._messages = [];
     this._presentationBreakpoints = {};
 
     WebInspector.debuggerModel.addEventListener(WebInspector.DebuggerModel.Events.DebuggerWasEnabled, this._debuggerWasEnabled, this);
@@ -44,6 +45,7 @@ WebInspector.DebuggerPresentationModel = function()
 WebInspector.DebuggerPresentationModel.Events = {
     SourceFileAdded: "source-file-added",
     SourceFileChanged: "source-file-changed",
+    ConsoleMessageAdded: "console-message-added",
     BreakpointAdded: "breakpoint-added",
     BreakpointRemoved: "breakpoint-removed"
 }
@@ -176,12 +178,45 @@ WebInspector.DebuggerPresentationModel.prototype = {
         if (this._formatSourceFiles && !this._formatter)
             this._formatter = new WebInspector.ScriptFormatter();
 
+        var messages = this._messages;
+
         this._reset();
+
         var scripts = WebInspector.debuggerModel.scripts;
         for (var id in scripts)
             this._addScript(scripts[id]);
 
+        for (var i = 0; i < messages.length; ++i)
+            this.addConsoleMessage(messages[i]);
+
         this._refreshBreakpoints();
+    },
+
+    addConsoleMessage: function(message)
+    {
+        this._messages.push(message);
+
+        var sourceFile = this._sourceFileForScriptURL(message.url);
+        if (!sourceFile)
+            return;
+
+        function didRequestSourceMapping(mapping)
+        {
+            var presentationMessage = {};
+            presentationMessage.sourceFileId = sourceFile.id;
+            presentationMessage.lineNumber = mapping.scriptLocationToSourceLocation(message.line - 1, 0).lineNumber;
+            presentationMessage.originalMessage = message;
+            sourceFile.messages.push(presentationMessage);
+            this.dispatchEventToListeners(WebInspector.DebuggerPresentationModel.Events.ConsoleMessageAdded, presentationMessage);
+        }
+        sourceFile.requestSourceMapping(didRequestSourceMapping.bind(this));
+    },
+
+    clearConsoleMessages: function()
+    {
+        this._messages = [];
+        for (var id in this._sourceFiles)
+            this._sourceFiles[id].messages = [];
     },
 
     continueToLine: function(sourceFileId, lineNumber)
@@ -366,6 +401,11 @@ WebInspector.DebuggerPresentationModel.prototype = {
         return this._sourceFiles[script.sourceURL || script.sourceID];
     },
 
+    _sourceFileForScriptURL: function(scriptURL)
+    {
+        return this._sourceFiles[scriptURL];
+    },
+
     _scriptForSourceFileId: function(sourceFileId)
     {
         function filter(script)
@@ -378,6 +418,7 @@ WebInspector.DebuggerPresentationModel.prototype = {
     _reset: function()
     {
         this._sourceFiles = {};
+        this._messages = [];
         this._presentationBreakpoints = {};
     }
 }
