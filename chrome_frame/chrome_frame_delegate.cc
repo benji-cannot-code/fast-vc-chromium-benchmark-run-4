60fdb3bdb0de4ad32af66c6adb2f823be214339f
joshia@google.com <joshia@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 20:38:24 +0000

    Chrome frame support for resizing popup windows
    Add necesssary plumbing for window.resizeTo to work within
    Chrome Frame.  
    
    BUG=76649
    TEST=create a popup window using window.open and resize it
    using window.resizeTo.  
    Sample test page is here: http://testopenpopup.comuf.com/popUp.html
    Review URL: http://codereview.chromium.org/6748010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79737 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_delegate.cc
+++ chrome_frame/chrome_frame_delegate.cc
@@ -18,6 +18,7 @@ bool ChromeFrameDelegateImpl::IsTabMessage(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER_GENERIC(AutomationMsg_NavigationFailed, )
     IPC_MESSAGE_HANDLER_GENERIC(AutomationMsg_DidNavigate, )
     IPC_MESSAGE_HANDLER_GENERIC(AutomationMsg_TabLoaded, )
+    IPC_MESSAGE_HANDLER_GENERIC(AutomationMsg_MoveWindow, )
     IPC_MESSAGE_HANDLER_GENERIC(AutomationMsg_ForwardMessageToExternalHost, )
     IPC_MESSAGE_HANDLER_GENERIC(
         AutomationMsg_ForwardContextMenuToExternalHost, )
@@ -55,6 +56,7 @@ bool ChromeFrameDelegateImpl::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER(AutomationMsg_NavigationFailed, OnNavigationFailed)
     IPC_MESSAGE_HANDLER(AutomationMsg_DidNavigate, OnDidNavigate)
     IPC_MESSAGE_HANDLER(AutomationMsg_TabLoaded, OnLoad)
+    IPC_MESSAGE_HANDLER(AutomationMsg_MoveWindow, OnMoveWindow)
     IPC_MESSAGE_HANDLER(AutomationMsg_ForwardMessageToExternalHost,
                         OnMessageFromChromeFrame)
     IPC_MESSAGE_HANDLER(AutomationMsg_ForwardContextMenuToExternalHost,
