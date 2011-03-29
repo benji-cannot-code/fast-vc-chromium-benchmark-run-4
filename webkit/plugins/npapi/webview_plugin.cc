80bc11432911dd5078195201712dc7630eae0c80
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 22:48:03 +0000

    Remove usages of WebView::Create that pass in devtools/autofill client. Review URL: http://codereview.chromium.org/6771025
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79759 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webview_plugin.cc
+++ webkit/plugins/npapi/webview_plugin.cc
@@ -51,7 +51,7 @@ WebViewPlugin::WebViewPlugin(WebViewPlugin::Delegate* delegate)
     : delegate_(delegate),
       container_(NULL),
       finished_loading_(false) {
-  web_view_ = WebView::create(this, NULL, NULL);
+  web_view_ = WebView::create(this);
   web_view_->initializeMainFrame(this);
 }
 
