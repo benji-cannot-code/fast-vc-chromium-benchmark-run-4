69e67fb7661df48ca779fffe28f24e5dd93f4c6b
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 19:59:21 +0000

    Makes instant delay showing any page that doens't result in a 403.
    BUG=72573
    TEST=see bug
    R=jcivelli@chromium.org
    
    Review URL: http://codereview.chromium.org/6683059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_loader.cc
+++ chrome/browser/instant/instant_loader.cc
@@ -614,6 +614,7 @@ InstantLoader::InstantLoader(InstantLoaderDelegate* delegate, TemplateURLID id)
     : delegate_(delegate),
       template_url_id_(id),
       ready_(false),
+      http_status_ok_(true),
       last_transition_type_(PageTransition::LINK),
       verbatim_(false),
       needs_reload_(false) {
@@ -627,7 +628,7 @@ InstantLoader::~InstantLoader() {
   preview_contents_.reset();
 }
 
-void InstantLoader::Update(TabContentsWrapper* tab_contents,
+bool InstantLoader::Update(TabContentsWrapper* tab_contents,
                            const TemplateURL* template_url,
                            const GURL& url,
                            PageTransition::Type transition_type,
@@ -661,7 +662,7 @@ void InstantLoader::Update(TabContentsWrapper* tab_contents,
     // when we get the suggest text we set user_text_ to the new suggest text,
     // but yet the url is much different.
     url_ = url;
-    return;
+    return false;
   }
 
   url_ = url;
@@ -682,7 +683,7 @@ void InstantLoader::Update(TabContentsWrapper* tab_contents,
         frame_load_observer_->set_text(user_text_);
         frame_load_observer_->set_verbatim(verbatim);
         preview_tab_contents_delegate_->set_user_typed_before_load();
-        return;
+        return true;
       }
       // TODO: support real cursor position.
       int text_length = static_cast<int>(user_text_.size());
@@ -732,6 +733,7 @@ void InstantLoader::Update(TabContentsWrapper* tab_contents,
     frame_load_observer_.reset(NULL);
     preview_contents_->controller().LoadURL(url_, GURL(), transition_type);
   }
+  return true;
 }
 
 void InstantLoader::SetOmniboxBounds(const gfx::Rect& bounds) {
@@ -877,10 +879,19 @@ void InstantLoader::PreviewPainted() {
     ShowPreview();
 }
 
+void InstantLoader::SetHTTPStatusOK(bool is_ok) {
+  if (is_ok == http_status_ok_)
+    return;
+
+  http_status_ok_ = is_ok;
+  if (ready_)
+    delegate_->InstantStatusChanged(this);
+}
+
 void InstantLoader::ShowPreview() {
   if (!ready_) {
     ready_ = true;
-    delegate_->ShowInstantLoader(this);
+    delegate_->InstantStatusChanged(this);
   }
 }
 
@@ -899,9 +910,12 @@ void InstantLoader::Observe(NotificationType type,
   if (type.value == NotificationType::NAV_ENTRY_COMMITTED) {
     NavigationController::LoadCommittedDetails* load_details =
         Details<NavigationController::LoadCommittedDetails>(details).ptr();
-    if (load_details->is_main_frame &&
-        load_details->http_status_code == kHostBlacklistStatusCode) {
-      delegate_->AddToBlacklist(this, load_details->entry->url());
+    if (load_details->is_main_frame) {
+      if (load_details->http_status_code == kHostBlacklistStatusCode) {
+        delegate_->AddToBlacklist(this, load_details->entry->url());
+      } else {
+        SetHTTPStatusOK(load_details->http_status_code == 200);
+      }
     }
     return;
   }
--- chrome/browser/instant/instant_loader_delegate.h
+++ chrome/browser/instant/instant_loader_delegate.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,8 +21,8 @@ class InstantLoader;
 // InstantLoader's delegate. This interface is implemented by InstantController.
 class InstantLoaderDelegate {
  public:
-  // Invoked when the loader is ready to be shown.
-  virtual void ShowInstantLoader(InstantLoader* loader) = 0;
+  // Invoked when the status (either http_status_ok or ready) has changed.
+  virtual void InstantStatusChanged(InstantLoader* loader) = 0;
 
   // Invoked when the loader has suggested text.
   virtual void SetSuggestedTextFor(
