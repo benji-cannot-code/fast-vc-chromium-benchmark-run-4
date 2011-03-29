cf5a6de6a0bd03b50dcb4799f9f0f22fa8a41e41
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 22:33:12 +0000

    Remove unnecessary member variable and forward declaration. Review URL: http://codereview.chromium.org/6748016
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/translate_helper.h
+++ chrome/renderer/translate_helper.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/translate_errors.h"
 #include "content/renderer/render_view_observer.h"
 
-class RenderView;
 namespace WebKit {
 class WebDocument;
 class WebFrame;
@@ -118,9 +117,6 @@ class TranslateHelper : public RenderViewObserver {
   // Convenience method to access the main frame.  Can return NULL, typically
   // if the page is being closed.
   WebKit::WebFrame* GetMainFrame();
-
-  // The RenderView we are performing translations for.
-  RenderView* render_view_;
 
   // The states associated with the current translation.
   bool translation_pending_;
