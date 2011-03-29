78dc778783de1bf86e78ea01cf9d71edb4debffe
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-29 22:50:37 +0000

    2011-03-29  Eric Seidel  <eric@webkit.org>
            Reviewed by Dimitri Glazkov.
    
            Rename BidiResolver::current to BidiResolver::m_current to match modern style
            https://bugs.webkit.org/show_bug.cgi?id=57363
    
            I was very confused by current until I realized it was a member variable.
            I also did m_reachedEndOfLine since that was small.
    
            * platform/text/BidiResolver.h:
            (WebCore::BidiResolver::position):
            (WebCore::BidiResolver::setPosition):
            (WebCore::BidiResolver::increment):
            (WebCore::::createBidiRunsForLine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/InlineIterator.h
+++ third_party/WebKit/Source/WebCore/rendering/InlineIterator.h
@@ -242,7 +242,7 @@ ALWAYS_INLINE WTF::Unicode::Direction InlineIterator::direction() const
 template<>
 inline void InlineBidiResolver::increment()
 {
-    current.increment(this);
+    m_current.increment(this);
 }
 
 template <>
@@ -259,7 +259,7 @@ inline void InlineBidiResolver::appendRun()
         if (obj) {
             unsigned pos = obj == eor.m_obj ? eor.m_pos : UINT_MAX;
             if (obj == endOfLine.m_obj && endOfLine.m_pos <= pos) {
-                reachedEndOfLine = true;
+                m_reachedEndOfLine = true;
                 pos = endOfLine.m_pos;
             }
             // It's OK to add runs for zero-length RenderObjects, just don't make the run larger than it should be
