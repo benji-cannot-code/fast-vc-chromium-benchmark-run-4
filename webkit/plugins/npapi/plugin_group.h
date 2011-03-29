c4d2d0a748da5048fcd89a7e4393a22b48c4ca46
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 20:44:48 +0000

    Report more detailed stats on which plug-ins are being blocked.
    BUG=77810
    Review URL: http://codereview.chromium.org/6708098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79740 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_group.h
+++ webkit/plugins/npapi/plugin_group.h
@@ -88,6 +88,8 @@ class PluginGroup {
   static const char* kJavaGroupName;
   static const char* kQuickTimeGroupName;
   static const char* kShockwaveGroupName;
+  static const char* kRealPlayerGroupName;
+  static const char* kSilverlightGroupName;
 
   PluginGroup(const PluginGroup& other);
 
