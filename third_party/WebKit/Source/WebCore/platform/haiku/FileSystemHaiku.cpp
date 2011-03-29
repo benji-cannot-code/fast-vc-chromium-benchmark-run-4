475e46fb45fcd0ef82c6a4c7f885db75e5645c60
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-29 05:29:03 +0000

    2011-03-28  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Darin Adler.
    
            Use String instead of CString as return value of openTemporaryFile
            https://bugs.webkit.org/show_bug.cgi?id=55332
    
            We usually store all paths as UTF-16. Do this for temporary files too.
    
            * WebCore.exp.in
            * platform/FileSystem.h:
            * platform/android/FileSystemAndroid.cpp:
            * platform/brew/FileSystemBrew.cpp:
            * platform/efl/FileSystemEfl.cpp:
            * platform/gtk/FileSystemGtk.cpp:
            * platform/haiku/FileSystemHaiku.cpp:
            * platform/mac/FileSystemMac.mm:
            * platform/qt/FileSystemQt.cpp:
            * platform/win/FileSystemWin.cpp:
            * platform/wince/FileSystemWinCE.cpp:
            * platform/wx/FileSystemWx.cpp:
            * plugins/PluginStream.cpp:
            (WebCore::PluginStream::destroyStream):
            * plugins/PluginStream.h:
    2011-03-28  Patrick Gansterer  <paroga@webkit.org>
    
            Reviewed by Darin Adler.
    
            Use String instead of CString as return value of openTemporaryFile
            https://bugs.webkit.org/show_bug.cgi?id=55332
    
            * Shared/WebMemorySampler.cpp:
            (WebKit::WebMemorySampler::initializeTempLogFile):
            * WebProcess/Plugins/Netscape/NetscapePluginStream.cpp:
            (WebKit::NetscapePluginStream::stop):
            * WebProcess/Plugins/Netscape/NetscapePluginStream.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/haiku/FileSystemHaiku.cpp
+++ third_party/WebKit/Source/WebCore/platform/haiku/FileSystemHaiku.cpp
@@ -57,11 +57,11 @@ String homeDirectoryPath()
     return String(path.Path());
 }
 
-CString openTemporaryFile(const char* prefix, PlatformFileHandle& handle)
+String openTemporaryFile(const String&, PlatformFileHandle& handle)
 {
     notImplemented();
     handle = invalidPlatformFileHandle;
-    return CString();
+    return String();
 }
 
 void closeFile(PlatformFileHandle&)
