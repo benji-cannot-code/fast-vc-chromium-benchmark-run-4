4ab8ca3f066f6abdcceb447cb548fd64a24ef18c
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-29 19:43:22 +0000

    WebKit2 bundle page needs to offer generated file hooks https://bugs.webkit.org/show_bug.cgi?id=57279
    Patch by Darin Adler <darin@apple.com> on 2011-03-29
    Reviewed by Adam Roben.
    
    (Land missing part of the original patch after r82289.)
    
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::InjectedBundlePage): Added new callbacks.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -214,6 +214,8 @@ InjectedBundlePage::InjectedBundlePage(WKBundlePageRef page)
         0, /*mouseDidMoveOverElement*/
         0, /*pageDidScroll*/
         0, /*paintCustomOverhangArea*/
+        0, /*shouldGenerateFileForUpload*/
+        0, /*generateFileForUpload*/
     };
     WKBundlePageSetUIClient(m_page, &uiClient);
 
