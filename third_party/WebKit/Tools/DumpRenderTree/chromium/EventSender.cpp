db4f189c31d996896b7f1224336be15baa8675ed
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-29 14:09:37 +0000

    2011-03-29  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Ojan Vafai.
    
            [chromium] DRT EventSender: remove identity from dragTargetDragEnter() calls
            https://bugs.webkit.org/show_bug.cgi?id=57303
    
            * DumpRenderTree/chromium/EventSender.cpp:
            (EventSender::doDragDrop):
            (EventSender::beginDragWithFiles):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82246 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/EventSender.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/EventSender.cpp
@@ -339,7 +339,7 @@ void EventSender::doDragDrop(const WebDragData& dragData, WebDragOperationsMask 
     WebPoint screenPoint(event.globalX, event.globalY);
     currentDragData = dragData;
     currentDragEffectsAllowed = mask;
-    currentDragEffect = webview()->dragTargetDragEnter(dragData, 0, clientPoint, screenPoint, currentDragEffectsAllowed);
+    currentDragEffect = webview()->dragTargetDragEnter(dragData, clientPoint, screenPoint, currentDragEffectsAllowed);
 
     // Finish processing events.
     replaySavedEvents();
@@ -827,7 +827,7 @@ void EventSender::beginDragWithFiles(const CppArgumentList& arguments, CppVarian
     currentDragEffectsAllowed = WebKit::WebDragOperationCopy;
 
     // Provide a drag source.
-    webview()->dragTargetDragEnter(currentDragData, 0, lastMousePos, lastMousePos, currentDragEffectsAllowed);
+    webview()->dragTargetDragEnter(currentDragData, lastMousePos, lastMousePos, currentDragEffectsAllowed);
 
     // dragMode saves events and then replays them later. We don't need/want that.
     dragMode.set(false);
