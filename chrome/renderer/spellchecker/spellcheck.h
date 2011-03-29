d71e22ae1d28e3fd244bed16dd4be6b808ed4c06
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 19:02:50 +0000

    Revert 79705 - Revert 79651 - Coverity: Pass parameters by reference.
    Also remove an unused function that I found.
    
    CID=2634,4471,5392,6433,6434,6535,6777,6780,6781,6792,6810,7665,7759,8056,8072,
    8637,8718,8726
    BUG=none
    TEST=non
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/6730046
    
    TBR=jhawkins@chromium.org
    Review URL: http://codereview.chromium.org/6773015
    
    TBR=dhollowa@chromium.org
    Review URL: http://codereview.chromium.org/6772013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/spellchecker/spellcheck.h
+++ chrome/renderer/spellchecker/spellcheck.h
@@ -33,7 +33,7 @@ class SpellCheck {
 
   void Init(base::PlatformFile file,
             const std::vector<std::string>& custom_words,
-            const std::string language);
+            const std::string& language);
 
   // SpellCheck a word.
   // Returns true if spelled correctly, false otherwise.
