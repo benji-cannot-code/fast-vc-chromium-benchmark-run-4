10cbd591513468ff39c90bc6e7151d81215c8386
rnorton10@gmail.com <rnorton10@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-29 00:07:57 +0000

    Make ExtensionResource::NormalizeSeperators pass by reference.
    Spawned off of a comment I made over a Coverity cl http://codereview.chromium.org/6730046/.
    
    Currently, there is ret_val which is simply unused - this changes the code in ExtensionResource::NormalizeSeperators to use the variable so that its single param can be passed by reference.
    
    BUG=none
    TEST=Compiles; functionality remains the same.
    
    Review URL: http://codereview.chromium.org/6698061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79637 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_resource.cc
+++ chrome/common/extensions/extension_resource.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -66,15 +66,17 @@ FilePath ExtensionResource::GetFilePath(
 
 // Unit-testing helpers.
 FilePath::StringType ExtensionResource::NormalizeSeperators(
-    FilePath::StringType path) const {
+    const FilePath::StringType& path) const {
 #if defined(FILE_PATH_USES_WIN_SEPARATORS)
-  FilePath::StringType ret_val;
-  for (size_t i = 0; i < path.length(); i++) {
-    if (FilePath::IsSeparator(path[i]))
-      path[i] = FilePath::kSeparators[0];
+  FilePath::StringType win_path = path;
+  for (size_t i = 0; i < win_path.length(); i++) {
+    if (FilePath::IsSeparator(win_path[i]))
+      win_path[i] = FilePath::kSeparators[0];
   }
-#endif  // FILE_PATH_USES_WIN_SEPARATORS
+  return win_path;
+#else
   return path;
+#endif  // FILE_PATH_USES_WIN_SEPARATORS
 }
 
 bool ExtensionResource::ComparePathWithDefault(const FilePath& path) const {
--- chrome/common/extensions/extension_resource.h
+++ chrome/common/extensions/extension_resource.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,7 +45,8 @@ class ExtensionResource {
   bool empty() { return extension_root().empty(); }
 
   // Unit test helpers.
-  FilePath::StringType NormalizeSeperators(FilePath::StringType path) const;
+  FilePath::StringType NormalizeSeperators(
+      const FilePath::StringType& path) const;
   bool ComparePathWithDefault(const FilePath& path) const;
 
  private:
