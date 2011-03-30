2257204b4c8a3764d6639310ae8c537fb007742c
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 20:12:31 +0000

    Mark ExtensionApiTest.Incognito as FLAKY.
    BUG=77951
    TEST=none
    
    TBR=mpcomplete@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_incognito_apitest.cc
+++ chrome/browser/extensions/extension_incognito_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -92,7 +92,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DontCreateIncognitoProfile) {
 }
 
 // Tests that the APIs in an incognito-enabled extension work properly.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Incognito) {
+// Flaky - see crbug.com/77951.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_Incognito) {
   host_resolver()->AddRule("*", "127.0.0.1");
   ASSERT_TRUE(StartTestServer());
 
