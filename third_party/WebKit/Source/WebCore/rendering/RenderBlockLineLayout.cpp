d5e6e25d2ef494f69c7c9f9370464c76b31b9fc4
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-30 12:48:08 +0000

    2011-03-29  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            RTL:  Directionality always reset on hard line break
            https://bugs.webkit.org/show_bug.cgi?id=23124
    
            Testing that hard line breaks are being treated as paragraph separators but only
            clearing directional state from Unicode control characters, not DOM nodes.
            Also adding expected pixel results for bidi-override-in-anonymous-block as we're
            passing more of the test with this patch.
    
            * fast/text/international/bidi-br-as-paragraph-separator.html: Added.
            * platform/mac/fast/css/bidi-override-in-anonymous-block-expected.checksum: Added.
            * platform/mac/fast/css/bidi-override-in-anonymous-block-expected.png: Added.
            * platform/mac/fast/text/international/bidi-br-as-paragraph-separator-expected.checksum: Added.
            * platform/mac/fast/text/international/bidi-br-as-paragraph-separator-expected.png: Added.
            * platform/mac/fast/text/international/bidi-br-as-paragraph-separator-expected.txt: Added.
    2011-03-30  Levi Weintraub  <leviw@chromium.org>
    
            Reviewed by Eric Seidel.
    
            RTL:  Directionality always reset on hard line break
            https://bugs.webkit.org/show_bug.cgi?id=23124
    
            No longer clearing all BidiContexts when we hit a hard line break.
            Instead, directionality applied by DOM elements is preserved by
            reconstructing the context stack ignoring those that didn't come
            from the DOM.
    
            Test: fast/text/international/bidi-br-as-paragraph-separator.html
    
            * platform/text/BidiContext.cpp:
            (WebCore::BidiContext::createUncached):
            (WebCore::BidiContext::create):
            (WebCore::copyContextAndRebaselineLevel): Helper to make a copy of a context
            and recalculate its bidi level.
            (WebCore::BidiContext::copyStackRemovingUnicodeEmbeddingContexts): Returns the top of
            a BidiContext stack that's equivalent but without contexts from Unicode directional
            characters.
            (WebCore::operator==): Now takes into account embedding source.
            * platform/text/BidiContext.h:
            (WebCore::BidiContext::source): Enum to specify whether an embedded
            bidirectional control came from the DOM/Style or Unicode characters
            (WebCore::BidiContext::BidiContext):
            * platform/text/BidiResolver.h:
            (WebCore::BidiEmbedding::BidiEmbedding): An embedding is now a direction
            and a hint about where it came from so we can differentiate DOM directions
            from unicode direction control characters.
            (WebCore::BidiEmbedding::direction):
            (WebCore::BidiEmbedding::source):
            (WebCore::::embed): Now takes a source as well as a direction.
            (WebCore::::commitExplicitEmbedding):
            (WebCore::::createBidiRunsForLine):
            * rendering/InlineIterator.h:
            (WebCore::bidiNext):
            (WebCore::bidiFirst):
            * rendering/RenderBlockLineLayout.cpp:
            (WebCore::RenderBlock::determineStartPosition):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -1214,7 +1214,7 @@ RootInlineBox* RenderBlock::determineStartPosition(bool& firstLine, bool& fullLa
         resolver.setLastStrongDir(direction);
         resolver.setLastDir(direction);
         resolver.setEorDir(direction);
-        resolver.setContext(BidiContext::create(ltr ? 0 : 1, direction, style()->unicodeBidi() == Override));
+        resolver.setContext(BidiContext::create(ltr ? 0 : 1, direction, style()->unicodeBidi() == Override, FromStyleOrDOM));
 
         startObj = bidiFirst(this, &resolver);
     }
