ed8788cbe5ac112ab128c36f68cd6d9673867458
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 17:05:15 +0000

    iwyu: Cleanup profile_import directory.
    BUG=None
    TEST=None
    
    R=jhawkins@chromium.org
    
    Review URL: http://codereview.chromium.org/6759032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/profile_import/profile_import_thread.cc
+++ chrome/profile_import/profile_import_thread.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/profile_import/profile_import_thread.h"
 
+#include <stddef.h>
 #include <algorithm>
 
+#include "base/message_loop.h"
+#include "base/threading/thread.h"
 #include "base/values.h"
 #include "chrome/browser/importer/external_process_importer_bridge.h"
 #include "chrome/browser/importer/importer.h"
@@ -14,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer_messages.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "content/common/child_process.h"
+#include "googleurl/src/gurl.h"
+#include "ipc/ipc_message_macros.h"
 
 namespace {
 // Rather than sending all import items over IPC at once we chunk them into
