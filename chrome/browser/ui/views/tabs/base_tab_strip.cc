228a16f41d5bad9214c6003b4ce678139f392736
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 23:48:27 +0000

    Second attempt at fixing regression in dragging text to the tabstrip. I'ved removed BaseTabStrip::CanDrop so that BrowserRootView is always targeted for dnd.
    BUG=76231
    TEST=see bug
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/6770015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/base_tab_strip.cc
+++ chrome/browser/ui/views/tabs/base_tab_strip.cc
@@ -398,10 +398,6 @@ void BaseTabStrip::Layout() {
   DoLayout();
 }
 
-bool BaseTabStrip::CanDrop(const ui::OSExchangeData& data) {
-  return IsVisible() && !IsAnimating();
-}
-
 bool BaseTabStrip::OnMouseDragged(const views::MouseEvent&  event) {
   if (drag_controller_.get())
     drag_controller_->Drag();
