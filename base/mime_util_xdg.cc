4eb49523446a7e6c54e3e15ba69c592eab3849ce
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 16:59:46 +0000

    XDG isn't thread safe.  Add a lock around calls to it.
    BUG=71586
    Review URL: http://codereview.chromium.org/6708072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79851 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mime_util_xdg.cc
+++ base/mime_util_xdg.cc
@@ -21,10 +21,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/string_split.h"
 #include "base/string_util.h"
+#include "base/synchronization/lock.h"
 #include "base/third_party/xdg_mime/xdgmime.h"
 #include "base/threading/thread_restrictions.h"
 
 namespace {
+
+// None of the XDG stuff is thread-safe, so serialize all accesss under
+// this lock.
+base::Lock g_mime_util_xdg_lock;
 
 class IconTheme;
 
@@ -554,11 +559,13 @@ namespace mime_util {
 
 std::string GetFileMimeType(const FilePath& filepath) {
   base::ThreadRestrictions::AssertIOAllowed();
+  base::AutoLock scoped_lock(g_mime_util_xdg_lock);
   return xdg_mime_get_mime_type_from_file_name(filepath.value().c_str());
 }
 
 std::string GetDataMimeType(const std::string& data) {
   base::ThreadRestrictions::AssertIOAllowed();
+  base::AutoLock scoped_lock(g_mime_util_xdg_lock);
   return xdg_mime_get_mime_type_for_data(data.data(), data.length(), NULL);
 }
 
@@ -586,8 +593,12 @@ FilePath GetMimeIcon(const std::string& mime_type, size_t size) {
   std::string icon_name;
   FilePath icon_file;
 
-  const char* icon = xdg_mime_get_icon(mime_type.c_str());
-  icon_name = std::string(icon ? icon : "");
+  {
+    base::AutoLock scoped_lock(g_mime_util_xdg_lock);
+    const char *icon = xdg_mime_get_icon(mime_type.c_str());
+    icon_name = std::string(icon ? icon : "");
+  }
+
   if (icon_name.length())
     icon_names.push_back(icon_name);
 
