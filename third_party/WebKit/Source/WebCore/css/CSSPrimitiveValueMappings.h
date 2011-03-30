8b7c8fa74bc1b37c95986f973f55c85b896a7999
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-30 17:20:26 +0000

    2011-03-30  Ofri Wolfus  <ofri@dhcp-172-28-40-178.tlv.corp.google.com>
            Reviewed by Eric Seidel.
    
            Add support for parsing unicode-bidi: -webkit-plaintext.
            https://bugs.webkit.org/show_bug.cgi?id=57457
    
            * css3/unicode-bidi-plaintext-parse-expected.txt: Added.
            * css3/unicode-bidi-plaintext-parse.html: Added.
    2011-03-30  Ofri Wolfus  <ofri@dhcp-172-28-40-178.tlv.corp.google.com>
    
            Reviewed by Eric Seidel.
    
            Add support for parsing unicode-bidi: -webkit-plaintext.
            https://bugs.webkit.org/show_bug.cgi?id=57457
    
            Test: css3/unicode-bidi-plaintext-parse.html
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue):
            * css/CSSPrimitiveValueMappings.h:
            (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
            (WebCore::CSSPrimitiveValue::operator EUnicodeBidi):
            * css/CSSValueKeywords.in:
            * rendering/style/RenderStyle.h:
            * rendering/style/RenderStyleConstants.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/Source/WebCore/css/CSSPrimitiveValueMappings.h
@@ -1753,6 +1753,10 @@ template<> inline CSSPrimitiveValue::CSSPrimitiveValue(EUnicodeBidi e)
         break;
     case Isolate:
         m_value.ident = CSSValueWebkitIsolate;
+        break;
+    case PlainText:
+        m_value.ident = CSSValueWebkitPlaintext;
+        break;
     }
 }
 
@@ -1767,6 +1771,8 @@ template<> inline CSSPrimitiveValue::operator EUnicodeBidi() const
         return Override;
     case CSSValueWebkitIsolate:
         return Isolate;
+    case CSSValueWebkitPlaintext:
+        return PlainText;
     default:
         ASSERT_NOT_REACHED();
         return UBNormal;
