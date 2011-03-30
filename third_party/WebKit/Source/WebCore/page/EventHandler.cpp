0fb4e85165b86af54e72bf3f74cc067183bf135c
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-30 19:58:07 +0000

    Crash when closing "Add Bookmark" dialog using the Enter Key https://bugs.webkit.org/show_bug.cgi?id=57294 <rdar://problem/9044756>
    Reviewed by Darin Adler.
    
    Protect the FrameView in EventHandler::keyEvent, like we do in other EventHandler
    functions that could destroy the frame.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::keyEvent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -2376,6 +2376,8 @@ bool EventHandler::needsKeyboardEventDisambiguationQuirks() const
 
 bool EventHandler::keyEvent(const PlatformKeyboardEvent& initialKeyEvent)
 {
+    RefPtr<FrameView> protector(m_frame->view()); 
+
 #if ENABLE(PAN_SCROLLING)
     if (Page* page = m_frame->page()) {
         if (page->mainFrame()->eventHandler()->panScrollInProgress() || m_autoscrollInProgress) {
