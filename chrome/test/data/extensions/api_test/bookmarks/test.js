7f86c170930f1c58d7f55d1c2a94e700f84d3122
bolms@chromium.org <bolms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 21:42:13 +0000

    Use correct error message when someone attempts to create a new child of the root bookmarks folder.
    BUG=29190
    TEST=chrome.bookmarks.create({parentId:"0", ...}) fails with the message
        "Can't modify the root bookmark folders."
    
    Review URL: http://codereview.chromium.org/6659020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79900 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/bookmarks/test.js
+++ chrome/test/data/extensions/api_test/bookmarks/test.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // bookmarks api test
 // browser_tests.exe --gtest_filter=ExtensionApiTest.Bookmarks
 
@@ -163,6 +167,12 @@ chrome.test.runTests([
                              "created node != source");
       expected[0].children[0].children.push(node);
     }));
+  },
+
+  function createInRoot() {
+    const error = "Can't modify the root bookmark folders.";
+    var node = {parentId:"0", title:"g404", url:"http://www.google.com/404"};
+    chrome.bookmarks.create(node, fail(error));
   },
 
   function createFolder() {
