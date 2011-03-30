68cef2377ef6964280ae516122f609daf2902c82
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-30 15:36:17 +0000

    2011-03-30  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed. Build fix.
    
            * UseJSC.cmake: fix typo PageDebugServer -> PageScriptDebugServer
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/UseJSC.cmake
+++ third_party/WebKit/Source/WebCore/UseJSC.cmake
@@ -142,7 +142,7 @@ LIST(APPEND WebCore_SOURCES
     bindings/js/JSXMLHttpRequestUploadCustom.cpp
     bindings/js/JSXSLTProcessorCustom.cpp
     bindings/js/JavaScriptCallFrame.cpp
-    bindings/js/PageDebugServer.cpp
+    bindings/js/PageScriptDebugServer.cpp
     bindings/js/ScheduledAction.cpp
     bindings/js/ScriptCachedFrameData.cpp
     bindings/js/ScriptCallStackFactory.cpp
@@ -158,7 +158,7 @@ LIST(APPEND WebCore_SOURCES
     bindings/js/ScriptValue.cpp
     bindings/js/SerializedScriptValue.cpp
     bindings/js/WorkerScriptController.cpp
-    bindings/js/WorkerDebugServer.cpp
+    bindings/js/WorkerScriptDebugServer.cpp
 
     bridge/IdentifierRep.cpp
     bridge/NP_jsobject.cpp
