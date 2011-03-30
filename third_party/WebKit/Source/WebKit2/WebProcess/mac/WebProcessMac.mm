ea3c012c1d2b9926c41da40220c87065d65dfb34
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-30 22:27:15 +0000

    Ensure consistent, readable working directory for WebProcess before entering sandbox <rdar://problem/8951176>
    Patch by Ivan Krstić <ike@apple.com> on 2011-03-30
    Reviewed by Oliver Hunt.
    
    * WebProcess/mac/WebProcessMac.mm:
    (WebKit::WebProcess::platformInitializeWebProcess):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
@@ -189,6 +189,8 @@ static id NSApplicationAccessibilityFocusedUIElement(NSApplication*, SEL)
     
 void WebProcess::platformInitializeWebProcess(const WebProcessCreationParameters& parameters, CoreIPC::ArgumentDecoder*)
 {
+    [[NSFileManager defaultManager] changeCurrentDirectoryPath:[[NSBundle mainBundle] bundlePath]];
+
     initializeSandbox(parameters);
 
     if (!parameters.parentProcessName.isNull()) {
