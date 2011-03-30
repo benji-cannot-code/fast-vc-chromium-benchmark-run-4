dc61ddd3279bab146ea1fd9f40c1900f4d7b8026
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 20:47:35 +0000

    Rerun configure to update linux/config.h and mac/config.h for open-vcdiff 0.7.
    Linux: Ubuntu Lucid 10.04
    Mac: Mac OS X 10.5 with Xcode 3.1.4
    
    It seems that the original linux/config.h was incorrectly generated.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6732031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79888 0039d316-1c4b-4281-b951-d872f2087c98

--- sdch/mac/config.h
+++ sdch/mac/config.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* src/config.h.  Generated from config.h.in by configure.  */
 /* src/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
@@ -95,19 +96,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_NAME "open-vcdiff"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "open-vcdiff 0.1"
+#define PACKAGE_STRING "open-vcdiff 0.7"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "open-vcdiff"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1"
+#define PACKAGE_VERSION "0.7"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Use custom compare function instead of memcmp */
+#if defined(__i386__) || defined(__x86_64__)
+#define VCDIFF_USE_BLOCK_COMPARE_WORDS 1
+#else
 /* #undef VCDIFF_USE_BLOCK_COMPARE_WORDS */
+#endif
 
 /* Version number of package */
-#define VERSION "0.1"
+#define VERSION "0.7"
