69ca3cb0cb059573b9ea0075558b795251646d59
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 08:33:55 +0000

    Adding 'explicit_expiration' cause to 'OnCookieChanged'
    BUG=77298
    TEST=browser_tests
    
    Review URL: http://codereview.chromium.org/6730049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79802 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_cookies_api_constants.cc
+++ chrome/browser/extensions/extension_cookies_api_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,10 +26,11 @@ const char kUrlKey[] = "url";
 const char kValueKey[] = "value";
 
 // Cause Constants
+extern const char kEvictedChangeCause[] = "evicted";
+extern const char kExpiredChangeCause[] = "expired";
+extern const char kExpiredOverwriteChangeCause[] = "expired_overwrite";
 extern const char kExplicitChangeCause[] = "explicit";
 extern const char kOverwriteChangeCause[] = "overwrite";
-extern const char kExpiredChangeCause[] = "expired";
-extern const char kEvictedChangeCause[] = "evicted";
 
 // Events
 const char kOnChanged[] = "cookies.onChanged";
--- chrome/browser/extensions/extension_cookies_api_constants.h
+++ chrome/browser/extensions/extension_cookies_api_constants.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,10 +30,11 @@ extern const char kUrlKey[];
 extern const char kValueKey[];
 
 // Cause Constants
+extern const char kEvictedChangeCause[];
+extern const char kExpiredChangeCause[];
+extern const char kExpiredOverwriteChangeCause[];
 extern const char kExplicitChangeCause[];
 extern const char kOverwriteChangeCause[];
-extern const char kExpiredChangeCause[];
-extern const char kEvictedChangeCause[];
 
 // Events.
 extern const char kOnChanged[];
