dc16acd1f4b7e535fddc7f32bba591c633a03942
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 16:12:55 +0000

    Fixes possible NULL pointer crash.
    BUG=none
    TEST=Run out of the box in the debug mode, no DCHECKs should fail.
    
    Review URL: http://codereview.chromium.org/6675041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79837 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -67,6 +67,7 @@ ExistingUserController::ExistingUserController(LoginDisplayHost* host)
       num_login_attempts_(0),
       user_settings_(new UserCrosSettingsProvider),
       method_factory_(this) {
+  DCHECK(current_controller_ == NULL);
   current_controller_ = this;
 
   login_display_.reset(host_->CreateLoginDisplay(this));
@@ -139,8 +140,11 @@ void ExistingUserController::Observe(NotificationType type,
 // ExistingUserController, private:
 
 ExistingUserController::~ExistingUserController() {
-  DCHECK(current_controller_ != NULL);
-  current_controller_ = NULL;
+  if (current_controller_ == this) {
+    current_controller_ = NULL;
+  } else {
+    NOTREACHED() << "More than one controller are alive.";
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
