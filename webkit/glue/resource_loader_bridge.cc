bba66453461982e0466d6ba07f3b477404e36387
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 14:17:34 +0000

    Save actual transfer size for future DevTools use
    BUG=40502
    
    Review URL: http://codereview.chromium.org/6690044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79990 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/resource_loader_bridge.cc
+++ webkit/glue/resource_loader_bridge.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,7 +29,8 @@ ResourceLoadTimingInfo::~ResourceLoadTimingInfo() {
 }
 
 ResourceDevToolsInfo::ResourceDevToolsInfo()
-    : http_status_code(0) {
+    : http_status_code(0),
+      bytes_received(0) {
 }
 
 ResourceDevToolsInfo::~ResourceDevToolsInfo() {}
