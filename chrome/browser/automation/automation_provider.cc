39982d6104185572c17ee7514e983d60eb0ce690
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 09:55:58 +0000

    GTTF: Make automation provider recognize more IPC error conditions
    1) Report IPC message deserialization errors in "delay reply" handlers.
    2) Handle those errors in automation provider.
    
    BUG=77875
    Review URL: http://codereview.chromium.org/6675047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -340,7 +340,8 @@ void AutomationProvider::OnChannelConnected(int pid) {
 
 bool AutomationProvider::OnMessageReceived(const IPC::Message& message) {
   bool handled = true;
-  IPC_BEGIN_MESSAGE_MAP(AutomationProvider, message)
+  bool deserialize_success = true;
+  IPC_BEGIN_MESSAGE_MAP_EX(AutomationProvider, message, deserialize_success)
 #if !defined(OS_MACOSX)
     IPC_MESSAGE_HANDLER_DELAY_REPLY(AutomationMsg_WindowDrag,
                                     WindowSimulateDrag)
@@ -408,8 +409,10 @@ bool AutomationProvider::OnMessageReceived(const IPC::Message& message) {
                                     OnRunUnloadHandlers)
     IPC_MESSAGE_HANDLER(AutomationMsg_SetZoomLevel, OnSetZoomLevel)
 #endif  // defined(OS_WIN)
-    IPC_MESSAGE_UNHANDLED(handled = false;OnUnhandledMessage())
-  IPC_END_MESSAGE_MAP()
+    IPC_MESSAGE_UNHANDLED(handled = false; OnUnhandledMessage())
+  IPC_END_MESSAGE_MAP_EX()
+  if (!deserialize_success)
+    OnMessageDeserializationFailure();
   return handled;
 }
 
@@ -423,6 +426,12 @@ void AutomationProvider::OnUnhandledMessage() {
              << "for test code (TestingAutomationProvider), and "
              << "switches::kAutomationClientChannelID for everything else "
              << "(like ChromeFrame). Closing the automation channel.";
+  channel_->Close();
+}
+
+void AutomationProvider::OnMessageDeserializationFailure() {
+  LOG(ERROR) << "Failed to deserialize IPC message. "
+             << "Closing the automation channel.";
   channel_->Close();
 }
 
