d95ca2b0f224b39915817c0757a5fbb64efde43f
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-31 16:34:13 +0000

    2011-03-30  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: remove unnecessary newBody output parameter from editScriptSource protocol method.
            https://bugs.webkit.org/show_bug.cgi?id=57444
    
            * http/tests/inspector/debugger-test.js:
            (initialize_DebuggerTest):
            * inspector/debugger/live-edit-expected.txt:
            * inspector/debugger/live-edit.html:
            * inspector/debugger/resources/edit-me-when-paused.js: Added.
            (f2):
            (f1):
    2011-03-30  Pavel Podivilov  <podivilov@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: remove unnecessary newBody output parameter from editScriptSource protocol method.
            https://bugs.webkit.org/show_bug.cgi?id=57444
    
            * bindings/js/ScriptDebugServer.cpp:
            (WebCore::ScriptDebugServer::editScriptSource):
            * bindings/js/ScriptDebugServer.h:
            * bindings/v8/ScriptDebugServer.cpp:
            (WebCore::ScriptDebugServer::editScriptSource):
            * bindings/v8/ScriptDebugServer.h:
            * inspector/Inspector.json:
            * inspector/InspectorDebuggerAgent.cpp:
            (WebCore::InspectorDebuggerAgent::editScriptSource):
            * inspector/InspectorDebuggerAgent.h:
            * inspector/front-end/DebuggerModel.js:
            (WebInspector.DebuggerModel.prototype.editScriptSource):
            (WebInspector.DebuggerModel.prototype._didEditScriptSource):
            * inspector/front-end/DebuggerPresentationModel.js:
            (WebInspector.DebuggerPresentationModel.prototype.editScriptSource.didEditScriptSource):
            (WebInspector.DebuggerPresentationModel.prototype.editScriptSource):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype._handleSave.didEditScriptSource):
            (WebInspector.SourceFrame.prototype._handleSave):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/ScriptDebugServer.h
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptDebugServer.h
@@ -79,7 +79,7 @@ public:
     void stepOverStatement();
     void stepOutOfFunction();
 
-    bool editScriptSource(const String& sourceID, const String& newContent, String& newSourceOrErrorMessage);
+    bool editScriptSource(const String& sourceID, const String& newContent, String* error);
 
     void recompileAllJSFunctionsSoon();
     virtual void recompileAllJSFunctions(Timer<ScriptDebugServer>* = 0) = 0;
--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptDebugServer.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptDebugServer.cpp
@@ -213,7 +213,7 @@ void ScriptDebugServer::stepOutOfFunction()
     continueProgram();
 }
 
-bool ScriptDebugServer::editScriptSource(const String& sourceID, const String& newContent, String& newSourceOrErrorMessage)
+bool ScriptDebugServer::editScriptSource(const String& sourceID, const String& newContent, String* error)
 {
     ensureDebuggerScriptCompiled();
     v8::HandleScope scope;
@@ -231,11 +231,12 @@ bool ScriptDebugServer::editScriptSource(const String& sourceID, const String& n
     if (tryCatch.HasCaught()) {
         v8::Local<v8::Message> message = tryCatch.Message();
         if (!message.IsEmpty())
-            newSourceOrErrorMessage = toWebCoreStringWithNullOrUndefinedCheck(message->Get());
+            *error = toWebCoreStringWithNullOrUndefinedCheck(message->Get());
+        else
+            *error = "Unknown error.";
         return false;
     }
     ASSERT(!result.IsEmpty());
-    newSourceOrErrorMessage = toWebCoreStringWithNullOrUndefinedCheck(result);
 
     // Call stack may have changed after if the edited function was on the stack.
     if (m_currentCallFrame)
