d0f63e9be4fd2847d7de5faeae8fd3e34b22c17e
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 17:07:39 +0000

    ntp4: Disable floating bookmark bar on Mac
    BUG=none
    TEST=manual
    Review URL: http://codereview.chromium.org/6737011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80016 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/browser_window_controller_private.h"
 
+#include "base/command_line.h"
 #import "base/memory/scoped_nsobject.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/pref_service.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/tabs/tab_strip_controller.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_strip_view.h"
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "content/browser/renderer_host/render_widget_host_view.h"
 #include "content/browser/tab_contents/tab_contents.h"
@@ -473,6 +475,10 @@ willPositionSheet:(NSWindow*)sheet
 }
 
 - (BOOL)shouldShowDetachedBookmarkBar {
+  // NTP4 never detaches the bookmark bar.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kNewTabPage4))
+    return NO;
+
   DCHECK(browser_.get());
   TabContents* contents = browser_->GetSelectedTabContents();
   return (contents &&
