22cfd45cac173c8b2acbb737e6d35e9a54c148c6
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-31 12:53:43 +0000

    2011-03-31  Vamshikrishna.Yellenki  <vamshi@motorola.com> and Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Martin Robinson.
    
            Implement MiniBrowser for Gtk port.
            https://bugs.webkit.org/show_bug.cgi?id=48512
    
            Initial implementation of the Gtk MiniBrowser.
    
            * GNUmakefile.am:
    
    2011-03-31  Vamshikrishna.Yellenki  <vamshi@motorola.com> and Alejandro G. Castro  <alex@igalia.com>
    
            Reviewed by Martin Robinson.
    
            Implement MiniBrowser for Gtk port.
            https://bugs.webkit.org/show_bug.cgi?id=48512
    
            Initial implementation of the Gtk MiniBrowser.
    
            * MiniBrowser/gtk/GNUmakefile.am: Added.
            * MiniBrowser/gtk/main.c: Added.
            (activateUriEntryCallback):
            (destroyCallback):
            (goBackCallback):
            (goForwardCallback):
            (createToolbar):
            (createWebView):
            (createWindow):
            (argumentToURL):
            (main):
    
    2011-03-31  Vamshikrishna.Yellenki  <vamshi@motorola.com> and Alejandro G. Castro  <alex@igalia.com>
    
            Reviewed by Martin Robinson.
    
            Implement MiniBrowser for Gtk port.
            https://bugs.webkit.org/show_bug.cgi?id=48512
    
            * Shared/API/c/gtk/WKBaseGtk.h: Add the stdbool.h include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/API/c/gtk/WKBaseGtk.h
+++ third_party/WebKit/Source/WebKit2/Shared/API/c/gtk/WKBaseGtk.h
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "Please #include \"WKBase.h\" instead of this file directly."
 #endif
 
+#include <stdbool.h>
+
 typedef const struct OpaqueWKView* WKViewRef;
 
 #endif /* WKBaseGtk_h */
