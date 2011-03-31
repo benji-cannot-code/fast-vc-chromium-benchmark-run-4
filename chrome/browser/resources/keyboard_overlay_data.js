cea3bba69c93a3567e92501c7652ceec10d7537e
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 10:01:06 +0000

    Fix the issue that Alt+Shift is not properly handled in the keyboard overlay.
    BUG=chromium-os:13614
    TEST=manually on the netbook
    
    Review URL: http://codereview.chromium.org/6675037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79981 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard_overlay_data.js
+++ chrome/browser/resources/keyboard_overlay_data.js
@@ -23546,7 +23546,7 @@ var keyboardOverlayData = {
     "7<>CTRL": "keyboardOverlayActivateTab7",
     "8<>CTRL": "keyboardOverlayActivateTab8",
     "9<>CTRL": "keyboardOverlayActivateLastTab",
-    "alt<>SHIFT": "keyboardOverlayCycleThroughInputMethods",
+    "ALT<>SHIFT": "keyboardOverlayCycleThroughInputMethods",
     "a<>CTRL": "keyboardOverlaySelectAll",
     "arrowkeys<>CTRL": "keyboardOverlayWordMove",
     "b<>CTRL<>SHIFT": "keyboardOverlayToggleBookmarkBar",
