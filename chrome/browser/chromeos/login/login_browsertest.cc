c3853daf03a780c6ebfe38ba48e2bae9adcd3fcb
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-31 11:19:53 +0000

    Revert "Enable component extensions for DefaultProfile used in login screen dialogs"
    Reverted commit: http://src.chromium.org/viewvc/chrome?view=rev&revision=79526
    
    BUG=chromium-os:12741,chromium-os:13464
    TEST=Run debug Chrome on device
    TBR=davemoore@chromium.org
    
    Review URL: http://codereview.chromium.org/6670111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79985 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_browsertest.cc
+++ chrome/browser/chromeos/login/login_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -92,9 +92,8 @@ IN_PROC_BROWSER_TEST_F(LoginProfileTest, UserNotPassed) {
   Profile* profile = browser()->profile();
   EXPECT_EQ("Default", profile->GetPath().BaseName().value());
   EXPECT_TRUE(profile->IsOffTheRecord());
-  // Ensure there's extension service for this profile.
-  EXPECT_NE(static_cast<ExtensionService*>(NULL),
-            profile->GetExtensionService());
+  // Ensure there's no extension service for this profile.
+  EXPECT_EQ(NULL, profile->GetExtensionService());
 }
 
 } // namespace chromeos
