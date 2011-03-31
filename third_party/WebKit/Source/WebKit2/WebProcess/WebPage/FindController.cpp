8af0e9c6632ff1cb40782b16e0f11c76a9b3c4c7
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-31 17:09:53 +0000

    <rdar://problem/9214824> Find client not told when WKPageCountStringMatches exceeds maximum https://bugs.webkit.org/show_bug.cgi?id=57552
    Reviewed by Darin Adler and Adam Roben.
    
    * WebProcess/WebPage/FindController.cpp:
    (WebKit::FindController::countStringMatches):
    Use the same technique as findString() to report kWKMoreThanMaximumMatchCount when appropriate.
    (WebKit::FindController::findString):
    Convert numeric_limits::max() to max() - 1 to avoid overflow case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/FindController.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/FindController.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/GraphicsContext.h>
 #include <WebCore/Page.h>
 
+using namespace std;
 using namespace WebCore;
 
 namespace WebKit {
@@ -65,9 +66,16 @@ FindController::~FindController()
 
 void FindController::countStringMatches(const String& string, FindOptions options, unsigned maxMatchCount)
 {
-    unsigned matchCount = m_webPage->corePage()->markAllMatchesForText(string, core(options), false, maxMatchCount);
+    if (maxMatchCount == numeric_limits<unsigned>::max())
+        --maxMatchCount;
+    
+    unsigned matchCount = m_webPage->corePage()->markAllMatchesForText(string, core(options), false, maxMatchCount + 1);
     m_webPage->corePage()->unmarkAllTextMatches();
 
+    // Check if we have more matches than allowed.
+    if (matchCount > maxMatchCount)
+        matchCount = static_cast<unsigned>(kWKMoreThanMaximumMatchCount);
+    
     m_webPage->send(Messages::WebPageProxy::DidCountStringMatches(string, matchCount));
 }
 
@@ -103,6 +111,9 @@ void FindController::findString(const String& string, FindOptions options, unsig
         shouldShowOverlay = options & FindOptionsShowOverlay;
 
         if (shouldShowOverlay) {
+            if (maxMatchCount == numeric_limits<unsigned>::max())
+                --maxMatchCount;
+            
             unsigned matchCount = m_webPage->corePage()->markAllMatchesForText(string, core(options), false, maxMatchCount + 1);
 
             // Check if we have more matches than allowed.
