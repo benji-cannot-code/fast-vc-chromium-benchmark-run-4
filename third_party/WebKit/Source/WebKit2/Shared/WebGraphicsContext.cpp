706fd084db3782c8fec3399e684f580a798c55be
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-31 01:06:24 +0000

    2011-03-30  Martin Robinson  <mrobinson@igalia.com>
            Fix the GTK+ WebKit2 build.
    
            * Shared/WebGraphicsContext.cpp:
            (WebKit::WebGraphicsContext::WebGraphicsContext):
            * WebProcess/WebPage/gtk/WebPageGtk.cpp:
            (WebKit::WebPage::platformCanHandleRequest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebGraphicsContext.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/WebGraphicsContext.cpp
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebGraphicsContext.h"
 
+#if PLATFORM(GTK)
+#include <WebCore/PlatformContextCairo.h>
+#endif
+
 using namespace WebCore;
 
 namespace WebKit {
@@ -35,7 +39,7 @@ WebGraphicsContext::WebGraphicsContext(GraphicsContext* graphicsContext)
 #if PLATFORM(CG)
     : m_platformContext(graphicsContext->platformContext())
 #elif PLATFORM(GTK)
-    : m_platformContext(graphicsContext->platformContext())
+    : m_platformContext(graphicsContext->platformContext()->cr())
 #endif
 {
 }
