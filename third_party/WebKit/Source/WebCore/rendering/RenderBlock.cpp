0ea3f1654a3dbf7a1dd40fc1c96694a4c4373712
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-03-31 09:48:31 +0000

    2011-03-31  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Ryosuke Niwa.
    
            Clean up from r82447.
            https://bugs.webkit.org/attachment.cgi?bugid=57532
    
            No new tests since this doesn't change behavior.
    
            * rendering/RenderBlock.cpp: Removing unnecessary visible_units.h include.
            * rendering/RootInlineBox.cpp:
            (WebCore::RootInlineBox::getLogicalStartBoxWithNode): Making const.
            (WebCore::RootInlineBox::getLogicalEndBoxWithNode): Ditto.
            * rendering/RootInlineBox.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Settings.h"
 #include "TextRun.h"
 #include "TransformState.h"
-#include "visible_units.h"
 #include <wtf/StdLibExtras.h>
 
 using namespace std;
