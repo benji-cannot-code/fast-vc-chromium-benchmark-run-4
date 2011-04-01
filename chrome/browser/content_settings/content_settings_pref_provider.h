6d097208d9256219c5ad3f720e5eaaafc641d386
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-01 11:26:02 +0000

    Get rid of PrefService::GetMutableDictionary/GetMutableList
    BUG=77914
    TEST=none, trybots remain green
    
    Review URL: http://codereview.chromium.org/6758035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_pref_provider.h
+++ chrome/browser/content_settings/content_settings_pref_provider.h
@@ -28,6 +28,8 @@ class Profile;
 
 namespace content_settings {
 
+// Content settings provider that provides default content settings based on
+// user prefs.
 class PrefDefaultProvider : public DefaultProviderInterface,
                             public NotificationObserver {
  public:
--- chrome/browser/content_settings/content_settings_pref_provider_unittest.cc
+++ chrome/browser/content_settings/content_settings_pref_provider_unittest.cc
@@ -174,6 +174,7 @@ class PrefProviderTest : public TestingBrowserProcessTest {
 
 TEST_F(PrefProviderTest, Observer) {
   TestingProfile profile;
+  profile.GetHostContentSettingsMap();
   Profile* p = &profile;
   PrefProvider pref_content_settings_provider(p);
   StubSettingsObserver observer;
@@ -189,7 +190,8 @@ TEST_F(PrefProviderTest, Observer) {
   EXPECT_EQ(pattern, observer.last_pattern);
   EXPECT_FALSE(observer.last_update_all);
   EXPECT_FALSE(observer.last_update_all_types);
-  EXPECT_EQ(1, observer.counter);
+  // Expect 2 calls: One from the update and one from canonicalization.
+  EXPECT_EQ(2, observer.counter);
 }
 
 // Test for regression in which the PrefProvider modified the user pref store
@@ -229,6 +231,7 @@ TEST_F(PrefProviderTest, Incognito) {
   profile.SetPrefService(regular_prefs);
   otr_profile->set_incognito(true);
   otr_profile->SetPrefService(otr_prefs);
+  profile.GetHostContentSettingsMap();
 
   PrefProvider pref_content_settings_provider(&profile);
   PrefProvider pref_content_settings_provider_incognito(otr_profile);
@@ -255,6 +258,7 @@ TEST_F(PrefProviderTest, Incognito) {
 
 TEST_F(PrefProviderTest, Patterns) {
   TestingProfile testing_profile;
+  testing_profile.GetHostContentSettingsMap();
   PrefProvider pref_content_settings_provider(
       testing_profile.GetOriginalProfile());
 
@@ -316,6 +320,7 @@ TEST_F(PrefProviderTest, ResourceIdentifier) {
   cmd->AppendSwitch(switches::kEnableResourceContentSettings);
 
   TestingProfile testing_profile;
+  testing_profile.GetHostContentSettingsMap();
   PrefProvider pref_content_settings_provider(
       testing_profile.GetOriginalProfile());
 
