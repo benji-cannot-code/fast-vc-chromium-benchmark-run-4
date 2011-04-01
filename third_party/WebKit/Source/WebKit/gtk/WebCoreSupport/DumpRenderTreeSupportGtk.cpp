ca24c0da4e31b918623b14e008090688f655dae2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-01 21:36:20 +0000

    2011-04-01  Dominic Cooney  <dominicc@google.com>
            Reviewed by Martin Robinson.
    
            Add layoutTestController.shadowRoot to GTK DumpRenderTree.
            https://bugs.webkit.org/show_bug.cgi?id=57551
    
            * platform/gtk/Skipped: unskip test that accesses shadowRoot
    2011-04-01  Dominic Cooney  <dominicc@google.com>
    
            Reviewed by Martin Robinson.
    
            Add layoutTestController.shadowRoot to GTK DumpRenderTree.
            https://bugs.webkit.org/show_bug.cgi?id=57551
    
            * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
            (DumpRenderTreeSupportGtk::shadowRoot):
            * WebCoreSupport/DumpRenderTreeSupportGtk.h:
    2011-04-01  Dominic Cooney  <dominicc@google.com>
    
            Reviewed by Martin Robinson.
    
            Add layoutTestController.shadowRoot to GTK DumpRenderTree.
            https://bugs.webkit.org/show_bug.cgi?id=57551
    
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::shadowRoot):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AnimationController.h"
 #include "DOMWrapperWorld.h"
 #include "Document.h"
+#include "Element.h"
 #include "FocusController.h"
 #include "FrameLoaderClientGtk.h"
 #include "FrameTree.h"
@@ -635,6 +636,16 @@ void DumpRenderTreeSupportGtk::clearOpener(WebKitWebFrame* frame)
     Frame* coreFrame = core(frame);
     if (coreFrame)
         coreFrame->loader()->setOpener(0);
+}
+
+JSValueRef DumpRenderTreeSupportGtk::shadowRoot(JSContextRef context, JSValueRef value)
+{
+    JSC::ExecState* exec = toJS(context);
+    Element* element = toElement(toJS(exec, value));
+    if (!element)
+      return JSValueMakeNull(context);
+
+    return toRef(exec, toJS(exec, element->shadowRoot()));
 }
 
 unsigned int DumpRenderTreeSupportGtk::workerThreadCount()
