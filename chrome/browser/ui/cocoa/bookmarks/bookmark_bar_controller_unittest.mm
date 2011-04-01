3926192af6a1e7f53e55d8dda6efda51e4c70f95
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-01 19:44:11 +0000

    Fix typo (accidentally deleted part of a new comment before checking in). Review URL: http://codereview.chromium.org/6764004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80202 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_unittest.mm
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #include "ui/base/theme_provider.h"
 
-// Unit tests don't need time-consuming a
+// Unit tests don't need time-consuming asynchronous animations.
 @interface BookmarkBarControllerTestable : BookmarkBarController {
 }
 @end
