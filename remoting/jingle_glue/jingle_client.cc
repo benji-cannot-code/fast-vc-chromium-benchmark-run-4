79515ecd03e262a31510d6bd26d22c59a3d952fe
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-01 18:51:46 +0000

    Correction for r80070: call DoStartSession(), enable TCP.
    TEST=remoting connection works
    BUG=None
    
    Review URL: http://codereview.chromium.org/6706024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80197 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/jingle_glue/jingle_client.cc
+++ remoting/jingle_glue/jingle_client.cc
@@ -170,7 +170,8 @@ JingleClient::JingleClient(JingleThread* thread,
                            SignalStrategy* signal_strategy,
                            PortAllocatorSessionFactory* session_factory,
                            Callback* callback)
-    : thread_(thread),
+    : enable_nat_traversing_(false),
+      thread_(thread),
       state_(START),
       initialized_(false),
       closed_(false),
@@ -234,8 +235,7 @@ void JingleClient::DoInitialize() {
           port_allocator_session_factory_.get(), "transp2"));
   if (!enable_nat_traversing_) {
     port_allocator_->set_flags(cricket::PORTALLOCATOR_DISABLE_STUN |
-                               cricket::PORTALLOCATOR_DISABLE_RELAY |
-                               cricket::PORTALLOCATOR_DISABLE_TCP);
+                               cricket::PORTALLOCATOR_DISABLE_RELAY);
   }
 
   // TODO(ajwong): The strategy needs a "start" command or something.  Right
@@ -251,6 +251,8 @@ void JingleClient::DoInitialize() {
         NewCallback(this, &JingleClient::OnJingleInfo));
     jingle_info_request_->Run(
         NewRunnableMethod(this, &JingleClient::DoStartSession));
+  } else {
+    DoStartSession();
   }
 }
 
