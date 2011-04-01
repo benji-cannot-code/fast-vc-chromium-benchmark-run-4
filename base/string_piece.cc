0f73ffaaab1ce06519740d6d19e495e46c515951
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-01 00:02:29 +0000

    Base: A few more changes to build base.dll
    BUG=76996
    TEST=none
    Review URL: http://codereview.chromium.org/6784002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80104 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_piece.cc
+++ base/string_piece.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 // Copied from strings/stringpiece.cc with modifications
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 typedef StringPiece::size_type size_type;
-
-std::ostream& operator<<(std::ostream& o, const StringPiece& piece) {
-  o.write(piece.data(), static_cast<std::streamsize>(piece.size()));
-  return o;
-}
 
 bool operator==(const StringPiece& x, const StringPiece& y) {
   if (x.size() != y.size())
