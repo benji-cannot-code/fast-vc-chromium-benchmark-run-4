d4587e8b046e9cbda334095c4427a2da21bcf877
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-01 00:29:24 +0000

    Let the unit test switch off the asynchronous animation if it wants, to make things more reproducible and to speed up the tests. Review URL: http://codereview.chromium.org/6790001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_unittest.mm
@@ -33,8 +33,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/ocmock/OCMock/OCMock.h"
 #include "ui/base/theme_provider.h"
 
+// Unit tests don't need time-consuming a
+@interface BookmarkBarControllerTestable : BookmarkBarController {
+}
+@end
+@implementation BookmarkBarControllerTestable
+- (BOOL)animationEnabled {
+  return NO;
+}
+@end
+
 // Just like a BookmarkBarController but openURL: is stubbed out.
-@interface BookmarkBarControllerNoOpen : BookmarkBarController {
+@interface BookmarkBarControllerNoOpen : BookmarkBarControllerTestable {
  @public
   std::vector<GURL> urls_;
   std::vector<WindowOpenDisposition> dispositions_;
@@ -137,7 +147,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Just like a BookmarkBarController but intercedes when providing
 // pasteboard drag data.
-@interface BookmarkBarControllerDragData : BookmarkBarController {
+@interface BookmarkBarControllerDragData : BookmarkBarControllerTestable {
   const BookmarkNode* dragDataNode_;  // Weak
 }
 - (void)setDragDataNode:(const BookmarkNode*)node;
