3c554e4e89d43a3c6eeeaf56e6993e37f1eeaa07
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-01 23:07:36 +0000

    2011-03-31  Jer Noble  <jer.noble@apple.com>
            Reviewed by Dan Bernstein.
    
            WebKit2: Save as PDF in Safari provides Untitled as default file name
            https://bugs.webkit.org/show_bug.cgi?id=57529
    
            Set the NSPrintOperation's jobTitle to the printing frame's title.
    
            * UIProcess/API/mac/WKView.mm:
            (-[WKView printOperationWithPrintInfo:forFrame:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -1761,6 +1761,7 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
         // NSPrintOperation takes ownership of the view.
         NSPrintOperation *printOperation = [NSPrintOperation printOperationWithView:printingView.get()];
         [printOperation setCanSpawnSeparateThread:YES];
+        [printOperation setJobTitle:toImpl(frameRef)->title()];
         printingView->_printOperation = printOperation;
         return printOperation;
     }
