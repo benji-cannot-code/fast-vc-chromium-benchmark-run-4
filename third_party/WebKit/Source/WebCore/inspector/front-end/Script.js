8a1b39e70cb4f4311e3b6847d844e0acc6fea0b9
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-01 15:19:38 +0000

    2011-04-01  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: make editScriptSource a Script's method.
            https://bugs.webkit.org/show_bug.cgi?id=57615
    
            * inspector/front-end/DebuggerModel.js:
            (WebInspector.DebuggerModel.prototype.editScriptSource):
            (WebInspector.DebuggerModel.prototype._didEditScriptSource):
            * inspector/front-end/Script.js:
            (WebInspector.Script.prototype.requestSource):
            (WebInspector.Script.prototype.editSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Script.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Script.js
@@ -55,5 +55,16 @@ WebInspector.Script.prototype = {
             callback(this._source);
         }
         DebuggerAgent.getScriptSource(this.sourceID, didGetScriptSource.bind(this));
+    },
+
+    editSource: function(newSource, callback)
+    {
+        function didEditScriptSource(error, callFrames)
+        {
+            if (!error)
+                this._source = newSource;
+            callback(error, callFrames);
+        }
+        DebuggerAgent.editScriptSource(this.sourceID, newSource, didEditScriptSource.bind(this));
     }
 }
