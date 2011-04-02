fc51f5df6ea27a46c00d8e85f5b2ec31ac79a49c
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-02 12:15:35 +0000

    2011-04-02  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Eric Seidel.
    
            Remove AbstractStep._run_script and move script names to ports.py
            https://bugs.webkit.org/show_bug.cgi?id=57704
    
            Replace deprecated _run_script with _tool.executive.run_and_throw_if_fail.
    
            * Scripts/webkitpy/common/config/ports.py:
            * Scripts/webkitpy/tool/commands/download_unittest.py:
            * Scripts/webkitpy/tool/commands/roll_unittest.py:
            * Scripts/webkitpy/tool/commands/upload_unittest.py:
            * Scripts/webkitpy/tool/steps/abstractstep.py:
            * Scripts/webkitpy/tool/steps/checkstyle.py:
            * Scripts/webkitpy/tool/steps/preparechangelog.py:
            * Scripts/webkitpy/tool/steps/preparechangelogfordepsroll.py:
            * Scripts/webkitpy/tool/steps/preparechangelogforrevert.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/abstractstep.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/abstractstep.py
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from webkitpy.common.system.deprecated_logging import log
 from webkitpy.common.system.executive import ScriptError
 from webkitpy.common.config.ports import WebKitPort
 from webkitpy.tool.steps.options import Options
@@ -37,14 +36,6 @@ class AbstractStep(object):
     def __init__(self, tool, options):
         self._tool = tool
         self._options = options
-
-    # FIXME: This should use tool.port()
-    def _run_script(self, script_name, args=None, quiet=False, port=WebKitPort):
-        log("Running %s" % script_name)
-        command = port.script_shell_command(script_name)
-        if args:
-            command.extend(args)
-        self._tool.executive.run_and_throw_if_fail(command, quiet)
 
     def _changed_files(self, state):
         return self.cached_lookup(state, "changed_files")
