b4e48c8e82df2860d65c7c8760aa1442bdace8fd
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-03 00:19:41 +0000

    Need to reset the scale, much like zoom. * DumpRenderTree/mac/DumpRenderTree.mm: (resetWebViewToConsistentStateBeforeTesting):
    Rubber-stamped by Geoff Garen.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/DumpRenderTree.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/DumpRenderTree.mm
@@ -1027,6 +1027,7 @@ static void resetWebViewToConsistentStateBeforeTesting()
     [(EditingDelegate *)[webView editingDelegate] setAcceptsEditing:YES];
     [webView makeTextStandardSize:nil];
     [webView resetPageZoom:nil];
+    [webView _scaleWebView:1.0 atOrigin:NSMakePoint(0, 0)];
     [webView setTabKeyCyclesThroughElements:YES];
     [webView setPolicyDelegate:nil];
     [policyDelegate setPermissive:NO];
