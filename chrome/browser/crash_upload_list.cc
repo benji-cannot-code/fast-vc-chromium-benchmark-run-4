415dc3e96998361bd8d5ee4366a227c7e34b6427
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-03 02:34:27 +0000

    VC2010e: Fix compilation; back_inserter requires <iterator>
    Not much more to say then that. Files modified were recently created.
    
    NOTE: Ignore the lack of whitespace in the individual diffs - the raw patchset is correct.
    
    BUG=None
    TEST=Compiles
    
    Review URL: http://codereview.chromium.org/6792013
    Patch from Ryan Norton <rnorton10@gmail.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80276 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/crash_upload_list.cc
+++ chrome/browser/crash_upload_list.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/crash_upload_list.h"
 
+#include <iterator>
+
 #include "base/path_service.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
--- chrome/browser/profiles/profile_dependency_manager.cc
+++ chrome/browser/profiles/profile_dependency_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <deque>
+#include <iterator>
 
 #include "chrome/browser/profiles/profile_keyed_service.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
