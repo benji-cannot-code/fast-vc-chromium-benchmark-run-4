f8c1d4b726ccd5db90b2831f1873a14b2fd65d4d
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 20:33:11 +0000

    Fix for regressions caused by FREEZE_UPDATE property  * menu can be closed before being shown. (native_menu_gtk.cc)  * virtual keyboard doesn't get expose event on window_contents_ due to renderer covering the entire area.
    BUG=chromium-os:13760, chromium-os:11514
    TEST=refer to bugs.
    
    Review URL: http://codereview.chromium.org/6794026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80366 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/frame/bubble_window.cc
+++ chrome/browser/chromeos/frame/bubble_window.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/skia_utils_gtk.h"
 #include "views/window/non_client_view.h"
 
-namespace chromeos {
+namespace {
 
 bool IsInsideCircle(int x0, int y0, int x1, int y1, int r) {
   return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1) <= r * r;
@@ -23,6 +23,10 @@ void SetRegionUnionWithPoint(int i, int j, GdkRegion* region) {
   GdkRectangle rect = {i, j, 1, 1};
   gdk_region_union_with_rect(region, &rect);
 }
+
+}  // namespace
+
+namespace chromeos {
 
 // static
 const SkColor BubbleWindow::kBackgroundColor = SK_ColorWHITE;
--- views/controls/menu/native_menu_gtk.cc
+++ views/controls/menu/native_menu_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -309,6 +309,13 @@ void NativeMenuGtk::OnMenuHidden(GtkWidget* widget) {
   }
   // Quit the nested message loop we spawned in RunMenuAt.
   MessageLoop::current()->Quit();
+
+  // Menu can be closed before the menu is shown.
+  if (expose_handler_id_) {
+    g_signal_handler_disconnect(menu_, expose_handler_id_);
+    expose_handler_id_ = 0;
+  }
+
   menu_hidden_ = true;
 }
 
