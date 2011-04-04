35abd375b289e1dcfd312cd7027d6b5184311722
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-04 21:09:29 +0000

    2011-04-04  Geoffrey Garen  <ggaren@apple.com>
            Not reviewed.
    
            Removed a redundant variable from HandleHeap
            https://bugs.webkit.org/show_bug.cgi?id=57786
            
            Forgot to commit the file that actually removes the data member!
            
            * collector/handles/HandleHeap.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/collector/handles/HandleHeap.h
+++ third_party/WebKit/Source/JavaScriptCore/collector/handles/HandleHeap.h
@@ -115,10 +115,6 @@ private:
     SentinelLinkedList<Node> m_immediateList;
     SinglyLinkedList<Node> m_freeList;
     Node* m_nextToFinalize;
-
-#if !ASSERT_DISABLED
-    bool m_handlingFinalizers;
-#endif
 };
 
 inline HandleHeap* HandleHeap::heapFor(HandleSlot handle)
