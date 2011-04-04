1f6370900e0b04e70dcdbb1ad67c436408bc40c0
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-04 09:44:06 +0000

    2011-04-04  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed. Fix Qt minimal build.
    
            * inspector/InspectorFrontendChannel.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorFrontendChannel.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorFrontendChannel.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef InspectorFrontendChannel_h
 #define InspectorFrontendChannel_h
 
-#if ENABLE(INSPECTOR)
-
 #include <wtf/Forward.h>
 
 namespace WebCore {
@@ -40,7 +38,5 @@ public:
 };
 
 } // namespace WebCore
-
-#endif // ENABLE(INSPECTOR)
 
 #endif // !defined(InspectorFrontendChannel_h)
