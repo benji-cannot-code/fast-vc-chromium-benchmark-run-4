87d0b374903d5c6eb65c7ab6371e492392a9c735
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-04 02:13:27 +0000

    <rdar://problem/9227839> REGRESSION: Reproducible crash in Snow Leopard when trying to show the Dictionary panel or application https://bugs.webkit.org/show_bug.cgi?id=57739
    Reviewed by Maciej Stachowiak.
    
    * Shared/DictionaryPopupInfo.cpp:
    (WebKit::DictionaryPopupInfo::encode): Do not encode the options dictionary on Snow Leopard.
    (WebKit::DictionaryPopupInfo::decode): Do not decode the options dictionary on Snow Leopard.
    * Shared/DictionaryPopupInfo.h: Removed the options member variable on Snow Leopard.
    * Shared/Plugins/PluginQuirks.h:
    * WebProcess/WebPage/mac/WebPageMac.mm:
    (WebKit::WebPage::performDictionaryLookupForRange): Do not set the options member variable on
    Snow Leopard.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/Plugins/PluginQuirks.h
+++ third_party/WebKit/Source/WebKit2/Shared/Plugins/PluginQuirks.h
@@ -50,7 +50,7 @@ public:
         MakeTransparentIfBackgroundAttributeExists,
 
 #ifndef NP_NO_QUICKDRAW
-        // Allow the plug-in to use the QuickDraw drawing model, since wek now that the plug-in
+        // Allow the plug-in to use the QuickDraw drawing model, since we know that the plug-in
         // will never paint or receive events. Used by the AppleConnect plug-in.
         AllowHalfBakedQuickDrawSupport,
 #endif
