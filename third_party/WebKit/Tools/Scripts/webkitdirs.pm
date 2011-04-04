0a948685f2e15de18c46cb5df7b65a4a9680befd
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-04 22:37:24 +0000

    [wx] Unreviewed build fix, run uninstall when cleaning to remove built files from WebKitBuild.
    git-svn-id: svn://svn.chromium.org/blink/trunk@82885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1333,7 +1333,7 @@ sub buildWafProject
         chomp($wafCommand);
     }
     if ($shouldClean) {
-        return system $wafCommand, "clean", "distclean";
+        return system $wafCommand, "uninstall", "clean", "distclean";
     }
     
     return system $wafCommand, 'configure', 'build', 'install', @options;
