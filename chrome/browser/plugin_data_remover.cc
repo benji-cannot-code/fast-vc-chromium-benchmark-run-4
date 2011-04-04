d1f5a43eabe2b9f3b389b3ba50277f455777c950
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 19:12:22 +0000

    Readability review for bauerb.
    PluginDataRemover opens a connection to the Flash plugin process, tells it to call the NPP_ClearSiteData function and waits for it to finish. This is used in Chrome to clear Flash LSO data from the Clear Browsing Data dialog and at shutdown.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6665034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_data_remover.cc
+++ chrome/browser/plugin_data_remover.cc
@@ -22,18 +22,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+
 const char* kFlashMimeType = "application/x-shockwave-flash";
 // The minimum Flash Player version that implements NPP_ClearSiteData.
 const char* kMinFlashVersion = "10.3";
 const int64 kRemovalTimeoutMs = 10000;
 const uint64 kClearAllData = 0;
+
 }  // namespace
 
 PluginDataRemover::PluginDataRemover()
     : mime_type_(kFlashMimeType),
       is_removing_(false),
       event_(new base::WaitableEvent(true, false)),
-      channel_(NULL) { }
+      channel_(NULL) {
+}
 
 PluginDataRemover::~PluginDataRemover() {
   DCHECK(!is_removing_);
@@ -41,14 +44,15 @@ PluginDataRemover::~PluginDataRemover() {
     BrowserThread::DeleteSoon(BrowserThread::IO, FROM_HERE, channel_);
 }
 
-base::WaitableEvent* PluginDataRemover::StartRemoving(
-    const base::Time& begin_time) {
+base::WaitableEvent* PluginDataRemover::StartRemoving(base::Time begin_time) {
   DCHECK(!is_removing_);
   remove_start_time_ = base::Time::Now();
   begin_time_ = begin_time;
 
   is_removing_ = true;
 
+  // Balanced in OnChannelOpened or OnError. Exactly one them will eventually be
+  // called, so we need to keep this object around until then.
   AddRef();
   PluginService::GetInstance()->OpenChannelToNpapiPlugin(
       0, 0, GURL(), mime_type_, this);
@@ -75,7 +79,7 @@ void PluginDataRemover::Wait() {
 }
 
 int PluginDataRemover::ID() {
-  // Generate an ID for the browser process.
+  // Generate a unique identifier for this PluginProcessHostClient.
   return ChildProcessInfo::GenerateChildProcessUniqueId();
 }
 
@@ -89,6 +93,7 @@ void PluginDataRemover::SetPluginInfo(
 
 void PluginDataRemover::OnChannelOpened(const IPC::ChannelHandle& handle) {
   ConnectToChannel(handle);
+  // Balancing the AddRef call in StartRemoving.
   Release();
 }
 
@@ -107,10 +112,9 @@ void PluginDataRemover::ConnectToChannel(const IPC::ChannelHandle& handle) {
     return;
   }
 
-  if (!channel_->Send(
-          new PluginMsg_ClearSiteData(std::string(),
-                                      kClearAllData,
-                                      begin_time_))) {
+  if (!channel_->Send(new PluginMsg_ClearSiteData(std::string(),
+                                                  kClearAllData,
+                                                  begin_time_))) {
     NOTREACHED() << "Couldn't send ClearSiteData message";
     SignalDone();
     return;
@@ -120,6 +124,7 @@ void PluginDataRemover::ConnectToChannel(const IPC::ChannelHandle& handle) {
 void PluginDataRemover::OnError() {
   LOG(DFATAL) << "Couldn't open plugin channel";
   SignalDone();
+  // Balancing the AddRef call in StartRemoving.
   Release();
 }
 
--- chrome/browser/plugin_data_remover_browsertest.cc
+++ chrome/browser/plugin_data_remover_browsertest.cc
@@ -50,8 +50,6 @@ class PluginDataRemoverTest : public InProcessBrowserTest,
     command_line->AppendSwitchPath(switches::kExtraPluginDir,
                                    browser_directory.AppendASCII("plugins"));
 #endif
-
-//    command_line->AppendSwitch(switches::kPluginStartupDialog);
   }
 
  private:
