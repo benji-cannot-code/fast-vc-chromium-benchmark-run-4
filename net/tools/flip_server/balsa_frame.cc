2975683ab16ee157895c715cf0bc189902ff2cfa
mmoss@google.com <mmoss@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 04:05:27 +0000

    Protect header that requires SSE2 on gcc4.4 and remove unused header.
    This fixes a compile error on the new 32-bit Lucid buildbots:
    
    In file included from net/tools/flip_server/balsa_headers.cc:7:
    /usr/lib/gcc/i486-linux-gnu/4.4.3/include/emmintrin.h:32:3: error: #error "SSE2 instruction set not enabled"
    In file included from net/tools/flip_server/balsa_frame.cc:8:
    /usr/lib/gcc/i486-linux-gnu/4.4.3/include/emmintrin.h:32:3: error: #error "SSE2 instruction set not enabled"
    
    BUG=
    TEST=Lucid builbot compiles
    
    Review URL: http://codereview.chromium.org/6677116
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80292 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/balsa_frame.cc
+++ net/tools/flip_server/balsa_frame.cc
@@ -1,12 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "net/tools/flip_server/balsa_frame.h"
 
 #include <assert.h>
+#if __SSE2__
 #include <emmintrin.h>
+#endif  // __SSE2__
 #include <strings.h>
 
 #include <limits>
@@ -1594,4 +1596,3 @@ const uint32 BalsaFrame::kValidTerm2;
 const uint32 BalsaFrame::kValidTerm2Mask;
 
 }  // namespace net
-
