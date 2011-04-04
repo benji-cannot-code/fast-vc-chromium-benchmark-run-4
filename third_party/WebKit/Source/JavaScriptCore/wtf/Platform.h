e27971affbfb90ba8d822e38bbcc6fec0499b9d7
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-04 21:59:48 +0000

    2011-04-04  Oliver Hunt  <oliver@apple.com>
            Reviewed by Geoffrey Garen.
    
            Make malloc validation useful
            https://bugs.webkit.org/show_bug.cgi?id=57502
    
            This patch changes FAST_MALLOC_MATCH_VALIDATION with a general
            corruption check that tags the beginning and end of all allocations
            to check for write overflows and overwrites the contents of
            memory on free in order to (hopefully) show up use-after-free issues
            sooner.
    
            We also turn it on by default for debug builds.
    
            * JavaScriptCore.exp:
            * wtf/FastMalloc.cpp:
            (WTF::tryFastMalloc):
            (WTF::fastMalloc):
            (WTF::tryFastCalloc):
            (WTF::fastCalloc):
            (WTF::fastFree):
            (WTF::tryFastRealloc):
            (WTF::fastRealloc):
            (WTF::TCMalloc_PageHeap::isScavengerSuspended):
            (WTF::TCMalloc_PageHeap::scheduleScavenger):
            (WTF::TCMalloc_PageHeap::suspendScavenger):
            (WTF::TCMalloc_PageHeap::signalScavenger):
            (WTF::TCMallocStats::malloc):
            (WTF::TCMallocStats::free):
            (WTF::TCMallocStats::fastCalloc):
            (WTF::TCMallocStats::tryFastCalloc):
            (WTF::TCMallocStats::calloc):
            (WTF::TCMallocStats::fastRealloc):
            (WTF::TCMallocStats::tryFastRealloc):
            (WTF::TCMallocStats::realloc):
            * wtf/FastMalloc.h:
            (WTF::Internal::fastMallocValidationHeader):
            (WTF::Internal::fastMallocValidationSuffix):
            (WTF::Internal::fastMallocMatchValidationType):
            (WTF::Internal::setFastMallocMatchValidationType):
            (WTF::fastMallocMatchValidateFree):
            (WTF::fastMallocValidate):
            * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef WTF_Platform_h
 #define WTF_Platform_h
+#define ENABLE_JIT 0
 
 /* ==== PLATFORM handles OS, operating environment, graphics API, and
    CPU. This macro will be phased out in favor of platform adaptation
@@ -876,8 +877,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* fastMalloc match validation allows for runtime verification that
    new is matched by delete, fastMalloc is matched by fastFree, etc. */
-#if !defined(ENABLE_FAST_MALLOC_MATCH_VALIDATION)
-#define ENABLE_FAST_MALLOC_MATCH_VALIDATION 0
+#if !defined(ENABLE_WTF_MALLOC_VALIDATION)
+#ifndef NDEBUG
+#define ENABLE_WTF_MALLOC_VALIDATION 1
+#else
+#define ENABLE_WTF_MALLOC_VALIDATION 0
+#endif
 #endif
 
 #if !defined(ENABLE_ICONDATABASE)
