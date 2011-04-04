7fe4db61c2b4970b70641210094c72f8e1c1f093
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 14:45:53 +0000

    Changes the HistoryURLProvider such that if the first pass produced an inline autocomplete match we don't attempt to fixup the 'what you typed match' in the second pass. The problem scenario is a url you've typed like 'pandora.com' and a url you haven't typed, like 'p'. If this happens, the first pass produces 'pandora.com' with a relevancy of 1400, but the second pass produce 'p' with 1200, and 'pandora.com' would get a much lower relevancy. If the SearchProvider also provided an inline autocomplete match > 1200, then the omnibox flickers between the results.
    TEST=covered by unit test
    BUG=77677
    R=pkasting@chromium.org
    
    Review URL: http://codereview.chromium.org/6749039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80311 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_url_provider.h
+++ chrome/browser/autocomplete/history_url_provider.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -121,6 +121,9 @@ struct HistoryURLProviderParams {
 
   // Languages we should pass to gfx::GetCleanStringFromUrl.
   std::string languages;
+
+  // When true, we should avoid calling SuggestExactInput().
+  bool dont_suggest_exact_input;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HistoryURLProviderParams);
