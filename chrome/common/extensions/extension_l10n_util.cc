42604c7a788ef28b1b7a43a7bff028b92ad695e5
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 14:15:38 +0000

    Remove unneeded headers from l10n_util. Review URL: http://codereview.chromium.org/6793012
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_l10n_util.cc
+++ chrome/common/extensions/extension_l10n_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/extensions/extension_l10n_util.h"
 
+#include <algorithm>
 #include <set>
 #include <string>
 #include <vector>
--- ui/base/l10n/l10n_util.h
+++ ui/base/l10n/l10n_util.h
@@ -10,15 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_L10N_L10N_UTIL_H_
 #pragma once
 
-#include <algorithm>
-#include <functional>
 #include <string>
 #include <vector>
 
-#include "build/build_config.h"
-
-#include "base/basictypes.h"
-#include "base/memory/scoped_ptr.h"
 #include "base/string16.h"
 #include "base/string_util.h"
 
