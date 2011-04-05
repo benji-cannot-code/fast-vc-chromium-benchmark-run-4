86fb352e4ca9394c48f57ca18f973866043e85b4
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 20:03:31 +0000

    PrintPreview: [MAC] Set the specified orientation in the print ticket.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6764006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80514 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/printing_context_mac.mm
+++ printing/printing_context_mac.mm
@@ -101,8 +101,6 @@ PrintingContext::Result PrintingContextMac::UpdatePrintSettings(
     return OnError();
   }
 
-  settings_.SetOrientation(landscape);
-
   if (!SetPrinter(printer_name))
     return OnError();
 
@@ -110,6 +108,9 @@ PrintingContext::Result PrintingContextMac::UpdatePrintSettings(
     return OnError();
 
   if (!SetCollateInPrintSettings(collate))
+    return OnError();
+
+  if (!SetOrientationIsLandscape(landscape))
     return OnError();
 
   [print_info_.get() updateFromPMPrintSettings];
@@ -158,6 +159,19 @@ bool PrintingContextMac::SetCollateInPrintSettings(bool collate) {
   PMPrintSettings pmPrintSettings =
       static_cast<PMPrintSettings>([print_info_.get() PMPrintSettings]);
   return PMSetCollate(pmPrintSettings, collate) == noErr;
+}
+
+bool PrintingContextMac::SetOrientationIsLandscape(bool landscape) {
+  PMPageFormat page_format =
+      static_cast<PMPageFormat>([print_info_.get() PMPageFormat]);
+
+  PMOrientation orientation = landscape ? kPMLandscape : kPMPortrait;
+
+  if (PMSetOrientation(page_format, orientation, false) != noErr)
+    return false;
+
+  [print_info_.get() updateFromPMPageFormat];
+  return true;
 }
 
 void PrintingContextMac::ParsePrintInfo(NSPrintInfo* print_info) {
