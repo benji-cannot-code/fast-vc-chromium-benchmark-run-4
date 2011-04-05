78660818e2cd9207b30db5fdb4e784f41a52e932
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 23:09:10 +0000

    Make BookmarkModelVerifier wait for favicon load during GetFavicon
    The test case for bookmark favicon sync was failing in its verification
    step because GetFavicon sometimes returned a blank favicon for a URL.
    This was because GetFavicon can be asynchronous if the favicon of a
    bookmark node is not already loaded.
    
    This patch modifies BookmarkModelVerifier::FaviconsMatch and wraps
    BookmarkModel::GetFavicon with an additional wait on favicon load,
    but only if a favicon was set for that particular URL.
    
    BUG=69694
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/6760033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80540 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/live_bookmarks_sync_test.cc
+++ chrome/test/live_sync/live_bookmarks_sync_test.cc
@@ -183,8 +183,8 @@ bool LiveBookmarksSyncTest::ModelMatchesVerifier(int profile) {
                << "DisableVerifier(). Use ModelsMatch() instead.";
     return false;
   }
-  return BookmarkModelVerifier::ModelsMatch(
-      GetVerifierBookmarkModel(), GetBookmarkModel(profile));
+  return verifier_helper_->ModelsMatch(GetVerifierBookmarkModel(),
+                                       GetBookmarkModel(profile));
 }
 
 bool LiveBookmarksSyncTest::AllModelsMatchVerifier() {
@@ -203,8 +203,8 @@ bool LiveBookmarksSyncTest::AllModelsMatchVerifier() {
 }
 
 bool LiveBookmarksSyncTest::ModelsMatch(int profile_a, int profile_b) {
-  return BookmarkModelVerifier::ModelsMatch(
-      GetBookmarkModel(profile_a), GetBookmarkModel(profile_b));
+  return verifier_helper_->ModelsMatch(GetBookmarkModel(profile_a),
+                                       GetBookmarkModel(profile_b));
 }
 
 bool LiveBookmarksSyncTest::AllModelsMatch() {
@@ -218,7 +218,7 @@ bool LiveBookmarksSyncTest::AllModelsMatch() {
 }
 
 bool LiveBookmarksSyncTest::ContainsDuplicateBookmarks(int profile) {
-  return BookmarkModelVerifier::ContainsDuplicateBookmarks(
+  return verifier_helper_->ContainsDuplicateBookmarks(
       GetBookmarkModel(profile));
 }
 
