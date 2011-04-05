613c2f59ef2e83e91bad77cf247a4f4cd1e3f92d
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 11:31:42 +0000

    Read ChromeOS version numbers from file before sandboxing and cache them.
    BUG=chromiumos-os:11573
    TEST=Check that user agent in javascript contains correct CrOS version.
    Review URL: http://codereview.chromium.org/6771012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
+#include "base/sys_info.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #endif
 
@@ -739,6 +740,16 @@ int ChromeMain(int argc, char** argv) {
 
   if (!process_type.empty())
     CommonSubprocessInit(process_type);
+
+#if defined(OS_CHROMEOS)
+  {
+    // Read and cache ChromeOS version from file,
+    // to be used from inside the sandbox.
+    int32 major_version, minor_version, bugfix_version;
+    base::SysInfo::OperatingSystemVersionNumbers(
+        &major_version, &minor_version, &bugfix_version);
+  }
+#endif
 
   // Initialize the sandbox for this process.
   SandboxInitWrapper sandbox_wrapper;
