f73497e2fe374e9b84ceedef22c8092d1fb217ed
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 07:13:21 +0000

    Transform GdkEvents for simpler MouseEvent construction. Handle MouseWheelEvent in Widget::OnMouseEvent.
    BUG=72040
    TEST=Mouse interaction on linux_views
    
    Review URL: http://codereview.chromium.org/6720025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80420 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/widget.cc
+++ views/widget/widget.cc
@@ -354,6 +354,9 @@ bool Widget::OnMouseEvent(const MouseEvent& event) {
       last_mouse_event_was_move_ = false;
       GetRootView()->OnMouseExited(event);
       return false;
+    case ui::ET_MOUSEWHEEL:
+      return GetRootView()->OnMouseWheel(
+          reinterpret_cast<const MouseWheelEvent&>(event));
     default:
       return false;
   }
@@ -365,7 +368,6 @@ void Widget::OnMouseCaptureLost() {
     GetRootView()->OnMouseCaptureLost();
   is_mouse_button_pressed_ = false;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Widget, FocusTraversable implementation:
