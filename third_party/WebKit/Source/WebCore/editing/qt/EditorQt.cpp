0675beff956c5a338d19bd4395c889444669946b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-05 16:18:43 +0000

    2011-04-05  Anders Bakken  <agbakken@gmail.com>
            Reviewed by Laszlo Gombos.
    
            [Qt] EditorQt.cpp has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=40260
    
            This patch does not require new test cases.
    
            * editing/qt/EditorQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/qt/EditorQt.cpp
+++ third_party/WebKit/Source/WebCore/editing/qt/EditorQt.cpp
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ClipboardQt.h"
 #include "Document.h"
 #include "Element.h"
-#include "VisibleSelection.h"
 #include "SelectionController.h"
 #include "TextIterator.h"
+#include "VisibleSelection.h"
 #include "htmlediting.h"
 #include "visible_units.h"
 
