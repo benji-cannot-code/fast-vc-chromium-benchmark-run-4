cf3d81b50eed2e34c7625acc6b6ef510d933be2f
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 19:13:50 +0000

    Clean up code in the ChromeFrame test COM message filter by detecting whether IE is hung in the WM_TIMER handler, instead of peeking with PM_NOREMOVE to detect the same.
    BUG=none
    TEST=ChromeFrame tests.
    TBR=amit
    Review URL: http://codereview.chromium.org/6693086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_test_utils.h
+++ chrome_frame/test/chrome_frame_test_utils.h
@@ -142,8 +142,7 @@ class HungCOMCallDetector
                                     DWORD tick_count,
                                     DWORD pending_type) {
     MSG msg = {0};
-    if (PeekMessage(&msg, m_hWnd, WM_TIMER, WM_TIMER, PM_NOREMOVE)) {
-      is_hung_ = true;
+    if (is_hung_) {
       return PENDINGMSG_CANCELCALL;
     }
     return PENDINGMSG_WAITDEFPROCESS;
@@ -154,6 +153,7 @@ class HungCOMCallDetector
   }
 
   LRESULT OnTimer(UINT msg, WPARAM wp, LPARAM lp, BOOL& handled) {  // NOLINT
+    is_hung_ = true;
     return 1;
   }
 
@@ -177,7 +177,7 @@ class HungCOMCallDetector
     }
 
     static const int kHungDetectTimerId = 0x0000baba;
-    SetTimer(kHungDetectTimerId, 1000 * (timeout_seconds + 20), NULL);
+    SetTimer(kHungDetectTimerId, 1000 * (timeout_seconds + 40), NULL);
     return S_OK;
   }
 
