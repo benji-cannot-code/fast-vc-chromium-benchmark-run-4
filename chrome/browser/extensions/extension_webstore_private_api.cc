4ab9bc1cf44145130ae8193540152e168aca08ad
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 23:14:26 +0000

    Fix Clang style errors.
    BUG=none
    TEST=none
    
    TBR=erg@chromium.org
    Review URL: http://codereview.chromium.org/6793053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webstore_private_api.cc
+++ chrome/browser/extensions/extension_webstore_private_api.cc
@@ -269,6 +269,10 @@ class SafeBeginInstallHelper : public UtilityProcessHost::Client {
   BeginInstallWithManifestFunction::ResultCode parse_error_;
 };
 
+BeginInstallWithManifestFunction::BeginInstallWithManifestFunction() {}
+
+BeginInstallWithManifestFunction::~BeginInstallWithManifestFunction() {}
+
 bool BeginInstallWithManifestFunction::RunImpl() {
   if (!IsWebStoreURL(profile_, source_url())) {
     SetResult(PERMISSION_DENIED);
