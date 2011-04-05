4c337725ecda4b9ff64ac991cbb385ba60772d7b
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-05 05:26:51 +0000

    2011-04-04  Kent Tamura  <tkent@chromium.org>
            Add a case label which was unexpectedly removed in r82899.
            https://bugs.webkit.org/show_bug.cgi?id=50661
    
            * css/CSSSelector.cpp:
            (WebCore::CSSSelector::extractPseudoType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82904 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSSelector.cpp
@@ -447,6 +447,7 @@ void CSSSelector::extractPseudoType() const
     case PseudoInputSpeechButton:
 #endif
     case PseudoInnerSpinButton:
+    case PseudoOuterSpinButton: 
     case PseudoResizer:
     case PseudoScrollbar:
     case PseudoScrollbarCorner:
