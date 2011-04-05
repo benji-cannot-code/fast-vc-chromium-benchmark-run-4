ba65a0faeeeba95de7e6a0d97556ae88610f221d
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 19:04:21 +0000

    Various fixes to gpu tracing UI
    - Use hidden instead of css style to hide things.
    - Add keyboard short help to summary when there is no selection.
    - For summation, report total per group as well as total across all slices
    - Fix stalling during mousemove by not showing duration text. Not best fix but better than nothing
    - Uniquely generate slice colors based on string hashcode.
    
    Review URL: http://codereview.chromium.org/6792039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gpu_internals/info_view.js
+++ chrome/browser/resources/gpu_internals/info_view.js
@@ -133,7 +133,7 @@ cr.define('gpu', function() {
               link.textContent = bugid;
               link.href = 'http://crbug.com/' + bugid;
               reasonEl.appendChild(link);
-              nreasons ++;
+              nreasons++;
             }
 
             for (j = 0; j < reason.webkit_bugs.length; ++j) {
@@ -149,7 +149,7 @@ cr.define('gpu', function() {
 
               link.href = 'https://bugs.webkit.org/show_bug.cgi?id=' + bugid;
               reasonEl.appendChild(link);
-              nreasons ++;
+              nreasons++;
             }
 
             reasonsEl.appendChild(reasonEl);
