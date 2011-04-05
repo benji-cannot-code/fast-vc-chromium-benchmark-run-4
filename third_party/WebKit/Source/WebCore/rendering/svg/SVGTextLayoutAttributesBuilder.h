cb0f28785de9259ebdd963eef2c3a5bf1c65ae82
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-05 16:38:10 +0000

    2011-04-05  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Adam Roben & Dirk Schulze.
    
            text-tspan-02-b.svg from SVG 1.1 2nd edition fails
            https://bugs.webkit.org/show_bug.cgi?id=57831
    
            Add text-tspan-02-b.svg from SVG 1.1 2nd edition, which was buggy.
            Add reduced testcase, which exposes a bug with midpoint splitting, which is now fixed.
    
            * platform/mac/svg/W3C-SVG-1.1-SE/text-tspan-02-b-expected.checksum: Added.
            * platform/mac/svg/W3C-SVG-1.1-SE/text-tspan-02-b-expected.png: Added.
            * platform/mac/svg/W3C-SVG-1.1-SE/text-tspan-02-b-expected.txt: Added.
            * platform/mac/svg/text/text-midpoint-split-bug-expected.checksum: Added.
            * platform/mac/svg/text/text-midpoint-split-bug-expected.png: Added.
            * platform/mac/svg/text/text-midpoint-split-bug-expected.txt: Added.
            * svg/W3C-SVG-1.1-SE/text-tspan-02-b.svg: Added.
            * svg/text/text-midpoint-split-bug.svg: Added.
    
    2011-04-05  Nikolas Zimmermann  <nzimmermann@rim.com>
    
            Reviewed by Adam Roben & Dirk Schulze.
    
            text-tspan-02-b.svg from SVG 1.1 2nd edition fails
            https://bugs.webkit.org/show_bug.cgi?id=57831
    
            We were not spanning rotation values correcty accross text children, which resulted in text-span-02-b.svg misrendering.
            While rewriting the rotation specific code, it came apparent that the LayoutScope concept is flawed in general and
            should be replaced by a global list of x/y/dx/dy/rotate values for the whole text subtree. This makes it much simpler
            to implement the demand that the last rotation value spans the whole scope. A follow-up patch will optimize the
            required memory.
    
            Example:
            <text rotate="10,20,999,50">AB<tspan rotate="30">CDE</>FGH</text>
        
            text:   A  B  C  D  E  F  G  H
            rotate="10 20 30 30 30 50 50 50"
    
            Tests: svg/W3C-SVG-1.1-SE/text-tspan-02-b.svg
                   svg/text/text-midpoint-split-bug.svg
    
            * rendering/RenderBlockLineLayout.cpp:     Move SVG specific midpoint creation after the non-SVG specific code that may mutate the midpoints. otherwhise we end up in an
            (WebCore::RenderBlock::findNextLineBreak): inconsistent state. We were intereferring with the ignoreSpaces code resulting in wrong truncations - covered by new testcase.
            * rendering/svg/SVGTextLayoutAttributes.cpp:
            (WebCore::SVGTextLayoutAttributes::reserveCapacity): Also reserveCapacity for the text metrics list (minor optimization).
            (WebCore::SVGTextLayoutAttributes::fillWithEmptyValue): Added helper function for use from SVGTextLayoutAttributesBuilder.
            (WebCore::SVGTextLayoutAttributes::appendEmptyValue): Ditto.
            (WebCore::safeValueAtPosition): Ditto. (Inline helper)
            (WebCore::SVGTextLayoutAttributes::appendSingleValueFromAttribute): Ditto.
            * rendering/svg/SVGTextLayoutAttributes.h: Ditto.
            * rendering/svg/SVGTextLayoutAttributesBuilder.cpp: Rewrite code, see explaination above.
            (WebCore::SVGTextLayoutAttributesBuilder::buildLayoutAttributesForTextSubtree):
            (WebCore::processRenderSVGInlineText):
            (WebCore::SVGTextLayoutAttributesBuilder::collectTextPositioningElements):
            (WebCore::SVGTextLayoutAttributesBuilder::buildLayoutAttributesForAllCharacters):
            (WebCore::SVGTextLayoutAttributesBuilder::propagateLayoutAttributes):
            (WebCore::fillListAtPosition):
            (WebCore::SVGTextLayoutAttributesBuilder::fillAttributesAtPosition):
            * rendering/svg/SVGTextLayoutAttributesBuilder.h:
            (WebCore::SVGTextLayoutAttributesBuilder::TextPosition::TextPosition):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@82947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/SVGTextLayoutAttributesBuilder.h
+++ third_party/WebKit/Source/WebCore/rendering/svg/SVGTextLayoutAttributesBuilder.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) Research In Motion Limited 2010. All rights reserved.
+ * Copyright (C) Research In Motion Limited 2010-2011. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -29,6 +29,7 @@ namespace WebCore {
 
 class RenderObject;
 class RenderSVGText;
+class SVGTextPositioningElement;
 
 // SVGTextLayoutAttributesBuilder performs the first layout phase for SVG text.
 //
@@ -46,37 +47,27 @@ public:
     void buildLayoutAttributesForTextSubtree(RenderSVGText*);
 
 private:
-    struct LayoutScope {
-        LayoutScope()
-            : textContentStart(0)
-            , textContentLength(0)
+    struct TextPosition {
+        TextPosition(SVGTextPositioningElement* newElement = 0, unsigned newStart = 0, unsigned newLength = 0)
+            : element(newElement)
+            , start(newStart)
+            , length(newLength)
         {
         }
 
-        unsigned textContentStart;
-        unsigned textContentLength;
-        SVGTextLayoutAttributes attributes;
+        SVGTextPositioningElement* element;
+        unsigned start;
+        unsigned length;
     };
 
-    void buildLayoutScope(LayoutScope&, RenderObject*, unsigned textContentStart, unsigned textContentLength) const;
-    void buildLayoutScopes(RenderObject*, unsigned& atCharacter, UChar& lastCharacter);
-    void buildOutermostLayoutScope(RenderSVGText*, unsigned textLength);
+    void collectTextPositioningElements(RenderObject*, unsigned& atCharacter, UChar& lastCharacter);
+    void buildLayoutAttributesForAllCharacters(RenderSVGText*, unsigned textLength);
     void propagateLayoutAttributes(RenderObject*, Vector<SVGTextLayoutAttributes>& allAttributes, unsigned& atCharacter, UChar& lastCharacter) const;
-
-    enum LayoutValueType {
-        XValueAttribute,
-        YValueAttribute,
-        DxValueAttribute,
-        DyValueAttribute,
-        RotateValueAttribute
-    };
-
-    float nextLayoutValue(LayoutValueType, unsigned atCharacter) const;
-    void assignLayoutAttributesForCharacter(SVGTextLayoutAttributes&, SVGTextMetrics&, unsigned valueListPosition) const;
-    void assignEmptyLayoutAttributesForCharacter(SVGTextLayoutAttributes&) const;
+    void fillAttributesAtPosition(const TextPosition&);
 
 private:
-    Vector<LayoutScope> m_scopes;
+    Vector<TextPosition> m_textPositions;
+    SVGTextLayoutAttributes::PositioningLists m_positioningLists;
 };
 
 } // namespace WebCore
