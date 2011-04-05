bbaf100198fe61772b9705f742fbe26ee9359bec
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 19:05:50 +0000

    Fix valgrind messages for uninitialized preload_ value.
    The preload_ field was uninitialized when PipelineImpl was constructed, so
    PipelineImpl unit tests failed Valgrind tests. CL 80465 introduced the problem.
    
    BUG=NONE
    TEST=NONE
    TBR=acolwell
    
    Review URL: http://codereview.chromium.org/6720052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80503 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_impl_unittest.cc
+++ media/base/pipeline_impl_unittest.cc
@@ -101,6 +101,7 @@ class PipelineImplTest : public ::testing::Test {
     mocks_->demuxer()->SetTotalAndBufferedBytesAndDuration(
         kTotalBytes, kBufferedBytes, duration);
     EXPECT_CALL(*mocks_->demuxer(), SetPlaybackRate(0.0f));
+    EXPECT_CALL(*mocks_->demuxer(), SetPreload(AUTO));
     EXPECT_CALL(*mocks_->demuxer(), Seek(base::TimeDelta(), NotNull()))
         .WillOnce(Invoke(&RunFilterCallback));
     EXPECT_CALL(*mocks_->demuxer(), Stop(NotNull()))
@@ -851,4 +852,3 @@ TEST(PipelineStatusNotificationTest, DelayedCallback) {
 }
 
 }  // namespace media
-
