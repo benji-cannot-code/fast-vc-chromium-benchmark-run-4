18b21a83db101ba56d8caa48dc8eb9632b6de8ec
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-06 15:47:57 +0000

    Move JavaScriptCoreGenerated's file-copying logic out to a new script
    Hopefully this will make it easier to modify this logic in the future. I also made the
    script much quieter than the old logic, since it didn't seem helpful to see long lists of
    filenames during the copying phase.
    
    If we like this new style, we could copy it for our other projects.
    
    Fixes <http://webkit.org/b/57950> JavaScriptCoreGenerated's file-copying logic is hard to
    modify and noisy
    
    Reviewed by Steve Falkenburg.
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCoreGenerated.make: Moved logic to copy
    files from here...
    * JavaScriptCore.vcproj/JavaScriptCore/copy-files.cmd: ...to here. (Added.)
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCoreGenerated.vcproj: Added copy-files.cmd
    for convenience.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/copy-files.cmd
@@ -0,0 +1,80 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@echo off
+
+set PublicHeadersDirectory=%CONFIGURATIONBUILDDIR%\include\JavaScriptCore
+set PrivateHeadersDirectory=%CONFIGURATIONBUILDDIR%\include\private\JavaScriptCore
+set ResourcesDirectory=%CONFIGURATIONBUILDDIR%\bin\JavaScriptCore.resources
+
+if "%1" EQU "clean" goto :clean
+if "%1" EQU "rebuild" call :clean
+
+echo Copying public headers...
+mkdir "%PublicHeadersDirectory%" 2>NUL
+for %%f in (
+    APICast.h
+    APIShims.h
+    JSBase.h
+    JSContextRef.h
+    JSContextRefPrivate.h
+    JSObjectRef.h
+    JSObjectRefPrivate.h
+    JSRetainPtr.h
+    JSRetainPtr.h
+    JSStringRef.h
+    JSStringRefBSTR.h
+    JSStringRefCF.h
+    JSValueRef.h
+    JSWeakObjectMapRefInternal.h
+    JSWeakObjectMapRefPrivate.h
+    JavaScript.h
+    JavaScriptCore.h
+    OpaqueJSString.h
+    WebKitAvailability.h
+) do (
+    xcopy /y /d ..\..\API\%%f "%PublicHeadersDirectory%" >NUL
+)
+
+echo Copying private headers...
+mkdir "%PrivateHeadersDirectory%" 2>NUL
+for %%d in (
+    assembler
+    bytecode
+    collector\handles
+    debugger
+    interpreter
+    jit
+    parser
+    profiler
+    runtime
+    wtf
+    wtf\text
+    wtf\unicode
+    wtf\unicode\icu
+    yarr
+) do (
+    xcopy /y /d ..\..\%%d\*.h "%PrivateHeadersDirectory%" >NUL
+)
+
+echo Copying resources...
+mkdir "%ResourcesDirectory%" 2>NUL
+xcopy /y /d ..\JavaScriptCore.resources\* "%ResourcesDirectory%" >NUL
+
+echo Copying other files...
+for %%f in (
+    create_hash_table
+    wtf\text\AtomicString.cpp
+    wtf\text\StringBuilder.cpp
+    wtf\text\StringImpl.cpp
+    wtf\text\WTFString.cpp
+) do (
+    xcopy /y /d ..\..\%%f "%PrivateHeadersDirectory%" >NUL
+)
+
+goto :EOF
+
+:clean
+
+echo Deleting copied files...
+del /s /q "%PublicHeadersDirectory%" >NUL
+del /s /q "%PrivateHeadersDirectory%" >NUL
+del /s /q "%ResourcesDirectory%" >NUL
