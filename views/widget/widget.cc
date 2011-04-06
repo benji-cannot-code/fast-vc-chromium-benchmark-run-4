5b52d85ff77130751d30fb71eac7d2f09452249b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 21:34:44 +0000

    RTL window styles was being set on too many HWNDs. This inverts the default. There may be cases where the window styles are necessary however. Those I do not know and am going to rely on RTL QA to tell me about.
    http://crbug.com/77166
    TEST=start chrome with --lang=he, content area should render and not be inverted, wrench menu should not be inverted. This change may cause some HWNDs to be incorrectly not flipped. Please let me know about those cases.
    Review URL: http://codereview.chromium.org/6804027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80701 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/widget.cc
+++ views/widget/widget.cc
@@ -27,7 +27,7 @@ Widget::CreateParams::CreateParams()
       can_activate(true),
       keep_on_top(false),
       delete_on_destroy(true),
-      mirror_origin_in_rtl(true),
+      mirror_origin_in_rtl(false),
       has_dropshadow(false),
       native_widget(NULL) {
 }
@@ -40,7 +40,7 @@ Widget::CreateParams::CreateParams(Type type)
       can_activate(type != TYPE_POPUP && type != TYPE_MENU),
       keep_on_top(type == TYPE_MENU),
       delete_on_destroy(true),
-      mirror_origin_in_rtl(true),
+      mirror_origin_in_rtl(false),
       has_dropshadow(false),
       native_widget(NULL) {
 }
