e061f3eaa29c27ecff7587e471d86f414bccce9f
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 22:07:35 +0000

    Refactor ExtensionService/ExtensionServiceInterface to be more testable
    In particular:
    - Add various status accessors to ExtensionServiceInterface (e.g.,
      IsExtensionEnabled())
    - Convert IsIncognitoEnabled() to take an ID instead of an Extension*.
    - Add CheckForUpdates() to ExtensionServiceInterface.
    - Remove various unneeded accessors from ExtensionServiceInterface.
    
    Rewrite some sync utility functions to take advantage of the new
    interface (although it will be rewritten more in a future CL).
    
    BUG=77995
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=80539
    
    Review URL: http://codereview.chromium.org/6720042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_updater.h
+++ chrome/browser/extensions/extension_updater.h
@@ -26,9 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 
 class Extension;
+class ExtensionPrefs;
 class ExtensionUpdaterTest;
 class ExtensionUpdaterFileHandler;
 class PrefService;
+class Profile;
 
 // To save on server resources we can request updates for multiple extensions
 // in one manifest check. This class helps us keep track of the id's for a
@@ -103,7 +105,8 @@ class ManifestFetchData {
 // extensions and pending extensions.
 class ManifestFetchesBuilder {
  public:
-  explicit ManifestFetchesBuilder(ExtensionServiceInterface* service);
+  ManifestFetchesBuilder(ExtensionServiceInterface* service,
+                         ExtensionPrefs* prefs);
   ~ManifestFetchesBuilder();
 
   void AddExtension(const Extension& extension);
@@ -139,7 +142,8 @@ class ManifestFetchesBuilder {
                         Extension::Type extension_type,
                         GURL update_url,
                         const std::string& update_url_data);
-  ExtensionServiceInterface* service_;
+  ExtensionServiceInterface* const service_;
+  ExtensionPrefs* const prefs_;
 
   // List of data on fetches we're going to do. We limit the number of
   // extensions grouped together in one batch to avoid running into the limits
@@ -168,7 +172,9 @@ class ExtensionUpdater
   // extensions and installing updated ones. The |frequency_seconds| parameter
   // controls how often update checks are scheduled.
   ExtensionUpdater(ExtensionServiceInterface* service,
+                   ExtensionPrefs* extension_prefs,
                    PrefService* prefs,
+                   Profile* profile,
                    int frequency_seconds);
 
   // Starts the updater running.  Should be called at most once.
@@ -323,7 +329,9 @@ class ExtensionUpdater
   base::OneShotTimer<ExtensionUpdater> timer_;
   int frequency_seconds_;
 
+  ExtensionPrefs* extension_prefs_;
   PrefService* prefs_;
+  Profile* profile_;
 
   scoped_refptr<ExtensionUpdaterFileHandler> file_handler_;
   bool blacklist_checks_enabled_;
