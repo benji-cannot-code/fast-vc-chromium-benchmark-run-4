2d7b5d16361535ad091d7d57afa8623b2209bf36
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-06 16:09:19 +0000

    2011-04-06  Liang Qi  <liang.qi@nokia.com>
            Reviewed by Benjamin Poulain.
    
            Correct a include file name.
            https://bugs.webkit.org/show_bug.cgi?id=57839
    
            * wtf/PageAllocatorSymbian.h: It should be case sensitive. This fix
            builds on Unix hosts.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/PageAllocatorSymbian.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/PageAllocatorSymbian.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PageAllocatorSymbian_h
 
 #include <e32std.h>
-#include <wtf/BitMap.h>
+#include <wtf/Bitmap.h>
 
 namespace WTF { 
 
