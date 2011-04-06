6f007f0ed435aa80fe0b7e91e093bf954ea31d1c
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 01:22:27 +0000

    [Mac] Don't route mouse events to the render view host if window is not frontmost.
    This change makes Mac Chrome match the behaviour of other browsers on the Mac
    (tested Firefox and Safari) as well as other Mac applications such as iTunes.
    
    Additionally, this fixes a number of bugs, including:
    - Mouse cursor changing when hovering over web content in background windows
    yet clicks in this case would be eaten to bring the window to the front.
    - Mouse cursor NOT changing when hovering web content in background windows when
    Chrome is not the active application - resulting in inconsistent behaviour.
    - http://crbug.com/77928 - resulting in wrong cursor persisting after moving out
    of the web content area of a background window.
    
    Also, change related unit tests to make the test window the keyWindow so that the
    tests continue to pass.
    
    BUG=77928
    TEST=Open two windows, A and B. Open a webpage in A, then make B the foreground
    window. Now, hover the cursor over web content in A. The cursor should not
    change and mouse-over actions should not occur.
    R=mark@chromium.org
    
    Review URL: http://codereview.chromium.org/6720023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_mac_unittest.mm
+++ chrome/browser/renderer_host/render_widget_host_view_mac_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -142,11 +142,13 @@ TEST_F(RenderWidgetHostViewMacTest, AcceptsFirstResponder) {
 }
 
 TEST_F(RenderWidgetHostViewMacTest, TakesFocusOnMouseDown) {
-  scoped_nsobject<NSWindow> window([[CocoaTestHelperWindow alloc] init]);
+  scoped_nsobject<CocoaTestHelperWindow>
+      window([[CocoaTestHelperWindow alloc] init]);
   [[window contentView] addSubview:rwhv_cocoa_.get()];
 
   // Even if the RWHVCocoa disallows first responder, clicking on it gives it
   // focus.
+  [window setPretendIsKeyWindow:YES];
   [window makeFirstResponder:nil];
   ASSERT_NE(rwhv_cocoa_.get(), [window firstResponder]);
 
@@ -173,8 +175,10 @@ TEST_F(RenderWidgetHostViewMacTest, TakesFocusOnMouseDownWithAcceleratedView) {
   EXPECT_FALSE([accelerated_view isHidden]);
 
   // Add the RWHVCocoa to the window and remove first responder status.
-  scoped_nsobject<NSWindow> window([[CocoaTestHelperWindow alloc] init]);
+  scoped_nsobject<CocoaTestHelperWindow>
+      window([[CocoaTestHelperWindow alloc] init]);
   [[window contentView] addSubview:rwhv_cocoa_.get()];
+  [window setPretendIsKeyWindow:YES];
   [window makeFirstResponder:nil];
   EXPECT_NE(rwhv_cocoa_.get(), [window firstResponder]);
 
