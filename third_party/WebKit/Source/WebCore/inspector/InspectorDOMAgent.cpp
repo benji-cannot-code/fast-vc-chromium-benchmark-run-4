38aaf70be8499ac04a57b3e47c9a5859aa159679
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-06 09:57:54 +0000

    2011-04-05  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Range insertNode function does not update content in the inspector
            https://bugs.webkit.org/show_bug.cgi?id=57829
    
            * inspector/elements/modify-chardata-expected.txt:
            * inspector/elements/modify-chardata.html:
    2011-04-05  Alexander Pavlov  <apavlov@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: Range insertNode function does not update content in the inspector
            https://bugs.webkit.org/show_bug.cgi?id=57829
    
            * inspector/InspectorDOMAgent.cpp:
            (WebCore::InspectorDOMAgent::pushChildNodesToFrontend):
            (WebCore::InspectorDOMAgent::buildArrayForContainerChildren):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.cpp
@@ -457,7 +457,6 @@ void InspectorDOMAgent::pushChildNodesToFrontend(int nodeId)
 
     NodeToIdMap* nodeMap = m_idToNodesMap.get(nodeId);
     RefPtr<InspectorArray> children = buildArrayForContainerChildren(node, 1, nodeMap);
-    m_childrenRequested.add(nodeId);
     m_frontend->setChildNodes(nodeId, children.release());
 }
 
@@ -1091,13 +1090,14 @@ PassRefPtr<InspectorArray> InspectorDOMAgent::buildArrayForContainerChildren(Nod
     Node* child = innerFirstChild(container);
 
     if (depth == 0) {
-        // Special case the_only text child.
+        // Special-case the only text child - pretend that container's children have been requested.
         if (child && child->nodeType() == Node::TEXT_NODE && !innerNextSibling(child))
-            children->pushObject(buildObjectForNode(child, 0, nodesMap));
+            return buildArrayForContainerChildren(container, 1, nodesMap);
         return children.release();
-    } else if (depth > 0) {
-        depth--;
     }
+
+    depth--;
+    m_childrenRequested.add(bind(container, nodesMap));
 
     while (child) {
         children->pushObject(buildObjectForNode(child, depth, nodesMap));
