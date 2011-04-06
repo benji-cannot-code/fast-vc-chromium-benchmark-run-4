3372d68705f953a398a029a8a3c3a9654116a9a4
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 18:15:00 +0000

    Build fix. (linux_views shouldn't deal with wmclass issues.)
    TBR=estade@
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/shell_integration_linux.cc
+++ chrome/browser/shell_integration_linux.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/build_config.h"
 #include "base/command_line.h"
 #include "base/eintr_wrapper.h"
 #include "base/environment.h"
@@ -426,10 +427,12 @@ std::string ShellIntegration::GetDesktopFileContents(
     }
   }
 
+#if defined(TOOLKIT_GTK)
   std::string wmclass = web_app::GetWMClassFromAppName(app_name);
   if (!wmclass.empty()) {
     output_buffer += StringPrintf("StartupWMClass=%s\n", wmclass.c_str());
   }
+#endif
 
   return output_buffer;
 }
