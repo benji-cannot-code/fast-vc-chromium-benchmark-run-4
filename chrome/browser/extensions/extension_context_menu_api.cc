eddbd14dc1acfaa958d45de2adc82d9655d6dc0e
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 21:02:51 +0000

    Add support for a "frame" context option to chrome.contextMenus.create/update.
    Also removes a couple of .html files from the "simple" context menu browser
    test, since they weren't needed.
    
    Includes documentation update for http://trac.webkit.org/changeset/83033.
    
    BUG=49783
    TEST=ExtensionContextMenuBrowserTest.Frames
    R=asargent@chromium.org
    
    Review URL: http://codereview.chromium.org/6799020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_context_menu_api.cc
+++ chrome/browser/extensions/extension_context_menu_api.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -66,6 +66,8 @@ bool ExtensionContextMenuFunction::ParseContexts(
       tmp_result.Add(ExtensionMenuItem::VIDEO);
     } else if (value == "audio") {
       tmp_result.Add(ExtensionMenuItem::AUDIO);
+    } else if (value == "frame") {
+      tmp_result.Add(ExtensionMenuItem::FRAME);
     } else {
       error_ = ExtensionErrorUtils::FormatErrorMessage(kInvalidValueError, key);
       return false;
---
+++ chrome/test/data/extensions/context_menus/frames/test.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.onload = function() {
+  chrome.contextMenus.create(
+      {"title":"Page item", contexts: ["page"]},
+      function() {
+        if (!chrome.extension.lastError) {
+          chrome.contextMenus.create(
+          {"title":"Frame item", contexts: ["frame"]},
+          function() {
+            if (!chrome.extension.lastError) {
+              chrome.test.sendMessage("created items");
+            }
+          });
+        }
+      });
+};
