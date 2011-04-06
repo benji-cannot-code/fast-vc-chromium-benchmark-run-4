44b09281833484abe36ce520359809c12884b2b7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 23:51:49 +0000

    importer: Get rid of ImporterList::GetSourceProfileNameAt method.
    This function is redundant as ImportList can return SourceProfile and we can get
    that information from there.
    
    Also, while I'm here, rename the data member |description| of SourceProfile
    struct to |importer_name|, so it's more clearer what it means.
    
    BUG=None
    TEST=None
    
    R=avi@chromium.org
    
    Review URL: http://codereview.chromium.org/6803025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/importer/import_progress_dialog_view.h
+++ chrome/browser/ui/views/importer/import_progress_dialog_view.h
@@ -35,7 +35,7 @@ class ImportProgressDialogView : public views::View,
                            uint16 items,
                            ImporterHost* importer_host,
                            ImporterObserver* importer_observer,
-                           const string16& source_profile,
+                           const string16& importer_name,
                            bool bookmarks_import);
   virtual ~ImportProgressDialogView();
 
--- chrome/browser/ui/webui/options/import_data_handler.cc
+++ chrome/browser/ui/webui/options/import_data_handler.cc
@@ -115,7 +115,7 @@ void ImportDataHandler::ImportData(const ListValue* args) {
                                         new ProfileWriter(profile), false);
   } else {
     LOG(WARNING) << "There were no settings to import from '"
-        << source_profile.description << "'.";
+        << source_profile.importer_name << "'.";
   }
 }
 
@@ -128,7 +128,7 @@ void ImportDataHandler::SourceProfilesLoaded() {
     uint16 browser_services = source_profile.services_supported;
 
     DictionaryValue* browser_profile = new DictionaryValue();
-    browser_profile->SetString("name", source_profile.description);
+    browser_profile->SetString("name", source_profile.importer_name);
     browser_profile->SetInteger("index", i);
     browser_profile->SetBoolean("history",
         (browser_services & importer::HISTORY) != 0);
