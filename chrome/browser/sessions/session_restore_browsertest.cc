6f3272f5882b4775771e90f826967b106589b41a
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 21:17:00 +0000

    Fix header include for reals.
    TBR=isherman@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6799023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80695 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/file_path.h"
-#include "chrome/browser/browser_list.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/tab_restore_service.h"
