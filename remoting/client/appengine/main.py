bed8c07353979671c13fd27928a3f7264e9a689d
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 01:22:44 +0000

    Update Chromoting app-engine to invoke sandboxed connections.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6758043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80733 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/appengine/main.py
+++ remoting/client/appengine/main.py
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
 
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 import logging
 import os
 
@@ -37,7 +41,10 @@ class ChromotingSessionHandler(webapp.RequestHandler):
       'hostname': self.request.get('hostname'),
       'username': users.get_current_user().email(),
       'hostjid': self.request.get('hostjid'),
+      'connect_method': self.request.get('connect_method'),
+      'insecure': self.request.get('insecure'),
       'xmpp_token': auth.GetXmppToken(),
+      'http_xmpp_proxy': auth.GetHttpXmppProxy(),
     }
     path = os.path.join(os.path.dirname(__file__), 'chromoting_session.html')
     self.response.out.write(template.render(path, template_params))
