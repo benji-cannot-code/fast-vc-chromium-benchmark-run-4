5270d03d75a395b09bf8071c6361f84e08855272
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-07 22:17:56 +0000

    2011-04-07  Martin Robinson  <mrobinson@igalia.com>
            Fix the WebKit2 GTK+ build for older versions of GTK+.
    
            * WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp: Include the GtkVersioning.h header.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QX11Info>
 #elif PLATFORM(GTK)
 #include <gdk/gdkx.h>
+#include <WebCore/GtkVersioning.h>
 #endif
 
 using namespace WebCore;
