c6401a2ae1e33f686d527c0359db91d5be524034
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 11:32:17 +0000

    Get rid of PrefService::GetMutableDictionary/GetMutableList
    BUG=77914
    TEST=none, trybots remain green
    
    Review URL: http://codereview.chromium.org/6720013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80762 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/host_zoom_map_unittest.cc
+++ content/browser/host_zoom_map_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/prefs/pref_service.h"
+#include "chrome/browser/prefs/scoped_user_pref_update.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/testing_profile.h"
 #include "content/browser/browser_thread.h"
@@ -63,9 +64,11 @@ TEST_F(HostZoomMapTest, LoadNoPrefs) {
 }
 
 TEST_F(HostZoomMapTest, Load) {
-  DictionaryValue* dict =
-      prefs_->GetMutableDictionary(prefs::kPerHostZoomLevels);
-  dict->SetWithoutPathExpansion(host_, Value::CreateDoubleValue(kZoomLevel));
+  {
+    DictionaryPrefUpdate update(prefs_, prefs::kPerHostZoomLevels);
+    DictionaryValue* dict = update.Get();
+    dict->SetWithoutPathExpansion(host_, Value::CreateDoubleValue(kZoomLevel));
+  }
   scoped_refptr<HostZoomMap> map(new HostZoomMap(&profile_));
   EXPECT_EQ(kZoomLevel, map->GetZoomLevel(url_));
 }
