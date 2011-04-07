9bec167abb7d287567713f5f64a078c988dd73ec
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-07 19:30:37 +0000

    2011-04-06  Jer Noble  <jer.noble@apple.com>
            Reviewed by Maciej Stachowiak.
    
            AVF: MediaPlayerPrivateAVFoundation never reaches playable state.
            https://bugs.webkit.org/show_bug.cgi?id=57962
    
            Add support for a new AVPlayerItem API which will notify clients when their
            seek completes.  This requires a new Notification type to be passed to the main
            thread in MediaPlayerPrivateAVFoundation.
    
            * platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp:
            (WebCore::MediaPlayerPrivateAVFoundation::seekCompleted): Added.
            (WebCore::MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification): Added two new
                overloaded functions which take a Notification; and a Notification::Type and boolean.
            (WebCore::MediaPlayerPrivateAVFoundation::dispatchNotification): Support new SeekCompleted
                Notification type.
            * platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.h:
            (WebCore::MediaPlayerPrivateAVFoundation::Notification::Notification): Added one new constructor.
            (WebCore::MediaPlayerPrivateAVFoundation::Notification::finished): Added ivar and accessor.
            * platform/graphics/avfoundation/MediaPlayerPrivateAVFoundationObjC.mm:
            (WebCore::MediaPlayerPrivateAVFoundationObjC::seekToTime): Call new AVPlayerItem API.
            (-[WebCoreAVFMovieObserver seekCompleted:]): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
@@ -569,6 +569,14 @@ void MediaPlayerPrivateAVFoundation::timeChanged(double time)
     }
 }
 
+void MediaPlayerPrivateAVFoundation::seekCompleted(bool finished)
+{
+    LOG(Media, "MediaPlayerPrivateAVFoundation::seekCompleted(%p) - finished = %d", this, finished);
+    
+    if (finished)
+        m_seekTo = invalidTime;
+}
+
 void MediaPlayerPrivateAVFoundation::didEnd()
 {
     // Hang onto the current time and use it as duration from now on since we are definitely at
@@ -632,12 +640,22 @@ void MediaPlayerPrivateAVFoundation::clearMainThreadPendingFlag()
 
 void MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification(Notification::Type type, double time)
 {
-    LOG(Media, "MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification(%p) - notification %d", this, static_cast<int>(type));
+    scheduleMainThreadNotification(Notification(type, time));
+}
+
+void MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification(Notification::Type type, bool finished)
+{
+    scheduleMainThreadNotification(Notification(type, finished));
+}
+
+void MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification(Notification notification)
+{
+    LOG(Media, "MediaPlayerPrivateAVFoundation::scheduleMainThreadNotification(%p) - notification %d", this, static_cast<int>(notification.type()));
     m_queueMutex.lock();
 
     // It is important to always process the properties in the order that we are notified, 
     // so always go through the queue because notifications happen on different threads.
-    m_queuedNotifications.append(Notification(type, time));
+    m_queuedNotifications.append(notification);
 
     bool delayDispatch = m_delayCallbacks || !isMainThread();
     if (delayDispatch && !m_mainThreadCallPending) {
@@ -716,6 +734,9 @@ void MediaPlayerPrivateAVFoundation::dispatchNotification()
         break;
     case Notification::PlayerTimeChanged:
         timeChanged(notification.time());
+        break;
+    case Notification::SeekCompleted:
+        seekCompleted(notification.finished());
         break;
     case Notification::AssetMetadataLoaded:
         metadataLoaded();
