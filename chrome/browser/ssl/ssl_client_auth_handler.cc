f0179921d0cc3e5cd54883138e3c41c3cdd23db9
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 19:50:08 +0000

    Fix DCHECK on multiple SSL client auths for the same request.
    BUG=78679
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6814009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_client_auth_handler.cc
+++ chrome/browser/ssl/ssl_client_auth_handler.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_view_host_delegate.h"
 #include "content/browser/renderer_host/render_view_host_notification_task.h"
 #include "content/browser/renderer_host/resource_dispatcher_host.h"
+#include "content/browser/renderer_host/resource_dispatcher_host_request_info.h"
 #include "net/url_request/url_request.h"
 
 SSLClientAuthHandler::SSLClientAuthHandler(
@@ -62,6 +63,12 @@ void SSLClientAuthHandler::DoCertificateSelected(net::X509Certificate* cert) {
   // certificate.
   if (request_) {
     request_->ContinueWithCertificate(cert);
+
+    ResourceDispatcherHostRequestInfo* info =
+        ResourceDispatcherHost::InfoForRequest(request_);
+    if (info)
+      info->set_ssl_client_auth_handler(NULL);
+
     request_ = NULL;
   }
 }
