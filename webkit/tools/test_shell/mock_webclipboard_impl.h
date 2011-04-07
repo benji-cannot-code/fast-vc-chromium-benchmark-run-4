608960c4144b4285e12bbbd0e09bf9b7cab74450
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 23:30:04 +0000

    Fix clang build error.
    The new WebImage member made clang consider MockWebClipboardImpl a complex
    class, so explicitly define a ctor/dtor in the .cc.
    
    BUG=none
    TEST=Compiles on clang.
    
    Review URL: http://codereview.chromium.org/6812034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80864 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/mock_webclipboard_impl.h
+++ webkit/tools/test_shell/mock_webclipboard_impl.h
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class MockWebClipboardImpl : public WebKit::WebClipboard {
  public:
+  MockWebClipboardImpl();
+  ~MockWebClipboardImpl();
+
   virtual bool isFormatAvailable(WebKit::WebClipboard::Format,
                                  WebKit::WebClipboard::Buffer);
 
