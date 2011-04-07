5df4968ef4f1b976db17430edf3c71fcd2a776b7
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 16:41:17 +0000

    Add a dcheck to make sure that RenderProcessHost::AllHostsIterator is called on the UI thread. Review URL: http://codereview.chromium.org/6720051
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/back_forward_menu_model_unittest.cc
+++ chrome/browser/ui/toolbar/back_forward_menu_model_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -55,6 +55,10 @@ class FaviconDelegate : public ui::MenuModelDelegate {
 
 class BackFwdMenuModelTest : public RenderViewHostTestHarness {
  public:
+  BackFwdMenuModelTest()
+      : ui_thread_(BrowserThread::UI, &message_loop_) {
+  }
+
   void ValidateModel(BackForwardMenuModel* model, int history_items,
                      int chapter_stops) {
     int h = std::min(BackForwardMenuModel::kMaxHistoryItems, history_items);
@@ -95,6 +99,8 @@ class BackFwdMenuModelTest : public RenderViewHostTestHarness {
     controller().GoForward();
     contents()->CommitPendingNavigation();
   }
+
+  BrowserThread ui_thread_;
 };
 
 TEST_F(BackFwdMenuModelTest, BasicCase) {
--- content/browser/site_instance_unittest.cc
+++ content/browser/site_instance_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/render_messages.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/testing_profile.h"
+#include "content/browser/browser_thread.h"
 #include "content/browser/browsing_instance.h"
 #include "content/browser/child_process_security_policy.h"
 #include "content/browser/content_browser_client.h"
@@ -51,7 +52,9 @@ class SiteInstanceTestBrowserClient : public content::ContentBrowserClient {
 
 class SiteInstanceTest : public testing::Test {
  public:
-  SiteInstanceTest() : old_browser_client_(NULL) {
+  SiteInstanceTest()
+      : ui_thread_(BrowserThread::UI, &message_loop_),
+        old_browser_client_(NULL) {
   }
 
   virtual void SetUp() {
@@ -65,6 +68,7 @@ class SiteInstanceTest : public testing::Test {
 
  private:
   MessageLoopForUI message_loop_;
+  BrowserThread ui_thread_;
 
   SiteInstanceTestBrowserClient browser_client_;
   content::ContentBrowserClient* old_browser_client_;
