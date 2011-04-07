9588249a5661bbe8180e84a380044597849fb02e
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 23:46:43 +0000

    Send the ref as part of the client-side phishing detection pingback.
    This will help determine whether differing scores on the same URL are related
    to data that is passed through the ref (as in Google Instant queries).  Also
    add a test for the url feature extractor to verify that the ref is not used to
    form any token features.
    
    BUG=none
    TEST=PhishingClassifierDelegateTest,PhishingUrlFeatureExtractorTest
    
    Review URL: http://codereview.chromium.org/6812009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate.cc
@@ -145,7 +145,7 @@ void PhishingClassifierDelegate::PageCaptured(const string16& page_text,
     return;
   }
   last_finished_load_id_ = render_view()->page_id();
-  last_finished_load_url_ = StripToplevelUrl();
+  last_finished_load_url_ = GetToplevelUrl();
   classifier_page_text_ = page_text;
   MaybeStartClassification();
 }
@@ -183,8 +183,8 @@ void PhishingClassifierDelegate::ClassificationDone(bool is_phishy,
       phishy_score));
 }
 
-GURL PhishingClassifierDelegate::StripToplevelUrl() {
-  return StripRef(render_view()->webview()->mainFrame()->url());
+GURL PhishingClassifierDelegate::GetToplevelUrl() {
+  return render_view()->webview()->mainFrame()->url();
 }
 
 void PhishingClassifierDelegate::MaybeStartClassification() {
@@ -221,9 +221,10 @@ void PhishingClassifierDelegate::MaybeStartClassification() {
     return;
   }
   // If the page id is unchanged, the toplevel URL should also be unchanged.
-  DCHECK_EQ(StripToplevelUrl(), last_finished_load_url_);
+  GURL stripped_last_load_url(StripRef(last_finished_load_url_));
+  DCHECK_EQ(StripRef(GetToplevelUrl()), stripped_last_load_url);
 
-  if (last_finished_load_url_ == last_url_sent_to_classifier_) {
+  if (stripped_last_load_url == StripRef(last_url_sent_to_classifier_)) {
     // We've already classified this toplevel URL, so this was likely an
     // in-page navigation or a subframe navigation.  The browser should not
     // send a StartPhishingDetection IPC in this case.
@@ -232,9 +233,11 @@ void PhishingClassifierDelegate::MaybeStartClassification() {
     return;
   }
 
-  if (last_url_received_from_browser_ != last_finished_load_url_) {
+  if (last_url_received_from_browser_ != stripped_last_load_url) {
     // The browser has not yet confirmed that this URL should be classified,
-    // so defer classification for now.
+    // so defer classification for now.  Note: the ref does not affect
+    // any of the browser's preclassification checks, so we don't require it
+    // to match.
     VLOG(2) << "Not starting classification, last url from browser is "
             << last_url_received_from_browser_ << ", last finished load is "
             << last_finished_load_url_;
