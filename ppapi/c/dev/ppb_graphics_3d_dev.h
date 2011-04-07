e7e242e41ac567fe5fc5bc2d2edf31dc77876922
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 20:07:18 +0000

    Added and partially implemented entry points for PPB_GRAPHICS_3D. Review URL: http://codereview.chromium.org/6814023
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80830 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_graphics_3d_dev.h
+++ ppapi/c/dev/ppb_graphics_3d_dev.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/pp_resource.h"
+#include "ppapi/c/pp_var.h"
 
 // Example usage from plugin code:
 //
