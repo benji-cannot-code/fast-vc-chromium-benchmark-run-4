a77c6625cc3d2b08405d37e940b86f2a23c32c93
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 17:14:01 +0000

    net: disable TestKnownRoots
    It hung the TSAN bot, although I don't know why:
    http://build.chromium.org/p/chromium.memory/builders/Windows%20Tests%20%28tsan%29/builds/3531/steps/memory%20test%3A%20net/logs/stdio
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80801 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate_unittest.cc
+++ net/base/x509_certificate_unittest.cc
@@ -482,7 +482,8 @@ TEST(X509CertificateTest, IntermediateCARequireExplicitPolicy) {
   root_certs->Clear();
 }
 
-TEST(X509CertificateTest, TestKnownRoot) {
+// agl: disabled because it hung the TSAN bots.
+TEST(X509CertificateTest, DISABLED_TestKnownRoot) {
   FilePath certs_dir = GetTestCertsDirectory();
   scoped_refptr<X509Certificate> cert =
       ImportCertFromFile(certs_dir, "nist.der");
