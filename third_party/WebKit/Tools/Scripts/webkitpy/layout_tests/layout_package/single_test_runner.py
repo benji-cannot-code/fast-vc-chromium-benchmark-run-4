856cd508eeacffbd01871e6d6a8aa41ebdd7ea14
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-08 20:08:01 +0000

    2011-04-08  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Tony Chang.
    
            new-run-webkit-tests: implement support for audio tests.
    
            https://bugs.webkit.org/show_bug.cgi?id=57987
    
            * Scripts/webkitpy/layout_tests/layout_package/json_layout_results_generator.py:
            * Scripts/webkitpy/layout_tests/layout_package/single_test_runner.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_expectations.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_failures.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_result_writer.py:
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/test.py:
            * Scripts/webkitpy/layout_tests/port/mock_drt.py:
            * Scripts/webkitpy/layout_tests/port/webkit.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/single_test_runner.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/single_test_runner.py
@@ -83,7 +83,7 @@ class SingleTestRunner:
             # For example, if 'foo.html' has two expectation files, 'foo-expected.html' and
             # 'foo-expected.txt', we should warn users. One test file must be used exclusively
             # in either layout tests or reftests, but not in both.
-            for suffix in ['.txt', '.checksum', '.png']:
+            for suffix in ('.txt', '.checksum', '.png', '.wav'):
                 expected_filename = self._port.expected_filename(self._filename, suffix)
                 if fs.exists(expected_filename):
                     _log.error('The reftest (%s) can not have an expectation file (%s).'
@@ -92,7 +92,8 @@ class SingleTestRunner:
     def _expected_driver_output(self):
         return base.DriverOutput(self._port.expected_text(self._filename),
                                  self._port.expected_image(self._filename),
-                                 self._port.expected_checksum(self._filename))
+                                 self._port.expected_checksum(self._filename),
+                                 self._port.expected_audio(self._filename))
 
     def _should_fetch_expected_checksum(self):
         return (self._options.pixel_tests and
@@ -143,6 +144,9 @@ class SingleTestRunner:
         # DumpRenderTree may not output utf-8 text (e.g. webarchives).
         self._save_baseline_data(driver_output.text, ".txt",
                                  generate_new_baseline=self._options.new_baseline)
+        if driver_output.audio:
+            self._save_baseline_data(driver_output.audio, '.wav',
+                                     generate_new_baseline=self._options.new_baseline)
         if self._options.pixel_tests and driver_output.image_hash:
             self._save_baseline_data(driver_output.image, ".png",
                                      generate_new_baseline=self._options.new_baseline)
@@ -217,6 +221,7 @@ class SingleTestRunner:
             return TestResult(self._filename, failures, driver_output.test_time)
 
         failures.extend(self._compare_text(driver_output.text, expected_driver_output.text))
+        failures.extend(self._compare_audio(driver_output.audio, expected_driver_output.audio))
         if self._options.pixel_tests:
             failures.extend(self._compare_image(driver_output, expected_driver_output))
         return TestResult(self._filename, failures, driver_output.test_time)
@@ -229,6 +234,15 @@ class SingleTestRunner:
             failures.append(test_failures.FailureTextMismatch())
         elif actual_text and not expected_text:
             failures.append(test_failures.FailureMissingResult())
+        return failures
+
+    def _compare_audio(self, actual_audio, expected_audio):
+        failures = []
+        if (expected_audio and actual_audio and
+            self._port.compare_audio(actual_audio, expected_audio)):
+            failures.append(test_failures.FailureAudioMismatch())
+        elif actual_audio and not expected_audio:
+            failures.append(test_failures.FailureMissingAudio())
         return failures
 
     def _get_normalized_output_text(self, output):
