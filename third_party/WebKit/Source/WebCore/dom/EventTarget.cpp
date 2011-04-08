4464e9f5ab91413ba0a626c95ed1498c3e67e929
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-08 06:46:52 +0000

    Not reviewed.
    Rolled out some accidentally-committed changes in my last commit.
    
    * dom/EventTarget.cpp:
    (WebCore::EventTarget::addEventListener):
    (WebCore::EventTarget::removeEventListener):
    * dom/EventTarget.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventTarget.cpp
+++ third_party/WebKit/Source/WebCore/dom/EventTarget.cpp
@@ -210,10 +210,10 @@ bool EventTarget::addEventListener(const AtomicString& eventType, PassRefPtr<Eve
     EventTargetData* d = ensureEventTargetData();
 
     pair<EventListenerMap::iterator, bool> result = d->eventListenerMap.add(eventType, 0);
-    OwnPtr<EventListenerVector>& entry = result.first->second;
+    EventListenerVector*& entry = result.first->second;
     const bool isNewEntry = result.second;
     if (isNewEntry)
-        entry = adoptPtr(new EventListenerVector());
+        entry = new EventListenerVector();
 
     RegisteredEventListener registeredListener(listener, useCapture);
     if (!isNewEntry) {
@@ -234,7 +234,7 @@ bool EventTarget::removeEventListener(const AtomicString& eventType, EventListen
     EventListenerMap::iterator result = d->eventListenerMap.find(eventType);
     if (result == d->eventListenerMap.end())
         return false;
-    OwnPtr<EventListenerVector>& entry = result->second;
+    EventListenerVector* entry = result->second;
 
     RegisteredEventListener registeredListener(listener, useCapture);
     size_t index = entry->find(registeredListener);
@@ -242,8 +242,10 @@ bool EventTarget::removeEventListener(const AtomicString& eventType, EventListen
         return false;
 
     entry->remove(index);
-    if (entry->isEmpty())
+    if (entry->isEmpty()) {
+        delete entry;
         d->eventListenerMap.remove(result);
+    }
 
     // Notify firing events planning to invoke the listener at 'index' that
     // they have one less listener to invoke.
