ff495d6edb21cc08a9d3a374ae37e5c2891b4a72
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-08 17:44:31 +0000

    2011-04-08  Abhishek Arya  <inferno@chromium.org>
            Unreviewed. Remove accidental printf added in r83306.
    
            * page/EventHandler.cpp:
            (WebCore::EventHandler::sendContextMenuEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -2176,7 +2176,6 @@ bool EventHandler::sendContextMenuEvent(const PlatformMouseEvent& event)
         selectClosestWordOrLinkFromMouseEvent(mev);
     }
 
-    printf("node: %s\n", targetNode(mev)->nodeName().utf8().data());
     swallowEvent = dispatchMouseEvent(eventNames().contextmenuEvent, targetNode(mev), true, 0, event, false);
     
     return swallowEvent;
