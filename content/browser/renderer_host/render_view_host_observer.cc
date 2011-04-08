f2abdaaf5becb59eef5c16c6453865f024e8ea36
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-08 17:33:54 +0000

    Revert 80946 - Fix heap corruption that occurs when RenderViewHostObserver calls RenderViewHost to unregister when the latter is in its destructor.TBR=dmichaelReview URL: http://codereview.chromium.org/6813043 TBR=jam@chromium.org
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80953 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_observer.cc
+++ content/browser/renderer_host/render_view_host_observer.cc
@@ -36,6 +36,7 @@ bool RenderViewHostObserver::Send(IPC::Message* message) {
 }
 
 void RenderViewHostObserver::RenderViewHostDestruction() {
-  render_view_host_ = NULL;
+  render_view_host_->RemoveObserver(this);
   RenderViewHostDestroyed();
+  render_view_host_ = NULL;
 }
