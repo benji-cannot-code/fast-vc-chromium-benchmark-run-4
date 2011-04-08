e0532f34f818308bf1b5bcdc9be791d5ba6bfeae
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-08 17:34:33 +0000

    Revert 80939 - Fix clang error TBR=jam@chromium.org
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/desktop_notification_handler.h
+++ chrome/browser/desktop_notification_handler.h
@@ -21,7 +21,7 @@ class DesktopNotificationHandler : public RenderViewHostObserver {
 
  private:
   // RenderViewHostObserver implementation.
-  virtual bool OnMessageReceived(const IPC::Message& message);
+  bool OnMessageReceived(const IPC::Message& message);
 
   // IPC handlers.
   void OnShow(const DesktopNotificationHostMsg_Show_Params& params);
