0cd8045362da6092ed57cfe55cc702ea879e7e4b
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-08 20:40:43 +0000

    Implement PanelManager and hook up with Panel creation. The platform dependent implementation will be added later.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6799001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80981 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel.h
+++ chrome/browser/ui/panels/panel.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 
 #include "base/memory/scoped_ptr.h"
+#include "ui/gfx/rect.h"
+
+class PanelManager;
 
 // A platform independent implementation of BrowserWindow for Panels.  This
 // class would get the first crack at all the BrowserWindow calls for Panels and
@@ -23,8 +26,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   other Panels.  For example deleting a panel would rearrange other panels.
 class Panel : public BrowserWindow {
  public:
-  explicit Panel(Browser* browser);
+  Panel(Browser* browser, const gfx::Rect& bounds);
   virtual ~Panel();
+
+  // Returns the PanelManager associated with this panel.
+  PanelManager* manager() const;
+
+  void Minimize();
+
+  void Restore();
+
+  const gfx::Rect& bounds() const { return bounds_; }
+
+  bool minimized() const { return minimized_; }
 
   // BrowserWindow overrides.
   virtual void Show();
@@ -123,6 +137,14 @@ class Panel : public BrowserWindow {
   // Platform specifc BrowserWindow implementation for panels.  It'd be one of
   // PanelBrowserWindowGtk/PanelBrowserView/PanelBrowserWindowCocoa.
   scoped_ptr<BrowserWindow> browser_window_;
+
+  // The bounds.
+  gfx::Rect bounds_;
+
+  // Is the panel minimized?
+  bool minimized_;
+
+  DISALLOW_COPY_AND_ASSIGN(Panel);
 };
 
 #endif  // CHROME_BROWSER_UI_PANELS_PANEL_H_
