aa84fba35b09901bddef8421dd6d5fac1e276bc5
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-08 07:05:32 +0000

    2011-04-08  Adam Barth  <abarth@webkit.org>
            Attempt to fix the Qt build.
    
            * css/CSSStyleApplyProperty.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CSSPrimitiveValueMappings.h"
 #include "CSSStyleSelector.h"
+#include "CSSValueList.h"
 #include "Document.h"
 #include "Element.h"
 #include "RenderStyle.h"
