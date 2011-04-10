c43e13690f60d0690dea2ef50949362762e23b11
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-10 03:41:08 +0000

    Not reviewed.
    Try recommitting some things svn left out of its last commit.
    
    * heap/Handle.h:
    (JSC::HandleBase::operator!):
    (JSC::HandleBase::HandleBase):
    (JSC::HandleBase::slot):
    (JSC::HandleBase::setSlot):
    (JSC::Handle::Handle):
    * heap/HandleHeap.cpp:
    (JSC::HandleHeap::markWeakHandles):
    (JSC::HandleHeap::finalizeWeakHandles):
    (JSC::HandleHeap::isValidWeakNode):
    * heap/HandleHeap.h:
    (JSC::HandleHeap::globalData):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/HandleHeap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/HandleHeap.cpp
@@ -31,26 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-#if !ASSERT_DISABLED
-static inline bool isValidWeakHandle(HandleSlot handle)
-{
-    JSValue value = *handle;
-    if (!value || !value.isCell())
-        return false;
-
-    JSCell* cell = value.asCell();
-    if (!cell || !cell->structure())
-        return false;
-
-#if ENABLE(JSC_ZOMBIES)
-    if (cell->isZombie())
-        return false;
-#endif
-
-    return true;
-}
-#endif
-
 WeakHandleOwner::~WeakHandleOwner()
 {
 }
@@ -98,7 +78,7 @@ void HandleHeap::markWeakHandles(HeapRootMarker& heapRootMarker)
 
         Node* end = m_weakList.end();
         for (Node* node = m_weakList.begin(); node != end; node = node->next()) {
-            ASSERT(isValidWeakHandle(node->slot()));
+            ASSERT(isValidWeakNode(node));
             JSCell* cell = node->slot()->asCell();
             if (Heap::isMarked(cell))
                 continue;
@@ -121,7 +101,7 @@ void HandleHeap::finalizeWeakHandles()
     for (Node* node = m_weakList.begin(); node != end; node = m_nextToFinalize) {
         m_nextToFinalize = node->next();
 
-        ASSERT(isValidWeakHandle(node->slot()));
+        ASSERT(isValidWeakNode(node));
         JSCell* cell = node->slot()->asCell();
         if (Heap::isMarked(cell))
             continue;
@@ -173,5 +153,28 @@ unsigned HandleHeap::protectedGlobalObjectCount()
     }
     return count;
 }
+
+#if !ASSERT_DISABLED
+bool HandleHeap::isValidWeakNode(Node* node)
+{
+    if (!node->isWeak())
+        return false;
+
+    JSValue value = *node->slot();
+    if (!value || !value.isCell())
+        return false;
+
+    JSCell* cell = value.asCell();
+    if (!cell || !cell->structure())
+        return false;
+
+#if ENABLE(JSC_ZOMBIES)
+    if (cell->isZombie())
+        return false;
+#endif
+
+    return true;
+}
+#endif
 
 } // namespace JSC
