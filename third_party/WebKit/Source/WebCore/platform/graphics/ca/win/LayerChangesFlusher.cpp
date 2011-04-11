8ecb5ffef035e054255be5c59d4a2b748a0f7685
jhoneycutt@apple.com <jhoneycutt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-11 21:41:01 +0000

    <rdar://problem/9267042> Assertion failure on launch in LayerChangesFlusher::setHook().
    https://bugs.webkit.org/show_bug.cgi?id=58270
    
    Reviewed by Adam Roben.
    
    * platform/graphics/ca/win/LayerChangesFlusher.cpp:
    (WebCore::LayerChangesFlusher::setHook):
    MSDN says that the HINSTANCE passed in must be null if the thread ID
    passed is a thread created by this process and the hook procedure is
    within this process.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83502 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/win/LayerChangesFlusher.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/win/LayerChangesFlusher.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "AbstractCACFLayerTreeHost.h"
-#include "WebCoreInstanceHandle.h"
 #include <wtf/StdLibExtras.h>
 #include <wtf/Vector.h>
 
@@ -108,7 +107,7 @@ void LayerChangesFlusher::setHook()
 
     DWORD threadID = ::GetCurrentThreadId();
 
-    m_hook = ::SetWindowsHookExW(WH_GETMESSAGE, hookCallback, instanceHandle(), threadID);
+    m_hook = ::SetWindowsHookExW(WH_GETMESSAGE, hookCallback, 0, threadID);
     ASSERT_WITH_MESSAGE(m_hook, "::SetWindowsHookExW failed with error %lu", ::GetLastError());
 
     // Post a message to the message queue to prevent ::GetMessage from blocking, which will ensure
