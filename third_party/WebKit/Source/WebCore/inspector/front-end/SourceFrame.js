954a589298bce47b7851c946c2ef9d0010e7bc3f
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-11 13:40:38 +0000

    2011-04-07  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: get rid of source frame delegates for resources panel.
            https://bugs.webkit.org/show_bug.cgi?id=58041
    
            Use TextViewer configured with a domain-specific delegate to show/edit resource contents in resources panel.
    
            * inspector/front-end/ResourceView.js:
            (WebInspector.ResourceView.createResourceView):
            (WebInspector.ResourceView.resourceViewTypeMatchesResource):
            (WebInspector.ResourceSourceFrame):
            (WebInspector.ResourceSourceFrame.prototype.get resource):
            (WebInspector.ResourceSourceFrame.prototype.contentEditable):
            (WebInspector.ResourceSourceFrame.prototype._requestContent):
            (WebInspector.CSSSourceFrame):
            (WebInspector.CSSSourceFrame.prototype.contentEditable):
            (WebInspector.CSSSourceFrame.prototype._editContent):
            (WebInspector.CSSSourceFrame.prototype._editContent.handleInfos):
            (WebInspector.CSSSourceFrame.prototype._saveStyleSheet):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype._ensureContentLoaded):
            (WebInspector.SourceFrame.prototype._requestContent):
            (WebInspector.SourceFrame.prototype._handleSave):
            (WebInspector.SourceFrame.prototype._editContent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -115,8 +115,13 @@ WebInspector.SourceFrame.prototype = {
     {
         if (!this._contentRequested) {
             this._contentRequested = true;
-            this._delegate.requestContent(this._createTextViewer.bind(this));
+            this._requestContent(this._createTextViewer.bind(this));
         }
+    },
+
+    _requestContent: function(callback)
+    {
+        this._delegate.requestContent(callback);
     },
 
     markDiff: function(diffData)
@@ -884,7 +889,7 @@ WebInspector.SourceFrame.prototype = {
 
     _handleSave: function()
     {
-        if (this._textViewer.readOnly || !this._delegate.canEditScriptSource())
+        if (this._textViewer.readOnly || !this.isContentEditable())
             return false;
 
         if (!this._viewerState) {
@@ -928,9 +933,14 @@ WebInspector.SourceFrame.prototype = {
                 this._delegate.setBreakpoint(Number(lineNumber), breakpoint.condition, breakpoint.enabled);
             }
         }
-        this._delegate.editScriptSource(newSource, didEditScriptSource.bind(this));
+        this._editContent(newSource, didEditScriptSource.bind(this));
         this._editScriptSourceInProgress = true;
         return true;
+    },
+
+    _editContent: function(newContent, callback)
+    {
+        this._delegate.editScriptSource(newContent, callback);
     },
 
     _handleRevertEditing: function()
