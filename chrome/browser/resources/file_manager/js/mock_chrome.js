449d195bcaa64c2a706dd94c90b3910c01f03c15
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 21:53:21 +0000

    File Manager: Add 'New Folder' button, fix sorting, assorted polish
    file_manager.css:
    * Smaller margin in header to account for buttons.
    * No padding on sort image for the 'icon type' column.
    * Add icon for folders
    
    file_manager.js:
    * Cache data for the sort column *just* before sorting.
    * Scan DOM for 'visibleif' attributes to control visibility of DialogType specific elements.
    * Add 'New Folder' functionality.
    * Add new 'Icon Type' column so that files can be sorted by type.  This column is a slightly special case because it has no label.
    * Scroll selected items back into view after a sort.
    * Remove leading '/' in virtual path passed in to selectFile(s).
    
    mock_chrome.js:
    * Remove unused strings.
    * Add new strings.
    
    util.js:
    * Refactor FileError.p.toString code so that callers can get a error mnemonic without the surrounding '[object ...]' string.
    
    main.html:
    * Add 'New folder' button.
    * Switch filename label/input to use visibleif attribute.
    
    generated_resources.grd:
    * Update strings
    
    extension_file_browser_private_api.cc:
    * Update strings.
    * Add local macro to reduce copy-pasta overload.
    
    BUG=chromium-os:14011
    TEST=manual testing in harness and chromeos-chrome build
    
    Review URL: http://codereview.chromium.org/6827001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81160 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/mock_chrome.js
+++ chrome/browser/resources/file_manager/js/mock_chrome.js
@@ -57,8 +57,6 @@ chrome.fileBrowserPrivate = {
       BODY_FONT_FAMILY: 'sans-serif',
       BODY_FONT_SIZE: '13px',
 
-      FILES_DISPLAYED_SUMMARY: '%1 Files Displayed',
-      FILES_SELECTED_SUMMARY: '%1 Files Selected',
       FILE_IS_DIRECTORY: 'Folder',
       PARENT_DIRECTORY: 'Parent Directory',
 
@@ -68,6 +66,10 @@ chrome.fileBrowserPrivate = {
       DATE_COLUMN_LABEL: 'Date',
       PREVIEW_COLUMN_LABEL: 'Preview',
 
+      ERROR_CREATING_FOLDER: 'Unable to create folder "$1": $2',
+      ERROR_INVALID_FOLDER_CHARACTER: 'Invalid character in folder name: $1',
+      NEW_FOLDER_PROMPT: 'Enter a name for the new folder',
+      NEW_FOLDER_BUTTON_LABEL: 'New Folder',
       FILENAME_LABEL: 'File Name',
 
       CANCEL_LABEL: 'Cancel',
