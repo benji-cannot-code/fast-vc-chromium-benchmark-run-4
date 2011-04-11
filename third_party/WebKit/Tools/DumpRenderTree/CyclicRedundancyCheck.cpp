f36fa3dbf192d2c4f548bba825374794095c7b96
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-11 18:36:13 +0000

    2011-04-11  Tony Chang  <tony@chromium.org>
            Reviewed by Eric Seidel.
    
            fix crc computation on 64 bit machines
            https://bugs.webkit.org/show_bug.cgi?id=58243
    
            unsigned long is dependenct on CPU architecture, so use unsigned which is always 32 bits
    
            * DumpRenderTree/CyclicRedundancyCheck.cpp:
            (makeCrcTable):
            (computeCrc):
            * DumpRenderTree/CyclicRedundancyCheck.h:
            * DumpRenderTree/PixelDumpSupport.cpp:
            (appendIntToVector):
            (convertChecksumToPNGComment):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/CyclicRedundancyCheck.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/CyclicRedundancyCheck.cpp
@@ -34,10 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Vector.h>
 
-static void makeCrcTable(unsigned long crcTable[256])
+static void makeCrcTable(unsigned crcTable[256])
 {
-    for (unsigned long i = 0; i < 256; i++) {
-        unsigned long c = i;
+    for (unsigned i = 0; i < 256; i++) {
+        unsigned c = i;
         for (int k = 0; k < 8; k++) {
             if (c & 1)
                 c = -306674912 ^ ((c >> 1) & 0x7fffffff);
@@ -48,16 +48,16 @@ static void makeCrcTable(unsigned long crcTable[256])
     }
 }
 
-unsigned long computeCrc(const Vector<unsigned char>& buffer)
+unsigned computeCrc(const Vector<unsigned char>& buffer)
 {
-    static unsigned long crcTable[256];
+    static unsigned crcTable[256];
     static bool crcTableComputed = false;
     if (!crcTableComputed) {
         makeCrcTable(crcTable);
         crcTableComputed = true;
     }
 
-    unsigned long crc = 0xffffffffL;
+    unsigned crc = 0xffffffffL;
     for (size_t i = 0; i < buffer.size(); ++i)
         crc = crcTable[(crc ^ buffer[i]) & 0xff] ^ ((crc >> 8) & 0x00ffffffL);
     return crc ^ 0xffffffffL;
--- third_party/WebKit/Tools/DumpRenderTree/CyclicRedundancyCheck.h
+++ third_party/WebKit/Tools/DumpRenderTree/CyclicRedundancyCheck.h
@@ -34,6 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Vector.h>
 
-unsigned long computeCrc(const Vector<unsigned char>&);
+unsigned computeCrc(const Vector<unsigned char>&);
 
 #endif
