ee720e491abd09f647db4289e44e44c103cae738
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-11 20:45:39 +0000

    2011-04-11  Vsevolod Vlasov  <vsevik@chromium.org>
            Reviewed by Pavel Feldman.
    
            PerformanceTiming returns inconsistent values when timing is null.
            https://bugs.webkit.org/show_bug.cgi?id=58143
    
            Made PerformanceTiming returned values consistent when there is no timing object.
    
            * page/PerformanceTiming.cpp:
            (WebCore::PerformanceTiming::domainLookupStart):
            (WebCore::PerformanceTiming::domainLookupEnd):
            (WebCore::PerformanceTiming::connectStart):
            (WebCore::PerformanceTiming::connectEnd):
            (WebCore::PerformanceTiming::requestStart):
            (WebCore::PerformanceTiming::responseStart):
            (WebCore::PerformanceTiming::domLoading):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/PerformanceTiming.cpp
+++ third_party/WebKit/Source/WebCore/page/PerformanceTiming.cpp
@@ -162,7 +162,7 @@ unsigned long long PerformanceTiming::domainLookupStart() const
 {
     ResourceLoadTiming* timing = resourceLoadTiming();
     if (!timing)
-        return 0;
+        return fetchStart();
 
     // This will be -1 when a DNS request is not performed.
     // Rather than exposing a special value that indicates no DNS, we "backfill" with fetchStart.
@@ -177,7 +177,7 @@ unsigned long long PerformanceTiming::domainLookupEnd() const
 {
     ResourceLoadTiming* timing = resourceLoadTiming();
     if (!timing)
-        return 0;
+        return domainLookupStart();
 
     // This will be -1 when a DNS request is not performed.
     // Rather than exposing a special value that indicates no DNS, we "backfill" with domainLookupStart.
@@ -192,11 +192,11 @@ unsigned long long PerformanceTiming::connectStart() const
 {
     DocumentLoader* loader = documentLoader();
     if (!loader)
-        return 0;
+        return domainLookupEnd();
 
     ResourceLoadTiming* timing = loader->response().resourceLoadTiming();
     if (!timing)
-        return 0;
+        return domainLookupEnd();
 
     // connectStart will be -1 when a network request is not made.
     // Rather than exposing a special value that indicates no new connection, we "backfill" with domainLookupEnd.
@@ -216,11 +216,11 @@ unsigned long long PerformanceTiming::connectEnd() const
 {
     DocumentLoader* loader = documentLoader();
     if (!loader)
-        return 0;
+        return connectStart();
 
     ResourceLoadTiming* timing = loader->response().resourceLoadTiming();
     if (!timing)
-        return 0;
+        return connectStart();
 
     // connectEnd will be -1 when a network request is not made.
     // Rather than exposing a special value that indicates no new connection, we "backfill" with connectStart.
@@ -252,7 +252,7 @@ unsigned long long PerformanceTiming::requestStart() const
 {
     ResourceLoadTiming* timing = resourceLoadTiming();
     if (!timing)
-        return 0;
+        return connectEnd();
 
     ASSERT(timing->sendStart >= 0);
     return resourceLoadTimeRelativeToAbsolute(timing->sendStart);
@@ -262,7 +262,7 @@ unsigned long long PerformanceTiming::responseStart() const
 {
     ResourceLoadTiming* timing = resourceLoadTiming();
     if (!timing)
-        return 0;
+        return requestStart();
 
     // FIXME: Response start needs to be the time of the first received byte.
     // However, the ResourceLoadTiming API currently only supports the time
@@ -287,7 +287,7 @@ unsigned long long PerformanceTiming::domLoading() const
 {
     const DocumentTiming* timing = documentTiming();
     if (!timing)
-        return 0;
+        return fetchStart();
 
     return toIntegerMilliseconds(timing->domLoading);
 }
