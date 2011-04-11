246eae115bfccd1efa2822cfc6f5ce793b560004
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-11 05:22:20 +0000

    2011-04-10  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Maciej Stachowiak.
    
            Car model dropdowns at audiusa.com lay out incorrectly with compositing enabled
            https://bugs.webkit.org/show_bug.cgi?id=56660
    
            When overflow on an element changes, we need to inform compositing
            layers which create and position an "ancestor clipping" layer based
            on that overflow. Do this by triggering a rebuild of compositing layers
            when overflow changes on a layer whose stacking context has compositing
            descendants. (We can't just check whether the layer itself has compositing
            descendants, because overflow follows the render tree, but compositing
            follows the z-order tree.)
    
            Test: compositing/geometry/ancestor-overflow-change.html
    
            * rendering/RenderLayer.cpp:
            (WebCore::RenderLayer::styleChanged):
            * rendering/RenderLayer.h:
            * rendering/RenderLayerBacking.cpp:
            (WebCore::RenderLayerBacking::updateGraphicsLayerGeometry):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -3868,7 +3868,7 @@ bool RenderLayer::isSelfPaintingLayer() const
         || renderer()->isRenderIFrame();
 }
 
-void RenderLayer::styleChanged(StyleDifference diff, const RenderStyle*)
+void RenderLayer::styleChanged(StyleDifference diff, const RenderStyle* oldStyle)
 {
     bool isNormalFlowOnly = shouldBeNormalFlowOnly();
     if (isNormalFlowOnly != m_isNormalFlowOnly) {
@@ -3913,7 +3913,11 @@ void RenderLayer::styleChanged(StyleDifference diff, const RenderStyle*)
         compositor()->setCompositingLayersNeedRebuild();
     else if (m_backing)
         m_backing->updateGraphicsLayerGeometry();
-
+    else if (oldStyle && oldStyle->overflowX() != renderer()->style()->overflowX()) {
+        if (stackingContext()->hasCompositingDescendant())
+            compositor()->setCompositingLayersNeedRebuild();
+    }
+    
     if (m_backing && diff >= StyleDifferenceRepaint)
         m_backing->setContentsNeedDisplay();
 #else
