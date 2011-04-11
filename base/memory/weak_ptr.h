e9828772e671529e058137679e929aca34350e70
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 20:13:46 +0000

    Revert 81142 - Fix base::internal::WeakReference::Flag to be RefCountedThreadSafe.This allows a base::WeakPtr to be deleted on a different thread, which will modify the refcount on Flag.BUG=noneTEST=noneReview URL: http://codereview.chromium.org/6826034 TBR=willchan@chromium.org Review URL: http://codereview.chromium.org/6821053
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81144 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_api.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
-#include "base/threading/thread_checker.h"
+#include "base/threading/non_thread_safe.h"
 
 namespace base {
 
@@ -66,23 +66,19 @@ namespace internal {
 
 class BASE_API WeakReference {
  public:
-  // While Flag is bound to a specific thread, it may be deleted from another
-  // via base::WeakPtr::~WeakPtr().
-  class Flag : public RefCountedThreadSafe<Flag> {
+  class Flag : public RefCounted<Flag>, public base::NonThreadSafe {
    public:
-    explicit Flag(Flag** handle);
+    Flag(Flag** handle);
+    ~Flag();
 
+    void AddRef() const;
+    void Release() const;
     void Invalidate();
     bool IsValid() const;
 
-    void DetachFromThread() { thread_checker_.DetachFromThread(); }
+    void DetachFromThread() { base::NonThreadSafe::DetachFromThread(); }
 
    private:
-    friend class base::RefCountedThreadSafe<Flag>;
-
-    ~Flag();
-
-    ThreadChecker thread_checker_;
     Flag** handle_;
   };
 
@@ -238,11 +234,6 @@ class WeakPtrFactory {
   // Call this method to determine if any weak pointers exist.
   bool HasWeakPtrs() const {
     return weak_reference_owner_.HasRefs();
-  }
-
-  // Indicates that this object will be used on another thread from now on.
-  void DetachFromThread() {
-    weak_reference_owner_.DetachFromThread();
   }
 
  private:
