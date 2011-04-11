80f979ccd68c64e67782a100289cb159e0eebddd
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 18:34:57 +0000

    PepperWidget needs implementation of new method added in WebWidget.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6825044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81126 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget_fullscreen_pepper.cc
+++ content/renderer/render_widget_fullscreen_pepper.cc
@@ -22,6 +22,7 @@ using WebKit::WebCanvas;
 using WebKit::WebCompositionUnderline;
 using WebKit::WebCursorInfo;
 using WebKit::WebInputEvent;
+using WebKit::WebPoint;
 using WebKit::WebRect;
 using WebKit::WebSize;
 using WebKit::WebString;
@@ -118,6 +119,10 @@ class PepperWidget : public WebWidget {
 
   virtual WebRect caretOrSelectionBounds() {
     return WebRect();
+  }
+
+  virtual bool selectionRange(WebPoint& start, WebPoint& end) const {
+    return false;
   }
 
   virtual void setTextDirection(WebTextDirection) {
