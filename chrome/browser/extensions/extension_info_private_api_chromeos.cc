5f2ef1bba18b479d9b1d28afdbe3a74dca856321
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 20:47:25 +0000

    Fix base::internal::WeakReference::Flag to be RefCountedThreadSafe.
    This allows a base::WeakPtr to be deleted on a different thread, which will modify the refcount on Flag.
    
    BUG=none
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=81142
    
    Review URL: http://codereview.chromium.org/6826034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81148 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_info_private_api_chromeos.cc
+++ chrome/browser/extensions/extension_info_private_api_chromeos.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extension_info_private_api_chromeos.h"
 
 #include <map>
+#include "base/threading/non_thread_safe.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/values.h"
