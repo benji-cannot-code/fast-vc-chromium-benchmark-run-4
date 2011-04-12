1a2b1603e2b9c8d92751b22bf92fec17b8018127
alice.liu@apple.com <alice.liu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 22:49:46 +0000

    A change that should have been part of http://trac.webkit.org/changeset/83628
    Reviewed by Jon Honeycutt.
    
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (resetDefaultsToConsistentValues): Setting should be off for most tests. Reset to false.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/win/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -877,6 +877,7 @@ static void resetDefaultsToConsistentValues(IWebPreferences* preferences)
         prefsPrivate->setXSSAuditorEnabled(FALSE);
         prefsPrivate->setFrameFlatteningEnabled(FALSE);
         prefsPrivate->setOfflineWebApplicationCacheEnabled(TRUE);
+        prefsPrivate->setLoadsSiteIconsIgnoringImageLoadingPreference(FALSE);
     }
     setAlwaysAcceptCookies(false);
 
