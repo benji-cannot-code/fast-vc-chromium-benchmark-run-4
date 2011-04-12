0981f88a98d2691080543a37e3a39a1773019669
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 16:48:37 +0000

    Fix Clang build after r81259
    In file included from ./net/base/cert_verifier.h:16:
    ./net/base/cert_verify_result.h:19:3: error: [chromium-style] Complex constructor has an inlined body.
      CertVerifyResult() { Reset(); }
      ^
    ./net/base/cert_verify_result.h:17:1: error: [chromium-style] Complex class/struct needs a declared destructor.
      class CertVerifyResult {
      ^
    
    BUG=none
    TEST=none
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81260 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/cert_verify_result.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/cert_verify_result.h"
+
+namespace net {
+
+CertVerifyResult::CertVerifyResult() {
+  Reset();
+}
+
+void CertVerifyResult::Reset() {
+  cert_status = 0;
+  has_md5 = false;
+  has_md2 = false;
+  has_md4 = false;
+  has_md5_ca = false;
+  has_md2_ca = false;
+  is_issued_by_known_root = false;
+  public_key_hashes.clear();
+}
+
+} // namespace net
