d85b1716e135529b3da96272f9fb9ba7cb8fc848
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 19:08:02 +0000

    Add FTP transaction factories to the system URLRequestContext, and the proxy script fetcher URLRequestContext
    BUG=76581
    Review URL: http://codereview.chromium.org/6820059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mapped_host_resolver.h"
 #include "net/base/net_util.h"
 #include "net/proxy/proxy_config_service.h"
+#include "net/ftp/ftp_network_layer.h"
 #include "net/http/http_auth_filter.h"
 #include "net/http/http_auth_handler_factory.h"
 #include "net/http/http_network_layer.h"
@@ -210,6 +211,8 @@ ConstructProxyScriptFetcherContext(IOThread::Globals* globals,
   context->set_proxy_service(globals->proxy_script_fetcher_proxy_service.get());
   context->set_http_transaction_factory(
       globals->proxy_script_fetcher_http_transaction_factory.get());
+  context->set_ftp_transaction_factory(
+      globals->proxy_script_fetcher_ftp_transaction_factory.get());
   // In-memory cookie store.
   context->set_cookie_store(new net::CookieMonster(NULL, NULL));
   context->set_network_delegate(globals->system_network_delegate.get());
@@ -230,6 +233,8 @@ ConstructSystemRequestContext(IOThread::Globals* globals,
   context->set_proxy_service(globals->system_proxy_service.get());
   context->set_http_transaction_factory(
       globals->system_http_transaction_factory.get());
+  context->set_ftp_transaction_factory(
+      globals->system_ftp_transaction_factory.get());
   // In-memory cookie store.
   context->set_cookie_store(new net::CookieMonster(NULL, NULL));
   return context;
@@ -452,6 +457,8 @@ void IOThread::Init() {
       new net::HttpNetworkSession(session_params));
   globals_->proxy_script_fetcher_http_transaction_factory.reset(
       new net::HttpNetworkLayer(network_session));
+  globals_->proxy_script_fetcher_ftp_transaction_factory.reset(
+      new net::FtpNetworkLayer(globals_->host_resolver.get()));
 
   scoped_refptr<net::URLRequestContext> proxy_script_fetcher_context =
       ConstructProxyScriptFetcherContext(globals_, net_log_);
@@ -655,6 +662,8 @@ void IOThread::InitSystemRequestContext() {
   globals_->system_http_transaction_factory.reset(
       new net::HttpNetworkLayer(
           new net::HttpNetworkSession(system_params)));
+  globals_->system_ftp_transaction_factory.reset(
+      new net::FtpNetworkLayer(globals_->host_resolver.get()));
   globals_->system_request_context =
       ConstructSystemRequestContext(globals_, net_log_);
 }
