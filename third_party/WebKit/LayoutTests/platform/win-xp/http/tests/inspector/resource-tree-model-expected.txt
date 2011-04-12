benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit ca29a65dd33e11d92c25f9a20749e629fbed95a6

2011-04-12  Pavel Feldman  <pfeldman@google.com>
        Reviewed by Yury Semikhatsky.

        Web Inspector: cookies view is empty even for sites with cookies.
        https://bugs.webkit.org/show_bug.cgi?id=58316

        * http/tests/inspector/resource-tree-model-expected.txt: Added.
        * http/tests/inspector/resource-tree-model.html: Added.
2011-04-12  Pavel Feldman  <pfeldman@google.com>

        Reviewed by Yury Semikhatsky.

        Web Inspector: cookies view is empty even for sites with cookies.
        https://bugs.webkit.org/show_bug.cgi?id=58316

        Test: http/tests/inspector/resource-tree-model.html

        * inspector/front-end/ResourceTreeModel.js:
        (WebInspector.ResourceTreeModel.prototype._callForFrameResources):

git-svn-id: svn://svn.chromium.org/blink/trunk@83584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/http/tests/inspector/resource-tree-model-expected.txt
@@ -1,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Tests resource tree model.
+
+
+http://127.0.0.1:8000/inspector/inspector-test.js => http://127.0.0.1:8000/inspector/resource-tree-model.html
+http://127.0.0.1:8000/inspector/resource-tree-model.html => http://127.0.0.1:8000/inspector/resource-tree-model.html
+http://127.0.0.1:8000/inspector/resources-test.js => http://127.0.0.1:8000/inspector/resource-tree-model.html
+http://127.0.0.1:8000/inspector/resources/dummy-iframe.php => http://127.0.0.1:8000/inspector/resources/dummy-iframe.php
+

third_party/WebKit/LayoutTests/http/tests/inspector/resource-tree-model.html
@@ -1,0 +1,47 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<head>
+<script src="inspector-test.js"></script>
+<script src="resources-test.js"></script>
+
+<script>
+
+function loadIframe()
+{
+    var iframe = document.createElement("iframe");
+    iframe.src = "resources/dummy-iframe.php";
+    document.body.appendChild(iframe);
+}
+
+var test = function()
+{
+    WebInspector.networkManager.addEventListener(WebInspector.NetworkManager.EventTypes.ResourceFinished, onResourceFinished);
+
+    var urls = [];
+    function onResourceFinished(event)
+    {
+        WebInspector.resourceTreeModel.forAllResources(printResource);
+        urls.sort();
+        urls.forEach(InspectorTest.addResult);
+        InspectorTest.completeTest();
+    }
+
+    function printResource(resource)
+    {
+        urls.push(resource.url + " => " + resource.documentURL);
+    }
+
+    InspectorTest.evaluateInPage("loadIframe()");    
+}
+
+</script>
+
+</head>
+
+<body onload="runTest()">
+<p>
+Tests resource tree model.
+</p>
+
+</body>
+</html>

third_party/WebKit/Source/WebCore/inspector/front-end/ResourceTreeModel.js
@@ -247,8 +247,6 @@ WebInspector.ResourceTreeModel.prototype = {
     _callForFrameResources: function(frameId, callback)
     {
         var resources = this._resourcesByFrameId[frameId];
-        if (!resources)
-            return;
 
         for (var url in resources) {
             if (callback(resources[url]))
@@ -302,6 +300,7 @@ WebInspector.ResourceTreeModel.prototype = {
         var resource = new WebInspector.Resource(null, url);
         resource.frameId = frame.id;
         resource.loaderId = frame.loaderId;
+        resource.documentURL = frame.url;
         return resource;
     }
 }

commit 1c588fe2a9669c243eeaa2974a6ca7900a57eada

[Windows XP Debug Tests] http/tests/inspector/resource-tree-model.html failing since introduction in r83584 https://bugs.webkit.org/show_bug.cgi?id=58367
Add expected failing results to get the bots green.

* platform/win-xp/http: Added.
* platform/win-xp/http/tests: Added.
* platform/win-xp/http/tests/inspector: Added.
* platform/win-xp/http/tests/inspector/resource-tree-model-expected.txt: Added.



git-svn-id: svn://svn.chromium.org/blink/trunk@83630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/platform/win-xp/http/tests/inspector/resource-tree-model-expected.txt
@@ -1,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Tests resource tree model.
+
+
+http://127.0.0.1:8000/inspector/inspector-test.js => http://127.0.0.1:8000/inspector/resource-tree-model.html
+http://127.0.0.1:8000/inspector/resources-test.js => http://127.0.0.1:8000/inspector/resource-tree-model.html
+http://127.0.0.1:8000/inspector/resources/dummy-iframe.php => http://127.0.0.1:8000/inspector/resources/dummy-iframe.php
+