cfd581dd364bc7413e90233c4d305b064ab6b9cf
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 19:10:37 +0000

    2011-04-12  Tony Chang  <tony@chromium.org>
            Reviewed by Andreas Kling.
    
            [Qt] embed checksums in PNGs written by Qt-DRT
            https://bugs.webkit.org/show_bug.cgi?id=58173
    
            * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
            (WebCore::DumpRenderTree::dump):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeQt.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeQt.cpp
@@ -998,6 +998,8 @@ void DumpRenderTree::dump()
         }
 
         if (dumpImage) {
+            image.setText("checksum", actualHash);
+
             QBuffer buffer;
             buffer.open(QBuffer::WriteOnly);
             image.save(&buffer, "PNG");
