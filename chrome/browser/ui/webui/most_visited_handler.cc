69692ff42c9809912380541f0d5ac8e26202e8a1
michaelbai@google.com <michaelbai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 16:18:31 +0000

    a Downloaded or retrieved favicon and touch in FaviconHelper.
    b.ViewHostMsg_UpdateFaviconURL can update multiple icon urls
    
    BUG=71571
    TEST=Tested with existing unit test and add 2 new unit tests
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=81158
    
    Review URL: http://codereview.chromium.org/6672065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/most_visited_handler.cc
+++ chrome/browser/ui/webui/most_visited_handler.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/new_tab_ui.h"
 #include "chrome/browser/ui/webui/thumbnail_source.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/url_constants.h"
 #include "content/browser/browser_thread.h"
 #include "content/common/notification_source.h"
 #include "content/common/notification_type.h"
@@ -69,8 +70,7 @@ WebUIMessageHandler* MostVisitedHandler::Attach(WebUI* web_ui) {
   ThumbnailSource* thumbnail_src = new ThumbnailSource(profile);
   profile->GetChromeURLDataManager()->AddDataSource(thumbnail_src);
 
-  FaviconSource* favicon_src = new FaviconSource(profile);
-  profile->GetChromeURLDataManager()->AddDataSource(favicon_src);
+  profile->GetChromeURLDataManager()->AddDataSource(new FaviconSource(profile));
 
   // Get notifications when history is cleared.
   registrar_.Add(this, NotificationType::HISTORY_URLS_DELETED,
