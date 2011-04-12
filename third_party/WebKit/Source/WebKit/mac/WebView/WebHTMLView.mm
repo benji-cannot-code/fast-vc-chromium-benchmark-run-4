fa5d9d28fc991b1f7fe7cfa35349be4a4049b423
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 22:48:39 +0000

    Infinite recursion in WebHTMLView executeSavedKeypressCommands. https://bugs.webkit.org/show_bug.cgi?id=58382 <rdar://problem/9239370>
    Reviewed by Alexey Proskuryakov.
    
    Execution of some editing commands could trigger a call to selectedRange that
    internally calls executeSavedKeypressCommands creating an infinite recursion.
    
    Source/WebKit/mac: 
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView _executeSavedKeypressCommands]): Added a flag to avoid recursion.
    (-[WebHTMLView _interpretKeyEvent:savingCommands:]): Added flag initialization.
    
    Source/WebKit2: 
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _executeSavedKeypressCommands]): Added flag to avoid recursion.
    (-[WKView _interpretKeyEvent:withCachedTextInputState:savingCommandsTo:WebCore::]):
    Added flag initialization.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
@@ -467,6 +467,7 @@ struct WebHTMLViewInterpretKeyEventsParameters {
     bool eventInterpretationHadSideEffects;
     bool shouldSaveCommands;
     bool consumedByIM;
+    bool executingSavedKeypressCommands;
 };
 
 @interface WebHTMLViewPrivate : NSObject {
@@ -5434,10 +5435,16 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
     if (!parameters || parameters->event->keypressCommands().isEmpty())
         return;
 
+    // We could be called again if the execution of one command triggers a call to selectedRange.
+    // In this case, the state is up to date, and we don't need to execute any more saved commands to return a result
+    if (parameters->executingSavedKeypressCommands)
+        return;
+    
     // Avoid an infinite loop that would occur if executing a command appended it to event->keypressCommands() again.
     bool wasSavingCommands = parameters->shouldSaveCommands;
     parameters->shouldSaveCommands = false;
-
+    parameters->executingSavedKeypressCommands = true;
+    
     const Vector<KeypressCommand>& commands = parameters->event->keypressCommands();
 
     for (size_t i = 0; i < commands.size(); ++i) {
@@ -5450,6 +5457,7 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
     }
     parameters->event->keypressCommands().clear();
     parameters->shouldSaveCommands = wasSavingCommands;
+    parameters->executingSavedKeypressCommands = false;
 }
 
 - (BOOL)_interpretKeyEvent:(KeyboardEvent*)event savingCommands:(BOOL)savingCommands
@@ -5460,6 +5468,7 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
     WebHTMLViewInterpretKeyEventsParameters parameters;
     parameters.eventInterpretationHadSideEffects = false;
     parameters.shouldSaveCommands = savingCommands;
+    parameters.executingSavedKeypressCommands = false;
     // If we're intercepting the initial IM call we assume that the IM has consumed the event, 
     // and only change this assumption if one of the NSTextInput/Responder callbacks is used.
     // We assume the IM will *not* consume hotkey sequences
