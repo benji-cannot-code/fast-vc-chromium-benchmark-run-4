0b0e44fc89b9c3da04ebb5953460933f9172e64c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 08:29:35 +0000

    2011-04-12  Vsevolod Vlasov  <vsevik@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Failed/canceled resource requests kept showing as Pending in network panel.
            https://bugs.webkit.org/show_bug.cgi?id=58135
    
            Added statuses for failed/canceled resource loading.
    
            * English.lproj/localizedStrings.js:
            * inspector/Inspector.json:
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::didFailLoading):
            * inspector/front-end/NetworkManager.js:
            * inspector/front-end/NetworkPanel.js:
            * inspector/front-end/Resource.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
@@ -293,6 +293,16 @@ WebInspector.Resource.prototype = {
         this._failed = x;
     },
 
+    get canceled()
+    {
+        return this._canceled;
+    },
+
+    set canceled(x)
+    {
+        this._canceled = x;
+    },
+
     get category()
     {
         return this._category;
