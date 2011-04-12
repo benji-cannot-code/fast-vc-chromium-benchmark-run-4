13bc765587dedb863cfc3a201f8a7edbb38f3325
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 16:44:56 +0000

    2011-04-11  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Carlson.
    
            Should MediaControls::changedNetworkState fiddle with Fullscreen controls?
            https://bugs.webkit.org/show_bug.cgi?id=58163
    
            Covered by existing tests and manual-tests/media-controls.html.
    
            * html/shadow/MediaControls.cpp:
            (WebCore::MediaControls::reset): Moved fullscreen showing/hiding here.
            (WebCore::MediaControls::changedNetworkState): ... from here.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElement.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElement.cpp
@@ -281,6 +281,12 @@ void MediaControlRootElement::reset()
         return;
 
     changedNetworkState();
+
+    if (m_mediaElement->supportsFullscreen())
+        m_fullScreenButton->show();
+    else
+        m_fullScreenButton->hide();
+
     float duration = m_mediaElement->duration();
     if (!isnan(duration) || page->theme()->hasOwnDisabledStateHandlingFor(MediaSliderPart)) {
         m_timeline->setDuration(duration);
@@ -376,12 +382,6 @@ void MediaControlRootElement::reportedError()
 
 void MediaControlRootElement::changedNetworkState()
 {
-    // FIXME: Why are we changing fullscreen button visibility here? <http://webkit.org/b/58163>
-    if (m_mediaElement->supportsFullscreen())
-        m_fullScreenButton->show();
-    else
-        m_fullScreenButton->hide();
-
     if (m_statusDisplay)
         m_statusDisplay->update();
 }
