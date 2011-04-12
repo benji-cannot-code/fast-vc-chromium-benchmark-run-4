c2cedb40e84a2c8e8e097f72d73e16bc7da9bfff
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 14:10:25 +0000

    DevTools: removed declarations of obsolete APU messages. Also fixed a couple of comments.
    BUG=Nonw
    TEST=Code compiles
    
    Review URL: http://codereview.chromium.org/6820066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/devtools_messages.h
+++ chrome/common/devtools_messages.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -65,10 +65,6 @@ IPC_MESSAGE_CONTROL1(DevToolsClientMsg_DispatchOnInspectorFrontend,
 IPC_MESSAGE_CONTROL1(DevToolsClientMsg_DebuggerOutput,
                      std::string /* message */)
 
-// Legacy APU dispatch message.
-IPC_MESSAGE_CONTROL1(DevToolsClientMsg_DispatchToAPU,
-                     std::string /* message */)
-
 
 //-----------------------------------------------------------------------------
 // These are messages sent from DevToolsClient to DevToolsAgent through the
@@ -99,12 +95,9 @@ IPC_MESSAGE_CONTROL2(DevToolsAgentMsg_InspectElement,
                      int /* x */,
                      int /* y */)
 
-// Enables/disables the apu agent.
-IPC_MESSAGE_CONTROL1(DevToolsAgentMsg_SetApuAgentEnabled, bool /* enabled */)
-
 
 //-----------------------------------------------------------------------------
-// These are messages sent from the renderer to the browser.
+// These are messages sent from the browser to the renderer.
 
 // RenderViewHostDelegate::RenderViewCreated method sends this message to a
 // new renderer to notify it that it will host developer tools UI and should
@@ -114,7 +107,7 @@ IPC_MESSAGE_ROUTED0(DevToolsMsg_SetupDevToolsClient)
 
 
 //-----------------------------------------------------------------------------
-// These are messages sent from the browser to the renderer.
+// These are messages sent from the renderer to the browser.
 
 // Wraps an IPC message that's destined to the DevToolsClient on
 // DevToolsAgent->browser hop.
