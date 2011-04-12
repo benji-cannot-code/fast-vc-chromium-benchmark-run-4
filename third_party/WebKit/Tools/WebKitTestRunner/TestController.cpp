214462a97eb7cd9e8e000bf7ac93a06158d9ff83
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 02:10:14 +0000

    2011-04-11  Sam Weinig  <sam@webkit.org>
            Reviewed by Anders Carlsson.
    
            REGRESSION(r77974): http/tests/security/aboutBlank/security-context-window-open.html is failing
            <rdar://problem/8981346>
            https://bugs.webkit.org/show_bug.cgi?id=54159
    
            * platform/mac-wk2/Skipped:
            Remove http/tests/security/aboutBlank/security-context-window-open.html from the skipped list.
    2011-04-11  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Anders Carlsson.
    
            REGRESSION(r77974): http/tests/security/aboutBlank/security-context-window-open.html is failing
            <rdar://problem/8981346>
            https://bugs.webkit.org/show_bug.cgi?id=54159
    
            * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
            (WTR::InjectedBundlePage::InjectedBundlePage):
            (WTR::InjectedBundlePage::decidePolicyForNavigationAction):
            (WTR::InjectedBundlePage::decidePolicyForNewWindowAction):
            (WTR::InjectedBundlePage::decidePolicyForResponse):
            (WTR::InjectedBundlePage::unableToImplementPolicy):
            * WebKitTestRunner/InjectedBundle/InjectedBundlePage.h:
            Add short-circuited policy client which matches WebKit1.
    
            * WebKitTestRunner/TestController.cpp:
            (WTR::TestController::resetStateToConsistentValues):
            Clear the cache between tests to ensure consistent results.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit2/WKContextPrivate.h>
 #include <WebKit2/WKPageGroup.h>
 #include <WebKit2/WKPreferencesPrivate.h>
+#include <WebKit2/WKResourceCacheManager.h>
 #include <WebKit2/WKRetainPtr.h>
 #include <wtf/PassOwnPtr.h>
 
@@ -377,6 +378,9 @@ bool TestController::resetStateToConsistentValues()
     WKPreferencesSetFixedFontFamily(preferences, fixedFontFamily);
     WKPreferencesSetSansSerifFontFamily(preferences, sansSerifFontFamily);
     WKPreferencesSetSerifFontFamily(preferences, serifFontFamily);
+
+    WKResourceCacheManagerRef cacheManager = WKContextGetResourceCacheManager(m_context.get());
+    WKResourceCacheManagerClearCacheForAllOrigins(cacheManager, WKResourceCachesToClearAll);
 
     m_mainWebView->focus();
 
