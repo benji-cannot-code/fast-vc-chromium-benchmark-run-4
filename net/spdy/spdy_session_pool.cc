1cbe0dac5b2beee8e59d7c1f4fbc8e0f1cfe7a4c
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-12 15:24:34 +0000

    The IP Aliases table was not being properly cleared when we manually close all connections (which is done in the benchmark code, and also when changing networks)
    BUG=78811
    TEST=SpdySessionPoolTest.IPPoolingCleanedOnClose
    Review URL: http://codereview.chromium.org/6820052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81252 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session_pool.cc
+++ net/spdy/spdy_session_pool.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -393,9 +393,12 @@ void SpdySessionPool::CloseCurrentSessions() {
     list->pop_front();
     if (list->empty()) {
       delete list;
+      RemoveAliases(old_map.begin()->first);
       old_map.erase(old_map.begin()->first);
     }
   }
+  DCHECK(sessions_.empty());
+  DCHECK(aliases_.empty());
 }
 
 }  // namespace net
