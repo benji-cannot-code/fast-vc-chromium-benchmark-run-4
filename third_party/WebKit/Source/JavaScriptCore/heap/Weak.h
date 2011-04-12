2d39a93bf6b5f7ea1dcb3ac336eea7e715110f14
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-12 21:12:33 +0000

    2011-04-12  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
    
            Cleaned up some Vector traits, and added missing Vector traits for handles
            https://bugs.webkit.org/show_bug.cgi?id=58372
    
            * heap/Local.h: Inherit from SimpleClassVectorTraits to avoid duplication.
    
            * heap/Strong.h: Ditto.
    
            * heap/Weak.h: Ditto.
    
            * parser/JSParser.cpp: Fixed a traits error. No test case because this
            particular trait is not currently exercised by the parser.
    
            * runtime/UString.h: No need to override canInitializeWithMemset, since
            our base class sets it to true.
    
            * wtf/VectorTraits.h: Inherit from VectorTraitsBase to avoid duplication.
    
            * wtf/text/WTFString.h: No need to override canInitializeWithMemset, since
            our base class sets it to true.
    2011-04-12  Geoffrey Garen  <ggaren@apple.com>
    
            Reviewed by Sam Weinig.
    
            Cleaned up some Vector traits, and added missing Vector traits for handles
            https://bugs.webkit.org/show_bug.cgi?id=58372
    
            * platform/graphics/BitmapImage.h: Added a FIXME because the current
            Vector traits for FrameData are logically incorrect, but I couldn't find
            a place where this currently results in bad behavior, and it's not
            immediately obvious what the right solution is.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/Weak.h
+++ third_party/WebKit/Source/JavaScriptCore/heap/Weak.h
@@ -109,4 +109,12 @@ private:
 
 } // namespace JSC
 
+namespace WTF {
+
+template<typename T> struct VectorTraits<JSC::Weak<T> > : SimpleClassVectorTraits {
+    static const bool canCompareWithMemcmp = false;
+};
+
+}
+
 #endif // Weak_h
