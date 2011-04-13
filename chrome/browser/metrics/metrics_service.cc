c42449826aa3aa3176a5585cc9d5cc8e1909661f
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 18:32:04 +0000

    Remove another unnecessary MetricsService API method.
    In practice, "user allows upload" and "reporting is enabled" are the same, and there's no clear conceptual difference, so remove one to make both the API and the implementation simpler.
    
    BUG=None
    TEST=Existing tests.
    
    Review URL: http://codereview.chromium.org/6813096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -439,7 +439,6 @@ void MetricsService::DiscardOldStabilityStats(PrefService* local_state) {
 MetricsService::MetricsService()
     : recording_active_(false),
       reporting_active_(false),
-      user_permits_upload_(false),
       server_permits_upload_(true),
       state_(INITIALIZED),
       current_fetch_(NULL),
@@ -458,12 +457,8 @@ MetricsService::~MetricsService() {
   SetRecording(false);
 }
 
-void MetricsService::SetUserPermitsUpload(bool enabled) {
-  HandleIdleSinceLastTransmission(false);
-  user_permits_upload_ = enabled;
-}
-
 void MetricsService::Start() {
+  HandleIdleSinceLastTransmission(false);
   SetRecording(true);
   SetReporting(true);
 }
@@ -474,6 +469,7 @@ void MetricsService::StartRecordingOnly() {
 }
 
 void MetricsService::Stop() {
+  HandleIdleSinceLastTransmission(false);
   SetReporting(false);
   SetRecording(false);
 }
@@ -1027,9 +1023,9 @@ void MetricsService::OnHistogramSynchronizationDone() {
     return;
   }
 
-  // If we're not supposed to upload any UMA data because the response or the
-  // user said so, cancel the upload at this point, but start the timer.
-  if (!TransmissionPermitted()) {
+  // If we're not supposed to upload any UMA data because the response said so,
+  // cancel the upload at this point, but start the timer.
+  if (!ServerPermitsTransmission()) {
     DiscardPendingLog();
     StartLogTransmissionTimer();
     return;
@@ -1106,13 +1102,9 @@ void MetricsService::MakePendingLog() {
   DCHECK(pending_log());
 }
 
-bool MetricsService::TransmissionPermitted() const {
-  // If the user forbids uploading that's they're business, and we don't upload
-  // anything.  If the server forbids uploading, that's our business, so we take
-  // that to mean it forbids current logs, but we still send up the inital logs
-  // and any old logs.
-  if (!user_permits_upload_)
-    return false;
+bool MetricsService::ServerPermitsTransmission() const {
+  // If the server forbids uploading, we take that to mean it forbids current
+  // logs, but we still send up the inital logs and any old logs.
   if (server_permits_upload_)
     return true;
 
