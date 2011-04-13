95e3519cde196cb74573987c144f0038b3769969
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-13 23:04:01 +0000

    2011-04-13  Mihai Parparita  <mihaip@chromium.org>
            Reviewed by Eric Seidel.
    
            [NRWT] Rename --baseline-search-patch to --additional-platform-directory to match ORWT
            https://bugs.webkit.org/show_bug.cgi?id=58489
    
            r83743 added --additional-platform-directory to old-run-webkit-tests,
            rename the equivalent flag in new-run-webkit-tests to be consistent.
    
            * Scripts/webkitpy/layout_tests/port/base.py:
            * Scripts/webkitpy/layout_tests/port/base_unittest.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -286,7 +286,7 @@ class Port(object):
 
         baseline_filename = testname + '-expected' + suffix
 
-        baseline_search_path = self.get_option('baseline_search_path', []) + self.baseline_search_path()
+        baseline_search_path = self.get_option('additional_platform_directory', []) + self.baseline_search_path()
 
         baselines = []
         for platform_dir in baseline_search_path:
