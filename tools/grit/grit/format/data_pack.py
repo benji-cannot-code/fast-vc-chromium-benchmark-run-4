c3316305d7e9273ed5abf0b5d6bbce09744b92e4
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 11:20:38 +0000

    Add whitelist support to grit.
    - Support -w path to add whitelists to grit (and grit_info).
    - Use the whitelist to tag what items should be skipped when generating output.
    
    A whitelist is used instead of a blacklist, since there has been some other
    work into generating what resources are really needed by the build.
    
    Another option instead of whitelists would have been to add yet more conditions
    to the grd files, but they are already getting pretty complex with the
    conditions already in the files.  And looking at trying to do conditions based
    on features not only made the files more complex, but also doesn't handle the
    problem when a single feature has some strings that are only valid for a subset
    of the supported platforms.
    Review URL: http://codereview.chromium.org/6821056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81393 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/grit/grit/format/data_pack.py
+++ tools/grit/grit/format/data_pack.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python2.4
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -42,7 +42,10 @@ class DataPack(interface.ItemFormatter):
       return nodes
     if (isinstance(item, include.IncludeNode) or
         isinstance(item, message.MessageNode)):
-      return [item]
+      # Include this node if it wasn't marked as skipped by a whitelist.
+      if not item.WhitelistMarkedAsSkip():
+        return [item]
+      return nodes
     for child in item.children:
       nodes.extend(DataPack.GetDataNodes(child))
     return nodes
