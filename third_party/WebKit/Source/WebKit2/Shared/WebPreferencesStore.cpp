f4e6386d6e4abbdf0f8077821c0b47d820f1a019
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-13 21:26:45 +0000

    REGRESSION: Links are clickable when a contentEditable is set to true. https://bugs.webkit.org/show_bug.cgi?id=58473 <rdar://problem/9256793>
    Reviewed by Dan Bernstein.
    
    Added support for editable link behavior property.
    
    * Shared/WebPreferencesStore.cpp:
    * Shared/WebPreferencesStore.h:
    * UIProcess/API/C/WKPreferences.cpp:
    (WKPreferencesSetEditableLinkBehavior):
    (WKPreferencesGetEditableLinkBehavior):
    * UIProcess/API/C/WKPreferencesPrivate.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebPreferencesStore.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/WebPreferencesStore.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FontSmoothingLevel.h"
 #include "WebCoreArgumentCoders.h"
+#include <WebCore/Settings.h>
 
 namespace WebKit {
 
