c132fd945af945ea5ee976e99990f7ec6e13f0dc
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-13 16:17:37 +0000

    2011-04-13  Thierry Reding  <thierry.reding@avionic-design.de>
            Gtk+ port fails to build when enabling WebGL
            https://bugs.webkit.org/show_bug.cgi?id=58434
    
            Fix the GTK+ WebGL build after the introduction of PlatformContextCairo.
    
            * platform/graphics/GraphicsContext3D.h:
            * platform/graphics/cairo/GraphicsContext3DCairo.cpp:
            (WebCore::GraphicsContext3D::paintToCanvas):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "GraphicsContext3D.h"
+#include "PlatformContextCairo.h"
 
 #if ENABLE(WEBGL)
 
@@ -84,30 +85,31 @@ bool GraphicsContext3D::getImageData(Image* image, unsigned int format, unsigned
                       width, height, srcUnpackAlignment, format, type, alphaOp, outputVector.data());
 }
 
-void GraphicsContext3D::paintToCanvas(const unsigned char* imagePixels, int imageWidth, int imageHeight, int canvasWidth, int canvasHeight, cairo_t* context)
+void GraphicsContext3D::paintToCanvas(const unsigned char* imagePixels, int imageWidth, int imageHeight, int canvasWidth, int canvasHeight, PlatformContextCairo* context)
 {
     if (!imagePixels || imageWidth <= 0 || imageHeight <= 0 || canvasWidth <= 0 || canvasHeight <= 0 || !context)
         return;
 
-    cairo_save(context);
+    cairo_t *cr = context->cr();
+    context->save();
 
-    cairo_rectangle(context, 0, 0, canvasWidth, canvasHeight);
-    cairo_set_operator(context, CAIRO_OPERATOR_CLEAR);
-    cairo_paint(context);
+    cairo_rectangle(cr, 0, 0, canvasWidth, canvasHeight);
+    cairo_set_operator(cr, CAIRO_OPERATOR_CLEAR);
+    cairo_paint(cr);
 
     RefPtr<cairo_surface_t> imageSurface = adoptRef(cairo_image_surface_create_for_data(
         const_cast<unsigned char*>(imagePixels), CAIRO_FORMAT_ARGB32, imageWidth, imageHeight, imageWidth * 4));
 
     // OpenGL keeps the pixels stored bottom up, so we need to flip the image here.
-    cairo_translate(context, 0, imageHeight);
-    cairo_scale(context, 1, -1);
+    cairo_translate(cr, 0, imageHeight);
+    cairo_scale(cr, 1, -1);
 
-    cairo_set_operator(context, CAIRO_OPERATOR_OVER);
-    cairo_set_source_surface(context, imageSurface.get(), 0, 0);
-    cairo_rectangle(context, 0, 0, canvasWidth, -canvasHeight);
+    cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
+    cairo_set_source_surface(cr, imageSurface.get(), 0, 0);
+    cairo_rectangle(cr, 0, 0, canvasWidth, -canvasHeight);
 
-    cairo_fill(context);
-    cairo_restore(context);
+    cairo_fill(cr);
+    context->restore();
 }
 
 void GraphicsContext3D::setContextLostCallback(PassOwnPtr<ContextLostCallback>)
