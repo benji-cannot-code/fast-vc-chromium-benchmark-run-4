3c75274be9595a19daaf4fe198278882f2b0db35
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 15:23:33 +0000

    Looks to have broken unittest, see http://chromegw.corp.google.com/i/chromium/builders/Vista%20Tests%20%28dbg%29%281%29/builds/8263/steps/unit_tests/logs/TestLongSafePureFilename
    Revert 81416 - Autofill extend profiles to include multi-valued fields, part 6 (Aggregation)Extends the Autofill aggregation logic to merge profiles with a common address into a single profile with multi-valued fields.  Also, extends the web_database migration to include this new logic when migrating the web_database preferences.BUG=65625TEST=WebDatabaseMigrationTest.MigrateVersion32ToCurrent:AutofillMergeTest.DataDrivenMergeProfiles:AutofillProfileTest.*Review URL: http://codereview.chromium.org/6826059
    TBR=dhollowa@chromium.org
    Review URL: http://codereview.chromium.org/6833026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/autofill_table.h
+++ chrome/browser/webdata/autofill_table.h
@@ -270,7 +270,7 @@ class AutofillTable : public WebDatabaseTable {
   bool MigrateToVersion33ProfilesBasedOnFirstName();
   bool MigrateToVersion34ProfilesBasedOnCountryCode();
   bool MigrateToVersion35GreatBritainCountryCodes();
-  bool MigrateToVersion37MergeAndCullOlderProfiles();
+  bool MigrateToVersion36MergeAndCullOlderProfiles();
 
  private:
   FRIEND_TEST_ALL_PREFIXES(AutofillTableTest, Autofill);
