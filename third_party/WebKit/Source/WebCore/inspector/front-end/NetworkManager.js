c1a15459bc0ace4ef6ecbff570b79c59b2ca9458
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-13 13:24:02 +0000

    2011-04-13  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: make optional event parameter last to prevent regressions.
            https://bugs.webkit.org/show_bug.cgi?id=58433
    
            * inspector/Inspector.json:
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::willSendRequest):
            * inspector/front-end/NetworkManager.js:
            (WebInspector.NetworkDispatcher.prototype.requestWillBeSent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
@@ -113,7 +113,7 @@ WebInspector.NetworkDispatcher.prototype = {
         this._updateResourceWithResponse(resource, cachedResource.response);
     },
 
-    requestWillBeSent: function(identifier, frameId, loaderId, documentURL, request, redirectResponse, time, stackTrace)
+    requestWillBeSent: function(identifier, frameId, loaderId, documentURL, request, time, stackTrace, redirectResponse)
     {
         var resource = this._inflightResourcesById[identifier];
         if (resource) {
