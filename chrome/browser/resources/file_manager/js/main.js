12b51e2a3cff5ef1313f96ece5c9f029fc900218
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 21:08:00 +0000

    File Manager: Respect defaultPath, keyboard handlers
    file_manager.js: Pay attent to defaultPath, Add keyboard handlers
    file_manager_dialogs.cc: Don't encode spaces as +'s in the dialog args
    main.js: Dump params post-parse
    harness.html, harness.js: Add Default Path knob.
    
    BUG=none
    TEST=manual via harness.html and chromeos-chrome on linux
    
    Review URL: http://codereview.chromium.org/6837005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main.js
+++ chrome/browser/resources/file_manager/js/main.js
@@ -18,11 +18,17 @@ function init() {
 
   var rootPaths = ['Downloads', 'media'];
 
+  if (document.location.search) {
+    var json = decodeURIComponent(document.location.search.substr(1));
+    var params = JSON.parse(json);
+    console.log('params: ' + JSON.stringify(params));
+  }
+
   function onEntriesFound(entries) {
     FileManager.initStrings(function () {
       fileManager = new FileManager(document.body, entries, params);
     });
-  };
+  }
 
   function onFileSystemFound(filesystem) {
     var entries = [];
@@ -48,12 +54,6 @@ function init() {
       util.forEachDirEntry(filesystem.root, onEntryFound);
     }
   };
-
-  if (document.location.search) {
-    var json = decodeURIComponent(document.location.search.substr(1));
-    console.log('params: ' + json);
-    params = JSON.parse(json);
-  }
 
   util.installFileErrorToString();
 
