0eadb1c8d8c0af81e2fc414de11e219092e14dd2
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 21:25:58 +0000

    Revert 81472 - Disk Cache: Perform extra validations to data comming fromdisk to detect obvious corruption going undetected.BUG=77841TEST=net_unittestsReview URL: http://codereview.chromium.org/6821064 TBR=rvargas@google.com Review URL: http://codereview.chromium.org/6837035
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81485 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/addr.cc
+++ net/disk_cache/addr.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,20 +25,6 @@ bool Addr::SetFileNumber(int file_number) {
     return false;
   value_ = kInitializedMask | file_number;
   return true;
-}
-
-bool Addr::SanityCheck() const {
-  if (!is_initialized())
-    return !value_;
-
-  if (((value_ & kFileTypeMask) >> kFileTypeOffset) > 4)
-    return false;
-
-  if (is_separate_file())
-    return true;
-
-  const uint32 kReservedBitsMask = 0x0c000000;
-  return !(value_ & kReservedBitsMask);
 }
 
 }  // namespace disk_cache
