c73bc263eaa744c0756c140eba6df6090b3d04aa
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 05:11:21 +0000

    [cros] Mark all OOBE updates as critical.
    TBR=dpolukhin@chromium.org
    BUG=chromium-os:10068
    TEST=Manual. UI waits for update to complete disregarding /tmp/update-check-response-deadline existence or it's content.
    
    Review URL: http://codereview.chromium.org/6850010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/update_screen.cc
+++ chrome/browser/chromeos/login/update_screen.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -61,7 +61,7 @@ UpdateScreen::UpdateScreen(WizardScreenDelegate* delegate)
       checking_for_update_(true),
       reboot_check_delay_(0),
       is_downloading_update_(false),
-      is_all_updates_critical_(false) {
+      is_all_updates_critical_(true) { // See http://crosbug.com/10068
   GetInstanceSet().insert(this);
 }
 
