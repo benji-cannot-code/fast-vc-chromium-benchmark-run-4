0b59596e0ffb8d7c40748bb2bf56e0c3cfd21903
dennisjeffrey@google.com <dennisjeffrey@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 19:16:47 +0000

    Fix for bug in TestingAutomationProvider::OpenNewBrowserWindowOfType.
    The function TestingAutomationProvider::OpenNewBrowserWindowOfType
    creates a new browser window and adds a blank tab to it, but failed to
    wait for the new tab to finish loading (it only waited for the new window
    to open).  This is now fixed.
    
    BUG=47457
    TEST=This change may fix flakiness in PyAuto test content.ContentTest.testThreeWindows.
    
    Review URL: http://codereview.chromium.org/6838024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81620 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/content.py
+++ chrome/test/functional/content.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -66,7 +66,7 @@ class ContentTest(pyauto.PyUITest):
                                   ['page has no title'],
                                   ['Awesomeness'])
     test_utils.StringContentCheck(self, self.GetTabContents(0, 1),
-                                  ['Awesomeness'],
+                                  ['Title Of Awesomeness'],
                                   ['page has no title'])
     test_utils.StringContentCheck(self, self.GetTabContents(0, 2),
                                   ['Title Of More Awesomeness'],
