eb92d7b961f309f90b9aae0cb8fe0de71b8f53ad
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-14 00:25:05 +0000

    2011-04-13  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Kenneth Russell.
    
            [chromium] Fix canvas.toDataURL mimeType assertion
            https://bugs.webkit.org/show_bug.cgi?id=58425
    
            Following r81213, reassert expected toDataURL() mimeType, remove an unused variable.
    
            No new tests. Covered by existing tests.
    
            * platform/graphics/skia/ImageBufferSkia.cpp:
            (WebCore::ImageToDataURL):
            (WebCore::ImageBuffer::toDataURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
@@ -346,6 +346,8 @@ void ImageBuffer::putPremultipliedImageData(ByteArray* source, const IntSize& so
 template <typename T>
 static String ImageToDataURL(T& source, const String& mimeType, const double* quality)
 {
+    ASSERT(MIMETypeRegistry::isSupportedImageMIMETypeForEncoding(mimeType));
+
     Vector<unsigned char> encodedImage;
     if (mimeType == "image/jpeg") {
         int compressionQuality = JPEGImageEncoder::DefaultCompressionQuality;
@@ -367,9 +369,6 @@ static String ImageToDataURL(T& source, const String& mimeType, const double* qu
 
 String ImageBuffer::toDataURL(const String& mimeType, const double* quality) const
 {
-    ASSERT(MIMETypeRegistry::isSupportedImageMIMETypeForEncoding(mimeType));
-
-    Vector<unsigned char> encodedImage;
     SkDevice* device = context()->platformContext()->canvas()->getDevice();
     SkBitmap bitmap = device->accessBitmap(false);
 
