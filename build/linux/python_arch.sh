c793c05fb5d287d4eade8c488b3610d90c6a1441
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 08:38:55 +0000

    OpenBSD: compatibility fixes for the build system.
    - switch from /bin/bash to /bin/sh (bash is not a part of base system
      on OpenBSD)
    - switch from /usr/bin/python to /usr/bin/env python (python is not a part
      of base system on OpenBSD)
    
    BUG=none
    
    Review URL: http://codereview.chromium.org/6845002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81550 0039d316-1c4b-4281-b951-d872f2087c98

--- build/linux/python_arch.sh
+++ build/linux/python_arch.sh
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/bin/bash
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+#!/bin/sh
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -14,12 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 python=$(readlink -f "$1")
 if [ ! -r "$python" ]; then
   echo unknown
-  exit 0;
+  exit 0
 fi
 file_out=$(file "$python")
 if [ $? -ne 0 ]; then
   echo unknown
-  exit 0;
+  exit 0
 fi
 
 echo $file_out | grep -qs "ARM"
