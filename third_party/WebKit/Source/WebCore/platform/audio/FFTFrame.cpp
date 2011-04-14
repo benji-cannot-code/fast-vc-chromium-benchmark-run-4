2f18c2247f39ec57292c2d728e8289f4fe09a171
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-14 18:19:49 +0000

    2011-04-14  Kenneth Russell  <kbr@google.com>
            Reviewed by Dimitri Glazkov.
    
            Fix Web Audio build on Linux Debug and with stub FFTFrame
            https://bugs.webkit.org/show_bug.cgi?id=58555
    
            Built Chromium/Linux in Debug mode to test.
    
            * platform/audio/FFTFrame.cpp:
            * platform/audio/FFTFrameStub.cpp:
            (WebCore::FFTFrame::initialize):
            * webaudio/AudioContext.cpp:
            * webaudio/DefaultAudioDestinationNode.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/FFTFrame.cpp
+++ third_party/WebKit/Source/WebCore/platform/audio/FFTFrame.cpp
@@ -33,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FFTFrame.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 #include <wtf/Complex.h>
 #include <wtf/MathExtras.h>
 #include <wtf/OwnPtr.h>
--- third_party/WebKit/Source/WebCore/webaudio/AudioContext.cpp
+++ third_party/WebKit/Source/WebCore/webaudio/AudioContext.cpp
@@ -54,6 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 #include "RealtimeAnalyserNode.h"
 
+#if DEBUG_AUDIONODE_REFERENCES
+#include <stdio.h>
+#endif
+
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 #include <wtf/RefCounted.h>
