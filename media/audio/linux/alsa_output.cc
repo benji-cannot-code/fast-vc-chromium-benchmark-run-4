cb19b83075bba33f0452341c4b0de310a96d642e
davej@chromium.org <davej@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 23:31:33 +0000

    Fix Linux audio playback
    Playing audio files shorter than about 11000 samples on Linux was either not working at all, or re-playing (starting and stopping on the same audio object) was working only intermittently.  It was possible to stop audio playback before the first buffer was played, resulting in no sound at all.  Also, if replaying, the buffer may have been dirty from the previous playback on the same device.
    
    So on Linux at least, these issues appear fixed.
    
    BUG=73045,47761,59367,59369,59370,65618
    TEST=Manual, quickly playing short audio files on Linux should work
    
    Review URL: http://codereview.chromium.org/6776024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81669 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/linux/alsa_output.cc
+++ media/audio/linux/alsa_output.cc
@@ -396,6 +396,14 @@ void AlsaPcmOutputStream::StartTask() {
     return;
   }
 
+  if (shared_data_.state() != kIsPlaying) {
+    return;
+  }
+
+  // Before starting, the buffer might have audio from previous user of this
+  // device.
+  buffer_->Clear();
+
   // When starting again, drop all packets in the device and prepare it again
   // incase we are restarting from a pause state and need to flush old data.
   int error = wrapper_->PcmDrop(playback_handle_);
@@ -539,6 +547,10 @@ void AlsaPcmOutputStream::WritePacket() {
     return;
   }
 
+  if (shared_data_.state() == kIsStopped) {
+    return;
+  }
+
   CHECK_EQ(buffer_->forward_bytes() % bytes_per_output_frame_, 0u);
 
   const uint8* buffer_data;
@@ -580,6 +592,14 @@ void AlsaPcmOutputStream::WritePacket() {
       // Seek forward in the buffer after we've written some data to ALSA.
       buffer_->Seek(frames_written * bytes_per_output_frame_);
     }
+  } else {
+    // If nothing left to write and playback hasn't started yet, start it now.
+    // This ensures that shorter sounds will still play.
+    if (playback_handle_ &&
+        (wrapper_->PcmState(playback_handle_) == SND_PCM_STATE_PREPARED) &&
+        GetCurrentDelay() > 0) {
+      wrapper_->PcmStart(playback_handle_);
+    }
   }
 }
 
@@ -587,6 +607,10 @@ void AlsaPcmOutputStream::WriteTask() {
   DCHECK_EQ(message_loop_, MessageLoop::current());
 
   if (stop_stream_) {
+    return;
+  }
+
+  if (shared_data_.state() == kIsStopped) {
     return;
   }
 
