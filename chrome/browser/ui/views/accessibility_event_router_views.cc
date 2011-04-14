16ed677465483e4e4300259d620e0cf3e4cc41ab
chaitanyag@chromium.org <chaitanyag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 20:32:28 +0000

    Make the usage stats checkbox on EULA screen speak and focus.
    BUG=chromium-os:14128
    TEST=Test manually, see issue 14128
    Review URL: http://codereview.chromium.org/6852008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/accessibility_event_router_views.cc
+++ chrome/browser/ui/views/accessibility_event_router_views.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/notification_type.h"
 #include "ui/base/models/combobox_model.h"
 #include "ui/base/accessibility/accessible_view_state.h"
+#include "views/controls/button/checkbox.h"
 #include "views/controls/button/custom_button.h"
 #include "views/controls/button/menu_button.h"
 #include "views/controls/button/native_button.h"
@@ -142,7 +143,10 @@ void AccessibilityEventRouterViews::DispatchAccessibilityNotification(
 
   most_recent_profile_ = profile;
   std::string class_name = view->GetClassName();
-  if (class_name == views::MenuButton::kViewClassName ||
+
+  if (class_name == views::Checkbox::kViewClassName) {
+    SendCheckboxNotification(view, type, profile);
+  } else if (class_name == views::MenuButton::kViewClassName ||
       type == NotificationType::ACCESSIBILITY_MENU_OPENED ||
       type == NotificationType::ACCESSIBILITY_MENU_CLOSED) {
     SendMenuNotification(view, type, profile);
@@ -279,3 +283,15 @@ void AccessibilityEventRouterViews::SendComboboxNotification(
       profile, name, value, state.index, state.count);
   SendAccessibilityNotification(type, &info);
 }
+
+void AccessibilityEventRouterViews::SendCheckboxNotification(
+    views::View* view, NotificationType type, Profile* profile) {
+  ui::AccessibleViewState state;
+  view->GetAccessibleState(&state);
+  std::string name = UTF16ToUTF8(state.name);
+  std::string value = UTF16ToUTF8(state.value);
+  AccessibilityCheckboxInfo info(
+      profile, name, state.state == ui::AccessibilityTypes::STATE_CHECKED);
+  SendAccessibilityNotification(type, &info);
+}
+
