64c8beba923f390c0fe7f73a700d622f9b151450
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-14 08:27:13 +0000

    2011-04-14  Tony Gentilcore  <tonyg@chromium.org>
            Reviewed by Kent Tamura.
    
            [chromium] http/tests/loading/preload-append-scan.php fails
            https://bugs.webkit.org/show_bug.cgi?id=58352
    
            On chromium, onload-vs-immediate-refresh always caused the subsequent test to fail
            with an additional didFinishLoadForFrame message. This was because the test
            navigates via a refresh, but did not actually wait for the navigation to complete.
    
            This patch fixes that by adding a waitUntilDone() and calling notifyDone() after the
            redirect target page loads. Now the log for this test shows both navigations and no
            part of the navigation leaks into the next test.
    
            Also, since dumpAsText() shows alerts, there was no reason to write vs. alert the
            SUCCESS message.
    
            * http/tests/loading/onload-vs-immediate-refresh-expected.txt:
            * http/tests/loading/onload-vs-immediate-refresh.pl:
            * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/loading/onload-vs-immediate-refresh.pl
+++ third_party/WebKit/LayoutTests/http/tests/loading/onload-vs-immediate-refresh.pl
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 select (STDOUT);
 $| = 1;
 
-print "Refresh: 0;url=data:text/plain,You should have seen an alert.\r\n";
+print "Refresh: 0;url=data:text/html,<body onload='if (window.layoutTestController) layoutTestController.notifyDone();'>You should have seen an alert.\r\n";
 print "Content-Type: text/html\r\n";
 print "\r\n";
 
@@ -15,10 +15,12 @@ print << "EOF";
 <html>
 <head>
 <script>
-if (window.layoutTestController)
+if (window.layoutTestController) {
     layoutTestController.dumpAsText();
+    layoutTestController.waitUntilDone();
+}
 </script>
-<meta http-equiv="refresh" content="0;url=data:text/plain,You should have seen an alert.">
+<meta http-equiv="refresh" content="0;url=data:text/html,%3Cbody%20onload='if(window.layoutTestController)layoutTestController.notifyDone();'%3EYou%20should%20have%20seen%20an%20alert.">
 </head>
 EOF
 
@@ -27,7 +29,7 @@ for ($count=1; $count<20000; $count++) {
 }
 
 print << "EOF";
-<body onload="if (window.layoutTestController) document.write('<p>SUCCESS</p>'); else alert('SUCCESS');">
+<body onload="alert('SUCCESS');">
 </body>
 </html>
 EOF
