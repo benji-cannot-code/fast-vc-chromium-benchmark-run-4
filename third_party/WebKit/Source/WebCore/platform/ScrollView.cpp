f231abc4c8e3882edbd3e4ad787bee738fb46b9f
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-14 22:09:04 +0000

    https://bugs.webkit.org/show_bug.cgi?id=56493 Drag-scrolling overlay scrollbars thumb in overflow regions does not work -and corresponding- <rdar://problem/9112688>
    Reviewed by Simon Fraser.
    
    There was an original change to fix this in the normal hit-testing case, but hit 
    testing of transformed and/or positioned objects was still broken. The transformed 
    case is fixed by sending an OverlayScrollbarSizeRelevancy parameter to 
    calculateRects(). Getting positioned objects right is a little trickier. Those need 
    to opt into using temporary clip rects during hit testing. To avoid doing that when 
    it is not necessary, I added a new bit to ScrollView to track whether there are 
    currently overlay scrollbars painted in the view.
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::ScrollView):
    (WebCore::ScrollView::wheelEvent):
    * platform/ScrollView.h:
    (WebCore::ScrollView::containsScrollableAreaWithOverlayScrollbars):
    (WebCore::ScrollView::setContainsScrollableAreaWithOverlayScrollbars):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::paintOverflowControls):
    (WebCore::RenderLayer::hitTestLayer):
    (WebCore::RenderLayer::updateClipRects):
    (WebCore::RenderLayer::calculateClipRects):
    (WebCore::RenderLayer::parentClipRects):
    (WebCore::RenderLayer::backgroundClipRect):
    (WebCore::RenderLayer::calculateRects):
    * rendering/RenderLayer.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
@@ -58,6 +58,7 @@ ScrollView::ScrollView()
     , m_paintsEntireContents(false)
     , m_clipsRepaints(true)
     , m_delegatesScrolling(false)
+    , m_containsScrollableAreaWithOverlayScrollbars(false)
 {
     platformInit();
 }
@@ -983,6 +984,9 @@ void ScrollView::paint(GraphicsContext* context, const IntRect& rect)
         return;
 
     notifyPageThatContentAreaWillPaint();
+
+    // If we encounter any overlay scrollbars as we paint, this will be set to true.
+    m_containsScrollableAreaWithOverlayScrollbars = false;
     
     IntRect documentDirtyRect = rect;
     documentDirtyRect.intersect(frameRect());
--- third_party/WebKit/Source/WebCore/platform/ScrollView.h
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.h
@@ -283,6 +283,9 @@ public:
     virtual IntPoint convertFromScrollbarToContainingView(const Scrollbar*, const IntPoint&) const;
     virtual IntPoint convertFromContainingViewToScrollbar(const Scrollbar*, const IntPoint&) const;
 
+    bool containsScrollableAreaWithOverlayScrollbars() const { return m_containsScrollableAreaWithOverlayScrollbars; }
+    void setContainsScrollableAreaWithOverlayScrollbars(bool contains) { m_containsScrollableAreaWithOverlayScrollbars = contains; }
+
 protected:
     ScrollView();
 
@@ -352,6 +355,8 @@ private:
     bool m_paintsEntireContents;
     bool m_clipsRepaints;
     bool m_delegatesScrolling;
+
+    bool m_containsScrollableAreaWithOverlayScrollbars;
 
     IntSize m_boundsSize;
 
