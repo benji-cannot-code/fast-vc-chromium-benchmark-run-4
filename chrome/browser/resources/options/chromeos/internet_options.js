53c98316d1ddfae9fa17a0489ff7e46ae98791cb
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-14 23:45:14 +0000

    Remove non-functional 802.1X widgets from Wi-Fi Connection Details panel.
    Get rid of identity, certificate path, and private key password fields,
    in favor of the versions built into the "Join Wi-Fi network" panel.
    
    BUG=chromium-os:13892
    TEST=Join a public Wi-Fi network, open connection details, verify it does not say "access is protected".  Join a password-protected Wi-Fi network, verify connection details says "access is protected".  Join an 802.1X PEAP network, verify connection details does not have widgets related to certificates and private keys, and does say "access is protected".  Using the enterprise extension for Google-A, verify you can still connect to that network.
    
    Review URL: http://codereview.chromium.org/6825083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81675 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/internet_options.js
+++ chrome/browser/resources/options/chromeos/internet_options.js
@@ -360,21 +360,8 @@ cr.define('options', function() {
         $('autoConnectNetwork').disabled = true;
       }
       page.removeAttribute('password');
-      page.removeAttribute('cert');
-      page.removeAttribute('certPkcs');
       if (data.encrypted) {
-        if (data.certNeeded) {
-          if (data.certInPkcs) {
-            page.setAttribute('certPkcs', true);
-            $('inetIdentPkcs').textContent = data.ident;
-          } else {
-            page.setAttribute('cert', true);
-            $('inetIdent').value = data.ident;
-            $('inetCert').value = data.certPath;
-          }
-        } else {
-          page.setAttribute('password', true);
-        }
+        page.setAttribute('password', true);
       }
     } else if(data.type == 5) {
       if (!data.gsm)
@@ -383,8 +370,6 @@ cr.define('options', function() {
         OptionsPage.showTab($('cellularConnNavTab'));
       page.removeAttribute('ethernet');
       page.removeAttribute('wireless');
-      page.removeAttribute('cert');
-      page.removeAttribute('certPkcs');
       page.setAttribute('cellular', true);
       $('serviceName').textContent = data.serviceName;
       $('networkTechnology').textContent = data.networkTechnology;
@@ -443,8 +428,6 @@ cr.define('options', function() {
       OptionsPage.showTab($('internetNavTab'));
       page.setAttribute('ethernet', true);
       page.removeAttribute('wireless');
-      page.removeAttribute('cert');
-      page.removeAttribute('certPkcs');
       page.removeAttribute('cellular');
       page.removeAttribute('gsm');
     }
