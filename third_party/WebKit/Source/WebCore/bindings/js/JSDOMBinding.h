3eb25dc898a81807c6b15aa4225beee7c2bc0a3b
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 23:38:04 +0000

    2011-04-15  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Oliver Hunt.
    
            DOM object handles are never removed from cache
            https://bugs.webkit.org/show_bug.cgi?id=58707
    
            We were trying to remove hash table items by value instead of by key.
    
            * bindings/js/DOMWrapperWorld.cpp:
            (WebCore::JSNodeHandleOwner::finalize): Changed to work more like
            DOMObjectHandleOwner::finalize because I'm going to merge them.
    
            (WebCore::DOMObjectHandleOwner::finalize): Remove hash table items
            by key, not value. (Oops!) Use a helper function to make sure we get
            this right.
    
            * bindings/js/JSDOMBinding.cpp:
            (WebCore::cacheDOMObjectWrapper): Store the hash table key as our weak
            handle context, so we can use it at destruction time.
    
            * bindings/js/JSDOMBinding.h: Removed unnecessary include.
    
            * bindings/js/JSNodeCustom.h:
            (WebCore::cacheDOMNodeWrapper): Store the hash table key as our weak
            handle context, so we can use it at destruction time.
    
            * bindings/js/ScriptWrappable.h:
            (WebCore::ScriptWrappable::setWrapper): Forward context parameter, to
            support the above.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include <runtime/Completion.h>
 #include <runtime/Lookup.h>
-#include <runtime/WeakGCMap.h>
 #include <wtf/Forward.h>
 #include <wtf/Noncopyable.h>
 
--- third_party/WebKit/Source/WebCore/bindings/js/ScriptWrappable.h
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptWrappable.h
@@ -44,9 +44,9 @@ public:
         return m_wrapper.get();
     }
 
-    void setWrapper(JSC::JSGlobalData& globalData, DOMObject* wrapper, JSC::WeakHandleOwner* wrapperOwner)
+    void setWrapper(JSC::JSGlobalData& globalData, DOMObject* wrapper, JSC::WeakHandleOwner* wrapperOwner, void* context)
     {
-        m_wrapper.set(globalData, wrapper, wrapperOwner);
+        m_wrapper.set(globalData, wrapper, wrapperOwner, context);
     }
 
     void clearWrapper()
