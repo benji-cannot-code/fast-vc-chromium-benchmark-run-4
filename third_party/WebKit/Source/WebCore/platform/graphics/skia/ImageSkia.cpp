76e399f7b63fe147d9344a1be734c0cd8cc538d2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 18:45:07 +0000

    2011-04-15  Mike Reed  <reed@google.com>
            Reviewed by Darin Fisher.
    
            hide unused static function when SKIA_GPU is enabled
            https://bugs.webkit.org/show_bug.cgi?id=58670
    
            No new tests. Just fixes a warning (treated as an error) when SKIA_GPU is enabled
    
            * platform/graphics/skia/ImageSkia.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -67,6 +67,7 @@ enum ResamplingMode {
     RESAMPLE_AWESOME,
 };
 
+#if !ENABLE(SKIA_GPU)
 static ResamplingMode computeResamplingMode(PlatformContextSkia* platformContext, const NativeImageSkia& bitmap, int srcWidth, int srcHeight, float destWidth, float destHeight)
 {
     if (platformContext->hasImageResamplingHint()) {
@@ -151,6 +152,7 @@ static ResamplingMode computeResamplingMode(PlatformContextSkia* platformContext
     
     return RESAMPLE_LINEAR;
 }
+#endif
 
 // Draws the given bitmap to the given canvas. The subset of the source bitmap
 // identified by src_rect is drawn to the given destination rect. The bitmap
