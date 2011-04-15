352b77340ac1b21bb2affb2768d059fc58b89be4
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 21:45:42 +0000

    Bug 58696 - DFG JIT split handling of vars/temporaries
    Reviewed by Sam Weinig.
    
    Presently all callee registers are treated as having single block scope,
    since the DFG JIT can only compile single block functions. In order to
    expand the JIT to support control flow we will need to change to retaining
    locals (but not temporaries) across basic block boundaries.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::ByteCodeParser):
    (JSC::DFG::ByteCodeParser::get):
    (JSC::DFG::ByteCodeParser::set):
    (JSC::DFG::ByteCodeParser::getVariable):
    (JSC::DFG::ByteCodeParser::setVariable):
    (JSC::DFG::ByteCodeParser::getTemporary):
    (JSC::DFG::ByteCodeParser::setTemporary):
    (JSC::DFG::ByteCodeParser::getArgument):
    (JSC::DFG::ByteCodeParser::getInt32Constant):
    (JSC::DFG::ByteCodeParser::getDoubleConstant):
    (JSC::DFG::ByteCodeParser::getJSConstant):
    (JSC::DFG::ByteCodeParser::constantUndefined):
    (JSC::DFG::ByteCodeParser::constantNull):
    (JSC::DFG::ByteCodeParser::one):
    (JSC::DFG::ByteCodeParser::parseBlock):
    (JSC::DFG::ByteCodeParser::parse):
    (JSC::DFG::parse):
    * dfg/DFGNode.h:
    * dfg/DFGScoreBoard.h:
    (JSC::DFG::ScoreBoard::ScoreBoard):
    (JSC::DFG::ScoreBoard::~ScoreBoard):
    (JSC::DFG::ScoreBoard::allocate):
    (JSC::DFG::ScoreBoard::use):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DFG_CONSISTENCY_CHECK 0
 // Emit a breakpoint into the head of every generated function, to aid debugging in GDB.
 #define DFG_JIT_BREAK_ON_ENTRY 0
+// Disable the DFG JIT without having to touch Platform.h!
+#define DFG_DEBUG_LOCAL_DISBALE 0
 
 
 namespace JSC { namespace DFG {
