409e8a19fed20cd759de25ad7a16b92e77e72233
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 23:15:51 +0000

    Mac: Fix official build failure from r81810.
    BUG=none
    TEST=Mac Official Build goes green.
    Review URL: http://codereview.chromium.org/6873022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run_mac.mm
+++ chrome/browser/first_run/first_run_mac.mm
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/google_update_settings.h"
 
+#if defined(GOOGLE_CHROME_BUILD)
+#include "chrome/browser/browser_process.h"
+#endif
+
 namespace {
 
 // Show the search engine selection dialog.
