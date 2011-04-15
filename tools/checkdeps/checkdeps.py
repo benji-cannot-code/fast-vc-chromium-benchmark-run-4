9ae6159d20c4f870e2685fba88bd4128dff7dea4
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 16:33:09 +0000

    Fix up checkdeps - use the options to get the root, don't assume the layout of sys.argv. - if no root was set, use __file__ instead of assuming sys.argv[0] will be a full path.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6871014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81752 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/checkdeps/checkdeps.py
+++ tools/checkdeps/checkdeps.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
-# Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -445,9 +445,9 @@ def main(options, args):
   global BASE_DIRECTORY
   if not options.base_directory:
     BASE_DIRECTORY = os.path.abspath(
-        os.path.join(os.path.abspath(sys.argv[0]), "../../.."))
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "../.."))
   else:
-    BASE_DIRECTORY = os.path.abspath(sys.argv[2])
+    BASE_DIRECTORY = os.path.abspath(options.base_directory)
 
   # Figure out which directory we have to check.
   if len(args) == 0:
