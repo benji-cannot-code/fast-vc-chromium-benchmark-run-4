2401993f0bc330b3b1ee4867067e23a2d1801224
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 17:36:04 +0000

    web-ui settings: Fix setting enabled state of commit button.
    BUG=78683
    TEST=Verify that commit button is always disabled when there are no browsers to import from.
    Review URL: http://codereview.chromium.org/6873010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/import_data_overlay.js
+++ chrome/browser/resources/options/import_data_overlay.js
@@ -167,8 +167,8 @@ cr.define('options', function() {
       ImportDataOverlay.getInstance().updateCheckboxes_();
     }
     $('import-browsers').disabled = state;
-    $('import-data-commit').disabled = state;
     $('import-throbber').style.visibility = state ? "visible" : "hidden";
+    ImportDataOverlay.getInstance().validateCommitButton_();
   };
 
   /**
