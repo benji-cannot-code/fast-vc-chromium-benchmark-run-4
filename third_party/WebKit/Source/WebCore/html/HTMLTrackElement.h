572ba08652fa212804382c12256fa8e598dc2a9d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 07:13:49 +0000

    2011-04-15  Anna Cavender  <annacc@chromium.org>
            Reviewed by Eric Carlson.
    
            Renaming TRACK feature define to VIDEO_TRACK
            https://bugs.webkit.org/show_bug.cgi?id=53556
    
            * configure.ac:
    2011-04-15  Anna Cavender  <annacc@chromium.org>
    
            Reviewed by Eric Carlson.
    
            Renaming TRACK feature define to VIDEO_TRACK
            https://bugs.webkit.org/show_bug.cgi?id=53556
    
            * Configurations/FeatureDefines.xcconfig:
    2011-04-15  Anna Cavender  <annacc@chromium.org>
    
            Reviewed by Eric Carlson.
    
            Renaming TRACK feature define to VIDEO_TRACK
            https://bugs.webkit.org/show_bug.cgi?id=53556
    
            No new tests. No new functionality.
    
            * CMakeLists.txt:
            * Configurations/FeatureDefines.xcconfig:
            * DerivedSources.make:
            * GNUmakefile.am:
            * features.pri:
            * html/HTMLTagNames.in:
            * html/HTMLTrackElement.cpp:
            * html/HTMLTrackElement.h:
            * html/HTMLTrackElement.idl:
    2011-04-15  Anna Cavender  <annacc@chromium.org>
    
            Reviewed by Eric Carlson.
    
            Renaming TRACK feature define to VIDEO_TRACK
            https://bugs.webkit.org/show_bug.cgi?id=53556
    
            * Configurations/FeatureDefines.xcconfig:
    2011-04-15  Anna Cavender  <annacc@chromium.org>
    
            Reviewed by Eric Carlson.
    
            Renaming TRACK feature define to VIDEO_TRACK
            https://bugs.webkit.org/show_bug.cgi?id=53556
    
            * Configurations/FeatureDefines.xcconfig:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLTrackElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLTrackElement.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLTrackElement_h
 #define HTMLTrackElement_h
 
-#if ENABLE(TRACK)
+#if ENABLE(VIDEO_TRACK)
 
 #include "HTMLElement.h"
 
