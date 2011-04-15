f575d5c2e79e7721a62234cda834b99c116711b0
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 20:51:24 +0000

    2011-04-15  Michael Saboff  <msaboff@apple.com>
            Reviewed by Oliver Hunt.
    
            globalObject moved to JSObjectWithGlobalObject.cpp inhibits inlining
            https://bugs.webkit.org/show_bug.cgi?id=58677
    
            Moved JSObjectWithGlobalObject::globalObject() to 
            runtime/JSObjectWithGlobalObject.h to allow the compiler to inline
            it for a performance benefit.  An equivalent instance had been in
            a header file before r60057.
    
            * JavaScriptCore.exp:
            * runtime/JSObjectWithGlobalObject.cpp:
            * runtime/JSObjectWithGlobalObject.h:
            (JSC::JSObjectWithGlobalObject::globalObject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObjectWithGlobalObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObjectWithGlobalObject.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JSObjectWithGlobalObject_h
 #define JSObjectWithGlobalObject_h
 
-#include "JSObject.h"
+#include "JSGlobalObject.h"
 
 namespace JSC {
 
@@ -40,7 +40,10 @@ public:
         return Structure::create(globalData, proto, TypeInfo(ObjectType, StructureFlags), AnonymousSlotCount, &s_info);
     }
 
-    JSGlobalObject* globalObject() const;
+    JSGlobalObject* globalObject() const
+    {
+        return asGlobalObject((getAnonymousValue(GlobalObjectSlot).asCell()));
+    }
 
 protected:
     JSObjectWithGlobalObject(JSGlobalObject*, NonNullPassRefPtr<Structure>);
