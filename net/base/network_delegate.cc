4d530b62cf711bc4af90e5a6cce223acae7c338a
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 19:14:12 +0000

    Allow extensions to redirect requests in onBeforeRequest.
    BUG=60101
    TEST=no
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=81479
    
    Review URL: http://codereview.chromium.org/6677148
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81782 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_delegate.cc
+++ net/base/network_delegate.cc
@@ -10,20 +10,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 int NetworkDelegate::NotifyBeforeURLRequest(URLRequest* request,
-                                            CompletionCallback* callback) {
+                                            CompletionCallback* callback,
+                                            GURL* new_url) {
   DCHECK(CalledOnValidThread());
   DCHECK(request);
   DCHECK(callback);
-  return OnBeforeURLRequest(request, callback);
+  return OnBeforeURLRequest(request, callback, new_url);
 }
 
 int NetworkDelegate::NotifyBeforeSendHeaders(uint64 request_id,
-                                             HttpRequestHeaders* headers,
-                                             CompletionCallback* callback) {
+                                             CompletionCallback* callback,
+                                             HttpRequestHeaders* headers) {
   DCHECK(CalledOnValidThread());
   DCHECK(headers);
   DCHECK(callback);
-  return OnBeforeSendHeaders(request_id, headers, callback);
+  return OnBeforeSendHeaders(request_id, callback, headers);
 }
 
 void NetworkDelegate::NotifyResponseStarted(URLRequest* request) {
--- net/url_request/url_request_test_util.h
+++ net/url_request/url_request_test_util.h
@@ -191,14 +191,17 @@ class TestNetworkDelegate : public net::NetworkDelegate {
 
   int last_os_error() const { return last_os_error_; }
   int error_count() const { return error_count_; }
+  int created_requests() const { return created_requests_; }
+  int destroyed_requests() const { return destroyed_requests_; }
 
- private:
+ protected:
   // net::NetworkDelegate:
   virtual int OnBeforeURLRequest(net::URLRequest* request,
-                                 net::CompletionCallback* callback);
+                                 net::CompletionCallback* callback,
+                                 GURL* new_url);
   virtual int OnBeforeSendHeaders(uint64 request_id,
-                                  net::HttpRequestHeaders* headers,
-                                  net::CompletionCallback* callback);
+                                  net::CompletionCallback* callback,
+                                  net::HttpRequestHeaders* headers);
   virtual void OnResponseStarted(net::URLRequest* request);
   virtual void OnReadCompleted(net::URLRequest* request, int bytes_read);
   virtual void OnURLRequestDestroyed(net::URLRequest* request);
@@ -207,6 +210,8 @@ class TestNetworkDelegate : public net::NetworkDelegate {
 
   int last_os_error_;
   int error_count_;
+  int created_requests_;
+  int destroyed_requests_;
 };
 
 #endif  // NET_URL_REQUEST_URL_REQUEST_TEST_UTIL_H_
