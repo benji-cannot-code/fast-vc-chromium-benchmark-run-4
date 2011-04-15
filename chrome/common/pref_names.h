16c904758fddae88ea3c3e1b8fc9921853698bee
sanjeevr@chromium.org <sanjeevr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 05:09:40 +0000

    Disabled polling for jobs when we lose XMPP connection. This behavior can be overridden with a pref switch.
    BUG=None
    TEST=Cloud Print proxy notifications
    Review URL: http://codereview.chromium.org/6853024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81704 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -435,6 +435,7 @@ extern const char kCloudPrintAuthToken[];
 extern const char kCloudPrintXMPPAuthToken[];
 extern const char kCloudPrintEmail[];
 extern const char kCloudPrintPrintSystemSettings[];
+extern const char kCloudPrintEnableJobPoll[];
 
 extern const char kRemotingHasSetupCompleted[];
 extern const char kRemotingHostEnabled[];
