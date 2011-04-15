bbea9af39d6a0c7e48e9c041f805487ca395fab1
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 07:39:40 +0000

    2011-04-15  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            CSP should block string arguments to setTimeout and setInterval unless options eval-script
            https://bugs.webkit.org/show_bug.cgi?id=58610
    
            It's somewhat sadness that the JSC and V8 code for setTimeout and
            setInterval are so different.  I struggled for a while with how to
            handle the worker case, but I decided to punt on it for now.
    
            Tests: http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-allowed.html
                   http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-blocked.html
                   http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-allowed.html
                   http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-blocked.html
    
            * bindings/js/JSDOMWindowCustom.cpp:
            (WebCore::JSDOMWindow::setTimeout):
            (WebCore::JSDOMWindow::setInterval):
            * bindings/js/JSWorkerContextCustom.cpp:
            (WebCore::JSWorkerContext::setTimeout):
            (WebCore::JSWorkerContext::setInterval):
            * bindings/js/ScheduledAction.cpp:
            (WebCore::ScheduledAction::create):
            * bindings/js/ScheduledAction.h:
            * bindings/v8/custom/V8DOMWindowCustom.cpp:
            (WebCore::WindowSetTimeoutImpl):
            * page/ContentSecurityPolicy.cpp:
            (WebCore::ContentSecurityPolicy::allowEval):
            * page/ContentSecurityPolicy.h:
    2011-04-15  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            CSP should block string arguments to setTimeout and setInterval unless options eval-script
            https://bugs.webkit.org/show_bug.cgi?id=58610
    
            Herein lies a four-way testing matrix.
    
            * http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-allowed-expected.txt: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-allowed.html: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-blocked-expected.txt: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setInterval-blocked.html: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-allowed-expected.txt: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-allowed.html: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-blocked-expected.txt: Added.
            * http/tests/security/contentSecurityPolicy/eval-scripts-setTimeout-blocked.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83954 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/ScheduledAction.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/ScheduledAction.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ScheduledAction.h"
 
+#include "ContentSecurityPolicy.h"
 #include "DOMWindow.h"
 #include "Document.h"
 #include "Frame.h"
@@ -47,11 +48,13 @@ using namespace JSC;
 
 namespace WebCore {
 
-PassOwnPtr<ScheduledAction> ScheduledAction::create(ExecState* exec, DOMWrapperWorld* isolatedWorld)
+PassOwnPtr<ScheduledAction> ScheduledAction::create(ExecState* exec, DOMWrapperWorld* isolatedWorld, ContentSecurityPolicy* policy)
 {
     JSValue v = exec->argument(0);
     CallData callData;
     if (getCallData(v, callData) == CallTypeNone) {
+        if (policy && !policy->allowEval())
+            return 0;
         UString string = v.toString(exec);
         if (exec->hadException())
             return 0;
