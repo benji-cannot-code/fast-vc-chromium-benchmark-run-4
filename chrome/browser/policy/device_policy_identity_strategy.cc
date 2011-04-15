2b9859801ae4d66e5a940013d67ab70952900af2
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 19:05:22 +0000

    Don't complain about empty machine ids.
    BUG=None
    TEST=Compiles
    
    Review URL: http://codereview.chromium.org/6874008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/device_policy_identity_strategy.cc
+++ chrome/browser/policy/device_policy_identity_strategy.cc
@@ -95,8 +95,6 @@ bool DevicePolicyIdentityStrategy::GetCredentials(std::string* username,
 
 void DevicePolicyIdentityStrategy::OnDeviceTokenAvailable(
     const std::string& token) {
-  DCHECK(!machine_id_.empty());
-
   device_token_ = token;
   NotifyDeviceTokenChanged();
 }
