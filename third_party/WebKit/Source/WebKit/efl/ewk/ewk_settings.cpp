105b91aa9834f73ddb0fad3180bf3da396206404
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-15 02:12:05 +0000

    2011-04-14  Grzegorz Czajkowski  <g.czajkowski@samsung.com>
            Reviewed by Antonio Gomes.
    
            Memory cache API
            https://bugs.webkit.org/show_bug.cgi?id=58016
    
            * ewk/ewk_settings.cpp:
            (ewk_settings_cache_enable_get):
            (ewk_settings_cache_enable_set):
            (ewk_settings_cache_capacity_set):
            * ewk/ewk_settings.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@83931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_settings.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_settings.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Image.h"
 #include "IntSize.h"
 #include "KURL.h"
+#include "MemoryCache.h"
 #include "ewk_private.h"
 
 #include <Eina.h>
@@ -324,6 +325,46 @@ const char* ewk_settings_proxy_uri_get(void)
 #elif USE(CURL)
     EINA_SAFETY_ON_TRUE_RETURN_VAL(1, 0);
 #endif
+}
+
+/**
+ * Gets status of the memory cache of WebCore.
+ *
+ * @return @c EINA_TRUE if the cache is enabled or @c EINA_FALSE if not
+ */
+Eina_Bool ewk_settings_cache_enable_get(void)
+{
+    WebCore::MemoryCache* cache = WebCore::memoryCache();
+    return !cache->disabled();
+}
+
+/**
+ * Enables/disables the memory cache of WebCore, possibly clearing it.
+ *
+ * Disabling the cache will remove all resources from the cache.
+ * They may still live on if they are referenced by some Web page though.
+ *
+ * @param set @c EINA_TRUE to enable memory cache, @c EINA_FALSE to disable
+ */
+void ewk_settings_cache_enable_set(Eina_Bool set)
+{
+    WebCore::MemoryCache* cache = WebCore::memoryCache();
+    set = !set;
+    if (cache->disabled() != set)
+        cache->setDisabled(set);
+}
+
+/**
+ * Sets capacity of memory cache of WebCore.
+ *
+ * WebCore sets default value of memory cache on 8192 * 1024 bytes.
+ *
+ * @param capacity the maximum number of bytes that the cache should consume overall
+ */
+void ewk_settings_cache_capacity_set(unsigned capacity)
+{
+    WebCore::MemoryCache* cache = WebCore::memoryCache();
+    cache->setCapacities(0, capacity, capacity);
 }
 
 /**
