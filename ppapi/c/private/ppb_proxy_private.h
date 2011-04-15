6912b3c3d84f7700759c6ff78e453b98d7c1aaa4
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 23:26:57 +0000

    Pre-buffer data from the host to the plugin in the PPAPI proxy to reduce the impact of IPC latency on streaming data. This allows most resource requests to be returned synchronously from the URLLoader in the plugin.
    TEST=manual
    BUG=none
    Review URL: http://codereview.chromium.org/6870018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81833 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_proxy_private.h
+++ ppapi/c/private/ppb_proxy_private.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_module.h"
 #include "ppapi/c/pp_resource.h"
 
-#define PPB_PROXY_PRIVATE_INTERFACE "PPB_Proxy_Private;3"
+#define PPB_PROXY_PRIVATE_INTERFACE "PPB_Proxy_Private;4"
 
 // Exposes functions needed by the out-of-process proxy to call into the
 // renderer PPAPI implementation.
@@ -34,6 +34,11 @@ struct PPB_Proxy_Private {
   void (*SetReserveInstanceIDCallback)(
       PP_Module module,
       PP_Bool (*is_seen)(PP_Module, PP_Instance));
+
+  // Returns the number of bytes synchronously readable out of the URLLoader's
+  // buffer. Returns 0 on failure or if the url loader doesn't have any data
+  // now.
+  int32_t (*GetURLLoaderBufferedBytes)(PP_Resource url_loader);
 };
 
 #endif  // PPAPI_C_PRIVATE_PROXY_PRIVATE_H_
