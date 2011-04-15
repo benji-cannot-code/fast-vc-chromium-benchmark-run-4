c1dccfcdb76ccfb9366a5db368bb436a755edbe4
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 08:52:39 +0000

    Fix the unit test expectations to handle Australia
    This fixes:
    http://src.chromium.org/viewvc/chrome?view=rev&revision=81707
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6872004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81713 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/language_options_handler_unittest.cc
+++ chrome/browser/ui/webui/options/language_options_handler_unittest.cc
@@ -99,7 +99,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   chromeos::InputMethodDescriptors descriptors = CreateInputMethodDescriptors();
   scoped_ptr<ListValue> list(
       chromeos::CrosLanguageOptionsHandler::GetLanguageList(descriptors));
-  ASSERT_EQ(6U, list->GetSize());
+  ASSERT_EQ(7U, list->GetSize());
 
   DictionaryValue* entry = NULL;
   std::string language_code;
@@ -108,7 +108,17 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
 
   // As shown below, the list should be sorted by the display names,
   // and these names should not have duplicates.
+
+  // This comes from kExtraLanguages.
   ASSERT_TRUE(list->GetDictionary(0, &entry));
+  ASSERT_TRUE(entry->GetString("code", &language_code));
+  ASSERT_TRUE(entry->GetString("displayName", &display_name));
+  ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
+  EXPECT_EQ("en-AU", language_code);
+  EXPECT_EQ("English (Australia)", display_name);
+  EXPECT_EQ("English (Australia)", native_display_name);
+
+  ASSERT_TRUE(list->GetDictionary(1, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
@@ -117,7 +127,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   EXPECT_EQ("English (United States)", native_display_name);
 
   // This comes from kExtraLanguages.
-  ASSERT_TRUE(list->GetDictionary(1, &entry));
+  ASSERT_TRUE(list->GetDictionary(2, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
@@ -125,7 +135,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   EXPECT_EQ("Filipino", display_name);
   EXPECT_EQ("Filipino", native_display_name);
 
-  ASSERT_TRUE(list->GetDictionary(2, &entry));
+  ASSERT_TRUE(list->GetDictionary(3, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
@@ -134,7 +144,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   EXPECT_EQ("fran\u00E7ais", native_display_name);
 
   // This comes from kExtraLanguages.
-  ASSERT_TRUE(list->GetDictionary(3, &entry));
+  ASSERT_TRUE(list->GetDictionary(4, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
@@ -142,7 +152,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   EXPECT_EQ("Indonesian", display_name);
   EXPECT_EQ("Bahasa Indonesia", native_display_name);
 
-  ASSERT_TRUE(list->GetDictionary(4, &entry));
+  ASSERT_TRUE(list->GetDictionary(5, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
@@ -151,7 +161,7 @@ TEST(LanguageOptionsHandlerTest, GetLanguageList) {
   EXPECT_EQ("\u65E5\u672C\u8A9E", native_display_name);
 
   // This comes from kExtraLanguages.
-  ASSERT_TRUE(list->GetDictionary(5, &entry));
+  ASSERT_TRUE(list->GetDictionary(6, &entry));
   ASSERT_TRUE(entry->GetString("code", &language_code));
   ASSERT_TRUE(entry->GetString("displayName", &display_name));
   ASSERT_TRUE(entry->GetString("nativeDisplayName", &native_display_name));
