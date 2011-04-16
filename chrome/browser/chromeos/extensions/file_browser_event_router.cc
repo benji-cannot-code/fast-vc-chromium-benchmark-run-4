23eb2ec202a2087a68efaf7c162706cc193c5193
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-16 00:08:44 +0000

    Hiding device notifications for devices that have been removed
    BUG=chromium-os:14110
    TEST= 1. Put removable device into chromeos machine. Pop up notification should
          emerge. While device is being scanned, remove the device from the machine.
          Notification should go away.
          2. Close chromeos machine lid and reopen it. After machine wakes up no 
          "Removable device detected" notification should be seen, unless a 
          removable device was attached to the machine while it was sleeping. 
    Review URL: http://codereview.chromium.org/6865029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_event_router.cc
+++ chrome/browser/chromeos/extensions/file_browser_event_router.cc
@@ -163,6 +163,7 @@ void ExtensionFileBrowserEventRouter::OnDeviceAdded(
 
 void ExtensionFileBrowserEventRouter::OnDeviceRemoved(
     const std::string& system_path) {
+  HideDeviceNotification(system_path);
 }
 
 void ExtensionFileBrowserEventRouter::OnDeviceScanned(
