ae1e5d35d2a9debe186e4daeabc3deef9320fcd4
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-17 14:04:43 +0000

    Fix missing include.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6869049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_store_interface.cc
+++ chrome/browser/policy/configuration_policy_store_interface.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/values.h"
 #include "chrome/browser/policy/configuration_policy_store_interface.h"
 
 namespace {
