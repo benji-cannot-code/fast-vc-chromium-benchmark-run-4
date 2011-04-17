5ac44d7275ab92820f418dcda05067e6e6086afa
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-17 21:09:12 +0000

    2011-04-17  Joseph Pecoraro  <joepeck@webkit.org>
            Reviewed by Dan Bernstein.
    
            Assign to self in WebInspectorWindowController constructors
            https://bugs.webkit.org/show_bug.cgi?id=58749
    
            * WebCoreSupport/WebInspectorClient.mm:
            (-[WebInspectorWindowController init]):
            (-[WebInspectorWindowController initWithInspectedWebView:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
+++ third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebInspectorClient.mm
@@ -217,7 +217,7 @@ void WebInspectorFrontendClient::updateWindowTitle() const
 @implementation WebInspectorWindowController
 - (id)init
 {
-    if (![super initWithWindow:nil])
+    if (!(self = [super initWithWindow:nil]))
         return nil;
 
     // Keep preferences separate from the rest of the client, making sure we are using expected preference values.
@@ -261,10 +261,10 @@ void WebInspectorFrontendClient::updateWindowTitle() const
 
 - (id)initWithInspectedWebView:(WebView *)webView
 {
-    if (![self init])
+    if (!(self = [self init]))
         return nil;
 
-    // Don't retain to avoid a circular reference
+    // Don't retain to avoid a circular reference.
     _inspectedWebView = webView;
     return self;
 }
