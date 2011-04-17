a58bf46256c4f76e1eaeaf7d9622a77e501d4c58
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-17 22:38:08 +0000

    Fix some typos. R=thakis@chromium.org Review URL: http://codereview.chromium.org/6873038
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81919 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/mac_util.mm
+++ base/mac/mac_util.mm
@@ -18,7 +18,7 @@ namespace mac {
 
 namespace {
 
-// a count of currently outstanding requests for full screen mode from browser
+// The current count of outstanding requests for full screen mode from browser
 // windows, plugins, etc.
 int g_full_screen_requests[kNumFullScreenModes] = { 0, 0, 0};
 
@@ -76,7 +76,7 @@ bool WasLaunchedAsLoginItem() {
 }
 
 // Looks into Shared File Lists corresponding to Login Items for the item
-// representing the current application. If such an item is found, returns
+// representing the current application. If such an item is found, returns a
 // retained reference to it. Caller is responsible for releasing the reference.
 LSSharedFileListItemRef GetLoginItemForApp() {
   ScopedCFTypeRef<LSSharedFileListRef> login_items(LSSharedFileListCreate(
@@ -277,7 +277,7 @@ bool SetFileBackupExclusion(const FilePath& file_path, bool exclude) {
   bool success =
       CSBackupSetItemExcluded((CFURLRef)url, exclude, true) == noErr;
   if (!success)
-    LOG(WARNING) << "Failed to set backup excluson for file '"
+    LOG(WARNING) << "Failed to set backup exclusion for file '"
                  << file_path.value().c_str() << "'.  Continuing.";
   return success;
 }
