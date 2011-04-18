8d4fceff29d5a66a1e1b2dff7468e4ac7fca5e5f
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-18 21:08:28 +0000

    Bug 58817 - DFG JIT - if speculative compilation fails, throw away code.
    Reviewed by Geoff Garen.
    
    If we detect a logical conflict, throw away generated code,
    and only compile through the NonSpeculativeJIT.
    
    * assembler/AbstractMacroAssembler.h:
    (JSC::AbstractMacroAssembler::rewindToLabel):
    * assembler/AssemblerBuffer.h:
    (JSC::AssemblerBuffer::rewindToOffset):
    * assembler/MacroAssemblerX86Common.h:
    (JSC::MacroAssemblerX86Common::branchAdd32):
    (JSC::MacroAssemblerX86Common::branchSub32):
    * assembler/X86Assembler.h:
    (JSC::X86Assembler::rewindToLabel):
    (JSC::X86Assembler::X86InstructionFormatter::rewindToLabel):
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::linkSpeculationChecks):
    (JSC::DFG::JITCompiler::compileFunction):
    * dfg/DFGNode.h:
    * dfg/DFGNonSpeculativeJIT.cpp:
    (JSC::DFG::NonSpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::SpeculationCheckIndexIterator::SpeculationCheckIndexIterator):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
@@ -34,7 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Consistency check contents compiler data structures.
 #define DFG_CONSISTENCY_CHECK 0
 // Emit a breakpoint into the head of every generated function, to aid debugging in GDB.
-#define DFG_JIT_BREAK_ON_ENTRY 0
+#define DFG_JIT_BREAK_ON_EVERY_FUNCTION 0
+// Emit a breakpoint into the head of every generated node, to aid debugging in GDB.
+#define DFG_JIT_BREAK_ON_EVERY_BLOCK 0
+// Emit a breakpoint into the head of every generated node, to aid debugging in GDB.
+#define DFG_JIT_BREAK_ON_EVERY_NODE 0
 // Disable the DFG JIT without having to touch Platform.h!
 #define DFG_DEBUG_LOCAL_DISBALE 0
 // Generate stats on how successful we were in making use of the DFG jit, and remaining on the hot path.
