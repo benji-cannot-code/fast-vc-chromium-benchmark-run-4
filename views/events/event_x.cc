3b35117b67fce81d1c212ec503ba627cb4982971
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 17:42:52 +0000

    Add extra touch information and related API to views::TouchEvent.
    Some touch devices provide, other than (x,y) location, extra touch information. views::TouchEvent is changed to keep 
    track of touch radius, touch angle and ratio between major and minor touch axis. 
    
    To be able to query what extra touch data is supported by the device, bookkeeping of the device supported TouchParams 
    is added to views::TouchFactory.
    
    Next step will be routing these touch information to WebKit.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/6820004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81963 0039d316-1c4b-4281-b951-d872f2087c98

--- views/events/event_x.cc
+++ views/events/event_x.cc
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/root_view.h"
 #include "views/widget/widget_gtk.h"
 
+#if defined(HAVE_XINPUT2)
+#include "views/touchui/touch_factory.h"
+#endif
+
 namespace views {
 
 namespace {
@@ -227,6 +231,55 @@ uint16 GetCharacterFromXKeyEvent(XKeyEvent* key) {
           result.length() == 1) ? result[0] : 0;
 }
 
+float GetTouchRadiusFromXEvent(XEvent* xev) {
+  float diameter = 0.0;
+
+#if defined(HAVE_XINPUT2)
+  TouchFactory* touch_factory = TouchFactory::GetInstance();
+  touch_factory->ExtractTouchParam(*xev, TouchFactory::TP_TOUCH_MAJOR,
+                                   &diameter);
+#endif
+
+  return diameter / 2.0;
+}
+
+float GetTouchAngleFromXEvent(XEvent* xev) {
+  float angle = 0.0;
+
+#if defined(HAVE_XINPUT2)
+  TouchFactory* touch_factory = TouchFactory::GetInstance();
+  touch_factory->ExtractTouchParam(*xev, TouchFactory::TP_ORIENTATION,
+                                   &angle);
+#endif
+
+  return angle;
+}
+
+
+float GetTouchRatioFromXEvent(XEvent* xev) {
+  float ratio = 1.0;
+
+#if defined(HAVE_XINPUT2)
+  TouchFactory* touch_factory = TouchFactory::GetInstance();
+  float major_v = -1.0;
+  float minor_v = -1.0;
+
+  if (!touch_factory->ExtractTouchParam(*xev,
+                                        TouchFactory::TP_TOUCH_MAJOR,
+                                        &major_v) ||
+      !touch_factory->ExtractTouchParam(*xev,
+                                        TouchFactory::TP_TOUCH_MINOR,
+                                        &minor_v))
+    return ratio;
+
+  // In case minor axis exists but is zero.
+  if (minor_v > 0.0)
+    ratio = major_v / minor_v;
+#endif
+
+  return ratio;
+}
+
 }  // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -319,7 +372,10 @@ TouchEvent::TouchEvent(NativeEvent2 native_event_2,
     : LocatedEvent(GetTouchEventType(native_event_2),
                    GetEventLocation(native_event_2),
                    GetLocatedEventFlags(native_event_2, true)),
-      touch_id_(GetTouchIDFromXEvent(native_event_2)) {
+      touch_id_(GetTouchIDFromXEvent(native_event_2)),
+      radius_(GetTouchRadiusFromXEvent(native_event_2)),
+      angle_(GetTouchAngleFromXEvent(native_event_2)),
+      ratio_(GetTouchRatioFromXEvent(native_event_2)) {
 }
 #endif
 
--- views/view_unittest.cc
+++ views/view_unittest.cc
@@ -507,7 +507,8 @@ TEST_F(ViewTest, TouchEvent) {
                        400,
                        400,
                        0, /* no flags */
-                       0  /* first finger touch */);
+                       0, /* first finger touch */
+                       1.0, 0.0, 1.0);
   root->OnTouchEvent(unhandled);
 
   EXPECT_EQ(v1->last_touch_event_type_, 0);
@@ -527,7 +528,8 @@ TEST_F(ViewTest, TouchEvent) {
                      110,
                      120,
                      0, /* no flags */
-                     0  /* first finger touch */);
+                     0, /* first finger touch */
+                     1.0, 0.0, 1.0);
   v2->last_touch_event_was_handled_ = true;
   root->OnTouchEvent(pressed);
 
@@ -549,7 +551,8 @@ TEST_F(ViewTest, TouchEvent) {
                      50,
                      40,
                      0, /* no flags */
-                     0  /* first finger touch */);
+                     0, /* first finger touch */
+                     1.0, 0.0, 1.0);
   root->OnTouchEvent(dragged);
   EXPECT_EQ(v2->last_touch_event_type_, ui::ET_TOUCH_MOVED);
   EXPECT_EQ(v2->location_.x(), -50);
@@ -564,7 +567,8 @@ TEST_F(ViewTest, TouchEvent) {
   // Released event out of bounds. Should still go to v2
   v1->Reset();
   v2->Reset();
-  TouchEvent released(ui::ET_TOUCH_RELEASED, 0, 0, 0, 0 /* first finger */);
+  TouchEvent released(ui::ET_TOUCH_RELEASED, 0, 0, 0, 0 /* first finger */,
+                      1.0, 0.0, 1.0);
   v2->last_touch_event_was_handled_ = true;
   root->OnTouchEvent(released);
   EXPECT_EQ(v2->last_touch_event_type_, ui::ET_TOUCH_RELEASED);
