8339a0e586b981cfed7f52e81103dd1d259003e4
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 09:52:02 +0000

    Delete PrerenderManager on UI thread.
    BUG=77930,79582
    TEST=None
    
    Review URL: http://codereview.chromium.org/6864017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.h
+++ chrome/browser/prerender/prerender_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/timer.h"
 #include "chrome/browser/prerender/prerender_contents.h"
+#include "content/browser/browser_thread.h"
 #include "googleurl/src/gurl.h"
 
 class Profile;
@@ -39,7 +40,9 @@ namespace prerender {
 
 // PrerenderManager is responsible for initiating and keeping prerendered
 // views of webpages.
-class PrerenderManager : public base::RefCountedThreadSafe<PrerenderManager> {
+class PrerenderManager
+    : public base::RefCountedThreadSafe<PrerenderManager,
+                                        BrowserThread::DeleteOnUIThread> {
  public:
   // PrerenderManagerMode is used in a UMA_HISTOGRAM, so please do not
   // add in the middle.
@@ -53,6 +56,10 @@ class PrerenderManager : public base::RefCountedThreadSafe<PrerenderManager> {
 
   // Owned by a Profile object for the lifetime of the profile.
   explicit PrerenderManager(Profile* profile);
+
+  // Destructor should no be called explicitly, but needs to be public
+  // for DeleteOnUIThread.
+  virtual ~PrerenderManager();
 
   // Preloads the URL supplied.  alias_urls indicates URLs that redirect
   // to the same URL to be preloaded. Returns true if the URL was added,
@@ -133,8 +140,6 @@ class PrerenderManager : public base::RefCountedThreadSafe<PrerenderManager> {
 
  protected:
   struct PendingContentsData;
-
-  virtual ~PrerenderManager();
 
   void SetPrerenderContentsFactory(
       PrerenderContents::Factory* prerender_contents_factory);
