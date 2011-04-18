d80ae30596982c4784a1cc7e329bc1ea470fd8e0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 21:56:58 +0000

    Fix ARM build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82004 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/issue_response_handler.cc
+++ chrome/browser/chromeos/login/issue_response_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/stringprintf.h"
 #include "chrome/browser/net/chrome_url_request_context.h"
 #include "chrome/common/net/url_fetcher.h"
 #include "net/base/load_flags.h"
