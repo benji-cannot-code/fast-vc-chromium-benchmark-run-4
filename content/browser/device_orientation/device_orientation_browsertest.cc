3315cd3f2b0208ada1eac365a0512131dd14072e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 21:28:36 +0000

    Ensure no chrome includes in content\common and content\ppapi_plugin.  Also tighten a little in device_orientation. Review URL: http://codereview.chromium.org/6883022
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82001 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/device_orientation/device_orientation_browsertest.cc
+++ content/browser/device_orientation/device_orientation_browsertest.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/test/in_process_browser_test.h"
 #include "chrome/test/ui_test_utils.h"
 #include "content/browser/device_orientation/orientation.h"
 #include "content/browser/device_orientation/provider.h"
 #include "content/browser/tab_contents/tab_contents.h"
+#include "content/common/content_switches.h"
 
 namespace device_orientation {
 
