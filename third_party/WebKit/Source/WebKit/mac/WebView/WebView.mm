88399bf0e3987ecbf797439c14a8f7a62ef58b22
cwzwarich@webkit.org <cwzwarich@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-18 22:20:03 +0000

    Reviewed by Timothy Hatcher.
    Stop using -[NSString initWithContentsOfFile:]
    https://bugs.webkit.org/show_bug.cgi?id=58763
    
    -[NSString initWithContentsOfFile:] was deprecated in 10.4, so we should stop using it.
    
    * WebView/WebView.mm:
    (leakMailQuirksUserScriptContents):
    (leakOutlookQuirksUserScriptContents):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
@@ -644,7 +644,8 @@ static bool shouldEnableLoadDeferring()
 static NSString *leakMailQuirksUserScriptContents()
 {
     NSString *scriptPath = [[NSBundle bundleForClass:[WebView class]] pathForResource:@"MailQuirksUserScript" ofType:@"js"];
-    return [[NSString alloc] initWithContentsOfFile:scriptPath];
+    NSStringEncoding encoding;
+    return [[NSString alloc] initWithContentsOfFile:scriptPath usedEncoding:&encoding error:0];
 }
 
 - (void)_injectMailQuirksScript
@@ -664,7 +665,8 @@ static bool needsOutlookQuirksScript()
 static NSString *leakOutlookQuirksUserScriptContents()
 {
     NSString *scriptPath = [[NSBundle bundleForClass:[WebView class]] pathForResource:@"OutlookQuirksUserScript" ofType:@"js"];
-    return [[NSString alloc] initWithContentsOfFile:scriptPath];
+    NSStringEncoding encoding;
+    return [[NSString alloc] initWithContentsOfFile:scriptPath usedEncoding:&encoding error:0];
 }
 
 -(void)_injectOutlookQuirksScript
