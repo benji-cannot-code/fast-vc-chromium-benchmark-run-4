b55c816835e4870f4e2019697c4903cfd7af4523
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 17:46:24 +0000

    git: Check in a script to auto-update copyright headers.
    Review URL: http://codereview.chromium.org/6878018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81964 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/git/update-copyrights.sh
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+tmp=$(mktemp)
+trap "rm -f $tmp" EXIT
+git diff --name-only $(git cl upstream)... | while read file; do
+    cp "$file" "$tmp"
+    # Rather than editing the temporary file, edit the original file in-place
+    # so that we preserve file modes.
+    sed -i -e '1,4s/Copyright .c. .* The Chromium Authors/Copyright (c) 2011 The Chromium Authors/' "$file"
+    if ! diff -q "$file" "$tmp" > /dev/null; then
+        echo "updated $file"
+    fi
+done
