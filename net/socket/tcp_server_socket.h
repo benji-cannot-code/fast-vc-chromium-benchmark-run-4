974371f339e7ac247f6603e525d4a02ebe1f0460
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 23:03:32 +0000

    Added TCPServerSocket.
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/6820057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82020 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/socket/tcp_server_socket.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SOCKET_TCP_SERVER_SOCKET_H_
+#define NET_SOCKET_TCP_SERVER_SOCKET_H_
+
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include "net/socket/tcp_server_socket_win.h"
+#elif defined(OS_POSIX)
+#include "net/socket/tcp_server_socket_libevent.h"
+#endif
+
+namespace net {
+
+#if defined(OS_WIN)
+typedef TCPServerSocketWin TCPServerSocket;
+#elif defined(OS_POSIX)
+typedef TCPServerSocketLibevent TCPServerSocket;
+#endif
+
+}  // namespace net
+
+#endif  // NET_SOCKET_TCP_SERVER_SOCKET_H_
