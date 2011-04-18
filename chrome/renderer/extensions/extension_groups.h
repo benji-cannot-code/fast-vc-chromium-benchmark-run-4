b0f266b6068443f14a33d19dd6d33bdd00d62637
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-18 21:28:24 +0000

    Move extension_groups.h to the extensions subdir of renderer. Review URL: http://codereview.chromium.org/6877014
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82000 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_groups.h
+++ chrome/renderer/extensions/extension_groups.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_RENDERER_EXTENSION_GROUPS_H_
-#define CHROME_RENDERER_EXTENSION_GROUPS_H_
+#ifndef CHROME_RENDERER_EXTENSIONS_EXTENSION_GROUPS_H_
+#define CHROME_RENDERER_EXTENSIONS_EXTENSION_GROUPS_H_
 #pragma once
 
 // A set of extension groups for use with WebKit::registerExtension and
@@ -16,4 +16,4 @@ enum ExtensionGroups {
 };
 
 
-#endif  // CHROME_RENDERER_EXTENSION_GROUPS_H_
+#endif  // CHROME_RENDERER_EXTENSIONS_EXTENSION_GROUPS_H_
