c0668b966ecae09f2d023afee314f590dc98e512
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 19:26:44 +0000

    Revert 82120 - Reland r81454 (sync non-frontend datatype controllers) with longer timeouts.Original codereview: http://codereview.chromium.org/6811003BUG=77964TEST=unitCommitted: http://src.chromium.org/viewvc/chrome?view=rev&revision=82028Review URL: http://codereview.chromium.org/6869016 TBR=zea@chromium.org Review URL: http://codereview.chromium.org/6873084
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82141 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_autofill_unittest.cc
+++ chrome/browser/sync/profile_sync_service_autofill_unittest.cc
@@ -208,7 +208,8 @@ class AutofillEntryFactory : public AbstractAutofillFactory {
       ProfileMock* profile,
       ProfileSyncService* service) {
     return new AutofillDataTypeController(factory,
-        profile);
+        profile,
+        service);
   }
 
   void SetExpectation(ProfileSyncFactoryMock* factory,
@@ -228,7 +229,8 @@ class AutofillProfileFactory : public AbstractAutofillFactory {
       ProfileMock* profile,
       ProfileSyncService* service) {
     return new AutofillProfileDataTypeController(factory,
-        profile);
+        profile,
+        service);
    }
 
   void SetExpectation(ProfileSyncFactoryMock* factory,
@@ -301,8 +303,6 @@ class ProfileSyncServiceAutofillTest : public AbstractProfileSyncServiceTest {
     service_.reset(
         new TestProfileSyncService(&factory_, &profile_, "test_user", false,
                                    task));
-    EXPECT_CALL(profile_, GetProfileSyncService()).WillRepeatedly(
-        Return(service_.get()));
     AutofillDataTypeController* data_type_controller =
         factory->CreateDataTypeController(&factory_,
             &profile_,
@@ -671,6 +671,7 @@ TEST_F(ProfileSyncServiceAutofillTest, HasNativeEntriesEmptySync) {
 }
 
 TEST_F(ProfileSyncServiceAutofillTest, HasProfileEmptySync) {
+
   std::vector<AutofillProfile*> profiles;
   std::vector<AutofillProfile> expected_profiles;
   // Owned by GetAutofillProfiles caller.
