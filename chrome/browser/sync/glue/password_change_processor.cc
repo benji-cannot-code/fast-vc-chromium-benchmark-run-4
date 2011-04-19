dc53f836f4190e9070b890b717686a43b02a86bb
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 22:29:27 +0000

    [Sync] Ensure we don't hold a transaction when we access password store.
    BUG=70658
    TEST=ProfileSyncSerivcePasswordTest.EnsureNoTransactions
    
    Review URL: http://codereview.chromium.org/6878038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82174 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/password_change_processor.cc
+++ chrome/browser/sync/glue/password_change_processor.cc
@@ -131,7 +131,6 @@ void PasswordChangeProcessor::ApplyChangesFromSyncModel(
   DCHECK(expected_loop_ == MessageLoop::current());
   if (!running())
     return;
-  StopObserving();
 
   sync_api::ReadNode password_root(trans);
   if (!password_root.InitByTagLookup(kPasswordTag)) {
@@ -140,9 +139,8 @@ void PasswordChangeProcessor::ApplyChangesFromSyncModel(
     return;
   }
 
-  PasswordModelAssociator::PasswordVector new_passwords;
-  PasswordModelAssociator::PasswordVector updated_passwords;
-  PasswordModelAssociator::PasswordVector deleted_passwords;
+  DCHECK(deleted_passwords_.empty() && new_passwords_.empty() &&
+         updated_passwords_.empty());
 
   for (int i = 0; i < change_count; ++i) {
     if (sync_api::SyncManager::ChangeRecord::ACTION_DELETE ==
@@ -155,7 +153,7 @@ void PasswordChangeProcessor::ApplyChangesFromSyncModel(
       const sync_pb::PasswordSpecificsData& password = extra->unencrypted();
       webkit_glue::PasswordForm form;
       PasswordModelAssociator::CopyPassword(password, &form);
-      deleted_passwords.push_back(form);
+      deleted_passwords_.push_back(form);
       model_associator_->Disassociate(changes[i].id);
       continue;
     }
@@ -179,20 +177,31 @@ void PasswordChangeProcessor::ApplyChangesFromSyncModel(
     if (sync_api::SyncManager::ChangeRecord::ACTION_ADD == changes[i].action) {
       std::string tag(PasswordModelAssociator::MakeTag(password));
       model_associator_->Associate(&tag, sync_node.GetId());
-      new_passwords.push_back(password);
+      new_passwords_.push_back(password);
     } else {
       DCHECK(sync_api::SyncManager::ChangeRecord::ACTION_UPDATE ==
              changes[i].action);
-      updated_passwords.push_back(password);
+      updated_passwords_.push_back(password);
     }
   }
+}
 
-  if (!model_associator_->WriteToPasswordStore(&new_passwords,
-                                               &updated_passwords,
-                                               &deleted_passwords)) {
+void PasswordChangeProcessor::CommitChangesFromSyncModel() {
+  DCHECK(expected_loop_ == MessageLoop::current());
+  if (!running())
+    return;
+  StopObserving();
+
+  if (!model_associator_->WriteToPasswordStore(&new_passwords_,
+                                               &updated_passwords_,
+                                               &deleted_passwords_)) {
     error_handler()->OnUnrecoverableError(FROM_HERE, "Error writing passwords");
     return;
   }
+
+  deleted_passwords_.clear();
+  new_passwords_.clear();
+  updated_passwords_.clear();
 
   StartObserving();
 }
