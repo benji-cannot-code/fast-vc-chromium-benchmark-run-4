b18bff3e2e59c52d7f33658253fd8ac85a54aa5e
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 17:01:36 +0000

    Fix some omnibox extension icon issues: * API naming: call it what it is -- rename GetSpecialIcon[...]() to GetIconIfExtensionMatch() * Fix layout/alignment problems since extension icons are 16x16 instead of 19x19.  (The new constant in LocationBarView will also be useful to me later in an infobar change.)
    While I'm mucking with the AutocompelteResultView, this cleans up a ton of crappiness in there, mostly related to the "touch" subclass that has been reworked to extend its parent in a much moe lightweight way.
    
    BUG=none
    TEST=Install an extension that provides an omnibox keyword; icons in the dropdown and in the special "keyword mode" omnibox appearance should align with each other and the other icons.
    Review URL: http://codereview.chromium.org/6878036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82112 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
+++ chrome/browser/autocomplete/autocomplete_popup_view_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -416,7 +416,7 @@ void AutocompletePopupViewMac::PositionPopup(const CGFloat matrixHeight) {
 
 NSImage* AutocompletePopupViewMac::ImageForMatch(
     const AutocompleteMatch& match) {
-  const SkBitmap* bitmap = model_->GetSpecialIconForMatch(match);
+  const SkBitmap* bitmap = model_->GetIconIfExtensionMatch(match);
   if (bitmap)
     return gfx::SkBitmapToNSImage(*bitmap);
 
