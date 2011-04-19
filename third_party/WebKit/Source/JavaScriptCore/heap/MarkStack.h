d7ee021849f8fd07ab6426cb535e64e4013774b8
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-19 22:33:13 +0000

    2011-04-19  Oliver Hunt  <oliver@apple.com>
            Reviewed by Gavin Barraclough.
    
            Remove unneeded deprecated methods from MarkStack
            https://bugs.webkit.org/show_bug.cgi?id=58853
    
            Remove deprecated methods
    
            * heap/MarkStack.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/MarkStack.h
+++ third_party/WebKit/Source/JavaScriptCore/heap/MarkStack.h
@@ -61,17 +61,8 @@ namespace JSC {
             ASSERT(m_values.isEmpty());
         }
 
-        void deprecatedAppend(JSValue*);
         void deprecatedAppend(JSCell**);
-        void deprecatedAppend(Register*);
         template <typename T> void append(WriteBarrierBase<T>*);
-        
-        ALWAYS_INLINE void deprecatedAppendValues(Register* registers, size_t count, MarkSetProperties properties = NoNullValues)
-        {
-            JSValue* values = reinterpret_cast<JSValue*>(registers);
-            if (count)
-                m_markSets.append(MarkSet(values, values + count, properties));
-        }
 
         void appendValues(WriteBarrierBase<Unknown>* barriers, size_t count, MarkSetProperties properties = NoNullValues)
         {
@@ -230,12 +221,6 @@ namespace JSC {
         internalAppend(*value);
     }
 
-    ALWAYS_INLINE void MarkStack::deprecatedAppend(JSValue* value)
-    {
-        ASSERT(value);
-        internalAppend(*value);
-    }
-    
     ALWAYS_INLINE void MarkStack::append(JSValue* value)
     {
         ASSERT(value);
@@ -246,12 +231,6 @@ namespace JSC {
     {
         ASSERT(value);
         internalAppend(*value);
-    }
-
-    ALWAYS_INLINE void MarkStack::deprecatedAppend(Register* value)
-    {
-        ASSERT(value);
-        internalAppend(value->jsValue());
     }
 
     ALWAYS_INLINE void MarkStack::internalAppend(JSValue value)
