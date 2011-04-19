5adb7f7f96e8a70719cbfbaaf06edb92d58083bc
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-19 19:03:34 +0000

    2011-04-19  Stephen White  <senorblanco@chromium.org>
            Reviewed by Kenneth Russell.
    
            Refactor GLES2Canvas state application out into its own function.
            https://bugs.webkit.org/show_bug.cgi?id=58906
    
            Covered by existing tests in fast/canvas and canvas/philip.
    
            * platform/graphics/chromium/GLES2Canvas.cpp:
            (WebCore::GLES2Canvas::applyState):
            New function to apply compositing and clipping state.
            (WebCore::GLES2Canvas::fillPath):
            (WebCore::GLES2Canvas::fillRect):
            (WebCore::GLES2Canvas::beginShadowDraw):
            (WebCore::GLES2Canvas::endShadowDraw):
            Refactor calls to applyState().
            * platform/graphics/chromium/GLES2Canvas.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/GLES2Canvas.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/GLES2Canvas.cpp
@@ -236,6 +236,13 @@ void GLES2Canvas::clearRect(const FloatRect& rect)
     }
 }
 
+void GLES2Canvas::applyState()
+{
+    bindFramebuffer();
+    m_context->applyCompositeOperator(m_state->m_compositeOp);
+    applyClipping(m_state->clippingEnabled());
+}
+
 void GLES2Canvas::scissorClear(float x, float y, float width, float height)
 {
     int intX = static_cast<int>(x + 0.5f);
@@ -257,10 +264,7 @@ void GLES2Canvas::fillPath(const Path& path)
         endShadowDraw(path.boundingRect());
     }
 
-    bindFramebuffer();
-    m_context->applyCompositeOperator(m_state->m_compositeOp);
-    applyClipping(m_state->clippingEnabled());
-
+    applyState();
     fillPathInternal(path, m_state->applyAlpha(m_state->m_fillColor));
 }
 
@@ -272,10 +276,7 @@ void GLES2Canvas::fillRect(const FloatRect& rect, const Color& color, ColorSpace
         endShadowDraw(rect);
     }
 
-    bindFramebuffer();
-    m_context->applyCompositeOperator(m_state->m_compositeOp);
-    applyClipping(m_state->clippingEnabled());
-
+    applyState();
     fillRectInternal(rect, color);
 }
 
@@ -778,9 +779,7 @@ void GLES2Canvas::beginShadowDraw()
         m_context->clearColor(Color(RGBA32(0)));
         m_context->clear(GraphicsContext3D::COLOR_BUFFER_BIT);
     } else {
-        bindFramebuffer();
-        m_context->applyCompositeOperator(m_state->m_compositeOp);
-        applyClipping(m_state->clippingEnabled());
+        applyState();
     }
 }
 
@@ -855,19 +854,14 @@ void GLES2Canvas::endShadowDraw(const FloatRect& boundingBox)
             std::swap(srcBuffer, dstBuffer);
 
             // Upsample srcBuffer -> main framebuffer using bicubic filtering.
-            bindFramebuffer();
-            m_context->applyCompositeOperator(m_state->m_compositeOp);
-            applyClipping(m_state->clippingEnabled());
+            applyState();
             m_context->bindTexture(GraphicsContext3D::TEXTURE_2D, srcBuffer->colorBuffer());
             FloatRect dstRect = srcRect;
             dstRect.scale(scaleFactor);
             drawTexturedQuadMitchell(srcBuffer->size(), flipRect(srcRect), dstRect, AffineTransform(), 1.0);
         } else {
             // Blur in Y directly to framebuffer.
-            bindFramebuffer();
-            m_context->applyCompositeOperator(m_state->m_compositeOp);
-            applyClipping(m_state->clippingEnabled());
-
+            applyState();
             convolveRect(srcBuffer->colorBuffer(), srcBuffer->size(), flipRect(srcRect), srcRect, imageIncrementY, kernel.get(), kernelWidth);
         }
     }
