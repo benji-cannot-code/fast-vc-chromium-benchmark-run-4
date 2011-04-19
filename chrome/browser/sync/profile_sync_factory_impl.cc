bd8dd5c7e9cfc499d83051a9e0f112a503816da6
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 17:52:39 +0000

    Reland r81454 (sync non-frontend datatype controllers) with longer timeouts.
    Original codereview: http://codereview.chromium.org/6811003
    
    BUG=77964
    TEST=unit
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82028
    
    Review URL: http://codereview.chromium.org/6869016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_factory_impl.cc
+++ chrome/browser/sync/profile_sync_factory_impl.cc
@@ -89,7 +89,10 @@ ProfileSyncService* ProfileSyncFactoryImpl::CreateProfileSyncService(
 
   ProfileSyncService* pss = new ProfileSyncService(
       this, profile_, cros_user);
+  return pss;
+}
 
+void ProfileSyncFactoryImpl::RegisterDataTypes(ProfileSyncService* pss) {
   // App sync is enabled by default.  Register unless explicitly
   // disabled.
   if (!command_line_->HasSwitch(switches::kDisableSyncApps)) {
@@ -101,7 +104,7 @@ ProfileSyncService* ProfileSyncFactoryImpl::CreateProfileSyncService(
   // disabled.
   if (!command_line_->HasSwitch(switches::kDisableSyncAutofill)) {
     pss->RegisterDataTypeController(
-        new AutofillDataTypeController(this, profile_, pss));
+        new AutofillDataTypeController(this, profile_));
   }
 
   // Bookmark sync is enabled by default.  Register unless explicitly
@@ -122,7 +125,7 @@ ProfileSyncService* ProfileSyncFactoryImpl::CreateProfileSyncService(
   // disabled.
   if (!command_line_->HasSwitch(switches::kDisableSyncPasswords)) {
     pss->RegisterDataTypeController(
-        new PasswordDataTypeController(this, profile_, pss));
+        new PasswordDataTypeController(this, profile_));
   }
 
   // Preference sync is enabled by default.  Register unless explicitly
@@ -142,7 +145,7 @@ ProfileSyncService* ProfileSyncFactoryImpl::CreateProfileSyncService(
   // explicitly enabled.
   if (command_line_->HasSwitch(switches::kEnableSyncTypedUrls)) {
     pss->RegisterDataTypeController(
-        new TypedUrlDataTypeController(this, profile_, pss));
+        new TypedUrlDataTypeController(this, profile_));
   }
 
   // Session sync is disabled by default.  Register only if explicitly
@@ -153,10 +156,9 @@ ProfileSyncService* ProfileSyncFactoryImpl::CreateProfileSyncService(
   }
 
   if (!command_line_->HasSwitch(switches::kDisableSyncAutofillProfile)) {
-    pss->RegisterDataTypeController(new AutofillProfileDataTypeController(
-        this, profile_, pss));
+    pss->RegisterDataTypeController(
+        new AutofillProfileDataTypeController(this, profile_));
   }
-  return pss;
 }
 
 DataTypeManager* ProfileSyncFactoryImpl::CreateDataTypeManager(
