735ad4c7911bdcb6766aff3b699e4671e7feb82b
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 21:23:47 +0000

    Update the crash reporting template for chrome://crashes
    Per discussion in final launch review.
    
    BUG=None
    TEST=File a bug from chrome://crashes. It should have a better template.
    
    Review URL: http://codereview.chromium.org/6878051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/crashes_ui.cc
+++ chrome/browser/ui/webui/crashes_ui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
+#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -178,7 +179,11 @@ void CrashesDOMHandler::UpdateUI() {
 
   FundamentalValue enabled(crash_reporting_enabled);
 
-  web_ui_->CallJavascriptFunction("updateCrashList", enabled, crash_list);
+  const chrome::VersionInfo version_info;
+  StringValue version(version_info.Version());
+
+  web_ui_->CallJavascriptFunction("updateCrashList", enabled, crash_list,
+                                  version);
 }
 
 bool CrashesDOMHandler::CrashReportingEnabled() const {
