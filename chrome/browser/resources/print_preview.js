241b958b609377e17e876a4662a11774792deada
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 02:38:55 +0000

    Print Preview: Do not show an invalid summary on load and remove the "no printers" UI code.
    BUG=none
    TEST=No 'invalid page range message' on print preview startup; No more 'no printer' item in printer drop down.
    Review URL: http://codereview.chromium.org/6883032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82058 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -15,8 +15,6 @@ var pageRangesInfo = [];
 function onLoad() {
   initializeAnimation();
 
-  updateSummary();
-
   $('printer-list').disabled = true;
   $('print-button').disabled = true;
   $('print-button').addEventListener('click', printFile);
@@ -116,7 +114,7 @@ function checkAndSetPageRangesField() {
     return;
   }
 
-  for (var i = 0; i < pageRanges.length; i++) {
+  for (var i = 0; i < pageRanges.length; ++i) {
     if (pageRanges[i].from == pageRanges[i].to)
       parsedPageRanges += pageRanges[i].from;
     else
@@ -225,25 +223,20 @@ function getPreview() {
  * @param {number} defaultPrinterIndex The index of the default printer.
  */
 function setPrinters(printers, defaultPrinterIndex) {
-  if (printers.length > 0) {
-    for (var i = 0; i < printers.length; ++i) {
-      var option = document.createElement('option');
-      option.textContent = printers[i];
-      $('printer-list').add(option);
-      if (i == defaultPrinterIndex)
-        option.selected = true;
-    }
-  } else {
+  var printerList = $('printer-list');
+  for (var i = 0; i < printers.length; ++i) {
     var option = document.createElement('option');
-    option.textContent = localStrings.getString('noPrinter');
-    $('printer-list').add(option);
+    option.textContent = printers[i];
+    printerList.add(option);
+    if (i == defaultPrinterIndex)
+      option.selected = true;
   }
 
   // Adding option for saving PDF to disk.
   var option = document.createElement('option');
   option.textContent = localStrings.getString('printToPDF');
-  $('printer-list').add(option);
-  $('printer-list').disabled = false;
+  printerList.add(option);
+  printerList.disabled = false;
 
   // Once the printer list is populated, generate the initial preview.
   getPreview();
@@ -460,7 +453,7 @@ function getPageList() {
   var pageList = [];
   var parts = pageText.split(/,/);
 
-  for (var i = 0; i < parts.length; i++) {
+  for (var i = 0; i < parts.length; ++i) {
     var part = parts[i];
     var match = part.match(/([0-9]+)-([0-9]+)/);
 
@@ -469,7 +462,7 @@ function getPageList() {
       var to = parseInt(match[2], 10);
 
       if (from && to) {
-        for (var j = from; j <= to; j++)
+        for (var j = from; j <= to; ++j)
           if (j <= expectedPageCount)
             pageList.push(j);
       }
@@ -491,10 +484,10 @@ function getPageList() {
 function getPageRanges() {
   var pageList = getPageList();
   var pageRanges = [];
-  for (var i = 0; i < pageList.length; i++) {
+  for (var i = 0; i < pageList.length; ++i) {
     tempFrom = pageList[i];
     while (i + 1 < pageList.length && pageList[i + 1] == pageList[i] + 1)
-      i++;
+      ++i;
     tempTo = pageList[i];
     pageRanges.push({'from': tempFrom, 'to': tempTo});
   }
