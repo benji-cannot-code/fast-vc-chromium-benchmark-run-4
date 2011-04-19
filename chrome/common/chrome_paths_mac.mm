19e1ab637d4c51ae7e8aca078f5b6ba870f2eaf2
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 17:33:00 +0000

    Fix potential memory errors in ProductDirName.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6880035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_paths_mac.mm
+++ chrome/common/chrome_paths_mac.mm
@@ -6,11 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths_internal.h"
 
 #import <Foundation/Foundation.h>
+#include <string.h>
+
+#include <string>
 
 #include "base/base_paths.h"
 #include "base/logging.h"
 #import "base/mac/foundation_util.h"
 #import "base/mac/mac_util.h"
+#import "base/mac/scoped_nsautorelease_pool.h"
 #include "base/path_service.h"
 #include "chrome/common/chrome_constants.h"
 
@@ -39,6 +43,8 @@ NSBundle* OuterAppBundle() {
 }
 
 const char* ProductDirNameInternal() {
+  base::mac::ScopedNSAutoreleasePool pool;
+
   // Use OuterAppBundle() to get the main app's bundle. This key needs to live
   // in the main app's bundle because it will be set differently on the canary
   // channel, and the autoupdate system dictates that there can be no
@@ -61,7 +67,9 @@ const char* ProductDirNameInternal() {
 #endif
   }
 
-  return product_dir_name;
+  // Leaked, but the only caller initializes a static with this result, so it
+  // only happens once, and that's OK.
+  return strdup(product_dir_name);
 }
 
 // ProductDirName returns the name of the directory inside
