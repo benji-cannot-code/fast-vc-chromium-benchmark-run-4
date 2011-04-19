7cccbad2fda5f8e46beeb188612c7868a3e193a6
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-19 21:37:06 +0000

    Reviewed by Timothy Hatcher.
            100% reproducible crash when right-clicking any element in Web Inspector on any page
            https://bugs.webkit.org/show_bug.cgi?id=58822
    
            Web Inspector changes selection from JavaScript, and checks in UI process are insufficient
            when selection is changing asynchronously.
    
            * WebProcess/WebPage/mac/WebPageMac.mm:
            (WebKit::WebPage::writeSelectionToPasteboard): Check that there is a selection, matching WK1
            and UI process side. Perhaps the check should really be in WebCore, but for now, it's a
            WebKit responsibility.
            (WebKit::WebPage::readSelectionFromPasteboard): Same check, for no other reason but consistency.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/WebPageMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/WebPageMac.mm
@@ -600,6 +600,10 @@ void WebPage::registerUIProcessAccessibilityTokens(const CoreIPC::DataReference&
 void WebPage::writeSelectionToPasteboard(const String& pasteboardName, const Vector<String>& pasteboardTypes, bool& result)
 {
     Frame* frame = m_page->focusController()->focusedOrMainFrame();
+    if (!frame || frame->selection()->isNone()) {
+        result = false;
+        return;
+    }
     frame->editor()->writeSelectionToPasteboard(pasteboardName, pasteboardTypes);
     result = true;
 }
@@ -607,6 +611,10 @@ void WebPage::writeSelectionToPasteboard(const String& pasteboardName, const Vec
 void WebPage::readSelectionFromPasteboard(const String& pasteboardName, bool& result)
 {
     Frame* frame = m_page->focusController()->focusedOrMainFrame();
+    if (!frame || frame->selection()->isNone()) {
+        result = false;
+        return;
+    }
     frame->editor()->readSelectionFromPasteboard(pasteboardName);
     result = true;
 }
