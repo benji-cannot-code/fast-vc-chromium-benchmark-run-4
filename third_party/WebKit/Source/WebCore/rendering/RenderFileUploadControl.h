03c9d81eb2c435c522de05b9224ba840c30ef1a9
johnnyg@google.com <johnnyg@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-19 10:07:51 +0000

    2011-04-19  John Gregg  <johnnyg@google.com>
            Reviewed by Darin Fisher.
    
            Enable folder drag-n-drop when using a "webkitdirectory" file input
            https://bugs.webkit.org/show_bug.cgi?id=58401
    
            This uses a new ChromeClient API to enumerate the directory and
            return all the files, as if the user had selected that directory
            by clicking the control in the normal way.
    
            * loader/EmptyClients.h:
            (WebCore::EmptyChromeClient::enumerateChosenDirectory):
            * page/Chrome.cpp:
            (WebCore::Chrome::enumerateChosenDirectory):
            * page/Chrome.h:
            * page/ChromeClient.h:
            * platform/network/FormData.cpp:
            (WebCore::FormData::appendKeyValuePairItems):
            * rendering/RenderFileUploadControl.cpp:
            (WebCore::RenderFileUploadControl::receiveDropForDirectoryUpload):
            (WebCore::RenderFileUploadControl::receiveDroppedFiles):
            * rendering/RenderFileUploadControl.h:
    2011-04-19  John Gregg  <johnnyg@google.com>
    
            Reviewed by Darin Fisher.
    
            Enable folder drag-n-drop when using a "webkitdirectory" file input
            https://bugs.webkit.org/show_bug.cgi?id=58401
    
            This uses a new ChromeClient API to enumerate the directory and
            return all the files, as if the user had selected that directory
            by clicking the control in the normal way.
    
            * public/WebViewClient.h:
            (WebKit::WebViewClient::enumerateChosenDirectory):
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::enumerateChosenDirectory):
            * src/ChromeClientImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFileUploadControl.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderFileUploadControl.h
@@ -65,6 +65,7 @@ private:
     bool allowsMultipleFiles();
 #if ENABLE(DIRECTORY_UPLOAD)
     bool allowsDirectoryUpload();
+    void receiveDropForDirectoryUpload(const Vector<String>&);
 #endif
     String acceptTypes();
     void chooseIconForFiles(FileChooser*, const Vector<String>&);
