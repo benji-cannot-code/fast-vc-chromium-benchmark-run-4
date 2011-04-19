5b5acfa570c3513aea0380681f33a6ced907d2a1
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 20:40:12 +0000

    Increase timeout values for pyauto sync tests.
    The pyauto sync test that restarts the browser was timing out on the
    pyauto bots, due to the default timeout value of 20 seconds.
    
    This patch increases the timeout for sync tests to 1 minute (for the
    basic sign in test) and 2 minutes (for tests that go through more than
    one sync cycle).
    
    It also adds debugging information to the error message for cases where
    sync does not complete within the wait period.
    
    BUG=79894
    TEST=sync.SyncTest.testRestartBrowser
    
    Review URL: http://codereview.chromium.org/6883052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82147 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/sync.py
+++ chrome/test/functional/sync.py
@@ -16,6 +16,8 @@ class SyncTest(pyauto.PyUITest):
     creds = self.GetPrivateInfo()['test_google_account']
     username = creds['username']
     password = creds['password']
+    new_timeout = pyauto.PyUITest.ActionTimeoutChanger(self,
+                                                       60 * 1000)  # 1 min.
     self.assertTrue(self.GetSyncInfo()['summary'] == 'OFFLINE_UNUSABLE')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Never')
     self.assertTrue(self.SignInToSync(username, password))
@@ -27,6 +29,8 @@ class SyncTest(pyauto.PyUITest):
     creds = self.GetPrivateInfo()['test_google_account']
     username = creds['username']
     password = creds['password']
+    new_timeout = pyauto.PyUITest.ActionTimeoutChanger(self,
+                                                       2 * 60 * 1000)  # 2 min.
     self.assertTrue(self.SignInToSync(username, password))
     self.assertTrue(self.GetSyncInfo()['summary'] == 'READY')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Just now')
@@ -40,6 +44,8 @@ class SyncTest(pyauto.PyUITest):
     creds = self.GetPrivateInfo()['test_google_account']
     username = creds['username']
     password = creds['password']
+    new_timeout = pyauto.PyUITest.ActionTimeoutChanger(self,
+                                                       2 * 60 * 1000)  # 2 min.
     self.assertTrue(self.SignInToSync(username, password))
     self.assertTrue(self.GetSyncInfo()['summary'] == 'READY')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Just now')
