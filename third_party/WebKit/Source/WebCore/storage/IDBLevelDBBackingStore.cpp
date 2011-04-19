8329cb6a6e6e5b665f1d42364a622ced78198cb5
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-19 09:04:47 +0000

    2011-04-19  Hans Wennborg  <hans@chromium.org>
            Reviewed by Tony Gentilcore.
    
            IndexedDB: Remove accidental header include directive from LevelDB backend.
            https://bugs.webkit.org/show_bug.cgi?id=58872
    
            No new functionality, no new tests.
    
            * storage/IDBLevelDBBackingStore.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBLevelDBBackingStore.cpp
+++ third_party/WebKit/Source/WebCore/storage/IDBLevelDBBackingStore.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LevelDBIterator.h"
 #include "LevelDBSlice.h"
 #include "SecurityOrigin.h"
-#include <string>
 
 #ifndef INT64_MAX
 // FIXME: We shouldn't need to rely on these macros.
