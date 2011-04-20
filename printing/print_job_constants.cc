e79ffd86d3d27d599e523f67b5ea6ed199c20cd1
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-20 18:00:45 +0000

    PrintPreview: [MAC] Set duplex binding settings in print ticket.
    BUG=none
    TEST=Set duplex binding settings in print preview tab and observe the printed data.
    
    Review URL: http://codereview.chromium.org/6879054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82335 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/print_job_constants.cc
+++ printing/print_job_constants.cc
@@ -16,6 +16,9 @@ const char kSettingColor[] = "color";
 // Number of copies.
 const char kSettingCopies[] = "copies";
 
+// Print job duplex mode.
+const char kSettingDuplexMode[] = "duplex";
+
 // Page orientation: true for landscape, false for portrait.
 const char kSettingLandscape[] = "landscape";
 
@@ -24,8 +27,5 @@ const char kSettingPrinterName[] = "printerName";
 
 // Print to PDF option: true if selected, false if not.
 const char kSettingPrintToPDF[] = "printToPDF";
-
-// Print job duplex setting.
-const char kSettingTwoSided[] = "twoSided";
 
 }  // namespace printing
--- printing/printing_context_mac.h
+++ printing/printing_context_mac.h
@@ -66,7 +66,7 @@ class PrintingContextMac : public PrintingContext {
 
   // Sets duplex mode in PMPrintSettings.
   // Returns true if duplex mode is set.
-  bool SetDuplexModeIsTwoSided(bool two_sided);
+  bool SetDuplexModeInPrintSettings(DuplexMode mode);
 
   // Sets output color mode in PMPrintSettings.
   // Returns true if color mode is set.
