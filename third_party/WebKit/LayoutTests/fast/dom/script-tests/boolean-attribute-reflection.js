03f33a883cc978a6ddd64a028de91052a91bd72b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-20 02:13:22 +0000

    2011-04-19  Julien Chaffraix  <jchaffraix@codeaurora.org>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=45425
            HTMLLinkElement.disabled does not forward value to the Stylesheet's disabled attribute on setting
    
            Test case by Bijan Amirzada <bijana@codeaurora.org>.
    
            * fast/dom/HTMLLinkElement/disabled-attribute-expected.txt: Added.
            * fast/dom/HTMLLinkElement/disabled-attribute.html: Added.
            Test that the properties are properly forwarded to the stylesheet.
    
            * fast/dom/boolean-attribute-reflection-expected.txt:
            * fast/dom/script-tests/boolean-attribute-reflection.js: Removed link:disabled as it is
            not reflected anymore.
    2011-04-19  Julien Chaffraix  <jchaffraix@codeaurora.org>
    
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=45425
            HTMLLinkElement.disabled does not forward value to the Stylesheet's disabled attribute on setting
    
            Test: fast/html/htmllink-disable.html
            Test: fast/dom/HTMLLinkElement/disabled-attribute.html
    
            Match the HTML5 specification by forwarding the 'disabled' attribute to our stylesheet.
    
            Based on previous work by Bijan Amirzada <bijana@codeaurora.org>.
    
            * html/HTMLLinkElement.cpp:
            (WebCore::HTMLLinkElement::disabled): Return our stylesheet's 'disabled' value or
            false if we don't have a stylesheet.
            (WebCore::HTMLLinkElement::setDisabled): Set our stylesheet's 'disabled' value or
            ignore the call if we don't have a stylesheet.
            * html/HTMLLinkElement.h:
            * html/HTMLLinkElement.idl: Remove 'Reflect' as it is not a reflected attribute anymore in HTML5.
            This matches the way FF4, Opera and IE9 handle the attribute.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/boolean-attribute-reflection.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/boolean-attribute-reflection.js
@@ -16,7 +16,6 @@ var attributes = [
     [ "input", "multiple" ],
     [ "input", "readOnly" ],
     [ "input", "required" ],
-    [ "link", "disabled" ],
     [ "menu", "compact" ],
     [ "object", "declare" ],
     [ "ol", "compact" ],
--- third_party/WebKit/Source/WebCore/html/HTMLLinkElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLLinkElement.cpp
@@ -513,4 +513,16 @@ void HTMLLinkElement::removePendingSheet()
     document()->removePendingSheet();
 }
 
+bool HTMLLinkElement::disabled() const
+{
+    return m_sheet && m_sheet->disabled();
+}
+
+void HTMLLinkElement::setDisabled(bool disabled)
+{
+    if (!m_sheet)
+        return;
+    m_sheet->setDisabled(disabled);
+}
+
 }
