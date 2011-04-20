3c9d0d85d477a285a95696c3d730901eb1faffb1
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-20 23:09:01 +0000

    [Mac] Reset isHandlingSendEvent flag in case of exceptional condition.
    NSExceptions can cause the flag to get latched to YES, which messes
    with window closing.  A number of suspected exceptions shouldn't cause
    too much other damage, so this patches over the problem for the short
    term.
    
    TBR=jam@chromium.org
    BUG=45928
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6873081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82397 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/chrome_application_mac.mm
+++ content/common/chrome_application_mac.mm
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   handlingSendEvent_ = handlingSendEvent;
 }
 
+- (void)clearIsHandlingSendEvent {
+  [self setHandlingSendEvent:NO];
+}
+
 - (void)sendEvent:(NSEvent*)event {
   chrome_application_mac::ScopedSendingEvent sendingEventScoper;
   for (id<CrApplicationEventHookProtocol> handler in eventHooks_.get()) {
