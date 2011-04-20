7da36f4122311a8f54cf38a1b71894a574efd4f1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-20 05:16:35 +0000

    2011-04-19  Naoki Takano  <takano.naoki@gmail.com>
            Reviewed by Kent Tamura.
    
            [Chromium]UI polishes and tweaks to Autofill dropdown menu.
            https://bugs.webkit.org/show_bug.cgi?id=58505
            http://code.google.com/p/chromium/issues/detail?id=51077
    
            No new tests. Because this is autofill looking problem in Chromium.
            Add m_menuType as PopupMenuStyle to change popup style change.
            Put kLinePaddingHeight at the top and bottom of each line if m_menuType is AutofillPopup.
            Change separator color to #dcdcdc and remove sparatorPadding at the edge if m_menuType is AutofillPopup.
            Change the line height of separator as only the piece if m_menuType is AutofillPopup.
            Change the label font size 0.9 time smaller than regular font size if m_menuType is AutofillPopup.
    
            * platform/PopupMenuStyle.h: Add enum PopupMenuType;
            (WebCore::PopupMenuStyle::PopupMenuStyle): Add m_menuType.
            (WebCore::PopupMenuStyle::menuType): Add to change the style according to the return value.
            * platform/chromium/PopupMenuChromium.cpp:
            (WebCore::PopupListBox::paintRow):Change the separator color to #dcdcdc.
            Change the edge padding according to menuStyle().
            (WebCore::PopupListBox::getRowHeight): Add kLineHeightMargin*2 for each line height.
    2011-04-19  Naoki Takano  <takano.naoki@gmail.com>
    
            Reviewed by Kent Tamura.
    
            [Chromium]UI polishes and tweaks to Autofill dropdown menu.
            https://bugs.webkit.org/show_bug.cgi?id=58505
    
            * src/AutoFillPopupMenuClient.cpp:
            (WebKit::AutoFillPopupMenuClient::initialize): Set AutofillPopup for menuStyle.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PopupMenuStyle.h
+++ third_party/WebKit/Source/WebCore/platform/PopupMenuStyle.h
@@ -36,7 +36,8 @@ namespace WebCore {
 
 class PopupMenuStyle {
 public:
-    PopupMenuStyle(const Color& foreground, const Color& background, const Font& font, bool visible, bool isDisplayNone, Length textIndent, TextDirection textDirection, bool hasTextDirectionOverride)
+    enum PopupMenuType { SelectPopup, AutofillPopup };
+    PopupMenuStyle(const Color& foreground, const Color& background, const Font& font, bool visible, bool isDisplayNone, Length textIndent, TextDirection textDirection, bool hasTextDirectionOverride, PopupMenuType menuType = SelectPopup)
         : m_foregroundColor(foreground)
         , m_backgroundColor(background)
         , m_font(font)
@@ -45,9 +46,10 @@ public:
         , m_textIndent(textIndent)
         , m_textDirection(textDirection)
         , m_hasTextDirectionOverride(hasTextDirectionOverride)
+        , m_menuType(menuType)
     {
     }
-    
+
     const Color& foregroundColor() const { return m_foregroundColor; }
     const Color& backgroundColor() const { return m_backgroundColor; }
     const Font& font() const { return m_font; }
@@ -56,7 +58,7 @@ public:
     Length textIndent() const { return m_textIndent; }
     TextDirection textDirection() const { return m_textDirection; }
     bool hasTextDirectionOverride() const { return m_hasTextDirectionOverride; }
-
+    PopupMenuType menuType() const { return m_menuType; }
 private:
     Color m_foregroundColor;
     Color m_backgroundColor;
@@ -66,6 +68,7 @@ private:
     Length m_textIndent;
     TextDirection m_textDirection;
     bool m_hasTextDirectionOverride;
+    PopupMenuType m_menuType;
 };
 
 } // namespace WebCore
