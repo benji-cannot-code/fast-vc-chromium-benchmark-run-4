eaa0277d1c74401b0738cba817eb128fbb29ce4e
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-20 17:57:58 +0000

    [wx] Unreviewed build fixes for wx 2.9.1, Cocoa and 64-bit builds.
    git-svn-id: svn://svn.chromium.org/blink/trunk@84396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -502,7 +502,7 @@ sub determinePassedArchitecture
         my $opt = $ARGV[$i];
         if ($opt =~ /^--32-bit$/i) {
             splice(@ARGV, $i, 1);
-            if (isAppleMacWebKit()) {
+            if (isAppleMacWebKit() || isWx()) {
                 $passedArchitecture = `arch`;
                 chomp $passedArchitecture;
             }
