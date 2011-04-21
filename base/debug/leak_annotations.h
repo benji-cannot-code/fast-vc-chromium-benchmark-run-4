1dd6827fea912f70376696c6d2402d109aee0d5b
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-21 07:34:50 +0000

    Introduce the ANNOTATE_LEAKING_OBJECT_PTR annotation that can be used to mark heap allocated objects as intentionally leaked ones. Annotate the histograms produced by {Histogram,BooleanHistogram,LinearHistogram,CustomHistogram}::FactoryGet(), as leaked.
    Rename StatsHistogram::StatsHistogramFactoryGet to StatsHistogram::FactoryGet, annotate the result as leaky, update the suppressions.
    
    BUG=79322
    Review URL: http://codereview.chromium.org/6869009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82460 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/leak_annotations.h
+++ base/debug/leak_annotations.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,10 +19,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ANNOTATE_SCOPED_MEMORY_LEAK \
     HeapLeakChecker::Disabler heap_leak_checker_disabler
 
+// Annotate an object pointer as referencing a leaky object. This object and all
+// the heap objects referenced by it will be ignored by the heap checker.
+//
+// X should be referencing an active allocated object. If it is not, the
+// annotation will be ignored.
+// No object should be annotated with ANNOTATE_SCOPED_MEMORY_LEAK twice.
+// Once an object is annotated with ANNOTATE_SCOPED_MEMORY_LEAK, it cannot be
+// deleted.
+#define ANNOTATE_LEAKING_OBJECT_PTR(X) \
+    HeapLeakChecker::IgnoreObject(X)
+
 #else
 
 // If tcmalloc is not used, the annotations should be no-ops.
 #define ANNOTATE_SCOPED_MEMORY_LEAK
+#define ANNOTATE_LEAKING_OBJECT_PTR(X)
 
 #endif
 
--- net/disk_cache/stats_histogram.cc
+++ net/disk_cache/stats_histogram.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/disk_cache/stats_histogram.h"
 
+#include "base/debug/leak_annotations.h"
 #include "base/logging.h"
 #include "net/disk_cache/stats.h"
 
@@ -22,8 +23,7 @@ StatsHistogram::~StatsHistogram() {
     stats_ = NULL;
 }
 
-StatsHistogram* StatsHistogram::StatsHistogramFactoryGet(
-    const std::string& name) {
+StatsHistogram* StatsHistogram::FactoryGet(const std::string& name) {
   Histogram* histogram(NULL);
 
   Sample minimum = 1;
@@ -50,6 +50,7 @@ StatsHistogram* StatsHistogram::StatsHistogramFactoryGet(
   // Validate upcast by seeing that we're probably providing the checksum.
   CHECK_EQ(return_histogram->StatsHistogram::CalculateRangeChecksum(),
            return_histogram->CalculateRangeChecksum());
+  ANNOTATE_LEAKING_OBJECT_PTR(return_histogram); // see crbug.com/79322
   return return_histogram;
 }
 
