71808caf691f696b4ca6a26093a3cd2ac6e361d1
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 23:58:40 +0000

    fix
    git-svn-id: svn://svn.chromium.org/blink/trunk@84568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/harness/resources/results-test.js
+++ third_party/WebKit/LayoutTests/fast/harness/resources/results-test.js
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // To run these tests, load json_results.html in a browser.
 // You should see a series of PASS lines.
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
 
 var testStyles = document.createElement('style');
 testStyles.innerText = ".test-pass { color: green; } .test-fail { color: red; }";
