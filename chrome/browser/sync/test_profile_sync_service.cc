b177edf2dcf43cab28dda595d56ae89b9f6afada
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-21 05:34:20 +0000

    Make the SBH wait until syncer is put in config mode before it triggers DTM to do model association.
    BUG=80124
    TEST=
    
    Review URL: http://codereview.chromium.org/6879099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82447 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test_profile_sync_service.cc
+++ chrome/browser/sync/test_profile_sync_service.cc
@@ -128,6 +128,10 @@ void SyncBackendHostForProfileSyncTest::ProcessMessage(
   }
 }
 
+void SyncBackendHostForProfileSyncTest::StartConfiguration(Callback0::Type*) {
+  SyncBackendHost::FinishConfigureDataTypesOnFrontendLoop();
+}
+
 void SyncBackendHostForProfileSyncTest::
     SetDefaultExpectationsForWorkerCreation(ProfileMock* profile) {
   EXPECT_CALL(*profile, GetPasswordStore(testing::_)).
--- chrome/browser/sync/test_profile_sync_service.h
+++ chrome/browser/sync/test_profile_sync_service.h
@@ -69,6 +69,8 @@ class SyncBackendHostForProfileSyncTest
   virtual void ProcessMessage(const std::string& name, const JsArgList& args,
                               const JsEventHandler* sender);
 
+  virtual void StartConfiguration(Callback0::Type* callback);
+
   static void SetDefaultExpectationsForWorkerCreation(ProfileMock* profile);
 
   static void SetHistoryServiceExpectations(ProfileMock* profile);
