f5ddb784628c06ef5a8ce3580f70cf4d604fc7af
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 23:24:50 +0000

    2011-04-21  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r82052.
            http://trac.webkit.org/changeset/82052
            https://bugs.webkit.org/show_bug.cgi?id=59135
    
            New full screen api should be opt-in, not opt-out. (Requested
            by jernoble on #webkit).
    
            * WebView/WebPreferences.mm:
            (+[WebPreferences initialize]):
    2011-04-21  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r82052.
            http://trac.webkit.org/changeset/82052
            https://bugs.webkit.org/show_bug.cgi?id=59135
    
            New full screen api should be opt-in, not opt-out. (Requested
            by jernoble on #webkit).
    
            * Shared/WebPreferencesStore.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
@@ -387,7 +387,7 @@ static bool useQuickLookQuirks(void)
         [NSNumber numberWithBool:NO],   WebKitFrameFlatteningEnabledPreferenceKey,
         [NSNumber numberWithBool:NO],   WebKitSpatialNavigationEnabledPreferenceKey,
         [NSNumber numberWithBool:NO],  WebKitDNSPrefetchingEnabledPreferenceKey,
-        [NSNumber numberWithBool:YES],  WebKitFullScreenEnabledPreferenceKey,
+        [NSNumber numberWithBool:NO],   WebKitFullScreenEnabledPreferenceKey,
         [NSNumber numberWithBool:NO],   WebKitAsynchronousSpellCheckingEnabledPreferenceKey,
         [NSNumber numberWithBool:NO],   WebKitMemoryInfoEnabledPreferenceKey,
         [NSNumber numberWithBool:YES],  WebKitHyperlinkAuditingEnabledPreferenceKey,
