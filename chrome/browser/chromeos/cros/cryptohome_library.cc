2f9bab499dc2058ab0f372f2e2fe879d5f1c69b7
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-21 15:37:34 +0000

    AsyncSetOwnerUser() added to pass owner user to Cryptohomed
    BUG=chromium-os:4065
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6880033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cryptohome_library.cc
+++ chrome/browser/chromeos/cros/cryptohome_library.cc
@@ -131,6 +131,13 @@ class CryptohomeLibraryImpl : public CryptohomeLibrary {
         "Couldn't do automatic free disk space control.");
   }
 
+  bool AsyncSetOwnerUser(const std::string& username, Delegate* d) {
+    return CacheCallback(
+        chromeos::CryptohomeAsyncSetOwnerUser(username.c_str()),
+        d,
+        "Couldn't do set owner user in Cryptohomed.");
+  }
+
   bool TpmIsReady() {
     return chromeos::CryptohomeTpmIsReady();
   }
@@ -349,6 +356,13 @@ class CryptohomeLibraryStubImpl : public CryptohomeLibrary {
   }
 
   bool AsyncDoAutomaticFreeDiskSpaceControl(Delegate* callback) {
+    BrowserThread::PostTask(
+        BrowserThread::UI, FROM_HERE,
+        NewRunnableFunction(&DoStubCallback, callback));
+    return true;
+  }
+
+  bool AsyncSetOwnerUser(const std::string& username, Delegate* callback) {
     BrowserThread::PostTask(
         BrowserThread::UI, FROM_HERE,
         NewRunnableFunction(&DoStubCallback, callback));
