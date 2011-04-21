d0dd5aa33a1fe3ce401ab167fe1eb166ed6ef0ae
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 15:38:37 +0000

    2011-04-21  Adam Barth  <abarth@webkit.org>
            Reviewed by Tony Gentilcore.
    
            Make test robust to PHP magic quotes
            https://bugs.webkit.org/show_bug.cgi?id=59104
    
            * http/tests/security/contentSecurityPolicy/report-uri-expected.txt:
            * http/tests/security/contentSecurityPolicy/report-uri-from-child-frame-expected.txt:
            * http/tests/security/contentSecurityPolicy/resources/save-report.php:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84502 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
@@ -1,14 +1,24 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+function undoMagicQuotes($value) {
+    if (get_magic_quotes_gpc())
+        return stripslashes($value);
+    return $value;
+}
+
 $reportFile = fopen("csp-report.txt.tmp", 'w');
 $httpHeaders = $_SERVER;
 ksort($httpHeaders, SORT_STRING);
 foreach ($httpHeaders as $name => $value) {
-    if ($name === "CONTENT_TYPE" || $name === "HTTP_REFERER" || $name === "REQUEST_METHOD")
+    if ($name === "CONTENT_TYPE" || $name === "HTTP_REFERER" || $name === "REQUEST_METHOD") {
+        $value = undoMagicQuotes($value);
         fwrite($reportFile, "$name: $value\n");
+    }
 }
 fwrite($reportFile, "=== POST DATA ===\n");
 foreach ($_POST as $name => $value) {
+    $name = undoMagicQuotes($name);
+    $value = undoMagicQuotes($value);
     fwrite($reportFile, "$name: $value\n");
 }
 fclose($reportFile);
