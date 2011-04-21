a1b626d7ede4975cea51ceaeba0216542a492578
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 19:35:55 +0000

    2011-04-21  Martin Robinson  <mrobinson@igalia.com>
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            Rolling out r84507 since it broke many GTK+ tests.
    
            * platform/gtk/Skipped:
            * platform/gtk/fast/repaint/scale-page-shrink-expected.checksum: Removed.
            * platform/gtk/fast/repaint/scale-page-shrink-expected.png: Removed.
            * platform/gtk/fast/repaint/scale-page-shrink-expected.txt: Removed.
    2011-04-21  Martin Robinson  <mrobinson@igalia.com>
    
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            Rolling out r84507 since it broke many GTK+ tests.
    
            * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
            * WebCoreSupport/DumpRenderTreeSupportGtk.h:
    2011-04-21  Martin Robinson  <mrobinson@igalia.com>
    
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            Rolling out r84507 since it broke many GTK+ tests.
    
            * DumpRenderTree/gtk/EventSender.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
@@ -104,7 +104,6 @@ public:
     static double defaultMinimumTimerInterval(); // Not really tied to WebView
     static void setMinimumTimerInterval(WebKitWebView*, double);
     static void rectangleForSelection(WebKitWebFrame*, GdkRectangle*);
-    static void scalePageBy(WebKitWebView*, float, float, float);
 
     // Accessibility
     static void incrementAccessibilityValue(AtkObject*);
