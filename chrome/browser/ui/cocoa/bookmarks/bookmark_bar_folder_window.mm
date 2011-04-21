a9731b1811aed916b59027f2acc58cbdf237fcb7
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-21 01:39:01 +0000

    Make bookmark menu windows not beep whenever they get a keystroke. R=mrossetti@chromium.org BUG=80065 Review URL: http://codereview.chromium.org/6879089
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82418 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_window.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_window.mm
@@ -39,6 +39,10 @@ using bookmarks::kBookmarkBarMenuCornerRadius;
   return NO;
 }
 
+// Override of keyDown as the NSWindow default implementation beeps.
+- (void)keyDown:(NSEvent *)theEvent {
+}
+
 @end
 
 
