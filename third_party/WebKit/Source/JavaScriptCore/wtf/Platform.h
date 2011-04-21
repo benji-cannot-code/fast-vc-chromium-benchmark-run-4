b2601725f1a4b6112159f3121a8149394ba6ea3c
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 01:04:34 +0000

    Build fix - revert accidental change.
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -1025,7 +1025,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(JIT) && USE(JSVALUE64) && PLATFORM(MAC)
 #define ENABLE_DFG_JIT 1
 /* Enabled with restrictions to circumvent known performance regressions. */
-#define ENABLE_DFG_JIT_RESTRICTIONS 0
+#define ENABLE_DFG_JIT_RESTRICTIONS 1
 #endif
 
 /* Ensure that either the JIT or the interpreter has been enabled. */
