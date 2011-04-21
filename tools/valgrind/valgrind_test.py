78531b4143947a067d83e1ddbae95c848f377b64
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-21 11:08:24 +0000

    Enable --smc-check=all for Memcheck on Mac OS to see if it helps to get rid of issue 51716
    TBR=timurrrr
    BUG=51716
    Review URL: http://codereview.chromium.org/6880106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82470 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/valgrind_test.py
+++ tools/valgrind/valgrind_test.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
-# Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -475,6 +475,11 @@ class Memcheck(ValgrindTool):
 
     if self._options.track_origins:
       ret += ["--track-origins=yes"]
+
+    # TODO(glider): this is a temporary workaround for http://crbug.com/51716
+    # Let's see whether it helps.
+    if common.IsMac():
+      ret += ["--smc-check=all"]
 
     return ret
 
