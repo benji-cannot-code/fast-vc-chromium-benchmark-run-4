c083d18fd981ac8ee776a0631663abc6770e3ab2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 01:38:12 +0000

    2011-04-20  Alexey Marinichev  <amarinichev@chromium.org>
            Reviewed by Kenneth Russell.
    
            Check for null m_context in WebGLLayerChromium and add resetting platform layer's context in the destructor of GraphicsContext3D.
            https://bugs.webkit.org/show_bug.cgi?id=59045
    
            Lose context extension test might not catch this bug, more reliable
            way is forcing the crash of the GPU process, which is done by Chromium
            browser tests.
    
            * platform/graphics/chromium/WebGLLayerChromium.cpp:
            (WebCore::WebGLLayerChromium::updateCompositorResources):
            (WebCore::WebGLLayerChromium::setContext):
            * platform/graphics/chromium/WebGLLayerChromium.h:
    2011-04-20  Alexey Marinichev  <amarinichev@chromium.org>
    
            Reviewed by Kenneth Russell.
    
            Check for null m_context in WebGLLayerChromium and add resetting platform layer's context in the destructor of GraphicsContext3D.
            https://bugs.webkit.org/show_bug.cgi?id=59045
    
            * src/GraphicsContext3DChromium.cpp:
            (WebCore::GraphicsContext3D::~GraphicsContext3D): call into platform
            layer and unregister itself.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/WebGLLayerChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/WebGLLayerChromium.cpp
@@ -60,11 +60,13 @@ WebGLLayerChromium::~WebGLLayerChromium()
 
 void WebGLLayerChromium::updateCompositorResources()
 {
+    if (!m_context)
+        return;
+
     if (!m_contentsDirty)
         return;
 
     GraphicsContext3D* rendererContext = layerRendererContext();
-    ASSERT(m_context);
     if (m_textureChanged) {
         rendererContext->bindTexture(GraphicsContext3D::TEXTURE_2D, m_textureId);
         // Set the min-mag filters to linear and wrap modes to GL_CLAMP_TO_EDGE
@@ -102,6 +104,9 @@ void WebGLLayerChromium::setContext(const GraphicsContext3D* context)
     }
 
     m_context = const_cast<GraphicsContext3D*>(context);
+
+    if (!m_context)
+        return;
 
     unsigned int textureId = m_context->platformTexture();
     if (textureId != m_textureId) {
