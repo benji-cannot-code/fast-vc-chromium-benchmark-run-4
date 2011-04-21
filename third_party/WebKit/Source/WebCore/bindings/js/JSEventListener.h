4478d3104db55e581f2362812d972858a6941c2e
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-21 23:54:26 +0000

    Whoops, change lost during merge
    git-svn-id: svn://svn.chromium.org/blink/trunk@84566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSEventListener.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSEventListener.h
@@ -58,7 +58,7 @@ namespace WebCore {
 
     private:
         virtual JSC::JSObject* initializeJSFunction(ScriptExecutionContext*) const;
-        virtual void markJSFunction(JSC::SlotVisitor&);
+        virtual void visitJSFunction(JSC::SlotVisitor&);
         virtual bool virtualisAttribute() const;
 
     protected:
