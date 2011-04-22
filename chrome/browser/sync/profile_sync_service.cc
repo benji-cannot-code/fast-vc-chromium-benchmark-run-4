a669e9f760b10b3cd0fb3f24369493f0aceae3ab
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 22:50:15 +0000

    More official builder fix.
    BUG=none
    TEST=none
    TBR=csilv
    
    Review URL: http://codereview.chromium.org/6895035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82744 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string16.h"
 #include "base/stringprintf.h"
 #include "base/task.h"
+#include "base/threading/thread_restrictions.h"
 #include "chrome/browser/net/gaia/token_service.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/prefs/pref_service.h"
