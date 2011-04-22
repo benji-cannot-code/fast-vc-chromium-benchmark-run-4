11da92498d54705546c737d92ddf97c4d0abffca
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 18:26:34 +0000

    2011-04-22  Oliver Hunt  <oliver@apple.com>
            Reviewed by Geoffrey Garen.
    
            Make it harder to use HandleSlot incorrectly
            https://bugs.webkit.org/show_bug.cgi?id=59205
    
            Just add a little type fudging to make it harder to
            incorrectly assign through a HandleSlot.
    
            * API/JSCallbackObjectFunctions.h:
            (JSC::::init):
            * JavaScriptCore.exp:
            * heap/Handle.h:
            (JSC::HandleBase::operator!):
            (JSC::HandleBase::operator UnspecifiedBoolType*):
            (JSC::HandleTypes::getFromSlot):
            * heap/HandleHeap.cpp:
            (JSC::HandleHeap::markStrongHandles):
            (JSC::HandleHeap::markWeakHandles):
            (JSC::HandleHeap::finalizeWeakHandles):
            (JSC::HandleHeap::writeBarrier):
            (JSC::HandleHeap::protectedGlobalObjectCount):
            (JSC::HandleHeap::isValidWeakNode):
            * heap/HandleHeap.h:
            (JSC::HandleHeap::copyWeak):
            (JSC::HandleHeap::makeWeak):
            (JSC::HandleHeap::Node::slot):
            * heap/HandleStack.cpp:
            (JSC::HandleStack::mark):
            (JSC::HandleStack::grow):
            * heap/HandleStack.h:
            (JSC::HandleStack::zapTo):
            (JSC::HandleStack::push):
            * heap/Heap.cpp:
            (JSC::HandleHeap::protectedObjectTypeCounts):
            * heap/Local.h:
            (JSC::::set):
            * heap/Strong.h:
            (JSC::Strong::set):
            * heap/Weak.h:
            (JSC::Weak::set):
            * runtime/StructureTransitionTable.h:
            (JSC::StructureTransitionTable::singleTransition):
            (JSC::StructureTransitionTable::setSingleTransition):
            * runtime/WeakGCMap.h:
            (JSC::WeakGCMap::add):
            (JSC::WeakGCMap::set):
            * runtime/WriteBarrier.h:
            (JSC::OpaqueJSValue::toJSValue):
            (JSC::OpaqueJSValue::toJSValueRef):
            (JSC::OpaqueJSValue::fromJSValue):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84650 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/API/JSCallbackObjectFunctions.h
+++ third_party/WebKit/Source/JavaScriptCore/API/JSCallbackObjectFunctions.h
@@ -97,7 +97,7 @@ void JSCallbackObject<Base>::init(ExecState* exec)
         HandleSlot slot = exec->globalData().allocateGlobalHandle();
         HandleHeap::heapFor(slot)->makeWeak(slot, m_callbackObjectData.get(), classRef());
         HandleHeap::heapFor(slot)->writeBarrier(slot, this);
-        *slot = this;
+        slot->fromJSValue(this);
     }
 }
 
--- third_party/WebKit/Source/JavaScriptCore/runtime/WeakGCMap.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/WeakGCMap.h
@@ -130,7 +130,7 @@ public:
             iter.first->second = slot;
             HandleHeap::heapFor(slot)->makeWeak(slot, this, FinalizerCallback::finalizerContextFor(key));
             HandleHeap::heapFor(slot)->writeBarrier(slot, value);
-            *slot = value;
+            slot->fromJSValue(value);
         }
         return iter;
     }
@@ -140,7 +140,7 @@ public:
         HandleSlot slot = iter.m_iterator->second;
         ASSERT(slot);
         HandleHeap::heapFor(slot)->writeBarrier(slot, value);
-        *slot = value;
+        slot->fromJSValue(value);
     }
 
     void set(JSGlobalData& globalData, const KeyType& key, ExternalType value)
@@ -153,7 +153,7 @@ public:
             iter.first->second = slot;
         }
         HandleHeap::heapFor(slot)->writeBarrier(slot, value);
-        *slot = value;
+        slot->fromJSValue(value);
     }
 
     ExternalType take(const KeyType& key)
