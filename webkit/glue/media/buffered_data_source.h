bb355be3a23cdb2c0ad28e64a08e9deb4080a32a
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 19:07:06 +0000

    Remove the watchdog timer and some member variables from BufferedDataSource.
    The root cause is that the WatchDogTask will always think the read has timed out since read_submitted_time_ only gets set during ReadTask.
    
    What this means is that while we may start the WatchDogTask inside InitializeTask... read_submitted_time_ remains 0 which means the initial loader can get killed if it takes longer than 2500ms to respond and for the initial ReadTask to get executed. In other words, a pretty likely scenario on laggier connections.
    
    Sequence of events:
     1) Initial loader created and watch dog timer started in BDS::InitializeTask
     2) The initial loader receives a response and BDS and rest of pipeline continues to initialize
     3) BDS::WatchDogTask gets posted to render thread
     4) FFmpegDemuxer issues a read, setting read_callback_ and posting BDS::ReadTask to render thread
     5) BDS::WatchDogTask is executed, but since read_callback_ isn't NULL and read_submitted_time_ is zero the loader is recreated
     6) BDS::ReadTask is executed and calls BRL::Read ***on the new loader***
     7) The new loader receives a response, which calls BDS::PartialReadStartCallback and calls BRL::Read
     8) DCHECK(!read_callback_.get()) is hit inside BRL::Read
    
    BUG=74473
    TEST=layout tests under valgrind
    
    Review URL: http://codereview.chromium.org/6899004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82683 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/media/buffered_data_source.h
+++ webkit/glue/media/buffered_data_source.h
@@ -62,11 +62,6 @@ class BufferedDataSource : public WebDataSource {
   virtual BufferedResourceLoader* CreateResourceLoader(
       int64 first_byte_position, int64 last_byte_position);
 
-  // Gets the number of milliseconds to declare a request timeout since
-  // the request was made. This method is made virtual so as to inject a
-  // different number for testing purpose.
-  virtual base::TimeDelta GetTimeoutMilliseconds();
-
  private:
   // Posted to perform initialization on render thread and start resource
   // loading.
@@ -82,11 +77,6 @@ class BufferedDataSource : public WebDataSource {
 
   // Restart resource loading on render thread.
   void RestartLoadingTask();
-
-  // This task monitors the current active read request. If the current read
-  // request has timed out, this task will destroy the current loader and
-  // creates a new one to accommodate the read request.
-  void WatchDogTask();
 
   // This task uses the current playback rate with the previous playback rate
   // to determine whether we are going from pause to play and play to pause,
@@ -172,8 +162,6 @@ class BufferedDataSource : public WebDataSource {
   int64 read_position_;
   int read_size_;
   uint8* read_buffer_;
-  base::Time read_submitted_time_;
-  int read_attempts_;
 
   // This buffer is intermediate, we use it for BufferedResourceLoader to write
   // to. And when read in BufferedResourceLoader is done, we copy data from
@@ -213,12 +201,6 @@ class BufferedDataSource : public WebDataSource {
   // This variable holds the value of the preload attribute for the video
   // element.
   media::Preload preload_;
-
-  // This timer is to run the WatchDogTask repeatedly. We use a timer instead
-  // of doing PostDelayedTask() reduce the extra reference held by the message
-  // loop. The RepeatingTimer does PostDelayedTask() internally, by using it
-  // the message loop doesn't hold a reference for the watch dog task.
-  base::RepeatingTimer<BufferedDataSource> watch_dog_timer_;
 
   // Keeps track of whether we used a Range header in the initialization
   // request.
