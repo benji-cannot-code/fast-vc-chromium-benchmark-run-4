1ce697d78e71f6d7771d4db3e8a40d5ee85925c2
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 18:53:15 +0000

    2011-04-22  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Darin Fisher.
    
            REGRESSION: left property broken with position:fixed elements in RTL documents
            https://bugs.webkit.org/show_bug.cgi?id=59204
            http://code.google.com/p/chromium/issues/detail?id=80216
    
            Test: fast/block/positioning/rtl-fixed-positioning.html
                  fast/block/positioning/vertical-rl/fixed-positioning.html
    
            Returning the proper scroll offsets for fixed position content in RTL documents.
            We regressed to X and Y offsets not updating while scrolling when we clamped to zero,
            but RTL documents scroll in negative space.
    
            * page/FrameView.cpp:
            (WebCore::FrameView::scrollXForFixedPosition): Properly handling RTL documents where
            the scroll origin and offsets are negative.
            (WebCore::FrameView::scrollYForFixedPosition): Properly handling vertical writing-
            mode RTL documents, like above.
            * platform/ScrollView.h:
            (WebCore::ScrollView::scrollOrigin): Changing this accessor to const.
    2011-04-22  Levi Weintraub  <leviw@chromium.org>
    
            Reviewed by Darin Fisher.
    
            REGRESSION: left property broken with position:fixed elements in RTL documents
            https://bugs.webkit.org/show_bug.cgi?id=59204
            http://code.google.com/p/chromium/issues/detail?id=80216
    
            Test to ensure position:fixed works properly in RTL documents.
    
            * fast/block/positioning/rtl-fixed-positioning.html: Added.
            * fast/block/positioning/vertical-rl/fixed-positioning.html: Added.
            * platform/mac/fast/block/positioning/rtl-fixed-positioning-expected.checksum: Added.
            * platform/mac/fast/block/positioning/rtl-fixed-positioning-expected.png: Added.
            * platform/mac/fast/block/positioning/rtl-fixed-positioning-expected.txt: Added.
            * platform/mac/fast/block/positioning/vertical-rl/fixed-positioning-expected.checksum: Added.
            * platform/mac/fast/block/positioning/vertical-rl/fixed-positioning-expected.png: Added.
            * platform/mac/fast/block/positioning/vertical-rl/fixed-positioning-expected.txt: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameView.cpp
@@ -1148,10 +1148,17 @@ int FrameView::scrollXForFixedPosition() const
 
     int x = scrollX();
 
-    if (x < 0)
-        x = 0;
-    else if (x > maxX)
-        x = maxX;
+    if (!ScrollView::scrollOrigin().x()) {
+        if (x < 0)
+            x = 0;
+        else if (x > maxX)
+            x = maxX;
+    } else {
+        if (x > 0)
+            x = 0;
+        else if (x < -maxX)
+            x = -maxX;
+    }
 
     if (!m_frame)
         return x;
@@ -1176,10 +1183,17 @@ int FrameView::scrollYForFixedPosition() const
 
     int y = scrollY();
 
-    if (y < 0)
-        y = 0;
-    else if (y > maxY)
-        y = maxY;
+    if (!ScrollView::scrollOrigin().y()) {
+        if (y < 0)
+            y = 0;
+        else if (y > maxY)
+            y = maxY;
+    } else {
+        if (y > 0)
+            y = 0;
+        else if (y < -maxY)
+            y = -maxY;
+    }
 
     if (!m_frame)
         return y;
