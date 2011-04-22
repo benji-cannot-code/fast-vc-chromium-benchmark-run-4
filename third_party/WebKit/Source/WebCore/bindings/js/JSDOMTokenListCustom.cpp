2e23e81f59221c8deb65a5bda148159dc24202ea
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 23:04:12 +0000

    2011-04-22  Geoffrey Garen  <ggaren@apple.com>
            Reviewed by Sam Weinig.
    
            We have an autogenerator, so use it
            https://bugs.webkit.org/show_bug.cgi?id=59247
    
            * bindings/js/JSCSSRuleCustom.cpp:
            * bindings/js/JSCSSRuleListCustom.cpp:
            * bindings/js/JSCSSStyleDeclarationCustom.cpp:
            * bindings/js/JSCSSValueCustom.cpp:
            * bindings/js/JSCanvasRenderingContextCustom.cpp:
            * bindings/js/JSDOMImplementationCustom.cpp:
            * bindings/js/JSDOMStringMapCustom.cpp:
            * bindings/js/JSHTMLCollectionCustom.cpp:
            * bindings/js/JSNamedNodeMapCustom.cpp:
            * bindings/js/JSNodeCustom.h:
            * bindings/js/JSNodeListCustom.cpp:
            * bindings/js/JSStyleSheetCustom.cpp:
            * bindings/js/JSStyleSheetListCustom.cpp: Removed repetitive hand-written code.
    
            * bindings/scripts/CodeGeneratorJS.pm: Augmented autogenerator to do the job.
    
            * css/CSSRule.idl:
            * css/CSSRuleList.idl:
            * css/CSSStyleDeclaration.idl:
            * css/CSSValue.idl:
            * css/MediaList.idl:
            * css/StyleSheet.idl:
            * css/StyleSheetList.idl:
            * dom/DOMImplementation.idl:
            * dom/DOMStringMap.idl:
            * dom/NamedNodeMap.idl:
            * dom/Node.idl:
            * dom/NodeList.idl:
            * html/DOMTokenList.idl:
            * html/HTMLCollection.idl:
            * html/canvas/CanvasRenderingContext.idl: Updated attributes to plug into
            the autogenerator.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84699 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMTokenListCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMTokenListCustom.cpp
@@ -35,11 +35,6 @@ using namespace JSC;
 
 namespace WebCore {
 
-class JSDOMTokenListOwner : public JSC::WeakHandleOwner {
-    virtual bool isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown>, void* context, JSC::SlotVisitor&);
-    virtual void finalize(JSC::Handle<JSC::Unknown>, void* context);
-};
-
 bool JSDOMTokenListOwner::isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown> handle, void*, SlotVisitor& visitor)
 {
     JSDOMTokenList* jsDOMTokenList = static_cast<JSDOMTokenList*>(handle.get().asCell());
@@ -49,29 +44,6 @@ bool JSDOMTokenListOwner::isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown> h
     if (!element)
         return false;
     return visitor.containsOpaqueRoot(root(element));
-}
-
-void JSDOMTokenListOwner::finalize(JSC::Handle<JSC::Unknown> handle, void* context)
-{
-    JSDOMTokenList* jsDOMTokenList = static_cast<JSDOMTokenList*>(handle.get().asCell());
-    DOMWrapperWorld* world = static_cast<DOMWrapperWorld*>(context);
-    uncacheWrapper(world, jsDOMTokenList->impl(), jsDOMTokenList);
-}
-
-inline JSC::WeakHandleOwner* wrapperOwner(DOMWrapperWorld*, DOMTokenList*)
-{
-    DEFINE_STATIC_LOCAL(JSDOMTokenListOwner, jsDOMTokenListOwner, ());
-    return &jsDOMTokenListOwner;
-}
-
-inline void* wrapperContext(DOMWrapperWorld* world, DOMTokenList*)
-{
-    return world;
-}
-
-JSC::JSValue toJS(JSC::ExecState* exec, JSDOMGlobalObject* globalObject, DOMTokenList* impl)
-{
-    return wrap<JSDOMTokenList>(exec, globalObject, impl);
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/bindings/js/JSNamedNodeMapCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSNamedNodeMapCustom.cpp
@@ -36,11 +36,6 @@ using namespace JSC;
 
 namespace WebCore {
 
-class JSNamedNodeMapOwner : public JSC::WeakHandleOwner {
-    virtual bool isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown>, void* context, JSC::SlotVisitor&);
-    virtual void finalize(JSC::Handle<JSC::Unknown>, void* context);
-};
-
 bool JSNamedNodeMapOwner::isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown> handle, void*, SlotVisitor& visitor)
 {
     JSNamedNodeMap* jsNamedNodeMap = static_cast<JSNamedNodeMap*>(handle.get().asCell());
@@ -50,24 +45,6 @@ bool JSNamedNodeMapOwner::isReachableFromOpaqueRoots(JSC::Handle<JSC::Unknown> h
     if (!element)
         return false;
     return visitor.containsOpaqueRoot(root(element));
-}
-
-void JSNamedNodeMapOwner::finalize(JSC::Handle<JSC::Unknown> handle, void* context)
-{
-    JSNamedNodeMap* jsNamedNodeMap = static_cast<JSNamedNodeMap*>(handle.get().asCell());
-    DOMWrapperWorld* world = static_cast<DOMWrapperWorld*>(context);
-    uncacheWrapper(world, jsNamedNodeMap->impl(), jsNamedNodeMap);
-}
-
-inline JSC::WeakHandleOwner* wrapperOwner(DOMWrapperWorld*, NamedNodeMap*)
-{
-    DEFINE_STATIC_LOCAL(JSNamedNodeMapOwner, jsNamedNodeMapOwner, ());
-    return &jsNamedNodeMapOwner;
-}
-
-inline void* wrapperContext(DOMWrapperWorld* world, NamedNodeMap*)
-{
-    return world;
 }
 
 bool JSNamedNodeMap::canGetItemsForName(ExecState*, NamedNodeMap* impl, const Identifier& propertyName)
@@ -93,11 +70,6 @@ void JSNamedNodeMap::visitChildren(SlotVisitor& visitor)
     if (!element)
         return;
     visitor.addOpaqueRoot(root(element));
-}
-
-JSC::JSValue toJS(JSC::ExecState* exec, JSDOMGlobalObject* globalObject, NamedNodeMap* impl)
-{
-    return wrap<JSNamedNodeMap>(exec, globalObject, impl);
 }
 
 } // namespace WebCore
