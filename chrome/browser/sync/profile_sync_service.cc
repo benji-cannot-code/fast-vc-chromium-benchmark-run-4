3c3d22af5001e2b88ce1ca868f4d935fc89576c6
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 22:29:46 +0000

    Fix official builder and clang builder.
    BUG=none
    TEST=none
    TBR=csilv
    
    Review URL: http://codereview.chromium.org/6895033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "base/task.h"
 #include "chrome/browser/net/gaia/token_service.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/backend_migrator.h"
