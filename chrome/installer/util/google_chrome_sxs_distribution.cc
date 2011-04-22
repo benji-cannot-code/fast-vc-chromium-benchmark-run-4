e45d5e6f60d5ee856f78de18c170825b7c976649
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 19:36:15 +0000

    Use "canary" instead of "canary build" as the channel name in the Windows canary, matching the Mac canary and the new approved naming.
    BUG=79814
    TEST=The About window
    Review URL: http://codereview.chromium.org/6895022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/google_chrome_sxs_distribution.cc
+++ chrome/installer/util/google_chrome_sxs_distribution.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const wchar_t kChromeSxSGuid[] = L"{4ea16ac7-fd5a-47c3-875b-dbf4a2008c20}";
-const wchar_t kChannelName[] = L"canary build";
+const wchar_t kChannelName[] = L"canary";
 const wchar_t kBrowserAppId[] = L"ChromeCanary";
 const int kSxSIconIndex = 4;
 
