1689eb998858d14438c12bc54c760ea503786ba7
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 23:42:31 +0000

    Revert 82751 - Add NativeWidgetDelegate/Widget::OnKeyEvent post-IME handling.Refactor XEvent code and InputMethodGtk::DispatchKeyEvent.Nix WidgetWin::GetFocusedViewRootView, rename RootView::OnKeyEvent.Cleanup headers and refactor code in extension_input_api.cc.BUG=72040TEST=Key event handling in win/linux_views/touch; extension input API SendKeyboardEventInputFunction use.Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82713Review URL: http://codereview.chromium.org/6823055 TBR=msw@chromium.org Review URL: http://codereview.chromium.org/6897033
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_input_api.h
+++ chrome/browser/extensions/extension_input_api.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_function.h"
 
 namespace views {
-class Widget;
+  class RootView;
 }  // namespace views
 
 // Base class for input APIs.
@@ -31,7 +31,7 @@ class SendKeyboardEventInputFunction : public InputFunction {
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.input.sendKeyboardEvent");
 
  private:
-  views::Widget* GetTopLevelWidget();
+  views::RootView* GetRootView();
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_INPUT_API_H_
