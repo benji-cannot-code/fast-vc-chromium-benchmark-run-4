49f7f4b740a46db11e1e61cc17ce414b423687b3
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 13:49:04 +0000

    2011-04-22  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: fix de-obfuscation of scripts inlined in html.
            https://bugs.webkit.org/show_bug.cgi?id=58538
    
            * inspector/debugger/resources/obfuscated.js:
            * inspector/debugger/script-formatter-expected.txt:
            * inspector/debugger/script-formatter.html: Renamed from LayoutTests/inspector/debugger/script-formatter.html-disabled.
    2011-04-22  Pavel Podivilov  <podivilov@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: fix de-obfuscation of scripts inlined in html.
            https://bugs.webkit.org/show_bug.cgi?id=58538
    
            Use html tokenizer to find script boundaries instead of relying on scriptParsed events.
    
            Test: inspector/debugger/script-formatter.html
    
            * WebCore.gypi:
            * WebCore.vcproj/WebCore.vcproj:
            * inspector/front-end/JavaScriptFormatter.js: Renamed from Source/WebCore/inspector/front-end/UglifyJS/JavaScriptFormatter.js.
            * inspector/front-end/ScriptFormatter.js:
            (WebInspector.ScriptFormatter):
            (WebInspector.ScriptFormatter.prototype.formatContent):
            (WebInspector.ScriptFormatter.prototype._didFormatContent):
            * inspector/front-end/ScriptFormatterWorker.js:
            (onmessage):
            (formatScript):
            (HTMLScriptFormatter):
            (HTMLScriptFormatter.prototype.format):
            (HTMLScriptFormatter.prototype.scriptStarted):
            (HTMLScriptFormatter.prototype.scriptEnded):
            (HTMLScriptFormatter.prototype.styleSheetStarted):
            (HTMLScriptFormatter.prototype.styleSheetEnded):
            * inspector/front-end/SourceFile.js:
            (WebInspector.FormattedSourceFile.prototype._didRequestContent):
            * inspector/front-end/SourceHTMLTokenizer.js:
            (WebInspector.SourceHTMLTokenizer.prototype.scriptStarted):
            (WebInspector.SourceHTMLTokenizer.prototype.scriptEnded):
            (WebInspector.SourceHTMLTokenizer.prototype.styleSheetStarted):
            (WebInspector.SourceHTMLTokenizer.prototype.styleSheetEnded):
            (WebInspector.SourceHTMLTokenizer.prototype.nextToken):
            * inspector/front-end/SourceHTMLTokenizer.re2js:
            * inspector/front-end/WebKit.qrc:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.gypi
+++ third_party/WebKit/Source/WebCore/WebCore.gypi
@@ -6235,6 +6235,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'inspector/front-end/InspectorFrontendHostStub.js',
             'inspector/front-end/InjectedFakeWorker.js',
             'inspector/front-end/inspector.js',
+            'inspector/front-end/JavaScriptFormatter.js',
             'inspector/front-end/KeyboardShortcut.js',
             'inspector/front-end/MetricsSidebarPane.js',
             'inspector/front-end/NetworkItemView.js',
@@ -6312,7 +6313,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'inspector/front-end/textViewer.css',
         ],
         'webinspector_uglifyjs_files': [
-            'inspector/front-end/UglifyJS/JavaScriptFormatter.js',
             'inspector/front-end/UglifyJS/parse-js.js',
         ],
         'webinspector_image_files': [
--- third_party/WebKit/Source/WebCore/inspector/front-end/JavaScriptFormatter.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/JavaScriptFormatter.js
@@ -29,16 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-function FormattedContentBuilder(content)
+function FormattedContentBuilder(content, mapping, originalOffset, formattedOffset)
 {
     this._originalContent = content;
-    this._originalPositions = [0];
+    this._originalOffset = originalOffset;
     this._lastOriginalPosition = 0;
 
     this._formattedContent = [];
     this._formattedContentLength = 0;
-    this._formattedPositions = [0];
-    this._lastFormattedlPosition = 0;
+    this._formattedOffset = formattedOffset;
+    this._lastFormattedPosition = 0;
+
+    this._mapping = mapping;
 
     this._lineNumber = 0;
     this._nestingLevelLevel = 0;
@@ -64,9 +66,9 @@ FormattedContentBuilder.prototype = {
         }
 
         if (token.pos - this._lastOriginalPosition !== this._formattedContentLength - this._lastFormattedPosition) {
-            this._originalPositions.push(token.pos);
-            this._formattedPositions.push(this._formattedContentLength);
+            this._mapping.original.push(this._originalOffset + token.pos);
             this._lastOriginalPosition = token.pos;
+            this._mapping.formatted.push(this._formattedOffset + this._formattedContentLength);
             this._lastFormattedPosition = this._formattedContentLength;
         }
 
