aad1b0af1f8a1926f3f71ba489bece6f13e8d6db
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 04:59:38 +0000

    Remove chrome/browser/chromeos/wm_overview_*
    BUG=chromium-os:14305
    TEST=
    
    
    Review URL: http://codereview.chromium.org/6882069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_init.cc
+++ chrome/browser/ui/browser_init.cc
@@ -95,7 +95,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/sms_observer.h"
 #include "chrome/browser/chromeos/update_observer.h"
 #include "chrome/browser/chromeos/wm_message_listener.h"
-#include "chrome/browser/chromeos/wm_overview_controller.h"
 #include "chrome/browser/ui/webui/mediaplayer_ui.h"
 #endif
 
@@ -565,9 +564,6 @@ bool BrowserInit::LaunchBrowser(const CommandLine& command_line,
   // Create the WmMessageListener so that it can listen for messages regardless
   // of what window has focus.
   chromeos::WmMessageListener::GetInstance();
-
-  // Create the WmOverviewController so it can register with the listener.
-  chromeos::WmOverviewController::GetInstance();
 
   // Install the GView request interceptor that will redirect requests
   // of compatible documents (PDF, etc) to the GView document viewer.
