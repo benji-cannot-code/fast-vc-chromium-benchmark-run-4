7e2911f5e7f445c8defeb9a3daccaf801cf4046e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 21:20:12 +0000

    2011-04-22  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            CSP frame-src is missing
            https://bugs.webkit.org/show_bug.cgi?id=58643
    
            Update expected result to show that this test is passing now.
    
            * http/tests/security/contentSecurityPolicy/frame-src-blocked-expected.txt:
    2011-04-22  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Eric Seidel.
    
            CSP frame-src is missing
            https://bugs.webkit.org/show_bug.cgi?id=58643
    
            In this patch, I've audited all the callers to
            SecurityOrigin::canDisplay to make sure they all the appropriate
            Content-Security-Policy method as well.  I'm not sure how to test all
            these cases, but making this changes fixed the frame-src test.
    
            * loader/SubframeLoader.cpp:
            (WebCore::SubframeLoader::loadMediaPlayerProxyPlugin):
            (WebCore::SubframeLoader::createJavaAppletWidget):
            (WebCore::SubframeLoader::loadSubframe):
            (WebCore::SubframeLoader::loadPlugin):
            * loader/SubresourceLoader.cpp:
            (WebCore::SubresourceLoader::create):
            * loader/cache/CachedResourceLoader.cpp:
            (WebCore::CachedResourceLoader::canRequest):
            (WebCore::CachedResourceLoader::requestResource):
                - While I was understanding this code, I fixed the FIXME here.
            * loader/cache/CachedResourceLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
@@ -196,8 +196,15 @@ CachedResource* CachedResourceLoader::requestLinkResource(const String& url, Res
 }
 #endif
 
-bool CachedResourceLoader::canRequest(CachedResource::Type type, const KURL& url)
+bool CachedResourceLoader::canRequest(CachedResource::Type type, const KURL& url, bool forPreload)
 {
+    if (!document()->securityOrigin()->canDisplay(url)) {
+        if (!forPreload)
+            FrameLoader::reportLocalLoadFailed(document()->frame(), url.string());
+        LOG(ResourceLoading, "CachedResourceLoader::requestResource URL was not allowed by SecurityOrigin::canDisplay");
+        return 0;
+    }
+
     // Some types of resources can be loaded only from the same origin.  Other
     // types of resources, like Images, Scripts, and CSS, can be loaded from
     // any URL.
@@ -229,6 +236,8 @@ bool CachedResourceLoader::canRequest(CachedResource::Type type, const KURL& url
     //       callback to the FrameLoaderClient in case the embedder wants to
     //       update any security indicators.
     // 
+    // FIXME: Should we consider forPreload here?
+    //
     switch (type) {
     case CachedResource::Script:
 #if ENABLE(XSLT)
@@ -297,17 +306,9 @@ CachedResource* CachedResourceLoader::requestResource(CachedResource::Type type,
 
     if (!url.isValid())
         return 0;
-    
-    if (!canRequest(type, url))
-        return 0;
 
-    // FIXME: Figure out what is the correct way to merge this security check with the one above.
-    if (!document()->securityOrigin()->canDisplay(url)) {
-        if (!forPreload)
-            FrameLoader::reportLocalLoadFailed(document()->frame(), url.string());
-        LOG(ResourceLoading, "CachedResourceLoader::requestResource URL was not allowed by SecurityOrigin::canDisplay");
+    if (!canRequest(type, url, forPreload))
         return 0;
-    }
 
     if (memoryCache()->disabled()) {
         DocumentResourceMap::iterator it = m_documentResources.find(url.string());
