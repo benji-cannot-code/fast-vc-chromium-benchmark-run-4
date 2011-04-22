07978af0e43c3e4dad6b5861070b42b78f11f577
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 18:43:45 +0000

    2011-04-22  MORITA Hajime  <morrita@google.com>
            Unreviewed crash fix.
    
            * html/HTMLDetailsElement.cpp:
            (WebCore::DetailsSummaryElement::shouldInclude):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLDetailsElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLDetailsElement.cpp
@@ -82,7 +82,6 @@ PassRefPtr<DetailsSummaryElement> DetailsSummaryElement::create(Document* docume
 bool DetailsSummaryElement::shouldInclude(Node* node)
 {
     HTMLDetailsElement* details = static_cast<HTMLDetailsElement*>(shadowAncestorNode());
-    ASSERT(details->mainSummary());
     return details->mainSummary() == node;
 }
 
