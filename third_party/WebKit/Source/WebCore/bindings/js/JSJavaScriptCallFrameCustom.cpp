03ba111df739ccc0419079e3d67616442ecc0648
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 20:11:08 +0000

    2011-04-22  Sam Weinig  <sam@webkit.org>
            Reviewed by Gavin Barraclough and Oliver Hunt.
    
            Arrays should participate in global object forwarding fun
            https://bugs.webkit.org/show_bug.cgi?id=59215
    
            - Update results of prototype-inheritance-2 for new array behavior.
            - Remove checks for primitives, since they were incorrect.
            - Force function check to check a function not on a primitive (this
              wasn't happening by itself, since functions normally aren't enumerable
              in the DOM).
            - Add exception to the test to not try and test HTMLProgressElement, since
              it is not available on all platforms. This allows us to remove the windows
              specific results.
    
            * fast/dom/prototype-inheritance-2-expected.txt:
            * fast/dom/script-tests/prototype-inheritance-2.js:
            (constructorNamesForWindow):
            (crawl):
            * platform/win/fast/dom/prototype-inheritance-2-expected.txt: Removed.
    2011-04-22  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Gavin Barraclough and Oliver Hunt.
    
            Arrays should participate in global object forwarding fun
            https://bugs.webkit.org/show_bug.cgi?id=59215
    
            * runtime/JSGlobalObject.h:
            (JSC::constructEmptyArray):
            (JSC::constructArray):
            Add variants of constructArray that take a global object.
    2011-04-22  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Gavin Barraclough and Oliver Hunt.
    
            Arrays should participate in global object forwarding fun
            https://bugs.webkit.org/show_bug.cgi?id=59215
    
            Change callers of constructArray to use the new variant that takes
            a global object.
    
            * bindings/js/JSClipboardCustom.cpp:
            (WebCore::JSClipboard::types):
            * bindings/js/JSConsoleCustom.cpp:
            (WebCore::JSConsole::profiles):
            * bindings/js/JSJavaScriptCallFrameCustom.cpp:
            (WebCore::JSJavaScriptCallFrame::scopeChain):
            * bindings/js/JSMessageEventCustom.cpp:
            (WebCore::JSMessageEvent::ports):
            * bindings/js/JSScriptProfileNodeCustom.cpp:
            (WebCore::JSScriptProfileNode::children):
            * bindings/js/JSWebGLRenderingContextCustom.cpp:
            (WebCore::toJS):
            (WebCore::JSWebGLRenderingContext::getAttachedShaders):
            (WebCore::JSWebGLRenderingContext::getSupportedExtensions):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSJavaScriptCallFrameCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSJavaScriptCallFrameCustom.cpp
@@ -84,7 +84,7 @@ JSValue JSJavaScriptCallFrame::scopeChain(ExecState* exec) const
         ++iter;
     } while (iter != end);
 
-    return constructArray(exec, list);
+    return constructArray(exec, globalObject(), list);
 }
 
 JSValue JSJavaScriptCallFrame::scopeType(ExecState* exec)
