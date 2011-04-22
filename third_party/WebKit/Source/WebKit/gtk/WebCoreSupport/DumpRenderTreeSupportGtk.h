24951a950457d6154528df76e10b4095b5b21c5c
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-22 16:45:50 +0000

    2011-04-22  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            * DumpRenderTree/gtk/EventSender.cpp:
            (scalePageByCallback): scalePageBy() support.
    
    2011-04-22  Philippe Normand  <pnormand@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            Unskipped tests that should pass now that
            eventSender.scalePageBy() is supported in GTK.
    
            * platform/gtk/Skipped:
            * platform/gtk/fast/repaint/scale-page-shrink-expected.checksum: Added.
            * platform/gtk/fast/repaint/scale-page-shrink-expected.png: Added.
            * platform/gtk/fast/repaint/scale-page-shrink-expected.txt: Added.
    
    2011-04-22  Philippe Normand  <pnormand@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] DRT: implement eventSender.scalePageBy
            https://bugs.webkit.org/show_bug.cgi?id=59082
    
            Added support in DRT for webview scaling.
    
            * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
            (DumpRenderTreeSupportGtk::scale):
            * WebCoreSupport/DumpRenderTreeSupportGtk.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
@@ -104,6 +104,7 @@ public:
     static double defaultMinimumTimerInterval(); // Not really tied to WebView
     static void setMinimumTimerInterval(WebKitWebView*, double);
     static void rectangleForSelection(WebKitWebFrame*, GdkRectangle*);
+    static void scalePageBy(WebKitWebView*, float, float, float);
 
     // Accessibility
     static void incrementAccessibilityValue(AtkObject*);
