d3042d4fa7934022d0fe7553e47d40e9ff73a4ae
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-23 02:02:46 +0000

    Remove StreamSocketAdapter.
    PseudoTcpChannel is no longer used, so we don't need
    StreamSocketAdapter anymore.
    
    BUG=None
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/6898032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82769 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/jingle_session.cc
+++ remoting/protocol/jingle_session.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/rsa_private_key.h"
 #include "jingle/glue/channel_socket_adapter.h"
 #include "jingle/glue/pseudotcp_adapter.h"
-#include "jingle/glue/stream_socket_adapter.h"
 #include "net/base/cert_status_flags.h"
 #include "net/base/cert_verifier.h"
 #include "net/base/host_port_pair.h"
