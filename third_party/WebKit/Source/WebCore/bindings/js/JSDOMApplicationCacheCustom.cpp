4227ff7cc5a5f907645eba53dd357d7a5db3d208
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-23 04:44:11 +0000

    Fixed an upside-down conditional in my last check-in.
    * bindings/js/JSDOMApplicationCacheCustom.cpp:
    (WebCore::isObservable):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMApplicationCacheCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMApplicationCacheCustom.cpp
@@ -45,7 +45,7 @@ namespace WebCore {
 
 static inline bool isObservable(JSDOMApplicationCache* jsDOMApplicationCache, DOMApplicationCache* domApplicationCache)
 {
-    if (!jsDOMApplicationCache->hasCustomProperties())
+    if (jsDOMApplicationCache->hasCustomProperties())
         return true;
     if (domApplicationCache->hasEventListeners())
         return true;
