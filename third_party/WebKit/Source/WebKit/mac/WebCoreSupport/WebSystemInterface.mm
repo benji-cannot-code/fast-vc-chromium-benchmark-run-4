77825ca8100a7b69e2a42080f5a67730956fabf1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-23 06:22:59 +0000

    2011-04-22  Jon Lee  <jonlee@apple.com>
            Reviewed by Beth Dakin.
    
            Overlay scroller hard to see on pages with dark background (59183)
            https://bugs.webkit.org/show_bug.cgi?id=59183
            <rdar://problem/8975367>
    
            * WebCore.exp.in: adding method to allow changing style
            * page/FrameView.cpp:
            (WebCore::FrameView::recommendedScrollbarOverlayStyle): overridden to suggest a style based on CSS background color
            * page/FrameView.h:
            * platform/ScrollTypes.h: new enum to represent different overlay scrollbar styles
            * platform/ScrollableArea.cpp:
            (WebCore::ScrollableArea::recommendedScrollbarOverlayStyle): new virtual function to return a suggested overlay style
            * platform/ScrollableArea.h:
            * platform/mac/ScrollbarThemeMac.mm:
            (WebCore::ScrollbarThemeMac::paint):
            * platform/mac/WebCoreSystemInterface.h:
            * platform/mac/WebCoreSystemInterface.mm:
    2011-04-22  Jon Lee  <jonlee@apple.com>
    
            Reviewed by Beth Dakin.
    
            Overlay scroller hard to see on pages with dark background (59183)
            https://bugs.webkit.org/show_bug.cgi?id=59183
            <rdar://problem/8975367>
    
            * WebCoreSupport/WebSystemInterface.mm:
            (InitWebCoreSystemInterface): adding method to allow changing style
    2011-04-22  Jon Lee  <jonlee@apple.com>
    
            Reviewed by Beth Dakin.
    
            Overlay scroller hard to see on pages with dark background (59183)
            https://bugs.webkit.org/show_bug.cgi?id=59183
            <rdar://problem/8975367>
    
            * WebProcess/WebCoreSupport/mac/WebSystemInterface.mm:
            (InitWebCoreSystemInterface): adding method to allow changing style
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
+++ third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
@@ -147,6 +147,7 @@ void InitWebCoreSystemInterface(void)
     INIT(ScrollbarPainterIsHorizontal);
     INIT(ScrollbarPainterKnobRect);
     INIT(ScrollbarPainterSetOverlayState);
+    INIT(SetScrollbarPainterKnobStyle);
     INIT(MakeScrollbarPainterController);
     INIT(MakeScrollbarReplacementPainter);
     INIT(SetPainterForPainterController);
