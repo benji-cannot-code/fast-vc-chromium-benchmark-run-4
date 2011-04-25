aca4d1efea6b86dd7e82ed8cf8cfc102ab409822
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-25 20:03:39 +0000

    Print Preview: Move a branding specific string out of generated_resources.grd.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6894039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82912 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_ui_html_source.cc
+++ chrome/browser/ui/webui/print_preview_ui_html_source.cc
@@ -11,11 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/shared_memory.h"
 #include "base/string_piece.h"
+#include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/url_constants.h"
 #include "grit/browser_resources.h"
+#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
+#include "grit/google_chrome_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
@@ -26,8 +29,14 @@ void SetLocalizedStrings(DictionaryValue* localized_strings) {
       l10n_util::GetStringUTF8(IDS_PRINT_PREVIEW_TITLE));
   localized_strings->SetString(std::string("loading"),
       l10n_util::GetStringUTF8(IDS_PRINT_PREVIEW_LOADING));
+#if defined(GOOGLE_CHROME_BUILD)
+  localized_strings->SetString(std::string("noPlugin"),
+      l10n_util::GetStringFUTF8(IDS_PRINT_PREVIEW_NO_PLUGIN,
+                                ASCIIToUTF16("chrome://plugins/")));
+#else
   localized_strings->SetString(std::string("noPlugin"),
       l10n_util::GetStringUTF8(IDS_PRINT_PREVIEW_NO_PLUGIN));
+#endif
   localized_strings->SetString(std::string("previewFailed"),
       l10n_util::GetStringUTF8(IDS_PRINT_PREVIEW_FAILED));
 
