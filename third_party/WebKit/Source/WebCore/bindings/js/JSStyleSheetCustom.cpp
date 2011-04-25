ab27475f1a565e6fd655e3fcd898d376f3bf6ad4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-25 22:47:32 +0000

    Merged CREATE_DOM_NODE_WRAPPER and CREATE_DOM_OBJECT_WRAPPER into CREATE_DOM_WRAPPER because there's no meaningful difference between them anymore.
    Rubber-stamped by Beth Dakin.
    
    * bindings/js/JSCDATASectionCustom.cpp:
    (WebCore::toJSNewlyCreated):
    * bindings/js/JSCSSRuleCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSCSSValueCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSDOMBinding.h:
    * bindings/js/JSDocumentCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSElementCustom.cpp:
    (WebCore::toJSNewlyCreated):
    * bindings/js/JSEventCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSHTMLCollectionCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSImageDataCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSNodeCustom.cpp:
    (WebCore::createWrapperInline):
    * bindings/js/JSSVGPathSegCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSStyleSheetCustom.cpp:
    (WebCore::toJS):
    * bindings/js/JSTextCustom.cpp:
    (WebCore::toJSNewlyCreated):
    * bindings/js/JSWebKitCSSMatrixCustom.cpp:
    (WebCore::JSWebKitCSSMatrixConstructor::constructJSWebKitCSSMatrix):
    * bindings/js/JSWebSocketCustom.cpp:
    (WebCore::JSWebSocketConstructor::constructJSWebSocket):
    * bindings/js/JSXMLHttpRequestCustom.cpp:
    (WebCore::JSXMLHttpRequestConstructor::constructJSXMLHttpRequest):
    * bindings/js/JSXSLTProcessorCustom.cpp:
    (WebCore::JSXSLTProcessorConstructor::constructJSXSLTProcessor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSStyleSheetCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSStyleSheetCustom.cpp
@@ -52,9 +52,9 @@ JSValue toJS(ExecState* exec, JSDOMGlobalObject* globalObject, StyleSheet* style
         return wrapper;
 
     if (styleSheet->isCSSStyleSheet())
-        wrapper = CREATE_DOM_OBJECT_WRAPPER(exec, globalObject, CSSStyleSheet, styleSheet);
+        wrapper = CREATE_DOM_WRAPPER(exec, globalObject, CSSStyleSheet, styleSheet);
     else
-        wrapper = CREATE_DOM_OBJECT_WRAPPER(exec, globalObject, StyleSheet, styleSheet);
+        wrapper = CREATE_DOM_WRAPPER(exec, globalObject, StyleSheet, styleSheet);
 
     return wrapper;
 }
