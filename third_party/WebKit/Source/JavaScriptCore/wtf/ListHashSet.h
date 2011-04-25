58865d93c5fb13c6fd2dab897abd31d3aea67293
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-25 22:22:36 +0000

    2011-04-25  James Robinson  <jamesr@chromium.org>
            Reviewed by David Levin.
    
            Fix strict OwnPtr violations in ListHashSet and RenderLayerCompositor
            https://bugs.webkit.org/show_bug.cgi?id=59353
    
            * wtf/ListHashSet.h:
            (WTF::::ListHashSet):
    2011-04-25  James Robinson  <jamesr@chromium.org>
    
            Reviewed by David Levin.
    
            Fix strict OwnPtr violations in ListHashSet and RenderLayerCompositor
            https://bugs.webkit.org/show_bug.cgi?id=59353
    
            * rendering/RenderLayerCompositor.cpp:
            (WebCore::RenderLayerCompositor::RenderLayerCompositor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/ListHashSet.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/ListHashSet.h
@@ -377,7 +377,7 @@ namespace WTF {
     inline ListHashSet<T, inlineCapacity, U>::ListHashSet()
         : m_head(0)
         , m_tail(0)
-        , m_allocator(new NodeAllocator)
+        , m_allocator(adoptPtr(new NodeAllocator))
     {
     }
 
@@ -385,7 +385,7 @@ namespace WTF {
     inline ListHashSet<T, inlineCapacity, U>::ListHashSet(const ListHashSet& other)
         : m_head(0)
         , m_tail(0)
-        , m_allocator(new NodeAllocator)
+        , m_allocator(adoptPtr(new NodeAllocator))
     {
         const_iterator end = other.end();
         for (const_iterator it = other.begin(); it != end; ++it)
