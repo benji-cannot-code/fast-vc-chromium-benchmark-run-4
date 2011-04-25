ec18b068a79f7dcdb4e793cc366ffbbc2d160a40
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-25 17:52:52 +0000

    Fix users of platform_util::GetVersionStringModifier that really only care about the channel name to use the new platform_util::GetInstallationChannel. GetVersionStringModifier should only be used for display purposes. Use GetInstallationChannel to determine the channel name for comparisons.
    Fix the upgrade detector to treat the canary channel the same way that it
    treats the dev channel, using a shorter notification interval.
    
    BUG=79814
    TEST=none
    Review URL: http://codereview.chromium.org/6899034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/promo_resource_service.h
+++ chrome/browser/web_resource/promo_resource_service.h
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_WEB_RESOURCE_PROMO_RESOURCE_SERVICE_H_
 #pragma once
 
+#include <string>
+
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/web_resource/web_resource_service.h"
 
 namespace PromoResourceServiceUtil {
@@ -30,7 +33,8 @@ class PrefService;
 class PromoResourceService
     : public WebResourceService {
  public:
-  static bool IsBuildTargeted(const std::string& channel, int builds_targeted);
+  static bool IsBuildTargeted(platform_util::Channel channel,
+                              int builds_targeted);
 
   static void RegisterPrefs(PrefService* local_state);
 
@@ -81,7 +85,7 @@ class PromoResourceService
   void ScheduleNotificationOnInit();
 
   // Overrides the current Chrome release channel for testing purposes.
-  void set_channel(const char* channel) { channel_ = channel; }
+  void set_channel(platform_util::Channel channel) { channel_ = channel; }
 
   virtual void Unpack(const DictionaryValue& parsed_json);
 
@@ -188,7 +192,7 @@ class PromoResourceService
   DictionaryValue* web_resource_cache_;
 
   // Overrides the current Chrome release channel for testing purposes.
-  const char* channel_;
+  platform_util::Channel channel_;
 
   DISALLOW_COPY_AND_ASSIGN(PromoResourceService);
 };
