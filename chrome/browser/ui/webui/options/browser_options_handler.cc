1c94e3e8dea9f43d3f5b51cf55287d946a70196a
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-25 21:12:16 +0000

    Prevent the Mac canary from being made the default browser in the preferences. I must have missed this call in r82179.
    BUG=79814
    TEST=The "Make Google Chrome My Default Browser" button should be disabled in
         the preferences' Basics section. The explanatory text should state that
         the canary is a secondary installation and cannot be made default. It
         should not be possible to make the canary the default browser from
         anywhere within the application.
    BACK_DOOR=It's still possible to set the canary as the default browser from
              within Safari's preferences.
    Review URL: http://codereview.chromium.org/6904004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/instant/instant_confirm_dialog.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/net/url_fixer_upper.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/profile.h"
@@ -161,13 +162,11 @@ void BrowserOptionsHandler::SetHomePage(const ListValue* args) {
 }
 
 void BrowserOptionsHandler::UpdateDefaultBrowserState() {
-#if defined(OS_WIN)
   // Check for side-by-side first.
-  if (!BrowserDistribution::GetDistribution()->CanSetAsDefault()) {
+  if (!platform_util::CanSetAsDefaultBrowser()) {
     SetDefaultBrowserUIString(IDS_OPTIONS_DEFAULTBROWSER_SXS);
     return;
   }
-#endif
 
 #if defined(OS_MACOSX)
   ShellIntegration::DefaultBrowserState state =
