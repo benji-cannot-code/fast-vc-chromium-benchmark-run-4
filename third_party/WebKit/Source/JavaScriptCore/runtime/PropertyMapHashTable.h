1b6c5d8d7131281548d2507ffdc4001ae82bd7ac
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-25 20:16:06 +0000

    2011-04-25  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            PropertyMapHashTable should work with strict OwnPtr
            https://bugs.webkit.org/show_bug.cgi?id=59337
    
            This patch is in preparation for the strict OwnPtr hack-a-thon.
    
            * runtime/PropertyMapHashTable.h:
            (JSC::PropertyTable::PropertyTable):
            (JSC::PropertyTable::addDeletedOffset):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/PropertyMapHashTable.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/PropertyMapHashTable.h
@@ -266,7 +266,7 @@ inline PropertyTable::PropertyTable(JSGlobalData& globalData, JSCell* owner, con
     // Copy the m_deletedOffsets vector.
     Vector<unsigned>* otherDeletedOffsets = other.m_deletedOffsets.get();
     if (otherDeletedOffsets)
-        m_deletedOffsets.set(new Vector<unsigned>(*otherDeletedOffsets));
+        m_deletedOffsets = adoptPtr(new Vector<unsigned>(*otherDeletedOffsets));
 }
 
 inline PropertyTable::PropertyTable(JSGlobalData& globalData, JSCell* owner, unsigned initialCapacity, const PropertyTable& other)
@@ -290,7 +290,7 @@ inline PropertyTable::PropertyTable(JSGlobalData& globalData, JSCell* owner, uns
     // Copy the m_deletedOffsets vector.
     Vector<unsigned>* otherDeletedOffsets = other.m_deletedOffsets.get();
     if (otherDeletedOffsets)
-        m_deletedOffsets.set(new Vector<unsigned>(*otherDeletedOffsets));
+        m_deletedOffsets = adoptPtr(new Vector<unsigned>(*otherDeletedOffsets));
 }
 
 inline PropertyTable::~PropertyTable()
@@ -445,7 +445,7 @@ inline unsigned PropertyTable::getDeletedOffset()
 inline void PropertyTable::addDeletedOffset(unsigned offset)
 {
     if (!m_deletedOffsets)
-        m_deletedOffsets.set(new Vector<unsigned>);
+        m_deletedOffsets = adoptPtr(new Vector<unsigned>);
     m_deletedOffsets->append(offset);
 }
 
