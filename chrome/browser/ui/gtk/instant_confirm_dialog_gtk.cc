0edfb5c34d29df9a935b3c7b61458c5cc22130cd
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-25 22:44:01 +0000

    options: Get rid of ShowOptionsURL() function.
    This function is only called once by gtk code, so is cheaper to move the code
    there instead.
    
    BUG=None
    TEST=None
    
    R=evan@chromium.org,sky@chromium.org
    
    Review URL: http://codereview.chromium.org/6902007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/instant_confirm_dialog_gtk.cc
+++ chrome/browser/ui/gtk/instant_confirm_dialog_gtk.cc
@@ -10,9 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/instant/instant_confirm_dialog.h"
 #include "chrome/browser/instant/instant_controller.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/gtk/gtk_chrome_link_button.h"
 #include "chrome/browser/ui/gtk/gtk_util.h"
-#include "chrome/browser/ui/options/show_options_url.h"
 #include "googleurl/src/gurl.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -72,5 +73,10 @@ void InstantConfirmDialogGtk::OnResponse(GtkWidget* dialog, int response_id) {
 }
 
 void InstantConfirmDialogGtk::OnLinkButtonClicked(GtkWidget* button) {
-  browser::ShowOptionsURL(profile_, browser::InstantLearnMoreURL());
+  // We open a new browser window so the Options dialog doesn't get lost behind
+  // other windows.
+  Browser* browser = Browser::Create(profile_);
+  browser->AddSelectedTabWithURL(browser::InstantLearnMoreURL(),
+                                 PageTransition::LINK);
+  browser->window()->Show();
 }
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2769,8 +2769,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/omnibox/location_bar_util.h',
         'browser/ui/options/options_util.cc',
         'browser/ui/options/options_util.h',
-        'browser/ui/options/show_options_url.cc',
-        'browser/ui/options/show_options_url.h',
         'browser/ui/panels/panel.cc',
         'browser/ui/panels/panel.h',
         'browser/ui/panels/panel_browser_window_cocoa.mm',
