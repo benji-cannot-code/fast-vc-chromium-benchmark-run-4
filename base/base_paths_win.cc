322ebe861afaf59e8a3fb284355ecc85494239dd
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-25 18:03:12 +0000

    Extend PathService::Get to be able to return the X86 program files path.
    Cloud print's virtual driver needs this to be able to find the Chrome install directory.  Since the port monitor is a 64 bit DLL on 64 bit Windows it needs to request the X86 folder explicitly.
    
    
    BUG=
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82567
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82686
    
    Review URL: http://codereview.chromium.org/6873159
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82891 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_paths_win.cc
+++ base/base_paths_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -50,6 +50,16 @@ bool PathProviderWin(int key, FilePath* result) {
       GetSystemDirectory(system_buffer, MAX_PATH);
       cur = FilePath(system_buffer);
       break;
+    case base::DIR_PROGRAM_FILESX86:
+      if (base::win::OSInfo::GetInstance()->architecture() !=
+          base::win::OSInfo::X86_ARCHITECTURE) {
+        if (FAILED(SHGetFolderPath(NULL, CSIDL_PROGRAM_FILESX86, NULL,
+                                   SHGFP_TYPE_CURRENT, system_buffer)))
+          return false;
+        cur = FilePath(system_buffer);
+        break;
+      }
+      // Fall through to base::DIR_PROGRAM_FILES if we're on an X86 machine.
     case base::DIR_PROGRAM_FILES:
       if (FAILED(SHGetFolderPath(NULL, CSIDL_PROGRAM_FILES, NULL,
                                  SHGFP_TYPE_CURRENT, system_buffer)))
