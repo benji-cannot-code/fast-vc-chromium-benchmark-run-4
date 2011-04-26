af7e905f887a121d2bc1b294e63b0962374cbc71
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 02:37:52 +0000

    Add NativeWidgetDelegate/Widget::OnKeyEvent post-IME handling. Refactor XEvent code and InputMethodGtk::DispatchKeyEvent. Nix WidgetWin::GetFocusedViewRootView, rename RootView::OnKeyEvent. Cleanup headers and refactor code in extension_input_api.cc. Rename WidgetGtk::OnEventKey to avoid overloading Widget::OnEventKey.
    BUG=72040
    TEST=Key event handling in win/linux_views/touch; extension input API SendKeyboardEventInputFunction use.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82713
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=82751
    
    Review URL: http://codereview.chromium.org/6823055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82983 0039d316-1c4b-4281-b951-d872f2087c98

--- views/focus/accelerator_handler_gtk.cc
+++ views/focus/accelerator_handler_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,7 +17,7 @@ AcceleratorHandler::AcceleratorHandler() {}
 
 bool AcceleratorHandler::Dispatch(GdkEvent* event) {
   // The logic for handling keyboard accelerators has been moved into
-  // WidgetGtk::OnKeyEvent handler (views/widget/widget_gtk.cc).
+  // WidgetGtk::OnEventKey handler (views/widget/widget_gtk.cc).
   gtk_main_do_event(event);
   return true;
 }
