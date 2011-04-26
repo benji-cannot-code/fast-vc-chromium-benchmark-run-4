b09c65317351c5b01b337f138c3b5af1b319106d
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 01:44:37 +0000

    Fix glitchy drawing in faded tab titles
    When fading tab titles we would sometimes have some glitches when transitioning from the solid title to the faded title.
    
    The problem was that we were switching the front from normal clear type to cleartype off. This changed caused visible glitches when transitioning between the two fonts.
    
    Initially I switched clear type off to make it easier to calculate the alpha of the text. msw recomended using the luminosity of the text to determine the alpha. It turns out that this works great and we no longer have to turn off clear type in the faded sections.
    
    BUG=78869
    TEST=Reproduced the bug. Applied my patch and verified that the bug no longer reproduced.
    
    Review URL: http://codereview.chromium.org/6902016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82972 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/color_utils.h
+++ ui/gfx/color_utils.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,6 +19,8 @@ struct HSL {
   double s;
   double l;
 };
+
+unsigned char GetLuminanceForColor(SkColor color);
 
 // Calculated according to http://www.w3.org/TR/WCAG20/#relativeluminancedef
 double RelativeLuminance(SkColor color);
