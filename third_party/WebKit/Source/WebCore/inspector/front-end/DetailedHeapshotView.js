ee7148f6b57149e0898ab0d54eb60a09549c29a5
mnaganov@chromium.org <mnaganov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-26 09:41:57 +0000

    2011-04-26  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: [Chromium] Fix columns resizing in the detailed heap snapshot grids.
            https://bugs.webkit.org/show_bug.cgi?id=59438
    
            * inspector/front-end/DetailedHeapshotView.js:
            (WebInspector.DetailedHeapshotView.prototype._changeView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotView.js
@@ -848,6 +848,7 @@ WebInspector.DetailedHeapshotView.prototype = {
         this.dataGrid = view.grid;
         this.currentView.show();
         this.refreshVisibleData();
+        this.dataGrid.updateWidths();
         if (this.currentView === this.diffView) {
             this.baseSelectElement.removeStyleClass("hidden");
             if (!this.dataGrid.snapshotView) {
