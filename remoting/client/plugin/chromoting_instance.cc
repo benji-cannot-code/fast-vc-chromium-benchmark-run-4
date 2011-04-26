f05062c573df30ae3cbffa0acfb4b349eece939b
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 01:45:51 +0000

    Cope gracefully with scripts querying stats before client has started.
    BUG=80185
    TEST=Connect sandboxed from ChromeOS, OR add a delay in ConnectSandboxed in a dev build and use it to connect sandboxed.
    
    
    Review URL: http://codereview.chromium.org/6902011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82973 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_instance.cc
+++ remoting/client/plugin/chromoting_instance.cc
@@ -292,6 +292,8 @@ pp::Var ChromotingInstance::GetInstanceObject() {
 }
 
 ChromotingStats* ChromotingInstance::GetStats() {
+  if (!client_.get())
+    return NULL;
   return client_->GetStats();
 }
 
