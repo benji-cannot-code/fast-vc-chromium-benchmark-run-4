836232247c35153e78929038a14691f2ccf1fc5e
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 16:09:17 +0000

    [cros] Persist default user image.
    BUG=chromium-os:14488
    TEST=Sign in, change picture to some of the default ones. Verify that image used on login screen when logged out.
    
    Review URL: http://codereview.chromium.org/6897043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/path_service.h"
 #include "base/values.h"
+#include "chrome/browser/chromeos/login/default_user_images.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/options/take_photo_dialog.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -25,31 +26,22 @@ namespace chromeos {
 
 namespace {
 
-// Resource IDs of default user images.
-const int kDefaultImageResources[] = {
-    IDR_LOGIN_DEFAULT_USER,
-    IDR_LOGIN_DEFAULT_USER_1,
-    IDR_LOGIN_DEFAULT_USER_2,
-    IDR_LOGIN_DEFAULT_USER_3,
-    IDR_LOGIN_DEFAULT_USER_4
-};
-
+// URLs to default user images through chrome://theme/ scheme.
 const char* kDefaultImagePaths[] = {
-    "chrome://theme/IDR_LOGIN_DEFAULT_USER",
-    "chrome://theme/IDR_LOGIN_DEFAULT_USER_1",
-    "chrome://theme/IDR_LOGIN_DEFAULT_USER_2",
-    "chrome://theme/IDR_LOGIN_DEFAULT_USER_3",
-    "chrome://theme/IDR_LOGIN_DEFAULT_USER_4"
+  "chrome://theme/IDR_LOGIN_DEFAULT_USER",
+  "chrome://theme/IDR_LOGIN_DEFAULT_USER_1",
+  "chrome://theme/IDR_LOGIN_DEFAULT_USER_2",
+  "chrome://theme/IDR_LOGIN_DEFAULT_USER_3",
+  "chrome://theme/IDR_LOGIN_DEFAULT_USER_4"
 };
 
-SkBitmap* GetUserImageFromURL(const std::string& url) {
+int GetUserImageIndexFromURL(const std::string& url) {
   for (size_t i = 0; i < arraysize(kDefaultImagePaths); ++i) {
     if (url == kDefaultImagePaths[i]) {
-      return ResourceBundle::GetSharedInstance().GetBitmapNamed(
-          kDefaultImageResources[i]);
+      return i;
     }
   }
-  return NULL;
+  return -1;
 }
 
 // Returns info about extensions for files we support as user images.
@@ -167,9 +159,16 @@ void ChangePictureOptionsHandler::SelectImage(const ListValue* args) {
     NOTREACHED();
     return;
   }
-  const SkBitmap* image = GetUserImageFromURL(image_url);
-  if (image)
-    UserManager::Get()->SetLoggedInUserImage(*image);
+  int user_image_index = GetUserImageIndexFromURL(image_url);
+  if (user_image_index == -1)
+    return;
+
+  const SkBitmap* image = ResourceBundle::GetSharedInstance().GetBitmapNamed(
+          kDefaultImageResources[user_image_index]);
+  UserManager* user_manager = UserManager::Get();
+  user_manager->SetLoggedInUserImage(*image);
+  user_manager->SaveUserImagePath(user_manager->logged_in_user().email(),
+                                  kDefaultImageNames[user_image_index]);
 }
 
 void ChangePictureOptionsHandler::FileSelected(const FilePath& path,
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -478,6 +478,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/login/cookie_fetcher.h',
         'browser/chromeos/login/cryptohome_op.cc',
         'browser/chromeos/login/cryptohome_op.h',
+        'browser/chromeos/login/default_user_images.h',
         'browser/chromeos/login/dom_login_display.cc',
         'browser/chromeos/login/dom_login_display.h',
         'browser/chromeos/login/dom_login_display_host.cc',
