aeace7b1832fee296325280e10c6492bb1bbdf0e
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-26 10:35:13 +0000

    Unreviewed rollout r84877 and StyleRareInheritedData.cpp changes of r84892, because it broke transitions/multiple-text-shadow-transition.html
    Fix OwnPtr strict errors in RenderStyle and make StyleRareInheritedData::textShadow an OwnPtr
    https://bugs.webkit.org/show_bug.cgi?id=59377
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    * page/animation/AnimationBase.cpp:
    (WebCore::PropertyWrapperShadow::PropertyWrapperShadow):
    (WebCore::PropertyWrapperShadow::blend):
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::setTextShadow):
    (WebCore::RenderStyle::setBoxShadow):
    * rendering/style/RenderStyle.h:
    (WebCore::InheritedFlags::textShadow):
    * rendering/style/StyleRareInheritedData.cpp:
    (WebCore::StyleRareInheritedData::StyleRareInheritedData):
    (WebCore::StyleRareInheritedData::~StyleRareInheritedData):
    * rendering/style/StyleRareInheritedData.h:
    * rendering/style/StyleRareNonInheritedData.cpp:
    (WebCore::StyleRareNonInheritedData::StyleRareNonInheritedData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/StyleRareInheritedData.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleRareInheritedData.cpp
@@ -33,6 +33,7 @@ namespace WebCore {
 
 StyleRareInheritedData::StyleRareInheritedData()
     : textStrokeWidth(RenderStyle::initialTextStrokeWidth())
+    , textShadow(0)
     , indent(RenderStyle::initialTextIndent())
     , m_effectiveZoom(RenderStyle::initialZoom())
     , widows(RenderStyle::initialWidows())
@@ -64,7 +65,7 @@ StyleRareInheritedData::StyleRareInheritedData(const StyleRareInheritedData& o)
     , textStrokeWidth(o.textStrokeWidth)
     , textFillColor(o.textFillColor)
     , textEmphasisColor(o.textEmphasisColor)
-    , textShadow(o.textShadow ? adoptPtr(new ShadowData(*o.textShadow)) : PassOwnPtr<ShadowData>())
+    , textShadow(o.textShadow ? new ShadowData(*o.textShadow) : 0)
     , highlight(o.highlight)
     , cursorData(o.cursorData)
     , indent(o.indent)
@@ -97,6 +98,7 @@ StyleRareInheritedData::StyleRareInheritedData(const StyleRareInheritedData& o)
 
 StyleRareInheritedData::~StyleRareInheritedData()
 {
+    delete textShadow;
 }
 
 static bool cursorDataEquivalent(const CursorList* c1, const CursorList* c2)
