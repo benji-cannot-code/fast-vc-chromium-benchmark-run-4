05085f0cee066ee53e36024428379b5a9cea43c2
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 01:36:46 +0000

    Remove obsolete constants from MostVisitedHandler
    BUG=none
    TEST=compile
    
    Review URL: http://codereview.chromium.org/6902015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/most_visited_handler.cc
+++ chrome/browser/ui/webui/most_visited_handler.cc
@@ -38,16 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/locale_settings.h"
 #include "ui/base/l10n/l10n_util.h"
 
-namespace {
-
-// The number of most visited pages we show.
-const size_t kMostVisitedPages = 8;
-
-// The number of days of history we consider for most visited entries.
-const int kMostVisitedScope = 90;
-
-}  // namespace
-
 // This struct is used when getting the pre-populated pages in case the user
 // hasn't filled up his most visited pages.
 struct MostVisitedHandler::MostVisitedPage {
