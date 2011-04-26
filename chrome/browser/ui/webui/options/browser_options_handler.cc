bcb640d1e1b95d28882b20ff659b8c7b89478a59
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 15:40:03 +0000

    Remove #include obsoleted by r82925.
    BUG=none
    TEST=build
    Review URL: http://codereview.chromium.org/6904009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/options/options_managed_banner_handler.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/installer/util/browser_distribution.h"
 #include "content/browser/browser_thread.h"
 #include "content/common/notification_service.h"
 #include "content/common/notification_source.h"
