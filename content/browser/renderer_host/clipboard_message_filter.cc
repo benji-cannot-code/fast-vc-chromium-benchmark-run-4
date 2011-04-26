b2b84b9f9f83d43933cfae5e65adf75f63a146e7
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 14:09:07 +0000

    Linux: include zlib.h properly
    Linux links with the system zlib, so system headers should be used.
    
    Add dependency on zlib to make sure -Ithird_party/zlib gets passed on other platforms.
    
    Also see Gentoo Linux bug report:
    http://bugs.gentoo.org/show_bug.cgi?id=364205
    
    Contributed by Mike Gilbert <floppymaster@gmail.com>.
    
    Original review: http://codereview.chromium.org/6873148/
    
    BUG=none
    
    Review URL: http://codereview.chromium.org/6893021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83009 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/clipboard_message_filter.cc
+++ content/browser/renderer_host/clipboard_message_filter.cc
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/clipboard_message_filter.h"
 
+#if defined(USE_SYSTEM_ZLIB)
+#include <zlib.h>
+#else
+#include "third_party/zlib/zlib.h"
+#endif
+
 #include "base/stl_util-inl.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/clipboard_dispatcher.h"
@@ -12,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 #include "ipc/ipc_message_macros.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/zlib/zlib.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/size.h"
 
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '../third_party/flac/flac.gyp:libflac',
         '../third_party/speex/speex.gyp:libspeex',
         '../third_party/WebKit/Source/WebKit/chromium/WebKit.gyp:webkit',
+        '../third_party/zlib/zlib.gyp:zlib',
         '../ui/ui.gyp:ui_base',
       ],
       'include_dirs': [
