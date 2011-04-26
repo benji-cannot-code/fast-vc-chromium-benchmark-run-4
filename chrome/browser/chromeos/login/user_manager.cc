d8baa73c9c6fe40fcfc707f5f494e498390b28e2
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 15:17:52 +0000

    Update logged-in user image when image is loaded.
    R=altimofeev@chromium.org
    BUG=chromium-os:13436
    TEST=Go to chrome://settings/changePicture. Click on folder icon and pick some image file. Hold power button long enough to get the screen locked. Verify that the image on lock screen is the chosen one on previous steps.
    
    Review URL: http://codereview.chromium.org/6894047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager.cc
+++ chrome/browser/chromeos/login/user_manager.cc
@@ -447,7 +447,6 @@ const UserManager::User& UserManager::logged_in_user() const {
 void UserManager::SetLoggedInUserImage(const SkBitmap& image) {
   if (logged_in_user_.email().empty())
     return;
-  logged_in_user_.set_image(image);
   OnImageLoaded(logged_in_user_.email(), image, false);
 }
 
@@ -521,6 +520,8 @@ void UserManager::OnImageLoaded(const std::string& username,
   User user;
   user.set_email(username);
   user.set_image(image);
+  if (logged_in_user_.email() == username)
+    logged_in_user_.set_image(image);
   if (should_save_image)
     SaveUserImage(username, image);
   NotificationService::current()->Notify(
