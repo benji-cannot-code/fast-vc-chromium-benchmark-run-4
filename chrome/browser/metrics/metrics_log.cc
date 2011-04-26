c1306ca002187ec5156ca4ea1029080ada946e1f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-26 19:44:45 +0000

    Move GpuProcessHostUIShim and GpuDataManager now that they don't depend on grit (which would have caused a circular dependency in gyp). Review URL: http://codereview.chromium.org/6903026
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83060 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_log.cc
+++ chrome/browser/metrics/metrics_log.cc
@@ -20,11 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/gpu_data_manager.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/logging_chrome.h"
 #include "chrome/common/pref_names.h"
+#include "content/browser/gpu_data_manager.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/plugins/npapi/webplugininfo.h"
 
