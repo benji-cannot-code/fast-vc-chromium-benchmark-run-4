bcb0cbeed10fad549170eaa74440ffb9641668f2
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-26 14:03:53 +0000

    Unreviewed build fix.
    * Platform/Region.cpp: Need to include stdio.h for printf
    (at least in my Qt-Linux environment).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/Region.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/Region.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "Region.h"
+#include <stdio.h>
 
 // A region class based on the paper "Scanline Coherent Shape Algebra"
 // by Jonathan E. Steinhart from the book "Graphics Gems II".
