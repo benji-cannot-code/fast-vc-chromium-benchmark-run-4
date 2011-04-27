664f0cec6d98fd4647050ea7a2891c6a17381e6e
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 21:01:30 +0000

    Revert 83198 - Remove DestroyingProfile from a few places.These guys are both non-refcounted and have a lifetime that isexactly equivalent to Profile.The DestroyingProfile code dates to a time whenExtensionService was ref-counted.Review URL: http://codereview.chromium.org/6905050 TBR=aa@chromium.org Review URL: http://codereview.chromium.org/6902085
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolbar_model.cc
+++ chrome/browser/extensions/extension_toolbar_model.cc
@@ -35,6 +35,10 @@ ExtensionToolbarModel::ExtensionToolbarModel(ExtensionService* service)
 ExtensionToolbarModel::~ExtensionToolbarModel() {
 }
 
+void ExtensionToolbarModel::DestroyingProfile() {
+  registrar_.RemoveAll();
+}
+
 void ExtensionToolbarModel::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
 }
