bd4b277928331a007f794ac2680a41d62ef2562a
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 17:23:34 +0000

    2011-04-27  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Move WebViewWidget to UIProcess/API/gtk
            https://bugs.webkit.org/show_bug.cgi?id=59594
    
            API files should be under the API directory. This view widget will
            be used by both C and gtk high level API, so it has been renamed
            to WebKitWebViewBase.
    
            * GNUmakefile.am:
            * UIProcess/API/gtk/WebKitWebViewBase.cpp: Renamed from Source/WebKit2/UIProcess/gtk/WebViewWidget.cpp.
            (webkitWebViewBaseRealize):
            (webkitWebViewBaseContainerAdd):
            (webkitWebViewBaseDispose):
            (webkitWebViewBaseInit):
            (webViewExpose):
            (webViewDraw):
            (webViewSizeAllocate):
            (webViewFocusInEvent):
            (webViewFocusOutEvent):
            (webViewKeyPressEvent):
            (webViewKeyReleaseEvent):
            (getEventTime):
            (webViewButtonPressEvent):
            (webViewButtonReleaseEvent):
            (webViewScrollEvent):
            (webViewMotionNotifyEvent):
            (webkitWebViewBaseClassInit):
            (webkitWebViewBaseGetType):
            (webkitWebViewBaseGetWebViewInstance):
            (webkitWebViewBaseSetWebViewInstance):
            (webkitWebViewBaseGetIMContext):
            * UIProcess/API/gtk/WebKitWebViewBase.h: Renamed from Source/WebKit2/UIProcess/gtk/WebViewWidget.h.
            * UIProcess/gtk/WebView.cpp:
            (WebKit::WebView::WebView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebView.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebContext.h"
 #include "WebContextMenuProxy.h"
 #include "WebEventFactory.h"
-#include "WebViewWidget.h"
+#include "WebKitWebViewBase.h"
 #include "WebPageProxy.h"
 #include <wtf/text/WTFString.h>
 
@@ -244,13 +244,13 @@ WebView::WebView(WebContext* context, WebPageGroup* pageGroup)
 {
     m_page = context->createWebPage(this, pageGroup);
 
-    m_viewWidget = static_cast<GtkWidget*>(g_object_new(WEB_VIEW_TYPE_WIDGET, NULL));
+    m_viewWidget = static_cast<GtkWidget*>(g_object_new(WEBKIT_TYPE_WEB_VIEW_BASE, NULL));
     ASSERT(m_viewWidget);
 
     m_page->initializeWebPage();
 
-    WebViewWidget* webViewWidget = WEB_VIEW_WIDGET(m_viewWidget);
-    webViewWidgetSetWebViewInstance(webViewWidget, this);
+    WebKitWebViewBase* webViewWidget = WEBKIT_WEB_VIEW_BASE(m_viewWidget);
+    webkitWebViewBaseSetWebViewInstance(webViewWidget, this);
 
     g_signal_connect(m_nativeWidget.get(), "backspace", G_CALLBACK(backspaceCallback), this);
     g_signal_connect(m_nativeWidget.get(), "cut-clipboard", G_CALLBACK(cutClipboardCallback), this);
