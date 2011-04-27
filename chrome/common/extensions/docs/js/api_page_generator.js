de1251c8cfb15ca7f1f729867519b7218924f31c
caseq@google.com <caseq@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 22:44:56 +0000

    Rename webInspector.* API to chrome.experimental.devtools.
    Move DevTools API schema to chromium land.
    Provide brief deprecation notices in old experimental.webInspector.* pages.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6902077
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83239 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/js/api_page_generator.js
+++ chrome/common/extensions/docs/js/api_page_generator.js
@@ -20,13 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 var API_TEMPLATE = "template/api_template.html";
-var WEBKIT_PATH = "../../../../third_party/WebKit";
 var SCHEMA = "../api/extension_api.json";
-var DEVTOOLS_SCHEMA = WEBKIT_PATH +
-  "/Source/WebCore/inspector/front-end/ExtensionAPISchema.json";
+var DEVTOOLS_SCHEMA = "../api/devtools_api.json";
 var USE_DEVTOOLS_SCHEMA =
-  /\.webInspector[^/]*\.html/.test(location.pathname);
-var API_MODULE_PREFIX = USE_DEVTOOLS_SCHEMA ? "" : "chrome.";
+  /\.devtools[^/]*\.html/.test(location.pathname);
+var API_MODULE_PREFIX = "chrome.";
 var SAMPLES = "samples.json";
 var REQUEST_TIMEOUT = 2000;
 
@@ -348,9 +346,9 @@ function experimentalAPIs() {
   }).sort();
 }
 
-function webInspectorAPIs() {
+function devtoolsAPIs() {
   return schema.filter(function(module) {
-    return !module.nodoc && module.namespace.indexOf("webInspector.") !== 0;
+    return !module.nodoc && module.namespace.indexOf("devtools.") !== 0;
   }).map(function(module) {
     return module.namespace;
   }).sort();
