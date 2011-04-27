9572093e15f6c562acf2e4cdbcc99b513546e511
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 12:37:13 +0000

    2011-04-27  Pavel Feldman  <pfeldman@google.com>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: clicking bookmarklet multiplies resources panel entries.
            https://bugs.webkit.org/show_bug.cgi?id=59590
    
            * inspector/front-end/ResourceTreeModel.js:
            (WebInspector.ResourceTreeModel):
            (WebInspector.ResourceTreeModel.prototype._processCachedResources):
            (WebInspector.ResourceTreeModel.prototype._frameNavigated):
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype._populateResourceTree):
            (WebInspector.FrameTreeElement):
            (WebInspector.FrameTreeElement.prototype.appendResource):
            * inspector/front-end/inspector.js:
            (WebInspector.frontendReused):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourcesPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourcesPanel.js
@@ -179,6 +179,7 @@ WebInspector.ResourcesPanel.prototype = {
         WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.FrameDetached, this._frameDetached, this);
         WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.ResourceAdded, this._resourceAdded, this);
         WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.CachedResourcesLoaded, this._cachedResourcesLoaded, this); 
+        WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.WillLoadCachedResources, this._resetResourcesTree, this); 
 
         function populateFrame(frameId)
         {
@@ -193,6 +194,8 @@ WebInspector.ResourcesPanel.prototype = {
                 this._resourceAdded({data:resources[i]});
         }
         populateFrame.call(this, 0);
+
+        this._initDefaultSelection();
     },
 
     _frameAdded: function(event)
@@ -258,9 +261,7 @@ WebInspector.ResourcesPanel.prototype = {
     {
         if (event.data.isMainFrame) {
             // Total update.
-            this.resourcesListTreeElement.removeChildren();
-            this._treeElementForFrameId = {};
-            this._reset();
+            this._resetResourcesTree();
             return;
         }
 
@@ -268,6 +269,13 @@ WebInspector.ResourcesPanel.prototype = {
         var frameTreeElement = this._treeElementForFrameId[frameId];
         if (frameTreeElement)
             frameTreeElement.removeChildren();        
+    },
+
+    _resetResourcesTree: function()
+    {
+        this.resourcesListTreeElement.removeChildren();
+        this._treeElementForFrameId = {};
+        this._reset();
     },
 
     _cachedResourcesLoaded: function()
@@ -927,6 +935,7 @@ WebInspector.FrameTreeElement = function(storagePanel, frameId, title, subtitle)
     this._frameId = frameId;
     this.setTitles(title, subtitle);
     this._categoryElements = {};
+    this._treeElementForResource = {};
 }
 
 WebInspector.FrameTreeElement.prototype = {
@@ -992,6 +1001,9 @@ WebInspector.FrameTreeElement.prototype = {
 
     appendResource: function(resource)
     {
+        if (this._treeElementForResource[resource.url])
+            return;
+
         var categoryName = resource.category.name;
         var categoryElement = resource.category === WebInspector.resourceCategories.documents ? this : this._categoryElements[categoryName];
         if (!categoryElement) {
@@ -1002,6 +1014,14 @@ WebInspector.FrameTreeElement.prototype = {
         var resourceTreeElement = new WebInspector.FrameResourceTreeElement(this._storagePanel, resource);
         this._insertInPresentationOrder(categoryElement, resourceTreeElement);
         resourceTreeElement._populateRevisions();
+
+        this._treeElementForResource[resource.url] = resourceTreeElement;
+    },
+
+    removeChildren: function()
+    {
+        WebInspector.BaseStorageTreeElement.prototype.removeChildren.call(this);
+        this._treeElementForResource = [];
     },
 
     appendChild: function(treeElement)
