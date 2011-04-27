160e97fccd37ab86815e966d74c458a0e87387cb
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 02:00:25 +0000

    2011-04-26  Dirk Pranke  <dpranke@chromium.org>
            Unreviewed, rolling out r84982.
            http://trac.webkit.org/changeset/84982
            https://bugs.webkit.org/show_bug.cgi?id=59558
    
            re-land r84951
    
            * WebKit.gyp:
            * public/WebFrame.h:
            * public/WebWidget.h:
            * public/mac/WebSubstringUtil.h: Added.
            * src/WebFrameImpl.cpp:
            (WebKit::WebFrameImpl::firstRectForCharacterRange):
            (WebKit::WebFrameImpl::characterIndexForPoint):
            * src/WebFrameImpl.h:
            * src/WebPopupMenuImpl.cpp:
            (WebKit::WebPopupMenuImpl::compositionRange):
            (WebKit::WebPopupMenuImpl::caretOrSelectionRange):
            * src/WebPopupMenuImpl.h:
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::compositionRange):
            (WebKit::WebViewImpl::caretOrSelectionRange):
            * src/WebViewImpl.h:
            * src/mac/WebSubstringUtil.mm: Added.
            (WebKit::WebSubstringUtil::attributedSubstringInRange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPopupMenuImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPopupMenuImpl.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebInputEvent.h"
 #include "WebInputEventConversion.h"
+#include "WebRange.h"
 #include "WebRect.h"
 #include "WebWidgetClient.h"
 
@@ -253,6 +254,13 @@ bool WebPopupMenuImpl::confirmComposition(const WebString& text)
     return false;
 }
 
+bool WebPopupMenuImpl::compositionRange(size_t* location, size_t* length)
+{
+    *location = 0;
+    *length = 0;
+    return false;
+}
+
 WebTextInputType WebPopupMenuImpl::textInputType()
 {
     return WebTextInputTypeNone;
@@ -261,6 +269,13 @@ WebTextInputType WebPopupMenuImpl::textInputType()
 WebRect WebPopupMenuImpl::caretOrSelectionBounds()
 {
     return WebRect();
+}
+
+bool WebPopupMenuImpl::caretOrSelectionRange(size_t* location, size_t* length)
+{
+    *location = 0;
+    *length = 0;
+    return false;
 }
 
 void WebPopupMenuImpl::setTextDirection(WebTextDirection direction)
