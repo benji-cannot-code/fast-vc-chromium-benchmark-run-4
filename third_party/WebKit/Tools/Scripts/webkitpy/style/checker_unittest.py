38a00edab5e379fe3c9769346ebbcf4bfc62c9a2
yi.4.shen@nokia.com <yi.4.shen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 17:55:30 +0000

    2011-04-27  Yi Shen  <yi.4.shen@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Upstream Symbian platform plugin
            https://bugs.webkit.org/show_bug.cgi?id=58435
    
            Upstream Symbian platform plugin.
    
            * Source/WebKit.pro: Add platformplugin path to the SUBDIRS.
    2011-04-27  Yi Shen  <yi.4.shen@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Upstream Symbian platform plugin
            https://bugs.webkit.org/show_bug.cgi?id=58435
    
            Exempting directory WebKit/qt/symbian/platformplugin from style guide.
    
            * Scripts/webkitpy/style/checker.py:
            * Scripts/webkitpy/style/checker_unittest.py:
    2011-04-27  Yi Shen  <yi.4.shen@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Upstream Symbian platform plugin
            https://bugs.webkit.org/show_bug.cgi?id=58435
    
            Upstream Symbian platform plugin.
    
            * QtWebKit.pro: Add platformplugin.dll to the QtWebKit.sis.
            * symbian/platformplugin: Added.
            * symbian/platformplugin/WebPlugin.cpp: Added.
            (ItemListDelegate::ItemListDelegate):
            (ItemListDelegate::paint):
            (Popup::Popup):
            (Popup::resizeEvent):
            (Popup::populateList):
            (Popup::onItemSelected):
            (Popup::updateSelectionsBeforeDialogClosing):
            (Popup::updateAndClose):
            (WebPopup::WebPopup):
            (WebPopup::~WebPopup):
            (WebPopup::createSingleSelectionPopup):
            (WebPopup::createMultipleSelectionPopup):
            (WebPopup::createPopup):
            (WebPopup::show):
            (WebPopup::hide):
            (WebPopup::popupClosed):
            (WebPopup::itemClicked):
            (SingleSelectionPopup::SingleSelectionPopup):
            (MultipleSelectionPopup::MultipleSelectionPopup):
            (WebNotificationPresenter::showNotification):
            (WebPlugin::supportsExtension):
            (WebPlugin::createExtension):
            * symbian/platformplugin/WebPlugin.h: Added.
            (Popup::preSelectedIndices):
            (Popup::listWidget):
            (WebNotificationPresenter::WebNotificationPresenter):
            (WebNotificationPresenter::~WebNotificationPresenter):
            * symbian/platformplugin/platformplugin.pro: Added.
            * symbian/platformplugin/qwebkitplatformplugin.h: Copied from WebKit/qt/Api/qwebkitplatformplugin.h.
            (QWebSelectData::~QWebSelectData):
            (QWebSelectMethod::~QWebSelectMethod):
            (QWebNotificationData::~QWebNotificationData):
            (QWebNotificationPresenter::QWebNotificationPresenter):
            (QWebNotificationPresenter::~QWebNotificationPresenter):
            (QWebTouchModifier::~QWebTouchModifier):
            (QWebFullScreenVideoHandler::QWebFullScreenVideoHandler):
            (QWebFullScreenVideoHandler::~QWebFullScreenVideoHandler):
            (QWebKitPlatformPlugin::~QWebKitPlatformPlugin):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checker_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checker_unittest.py
@@ -238,6 +238,8 @@ class GlobalVariablesTest(unittest.TestCase):
                     "readability/braces")
         assertCheck("Source/WebKit/qt/examples/platformplugin/WebPlugin.cpp",
                     "readability/braces")
+        assertCheck("Source/WebKit/qt/symbian/platformplugin/WebPlugin.cpp",
+                    "readability/braces")
         assertNoCheck("Source/JavaScriptCore/qt/api/qscriptengine.cpp",
                       "readability/naming")
         assertNoCheck("Source/JavaScriptCore/qt/benchmarks"
@@ -251,6 +253,8 @@ class GlobalVariablesTest(unittest.TestCase):
                       "readability/naming")
         assertNoCheck("Source/WebKit/qt/examples/platformplugin/WebPlugin.cpp",
                       "readability/naming")
+        assertNoCheck("Source/WebKit/qt/symbian/platformplugin/WebPlugin.cpp",
+                      "build/header_guard")
 
         assertNoCheck("Tools/MiniBrowser/qt/UrlLoader.cpp",
                     "build/include")
