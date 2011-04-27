99148d1c7d117b9948fa68c1bc21162b0fc751ad
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 23:56:33 +0000

    Fix clang compilation issues following r83248. This might be necessary to merge bug 77536 to avoid clang compilation errors.
    TEST=linux_clang
    BUG=77536
    Review URL: http://codereview.chromium.org/6903092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83258 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_surface.cc
+++ ui/gfx/gl/gl_surface.cc
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
+bool GLSurface::Initialize()
+{
+  return true;
+}
+
 unsigned int GLSurface::GetBackingFrameBufferObject() {
   return 0;
 }
