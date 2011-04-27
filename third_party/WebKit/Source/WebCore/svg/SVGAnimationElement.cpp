c1cbaad6651102254b4a603476d931e8f673fb56
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 01:28:10 +0000

    2011-04-26  Dinesh K Garg  <dineshg@codeaurora.org>
            Reviewed by Eric Seidel.
    
            Test case for: beginElement broken by setAttribute
            https://bugs.webkit.org/show_bug.cgi?id=26019
    
            * svg/animations/animate-beginElementAt-expected.txt: Added.
            * svg/animations/animate-beginElementAt.svg: Added.
            * svg/animations/animate-dynamic-update-attributeName-expected.txt:
            Nice side effect of the patch, we are now fully passing this test.
    2011-04-26  Dinesh K Garg  <dineshg@codeaurora.org>
    
            Reviewed by Eric Seidel.
    
            beginElement broken by setAttribute
            https://bugs.webkit.org/show_bug.cgi?id=26019
    
            Test: svg/animations/animate-beginElementAt.svg
    
            * svg/SVGAnimationElement.cpp:
            (WebCore::SVGAnimationElement::attributeChanged):
            Reset the animation state here as the rest of the code reads it and would still
            think we are animating when we are not.
    
            * svg/animation/SVGSMILElement.h:
            (WebCore::SVGSMILElement::setInactive): Helper function to reset the state.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@84999 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
@@ -162,6 +162,7 @@ void SVGAnimationElement::attributeChanged(Attribute* attr, bool preserveDecls)
 {
     // Assumptions may not hold after an attribute change.
     m_animationValid = false;
+    setInactive();
     SVGSMILElement::attributeChanged(attr, preserveDecls);
 }
 
