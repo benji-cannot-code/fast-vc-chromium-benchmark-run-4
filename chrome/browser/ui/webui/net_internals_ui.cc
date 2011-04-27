52ed2fef0a10766eef7f8971341b1634fe373dd5
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 23:15:34 +0000

    Report cert-pin only metadata correctly in the chrome://net-internals/#hsts UI. Review URL: http://codereview.chromium.org/6893072
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83249 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals_ui.cc
@@ -1068,7 +1068,7 @@ void NetInternalsMessageHandler::IOThreadImpl::OnHSTSQuery(
       result->SetString("error", "no TransportSecurityState active");
     } else {
       net::TransportSecurityState::DomainState state;
-      const bool found = transport_security_state->IsEnabledForHost(
+      const bool found = transport_security_state->HasMetadata(
           &state, domain, true);
 
       result->SetBoolean("result", found);
