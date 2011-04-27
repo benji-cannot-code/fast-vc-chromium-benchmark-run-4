05cc385d07a73c5518d11dc24b31c820038a840e
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 18:08:20 +0000

    Undo raw DNS resolver changes, this will be implemented differently, on IO-thread.
    BUG=60149
    TEST=None
    
    Review URL: http://codereview.chromium.org/6893007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83177 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl.cc
+++ net/base/host_resolver_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -73,7 +73,6 @@ HostCache* CreateDefaultCache() {
 }  // anonymous namespace
 
 HostResolver* CreateSystemHostResolver(size_t max_concurrent_resolves,
-                                       HostResolverProc* resolver_proc,
                                        NetLog* net_log) {
   // Maximum of 8 concurrent resolver threads.
   // Some routers (or resolvers) appear to start to provide host-not-found if
@@ -85,7 +84,7 @@ HostResolver* CreateSystemHostResolver(size_t max_concurrent_resolves,
     max_concurrent_resolves = kDefaultMaxJobs;
 
   HostResolverImpl* resolver =
-      new HostResolverImpl(resolver_proc, CreateDefaultCache(),
+      new HostResolverImpl(NULL, CreateDefaultCache(),
                            max_concurrent_resolves, net_log);
 
   return resolver;
