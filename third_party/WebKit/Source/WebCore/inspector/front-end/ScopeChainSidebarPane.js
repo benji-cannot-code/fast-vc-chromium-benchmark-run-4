30ec2507d2d28709a5f89c612559e82049990804
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 10:00:47 +0000

    2011-04-27  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: expose exception details when script is paused on exception
            https://bugs.webkit.org/show_bug.cgi?id=59591
    
            When script is paused on an exception, the exception value is added as <exception>
            property in the local scope and the breakpoint details message says
            "Paused on excepion: '<exception to strin value>'."
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/ScopeChainSidebarPane.js:
            (WebInspector.ScopeChainSidebarPane.prototype.update):
            * inspector/front-end/ScriptsPanel.js:
            (WebInspector.ScriptsPanel.prototype._debuggerPaused.else.didGetSourceLocation):
            (WebInspector.ScriptsPanel.prototype._debuggerPaused):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScopeChainSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScopeChainSidebarPane.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2008 Apple Inc. All Rights Reserved.
+ * Copyright (C) 2011 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -74,6 +75,13 @@ WebInspector.ScopeChainSidebarPane.prototype = {
                     subtitle = null;
                     if (scope.this)
                         extraProperties = [ new WebInspector.RemoteObjectProperty("this", WebInspector.RemoteObject.fromPayload(scope.this)) ];
+                    if (i == 0) {
+                        var exception = WebInspector.debuggerModel.debuggerPausedDetails.exception;
+                        if (exception) {
+                            extraProperties = extraProperties || [];
+                            extraProperties.push(new WebInspector.RemoteObjectProperty("<exception>", WebInspector.RemoteObject.fromPayload(exception)));
+                        }
+                    }
                     break;
                 case "closure":
                     title = WebInspector.UIString("Closure");
--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2008 Apple Inc. All Rights Reserved.
+ * Copyright (C) 2011 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -413,6 +414,12 @@ WebInspector.ScriptsPanel.prototype = {
         } else {
             function didGetSourceLocation(sourceFileId, lineNumber)
             {
+                var exception = WebInspector.debuggerModel.debuggerPausedDetails.exception;
+                if (exception) {
+                    this.sidebarPanes.callstack.setStatus(WebInspector.UIString("Paused on exception: '%s'.", exception.description));
+                    return;
+                }
+
                 if (!sourceFileId || !this._presentationModel.findBreakpoint(sourceFileId, lineNumber))
                     return;
                 this.sidebarPanes.jsBreakpoints.highlightBreakpoint(sourceFileId, lineNumber);
