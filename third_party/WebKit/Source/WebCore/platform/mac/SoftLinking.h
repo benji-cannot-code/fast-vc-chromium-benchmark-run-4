a312e8fba59263198b7c2b94e9a1e1425586761a
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 18:52:09 +0000

    Part of WebCore should use CFNetwork-based loader on Mac https://bugs.webkit.org/show_bug.cgi?id=51836
    Reviewed by Antti Koivisto.
    
    * Configurations/WebCore.xcconfig: Add location of CFNetwork.framework.
    * platform/mac/SoftLinking.h:
    * platform/network/cf/CookieJarCFNet.cpp: Soft-link in CFNetwork functions on Mac.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/SoftLinking.h
+++ third_party/WebKit/Source/WebCore/platform/mac/SoftLinking.h
@@ -46,6 +46,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return frameworkLibrary; \
     }
 
+#define SOFT_LINK_FRAMEWORK_IN_CORESERVICES_UMBRELLA(framework) \
+    static void* framework##Library() \
+    { \
+        static void* frameworkLibrary = dlopen("/System/Library/Frameworks/CoreServices.framework/Frameworks/" #framework ".framework/" #framework, RTLD_NOW); \
+        ASSERT(frameworkLibrary); \
+        return frameworkLibrary; \
+    }
+
 #define SOFT_LINK(framework, functionName, resultType, parameterDeclarations, parameterNames) \
     static resultType init##functionName parameterDeclarations; \
     static resultType (*softLink##functionName) parameterDeclarations = init##functionName; \
@@ -60,6 +68,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     inline resultType functionName parameterDeclarations \
     {\
         return softLink##functionName parameterNames; \
+    }
+
+/* callingConvention is unused on Mac but is here to keep the macro prototype the same between Mac and Windows. */
+#define SOFT_LINK_OPTIONAL(framework, functionName, resultType, callingConvention, parameterDeclarations) \
+    typedef resultType (*functionName##PtrType) parameterDeclarations; \
+    \
+    static functionName##PtrType functionName##Ptr() \
+    { \
+        static functionName##PtrType ptr = reinterpret_cast<functionName##PtrType>(dlsym(framework##Library(), #functionName)); \
+        return ptr; \
     }
 
 #define SOFT_LINK_CLASS(framework, className) \
--- third_party/WebKit/Source/WebCore/platform/network/cf/CookieJarCFNet.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/CookieJarCFNet.cpp
@@ -38,18 +38,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SoftLinking.h"
 #include <CFNetwork/CFHTTPCookiesPriv.h>
 #include <CoreFoundation/CoreFoundation.h>
+
+#if PLATFORM(WIN)
 #include <WebKitSystemInterface/WebKitSystemInterface.h>
 #include <windows.h>
+#endif
 
 namespace WebCore {
 
 static const CFStringRef s_setCookieKeyCF = CFSTR("Set-Cookie");
 static const CFStringRef s_cookieCF = CFSTR("Cookie");
 
+#if PLATFORM(WIN)
 #ifdef DEBUG_ALL
 SOFT_LINK_DEBUG_LIBRARY(CFNetwork)
 #else
 SOFT_LINK_LIBRARY(CFNetwork)
+#endif
+#else
+SOFT_LINK_FRAMEWORK_IN_CORESERVICES_UMBRELLA(CFNetwork)
 #endif
 
 SOFT_LINK_OPTIONAL(CFNetwork, CFHTTPCookieCopyDomain, CFStringRef, __cdecl, (CFHTTPCookieRef))
