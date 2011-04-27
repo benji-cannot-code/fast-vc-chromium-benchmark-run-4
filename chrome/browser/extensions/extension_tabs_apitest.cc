c8dca4d9ac9eb0b5719abb5c199be48ac3b3eb07
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 23:07:40 +0000

    Disable ExtensionApiTest.CaptureVisibleTabRace since it still turns the bot red, even when marked as flaky.
    BUG=80212
    TEST=ExtensionApiTest.CaptureVisibleTabRace
    TBR=acolwell@chromium.org
    Review URL: http://codereview.chromium.org/6880258
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83245 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Times out on Mac (especially Leopard). http://crbug.com/80212
 #if defined(OS_MACOSX)
-#define MAYBE_CaptureVisibleTabRace FLAKY_CaptureVisibleTabRace
+#define MAYBE_CaptureVisibleTabRace DISABLED_CaptureVisibleTabRace
 #else
 #define MAYBE_CaptureVisibleTabRace CaptureVisibleTabRace
 #endif
