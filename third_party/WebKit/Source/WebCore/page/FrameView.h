153a81c4b9e9ff06e52fff33338143d80f73c199
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 23:01:48 +0000

    2011-04-27  James Robinson  <jamesr@chromium.org>
            Unreviewed, rolling out r85112.
            http://trac.webkit.org/changeset/85112
            https://bugs.webkit.org/show_bug.cgi?id=59159
    
            Broke mac compile
    
            * page/FrameView.cpp:
            (WebCore::FrameView::updateCompositingLayers):
            * page/FrameView.h:
            * platform/graphics/chromium/GraphicsLayerChromium.cpp:
            (WebCore::GraphicsLayerChromium::updateLayerPreserves3D):
            * platform/graphics/chromium/LayerChromium.cpp:
            (WebCore::LayerChromium::LayerChromium):
            (WebCore::LayerChromium::~LayerChromium):
            * platform/graphics/chromium/LayerRendererChromium.cpp:
            (WebCore::LayerRendererChromium::updateLayers):
            * platform/graphics/chromium/LayerRendererChromium.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85116 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.h
+++ third_party/WebKit/Source/WebCore/page/FrameView.h
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 #include <wtf/OwnPtr.h>
 
-#if USE(ACCELERATED_COMPOSITING)
-#include "RenderLayerCompositor.h" // For CompositingUpdateType
-#endif
-
 namespace WebCore {
 
 class Color;
@@ -110,7 +106,7 @@ public:
 #endif
 
 #if USE(ACCELERATED_COMPOSITING)
-    void updateCompositingLayers(CompositingUpdateType = CompositingUpdateAfterLayoutOrStyleChange);
+    void updateCompositingLayers();
     bool syncCompositingStateForThisFrame();
 
     // Called when changes to the GraphicsLayer hierarchy have to be synchronized with
