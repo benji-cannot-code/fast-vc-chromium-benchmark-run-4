1db839e3b9643b459fea345d2d5262ea6b7fb54a
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-27 20:01:56 +0000

    Attempt to fix PrerenderBrowserTest.PrerenderExcessiveMemory by doing three things:
    1)  Allocating only as much memory as is needed to fail the
    test, to reduce risk of heavy paging or JSOutOfMemory errors.
    2)  Removing copies of large amounts of data.  This speeds up
    the test significantly in debug builds, at least.
    3)  Reduce the maximum amount of memory per PrerenderContents
    during the test.
    
    BUG=77870
    TEST=PrerenderBrowserTest.PrerenderExcessiveMemory
    
    Review URL: http://codereview.chromium.org/6883162
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -32,6 +32,10 @@ namespace {
 // Default maximum permitted elements to prerender.
 const unsigned int kDefaultMaxPrerenderElements = 1;
 
+// Default maximum amount of private memory that may be used per
+// PrerenderContents, in MB.
+const unsigned int kDefaultMaxPrerenderMemoryMB = 100;
+
 // Default maximum age a prerendered element may have, in seconds.
 const int kDefaultMaxPrerenderAgeSeconds = 30;
 
@@ -150,6 +154,7 @@ PrerenderManager::PrerenderManager(Profile* profile)
       profile_(profile),
       max_prerender_age_(base::TimeDelta::FromSeconds(
           kDefaultMaxPrerenderAgeSeconds)),
+      max_prerender_memory_mb_(kDefaultMaxPrerenderMemoryMB),
       max_elements_(kDefaultMaxPrerenderElements),
       prerender_contents_factory_(PrerenderContents::CreateFactory()),
       last_prerender_start_time_(GetCurrentTimeTicks() -
@@ -508,6 +513,16 @@ base::TimeDelta PrerenderManager::max_prerender_age() const {
 void PrerenderManager::set_max_prerender_age(base::TimeDelta max_age) {
   DCHECK(CalledOnValidThread());
   max_prerender_age_ = max_age;
+}
+
+size_t PrerenderManager::max_prerender_memory_mb() const {
+  DCHECK(CalledOnValidThread());
+  return max_prerender_memory_mb_;
+}
+
+void PrerenderManager::set_max_prerender_memory_mb(size_t max_memory_mb) {
+  DCHECK(CalledOnValidThread());
+  max_prerender_memory_mb_ = max_memory_mb;
 }
 
 unsigned int PrerenderManager::max_elements() const {
