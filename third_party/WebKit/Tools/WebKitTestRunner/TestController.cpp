0bff17d15c318f9c5fe9077f75a45d0efa55d4dc
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-27 22:57:08 +0000

    Build fix.
    Fix an OwnPtr-related issue that was missed before the strictness was turned on.
    
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::runTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -425,7 +425,7 @@ bool TestController::runTest(const char* test)
 
     m_state = RunningTest;
 
-    m_currentInvocation.set(new TestInvocation(pathOrURL));
+    m_currentInvocation = adoptPtr(new TestInvocation(pathOrURL));
     if (m_dumpPixels)
         m_currentInvocation->setIsPixelTest(expectedPixelHash);    
 
