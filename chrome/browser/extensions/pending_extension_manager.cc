e127c685607340a192e143095a020de88f0cedcd
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 21:50:09 +0000

    [Extensions] Remove dead code in PendingExtensionManager
    AddFromDefaultAppList hasn't been used since 81750.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6893106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83414 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/pending_extension_manager.cc
+++ chrome/browser/extensions/pending_extension_manager.cc
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Install predicate used by AddFromDefaultAppList().
-bool IsApp(const Extension& extension) {
-  return extension.is_app();
-}
-
 // Install predicate used by AddFromExternalUpdateUrl().
 bool AlwaysInstall(const Extension& extension) {
   return true;
@@ -102,28 +97,6 @@ void PendingExtensionManager::AddFromExternalUpdateUrl(
                    location);
 }
 
-
-// TODO(akalin): Change DefaultAppList to DefaultExtensionList and
-// remove the IsApp() check.
-void PendingExtensionManager::AddFromDefaultAppList(
-    const std::string& id) {
-  CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-
-  const bool kIsFromSync = false;
-  const bool kInstallSilently = true;
-  const bool kEnableOnInstall = true;
-  const bool kEnableIncognitoOnInstall = true;
-
-  // This can legitimately happen if the user manually installed one of the
-  // default apps before this code ran.
-  if (service_.GetExtensionById(id, true))
-    return;
-
-  AddExtensionImpl(id, GURL(), &IsApp,
-                   kIsFromSync, kInstallSilently,
-                   kEnableOnInstall, kEnableIncognitoOnInstall,
-                   Extension::INTERNAL);
-}
 
 void PendingExtensionManager::AddFromExternalFile(
     const std::string& id,
