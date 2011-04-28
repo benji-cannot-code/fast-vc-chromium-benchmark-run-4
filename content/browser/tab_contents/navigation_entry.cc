dce5250cae7e0957bb54e51571ea6adf12ff6c0e
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 20:42:37 +0000

    Remove a unnecessary #include.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6901066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83394 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/navigation_entry.cc
+++ content/browser/tab_contents/navigation_entry.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/site_instance.h"
-#include "content/browser/tab_contents/navigation_controller.h"
 #include "content/common/content_constants.h"
 #include "grit/app_resources.h"
 #include "net/base/net_util.h"
