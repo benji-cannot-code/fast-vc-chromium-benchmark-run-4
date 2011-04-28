39c52a0ac0e8b5679a54b28959659924b50e5d68
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 18:13:21 +0000

    Build fix.
    * css/CSSParser.cpp:
    (WebCore::cssPropertyID):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -6609,6 +6609,7 @@ static int cssPropertyID(const UChar* propertyName, unsigned length)
             const char* const webkitLocale = "-webkit-locale";
             name = webkitLocale;
             length = strlen(webkitLocale);
+        }
 #endif
     }
 
