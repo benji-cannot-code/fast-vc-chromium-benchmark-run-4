8ef34ff031ad8855e36d198abd4bb7c5c45944a7
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 22:29:02 +0000

    Sync: Fix several default focus issues in the setup overlay.
    BUG=80531
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/6883248
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83427 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/sync_setup_overlay.js
+++ chrome/browser/resources/options/sync_setup_overlay.js
@@ -358,6 +358,8 @@ cr.define('options', function() {
         self.setCheckboxesToKeepEverythingSynced_(syncAll);
       };
 
+      $('sync-setup-configure').classList.remove('hidden');
+
       if (args) {
         this.setCheckboxesAndErrors_(args);
 
@@ -372,8 +374,6 @@ cr.define('options', function() {
           this.showCustomizePage_(syncAllDataTypes);
         }
       }
-
-      $('sync-setup-configure').classList.remove('hidden');
     },
 
     showSyncEverythingPage_: function() {
@@ -386,6 +386,7 @@ cr.define('options', function() {
       // The default state is to sync everything; the passphrase option is
       // unchanged.
       this.setCheckboxesToKeepEverythingSynced_(true);
+      $('confirm-everything-ok').focus();
     },
 
     showCustomizePage_: function(syncEverything) {
@@ -398,6 +399,7 @@ cr.define('options', function() {
       var index = syncEverything ? 0 : 1;
       document.getElementById('sync-select-datatypes').selectedIndex = index;
       this.setDataTypeCheckboxesEnabled_(!syncEverything);
+      $('choose-datatypes-ok').focus();
     },
 
     showSyncSetupPage_: function(page, args) {
@@ -434,14 +436,18 @@ cr.define('options', function() {
       if (args["passphrase_setting_rejected"]) {
         $('incorrectPassphrase').style.display = "block";
       }
+
+      $('passphrase-ok').focus();
     },
 
     showSettingUp_: function() {
       $('sync-setup-setting-up').classList.remove('hidden');
+      $('setting-up-cancel').focus();
     },
 
     showSetupDone_: function() {
       $('sync-setup-done').classList.remove('hidden');
+      $('setup-done-close').focus();
     },
 
     setElementDisplay_: function(id, display) {
@@ -463,8 +469,7 @@ cr.define('options', function() {
     showAccessCodeRequired_: function() {
       this.setElementDisplay_("password-row", "none");
       this.setElementDisplay_("email-row", "none");
-      $('create-account-cell').style.visibility =
-          "hidden";
+      $('create-account-cell').style.visibility = "hidden";
 
       this.setElementDisplay_("access-code-label-row", "table-row");
       this.setElementDisplay_("access-code-input-row", "table-row");
@@ -491,8 +496,9 @@ cr.define('options', function() {
     },
 
     showGaiaLogin_: function(args) {
-      document.getElementById('logging-in-throbber').style.display = "none";
+      $('sync-setup-login').classList.remove('hidden');
 
+      document.getElementById('logging-in-throbber').style.display = "none";
       document.getElementById('gaia-email').disabled = false;
       document.getElementById('gaia-passwd').disabled = false;
 
@@ -540,8 +546,6 @@ cr.define('options', function() {
       $('sign-in').disabled = false;
       $('sign-in').value = templateData['signin'];
       this.loginSetFocus_();
-
-      $('sync-setup-login').classList.remove('hidden');
     },
 
     resetErrorVisibility_: function() {
