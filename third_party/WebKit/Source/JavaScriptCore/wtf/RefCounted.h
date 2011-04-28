8c5d0457970efd7897550217c52cf4d99e2d63a0
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 20:54:23 +0000

    Build fix.
    * wtf/RefCounted.h: Fix inverted ifdef.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/RefCounted.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/RefCounted.h
@@ -204,7 +204,7 @@ protected:
     }
 };
 
-#ifndef NDEBUG
+#ifdef NDEBUG
 inline void RefCountedBase::setMutexForVerifier(Mutex&) { }
 #else
 inline void RefCountedBase::setMutexForVerifier(Mutex& mutex)
