32128400598ac64681344a031eaeaea7482f0438
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 23:36:28 +0000

    https://bugs.webkit.org/show_bug.cgi?id=59759 Overflow:scroll regions with overlay scrollbars show full thumb instead of just a track when there is nothing to scroll to <rdar://problem/9282066>
    Reviewed by Darin Adler.
    
    Scrollbar::totalSize() and Scrollbar::visibleSize() are the same when there is 
    nothing to scroll to. Just dividing them to get a knob proportion yields 1, which 
    is not right. Set the proportion to 0 in this case instead.
    * platform/mac/ScrollbarThemeMac.mm:
    (WebCore::ScrollbarThemeMac::paint):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85258 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.mm
@@ -446,6 +446,9 @@ static inline wkScrollerKnobStyle toScrollbarPainterKnobStyle(ScrollbarOverlaySt
 
 bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, const IntRect& damageRect)
 {
+    int visibleSize = scrollbar->visibleSize();
+    int totalSize = scrollbar->totalSize();
+
 #if USE(WK_SCROLLBAR_PAINTER)
     float value = 0;
     float overhang = 0;
@@ -454,10 +457,10 @@ bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, co
         // Scrolled past the top.
         value = 0;
         overhang = -scrollbar->currentPos();
-    } else if (scrollbar->visibleSize() + scrollbar->currentPos() > scrollbar->totalSize()) {
+    } else if (visibleSize + scrollbar->currentPos() > totalSize) {
         // Scrolled past the bottom.
         value = 1;
-        overhang = scrollbar->currentPos() + scrollbar->visibleSize() - scrollbar->totalSize();
+        overhang = scrollbar->currentPos() + visibleSize - totalSize;
     } else {
         // Within the bounds of the scrollable area.
         int maximum = scrollbar->maximum();
@@ -466,6 +469,8 @@ bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, co
         else
             value = 0;
     }
+
+    float proportion = visibleSize < totalSize ? (visibleSize - overhang) / totalSize : 0;
     
     ScrollAnimatorMac* scrollAnimator = static_cast<ScrollAnimatorMac*>(scrollbar->scrollableArea()->scrollAnimator());
     scrollAnimator->setIsDrawingIntoLayer(context->isCALayerContext());
@@ -481,7 +486,7 @@ bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, co
     wkScrollbarPainterPaint(scrollbarMap()->get(scrollbar).get(),
                             scrollbar->enabled(),
                             value,
-                            (static_cast<CGFloat>(scrollbar->visibleSize()) - overhang) / scrollbar->totalSize(),
+                            proportion,
                             scrollbar->frameRect());
 
     scrollAnimator->setIsDrawingIntoLayer(false);
@@ -497,9 +502,9 @@ bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, co
     float position = 0.0f;
     if (scrollbar->currentPos() < 0) {
         // Scrolled past the top.
-        maximum = (scrollbar->totalSize() - scrollbar->currentPos()) - scrollbar->visibleSize();
+        maximum = (totalSize - scrollbar->currentPos()) - visibleSize;
         position = 0;
-    } else if (scrollbar->visibleSize() + scrollbar->currentPos() > scrollbar->totalSize()) {
+    } else if (visibleSize + scrollbar->currentPos() > totalSize) {
         // Scrolled past the bottom.
         maximum = scrollbar->currentPos();
         position = maximum;
@@ -513,7 +518,7 @@ bool ScrollbarThemeMac::paint(Scrollbar* scrollbar, GraphicsContext* context, co
     trackInfo.max = static_cast<int>(maximum);
     trackInfo.value = static_cast<int>(position);
 
-    trackInfo.trackInfo.scrollbar.viewsize = scrollbar->visibleSize();
+    trackInfo.trackInfo.scrollbar.viewsize = visibleSize;
     trackInfo.attributes = 0;
     if (scrollbar->orientation() == HorizontalScrollbar)
         trackInfo.attributes |= kThemeTrackHorizontal;
