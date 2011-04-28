84b1cb01a2bba07ebd2eed8d585e6750b01fa82f
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 21:27:17 +0000

    Clean up ExtensionService shutdown and related notifications.
    It is not necessary to unload extensions during destruction because that only happens when the profile is going away, so the unloading would be wasted effort.
    
    With that gone, you can also get rid of DestroyingProfile(), which simplifies things.
    
    Also moved all hooking of outside components that happens during extension load and unload into a central location.
    
    Review URL: http://codereview.chromium.org/6902059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_toolbar_model.cc
+++ chrome/browser/extensions/extension_toolbar_model.cc
@@ -35,10 +35,6 @@ ExtensionToolbarModel::ExtensionToolbarModel(ExtensionService* service)
 ExtensionToolbarModel::~ExtensionToolbarModel() {
 }
 
-void ExtensionToolbarModel::DestroyingProfile() {
-  registrar_.RemoveAll();
-}
-
 void ExtensionToolbarModel::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
 }
