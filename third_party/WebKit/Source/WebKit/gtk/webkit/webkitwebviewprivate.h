1864390c735dc241762afd568f5490b21b14c7cc
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 19:10:14 +0000

    2011-04-28  ojab  <ojab@ojab.ru>
            Reviewed by Martin Robinson.
    
            Webkit-gtk build failure, no type/member 'Page' in namespace 'WebCore'
            https://bugs.webkit.org/show_bug.cgi?id=59585
    
            * webkit/webkitwebviewprivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebviewprivate.h
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebviewprivate.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FullscreenVideoController.h"
 #include "GtkClickCounter.h"
 #include "GOwnPtr.h"
+#include "Page.h"
 #include "ResourceHandle.h"
 #include <webkit/webkitwebview.h>
 
