8909a90520419d194c1cf8f0176d381246c3731b
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 18:50:26 +0000

    Reverting revision 83279, which caused the Print Preview tab to crash. TBR=dpapad@chromium.org
    BUG=Print preview tab was all grey after trying to print.
    TEST=Printing should work again.
    
    
    Review URL: http://codereview.chromium.org/6903106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83368 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -27,12 +27,6 @@ var previouslySelectedLayout = null;
 // needed.
 var timerId;
 
-// Indicates whether a preview has been requested but not received yet.
-var isPreviewStillLoading = false;
-
-// Currently selected printer capabilities.
-var printerCapabilities;
-
 // Store the last selected printer index.
 var lastSelectedPrinterIndex = 0;
 
@@ -90,7 +84,7 @@ function disablePreviewControls() {
                     'individual-pages'];
   var controlCount = controlIDs.length;
   for (var i = 0; i < controlCount; i++)
-    controlIDs[i].disabled = true;
+    setControlAndLabelDisabled($(controlIDs[i]), true);
 }
 
 /**
@@ -124,19 +118,14 @@ function updateControlsWithSelectedPrinterCapabilities() {
  * @param {Object} settingInfo printer setting information.
  */
 function updateWithPrinterCapabilities(settingInfo) {
-  printerCapabilities = settingInfo;
-
-  if (isPreviewStillLoading)
-    return;
-
   var disableColorOption = settingInfo.disableColorOption;
   var setColorAsDefault = settingInfo.setColorAsDefault;
   var colorOption = $('color');
   var bwOption = $('bw');
 
   if (disableColorOption != colorOption.disabled) {
-    colorOption.disabled = disableColorOption;
-    bwOption.disabled = disableColorOption;
+    setControlAndLabelDisabled(colorOption, disableColorOption);
+    setControlAndLabelDisabled(bwOption, disableColorOption);
   }
 
   if (colorOption.checked != setColorAsDefault) {
@@ -147,16 +136,20 @@ function updateWithPrinterCapabilities(settingInfo) {
 }
 
 /**
- * Disables or enables all controls in the options pane except from the cancel
- * button.
+ * Disables the input control element and its associated label.
+ * @param {HTMLElement} controlElm An input control element.
+ * @param {boolean} disable set to true to disable element and label.
  */
-function setControlsDisabled(disabled) {
-  var elementList = $('controls').elements;
-  for (var i = 0; i < elementList.length; ++i) {
-    if (elementList[i] == $('cancel-button'))
-      continue;
-    elementList[i].disabled =  disabled;
-  }
+function setControlAndLabelDisabled(controlElm, disable) {
+  controlElm.disabled = disable;
+  var label = $(controlElm.getAttribute('label'));
+  if (label == undefined)
+    return;
+
+  if (disable)
+    label.classList.add('disabled-label-text');
+  else
+    label.classList.remove('disabled-label-text');
 }
 
 /**
@@ -332,8 +325,7 @@ function printFile() {
  * Asks the browser to generate a preview PDF based on current print settings.
  */
 function requestPrintPreview() {
-  isPreviewStillLoading = true;
-  setControlsDisabled(true);
+  $('dancing-dots').classList.remove('hidden');
   $('dancing-dots').classList.remove('invisible');
   chrome.send('getPreview', [getSettingsJSON()]);
 }
@@ -399,7 +391,6 @@ function setColor(color) {
 function printPreviewFailed() {
   $('dancing-dots').classList.add('hidden');
   $('preview-failed').classList.remove('hidden');
-  setControlsDisabled(true);
 
   var pdfViewer = $('pdf-viewer');
   if (pdfViewer)
@@ -416,8 +407,6 @@ function onPDFLoad() {
     $('pdf-viewer').fitToHeight();
 
   $('dancing-dots').classList.add('invisible');
-  setControlsDisabled(false);
-  updateWithPrinterCapabilities(printerCapabilities);
 }
 
 /**
@@ -445,9 +434,8 @@ function updatePrintPreview(pageCount, jobTitle) {
   document.title = localStrings.getStringF('printPreviewTitleFormat', jobTitle);
 
   createPDFPlugin();
-  isPreviewStillLoading = false;
-  updatePrintSummary();
 
+  updatePrintSummary();
 }
 
 /**
