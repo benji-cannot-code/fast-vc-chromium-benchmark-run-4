3d5c977d3fd44c6e7c766ca36b0da137bb1a249b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 06:56:36 +0000

    2011-04-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Maciej Stachowiak.
    
            Fix OwnPtr issues in IndexedDB
            https://bugs.webkit.org/show_bug.cgi?id=59656
    
            This patch is an attempt to fix the clang build.  Clang can't seem to
            cope with OwnPtrs to classes declared in an anonymous namespace because
            OwnPtr's copy constructor isn't defined (but would need to be defined
            in this translation unit).
    
            * platform/leveldb/LevelDBDatabase.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBDatabase.cpp
+++ third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBDatabase.cpp
@@ -64,7 +64,6 @@ static Vector<char> makeVector(const std::string& s)
     return res;
 }
 
-namespace {
 class ComparatorAdapter : public leveldb::Comparator {
 public:
     ComparatorAdapter(const LevelDBComparator* comparator)
@@ -86,7 +85,6 @@ public:
 private:
     const LevelDBComparator* m_comparator;
 };
-}
 
 LevelDBDatabase::LevelDBDatabase()
 {
