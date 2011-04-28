cd4e126e63791f49ca2acb2017b6155ab44067b2
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-28 22:52:04 +0000

    Only grab the full-screen image once, not per damage rect.
    BUG=None
    TEST=Dragging a large window around isn't glacially slow.
    (ensure "xdpyinfo -ext MIT-SHM" shows SHM enabled, with no pixmap support)
    
    Review URL: http://codereview.chromium.org/6905110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83434 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/capturer_linux.cc
+++ remoting/host/capturer_linux.cc
@@ -289,6 +289,7 @@ void CapturerLinux::CaptureRects(
     }
   }
 
+  x_server_pixel_buffer_.Synchronize();
   for (InvalidRects::const_iterator it = rects.begin();
        it != rects.end();
        ++it) {
