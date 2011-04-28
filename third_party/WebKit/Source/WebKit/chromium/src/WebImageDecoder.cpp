a33279e8a15dbe676ed786c1d026d44c194dde12
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-28 01:42:20 +0000

    2011-04-27  James Robinson  <jamesr@chromium.org>
            Reviewed by Adam Barth.
    
            Fix OwnPtr strict issues in chromium linux build
            https://bugs.webkit.org/show_bug.cgi?id=59664
    
            * platform/graphics/chromium/ComplexTextControllerLinux.cpp:
            (WebCore::ComplexTextController::getNormalizedTextRun):
            * platform/graphics/skia/ImageBufferSkia.cpp:
            (WebCore::ImageBuffer::ImageBuffer):
            * platform/graphics/skia/PlatformContextSkia.cpp:
            (WebCore::PlatformContextSkia::PlatformContextSkia):
    2011-04-27  James Robinson  <jamesr@chromium.org>
    
            Reviewed by Adam Barth.
    
            Fix OwnPtr strict issues in chromium linux build
            https://bugs.webkit.org/show_bug.cgi?id=59664
    
            * src/WebImageDecoder.cpp:
            (WebKit::WebImageDecoder::getFrameAtIndex):
            * src/WebImageSkia.cpp:
            (WebKit::WebImage::fromData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebImageDecoder.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebImageDecoder.cpp
@@ -41,8 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if WEBKIT_USING_SKIA
 #include <wtf/OwnPtr.h>
-#include <wtf/PassRefPtr.h>
+#include <wtf/PassOwnPtr.h>
 #endif
+
+#include <wtf/PassRefPtr.h>
 
 using namespace WebCore;
 
@@ -111,7 +113,7 @@ WebImage WebImageDecoder::getFrameAtIndex(int index = 0) const
     if (!frameBuffer)
         return WebImage();
 #if WEBKIT_USING_SKIA
-    OwnPtr<NativeImageSkia>image(frameBuffer->asNewNativeImage());
+    OwnPtr<NativeImageSkia> image = adoptPtr(frameBuffer->asNewNativeImage());
     return WebImage(*image);
 #elif WEBKIT_USING_CG
     // FIXME: Implement CG side of this.
--- third_party/WebKit/Source/WebKit/chromium/src/WebImageSkia.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebImageSkia.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebSize.h"
 
 #include <wtf/OwnPtr.h>
+#include <wtf/PassOwnPtr.h>
 #include <wtf/PassRefPtr.h>
 
 using namespace WebCore;
@@ -77,8 +78,8 @@ WebImage WebImage::fromData(const WebData& data, const WebSize& desiredSize)
         }
     }
 
-    OwnPtr<NativeImageSkia> frame(source.createFrameAtIndex(index));
-    if (!frame.get())
+    OwnPtr<NativeImageSkia> frame = adoptPtr(source.createFrameAtIndex(index));
+    if (!frame)
         return WebImage();
 
     return WebImage(*frame);
