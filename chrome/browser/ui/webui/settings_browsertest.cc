17e1c320f522c2688dd538c85f0a3e7707507f05
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 01:10:54 +0000

    Fix disabling to test on OSX.
    TBR=dtseng@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/settings_browsertest.cc
+++ chrome/browser/ui/webui/settings_browsertest.cc
@@ -111,7 +111,7 @@ IN_PROC_BROWSER_TEST_F(SettingsWebUITest, MAYBE_TestSetBooleanPrefTriggers) {
 }
 
 // Not meant to run on ChromeOS at this time.
-#if defined(OS_CHROMEOS) || OS_MACOSX
+#if defined(OS_CHROMEOS) || defined(OS_MACOSX)
 #define MAYBE_TestRefreshStaysOnCurrentPage\
     DISABLED_TestRefreshStaysOnCurrentPage
 #else
