952a9ae44d73e1f6c11f7fc526c181072f2eecdf
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 20:15:49 +0000

    Include information about PSS in the logs when a test case fails setting up sync..
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6902143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83579 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_harness.cc
+++ chrome/browser/sync/profile_sync_service_harness.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
+#include "base/json/json_writer.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/message_loop.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sessions/session_state.h"
 #include "chrome/browser/sync/signin_manager.h"
+#include "chrome/browser/sync/sync_ui_util.h"
 
 using browser_sync::sessions::SyncSessionSnapshot;
 
@@ -133,7 +135,12 @@ bool ProfileSyncServiceHarness::SetupSync() {
     synced_datatypes.insert(syncable::ModelTypeFromInt(i));
   }
   bool result = SetupSync(synced_datatypes);
-  VLOG(0) << "Client " << id_ << " PSH: Set up sync completed";
+  VLOG(0) << "Client " << id_ << ": Set up sync completed with result "
+          << result;
+  if (result == false) {
+    std::string pss_status = GetServiceStatus();
+    VLOG(0) << pss_status;
+  }
   return result;
 }
 
@@ -675,4 +682,12 @@ bool ProfileSyncServiceHarness::IsTypeEncrypted(syncable::ModelType type) {
     return false;
   }
   return true;
+}
+
+std::string ProfileSyncServiceHarness::GetServiceStatus() {
+  DictionaryValue value;
+  sync_ui_util::ConstructAboutInformation(service_, &value);
+  std::string service_status;
+  base::JSONWriter::Write(&value, true, &service_status);
+  return service_status;
 }
