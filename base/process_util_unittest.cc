50378f9ad160cdef1ea19a2ddc5a718253a3ad53
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 01:22:31 +0000

    Fix one more time the linux tests, in other words giving up on improving the tests TBR=brettw
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83469 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_unittest.cc
+++ base/process_util_unittest.cc
@@ -261,13 +261,15 @@ TEST_F(ProcessUtilTest, SetProcessBackgrounded) {
   base::ProcessHandle handle = this->SpawnChild("SimpleChildProcess", false);
   base::Process process(handle);
   int old_priority = process.GetPriority();
-  if (process.SetProcessBackgrounded(true)) {
 #if defined(OS_WIN)
-    EXPECT_TRUE(process.IsProcessBackgrounded());
+  EXPECT_TRUE(process.SetProcessBackgrounded(true));
+  EXPECT_TRUE(process.IsProcessBackgrounded());
+  EXPECT_TRUE(process.SetProcessBackgrounded(false));
+  EXPECT_FALSE(process.IsProcessBackgrounded());
+#else
+  process.SetProcessBackgrounded(true);
+  process.SetProcessBackgrounded(false);
 #endif
-    EXPECT_TRUE(process.SetProcessBackgrounded(false));
-    EXPECT_FALSE(process.IsProcessBackgrounded());
-  }
   int new_priority = process.GetPriority();
   EXPECT_EQ(old_priority, new_priority);
 }
@@ -277,13 +279,15 @@ TEST_F(ProcessUtilTest, SetProcessBackgrounded) {
 TEST_F(ProcessUtilTest, SetProcessBackgroundedSelf) {
   base::Process process(base::Process::Current().handle());
   int old_priority = process.GetPriority();
-  if (process.SetProcessBackgrounded(true)) {
 #if defined(OS_WIN)
-    EXPECT_TRUE(process.IsProcessBackgrounded());
+  EXPECT_TRUE(process.SetProcessBackgrounded(true));
+  EXPECT_TRUE(process.IsProcessBackgrounded());
+  EXPECT_TRUE(process.SetProcessBackgrounded(false));
+  EXPECT_FALSE(process.IsProcessBackgrounded());
+#else
+  process.SetProcessBackgrounded(true);
+  process.SetProcessBackgrounded(false);
 #endif
-    EXPECT_TRUE(process.SetProcessBackgrounded(false));
-    EXPECT_FALSE(process.IsProcessBackgrounded());
-  }
   int new_priority = process.GetPriority();
   EXPECT_EQ(old_priority, new_priority);
 }
