3124e44984715db3e000115c73951c91bf776e0b
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-29 00:03:50 +0000

    Revert "<http://webkit.org/b/59705> WTF::postTimer() leaks a CFRunLoopTimerRef every time it's called"
    This reverts commit r85195.  It was crashing DumpRenderTree on Lion.
    
    * wtf/mac/MainThreadMac.mm:
    (WTF::postTimer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/mac/MainThreadMac.mm
+++ third_party/WebKit/Source/JavaScriptCore/wtf/mac/MainThreadMac.mm
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/NSThread.h>
 #import <stdio.h>
 #import <wtf/Assertions.h>
-#import <wtf/RetainPtr.h>
 #import <wtf/Threading.h>
 
 @interface WTFMainThreadCaller : NSObject {
@@ -97,8 +96,7 @@ static void postTimer()
         return;
 
     isTimerPosted = true;
-    RetainPtr<CFRunLoopTimerRef> runLoopTimer(AdoptCF, CFRunLoopTimerCreate(0, 0, 0, 0, 0, timerFired, 0));
-    CFRunLoopAddTimer(CFRunLoopGetCurrent(), runLoopTimer.get(), kCFRunLoopCommonModes);
+    CFRunLoopAddTimer(CFRunLoopGetCurrent(), CFRunLoopTimerCreate(0, 0, 0, 0, 0, timerFired, 0), kCFRunLoopCommonModes);
 }
 
 void scheduleDispatchFunctionsOnMainThread()
