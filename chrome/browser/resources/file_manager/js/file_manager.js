e18969168a82bdfb548c2b32fa3116a7e8d0c65a
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 23:32:22 +0000

    file manager: respect back/fwd buttons
    BUG=chromium-os:14666
    TEST=manual testing on chromeos-chrome on linux
    
    Review URL: http://codereview.chromium.org/6905143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -53,6 +53,7 @@ function FileManager(dialogDom, rootEntries, params) {
   // DirectoryEntry representing the current directory of the dialog.
   this.currentDirEntry_ = null;
 
+  window.addEventListener('popstate', this.onPopState_.bind(this));
   this.addEventListener('directory-changed',
                         this.onDirectoryChanged_.bind(this));
   this.addEventListener('selection-summarized',
@@ -560,6 +561,16 @@ FileManager.prototype = {
         'change', this.onDetailSelectionChanged_.bind(this));
   };
 
+  /**
+   * Respond to the back button.
+   */
+  FileManager.prototype.onPopState_ = function(event) {
+    this.changeDirectory(event.state, false);
+  };
+
+  /**
+   * Resize details and thumb views to fit the new window size.
+   */
   FileManager.prototype.onResize_ = function() {
     this.table_.style.height = this.grid_.style.height =
       this.grid_.parentNode.clientHeight + 'px';
@@ -1155,9 +1166,17 @@ FileManager.prototype = {
    * changed.
    *
    * @param {string} path The absolute path to the new directory.
+   * @param {bool} opt_saveHistory Save this in the history stack (defaults
+   *     to true).
    */
-  FileManager.prototype.changeDirectory = function(path) {
+  FileManager.prototype.changeDirectory = function(path, opt_saveHistory) {
     var self = this;
+
+    if (arguments.length == 1) {
+      opt_saveHistory = true;
+    } else {
+      opt_saveHistory = !!opt_saveHistory;
+    }
 
     function onPathFound(dirEntry) {
       if (self.currentDirEntry_ &&
@@ -1169,6 +1188,7 @@ FileManager.prototype = {
       var e = new cr.Event('directory-changed');
       e.previousDirEntry = self.currentDirEntry_;
       e.newDirEntry = dirEntry;
+      e.saveHistory = opt_saveHistory;
       self.currentDirEntry_ = dirEntry;
       self.dispatchEvent(e);
     };
@@ -1310,6 +1330,12 @@ FileManager.prototype = {
    * @param {cr.Event} event The directory-changed event.
    */
   FileManager.prototype.onDirectoryChanged_ = function(event) {
+    if (event.saveHistory) {
+      history.pushState(this.currentDirEntry_.fullPath,
+                        this.currentDirEntry_.fullPath,
+                        location.href);
+    }
+    this.document_.title = this.currentDirEntry_.fullPath;
     this.rescanDirectory_();
   };
 
