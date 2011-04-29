7664b3d75cfbaa5482a2e9203918c0fdfcbc8ed9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 14:05:08 +0000

    gtk: Get rid of GtkThemeService::UseGtkTheme() function.
    (Note: This was a TODO for akalin).
    
    BUG=None
    TEST=None
    
    R=akalin@chromium.org,evan@chromium.org
    
    Review URL: http://codereview.chromium.org/6883258
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83510 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/gtk_theme_service.cc
+++ chrome/browser/ui/gtk/gtk_theme_service.cc
@@ -394,10 +394,6 @@ GtkWidget* GtkThemeService::CreateToolbarSeparator() {
   return alignment;
 }
 
-bool GtkThemeService::UseGtkTheme() const {
-  return UsingNativeTheme();
-}
-
 GdkColor GtkThemeService::GetGdkColor(int id) const {
   return gfx::SkColorToGdkColor(GetColor(id));
 }
@@ -1105,7 +1101,7 @@ void GtkThemeService::OnDestroyChromeButton(GtkWidget* button) {
 
 gboolean GtkThemeService::OnSeparatorExpose(GtkWidget* widget,
                                             GdkEventExpose* event) {
-  if (UseGtkTheme())
+  if (UsingNativeTheme())
     return FALSE;
 
   cairo_t* cr = gdk_cairo_create(GDK_DRAWABLE(widget->window));
