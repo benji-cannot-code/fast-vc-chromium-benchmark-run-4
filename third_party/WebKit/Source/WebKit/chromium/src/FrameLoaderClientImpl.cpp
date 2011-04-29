fd2e4eca10e89e9114052536c7ca3a90c56dd722
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-04-29 02:19:58 +0000

    2011-04-28  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by Tony Chang.
    
            [chromium] Fix silly bug with WebPermissionClient
            https://bugs.webkit.org/show_bug.cgi?id=59769
    
            * src/FrameLoaderClientImpl.cpp:
            (WebKit::FrameLoaderClientImpl::allowJavaScript):
            (WebKit::FrameLoaderClientImpl::allowPlugins):
            (WebKit::FrameLoaderClientImpl::allowImages):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -184,7 +184,7 @@ bool FrameLoaderClientImpl::allowJavaScript(bool enabledPerSettings)
 {
     WebViewImpl* webview = m_webFrame->viewImpl();
     if (webview && webview->permissionClient())
-        webview->permissionClient()->allowScript(m_webFrame, enabledPerSettings);
+        return webview->permissionClient()->allowScript(m_webFrame, enabledPerSettings);
 
     // FIXME(jam): remove this.
     if (m_webFrame->client())
@@ -197,7 +197,7 @@ bool FrameLoaderClientImpl::allowPlugins(bool enabledPerSettings)
 {
     WebViewImpl* webview = m_webFrame->viewImpl();
     if (webview && webview->permissionClient())
-        webview->permissionClient()->allowPlugins(m_webFrame, enabledPerSettings);
+        return webview->permissionClient()->allowPlugins(m_webFrame, enabledPerSettings);
 
     // FIXME(jam): remove this.
     if (m_webFrame->client())
@@ -210,7 +210,7 @@ bool FrameLoaderClientImpl::allowImages(bool enabledPerSettings)
 {
     WebViewImpl* webview = m_webFrame->viewImpl();
     if (webview && webview->permissionClient())
-        webview->permissionClient()->allowImages(m_webFrame, enabledPerSettings);
+        return webview->permissionClient()->allowImages(m_webFrame, enabledPerSettings);
 
     // FIXME(jam): remove this.
     if (m_webFrame->client())
