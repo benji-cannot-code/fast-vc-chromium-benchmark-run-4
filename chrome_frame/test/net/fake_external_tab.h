d3c5c0d5bbf1f2a1a4ac7f50a7690b9234d3315b
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 22:36:07 +0000

    Ensure that the exit status from the chrome frame net tests suite correctly reflects the result of the net test run. This will fix issues with the chrome frame builders staying green inspite of net tests failing.
    Next step : Fix ChromeFrame tests on similar lines.
    
    BUG=80820
    TEST=ChromeFrame net tests should cause the builder to turn red on failures.
    Review URL: http://codereview.chromium.org/6902167
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83612 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.h
+++ chrome_frame/test/net/fake_external_tab.h
@@ -78,6 +78,10 @@ class CFUrlRequestUnittestRunner
   // Borrowed from TestSuite::Initialize().
   static void InitializeLogging();
 
+  int test_result() const {
+    return test_result_;
+  }
+
  protected:
   // This is the thread that runs all the UrlRequest tests.
   // Within its context, the Initialize() and Shutdown() routines above
@@ -85,8 +89,6 @@ class CFUrlRequestUnittestRunner
   static DWORD WINAPI RunAllUnittests(void* param);
 
   static void TakeDownBrowser(CFUrlRequestUnittestRunner* me);
-
- protected:
 
  protected:
   base::win::ScopedHandle test_thread_;
@@ -101,6 +103,7 @@ class CFUrlRequestUnittestRunner
   scoped_ptr<ProcessSingletonSubclass> pss_subclass_;
   scoped_ptr<BrowserThread> main_thread_;
   ScopedChromeFrameRegistrar registrar_;
+  int test_result_;
 };
 
 #endif  // CHROME_FRAME_TEST_NET_FAKE_EXTERNAL_TAB_H_
