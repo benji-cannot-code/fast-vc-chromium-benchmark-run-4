08c02a0b371068b5b62902c8548e91ac2068d76d
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 11:52:30 +0000

    Added policy to allow plugins that require authorization to run.
    BUG=80818
    TEST=Enable the AlwaysAuthorizePlugins policy, and plugins that require authorization are always allowed. Otherwise, the user is asked for authorization.
    
    Review URL: http://codereview.chromium.org/6893092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -207,6 +207,7 @@ extern const char kPluginsEnabledInternalPDF[];
 extern const char kPluginsShowSetReaderDefaultInfobar[];
 extern const char kPluginsShowDetails[];
 extern const char kPluginsAllowOutdated[];
+extern const char kPluginsAlwaysAuthorize[];
 extern const char kCheckDefaultBrowser[];
 extern const char kDefaultBrowserSettingEnabled[];
 #if defined(OS_MACOSX)
