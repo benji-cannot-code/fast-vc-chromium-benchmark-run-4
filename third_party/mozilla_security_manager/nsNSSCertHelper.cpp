8a929ff8fee1f2ffddd2807e6f2d2a98f6133056
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 19:19:00 +0000

    Move chrome/third_party/mozilla_security_manager to third_party.
    All third_party libraries should be directly in third_party, not scattered throughout the tree.
    
    Note that net/third_party contains a folder of the same name, I will
    move files in there into third_party/mozilla_security_manager later.
    
    BUG=80916
    TEST=none
    
    Review URL: http://codereview.chromium.org/6902141
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83569 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/mozilla_security_manager/nsNSSCertHelper.cpp
+++ third_party/mozilla_security_manager/nsNSSCertHelper.cpp
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * ***** END LICENSE BLOCK ***** */
 
-#include "chrome/third_party/mozilla_security_manager/nsNSSCertHelper.h"
+#include "third_party/mozilla_security_manager/nsNSSCertHelper.h"
 
 #include <keyhi.h>
 #include <prprf.h>
