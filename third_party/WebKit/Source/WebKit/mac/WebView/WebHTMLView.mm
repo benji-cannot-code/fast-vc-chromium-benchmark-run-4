19c47388980800d8383927d78efc72cd7c2ec74c
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-01 19:46:01 +0000

    2011-05-01  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Eric Seidel.
    
            Get rid of versions of sendContextMenuEvent and eventMayStartDrag that takes NSEvent
            https://bugs.webkit.org/show_bug.cgi?id=59837
    
            Removed EventHandler::sendContextMenuEvent(NSEvent*) and EventHandler::eventMayStartDrag(NSEvent*)
    
            * WebCore.exp.in:
            * page/EventHandler.h:
            * page/mac/EventHandlerMac.mm:
    2011-05-01  Ryosuke Niwa  <rniwa@webkit.org>
    
            Reviewed by Eric Seidel.
    
            Get rid of versions of sendContextMenuEvent and eventMayStartDrag that takes NSEvent
            https://bugs.webkit.org/show_bug.cgi?id=59837
    
            Call sendContextMenuEvent and eventMayStartDrag with PlatformMouseEvent instead of NSEvent.
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView menuForEvent:]):
            (-[WebHTMLView acceptsFirstMouse:]):
            (-[WebHTMLView shouldDelayWindowOrderingForEvent:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebHTMLView.mm
@@ -77,6 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/CSSMutableStyleDeclaration.h>
 #import <WebCore/CachedImage.h>
 #import <WebCore/CachedResourceClient.h>
+#import <WebCore/Chrome.h>
 #import <WebCore/ColorMac.h>
 #import <WebCore/ContextMenu.h>
 #import <WebCore/ContextMenuController.h>
@@ -3109,7 +3110,7 @@ static void setMenuTargets(NSMenu* menu)
     _private->handlingMouseDownEvent = YES;
     page->contextMenuController()->clearContextMenu();
     coreFrame->eventHandler()->mouseDown(event);
-    BOOL handledEvent = coreFrame->eventHandler()->sendContextMenuEvent(event);
+    BOOL handledEvent = coreFrame->eventHandler()->sendContextMenuEvent(PlatformMouseEvent(event, page->chrome()->platformPageClient()));
     _private->handlingMouseDownEvent = NO;
 
     if (!handledEvent)
@@ -3426,9 +3427,10 @@ static void setMenuTargets(NSMenu* menu)
         if (Frame* coreFrame = core([hitHTMLView _frame])) {
             coreFrame->eventHandler()->setActivationEventNumber([event eventNumber]);
             [hitHTMLView _setMouseDownEvent:event];
-            if ([hitHTMLView _isSelectionEvent:event])
-                result = coreFrame->eventHandler()->eventMayStartDrag(event);
-            else if ([hitHTMLView _isScrollBarEvent:event])
+            if ([hitHTMLView _isSelectionEvent:event]) {
+                if (Page* page = coreFrame->page())
+                    result = coreFrame->eventHandler()->eventMayStartDrag(PlatformMouseEvent(event, page->chrome()->platformPageClient()));
+            } else if ([hitHTMLView _isScrollBarEvent:event])
                 result = true;
             [hitHTMLView _setMouseDownEvent:nil];
         }
@@ -3451,7 +3453,8 @@ static void setMenuTargets(NSMenu* menu)
         if ([hitHTMLView _isSelectionEvent:event]) {
             if (Frame* coreFrame = core([hitHTMLView _frame])) {
                 [hitHTMLView _setMouseDownEvent:event];
-                result = coreFrame->eventHandler()->eventMayStartDrag(event);
+                if (Page* page = coreFrame->page())
+                    result = coreFrame->eventHandler()->eventMayStartDrag(PlatformMouseEvent(event, page->chrome()->platformPageClient()));
                 [hitHTMLView _setMouseDownEvent:nil];
             }
         }
