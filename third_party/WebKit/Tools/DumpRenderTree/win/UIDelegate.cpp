fe4d3ee912c497eeb71f76a77b0ec40c1e8e1e1f
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-01 23:44:28 +0000

    2011-05-01  Patrick Gansterer  <paroga@webkit.org>
            [WIN] Unreviewed buildfix after r85434.
    
            * WebView.cpp:
            (WebView::paint):
    2011-05-01  Patrick Gansterer  <paroga@webkit.org>
    
            [WIN] Unreviewed buildfix after r85434.
    
            * DumpRenderTree/win/FrameLoadDelegate.cpp:
            (FrameLoadDelegate::FrameLoadDelegate):
            * DumpRenderTree/win/UIDelegate.cpp:
            (DRTUndoManager::DRTUndoManager):
            (UIDelegate::UIDelegate):
            (UIDelegate::resetUndoManager):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/win/UIDelegate.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/UIDelegate.cpp
@@ -104,8 +104,8 @@ private:
 };
 
 DRTUndoManager::DRTUndoManager()
-    : m_redoStack(new DRTUndoStack)
-    , m_undoStack(new DRTUndoStack)
+    : m_redoStack(adoptPtr(new DRTUndoStack))
+    , m_undoStack(adoptPtr(new DRTUndoStack))
     , m_isRedoing(false)
     , m_isUndoing(false)
 {
@@ -156,8 +156,8 @@ void DRTUndoManager::undo()
 
 UIDelegate::UIDelegate()
     : m_refCount(1)
-    , m_undoManager(new DRTUndoManager)
-    , m_desktopNotifications(new DRTDesktopNotificationPresenter)
+    , m_undoManager(adoptPtr(new DRTUndoManager))
+    , m_desktopNotifications(adoptPtr(new DRTDesktopNotificationPresenter))
 {
     m_frame.bottom = 0;
     m_frame.top = 0;
@@ -167,7 +167,7 @@ UIDelegate::UIDelegate()
 
 void UIDelegate::resetUndoManager()
 {
-    m_undoManager.set(new DRTUndoManager);
+    m_undoManager = adoptPtr(new DRTUndoManager);
 }
 
 HRESULT STDMETHODCALLTYPE UIDelegate::QueryInterface(REFIID riid, void** ppvObject)
