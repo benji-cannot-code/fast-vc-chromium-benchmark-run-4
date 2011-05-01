f6470c0aa24c110b68435546a795e4ea93ce6cf4
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-01 22:28:12 +0000

    2011-05-01  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Barth.
    
            [WINCE] Fix OwnPtr strict issues
            https://bugs.webkit.org/show_bug.cgi?id=59898
    
            * page/wince/FrameWinCE.cpp:
            (WebCore::imageFromSelection):
            * platform/graphics/wince/FontPlatformData.cpp:
            (WebCore::FixedSizeFontData::create):
            (WebCore::FontPlatformPrivateData::FontPlatformData::hfont):
            (WebCore::FontPlatformPrivateData::FontPlatformData::getScaledFontHandle):
            (WebCore::FontPlatformPrivateData::FontPlatformData::discardFontHandle):
            * platform/graphics/wince/GraphicsContextWinCE.cpp:
            (WebCore::GraphicsContext::strokeArc):
            (WebCore::GraphicsContext::fillRect):
            (WebCore::GraphicsContext::clip):
            (WebCore::GraphicsContext::fillRoundedRect):
            (WebCore::GraphicsContext::drawRoundCorner):
            (WebCore::GraphicsContext::drawText):
            * platform/graphics/wince/ImageBufferWinCE.cpp:
            (WebCore::ImageBuffer::ImageBuffer):
            * platform/graphics/wince/ImageWinCE.cpp:
            (WebCore::BitmapImage::getHBITMAPOfSize):
            * platform/graphics/wince/SharedBitmap.cpp:
            (WebCore::SharedBitmap::SharedBitmap):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
@@ -832,11 +832,11 @@ void GraphicsContext::strokeArc(const IntRect& rect, int startAngle, int angleSp
         clipRect.bottom = startY;
     }
 
-    OwnPtr<HRGN> clipRgn(CreateRectRgn(0, 0, 0, 0));
+    OwnPtr<HRGN> clipRgn = adoptPtr(CreateRectRgn(0, 0, 0, 0));
     bool newClip;
     if (GetClipRgn(dc, clipRgn.get()) <= 0) {
         newClip = true;
-        clipRgn.set(CreateRectRgn(clipRect.left, clipRect.top, clipRect.right, clipRect.bottom));
+        clipRgn = adoptPtr(CreateRectRgn(clipRect.left, clipRect.top, clipRect.right, clipRect.bottom));
         SelectClipRgn(dc, clipRgn.get());
     } else {
         newClip = false;
@@ -949,7 +949,7 @@ void GraphicsContext::fillRect(const FloatRect& rect, const Color& color, ColorS
     if (!transparentDC.hdc())
         return;
 
-    OwnPtr<HBRUSH> hbrush(CreateSolidBrush(RGB(color.red(), color.green(), color.blue())));
+    OwnPtr<HBRUSH> hbrush = adoptPtr(CreateSolidBrush(RGB(color.red(), color.green(), color.blue())));
     FillRect(transparentDC.hdc(), &transparentDC.rect(), hbrush.get());
 }
 
@@ -963,11 +963,11 @@ void GraphicsContext::clip(const FloatRect& rect)
 
     IntRect trRect = enclosingIntRect(m_data->mapRect(rect));
 
-    OwnPtr<HRGN> clipRgn(CreateRectRgn(0, 0, 0, 0));
+    OwnPtr<HRGN> clipRgn = adoptPtr(CreateRectRgn(0, 0, 0, 0));
     if (GetClipRgn(m_data->m_dc, clipRgn.get()) > 0)
         IntersectClipRect(m_data->m_dc, trRect.x(), trRect.y(), trRect.maxX(), trRect.maxY());
     else {
-        clipRgn.set(CreateRectRgn(trRect.x(), trRect.y(), trRect.maxX(), trRect.maxY()));
+        clipRgn = adoptPtr(CreateRectRgn(trRect.x(), trRect.y(), trRect.maxX(), trRect.maxY()));
         SelectClipRgn(m_data->m_dc, clipRgn.get());
     }
 }
@@ -1253,7 +1253,7 @@ void GraphicsContext::fillRoundedRect(const IntRect& fillRect, const IntSize& to
     clipRect.right = centerPoint.x();
     clipRect.bottom = centerPoint.y();
 
-    OwnPtr<HRGN> clipRgn(CreateRectRgn(0, 0, 0, 0));
+    OwnPtr<HRGN> clipRgn = adoptPtr(CreateRectRgn(0, 0, 0, 0));
     bool needsNewClip = (GetClipRgn(dc, clipRgn.get()) <= 0);
     
     drawRoundCorner(needsNewClip, clipRect, rectWin, dc, stableRound(newTopLeft.width() * 2), stableRound(newTopLeft.height() * 2));
@@ -1288,15 +1288,15 @@ void GraphicsContext::drawRoundCorner(bool needsNewClip, RECT clipRect, RECT rec
     if (!dc)
         return;
 
-    OwnPtr<HRGN> clipRgn(CreateRectRgn(0, 0, 0, 0));
-    if (needsNewClip)  {
-        clipRgn.set(CreateRectRgn(clipRect.left, clipRect.top, clipRect.right, clipRect.bottom));
+    OwnPtr<HRGN> clipRgn = adoptPtr(CreateRectRgn(0, 0, 0, 0));
+    if (needsNewClip) {
+        clipRgn = adoptPtr(CreateRectRgn(clipRect.left, clipRect.top, clipRect.right, clipRect.bottom));
         SelectClipRgn(dc, clipRgn.get());
-    } else 
+    } else
         IntersectClipRect(dc, clipRect.left, clipRect.top, clipRect.right, clipRect.bottom);
-    
+
     ::RoundRect(dc, rectWin.left , rectWin.top , rectWin.right , rectWin.bottom , width, height);
-    
+
     SelectClipRgn(dc, needsNewClip ? 0 : clipRgn.get());
 }
 
@@ -1623,7 +1623,7 @@ void GraphicsContext::drawText(const SimpleFontData* fontData, const GlyphBuffer
 
         offset += width;
 
-        OwnPtr<HPEN> hPen(CreatePen(PS_DASH, 1, fontColor));
+        OwnPtr<HPEN> hPen = adoptPtr(CreatePen(PS_DASH, 1, fontColor));
         HGDIOBJ oldPen = SelectObject(m_data->m_dc, hPen.get());
 
         MoveToEx(m_data->m_dc, stableRound(trPoint.x()), y, 0);
