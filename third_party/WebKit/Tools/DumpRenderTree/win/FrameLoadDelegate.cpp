65720c5638f8c4a679273cd3addf0be4b0e7755a
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-01 23:53:43 +0000

    2011-05-01  Patrick Gansterer  <paroga@webkit.org>
            [WIN] Unreviewed buildfix after r85434.
    
            * DumpRenderTree/win/FrameLoadDelegate.cpp: Added missing header.
            * DumpRenderTree/win/UIDelegate.cpp: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/win/FrameLoadDelegate.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/FrameLoadDelegate.cpp
@@ -42,9 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <JavaScriptCore/Assertions.h>
 #include <JavaScriptCore/JavaScriptCore.h>
 #include <WebKit/WebKit.h>
-#include <wtf/Vector.h>
 #include <stdio.h>
 #include <string>
+#include <wtf/PassOwnPtr.h>
+#include <wtf/Vector.h>
 
 using std::string;
 
--- third_party/WebKit/Tools/DumpRenderTree/win/UIDelegate.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/UIDelegate.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DRTDesktopNotificationPresenter.h"
 
 #include <WebCore/COMPtr.h>
+#include <wtf/PassOwnPtr.h>
 #include <wtf/Platform.h>
 #include <wtf/Vector.h>
 #include <JavaScriptCore/Assertions.h>
