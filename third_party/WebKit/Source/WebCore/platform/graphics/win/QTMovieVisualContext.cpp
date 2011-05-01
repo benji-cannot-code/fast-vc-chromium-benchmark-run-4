4bce931ba3496eafa76426d806aa3e7f8db4f94e
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-01 22:53:54 +0000

    2011-05-01  Patrick Gansterer  <paroga@webkit.org>
            [WIN] Unreviewed buildfix after r85434.
    
            * platform/graphics/win/QTMovieVisualContext.cpp:
            (QTMovieVisualContext::QTMovieVisualContext): Added missing header.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/win/QTMovieVisualContext.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/win/QTMovieVisualContext.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ImageCompression.h>
 #include <Movies.h>
 #include <windows.h>
+#include <wtf/PassOwnPtr.h>
 
 struct QTCVTimeStamp {
     CVTimeStamp t;
