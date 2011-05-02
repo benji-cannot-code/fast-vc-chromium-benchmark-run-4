c893b8a44eb259d6af1f8a5c161496b222582e37
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-02 20:23:21 +0000

    2011-05-02  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            showLineTree/showLineTreeForThis would make working with the line box tree easier
            https://bugs.webkit.org/show_bug.cgi?id=59662
    
            Adding a showLineTree/showLineTreeForThis method to help visualize and debug
            the line tree. Also adding a missing showRenderTreeForThis method to RenderObject.
    
            No new tests since this is a debugging feature only and not compiled in release.
    
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::showLineTreeForThis):
            (WebCore::InlineBox::showLineTreeAndMark):
            (WebCore::InlineBox::showBox):
            (showLineTree):
            * rendering/InlineBox.h:
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::showLineTreeAndMark):
            * rendering/InlineFlowBox.h:
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::showBox):
            * rendering/InlineTextBox.h:
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::showLineTreeAndMark):
            * rendering/RenderBlock.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::showRenderTreeForThis):
            (WebCore::RenderObject::showLineTreeForThis):
            (showTree):
            (showLineTree):
            * rendering/RenderObject.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
@@ -814,4 +814,11 @@ Node* RootInlineBox::getLogicalEndBoxWithNode(InlineBox*& endBox) const
     return 0;
 }
 
+#ifndef NDEBUG
+const char* RootInlineBox::boxName() const
+{
+    return "RootInlineBox";
+}
+#endif
+
 } // namespace WebCore
