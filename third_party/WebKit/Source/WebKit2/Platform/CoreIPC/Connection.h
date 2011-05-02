1e4600b71de5f2eabc8b94bd7c549726a425b63b
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-02 19:41:26 +0000

    2011-05-02  Anders Carlsson  <andersca@apple.com>
            Reviewed by Adam Roben.
    
            Need a way to handle CoreIPC messages on the connection work queue
            https://bugs.webkit.org/show_bug.cgi?id=59954
    
            Add a Connection::QueueClient abstract class. This class has a single pure virtual
            member function, willProcessMessageOnClientRunLoop which should return true if the message 
            should be forwarded to the next Connection::QueueClient in the list or Connection::Client's 
            run loop for processing and false otherwise. Users of Connection can use addQueueClient to
            add clients and all messages will be dispatched  to the queue clients first.
    
            * Platform/CoreIPC/Connection.cpp:
            (CoreIPC::Connection::addQueueClient):
            (CoreIPC::Connection::removeQueueClient):
            (CoreIPC::Connection::processIncomingMessage):
            * Platform/CoreIPC/Connection.h:
            (CoreIPC::Connection::QueueClient::~QueueClient):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/CoreIPC/Connection.h
+++ third_party/WebKit/Source/WebKit2/Platform/CoreIPC/Connection.h
@@ -79,18 +79,15 @@ while (0)
 class Connection : public ThreadSafeRefCounted<Connection> {
 public:
     class MessageReceiver {
-    protected:
-        virtual ~MessageReceiver() { }
-
     public:
         virtual void didReceiveMessage(Connection*, MessageID, ArgumentDecoder*) = 0;
         virtual SyncReplyMode didReceiveSyncMessage(Connection*, MessageID, ArgumentDecoder*, ArgumentEncoder*) { ASSERT_NOT_REACHED(); return AutomaticReply; }
+
+    protected:
+        virtual ~MessageReceiver() { }
     };
     
     class Client : public MessageReceiver {
-    protected:
-        virtual ~Client() { }
-
     public:
         virtual void didClose(Connection*) = 0;
         virtual void didReceiveInvalidMessage(Connection*, MessageID) = 0;
@@ -99,6 +96,17 @@ public:
 #if PLATFORM(WIN)
         virtual Vector<HWND> windowsToReceiveSentMessagesWhileWaitingForSyncReply() = 0;
 #endif
+
+    protected:
+        virtual ~Client() { }
+    };
+
+    class QueueClient {
+    public:
+        virtual bool willProcessMessageOnClientRunLoop(Connection*, MessageID, ArgumentDecoder*) = 0;
+
+    protected:
+        virtual ~QueueClient() { }
     };
 
 #if PLATFORM(MAC)
@@ -130,7 +138,10 @@ public:
     // handling the message on the client thread first.
     typedef void (*DidCloseOnConnectionWorkQueueCallback)(WorkQueue&, Connection*);
     void setDidCloseOnConnectionWorkQueueCallback(DidCloseOnConnectionWorkQueueCallback callback);
-                                                
+
+    void addQueueClient(QueueClient*);
+    void removeQueueClient(QueueClient*);
+
     bool open();
     void invalidate();
     void markCurrentlyDispatchedMessageAsInvalid();
@@ -234,6 +245,9 @@ private:
     bool m_isConnected;
     WorkQueue m_connectionQueue;
     RunLoop* m_clientRunLoop;
+
+    Mutex m_connectionQueueClientsMutex;
+    Vector<QueueClient*> m_connectionQueueClients;
 
     unsigned m_inDispatchMessageCount;
     unsigned m_inDispatchMessageMarkedDispatchWhenWaitingForSyncReplyCount;
