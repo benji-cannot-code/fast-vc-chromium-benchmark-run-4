e6dd5e63d8d9000b93f1a943ebaa456b3611ed22
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-02 15:13:55 +0000

    Review URL: http://codereview.chromium.org/6893138
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83721 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppp_instance.h
+++ ppapi/c/ppp_instance.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/* Copyright (c) 2010 The Chromium Authors. All rights reserved.
+/* Copyright (c) 2011 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -18,9 +18,8 @@ struct PP_Var;
 
 /**
  * @file
- * This file defines the PPP_Instance structure - a series of points to methods
- * that you must implement in your model.
- *
+ * This file defines the PPP_Instance structure - a series of pointers to
+ * methods that you must implement in your module.
  */
 
 /** @addtogroup Interfaces
