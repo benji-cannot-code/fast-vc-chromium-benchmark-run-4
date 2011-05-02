d9c58360e80319cdf18d05720aa8d5a3a0d44b69
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-02 18:38:59 +0000

    Build fix.
    * WebKitTestRunner/InjectedBundle/LayoutTestController.h:
    * WebKitTestRunner/InjectedBundle/mac/InjectedBundleMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(MAC)
 #include <wtf/RetainPtr.h>
+#include <CoreFoundation/CFRunLoop.h>
 typedef RetainPtr<CFRunLoopTimerRef> PlatformTimerRef;
 #elif PLATFORM(WIN)
 typedef UINT_PTR PlatformTimerRef;
