701e3a2f0a2d2c659f040979118591f8dc115f68
gfeher@chromium.org <gfeher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-02 13:23:04 +0000

    Disable appropriate menus if file-selection dialogs are disabled by policy.
    Disable the following menu-items, in case the AllowFileSelectionDialogs-Policy is set to false:
    * Wrench/File - Save Page As...
    * File - Open
    * Right-Click - Save Link As...
    * Right-Click - Save Image As...
    * Right-Click - Save As...
    * Advanced Options - Downloads - Change
    
    BUG=73174
    TEST=Manually set the policy and verify that the menus are disabled.
    
    Review URL: http://codereview.chromium.org/6877092
    Patch from Sandro Feuz <sfeuz@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/debugger/devtools_toggle_action.h"
 #include "chrome/browser/instant/instant_delegate.h"
 #include "chrome/browser/prefs/pref_member.h"
+#include "chrome/browser/prefs/pref_change_registrar.h"
 #include "chrome/browser/sessions/session_id.h"
 #include "chrome/browser/sessions/tab_restore_service_observer.h"
 #include "chrome/browser/sync/profile_sync_service_observer.h"
@@ -884,6 +885,12 @@ class Browser : public TabHandlerDelegate,
   // Updates the printing command state.
   void UpdatePrintingState(int content_restrictions);
 
+  // Updates the save-page-as command state.
+  void UpdateSaveAsState(int content_restrictions);
+
+  // Updates the open-file state (Mac Only).
+  void UpdateOpenFileState();
+
   // Ask the Reload/Stop button to change its icon, and update the Stop command
   // state.  |is_loading| is true if the current TabContents is loading.
   // |force| is true if the button should change its icon immediately.
@@ -1053,6 +1060,10 @@ class Browser : public TabHandlerDelegate,
 
   NotificationRegistrar registrar_;
 
+  PrefChangeRegistrar profile_pref_registrar_;
+
+  PrefChangeRegistrar local_pref_registrar_;
+
   // This Browser's type.
   const Type type_;
 
@@ -1142,21 +1153,6 @@ class Browser : public TabHandlerDelegate,
 
   // Keep track of the encoding auto detect pref.
   BooleanPrefMember encoding_auto_detect_;
-
-  // Keep track of the printing enabled pref.
-  BooleanPrefMember printing_enabled_;
-
-  // Keep track of the development tools disabled pref.
-  BooleanPrefMember dev_tools_disabled_;
-
-  // Keep track of when instant enabled changes.
-  BooleanPrefMember instant_enabled_;
-
-  // Tracks the preference that controls whether incognito mode is allowed.
-  BooleanPrefMember incognito_mode_allowed_;
-
-  // Tracks whether bookmarks can be modified.
-  BooleanPrefMember edit_bookmarks_enabled_;
 
   // Indicates if command execution is blocked.
   bool block_command_execution_;
