fc42495af6699fe1b0cdc11628f3c95644c44367
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-02 01:27:17 +0000

    2011-05-01  Arno Renevier  <arno@renevier.net>
            Reviewed by Eric Seidel.
    
            wrong exception raised in Lighttpd start method
            https://bugs.webkit.org/show_bug.cgi?id=59283
    
            raise standard Exception instead of non existent
            google.httpd_utils.HttpdNotStarted
    
            * Scripts/webkitpy/layout_tests/port/http_server.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/__init__.py
@@ -2,4 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Required for Python to search this directory for module files
 
 # We only export public API here.
-from .scm import SCM, SVN, Git, CommitMessage, detect_scm_system, find_checkout_root, default_scm, AuthenticationError, AmbiguousCommitError, CheckoutNeedsUpdate
+from .commitmessage import CommitMessage
+from .detection import find_checkout_root, default_scm, detect_scm_system
+from .git import Git, AmbiguousCommitError
+from .scm import SCM, AuthenticationError, CheckoutNeedsUpdate
+from .svn import SVN
