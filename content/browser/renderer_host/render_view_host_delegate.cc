f4db861e571c08901d15dc5eb89d2562ff2afbe0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-02 20:13:08 +0000

    Remove some unnecessary includes. Review URL: http://codereview.chromium.org/6883304
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83769 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_delegate.cc
+++ content/browser/renderer_host/render_view_host_delegate.cc
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 #include "webkit/glue/webpreferences.h"
 
-#if defined(TOOLKIT_USES_GTK)
-#include "chrome/browser/ui/gtk/gtk_util.h"
-#endif
-
 RenderViewHostDelegate::View* RenderViewHostDelegate::GetViewDelegate() {
   return NULL;
 }
--- content/browser/tab_contents/tab_contents_delegate.h
+++ content/browser/tab_contents/tab_contents_delegate.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "chrome/common/content_settings_types.h"
-#include "chrome/common/instant_types.h"
 #include "content/browser/tab_contents/navigation_entry.h"
 #include "content/common/navigation_types.h"
 #include "content/common/page_transition_types.h"
