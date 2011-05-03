9df55800c77a199bf367434a1b145ec01a3c5348
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-03 20:55:44 +0000

    Add some missing virtual keywords on derived-class destructors
    Darin Adler noticed one, I noticed the other. This doesn't change the generated code; it
    just matches our style of explicitly marking overrides "virtual".
    
    * WebCoreSupport/WebInspectorClient.h: Made the WebInspectorClient and
    WebInspectorFrontendClient destructors explicitly virtual.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/win/WebCoreSupport/WebInspectorClient.h
+++ third_party/WebKit/Source/WebKit/win/WebCoreSupport/WebInspectorClient.h
@@ -80,7 +80,7 @@ public:
     void loadSessionSetting(const WTF::String& key, WTF::String* value);
 
 private:
-    ~WebInspectorClient();
+    virtual ~WebInspectorClient();
     WTF::PassOwnPtr<WebCore::InspectorFrontendClientLocal::Settings> createFrontendSettings();
 
     WebView* m_inspectedWebView;
@@ -96,7 +96,7 @@ private:
 class WebInspectorFrontendClient : public WebCore::InspectorFrontendClientLocal, WebCore::WindowMessageListener {
 public:
     WebInspectorFrontendClient(WebView* inspectedWebView, HWND inspectedWebViewHwnd, HWND frontendHwnd, const COMPtr<WebView>& frotnendWebView, HWND frontendWebViewHwnd, WebInspectorClient*, WTF::PassOwnPtr<Settings>);
-    ~WebInspectorFrontendClient();
+    virtual ~WebInspectorFrontendClient();
 
     virtual void frontendLoaded();
     
