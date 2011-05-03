3896796cdc418c38759bd025a44b920d32974a99
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-03 17:18:28 +0000

    Fix chromium build.
    * loader/HistoryController.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/HistoryController.cpp
+++ third_party/WebKit/Source/WebCore/loader/HistoryController.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "BackForwardController.h"
 #include "CachedPage.h"
+#include "Document.h"
 #include "DocumentLoader.h"
 #include "Frame.h"
 #include "FrameLoader.h"
