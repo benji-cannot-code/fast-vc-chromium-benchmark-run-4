279f4c312a2073e4fb287a6251f5480dd0f29922
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-03 23:08:21 +0000

    2011-05-03  Roland Steiner  <rolandsteiner@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Allow access keys to be used in shadow DOM
            https://bugs.webkit.org/show_bug.cgi?id=59979
    
            Move access key methods and members back to Document from TreeScope.
            Also traverse into shadow trees when building the access key map.
    
            No new tests. (refactoring)
    
            * dom/Document.cpp:
            (WebCore::Document::Document):
            (WebCore::Document::getElementByAccessKey):
            (WebCore::Document::buildAccessKeyMap):
            (WebCore::Document::invalidateAccessKeyMap):
            * dom/Document.h:
            * dom/TreeScope.cpp:
            (WebCore::TreeScope::TreeScope):
            (WebCore::TreeScope::destroyTreeScopeData):
            * dom/TreeScope.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85680 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -246,6 +246,9 @@ public:
 
     Element* getElementById(const AtomicString& id) const;
 
+    Element* getElementByAccessKey(const String& key);
+    void invalidateAccessKeyMap();
+
     // DOM methods & attributes for Document
 
     DEFINE_ATTRIBUTE_EVENT_LISTENER(abort);
@@ -1120,6 +1123,8 @@ private:
 
     void cacheDocumentElement() const;
 
+    void buildAccessKeyMap(ContainerNode* root);
+
     void createStyleSelector();
 
     PassRefPtr<NodeList> handleZeroPadding(const HitTestRequest&, HitTestResult&) const;
@@ -1330,6 +1335,9 @@ private:
     HashSet<Element*> m_documentActivationCallbackElements;
     HashSet<Element*> m_mediaVolumeCallbackElements;
     HashSet<Element*> m_privateBrowsingStateChangedElements;
+
+    HashMap<StringImpl*, Element*, CaseFoldingHash> m_elementsByAccessKey;    
+    bool m_accessKeyMapValid;
 
     bool m_useSecureKeyboardEntryWhenActive;
 
