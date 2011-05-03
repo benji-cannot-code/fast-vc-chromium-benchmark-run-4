8c970211dbbab502f787547fad8bdf4fa7e7b9b5
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-03 17:27:01 +0000

    Fix chromium build.
    * page/Settings.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Settings.cpp
+++ third_party/WebKit/Source/WebCore/page/Settings.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CookieStorage.h"
 #include "DOMTimer.h"
 #include "Database.h"
+#include "Document.h"
 #include "Frame.h"
 #include "FrameTree.h"
 #include "FrameView.h"
