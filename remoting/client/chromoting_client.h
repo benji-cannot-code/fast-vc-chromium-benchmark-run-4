c7c4a793b34ef78e10f2334920e086b82e87e508
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 15:30:19 +0000

    Revert 84060 - Remove cancel from RunnableFunctions.
    BUG=NONE
    TEST=BUILD
    
    Review URL: http://codereview.chromium.org/6690031
    
    TBR=dmaclach@chromium.org
    Review URL: http://codereview.chromium.org/6927012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84063 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/chromoting_client.h
+++ remoting/client/chromoting_client.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -46,7 +46,7 @@ class ChromotingClient : public protocol::ConnectionToHost::HostEventCallback,
                    ChromotingView* view,
                    RectangleUpdateDecoder* rectangle_decoder,
                    InputHandler* input_handler,
-                   Task* client_done);
+                   CancelableTask* client_done);
   virtual ~ChromotingClient();
 
   void Start();
@@ -120,7 +120,7 @@ class ChromotingClient : public protocol::ConnectionToHost::HostEventCallback,
   InputHandler* input_handler_;
 
   // If non-NULL, this is called when the client is done.
-  Task* client_done_;
+  CancelableTask* client_done_;
 
   ConnectionState state_;
 
