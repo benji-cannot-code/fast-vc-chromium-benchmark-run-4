13f52837e65d4ff7102e1548bf35b5c629b547f5
kushi.p@gmail.com <kushi.p@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 01:29:38 +0000

    Updating logging in src/base/. Using DCHECK_NE/EQ/LE/GE/GT() where possible
    BUG=58409
    
    
    Review URL: http://codereview.chromium.org/6883295
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84016 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread_local_posix.cc
+++ base/threading/thread_local_posix.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,7 +22,7 @@ void ThreadLocalPlatform::AllocateSlot(SlotType& slot) {
 // static
 void ThreadLocalPlatform::FreeSlot(SlotType& slot) {
   int error = pthread_key_delete(slot);
-  DCHECK(error == 0);
+  DCHECK_EQ(0, error);
 }
 
 // static
--- base/time_mac.cc
+++ base/time_mac.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -113,7 +113,7 @@ TimeTicks TimeTicks::Now() {
     // whether mach_timebase_info has already been called.  This is
     // recommended by Apple's QA1398.
     kern_return_t kr = mach_timebase_info(&timebase_info);
-    DCHECK(kr == KERN_SUCCESS);
+    DCHECK_EQ(KERN_SUCCESS, kr);
   }
 
   // mach_absolute_time is it when it comes to ticks on the Mac.  Other calls
