5c507b60351ffe628339893389bee9021ffbb602
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 20:59:04 +0000

    Cleanup: Change LOG(INFO) to VLOG(1) in chrome/browser/prerender.
    BUG=none
    TEST=less spew on the console.
    Review URL: http://codereview.chromium.org/6928005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/prerender/prerender_manager.h"
 
+#include <string>
+
 #include "base/logging.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
@@ -24,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/tab_contents/render_view_host_manager.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/common/notification_service.h"
-#include "googleurl/src/url_parse.h"
 #include "googleurl/src/url_canon.h"
+#include "googleurl/src/url_parse.h"
 #include "googleurl/src/url_util.h"
 
 namespace prerender {
@@ -505,10 +507,10 @@ bool PrerenderManager::MaybeUsePreloadedPageOld(TabContents* tab_contents,
 bool PrerenderManager::MaybeUsePreloadedPage(TabContents* tab_contents,
                                              const GURL& url) {
   if (!PrerenderContents::UseTabContents()) {
-    LOG(INFO) << "Checking for prerender with LEGACY code\n";
+    VLOG(1) << "Checking for prerender with LEGACY code\n";
     return PrerenderManager::MaybeUsePreloadedPageOld(tab_contents, url);
   }
-  LOG(INFO) << "Checking for prerender with NEW code\n";
+  VLOG(1) << "Checking for prerender with NEW code\n";
   DCHECK(CalledOnValidThread());
   scoped_ptr<PrerenderContents> prerender_contents(
       GetEntryButNotSpecifiedTC(url, tab_contents));
