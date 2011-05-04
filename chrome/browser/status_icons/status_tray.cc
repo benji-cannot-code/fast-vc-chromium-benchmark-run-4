3c1b4e09d7cd9444974fb67bbfad88b421d40419
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 21:17:55 +0000

    status_icons: Use STLDeleteElements on StatusTray::RemoveAllIcons().
    BUG=None
    TEST=None
    
    R=atwilson@chromium.org
    
    Review URL: http://codereview.chromium.org/6929019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/status_icons/status_tray.cc
+++ chrome/browser/status_icons/status_tray.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,9 +18,7 @@ StatusTray::~StatusTray() {
 }
 
 void StatusTray::RemoveAllIcons() {
-  // Walk any active status icons and delete them.
-  STLDeleteContainerPointers(status_icons_.begin(), status_icons_.end());
-  status_icons_.clear();
+  STLDeleteElements(&status_icons_);
 }
 
 StatusIcon* StatusTray::CreateStatusIcon() {
