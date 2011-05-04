e339021fad09e0833c25035a9be8761ecb23349b
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-04 06:08:49 +0000

    2011-05-03  Alexis Menard  <alexis.menard@openbossa.org>
            Reviewed by Eric Seidel.
    
            Unused but set variable warning in MacroAssemberX86_64
            https://bugs.webkit.org/show_bug.cgi?id=59482
    
            * assembler/MacroAssemblerX86_64.h:
            (JSC::MacroAssemblerX86_64::call):
            (JSC::MacroAssemblerX86_64::tailRecursiveCall):
            (JSC::MacroAssemblerX86_64::makeTailRecursiveCall):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerX86_64.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerX86_64.h
@@ -118,7 +118,7 @@ public:
     {
         DataLabelPtr label = moveWithPatch(TrustedImmPtr(0), scratchRegister);
         Call result = Call(m_assembler.call(scratchRegister), Call::Linkable);
-        ASSERT(differenceBetween(label, result) == REPTACH_OFFSET_CALL_R11);
+        ASSERT_UNUSED(label, differenceBetween(label, result) == REPTACH_OFFSET_CALL_R11);
         return result;
     }
 
@@ -126,7 +126,7 @@ public:
     {
         DataLabelPtr label = moveWithPatch(TrustedImmPtr(0), scratchRegister);
         Jump newJump = Jump(m_assembler.jmp_r(scratchRegister));
-        ASSERT(differenceBetween(label, newJump) == REPTACH_OFFSET_CALL_R11);
+        ASSERT_UNUSED(label, differenceBetween(label, newJump) == REPTACH_OFFSET_CALL_R11);
         return Call::fromTailJump(newJump);
     }
 
@@ -135,7 +135,7 @@ public:
         oldJump.link(this);
         DataLabelPtr label = moveWithPatch(TrustedImmPtr(0), scratchRegister);
         Jump newJump = Jump(m_assembler.jmp_r(scratchRegister));
-        ASSERT(differenceBetween(label, newJump) == REPTACH_OFFSET_CALL_R11);
+        ASSERT_UNUSED(label, differenceBetween(label, newJump) == REPTACH_OFFSET_CALL_R11);
         return Call::fromTailJump(newJump);
     }
 
