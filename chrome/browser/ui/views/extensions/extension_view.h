35b7719644b1bc98b6fefb8e56b60ffb4906d095
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 22:52:09 +0000

    Add ExtensionView::GetCursor OVERRIDE to return NULL.
    RenderWidgetHostViewWin::UpdateCursorIfOverSelf() sets ExtensionView cursors correctly on WM_SETCURSOR (no mouse capture).
    The new View::GetCursor arrow default (crrev.com/83123) conflicted with that, override ExtensionView to no-op.
    
    BUG=81346
    TEST=Cursor appearance when mousing over extension bubbles.
    
    Review URL: http://codereview.chromium.org/6929020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84145 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_view.h
+++ chrome/browser/ui/views/extensions/extension_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -58,6 +58,7 @@ class ExtensionView : public views::NativeViewHost {
   void SetContainer(Container* container) { container_ = container; }
 
   // Overridden from views::NativeViewHost:
+  virtual gfx::NativeCursor GetCursor(const views::MouseEvent& event) OVERRIDE;
   virtual void SetVisible(bool is_visible) OVERRIDE;
   virtual void ViewHierarchyChanged(
       bool is_add, views::View *parent, views::View *child) OVERRIDE;
