0d933866ec270fe96735c24bee06459a707c357f
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 17:16:30 +0000

    class PageLoadTrackerUnitTest is not used anymore.  Remove.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6929014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84077 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time.h
+++ base/time.h
@@ -45,9 +45,6 @@ namespace base {
 class Time;
 class TimeTicks;
 
-// This unit test does a lot of manual time manipulation.
-class PageLoadTrackerUnitTest;
-
 // TimeDelta ------------------------------------------------------------------
 
 class BASE_API TimeDelta {
@@ -536,7 +533,6 @@ class BASE_API TimeTicks {
 
  protected:
   friend class TimeDelta;
-  friend class PageLoadTrackerUnitTest;
 
   // Please use Now() to create a new object. This is for internal use
   // and testing. Ticks is in microseconds.
