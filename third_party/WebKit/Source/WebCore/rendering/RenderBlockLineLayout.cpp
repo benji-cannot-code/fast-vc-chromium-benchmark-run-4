9df61a2ce3ee23e29556b8664c4d189a6fe25f0d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-04 02:20:02 +0000

    <rdar://problem/9313186> Make the fix for <rdar://problem/9190108> more robust.
    Reviewed by Darin Adler.
    
    Added a pointer from FloatingObject to its originating line, if there is one, and made sure to
    dirty the line when the float is removed, instead of relying on the float always intersecting
    its originating line.
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::removeFloatingObject):
    (WebCore::RenderBlock::removeFloatingObjectsBelow):
    (WebCore::RenderBlock::clearFloats):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::FloatingObject::FloatingObject):
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::appendFloatingObjectToLastLine):
    (WebCore::RenderBlock::layoutRunsAndFloats):
    (WebCore::RenderBlock::determineStartPosition):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlockLineLayout.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -758,12 +758,8 @@ inline BidiRun* RenderBlock::handleTrailingSpaces(BidiRunList<BidiRun>& bidiRuns
 
 void RenderBlock::appendFloatingObjectToLastLine(FloatingObject* floatingObject)
 {
-    // Ensure that the float touches the line.
-    if (RootInlineBox* previousLine = lastRootBox()->prevRootBox()) {
-        if (logicalBottomForFloat(floatingObject) < previousLine->blockLogicalHeight())
-            setLogicalHeightForFloat(floatingObject, previousLine->blockLogicalHeight() - logicalTopForFloat(floatingObject));
-    }
-
+    ASSERT(!floatingObject->m_originatingLine);
+    floatingObject->m_originatingLine = lastRootBox();
     lastRootBox()->appendFloat(floatingObject->renderer());
 }
 
@@ -1025,7 +1021,9 @@ void RenderBlock::layoutRunsAndFloats(bool fullLayout, bool hasInlineChild, Vect
                 if (Vector<RenderBox*>* cleanLineFloats = line->floatsPtr()) {
                     Vector<RenderBox*>::iterator end = cleanLineFloats->end();
                     for (Vector<RenderBox*>::iterator f = cleanLineFloats->begin(); f != end; ++f) {
-                        insertFloatingObject(*f);
+                        FloatingObject* floatingObject = insertFloatingObject(*f);
+                        ASSERT(!floatingObject->m_originatingLine);
+                        floatingObject->m_originatingLine = line;
                         setLogicalHeight(logicalTopForChild(*f) - marginBeforeForChild(*f) + delta);
                         positionNewFloats();
                     }
@@ -1292,7 +1290,9 @@ RootInlineBox* RenderBlock::determineStartPosition(LineInfo& lineInfo, bool& ful
             if (Vector<RenderBox*>* cleanLineFloats = line->floatsPtr()) {
                 Vector<RenderBox*>::iterator end = cleanLineFloats->end();
                 for (Vector<RenderBox*>::iterator f = cleanLineFloats->begin(); f != end; ++f) {
-                    insertFloatingObject(*f);
+                    FloatingObject* floatingObject = insertFloatingObject(*f);
+                    ASSERT(!floatingObject->m_originatingLine);
+                    floatingObject->m_originatingLine = line;
                     setLogicalHeight(logicalTopForChild(*f) - marginBeforeForChild(*f));
                     positionNewFloats();
                     ASSERT(floats[numCleanFloats].object == *f);
