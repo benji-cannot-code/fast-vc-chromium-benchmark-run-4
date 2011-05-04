a02022f805bb8f1e849fb8fe1a0998b85056a079
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 19:42:41 +0000

    Move AcceleratedPluginContainer from render_widget_host_view_mac.mm to its own file.
    I'd like to add tests to it, and that's easier this way. It's also nicer in general.
    No intended functionality change.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6933010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_mac.h
+++ chrome/browser/renderer_host/render_widget_host_view_mac.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #import <Cocoa/Cocoa.h>
-#import <QuartzCore/CALayer.h>
 
 #include "base/memory/scoped_nsobject.h"
 #include "base/memory/scoped_ptr.h"
