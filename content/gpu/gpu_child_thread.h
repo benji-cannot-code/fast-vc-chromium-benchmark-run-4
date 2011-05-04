26b884f324c18509909d3bfb93f316565b8efb61
amarinichev@chromium.org <amarinichev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 20:48:52 +0000

    Added about:gpuclean
    It removes all contexts from the GPU process.  Can rename it to what was originally suggested in the bug, about:gpuclean seemed a little more consistent with other about:gpu* URLs we have.
    
    BUG=75215
    TEST=tried it
    
    Review URL: http://codereview.chromium.org/6870019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84119 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_child_thread.h
+++ content/gpu/gpu_child_thread.h
@@ -61,6 +61,7 @@ class GpuChildThread : public ChildThread {
   // Message handlers.
   void OnInitialize();
   void OnCollectGraphicsInfo();
+  void OnClean();
   void OnCrash();
   void OnHang();
 
