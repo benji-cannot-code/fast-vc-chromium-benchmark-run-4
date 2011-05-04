b36ec90214c71ff2e2acd58f2fdd6ac9a522e649
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-04 20:08:11 +0000

    2011-05-04  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Anders Carlsson.
    
            WebKit2 flashes if entering and exiting compositing rapidly
            https://bugs.webkit.org/show_bug.cgi?id=60198
    
            If a page enters then immediately leaves compositing mode, the
            DrawingAreaImpl would send a message to the UI process that it
            had entered compositing even after all the layers had been torn
            down. Fix this by never sending the 'enter' message if we're
            waiting for the 'exit' timer to fire.
    
            Also fix an assertion in the UI process caused by sending mismatched
            'enter' and 'leave' messages by keeping track of which messages
            we've sent.
    
            * WebProcess/WebPage/DrawingAreaImpl.cpp:
            (WebKit::DrawingAreaImpl::DrawingAreaImpl):
            (WebKit::DrawingAreaImpl::layerHostDidFlushLayers):
            (WebKit::DrawingAreaImpl::exitAcceleratedCompositingMode):
            (WebKit::DrawingAreaImpl::exitAcceleratedCompositingModeSoon):
            * WebProcess/WebPage/DrawingAreaImpl.h:
            (WebKit::DrawingAreaImpl::exitAcceleratedCompositingModePending):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85780 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -60,6 +60,7 @@ DrawingAreaImpl::DrawingAreaImpl(WebPage* webPage, const WebPageCreationParamete
     , m_inUpdateBackingStoreState(false)
     , m_shouldSendDidUpdateBackingStoreState(false)
     , m_isWaitingForDidUpdate(false)
+    , m_compositingAccordingToProxyMessages(false)
     , m_isPaintingSuspended(!parameters.isVisible)
     , m_alwaysUseCompositing(false)
     , m_lastDisplayTime(0)
@@ -215,7 +216,11 @@ void DrawingAreaImpl::layerHostDidFlushLayers()
     }
 
 #if USE(ACCELERATED_COMPOSITING)
-    m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(m_backingStoreStateID, m_layerTreeHost->layerTreeContext()));
+    ASSERT(!m_compositingAccordingToProxyMessages);
+    if (!exitAcceleratedCompositingModePending()) {
+        m_webPage->send(Messages::DrawingAreaProxy::EnterAcceleratedCompositingMode(m_backingStoreStateID, m_layerTreeHost->layerTreeContext()));
+        m_compositingAccordingToProxyMessages = true;
+    }
 #endif
 }
 
@@ -438,14 +443,17 @@ void DrawingAreaImpl::exitAcceleratedCompositingMode()
     } else {
         // Send along a complete update of the page so we can paint the contents right after we exit the
         // accelerated compositing mode, eliminiating flicker.
-        m_webPage->send(Messages::DrawingAreaProxy::ExitAcceleratedCompositingMode(m_backingStoreStateID, updateInfo));
+        if (m_compositingAccordingToProxyMessages) {
+            m_webPage->send(Messages::DrawingAreaProxy::ExitAcceleratedCompositingMode(m_backingStoreStateID, updateInfo));
+            m_compositingAccordingToProxyMessages = false;
+        }
     }
 #endif
 }
 
 void DrawingAreaImpl::exitAcceleratedCompositingModeSoon()
 {
-    if (m_exitCompositingTimer.isActive())
+    if (exitAcceleratedCompositingModePending())
         return;
 
     m_exitCompositingTimer.startOneShot(0);
