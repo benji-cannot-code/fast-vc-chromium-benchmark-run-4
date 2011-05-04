9b38d651f70b95aaa61b35ba103f7cef23a8cfeb
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-04 23:50:55 +0000

    Options: Clean up the sync setup overlay, part 2. * Use camelCase for i18n values.
    BUG=none
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/6927035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84158 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/sync_setup_overlay.js
+++ chrome/browser/resources/options/sync_setup_overlay.js
@@ -633,7 +633,7 @@ cr.define('options', function() {
     },
 
     showSuccessAndSettingUp_: function() {
-      $('sign-in').value = localStrings.getString('settingup');
+      $('sign-in').value = localStrings.getString('settingUp');
     },
 
     /** @inheritDoc */
