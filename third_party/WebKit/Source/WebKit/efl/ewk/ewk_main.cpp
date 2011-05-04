19312283b5d2052836e5354f0d672d8a6272fc6a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-04 23:34:06 +0000

    2011-05-04  Tomasz Morawski  <t.morawski@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Added error check for _ewk_init_body result.
            https://bugs.webkit.org/show_bug.cgi?id=60149
    
            The ewk_init function should be failed when _ewk_init_body function
            return false.
    
            * ewk/ewk_main.cpp:
            (ewk_init):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_main.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_main.cpp
@@ -110,7 +110,10 @@ int ewk_init(void)
         goto error_edje;
     }
 
-    _ewk_init_body();
+    if (!_ewk_init_body()) {
+        CRITICAL("could not init body");
+        goto error_edje;
+    }
 
     return ++_ewk_init_count;
 
