c0ec55a6f0ca45846cb0be6a711f22ca27dc6e0f
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 22:35:53 +0000

    Revert "wstring: remove wstring version of SplitString"
    This reverts commit r84336.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84339 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_split.cc
+++ base/string_split.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,12 +36,20 @@ static void SplitStringT(const STR& str,
   }
 }
 
+void SplitString(const std::wstring& str,
+                 wchar_t c,
+                 std::vector<std::wstring>* r) {
+  SplitStringT(str, c, true, r);
+}
+
+#if !defined(WCHAR_T_IS_UTF16)
 void SplitString(const string16& str,
                  char16 c,
                  std::vector<string16>* r) {
   DCHECK(CBU16_IS_SINGLE(c));
   SplitStringT(str, c, true, r);
 }
+#endif
 
 void SplitString(const std::string& str,
                  char c,
