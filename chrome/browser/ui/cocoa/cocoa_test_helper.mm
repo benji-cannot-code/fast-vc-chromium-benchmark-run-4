ce84a3d5db26e22e283fe890555c353818c11547
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 19:09:22 +0000

    Mac: Remove code that was rendered obsolete by http://crrev.com/73581
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6932024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84278 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/cocoa_test_helper.mm
+++ chrome/browser/ui/cocoa/cocoa_test_helper.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "base/test/test_timeouts.h"
-#import "chrome/browser/chrome_browser_application_mac.h"
 
 @implementation CocoaTestHelperWindow
 
@@ -82,9 +81,6 @@ void CocoaTest::BootstrapCocoa() {
   PathService::Get(base::DIR_EXE, &path);
   path = path.Append(chrome::kFrameworkName);
   base::mac::SetOverrideAppBundlePath(path);
-
-  // Bootstrap Cocoa. It's very unhappy without this.
-  [CrApplication sharedApplication];
 }
 
 void CocoaTest::TearDown() {
