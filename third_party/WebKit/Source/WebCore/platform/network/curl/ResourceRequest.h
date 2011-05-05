6c720a3b57f9d4ba6fac1cf158e9c415827b7477
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-05 17:54:15 +0000

    [WinCairo] unreviewed build correction.
    * platform/network/curl/ResourceRequest.h:
    Stub out the new pipelined http logic for cURL.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/curl/ResourceRequest.h
+++ third_party/WebKit/Source/WebCore/platform/network/curl/ResourceRequest.h
@@ -65,6 +65,10 @@ namespace WebCore {
         // Needed for compatibility.
         CFURLRequestRef cfURLRequest() const { return 0; }
 
+        // The following two stubs are for compatibility with CFNetwork, and are not used.
+        static bool httpPipeliningEnabled() { return false; }
+        static void setHTTPPipeliningEnabled(bool) { }
+
     private:
         friend class ResourceRequestBase;
 
@@ -73,6 +77,8 @@ namespace WebCore {
 
         PassOwnPtr<CrossThreadResourceRequestData> doPlatformCopyData(PassOwnPtr<CrossThreadResourceRequestData> data) const { return data; }
         void doPlatformAdopt(PassOwnPtr<CrossThreadResourceRequestData>) { }
+ 
+        static bool s_httpPipeliningEnabled;
     };
 
     struct CrossThreadResourceRequestData : public CrossThreadResourceRequestDataBase {
