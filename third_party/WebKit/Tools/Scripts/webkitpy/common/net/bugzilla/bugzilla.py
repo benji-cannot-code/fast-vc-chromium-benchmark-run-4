6b035b1511a92edd85ef05c076a15b72c8da3acd
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-05 20:18:40 +0000

    2011-05-05  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            commit-queue should reject patches which fail ewses
            https://bugs.webkit.org/show_bug.cgi?id=47534
    
            * Scripts/webkitpy/common/net/bugzilla/bugzilla.py:
            * Scripts/webkitpy/tool/commands/earlywarningsystem.py:
            * Scripts/webkitpy/tool/commands/earlywarningsystem_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/bugzilla/bugzilla.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/bugzilla/bugzilla.py
@@ -617,6 +617,7 @@ class Bugzilla(object):
 
         self.authenticate()
 
+        # FIXME: additional_comment_text seems useless and should be merged into comment-text.
         if additional_comment_text:
             comment_text += "\n\n%s" % additional_comment_text
         log(comment_text)
@@ -660,8 +661,7 @@ class Bugzilla(object):
     def add_cc_to_bug(self, bug_id, email_address_list):
         self.authenticate()
 
-        log("Adding %s to the CC list for bug %s" % (email_address_list,
-                                                     bug_id))
+        log("Adding %s to the CC list for bug %s" % (email_address_list, bug_id))
         if self.dryrun:
             return
 
