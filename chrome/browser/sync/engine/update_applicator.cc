0895668783dea7c9fd134d200229eb10cfab0faa
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 01:17:58 +0000

    Fix clang breakage from r84177
    TBR=tim@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/update_applicator.cc
+++ chrome/browser/sync/engine/update_applicator.cc
@@ -131,6 +131,9 @@ UpdateApplicator::ResultTracker::ResultTracker(size_t num_results) {
   successful_ids_.reserve(num_results);
 }
 
+UpdateApplicator::ResultTracker::~ResultTracker() {
+}
+
 void UpdateApplicator::ResultTracker::AddConflict(syncable::Id id) {
   conflicting_ids_.push_back(id);
 }
