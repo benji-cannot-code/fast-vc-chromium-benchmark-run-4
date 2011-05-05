74229dcf6d3c8679df19ba9b936ae72db5317dc6
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 22:15:17 +0000

    [Sync] Mark session encryption/passphrase integration tests as flaky.
    BUG=81688
    TBR=rsimha@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/two_client_live_sessions_sync_test.cc
+++ chrome/test/live_sync/two_client_live_sessions_sync_test.cc
@@ -116,8 +116,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest, BothChanged) {
   ASSERT_TRUE(WindowsMatch(sessions0[0]->windows, *client1_windows));
 }
 
+// Flaky. http://crbug.com/81688
 IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
-                       FirstChangesAndSetsPassphrase) {
+                       FLAKY_FirstChangesAndSetsPassphrase) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
@@ -158,8 +159,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
   ASSERT_TRUE(WindowsMatch(sessions1[0]->windows, *client0_windows));
 }
 
+// Flaky. http://crbug.com/81688
 IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
-                       FirstChangesWhileSecondWaitingForPassphrase) {
+                       FLAKY_FirstChangesWhileSecondWaitingForPassphrase) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
@@ -206,8 +208,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
   ASSERT_TRUE(WindowsMatch(sessions1[0]->windows, *client0_windows));
 }
 
+// Flaky. http://crbug.com/81688
 IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
-                       SecondChangesAfterEncrAndPassphraseChange) {
+                       FLAKY_SecondChangesAfterEncrAndPassphraseChange) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
@@ -256,8 +259,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
   ASSERT_FALSE(GetSessionData(1, &sessions1));
 }
 
+// Flaky. http://crbug.com/81688
 IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
-                       SecondChangesBeforeEncrAndPassphraseChange) {
+                       FLAKY_SecondChangesBeforeEncrAndPassphraseChange) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
