e55fc741ed4281e164652c5ba86967c38ef556ec
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-05 03:07:02 +0000

    2011-05-04  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Eric Seidel.
    
            Make more member functions in EventHandler private
            https://bugs.webkit.org/show_bug.cgi?id=60200
    
            Reduced the number of public member functions in EventHandler.
    
            * WebCore.exp.in:
            * page/EventHandler.cpp:
            (WebCore::EventHandler::handleAutoscroll):
            (WebCore::EventHandler::autoscrollTimerFired):
            (WebCore::EventHandler::stopAutoscrollTimer):
            (WebCore::EventHandler::handleMousePressEvent):
            (WebCore::EventHandler::handleMouseMoveEvent):
            (WebCore::EventHandler::keyEvent):
            (WebCore::EventHandler::eventInvertsTabsToLinksClientCallResult):
            * page/EventHandler.h:
            (WebCore::EventHandler::mouseDownMayStartSelect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -775,7 +775,7 @@ void EventHandler::handleAutoscroll(RenderObject* renderer)
         if (Page* page = m_frame->page()) {
             Frame* mainFrame = page->mainFrame();
             if (m_frame != mainFrame)
-                mainFrame->eventHandler()->setPanScrollInProgress(true);
+                mainFrame->eventHandler()->m_panScrollInProgress = true;
         }
     }
 #endif
@@ -800,7 +800,7 @@ void EventHandler::autoscrollTimerFired(Timer<EventHandler>*)
     } else {
         // we verify that the main frame hasn't received the order to stop the panScroll
         if (Page* page = m_frame->page()) {
-            if (!page->mainFrame()->eventHandler()->panScrollInProgress()) {
+            if (!page->mainFrame()->eventHandler()->m_panScrollInProgress) {
                 stopAutoscrollTimer();
                 return;
             }
@@ -1005,7 +1005,7 @@ void EventHandler::stopAutoscrollTimer(bool rendererIsBeingDestroyed)
     if (Page* page = m_frame->page()) {
         Frame* mainFrame = page->mainFrame();
         if (m_frame != mainFrame)
-            mainFrame->eventHandler()->setPanScrollInProgress(false);
+            mainFrame->eventHandler()->m_panScrollInProgress = false;
     }
 
     m_autoscrollInProgress = false;
@@ -1394,7 +1394,7 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
 #if ENABLE(PAN_SCROLLING)
     // We store whether pan scrolling is in progress before calling stopAutoscrollTimer()
     // because it will set m_panScrollInProgress to false on return.
-    bool isPanScrollInProgress = m_frame->page() && m_frame->page()->mainFrame()->eventHandler()->panScrollInProgress();
+    bool isPanScrollInProgress = m_frame->page() && m_frame->page()->mainFrame()->eventHandler()->m_panScrollInProgress;
     if (isPanScrollInProgress || m_autoscrollInProgress)
         stopAutoscrollTimer();
     if (isPanScrollInProgress) {
@@ -1627,7 +1627,7 @@ bool EventHandler::handleMouseMoveEvent(const PlatformMouseEvent& mouseEvent, Hi
         if (scrollbar && !m_mousePressed)
             scrollbar->mouseMoved(mouseEvent); // Handle hover effects on platforms that support visual feedback on scrollbar hovering.
         if (Page* page = m_frame->page()) {
-            if ((!m_resizeLayer || !m_resizeLayer->inResizeMode()) && !page->mainFrame()->eventHandler()->panScrollInProgress()) {
+            if ((!m_resizeLayer || !m_resizeLayer->inResizeMode()) && !page->mainFrame()->eventHandler()->m_panScrollInProgress) {
                 // Plugins set cursor on their own. The only case WebKit intervenes is resetting cursor to arrow on mouse enter,
                 // in case the particular plugin doesn't manipulate cursor at all. Thus,  even a CSS cursor set on body has no
                 // effect on plugins (which matches Firefox).
@@ -2429,7 +2429,7 @@ bool EventHandler::keyEvent(const PlatformKeyboardEvent& initialKeyEvent)
 
 #if ENABLE(PAN_SCROLLING)
     if (Page* page = m_frame->page()) {
-        if (page->mainFrame()->eventHandler()->panScrollInProgress() || m_autoscrollInProgress) {
+        if (page->mainFrame()->eventHandler()->m_panScrollInProgress || m_autoscrollInProgress) {
             // If a key is pressed while the autoscroll/panScroll is in progress then we want to stop
             if (initialKeyEvent.type() == PlatformKeyboardEvent::KeyDown || initialKeyEvent.type() == PlatformKeyboardEvent::RawKeyDown) 
                 stopAutoscrollTimer();
@@ -2842,7 +2842,7 @@ bool EventHandler::isKeyboardOptionTab(KeyboardEvent* event)
         && event->keyIdentifier() == "U+0009";    
 }
 
-static bool eventInvertsTabsToLinksClientCallResult(KeyboardEvent* event)
+bool EventHandler::eventInvertsTabsToLinksClientCallResult(KeyboardEvent* event)
 {
 #if PLATFORM(MAC) || PLATFORM(QT) || PLATFORM(HAIKU) || PLATFORM(EFL)
     return EventHandler::isKeyboardOptionTab(event);
--- third_party/WebKit/Source/WebCore/page/EventHandler.h
+++ third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -107,8 +107,6 @@ public:
     void setMousePressNode(PassRefPtr<Node>);
 
     void startPanScrolling(RenderObject*);
-    bool panScrollInProgress() { return m_panScrollInProgress; }
-    void setPanScrollInProgress(bool inProgress) { m_panScrollInProgress = inProgress; }
 
     void stopAutoscrollTimer(bool rendererIsBeingDestroyed = false);
     RenderObject* autoscrollRenderer() const;
@@ -141,14 +139,10 @@ public:
 
     IntPoint currentMousePosition() const;
 
-    void setIgnoreWheelEvents(bool);
-
     static Frame* subframeForTargetNode(Node*);
     static Frame* subframeForHitTestResult(const MouseEventWithHitTestResults&);
 
     bool scrollOverflow(ScrollDirection, ScrollGranularity, Node* startingNode = 0);
-    bool logicalScrollOverflow(ScrollLogicalDirection, ScrollGranularity, Node* startingNode = 0);
-
     bool scrollRecursively(ScrollDirection, ScrollGranularity, Node* startingNode = 0);
     bool logicalScrollRecursively(ScrollLogicalDirection, ScrollGranularity, Node* startingNode = 0);
 
@@ -156,12 +150,8 @@ public:
     bool shouldDragAutoNode(Node*, const IntPoint&) const; // -webkit-user-drag == auto
 #endif
 
-    bool shouldTurnVerticalTicksIntoHorizontal(const HitTestResult&) const;
-
     bool tabsToLinks(KeyboardEvent*) const;
     bool tabsToAllFormControls(KeyboardEvent*) const;
-
-    bool mouseDownMayStartSelect() const { return m_mouseDownMayStartSelect; }
 
     bool mouseMoved(const PlatformMouseEvent&);
 
@@ -202,11 +192,11 @@ public:
 
     void focusDocumentView();
 
-    void capsLockStateMayHaveChanged();
+    void capsLockStateMayHaveChanged(); // Only called by SelectionController
     
-    void sendResizeEvent();
-    void sendScrollEvent();
-    
+    void sendResizeEvent(); // Only called in FrameView
+    void sendScrollEvent(); // Ditto
+
 #if PLATFORM(MAC) && defined(__OBJC__)
     void mouseDown(NSEvent *);
     void mouseDragged(NSEvent *);
@@ -225,8 +215,6 @@ public:
 #if ENABLE(TOUCH_EVENTS)
     bool handleTouchEvent(const PlatformTouchEvent&);
 #endif
-
-    static bool isKeyboardOptionTab(KeyboardEvent*);
 
 private:
 #if ENABLE(DRAG_SUPPORT)
@@ -293,6 +281,13 @@ private:
     void startAutoscrollTimer();
     void setAutoscrollRenderer(RenderObject*);
     void autoscrollTimerFired(Timer<EventHandler>*);
+    bool logicalScrollOverflow(ScrollLogicalDirection, ScrollGranularity, Node* startingNode = 0);
+    
+    bool shouldTurnVerticalTicksIntoHorizontal(const HitTestResult&) const;
+    bool mouseDownMayStartSelect() const { return m_mouseDownMayStartSelect; }
+
+    static bool isKeyboardOptionTab(KeyboardEvent*);
+    static bool eventInvertsTabsToLinksClientCallResult(KeyboardEvent*);
 
     void fakeMouseMoveEventTimerFired(Timer<EventHandler>*);
     void cancelFakeMouseMoveEvent();
