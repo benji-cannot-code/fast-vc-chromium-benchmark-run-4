9316b42a405f1add3bf53c3aa6d2069c56c16e62
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 22:48:21 +0000

    Remove old Heap Profiler interactive test.
    R=pfeldman@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6935027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -50,7 +50,6 @@ class BrowserClosedObserver : public NotificationObserver {
 const int kActionDelayMs = 500;
 
 const char kDebuggerTestPage[] = "files/devtools/debugger_test_page.html";
-const char kHeapProfilerPage[] = "files/devtools/heap_profiler.html";
 const char kPauseWhenLoadingDevTools[] =
     "files/devtools/pause_when_loading_devtools.html";
 const char kPauseWhenScriptIsRunning[] =
@@ -246,11 +245,6 @@ class DevToolsExtensionDebugTest : public DevToolsSanityTest,
 
   FilePath test_extensions_dir_;
 };
-
-// Tests heap profiler.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, FAILS_TestHeapProfiler) {
-  RunTest("testHeapProfiler", kHeapProfilerPage);
-}
 
 // Tests scripts panel showing.
 IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestShowScriptsTab) {
