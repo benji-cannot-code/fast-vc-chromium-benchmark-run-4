d3fd24f688c2cb42359fd879a4d13d154c01bb33
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-05 16:40:14 +0000

    2011-05-05  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Return correct GTK version for NPNVToolkit
            https://bugs.webkit.org/show_bug.cgi?id=60275
    
            * WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp:
            (WebKit::NPN_GetValue): Return always 2 for NPNVToolkit in GTK
            platform.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85859 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapeBrowserFuncs.cpp
@@ -509,6 +509,9 @@ static NPError NPN_GetValue(NPP npp, NPNVariable variable, void *value)
            break;
 
        case NPNVToolkit: {
+#if PLATFORM(GTK)
+           *reinterpret_cast<uint32_t*>(value) = 2;
+#else
            const uint32_t expectedGTKToolKitVersion = 2;
 
            // Set the expected GTK version if we know that this plugin needs it or if the plugin call us
@@ -522,6 +525,7 @@ static NPError NPN_GetValue(NPP npp, NPNVariable variable, void *value)
            }
 
            *reinterpret_cast<uint32_t*>(value) = requiresGTKToolKitVersion ? expectedGTKToolKitVersion : 0;
+#endif
            break;
        }
 
