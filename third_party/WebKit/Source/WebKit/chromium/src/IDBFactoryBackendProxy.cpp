11c71e4c704f4d5208e87d585eab49beec2982d8
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-06 19:09:31 +0000

    2011-05-06  John Abd-El-Malek  <jam@chromium.org>
            Reviewed by James Robinson.
    
            [chromium]: Reland using WebPermissionClient for checking indexed db usage
            https://bugs.webkit.org/show_bug.cgi?id=60386
    
            * public/WebPermissionClient.h:
            (WebKit::WebPermissionClient::allowIndexedDB):
            * src/IDBFactoryBackendProxy.cpp:
            (WebKit::IDBFactoryBackendProxy::open):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/IDBFactoryBackendProxy.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/IDBFactoryBackendProxy.cpp
@@ -43,7 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebIDBFactory.h"
 #include "WebKit.h"
 #include "WebKitClient.h"
+#include "WebPermissionClient.h"
 #include "WebVector.h"
+#include "WebViewImpl.h"
 
 using namespace WebCore;
 
@@ -63,9 +65,16 @@ IDBFactoryBackendProxy::~IDBFactoryBackendProxy()
 {
 }
 
-void IDBFactoryBackendProxy::open(const String& name, PassRefPtr<IDBCallbacks> callbacks, PassRefPtr<SecurityOrigin> origin, Frame* frame, const String& dataDir, int64_t maximumSize, BackingStoreType backingStoreType)
+void IDBFactoryBackendProxy::open(const String& name, PassRefPtr<IDBCallbacks> callbacks, PassRefPtr<SecurityOrigin> prpOrigin, Frame* frame, const String& dataDir, int64_t maximumSize, BackingStoreType backingStoreType)
 {
-    WebFrame* webFrame = WebFrameImpl::fromFrame(frame);
+    WebSecurityOrigin origin(prpOrigin);
+    WebFrameImpl* webFrame = WebFrameImpl::fromFrame(frame);
+    WebViewImpl* webView = webFrame->viewImpl();
+    if (webView->permissionClient() && !webView->permissionClient()->allowIndexedDB(webFrame, name, origin)) {
+        callbacks->onError(WebIDBDatabaseError(0, "The user denied permission to access the database."));
+        return;
+    }
+
     m_webIDBFactory->open(name, new WebIDBCallbacksImpl(callbacks), origin, webFrame, dataDir, maximumSize, static_cast<WebIDBFactory::BackingStoreType>(backingStoreType));
 }
 
