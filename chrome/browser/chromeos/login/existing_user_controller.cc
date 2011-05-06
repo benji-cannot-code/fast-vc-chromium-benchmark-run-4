21ba02117f7994ef21232794e15d5bd906fbccea
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 06:37:59 +0000

    Close throbber when the browser was launched.
    BUG=chromium-os:14939
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6927042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84407 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -369,6 +369,7 @@ void ExistingUserController::OnProfilePrepared(Profile* profile) {
     LoginUtils::DoBrowserLaunch(profile, host_);
     host_ = NULL;
   }
+  login_display_->OnFadeOut();
 }
 
 void ExistingUserController::OnOffTheRecordLoginSuccess() {
@@ -452,7 +453,6 @@ void ExistingUserController::ActivateWizard(const std::string& screen_name) {
   if (chromeos::UserManager::Get()->IsLoggedInAsGuest())
     start_url = guest_mode_url_;
   host_->StartWizard(screen_name, start_url);
-  login_display_->OnFadeOut();
 }
 
 gfx::NativeWindow ExistingUserController::GetNativeWindow() const {
