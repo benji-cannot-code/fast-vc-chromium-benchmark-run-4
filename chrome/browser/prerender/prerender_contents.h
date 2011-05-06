9af6a71875e19faa983039767d895a65eb5995cd
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 03:15:53 +0000

    Fix the PrerenderBrowserTest.PrerenderPopup test to work when UseTabContents returns true.
    BUG=81229
    TEST=PrerenderBrowserTest.PrerenderPopup
    
    
    Review URL: http://codereview.chromium.org/6928030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_contents.h
+++ chrome/browser/prerender/prerender_contents.h
@@ -136,7 +136,6 @@ class PrerenderContents : public RenderViewHostDelegate,
                               std::wstring* prompt_field);
   virtual void OnRenderViewGone(int status, int exit_code);
 
-
   // RenderViewHostDelegate implementation.
   // TODO(dominich): Remove when RenderViewHostDelegate is removed as a base
   // class.
@@ -158,6 +157,7 @@ class PrerenderContents : public RenderViewHostDelegate,
   virtual void DidStopLoading() OVERRIDE;
 
   // RenderViewHostDelegate::View
+  // TODO(dominich): Remove when no longer a delegate for the view.
   virtual void CreateNewWindow(
       int route_id,
       const ViewHostMsg_CreateWindow_Params& params) OVERRIDE;
