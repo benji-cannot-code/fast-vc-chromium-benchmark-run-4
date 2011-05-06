772edb904542e1271fe8736291f244594af5c2ec
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-06 14:46:49 +0000

    2011-05-06  Alexis Menard  <alexis.menard@openbossa.org>
            Unreviewed build fix with gcc 4.6.0 on linux and c++0x support.
    
            std::tr1::has_trivial_constructor is in <tr1/memory>.
    
            * wtf/TypeTraits.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/TypeTraits.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/TypeTraits.h
@@ -27,6 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if (defined(__GLIBCXX__) && (__GLIBCXX__ >= 20070724) && defined(__GXX_EXPERIMENTAL_CXX0X__)) || (defined(_MSC_VER) && (_MSC_VER >= 1600))
 #include <type_traits>
+#if defined(__GXX_EXPERIMENTAL_CXX0X__)
+#include <tr1/memory>
+#endif
 #endif
 
 namespace WTF {
