01873646dfacc395f6c3aa2807bba04fac4e0a8d
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-06 18:48:03 +0000

    Mac build fix
    * WebProcess/Plugins/Netscape/mac/NetscapePluginMac.mm: Added a
    missing #import.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85962 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/mac/NetscapePluginMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/mac/NetscapePluginMac.mm
@@ -29,8 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "PluginController.h"
 #import "WebEvent.h"
-#import <WebCore/GraphicsContext.h>
 #import <Carbon/Carbon.h>
+#import <WebCore/GraphicsContext.h>
+#import <WebCore/NotImplemented.h>
 #import <WebKitSystemInterface.h>
 
 using namespace WebCore;
