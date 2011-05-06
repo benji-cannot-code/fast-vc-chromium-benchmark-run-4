75b7bee0f776422f2f4383bc11d802b2060d4f82
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-06 16:55:47 +0000

    2011-05-06  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Implement NetscapePlugin::platformPaint for GTK platform
            https://bugs.webkit.org/show_bug.cgi?id=60368
    
            * WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp:
            (WebKit::NetscapePlugin::platformPaint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@85953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
@@ -40,6 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QPixmap>
 #include <QX11Info>
 #elif PLATFORM(GTK)
+#include "PlatformContextCairo.h"
+#include "RefPtrCairo.h"
+#include <cairo/cairo-xlib.h>
 #include <gdk/gdkx.h>
 #include <WebCore/GtkVersioning.h>
 #endif
@@ -214,7 +217,7 @@ void NetscapePlugin::platformPaint(GraphicsContext* context, const IntRect& dirt
 #if PLATFORM(QT)
     QPainter* painter = context->platformContext();
     painter->translate(m_frameRect.x(), m_frameRect.y());
-#else
+#elif !PLATFORM(GTK)
     notImplemented();
     return;
 #endif
@@ -248,6 +251,23 @@ void NetscapePlugin::platformPaint(GraphicsContext* context, const IntRect& dirt
     painter->drawPixmap(QPoint(exposedRect.x(), exposedRect.y()), qtDrawable, exposedRect);
 
     painter->translate(-m_frameRect.x(), -m_frameRect.y());
+#elif PLATFORM(GTK)
+    RefPtr<cairo_surface_t> drawableSurface = adoptRef(cairo_xlib_surface_create(m_pluginDisplay,
+                                                                                 m_drawable,
+                                                                                 static_cast<NPSetWindowCallbackStruct*>(m_npWindow.ws_info)->visual,
+                                                                                 m_frameRect.width(),
+                                                                                 m_frameRect.height()));
+    cairo_t* cr = context->platformContext()->cr();
+    cairo_save(cr);
+
+    cairo_set_source_surface(cr, drawableSurface.get(), m_frameRect.x(), m_frameRect.y());
+
+    cairo_rectangle(cr, m_frameRect.x() + exposedRect.x(), m_frameRect.y() + exposedRect.y(), exposedRect.width(), exposedRect.height());
+    cairo_clip(cr);
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+    cairo_paint(cr);
+
+    cairo_restore(cr);
 #endif
 }
 
