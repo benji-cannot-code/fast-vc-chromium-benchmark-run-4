c5b32a0add38a627ad53d8f8a9571a2bf7b6c60f
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 04:06:40 +0000

    Printing: Notify the browser process of renderer print failures. (try 2)
    Original code review: http://codereview.chromium.org/6883278
    
    BUG=none
    TEST=included
    TBR=kmadhusu
    Review URL: http://codereview.chromium.org/6932061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -120,7 +120,8 @@ PrintWebViewHelper::PrintWebViewHelper(RenderView* render_view)
       print_web_view_(NULL),
       script_initiated_preview_frame_(NULL),
       context_menu_preview_node_(NULL),
-      user_cancelled_scripted_print_count_(0) {
+      user_cancelled_scripted_print_count_(0),
+      notify_browser_of_print_failure_(true) {
   is_preview_ = CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnablePrintPreview);
 }
@@ -241,6 +242,7 @@ void PrintWebViewHelper::OnPrintPreview(const DictionaryValue& settings) {
 }
 
 void PrintWebViewHelper::OnPrintingDone(bool success) {
+  notify_browser_of_print_failure_ = false;
   DidFinishPrinting(success ? OK : FAIL_PRINT);
 }
 
@@ -324,6 +326,7 @@ void PrintWebViewHelper::PrintPreview(WebKit::WebFrame* frame,
 }
 
 void PrintWebViewHelper::DidFinishPrinting(PrintingResult result) {
+  int cookie = print_pages_params_->params.document_cookie;
   if (result == FAIL_PRINT) {
     WebView* web_view = print_web_view_;
     if (!web_view)
@@ -332,8 +335,10 @@ void PrintWebViewHelper::DidFinishPrinting(PrintingResult result) {
     render_view()->runModalAlertDialog(
         web_view->mainFrame(),
         l10n_util::GetStringUTF16(IDS_PRINT_SPOOL_FAILED_ERROR_TEXT));
+
+    if (notify_browser_of_print_failure_)
+      Send(new PrintHostMsg_PrintingFailed(routing_id(), cookie));
   } else if (result == FAIL_PREVIEW) {
-    int cookie = print_pages_params_->params.document_cookie;
     Send(new PrintHostMsg_PrintPreviewFailed(routing_id(), cookie));
   }
 
@@ -342,6 +347,7 @@ void PrintWebViewHelper::DidFinishPrinting(PrintingResult result) {
     print_web_view_ = NULL;
   }
   print_pages_params_.reset();
+  notify_browser_of_print_failure_ = true;
 }
 
 bool PrintWebViewHelper::CopyAndPrint(WebKit::WebFrame* web_frame) {
