1d63e708673bc1dd361f565593ce7a3358b3e2a9
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 20:57:25 +0000

    Add validation support for Google Update's usagestats value.
    BUG=none
    TEST=Check logs after ordinary installation runs to be sure no errors are reported.  Optionally run validate_installation.exe manually and see what it reports.
    
    Review URL: http://codereview.chromium.org/6930036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/installation_validator.h
+++ chrome/installer/util/installation_validator.h
@@ -96,6 +96,8 @@ class InstallationValidator {
         bool system_install,
         const ProductState& product_state,
         SwitchExpectations* expectations) const = 0;
+    // Return true if the rules allow usagestats setting.
+    virtual bool UsageStatsAllowed(const ProductState& product_state) const = 0;
   };
 
   // Validation rules for the Chrome browser.
@@ -107,6 +109,8 @@ class InstallationValidator {
         bool system_install,
         const ProductState& product_state,
         SwitchExpectations* expectations) const OVERRIDE;
+    virtual bool UsageStatsAllowed(
+        const ProductState& product_state) const OVERRIDE;
   };
 
   // Validation rules for Chrome Frame.
@@ -118,6 +122,8 @@ class InstallationValidator {
         bool system_install,
         const ProductState& product_state,
         SwitchExpectations* expectations) const OVERRIDE;
+    virtual bool UsageStatsAllowed(
+        const ProductState& product_state) const OVERRIDE;
   };
 
   // Validation rules for the multi-install Chrome binaries.
@@ -129,6 +135,8 @@ class InstallationValidator {
         bool system_install,
         const ProductState& product_state,
         SwitchExpectations* expectations) const OVERRIDE;
+    virtual bool UsageStatsAllowed(
+        const ProductState& product_state) const OVERRIDE;
   };
 
   struct ProductContext {
@@ -173,6 +181,8 @@ class InstallationValidator {
                                           bool* is_valid);
   static void ValidateAppCommands(const ProductContext& ctx,
                                   bool* is_valid);
+  static void ValidateUsageStats(const ProductContext& ctx,
+                                 bool* is_valid);
   static void ValidateProduct(const InstallationState& machine_state,
                               bool system_install,
                               const ProductState& product_state,
