04b837feafe03bd985b63480072543d6ab0e6fef
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-06 08:35:06 +0000

    Increase the size of stacks (--num-callers) being tracked by Memcheck to match the number used on bots.
    TBR=timurrrr
    Review URL: http://codereview.chromium.org/6927077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84412 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
 
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -28,7 +28,9 @@ setup_memcheck() {
   DEFAULT_TOOL_FLAGS=("--db-command=$GDB -nw %f %p" "--db-attach=yes" \
                       # Overwrite newly allocated or freed objects
                       # with 0x41 to catch inproper use.
-                      "--malloc-fill=41" "--free-fill=41")
+                      "--malloc-fill=41" "--free-fill=41" \
+                      # Increase the size of stacks being tracked.
+                      "--num-callers=30")
 }
 
 setup_tsan() {
