312c1f5043aae6c96a5547dd71c988ef54ab6a43
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-08 16:24:17 +0000

    Added enqueue task to media player. Fixed task ordering - the last used handler task comes first, all others are sorted alphabetically.
    BUG=chromium-os:15023
    TEST=make sure enqueue task shows up when media content is selected in file browser
    Review URL: http://codereview.chromium.org/6953007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1002,6 +1002,10 @@ FileManager.prototype = {
           task.iconUrl =
               chrome.extension.getURL('images/icon_play_16x16.png');
           task.title = str('PLAY_MEDIA').replace("&", "");
+        } else if (task_parts[1] == 'enqueue') {
+          task.iconUrl =
+              chrome.extension.getURL('images/icon_add_to_queue_16x16.png');
+          task.title = str('ENQUEUE');
         }
       }
 
@@ -1032,7 +1036,11 @@ FileManager.prototype = {
         g_slideshow_data = this.selection.urls;
         chrome.tabs.create({url: "slideshow.html"});
       } else if (task_parts[1] == 'play') {
-        chrome.fileBrowserPrivate.viewFiles(this.selection.urls);
+        chrome.fileBrowserPrivate.viewFiles(this.selection.urls,
+            event.srcElement.task.taskId);
+      } else if (task_parts[1] == 'enqueue') {
+        chrome.fileBrowserPrivate.viewFiles(this.selection.urls,
+            event.srcElement.task.taskId);
       }
       return;
     }
@@ -1754,7 +1762,7 @@ FileManager.prototype = {
 
     // In full screen mode, open all files for vieweing.
     if (this.dialogType_ == FileManager.DialogType.FULL_PAGE) {
-      chrome.fileBrowserPrivate.viewFiles(ary);
+      chrome.fileBrowserPrivate.viewFiles(ary, "default");
       // Window stays open.
       return;
     }
