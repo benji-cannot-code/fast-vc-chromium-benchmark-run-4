f336fe71e5170611f1ce57e502fb81709755fedb
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-08 00:58:25 +0000

    2011-05-07  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r85974.
            http://trac.webkit.org/changeset/85974
            https://bugs.webkit.org/show_bug.cgi?id=60444
    
            broke browser tests (Requested by inferno-sec on #webkit).
    
            * src/WebURLResponse.cpp:
            (WebKit::WebURLResponse::downloadFilePath):
            (WebKit::WebURLResponse::setDownloadFilePath):
    2011-05-07  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r85974.
            http://trac.webkit.org/changeset/85974
            https://bugs.webkit.org/show_bug.cgi?id=60444
    
            broke browser tests (Requested by inferno-sec on #webkit).
    
            * platform/network/chromium/ResourceResponse.cpp:
            (WebCore::ResourceResponse::doPlatformCopyData):
            (WebCore::ResourceResponse::doPlatformAdopt):
            * platform/network/chromium/ResourceResponse.h:
            (WebCore::ResourceResponse::downloadFilePath):
            (WebCore::ResourceResponse::setDownloadFilePath):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceResponse.h
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceResponse.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ResourceResponse_h
 #define ResourceResponse_h
 
-#include "File.h"
 #include "NotImplemented.h"
 #include "ResourceResponseBase.h"
 #include <wtf/text/CString.h>
@@ -101,8 +100,8 @@ namespace WebCore {
         unsigned short remotePort() const { return m_remotePort; }
         void setRemotePort(unsigned short value) { m_remotePort = value; }
 
-        const File* downloadedFile() const { return m_downloadedFile.get(); }
-        void setDownloadedFile(PassRefPtr<File> downloadedFile) { m_downloadedFile = downloadedFile; }
+        const String& downloadFilePath() const { return m_downloadFilePath; }
+        void setDownloadFilePath(const String& downloadFilePath) { m_downloadFilePath = downloadFilePath; }
 
     private:
         friend class ResourceResponseBase;
@@ -154,8 +153,8 @@ namespace WebCore {
         // Remote port number of the socket which fetched this resource.
         unsigned short m_remotePort;
 
-        // The downloaded file if the load streamed to a file.
-        RefPtr<File> m_downloadedFile;
+        // The path to the downloaded file.
+        String m_downloadFilePath;
     };
 
     struct CrossThreadResourceResponseData : public CrossThreadResourceResponseDataBase {
