a2566c4d88a9ebebe29272296682573f8d3ef08e
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 23:18:55 +0000

    WebUI: Don't attempt to activate an invalid index (-1).
    BUG=none
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/6981008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/list.js
+++ chrome/browser/resources/shared/js/cr/ui/list.js
@@ -398,8 +398,8 @@ cr.define('cr.ui', function() {
         return;
 
       var target = this.getListItemAncestor(e.target);
-      var index = target ? this.getIndexOfListItem(target) : -1;
-      this.activateItemAtIndex(index);
+      if (target)
+        this.activateItemAtIndex(this.getIndexOfListItem(target));
     },
 
     /**
