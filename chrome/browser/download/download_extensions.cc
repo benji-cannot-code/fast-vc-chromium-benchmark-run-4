9bb1f70a53b83df7c927d605c7c332a8f81b3e30
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 18:44:35 +0000

    Misc. tiny cleanups to downloads.  Reduce API surface, shorten code.  Only functional change should be that DownloadItem::Completed() now effectively DCHECK()s that |all_data_saved_| is true before returning, which I think is a correct assertion.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6935031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84650 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extensions.cc
+++ chrome/browser/download/download_extensions.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -188,11 +188,7 @@ static const struct Executables {
 };
 
 DownloadDangerLevel GetFileDangerLevel(const FilePath& path) {
-  return GetFileExtensionDangerLevel(path.Extension());
-}
-
-DownloadDangerLevel GetFileExtensionDangerLevel(
-    const FilePath::StringType& extension) {
+  FilePath::StringType extension(path.Extension());
   if (extension.empty())
     return NotDangerous;
   if (!IsStringASCII(extension))
@@ -212,10 +208,6 @@ DownloadDangerLevel GetFileExtensionDangerLevel(
       return g_executables[i].level;
   }
   return NotDangerous;
-}
-
-bool IsFileSafe(const FilePath& path) {
-  return GetFileDangerLevel(path) == NotDangerous;
 }
 
 static const char* kExecutableWhiteList[] = {
--- chrome/browser/download/download_extensions.h
+++ chrome/browser/download/download_extensions.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,13 +21,6 @@ enum DownloadDangerLevel {
 
 // Determine the download danger level of a file.
 DownloadDangerLevel GetFileDangerLevel(const FilePath& path);
-
-// Determine the download danger level using a file extension.
-DownloadDangerLevel GetFileExtensionDangerLevel(
-    const FilePath::StringType& extension);
-
-// True if the download danger level of the file is NotDangerous.
-bool IsFileSafe(const FilePath& path);
 
 // Tests if we think the server means for this mime_type to be executable.
 bool IsExecutableMimeType(const std::string& mime_type);
