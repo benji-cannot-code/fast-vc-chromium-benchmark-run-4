5e6cbf9c11d9f1990ccfc392876237cb00b2014f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 17:59:09 +0000

    Take out ResourceDispatcherHost::Observer since it's unused. Review URL: http://codereview.chromium.org/6960005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84642 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host.cc
+++ content/browser/renderer_host/resource_dispatcher_host.cc
@@ -1567,7 +1567,6 @@ void ResourceDispatcherHost::OnResponseCompleted(net::URLRequest* request) {
   if (info->resource_handler()->OnResponseCompleted(info->request_id(),
                                                     request->status(),
                                                     security_info)) {
-    NotifyResponseCompleted(request, info->child_id());
 
     // The request is complete so we can remove it.
     RemovePendingRequest(info->child_id(), info->request_id());
@@ -1624,14 +1623,6 @@ bool ResourceDispatcherHost::RenderViewForRequest(
   return true;
 }
 
-void ResourceDispatcherHost::AddObserver(Observer* obs) {
-  observer_list_.AddObserver(obs);
-}
-
-void ResourceDispatcherHost::RemoveObserver(Observer* obs) {
-  observer_list_.RemoveObserver(obs);
-}
-
 net::URLRequest* ResourceDispatcherHost::GetURLRequest(
     const GlobalRequestID& request_id) const {
   // This should be running in the IO loop.
@@ -1655,8 +1646,6 @@ static int GetCertID(net::URLRequest* request, int child_id) {
 void ResourceDispatcherHost::NotifyResponseStarted(net::URLRequest* request,
                                                    int child_id) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-  // Notify the observers on the IO thread.
-  FOR_EACH_OBSERVER(Observer, observer_list_, OnRequestStarted(this, request));
 
   int render_process_id, render_view_id;
   if (!RenderViewForRequest(request, &render_process_id, &render_view_id))
@@ -1673,20 +1662,9 @@ void ResourceDispatcherHost::NotifyResponseStarted(net::URLRequest* request,
           render_process_id, render_view_id, detail));
 }
 
-void ResourceDispatcherHost::NotifyResponseCompleted(net::URLRequest* request,
-                                                     int child_id) {
-  // Notify the observers on the IO thread.
-  FOR_EACH_OBSERVER(Observer, observer_list_,
-                    OnResponseCompleted(this, request));
-}
-
 void ResourceDispatcherHost::NotifyReceivedRedirect(net::URLRequest* request,
                                                     int child_id,
                                                     const GURL& new_url) {
-  // Notify the observers on the IO thread.
-  FOR_EACH_OBSERVER(Observer, observer_list_,
-                    OnReceivedRedirect(this, request, new_url));
-
   int render_process_id, render_view_id;
   if (!RenderViewForRequest(request, &render_process_id, &render_view_id))
     return;
