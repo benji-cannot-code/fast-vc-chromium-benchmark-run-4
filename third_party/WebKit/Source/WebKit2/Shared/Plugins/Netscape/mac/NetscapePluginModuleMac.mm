37e2ad74910a09e170a27917c720ccb0bd391f23
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-09 22:49:41 +0000

    2011-05-09  Anders Carlsson  <andersca@apple.com>
            Reviewed by Sam Weinig.
    
            Create a plug-in's MIME types preferences plist if it doesn't exist.
            https://bugs.webkit.org/show_bug.cgi?id=57204
            <rdar://problem/9195048>
    
            Spawn a plug-in process and let it create the plist. For now, we'll do this on the
            main thread since it's a rare one-time computation and we have an API function,
            WKFrameCanShowMIMEType, which would have to block anyway.
    
            * PluginProcess/mac/PluginProcessMainMac.mm:
            (WebKit::PluginProcessMain):
            If -createPluginMIMETypesPreferences is one of the command line argument keys,
            the plug-in path will be the argument value. Call NetscapePluginModule::createPluginMIMETypesPreferences
            to create the preferences file and then exit.
    
            * Shared/Plugins/Netscape/mac/NetscapePluginModuleMac.mm:
            (WebKit::contentsOfPropertyListAtURL):
            Move code to read a property list out into a helper function.
    
            (WebKit::getMIMETypesFromPluginBundle):
            If there's no property list file, try to create one.
    
            (WebKit::NetscapePluginModule::createPluginMIMETypesPreferences):
            Load the plug-in, find the BP_CreatePluginMIMETypesPreferences and call it.
    
            * UIProcess/Plugins/mac/PluginProcessProxyMac.mm:
            (WebKit::PluginProcessProxy::createPropertyListFile):
            Spawn a plug-in process and pass the -createPluginMIMETypesPreferences flag to it and then wait
            for it to exit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/Plugins/Netscape/mac/NetscapePluginModuleMac.mm
+++ third_party/WebKit/Source/WebKit2/Shared/Plugins/Netscape/mac/NetscapePluginModuleMac.mm
@@ -27,8 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "config.h"
 #import "NetscapePluginModule.h"
 
+#import "PluginProcessProxy.h"
 #import <WebCore/WebCoreNSStringExtras.h>
 #import <wtf/HashSet.h>
+
 
 using namespace WebCore;
 
@@ -87,26 +89,44 @@ static bool getPluginArchitecture(CFBundleRef bundle, PluginInfoStore::Plugin& p
 
     return false;
 }
-    
-static RetainPtr<CFDictionaryRef> getMIMETypesFromPluginBundle(CFBundleRef bundle)
+
+static RetainPtr<CFDictionaryRef> contentsOfPropertyListAtURL(CFURLRef propertyListURL)
+{
+    CFDataRef propertyListData;
+    CFURLCreateDataAndPropertiesFromResource(kCFAllocatorDefault, propertyListURL, &propertyListData, 0, 0, 0);
+    if (!propertyListData)
+        return 0;
+
+    RetainPtr<CFPropertyListRef> propertyList(AdoptCF, CFPropertyListCreateWithData(kCFAllocatorDefault, propertyListData, kCFPropertyListImmutable, 0, 0));
+    CFRelease(propertyListData);
+
+    if (!propertyList)
+        return 0;
+
+    if (CFGetTypeID(propertyList.get()) != CFDictionaryGetTypeID())
+        return 0;
+
+    return RetainPtr<CFDictionaryRef>(AdoptCF, static_cast<CFDictionaryRef>(propertyList.leakRef()));
+}
+
+static RetainPtr<CFDictionaryRef> getMIMETypesFromPluginBundle(CFBundleRef bundle, const PluginInfoStore::Plugin& plugin)
 {
     CFStringRef propertyListFilename = static_cast<CFStringRef>(CFBundleGetValueForInfoDictionaryKey(bundle, CFSTR("WebPluginMIMETypesFilename")));
     if (propertyListFilename) {
         RetainPtr<CFStringRef> propertyListPath(AdoptCF, CFStringCreateWithFormat(kCFAllocatorDefault, 0, CFSTR("%@/Library/Preferences/%@"), NSHomeDirectory(), propertyListFilename));
         RetainPtr<CFURLRef> propertyListURL(AdoptCF, CFURLCreateWithFileSystemPath(kCFAllocatorDefault, propertyListPath.get(), kCFURLPOSIXPathStyle, FALSE));
 
-        CFDataRef propertyListData;
-        CFURLCreateDataAndPropertiesFromResource(kCFAllocatorDefault, propertyListURL.get(), &propertyListData, 0, 0, 0);
-        RetainPtr<CFPropertyListRef> propertyList(AdoptCF, CFPropertyListCreateWithData(kCFAllocatorDefault, propertyListData, kCFPropertyListImmutable, 0, 0));
-        if (propertyListData)
-            CFRelease(propertyListData);
-        
-        // FIXME: Have the plug-in create the MIME types property list if it doesn't exist.
-        // https://bugs.webkit.org/show_bug.cgi?id=57204
-        if (!propertyList || CFGetTypeID(propertyList.get()) != CFDictionaryGetTypeID())
+        RetainPtr<CFDictionaryRef> propertyList = contentsOfPropertyListAtURL(propertyListURL.get());
+
+#if ENABLE(PLUGIN_PROCESS)
+        if (!propertyList && PluginProcessProxy::createPropertyListFile(plugin))
+            propertyList = contentsOfPropertyListAtURL(propertyListURL.get());
+#endif
+
+        if (!propertyList)
             return 0;
         
-        return static_cast<CFDictionaryRef>(CFDictionaryGetValue(static_cast<CFDictionaryRef>(propertyList.get()), CFSTR("WebPluginMIMETypes")));
+        return static_cast<CFDictionaryRef>(CFDictionaryGetValue(propertyList.get(), CFSTR("WebPluginMIMETypes")));
     }
     
     return static_cast<CFDictionaryRef>(CFBundleGetValueForInfoDictionaryKey(bundle, CFSTR("WebPluginMIMETypes")));
@@ -114,7 +134,7 @@ static RetainPtr<CFDictionaryRef> getMIMETypesFromPluginBundle(CFBundleRef bundl
 
 static bool getPluginInfoFromPropertyLists(CFBundleRef bundle, PluginInfoStore::Plugin& plugin)
 {
-    RetainPtr<CFDictionaryRef> mimeTypes = getMIMETypesFromPluginBundle(bundle);
+    RetainPtr<CFDictionaryRef> mimeTypes = getMIMETypesFromPluginBundle(bundle, plugin);
     if (!mimeTypes || CFGetTypeID(mimeTypes.get()) != CFDictionaryGetTypeID())
         return false;
 
@@ -360,6 +380,27 @@ bool NetscapePluginModule::getPluginInfo(const String& pluginPath, PluginInfoSto
     if (plugin.info.desc.isNull())
         plugin.info.desc = plugin.info.file;
     
+    return true;
+}
+
+bool NetscapePluginModule::createPluginMIMETypesPreferences(const String& pluginPath)
+{
+    RetainPtr<CFStringRef> bundlePath(AdoptCF, pluginPath.createCFString());
+    RetainPtr<CFURLRef> bundleURL(AdoptCF, CFURLCreateWithFileSystemPath(kCFAllocatorDefault, bundlePath.get(), kCFURLPOSIXPathStyle, false));
+    
+    // Try to initialize the bundle.
+    RetainPtr<CFBundleRef> bundle(AdoptCF, CFBundleCreate(kCFAllocatorDefault, bundleURL.get()));
+    if (!bundle)
+        return false;
+
+    if (!CFBundleLoadExecutable(bundle.get()))
+        return false;
+
+    void (*createPluginMIMETypesPreferences)(void) = reinterpret_cast<void (*)(void)>(CFBundleGetFunctionPointerForName(bundle.get(), CFSTR("BP_CreatePluginMIMETypesPreferences")));
+    if (!createPluginMIMETypesPreferences)
+        return false;
+    
+    createPluginMIMETypesPreferences();
     return true;
 }
 
--- third_party/WebKit/Source/WebKit2/UIProcess/Plugins/mac/PluginProcessProxyMac.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/Plugins/mac/PluginProcessProxyMac.mm
@@ -29,8 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(PLUGIN_PROCESS)
 
+#import "EnvironmentVariables.h"
 #import "PluginProcessCreationParameters.h"
 #import "WebKitSystemInterface.h"
+#import <WebCore/FileSystem.h>
+#import <spawn.h>
+#import <wtf/text/CString.h>
 
 @interface WKPlaceholderModalWindow : NSWindow 
 @end
@@ -46,6 +50,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
+using namespace WebCore;
+
 namespace WebKit {
     
 bool PluginProcessProxy::pluginNeedsExecutableHeap(const PluginInfoStore::Plugin& pluginInfo)
@@ -57,6 +63,53 @@ bool PluginProcessProxy::pluginNeedsExecutableHeap(const PluginInfoStore::Plugin
     if (pluginInfo.bundleIdentifier == "com.apple.QuickTime Plugin.plugin")
         return false;
     
+    return true;
+}
+
+bool PluginProcessProxy::createPropertyListFile(const PluginInfoStore::Plugin& plugin)
+{
+    NSBundle *webKit2Bundle = [NSBundle bundleWithIdentifier:@"com.apple.WebKit2"];
+    NSString *frameworksPath = [[webKit2Bundle bundlePath] stringByDeletingLastPathComponent];
+    const char* frameworkExecutablePath = [[webKit2Bundle executablePath] fileSystemRepresentation];
+    
+    NSString *processPath = [webKit2Bundle pathForAuxiliaryExecutable:@"PluginProcess.app"];
+    NSString *processAppExecutablePath = [[NSBundle bundleWithPath:processPath] executablePath];
+
+    CString pluginPathString = fileSystemRepresentation(plugin.path);
+
+    posix_spawnattr_t attr;
+    posix_spawnattr_init(&attr);
+
+    cpu_type_t cpuTypes[] = { plugin.pluginArchitecture };    
+    size_t outCount = 0;
+    posix_spawnattr_setbinpref_np(&attr, 1, cpuTypes, &outCount);
+
+    EnvironmentVariables environmentVariables;
+    
+    // To make engineering builds work, if the path is outside of /System set up
+    // DYLD_FRAMEWORK_PATH to pick up other frameworks, but don't do it for the
+    // production configuration because it involves extra file system access.
+    if (![frameworksPath hasPrefix:@"/System/"])
+        environmentVariables.appendValue("DYLD_FRAMEWORK_PATH", [frameworksPath fileSystemRepresentation], ':');
+
+    const char* args[] = { [processAppExecutablePath fileSystemRepresentation], frameworkExecutablePath, "-type", "pluginprocess", "-createPluginMIMETypesPreferences", pluginPathString.data(), 0 };
+
+    pid_t pid;
+    int result = posix_spawn(&pid, args[0], 0, &attr, const_cast<char* const*>(args), environmentVariables.environmentPointer());
+    posix_spawnattr_destroy(&attr);
+
+    if (result < 0)
+        return false;
+    int status;
+    if (waitpid(pid, &status, 0) < 0)
+        return false;
+
+    if (!WIFEXITED(status))
+        return false;
+
+    if (WEXITSTATUS(status) != EXIT_SUCCESS)
+        return false;
+
     return true;
 }
 
