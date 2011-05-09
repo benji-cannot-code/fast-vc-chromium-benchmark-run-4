697ae0cd516eed0a243b3007a6b1dd7ca835cb39
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 07:15:56 +0000

    Fix crash in ui test due to non-local state pref.
    TBR=zea@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_package.cc
+++ chrome/browser/download/save_package.cc
@@ -1243,7 +1243,8 @@ FilePath SavePackage::GetSaveDirPreference(PrefService* prefs) {
     FilePath default_save_path = prefs->GetFilePath(
         prefs::kDownloadDefaultDirectory);
     prefs->RegisterFilePathPref(prefs::kSaveFileDefaultDirectory,
-                                default_save_path);
+                                default_save_path,
+                                PrefService::UNSYNCABLE_PREF);
   }
 
   // Get the directory from preference.
