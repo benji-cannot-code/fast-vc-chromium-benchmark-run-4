e56babba1ea2be551307d24084463b22ac62ac33
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 18:51:44 +0000

    Add simple drag and drop to NativeTextfieldViews. Supports DRAG_COPY to other views/windows. Supports DRAG_MOVE within the same view. Fix DropHelper coordinate translation (not used elsewhere). Fix NativeTextfieldViewsTest.DoubleAndTripleClickTest.
    This patch set depends on my changes at:
    http://codereview.chromium.org/6893096/
    
    BUG=72040
    TEST=--enable-textfield-views drag and drop interaction.
    
    Review URL: http://codereview.chromium.org/6902145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84652 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/drop_helper.cc
+++ views/widget/drop_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -62,7 +62,8 @@ int DropHelper::OnDrop(const OSExchangeData& data,
   }
 
   gfx::Point view_location(root_view_location);
-  View::ConvertPointToView(NULL, drop_view, &view_location);
+  View* root_view = drop_view->GetRootView();
+  View::ConvertPointToView(root_view, drop_view, &view_location);
   DropTargetEvent drop_event(data, view_location.x(), view_location.y(),
                              drag_operation);
   return drop_view->OnPerformDrop(drop_event);
