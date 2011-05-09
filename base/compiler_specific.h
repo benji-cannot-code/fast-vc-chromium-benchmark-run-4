cec0a9c8a467a3afae06ac34e4f473be8555c5fa
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-09 20:11:01 +0000

    Add shutdown trace functions
    BUG=chromium-os:14737
    TEST=None
    
    Review URL: http://codereview.chromium.org/6943001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84662 0039d316-1c4b-4281-b951-d872f2087c98

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -75,8 +75,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   int x ALLOW_UNUSED = ...;
 #if defined(COMPILER_GCC)
 #define ALLOW_UNUSED __attribute__((unused))
+#define NOINLINE __attribute__((noinline))
 #else
 #define ALLOW_UNUSED
+#define NOINLINE
 #endif
 
 // Annotate a virtual method indicating it must be overriding a virtual
