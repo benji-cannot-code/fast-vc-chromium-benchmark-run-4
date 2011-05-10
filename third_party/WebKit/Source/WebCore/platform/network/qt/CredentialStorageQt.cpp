2dc186237734cbaba5e151c1459cd50d53da2c46
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-10 18:28:19 +0000

    Attempt to fix Qt build.
    * platform/network/qt/CredentialStorageQt.cpp:
    (WebCore::CredentialStorage::get):
    Add stub.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/qt/CredentialStorageQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/qt/CredentialStorageQt.cpp
@@ -36,4 +36,9 @@ Credential CredentialStorage::getFromPersistentStorage(const ProtectionSpace&)
     return Credential();
 }
 
+Credential CredentialStorage::get(const ProtectionSpace&)
+{
+    return Credential();
+}
+
 } // namespace WebCore
