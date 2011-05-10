792e282f76aa59ff1b037acae25bb51be662abeb
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 18:51:28 +0000

    Revert 84748 - Fix PyAuto Logout() hook. Currently, logging out disconnects the automation channel but does not re-enable Chrome testing.
    BUG=chromium-os:14990
    TEST=Manual. Tests will be in chromeos_login.py
    
    Review URL: http://codereview.chromium.org/6981005
    
    TBR=dtu@chromium.org
    Review URL: http://codereview.chromium.org/7003004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.h
+++ chrome/test/ui/ui_test.h
@@ -265,8 +265,6 @@ class UITestBase {
   // Closes the browser window.
   virtual void TearDown();
 
-  void ResetConnection();
-
   AutomationProxy* automation() const {
     return launcher_->automation();
   }
