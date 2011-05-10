061bda489a1e7582402fa8b5ac4ca2b027a8b22c
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 21:40:47 +0000

    Include full redirect chain in downloads safebrowsing ping.
    BUG=80097
    TEST=tcpdump
    
    Review URL: http://codereview.chromium.org/6935032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager_unittest.cc
+++ chrome/browser/download/download_manager_unittest.cc
@@ -349,6 +349,7 @@ TEST_F(DownloadManagerTest, DownloadRenameTest) {
     DownloadCreateInfo* info(new DownloadCreateInfo);
     info->download_id = static_cast<int>(i);
     info->prompt_user_for_save_location = false;
+    info->url_chain.push_back(GURL());
     info->is_dangerous_file = kDownloadRenameCases[i].is_dangerous_file;
     info->is_dangerous_url = kDownloadRenameCases[i].is_dangerous_url;
     FilePath new_path(kDownloadRenameCases[i].suggested_path);
@@ -403,6 +404,7 @@ TEST_F(DownloadManagerTest, DownloadInterruptTest) {
   DownloadCreateInfo* info(new DownloadCreateInfo);
   info->download_id = static_cast<int>(0);
   info->prompt_user_for_save_location = false;
+  info->url_chain.push_back(GURL());
   info->is_dangerous_file = false;
   info->is_dangerous_url = false;
   const FilePath new_path(FILE_PATH_LITERAL("foo.zip"));
@@ -468,6 +470,7 @@ TEST_F(DownloadManagerTest, DownloadCancelTest) {
   DownloadCreateInfo* info(new DownloadCreateInfo);
   info->download_id = static_cast<int>(0);
   info->prompt_user_for_save_location = false;
+  info->url_chain.push_back(GURL());
   info->is_dangerous_file = false;
   info->is_dangerous_url = false;
   const FilePath new_path(FILE_PATH_LITERAL("foo.zip"));
@@ -546,6 +549,7 @@ TEST_F(DownloadManagerTest, DownloadOverwriteTest) {
   DownloadCreateInfo* info(new DownloadCreateInfo);
   info->download_id = static_cast<int>(0);
   info->prompt_user_for_save_location = true;
+  info->url_chain.push_back(GURL());
   info->is_dangerous_file = false;
   info->is_dangerous_url = false;
 
