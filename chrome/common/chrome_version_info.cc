17e343400d84839aecacb10add72ab209c6417e7
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 17:53:22 +0000

    Add more information to about:version and "About Chromium" dialog - Add the information of an executable full-path, a profile path and an OS to about:version - Add the information of a build type and an OS to "About Chromium" dialog
    BUG=18679,37186
    TEST=Observe that about:version and "About Chromium" dialog display enough information
    
    Patch by Kentaro Hara <haraken@google.com>.
    
    Review URL: http://codereview.chromium.org/6894037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_version_info.cc
+++ chrome/common/chrome_version_info.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_version_info.h"
 #include "base/string_util.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 
@@ -87,5 +88,25 @@ bool VersionInfo::IsOfficialBuild() const {
 }
 
 #endif
+
+std::string VersionInfo::OSType() const {
+#if defined(OS_WIN)
+  return "Windows";
+#elif defined(OS_MACOSX)
+  return "Mac OS";
+#elif defined(OS_CHROMEOS)
+  return l10n_util::GetStringUTF16(IDS_PRODUCT_OS_NAME);
+#elif defined(OS_LINUX)
+  return "Linux";
+#elif defined(OS_FREEBSD)
+  return "FreeBSD";
+#elif defined(OS_OPENBSD)
+  return "OpenBSD";
+#elif defined(OS_SOLARIS)
+  return "Solaris";
+#else
+  return "Unknown";
+#endif
+}
 
 }  // namespace chrome
