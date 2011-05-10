ad944bbeabede6665960f633f2be874ec6da877f
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 21:00:08 +0000

    Have List redraw on sort.
    BUG=chromium-os:14920
    TEST=Click breadcrumbs to cd into a directory with some files, while in thumbnail mode. Double click on a file - the correct file should launch.
    Review URL: http://codereview.chromium.org/6997012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84845 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/list_selection_model.js
+++ chrome/browser/resources/shared/js/cr/ui/list_selection_model.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -241,6 +241,13 @@ cr.define('cr.ui', function() {
      */
     get multiple() {
       return true;
+    },
+
+    /**
+     * Adjusts the selection after reordering of items in the table.
+     * @param {!Array.<number>} permutation The reordering permutation.
+     */
+    adjustToReordering: function(permutation) {
     },
 
     /**
