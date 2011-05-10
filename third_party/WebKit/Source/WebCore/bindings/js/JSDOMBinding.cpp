55d3361c142c401fd48618548bf3a64ff8e12dd0
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-10 08:27:11 +0000

    2011-05-10  Tony Gentilcore  <tonyg@chromium.org>
            Build fix: include ExceptionCode
            https://bugs.webkit.org/show_bug.cgi?id=60538
    
            * bindings/js/JSDOMBinding.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMObjectHashTableMap.h"
 #include "EventException.h"
 #include "ExceptionBase.h"
+#include "ExceptionCode.h"
 #include "FileException.h"
 #include "Frame.h"
 #include "IDBDatabaseException.h"
