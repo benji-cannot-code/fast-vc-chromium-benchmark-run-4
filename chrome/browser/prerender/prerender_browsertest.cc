8a73178d36a8c2405569c2697b12b17543fc246c
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 11:15:28 +0000

    Mark PrerenderBrowserTest.TaskManager flaky.
    Also, rename to PrerenderTaskManager to be consistent.
    
    TBR=dominich@chromium.org
    BUG=82118
    TEST=None
    
    Review URL: http://codereview.chromium.org/6993007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -705,7 +705,8 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
   EXPECT_FALSE(UrlIsPendingInPrerenderManager(kHtmlFileC));
 }
 
-IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, TaskManager) {
+// Flaky: http://crbug.com/82118
+IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, FLAKY_PrerenderTaskManager) {
   // Early out if we're not using TabContents for Prerendering.
   if (!prerender::PrerenderContents::UseTabContents()) {
     SUCCEED();
