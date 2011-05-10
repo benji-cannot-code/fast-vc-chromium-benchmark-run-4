2f29a697d7fa1f8bf35a7c4fed43ac424f70e474
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 07:57:29 +0000

    Do not assume that the initial XKB layout on Chrome startup is always US-qwerty.
    The assumption might not be true e.g. when Chrome crashes and then restarts.
    
    BUG=chromium-os:14410
    TEST=manually done
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=84530
    
    Review URL: http://codereview.chromium.org/6954005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84762 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/xkeyboard.cc
+++ chrome/browser/chromeos/input_method/xkeyboard.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "base/process_util.h"
+#include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
 
 namespace chromeos {
@@ -143,7 +144,10 @@ class XKeyboard {
   // Remaps modifier keys. This function does not change the current keyboard
   // layout. Returns true on success.
   bool RemapModifierKeys(const ModifierMap& modifier_map) {
-    if (SetLayoutInternal(current_layout_name_, modifier_map)) {
+    const std::string layout_name = current_layout_name_.empty() ?
+        kDefaultLayoutName : current_layout_name_;
+    if (SetLayoutInternal(layout_name, modifier_map)) {
+      current_layout_name_ = layout_name;
       current_modifier_map_ = modifier_map;
       return true;
     }
@@ -191,7 +195,7 @@ class XKeyboard {
  private:
   friend struct DefaultSingletonTraits<XKeyboard>;
 
-  XKeyboard() : current_layout_name_(kDefaultLayoutName) {
+  XKeyboard() {
     for (size_t i = 0; i < arraysize(kCustomizableKeys); ++i) {
       ModifierKey key = kCustomizableKeys[i];
       current_modifier_map_.push_back(ModifierKeyPair(key, key));
@@ -204,17 +208,24 @@ class XKeyboard {
   // setxkbmap command if needed, and updates the last_full_layout_name_ cache.
   bool SetLayoutInternal(const std::string& layout_name,
                          const ModifierMap& modifier_map) {
+    if (!CrosLibrary::Get()->EnsureLoaded()) {
+      // We should not try to change a layout inside ui_tests.
+      return false;
+    }
+
     const std::string layouts_to_set = CreateFullXkbLayoutName(
         layout_name, modifier_map);
     if (layouts_to_set.empty()) {
       return false;
     }
 
-    const std::string current_layout = CreateFullXkbLayoutName(
-        current_layout_name_, current_modifier_map_);
-    if (current_layout == layouts_to_set) {
-      DLOG(INFO) << "The requested layout is already set: " << layouts_to_set;
-      return true;
+    if (!current_layout_name_.empty()) {
+      const std::string current_layout = CreateFullXkbLayoutName(
+          current_layout_name_, current_modifier_map_);
+      if (current_layout == layouts_to_set) {
+        DLOG(INFO) << "The requested layout is already set: " << layouts_to_set;
+        return true;
+      }
     }
 
     // Turn off caps lock if there is no kCapsLockKey in the remapped keys.
