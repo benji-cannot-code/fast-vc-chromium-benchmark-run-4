7f806d967f29275f5b69abfc103ab276c69e9400
ademar.reis@openbossa.org <ademar.reis@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-10 15:32:44 +0000

    2011-05-10  Ademar de Souza Reis Jr.  <ademar.reis@openbossa.org>
            Reviewed by Csaba Osztrogonác.
    
            [Qt] tst_QWebPage creates temporary files in the current working dir
            https://bugs.webkit.org/show_bug.cgi?id=60497
    
            tst_QWebPage was using QDir::currentPath when creating temporary dirs
            and leaving them after the test was run. I basically borrowed the fix
            from tst_QDeclarativeWebView.
    
            * tests/qwebpage/tst_qwebpage.cpp:
            (removeRecursive):
            (tst_QWebPage::tmpDirPath):
            (tst_QWebPage::cleanupFiles):
            (tst_QWebPage::database):
            (tst_QWebPage::multiplePageGroupsAndLocalStorage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
+++ third_party/WebKit/Source/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
@@ -46,6 +46,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qwebview.h>
 #include <qimagewriter.h>
 
+static void removeRecursive(const QString& dirname)
+{
+    QDir dir(dirname);
+    QFileInfoList entries(dir.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot));
+    for (int i = 0; i < entries.count(); ++i)
+        if (entries[i].isDir())
+            removeRecursive(entries[i].filePath());
+        else
+            dir.remove(entries[i].fileName());
+    QDir().rmdir(dirname);
+}
+
 class EventSpy : public QObject, public QList<QEvent::Type>
 {
     Q_OBJECT
@@ -146,10 +158,16 @@ private slots:
 #ifdef Q_OS_MAC
     void macCopyUnicodeToClipboard();
 #endif
-    
+
 private:
     QWebView* m_view;
     QWebPage* m_page;
+    QString tmpDirPath() const
+    {
+        static QString tmpd = QDir::tempPath() + "/tst_qwebpage-"
+            + QDateTime::currentDateTime().toString(QLatin1String("yyyyMMddhhmmss"));
+        return tmpd;
+    }
 };
 
 tst_QWebPage::tst_QWebPage()
@@ -173,9 +191,7 @@ void tst_QWebPage::cleanup()
 
 void tst_QWebPage::cleanupFiles()
 {
-    QFile::remove("Databases.db");
-    QDir::current().rmdir("http_www.myexample.com_0");
-    QFile::remove("http_www.myexample.com_0.localstorage");
+    removeRecursive(tmpDirPath());
 }
 
 void tst_QWebPage::initTestCase()
@@ -554,7 +570,7 @@ void tst_QWebPage::contextMenuCrash()
 
 void tst_QWebPage::database()
 {
-    QString path = QDir::currentPath();
+    QString path = tmpDirPath();
     m_page->settings()->setOfflineStoragePath(path);
     QVERIFY(m_page->settings()->offlineStoragePath() == path);
 
@@ -908,7 +924,7 @@ void tst_QWebPage::createViewlessPlugin()
 
 void tst_QWebPage::multiplePageGroupsAndLocalStorage()
 {
-    QDir dir(QDir::currentPath());
+    QDir dir(tmpDirPath());
     dir.mkdir("path1");
     dir.mkdir("path2");
 
@@ -916,10 +932,10 @@ void tst_QWebPage::multiplePageGroupsAndLocalStorage()
     QWebView view2;
 
     view1.page()->settings()->setAttribute(QWebSettings::LocalStorageEnabled, true);
-    view1.page()->settings()->setLocalStoragePath(QDir::toNativeSeparators(QDir::currentPath() + "/path1"));
+    view1.page()->settings()->setLocalStoragePath(QDir::toNativeSeparators(tmpDirPath() + "/path1"));
     DumpRenderTreeSupportQt::webPageSetGroupName(view1.page(), "group1");
     view2.page()->settings()->setAttribute(QWebSettings::LocalStorageEnabled, true);    
-    view2.page()->settings()->setLocalStoragePath(QDir::toNativeSeparators(QDir::currentPath() + "/path2"));
+    view2.page()->settings()->setLocalStoragePath(QDir::toNativeSeparators(tmpDirPath() + "/path2"));
     DumpRenderTreeSupportQt::webPageSetGroupName(view2.page(), "group2");
     QCOMPARE(DumpRenderTreeSupportQt::webPageGroupName(view1.page()), QString("group1"));
     QCOMPARE(DumpRenderTreeSupportQt::webPageGroupName(view2.page()), QString("group2"));
@@ -942,8 +958,8 @@ void tst_QWebPage::multiplePageGroupsAndLocalStorage()
 
     QTest::qWait(1000);
 
-    QFile::remove(QDir::toNativeSeparators(QDir::currentPath() + "/path1/http_www.myexample.com_0.localstorage"));
-    QFile::remove(QDir::toNativeSeparators(QDir::currentPath() + "/path2/http_www.myexample.com_0.localstorage"));
+    QFile::remove(QDir::toNativeSeparators(tmpDirPath() + "/path1/http_www.myexample.com_0.localstorage"));
+    QFile::remove(QDir::toNativeSeparators(tmpDirPath() + "/path2/http_www.myexample.com_0.localstorage"));
     dir.rmdir(QDir::toNativeSeparators("./path1"));
     dir.rmdir(QDir::toNativeSeparators("./path2"));
 }
