bff38a1deeaaadf8a3e29c92ec5fdee70fb1d481
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 21:59:24 +0000

    base: Replace "using namespace base;" by wrapping the tests around "namespace base { ... }"
    BUG=82078
    TEST=base_unittests
    
    R=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7000011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84870 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_piece_unittest.cc
+++ base/string_piece_unittest.cc
@@ -1,15 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <string>
 
 #include "base/string_piece.h"
-
 #include "testing/gtest/include/gtest/gtest.h"
 
-using namespace base;
+namespace base {
 
 TEST(StringPieceTest, CheckComparisonOperators) {
 #define CMP_Y(op, x, y)                                               \
@@ -541,3 +540,5 @@ TEST(StringPieceTest, HeterogenousStringPieceEquals) {
   ASSERT_TRUE(StringPiece("hello") == std::string("hello"));
   ASSERT_TRUE("hello" == StringPiece("hello"));
 }
+
+}  // namespace base
