13209f448d64b67447a50cb51e384f8efaab8c5a
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 16:16:01 +0000

    Turn on strict PassOwnPtr on Windows
    Fixes <http://webkit.org/b/60632> Windows should build with strict PassOwnPtr enabled
    
    Reviewed by Adam Barth.
    
    Source/JavaScriptCore:
    
    * wtf/PassOwnPtr.h:
    
    Source/WebCore:
    
    * platform/graphics/win/SimpleFontDataWin.cpp: Keep LOOSE_PASS_OWN_PTR turned on for this
    file until SimpleFontData::scaledFontData is dealt with on all platforms.
    
    Source/WebKit2:
    
    * WebProcess/FullScreen/win/WebFullScreenManagerWin.cpp:
    (WebKit::WebFullScreenManagerWin::setRootFullScreenLayer): Use nullptr instead of 0.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86237 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/win/SimpleFontDataWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/win/SimpleFontDataWin.cpp
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+
+// FIXME: Remove this define!
+#define LOOSE_PASS_OWN_PTR
+
 #include "SimpleFontData.h"
 
 #include "Font.h"
