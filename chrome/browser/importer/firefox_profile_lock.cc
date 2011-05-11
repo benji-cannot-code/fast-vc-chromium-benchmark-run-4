5035912939fca6fb2bffa002341fe5a4428dded2
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 17:24:08 +0000

    Allow the Firefox lock to be deleted on the main thread during import.
    BUG=77777
    TEST=import tests don't complain.
    Review URL: http://codereview.chromium.org/6995051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox_profile_lock.cc
+++ chrome/browser/importer/firefox_profile_lock.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/firefox_profile_lock.h"
 
 #include "base/file_path.h"
+#include "base/threading/thread_restrictions.h"
 
 // This class is based on Firefox code in:
 //   profile/dirserviceprovider/src/nsProfileLock.cpp
@@ -76,5 +77,8 @@ FirefoxProfileLock::FirefoxProfileLock(const FilePath& path) {
 }
 
 FirefoxProfileLock::~FirefoxProfileLock() {
+  // Because this destructor happens in first run on the profile import thread,
+  // with no UI to jank, it's ok to allow deletion of the lock here.
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
   Unlock();
 }
