392ce0ed460380a2d3133ce6b62e01ac2c61b608
jknotten@chromium.org <jknotten@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 09:03:35 +0000

    Use const operator to enable compilation with stlport.
    This is required if you want to use the user configuration
    _STLP_USE_PTR_SPECIALIZATIONS=1 for template bloat reduction.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6995025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84956 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_auth_cache.cc
+++ net/http/http_auth_cache.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -50,7 +50,7 @@ void CheckOriginIsValid(const GURL& origin) {
 // Functor used by remove_if.
 struct IsEnclosedBy {
   explicit IsEnclosedBy(const std::string& path) : path(path) { }
-  bool operator() (const std::string& x) {
+  bool operator() (const std::string& x) const {
     return IsEnclosingPath(path, x);
   }
   const std::string& path;
