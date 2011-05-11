d89cfa3a8d00fe03b63a2791695e559c941d974b
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 21:56:16 +0000

    Avoid exiting the renderer process if it has a pending render view.
    This addresses an issue for a future CL when we switch to swapping
    RenderViewHosts in and out, rather than throwing them away each time.
    
    BUG=65953
    TEST=None
    
    Review URL: http://codereview.chromium.org/6927014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85054 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/browser_render_process_host.h
+++ content/browser/renderer_host/browser_render_process_host.h
@@ -82,6 +82,7 @@ class BrowserRenderProcessHost : public RenderProcessHost,
   void CreateMessageFilters();
 
   // Control message handlers.
+  void OnShutdownRequest();
   void OnUpdatedCacheStats(const WebKit::WebCache::UsageStats& stats);
   void SuddenTerminationChanged(bool enabled);
   void OnUserMetricsRecordAction(const std::string& action);
