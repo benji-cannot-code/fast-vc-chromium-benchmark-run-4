4765613751db78fc29398f38043e5c5806857664
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 09:11:36 +0000

    2011-05-11  Kent Tamura  <tkent@chromium.org>
            Reviewed by Hajime Morita.
    
            The position of validation message bubble is wrong for non text fields.
            https://bugs.webkit.org/show_bug.cgi?id=60341
    
            * fast/forms/resources/common.js: Added. Helper functions for fast/forms.
            * fast/forms/validation-message-on-checkbox-expected.txt: Added.
            * fast/forms/validation-message-on-checkbox.html: Added.
            * fast/forms/validation-message-on-listbox-expected.txt: Added.
            * fast/forms/validation-message-on-listbox.html: Added.
            * fast/forms/validation-message-on-menulist-expected.txt: Added.
            * fast/forms/validation-message-on-menulist.html: Added.
            * fast/forms/validation-message-on-radio-expected.txt: Added.
            * fast/forms/validation-message-on-radio.html: Added.
            * fast/forms/validation-message-on-textarea-expected.txt: Added.
            * fast/forms/validation-message-on-textarea.html: Added.
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/mac/fast/forms/validation-message-appearance-expected.png: Updated.
            * platform/mac/fast/forms/validation-message-appearance-expected.txt: Updated.
            * platform/qt/Skipped:
            * platform/win/Skipped:
    2011-05-11  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Hajime Morita.
    
            The position of validation message bubble is wrong for non text fields.
            https://bugs.webkit.org/show_bug.cgi?id=60341
    
            Tests: fast/forms/validation-message-on-checkbox.html
                   fast/forms/validation-message-on-listbox.html
                   fast/forms/validation-message-on-menulist.html
                   fast/forms/validation-message-on-radio.html
                   fast/forms/validation-message-on-textarea.html
    
            * css/html.css:
            (::-webkit-validation-bubble-message):
              Add white-space:normal in order not to inherit white-space:pre for <select>.
            * html/ValidationMessage.cpp:
            (WebCore::adjustBubblePosition):
              Adjust the bubble position.
              - The top of the bubble should be same as the bottom of the target control.
              - The left of the bubble should be same as the left of the target control.
                But If the left+32 is larger than the center of the target control,
                left+32 should be at the center of the target control.
                But the left position should not be less than 0.
            (WebCore::ValidationMessage::buildBubbleTree): Call adjustBubblePosition().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/forms/resources/common.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function $(id) {
+    return document.getElementById(id);
+}
+
+function getValidationMessageBubbleNode(host) {
+    // FIXME: We should search for a pseudo ID.
+    return layoutTestController.shadowRoot(host).lastChild;
+}
+
+function getAbsoluteRect(element) {
+    var rect = element.getBoundingClientRect();
+    rect.top += document.body.scrollTop;
+    rect.bottom += document.body.scrollTop;
+    rect.left += document.body.scrollLeft;
+    rect.right += document.body.scrollLeft;
+    return rect;
+}
