0055475526dfbd3dea65696dcf96c0ed6dff78de
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 21:02:58 +0000

    2011-05-11  Noam Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Texmap][Qt] Upstream texture-mapper changes from Qt's WebKit2 branch
            https://bugs.webkit.org/show_bug.cgi?id=60439
    
            Patch 1/12: Change the TextureMapperPlatformLayer API to allow a 3D-context or a
            media player to decide how to paint into the TextureMapper, rather than mandate specific
            types of platform layers.
    
            No new tests. Tests in LayoutTests/compositing test this.
    
            * platform/graphics/GraphicsLayer.h:
            * platform/graphics/texmap/TextureMapperPlatformLayer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86264 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
-#if USE(TEXTURE_MAPPER)
-#include "texmap/TextureMapperPlatformLayer.h"
-#endif
-
 #if PLATFORM(MAC)
 #ifdef __OBJC__
 @class CALayer;
