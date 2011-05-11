d5aa173ef4c796be3acbdfe2b0ea94de915d653e
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 18:35:15 +0000

    2011-05-11  Dimitri Glazkov  <dglazkov@chromium.org>
            Unreviewed, rolling out r85650.
            http://trac.webkit.org/changeset/85650
            https://bugs.webkit.org/show_bug.cgi?id=59983
    
            Triggers m_numNodeListCaches > 0 assert in gc-heavy sites.
    
            * dom/ContainerNode.cpp:
            (WebCore::ContainerNode::childrenChanged):
            * dom/Node.cpp:
            (WebCore::Node::~Node):
            (WebCore::Node::setDocument):
            (WebCore::Node::setTreeScopeRecursively):
            (WebCore::Node::childNodes):
            (WebCore::Node::registerDynamicNodeList):
            (WebCore::Node::unregisterDynamicNodeList):
            (WebCore::Node::notifyLocalNodeListsAttributeChanged):
            (WebCore::Node::notifyLocalNodeListsChildrenChanged):
            (WebCore::Node::getElementsByTagName):
            (WebCore::Node::getElementsByTagNameNS):
            (WebCore::Node::getElementsByName):
            (WebCore::Node::getElementsByClassName):
            * dom/Node.h:
            * html/HTMLFormControlElement.cpp:
            (WebCore::HTMLFormControlElement::labels):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -803,7 +803,7 @@ void ContainerNode::childrenChanged(bool changedByParser, Node* beforeChange, No
     Node::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
     if (!changedByParser && childCountDelta)
         document()->nodeChildrenChanged(this);
-    if (treeScope()->hasNodeListCaches())
+    if (document()->hasNodeListCaches())
         notifyNodeListsChildrenChanged();
 }
 
