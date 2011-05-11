5d42cdf229ac0ca3a3821024bdff9509ebcc8723
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 21:19:33 +0000

    Add a unit test for ChromeCookiePolicy.
    BUG=none
    TEST=yup
    
    Review URL: http://codereview.chromium.org/7003011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_cookie_policy.h
+++ chrome/browser/net/chrome_cookie_policy.h
@@ -38,6 +38,7 @@ class ChromeCookiePolicy : public net::CookiePolicy {
   // True if blocking third-party cookies also applies to reading them.
   bool strict_third_party_blocking_;
 
+  friend class ChromeCookiePolicyTest;
   DISALLOW_COPY_AND_ASSIGN(ChromeCookiePolicy);
 };
 
