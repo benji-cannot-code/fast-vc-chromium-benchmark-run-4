c68d1b27bcadc44fe58ff332d75818771ca7fc15
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 04:32:28 +0000

    Move ExtensionFunctionDispatcher to ExtensionTabHelper. This sets the stage for us to expose certain extension functions to content scripts and normal web pages.
    This required two major structural changes:
    
    1. Made EFD stateless, except for the pointer to its
    delegate. This is important to gracefully handle the case
    of a RVH navigating between different extensions or even
    to normal web content. Especially in the case of
    TabContents, where the entire RVH can be torn down and
    replaced during navigation.
    
    2. Centralize all per-(extension, RVH) setup in
    ChromeContentBrowserClient::RenderViewCreated(). In
    particular, responsibility for enabling extension bindings
    was very spread out before, making it hard to follow when
    exactly they were enabled.
    
    BUG=80308
    
    Review URL: http://codereview.chromium.org/6927076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/visitedlink/visitedlink_unittest.cc
+++ chrome/browser/visitedlink/visitedlink_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -506,7 +506,7 @@ class VisitRelayingRenderProcessHost : public BrowserRenderProcessHost {
         Source<RenderProcessHost>(this), NotificationService::NoDetails());
   }
 
-  virtual bool Init(bool is_accessibility_enabled, bool is_extension_process) {
+  virtual bool Init(bool is_accessibility_enabled) {
     return true;
   }
 
