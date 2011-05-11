c4c2634b20e6b4863c19a4637e18de0b4087e2ed
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 03:48:54 +0000

    Fix a browser crash involving omnibox extension keywords.
    The |match| sometimes did not have a valid template_url, because GetInfoForCurrentText was classifying the match using the wrong parameters. Fixed to use prefer_keyword==true in this case.
    
    BUG=81776
    TEST=see bug for repro
    
    
    Review URL: http://codereview.chromium.org/6995032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit.cc
+++ chrome/browser/autocomplete/autocomplete_edit.cc
@@ -412,8 +412,8 @@ bool AutocompleteEditModel::CanPasteAndGo(const string16& text) const {
   if (!view_->GetCommandUpdater()->IsCommandEnabled(IDC_OPEN_CURRENT_URL))
     return false;
 
-  profile_->GetAutocompleteClassifier()->Classify(text, string16(), false,
-      &paste_and_go_match_, &paste_and_go_alternate_nav_url_);
+  profile_->GetAutocompleteClassifier()->Classify(text, string16(),
+      false, false, &paste_and_go_match_, &paste_and_go_alternate_nav_url_);
   return paste_and_go_match_.destination_url.is_valid();
 }
 
@@ -935,8 +935,8 @@ void AutocompleteEditModel::GetInfoForCurrentText(
     InfoForCurrentSelection(match, alternate_nav_url);
   } else {
     profile_->GetAutocompleteClassifier()->Classify(
-        UserTextFromDisplayText(view_->GetText()), GetDesiredTLD(), true,
-        match, alternate_nav_url);
+        UserTextFromDisplayText(view_->GetText()), GetDesiredTLD(),
+        KeywordIsSelected(), true, match, alternate_nav_url);
   }
 }
 
