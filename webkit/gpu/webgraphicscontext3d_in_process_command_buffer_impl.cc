40c59eb9c5e65d511211c3a4a0673f4841ed0dab
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 19:22:37 +0000

    Split GLContext::Create*GLContext into GLSurface::Create*GLSurface plus a surface type independent GLContext::CreateGLContext.
    TEST=webgl on windows and mac, trybots
    BUG=none
    Review URL: http://codereview.chromium.org/6997003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85013 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
+++ webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/gl/gl_bindings_skia_in_process.h"
 #include "ui/gfx/gl/gl_context.h"
 #include "ui/gfx/gl/gl_implementation.h"
+#include "ui/gfx/gl/gl_surface.h"
 
 namespace webkit {
 namespace gpu {
@@ -103,7 +104,7 @@ bool WebGraphicsContext3DInProcessCommandBufferImpl::initialize(
     WebGraphicsContext3D::Attributes attributes,
     WebView* webView,
     bool render_directly_to_web_view) {
-  if (!gfx::GLContext::InitializeOneOff())
+  if (!gfx::GLSurface::InitializeOneOff())
     return false;
   gfx::BindSkiaToInProcessGL();
 
@@ -133,7 +134,13 @@ bool WebGraphicsContext3DInProcessCommandBufferImpl::initialize(
   // and from there to the window, and WebViewImpl::paint already
   // correctly handles the case where the compositor is active but
   // the output needs to go to a WebCanvas.
-  gl_context_.reset(gfx::GLContext::CreateOffscreenGLContext(share_context));
+  scoped_ptr<gfx::GLSurface> surface(gfx::GLSurface::CreateOffscreenGLSurface(
+      gfx::Size(1, 1)));
+  if (!surface->Initialize())
+    return false;
+
+  gl_context_.reset(gfx::GLContext::CreateGLContext(surface.release(),
+                                                    share_context));
   if (!gl_context_.get()) {
     if (!is_gles2_)
       return false;
@@ -146,7 +153,12 @@ bool WebGraphicsContext3DInProcessCommandBufferImpl::initialize(
     // and force them to drop their contexts, sending a context lost event if
     // necessary.
     webView->mainFrame()->collectGarbage();
-    gl_context_.reset(gfx::GLContext::CreateOffscreenGLContext(share_context));
+
+    surface.reset(gfx::GLSurface::CreateOffscreenGLSurface(gfx::Size(1, 1)));
+
+    gl_context_.reset(gfx::GLContext::CreateGLContext(
+        surface.release(),
+        share_context));
     if (!gl_context_.get())
       return false;
   }
