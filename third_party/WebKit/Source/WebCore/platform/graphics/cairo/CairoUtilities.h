32c024c3ef5e56c539f15a2b48530758d1dd4ec4
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 19:36:01 +0000

    2011-05-11  Martin Robinson  <mrobinson@igalia.com>
            Try once more to fix the WinCairo build.
    
            * platform/graphics/cairo/CairoUtilities.h: Include GraphicsTypes.h with a relative
            include rather than a system include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/CairoUtilities.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/CairoUtilities.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CairoUtilities_h
 #define CairoUtilities_h
 
-#include <GraphicsTypes.h>
+#include "GraphicsTypes.h"
 #include <cairo.h>
 
 namespace WebCore {
