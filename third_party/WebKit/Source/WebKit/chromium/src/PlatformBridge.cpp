b1e4ddd64717e7615106255f54c2561da8bf8c44
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-11 06:07:37 +0000

    2011-05-10  Ilya Tikhonovsky  <loislo@chromium.org>
            Unreviewed buildfix for strict OwnPtr patch.
    
            * src/PlatformBridge.cpp:
            (WebCore::PlatformBridge::loadPlatformAudioResource):
            (WebCore::PlatformBridge::decodeAudioFileData):
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/PlatformBridge.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/PlatformBridge.cpp
@@ -621,7 +621,7 @@ PassOwnPtr<AudioBus> PlatformBridge::loadPlatformAudioResource(const char* name,
 {
     const WebData& resource = webKitClient()->loadResource(name);
     if (resource.isEmpty())
-        return 0;
+        return nullptr;
     
     return decodeAudioFileData(resource.data(), resource.size(), sampleRate);
 }
@@ -631,7 +631,7 @@ PassOwnPtr<AudioBus> PlatformBridge::decodeAudioFileData(const char* data, size_
     WebAudioBus webAudioBus;
     if (webKitClient()->loadAudioResource(&webAudioBus, data, size, sampleRate))
         return webAudioBus.release();
-    return 0;
+    return nullptr;
 }
 
 #endif // ENABLE(WEB_AUDIO)
