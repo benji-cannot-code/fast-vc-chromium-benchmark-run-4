39b0f7ddc29f86dea2ab68fe24dd0ea12429cd25
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 03:49:04 +0000

    Block the creation of a new window when prerendering.
    BUG=none
    TEST=PrerenderBrowserTest.PrerenderPopup
    
    
    Review URL: http://codereview.chromium.org/6955003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84922 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_contents.cc
+++ chrome/browser/prerender/prerender_contents.cc
@@ -286,7 +286,8 @@ void PrerenderContents::StartPrerendering(
       Source<RenderViewHostDelegate>(GetRenderViewHostDelegate()));
 
   // Register for new windows from any source.
-  notification_registrar_.Add(this, NotificationType::CREATING_NEW_WINDOW,
+  notification_registrar_.Add(this,
+                              NotificationType::CREATING_NEW_WINDOW_CANCELLED,
                               Source<TabContents>(new_contents));
 
   DCHECK(load_start_time_.is_null());
@@ -491,7 +492,7 @@ void PrerenderContents::Observe(NotificationType type,
       break;
     }
 
-    case NotificationType::CREATING_NEW_WINDOW: {
+    case NotificationType::CREATING_NEW_WINDOW_CANCELLED: {
       if (prerender_contents_.get()) {
         CHECK(Source<TabContents>(source).ptr() ==
               prerender_contents_->tab_contents());
