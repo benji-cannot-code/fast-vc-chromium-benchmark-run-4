1fbeb0d8e20a2f826dfa47e6a8c839b43e386835
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 21:55:28 +0000

    webui/chromeos: Remove "using namespace chromeos::input_method;"
    BUG=82078
    TEST=None
    
    R=estade@chromium.org
    
    Review URL: http://codereview.chromium.org/7004010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.cc
+++ chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.cc
@@ -26,11 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
+using chromeos::input_method::ModifierKey;
 
 namespace {
 
 struct ModifierToLabel {
-  const chromeos::input_method::ModifierKey modifier;
+  const ModifierKey modifier;
   const char* label;
 } kModifierToLabels[] = {
   {chromeos::input_method::kSearchKey, "search"},
@@ -40,7 +41,7 @@ struct ModifierToLabel {
   {chromeos::input_method::kCapsLockKey, "caps lock"},
 };
 
-std::string ModifierKeyToLabel(chromeos::input_method::ModifierKey modifier) {
+std::string ModifierKeyToLabel(ModifierKey modifier) {
   for (size_t i = 0; i < arraysize(kModifierToLabels); ++i) {
     if (modifier == kModifierToLabels[i].modifier) {
       return kModifierToLabels[i].label;
@@ -50,7 +51,6 @@ std::string ModifierKeyToLabel(chromeos::input_method::ModifierKey modifier) {
 }
 
 }  // namespace
-
 
 class KeyboardOverlayUIHTMLSource : public ChromeURLDataManager::DataSource {
  public:
@@ -327,17 +327,16 @@ void KeyboardOverlayHandler::GetKeyboardOverlayId(const ListValue* args) {
 }
 
 void KeyboardOverlayHandler::GetLabelMap(const ListValue* args) {
-  using namespace chromeos::input_method;
   DCHECK(profile_);
-
   PrefService* pref_service = profile_->GetPrefs();
   typedef std::map<ModifierKey, ModifierKey> ModifierMap;
   ModifierMap modifier_map;
-  modifier_map[kSearchKey] = static_cast<ModifierKey>(
+  modifier_map[chromeos::input_method::kSearchKey] = static_cast<ModifierKey>(
       pref_service->GetInteger(prefs::kLanguageXkbRemapSearchKeyTo));
-  modifier_map[kLeftControlKey] = static_cast<ModifierKey>(
-      pref_service->GetInteger(prefs::kLanguageXkbRemapControlKeyTo));
-  modifier_map[kLeftAltKey] = static_cast<ModifierKey>(
+  modifier_map[chromeos::input_method::kLeftControlKey] =
+      static_cast<ModifierKey>(
+          pref_service->GetInteger(prefs::kLanguageXkbRemapControlKeyTo));
+  modifier_map[chromeos::input_method::kLeftAltKey] = static_cast<ModifierKey>(
       pref_service->GetInteger(prefs::kLanguageXkbRemapAltKeyTo));
 
   DictionaryValue dict;
