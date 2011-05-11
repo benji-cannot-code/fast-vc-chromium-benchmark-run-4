5f6f0ea29fde788c2f769434c189edb23f93ac0b
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 19:13:42 +0000

    Set minimum width on menu for NativeComboboxViews.
    The combobox menu is forced to be at least as wide as the combobox itself.
    
    Besides the addition to native_combobox_views.cc, an edit was needed to views::SubmenuView.  The minimum preferred width must account for the border (specified in SubmenuView, applied in MenuScrollViewContainer) to make the ultimate menu width match the size requested by the combobox.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7002004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85010 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/combobox/native_combobox_views.cc
+++ views/controls/combobox/native_combobox_views.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/border.h"
 #include "views/controls/combobox/combobox.h"
 #include "views/controls/focusable_border.h"
+#include "views/controls/menu/submenu_view.h"
 #include "views/widget/root_view.h"
 #include "views/window/window.h"
 
@@ -365,6 +366,10 @@ void NativeComboboxViews::ShowDropDownMenu() {
 
   if (!dropdown_list_menu_.get())
     UpdateFromModel();
+
+  // Extend the menu to the width of the combobox.
+  SubmenuView* submenu = dropdown_list_menu_->CreateSubmenu();
+  submenu->set_minimum_preferred_width(size().width());
 
   gfx::Rect lb = GetLocalBounds();
   gfx::Point menu_position(lb.origin());
--- views/controls/combobox/native_combobox_views.h
+++ views/controls/combobox/native_combobox_views.h
@@ -19,7 +19,6 @@ class Font;
 namespace views {
 
 class KeyEvent;
-class Menu2;
 class FocusableBorder;
 
 // A views/skia only implementation of NativeComboboxWrapper.
