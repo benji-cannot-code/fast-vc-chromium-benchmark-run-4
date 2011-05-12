b36476217bc7f7a2f809b3246d7d73a2867a6623
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 16:08:56 +0000

    Fix Clang compile errors in content_client.
    BUG=65953
    TEST=Compiles on Clang.
    TBR=thakis
    
    Review URL: http://codereview.chromium.org/7014016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85137 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/content_client.cc
+++ content/common/content_client.cc
@@ -24,4 +24,12 @@ ContentClient::ContentClient() :
 ContentClient::~ContentClient() {
 }
 
+bool ContentClient::CanSendWhileSwappedOut(const IPC::Message* msg) {
+  return false;
+}
+
+bool ContentClient::CanHandleWhileSwappedOut(const IPC::Message& msg) {
+  return false;
+}
+
 }  // namespace content
