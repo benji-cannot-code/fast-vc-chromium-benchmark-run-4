fb9b14de3f1f82adf5078f92be0b6b1d2e11b402
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 14:13:57 +0000

    Build fixes after r86325
    * editing/SpellingCorrectionController.cpp:
    * editing/SpellingCorrectionController.h:
    Added missing declarations/#includes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.cpp
+++ third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DocumentMarkerController.h"
 #include "EditCommand.h"
 #include "EditorClient.h"
+#include "FloatQuad.h"
 #include "Frame.h"
 #include "FrameView.h"
 #include "SpellingCorrectionCommand.h"
--- third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.h
+++ third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.h
@@ -47,8 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class EditorClient;
 class EditCommand;
 class Frame;
+class TextCheckerClient;
 
 struct CorrectionPanelInfo {
     enum PanelType {
