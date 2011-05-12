5e3e19bf6b8944e26e98eb04ef762452ae28b800
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 23:56:14 +0000

    chromeos: Get rid of "using namespace file_util;"
    BUG=82078
    TEST=None
    
    R=cmasone@chromium.org
    
    Review URL: http://codereview.chromium.org/7012018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/google_authenticator_unittest.cc
+++ chrome/browser/chromeos/login/google_authenticator_unittest.cc
@@ -35,7 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using namespace file_util;
+using file_util::CloseFile;
+using file_util::CreateAndOpenTemporaryFile;
+using file_util::CreateAndOpenTemporaryFileInDir;
+using file_util::Delete;
+using file_util::WriteFile;
 using ::testing::AnyNumber;
 using ::testing::DoAll;
 using ::testing::Eq;
