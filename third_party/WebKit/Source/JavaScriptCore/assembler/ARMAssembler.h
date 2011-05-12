fd871a3b1d1fc4e23fd61bf20b78cef80ceaae8e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 07:31:43 +0000

    2011-05-12  Keith Kyzivat  <keith.kyzivat@nokia.com>
            Reviewed by Csaba Osztrogonác.
    
            [Qt] Arm debug build failing on ARMAssembler::debugOffset()
            https://bugs.webkit.org/show_bug.cgi?id=60688
    
            Related to svn rev 85523
    
            * assembler/ARMAssembler.h:
            (JSC::ARMAssembler::debugOffset):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/ARMAssembler.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/ARMAssembler.h
@@ -684,7 +684,7 @@ namespace JSC {
         void* executableCopy(ExecutablePool* allocator);
 
 #ifndef NDEBUG
-        unsigned debugOffset() { return m_formatter.debugOffset(); }
+        unsigned debugOffset() { return m_buffer.debugOffset(); }
 #endif
 
         // Patching helpers
