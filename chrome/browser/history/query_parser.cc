a212150e1b2976105ddc8307b3b7bc8897abe47b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 21:55:45 +0000

    history: Make QueryParser non-copyable.
    Unrelated changes:
    - Tweak several comments, rephrasing some of them and matching the style when
      referring to parameters.
    - Put some arguments onto the same line when they fit.
    
    BUG=None
    TEST=None
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7015025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/query_parser.cc
+++ chrome/browser/history/query_parser.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/i18n/break_iterator.h"
 #include "base/i18n/case_conversion.h"
