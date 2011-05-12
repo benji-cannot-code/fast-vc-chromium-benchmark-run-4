7129a31910394fae985689bb5ba6c538c4d340c1
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 21:59:46 +0000

    Print Preview: Disable controls when printing an embedded PDF.
    BUG=79524
    TEST=Open a page with an embedded PDF, right click and print.
    Review URL: http://codereview.chromium.org/6969031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -18,10 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/metafile.h"
 #include "printing/units.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebConsoleMessage.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebDataSource.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebElement.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebNode.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebURLResponse.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebSize.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURLRequest.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
@@ -496,6 +498,16 @@ void PrintWebViewHelper::GetPageSizeAndMarginsInPoints(
   if (margin_left_in_points)
     *margin_left_in_points =
         ConvertPixelsToPointDouble(margin_left_in_pixels);
+}
+
+bool PrintWebViewHelper::IsModifiable(WebKit::WebFrame* frame,
+                                      WebKit::WebNode* node) {
+  if (node)
+    return false;
+  std::string mime(frame->dataSource()->response().mimeType().utf8());
+  if (mime == "application/pdf")
+    return false;
+  return true;
 }
 
 void PrintWebViewHelper::UpdatePrintableSizeInPrintParameters(
