4d2f3fac50ce3647b2362d9bcc4fb001296ba781
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 07:32:51 +0000

    2011-05-12  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            Fix the build with ENABLE_PLUGIN_PROCESS
            https://bugs.webkit.org/show_bug.cgi?id=60628
    
            * WebProcess/Plugins/PluginProxy.cpp:
            (WebKit::PluginProxy::initialize): Move the call to
            pluginController->isAcceleratedCompositingEnabled() to a #ifdef block.
            * WebProcess/Plugins/PluginProxy.h: Add include for <WebCore/IntRect.h>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginProxy.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginProxy.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Connection.h"
 #include "Plugin.h"
+#include <WebCore/IntRect.h>
 
 #if PLATFORM(MAC)
 #include <wtf/RetainPtr.h>
