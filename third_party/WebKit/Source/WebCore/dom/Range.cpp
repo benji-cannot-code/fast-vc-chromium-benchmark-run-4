8844b3b10add1a2a13ab5d5235caf8b7716e17cf
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 10:38:28 +0000

    2011-05-12  Tony Gentilcore  <tonyg@chromium.org>
            Build fix: include ExceptionCode.h
            https://bugs.webkit.org/show_bug.cgi?id=60694
    
            * dom/Range.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Range.cpp
+++ third_party/WebKit/Source/WebCore/dom/Range.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ClientRect.h"
 #include "ClientRectList.h"
 #include "DocumentFragment.h"
+#include "ExceptionCode.h"
 #include "FloatQuad.h"
 #include "FrameView.h"
 #include "HTMLElement.h"
