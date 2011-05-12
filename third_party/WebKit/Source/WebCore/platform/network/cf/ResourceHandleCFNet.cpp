2a0a4f0fcbdd633e8e7886c8acd20711a80ae146
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 19:10:08 +0000

    [Windows WebKit2] Use cookies set in WebKit1 https://bugs.webkit.org/show_bug.cgi?id=60274
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    Make sure any Private Browsing session is based off the default session.
    
    * platform/network/cf/ResourceHandleCFNet.cpp:
    (WebCore::ResourceHandle::createPrivateBrowsingStorageSession):
    
    Source/WebKit2: 
    
    Properly handle the case where there is no serialized default storage session by not trying
    to encode or decode 0, while still making sure to decode any valid storage session.
    
    * Shared/WebProcessCreationParameters.cpp:
    (WebKit::WebProcessCreationParameters::WebProcessCreationParameters):
    (WebKit::WebProcessCreationParameters::encode):
    Only encode the storage session if there is one to encode.
    (WebKit::WebProcessCreationParameters::decode):
    Only decode the storage session there is one to decode.
    * Shared/WebProcessCreationParameters.h:
    
    WebKitLibraries: 
    
    Update WKSI with the changes to wkCreatePrivateStorageSession.
    
    * win/include/WebKitSystemInterface/WebKitSystemInterface.h:
    * win/lib/WebKitSystemInterface.lib:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/ResourceHandleCFNet.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/ResourceHandleCFNet.cpp
@@ -737,7 +737,7 @@ bool ResourceHandle::willLoadFromCache(ResourceRequest& request, Frame* frame)
 
 RetainPtr<CFURLStorageSessionRef> ResourceHandle::createPrivateBrowsingStorageSession(CFStringRef identifier)
 {
-    return RetainPtr<CFURLStorageSessionRef>(AdoptCF, wkCreatePrivateStorageSession(identifier));
+    return RetainPtr<CFURLStorageSessionRef>(AdoptCF, wkCreatePrivateStorageSession(identifier, defaultStorageSession()));
 }
 
 String ResourceHandle::privateBrowsingStorageSessionIdentifierDefaultBase()
