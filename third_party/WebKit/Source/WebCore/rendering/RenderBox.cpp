33760bb90068bf01bfb9828fab66e781f11d7be4
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-12 22:15:26 +0000

    2011-05-12  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch paintBoxDecorationWithSize to IntRect
            https://bugs.webkit.org/show_bug.cgi?id=60679
    
            Switching paintBoxDecorationWithSize to take an IntRect instead of 4 ints.
    
            No new tests as this is simple refactoring.
    
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::paintBoxDecorations): Cleaning up amazingly confusing logic.
            (WebCore::RenderBox::paintBoxDecorationsWithSize):
            * rendering/RenderBox.h:
            * rendering/RenderTextControlSingleLine.cpp:
            (WebCore::RenderTextControlSingleLine::paintBoxDecorations):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -805,9 +805,8 @@ void RenderBox::paintRootBoxFillLayers(const PaintInfo& paintInfo)
 
 void RenderBox::paintBoxDecorations(PaintInfo& paintInfo, int tx, int ty)
 {
-    if (!paintInfo.shouldPaintWithinRoot(this))
-        return;
-    return paintBoxDecorationsWithSize(paintInfo, tx, ty, width(), height());
+    if (paintInfo.shouldPaintWithinRoot(this))
+        paintBoxDecorationsWithSize(paintInfo, IntRect(tx, ty, width(), height()));
 }
 
 BackgroundBleedAvoidance RenderBox::determineBackgroundBleedAvoidance(GraphicsContext* context) const
@@ -833,9 +832,8 @@ BackgroundBleedAvoidance RenderBox::determineBackgroundBleedAvoidance(GraphicsCo
     return BackgroundBleedUseTransparencyLayer;
 }
 
-void RenderBox::paintBoxDecorationsWithSize(PaintInfo& paintInfo, int tx, int ty, int width, int height)
+void RenderBox::paintBoxDecorationsWithSize(PaintInfo& paintInfo, IntRect paintRect)
 {
-    IntRect paintRect = IntRect(tx, ty, width, height);
     // border-fit can adjust where we paint our border and background.  If set, we snugly fit our line box descendants.  (The iChat
     // balloon layout is an example of this).
     borderFitAdjust(paintRect);
