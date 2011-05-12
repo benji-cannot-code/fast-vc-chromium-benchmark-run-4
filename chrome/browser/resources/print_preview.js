677813d3465ea599c09ec1c56dede86382033f16
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 23:41:34 +0000

    Print Preview: Detecting plugin existence before generating the preview.
    In case there is no compatible pdf plugin the user is immediately notified
    that preview is not available instead of waitingi for the preview to be
    generated and then being notified.
    
    BUG=82202
    TEST=Load a multi-page doc in print preview, observe the error messages.
    
    
    Review URL: http://codereview.chromium.org/6982030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview.js
+++ chrome/browser/resources/print_preview.js
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var localStrings = new LocalStrings();
 
-// Whether or not the PDF plugin supports all the capabilities needed for
-// print preview.
-var hasCompatiblePDFPlugin = true;
-
 // The total page count of the previewed document regardless of which pages the
 // user has selected.
 var totalPageCount = -1;
@@ -48,10 +44,19 @@ const MANAGE_PRINTERS = 'Manage Printers';
  * the printer list.
  */
 function onLoad() {
+  $('system-dialog-link').addEventListener('click', showSystemDialog);
+  $('cancel-button').addEventListener('click', handleCancelButtonClick);
+
+  if (!checkCompatiblePluginExists()) {
+    displayErrorMessage(localStrings.getString('noPlugin'));
+    $('mainview').parentElement.removeChild($('dummy-viewer'));
+    return;
+  }
+  $('mainview').parentElement.removeChild($('dummy-viewer'));
+
   $('printer-list').disabled = true;
   $('print-button').disabled = true;
   $('print-button').addEventListener('click', printFile);
-  $('cancel-button').addEventListener('click', handleCancelButtonClick);
   $('all-pages').addEventListener('click', onPageSelectionMayHaveChanged);
   $('copies').addEventListener('input', copiesFieldChanged);
   $('print-pages').addEventListener('click', handleIndividualPagesCheckbox);
@@ -68,7 +73,6 @@ function onLoad() {
   $('bw').addEventListener('click', function() { setColor(false); });
   $('printer-list').addEventListener(
       'change', updateControlsWithSelectedPrinterCapabilities);
-  $('system-dialog-link').addEventListener('click', showSystemDialog);
   $('increment').addEventListener('click',
                                   function() { onCopiesButtonsClicked(1); });
   $('decrement').addEventListener('click',
@@ -323,7 +327,6 @@ function printFile() {
 function requestPrintPreview() {
   isPreviewStillLoading = true;
   setControlsDisabled(true);
-  $('dancing-dots').classList.remove('hidden');
   $('dancing-dots').classList.remove('invisible');
   chrome.send('getPreview', [getSettingsJSON()]);
 }
@@ -374,9 +377,6 @@ function addDestinationListOption(optionText, optionValue, is_default) {
  * @param {boolean} color is true if the PDF plugin should display in color.
  */
 function setColor(color) {
-  if (!hasCompatiblePDFPlugin) {
-    return;
-  }
   var pdfViewer = $('pdf-viewer');
   if (!pdfViewer) {
     return;
@@ -390,6 +390,7 @@ function setColor(color) {
  */
 function displayErrorMessage(errorMessage) {
   isPreviewStillLoading = false;
+  $('dancing-dots').classList.remove('invisible');
   $('dancing-dots-text').classList.add('hidden');
   $('error-text').innerHTML = errorMessage;
   $('error-text').classList.remove('hidden');
@@ -463,10 +464,6 @@ function updatePrintPreview(pageCount, jobTitle, modifiable) {
  * Create the PDF plugin or reload the existing one.
  */
 function createPDFPlugin() {
-  if (!hasCompatiblePDFPlugin) {
-    return;
-  }
-
   // Enable the print button.
   if (!$('printer-list').disabled) {
     $('print-button').disabled = false;
@@ -492,13 +489,6 @@ function createPDFPlugin() {
   pdfPlugin.setAttribute('src', 'chrome://print/print.pdf');
   var mainView = $('mainview');
   mainView.appendChild(pdfPlugin);
-
-  // Check to see if the PDF plugin is our PDF plugin. (or compatible)
-  if (!pdfPlugin.onload) {
-    hasCompatiblePDFPlugin = false;
-    displayErrorMessage(localStrings.getString('noPlugin'));
-    return;
-  }
   pdfPlugin.onload('onPDFLoad()');
 
   // Older version of the PDF plugin may not have this method.
@@ -508,6 +498,14 @@ function createPDFPlugin() {
   }
 
   pdfPlugin.grayscale(true);
+}
+
+/**
+ * Returns true if a compatible pdf plugin exists, false if it doesn't.
+ */
+function checkCompatiblePluginExists() {
+  var dummyPlugin = $('dummy-viewer')
+  return !!dummyPlugin.onload;
 }
 
 /**
