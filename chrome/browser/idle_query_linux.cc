3b6d194e3c65e4cfb22caf5d032f4757b46115c0
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 16:20:48 +0000

    Use GetXDisplay() instead of XOpenDisplay() in IdleQueryLinux.
    We weren't checking that XOpenDisplay was actually succeeding before
    attempting to use the display.
    
    BUG=82308
    TEST=none
    
    Review URL: http://codereview.chromium.org/7015007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85138 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/idle_query_linux.cc
+++ chrome/browser/idle_query_linux.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/idle_query_linux.h"
 
-#include <X11/Xlib.h>
 #include <X11/extensions/scrnsaver.h>
+#include "ui/base/x/x11_util.h"
 
 namespace browser {
 
@@ -15,8 +15,8 @@ class IdleData {
   IdleData() {
     int event_base;
     int error_base;
-    display = XOpenDisplay(NULL);
-    if (XScreenSaverQueryExtension(display, &event_base, &error_base)) {
+    if (XScreenSaverQueryExtension(ui::GetXDisplay(), &event_base,
+                                   &error_base)) {
       mit_info = XScreenSaverAllocInfo();
     } else {
       mit_info = NULL;
@@ -24,16 +24,11 @@ class IdleData {
   }
 
   ~IdleData() {
-    if (display) {
-      XCloseDisplay(display);
-      display = NULL;
-    }
     if (mit_info)
       XFree(mit_info);
   }
 
   XScreenSaverInfo *mit_info;
-  Display *display;
 };
 
 IdleQueryLinux::IdleQueryLinux() : idle_data_(new IdleData()) {}
@@ -41,11 +36,11 @@ IdleQueryLinux::IdleQueryLinux() : idle_data_(new IdleData()) {}
 IdleQueryLinux::~IdleQueryLinux() {}
 
 int IdleQueryLinux::IdleTime() {
-  if (!idle_data_->mit_info || !idle_data_->display)
+  if (!idle_data_->mit_info)
     return 0;
 
-  if (XScreenSaverQueryInfo(idle_data_->display,
-                            RootWindow(idle_data_->display, 0),
+  if (XScreenSaverQueryInfo(ui::GetXDisplay(),
+                            RootWindow(ui::GetXDisplay(), 0),
                             idle_data_->mit_info)) {
     return (idle_data_->mit_info->idle) / 1000;
   } else {
