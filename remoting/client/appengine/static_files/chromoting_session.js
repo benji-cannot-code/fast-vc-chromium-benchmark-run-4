1e5d1f81524548ff3428da25640c4beefbe4fd17
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-12 06:59:19 +0000

    Keep quiet when there's no IQ data to process.
    BUG=
    TEST=Start a Remoting Viewer, let it idle for a while, and check the Debug Log for messages about Malformed XML packets.  There should be no such messages.
    
    
    Review URL: http://codereview.chromium.org/6973023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85103 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/appengine/static_files/chromoting_session.js
+++ remoting/client/appengine/static_files/chromoting_session.js
@@ -12,6 +12,7 @@ var MAX_DEBUG_LOG_SIZE = 1000;
 chromoting.messageId = 1;
 
 chromoting.scaleToFit = false;
+
 // Default to trying to sandboxed connections.
 chromoting.connectMethod = 'sandboxed';
 
@@ -24,12 +25,16 @@ function feedIq() {
   xhr.withCredentials = true;
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      if (xhr.status == 200 || xhr.status == 204) {
+      if (xhr.status == 200) {
         addToDebugLog('Receiving Iq: --' + xhr.responseText + '--');
         chromoting.plugin.onIq(xhr.responseText);
+      }
+      if (xhr.status == 200 || xhr.status == 204) {
         window.setTimeout(feedIq, 0);
       } else {
         addToDebugLog("HttpXmpp gateway returned code: " + xhr.status);
+        chromoting.plugin.disconnect();
+        setClientStateMessage("Failed");
       }
     }
   }
@@ -52,7 +57,6 @@ function registerConnection() {
         chromoting.plugin.connectSandboxed(clientjid, chromoting.hostjid, '');
         // TODO(ajwong): This should just be feedIq();
         window.setTimeout(feedIq, 1000);
-        window.setTimeout(updateStatusBarStats, 1000);
       } else {
         addToDebugLog('FailedToConnect: --' + xhr.responseText +
                       '-- (status=' + xhr.status + ')');
@@ -130,7 +134,6 @@ function init() {
       // TODO:(jamiewalch): Pass in the correct nonce.
       plugin.connect(chromoting.username, chromoting.hostjid,
                      chromoting.xmppAuthToken, '');
-      window.setTimeout("updateStatusBarStats()", 1000);
     }
   } else {
     addToDebugLog('ERROR: chromoting plugin not loaded');
@@ -213,6 +216,7 @@ function connectionInfoUpdateCallback() {
   } else if (status == chromoting.plugin.STATUS_CONNECTED) {
     desktopSizeChanged();
     setClientStateMessageFade('Connected to ' + chromoting.hostname, 1000);
+    window.setTimeout(updateStatusBarStats, 1000);
   } else if (status == chromoting.plugin.STATUS_CLOSED) {
     setClientStateMessage('Closed');
   } else if (status == chromoting.plugin.STATUS_FAILED) {
@@ -325,6 +329,8 @@ function addToDebugLog(message) {
  * @param {string} message The message to display.
  */
 function updateStatusBarStats() {
+  if (chromoting.plugin.status != chromoting.plugin.STATUS_CONNECTED)
+    return;
   var videoBandwidth = chromoting.plugin.videoBandwidth;
   var videoCaptureLatency = chromoting.plugin.videoCaptureLatency;
   var videoEncodeLatency = chromoting.plugin.videoEncodeLatency;
