4840e9a784fa6865943668381388b046cf594f67
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 21:31:36 +0000

    linux: glx_window_ is an XID, so it can't be NULLed
    Fixes a compiler warning.
    
    Review URL: http://codereview.chromium.org/6969074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85325 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_surface_glx.cc
+++ ui/gfx/gl/gl_surface_glx.cc
@@ -78,7 +78,7 @@ Display* GLSurfaceGLX::GetDisplay() {
 NativeViewGLSurfaceGLX::NativeViewGLSurfaceGLX(gfx::PluginWindowHandle window)
   : window_(window),
     config_(NULL),
-    glx_window_(NULL) {
+    glx_window_(0) {
 }
 
 NativeViewGLSurfaceGLX::~NativeViewGLSurfaceGLX() {
@@ -160,7 +160,7 @@ bool NativeViewGLSurfaceGLX::Initialize() {
 void NativeViewGLSurfaceGLX::Destroy() {
   if (glx_window_) {
     glXDestroyWindow(g_display, glx_window_);
-    glx_window_ = NULL;
+    glx_window_ = 0;
   }
 
   config_ = NULL;
