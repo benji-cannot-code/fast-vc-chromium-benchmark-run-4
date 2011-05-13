cef37071ca0d5ac45ab10bf822e78a4f67b26979
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 11:05:15 +0000

    Implemented RequestHeaders field in extraInfoSpec of webRequest API
    BUG=60101
    TEST=no
    
    Review URL: http://codereview.chromium.org/6931055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_network_delegate.h
+++ chrome/browser/net/chrome_network_delegate.h
@@ -47,7 +47,8 @@ class ChromeNetworkDelegate : public net::NetworkDelegate {
                                   net::CompletionCallback* callback,
                                   net::HttpRequestHeaders* headers);
   virtual void OnRequestSent(uint64 request_id,
-                             const net::HostPortPair& socket_address);
+                             const net::HostPortPair& socket_address,
+                             const net::HttpRequestHeaders& headers);
   virtual void OnBeforeRedirect(net::URLRequest* request,
                                 const GURL& new_location);
   virtual void OnResponseStarted(net::URLRequest* request);
