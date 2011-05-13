69533f3fcced37d6a0b7aea3765f255adf5cb19d
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-13 06:16:01 +0000

    2011-05-12  Pratik Solanki  <psolanki@apple.com>
            Reviewed by Antti Koivisto.
    
            Part of WebCore should use CFNetwork-based loader on Mac
            https://bugs.webkit.org/show_bug.cgi?id=51836
    
            Implement ResourceHandle::schedule()/unschedule() based on CF for Mac.
    
            * WebCore.exp.in:
            * platform/cf/SchedulePair.h:
            * platform/mac/SchedulePairMac.mm:
            * platform/network/cf/ResourceHandleCFNet.cpp:
            (WebCore::ResourceHandle::schedule):
            (WebCore::ResourceHandle::unschedule):
    2011-05-12  Pratik Solanki  <psolanki@apple.com>
    
            Reviewed by Antti Koivisto.
    
            Part of WebCore should use CFNetwork-based loader on Mac
            https://bugs.webkit.org/show_bug.cgi?id=51836
    
            Use CFRunLoop when using CFNetwork.
    
            * WebView/WebView.mm:
            (-[WebView scheduleInRunLoop:forMode:]):
            (-[WebView unscheduleFromRunLoop:forMode:]):
    2011-05-12  Pratik Solanki  <psolanki@apple.com>
    
            Reviewed by Antti Koivisto.
    
            Part of WebCore should use CFNetwork-based loader on Mac
            https://bugs.webkit.org/show_bug.cgi?id=51836
    
            Use CFRunLoop when using CFNetwork.
    
            * WebProcess/WebPage/mac/WebPageMac.mm:
            (WebKit::WebPage::platformInitialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/SchedulePairMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/SchedulePairMac.mm
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "SchedulePair.h"
 
+#if !USE(CFNETWORK)
+
 namespace WebCore {
 
 SchedulePair::SchedulePair(NSRunLoop* runLoop, CFStringRef mode)
@@ -42,3 +44,5 @@ SchedulePair::SchedulePair(NSRunLoop* runLoop, CFStringRef mode)
 }
 
 } // namespace
+
+#endif
