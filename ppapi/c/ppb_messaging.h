ffb795385e22986c20bddd4b423ba20b9181c0d5
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 23:11:54 +0000

    Fix event dispatching for PPB_Messaging::PostMessage.  I asked Adam about the "plugin.onmessage = function(x){...}" style vs the 'plugin.addEventListener' style, and it seems that we should prefer supporting the addEventListener style.
    This breaks the ability to do "plugin.onmessage =" :-(.  I expected dispatchEvent to do it for me, but it does not.  To add that support (if desired), I could work on making dispatchEvent do it, or I could fake it out via the script in MessageChannel (Basically have the old path of invoking onmessage directly, plus the new dispatchEvent code).
    
    Brett, please focus on PPAPI-specific stuff.  Adam, please focus on my event code (especially message_channel.cc and the JavaScript in test_post_message.cc).
    
    BUG=None
    TEST=test_post_message.cc
    
    
    Review URL: http://codereview.chromium.org/6901060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85348 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_messaging.h
+++ ppapi/c/ppb_messaging.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @file
  * This file defines the PPB_Messaging interface implemented by the browser.
  * The PPB_Messaging interface contains pointers to functions related to
- * sending messages to the JavaScript onmessage handler on the DOM element
+ * sending messages to JavaScript message event listeners on the DOM element
  * associated with a specific module instance.
  *
  * @addtogroup Interfaces
@@ -25,23 +25,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * The PPB_Messaging interface contains pointers to functions related to
- * sending messages to the JavaScript onmessage handler on the DOM element
+ * sending messages to JavaScript message event listeners on the DOM element
  * associated with a specific module instance.
  */
 struct PPB_Messaging {
   /**
-   * @a PostMessage is a pointer to a function which asynchronously invokes the
-   * onmessage handler on the DOM element for the given module instance, if one
-   * exists. This means that a call to @a PostMessage will not block while the
+   * @a PostMessage is a pointer to a function which asynchronously invokes any
+   * listeners for message events on the DOM element for the given module
+   * instance. This means that a call to @a PostMessage will not block while the
    * message is processed.
    *
    * @param message is a PP_Var containing the data to be sent to JavaScript.
    * Currently, it can have an int32_t, double, bool, or string value (objects
    * are not supported.)
    *
-   * The onmessage handler in JavaScript code will receive an object conforming
-   * to the MessageEvent interface. In particular, the value of @a message will
-   * be contained as a property called @a data in the received MessageEvent.
+   * Listeners for message events in JavaScript code will receive an object
+   * conforming to the MessageEvent interface. In particular, the value of @a
+   * message will be contained as a property called @a data in the received
+   * MessageEvent.
+   *
    * This is analogous to listening for messages from Web Workers.
    *
    * See:
@@ -55,9 +57,10 @@ struct PPB_Messaging {
    *   <object id="plugin"
    *           type="application/x-ppapi-postMessage-example"/>
    *   <script type="text/javascript">
-   *     document.getElementById('plugin').onmessage = function(message) {
-   *       alert(message.data);
-   *     }
+   *     var plugin = document.getElementById('plugin');
+   *     plugin.AddEventListener("message",
+   *                             function(message) { alert(message.data); },
+   *                             false);
    *   </script>
    * </body>
    *
