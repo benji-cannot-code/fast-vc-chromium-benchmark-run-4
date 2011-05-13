6e45d4cd874e77883bc7fd093bbd1dbb3e6df3ff
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-13 03:13:11 +0000

    2011-05-12  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            [Chromium] Add a WebSetting to control DNS prefetch
            https://bugs.webkit.org/show_bug.cgi?id=60736
    
            DNS prefetch got disabled in Chrome accidentially in
            http://trac.webkit.org/changeset/74041.  This patch adds a WebSettings
            API so we can enable it again.
    
            * public/WebSettings.h:
            * src/WebSettingsImpl.cpp:
            (WebKit::WebSettingsImpl::setDNSPrefetchingEnabled):
            * src/WebSettingsImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
@@ -76,6 +76,7 @@ public:
     virtual void setDownloadableBinaryFontsEnabled(bool);
     virtual void setJavaScriptCanAccessClipboard(bool);
     virtual void setXSSAuditorEnabled(bool);
+    virtual void setDNSPrefetchingEnabled(bool);
     virtual void setLocalStorageEnabled(bool);
     virtual void setEditableLinkBehaviorNeverLive();
     virtual void setFrameFlatteningEnabled(bool);
