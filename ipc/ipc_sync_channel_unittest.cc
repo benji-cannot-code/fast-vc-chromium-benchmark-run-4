48bccc17f91ba20089e6f2151ec941892c26dfff
kushi.p@gmail.com <kushi.p@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 22:42:41 +0000

    Removing 'using namespace' style violations in src/ipc/
    BUG=82078
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/6973055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85340 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_channel_unittest.cc
+++ ipc/ipc_sync_channel_unittest.cc
@@ -25,8 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_sync_message_unittest.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using namespace IPC;
 using base::WaitableEvent;
+
+namespace IPC {
 
 namespace {
 
@@ -128,7 +129,7 @@ class Worker : public Channel::Listener, public Message::Sender {
   void Done() { done_->Signal(); }
 
  protected:
-  IPC::SyncChannel* channel() { return channel_.get(); }
+  SyncChannel* channel() { return channel_.get(); }
   // Functions for dervied classes to implement if they wish.
   virtual void Run() { }
   virtual void OnAnswer(int* answer) { NOTREACHED(); }
@@ -461,7 +462,7 @@ TEST_F(IPCSyncChannelTest, Unblock) {
 
 //-----------------------------------------------------------------------------
 
-// Tests that the the IPC::SyncChannel object can be deleted during a Send.
+// Tests that the the SyncChannel object can be deleted during a Send.
 TEST_F(IPCSyncChannelTest, ChannelDeleteDuringSend) {
   Unblock(false, false, true);
   Unblock(false, true, true);
@@ -1065,7 +1066,7 @@ TEST_F(IPCSyncChannelTest, DoneEventRace) {
 
 namespace {
 
-class TestSyncMessageFilter : public IPC::SyncMessageFilter {
+class TestSyncMessageFilter : public SyncMessageFilter {
  public:
   TestSyncMessageFilter(base::WaitableEvent* shutdown_event, Worker* worker)
       : SyncMessageFilter(shutdown_event),
@@ -1179,7 +1180,7 @@ class RestrictedDispatchServer : public Worker {
 
   void OnDoPing(int ping) {
     // Send an asynchronous message that unblocks the caller.
-    IPC::Message* msg = new SyncChannelTestMsg_Ping(ping);
+    Message* msg = new SyncChannelTestMsg_Ping(ping);
     msg->set_unblock(true);
     Send(msg);
     // Signal the event after the message has been sent on the channel, on the
@@ -1316,3 +1317,5 @@ TEST_F(IPCSyncChannelTest, RestrictedDispatch) {
   RunTest(workers);
   EXPECT_EQ(3, success);
 }
+
+}  // namespace IPC
