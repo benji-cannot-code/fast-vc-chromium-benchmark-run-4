5516327324077ff62ea04304e3d146dd4c7da10e
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-13 19:54:33 +0000

    Load plugins in the order they're found
    Fixes <http://webkit.org/b/60788> <rdar://problem/9435787> REGRESSION (r69790): WebKit2
    doesn't prefer plugins in the additional plugins directory
    
    Reviewed by Anders Carlsson.
    
    * UIProcess/Plugins/PluginInfoStore.cpp:
    (WebKit::addFromVector):
    Changed to use a ListHashSet instead of a HashSet to store the plugin paths.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86456 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginInfoStore.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginInfoStore.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/KURL.h>
 #include <WebCore/MIMETypeRegistry.h>
 #include <algorithm>
+#include <wtf/ListHashSet.h>
 #include <wtf/StdLibExtras.h>
 
 using namespace std;
@@ -53,17 +54,19 @@ void PluginInfoStore::refresh()
     m_pluginListIsUpToDate = false;
 }
 
-template <typename T, typename U, typename V, typename W>
-static void addFromVector(HashSet<T, U, V>& hashSet, const W& vector)
+template <typename T, typename U>
+static void addFromVector(T& hashSet, const U& vector)
 {
     for (size_t i = 0; i < vector.size(); ++i)
         hashSet.add(vector[i]);
 }
 
+// We use a ListHashSet so that plugins will be loaded from the additional plugins directories first
+// (which in turn means those plugins will be preferred if two plugins claim the same MIME type).
 #if OS(WINDOWS)
-typedef HashSet<String, CaseFoldingHash> PathHashSet;
+typedef ListHashSet<String, 32, CaseFoldingHash> PathHashSet;
 #else
-typedef HashSet<String> PathHashSet;
+typedef ListHashSet<String, 32> PathHashSet;
 #endif
 
 void PluginInfoStore::loadPluginsIfNecessary()
