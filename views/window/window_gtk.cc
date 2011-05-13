286219ede4fbe98e2c998a325a78dc1eb636a5f5
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 10:45:13 +0000

    Removing DeleteSoon() from WigetGtk so it behaves like WidgetWin
    This is a retry of http://codereview.chromium.org/7002029/ after it has been reverted because of the failing test BrowserTest.CloseWithAppMenuOpen (failed only on buildbot)
    
    BUG=chromium-os:15129
    TEST=tests
    TBR=dpolukhin
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85257 0039d316-1c4b-4281-b951-d872f2087c98

--- views/window/window_gtk.cc
+++ views/window/window_gtk.cc
@@ -444,6 +444,9 @@ void WindowGtk::SaveWindowPosition() {
 void WindowGtk::OnDestroy(GtkWidget* widget) {
   delegate_->OnNativeWindowDestroying();
   WidgetGtk::OnDestroy(widget);
+}
+
+void WindowGtk::OnDestroyed() {
   delegate_->OnNativeWindowDestroyed();
 }
 
