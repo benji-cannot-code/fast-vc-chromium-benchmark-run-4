a01108022e64159e33aa66f57c5c5be9b29fca99
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 01:54:21 +0000

    Pass net_log parameter properly for ProxyResolvingClientSocket
    Also initialize a few more fields in the session_params object.
    
    Remove unused net_log params.
    
    BUG=82365
    TEST=Turn on sync, look in chrome://sync-internals for entries for talk.google.com
    
    Review URL: http://codereview.chromium.org/7014009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85235 0039d316-1c4b-4281-b951-d872f2087c98

--- jingle/notifier/base/chrome_async_socket_unittest.cc
+++ jingle/notifier/base/chrome_async_socket_unittest.cc
@@ -19,10 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "jingle/notifier/base/resolving_client_socket_factory.h"
-#include "net/base/capturing_net_log.h"
 #include "net/base/cert_verifier.h"
 #include "net/base/net_errors.h"
-#include "net/base/net_log.h"
 #include "net/base/ssl_config_service.h"
 #include "net/socket/socket_test_util.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -132,9 +130,9 @@ class MockXmppClientSocketFactory : public ResolvingClientSocketFactory {
 
   // ResolvingClientSocketFactory implementation.
   virtual net::StreamSocket* CreateTransportClientSocket(
-      const net::HostPortPair& host_and_port, net::NetLog* net_log) {
+      const net::HostPortPair& host_and_port) {
     return mock_client_socket_factory_->CreateTransportClientSocket(
-        address_list_, net_log, net::NetLog::Source());
+        address_list_, NULL, net::NetLog::Source());
   }
 
   virtual net::SSLClientSocket* CreateSSLClientSocket(
@@ -158,7 +156,6 @@ class ChromeAsyncSocketTest
  protected:
   ChromeAsyncSocketTest()
       : ssl_socket_data_provider_(true, net::OK),
-        capturing_net_log_(net::CapturingNetLog::kUnbounded),
         addr_(0xaabbccdd, 35) {}
 
   virtual ~ChromeAsyncSocketTest() {}
@@ -177,8 +174,7 @@ class ChromeAsyncSocketTest
             SocketAddressToAddressList(addr_)));
     chrome_async_socket_.reset(
         new ChromeAsyncSocket(mock_xmpp_client_socket_factory.release(),
-                              14, 20,
-                              &capturing_net_log_)),
+                              14, 20)),
 
     chrome_async_socket_->SignalConnected.connect(
         this, &ChromeAsyncSocketTest::OnConnect);
@@ -432,7 +428,6 @@ class ChromeAsyncSocketTest
   AsyncSocketDataProvider async_socket_data_provider_;
   net::SSLSocketDataProvider ssl_socket_data_provider_;
 
-  net::CapturingNetLog capturing_net_log_;
   scoped_ptr<ChromeAsyncSocket> chrome_async_socket_;
   std::deque<SignalSocketState> signal_socket_states_;
   const talk_base::SocketAddress addr_;
--- jingle/notifier/base/proxy_resolving_client_socket_unittest.cc
+++ jingle/notifier/base/proxy_resolving_client_socket_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/message_loop.h"
-#include "net/base/capturing_net_log.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -45,7 +44,6 @@ class ProxyResolvingClientSocketTest : public testing::Test {
  protected:
   ProxyResolvingClientSocketTest()
       : url_request_context_getter_(new TestURLRequestContextGetter()),
-        capturing_net_log_(net::CapturingNetLog::kUnbounded),
         connect_callback_(ALLOW_THIS_IN_INITIALIZER_LIST(this),
                           &ProxyResolvingClientSocketTest::NetCallback) { }
 
@@ -62,7 +60,6 @@ class ProxyResolvingClientSocketTest : public testing::Test {
   // Needed by XmppConnection.
   MessageLoopForIO message_loop_;
   scoped_refptr<TestURLRequestContextGetter> url_request_context_getter_;
-  net::CapturingNetLog capturing_net_log_;
   net::CompletionCallbackImpl<ProxyResolvingClientSocketTest> connect_callback_;
 };
 
@@ -72,8 +69,7 @@ TEST_F(ProxyResolvingClientSocketTest, DISABLED_ConnectError) {
   ProxyResolvingClientSocket proxy_resolving_socket(
       url_request_context_getter_,
       net::SSLConfig(),
-      dest,
-      &capturing_net_log_);
+      dest);
   // ProxyResolvingClientSocket::Connect() will always return an error of
   // ERR_ADDRESS_INVALID for a 0 IP address.
   EXPECT_CALL(*this, NetCallback(net::ERR_ADDRESS_INVALID)).Times(1);
