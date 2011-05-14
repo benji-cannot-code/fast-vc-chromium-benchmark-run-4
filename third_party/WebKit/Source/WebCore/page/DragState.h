a9534b9496f8d4bed49494226ac19fdb37217092
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-14 00:55:52 +0000

    2011-05-13  Daniel Cheng  <dcheng@chromium.org>
            Fix the build.
    
            Add includes that the builders are suddenly complaining about...
    
            * page/DragState.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DragState.h
+++ third_party/WebKit/Source/WebCore/page/DragState.h
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DragState_h
 #define DragState_h
 
+#include "Clipboard.h"
 #include "DragActions.h"
+#include "Node.h"
 #include <wtf/Forward.h>
 #include <wtf/Noncopyable.h>
 #include <wtf/RefPtr.h>
