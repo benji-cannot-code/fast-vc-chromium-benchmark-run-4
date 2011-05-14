e91f062b4cf74fb260f2a54434566511484b7b71
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-14 01:37:50 +0000

    security_tests: Get rid of "using namespace sandbox;"
    BUG=82078
    TEST=None
    
    Review URL: http://codereview.chromium.org/7015062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85363 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/security_tests/security_tests.cc
+++ chrome/test/security_tests/security_tests.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,8 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/tests/common/controller.h"
 #include "sandbox/tests/validation_tests/commands.h"
 
+using sandbox::TestOpenKey;
+using sandbox::TestOpenReadFile;
+using sandbox::TestOpenWriteFile;
+
 #define SECURITY_CHECK(x) (*test_count)++; \
-                          if (SBOX_TEST_DENIED != x) { \
+                          if (sandbox::SBOX_TEST_DENIED != x) { \
                             return FALSE; \
                           };
 
@@ -26,7 +30,6 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD ul_reason_for_call,
 // If a test fails, the return value is FALSE and test_count contains the
 // number of tests executed, including the failing test.
 BOOL __declspec(dllexport) __cdecl RunRendererTests(int *test_count) {
-  using namespace sandbox;
   *test_count = 0;
   SECURITY_CHECK(TestOpenReadFile(L"%SystemDrive%"));
   SECURITY_CHECK(TestOpenReadFile(L"%SystemRoot%"));
@@ -52,7 +55,6 @@ BOOL __declspec(dllexport) __cdecl RunRendererTests(int *test_count) {
 // If a test fails, the return value is FALSE and test_count contains the
 // number of tests executed, including the failing test.
 BOOL __declspec(dllexport) __cdecl RunPluginTests(int *test_count) {
-  using namespace sandbox;
   *test_count = 0;
   SECURITY_CHECK(TestOpenWriteFile(L"%SystemRoot%"));
   SECURITY_CHECK(TestOpenWriteFile(L"%ProgramFiles%"));
