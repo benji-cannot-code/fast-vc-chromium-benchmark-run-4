9801ebfc976565ae8c55d1d18b83a57c9d0c9533
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-14 02:20:53 +0000

    Use callback pointers as implicit "pending" indicators. Check that Read and Write are passed callbacks in DEBUG builds.
    BUG=
    TEST=Existing tests should continue to work unchanged.
    
    
    Review URL: http://codereview.chromium.org/7012014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85365 0039d316-1c4b-4281-b951-d872f2087c98

--- jingle/glue/channel_socket_adapter.h
+++ jingle/glue/channel_socket_adapter.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,12 +52,10 @@ class TransportChannelSocketAdapter : public net::Socket,
 
   cricket::TransportChannel* channel_;
 
-  bool read_pending_;
   net::CompletionCallback* read_callback_;  // Not owned.
   scoped_refptr<net::IOBuffer> read_buffer_;
   int read_buffer_size_;
 
-  bool write_pending_;
   net::CompletionCallback* write_callback_;  // Not owned.
   scoped_refptr<net::IOBuffer> write_buffer_;
   int write_buffer_size_;
