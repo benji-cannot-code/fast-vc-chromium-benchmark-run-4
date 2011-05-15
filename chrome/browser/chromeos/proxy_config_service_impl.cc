1ac51500a86f8f42d8744cf60e9876ffbfa3f153
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-15 20:42:29 +0000

    cros: build fix
    BUG=none
    TEST=none
    TBR=dpolukhin
    
    Review URL: http://codereview.chromium.org/7026003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/proxy_config_service_impl.cc
+++ chrome/browser/chromeos/proxy_config_service_impl.cc
@@ -110,9 +110,9 @@ bool ProxyConfigServiceImpl::ProxyConfig::Setting::CanBeWrittenByUser(
 
 //----------- ProxyConfigServiceImpl::ProxyConfig: public methods --------------
 
-ProxyConfig::ProxyConfig() : mode(MODE_DIRECT) {}
+ProxyConfigServiceImpl::ProxyConfig::ProxyConfig() : mode(MODE_DIRECT) {}
 
-ProxyConfig::~ProxyConfig() {}
+ProxyConfigServiceImpl::ProxyConfig::~ProxyConfig() {}
 
 void ProxyConfigServiceImpl::ProxyConfig::ToNetProxyConfig(
     net::ProxyConfig* net_config) {
