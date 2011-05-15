fabb73dd4aa8de08822df6c715a81eddaa9e85bc
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-15 23:54:51 +0000

    2011-05-15  Daniel Bates  <dbates@webkit.org>
            Reviewed by Chris Jerdonek.
    
            svn-apply can't handle Git diff that only changes executable bit
            https://bugs.webkit.org/show_bug.cgi?id=60848
    
            Fixes an issue where svn-{apply, unapply} fail to handle a Git diff for a file
            that only has an executable bit change.
    
            * Scripts/VCSUtils.pm:
              - Modified regex $chunkRangeRegEx to match chunk range with ending text, say: @@ -2,6 +2,18 @@ foo().
            (parseDiff): Modified to count the number of text chunks in the diff. This should also help towards
            fixing <https://bugs.webkit.org/show_bug.cgi?id=29684>.
            * Scripts/svn-apply:
            (patch): Only use standard patch tool if the patch is non-empty and for a text file (i.e. not a binary patch).
            * Scripts/svn-unapply:
            (patch): Ditto.
            * Scripts/webkitperl/VCSUtils_unittest/parseDiff.pl:
              - Updated expected results for test cases to account for numTextChunks.
              - Fixed malformed chunk range in test cases: "Git: simple", and "Git: Git diff followed by SVN diff".
              - Added test case "Git: file that only has an executable bit change".
            * Scripts/webkitperl/VCSUtils_unittest/parseDiffWithMockFiles.pl:
              - Updated expected results for test cases to account for numTextChunks.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitperl/VCSUtils_unittest/parseDiff.pl
+++ third_party/WebKit/Tools/Scripts/webkitperl/VCSUtils_unittest/parseDiff.pl
@@ -62,6 +62,7 @@ Index: Makefile
 END
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "53052",
 }],
 undef],
@@ -176,6 +177,7 @@ Index: Makefile
 END
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "53052",
 }],
 undef],
@@ -227,6 +229,7 @@ Index: Makefile
 END
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "53131",
 }],
 "Index: Makefile_new\n"],
@@ -265,6 +268,7 @@ index f5d5e74..3b6aa92 100644
 END
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "53131",
 }],
 undef],
@@ -309,6 +313,7 @@ END
     executableBitDelta => 1,
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 undef],
@@ -349,6 +354,7 @@ Index: Makefile
 END
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 "Property changes on: Makefile.shared\n"],
@@ -796,6 +802,7 @@ END
     executableBitDelta => 1,
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 "Property changes on: Makefile.shared\n"],
@@ -847,6 +854,7 @@ END
     executableBitDelta => 1,
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 "Property changes on: Makefile.shared\r\n"],
@@ -898,6 +906,7 @@ END
     executableBitDelta => -1,
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 "Index: Makefile.shared\n"],
@@ -951,6 +960,7 @@ END
     executableBitDelta => -1,
     indexPath => "Makefile",
     isSvn => 1,
+    numTextChunks => 1,
     sourceRevision => "60021",
 }],
 "Index: Makefile.shared\r\n"],
@@ -967,7 +977,7 @@ diff --git a/Makefile b/Makefile
 index f5d5e74..3b6aa92 100644
 --- a/Makefile
 +++ b/Makefile
-@@ -1,1 1,1 @@ public:
+@@ -1,1 +1,1 @@ public:
 END
     expectedReturn => [
 [{
@@ -976,10 +986,11 @@ Index: Makefile
 index f5d5e74..3b6aa92 100644
 --- Makefile
 +++ Makefile
-@@ -1,1 1,1 @@ public:
+@@ -1,1 +1,1 @@ public:
 END
     indexPath => "Makefile",
     isGit => 1,
+    numTextChunks => 1,
 }],
 undef],
     expectedNextLine => undef,
@@ -1011,6 +1022,7 @@ END
     indexPath => "foo.h",
     isGit => 1,
     isNew => 1,
+    numTextChunks => 1,
 }],
 "diff --git a/bar b/bar\n"],
     expectedNextLine => "index d45dd40..3494526 100644\n",
@@ -1042,6 +1054,7 @@ END
     indexPath => "foo",
     isDeletion => 1,
     isGit => 1,
+    numTextChunks => 1,
 }],
 "diff --git a/bar b/bar\n"],
     expectedNextLine => "index d45dd40..3494526 100644\n",
@@ -1054,7 +1067,7 @@ diff --git a/Makefile b/Makefile
 index f5d5e74..3b6aa92 100644
 --- a/Makefile
 +++ b/Makefile
-@@ -1,1 1,1 @@ public:
+@@ -1,1 +1,1 @@ public:
 Index: Makefile_new
 ===================================================================
 --- Makefile_new	(revision 53131)	(from Makefile:53131)
@@ -1066,13 +1079,37 @@ Index: Makefile
 index f5d5e74..3b6aa92 100644
 --- Makefile
 +++ Makefile
-@@ -1,1 1,1 @@ public:
+@@ -1,1 +1,1 @@ public:
 Index: Makefile_new
 ===================================================================
 --- Makefile_new	(revision 53131)	(from Makefile:53131)
 END
     indexPath => "Makefile",
     isGit => 1,
+    numTextChunks => 1,
+}],
+undef],
+    expectedNextLine => undef,
+},
+{
+    # New test
+    diffName => "Git: file that only has an executable bit change",
+    inputText => <<'END',
+diff --git a/foo b/foo
+old mode 100644
+new mode 100755
+END
+    expectedReturn => [
+[{
+    svnConvertedText =>  <<'END',
+Index: foo
+old mode 100644
+new mode 100755
+END
+    executableBitDelta => 1,
+    indexPath => "foo",
+    isGit => 1,
+    numTextChunks => 0,
 }],
 undef],
     expectedNextLine => undef,
@@ -1132,6 +1169,7 @@ END
 {
     indexPath => "foo_new",
     isGit => 1,
+    numTextChunks => 1,
     svnConvertedText => <<'END',
 Index: foo_new
 similarity index 99%
@@ -1175,6 +1213,7 @@ END
     executableBitDelta => 1,
     indexPath => "foo_new",
     isGit => 1,
+    numTextChunks => 0,
     svnConvertedText => <<'END',
 Index: foo_new
 old mode 100644
