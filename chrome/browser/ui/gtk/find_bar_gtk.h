a30ebe71c3e4b305ad8316f18693d8a8f8cf2506
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-15 20:13:34 +0000

    gtk: Convert FindBarGtk::OnClicked event to CHROMEGTK_ macro.
    BUG=None
    TEST=None
    
    R=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/6961010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/find_bar_gtk.h
+++ chrome/browser/ui/gtk/find_bar_gtk.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/slide_animator_gtk.h"
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
+#include "ui/base/gtk/gtk_signal.h"
 #include "ui/gfx/point.h"
 
 class Browser;
@@ -128,7 +129,7 @@ class FindBarGtk : public FindBar,
                                     FindBarGtk* find_bar);
 
   // Callback for previous, next, and close button.
-  static void OnClicked(GtkWidget* button, FindBarGtk* find_bar);
+  CHROMEGTK_CALLBACK_0(FindBarGtk, void, OnClicked);
 
   // Handles shapping and drawing the find bar background.
   static gboolean OnExpose(GtkWidget* widget, GdkEventExpose* event,
