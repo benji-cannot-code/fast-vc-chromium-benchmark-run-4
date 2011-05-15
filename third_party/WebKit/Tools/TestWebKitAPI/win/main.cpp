9e3d635304d8eabc6d6853a568e680d4eff6e59d
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-15 23:02:17 +0000

    Fix windows build.
    * TestWebKitAPI/win/main.cpp:
    (main):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/win/main.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/win/main.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,20 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "TestsController.h"
 
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
     // Cygwin calls ::SetErrorMode(SEM_FAILCRITICALERRORS), which we will inherit. This is bad for
     // testing/debugging, as it causes the post-mortem debugger not to be invoked. We reset the
     // error mode here to work around Cygwin's behavior. See <http://webkit.org/b/55222>.
     ::SetErrorMode(0);
 
-    bool passed = true;
-
-    std::string argument(argv[1]);
-    if (argument == "--dump-tests")
-        TestWebKitAPI::TestsController::shared().dumpTestNames();
-    else   
-        passed = TestWebKitAPI::TestsController::shared().runTestNamed(argument);
+    bool passed = TestWebKitAPI::TestsController::shared().run(argc, argv);
 
     return passed ? EXIT_SUCCESS : EXIT_FAILURE;
 }
