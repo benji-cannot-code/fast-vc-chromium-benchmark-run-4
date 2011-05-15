2f379b083e0c525773d86d2579a7658efe7e4a69
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-15 01:05:49 +0000

    Revert 85394 - This change broke "PrerenderBrowserTest.PrerenderWindowOpenerJsOpenInNewPageTest" on all platforms.
    Original description:
    
    Changes to not use the prerendered contents when window.opener needs to be set.
    
    BUG=79922
    TEST=browser_tests
    
    
    Review URL: http://codereview.chromium.org/6915019
    
    TBR=shishir@chromium.org
    Review URL: http://codereview.chromium.org/7022005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85401 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents.cc
+++ content/browser/tab_contents/tab_contents.cc
@@ -1016,7 +1016,6 @@ void TabContents::SetContentRestrictions(int restrictions) {
 
 void TabContents::OnDidStartProvisionalLoadForFrame(int64 frame_id,
                                                     bool is_main_frame,
-                                                    bool has_opener_set,
                                                     const GURL& url) {
   bool is_error_page = (url.spec() == chrome::kUnreachableWebDataURL);
   GURL validated_url(url);
@@ -1034,12 +1033,11 @@ void TabContents::OnDidStartProvisionalLoadForFrame(int64 frame_id,
   if (is_main_frame) {
     // Notify observers about the provisional change in the main frame URL.
     FOR_EACH_OBSERVER(TabContentsObserver, observers_,
-                      ProvisionalChangeToMainFrameUrl(url, has_opener_set));
+                      ProvisionalChangeToMainFrameUrl(url));
   }
 }
 
 void TabContents::OnDidRedirectProvisionalLoad(int32 page_id,
-                                               bool has_opener_set,
                                                const GURL& source_url,
                                                const GURL& target_url) {
   // TODO(creis): Remove this method and have the pre-rendering code listen to
@@ -1055,8 +1053,7 @@ void TabContents::OnDidRedirectProvisionalLoad(int32 page_id,
 
   // Notify observers about the provisional change in the main frame URL.
   FOR_EACH_OBSERVER(TabContentsObserver, observers_,
-                    ProvisionalChangeToMainFrameUrl(target_url,
-                                                    has_opener_set));
+                    ProvisionalChangeToMainFrameUrl(target_url));
 }
 
 void TabContents::OnDidFailProvisionalLoadWithError(
