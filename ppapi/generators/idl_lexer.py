f414920943565d471fd610696906427728a36948
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-15 22:08:56 +0000

    Add idl_option
    Cleanup IDL parser by unifying option processing code.
    
    BUG=77551
    TEST= python idl_parser.py --test
    Review URL: http://codereview.chromium.org/6993005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85433 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_lexer.py
+++ ppapi/generators/idl_lexer.py
@@ -18,18 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # PLY can be found at:
 #   http://www.dabeaz.com/ply/
 
-import getopt
 import os.path
 import re
 import sys
-
-LEXER_OPTIONS = {
-  'output': False,
-  'test_expect' : False,
-  'test_same' : False,
-  'verbose': False
-}
-
 
 #
 # Try to load the ply module, if not, then assume it is in the third_party
@@ -43,7 +34,12 @@ except:
   sys.path.append(third_party)
   from ply import lex
 
+from idl_option import GetOption, Option, ParseOptions
 
+
+Option('output', 'Generate output.')
+Option('test_same', 'Test that the output matches.')
+Option('test_expect', 'Test that the output matches.')
 
 #
 # IDL Lexer
@@ -173,10 +169,8 @@ class IDLLexer(object):
     self.index = [0]
     self.lexobj.input(data)
 
-  def __init__(self, options = {}):
+  def __init__(self):
     self.lexobj = lex.lex(object=self, lextab=None, optimize=0)
-    for k in options:
-      LEXER_OPTIONS[k] = True
 
 
 
@@ -222,12 +216,10 @@ def TextToTokens(source):
 # old set.
 #
 def TestSame(values):
-  global LEXER_OPTIONS
-
   src1 = ' '.join(values)
   src2 = ' '.join(TextToTokens(src1))
 
-  if LEXER_OPTIONS['output']:
+  if GetOption('output'):
     sys.stdout.write('Generating original.txt and tokenized.txt\n')
     open('original.txt', 'w').write(src1)
     open('tokenized.txt', 'w').write(src2)
@@ -274,35 +266,15 @@ def TestExpect(tokens):
 
 
 def Main(args):
-  global LEXER_OPTIONS
+  filenames = ParseOptions(args)
 
   try:
-    long_opts = ['output', 'verbose', 'test_expect', 'test_same']
-    usage = 'Usage: idl_lexer.py %s [<src.idl> ...]' % ' '.join(
-       ['--%s' % opt for opt in long_opts])
-
-    opts, filenames = getopt.getopt(args, '', long_opts)
-  except getopt.error, e:
-    sys.stderr.write('Illegal option: %s\n%s\n' % (str(e), usage))
-    return 1
-
-  output = False
-  test_same = False
-  test_expect = False
-  verbose = False
-
-  for opt, val in opts:
-    LEXER_OPTIONS[opt[2:]] = True
-
-  try:
-    tokens = FilesToTokens(filenames, verbose)
+    tokens = FilesToTokens(filenames, GetOption('verbose'))
     values = [tok.value for tok in tokens]
-    if LEXER_OPTIONS['output']: sys.stdout.write(' <> '.join(values) + '\n')
-    if LEXER_OPTIONS['test_same']:
+    if GetOption('output'): sys.stdout.write(' <> '.join(values) + '\n')
+    if GetOption('test'):
       if TestSame(values):
         return -1
-
-    if LEXER_OPTIONS['test_expect']:
       if TestExpect(tokens):
         return -1
     return 0
@@ -310,7 +282,6 @@ def Main(args):
   except lex.LexError as le:
     sys.stderr.write('%s\n' % str(le))
   return -1
-
 
 if __name__ == '__main__':
   sys.exit(Main(sys.argv[1:]))
