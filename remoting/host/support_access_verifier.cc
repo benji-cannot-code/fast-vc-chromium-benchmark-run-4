9bf72b7b9af01c3ec8508938a16b23374ad7ba11
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 16:30:05 +0000

    Store (and check) support id in SupportAccessVerifier.
    BUG=None
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7013065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85492 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/support_access_verifier.cc
+++ remoting/host/support_access_verifier.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdlib.h>
 #include <vector>
+#include <iostream>
 
 #include "base/logging.h"
 #include "base/rand_util.h"
@@ -61,10 +62,26 @@ bool SupportAccessVerifier::VerifyPermissions(
     const std::string& client_jid,
     const std::string& encoded_access_token) {
   CHECK(initialized_);
+  if (support_id_.empty())
+    return false;
   // TODO(jamiewalch): This needs to compose the support id with the host
   // secret before verification.
+  std::string access_code = support_id_ + "-" + host_secret_;
   return protocol::VerifySupportAuthToken(
-      client_jid, host_secret_, encoded_access_token);
+      client_jid, access_code, encoded_access_token);
+}
+
+void SupportAccessVerifier::OnMe2MomHostRegistered(
+    bool successful, const std::string& support_id) {
+  if (successful) {
+    // TODO(jamiewalch): cout is only needed so that remoting_simple_host can
+    // report the support id to the user. Find a better way (issue 82651).
+    std::cout << "Support id: "
+              << support_id << std::endl;
+    support_id_ = support_id;
+  } else {
+    LOG(ERROR) << "Failed to register support host";
+  }
 }
 
 }  // namespace remoting
