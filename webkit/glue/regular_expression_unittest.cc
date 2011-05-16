9e094092a64e1095dd153a840f4cc4240c47bffb
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 22:18:49 +0000

    'using namespace' fix in webkit/glue
    BUG=82078
    TEST=none
    
    Review URL: http://codereview.chromium.org/7013047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85543 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/regular_expression_unittest.cc
+++ webkit/glue/regular_expression_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebTextCaseSensitivity.h"
 
-using namespace WebKit;
+using WebKit::WebRegularExpression;
+using WebKit::WebString;
+using WebKit::WebTextCaseInsensitive;
+using WebKit::WebTextCaseSensitive;
+using WebKit::WebUChar;
 
 namespace {
 
