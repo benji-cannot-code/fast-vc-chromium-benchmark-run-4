b4e9d20a958f93b5049d63f272040e0283d4897f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 00:01:28 +0000

    cros: More out-of-lining in mocks.
    Also replace EXPECT_TRUE with DCHECK in a few non-test classes.
    EXPECT_TRUE does not what you expect in classes that are not test classes.
    
    BUG=none
    TEST=none
    TBR=dpolukhin
    
    Review URL: http://codereview.chromium.org/7034005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/mock_owner_key_utils.cc
+++ chrome/browser/chromeos/login/mock_owner_key_utils.cc
@@ -7,4 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+MockKeyUtils::MockKeyUtils() {}
+
+MockKeyUtils::~MockKeyUtils() {}
+
+MockInjector::MockInjector(MockKeyUtils* mock) : transient_(mock) {}
+
+MockInjector::~MockInjector() {}
+
+OwnerKeyUtils* MockInjector::CreateOwnerKeyUtils() {
+  return transient_.get();
+}
 }  // namespace chromeos
--- chrome/browser/chromeos/login/mock_owner_key_utils.h
+++ chrome/browser/chromeos/login/mock_owner_key_utils.h
@@ -22,7 +22,9 @@ namespace chromeos {
 
 class MockKeyUtils : public OwnerKeyUtils {
  public:
-  MockKeyUtils() {}
+  MockKeyUtils();
+  virtual ~MockKeyUtils();
+
   MOCK_METHOD2(ImportPublicKey, bool(const FilePath& key_file,
                                      std::vector<uint8>* output));
   MOCK_METHOD3(Verify, bool(const std::string& data,
@@ -46,16 +48,12 @@ class MockKeyUtils : public OwnerKeyUtils {
 class MockInjector : public OwnerKeyUtils::Factory {
  public:
   // Takes ownership of |mock|.
-  explicit MockInjector(MockKeyUtils* mock) : transient_(mock) {
-  }
-
-  virtual ~MockInjector() {}
+  explicit MockInjector(MockKeyUtils* mock);
+  virtual ~MockInjector();
 
   // If this is called, its caller takes ownership of |transient_|.
   // If it's never called, |transient_| remains our problem.
-  OwnerKeyUtils* CreateOwnerKeyUtils() {
-    return transient_.get();
-  }
+  virtual OwnerKeyUtils* CreateOwnerKeyUtils();
 
  private:
   scoped_refptr<MockKeyUtils> transient_;
