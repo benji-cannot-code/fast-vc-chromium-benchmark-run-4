00b314c567d1e69236bae82707b72bac2934bc03
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-16 07:01:28 +0000

    2011-05-12  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Darin Adler.
    
            Move mediaControlsButtonCoordinates() out of video-test.js
            https://bugs.webkit.org/show_bug.cgi?id=60693
    
            Moved the function to a new js file called media-controls.js. Also
            made the function throw an exception if the button is not found
            and handle the exception in the various tests using the function.
    
            * media/audio-delete-while-step-button-clicked.html:
            * media/media-controls.js: Added.
            (mediaControlsButtonCoordinates):
            * media/video-controls-transformed.html:
            * media/video-controls-visible-audio-only.html:
            * media/video-controls-zoomed.html:
            * media/video-test.js:
            (isInTimeRanges):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/media/media-controls.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+function mediaControlsButtonCoordinates(element, id)
+{
+    var button;
+    var controlsShadow = layoutTestController.shadowRoot(element).firstChild.firstChild;
+    for (child = controlsShadow.firstChild; child; child = child.nextSibling) {
+        if (layoutTestController.shadowPseudoId(child) == "-webkit-media-controls-" + id) {
+            button = child;
+            break;
+        }
+    }
+
+    if (!button)
+        throw "Failed to find button " + id;
+
+    var buttonBoundingRect = button.getBoundingClientRect();
+    var x = buttonBoundingRect.left + buttonBoundingRect.width / 2;
+    var y = buttonBoundingRect.top + buttonBoundingRect.height / 2;
+    return new Array(x, y);
+}
