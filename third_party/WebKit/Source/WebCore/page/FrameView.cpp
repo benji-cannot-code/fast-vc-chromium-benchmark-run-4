8fd21cff2f73fd6b22d006ff541a4fbadbd6e7b0
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-16 19:25:38 +0000

    Fix for broken regression tests. Adding null pointer check
    * page/FrameView.cpp:
    (WebCore::FrameView::didAddHorizontalScrollbar):
    (WebCore::FrameView::willRemoveHorizontalScrollbar):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameView.cpp
@@ -310,7 +310,7 @@ void FrameView::detachCustomScrollbars()
 
 void FrameView::didAddHorizontalScrollbar(Scrollbar* scrollbar)
 {
-    if (m_frame->document())
+    if (m_frame && m_frame->document())
         m_frame->document()->didAddWheelEventHandler();
     ScrollView::didAddHorizontalScrollbar(scrollbar);
 }
@@ -319,7 +319,7 @@ void FrameView::willRemoveHorizontalScrollbar(Scrollbar* scrollbar)
 {
     ScrollView::willRemoveHorizontalScrollbar(scrollbar);
     // FIXME: maybe need a separate ScrollableArea::didRemoveHorizontalScrollbar callback?
-    if (m_frame->document())
+    if (m_frame && m_frame->document())
         m_frame->document()->didRemoveWheelEventHandler();
 }
 
