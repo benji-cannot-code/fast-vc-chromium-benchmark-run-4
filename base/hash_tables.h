780115ed8ed947f1f023fe8c49c23988035f3cba
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 23:40:05 +0000

    wstring: Remove the ability to put wstrings in hash_map<>s.
    (On Windows, where wstring==string16, either will still work.)
    
    BUG=23581
    
    Review URL: http://codereview.chromium.org/7031017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85558 0039d316-1c4b-4281-b951-d872f2087c98

--- base/hash_tables.h
+++ base/hash_tables.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -95,13 +95,7 @@ DEFINE_TRIVIAL_HASH(unsigned long long);
     }
 
 DEFINE_STRING_HASH(std::string);
-DEFINE_STRING_HASH(std::wstring);
-
-#if defined(WCHAR_T_IS_UTF32)
-// If string16 and std::wstring are not the same type, provide a
-// specialization for string16.
 DEFINE_STRING_HASH(string16);
-#endif  // WCHAR_T_IS_UTF32
 
 #undef DEFINE_STRING_HASH
 
