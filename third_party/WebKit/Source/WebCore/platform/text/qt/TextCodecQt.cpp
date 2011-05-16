96835dbf55d633eee1702a024a9187a3bd928eab
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-16 06:41:08 +0000

    2011-05-15  Adam Barth  <abarth@webkit.org>
            Attempt to fix Qt build.  (Strict PassOwnPtr fix.)
    
            * platform/text/qt/TextCodecQt.cpp:
            (WebCore::newTextCodecQt):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/qt/TextCodecQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/qt/TextCodecQt.cpp
@@ -65,7 +65,7 @@ void TextCodecQt::registerEncodingNames(EncodingNameRegistrar registrar)
 
 static PassOwnPtr<TextCodec> newTextCodecQt(const TextEncoding& encoding, const void*)
 {
-    return new TextCodecQt(encoding);
+    return adoptPtr(new TextCodecQt(encoding));
 }
 
 void TextCodecQt::registerCodecs(TextCodecRegistrar registrar)
