bb8879e6ac4573a1e03acb900bcc3754068d467f
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 13:42:55 +0000

    Add GetLRUOrigin into QuotaManager.
    BUG=61676
    TEST=QuotaDatabaseTest.*, (no tests for QuotaManagerTest.*).
    
    
    Review URL: http://codereview.chromium.org/7033006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85480 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_manager.h
+++ webkit/quota/quota_manager.h
@@ -47,6 +47,7 @@ class QuotaManager : public QuotaTaskObserver,
                     int64 /* quota */>::Type GetUsageAndQuotaCallback;
   typedef Callback2<QuotaStatusCode,
                     int64 /* granted_quota */>::Type RequestQuotaCallback;
+  typedef Callback1<GURL>::Type GetLRUOriginCallback;
 
   QuotaManager(bool is_incognito,
                const FilePath& profile_path,
@@ -118,6 +119,7 @@ class QuotaManager : public QuotaTaskObserver,
   class TemporaryGlobalQuotaUpdateTask;
   class PersistentHostQuotaUpdateTask;
   class PersistentHostQuotaQueryTask;
+  class GetLRUOriginTask;
 
   class UsageAndQuotaDispatcherTask;
   class UsageAndQuotaDispatcherTaskForTemporary;
@@ -140,6 +142,11 @@ class QuotaManager : public QuotaTaskObserver,
   // Registers a quota client to the manager.
   // The client must remain valid until OnQuotaManagerDestored is called.
   void RegisterClient(QuotaClient* client);
+
+  // TODO(dmikurube): Add a test for this method.
+  virtual void GetLRUOrigin(
+      StorageType type,
+      GetLRUOriginCallback* callback);
 
   UsageTracker* GetUsageTracker(StorageType type) const;
 
