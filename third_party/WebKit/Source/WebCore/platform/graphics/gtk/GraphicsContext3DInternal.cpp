0755ab41459c309dc2dade4361fc40d7c6dee26e
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-16 12:05:01 +0000

    2011-05-16  Naiem Shaik  <naiem.shaik@gmail.com>
            Reviewed by Adam Barth.
    
            This is for fixing build break in webgl due to https://bugs.webkit.org/show_bug.cgi?id=59861
            The bug raised for fixing this is https://bugs.webkit.org/show_bug.cgi?id=60867
    
            No new tests. This does not change any functionality.
    
            * platform/graphics/gtk/GraphicsContext3DGtk.cpp:
            (WebCore::GraphicsContext3D::create):
            * platform/graphics/gtk/GraphicsContext3DInternal.cpp:
            (WebCore::GraphicsContext3DInternal::create):
            * platform/graphics/gtk/GraphicsContext3DInternal.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gtk/GraphicsContext3DInternal.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gtk/GraphicsContext3DInternal.cpp
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include <wtf/PassOwnPtr.h>
 #include "GraphicsContext3DInternal.h"
 
 #if ENABLE(WEBGL)
@@ -88,10 +89,10 @@ void GraphicsContext3DInternal::cleanupActiveContextsAtExit()
     gSharedDisplay = 0;
 }
 
-GraphicsContext3DInternal* GraphicsContext3DInternal::create()
+PassOwnPtr<GraphicsContext3DInternal> GraphicsContext3DInternal::create()
 {
     if (!sharedDisplay())
-        return 0;
+        return nullptr;
 
     static bool initialized = false;
     static bool success = true;
@@ -100,17 +101,17 @@ GraphicsContext3DInternal* GraphicsContext3DInternal::create()
         initialized = true;
     }
     if (!success)
-        return 0;
+        return nullptr;
 
     GraphicsContext3DInternal* internal = createPbufferContext();
     if (!internal)
         internal = createPixmapContext();
     if (!internal)
-        return 0;
+        return nullptr;
 
     // The GraphicsContext3D constructor requires that this context is the current OpenGL context.
     internal->makeContextCurrent();
-    return internal;
+    return adoptPtr(internal);
 }
 
 GraphicsContext3DInternal* GraphicsContext3DInternal::createPbufferContext()
