2ff80aaecc851bd8c8047467637517f606271eb7
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 20:04:26 +0000

    In ChromeDriver, add support for WebDriver's alert commands.
    BUG=None
    TEST=None
    
    R=tfarina@chromium.org,hnguyen@chromium.org
    
    
    Review URL: http://codereview.chromium.org/6952005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -823,6 +823,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test/webdriver/webdriver_key_converter.cc',
         'test/webdriver/web_element_id.h',
         'test/webdriver/web_element_id.cc',
+        'test/webdriver/commands/alert_commands.h',
+        'test/webdriver/commands/alert_commands.cc',
         'test/webdriver/commands/command.h',
         'test/webdriver/commands/command.cc',
         'test/webdriver/commands/cookie_commands.h',
--- chrome/test/webdriver/server.cc
+++ chrome/test/webdriver/server.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/webdriver/error_codes.h"
 #include "chrome/test/webdriver/session_manager.h"
 #include "chrome/test/webdriver/utility_functions.h"
+#include "chrome/test/webdriver/commands/alert_commands.h"
 #include "chrome/test/webdriver/commands/cookie_commands.h"
 #include "chrome/test/webdriver/commands/create_session.h"
 #include "chrome/test/webdriver/commands/execute_async_script_command.h"
@@ -127,7 +128,10 @@ void InitCallbacks(struct mg_context* ctx, Dispatcher* dispatcher,
   // fetch the page title. If mapped first, this would overwrite the handler
   // for /session/*/element/*/attribute/title, which should fetch the title
   // attribute of the element.
+  dispatcher->Add<AcceptAlertCommand>(  "/session/*/accept_alert");
+  dispatcher->Add<AlertTextCommand>(    "/session/*/alert_text");
   dispatcher->Add<BackCommand>(         "/session/*/back");
+  dispatcher->Add<DismissAlertCommand>( "/session/*/dismiss_alert");
   dispatcher->Add<ExecuteCommand>(      "/session/*/execute");
   dispatcher->Add<ExecuteAsyncScriptCommand>(
                                         "/session/*/execute_async");
