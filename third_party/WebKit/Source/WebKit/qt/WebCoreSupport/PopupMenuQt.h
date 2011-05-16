689c04605c0b108e0b3dbfb00974a9c3dc7fff05
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-16 07:27:51 +0000

    2011-05-16  Adam Barth  <abarth@webkit.org>
            Missing include.
    
            * WebCoreSupport/PopupMenuQt.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PopupMenuQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PopupMenuQt.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PopupMenu.h"
 #include <QObject>
+#include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
 class QWebSelectData;
