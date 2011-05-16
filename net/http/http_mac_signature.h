831f22fcaf9de7e264fc2cd9772c6dd66feca697
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 22:18:41 +0000

    MAC Cookies (patch 5 of N)
    To help folks deal with clock skew, we've changed the timestamp to the "age" of
    the credential, which is the number of seconds since the cookie was set (as
    opposed to the number of seconds since the epoch).  This patch updates our
    implementation to match.
    
    Review URL: http://codereview.chromium.org/6969075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85542 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_mac_signature.h
+++ net/http/http_mac_signature.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/gtest_prod_util.h"
+#include "base/time.h"
 #include "crypto/hmac.h"
 
 namespace net {
@@ -28,6 +29,7 @@ class HttpMacSignature {
 
   // Returns whether this information is valid.
   bool AddStateInfo(const std::string& id,
+                    const base::Time& creation_date,
                     const std::string& mac_key,
                     const std::string& mac_algorithm);
 
@@ -45,14 +47,15 @@ class HttpMacSignature {
   FRIEND_TEST_ALL_PREFIXES(HttpMacSignatureTest, GenerateNormalizedRequest);
   FRIEND_TEST_ALL_PREFIXES(HttpMacSignatureTest, GenerateMAC);
 
-  std::string GenerateHeaderString(const std::string& timestamp,
+  std::string GenerateHeaderString(const std::string& age,
                                    const std::string& nonce);
-  std::string GenerateNormalizedRequest(const std::string& timestamp,
+  std::string GenerateNormalizedRequest(const std::string& age,
                                         const std::string& nonce);
-  std::string GenerateMAC(const std::string& timestamp,
+  std::string GenerateMAC(const std::string& age,
                           const std::string& nonce);
 
   std::string id_;
+  base::Time creation_date_;
   std::string mac_key_;
   crypto::HMAC::HashAlgorithm mac_algorithm_;
 
