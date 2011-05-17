6151fa571bd91c4b44a62cb269c7f3812760d6a2
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 16:17:45 +0000

    Cancel prerenders for pages that call window.print()
    BUG=82206
    TEST=PrerenderBrowserTest.PrerenderPopup
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=85545
    
    Review URL: http://codereview.chromium.org/7015026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85639 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_render_view_host_observer.cc
+++ chrome/browser/prerender/prerender_render_view_host_observer.cc
@@ -42,6 +42,8 @@ bool PrerenderRenderViewHostObserver::OnMessageReceived(
     IPC_MESSAGE_HANDLER(ViewHostMsg_RunJavaScriptMessage,
                         OnRunJavaScriptMessage)
     IPC_MESSAGE_HANDLER(ViewHostMsg_RenderViewGone, OnRenderViewGone)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_CancelPrerenderForPrinting,
+                        OnCancelPrerenderForPrinting)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
@@ -98,7 +100,11 @@ void PrerenderRenderViewHostObserver::OnUpdateFaviconURL(
 }
 
 void PrerenderRenderViewHostObserver::OnMaybeCancelPrerenderForHTML5Media() {
-  prerender_contents_->OnMaybeCancelPrerenderForHTML5Media();
+  prerender_contents_->Destroy(FINAL_STATUS_HTML5_MEDIA);
+}
+
+void PrerenderRenderViewHostObserver::OnCancelPrerenderForPrinting() {
+  prerender_contents_->Destroy(FINAL_STATUS_WINDOW_PRINT);
 }
 
 }
