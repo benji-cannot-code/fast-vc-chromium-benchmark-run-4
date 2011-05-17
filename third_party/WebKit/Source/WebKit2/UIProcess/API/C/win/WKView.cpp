1cbfefffeeb012ddbc33d86b0744ca6699b8ff8b
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 23:15:06 +0000

    WebKit2: Needs API to set a custom drop target https://bugs.webkit.org/show_bug.cgi?id=60991 <rdar://problem/9090868>
    Reviewed by Adam Roben.
    
    Add API to set a custom drop target on a WKView.
    
    * UIProcess/API/C/win/WKView.cpp:
    (WKViewSetCustomDropTarget): Call through to WebView::setCustomDropTarget.
    * UIProcess/API/C/win/WKView.h:
    * UIProcess/win/WebView.cpp:
    (WebKit::WebView::setCustomDropTarget): Revoke the current drop target, and register the
        custom one.
    * UIProcess/win/WebView.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.cpp
@@ -120,3 +120,8 @@ bool WKViewDrawsTransparentBackground(WKViewRef viewRef)
     return toImpl(viewRef)->page()->drawsTransparentBackground();
 }
 
+void WKViewSetCustomDropTarget(WKViewRef viewRef, IDropTarget* dropTarget)
+{
+    return toImpl(viewRef)->setCustomDropTarget(dropTarget);
+}
+
