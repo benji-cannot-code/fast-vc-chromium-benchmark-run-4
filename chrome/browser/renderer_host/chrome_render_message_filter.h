17e96cee554cb78f47e6ea82f1967bc94786e30a
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 18:20:54 +0000

    Experimental CL to test webrequest event handling on the IO thread.
    Currently, webrequest events need to do a roundtrip from browser IO thread ->
    UI -> renderer -> UI -> IO. This CL skips the UI bounce, and tries just IO ->
    renderer -> IO. I want to check this in temporarily and get some timing info
    from the buildbot to see if this has an effect. (Running locally gave me noisy
    data and was hard to analyze.)
    
    BUG=no
    TEST=no
    
    Review URL: http://codereview.chromium.org/7013062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_message_filter.h
+++ chrome/browser/renderer_host/chrome_render_message_filter.h
@@ -12,11 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_message_filter.h"
 #include "content/common/dom_storage_common.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
+#include "chrome/browser/profiles/profile.h"
 
 class FilePath;
 class GURL;
 class HostContentSettingsMap;
 class Profile;
+struct ExtensionHostMsg_Request_Params;
 
 namespace net {
 class URLRequestContextGetter;
@@ -75,6 +77,7 @@ class ChromeRenderMessageFilter : public BrowserMessageFilter {
       const std::string& extension_id,
       const std::string& default_locale,
       IPC::Message* reply_msg);
+  void OnExtensionRequest(const ExtensionHostMsg_Request_Params& params);
   void OnExtensionAddListener(const std::string& extension_id,
                               const std::string& event_name);
   void OnExtensionRemoveListener(const std::string& extension_id,
@@ -117,6 +120,7 @@ class ChromeRenderMessageFilter : public BrowserMessageFilter {
   // The Profile associated with our renderer process.  This should only be
   // accessed on the UI thread!
   Profile* profile_;
+  ProfileId profile_id_;
   scoped_refptr<net::URLRequestContextGetter> request_context_;
   // Used to look up permissions at database creation time.
   scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
