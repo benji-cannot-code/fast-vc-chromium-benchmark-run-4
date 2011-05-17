4c0bc7d0aa6e5a64914286ef2fd2b2f0f95df6d3
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 16:25:34 +0000

    Memory leak fix in FileSystemOperationWriteTest
    BUG=none
    TEST=FileSystemOperationWriteTest.*
    TBR=hbono@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85640 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_operation_write_unittest.cc
+++ webkit/fileapi/file_system_operation_write_unittest.cc
@@ -218,6 +218,7 @@ void FileSystemOperationWriteTest::SetUp() {
 void FileSystemOperationWriteTest::TearDown() {
   net::URLRequest::RegisterProtocolFactory("blob", NULL);
   quota_manager_ = NULL;
+  MessageLoop::current()->RunAllPending();
 }
 
 FileSystemOperation* FileSystemOperationWriteTest::operation() {
