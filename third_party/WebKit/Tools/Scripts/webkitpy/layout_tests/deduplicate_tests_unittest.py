ebb9603a1e6de3a5cfc160a1bc86afdc364d0de1
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 19:29:25 +0000

    2011-05-17  Tony Chang  <tony@chromium.org>
            Reviewed by Ojan Vafai.
    
            [chromium] move Lucid 64 bit results into LayoutTests/platform/chromium-linux
            https://bugs.webkit.org/show_bug.cgi?id=60895
    
            Update the tools to handle the move.  Lucid 32 results now go in
            chromium-linux-x86 and the default platform on Linux is now x86_64.
    
            * Scripts/webkitpy/layout_tests/deduplicate_tests.py: Default to x86_64
            * Scripts/webkitpy/layout_tests/deduplicate_tests_unittest.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_expectations.py: Default to Lucid
            * Scripts/webkitpy/layout_tests/port/base.py: ditto
            * Scripts/webkitpy/layout_tests/port/chromium_linux.py: Update directory fallback and default to Lucid 64
            * Scripts/webkitpy/layout_tests/port/chromium_linux_unittest.py:
            * Scripts/webkitpy/layout_tests/port/test.py: Update tests to default to x86_64
            * Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests.py: Update bot names.
            * Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests_unittest.py: Whitespace cleanup.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86696 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/deduplicate_tests_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/deduplicate_tests_unittest.py
@@ -160,12 +160,12 @@ class ListDuplicatesTest(unittest.TestCase):
         self.assertEquals({'test': 'animage.png',
                            'path': 'LayoutTests/platform/chromium-linux/animage.png',
                            'fallback': 'chromium-win',
-                           'platform': 'chromium-linux-x86'},
+                           'platform': 'chromium-linux-x86_64'},
                           result[0])
         self.assertEquals({'test': 'foo-expected.txt',
                            'path': 'LayoutTests/platform/chromium-linux/foo-expected.txt',
                            'fallback': 'chromium-win',
-                           'platform': 'chromium-linux-x86'},
+                           'platform': 'chromium-linux-x86_64'},
                           result[1])
 
         result = deduplicate_tests.deduplicate('*.txt')
@@ -175,7 +175,7 @@ class ListDuplicatesTest(unittest.TestCase):
         self.assertEquals({'test': 'foo-expected.txt',
                            'path': 'LayoutTests/platform/chromium-linux/foo-expected.txt',
                            'fallback': 'chromium-win',
-                           'platform': 'chromium-linux-x86'},
+                           'platform': 'chromium-linux-x86_64'},
                           result[0])
 
         result = deduplicate_tests.deduplicate('*.png')
@@ -185,7 +185,7 @@ class ListDuplicatesTest(unittest.TestCase):
         self.assertEquals({'test': 'animage.png',
                            'path': 'LayoutTests/platform/chromium-linux/animage.png',
                            'fallback': 'chromium-win',
-                           'platform': 'chromium-linux-x86'},
+                           'platform': 'chromium-linux-x86_64'},
                           result[0])
 
     def test_get_relative_test_path(self):
