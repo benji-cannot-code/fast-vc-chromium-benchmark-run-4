950d748a9dc1a106cc721b1234426fece6d27e6f
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 22:32:30 +0000

    Changes to not use the prerendered contents when window.opener needs to be set.
    BUG=79922
    TEST=browser_tests
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=85394
    
    Review URL: http://codereview.chromium.org/6915019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85689 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents_observer.cc
+++ content/browser/tab_contents/tab_contents_observer.cc
@@ -49,7 +49,9 @@ void TabContentsObserver::DidStartProvisionalLoadForFrame(
     RenderViewHost* render_view_host) {
 }
 
-void TabContentsObserver::ProvisionalChangeToMainFrameUrl(const GURL& url) {
+void TabContentsObserver::ProvisionalChangeToMainFrameUrl(
+    const GURL& url,
+    bool has_opener_set) {
 }
 
 void TabContentsObserver::DidCommitProvisionalLoadForFrame(
