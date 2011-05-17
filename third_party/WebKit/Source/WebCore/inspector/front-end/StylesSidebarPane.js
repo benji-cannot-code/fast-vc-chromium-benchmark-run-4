87fbef797d013acfae0a9212a956615d84096afb
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 09:34:49 +0000

    2011-05-17  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: Increment/decrement of very big CSS numeric values results in invalid CSS
            https://bugs.webkit.org/show_bug.cgi?id=60890
    
            * inspector/styles/up-down-numerics-and-colors-expected.txt:
            * inspector/styles/up-down-numerics-and-colors.html:
    2011-05-17  Alexander Pavlov  <apavlov@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: Increment/decrement of very big CSS numeric values results in invalid CSS
            https://bugs.webkit.org/show_bug.cgi?id=60890
    
            * inspector/front-end/MetricsSidebarPane.js:
            (WebInspector.MetricsSidebarPane.prototype._handleKeyDown):
            * inspector/front-end/StylesSidebarPane.js:
            (WebInspector.StylesSidebarPane.alteredFloatNumber):
            (WebInspector.StylePropertyTreeElement.prototype):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
@@ -111,6 +111,8 @@ WebInspector.StylesSidebarPane.PseudoIdNames = [
     "-webkit-resizer", "-webkit-input-list-button", "-webkit-inner-spin-button", "-webkit-outer-spin-button"
 ];
 
+WebInspector.StylesSidebarPane.CSSNumberRegex = /^(-?(?:\d+(?:\.\d+)?|\.\d+))$/;
+
 WebInspector.StylesSidebarPane.alteredFloatNumber = function(number, event)
 {
     var arrowKeyPressed = (event.keyIdentifier === "Up" || event.keyIdentifier === "Down");
@@ -144,6 +146,8 @@ WebInspector.StylesSidebarPane.alteredFloatNumber = function(number, event)
         // Make the new number and constrain it to a precision of 6, this matches numbers the engine returns.
         // Use the Number constructor to forget the fixed precision, so 1.100000 will print as 1.1.
         result = Number((number + changeAmount).toFixed(6));
+        if (!String(result).match(WebInspector.StylesSidebarPane.CSSNumberRegex))
+            return null;
     }
 
     return result;
@@ -1758,6 +1762,10 @@ WebInspector.StylePropertyTreeElement.prototype = {
                 prefix = matches[1];
                 suffix = matches[3];
                 number = WebInspector.StylesSidebarPane.alteredFloatNumber(parseFloat(matches[2]), event);
+                if (number === null) {
+                    // Need to check for null explicitly.
+                    return false;
+                }
 
                 replacementString = prefix + number + suffix;
             }
