95e627a1292f48708191a7ad8e5fc97d49fd9d67
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 01:01:33 +0000

    2011-05-16  Martin Robinson  <mrobinson@igalia.com>
            Fix the Cairo build for older versions of GTK+.
    
            * platform/gtk/GtkWidgetBackingStoreX11.cpp: Include GtkVersioning.h to
            satisfy older versions of GTK+ 2.x.
    2011-05-16  Martin Robinson  <mrobinson@igalia.com>
    
            Take another shot at fixing the WinCairo build.
    
            * Shared/cairo/LayerTreeContextCairo.cpp: Include NotImplemented.h using a framework include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/GtkWidgetBackingStoreX11.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/GtkWidgetBackingStoreX11.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifdef XP_UNIX
 
+#include "GtkVersioning.h"
 #include "RefPtrCairo.h"
 #include <X11/Xlib.h>
 #include <cairo/cairo-xlib.h>
