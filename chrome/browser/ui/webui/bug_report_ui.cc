9b8a5d33c5c2d2bd66e6eb3a15341c10e50a41c5
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 18:14:09 +0000

    Fix for crash in feedback. On sending the report, sometimes feedback seems to crash. It seems like the crash is happening in ClobberScreenshotSource, when we try to access tab_. Right before we call ClobberScreenshotSource, we CloseTabContents on the tab_ object; this probably isn't crashing in the majority case since the browser completely unloads the tab async, allowing ClobberScreenshotSource enough time to access the still valid tab_ object. There is no real reason to Clobber after the tab is closed, hence moving the call to before we actually close the tab and we're gauranteed to have a valid object in tab_
    R=stevenjb@chromium.org,zelidrag@chromium.org
    BUG=15253
    TEST=There is no repro for this crash, so we'll have to see if this gets fixed once we push this out to users.
    
    Review URL: http://codereview.chromium.org/7036013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/bug_report_ui.cc
+++ chrome/browser/ui/webui/bug_report_ui.cc
@@ -718,13 +718,14 @@ void BugReportHandler::CancelFeedbackCollection() {
 }
 
 void BugReportHandler::CloseFeedbackTab() {
+  ClobberScreenshotsSource();
+
   Browser* browser = BrowserList::GetLastActive();
   if (browser) {
     browser->CloseTabContents(tab_);
   } else {
     LOG(FATAL) << "Failed to get last active browser.";
   }
-  ClobberScreenshotsSource();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
