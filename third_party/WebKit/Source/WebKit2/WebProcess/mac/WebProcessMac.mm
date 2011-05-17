f5c37c7d03fb948ad2dc8cb24fb9e596fb8394e4
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 17:12:37 +0000

    Part one of <rdar://problem/8814289> and https://bugs.webkit.org/show_bug.cgi?id=60595 Mac WebKit2 WebProcess needs a shim to make prompts appear to be from the UIProcess
    Reviewed by Anders Carlsson.
    
    Add am empty shim for a new WebProcess shim and install it at launch.
    
    Use the same Shim.xcconfig for both the web and plugin processes:
    * Configurations/PluginProcessShim.xcconfig: Removed.
    * Configurations/Shim.xcconfig: Copied from Configurations/PluginProcessShim.xcconfig.
    * WebKit2.xcodeproj/project.pbxproj:
    
    * WebProcess/WebProcess.h: Add initializeShim() for Mac-only
    * WebProcess/mac/WebProcessMac.mm:
    (WebKit::WebProcess::initializeShim): Initialize the (empty) shim callbacks.
    
    * WebProcess/mac/WebProcessMainMac.mm:
    (WebKit::WebProcessMain): Call initializeShim()
    
    * WebProcess/mac/WebProcessShim.h: Added.
    * WebProcess/mac/WebProcessShim.mm: Added.
    (WebKit::WebKitWebProcessShimInitialize): Empty for now.
    
    * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
    (WebKit::ProcessLauncher::launchProcess): Install the Plugin Process shim for plugin processes
      and the WebProcess shim for the web content process.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMac.mm
@@ -31,17 +31,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "SandboxExtension.h"
 #import "WebPage.h"
 #import "WebProcessCreationParameters.h"
+#import "WebProcessShim.h"
 #import <WebCore/FileSystem.h>
+#import <WebCore/LocalizedStrings.h>
 #import <WebCore/MemoryCache.h>
 #import <WebCore/PageCache.h>
 #import <WebKitSystemInterface.h>
 #import <algorithm>
 #import <dispatch/dispatch.h>
+#import <dlfcn.h>
 #import <mach/host_info.h>
 #import <mach/mach.h>
 #import <mach/mach_error.h>
 #import <objc/runtime.h>
-#import <WebCore/LocalizedStrings.h>
 
 #if ENABLE(WEB_PROCESS_SANDBOX)
 #import <sandbox.h>
@@ -236,6 +238,15 @@ void WebProcess::platformInitializeWebProcess(const WebProcessCreationParameters
     // no window in WK2, NSApplication needs to use the focused page's focused element.
     Method methodToPatch = class_getInstanceMethod([NSApplication class], @selector(accessibilityFocusedUIElement));
     method_setImplementation(methodToPatch, (IMP)NSApplicationAccessibilityFocusedUIElement);
+}
+
+void WebProcess::initializeShim()
+{
+    const WebProcessShimCallbacks callbacks = {
+    };
+    
+    WebProcessShimInitializeFunc initFunc = reinterpret_cast<WebProcessShimInitializeFunc>(dlsym(RTLD_DEFAULT, "WebKitWebProcessShimInitialize"));
+    initFunc(callbacks);
 }
 
 void WebProcess::platformTerminate()
