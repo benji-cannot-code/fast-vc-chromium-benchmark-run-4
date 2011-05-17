2bd15891b337c90aa5e3d64fd3cbe994f690cde4
crogers@google.com <crogers@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 20:01:25 +0000

    Add support for real-time audio threads. BUG=none TEST=none (tested locally on Mac OS X) Review URL: http://codereview.chromium.org/6949009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85663 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -143,6 +143,12 @@ void PlatformThread::Join(PlatformThreadHandle thread_handle) {
   DCHECK_EQ(WAIT_OBJECT_0, result);
 
   CloseHandle(thread_handle);
+}
+
+// static
+void PlatformThread::SetThreadPriority(PlatformThreadHandle, ThreadPriority) {
+  // TODO(crogers): implement
+  NOTIMPLEMENTED();
 }
 
 }  // namespace base
