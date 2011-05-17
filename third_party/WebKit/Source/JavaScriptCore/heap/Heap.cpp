554309c07f405b9f9ff1ea52d2167e118c13e6ef
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 06:12:46 +0000

    2011-05-16  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r86653.
            http://trac.webkit.org/changeset/86653
            https://bugs.webkit.org/show_bug.cgi?id=60944
    
            "Caused regressions on Windows, OSX and EFL" (Requested by
            yutak on #webkit).
    
            * DerivedSources.make:
            * DerivedSources.pro:
            * GNUmakefile.am:
            * GNUmakefile.list.am:
            * JavaScriptCore.exp:
            * JavaScriptCore.gypi:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
            * create_hash_table:
            * heap/Heap.cpp:
            (JSC::TypeCounter::operator()):
            * interpreter/CallFrame.h:
            (JSC::ExecState::arrayTable):
            (JSC::ExecState::numberTable):
            * runtime/ArrayConstructor.cpp:
            (JSC::ArrayConstructor::ArrayConstructor):
            * runtime/ArrayConstructor.h:
            * runtime/ArrayPrototype.cpp:
            (JSC::ArrayPrototype::getOwnPropertySlot):
            (JSC::ArrayPrototype::getOwnPropertyDescriptor):
            * runtime/ArrayPrototype.h:
            * runtime/BooleanPrototype.cpp:
            (JSC::BooleanPrototype::BooleanPrototype):
            * runtime/BooleanPrototype.h:
            * runtime/DateConstructor.cpp:
            (JSC::DateConstructor::DateConstructor):
            * runtime/DateConstructor.h:
            * runtime/ErrorPrototype.cpp:
            (JSC::ErrorPrototype::ErrorPrototype):
            * runtime/ErrorPrototype.h:
            * runtime/JSGlobalData.cpp:
            (JSC::JSGlobalData::JSGlobalData):
            (JSC::JSGlobalData::~JSGlobalData):
            * runtime/JSGlobalData.h:
            * runtime/JSGlobalObject.cpp:
            (JSC::JSGlobalObject::reset):
            * runtime/JSGlobalObject.h:
            (JSC::JSGlobalObject::addStaticGlobals):
            (JSC::JSGlobalObject::getOwnPropertySlot):
            (JSC::JSGlobalObject::getOwnPropertyDescriptor):
            * runtime/JSGlobalObjectFunctions.cpp:
            (JSC::globalFuncJSCPrint):
            * runtime/JSGlobalObjectFunctions.h:
            * runtime/MathObject.cpp:
            * runtime/NumberConstructor.cpp:
            (JSC::NumberConstructor::getOwnPropertySlot):
            (JSC::NumberConstructor::getOwnPropertyDescriptor):
            * runtime/NumberPrototype.cpp:
            (JSC::NumberPrototype::NumberPrototype):
            * runtime/NumberPrototype.h:
            * runtime/ObjectPrototype.cpp:
            (JSC::ObjectPrototype::ObjectPrototype):
            (JSC::ObjectPrototype::put):
            (JSC::ObjectPrototype::getOwnPropertySlot):
            * runtime/ObjectPrototype.h:
            * runtime/RegExpPrototype.cpp:
            (JSC::RegExpPrototype::RegExpPrototype):
            * runtime/RegExpPrototype.h:
            * runtime/StringConstructor.cpp:
            (JSC::StringConstructor::StringConstructor):
            * runtime/StringConstructor.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2011 Apple Inc. All rights reserved.
+ *  Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  *  Copyright (C) 2007 Eric Seidel <eric@webkit.org>
  *
  *  This library is free software; you can redistribute it and/or
@@ -314,7 +314,6 @@ public:
 private:
     const char* typeName(JSCell*);
     OwnPtr<TypeCountSet> m_typeCountSet;
-    HashSet<JSCell*> m_cells;
 };
 
 inline TypeCounter::TypeCounter()
@@ -341,8 +340,6 @@ inline const char* TypeCounter::typeName(JSCell* cell)
 
 inline void TypeCounter::operator()(JSCell* cell)
 {
-    if (!m_cells.add(cell).second)
-        return;
     m_typeCountSet->add(typeName(cell));
 }
 
