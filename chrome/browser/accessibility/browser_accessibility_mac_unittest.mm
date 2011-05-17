d05e75a63645b13c3011008afc5ae1402d59a212
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 04:24:46 +0000

    Implement the ShowMenu action. BUG=65808 TEST=manually with VoiceOver.
    Review URL: http://codereview.chromium.org/5543005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/browser_accessibility_mac_unittest.mm
+++ chrome/browser/accessibility/browser_accessibility_mac_unittest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSPoint)accessibilityPointInScreen:(BrowserAccessibilityCocoa*)accessibility;
 - (void)doDefaultAction:(int32)accessibilityObjectId;
+- (void)performShowMenuAction:(BrowserAccessibilityCocoa*)accessibility;
 - (void)setAccessibilityFocus:(BOOL)focus
               accessibilityId:(int32)accessibilityObjectId;
 - (NSWindow*)window;
@@ -32,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return NSZeroPoint;
 }
 - (void)doDefaultAction:(int32)accessibilityObjectId {
+}
+- (void)performShowMenuAction:(BrowserAccessibilityCocoa*)accessibility {
 }
 - (void)setAccessibilityFocus:(BOOL)focus
               accessibilityId:(int32)accessibilityObjectId {
