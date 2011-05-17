60d37d861970c41546dd205be0c6518f140ecf86
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 04:11:01 +0000

    Display non-ASCII Wifi SSID correctly.
    BUG=chromium-os:8563
    TEST=manually, generating latain-1 SSID with stub and UTF-8 SSID with real AP
    
    Review URL: http://codereview.chromium.org/6973021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85589 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/network_dropdown_button.cc
+++ chrome/browser/chromeos/status/network_dropdown_button.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -103,12 +103,12 @@ void NetworkDropdownButton::OnNetworkManagerChanged(NetworkLibrary* cros) {
         const WifiNetwork* wifi =
             static_cast<const WifiNetwork*>(active_network);
         SetIcon(*IconForNetworkStrength(wifi, true));
-        SetText(ASCIIToWide(wifi->name()));
+        SetText(UTF8ToWide(wifi->name()));
       } else if (active_network->type() == TYPE_CELLULAR) {
         const CellularNetwork* cellular =
             static_cast<const CellularNetwork*>(active_network);
         SetIcon(*IconForNetworkStrength(cellular, true));
-        SetText(ASCIIToWide(cellular->name()));
+        SetText(UTF8ToWide(cellular->name()));
       } else {
         NOTREACHED();
       }
@@ -119,9 +119,9 @@ void NetworkDropdownButton::OnNetworkManagerChanged(NetworkLibrary* cros) {
         SetIcon(*IconForNetworkConnecting(0, true));
       }
       if (cros->wifi_connecting())
-        SetText(ASCIIToWide(cros->wifi_network()->name()));
+        SetText(UTF8ToWide(cros->wifi_network()->name()));
       else if (cros->cellular_connecting())
-        SetText(ASCIIToWide(cros->cellular_network()->name()));
+        SetText(UTF8ToWide(cros->cellular_network()->name()));
     }
 
     if (!cros->Connected() && !cros->Connecting()) {
