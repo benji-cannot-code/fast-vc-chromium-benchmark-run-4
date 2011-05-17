18ca6fe134ae0f13930a902ea1d88d93cd349101
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-17 19:56:03 +0000

    Adding documentation directory, new Doxyfile, DoxygenLayout.xml. These changes were approved by David Michael a week or two ago, just couldn't check them in. Review URL: http://codereview.chromium.org/7030022
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85662 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ppapi/cpp/documentation/removefilesCPP.sh
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+rm annotated.ezt
+rm bc_s.png
+rm classes.ezt
+rm closed.png
+rm doxygen.png
+rm footer_8dox.ezt
+rm functions*.*
+rm globals*.*
+rm group___c_p_p.ezt
+rm header_8dox.ezt
+rm index_8dox.ezt
+rm modules.ezt
+rm modules_8h.ezt
+rm module_embedder_8h.ezt
+rm module_impl_8h.ezt
+rm namespace*.*
+rm nav_f.png
+rm nav_h.png
+rm open.png
+rm tab_a.png
+rm tab_b.png
+rm tab_h.png
+rm tab_s.png
+# all .map and all .md5 files
+rm *.md5
+rm *.map
+rm *.css
+rm *.py
+rm index.ezt
+
+
