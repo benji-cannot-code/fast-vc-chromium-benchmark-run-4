38f79f79c2652c93c7d797bb6458515e7ee9885b
bweinstein@apple.com <bweinstein@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-17 23:21:39 +0000

    Build fix after r86717.
    * UIProcess/API/C/win/WKView.h: Add a forward declaration of IDropTarget
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/win/WKView.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit2/WKGeometry.h>
 #include <windows.h>
 
+struct IDropTarget;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
