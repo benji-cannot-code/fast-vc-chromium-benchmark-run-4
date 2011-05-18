5bbd510059727191bd4e5514054a9eb147dfbafc
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 22:00:14 +0000

    Use NSAccessibilityURLAttribute instead of @"AXURL"
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/7043010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85822 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/accessibility/browser_accessibility_cocoa.mm
+++ chrome/browser/accessibility/browser_accessibility_cocoa.mm
@@ -293,7 +293,7 @@ bool GetState(BrowserAccessibility* accessibility, int state) {
   // AXWebArea attributes.
   if ([attribute isEqualToString:@"AXLoaded"])
     return [NSNumber numberWithBool:YES];
-  if ([attribute isEqualToString:@"AXURL"]) {
+  if ([attribute isEqualToString:NSAccessibilityURLAttribute]) {
     WebAccessibility::Attribute urlAttribute =
         [[self role] isEqualToString:@"AXWebArea"] ?
             WebAccessibility::ATTR_DOC_URL :
@@ -451,7 +451,7 @@ bool GetState(BrowserAccessibility* accessibility, int state) {
       NSAccessibilityTopLevelUIElementAttribute,
       NSAccessibilityValueAttribute,
       NSAccessibilityWindowAttribute,
-      @"AXURL",
+      NSAccessibilityURLAttribute,
       @"AXVisited",
       nil]];
 
