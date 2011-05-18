0cb75e3f36576d7afe6b45ce42a50d87becfa5ed
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 06:51:15 +0000

    Hide translate infobar on programmatic navigation.
    In Google news, you can change the UI language through a popup menu [which does so by submitting a form programmatically].  The translation infobar wasn't being refreshed in this situation because the page navigation being performed is programmatic.
    
    Infobar hiding on navigation, goes through TabContents::ExpireInfoBars() which has an early return if the navigation just performed wasn't initiated by the user.  After this, the code loops through the list of infobars and calls ShouldExpire() on each one to determine if it can be closed.
    
    This CL removes the early return and moves the code to determine dismissal to the ShouldExpire() method of the infobar delegates, thus allowing the translate infobar to indicate that it can be dismissed on page navigation.
    
    BUG=70261
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/6883299
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85731 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_infobar_delegate.h
+++ chrome/browser/translate/translate_infobar_delegate.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "chrome/browser/tab_contents/confirm_infobar_delegate.h"
+#include "chrome/browser/tab_contents/infobar_delegate.h"
 #include "chrome/browser/translate/translate_prefs.h"
 #include "chrome/common/translate_errors.h"
 
@@ -164,6 +164,8 @@ class TranslateInfoBarDelegate : public InfoBarDelegate {
   virtual void InfoBarDismissed() OVERRIDE;
   virtual gfx::Image* GetIcon() const OVERRIDE;
   virtual InfoBarDelegate::Type GetInfoBarType() const OVERRIDE;
+   virtual bool ShouldExpire(
+       const NavigationController::LoadCommittedDetails& details) const;
   virtual TranslateInfoBarDelegate* AsTranslateInfoBarDelegate() OVERRIDE;
 
   // Gets the host of the page being translated, or an empty string if no URL is
