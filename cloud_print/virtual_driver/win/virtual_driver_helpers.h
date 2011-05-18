ede9e08535a2ee2dd14ca64f450351b893f9c25c
abodenha@google.com <abodenha@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 22:12:41 +0000

    Added resources. Virtual_driver_setup now has localization support. All dlls and exes now have a version resource. Common (untranslated) strings are now pulled from a common resource file. Installer and port monitor now have separate .gyp files. Still to do: Integrate with TC and add all languages.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6930019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85825 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/win/virtual_driver_helpers.h
+++ cloud_print/virtual_driver/win/virtual_driver_helpers.h
@@ -8,19 +8,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <windows.h>
+#include "base/string16.h"
 
 class FilePath;
 
 namespace cloud_print {
 
+// Returns TRUE if the current OS is 64 bit.
+bool IsSystem64Bit();
+
 // Convert an HRESULT to a localized string and display it in a message box.
-void DisplayWindowsMessage(HWND hwnd, HRESULT message_id);
+void DisplayWindowsMessage(HWND hwnd,
+                           HRESULT message_id,
+                           const string16 &caption);
 
 // Similar to the Windows API call GetLastError but returns an HRESULT.
 HRESULT GetLastHResult();
 
+// Returns the correct port monitor DLL file name for the current machine.
+string16 GetPortMonitorDllName();
+
 // Gets the standard install path for "version 3" print drivers.
 HRESULT GetPrinterDriverDir(FilePath* path);
+
+// Retrieves a string from the string table of the module that contains the
+// calling code.
+string16 LoadLocalString(DWORD string_id);
 }
 
 #endif  // CLOUD_PRINT_VIRTUAL_DRIVER_WIN_HELPERS_H_
