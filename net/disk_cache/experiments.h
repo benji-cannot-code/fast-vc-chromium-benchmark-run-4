e7534e6bf2d43b8947f942b330a4a0495449a88a
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 22:14:42 +0000

    Disk cache: Re-open the deleted list length experiment.
    BUG=79186
    TEST=none
    Review URL: http://codereview.chromium.org/7036028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85826 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/experiments.h
+++ net/disk_cache/experiments.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,7 +18,8 @@ enum {
   EXPERIMENT_OLD_FILE2 = 4,
   EXPERIMENT_DELETED_LIST_OUT = 11,
   EXPERIMENT_DELETED_LIST_CONTROL = 12,
-  EXPERIMENT_DELETED_LIST_IN = 13
+  EXPERIMENT_DELETED_LIST_IN = 13,
+  EXPERIMENT_DELETED_LIST_OUT2 = 14
 };
 
 }  // namespace disk_cache
