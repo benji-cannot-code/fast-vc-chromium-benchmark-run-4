47e8869102ba428864107dac606561b8e246bbeb
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 01:02:29 +0000

    Call SwapBuffersCallback multiple times if a frame was dropped.
    BUG=82084
    
    Review URL: http://codereview.chromium.org/7031028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85710 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/accelerated_plugin_view_mac.mm
+++ chrome/browser/renderer_host/accelerated_plugin_view_mac.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/renderer_host/accelerated_plugin_view_mac.h"
 
 #include "base/command_line.h"
+#include "base/debug/trace_event.h"
 #import "base/mac/scoped_nsautorelease_pool.h"
 #include "chrome/browser/renderer_host/render_widget_host_view_mac.h"
 #include "chrome/common/chrome_switches.h"
@@ -106,6 +107,8 @@ static CVReturn DrawOneAcceleratedPluginCallback(
 }
 
 - (void)drawView {
+  TRACE_EVENT1("browser", "AcceleratedPluginViewMac::drawView",
+      "frameNum", swapBuffersCount_);
   // Called on a background thread. Synchronized via the CGL context lock.
   CGLLockContext(cglContext_);
 
@@ -289,6 +292,7 @@ static CVReturn DrawOneAcceleratedPluginCallback(
 }
 
 - (void)setFrame:(NSRect)frameRect {
+  TRACE_EVENT0("browser", "AcceleratedPluginViewMac::setFrame");
   [self setCachedSize:frameRect.size];
   [super setFrame:frameRect];
 }
