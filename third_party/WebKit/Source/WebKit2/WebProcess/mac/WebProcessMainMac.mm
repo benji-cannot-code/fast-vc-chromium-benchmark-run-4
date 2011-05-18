b694a3d3b8488e9ed03cd03bcf7a25837c7c571c
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-18 04:43:44 +0000

    <rdar://problem/9458300> REGRESSION (r86724): Repro crash loading any webpage in WebKit2 on SnowLeopard https://bugs.webkit.org/show_bug.cgi?id=61022
    Reviewed by Alice Liu.
    
    * WebProcess/mac/WebProcessMainMac.mm:
    (WebKit::WebProcessMain): As long as we are not loading the shim on Snow Leopard, we should not
    try to initialize it.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
@@ -91,9 +91,11 @@ int WebProcessMain(const CommandLine& commandLine)
     WTF::initializeMainThread();
     RunLoop::initializeMainRunLoop();
 
+#ifndef BUILDING_ON_SNOW_LEOPARD
     // Initialize the shim.
     WebProcess::shared().initializeShim();
-    
+#endif
+
     // Create the connection.
     WebProcess::shared().initialize(serverPort, RunLoop::main());
 
