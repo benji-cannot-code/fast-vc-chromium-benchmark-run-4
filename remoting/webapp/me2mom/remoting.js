d29ffc2aaacba55e4309f9a5ba5bb6ea19152059
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 17:28:46 +0000

    Added client plug-in connection.
    BUG=None
    TEST=Run "remoting_simple_host --me2mom" and paste the support id into the web app. You should be able to connect.
    
    Review URL: http://codereview.chromium.org/7031037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85777 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var remoting = {};
+var remoting = chrome.extension.getBackgroundPage().remoting;
 XMPP_LOGIN_NAME = 'xmpp_login';
 XMPP_TOKEN_NAME = 'xmpp_token';
 OAUTH2_TOKEN_NAME = 'oauth2_token';
@@ -19,11 +19,12 @@ function updateAuthStatus_() {
     oauth1_status.style.color = 'red';
   }
   var xmpp_status = document.getElementById('xmpp_status');
-  if (remoting.getItem(XMPP_TOKEN_NAME)) {
+  if (remoting.getItem(XMPP_TOKEN_NAME) && remoting.getItem(XMPP_LOGIN_NAME)) {
     document.getElementById('xmpp_clear').style.display = 'inline';
     document.getElementById('xmpp_form').style.display = 'none';
     xmpp_status.innerText = 'OK';
     xmpp_status.style.color = 'green';
+    remoting.xmppAuthToken = remoting.getItem(XMPP_TOKEN_NAME);
   } else {
     document.getElementById('xmpp_clear').style.display = 'none';
     document.getElementById('xmpp_form').style.display = 'inline';
@@ -166,7 +167,7 @@ function init() {
 }
 
 function setGlobalMode(mode) {
-  setMode_(mode, ['host', 'client', 'session']);
+  setMode_(mode, ['host', 'client']);
 }
 
 function setGlobalModePersistent(mode) {
@@ -223,13 +224,8 @@ function cancelShare() {
 }
 
 function startSession_() {
-  var div = document.getElementById('plugin_container');
-  var plugin = document.createElement('embed');
-  plugin.setAttribute('type', 'pepper-application/x-chromoting');
-  plugin.setAttribute('id', 'chromoting_client_plugin');
-  div.appendChild(plugin);
-  remoting.plugin = plugin;
-  setGlobalMode('session');
+  remoting.username = remoting.getItem(XMPP_LOGIN_NAME);
+  document.location = 'remoting_session.html';
 }
 
 function showConnectError_(responseCode, responseString) {
@@ -252,9 +248,7 @@ function parseServerResponse_(reply, xhr) {
   if (xhr.status == 200) {
     var host = JSON.parse(xhr.responseText);
     if (host.data && host.data.jabberId) {
-      remoting.jid = host.data.jabberId;
-      var proof = document.getElementById('host_jid_debug');
-      proof.innerText = remoting.jid;
+      remoting.hostjid = host.data.jabberId;
       startSession_();
       return;
     }
