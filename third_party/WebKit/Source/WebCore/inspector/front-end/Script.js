81ecd34b6c6ef18f211f7fd1f52add0eaba71e8c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-18 11:33:52 +0000

    2011-05-18  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r86747.
            http://trac.webkit.org/changeset/86747
            https://bugs.webkit.org/show_bug.cgi?id=61039
    
            Breaks JSC debugger tests. (Requested by pfeldman on #webkit).
    
            * inspector/InjectedScriptSource.js:
            ():
            * inspector/Inspector.json:
            * inspector/InspectorConsoleAgent.cpp:
            (WebCore::InspectorConsoleAgent::addMessageToConsole):
            * inspector/InspectorConsoleAgent.h:
            * inspector/InspectorConsoleInstrumentation.h:
            (WebCore::InspectorInstrumentation::addMessageToConsole):
            * inspector/InspectorDebuggerAgent.cpp:
            (WebCore::parseLocation):
            (WebCore::InspectorDebuggerAgent::resolveBreakpoint):
            (WebCore::InspectorDebuggerAgent::editScriptSource):
            (WebCore::InspectorDebuggerAgent::getScriptSource):
            (WebCore::InspectorDebuggerAgent::didParseSource):
            * inspector/InspectorDebuggerAgent.h:
            * inspector/InspectorInstrumentation.cpp:
            (WebCore::InspectorInstrumentation::addMessageToConsoleImpl):
            * inspector/InspectorInstrumentation.h:
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::buildObjectForResourceResponse):
            * inspector/ScriptDebugListener.h:
            * inspector/front-end/DebuggerModel.js:
            (WebInspector.DebuggerModel.prototype.scriptForSourceID):
            (WebInspector.DebuggerModel.prototype.queryScripts):
            (WebInspector.DebuggerModel.prototype.editScriptSource):
            (WebInspector.DebuggerModel.prototype._didEditScriptSource):
            (WebInspector.DebuggerModel.prototype._parsedScriptSource):
            (WebInspector.DebuggerDispatcher.prototype.scriptParsed):
            (WebInspector.DebuggerDispatcher.prototype.breakpointResolved):
            * inspector/front-end/DebuggerPresentationModel.js:
            (WebInspector.DebuggerPresentationModel.prototype.editScriptSource.didReceiveSource):
            (WebInspector.DebuggerPresentationModel.prototype.editScriptSource):
            (WebInspector.DebuggerPresentationModel.prototype._setBreakpointInDebugger.didRequestSourceMapping):
            (WebInspector.DebuggerPresentationModel.prototype._setBreakpointInDebugger):
            (WebInspector.DebuggerPresentationModel.prototype._debuggerPaused):
            (WebInspector.DebuggerPresentationModel.prototype._sourceFileForScript):
            (WebInspector.DebuggerPresentationModel.prototype._scriptForSourceFileId):
            (WebInspector.DebuggerPresentationModel.prototype._createSourceFileId):
            (WebInspector.PresenationCallFrame):
            * inspector/front-end/NetworkManager.js:
            (WebInspector.NetworkDispatcher.prototype._updateResourceWithResponse):
            * inspector/front-end/Script.js:
            (WebInspector.Script):
            (WebInspector.Script.prototype.requestSource):
            (WebInspector.Script.prototype.editSource):
            * inspector/front-end/SourceFile.js:
            (WebInspector.SourceFile.prototype.forceLoadContent):
            (WebInspector.SourceFile.prototype._concatenateScriptsContent):
            (WebInspector.SourceMapping.prototype._sourceLocationToScriptLocation):
            (WebInspector):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Script.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Script.js
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-WebInspector.Script = function(sourceId, sourceURL, startLine, startColumn, endLine, endColumn, errorLine, errorMessage, isContentScript)
+WebInspector.Script = function(sourceID, sourceURL, startLine, startColumn, endLine, endColumn, errorLine, errorMessage, isContentScript)
 {
-    this.sourceId = sourceId;
+    this.sourceID = sourceID;
     this.sourceURL = sourceURL;
     this.lineOffset = startLine;
     this.columnOffset = startColumn;
@@ -50,7 +50,7 @@ WebInspector.Script.prototype = {
             this._source = source;
             callback(this._source);
         }
-        DebuggerAgent.getScriptSource(this.sourceId, didGetScriptSource.bind(this));
+        DebuggerAgent.getScriptSource(this.sourceID, didGetScriptSource.bind(this));
     },
 
     editSource: function(newSource, callback)
@@ -61,6 +61,6 @@ WebInspector.Script.prototype = {
                 this._source = newSource;
             callback(error, callFrames);
         }
-        DebuggerAgent.editScriptSource(this.sourceId, newSource, didEditScriptSource.bind(this));
+        DebuggerAgent.editScriptSource(this.sourceID, newSource, didEditScriptSource.bind(this));
     }
 }
