c925ea682e1e9b9a844f7cad386a8de38f52f779
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-18 03:23:10 +0000

    Rolled out attempts to fix EFL build because they're not enough -- the build script needs to be fixed.
    * runtime/BooleanPrototype.cpp:
    * runtime/DateConstructor.cpp:
    * runtime/ErrorPrototype.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/BooleanPrototype.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/BooleanPrototype.cpp
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExceptionHelpers.h"
 #include "JSFunction.h"
 #include "JSString.h"
-#include "Lookup.h"
 #include "ObjectPrototype.h"
 
 namespace JSC {
