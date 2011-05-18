e2f4a0c9c0b31e15375fb65923eb1ad3bad1fc3f
cdn@chromium.org <cdn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-18 00:53:22 +0000

    2011-05-17  Cris Neckar  <cdn@chromium.org>
            Reviewed by Adam Barth.
    
            Clear the image from ImageLoader rather than clearing the ImageLoader in HTMLObjectElement::renderFallbackContent.
            https://bugs.webkit.org/show_bug.cgi?id=61005
    
            Test: http/tests/loading/nested_bad_objects.php
    
            * html/HTMLObjectElement.cpp:
            (WebCore::HTMLObjectElement::renderFallbackContent):
    2011-05-17  Cris Neckar  <cdn@chromium.org>
    
            Reviewed by Adam Barth.
    
            Tests for crash when two nested image objects with invalid data are loaded.
            https://bugs.webkit.org/show_bug.cgi?id=61005
    
            * http/tests/loading/nested_bad_objects-expected.txt: Added.
            * http/tests/loading/nested_bad_objects.php: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/loading/nested_bad_objects.php
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?
+if (isset($_GET['object'])) {
+    header("Content-Type: nothing/bad-type");
+    exit();
+}
+?>
+<html>
+    <script>
+    if (window.layoutTestController)
+        layoutTestController.dumpAsText();
+    </script>
+    <object type="image/jpeg" data="nested_bad_objects.php?object">
+        <object type="image/jpeg" data="nested_bad_objects.php?object" />
+    </object>
+    PASS - nested image objects with bad mimetype do not cause a crash.
+</html>
