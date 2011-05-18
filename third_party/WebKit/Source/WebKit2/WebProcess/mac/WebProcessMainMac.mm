3a66dc18d4bdabb1ebd9c0013b67e477c1d3dccb
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-18 21:46:33 +0000

    <rdar://problem/9457633> and https://bugs.webkit.org/show_bug.cgi?id=61009 Processes spawned by SnowLeopard's WebProcess attempt to install WebKit2 shims.
    Reviewed by Anders Carlsson.
    
    If the WebProcess or PluginProcess forks, it shouldn't pass WebKit2 shims along to the new process
    in the DYLD_INSERT_LIBRARIES environment variable.
    
    Add Environment Utilities helper to strip unwanted values from an environment variable:
    * Platform/unix/EnvironmentUtilities.cpp: Added.
    (WebKit::EnvironmentUtilities::stripValuesEndingWithString):
    * Platform/unix/EnvironmentUtilities.h: Added.
    * WebKit2.xcodeproj/project.pbxproj:
    
    Strip PluginProcessShim.dylib from DYLD_INSERT_LIBRARIES:
    * PluginProcess/mac/PluginProcessMainMac.mm:
    (WebKit::PluginProcessMain):
    
    Strip WebProcessShim.dylib from DYLD_INSERT_LIBRARIES:
    * WebProcess/mac/WebProcessMainMac.mm:
    (WebKit::WebProcessMain):
    
    Unprotect SnowLeopard now that it will behave and not spawn processes trying to use WebKit2 shims:
    * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
    (WebKit::ProcessLauncher::launchProcess):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebProcessMain.h"
 
 #import "CommandLine.h"
+#import "EnvironmentUtilities.h"
 #import "RunLoop.h"
 #import "WebProcess.h"
 #import "WebSystemInterface.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/RetainPtr.h>
 #import <wtf/Threading.h>
 #import <wtf/text/CString.h>
+#import <wtf/text/StringBuilder.h>
 
 // FIXME: We should be doing this another way.
 extern "C" kern_return_t bootstrap_look_up2(mach_port_t, const name_t, mach_port_t*, pid_t, uint64_t);
@@ -59,6 +61,12 @@ namespace WebKit {
 
 int WebProcessMain(const CommandLine& commandLine)
 {
+#ifdef BUILDING_ON_SNOWLEOPARD
+    // Remove the WebProcess shim from the DYLD_INSERT_LIBRARIES environment variable so any processes spawned by
+    // the WebProcess don't try to insert the shim and crash.
+    EnvironmentUtilities::stripValuesEndingWithString("DYLD_INSERT_LIBRARIES", "/WebProcessShim.dylib");
+#endif
+
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
     String serviceName = commandLine["servicename"];
@@ -91,10 +99,8 @@ int WebProcessMain(const CommandLine& commandLine)
     WTF::initializeMainThread();
     RunLoop::initializeMainRunLoop();
 
-#ifndef BUILDING_ON_SNOW_LEOPARD
     // Initialize the shim.
     WebProcess::shared().initializeShim();
-#endif
 
     // Create the connection.
     WebProcess::shared().initialize(serverPort, RunLoop::main());
