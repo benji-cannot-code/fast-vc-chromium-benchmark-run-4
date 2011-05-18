c7ef8b200124b627f4a0510eca5863452b69e3a8
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 12:38:15 +0000

    Fix clang build.
    TBR=dpolukhin@chromium.org
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7029029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_web_socket_proxy_private_api.cc
+++ chrome/browser/extensions/extension_web_socket_proxy_private_api.cc
@@ -16,6 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/web_socket_proxy_controller.h"
 #endif
 
+WebSocketProxyPrivateGetPassportForTCPFunction::
+    WebSocketProxyPrivateGetPassportForTCPFunction() {
+}
+
+WebSocketProxyPrivateGetPassportForTCPFunction::
+    ~WebSocketProxyPrivateGetPassportForTCPFunction() {
+}
+
 bool WebSocketProxyPrivateGetPassportForTCPFunction::RunImpl() {
   bool delay_response = false;
   result_.reset(Value::CreateStringValue(""));
--- chrome/browser/extensions/extension_web_socket_proxy_private_api.h
+++ chrome/browser/extensions/extension_web_socket_proxy_private_api.h
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WebSocketProxyPrivateGetPassportForTCPFunction
     : public AsyncExtensionFunction, public NotificationObserver {
+ public:
+  WebSocketProxyPrivateGetPassportForTCPFunction();
+
+  virtual ~WebSocketProxyPrivateGetPassportForTCPFunction();
+
  private:
   // ExtensionFunction implementation.
   virtual bool RunImpl() OVERRIDE;
