b0363fc758a563feac45ae5771528b3e30c10dab
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 17:01:17 +0000

    autocomplete: Make use of CHROMEGTK_* macro in autocomplete_popup_view_gtk.*
    BUG=None
    TEST=None
    
    R=pkasting@chromium.org,evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7044022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_popup_view_gtk.h
+++ chrome/browser/autocomplete/autocomplete_popup_view_gtk.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <gtk/gtk.h>
+
 #include <map>
 #include <string>
 
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_popup_view.h"
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
+#include "ui/base/gtk/gtk_signal.h"
 #include "ui/gfx/font.h"
 #include "webkit/glue/window_open_disposition.h"
 
@@ -76,35 +78,17 @@ class AutocompletePopupViewGtk : public AutocompletePopupView,
 
   GdkPixbuf* IconForMatch(const AutocompleteMatch& match, bool selected);
 
-  static gboolean HandleExposeThunk(GtkWidget* widget, GdkEventExpose* event,
-                                    gpointer userdata) {
-    return reinterpret_cast<AutocompletePopupViewGtk*>(userdata)->
-        HandleExpose(widget, event);
-  }
-  gboolean HandleExpose(GtkWidget* widget, GdkEventExpose* event);
+  CHROMEGTK_CALLBACK_1(AutocompletePopupViewGtk, gboolean, HandleMotion,
+                       GdkEventMotion*);
 
-  static gboolean HandleMotionThunk(GtkWidget* widget, GdkEventMotion* event,
-                                    gpointer userdata) {
-    return reinterpret_cast<AutocompletePopupViewGtk*>(userdata)->
-        HandleMotion(widget, event);
-  }
-  gboolean HandleMotion(GtkWidget* widget, GdkEventMotion* event);
+  CHROMEGTK_CALLBACK_1(AutocompletePopupViewGtk, gboolean, HandleButtonPress,
+                       GdkEventButton*);
 
-  static gboolean HandleButtonPressThunk(GtkWidget* widget,
-                                         GdkEventButton* event,
-                                         gpointer userdata) {
-    return reinterpret_cast<AutocompletePopupViewGtk*>(userdata)->
-        HandleButtonPress(widget, event);
-  }
-  gboolean HandleButtonPress(GtkWidget* widget, GdkEventButton* event);
+  CHROMEGTK_CALLBACK_1(AutocompletePopupViewGtk, gboolean, HandleButtonRelease,
+                       GdkEventButton*);
 
-  static gboolean HandleButtonReleaseThunk(GtkWidget* widget,
-                                           GdkEventButton* event,
-                                           gpointer userdata) {
-    return reinterpret_cast<AutocompletePopupViewGtk*>(userdata)->
-        HandleButtonRelease(widget, event);
-  }
-  gboolean HandleButtonRelease(GtkWidget* widget, GdkEventButton* event);
+  CHROMEGTK_CALLBACK_1(AutocompletePopupViewGtk, gboolean, HandleExpose,
+                       GdkEventExpose*);
 
   scoped_ptr<AutocompletePopupModel> model_;
   OmniboxView* omnibox_view_;
