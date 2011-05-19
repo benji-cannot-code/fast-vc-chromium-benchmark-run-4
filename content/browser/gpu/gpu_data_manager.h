6d975402cf8a02768203c730faae5bf25837ddf0
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 21:39:52 +0000

    Make about:gpu report driver information again.
    gpu_data_manager was erroneously setting the finalized bit on
    gpu_info, preventing new information about the driver from being
    merged into the active GpuInfo state. This is not needed.
    
    gpu_data_manager was not running the GpuInfoChanged callbacks
    when the gpu info changed. This prevented about:gpu from
    seeing the new gpu strings when they are collected.
    
    BUG=83149
    
    Review URL: http://codereview.chromium.org/7049022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85976 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager.h
+++ content/browser/gpu/gpu_data_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/synchronization/lock.h"
+#include "base/task.h"
 #include "base/values.h"
 #include "content/common/gpu/gpu_feature_flags.h"
 #include "content/common/gpu/gpu_info.h"
@@ -105,5 +106,7 @@ class GpuDataManager {
 
   DISALLOW_COPY_AND_ASSIGN(GpuDataManager);
 };
+
+DISABLE_RUNNABLE_METHOD_REFCOUNT(GpuDataManager);
 
 #endif  // CONTENT_BROWSER_GPU_GPU_DATA_MANAGER_H_
