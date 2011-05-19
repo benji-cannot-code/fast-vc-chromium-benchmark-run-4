fd79cb9ec6be44d0d1a59f80ea52bd05117ec9fe
wyck@chromium.org <wyck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 21:04:30 +0000

    Added logging include.
    Because interactive_ui_tests weren't compiling with touchui.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7050014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85970 0039d316-1c4b-4281-b951-d872f2087c98

--- views/events/event_x.cc
+++ views/events/event_x.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include <X11/Xlib.h>
 
+#include "base/logging.h"
 #include "base/utf_string_conversions.h"
 #include "ui/base/keycodes/keyboard_code_conversion_x.h"
 #include "views/widget/root_view.h"
