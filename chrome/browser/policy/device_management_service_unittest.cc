57a20b6a2de6eeb25474fc82088fe2800a0006aa
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 08:02:49 +0000

    Introduce new error codes for DM server communication but keep the old ones for compatibility.
    BUG=83061
    TEST=unit_test DeviceManagementServiceFailedRequestTest and manually against DM server.
    
    Review URL: http://codereview.chromium.org/7042012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/device_management_service_unittest.cc
+++ chrome/browser/policy/device_management_service_unittest.cc
@@ -177,6 +177,12 @@ INSTANTIATE_TEST_CASE_P(
         FailedRequestParams(
             DeviceManagementBackend::kErrorServiceDeviceNotFound,
             net::URLRequestStatus::SUCCESS,
+            410,
+            PROTO_STRING(kResponseEmpty)),
+        // TODO(pastarmovj): Remove once DM server is deployed.
+        FailedRequestParams(
+            DeviceManagementBackend::kErrorServiceDeviceNotFound,
+            net::URLRequestStatus::SUCCESS,
             901,
             PROTO_STRING(kResponseEmpty)),
         FailedRequestParams(
@@ -194,6 +200,12 @@ INSTANTIATE_TEST_CASE_P(
             net::URLRequestStatus::SUCCESS,
             404,
             PROTO_STRING(kResponseEmpty)),
+        FailedRequestParams(
+            DeviceManagementBackend::kErrorServiceActivationPending,
+            net::URLRequestStatus::SUCCESS,
+            412,
+            PROTO_STRING(kResponseEmpty)),
+        // TODO(pastarmovj): Remove once DM server is deployed.
         FailedRequestParams(
             DeviceManagementBackend::kErrorServiceActivationPending,
             net::URLRequestStatus::SUCCESS,
