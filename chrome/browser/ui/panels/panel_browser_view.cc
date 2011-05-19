46d991a2309f9af0f36e674abc0df98d6f889035
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 17:50:10 +0000

    Support cancelling dragging panel on Windows. Also add more test cases to cover the dragging test.
    BUG=none
    TEST=panel_browser_view_browsertest
    Review URL: http://codereview.chromium.org/7030026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85933 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_view.cc
+++ chrome/browser/ui/panels/panel_browser_view.cc
@@ -15,11 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/window/window.h"
 
 BrowserWindow* Panel::CreateNativePanel(Browser* browser, Panel* panel) {
-  BrowserView* view = new PanelBrowserView(browser, panel);
+  PanelBrowserView* view = new PanelBrowserView(browser, panel);
   (new BrowserFrame(view))->InitBrowserFrame();
-  view->GetWidget()->SetAlwaysOnTop(true);
-  view->GetWindow()->non_client_view()->SetAccessibleName(
-      l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
   return view;
 }
 
@@ -31,6 +28,14 @@ PanelBrowserView::PanelBrowserView(Browser* browser, Panel* panel)
 }
 
 PanelBrowserView::~PanelBrowserView() {
+}
+
+void PanelBrowserView::Init() {
+  BrowserView::Init();
+
+  GetWidget()->SetAlwaysOnTop(true);
+  GetWindow()->non_client_view()->SetAccessibleName(
+      l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
 }
 
 void PanelBrowserView::Close() {
@@ -62,6 +67,15 @@ void PanelBrowserView::OnWindowActivationChanged(bool active) {
   GetFrameView()->OnActivationChanged(active);
 }
 
+bool PanelBrowserView::AcceleratorPressed(
+    const views::Accelerator& accelerator) {
+  if (mouse_pressed_ && accelerator.GetKeyCode() == ui::VKEY_ESCAPE) {
+    OnTitleBarMouseCaptureLost();
+    return true;
+  }
+  return BrowserView::AcceleratorPressed(accelerator);
+}
+
 PanelBrowserFrameView* PanelBrowserView::GetFrameView() const {
   return static_cast<PanelBrowserFrameView*>(frame()->GetFrameView());
 }
@@ -90,16 +104,20 @@ bool PanelBrowserView::OnTitleBarMouseDragged(const views::MouseEvent& event) {
 }
 
 bool PanelBrowserView::OnTitleBarMouseReleased(const views::MouseEvent& event) {
+  return EndDragging(false);
+}
+
+bool PanelBrowserView::OnTitleBarMouseCaptureLost() {
+  return EndDragging(true);
+}
+
+bool PanelBrowserView::EndDragging(bool cancelled) {
   // Only handle clicks that started in our window.
   if (!mouse_pressed_)
     return false;
   mouse_pressed_ = false;
 
-  if (mouse_dragging_) {
-    mouse_dragging_ = false;
-    panel_->manager()->EndDragging(false);
-  } else {
-    panel_->manager()->EndDragging(true);
-  }
+  panel_->manager()->EndDragging(cancelled || !mouse_dragging_);
+  mouse_dragging_ = false;
   return true;
 }
