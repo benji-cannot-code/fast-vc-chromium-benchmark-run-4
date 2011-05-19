5a10cafcc67596a0f749b2045b484ec26ad9164e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-19 12:44:39 +0000

    2011-05-19  Andrey Adaikin  <aandrey@google.com>
            Reviewed by Pavel Feldman.
    
            Web Inspector: switch to Scripts panel is too slow
            https://bugs.webkit.org/show_bug.cgi?id=61030
    
            - It is very expensive to listen to the DOM mutation events, thus we remove the listeners whenever we do any internal
            DOM manipulations (such as expand/collapse line rows) and set the listeners back when we are finished.
            - Also, when we switch to the Scripts panel that have a non-zero scrollTop offset, we would do the rendering work twice.
    
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype.show):
            * inspector/front-end/TextViewer.js:
            (WebInspector.TextEditorMainPanel):
            (WebInspector.TextEditorMainPanel.prototype.beginDomUpdates):
            (WebInspector.TextEditorMainPanel.prototype.endDomUpdates):
            (WebInspector.TextEditorMainPanel.prototype._enableDOMNodeRemovedListener):
            (WebInspector.TextEditorMainChunk):
            (WebInspector.TextEditorMainChunk.prototype.set expanded):
            (WebInspector.TextEditorMainChunk.prototype._createRow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -93,7 +93,6 @@ WebInspector.SourceFrame.prototype = {
         this._ensureContentLoaded();
 
         this._textViewer.show(parentElement);
-        this._textViewer.resize();
 
         if (this.loaded) {
             if (this._scrollTop)
@@ -101,6 +100,8 @@ WebInspector.SourceFrame.prototype = {
             if (this._scrollLeft)
                 this._textViewer.scrollLeft = this._scrollLeft;
         }
+        // Resize after setting the initial scroll positions to avoid unnecessary rendering work.
+        this._textViewer.resize();
     },
 
     hide: function()
