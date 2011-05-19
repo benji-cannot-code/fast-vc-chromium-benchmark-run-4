8ad638f00785fd4da122675f2982100430b118c2
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 04:24:58 +0000

    Revert of 85854 which is a revert of 85852 - Add a command line option ("host-resolver-retry-attempts") to specify the number of retry attempts to resolve host.
    Relanding CL http://codereview.chromium.org/7011044/
    
    BUG=82580
    TEST=host resolver unit tests
    TBR=eroman
    Review URL: http://codereview.chromium.org/7011044
    Review URL: http://codereview.chromium.org/7029050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85871 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/transport_client_socket_unittest.cc
+++ net/socket/transport_client_socket_unittest.cc
@@ -120,6 +120,7 @@ void TransportClientSocketTest::SetUp() {
   AddressList addr;
   scoped_ptr<HostResolver> resolver(
       CreateSystemHostResolver(HostResolver::kDefaultParallelism,
+                               HostResolver::kDefaultRetryAttempts,
                                NULL));
   HostResolver::RequestInfo info(HostPortPair("localhost", listen_port_));
   int rv = resolver->Resolve(info, &addr, NULL, NULL, BoundNetLog());
--- net/url_request/url_request_test_util.cc
+++ net/url_request/url_request_test_util.cc
@@ -17,6 +17,7 @@ TestURLRequestContext::TestURLRequestContext()
     : ALLOW_THIS_IN_INITIALIZER_LIST(context_storage_(this)) {
   context_storage_.set_host_resolver(
       net::CreateSystemHostResolver(net::HostResolver::kDefaultParallelism,
+                                    net::HostResolver::kDefaultRetryAttempts,
                                     NULL));
   context_storage_.set_proxy_service(net::ProxyService::CreateDirect());
   Init();
@@ -26,6 +27,7 @@ TestURLRequestContext::TestURLRequestContext(const std::string& proxy)
     : ALLOW_THIS_IN_INITIALIZER_LIST(context_storage_(this)) {
   context_storage_.set_host_resolver(
       net::CreateSystemHostResolver(net::HostResolver::kDefaultParallelism,
+                                    net::HostResolver::kDefaultRetryAttempts,
                                     NULL));
   net::ProxyConfig proxy_config;
   proxy_config.proxy_rules().ParseFromString(proxy);
