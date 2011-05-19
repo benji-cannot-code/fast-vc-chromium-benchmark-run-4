8fe4c453496b03c8bf997664b384c6549874c226
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 23:17:42 +0000

    Add a missing head to fix touch compile.
    BUG=none
    TEST=compiles with touch
    TBR=jam@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
+++ chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/tab_contents/tab_contents_view.h"
 #include "content/common/notification_service.h"
 #include "content/common/notification_type.h"
+#include "content/common/view_messages.h"
 #include "ui/base/animation/slide_animation.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/transform.h"
