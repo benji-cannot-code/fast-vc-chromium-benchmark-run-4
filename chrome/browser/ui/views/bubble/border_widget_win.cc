2b29cf57ec67448d40813de37a8512919e75b46c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 14:56:26 +0000

    Fix bustage... these files were ommited from the CL.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bubble/border_widget_win.cc
+++ chrome/browser/ui/views/bubble/border_widget_win.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/widget.h"
 
 BorderWidgetWin::BorderWidgetWin()
-    : views::WidgetWin(new views::Widget),
+    : views::NativeWidgetWin(new views::Widget),
       border_contents_(NULL) {
 }
 
