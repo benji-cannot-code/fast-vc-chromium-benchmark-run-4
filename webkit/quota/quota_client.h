aa597282d34a44e14628d352d89a4811e4d8f40b
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 05:26:06 +0000

    Implement GetUsageAndQuotaForEviction in QuotaManager.
    This change includes :
    * Merge EvictOriginData members into EvictionContext.
    * Fix GetAvailableSpaceTest.
    
    BUG=61676
    TEST=QuotaManagerTest.GetUsageANdQuotaForEviction.
    
    Review URL: http://codereview.chromium.org/7039006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85874 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_client.h
+++ webkit/quota/quota_client.h
@@ -67,6 +67,7 @@ class QuotaClient {
                                 DeletionCallback* callback) = 0;
 };
 
+// TODO(dmikurube): Replace it to std::vector for efficiency.
 typedef std::list<QuotaClient*> QuotaClientList;
 
 }  // namespace quota
--- webkit/quota/quota_manager.h
+++ webkit/quota/quota_manager.h
@@ -57,6 +57,9 @@ class QuotaEvictionHandler {
 
   virtual void GetUsageAndQuotaForEviction(
       GetUsageAndQuotaForEvictionCallback* callback) = 0;
+
+ protected:
+  virtual ~QuotaEvictionHandler() {}
 };
 
 // The quota manager class.  This class is instantiated per profile and
@@ -155,6 +158,26 @@ class QuotaManager : public QuotaTaskObserver,
 
   class AvailableSpaceQueryTask;
 
+  struct EvictionContext {
+    EvictionContext()
+        : num_eviction_requested_clients(0),
+          num_evicted_clients(0),
+          num_eviction_error(0),
+          usage(0),
+          quota(0) {}
+    virtual ~EvictionContext() {}
+
+    scoped_ptr<EvictOriginDataCallback> evict_origin_data_callback;
+    int num_eviction_requested_clients;
+    int num_evicted_clients;
+    int num_eviction_error;
+
+    scoped_ptr<GetUsageAndQuotaForEvictionCallback>
+        get_usage_and_quota_callback;
+    int64 usage;
+    int64 quota;
+  };
+
   typedef std::pair<std::string, StorageType> HostAndType;
   typedef std::map<HostAndType, UsageAndQuotaDispatcherTask*>
       UsageAndQuotaDispatcherTaskMap;
@@ -182,6 +205,13 @@ class QuotaManager : public QuotaTaskObserver,
   void DeleteOriginFromDatabase(const GURL& origin, StorageType type);
 
   void DidOriginDataEvicted(QuotaStatusCode status);
+  void DidGetAvailableSpaceForEviction(
+      QuotaStatusCode status,
+      int64 available_space);
+  void DidGetGlobalQuotaForEviction(
+      QuotaStatusCode status,
+      int64 quota);
+  void DidGetGlobalUsageForEviction(int64 usage);
 
   virtual void GetLRUOrigin(
       StorageType type,
@@ -209,11 +239,9 @@ class QuotaManager : public QuotaTaskObserver,
   scoped_ptr<UsageTracker> temporary_usage_tracker_;
   scoped_ptr<UsageTracker> persistent_usage_tracker_;
 
-  UsageAndQuotaDispatcherTaskMap usage_and_quota_dispatchers_;
+  EvictionContext eviction_context_;
 
-  scoped_ptr<EvictOriginDataCallback> evict_origin_data_callback_;
-  int num_eviction_requested_clients_;
-  int num_evicted_clients_;
+  UsageAndQuotaDispatcherTaskMap usage_and_quota_dispatchers_;
 
   int64 temporary_global_quota_;
   QuotaCallbackQueue temporary_global_quota_callbacks_;
