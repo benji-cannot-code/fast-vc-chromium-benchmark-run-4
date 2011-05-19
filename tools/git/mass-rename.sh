ac7b70e48272dce3cfbb335943fcd9d991f4d3d5
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 19:25:17 +0000

    Add a comment to mass-rename.sh that points to sort-headers.py
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7050018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85952 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/git/mass-rename.sh
+++ tools/git/mass-rename.sh
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #   1) git mv foo1 bar1; git mv foo2 bar2; etc.
 #   2) *without committing*, ./tools/git/mass-rename.sh
 #   3) look at git diff (without --cached) to see what the damage is
+#   4) commit, then use tools/sort-headers.py to fix #include ordering:
+#   for f in $(git diff --name-only origin); do ./tools/sort-headers.py $f; done
 
 # rename:
 # update all uses of |from| (first argument) to |to| (second argument).
