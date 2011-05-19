ebdd937d19e14efb71f5fc81c458907c0445195b
bbudge@google.com <bbudge@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 19:24:49 +0000

    Fix a problem where the ExtensionService caused File IO on the wrong thread when updating the PluginList. BUG=none TEST=manual Review URL: http://codereview.chromium.org/7042023
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -575,10 +575,6 @@ class ExtensionService
   void RegisterNaClModule(const GURL& url, const std::string& mime_type);
   void UnregisterNaClModule(const GURL& url);
 
-  // Call UpdatePluginListWithNaClModules() after registering or unregistering
-  // a NaCl module to see those changes reflected in the PluginList.
-  void UpdatePluginListWithNaClModules();
-
   NaClModuleInfoList::iterator FindNaClModule(const GURL& url);
 
   base::WeakPtrFactory<ExtensionService> weak_ptr_factory_;
@@ -692,6 +688,9 @@ class ExtensionService
   bool external_extension_url_added_;
 
   NaClModuleInfoList nacl_module_list_;
+
+  // The back end needs to reference nacl_module_list_.
+  friend class ExtensionServiceBackend;
 
   FRIEND_TEST_ALL_PREFIXES(ExtensionServiceTest,
                            InstallAppsWithUnlimtedStorage);
