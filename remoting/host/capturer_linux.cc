3516c6849710528d22ae694c86b9ce66c8b4bff6
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-19 00:23:34 +0000

    Disable DAMAGE and do full-screen polling in Linux capturer.
    BUG=None
    TEST=Enable Desktop Effects, verify capturer now works.
    
    Review URL: http://codereview.chromium.org/7045011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85851 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/capturer_linux.cc
+++ remoting/host/capturer_linux.cc
@@ -25,6 +25,12 @@ namespace {
 
 static const int kBytesPerPixel = 4;
 
+static bool ShouldUseXDamage() {
+  // For now, always use full-screen polling instead of the DAMAGE extension,
+  // as this extension is broken on many current systems OOTB.
+  return false;
+}
+
 // A class representing a full-frame pixel buffer
 class VideoFrameBuffer {
  public:
@@ -79,6 +85,8 @@ class CapturerLinux : public Capturer {
 
  private:
   bool Init();  // TODO(ajwong): Do we really want this to be synchronous?
+
+  void InitXDamage();
 
   // Read and handle all currently-pending XEvents.
   // In the DAMAGE case, process the XDamage events and store the resulting
@@ -196,6 +204,17 @@ bool CapturerLinux::Init() {
     return false;
   }
 
+  if (ShouldUseXDamage()) {
+    InitXDamage();
+  }
+
+  // Register for changes to the dimensions of the root window.
+  XSelectInput(display_, root_window_, StructureNotifyMask);
+
+  return true;
+}
+
+void CapturerLinux::InitXDamage() {
   // Setup XDamage to report changes in the damage window.  Mark the whole
   // window as invalid.
   if (XDamageQueryExtension(display_, &damage_event_base_,
@@ -215,11 +234,6 @@ bool CapturerLinux::Init() {
   } else {
     LOG(INFO) << "Server does not support XDamage.";
   }
-
-  // Register for changes to the dimensions of the root window.
-  XSelectInput(display_, root_window_, StructureNotifyMask);
-
-  return true;
 }
 
 void CapturerLinux::ScreenConfigurationChanged() {
