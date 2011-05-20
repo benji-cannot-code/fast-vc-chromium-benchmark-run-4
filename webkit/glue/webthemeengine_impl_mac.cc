8cbd4993518eef4327fa839e16e299bda2f2395b
caryclark@chromium.org <caryclark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-20 21:54:07 +0000

    Map webkit glue when Mac build uses Skia.
    In webkit_glue::DecodeImage(), the WebImage
    is of type SkBitmap instead of CGImage, so
    take the general case if USE_SKIA is defined.
    
    The generic version of WebCursor defines
    SetCustomData and ImageFromCustomData only if
    WEBKIT_USING_SKIA is enabled, so disable the
    corresponding routines in the Mac version.
    
    In webkit_glue::WebThemeEngineImpl::paintScrollbarThumb(),
    map the WebCanvas (i.e. SkCanvas) to a CGContext
    to paint the scrollbar thumb, using the SkiaBitLocker
    utility class.
    
    SkiaBitLocker is described in
    http://codereview.chromium.org/7031006/
    
    These changes make no functional change to any
    existing port, so have no new tests.
    
    BUG=79463
    TEST=none
    
    Review URL: http://codereview.chromium.org/6840057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86151 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webthemeengine_impl_mac.cc
+++ webkit/glue/webthemeengine_impl_mac.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCanvas.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebRect.h"
+
+#if WEBKIT_USING_SKIA
+#include "skia/ext/skia_utils_mac.h"
+#endif
 
 using WebKit::WebCanvas;
 using WebKit::WebRect;
@@ -53,7 +57,13 @@ void WebThemeEngineImpl::paintScrollbarThumb(
   trackInfo.trackInfo.scrollbar.pressState =
       state == WebThemeEngine::StatePressed ? kThemeThumbPressed : 0;
   trackInfo.attributes |= (kThemeTrackShowThumb | kThemeTrackHideTrack);
-  HIThemeDrawTrack(&trackInfo, 0, canvas, kHIThemeOrientationNormal);
+#if WEBKIT_USING_SKIA
+  gfx::SkiaBitLocker bitLocker(canvas);
+  CGContextRef cgContext = bitLocker.cgContext();
+#else
+  CGContextRef cgContext = canvas;
+#endif
+  HIThemeDrawTrack(&trackInfo, 0, cgContext, kHIThemeOrientationNormal);
 }
 
 }  // namespace webkit_glue
