ed9584c906cd0dd66453a8f92c301e0b9b2bbd69
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-20 09:32:52 +0000

    2011-05-19  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: ctrl+s should not switch source frame to read only mode.
            https://bugs.webkit.org/show_bug.cgi?id=61125
    
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype.commitEditing.didEditContent):
            (WebInspector.SourceFrame.prototype.commitEditing):
            * inspector/front-end/inspector.js:
            (WebInspector.documentKeyDown):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@86932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
@@ -691,9 +691,6 @@ WebInspector.documentKeyDown = function(event)
         return;
     }
 
-    if (WebInspector.isEditingAnyField())
-        return;
-
     if (this.currentFocusElement && this.currentFocusElement.handleKeyEvent) {
         this.currentFocusElement.handleKeyEvent(event);
         if (event.handled) {
@@ -715,6 +712,9 @@ WebInspector.documentKeyDown = function(event)
         event.preventDefault();
         return;
     }
+
+    if (WebInspector.isEditingAnyField())
+        return;
 
     var isMac = WebInspector.isMac();
     switch (event.keyIdentifier) {
