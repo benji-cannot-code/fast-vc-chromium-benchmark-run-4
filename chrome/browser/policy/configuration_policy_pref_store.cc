04923259528bddb6cba3bc1a44efbc1776d9a232
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-20 11:50:44 +0000

    Make sure policies concerning paths are not enabled for ChromeOS.
    ChromeOS has fixed predefined paths that should not be changeable neither
    by the user nor by the admin.
    
    It turned out only the DownloadDirectory policy needed to be taken care of.
    
    BUG=83205
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7050007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86072 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store.cc
+++ chrome/browser/policy/configuration_policy_pref_store.cc
@@ -432,6 +432,9 @@ bool ConfigurationPolicyPrefKeeper::ApplyDownloadDirPolicy(
   // Replace the policy string which might contain some user variables to an
   // expanded string.
   if (policy == kPolicyDownloadDirectory) {
+    // This policy is ignored on ChromeOS because the download path there is
+    // fixed and can not be configured by the user.
+#if !defined(OS_CHROMEOS)
     FilePath::StringType string_value;
     bool result = value->GetAsString(&string_value);
     DCHECK(result);
@@ -441,6 +444,7 @@ bool ConfigurationPolicyPrefKeeper::ApplyDownloadDirPolicy(
                     Value::CreateStringValue(expanded_value));
     prefs_.SetValue(prefs::kPromptForDownload,
                     Value::CreateBooleanValue(false));
+#endif  // !defined(OS_CHROMEOS)
     delete value;
     return true;
   }
--- chrome/browser/policy/configuration_policy_pref_store_unittest.cc
+++ chrome/browser/policy/configuration_policy_pref_store_unittest.cc
@@ -126,10 +126,16 @@ INSTANTIATE_TEST_CASE_P(
                     prefs::kAuthServerWhitelist),
         TypeAndName(kPolicyAuthNegotiateDelegateWhitelist,
                     prefs::kAuthNegotiateDelegateWhitelist),
-        TypeAndName(kPolicyDownloadDirectory,
-                    prefs::kDownloadDefaultDirectory),
         TypeAndName(kPolicyGSSAPILibraryName,
                     prefs::kGSSAPILibraryName)));
+
+#if !defined(OS_CHROMEOS)
+INSTANTIATE_TEST_CASE_P(
+    ConfigurationPolicyPrefStoreDownloadDirectoryInstance,
+    ConfigurationPolicyPrefStoreStringTest,
+    testing::Values(TypeAndName(kPolicyDownloadDirectory,
+                                prefs::kDownloadDefaultDirectory)));
+#endif  // !defined(OS_CHROMEOS)
 
 // Test cases for boolean-valued policy settings.
 class ConfigurationPolicyPrefStoreBooleanTest
@@ -719,6 +725,7 @@ TEST_F(ConfigurationPolicyPrefStorePromptDownloadTest, Default) {
             store_->GetValue(prefs::kPromptForDownload, NULL));
 }
 
+#if !defined(OS_CHROMEOS)
 TEST_F(ConfigurationPolicyPrefStorePromptDownloadTest, SetDownloadDirectory) {
   EXPECT_EQ(PrefStore::READ_NO_VALUE,
             store_->GetValue(prefs::kPromptForDownload, NULL));
@@ -735,6 +742,7 @@ TEST_F(ConfigurationPolicyPrefStorePromptDownloadTest, SetDownloadDirectory) {
   ASSERT_TRUE(result);
   EXPECT_FALSE(prompt_for_download);
 }
+#endif  // !defined(OS_CHROMEOS)
 
 TEST_F(ConfigurationPolicyPrefStorePromptDownloadTest,
        EnableFileSelectionDialogs) {
