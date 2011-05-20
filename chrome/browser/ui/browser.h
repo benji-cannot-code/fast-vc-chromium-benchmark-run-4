a6dbfbfb8347ce027d0a7b16c010b6199d548bfc
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-20 23:03:45 +0000

    Fix to reload He's Dead Jim page when navigating through windows
    Add Browser::OnWindowActivated
    Add reload in Browser::OnWindowActivated
    
    BUG=chromium-os:12596
    TEST=tested manually
    
    Review URL: http://codereview.chromium.org/6973047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -334,6 +334,11 @@ class Browser : public TabHandlerDelegate,
   // cleanup.
   void OnWindowClosing();
 
+  // OnWindowActivationChanged handling ///////////////////////////////////////
+
+  // Invoked when the window containing us is activated.
+  void OnWindowActivated();
+
   // In-progress download termination handling /////////////////////////////////
 
   // Are normal and/or incognito downloads in progress?
