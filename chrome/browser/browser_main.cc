736b9776a5c063890ff57c90bba8ed6ceb787808
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-20 07:15:56 +0000

    Made network change notifier created after the main thread is initialized to avoid assert on ChromeOS side. Turned off connectivity detection since it is not yet present in M12. I will reactivate this once this lands in flimflam.
    BUG=chromium-os:15327, chromium-os:14589
    TEST=make sure SPDY and Proxy settings are properly invalidated once we resume ChromeOS session
    Review URL: http://codereview.chromium.org/7049024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -531,6 +531,9 @@ void BrowserMainParts::MainMessageLoopStart() {
   // TODO(viettrungluu): should these really go before setting the thread name?
   system_monitor_.reset(new base::SystemMonitor);
   hi_res_timer_manager_.reset(new HighResolutionTimerManager);
+
+  InitializeMainThread();
+
 #if defined(OS_CHROMEOS)
   // TODO(zelidrag): We need to move cros library glue code outside of
   // chrome/browser directory to avoid check_deps issues and then migrate
@@ -540,7 +543,6 @@ void BrowserMainParts::MainMessageLoopStart() {
 #else
   network_change_notifier_.reset(net::NetworkChangeNotifier::Create());
 #endif
-  InitializeMainThread();
 
   PostMainMessageLoopStart();
   Profiling::MainMessageLoopStarted();
