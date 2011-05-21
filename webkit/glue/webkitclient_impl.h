16cf0944f97d6d4777b29bc885d86543a221008d
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-21 04:34:26 +0000

    Revert r75078 now that crbug.com/72007 is fixed.
    R=jamesr
    BUG=71735
    TEST=none
    Review URL: http://codereview.chromium.org/6961039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86207 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitclient_impl.h
+++ webkit/glue/webkitclient_impl.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,15 +79,12 @@ class WebKitClientImpl : public WebKit::WebKitClient {
   void SuspendSharedTimer();
   void ResumeSharedTimer();
 
-  // Hack for http://crbug.com/71735.
-  // TODO(jamesr): move this back to the private section once
-  // http://crbug.com/72007 is fixed.
+ private:
   void DoTimeout() {
     if (shared_timer_func_ && !shared_timer_suspended_)
       shared_timer_func_();
   }
 
- private:
   MessageLoop* main_loop_;
   base::OneShotTimer<WebKitClientImpl> shared_timer_;
   void (*shared_timer_func_)();
