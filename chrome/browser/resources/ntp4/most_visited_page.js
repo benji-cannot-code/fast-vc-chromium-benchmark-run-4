5ce176ec5c06c37c600fdf17b83d3ba0b1c69f1d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-21 00:21:55 +0000

    NTP4: Add ability to drag links to the apps pages.
    again, backend updates are not hooked up yet (because no backend exists)
    
    1. allow tile pages to accept drags of non-tiles (with stub implementation of the tile's appearance)
    2. refactor ugliness of dealing with multiple drag events into event handler thunks. This will also hopefully make it easier to hook up synthesized touch drag events.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7038051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/most_visited_page.js
+++ chrome/browser/resources/ntp4/most_visited_page.js
@@ -281,8 +281,8 @@ cr.define('ntp4', function() {
     },
 
     /** @inheritDoc */
-    acceptOutsideDrags: function() {
-      return false;
+    shouldAcceptDrag: function(tile, dataTransfer) {
+      return this.contains(tile);
     },
 
     /** @inheritDoc */
