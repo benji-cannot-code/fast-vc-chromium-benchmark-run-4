09d465c4081257040bf4b3b00b513b1fad35e826
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-22 00:36:44 +0000

    WebUI: Add password manager search feature. Also add tooltips for the URLs. BUG=9575,79690 Review URL: http://codereview.chromium.org/6990026
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86230 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/cookies_view.js
+++ chrome/browser/resources/options/cookies_view.js
@@ -25,6 +25,20 @@ cr.define('options', function() {
   CookiesView.prototype = {
     __proto__: OptionsPage.prototype,
 
+    /**
+     * The timer id of the timer set on search query change events.
+     * @type {number}
+     * @private
+     */
+    queryDelayTimerId_: 0,
+
+    /**
+     * The most recent search query, or null if the query is empty.
+     * @type {?string}
+     * @private
+     */
+    lastQuery_ : null,
+
     initializePage: function() {
       OptionsPage.prototype.initializePage.call(this);
 
@@ -41,8 +55,6 @@ cr.define('options', function() {
       this.addEventListener('visibleChange', this.handleVisibleChange_);
     },
 
-    lastQuery_ : null,
-
     /**
      * Search cookie using text in |cookies-search-box|.
      */
@@ -55,12 +67,10 @@ cr.define('options', function() {
       }
     },
 
-    queryDelayTimerId_: 0,
-
     /**
      * Handles search query changes.
-     * @private
      * @param {!Event} e The event object.
+     * @private
      */
     handleSearchQueryChange_: function(e) {
       if (this.queryDelayTimerId_)
