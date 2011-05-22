2a6822202e48a3d78f3aed6d603f85a11fc5a8b6
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-22 09:27:39 +0000

    Modify settings page text when syncing.
    - Created new strings for syncing with and without profile.
    - Altered sync service observation to check service's StatusSummary for SYNCING.
    - use innerHTML to allow links in the status string.
    
    BUG=80892
    R=jhawkins@chromium.org
    TEST=set up sync, view chrome://settings/personal, change, add, or delete a bookmark - see syncing briefly, then back to synced.
    
    
    Review URL: http://codereview.chromium.org/6961014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -163,6 +163,7 @@ extern const char kNetworkViewInternalsURL[];
 
 // Sync related URLs.
 extern const char kSyncViewInternalsURL[];
+extern const char kSyncGoogleDashboardURL[];
 
 // GPU related URLs
 extern const char kGpuInternalsURL[];
