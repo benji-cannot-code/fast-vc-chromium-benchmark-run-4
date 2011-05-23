e2c19f9d45601cc1d2a3ea1a9259946fd668e802
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-23 18:05:50 +0000

    Updated video decoder IPC to use PPB_Buffer_Dev's new support for SharedMemory. Plumb through from renderer to GPU process' OMX decoder.
    BUG=none
    TEST=trybots
    
    
    Review URL: http://codereview.chromium.org/7034040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86302 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/omx_video_decode_accelerator.h
+++ content/gpu/omx_video_decode_accelerator.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
+#include "base/shared_memory.h"
 #include "media/video/video_decode_accelerator.h"
 #include "third_party/openmax/il/OMX_Component.h"
 #include "third_party/openmax/il/OMX_Core.h"
@@ -33,7 +34,7 @@ class OmxVideoDecodeAccelerator : public media::VideoDecodeAccelerator {
   const std::vector<uint32>& GetConfig(
       const std::vector<uint32>& prototype_config);
   bool Initialize(const std::vector<uint32>& config);
-  bool Decode(media::BitstreamBuffer* bitstream_buffer,
+  bool Decode(const media::BitstreamBuffer& bitstream_buffer,
               const media::VideoDecodeAcceleratorCallback& callback);
   void AssignPictureBuffer(std::vector<PictureBuffer*> picture_buffers);
   void ReusePictureBuffer(int32 picture_buffer_id);
@@ -130,8 +131,10 @@ class OmxVideoDecodeAccelerator : public media::VideoDecodeAccelerator {
   // void pointer to hold EGLImage handle.
   void* egl_image_;
 
-  typedef std::map<OMX_BUFFERHEADERTYPE*,
-                   media::VideoDecodeAcceleratorCallback> OMXBufferCallbackMap;
+  typedef std::map<
+    OMX_BUFFERHEADERTYPE*,
+    std::pair<base::SharedMemory*,
+              media::VideoDecodeAcceleratorCallback> > OMXBufferCallbackMap;
   OMXBufferCallbackMap omx_buff_cb_;
 
   // Method used the change the state of the port.
@@ -161,4 +164,3 @@ class OmxVideoDecodeAccelerator : public media::VideoDecodeAccelerator {
 };
 
 #endif  // CONTENT_GPU_OMX_VIDEO_DECODE_ACCELERATOR_H_
-
