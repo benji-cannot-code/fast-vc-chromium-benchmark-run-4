4583e736f45bdcdf6bfde376d61a7f7d51460e6e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-23 12:54:54 +0000

    download: Add a GetMenuModel() that grabs the correct model.
    It returns the correct menu model depending on the state of download_item().
    
    BUG=28978
    TEST=None
    
    R=dmazzoni@chromium.org,asanka@chromium.org
    
    Review URL: http://codereview.chromium.org/6974005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_shelf_context_menu.h
+++ chrome/browser/download/download_shelf_context_menu.h
@@ -37,8 +37,9 @@ class DownloadShelfContextMenu : public ui::SimpleMenuModel::Delegate {
  protected:
   explicit DownloadShelfContextMenu(BaseDownloadItemModel* download_model);
 
-  ui::SimpleMenuModel* GetInProgressMenuModel();
-  ui::SimpleMenuModel* GetFinishedMenuModel();
+  // Returns the correct menu model depending whether the download item is
+  // completed or not.
+  ui::SimpleMenuModel* GetMenuModel();
 
   // ui::SimpleMenuModel::Delegate:
   virtual bool IsCommandIdEnabled(int command_id) const OVERRIDE;
@@ -51,6 +52,9 @@ class DownloadShelfContextMenu : public ui::SimpleMenuModel::Delegate {
   virtual string16 GetLabelForCommandId(int command_id) const OVERRIDE;
 
  private:
+  ui::SimpleMenuModel* GetInProgressMenuModel();
+  ui::SimpleMenuModel* GetFinishedMenuModel();
+
   // We show slightly different menus if the download is in progress vs. if the
   // download has finished.
   scoped_ptr<ui::SimpleMenuModel> in_progress_download_menu_model_;
