d388f160686f4e9400dc2123628bccaad5227ebc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-23 16:40:10 +0000

    2011-05-23  Thouraya ANDOLSI  <thouraya.andolsi@st.com>
            Reviewed by Gavin Barraclough.
    
            [SH4] AssemblerLabel does not name a type
            https://bugs.webkit.org/show_bug.cgi?id=59927
    
            SH4Assembler.h file shoold be included before AbstractMacroAssembler.h.
    
            * assembler/MacroAssemblerSH4.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerSH4.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerSH4.h
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(ASSEMBLER) && CPU(SH4)
 
-#include "AbstractMacroAssembler.h"
 #include "SH4Assembler.h"
+#include "AbstractMacroAssembler.h"
 #include <wtf/Assertions.h>
 
 namespace JSC {
