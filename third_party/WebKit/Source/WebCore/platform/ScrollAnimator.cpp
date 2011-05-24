6721c2ee7aaee61f1ac3c2a18edda6e5dca6dc99
ukai@chromium.org <ukai@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 06:39:27 +0000

    2011-05-23  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r87118.
            http://trac.webkit.org/changeset/87118
            https://bugs.webkit.org/show_bug.cgi?id=61347
    
            patch breaks layout tests
            (https://bugs.webkit.org/show_bug.cgi?id=61342), and chromium
            tests (browser_test MacTestCmdUp and ui_test Scrollbar)
            (Requested by ukai_ on #webkit).
    
            * WebCore.gyp/WebCore.gyp:
            * WebCore.gypi:
            * platform/ScrollAnimator.cpp:
            * platform/chromium/ScrollAnimatorChromiumMac.h: Removed.
            * platform/chromium/ScrollAnimatorChromiumMac.mm: Removed.
            * platform/graphics/IntPoint.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollAnimator.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollAnimator.cpp
@@ -42,7 +42,7 @@ using namespace std;
 
 namespace WebCore {
 
-#if !ENABLE(SMOOTH_SCROLLING) && !(PLATFORM(CHROMIUM) && OS(DARWIN))
+#if !ENABLE(SMOOTH_SCROLLING)
 PassOwnPtr<ScrollAnimator> ScrollAnimator::create(ScrollableArea* scrollableArea)
 {
     return adoptPtr(new ScrollAnimator(scrollableArea));
