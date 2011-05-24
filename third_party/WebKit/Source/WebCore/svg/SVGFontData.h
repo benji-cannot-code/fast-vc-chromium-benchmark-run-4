8e77a89cc8528ec8d4d2538b354db47c4842ec7a
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 15:49:28 +0000

    2011-05-24  Nikolas Zimmermann  <nzimmermann@rim.com>
            Not reviewed, build fix.
    
            Remove platform layering violation: TextRun stores RenderObjects for SVG Fonts support
            https://bugs.webkit.org/show_bug.cgi?id=60254
    
            Rename SimpleFontData::FontData subclass to AdditionalFontData, to avoid clashes with the WebCore::FontData class, which breaks the win build.
    
            * platform/graphics/SimpleFontData.cpp:
            (WebCore::SimpleFontData::SimpleFontData):
            * platform/graphics/SimpleFontData.h:
            (WebCore::SimpleFontData::fontData):
            * svg/SVGFontData.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGFontData.h
+++ third_party/WebKit/Source/WebCore/svg/SVGFontData.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class SVGFontData : public SimpleFontData::FontData {
+class SVGFontData : public SimpleFontData::AdditionalFontData {
 public:
     static PassOwnPtr<SVGFontData> create(SVGFontFaceElement* element)
     {
@@ -36,7 +36,6 @@ public:
 
     virtual ~SVGFontData() { }
 
-    virtual bool isSVGFontData() const { return true; }
     virtual void initializeFontData(SimpleFontData*, int size);
 
     SVGFontFaceElement* svgFontFaceElement() const { return m_svgFontFaceElement; }
