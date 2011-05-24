9063aa2ede1a50edcce8933c21fbd8ff6a9bcb67
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 03:26:30 +0000

    Build fix after r87117.
    When moving #defines out in to separate files I find that code is more likely to compile
    when the new header file is actually #included.
    
    * editing/SpellingCorrectionController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.h
+++ third_party/WebKit/Source/WebCore/editing/SpellingCorrectionController.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "DocumentMarker.h"
 #include "Range.h"
+#include "TextChecking.h"
 #include "Timer.h"
 #include "VisibleSelection.h"
 #include <wtf/Noncopyable.h>
