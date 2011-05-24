9ba6062dae4348e044e1cf0f35ad0f42a21eb53b
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 23:34:47 +0000

    Fix up stupid mistake caused by last minute change
    BUG=NONE
    TEST=BUILD
    
    Review URL: http://codereview.chromium.org/7067046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_view_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_view_unittest.mm
@@ -21,7 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @property(readonly, nonatomic) BOOL dropIndicatorShown;
 @property(readonly, nonatomic) CGFloat dropIndicatorPosition;
-@property(assign, nonatomic) id<BookmarkButtonControllerProtocol> controller;
+
+-(void)setController:(id<BookmarkButtonControllerProtocol>)controller;
 
 @end
 
