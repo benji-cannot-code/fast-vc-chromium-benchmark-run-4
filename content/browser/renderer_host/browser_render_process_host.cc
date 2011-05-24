ee8b4c56b715fd8aee7aea330a7f41795d3dd389
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 14:34:24 +0000

    Pull platform_util dependency from TC.
    BUG=71097
    TEST="email link" works
    
    Review URL: http://codereview.chromium.org/6966023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86423 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/browser_render_process_host.cc
+++ content/browser/renderer_host/browser_render_process_host.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history.h"
-#include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_host/web_cache_manager.h"
 #include "chrome/common/chrome_paths.h"
