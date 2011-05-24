60a01ddbd4aa89d9325aca78c6bea1056635d672
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 23:06:30 +0000

    Prepare PepperWidget and RenderWidget for WebKit threaded compositing changes.
    Review URL: http://codereview.chromium.org/6879076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86513 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget_fullscreen_pepper.cc
+++ content/renderer/render_widget_fullscreen_pepper.cc
@@ -60,8 +60,13 @@ class PepperWidget : public WebWidget {
     widget_->Invalidate();
   }
 
+#ifndef WEBWIDGET_HAS_ANIMATE_CHANGES
   virtual void animate() {
   }
+#else
+  virtual void animate(double frameBeginTime) {
+  }
+#endif
 
   virtual void layout() {
   }
