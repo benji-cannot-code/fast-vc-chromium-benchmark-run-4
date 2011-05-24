1bc256f7d54947b553ce3f417a73f3535c405241
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 21:54:23 +0000

    GTK: Buttons on Confirm Infobars should have matching sizes.
    BUG=77576
    TEST=On a confirm infobar with multiple buttons, all buttons are the same width.
    
    Review URL: http://codereview.chromium.org/7066031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86500 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/confirm_infobar_gtk.h
+++ chrome/browser/ui/gtk/infobars/confirm_infobar_gtk.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/infobars/infobar_gtk.h"
 #include "ui/base/gtk/gtk_signal.h"
 
+typedef struct _GtkSizeGroup GtkSizeGroup;
 typedef struct _GtkWidget GtkWidget;
 
 // An infobar that shows a message, up to two optional buttons, and an optional,
@@ -33,6 +34,8 @@ class ConfirmInfoBarGtk : public InfoBar {
   CHROMEGTK_CALLBACK_0(ConfirmInfoBarGtk, void, OnLinkClicked);
 
   GtkWidget* confirm_hbox_;
+
+  GtkSizeGroup* size_group_;
 
   DISALLOW_COPY_AND_ASSIGN(ConfirmInfoBarGtk);
 };
