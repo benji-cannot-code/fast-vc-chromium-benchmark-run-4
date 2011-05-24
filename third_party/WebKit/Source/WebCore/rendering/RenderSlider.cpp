fd9b2064093833036ac6a4cf73b94ca0069c0984
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 17:43:55 +0000

    2011-05-24  Tony Chang  <tony@chromium.org>
            Reviewed by James Robinson.
    
            fix render overflow computation for input type=range
            https://bugs.webkit.org/show_bug.cgi?id=61132
    
            * fast/forms/slider-hit-testing-expected.txt: Added.
            * fast/forms/slider-hit-testing.html: Added.
    2011-05-24  Tony Chang  <tony@chromium.org>
    
            Reviewed by James Robinson.
    
            fix render overflow computation for input type=range
            https://bugs.webkit.org/show_bug.cgi?id=61132
    
            Test: fast/forms/slider-hit-testing.html
    
            We need to clear m_overflow otherwise we use the first
            size of the slider for hit testing (i.e., changes to the size
            cause problems).  This only shows up if the thumb isn't contained
            in the bounds of the slider.  Making the range have a smaller height
            than the thumb shows the bug.
    
            * rendering/RenderSlider.cpp:
            (WebCore::RenderSlider::layout): Clear the overflow during layout
                so we re-compute the overflow based on the current size of the
                slider.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderSlider.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderSlider.cpp
@@ -154,6 +154,8 @@ void RenderSlider::layout()
     computeLogicalHeight();
     updateLayerTransform();
 
+    m_overflow.clear();
+
     if (thumb) {
         if (oldSize != size())
             thumb->setChildNeedsLayout(true, false);
