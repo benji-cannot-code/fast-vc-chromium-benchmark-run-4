225a79b08b1f5565dac6182db9fe909bedc8a7b2
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 20:18:55 +0000

    Clean up after Geoff and fix the WebKit2 build.
    * WebProcess/Plugins/Netscape/NPRuntimeObjectMap.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NPRuntimeObjectMap.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NPRuntimeObjectMap.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NPJSObjectWrapperMap_h
 #define NPJSObjectWrapperMap_h
 
-#include <JavaScriptCore/HandleHeap.h>
+#include <heap/Weak.h>
 #include <wtf/Forward.h>
 #include <wtf/HashMap.h>
 
