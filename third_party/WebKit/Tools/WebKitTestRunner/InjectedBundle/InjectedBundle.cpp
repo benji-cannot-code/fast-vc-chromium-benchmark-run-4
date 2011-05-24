8776ca2db606533a1b42eb4a4ee40792a7b6d11a
qi.2.zhang@nokia.com <qi.2.zhang@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 18:39:04 +0000

    2011-05-24  Qi Zhang  <qi.2.zhang@nokia.com>
            Reviewed by Adam Roben.
    
            WebKitTestRunner needs layoutTestController.setJavaScriptCanAccessClipboard
            https://bugs.webkit.org/show_bug.cgi?id=42669
    
            Fixes a typo issue for patch 94265.
    
            * WebProcess/InjectedBundle/API/c/WKBundle.cpp:
            (WKBundleSetJavaScriptCanAccessClipboard):
    2011-05-24  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Adam Roben.
    
            WebKitTestRunner needs layoutTestController.setJavaScriptCanAccessClipboard
            https://bugs.webkit.org/show_bug.cgi?id=42669
    
            Fixes the regression issue caused by patch 94265. The patch didn't intialize the setting before the
            layouttest starting, it will make the test cases after editing/execCommand/clipboard-access.html failed.
            Because that case changed the setting.
    
            * WebKitTestRunner/InjectedBundle/InjectedBundle.cpp:
            (WTR::InjectedBundle::beginTesting):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundle.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundle.cpp
@@ -179,6 +179,7 @@ void InjectedBundle::beginTesting()
     WKBundleSetShouldTrackVisitedLinks(m_bundle, false);
     WKBundleRemoveAllVisitedLinks(m_bundle);
     WKBundleSetAllowUniversalAccessFromFileURLs(m_bundle, m_pageGroup, true);
+    WKBundleSetJavaScriptCanAccessClipboard(m_bundle, m_pageGroup, true);
 
     WKBundleRemoveAllUserContent(m_bundle, m_pageGroup);
 
