75f50566229ad86e7d3f89894b106c8eaf381286
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-24 01:42:07 +0000

    [Sync] Implement search frontend in chrome://sync-internals.
    Add new 'Search' tab.
    
    Refactor some javascript code slightly.
    
    BUG=76812
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7055023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86384 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_internals/sync_node_browser.js
+++ chrome/browser/resources/sync_internals/sync_node_browser.js
@@ -8,33 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // require: cr/ui/tree.js
 
 cr.define('chrome.sync', function() {
-  // Allow platform specific CSS rules.
-  //
-  // TODO(akalin): BMM and options page does something similar, too.
-  // Move this to util.js.
-  if (cr.isWindows)
-    document.documentElement.setAttribute('os', 'win');
-
-  // TODO(akalin): Create SyncNodeTree/SyncNodeTreeItem classes that
-  // hide all these details.
-
-  /**
-   * Returns an object which measures elapsed time.
-   */
-  var makeTimer = function() {
-    var start = new Date();
-
-    return {
-      /**
-       * @return {number} The number of seconds since the timer was
-       * created.
-       */
-      get elapsedSeconds() {
-        return ((new Date()).getTime() - start.getTime()) / 1000.0;
-      }
-    };
-  };
-
   /**
    * Gets all children of the given node and passes it to the given
    * callback.
@@ -44,12 +17,12 @@ cr.define('chrome.sync', function() {
    *     children.
    */
   function getSyncNodeChildren(nodeInfo, callback) {
-    var timer = makeTimer();
+    var timer = chrome.sync.makeTimer();
     chrome.sync.getChildNodeIds(nodeInfo.id, function(childNodeIds) {
       console.debug('getChildNodeIds took ' +
                     timer.elapsedSeconds + 's to retrieve ' +
                     childNodeIds.length + ' ids');
-      timer = makeTimer();
+      timer = chrome.sync.makeTimer();
       chrome.sync.getNodesById(childNodeIds, function(children) {
         console.debug('getNodesById took ' +
                       timer.elapsedSeconds + 's to retrieve ' +
@@ -80,7 +53,7 @@ cr.define('chrome.sync', function() {
       treeItem.addEventListener('expand', function(event) {
         if (!treeItem.triggeredLoad_) {
           getSyncNodeChildren(nodeInfo, function(children) {
-            var timer = makeTimer();
+            var timer = chrome.sync.makeTimer();
             for (var i = 0; i < children.length; ++i) {
               var childTreeItem = makeNodeTreeItem(children[i]);
               treeItem.add(childTreeItem);
