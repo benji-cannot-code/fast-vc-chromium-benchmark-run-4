35fa40a1e1ffc3ad018aad2e0c4d13b5b56ae8bf
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-24 16:43:05 +0000

    2011-05-24  Rob Buis  <rbuis@rim.com>
            Rubber-stamped by Dirk Schulze.
    
            Remove requiresLayer inlines, already provided by RenderSVGModelObject.
    
            * rendering/svg/RenderSVGHiddenContainer.h:
            (WebCore::RenderSVGHiddenContainer::isSVGHiddenContainer):
            * rendering/svg/RenderSVGImage.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGHiddenContainer.h
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGHiddenContainer.h
@@ -41,7 +41,6 @@ protected:
 
 private:
     virtual bool isSVGHiddenContainer() const { return true; }
-    virtual bool requiresLayer() const { return false; }
 
     virtual void paint(PaintInfo&, int parentX, int parentY);
     
