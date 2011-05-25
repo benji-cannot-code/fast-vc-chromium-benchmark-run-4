411779cc62bc171b4050b62abb5739a985ca187b
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 03:07:19 +0000

    Implement DatabaseQuotaClient's DeteleteOriginData method.
    TEST=database_quota_client_unittest.cc
    BUG=61676
    Review URL: http://codereview.chromium.org/7046019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86561 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/database/database_quota_client.h
+++ webkit/database/database_quota_client.h
@@ -50,6 +50,7 @@ class DatabaseQuotaClient : public quota::QuotaClient,
   class GetOriginsTaskBase;
   class GetAllOriginsTask;
   class GetOriginsForHostTask;
+  class DeleteOriginTask;
 
   typedef quota::CallbackQueueMap1
       <GetUsageCallback*,
