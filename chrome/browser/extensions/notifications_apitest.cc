ad63d40b63d28cfbce134b336325de6f5332957a
bolms@chromium.org <bolms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 20:00:34 +0000

    ExtensionApiTest.Notifications: wait until part 1 finishes cleanup before going to part 2.  Fixes flake.
    Without change: 18 failures out of 250 runs on my machine.  3 out of 250 on Linux try bot.
    
    With change: 0 failures out of 250 runs on my machine.  0 out of 250 on Linux try bot.
    
    TEST=Run ExtensionApiTest.Notifications many times.
    BUG=42314
    
    
    Review URL: http://codereview.chromium.org/7064038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/notifications_apitest.cc
+++ chrome/browser/extensions/notifications_apitest.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 
-// Flaky, http://crbug.com/42314.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_Notifications) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Notifications) {
 #if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
   // Notifications not supported on linux/views yet.
 #else
