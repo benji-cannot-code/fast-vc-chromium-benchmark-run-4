982210a0ed8c88c9e4df2f4139f326d45f84886a
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 03:59:47 +0000

    Add initial framework for performance tests that measure frame rate.
    Add the "blank page" test, which just scrolls a page with a very large body.
    The actual frame rate measurement is based on the webkitRequestAnimationFrame
    callback.  The framework records how frequently that API generates a callback.
    If it generates a callback at 60 FPS, then we know nothing is slowing us down.
    This of course assumes that webkitRequestAnimationFrame is well implemented.
    
    The advantage of this test framework over one that directly reads a framerate
    from Chromium is that this one works outside of the test harness.  You can just
    navigate your browser to the test file and run the test manually.
    
    R=brettw,nduca
    Review URL: http://codereview.chromium.org/7038052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/javascript_test_util.cc
+++ chrome/test/ui/javascript_test_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "content/common/json_value_serializer.h"
@@ -39,17 +40,28 @@ bool JsonDictionaryToMap(const std::string& json,
     if (!succeeded)
       continue;
 
-    EXPECT_TRUE(value->IsType(Value::TYPE_STRING));
-    if (value->IsType(Value::TYPE_STRING)) {
-      const std::string& key(*it);
+    const std::string& key(*it);
+    std::string result;
 
-      std::string result;
-      succeeded = value->GetAsString(&result);
-      EXPECT_TRUE(succeeded);
-
-      if (succeeded)
-        results->insert(std::make_pair(key, result));
+    switch (value->GetType()) {
+      case Value::TYPE_STRING:
+        succeeded = value->GetAsString(&result);
+        break;
+      case Value::TYPE_DOUBLE: {
+        double double_result;
+        succeeded = value->GetAsDouble(&double_result);
+        if (succeeded)
+          result = base::DoubleToString(double_result);
+        break;
+      }
+      default:
+        NOTREACHED() << "Value type not supported!";
+        return false;
     }
+
+    EXPECT_TRUE(succeeded);
+    if (succeeded)
+      results->insert(std::make_pair(key, result));
   }
 
   return true;
