874e85af169b61749fbddcd59d0cfc02eaf10f61
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 16:43:43 +0000

    Proxy PPB_Instance_Private and PPP_Instance_Private.
    BUG=82356
    TEST=Run example plugin out-of-process, click on it.  Before this, it crashes the plugin.  After, behaves as expected.
    
    Review URL: http://codereview.chromium.org/6966043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86640 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/dispatcher.cc
+++ ppapi/proxy/dispatcher.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/ppb_gles_chromium_texture_mapping_proxy.h"
 #include "ppapi/proxy/ppb_graphics_2d_proxy.h"
 #include "ppapi/proxy/ppb_image_data_proxy.h"
+#include "ppapi/proxy/ppb_instance_private_proxy.h"
 #include "ppapi/proxy/ppb_instance_proxy.h"
 #include "ppapi/proxy/ppb_opengles2_proxy.h"
 #include "ppapi/proxy/ppb_pdf_proxy.h"
@@ -79,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/ppb_var_deprecated_proxy.h"
 #include "ppapi/proxy/ppp_class_proxy.h"
 #include "ppapi/proxy/ppp_graphics_3d_proxy.h"
+#include "ppapi/proxy/ppp_instance_private_proxy.h"
 #include "ppapi/proxy/ppp_instance_proxy.h"
 #include "ppapi/proxy/var_serialization_rules.h"
 
@@ -131,6 +133,7 @@ InterfaceList::InterfaceList() {
   AddPPB(PPB_GLESChromiumTextureMapping_Proxy::GetInfo());
   AddPPB(PPB_Graphics2D_Proxy::GetInfo());
   AddPPB(PPB_ImageData_Proxy::GetInfo());
+  AddPPB(PPB_Instance_Private_Proxy::GetInfo());
   AddPPB(PPB_Instance_Proxy::GetInfo());
   AddPPB(PPB_OpenGLES2_Proxy::GetInfo());
   AddPPB(PPB_PDF_Proxy::GetInfo());
@@ -149,6 +152,7 @@ InterfaceList::InterfaceList() {
 
   // PPP (plugin) interfaces.
   AddPPP(PPP_Graphics3D_Proxy::GetInfo());
+  AddPPP(PPP_Instance_Private_Proxy::GetInfo());
   AddPPP(PPP_Instance_Proxy::GetInfo());
 }
 
