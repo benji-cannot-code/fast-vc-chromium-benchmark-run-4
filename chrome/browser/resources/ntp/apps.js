1a10ed88b4b50fba1e115a194d133c40b0077ae3
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 14:47:02 +0000

    Support for component extensions as apps on the new tab page. Added filebrowser.
    BUG=chromium-os:14543
    TEST=File Manager should be on NTP.
    Review URL: http://codereview.chromium.org/6912024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp/apps.js
+++ chrome/browser/resources/ntp/apps.js
@@ -706,11 +706,13 @@ var apps = (function() {
       img.onload = function() { this.loadedImages++; }.bind(this);
       img.src = app['icon_big'];
 
-      var settingsButton = div.appendChild(new cr.ui.ContextMenuButton);
-      settingsButton.className = 'app-settings';
-      settingsButton.title = localStrings.getString('appsettings');
-
-      addContextMenu(div, app);
+      // User cannot change launch options or uninstall component extension.
+      if (!app['is_component']) {
+        var settingsButton = div.appendChild(new cr.ui.ContextMenuButton);
+        settingsButton.className = 'app-settings';
+        settingsButton.title = localStrings.getString('appsettings');
+        addContextMenu(div, app);
+      }
 
       return div;
     },
@@ -729,7 +731,10 @@ var apps = (function() {
       a.className = 'item';
       span.appendChild(a);
 
-      addContextMenu(span, app);
+      // User cannot change launch options or uninstall component extension.
+      if (!app['is_component']) {
+        addContextMenu(span, app);
+      }
 
       return span;
     },
@@ -745,7 +750,10 @@ var apps = (function() {
       a.style.backgroundImage = url(app['icon_small']);
       a.className = 'item';
 
-      addContextMenu(a, app);
+      // User cannot change launch options or uninstall component extension.
+      if (!app['is_component']) {
+        addContextMenu(a, app);
+      }
 
       return a;
     },
