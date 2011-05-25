e8ba01e88fa5cc46b563ce1cddab2ba50ca932af
imasaki@chromium.org <imasaki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 15:58:24 +0000

    Bug fixes in matrix driven test and some minor changes. Also, adding media_matrix_data_public.csv for videos that are in trunk/deps/avperf/{bear,tulip}.
    Bug fixes includes:
    * CSV file abnormality
    * the order of returned tuples in _GenerateMediaInfo and LookForMediaInfoInCompactFormByNickName was incorrect
    * not recognize the second column of CSV as tag
    * par_filename was not set in the case of matrix driven test. 
    
    Added features:
    * -c and -z option to control the number of tests
    
    
    Review URL: http://codereview.chromium.org/6992026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/media/media_test_runner.py
+++ chrome/test/functional/media/media_test_runner.py
@@ -94,16 +94,18 @@ def main():
                     default=DEFAULT_MEASURE_INTERVALS,
                     help='Interval for measurement data [defaults to "%d"]' %
                          DEFAULT_MEASURE_INTERVALS)
-  parser.add_option('-o', '--test-one-combination', dest='one_combination',
-                    default=True, # Currently default is True
-                                  # since we want to test only 1 combination.
-                    help='Run only one parameter combination')
+  parser.add_option('-c', '--cache_test', dest='cache_test',
+                    default=False, help='Include cache test',
+                    action='store_true')
+  parser.add_option('-z', '--test-one-video', dest='one_video',
+                    default=False, help='Run only one video',
+                    action='store_true')
   parser.add_option(
       '-w', '--test_scenario_input_filename',
       dest='test_scenario_input_filename',
       default='', help='Test scenario file (CSV form)', metavar='FILE')
   parser.add_option(
-      '-c', '--test_scenario', dest='test_scenario',
+      '-q', '--test_scenario', dest='test_scenario',
       default='', help='Test scenario (action triples delimited by \'|\')')
   parser.add_option('-s', '--suite', dest='suite',
                     help='Suite file')
@@ -175,6 +177,8 @@ def main():
           parent_envs = copy.deepcopy(os.environ)
           if options.input_matrix_filename is None:
             par_filename = os.path.join(os.pardir, filename)
+          else:
+            par_filename = filename
           envs = {
             MediaTestEnvNames.MEDIA_TAG_ENV_NAME: tag,
             MediaTestEnvNames.MEDIA_FILENAME_ENV_NAME: par_filename,
@@ -228,8 +232,10 @@ def main():
             cmd += ' -v'
           proc = Popen(cmd, env=envs, shell=True)
           proc.communicate()
-        if options.one_combination:
-          sys.exit(0)
+        if not options.cache_test:
+          break
+      if options.one_video:
+        break
 
 
 if __name__ == '__main__':
