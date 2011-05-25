975e4b5b5515b8c63844986ccd0ca6ae03d8f9e7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 20:44:01 +0000

    Commit inline editable list changes when window loses focus.
    BUG=79251
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7004058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86694 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/inline_editable_list.js
+++ chrome/browser/resources/options/inline_editable_list.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -73,8 +73,8 @@ cr.define('options', function() {
       DeletableItem.prototype.decorate.call(this);
 
       this.editFields_ = [];
-      this.addEventListener('mousedown', this.handleMouseDown_.bind(this));
-      this.addEventListener('keydown', this.handleKeyDown_.bind(this));
+      this.addEventListener('mousedown', this.handleMouseDown_);
+      this.addEventListener('keydown', this.handleKeyDown_);
       this.addEventListener('leadChange', this.handleLeadChange_);
     },
 
@@ -328,6 +328,21 @@ cr.define('options', function() {
       }
     },
   };
+
+  /**
+   * Takes care of committing changes to inline editable list items when the
+   * window loses focus.
+   */
+  function handleWindowBlurs() {
+    window.addEventListener('blur', function(e) {
+      var itemAncestor = findAncestor(document.activeElement, function(node) {
+        return node instanceof InlineEditableItem;
+      });
+      if (itemAncestor);
+        document.activeElement.blur();
+    });
+  }
+  handleWindowBlurs();
 
   var InlineEditableItemList = cr.ui.define('list');
 
