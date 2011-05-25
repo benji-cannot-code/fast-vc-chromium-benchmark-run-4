e3c2100c6829a061ec4c75a150af98b0489ce3e2
bolms@chromium.org <bolms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 15:59:44 +0000

    For extension context menus, only apply targetUrlPatterns to appropriate contexts.
    BUG=63545
    TEST=Use contextMenus.create() with "page" context and targetUrlPattern; verify that menu does show.
    
    
    Review URL: http://codereview.chromium.org/7057029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1612,6 +1612,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/sync/test_profile_sync_service.h',
         'browser/sync/util/cryptographer_unittest.cc',
         'browser/sync/util/nigori_unittest.cc',
+        'browser/tab_contents/render_view_context_menu_unittest.cc',
         'browser/tab_contents/thumbnail_generator_unittest.cc',
         'browser/tab_contents/web_contents_unittest.cc',
         'browser/tabs/pinned_tab_codec_unittest.cc',
--- chrome/test/data/extensions/context_menus/target_urls/test.js
+++ chrome/test/data/extensions/context_menus/target_urls/test.js
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 window.onload = function() {
   var patterns = [ "http://*.google.com/*" ];
-  chrome.contextMenus.create({"title":"item1", "contexts": ["all"],
+  chrome.contextMenus.create({"title":"item1", "contexts": ["link"],
                               "targetUrlPatterns": patterns}, function() {
     if (!chrome.extension.lastError) {
       chrome.test.sendMessage("created items");
