6d86c9f051a5d788a330162958caeee171897a33
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 09:15:43 +0000

    2nd try: Notify read access to the QuotaManager in FileSystemOperation
    original review page (reverted due to chromeos breakage):
    http://codereview.chromium.org/7054012/
    
    BUG=none
    TEST=FileSystemOperationTest.*
    TBR=ericu
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86588 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_operation.cc
+++ webkit/fileapi/file_system_operation.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/file_system_file_util_proxy.h"
 #include "webkit/fileapi/file_system_operation_context.h"
 #include "webkit/fileapi/file_system_path_manager.h"
+#include "webkit/fileapi/file_system_quota_util.h"
 #include "webkit/fileapi/file_system_types.h"
 #include "webkit/fileapi/file_system_util.h"
 #include "webkit/fileapi/file_writer_delegate.h"
@@ -37,9 +38,6 @@ FileSystemOperation::FileSystemOperation(
 #ifndef NDEBUG
   pending_operation_ = kOperationNone;
 #endif
-  // TODO(dmikurube): Read and set available bytes from the Quota Manager.
-  file_system_operation_context_.set_allowed_bytes_growth(
-      QuotaFileUtil::kNoLimit);
 }
 
 FileSystemOperation::~FileSystemOperation() {
@@ -657,8 +655,9 @@ void FileSystemOperation::DidEnsureFileExistsExclusive(
   if (rv == base::PLATFORM_FILE_OK && !created) {
     dispatcher_->DidFail(base::PLATFORM_FILE_ERROR_EXISTS);
     delete this;
-  } else
+  } else {
     DidFinishFileOperation(rv);
+  }
 }
 
 void FileSystemOperation::DidEnsureFileExistsNonExclusive(
@@ -818,6 +817,18 @@ bool FileSystemOperation::VerifyFileSystemPathForRead(
   *file_system_file_util =
       file_system_context()->path_manager()->GetFileSystemFileUtil(*type);
   DCHECK(*file_system_file_util);
+
+  // We notify this read access whether the read access succeeds or not.
+  // This must be ok since this is used to let the QM's eviction logic know
+  // someone is interested in reading the origin data and therefore to indicate
+  // that evicting this origin may not be a good idea.
+  FileSystemQuotaUtil* quota_util = file_system_context()->GetQuotaUtil(*type);
+  if (quota_util) {
+    quota_util->NotifyOriginWasAccessedOnIOThread(
+        file_system_context()->quota_manager_proxy(),
+        *origin_url,
+        *type);
+  }
 
   return true;
 }
