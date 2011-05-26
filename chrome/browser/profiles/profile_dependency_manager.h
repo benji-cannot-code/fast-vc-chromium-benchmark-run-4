442c5c09476f2f33b1a0f675802734397b77a00a
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 23:38:18 +0000

    Profiles: When built in debug mode, NOTREACHED() after a Profile is shutdown if it is accessed.
    BUG=77155
    TEST=These extra NOTREACHED() don't trigger.
    
    Review URL: http://codereview.chromium.org/7074019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_dependency_manager.h
+++ chrome/browser/profiles/profile_dependency_manager.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 
+#ifndef NDEBUG
+#include <set>
+#endif
+
 class Profile;
 class ProfileKeyedServiceFactory;
 
@@ -40,6 +44,18 @@ class ProfileDependencyManager {
   //   replaced in many tests.
   void DestroyProfileServices(Profile* profile);
 
+#ifndef NDEBUG
+  // Unmark |profile| as dead. This exists because of unit tests, which will
+  // often have similar stack structures. 0xWhatever might be created, go out
+  // of scope, and then a new Profile object might be created at 0xWhatever.
+  void ProfileNowExists(Profile* profile);
+
+  // Debugging assertion called as part of GetServiceForProfile in debug
+  // mode. This will NOTREACHED() whenever the user is trying to access a stale
+  // Profile*.
+  void AssertProfileWasntDestroyed(Profile* profile);
+#endif
+
   static ProfileDependencyManager* GetInstance();
 
  private:
@@ -61,6 +77,14 @@ class ProfileDependencyManager {
   EdgeMap edges_;
 
   std::vector<ProfileKeyedServiceFactory*> destruction_order_;
+
+#ifndef NDEBUG
+  // A list of profile objects that have gone through the Shutdown()
+  // phase. These pointers are most likely invalid, but we keep track of their
+  // locations in memory so we can nicely assert if we're asked to do anything
+  // with them.
+  std::set<Profile*> dead_profile_pointers_;
+#endif
 };
 
 #endif  // CHROME_BROWSER_PROFILES_PROFILE_DEPENDENCY_MANAGER_H_
