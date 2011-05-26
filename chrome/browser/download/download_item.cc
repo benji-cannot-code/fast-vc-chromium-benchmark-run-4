e7db443f301f43a34ce81598249c671e7444b87d
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 15:45:27 +0000

    Fix auto-open behavior for Downloads. (Broken by r84459; state entering DownloadItem::Completed() not COMPLETE.)
    BUG=82728
    TEST=All known tests pass, new auto-open test passes.
    
    
    Review URL: http://codereview.chromium.org/7029033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86829 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_item.cc
+++ chrome/browser/download/download_item.cc
@@ -136,7 +136,8 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_otr_(false),
       is_temporary_(false),
       all_data_saved_(false),
-      opened_(false) {
+      opened_(false),
+      open_enabled_(true) {
   if (IsInProgress())
     state_ = CANCELLED;
   if (IsComplete())
@@ -176,7 +177,8 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_otr_(is_otr),
       is_temporary_(!info.save_info.file_path.empty()),
       all_data_saved_(false),
-      opened_(false) {
+      opened_(false),
+      open_enabled_(true) {
   Init(true /* start progress timer */);
 }
 
@@ -204,7 +206,8 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_otr_(is_otr),
       is_temporary_(false),
       all_data_saved_(false),
-      opened_(false) {
+      opened_(false),
+      open_enabled_(true) {
   Init(true /* start progress timer */);
 }
 
@@ -248,6 +251,12 @@ void DownloadItem::OpenDownload() {
   } else if (IsComplete()) {
     opened_ = true;
     FOR_EACH_OBSERVER(Observer, observers_, OnDownloadOpened(this));
+
+    // For testing: If download opening is disabled on this item,
+    // make the rest of the routine a no-op.
+    if (!open_enabled_)
+      return;
+
     if (is_extension_install()) {
       download_util::OpenChromeExtension(download_manager_->profile(),
                                          download_manager_,
@@ -348,6 +357,10 @@ void DownloadItem::OnAllDataSaved(int64 size) {
 void DownloadItem::Completed() {
   VLOG(20) << __FUNCTION__ << "() " << DebugString(false);
 
+  DCHECK(all_data_saved_);
+  state_ = COMPLETE;
+  UpdateObservers();
+  download_manager_->DownloadCompleted(id());
   download_util::RecordDownloadCount(download_util::COMPLETED_COUNT);
 
   // Handle chrome extensions explicitly and skip the shell execute.
@@ -365,13 +378,10 @@ void DownloadItem::Completed() {
     // download shelf.
     if (!is_temporary())
       OpenDownload();
-    auto_opened_ = true;
-  }
 
-  DCHECK(all_data_saved_);
-  state_ = COMPLETE;
-  UpdateObservers();
-  download_manager_->DownloadCompleted(id());
+    auto_opened_ = true;
+    UpdateObservers();
+  }
 }
 
 void DownloadItem::Interrupted(int64 size, int os_error) {
