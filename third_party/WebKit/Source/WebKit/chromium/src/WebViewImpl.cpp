31aad8c066ffc72b0007668187cfd1ea0b844ef8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-26 19:36:13 +0000

    2011-05-26  Sailesh Agrawal  <sail@chromium.org>
            Reviewed by Tony Chang.
    
            Expose willStartLiveResize and willEndLiveResize in WebWidget
            https://bugs.webkit.org/show_bug.cgi?id=60518
    
            * public/WebWidget.h:
            * src/WebPopupMenuImpl.cpp:
            (WebKit::WebPopupMenuImpl::willStartLiveResize):
            (WebKit::WebPopupMenuImpl::willEndLiveResize):
            * src/WebPopupMenuImpl.h:
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::willStartLiveResize):
            (WebKit::WebViewImpl::willEndLiveResize):
            * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87417 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -974,6 +974,12 @@ void WebViewImpl::close()
     deref();  // Balances ref() acquired in WebView::create
 }
 
+void WebViewImpl::willStartLiveResize()
+{
+    if (mainFrameImpl() && mainFrameImpl()->frameView())
+        mainFrameImpl()->frameView()->willStartLiveResize();
+}
+
 void WebViewImpl::resize(const WebSize& newSize)
 {
     if (m_size == newSize)
@@ -1002,6 +1008,12 @@ void WebViewImpl::resize(const WebSize& newSize)
                                                        std::max(1, m_size.height)));
     }
 #endif
+}
+
+void WebViewImpl::willEndLiveResize()
+{
+    if (mainFrameImpl() && mainFrameImpl()->frameView())
+        mainFrameImpl()->frameView()->willEndLiveResize();
 }
 
 void WebViewImpl::animate()
