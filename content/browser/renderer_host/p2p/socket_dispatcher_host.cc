74380d84f5bfd826e3038fef8a27b97c00966217
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 20:19:16 +0000

    Fix memory leaks in P2PSocketDispatcherHost
    BUG=83949
    TEST=No memleaks detected by valgrind
    
    Review URL: http://codereview.chromium.org/7072001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86885 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_dispatcher_host.cc
+++ content/browser/renderer_host/p2p/socket_dispatcher_host.cc
@@ -13,6 +13,7 @@ P2PSocketDispatcherHost::P2PSocketDispatcherHost() {
 }
 
 P2PSocketDispatcherHost::~P2PSocketDispatcherHost() {
+  DCHECK(sockets_.empty());
 }
 
 void P2PSocketDispatcherHost::OnChannelClosing() {
@@ -126,5 +127,12 @@ void P2PSocketDispatcherHost::OnSend(const IPC::Message& msg, int socket_id,
 
 void P2PSocketDispatcherHost::OnDestroySocket(const IPC::Message& msg,
                                               int socket_id) {
-  sockets_.erase(ExtendedSocketId(msg.routing_id(), socket_id));
+  SocketsMap::iterator it = sockets_.find(
+      ExtendedSocketId(msg.routing_id(), socket_id));
+  if (it != sockets_.end()) {
+    delete it->second;
+    sockets_.erase(it);
+  } else {
+    LOG(ERROR) << "Received P2PHostMsg_DestroySocket for invalid socket_id.";
+  }
 }
