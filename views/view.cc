926cb685d61fda52eb1941c912d7d43a6dc6c9c2
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 17:44:03 +0000

    views: Cleanup parent_owned getter/setter accessors.
    BUG=72040
    TEST=None
    
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/6962004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86848 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+
 // Whether to use accelerated compositing when necessary (e.g. when a view has a
 // transformation).
 bool use_acceleration_when_possible = true;
@@ -64,7 +65,7 @@ class ScopedCanvas {
   DISALLOW_COPY_AND_ASSIGN(ScopedCanvas);
 };
 
-}
+}  // namespace
 
 namespace views {
 
@@ -100,7 +101,7 @@ View::View()
       group_(-1),
       focusable_(false),
       accessibility_focusable_(false),
-      is_parent_owned_(true),
+      parent_owned_(true),
       parent_(NULL),
       is_visible_(true),
       registered_for_visible_bounds_notification_(false),
@@ -128,7 +129,7 @@ View::~View() {
   int c = static_cast<int>(children_.size());
   while (--c >= 0) {
     children_[c]->parent_ = NULL;
-    if (children_[c]->IsParentOwned())
+    if (children_[c]->parent_owned())
       delete children_[c];
   }
 
@@ -1346,7 +1347,7 @@ void View::DoRemoveChildView(View* view,
     view->PropagateRemoveNotifications(this);
     view->parent_ = NULL;
 
-    if (delete_removed_view && view->IsParentOwned())
+    if (delete_removed_view && view->parent_owned())
       view_to_be_deleted.reset(view);
 
     children_.erase(i);
