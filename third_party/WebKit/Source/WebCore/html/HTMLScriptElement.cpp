2c8c35ff213af3ec88045c2ba92d1e6b6b8b8c87
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-26 06:20:35 +0000

    2011-05-25  James Simonsen  <simonjam@chromium.org>
            Reviewed by Adam Barth.
    
            Add site-specific hack for zipcar.com with old versions of requirejs.
            https://bugs.webkit.org/show_bug.cgi?id=61321
    
            Old versions of requirejs (< 0.15.0) try to load scripts in parallel but execute them in
            order. This used to work in webkit by setting a bogus script type (script/cache), then
            changing the type to a valid one when they wanted to execute it. This hack translates the
            behavior into the new API (by disabling forceAsync).
    
            * html/HTMLScriptElement.cpp:
            (WebCore::needsOldRequirejsQuirk): Added.
            (WebCore::HTMLScriptElement::insertedIntoDocument):
            If hack is needed, set a proper script type so script loads.
            If script isn't async, disable forceAsync so script executes in order.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLScriptElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLScriptElement.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventNames.h"
 #include "HTMLNames.h"
 #include "ScriptEventListener.h"
+#include "Settings.h"
 #include "Text.h"
 
 namespace WebCore {
@@ -82,8 +83,33 @@ void HTMLScriptElement::parseMappedAttribute(Attribute* attr)
         HTMLElement::parseMappedAttribute(attr);
 }
 
+static bool needsOldRequirejsQuirk(HTMLScriptElement* element)
+{
+    if (element->fastGetAttribute(typeAttr) != "script/cache")
+        return false;
+
+    Document* document = element->document();
+
+    const KURL& url = document->url();
+    if (!equalIgnoringCase(url.host(), "www.zipcar.com"))
+        return false;
+
+    Settings* settings = document->settings();
+    if (!settings)
+        return false;
+    if (!settings->needsSiteSpecificQuirks())
+        return false;
+
+    return true;
+}
+
 void HTMLScriptElement::insertedIntoDocument()
 {
+    if (needsOldRequirejsQuirk(this)) {
+        if (!asyncAttributeValue())
+            handleAsyncAttribute(); // Clear forceAsync, so this script loads in parallel, but executes in order.
+        setAttribute(typeAttr, "text/javascript");
+    }
     HTMLElement::insertedIntoDocument();
     ScriptElement::insertedIntoDocument();
 }
