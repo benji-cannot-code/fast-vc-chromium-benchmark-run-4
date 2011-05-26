a300f11b1dfa4505e07442482a93fcd5e9771b55
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 16:48:12 +0000

    Cancel prerenders if safebrowsing triggers. BUG=71847 Review URL: http://codereview.chromium.org/7068024
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/safe_browsing_resource_handler.cc
+++ chrome/browser/renderer_host/safe_browsing_resource_handler.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/safe_browsing_resource_handler.h"
 
 #include "base/logging.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/prerender/prerender_final_status.h"
+#include "chrome/browser/prerender/prerender_tracker.h"
 #include "content/browser/renderer_host/global_request_id.h"
 #include "content/browser/renderer_host/resource_dispatcher_host.h"
 #include "content/browser/renderer_host/resource_message_filter.h"
@@ -176,6 +179,16 @@ void SafeBrowsingResourceHandler::OnBrowseUrlCheckResult(
   } else {
     const net::URLRequest* request = rdh_->GetURLRequest(
         GlobalRequestID(render_process_host_id_, deferred_request_id_));
+    if (request->load_flags() & net::LOAD_PRERENDERING) {
+      prerender::PrerenderTracker* prerender_tracker = g_browser_process->
+          prerender_tracker();
+      if (prerender_tracker->TryCancelOnIOThread(render_process_host_id_,
+                    render_view_id_,
+                    prerender::FINAL_STATUS_SAFE_BROWSING)) {
+        rdh_->CancelRequest(render_process_host_id_, deferred_request_id_,
+                            false);
+      }
+    }
     if (request->load_flags() & net::LOAD_PREFETCH) {
       // Don't prefetch resources that fail safe browsing, disallow
       // them.
