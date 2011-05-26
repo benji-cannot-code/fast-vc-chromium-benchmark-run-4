9a694d80ecafd6468ee7f1f9d787943818e51d12
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 01:46:32 +0000

    Fixes bug in NativeWidgetWin::GetAcceleratedWidget. I had the ifdef wrong.
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/6992086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86767 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_win.cc
+++ views/widget/native_widget_win.cc
@@ -1148,9 +1148,9 @@ void NativeWidgetWin::ClientAreaSizeChanged() {
 
 gfx::AcceleratedWidget NativeWidgetWin::GetAcceleratedWidget() {
 #if defined(VIEWS_COMPOSITOR)
-  return gfx::kNullAcceleratedWidget;
-#else
   return hwnd();
+#else
+  return gfx::kNullAcceleratedWidget;
 #endif
 }
 
