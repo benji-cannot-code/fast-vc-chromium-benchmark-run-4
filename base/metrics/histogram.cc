11e9f002e873b0d0fb1957fc1cbb1fb18d8af8bc
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 08:36:42 +0000

    Land http://codereview.chromium.org/6869009 for the third time.
    Introduce the ANNOTATE_LEAKING_OBJECT_PTR annotation that can be used to mark
    heap allocated objects as intentionally leaked ones.
    Annotate the histograms produced by {Histogram,BooleanHistogram,LinearHistogram,CustomHistogram}::FactoryGet(), as leaked.
    
    Rename StatsHistogram::StatsHistogramFactoryGet to StatsHistogram::FactoryGet, annotate the result as leaky, update the suppressions.
    
    BUG=79322
    TBR=jar,willchan
    Review URL: http://codereview.chromium.org/7062016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86794 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/histogram.cc
+++ base/metrics/histogram.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string>
 
+#include "base/debug/leak_annotations.h"
 #include "base/logging.h"
 #include "base/pickle.h"
 #include "base/stringprintf.h"
@@ -102,6 +103,7 @@ Histogram* Histogram::FactoryGet(const std::string& name,
 
   DCHECK_EQ(HISTOGRAM, histogram->histogram_type());
   DCHECK(histogram->HasConstructorArguments(minimum, maximum, bucket_count));
+  ANNOTATE_LEAKING_OBJECT_PTR(histogram);  // see crbug.com/79322
   return histogram;
 }
 
@@ -795,6 +797,7 @@ Histogram* LinearHistogram::FactoryGet(const std::string& name,
 
   DCHECK_EQ(LINEAR_HISTOGRAM, histogram->histogram_type());
   DCHECK(histogram->HasConstructorArguments(minimum, maximum, bucket_count));
+  ANNOTATE_LEAKING_OBJECT_PTR(histogram);  // see crbug.com/79322
   return histogram;
 }
 
@@ -885,6 +888,7 @@ Histogram* BooleanHistogram::FactoryGet(const std::string& name, Flags flags) {
   }
 
   DCHECK_EQ(BOOLEAN_HISTOGRAM, histogram->histogram_type());
+  ANNOTATE_LEAKING_OBJECT_PTR(histogram);  // see crbug.com/79322
   return histogram;
 }
 
@@ -934,6 +938,7 @@ Histogram* CustomHistogram::FactoryGet(const std::string& name,
   DCHECK_EQ(histogram->histogram_type(), CUSTOM_HISTOGRAM);
   DCHECK(histogram->HasConstructorArguments(ranges[1], ranges.back(),
                                             ranges.size()));
+  ANNOTATE_LEAKING_OBJECT_PTR(histogram);  // see crbug.com/79322
   return histogram;
 }
 
