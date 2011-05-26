31d7a1fa390c23c2e7c0bb739cccd2565b9e66f5
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-26 06:48:12 +0000

    2011-05-25  Jer Noble  <jer.noble@apple.com>
            Reviewed by Dan Bernstein.
    
            30 second rewind button dysfunctional viewing trailers fullscreen.
            https://bugs.webkit.org/show_bug.cgi?id=61505
    
            Add support for painting MediaReturnToRealtimeButton, as well as hide and show
            the correct default video control buttons when entering full-screen mode.
    
            * css/fullscreenQuickTime.css:
            (video:-webkit-full-screen::-webkit-media-controls-return-to-realtime-button):
            * html/shadow/MediaControlRootElement.cpp:
            (WebCore::MediaControlRootElement::enteredFullscreen): Show the "Back 30s" and
                "Return to Realtime" buttons for Live Streams in full-screen mode.
            (WebCore::MediaControlRootElement::exitedFullscreen): Reset the styles of
                all the buttons affected by enteredFullscreen().
            * rendering/RenderMediaControls.cpp:
            (WebCore::RenderMediaControls::paintMediaControlsPart): Add support for
                MediaReturnToRealtimeButton.
    2011-05-25  Jer Noble  <jer.noble@apple.com>
    
            Reviewed by Dan Bernstein.
    
            30 second rewind button dysfunctional viewing trailers fullscreen.
            https://bugs.webkit.org/show_bug.cgi?id=61505
    
            Updated WKSI Libraries with new images for Backward buttons in default
            video controls.
    
            * libWebKitSystemInterfaceLeopard.a:
            * libWebKitSystemInterfaceSnowLeopard.a:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElement.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElement.cpp
@@ -410,11 +410,17 @@ void MediaControlRootElement::changedVolume()
 
 void MediaControlRootElement::enteredFullscreen()
 {
-    if (m_mediaElement->movieLoadType() == MediaPlayer::LiveStream || m_mediaElement->movieLoadType() == MediaPlayer::StoredStream) {
+    if (m_mediaElement->movieLoadType() == MediaPlayer::LiveStream) {
         m_seekBackButton->hide();
         m_seekForwardButton->hide();
-    } else
+        m_rewindButton->show();
+        m_returnToRealTimeButton->show();
+    } else {
+        m_seekBackButton->show();
+        m_seekForwardButton->show();
         m_rewindButton->hide();
+        m_returnToRealTimeButton->hide();
+    }
 }
 
 void MediaControlRootElement::exitedFullscreen()
@@ -425,6 +431,7 @@ void MediaControlRootElement::exitedFullscreen()
     m_rewindButton->show();
     m_seekBackButton->show();
     m_seekForwardButton->show();
+    m_returnToRealTimeButton->show();
 }
 
 void MediaControlRootElement::showVolumeSlider()
