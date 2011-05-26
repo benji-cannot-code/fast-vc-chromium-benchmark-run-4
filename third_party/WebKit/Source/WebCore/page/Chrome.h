6f92d4fd2968f2d4431ca38c808048a23c866873
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-26 22:49:48 +0000

    Need a callback for when the preferred rendered size changed. https://bugs.webkit.org/show_bug.cgi?id=61309
    Reviewed by Darin Fisher.
    
    Source/WebCore:
    
    No new tests needed since no new functionality is exposed.
    
    * page/Chrome.cpp:
    (WebCore::Chrome::contentsPreferredSizeChanged): Plumbed through the call.
    * page/Chrome.h: Added the new method.
    * page/ChromeClient.h: Ditto.
    * page/FrameView.cpp:
    (WebCore::FrameView::setContentsSize): Added calculations for the preferred size
    and the call to contentsPreferredSizeChanged.
    
    Source/WebKit/chromium:
    
    * public/WebFrameClient.h
    (WebKit::WebFrameClient::didChangeContentsPreferredSize): Added stub.
    * src/ChromeClientImpl.h:
    (WebKit::ChromeClientImpl::contentsPreferredSizeChanged): Added declaration.
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::contentsPreferredSizeChanged): Added call to WebFrameClient::didChangeContentsPreferredSize.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Chrome.h
+++ third_party/WebKit/Source/WebCore/page/Chrome.h
@@ -85,6 +85,7 @@ namespace WebCore {
         void scrollRectIntoView(const IntRect&) const;
 
         void contentsSizeChanged(Frame*, const IntSize&) const;
+        void contentsPreferredSizeChanged(Frame*, const IntSize&) const;
 
         void setWindowRect(const FloatRect&) const;
         FloatRect windowRect() const;
