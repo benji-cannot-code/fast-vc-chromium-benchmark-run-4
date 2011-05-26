77c46d0cc07810bc938bbc909335c42f10feaa38
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 13:17:59 +0000

    Add a websocket-over-spdy flag. This change is a part of WebSocket over SPDY support in progress.
    BUG=42320
    TEST=N/A
    
    Review URL: http://codereview.chromium.org/7047003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86812 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_session_pool.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_throttler_manager.h"
+#include "net/websockets/websocket_job.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/gl/gl_implementation.h"
@@ -726,6 +727,11 @@ void InitializeNetworkOptions(const CommandLine& parsed_command_line) {
       true);
 
   SetDnsCertProvenanceCheckerFactory(CreateChromeDnsCertProvenanceChecker);
+
+  if (parsed_command_line.HasSwitch(switches::kEnableWebSocketOverSpdy)) {
+    // Enable WebSocket over SPDY.
+    net::WebSocketJob::set_websocket_over_spdy_enabled(true);
+  }
 }
 
 // Creates key child threads. We need to do this explicitly since
