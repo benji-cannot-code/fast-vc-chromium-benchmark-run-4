8ae1ac66de672f50542d52038660d5bf70e3a712
annacc@chromium.org <annacc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 17:52:17 +0000

    Surround sound swizzling for Mac based on device channel layout preferences.
    This patch gets the surround sound channel layout from the user's device using CoreAudio.
    It then swizzles the channel layout from the source to match the user's prefered channel layout (if needed).
    
    BUG=none.
    TEST=none.
    
    
    Review URL: http://codereview.chromium.org/7047020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86851 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/audio_messages.h
+++ content/common/audio_messages.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 IPC_ENUM_TRAITS(AudioStreamState)
 IPC_ENUM_TRAITS(AudioParameters::Format)
+IPC_ENUM_TRAITS(ChannelLayout)
 
 IPC_STRUCT_TRAITS_BEGIN(AudioBuffersState)
   IPC_STRUCT_TRAITS_MEMBER(pending_bytes)
@@ -26,10 +27,11 @@ IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(AudioParameters)
   IPC_STRUCT_TRAITS_MEMBER(format)
-  IPC_STRUCT_TRAITS_MEMBER(channels)
+  IPC_STRUCT_TRAITS_MEMBER(channel_layout)
   IPC_STRUCT_TRAITS_MEMBER(sample_rate)
   IPC_STRUCT_TRAITS_MEMBER(bits_per_sample)
   IPC_STRUCT_TRAITS_MEMBER(samples_per_packet)
+  IPC_STRUCT_TRAITS_MEMBER(channels)
 IPC_STRUCT_TRAITS_END()
 
 // Messages sent from the browser to the renderer.
--- media/audio/mac/audio_output_mac.h
+++ media/audio/mac/audio_output_mac.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <AudioToolbox/AudioFormat.h>
 #include <AudioToolbox/AudioQueue.h>
+#include <AudioUnit/AudioUnit.h>
 
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_parameters.h"
@@ -38,6 +39,13 @@ class PCMQueueOutAudioOutputStream : public AudioOutputStream {
  private:
   // The audio is double buffered.
   static const uint32 kNumBuffers = 2;
+  static const int kEmptyChannel = -1;
+
+  // Reorder PCM from source layout to device layout found in Core Audio.
+  template<class Format>
+  void SwizzleLayout(Format* b, uint32 filled);
+  // Check and move channels if surround sound layout needs adjusted.
+  bool CheckForAdjustedLayout(Channels input_channel, Channels output_channel);
 
   // The OS calls back here when an audio buffer has been processed.
   static void RenderCallback(void* p_this, AudioQueueRef queue,
@@ -63,6 +71,20 @@ class PCMQueueOutAudioOutputStream : public AudioOutputStream {
   float volume_;
   // Number of bytes yet to be played in audio buffer.
   uint32 pending_bytes_;
+  // Number of channels in the source audio.
+  int num_source_channels_;
+  // Source's channel layout for surround sound channels.
+  ChannelLayout source_layout_;
+  // Device's channel layout.
+  int core_channel_orderings_[CHANNELS_MAX];
+  // An array for remapping source to device channel layouts during a swizzle.
+  int channel_remap_[CHANNELS_MAX];
+  // Number of channels in device layout.
+  int num_core_channels_;
+  // A flag to determine if swizzle is needed from source to device layouts.
+  bool should_swizzle_;
+  // A flag to determine if downmix is needed from source to device layouts.
+  bool should_down_mix_;
 
   DISALLOW_COPY_AND_ASSIGN(PCMQueueOutAudioOutputStream);
 };
