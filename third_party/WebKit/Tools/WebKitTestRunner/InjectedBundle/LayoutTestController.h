43d0a6d06ab9cbeea788bf6e59bdbcc298397bba
qi.2.zhang@nokia.com <qi.2.zhang@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-26 01:48:25 +0000

    2011-05-25  Qi Zhang  <qi.2.zhang@nokia.com>
            Reviewed by Simon Hausmann.
    
            WebKitTestRunner needs layoutTestController.setPrivateBrowsingEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42697
    
            * platform/mac-wk2/Skipped:
            * platform/qt-wk2/Skipped:
    2011-05-25  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Simon Hausmann.
    
            WebKitTestRunner needs layoutTestController.setPrivateBrowsingEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42697
    
            Implemented layoutTestController.setPrivateBrowsingEnabled for WebKitTestRunner.
    
            * WebProcess/InjectedBundle/API/c/WKBundle.cpp:
            (WKBundleSetPrivateBrowsingEnabled):
            * WebProcess/InjectedBundle/API/c/WKBundlePrivate.h:
            * WebProcess/InjectedBundle/InjectedBundle.cpp:
            (WebKit::InjectedBundle::setPrivateBrowsingEnabled):
            * WebProcess/InjectedBundle/InjectedBundle.h:
    2011-05-25  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Simon Hausmann.
    
            WebKitTestRunner needs layoutTestController.setPrivateBrowsingEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42697
    
            Implemented layoutTestController.setPrivateBrowsingEnabled for WebKitTestRunner.
    
            * WebKitTestRunner/InjectedBundle/Bindings/LayoutTestController.idl:
            * WebKitTestRunner/InjectedBundle/InjectedBundle.cpp:
            (WTR::InjectedBundle::beginTesting):
            * WebKitTestRunner/InjectedBundle/LayoutTestController.cpp:
            (WTR::LayoutTestController::setPrivateBrowsingEnabled):
            * WebKitTestRunner/InjectedBundle/LayoutTestController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
@@ -81,6 +81,7 @@ public:
     void setAllowFileAccessFromFileURLs(bool);
     void setFrameFlatteningEnabled(bool);
     void setJavaScriptCanAccessClipboard(bool);
+    void setPrivateBrowsingEnabled(bool);
     void addOriginAccessWhitelistEntry(JSStringRef sourceOrigin, JSStringRef destinationProtocol, JSStringRef destinationHost, bool allowDestinationSubdomains);
     void removeOriginAccessWhitelistEntry(JSStringRef sourceOrigin, JSStringRef destinationProtocol, JSStringRef destinationHost, bool allowDestinationSubdomains);
 
