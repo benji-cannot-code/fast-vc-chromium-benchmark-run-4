b89fb5f2cc95d940125afbc28a89dfd43dec2fcf
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 12:10:54 +0000

    Sanitize enclosing quotes around paths in policies.
    BUG=80211
    TEST=unit_tests PolicyPathParser*
    
    Review URL: http://codereview.chromium.org/6966032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_path_parser_unittest.cc
+++ chrome/browser/policy/policy_path_parser_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,6 +33,16 @@ TEST_F(PolicyPathParserTests, AllPlatformVariables) {
   FilePath::StringType unknown_vars_result =
       path_parser::ExpandPathVariables(unknown_vars);
   ASSERT_EQ(unknown_vars_result, unknown_vars);
+
+  // Trim quotes around, but not inside paths. Test against bug 80211.
+  FilePath::StringType no_quotes(FILE_PATH_LITERAL("//$C/\"a\"/$path"));
+  FilePath::StringType single_quotes(FILE_PATH_LITERAL("'//$C/\"a\"/$path'"));
+  FilePath::StringType double_quotes(FILE_PATH_LITERAL("\"//$C/\"a\"/$path\""));
+  FilePath::StringType quotes_result =
+      path_parser::ExpandPathVariables(single_quotes);
+  ASSERT_EQ(quotes_result, no_quotes);
+  quotes_result = path_parser::ExpandPathVariables(double_quotes);
+  ASSERT_EQ(quotes_result, no_quotes);
 
   // Both should have been substituted.
   FilePath::StringType vars(FILE_PATH_LITERAL("${user_name}${machine_name}"));
