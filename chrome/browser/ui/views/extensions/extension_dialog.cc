6cc1ceb277c46f848d6f958c311f145577dfc294
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 20:36:35 +0000

    CrOS - Fix crash on opening/saving file with file browser
    Users of SelectFileDialog may delete the object before the window is done closing, so we need to clean things up early.
    
    BUG=chromium-os:15758
    TEST=Use bookmarks manager to export and import a set of bookmarks.
    
    Review URL: http://codereview.chromium.org/6962020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86889 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_dialog.cc
+++ chrome/browser/ui/views/extensions/extension_dialog.cc
@@ -80,6 +80,10 @@ ExtensionDialog* ExtensionDialog::Show(
   return popup;
 }
 
+void ExtensionDialog::ObserverDestroyed() {
+  observer_ = NULL;
+}
+
 void ExtensionDialog::Close() {
   if (closing_)
     return;
