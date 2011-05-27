66d3453a24a106ba20a41ebae37bb74002d7b38d
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-27 10:37:23 +0000

    2011-05-26  Hans Wennborg  <hans@chromium.org>
            Reviewed by Tony Gentilcore.
    
            LevelDB: turn on paranoid checks and verify checksums, log errors
            https://bugs.webkit.org/show_bug.cgi?id=61516
    
            This allows for detection of corrupted databases.
            Even if we can't recover from a corrupted database, discovering the
            problem is a step in the right direction.
    
            No new functionality, no new tests.
    
            * platform/leveldb/LevelDBDatabase.cpp:
            (WebCore::LevelDBDatabase::open):
            (WebCore::LevelDBDatabase::put):
            (WebCore::LevelDBDatabase::remove):
            (WebCore::LevelDBDatabase::get):
            (WebCore::LevelDBDatabase::write):
            (WebCore::IteratorImpl::checkStatus):
            (WebCore::IteratorImpl::seekToLast):
            (WebCore::IteratorImpl::seek):
            (WebCore::IteratorImpl::next):
            (WebCore::IteratorImpl::prev):
            (WebCore::LevelDBDatabase::createIterator):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBDatabase.cpp
+++ third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBDatabase.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LevelDBIterator.h"
 #include "LevelDBSlice.h"
 #include "LevelDBWriteBatch.h"
+#include "Logging.h"
 #include <leveldb/comparator.h>
 #include <leveldb/db.h>
 #include <leveldb/slice.h>
@@ -99,17 +100,19 @@ PassOwnPtr<LevelDBDatabase> LevelDBDatabase::open(const String& fileName, const 
 {
     OwnPtr<ComparatorAdapter> comparatorAdapter = adoptPtr(new ComparatorAdapter(comparator));
 
-    OwnPtr<LevelDBDatabase> result = adoptPtr(new LevelDBDatabase);
-
     leveldb::Options options;
     options.comparator = comparatorAdapter.get();
     options.create_if_missing = true;
+    options.paranoid_checks = true;
     leveldb::DB* db;
-    leveldb::Status s = leveldb::DB::Open(options, fileName.utf8().data(), &db);
+    const leveldb::Status s = leveldb::DB::Open(options, fileName.utf8().data(), &db);
 
-    if (!s.ok())
+    if (!s.ok()) {
+        LOG_ERROR("Failed to open LevelDB database from %s: %s", fileName.ascii().data(), s.ToString().c_str());
         return nullptr;
+    }
 
+    OwnPtr<LevelDBDatabase> result = adoptPtr(new LevelDBDatabase);
     result->m_db = adoptPtr(db);
     result->m_comparatorAdapter = comparatorAdapter.release();
     result->m_comparator = comparator;
@@ -122,7 +125,11 @@ bool LevelDBDatabase::put(const LevelDBSlice& key, const Vector<char>& value)
     leveldb::WriteOptions writeOptions;
     writeOptions.sync = true;
 
-    return m_db->Put(writeOptions, makeSlice(key), makeSlice(value)).ok();
+    const leveldb::Status s = m_db->Put(writeOptions, makeSlice(key), makeSlice(value));
+    if (s.ok())
+        return true;
+    LOG_ERROR("LevelDB put failed: %s", s.ToString().c_str());
+    return false;
 }
 
 bool LevelDBDatabase::remove(const LevelDBSlice& key)
@@ -130,17 +137,30 @@ bool LevelDBDatabase::remove(const LevelDBSlice& key)
     leveldb::WriteOptions writeOptions;
     writeOptions.sync = true;
 
-    return m_db->Delete(writeOptions, makeSlice(key)).ok();
+    const leveldb::Status s = m_db->Delete(writeOptions, makeSlice(key));
+    if (s.ok())
+        return true;
+    if (s.IsNotFound())
+        return false;
+    LOG_ERROR("LevelDB remove failed: %s", s.ToString().c_str());
+    return false;
 }
 
 bool LevelDBDatabase::get(const LevelDBSlice& key, Vector<char>& value)
 {
     std::string result;
-    if (!m_db->Get(leveldb::ReadOptions(), makeSlice(key), &result).ok())
-        return false;
+    leveldb::ReadOptions readOptions;
+    readOptions.verify_checksums = true; // FIXME: Disable this if the performance impact is too great.
 
-    value = makeVector(result);
-    return true;
+    const leveldb::Status s = m_db->Get(readOptions, makeSlice(key), &result);
+    if (s.ok()) {
+        value = makeVector(result);
+        return true;
+    }
+    if (s.IsNotFound())
+        return false;
+    LOG_ERROR("LevelDB get failed: %s", s.ToString().c_str());
+    return false;
 }
 
 bool LevelDBDatabase::write(LevelDBWriteBatch& writeBatch)
@@ -148,7 +168,11 @@ bool LevelDBDatabase::write(LevelDBWriteBatch& writeBatch)
     leveldb::WriteOptions writeOptions;
     writeOptions.sync = true;
 
-    return m_db->Write(writeOptions, writeBatch.m_writeBatch.get()).ok();
+    const leveldb::Status s = m_db->Write(writeOptions, writeBatch.m_writeBatch.get());
+    if (s.ok())
+        return true;
+    LOG_ERROR("LevelDB write failed: %s", s.ToString().c_str());
+    return false;
 }
 
 namespace {
@@ -167,6 +191,7 @@ public:
 private:
     friend class WebCore::LevelDBDatabase;
     IteratorImpl(PassOwnPtr<leveldb::Iterator>);
+    void checkStatus();
 
     OwnPtr<leveldb::Iterator> m_iterator;
 };
@@ -177,6 +202,13 @@ IteratorImpl::IteratorImpl(PassOwnPtr<leveldb::Iterator> it)
 {
 }
 
+void IteratorImpl::checkStatus()
+{
+    const leveldb::Status s = m_iterator->status();
+    if (!s.ok())
+        LOG_ERROR("LevelDB iterator error: %s", s.ToString().c_str());
+}
+
 bool IteratorImpl::isValid() const
 {
     return m_iterator->Valid();
@@ -185,23 +217,27 @@ bool IteratorImpl::isValid() const
 void IteratorImpl::seekToLast()
 {
     m_iterator->SeekToLast();
+    checkStatus();
 }
 
 void IteratorImpl::seek(const LevelDBSlice& target)
 {
     m_iterator->Seek(makeSlice(target));
+    checkStatus();
 }
 
 void IteratorImpl::next()
 {
     ASSERT(isValid());
     m_iterator->Next();
+    checkStatus();
 }
 
 void IteratorImpl::prev()
 {
     ASSERT(isValid());
     m_iterator->Prev();
+    checkStatus();
 }
 
 LevelDBSlice IteratorImpl::key() const
@@ -218,7 +254,9 @@ LevelDBSlice IteratorImpl::value() const
 
 PassOwnPtr<LevelDBIterator> LevelDBDatabase::createIterator()
 {
-    OwnPtr<leveldb::Iterator> i = adoptPtr(m_db->NewIterator(leveldb::ReadOptions()));
+    leveldb::ReadOptions readOptions;
+    readOptions.verify_checksums = true; // FIXME: Disable this if the performance impact is too great.
+    OwnPtr<leveldb::Iterator> i = adoptPtr(m_db->NewIterator(readOptions));
     if (!i) // FIXME: Double check if we actually need to check this.
         return nullptr;
     return adoptPtr(new IteratorImpl(i.release()));
