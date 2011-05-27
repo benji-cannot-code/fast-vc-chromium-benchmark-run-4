3285d595c213dabae9b6c745d1eb4af43f6a11cc
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-27 19:02:10 +0000

    Fix overlapping_rect's screen coordinate calculation in find bar
    The find bar is supposed to move when it is intersected with the selected text rectangle.
    To decide the intersection, the selected text rect needs to be translated to the screen
    coordinate first.
    
    Currently the webcontents_rect is the web page rect in its *parent's* coordinate which is
    wrong for calculating the translation for avoid_overlapping_rect.
    
    The patch fixes the translation by using webcontents_rect's screen coordinate. 
    
    
    BUG=
    TEST= FindInPageControllerTest.FindMovesWhenObscuring in browser_tests pass for touch build
    
    
    Review URL: http://codereview.chromium.org/7083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87059 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/find_bar_host_gtk.cc
+++ chrome/browser/ui/views/find_bar_host_gtk.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/browser/tab_contents/tab_contents_view.h"
 
+#if defined(TOUCH_UI)
+#include "chrome/browser/ui/views/tab_contents/tab_contents_view_touch.h"
+#endif
+
 void FindBarHost::AudibleAlert() {
   // TODO(davemoore) implement.
   NOTIMPLEMENTED();
@@ -20,7 +24,12 @@ void FindBarHost::GetWidgetPositionNative(gfx::Rect* avoid_overlapping_rect) {
   gfx::Rect frame_rect = host()->GetTopLevelWidget()->GetWindowScreenBounds();
   TabContentsView* tab_view = find_bar_controller_->tab_contents()->view();
   gfx::Rect webcontents_rect;
+#if defined(TOUCH_UI)
+  webcontents_rect =
+      static_cast<TabContentsViewTouch*>(tab_view)->GetScreenBounds();
+#else
   tab_view->GetViewBounds(&webcontents_rect);
+#endif
   avoid_overlapping_rect->Offset(0, webcontents_rect.y() - frame_rect.y());
 }
 
