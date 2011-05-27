327bfcdf46bf663606e5fddff1661a088d6fcef5
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-27 17:36:31 +0000

    Fixes bug in updating only part of a texture and removes irrelevant TODO.
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/6962042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87042 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -1166,7 +1166,6 @@ void View::PaintComposite() {
     return;
 
   if (texture_.get()) {
-    // TODO: if dirty_region doesn't itersect bounds, return.
     ui::Transform transform;
     GetTransformRelativeTo(NULL, &transform);
     texture_->Draw(transform);
