066e7bc1b542daa813a19a13bf044b12e3ea526c
qi.2.zhang@nokia.com <qi.2.zhang@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-27 00:00:42 +0000

    2011-05-26  Qi Zhang  <qi.2.zhang@nokia.com>
            Reviewed by Andreas Kling.
    
            WebKitTestRunner needs layoutTestController.setAuthorAndUserStylesEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42676
    
            Implemented layoutTestController.setAuthorAndUserStylesEnabled for WebKitTestRunner.
    
            * platform/mac-wk2/Skipped:
            * platform/qt-wk2/Skipped:
    2011-05-26  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Andreas Kling.
    
            WebKitTestRunner needs layoutTestController.setAuthorAndUserStylesEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42676
    
            Implemented layoutTestController.setAuthorAndUserStylesEnabled for WebKitTestRunner.
    
            * WebProcess/InjectedBundle/API/c/WKBundle.cpp:
            (WKBundleSetAuthorAndUserStylesEnabled):
            * WebProcess/InjectedBundle/API/c/WKBundlePrivate.h:
            * WebProcess/InjectedBundle/InjectedBundle.cpp:
            (WebKit::InjectedBundle::setAuthorAndUserStylesEnabled):
            * WebProcess/InjectedBundle/InjectedBundle.h:
    2011-05-26  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Andreas Kling.
    
            WebKitTestRunner needs layoutTestController.setAuthorAndUserStylesEnabled
            https://bugs.webkit.org/show_bug.cgi?id=42676
    
            Implemented layoutTestController.setAuthorAndUserStylesEnabled for WebKitTestRunner.
    
            * WebKitTestRunner/InjectedBundle/Bindings/LayoutTestController.idl:
            * WebKitTestRunner/InjectedBundle/InjectedBundle.cpp:
            (WTR::InjectedBundle::beginTesting):
            * WebKitTestRunner/InjectedBundle/LayoutTestController.cpp:
            (WTR::LayoutTestController::setAuthorAndUserStylesEnabled):
            * WebKitTestRunner/InjectedBundle/LayoutTestController.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundle.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundle.cpp
@@ -181,6 +181,7 @@ void InjectedBundle::beginTesting()
     WKBundleSetAllowUniversalAccessFromFileURLs(m_bundle, m_pageGroup, true);
     WKBundleSetJavaScriptCanAccessClipboard(m_bundle, m_pageGroup, true);
     WKBundleSetPrivateBrowsingEnabled(m_bundle, m_pageGroup, false);
+    WKBundleSetAuthorAndUserStylesEnabled(m_bundle, m_pageGroup, true);
 
     WKBundleRemoveAllUserContent(m_bundle, m_pageGroup);
 
