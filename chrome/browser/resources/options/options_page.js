9ee33215e47d2c6615d774f1e95930c7d2748de8
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-27 17:35:22 +0000

    Fix OptionsPage overlay not showing up.
    BUG=chromium-os:15787,chromium-os:15788
    TEST=Verify fix for chromium-os:15787 and 15788
    
    Review URL: http://codereview.chromium.org/7075022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87040 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_page.js
+++ chrome/browser/resources/options/options_page.js
@@ -835,9 +835,7 @@ cr.define('options', function() {
      * Gets page visibility state.
      */
     get visible() {
-      var page = $(this.pageDivName);
-      return page && page.ownerDocument.defaultView.getComputedStyle(
-          page).display == 'block';
+      return !this.pageDiv.hidden;
     },
 
     /**
