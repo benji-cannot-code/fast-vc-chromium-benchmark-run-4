3c29a85f1e5dce838d27d7195c919e5d0b7a7ab9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-27 06:26:57 +0000

    2011-05-26  Luke Macpherson   <macpherson@chromium.org>
            Reviewed by Eric Seidel.
    
            Implement webkit margin collapse CSS properties in CSSStyleApplyProperty
            https://bugs.webkit.org/show_bug.cgi?id=61589
    
            No new tests as no new functionality added.
    
            * css/CSSStyleApplyProperty.cpp:
            (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
            Add handlers for:
              CSSPropertyWebkitMarginBeforeCollapse
              CSSPropertyWebkitMarginAfterCollapse
              CSSPropertyWebkitMarginTopCollapse
              CSSPropertyWebkitMarginBottomCollapse
              CSSPropertyWebkitMarginCollapse
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            Remove cases that are no longer required.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -4967,26 +4967,6 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         m_style->setTextOverflow(primitiveValue->getIdent() == CSSValueEllipsis);
         return;
     }
-    case CSSPropertyWebkitMarginCollapse: {
-        if (isInherit) {
-            m_style->setMarginBeforeCollapse(m_parentStyle->marginBeforeCollapse());
-            m_style->setMarginAfterCollapse(m_parentStyle->marginAfterCollapse());
-        }
-        else if (isInitial) {
-            m_style->setMarginBeforeCollapse(MCOLLAPSE);
-            m_style->setMarginAfterCollapse(MCOLLAPSE);
-        }
-        return;
-    }
-
-    case CSSPropertyWebkitMarginBeforeCollapse:
-    case CSSPropertyWebkitMarginTopCollapse:
-        HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(marginBeforeCollapse, MarginBeforeCollapse)
-        return;
-    case CSSPropertyWebkitMarginAfterCollapse:
-    case CSSPropertyWebkitMarginBottomCollapse:
-        HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(marginAfterCollapse, MarginAfterCollapse)
-        return;
     case CSSPropertyWebkitLineClamp: {
         HANDLE_INHERIT_AND_INITIAL(lineClamp, LineClamp)
         if (!primitiveValue)
@@ -5268,6 +5248,11 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWebkitMarginStart:
     case CSSPropertyWebkitMarginBefore:
     case CSSPropertyWebkitMarginAfter:
+    case CSSPropertyWebkitMarginCollapse:
+    case CSSPropertyWebkitMarginBeforeCollapse:
+    case CSSPropertyWebkitMarginTopCollapse:
+    case CSSPropertyWebkitMarginAfterCollapse:
+    case CSSPropertyWebkitMarginBottomCollapse:
     case CSSPropertyWebkitPaddingEnd:
     case CSSPropertyWebkitPaddingStart:
     case CSSPropertyWebkitPaddingBefore:
