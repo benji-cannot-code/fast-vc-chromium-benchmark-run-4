c3f493ba4f7da35a5c04286092b8c9025afceab9
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-27 14:02:26 +0000

    2011-05-27  Hans Wennborg  <hans@chromium.org>
            Reviewed by Tony Gentilcore.
    
            IndexedDB: Support mutating cursors on top of LevelDB
            https://bugs.webkit.org/show_bug.cgi?id=61615
    
            New test for adding new entries to an objecet store while running a
            cursor over it.
    
            * storage/indexeddb/mutating-cursor-expected.txt: Added.
            * storage/indexeddb/mutating-cursor.html: Added.
    2011-05-27  Hans Wennborg  <hans@chromium.org>
    
            Reviewed by Tony Gentilcore.
    
            IndexedDB: Support mutating cursors on top of LevelDB
            https://bugs.webkit.org/show_bug.cgi?id=61615
    
            We need to support the case where a new node is added to the tree in a
            transaction after the TreeIterator has covered the whole tree.
    
            Since this is done lazily, i.e. we set a flag that the tree might have
            changed, and act upon it later, some members need to be mutable,
            because we might need to re-seek the tree iterator in a const function.
    
            Test: storage/indexeddb/mutating-cursor.html
                  storage/indexeddb/mozilla/cursor-mutation-objectstore-only.html (existing)
    
            * platform/leveldb/LevelDBTransaction.cpp:
            (WebCore::LevelDBTransaction::set):
            (WebCore::LevelDBTransaction::TreeIterator::reset):
            (WebCore::LevelDBTransaction::TreeIterator::~TreeIterator):
            (WebCore::LevelDBTransaction::TreeIterator::TreeIterator):
            (WebCore::LevelDBTransaction::TransactionIterator::TransactionIterator):
            (WebCore::LevelDBTransaction::TransactionIterator::~TransactionIterator):
            (WebCore::LevelDBTransaction::TransactionIterator::next):
            (WebCore::LevelDBTransaction::TransactionIterator::prev):
            (WebCore::LevelDBTransaction::TransactionIterator::key):
            (WebCore::LevelDBTransaction::TransactionIterator::value):
            (WebCore::LevelDBTransaction::TransactionIterator::treeChanged):
            (WebCore::LevelDBTransaction::TransactionIterator::refreshTreeIterator):
            (WebCore::LevelDBTransaction::registerIterator):
            (WebCore::LevelDBTransaction::unregisterIterator):
            (WebCore::LevelDBTransaction::notifyIteratorsOfTreeChange):
            * platform/leveldb/LevelDBTransaction.h:
            * storage/IDBFactoryBackendImpl.cpp:
            (WebCore::IDBFactoryBackendImpl::open):
            * storage/IDBLevelDBBackingStore.cpp:
            (WebCore::IDBLevelDBBackingStore::open):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBTransaction.h
+++ third_party/WebKit/Source/WebCore/platform/leveldb/LevelDBTransaction.h
@@ -122,7 +122,7 @@ private:
 
     class TransactionIterator : public LevelDBIterator {
     public:
-        ~TransactionIterator() { };
+        ~TransactionIterator();
         static PassOwnPtr<TransactionIterator> create(PassRefPtr<LevelDBTransaction>);
 
         virtual bool isValid() const;
@@ -132,16 +132,18 @@ private:
         virtual void prev();
         virtual LevelDBSlice key() const;
         virtual LevelDBSlice value() const;
+        void treeChanged();
 
     private:
         TransactionIterator(PassRefPtr<LevelDBTransaction>);
         void handleConflictsAndDeletes();
         void setCurrentIteratorToSmallestKey();
         void setCurrentIteratorToLargestKey();
+        void refreshTreeIterator() const;
 
         RefPtr<LevelDBTransaction> m_transaction;
         const LevelDBComparator* m_comparator;
-        OwnPtr<TreeIterator> m_treeIterator;
+        mutable OwnPtr<TreeIterator> m_treeIterator;
         OwnPtr<LevelDBIterator> m_dbIterator;
         LevelDBIterator* m_current;
 
@@ -150,19 +152,20 @@ private:
             kReverse
         };
         Direction m_direction;
+        mutable bool m_treeChanged;
     };
 
     bool set(const LevelDBSlice& key, const Vector<char>& value, bool deleted);
     void clearTree();
-    void registerIterator(TreeIterator*);
-    void unregisterIterator(TreeIterator*);
-    void resetIterators();
+    void registerIterator(TransactionIterator*);
+    void unregisterIterator(TransactionIterator*);
+    void notifyIteratorsOfTreeChange();
 
     LevelDBDatabase* m_db;
     const LevelDBComparator* m_comparator;
     TreeType m_tree;
     bool m_finished;
-    HashSet<TreeIterator*> m_treeIterators;
+    HashSet<TransactionIterator*> m_iterators;
 };
 
 }
