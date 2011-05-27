4aa2d97124cf8132572cba890d65d72f9163e74a
koz@chromium.org <koz@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-27 01:16:57 +0000

    2011-05-26  James Kozianski  <koz@chromium.org>
            Reviewed by Eric Seidel.
    
            Implement a whitelist for registerProtocolHandler.
    
            Described in the thread here
            http://lists.whatwg.org/htdig.cgi/whatwg-whatwg.org/2011-April/031220.html
    
            https://bugs.webkit.org/show_bug.cgi?id=60322
    
            * fast/dom/register-protocol-handler.html:
    2011-05-26  James Kozianski  <koz@chromium.org>
    
            Reviewed by Eric Seidel.
    
            Implement a whitelist for registerProtocolHandler
            https://bugs.webkit.org/show_bug.cgi?id=60322
    
            * page/Navigator.cpp:
            (WebCore::initProtocolHandlerWhitelist):
            (WebCore::isProtocolWhitelisted):
            (WebCore::verifyProtocolHandlerScheme):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Navigator.cpp
+++ third_party/WebKit/Source/WebCore/page/Navigator.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginData.h"
 #include "Settings.h"
 #include "StorageNamespace.h"
+#include <wtf/HashSet.h>
 #include <wtf/StdLibExtras.h>
 
 namespace WebCore {
@@ -184,6 +185,22 @@ void Navigator::getStorageUpdates()
 #endif
 
 #if ENABLE(REGISTER_PROTOCOL_HANDLER)
+static HashSet<String>* protocolWhitelist;
+
+static void initProtocolHandlerWhitelist()
+{
+    protocolWhitelist = new HashSet<String>;
+    static const char* protocols[] = {
+        "mailto",
+        "mms",
+        "nntp",
+        "rtsp",
+        "webcal",
+    };
+    for (size_t i = 0; i < WTF_ARRAY_LENGTH(protocols); ++i)
+        protocolWhitelist->add(protocols[i]);
+}
+
 static bool verifyCustomHandlerURL(const String& baseURL, const String& url, ExceptionCode& ec)
 {
     // The specification requires that it is a SYNTAX_ERR if the "%s" token is
@@ -211,14 +228,26 @@ static bool verifyCustomHandlerURL(const String& baseURL, const String& url, Exc
     return true;
 }
 
+static bool isProtocolWhitelisted(const String& scheme)
+{
+    if (!protocolWhitelist)
+        initProtocolHandlerWhitelist();
+    return protocolWhitelist->contains(scheme);
+}
+
 static bool verifyProtocolHandlerScheme(const String& scheme, ExceptionCode& ec)
 {
-    // It is a SECURITY_ERR for these schemes to be handled by a custom handler.
-    if (equalIgnoringCase(scheme, "http") || equalIgnoringCase(scheme, "https") || equalIgnoringCase(scheme, "file")) {
+    if (scheme.startsWith("web+")) {
+        if (isValidProtocol(scheme))
+            return true;
         ec = SECURITY_ERR;
         return false;
     }
-    return true;
+
+    if (isProtocolWhitelisted(scheme))
+        return true;
+    ec = SECURITY_ERR;
+    return false;
 }
 
 void Navigator::registerProtocolHandler(const String& scheme, const String& url, const String& title, ExceptionCode& ec)
