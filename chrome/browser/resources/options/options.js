8d4ebb16b1c4bf6c8945d8fed0d3e5592b88ccbb
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-27 19:32:54 +0000

    Fix the close button on the modifier keys setting overlay not working.
    In order to avoid the error because of Content-Security-Policy,
    I moved the code of onclick event handler to options.js.
    
    BUG=chromium-os:15821
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7062047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87065 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options.js
+++ chrome/browser/resources/options/options.js
@@ -167,6 +167,10 @@ function load() {
         'languageCustomizeModifierKeysOverlay',
         localStrings.getString('languageCustomizeModifierKeysOverlay'),
         'languageCustomizeModifierKeysOverlay')
+    $('languageCustomizeModifierKeysOverleyDismissButton').onclick =
+        function() {
+      OptionsPage.closeOverlay();
+    };
     OptionsPage.registerOverlay(languageModifierKeysOverlay,
                                 SystemOptions.getInstance(),
                                 [$('modifier-keys-button')]);
