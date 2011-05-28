200eb5fea89c3db0b522308e528c998126fd5654
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-28 17:55:16 +0000

    2011-05-28  Tonis Tiigi  <tonistiigi@gmail.com>
            Reviewed by Pavel Feldman.
    
            Web Inspector: there is a problem with computed style properties with wrong values
            https://bugs.webkit.org/show_bug.cgi?id=60535
    
            Adding cases that test invalid properties.
    
            * inspector/styles/styles-computed-trace-expected.txt:
            * inspector/styles/styles-computed-trace.html:
    2011-05-28  Tonis Tiigi  <tonistiigi@gmail.com>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: there is a problem with computed style properties with wrong values
            https://bugs.webkit.org/show_bug.cgi?id=60535
    
            * inspector/front-end/StylesSidebarPane.js:
            (WebInspector.StylesSidebarPane.prototype._markUsedProperties):
            (WebInspector.ComputedStylePropertiesSection.prototype.rebuildComputedTrace):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
@@ -397,7 +397,7 @@ WebInspector.StylesSidebarPane.prototype = {
             var allProperties = style.allProperties;
             for (var j = 0; j < allProperties.length; ++j) {
                 var property = allProperties[j];
-                if (!property.isLive)
+                if (!property.isLive || !property.parsedOk)
                     continue;
                 var name = property.name;
 
@@ -1130,6 +1130,8 @@ WebInspector.ComputedStylePropertiesSection.prototype = {
                     treeElement.appendChild(childElement);
                     if (section.isPropertyOverloaded(property.name))
                         childElement.listItemElement.addStyleClass("overloaded");
+                    if (!property.parsedOk)
+                        childElement.listItemElement.addStyleClass("not-parsed-ok");
                 }
             }
         }
