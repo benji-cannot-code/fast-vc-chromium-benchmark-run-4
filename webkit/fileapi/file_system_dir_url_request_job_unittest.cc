573646e49bfeb0c7d60c047478c58ee640d5f4ec
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-28 01:12:18 +0000

    Code to turn on obfuscated filesystems for all, and to migrate all existing users automatically whenever they next access the filesystem or check its quota usage.
    Avi, I've added you just for the ChildProcessSecurity stuff in worker_process_host.cc and browser_render_process_host.cc.  Drop me a line if you need to know what's going on there.  This is for M13, so please ask soon.
    
    BUG=none
    TEST=unit tests
    
    Review URL: http://codereview.chromium.org/6976017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87129 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_dir_url_request_job_unittest.cc
+++ webkit/fileapi/file_system_dir_url_request_job_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/file_path.h"
-#include "base/file_util.h"
 #include "base/format_macros.h"
 #include "base/message_loop.h"
 #include "base/platform_file.h"
@@ -29,7 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "webkit/fileapi/file_system_context.h"
+#include "webkit/fileapi/file_system_file_util.h"
+#include "webkit/fileapi/file_system_operation_context.h"
 #include "webkit/fileapi/file_system_path_manager.h"
+#include "webkit/fileapi/sandbox_mount_point_provider.h"
 
 namespace fileapi {
 namespace {
@@ -123,8 +125,20 @@ class FileSystemDirURLRequestJobTest : public testing::Test {
   }
 
   void CreateDirectory(const base::StringPiece dir_name) {
-    FilePath path = root_path_.AppendASCII(dir_name);
-    ASSERT_TRUE(file_util::CreateDirectory(path));
+    FilePath path = FilePath().AppendASCII(dir_name);
+    FileSystemFileUtil* file_util = file_system_context_->path_manager()->
+        sandbox_provider()->GetFileSystemFileUtil();
+    FileSystemOperationContext context(file_system_context_, file_util);
+    context.set_src_origin_url(GURL("http://remote"));
+    context.set_src_virtual_path(path);
+    context.set_src_type(fileapi::kFileSystemTypeTemporary);
+    context.set_allowed_bytes_growth(1024);
+
+    ASSERT_EQ(base::PLATFORM_FILE_OK, file_util->CreateDirectory(
+        &context,
+        path,
+        false /* exclusive */,
+        false /* recursive */));
   }
 
   GURL CreateFileSystemURL(const std::string path) {
--- webkit/fileapi/quota_file_util_unittest.cc
+++ webkit/fileapi/quota_file_util_unittest.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/file_system_test_helper.h"
 #include "webkit/fileapi/file_system_types.h"
 #include "webkit/fileapi/file_system_usage_cache.h"
-#include "webkit/fileapi/local_file_system_file_util.h"
+#include "webkit/fileapi/obfuscated_file_system_file_util.h"
 
 namespace fileapi {
 
@@ -30,16 +30,15 @@ class QuotaFileUtilTest : public testing::Test {
 
   void SetUp() {
     ASSERT_TRUE(data_dir_.CreateUniqueTempDir());
-    FilePath base_dir = data_dir_.path().AppendASCII("filesystem");
-
-    // For path translation we rely on LocalFileSystemFileUtil::GetLocalPath().
-    local_test_helper_.SetUp(base_dir, LocalFileSystemFileUtil::GetInstance());
-    quota_test_helper_.SetUp(base_dir, QuotaFileUtil::GetInstance());
+    quota_test_helper_.SetUp(data_dir_.path(), QuotaFileUtil::GetInstance());
+    obfuscated_test_helper_.SetUp(
+        quota_test_helper_.file_system_context(), NULL);
+    base_dir_ = obfuscated_test_helper_.GetOriginRootPath();
   }
 
   void TearDown() {
-    local_test_helper_.TearDown();
     quota_test_helper_.TearDown();
+    obfuscated_test_helper_.TearDown();
   }
 
  protected:
@@ -48,7 +47,7 @@ class QuotaFileUtilTest : public testing::Test {
   }
 
   FilePath Path(const std::string& file_name) {
-    return local_test_helper_.GetLocalPathFromASCII(file_name);
+    return base_dir_.AppendASCII(file_name);
   }
 
   base::PlatformFileError CreateFile(const char* file_name,
@@ -74,7 +73,8 @@ class QuotaFileUtilTest : public testing::Test {
 
  private:
   ScopedTempDir data_dir_;
-  FileSystemTestOriginHelper local_test_helper_;
+  FilePath base_dir_;
+  FileSystemTestOriginHelper obfuscated_test_helper_;
   FileSystemTestOriginHelper quota_test_helper_;
   base::ScopedCallbackFactory<QuotaFileUtilTest> callback_factory_;
 
