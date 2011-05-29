9f4f1d92ca4548baf2b7b30e370109691583f68e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-29 08:30:35 +0000

    2011-05-29  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r87566.
            http://trac.webkit.org/changeset/87566
            https://bugs.webkit.org/show_bug.cgi?id=61702
    
            It made all tests assert on Qt in debug mode (Requested by
            Ossy_weekend on #webkit).
    
            * WebCore.exp.in:
            * bindings/ScriptControllerBase.cpp:
            (WebCore::ScriptController::executeIfJavaScriptURL):
            * dom/Document.cpp:
            (WebCore::Document::Document):
            (WebCore::Document::suggestedMIMEType):
            (WebCore::Document::lastModified):
            (WebCore::Document::initSecurityContext):
            (WebCore::Document::updateURLForPushOrReplaceState):
            * dom/Document.h:
            (WebCore::Document::setDocumentLoader):
            (WebCore::Document::loader):
            * html/MediaDocument.cpp:
            (WebCore::MediaDocument::replaceMediaElementTimerFired):
            * html/PluginDocument.cpp:
            (WebCore::PluginDocumentParser::createDocumentStructure):
            * platform/mac/HTMLConverter.mm:
            (fileWrapperForElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -436,6 +436,7 @@ Document::Document(Frame* frame, const KURL& url, bool isXHTML, bool isHTML)
     m_ignoreAutofocus = false;
 
     m_frame = frame;
+    m_documentLoader = frame ? frame->loader()->activeDocumentLoader() : 0;
 
     // We depend on the url getting immediately set in subframes, but we
     // also depend on the url NOT getting immediately set in opened windows.
@@ -1106,9 +1107,7 @@ String Document::suggestedMIMEType() const
     if (m_document->isHTMLDocument())
         return "text/html";
 
-    if (DocumentLoader* documentLoader = loader())
-        return documentLoader->responseMIMEType();
-    return String();
+    return m_documentLoader->responseMIMEType();
 }
 
 // FIXME: We need to discuss the DOM API here at some point. Ideas:
@@ -3783,11 +3782,7 @@ String Document::lastModified() const
     DateComponents date;
     bool foundDate = false;
     if (m_frame) {
-        // Since we're still in a Frame, we should have a DocumentLoader.
-        ASSERT(loader());
-        String httpLastModified;
-        if (loader()) 
-            httpLastModified = loader()->response().httpHeaderField("Last-Modified");
+        String httpLastModified = m_documentLoader->response().httpHeaderField("Last-Modified");
         if (!httpLastModified.isEmpty()) {
             date.setMillisecondsSinceEpochForDateTime(parseDate(httpLastModified));
             foundDate = true;
@@ -4516,11 +4511,7 @@ void Document::initSecurityContext()
         // load local resources.  See https://bugs.webkit.org/show_bug.cgi?id=16756
         // and https://bugs.webkit.org/show_bug.cgi?id=19760 for further
         // discussion.
-        
-        DocumentLoader* documentLoader = loader();
-        // Since we're still in a Frame, we should have a DocumentLoader.
-        ASSERT(documentLoader);
-        if (documentLoader && documentLoader->substituteData().isValid())
+        if (m_documentLoader->substituteData().isValid())
             securityOrigin()->grantLoadLocalResources();
     }
 
@@ -4601,10 +4592,7 @@ void Document::updateURLForPushOrReplaceState(const KURL& url)
 
     setURL(url);
     f->loader()->setOutgoingReferrer(url);
-    // Since we're still in a frame, we should have a DocumentLoader.
-    ASSERT(loader());
-    if (DocumentLoader* documentLoader = loader())
-        documentLoader->replaceRequestURLForSameDocumentNavigation(url);
+    m_documentLoader->replaceRequestURLForSameDocumentNavigation(url);
 }
 
 void Document::statePopped(SerializedScriptValue* stateObject)
@@ -5153,21 +5141,6 @@ void Document::didRemoveWheelEventHandler()
     Frame* mainFrame = page() ? page()->mainFrame() : 0;
     if (mainFrame)
         mainFrame->notifyChromeClientWheelEventHandlerCountChanged();
-}
-
-DocumentLoader* Document::loader() const
-{
-    if (!m_frame)
-        return 0;
-    
-    DocumentLoader* loader = m_frame->loader()->activeDocumentLoader();
-    if (!loader)
-        return 0;
-    
-    if (m_frame->document() != this)
-        return 0;
-    
-    return loader;
 }
 
 } // namespace WebCore
