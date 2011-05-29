29f7cebb38212d7692543f48c1faffdab0f24148
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-29 20:12:01 +0000

    2011-05-29  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            Fix assertion seen when entering full screen mode for standalone video
            https://bugs.webkit.org/show_bug.cgi?id=61708
    
            We don't currently have a way to make a regression test for this.
    
            * dom/Document.cpp:
            (WebCore::Document::setAnimatingFullScreen): Call scheduleForcedStyleRecalc,
            because scheduleStyleRecalc is intended to only be called when the style
            system itself detects recalc is needed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -5055,7 +5055,7 @@ void Document::setAnimatingFullScreen(bool flag)
 
     if (m_fullScreenElement && m_fullScreenElement->isDescendantOf(this)) {
         m_fullScreenElement->setNeedsStyleRecalc();
-        scheduleStyleRecalc();
+        scheduleForcedStyleRecalc();
     }
 
 #if USE(ACCELERATED_COMPOSITING)
