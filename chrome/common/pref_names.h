540c2f609539b4c1110e0858033eb2abe87a11f5
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-29 22:50:16 +0000

    Add ability to change web store promo logo at runtime.
    This updates the web store promo system so that we can change the promo's logo to any URL from the promo server.
    
    BUG=84082
    TEST=ExtensionAppsPromo, PromoResourceServiceTest
    
    
    Review URL: http://codereview.chromium.org/7074032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -406,6 +406,7 @@ extern const char kNTPWebStorePromoId[];
 extern const char kNTPWebStorePromoHeader[];
 extern const char kNTPWebStorePromoButton[];
 extern const char kNTPWebStorePromoLink[];
+extern const char kNTPWebStorePromoLogo[];
 extern const char kNTPWebStorePromoExpire[];
 
 extern const char kGpuBlacklist[];
