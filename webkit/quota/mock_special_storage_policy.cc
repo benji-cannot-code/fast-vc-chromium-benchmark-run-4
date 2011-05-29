3e6515f2a1f2ed6a0a87a6bcfd8b06514ce87fea
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-29 21:38:11 +0000

    Make clang happy again.
    TBR=ericu
    BUG=none
    Review URL: http://codereview.chromium.org/7082033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87208 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/mock_special_storage_policy.cc
+++ webkit/quota/mock_special_storage_policy.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace quota {
 
+MockSpecialStoragePolicy::MockSpecialStoragePolicy() {}
+MockSpecialStoragePolicy::~MockSpecialStoragePolicy() {}
+
 bool MockSpecialStoragePolicy::IsStorageProtected(const GURL& origin) {
   return protected_.find(origin) != protected_.end();
 }
