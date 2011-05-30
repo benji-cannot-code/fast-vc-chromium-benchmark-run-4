16c0d0f67a9d5b47e2a0a0d5462dc1972f8703c0
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-30 13:01:19 +0000

    mac: turn download shelf link into a button
    xib changes:
    * Change button cell from HyperlinkButtonCell to DownloadShowAllCell
    * Disconnect |showAllCell_|
    * Change button text from IDS_SHOW_ALL_DOWNLOADS to IDS_SHOW_ALL
    * Make button higher
    * Give button class DownloadShowAllButton
    * Remove NSImageView as the image is now on the button
    * Reduce padding between "show all" button and close button by 2
    
    DownloadShowAllCell is a new class that looks like a button with an icon. Some common logic with the download items has been extracted.
    
    BUG=79502
    TEST=Download something, look at bottom right corner.
    With several downloads, the new button should interact with the download items in a sane way on window resize.
    It should look right with themes.
    
    Review URL: http://codereview.chromium.org/7084009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/download/download_shelf_controller.h
+++ chrome/browser/ui/cocoa/download/download_shelf_controller.h
@@ -39,10 +39,6 @@ class DownloadShelf;
 
 @interface DownloadShelfController : NSViewController<NSTextViewDelegate> {
  @private
-  IBOutlet HyperlinkButtonCell* showAllDownloadsCell_;
-
-  IBOutlet NSImageView* image_;
-
   IBOutlet HoverButton* hoverCloseButton_;
 
   BOOL barIsVisible_;
