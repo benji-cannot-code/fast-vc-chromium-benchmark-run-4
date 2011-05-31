39412475b36b9f5aa9f71eef56f4e79982b16eb7
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 01:44:55 +0000

    2011-05-30  Eric Carlson  <eric.carlson@apple.com>
            Reviewed by Alexey Proskuryakov.
    
            Audio and video files saved to the Application Cache should preserve the original file extension
            https://bugs.webkit.org/show_bug.cgi?id=61750
            <rdar://9524922>
    
            No new tests, it isn't possible to check the name of the file in the cache from within
            DRT. Changes verified manually.
    
            * loader/appcache/ApplicationCacheStorage.cpp:
            (WebCore::ApplicationCacheStorage::store): Append the original file extension to the cache
                file name.
            (WebCore::ApplicationCacheStorage::writeDataToUniqueFileInDirectory): Add extension parameter.
            * loader/appcache/ApplicationCacheStorage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87704 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/appcache/ApplicationCacheStorage.h
+++ third_party/WebKit/Source/WebCore/loader/appcache/ApplicationCacheStorage.h
@@ -116,7 +116,7 @@ private:
     bool ensureOriginRecord(const SecurityOrigin*);
     bool shouldStoreResourceAsFlatFile(ApplicationCacheResource*);
     void deleteTables();
-    bool writeDataToUniqueFileInDirectory(SharedBuffer*, const String& directory, String& outFilename);
+    bool writeDataToUniqueFileInDirectory(SharedBuffer*, const String& directory, String& outFilename, const String& fileExtension);
 
     void loadManifestHostHashes();
     
