22b6a33ab502aeaecaaf6692f9ef731e8d62fdeb
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:10:39 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for strokeText()
            https://bugs.webkit.org/show_bug.cgi?id=61626
    
            Move CanvasRenderingContext2D.strokeText() to auto-generated JSC bindings.
            Make it [RequiresAllArguments=Raise] to match the old behavior.
            This has the side-effect of aligning the behaviors of JSC and V8.
    
            Test: fast/canvas/canvas-overloads-strokeText.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for strokeText()
            https://bugs.webkit.org/show_bug.cgi?id=61626
    
            Add a test to verify the behavior of strokeText() when called with different
            numbers of arguments. There are two differences to the previous behavior:
    
            - SyntaxError exceptions are now raised with the message "Not enough arguments."
            - Calling strokeText() with superfluous arguments now lets the call through
              instead of raising a SyntaxError. This matches both Gecko and Presto.
    
            * fast/canvas/canvas-overloads-strokeText-expected.txt: Added.
            * fast/canvas/canvas-overloads-strokeText.html: Added.
            * fast/canvas/script-tests/canvas-overloads-strokeText.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-strokeText.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.strokeText() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var SyntaxError = "SyntaxError: Not enough arguments";
+
+shouldThrow("ctx.strokeText()", "SyntaxError");
+shouldThrow("ctx.strokeText('moo')", "SyntaxError");
+shouldThrow("ctx.strokeText('moo',0)", "SyntaxError");
+shouldBe("ctx.strokeText('moo',0,0)", "undefined");
+shouldBe("ctx.strokeText('moo',0,0,0)", "undefined");
+shouldBe("ctx.strokeText('moo',0,0,0,0)", "undefined");
+
+var successfullyParsed = true;
