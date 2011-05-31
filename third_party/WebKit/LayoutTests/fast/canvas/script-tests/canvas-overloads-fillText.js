9bddd1a9ab066843d9bf4366e4b861108422b639
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:08:49 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for fillText()
            https://bugs.webkit.org/show_bug.cgi?id=61623
    
            Add a test to verify the behavior of fillText() when called with different
            numbers of arguments. There are two differences to the previous behavior:
    
            - SyntaxError exceptions are now raised with the message "Not enough arguments."
            - Calling fillText() with superfluous arguments now lets the call through
              instead of raising a SyntaxError. This matches both Gecko and Presto.
    
            * fast/canvas/canvas-overloads-fillText-expected.txt: Added.
            * fast/canvas/canvas-overloads-fillText.html: Added.
            * fast/canvas/script-tests/canvas-overloads-fillText.js: Added.
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for fillText()
            https://bugs.webkit.org/show_bug.cgi?id=61623
    
            Move CanvasRenderingContext2D.fillText() to auto-generated JSC bindings.
            Make it [RequiresAllArguments=Raise] to match the old behavior.
            This has the side-effect of aligning the behaviors of JSC and V8.
    
            Test: fast/canvas/canvas-overloads-fillText.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-fillText.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.fillText() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var SyntaxError = "SyntaxError: Not enough arguments";
+var TypeError = "TypeError: Type error";
+
+shouldThrow("ctx.fillText()", "SyntaxError");
+shouldThrow("ctx.fillText('moo')", "SyntaxError");
+shouldThrow("ctx.fillText('moo',0)", "SyntaxError");
+shouldBe("ctx.fillText('moo',0,0)", "undefined");
+shouldBe("ctx.fillText('moo',0,0,0)", "undefined");
+shouldBe("ctx.fillText('moo',0,0,0,0)", "undefined");
+
+var successfullyParsed = true;
