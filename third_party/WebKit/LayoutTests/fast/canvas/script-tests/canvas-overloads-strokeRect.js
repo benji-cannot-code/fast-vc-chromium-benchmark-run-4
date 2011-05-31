49a8535d5da4bc78dc403b51da3925dd2580979a
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:20:32 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for strokeRect()
            https://bugs.webkit.org/show_bug.cgi?id=61641
    
            Add a test to verify the behavior of strokeRect() when called with
            different numbers of arguments.
    
            * fast/canvas/canvas-overloads-strokeRect-expected.txt: Added.
            * fast/canvas/canvas-overloads-strokeRect.html: Added.
            * fast/canvas/script-tests/canvas-overloads-strokeRect.js: Added.
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for strokeRect()
            https://bugs.webkit.org/show_bug.cgi?id=61641
    
            Move CanvasRenderingContext2D.setStrokeColor() to auto-generated JSC bindings.
    
            No behavioral change.
    
            Test: fast/canvas/canvas-overloads-strokeRect.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-strokeRect.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.strokeRect() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var SyntaxError = "SyntaxError: Syntax error";
+var TypeError = "TypeError: Type error";
+
+shouldBe("ctx.strokeRect()", "undefined");
+shouldBe("ctx.strokeRect(0)", "undefined");
+shouldBe("ctx.strokeRect(0, 0)", "undefined");
+shouldBe("ctx.strokeRect(0, 0, 0)", "undefined");
+shouldBe("ctx.strokeRect(0, 0, 0, 0)", "undefined");
+shouldBe("ctx.strokeRect(0, 0, 0, 0, 0)", "undefined");
+
+var successfullyParsed = true;
