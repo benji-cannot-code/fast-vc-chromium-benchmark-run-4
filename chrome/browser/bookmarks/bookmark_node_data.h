13a123ef4be1d3eb66c9e79aedddd127d53623c6
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-31 18:04:33 +0000

    FilePath: remove some functions that aren't really used.
    And fix the one caller.
    
    Review URL: http://codereview.chromium.org/7087009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87327 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_node_data.h
+++ chrome/browser/bookmarks/bookmark_node_data.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -161,7 +161,7 @@ struct BookmarkNodeData {
 
  private:
   // Path of the profile we originated from.
-  FilePath::StringType profile_path_;
+  FilePath profile_path_;
 };
 
 #endif  // CHROME_BROWSER_BOOKMARKS_BOOKMARK_NODE_DATA_H_
