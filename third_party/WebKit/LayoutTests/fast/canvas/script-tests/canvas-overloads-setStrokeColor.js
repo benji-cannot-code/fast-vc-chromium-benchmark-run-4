6f61eaac68ec9c4a365f113775f18f99e7694c94
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:12:35 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setStrokeColor()
            https://bugs.webkit.org/show_bug.cgi?id=61629
    
            Add a test to verify the behavior of setStrokeColor() when called with
            different numbers of arguments.
    
            * fast/canvas/canvas-overloads-setStrokeColor-expected.txt: Added.
            * fast/canvas/canvas-overloads-setStrokeColor.html: Added.
            * fast/canvas/script-tests/canvas-overloads-setStrokeColor.js: Added.
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setStrokeColor()
            https://bugs.webkit.org/show_bug.cgi?id=61629
    
            Move CanvasRenderingContext2D.setStrokeColor() to auto-generated JSC bindings.
    
            There is a subtle difference to the previous behavior: invalid numbers of
            arguments now raise TypeError instead of SyntaxError. This is in accordance
            with Web IDL, but doesn't matter much anyway since this method is WebKit-only.
    
            Test: fast/canvas/canvas-overloads-setStrokeColor.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87731 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-setStrokeColor.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.setStrokeColor() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var TypeError = "TypeError: Type error";
+
+shouldThrow("ctx.setStrokeColor()", "TypeError");
+shouldBe("ctx.setStrokeColor('red')", "undefined");
+shouldBe("ctx.setStrokeColor(0)", "undefined");
+shouldBe("ctx.setStrokeColor(0, 0)", "undefined");
+shouldThrow("ctx.setStrokeColor(0, 0, 0)", "TypeError");
+shouldBe("ctx.setStrokeColor(0, 0, 0, 0)", "undefined");
+shouldBe("ctx.setStrokeColor(0, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.setStrokeColor(0, 0, 0, 0, 0, 0)", "TypeError");
+
+var successfullyParsed = true;
