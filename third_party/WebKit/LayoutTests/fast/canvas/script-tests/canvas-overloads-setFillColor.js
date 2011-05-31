d8e4f8cea74592679e87c6d38105eb6b7f91eb73
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:15:27 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setFillColor()
            https://bugs.webkit.org/show_bug.cgi?id=61635
    
            Move CanvasRenderingContext2D.setStrokeColor() to auto-generated JSC bindings.
    
            There is a subtle difference to the previous behavior: invalid numbers of
            arguments now raise TypeError instead of SyntaxError. This is in accordance
            with Web IDL, but doesn't matter much anyway since this method is WebKit-only.
    
            Test: fast/canvas/canvas-overloads-setFillColor.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setFillColor()
            https://bugs.webkit.org/show_bug.cgi?id=61635
    
            Add a test to verify the behavior of setFillColor() when called with
            different numbers of arguments.
    
            * fast/canvas/canvas-overloads-setFillColor-expected.txt: Added.
            * fast/canvas/canvas-overloads-setFillColor.html: Added.
            * fast/canvas/script-tests/canvas-overloads-setFillColor.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-setFillColor.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.setFillColor() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var TypeError = "TypeError: Type error";
+
+shouldThrow("ctx.setFillColor()", "TypeError");
+shouldBe("ctx.setFillColor('red')", "undefined");
+shouldBe("ctx.setFillColor(0)", "undefined");
+shouldBe("ctx.setFillColor(0, 0)", "undefined");
+shouldThrow("ctx.setFillColor(0, 0, 0)", "TypeError");
+shouldBe("ctx.setFillColor(0, 0, 0, 0)", "undefined");
+shouldBe("ctx.setFillColor(0, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.setFillColor(0, 0, 0, 0, 0, 0)", "TypeError");
+
+var successfullyParsed = true;
