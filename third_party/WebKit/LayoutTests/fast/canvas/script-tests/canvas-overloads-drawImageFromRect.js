72d7eca14fc9eca5e55871dd56bb7f497ea14cd0
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:25:34 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for drawImageFromRect()
            https://bugs.webkit.org/show_bug.cgi?id=61709
    
            Add a test to verify the behavior of drawImageFromRect() when called
            with different numbers of arguments.
    
            * fast/canvas/canvas-overloads-drawImageFromRect-expected.txt: Added.
            * fast/canvas/canvas-overloads-drawImageFromRect.html: Added.
            * fast/canvas/script-tests/canvas-overloads-drawImageFromRect.js: Added.
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for drawImageFromRect()
            https://bugs.webkit.org/show_bug.cgi?id=61709
    
            Move CanvasRenderingContext2D.drawImageFromRect() to auto-generated JSC bindings.
            Make it [RequiresAllArguments=Raise] to match the old behavior.
            Side effect: calling drawImageFromRect() with zero arguments will now raise
            SyntaxError instead of TypeError.
    
            Test: fast/canvas/canvas-overloads-drawImageFromRect.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.h:
            * html/canvas/CanvasRenderingContext2D.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-drawImageFromRect.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.drawImageFromRect() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var SyntaxError = "SyntaxError: Not enough arguments";
+
+var imageElement = document.createElement("img");
+shouldThrow("ctx.drawImageFromRect()", "SyntaxError");
+shouldBe("ctx.drawImageFromRect(imageElement)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.drawImageFromRect(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+
+var successfullyParsed = true;
