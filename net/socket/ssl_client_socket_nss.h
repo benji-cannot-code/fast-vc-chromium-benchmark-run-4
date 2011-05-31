3c30e4f0e1de14fe5c2f2f5b17d513d192afaf44
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-31 21:43:40 +0000

    Revert 86966 - Add back the state that waits for the ssl_host_info_ data to be ready.
    This state ensures that ssl_host_info_->state().certs has been
    initialized.  This helps the DoHandshake and DoVerifyCert states, and
    will be required for the TLS cached information extension.
    
    R=agl@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/6992042
    
    Reason for revert: it causes a crash in HttpCache::GetBackend.
    
    TBR=wtc@chromium.org
    BUG=84426
    Review URL: http://codereview.chromium.org/6975052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87372 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.h
+++ net/socket/ssl_client_socket_nss.h
@@ -87,7 +87,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
  private:
   enum State {
     STATE_NONE,
-    STATE_LOAD_SSL_HOST_INFO,
     STATE_HANDSHAKE,
     STATE_VERIFY_DNSSEC,
     STATE_VERIFY_DNSSEC_COMPLETE,
@@ -116,7 +115,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
   int DoReadLoop(int result);
   int DoWriteLoop(int result);
 
-  int DoLoadSSLHostInfo();
   int DoHandshake();
 
   int DoVerifyDNSSEC(int result);
