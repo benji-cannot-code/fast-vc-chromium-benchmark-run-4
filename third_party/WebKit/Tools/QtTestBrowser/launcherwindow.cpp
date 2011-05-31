29287f08c9651fa6962dc8d77747fe43a395a832
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 13:32:46 +0000

    [Qt] Implement find feature for QtTestBrowser https://bugs.webkit.org/show_bug.cgi?id=61425
    Patch by Kristóf Kosztyó <Kosztyo.Kristof@stud.u-szeged.hu> on 2011-05-31
    Reviewed by Csaba Osztrogonác.
    
    Buildfix for --minimal and CONFIG+=qt_minimal build.
    
    * QtTestBrowser/launcherwindow.cpp:
    (LauncherWindow::LauncherWindow):
    (LauncherWindow::createChrome):
    * QtTestBrowser/launcherwindow.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/QtTestBrowser/launcherwindow.cpp
+++ third_party/WebKit/Tools/QtTestBrowser/launcherwindow.cpp
@@ -47,7 +47,9 @@ LauncherWindow::LauncherWindow(WindowOptions* data, QGraphicsScene* sharedScene)
     , m_inspector(0)
     , m_formatMenuAction(0)
     , m_zoomAnimation(0)
+#ifndef QT_NO_LINEEDIT
     , m_findFlag(0)
+#endif
 {
     if (data)
         m_windowOptions = *data;
@@ -199,8 +201,10 @@ void LauncherWindow::createChrome()
     editMenu->addAction(page()->action(QWebPage::Copy));
     editMenu->addAction(page()->action(QWebPage::Paste));
     editMenu->addSeparator();
+#ifndef QT_NO_LINEEDIT
     editMenu->addAction("&Find", this, SLOT(showFindBar()), QKeySequence(Qt::CTRL | Qt::Key_F));
     editMenu->addSeparator();
+#endif
     QAction* setEditable = editMenu->addAction("Set Editable", this, SLOT(setEditable(bool)));
     setEditable->setCheckable(true);
 
@@ -411,7 +415,7 @@ void LauncherWindow::createChrome()
     QAction* toggleJavascriptCanOpenWindows = settingsMenu->addAction("Enable js popup windows", this, SLOT(toggleJavascriptCanOpenWindows(bool)));
     toggleJavascriptCanOpenWindows->setCheckable(true);
     toggleJavascriptCanOpenWindows->setChecked(false);
-
+#ifndef QT_NO_LINEEDIT
     m_findBar = new QToolBar("Find", this);
     addToolBar(Qt::BottomToolBarArea, m_findBar);
 
@@ -454,6 +458,7 @@ void LauncherWindow::createChrome()
     m_findBar->addWidget(findHighLightAll);
     m_findBar->setMovable(false);
     m_findBar->setVisible(false);
+#endif
 #endif
 }
 
@@ -1023,6 +1028,7 @@ LauncherWindow* LauncherWindow::cloneWindow()
     return mw;
 }
 
+#ifndef QT_NO_LINEEDIT
 void LauncherWindow::showFindBar()
 {
     if (!m_findBar->isVisible()) {
@@ -1058,3 +1064,4 @@ void LauncherWindow::find(int mode = s_findNormalFlag)
     if (m_findFlag & QWebPage::HighlightAllOccurrences)
         page()->findText(m_lineEdit->text(), QFlag(m_findFlag));
 }
+#endif
