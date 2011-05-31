ad720cf1a7b2f2afc5807f71d2b7984413206945
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-31 08:22:13 +0000

    Always enable quota storage API
    BUG=61676
    TEST=QuotaManagerTest.*
    
    Review URL: http://codereview.chromium.org/7083020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87283 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread.cc
+++ content/renderer/render_thread.cc
@@ -604,8 +604,7 @@ void RenderThread::EnsureWebKitInitialized() {
   WebRuntimeFeatures::enableJavaScriptI18NAPI(
       !command_line.HasSwitch(switches::kDisableJavaScriptI18NAPI));
 
-  WebRuntimeFeatures::enableQuota(
-      command_line.HasSwitch(switches::kEnableQuota));
+  WebRuntimeFeatures::enableQuota(true);
 
   FOR_EACH_OBSERVER(RenderProcessObserver, observers_, WebKitInitialized());
 }
