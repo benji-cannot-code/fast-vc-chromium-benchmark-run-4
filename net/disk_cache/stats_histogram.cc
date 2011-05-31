5a0fadedc2294ffc31d96c854dd9d9624b7da823
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-31 07:37:09 +0000

    Introduce the ANNOTATE_LEAKING_OBJECT_PTR annotation that can be used to mark heap allocated objects as intentionally leaked ones. Annotate the histograms produced by {Histogram,BooleanHistogram,LinearHistogram,CustomHistogram}::FactoryGet(), as leaked.
    Rename StatsHistogram::StatsHistogramFactoryGet to StatsHistogram::FactoryGet, annotate the result as leaky, update the suppressions.
    
    BUG=79322
    Review URL: http://codereview.chromium.org/7071036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87278 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/stats_histogram.cc
+++ net/disk_cache/stats_histogram.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/disk_cache/stats_histogram.h"
 
+#include "base/debug/leak_annotations.h"
 #include "base/logging.h"
 #include "net/disk_cache/stats.h"
 
@@ -22,8 +23,7 @@ StatsHistogram::~StatsHistogram() {
     stats_ = NULL;
 }
 
-StatsHistogram* StatsHistogram::StatsHistogramFactoryGet(
-    const std::string& name) {
+StatsHistogram* StatsHistogram::FactoryGet(const std::string& name) {
   Histogram* histogram(NULL);
 
   Sample minimum = 1;
