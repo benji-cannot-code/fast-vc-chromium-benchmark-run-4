a2867f7dc5d404c65c7a7e7d207d94655dff3c92
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-05-31 16:28:33 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setShadow()
            https://bugs.webkit.org/show_bug.cgi?id=61786
    
            Move CanvasRenderingContext2D.setShadow() to auto-generated JSC bindings.
    
            There is a subtle difference to the previous behavior: invalid numbers of
            arguments now raise TypeError instead of SyntaxError. This is in accordance
            with Web IDL, and matches the existing V8 bindings.
    
            Test: fast/canvas/canvas-overloads-setShadow.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for setShadow()
            https://bugs.webkit.org/show_bug.cgi?id=61786
    
            Add a test to verify the behavior of setShadow() when called
            with different numbers of arguments.
    
            * fast/canvas/canvas-overloads-setShadow-expected.txt: Added.
            * fast/canvas/canvas-overloads-setShadow.html: Added.
            * fast/canvas/script-tests/canvas-overloads-setShadow.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87736 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-overloads-setShadow.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test the behavior of CanvasRenderingContext2D.setShadow() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var TypeError = "TypeError: Type error";
+
+shouldThrow("ctx.setShadow()", "TypeError");
+shouldThrow("ctx.setShadow(0)", "TypeError");
+shouldThrow("ctx.setShadow(0, 0)", "TypeError");
+shouldBe("ctx.setShadow(0, 0, 0)", "undefined");
+shouldBe("ctx.setShadow(0, 0, 0, 0)", "undefined");
+shouldBe("ctx.setShadow(0, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.setShadow(0, 0, 0, 0, 0, 0)", "TypeError");
+shouldBe("ctx.setShadow(0, 0, 0, 0, 'red')", "undefined");
+shouldThrow("ctx.setShadow(0, 0, 0, 0, 'red', 0)", "TypeError");
+shouldBe("ctx.setShadow(0, 0, 0, 0, 'red', 0, 0)", "undefined");
+shouldThrow("ctx.setShadow(0, 0, 0, 0, 0, 0)", "TypeError");
+shouldBe("ctx.setShadow(0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldBe("ctx.setShadow(0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.setShadow(0, 0, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+
+var successfullyParsed = true;
