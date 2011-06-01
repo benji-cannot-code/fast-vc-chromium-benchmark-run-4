7f543d235840b150bf41ae83ebc22a43e840a8e0
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 02:42:27 +0000

    Initial IndexedDBQuotaClient implementation
    * Heavily based off of DatabaseQuotaClient.
    * Only responds to one request so far, GetOriginUsage.
    
    BUG=83652
    TEST=llvm/Debug/unit_tests --gtest_filter=IndexedDB*
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=87076
    
    Review URL: http://codereview.chromium.org/7053030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87424 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_context.h
+++ content/browser/in_process_webkit/indexed_db_context.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "content/browser/browser_thread.h"
 
 class GURL;
 class FilePath;
@@ -20,14 +21,24 @@ namespace WebKit {
 class WebIDBFactory;
 }
 
+namespace base {
+class MessageLoopProxy;
+}
+
 namespace quota {
+class QuotaManagerProxy;
 class SpecialStoragePolicy;
 }
 
-class IndexedDBContext {
+class IndexedDBContext :
+    public base::RefCountedThreadSafe<IndexedDBContext,
+                                      BrowserThread::DeleteOnWebKitThread> {
  public:
   IndexedDBContext(WebKitContext* webkit_context,
-                   quota::SpecialStoragePolicy* special_storage_policy);
+                   quota::SpecialStoragePolicy* special_storage_policy,
+                   quota::QuotaManagerProxy* quota_manager_proxy,
+                   base::MessageLoopProxy* webkit_thread_loop);
+
   ~IndexedDBContext();
 
   WebKit::WebIDBFactory* GetIDBFactory();
@@ -69,6 +80,8 @@ class IndexedDBContext {
   bool clear_local_state_on_exit_;
 
   scoped_refptr<quota::SpecialStoragePolicy> special_storage_policy_;
+
+  scoped_refptr<quota::QuotaManagerProxy> quota_manager_proxy_;
 
   DISALLOW_COPY_AND_ASSIGN(IndexedDBContext);
 };
