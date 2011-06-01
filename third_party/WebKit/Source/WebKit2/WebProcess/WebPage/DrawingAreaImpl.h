b059b3788a4cfecf7544c0cf653707bcbd41e8d7
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-01 18:41:23 +0000

    Delete a bunch of dead code in DrawingAreaImpl
    LayerTreeHost::participatesInDisplay always returns false now, which means a bunch of code
    in DrawingAreaImpl no longer runs.
    
    Cleanup after fixing <http://webkit.org/b/58054>
    <rdar://problem/9249839> REGRESSION (WebKit2): Accelerated CSS animations have a lower
    framerate than in WebKit1
    
    Reviewed by Anders Carlsson.
    
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit::DrawingAreaImpl::forceRepaint):
    (WebKit::DrawingAreaImpl::layerHostDidFlushLayers):
    (WebKit::DrawingAreaImpl::sendDidUpdateBackingStoreState):
    (WebKit::DrawingAreaImpl::didUpdate):
    (WebKit::DrawingAreaImpl::enterAcceleratedCompositingMode):
    (WebKit::DrawingAreaImpl::exitAcceleratedCompositingMode):
    (WebKit::DrawingAreaImpl::scheduleDisplay):
    (WebKit::DrawingAreaImpl::displayTimerFired):
    (WebKit::DrawingAreaImpl::display):
    * WebProcess/WebPage/DrawingAreaImpl.h:
    * WebProcess/WebPage/LayerTreeHost.h:
    Reverted r82959.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.h
@@ -41,7 +41,6 @@ public:
     static PassOwnPtr<DrawingAreaImpl> create(WebPage*, const WebPageCreationParameters&);
     virtual ~DrawingAreaImpl();
 
-    void setLayerHostNeedsDisplay();
     void layerHostDidFlushLayers();
 
 private:
