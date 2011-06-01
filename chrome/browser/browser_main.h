92ab5bd868686e1f52f668c136b044e543e72079
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 20:16:23 +0000

    Remove the SSL FalseStart field trial, since we've finished studying it.
    SSL Handshake times (windows 11.0.696.71):
    %-tile    FalseStart  No-FalseStart   Delta
    50         65.4           91.9       -28.86%
    75        118.3          170.7       -30.69%
    90        174.7          266.8       -30.36%
    99        310.8          454.5       -27.21%
    Review URL: http://codereview.chromium.org/7050041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.h
+++ chrome/browser/browser_main.h
@@ -130,9 +130,6 @@ class BrowserMainParts {
   // specified timeout value is reached.
   void ConnectBackupJobsFieldTrial();
 
-  // A/B test for SSL False Start.
-  void SSLFalseStartFieldTrial();
-
   // Used to initialize NSPR where appropriate.
   virtual void InitializeSSL() = 0;
 
