d08d9cec6d39eba69ab08053699c3b331749ef4b
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 21:15:19 +0000

    Adds URLRequestJob bytes read to NetLog.
    Also no longer include raw bytes for 0-length transfers.
    
    BUG=81003
    TEST=none
    
    Review URL: http://codereview.chromium.org/7084007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87528 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/stream_socket.cc
+++ net/socket/stream_socket.cc
@@ -12,42 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
-namespace {
-
-// Parameters for SOCKET_BYTES_RECEIVED and SOCKET_BYTES_SENT events.
-// Includes bytes transferred and, if |bytes| is not NULL, the bytes themselves.
-class NetLogBytesTransferredParameter : public NetLog::EventParameters {
- public:
-  NetLogBytesTransferredParameter(int byte_count, const char* bytes);
-
-  virtual Value* ToValue() const;
-
- private:
-  const int byte_count_;
-  std::string hex_encoded_bytes_;
-  bool has_bytes_;
-};
-
-NetLogBytesTransferredParameter::NetLogBytesTransferredParameter(
-    int byte_count, const char* transferred_bytes)
-    : byte_count_(byte_count),
-      has_bytes_(false) {
-  if (transferred_bytes) {
-    hex_encoded_bytes_ = base::HexEncode(transferred_bytes, byte_count);
-    has_bytes_ = true;
-  }
-}
-
-Value* NetLogBytesTransferredParameter::ToValue() const {
-  DictionaryValue* dict = new DictionaryValue();
-  dict->SetInteger("byte_count", byte_count_);
-  if (has_bytes_)
-    dict->SetString("hex_encoded_bytes", hex_encoded_bytes_);
-  return dict;
-}
-
-}  // namespace
-
 StreamSocket::UseHistory::UseHistory()
     : was_ever_connected_(false),
       was_used_to_convey_data_(false),
@@ -142,19 +106,6 @@ void StreamSocket::UseHistory::EmitPreconnectionHistograms() const {
                                    "ConnnectBackupJobs"),
         result, 9);
   }
-}
-
-void StreamSocket::LogByteTransfer(const BoundNetLog& net_log,
-                                   NetLog::EventType event_type,
-                                   int byte_count,
-                                   char* bytes) const {
-  scoped_refptr<NetLog::EventParameters> params;
-  if (net_log.IsLoggingBytes()) {
-    params = new NetLogBytesTransferredParameter(byte_count, bytes);
-  } else {
-    params = new NetLogBytesTransferredParameter(byte_count, NULL);
-  }
-  net_log.AddEvent(event_type, params);
 }
 
 }  // namespace net
