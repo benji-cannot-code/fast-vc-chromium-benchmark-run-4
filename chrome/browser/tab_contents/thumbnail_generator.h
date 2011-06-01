e9b5328cdf72f4528a195bb865e75ff2b90ae88b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 08:21:21 +0000

    Fix invalid memory access caused by ThumbnailGenerator.
    The invalid access occurred as TabContentsObserver::Registrar
    outlived TabContents, and the registrar touched a pointer to a
    deleted TabContents in the destructor.
    
    BUG=84584
    TEST=confirm the valgrind error is gone by sh tools/valgrind/chrome_tests.sh -b ninja -t ui --gtest_filter=AutomationProxyTest.GetTab
    
    Review URL: http://codereview.chromium.org/7104021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87445 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/thumbnail_generator.h
+++ chrome/browser/tab_contents/thumbnail_generator.h
@@ -127,6 +127,7 @@ class ThumbnailGenerator : public NotificationObserver,
   // TabContentsObserver overrides.
   virtual void DidStartLoading();
   virtual void StopNavigation();
+  virtual void TabContentsDestroyed(TabContents* tab);
 
  private:
   virtual void WidgetDidReceivePaintAtSizeAck(
