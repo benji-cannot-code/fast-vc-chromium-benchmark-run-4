4622da317a7687371b9529c61b4fe15e4f0238ac
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-01 22:57:33 +0000

    2011-06-01  Jochen Eisinger  <jochen@chromium.org>
            Reviewed by Adam Barth.
    
            Add a test for FrameLoaderClient::allowImages
            https://bugs.webkit.org/show_bug.cgi?id=34314
    
            * platform/chromium/permissionclient/image-permissions-expected.txt: Added.
            * platform/chromium/permissionclient/image-permissions.html: Added.
            * platform/chromium/permissionclient/resources/boston.gif: Added.
            * platform/chromium/permissionclient/resources/image.html: Added.
    2011-06-01  Jochen Eisinger  <jochen@chromium.org>
    
            Reviewed by Adam Barth.
    
            Implement layoutTestController.setImagesAllowed for controlling the respective behavior of the WebPermissionClient
            https://bugs.webkit.org/show_bug.cgi?id=34314
    
            * DumpRenderTree/DumpRenderTree.gypi:
            * DumpRenderTree/chromium/LayoutTestController.cpp:
            (LayoutTestController::LayoutTestController):
            (LayoutTestController::setImagesAllowed):
            (LayoutTestController::setStorageAllowed):
            * DumpRenderTree/chromium/LayoutTestController.h:
            * DumpRenderTree/chromium/WebPermissions.cpp: Copied from Tools/DumpRenderTree/chromium/WebPermissions.h.
            (WebPermissions::WebPermissions):
            (WebPermissions::~WebPermissions):
            (WebPermissions::allowImages):
            (WebPermissions::allowStorage):
            (WebPermissions::setImagesAllowed):
            (WebPermissions::setStorageAllowed):
            (WebPermissions::reset):
            * DumpRenderTree/chromium/WebPermissions.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gypi
+++ third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.gypi
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'chromium/TestShellWin.cpp',
             'chromium/TextInputController.cpp',
             'chromium/TextInputController.h',
+            'chromium/WebPermissions.cpp',
+            'chromium/WebPermissions.h',
             'chromium/WebPreferences.cpp',
             'chromium/WebPreferences.h',
             'chromium/WebViewHost.cpp',
--- third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -192,6 +192,8 @@ LayoutTestController::LayoutTestController(TestShell* shell)
     bindMethod("waitUntilDone", &LayoutTestController::waitUntilDone);
     bindMethod("windowCount", &LayoutTestController::windowCount);
     bindMethod("setTextDirection", &LayoutTestController::setTextDirection);
+    bindMethod("setImagesAllowed", &LayoutTestController::setImagesAllowed);
+    bindMethod("setStorageAllowed", &LayoutTestController::setStorageAllowed);
 
     // The following are stubs.
     bindMethod("abortModal", &LayoutTestController::abortModal);
@@ -217,7 +219,6 @@ LayoutTestController::LayoutTestController(TestShell* shell)
     bindMethod("deleteLocalStorageForOrigin", &LayoutTestController::deleteLocalStorageForOrigin);
     bindMethod("observeStorageTrackerNotifications", &LayoutTestController::observeStorageTrackerNotifications);
     bindMethod("syncLocalStorage", &LayoutTestController::syncLocalStorage);
-    bindMethod("setStorageAllowed", &LayoutTestController::setStorageAllowed);
     
     // The fallback method is called when an unknown method is invoked.
     bindFallbackMethod(&LayoutTestController::fallbackMethod);
@@ -819,6 +820,20 @@ void LayoutTestController::setPopupBlockingEnabled(const CppArgumentList& argume
         m_shell->preferences()->javaScriptCanOpenWindowsAutomatically = !blockPopups;
         m_shell->applyPreferences();
     }
+    result->setNull();
+}
+
+void LayoutTestController::setImagesAllowed(const CppArgumentList& arguments, CppVariant* result)
+{
+    if (arguments.size() > 0 && arguments[0].isBool())
+        m_shell->webPermissions()->setImagesAllowed(arguments[0].toBoolean());
+    result->setNull();
+}
+
+void LayoutTestController::setStorageAllowed(const CppArgumentList& arguments, CppVariant* result)
+{
+    if (arguments.size() > 0 && arguments[0].isBool())
+        m_shell->webPermissions()->setStorageAllowed(arguments[0].toBoolean());
     result->setNull();
 }
 
@@ -1822,13 +1837,6 @@ void LayoutTestController::observeStorageTrackerNotifications(const CppArgumentL
 void LayoutTestController::syncLocalStorage(const CppArgumentList&, CppVariant*)
 {
     // Not Implemented
-}
-
-void LayoutTestController::setStorageAllowed(const CppArgumentList& arguments, CppVariant* result)
-{
-    if (arguments.size() > 0 && arguments[0].isBool())
-        m_shell->webPermissions()->setStorageAllowed(arguments[0].toBoolean());
-    result->setNull();
 }
 
 void LayoutTestController::setPluginsEnabled(const CppArgumentList& arguments, CppVariant* result)
