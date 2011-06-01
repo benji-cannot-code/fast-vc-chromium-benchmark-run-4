4fb0a23b60d363ad7c7fb79761145d75e1404a43
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 04:57:44 +0000

    Enable in-browser thumbnailing for Linux and Chrome OS.
    BUG=65936
    TEST=confirm that thumbnails in the new tab page are updated properly
    
    Review URL: http://codereview.chromium.org/7080035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1182,6 +1182,16 @@ bool IsPrintPreviewEnabled() {
 }
 #endif
 
+bool IsInBrowserThumbnailingEnabled() {
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+  // The in-browser-thumbnailing is enabled for Linux and Chrome OS.
+  return true;
+#else
+  return CommandLine::ForCurrentProcess()->HasSwitch(
+      kEnableInBrowserThumbnailing);
+#endif
+}
+
 // -----------------------------------------------------------------------------
 // DO NOT ADD YOUR CRAP TO THE BOTTOM OF THIS FILE.
 //
