c7e5ccbcea3f25c4b2ac9fd229783d76dd11e319
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-01 19:52:13 +0000

    2011-06-01  Kenneth Russell  <kbr@google.com>
            Reviewed by Nate Chapin.
    
            [V8] Optimize fetches of indexed properties in custom bindings
            https://bugs.webkit.org/show_bug.cgi?id=61821
    
            Avoid allocating garbage in affected custom bindings. This speeds
            up one test case by at least a factor of two. No new tests;
            covered by existing layout tests (typed array and otherwise).
    
            * bindings/v8/custom/V8ArrayBufferViewCustom.h:
            (WebCore::constructWebGLArray):
            (WebCore::setWebGLArrayHelper):
            * bindings/v8/custom/V8InspectorFrontendHostCustom.cpp:
            (WebCore::V8InspectorFrontendHost::showContextMenuCallback):
            * bindings/v8/custom/V8MessagePortCustom.cpp:
            (WebCore::getMessagePortArray):
            * bindings/v8/custom/V8WebGLRenderingContextCustom.cpp:
            (WebCore::jsArrayToFloatArray):
            (WebCore::jsArrayToIntArray):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
@@ -78,7 +78,7 @@ static float* jsArrayToFloatArray(v8::Handle<v8::Array> array, uint32_t len)
     if (!tryFastMalloc(len * sizeof(float)).getValue(data))
         return 0;
     for (uint32_t i = 0; i < len; i++) {
-        v8::Local<v8::Value> val = array->Get(v8::Integer::New(i));
+        v8::Local<v8::Value> val = array->Get(i);
         if (!val->IsNumber()) {
             fastFree(data);
             return 0;
@@ -97,7 +97,7 @@ static int* jsArrayToIntArray(v8::Handle<v8::Array> array, uint32_t len)
     if (!tryFastMalloc(len * sizeof(int)).getValue(data))
         return 0;
     for (uint32_t i = 0; i < len; i++) {
-        v8::Local<v8::Value> val = array->Get(v8::Integer::New(i));
+        v8::Local<v8::Value> val = array->Get(i);
         bool ok;
         int ival = toInt32(val, ok);
         if (!ok) {
