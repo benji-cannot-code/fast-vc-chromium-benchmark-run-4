130370b2a2a86b608e0d239aabbe1d5d76d35471
rharrison@chromium.org <rharrison@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-01 15:05:20 +0000

    Changed kKeyboardHeight in WebUILoginView to reflect change in TouchBrowserFrameView
    There will be a CL in the near future that refactors shared code between these two classes into a common location.
    
    Patch from Ryan Harrison <rharrison@chromium.org>
    
    BUG=none
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7050042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_view.cc
+++ chrome/browser/chromeos/login/webui_login_view.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kKeyboardHeight = 300;
+const int kKeyboardHeight = 360;
 const int kKeyboardSlideDuration = 500;  // In milliseconds
 
 PropertyAccessor<bool>* GetFocusedStateAccessor() {
