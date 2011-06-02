528d775c44b851057b4cff92ef6900e68293b3fc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-02 04:21:26 +0000

    2011-06-01  Jaehun Lim  <ljaehun.lim@samsung.com>
            Reviewed by Eric Seidel.
    
            [EFL] Add adjustMenuListButtonStyle and paintMenuListButton functions
            https://bugs.webkit.org/show_bug.cgi?id=61836
    
            Add functions for rendering drop-down list when style attributes are applied.
    
            * platform/efl/RenderThemeEfl.cpp:
            (WebCore::RenderThemeEfl::adjustMenuListButtonStyle):
            (WebCore::RenderThemeEfl::paintMenuListButton):
            * platform/efl/RenderThemeEfl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
@@ -135,6 +135,9 @@ public:
     virtual void adjustMenuListStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
     virtual bool paintMenuList(RenderObject*, const PaintInfo&, const IntRect&);
 
+    virtual void adjustMenuListButtonStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
+    virtual bool paintMenuListButton(RenderObject*, const PaintInfo&, const IntRect&);
+
     virtual void adjustSearchFieldResultsDecorationStyle(CSSStyleSelector*, RenderStyle*, Element*) const;
     virtual bool paintSearchFieldResultsDecoration(RenderObject*, const PaintInfo&, const IntRect&);
 
