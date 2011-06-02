2336a059a7d90e88554d9386aa60350584a78e19
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 22:39:51 +0000

    Revert 87712 checked in by mistake- Allow multiple ChildProcess objects in one process for the --single-process case, when we have both renderer and gpu. Review URL: http://codereview.chromium.org/6966030
    TBR=jam@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87714 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/child_process.cc
+++ content/common/child_process.cc
@@ -9,12 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <signal.h>  // For SigUSR1Handler below.
 #endif
 
-#include "base/lazy_instance.h"
 #include "base/message_loop.h"
 #include "base/process_util.h"
 #include "base/string_number_conversions.h"
 #include "base/threading/thread.h"
-#include "base/threading/thread_local.h"
 #include "base/utf_string_conversions.h"
 #include "content/common/child_thread.h"
 
@@ -22,23 +20,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static void SigUSR1Handler(int signal) { }
 #endif
 
-namespace {
-static base::LazyInstance<base::ThreadLocalPointer<ChildProcess> > lazy_tls(
-    base::LINKER_INITIALIZED);
-}
+ChildProcess* ChildProcess::child_process_;
 
 ChildProcess::ChildProcess()
     : ref_count_(0),
       shutdown_event_(true, false),
       io_thread_("Chrome_ChildIOThread") {
-  DCHECK(!lazy_tls.Pointer()->Get());
-  lazy_tls.Pointer()->Set(this);
+  DCHECK(!child_process_);
+  child_process_ = this;
 
   io_thread_.StartWithOptions(base::Thread::Options(MessageLoop::TYPE_IO, 0));
 }
 
 ChildProcess::~ChildProcess() {
-  DCHECK(lazy_tls.Pointer()->Get() == this);
+  DCHECK(child_process_ == this);
 
   // Signal this event before destroying the child process.  That way all
   // background threads can cleanup.
@@ -46,15 +41,11 @@ ChildProcess::~ChildProcess() {
   // notice shutdown before the render process begins waiting for them to exit.
   shutdown_event_.Signal();
 
-  // Kill the main thread object before nulling lazy_tls, since
+  // Kill the main thread object before nulling child_process_, since
   // destruction code might depend on it.
   main_thread_.reset();
 
-  lazy_tls.Pointer()->Set(NULL);
-}
-
-ChildProcess* ChildProcess::current() {
-  return lazy_tls.Pointer()->Get();
+  child_process_ = NULL;
 }
 
 ChildThread* ChildProcess::main_thread() {
@@ -75,6 +66,7 @@ void ChildProcess::ReleaseProcess() {
   DCHECK(!main_thread_.get() ||  // null in unittests.
          MessageLoop::current() == main_thread_->message_loop());
   DCHECK(ref_count_);
+  DCHECK(child_process_);
   if (--ref_count_)
     return;
 
@@ -83,7 +75,8 @@ void ChildProcess::ReleaseProcess() {
 }
 
 base::WaitableEvent* ChildProcess::GetShutDownEvent() {
-  return &lazy_tls.Pointer()->Get()->shutdown_event_;
+  DCHECK(child_process_);
+  return &child_process_->shutdown_event_;
 }
 
 void ChildProcess::WaitForDebugger(const std::string& label) {
