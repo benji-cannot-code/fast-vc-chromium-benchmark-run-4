552acd56aeb04b815bba809370b08f9bde5fa6ef
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 19:49:10 +0000

    Fix extension docs build.
    BUG=84674
    TEST=chrome/common/extensions/docs/build/build.sh runs
    Review URL: http://codereview.chromium.org/7066055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/third_party/jstemplate/compile.py
+++ chrome/third_party/jstemplate/compile.py
@@ -5,14 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 # A python script that combines the javascript files needed by jstemplate into
-# a single file.  Writes to standard output; you are responsible for putting
-# the file into the tree where it belongs.
+# a single file.
 
 import httplib
-import sys
 import urllib
 
 srcs ="util.js jsevalcontext.js jstemplate.js exports.js".split()
+out = "jstemplate_compiled.js"
 
 # Wrap the output in an anonymous function to prevent poluting the global
 # namespace.
@@ -34,5 +33,7 @@ headers = {'Content-type': 'application/x-www-form-urlencoded'}
 conn = httplib.HTTPConnection('closure-compiler.appspot.com')
 conn.request('POST', '/compile', params, headers)
 response = conn.getresponse()
-sys.stdout.write(output_wrapper % response.read())
+out_file = file(out, 'w')
+out_file.write(output_wrapper % response.read())
+out_file.close()
 conn.close()
