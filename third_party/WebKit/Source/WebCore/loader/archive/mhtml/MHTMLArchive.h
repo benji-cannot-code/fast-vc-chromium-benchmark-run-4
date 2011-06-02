167c79c4a24266a8391cf5925b1f463a08b13ac7
jcivelli@chromium.org <jcivelli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-02 21:59:29 +0000

    2011-06-02  Jay Civelli  <jcivelli@chromium.org>
            Reviewed by Adam Barth.
    
            Added a method to generate RFC 2822 compliant date strings.
            https://bugs.webkit.org/show_bug.cgi?id=7169
    
            * wtf/DateMath.cpp:
            (WTF::twoDigitStringFromNumber):
            (WTF::makeRFC2822DateString):
            * wtf/DateMath.h:
    2011-06-02  Jay Civelli  <jcivelli@chromium.org>
    
            Reviewed by Adam Barth.
    
            Adding MHTML generation support to MHTMLArchive.
            https://bugs.webkit.org/show_bug.cgi?id=7169
    
            * loader/archive/mhtml/MHTMLArchive.cpp:
            (WebCore::generateRandomBoundary):
            (WebCore::replaceNonPrintableCharacters):
            (WebCore::MHTMLArchive::generateMHTMLData):
            * loader/archive/mhtml/MHTMLArchive.h:
            * page/PageSerializer.cpp:
            (WebCore::PageSerializer::serializeFrame):
            (WebCore::PageSerializer::serializeCSSStyleSheet):
            * platform/SharedBuffer.cpp:
            (WebCore::SharedBuffer::append):
            * platform/SharedBuffer.h:
    2011-06-02  Jay Civelli  <jcivelli@chromium.org>
    
            Reviewed by Adam Barth.
    
            Adding MHTML generation support to MHTMLArchive.
            https://bugs.webkit.org/show_bug.cgi?id=7169
    
            * public/WebPageSerializer.h:
            * src/WebPageSerializer.cpp:
            (WebKit::WebPageSerializer::serializeToMHTML):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/archive/mhtml/MHTMLArchive.h
+++ third_party/WebKit/Source/WebCore/loader/archive/mhtml/MHTMLArchive.h
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class MHTMLParser;
+class Page;
+class SharedBuffer;
 
 class MHTMLArchive : public Archive {
 public:
@@ -46,6 +48,8 @@ public:
 
     static PassRefPtr<MHTMLArchive> create();
     static PassRefPtr<MHTMLArchive> create(const KURL&, SharedBuffer*);
+
+    static PassRefPtr<SharedBuffer> generateMHTMLData(Page*);
 
 private:
     friend class MHTMLParser;
