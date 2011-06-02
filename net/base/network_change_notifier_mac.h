b5b593fd0cc2addb58635a4f5a84bbb3bee7e2a3
adamk@google.com <adamk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 21:56:44 +0000

    Implement NetworkChangeNotifier::IsCurrentlyOffline() for Mac.
    R=willchan@chromium.org
    BUG=7469,53473
    TEST=load onlineTest.html attachment on bug 7469; disconnect/connect network cable
    
    Review URL: http://codereview.chromium.org/7006015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87699 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_mac.h
+++ net/base/network_change_notifier_mac.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <SystemConfiguration/SCDynamicStore.h>
+#include <SystemConfiguration/SCNetworkReachability.h>
 
 #include "base/basictypes.h"
+#include "base/mac/scoped_cftyperef.h"
 #include "net/base/network_change_notifier.h"
 #include "net/base/network_config_watcher_mac.h"
 
@@ -48,8 +50,15 @@ class NetworkChangeNotifierMac: public NetworkChangeNotifier {
   void SetDynamicStoreNotificationKeys(SCDynamicStoreRef store);
   void OnNetworkConfigChange(CFArrayRef changed_keys);
 
+  static void ReachabilityCallback(SCNetworkReachabilityRef target,
+                                   SCNetworkConnectionFlags flags,
+                                   void* notifier);
+
   Forwarder forwarder_;
   const NetworkConfigWatcherMac config_watcher_;
+  base::mac::ScopedCFTypeRef<CFRunLoopRef> run_loop_;
+  base::mac::ScopedCFTypeRef<SCNetworkReachabilityRef> reachability_;
+  bool network_reachable_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierMac);
 };
