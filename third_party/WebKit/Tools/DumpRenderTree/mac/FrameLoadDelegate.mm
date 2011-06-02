63d057869bb25be6d65133c9354d048360b2fb70
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-02 20:15:34 +0000

    2011-06-02  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Darin Adler.
    
            Add build logistics and plumbing for window.internals object.
            https://bugs.webkit.org/show_bug.cgi?id=60313
    
            * fast/dom/Window/script-tests/window-property-descriptors.js: Added internals object to list of properties to skip.
            * fast/dom/Window/window-properties.html: Ditto.
            * fast/dom/script-tests/prototype-inheritance-2.js: Ditto.
            * fast/dom/script-tests/prototype-inheritance.js: Ditto.
            * fast/harness/internals-object-expected.txt: Added.
            * fast/harness/internals-object.html: Added.
            * platform/gtk/Skipped: Skipped the newly added test until platform supports window.internals.
            * platform/mac-wk2/Skipped: Ditto.
            * platform/qt/Skipped: Ditto.
            * platform/win/Skipped: Ditto.
    2011-06-02  Dimitri Glazkov  <dglazkov@chromium.org>
    
            Reviewed by Darin Adler.
    
            Add build logistics and plumbing for window.internals object.
            https://bugs.webkit.org/show_bug.cgi?id=60313
    
            Test: fast/harness/internals-object.html
    
            * Configurations/WebCoreTestSupport.xcconfig: Added.
            * DerivedSources.make: Added support for generating from Internals.idl.
            * WebCore.gyp/WebCore.gyp: Added new webcore_test_support library.
            * WebCore.gypi: Ditto.
            * WebCore.xcodeproj/project.pbxproj: Added WebCoreTestSupport library.
            * testing/Internals.cpp: Added.
            * testing/Internals.h: Added.
            * testing/Internals.idl: Added.
            * testing/js/WebCoreTestSupport.cpp: Added.
            * testing/js/WebCoreTestSupport.h: Added.
            * testing/v8/WebCoreTestSupport.cpp: Added.
            * testing/v8/WebCoreTestSupport.h: Added.
    2011-06-02  Dimitri Glazkov  <dglazkov@chromium.org>
    
            Reviewed by Darin Adler.
    
            Add build logistics and plumbing for window.internals object.
            https://bugs.webkit.org/show_bug.cgi?id=60313
    
            * WebKit.gyp: Added linking new webkit_test_support library.
            * public/WebTestingSupport.h: Added.
            * src/WebTestingSupport.cpp: Added.
    2011-06-02  Dimitri Glazkov  <dglazkov@chromium.org>
    
            Reviewed by Darin Adler.
    
            Add build logistics and plumbing for window.internals object.
            https://bugs.webkit.org/show_bug.cgi?id=60313
    
            * DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj: Added linking new WebCoreTestSupport library.
            * DumpRenderTree/chromium/TestShell.cpp:
            (TestShell::bindJSObjectsToWindow): Added injection code.
            * DumpRenderTree/mac/FrameLoadDelegate.mm:
            (-[FrameLoadDelegate didClearWindowObjectInStandardWorldForFrame:]): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/FrameLoadDelegate.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/FrameLoadDelegate.mm
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ObjCPluginFunction.h"
 #import "PlainTextController.h"
 #import "TextInputController.h"
+#import "WebCoreTestSupport.h"
 #import "WorkQueue.h"
 #import "WorkQueueItem.h"
 #import <JavaScriptCore/JavaScriptCore.h>
@@ -266,6 +267,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     accessibilityController->makeWindowObject(context, globalObject, &exception);
     ASSERT(!exception);
+
+    WebCoreTestSupport::injectInternalsObject(context);
 
     // Make Old-Style controllers
 
