0ee9c9eb350bfe86d0d91b47d0e25af73cd60bae
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-02 18:08:02 +0000

    2011-06-02  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r87920.
            http://trac.webkit.org/changeset/87920
            https://bugs.webkit.org/show_bug.cgi?id=61943
    
            This patch does not build in Win Builder (dbg)(shared)
            downstream (Requested by abarth|gardener on #webkit).
    
            * tests/PopupMenuTest.cpp:
            (WebKit::TestPopupMenuClient::TestPopupMenuClient):
            (WebKit::TestPopupMenuClient::valueChanged):
            (WebKit::TestPopupMenuClient::itemIsEnabled):
            (WebKit::SelectPopupMenuTest::TearDown):
            * tests/data/popup/select_event.html: Removed.
            * tests/data/popup/select_event_remove_on_change.html: Removed.
            * tests/data/popup/select_event_remove_on_click.html: Removed.
    2011-06-02  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r87920.
            http://trac.webkit.org/changeset/87920
            https://bugs.webkit.org/show_bug.cgi?id=61943
    
            This patch does not build in Win Builder (dbg)(shared)
            downstream (Requested by abarth|gardener on #webkit).
    
            * platform/chromium/PopupMenuChromium.cpp:
            (WebCore::PopupContainer::showPopup):
            (WebCore::PopupListBox::handleMouseReleaseEvent):
            (WebCore::PopupListBox::acceptIndex):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@87932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
@@ -189,8 +189,8 @@ private:
     void selectIndex(int index);
 
     // Accepts the selected index as the value to be displayed in the <select> widget on
-    // the web page, and closes the popup. Returns true if index is accepted.
-    bool acceptIndex(int index);
+    // the web page, and closes the popup.
+    void acceptIndex(int index);
 
     // Clears the selection (so no row appears selected).
     void clearSelection();
@@ -278,9 +278,6 @@ private:
 
     // If width exeeds screen width, we have to clip it.
     int m_maxWindowWidth;
-
-    // To forward last mouse release event.
-    RefPtr<Node> m_focusedNode;
 };
 
 static PlatformMouseEvent constructRelativeMouseEvent(const PlatformMouseEvent& e,
@@ -411,7 +408,6 @@ IntRect PopupContainer::layoutAndCalculateWidgetRect(int targetControlHeight, co
 void PopupContainer::showPopup(FrameView* view)
 {
     m_frameView = view;
-    listBox()->m_focusedNode = m_frameView->frame()->document()->focusedNode();
 
     ChromeClientChromium* chromeClient = chromeClientChromium();
     if (chromeClient) {
@@ -677,16 +673,7 @@ bool PopupListBox::handleMouseReleaseEvent(const PlatformMouseEvent& event)
     if (!isPointInBounds(event.pos()))
         return true;
 
-    // Need to check before calling acceptIndex(), because m_popupClient might be removed in acceptIndex() calling because of event handler.
-    bool isSelectPopup = m_popupClient->menuStyle().menuType() == PopupMenuStyle::SelectPopup;
-    if (acceptIndex(pointToRowIndex(event.pos())) && m_focusedNode && isSelectPopup) {
-        m_focusedNode->dispatchMouseEvent(event, eventNames().mouseupEvent);
-        m_focusedNode->dispatchMouseEvent(event, eventNames().clickEvent);
-
-        // Clear m_focusedNode here, because we cannot clear in hidePopup() which is called before dispatchMouseEvent() is called.
-        m_focusedNode = 0;
-    }
-
+    acceptIndex(pointToRowIndex(event.pos()));
     return true;
 }
 
@@ -1082,21 +1069,21 @@ int PopupListBox::pointToRowIndex(const IntPoint& point)
     return -1;
 }
 
-bool PopupListBox::acceptIndex(int index)
+void PopupListBox::acceptIndex(int index)
 {
     // Clear m_acceptedIndexOnAbandon once user accepts the selected index.
     if (m_acceptedIndexOnAbandon >= 0)
         m_acceptedIndexOnAbandon = -1;
 
     if (index >= numItems())
-        return false;
+        return;
 
     if (index < 0) {
         if (m_popupClient) {
             // Enter pressed with no selection, just close the popup.
             hidePopup();
         }
-        return false;
+        return;
     }
 
     if (isSelectableItem(index)) {
@@ -1107,11 +1094,7 @@ bool PopupListBox::acceptIndex(int index)
 
         // Tell the <select> PopupMenuClient what index was selected.
         m_popupClient->valueChanged(index);
-
-        return true;
     }
-
-    return false;
 }
 
 void PopupListBox::selectIndex(int index)
