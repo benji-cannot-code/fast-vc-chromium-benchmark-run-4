40b49103dcd2cd4430b686cd59d9643ec202ebb6
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 21:06:54 +0000

    net: switch from TXT DNS records to CAA.
    The format of the keys-in-DNS record has started to solidify into CAA.
    This change starts to switch over to using CAA records. None of this
    code is enabled by default in Chrome.
    
    BUG=none
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/6281012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87677 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.h
+++ net/socket/ssl_client_socket_nss.h
@@ -63,7 +63,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
   virtual void GetSSLInfo(SSLInfo* ssl_info);
   virtual void GetSSLCertRequestInfo(SSLCertRequestInfo* cert_request_info);
   virtual NextProtoStatus GetNextProto(std::string* proto);
-  virtual void UseDNSSEC(DNSSECProvider* provider);
 
   // StreamSocket methods:
   virtual int Connect(CompletionCallback* callback);
@@ -89,7 +88,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
     STATE_NONE,
     STATE_HANDSHAKE,
     STATE_VERIFY_DNSSEC,
-    STATE_VERIFY_DNSSEC_COMPLETE,
     STATE_VERIFY_CERT,
     STATE_VERIFY_CERT_COMPLETE,
   };
@@ -118,7 +116,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
   int DoHandshake();
 
   int DoVerifyDNSSEC(int result);
-  int DoVerifyDNSSECComplete(int result);
   int DoVerifyCert(int result);
   int DoVerifyCertComplete(int result);
   int DoPayloadRead();
@@ -230,8 +227,6 @@ class SSLClientSocketNSS : public SSLClientSocket {
   // True if the peer name has been initialized.
   bool peername_initialized_;
 
-  // This pointer is owned by the caller of UseDNSSEC.
-  DNSSECProvider* dnssec_provider_;
   // The time when we started waiting for DNSSEC records.
   base::Time dnssec_wait_start_time_;
 
