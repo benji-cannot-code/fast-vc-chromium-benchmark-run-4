2f343e10084e6cb9aab03a86f2bb50b6fb200ef9
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 18:09:07 +0000

    Extension API: Rename contentSettings.misc to .global and rename preferences under it.
    BUG=71067
    
    Review URL: http://codereview.chromium.org/7031063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87632 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/renderer_extension_bindings.js
+++ chrome/renderer/resources/renderer_extension_bindings.js
@@ -302,7 +302,7 @@ var chrome = chrome || {};
       "devtools",
       "experimental.accessibility",
       "experimental.bookmarkManager",
-      "experimental.contentSettings.misc",
+      "experimental.contentSettings.global",
       "experimental.debugger",
       "experimental.extension",
       "experimental.infobars",
