affaba3acaacfb25e02c0ea9fc692ffbf8e47fc1
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 14:55:04 +0000

    Adds unit tests that attempt to verify the following through simulation: a) That a server experiencing overload will actually benefit from the    anti-DDoS throttling logic, i.e. that its traffic spike will subside    and be distributed over a longer period of time; b) That "well-behaved" clients of a server under DDoS attack actually    benefit from the anti-DDoS throttling logic, i.e. they get a better    level of service during the attack; and c) That the approximate increase in "perceived downtime" introduced by    anti-DDoS throttling for various different actual downtimes is what    we expect it to be.
    BUG=84210
    TEST=net_unittests --gtest_filter=*Simulation*
    
    Review URL: http://codereview.chromium.org/7082005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87608 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_throttler_entry_interface.h
+++ net/url_request/url_request_throttler_entry_interface.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_URL_REQUEST_URL_REQUEST_THROTTLER_ENTRY_INTERFACE_H_
 #pragma once
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
 #include "base/time.h"
--- net/url_request/url_request_throttler_unittest.cc
+++ net/url_request/url_request_throttler_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "net/url_request/url_request_throttler_manager.h"
+
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/histogram.h"
 #include "base/pickle.h"
@@ -12,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/test_completion_callback.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_throttler_header_interface.h"
-#include "net/url_request/url_request_throttler_manager.h"
+#include "net/url_request/url_request_throttler_test_support.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::TimeDelta;
@@ -25,30 +27,8 @@ namespace {
 using base::Histogram;
 using base::StatisticsRecorder;
 
-class MockURLRequestThrottlerManager;
-
-class MockBackoffEntry : public BackoffEntry {
- public:
-  explicit MockBackoffEntry(const BackoffEntry::Policy* const policy)
-      : BackoffEntry(policy), fake_now_(TimeTicks()) {
-  }
-
-  virtual ~MockBackoffEntry() {}
-
-  virtual TimeTicks ImplGetTimeNow() const OVERRIDE {
-    return fake_now_;
-  }
-
-  void SetFakeNow(const TimeTicks& now) {
-    fake_now_ = now;
-  }
-
- private:
-  TimeTicks fake_now_;
-};
-
 class MockURLRequestThrottlerEntry : public URLRequestThrottlerEntry {
- public :
+ public:
   explicit MockURLRequestThrottlerEntry(
       net::URLRequestThrottlerManager* manager)
       : net::URLRequestThrottlerEntry(manager, ""),
@@ -65,7 +45,7 @@ class MockURLRequestThrottlerEntry : public URLRequestThrottlerEntry {
         mock_backoff_entry_(&backoff_policy_) {
     InitPolicy();
 
-    mock_backoff_entry_.SetFakeNow(fake_now);
+    mock_backoff_entry_.set_fake_now(fake_now);
     set_exponential_backoff_release_time(exponential_backoff_release_time);
     set_sliding_window_release_time(sliding_window_release_time);
   }
@@ -90,7 +70,7 @@ class MockURLRequestThrottlerEntry : public URLRequestThrottlerEntry {
 
   void ResetToBlank(const TimeTicks& time_now) {
     fake_time_now_ = time_now;
-    mock_backoff_entry_.SetFakeNow(time_now);
+    mock_backoff_entry_.set_fake_now(time_now);
 
     GetBackoffEntry()->Reset();
     GetBackoffEntry()->SetCustomReleaseTime(time_now);
@@ -117,50 +97,6 @@ class MockURLRequestThrottlerEntry : public URLRequestThrottlerEntry {
 
   TimeTicks fake_time_now_;
   MockBackoffEntry mock_backoff_entry_;
-};
-
-class MockURLRequestThrottlerHeaderAdapter
-    : public URLRequestThrottlerHeaderInterface {
- public:
-  MockURLRequestThrottlerHeaderAdapter()
-      : fake_retry_value_(""),
-        fake_opt_out_value_(""),
-        fake_response_code_(0) {
-  }
-
-  explicit MockURLRequestThrottlerHeaderAdapter(int response_code)
-      : fake_retry_value_(""),
-        fake_opt_out_value_(""),
-        fake_response_code_(response_code) {
-  }
-
-  MockURLRequestThrottlerHeaderAdapter(const std::string& retry_value,
-                                       const std::string& opt_out_value,
-                                       int response_code)
-      : fake_retry_value_(retry_value),
-        fake_opt_out_value_(opt_out_value),
-        fake_response_code_(response_code) {
-  }
-
-  virtual ~MockURLRequestThrottlerHeaderAdapter() {}
-
-  virtual std::string GetNormalizedValue(const std::string& key) const {
-    if (key == MockURLRequestThrottlerEntry::kRetryHeaderName &&
-        !fake_retry_value_.empty()) {
-      return fake_retry_value_;
-    } else if (key ==
-        MockURLRequestThrottlerEntry::kExponentialThrottlingHeader &&
-        !fake_opt_out_value_.empty()) {
-      return fake_opt_out_value_;
-    }
-    return "";
-  }
-
-  virtual int GetResponseCode() const { return fake_response_code_; }
-
-  std::string fake_retry_value_;
-  std::string fake_opt_out_value_;
-  int fake_response_code_;
 };
 
 class MockURLRequestThrottlerManager : public URLRequestThrottlerManager {
