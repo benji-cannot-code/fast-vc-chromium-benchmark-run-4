387b78ee1f6740f124e2558a1a1c1cda88c812d3
annacc@chromium.org <annacc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 21:08:47 +0000

    Unmark VideoBearWebm, VideoBearTheora, and VideoBearWav as flaky on Mac.
    This flakiness was fixed with r87616 so unmarking the test as flaky.
    
    BUG=84170
    TEST=MediaTest.VideoBearWebm, MediaTest.VideoBearTheora, and MediaTest.VideoBearWav
    
    
    Review URL: http://codereview.chromium.org/7054051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87862 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_uitest.cc
+++ chrome/browser/media_uitest.cc
@@ -66,19 +66,7 @@ class MediaTest : public UITest {
   }
 };
 
-#if defined(OS_MACOSX)
-// http://crbug.com/84170 - VideoBearTheora, VideoBearWav and VideoBearWebm
-// are flaky on Mac.
-#define MAYBE_VideoBearTheora FLAKY_VideoBearTheora
-#define MAYBE_VideoBearWav FLAKY_VideoBearWav
-#define MAYBE_VideoBearWebm FLAKY_VideoBearWebm
-#else
-#define MAYBE_VideoBearTheora VideoBearTheora
-#define MAYBE_VideoBearWav  VideoBearWav
-#define MAYBE_VideoBearWebm VideoBearWebm
-#endif
-
-TEST_F(MediaTest, MAYBE_VideoBearTheora) {
+TEST_F(MediaTest, VideoBearTheora) {
   PlayVideo("bear.ogv");
 }
 
@@ -86,7 +74,7 @@ TEST_F(MediaTest, VideoBearSilentTheora) {
   PlayVideo("bear_silent.ogv");
 }
 
-TEST_F(MediaTest, MAYBE_VideoBearWebm) {
+TEST_F(MediaTest, VideoBearWebm) {
   PlayVideo("bear.webm");
 }
 
@@ -104,7 +92,7 @@ TEST_F(MediaTest, VideoBearSilentMp4) {
 }
 #endif
 
-TEST_F(MediaTest, MAYBE_VideoBearWav) {
+TEST_F(MediaTest, VideoBearWav) {
   PlayVideo("bear.wav");
 }
 
