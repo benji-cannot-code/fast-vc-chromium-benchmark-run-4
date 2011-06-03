c709ac4d475635094dcf0c0d17387cc0104af665
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 05:49:35 +0000

    Listen to XI_HierarchyChanged events and call setxkbmap when needed.
    BUG=chromium-os:15851
    BUG=chromium-os:15516
    BUG=84694
    TEST=manually done: Log in, change the layout from US Qwerty (US) to US Extended (EXTD), focus Omnibox, press AltGr+a, confirm á is shown,  plug a USB keyboard, press AltGr+a, confirm the character again, press ctrl+alt+f2 to show VT2, press ctrl+alt+f1 to go back to X, press AltGr+a, confirm the char again, close lid, open lid, press AltGr+a, confirm the char again.
    TEST=On login screen, change the keyboard layout to EXTD, add "sleep 30" to /etc/init/udev-*.conf files, reboot, on login screen, wait for ~30 seconds, press AltGr+a, confirm á is shown.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=87585
    
    Review URL: http://codereview.chromium.org/6975057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87756 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
 #include "chrome/browser/chromeos/system_key_event_listener.h"
+#include "chrome/browser/chromeos/xinput_hierarchy_changed_event_listener.h"
 #endif
 
 using base::Time;
@@ -132,6 +133,13 @@ void Shutdown() {
   // The system key event listener needs to be shut down earlier than when
   // Singletons are finally destroyed in AtExitManager.
   chromeos::SystemKeyEventListener::GetInstance()->Stop();
+
+  // TODO(yusukes): Remove the #if once the ARM bot (crbug.com/84694) is fixed.
+#if defined(HAVE_XINPUT2)
+  // The XInput2 event listener needs to be shut down earlier than when
+  // Singletons are finally destroyed in AtExitManager.
+  chromeos::XInputHierarchyChangedEventListener::GetInstance()->Stop();
+#endif
 #endif
 
   // WARNING: During logoff/shutdown (WM_ENDSESSION) we may not have enough
