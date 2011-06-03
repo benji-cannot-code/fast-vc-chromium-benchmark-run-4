3b19e5cffb50f296d9ed653ebfbc28149616d171
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 16:21:57 +0000

    Convert ProfileMenuButton menu implementation from Menu2 to MenuItemView.
    BUG=chromium-os:13887
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7027015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87792 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/profile_menu_button.cc
+++ chrome/browser/ui/views/profile_menu_button.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/text/text_elider.h"
 #include "ui/gfx/color_utils.h"
 #include "views/controls/button/button.h"
-#include "views/controls/menu/menu_2.h"
+#include "views/controls/menu/menu_item_view.h"
+#include "views/controls/menu/menu_model_adapter.h"
+#include "views/window/window.h"
 
 // Menu should display below the profile button tag image on the frame. This
 // offset size depends on whether the frame is in glass or opaque mode.
@@ -38,7 +40,6 @@ ProfileMenuButton::ProfileMenuButton(const std::wstring& text, Profile* profile)
   SetHighlightColor(kTextHighlighted);
 
   profile_menu_model_.reset(new ProfileMenuModel);
-  menu_.reset(new views::Menu2(profile_menu_model_.get()));
 }
 
 ProfileMenuButton::~ProfileMenuButton() {}
@@ -50,6 +51,13 @@ void ProfileMenuButton::SetText(const std::wstring& text) {
 
 // views::ViewMenuDelegate implementation
 void ProfileMenuButton::RunMenu(views::View* source, const gfx::Point &pt) {
+  views::MenuModelAdapter menu_model_adapter(profile_menu_model_.get());
+  views::MenuItemView menu(&menu_model_adapter);
+  menu_model_adapter.BuildMenu(&menu);
+
   gfx::Point menu_point(pt.x(), pt.y() + kMenuDisplayOffset);
-  menu_->RunMenuAt(menu_point, views::Menu2::ALIGN_TOPRIGHT);
+  menu.RunMenuAt(source->GetWindow()->GetNativeWindow(), NULL,
+                 gfx::Rect(pt, gfx::Size(0, 0)),
+                 views::MenuItemView::TOPRIGHT,
+                 true);
 }
