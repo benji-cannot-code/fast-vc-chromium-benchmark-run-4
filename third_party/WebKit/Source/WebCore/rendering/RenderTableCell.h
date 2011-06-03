7f63e473384c8179242959ac9acd5880c7c0e931
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-03 20:59:40 +0000

    2011-06-03  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch paintBackgroundsBehindCell to use IntPoint
            https://bugs.webkit.org/show_bug.cgi?id=62031
    
            Switching paintBackgroundsBehindCell to take an IntPoint representing
            the paint offset instead of a pair of ints.
    
            No new tests as this is simple refactoring.
    
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::paintBackgroundsBehindCell):
            (WebCore::RenderTableCell::paintBoxDecorations):
            * rendering/RenderTableCell.h:
            * rendering/RenderTableRow.cpp:
            (WebCore::RenderTableRow::paint):
            * rendering/RenderTableSection.cpp:
            (WebCore::RenderTableSection::paintCell):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88051 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
@@ -97,7 +97,7 @@ public:
 
     virtual void paint(PaintInfo&, int tx, int ty);
 
-    void paintBackgroundsBehindCell(PaintInfo&, int tx, int ty, RenderObject* backgroundObject);
+    void paintBackgroundsBehindCell(PaintInfo&, const IntPoint&, RenderObject* backgroundObject);
 
     int cellBaselinePosition() const;
 
