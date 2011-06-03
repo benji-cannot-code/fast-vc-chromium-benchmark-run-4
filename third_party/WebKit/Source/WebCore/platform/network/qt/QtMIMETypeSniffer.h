c28602071c87833bddfd8d32eb713a8340dde0ff
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-03 20:32:47 +0000

    2011-06-03  Alexis Menard  <alexis.menard@openbossa.org>
            Reviewed by Andreas Kling.
    
            To support building namespaced Qt, we require that forward-declarations
            of Qt classes be wrapped in QT_BEGIN_NAMESPACE and QT_END_NAMESPACE.
    
            * WebCoreSupport/FrameLoaderClientQt.h:
    2011-06-03  Alexis Menard  <alexis.menard@openbossa.org>
    
            Reviewed by Andreas Kling.
    
            To support building namespaced Qt, we require that forward-declarations
            of Qt classes be wrapped in QT_BEGIN_NAMESPACE and QT_END_NAMESPACE.
    
            * platform/network/qt/QtMIMETypeSniffer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88045 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/qt/QtMIMETypeSniffer.h
+++ third_party/WebKit/Source/WebCore/platform/network/qt/QtMIMETypeSniffer.h
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MIMESniffing.h"
 #include <QObject>
 
+QT_BEGIN_NAMESPACE
 class QNetworkReply;
+QT_END_NAMESPACE
 
 class QtMIMETypeSniffer : public QObject {
     Q_OBJECT
--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -45,7 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <qobject.h>
 #include <wtf/Forward.h>
 
+QT_BEGIN_NAMESPACE
 class QNetworkReply;
+QT_END_NAMESPACE
+
 class QWebFrame;
 
 namespace WebCore {
