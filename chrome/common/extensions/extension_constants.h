d977d35a92d65330a08fde8da5e72a96659b3de6
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 07:05:27 +0000

    Add input_components loading to the manifest
    BUG=chromium-os:16090
    TEST=None
    
    Review URL: http://codereview.chromium.org/7066046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Keys used in JSON representation of extensions.
 namespace extension_manifest_keys {
   extern const char* kAllFrames;
+  extern const char* kAltKey;
   extern const char* kApp;
   extern const char* kBackground;
   extern const char* kBrowserAction;
@@ -21,6 +22,7 @@ namespace extension_manifest_keys {
   extern const char* kContentSecurityPolicy;
   extern const char* kConvertedFromUserScript;
   extern const char* kCss;
+  extern const char* kCtrlKey;
   extern const char* kCurrentLocale;
   extern const char* kDefaultLocale;
   extern const char* kDescription;
@@ -31,16 +33,21 @@ namespace extension_manifest_keys {
   extern const char* kFileBrowserHandlers;
   extern const char* kHomepageURL;
   extern const char* kIcons;
+  extern const char* kId;
   extern const char* kIncognito;
   extern const char* kIncludeGlobs;
+  extern const char* kInputComponents;
   extern const char* kIsolation;
   extern const char* kJs;
+  extern const char* kKeycode;
+  extern const char* kLanguage;
   extern const char* kLaunch;
   extern const char* kLaunchContainer;
   extern const char* kLaunchHeight;
   extern const char* kLaunchLocalPath;
   extern const char* kLaunchWebURL;
   extern const char* kLaunchWidth;
+  extern const char* kLayouts;
   extern const char* kMatches;
   extern const char* kMinimumChromeVersion;
   extern const char* kNaClModules;
@@ -66,6 +73,8 @@ namespace extension_manifest_keys {
   extern const char* kPluginsPublic;
   extern const char* kPublicKey;
   extern const char* kRunAt;
+  extern const char* kShiftKey;
+  extern const char* kShortcutKey;
   extern const char* kSidebar;
   extern const char* kSidebarDefaultIcon;
   extern const char* kSidebarDefaultPage;
@@ -148,6 +157,14 @@ namespace extension_manifest_errors {
   extern const char* kInvalidIconPath;
   extern const char* kInvalidIcons;
   extern const char* kInvalidIncognitoBehavior;
+  extern const char* kInvalidInputComponents;
+  extern const char* kInvalidInputComponentDescription;
+  extern const char* kInvalidInputComponentLayoutName;
+  extern const char* kInvalidInputComponentLayouts;
+  extern const char* kInvalidInputComponentName;
+  extern const char* kInvalidInputComponentShortcutKey;
+  extern const char* kInvalidInputComponentShortcutKeycode;
+  extern const char* kInvalidInputComponentType;
   extern const char* kInvalidIsolation;
   extern const char* kInvalidIsolationValue;
   extern const char* kInvalidJs;
