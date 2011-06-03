7fb53f9dca144f0266691f59f654d5e9272d0b5a
rkn@chromium.org <rkn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 19:43:08 +0000

    Changed "set_id" in "union SettingsFlagsAndId" so that it gives "flags()" the correct byte ordering.
    Review URL: http://codereview.chromium.org/7066009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87833 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -304,8 +304,7 @@ union SettingsFlagsAndId {
   uint32 id() const { return (ntohl(id_) & kSettingsIdMask); }
   void set_id(uint32 id) {
     DCHECK_EQ(0u, (id & ~kSettingsIdMask));
-    id = htonl(id & kSettingsIdMask);
-    id_ = flags() | id;
+    id_ = htonl((flags() << 24) | (id & kSettingsIdMask));
   }
 
   uint8 flags_[4];  // 8 bits
