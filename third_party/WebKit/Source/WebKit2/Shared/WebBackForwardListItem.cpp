52b6b27f87dccf7e7780a85505acc1d94e3a8f4e
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-03 23:29:52 +0000

    <rdar://problem/9553645> and https://bugs.webkit.org/show_bug.cgi?id=62064 WebBackForwardListItem::setBackForwardData actually appends data
    Reviewed by Steve Falkenburg.
    
    * Shared/WebBackForwardListItem.cpp:
    (WebKit::WebBackForwardListItem::setBackForwardData): Clear the Vector out before appending
      the replacement data.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebBackForwardListItem.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/WebBackForwardListItem.cpp
@@ -57,6 +57,7 @@ uint64_t WebBackForwardListItem::highedUsedItemID()
 
 void WebBackForwardListItem::setBackForwardData(const uint8_t* data, size_t size)
 {
+    m_backForwardData.clear();
     m_backForwardData.reserveCapacity(size);
     m_backForwardData.append(data, size);
 }
