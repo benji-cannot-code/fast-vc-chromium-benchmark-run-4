3cad7bdc65268080d34ddc793d447cb3a50ad4bd
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-03 19:35:34 +0000

    2011-06-03  Rob Buis  <rbuis@rim.com>
            Reviewed by Nikolas Zimmermann.
    
            Cleanup member variable usage in svg/animation classes
            https://bugs.webkit.org/show_bug.cgi?id=62029
    
            Cleanup member variables.
    
            * svg/animation/SMILTimeContainer.cpp:
            (WebCore::SMILTimeContainer::SMILTimeContainer):
            (WebCore::SMILTimeContainer::sampleAnimationAtTime):
            (WebCore::SMILTimeContainer::updateAnimations):
            * svg/animation/SMILTimeContainer.h:
            * svg/animation/SVGSMILElement.cpp:
            (WebCore::SVGSMILElement::SVGSMILElement):
            * svg/animation/SVGSMILElement.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88039 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/animation/SMILTimeContainer.h
+++ third_party/WebKit/Source/WebCore/svg/animation/SMILTimeContainer.h
@@ -71,7 +71,7 @@ private:
     
     void timerFired(Timer<SMILTimeContainer>*);
     void startTimer(SMILTime fireTime, SMILTime minimumDelay = 0);
-    void updateAnimations(SMILTime elapsed);
+    void updateAnimations(SMILTime elapsed, double nextManualSampleTime = 0, String nextSamplingTarget = String());
     
     void updateDocumentOrderIndexes();
     void sortByPriority(Vector<SVGSMILElement*>& smilElements, SMILTime elapsed);
@@ -82,8 +82,6 @@ private:
     double m_beginTime;
     double m_pauseTime;
     double m_accumulatedPauseTime;
-    double m_nextManualSampleTime;
-    String m_nextSamplingTarget;
 
     bool m_documentOrderIndexesDirty;
     
