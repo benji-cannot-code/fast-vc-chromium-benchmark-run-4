31bf403d209e4b18f1704b5d1519927e9682f9f5
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 22:19:42 +0000

    net: Add NET_TEST to a new code.
    BUG=76997
    TEST=none
    Review URL: http://codereview.chromium.org/7058052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87885 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/crl_filter.h
+++ net/base/crl_filter.h
@@ -12,10 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include <base/memory/ref_counted.h>
-#include <base/memory/scoped_ptr.h>
-#include <base/string_piece.h>
-#include <base/synchronization/lock.h>
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/string_piece.h"
+#include "base/synchronization/lock.h"
+#include "net/base/net_api.h"
 
 class DictionaryValue;
 
@@ -31,7 +32,7 @@ class GolombCompressedSet;
 // a firm "not revoked" answer or a probabilistic "revoked" answer.
 // Additionally, a CRLFilter can contain a list of blocked public keys and, in
 // that case, it can give a firm "revoked" answer.
-class CRLFilter : public base::RefCounted<CRLFilter> {
+class NET_TEST CRLFilter : public base::RefCounted<CRLFilter> {
  public:
   enum Result {
     REVOKED,  // the certificate should be rejected.
