a47d83925b0cdbfa624e0c6a0e145e6cf7701b81
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 01:00:44 +0000

    Fix flaky crashing safe browsing tests.
    We cannot run the message loop after the RenderThread object has been destroyed, as some tasks may try to access it (such as RendererNetPredictor::DnsPrefetchNames)
    
    BUG=84623
    TEST=PhishingDOMFeatureExtractorTest.LinkFeatures, PhishingDOMFeatureExtractorTest.SubFrames, PhishingClassifierTest.TestClassification become less flaky.
    
    Review URL: http://codereview.chromium.org/6992078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -188,14 +188,7 @@ TEST_F(PhishingDOMFeatureExtractorTest, ScriptAndImageFeatures) {
   EXPECT_THAT(features.features(), ContainerEq(expected_features.features()));
 }
 
-// Flaky on windows, http://crbug.com/84623
-#if defined(OS_WIN)
-#define MAYBE_SubFrames FLAKY_SubFrames
-#else
-#define MAYBE_SubFrames SubFrames
-#endif
-
-TEST_F(PhishingDOMFeatureExtractorTest, MAYBE_SubFrames) {
+TEST_F(PhishingDOMFeatureExtractorTest, SubFrames) {
   // This test doesn't exercise the extraction timing.
   EXPECT_CALL(clock_, Now()).WillRepeatedly(Return(base::TimeTicks::Now()));
 
