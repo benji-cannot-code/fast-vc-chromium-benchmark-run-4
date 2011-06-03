0ae058dbad7483545d439cbcfe15c88fa34c0bd2
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 12:27:16 +0000

    Restriction by extention ID added.
    BUG=84495
    TEST=ExtensionApiTest.DecodeJPEG
    
    Review URL: http://codereview.chromium.org/7066051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_decode_jpeg_apitest.cc
+++ chrome/browser/extensions/extension_decode_jpeg_apitest.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-#if defined(OS_CHROMEOS)
-
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DecodeJPEG) {
   ASSERT_TRUE(RunExtensionTest("decode_jpeg")) << message_;
 }
-
-#endif
--- chrome/renderer/extensions/extension_process_bindings.cc
+++ chrome/renderer/extensions/extension_process_bindings.cc
@@ -35,10 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/render_view_visitor.h"
 #include "grit/common_resources.h"
 #include "grit/renderer_resources.h"
-#include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/skia/include/core/SkColor.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "webkit/glue/webkit_glue.h"
 
 using bindings_utils::GetStringResource;
@@ -184,7 +184,7 @@ class ExtensionImpl : public ExtensionBase {
     } else if (name->Equals(v8::String::New("GetLocalFileSystem"))) {
       return v8::FunctionTemplate::New(GetLocalFileSystem);
     } else if (name->Equals(v8::String::New("DecodeJPEG"))) {
-      return v8::FunctionTemplate::New(DecodeJPEG);
+      return v8::FunctionTemplate::New(DecodeJPEG, v8::External::New(this));
     }
 
     return ExtensionBase::GetNativeFunction(name);
@@ -278,6 +278,16 @@ class ExtensionImpl : public ExtensionBase {
 
   // Decodes supplied JPEG byte array to image pixel array.
   static v8::Handle<v8::Value> DecodeJPEG(const v8::Arguments& args) {
+    static const char* kWebAppId = "haiffjcadagjlijoggckpgfnoeiflnem";
+    static const char* kWebAppIdTest = "oflbaaikkabfdfkimeclgkackhdkpnip";
+    ExtensionImpl* v8_extension = GetFromArguments<ExtensionImpl>(args);
+    const ::Extension* extension =
+        v8_extension->GetExtensionForCurrentContext();
+    if (!extension)
+      return v8::Undefined();
+    if (extension->id() != kWebAppId && extension->id() != kWebAppIdTest)
+      return v8::Undefined();
+
     DCHECK(args.Length() == 1);
     DCHECK(args[0]->IsArray());
     v8::Local<v8::Object> jpeg_array = args[0]->ToObject();
