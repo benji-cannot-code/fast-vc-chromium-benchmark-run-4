ef6f6991359c20b092311a35d9fcac6e5f294bb5
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-04 11:14:49 +0000

    2011-06-04  Nico Weber  <thakis@chromium.org>
            Reviewed by James Robinson.
    
            [chromium] Make ChromePrintContext's destructor virtual
            https://bugs.webkit.org/show_bug.cgi?id=62076
    
            Since WebFrameImpl's m_printContext is a OnwPtr<ChromePrintContext>
            that is dynamically set to either a ChromePrintContext or a
            ChromePluginPrintContext, this fixes an actual bug.
    
            * src/WebFrameImpl.cpp:
            (WebKit::ChromePrintContext::~ChromePrintContext):
            (WebKit::ChromePluginPrintContext::~ChromePluginPrintContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -307,6 +307,8 @@ public:
     {
     }
 
+    virtual ~ChromePrintContext() { }
+
     virtual void begin(float width, float height)
     {
         ASSERT(!m_printedPageWidth);
@@ -376,6 +378,8 @@ public:
         : ChromePrintContext(frame), m_plugin(plugin), m_pageCount(0), m_printerDPI(printerDPI)
     {
     }
+
+    virtual ~ChromePluginPrintContext() { }
 
     virtual void begin(float width, float height)
     {
