befffdc08ef0a1b6d41d9b83456fb507fba77a03
stanleyw@chromium.org <stanleyw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-04 23:35:54 +0000

    Fixed for when the wifi power strip is not used so it doesn't error out.
    chromeos_wifi.py has been failing because of this.
    
    BUG=None
    TEST=Run chromeos_wifi.py and it won't error out
    
    
    Review URL: http://codereview.chromium.org/7058059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87939 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/chromeos_network.py
+++ chrome/test/pyautolib/chromeos_network.py
@@ -147,7 +147,8 @@ class PyNetworkUITest(pyauto.PyUITest):
   def tearDown(self):
     pyauto.PyUITest.tearDown(self)
     self._PopServiceOrder()
-    self._wifi_power_strip.TurnOffUsedRouters()
+    if self._wifi_power_strip:
+      self._wifi_power_strip.TurnOffUsedRouters()
 
   def _SetServiceOrder(self, service_order):
     self._manager.SetServiceOrder(service_order)
