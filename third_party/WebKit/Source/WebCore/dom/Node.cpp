cbd2b44cc2ddaba214cf43a014f8ef8fa94cb22f
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-06 23:45:22 +0000

    2011-06-06  Emil A Eklund  <eae@chromium.org>
            Reviewed by Eric Seidel.
    
            Convert RenderBox::absoluteRects to IntPoint
            https://bugs.webkit.org/show_bug.cgi?id=62130
    
            Covered by existing tests.
    
            * dom/Node.cpp:
            (WebCore::Node::hasNonEmptyBoundingBox):
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::absoluteRects):
            * rendering/RenderBlock.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::absoluteRects):
            * rendering/RenderBox.h:
            * rendering/RenderInline.cpp:
            (WebCore::RenderInline::absoluteRects):
            * rendering/RenderInline.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::absoluteBoundingBoxRect):
            * rendering/RenderObject.h:
            (WebCore::RenderObject::absoluteRects):
            * rendering/RenderText.cpp:
            (WebCore::RenderText::absoluteRects):
            * rendering/RenderText.h:
            * rendering/RenderView.cpp:
            (WebCore::RenderView::absoluteRects):
            * rendering/RenderView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -840,7 +840,7 @@ bool Node::hasNonEmptyBoundingBox() const
 
     Vector<IntRect> rects;
     FloatPoint absPos = renderer()->localToAbsolute();
-    renderer()->absoluteRects(rects, absPos.x(), absPos.y());
+    renderer()->absoluteRects(rects, flooredIntPoint(absPos));
     size_t n = rects.size();
     for (size_t i = 0; i < n; ++i)
         if (!rects[i].isEmpty())
