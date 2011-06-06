acf8bbc883c0a9a01d5a379b369515d21aa0f21b
polina@google.com <polina@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-06 22:30:43 +0000

    Revert 87919 - Revert 87905 (broke PPAPITest.Scrollbar on win) - PPAPI: Fix interface functions that take PP_CompletionCallbacks, but don't return codes from pp_errors.h
    87905 - Review URL: http://codereview.chromium.org/6975053
    87919 - Review URL: http://codereview.chromium.org/7058061
    
    BUG=85010
    TEST=none
    
    Review URL: http://codereview.chromium.org/6975053
    Review URL: http://codereview.chromium.org/7058061
    
    TBR=dhollowa@chromium.org
    Review URL: http://codereview.chromium.org/7112040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88055 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppb_layer_compositor_impl.cc
+++ webkit/plugins/ppapi/ppb_layer_compositor_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/plugins/ppapi/ppb_layer_compositor_impl.h"
 
+#include "ppapi/c/pp_errors.h"
 #include "webkit/plugins/ppapi/common.h"
 
 namespace webkit {
@@ -41,9 +42,9 @@ void SetDisplay(PP_Resource compositor, PP_Resource layer,
 void MarkAsDirty(PP_Resource compositor, PP_Resource layer) {
 }
 
-PP_Bool SwapBuffers(PP_Resource compositor,
-                       struct PP_CompletionCallback callback) {
-  return PP_FALSE;
+int32_t SwapBuffers(PP_Resource compositor,
+                    struct PP_CompletionCallback callback) {
+  return PP_ERROR_FAILED;
 }
 
 const PPB_LayerCompositor_Dev ppb_layercompositor = {
@@ -98,9 +99,9 @@ void PPB_LayerCompositor_Impl::SetDisplay(PP_Resource layer,
 void PPB_LayerCompositor_Impl::MarkAsDirty(PP_Resource layer) {
 }
 
-PP_Bool PPB_LayerCompositor_Impl::SwapBuffers(
+int32_t PPB_LayerCompositor_Impl::SwapBuffers(
     struct PP_CompletionCallback callback) {
-  return PP_FALSE;
+  return PP_ERROR_FAILED;
 }
 
 }  // namespace ppapi
