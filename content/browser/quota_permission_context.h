12f6951482c84c8d2b2c4b0e8ef96044636325c9
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-06 18:23:25 +0000

    QuotaPermissionContext's dtor must be virtual
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6993056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88005 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/quota_permission_context.h
+++ content/browser/quota_permission_context.h
@@ -33,7 +33,7 @@ class QuotaPermissionContext
 
  protected:
   friend class base::RefCountedThreadSafe<QuotaPermissionContext>;
-  ~QuotaPermissionContext() {}
+  virtual ~QuotaPermissionContext() {}
 };
 
 #endif  // CONTENT_BROWSER_QUOTA_PERMISSION_CONTEXT_H_
