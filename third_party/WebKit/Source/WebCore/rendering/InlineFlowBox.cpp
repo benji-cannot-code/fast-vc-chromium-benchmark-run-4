f0780f16fafb700a22712cb046b8e365f74f8060
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-06 22:27:19 +0000

    2011-06-06  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch InlineFlowBox::paintMask to use IntPoint
            https://bugs.webkit.org/show_bug.cgi?id=62156
    
            Switching InlineFlowBox::paintMask to take an IntPoint representing the paint
            offset instead of a pair of ints.
    
            No new tests as this is simple refactoring.
    
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::paint):
            (WebCore::InlineFlowBox::paintMask):
            * rendering/InlineFlowBox.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/InlineFlowBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/InlineFlowBox.cpp
@@ -992,7 +992,7 @@ void InlineFlowBox::paint(PaintInfo& paintInfo, const IntPoint& paintOffset, int
                     paintInfo.outlineObjects->add(inlineFlow);
             }
         } else if (paintInfo.phase == PaintPhaseMask) {
-            paintMask(paintInfo, paintOffset.x(), paintOffset.y());
+            paintMask(paintInfo, paintOffset);
             return;
         } else {
             // Paint our background, border and box-shadow.
@@ -1164,7 +1164,7 @@ void InlineFlowBox::paintBoxDecorations(PaintInfo& paintInfo, const IntPoint& pa
     }
 }
 
-void InlineFlowBox::paintMask(PaintInfo& paintInfo, int tx, int ty)
+void InlineFlowBox::paintMask(PaintInfo& paintInfo, const IntPoint& paintOffset)
 {
     if (!paintInfo.shouldPaintWithinRoot(renderer()) || renderer()->style()->visibility() != VISIBLE || paintInfo.phase != PaintPhaseMask)
         return;
@@ -1177,8 +1177,7 @@ void InlineFlowBox::paintMask(PaintInfo& paintInfo, int tx, int ty)
     // Move x/y to our coordinates.
     IntRect localRect(frameRect);
     flipForWritingMode(localRect);
-    tx += localRect.x();
-    ty += localRect.y();
+    IntPoint adjustedPaintOffset = paintOffset + localRect.location();
 
     const NinePieceImage& maskNinePieceImage = renderer()->style()->maskBoxImage();
     StyleImage* maskBoxImage = renderer()->style()->maskBoxImage().image();
@@ -1199,7 +1198,7 @@ void InlineFlowBox::paintMask(PaintInfo& paintInfo, int tx, int ty)
         }
     }
 
-    IntRect paintRect = IntRect(IntPoint(tx, ty), frameRect.size());
+    IntRect paintRect = IntRect(adjustedPaintOffset, frameRect.size());
     paintFillLayers(paintInfo, Color(), renderer()->style()->maskLayers(), paintRect, compositeOp);
     
     bool hasBoxImage = maskBoxImage && maskBoxImage->canRender(renderer()->style()->effectiveZoom());
@@ -1209,7 +1208,7 @@ void InlineFlowBox::paintMask(PaintInfo& paintInfo, int tx, int ty)
     // The simple case is where we are the only box for this object.  In those
     // cases only a single call to draw is required.
     if (!prevLineBox() && !nextLineBox()) {
-        boxModelObject()->paintNinePieceImage(paintInfo.context, IntRect(IntPoint(tx, ty), frameRect.size()), renderer()->style(), maskNinePieceImage, compositeOp);
+        boxModelObject()->paintNinePieceImage(paintInfo.context, IntRect(adjustedPaintOffset, frameRect.size()), renderer()->style(), maskNinePieceImage, compositeOp);
     } else {
         // We have a mask image that spans multiple lines.
         // We need to adjust _tx and _ty by the width of all previous lines.
@@ -1219,8 +1218,8 @@ void InlineFlowBox::paintMask(PaintInfo& paintInfo, int tx, int ty)
         int totalLogicalWidth = logicalOffsetOnLine;
         for (InlineFlowBox* curr = this; curr; curr = curr->nextLineBox())
             totalLogicalWidth += curr->logicalWidth();
-        int stripX = tx - (isHorizontal() ? logicalOffsetOnLine : 0);
-        int stripY = ty - (isHorizontal() ? 0 : logicalOffsetOnLine);
+        int stripX = adjustedPaintOffset.x() - (isHorizontal() ? logicalOffsetOnLine : 0);
+        int stripY = adjustedPaintOffset.y() - (isHorizontal() ? 0 : logicalOffsetOnLine);
         int stripWidth = isHorizontal() ? totalLogicalWidth : frameRect.width();
         int stripHeight = isHorizontal() ? frameRect.height() : totalLogicalWidth;
 
