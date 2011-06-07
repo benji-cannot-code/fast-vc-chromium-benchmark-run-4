27aed3becd091b19dfa286d5c28e861eb2811ee5
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 19:43:52 +0000

    In ViewHttpCacheJob, allow Kill() to be called multiple times as required by the URLRequestJob interface spec.
    R=willchan@chromium.org
    BUG=85139
    TEST=testcase.crx attached to bug
    
    
    Review URL: http://codereview.chromium.org/7132004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88179 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/view_http_cache_job_factory.cc
+++ chrome/browser/net/view_http_cache_job_factory.cc
@@ -54,7 +54,6 @@ class ViewHttpCacheJob : public net::URLRequestJob {
 
     // Prevents it from invoking its callback. It will self-delete.
     void Orphan() {
-      DCHECK(user_callback_);
       user_callback_ = NULL;
     }
 
@@ -99,8 +98,10 @@ void ViewHttpCacheJob::Start() {
 
 void ViewHttpCacheJob::Kill() {
   method_factory_.RevokeAll();
-  core_->Orphan();
-  core_ = NULL;
+  if (core_) {
+    core_->Orphan();
+    core_ = NULL;
+  }
   net::URLRequestJob::Kill();
 }
 
