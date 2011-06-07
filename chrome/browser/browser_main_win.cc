c4f66e48dd0fbccaf67f56037dd731e932ab467e
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 23:55:36 +0000

    Back out the core of 85152 CL
    - It turns out that every time we spawn a chrome process we tickle the "dr"
    registry value so there is no point on doing it again every 24 hours.
    
    TEST=none
    BUG=82180
    Review URL: http://codereview.chromium.org/7104033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main_win.cc
+++ chrome/browser/browser_main_win.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/path_service.h"
 #include "base/scoped_native_library.h"
-#include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/windows_version.h"
 #include "base/win/wrapped_window_proc.h"
@@ -30,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/env_vars.h"
 #include "chrome/installer/util/browser_distribution.h"
-#include "chrome/installer/util/google_update_settings.h"
 #include "chrome/installer/util/helper.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/shell_util.h"
@@ -63,38 +61,6 @@ void InitializeWindowProcExceptions() {
   exception_filter = base::win::SetWinProcExceptionFilter(exception_filter);
   DCHECK(!exception_filter);
 }
-
-// BrowserUsageUpdater --------------------------------------------------------
-// This class' job is to update the registry 'dr' value every 24 hours
-// that way google update can accurately track browser usage without
-// undercounting users that do not close chrome for long periods of time.
-class BrowserUsageUpdater : public Task {
- public:
-  virtual ~BrowserUsageUpdater() {}
-
-  virtual void Run() OVERRIDE {
-    if (UpdateUsageRegKey())
-      Track();
-  }
-
-  static void Track() {
-    BrowserThread::PostDelayedTask(
-        BrowserThread::FILE,
-        FROM_HERE, new BrowserUsageUpdater,
-        base::TimeDelta::FromHours(24).InMillisecondsRoundedUp());
-  }
-
- private:
-  bool UpdateUsageRegKey() {
-    FilePath module_dir;
-    if (!PathService::Get(base::DIR_MODULE, &module_dir))
-      return false;
-    bool system_level =
-        !InstallUtil::IsPerUserInstall(module_dir.value().c_str());
-    return GoogleUpdateSettings::UpdateDidRunState(true, system_level);
-  }
-};
-
 }  // namespace
 
 void DidEndMainMessageLoop() {
@@ -332,13 +298,6 @@ class BrowserMainPartsWin : public BrowserMainParts {
       // Make sure that we know how to handle exceptions from the message loop.
       InitializeWindowProcExceptions();
     }
-  }
-
-  virtual void PostMainMessageLoopStart() OVERRIDE {
-    MessageLoop::current()->PostDelayedTask(
-        FROM_HERE,
-        NewRunnableFunction(&BrowserUsageUpdater::Track),
-        base::TimeDelta::FromSeconds(30).InMillisecondsRoundedUp());
   }
 
  private:
