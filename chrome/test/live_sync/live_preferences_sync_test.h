9a4bffaedf3df77bd92d5daca6c3326c8cd4e3ae
braffert@chromium.org <braffert@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 21:10:36 +0000

    Preference sync tests for kAcceptLanguages, kUsesSystemTheme, and kUseCustomChromeFrame.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7024031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/live_preferences_sync_test.h
+++ chrome/test/live_sync/live_preferences_sync_test.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_TEST_LIVE_SYNC_LIVE_PREFERENCES_SYNC_TEST_H_
 #pragma once
 
+#include <string>
 #include "chrome/test/live_sync/live_sync_test.h"
 
 class PrefService;
@@ -43,6 +44,14 @@ class LivePreferencesSyncTest : public LiveSyncTest {
   void ChangeStringPref(int index,
                         const char* pref_name,
                         const std::string& new_value);
+
+  // Modifies the value of the string preference with name |pref_name| in the
+  // profile with index |index| by appending |append_value| to its current
+  // value. Also changes its value in |verifier| if DisableVerifier() hasn't
+  // been called.
+  void AppendStringPref(int index,
+                        const char* pref_name,
+                        const std::string& append_value);
 
   // Changes the value of the file path preference with name |pref_name| in the
   // profile with index |index| to |new_value|. Also changes its value in
