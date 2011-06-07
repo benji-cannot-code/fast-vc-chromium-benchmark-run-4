521ad7d8e99e31d9ce9b16e49d6278e108bb7212
asharif@chromium.org <asharif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 19:42:32 +0000

    Added stddef.h to cmd_buffer_common.h to build with gcc-4.6.0.
    Review URL: http://codereview.chromium.org/7118001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88173 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/common/cmd_buffer_common.h
+++ gpu/command_buffer/common/cmd_buffer_common.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_COMMON_CMD_BUFFER_COMMON_H_
 #define GPU_COMMAND_BUFFER_COMMON_CMD_BUFFER_COMMON_H_
 
+#include <stddef.h>
 #include "../common/types.h"
 #include "../common/bitfield_helpers.h"
 #include "../common/logging.h"
