8e47262636e3c620008fc388988214a7be01718a
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 00:16:51 +0000

    Add #ifdef TOUCH_UI configuration of the bookmark bar height.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7014023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser_shutdown.h"
+#include "chrome/browser/defaults.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -76,12 +77,6 @@ static const int kDetachedTopMargin = 1;  // When attached, we use 0 and let the
 static const int kBottomMargin = 2;
 static const int kLeftMargin = 1;
 static const int kRightMargin = 1;
-
-// Preferred height of the bookmarks bar.
-static const int kBarHeight = 28;
-
-// Preferred height of the bookmarks bar when only shown on the new tab page.
-const int BookmarkBarView::kNewtabBarHeight = 57;
 
 // static
 const char BookmarkBarView::kViewClassName[] =
@@ -643,7 +638,7 @@ gfx::Size BookmarkBarView::GetMinimumSize() {
       overflow_pref.width() + kButtonPadding +
       bookmarks_separator_pref.width() + sync_error_total_width);
 
-  return gfx::Size(width, kBarHeight);
+  return gfx::Size(width, browser_defaults::kBookmarkBarHeight);
 }
 
 void BookmarkBarView::Layout() {
@@ -1660,8 +1655,8 @@ gfx::Size BookmarkBarView::LayoutItems(bool compute_bounds_only) {
     // For the attached appearance, pin the content to the bottom of the bar
     // when animating in/out, as shrinking its height instead looks weird.  This
     // also matches how we layout infobars.
-    y += View::height() - kBarHeight;
-    height += kBarHeight;
+    y += View::height() - browser_defaults::kBookmarkBarHeight;
+    height += browser_defaults::kBookmarkBarHeight;
   }
 
   gfx::Size other_bookmarked_pref =
@@ -1771,12 +1766,17 @@ gfx::Size BookmarkBarView::LayoutItems(bool compute_bounds_only) {
     if (OnNewTabPage()) {
       x += static_cast<int>(
           kNewtabHorizontalPadding * (1 - size_animation_->GetCurrentValue()));
-      prefsize.set_height(kBarHeight +
-          static_cast<int>((kNewtabBarHeight - kBarHeight) *
+      prefsize.set_height(
+          browser_defaults::kBookmarkBarHeight +
+          static_cast<int>(
+              (browser_defaults::kNewtabBookmarkBarHeight -
+               browser_defaults::kBookmarkBarHeight) *
               (1 - size_animation_->GetCurrentValue())));
     } else {
       prefsize.set_height(
-          static_cast<int>(kBarHeight * size_animation_->GetCurrentValue()));
+          static_cast<int>(
+              browser_defaults::kBookmarkBarHeight *
+              size_animation_->GetCurrentValue()));
     }
   }
   return prefsize;
--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/defaults.h"
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -115,7 +116,7 @@ std::string GetNewTabBackgroundCSS(const ui::ThemeProvider* theme_provider,
   // The bar is detached, so we must offset the background by the bar size
   // if it's a top-aligned bar.
 #if defined(OS_WIN) || defined(TOOLKIT_VIEWS)
-  int offset = BookmarkBarView::kNewtabBarHeight;
+  int offset = browser_defaults::kNewtabBookmarkBarHeight;
 #elif defined(OS_MACOSX)
   int offset = bookmarks::kNTPBookmarkBarHeight;
 #elif defined(OS_POSIX)
