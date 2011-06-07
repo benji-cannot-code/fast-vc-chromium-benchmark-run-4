5b967c1a2da89c0d1059a7175f442d6678532e9e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 16:56:12 +0000

    Add code to calculate the dominant color for a favicon.
    Currently we calculate the dominant/representative color only for those favicons we need it for (i.e. the ones shown on the NTP). We don't do any caching either in memory or in the favicon db but that can be tacked on later if deemed suitable.
    
    Code in color_analysis.* authored by dtrainor
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7099001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/most_visited_page.js
+++ chrome/browser/resources/ntp4/most_visited_page.js
@@ -9,6 +9,10 @@ cr.define('ntp4', function() {
   var TilePage = ntp4.TilePage;
 
   /**
+   */
+  var tileID = 0;
+
+  /**
    * Creates a new Most Visited object for tiling.
    * @constructor
    * @extends {HTMLAnchorElement}
@@ -64,6 +68,7 @@ cr.define('ntp4', function() {
 
       this.tabIndex = -1;
       this.data_ = null;
+      this.removeAttribute('id');
     },
 
     /**
@@ -77,6 +82,8 @@ cr.define('ntp4', function() {
         return;
       }
 
+      var id = tileID++;
+      this.setAttribute('id', 'tile' + id);
       this.data_ = data;
       this.tabIndex = 0;
       this.classList.remove('filler');
@@ -85,7 +92,7 @@ cr.define('ntp4', function() {
       var faviconUrl = data.faviconUrl || 'chrome://favicon/' + data.url;
       colorBar.style.backgroundImage = url(faviconUrl);
       colorBar.dir = data.direction;
-      // TODO(estade): add a band of color based on the favicon.
+      chrome.send('getFaviconDominantColor', [faviconUrl, id]);
 
       var title = this.querySelector('.title');
       title.textContent = data.title;
@@ -98,6 +105,11 @@ cr.define('ntp4', function() {
       this.href = data.url;
 
       this.updatePinnedState_();
+    },
+
+    setBarColor: function(r, g, b) {
+      var color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+      // TODO(estade): use color.
     },
 
     /**
@@ -360,8 +372,15 @@ cr.define('ntp4', function() {
     return oldData;
   };
 
+  function setFaviconDominantColor(id, r, g, b) {
+    var tile = $('tile' + id);
+    if (tile)
+      tile.setBarColor(r, g, b);
+  };
+
   return {
     MostVisitedPage: MostVisitedPage,
     refreshData: refreshData,
+    setFaviconDominantColor: setFaviconDominantColor,
   };
 });
