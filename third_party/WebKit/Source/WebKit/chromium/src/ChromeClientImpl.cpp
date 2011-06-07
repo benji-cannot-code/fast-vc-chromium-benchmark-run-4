ea844b47c561c781781a103b1be4e0776d047717
vangelis@chromium.org <vangelis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-07 23:59:14 +0000

    2011-06-07  Vangelis Kokkevis  <vangelis@chromium.org>
            Reviewed by James Robinson.
    
            Add a test to verify that RTL pages that trigger the compositor
            render correctly.
            https://bugs.webkit.org/show_bug.cgi?id=62246
    
            * compositing/geometry/rtl-composited-expected.png: Added.
            * compositing/geometry/rtl-composited-expected.txt: Added.
            * compositing/geometry/rtl-composited.html: Added.
    2011-06-07  Vangelis Kokkevis  <vangelis@chromium.org>
    
            Reviewed by James Robinson.
    
            Disable compositing for RTL pages in the RenderLayerCompositor rather
            than later on in the WebViewImpl.
            https://bugs.webkit.org/show_bug.cgi?id=62246
    
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::allowedCompositingTriggers):
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::layout):
            (WebKit::WebViewImpl::setRootGraphicsLayer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/ChromeClientImpl.cpp
@@ -847,7 +847,8 @@ void ChromeClientImpl::scheduleCompositingLayerSync()
 
 ChromeClient::CompositingTriggerFlags ChromeClientImpl::allowedCompositingTriggers() const
 {
-    if (!m_webView->allowsAcceleratedCompositing())
+    // FIXME: RTL style not supported by the compositor yet.
+    if (!m_webView->allowsAcceleratedCompositing() || m_webView->pageHasRTLStyle())
         return 0;
 
     CompositingTriggerFlags flags = 0;
--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -1037,11 +1037,6 @@ void WebViewImpl::animate()
 void WebViewImpl::layout()
 {
     TRACE_EVENT("WebViewImpl::layout", this, 0);
-#if USE(ACCELERATED_COMPOSITING)
-    // FIXME: RTL style not supported by the compositor yet.
-    if (isAcceleratedCompositingActive() && pageHasRTLStyle())
-        setIsAcceleratedCompositingActive(false);
-#endif
 
     WebFrameImpl* webframe = mainFrameImpl();
     if (webframe) {
@@ -2380,8 +2375,7 @@ bool WebViewImpl::pageHasRTLStyle() const
 
 void WebViewImpl::setRootGraphicsLayer(WebCore::PlatformLayer* layer)
 {
-    // FIXME: RTL style not supported by the compositor yet.
-    setIsAcceleratedCompositingActive(layer && !pageHasRTLStyle() ? true : false);
+    setIsAcceleratedCompositingActive(layer);
     if (m_layerRenderer)
         m_layerRenderer->setRootLayer(layer);
 
