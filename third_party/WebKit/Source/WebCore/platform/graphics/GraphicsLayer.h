b0b04baa3aa91e3c9b032a5d49f4a2ba70d326c2
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-07 04:42:45 +0000

    2011-06-06  No'am Rosenthal  <noam.rosenthal@nokia.com>
            Reviewed by Simon Fraser.
    
            Allow copying of KeyframeValueList.
            https://bugs.webkit.org/show_bug.cgi?id=62171
    
            Make a deep copy of KeyframeValueList, copying each of the keyframes with a virtual clone() function.
    
            No new functionality so no new tests.
    
            * platform/graphics/GraphicsLayer.h:
            (WebCore::FloatAnimationValue::clone):
            (WebCore::TransformAnimationValue::TransformAnimationValue):
            (WebCore::TransformAnimationValue::clone):
            (WebCore::TransformAnimationValue::value):
            (WebCore::KeyframeValueList::KeyframeValueList):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -91,7 +91,6 @@ class TimingFunction;
 // represent values for properties being animated via the GraphicsLayer,
 // without pulling in style-related data from outside of the platform directory.
 class AnimationValue {
-    WTF_MAKE_NONCOPYABLE(AnimationValue); WTF_MAKE_FAST_ALLOCATED;
 public:
     AnimationValue(float keyTime, PassRefPtr<TimingFunction> timingFunction = 0)
         : m_keyTime(keyTime)
@@ -104,6 +103,7 @@ public:
 
     float keyTime() const { return m_keyTime; }
     const TimingFunction* timingFunction() const { return m_timingFunction.get(); }
+    virtual AnimationValue* clone() const = 0;
 
 private:
     float m_keyTime;
@@ -118,6 +118,7 @@ public:
         , m_value(value)
     {
     }
+    virtual AnimationValue* clone() const { return new FloatAnimationValue(*this); }
 
     float value() const { return m_value; }
 
@@ -132,26 +133,33 @@ public:
         : AnimationValue(keyTime, timingFunction)
     {
         if (value)
-            m_value = adoptPtr(new TransformOperations(*value));
+            m_value = *value;
     }
+    virtual AnimationValue* clone() const { return new TransformAnimationValue(*this); }
 
-    const TransformOperations* value() const { return m_value.get(); }
+    const TransformOperations* value() const { return &m_value; }
 
 private:
-    OwnPtr<TransformOperations> m_value;
+    TransformOperations m_value;
 };
 
 // Used to store a series of values in a keyframe list. Values will all be of the same type,
 // which can be inferred from the property.
 class KeyframeValueList {
-    WTF_MAKE_NONCOPYABLE(KeyframeValueList); WTF_MAKE_FAST_ALLOCATED;
 public:
 
     KeyframeValueList(AnimatedPropertyID property)
         : m_property(property)
     {
     }
-    
+
+    KeyframeValueList(const KeyframeValueList& other)
+        : m_property(other.property())
+    {
+        for (size_t i = 0; i < other.m_values.size(); ++i)
+            m_values.append(other.m_values[i]->clone());
+    }
+
     ~KeyframeValueList()
     {
         deleteAllValues(m_values);
