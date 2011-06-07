f2f556d4338d119cb2278ec96e21b1ecaf083e23
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 00:17:13 +0000

    Fix a null pointer crasher.
    BUG=84616
    Review URL: http://codereview.chromium.org/7054077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88063 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_update_job.cc
+++ webkit/appcache/appcache_update_job.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1181,7 +1181,8 @@ void AppCacheUpdateJob::OnResponseInfoLoaded(
     const std::string name = "vary";
     std::string value;
     void* iter = NULL;
-    if (http_info->headers->RequiresValidation(http_info->request_time,
+    if (!http_info->headers ||
+        http_info->headers->RequiresValidation(http_info->request_time,
                                                http_info->response_time,
                                                base::Time::Now()) ||
         http_info->headers->EnumerateHeader(&iter, name, &value)) {
