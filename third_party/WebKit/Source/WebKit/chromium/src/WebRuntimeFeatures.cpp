267bc3af69a9d85b4f2e48c3227970cb0658f4a1
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-08 13:57:12 +0000

    2011-06-08  Ilya Tikhonovsky  <loislo@chromium.org>
            Unreviewed chromium build fix for r88332.
    
            * src/WebRuntimeFeatures.cpp:
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebMediaPlayerClientImpl.h"
 #include "WebSocket.h"
 
+#include <wtf/UnusedParam.h>
+
 using namespace WebCore;
 
 namespace WebKit {
