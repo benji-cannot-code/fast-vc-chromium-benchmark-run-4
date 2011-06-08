53ed5aafe4d0247c2fb7b646ac8a3f3b688d9b6f
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 16:34:07 +0000

    Convert NativeTextFieldViews context menu from Menu2 to MenuItemView.
    BUG=chromium-os:13887
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7104013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88340 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/native_textfield_views.cc
+++ views/controls/textfield/native_textfield_views.cc
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/background.h"
 #include "views/border.h"
 #include "views/controls/focusable_border.h"
-#include "views/controls/menu/menu_2.h"
+#include "views/controls/menu/menu_item_view.h"
+#include "views/controls/menu/menu_model_adapter.h"
 #include "views/controls/textfield/text_style.h"
 #include "views/controls/textfield/textfield.h"
 #include "views/controls/textfield/textfield_controller.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/metrics.h"
 #include "views/views_delegate.h"
 #include "views/widget/widget.h"
+#include "views/window/window.h"
 
 #if defined(OS_LINUX)
 #include "ui/gfx/gtk_util.h"
@@ -259,8 +261,12 @@ gfx::NativeCursor NativeTextfieldViews::GetCursor(const MouseEvent& event) {
 void NativeTextfieldViews::ShowContextMenuForView(View* source,
                                                   const gfx::Point& p,
                                                   bool is_mouse_gesture) {
-  InitContextMenuIfRequired();
-  context_menu_menu_->RunContextMenuAt(p);
+  UpdateContextMenu();
+  context_menu_menu_->RunMenuAt(GetWindow()->GetNativeWindow(),
+                                NULL,
+                                gfx::Rect(p, gfx::Size()),
+                                views::MenuItemView::TOPLEFT,
+                                true);
 }
 
 /////////////////////////////////////////////////////////////////
@@ -1034,18 +1040,24 @@ void NativeTextfieldViews::UpdateAfterChange(bool text_changed,
   }
 }
 
-void NativeTextfieldViews::InitContextMenuIfRequired() {
-  if (context_menu_menu_.get())
-    return;
-  context_menu_contents_.reset(new ui::SimpleMenuModel(this));
-  context_menu_contents_->AddItemWithStringId(IDS_APP_CUT, IDS_APP_CUT);
-  context_menu_contents_->AddItemWithStringId(IDS_APP_COPY, IDS_APP_COPY);
-  context_menu_contents_->AddItemWithStringId(IDS_APP_PASTE, IDS_APP_PASTE);
-  context_menu_contents_->AddItemWithStringId(IDS_APP_DELETE, IDS_APP_DELETE);
-  context_menu_contents_->AddSeparator();
-  context_menu_contents_->AddItemWithStringId(IDS_APP_SELECT_ALL,
-                                              IDS_APP_SELECT_ALL);
-  context_menu_menu_.reset(new Menu2(context_menu_contents_.get()));
+void NativeTextfieldViews::UpdateContextMenu() {
+  if (!context_menu_contents_.get()) {
+    context_menu_contents_.reset(new ui::SimpleMenuModel(this));
+    context_menu_contents_->AddItemWithStringId(IDS_APP_CUT, IDS_APP_CUT);
+    context_menu_contents_->AddItemWithStringId(IDS_APP_COPY, IDS_APP_COPY);
+    context_menu_contents_->AddItemWithStringId(IDS_APP_PASTE, IDS_APP_PASTE);
+    context_menu_contents_->AddItemWithStringId(IDS_APP_DELETE, IDS_APP_DELETE);
+    context_menu_contents_->AddSeparator();
+    context_menu_contents_->AddItemWithStringId(IDS_APP_SELECT_ALL,
+                                                IDS_APP_SELECT_ALL);
+
+    context_menu_delegate_.reset(
+        new views::MenuModelAdapter(context_menu_contents_.get()));
+    context_menu_menu_.reset(
+        new views::MenuItemView(context_menu_delegate_.get()));
+  }
+
+  context_menu_delegate_->BuildMenu(context_menu_menu_.get());
 }
 
 void NativeTextfieldViews::OnTextInputTypeChanged() {
--- views/controls/textfield/native_textfield_views.h
+++ views/controls/textfield/native_textfield_views.h
@@ -29,7 +29,8 @@ namespace views {
 
 class FocusableBorder;
 class KeyEvent;
-class Menu2;
+class MenuItemView;
+class MenuModelAdapter;
 
 // A views/skia only implementation of NativeTextfieldWrapper.
 // No platform specific code is used.
@@ -207,8 +208,8 @@ class NativeTextfieldViews : public View,
   // changed.
   void UpdateAfterChange(bool text_changed, bool cursor_changed);
 
-  // Utility function to create the context menu if one does not already exist.
-  void InitContextMenuIfRequired();
+  // Utility function to prepare the context menu..
+  void UpdateContextMenu();
 
   // Convenience method to call InputMethod::OnTextInputTypeChanged();
   void OnTextInputTypeChanged();
@@ -267,7 +268,8 @@ class NativeTextfieldViews : public View,
 
   // Context menu and its content list for the textfield.
   scoped_ptr<ui::SimpleMenuModel> context_menu_contents_;
-  scoped_ptr<Menu2> context_menu_menu_;
+  scoped_ptr<views::MenuModelAdapter> context_menu_delegate_;
+  scoped_ptr<views::MenuItemView> context_menu_menu_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeTextfieldViews);
 };
