21aa33f66f95508e7c6dfaf49618d29e9d618889
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 01:01:10 +0000

    views: Use vector<T>::operator[] instead of vector<T>::at in the remaining cases.
    The implementation of vector<T>::at performs bounds checks and can through an
    exception std::out_of_range if you try to access an element that is not present
    in the array. That is less eficient and we don't want to through an exception
    anyway.
    
    BUG=None
    TEST=None
    
    R=pkasting@chromium.org,sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7125012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88254 0039d316-1c4b-4281-b951-d872f2087c98

--- views/accelerator.cc
+++ views/accelerator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -112,7 +112,7 @@ string16 Accelerator::GetShortcutText() const {
   string16 shortcut_rtl;
   bool adjust_shortcut_for_rtl = false;
   if (base::i18n::IsRTL() && shortcut.length() == 1 &&
-      !IsAsciiAlpha(shortcut.at(0)) && !IsAsciiDigit(shortcut.at(0))) {
+      !IsAsciiAlpha(shortcut[0]) && !IsAsciiDigit(shortcut[0])) {
     adjust_shortcut_for_rtl = true;
     shortcut_rtl.assign(shortcut);
   }
