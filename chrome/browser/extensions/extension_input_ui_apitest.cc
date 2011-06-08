7bf269c27ae35a6bda2f6db03cfe5b5fc271628b
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 20:45:01 +0000

    Does not crash with unexpected function in debug build.
    Add apitest for InputUI extension API.
    
    BUG=Crashes when extension calls a undefined function
    TEST=Manually
    
    Review URL: http://codereview.chromium.org/7104002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88390 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/extension_input_ui_apitest.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/test/ui_test_utils.h"
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, InputUI) {
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableExperimentalExtensionApis);
+#if defined(OS_CHROMEOS) && defined(TOUCH_UI)
+  ASSERT_TRUE(RunExtensionTest("input_ui/chromeos_touchui")) << message_;
+#else
+  ASSERT_TRUE(RunExtensionTest("input_ui/other")) << message_;
+#endif
+}
