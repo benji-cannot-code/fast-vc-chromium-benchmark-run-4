4dc706af06722c9fa8d9b8e12dc7cef883cf4ce9
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 13:43:28 +0000

    Landing http://codereview.chromium.org/7033062/: Replaced ptrdiff_t with std::ptrdiff_t in all files. Review URL: http://codereview.chromium.org/7046043
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88328 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/malloc_hook.cc
+++ third_party/tcmalloc/chromium/src/malloc_hook.cc
@@ -103,10 +103,10 @@ extern void InitialMallocHook_MMap(const void* result,
                                    off_t offset);
 
 ATTRIBUTE_WEAK
-extern void InitialMallocHook_PreSbrk(ptrdiff_t increment);
+extern void InitialMallocHook_PreSbrk(std::ptrdiff_t increment);
 
 ATTRIBUTE_WEAK
-extern void InitialMallocHook_Sbrk(const void* result, ptrdiff_t increment);
+extern void InitialMallocHook_Sbrk(const void* result, std::ptrdiff_t increment);
 
 namespace base { namespace internal {
 template<typename PtrT>
@@ -241,12 +241,12 @@ void InitialMallocHook_MMap(const void* result,
     MallocHook::SetMmapHook(NULL);
 }
 
-void InitialMallocHook_PreSbrk(ptrdiff_t increment) {
+void InitialMallocHook_PreSbrk(std::ptrdiff_t increment) {
   if (MallocHook::GetPreSbrkHook() == &InitialMallocHook_PreSbrk)
     MallocHook::SetPreSbrkHook(NULL);
 }
 
-void InitialMallocHook_Sbrk(const void* result, ptrdiff_t increment) {
+void InitialMallocHook_Sbrk(const void* result, std::ptrdiff_t increment) {
   if (MallocHook::GetSbrkHook() == &InitialMallocHook_Sbrk)
     MallocHook::SetSbrkHook(NULL);
 }
@@ -450,7 +450,7 @@ extern "C" {
   void* mremap(void* old_addr, size_t old_size, size_t new_size,
                int flags, ...) __THROW
     ATTRIBUTE_SECTION(malloc_hook);
-  void* sbrk(ptrdiff_t increment) __THROW
+  void* sbrk(std::ptrdiff_t increment) __THROW
     ATTRIBUTE_SECTION(malloc_hook);
 }
 
@@ -493,9 +493,9 @@ extern "C" void* mremap(void* old_addr, size_t old_size, size_t new_size,
 }
 
 // libc's version:
-extern "C" void* __sbrk(ptrdiff_t increment);
+extern "C" void* __sbrk(std::ptrdiff_t increment);
 
-extern "C" void* sbrk(ptrdiff_t increment) __THROW {
+extern "C" void* sbrk(std::ptrdiff_t increment) __THROW {
   MallocHook::InvokePreSbrkHook(increment);
   void *result = __sbrk(increment);
   MallocHook::InvokeSbrkHook(result, increment);
