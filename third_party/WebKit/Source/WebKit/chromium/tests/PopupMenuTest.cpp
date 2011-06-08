f631e743607ab2b7e34ea901320528805be6a7de
koz@chromium.org <koz@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-08 03:22:37 +0000

    2011-06-07  James Kozianski  <koz@chromium.org>
            [Chromium] Unreviewed. Disable SelectItemEvenFire that fails on
            chromium canaries.
    
            * tests/PopupMenuTest.cpp:
            (WebKit::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
@@ -403,7 +403,7 @@ TEST_F(SelectPopupMenuTest, SelectItemWithKeyboardItemClickOutside)
     EXPECT_EQ(2, selectedIndex());
 }
 
-TEST_F(SelectPopupMenuTest, SelectItemEventFire)
+TEST_F(SelectPopupMenuTest, DISABLED_SelectItemEventFire)
 {
     registerMockedURLLoad("select_event.html");
     m_webView->settings()->setJavaScriptEnabled(true);
