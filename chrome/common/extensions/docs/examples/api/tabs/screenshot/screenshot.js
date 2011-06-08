e2c52df62f03a9c0348491bb58f147487be3c5b9
smus@chromium.org <smus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 17:25:00 +0000

    Added license header
    Updated broken screenshot sample to use domain permissions (only works for code.google.com)
    
    Review URL: http://codereview.chromium.org/7003059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/api/tabs/screenshot/screenshot.js
+++ chrome/common/extensions/docs/examples/api/tabs/screenshot/screenshot.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // To make sure we can uniquely identify each screenshot tab, add an id as a
 // query param to the url that displays the screenshot.
 var id = 100;
@@ -45,5 +49,9 @@ function takeScreenshot() {
 
 // Listen for a click on the camera icon.  On that click, take a screenshot.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  takeScreenshot();
+  if (tab.url.match(/code.google.com/)) {
+    takeScreenshot();
+  } else {
+    alert('This sample can only take screenshots of code.google.com pages');
+  }
 });
