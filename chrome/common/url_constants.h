72b1ddbb05731932c297927c828fb609e4c9d8bb
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 18:13:34 +0000

    Adding chrome://sessions webui for prototyping tab sync NTP stuff.
    This adds the necessary plumbing for chrome://sessions webui page which shows a hierarchy of sessions and their windows and tabs as well as a "magic" list of suggested tabs. The magic list is currently just the last 10 tabs by timestamp for now - but can later be changed to prototype other heuristics.
    
    BUG=none
    TEST=manual
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=88334
    Review URL: http://codereview.chromium.org/6969016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -66,6 +66,7 @@ extern const char kChromeUIKeyboardURL[];
 extern const char kChromeUINewTabURL[];
 extern const char kChromeUIPluginsURL[];
 extern const char kChromeUIPrintURL[];
+extern const char kChromeUISessionsURL[];
 extern const char kChromeUISettingsURL[];
 extern const char kChromeUITextfieldsURL[];
 
@@ -115,6 +116,7 @@ extern const char kChromeUIPluginsHost[];
 extern const char kChromeUIPrintHost[];
 extern const char kChromeUIResourcesHost[];
 extern const char kChromeUIScreenshotPath[];
+extern const char kChromeUISessionsHost[];
 extern const char kChromeUISettingsHost[];
 extern const char kChromeUISyncInternalsHost[];
 extern const char kChromeUISyncResourcesHost[];
