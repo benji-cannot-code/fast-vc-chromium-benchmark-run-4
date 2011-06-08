d8798cd92f6385c0d69712f0644fbc12fbe3bf23
garykac@google.com <garykac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-08 21:32:14 +0000

    Add basic debug log to host page.
    BUG=none
    TEST=manual
    Review URL: http://codereview.chromium.org/7108007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88409 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/webapp/me2mom/debug_log.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Maximum numer of lines to record in the debug log.
+// Only the most recent <n> lines are displayed.
+var MAX_DEBUG_LOG_SIZE = 1000;
+
+function toggleDebugLog() {
+  debugLog = document.getElementById('debug-log');
+  toggleButton = document.getElementById('debug-log-toggle');
+
+  if (!debugLog.style.display || debugLog.style.display == 'none') {
+    debugLog.style.display = 'block';
+    toggleButton.value = 'Hide Debug Log';
+  } else {
+    debugLog.style.display = 'none';
+    toggleButton.value = 'Show Debug Log';
+  }
+}
+
+/**
+ * Add the given message to the debug log.
+ *
+ * @param {string} message The debug info to add to the log.
+ */
+function addToDebugLog(message) {
+  var debugLog = document.getElementById('debug-log');
+
+  // Remove lines from top if we've hit our max log size.
+  if (debugLog.childNodes.length == MAX_DEBUG_LOG_SIZE) {
+    debugLog.removeChild(debugLog.firstChild);
+  }
+
+  // Add the new <p> to the end of the debug log.
+  var p = document.createElement('p');
+  p.appendChild(document.createTextNode(message));
+  debugLog.appendChild(p);
+
+  // Scroll to bottom of div
+  debugLog.scrollTop = debugLog.scrollHeight;
+}
--- remoting/webapp/me2mom/remoting_session.js
+++ remoting/webapp/me2mom/remoting_session.js
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Maximum numer of lines to record in the debug log.
-// Only the most recent <n> lines are displayed.
-var MAX_DEBUG_LOG_SIZE = 1000;
-
 var remoting = chrome.extension.getBackgroundPage().remoting;
 
 // Chromoting session API version (for this javascript).
@@ -171,19 +167,6 @@ function init_() {
 
 }
 
-function toggleDebugLog() {
-  debugLog = document.getElementById('debug-log');
-  toggleButton = document.getElementById('debug-log-toggle');
-
-  if (!debugLog.style.display || debugLog.style.display == 'none') {
-    debugLog.style.display = 'block';
-    toggleButton.value = 'Hide Debug Log';
-  } else {
-    debugLog.style.display = 'none';
-    toggleButton.value = 'Show Debug Log';
-  }
-}
-
 function toggleScaleToFit() {
   remoting.scaleToFit = !remoting.scaleToFit;
   document.getElementById('scale-to-fit-toggle').value =
@@ -312,28 +295,6 @@ function fade(name, id, val, delta, delay) {
  */
 function debugInfoCallback(msg) {
   addToDebugLog('plugin: ' + msg);
-}
-
-/**
- * Add the given message to the debug log.
- *
- * @param {string} message The debug info to add to the log.
- */
-function addToDebugLog(message) {
-  var debugLog = document.getElementById('debug-log');
-
-  // Remove lines from top if we've hit our max log size.
-  if (debugLog.childNodes.length == MAX_DEBUG_LOG_SIZE) {
-    debugLog.removeChild(debugLog.firstChild);
-  }
-
-  // Add the new <p> to the end of the debug log.
-  var p = document.createElement('p');
-  p.appendChild(document.createTextNode(message));
-  debugLog.appendChild(p);
-
-  // Scroll to bottom of div
-  debugLog.scrollTop = debugLog.scrollHeight;
 }
 
 function updateStatusBarStats() {
