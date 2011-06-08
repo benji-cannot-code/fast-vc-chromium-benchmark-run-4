cd1e0acea90be5bbfa08539985966ac06e2e5782
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-08 21:27:51 +0000

    2011-06-08  Kenneth Russell  <kbr@google.com>
            Reviewed by Adam Barth.
    
            Disallow use of cross-domain media (images, video) in WebGL
            https://bugs.webkit.org/show_bug.cgi?id=62257
    
            Updated origin-clean-conformance.html to track upstream version in
            Khronos repository. Added new layout tests mirroring those added
            in bug 61015 which verify that new CORS support for images is
            working in the context of WebGL.
    
            Verified new tests in WebKit and Chromium.
    
            * http/tests/canvas/webgl/origin-clean-conformance-expected.txt:
            * http/tests/canvas/webgl/origin-clean-conformance.html:
            * http/tests/security/webgl-remote-read-remote-image-allowed-expected.txt: Added.
            * http/tests/security/webgl-remote-read-remote-image-allowed-with-credentials-expected.txt: Added.
            * http/tests/security/webgl-remote-read-remote-image-allowed-with-credentials.html: Added.
            * http/tests/security/webgl-remote-read-remote-image-allowed.html: Added.
            * http/tests/security/webgl-remote-read-remote-image-blocked-no-crossorigin-expected.txt: Added.
            * http/tests/security/webgl-remote-read-remote-image-blocked-no-crossorigin.html: Added.
            * platform/mac-wk2/Skipped:
    2011-06-08  Kenneth Russell  <kbr@google.com>
    
            Reviewed by Adam Barth.
    
            Disallow use of cross-domain media (images, video) in WebGL
            https://bugs.webkit.org/show_bug.cgi?id=62257
    
            Updated WebGL implementation to track recent spec updates in this area.
    
            Tests: http/tests/security/webgl-remote-read-remote-image-allowed-with-credentials.html
                   http/tests/security/webgl-remote-read-remote-image-allowed.html
                   http/tests/security/webgl-remote-read-remote-image-blocked-no-crossorigin.html
    
            * html/canvas/CanvasRenderingContext.cpp:
            (WebCore::CanvasRenderingContext::wouldTaintOrigin):
            (WebCore::CanvasRenderingContext::checkOrigin):
            * html/canvas/CanvasRenderingContext.h:
            (WebCore::CanvasRenderingContext::checkOrigin):
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::WebGLRenderingContext::readPixels):
            (WebCore::WebGLRenderingContext::texImage2D):
            (WebCore::WebGLRenderingContext::videoFrameToImage):
            (WebCore::WebGLRenderingContext::texSubImage2D):
            * html/canvas/WebGLRenderingContext.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88387 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/WebGLRenderingContext.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -2808,14 +2808,13 @@ void WebGLRenderingContext::polygonOffset(GC3Dfloat factor, GC3Dfloat units)
     cleanupAfterGraphicsCall(false);
 }
 
-void WebGLRenderingContext::readPixels(GC3Dint x, GC3Dint y, GC3Dsizei width, GC3Dsizei height, GC3Denum format, GC3Denum type, ArrayBufferView* pixels, ExceptionCode& ec)
+void WebGLRenderingContext::readPixels(GC3Dint x, GC3Dint y, GC3Dsizei width, GC3Dsizei height, GC3Denum format, GC3Denum type, ArrayBufferView* pixels, ExceptionCode&)
 {
     if (isContextLost())
         return;
-    if (!canvas()->originClean()) {
-        ec = SECURITY_ERR;
-        return;
-    }
+    // Due to WebGL's same-origin restrictions, it is not possible to
+    // taint the origin using the WebGL API.
+    ASSERT(canvas()->originClean());
     // Validate input parameters.
     if (!pixels) {
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
@@ -3158,7 +3157,11 @@ void WebGLRenderingContext::texImage2D(GC3Denum target, GC3Dint level, GC3Denum 
         return;
     if (!validateHTMLImageElement(image))
         return;
-    checkOrigin(image);
+    if (wouldTaintOrigin(image)) {
+        ec = SECURITY_ERR;
+        return;
+    }
+
     texImage2DImpl(target, level, internalformat, format, type, image->cachedImage()->image(),
                    m_unpackFlipY, m_unpackPremultiplyAlpha, ec);
 }
@@ -3173,7 +3176,10 @@ void WebGLRenderingContext::texImage2D(GC3Denum target, GC3Dint level, GC3Denum 
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
         return;
     }
-    checkOrigin(canvas);
+    if (wouldTaintOrigin(canvas)) {
+        ec = SECURITY_ERR;
+        return;
+    }
     RefPtr<ImageData> imageData = canvas->getImageData();
     if (imageData)
         texImage2D(target, level, internalformat, format, type, imageData.get(), ec);
@@ -3183,7 +3189,7 @@ void WebGLRenderingContext::texImage2D(GC3Denum target, GC3Dint level, GC3Denum 
 }
 
 #if ENABLE(VIDEO)
-PassRefPtr<Image> WebGLRenderingContext::videoFrameToImage(HTMLVideoElement* video)
+PassRefPtr<Image> WebGLRenderingContext::videoFrameToImage(HTMLVideoElement* video, ExceptionCode& ec)
 {
     if (!video || !video->videoWidth() || !video->videoHeight()) {
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
@@ -3195,7 +3201,10 @@ PassRefPtr<Image> WebGLRenderingContext::videoFrameToImage(HTMLVideoElement* vid
         m_context->synthesizeGLError(GraphicsContext3D::OUT_OF_MEMORY);
         return 0;
     }
-    checkOrigin(video);
+    if (wouldTaintOrigin(video)) {
+        ec = SECURITY_ERR;
+        return 0;
+    }
     IntRect destRect(0, 0, size.width(), size.height());
     // FIXME: Turn this into a GPU-GPU texture copy instead of CPU readback.
     video->paintCurrentFrameInContext(buf->context(), destRect);
@@ -3208,8 +3217,8 @@ void WebGLRenderingContext::texImage2D(GC3Denum target, GC3Dint level, GC3Denum 
     ec = 0;
     if (isContextLost())
         return;
-    RefPtr<Image> image = videoFrameToImage(video);
-    if (!video)
+    RefPtr<Image> image = videoFrameToImage(video, ec);
+    if (!image)
         return;
     texImage2DImpl(target, level, internalformat, format, type, image.get(), m_unpackFlipY, m_unpackPremultiplyAlpha, ec);
 }
@@ -3350,7 +3359,10 @@ void WebGLRenderingContext::texSubImage2D(GC3Denum target, GC3Dint level, GC3Din
         return;
     if (!validateHTMLImageElement(image))
         return;
-    checkOrigin(image);
+    if (wouldTaintOrigin(image)) {
+        ec = SECURITY_ERR;
+        return;
+    }
     texSubImage2DImpl(target, level, xoffset, yoffset, format, type, image->cachedImage()->image(),
                       m_unpackFlipY, m_unpackPremultiplyAlpha, ec);
 }
@@ -3365,7 +3377,10 @@ void WebGLRenderingContext::texSubImage2D(GC3Denum target, GC3Dint level, GC3Din
         m_context->synthesizeGLError(GraphicsContext3D::INVALID_VALUE);
         return;
     }
-    checkOrigin(canvas);
+    if (wouldTaintOrigin(canvas)) {
+        ec = SECURITY_ERR;
+        return;
+    }
     RefPtr<ImageData> imageData = canvas->getImageData();
     if (imageData)
         texSubImage2D(target, level, xoffset, yoffset, format, type, imageData.get(), ec);
@@ -3381,8 +3396,8 @@ void WebGLRenderingContext::texSubImage2D(GC3Denum target, GC3Dint level, GC3Din
     ec = 0;
     if (isContextLost())
         return;
-    RefPtr<Image> image = videoFrameToImage(video);
-    if (!video)
+    RefPtr<Image> image = videoFrameToImage(video, ec);
+    if (!image)
         return;
     texSubImage2DImpl(target, level, xoffset, yoffset, format, type, image.get(), m_unpackFlipY, m_unpackPremultiplyAlpha, ec);
 }
