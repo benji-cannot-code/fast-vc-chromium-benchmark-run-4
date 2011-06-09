173dc697bea7b9dc7e809857a9c5faa956420bf2
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-09 09:36:48 +0000

    2011-06-09  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Disable running layout tests on Mac EWS bots (and fix cr-linux bots to run them again)
            https://bugs.webkit.org/show_bug.cgi?id=62362
    
            The mac bots are failing inspector tests.  We don't know why yet.
            So for now, turning run-webkit-tests off for the mac-ews.
    
            Also noticed that due to the wrong variable name the cr-linux
            bots weren't running tests!
    
            * Scripts/webkitpy/tool/commands/earlywarningsystem.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88437 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/earlywarningsystem.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/earlywarningsystem.py
@@ -180,7 +180,7 @@ class ChromiumLinuxEWS(AbstractChromiumEWS):
     #        a database migration. :(
     name = "chromium-ews"
     port_name = "chromium-xvfb"
-    run_tests = True
+    _run_tests = True
 
 
 class ChromiumWindowsEWS(AbstractChromiumEWS):
@@ -208,4 +208,3 @@ class ChromiumMacEWS(AbstractChromiumEWS, AbstractCommitterOnlyEWS):
 class MacEWS(AbstractCommitterOnlyEWS):
     name = "mac-ews"
     port_name = "mac"
-    _run_tests = True
