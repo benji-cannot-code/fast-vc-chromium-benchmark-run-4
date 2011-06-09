9c656e0164cd1a400d0fcb311f6d5600bbeb0f93
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-09 20:33:20 +0000

    2011-06-09  Julien Chaffraix  <jchaffraix@codeaurora.org>
            Reviewed by Antti Koivisto.
    
            REGRESSION(84329): Stylesheets on some pages do not load
            https://bugs.webkit.org/show_bug.cgi?id=61400
    
            Adding test to cover the regression. The test actually uncovered
            a bug in the way we handle alternate stylesheet and thus is
            failing some parts.
    
            * fast/css/link-disabled-attr-expected.txt: Added.
            * fast/css/link-disabled-attr.html: Added.
    2011-06-09  Julien Chaffraix  <jchaffraix@codeaurora.org>
    
            Reviewed by Antti Koivisto.
    
            REGRESSION(84329): Stylesheets on some pages do not load
            https://bugs.webkit.org/show_bug.cgi?id=61400
    
            Test: fast/css/link-disabled-attr.html
    
            Fixed r84329: the change did not take into account the fact
            that HTMLLinkElement did already contain the disabled information
            and the 2 information were not linked as they should have!
    
            The new logic pushes the information to the stylesheet as this
            is what the spec mandates and what FF is doing. Also it keeps
            one bit of information (that JS enabled the stylesheet) as it
            is needed for the recalcStyleSelector logic.
    
            * dom/Document.cpp:
            (WebCore::Document::recalcStyleSelector): s/isDisabled/disabled.
    
            * html/HTMLLinkElement.cpp:
            (WebCore::HTMLLinkElement::HTMLLinkElement): Removed m_disabledState,
            replaced by m_isEnabledViaScript.
            (WebCore::HTMLLinkElement::setDisabled): Updated the logic after
            m_disabledState removal. It also matches the spec by forwarding
            the disabled state to our stylesheet if we have one.
            (WebCore::HTMLLinkElement::parseMappedAttribute): Removed harmful
            handling of the disabledAttr.
            (WebCore::HTMLLinkElement::process): Updated after m_disabledState removal.
            * html/HTMLLinkElement.h:
            (WebCore::HTMLLinkElement::isEnabledViaScript): Ditto.
            (WebCore::HTMLLinkElement::isAlternate): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLLinkElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLLinkElement.h
@@ -76,10 +76,9 @@ public:
 
     StyleSheet* sheet() const;
 
+    // FIXME: This should be remaned isStyleSheetLoading as this is only used for stylesheets.
     bool isLoading() const;
-
-    bool isDisabled() const { return m_disabledState == Disabled; }
-    bool isEnabledViaScript() const { return m_disabledState == EnabledViaScript; }
+    bool isEnabledViaScript() const { return m_isEnabledViaScript; }
     bool disabled() const;
     void setDisabled(bool);
 
@@ -104,10 +103,8 @@ private:
     virtual bool sheetLoaded();
     virtual void startLoadingDynamicSheet();
 
-    bool isAlternate() const { return m_disabledState == Unset && m_relAttribute.m_isAlternate; }
+    bool isAlternate() const { return m_relAttribute.m_isAlternate; }
     
-    void setDisabledState(bool _disabled);
-
     virtual bool isURLAttribute(Attribute*) const;
 
 public:
@@ -125,12 +122,6 @@ private:
 private:
     HTMLLinkElement(const QualifiedName&, Document*, bool createdByParser);
 
-    enum DisabledState {
-        Unset,
-        EnabledViaScript,
-        Disabled
-    };
-
     CachedResourceHandle<CachedCSSStyleSheet> m_cachedSheet;
     RefPtr<CSSStyleSheet> m_sheet;
 #if ENABLE(LINK_PREFETCH)
@@ -140,9 +131,9 @@ private:
     KURL m_url;
     String m_type;
     String m_media;
-    DisabledState m_disabledState;
     RelAttribute m_relAttribute;
     bool m_loading;
+    bool m_isEnabledViaScript;
     bool m_createdByParser;
     bool m_isInShadowTree;
     
