d1199f74e4bb5669ddc184b2bad862e0e7d7d7a7
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 13:31:29 +0000

    Adding a new window type for frameless non-transient top level windows, used by ChromeOS' window manager.
    BUG=chromium-os:16323
    TEST=none
    Review URL: http://codereview.chromium.org/7129033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88523 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_win.cc
+++ views/widget/native_widget_win.cc
@@ -1784,6 +1784,9 @@ void NativeWidgetWin::SetInitParams(const Widget::InitParams& params) {
     case Widget::InitParams::TYPE_WINDOW:
     case Widget::InitParams::TYPE_CONTROL:
       break;
+    case Widget::InitParams::TYPE_WINDOW_FRAMELESS:
+      style |= WS_POPUP;
+      break;
     case Widget::InitParams::TYPE_POPUP:
       style |= WS_POPUP;
       ex_style |= WS_EX_TOOLWINDOW;
