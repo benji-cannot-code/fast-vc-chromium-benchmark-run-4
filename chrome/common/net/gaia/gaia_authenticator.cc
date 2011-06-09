af62d129a39cea78bc04f3a0fafa5fcf2eac277c
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 08:35:07 +0000

    Escaping file names correctly. Also fixed a crush in chromeos debug build while saving a web page.
    BUG=chromium-os:13130
    TEST=Save a file with special characters like '#', '&', '"'.
    
    
    Review URL: http://codereview.chromium.org/7057053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88510 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/gaia/gaia_authenticator.cc
+++ chrome/common/net/gaia/gaia_authenticator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -159,13 +159,15 @@ bool GaiaAuthenticator::PerformGaiaRequest(const AuthParams& params,
   GURL gaia_auth_url(gaia_url_);
 
   string post_body;
-  post_body += "Email=" + EscapeUrlEncodedData(params.email);
-  post_body += "&Passwd=" + EscapeUrlEncodedData(params.password);
-  post_body += "&source=" + EscapeUrlEncodedData(user_agent_);
+  post_body += "Email=" + EscapeUrlEncodedData(params.email, true);
+  post_body += "&Passwd=" + EscapeUrlEncodedData(params.password, true);
+  post_body += "&source=" + EscapeUrlEncodedData(user_agent_, true);
   post_body += "&service=" + service_id_;
   if (!params.captcha_token.empty() && !params.captcha_value.empty()) {
-    post_body += "&logintoken=" + EscapeUrlEncodedData(params.captcha_token);
-    post_body += "&logincaptcha=" + EscapeUrlEncodedData(params.captcha_value);
+    post_body += "&logintoken=" +
+                 EscapeUrlEncodedData(params.captcha_token, true);
+    post_body += "&logincaptcha=" +
+                 EscapeUrlEncodedData(params.captcha_value, true);
   }
   post_body += "&PersistentCookie=true";
   // We set it to GOOGLE (and not HOSTED or HOSTED_OR_GOOGLE) because we only
@@ -216,7 +218,7 @@ bool GaiaAuthenticator::LookupEmail(AuthResults* results) {
 
   string post_body;
   post_body += "LSID=";
-  post_body += EscapeUrlEncodedData(results->lsid);
+  post_body += EscapeUrlEncodedData(results->lsid, true);
 
   unsigned long server_response_code;
   string message_text;
@@ -266,7 +268,7 @@ bool GaiaAuthenticator::IssueAuthToken(AuthResults* results,
 
   string post_body;
   post_body += "LSID=";
-  post_body += EscapeUrlEncodedData(results->lsid);
+  post_body += EscapeUrlEncodedData(results->lsid, true);
   post_body += "&service=" + service_id;
   post_body += "&Session=true";
 
