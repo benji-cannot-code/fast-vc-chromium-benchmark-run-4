c9c94723336bf08baeb4d77d0c25c3b259edbe41
mario@webkit.org <mario@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-09 07:35:28 +0000

    2011-06-09  Mario Sanchez Prada  <msanchez@igalia.com>
            Reviewed by Chris Fleizach.
    
            [GTK] Provide a way in DRT to check the platform name
            https://bugs.webkit.org/show_bug.cgi?id=56855
    
            New layout test and expected results to check the new function.
    
            * accessibility/platform-name.html: Added.
            * platform/gtk/accessibility/platform-name-expected.txt: Added.
            * platform/mac/accessibility/platform-name-expected.txt: Added.
            * platform/win/accessibility/platform-name-expected.txt: Added.
    2011-06-09  Mario Sanchez Prada  <msanchez@igalia.com>
    
            Reviewed by Chris Fleizach.
    
            [GTK] Provide a way in DRT to check the platform name
            https://bugs.webkit.org/show_bug.cgi?id=56855
    
            Patch for adding 'platformName' static property to DRT's
            LayoutController and provide platform specific implementations.
    
            In principle, this feature will be only of interest for writing
            accessibility tests, so that's why it's only implemented in the
            Mac, Gtk and Win platforms.
    
            * DumpRenderTree/LayoutTestController.h: New method platformName().
            * DumpRenderTree/LayoutTestController.cpp:
            (getPlatformNameCallback): New callback for 'platformName'.
            (LayoutTestController::staticValues): New static property.
    
            * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
            (LayoutTestController::platformName): Implemented.
    
            * DumpRenderTree/mac/LayoutTestControllerMac.mm:
            (LayoutTestController::platformName): Implemented.
    
            * DumpRenderTree/win/LayoutTestControllerWin.cpp:
            (LayoutTestController::platformName): Implemented.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/LayoutTestController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/LayoutTestController.cpp
@@ -2193,6 +2193,17 @@ static JSValueRef getWorkerThreadCountCallback(JSContextRef context, JSObjectRef
     return JSValueMakeNumber(context, controller->workerThreadCount());
 }
 
+#if PLATFORM(MAC) || PLATFORM(GTK) || PLATFORM(WIN)
+static JSValueRef getPlatformNameCallback(JSContextRef context, JSObjectRef thisObject, JSStringRef propertyName, JSValueRef* exception)
+{
+    LayoutTestController* controller = static_cast<LayoutTestController*>(JSObjectGetPrivate(thisObject));
+    JSRetainPtr<JSStringRef> platformName(controller->platformName());
+    if (!platformName.get())
+        return JSValueMakeUndefined(context);
+    return JSValueMakeString(context, platformName.get());
+}
+#endif
+
 static bool setGlobalFlagCallback(JSContextRef context, JSObjectRef thisObject, JSStringRef propertyName, JSValueRef value, JSValueRef* exception)
 {
     LayoutTestController* controller = static_cast<LayoutTestController*>(JSObjectGetPrivate(thisObject));
@@ -2263,6 +2274,9 @@ JSStaticValue* LayoutTestController::staticValues()
         { "globalFlag", getGlobalFlagCallback, setGlobalFlagCallback, kJSPropertyAttributeNone },
         { "webHistoryItemCount", getWebHistoryItemCountCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
         { "workerThreadCount", getWorkerThreadCountCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
+#if PLATFORM(MAC) || PLATFORM(GTK) || PLATFORM(WIN)
+        { "platformName", getPlatformNameCallback, 0, kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete },
+#endif
         { 0, 0, 0, 0 }
     };
     return staticValues;
