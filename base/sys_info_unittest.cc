fbc37e03ba34010555aef671f7c97e0b97d53225
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 17:24:38 +0000

    Revert 88470 (broke ServiceProcessStateTest.ForceShutdown on CrOS) - Fix for system version unit tests in cros.
    BUG=chromium-os:15789
    TEST=Ran try servers to make sure the unit test is not failing anymore, plus viewed all the screens and took screenshots. Screen shots of all effected screens are attached to the cros bug
    TBR=rkc@chromium.org
    
    TBR=rkc@chromium.org
    Review URL: http://codereview.chromium.org/7044086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88545 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_info_unittest.cc
+++ base/sys_info_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -68,9 +68,9 @@ TEST_F(SysInfoTest, GoogleChromeOSVersionNumbers) {
                                  &os_major_version,
                                  &os_minor_version,
                                  &os_bugfix_version);
-  EXPECT_EQ(2, os_major_version);
-  EXPECT_EQ(3, os_minor_version);
-  EXPECT_EQ(4, os_bugfix_version);
+  EXPECT_EQ(1, os_major_version);
+  EXPECT_EQ(2, os_minor_version);
+  EXPECT_EQ(3, os_bugfix_version);
 }
 
 TEST_F(SysInfoTest, GoogleChromeOSVersionNumbersFirst) {
@@ -84,9 +84,9 @@ TEST_F(SysInfoTest, GoogleChromeOSVersionNumbersFirst) {
                                  &os_major_version,
                                  &os_minor_version,
                                  &os_bugfix_version);
-  EXPECT_EQ(2, os_major_version);
-  EXPECT_EQ(3, os_minor_version);
-  EXPECT_EQ(4, os_bugfix_version);
+  EXPECT_EQ(1, os_major_version);
+  EXPECT_EQ(2, os_minor_version);
+  EXPECT_EQ(3, os_bugfix_version);
 }
 
 TEST_F(SysInfoTest, GoogleChromeOSNoVersionNumbers) {
