c6113922330e17c3a2e861310ff280da620861d3
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 04:35:48 +0000

    Resore cros wrench about navigation to chrome://settings/about.
    BUG=chromium-os:16324
    TEST=Click wrench->"About Chrom[e|ium] OS", see chrome://settings/about.
    
    Review URL: http://codereview.chromium.org/7129032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2026,7 +2026,8 @@ void Browser::OpenSyncMyBookmarksDialog() {
 void Browser::OpenAboutChromeDialog() {
   UserMetrics::RecordAction(UserMetricsAction("AboutChrome"));
 #if defined(OS_CHROMEOS)
-  ShowSingletonTab(GURL(chrome::kChromeUIAboutURL));
+  std::string chrome_settings(chrome::kChromeUISettingsURL);
+  ShowSingletonTab(GURL(chrome_settings.append(chrome::kAboutOptionsSubPage)));
 #else
   window_->ShowAboutChromeDialog();
 #endif
