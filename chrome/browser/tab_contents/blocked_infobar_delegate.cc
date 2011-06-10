ae608772f80b8a139f53937cd508fdf486652803
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 22:23:34 +0000

    Fix busted clang compile.  TBR=jam@chromium.org
    M      chrome/browser/tab_contents/blocked_infobar_delegate.cc
    M      chrome/browser/tab_contents/blocked_infobar_delegate.h
    Review URL: http://codereview.chromium.org/7046095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88738 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/blocked_infobar_delegate.cc
+++ chrome/browser/tab_contents/blocked_infobar_delegate.cc
@@ -47,6 +47,11 @@ bool BlockedInfoBarDelegate::LinkClicked(WindowOpenDisposition disposition) {
   return false;
 }
 
+BlockedRunningInfoBarDelegate*
+BlockedInfoBarDelegate::AsBlockedRunningInfoBarDelegate() {
+  return NULL;
+}
+
 BlockedDisplayingInfoBarDelegate::BlockedDisplayingInfoBarDelegate(
     TabContentsWrapper* wrapper)
     : BlockedInfoBarDelegate(
