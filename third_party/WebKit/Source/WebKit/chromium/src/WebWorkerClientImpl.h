212500bd318a8a43ec3ced4ee0291cffe37eecd7
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-10 07:35:55 +0000

    2011-06-09  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Dmitry Titov.
    
            [Chromium] Worker object may be garbage collected even if it has message handlers
            https://bugs.webkit.org/show_bug.cgi?id=62292
    
            I'd like to convert the test case into a layout test but I don't see yet
            how to do this so that the test isn't too slow and reliable. The problem
            with test is that full-GC needs to be triggered right after
            WebWorkerClientImpl::confirmMessageFromWorkerObject is called and there
            is no way to hook WebWorkerClientImpl::confirmMessageFromWorkerObject from JavaScript.
            So no test for now.
    
            * src/WebWorkerClientImpl.cpp:
            (WebKit::WebWorkerClientImpl::confirmMessageFromWorkerObject):
            (WebKit::WebWorkerClientImpl::confirmMessageFromWorkerObjectTask): update worker context pending
            activity state every time a message is confirmed since the message may have created a new activity.
            * src/WebWorkerClientImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebWorkerClientImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebWorkerClientImpl.h
@@ -154,7 +154,8 @@ private:
                                                      int lineNumber,
                                                      const WTF::String& sourceURL);
     static void confirmMessageFromWorkerObjectTask(WebCore::ScriptExecutionContext* context,
-                                                   WebWorkerClientImpl* thisPtr);
+                                                   WebWorkerClientImpl* thisPtr,
+                                                   bool hasPendingActivity);
     static void reportPendingActivityTask(WebCore::ScriptExecutionContext* context,
                                           WebWorkerClientImpl* thisPtr,
                                           bool hasPendingActivity);
