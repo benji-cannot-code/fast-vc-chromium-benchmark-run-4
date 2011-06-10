c50de6e71f459e640b8dfe890f4fe43b84087f87
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 11:04:17 +0000

    Add PrerenderUnload browser test to ensure that unload handlers are run on the navigated-away from page.
    This test fails if the bug fix for 85350 is not in, and succeeds with it in.
    
    BUG=85350,82856
    TEST=browser_tests --gtest_filter=*PrerenderUnload
    
    Review URL: http://codereview.chromium.org/7046053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "base/test/test_timeouts.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/browser/favicon/favicon_tab_helper.h"
@@ -318,7 +319,8 @@ class PrerenderBrowserTest : public InProcessBrowserTest {
       : safe_browsing_factory_(new TestSafeBrowsingServiceFactory()),
         prerender_contents_factory_(NULL),
         use_https_src_server_(false),
-        call_javascript_(true) {
+        call_javascript_(true),
+        loader_path_("files/prerender/prerender_loader.html") {
     EnableDOMAutomation();
   }
 
@@ -469,6 +471,10 @@ class PrerenderBrowserTest : public InProcessBrowserTest {
         prerender_manager()->FindEntry(dest_url_));
   }
 
+  void set_loader_path(const std::string& path) {
+    loader_path_ = path;
+  }
+
  private:
   void PrerenderTestURLImpl(
       const GURL& url,
@@ -481,7 +487,7 @@ class PrerenderBrowserTest : public InProcessBrowserTest {
         make_pair("REPLACE_WITH_PRERENDER_URL", dest_url_.spec()));
     std::string replacement_path;
     ASSERT_TRUE(net::TestServer::GetFilePathWithReplacements(
-        "files/prerender/prerender_loader.html",
+        loader_path_,
         replacement_text,
         &replacement_path));
 
@@ -572,6 +578,7 @@ class PrerenderBrowserTest : public InProcessBrowserTest {
   GURL dest_url_;
   bool use_https_src_server_;
   bool call_javascript_;
+  std::string loader_path_;
 };
 
 // Checks that a page is correctly prerendered in the case of a
@@ -1359,6 +1366,17 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, PrerenderFavicon) {
   ASSERT_TRUE(TabContentsWrapper::GetCurrentWrapperForContents(
       browser()->GetSelectedTabContents())->favicon_tab_helper()
               ->FaviconIsValid());
+}
+
+// Checks that when a prerendered page is swapped in to a referring page, the
+// unload handlers on the referring page are executed.
+IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, PrerenderUnload) {
+  set_loader_path("files/prerender/prerender_loader_with_unload.html");
+  PrerenderTestURL("files/prerender/prerender_page.html", FINAL_STATUS_USED, 1);
+  ui_test_utils::TitleWatcher title_watcher(browser()->GetSelectedTabContents(),
+                                            ASCIIToUTF16("Unloaded"));
+  NavigateToDestURL();
+  EXPECT_TRUE(title_watcher.Wait());
 }
 
 }  // namespace prerender
