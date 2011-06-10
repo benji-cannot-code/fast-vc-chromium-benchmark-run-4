3e9b59c5f318959629bf91650deeae46cdf4e2ea
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 15:13:49 +0000

    Reduce the duration of the keyboard animation.
    This is not a final value, but should be fast enough to never be
    annoying when using it.
    
    BUG=none
    TEST=manually
    
    
    Review URL: http://codereview.chromium.org/7003097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
+++ chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const int kDefaultKeyboardHeight = 300;
-const int kKeyboardSlideDuration = 500;  // In milliseconds
+const int kKeyboardSlideDuration = 300;  // In milliseconds
 
 PropertyAccessor<bool>* GetFocusedStateAccessor() {
   static PropertyAccessor<bool> state;
