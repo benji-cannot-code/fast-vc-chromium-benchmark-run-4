16f691db3a88f28010951a2b8494fe7d7b974777
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 19:28:50 +0000

    [Mac] Fix crash in GetFirefoxDylibPath().
    If the bundle exists but the executable in it doesn't, a NULL was
    passed to FilePath().
    
    Additionally, converted -fileSystemRepresentation to
    -getFileSystemRepresentation:maxLength:, because the former can throw
    an NSException.
    
    BUG=84520
    TEST=Firefox import still works.
    
    Review URL: http://codereview.chromium.org/7104058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88709 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox_importer_utils_mac.mm
+++ chrome/browser/importer/firefox_importer_utils_mac.mm
@@ -1,13 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <Cocoa/Cocoa.h>
+#include <sys/param.h>
 
 #include "chrome/browser/importer/firefox_importer_utils.h"
 
 #include "base/file_util.h"
+#include "base/mac/foundation_util.h"
 #include "base/path_service.h"
 
 FilePath GetProfilesINI() {
@@ -31,11 +33,13 @@ FilePath GetFirefoxDylibPath() {
                               &appURL) != noErr) {
     return FilePath();
   }
-  NSBundle *ff_bundle = [NSBundle
-      bundleWithPath:[reinterpret_cast<const NSURL*>(appURL) path]];
+  NSBundle *ff_bundle =
+      [NSBundle bundleWithPath:[base::mac::CFToNSCast(appURL) path]];
   CFRelease(appURL);
-  NSString *ff_library_path = [[ff_bundle executablePath]
-                                  stringByDeletingLastPathComponent];
-
-  return FilePath([ff_library_path fileSystemRepresentation]);
+  NSString *ff_library_path =
+      [[ff_bundle executablePath] stringByDeletingLastPathComponent];
+  char buf[MAXPATHLEN];
+  if (![ff_library_path getFileSystemRepresentation:buf maxLength:sizeof(buf)])
+    return FilePath();
+  return FilePath(buf);
 }
