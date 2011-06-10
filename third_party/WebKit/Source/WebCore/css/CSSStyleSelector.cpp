f4fb88f44c63ac435f831f76f77186a89605572e
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-10 06:19:22 +0000

    2011-06-09  Luke Macpherson   <macpherson@chromium.org>
            Reviewed by Eric Seidel.
    
            Implement CSSPropertyOutlineStyle handler in CSSStyleApplyProperty
            https://bugs.webkit.org/show_bug.cgi?id=61601
    
            No new tests. No new functionality added / covered by existing tests.
    
            * css/CSSPrimitiveValueMappings.h:
            (WebCore::CSSPrimitiveValue::operator EBorderStyle):
            Support CSSValueAuto as required by outline-style property.
            (WebCore::CSSPrimitiveValue::operator OutlineIsAuto):
            Add cast to new OutlineIsAuto enum.
            * css/CSSStyleApplyProperty.cpp:
            (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
            Initialize handler for CSSPropertyOutlineStyle.
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::applyProperty):
            Remove old handler for CSSPropertyOutlineStyle.
            * rendering/style/OutlineValue.h:
            Use new OutlineIsAuto enum instead of bool.
            * rendering/style/RenderStyle.h:
            Split existing two-parameter setter setOutlineStyle into separate setters for style and auto properties.
            Use new OutlineIsAuto enum type.
            * rendering/style/RenderStyleConstants.h:
            Define new enum OutlineIsAuto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -3661,15 +3661,6 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyBorderCollapse:
         HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(borderCollapse, BorderCollapse)
         return;
-    case CSSPropertyOutlineStyle:
-        HANDLE_INHERIT_AND_INITIAL_WITH_VALUE(outlineStyle, OutlineStyle, BorderStyle)
-        if (primitiveValue) {
-            if (primitiveValue->getIdent() == CSSValueAuto)
-                m_style->setOutlineStyle(DOTTED, true);
-            else
-                m_style->setOutlineStyle(*primitiveValue);
-        }
-        return;
     case CSSPropertyCaptionSide:
         HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(captionSide, CaptionSide)
         return;
@@ -5411,6 +5402,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWebkitTextOrientation:
     case CSSPropertyWebkitFontSmoothing:
     case CSSPropertyFontWeight:
+    case CSSPropertyOutlineStyle:
     case CSSPropertyOutlineWidth:
     case CSSPropertyWebkitColumnRuleWidth:
     case CSSPropertyOutlineColor:
