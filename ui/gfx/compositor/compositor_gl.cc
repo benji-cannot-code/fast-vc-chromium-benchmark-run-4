19823a269ab8eef23698c2c9285415eb4469aed1
wjmaclean@chromium.org <wjmaclean@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 18:20:02 +0000

    Re-enable thread IO restriction override.
    The Linux Views browser ui tests are failing. The test dies on an thread io restriction violation. I'm guessing (with Backer's help) that this may be due to the bot not having GL enabled, and continuing attempts to init the bindings fail (normally it would never get to us, hence why we thought the IO restriction was gone).
    
    BUG=linux views fails on compositor startup due to thread io restriction
    TEST=browser ui tests
    
    Review URL: http://codereview.chromium.org/7104115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88691 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor_gl.cc
+++ ui/gfx/compositor/compositor_gl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/threading/thread_restrictions.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkMatrix.h"
 #include "third_party/skia/include/core/SkScalar.h"
@@ -357,6 +358,10 @@ bool CompositorGL::InitShaders() {
 
 // static
 Compositor* Compositor::Create(gfx::AcceleratedWidget widget) {
+  // The following line of code exists soley to disable IO restrictions
+  // on this thread long enough to perform the GL bindings.
+  // TODO(wjmaclean) Remove this when GL initialisation cleaned up.
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
   gfx::GLSurface::InitializeOneOff();
   if (gfx::GetGLImplementation() != gfx::kGLImplementationNone)
     return new glHidden::CompositorGL(widget);
