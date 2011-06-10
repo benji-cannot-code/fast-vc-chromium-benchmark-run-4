795b0223dc2ce0eb025c7e2649b3767408df8248
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 20:29:52 +0000

    Set background color of events in about:net-internals according to event status:
    white = in progress.
    green = finished.
    red = net error at some point.  Takes priority over other colors.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7129064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/util.js
+++ chrome/browser/resources/net_internals/util.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -82,9 +82,11 @@ function changeClassName(node, classNameToAddOrRemove, isAdd) {
   var currentNames = node.className.split(' ');
 
   if (isAdd) {
-    if (!(classNameToAddOrRemove in currentNames)) {
-      currentNames.push(classNameToAddOrRemove);
+    for (var i = 0; i < currentNames.length; ++i) {
+      if (currentNames[i] == classNameToAddOrRemove)
+        return;
     }
+    currentNames.push(classNameToAddOrRemove);
   } else {
     for (var i = 0; i < currentNames.length; ++i) {
       if (currentNames[i] == classNameToAddOrRemove) {
