b5820b549070e83e0f430fa7f2b7de88d9e447e6
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-10 22:43:20 +0000

    Roll out r88568. It is not the right fix for the problem.
    * loader/appcache/ApplicationCacheHost.cpp:
    (WebCore::ApplicationCacheHost::isApplicationCacheEnabled):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/appcache/ApplicationCacheHost.cpp
+++ third_party/WebKit/Source/WebCore/loader/appcache/ApplicationCacheHost.cpp
@@ -460,7 +460,7 @@ bool ApplicationCacheHost::swapCache()
 
 bool ApplicationCacheHost::isApplicationCacheEnabled()
 {
-    return m_documentLoader->frame() && m_documentLoader->frame()->settings()
+    return m_documentLoader->frame()->settings()
            && m_documentLoader->frame()->settings()->offlineWebApplicationCacheEnabled();
 }
 
