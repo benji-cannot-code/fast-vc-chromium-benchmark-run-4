68e81e31f27b21cae172c86839025fd27761a685
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 01:42:46 +0000

    Mark DevToolsSanityTest.TestPauseWhenScriptIsRunning as flaky.
    BUG=69719
    TEST=n/a
    TBR=pfeldman
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -288,7 +288,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestPauseWhenLoadingDevTools) {
 
 // Tests that pressing 'Pause' will pause script execution if the script
 // is already running.
-IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestPauseWhenScriptIsRunning) {
+IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, FLAKY_TestPauseWhenScriptIsRunning) {
   RunTest("testPauseWhenScriptIsRunning", kPauseWhenScriptIsRunning);
 }
 
