7976c619d876b8465de4f1d508c9cb415582c9ec
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-11 04:14:55 +0000

    CrOS - Fix crash when pressing Enter in file browser dialog
    If focus is on the cancel button, pressing enter will both cancel the dialog and attempt to select a file.  Fix by ensuring that SelectFileDialog::Listener objects are invoked exactly once (some listeners delete themselves after being invoked).
    
    BUG=chromium-os:16349
    TEST=manual, see bug.  Also browsertest FileManagerDialogTest.*
    
    Review URL: http://codereview.chromium.org/7108066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_file_browser_private_api.cc
+++ chrome/browser/extensions/extension_file_browser_private_api.cc
@@ -910,6 +910,12 @@ const FileDialogFunction::Callback& FileDialogFunction::GetCallback() const {
   return Callback::Find(GetTabId());
 }
 
+void FileDialogFunction::RemoveCallback() {
+  // Listeners expect to be invoked exactly once, so we need to remove our
+  // callback objects afterwards.
+  Callback::Remove(GetTabId());
+}
+
 // GetFileSystemRootPathOnFileThread can only be called from the file thread,
 // so here we are. This function takes a vector of virtual paths, converts
 // them to local paths and calls GetLocalPathsResponseOnUIThread with the
@@ -996,6 +1002,7 @@ void SelectFileFunction::GetLocalPathsResponseOnUIThread(
     callback.listener()->FileSelected(files[0],
                                       index,
                                       callback.params());
+    RemoveCallback();  // Listeners expect to be invoked exactly once.
   }
   SendResponse(true);
 }
@@ -1090,6 +1097,7 @@ void SelectFilesFunction::GetLocalPathsResponseOnUIThread(
   DCHECK(!callback.IsNull());
   if (!callback.IsNull()) {
     callback.listener()->MultiFilesSelected(files, callback.params());
+    RemoveCallback();  // Listeners expect to be invoked exactly once.
   }
   SendResponse(true);
 }
@@ -1099,6 +1107,7 @@ bool CancelFileDialogFunction::RunImpl() {
   DCHECK(!callback.IsNull());
   if (!callback.IsNull()) {
     callback.listener()->FileSelectionCanceled(callback.params());
+    RemoveCallback();  // Listeners expect to be invoked exactly once.
   }
   SendResponse(true);
   return true;
