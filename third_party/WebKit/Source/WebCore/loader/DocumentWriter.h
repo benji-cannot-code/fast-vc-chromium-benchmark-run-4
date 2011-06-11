1b7dff810c46a97857620e9241398b8fafbeca0b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-11 00:56:13 +0000

    2011-06-10  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            Script-created parsers should ignore data from the network
            https://bugs.webkit.org/show_bug.cgi?id=62336
    
            Triggering this condition is very hard to do deterministically.  This
            test attempts to trigger the condition by sending exactly the right
            number of bytes for the Chromium network stack.  As network stacks vary
            and evolve, it's likely we'll lose test coverage for this issue, but
            this is the best test I could come up with.
    
            * fast/parser/document-write-ignores-later-network-bytes-expected.txt: Added.
            * fast/parser/document-write-ignores-later-network-bytes.html: Added.
    2011-06-10  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Darin Adler.
    
            Script-created parsers should ignore data from the network
            https://bugs.webkit.org/show_bug.cgi?id=62336
    
            If the network packet boundaries line up just right, we can end up
            feeding network data to a parser created by document.write (or
            document.open).  That's incorrect.
    
            This patch causes DocumentWriter to grab hold of the original parser
            and direct all input to that parser.
    
            Test: fast/parser/document-write-ignores-later-network-bytes.html
    
            * dom/Document.cpp:
            (WebCore::Document::explicitClose):
            * loader/DocumentWriter.cpp:
            (WebCore::DocumentWriter::begin):
            (WebCore::DocumentWriter::addData):
            (WebCore::DocumentWriter::endIfNotLoadingMainResource):
            (WebCore::DocumentWriter::setDocumentWasLoadedAsPartOfNavigation):
            * loader/DocumentWriter.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88583 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/DocumentWriter.h
+++ third_party/WebKit/Source/WebCore/loader/DocumentWriter.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class Document;
+class DocumentParser;
 class Frame;
 class SecurityOrigin;
 class TextResourceDecoder;
@@ -88,6 +89,7 @@ private:
     bool m_encodingWasChosenByUser;
     String m_encoding;
     RefPtr<TextResourceDecoder> m_decoder;
+    RefPtr<DocumentParser> m_parser;
 };
 
 } // namespace WebCore
