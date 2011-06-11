c0e0a9a0803ecd5a88dd9b071ce7923099e823d7
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-11 00:21:23 +0000

    Web Worker fails to fire error event when a resource fetch fails. https://bugs.webkit.org/show_bug.cgi?id=62475
    Reviewed by Dmitry Titov.
    
    Source/WebCore:
    
    Test: http/tests/workers/worker-workerScriptNotThere.html
    
    * workers/WorkerScriptLoader.cpp:
    (WebCore::WorkerScriptLoader::didFinishLoading): Ensure that
    the client is notified of the error when it happens during loading.
    
    LayoutTests:
    
    * http/tests/workers/worker-workerScriptNotThere-expected.txt: Added.
    * http/tests/workers/worker-workerScriptNotThere.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/workers/WorkerScriptLoader.cpp
+++ third_party/WebKit/Source/WebCore/workers/WorkerScriptLoader.cpp
@@ -147,8 +147,10 @@ void WorkerScriptLoader::didReceiveData(const char* data, int len)
 
 void WorkerScriptLoader::didFinishLoading(unsigned long identifier, double)
 {
-    if (m_failed)
+    if (m_failed) {
+        notifyError();
         return;
+    }
 
     if (m_decoder)
         m_script += m_decoder->flush();
