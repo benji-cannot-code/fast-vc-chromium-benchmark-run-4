da667c72c12192156d1ff63e682ae0f1f5f82e14
mahesh.kulkarni@nokia.com <mahesh.kulkarni@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-12 17:23:28 +0000

    [Qt] LayoutTestController needs setTextDirection implementation https://bugs.webkit.org/show_bug.cgi?id=62442
    Reviewed by Antonio Gomes.
    
    Tools: 
    
    Implements LayoutController.setTextDirection.
    
    * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
    (LayoutTestController::setTextDirection):
    * DumpRenderTree/qt/LayoutTestControllerQt.h:
    
    LayoutTests: 
    
    Unskipping fast/html/set-text-direction.html. 
    
    * platform/qt/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
+++ third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
@@ -275,6 +275,7 @@ public slots:
     void observeStorageTrackerNotifications(unsigned number);
     void syncLocalStorage();
     QString layerTreeAsText();
+    void setTextDirection(const QString& directionName);
 
 private slots:
     void processWork();
