47ebe57a0012aa1948780f65eb6c0b75b5c8571d
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-12 11:51:45 +0000

    2011-06-12  Robert Hogan  <robert@webkit.org>
            Reviewed by Andreas Kling.
    
            [Qt] Fix methods.html and methods-lower-case.html
            https://bugs.webkit.org/show_bug.cgi?id=62221
    
            Our expectations for the unskipped tests have the following differences from the mac
            platform:
              - POST requests with no data get a default Content-Type of 'application/octet-stream' in
                Qt 4.8 and 'application/x-www-form-urlencoded' in Qt 4.7.3.
                This is Qt-defined behaviour in QNetworkAccessManager (see QHttpNetworkRequestPrivate::header
                in qhttpnetworkrequest.cpp)
              - DELETE and HEAD requests get their ContentType and ContentLength headers (if present) removed
                by QNetworkReplyHandler. QtWebKit does not send data for either of these request types.
    
            * platform/qt-4.8/http/tests/xmlhttprequest/methods-expected.txt: Added.
            * platform/qt-4.8/http/tests/xmlhttprequest/workers/methods-async-expected.txt: Added.
            * platform/qt-4.8/http/tests/xmlhttprequest/workers/methods-expected.txt: Added.
            * platform/qt-4.8/http/tests/xmlhttprequest/workers/shared-worker-methods-async-expected.txt: Added.
            * platform/qt/Skipped: Unskip: http/tests/xmlhttprequest/methods.html
                                           http/tests/xmlhttprequest/methods-lower-case.html
                                           http/tests/xmlhttprequest/workers/methods-async.html
                                           http/tests/xmlhttprequest/workers/methods.html
                                           http/tests/xmlhttprequest/workers/shared-worker-methods-async.html
            * platform/qt/http/tests/xmlhttprequest/methods-expected.txt: Added.
            * platform/qt/http/tests/xmlhttprequest/workers/methods-async-expected.txt: Added.
            * platform/qt/http/tests/xmlhttprequest/workers/methods-expected.txt: Added.
            * platform/qt/http/tests/xmlhttprequest/workers/shared-worker-methods-async-expected.txt: Added.
    2011-06-12  Robert Hogan  <robert@webkit.org>
    
            Reviewed by Andreas Kling.
    
            [Qt] Fix methods.html and methods-lower-case.html
            https://bugs.webkit.org/show_bug.cgi?id=62221
    
            * platform/network/qt/QNetworkReplyHandler.cpp:
            (WebCore::QNetworkReplyHandler::clearContentsHeaders): Refactor code from sendNetworkRequest
            (WebCore::QNetworkReplyHandler::getIODevice): ditto
            (WebCore::QNetworkReplyHandler::sendNetworkRequest):
                - Remove ContentType and ContentLength headers from GET, DELETE and HEAD requests since
                  we do not send data with any of these requests.
                - If a custom HTTP operation contains data, send it.
            * platform/network/qt/QNetworkReplyHandler.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/qt/QNetworkReplyHandler.h
+++ third_party/WebKit/Source/WebCore/platform/network/qt/QNetworkReplyHandler.h
@@ -36,6 +36,7 @@ QT_END_NAMESPACE
 
 namespace WebCore {
 
+class FormDataIODevice;
 class ResourceHandle;
 class ResourceRequest;
 class ResourceResponse;
@@ -141,6 +142,8 @@ private:
     void redirect(ResourceResponse&, const QUrl&);
     bool wasAborted() const { return !m_resourceHandle; }
     QNetworkReply* sendNetworkRequest(QNetworkAccessManager*, const ResourceRequest&);
+    FormDataIODevice* getIODevice(const ResourceRequest&);
+    void clearContentHeaders();
 
     OwnPtr<QNetworkReplyWrapper> m_replyWrapper;
     ResourceHandle* m_resourceHandle;
