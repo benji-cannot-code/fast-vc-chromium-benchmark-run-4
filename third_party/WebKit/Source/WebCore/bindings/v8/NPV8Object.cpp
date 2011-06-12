f765d287faac5eef74e874e4e5297312fbbb9caf
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-12 16:57:54 +0000

    2011-06-12  Adam Barth  <abarth@webkit.org>
            Reviewed by Andreas Kling.
    
            Minor user-gesture related cleanup
            https://bugs.webkit.org/show_bug.cgi?id=62511
    
            In reading the user gesture code, I noticed that these lines of code
            should be deleted because they don't do anything.
    
            * bindings/v8/NPV8Object.cpp:
            * html/HTMLAnchorElement.cpp:
            * html/HTMLVideoElement.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/NPV8Object.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/NPV8Object.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "OwnArrayPtr.h"
 #include "PlatformString.h"
 #include "ScriptSourceCode.h"
-#include "UserGestureIndicator.h"
 #include "V8GCController.h"
 #include "V8Helpers.h"
 #include "V8NPUtils.h"
--- third_party/WebKit/Source/WebCore/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLAnchorElement.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandle.h"
 #include "SecurityOrigin.h"
 #include "Settings.h"
-#include "UserGestureIndicator.h"
 
 namespace WebCore {
 
