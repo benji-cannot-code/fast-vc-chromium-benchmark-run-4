03e70c6ebbb1330a7e5482283f76a80328ae204f
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-13 22:06:22 +0000

    Fix clang build.
    Removes vestigial IsAlwaysOnTop call. No one was calling this anyway.
    
    BUG=none
    TEST=none
    TBR=sky
    Review URL: http://codereview.chromium.org/7144005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/external_protocol_dialog.cc
+++ chrome/browser/chromeos/external_protocol_dialog.cc
@@ -80,10 +80,6 @@ views::View* ExternalProtocolDialog::GetContentsView() {
   return message_box_view_;
 }
 
-bool ExternalProtocolDialog::IsAlwaysOnTop() const {
-  return false;
-}
-
 bool ExternalProtocolDialog::IsModal() const {
   return false;
 }
