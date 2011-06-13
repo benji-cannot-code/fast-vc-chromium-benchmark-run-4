a8df94ec1e9efa44b381437c1a0c3f9a1457623b
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-13 20:07:28 +0000

    Previously, the DisplayLink would never start until a software draw was done at least once. With force-compositing-mode, the software draw was never done, causing tabs to lockup indefinitely waiting for the swap acknowledgement. This fix ensures that the DisplayLink is running immediately.
    The Cocoa setUpGState callback was required to avoid "invalid drawable" errors when setView was called on the NSOpenGLContext.
    
    Another deadlock cause could be the GpuChannel route getting removed while the renderer is blocked on a FlushSync. For this, a method has been added to GpuCommandBufferStub to unblock/cleanup that is called from GpuChannel before the IPC route is removed.
    
    Trace events have been added in places that will help debug related issues in the future.
    
    BUG=84343
    TEST=launch with --force-compositing-mode; open about:flags; in the same tab, open ycombinator.com and it should not hang.
    
    Review URL: http://codereview.chromium.org/6993043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88877 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.cc
+++ content/common/gpu/gpu_channel.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/gpu_channel.h"
 
 #include "base/command_line.h"
+#include "base/debug/trace_event.h"
 #include "base/process_util.h"
 #include "base/string_util.h"
 #include "content/common/child_process.h"
@@ -255,6 +256,8 @@ void GpuChannel::OnCreateOffscreenCommandBuffer(
       0, 0, watchdog_));
   router_.AddRoute(*route_id, stub.get());
   stubs_.AddWithID(stub.release(), *route_id);
+  TRACE_EVENT1("gpu", "GpuChannel::OnCreateOffscreenCommandBuffer",
+               "route_id", route_id);
 #else
   *route_id = MSG_ROUTING_NONE;
 #endif
@@ -262,7 +265,14 @@ void GpuChannel::OnCreateOffscreenCommandBuffer(
 
 void GpuChannel::OnDestroyCommandBuffer(int32 route_id) {
 #if defined(ENABLE_GPU)
+  TRACE_EVENT1("gpu", "GpuChannel::OnDestroyCommandBuffer",
+               "route_id", route_id);
   if (router_.ResolveRoute(route_id)) {
+    GpuCommandBufferStub* stub = stubs_.Lookup(route_id);
+    // In case the renderer is currently blocked waiting for a sync reply from
+    // the stub, allow the stub to clean up and unblock pending messages here:
+    if (stub != NULL)
+      stub->CommandBufferWasDestroyed();
     router_.RemoveRoute(route_id);
     stubs_.Remove(route_id);
   }
