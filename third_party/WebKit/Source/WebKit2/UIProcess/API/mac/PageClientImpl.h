3a70e6230c115ee077ad77ead25a8f602bc801b4
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-13 19:10:16 +0000

    kAXTextMarkerForPositionParametrizedAttribute does not work correctly in WK2 https://bugs.webkit.org/show_bug.cgi?id=62547
    Reviewed by Darin Adler.
    
    Implement the missing screenToWindow method for WK2.
    
    * UIProcess/API/gtk/PageClientImpl.cpp:
    (WebKit::PageClientImpl::screenToWindow):
    * UIProcess/API/gtk/PageClientImpl.h:
    * UIProcess/API/mac/PageClientImpl.h:
    * UIProcess/API/mac/PageClientImpl.mm:
    (WebKit::PageClientImpl::screenToWindow):
    * UIProcess/API/qt/qwkpage.cpp:
    (QWKPagePrivate::screenToWindow):
    * UIProcess/API/qt/qwkpage_p.h:
    * UIProcess/PageClient.h:
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::screenToWindow):
    * UIProcess/WebPageProxy.h:
    * UIProcess/WebPageProxy.messages.in:
    * UIProcess/win/WebView.cpp:
    (WebKit::WebView::screenToWindow):
    * UIProcess/win/WebView.h:
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::screenToWindow):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::screenToWindow):
    * WebProcess/WebPage/WebPage.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/PageClientImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/PageClientImpl.h
@@ -80,6 +80,7 @@ private:
 
     virtual WebCore::FloatRect convertToDeviceSpace(const WebCore::FloatRect&);
     virtual WebCore::FloatRect convertToUserSpace(const WebCore::FloatRect&);
+    virtual WebCore::IntPoint screenToWindow(const WebCore::IntPoint&);
     virtual WebCore::IntRect windowToScreen(const WebCore::IntRect&);
     
     virtual void doneWithKeyEvent(const NativeWebKeyboardEvent&, bool wasEventHandled);
--- third_party/WebKit/Source/WebKit2/UIProcess/win/WebView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/win/WebView.cpp
@@ -1132,6 +1132,11 @@ FloatRect WebView::convertToDeviceSpace(const FloatRect& rect)
     return rect;
 }
 
+IntPoint WebView::screenToWindow(const IntPoint& point)
+{
+    return point;
+}
+
 IntRect WebView::windowToScreen(const IntRect& rect)
 {
     return rect;
