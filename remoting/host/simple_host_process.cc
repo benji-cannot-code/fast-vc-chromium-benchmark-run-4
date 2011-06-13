13e896a4b1ea6c2dcf33ad63fff705e9251b44dd
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-13 20:08:22 +0000

    Block remote mouse inputs for a short time when local input is received.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7134048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88878 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/simple_host_process.cc
+++ remoting/host/simple_host_process.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/disconnect_window.h"
 #include "remoting/host/event_executor.h"
 #include "remoting/host/heartbeat_sender.h"
+#include "remoting/host/local_input_monitor.h"
 #include "remoting/host/json_host_config.h"
 #include "remoting/host/register_support_host_request.h"
 #include "remoting/host/self_access_verifier.h"
@@ -175,10 +176,12 @@ class SimpleHost {
       remoting::Curtain* curtain = remoting::Curtain::Create();
       remoting::DisconnectWindow* disconnect_window =
           remoting::DisconnectWindow::Create();
+      remoting::LocalInputMonitor* local_input_monitor =
+          remoting::LocalInputMonitor::Create();
       host = ChromotingHost::Create(
           &context, config,
           new DesktopEnvironment(capturer, event_executor, curtain,
-                                 disconnect_window),
+                                 disconnect_window, local_input_monitor),
           access_verifier.release());
     } else {
       host = ChromotingHost::Create(&context, config,
