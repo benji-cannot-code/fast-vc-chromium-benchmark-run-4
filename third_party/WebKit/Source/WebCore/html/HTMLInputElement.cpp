c83afb70f15c4a928f987c448df2d34ea9fdbe65
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-13 04:55:42 +0000

    2011-06-12  Hironori Bono  <hbono@chromium.org>
            Reviewed by Hajime Morita.
    
            Add null checks to HTMLTextAreaElement::removeSpellcheckRange().
            https://bugs.webkit.org/show_bug.cgi?id=62526
    
            This change adds null checks to the following function to prevent crashes
            when calling removeSpellcheckRange() with null:
            HTMLTextAreaElement::removeSpellcheckRange(),
            HTMLInputElement::removeSpellcheckRange(), and
            HTMLDivElement::removeSpellcheckRange().
    
            * editing/spelling/spellcheck-api-crash-expected.txt: Added.
            * editing/spelling/spellcheck-api-crash.html: Added.
    2011-06-12  Hironori Bono  <hbono@chromium.org>
    
            Reviewed by Hajime Morita.
    
            Add null checks to HTMLTextAreaElement::removeSpellcheckRange().
            https://bugs.webkit.org/show_bug.cgi?id=62526
    
            This change adds null checks to the following function to prevent crashes
            when calling removeSpellcheckRange() with null:
            HTMLTextAreaElement::removeSpellcheckRange(),
            HTMLInputElement::removeSpellcheckRange(), and
            HTMLDivElement::removeSpellcheckRange().
    
            Test: editing/spelling/spellcheck-api-crash.html
    
            * html/HTMLDivElement.cpp:
            (WebCore::HTMLDivElement::removeSpellcheckRange):
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::removeSpellcheckRange):
            * html/HTMLTextAreaElement.cpp:
            (WebCore::HTMLTextAreaElement::removeSpellcheckRange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
@@ -1862,6 +1862,8 @@ void HTMLInputElement::addSpellcheckRange(unsigned long start, unsigned long len
 
 void HTMLInputElement::removeSpellcheckRange(RefPtr<SpellcheckRange> range)
 {
+    if (!range)
+        return;
     document()->markers()->removeUserSpellingMarker(this, range->start(), range->length());
 }
 #endif
