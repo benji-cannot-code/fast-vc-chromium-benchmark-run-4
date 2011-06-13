03560ae1d7662faf1a23cea7a5a1c42e1c520de1
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-13 17:09:22 +0000

    Update rich touch information to conform to standard
    W3C Touch Event standard (draft)
    
    https://dvcs.w3.org/hg/webevents/raw-file/tip/touchevents.html
    
    introduces RadiusX, RadiusY field for touch events. This patch makes views::TouchEvent to have corresonding
    fields and proper default value extracted from XEvent, and pass them from RWHVV to WebKit side.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7129008
    Patch from Yufeng Shen <miletus@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_views.cc
+++ chrome/browser/renderer_host/render_widget_host_view_views.cc
@@ -108,6 +108,13 @@ WebKit::WebInputEvent::Type TouchEventTypeFromEvent(
   }
 }
 
+inline void UpdateTouchParams(const views::TouchEvent& event,
+                              WebKit::WebTouchPoint* tpoint) {
+  tpoint->radiusX = event.radius_x();
+  tpoint->radiusY = event.radius_y();
+  tpoint->rotationAngle = event.angle();
+}
+
 void UpdateTouchPointPosition(const views::TouchEvent* event,
                               const gfx::Point& origin,
                               WebKit::WebTouchPoint* tpoint) {
@@ -563,6 +570,8 @@ views::View::TouchStatus RenderWidgetHostViewViews::OnTouchEvent(
 
   if (status != TOUCH_STATUS_START)
     status = TOUCH_STATUS_CONTINUE;
+
+  UpdateTouchParams(event, point);
 
   // Update the location and state of the point.
   point->state = TouchPointStateFromEvent(&event);
