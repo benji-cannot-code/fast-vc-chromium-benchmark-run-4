benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit da62d902cc850a07d66f7ac9a3864ab6d7d71698

Make IndexedDBDispatcherHost be a message filter and move its messages into a separate file. Review URL: http://codereview.chromium.org/5680007
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69050 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/in_process_webkit/indexed_db_dispatcher_host.h
@@ -9,21 +9,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/id_map.h"
-#include "base/process.h"
-#include "base/ref_counted.h"
+#include "chrome/browser/browser_message_filter.h"
 #include "chrome/browser/in_process_webkit/webkit_context.h"
-#include "ipc/ipc_message.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebExceptionCode.h"
 
 class HostContentSettingsMap;
 class IndexedDBKey;
+class NullableString16;
 class Profile;
 class SerializedScriptValue;
-struct ViewHostMsg_IDBDatabaseCreateObjectStore_Params;
-struct ViewHostMsg_IDBFactoryOpen_Params;
-struct ViewHostMsg_IDBIndexOpenCursor_Params;
-struct ViewHostMsg_IDBObjectStoreCreateIndex_Params;
-struct ViewHostMsg_IDBObjectStoreOpenCursor_Params;
-struct ViewHostMsg_IDBObjectStorePut_Params;
+struct IndexedDBHostMsg_DatabaseCreateObjectStore_Params;
+struct IndexedDBHostMsg_FactoryOpen_Params;
+struct IndexedDBHostMsg_IndexOpenCursor_Params;
+struct IndexedDBHostMsg_ObjectStoreCreateIndex_Params;
+struct IndexedDBHostMsg_ObjectStoreOpenCursor_Params;
+struct IndexedDBHostMsg_ObjectStorePut_Params;
 
 namespace WebKit {
 class WebIDBCursor;
@@ -34,25 +34,17 @@ class WebIDBTransaction;
 }
 
 // Handles all IndexedDB related messages from a particular renderer process.
-class IndexedDBDispatcherHost
-    : public base::RefCountedThreadSafe<IndexedDBDispatcherHost> {
+class IndexedDBDispatcherHost : public BrowserMessageFilter {
  public:
   // Only call the constructor from the UI thread.
-  IndexedDBDispatcherHost(IPC::Message::Sender* sender, Profile* profile);
+  IndexedDBDispatcherHost(int process_id, Profile* profile);
 
-  // Only call from ResourceMessageFilter on the IO thread.
-  void Init(int process_id, base::ProcessHandle process_handle);
-
-  // Only call from ResourceMessageFilter on the IO thread.  Calls self on the
-  // WebKit thread in some cases.
-  void Shutdown();
-
-  // Only call from ResourceMessageFilter on the IO thread.
-  bool OnMessageReceived(const IPC::Message& message);
-
-  // Send a message to the renderer process associated with our sender_ via the
-  // IO thread.  May be called from any thread.
-  void Send(IPC::Message* message);
+  // BrowserMessageFilter implementation.
+  virtual void OnChannelClosing();
+  virtual void OverrideThreadForMessage(const IPC::Message& message,
+                                        BrowserThread::ID* thread);
+  virtual bool OnMessageReceived(const IPC::Message& message,
+                                 bool* message_was_ok);
 
   // A shortcut for accessing our context.
   IndexedDBContext* Context() {
@@ -68,24 +60,22 @@ class IndexedDBDispatcherHost
   int32 Add(WebKit::WebIDBTransaction* idb_transaction);
 
  private:
-  friend class base::RefCountedThreadSafe<IndexedDBDispatcherHost>;
   ~IndexedDBDispatcherHost();
 
   // Message processing. Most of the work is delegated to the dispatcher hosts
   // below.
-  void OnMessageReceivedWebKit(const IPC::Message& message);
-  void OnIDBFactoryOpen(const ViewHostMsg_IDBFactoryOpen_Params& p);
+  void OnIDBFactoryOpen(const IndexedDBHostMsg_FactoryOpen_Params& p);
 
   // Helper templates.
   template <class ReturnType>
   ReturnType* GetOrTerminateProcess(
     IDMap<ReturnType, IDMapOwnPointer>* map, int32 return_object_id,
-    IPC::Message* reply_msg, uint32 message_type);
+    uint32 message_type);
 
   template <typename ReplyType, typename MessageType,
             typename WebObjectType, typename Method>
   void SyncGetter(IDMap<WebObjectType, IDMapOwnPointer>* map, int32 object_id,
-                  IPC::Message* reply_msg, Method method);
+                  ReplyType* reply, Method method);
 
   template <typename ObjectType>
   void DestroyObject(IDMap<ObjectType, IDMapOwnPointer>* map, int32 object_id,
@@ -99,24 +89,26 @@ class IndexedDBDispatcherHost
     bool OnMessageReceived(const IPC::Message& message, bool *msg_is_ok);
     void Send(IPC::Message* message);
 
-    void OnName(int32 idb_database_id, IPC::Message* reply_msg);
-    void OnVersion(int32 idb_database_id, IPC::Message* reply_msg);
-    void OnObjectStoreNames(int32 idb_database_id, IPC::Message* reply_msg);
+    void OnName(int32 idb_database_id, string16* name);
+    void OnVersion(int32 idb_database_id, string16* version);
+    void OnObjectStoreNames(int32 idb_database_id,
+                            std::vector<string16>* object_stores);
     void OnCreateObjectStore(
-        const ViewHostMsg_IDBDatabaseCreateObjectStore_Params& params,
-        IPC::Message* reply_msg);
+        const IndexedDBHostMsg_DatabaseCreateObjectStore_Params& params,
+        int32* object_store_id, WebKit::WebExceptionCode* ec);
     void OnDeleteObjectStore(int32 idb_database_id,
                              const string16& name,
                              int32 transaction_id,
-                             IPC::Message* reply_msg);
+                             WebKit::WebExceptionCode* ec);
     void OnSetVersion(int32 idb_database_id,
                       int32 response_id,
                       const string16& version,
-                      IPC::Message* reply_msg);
+                      WebKit::WebExceptionCode* ec);
     void OnTransaction(int32 idb_database_id,
                        const std::vector<string16>& names,
                        int32 mode, int32 timeout,
-                       IPC::Message* reply_msg);
+                       int32* idb_transaction_id,
+                       WebKit::WebExceptionCode* ec);
     void OnDestroyed(int32 idb_database_id);
 
     IndexedDBDispatcherHost* parent_;
@@ -131,25 +123,25 @@ class IndexedDBDispatcherHost
     bool OnMessageReceived(const IPC::Message& message, bool *msg_is_ok);
     void Send(IPC::Message* message);
 
-    void OnName(int32 idb_index_id, IPC::Message* reply_msg);
-    void OnStoreName(int32 idb_index_id, IPC::Message* reply_msg);
-    void OnKeyPath(int32 idb_index_id, IPC::Message* reply_msg);
-    void OnUnique(int32 idb_index_id, IPC::Message* reply_msg);
+    void OnName(int32 idb_index_id, string16* name);
+    void OnStoreName(int32 idb_index_id, string16* store_name);
+    void OnKeyPath(int32 idb_index_id, NullableString16* key_path);
+    void OnUnique(int32 idb_index_id, bool* unique);
     void OnOpenObjectCursor(
-        const ViewHostMsg_IDBIndexOpenCursor_Params& params,
-        IPC::Message* reply_msg);
-    void OnOpenKeyCursor(const ViewHostMsg_IDBIndexOpenCursor_Params& params,
-                         IPC::Message* reply_msg);
+        const IndexedDBHostMsg_IndexOpenCursor_Params& params,
+        WebKit::WebExceptionCode* ec);
+    void OnOpenKeyCursor(const IndexedDBHostMsg_IndexOpenCursor_Params& params,
+                         WebKit::WebExceptionCode* ec);
     void OnGetObject(int idb_index_id,
                      int32 response_id,
                      const IndexedDBKey& key,
                      int32 transaction_id,
-                     IPC::Message* reply_msg);
+                     WebKit::WebExceptionCode* ec);
     void OnGetKey(int idb_index_id,
                   int32 response_id,
                   const IndexedDBKey& key,
                   int32 transaction_id,
-                  IPC::Message* reply_msg);
+                  WebKit::WebExceptionCode* ec);
     void OnDestroyed(int32 idb_index_id);
 
     IndexedDBDispatcherHost* parent_;
@@ -164,34 +156,37 @@ class IndexedDBDispatcherHost
     bool OnMessageReceived(const IPC::Message& message, bool *msg_is_ok);
     void Send(IPC::Message* message);
 
-    void OnName(int32 idb_object_store_id, IPC::Message* reply_msg);
-    void OnKeyPath(int32 idb_object_store_id, IPC::Message* reply_msg);
-    void OnIndexNames(int32 idb_object_store_id, IPC::Message* reply_msg);
+    void OnName(int32 idb_object_store_id, string16* name);
+    void OnKeyPath(int32 idb_object_store_id, NullableString16* keyPath);
+    void OnIndexNames(int32 idb_object_store_id,
+                      std::vector<string16>* index_names);
     void OnGet(int idb_object_store_id,
                int32 response_id,
                const IndexedDBKey& key,
                int32 transaction_id,
-               IPC::Message* reply_msg);
-    void OnPut(const ViewHostMsg_IDBObjectStorePut_Params& params,
-               IPC::Message* reply_msg);
+               WebKit::WebExceptionCode* ec);
+    void OnPut(const IndexedDBHostMsg_ObjectStorePut_Params& params,
+               WebKit::WebExceptionCode* ec);
     void OnDelete(int idb_object_store_id,
                   int32 response_id,
                   const IndexedDBKey& key,
                   int32 transaction_id,
-                  IPC::Message* reply_msg);
+                  WebKit::WebExceptionCode* ec);
     void OnCreateIndex(
-        const ViewHostMsg_IDBObjectStoreCreateIndex_Params& params,
-        IPC::Message* reply_msg);
+        const IndexedDBHostMsg_ObjectStoreCreateIndex_Params& params,
+        int32* index_id,
+        WebKit::WebExceptionCode* ec);
     void OnIndex(int32 idb_object_store_id,
                  const string16& name,
-                 IPC::Message* reply_msg);
+                 int32* idb_index_id,
+                 WebKit::WebExceptionCode* ec);
     void OnDeleteIndex(int32 idb_object_store_id,
                        const string16& name,
                        int32 transaction_id,
-                       IPC::Message* reply_msg);
+                       WebKit::WebExceptionCode* ec);
     void OnOpenCursor(
-        const ViewHostMsg_IDBObjectStoreOpenCursor_Params& params,
-        IPC::Message* reply_msg);
+        const IndexedDBHostMsg_ObjectStoreOpenCursor_Params& params,
+        WebKit::WebExceptionCode* ec);
     void OnDestroyed(int32 idb_object_store_id);
 
     IndexedDBDispatcherHost* parent_;
@@ -206,20 +201,22 @@ class IndexedDBDispatcherHost
     bool OnMessageReceived(const IPC::Message& message, bool *msg_is_ok);
     void Send(IPC::Message* message);
 
-    void OnDirection(int32 idb_object_store_id, IPC::Message* reply_msg);
-    void OnKey(int32 idb_object_store_id, IPC::Message* reply_msg);
-    void OnValue(int32 idb_object_store_id, IPC::Message* reply_msg);
+    void OnDirection(int32 idb_object_store_id, int32* direction);
+    void OnKey(int32 idb_object_store_id, IndexedDBKey* key);
+    void OnValue(int32 idb_object_store_id,
+                 SerializedScriptValue* script_value,
+                 IndexedDBKey* key);
     void OnUpdate(int32 idb_object_store_id,
                   int32 response_id,
                   const SerializedScriptValue& value,
-                  IPC::Message* reply_msg);
+                  WebKit::WebExceptionCode* ec);
     void OnContinue(int32 idb_object_store_id,
                     int32 response_id,
                     const IndexedDBKey& key,
-                    IPC::Message* reply_msg);
+                    WebKit::WebExceptionCode* ec);
     void OnDelete(int32 idb_object_store_id,
                   int32 response_id,
-                  IPC::Message* reply_msg);
+                  WebKit::WebExceptionCode* ec);
     void OnDestroyed(int32 idb_cursor_id);
 
     IndexedDBDispatcherHost* parent_;
@@ -236,10 +233,11 @@ class IndexedDBDispatcherHost
 
     // TODO: add the rest of the transaction methods.
     void OnAbort(int32 transaction_id);
-    void OnMode(int32 transaction_id, IPC::Message* reply_msg);
+    void OnMode(int32 transaction_id, int* mode);
     void OnObjectStore(int32 transaction_id,
                        const string16& name,
-                       IPC::Message* reply_msg);
+                       int32* object_store_id,
+                       WebKit::WebExceptionCode* ec);
     void OnDidCompleteTaskEvents(int transaction_id);
     void OnDestroyed(int32 idb_transaction_id);
 
@@ -247,9 +245,6 @@ class IndexedDBDispatcherHost
     typedef IDMap<WebKit::WebIDBTransaction, IDMapOwnPointer> MapType;
     MapType map_;
   };
-
-  // Only use on the IO thread.
-  IPC::Message::Sender* sender_;
 
   // Data shared between renderer processes with the same profile.
   scoped_refptr<WebKitContext> webkit_context_;
@@ -263,10 +258,6 @@ class IndexedDBDispatcherHost
   scoped_ptr<ObjectStoreDispatcherHost> object_store_dispatcher_host_;
   scoped_ptr<CursorDispatcherHost> cursor_dispatcher_host_;
   scoped_ptr<TransactionDispatcherHost> transaction_dispatcher_host_;
-
-  // If we get a corrupt message from a renderer, we need to kill it using this
-  // handle.
-  base::ProcessHandle process_handle_;
 
   // Used to dispatch messages to the correct view host.
   int process_id_;

chrome/browser/in_process_webkit/indexed_db_callbacks.cc
@@ -24,21 +24,21 @@ IndexedDBTransactionCallbacks::IndexedDBTransactionCallbacks(
 IndexedDBTransactionCallbacks::~IndexedDBTransactionCallbacks() {}
 
 void IndexedDBCallbacksBase::onError(const WebKit::WebIDBDatabaseError& error) {
-  dispatcher_host_->Send(new ViewMsg_IDBCallbacksError(
+  dispatcher_host_->Send(new IndexedDBMsg_CallbacksError(
       response_id_, error.code(), error.message()));
 }
 
 void IndexedDBTransactionCallbacks::onAbort() {
   dispatcher_host_->Send(
-      new ViewMsg_IDBTransactionCallbacksAbort(transaction_id_));
+      new IndexedDBMsg_TransactionCallbacksAbort(transaction_id_));
 }
 
 void IndexedDBTransactionCallbacks::onComplete() {
   dispatcher_host_->Send(
-      new ViewMsg_IDBTransactionCallbacksComplete(transaction_id_));
+      new IndexedDBMsg_TransactionCallbacksComplete(transaction_id_));
 }
 
 void IndexedDBTransactionCallbacks::onTimeout() {
   dispatcher_host_->Send(
-      new ViewMsg_IDBTransactionCallbacksTimeout(transaction_id_));
+      new IndexedDBMsg_TransactionCallbacksTimeout(transaction_id_));
 }

chrome/browser/in_process_webkit/indexed_db_callbacks.h
@@ -10,9 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/ref_counted.h"
 #include "chrome/browser/in_process_webkit/indexed_db_dispatcher_host.h"
-#include "chrome/common/indexed_db_key.h"
-#include "chrome/common/render_messages.h"
-#include "chrome/common/serialized_script_value.h"
+#include "chrome/common/indexed_db_messages.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebIDBCallbacks.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebIDBCursor.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebIDBDatabaseError.h"
@@ -23,16 +21,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // which (overloaded) onSuccess method we expect to be called.
 template <class Type> struct WebIDBToMsgHelper { };
 template <> struct WebIDBToMsgHelper<WebKit::WebIDBDatabase> {
-  typedef ViewMsg_IDBCallbacksSuccessIDBDatabase MsgType;
+  typedef IndexedDBMsg_CallbacksSuccessIDBDatabase MsgType;
 };
 template <> struct WebIDBToMsgHelper<WebKit::WebIDBIndex> {
-  typedef ViewMsg_IDBCallbacksSuccessIDBIndex MsgType;
+  typedef IndexedDBMsg_CallbacksSuccessIDBIndex MsgType;
 };
 template <> struct WebIDBToMsgHelper<WebKit::WebIDBObjectStore> {
-  typedef ViewMsg_IDBCallbacksSuccessIDBObjectStore MsgType;
+  typedef IndexedDBMsg_CallbacksSuccessIDBObjectStore MsgType;
 };
 template <> struct WebIDBToMsgHelper<WebKit::WebIDBTransaction> {
-  typedef ViewMsg_IDBCallbacksSuccessIDBTransaction MsgType;
+  typedef IndexedDBMsg_CallbacksSuccessIDBTransaction MsgType;
 };
 
 // The code the following two classes share.
@@ -91,11 +89,12 @@ class IndexedDBCallbacks<WebKit::WebIDBCursor>
   virtual void onSuccess(WebKit::WebIDBCursor* idb_object) {
     int32 object_id = dispatcher_host()->Add(idb_object);
     dispatcher_host()->Send(
-        new ViewMsg_IDBCallbacksSuccessIDBCursor(response_id(), object_id));
+        new IndexedDBMsg_CallbacksSuccessIDBCursor(response_id(), object_id));
   }
 
   virtual void onSuccess() {
-    dispatcher_host()->Send(new ViewMsg_IDBCallbacksSuccessNull(response_id()));
+    dispatcher_host()->Send(new IndexedDBMsg_CallbacksSuccessNull(
+        response_id()));
   }
 
  private:
@@ -115,7 +114,7 @@ class IndexedDBCallbacks<WebKit::WebIDBKey>
 
   virtual void onSuccess(const WebKit::WebIDBKey& value) {
     dispatcher_host()->Send(
-        new ViewMsg_IDBCallbacksSuccessIndexedDBKey(
+        new IndexedDBMsg_CallbacksSuccessIndexedDBKey(
             response_id(), IndexedDBKey(value)));
   }
 
@@ -136,7 +135,7 @@ class IndexedDBCallbacks<WebKit::WebSerializedScriptValue>
 
   virtual void onSuccess(const WebKit::WebSerializedScriptValue& value) {
     dispatcher_host()->Send(
-        new ViewMsg_IDBCallbacksSuccessSerializedScriptValue(
+        new IndexedDBMsg_CallbacksSuccessSerializedScriptValue(
             response_id(), SerializedScriptValue(value)));
   }
 
@@ -154,7 +153,7 @@ class IndexedDBCallbacks<void> : public IndexedDBCallbacksBase {
 
   virtual void onSuccess() {
     dispatcher_host()->Send(
-        new ViewMsg_IDBCallbacksSuccessNull(response_id()));
+        new IndexedDBMsg_CallbacksSuccessNull(response_id()));
   }
 
  private:

chrome/browser/renderer_host/browser_render_process_host.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/gpu_process_host.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/in_process_webkit/dom_storage_message_filter.h"
+#include "chrome/browser/in_process_webkit/indexed_db_dispatcher_host.h"
 #include "chrome/browser/io_thread.h"
 #include "chrome/browser/mime_registry_message_filter.h"
 #include "chrome/browser/platform_util.h"
@@ -399,6 +400,7 @@ void BrowserRenderProcessHost::CreateMessageFilters() {
   channel_->AddFilter(
       new AppCacheDispatcherHost(profile()->GetRequestContext(), id()));
   channel_->AddFilter(new DOMStorageMessageFilter(id(), profile()));
+  channel_->AddFilter(new IndexedDBDispatcherHost(id(), profile()));
   channel_->AddFilter(new PepperFileMessageFilter(id(), profile()));
   channel_->AddFilter(new speech_input::SpeechInputDispatcherHost(id()));
   channel_->AddFilter(

chrome/browser/renderer_host/resource_message_filter.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/geolocation/geolocation_permission_context.h"
 #include "chrome/browser/gpu_process_host.h"
 #include "chrome/browser/host_zoom_map.h"
-#include "chrome/browser/in_process_webkit/indexed_db_dispatcher_host.h"
 #include "chrome/browser/metrics/histogram_synchronizer.h"
 #include "chrome/browser/nacl_host/nacl_process_host.h"
 #include "chrome/browser/net/chrome_url_request_context.h"
@@ -247,8 +246,6 @@ ResourceMessageFilter::ResourceMessageFilter(
       media_request_context_(profile->GetRequestContextForMedia()),
       extensions_request_context_(profile->GetRequestContextForExtensions()),
       render_widget_helper_(render_widget_helper),
-      ALLOW_THIS_IN_INITIALIZER_LIST(indexed_db_dispatcher_host_(
-          new IndexedDBDispatcherHost(this, profile))),
       notification_prefs_(
           profile->GetDesktopNotificationService()->prefs_cache()),
       host_zoom_map_(profile->GetHostZoomMap()),
@@ -275,9 +272,6 @@ ResourceMessageFilter::ResourceMessageFilter(
 ResourceMessageFilter::~ResourceMessageFilter() {
   // This function should be called on the IO thread.
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-
-  // Tell the Indexed DB dispatcher host to stop sending messages via us.
-  indexed_db_dispatcher_host_->Shutdown();
 
   // Let interested observers know we are being deleted.
   NotificationService::current()->Notify(
@@ -306,7 +300,6 @@ void ResourceMessageFilter::OnChannelConnected(int32 peer_pid) {
   set_handle(peer_handle);
 
   WorkerService::GetInstance()->Initialize(resource_dispatcher_host_);
-  indexed_db_dispatcher_host_->Init(id(), handle());
 }
 
 void ResourceMessageFilter::OnChannelError() {
@@ -331,10 +324,9 @@ bool ResourceMessageFilter::OnMessageReceived(const IPC::Message& msg) {
   bool msg_is_ok = true;
   bool handled =
       resource_dispatcher_host_->OnMessageReceived(msg, this, &msg_is_ok) ||
-      indexed_db_dispatcher_host_->OnMessageReceived(msg) ||
       mp_dispatcher->OnMessageReceived(
           msg, this, next_route_id_callback(), &msg_is_ok) ||
-      geolocation_dispatcher_host_->OnMessageReceived(msg, &msg_is_ok) ;
+      geolocation_dispatcher_host_->OnMessageReceived(msg, &msg_is_ok);
 
   if (!handled) {
     DCHECK(msg_is_ok);  // It should have been marked handled if it wasn't OK.

chrome/browser/renderer_host/resource_message_filter.h
@@ -37,7 +37,6 @@ class ChromeURLRequestContext;
 struct FontDescriptor;
 class GeolocationDispatcherHostOld;
 class HostZoomMap;
-class IndexedDBDispatcherHost;
 class NotificationsPrefsCache;
 class PpapiPluginProcessHost;
 class Profile;
@@ -430,9 +429,6 @@ class ResourceMessageFilter : public IPC::ChannelProxy::MessageFilter,
   scoped_refptr<URLRequestContextGetter> extensions_request_context_;
 
   scoped_refptr<RenderWidgetHelper> render_widget_helper_;
-
-  // Handles Indexed Database related messages.
-  scoped_refptr<IndexedDBDispatcherHost> indexed_db_dispatcher_host_;
 
   // A cache of notifications preferences which is used to handle
   // Desktop Notifications permission messages.

chrome/chrome_common.gypi
@@ -86,6 +86,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'common/gpu_video_common.h',
           'common/indexed_db_key.cc',
           'common/indexed_db_key.h',
+          'common/indexed_db_messages.cc',
+          'common/indexed_db_messages.h',
           'common/indexed_db_param_traits.cc',
           'common/indexed_db_param_traits.h',
           'common/logging_chrome.cc',

commit 444a2106a3d04367c5cb339c527a484a49b58c41

In IndexedDBDispatcherHost::OnChannelClosing(), reset dispatcher hosts on the correct thread.
It's important that database_dispatcher_host_, et. al are reset on the WebKit
thread, as there may be pending tasks on that thread that need them.
This regressed in r69050.

BUG=84933
TEST=browser_tests --gtest_filter=IndexedDBBrowserTest.Bug84933Test

Review URL: http://codereview.chromium.org/6995111

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88972 0039d316-1c4b-4281-b951-d872f2087c98

content/browser/in_process_webkit/indexed_db_browsertest.cc
@@ -27,6 +27,10 @@ class IndexedDBBrowserTest : public InProcessBrowserTest {
     EnableDOMAutomation();
   }
 
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    command_line->AppendSwitch(switches::kUnlimitedQuotaForIndexedDB);
+  }
+
   GURL testUrl(const FilePath& file_path) {
     const FilePath kTestDir(FILE_PATH_LITERAL("indexeddb"));
     return ui_test_utils::GetTestUrl(kTestDir, file_path);
@@ -99,6 +103,13 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, DISABLED_DoesntHangTest) {
       FILE_PATH_LITERAL("transaction_run_forever.html"))));
   ui_test_utils::CrashTab(browser()->GetSelectedTabContents());
   SimpleTest(testUrl(FilePath(FILE_PATH_LITERAL("transaction_test.html"))));
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTest, Bug84933Test) {
+  const GURL url = testUrl(FilePath(FILE_PATH_LITERAL("bug_84933.html")));
+
+  // Just navigate to the URL. Test will crash if it fails.
+  ui_test_utils::NavigateToURLBlockUntilNavigationsComplete(browser(), url, 1);
 }
 
 // In proc browser test is needed here because ClearLocalState indirectly calls

content/browser/in_process_webkit/indexed_db_dispatcher_host.cc
@@ -81,18 +81,28 @@ IndexedDBDispatcherHost::~IndexedDBDispatcherHost() {
 
 void IndexedDBDispatcherHost::OnChannelClosing() {
   BrowserMessageFilter::OnChannelClosing();
-  BrowserThread::DeleteSoon(
-        BrowserThread::WEBKIT, FROM_HERE, database_dispatcher_host_.release());
-  BrowserThread::DeleteSoon(
-        BrowserThread::WEBKIT, FROM_HERE, index_dispatcher_host_.release());
-  BrowserThread::DeleteSoon(
-        BrowserThread::WEBKIT, FROM_HERE,
-        object_store_dispatcher_host_.release());
-  BrowserThread::DeleteSoon(
-        BrowserThread::WEBKIT, FROM_HERE, cursor_dispatcher_host_.release());
-  BrowserThread::DeleteSoon(
-        BrowserThread::WEBKIT, FROM_HERE,
-        transaction_dispatcher_host_.release());
+
+  bool success = BrowserThread::PostTask(
+      BrowserThread::WEBKIT, FROM_HERE,
+      NewRunnableMethod(this, &IndexedDBDispatcherHost::ResetDispatcherHosts));
+
+  if (!success)
+    ResetDispatcherHosts();
+}
+
+void IndexedDBDispatcherHost::ResetDispatcherHosts() {
+  // It is important that the various *_dispatcher_host_ members are reset
+  // on the WebKit thread, since there might be incoming messages on that
+  // thread, and we must not reset the dispatcher hosts until after those
+  // messages are processed.
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT) ||
+         CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess));
+
+  database_dispatcher_host_.reset();
+  index_dispatcher_host_.reset();
+  object_store_dispatcher_host_.reset();
+  cursor_dispatcher_host_.reset();
+  transaction_dispatcher_host_.reset();
 }
 
 void IndexedDBDispatcherHost::OverrideThreadForMessage(
@@ -109,8 +119,6 @@ bool IndexedDBDispatcherHost::OnMessageReceived(const IPC::Message& message,
 
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT));
 
-  // TODO(dgrogan): The page cycler test can crash here because
-  // database_dispatcher_host_ becomes invalid.
   bool handled =
       database_dispatcher_host_->OnMessageReceived(message, message_was_ok) ||
       index_dispatcher_host_->OnMessageReceived(message, message_was_ok) ||

content/browser/in_process_webkit/indexed_db_dispatcher_host.h
@@ -69,6 +69,8 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
   void OnIDBFactoryDeleteDatabase(
       const IndexedDBHostMsg_FactoryDeleteDatabase_Params& p);
 
+  void ResetDispatcherHosts();
+
   // Helper templates.
   template <class ReturnType>
   ReturnType* GetOrTerminateProcess(

chrome/test/data/indexeddb/bug_84933.html
@@ -1,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+  <head>
+    <title>IndexedDB regression test for bug 84933</title>
+    <script type="text/javascript" src="common.js"></script>
+    <script type="text/javascript" src="bug_84933.js"></script>
+  </head>
+  <body onLoad="test()">
+    <div id="status">Starting...</div>
+  </body>
+</html>
+

chrome/test/data/indexeddb/bug_84933.js
@@ -1,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function test()
+{
+  var n = 25;
+  for (var i = 0; i < n; i++) {
+    webkitIndexedDB.open("bug_84933_" + i.toString()).onsuccess = function() {
+      window.close();
+    }
+  }
+}