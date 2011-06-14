46464b0250da3a8c1df3eb4ac2e30769a152925b
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 18:43:47 +0000

    Sync: Close the sync UI in a few more tests.
    BUG=86006
    TEST=SyncSetupWizardTest.*
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/7147009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_setup_wizard_unittest.cc
+++ chrome/browser/sync/sync_setup_wizard_unittest.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(jhawkins): Rewrite these tests to handle the new inlined sync UI.
-
 #include "chrome/browser/sync/sync_setup_wizard.h"
 
 #include "base/json/json_writer.h"
@@ -360,6 +358,7 @@ TEST_F(SyncSetupWizardTest, EnterPassphraseRequired) {
                                 "\"mode\":\"gaia\"}"));
   handler_.HandlePassphraseEntry(&value);
   EXPECT_EQ("myPassphrase", service_->passphrase_);
+  CloseSetupUI();
 }
 
 TEST_F(SyncSetupWizardTest, DialogCancelled) {
@@ -408,6 +407,7 @@ TEST_F(SyncSetupWizardTest, InvalidTransitions) {
 
   wizard_->Step(SyncSetupWizard::FATAL_ERROR);
   EXPECT_EQ(SyncSetupWizard::FATAL_ERROR, flow_->current_state_);
+  CloseSetupUI();
 }
 
 TEST_F(SyncSetupWizardTest, FullSuccessfulRunSetsPref) {
@@ -486,6 +486,7 @@ TEST_F(SyncSetupWizardTest, DiscreteRunGaiaLogin) {
   service_->set_auth_state(kTestUser, AuthError::None());
 
   wizard_->Step(SyncSetupWizard::GAIA_SUCCESS);
+  CloseSetupUI();
 }
 
 TEST_F(SyncSetupWizardTest, NonFatalError) {
