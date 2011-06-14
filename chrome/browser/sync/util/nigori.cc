cc51372a185077de24a58d9b372232d7197a486c
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 21:05:16 +0000

    Revert 89038 - [Sync] Ensure cryptographer ready before encrypting.
    Also, make cryptographer more robust to errors and add error logging for cases
    where the cryptographer is not ready when we expect it to be.
    
    BUG=81152
    TEST=new cryptographer unit tests and synapi unit tests.
    
    
    Review URL: http://codereview.chromium.org/7108067
    
    TBR=zea@chromium.org
    Review URL: http://codereview.chromium.org/7150024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89068 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/util/nigori.cc
+++ chrome/browser/sync/util/nigori.cc
@@ -169,8 +169,7 @@ std::string GenerateRandomString(size_t size) {
 
 // Enc[Kenc,Kmac](value)
 bool Nigori::Encrypt(const std::string& value, std::string* encrypted) const {
-  if (0U >= value.size())
-    return false;
+  DCHECK_LT(0U, value.size());
 
   std::string iv = GenerateRandomString(kIvSize);
 
