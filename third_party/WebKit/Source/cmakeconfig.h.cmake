87e7e6436b3c72cea21d9392f3e17b077d3bf657
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 16:29:17 +0000

    2011-06-14  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Eric Seidel.
    
            [CMAKE] Add ENABLE_TOUCH_ICON_LOADING feature.
            https://bugs.webkit.org/show_bug.cgi?id=62604
    
            Add feature to enable or disable ENABLE_TOUCH_ICON which supports
            apple-touch-icon and apple-touch-icon-precomposed in link tag.
    
            * Source/cmake/OptionsEfl.cmake:
            * Source/cmakeconfig.h.cmake:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmakeconfig.h.cmake
+++ third_party/WebKit/Source/cmakeconfig.h.cmake
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #cmakedefine01 ENABLE_SVG_FOREIGN_OBJECT
 #cmakedefine01 ENABLE_SVG_USE
 #cmakedefine01 ENABLE_TOUCH_EVENTS
+#cmakedefine01 ENABLE_TOUCH_ICON_LOADING
 #cmakedefine01 ENABLE_VIDEO
 #cmakedefine01 ENABLE_WEBGL
 #cmakedefine01 ENABLE_WEB_SOCKETS
