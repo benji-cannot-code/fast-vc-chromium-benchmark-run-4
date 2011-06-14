48592ad4c394f0d09bbe1115759483a28dac0cef
jrt@chromium.org <jrt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 23:52:38 +0000

    Extended the frame rate testing framework.
    Redesigned the scrolling behaviors, or "gestures," to be independent of the frame rate and allow for human-like tests. Added test examples: steady, reading, mouse wheel, and Mac "fling." Also added the ability to record new scrolling behaviors.
    
    Review URL: http://codereview.chromium.org/7134031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89101 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/perf/frame_rate/frame_rate_tests.cc
+++ chrome/test/perf/frame_rate/frame_rate_tests.cc
@@ -35,7 +35,8 @@ class FrameRateTest : public UIPerfTest {
     return test_path;
   }
 
-  void RunTest(const std::string& name, const std::string& suffix) {
+  void RunTest(const std::string& name, const std::string& gesture,
+               const std::string& suffix) {
     FilePath test_path = GetDataPath(name);
     ASSERT_TRUE(file_util::DirectoryExists(test_path))
         << "Missing test directory: " << test_path.value();
@@ -49,7 +50,8 @@ class FrameRateTest : public UIPerfTest {
               tab->NavigateToURL(net::FilePathToFileURL(test_path)));
 
     // Start the test.
-    ASSERT_TRUE(tab->NavigateToURLAsync(GURL("javascript:__start();")));
+    ASSERT_TRUE(tab->NavigateToURLAsync(GURL("javascript:__start('" +
+        gesture + "');")));
 
     // Block until the test completes.
     ASSERT_TRUE(WaitUntilJavaScriptCondition(
@@ -96,8 +98,20 @@ class FrameRateTest_Reference : public FrameRateTest {
   }
 };
 
-TEST_F(FrameRateTest, Blank) {
-  RunTest("blank", "");
+TEST_F(FrameRateTest, Blank_Steady) {
+  RunTest("blank", "steady", "");
+}
+
+TEST_F(FrameRateTest, Blank_Reading) {
+  RunTest("blank", "reading", "");
+}
+
+TEST_F(FrameRateTest, Blank_Mouse_Wheel) {
+  RunTest("blank", "mouse_wheel", "");
+}
+
+TEST_F(FrameRateTest, Blank_Mac_Fling) {
+  RunTest("blank", "mac_fling", "");
 }
 
 // TODO(darin): Need to update the reference build to a version that supports
