a85cba022ea2089ef9b36f89d35f4104e99f267b
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 13:42:40 +0000

    2011-06-14  Andreas Kling  <kling@webkit.org>
            Reviewed by Benjamin Poulain.
    
            [Qt] Don't include convenience "QWebFoo" headers in WebKit code.
            https://bugs.webkit.org/show_bug.cgi?id=62632
    
            * WebCoreSupport/FrameNetworkingContextQt.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameNetworkingContextQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameNetworkingContextQt.cpp
@@ -22,10 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FrameNetworkingContextQt.h"
 
+#include "qwebframe.h"
+#include "qwebpage.h"
 #include <QNetworkAccessManager>
 #include <QObject>
-#include <QWebFrame>
-#include <QWebPage>
 
 namespace WebCore {
 
