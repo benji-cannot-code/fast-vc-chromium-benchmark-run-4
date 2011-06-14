9f7d80623db7aada49eb5f52beaf374b7ad4fc51
qi.2.zhang@nokia.com <qi.2.zhang@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 18:44:25 +0000

    2011-06-14  Qi Zhang  <qi.2.zhang@nokia.com>
            Reviewed by Laszlo Gombos.
    
            [Qt] fast/dom/HTMLScriptElement/nested-execution.html failed
            https://bugs.webkit.org/show_bug.cgi?id=62227
    
            * platform/qt/Skipped:
    2011-06-14  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Laszlo Gombos.
    
            [Qt] fast/dom/HTMLScriptElement/nested-execution.html failed
            https://bugs.webkit.org/show_bug.cgi?id=62227
    
            QtWebkit does not yet support different CacheModels. This change will
            expose setCacheModel() with a stub implementation, which is enough to pass the LayoutTest.
    
            * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
            (LayoutTestController::setCacheModel):
            * DumpRenderTree/qt/LayoutTestControllerQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
+++ third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
@@ -198,6 +198,7 @@ public slots:
     void setApplicationCacheOriginQuota(unsigned long long quota);
     QStringList originsWithApplicationCache();
     long long applicationCacheDiskUsageForOrigin(const QString&); 
+    void setCacheModel(int);
 
     void setDatabaseQuota(int size);
     void clearAllDatabases();
