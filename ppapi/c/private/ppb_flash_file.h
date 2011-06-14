aef2e40810e9e90c7f2f50f3fd35eeb13f37aa4f
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 02:08:50 +0000

    Add missing include for ppb_flash_file.h
    TEST=None
    BUG=None
    
    Review URL: http://codereview.chromium.org/7148003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88946 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_flash_file.h
+++ ppapi/c/private/ppb_flash_file.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_instance.h"
+#include "ppapi/c/pp_resource.h"
 
 #ifdef _WIN32
 typedef HANDLE PP_FileHandle;
