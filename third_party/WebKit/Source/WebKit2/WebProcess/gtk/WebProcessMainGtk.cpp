fdce9aabf75c2a7b39dd48aba5f9f3aa8ddb51ee
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 14:36:50 +0000

    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Support authentication dialogs in WebKit2
            https://bugs.webkit.org/show_bug.cgi?id=62366
    
            Add a new class to show an authentication dialog that is used by
            both WebKit1 and WebKit2.
    
            * GNUmakefile.list.am: Add new files to compilation.
            * platform/gtk/GtkAuthenticationDialog.cpp: Added.
            (WebCore::addEntryToTable):
            (WebCore::sessionCanSavePasswords):
            (WebCore::GtkAuthenticationDialog::~GtkAuthenticationDialog):
            (WebCore::GtkAuthenticationDialog::GtkAuthenticationDialog):
            (WebCore::getSavedLogin):
            (WebCore::GtkAuthenticationDialog::show):
            (WebCore::GtkAuthenticationDialog::destroy):
            (WebCore::GtkAuthenticationDialog::savePasswordCallback):
            (WebCore::GtkAuthenticationDialog::savePassword):
            (WebCore::GtkAuthenticationDialog::authenticate):
            (WebCore::GtkAuthenticationDialog::authenticationDialogResponseCallback):
            * platform/gtk/GtkAuthenticationDialog.h: Added.
    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Support authentication dialogs in WebKit2
            https://bugs.webkit.org/show_bug.cgi?id=62366
    
            Move the common code of webkitsoupauthdialog to a common C++ class
            in WebCore so that it can be used by WebKit2 too. The file has
            been converted into a C++ file.
    
            * GNUmakefile.am: Rename webkitsoupauthdialog.c to
            webkitsoupauthdialog.cpp.
            * webkit/webkitsoupauthdialog.c: Removed.
            * webkit/webkitsoupauthdialog.cpp: Added.
            (webkit_soup_auth_dialog_class_init):
            (webkit_soup_auth_dialog_init):
            (webkit_soup_auth_dialog_session_feature_init):
            (sessionAuthenticate): Use GtkAuthenticationDialog object from
            WebCore.
            (attach):
            (detach):
            * webkit/webkitsoupauthdialog.h:
    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Support authentication dialogs in WebKit2
            https://bugs.webkit.org/show_bug.cgi?id=62366
    
            Add a new soup feature to show an authentication dialog when it
            gets a 401 HTTP response.
    
            * GNUmakefile.am: Add new files to compilation.
            * WebProcess/gtk/WebAuthDialog.cpp: Added.
            (web_auth_dialog_class_init):
            (web_auth_dialog_init):
            (sessionAuthenticate):
            (attach):
            (detach):
            (webAuthDialogSessionFeatureInit):
            * WebProcess/gtk/WebAuthDialog.h: Added.
            * WebProcess/gtk/WebProcessMainGtk.cpp:
            (WebKit::WebProcessMainGtk):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebProcessMainGtk.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebProcessMainGtk.cpp
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebProcessMainGtk.h"
 
+#include "WebAuthDialog.h"
 #include "WKBase.h"
+#include <WebCore/GtkAuthenticationDialog.h>
 #include <WebCore/ResourceHandle.h>
 #include <WebKit2/RunLoop.h>
 #include <WebKit2/WebProcess.h>
@@ -57,11 +59,13 @@ WK_EXPORT int WebProcessMainGtk(int argc, char* argv[])
     RunLoop::initializeMainRunLoop();
     SoupSession* session = WebCore::ResourceHandle::defaultSession();
 
-    SoupSessionFeature* sniffer = static_cast<SoupSessionFeature*>(g_object_new(SOUP_TYPE_CONTENT_SNIFFER, NULL));
-    soup_session_add_feature(session, sniffer);
-    g_object_unref(sniffer);
+    GRefPtr<SoupSessionFeature> sniffer = adoptGRef(static_cast<SoupSessionFeature*>(g_object_new(SOUP_TYPE_CONTENT_SNIFFER, NULL)));
+    soup_session_add_feature(session, sniffer.get());
 
     soup_session_add_feature_by_type(session, SOUP_TYPE_CONTENT_DECODER);
+
+    GRefPtr<SoupSessionFeature> authDialog = adoptGRef(static_cast<SoupSessionFeature*>(g_object_new(WEB_TYPE_AUTH_DIALOG, NULL)));
+    soup_session_add_feature(session, authDialog.get());
 
     int socket = atoi(argv[1]);
     WebProcess::shared().initialize(socket, RunLoop::main());
