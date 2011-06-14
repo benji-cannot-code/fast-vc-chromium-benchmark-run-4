52b6a5a424b2b3ee0f321df8ac7341a62f1d0083
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 14:58:30 +0000

    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Rename convertWidgetRectToScreenRect() to convertWidgetPointToScreenPoint()
            https://bugs.webkit.org/show_bug.cgi?id=62626
    
            The method actually converts coordinates, the rectangle size is
            not affected, so it can be modified to return a point
            instead. Fix also a bug in the implementation, it was translating
            the coordinates twice returning the wrong position when the given
            rectangle was not at 0,0.
    
            * platform/gtk/GtkUtilities.cpp:
            (WebCore::convertWidgetPointToScreenPoint):
            * platform/gtk/GtkUtilities.h:
            * platform/gtk/PopupMenuGtk.cpp:
            (WebCore::PopupMenuGtk::show): Use
            convertWidgetPointToScreenPoint().
    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Rename convertWidgetRectToScreenRect() to convertWidgetPointToScreenPoint()
            https://bugs.webkit.org/show_bug.cgi?id=62626
    
            * WebCoreSupport/ChromeClientGtk.cpp:
            (WebKit::ChromeClient::windowToScreen): Update to use
            convertWidgetPointToScreenPoint()
            (WebKit::ChromeClient::screenToWindow): Ditto.
            * webkit/webkitwebview.cpp: Remove globalPointForClientPoint()
            since it does the same than convertWidgetPointToScreenPoint().
            (webkit_web_view_popup_menu_handler): Use
            convertWidgetPointToScreenPoint() instead of globalPointForClientPoint().
            (doDragLeaveLater): Ditto.
            (webkit_web_view_drag_motion): Ditto.
            (webkit_web_view_drag_data_received): Ditto.
            (webkit_web_view_drag_drop): Ditto.
    2011-06-14  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] Rename convertWidgetRectToScreenRect() to convertWidgetPointToScreenPoint()
            https://bugs.webkit.org/show_bug.cgi?id=62626
    
            * UIProcess/API/gtk/PageClientImpl.cpp:
            (WebKit::PageClientImpl::screenToWindow): Implement this method
            using convertWidgetPointToScreenPoint().
            (WebKit::PageClientImpl::windowToScreen): Update to use
            convertWidgetPointToScreenPoint().
            * UIProcess/gtk/WebPopupMenuProxyGtk.cpp:
            (WebKit::WebPopupMenuProxyGtk::showPopupMenu): Use
            convertWidgetPointToScreenPoint().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/GtkUtilities.h
+++ third_party/WebKit/Source/WebCore/platform/gtk/GtkUtilities.h
@@ -22,9 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class IntRect;
+class IntPoint;
 
-IntRect convertWidgetRectToScreenRect(GtkWidget*, const IntRect&);
+IntPoint convertWidgetPointToScreenPoint(GtkWidget*, const IntPoint&);
 
 } // namespace WebCore
 
