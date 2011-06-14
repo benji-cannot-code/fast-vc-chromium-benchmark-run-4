8cd9a3343326338faeaf2f36f2ff81784bae2b91
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 19:03:16 +0000

    Change the files in base and net to include foundation_util.h instead of mac_util.h.
    R=rohitrao@chromium.org,mark@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7145016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89040 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_paths_mac.mm
+++ base/base_paths_mac.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/logging.h"
-#include "base/mac/mac_util.h"
+#include "base/mac/foundation_util.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
 
