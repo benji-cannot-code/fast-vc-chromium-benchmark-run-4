7cbd9312a974342e42d06a878e602d778299bec4
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 20:14:34 +0000

    Add plumbing to toggle the printing backgrounds web setting. No behavior change.
    BUG=2294
    TEST=none
    
    Review URL: http://codereview.chromium.org/7149004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89059 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -81,7 +81,8 @@ WebPreferences::WebPreferences()
       interactive_form_validation_enabled(true),
       fullscreen_enabled(false),
       allow_displaying_insecure_content(true),
-      allow_running_insecure_content(false) {
+      allow_running_insecure_content(false),
+      should_print_backgrounds(false) {
 }
 
 WebPreferences::~WebPreferences() {
@@ -230,4 +231,5 @@ void WebPreferences::Apply(WebView* web_view) const {
   settings->setFullScreenEnabled(fullscreen_enabled);
   settings->setAllowDisplayOfInsecureContent(allow_displaying_insecure_content);
   settings->setAllowRunningOfInsecureContent(allow_running_insecure_content);
+  settings->setShouldPrintBackgrounds(should_print_backgrounds);
 }
