a8aedb5ddbaf2f6ddbb49a93d2ef2ddae72b6ed6
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-14 17:44:45 +0000

    2011-06-13  Adrienne Walker  <enne@google.com>
            Reviewed by James Robinson.
    
            [chromium] Disable drawing for huge mask layers
            https://bugs.webkit.org/show_bug.cgi?id=62607
    
            * platform/chromium/compositing/huge-mask-layer-expected.txt: Added.
            * platform/chromium/compositing/huge-mask-layer.html: Added.
    2011-06-13  Adrienne Walker  <enne@google.com>
    
            Reviewed by James Robinson.
    
            [chromium] Disable drawing for huge mask layers
            https://bugs.webkit.org/show_bug.cgi?id=62607
    
            Because masks have a different layer size than the layer they are
            masking, they are untiled.  If they are too large to be contained
            within a single texture, then they should just be disabled.
    
            Test: platform/chromium/compositing/huge-mask-layer.html
    
            * platform/graphics/chromium/ContentLayerChromium.cpp:
            (WebCore::ContentLayerChromium::drawsContent):
            (WebCore::ContentLayerChromium::paintContentsIfDirty):
            * platform/graphics/chromium/LayerTilerChromium.h:
            (WebCore::LayerTilerChromium::getSingleTexture):
            (WebCore::LayerTilerChromium::numTiles):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@88822 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.cpp
@@ -84,6 +84,9 @@ void LayerTilerChromium::setTileSize(const IntSize& size)
 
 LayerTexture* LayerTilerChromium::getSingleTexture()
 {
+    if (m_tilingData.numTiles() != 1)
+        return 0;
+
     Tile* tile = tileAt(0, 0);
     return tile ? tile->texture() : 0;
 }
