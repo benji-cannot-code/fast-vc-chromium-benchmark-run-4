f63c0e6ac57b6b39012e6e3d85f178455bb00692
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 01:44:32 +0000

    Add profiles to wrench menu
    This change adds a profile menu to the wrench menu.
    
    If the user has one profile then the wrench menu looks like this:
      Wrench Menu >
        ...
        Downloads
        -------
        New Browsing Profile...
        -------
        ...
    
    If the user has two or more profile then the wrench menu looks like this:
       Wrench Menu >
        ...
        Downloads
        -------
        Browsing Profiles >
           Profile 1
           Profile 2
           --------
           New Browsing Profile...
        -------
        ...
    
    BUG=None
    TEST=None
    Review URL: http://codereview.chromium.org/7138002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CHROME_APP_CHROME_COMMAND_IDS_H_
+#define CHROME_APP_CHROME_COMMAND_IDS_H_
+#pragma once
+
 // This file lists all the command IDs understood by e.g. the browser.
 // It is used by Windows RC files, Mac NIB files, and other platforms too.
 
@@ -167,6 +171,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_PROFILING_ENABLED           40030
 #define IDC_FILE_MANAGER                40031
 #define IDC_BOOKMARKS_MENU              40032
+#define IDC_PROFILE_MENU                40033
+#define IDC_CREATE_NEW_PROFILE          40034
 
 // Spell-check
 // Insert any additional suggestions before _LAST; these have to be consecutive.
@@ -277,3 +283,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Context menu items in the status tray
 #define IDC_STATUS_TRAY_KEEP_CHROME_RUNNING_IN_BACKGROUND 51100
+
+#endif  // CHROME_APP_CHROME_COMMAND_IDS_H_
