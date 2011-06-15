592e0155a23356cbf6520a2e90dfff82899de3e4
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 09:52:12 +0000

    Uninitialized struct member in ProviderInterface::Rule.
    BUG=None
    TEST=None
    CID=16917
    Review URL: http://codereview.chromium.org/7108057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89161 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_provider.cc
+++ chrome/browser/content_settings/content_settings_provider.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content_settings {
 
-ProviderInterface::Rule::Rule() {
+ProviderInterface::Rule::Rule()
+    : content_setting(CONTENT_SETTING_DEFAULT) {
 }
 
 ProviderInterface::Rule::Rule(const ContentSettingsPattern& requesting_pattern,
