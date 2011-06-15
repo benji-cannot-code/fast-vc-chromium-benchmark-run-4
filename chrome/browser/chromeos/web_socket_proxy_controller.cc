de3b76f5d3c2e593a4b675975e6be8711e586383
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 12:33:29 +0000

    Extend access to webSocketProxyPrivate and decodeJPEG private APIs.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7170013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89169 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/web_socket_proxy_controller.cc
+++ chrome/browser/chromeos/web_socket_proxy_controller.cc
@@ -26,10 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char* kWebAppId = "haiffjcadagjlijoggckpgfnoeiflnem";
+const char* kSomeWebAppId    = "haiffjcadagjlijoggckpgfnoeiflnem";
+const char* kAnotherWebAppId = "gnedhmakppccajfpfiihfcdlnpgomkcf";
 
 const char* kAllowedIds[] = {
-    kWebAppId
+    kSomeWebAppId,
+    kAnotherWebAppId
 };
 
 class OriginValidator {
