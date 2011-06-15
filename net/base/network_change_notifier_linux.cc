66dc8b14c131c26ebc7abfdb3cb78f9b7e11e922
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 17:59:34 +0000

    Fix memset bug in NetworkChangeNotifierLinux.
    Props to thakis/clang for catching this.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7167009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89212 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier_linux.cc
+++ net/base/network_change_notifier_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -133,7 +133,7 @@ int NetworkChangeNotifierLinux::Thread::ReadNotificationMessage(
     size_t len) {
   DCHECK_NE(len, 0u);
   DCHECK(buf);
-  memset(buf, 0, sizeof(buf));
+  memset(buf, 0, len);
   int rv = recv(netlink_fd_, buf, len, 0);
   if (rv > 0)
     return rv;
