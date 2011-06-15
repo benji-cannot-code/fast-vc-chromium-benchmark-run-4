cf59cc0331fc3537908e364801decd80fc115b0c
vitalyr@chromium.org <vitalyr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 14:26:56 +0000

    WebKit roll 88829:88857.
    Also add missing WebString include to cros_mount_point_provider.cc.
    
    Review URL: http://codereview.chromium.org/7170017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89183 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/chromeos/fileapi/cros_mount_point_provider.cc
+++ webkit/chromeos/fileapi/cros_mount_point_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "base/synchronization/lock.h"
 #include "base/utf_string_conversions.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFileSystem.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityOrigin.h"
@@ -189,4 +190,3 @@ bool CrosMountPointProvider::GetVirtualPath(const FilePath& filesystem_path,
 }
 
 }  // namespace chromeos
-
