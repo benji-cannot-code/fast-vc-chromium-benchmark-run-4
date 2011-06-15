b4a094991ddcae1cd1ce93c5f93b99c459e5fc78
shinyak@google.com <shinyak@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 12:33:48 +0000

    added right click check.
    BUG=27882
    TEST=Observe that right click works on the context menu on Windows.
    
    Review URL: http://codereview.chromium.org/6995152
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89170 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/native_menu_win.cc
+++ views/controls/menu/native_menu_win.cc
@@ -400,7 +400,7 @@ NativeMenuWin::~NativeMenuWin() {
 void NativeMenuWin::RunMenuAt(const gfx::Point& point, int alignment) {
   CreateHostWindow();
   UpdateStates();
-  UINT flags = TPM_LEFTBUTTON | TPM_RECURSE;
+  UINT flags = TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RECURSE;
   flags |= GetAlignmentFlags(alignment);
   menu_action_ = MENU_ACTION_NONE;
 
@@ -568,7 +568,7 @@ LRESULT CALLBACK NativeMenuWin::MenuMessageHook(
   }
 
   MSG* msg = reinterpret_cast<MSG*>(l_param);
-  if (msg->message == WM_LBUTTONUP) {
+  if (msg->message == WM_LBUTTONUP || msg->message == WM_RBUTTONUP) {
     HighlightedMenuItemInfo info;
     if (GetHighlightedMenuItemInfo(this_ptr->menu_, &info) && info.menu) {
       // It appears that when running a menu by way of TrackPopupMenu(Ex) win32
