b3900236a35cedfecc158231375bc0cf87092a92
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 12:13:32 +0000

    Stub WebUI handler for Update screen.
    BUG=chromium-os:15923
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7003110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89323 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/webui/chromeos/login/update_screen_handler.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_UPDATE_SCREEN_HANDLER_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_UPDATE_SCREEN_HANDLER_H_
+
+#include "chrome/browser/chromeos/login/update_screen_actor.h"
+#include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
+
+namespace chromeos {
+
+class UpdateScreenHandler : public UpdateScreenActor,
+                            public OobeMessageHandler {
+ public:
+  UpdateScreenHandler();
+  virtual ~UpdateScreenHandler();
+
+  // OobeMessageHandler implementation:
+  virtual void GetLocalizedStrings(DictionaryValue* localized_strings);
+  virtual void Initialize();
+
+  // UpdateScreenActor implementation:
+  virtual void Show();
+  virtual void Hide();
+  virtual void PrepareToShow();
+  virtual void ShowManualRebootInfo();
+  virtual void SetProgress(int progress);
+  virtual void ShowCurtain(bool enable);
+  virtual void ShowPreparingUpdatesInfo(bool visible);
+
+  // WebUIMessageHandler implementation:
+  virtual void RegisterMessages();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(UpdateScreenHandler);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_UPDATE_SCREEN_HANDLER_H_
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -3319,6 +3319,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/webui/chromeos/login/network_screen_handler.h',
         'browser/ui/webui/chromeos/login/oobe_ui.cc',
         'browser/ui/webui/chromeos/login/oobe_ui.h',
+        'browser/ui/webui/chromeos/login/update_screen_handler.cc',
+        'browser/ui/webui/chromeos/login/update_screen_handler.h',
         'browser/ui/webui/chromeos/mobile_setup_ui.cc',
         'browser/ui/webui/chromeos/mobile_setup_ui.h',
         'browser/ui/webui/chromeos/proxy_settings_ui.cc',
