b85370f61782284cab7e095ba73c9c794ed97244
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 22:53:13 +0000

    Clean up no longer needed prerender_contents includes.
    Also add OVERRIDE where it's missing.
    
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/7184019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89417 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_contents.h
+++ chrome/browser/prerender/prerender_contents.h
@@ -13,20 +13,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "chrome/browser/prerender/prerender_final_status.h"
-#include "chrome/browser/prerender/prerender_render_view_host_observer.h"
 #include "chrome/browser/tab_contents/render_view_host_delegate_helper.h"
 #include "chrome/browser/ui/download/download_tab_helper_delegate.h"
 #include "content/browser/tab_contents/tab_contents_observer.h"
 #include "content/common/notification_registrar.h"
-#include "content/common/window_container_type.h"
-#include "webkit/glue/window_open_disposition.h"
 
 class RenderViewHost;
 class TabContents;
 class TabContentsWrapper;
 struct FaviconURL;
 struct ViewHostMsg_FrameNavigate_Params;
-struct WebPreferences;
 
 namespace base {
 class ProcessMetrics;
@@ -39,6 +35,7 @@ class Rect;
 namespace prerender {
 
 class PrerenderManager;
+class PrerenderRenderViewHostObserver;
 class PrerenderTracker;
 
 // This class is a peer of TabContents. It can host a renderer, but does not
