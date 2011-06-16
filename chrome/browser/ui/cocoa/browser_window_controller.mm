6adddaaa6efe4a4b8a635f21f7fa6eef3c67b2dc
shinyak@google.com <shinyak@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 04:17:17 +0000

    Reverted r88133 (http://codereview.chromium.org/7065054/) , and removed only tab count check.
    XIB Changes:
    * Reverted the change r88133.
      * Make the default key equivalent of 'Close Tab' nothing as the previous one is so.
      * Make the default key equivalent of 'Close Window' Cmd+w as well.
      * Added the referencing outlets from 'Close Tab' and 'Close Window' as well.
    
    BUG=85727
    TEST=Observe that About dialog and task manager can be closed with Cmd+w. Also a usual window having only one tab can be closed with Cmd+Shift+w.
    
    
    Review URL: http://codereview.chromium.org/7097010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1000,7 +1000,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       enable = browser_->command_updater()->IsCommandEnabled(tag);
       switch (tag) {
         case IDC_CLOSE_TAB:
-          // Disable "close tab" if we're not the key window.
+          // Disable "close tab" if we're not the key window or if there's only
+          // one tab.
           enable &= [[self window] isKeyWindow];
           break;
         case IDC_FULLSCREEN: {
