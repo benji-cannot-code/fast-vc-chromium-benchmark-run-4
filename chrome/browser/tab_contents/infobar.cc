7eafce798ca5fc45903db1679e1dc2b3d2a5ea2d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 01:51:28 +0000

    infobars: Use TCW directly in InfoBar to remove the delegate.
    Note: This is a TODO in infobar.h for pkasting.
    
    BUG=None
    TEST=None
    
    R=pkasting@chromium.org
    
    Review URL: http://codereview.chromium.org/7182004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/infobar.cc
+++ chrome/browser/tab_contents/infobar.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/browser/tab_contents/infobar_container.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "ui/base/animation/slide_animation.h"
 
 SkColor GetInfoBarTopColor(InfoBarDelegate::Type infobar_type) {
@@ -89,8 +90,7 @@ void InfoBar::AnimationProgressed(const ui::Animation* animation) {
 }
 
 void InfoBar::RemoveInfoBar() {
-  if (container_)
-    container_->RemoveDelegate(delegate_);
+  owner_->RemoveInfoBar(delegate_);
 }
 
 void InfoBar::SetBarTargetHeight(int height) {
