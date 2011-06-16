6910692291935f23f6159e596da0f711e4c9a8b7
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 00:27:21 +0000

    Fixed invalid use of CancelableRequestConsumer in TabRestoreService.
    BUG=85970
    TEST=Crash browser, restart with tab restore on, see no assertion.
    
    Review URL: http://codereview.chromium.org/7170023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/tab_restore_service.h
+++ chrome/browser/sessions/tab_restore_service.h
@@ -328,6 +328,9 @@ class TabRestoreService : public BaseSessionService {
   // avoid creating historical tabs for them.
   std::set<TabRestoreServiceDelegate*> closing_delegates_;
 
+  // Used when loading open tabs/session when recovering from a crash.
+  CancelableRequestConsumer crash_consumer_;
+
   // Used when loading previous tabs/session.
   CancelableRequestConsumer load_consumer_;
 
