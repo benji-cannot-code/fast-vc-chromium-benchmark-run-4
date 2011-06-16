d2f3704977fda903495582f57f9ef85fe41fb972
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 06:41:11 +0000

    Fix to make 'Enable accessibility features.' option change take effect immediately
    Add loading and unloading code to SystemOptionsHandler
    Add unregister_component_extension to ExtensionService
    
    BUG=chromium-os:11887
    TEST=manually confirmed that enabling/disabling a11y features works immediately
    
    Review URL: http://codereview.chromium.org/7129015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89302 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -135,6 +135,8 @@ class ExtensionService
           root_directory(root_directory) {
     }
 
+    bool Equals(const ComponentExtensionInfo& other) const;
+
     // The extension's manifest. This is required for component extensions so
     // that ExtensionService doesn't need to go to disk to load them.
     std::string manifest;
@@ -204,6 +206,9 @@ class ExtensionService
   void register_component_extension(const ComponentExtensionInfo& info) {
     component_extension_manifests_.push_back(info);
   }
+
+  // Unregisters a component extension from the list of extensions to be loaded
+  void UnregisterComponentExtension(const ComponentExtensionInfo& info);
 
   const FilePath& install_directory() const { return install_directory_; }
 
@@ -317,6 +322,9 @@ class ExtensionService
 
   // Loads particular component extension.
   const Extension* LoadComponentExtension(const ComponentExtensionInfo& info);
+
+  // Unloads particular component extension.
+  void UnloadComponentExtension(const ComponentExtensionInfo& info);
 
   // Loads all known extensions (used by startup and testing code).
   void LoadAllExtensions();
--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -413,5 +413,6 @@ const char* kAppLaunchHistogram = "Extensions.AppLaunch";
 #if defined(OS_CHROMEOS)
 const char* kAccessExtensionPath =
     "/usr/share/chromeos-assets/accessibility/extensions";
+const char* kChromeVoxDirectoryName = "access_chromevox";
 #endif
 }
