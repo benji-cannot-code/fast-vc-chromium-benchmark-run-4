d35fe6df7f6a25eee7049d5cbaf11c04b8cacde9
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-16 17:49:04 +0000

    Add View::ReorderChildView and Widget::MoveToTop.
    The z-ordering of child views is currently maintained (implicitly?) by the order
    they are added to the parent view. It may often be necessary to change the
    z-order of views, and View::ReorderChildView can be used to do that.
    
    I changed views_desktop to have the two pure-views windows to have overlapping
    regions, and View::ReorderChildView is used to change the z-ordering of the two
    windows. So it is possible to click a window to activate it and bring it on top
    of the other. It could have been done without adding Widget::MoveToTop, but
    given Widget::MoveAbove and MoveAboveWidget, it sounded like a good idea to have
    Widget::MoveToTop (MoveToFront?). I do now know what the implementation for
    windows should be, though. So left it as NOTIMPLEMENTED.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7185005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89363 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_views.cc
+++ views/widget/native_widget_views.cc
@@ -43,6 +43,7 @@ void NativeWidgetViews::OnActivate(bool active) {
 
 void NativeWidgetViews::InitNativeWidget(const Widget::InitParams& params) {
   view_ = new internal::NativeWidgetView(this);
+  view_->SetPaintToTexture(true);
   host_view_->AddChildView(view_);
 
   // TODO(beng): handle parenting.
@@ -201,6 +202,10 @@ void NativeWidgetViews::SetBoundsConstrained(const gfx::Rect& bounds,
 
 void NativeWidgetViews::MoveAbove(gfx::NativeView native_view) {
   NOTIMPLEMENTED();
+}
+
+void NativeWidgetViews::MoveToTop() {
+  host_view_->ReorderChildView(view_, -1);
 }
 
 void NativeWidgetViews::SetShape(gfx::NativeRegion region) {
--- views/widget/native_widget_win.cc
+++ views/widget/native_widget_win.cc
@@ -791,6 +791,10 @@ void NativeWidgetWin::MoveAbove(gfx::NativeView native_view) {
                SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
 }
 
+void NativeWidgetWin::MoveToTop() {
+  NOTIMPLEMENTED();
+}
+
 void NativeWidgetWin::SetShape(gfx::NativeRegion region) {
   SetWindowRgn(region, TRUE);
 }
