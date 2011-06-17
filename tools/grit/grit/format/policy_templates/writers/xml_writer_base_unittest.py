8798f870a0a264314d6b692d5768fc3a2c9b0585
gfeher@chromium.org <gfeher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 12:31:38 +0000

    Fix string-enums in ADMX templates
    BUG=86141
    TEST=verify that the value of ProxyMode set via the ADMX template is correctly reflected in about:net-internals
    
    Review URL: http://codereview.chromium.org/7155025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89473 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/grit/grit/format/policy_templates/writers/xml_writer_base_unittest.py
+++ tools/grit/grit/format/policy_templates/writers/xml_writer_base_unittest.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python2.4
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -28,7 +28,7 @@ class XmlWriterBaseTest(unittest.TestCase):
     Return: XML of the chrildren of the parent node.
     '''
     return ''.join(
-      child.toprettyxml(indent = '  ') for child in parent.childNodes)
+      child.toprettyxml(indent='  ') for child in parent.childNodes)
 
   def AssertXMLEquals(self, output, expected_output):
     '''Asserts if the passed XML arguements are equal.
