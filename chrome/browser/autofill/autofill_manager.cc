eada5b1452545d6e8dfbf80746760a7f60fd3dcd
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 18:16:02 +0000

    Make sure autofill_manager brings up the browser with the correct profile.
    BUG=86361
    TEST=autofill tests remain working.
    
    Review URL: http://codereview.chromium.org/7189036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -606,7 +606,8 @@ void AutofillManager::OnFillAutofillFormData(int query_id,
 }
 
 void AutofillManager::OnShowAutofillDialog() {
-  Browser* browser = BrowserList::GetLastActive();
+  Browser* browser = BrowserList::GetLastActiveWithProfile(
+      tab_contents()->profile());
   if (browser)
     browser->ShowOptionsTab(chrome::kAutofillSubPage);
 }
