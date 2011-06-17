25d2fc05a64654f1fce1b74a1dc0addfca7c0154
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 18:51:57 +0000

    Create explicit out-of-line ctor and dtor for infobars::Tip struct.
    BUG=76381
    TEST=Clang compiles
    
    Review URL: http://codereview.chromium.org/7206005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89530 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/infobars/infobar_tip_drawing_model.mm
+++ chrome/browser/ui/cocoa/infobars/infobar_tip_drawing_model.mm
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)firstInfobarUsesExpandedTip;
 @end
 
+infobars::Tip::Tip() {}
+infobars::Tip::~Tip() {}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 @implementation InfobarTipDrawingModel
