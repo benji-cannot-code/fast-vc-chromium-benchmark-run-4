7f12e992184e49cdd7e9d1e5eba853fcd5ae1291
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 18:06:08 +0000

    Remove dead code from sync_setup_flow.
    BUG=86361
    TEST=all sync tests still work.
    
    Review URL: http://codereview.chromium.org/7201004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89514 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_setup_flow.h
+++ chrome/browser/sync/sync_setup_flow.h
@@ -87,8 +87,6 @@ class SyncSetupFlow {
   // The user canceled the passphrase entry without supplying a passphrase.
   void OnPassphraseCancel();
 
-  void OnGoToDashboard();
-
   void OnDialogClosed(const std::string& json_retval);
 
  private:
