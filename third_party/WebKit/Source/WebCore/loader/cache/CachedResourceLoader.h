049e3e1e4061ea1f760536f76b5f0433d7e8ae26
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-17 19:11:14 +0000

    2011-06-17  Chris Evans  <cevans@chromium.org>
            Reviewed by Adam Barth.
    
            Detect mixed-scripting involving https -> http redirects
            https://bugs.webkit.org/show_bug.cgi?id=62846
    
            * http/tests/security/mixedContent/redirect-https-to-http-script-in-iframe-expected.txt: Update expectation.
            * http/tests/security/mixedContent/redirect-https-to-http-script-in-iframe.html: Remove FIXME as the test is now working as expected.
    2011-06-17  Chris Evans  <cevans@chromium.org>
    
            Reviewed by Adam Barth.
    
            Detect mixed-scripting involving https -> http redirects
            https://bugs.webkit.org/show_bug.cgi?id=62846
    
            Test: http/tests/security/mixedContent/redirect-https-to-http-script-in-iframe.html
    
            * loader/cache/CachedResourceLoader.cpp:
            (WebCore::CachedResourceLoader::checkInsecureContent):
            (WebCore::CachedResourceLoader::canRequest): break out insecure content logic.
            * loader/cache/CachedResourceLoader.h:
            * loader/cache/CachedResourceRequest.cpp:
            (WebCore::CachedResourceRequest::willSendRequest): check the redirect target for possible insecure content issues.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.h
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.h
@@ -103,6 +103,7 @@ public:
     void preload(CachedResource::Type, ResourceRequest&, const String& charset, bool referencedFromBody);
     void checkForPendingPreloads();
     void printPreloadStats();
+    bool checkInsecureContent(CachedResource::Type, const KURL&) const;
     
 private:
     CachedResource* requestResource(CachedResource::Type, ResourceRequest&, const String& charset, ResourceLoadPriority = ResourceLoadPriorityUnresolved, bool isPreload = false);
