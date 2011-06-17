5f86e9780f29cd558d3c61ab9598ff2b073b4823
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 01:58:54 +0000

    Switch virtual keyboard layout when IME is switched.
    * chrome/browser/extensions/extension_service.cc:
    Register virtual keyboard extension's URL and layout names to the IME library.
    
    * chrome/browser/ui/touch/frame/:
    Register a callback function which is called when IME is switched. The callback function sends a notification to the current virtual keyboard extension.
    
    * chrome/browser/resources/keyboard/main.js:
    Handles window.onhashchange event.
    
    This change depends on http://codereview.chromium.org/7096001/ .
    All-in-one CL for testing is also available at http://codereview.chromium.org/7074008/ .
    
    BUG=chromium-os:14241
    TEST=ran browser_test
    
    Review URL: http://codereview.chromium.org/7042033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -94,6 +94,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS) && defined(TOUCH_UI)
+#include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/cros/input_method_library.h"
 #include "chrome/browser/extensions/extension_input_ui_api.h"
 #endif
 
@@ -1345,6 +1347,24 @@ void ExtensionService::NotifyExtensionLoaded(const Extension* extension) {
 
   if (plugins_changed || nacl_modules_changed)
     PluginService::GetInstance()->PurgePluginListCache(false);
+
+#if defined(OS_CHROMEOS) && defined(TOUCH_UI)
+  chromeos::InputMethodLibrary* input_method_library =
+      chromeos::CrosLibrary::Get()->GetInputMethodLibrary();
+  for (std::vector<Extension::InputComponentInfo>::const_iterator component =
+           extension->input_components().begin();
+       component != extension->input_components().end();
+       ++component) {
+    if (component->type == Extension::INPUT_COMPONENT_TYPE_VIRTUAL_KEYBOARD &&
+        !component->layouts.empty()) {
+      const bool is_system =
+          !Extension::IsExternalLocation(extension->location());
+      input_method_library->RegisterVirtualKeyboard(extension->url(),
+                                                    component->layouts,
+                                                    is_system);
+    }
+  }
+#endif
 }
 
 void ExtensionService::NotifyExtensionUnloaded(
