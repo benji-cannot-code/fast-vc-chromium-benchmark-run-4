9c37645d3056bec187e65aa118def59fa17fe912
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-17 20:21:06 +0000

    Fix Qt build.
    * WebProcess/ResourceCache/WebResourceCacheManager.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/ResourceCache/WebResourceCacheManager.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/ResourceCache/WebResourceCacheManager.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/MemoryCache.h>
 #include <WebCore/SecurityOrigin.h>
 #include <WebCore/SecurityOriginHash.h>
+#include <wtf/UnusedParam.h>
 
 using namespace WebCore;
 
