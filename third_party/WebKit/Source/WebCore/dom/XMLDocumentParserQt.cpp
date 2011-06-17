be3e704dc868b0b553a61c5eaf9e9dd9ac210077
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-17 06:56:00 +0000

    2011-06-16 Gabor Loki <loki@webkit.org>
            [Qt] Unreviewed, build fix after r89118.
    
            * dom/XMLDocumentParserQt.cpp:
            (WebCore::XMLDocumentParser::initializeParserContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/XMLDocumentParserQt.cpp
+++ third_party/WebKit/Source/WebCore/dom/XMLDocumentParserQt.cpp
@@ -203,7 +203,7 @@ void XMLDocumentParser::doWrite(const String& parseString)
     return;
 }
 
-void XMLDocumentParser::initializeParserContext(const char*)
+void XMLDocumentParser::initializeParserContext(const CString&)
 {
     DocumentParser::startParsing();
     m_sawError = false;
