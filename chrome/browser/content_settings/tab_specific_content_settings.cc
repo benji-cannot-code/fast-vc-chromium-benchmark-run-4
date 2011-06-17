b0894635f0c4c5c2bd3caa1a8e5168647cfc66ce
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 10:39:09 +0000

    Correctly apply content settings to data URLs.
    Data URLs don't round-trip through the browser's network stack, so they don't
    trigger the content settings code to upload the proper settings to the
    renderer.  This patch proactively pushes the default content settings into the
    renderer so that it can use them for data URLs (and other sandboxed frames).
    
    Review URL: http://codereview.chromium.org/7167003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/tab_specific_content_settings.cc
+++ chrome/browser/content_settings/tab_specific_content_settings.cc
@@ -431,6 +431,14 @@ void TabSpecificContentSettings::DidNavigateMainFramePostCommit(
   }
 }
 
+void TabSpecificContentSettings::RenderViewCreated(
+    RenderViewHost* render_view_host) {
+  HostContentSettingsMap* map =
+      tab_contents()->profile()->GetHostContentSettingsMap();
+  render_view_host->Send(new ViewMsg_SetDefaultContentSettings(
+      map->GetDefaultContentSettings()));
+}
+
 void TabSpecificContentSettings::DidStartProvisionalLoadForFrame(
     int64 frame_id,
     bool is_main_frame,
@@ -469,9 +477,12 @@ void TabSpecificContentSettings::Observe(NotificationType type,
       // The active NavigationEntry is the URL in the URL field of a tab.
       // Currently this should be matched by the |primary_pattern|.
       settings_details.ptr()->primary_pattern().Matches(entry_url)) {
+    HostContentSettingsMap* map =
+        tab_contents()->profile()->GetHostContentSettingsMap();
+    Send(new ViewMsg_SetDefaultContentSettings(
+        map->GetDefaultContentSettings()));
     Send(new ViewMsg_SetContentSettingsForCurrentURL(
-        entry_url, tab_contents()->profile()->GetHostContentSettingsMap()->
-            GetContentSettings(entry_url, entry_url)));
+        entry_url, map->GetContentSettings(entry_url, entry_url)));
   }
 }
 
