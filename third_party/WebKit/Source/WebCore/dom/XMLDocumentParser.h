c57145fcefc867ed3d065db286dd8da6a583dc4f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-17 05:54:14 +0000

    2011-06-16  Jeffrey Pfau  <jpfau@apple.com>
            Reviewed by Alexey Proskuryakov.
    
            Using null bytes when setting innerHTML in XTHML results in assertion and a crash due to null-pointer dereference
            https://bugs.webkit.org/show_bug.cgi?id=61053
    
            Added test cases covering two cases of using innerHTML with null bytes in XHTML.
    
            * fast/parser/xhtml-innerhtml-null-byte-expected.txt: Added.
            * fast/parser/xhtml-innerhtml-null-byte-first-expected.txt: Added.
            * fast/parser/xhtml-innerhtml-null-byte-first.xhtml: Added.
            * fast/parser/xhtml-innerhtml-null-byte.xhtml: Added.
    2011-06-16  Jeffrey Pfau  <jpfau@apple.com>
    
            Reviewed by Alexey Proskuryakov.
    
            Using null bytes when setting innerHTML in XTHML results in assertion and a crash due to null-pointer dereference
            https://bugs.webkit.org/show_bug.cgi?id=61053
    
            XML parsing in-memory XML chunks now passes around a string object instead of a C string, ensuring null characters are properly handled.
    
            Tests: fast/parser/xhtml-innerhtml-null-byte-first.xhtml
                   fast/parser/xhtml-innerhtml-null-byte.xhtml
    
            * dom/XMLDocumentParser.h:
            * dom/XMLDocumentParserLibxml2.cpp:
            (WebCore::XMLParserContext::createMemoryParser):
            (WebCore::XMLDocumentParser::initializeParserContext):
            (WebCore::XMLDocumentParser::appendFragmentSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/XMLDocumentParser.h
+++ third_party/WebKit/Source/WebCore/dom/XMLDocumentParser.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SegmentedString.h"
 #include <wtf/HashMap.h>
 #include <wtf/OwnPtr.h>
+#include <wtf/text/CString.h>
 #include <wtf/text/StringHash.h>
 
 #if USE(QXMLSTREAM)
@@ -57,8 +58,8 @@ class Text;
 #if !USE(QXMLSTREAM)
     class XMLParserContext : public RefCounted<XMLParserContext> {
     public:
-        static PassRefPtr<XMLParserContext> createMemoryParser(xmlSAXHandlerPtr, void*, const char*);
-        static PassRefPtr<XMLParserContext> createStringParser(xmlSAXHandlerPtr, void*);
+        static PassRefPtr<XMLParserContext> createMemoryParser(xmlSAXHandlerPtr, void* userData, const CString& chunk);
+        static PassRefPtr<XMLParserContext> createStringParser(xmlSAXHandlerPtr, void* userData);
         ~XMLParserContext();
         xmlParserCtxtPtr context() const { return m_context; }
 
@@ -161,7 +162,7 @@ public:
         void endDocument();
 #endif
     private:
-        void initializeParserContext(const char* chunk = 0);
+        void initializeParserContext(const CString& chunk = CString());
 
         void pushCurrentNode(ContainerNode*);
         void popCurrentNode();
