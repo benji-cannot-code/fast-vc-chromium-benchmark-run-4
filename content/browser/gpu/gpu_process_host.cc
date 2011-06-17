19674b28350bf493d17dc5a5cee4f3948ee83b34
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 17:35:00 +0000

    Clamping down media DEPS rules.
    BUG=none
    TEST=checkdeps passes
    
    Review URL: http://codereview.chromium.org/7170008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89505 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/gpu/gpu_process.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_switches.h"
-#include "media/base/media_switches.h"
 #include "ui/gfx/gl/gl_context.h"
 #include "ui/gfx/gl/gl_switches.h"
 
