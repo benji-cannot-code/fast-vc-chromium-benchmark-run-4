7318bb07d88717af17cec1cf9291b40b8ebc2bd7
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-18 11:53:14 +0000

    Adding missing inclusions
    BUG=None
    TEST='LoggingTest.*,FileUtilTest.*,PlatformFile.*,StringNumberConversionsTest.*,Time,TimeTicks,TimeDelta'
    
    
    Review URL: http://codereview.chromium.org/7108033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89595 0039d316-1c4b-4281-b951-d872f2087c98

--- base/platform_file_posix.cc
+++ base/platform_file_posix.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include "base/eintr_wrapper.h"
 #include "base/file_path.h"
