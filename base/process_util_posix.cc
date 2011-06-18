a244dc0d332017906f8e7ec0bdefdcc07ea6c306
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-18 11:49:27 +0000

    Increase the KillProcess timeout when running under Valgrind BUG=17453 Review URL: http://codereview.chromium.org/7204002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89593 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_posix.cc
+++ base/process_util_posix.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 #include "base/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time.h"
@@ -228,6 +229,13 @@ bool KillProcess(ProcessHandle process_id, int exit_code, bool wait) {
 
   if (result && wait) {
     int tries = 60;
+
+    if (RunningOnValgrind()) {
+      // Wait for some extra time when running under Valgrind since the child
+      // processes may take some time doing leak checking.
+      tries *= 2;
+    }
+
     // The process may not end immediately due to pending I/O
     bool exited = false;
     while (tries-- > 0) {
