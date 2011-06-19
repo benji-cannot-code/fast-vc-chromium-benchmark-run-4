de6ede073e9712aae27002d9434d59119d7d6da4
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-19 17:56:04 +0000

    Revert 89626 - Support automatic javascript test registry in gtest when creating WebUI tests.
    The goal was to support something as simple as the following, where the tests in the
    javascript files would be 1st class GTESTs, supporting FLAKY_, DISABLED_, etc, and
    registered at linker_initialization time.
    
    WEB_UI_BROWSER_TEST_JS(WebUIBrowserTest, TestJSPass,
                           FILE_PATH_LITERAL("sample_passing.js")) {
      ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIDownloadsURL));
    }
    
    This solution ended up being fairly fragile, and I ended up with a script to parse javascript (with the help of v8_shell) and generate an includable ...-inl.h file with the following for every test in the .js file specified in the |rules| section of tools/js2webui.py
    
    IN_PROC_BROWSER_TEST_F(WebUIBrowserTestPass, testHelper) {
      AddLibrary(FilePath(FILE_PATH_LITERAL("sample_pass.js")));
      ASSERT_TRUE(RunJavascriptTest("testHelper"));
    }
    
    http://www.chromium.org/Home/domui-testing
    
    BUG=82437
    R=estade@chromium.org,jhawkins@chromium.org
    TEST=browser_tests --gtest_filter=WebUIBrowserTest*
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=89453
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=89605
    
    Review URL: http://codereview.chromium.org/7087014
    
    TBR=scr@chromium.org
    Review URL: http://codereview.chromium.org/7204022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/web_ui_browsertest.h
+++ chrome/browser/ui/webui/web_ui_browsertest.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_WEB_UI_BROWSERTEST_H_
-#define CHROME_BROWSER_UI_WEBUI_WEB_UI_BROWSERTEST_H_
+#ifndef CONTENT_BROWSER_WEBUI_WEB_UI_BROWSERTEST_H_
+#define CONTENT_BROWSER_WEBUI_WEB_UI_BROWSERTEST_H_
 #pragma once
 
 #include <string>
@@ -99,4 +99,4 @@ class WebUIBrowserTest : public InProcessBrowserTest {
   std::vector<FilePath> user_libraries;
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_WEB_UI_BROWSERTEST_H_
+#endif  // CONTENT_BROWSER_WEBUI_WEB_UI_BROWSERTEST_H_
