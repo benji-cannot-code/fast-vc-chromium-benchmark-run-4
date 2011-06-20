9c4fea944406550eaefa7833235922eb76102314
craigdh@google.com <craigdh@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 18:44:11 +0000

    Added automation hooks for private networks.
    BUG=chromium-os:16586
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7144011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_observers.h
+++ chrome/browser/automation/automation_provider_observers.h
@@ -814,8 +814,8 @@ class NetworkScanObserver
   virtual void OnNetworkManagerChanged(chromeos::NetworkLibrary* obj);
 
  private:
-  AutomationProvider* automation_;
-  IPC::Message* reply_message_;
+  base::WeakPtr<AutomationProvider> automation_;
+  scoped_ptr<IPC::Message> reply_message_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkScanObserver);
 };
@@ -837,8 +837,8 @@ class NetworkConnectObserver
   virtual void OnNetworkManagerChanged(chromeos::NetworkLibrary* obj);
 
  private:
-  AutomationProvider* automation_;
-  IPC::Message* reply_message_;
+  base::WeakPtr<AutomationProvider> automation_;
+  scoped_ptr<IPC::Message> reply_message_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkConnectObserver);
 };
@@ -858,6 +858,31 @@ class ServicePathConnectObserver : public NetworkConnectObserver {
   std::string service_path_;
 
   DISALLOW_COPY_AND_ASSIGN(ServicePathConnectObserver);
+};
+
+// Waits for a connection success or failure for the specified
+// virtual network and returns the status to the automation provider.
+class VirtualConnectObserver
+    : public chromeos::NetworkLibrary::NetworkManagerObserver {
+ public:
+  VirtualConnectObserver(AutomationProvider* automation,
+                         IPC::Message* reply_message,
+                         const std::string& service_name);
+
+  virtual ~VirtualConnectObserver();
+
+  // NetworkLibrary::NetworkManagerObserver implementation.
+  virtual void OnNetworkManagerChanged(chromeos::NetworkLibrary* cros);
+
+ private:
+  virtual chromeos::VirtualNetwork* GetVirtualNetwork(
+      const chromeos::NetworkLibrary* cros);
+
+  base::WeakPtr<AutomationProvider> automation_;
+  scoped_ptr<IPC::Message> reply_message_;
+  std::string service_name_;
+
+  DISALLOW_COPY_AND_ASSIGN(VirtualConnectObserver);
 };
 
 // Waits for a connection success or failure for the specified
