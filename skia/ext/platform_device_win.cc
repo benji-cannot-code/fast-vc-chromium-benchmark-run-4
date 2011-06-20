d185c60dcc02db91394f0024f70abfd0fb8d0ce7
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 14:51:42 +0000

    skia roll Review URL: http://codereview.chromium.org/7200003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89666 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/platform_device_win.cc
+++ skia/ext/platform_device_win.cc
@@ -53,7 +53,7 @@ void InitializeDC(HDC context) {
 }
 
 PlatformDevice::PlatformDevice(const SkBitmap& bitmap)
-    : SkDevice(NULL, bitmap, /*isForLayer=*/false) {
+    : SkDevice(bitmap) {
   SetPlatformDevice(this, this);
 }
 
