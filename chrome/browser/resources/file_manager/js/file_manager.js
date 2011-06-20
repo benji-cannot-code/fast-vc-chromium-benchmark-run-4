7a0a5f6ccd45b3bb898191a42d5edbec7d6bdc78
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 18:15:01 +0000

    file manager: set explicit tabindex ordering
    BUG=chromium-os:16379
    TEST=manual testing on chromeos-chrome on linux
    
    Review URL: http://codereview.chromium.org/7189015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -88,7 +88,7 @@ function FileManager(dialogDom, rootEntries, params) {
   chrome.fileBrowserPrivate.onDiskChanged.addListener(
       this.onDiskChanged_.bind(this));
 
-  this.table_.list_.focus();
+  this.document_.querySelector('[tabindex="0"]').focus();
 
   if (ENABLE_METADATA) {
     // Pass all URLs to the metadata reader until we have a correct filter.
@@ -959,6 +959,7 @@ FileManager.prototype = {
   FileManager.prototype.renderCheckbox_ = function(entry) {
     var input = this.document_.createElement('input');
     input.setAttribute('type', 'checkbox');
+    input.setAttribute('tabindex', -1);
     input.className = 'file-checkbox';
     input.addEventListener('mousedown',
                            this.onCheckboxMouseDownUp_.bind(this));
