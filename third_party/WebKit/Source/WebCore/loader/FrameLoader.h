cb118c950bb0b258640fa7fc638fa283000263f6
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-20 23:32:54 +0000

    2011-06-20  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Remove FrameLoader::m_workingURL
            https://bugs.webkit.org/show_bug.cgi?id=63021
    
            FrameLoader shouldn't keep the working URL in separate state.  It's
            just a function of other state.  m_workingURL exists only to create
            bugs and confusion.
    
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::documentURL):
            * loader/DocumentLoader.h:
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::stopLoading):
            (WebCore::FrameLoader::didOpenURL):
            (WebCore::FrameLoader::receivedFirstData):
            (WebCore::FrameLoader::willSetEncoding):
            (WebCore::FrameLoader::commitProvisionalLoad):
            (WebCore::FrameLoader::open):
            (WebCore::FrameLoader::finishedLoadingDocument):
            * loader/FrameLoader.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.h
@@ -388,7 +388,7 @@ private:
 
     void provisionalLoadStarted();
 
-    bool didOpenURL(const KURL&);
+    bool didOpenURL();
 
     void scheduleCheckCompleted();
     void scheduleCheckLoadComplete();
@@ -437,7 +437,7 @@ private:
 
     RefPtr<SerializedScriptValue> m_pendingStateObject;
 
-    KURL m_workingURL;
+    bool m_hasReceivedFirstData;
 
     bool m_needsClear;
 
