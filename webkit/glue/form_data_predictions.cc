b8789150271a2d6af4f0cea6f12f45050c9e9d62
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 20:03:25 +0000

    Add a debugging flag to annotate web forms with their Autofill field type predictions.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7187029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89716 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/form_data_predictions.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "webkit/glue/form_data_predictions.h"
+
+namespace webkit_glue {
+
+FormDataPredictions::FormDataPredictions() {
+}
+
+FormDataPredictions::FormDataPredictions(const FormDataPredictions& other)
+    : data(other.data),
+      signature(other.signature),
+      experiment_id(other.experiment_id),
+      fields(other.fields) {
+}
+
+FormDataPredictions::~FormDataPredictions() {
+}
+
+}  // namespace webkit_glue
