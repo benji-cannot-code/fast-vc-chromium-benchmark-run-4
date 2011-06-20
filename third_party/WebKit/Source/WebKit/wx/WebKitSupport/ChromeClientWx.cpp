05d100e3468af4c6ee663426846c8a048cd789bf
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-20 19:36:24 +0000

    [wx] Unreviewed build fix after method rename.
    git-svn-id: svn://svn.chromium.org/blink/trunk@89274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
+++ third_party/WebKit/Source/WebKit/wx/WebKitSupport/ChromeClientWx.cpp
@@ -457,7 +457,7 @@ void ChromeClientWx::runOpenPanel(Frame*, PassRefPtr<FileChooser>)
     notImplemented();
 }
 
-void ChromeClientWx::chooseIconForFiles(const Vector<String>& filenames, FileIconLoader* loader)
+void ChromeClientWx::loadIconForFiles(const Vector<String>& filenames, FileIconLoader* loader)
 {
     loader->notifyFinished(Icon::createIconForFiles(filenames));
 }
