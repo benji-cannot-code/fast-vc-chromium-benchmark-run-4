7dfa6f4a10c63ebb8516da9243c76ed9f81518ab
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 15:51:22 +0000

    use path in #includes
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7210002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89671 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autocomplete/touch_autocomplete_popup_contents_view.cc
+++ chrome/browser/ui/views/autocomplete/touch_autocomplete_popup_contents_view.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "touch_autocomplete_popup_contents_view.h"
+#include "chrome/browser/ui/views/autocomplete/touch_autocomplete_popup_contents_view.h"
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/omnibox/omnibox_view.h"
--- chrome/test/model_test_utils.cc
+++ chrome/test/model_test_utils.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "model_test_utils.h"
+#include "chrome/test/model_test_utils.h"
 
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
