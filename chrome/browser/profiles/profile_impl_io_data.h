4eddc9931834e5a7835baf9271f0d6b048c6b6e2
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 19:18:05 +0000

    Remove unnecessary forward declarations.
    R=willchan@chromium.org
    BUG=none
    TEST=no compilation errors.
    
    Review URL: http://codereview.chromium.org/7204011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl_io_data.h
+++ chrome/browser/profiles/profile_impl_io_data.h
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_io_data.h"
 
 namespace net {
-class CookiePolicy;
-class NetworkDelegate;
-class DnsCertProvenanceChecker;
 class HttpTransactionFactory;
 }  // namespace net
 
