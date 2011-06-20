eebd8f09f04d6ac45e8c073d992c211982163882
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-20 19:32:45 +0000

    Completed basic implementation.
    BUG=None
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7200009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89714 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/client_session.h
+++ remoting/host/client_session.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_HOST_CLIENT_SESSION_H_
 
 #include <list>
+#include <set>
 
 #include "remoting/protocol/connection_to_client.h"
 #include "remoting/protocol/host_stub.h"
@@ -69,12 +70,17 @@ class ClientSession : public protocol::HostStub,
     return authenticated_;
   }
 
+  void set_awaiting_continue_approval(bool awaiting) {
+    awaiting_continue_approval_ = awaiting;
+  }
+
   // Indicate that local mouse activity has been detected. This causes remote
   // inputs to be ignored for a short time so that the local user will always
   // have the upper hand in 'pointer wars'.
   void LocalMouseMoved(const gfx::Point& new_pos);
 
-  bool ShouldIgnoreRemoteInput() const;
+  bool ShouldIgnoreRemoteMouseInput(const protocol::MouseEvent* event) const;
+  bool ShouldIgnoreRemoteKeyboardInput(const protocol::KeyEvent* event) const;
 
  private:
   friend class base::RefCountedThreadSafe<ClientSession>;
@@ -94,10 +100,15 @@ class ClientSession : public protocol::HostStub,
   // Whether this client is authenticated.
   bool authenticated_;
 
+  // Whether or not inputs from this client are blocked pending approval from
+  // the host user to continue the connection.
+  bool awaiting_continue_approval_;
+
   // State to control remote input blocking while the local pointer is in use.
   uint32 remote_mouse_button_state_;
   std::list<gfx::Point> recent_remote_mouse_positions_;
   base::Time latest_local_input_time_;
+  std::set<int> pressed_keys_;
 
   DISALLOW_COPY_AND_ASSIGN(ClientSession);
 };
