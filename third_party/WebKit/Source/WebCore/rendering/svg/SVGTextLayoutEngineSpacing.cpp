58c6d89a06addfb9bafe616a8d34584e63985ee7
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-20 17:18:47 +0000

    2011-06-20  Andras Becsi  <abecsi@webkit.org>
            Unreviewed build fix.
    
            [Qt][V8] Include missing UnusedParam.h.
    
            No new tests needed.
    
            * rendering/svg/SVGTextLayoutEngineSpacing.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/SVGTextLayoutEngineSpacing.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/SVGTextLayoutEngineSpacing.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGFontData.h"
 #include "SVGFontElement.h"
 #include "SVGFontFaceElement.h"
+#else
+#include <wtf/UnusedParam.h>
 #endif
 
 namespace WebCore {
