8f71907560d266db7c27e0f4d9e05f42f98df460
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-20 21:33:52 +0000

    Expand Web Worker test coverage for onerror to have test for url/line numbers. https://bugs.webkit.org/show_bug.cgi?id=62898
    Reviewed by Dmitry Titov.
    
    * fast/workers/resources/worker-script-error-bubbled.js:
    (onerror): Added a check for the url and put the last part in the message.
    * fast/workers/worker-script-error-expected.txt: Update to have the new result.
    * fast/workers/worker-script-error.html: Actually print the message from the worker.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/workers/resources/worker-script-error-bubbled.js
+++ third_party/WebKit/LayoutTests/fast/workers/resources/worker-script-error-bubbled.js
@@ -1,7 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 onerror = function(message, url, lineno)
 {
-    postMessage("onerror invoked for a script that has script error '" + message + "' at line " + lineno);
+    if (url != location.href)
+        postMessage("FAIL: Bad location. Actual: " + url + " Expected: " + location.href);
+    splitUrl = url.split('/');
+    postMessage("PASS: onerror in worker context invoked for a script that has script error '" + message + "' at line " + lineno + " in " + splitUrl[splitUrl.length - 1]);
     return true;
 }
 
