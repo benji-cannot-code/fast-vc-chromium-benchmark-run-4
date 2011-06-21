ca58572add73ee590b5da65d9ae3d3486f47ad18
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 14:38:15 +0000

    Augment the gfx::Image conversion methods to let callers choose between weak and strong refs.
    This deprecates the operator-based conversion methods in favor of To/CopyType().
    New callsites should use these methods. Other uses will be converted in a later
    CL.
    
    BUG=58030
    TEST=ui_unittests
    
    
    Review URL: http://codereview.chromium.org/7205031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89826 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/image/image_unittest_util.h
+++ ui/gfx/image/image_unittest_util.h
@@ -28,6 +28,8 @@ PlatformImage CreatePlatformImage();
 
 gfx::Image::RepresentationType GetPlatformRepresentationType();
 
+PlatformImage ToPlatformType(const gfx::Image& image);
+
 }  // namespace test
 }  // namespace gfx
 
