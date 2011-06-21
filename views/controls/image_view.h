2bdfe29c0f6d339c788ceaf92d84a4c2d61cfe0c
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 18:04:40 +0000

    [Views] Make ImageView::SetImage() take a const SkBitmap* rather than a non-const one.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7214001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89857 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/image_view.h
+++ views/controls/image_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -43,7 +43,7 @@ class ImageView : public View {
   // Set the bitmap that should be displayed from a pointer. Reset the image
   // if the pointer is NULL. The pointer contents is copied in the receiver's
   // bitmap.
-  void SetImage(SkBitmap* bm);
+  void SetImage(const SkBitmap* bm);
 
   // Returns the bitmap currently displayed or NULL of none is currently set.
   // The returned bitmap is still owned by the ImageView.
