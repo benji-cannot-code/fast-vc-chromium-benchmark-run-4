4eaf78e8a2bcc193bae3fc08a60c94e3f4627c47
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-21 16:59:56 +0000

    2011-06-21  Lukasz Slachciak  <l.slachciak@samsung.com>
            Reviewed by Sam Weinig.
    
            [GTK] [WK2] WebKit2 build break fixes.
            https://bugs.webkit.org/show_bug.cgi?id=62950
    
            When building WebKit2 for GTK there are few build breaks connected with missing include files.
    
            * Platform/unix/SharedMemoryUnix.cpp:
            * Shared/gtk/WebCoreArgumentCodersGtk.cpp:
            * UIProcess/API/gtk/PageClientImpl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/unix/SharedMemoryUnix.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/unix/SharedMemoryUnix.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(QT)
 #include <QDir>
 #elif PLATFORM(GTK)
+#include <glib.h>
 #include <wtf/gobject/GOwnPtr.h>
 #endif
 
--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/PageClientImpl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/PageClientImpl.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitWebViewBasePrivate.h"
 #include "WebPageProxy.h"
 #include "WebPopupMenuProxyGtk.h"
+#include <WebCore/Cursor.h>
 #include <WebCore/GtkUtilities.h>
 #include <wtf/text/WTFString.h>
 
