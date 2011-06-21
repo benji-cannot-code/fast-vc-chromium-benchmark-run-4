7ae89ea601bd084b989058de6e6078e241b13daf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 03:57:05 +0000

    Plugins: Force enable the internal PDF plugin if there is no saved plugin list.
    BUG=86256
    TEST=Run chrome with a new profile; immediately close chrome; launch chrome and visit about:plugins and make sure there's only 1 PDF plugin enabled.
    
    Review URL: http://codereview.chromium.org/7189077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89790 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_updater.cc
+++ chrome/browser/plugin_updater.cc
@@ -158,7 +158,7 @@ void PluginUpdater::SetProfile(Profile* profile) {
   {  // Scoped update of prefs::kPluginsPluginsList.
     ListPrefUpdate update(profile->GetPrefs(), prefs::kPluginsPluginsList);
     ListValue* saved_plugins_list = update.Get();
-    if (saved_plugins_list) {
+    if (saved_plugins_list && !saved_plugins_list->empty()) {
       for (ListValue::const_iterator it = saved_plugins_list->begin();
            it != saved_plugins_list->end();
            ++it) {
@@ -209,6 +209,11 @@ void PluginUpdater::SetProfile(Profile* profile) {
           EnablePluginGroup(false, group_name);
         }
       }
+    } else {
+      // If the saved plugin list is empty, then the call to UpdatePreferences()
+      // below failed in an earlier run, possibly because the user closed the
+      // browser too quickly. Try to force enable the internal PDF plugin again.
+      force_enable_internal_pdf = true;
     }
   }  // Scoped update of prefs::kPluginsPluginsList.
 
