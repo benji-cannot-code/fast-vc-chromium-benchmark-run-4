5c49bdae8dd3f1e6d4fc9a33ea4ced86d94f54ad
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 21:18:53 +0000

    Don't tell the webapp that someone is connected in OnSignallingConnected.
    BUG=None
    TEST=Start sharing with the web-app. Ensure that the "Your desktop is currently being shared" message is not shown briefly before the access code appears.
    
    Review URL: http://codereview.chromium.org/7222017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89895 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/host_script_object.cc
+++ remoting/host/host_script_object.cc
@@ -270,7 +270,6 @@ bool HostNPScriptObject::Enumerate(std::vector<std::string>* values) {
 
 void HostNPScriptObject::OnSignallingConnected(SignalStrategy* signal_strategy,
                                                const std::string& full_jid) {
-  OnStateChanged(kConnected);
 }
 
 void HostNPScriptObject::OnSignallingDisconnected() {
