04226214d535b655425bd24ba76b950718625c02
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 21:34:39 +0000

    Fix compile: add #include that was (incorrectly) transitively #included before.
    BUG=62154
    TEST=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89899 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/infobars/confirm_infobar.cc
+++ chrome/browser/ui/views/infobars/confirm_infobar.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/infobars/confirm_infobar.h"
 
+#include "base/logging.h"
 #include "chrome/browser/tab_contents/confirm_infobar_delegate.h"
 #include "chrome/browser/ui/views/event_utils.h"
 #include "views/controls/button/text_button.h"
