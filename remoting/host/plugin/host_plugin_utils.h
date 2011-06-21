fd786be5e32af0ebe71e318d215fac112c6eeae0
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-21 21:40:11 +0000

    Move the Remoting Host plugin into its own sub-directory.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7216018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89901 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/host_plugin_utils.h
+++ remoting/host/plugin/host_plugin_utils.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_HOST_HOST_PLUGIN_UTILS_H_
-#define REMOTING_HOST_HOST_PLUGIN_UTILS_H_
+#ifndef REMOTING_HOST_PLUGIN_HOST_PLUGIN_UTILS_H_
+#define REMOTING_HOST_PLUGIN_HOST_PLUGIN_UTILS_H_
 
 #include <string>
 
@@ -33,4 +33,4 @@ NPObject* ObjectFromNPVariant(const NPVariant& variant);
 
 }  // namespace remoting
 
-#endif  // REMOTING_HOST_HOST_PLUGIN_UTILS_H_
+#endif  // REMOTING_HOST_PLUGIN_HOST_PLUGIN_UTILS_H_
