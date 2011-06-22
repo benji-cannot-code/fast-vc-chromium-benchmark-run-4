abc73bfe207599ce728925a32b7a0c12750072d3
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 14:21:04 +0000

    Move deleting GEARS data to the profile destructor
    BUG=86928
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7229006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_main.h"
 #include "chrome/browser/browser_process_sub_thread.h"
 #include "chrome/browser/browser_trial.h"
-#include "chrome/browser/browsing_data_remover.h"
 #include "chrome/browser/debugger/browser_list_tabcontents_provider.h"
 #include "chrome/browser/debugger/devtools_http_protocol_handler.h"
 #include "chrome/browser/debugger/devtools_manager.h"
@@ -702,7 +701,6 @@ MHTMLGenerationManager* BrowserProcessImpl::mhtml_generation_manager() {
 
 void BrowserProcessImpl::ClearLocalState(const FilePath& profile_path) {
   webkit_database::DatabaseTracker::ClearLocalState(profile_path);
-  BrowsingDataRemover::ClearGearsData(profile_path);
 }
 
 bool BrowserProcessImpl::ShouldClearLocalState(FilePath* profile_path) {
--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_signin.h"
+#include "chrome/browser/browsing_data_remover.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/defaults.h"
@@ -603,6 +604,12 @@ ProfileImpl::~ProfileImpl() {
   StopCreateSessionServiceTimer();
 
   ProfileDependencyManager::GetInstance()->DestroyProfileServices(this);
+
+  if (clear_local_state_on_exit_) {
+    BrowserThread::PostTask(
+        BrowserThread::FILE, FROM_HERE,
+        NewRunnableFunction(&BrowsingDataRemover::ClearGearsData, path_));
+  }
 
   // DownloadManager is lazily created, so check before accessing it.
   if (download_manager_.get()) {
