73464a5649be7006b2f4995a4d25f6c6fc4eb739
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 01:54:56 +0000

    Fix building views_unittests.
    TBR=sky@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89954 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view_unittest.cc
+++ views/view_unittest.cc
@@ -2253,6 +2253,7 @@ class TestCompositor : public ui::Compositor {
   virtual void NotifyStart() OVERRIDE {}
   virtual void NotifyEnd() OVERRIDE {}
   virtual void Blur(const gfx::Rect& bounds) OVERRIDE {}
+  virtual void SchedulePaint() OVERRIDE {}
 
  private:
   DISALLOW_COPY_AND_ASSIGN(TestCompositor);
