de503c7f3ecf99343df61f7b605327d3e843b3f6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-22 17:51:59 +0000

    2011-06-22  Vsevolod Vlasov  <vsevik@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Move logic for deciding whether resource content should be base64 encoded on backend.
            https://bugs.webkit.org/show_bug.cgi?id=63069
    
            * inspector/Inspector.json:
            * inspector/InspectorPageAgent.cpp:
            (WebCore::InspectorPageAgent::cachedResourceContent):
            (WebCore::InspectorPageAgent::resourceContent):
            (WebCore::InspectorPageAgent::getResourceContent):
            (WebCore::InspectorPageAgent::searchInResources):
            * inspector/InspectorPageAgent.h:
            * inspector/InspectorResourceAgent.cpp:
            (WebCore::InspectorResourceAgent::getResourceContent):
            * inspector/InspectorResourceAgent.h:
            * inspector/InspectorStyleSheet.cpp:
            (WebCore::InspectorStyleSheet::resourceStyleSheetText):
            * inspector/front-end/NetworkManager.js:
            (WebInspector.NetworkManager.prototype.requestContent):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.prototype._innerRequestContent.onResourceContent):
            (WebInspector.Resource.prototype._innerRequestContent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
@@ -1070,8 +1070,9 @@ bool InspectorStyleSheet::resourceStyleSheetText(String* result) const
         return false;
 
     String error;
-    InspectorPageAgent::resourceContent(&error, ownerDocument()->frame(), m_pageStyleSheet->finalURL(), false, result);
-    return error.isEmpty();
+    bool base64Encoded;
+    InspectorPageAgent::resourceContent(&error, ownerDocument()->frame(), m_pageStyleSheet->finalURL(), result, &base64Encoded);
+    return error.isEmpty() && !base64Encoded;
 }
 
 bool InspectorStyleSheet::inlineStyleSheetText(String* result) const
