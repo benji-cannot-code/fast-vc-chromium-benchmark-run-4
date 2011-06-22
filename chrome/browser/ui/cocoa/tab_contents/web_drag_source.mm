1167125b7b2a22444f6498647a230f62f6bd529a
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 20:18:41 +0000

    Mac. When a javascript URL cannot be placed on the drag pasteboard, try escaping it. Fixes bugs where badly escaped but popular bookmarklets cannot be dragged to the bookmark bar. BUG=82283,86643 Review URL: http://codereview.chromium.org/7229004
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/web_drag_source.mm
+++ chrome/browser/ui/cocoa/tab_contents/web_drag_source.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/tab_contents_view_mac.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/browser/tab_contents/tab_contents.h"
+#include "content/common/url_constants.h"
 #include "net/base/file_stream.h"
 #include "net/base/net_util.h"
 #import "third_party/mozilla/NSPasteboard+Utils.h"
@@ -187,9 +188,15 @@ void PromiseWriterTask::Run() {
   // URL.
   } else if ([type isEqualToString:NSURLPboardType]) {
     DCHECK(dropData_->url.is_valid());
-    NSURL* url = [NSURL URLWithString:SysUTF8ToNSString(dropData_->url.spec())];
+    NSString* urlStr = SysUTF8ToNSString(dropData_->url.spec());
+    NSURL* url = [NSURL URLWithString:urlStr];
+    // If NSURL creation failed, check for a badly-escaped javascript URL.
+    if (!url && urlStr && dropData_->url.SchemeIs(chrome::kJavaScriptScheme)) {
+      NSString *escapedStr =
+        [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+      url = [NSURL URLWithString:escapedStr];
+    }
     [url writeToPasteboard:pboard];
-
   // URL title.
   } else if ([type isEqualToString:kNSURLTitlePboardType]) {
     [pboard setString:SysUTF16ToNSString(dropData_->url_title)
