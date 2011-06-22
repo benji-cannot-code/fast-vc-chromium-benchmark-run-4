84998a02c4c1e149378b18ad524cbfccd881f283
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 00:16:04 +0000

    Breaking imageburner_ui.{h,cc} into smaller pieces.
    imageburner_ui.h 
    ->ImageBurnUI
    
    imageburner_impl.h
    ->HandlerTaskProxy, Handler, 
    
    imageburner_ui.cc
    -> implementation of imageburner_ui.h and imageburner_ui.impl
    
    imageburner_utils.h
    ->Downlaoder, ConfigFile, StateMachine, ResourceManager
    
    All files are moved to new folder imageburner.
    
    Introducing imageburner namespace and removing ImageBurner from class names. 
    
    Additionally:
      *in Handler::OnConfigFileFetched:
        Changed order we check download state, we now first check if download has
        finished, and if so, we call BunrImage directly. This way we avoid adding
        DownloadManager observer, and removing it just a moment later.
      *in Handler::ProcessError
        We delete download instead of just canceling it + we check that download is
        really a partial download before we delete it.
     
    TEST= Ran ImageBurner and made sure it still runs
    BUG= None
    Review URL: http://codereview.chromium.org/7191029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89941 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_factory.cc
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/ui/webui/chromeos/choose_mobile_network_ui.h"
 #include "chrome/browser/ui/webui/chromeos/enterprise_enrollment_ui.h"
-#include "chrome/browser/ui/webui/chromeos/imageburner_ui.h"
+#include "chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.h"
 #include "chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/login_ui.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
---
+++ chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
+
+#include "chrome/browser/ui/webui/chrome_web_ui.h"
+#include "content/browser/tab_contents/tab_contents.h"
+
+class ImageBurnUI : public ChromeWebUI {
+ public:
+  explicit ImageBurnUI(TabContents* contents);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ImageBurnUI);
+};
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_IMAGEBURNER_IMAGEBURNER_UI_H_
+
