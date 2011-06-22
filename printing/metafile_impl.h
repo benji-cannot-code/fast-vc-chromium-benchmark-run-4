8a3f28294d4092fc0ff35624fc42d976de63e883
caryclark@chromium.org <caryclark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 13:34:57 +0000

    Add support to use Skia printing from Mac.
    Reference the CG metafile from the Skia
    version, so CG can be used to pass the PDF
    data to the OS X pipeline.
    
    If Skia is enabled as the Mac rendering engine,
    generate Skia PDF files instead of CG ones.
    
    This change adds a code path that will be enabled
    in the future, but does not modify any existing
    code, so there is no functional change.
    
    BUG=79463
    TEST=none
    
    Review URL: http://codereview.chromium.org/7120006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90023 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/metafile_impl.h
+++ printing/metafile_impl.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "printing/emf_win.h"
-#include "printing/pdf_metafile_skia.h"
 #elif defined(OS_MACOSX)
 #include "printing/pdf_metafile_cg_mac.h"
 #elif defined(OS_POSIX)
 #include "printing/pdf_metafile_cairo_linux.h"
+#endif
+
+#if !defined(OS_MACOSX) || defined(USE_SKIA)
 #include "printing/pdf_metafile_skia.h"
 #endif
 
@@ -22,8 +24,13 @@ namespace printing {
 typedef Emf NativeMetafile;
 typedef PdfMetafileSkia PreviewMetafile;
 #elif defined(OS_MACOSX)
+#if defined(USE_SKIA)
+typedef PdfMetafileSkia NativeMetafile;
+typedef PdfMetafileSkia PreviewMetafile;
+#else
 typedef PdfMetafileCg NativeMetafile;
 typedef PdfMetafileCg PreviewMetafile;
+#endif
 #elif defined(OS_POSIX)
 typedef PdfMetafileCairo NativeMetafile;
 typedef PdfMetafileSkia PreviewMetafile;
