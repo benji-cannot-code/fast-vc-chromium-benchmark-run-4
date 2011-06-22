b8e0beef9c31a90d8893c4852a2e3a856906f9da
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 20:26:57 +0000

    Coverity: Pass parameters by reference.
    CID=7875,14289,17142,17143
    
    R=kmadhusu@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7217030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90087 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_storage_impl.cc
+++ webkit/appcache/appcache_storage_impl.cc
@@ -1288,8 +1288,10 @@ bool AppCacheStorageImpl::FindResponseForMainRequestInGroup(
 }
 
 void AppCacheStorageImpl::DeliverShortCircuitedFindMainResponse(
-    const GURL& url, AppCacheEntry found_entry,
-    scoped_refptr<AppCacheGroup> group, scoped_refptr<AppCache> cache,
+    const GURL& url,
+    const AppCacheEntry& found_entry,
+    scoped_refptr<AppCacheGroup> group,
+    scoped_refptr<AppCache> cache,
     scoped_refptr<DelegateReference> delegate_ref) {
   if (delegate_ref->delegate) {
     DelegateReferenceVector delegates(1, delegate_ref);
