04d7f71086a08a05744f0f9edc78db72429327d0
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 22:21:15 +0000

    Whitelist one more extension Id for webSocketProxyPrivate and decodeJPEG private APIs.
    BUG=chromium-os:16779
    TEST=None
    
    Review URL: http://codereview.chromium.org/7216032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/web_socket_proxy_controller.cc
+++ chrome/browser/chromeos/web_socket_proxy_controller.cc
@@ -26,12 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char* kSomeWebAppId    = "haiffjcadagjlijoggckpgfnoeiflnem";
-const char* kAnotherWebAppId = "gnedhmakppccajfpfiihfcdlnpgomkcf";
-
 const char* kAllowedIds[] = {
-    kSomeWebAppId,
-    kAnotherWebAppId
+    "haiffjcadagjlijoggckpgfnoeiflnem",
+    "gnedhmakppccajfpfiihfcdlnpgomkcf",
+    "fjcibdnjlbfnbfdjneajpipnlcppleek"
 };
 
 class OriginValidator {
