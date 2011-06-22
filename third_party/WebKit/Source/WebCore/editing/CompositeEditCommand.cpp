47449e0fa8c703bd5b3ec6246772966525c2454a
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-22 02:36:45 +0000

    2011-06-21  Darin Adler  <darin@apple.com>
            Reviewed by Ryosuke Niwa.
    
            RefPtr misused as argument type in a few classes
            https://bugs.webkit.org/show_bug.cgi?id=62955
    
            * dom/DataTransferItem.cpp:
            (WebCore::DataTransferItem::DataTransferItem):
            * dom/DataTransferItem.h:
            * dom/DataTransferItems.cpp:
            (WebCore::DataTransferItems::DataTransferItems):
            * dom/DataTransferItems.h:
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::rebalanceWhitespaceOnTextSubstring):
            * editing/CompositeEditCommand.h:
            * page/WebKitAnimationList.cpp:
            (WebCore::WebKitAnimationList::append):
            (WebCore::WebKitAnimationList::insertAnimation):
            * page/WebKitAnimationList.h:
            * svg/graphics/filters/SVGFEImage.cpp:
            (WebCore::FEImage::FEImage):
            (WebCore::FEImage::create):
            * svg/graphics/filters/SVGFEImage.h:
            * svg/graphics/filters/SVGFilterBuilder.cpp:
            (WebCore::SVGFilterBuilder::SVGFilterBuilder):
            (WebCore::SVGFilterBuilder::add):
            (WebCore::SVGFilterBuilder::appendEffectToEffectReferences):
            * svg/graphics/filters/SVGFilterBuilder.h:
            * websockets/ThreadableWebSocketChannelClientWrapper.cpp:
            (WebCore::ThreadableWebSocketChannelClientWrapper::didConnectCallback):
            (WebCore::ThreadableWebSocketChannelClientWrapper::didReceiveMessageCallback):
            (WebCore::ThreadableWebSocketChannelClientWrapper::didStartClosingHandshakeCallback):
            (WebCore::ThreadableWebSocketChannelClientWrapper::didCloseCallback):
            * websockets/ThreadableWebSocketChannelClientWrapper.h:
            * websockets/WorkerThreadableWebSocketChannel.cpp:
            (WebCore::WorkerThreadableWebSocketChannel::Peer::Peer):
            (WebCore::WorkerThreadableWebSocketChannel::Bridge::setWebSocketChannel):
            (WebCore::WorkerThreadableWebSocketChannel::Bridge::mainThreadCreateWebSocketChannel):
            * websockets/WorkerThreadableWebSocketChannel.h:
            Use PassRefPtr or raw pointer as appropriate for RefPtr arguments.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
@@ -462,8 +462,10 @@ void CompositeEditCommand::rebalanceWhitespaceAt(const Position& position)
     rebalanceWhitespaceOnTextSubstring(static_cast<Text*>(node), position.offsetInContainerNode(), position.offsetInContainerNode());
 }
 
-void CompositeEditCommand::rebalanceWhitespaceOnTextSubstring(RefPtr<Text> textNode, int startOffset, int endOffset)
+void CompositeEditCommand::rebalanceWhitespaceOnTextSubstring(PassRefPtr<Text> prpTextNode, int startOffset, int endOffset)
 {
+    RefPtr<Text> textNode = prpTextNode;
+
     String text = textNode->data();
     ASSERT(!text.isEmpty());
 
@@ -491,7 +493,7 @@ void CompositeEditCommand::rebalanceWhitespaceOnTextSubstring(RefPtr<Text> textN
                                                              isEndOfParagraph(visibleDownstreamPos) || (unsigned)downstream == text.length());
     
     if (string != rebalancedString)
-        replaceTextInNodePreservingMarkers(textNode, upstream, length, rebalancedString);
+        replaceTextInNodePreservingMarkers(textNode.release(), upstream, length, rebalancedString);
 }
 
 void CompositeEditCommand::prepareWhitespaceAtPositionForSplit(Position& position)
