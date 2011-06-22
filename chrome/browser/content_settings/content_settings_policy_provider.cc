07bfe4fc10692a5aded65bb27a427596ccf71d5e
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 20:28:18 +0000

    Coverity:  Null returns and negative returns
    CID = 14804, 16939,  16937, 16904,  16793,  16308, 14956, 15404, 11823
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7211019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_policy_provider.cc
+++ chrome/browser/content_settings/content_settings_policy_provider.cc
@@ -336,6 +336,7 @@ void PolicyProvider::GetContentSettingsFromPreferences(
     }
 
     const PrefService::Preference* pref = prefs->FindPreference(pref_name);
+    DCHECK(pref);
     DCHECK(pref->IsManaged());
     DCHECK_EQ(Value::TYPE_LIST, pref->GetType());
 
