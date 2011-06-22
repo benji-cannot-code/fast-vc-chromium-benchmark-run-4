8b155107693f302579569de9f04049f3954b0be8
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 04:01:15 +0000

    Add Autofill metric logging for uploads corresponding to forms with no server response.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7216006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89973 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_metrics.cc
+++ chrome/browser/autofill/autofill_metrics.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
 #include "chrome/browser/autofill/autofill_type.h"
+#include "chrome/browser/autofill/form_structure.h"
+#include "webkit/glue/form_data.h"
 
 namespace {
 
@@ -24,6 +26,7 @@ enum ServerExperiment {
   ACCEPTANCE_RATIO_05_WINNER_LEAD_RATIO_15_MIN_FORM_SCORE_5,
   TOOLBAR_DATA_ONLY,
   ACCEPTANCE_RATIO_04_WINNER_LEAD_RATIO_3_MIN_FORM_SCORE_4,
+  NO_SERVER_RESPONSE,
   NUM_SERVER_EXPERIMENTS
 };
 
@@ -197,6 +200,8 @@ void LogServerExperimentId(const std::string& histogram_name,
                            const std::string& experiment_id) {
   ServerExperiment metric = UNKNOWN_EXPERIMENT;
 
+  const std::string default_experiment_name =
+      FormStructure(webkit_glue::FormData()).server_experiment_id();
   if (experiment_id.empty())
     metric = NO_EXPERIMENT;
   else if (experiment_id == "ar06")
@@ -217,6 +222,8 @@ void LogServerExperimentId(const std::string& histogram_name,
     metric = TOOLBAR_DATA_ONLY;
   else if (experiment_id == "ar04wr3fs4")
     metric = ACCEPTANCE_RATIO_04_WINNER_LEAD_RATIO_3_MIN_FORM_SCORE_4;
+  else if (experiment_id == default_experiment_name)
+    metric = NO_SERVER_RESPONSE;
 
   DCHECK(metric < NUM_SERVER_EXPERIMENTS);
   LogUMAHistogramEnumeration(histogram_name, metric, NUM_SERVER_EXPERIMENTS);
