58d51282bdd026d4277e7587374f4f12d2e75bf3
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 04:01:34 +0000

    Coverity: Add a couple return checks to TestingAutomationProvider.
    BUG=none
    TEST=none
    CID=12868,12872
    
    Review URL: http://codereview.chromium.org/7219017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -3481,7 +3481,7 @@ webkit_glue::PasswordForm GetPasswordFormFromDict(
   string16 password_element;
   string16 submit_element;
   string16 action_target_text;
-  bool blacklist = false;
+  bool blacklist;
   string16 old_password_element;
   string16 old_password_value;
 
@@ -3495,7 +3495,8 @@ webkit_glue::PasswordForm GetPasswordFormFromDict(
   password_dict.GetString("password_element", &password_element);
   password_dict.GetString("submit_element", &submit_element);
   password_dict.GetString("action_target", &action_target_text);
-  password_dict.GetBoolean("blacklist", &blacklist);
+  if (!password_dict.GetBoolean("blacklist", &blacklist))
+    blacklist = false;
 
   GURL origin_gurl(origin_url_text);
   GURL action_target(action_target_text);
@@ -4680,7 +4681,8 @@ std::vector<CreditCard> TestingAutomationProvider::GetCreditCardsFromList(
 
   int num_credit_cards = cards.GetSize();
   for (int i = 0; i < num_credit_cards; i++) {
-    cards.GetDictionary(i, &card_info);
+    if (!cards.GetDictionary(i, &card_info))
+      continue;
     CreditCard card;
     // Loop through the possible credit card fields and add those provided.
     for (std::map<AutofillFieldType, std::string>::iterator type_it =
