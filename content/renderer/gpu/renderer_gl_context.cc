3ca67fcbf94607413ac324052d37de817eec3eae
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 23:39:14 +0000

    Add more trace events related to latch usage and to track RendererGlContext destruction.
    Review URL: http://codereview.chromium.org/7237005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90131 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/renderer_gl_context.cc
+++ content/renderer/gpu/renderer_gl_context.cc
@@ -544,6 +544,7 @@ bool RendererGLContext::Initialize(bool onscreen,
 }
 
 void RendererGLContext::Destroy() {
+  TRACE_EVENT0("gpu", "RendererGLContext::Destroy");
   SetParent(NULL);
 
   delete gles2_implementation_;
