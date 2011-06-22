17c8dda21e61e5af9bad3fa0895958d2252ee9dd
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 15:50:30 +0000

    Autofill heuristics checkout files for 06_checkout_urbanoutfitters.com.html
    Extends heuristics engine to look at field values.  Tweaks regexes to match idioms in this form.
    
    BUG=86606
    TEST=FormStructureBrowserTest.* with 06_checkout_urbanoutfitters.com.html
    
    Review URL: http://codereview.chromium.org/7206069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/form_field.cc
+++ chrome/browser/autofill/form_field.cc
@@ -162,6 +162,11 @@ bool FormField::Match(const AutofillField* field,
     return true;
   }
 
+  if ((match_type & FormField::MATCH_VALUE) &&
+      autofill::MatchesPattern(field->value, pattern)) {
+    return true;
+  }
+
   return false;
 }
 
