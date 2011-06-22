feb587710d7e78433c4fb72ea30ed619a8e04427
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-22 20:13:53 +0000

    2011-06-22  Sam Weinig  <sam@webkit.org>
            Reviewed by Adam Barth.
    
            Add preference for setting the html parser depth limit
            https://bugs.webkit.org/show_bug.cgi?id=63163
    
            * WebView/WebPreferenceKeysPrivate.h:
            * WebView/WebPreferences.mm:
            (+[WebPreferences initialize]):
            (-[WebPreferences setMaximumHTMLParserDOMTreeDepth:]):
            (-[WebPreferences maximumHTMLParserDOMTreeDepth]):
            * WebView/WebPreferencesPrivate.h:
            * WebView/WebView.mm:
            (-[WebView _preferencesChanged:]):
            Add private preference for setting the parser depth limit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebNSDictionaryExtras.h"
 #import "WebNSURLExtras.h"
 #import <WebCore/ApplicationCacheStorage.h>
+#import <WebCore/Settings.h>
 
 NSString *WebPreferencesChangedNotification = @"WebPreferencesChangedNotification";
 NSString *WebPreferencesRemovedNotification = @"WebPreferencesRemovedNotification";
@@ -376,6 +377,7 @@ static WebCacheModel cacheModelForMainBundle(void)
         [NSNumber numberWithBool:NO],   WebKitMemoryInfoEnabledPreferenceKey,
         [NSNumber numberWithBool:YES],  WebKitHyperlinkAuditingEnabledPreferenceKey,
         [NSNumber numberWithBool:NO],   WebKitUsePreHTML5ParserQuirksKey,
+        [NSNumber numberWithUnsignedInt:WebCore::Settings::defaultMaximumHTMLParserDOMTreeDepth], WebKitMaximumHTMLParserDOMTreeDepthKey,
         [NSNumber numberWithBool:YES],  WebKitAVFoundationEnabledKey,
         [NSNumber numberWithLongLong:WebCore::ApplicationCacheStorage::noQuota()], WebKitApplicationCacheTotalQuota,
         [NSNumber numberWithLongLong:WebCore::ApplicationCacheStorage::noQuota()], WebKitApplicationCacheDefaultOriginQuota,
@@ -1407,6 +1409,16 @@ static NSString *classIBCreatorID = nil;
 - (void)setUsePreHTML5ParserQuirks:(BOOL)flag
 {
     [self _setBoolValue:flag forKey:WebKitUsePreHTML5ParserQuirksKey];
+}
+
+- (void)setMaximumHTMLParserDOMTreeDepth:(unsigned)depth
+{
+    [self _setIntegerValue:depth forKey:WebKitMaximumHTMLParserDOMTreeDepthKey];
+}
+
+- (unsigned)maximumHTMLParserDOMTreeDepth
+{
+    return [self _integerValueForKey:WebKitMaximumHTMLParserDOMTreeDepthKey];
 }
 
 - (void)didRemoveFromWebView
