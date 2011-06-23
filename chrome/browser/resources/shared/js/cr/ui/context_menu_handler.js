362ce1b7834bedfc1b8a6533dde3f980221229e3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 15:52:47 +0000

    ntp4: add app context menu
    BUG=none
    TEST=manual (see below)
    1: all menu items work
    2. middle clicking or ctrl-clicking on an app affects the launch type
    3: when you change the app launch type, that change is visible both on the current NTP and all other currently open NTP tabs
    
    Review URL: http://codereview.chromium.org/7215035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90218 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/context_menu_handler.js
+++ chrome/browser/resources/shared/js/cr/ui/context_menu_handler.js
@@ -1,11 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 cr.define('cr.ui', function() {
 
-  const positionPopupAtPoint = cr.ui.positionPopupAtPoint;
   const Menu = cr.ui.Menu;
 
   /**
@@ -96,7 +95,7 @@ cr.define('cr.ui', function() {
         y = e.clientY;
       }
 
-      positionPopupAtPoint(x, y, menu);
+      cr.ui.positionPopupAtPoint(x, y, menu);
     },
 
     /**
