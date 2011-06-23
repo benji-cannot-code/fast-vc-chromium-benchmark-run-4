0783b145c42243ba689b0719b22c9def0b84e70b
husky@google.com <husky@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 10:52:25 +0000

    Style fix for member variable.
    Changing m_file to file_.
    Review URL: http://codereview.chromium.org/7207005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90193 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/trace_subscriber_stdio.cc
+++ content/browser/trace_subscriber_stdio.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 TraceSubscriberStdio::TraceSubscriberStdio(const FilePath& path) {
   LOG(INFO) << "Logging performance trace to file: " << path.value();
-  m_file = file_util::OpenFile(path, "w+");
+  file_ = file_util::OpenFile(path, "w+");
   if (IsValid()) {
     // FIXME: the file format expects it to start with "[".
-    fputc('[', m_file);
+    fputc('[', file_);
   } else {
     LOG(ERROR) << "Failed to open performance trace file: " << path.value();
   }
@@ -23,15 +23,15 @@ TraceSubscriberStdio::~TraceSubscriberStdio() {
 }
 
 bool TraceSubscriberStdio::IsValid() {
-  return m_file && (0 == ferror(m_file));
+  return file_ && (0 == ferror(file_));
 }
 
 void TraceSubscriberStdio::OnEndTracingComplete() {
-  if (m_file) {
+  if (file_) {
     // FIXME: the file format expects it to end with "]".
-    fputc(']', m_file);
-    fclose(m_file);
-    m_file = 0;
+    fputc(']', file_);
+    fclose(file_);
+    file_ = 0;
   }
 }
 
@@ -47,12 +47,12 @@ void TraceSubscriberStdio::OnTraceDataCollected(
   const char* data = json_events.data() + 1;
   size_t size = json_events.size() - 2;
 
-  size_t written = fwrite(data, 1, size, m_file);
+  size_t written = fwrite(data, 1, size, file_);
   if (written != size) {
-    LOG(ERROR) << "Error " << ferror(m_file) << " when writing to trace file";
-    fclose(m_file);
-    m_file = 0;
+    LOG(ERROR) << "Error " << ferror(file_) << " when writing to trace file";
+    fclose(file_);
+    file_ = 0;
   } else {
-    fputc(',', m_file);
+    fputc(',', file_);
   }
 }
