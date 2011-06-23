6dd89650ca06f638013361b44aace8357bde1584
yael.aharon@nokia.com <yael.aharon@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 03:26:53 +0000

    Unreviewed build fix after r89472.
    No new tests, just a build fix.
    
    * rendering/InlineBox.cpp:
    * rendering/RenderCounter.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/InlineBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/InlineBox.cpp
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderBlock.h"
 #include "RootInlineBox.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 using namespace std;
 
 namespace WebCore {
