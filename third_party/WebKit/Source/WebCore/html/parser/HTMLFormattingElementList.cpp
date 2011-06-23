efdce10b3c40a7ce285e7d7b070f1d8192cbddc3
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 01:27:19 +0000

    2011-06-22  Ryosuke Niwa  <rniwa@webkit.org>
            Yet another build fix after r89472.
    
            * html/parser/HTMLFormattingElementList.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/HTMLFormattingElementList.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLFormattingElementList.cpp
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Element.h"
 #include "NotImplemented.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace WebCore {
 
 HTMLFormattingElementList::HTMLFormattingElementList()
