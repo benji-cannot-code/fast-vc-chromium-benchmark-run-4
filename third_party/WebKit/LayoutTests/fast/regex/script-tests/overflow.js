cfddd48829a3e4a368448b0c8768485a082b4345
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 21:35:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=61585 Crash running regexp /(?:(?=g))|(?:m).{2147483648,}/
    Reviewed by Oliver Hunt.
    
    Source/JavaScriptCore: 
    
    This is due to use of int instead of unsigned, bad math around
    the 2^31 boundary.
    
    * yarr/YarrInterpreter.cpp:
    (JSC::Yarr::ByteCompiler::emitDisjunction):
        - Change some uses of int to unsigned, refactor compare logic to
          restrict to the range 0..2^32-1 (rather than -2^32-1..2^32-1).
    * yarr/YarrJIT.cpp:
    (JSC::Yarr::YarrGenerator::generate):
    (JSC::Yarr::YarrGenerator::backtrack):
        - Ditto.
    
    LayoutTests: 
    
    Add regression tests where an alterative has a size of ~2^31.
    
    * fast/regex/overflow-expected.txt: Added.
    * fast/regex/overflow.html: Added.
    * fast/regex/script-tests/overflow.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/regex/script-tests/overflow.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This test checks expressions with alternative lengths of appox. 2^31.");
+
+var regexp1 = /(?:(?=g))|(?:m).{2147483648,}/;
+shouldBe("regexp1.exec('')", 'null');
+
+var regexp2 = /(?:(?=g)).{2147483648,}/;
+shouldBe("regexp2.exec('')", 'null');
+
+var successfullyParsed = true;
+
