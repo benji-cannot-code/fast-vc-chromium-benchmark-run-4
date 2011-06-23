6398e299a1434254fe7fa0532f1f2e6cb63a8275
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 22:01:45 +0000

    audio.h: Fixed path core.h: Fixed duplication of @param[in] rect.h: changed point to origin, changed argument size to s, misspelled retrn (return) size.h: changed argument point to s Review URL: http://codereview.chromium.org/7245008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90275 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/audio.h
+++ ppapi/cpp/audio.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace pp {
 
 /// An audio resource. Refer to the
-/// <a href="chrome/nativeclient/docs/audio.html">Pepper
+/// <a href="/chrome/nativeclient/docs/audio.html">Pepper
 /// Audio API Code Walkthrough</a> for information on using this interface.
 class Audio : public Resource {
  public:
