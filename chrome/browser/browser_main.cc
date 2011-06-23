772a8bdfad52758d466f73a7bdd416667b5a3009
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 19:09:37 +0000

    Fix mac
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -115,7 +115,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/gl/gl_implementation.h"
 #include "ui/gfx/gl/gl_switches.h"
-#include "views/desktop/desktop_window.h"
 
 #if defined(USE_LINUX_BREAKPAD)
 #include "base/linux_util.h"
@@ -194,6 +193,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/browser/ui/views/chrome_views_delegate.h"
+#include "views/desktop/desktop_window.h"
 #include "views/focus/accelerator_handler.h"
 #include "views/widget/widget.h"
 #if defined(TOOLKIT_USES_GTK)
