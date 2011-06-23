6cfb3864b4982ee0fc897405f5e4f01c0e482b58
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 23:20:39 +0000

    Remove default initializtion of BirthPlace in Tracked.
    Previously, MessageLoop::PostTask set the BirthPlace of the Task object
    (subclass of Tracked), which would decrement the counter for the
    Location("NoFunctionName", "NeedToSetBirthPlace", -1) Birth, and replace
    it with a more appropriate Location provided by the FROM_HERE argument.
    
    With the MessageLoop restructuring in r82300, tracking of Births is moved
    up from the Task object into the MessageLoop::PendingTask structure. The
    side-effect is that the default birth is never decremented, and we double
    count each task's creation.
    
    This default Birth is effectively a count of "tasks that were created, but
    not posted" without a stored reference to location, so removing it is
    the simplest fix to the double counting.
    
    BUG=none
    TEST=about:tasks looks sane.
    
    Review URL: http://codereview.chromium.org/7029038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90290 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tracked_objects_unittest.cc
+++ base/tracked_objects_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -61,10 +61,12 @@ TEST_F(TrackedObjectsTest, TinyStartupShutdown) {
     return;
 
   // Instigate tracking on a single tracked object, or our thread.
+  const Location& location = FROM_HERE;
   NoopTracked tracked;
+  tracked.SetBirthPlace(location);
 
   const ThreadData* data = ThreadData::first();
-  EXPECT_TRUE(data);
+  ASSERT_TRUE(data);
   EXPECT_TRUE(!data->next());
   EXPECT_EQ(data, ThreadData::current());
   ThreadData::BirthMap birth_map;
@@ -77,7 +79,9 @@ TEST_F(TrackedObjectsTest, TinyStartupShutdown) {
 
 
   // Now instigate a birth, and a death.
-  delete new NoopTracked;
+  NoopTracked* new_tracked = new NoopTracked;
+  new_tracked->SetBirthPlace(location);
+  delete new_tracked;
 
   birth_map.clear();
   data->SnapshotBirthMap(&birth_map);
