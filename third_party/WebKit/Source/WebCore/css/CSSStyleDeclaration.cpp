4d0481cccbe90d28ed7f2e24bb9bec8fedae8892
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 01:05:09 +0000

    2011-06-22  Ryosuke Niwa  <rniwa@webkit.org>
            Build fix after r89472.
    
            * css/CSSStyleDeclaration.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleDeclaration.cpp
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGElement.h"
 #include <wtf/ASCIICType.h>
 #include <wtf/text/CString.h>
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
 
 using namespace WTF;
 
