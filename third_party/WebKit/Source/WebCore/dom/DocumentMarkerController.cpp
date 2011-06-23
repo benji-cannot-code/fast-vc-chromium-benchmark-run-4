5e8eef46b52a905992336b9cc4a4f5fbbcb51416
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 01:20:45 +0000

    2011-06-22  Ryosuke Niwa  <rniwa@webkit.org>
            Another build fix after r89472.
    
            * dom/DocumentMarkerController.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DocumentMarkerController.cpp
+++ third_party/WebKit/Source/WebCore/dom/DocumentMarkerController.cpp
@@ -34,6 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderedDocumentMarker.h"
 #include "TextIterator.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace WebCore {
 
 inline bool DocumentMarkerController::possiblyHasMarkers(DocumentMarker::MarkerTypes types)
