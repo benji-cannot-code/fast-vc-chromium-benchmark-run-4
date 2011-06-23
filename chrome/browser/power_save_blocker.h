c9517013293e4c92caed7d414d0aabeb1f019559
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 21:29:36 +0000

    Move power management calls to their own thread.
    BUG=79559
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/7247016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90270 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/power_save_blocker.h
+++ chrome/browser/power_save_blocker.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 
-// A RAII-style class to block the system from entering low-power
-// (sleep) mode.
+// A RAII-style class to block the system from entering low-power (sleep) mode.
 class PowerSaveBlocker {
  public:
   explicit PowerSaveBlocker(bool enabled);
@@ -25,7 +24,7 @@ class PowerSaveBlocker {
 
  private:
   // Platform-specific function called when enable state is changed.
-  // Guarenteed to be called only from the UI thread.
+  // Guaranteed to be called only from the UI thread.
   static void ApplyBlock(bool blocked);
 
   // Called only from UI thread.
