8c483c59cca9494354aa4ae2a6d244b9683136c5
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 21:06:28 +0000

    Use BrowserList::GetLastActive with profile when possible.
    BUG=86361
    TEST=first run bubble tests continue to work.
    
    Review URL: http://codereview.chromium.org/7249005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90264 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/first_run_bubble.cc
+++ chrome/browser/ui/views/first_run_bubble.cc
@@ -162,7 +162,7 @@ void FirstRunBubbleView::ButtonPressed(views::Button* sender,
     UserMetrics::RecordAction(
                     UserMetricsAction("FirstRunBubbleView_ChangeButton"));
 
-    Browser* browser = BrowserList::GetLastActive();
+    Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
     if (browser) {
       browser->OpenSearchEngineOptionsDialog();
     }
