bbb5c4cb8284684b4f6ec17a47da2dd679556ba6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 01:07:19 +0000

    2011-06-22  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Antonio Gomes.
    
            [EFL] Add an option to enable Device Orientation Event.
            https://bugs.webkit.org/show_bug.cgi?id=63120
    
            ADD ENABLE_DEVICE_ORIENTATION.
    
            * Source/cmake/OptionsEfl.cmake:
            * Source/cmakeconfig.h.cmake:
    2011-06-22  Ryuan Choi  <ryuan.choi@samsung.com>
    
            Reviewed by Antonio Gomes.
    
            [EFL] Add an option to enable Device Orientation Event.
            https://bugs.webkit.org/show_bug.cgi?id=63120
    
            * CMakeListsEfl.txt: Add files to build with ENABLE_DEVICE_ORIENTATION.
            * ewk/ewk_view.cpp:
            (_ewk_view_priv_new): Create DeviceOrientationClientEfl and DeviceMotionClientEfl.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
@@ -53,6 +53,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <inttypes.h>
 #include <sys/time.h>
 
+#if ENABLE(DEVICE_ORIENTATION)
+#include "DeviceMotionClientEfl.h"
+#include "DeviceOrientationClientEfl.h"
+#endif
+
 #define ZOOM_MIN (0.05)
 #define ZOOM_MAX (4.0)
 
@@ -544,6 +549,10 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* sd)
     pageClients.editorClient = static_cast<WebCore::EditorClient*>(new WebCore::EditorClientEfl(sd->self));
     pageClients.dragClient = static_cast<WebCore::DragClient*>(new WebCore::DragClientEfl);
     pageClients.inspectorClient = static_cast<WebCore::InspectorClient*>(new WebCore::InspectorClientEfl);
+#if ENABLE(DEVICE_ORIENTATION)
+    pageClients.deviceMotionClient = static_cast<WebCore::DeviceMotionClient*>(new WebCore::DeviceMotionClientEfl);
+    pageClients.deviceOrientationClient = static_cast<WebCore::DeviceOrientationClient*>(new WebCore::DeviceOrientationClientEfl);
+#endif
     priv->page = new WebCore::Page(pageClients);
     if (!priv->page) {
         CRITICAL("Could not create WebKit Page");
