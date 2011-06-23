benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 947bc99facc95164cbba8f520521566a989dd91c

2011-06-13  Nate Chapin  <japhet@chromium.org>
        Reviewed by Darin Fisher.

        Test for https://bugs.webkit.org/show_bug.cgi?id=61482,
        mostly written by Kelly Norton (knorton@google.com).

        * plugins/npruntime/embed-property-equality-expected.txt: Added.
        * plugins/npruntime/embed-property-equality.html: Added.
2011-06-13  Nate Chapin  <japhet@chromium.org>

        Reviewed by Darin Fisher.

        [V8] Cache V8NPObjects so that we don't create multiple
        NPObjects for the same v8::Object.
        https://bugs.webkit.org/show_bug.cgi?id=61482

        Test: plugins/npruntime/embed-property-equality.html

        * bindings/v8/NPV8Object.cpp:
        (WebCore::freeV8NPObject):
        (WebCore::npCreateV8ScriptObject):
2011-06-13  Nate Chapin  <japhet@chromium.org>

        Reviewed by Darin Fisher.

        New test method on TestNetscapePlugin for https://bugs.webkit.org/show_bug.cgi?id=61482.

        * DumpRenderTree/TestNetscapePlugIn/PluginObject.cpp:

git-svn-id: svn://svn.chromium.org/blink/trunk@88679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/plugins/npruntime/embed-property-equality-expected.txt
@@ -1,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Test equality of plugin object properties.
+
+
+Send two references of a JavaScript object to the plugin for identity comparison in C++ PASS

third_party/WebKit/LayoutTests/plugins/npruntime/embed-property-equality.html
@@ -1,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<body onload="test()">
+<p>Test equality of plugin object properties.</p>
+<div id=div>
+<embed id="plugin" type="application/x-webkit-test-netscape">
+</div>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+    
+window.onload = function() {
+    var plugin = document.getElementById('plugin');
+    var div = document.createElement('div');
+    div.textContent = "Send two references of a JavaScript object to the plugin for identity comparison in C++ " 
+        + (plugin.objectsAreSame(document, document) ? "PASS" : "FAIL");
+    document.body.appendChild(div);
+}
+</script>
+</body>

third_party/WebKit/Source/WebCore/bindings/v8/NPV8Object.cpp
@@ -62,6 +62,14 @@ WrapperTypeInfo* npObjectTypeInfo()
     return &typeInfo;
 }
 
+typedef HashMap<int, V8NPObject*> V8NPObjectMap;
+
+static V8NPObjectMap* staticV8NPObjectMap()
+{
+    DEFINE_STATIC_LOCAL(V8NPObjectMap, v8npObjectMap, ());
+    return &v8npObjectMap;
+}
+
 // FIXME: Comments on why use malloc and free.
 static NPObject* allocV8NPObject(NPP, NPClass*)
 {
@@ -71,6 +79,14 @@ static NPObject* allocV8NPObject(NPP, NPClass*)
 static void freeV8NPObject(NPObject* npObject)
 {
     V8NPObject* v8NpObject = reinterpret_cast<V8NPObject*>(npObject);
+    if (int v8ObjectHash = v8NpObject->v8Object->GetIdentityHash()) {
+        ASSERT(staticV8NPObjectMap()->contains(v8ObjectHash));
+        staticV8NPObjectMap()->remove(v8ObjectHash);
+    } else {
+        ASSERT(!v8::Context::InContext());
+        staticV8NPObjectMap()->clear();
+    }
+
 #ifndef NDEBUG
     V8GCController::unregisterGlobalHandle(v8NpObject, v8NpObject->v8Object);
 #endif
@@ -126,12 +142,24 @@ NPObject* npCreateV8ScriptObject(NPP npp, v8::Handle<v8::Object> object, DOMWind
         }
     }
 
+    int v8ObjectHash = object->GetIdentityHash();
+    ASSERT(v8ObjectHash);
+    if (staticV8NPObjectMap()->contains(v8ObjectHash)) {
+        V8NPObject* v8npObject = staticV8NPObjectMap()->get(v8ObjectHash);
+        ASSERT(v8npObject->v8Object == object);
+        _NPN_RetainObject(&v8npObject->object);
+        return reinterpret_cast<NPObject*>(v8npObject);
+    }
+
     V8NPObject* v8npObject = reinterpret_cast<V8NPObject*>(_NPN_CreateObject(npp, &V8NPObjectClass));
     v8npObject->v8Object = v8::Persistent<v8::Object>::New(object);
 #ifndef NDEBUG
     V8GCController::registerGlobalHandle(NPOBJECT, v8npObject, v8npObject->v8Object);
 #endif
     v8npObject->rootObject = root;
+
+    staticV8NPObjectMap()->set(v8ObjectHash, v8npObject);
+
     return reinterpret_cast<NPObject*>(v8npObject);
 }
 

third_party/WebKit/Tools/DumpRenderTree/TestNetscapePlugIn/PluginObject.cpp
@@ -206,6 +206,7 @@ enum {
     ID_RESIZE_TO,
     ID_NORMALIZE,
     ID_INVALIDATE_RECT,
+    ID_OBJECTS_ARE_SAME,
     NUM_METHOD_IDENTIFIERS
 };
 
@@ -247,7 +248,8 @@ static const NPUTF8 *pluginMethodIdentifierNames[NUM_METHOD_IDENTIFIERS] = {
     "setStatus",
     "resizeTo",
     "normalize",
-    "invalidateRect"
+    "invalidateRect",
+    "objectsAreSame"
 };
 
 static NPUTF8* createCStringFromNPVariant(const NPVariant* variant)
@@ -1009,6 +1011,15 @@ static bool invalidateRect(PluginObject* obj, const NPVariant* args, uint32_t ar
     return true;
 }
 
+static bool objectsAreSame(PluginObject* obj, const NPVariant* args, uint32_t argCount, NPVariant* result)
+{
+    if (argCount != 2 || !NPVARIANT_IS_OBJECT(args[0]) || !NPVARIANT_IS_OBJECT(args[1]))
+        return false;
+
+    BOOLEAN_TO_NPVARIANT(NPVARIANT_TO_OBJECT(args[0]) == NPVARIANT_TO_OBJECT(args[1]), *result);
+    return true;
+}
+
 static bool pluginInvoke(NPObject* header, NPIdentifier name, const NPVariant* args, uint32_t argCount, NPVariant* result)
 {
     PluginObject* plugin = reinterpret_cast<PluginObject*>(header);
@@ -1125,6 +1136,8 @@ static bool pluginInvoke(NPObject* header, NPIdentifier name, const NPVariant* a
         return normalizeOverride(plugin, args, argCount, result);
     if (name == pluginMethodIdentifiers[ID_INVALIDATE_RECT])
         return invalidateRect(plugin, args, argCount, result);
+    if (name == pluginMethodIdentifiers[ID_OBJECTS_ARE_SAME])
+        return objectsAreSame(plugin, args, argCount, result);
 
     return false;
 }

commit 0638d30f60148df925314a1121293e47f620238f

[WebKit2 Tests] plugins/npruntime/embed-property-equality.html failing since introduction in r88679. https://bugs.webkit.org/show_bug.cgi?id=63205
Add expected failing result in order to get the bots green.

* platform/mac-wk2/plugins: Added.
* platform/mac-wk2/plugins/npruntime: Added.
* platform/mac-wk2/plugins/npruntime/embed-property-equality-expected.txt: Added.



git-svn-id: svn://svn.chromium.org/blink/trunk@89500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/LayoutTests/platform/mac-wk2/plugins/npruntime/embed-property-equality-expected.txt
@@ -1,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Test equality of plugin object properties.
+
+
+Send two references of a JavaScript object to the plugin for identity comparison in C++ FAIL