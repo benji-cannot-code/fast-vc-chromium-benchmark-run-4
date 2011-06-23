66668db107dd92ad184bf5cf585d907bcf1e44ad
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 23:20:47 +0000

    Fix panel focus issue on Windows.
    BUG=none
    TEST=existing tests
    
    Review URL: http://codereview.chromium.org/7245011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90291 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_view.h
+++ chrome/browser/ui/panels/panel_browser_view.h
@@ -30,6 +30,7 @@ class PanelBrowserView : public BrowserView,
 
   Panel* panel() const { return panel_.get(); }
   bool closed() const { return closed_; }
+  bool focused() const { return focused_; }
 
   // Called from frame view when title bar receives a mouse event.
   // Return true if the event is handled.
@@ -93,6 +94,9 @@ class PanelBrowserView : public BrowserView,
 
   // Is the panel being closed? Do not use it when it is closed.
   bool closed_;
+
+  // Is the panel receiving the focus?
+  bool focused_;
 
   // Is the mouse button currently down?
   bool mouse_pressed_;
