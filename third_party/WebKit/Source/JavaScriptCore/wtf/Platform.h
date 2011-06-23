faf7b51de767ac27a0f6cad15ef53baa4d7240fe
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 17:02:42 +0000

    2011-06-23  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Andreas Kling.
    
            [EFL][WK2] Add PLATFORM(EFL) to use UNIX_DOMAIN_SOCKETS.
            https://bugs.webkit.org/show_bug.cgi?id=63228
    
            * wtf/Platform.h: Add PLATFORM(EFL) guard.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -1201,7 +1201,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    breakages one port at a time. */
 #define WTF_USE_EXPORT_MACROS 0
 
-#if (PLATFORM(QT) && !OS(SYMBIAN)) || PLATFORM(GTK)
+#if (PLATFORM(QT) && !OS(SYMBIAN)) || PLATFORM(GTK) || PLATFORM(EFL)
 #define WTF_USE_UNIX_DOMAIN_SOCKETS 1
 #endif
 
