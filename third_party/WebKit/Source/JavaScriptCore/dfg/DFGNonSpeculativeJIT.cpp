1406446fc9f05f04d1251c6ad5c8478872c2400a
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-23 21:04:40 +0000

    https://bugs.webkit.org/show_bug.cgi?id=63218 DFG JIT - remove machine type guarantees from graph
    Reviewed by Sam Weinig.
    
    The DFG JIT currently makes assumptions about the types of machine registers
    that certain nodes will be loaded into. This will be broken as we generate
    nodes to produce both integer and double code paths. Remove int<->double
    conversions nodes. This design decision also gave rise to multiple types of
    constant nodes, requiring separate handling for each type. Merge these back
    into JSConstant.
    
    * dfg/DFGAliasTracker.h:
    (JSC::DFG::AliasTracker::equalIgnoringLaterNumericConversion):
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::getToInt32):
    (JSC::DFG::ByteCodeParser::getToNumber):
    (JSC::DFG::ByteCodeParser::toInt32):
    (JSC::DFG::ByteCodeParser::toNumber):
    (JSC::DFG::ByteCodeParser::isInt32Constant):
    (JSC::DFG::ByteCodeParser::isDoubleConstant):
    (JSC::DFG::ByteCodeParser::valueOfInt32Constant):
    (JSC::DFG::ByteCodeParser::valueOfDoubleConstant):
    (JSC::DFG::ByteCodeParser::one):
    (JSC::DFG::ByteCodeParser::predictInt32):
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::dump):
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::JITCodeGenerator::silentFillGPR):
    (JSC::DFG::JITCodeGenerator::silentFillFPR):
    (JSC::DFG::JITCodeGenerator::isJSConstant):
    (JSC::DFG::JITCodeGenerator::isDoubleConstant):
    (JSC::DFG::JITCodeGenerator::valueOfJSConstantAsImmPtr):
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::fillNumericToDouble):
    (JSC::DFG::JITCompiler::fillInt32ToInteger):
    * dfg/DFGJITCompiler.h:
    (JSC::DFG::JITCompiler::isJSConstant):
    (JSC::DFG::JITCompiler::isInt32Constant):
    (JSC::DFG::JITCompiler::isDoubleConstant):
    (JSC::DFG::JITCompiler::valueOfJSConstant):
    (JSC::DFG::JITCompiler::valueOfInt32Constant):
    (JSC::DFG::JITCompiler::valueOfDoubleConstant):
    * dfg/DFGNode.h:
    (JSC::DFG::Node::Node):
    (JSC::DFG::Node::isConstant):
    (JSC::DFG::Node::notTakenBytecodeOffset):
    * dfg/DFGNonSpeculativeJIT.cpp:
    (JSC::DFG::NonSpeculativeJIT::isKnownInteger):
    (JSC::DFG::NonSpeculativeJIT::isKnownNumeric):
    (JSC::DFG::NonSpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::fillSpeculateIntInternal):
    (JSC::DFG::SpeculativeJIT::fillSpeculateCell):
    (JSC::DFG::SpeculativeJIT::compilePeepHoleIntegerBranch):
    (JSC::DFG::SpeculativeJIT::compile):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.cpp
@@ -104,7 +104,7 @@ void NonSpeculativeJIT::valueToInt32(JSValueOperand& operand, GPRReg result)
     m_jit.zeroExtend32ToPtr(GPRInfo::returnValueGPR, result);
     silentFillAllRegisters(result);
     JITCompiler::Jump hasCalledToInt32 = m_jit.jump();
-    
+
     // Then handle integers.
     isInteger.link(&m_jit);
     m_jit.zeroExtend32ToPtr(jsValueGpr, result);
@@ -128,6 +128,9 @@ void NonSpeculativeJIT::numberToInt32(FPRReg fpr, GPRReg gpr)
 
 bool NonSpeculativeJIT::isKnownInteger(NodeIndex nodeIndex)
 {
+    if (isInt32Constant(nodeIndex))
+        return true;
+
     GenerationInfo& info = m_generationInfo[m_jit.graph()[nodeIndex].virtualRegister()];
 
     DataFormat registerFormat = info.registerFormat();
@@ -139,11 +142,14 @@ bool NonSpeculativeJIT::isKnownInteger(NodeIndex nodeIndex)
         return (spillFormat | DataFormatJS) == DataFormatJSInteger;
 
     ASSERT(isConstant(nodeIndex));
-    return isInt32Constant(nodeIndex);
+    return false;
 }
 
 bool NonSpeculativeJIT::isKnownNumeric(NodeIndex nodeIndex)
 {
+    if (isInt32Constant(nodeIndex) || isDoubleConstant(nodeIndex))
+        return true;
+
     GenerationInfo& info = m_generationInfo[m_jit.graph()[nodeIndex].virtualRegister()];
 
     DataFormat registerFormat = info.registerFormat();
@@ -157,7 +163,7 @@ bool NonSpeculativeJIT::isKnownNumeric(NodeIndex nodeIndex)
             || (spillFormat | DataFormatJS) == DataFormatJSDouble;
 
     ASSERT(isConstant(nodeIndex));
-    return isInt32Constant(nodeIndex) || isDoubleConstant(nodeIndex);
+    return false;
 }
 
 void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, Node& node)
@@ -185,8 +191,6 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
         break;
     }
 
-    case Int32Constant:
-    case DoubleConstant:
     case JSConstant:
         initConstantInfo(m_compileIndex);
         break;
@@ -280,15 +284,6 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
         break;
     }
 
-    case Int32ToNumber: {
-        IntegerOperand op1(this, node.child1);
-        FPRTemporary result(this);
-        m_jit.convertInt32ToDouble(op1.gpr(), result.fpr());
-        doubleResult(result.fpr(), m_compileIndex);
-        break;
-    }
-
-    case NumberToInt32:
     case ValueToInt32: {
         ASSERT(!isInt32Constant(node.child1));
 
@@ -300,16 +295,14 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
             break;
         }
 
-        if (isKnownNumeric(node.child1)) {
+        GenerationInfo& childInfo = m_generationInfo[m_jit.graph()[node.child1].virtualRegister()];
+        if ((childInfo.registerFormat() || DataFormatJS) == DataFormatJSDouble) {
             DoubleOperand op1(this, node.child1);
             GPRTemporary result(this);
             numberToInt32(op1.fpr(), result.gpr());
             integerResult(result.gpr(), m_compileIndex);
             break;
         }
-
-        // We should have handled this via isKnownInteger, or isKnownNumeric!
-        ASSERT(op != NumberToInt32);
 
         JSValueOperand op1(this, node.child1);
         GPRTemporary result(this, op1);
@@ -330,7 +323,8 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
             break;
         }
 
-        if (isKnownNumeric(node.child1)) {
+        GenerationInfo& childInfo = m_generationInfo[m_jit.graph()[node.child1].virtualRegister()];
+        if ((childInfo.registerFormat() || DataFormatJS) == DataFormatJSDouble) {
             DoubleOperand op1(this, node.child1);
             FPRTemporary result(this, op1);
             m_jit.moveDouble(op1.fpr(), result.fpr());
