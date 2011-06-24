ae70e2e91d33f0018002482391fe9136afcad2f1
rkn@chromium.org <rkn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-24 00:05:57 +0000

    Moved listen_socket_unittest.{h,cc} to the net namespace. Fixed nits related to this.
    BUG=87032
    TEST=None
    
    Review URL: http://codereview.chromium.org/7244020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90300 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/listen_socket_unittest.cc
+++ net/base/listen_socket_unittest.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_util.h"
 #include "testing/platform_test.h"
 
+namespace net {
+
 const int ListenSocketTester::kTestPort = 9999;
 
 static const int kReadBufSize = 1024;
@@ -187,30 +189,29 @@ bool ListenSocketTester::Send(SOCKET sock, const std::string& str) {
   return true;
 }
 
-void ListenSocketTester::DidAccept(net::ListenSocket *server,
-                                   net::ListenSocket *connection) {
+void ListenSocketTester::DidAccept(ListenSocket *server,
+                                   ListenSocket *connection) {
   connection_ = connection;
   connection_->AddRef();
   ReportAction(ListenSocketTestAction(ACTION_ACCEPT));
 }
 
-void ListenSocketTester::DidRead(net::ListenSocket *connection,
+void ListenSocketTester::DidRead(ListenSocket *connection,
                                  const char* data,
                                  int len) {
   std::string str(data, len);
   ReportAction(ListenSocketTestAction(ACTION_READ, str));
 }
 
-void ListenSocketTester::DidClose(net::ListenSocket *sock) {
+void ListenSocketTester::DidClose(ListenSocket *sock) {
   ReportAction(ListenSocketTestAction(ACTION_CLOSE));
 }
 
 ListenSocketTester::~ListenSocketTester() {}
 
-net::ListenSocket* ListenSocketTester::DoListen() {
-  return net::ListenSocket::Listen(kLoopback, kTestPort, this);
+ListenSocket* ListenSocketTester::DoListen() {
+  return ListenSocket::Listen(kLoopback, kTestPort, this);
 }
-
 
 class ListenSocketTest: public PlatformTest {
  public:
@@ -244,3 +245,5 @@ TEST_F(ListenSocketTest, ClientSendLong) {
 TEST_F(ListenSocketTest, ServerSend) {
   tester_->TestServerSend();
 }
+
+}  // namespace net
