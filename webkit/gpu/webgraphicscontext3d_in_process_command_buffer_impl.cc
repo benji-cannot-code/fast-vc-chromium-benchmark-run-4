30778173946f2b08780b078f4d3a187c5f66dc04
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-24 00:50:55 +0000

    Update src/webkit/gpu to reflect WEBKIT_FRAME_TO_DOCUMENT_API_MOVE
    Review URL: http://codereview.chromium.org/7240013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90313 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
+++ webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/gpu_scheduler.h"
 #include "gpu/command_buffer/service/command_buffer_service.h"
 #include "gpu/GLES2/gles2_command_buffer.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "ui/gfx/gl/gl_share_group.h"
@@ -796,7 +797,7 @@ bool WebGraphicsContext3DInProcessCommandBufferImpl::initialize(
 
   GURL active_url;
   if (web_view && web_view->mainFrame())
-    active_url = GURL(web_view->mainFrame()->url());
+    active_url = GURL(web_view->mainFrame()->document().url());
 
   GLInProcessContext* parent_context = NULL;
   if (!render_directly_to_web_view) {
