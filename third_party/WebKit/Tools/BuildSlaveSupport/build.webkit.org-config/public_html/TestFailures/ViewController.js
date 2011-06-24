74b5a75711815978e01b7f4ae3bc9d98182bc7dc
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-24 19:36:44 +0000

    Add links to regression ranges in Trac to the TestFailures page
    Fixes <http://webkit.org/b/61060> <rdar://problem/9452153> TestFailures page should provide
    links to regression ranges in Trac
    
    Reviewed by David Kilzer.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Trac.js: Added.
    (Trac): This new class represents a particular instance of Trac for a single project.
    (Trac.prototype.logURL): Returns the URL that can be used to see the log of the given
    repository path between the two specified revisions
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ViewController.js:
    (ViewController): Added new trac argument.
    (ViewController.prototype._displayBuilder): Moved code to create the DOM for the
    passed/failed builds from here...
    (ViewController.prototype._domForRegressionRange): ...to here. Now also includes a link to
    Trac if there are multiple suspect revisions.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/index.html: Pull in
    Trac.js and pass a Trac instance for trac.webkit.org to the ViewController.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ViewController.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ViewController.js
@@ -24,9 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-function ViewController(buildbot, bugzilla) {
+function ViewController(buildbot, bugzilla, trac) {
     this._buildbot = buildbot;
     this._bugzilla = bugzilla;
+    this._trac = trac;
 
     var self = this;
     addEventListener('load', function() { self.loaded() }, false);
@@ -80,12 +81,7 @@ ViewController.prototype = {
                 if (buildIndex + 1 < buildNameArray.length)
                     passingBuildName = buildNameArray[buildIndex + 1];
 
-                var dlItems = [
-                    [document.createTextNode('Failed'), self._domForBuildName(builder, buildName)],
-                ];
-                if (passingBuildName)
-                    dlItems.push([document.createTextNode('Passed'), self._domForBuildName(builder, buildNameArray[buildIndex + 1])]);
-                item.appendChild(createDefinitionList(dlItems));
+                item.appendChild(self._domForRegressionRange(builder, passingBuildName, buildName));
 
                 if (passingBuildName || !stillFetchingData)
                     item.appendChild(self._domForNewAndExistingBugs(builder, buildName, passingBuildName, failingTestNames));
@@ -144,6 +140,34 @@ ViewController.prototype = {
             document.title = 'Testers';
             document.body.appendChild(list);
         });
+    },
+
+    _domForRegressionRange: function(builder, passingBuildName, failingBuildName) {
+        var result = document.createDocumentFragment();
+
+        var dlItems = [
+            [document.createTextNode('Failed'), this._domForBuildName(builder, failingBuildName)],
+        ];
+        if (passingBuildName)
+            dlItems.push([document.createTextNode('Passed'), this._domForBuildName(builder, passingBuildName)]);
+        result.appendChild(createDefinitionList(dlItems));
+
+        if (!passingBuildName)
+            return result;
+
+        var firstSuspectRevision = this._buildbot.parseBuildName(passingBuildName).revision + 1;
+        var lastSuspectRevision = this._buildbot.parseBuildName(failingBuildName).revision;
+
+        if (firstSuspectRevision === lastSuspectRevision)
+            return result;
+
+        var link = document.createElement('a');
+        result.appendChild(link);
+
+        link.href = this._trac.logURL('trunk', firstSuspectRevision, lastSuspectRevision);
+        link.appendChild(document.createTextNode('View regression range in Trac'));
+
+        return result;
     },
 
     _domForBuildName: function(builder, buildName) {
