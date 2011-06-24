6e85f24377fdf7a963b68d47c92a4c3b773aba38
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-24 22:05:16 +0000

    2011-06-24  Tony Chang  <tony@chromium.org>
            Reviewed by Adam Barth.
    
            Pass Strings by const reference to functions
            https://bugs.webkit.org/show_bug.cgi?id=63341
    
            Note that since Strings hold a RefPtr to StringImpl, passing Strings
            by value isn't horrible, but it does cause ref count churn and using
            const references is more consistent with the rest of the code base.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::addHTTPOriginIfNeeded): Rework the code to not
                change the value of the passed in String.
            * loader/FrameLoader.h:
            * platform/win/ClipboardUtilitiesWin.cpp:
            (WebCore::setFileDescriptorData):
            * platform/win/ClipboardUtilitiesWin.h:
            * storage/DatabaseTracker.h:
            * storage/IDBLevelDBBackingStore.cpp:
            (WebCore::IDBLevelDBBackingStore::IDBLevelDBBackingStore):
            * storage/IDBLevelDBBackingStore.h:
            * storage/IDBSQLiteBackingStore.cpp:
            (WebCore::IDBSQLiteBackingStore::IDBSQLiteBackingStore):
            * storage/IDBSQLiteBackingStore.h:
            * storage/chromium/DatabaseTrackerChromium.cpp:
            (WebCore::DatabaseTracker::getOpenDatabases):
            * svg/SVGPaint.cpp:
            (WebCore::SVGPaint::SVGPaint):
            * svg/SVGPaint.h:
            * svg/animation/SMILTimeContainer.cpp:
            (WebCore::SMILTimeContainer::updateAnimations):
            * svg/animation/SMILTimeContainer.h:
            * websockets/ThreadableWebSocketChannelClientWrapper.cpp:
            (WebCore::ThreadableWebSocketChannelClientWrapper::didReceiveMessageCallback):
            * websockets/ThreadableWebSocketChannelClientWrapper.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89707 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBSQLiteBackingStore.cpp
+++ third_party/WebKit/Source/WebCore/storage/IDBSQLiteBackingStore.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-IDBSQLiteBackingStore::IDBSQLiteBackingStore(String identifier, IDBFactoryBackendImpl* factory)
+IDBSQLiteBackingStore::IDBSQLiteBackingStore(const String& identifier, IDBFactoryBackendImpl* factory)
     : m_identifier(identifier)
     , m_factory(factory)
 {
