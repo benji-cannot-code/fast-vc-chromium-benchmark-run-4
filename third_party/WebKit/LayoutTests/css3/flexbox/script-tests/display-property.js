9eac87c5af817b7a9911b702ec3b55ed56086aa3
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-24 23:39:35 +0000

    2011-06-24  Tony Chang  <tony@chromium.org>
            Reviewed by Ojan Vafai.
    
            add flexbox and inline-flexbox as possible CSS display values
            https://bugs.webkit.org/show_bug.cgi?id=63361
    
            * css3/flexbox/display-property-expected.txt: Added.
            * css3/flexbox/display-property.html: Added.
            * css3/flexbox/script-tests/display-property.js: Added.
            (testFlex):
    2011-06-24  Tony Chang  <tony@chromium.org>
    
            Reviewed by Ojan Vafai.
    
            add flexbox and inline-flexbox as possible CSS display values
            https://bugs.webkit.org/show_bug.cgi?id=63361
    
            Test: css3/flexbox/display-property.html
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseValue):
            * css/CSSValueKeywords.in:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/css3/flexbox/script-tests/display-property.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Tests being able to set the display to -webkit-flexbox and -webkit-inline-flexbox.');
+
+var div = document.createElement('div');
+
+div.style.display = '-webkit-flexbox';
+shouldBeEqualToString('div.style.display', '-webkit-flexbox');
+
+div.style.display = 'inline';
+shouldBeEqualToString('div.style.display', 'inline');
+
+div.style.display = '-webkit-inline-flexbox';
+shouldBeEqualToString('div.style.display', '-webkit-inline-flexbox');
+
+div.style.display = 'junk';
+shouldBeEqualToString('div.style.display', '-webkit-inline-flexbox');
+
+div.style.display = 'block';
+shouldBeEqualToString('div.style.display', 'block');
+
+successfullyParsed = true;
