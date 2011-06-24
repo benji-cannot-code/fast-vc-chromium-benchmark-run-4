c352ab523efb2af320d15996383e3171fac94e4d
abeera@google.com <abeera@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-24 18:57:24 +0000

    Fix to bug 75303 Makes the sun_path an absolute path("/tmp/pipe_name") to allow IPC between processes with different working directories BUG=75303 TEST=NONE
    Review URL: http://codereview.chromium.org/7167017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90400 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_handle.h
+++ ipc/ipc_channel_handle.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,6 +31,9 @@ struct ChannelHandle {
   // Note that serialization for this object is defined in the ParamTraits
   // template specialization in ipc_message_utils.h.
   ChannelHandle() {}
+  // The name that is passed in should be an absolute path for Posix.
+  // Otherwise there may be a problem in IPC communication between
+  // processes with different working directories.
   ChannelHandle(const std::string& n) : name(n) {}
   ChannelHandle(const char* n) : name(n) {}
 #if defined(OS_POSIX)
