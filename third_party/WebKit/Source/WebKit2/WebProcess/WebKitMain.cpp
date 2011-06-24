dbe8e8567365f74daa25cf2a40044b70d18bbc48
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-24 18:09:14 +0000

    Make the web process pause dialog look better on Vista/7 and robust against executable renames
    Fixes <http://webkit.org/b/63335> Web process pause dialog looks bad on Vista/7
    
    Reviewed by Brian Weinstein.
    
    * WebProcess/WebKitMain.cpp:
    (pauseProcessIfNeeded): Moved code to show the pause dialog here from WebKitMain. We now
    fetch the executable name using ::GetModuleFileNameW instead of hard-coding it. The string
    we pass to ::MessageBoxW no longer has embedded newlines, which improves its appearance on
    Vista/7 (which were doing their own wrapping in addition to our newlines). Appearance is
    different but fine on XP. Note that the message text now contains the "[_debug].exe" suffix when
    referring to the process; that is different from before but seems fine.
    (WebKitMain): Call the new function.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89685 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebKitMain.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebKitMain.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginProcessMain.h"
 #include "ProcessLauncher.h"
 #include "WebProcessMain.h"
+#include <shlwapi.h>
 #include <wtf/text/CString.h>
 
 #if PLATFORM(MAC)
@@ -74,12 +75,6 @@ int WebKitMain(int argc, char** argv)
 }
 
 #elif PLATFORM(WIN)
-
-#ifndef DEBUG_ALL
-#define PROCESS_NAME L"WebKit2WebKitProcess.exe"
-#else
-#define PROCESS_NAME L"WebKit2WebProcess_debug.exe"
-#endif
 
 static void enableDataExecutionPrevention()
 {
@@ -149,15 +144,34 @@ static void disableUserModeCallbackExceptionFilter()
     FreeLibrary(lib);
 }
 
+#ifndef NDEBUG
+static void pauseProcessIfNeeded(HMODULE module)
+{
+    // Show an alert when Ctrl-Alt-Shift is held down during launch to give the user time to attach a
+    // debugger. This is useful for debugging problems that happen early in the web process's lifetime.
+    const unsigned short highBitMaskShort = 0x8000;
+    if (!getenv("WEBKIT2_PAUSE_WEB_PROCESS_ON_LAUNCH") && !(::GetKeyState(VK_CONTROL) & highBitMaskShort) && (::GetKeyState(VK_MENU) & highBitMaskShort) && (::GetKeyState(VK_SHIFT) & highBitMaskShort))
+        return;
+
+    wchar_t path[MAX_PATH];
+    DWORD length = ::GetModuleFileNameW(module, path, WTF_ARRAY_LENGTH(path));
+    if (!length || length == WTF_ARRAY_LENGTH(path))
+        return;
+
+    wchar_t* startOfFilename = ::PathFindFileNameW(path);
+    String filenameString(startOfFilename, length - (startOfFilename - path));
+
+    String message = L"You can now attach a debugger to " + filenameString + L". You can use the same debugger for " + filenameString + L" and the UI process, if desired. Click OK when you are ready for " + filenameString + L" to continue.";
+    String title = filenameString + L" has launched";
+    ::MessageBoxW(0, message.charactersWithNullTermination(), title.charactersWithNullTermination(), MB_OK | MB_ICONINFORMATION);
+}
+#endif
+
 extern "C" __declspec(dllexport) 
 int WebKitMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpstrCmdLine, int nCmdShow)
 {
 #ifndef NDEBUG
-    // Show an alert when Ctrl-Alt-Shift is held down during launch to give the user time to attach a
-    // debugger. This is useful for debugging problems that happen early in the web process's lifetime.
-    const unsigned short highBitMaskShort = 0x8000;
-    if (getenv("WEBKIT2_PAUSE_WEB_PROCESS_ON_LAUNCH") || (::GetKeyState(VK_CONTROL) & highBitMaskShort) && (::GetKeyState(VK_MENU) & highBitMaskShort) && (::GetKeyState(VK_SHIFT) & highBitMaskShort))
-        ::MessageBoxW(0, L"You can now attach a debugger to " PROCESS_NAME L". You can use\nthe same debugger for WebKit2WebProcessand the UI process, if desired.\nClick OK when you are ready for WebKit2WebProcess to continue.", L"WebKit2WebProcess has launched", MB_OK | MB_ICONINFORMATION);
+    pauseProcessIfNeeded(hInstance);
 #endif
 
     enableDataExecutionPrevention();
