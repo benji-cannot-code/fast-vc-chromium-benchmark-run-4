5640945c5278e697abc073d74ee1945af55c08f7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-24 01:14:49 +0000

    add some metrics for the NTP
    added action counts for:
    - whenever a most visited thumbnail is reordered
    - whenever a most visited thumbnail is pinned or unpinned
    - whenever a most visited thumbnail is blacklisted (un-blacklisting was already being tracked)
    
    added histograms for:
    - recently closed entries being restored (histogram tracks display index (i.e. recency) of restored tab)
    - most visited thumbnail (histogram tracks display index). This was previously tracked by an action count (which didn't appear in chromeactions.txt so I really doubt anyone cared about it)
    
    BUG=86177
    TEST=none
    
    Review URL: http://codereview.chromium.org/7194003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90319 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp/most_visited.js
+++ chrome/browser/resources/ntp/most_visited.js
@@ -103,6 +103,8 @@ var MostVisited = (function() {
       }
       this.data[destinationIndex] = sourceData;
       this.data[sourceIndex] = destinationData;
+
+      chrome.send('recordAction', ['MostVisitedReordered']);
     },
 
     updateSettingsLink: function(hasBlacklistedUrls) {
@@ -643,7 +645,8 @@ var MostVisited = (function() {
           var index = Array.prototype.indexOf.call(item.parentNode.children,
                                                    item);
           if (index != -1)
-            chrome.send('metrics', ['NTP_MostVisited' + index]);
+            chrome.send('recordInHistogram',
+                        ['NewTabPage.MostVisited', index, 8]);
         }
       }
     },
