f9d8e1caf6f16e497f216621779dd4d895208a3b
rickcam@chromium.org <rickcam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-24 21:31:30 +0000

    Adds --enable-sync-oauth, enabling INFO logging of oauth_token cookies
    BUG=87404
    TEST=Run with --enable-sync-oauth, grep log for COOKIE_CHANGED
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=90421
    
    Review URL: http://codereview.chromium.org/7198025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90440 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -39,6 +39,7 @@ class NotificationSource;
 class Profile;
 class ProfileSyncFactory;
 class SigninManager;
+struct ChromeCookieDetails;
 
 namespace browser_sync {
 class BackendMigrator;
@@ -208,6 +209,10 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
                                    const std::string& password,
                                    const std::string& captcha,
                                    const std::string& access_code);
+
+  // Called when a cookie, e. g. oauth_token, changes
+  virtual void OnCookieChanged(Profile* profile,
+                               ChromeCookieDetails* cookie_details);
 
   // Update the last auth error and notify observers of error state.
   void UpdateAuthErrorState(const GoogleServiceAuthError& error);
