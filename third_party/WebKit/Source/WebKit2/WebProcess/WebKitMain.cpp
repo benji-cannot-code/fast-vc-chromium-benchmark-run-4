aeecf456b26b4bab31a12781dbcb10f8a40b27b1
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-24 18:47:14 +0000

    Mac build fix after r89685
    * WebProcess/WebKitMain.cpp: Make a Windows-only #include really be Windows-only.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89689 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebKitMain.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebKitMain.cpp
@@ -30,11 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginProcessMain.h"
 #include "ProcessLauncher.h"
 #include "WebProcessMain.h"
-#include <shlwapi.h>
 #include <wtf/text/CString.h>
 
 #if PLATFORM(MAC)
 #include <objc/objc-auto.h>
+#elif PLATFORM(WIN)
+#include <shlwapi.h>
 #endif
 
 using namespace WebKit;
