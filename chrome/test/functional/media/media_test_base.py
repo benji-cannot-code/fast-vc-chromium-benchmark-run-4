4ed7eece75a0db812f51d755e877215bf5b7fe09
imasaki@chromium.org <imasaki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-25 04:32:48 +0000

    Adding basic track(caption) event test in media functional test.
    Review URL: http://codereview.chromium.org/7259006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/media/media_test_base.py
+++ chrome/test/functional/media/media_test_base.py
@@ -14,6 +14,7 @@ media_test_runner.py is used for generating these variables
 """
 
 import csv
+import logging
 import os
 import time
 
@@ -146,6 +147,7 @@ class MediaTestBase(pyauto.PyUITest):
       url = self.url
       if self._test_scenarios:
         url += '&actions=' + self.test_scenarios[run_counter]
+      logging.debug('Navigate to %s', url)
       self.NavigateToURL(url)
       self.WaitUntil(lambda: _VideoEnded(),
                      self.TIMEOUT)
