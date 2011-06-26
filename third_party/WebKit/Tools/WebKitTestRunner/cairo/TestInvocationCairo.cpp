9107f822a7b712d5e2f4a55b806b4952240a692d
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-26 00:39:52 +0000

    Unreviewed build correction after r89426.
    * WebKitTestRunner/Configurations/WebKitTestRunnerCFLite.vsprops:
      Link with Cairo DLL for new functionality.
    * WebKitTestRunner/cairo/TestInvocationCairo.cpp: Include compatibility
      code for snprintf.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/cairo/TestInvocationCairo.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/cairo/TestInvocationCairo.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdio>
 #include <wtf/Assertions.h>
 #include <wtf/MD5.h>
+#include <wtf/StringExtras.h>
 
 namespace WTR {
 
