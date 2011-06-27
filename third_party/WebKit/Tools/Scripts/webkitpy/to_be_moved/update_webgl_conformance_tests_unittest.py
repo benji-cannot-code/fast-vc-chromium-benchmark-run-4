1fcb81ef675f00c3bf870f6551f0aff39624c9b5
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-27 07:20:11 +0000

    2011-06-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Move update_webgl_conformance_tests.py to webkitpy.to_be_moved
            https://bugs.webkit.org/show_bug.cgi?id=63429
    
            This package as nothing to do with the layout_tests package.
    
            * Scripts/update-webgl-conformance-tests:
            * Scripts/webkitpy/layout_tests/update_webgl_conformance_tests.py: Removed.
            * Scripts/webkitpy/layout_tests/update_webgl_conformance_tests_unittest.py: Removed.
            * Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests.py: Copied from Tools/Scripts/webkitpy/layout_tests/update_webgl_conformance_tests.py.
            * Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests_unittest.py: Copied from Tools/Scripts/webkitpy/layout_tests/update_webgl_conformance_tests_unittest.py.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests_unittest.py
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Unit tests for update_webgl_conformance_tests."""
 
 import unittest
-from webkitpy.layout_tests import update_webgl_conformance_tests as webgl
+from webkitpy.to_be_moved import update_webgl_conformance_tests as webgl
 
 
 def construct_script(name):
