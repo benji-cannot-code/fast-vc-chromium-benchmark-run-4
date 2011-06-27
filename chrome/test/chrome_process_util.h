fb37f169c187d8ce7c780a2b16cb86af16676b83
jrt@chromium.org <jrt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-27 16:47:45 +0000

    Chromium-branding bug fix and test re-enable.
    After revision 90141, the WorkerTest.TerminateQueuedWorkers failed on mac
    ui_tests. The test was disabled in revision 90152. Now the bug is resolved,
    the test is re-enabled and passes on mac ui_tests.
    
    BUG=87204
    
    Review URL: http://codereview.chromium.org/7240014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90593 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chrome_process_util.h
+++ chrome/test/chrome_process_util.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef std::vector<base::ProcessId> ChromeProcessList;
 
 // Returns the executable name of the current Chrome browser process.
-const FilePath::CharType* GetRunningExecutableName();
+const FilePath::CharType* GetRunningBrowserExecutableName();
+
+// Returns the executable name of the current Chrome helper process.
+const FilePath::CharType* GetRunningHelperExecutableName();
 
 // Returns a vector of PIDs of all chrome processes (main and renderers etc)
 // based on |browser_pid|, the PID of the main browser process.
