c1565976add9ea794d694d02ba534f04359e245b
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-27 11:33:22 +0000

    Add migration code to migrate the obsolete kContentSettingsPatterns pref.
    BUG=86751
    TEST=content_settings_pref_provider_unittest.cc,
              host_content_settings_map_unittest.cc
    
    
    Review URL: http://codereview.chromium.org/7190034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/cookies.py
+++ chrome/test/functional/cookies.py
@@ -120,7 +120,7 @@ class CookiesTest(pyauto.PyUITest):
     self.assertFalse(self.GetCookie(pyauto.GURL(file_url)))
 
     # Creating an exception to allow cookies from http://www.google.com.
-    self.SetPrefs(pyauto.kContentSettingsPatterns,
+    self.SetPrefs(pyauto.kContentSettingsPatternPairs,
                  {'[*.]google.com,*': { 'cookies': 1}})
     # Navigate to google.com and check if cookies are set.
     self.NavigateToURL('http://www.google.com')
@@ -134,7 +134,7 @@ class CookiesTest(pyauto.PyUITest):
     self.assertFalse(self.GetCookie(pyauto.GURL(file_url)))
 
     # Create an exception to block cookies from http://www.google.com
-    self.SetPrefs(pyauto.kContentSettingsPatterns,
+    self.SetPrefs(pyauto.kContentSettingsPatternPairs,
                  {'[*.]google.com,*': { 'cookies': 2}})
 
     # Navigate to google.com and check if cookies are blocked.
@@ -160,7 +160,7 @@ class CookiesTest(pyauto.PyUITest):
     self.assertFalse(self.GetCookie(pyauto.GURL(file_url)))
 
     # Creating an exception to allow cookies for a session for google.com.
-    self.SetPrefs(pyauto.kContentSettingsPatterns,
+    self.SetPrefs(pyauto.kContentSettingsPatternPairs,
                  {'[*.]google.com,*': { 'cookies': 4}})
 
     # Navigate to google.com and check if cookies are set.
