0607156fa4aecb7f343b89c3c8ae4e3170d6fc80
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-27 08:07:18 +0000

    2011-06-27  Kent Tamura  <tkent@chromium.org>
            Reviewed by Hajime Morita.
    
            [Chromium] WebViewHost should be destructed before LayoutTestController, etc.
            https://bugs.webkit.org/show_bug.cgi?id=63427
    
            ~WebViewHost() navigates to about:blank, so it calls some
            callbacks and LayoutTestController functions.
    
            * DumpRenderTree/chromium/TestShell.h:
             Move m_printer upper and move m_webViewHost lower.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
@@ -194,7 +194,9 @@ private:
     WebKit::WebWidget* m_focusedWidget;
     bool m_testShellMode;
     WebViewHost* m_devTools;
-    OwnPtr<WebViewHost> m_webViewHost;
+
+    // Be careful of the destruction order of the following objects.
+    OwnPtr<TestEventPrinter> m_printer;
     OwnPtr<WebPermissions> m_webPermissions;
     OwnPtr<DRTDevToolsAgent> m_drtDevToolsAgent;
     OwnPtr<DRTDevToolsClient> m_drtDevToolsClient;
@@ -204,7 +206,8 @@ private:
     OwnPtr<PlainTextController> m_plainTextController;
     OwnPtr<TextInputController> m_textInputController;
     OwnPtr<NotificationPresenter> m_notificationPresenter;
-    OwnPtr<TestEventPrinter> m_printer;
+    OwnPtr<WebViewHost> m_webViewHost;
+
     TestParams m_params;
     int m_timeout; // timeout value in millisecond
     bool m_allowExternalPages;
