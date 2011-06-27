bcf9120a1da57bdab78108772f6b6d2da73d82ad
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-27 11:50:29 +0000

    WebSocket over SPDY: SpdyWebSocketStream
    SpdyWebSocketStream implementation.
    This class will be used by WebSocketJob to enable WebSocket over SPDY support.
    
    BUG=42320
    TEST=net_unittest --gtest_filter=SpdyWebSocket\*
    
    Review URL: http://codereview.chromium.org/7089001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90555 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_framer.h
+++ net/spdy/spdy_framer.h
@@ -30,6 +30,7 @@ class SpdyNetworkTransactionTest;
 class SpdyProxyClientSocketTest;
 class SpdySessionTest;
 class SpdyStreamTest;
+class SpdyWebSocketStreamTest;
 }
 
 namespace spdy {
@@ -269,6 +270,7 @@ class NET_TEST SpdyFramer {
   friend class net::SpdyProxyClientSocketTest;
   friend class net::SpdySessionTest;
   friend class net::SpdyStreamTest;
+  friend class net::SpdyWebSocketStreamTest;
   friend class test::TestSpdyVisitor;
   friend void test::FramerSetEnableCompressionHelper(SpdyFramer* framer,
                                                      bool compress);
