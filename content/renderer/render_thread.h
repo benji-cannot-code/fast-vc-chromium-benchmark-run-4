20059976f774528570d0d0e8ebe43718e3bf1156
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-27 17:42:11 +0000

    move VideoCaptureImplManager from singleton to a member of RenderThread. fix some comments. BUG=none TEST=try bots
    Review URL: http://codereview.chromium.org/7265004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90605 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread.h
+++ content/renderer/render_thread.h
@@ -33,6 +33,7 @@ class RenderProcessObserver;
 class RendererNetPredictor;
 class RendererWebKitClientImpl;
 class SkBitmap;
+class VideoCaptureImplManager;
 class WebDatabaseObserverImpl;
 
 struct RendererPreferences;
@@ -165,6 +166,10 @@ class RenderThread : public RenderThreadBase,
     return indexed_db_dispatcher_.get();
   }
 
+  VideoCaptureImplManager* video_capture_impl_manager() const {
+    return vc_manager_.get();
+  }
+
   bool plugin_refresh_allowed() const { return plugin_refresh_allowed_; }
 
   double idle_notification_delay_in_s() const {
@@ -247,6 +252,9 @@ class RenderThread : public RenderThreadBase,
 
   // Used on the renderer and IPC threads.
   scoped_refptr<DBMessageFilter> db_message_filter_;
+
+  // Used on multiple threads.
+  scoped_refptr<VideoCaptureImplManager> vc_manager_;
 
   // Used on multiple script execution context threads.
   scoped_ptr<WebDatabaseObserverImpl> web_database_observer_impl_;
