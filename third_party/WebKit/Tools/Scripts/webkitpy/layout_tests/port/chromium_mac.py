4322da47f0dd74be18027edc54d7335041f0af4c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-27 07:54:12 +0000

    2011-06-27  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Remove Chromium and ChromiumMac implementations of driver_name now that everything uses DumpRenderTree
            https://bugs.webkit.org/show_bug.cgi?id=63428
    
            * Scripts/webkitpy/layout_tests/port/chromium.py:
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89797 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
@@ -102,9 +102,6 @@ class ChromiumMacPort(chromium.ChromiumPort):
             return 1
         return chromium.ChromiumPort.default_child_processes(self)
 
-    def driver_name(self):
-        return "DumpRenderTree"
-
     #
     # PROTECTED METHODS
     #
