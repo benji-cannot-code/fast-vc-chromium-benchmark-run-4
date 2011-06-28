f1ed5dc70721f6b85533e0b99c0d7ea4ce0d23c5
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 14:06:34 +0000

    Remove Profiles from JavaScript dialogs.
    BUG=84601
    TEST=no change
    
    Review URL: http://codereview.chromium.org/7253019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_host.cc
+++ chrome/browser/extensions/extension_host.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
@@ -48,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/notification_service.h"
 #include "content/common/view_messages.h"
 #include "grit/browser_resources.h"
+#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -433,15 +435,22 @@ void ExtensionHost::RunJavaScriptMessage(const RenderViewHost* rvh,
                                          IPC::Message* reply_msg,
                                          bool* did_suppress_message) {
   bool suppress_this_message = false;
+
+  string16 title;
+  if (extension_->location() == Extension::COMPONENT)
+    title = l10n_util::GetStringUTF16(IDS_PRODUCT_NAME);
+  else
+    title = UTF8ToUTF16(extension_->name());
+
   GetJavaScriptDialogCreatorInstance()->RunJavaScriptDialog(
       this,
-      frame_url,
+      content::JavaScriptDialogCreator::DIALOG_TITLE_PLAIN_STRING,
+      title,
       flags,
       message,
       default_prompt,
       reply_msg,
-      &suppress_this_message,
-      profile());
+      &suppress_this_message);
 
   if (suppress_this_message) {
     // If we are suppressing messages, just reply as if the user immediately
--- chrome/browser/ui/app_modal_dialogs/message_box_handler.h
+++ chrome/browser/ui/app_modal_dialogs/message_box_handler.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 namespace content {
-  class JavaScriptDialogCreator;
+class JavaScriptDialogCreator;
 }
 
 // Returns a JavaScriptDialogCreator that creates real dialogs.
