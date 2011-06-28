ebe77cd09014dbd3589a618a0ee5af7a4ae5667e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 18:35:32 +0000

    Remove unused app/sql/connection.h include.
    This was introduced in commit 3b3ee61c, but it seems the code was refactored and
    the test that used to use the sql API does not exists anymore.
    
    BUG=72317
    TEST=test_support_ui
    
    R=tony@chromium.org
    TBR=phajdan.jr@chromium.org
    
    Review URL: http://codereview.chromium.org/7277028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90806 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test.cc
+++ chrome/test/ui/ui_test.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
-#include "app/sql/connection.h"
 #include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
