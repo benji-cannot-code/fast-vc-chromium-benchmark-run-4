f5ec17f3abf9dac57cfea242881bb2c4a6a4c565
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 09:26:07 +0000

    2011-06-28  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: console group boundaries should be more distinct in UI
            https://bugs.webkit.org/show_bug.cgi?id=63455
    
            Show grey bracket to on the left side of a console group message to highlight
            the group extent.
    
            * inspector/front-end/ConsoleView.js:
            (WebInspector.ConsoleGroup):
            * inspector/front-end/inspector.css:
            (#console-prompt):
            (.console-message, .console-user-command):
            (.console-message::before, .console-user-command::before, #console-prompt::before, .console-group-title::before):
            (.console-group-title::before):
            (.console-group):
            (.console-group-bracket):
            (.console-group.collapsed > .console-group-bracket):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleView.js
@@ -1213,6 +1213,12 @@ WebInspector.ConsoleGroup = function(parentGroup)
     element.group = this;
     this.element = element;
 
+    if (parentGroup) {
+        var bracketElement = document.createElement("div");
+        bracketElement.className = "console-group-bracket";
+        element.appendChild(bracketElement);
+    }
+
     var messagesElement = document.createElement("div");
     messagesElement.className = "console-group-messages";
     element.appendChild(messagesElement);
