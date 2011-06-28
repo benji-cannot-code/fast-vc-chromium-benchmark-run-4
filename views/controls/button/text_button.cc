a6b33cbc218f7f6dc56dcc531ed6b0f096b2d3a4
motek@chromium.org <motek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 22:21:44 +0000

    Corrects setting default text colors on Windows.
    BUG=86979
    Review URL: http://codereview.chromium.org/7276040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90849 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/text_button.cc
+++ views/controls/button/text_button.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/widget.h"
 
 #if defined(OS_WIN)
+#include "skia/ext/skia_utils_win.h"
 #include "ui/gfx/native_theme_win.h"
 #include "ui/gfx/platform_font_win.h"
 #endif
@@ -786,6 +787,13 @@ NativeTextButton::NativeTextButton(ButtonListener* listener,
 }
 
 void NativeTextButton::Init() {
+#if defined(OS_WIN)
+  // Windows will like to show its own colors.
+  // Halos and such are ignored as they are always set by specific calls.
+  color_enabled_ = skia::COLORREFToSkColor(GetSysColor(COLOR_BTNTEXT));
+  color_disabled_ = skia::COLORREFToSkColor(GetSysColor(COLOR_GRAYTEXT));
+  color_hover_ = color_ = color_enabled_;
+#endif
   set_border(new TextButtonNativeThemeBorder(this));
   set_ignore_minimum_size(false);
   set_alignment(ALIGN_CENTER);
