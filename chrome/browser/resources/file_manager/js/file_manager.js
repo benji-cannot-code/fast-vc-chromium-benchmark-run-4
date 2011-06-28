6550110bb8564017d5df5f0dd8b38e4fd10d225a
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 21:02:42 +0000

    CrOS - Add test for file picker "Save As"
    + Also fixed problem where "Save" button doesn't enable when dialog is opened into an empty directory.
    + Added another chrome.test.sendMessage() so I can tell when the dialog is stabilized.
    + Pulled common test init code into SetUp() and TearDown().
    
    BUG=chromium-os:16999
    TEST=FileManagerDialogTest.SelectFileAndSave
    
    Review URL: http://codereview.chromium.org/7230051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90837 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -855,6 +855,8 @@ FileManager.prototype = {
       }
 
       function onLeafError(err) {
+        // Set filename first so OK button will update in changeDirectoryEntry.
+        self.filenameInput_.value = leafName;
         if (err = FileError.NOT_FOUND_ERR) {
           // Leaf does not exist, it's just a suggested file name.
           self.changeDirectoryEntry(baseDirEntry, CD_NO_HISTORY);
@@ -862,16 +864,16 @@ FileManager.prototype = {
           console.log('Unexpected error resolving default leaf: ' + err);
           self.changeDirectoryEntry('/', CD_NO_HISTORY);
         }
-        self.filenameInput_.value = leafName;
       }
 
       self.resolvePath(self.defaultPath_, onLeafFound, onLeafError);
     }
 
     function onBaseError(err) {
+      // Set filename first so OK button will update in changeDirectory.
+      self.filenameInput_.value = leafName;
       console.log('Unexpected error resolving default base: ' + err);
       self.changeDirectory('/', CD_NO_HISTORY);
-      self.filenameInput_.value = leafName;
     }
 
     this.filesystem_.root.getDirectory(
@@ -1809,6 +1811,9 @@ FileManager.prototype = {
     this.rescanDirectory_(function() {
         if (event.selectedEntry)
           self.selectEntry(event.selectedEntry);
+        // For tests that open the dialog to empty directories, everything
+        // is loaded at this point.
+        chrome.test.sendMessage('directory-change-complete');
       });
   };
 
