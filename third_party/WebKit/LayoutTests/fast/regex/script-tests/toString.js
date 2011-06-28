3432f91ee4442b02db8deb5d872ead5371ccd47d
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 05:55:45 +0000

    Source/JavaScriptCore: Build fix attempt after r89885.
    Patch by Ryosuke Niwa <rniwa@webkit.org> on 2011-06-27
    * JavaScriptCore.exp:
    * jsc.cpp:
    
    LayoutTests: https://bugs.webkit.org/show_bug.cgi?id=50554
    RegExp.prototype.toString does not escape slashes
    
    Reviewed by Darin Adler & Oliver Hunt.
    
    The problem here is that we don't escape forwards slashes when converting
    a RegExp to a string. This means that RegExp("/").toString() is "///",
    which is not a valid RegExp literal. Also, we return an invalid literal
    for RegExp.prototype.toString() ("//", which is an empty single-line comment).
    
    From ES5:
    "NOTE: The returned String has the form of a RegularExpressionLiteral that
    evaluates to another RegExp object with the same behaviour as this object."
    
    Added test cases.
    
    * fast/regex/script-tests/toString.js: Added.
    (testFwdSlash):
    * fast/regex/toString-expected.txt: Added.
    * fast/regex/toString.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89895 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/regex/script-tests/toString.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("This page tests toString conversion of RegExp objects, particularly wrt to '/' characters and RegExp.prototype.");
+
+function testForwardSlash(pattern, _string)
+{
+    string = _string;
+
+    re1 = new RegExp(pattern);
+    re2 = eval(re1.toString());
+
+    return re1.test(string) && re2.test(string);
+}
+
+shouldBe("RegExp('/').source", '"\\\\/"');
+shouldBe("RegExp('').source", '""');
+shouldBe("RegExp.prototype.source", '""');
+
+shouldBe("RegExp('/').toString()", '"/\\\\//"');
+shouldBe("RegExp('').toString()", '"/(?:)/"');
+shouldBe("RegExp.prototype.toString()", '"/(?:)/"');
+
+// These strings are equivalent, since the '\' is identity escaping the '/' at the string level.
+shouldBeTrue('testForwardSlash("^/$", "/");');
+shouldBeTrue('testForwardSlash("^\/$", "/");');
+// This string passes "^\/$" to the RegExp, so the '/' is escaped in the re!
+shouldBeTrue('testForwardSlash("^\\/$", "/");');
+// These strings pass "^\\/$" and "^\\\/$" respectively to the RegExp, giving one '\' to match.
+shouldBeTrue('testForwardSlash("^\\\\/$", "\\/");');
+shouldBeTrue('testForwardSlash("^\\\\\\/$", "\\/");');
+// These strings match two backslashes (the second with the '/' escaped).
+shouldBeTrue('testForwardSlash("^\\\\\\\\/$", "\\\\/");');
+shouldBeTrue('testForwardSlash("^\\\\\\\\\\/$", "\\\\/");');
+
+var successfullyParsed = true;
+
