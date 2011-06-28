6e760510a6c8e274e7d4f198f64a243d92fb3e99
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 18:35:37 +0000

    https://bugs.webkit.org/show_bug.cgi?id=55040 RegExp constructor returns the argument regexp instead of a new object
    Reviewed by Oliver Hunt.
    
    Per 15.10.3.1, our current behaviour is correct if called as a function,
    but incorrect when called as a constructor.
    
    Source/JavaScriptCore: 
    
    * runtime/RegExpConstructor.cpp:
    (JSC::constructRegExp):
    (JSC::constructWithRegExpConstructor):
    * runtime/RegExpConstructor.h:
    
    LayoutTests: 
    
    * fast/regex/constructor-expected.txt: Added.
    * fast/regex/constructor.html: Added.
    * fast/regex/script-tests/constructor.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/RegExpConstructor.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/RegExpConstructor.h
@@ -97,7 +97,7 @@ namespace JSC {
 
     RegExpConstructor* asRegExpConstructor(JSValue);
 
-    JSObject* constructRegExp(ExecState*, JSGlobalObject*, const ArgList&);
+    JSObject* constructRegExp(ExecState*, JSGlobalObject*, const ArgList&, bool callAsConstructor = false);
 
     inline RegExpConstructor* asRegExpConstructor(JSValue value)
     {
