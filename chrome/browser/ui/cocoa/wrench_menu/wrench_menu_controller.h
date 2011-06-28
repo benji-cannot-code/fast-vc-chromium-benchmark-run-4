894edc19145c342d6a9207085d35e9d34f3480aa
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 22:18:29 +0000

    Fix Mac bookmark bridge crash. Sharing a BookmarkMenuBridge between different bookmark menus is bad. Giving each Wrench menu its own private BookmarkMenuBridge keeps the bridge's state coherent, and also enables it to avoid unnecessary updates. R=shess@chromium.org BUG=87193 Review URL: http://codereview.chromium.org/7230056
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90846 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/wrench_menu/wrench_menu_controller.h
+++ chrome/browser/ui/cocoa/wrench_menu/wrench_menu_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/cocoa_protocols.h"
 #include "base/memory/scoped_ptr.h"
+#import "chrome/browser/ui/cocoa/bookmarks/bookmark_menu_bridge.h"
 #import "chrome/browser/ui/cocoa/menu_controller.h"
 
 @class MenuTrackedRootView;
@@ -41,6 +42,7 @@ class ZoomLevelObserver;
   IBOutlet NSButton* zoomMinus_;
   IBOutlet NSButton* zoomFullScreen_;
 
+  scoped_ptr<BookmarkMenuBridge> bookmarkMenuBridge_;
   scoped_ptr<WrenchMenuControllerInternal::ZoomLevelObserver> observer_;
 }
 
