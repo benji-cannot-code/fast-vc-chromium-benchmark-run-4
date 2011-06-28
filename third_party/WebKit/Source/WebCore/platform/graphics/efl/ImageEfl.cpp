ed2e3f710d4f0897f148b14abea7a6950871672f
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 05:02:54 +0000

    2011-06-27  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Unreviewed. Fix style error related to wrong include order.
    
            * platform/graphics/efl/ImageEfl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/efl/ImageEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/efl/ImageEfl.cpp
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BitmapImage.h"
 #include "SharedBuffer.h"
 
-#include <wtf/text/StringConcatenate.h>
 #include <cairo.h>
+#include <wtf/text/StringConcatenate.h>
 
 namespace WebCore {
 
