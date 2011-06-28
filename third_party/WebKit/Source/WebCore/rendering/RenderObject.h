6e36d91767d46a081500524432ab88d8c1e00380
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 22:41:36 +0000

    2011-06-28  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch paintFillLayer* to new layout types
            https://bugs.webkit.org/show_bug.cgi?id=63570
    
            Switching painFillLayer* to layout type abstraction from more integral types.
    
            No new tests as this is just moving to an abstraction.
    
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::paintFillLayers):
            (WebCore::InlineFlowBox::paintFillLayer):
            * rendering/InlineFlowBox.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::paintFillLayers):
            (WebCore::RenderBox::paintFillLayer):
            * rendering/RenderBox.h:
            * rendering/RenderBoxModelObject.cpp:
            (WebCore::RenderBoxModelObject::paintFillLayerExtended):
            * rendering/RenderBoxModelObject.h:
            * rendering/RenderObject.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "Element.h"
 #include "FloatQuad.h"
+#include "LayoutTypes.h"
 #include "PaintPhase.h"
 #include "RenderObjectChildList.h"
 #include "RenderStyle.h"
