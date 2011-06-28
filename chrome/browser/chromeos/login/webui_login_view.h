2dcfca4a7deaad74348b6c569dfa5a86092365fb
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 18:23:26 +0000

    Suppress right-click events in the login page.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7276013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90801 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_view.h
+++ chrome/browser/chromeos/login/webui_login_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/login_html_dialog.h"
 #include "chrome/browser/chromeos/status/status_area_host.h"
+#include "content/browser/tab_contents/tab_contents_delegate.h"
 #include "views/view.h"
 
 class DOMView;
@@ -25,6 +26,7 @@ class StatusAreaView;
 // DOMView.
 class WebUILoginView : public views::View,
                        public StatusAreaHost,
+                       public TabContentsDelegate,
                        public chromeos::LoginHtmlDialog::Delegate {
  public:
   WebUILoginView();
@@ -70,6 +72,27 @@ class WebUILoginView : public views::View,
  protected:
   // Creates and adds the status_area.
   void InitStatusArea();
+
+  // Overridden from TabContentsDelegate.
+  virtual void OpenURLFromTab(TabContents* source,
+                              const GURL& url,
+                              const GURL& referrer,
+                              WindowOpenDisposition disposition,
+                              PageTransition::Type transition) OVERRIDE;
+  virtual void NavigationStateChanged(const TabContents* source,
+                                      unsigned changed_flags) OVERRIDE;
+  virtual void AddNewContents(TabContents* source,
+                              TabContents* new_contents,
+                              WindowOpenDisposition disposition,
+                              const gfx::Rect& initial_pos,
+                              bool user_gesture) OVERRIDE;
+  virtual void ActivateContents(TabContents* contents) OVERRIDE;
+  virtual void DeactivateContents(TabContents* contents) OVERRIDE;
+  virtual void LoadingStateChanged(TabContents* source) OVERRIDE;
+  virtual void CloseContents(TabContents* source) OVERRIDE;
+  virtual void MoveContents(TabContents* source, const gfx::Rect& pos) OVERRIDE;
+  virtual void UpdateTargetURL(TabContents* source, const GURL& url) OVERRIDE;
+  virtual bool HandleContextMenu(const ContextMenuParams& params) OVERRIDE;
 
   Profile* profile_;
   StatusAreaView* status_area_;
