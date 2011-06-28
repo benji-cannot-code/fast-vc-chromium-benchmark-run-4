48a0cda10b83d7857569005afcb87bee1383ab87
yutak@chromium.org <yutak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 05:39:07 +0000

    2011-06-27  Yuta Kitamura  <yutak@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            [WebKit2] Add hixie76WebSocketProtocolEnabled flag to WebPreferences
            https://bugs.webkit.org/show_bug.cgi?id=63447
    
            Make a new flag added in WebCore::Settings (useHixie76WebSocketProtocol)
            switchable from WebPage and WebPreferences.
    
            * Shared/WebPreferencesStore.h:
            * UIProcess/API/C/WKPreferences.cpp:
            (WKPreferencesSetHixie76WebSocketProtocolEnabled):
            (WKPreferencesGetHixie76WebSocketProtocolEnabled):
            * UIProcess/API/C/WKPreferencesPrivate.h:
            * WebProcess/WebPage/WebPage.cpp:
            (WebKit::WebPage::updatePreferences):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKPreferences.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKPreferences.cpp
@@ -562,3 +562,13 @@ bool WKPreferencesGetFileAccessFromFileURLsAllowed(WKPreferencesRef preferencesR
 {
     return toImpl(preferencesRef)->allowFileAccessFromFileURLs();
 }
+
+void WKPreferencesSetHixie76WebSocketProtocolEnabled(WKPreferencesRef preferencesRef, bool enabled)
+{
+    toImpl(preferencesRef)->setHixie76WebSocketProtocolEnabled(enabled);
+}
+
+bool WKPreferencesGetHixie76WebSocketProtocolEnabled(WKPreferencesRef preferencesRef)
+{
+    return toImpl(preferencesRef)->hixie76WebSocketProtocolEnabled();
+}
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -1610,6 +1610,10 @@ void WebPage::updatePreferences(const WebPreferencesStore& store)
     settings->setAVFoundationEnabled(store.getBoolValueForKey(WebPreferencesKey::isAVFoundationEnabledKey()));
 #endif
 
+#if ENABLE(WEB_SOCKETS)
+    settings->setUseHixie76WebSocketProtocol(store.getBoolValueForKey(WebPreferencesKey::hixie76WebSocketProtocolEnabledKey()));
+#endif
+
     platformPreferencesDidChange(store);
 }
 
