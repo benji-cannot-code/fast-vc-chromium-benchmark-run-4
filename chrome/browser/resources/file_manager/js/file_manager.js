623d622f22b408143dc578b8db63dfbbc7a3e53c
sidor@chromium.org <sidor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 01:13:58 +0000

    Check for nonempty file name on creation The message notifying user about empty file name was created. The comment in mock_chrome.js was updated with up-to-date filenames. Nonempty file name notification + comment fix
    BUG=chromium-os:16059
    TEST=Try to create the file with empty filename field. The error message should be displayed.
    
    
    Review URL: http://codereview.chromium.org/7253023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2086,8 +2086,12 @@ FileManager.prototype = {
 
     while (1) {
       name = window.prompt(str('NEW_FOLDER_PROMPT'), name);
-      if (!name)
+      if (name == '') {
+        window.alert(str('ERROR_NEW_FOLDER_EMPTY_NAME'));
+        continue;
+      } else if (!name) {
         return;
+      }
 
       if (this.validateFileName_(name, true)) break;
     }
