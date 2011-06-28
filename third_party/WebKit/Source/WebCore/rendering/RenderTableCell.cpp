bb69ed6eed5f6cd5e5b001cec291567fe0201b6c
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 23:36:28 +0000

    2011-06-28  Levi Weintraub  <leviw@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch PaintMask* to new layout types
            https://bugs.webkit.org/show_bug.cgi?id=63576
    
            Switching paintMask* to layout type abstraction from more integral types.
    
            No new tests as this is just moving to an abstraction.
    
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::paintMask):
            * rendering/InlineFlowBox.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::paintMask):
            (WebCore::RenderBox::paintMaskImages):
            * rendering/RenderBox.h:
            * rendering/RenderFieldset.cpp:
            (WebCore::RenderFieldset::paintMask):
            * rendering/RenderFieldset.h:
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::paintMask):
            * rendering/RenderTable.h:
            * rendering/RenderTableCell.cpp:
            (WebCore::RenderTableCell::paintMask):
            * rendering/RenderTableCell.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTableCell.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTableCell.cpp
@@ -1020,7 +1020,7 @@ void RenderTableCell::paintBoxDecorations(PaintInfo& paintInfo, const IntPoint& 
     paintBorder(paintInfo.context, paintRect, style());
 }
 
-void RenderTableCell::paintMask(PaintInfo& paintInfo, const IntPoint& paintOffset)
+void RenderTableCell::paintMask(PaintInfo& paintInfo, const LayoutPoint& paintOffset)
 {
     if (style()->visibility() != VISIBLE || paintInfo.phase != PaintPhaseMask)
         return;
@@ -1029,7 +1029,7 @@ void RenderTableCell::paintMask(PaintInfo& paintInfo, const IntPoint& paintOffse
     if (!tableElt->collapseBorders() && style()->emptyCells() == HIDE && !firstChild())
         return;
    
-    paintMaskImages(paintInfo, IntRect(paintOffset, size()));
+    paintMaskImages(paintInfo, LayoutRect(paintOffset, size()));
 }
 
 void RenderTableCell::scrollbarsChanged(bool horizontalScrollbarChanged, bool verticalScrollbarChanged)
