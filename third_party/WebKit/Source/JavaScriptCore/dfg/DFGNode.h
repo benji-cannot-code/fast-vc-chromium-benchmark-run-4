a91dfef44f3ef8aebbf56e664c2b2df7caff11eb
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-28 00:34:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=63497 Add DEBUG_WITH_BREAKPOINT support to the DFG JIT.
    Reviewed by Oliver Hunt.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::parseBlock):
    * dfg/DFGNode.h:
    * dfg/DFGNonSpeculativeJIT.cpp:
    (JSC::DFG::NonSpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
@@ -138,6 +138,7 @@ typedef uint32_t ExceptionInfo;
     macro(CompareStrictEq, NodeResultJS) \
     \
     /* Nodes for misc operations. */\
+    macro(Breakpoint, NodeMustGenerate) \
     macro(LogicalNot, NodeResultJS) \
     \
     /* Block terminals. */\
