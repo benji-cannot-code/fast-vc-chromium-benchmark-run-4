d508de4d2d978838814060b1cebf9dd7a963655d
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-29 19:40:51 +0000

    chromeos: Add Alt-, and Alt-. resize shortcuts to overlay.
    This adds two new window-resizing accelerators to the
    keyboard overlay.
    
    BUG=chromium-os:13963
    TEST=manual: checked that the accelerators are listed
    
    
    Review URL: http://codereview.chromium.org/7277054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90994 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard_overlay_data.js
+++ chrome/browser/resources/keyboard_overlay_data.js
@@ -16855,8 +16855,10 @@ var keyboardOverlayData = {
   "shortcut": {
     "+<>CTRL": "keyboardOverlayZoomIn",
     "+<>CTRL<>SHIFT": "keyboardOverlayZoomIn",
+    ",<>ALT": "keyboardOverlayResizeLeft",
     "-<>CTRL": "keyboardOverlayZoomOut",
     "-<>CTRL<>SHIFT": "keyboardOverlayZoomOut",
+    ".<>ALT": "keyboardOverlayResizeRight",
     "/<>ALT<>CTRL": "keyboardOverlayViewKeyboardOverlay",
     "/<>ALT<>CTRL<>SHIFT": "keyboardOverlayViewKeyboardOverlay",
     "/<>CTRL": "keyboardOverlayHelp",
