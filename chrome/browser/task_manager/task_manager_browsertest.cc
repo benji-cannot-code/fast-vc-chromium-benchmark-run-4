26975641c0a4a0e7ef83b9676dc8abe5a82a4d76
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-29 15:41:48 +0000

    Disable crashing browser tests on Linux.
    ExtensionCrashRecoveryTest.ShutdownWhileCrashed and
    TaskManagerBrowserTest.KillExtension are crashing intermittently on
    Linux; disabling them (instead of marking them flaky) until this is
    resolved.
    
    BUG=84719
    
    Review URL: http://codereview.chromium.org/7282010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -32,8 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
+// On Linux this is crashing intermittently http://crbug/84719
 // In some environments this test fails about 1/6 http://crbug/84850
-#if defined(TOUCH_UI)
+#if defined(OS_LINUX)
+#define MAYBE_KillExtension DISABLED_KillExtension
+#elif defined(TOUCH_UI)
 #define MAYBE_KillExtension FLAKY_KillExtension
 #else
 #define MAYBE_KillExtension KillExtension
