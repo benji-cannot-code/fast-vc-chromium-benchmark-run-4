17fd788f9d988fe75312c75835de6b5a0f674807
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-29 10:24:32 +0000

    2011-06-29  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Kent Tamura.
    
            [Chromium] [DRT] ImageDiff should compile in VS2010
            https://bugs.webkit.org/show_bug.cgi?id=63614
    
            * DumpRenderTree/chromium/ImageDiff.cpp: Include <iterator> for back_inserter.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/ImageDiff.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/ImageDiff.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/support/webkit_support_gfx.h"
 #include <algorithm>
+#include <iterator>
 #include <stdio.h>
 #include <string.h>
 #include <vector>
