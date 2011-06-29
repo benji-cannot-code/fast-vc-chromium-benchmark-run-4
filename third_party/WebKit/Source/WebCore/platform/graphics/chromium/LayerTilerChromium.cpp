87ebacbac29f2b4a91ae446e7e96c1828867bd8d
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-29 18:30:07 +0000

    2011-06-29  Adrienne Walker  <enne@google.com>
            Reviewed by James Robinson.
    
            [chromium] Divide by zero in TilingData
            https://bugs.webkit.org/show_bug.cgi?id=63305
    
            Add safety checks for empty layers.
    
            * platform/graphics/chromium/LayerTilerChromium.cpp:
            (WebCore::LayerTilerChromium::prepareToUpdate):
            (WebCore::LayerTilerChromium::reset):
            (WebCore::LayerTilerChromium::updateRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerTilerChromium.cpp
@@ -97,6 +97,8 @@ void LayerTilerChromium::reset()
     m_tiles.clear();
     m_unusedTiles.clear();
     m_tilingData.setTotalSize(0, 0);
+    m_paintRect = IntRect();
+    m_updateRect = IntRect();
 }
 
 LayerTilerChromium::Tile* LayerTilerChromium::createTile(int i, int j)
@@ -237,6 +239,11 @@ void LayerTilerChromium::prepareToUpdate(const IntRect& contentRect, LayerTextur
     invalidateTiles(contentRect);
     growLayerToContain(contentRect);
 
+    if (!numTiles()) {
+        m_updateRect = IntRect();
+        return;
+    }
+
     // Create tiles as needed, expanding a dirty rect to contain all
     // the dirty regions currently being drawn.
     IntRect dirtyLayerRect;
@@ -270,7 +277,7 @@ void LayerTilerChromium::prepareToUpdate(const IntRect& contentRect, LayerTextur
 void LayerTilerChromium::updateRect(LayerTextureUpdater* textureUpdater)
 {
     // Painting could cause compositing to get turned off, which may cause the tiler to become invalidated mid-update.
-    if (!m_tilingData.totalSizeX() || !m_tilingData.totalSizeY() || m_updateRect.isEmpty())
+    if (!m_tilingData.totalSizeX() || !m_tilingData.totalSizeY() || m_updateRect.isEmpty() || !numTiles())
         return;
 
     GraphicsContext3D* context = layerRendererContext();
