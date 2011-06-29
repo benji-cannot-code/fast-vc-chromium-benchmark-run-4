668267c49c0efb2196f640abbed77b066dd051d8
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-29 21:28:55 +0000

    WebGL test failing on SL bots: fast/canvas/webgl/data-view-test.html https://bugs.webkit.org/show_bug.cgi?id=63151
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    The fix is to change to throw TypeError in custom JS/V8 bindings.
    
    * bindings/js/JSDataViewCustom.cpp:
    (WebCore::getDataViewMember):
    (WebCore::setDataViewMember):
    * bindings/v8/custom/V8DataViewCustom.cpp:
    (WebCore::V8DataView::constructorCallback):
    (WebCore::V8DataView::getInt8Callback):
    
    LayoutTests:
    
    Updated the expected result and removed the rebaselined files that are not needed.
    
    * fast/canvas/webgl/data-view-test-expected.txt:
    * platform/chromium-mac/fast/canvas/webgl/data-view-test-expected.txt: Removed.
    * platform/chromium-win/fast/canvas/webgl/data-view-test-expected.txt: Removed.
    * platform/mac/fast/canvas/webgl/data-view-test-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8DataViewCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8DataViewCustom.cpp
@@ -40,7 +40,7 @@ v8::Handle<v8::Value> V8DataView::constructorCallback(const v8::Arguments& args)
     INC_STATS("DOM.DataView.Constructor");
 
     if (!args.IsConstructCall())
-        return throwError("DOM object constructor cannot be called as a function", V8Proxy::SyntaxError);
+        return throwError("DOM object constructor cannot be called as a function", V8Proxy::TypeError);
 
     if (args[0]->IsNull() || !V8ArrayBuffer::HasInstance(args[0]))
         return V8Proxy::throwTypeError();
@@ -58,7 +58,7 @@ v8::Handle<v8::Value> V8DataView::getInt8Callback(const v8::Arguments& args)
 {
     INC_STATS("DOM.DataView.getInt8");
     if (args.Length() < 1)
-        return throwError("Not enough arguments", V8Proxy::SyntaxError);
+        return throwError("Not enough arguments", V8Proxy::TypeError);
 
     DataView* imp = V8DataView::toNative(args.Holder());
     ExceptionCode ec = 0;
@@ -75,7 +75,7 @@ v8::Handle<v8::Value> V8DataView::getUint8Callback(const v8::Arguments& args)
 {
     INC_STATS("DOM.DataView.getUint8");
     if (args.Length() < 1)
-        return throwError("Not enough arguments", V8Proxy::SyntaxError);
+        return throwError("Not enough arguments", V8Proxy::TypeError);
 
     DataView* imp = V8DataView::toNative(args.Holder());
     ExceptionCode ec = 0;
@@ -92,7 +92,7 @@ v8::Handle<v8::Value> V8DataView::setInt8Callback(const v8::Arguments& args)
 {
     INC_STATS("DOM.DataView.setInt8");
     if (args.Length() < 2)
-        return throwError("Not enough arguments", V8Proxy::SyntaxError);
+        return throwError("Not enough arguments", V8Proxy::TypeError);
 
     DataView* imp = V8DataView::toNative(args.Holder());
     ExceptionCode ec = 0;
@@ -108,7 +108,7 @@ v8::Handle<v8::Value> V8DataView::setUint8Callback(const v8::Arguments& args)
 {
     INC_STATS("DOM.DataView.setUint8");
     if (args.Length() < 2)
-        return throwError("Not enough arguments", V8Proxy::SyntaxError);
+        return throwError("Not enough arguments", V8Proxy::TypeError);
 
     DataView* imp = V8DataView::toNative(args.Holder());
     ExceptionCode ec = 0;
