bdecc69e803cade989f7e181753af5b560c80896
keishi@google.com <keishi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-29 03:47:16 +0000

    Wrong name when creating new folder in bookmark editor
    BUG=81141
    TEST=manual test
    1. Open bookmark edit sheet (ex. click on star and then the "Edit..." button)
    2. Create new folder
    3. Type in new name (ex. "foo")
    4. Without pressing enter, click the Done button.
    5. The new folder should be named "foo", not "New Folder"
    
    Review URL: http://codereview.chromium.org/7276035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_editor_base_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_editor_base_controller.mm
@@ -240,6 +240,8 @@ class BookmarkEditorBaseControllerBridge : public BookmarkModelObserver {
 }
 
 - (IBAction)ok:(id)sender {
+  NSWindow* window = [self window];
+  [window makeFirstResponder:window];
   // At least one of these two functions should be provided by derived classes.
   BOOL hasWillCommit = [self respondsToSelector:@selector(willCommit)];
   BOOL hasDidCommit = [self respondsToSelector:@selector(didCommit)];
@@ -258,7 +260,7 @@ class BookmarkEditorBaseControllerBridge : public BookmarkModelObserver {
       shouldContinue = [hasDidContinue boolValue];
   }
   if (shouldContinue)
-    [NSApp endSheet:[self window]];
+    [NSApp endSheet:window];
 }
 
 - (IBAction)cancel:(id)sender {
