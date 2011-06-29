2efc5a1efd24516e3ea0f547d68ac0aac431b823
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-29 00:58:45 +0000

    2011-06-28  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Adam Barth.
    
            Remove errant isShadowBoundary on ShadowRoot.
            https://bugs.webkit.org/show_bug.cgi?id=63588
    
            Removing dead code, no tests for that.
    
            * dom/ShadowRoot.h: Removed a function that's no longer used.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@89981 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
@@ -39,7 +39,6 @@ class ShadowRoot : public TreeScope {
 public:
     static PassRefPtr<ShadowRoot> create(Document*);
 
-    virtual bool isShadowBoundary() const { return true; }
     virtual void recalcStyle(StyleChange = NoChange);
 
     ShadowContentElement* activeContentElement();
