215871949512a8483860f1fa5b506c8c8f18f90d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-29 19:46:28 +0000

    2011-06-29  Filip Pizlo  <fpizlo@apple.com>
            Reviewed by Gavin Barraclough.
    
            DFG JIT does not perform get_by_id self list caching.
            https://bugs.webkit.org/show_bug.cgi?id=63605
    
            * bytecode/StructureStubInfo.h:
            * dfg/DFGJITCompiler.cpp:
            (JSC::DFG::JITCompiler::compileFunction):
            * dfg/DFGOperations.cpp:
            * dfg/DFGOperations.h:
            * dfg/DFGRepatch.cpp:
            (JSC::DFG::tryCacheGetByID):
            (JSC::DFG::tryBuildGetByIDList):
            (JSC::DFG::dfgBuildGetByIDList):
            * dfg/DFGRepatch.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGRepatch.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGRepatch.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace JSC { namespace DFG {
 
 void dfgRepatchGetByID(ExecState*, JSValue, const Identifier&, const PropertySlot&, StructureStubInfo&);
+void dfgBuildGetByIDList(ExecState*, JSValue, const Identifier&, const PropertySlot&, StructureStubInfo&);
 void dfgRepatchPutByID(ExecState*, JSValue, const Identifier&, const PutPropertySlot&, StructureStubInfo&, PutKind);
 
 } } // namespace JSC::DFG
