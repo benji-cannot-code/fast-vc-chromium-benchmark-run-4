bd29a8d1ef6c42dd49c14e772e4dad0d8eeb9f1d
mnaganov@chromium.org <mnaganov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 12:10:13 +0000

    2011-06-30  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: [Chromium] Support annotations for arrays in heap profiler.
            https://bugs.webkit.org/show_bug.cgi?id=63690
    
            * inspector/front-end/DetailedHeapshotGridNodes.js:
            (WebInspector.HeapSnapshotGenericObjectNode.prototype.get data):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotGridNodes.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotGridNodes.js
@@ -258,7 +258,10 @@ WebInspector.HeapSnapshotGenericObjectNode.prototype = {
             valueStyle = "null";
             break;
         case "array":
-            value += "[]";
+            if (!value)
+                value = "[]";
+            else
+                value += " []";
             break;
         };
         data["object"] = { valueStyle: valueStyle, value: value + " @" + this.snapshotNodeId };
