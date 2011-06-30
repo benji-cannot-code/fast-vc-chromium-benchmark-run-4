5a9f63162eee7ecfbd0215e2d51316351199d615
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 21:41:58 +0000

    2011-06-30  Juan C. Montemayor  <jmont@apple.com>
            Reviewed by Geoffrey Garen.
    
            Web Inspector fails to display source for eval with syntax error
            https://bugs.webkit.org/show_bug.cgi?id=63583
    
            Added new LayoutTest to check that the correct line number is added to a
            syntax error inside an eval statement.
    
            * fast/js/eval-contained-syntax-error-expected.txt: Added.
            * fast/js/eval-contained-syntax-error.html: Added.
            * fast/js/script-tests/eval-contained-syntax-error.js: Added.
            * platform/chromium/fast/js/eval-contained-syntax-error-expected.txt: Added.
    2011-06-30  Juan C. Montemayor  <jmont@apple.com>
    
            Reviewed by Geoffrey Garen.
    
            Web Inspector fails to display source for eval with syntax error
            https://bugs.webkit.org/show_bug.cgi?id=63583
    
            Web Inspector now displays a link to an eval statement that contains
            a syntax error.
    
            * parser/Parser.h:
            (JSC::isEvalNode):
            (JSC::EvalNode):
            (JSC::Parser::parse):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/parser/Parser.h
+++ third_party/WebKit/Source/JavaScriptCore/parser/Parser.h
@@ -44,6 +44,8 @@ namespace JSC {
     class ProgramNode;
     class UString;
 
+    template <typename T> inline bool isEvalNode() { return false; }
+    template <> inline bool isEvalNode<EvalNode>() { return true; }
     template <typename T> struct ParserArenaData : ParserArenaDeletable { T data; };
 
     class Parser {
@@ -110,6 +112,8 @@ namespace JSC {
             // likely, and we are currently unable to distinguish between the two cases.
             if (isFunctionBodyNode(static_cast<ParsedNode*>(0)))
                 *exception = createStackOverflowError(lexicalGlobalObject);
+            else if (isEvalNode<ParsedNode>())
+                *exception = createSyntaxError(lexicalGlobalObject, errMsg);
             else
                 *exception = addErrorInfo(&lexicalGlobalObject->globalData(), createSyntaxError(lexicalGlobalObject, errMsg), errLine, source);
         }
