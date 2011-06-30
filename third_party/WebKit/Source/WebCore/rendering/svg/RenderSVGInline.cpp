357cde3c85414f7001ad9e2d9874bc9327c9cc68
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 21:51:17 +0000

    2011-06-30  Julien Chaffraix  <jchaffraix@webkit.org>
            Reviewed by Nikolas Zimmermann.
    
            Update SVG position values on SVG DOM updates
            https://bugs.webkit.org/show_bug.cgi?id=62439
    
            * svg/custom/crash-textPath-attributes-expected.txt: Added.
            * svg/custom/crash-textPath-attributes.html: Added.
            * svg/custom/resources/crash-textPath-attributes-iframe.svg: Added.
    2011-06-30  Julien Chaffraix  <jchaffraix@webkit.org>
    
            Reviewed by Nikolas Zimmermann.
    
            Update SVG position values on SVG DOM updates
            https://bugs.webkit.org/show_bug.cgi?id=62439
    
            Test: svg/custom/crash-textPath-attributes.html
    
            * rendering/svg/RenderSVGInline.cpp:
            (WebCore::RenderSVGInline::destroy): Notify our containing RenderSVGText that it needs
            to update its positioning information.
    
            * rendering/svg/SVGInlineFlowBox.cpp:
            (WebCore::SVGInlineFlowBox::calculateBoundaries): Check the type of the InlineBox
            like the rest of the code (fixes an ASSERT_NOT_REACHED in InlineBox::calculateBoudaries).
    
            * rendering/svg/SVGTextLayoutAttributesBuilder.cpp:
            (WebCore::SVGTextLayoutAttributesBuilder::buildLayoutAttributesForTextSubtree): Clear
            our cached layout attributes every time we invalidate them. This avoids keeping stale
            attribute that have a backpointer to a RenderObject.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGInline.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGInline.cpp
@@ -96,6 +96,9 @@ void RenderSVGInline::absoluteQuads(Vector<FloatQuad>& quads)
 
 void RenderSVGInline::destroy()
 {
+    if (RenderSVGText* textRenderer = RenderSVGText::locateRenderSVGTextAncestor(this))
+        textRenderer->setNeedsPositioningValuesUpdate();
+
     SVGResourcesCache::clientDestroyed(this);
     RenderInline::destroy();
 }
