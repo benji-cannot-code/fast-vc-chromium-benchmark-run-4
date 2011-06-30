f5922647397fd5a154ae3a0e5de8f2d6e5adcb60
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 19:34:20 +0000

    2011-06-30  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            Rename should_kill_dump_render_tree to driver_needs_restart in preparation for supporting WebKitTestRunner
            https://bugs.webkit.org/show_bug.cgi?id=63743
    
            * Scripts/webkitpy/layout_tests/layout_package/test_failures.py:
            * Scripts/webkitpy/layout_tests/layout_package/worker.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures.py
@@ -104,8 +104,8 @@ class TestFailure(object):
         """Returns the string/JSON representation of a TestFailure."""
         return cPickle.dumps(self)
 
-    def should_kill_dump_render_tree(self):
-        """Returns True if we should kill DumpRenderTree before the next test."""
+    def driver_needs_restart(self):
+        """Returns True if we should kill DumpRenderTree/WebKitTestRunner before the next test."""
         return False
 
 
@@ -118,7 +118,7 @@ class FailureTimeout(TestFailure):
     def message():
         return "Test timed out"
 
-    def should_kill_dump_render_tree(self):
+    def driver_needs_restart(self):
         return True
 
 
@@ -132,7 +132,7 @@ class FailureCrash(TestFailure):
         # FIXME: This is wrong for WebKit2 (which uses WebKitTestRunner).
         return "DumpRenderTree crashed"
 
-    def should_kill_dump_render_tree(self):
+    def driver_needs_restart(self):
         return True
 
 
