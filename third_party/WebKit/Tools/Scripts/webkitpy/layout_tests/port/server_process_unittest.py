0431587b5405e18426d372e741c906e16448a0ef
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 00:42:39 +0000

    2011-06-29  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            new-run-webkit-tests doesn't support sample-on-timeout
            https://bugs.webkit.org/show_bug.cgi?id=56731
    
            * Scripts/webkitpy/layout_tests/port/server_process.py:
            * Scripts/webkitpy/layout_tests/port/server_process_unittest.py:
            * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/server_process_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/server_process_unittest.py
@@ -27,9 +27,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import sys
 import unittest
 
 from webkitpy.layout_tests.port import server_process
+from webkitpy.common.system.executive_mock import MockExecutive2
+from webkitpy.common.system.outputcapture import OutputCapture
+
+
+def _logging_run_command(args):
+    print args
+
+
+class TrivialMockPort(object):
+    def results_directory(self):
+        return "/mock/results"
 
 
 class MockFile(object):
@@ -73,6 +85,11 @@ class TestServerProcess(unittest.TestCase):
         self.assertEquals(server_process._proc, None)
         self.assertEquals(server_process.broken_pipes, [server_process.stdin])
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_sample_process(self):
+        # Currently, sample-on-timeout only works on Darwin.
+        if sys.platform != "darwin":
+            return
+        server_process = FakeServerProcess(port_obj=TrivialMockPort(), name="test", cmd=["test"], executive=MockExecutive2(run_command_fn=_logging_run_command))
+        server_process._proc = MockProc(server_process)
+        expected_stdout = "['/usr/bin/sample', 1, 10, 10, '-file', '/mock/results/test-1.sample.txt']\n"
+        OutputCapture().assert_outputs(self, server_process._sample, expected_stdout=expected_stdout)
