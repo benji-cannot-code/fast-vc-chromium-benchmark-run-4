32f437a99b0d91e3e6828583c579d837ba812604
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 14:45:21 +0000

    2011-06-30  Alexander Pavlov  <apavlov@chromium.org>
            Reviewed by Kent Tamura.
    
            [Chromium] Autofill suggestions appear in upper left corner after input change
            https://bugs.webkit.org/show_bug.cgi?id=63702
    
            This change fixes a few popup layout issues, some of which have existed for quite a while:
            - the absence of the ChromeClientImpl::screenToWindow() implementation (the method used to always return (0, 0));
            - the confusion of window and screen coordinates passed into the autofill popup client's setWindowRect() method;
            - the use of the current frameRect() width as the target element's width (which was wrong when refreshing
              a popup resized during the initial layout (e.g. made wider than the target element)).
    
            No new tests, as this is a change to platform-specific widget code. The test case is provided in the bug description.
    
            * platform/chromium/PopupMenuChromium.cpp:
            (WebCore::PopupContainer::layoutAndGetRTLOffset): Use original target element width, not frameRect().width(),
            since the latter gets updated if the popup is not the same width as the target element.
            (WebCore::PopupContainer::refresh): Restore only the popup bounds rather than its original rectangle,
            since it sometimes breaks the invalidation region and layout.
    2011-06-30  Alexander Pavlov  <apavlov@chromium.org>
    
            Reviewed by Kent Tamura.
    
            [Chromium] Autofill suggestions appear in upper left corner after input change
            https://bugs.webkit.org/show_bug.cgi?id=63702
    
            * src/ChromeClientImpl.cpp:
            (WebKit::ChromeClientImpl::screenToWindow): Implemented.
            * src/WebViewImpl.cpp:
            (WebKit::WebViewImpl::refreshAutoFillPopup): Pass screen (not window) coordinates into setWindowRect().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90119 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -2362,7 +2362,7 @@ void WebViewImpl::refreshAutoFillPopup()
         WebPopupMenuImpl* popupMenu =
             static_cast<WebPopupMenuImpl*>(m_autoFillPopup->client());
         if (popupMenu)
-            popupMenu->client()->setWindowRect(newBounds);
+            popupMenu->client()->setWindowRect(m_chromeClientImpl.windowToScreen(newBounds));
     }
 }
 
