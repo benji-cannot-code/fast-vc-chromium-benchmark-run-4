704672d5dc33997d971f72d29d048b16f87271b6
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-30 23:49:19 +0000

    Add save package download items to transient history on cros.
    BUG=chromium-os:16431
    TEST=Use right click 'Save As' to save an html file on cros. It should appear in the downloads panel.
    
    Review URL: http://codereview.chromium.org/7212017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/scoped_temp_dir.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/download/download_item.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/download/download_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/webui/active_downloads_ui.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/in_process_browser_test.h"
@@ -48,6 +50,30 @@ class SavePageBrowserTest : public InProcessBrowserTest {
     return *Details<GURL>(observer.details()).ptr();
   }
 
+  void CheckDownloadUI(const FilePath& download_path) {
+#if defined(OS_CHROMEOS)
+    Browser* popup = ActiveDownloadsUI::GetPopup();
+    EXPECT_TRUE(popup);
+    ActiveDownloadsUI* downloads_ui = static_cast<ActiveDownloadsUI*>(
+        popup->GetSelectedTabContents()->web_ui());
+    ASSERT_TRUE(downloads_ui);
+    const ActiveDownloadsUI::DownloadList& downloads =
+        downloads_ui->GetDownloads();
+    EXPECT_EQ(downloads.size(), 1U);
+
+    bool found = false;
+    for (size_t i = 0; i < downloads.size(); ++i) {
+      if (downloads[i]->full_path() == download_path) {
+        found = true;
+        break;
+      }
+    }
+    EXPECT_TRUE(found);
+#else
+    EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+#endif
+  }
+
   // Path to directory containing test data.
   FilePath test_dir_;
 
@@ -71,8 +97,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnly) {
 
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
-  if (browser()->SupportsWindowFeature(Browser::FEATURE_DOWNLOADSHELF))
-    EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+  CheckDownloadUI(full_file_name);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_FALSE(file_util::PathExists(dir));
@@ -100,8 +125,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveViewSourceHTMLOnly) {
 
   EXPECT_EQ(actual_page_url, WaitForSavePackageToFinish());
 
-  if (browser()->SupportsWindowFeature(Browser::FEATURE_DOWNLOADSHELF))
-    EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+  CheckDownloadUI(full_file_name);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_FALSE(file_util::PathExists(dir));
@@ -126,8 +150,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveCompleteHTML) {
 
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
-  if (browser()->SupportsWindowFeature(Browser::FEATURE_DOWNLOADSHELF))
-    EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+  CheckDownloadUI(full_file_name);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_TRUE(file_util::PathExists(dir));
@@ -168,8 +191,7 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, FileNameFromPageTitle) {
 
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
-  if (browser()->SupportsWindowFeature(Browser::FEATURE_DOWNLOADSHELF))
-    EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
+  CheckDownloadUI(full_file_name);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_TRUE(file_util::PathExists(dir));
