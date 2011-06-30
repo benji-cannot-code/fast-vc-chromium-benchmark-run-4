17794a0476ff4b92385566ebbb5c0ac63e38b93e
diegohcg@webkit.org <diegohcg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-06-30 23:09:50 +0000

    2011-06-27  Diego Gonzalez  <diegohcg@webkit.org>
        Reviewed by Antonio Gomes.
    
        [Qt] DRT: Provide LayoutTestController::setDefersLoading and ::goBack support
        https://bugs.webkit.org/show_bug.cgi?id=63489
    
        Unskipped:
            loader/navigation-while-deferring-loads.html
    
        Source/WebKit/qt:
    
        * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
        (DumpRenderTreeSupportQt::setDefersLoading):
        (DumpRenderTreeSupportQt::goBack):
        * WebCoreSupport/DumpRenderTreeSupportQt.h:
    
        Tools:
    
        * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
        (LayoutTestController::goBack):
        (LayoutTestController::setDefersLoading):
        * DumpRenderTree/qt/LayoutTestControllerQt.h:
    
        LayoutTests:
    
        * platform/qt/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90178 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/DumpRenderTreeSupportQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/DumpRenderTreeSupportQt.h
@@ -215,6 +215,9 @@ public:
     static void injectInternalsObject(QWebFrame*);
 
     static void setInteractiveFormValidationEnabled(QWebPage*, bool);
+
+    static void setDefersLoading(QWebPage*, bool flag);
+    static void goBack(QWebPage*);
 };
 
 #endif
--- third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
+++ third_party/WebKit/Tools/DumpRenderTree/qt/LayoutTestControllerQt.h
@@ -147,7 +147,6 @@ public slots:
     void closeWebInspector();
     void evaluateInWebInspector(long callId, const QString& script);
     void removeAllVisitedLinks();
-
     void setMediaType(const QString& type);
     void setFrameFlatteningEnabled(bool enable);
     void setAllowUniversalAccessFromFileURLs(bool enable);
@@ -271,6 +270,8 @@ public slots:
     void syncLocalStorage();
     QString layerTreeAsText();
     void setTextDirection(const QString& directionName);
+    void goBack();
+    void setDefersLoading(bool);
 
 private slots:
     void processWork();
