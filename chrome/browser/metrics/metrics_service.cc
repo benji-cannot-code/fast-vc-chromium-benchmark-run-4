b05bd7e68515672f05171af501dcc4cf94314419
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-30 15:54:57 +0000

    Remove logs from the unsent metrics log as soon as they are made pending
    This prevents a pending log from being duplicated if it is pending during shutdown (because of the code that saves the currently pending log to the unsent list). Since the write of the trimmed list back to the prefs system is still only done post-upload, it won't be lost during a crash.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7248048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91140 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -1055,6 +1055,7 @@ void MetricsService::MakePendingLog() {
     case SEND_OLD_INITIAL_LOGS:
       if (!unsent_initial_logs_.empty()) {
         compressed_log_ = unsent_initial_logs_.back();
+        unsent_initial_logs_.pop_back();
         break;
       }
       state_ = SENDING_OLD_LOGS;
@@ -1063,6 +1064,7 @@ void MetricsService::MakePendingLog() {
     case SENDING_OLD_LOGS:
       if (!unsent_ongoing_logs_.empty()) {
         compressed_log_ = unsent_ongoing_logs_.back();
+        unsent_ongoing_logs_.pop_back();
         break;
       }
       state_ = SENDING_CURRENT_LOGS;
@@ -1337,14 +1339,8 @@ void MetricsService::OnURLFetchComplete(const URLFetcher* source,
         break;
 
       case SEND_OLD_INITIAL_LOGS:
-        DCHECK(!unsent_initial_logs_.empty());
-        unsent_initial_logs_.pop_back();
-        StoreUnsentLogs();
-        break;
-
       case SENDING_OLD_LOGS:
-        DCHECK(!unsent_ongoing_logs_.empty());
-        unsent_ongoing_logs_.pop_back();
+        // Store the updated list to disk now that the removed log is uploaded.
         StoreUnsentLogs();
         break;
 
