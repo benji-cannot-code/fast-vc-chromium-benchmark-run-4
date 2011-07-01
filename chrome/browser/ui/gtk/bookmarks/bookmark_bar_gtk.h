3a917d1c0613965c53b6dccf51c5a3da68b86ac4
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-01 16:22:34 +0000

    Makes the definition order of the methods in BookmarkBarGtk match the declaration order. I wanted to just move Show/Hide, but it turns out many of the methods didn't match, so, I moved a ton of things around.
    I'm TBR'ing since this just shuffles around code (well, I did change
    one comment too).
    
    R=estade@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7290012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/bookmarks/bookmark_bar_gtk.h
+++ chrome/browser/ui/gtk/bookmarks/bookmark_bar_gtk.h
@@ -49,6 +49,9 @@ class BookmarkBarGtk : public ui::AnimationDelegate,
                        public BookmarkBarInstructionsGtk::Delegate,
                        public BookmarkContextMenuControllerDelegate {
  public:
+  // The NTP needs to have access to this.
+  static const int kBookmarkBarNTPHeight;
+
   BookmarkBarGtk(BrowserWindowGtk* window,
                  Profile* profile,
                  Browser* browser,
@@ -93,9 +96,6 @@ class BookmarkBarGtk : public ui::AnimationDelegate,
   virtual void PopupForButton(GtkWidget* button);
   virtual void PopupForButtonNextTo(GtkWidget* button,
                                     GtkMenuDirectionType dir);
-
-  // The NTP needs to have access to this.
-  static const int kBookmarkBarNTPHeight;
 
   // BookmarkContextMenuController::Delegate implementation --------------------
   virtual void CloseMenu();
