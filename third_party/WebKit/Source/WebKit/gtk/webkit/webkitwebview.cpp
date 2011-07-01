304d185c8c2c8459714b9bc3d09b7c505b4f97fa
joone.hur@collabora.co.uk <joone.hur@collabora.co.uk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-01 16:56:20 +0000

    2011-07-01  Joone Hur  <joone.hur@collabora.co.uk>
            Reviewed by Martin Robinson.
    
            [GTK] Add DeviceOrientation feature
            https://bugs.webkit.org/show_bug.cgi?id=63720
    
            Add configure option to enable/disable DeviceOrientation feature.
    
            * configure.ac:
    2011-07-01  Joone Hur  <joone.hur@collabora.co.uk>
    
            Reviewed by Martin Robinson.
    
            [GTK] Add DeviceOrientation feature
            https://bugs.webkit.org/show_bug.cgi?id=63720
    
            Add DeviceMotionClientGtk.cpp and DeviceOrientationClientGtk.cpp to WebKitGtk+ build.
    
            * GNUmakefile.am: Add DeviceMotionClientGtk.cpp, DeviceOrientationClientGtk.cpp.
    2011-07-01  Joone Hur  <joone.hur@collabora.co.uk>
    
            Reviewed by Martin Robinson.
    
            [GTK] Add DeviceOrientation feature
            https://bugs.webkit.org/show_bug.cgi?id=63720
    
            Add empty implementation of DeviceMotionClient and DeviceOrientationClient callback methods.
    
            * GNUmakefile.am:
            * WebCoreSupport/DeviceMotionClientGtk.cpp: Added.
            (WebCore::DeviceMotionClientGtk::DeviceMotionClientGtk):
            (WebCore::DeviceMotionClientGtk::~DeviceMotionClientGtk):
            (WebCore::DeviceMotionClientGtk::deviceMotionControllerDestroyed):
            (WebCore::DeviceMotionClientGtk::setController):
            (WebCore::DeviceMotionClientGtk::startUpdating):
            (WebCore::DeviceMotionClientGtk::stopUpdating):
            (WebCore::DeviceMotionClientGtk::currentDeviceMotion):
            * WebCoreSupport/DeviceMotionClientGtk.h: Added.
            * WebCoreSupport/DeviceOrientationClientGtk.cpp: Added.
            (WebCore::DeviceOrientationClientGtk::DeviceOrientationClientGtk):
            (WebCore::DeviceOrientationClientGtk::~DeviceOrientationClientGtk):
            (WebCore::DeviceOrientationClientGtk::deviceOrientationControllerDestroyed):
            (WebCore::DeviceOrientationClientGtk::setController):
            (WebCore::DeviceOrientationClientGtk::startUpdating):
            (WebCore::DeviceOrientationClientGtk::stopUpdating):
            (WebCore::DeviceOrientationClientGtk::lastOrientation):
            * WebCoreSupport/DeviceOrientationClientGtk.h: Added.
            * webkit/webkitwebview.cpp:
            (webkit_web_view_init):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebview.cpp
@@ -109,6 +109,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/gobject/GOwnPtr.h>
 #include <wtf/text/CString.h>
 
+#if ENABLE(DEVICE_ORIENTATION)
+#include "DeviceMotionClientGtk.h"
+#include "DeviceOrientationClientGtk.h"
+#endif
+
 /**
  * SECTION:webkitwebview
  * @short_description: The central class of the WebKitGTK+ API
@@ -3376,6 +3381,12 @@ static void webkit_web_view_init(WebKitWebView* webView)
     pageClients.editorClient = new WebKit::EditorClient(webView);
     pageClients.dragClient = new WebKit::DragClient(webView);
     pageClients.inspectorClient = new WebKit::InspectorClient(webView);
+
+#if ENABLE(DEVICE_ORIENTATION)
+    pageClients.deviceMotionClient = static_cast<WebCore::DeviceMotionClient*>(new DeviceMotionClientGtk);
+    pageClients.deviceOrientationClient = static_cast<WebCore::DeviceOrientationClient*>(new DeviceOrientationClientGtk);
+#endif
+
     priv->corePage = new Page(pageClients);
 
     // Pages within a same session need to be linked together otherwise some functionalities such
