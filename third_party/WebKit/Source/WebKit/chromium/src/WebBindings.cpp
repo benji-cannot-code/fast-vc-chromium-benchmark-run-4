eada57d8dc2f0b0e68fc01a6785b39a60c6baf87
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-01 06:06:59 +0000

    2011-06-30  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Hajime Morita.
    
            [Chromium] Remove WebBindings::makeNode.
            https://bugs.webkit.org/show_bug.cgi?id=63613
    
            Dead code elimination. No change in behaviour, so no new tests.
    
            * public/WebBindings.h:
            * src/WebBindings.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebBindings.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebBindings.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8DOMWrapper.h"
 #include "V8Element.h"
 #include "V8NPUtils.h"
-#include "V8Node.h"
 #include "V8Proxy.h"
 #include "V8Range.h"
 #elif USE(JSC)
@@ -258,16 +257,6 @@ static NPObject* makeStringArrayImpl(const WebVector<WebString>& data)
     return npCreateV8ScriptObject(0, result, window);
 }
 
-static NPObject* makeNodeImpl(WebNode data)
-{
-    v8::HandleScope handleScope;
-    if (data.isNull())
-        return 0;
-    v8::Handle<v8::Object> result = V8Node::wrap(data.unwrap<Node>());
-    WebCore::DOMWindow* window = WebCore::V8Proxy::retrieveWindow(WebCore::V8Proxy::currentContext());
-    return npCreateV8ScriptObject(0, result, window);
-}
-
 #endif
 
 bool WebBindings::getRange(NPObject* range, WebRange* webRange)
@@ -304,16 +293,6 @@ NPObject* WebBindings::makeStringArray(const WebVector<WebString>& data)
 {
 #if USE(V8)
     return makeStringArrayImpl(data);
-#else
-    // Not supported on other ports (JSC, etc.).
-    return 0;
-#endif
-}
-
-NPObject* WebBindings::makeNode(const WebNode& data)
-{
-#if USE(V8)
-    return makeNodeImpl(data);
 #else
     // Not supported on other ports (JSC, etc.).
     return 0;
