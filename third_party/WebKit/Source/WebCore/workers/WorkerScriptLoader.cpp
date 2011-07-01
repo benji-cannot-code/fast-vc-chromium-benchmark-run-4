1712cd3c01bb5eaadd88b9ee78b356bfdb4c182a
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-01 22:06:07 +0000

    2011-07-01  Nate Chapin  <japhet@chromium.org>
            Test for https://bugs.webkit.org/show_bug.cgi?id=63835.
    
            Reviewed by Dmitry Titov.
    
            * fast/workers/worker-finish-crash-expected.txt: Added.
            * fast/workers/worker-finish-crash.html: Added.
    2011-07-01  Nate Chapin  <japhet@chromium.org>
    
            Ensure WorkerScriptLoader::notifyFinished() doesn't re-enter.
            https://bugs.webkit.org/show_bug.cgi?id=63835
    
            Reviewed by Dmitry Titov.
    
            Test: fast/workers/worker-finish-crash.html
    
            * workers/WorkerScriptLoader.cpp:
            (WebCore::WorkerScriptLoader::WorkerScriptLoader):
            (WebCore::WorkerScriptLoader::notifyFinished):
            * workers/WorkerScriptLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90284 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/workers/WorkerScriptLoader.cpp
+++ third_party/WebKit/Source/WebCore/workers/WorkerScriptLoader.cpp
@@ -53,6 +53,7 @@ WorkerScriptLoader::WorkerScriptLoader(ResourceRequestBase::TargetType targetTyp
     , m_failed(false)
     , m_identifier(0)
     , m_targetType(targetType)
+    , m_finishing(false)
 {
 }
 
@@ -182,8 +183,11 @@ void WorkerScriptLoader::notifyError()
     
 void WorkerScriptLoader::notifyFinished()
 {
-    if (m_client)
-        m_client->notifyFinished();
+    if (!m_client || m_finishing)
+        return;
+
+    m_finishing = true;
+    m_client->notifyFinished();
 }
 
 } // namespace WebCore
