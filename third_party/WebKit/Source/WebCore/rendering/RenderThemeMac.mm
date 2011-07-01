8a3251f6f78af9b92ac66176e26ccbbfdc7a6d1b
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-01 03:54:58 +0000

    2011-06-30  Kent Tamura  <tkent@chromium.org>
            Reviewed by Tony Chang.
    
            [Chromium] Input type="range" slider thumb is rendered upside down on Mac OS X.
            https://bugs.webkit.org/show_bug.cgi?id=63697
    
            * platform/chromium/test_expectations.txt: Some tests need new expectation images.
    2011-06-30  Kent Tamura  <tkent@chromium.org>
    
            Reviewed by Tony Chang.
    
            [Chromium] Input type="range" slider thumb is rendered upside down on Mac OS X.
            https://bugs.webkit.org/show_bug.cgi?id=63697
    
            http://trac.webkit.org/changeset/86261 added a thumb flipping code
            for Mac-WebKit2. Chromium also needs it.
    
            * rendering/RenderThemeMac.mm:
            (WebCore::RenderThemeMac::paintSliderThumb): Always flip vertically on Chromium.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.mm
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.mm
@@ -1366,6 +1366,10 @@ bool RenderThemeMac::paintSliderThumb(RenderObject* o, const PaintInfo& paintInf
         paintInfo.context->scale(FloatSize(1, -1));
         paintInfo.context->translate(0, -(unzoomedRect.y() + unzoomedRect.height()));
     }
+#elif PLATFORM(CHROMIUM)
+    paintInfo.context->translate(0, unzoomedRect.y());
+    paintInfo.context->scale(FloatSize(1, -1));
+    paintInfo.context->translate(0, -(unzoomedRect.y() + unzoomedRect.height()));
 #endif
     
     [sliderThumbCell drawInteriorWithFrame:unzoomedRect inView:documentViewFor(o)];
