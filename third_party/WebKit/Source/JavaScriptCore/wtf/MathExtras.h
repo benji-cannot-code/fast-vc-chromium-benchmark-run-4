04defa56ac4e4aacf7ba8c48ad7797c760af554a
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 02:59:02 +0000

    Reviewed by Sam Weinig.
    https://bugs.webkit.org/show_bug.cgi?id=16652
    Firefox and JavaScriptCore differ in Number.toString(integer)
    
    Source/JavaScriptCore: 
    
    Our arbitrary radix (2..36) toString conversion is inaccurate.
    This is partly because it uses doubles to perform math that requires
    higher accuracy, and partly becasue it does not attempt to correctly
    detect where to terminate, instead relying on a simple 'epsilon'.
    
    * runtime/NumberPrototype.cpp:
    (JSC::decomposeDouble):
        - helper function to extract sign, exponent, mantissa from IEEE doubles.
    (JSC::Uint16WithFraction::Uint16WithFraction):
        - helper class, u16int with infinite precision fraction, used to convert
          the fractional part of the number to a string.
    (JSC::Uint16WithFraction::operator*=):
        - Multiply by a uint16.
    (JSC::Uint16WithFraction::operator<):
        - Compare two Uint16WithFractions.
    (JSC::Uint16WithFraction::floorAndSubtract):
        - Extract the integer portion of the number, and subtract it (clears the integer portion).
    (JSC::Uint16WithFraction::comparePoint5):
        - Compare to 0.5.
    (JSC::Uint16WithFraction::sumGreaterThanOne):
        - Passed a second Uint16WithFraction, returns true if the result of adding
          the two values would be greater than one.
    (JSC::Uint16WithFraction::isNormalized):
        - Used by ASSERTs to consistency check internal representation.
    (JSC::BigInteger::BigInteger):
        - helper class, unbounded integer value, used to convert the integer part
          of the number to a string.
    (JSC::BigInteger::divide):
        - Divide this value through by a uint32.
    (JSC::BigInteger::operator!):
        - test for zero.
    (JSC::toStringWithRadix):
        - Performs number to string conversion, with the given radix (2..36).
    (JSC::numberProtoFuncToString):
        - Changed to use toStringWithRadix.
    
    Tools: 
    
    Added forwarding header.
    
    * DumpRenderTree/ForwardingHeaders/wtf/StdLibExtras.h: Added.
    
    LayoutTests: 
    
    Our arbitrary radix (2..36) toString conversion is inaccurate.
    This is partly because it uses doubles to perform math that requires
    higher accuracy, and partly becasue it does not attempt to correctly
    detect where to terminate, instead relying on a simple 'epsilon'.
    
    * fast/js/number-toString-expected.txt:
        - Update expected results from FAIL to PASS.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/MathExtras.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/MathExtras.h
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <float.h>
 #include <limits>
+#include <stdint.h>
 #include <stdlib.h>
+#include <wtf/StdLibExtras.h>
 
 #if OS(SOLARIS)
 #include <ieeefp.h>
@@ -259,5 +261,26 @@ using std::isinf;
 using std::isnan;
 using std::signbit;
 #endif
+
+// decompose 'number' to its sign, exponent, and mantissa components.
+// The result is interpreted as:
+//     (sign ? -1 : 1) * pow(2, exponent) * (mantissa / (1 << 52))
+inline void decomposeDouble(double number, bool& sign, int32_t& exponent, uint64_t& mantissa)
+{
+    ASSERT(isfinite(number));
+
+    sign = signbit(number);
+
+    uint64_t bits = WTF::bitwise_cast<uint64_t>(number);
+    exponent = (static_cast<int32_t>(bits >> 52) & 0x7ff) - 0x3ff;
+    mantissa = bits & 0xFFFFFFFFFFFFFull;
+
+    // Check for zero/denormal values; if so, adjust the exponent,
+    // if not insert the implicit, omitted leading 1 bit.
+    if (exponent == -0x3ff)
+        exponent = mantissa ? -0x3fe : 0;
+    else
+        mantissa |= 0x10000000000000ull;
+}
 
 #endif // #ifndef WTF_MathExtras_h
