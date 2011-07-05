c1dd3d3856a9f69e7ab39ec7e9844cfe9987f63d
keishi@google.com <keishi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-05 08:32:57 +0000

    Fix regression: Download status doesn't appear anymore in the download-shelf
    Caused by http://codereview.chromium.org/6905049
    
    Replaced show/hideStatusAnimation_ with toggleStatusVisibilityAnimation_
    because show/hideStatusAnimation_ shouldn't exist at the same time. 
    
    BUG=86640
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7272024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91492 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/cocoa/download/download_item_cell_unittest.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/memory/scoped_nsobject.h"
+#import "chrome/browser/ui/cocoa/download/download_item_cell.h"
+#import "chrome/browser/ui/cocoa/cocoa_test_helper.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/platform_test.h"
+
+TEST(DownloadItemCellTest, ToggleStatusText) {
+  scoped_nsobject<DownloadItemCell> cell;
+  cell.reset([[DownloadItemCell alloc] initTextCell:@""]);
+  EXPECT_FALSE([cell isStatusTextVisible]);
+  EXPECT_FLOAT_EQ(0.0, [cell statusTextAlpha]);
+
+  [cell showSecondaryTitle];
+  [cell skipVisibilityAnimation];
+  EXPECT_TRUE([cell isStatusTextVisible]);
+  EXPECT_FLOAT_EQ(1.0, [cell statusTextAlpha]);
+
+  [cell hideSecondaryTitle];
+  [cell skipVisibilityAnimation];
+  EXPECT_FALSE([cell isStatusTextVisible]);
+  EXPECT_FLOAT_EQ(0.0, [cell statusTextAlpha]);
+}
