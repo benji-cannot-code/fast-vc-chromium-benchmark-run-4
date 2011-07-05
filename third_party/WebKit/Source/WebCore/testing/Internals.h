9da2a184e31d1279c825f2d1caa45bdf4cf5f17f
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 10:36:06 +0000

    2011-07-05  Vsevolod Vlasov  <vsevik@chromium.org>
            Unreviewed; Qt Linux Release minimal compile fix.
    
            * testing/Internals.h:
            (WebCore::Internals::setInspectorResourcesDataSizeLimits):
            * testing/Internals.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.h
+++ third_party/WebKit/Source/WebCore/testing/Internals.h
@@ -56,6 +56,8 @@ public:
 
 #if ENABLE(INSPECTOR)
     void setInspectorResourcesDataSizeLimits(Document*, int maximumResourcesContentSize, int maximumSingleResourceContentSize, ExceptionCode&);
+#else
+    void setInspectorResourcesDataSizeLimits(Document*, int maximumResourcesContentSize, int maximumSingleResourceContentSize, ExceptionCode&) { }
 #endif
 
 private:
