d451d641332bd95764737ddc941447a1358479e5
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 01:27:45 +0000

    2011-07-04  Kent Tamura  <tkent@chromium.org>
            REGRESSION (r87067): Text overflows from short height text field.
            https://bugs.webkit.org/show_bug.cgi?id=61768
    
            Reviewed by Dimitri Glazkov.
    
            * fast/forms/textfield-overflow-by-value-update.html: Added.
            * platform/chromium/test_expectations.txt: Add textfield-overflow-by-value-update.html.
            * platform/mac/fast/forms/textfield-overflow-by-value-update-expected.png: Added.
            * platform/mac/fast/forms/textfield-overflow-by-value-update-expected.txt: Added.
    2011-07-04  Kent Tamura  <tkent@chromium.org>
    
            REGRESSION (r87067): Text overflows from short height text field.
            https://bugs.webkit.org/show_bug.cgi?id=61768
    
            Reviewed by Dimitri Glazkov.
    
            Since r87067, the RenderStyle for the inner text element is reset
            by the value update, but the renderer for the inner text element
            keeps to have metrics calculated by the previous style. So
            RenderTextControlSingleLine::layout() didn't set a desired height
            to the renderer for the inner text element.
    
            This patch changes the style creation code so that it has the
            desired height initially.
    
            Test: fast/forms/textfield-overflow-by-value-update.html
    
            * rendering/RenderTextControlSingleLine.cpp:
            (WebCore::RenderTextControlSingleLine::RenderTextControlSingleLine):
            Initialize m_desiredInnerTextHeight.
            (WebCore::RenderTextControlSingleLine::layout):
            Update m_desiredInnerTextHeight.
            (WebCore::RenderTextControlSingleLine::styleDidChange):
            Reset m_desiredInnerTextHeight.
            (WebCore::RenderTextControlSingleLine::createInnerTextStyle):
            Sets the height if m_desiredInnerTextHeight has a concrete value.
            * rendering/RenderTextControlSingleLine.h:
            Declare m_desiredInnerTextHeight.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTextControlSingleLine.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderTextControlSingleLine.h
@@ -144,7 +144,7 @@ private:
 
     bool m_searchPopupIsVisible;
     bool m_shouldDrawCapsLockIndicator;
-
+    LayoutUnit m_desiredInnerTextHeight;
     Timer<RenderTextControlSingleLine> m_searchEventTimer;
     RefPtr<SearchPopupMenu> m_searchPopup;
     Vector<String> m_recentSearches;
