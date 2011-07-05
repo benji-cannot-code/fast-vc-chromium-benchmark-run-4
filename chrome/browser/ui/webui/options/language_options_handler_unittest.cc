8a9c8c70763b4ed68e4d14935f43f49c7e859e61
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-05 07:43:26 +0000

    Cleanup InputMethodDescriptor.
    - Make all member variables private.
    - Hide a constructor.
    - Remove one TODO.
    
    BUG=None
    TEST=try
    
    
    Review URL: http://codereview.chromium.org/7307001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/language_options_handler_unittest.cc
+++ chrome/browser/ui/webui/options/language_options_handler_unittest.cc
@@ -21,16 +21,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chromeos::input_method::InputMethodDescriptor;
 using chromeos::input_method::InputMethodDescriptors;
 
+static InputMethodDescriptor GetDesc(const std::string& id,
+                                     const std::string& raw_layout,
+                                     const std::string& language_code) {
+  return InputMethodDescriptor::CreateInputMethodDescriptor(
+      id, raw_layout, language_code);
+}
+
 static InputMethodDescriptors CreateInputMethodDescriptors() {
   InputMethodDescriptors descriptors;
-  descriptors.push_back(
-      InputMethodDescriptor("xkb:us::eng", "us", "us", "eng"));
-  descriptors.push_back(
-      InputMethodDescriptor("xkb:fr::fra", "fr", "fr", "fra"));
-  descriptors.push_back(
-      InputMethodDescriptor("xkb:be::fra", "be", "be", "fr"));
-  descriptors.push_back(
-      InputMethodDescriptor("mozc", "us", "us", "ja"));
+  descriptors.push_back(GetDesc("xkb:us::eng", "us", "eng"));
+  descriptors.push_back(GetDesc("xkb:fr::fra", "fr", "fra"));
+  descriptors.push_back(GetDesc("xkb:be::fra", "be", "fr"));
+  descriptors.push_back(GetDesc("mozc", "us", "ja"));
   return descriptors;
 }
 
