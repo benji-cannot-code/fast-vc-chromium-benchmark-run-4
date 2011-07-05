3ebe985047619b61ea6cc900fb41e35e2940b4db
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 07:18:01 +0000

    2011-07-05  Shinya Kawanaka  <shinyak@google.com>
            Reviewed by Kent Tamura.
    
            The default step is used when the step attribute of input[type='number'] is "any".
            https://bugs.webkit.org/show_bug.cgi?id=57723
    
            Changed Layout tests to use the default step when the step attribute of
            input[type='number'] is "any".
    
            * fast/forms/input-stepup-stepdown-from-renderer-expected.txt: Regenerated.
            * fast/forms/script-tests/input-stepup-stepdown-from-renderer.js:
    2011-07-05  Shinya Kawanaka  <shinyak@google.com>
    
            Reviewed by Kent Tamura.
    
            The default step is used when attribute step of input[type='number'] is "any".
            https://bugs.webkit.org/show_bug.cgi?id=57723
    
            When step is "any", 0 was chosen as step, but this is not intuitive.
            So changed to use the default step.
    
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::getAllowedValueStep):
              Calls getAllowedValueStepWithDecimalPlaces with RejectAny.
            (WebCore::HTMLInputElement::getAllowedValueStepWithDecimalPlaces):
              Takes a new arugument AnyStepHandling. If it is AnyIsDefaultStep, this method
              allows "step" attribute to take a value "any" to use the default step as step value.
              Otherwise, INVALID_STATE_ERR will be returned if "step" is "any".
            (WebCore::HTMLInputElement::applyStep): ditto.
            (WebCore::HTMLInputElement::stepUp):
              Calls applyStep with RejectAny.
            (WebCore::HTMLInputElement::stepDown): ditto.
            (WebCore::HTMLInputElement::stepUpFromRenderer):
              Removes a check that step is "any". It is checked in getAllowedValueStepWithDecimalPlaces.
            * html/HTMLInputElement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
@@ -63,7 +63,7 @@ public:
     // Sets the "allowed value step" defined in the HTML spec to the specified double pointer.
     // Returns false if there is no "allowed value step."
     bool getAllowedValueStep(double*) const;
-    bool getAllowedValueStepWithDecimalPlaces(double*, unsigned*) const;
+
     // For ValidityState.
     bool stepMismatch(const String&) const;
     String minimumString() const;
@@ -241,6 +241,7 @@ protected:
 
 private:
     enum AutoCompleteSetting { Uninitialized, On, Off };
+    enum AnyStepHandling { RejectAny, AnyIsDefaultStep };
 
     virtual void willMoveToNewOwnerDocument();
     virtual void didMoveToNewOwnerDocument();
@@ -317,8 +318,10 @@ private:
 
     void updateType();
 
+    bool getAllowedValueStepWithDecimalPlaces(AnyStepHandling, double*, unsigned*) const;
+
     // Helper for stepUp()/stepDown().  Adds step value * count to the current value.
-    void applyStep(double count, ExceptionCode&);
+    void applyStep(double count, AnyStepHandling, ExceptionCode&);
 
 #if ENABLE(DATALIST)
     HTMLDataListElement* dataList() const;
