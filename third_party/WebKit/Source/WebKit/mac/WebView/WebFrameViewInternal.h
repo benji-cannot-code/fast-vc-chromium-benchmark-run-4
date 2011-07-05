83023c819bbcfb02c37b9a4d678241d454561477
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 01:54:27 +0000

    Moved handling of frame size changes into its own method.
    Reviewed by Oliver Hunt.
    
    * WebView/WebFrameView.mm:
    (-[WebFrameView _frameSizeChanged]): Added. Moved code here...
    (-[WebFrameView setFrameSize:]): ...from here.
    * WebView/WebFrameViewInternal.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebFrameViewInternal.h
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebFrameViewInternal.h
@@ -45,5 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (BOOL)_canShowMIMETypeAsHTML:(NSString *)MIMEType;
 - (WebDynamicScrollBarsView *)_scrollView;
 - (void)_install;
+- (void)_frameSizeChanged;
 
 @end
