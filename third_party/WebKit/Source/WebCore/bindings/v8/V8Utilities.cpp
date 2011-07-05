c89bc97aa90d42baf0ced33fffecb8d0bf4120a4
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-05 22:13:41 +0000

    2011-07-05  Hans Wennborg  <hans@chromium.org>
            [V8][Chromium] Remove use of OwnHandle from V8LocalContext
            https://bugs.webkit.org/show_bug.cgi?id=63925
    
            Reviewed by Adam Barth.
    
            Replace the OwnHandle<> member in V8LocalContext with
            v8::Persistent<v8::Context>.
            The code that used OwnHandle<> would not correctly Dispose
            of the Context, which would cause the memory to fill up when creating
            and destructing many V8LocalContext objects, such as in the case
            of IndexedDB.
    
            * bindings/v8/V8Utilities.cpp:
            (WebCore::V8LocalContext::V8LocalContext):
            (WebCore::V8LocalContext::~V8LocalContext):
            * bindings/v8/V8Utilities.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Utilities.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Utilities.cpp
@@ -50,16 +50,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 V8LocalContext::V8LocalContext()
+    : m_context(v8::Context::New())
 {
     V8BindingPerIsolateData::ensureInitialized(v8::Isolate::GetCurrent());
-    m_context.set(v8::Context::New());
-    m_context.get()->Enter();
+    m_context->Enter();
 }
 
 
-V8LocalContext::~V8LocalContext() 
+V8LocalContext::~V8LocalContext()
 {
-    m_context.get()->Exit();
+    m_context->Exit();
+    m_context.Dispose();
 }
 
 // Use an array to hold dependents. It works like a ref-counted scheme.
