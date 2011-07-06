26fe1d07c62c188ad6ba838781ce21d5b51caf43
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 23:38:32 +0000

    2011-07-06  Emil A Eklund  <eae@chromium.org>
            Reviewed by Eric Seidel.
    
            Switch nodeAtPoint to to new layout types
            https://bugs.webkit.org/show_bug.cgi?id=63663
    
            No new tests, no functionality changes.
    
            * rendering/EllipsisBox.cpp:
            (WebCore::EllipsisBox::nodeAtPoint):
            * rendering/EllipsisBox.h:
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::nodeAtPoint):
            * rendering/InlineBox.h:
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::nodeAtPoint):
            * rendering/InlineFlowBox.h:
            * rendering/InlineTextBox.cpp:
            (WebCore::InlineTextBox::nodeAtPoint):
            * rendering/InlineTextBox.h:
            * rendering/LayoutTypes.h:
            (WebCore::toLayoutSize):
            (WebCore::toLayoutPoint):
            (WebCore::roundedLayoutPoint):
            (WebCore::roundedLayoutUnit):
            * rendering/RenderBlock.cpp:
            (WebCore::RenderBlock::nodeAtPoint):
            * rendering/RenderBlock.h:
            * rendering/RenderBox.cpp:
            (WebCore::RenderBox::nodeAtPoint):
            * rendering/RenderBox.h:
            * rendering/RenderFrameSet.cpp:
            (WebCore::RenderFrameSet::nodeAtPoint):
            * rendering/RenderFrameSet.h:
            * rendering/RenderImage.cpp:
            (WebCore::RenderImage::nodeAtPoint):
            * rendering/RenderImage.h:
            * rendering/RenderInline.cpp:
            (WebCore::RenderInline::nodeAtPoint):
            * rendering/RenderInline.h:
            * rendering/RenderListBox.cpp:
            (WebCore::RenderListBox::nodeAtPoint):
            * rendering/RenderListBox.h:
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::nodeAtPoint):
            * rendering/RenderObject.h:
            * rendering/RenderTable.cpp:
            (WebCore::RenderTable::nodeAtPoint):
            * rendering/RenderTable.h:
            * rendering/RenderTableRow.cpp:
            (WebCore::RenderTableRow::nodeAtPoint):
            * rendering/RenderTableRow.h:
            * rendering/RenderTableSection.cpp:
            (WebCore::RenderTableSection::nodeAtPoint):
            * rendering/RenderTableSection.h:
            * rendering/RenderText.h:
            (WebCore::RenderText::nodeAtPoint):
            * rendering/RenderTextControlMultiLine.cpp:
            (WebCore::RenderTextControlMultiLine::nodeAtPoint):
            * rendering/RenderTextControlMultiLine.h:
            * rendering/RenderTextControlSingleLine.cpp:
            (WebCore::RenderTextControlSingleLine::nodeAtPoint):
            * rendering/RenderTextControlSingleLine.h:
            * rendering/RenderWidget.cpp:
            (WebCore::RenderWidget::nodeAtPoint):
            * rendering/RenderWidget.h:
            * rendering/RootInlineBox.cpp:
            (WebCore::RootInlineBox::nodeAtPoint):
            * rendering/RootInlineBox.h:
            * rendering/svg/RenderSVGForeignObject.cpp:
            (WebCore::RenderSVGForeignObject::nodeAtFloatPoint):
            (WebCore::RenderSVGForeignObject::nodeAtPoint):
            * rendering/svg/RenderSVGForeignObject.h:
            * rendering/svg/RenderSVGModelObject.cpp:
            (WebCore::RenderSVGModelObject::nodeAtPoint):
            * rendering/svg/RenderSVGModelObject.h:
            * rendering/svg/RenderSVGRoot.cpp:
            (WebCore::RenderSVGRoot::nodeAtPoint):
            * rendering/svg/RenderSVGRoot.h:
            * rendering/svg/RenderSVGText.cpp:
            (WebCore::RenderSVGText::nodeAtPoint):
            * rendering/svg/RenderSVGText.h:
            * rendering/svg/SVGInlineTextBox.cpp:
            (WebCore::SVGInlineTextBox::nodeAtPoint):
            * rendering/svg/SVGInlineTextBox.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90515 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.h
@@ -238,7 +238,7 @@ public:
 
     virtual void layout();
     virtual void paint(PaintInfo&, const LayoutPoint&);
-    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, const IntPoint& pointInContainer, const IntPoint& accumulatedOffset, HitTestAction);
+    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, const LayoutPoint& pointInContainer, const LayoutPoint& accumulatedOffset, HitTestAction);
 
     virtual void destroy();
 
