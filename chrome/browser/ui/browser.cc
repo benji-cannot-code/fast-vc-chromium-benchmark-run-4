14b23030e65eeb02ad26470b272f38a04efef432
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-06 22:47:04 +0000

    Remove DownloadShelf from ChromiumOS build.
    Besides removing some dead code, this eliminates a GTK link dependency
    from DownloadShelfContextMenuView.
    
    BUG=chromium-os:13887
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7284015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_item.h"
 #include "chrome/browser/download/download_item_model.h"
 #include "chrome/browser/download/download_manager.h"
-#include "chrome/browser/download/download_shelf.h"
 #include "chrome/browser/download/download_started_animation.h"
 #include "chrome/browser/download/save_package.h"
 #include "chrome/browser/extensions/crx_installer.h"
@@ -95,7 +94,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/dock_info.h"
 #include "chrome/browser/ui/tabs/tab_menu_model.h"
 #include "chrome/browser/ui/web_applications/web_app_ui.h"
-#include "chrome/browser/ui/webui/active_downloads_ui.h"
 #include "chrome/browser/ui/webui/bug_report_ui.h"
 #include "chrome/browser/ui/webui/options/content_settings_handler.h"
 #include "chrome/browser/ui/window_sizer.h"
@@ -155,6 +153,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/extensions/file_manager_util.h"
+#include "chrome/browser/ui/webui/active_downloads_ui.h"
+#else
+#include "chrome/browser/download/download_shelf.h"
 #endif
 
 using base::TimeDelta;
@@ -1911,11 +1912,14 @@ void Browser::ShowHistoryTab() {
 
 void Browser::ShowDownloadsTab() {
   UserMetrics::RecordAction(UserMetricsAction("ShowDownloads"));
+#if !defined(OS_CHROMEOS)
+  // ChromiumOS uses ActiveDownloadsUI instead of of DownloadShelf.
   if (window()) {
     DownloadShelf* shelf = window()->GetDownloadShelf();
     if (shelf->IsShowing())
       shelf->Close();
   }
+#endif
   ShowSingletonTab(GURL(chrome::kChromeUIDownloadsURL));
 }
 
--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/compact_nav/compact_options_bar.h"
 #include "chrome/browser/ui/views/default_search_view.h"
 #include "chrome/browser/ui/views/download/download_in_progress_dialog_view.h"
-#include "chrome/browser/ui/views/download/download_shelf_view.h"
 #include "chrome/browser/ui/views/frame/browser_view_layout.h"
 #include "chrome/browser/ui/views/frame/contents_container.h"
 #include "chrome/browser/ui/views/fullscreen_exit_bubble.h"
@@ -120,6 +119,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/ui/views/keyboard_overlay_dialog_view.h"
+#else
+#include "chrome/browser/ui/views/download/download_shelf_view.h"
 #endif
 
 using base::TimeDelta;
@@ -352,11 +353,13 @@ BrowserView::~BrowserView() {
   ticker_.UnregisterTickHandler(&hung_window_detector_);
 #endif
 
+#if !defined(OS_CHROMEOS)
   // We destroy the download shelf before |browser_| to remove its child
   // download views from the set of download observers (since the observed
   // downloads can be destroyed along with |browser_| and the observer
   // notifications will call back into deleted objects).
   download_shelf_.reset();
+#endif
 
   // The TabStrip attaches a listener to the model. Make sure we shut down the
   // TabStrip first so that it can cleanly remove the listener.
@@ -1074,15 +1077,24 @@ void BrowserView::SetDownloadShelfVisible(bool visible) {
 }
 
 bool BrowserView::IsDownloadShelfVisible() const {
+#if defined(OS_CHROMEOS)
+  return false;
+#else
   return download_shelf_.get() && download_shelf_->IsShowing();
+#endif
 }
 
 DownloadShelf* BrowserView::GetDownloadShelf() {
+#if defined(OS_CHROMEOS)
+  NOTREACHED();
+  return NULL;
+#else
   if (!download_shelf_.get()) {
     download_shelf_.reset(new DownloadShelfView(browser_.get(), this));
     download_shelf_->set_parent_owned(false);
   }
   return download_shelf_.get();
+#endif
 }
 
 void BrowserView::ShowRepostFormWarningDialog(TabContents* tab_contents) {
@@ -1756,8 +1768,10 @@ void BrowserView::GetAccessiblePanes(
     panes->push_back(bookmark_bar_view_.get());
   if (infobar_container_)
     panes->push_back(infobar_container_);
+#if !defined(OS_CHROMEOS)
   if (download_shelf_.get())
     panes->push_back(download_shelf_.get());
+#endif
 }
 
 ///////////////////////////////////////////////////////////////////////////////
