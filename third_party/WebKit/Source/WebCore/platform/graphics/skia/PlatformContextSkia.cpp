d6b999dac0b714c92640d40490beeb9ab5086b9f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 21:16:18 +0000

    2011-07-06  Mike Reed  <reed@google.com>
            [skia] don't include deprecated header
            https://bugs.webkit.org/show_bug.cgi?id=64025
    
            Reviewed by James Robinson.
    
            No new tests. Just removing #include of an unused header that will be gone soon
    
            * platform/graphics/chromium/LayerTextureUpdaterCanvas.cpp:
            * platform/graphics/skia/PlatformContextSkia.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "GrContext.h"
 #include "SkGpuDevice.h"
-#include "SkGpuDeviceFactory.h"
 
 #include <wtf/MathExtras.h>
 #include <wtf/Vector.h>
