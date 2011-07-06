1e1f18e595ae438d6fda509b932df4bc45494bcc
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-06 16:01:11 +0000

    [cros] Match cellular technology with flimlam, add GSM.
    BUG=chrome-os-partner:4666
    TEST=Manual.
    
    
    Review URL: http://codereview.chromium.org/7312002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91567 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -308,6 +308,7 @@ const char* kNetworkTechnologyHspa = "HSPA";
 const char* kNetworkTechnologyHspaPlus = "HSPA+";
 const char* kNetworkTechnologyLte = "LTE";
 const char* kNetworkTechnologyLteAdvanced = "LTE Advanced";
+const char* kNetworkTechnologyGsm = "GSM";
 
 // Flimflam roaming state options
 const char* kRoamingStateHome = "home";
@@ -737,6 +738,7 @@ static NetworkTechnology ParseNetworkTechnology(const std::string& technology) {
     { kNetworkTechnologyHspaPlus, NETWORK_TECHNOLOGY_HSPA_PLUS },
     { kNetworkTechnologyLte, NETWORK_TECHNOLOGY_LTE },
     { kNetworkTechnologyLteAdvanced, NETWORK_TECHNOLOGY_LTE_ADVANCED },
+    { kNetworkTechnologyGsm, NETWORK_TECHNOLOGY_GSM },
   };
   static StringToEnum<NetworkTechnology> parser(
       table, arraysize(table), NETWORK_TECHNOLOGY_UNKNOWN);
@@ -1929,6 +1931,9 @@ std::string CellularNetwork::GetNetworkTechnologyString() const {
       break;
     case NETWORK_TECHNOLOGY_LTE_ADVANCED:
       return "LTE Advanced";
+      break;
+    case NETWORK_TECHNOLOGY_GSM:
+      return "GSM";
       break;
     default:
       return l10n_util::GetStringUTF8(
@@ -4794,7 +4799,7 @@ class NetworkLibraryImpl : public NetworkLibrary  {
     cellular2->set_network_technology(NETWORK_TECHNOLOGY_UMTS);
     AddNetwork(cellular2);
 
-    // VPNs.
+    // VPNs
     VirtualNetwork* vpn1 = new VirtualNetwork("fv1");
     vpn1->set_name("Fake VPN Provider 1");
     vpn1->set_server_hostname("vpn1server.fake.com");
