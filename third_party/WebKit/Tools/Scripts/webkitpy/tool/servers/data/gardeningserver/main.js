4c0c4f53d9c1137bdd61b6cf23de4e9227ba3330
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 07:04:02 +0000

    2011-07-05  Adam Barth  <abarth@webkit.org>
            garden-o-matic should list the failing tests
            https://bugs.webkit.org/show_bug.cgi?id=63978
    
            Reviewed by Eric Seidel.
    
            * Scripts/webkitpy/tool/servers/data/gardeningserver/config.js: Added.
            * Scripts/webkitpy/tool/servers/data/gardeningserver/index.html:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/main.js:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/results.js:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/results_unittests.js:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/run-unittests.html:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js: Added.
            * Scripts/webkitpy/tool/servers/data/gardeningserver/ui_unittests.js: Added.
            * Scripts/webkitpy/tool/servers/gardeningserver.py:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/main.js
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/main.js
@@ -1,20 +1,35 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (function() {
-  function quit() {
+
+function quit()
+{
     $.post('/quitquitquit', function(data){
-      $('.butterbar .status').html(data)
-      $('.butterbar').fadeIn();
+        $('.butterbar .status').html(data)
+        $('.butterbar').fadeIn();
     });
-  }
+}
 
-  function hide() {
+function hide()
+{
     $(this).parent().fadeOut();
-  }
+}
 
-  $('.hide').live('click', hide);
-  $('.quit').live('click', quit);
+function fetchResults(onsuccess)
+{
+    results.fetchResultsByBuilder(config.builders, function(resultsByBuilder) {
+        unexpectedFailures = ui.resultsByTest(results.unexpectedFailuresByTest(resultsByBuilder));
+        $('.failures').append(unexpectedFailures);
+        onsuccess();
+    });
+}
 
-  $(document).ready(function() {
-    $('.butterbar').fadeOut();
-  })
+$('.hide').live('click', hide);
+$('.quit').live('click', quit);
+
+$(document).ready(function() {
+    fetchResults(function() {
+        $('.butterbar').fadeOut();
+    });
+});
+
 })();
