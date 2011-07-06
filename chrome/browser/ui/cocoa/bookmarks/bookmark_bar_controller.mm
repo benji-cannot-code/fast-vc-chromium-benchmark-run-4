19f5c4d5cdadea804b2823dfe71d130fb494170c
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-06 22:54:53 +0000

    Give tooltip to folder buttons on bookmark bar if name is long enough to be visibly truncated. R=sail@chromium.org BUG=88595 Review URL: http://codereview.chromium.org/7247024
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.mm
@@ -1230,7 +1230,6 @@ void RecordAppLaunch(Profile* profile, GURL url) {
   }
 }
 
-
 - (BookmarkButton*)buttonForNode:(const BookmarkNode*)node
                          xOffset:(int*)xOffset {
   BookmarkButtonCell* cell = [self cellForBookmarkNode:node];
@@ -1266,6 +1265,13 @@ void RecordAppLaunch(Profile* profile, GURL url) {
     [button setTarget:self];
     [button setAction:@selector(openBookmarkFolderFromButton:)];
     [button setActsOnMouseDown:YES];
+    // If it has a title, and it will be truncated, show full title in
+    // tooltip.
+    NSString* title = base::SysUTF16ToNSString(node->GetTitle());
+    if ([title length] &&
+        [[button cell] cellSize].width > bookmarks::kDefaultBookmarkWidth) {
+      [button setToolTip:title];
+    }
   } else {
     // Make the button do something
     [button setTarget:self];
