a585df0488443e515b29320d66b82be0d3b16044
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 01:18:20 +0000

    2011-07-05  Adam Barth  <abarth@webkit.org>
            garden-o-matic should know how to fetch test results from the (NRWT) bots
            https://bugs.webkit.org/show_bug.cgi?id=63959
    
            Reviewed by Eric Seidel.
    
            Currently, this code just fetches the results and expectations JSON
            blob for Linux from the server and logs it to the console.
    
            * Scripts/webkitpy/tool/servers/data/gardeningserver/index.html:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/results.js: Added.
            * Scripts/webkitpy/tool/servers/gardeningserver.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/results.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(function() {
+  var TEST_RESULTS_SERVER = 'http://test-results.appspot.com/';
+  var TEST_TYPE = 'layout-tests';
+  var RESULTS_NAME = 'full_results.json';
+  var MASTER_NAME = 'ChromiumWebkit';
+
+  function resultsURL(builder_name, name) {
+    return TEST_RESULTS_SERVER + 'testfile' +
+        '?builder=' + builder_name +
+        '&master=' + MASTER_NAME +
+        '&testtype=' + TEST_TYPE +
+        '&name=' + name;
+  }
+
+  function fetchResults(builder_name, onsuccess) {
+    $.ajax({
+      url: resultsURL(builder_name, RESULTS_NAME),
+      dataType: 'jsonp',
+      success: onsuccess
+    });
+  }
+
+  fetchResults('Webkit Linux', function(data) {
+    console.log("== Results ==")
+    console.log(data);
+  });
+})();
