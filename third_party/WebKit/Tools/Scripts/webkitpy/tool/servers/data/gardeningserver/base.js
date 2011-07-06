abfc0371117d78389413fed75a0e0b1991ca40bf
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 03:15:24 +0000

    2011-07-05  Adam Barth  <abarth@webkit.org>
            Add first unit tests for garden-o-matic
            https://bugs.webkit.org/show_bug.cgi?id=63969
    
            Reviewed by Eric Seidel.
    
            I've also reformated base.js to match WebKit style, as discussed in the
            previous bug.
    
            * Scripts/webkitpy/tool/servers/data/gardeningserver/base.js:
            * Scripts/webkitpy/tool/servers/data/gardeningserver/base_unittests.js: Added.
            * Scripts/webkitpy/tool/servers/data/gardeningserver/run-unittests.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/base.js
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/base.js
@@ -1,28 +1,35 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var base = base || {};
 
-(function() {
-  base.joinPath = function(parent, child) {
+(function(){
+
+base.joinPath = function(parent, child)
+{
+    if (parent.length == 0)
+        return child;
     return parent + '/' + child;
-  }
+}
 
-  base.filterTree = function(tree, is_leaf, predicate) {
-    var filtered_tree = {};
+base.filterTree = function(tree, isLeaf, predicate)
+{
+    var filteredTree = {};
 
-    function walkSubtree(subtree, directory) {
-      for (var child_name in subtree) {
-        var child = subtree[child_name];
-        var child_path = base.joinPath(directory, child_name);
-        if (is_leaf(child)) {
-          if (predicate(child))
-            filtered_tree[child_path] = child;
-          continue;
+    function walkSubtree(subtree, directory)
+    {
+        for (var childName in subtree) {
+            var child = subtree[childName];
+            var childPath = base.joinPath(directory, childName);
+            if (isLeaf(child)) {
+                if (predicate(child))
+                    filteredTree[childPath] = child;
+                continue;
+            }
+            walkSubtree(child, childPath);
         }
-        walkSubtree(child, child_path);
-      }
     }
 
     walkSubtree(tree, '');
-    return filtered_tree;
-  }
+    return filteredTree;
+}
+
 })();
