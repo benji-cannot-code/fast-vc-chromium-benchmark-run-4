36c924ac3eda4c5dc99504e2023eafd90aa55529
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-06 17:59:59 +0000

    2011-07-06  Nate Chapin  <japhet@chromium.org>
            Don't let all subresources keep isLoadingInAPISense() from
            returning false, only requests that affect
            CachedResourceRequest::requestCount().
            https://bugs.webkit.org/show_bug.cgi?id=62066
    
            Reviewed by Adam Barth.
    
            * dom/Document.cpp:
            (WebCore::Document::Document):
            (WebCore::Document::dispatchWindowLoadEvent):
            * dom/Document.h:
            (WebCore::Document::loadEventFinished):
            * loader/DocumentLoader.cpp:
            (WebCore::DocumentLoader::isLoadingInAPISense):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -388,6 +388,7 @@ Document::Document(Frame* frame, const KURL& url, bool isXHTML, bool isHTML)
     , m_ignoreDestructiveWriteCount(0)
     , m_titleSetExplicitly(false)
     , m_updateFocusAppearanceTimer(this, &Document::updateFocusAppearanceTimerFired)
+    , m_loadEventFinished(false)
     , m_startTime(currentTime())
     , m_overMinimumLayoutThreshold(false)
     , m_extraLayoutDelay(0)
@@ -3401,6 +3402,7 @@ void Document::dispatchWindowLoadEvent()
     if (!domWindow)
         return;
     domWindow->dispatchLoadEvent();
+    m_loadEventFinished = true;
 }
 
 void Document::enqueueWindowEvent(PassRefPtr<Event> event)
