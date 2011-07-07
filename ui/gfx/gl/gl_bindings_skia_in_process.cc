45e3c20e367e81cd18d4f2461a413e87bcda9ab0
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 21:52:25 +0000

    When GL implementation is Mock or None return rather than setting the skia-gpu GL interface with an uninitialized binding. Fixes a -Werror issue. Review URL: http://codereview.chromium.org/7316023
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91763 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_bindings_skia_in_process.cc
+++ ui/gfx/gl/gl_bindings_skia_in_process.cc
@@ -444,7 +444,7 @@ void BindSkiaToInProcessGL() {
     switch (gfx::GetGLImplementation()) {
       case gfx::kGLImplementationNone:
         NOTREACHED();
-        break;
+        return;
       case gfx::kGLImplementationDesktopGL:
         binding = kDesktop_GrGLBinding;
         break;
@@ -456,7 +456,7 @@ void BindSkiaToInProcessGL() {
         break;
       case gfx::kGLImplementationMockGL:
         NOTREACHED();
-        break;
+        return;
     }
 
     static GrGLInterface host_gl_interface = {
