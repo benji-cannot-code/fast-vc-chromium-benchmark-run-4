44e92dbdb1c08c1612b54569764ba7d9d0215afe
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 16:22:25 +0000

    history: Remove unused bookmark constant.
    BUG=None
    TEST=None
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7316009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_history_backend.cc
+++ chrome/browser/history/in_memory_history_backend.cc
@@ -23,10 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace history {
 
-// If a page becomes starred we use this id in place of the real starred id.
-// See note in OnURLsStarred.
-static const StarID kBogusStarredID = 0x0FFFFFFF;
-
 InMemoryHistoryBackend::InMemoryHistoryBackend()
     : profile_(NULL) {
 }
