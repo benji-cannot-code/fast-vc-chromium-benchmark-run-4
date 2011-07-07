33d3947f0cb7aa1d56d474e06a2da255e128e165
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-07 22:09:36 +0000

    Patch by James Robinson <jamesr@chromium.org> on 2011-07-07 Reviewed by Kenneth Russell.
    Use v8::AdjustAmountOfExternalAllocatedMemory for ArrayBuffers
    https://bugs.webkit.org/show_bug.cgi?id=42912
    
    This calls v8's AdjustAmountOfExternalAllocatedMemory when ArrayBuffers are allocated/deallocated so that V8's
    garbage collection heuristics can account for the memory held by these objects.  On the new test page, this
    reduces the peak memory use from 5BG+ (or a crash in 32-bit systems) to <300MB.
    
    Test: WebCore/manual-tests/array-buffer-memory.html
    
    * html/canvas/ArrayBuffer.cpp:
    (WebCore::ArrayBuffer::~ArrayBuffer):
    (WebCore::ArrayBuffer::tryAllocate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90592 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/ArrayBuffer.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/ArrayBuffer.cpp
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ArrayBuffer.h"
 
+#if USE(V8)
+#include "V8Binding.h"
+#endif
+
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
@@ -77,6 +81,9 @@ unsigned ArrayBuffer::byteLength() const
 
 ArrayBuffer::~ArrayBuffer()
 {
+#if USE(V8)
+    v8::V8::AdjustAmountOfExternalAllocatedMemory(-m_sizeInBytes);
+#endif
     WTF::fastFree(m_data);
 }
 
@@ -92,8 +99,12 @@ void* ArrayBuffer::tryAllocate(unsigned numElements, unsigned elementByteSize)
         if (totalSize / numElements != elementByteSize)
             return 0;
     }
-    if (WTF::tryFastCalloc(numElements, elementByteSize).getValue(result))
+    if (WTF::tryFastCalloc(numElements, elementByteSize).getValue(result)) {
+#if USE(V8)
+        v8::V8::AdjustAmountOfExternalAllocatedMemory(numElements * elementByteSize);
+#endif
         return result;
+    }
     return 0;
 }
 
