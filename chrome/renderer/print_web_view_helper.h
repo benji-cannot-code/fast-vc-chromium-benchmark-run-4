7ba06bc042231890c7571d2ed262bdd618e05b32
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 01:06:40 +0000

    Print Preview: Add the ability to cancel an in-flight print preview.
    BUG=85103
    TEST=none
    
    Review URL: http://codereview.chromium.org/7104127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91645 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.h
+++ chrome/renderer/print_web_view_helper.h
@@ -83,6 +83,7 @@ class PrintWebViewHelper : public RenderViewObserver,
                            BlockScriptInitiatedPrinting);
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperTest, OnPrintPages);
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest, OnPrintPreview);
+  FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest, OnPrintPreviewCancel);
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest, OnPrintPreviewFail);
   FRIEND_TEST_ALL_PREFIXES(PrintWebViewHelperPreviewTest,
                            OnPrintForPrintPreview);
@@ -198,7 +199,6 @@ class PrintWebViewHelper : public RenderViewObserver,
   bool RenderPages(const PrintMsg_PrintPages_Params& params,
                    WebKit::WebFrame* frame,
                    WebKit::WebNode* node,
-                   bool send_expected_page_count,
                    int* page_count,
                    printing::Metafile* metafile,
                    bool is_preview);
@@ -255,6 +255,12 @@ class PrintWebViewHelper : public RenderViewObserver,
   void DisplayPrintJobError();
 
   void RequestPrintPreview();
+
+  // Called every time print preview renders a page. Notify the browser of the
+  // event and check if print preview should be cancelled. Returns false if
+  // print preview has been cancelled. |page_number| is 0-based, or negative to
+  // indicate its a cancel check only.
+  bool PreviewPageRendered(int page_number);
 
   WebKit::WebView* print_web_view_;
 
