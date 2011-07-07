a937c7e797efd81ddbc4c4bb5a0e79b25bdf1912
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 05:56:33 +0000

    Fix clang error.
    BUG=none
    TEST=none
    TBR=bolms@chromium.org
    Review URL: http://codereview.chromium.org/7321002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.cc
+++ chrome/common/extensions/extension.cc
@@ -221,6 +221,9 @@ Extension::InputComponentInfo::InputComponentInfo()
 
 Extension::InputComponentInfo::~InputComponentInfo() {}
 
+Extension::TtsVoice::TtsVoice() {}
+Extension::TtsVoice::~TtsVoice() {}
+
 //
 // Extension
 //
