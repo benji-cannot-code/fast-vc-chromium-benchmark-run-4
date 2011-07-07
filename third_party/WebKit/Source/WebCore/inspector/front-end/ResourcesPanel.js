c1e404b794a79c3b54560902665890efd7847364
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-07 11:33:41 +0000

    2011-07-07  Vsevolod Vlasov  <vsevik@chromium.org>
            Web Inspector: Resources panel shows only one resource when multiple resources have the same url.
            https://bugs.webkit.org/show_bug.cgi?id=63936
    
            Reviewed by Pavel Feldman.
    
            * http/tests/inspector/resource-tree/resource-tree-non-unique-url-expected.txt: Added.
            * http/tests/inspector/resource-tree/resource-tree-non-unique-url.html: Added.
            * http/tests/inspector/resource-tree/resource-tree-test.js:
            (initialize_ResourceTreeTest.InspectorTest.dumpResourcesTree):
            * http/tests/inspector/resource-tree/resources/resource-tree-non-unique-url-iframe.html: Added.
            * http/tests/inspector/resource-tree/resources/styles-non-unique-url.css: Added.
            (body):
    2011-07-07  Vsevolod Vlasov  <vsevik@chromium.org>
    
            Web Inspector: Resources panel shows only one resource when multiple resources have the same url.
            https://bugs.webkit.org/show_bug.cgi?id=63936
    
            Reviewed by Pavel Feldman.
    
            Test: http/tests/inspector/resource-tree/resource-tree-non-unique-url.html
    
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.FrameTreeElement.prototype.appendResource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourcesPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourcesPanel.js
@@ -1115,9 +1115,6 @@ WebInspector.FrameTreeElement.prototype = {
 
     appendResource: function(resource)
     {
-        if (this._treeElementForResource[resource.url])
-            return;
-
         var categoryName = resource.category.name;
         var categoryElement = resource.category === WebInspector.resourceCategories.documents ? this : this._categoryElements[categoryName];
         if (!categoryElement) {
