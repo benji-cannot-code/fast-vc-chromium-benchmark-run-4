8733608c933a95cb10cc761736c191fdef9f016c
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 18:28:34 +0000

    GTK: Now that we depend on gtk 2.18+, replace this pattern:
      if (visible)
        gtk_widget_show(...);
      else
        gtk_widget_hide(...);
    
    with:
    
      gtk_widget_set_visible(..., visible);
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7310016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_actions_toolbar_gtk.cc
+++ chrome/browser/ui/gtk/browser_actions_toolbar_gtk.cc
@@ -481,10 +481,7 @@ void BrowserActionsToolbarGtk::Observe(NotificationType type,
                                        const NotificationSource& source,
                                        const NotificationDetails& details) {
   DCHECK(NotificationType::BROWSER_THEME_CHANGED == type);
-  if (theme_service_->UsingNativeTheme())
-    gtk_widget_show(separator_);
-  else
-    gtk_widget_hide(separator_);
+  gtk_widget_set_visible(separator_, theme_service_->UsingNativeTheme());
 }
 
 void BrowserActionsToolbarGtk::SetupDrags() {
@@ -567,10 +564,7 @@ void BrowserActionsToolbarGtk::RemoveButtonForExtension(
 }
 
 void BrowserActionsToolbarGtk::UpdateVisibility() {
-  if (button_count() == 0)
-    gtk_widget_hide(widget());
-  else
-    gtk_widget_show(widget());
+  gtk_widget_set_visible(widget(), button_count() != 0);
 }
 
 bool BrowserActionsToolbarGtk::ShouldDisplayBrowserAction(
