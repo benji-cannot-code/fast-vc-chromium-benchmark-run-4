878076e15170516bf82e666c5a9d975bd0721d23
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-07 16:48:27 +0000

    2011-07-07  Andrey Kosyakov  <caseq@chromium.org>
            Web Inspector: active panel does not receive resize event when drawer is resized
            https://bugs.webkit.org/show_bug.cgi?id=64094
    
            Reviewed by Pavel Feldman.
    
            * inspector/front-end/Drawer.js:
            (WebInspector.Drawer.prototype._statusBarDragging):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Drawer.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Drawer.js
@@ -344,6 +344,8 @@ WebInspector.Drawer.prototype = {
 
         this._mainElement.style.bottom = height + "px";
         this.element.style.height = height + "px";
+        if (WebInspector.currentPanel)
+            WebInspector.currentPanel.resize();
 
         event.preventDefault();
         event.stopPropagation();
