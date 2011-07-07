13e7e3ed31e5ae25391b9cc77f9fa078212af8ac
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-07 22:37:55 +0000

    Switch rendering tree selection code to to new layout types https://bugs.webkit.org/show_bug.cgi?id=63762
    Reviewed by Eric Seidel.
    
    Switch selection getters and selection gap calculation methods over to
    new layout abstraction.
    
    No new tests, no functionality changes.
    
    * rendering/InlineTextBox.cpp:
    (WebCore::InlineTextBox::selectionTop):
    (WebCore::InlineTextBox::selectionBottom):
    (WebCore::InlineTextBox::selectionHeight):
    (WebCore::InlineTextBox::isSelected):
    * rendering/InlineTextBox.h:
    * rendering/RenderBR.h:
    (WebCore::RenderBR::selectionRectForRepaint):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::selectionGapRectsForRepaint):
    (WebCore::RenderBlock::selectionGaps):
    (WebCore::RenderBlock::inlineSelectionGaps):
    (WebCore::RenderBlock::blockSelectionGaps):
    (WebCore::RenderBlock::blockSelectionGap):
    (WebCore::RenderBlock::logicalLeftSelectionGap):
    (WebCore::RenderBlock::logicalRightSelectionGap):
    (WebCore::RenderBlock::logicalLeftSelectionOffset):
    (WebCore::RenderBlock::logicalRightSelectionOffset):
    (WebCore::RenderBlock::positionForPointWithInlineChildren):
    (WebCore::RenderBlock::desiredColumnWidth):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::selectionRectForRepaint):
    * rendering/RenderListMarker.cpp:
    (WebCore::RenderListMarker::selectionRectForRepaint):
    * rendering/RenderListMarker.h:
    * rendering/RenderObject.h:
    (WebCore::RenderObject::selectionRect):
    (WebCore::RenderObject::selectionRectForRepaint):
    * rendering/RenderReplaced.cpp:
    (WebCore::RenderReplaced::selectionRectForRepaint):
    * rendering/RenderReplaced.h:
    * rendering/RenderSelectionInfo.h:
    (WebCore::RenderSelectionInfo::RenderSelectionInfo):
    * rendering/RenderText.cpp:
    (WebCore::RenderText::selectionRectForRepaint):
    * rendering/RenderText.h:
    * rendering/RootInlineBox.cpp:
    (WebCore::RootInlineBox::selectionTop):
    (WebCore::RootInlineBox::selectionBottom):
    * rendering/RootInlineBox.h:
    (WebCore::RootInlineBox::selectionHeight):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderListMarker.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderListMarker.h
@@ -66,7 +66,7 @@ private:
     bool isText() const { return !isImage(); }
 
     virtual void setSelectionState(SelectionState);
-    virtual IntRect selectionRectForRepaint(RenderBoxModelObject* repaintContainer, bool clipToVisibleContent = true);
+    virtual LayoutRect selectionRectForRepaint(RenderBoxModelObject* repaintContainer, bool clipToVisibleContent = true);
     virtual bool canBeSelectionLeaf() const { return true; }
 
     void updateMargins();
