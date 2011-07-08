0e0c0aefc50f2bdedd09dd7267747089111250a6
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 11:16:24 +0000

    Web Inspector: Quotes are rendered as &quot; in the DOM tree https://bugs.webkit.org/show_bug.cgi?id=64154
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    Since we currently rely on setting textContent rather than innerHTML for the DOM tree elements,
    escapeHTML() calls unnecessarily HTML-escape certain characters in the DOM tree contents.
    
    * inspector/front-end/ElementsTreeOutline.js:
    (WebInspector.ElementsTreeElement.prototype._buildAttributeDOM):
    ():
    
    LayoutTests:
    
    * inspector/elements/elements-panel-structure-expected.txt:
    * inspector/elements/elements-panel-structure.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
@@ -1365,7 +1365,7 @@ WebInspector.ElementsTreeElement.prototype = {
         var hasText = (value.length > 0);
         var attrSpanElement = parentElement.createChild("span", "webkit-html-attribute");
         var attrNameElement = attrSpanElement.createChild("span", "webkit-html-attribute-name");
-        attrNameElement.textContent = name.escapeHTML();
+        attrNameElement.textContent = name;
 
         if (hasText)
             attrSpanElement.appendChild(document.createTextNode("=\u200B\""));
@@ -1375,7 +1375,7 @@ WebInspector.ElementsTreeElement.prototype = {
             value = value.replace(/([\/;:\)\]\}])/g, "$1\u200B");
             attrSpanElement.appendChild(linkify(rewrittenHref, value, "webkit-html-attribute-value", node.nodeName().toLowerCase() === "a"));
         } else {
-            value = value.escapeHTML().replace(/([\/;:\)\]\}])/g, "$1\u200B");
+            value = value.replace(/([\/;:\)\]\}])/g, "$1\u200B");
             var attrValueElement = attrSpanElement.createChild("span", "webkit-html-attribute-value");
             attrValueElement.textContent = value;
         }
@@ -1426,7 +1426,7 @@ WebInspector.ElementsTreeElement.prototype = {
                 break;
 
             case Node.ELEMENT_NODE:
-                var tagName = this.treeOutline.nodeNameToCorrectCase(node.nodeName()).escapeHTML();
+                var tagName = this.treeOutline.nodeNameToCorrectCase(node.nodeName());
                 if (this._elementCloseTag) {
                     this._buildTagDOM(info.titleDOM, tagName, true, true);
                     info.hasChildren = false;
@@ -1452,7 +1452,7 @@ WebInspector.ElementsTreeElement.prototype = {
                 // create a subtree for them
                 if (showInlineText) {
                     var textNodeElement = info.titleDOM.createChild("span", "webkit-html-text-node");
-                    textNodeElement.textContent = textChild.nodeValue().escapeHTML();
+                    textNodeElement.textContent = textChild.nodeValue();
                     info.titleDOM.appendChild(document.createTextNode("\u200B"));
                     this._buildTagDOM(info.titleDOM, tagName, true, false);
                     info.hasChildren = false;
@@ -1478,7 +1478,7 @@ WebInspector.ElementsTreeElement.prototype = {
                     } else {
                         info.titleDOM.appendChild(document.createTextNode("\""));
                         var textNodeElement = info.titleDOM.createChild("span", "webkit-html-text-node");
-                        textNodeElement.textContent = node.nodeValue().escapeHTML();
+                        textNodeElement.textContent = node.nodeValue();
                         info.titleDOM.appendChild(document.createTextNode("\""));
                     }
                 }
@@ -1486,7 +1486,7 @@ WebInspector.ElementsTreeElement.prototype = {
 
             case Node.COMMENT_NODE:
                 var commentElement = info.titleDOM.createChild("span", "webkit-html-comment");
-                commentElement.appendChild(document.createTextNode("<!--" + node.nodeValue().escapeHTML() + "-->"));
+                commentElement.appendChild(document.createTextNode("<!--" + node.nodeValue() + "-->"));
                 break;
 
             case Node.DOCUMENT_TYPE_NODE:
@@ -1507,10 +1507,10 @@ WebInspector.ElementsTreeElement.prototype = {
 
             case Node.CDATA_SECTION_NODE:
                 var cdataElement = info.titleDOM.createChild("span", "webkit-html-text-node");
-                cdataElement.appendChild(document.createTextNode("<![CDATA[" + node.nodeValue().escapeHTML() + "]]>"));
+                cdataElement.appendChild(document.createTextNode("<![CDATA[" + node.nodeValue() + "]]>"));
                 break;
             default:
-                var defaultElement = info.titleDOM.appendChild(document.createTextNode(this.treeOutline.nodeNameToCorrectCase(node.nodeName()).collapseWhitespace().escapeHTML()));
+                var defaultElement = info.titleDOM.appendChild(document.createTextNode(this.treeOutline.nodeNameToCorrectCase(node.nodeName()).collapseWhitespace()));
         }
 
         return info;
