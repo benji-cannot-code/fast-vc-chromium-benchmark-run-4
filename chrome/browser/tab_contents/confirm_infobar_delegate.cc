b00221ca5cd92840a60d59c569799f0cb37f385c
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 06:44:56 +0000

    Re-land r89864.  Hopefully this won't trigger the apparent perf regression now that we've tweaked things.  If it does, I'll continue to investigate.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91811 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/confirm_infobar_delegate.cc
+++ chrome/browser/tab_contents/confirm_infobar_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/tab_contents/confirm_infobar_delegate.h"
 
+#include "content/browser/tab_contents/navigation_details.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -49,6 +50,13 @@ bool ConfirmInfoBarDelegate::EqualsDelegate(InfoBarDelegate* delegate) const {
       delegate->AsConfirmInfoBarDelegate();
   return confirm_delegate &&
       (confirm_delegate->GetMessageText() == GetMessageText());
+}
+
+bool ConfirmInfoBarDelegate::ShouldExpire(
+    const content::LoadCommittedDetails& details) const {
+  if (details.did_replace_entry)
+    return false;
+  return InfoBarDelegate::ShouldExpire(details);
 }
 
 ConfirmInfoBarDelegate* ConfirmInfoBarDelegate::AsConfirmInfoBarDelegate() {
