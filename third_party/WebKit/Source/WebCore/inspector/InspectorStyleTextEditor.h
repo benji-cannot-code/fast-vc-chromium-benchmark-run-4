ae9e3251e5dce3349751b82794451a5da41d5074
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 14:34:55 +0000

    [Chromium] Unreviewed, clang build fix.
    * inspector/InspectorStyleTextEditor.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorStyleTextEditor.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorStyleTextEditor.h
@@ -35,8 +35,8 @@ namespace WebCore {
 
 #if ENABLE(INSPECTOR)
 
-class InspectorStyleProperty;
-class SourceRange;
+struct InspectorStyleProperty;
+struct SourceRange;
 
 typedef std::pair<String, String> NewLineAndWhitespace;
 
