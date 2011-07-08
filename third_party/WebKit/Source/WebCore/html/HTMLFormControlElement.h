cb36e7ad934995540538e2d74634d5149cc1c3e7
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 20:30:58 +0000

    Move selection related code from RenderTextControl to HTMLTextFormControlElement https://bugs.webkit.org/show_bug.cgi?id=64133
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore: 
    
    Moved selectionStart, selectionEnd, hasVisibleTextArea, setSelectionRange, setContainerAndOffsetForRange
    and selection from RenderTextControl.cpp to HTMLFormControlElement.cpp.
    
    This refactoring removes RenderTextControl's dependency on FrameSelection.
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::selectedText): Calls HTMLTextFromControl::selectedText.
    (WebCore::AccessibilityRenderObject::selectedTextRange): Calls selectionStart and selectionEnd.
    (WebCore::AccessibilityRenderObject::setSelectedTextRange): Ditto.
    * html/HTMLFormControlElement.cpp:
    (WebCore::HTMLTextFormControlElement::selectedText): Extracted from AccessibilityRenderObject::selectedText.
    (WebCore::hasVisibleTextArea):
    (WebCore::HTMLTextFormControlElement::setSelectionRange): Merged with the function of the same name in
    RenderTextControl.
    (WebCore::HTMLTextFormControlElement::selectionStart): Ditto.
    (WebCore::HTMLTextFormControlElement::selectionEnd): Ditto.
    (WebCore::setContainerAndOffsetForRange): Moved from RenderTextControl.cpp
    (WebCore::HTMLTextFormControlElement::selection): Merged with the function of the same name in RenderTextControl.
    (WebCore::HTMLTextFormControlElement::selectionChanged): Calls selectionStart and selectionEnd.
    * html/HTMLFormControlElement.h:
    (WebCore::HTMLTextFormControlElement::restoreCachedSelection): Moved from HTMLFormControlElement.cpp now that
    all functions are self-contained in HTMLTextFormControlElement.
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::setValue): Calls setSelectionRange.
    * rendering/RenderTextControl.cpp:
    (WebCore::RenderTextControl::textFormControlElement): Added.
    * rendering/RenderTextControl.h:
    
    Source/WebKit/qt: 
    
    Replaced calls to WebCore::setSelectionRange by calls to HTMLTextFormControlElement::setSelectionRange.
    
    * Api/qwebpage.cpp:
    (QWebPagePrivate::inputMethodEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90662 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFormControlElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLFormControlElement.h
@@ -208,6 +208,7 @@ public:
     void select();
     void setSelectionRange(int start, int end);
     PassRefPtr<Range> selection() const;
+    String selectedText() const;
 
     virtual void dispatchFormControlChangeEvent();
 
@@ -231,7 +232,7 @@ protected:
 
     void setTextAsOfLastFormControlChangeEvent(const String& text) { m_textAsOfLastFormControlChangeEvent = text; }
 
-    void restoreCachedSelection();
+    void restoreCachedSelection() { setSelectionRange(m_cachedSelectionStart, m_cachedSelectionEnd); }
     bool hasCachedSelectionStart() const { return m_cachedSelectionStart >= 0; }
     bool hasCachedSelectionEnd() const { return m_cachedSelectionEnd >= 0; }
 
