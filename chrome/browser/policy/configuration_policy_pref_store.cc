952689e0a893e2472f6788a7993efea5525dcc10
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 12:17:18 +0000

    Introduce a policy to control the maximal number of connections per proxy server.
    BUG=63658
    TEST=All policy unit_tests should pass.
    
    
    Review URL: http://codereview.chromium.org/7283018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store.cc
+++ chrome/browser/policy/configuration_policy_pref_store.cc
@@ -275,6 +275,8 @@ const ConfigurationPolicyPrefKeeper::PolicyToPreferenceMapEntry
     prefs::kEditBookmarksEnabled },
   { Value::TYPE_BOOLEAN, kPolicyAllowFileSelectionDialogs,
     prefs::kAllowFileSelectionDialogs },
+  { Value::TYPE_INTEGER, kPolicyMaxConnectionsPerProxy,
+    prefs::kMaxConnectionsPerProxy },
 
 #if defined(OS_CHROMEOS)
   { Value::TYPE_BOOLEAN, kPolicyChromeOsLockOnIdleSuspend,
@@ -1066,6 +1068,8 @@ ConfigurationPolicyPrefStore::GetChromePolicyDefinitionList() {
       key::kAllowFileSelectionDialogs },
     { kPolicyDiskCacheDir, Value::TYPE_STRING,
       key::kDiskCacheDir },
+    { kPolicyMaxConnectionsPerProxy, Value::TYPE_INTEGER,
+      key::kMaxConnectionsPerProxy },
 
 #if defined(OS_CHROMEOS)
     { kPolicyChromeOsLockOnIdleSuspend, Value::TYPE_BOOLEAN,
--- net/socket/client_socket_pool_manager.h
+++ net/socket/client_socket_pool_manager.h
@@ -52,6 +52,8 @@ class TransportClientSocketPool;
 
 struct SSLConfig;
 
+extern const int kDefaultMaxSocketsPerProxyServer;
+
 namespace internal {
 
 // A helper class for auto-deleting Values in the destructor.
