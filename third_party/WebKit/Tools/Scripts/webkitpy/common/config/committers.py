c144800e6e4defd53e87a7d666ddb48e1426fd31
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 13:19:25 +0000

    Reviewed by Andreas Kling.
    Adding myself as a reviewer.
    
    * Scripts/webkitpy/common/config/committers.py:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/config/committers.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/config/committers.py
@@ -265,7 +265,6 @@ committers_unable_to_review = [
     Committer("Yuta Kitamura", "yutak@chromium.org", "yutak"),
     Committer("Yuzo Fujishima", "yuzo@google.com", "yuzo"),
     Committer("Zhenyao Mo", "zmo@google.com", "zhenyao"),
-    Committer("Zoltan Herczeg", "zherczeg@webkit.org", "zherczeg"),
     Committer("Zoltan Horvath", ["zoltan@webkit.org", "hzoltan@inf.u-szeged.hu", "horvath.zoltan.6@stud.u-szeged.hu"], "zoltan"),
     Committer("Naoki Takano", "honten@chromium.org", "honten"),
 ]
@@ -370,6 +369,7 @@ reviewers_list = [
     Reviewer("Xan Lopez", ["xan.lopez@gmail.com", "xan@gnome.org", "xan@webkit.org"], "xan"),
     Reviewer("Yury Semikhatsky", "yurys@chromium.org", "yurys"),
     Reviewer("Zack Rusin", "zack@kde.org", "zackr"),
+    Reviewer("Zoltan Herczeg", "zherczeg@webkit.org", "zherczeg"),
 ]
 
 
