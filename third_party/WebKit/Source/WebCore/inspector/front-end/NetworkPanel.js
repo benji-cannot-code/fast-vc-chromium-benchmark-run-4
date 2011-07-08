f1d0c7fde349481fddbe4e5c0d25b0b6f16e82f9
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 15:53:56 +0000

    Web Inspector: NetworkPanel search failed if the matched sting is in the query part of url https://bugs.webkit.org/show_bug.cgi?id=64167
    Reviewed by Yury Semikhatsky.
    
    * inspector/front-end/NetworkPanel.js:
    (WebInspector.NetworkPanel.prototype._matchResource):
    (WebInspector.NetworkPanel.prototype._highlightNthMatchedResource):
    (WebInspector.NetworkDataGridNode.prototype._refreshNameCell):
    * inspector/front-end/Resource.js:
    (WebInspector.Resource.prototype.get folder):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
@@ -1118,7 +1118,7 @@ WebInspector.NetworkPanel.prototype = {
         if (!this._searchRegExp)
             return -1;
 
-        if ((!resource.displayName || !resource.displayName.match(this._searchRegExp)) && (!resource.path || !resource.path.match(this._searchRegExp)))
+        if ((!resource.displayName || !resource.displayName.match(this._searchRegExp)) && !resource.folder.match(this._searchRegExp))
             return -1;
 
         if (resource.identifier in this._matchedResourcesMap)
@@ -1169,7 +1169,7 @@ WebInspector.NetworkPanel.prototype = {
             return;
 
         var nameMatched = resource.displayName && resource.displayName.match(this._searchRegExp);
-        var pathMatched = resource.path && resource.path.match(this._searchRegExp);
+        var pathMatched = resource.path && resource.folder.match(this._searchRegExp);
         if (!nameMatched && pathMatched && !this._largerResourcesButton.toggled)
             this._toggleLargerResources();
 
@@ -1659,11 +1659,8 @@ WebInspector.NetworkDataGridNode.prototype = {
 
         var subtitle = this._resource.displayDomain;
 
-        if (this._resource.path && this._resource.lastPathComponent) {
-            var lastPathComponentIndex = this._resource.path.lastIndexOf("/" + this._resource.lastPathComponent);
-            if (lastPathComponentIndex != -1)
-                subtitle += this._resource.path.substring(0, lastPathComponentIndex);
-        }
+        if (this._resource.path)
+            subtitle += this._resource.folder;
 
         this._appendSubtitle(this._nameCell, subtitle);
         this._nameCell.title = this._resource.url;
