3399e97b101f5a662781a0f2322b6bd2f8020f77
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 17:54:05 +0000

    Source/JavaScriptCore: Rename "makeSecure" to "fill" and remove the support for displaying last character to avoid layering violatation. https://bugs.webkit.org/show_bug.cgi?id=59114
    Patch by Chang Shu <cshu@webkit.org> on 2011-07-08
    Reviewed by Alexey Proskuryakov.
    
    * JavaScriptCore.exp:
    * JavaScriptCore.order:
    * wtf/text/StringImpl.cpp:
    (WTF::StringImpl::fill):
    * wtf/text/StringImpl.h:
    * wtf/text/WTFString.h:
    (WTF::String::fill):
    
    Source/WebCore: Update calling sites after function renamed.
    https://bugs.webkit.org/show_bug.cgi?id=59114
    
    Patch by Chang Shu <cshu@webkit.org> on 2011-07-08
    Reviewed by Alexey Proskuryakov.
    
    No new tests, just refactoring.
    
    * editing/visible_units.cpp:
    (WebCore::previousBoundary):
    (WebCore::nextBoundary):
    * rendering/RenderText.cpp:
    (WebCore::RenderText::setTextInternal):
    
    Source/WebKit/win: Update calling sites after function renamed.
    https://bugs.webkit.org/show_bug.cgi?id=59114
    
    Patch by Chang Shu <cshu@webkit.org> on 2011-07-08
    Reviewed by Alexey Proskuryakov.
    
    * WebKitGraphics.cpp:
    (WebDrawText):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/text/StringImpl.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/text/StringImpl.h
@@ -285,9 +285,7 @@ public:
     PassRefPtr<StringImpl> lower();
     PassRefPtr<StringImpl> upper();
 
-    enum LastCharacterBehavior { ObscureLastCharacter, DisplayLastCharacter };
-
-    PassRefPtr<StringImpl> secure(UChar, LastCharacterBehavior = ObscureLastCharacter);
+    PassRefPtr<StringImpl> fill(UChar);
     PassRefPtr<StringImpl> foldCase();
 
     PassRefPtr<StringImpl> stripWhiteSpace();
