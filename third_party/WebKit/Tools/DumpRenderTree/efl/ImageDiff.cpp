cdbeb2455282b0341a62c150432599f6cbd90555
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-08 02:13:05 +0000

    [ImageDiff] Calculate/print difference right after reading baseline image. https://bugs.webkit.org/show_bug.cgi?id=64117
    Patch by Leandro Pereira <leandro@profusion.mobi> on 2011-07-07
    Reviewed by Kent Tamura.
    
    Otherwise, ImageDiff will block on fgets() until the universe ends or you ^C
    it.  Whichever happens first.
    
    * DumpRenderTree/efl/ImageDiff.cpp:
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/ImageDiff.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/ImageDiff.cpp
@@ -331,9 +331,9 @@ int main(int argc, char* argv[])
 
             if (!actualImage)
                 actualImage = readImageFromStdin(evas, imageSize);
-            else if (!baselineImage)
+            else if (!baselineImage) {
                 baselineImage = readImageFromStdin(evas, imageSize);
-            else {
+
                 printImageDifferences(baselineImage.get(), actualImage.get());
 
                 actualImage.clear();
