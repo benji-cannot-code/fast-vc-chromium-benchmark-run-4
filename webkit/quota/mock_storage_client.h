a32f48c345f598a2763aef9d26210089a680d9ad
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 11:07:14 +0000

    2nd try: Implement QM::GetOriginsModifiedSince for browser data deleter support
    Original review URL: http://codereview.chromium.org/7168019
    (reverted due to clang build breakage)
    
    BUG=86308
    TEST={QuotaDatabaseTest,QuotaManagerTest}.OriginModifiedSince
    
    Review URL: http://codereview.chromium.org/7331006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91835 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/mock_storage_client.h
+++ webkit/quota/mock_storage_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/task.h"
+#include "base/time.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/quota/quota_client.h"
 
@@ -39,6 +40,8 @@ class MockStorageClient : public QuotaClient {
       const GURL& origin_url, StorageType type, int64 delta);
 
   void AddOriginToErrorSet(const GURL& origin_url, StorageType type);
+
+  base::Time IncrementMockTime();
 
   // QuotaClient methods.
   virtual QuotaClient::ID id() const OVERRIDE;
@@ -78,6 +81,8 @@ class MockStorageClient : public QuotaClient {
   std::set<GetUsageCallback*> usage_callbacks_;
   std::set<GetOriginsCallback*> origins_callbacks_;
   std::set<DeletionCallback*> deletion_callbacks_;
+
+  int mock_time_counter_;
 
   ScopedRunnableMethodFactory<MockStorageClient> runnable_factory_;
 
--- webkit/quota/quota_types.h
+++ webkit/quota/quota_types.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_old.h"
 #include "base/stl_util-inl.h"
 
+class GURL;
+
 namespace quota {
 
 enum StorageType {
@@ -51,6 +53,7 @@ typedef Callback4<QuotaStatusCode,
 typedef Callback2<QuotaStatusCode,
                   int64>::Type AvailableSpaceCallback;
 typedef Callback1<QuotaStatusCode>::Type StatusCallback;
+typedef Callback1<const std::set<GURL>&>::Type GetOriginsCallback;
 
 // Simple template wrapper for a callback queue.
 template <typename CallbackType>
