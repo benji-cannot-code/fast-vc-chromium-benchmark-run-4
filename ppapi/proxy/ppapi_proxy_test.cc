64fcd38810e9648fce1ba208aac1c409a22f784f
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 16:23:36 +0000

    Porting ppapi_tests framework to postMessage.
    Some tests still rely on scripting, so we changed to using InstancePrivate (since scripting will disappear from Instance soon). Also use conditional compilation so that if compiled as untrusted with NaCl, the tests use Instance instead of InstancePrivate. This means that tests which rely on scripting aren't runnable in NaCl.
    
    I also added a gyp option: pepper_scripting. The default is that scripting is on in this CL, but it will make it easy to turn it off in local builds. Soon we'll switch the default to no scripting, and soon after we can remove the option entirely.
    
    BUG=82606
    TEST=these tests
    
    Review URL: http://codereview.chromium.org/7312008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91859 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppapi_proxy_test.cc
+++ ppapi/proxy/ppapi_proxy_test.cc
@@ -225,6 +225,7 @@ uint32 PluginProxyTestHarness::PluginDelegateMock::Register(
 void PluginProxyTestHarness::PluginDelegateMock::Unregister(
     uint32 plugin_dispatcher_id) {
 }
+
 // PluginProxyTest -------------------------------------------------------------
 
 PluginProxyTest::PluginProxyTest() {
--- ppapi/tests/testing_instance.h
+++ ppapi/tests/testing_instance.h
@@ -9,11 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ppapi/cpp/completion_callback.h"
+
+#if defined(__native_client__)
 #include "ppapi/cpp/instance.h"
+#else
+#include "ppapi/cpp/private/instance_private.h"
+#endif
 
 class TestCase;
 
-class TestingInstance : public pp::Instance {
+// In trusted builds, we use InstancePrivate and allow tests that use
+// synchronous scripting. NaCl does not support synchronous scripting.
+class TestingInstance : public
+#if defined(__native_client__)
+pp::Instance {
+#else
+pp::InstancePrivate {
+#endif
  public:
   explicit TestingInstance(PP_Instance instance);
   virtual ~TestingInstance();
@@ -21,7 +33,10 @@ class TestingInstance : public pp::Instance {
   // pp::Instance override.
   virtual bool Init(uint32_t argc, const char* argn[], const char* argv[]);
   virtual void DidChangeView(const pp::Rect& position, const pp::Rect& clip);
+
+#if !(defined __native_client__)
   virtual pp::Var GetInstanceObject();
+#endif
 
   // Outputs the information from one test run, using the format
   //   <test_name> [PASS|FAIL <error_message>]
@@ -46,6 +61,10 @@ class TestingInstance : public pp::Instance {
   // Passes the message_data through to the HandleMessage method on the
   // TestClass object that's associated with this instance.
   virtual void HandleMessage(const pp::Var& message_data);
+
+  const std::string& protocol() {
+    return protocol_;
+  }
 
  private:
   void ExecuteTests(int32_t unused);
@@ -79,6 +98,10 @@ class TestingInstance : public pp::Instance {
 
   // True if running in Native Client.
   bool nacl_mode_;
+
+  // String representing the protocol.  Used for detecting whether we're running
+  // with http.
+  std::string protocol_;
 };
 
 #endif  // PPAPI_TESTS_TESTING_INSTANCE_H_
