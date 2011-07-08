3777739ce50e1f81b679c073036b26871fa73989
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 00:37:33 +0000

    Made Print Preview tests autogenerated from print_preview.js.
    Now that the generator exists, make the print preview tests use it as well.  Since the test was flaky when I found it, porting FLAKY_ to the javascript test name.
    
    Since Print Preview needs the preloading, provided a way to tell the generator to not AddLibrary.
    
    BUG=82437
    R=thestig@chromium.org,dtseng@chromium.org
    TEST=browser_tests --gtest_filter=PrintPreview*.*
    
    
    Review URL: http://codereview.chromium.org/7304014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91787 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -2543,6 +2543,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'renderer/safe_browsing/render_view_fake_resources_test.h',
         'renderer/translate_helper_browsertest.cc',
         'test/automation/dom_automation_browsertest.cc',
+        'test/data/webui/print_preview.js',
         'test/data/webui/sample_pass.js',
         'test/gpu/gpu_browsertest.cc',
         'test/in_process_browser_test_browsertest.cc',
@@ -2737,7 +2738,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 '<(js2webui)',
               ],
               'outputs': [
-                '<(js2webui_out_dir)/chrome/<(rule_input_relpath)/<(RULE_INPUT_ROOT)-inl.h',
+                '<(js2webui_out_dir)/js2webui/chrome/<(rule_input_relpath)/<(RULE_INPUT_ROOT)-inl.h',
               ],
               'process_outputs_as_sources': 1,
               'action': [
--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -71,13 +71,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  })();
 
 // Tests.
-function testPrintPreview(printEnabled) {
-  var printButton = $('print-button');
-  assertTrue(printButton != null, 'printButton != null');
-  var cancelButton = $('cancel-button');
-  assertTrue(cancelButton != null, 'cancelButton != null');
+function FLAKY_TestPrinterList() {
   var printer_list = $('printer-list');
-  assertTrue(printEnabled, 'printEnabled');
   assertTrue(!!printer_list, 'printer_list');
   assertTrue(printer_list.options.length >= 2, 'printer-list has at least 2');
   expectEquals('FooName', printer_list.options[0].text, '0 text is FooName');
@@ -87,3 +82,6 @@ function testPrintPreview(printEnabled) {
   expectEquals('BarDevice', printer_list.options[1].value,
                '1 value is BarDevice');
 }
+
+var test_fixture = 'PrintPreviewWebUITest';
+var test_add_library = false;
