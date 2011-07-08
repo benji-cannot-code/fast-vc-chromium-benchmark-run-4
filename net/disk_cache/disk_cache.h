825210b604b21f5970973b73ff446c5f8994b654
marja@google.com <marja@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 15:40:08 +0000

    Updating a link in the comments.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7328019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91856 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/disk_cache.h
+++ net/disk_cache/disk_cache.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Defines the public interface of the disk cache. For more details see
-// http://dev.chromium.org/developers/design-documents/disk-cache
+// http://dev.chromium.org/developers/design-documents/network-stack/disk-cache
 
 #ifndef NET_DISK_CACHE_DISK_CACHE_H_
 #define NET_DISK_CACHE_DISK_CACHE_H_
