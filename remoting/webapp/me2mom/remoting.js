8a9676f6da63ad798ab2f942d13e2b5cb4941b88
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 17:37:22 +0000

    Chromoting: Move set(Client|Host)Mode into remoting namespace. Likewise for cancelPendingOperation. This fixes failing JS calls from choice.html (e.g., "OK" button for "Invalid Access Code")
    Also replace occurrences of:
      function f() {}
      remoting.f = f;
    with:
      remoting.f = function() {}
    to be consistent.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7329011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91871 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -175,8 +175,8 @@ remoting.init = function() {
 
   updateAuthStatus_();
   refreshEmail_();
-  setHostMode('unshared');
-  setClientMode('unconnected');
+  remoting.setHostMode('unshared');
+  remoting.setClientMode('unconnected');
   setGlobalMode(getAppStartupMode());
   addClass(document.getElementById('loading-mode'), 'hidden');
   removeClass(document.getElementById('choice-mode'), 'hidden');
@@ -225,14 +225,14 @@ function setGlobalModePersistent(mode) {
   }
 }
 
-function setHostMode(mode) {
+remoting.setHostMode = function(mode) {
   var section = document.getElementById('host-panel');
   var modes = section.getElementsByClassName('mode');
   remoting.debug.log('Host mode: ' + mode);
   setMode_(mode, modes);
 }
 
-function setClientMode(mode) {
+remoting.setClientMode = function(mode) {
   var section = document.getElementById('client-panel');
   var modes = section.getElementsByClassName('mode');
   remoting.debug.log('Client mode: ' + mode);
@@ -246,7 +246,7 @@ function showWaiting_() {
   document.getElementById('cancel-button').disabled = false;
 }
 
-function tryShare() {
+remoting.tryShare = function() {
   remoting.debug.log('Attempting to share...');
   if (remoting.oauth2.needsNewAccessToken()) {
     remoting.debug.log('Refreshing token...');
@@ -256,7 +256,7 @@ function tryShare() {
         showShareError_('unable-to-get-token');
         throw 'Unable to get access token';
       }
-      tryShare();
+      remoting.tryShare();
     });
     return;
   }
@@ -274,13 +274,12 @@ function tryShare() {
   plugin.connect(getEmail(),
                  'oauth2:' + remoting.oauth2.getAccessToken());
 }
-remoting.tryShare = tryShare;
 
 function onStateChanged_() {
   var plugin = document.getElementById(remoting.HOST_PLUGIN_ID);
   var state = plugin.state;
   if (state == plugin.REQUESTED_ACCESS_CODE) {
-    setHostMode('preparing-to-share');
+    remoting.setHostMode('preparing-to-share');
   } else if (state == plugin.RECEIVED_ACCESS_CODE) {
     var accessCode = plugin.accessCode;
     var accessCodeDisplay = document.getElementById('access-code-display');
@@ -292,12 +291,12 @@ function onStateChanged_() {
       nextFourDigits.innerText = accessCode.substring(i, i + kDigitsPerGroup);
       accessCodeDisplay.appendChild(nextFourDigits);
     }
-    setHostMode('ready-to-share');
+    remoting.setHostMode('ready-to-share');
   } else if (state == plugin.CONNECTED) {
-    setHostMode('shared');
+    remoting.setHostMode('shared');
   } else if (state == plugin.DISCONNECTED) {
     setGlobalMode(remoting.AppMode.HOST);
-    setHostMode('unshared');
+    remoting.setHostMode('unshared');
     plugin.parentNode.removeChild(plugin);
   } else {
     remoting.debug.log('Unknown state -> ' + state);
@@ -316,15 +315,14 @@ function showShareError_(errorCode) {
   var errorDiv = document.getElementById(errorCode);
   errorDiv.style.display = 'block';
   remoting.debug.log("Sharing error: " + errorCode);
-  setHostMode('share-failed');
+  remoting.setHostMode('share-failed');
 }
 
-function cancelShare() {
+remoting.cancelShare = function() {
   remoting.debug.log('Canceling share...');
   var plugin = document.getElementById(remoting.HOST_PLUGIN_ID);
   plugin.disconnect();
 }
-remoting.cancelShare = cancelShare;
 
 /**
  * Show a client message that stays on the screeen until the state changes.
@@ -420,7 +418,7 @@ function showConnectError_(responseCode, responseString) {
     responseNode.innerText = responseString + ' (' + responseCode + ')';
   }
   remoting.accessCode = '';
-  setClientMode('connect-failed');
+  remoting.setClientMode('connect-failed');
 }
 
 function parseServerResponse_(xhr) {
@@ -455,14 +453,14 @@ function resolveSupportId(supportId) {
       headers);
 }
 
-function tryConnect() {
+remoting.tryConnect = function() {
   if (remoting.oauth2.needsNewAccessToken()) {
     remoting.oauth2.refreshAccessToken(function() {
       if (remoting.oauth2.needsNewAccessToken()) {
         // If we still need it, we're going to infinite loop.
         throw 'Unable to get access token.';
       }
-      tryConnect();
+      remoting.tryConnect();
     });
     return;
   }
@@ -474,16 +472,15 @@ function tryConnect() {
     showConnectError_(404);
   } else {
     var supportId = remoting.accessCode.substring(0, kSupportIdLen);
-    setClientMode('connecting');
+    remoting.setClientMode('connecting');
     resolveSupportId(supportId);
   }
 }
-remoting.tryConnect = tryConnect;
 
-function cancelPendingOperation() {
+remoting.cancelPendingOperation = function() {
   document.getElementById('cancel-button').disabled = true;
   if (remoting.currentMode == remoting.AppMode.HOST) {
-    cancelShare();
+    remoting.cancelShare();
   }
 }
 
