59e0958205c8cf260446de9a9f9a8ce318f54a01
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-08 16:51:32 +0000

    Move chromedriver tests to test subdirectory. Add script to run all chromedriver tests. Delete unused test script. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/7331004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91864 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/webdriver/test/run_chromedriver_tests.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs all of the ChromeDriver tests.
+
+For ChromeDriver documentation, refer to:
+  http://dev.chromium.org/developers/testing/webdriver-for-chrome
+"""
+
+import unittest
+from gtest_text_test_runner import GTestTextTestRunner
+
+if __name__ == '__main__':
+  unittest.main(module='chromedriver_tests',
+                testRunner=GTestTextTestRunner(verbosity=1))
