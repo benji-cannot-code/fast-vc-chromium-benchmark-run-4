f1ecd5824c72529c1b96f59d3ea9e6098e57b9e1
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-09 02:00:39 +0000

    Update unit test to fix Mac build. Review URL: http://codereview.chromium.org/7331027
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91937 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_menu_bridge_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_menu_bridge_unittest.mm
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TestBookmarkMenuBridge : public BookmarkMenuBridge {
  public:
   TestBookmarkMenuBridge(Profile* profile)
-      : BookmarkMenuBridge(profile),
-        menu_([[NSMenu alloc] initWithTitle:@"test"]) {
+      :  menu_([[NSMenu alloc] initWithTitle:@"test"]),
+         BookmarkMenuBridge(profile, menu_) {
   }
   virtual ~TestBookmarkMenuBridge() {}
 
