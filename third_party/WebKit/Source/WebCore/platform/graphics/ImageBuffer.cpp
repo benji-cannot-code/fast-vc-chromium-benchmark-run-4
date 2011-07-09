62a217fd317eff0a33732e617c1132d5b43f4827
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-09 06:05:14 +0000

    Refactoring luminance code in RenderSVGResourceMasker https://bugs.webkit.org/show_bug.cgi?id=64146
    Reviewed by Simon Fraser.
    
    Moved luminance calculcation code to ImageBuffer. The code is doing pixel manipulations and can now get replaced
    by platform specific algorithms in the ImmageBuffer*.cpp files.
    
    No change of functionality. No new tests.
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/graphics/ImageBuffer.cpp:
    (WebCore::ImageBuffer::transformColorSpace):
    (WebCore::ImageBuffer::genericConvertToLuminanceMask):
    (WebCore::ImageBuffer::convertToLuminanceMask):
    * platform/graphics/ImageBuffer.h:
    * rendering/svg/RenderSVGResourceMasker.cpp:
    (WebCore::RenderSVGResourceMasker::drawContentIntoMaskImage):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2009 Dirk Schulze <krit@webkit.org>
+ * Copyright (C) Research In Motion Limited 2011. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,19 +28,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ImageBuffer.h"
 
-#if !USE(CG)
-
-#include <math.h>
+#include <wtf/MathExtras.h>
 
 namespace WebCore {
 
+#if !USE(CG)
 void ImageBuffer::transformColorSpace(ColorSpace srcColorSpace, ColorSpace dstColorSpace)
 {
     if (srcColorSpace == dstColorSpace)
         return;
 
     // only sRGB <-> linearRGB are supported at the moment
-    if ((srcColorSpace != ColorSpaceLinearRGB && srcColorSpace != ColorSpaceDeviceRGB) 
+    if ((srcColorSpace != ColorSpaceLinearRGB && srcColorSpace != ColorSpaceDeviceRGB)
         || (dstColorSpace != ColorSpaceLinearRGB && dstColorSpace != ColorSpaceDeviceRGB))
         return;
 
@@ -67,7 +67,32 @@ void ImageBuffer::transformColorSpace(ColorSpace srcColorSpace, ColorSpace dstCo
         platformTransformColorSpace(m_deviceRgbLUT);
     }
 }
+#endif // USE(CG)
 
+inline void ImageBuffer::genericConvertToLuminanceMask()
+{
+    IntRect luminanceRect(IntPoint(), size());
+    RefPtr<ByteArray> srcPixelArray = getUnmultipliedImageData(luminanceRect);
+    
+    unsigned pixelArrayLength = srcPixelArray->length();
+    for (unsigned pixelOffset = 0; pixelOffset < pixelArrayLength; pixelOffset += 4) {
+        unsigned char a = srcPixelArray->get(pixelOffset + 3);
+        if (!a)
+            continue;
+        unsigned char r = srcPixelArray->get(pixelOffset);
+        unsigned char g = srcPixelArray->get(pixelOffset + 1);
+        unsigned char b = srcPixelArray->get(pixelOffset + 2);
+        
+        double luma = (r * 0.2125 + g * 0.7154 + b * 0.0721) * ((double)a / 255.0);
+        srcPixelArray->set(pixelOffset + 3, luma);
+    }
+    putUnmultipliedImageData(srcPixelArray.get(), luminanceRect.size(), luminanceRect, IntPoint());
 }
 
-#endif // USE(CG)
+void ImageBuffer::convertToLuminanceMask()
+{
+    // Add platform specific functions with platformConvertToLuminanceMask here later.
+    genericConvertToLuminanceMask();
+}
+
+}
