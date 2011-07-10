4e92764992d073701b52b44677fd43336b6131d8
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-10 19:20:37 +0000

    Switch updateHitTestResult to to new layout types https://bugs.webkit.org/show_bug.cgi?id=64209
    Reviewed by Eric Seidel.
    
    No new tests, no functionality changes.
    
    * rendering/EllipsisBox.cpp:
    (WebCore::EllipsisBox::nodeAtPoint):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::hitTestFloats):
    (WebCore::RenderBlock::updateHitTestResult):
    * rendering/RenderBlock.h:
    * rendering/RenderInline.cpp:
    (WebCore::RenderInline::updateHitTestResult):
    * rendering/RenderInline.h:
    * rendering/RenderLineBoxList.cpp:
    (WebCore::RenderLineBoxList::hitTest):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::updateHitTestResult):
    * rendering/RenderObject.h:
    * rendering/RenderView.cpp:
    (WebCore::RenderView::updateHitTestResult):
    * rendering/RenderView.h:
    * rendering/svg/RenderSVGContainer.cpp:
    (WebCore::RenderSVGContainer::nodeAtFloatPoint):
    * rendering/svg/RenderSVGImage.cpp:
    (WebCore::RenderSVGImage::nodeAtFloatPoint):
    * rendering/svg/RenderSVGPath.cpp:
    (WebCore::RenderSVGPath::nodeAtFloatPoint):
    * rendering/svg/RenderSVGRoot.cpp:
    (WebCore::RenderSVGRoot::nodeAtPoint):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGImage.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGImage.cpp
@@ -158,7 +158,7 @@ bool RenderSVGImage::nodeAtFloatPoint(const HitTestRequest& request, HitTestResu
 
         if (hitRules.canHitFill) {
             if (m_objectBoundingBox.contains(localPoint)) {
-                updateHitTestResult(result, roundedIntPoint(localPoint));
+                updateHitTestResult(result, roundedLayoutPoint(localPoint));
                 return true;
             }
         }
