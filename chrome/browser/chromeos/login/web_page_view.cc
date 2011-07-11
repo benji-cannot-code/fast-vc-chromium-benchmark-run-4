2faba0c44f1aa479de959209a3e31b2a89ec94ae
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-11 04:29:22 +0000

    fix compile error that just started happening on linux views builders, not sure why
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/web_page_view.cc
+++ chrome/browser/chromeos/login/web_page_view.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::TimeDelta;
 using views::Label;
 using views::View;
-using webkit_glue::FormData;
 
 namespace chromeos {
 
