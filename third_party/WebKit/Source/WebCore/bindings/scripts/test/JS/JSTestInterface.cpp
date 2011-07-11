efbcaf5e6bbdeb4452b4f5474bcf9bbd5ae14a6c
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-11 02:55:23 +0000

    Remove array size from generated JSC binding tables https://bugs.webkit.org/show_bug.cgi?id=64229
    Reviewed by Brent Fulgham.
    
    Because many items in the array are surrounded with #if(ENABLE_XXX) lines,
    the array size isn't fix and correct in all cases.
    So let the compiler calculate the correct size itself.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateHashTable):
    * bindings/scripts/test/JS/JSTestInterface.cpp:
    * bindings/scripts/test/JS/JSTestMediaQueryListListener.cpp:
    * bindings/scripts/test/JS/JSTestObj.cpp:
    * bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
@@ -41,7 +41,7 @@ ASSERT_CLASS_FITS_IN_CELL(JSTestInterface);
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestInterfaceTableValues[2] =
+static const HashTableValue JSTestInterfaceTableValues[] =
 {
     { "constructor", DontEnum | ReadOnly, (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestInterfaceConstructor), (intptr_t)0 THUNK_GENERATOR(0) },
     { 0, 0, 0, 0 THUNK_GENERATOR(0) }
@@ -56,7 +56,7 @@ static JSC_CONST_HASHTABLE HashTable JSTestInterfaceTable = { 2, 1, JSTestInterf
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestInterfaceConstructorTableValues[1] =
+static const HashTableValue JSTestInterfaceConstructorTableValues[] =
 {
     { 0, 0, 0, 0 THUNK_GENERATOR(0) }
 };
@@ -120,7 +120,7 @@ ConstructType JSTestInterfaceConstructor::getConstructData(ConstructData& constr
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestInterfacePrototypeTableValues[1] =
+static const HashTableValue JSTestInterfacePrototypeTableValues[] =
 {
     { 0, 0, 0, 0 THUNK_GENERATOR(0) }
 };
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp
@@ -42,7 +42,7 @@ ASSERT_CLASS_FITS_IN_CELL(JSTestSerializedScriptValueInterface);
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestSerializedScriptValueInterfaceTableValues[3] =
+static const HashTableValue JSTestSerializedScriptValueInterfaceTableValues[] =
 {
     { "value", DontDelete | ReadOnly, (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestSerializedScriptValueInterfaceValue), (intptr_t)0 THUNK_GENERATOR(0) },
     { "constructor", DontEnum | ReadOnly, (intptr_t)static_cast<PropertySlot::GetValueFunc>(jsTestSerializedScriptValueInterfaceConstructor), (intptr_t)0 THUNK_GENERATOR(0) },
@@ -58,7 +58,7 @@ static JSC_CONST_HASHTABLE HashTable JSTestSerializedScriptValueInterfaceTable =
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestSerializedScriptValueInterfaceConstructorTableValues[1] =
+static const HashTableValue JSTestSerializedScriptValueInterfaceConstructorTableValues[] =
 {
     { 0, 0, 0, 0 THUNK_GENERATOR(0) }
 };
@@ -106,7 +106,7 @@ bool JSTestSerializedScriptValueInterfaceConstructor::getOwnPropertyDescriptor(E
 #define THUNK_GENERATOR(generator)
 #endif
 
-static const HashTableValue JSTestSerializedScriptValueInterfacePrototypeTableValues[1] =
+static const HashTableValue JSTestSerializedScriptValueInterfacePrototypeTableValues[] =
 {
     { 0, 0, 0, 0 THUNK_GENERATOR(0) }
 };
