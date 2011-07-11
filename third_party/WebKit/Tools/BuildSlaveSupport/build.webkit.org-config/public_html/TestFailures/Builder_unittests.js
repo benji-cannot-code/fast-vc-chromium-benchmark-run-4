73ab95e6a08afde6cace8d96828aa4ecea4e4eab
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-11 19:20:49 +0000

    Don't count leaks as test failures on TestFailures's front page
    As a bonus, this patch adds our first unit test.
    
    Fixes <http://webkit.org/b/64303> REGRESSION (r90489): TestFailures page says far more tests
    are failing on the Leaks bot than actually are
    
    Reviewed by Adam Barth.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder.js:
    (Builder.prototype.getNumberOfFailingTests): Exclude lines containing "leak" when summing
    failure counts.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder_unittests.js:
    Added. Contains a simple test to show that leaks aren't counted as test failures.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/run-unittests.html:
    Added. This is the test harness.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Builder_unittests.js
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+module("Builder");
+
+test("getNumberOfFailingTests from Leaks bot", 4, function() {
+    var mockBuildbot = {};
+    mockBuildbot.baseURL = 'http://example.com/';
+
+    var builder = new Builder('test builder', mockBuildbot);
+
+    var realGetResource = window.getResource;
+    window.getResource = function(url, successCallback, errorCallback) {
+        var mockXHR = {};
+        mockXHR.responseText = JSON.stringify({
+            steps: [
+                {
+                    name: 'layout-test',
+                    isStarted: true,
+                    results: [
+                        2,
+                        [
+                            "2178 total leaks found!", 
+                            "2 test cases (<1%) had incorrect layout", 
+                            "2 test cases (<1%) crashed",
+                        ],
+                    ],
+                },
+            ],
+        });
+
+        successCallback(mockXHR);
+    };
+
+    // FIXME: It's lame to be modifying singletons like this. We should get rid
+    // of our singleton usage entirely!
+    var realPersistentCache = window.PersistentCache;
+    window.PersistentCache = {
+        contains: function() { return false; },
+        set: function() { },
+        get: function() { },
+    };
+
+    builder.getNumberOfFailingTests(1, function(failureCount, tooManyFailures) {
+        equals(failureCount, 4);
+        equals(tooManyFailures, false);
+    });
+
+    window.getResource = realGetResource;
+    equals(window.getResource, realGetResource);
+    window.PersistentCache = realPersistentCache;
+    equals(window.PersistentCache, realPersistentCache);
+});
