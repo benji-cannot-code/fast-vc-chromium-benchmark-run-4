6a766dc2b5bfc3670cd10182761143dbb7f51cc2
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-11 18:58:39 +0000

    Add some basic UI for showing regression ranges https://bugs.webkit.org/show_bug.cgi?id=64243
    Reviewed by Eric Seidel.
    
    This UI is all temporary.  It exists mostly just to have some buttons
    to click to exercise the code.
    
    * Scripts/webkitpy/tool/servers/data/gardeningserver/main.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/results.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/results_unittests.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/ui_unittests.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/results_unittests.js
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/results_unittests.js
@@ -139,7 +139,7 @@ NetworkSimulator.prototype.runTest = function(testCase)
 }
 
 test("regressionRangeForFailure", 3, function() {
-    simulator = new NetworkSimulator();
+    var simulator = new NetworkSimulator();
 
     var keyMap = {
         "agx0ZXN0LXJlc3VsdHNyEAsSCFRlc3RGaWxlGLncUAw": {
@@ -213,8 +213,8 @@ test("regressionRangeForFailure", 3, function() {
     };
     simulator.runTest(function() {
         results.regressionRangeForFailure("Mock Builder", "userscripts/another-test.html", function(oldestFailingRevision, newestPassingRevision) {
-            equals(oldestFailingRevision, "90426");
-            equals(newestPassingRevision, "90424");
+            equals(oldestFailingRevision, 90426);
+            equals(newestPassingRevision, 90424);
         });
     });
 });
