6ce7b440fe73a4bdd90f26170b19936f5bfb6e53
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-11 20:25:18 +0000

    Tweak IsExtensionEnabled and consolidate callers of related functions
    BUG=none
    TEST=ExtensionService*, ExtensionPrefs*
    
    Review URL: http://codereview.chromium.org/7330029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92047 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -1482,12 +1482,14 @@ TEST_F(ExtensionServiceTest, GrantedAPIAndHostPermissions) {
   extension = service_->disabled_extensions()->at(0);
 
   ASSERT_TRUE(prefs->GetExtensionState(extension_id) == Extension::DISABLED);
+  ASSERT_FALSE(service_->IsExtensionEnabled(extension_id));
   ASSERT_TRUE(prefs->DidExtensionEscalatePermissions(extension_id));
 
   // Now grant and re-enable the extension, making sure the prefs are updated.
   service_->GrantPermissionsAndEnableExtension(extension);
 
   ASSERT_TRUE(prefs->GetExtensionState(extension_id) == Extension::ENABLED);
+  ASSERT_TRUE(service_->IsExtensionEnabled(extension_id));
   ASSERT_FALSE(prefs->DidExtensionEscalatePermissions(extension_id));
 
   scoped_ptr<ExtensionPermissionSet> current_perms(
@@ -1524,12 +1526,13 @@ TEST_F(ExtensionServiceTest, GrantedAPIAndHostPermissions) {
   extension = service_->disabled_extensions()->at(0);
 
   ASSERT_TRUE(prefs->GetExtensionState(extension_id) == Extension::DISABLED);
+  ASSERT_FALSE(service_->IsExtensionEnabled(extension_id));
   ASSERT_TRUE(prefs->DidExtensionEscalatePermissions(extension_id));
 
   // Now grant and re-enable the extension, making sure the prefs are updated.
   service_->GrantPermissionsAndEnableExtension(extension);
 
-  ASSERT_TRUE(prefs->GetExtensionState(extension_id) == Extension::ENABLED);
+  ASSERT_TRUE(service_->IsExtensionEnabled(extension_id));
   ASSERT_FALSE(prefs->DidExtensionEscalatePermissions(extension_id));
 
   current_perms.reset(prefs->GetGrantedPermissions(extension_id));
@@ -2184,6 +2187,7 @@ TEST_F(ExtensionServiceTest, UpdatePendingTheme) {
 
   EXPECT_EQ(Extension::ENABLED,
             service_->extension_prefs()->GetExtensionState(extension->id()));
+  EXPECT_TRUE(service_->IsExtensionEnabled(theme_crx));
 }
 
 #if defined(OS_CHROMEOS)
@@ -2212,6 +2216,7 @@ TEST_F(ExtensionServiceTest, MAYBE_UpdatePendingExternalCrx) {
 
   EXPECT_EQ(Extension::ENABLED,
             service_->extension_prefs()->GetExtensionState(extension->id()));
+  EXPECT_TRUE(service_->IsExtensionEnabled(extension->id()));
   EXPECT_FALSE(service_->IsIncognitoEnabled(extension->id()));
 }
 
