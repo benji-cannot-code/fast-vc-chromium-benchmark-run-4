720f1b4d109918b5e392906a19a2118a4b9dd95e
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 18:09:46 +0000

    Clean up users of a deprecated base::LaunchApp API.
    BUG=88990
    
    Review URL: http://codereview.chromium.org/7258005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/platform_util_linux.cc
+++ chrome/browser/platform_util_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,7 +38,10 @@ void XDGUtil(const std::string& util, const std::string& arg) {
 
   base::file_handle_mapping_vector no_files;
   base::ProcessHandle handle;
-  if (base::LaunchApp(argv, env, no_files, false, &handle))
+  base::LaunchOptions options;
+  options.process_handle = &handle;
+  options.environ = &env;
+  if (base::LaunchProcess(argv, options))
     ProcessWatcher::EnsureProcessGetsReaped(handle);
 }
 
