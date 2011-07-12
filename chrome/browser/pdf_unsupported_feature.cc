6c2fbad4fc5056a3e0fc5043826cddc467ef13d2
aayushkumar@chromium.org <aayushkumar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 12:54:48 +0000

    Adobe reader actually becomes default browser when user says so!
    BUG=77856
    TEST=
    1) Ensure that the chrome pdf viewer plugin is enabled and the adobe reader is disabled.
    2) Open a pdf that has to be opened in adobe reader (eg. http://www.irs.gov/pub/irs-pdf/p17.pdf)
    3) Agree to open this pdf in adobe reader.  You will also get an option asking if you want adobe reader as your default reader.  Accept.
    4) Restart the browser.  Ensure that adobe reader plugin is still your default pdf viewer.
    
    Review URL: http://codereview.chromium.org/7285024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/pdf_unsupported_feature.cc
+++ chrome/browser/pdf_unsupported_feature.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "base/version.h"
+#include "chrome/browser/plugin_updater.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_contents/chrome_interstitial_page.h"
@@ -112,8 +113,10 @@ void PDFEnableAdobeReaderInfoBarDelegate::OnYes() {
   UserMetrics::RecordAction(UserMetricsAction("PDF_EnableReaderInfoBarOK"));
   webkit::npapi::PluginList::Singleton()->EnableGroup(false,
       ASCIIToUTF16(chrome::ChromeContentClient::kPDFPluginName));
-  webkit::npapi::PluginList::Singleton()->EnableGroup(true,
+  PluginUpdater* plugin_updater = PluginUpdater::GetInstance();
+  plugin_updater->EnablePluginGroup(true,
       ASCIIToUTF16(webkit::npapi::PluginGroup::kAdobeReaderGroupName));
+  plugin_updater->UpdatePreferences(tab_contents_->profile(), 0);
 }
 
 void PDFEnableAdobeReaderInfoBarDelegate::OnNo() {
