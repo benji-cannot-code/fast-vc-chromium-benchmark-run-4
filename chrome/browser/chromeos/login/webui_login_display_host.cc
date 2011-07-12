5462c2aa68310300765209e0f91b8a7527a12611
nkostylev@google.com <nkostylev@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 09:10:09 +0000

    [cros] Move status area into separate window for cros OOBE/signin WebUI.
    BUG=chromium-os:
    TEST=Manual, loaded with --webui-login and --allow-webui-oobe, status area is fully functional.
    
    Review URL: http://codereview.chromium.org/7331013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92129 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_display_host.cc
+++ chrome/browser/chromeos/login/webui_login_display_host.cc
@@ -61,9 +61,13 @@ void WebUILoginDisplayHost::SetShutdownButtonEnabled(bool enable) {
 }
 
 void WebUILoginDisplayHost::SetStatusAreaEnabled(bool enable) {
+  if (login_view_)
+    login_view_->SetStatusAreaEnabled(enable);
 }
 
 void WebUILoginDisplayHost::SetStatusAreaVisible(bool visible) {
+  if (login_view_)
+    login_view_->SetStatusAreaVisible(visible);
 }
 
 void WebUILoginDisplayHost::ShowBackground() {
@@ -106,6 +110,7 @@ void WebUILoginDisplayHost::LoadURL(const GURL& url) {
 
     login_window_->Show();
     WebUILoginDisplay::GetInstance()->set_login_window(login_window_);
+    login_view_->OnWindowCreated();
   }
   login_view_->LoadURL(url);
 }
