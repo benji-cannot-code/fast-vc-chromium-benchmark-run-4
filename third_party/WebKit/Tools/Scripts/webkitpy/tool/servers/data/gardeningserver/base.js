9460a7c06c804c5c5c439e84fc836cdc714cde9b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-12 23:31:05 +0000

    garden-o-matic should display regression ranges https://bugs.webkit.org/show_bug.cgi?id=64407
    Reviewed by Dimitri Glazkov.
    
    This patch computes a regression range for a failure by intersecting
    the regression ranges seen by the various bots.  We make the underlying
    assumption that a test is only failing due to one revision at any given
    moment.  If that's not true, this code probably explodes.
    
    The regression ranges appear asynchronously, which might be a jarring
    UI.  We'll have to experiment to see.
    
    * Scripts/webkitpy/tool/servers/data/gardeningserver/base.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/base_unittests.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/main.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/results.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/results_unittests.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js:
    * Scripts/webkitpy/tool/servers/data/gardeningserver/ui_unittests.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/base.js
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/base.js
@@ -39,6 +39,15 @@ base.uniquifyArray = function(array)
     return result;
 };
 
+base.keys = function(dictionary)
+{
+    var keys = [];
+    $.each(dictionary, function(key, value) {
+        keys.push(key);
+    });
+    return keys;
+};
+
 base.filterTree = function(tree, isLeaf, predicate)
 {
     var filteredTree = {};
