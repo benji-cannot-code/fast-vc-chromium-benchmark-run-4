5d96bb3fbf00f25fda1c17dfb528cfea2001e840
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-12 13:35:57 +0000

    Ensure no intermediate WTF::Strings are created when concatenating with string literals
    Fixes <http://webkit.org/b/63330> Concatenating string literals and WTF::Strings using
    operator+ is suboptimal
    
    Reviewed by Darin Adler.
    
    Source/JavaScriptCore:
    
    * wtf/text/StringConcatenate.h:
    (WTF::StringTypeAdapter<String>::writeTo): Added a macro that can be used for testing how
    many WTF::Strings get copied while evaluating an operator+ expression.
    
    * wtf/text/StringOperators.h:
    (WTF::operator+): Changed the overload that takes a StringAppend to take it on the left-hand
    side, since operator+ is left-associative. Having the StringAppend on the right-hand side
    was causing us to make intermediate WTF::Strings when evaluating expressions that contained
    multiple calls to operator+. Added some more overloads for that take a left-hand side of
    const char* to resolve overload ambiguity for certain expressions. Added overloads that take
    a left-hand side of const UChar* (matching the const char* overloads) so that wide string
    literals don't first have to be converted to a WTF::String in operator+ expressions.
    
    Source/WebKit2:
    
    Export some symbols needed by TestWebKitAPI
    
    * win/WebKit2.def:
    
    Tools:
    
    Test that no intermediate WTF::Strings are created when concatenating with string literals
    
    * TestWebKitAPI/Tests/WTF/StringOperators.cpp: Added.
    (TestWebKitAPI::TEST): Test that a bunch of different string concatenation expressions don't
    create any intermediate WTF::Strings while they're being evaluated.
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/win/TestWebKitAPI.vcproj:
    Added new file.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/text/StringConcatenate.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/text/StringConcatenate.h
@@ -31,6 +31,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AtomicString.h"
 #endif
 
+// This macro is helpful for testing how many intermediate Strings are created while evaluating an
+// expression containing operator+.
+#ifndef WTF_STRINGTYPEADAPTER_COPIED_WTF_STRING
+#define WTF_STRINGTYPEADAPTER_COPIED_WTF_STRING() ((void)0)
+#endif
+
 namespace WTF {
 
 template<typename StringType>
@@ -181,6 +187,8 @@ public:
         unsigned length = m_buffer.length();
         for (unsigned i = 0; i < length; ++i)
             destination[i] = data[i];
+
+        WTF_STRINGTYPEADAPTER_COPIED_WTF_STRING();
     }
 
 private:
