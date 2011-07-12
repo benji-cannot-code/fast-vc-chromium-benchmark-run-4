cbfb59b801384b465b4a043b5a6f97de880e4929
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 01:35:55 +0000

    Print Preview: Removing unnecessary timer.
    Also making the previewed doc to fit to height always instead of fitting to
    width for landscape layout.
    
    BUG=88805
    TEST=See bug description.
    
    Review URL: http://codereview.chromium.org/7324055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/page_settings.js
+++ chrome/browser/resources/print_preview/page_settings.js
@@ -22,8 +22,8 @@ cr.define('print_preview', function() {
     this.timerId_;
 
     // Contains the previously selected pages. It is used in
-    // onPageSelectionMayHaveChanged() to make sure that a new preview is not
-    // requested more often than necessary.
+    // |this.onSelectedPagesMayHaveChanged_()| to make sure that a new preview
+    // is not requested more often than necessary.
     this.previouslySelectedPages_ = [];
 
     // The total page count of the previewed document regardless of which pages
@@ -241,7 +241,6 @@ cr.define('print_preview', function() {
      */
     onSelectedPagesTextfieldChanged: function() {
       this.validateSelectedPages_();
-      this.resetSelectedPagesTextfieldTimer_();
       updatePrintButtonState();
       updatePrintSummary();
     },
