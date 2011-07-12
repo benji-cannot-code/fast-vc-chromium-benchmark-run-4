81e8f6556017e44187eb80cb22f41dd86e25d21b
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-12 17:52:54 +0000

    NRWT should open test results page with Safari trunk, not the system provided one on Mac https://bugs.webkit.org/show_bug.cgi?id=64346
    Reviewed by Adam Barth.
    
    To fix this I implemented Port.show_results_html_file in Mac, Gtk and Qt ports with
    implementations (mostly) matching those found in old-run-webkit-tests.
    There are still some minor differences for Qt which Qt hackers may wish to tweak.
    
    I had to add a WebKitPort._port_flag_for_scripts method (similar to flag() in common.config.ports.py)
    for the Qt/Gtk ports which always require a flag passed to scripts.
    
    While trying to test this, I found that FactoryTest.test_chromium_gpu_linux
    was using a real filesystem (due to assert_platform) which due to
    global variables in config.py was causing set-webkit-configuration to have
    an affect on unit test results!  So I fixed this by making FactoryTest.assert_port
    pass a mock file system whenever calling factory.get.
    
    Unfortunately TestPort was depending on always being passed a None filesystem
    and asserting filesystem._tests (only true for unit_test_filesystem()).
    So I just removed the FactoryTest.test_test and FactoryTest.test_dryrun tests
    deciding that they were pretty much useless anyway.  If others feel strongly
    I'm happy to fix this in a different way.
    
    * Scripts/webkitpy/common/system/executive.py:
     - default arguments in python are screwy.  They use a single shared
       instance, so it's better to use argument=None and then argument = argument or Default()
       if you have any chance of mutating (or returning) the default argument.
    * Scripts/webkitpy/layout_tests/port/config.py:
     - This code is wrong.  We don't need to use a global variable here (as far as I can tell).
       I'm not fixing it in this patch, but I've marked it with a FIXME and we can convert to
       storing the results of the read on the Config object (which should only be created once during normal operation).
       Unit tests shouldn't be hitting the disk anyway.  It's possible Config should move off of Port and onto Tool/Host directly.
    * Scripts/webkitpy/layout_tests/port/factory.py:
    * Scripts/webkitpy/layout_tests/port/factory_unittest.py:
    * Scripts/webkitpy/layout_tests/port/gtk.py:
    * Scripts/webkitpy/layout_tests/port/gtk_unittest.py: Added.
    * Scripts/webkitpy/layout_tests/port/mac.py:
    * Scripts/webkitpy/layout_tests/port/mac_unittest.py:
    * Scripts/webkitpy/layout_tests/port/qt.py:
    * Scripts/webkitpy/layout_tests/port/qt_unittest.py:
    * Scripts/webkitpy/layout_tests/port/webkit.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/factory_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/factory_unittest.py
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import sys
 import unittest
 
-from webkitpy.tool import mocktool
+from webkitpy.common.system.filesystem_mock import MockFileSystem
+from webkitpy.tool.mocktool import MockOptions, MockUser, MockExecutive
 
 import chromium_gpu
 import chromium_linux
@@ -57,9 +58,8 @@ class FactoryTest(unittest.TestCase):
 
     def setUp(self):
         self.real_sys_platform = sys.platform
-        self.webkit_options = mocktool.MockOptions(pixel_tests=False)
-        self.chromium_options = mocktool.MockOptions(pixel_tests=False,
-                                                    chromium=True)
+        self.webkit_options = MockOptions(pixel_tests=False)
+        self.chromium_options = MockOptions(pixel_tests=False, chromium=True)
 
     def tearDown(self):
         sys.platform = self.real_sys_platform
@@ -72,7 +72,8 @@ class FactoryTest(unittest.TestCase):
           expected_port: class of expected port object.
           port_obj: optional port object
         """
-        port_obj = port_obj or factory.get(port_name=port_name)
+
+        port_obj = port_obj or factory.get(port_name=port_name, filesystem=MockFileSystem(), user=MockUser(), executive=MockExecutive())
         self.assertTrue(isinstance(port_obj, expected_port))
 
     def assert_platform_port(self, platform, options, expected_port):
@@ -86,16 +87,8 @@ class FactoryTest(unittest.TestCase):
         """
         orig_platform = sys.platform
         sys.platform = platform
-        self.assertTrue(isinstance(factory.get(options=options),
-                                   expected_port))
+        self.assertTrue(isinstance(factory.get(options=options, filesystem=MockFileSystem(), user=MockUser(), executive=MockExecutive()), expected_port))
         sys.platform = orig_platform
-
-    def test_test(self):
-        self.assert_port("test", test.TestPort)
-
-    def test_dryrun(self):
-        self.assert_port("dryrun-test", dryrun.DryRunPort)
-        self.assert_port("dryrun-mac", dryrun.DryRunPort)
 
     def test_mac(self):
         self.assert_port("mac", mac.MacPort)
@@ -112,14 +105,10 @@ class FactoryTest(unittest.TestCase):
     def test_google_chrome(self):
         # The actual Chrome class names aren't available so we test that the
         # objects we get are at least subclasses of the Chromium versions.
-        self.assert_port("google-chrome-linux32",
-                         chromium_linux.ChromiumLinuxPort)
-        self.assert_port("google-chrome-linux64",
-                         chromium_linux.ChromiumLinuxPort)
-        self.assert_port("google-chrome-win",
-                         chromium_win.ChromiumWinPort)
-        self.assert_port("google-chrome-mac",
-                         chromium_mac.ChromiumMacPort)
+        self.assert_port("google-chrome-linux32", chromium_linux.ChromiumLinuxPort)
+        self.assert_port("google-chrome-linux64", chromium_linux.ChromiumLinuxPort)
+        self.assert_port("google-chrome-win", chromium_win.ChromiumWinPort)
+        self.assert_port("google-chrome-mac", chromium_mac.ChromiumMacPort)
 
     def test_gtk(self):
         self.assert_port("gtk", gtk.GtkPort)
@@ -158,8 +147,7 @@ class FactoryTest(unittest.TestCase):
     def test_unknown_specified(self):
         # Test what happens when you specify an unknown port.
         orig_platform = sys.platform
-        self.assertRaises(NotImplementedError, factory.get,
-                          port_name='unknown')
+        self.assertRaises(NotImplementedError, factory.get, port_name='unknown')
 
     def test_unknown_default(self):
         # Test what happens when you're running on an unknown platform.
