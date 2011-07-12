dfa025d69174f33484fc5b7e11a0c8a1e2e8b5e8
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 20:08:14 +0000

    Make sure std::inserter is imported.
    BUG=none
    TEST=win compile
    
    Review URL: http://codereview.chromium.org/7343015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/url_pattern_set.cc
+++ chrome/common/extensions/url_pattern_set.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/url_pattern_set.h"
 
 #include <algorithm>
+#include <iterator>
 
 #include "chrome/common/extensions/url_pattern.h"
 #include "googleurl/src/gurl.h"
