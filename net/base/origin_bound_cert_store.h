37473129868ce0b9430163bece63f98ed9eb9d54
rkn@chromium.org <rkn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 23:27:28 +0000

    Fixed issues with previous CL.
    BUG=88782
    TEST=None
    
    Review URL: http://codereview.chromium.org/7335013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92261 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/origin_bound_cert_store.h
+++ net/base/origin_bound_cert_store.h
@@ -9,28 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/basictypes.h"
-
 class GURL;
 
 namespace net {
 
-// An interface for storing and retrieving origin bound certs.
+// An interface for storing and retrieving origin bound certs. Origin bound
+// certificates are specified in
+// http://balfanz.github.com/tls-obc-spec/draft-balfanz-tls-obc-00.html.
 
 class OriginBoundCertStore {
  public:
-
   virtual bool HasOriginBoundCert(const GURL& url) = 0;
 
   // TODO(rkn): Specify certificate type (RSA or DSA).
   // TODO(rkn): Key generation can be time consuming, so this should have an
   // asynchronous interface.
-  // The output is stored in |private_key| and |cert|.
-  virtual void GetOriginBoundCert(const GURL& url,
+  // The output is stored in |private_key_result| and |cert_result|.
+  virtual bool GetOriginBoundCert(const GURL& url,
                                   std::string* private_key_result,
                                   std::string* cert_result) = 0;
 
-  virtual void SetOriginBoundCert(const GURL& url,
+  virtual bool SetOriginBoundCert(const GURL& url,
                                   const std::string& private_key,
                                   const std::string& cert) = 0;
 };
