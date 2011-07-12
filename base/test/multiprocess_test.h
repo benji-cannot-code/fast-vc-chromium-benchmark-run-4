533316ff4676806bd721877b86c88520c895e1f0
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 21:44:34 +0000

    Clean up users of a deprecated base::LaunchApp API.
    BUG=88990
    
    Review URL: http://codereview.chromium.org/7346017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92240 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/multiprocess_test.h
+++ base/test/multiprocess_test.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -59,6 +59,9 @@ class MultiProcessTest : public PlatformTest {
   ProcessHandle SpawnChild(const std::string& procname, bool debug_on_start);
 
 #if defined(OS_POSIX)
+  // TODO(evan): see if we can delete this via more refactoring.
+  // SpawnChild() should just take a base::LaunchOptions so that we don't
+  // need multiple versions of it.
   ProcessHandle SpawnChild(const std::string& procname,
                            const file_handle_mapping_vector& fds_to_map,
                            bool debug_on_start);
@@ -69,17 +72,12 @@ class MultiProcessTest : public PlatformTest {
                                   bool debug_on_start);
 
  private:
-#if defined(OS_WIN)
-  ProcessHandle SpawnChildImpl(const std::string& procname,
-                               bool debug_on_start);
-
-#elif defined(OS_POSIX)
-  // TODO(port): with the CommandLine refactoring, this code is very similar
-  // to the Windows code.  Investigate whether this can be made shorter.
+  // Shared implementation of SpawnChild.
+  // TODO: |fds_to_map| is unused on Windows; see above TODO about
+  // further refactoring.
   ProcessHandle SpawnChildImpl(const std::string& procname,
                                const file_handle_mapping_vector& fds_to_map,
                                bool debug_on_start);
-#endif
 
   DISALLOW_COPY_AND_ASSIGN(MultiProcessTest);
 };
