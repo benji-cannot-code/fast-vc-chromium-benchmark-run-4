2b2f89015cd0f356c5a4f11811e45dc748ef45c8
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 17:08:55 +0000

    Taking advantage of the larger favicon for extensions added in previous CL http://codereview.chromium.org/7338014/
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7343001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92187 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_resource_providers.cc
+++ chrome/browser/task_manager/task_manager_resource_providers.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/view_messages.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "third_party/sqlite/sqlite3.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
