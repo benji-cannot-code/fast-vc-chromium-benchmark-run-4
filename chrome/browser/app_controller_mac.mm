bc05746c912c1b5f75b530f795bb5159e68cdea4
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-13 01:23:54 +0000

    Revert 92289 - Change history, downloads, bookmarks to also load over NTP. Change tests away from racy code.
    R=jhawkins@chromium.org,phajdan.jr@chromium.org
    BUG=22951
    TEST=BrowserNavigatorTest
    
    
    Review URL: http://codereview.chromium.org/7233021
    
    TBR=gbillock@chromium.org
    Review URL: http://codereview.chromium.org/7342030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92291 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -786,7 +786,7 @@ void RecordLastRunAppBundlePath() {
       if (Browser* browser = ActivateBrowser(lastProfile)) {
         browser->OpenClearBrowsingDataDialog();
       } else {
-        Browser::OpenClearBrowsingDataDialogWindow(lastProfile);
+        Browser::OpenClearBrowingDataDialogWindow(lastProfile);
       }
       break;
     }
--- chrome/browser/ui/browser_navigator_browsertest.h
+++ chrome/browser/ui/browser_navigator_browsertest.h
@@ -26,6 +26,8 @@ struct NavigateParams;
 class BrowserNavigatorTest : public InProcessBrowserTest,
                              public NotificationObserver {
  protected:
+  GURL GetGoogleURL() const;
+
   browser::NavigateParams MakeNavigateParams() const;
   browser::NavigateParams MakeNavigateParams(Browser* browser) const;
 
