f4f2313b5aeadb9743c17b03068b3eb4b68742fa
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-13 05:12:57 +0000

    Removed unnecessary function parameters from TilingData::textureOffset. https://bugs.webkit.org/show_bug.cgi?id=64245
    Patch by David Reveman <reveman@chromium.org> on 2011-07-12
    Reviewed by Brent Fulgham.
    
    No new tests, no functionality changes.
    
    * platform/graphics/chromium/LayerTilerChromium.cpp:
    (WebCore::LayerTilerChromium::drawTiles):
    * platform/graphics/gpu/TilingData.cpp:
    (WebCore::TilingData::textureOffset):
    * platform/graphics/gpu/TilingData.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90886 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gpu/TilingData.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gpu/TilingData.cpp
@@ -249,7 +249,7 @@ void TilingData::intersectDrawQuad(const FloatRect& srcRect, const FloatRect& ds
         srcRectIntersectedNormH * dstRect.height());
 }
 
-IntPoint TilingData::textureOffset(int, int) const
+IntPoint TilingData::textureOffset() const
 {
     return IntPoint(m_borderTexels, m_borderTexels);
 }
