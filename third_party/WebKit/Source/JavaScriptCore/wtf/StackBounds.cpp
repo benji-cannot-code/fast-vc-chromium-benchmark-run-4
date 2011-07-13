4f0ebc3543cf63fdd31c9e8ab2c7967b48781b40
ademar.reis@openbossa.org <ademar.reis@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-13 14:25:23 +0000

    Reviewed by Andreas Kling.
    Broken build on QNX
    https://bugs.webkit.org/show_bug.cgi?id=63717
    
    QNX doesn't support pthread's SA_RESTART (required by
    JSC_MULTIPLE_THREADS), JIT is broken at runtime and there a
    few minor compilation errors here and there.
    
    Original patch by Ritt Konstantin <ritt.ks@gmail.com>, also
    tested by him on QNX v6.5 (x86)
    
    * wtf/DateMath.cpp: fix usage of abs/labs
    * wtf/Platform.h: Disable JIT and JSC_MULTIPLE_THREADS
    * wtf/StackBounds.cpp: Add a couple of missing includes (and sort them)
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/StackBounds.cpp
+++ third_party/WebKit/Source/JavaScriptCore/wtf/StackBounds.cpp
@@ -42,10 +42,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #elif OS(QNX)
 
-#include <fcntl.h>
-#include <sys/procfs.h>
-#include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/procfs.h>
 
 #elif OS(UNIX)
 
