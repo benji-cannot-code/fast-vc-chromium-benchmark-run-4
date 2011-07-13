80b3ac8603718650ad3daaf8733add66009d5016
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-13 22:06:28 +0000

    views: Add unittests for View::GetViewsWithGroup() method.
    Also, rename it to GetViewsInGroup().
    
    BUG=72040
    TEST=views_unittests --gtest_filter=ViewTest.GetViewByID
    
    R=ben@chromium.org,sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7328031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92430 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/button/radio_button.cc
+++ views/controls/button/radio_button.cc
@@ -44,7 +44,7 @@ void NativeRadioButton::SetChecked(bool checked) {
       container = container->parent();
     if (container) {
       Views other;
-      container->GetViewsWithGroup(GetGroup(), &other);
+      container->GetViewsInGroup(GetGroup(), &other);
       for (Views::iterator i(other.begin()); i != other.end(); ++i) {
         if (*i != this) {
           if ((*i)->GetClassName() != kViewClassName) {
@@ -69,9 +69,9 @@ void NativeRadioButton::GetAccessibleState(ui::AccessibleViewState* state) {
   state->role = ui::AccessibilityTypes::ROLE_RADIOBUTTON;
 }
 
-View* NativeRadioButton::GetSelectedViewForGroup(int group_id) {
+View* NativeRadioButton::GetSelectedViewForGroup(int group) {
   Views views;
-  GetWidget()->GetRootView()->GetViewsWithGroup(group_id, &views);
+  GetWidget()->GetRootView()->GetViewsInGroup(group, &views);
   if (views.empty())
     return NULL;
 
@@ -141,7 +141,7 @@ void RadioButton::SetChecked(bool checked) {
       container = container->parent();
     if (container) {
       Views other;
-      container->GetViewsWithGroup(GetGroup(), &other);
+      container->GetViewsInGroup(GetGroup(), &other);
       for (Views::iterator i(other.begin()); i != other.end(); ++i) {
         if (*i != this) {
           if ((*i)->GetClassName() != kViewClassName) {
@@ -167,9 +167,9 @@ void RadioButton::GetAccessibleState(ui::AccessibleViewState* state) {
   state->role = ui::AccessibilityTypes::ROLE_RADIOBUTTON;
 }
 
-View* RadioButton::GetSelectedViewForGroup(int group_id) {
+View* RadioButton::GetSelectedViewForGroup(int group) {
   Views views;
-  GetWidget()->GetRootView()->GetViewsWithGroup(group_id, &views);
+  GetWidget()->GetRootView()->GetViewsInGroup(group, &views);
   if (views.empty())
     return NULL;
 
