66140994abf155741974e761bf64858df7570577
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-13 12:02:03 +0000

    Chromium DevTools: Enable clear cache and cookies from network panel in chromium. https://bugs.webkit.org/show_bug.cgi?id=64095
    Reviewed by Pavel Feldman.
    
    * src/js/DevTools.js:
    ():
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/js/DevTools.js
+++ third_party/WebKit/Source/WebKit/chromium/src/js/DevTools.js
@@ -54,6 +54,7 @@ var context = {};  // Used by WebCore's inspector routines.
     Preferences.saveAsAvailable = true;
     Preferences.useLowerCaseMenuTitlesOnWindows = true;
     Preferences.canInspectWorkers = true;
+    Preferences.canClearCacheAndCookies = true;
 })();
 
 // Recognize WebP as a valid image mime type.
