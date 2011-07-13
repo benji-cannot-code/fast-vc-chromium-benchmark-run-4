77d4725fcf861af558b1312a3b7fe5e11f8bebe7
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-13 03:40:44 +0000

    Don't show a prompt when loading an extension with NPAPI plugins via --load-extension.
    BUG=89075
    TEST=load an extension with an NPAPI plugin via --load-extension and via the extension managment page. Only the latter should prompt with an install dialog.
    
    
    Review URL: http://codereview.chromium.org/7349020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.h
+++ chrome/browser/extensions/extension_service.h
@@ -326,6 +326,9 @@ class ExtensionService
   // Loads the extension from the directory |extension_path|.
   void LoadExtension(const FilePath& extension_path);
 
+  // Same as above, but for use with command line switch --load-extension=path.
+  void LoadExtensionFromCommandLine(const FilePath& extension_path);
+
   // Loads any component extensions.
   void LoadComponentExtensions();
 
@@ -398,7 +401,8 @@ class ExtensionService
   void AddExtension(const Extension* extension);
 
   // Called by the backend when an unpacked extension has been loaded.
-  void OnLoadSingleExtension(const Extension* extension);
+  void OnLoadSingleExtension(const Extension* extension,
+                             bool prompt_for_plugins);
 
   // Called by the backend when an extension has been installed.
   void OnExtensionInstalled(
