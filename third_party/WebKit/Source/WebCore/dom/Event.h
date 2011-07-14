cb880a77d843764c2cf58a1e85418a1ccb5dfdb4
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-14 02:25:55 +0000

    Raise if dispatchEvent dispatches an event that is being dispatched https://bugs.webkit.org/show_bug.cgi?id=64150
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Spec: http://www.w3.org/TR/DOM-Level-3-Events/#events-EventTarget-dispatchEvent
    
    Test: fast/events/dispatch-event-being-dispatched.html
    
    * dom/Event.h:
    (WebCore::Event::isBeingDispatched): Added.
    * dom/EventException.h:
    * dom/EventException.idl: Add DISPATCH_REQUEST_ERR.
    * dom/EventTarget.cpp:
    (WebCore::EventTarget::dispatchEvent): Raise if being dispatched.
    * dom/ExceptionCode.cpp: Add gunk for DISPATCH_REQUEST_ERR.
    
    LayoutTests:
    
    * fast/dom/Window/window-properties-expected.txt: Added DISPATCH_REQUEST_ERR.
    * fast/events/dispatch-event-being-dispatched-expected.txt: Added.
    * fast/events/dispatch-event-being-dispatched.html: Added.
    * fast/workers/resources/worker-event-listener.js:
    (onmessage): Redispatching the MessageEvent throws an exception now.
    * platform/gtk/fast/dom/Window/window-properties-expected.txt:
    * platform/qt-wk2/fast/dom/Window/window-properties-expected.txt:
    * platform/qt/fast/dom/Window/window-properties-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Event.h
+++ third_party/WebKit/Source/WebCore/dom/Event.h
@@ -172,6 +172,7 @@ namespace WebCore {
 
         virtual Clipboard* clipboard() const { return 0; }
 
+        bool isBeingDispatched() const { return eventPhase(); }
 
     protected:
         Event();
