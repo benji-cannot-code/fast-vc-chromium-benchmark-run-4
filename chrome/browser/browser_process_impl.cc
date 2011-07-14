8e52b2aa11dc8a94069d7cdb6f696df8d5ff94e7
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 20:01:13 +0000

    Use BrowserThreadMessageLoopProxy to post messages to watched browser thread.
    Store away thread_id using Debug::Alias so that optimizer doesn't
    optimize away the code so that we can see the thread_id that is 
    hung from the crash dumps in windbg.
    
    Stop the WatchDog thread before stopping other threads to see if
    it fixes the racy condition duriong shutdown.
    
    BUG=83564, 86818, 89141, 83343, 83430
    TEST=ThreadWatcher unit tests
    R=jar
    Review URL: http://codereview.chromium.org/7218030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -226,6 +226,9 @@ BrowserProcessImpl::~BrowserProcessImpl() {
   // processes so this has to happen before stopping the IO thread.
   GpuProcessHostUIShim::DestroyAll();
 
+  // Stop the watchdog thread before stopping other threads.
+  watchdog_thread_.reset();
+
   // Need to stop io_thread_ before resource_dispatcher_host_, since
   // io_thread_ may still deref ResourceDispatcherHost and handle resource
   // request before going away.
@@ -267,9 +270,6 @@ BrowserProcessImpl::~BrowserProcessImpl() {
 
   // Now OK to destroy NotificationService.
   main_notification_service_.reset();
-
-  // Stop the watchdog thread after stopping other threads.
-  watchdog_thread_.reset();
 
   g_browser_process = NULL;
 }
