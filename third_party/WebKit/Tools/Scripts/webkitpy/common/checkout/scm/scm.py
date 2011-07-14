ce4bb967417fcffd0fcebd5bc1f833f7cc265f76
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-14 01:03:02 +0000

    REGRESSION: GitTestWithMock.test_create_patch fails https://bugs.webkit.org/show_bug.cgi?id=62945
    Reviewed by Daniel Bates.
    
    I was not able to reproduce the exact failure seen in the bug,
    however this test was failing on my machine for other reasons.
    
    I went through and did an audit of our run_command usage, it's
    entirely in scm classes after this change.  (Not surprising given
    that scm.py was the second file ever created in webkit.py.)
    
    The real bug I'm fixing here is that we were setting executive.should_log
    when the value had changed to executive._should_log.  Now we set the right one
    and the test works again.
    
    * Scripts/webkitpy/common/checkout/checkout.py:
    * Scripts/webkitpy/common/checkout/scm/git.py:
    * Scripts/webkitpy/common/checkout/scm/scm.py:
    * Scripts/webkitpy/common/checkout/scm/scm_unittest.py:
    * Scripts/webkitpy/common/checkout/scm/svn.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@90967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/scm.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/scm.py
@@ -37,7 +37,7 @@ import sys
 import shutil
 
 from webkitpy.common.system.deprecated_logging import error, log
-from webkitpy.common.system.executive import Executive, run_command, ScriptError
+from webkitpy.common.system.executive import Executive, ScriptError
 from webkitpy.common.system import ospath
 
 
