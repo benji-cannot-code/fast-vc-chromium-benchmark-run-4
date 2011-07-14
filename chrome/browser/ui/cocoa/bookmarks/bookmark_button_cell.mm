9f7f20dad81ee186608f55c972f7a92cdfd4b8f5
keishi@google.com <keishi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 05:07:49 +0000

    Regression:Fix Bookmarksbar Folder Fontcolor
    Caused by http://codereview.chromium.org/7326043
    
    BUG=89218
    TEST=Manually. Open Bookmark Bar folder and see if the font color is black.
    
    
    Review URL: http://codereview.chromium.org/7358010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_button_cell.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_button_cell.mm
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         cellImage:(NSImage*)cellImage {
   if ((self = [super initTextCell:cellText])) {
     [self configureBookmarkButtonCell];
-    [self setTextColor:[NSColor redColor]];
+    [self setTextColor:[NSColor blackColor]];
     [self setBookmarkNode:node];
 
     if (node) {
