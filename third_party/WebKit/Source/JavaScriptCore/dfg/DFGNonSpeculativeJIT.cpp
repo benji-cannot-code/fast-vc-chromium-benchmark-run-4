f483908b97fd646ecc9a4a664f798f954daae457
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-14 17:32:13 +0000

    DFG JIT unnecessarily boxes and unboxes values during silent spilling. https://bugs.webkit.org/show_bug.cgi?id=64068
    Patch by Filip Pizlo <fpizlo@apple.com> on 2011-07-14
    Reviewed by Gavin Barraclough.
    
    Silent spilling and filling of registers is done during slow-path C
    function calls.  The silent spill/fill logic does not affect register
    allocation on paths that don't involve the C function call.
    
    This changes the silent spilling code to spill in unboxed form.  The
    silent fill will refill in whatever form the register was spilled in.
    For example, the silent spill code may choose not to spill the register
    because it was already spilled previously, which would imply that it
    was spilled in boxed form.  The filling code detects this and either
    unboxes, or not, depending on what is appropriate.
    
    This change also results in a simplification of the silent spill/fill
    API: silent spilling no longer needs to know about the set of registers
    that cannot be trampled, since it never does boxing and hence does not
    need a temporary register.
    
    * dfg/DFGJITCodeGenerator.cpp:
    (JSC::DFG::JITCodeGenerator::cachedGetById):
    (JSC::DFG::JITCodeGenerator::cachedPutById):
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::JITCodeGenerator::silentSpillGPR):
    (JSC::DFG::JITCodeGenerator::silentSpillFPR):
    (JSC::DFG::JITCodeGenerator::silentFillFPR):
    (JSC::DFG::JITCodeGenerator::silentSpillAllRegisters):
    * dfg/DFGNonSpeculativeJIT.cpp:
    (JSC::DFG::NonSpeculativeJIT::valueToNumber):
    (JSC::DFG::NonSpeculativeJIT::valueToInt32):
    (JSC::DFG::NonSpeculativeJIT::knownConstantArithOp):
    (JSC::DFG::NonSpeculativeJIT::basicArithOp):
    (JSC::DFG::NonSpeculativeJIT::compare):
    (JSC::DFG::NonSpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.cpp
@@ -70,7 +70,7 @@ void NonSpeculativeJIT::valueToNumber(JSValueOperand& operand, GPRReg gpr)
 
     // Next handle cells (& other JS immediates)
     nonNumeric.link(&m_jit);
-    silentSpillAllRegisters(gpr, jsValueGpr);
+    silentSpillAllRegisters(gpr);
     m_jit.move(jsValueGpr, GPRInfo::argumentGPR1);
     m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     appendCallWithExceptionCheck(dfgConvertJSValueToNumber);
@@ -92,7 +92,7 @@ void NonSpeculativeJIT::valueToInt32(JSValueOperand& operand, GPRReg result)
     JITCompiler::Jump isInteger = m_jit.branchPtr(MacroAssembler::AboveOrEqual, jsValueGpr, GPRInfo::tagTypeNumberRegister);
 
     // First handle non-integers
-    silentSpillAllRegisters(result, jsValueGpr);
+    silentSpillAllRegisters(result);
     m_jit.move(jsValueGpr, GPRInfo::argumentGPR1);
     m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     appendCallWithExceptionCheck(dfgConvertJSValueToInt32);
@@ -221,7 +221,7 @@ void NonSpeculativeJIT::knownConstantArithOp(NodeType op, NodeIndex regChild, No
     if (!isKnownInteger(regChild))
         notInt.link(&m_jit);
     
-    silentSpillAllRegisters(resultGPR, regArgGPR);
+    silentSpillAllRegisters(resultGPR);
     switch (op) {
     case ValueAdd:
         if (commute) {
@@ -311,7 +311,7 @@ void NonSpeculativeJIT::basicArithOp(NodeType op, Node &node)
     
     slowPath.link(&m_jit);
     
-    silentSpillAllRegisters(resultGPR, arg1GPR, arg2GPR);
+    silentSpillAllRegisters(resultGPR);
     if (op == ValueAdd) {
         setupStubArguments(arg1GPR, arg2GPR);
         m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
@@ -368,7 +368,7 @@ void NonSpeculativeJIT::compare(Node& node, MacroAssembler::RelationalCondition 
     
     slowPath.link(&m_jit);
     
-    silentSpillAllRegisters(resultGPR, arg1GPR, arg2GPR);
+    silentSpillAllRegisters(resultGPR);
     setupStubArguments(arg1GPR, arg2GPR);
     m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     appendCallWithExceptionCheck(helperFunction);
@@ -719,7 +719,7 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
         m_jit.xorPtr(TrustedImm32(static_cast<int32_t>(ValueFalse)), resultGPR);
         JITCompiler::Jump fastCase = m_jit.branchTestPtr(JITCompiler::Zero, resultGPR, TrustedImm32(static_cast<int32_t>(~1)));
         
-        silentSpillAllRegisters(resultGPR, arg1GPR);
+        silentSpillAllRegisters(resultGPR);
         m_jit.move(arg1GPR, GPRInfo::argumentGPR1);
         m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
         appendCallWithExceptionCheck(dfgConvertJSValueToBoolean);
@@ -795,7 +795,7 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
         outOfBounds.link(&m_jit);
         loadFailed.link(&m_jit);
 
-        silentSpillAllRegisters(storageGPR, baseGPR, propertyGPR);
+        silentSpillAllRegisters(storageGPR);
         setupStubArguments(baseGPR, propertyGPR);
         m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
         appendCallWithExceptionCheck(operationGetByVal);
@@ -1039,7 +1039,7 @@ void NonSpeculativeJIT::compile(SpeculationCheckIndexIterator& checkIterator, No
         notDefaultHasInstance.link(&m_jit);
         protoNotObject.link(&m_jit);
 
-        silentSpillAllRegisters(scratchReg, valueReg, baseReg, prototypeReg);
+        silentSpillAllRegisters(scratchReg);
         setupStubArguments(valueReg, baseReg, prototypeReg);
         m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
         appendCallWithExceptionCheck(operationInstanceOf);
