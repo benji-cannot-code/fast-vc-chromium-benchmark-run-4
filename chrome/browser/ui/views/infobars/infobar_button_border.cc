b917918aeb8fd00a0d9eefa870d70f12be59faf3
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 18:11:16 +0000

    Minor comment.
    BUG=none
    TEST=none
    TBR=rhashimoto@chromium.org
    
    Review URL: http://codereview.chromium.org/7378001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92567 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/infobars/infobar_button_border.cc
+++ chrome/browser/ui/views/infobars/infobar_button_border.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 InfoBarButtonBorder::InfoBarButtonBorder() {
 #ifdef TOUCH_UI
+  // The vertical padding would be 5 without the code below.
   static const int kPreferredPaddingVertical = 12;
   set_vertical_padding(kPreferredPaddingVertical);
 #endif
