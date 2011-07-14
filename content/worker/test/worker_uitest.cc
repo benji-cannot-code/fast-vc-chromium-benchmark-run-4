05bb4914945b5e4624061b433122ae70b1267cb9
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 09:07:56 +0000

    Fix a few places using OS_MAC instead of OS_MACOSX to disable flaky/failing tests
    BUG=86562,86550,88958,48664,28445
    TEST=none
    TBR=jam,akalin
    
    Review URL: http://codereview.chromium.org/7365002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92504 0039d316-1c4b-4281-b951-d872f2087c98

--- content/worker/test/worker_uitest.cc
+++ content/worker/test/worker_uitest.cc
@@ -558,7 +558,7 @@ TEST_F(WorkerTest, FLAKY_MessagePorts) {
     RunLayoutTest(kLayoutTestFiles[i], kNoHttpPort);
 }
 
-#if defined(OS_MAC)
+#if defined(OS_MACOSX)
 // See http://crbug.com/48664
 #define MAYBE_LimitPerPage DISABLED_LimitPerPage
 #else
@@ -580,7 +580,7 @@ TEST_F(WorkerTest, MAYBE_LimitPerPage) {
 #if defined(OS_LINUX)
 // Hangs on Linux: http://crbug.com/30332
 #define FLAKY_LimitTotal DISABLED_LimitTotal
-#elif defined(OS_MAC)
+#elif defined(OS_MACOSX)
 // Possibly causing ui_tests to hang on Mac: http://crbug.com/88958
 #define FLAKY_LimitTotal DISABLED_LimitTotal
 #endif
