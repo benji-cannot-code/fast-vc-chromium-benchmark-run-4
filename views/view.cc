273bacf5bd58ad255379a584035f941570412f5d
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-15 17:49:19 +0000

    Don't schedule paint if one of view in parent chain is invisible
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7374007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92711 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -1170,7 +1170,7 @@ void View::PaintComposite() {
 }
 
 void View::SchedulePaintInternal(const gfx::Rect& rect) {
-  if (parent_) {
+  if (parent_ && parent_->IsVisible()) {
     // Translate the requested paint rect to the parent's coordinate system
     // then pass this notification up to the parent.
     parent_->SchedulePaintInternal(ConvertRectToParent(rect));
