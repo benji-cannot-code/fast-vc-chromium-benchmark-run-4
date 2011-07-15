de748e40e86c6017a3ea79f09fffd5dfeac9e748
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-15 04:36:29 +0000

    <input form="x"> should not associate the input with any forms when there is no  form with id="x" https://bugs.webkit.org/show_bug.cgi?id=64509
    Source/WebCore:
    
    Don't set form owner when a form associated element has the form attribute but there is no such form element.
    
    Patch by Kenichi Ishibashi <bashi@chromium.org> on 2011-07-14
    Reviewed by Kent Tamura.
    
    Test: fast/forms/form-attribute-nonexistence-form-id.html
    
    * dom/Document.cpp:
    (WebCore::Document::resetFormElementsOwner): Removed the argument which is no longer needed.
    * dom/Document.h: Ditto.
    * html/FormAssociatedElement.cpp:
    (WebCore::FormAssociatedElement::insertedIntoTree): Resets m_form whenever the element has the form attribute.
    (WebCore::FormAssociatedElement::resetFormOwner): Don't set m_form when there is no form element which has the given id.
    (WebCore::FormAssociatedElement::formAttributeChanged): Removed the argument of resetFormOwner().
    * html/FormAssociatedElement.h: Ditto.
    * html/HTMLFormElement.cpp: Ditto.
    (WebCore::HTMLFormElement::insertedIntoDocument):
    (WebCore::HTMLFormElement::removedFromDocument):
    
    LayoutTests:
    
    Adds a test for checking input elements don't associate any form elements when they have the form attribute which points nonexistence form.
    Fixes existing tests that contain unintended form attributes.
    
    Patch by Kenichi Ishibashi <bashi@chromium.org> on 2011-07-14
    Reviewed by Kent Tamura.
    
    * fast/forms/form-attribute-nonexistence-form-id-expected.txt: Added.
    * fast/forms/form-attribute-nonexistence-form-id.html: Added.
    * fast/forms/script-tests/form-attribute.js: Fixed unintended form attributes.
    * fast/forms/script-tests/form-collection-elements-order.js: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFormElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLFormElement.cpp
@@ -139,7 +139,7 @@ void HTMLFormElement::insertedIntoDocument()
     HTMLElement::insertedIntoDocument();
 
     if (hasID())
-        document()->resetFormElementsOwner(this);
+        document()->resetFormElementsOwner();
 }
 
 void HTMLFormElement::removedFromDocument()
@@ -150,7 +150,7 @@ void HTMLFormElement::removedFromDocument()
     HTMLElement::removedFromDocument();
 
     if (hasID())
-        document()->resetFormElementsOwner(0);
+        document()->resetFormElementsOwner();
 }
 
 void HTMLFormElement::handleLocalEvents(Event* event)
