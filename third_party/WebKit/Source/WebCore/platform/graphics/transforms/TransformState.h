fa4be4804a52c149f5440fffd9ad95b774d8fe2c
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-15 23:52:08 +0000

    Fixing style issues pointed out by Simon for         https://bugs.webkit.org/show_bug.cgi?id=64542
            Unreviewed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/transforms/TransformState.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/transforms/TransformState.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Apple Inc.  All rights reserved.
+ * Copyright (C) 2011 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,9 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatQuad.h"
 #include "IntSize.h"
 #include "TransformationMatrix.h"
-#include <wtf/PassRefPtr.h>
 #include <wtf/OwnPtr.h>
-#include <wtf/RefCounted.h>
 
 namespace WebCore {
 
