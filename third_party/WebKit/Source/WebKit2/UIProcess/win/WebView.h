a1747695ed6d1e0cceadb37ffa09ae9754a6865d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-15 20:10:07 +0000

    Windows build fix.
    * UIProcess/win/WebView.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/win/WebView.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/win/WebView.h
@@ -186,6 +186,7 @@ private:
     virtual void pageClosed();
     virtual void toolTipChanged(const WTF::String&, const WTF::String&);
     virtual void setCursor(const WebCore::Cursor&);
+    virtual void setCursorHiddenUntilMouseMoves(bool);
     virtual void setViewportArguments(const WebCore::ViewportArguments&);
     virtual void registerEditCommand(PassRefPtr<WebEditCommandProxy>, WebPageProxy::UndoOrRedo);
     virtual void clearAllEditCommands();
