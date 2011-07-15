8b4eadb2256cbaf89d45007437f67fb7bd38c559
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-15 21:35:06 +0000

    [Mac] Allow NSTask exceptions w/in Keystone.
    If /usr/bin/python is missing, an NSException is thrown on first run
    (when Keystone attempts to register).  Keystone should be fixed to
    avoid the exception as much as possible, but meanwhile don't crash the
    browser on first run.
    
    BUG=86221
    TEST=Remove/rename /usr/bin/python link and do a fresh install of Chrome.  Should not crash, even though Keystone bundle is not installed.
    
    
    Review URL: http://codereview.chromium.org/7359003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/mac/keystone_glue.mm
+++ chrome/browser/mac/keystone_glue.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
+#include "base/mac/scoped_nsexception_enabler.h"
 #include "base/memory/ref_counted.h"
 #include "base/sys_string_conversions.h"
 #include "base/task.h"
@@ -458,7 +459,15 @@ NSString* const kVersionKey = @"KSVersion";
   [self updateStatus:kAutoupdateRegistering version:nil];
 
   NSDictionary* parameters = [self keystoneParameters];
-  if (![registration_ registerWithParameters:parameters]) {
+  BOOL result;
+  {
+    // TODO(shess): Allows Keystone to throw an exception when
+    // /usr/bin/python does not exist (really!).
+    // http://crbug.com/86221 and http://crbug.com/87931
+    base::mac::ScopedNSExceptionEnabler enabler;
+    result = [registration_ registerWithParameters:parameters];
+  }
+  if (!result) {
     [self updateStatus:kAutoupdateRegisterFailed version:nil];
     return;
   }
