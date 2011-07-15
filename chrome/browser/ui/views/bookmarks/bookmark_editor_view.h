9dee541b979f62a9b2bff360f26399de2b07b588
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-15 22:19:29 +0000

    Wires up persisting expanded folders in the bookmark editor for views.
    BUG=6648
    TEST=add a bookmark, expand some folders, add another bookmark and
    make sure the same set of folders remain expanded.
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/7387014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92757 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_editor_view.h
+++ chrome/browser/ui/views/bookmarks/bookmark_editor_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/string16.h"
 #include "chrome/browser/bookmarks/bookmark_editor.h"
+#include "chrome/browser/bookmarks/bookmark_expanded_state_tracker.h"
 #include "chrome/browser/bookmarks/bookmark_model_observer.h"
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -218,6 +219,11 @@ class BookmarkEditorView : public BookmarkEditor,
   // added to the model and returned. This does NOT start editing. This is used
   // internally by NewFolder and broken into a separate method for testing.
   EditorNode* AddNewFolder(EditorNode* parent);
+
+  // If |editor_node| is expanded it's added to |expanded_nodes| and this is
+  // recursively invoked for all the children.
+  void UpdateExpandedNodes(EditorNode* editor_node,
+                           BookmarkExpandedStateTracker::Nodes* expanded_nodes);
 
   // Profile the entry is from.
   Profile* profile_;
