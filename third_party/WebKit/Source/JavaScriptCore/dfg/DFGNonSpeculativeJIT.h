125d7e8cb5e7e787015383dcea91e872d3de0340
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-15 20:14:27 +0000

    DFG JIT is inconsistent about fusing branches and speculating integer comparisons for branches. https://bugs.webkit.org/show_bug.cgi?id=64573
    Patch by Filip Pizlo <fpizlo@apple.com> on 2011-07-15
    Reviewed by Gavin Barraclough.
    
    This patch moves some of NonSpeculativeJIT's functionality up into the
    JITCodeGenerator superclass so that it can be used from both JITs.  Now,
    in cases where the speculative JIT doesn't want to speculate but still
    wants to emit good code, it can reliably emit the same code sequence as
    the non-speculative JIT.  This patch also extends the non-speculative
    JIT's compare optimizations to include compare/branch fusing, and
    extends the speculative JIT's compare optimizations to cover StrictEqual.
    
    * dfg/DFGJITCodeGenerator.cpp:
    (JSC::DFG::JITCodeGenerator::isKnownInteger):
    (JSC::DFG::JITCodeGenerator::isKnownNumeric):
    (JSC::DFG::JITCodeGenerator::nonSpeculativePeepholeBranch):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeCompare):
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::JITCodeGenerator::detectPeepHoleBranch):
    * dfg/DFGNonSpeculativeJIT.cpp:
    (JSC::DFG::NonSpeculativeJIT::compile):
    * dfg/DFGNonSpeculativeJIT.h:
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compare):
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.h:
    * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNonSpeculativeJIT.h
@@ -83,9 +83,6 @@ private:
     void compile(SpeculationCheckIndexIterator&, Node&);
     void compile(SpeculationCheckIndexIterator&, BasicBlock&);
 
-    bool isKnownInteger(NodeIndex);
-    bool isKnownNumeric(NodeIndex);
-
     // These methods are used to plant calls out to C++
     // helper routines to convert between types.
     void valueToNumber(JSValueOperand&, GPRReg result);
@@ -103,7 +100,6 @@ private:
     // internal helpers for add/sub/mul operations
     void knownConstantArithOp(NodeType op, NodeIndex regChild, NodeIndex immChild, bool commute);
     void basicArithOp(NodeType op, Node&);
-    void compare(Node&, MacroAssembler::RelationalCondition, Z_DFGOperation_EJJ);
 
     EntryLocationVector m_entryLocations;
 };
