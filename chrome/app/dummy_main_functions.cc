a71ff20167b96efd3e8e918c4926138edca6e2c8
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-15 21:05:13 +0000

    fix include path left over from merge conflict.
    TBR=jam@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7401004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92734 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/dummy_main_functions.cc
+++ chrome/app/dummy_main_functions.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/common/main_function_params.h"
-#include "chrome/common/chrome_result_codes.h"
+#include "chrome/common/result_codes.h"
 
 // Native Client binary for 64-bit Windows can run only the NaCl loader or
 // the sandbox broker processes. Other process types are not supported.
