57c778fb34d348b01bce520f0728f975d7ab09fa
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-16 05:55:05 +0000

    2011-07-15  Simon Fraser  <simon.fraser@apple.com>
            Fix the build.
    
            * platform/mac/ScrollbarThemeMac.mm:
            (WebCore::ScrollbarThemeMac::updateEnabledState):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.mm
@@ -451,6 +451,8 @@ void ScrollbarThemeMac::updateEnabledState(Scrollbar* scrollbar)
 {
 #if USE(WK_SCROLLBAR_PAINTER)
     wkScrollbarPainterSetEnabled(scrollbarMap()->get(scrollbar).get(), scrollbar->enabled());
+#else
+    UNUSED_PARAM(scrollbar);
 #endif
 }
 
