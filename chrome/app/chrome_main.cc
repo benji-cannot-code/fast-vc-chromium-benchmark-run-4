289a5aa8c651d8069037197d63a93a5346a72a06
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-16 02:47:37 +0000

    Revert 92787 - Modify the Chrome Linux zygote to support a ZygoteForkHelper for flexible fork implementations.
    BUG=nativeclient:480
    TEST=existing zygote tests
    
    
    Review URL: http://codereview.chromium.org/7397003
    
    TBR=bradchen@google.com
    Review URL: http://codereview.chromium.org/7400014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -76,10 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <signal.h>
 #endif
 
-#if defined(OS_POSIX) && !defined(OS_MACOSX)
-#include "content/common/zygote_fork_delegate_linux.h"
-#endif
-
 #if defined(OS_CHROMEOS)
 #include "base/sys_info.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
@@ -118,10 +114,7 @@ extern int WorkerMain(const MainFunctionParams&);
 extern int NaClMain(const MainFunctionParams&);
 extern int UtilityMain(const MainFunctionParams&);
 extern int ProfileImportMain(const MainFunctionParams&);
-#if defined(OS_POSIX) && !defined(OS_MACOSX)
-extern int ZygoteMain(const MainFunctionParams&,
-                      ZygoteForkDelegate* forkdelegate);
-#endif
+extern int ZygoteMain(const MainFunctionParams&);
 #if defined(_WIN64)
 extern int NaClBrokerMain(const MainFunctionParams&);
 #endif
@@ -459,7 +452,7 @@ int RunZygote(const MainFunctionParams& main_function_params) {
     media::InitializeMediaLibrary(media_path);
 
   // This function call can return multiple times, once per fork().
-  if (!ZygoteMain(main_function_params, NULL))
+  if (!ZygoteMain(main_function_params))
     return 1;
 
   // Zygote::HandleForkRequest may have reallocated the command
