75148c8a3caffca38c001e691f15ab8b9f7437dd
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-17 00:46:03 +0000

    2011-07-16  Daniel Bates  <dbates@webkit.org>
            Attempt to fix the Qt Linux Release build after changeset <http://trac.webkit.org/changeset/91097>
            (https://bugs.webkit.org/show_bug.cgi?id=64615).
    
            * UIProcess/qt/QtWebPageProxy.cpp:
            (QtWebPageProxy::setCursorHiddenUntilMouseMoves): Added stub method.
            * UIProcess/qt/QtWebPageProxy.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91159 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageProxy.cpp
@@ -266,6 +266,11 @@ void QtWebPageProxy::setCursor(const WebCore::Cursor& cursor)
     m_viewInterface->didChangeCursor(*cursor.platformCursor());
 }
 
+void QtWebPageProxy::setCursorHiddenUntilMouseMoves(bool hiddenUntilMouseMoves)
+{
+    notImplemented();
+}
+
 void QtWebPageProxy::setViewNeedsDisplay(const WebCore::IntRect& rect)
 {
     m_viewInterface->setViewNeedsDisplay(QRect(rect));
