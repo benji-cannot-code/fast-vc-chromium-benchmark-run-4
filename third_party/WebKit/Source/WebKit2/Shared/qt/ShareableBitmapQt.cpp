60dd977d601d07346a76156f0804c3a6bf028f9f
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-17 04:21:47 +0000

    2011-07-16  Daniel Bates  <dbates@webkit.org>
            Fix Qt Linux Release build after changeset <http://trac.webkit.org/changeset/91085>
            (https://bugs.webkit.org/show_bug.cgi?id=64611).
    
            Stub out scale factor-variant implementation of ShareableBitmap::paint().
            We'll need to implement this. See <https://bugs.webkit.org/show_bug.cgi?id=64663>
            for more details.
    
            * Shared/qt/ShareableBitmapQt.cpp:
            (WebKit::ShareableBitmap::paint): Added scale-factor variant.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/qt/ShareableBitmapQt.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/qt/ShareableBitmapQt.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QPainter>
 #include <WebCore/BitmapImage.h>
 #include <WebCore/GraphicsContext.h>
+#include <WebCore/NotImplemented.h>
 
 using namespace WebCore;
 
@@ -64,6 +65,12 @@ void ShareableBitmap::paint(GraphicsContext& context, const IntPoint& dstPoint, 
     QImage image = createQImage();
     QPainter* painter = context.platformContext();
     painter->drawImage(dstPoint, image, QRect(srcRect));
+}
+
+void ShareableBitmap::paint(GraphicsContext& /*context*/, float /*scaleFactor*/, const IntPoint& /*dstPoint*/, const IntRect& /*srcRect*/)
+{
+    // See <https://bugs.webkit.org/show_bug.cgi?id=64663>.
+    notImplemented();
 }
 
 } // namespace WebKit
