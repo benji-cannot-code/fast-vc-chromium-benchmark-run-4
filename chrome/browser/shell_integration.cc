bf8388e0b81ec5d672a540d5660063ee34611ce6
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-18 02:25:31 +0000

    Canary versions no longer always remove protocol handlers at startup.
    Canary versions no longer check OS registration at startup as they cannot
    make themselves the default OS handler.
    
    BUG=88196
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/7388009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/shell_integration.cc
+++ chrome/browser/shell_integration.cc
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "content/browser/browser_thread.h"
 
-bool ShellIntegration::CanSetAsDefaultProtocolClient(
-    const std::string& protocol) {
+bool ShellIntegration::CanSetAsDefaultProtocolClient() {
   // Allowed as long as the browser can become the operating system default
   // browser.
   return CanSetAsDefaultBrowser();
