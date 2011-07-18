150e14fb5c2c0ed4a5ea0ade221300c6aef05ace
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-18 20:58:20 +0000

    Completed the implementation for PPB_Graphics3D interface. - Mostly copied from the implementations for PPB_Surface3D and PPB_Context3D. - Added the proxy implementation - Refactored common code between host and plugin side into a common class.
    I will send the changes to bind Graphics3D with Instance and OpenGLES2 interface in a separate patch.
    
    BUG=86370,78087
    
    Review URL: http://codereview.chromium.org/6824006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92885 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/plugin_resource.h
+++ ppapi/proxy/plugin_resource.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   F(FlashNetConnector) \
   F(Font) \
   F(Graphics2D) \
+  F(Graphics3D) \
   F(ImageData) \
   F(MockResource) \
   F(PrivateFontFile) \
