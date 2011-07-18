9bf894675ac64de72346cba54c90c98600f6cdc4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-18 06:30:31 +0000

    [GTK] Fix selection of elements in a multi select list and remove an unnecessary include in WorkQueue https://bugs.webkit.org/show_bug.cgi?id=64666
    Patch by Amruth Raj <amruthraj@motorola.com> on 2011-07-17
    Reviewed by Martin Robinson.
    
    * Platform/gtk/WorkQueueGtk.cpp: Remove an include which is no longer required
    * Shared/gtk/WebEventFactory.cpp:
    (WebKit::modifiersForEvent): Fix an incorrect ASSERT statement
    (WebKit::WebEventFactory::createWebMouseEvent): Call the function to determine the modifier keys
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91173 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/gtk/WorkQueueGtk.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/gtk/WorkQueueGtk.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkQueue.h"
 
 #include "WKBase.h"
-#include <WebCore/NotImplemented.h>
 #include <gio/gio.h>
 #include <glib.h>
 #include <wtf/gobject/GRefPtr.h>
