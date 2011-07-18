1def705c74ab1e9607c8c95c7c80aa8df9390963
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-18 02:47:26 +0000

    Print Preview: Make preview generation event driven to eliminate synchronous messages.
    BUG=89069
    TEST=Print preview still works.
    
    Review URL: http://codereview.chromium.org/7365003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview_ui.cc
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 PrintPreviewUI::PrintPreviewUI(TabContents* contents)
     : ChromeWebUI(contents),
       initial_preview_start_time_(base::TimeTicks::Now()),
-      request_count_(0U) {
+      request_count_(0U),
+      document_cookie_(0) {
   // WebUI owns |handler_|.
   handler_ = new PrintPreviewHandler();
   AddMessageHandler(handler_->Attach(this));
@@ -55,8 +56,10 @@ void PrintPreviewUI::OnPrintPreviewRequest() {
   request_count_++;
 }
 
-void PrintPreviewUI::OnDidGetPreviewPageCount(int page_count) {
+void PrintPreviewUI::OnDidGetPreviewPageCount(int document_cookie,
+                                              int page_count) {
   DCHECK_GT(page_count, 0);
+  document_cookie_ = document_cookie;
   FundamentalValue count(page_count);
   CallJavascriptFunction("onDidGetPreviewPageCount", count);
 }
@@ -117,7 +120,10 @@ PrintPreviewDataService* PrintPreviewUI::print_preview_data_service() {
 }
 
 void PrintPreviewUI::DecrementRequestCount() {
-  DCHECK_GT(request_count_, 0U);
   if (request_count_ > 0)
     request_count_--;
+}
+
+int PrintPreviewUI::document_cookie() {
+  return document_cookie_;
 }
