53af9400e2a6c37b2c5c65fe47f0adc9bbe1b883
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 18:50:19 +0000

    Marking ExtensionApiTest.Bookmarks as flaky.
    BUG=89762
    TBR=robertshield@chromium.org
    TEST=browser_tests
    Review URL: http://codereview.chromium.org/7449002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_bookmarks_apitest.cc
+++ chrome/browser/extensions/extension_bookmarks_apitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Bookmarks) {
+// Flaky test, http://crbug.com/89762.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_Bookmarks) {
   ASSERT_TRUE(RunExtensionTest("bookmarks")) << message_;
 }
