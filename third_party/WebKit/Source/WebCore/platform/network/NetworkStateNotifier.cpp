eaf9cdebe9b0a9cff06b7650e87775eb0a31dc92
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-19 15:26:16 +0000

    [EFL] Add ewk_network.cpp|h files. https://bugs.webkit.org/show_bug.cgi?id=63315
    Source/WebCore:
    
    Enable setOnLine(bool) on EFL port as well as Android and Chromium.
    
    Reviewed by Antonio Gomes.
    
    * platform/network/NetworkStateNotifier.cpp:
    * platform/network/NetworkStateNotifier.h:
    
    Source/WebKit/efl:
    
    ewk_network.cpp|h files are added to ewk in order to support APIs related to networking.
    In addition, ewk_network_state_notifier_online_set() is added to ewk_network.cpp in order
    to notify NetworkStateNotifier of network status.
    
    Reviewed by Antonio Gomes.
    
    * CMakeListsEfl.txt:
    * ewk/EWebKit.h:
    * ewk/ewk_network.cpp: Added.
    (ewk_network_proxy_uri_set):
    (ewk_network_proxy_uri_get):
    (ewk_network_state_notifier_online_set):
    * ewk/ewk_network.h:
    * ewk/ewk_settings.cpp:
    * ewk/ewk_settings.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/NetworkStateNotifier.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/NetworkStateNotifier.cpp
@@ -47,7 +47,7 @@ void NetworkStateNotifier::setNetworkStateChangedFunction(void(*function)())
     m_networkStateChangedFunction = function;
 }
 
-#if PLATFORM(ANDROID) || PLATFORM(CHROMIUM)
+#if PLATFORM(ANDROID) || PLATFORM(CHROMIUM) || PLATFORM(EFL)
 void NetworkStateNotifier::setOnLine(bool onLine)
 {
     if (m_isOnLine == onLine)
