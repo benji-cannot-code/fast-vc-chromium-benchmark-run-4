2456258e8c35f87d966def71a177f71ba68d72dc
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-19 22:57:33 +0000

    Work towards determining the cause of frequent crashes due to null frame below ApplicationCacheHost::scheduleLoadFallbackResourceFromApplicationCache. https://bugs.webkit.org/show_bug.cgi?id=62764
    Reviewed by Alexey Proskuryakov.
    
    Make these unexpected cases crash in the WebKit nightlies so we can gather more
    information and potentially find a repro case.
    
    * loader/ResourceLoader.cpp:
    (WebCore::ResourceLoader::willSendRequest):
    (WebCore::ResourceLoader::didSendData):
    (WebCore::ResourceLoader::didReceiveResponse):
    (WebCore::ResourceLoader::didReceiveData):
    (WebCore::ResourceLoader::didFinishLoading):
    (WebCore::ResourceLoader::didFail):
    (WebCore::ResourceLoader::wasBlocked):
    (WebCore::ResourceLoader::cannotShowURL):
    (WebCore::ResourceLoader::shouldUseCredentialStorage):
    (WebCore::ResourceLoader::willCacheResponse):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/ResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/ResourceLoader.cpp
@@ -225,7 +225,10 @@ void ResourceLoader::clearResourceData()
 
 void ResourceLoader::willSendRequest(ResourceRequest& request, const ResourceResponse& redirectResponse)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     // Protect this in this delegate method since the additional processing can do
     // anything including possibly derefing this; one example of this is Radar 3266216.
     RefPtr<ResourceLoader> protector(this);
@@ -248,12 +251,18 @@ void ResourceLoader::willSendRequest(ResourceRequest& request, const ResourceRes
 
 void ResourceLoader::didSendData(unsigned long long, unsigned long long)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
 }
 
 void ResourceLoader::didReceiveResponse(const ResourceResponse& r)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     ASSERT(!m_reachedTerminalState);
 
     // Protect this in this delegate method since the additional processing can do
@@ -271,6 +280,10 @@ void ResourceLoader::didReceiveResponse(const ResourceResponse& r)
 
 void ResourceLoader::didReceiveData(const char* data, int length, long long encodedDataLength, bool allAtOnce)
 {
+    if (!m_cancelled && !fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!m_cancelled && !fastMallocSize(documentLoader()->frame()))
+        CRASH();
     // The following assertions are not quite valid here, since a subclass
     // might override didReceiveData in a way that invalidates them. This
     // happens with the steps listed in 3266216
@@ -445,13 +458,19 @@ void ResourceLoader::didReceiveData(ResourceHandle*, const char* data, int lengt
 
 void ResourceLoader::didFinishLoading(ResourceHandle*, double finishTime)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     didFinishLoading(finishTime);
 }
 
 void ResourceLoader::didFail(ResourceHandle*, const ResourceError& error)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
     if (documentLoader()->applicationCacheHost()->maybeLoadFallbackForError(this, error))
         return;
@@ -461,19 +480,28 @@ void ResourceLoader::didFail(ResourceHandle*, const ResourceError& error)
 
 void ResourceLoader::wasBlocked(ResourceHandle*)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     didFail(blockedError());
 }
 
 void ResourceLoader::cannotShowURL(ResourceHandle*)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     didFail(cannotShowURLError());
 }
 
 bool ResourceLoader::shouldUseCredentialStorage()
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     RefPtr<ResourceLoader> protector(this);
     return frameLoader()->client()->shouldUseCredentialStorage(documentLoader(), identifier());
 }
@@ -509,7 +537,10 @@ void ResourceLoader::receivedCancellation(const AuthenticationChallenge&)
 
 void ResourceLoader::willCacheResponse(ResourceHandle*, CacheStoragePolicy& policy)
 {
-    ASSERT(documentLoader()->frame());
+    if (!fastMallocSize(documentLoader()->applicationCacheHost()))
+        CRASH();
+    if (!fastMallocSize(documentLoader()->frame()))
+        CRASH();
     // <rdar://problem/7249553> - There are reports of crashes with this method being called
     // with a null m_frame->settings(), which can only happen if the frame doesn't have a page.
     // Sadly we have no reproducible cases of this.
