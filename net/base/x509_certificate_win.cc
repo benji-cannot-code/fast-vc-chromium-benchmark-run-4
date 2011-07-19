292b9adf932ae770c7133297148b0a3006b8fbcb
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 05:12:17 +0000

    Cache the underlying OS certificate handle within the X509CertificateCache, rather than caching raw X509Certificate pointers.
    TEST=X509CertificateTest.Cache, X509CertificateTest.Intermediates
    BUG=32623, 47648, 49377, 68448, 70216, 77374, 78038
    
    Review URL: http://codereview.chromium.org/2944008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92977 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate_win.cc
+++ net/base/x509_certificate_win.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/x509_certificate.h"
 
-#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/pickle.h"
 #include "base/sha1.h"
@@ -606,9 +605,7 @@ X509Certificate* X509Certificate::CreateSelfSigned(
   if (!cert_handle)
     return NULL;
 
-  X509Certificate* cert = CreateFromHandle(cert_handle,
-                                           SOURCE_LONE_CERT_IMPORT,
-                                           OSCertHandles());
+  X509Certificate* cert = CreateFromHandle(cert_handle, OSCertHandles());
   FreeOSCertHandle(cert_handle);
   return cert;
 }
@@ -632,36 +629,6 @@ void X509Certificate::GetDNSNames(std::vector<std::string>* dns_names) const {
   }
   if (dns_names->empty())
     dns_names->push_back(subject_.common_name);
-}
-
-class GlobalCertStore {
- public:
-  HCERTSTORE cert_store() {
-    return cert_store_;
-  }
-
- private:
-  friend struct base::DefaultLazyInstanceTraits<GlobalCertStore>;
-
-  GlobalCertStore()
-      : cert_store_(CertOpenStore(CERT_STORE_PROV_MEMORY, 0, NULL, 0, NULL)) {
-  }
-
-  ~GlobalCertStore() {
-    CertCloseStore(cert_store_, 0 /* flags */);
-  }
-
-  const HCERTSTORE cert_store_;
-
-  DISALLOW_COPY_AND_ASSIGN(GlobalCertStore);
-};
-
-static base::LazyInstance<GlobalCertStore> g_cert_store(
-    base::LINKER_INITIALIZED);
-
-// static
-HCERTSTORE X509Certificate::cert_store() {
-  return g_cert_store.Get().cert_store();
 }
 
 int X509Certificate::VerifyInternal(const std::string& hostname,
