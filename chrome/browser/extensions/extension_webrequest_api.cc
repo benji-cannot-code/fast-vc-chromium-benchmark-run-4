3ed9ceaf82618eacbc6f44f2f298ab961267dc88
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 09:28:47 +0000

    Added 'wasCached' parameter to webRequest API
    Added a parameter that indicates whether a request was answered from the cache on disk.
    
    BUG=81262
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/7390006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92990 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webrequest_api.cc
+++ chrome/browser/extensions/extension_webrequest_api.cc
@@ -508,6 +508,7 @@ void ExtensionWebRequestEventRouter::OnBeforeRedirect(
   dict->SetInteger(keys::kStatusCodeKey, http_status_code);
   if (!response_ip.empty())
     dict->SetString(keys::kIpKey, response_ip);
+  dict->SetBoolean(keys::kFromCache, request->was_cached());
   dict->SetDouble(keys::kTimeStampKey, time.ToDoubleT() * 1000);
   if (extra_info_spec & ExtraInfoSpec::RESPONSE_HEADERS) {
     dict->Set(keys::kResponseHeadersKey,
@@ -557,6 +558,7 @@ void ExtensionWebRequestEventRouter::OnResponseStarted(
   dict->SetString(keys::kUrlKey, request->url().spec());
   if (!response_ip.empty())
     dict->SetString(keys::kIpKey, response_ip);
+  dict->SetBoolean(keys::kFromCache, request->was_cached());
   dict->SetInteger(keys::kStatusCodeKey, response_code);
   dict->SetDouble(keys::kTimeStampKey, time.ToDoubleT() * 1000);
   if (extra_info_spec & ExtraInfoSpec::RESPONSE_HEADERS) {
@@ -608,6 +610,7 @@ void ExtensionWebRequestEventRouter::OnCompleted(
   dict->SetInteger(keys::kStatusCodeKey, response_code);
   if (!response_ip.empty())
     dict->SetString(keys::kIpKey, response_ip);
+  dict->SetBoolean(keys::kFromCache, request->was_cached());
   dict->SetDouble(keys::kTimeStampKey, time.ToDoubleT() * 1000);
   if (extra_info_spec & ExtraInfoSpec::RESPONSE_HEADERS) {
     dict->Set(keys::kResponseHeadersKey,
@@ -653,6 +656,7 @@ void ExtensionWebRequestEventRouter::OnErrorOccurred(
   dict->SetString(keys::kUrlKey, request->url().spec());
   if (!response_ip.empty())
     dict->SetString(keys::kIpKey, response_ip);
+  dict->SetBoolean(keys::kFromCache, request->was_cached());
   dict->SetString(keys::kErrorKey,
                   net::ErrorToString(request->status().os_error()));
   dict->SetDouble(keys::kTimeStampKey, time.ToDoubleT() * 1000);
