a46cd6915bbc186fd8843160a212c89cb8eb5c9e
serya@google.com <serya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 12:08:14 +0000

    Expose new file browser as chrome:files
    BUG=chromium-os:14342
    TEST=ExtensionManifestTest.FileManagerURLOverride, ExtensionURLRewriteBrowserTest.FileManagerURL
    
    Review URL: http://codereview.chromium.org/7366003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -86,6 +86,10 @@ extern const char kChromeUIUserImageURL[];
 extern const char kChromeUIEnterpriseEnrollmentURL[];
 #endif
 
+#if defined(FILE_MANAGER_EXTENSION)
+extern const char kChromeUIFileManagerURL[];
+#endif
+
 // chrome components of URLs. Should be kept in sync with the full URLs above.
 extern const char kChromeUIAboutHost[];
 extern const char kChromeUIAppCacheInternalsHost[];
@@ -191,6 +195,10 @@ extern const char kChromeUINetworkMenu[];
 
 extern const char kEULAPathFormat[];
 extern const char kOemEulaURLPath[];
+#endif
+
+#if defined(FILE_MANAGER_EXTENSION)
+extern const char kChromeUIFileManagerHost[];
 #endif
 
 // Options sub-pages.
