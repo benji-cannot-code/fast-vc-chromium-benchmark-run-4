bd360dcea609cbe151345e7fc5d5c7c86feee9f0
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 00:02:57 +0000

    [Mac] Only highlight the button cell contents for AvatarButton.
    BUG=89046
    TEST=Install Justin Bieber and run with --multi-profiles. Click on the beaker. No gray background on button but beaker dims.
    
    
    Review URL: http://codereview.chromium.org/7412002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser/avatar_button.mm
+++ chrome/browser/ui/cocoa/browser/avatar_button.mm
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [button_ setImagePosition:NSImageOnly];
     [[button_ cell] setImageScaling:NSImageScaleProportionallyDown];
     [[button_ cell] setImageDimsWhenDisabled:NO];
+    [[button_ cell] setHighlightsBy:NSContentsCellMask];
+    [[button_ cell] setShowsStateBy:NSContentsCellMask];
     [button_ setBordered:NO];
     [button_ setTarget:self];
     [button_ setAction:@selector(buttonClicked:)];
