21f60aa1cd15d47b074f49d03e2cdae7ab5928be
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-19 22:58:59 +0000

    Build fix after r91307.
    * Platform/Logging.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/Logging.h
+++ third_party/WebKit/Source/WebKit2/Platform/Logging.h
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebKitLogging_h
 #define WebKitLogging_h
 
-#include "PlatformString.h"
 #include <wtf/Assertions.h>
+#include <wtf/text/WTFString.h>
 
 #if !LOG_DISABLED
 
