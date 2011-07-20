03b7e629da488aad7927278c08d994f5c14a0bd2
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-20 00:48:47 +0000

    Remove some unused code in FormDataStreamCFNet
    Rubber-stamped by Steve Falkenburg.
    
    * platform/network/cf/FormDataStreamCFNet.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/FormDataStreamCFNet.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/FormDataStreamCFNet.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FileSystem.h"
 #include "FormData.h"
 #include <CFNetwork/CFURLRequestPriv.h>
-#include <CoreFoundation/CFStreamAbstract.h>
 #include <sys/types.h>
 #include <wtf/Assertions.h>
 #include <wtf/HashMap.h>
@@ -48,13 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCoreSystemInterface.h"
 #elif PLATFORM(WIN)
 #include <WebKitSystemInterface/WebKitSystemInterface.h>
-#endif
-
-#define USE_V1_CFSTREAM_CALLBACKS
-#ifdef USE_V1_CFSTREAM_CALLBACKS
-typedef CFReadStreamCallBacksV1 WCReadStreamCallBacks;
-#else
-typedef CFReadStreamCallBacks WCReadStreamCallBacks;
 #endif
 
 namespace WebCore {
