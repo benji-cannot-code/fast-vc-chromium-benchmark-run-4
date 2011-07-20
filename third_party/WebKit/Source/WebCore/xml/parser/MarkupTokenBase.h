7814b629c6509b1ef4fb689bf3438eef468ce639
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-20 21:56:05 +0000

    2011-07-20  Daniel Bates  <dbates@rim.com>
            Attempt to fix the GTK build after changeset 91396 <http://trac.webkit.org/changeset/91396>
            (https://bugs.webkit.org/show_bug.cgi?id=64764).
    
            * xml/parser/MarkupTokenBase.h: #include <stdio.h> in debug build for printf(3), which is used in
            MarkupTokenBase::printString() and MarkupTokenBase::printAttrs().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenBase.h
+++ third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenBase.h
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/PassOwnPtr.h>
 #include <wtf/Vector.h>
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace WebCore {
 
 class DoctypeDataBase {
