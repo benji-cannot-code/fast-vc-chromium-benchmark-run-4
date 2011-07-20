17e7f1586e9aae793f94fbdd572e5c55935a68c3
qsr@google.com <qsr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 23:51:39 +0000

    Correct crash when calling ShowLoginDialog.
    BUG=89800,89783
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7458013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/sync_setup_handler.cc
+++ chrome/browser/ui/webui/options/sync_setup_handler.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/sync_setup_flow.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "grit/chromium_strings.h"
@@ -332,11 +331,6 @@ void SyncSetupHandler::SetFlow(SyncSetupFlow* flow) {
 
 void SyncSetupHandler::Focus() {
   static_cast<RenderViewHostDelegate*>(web_ui_->tab_contents())->Activate();
-}
-
-void SyncSetupHandler::ShowSyncSetup() {
-  BrowserList::GetLastActiveWithProfile(web_ui_->GetProfile())->ShowOptionsTab(
-      chrome::kSyncSetupSubPage);
 }
 
 void SyncSetupHandler::OnDidClosePage(const ListValue* args) {
