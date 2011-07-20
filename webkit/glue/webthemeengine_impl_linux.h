e858b82dbf4ec21c070e9f3601565dabd164659b
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 23:17:19 +0000

    Dup of Issue 7465011: Larger Combo box Hit Targets for TOUCH_UI
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7465012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93279 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webthemeengine_impl_linux.h
+++ webkit/glue/webthemeengine_impl_linux.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,6 +20,7 @@ class WebThemeEngineImpl : public WebKit::WebThemeEngine {
       WebKit::WebThemeEngine::State,
       const WebKit::WebRect&,
       const WebKit::WebThemeEngine::ExtraParams*);
+  virtual int getPopupListBoxMinimumRowHeight() const;
 };
 
 }  // namespace webkit_glue
