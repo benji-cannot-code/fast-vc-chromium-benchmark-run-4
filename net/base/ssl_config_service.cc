b35586e346f96ae6db0f4f40667e1610fb43e3d6
rkn@chromium.org <rkn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 23:50:34 +0000

    Adds command-line switch for TLS origin bound certificate extension. This extension is disabled by default. To enable, pass in the command line switch "--enable-ssl-origin-bound-certs".
    BUG=88782
    TEST=None
    
    Review URL: http://codereview.chromium.org/7460002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93289 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ssl_config_service.cc
+++ net/base/ssl_config_service.cc
@@ -18,6 +18,7 @@ SSLConfig::SSLConfig()
     : rev_checking_enabled(true), ssl3_enabled(true),
       tls1_enabled(true),
       dns_cert_provenance_checking_enabled(false), cached_info_enabled(false),
+      origin_bound_certs_enabled(false),
       false_start_enabled(true),
       send_client_cert(false), verify_ev_cert(false), ssl3_fallback(false) {
 }
@@ -56,6 +57,7 @@ bool SSLConfigService::IsKnownFalseStartIncompatibleServer(
 }
 
 static bool g_cached_info_enabled = false;
+static bool g_origin_bound_certs_enabled = false;
 static bool g_false_start_enabled = true;
 static bool g_dns_cert_provenance_checking = false;
 
@@ -89,6 +91,16 @@ bool SSLConfigService::cached_info_enabled() {
   return g_cached_info_enabled;
 }
 
+// static
+void SSLConfigService::EnableOriginBoundCerts() {
+  g_origin_bound_certs_enabled = true;
+}
+
+// static
+bool SSLConfigService::origin_bound_certs_enabled() {
+  return g_origin_bound_certs_enabled;
+}
+
 void SSLConfigService::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
@@ -106,6 +118,7 @@ void SSLConfigService::SetSSLConfigFlags(SSLConfig* ssl_config) {
   ssl_config->dns_cert_provenance_checking_enabled =
       g_dns_cert_provenance_checking;
   ssl_config->cached_info_enabled = g_cached_info_enabled;
+  ssl_config->origin_bound_certs_enabled = g_origin_bound_certs_enabled;
 }
 
 void SSLConfigService::ProcessConfigUpdate(const SSLConfig& orig_config,
