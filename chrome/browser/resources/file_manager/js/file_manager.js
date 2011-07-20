ef7f6ba7fbe1a3f2fcb225ce16e784fa4c7264c5
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 14:25:48 +0000

    Revert 93187 - Minor code style cleanup. Added id3 metadata parser to filemanager.
    BUG=14879
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7217007
    
    TBR=zvorygin@chromium.org
    Review URL: http://codereview.chromium.org/7462014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const EMPTY_IMAGE_URI = 'data:image/gif;base64,'
-        + 'R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw%3D%3D';
-
-// Thumbnail view is painful without the exif reader.
-const ENABLE_THUMBNAIL_VIEW = ENABLE_METADATA;
-
 var g_slideshow_data = null;
 
 /**
@@ -1409,10 +1403,7 @@ FileManager.prototype = {
    */
   FileManager.prototype.updatePreview_ = function() {
     // Clear the preview image first, in case the thumbnail takes long to load.
-    // Do not set url to empty string in plugins because it crashes browser,
-    // instead we use empty 1x1 gif
-    this.previewImage_.src = EMPTY_IMAGE_URI;
-
+    this.previewImage_.src = '';
     // The transparent-background class is used to display the checkerboard
     // background for image thumbnails.  We don't want to display it for
     // non-thumbnail preview images.
