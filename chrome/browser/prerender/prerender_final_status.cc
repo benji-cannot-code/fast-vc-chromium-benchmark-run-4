38908af8d0dadbf7a73fe1db481da5de2cc26f34
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 15:01:02 +0000

    Adding API to cancel all prerenders.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7450001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_final_status.cc
+++ chrome/browser/prerender/prerender_final_status.cc
@@ -44,6 +44,7 @@ const char* kFinalStatusNames[] = {
   "Fragment Mismatch",
   "SSL Client Certificate Requested",
   "Cache or History Cleared",
+  "Cancelled",
   "Max",
 };
 COMPILE_ASSERT(arraysize(kFinalStatusNames) == FINAL_STATUS_MAX + 1,
--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -172,6 +172,7 @@ struct PrerenderManager::PrerenderContentsData {
   PrerenderContentsData(PrerenderContents* contents, base::Time start_time)
       : contents_(contents),
         start_time_(start_time) {
+    CHECK(contents);
   }
 };
 
@@ -406,6 +407,15 @@ void PrerenderManager::DestroyPrerenderForRenderView(
   if (it != prerender_list_.end()) {
     PrerenderContents* prerender_contents = it->contents_;
     prerender_contents->Destroy(final_status);
+  }
+}
+
+void PrerenderManager::CancelAllPrerenders() {
+  DCHECK(CalledOnValidThread());
+  while (!prerender_list_.empty()) {
+    PrerenderContentsData data = prerender_list_.front();
+    DCHECK(data.contents_);
+    data.contents_->Destroy(FINAL_STATUS_CANCELLED);
   }
 }
 
@@ -812,6 +822,7 @@ void PrerenderManager::PeriodicCleanup() {
   for (std::list<PrerenderContentsData>::iterator it = prerender_list_.begin();
        it != prerender_list_.end();
        ++it) {
+    DCHECK(it->contents_);
     prerender_contents.push_back(it->contents_);
   }
   for (std::vector<PrerenderContents*>::iterator it =
