c3de6ccb77630130b0500851aca45da3a3fb6cdf
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 17:36:16 +0000

    Fix issue in print preview where selecting a cloud printer does not generate a new preview.
    Adds handling of the request ID to PrintWebViewHelper::UpdatePrintSettingsCloud
    
    
    BUG=http://code.google.com/p/chromium-os/issues/detail?id=16082
    TEST=Run chrome with --enable-cloud-printers and --enable-print-preview.
    Open a web page and hit ctrl-p to open print preview
    Select a cloud printer from the dropdown.
    You should get a preview.
    
    
    Review URL: http://codereview.chromium.org/7461014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -701,6 +701,17 @@ bool PrintWebViewHelper::InitPrintSettings(WebKit::WebFrame* frame,
   return true;
 }
 
+bool PrintWebViewHelper::UpdatePrintSettingsRequestId(
+    const DictionaryValue& job_settings,
+    PrintMsg_Print_Params* params) {
+  if (!job_settings.GetInteger(printing::kPreviewRequestID,
+                               &(params->preview_request_id))) {
+    NOTREACHED();
+    return false;
+  }
+  return true;
+}
+
 bool PrintWebViewHelper::UpdatePrintSettingsCloud(
     const DictionaryValue& job_settings) {
   // Document cookie and pages are set by the
@@ -719,6 +730,8 @@ bool PrintWebViewHelper::UpdatePrintSettingsCloud(
   settings.params.desired_dpi = 72;
   settings.params.selection_only = false;
   settings.params.supports_alpha_blend = false;
+  if (!UpdatePrintSettingsRequestId(job_settings, &(settings.params)))
+    return false;
   // TODO(abodenha@chromium.org) Parse page ranges from the job_settings.
   print_pages_params_.reset(new PrintMsg_PrintPages_Params(settings));
   return true;
@@ -734,11 +747,8 @@ bool PrintWebViewHelper::UpdatePrintSettingsLocal(
   if (settings.params.dpi < kMinDpi || !settings.params.document_cookie)
     return false;
 
-  if (!job_settings.GetInteger(printing::kPreviewRequestID,
-                               &settings.params.preview_request_id)) {
-    NOTREACHED();
+  if (!UpdatePrintSettingsRequestId(job_settings, &(settings.params)))
     return false;
-  }
 
   print_pages_params_.reset(new PrintMsg_PrintPages_Params(settings));
   Send(new PrintHostMsg_DidGetDocumentCookie(routing_id(),
