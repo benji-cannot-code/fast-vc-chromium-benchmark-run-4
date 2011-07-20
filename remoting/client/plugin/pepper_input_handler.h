433e56720ee668bd8e1847a54fe29be35b7a19a4
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 18:53:59 +0000

    Revert 93223 - Reland http://codereview.chromium.org/7452002/
    Update chromoting input events. (Clang caught this. Thanks, Clang!)
    
    Note I'm leaving in temporary backwards-compatibility.
    
    brettw, just need a rubber-stamp. This is the same as the last CL. awong/garykac/sergeyu, looking for someone on chromoting team to glance at my changes and make sure I didn't do anything you don't like. This is a high priority for 14, so please look if you can.
    
    BUG=None
    TEST=ppapi tests
    
    Review URL: http://codereview.chromium.org/7466002
    
    TBR=dmichael@chromium.org
    Review URL: http://codereview.chromium.org/7471006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93229 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_input_handler.h
+++ remoting/client/plugin/pepper_input_handler.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,11 +12,6 @@ struct PP_InputEvent_Character;
 struct PP_InputEvent_Key;
 struct PP_InputEvent_Mouse;
 
-namespace pp {
-class KeyboardInputEvent;
-class MouseInputEvent;
-}  // namespace pp
-
 namespace remoting {
 
 class PepperInputHandler : public InputHandler {
@@ -28,12 +23,12 @@ class PepperInputHandler : public InputHandler {
 
   virtual void Initialize();
 
-  void HandleKeyEvent(bool keydown, const pp::KeyboardInputEvent& event);
-  void HandleCharacterEvent(const pp::KeyboardInputEvent& event);
+  void HandleKeyEvent(bool keydown, const PP_InputEvent_Key& event);
+  void HandleCharacterEvent(const PP_InputEvent_Character& event);
 
-  void HandleMouseMoveEvent(const pp::MouseInputEvent& event);
+  void HandleMouseMoveEvent(const PP_InputEvent_Mouse& event);
   void HandleMouseButtonEvent(bool button_down,
-                              const pp::MouseInputEvent& event);
+                              const PP_InputEvent_Mouse& event);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(PepperInputHandler);
