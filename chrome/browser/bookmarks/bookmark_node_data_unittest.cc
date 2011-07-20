2855290a37cb4a32e2a5ff7fc060178e27403168
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 04:01:37 +0000

    bookmarks: Use string16 instead of std::wstring in bookmark_node_data_unittest.cc.
    This fixes the remaining occurrences of std::wstring under bookmarks/ directory.
    
    BUG=32013
    TEST=unit_tests --gtest_filter=BookmarkNodeDataTest.*
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7461010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_node_data_unittest.cc
+++ chrome/browser/bookmarks/bookmark_node_data_unittest.cc
@@ -55,7 +55,7 @@ TEST_F(BookmarkNodeDataTest, BogusRead) {
 // read it.
 TEST_F(BookmarkNodeDataTest, JustURL) {
   const GURL url("http://google.com");
-  const std::wstring title(L"title");
+  const string16 title(ASCIIToUTF16("title"));
 
   ui::OSExchangeData data;
   data.SetURL(url, title);
@@ -107,7 +107,7 @@ TEST_F(BookmarkNodeDataTest, URL) {
 
   // Writing should also put the URL and title on the clipboard.
   GURL read_url;
-  std::wstring read_title;
+  string16 read_title;
   EXPECT_TRUE(data2.GetURLAndTitle(&read_url, &read_title));
   EXPECT_EQ(url, read_url);
   EXPECT_EQ(title, read_title);
