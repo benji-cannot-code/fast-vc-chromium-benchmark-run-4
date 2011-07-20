c7ccf83ba53451eec97e196e0fab76c5dc464b1f
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-20 00:46:57 +0000

    2011-07-19  Simon Fraser  <simon.fraser@apple.com>
            REGRESSION (r91136-r91146): 40 tests failing on Windows 7 Release (Tests)
            https://bugs.webkit.org/show_bug.cgi?id=64808
    
            Reviewed by Adam Roben.
    
            Initializing m_uncommittedChanges to a non-zero value
            caused the first call to noteLayerPropertyChanged() to
            not call m_client->notifySyncRequired(). This resulted in
            animations never getting committed on Windows, which broke
            a lot of tests.
    
            * platform/graphics/ca/GraphicsLayerCA.cpp:
            (WebCore::GraphicsLayerCA::GraphicsLayerCA):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91324 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -251,7 +251,7 @@ GraphicsLayerCA::GraphicsLayerCA(GraphicsLayerClient* client)
     , m_contentsLayerPurpose(NoContentsLayer)
     , m_contentsLayerHasBackgroundColor(false)
     , m_allowTiledLayer(true)
-    , m_uncommittedChanges(ContentsScaleChanged)
+    , m_uncommittedChanges(0)
 {
     m_layer = PlatformCALayer::create(PlatformCALayer::LayerTypeWebLayer, this);
 
@@ -260,6 +260,7 @@ GraphicsLayerCA::GraphicsLayerCA(GraphicsLayerClient* client)
 #endif
 
     updateDebugIndicators();
+    noteLayerPropertyChanged(ContentsScaleChanged);
 }
 
 GraphicsLayerCA::~GraphicsLayerCA()
