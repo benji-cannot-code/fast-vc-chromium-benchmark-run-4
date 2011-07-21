e26fe80bf880f5790c09c32bdea058f0198cd313
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-21 05:56:42 +0000

    Unreviewed, rolling out r91332. http://trac.webkit.org/changeset/91332 https://bugs.webkit.org/show_bug.cgi?id=64925
    Crashes css3/images/optimize-contrast-canvas.html on safari-
    mac,chromium-mac (Requested by mikelawther on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-07-20
    
    * html/HTMLCanvasElement.cpp:
    (WebCore::HTMLCanvasElement::makePresentationCopy):
    (WebCore::HTMLCanvasElement::copiedImage):
    * html/canvas/WebGLRenderingContext.cpp:
    (WebCore::WebGLRenderingContext::videoFrameToImage):
    * platform/graphics/GraphicsContext.cpp:
    (WebCore::GraphicsContext::drawImage):
    * platform/graphics/GraphicsContext.h:
    * platform/graphics/Image.h:
    * platform/graphics/ImageBuffer.h:
    * platform/graphics/cairo/ImageBufferCairo.cpp:
    (WebCore::ImageBuffer::copyImage):
    * platform/graphics/cg/GraphicsContextCG.cpp:
    * platform/graphics/cg/ImageBufferCG.cpp:
    (WebCore::ImageBuffer::copyImage):
    (WebCore::cgImage):
    (WebCore::ImageBuffer::draw):
    (WebCore::ImageBuffer::drawPattern):
    (WebCore::ImageBuffer::clip):
    (WebCore::ImageBuffer::toDataURL):
    * platform/graphics/cg/ImageCG.cpp:
    (WebCore::imageWithColorSpace):
    (WebCore::BitmapImage::draw):
    (WebCore::Image::drawPattern):
    * platform/graphics/filters/FETile.cpp:
    (WebCore::FETile::apply):
    * platform/graphics/qt/ImageBufferQt.cpp:
    (WebCore::ImageBuffer::copyImage):
    (WebCore::ImageBuffer::draw):
    (WebCore::ImageBuffer::drawPattern):
    * platform/graphics/skia/ImageBufferSkia.cpp:
    (WebCore::ImageBuffer::copyImage):
    * platform/graphics/wince/ImageBufferWinCE.cpp:
    (WebCore::ImageBuffer::copyImage):
    (WebCore::ImageBuffer::draw):
    (WebCore::ImageBuffer::drawPattern):
    * platform/graphics/wx/ImageBufferWx.cpp:
    (WebCore::ImageBuffer::copyImage):
    (WebCore::ImageBuffer::draw):
    (WebCore::ImageBuffer::drawPattern):
    * rendering/svg/RenderSVGResourcePattern.cpp:
    (WebCore::RenderSVGResourcePattern::applyResource):
    * svg/SVGFEImageElement.cpp:
    (WebCore::SVGFEImageElement::build):
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::nativeImageForCurrentFrame):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.cpp
@@ -310,7 +310,7 @@ void HTMLCanvasElement::makePresentationCopy()
 {
     if (!m_presentedImage) {
         // The buffer contains the last presented data, so save a copy of it.
-        m_presentedImage = buffer()->copyImage(CopyBackingStore);
+        m_presentedImage = buffer()->copyImage();
     }
 }
 
@@ -473,7 +473,7 @@ Image* HTMLCanvasElement::copiedImage() const
     if (!m_copiedImage && buffer()) {
         if (m_context)
             m_context->paintRenderingResultsToCanvas();
-        m_copiedImage = buffer()->copyImage(CopyBackingStore);
+        m_copiedImage = buffer()->copyImage();
     }
     return m_copiedImage.get();
 }
