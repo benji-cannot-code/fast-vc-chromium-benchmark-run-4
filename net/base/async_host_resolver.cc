9c7c709e8261d6ef1a40f7b49d986af9b2a068dd
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 15:58:49 +0000

    AsyncHostResolver: destruct pending requests to avoid memory leak.
    BUG=60149
    TEST=net_unittests --gtest_filter="AsyncHostResolverTest*"
    
    Review URL: http://codereview.chromium.org/7470002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93406 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/async_host_resolver.cc
+++ net/base/async_host_resolver.cc
@@ -149,10 +149,17 @@ AsyncHostResolver::AsyncHostResolver(const IPEndPoint& dns_server,
 }
 
 AsyncHostResolver::~AsyncHostResolver() {
+  // Destroy request lists.
   for (KeyRequestListMap::iterator it = requestlist_map_.begin();
        it != requestlist_map_.end(); ++it)
     STLDeleteElements(&it->second);
+
+  // Destroy transactions.
   STLDeleteElements(&transactions_);
+
+  // Destroy pending requests.
+  for (size_t i = 0; i < arraysize(pending_requests_); ++i)
+    STLDeleteElements(&pending_requests_[i]);
 }
 
 int AsyncHostResolver::Resolve(const RequestInfo& info,
@@ -161,7 +168,6 @@ int AsyncHostResolver::Resolve(const RequestInfo& info,
                                RequestHandle* out_req,
                                const BoundNetLog& source_net_log) {
   DCHECK(addresses);
-
   IPAddressNumber ip_number;
   std::string dns_name;
   int rv = ERR_UNEXPECTED;
@@ -319,9 +325,8 @@ AsyncHostResolver::Request* AsyncHostResolver::CreateNewRequest(
       NetLog::SOURCE_ASYNC_HOST_RESOLVER_REQUEST);
 
   int id = next_request_id_++;
-  Request* request = new Request(
+  return new Request(
       source_net_log, request_net_log, id, info, key, callback, addresses);
-  return request;
 }
 
 bool AsyncHostResolver::AttachToRequestList(Request* request) {
