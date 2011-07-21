6255985fe28299dcf54284580146ef8c62f5cd6d
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 17:07:11 +0000

    Switch to finer grained histograms for NTP hover time.
    BUG=89341
    TEST=none
    
    Review URL: http://codereview.chromium.org/7471001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp/most_visited.js
+++ chrome/browser/resources/ntp/most_visited.js
@@ -527,8 +527,8 @@ var MostVisited = (function() {
 
     // Thumbnail hovering
 
-    // TODO(mmenke):  Either implement prerendering based on hovering,
-    //                or remove this code.
+    // TODO(mmenke):  Either implement preconnect/prerendering based on
+    //                hovering, or remove this code.
 
     /**
      * Record the time the mouse has been hovering over a thumbnail.
@@ -539,13 +539,13 @@ var MostVisited = (function() {
       if (!this.hoverStartTime_)
         return;
       var hoverDuration = (new Date()).getTime() - this.hoverStartTime_;
-      if (hoverDuration > 4000)
-        hoverDuration = 4000;
+      if (hoverDuration > 500)
+        hoverDuration = 500;
       chrome.send('recordInHistogram',
                   [clicked ? 'NewTabPage.HoverTimeClicked'
                            : 'NewTabPage.HoverTimeNotClicked',
                    hoverDuration,
-                   4000]);
+                   500]);
       this.hoverStartTime_ = null;
     },
 
