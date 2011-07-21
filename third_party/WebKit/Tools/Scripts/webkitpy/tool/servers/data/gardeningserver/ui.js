e33b0a0b9bbb25c8f069f7b18f798ac75df7ccfe
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-21 19:22:47 +0000

    Add missing column header.
    * Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/data/gardeningserver/ui.js
@@ -38,7 +38,7 @@ ui.regressionsContainer = function()
         '<table class="results-summary">' +
             '<thead>' +
                 '<tr>' +
-                    '<th>Test</th><th>Bot</th><th>Regression Range</th><th>Frequency</th>' +
+                    '<th></th><th>Test</th><th>Bot</th><th>Regression Range</th><th>Frequency</th>' +
             '</thead>' +
             '<tbody></tbody>' +
         '</table>');
