b00a155d392500aa637a5f6b78ab5c5ea2137ff3
yuzo@chromium.org <yuzo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 08:42:08 +0000

    Record language usage as UMA histograms.
    Record accept languages and application language as UMA histograms on
    browser start-up.
    
    See also http://crosbug.com/17419
    
    BUG=none
    TEST=run the unit tests
    
    
    Review URL: http://codereview.chromium.org/7348004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_main.cc
+++ chrome/browser/browser_main.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/first_run/first_run_browser_process.h"
 #include "chrome/browser/first_run/upgrade_util.h"
 #include "chrome/browser/jankometer.h"
+#include "chrome/browser/language_usage_metrics.h"
 #include "chrome/browser/metrics/field_trial_synchronizer.h"
 #include "chrome/browser/metrics/histogram_synchronizer.h"
 #include "chrome/browser/metrics/metrics_log.h"
@@ -1884,6 +1885,10 @@ int BrowserMain(const MainFunctionParams& parameters) {
   HandleTestParameters(parsed_command_line);
   RecordBreakpadStatusUMA(metrics);
   about_flags::RecordUMAStatistics(local_state);
+  LanguageUsageMetrics::RecordAcceptLanguages(
+      profile->GetPrefs()->GetString(prefs::kAcceptLanguages));
+  LanguageUsageMetrics::RecordApplicationLanguage(
+      g_browser_process->GetApplicationLocale());
 
 #if defined(OS_CHROMEOS)
   metrics->StartExternalMetrics();
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1468,6 +1468,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/instant/instant_loader_manager_unittest.cc',
         'browser/instant/promo_counter_unittest.cc',
         'browser/internal_auth_unittest.cc',
+        'browser/language_usage_metrics_unittest.cc',
         'browser/mac/keystone_glue_unittest.mm',
         'browser/media/media_internals_unittest.cc',
         'browser/metrics/display_utils_unittest.cc',
