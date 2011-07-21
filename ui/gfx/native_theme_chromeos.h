2a30c329885ad9c80617537482caf150014ecbd3
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 17:33:06 +0000

    Dup of Issue 7465011: Larger Combo box Hit Targets for TOUCH_UI
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7465015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93431 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/native_theme_chromeos.h
+++ ui/gfx/native_theme_chromeos.h
@@ -23,6 +23,8 @@ class NativeThemeChromeos : public gfx::NativeThemeLinux {
                                 State state,
                                 const ExtraParams& extra) const OVERRIDE;
 
+  virtual int GetPopupListBoxMinimumRowHeight() const OVERRIDE;
+
   // NativeThemeLinux overrides
   virtual void PaintScrollbarTrack(SkCanvas* canvas,
       Part part, State state,
