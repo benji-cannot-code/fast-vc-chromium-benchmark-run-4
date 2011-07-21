0c7783802aeeaafb5d109011d43608967dbfedb5
abeera@google.com <abeera@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 00:21:27 +0000

    Code for the Linux virtual printer driver. A backend for CUPS that spools the data to a file and then passes that to Chrome.
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=92378
    
    Review URL: http://codereview.chromium.org/7222011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93298 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ cloud_print/virtual_driver/virtual_driver_switches.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cloud_print/virtual_driver/virtual_driver_switches.h"
+
+namespace switches {
+const char kCloudPrintDeleteFile[]          = "cloud-print-delete-file";
+const char kCloudPrintFile[]                = "cloud-print-file";
+const char kCloudPrintJobTitle[]            = "cloud-print-job-title";
+const char kCloudPrintFileType[]            = "cloud-print-file-type";
+} //namespace switches
+
