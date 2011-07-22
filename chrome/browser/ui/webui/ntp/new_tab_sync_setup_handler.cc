867e49cce40ee89df23f238cf24ce84bf7473f24
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 09:00:40 +0000

    Fix clang error
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.cc
+++ chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.cc
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "content/common/notification_details.h"
 
+NewTabSyncSetupHandler::~NewTabSyncSetupHandler() {
+}
+
 bool NewTabSyncSetupHandler::ShouldShowSyncPromo() {
 #if defined(OS_CHROMEOS)
   // There's no need to show the sync promo on cros since cros users are logged
--- chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.h
+++ chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // tab page.
 class NewTabSyncSetupHandler : public SyncSetupHandler {
  public:
+  virtual ~NewTabSyncSetupHandler();
+
   // Checks if the sync promo should be visible.
   static bool ShouldShowSyncPromo();
 
