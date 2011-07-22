6ab977c085396c13d30c3406ff254078bd0f4d63
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 16:34:13 +0000

    Adding linter for IDL
    Removing current warnings from parser and create a new file
    which will traverse AST after it is built looking for possible
    issues.
    
    Also includes minor comment cleanup for visitor class on which
    the linter is based.
    
    
    TEST= python idl_parser.py
    BUG= http://code.google.com/p/chromium/issues/detail?id=76271
    
    R= sehr@google.com
    Review URL: http://codereview.chromium.org/7468012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93655 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_visitor.py
+++ ppapi/generators/idl_visitor.py
@@ -11,11 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # IDLVisitor
 #
 # The IDLVisitor class will traverse an AST truncating portions of the tree
-# that fail due to class or version filters.  For each node, after the filter
-# passes, the visitor will call the 'Arive' member passing in the node and
-# and data passing in from the parent call.   It will then Visit the children.
-# When done processing children, the visitor will call the 'Depart' member
-# before returning
+# when 'VisitFilter' returns false.  After the filter returns true, for each
+# node, the visitor will call the 'Arrive' member passing in the node and
+# and generic data object from the parent call.  The returned value is then
+# passed to all children who's results are aggregated into a list.  The child
+# results along with the original Arrive result are passed to the Depart
+# function which returns the final result of the Visit.  By default this is
+# the exact value that was return from the original arrive.
 #
 
 class IDLVisitor(object):
@@ -46,9 +48,11 @@ class IDLVisitor(object):
     return out
 
   def Arrive(self, node, data):
+    __pychecker__ = 'unusednames=node'
     return data
 
   def Depart(self, node, data, childdata):
+    __pychecker__ = 'unusednames=node,childdata'
     return data
 
 
