1986e59dc79f4d652b677a40e285eff621c47f1f
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-22 07:30:27 +0000

    Revert r91510 due to Chromium debug test failures.
    * src/WebKit.cpp:
    (WebKit::initialize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Settings.h"
 #include "TextEncoding.h"
 #include "V8Binding.h"
-#include "WebKitClient.h"
 #include "WebMediaPlayerClientImpl.h"
 #include "WebSocket.h"
 #include "WorkerContextExecutionProxy.h"
@@ -56,15 +55,6 @@ static bool s_webKitInitialized = false;
 
 static WebKitClient* s_webKitClient = 0;
 static bool s_layoutTestMode = false;
-
-static bool generateEntropy(unsigned char* buffer, size_t length)
-{
-    if (s_webKitClient) {
-        s_webKitClient->cryptographicallyRandomValues(buffer, length);
-        return true;
-    }
-    return false;
-}
 
 void initialize(WebKitClient* webKitClient)
 {
@@ -88,7 +78,6 @@ void initialize(WebKitClient* webKitClient)
     // this, initializing this lazily probably doesn't buy us much.
     WebCore::UTF8Encoding();
 
-    v8::V8::SetEntropySource(&generateEntropy);
     v8::V8::Initialize();
     WebCore::V8BindingPerIsolateData::ensureInitialized(v8::Isolate::GetCurrent());
 }
