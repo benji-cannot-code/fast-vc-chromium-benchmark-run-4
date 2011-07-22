c97cfcd5bb33065eebdf01049fce280fc16e93ec
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-22 05:34:08 +0000

    https://bugs.webkit.org/show_bug.cgi?id=19271 eliminate PIC branches by changing NaN handling in JSValue::toNumber
    Reviewed by Sam Weinig.
    
    Moving the non-numeric cases out of line seems to be a consistent
    win on SunSpider for me, to the order of about 0.5%.
    
    * runtime/JSCell.h:
    (JSC::JSCell::JSValue::toNumber):
        - Changed to only handle values that are already numbers, moce non-numeric cases out of line.
    * runtime/JSValue.cpp:
    (JSC::JSValue::toNumberSlowCase):
        - Added toNumberSlowCase, handling non-numeric cases.
    * runtime/JSValue.h:
        - Add declaration of toNumberSlowCase.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91555 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h
@@ -237,6 +237,7 @@ namespace JSC {
         JSValue(HashTableDeletedValueTag);
 
         inline const JSValue asValue() const { return *this; }
+        double toNumberSlowCase(ExecState*) const;
         JSObject* toObjectSlowCase(ExecState*, JSGlobalObject*) const;
         JSObject* toThisObjectSlowCase(ExecState*) const;
 
