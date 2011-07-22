ae476809b870f3a137bb599c55c9724e02794f25
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 15:46:36 +0000

    Disable MultiClutchInputManager on Lion.
    BUG=90075
    TEST=Install that thing on Lion (good luck) and gesticulate. No crash.
    Review URL: http://codereview.chromium.org/7481015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93637 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_application_mac.mm
+++ chrome/browser/chrome_browser_application_mac.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/chrome_browser_application_mac.h"
 
 #import "base/logging.h"
+#import "base/mac/mac_util.h"
 #import "base/mac/scoped_nsexception_enabler.h"
 #import "base/metrics/histogram.h"
 #import "base/memory/scoped_nsobject.h"
@@ -25,18 +26,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the debugger when an exception is raised.  -raise sounds more
 // obvious to intercept, but it doesn't catch the original throw
 // because the objc runtime doesn't use it.
-@interface NSException (NSExceptionSwizzle)
-- (id)chromeInitWithName:(NSString*)aName
-                  reason:(NSString*)aReason
-                userInfo:(NSDictionary *)someUserInfo;
+@interface NSException (CrNSExceptionSwizzle)
+- (id)crInitWithName:(NSString*)aName
+              reason:(NSString*)aReason
+            userInfo:(NSDictionary*)someUserInfo;
 @end
 
 static IMP gOriginalInitIMP = NULL;
 
-@implementation NSException (NSExceptionSwizzle)
-- (id)chromeInitWithName:(NSString*)aName
-                  reason:(NSString*)aReason
-                userInfo:(NSDictionary *)someUserInfo {
+@implementation NSException (CrNSExceptionSwizzle)
+- (id)crInitWithName:(NSString*)aName
+              reason:(NSString*)aReason
+            userInfo:(NSDictionary*)someUserInfo {
   // Method only called when swizzled.
   DCHECK(_cmd == @selector(initWithName:reason:userInfo:));
 
@@ -122,6 +123,29 @@ static IMP gOriginalInitIMP = NULL;
 }
 @end
 
+static IMP gOriginalNSBundleLoadIMP = NULL;
+
+@interface NSBundle (CrNSBundleSwizzle)
+- (BOOL)crLoad;
+@end
+
+@implementation NSBundle (CrNSBundleSwizzle)
+- (BOOL)crLoad {
+  // Method only called when swizzled.
+  DCHECK(_cmd == @selector(load));
+
+  // MultiClutchInputManager is broken in Chrome on Lion.
+  // http://crbug.com/90075.
+  if (base::mac::IsOSLionOrLater() &&
+      [[self bundleIdentifier]
+       isEqualToString:@"net.wonderboots.multiclutchinputmanager"]) {
+    return NO;
+  }
+
+  return gOriginalNSBundleLoadIMP(self, _cmd) != nil;
+}
+@end
+
 namespace chrome_browser_application_mac {
 
 // Maximum number of known named exceptions we'll support.  There is
@@ -194,15 +218,21 @@ void CancelTerminate() {
 
 namespace {
 
-// Do-nothing wrapper so that we can arrange to only swizzle
-// -[NSException raise] when DCHECK() is turned on (as opposed to
-// replicating the preprocess logic which turns DCHECK() on).
-BOOL SwizzleNSExceptionInit() {
+void SwizzleInit() {
+  // Do-nothing wrapper so that we can arrange to only swizzle
+  // -[NSException raise] when DCHECK() is turned on (as opposed to
+  // replicating the preprocess logic which turns DCHECK() on).
   gOriginalInitIMP = ObjcEvilDoers::SwizzleImplementedInstanceMethods(
       [NSException class],
       @selector(initWithName:reason:userInfo:),
-      @selector(chromeInitWithName:reason:userInfo:));
-  return YES;
+      @selector(crInitWithName:reason:userInfo:));
+
+  // Avoid loading broken input managers.
+  gOriginalNSBundleLoadIMP =
+      ObjcEvilDoers::SwizzleImplementedInstanceMethods(
+          [NSBundle class],
+          @selector(load),
+          @selector(crLoad));
 }
 
 }  // namespace
@@ -215,8 +245,8 @@ BOOL SwizzleNSExceptionInit() {
   ObjcEvilDoers::ZombieEnable(YES, 10000);
 }
 
-- init {
-  CHECK(SwizzleNSExceptionInit());
+- (id)init {
+  SwizzleInit();
   return [super init];
 }
 
