b0adfb4b15ab9f8dc7eec8e082f28326823197d9
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-22 15:29:06 +0000

    Web Inspector: fix exception when paused in internal script. https://bugs.webkit.org/show_bug.cgi?id=64995
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    Test: inspector/debugger/pause-in-internal-script.html
    
    * inspector/front-end/DebuggerPresentationModel.js:
    (WebInspector.PresenationCallFrame.prototype.sourceLine):
    
    LayoutTests:
    
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    * inspector/debugger/pause-in-internal-script-expected.txt: Added.
    * inspector/debugger/pause-in-internal-script.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerPresentationModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerPresentationModel.js
@@ -782,7 +782,10 @@ WebInspector.PresenationCallFrame.prototype = {
     sourceLine: function(callback)
     {
         var location = this._callFrame.location;
-        this._model._scriptLocationToUILocation(null, location.sourceId, location.lineNumber, location.columnNumber, callback);
+        if (!this.isInternalScript)
+            this._model._scriptLocationToUILocation(null, location.sourceId, location.lineNumber, location.columnNumber, callback);
+        else
+            callback(undefined, location.lineNumber);
     }
 }
 
