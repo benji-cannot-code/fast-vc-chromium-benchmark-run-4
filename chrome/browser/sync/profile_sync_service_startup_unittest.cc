a09fb5b212e4030f6ec0cf9011f13e325a9bb92e
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 03:12:58 +0000

    sync: enable ProfileSyncServiceStartupTests on mac.
    BUG=
    TEST=ProfileSyncServiceStartupTest
    
    
    Review URL: http://codereview.chromium.org/7464033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93537 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_startup_unittest.cc
+++ chrome/browser/sync/profile_sync_service_startup_unittest.cc
@@ -37,13 +37,6 @@ ACTION_P(InvokeCallback, callback_result) {
   delete arg0;
 }
 
-// TODO(skrul) This test fails on the mac. See http://crbug.com/33443
-#if defined(OS_MACOSX)
-#define SKIP_MACOSX(test) DISABLED_##test
-#else
-#define SKIP_MACOSX(test) test
-#endif
-
 // TODO(chron): Test not using cros_user flag and use signin_
 class ProfileSyncServiceStartupTest : public testing::Test {
  public:
@@ -106,7 +99,7 @@ class ProfileSyncServiceStartupCrosTest : public ProfileSyncServiceStartupTest {
   }
 };
 
-TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(StartFirstTime)) {
+TEST_F(ProfileSyncServiceStartupTest, StartFirstTime) {
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManager();
   EXPECT_CALL(*data_type_manager, Configure(_, _)).Times(0);
 
@@ -159,7 +152,7 @@ TEST_F(ProfileSyncServiceStartupCrosTest, StartFirstTime) {
   EXPECT_TRUE(service_->ShouldPushChanges());
 }
 
-TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(StartNormal)) {
+TEST_F(ProfileSyncServiceStartupTest, StartNormal) {
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManager();
   EXPECT_CALL(*data_type_manager, Configure(_, _)).Times(2);
   EXPECT_CALL(*data_type_manager, state()).
@@ -175,7 +168,7 @@ TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(StartNormal)) {
   service_->Initialize();
 }
 
-TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(ManagedStartup)) {
+TEST_F(ProfileSyncServiceStartupTest, ManagedStartup) {
   // Disable sync through policy.
   profile_.GetPrefs()->SetBoolean(prefs::kSyncManaged, true);
 
@@ -189,7 +182,7 @@ TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(ManagedStartup)) {
   service_->Initialize();
 }
 
-TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(SwitchManaged)) {
+TEST_F(ProfileSyncServiceStartupTest, SwitchManaged) {
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManager();
   EXPECT_CALL(*data_type_manager, Configure(_, _)).Times(2);
   EXPECT_CALL(observer_, OnStateChanged()).Times(AnyNumber());
@@ -281,7 +274,7 @@ TEST_F(ProfileSyncServiceStartupTest, ClearServerData) {
   service_->ResetClearServerDataState();
 }
 
-TEST_F(ProfileSyncServiceStartupTest, SKIP_MACOSX(StartFailure)) {
+TEST_F(ProfileSyncServiceStartupTest, StartFailure) {
   DataTypeManagerMock* data_type_manager = SetUpDataTypeManager();
   DataTypeManager::ConfigureResult configure_result =
       DataTypeManager::ASSOCIATION_FAILED;
