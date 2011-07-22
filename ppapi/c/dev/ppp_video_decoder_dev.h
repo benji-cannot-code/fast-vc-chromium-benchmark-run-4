0386ba9bfb34f4288b00e6694e7cac91890211e2
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 20:15:28 +0000

    Re-added decoder id to PPP_VideoDecoder_Dev methods
    This supports having multiple decoders per PP_Instance, which was removed as
    part of 92704.
    
    BUG=none
    TEST=gles2 works, trybots
    
    Review URL: http://codereview.chromium.org/7489012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93691 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppp_video_decoder_dev.h
+++ ppapi/c/dev/ppp_video_decoder_dev.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_resource.h"
 #include "ppapi/c/dev/pp_video_dev.h"
 
-#define PPP_VIDEODECODER_DEV_INTERFACE "PPP_VideoDecoder(Dev);0.7"
+#define PPP_VIDEODECODER_DEV_INTERFACE "PPP_VideoDecoder(Dev);0.8"
 
 // PPP_VideoDecoder_Dev structure contains the function pointers that the
 // plugin MUST implement to provide services needed by the video decoder
@@ -26,21 +26,25 @@ struct PPP_VideoDecoder_Dev {
   //
   // Parameters:
   //  |instance| the plugin instance to which the callback is responding.
+  //  |decoder| the PPB_VideoDecoder_Dev resource.
   //  |req_num_of_bufs| tells how many buffers are needed by the decoder.
   //  |dimensions| tells the dimensions of the buffer to allocate.
   //  |type| specifies whether the buffer lives in system memory or GL texture.
-  void (*ProvidePictureBuffers)(
-      PP_Instance instance,
-      uint32_t req_num_of_bufs,
-      struct PP_Size dimensions);
+  void (*ProvidePictureBuffers)(PP_Instance instance,
+                                PP_Resource decoder,
+                                uint32_t req_num_of_bufs,
+                                struct PP_Size dimensions);
 
   // Callback function for decoder to deliver unneeded picture buffers back to
   // the plugin.
   //
   // Parameters:
   //  |instance| the plugin instance to which the callback is responding.
+  //  |decoder| the PPB_VideoDecoder_Dev resource.
   //  |picture_buffer| points to the picture buffer that is no longer needed.
-  void (*DismissPictureBuffer)(PP_Instance instance, int32_t picture_buffer_id);
+  void (*DismissPictureBuffer)(PP_Instance instance,
+                               PP_Resource decoder,
+                               int32_t picture_buffer_id);
 
   // Callback function for decoder to deliver decoded pictures ready to be
   // displayed. Decoder expects the plugin to return the buffer back to the
@@ -48,8 +52,11 @@ struct PPP_VideoDecoder_Dev {
   //
   // Parameters:
   //  |instance| the plugin instance to which the callback is responding.
+  //  |decoder| the PPB_VideoDecoder_Dev resource.
   //  |picture| is the picture that is ready.
-  void (*PictureReady)(PP_Instance instance, struct PP_Picture_Dev picture);
+  void (*PictureReady)(PP_Instance instance,
+                       PP_Resource decoder,
+                       struct PP_Picture_Dev picture);
 
   // Callback function to tell the plugin that decoder has decoded end of stream
   // marker and output all the pictures that should be displayed from the
@@ -57,15 +64,19 @@ struct PPP_VideoDecoder_Dev {
   //
   // Parameters:
   //  |instance| the plugin instance to which the callback is responding.
-  void (*EndOfStream)(PP_Instance instance);
+  //  |decoder| the PPB_VideoDecoder_Dev resource.
+  void (*EndOfStream)(PP_Instance instance, PP_Resource decoder);
 
   // Error handler callback for decoder to deliver information about detected
   // errors to the plugin.
   //
   // Parameters:
   //  |instance| the plugin instance to which the callback is responding.
+  //  |decoder| the PPB_VideoDecoder_Dev resource.
   //  |error| error is the enumeration specifying the error.
-  void (*NotifyError)(PP_Instance instance, enum PP_VideoDecodeError_Dev error);
+  void (*NotifyError)(PP_Instance instance,
+                      PP_Resource decoder,
+                      enum PP_VideoDecodeError_Dev error);
 };
 
 #endif  /* PPAPI_C_DEV_PPP_VIDEO_DECODER_DEV_H_ */
