6946f4a925dd871b1ef563a729f4182a7cb533c0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-23 16:21:14 +0000

    Source/WebCore: Display number field without using a thousand separator. https://bugs.webkit.org/show_bug.cgi?id=65064
    Patch by Shinya Kawanaka <shinyak@google.com> on 2011-07-23
    Reviewed by Kent Tamura.
    
    Separated formatters for parsing and displaying.
    Removed a test for thousand separator.
    
    * manual-tests/input-number-localization.html:
    Removed a test for thousand separator.
    Made the test description more specific.
    * platform/text/LocalizedNumberICU.cpp:
    (WebCore::createFormatterForCurrentLocaleToDisplay):
    Added to introduce a formatter for display.
    (WebCore::numberFormatterForParsing):
    Changed the name to be clear.
    (WebCore::numberFormatterForDisplay): Added.
    (WebCore::parseLocalizedNumber): Used numberFormatterForParsing.
    (WebCore::formatLocalizedNumber): Used numberFormatterForDisplay.
    * platform/text/mac/LocalizedNumberMac.mm:
    (WebCore::createFormatterForCurrentLocaleForDisplay):
    Added to introduce a formatter for display.
    (WebCore::numberFormatterForParsing):
    Changed the name to be clear.
    (WebCore::numberFormatterForDisplay): Added.
    (WebCore::parseLocalizedNumber): Used numberFormatterForParsing.
    (WebCore::formatLocalizedNumber): Used numberFormatterForDisplay.
    
    LayoutTests: Marked fast/speech/input-appearance-numberandspeech.html flaky on Mac and Windows.
    https://bugs.webkit.org/show_bug.cgi?id=65064
    
    Patch by Shinya Kawanaka <shinyak@google.com> on 2011-07-23
    Reviewed by Kent Tamura.
    
    * platform/chromium/test_expectations.txt:
      Mark fast/speech/input-appearance-numberandspeech.html flaky on Mac and Windows.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/mac/LocalizedNumberMac.mm
+++ third_party/WebKit/Source/WebCore/platform/text/mac/LocalizedNumberMac.mm
@@ -49,10 +49,24 @@ static RetainPtr<NSNumberFormatter> createFormatterForCurrentLocale()
     return formatter;
 }
 
-static NSNumberFormatter *numberFormatter()
+static RetainPtr<NSNumberFormatter> createFormatterForCurrentLocaleForDisplay()
+{
+    RetainPtr<NSNumberFormatter> formatter = createFormatterForCurrentLocale();
+    [formatter.get() setHasThousandSeparators:NO];
+    return formatter;
+}
+
+static NSNumberFormatter *numberFormatterForParsing()
 {
     ASSERT(isMainThread());
     static NSNumberFormatter *formatter = createFormatterForCurrentLocale().leakRef();
+    return formatter;    
+}
+
+static NSNumberFormatter *numberFormatterForDisplay()
+{
+    ASSERT(isMainThread());
+    static NSNumberFormatter *formatter = createFormatterForCurrentLocaleForDisplay().leakRef();
     return formatter;
 }
 
@@ -60,7 +74,7 @@ double parseLocalizedNumber(const String& numberString)
 {
     if (numberString.isEmpty())
         return numeric_limits<double>::quiet_NaN();
-    NSNumber *number = [numberFormatter() numberFromString:numberString];
+    NSNumber *number = [numberFormatterForParsing() numberFromString:numberString];
     if (!number)
         return numeric_limits<double>::quiet_NaN();
     return [number doubleValue];
@@ -69,7 +83,7 @@ double parseLocalizedNumber(const String& numberString)
 String formatLocalizedNumber(double inputNumber, unsigned fractionDigits)
 {
     RetainPtr<NSNumber> number(AdoptNS, [[NSNumber alloc] initWithDouble:inputNumber]);
-    RetainPtr<NSNumberFormatter> formatter = numberFormatter();
+    RetainPtr<NSNumberFormatter> formatter = numberFormatterForDisplay();
     [formatter.get() setMaximumFractionDigits:fractionDigits];
     return String([formatter.get() stringFromNumber:number.get()]);
 }
