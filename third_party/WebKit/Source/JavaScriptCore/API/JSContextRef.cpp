9276280664b0c082646b5c2bbf76fa69e7c27353
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-23 00:19:05 +0000

    Patch by Sommer Panage <panage@apple.com> on 2011-07-22 Reviewed by Oliver Hunt.
    export JSContextCreateBacktrace as SPI in JSContextRefPrivate.h
    https://bugs.webkit.org/show_bug.cgi?id=64981
    
    UIAutomation for iOS would like to support a Javascript backtrace in our error logs.
    Currently, the C API does not provide the tools to do this. However, the private API
    does expose the necessary functionality to get a backtrace
    (via Interpreter::retrieveLastCaller). We recognize this information may result in
    failure in the cases of programs run by 'eval', stack frames beneath host function
    call frames, and in programs run from other programs. Thus, we propose exporting our
    JSContextCreateBacktrace in JSContextRefPrivate.h. This will provide us with the tools
    we need while not advertising an API that isn't really ready for full use.
    
    * API/JSContextRef.cpp:
    * API/JSContextRefPrivate.h:
    * JavaScriptCore.exp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/API/JSContextRef.cpp
+++ third_party/WebKit/Source/JavaScriptCore/API/JSContextRef.cpp
@@ -30,11 +30,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "APICast.h"
 #include "InitializeThreading.h"
+#include <interpreter/CallFrame.h>
+#include <interpreter/Interpreter.h>
 #include "JSCallbackObject.h"
 #include "JSClassRef.h"
 #include "JSGlobalObject.h"
 #include "JSObject.h"
+#include "UStringBuilder.h"
 #include <wtf/text/StringHash.h>
+
 
 #if OS(DARWIN)
 #include <mach-o/dyld.h>
@@ -178,3 +182,60 @@ JSGlobalContextRef JSContextGetGlobalContext(JSContextRef ctx)
 
     return toGlobalRef(exec->lexicalGlobalObject()->globalExec());
 }
+    
+JSStringRef JSContextCreateBacktrace(JSContextRef ctx, unsigned maxStackSize)
+{
+    ExecState* exec = toJS(ctx);
+    JSLock lock(exec);
+
+    unsigned count = 0;
+    UStringBuilder builder;
+    CallFrame* callFrame = exec;
+    UString functionName;
+    if (exec->callee()) {
+        if (asObject(exec->callee())->inherits(&InternalFunction::s_info)) {
+            functionName = asInternalFunction(exec->callee())->name(exec);
+            builder.append("#0 ");
+            builder.append(functionName);
+            builder.append("() ");
+            count++;
+        }
+    }
+    while (true) {
+        ASSERT(callFrame);
+        int signedLineNumber;
+        intptr_t sourceID;
+        UString urlString;
+        JSValue function;
+        
+        UString levelStr = UString::number(count);
+        
+        exec->interpreter()->retrieveLastCaller(callFrame, signedLineNumber, sourceID, urlString, function);
+
+        if (function)
+            functionName = asFunction(function)->name(exec);
+        else {
+            // Caller is unknown, but if frame is empty we should still add the frame, because
+            // something called us, and gave us arguments.
+            if (count)
+                break;
+        }
+        unsigned lineNumber = signedLineNumber >= 0 ? signedLineNumber : 0;
+        if (!builder.isEmpty())
+            builder.append("\n");
+        builder.append("#");
+        builder.append(levelStr);
+        builder.append(" ");
+        builder.append(functionName);
+        builder.append("() at ");
+        builder.append(urlString);
+        builder.append(":");
+        builder.append(UString::number(lineNumber));
+        if (!function || ++count == maxStackSize)
+            break;
+        callFrame = callFrame->callerFrame();
+    }
+    return OpaqueJSString::create(builder.toUString()).leakRef();
+}
+
+
