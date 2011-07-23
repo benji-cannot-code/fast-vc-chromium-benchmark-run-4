ec8153a7e31a207fe621d3bbac1bc14df7172842
ycxiao@chromium.org <ycxiao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-23 01:37:16 +0000

    Apply the asynchronous CookieMonster API to render message filter. BUG=68657 TEST=XXX
    Review URL: http://codereview.chromium.org/7291015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93756 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_message_filter.h
+++ content/browser/renderer_host/render_message_filter.h
@@ -51,6 +51,7 @@ class Rect;
 }
 
 namespace net {
+class CookieList;
 class URLRequestContextGetter;
 }
 
@@ -116,7 +117,7 @@ class RenderMessageFilter : public BrowserMessageFilter {
                     IPC::Message* reply_msg);
   void OnGetRawCookies(const GURL& url,
                        const GURL& first_party_for_cookies,
-                       std::vector<webkit_glue::WebCookie>* cookies);
+                       IPC::Message* reply_msg);
   void OnDeleteCookie(const GURL& url,
                       const std::string& cookieName);
   void OnCookiesEnabled(const GURL& url,
@@ -211,6 +212,19 @@ class RenderMessageFilter : public BrowserMessageFilter {
                                  int flags,
                                  int message_id,
                                  int routing_id);
+
+  // Check the policy for getting cookies. Gets the cookies if allowed.
+  void CheckPolicyForCookies(const GURL& url,
+                             const GURL& first_party_for_cookies,
+                             IPC::Message* reply_msg,
+                             const net::CookieList& cookie_list);
+
+  // Writes the cookies to reply messages, and sends the message.
+  // Callback functions for getting cookies from cookie store.
+  void SendGetCookiesResponse(IPC::Message* reply_msg,
+                              const std::string& cookies);
+  void SendGetRawCookiesResponse(IPC::Message* reply_msg,
+                                 const net::CookieList& cookie_list);
 
   bool CheckBenchmarkingEnabled() const;
   bool CheckPreparsedJsCachingEnabled() const;
