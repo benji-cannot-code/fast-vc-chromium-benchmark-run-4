a0c397ba08a9be5d2fada4002e60427f81252285
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 17:51:07 +0000

    Overhaul garden-o-matic frontend to integrate revision history and failures https://bugs.webkit.org/show_bug.cgi?id=65089
    Reviewed by Dimitri Glazkov.
    
    This is a major overhaul of the garden-o-matic frontend.  The new UI
    integrates the revision history information with failures.  Failures
    appear in context adjacent to the revisions that caused them (which are
    highlighted).  Each buildbot receives a collumn to the left, which
    indicates which revisions that bot has tested and whether that bot
    showed the indicated failures.
    
    Failures are selectable with checkboxes, and can then either be
    examined or rebaselined.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Trac.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/base.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/garden-o-matic.html:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.css:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/results.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/results_unittests.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui_unittests.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91677 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/model.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var model = model || {};
+
+(function () {
+
+var kCommitLogLength = 20;
+
+model.state = {};
+
+model.updateRecentCommits = function(callback)
+{
+    trac.recentCommitData('trunk', kCommitLogLength, function(commitDataList) {
+        model.state.recentCommits = commitDataList;
+        callback();
+    });
+};
+
+model.updateResultsByBuilder = function(callback)
+{
+    results.fetchResultsByBuilder(config.kBuilders, function(resultsByBuilder) {
+        model.state.resultsByBuilder = resultsByBuilder;
+        callback();
+    });
+};
+
+model.analyzeUnexpectedFailures = function(callback)
+{
+    var unexpectedFailures = results.unexpectedFailuresByTest(model.state.resultsByBuilder);
+    $.each(unexpectedFailures, function(testName, resultNodesByBuilder) {
+        var builderNameList = base.keys(resultNodesByBuilder);
+        results.unifyRegressionRanges(builderNameList, testName, function(oldestFailingRevision, newestPassingRevision) {
+            var failureAnalysis = {
+                'testName': testName,
+                'resultNodesByBuilder': resultNodesByBuilder,
+                'oldestFailingRevision': oldestFailingRevision,
+                'newestPassingRevision': newestPassingRevision,
+            };
+            callback(failureAnalysis);
+        });
+    });
+};
+
+})();
