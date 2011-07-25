9d8a6b3511fadb933bcbcc6460c283060422f9e8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 23:57:43 +0000

    REGRESSION (r39725?): Resources removed from document can not be freed until the document is deleted https://bugs.webkit.org/show_bug.cgi?id=61006
    Patch by Scott Graham <scottmg@chromium.org> on 2011-07-25
    Reviewed by Nate Chapin.
    
    Rollback previous patch. Occasional crashes seen caused by
    CachedResource being deleted twice.
    
    * loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::CachedResourceLoader):
    (WebCore::CachedResourceLoader::loadDone):
    * loader/cache/CachedResourceLoader.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.h
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CachedResourceHandle.h"
 #include "CachePolicy.h"
 #include "ResourceLoadPriority.h"
-#include "Timer.h"
 #include <wtf/Deque.h>
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
@@ -119,7 +118,6 @@ private:
     void notifyLoadedFromMemoryCache(CachedResource*);
     bool canRequest(CachedResource::Type, const KURL&, bool forPreload = false);
 
-    void garbageCollectDocumentResourcesTimerFired(Timer<CachedResourceLoader>*);
     void performPostLoadActions();
     
     HashSet<String> m_validatedURLs;
@@ -136,8 +134,6 @@ private:
     };
     Deque<PendingPreload> m_pendingPreloads;
 
-    Timer<CachedResourceLoader> m_garbageCollectDocumentResourcesTimer;
-    
     //29 bits left
     bool m_autoLoadImages : 1;
     bool m_loadFinishing : 1;
