950fa34482eddf82aed6e4d015265a9035dd7a0a
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-25 22:06:32 +0000

    Revert 93948 - Added EnrollEnterpriseDevice(user, password) automation hook.
    Enrolls an unenrolled device as a managed enterprise device.
    
    BUG=chromium-os:17873
    TEST=Manual test
    
    Review URL: http://codereview.chromium.org/7448009
    
    TBR=craigdh@chromium.org
    Review URL: http://codereview.chromium.org/7508005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/views_enterprise_enrollment_screen_actor.cc
+++ chrome/browser/chromeos/login/views_enterprise_enrollment_screen_actor.cc
@@ -18,7 +18,6 @@ ViewsEnterpriseEnrollmentScreenActor::ViewsEnterpriseEnrollmentScreenActor(
 }
 
 ViewsEnterpriseEnrollmentScreenActor::~ViewsEnterpriseEnrollmentScreenActor() {
-  NotifyObservers(false);
 }
 
 // ViewsEnterpriseEnrollmentScreenActor, ViewScreen implementation -------------
@@ -56,33 +55,27 @@ void ViewsEnterpriseEnrollmentScreenActor::Hide() {
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowConfirmationScreen() {
   GetUIActor()->ShowConfirmationScreen();
-  NotifyObservers(true);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowAuthError(
     const GoogleServiceAuthError & error) {
   GetUIActor()->ShowAuthError(error);
-  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowAccountError() {
   GetUIActor()->ShowAccountError();
-  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowFatalAuthError() {
   GetUIActor()->ShowFatalAuthError();
-  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowFatalEnrollmentError() {
   GetUIActor()->ShowFatalEnrollmentError();
-  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowNetworkEnrollmentError() {
   GetUIActor()->ShowNetworkEnrollmentError();
-  NotifyObservers(false);
 }
 
 // ViewsEnterpriseEnrollmentScreenActor, public --------------------------------
