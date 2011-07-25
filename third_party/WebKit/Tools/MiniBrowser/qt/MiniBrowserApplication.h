010a3221dc0bab1b2b7d9e5032120a2f56be5bb0
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 14:48:10 +0000

    [Qt][WK2] Add multi-point touch mocking to MiniBrowser
    https://bugs.webkit.org/show_bug.cgi?id=64374
    
    Reviewed by Benjamin Poulain.
    
    Each mouse button generate a touch point. The touch point stay
    virtually on screen if the mouse release happened while the Ctrl
    modifier was pressed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/qt/MiniBrowserApplication.h
+++ third_party/WebKit/Tools/MiniBrowser/qt/MiniBrowserApplication.h
@@ -30,7 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MiniBrowserApplication_h
 #define MiniBrowserApplication_h
 
+#include <QHash>
 #include <QStringList>
+#include <QTouchEvent>
 #include <QtGui>
 
 struct WindowOptions {
@@ -76,6 +78,8 @@ private:
     int m_robotTimeoutSeconds;
     int m_robotExtraTimeSeconds;
     QStringList m_urls;
+
+    QHash<int, QTouchEvent::TouchPoint> m_touchPoints;
 };
 
 #endif
