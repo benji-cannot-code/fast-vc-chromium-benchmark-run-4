834b47384237ebbb6eb95af9f62b3918aaa55fae
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-25 21:15:44 +0000

    Added EnrollEnterpriseDevice(user, password) automation hook.
    Enrolls an unenrolled device as a managed enterprise device.
    
    BUG=chromium-os:17873
    TEST=Manual test
    
    Review URL: http://codereview.chromium.org/7448009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/views_enterprise_enrollment_screen_actor.cc
+++ chrome/browser/chromeos/login/views_enterprise_enrollment_screen_actor.cc
@@ -18,6 +18,7 @@ ViewsEnterpriseEnrollmentScreenActor::ViewsEnterpriseEnrollmentScreenActor(
 }
 
 ViewsEnterpriseEnrollmentScreenActor::~ViewsEnterpriseEnrollmentScreenActor() {
+  NotifyObservers(false);
 }
 
 // ViewsEnterpriseEnrollmentScreenActor, ViewScreen implementation -------------
@@ -55,27 +56,33 @@ void ViewsEnterpriseEnrollmentScreenActor::Hide() {
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowConfirmationScreen() {
   GetUIActor()->ShowConfirmationScreen();
+  NotifyObservers(true);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowAuthError(
     const GoogleServiceAuthError & error) {
   GetUIActor()->ShowAuthError(error);
+  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowAccountError() {
   GetUIActor()->ShowAccountError();
+  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowFatalAuthError() {
   GetUIActor()->ShowFatalAuthError();
+  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowFatalEnrollmentError() {
   GetUIActor()->ShowFatalEnrollmentError();
+  NotifyObservers(false);
 }
 
 void ViewsEnterpriseEnrollmentScreenActor::ShowNetworkEnrollmentError() {
   GetUIActor()->ShowNetworkEnrollmentError();
+  NotifyObservers(false);
 }
 
 // ViewsEnterpriseEnrollmentScreenActor, public --------------------------------
