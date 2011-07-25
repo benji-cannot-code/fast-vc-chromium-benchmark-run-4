e465d89db7e466e9cffb803f929db2b94fbfa5d1
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 23:40:16 +0000

    Deprecate AudioBufferSourceNode .looping attribute and implement .loop https://bugs.webkit.org/show_bug.cgi?id=65133
    Reviewed by Kenneth Russell.
    
    No new tests since audio API is not yet implemented.
    
    * webaudio/AudioBufferSourceNode.cpp:
    (WebCore::AudioBufferSourceNode::renderFromBuffer):
    (WebCore::AudioBufferSourceNode::totalPitchRate):
    (WebCore::AudioBufferSourceNode::looping):
    (WebCore::AudioBufferSourceNode::setLooping):
    * webaudio/AudioBufferSourceNode.h:
    (WebCore::AudioBufferSourceNode::loop):
    (WebCore::AudioBufferSourceNode::setLoop):
    * webaudio/AudioBufferSourceNode.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/webaudio/AudioBufferSourceNode.cpp
+++ third_party/WebKit/Source/WebCore/webaudio/AudioBufferSourceNode.cpp
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AudioContext.h"
 #include "AudioNodeOutput.h"
+#include "Document.h"
+#include "ScriptCallStack.h"
 #include <algorithm>
 #include <wtf/MathExtras.h>
 
@@ -248,7 +250,7 @@ void AudioBufferSourceNode::renderFromBuffer(AudioBus* bus, unsigned destination
         // For linear interpolation we need the next sample-frame too.
         unsigned readIndex2 = readIndex + 1;
         if (readIndex2 >= endFrame) {
-            if (looping()) {
+            if (loop()) {
                 // Make sure to wrap around at the end of the buffer.
                 readIndex2 -= deltaFrames;
             } else
@@ -279,7 +281,7 @@ void AudioBufferSourceNode::renderFromBuffer(AudioBus* bus, unsigned destination
         if (virtualReadIndex >= endFrame) {
             virtualReadIndex -= deltaFrames;
 
-            if (!looping()) {
+            if (!loop()) {
                 // If we're not looping, then stop playing when we get to the end.
                 m_isPlaying = false;
 
@@ -425,8 +427,30 @@ double AudioBufferSourceNode::totalPitchRate()
     ASSERT(isTotalRateValid);
     if (!isTotalRateValid)
         totalRate = 1.0;
-    
+
     return totalRate;
+}
+
+bool AudioBufferSourceNode::looping()
+{
+    static bool firstTime = true;
+    if (firstTime && context() && context()->document()) {
+        context()->document()->addMessage(JSMessageSource, LogMessageType, WarningMessageLevel, "AudioBufferSourceNode 'looping' attribute is deprecated.  Use 'loop' instead.", 1, String(), 0);
+        firstTime = false;
+    }
+
+    return m_isLooping;
+}
+
+void AudioBufferSourceNode::setLooping(bool looping)
+{
+    static bool firstTime = true;
+    if (firstTime && context() && context()->document()) {
+        context()->document()->addMessage(JSMessageSource, LogMessageType, WarningMessageLevel, "AudioBufferSourceNode 'looping' attribute is deprecated.  Use 'loop' instead.", 1, String(), 0);
+        firstTime = false;
+    }
+
+    m_isLooping = looping;
 }
 
 } // namespace WebCore
