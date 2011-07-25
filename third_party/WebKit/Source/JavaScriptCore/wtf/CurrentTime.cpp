736ffaa687aa76d581c9076d7c69bb615a94bd09
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 07:18:49 +0000

    [EFL] Implement EFL-specific current time and monotonicallyIncreasingTime. https://bugs.webkit.org/show_bug.cgi?id=64354
    Use ecore_time_unix_get which returns unix time as double type for currentTime
    and ecore_time_get which uses monotonic clock for monotonicallyIncreasingTime.
    
    Reviewed by Kent Tamura.
    
    * wtf/CurrentTime.cpp:
    (WTF::currentTime):
    (WTF::monotonicallyIncreasingTime):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/CurrentTime.cpp
+++ third_party/WebKit/Source/JavaScriptCore/wtf/CurrentTime.cpp
@@ -63,6 +63,8 @@ extern "C" time_t mktime(struct tm *t);
 #include <wx/datetime.h>
 #elif PLATFORM(BREWMP)
 #include <AEEStdLib.h>
+#elif PLATFORM(EFL)
+#include <Ecore.h>
 #else
 #include <sys/time.h>
 #endif
@@ -287,6 +289,13 @@ double currentTime()
     return static_cast<double>(diffSeconds + GETUTCSECONDS() + ((GETTIMEMS() % 1000) / msPerSecond));
 }
 
+#elif PLATFORM(EFL)
+
+double currentTime()
+{
+    return ecore_time_unix_get();
+}
+
 #else
 
 double currentTime()
@@ -309,6 +318,13 @@ double monotonicallyIncreasingTime()
         ASSERT_UNUSED(kr, kr == KERN_SUCCESS);
     }
     return (mach_absolute_time() * timebaseInfo.numer) / (1.0e9 * timebaseInfo.denom);
+}
+
+#elif PLATFORM(EFL)
+
+double monotonicallyIncreasingTime()
+{
+    return ecore_time_get();
 }
 
 #else
