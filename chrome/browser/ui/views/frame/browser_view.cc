5981dc3f2a9d6bac1caf133a1a301323307381e7
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-25 17:56:15 +0000

    WebUI TaskManager: make it default on Chrome OS
    Got the approval from Linus to ship this on Chrome OS in M14
    (cf. Comment 20 on http://crbug.com/69222)
    
    BUG=chromium-os:13885
    TEST=manual on chromeos-chrome in linux
    
    Review URL: http://codereview.chromium.org/7493046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1052,7 +1052,7 @@ void BrowserView::ShowCompactLocationBarUnderSelectedTab() {
 }
 
 void BrowserView::ShowTaskManager() {
-#if defined(TOUCH_UI)
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
   TaskManagerDialog::Show();
 #else
   // Uses WebUI TaskManager when swiches is set. It is beta feature.
@@ -1062,7 +1062,7 @@ void BrowserView::ShowTaskManager() {
   } else {
     browser::ShowTaskManager();
   }
-#endif  // defined(TOUCH_UI)
+#endif  // defined(TOUCH_UI) || defined(OS_CHROMEOS)
 }
 
 void BrowserView::ShowBackgroundPages() {
