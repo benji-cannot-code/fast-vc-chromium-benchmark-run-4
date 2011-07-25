b6a3ee21047376d3463d390be74df8748ff91408
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-25 22:41:52 +0000

    Minor tweak to wrap label elements as atomic units.
    * TestResultServer/static-dashboards/dashboard_base.js:
    * TestResultServer/static-dashboards/flakiness_dashboard.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91713 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
@@ -799,7 +799,7 @@ function checkboxHTML(queryParameter, label, isChecked, opt_extraJavaScript)
     return '<label style="padding-left: 2em">' +
         '<input type="checkbox" onchange="toggleQueryParameter(\'' + queryParameter + '\');' + js + '" ' +
             (isChecked ? 'checked' : '') + '>' + label +
-        '</label>';
+        '</label> ';
 }
 
 function selectHTML(label, queryParameter, options)
@@ -813,7 +813,7 @@ function selectHTML(label, queryParameter, options)
             (g_currentState[queryParameter] == value ? 'selected' : '') +
             '>' + value + '</option>'
     }
-    html += '</select><label>';
+    html += '</select></label> ';
     return html;
 }
 
