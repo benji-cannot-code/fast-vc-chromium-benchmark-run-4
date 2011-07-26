b572c5dc18a42cd9940a071cc2ceb37bba709c3e
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 08:55:56 +0000

    Revert 93987 - Close the URL request in the destroctor. This ensures the load is actually stopped when the request is deleted. This caused the pdf plugin to not display any pdf.
    TEST=manual
    BUG=84765
    Review URL: http://codereview.chromium.org/7491046
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/7497008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94063 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppb_url_loader_impl.cc
+++ webkit/plugins/ppapi/ppb_url_loader_impl.cc
@@ -71,7 +71,6 @@ PPB_URLLoader_Impl::PPB_URLLoader_Impl(PluginInstance* instance,
 }
 
 PPB_URLLoader_Impl::~PPB_URLLoader_Impl() {
-  Close();
 }
 
 PPB_URLLoader_API* PPB_URLLoader_Impl::AsPPB_URLLoader_API() {
