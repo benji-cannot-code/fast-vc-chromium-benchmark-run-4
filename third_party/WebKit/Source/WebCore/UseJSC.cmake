c4fc2c4df7773f965a7afcf3f36d450432005d4d
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-26 05:21:06 +0000

    [CMAKE] Split JSC related files from WebCore/CMakeLists.txt https://bugs.webkit.org/show_bug.cgi?id=61350
    Reviewed by Daniel Bates.
    
    No new tests, just moving jsc related files from CMakeLists.txt to UseJSC.cmake.
    
    * CMakeLists.txt:
    * UseJSC.cmake:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91742 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/UseJSC.cmake
+++ third_party/WebKit/Source/WebCore/UseJSC.cmake
@@ -9,6 +9,7 @@ LIST(APPEND WebCore_IDL_INCLUDES
 )
 
 LIST(APPEND WebCore_SOURCES
+    bindings/js/CallbackFunction.cpp
     bindings/js/DOMObjectHashTableMap.cpp
     bindings/js/DOMWrapperWorld.cpp
     bindings/js/GCController.cpp
@@ -163,7 +164,6 @@ LIST(APPEND WebCore_SOURCES
     bridge/jsc/BridgeJSC.cpp
 )
 
-
 IF (ENABLE_BLOB)
     LIST(APPEND WebCore_SOURCES
         bindings/js/JSFileReaderCustom.cpp
@@ -212,6 +212,47 @@ IF (ENABLE_XPATH)
     )
 ENDIF ()
 
+
+IF (ENABLE_NOTIFICATIONS)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSDesktopNotificationsCustom.cpp
+    )
+ENDIF ()
+
+if (ENABLE_FILE_SYSTEM)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSDirectoryEntryCustom.cpp
+        bindings/js/JSDirectoryEntrySyncCustom.cpp
+        bindings/js/JSEntryCustom.cpp
+        bindings/js/JSEntrySyncCustom.cpp
+    )
+ENDIF ()
+
+IF (ENABLE_SVG)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSSVGElementInstanceCustom.cpp
+        bindings/js/JSSVGLengthCustom.cpp
+        bindings/js/JSSVGPathSegCustom.cpp
+    )
+ENDIF ()
+
+IF (ENABLE_VIDEO)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSAudioConstructor.cpp
+    )
+ENDIF ()
+
+IF (ENABLE_MEDIA_STREAM)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSNavigatorCustom.cpp
+    )
+ENDIF ()
+
+IF (ENABLE_WEBGL)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSWebGLRenderingContextCustom.cpp
+    )
+ENDIF ()
 
 LIST(APPEND SCRIPTS_BINDINGS
     ${WEBCORE_DIR}/bindings/scripts/CodeGenerator.pm
