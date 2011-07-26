896f34ba8b225fda8449d63949e3bfcd3b89d75a
cwolfe@chromium.org <cwolfe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 22:03:57 +0000

    Initial sensor event provider for testing views screen rotation
    The sensors API will unify various types of sensor data into a set of channels, each of which provides change events and periodic updates.
    
    This version of the API is intended only to support the experimental screen rotation code and is not for general use. In particular, the final listener will declare generic |OnSensorChanged| and |OnSensorUpdated| methods, rather than the special-purpose |OnScreenOrientationChanged|.
    
    BUG=none
    TEST=manual with http://codereview.chromium.org/7273073/
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=93094
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=93480
    
    Review URL: http://codereview.chromium.org/7366011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94181 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/browser/sensors/sensors_provider_impl.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/sensors/sensors_provider_impl.h"
+
+#include "base/memory/singleton.h"
+
+namespace sensors {
+
+ProviderImpl* ProviderImpl::GetInstance() {
+  return Singleton<ProviderImpl>::get();
+}
+
+ProviderImpl::ProviderImpl()
+    : listeners_(new ListenerList()) {
+}
+
+ProviderImpl::~ProviderImpl() {
+}
+
+void ProviderImpl::AddListener(Listener* listener) {
+  listeners_->AddObserver(listener);
+}
+
+void ProviderImpl::RemoveListener(Listener* listener) {
+  listeners_->RemoveObserver(listener);
+}
+
+void ProviderImpl::ScreenOrientationChanged(
+    const ScreenOrientation& change) {
+  listeners_->Notify(&Listener::OnScreenOrientationChanged, change);
+}
+
+}  // namespace sensors
