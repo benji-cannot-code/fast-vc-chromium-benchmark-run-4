812cc64261ebfe374d2c56605374cde7ec6ead90
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-26 22:23:16 +0000

    Rubber-stamped by Anders Carlsson.
    Update DumpRenderTree and API tests for the pictograph font family preference.
    
    * DumpRenderTree/mac/DumpRenderTree.mm:
    (resetDefaultsToConsistentValues):
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (resetDefaultsToConsistentValues):
    * TestWebKitAPI/Tests/WebKit2/WKPreferences.cpp:
    (TestWebKitAPI::TEST):
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::resetStateToConsistentValues):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/Tests/WebKit2/WKPreferences.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/Tests/WebKit2/WKPreferences.cpp
@@ -53,6 +53,7 @@ TEST(WebKit2, WKPreferencesDefaults)
     static const char* expectedSansSerifFontFamily = "Arial";
     static const char* expectedCursiveFontFamily = "Comic Sans MS";
     static const char* expectedFantasyFontFamily = "Comic Sans MS";
+    static const char* expectedPictographFontFamily = "Times New Roman";
 #elif PLATFORM(MAC)
     static const char* expectedStandardFontFamily = "Times";
     static const char* expectedFixedFontFamily = "Courier";
@@ -60,6 +61,7 @@ TEST(WebKit2, WKPreferencesDefaults)
     static const char* expectedSansSerifFontFamily = "Helvetica";
     static const char* expectedCursiveFontFamily = "Apple Chancery";
     static const char* expectedFantasyFontFamily = "Papyrus";
+    static const char* expectedPictographFontFamily = "Apple Color Emoji";
 #endif
 
     WKPreferencesRef preference = WKPreferencesCreate();
@@ -80,6 +82,7 @@ TEST(WebKit2, WKPreferencesDefaults)
     EXPECT_WK_STREQ(expectedSansSerifFontFamily, adoptWK(WKPreferencesCopySansSerifFontFamily(preference)));
     EXPECT_WK_STREQ(expectedCursiveFontFamily, adoptWK(WKPreferencesCopyCursiveFontFamily(preference)));
     EXPECT_WK_STREQ(expectedFantasyFontFamily, adoptWK(WKPreferencesCopyFantasyFontFamily(preference)));
+    EXPECT_WK_STREQ(expectedPictographFontFamily, adoptWK(WKPreferencesCopyPictographFontFamily(preference)));
     EXPECT_EQ(0u, WKPreferencesGetMinimumFontSize(preference));
     EXPECT_FALSE(WKPreferencesGetPrivateBrowsingEnabled(preference));
     EXPECT_FALSE(WKPreferencesGetDeveloperExtrasEnabled(preference));
