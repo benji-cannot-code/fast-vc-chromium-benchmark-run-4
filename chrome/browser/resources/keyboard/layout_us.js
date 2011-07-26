f2abb3a63c04c5ad83c9becd0b128a967938fdc1
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 18:40:36 +0000

    Add repeat parameter to SvgKey.
    This makes it easy for other layouts to add repeatable keys.
    
    BUG=none
    TEST=Manually tested key repeat worked with handwriting-vk.
    
    
    Review URL: http://codereview.chromium.org/7497009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard/layout_us.js
+++ chrome/browser/resources/keyboard/layout_us.js
@@ -8,12 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 /**
- * The backspace key.
- * @type {BaseKey}
- */
-var backspaceKey;
-
-/**
  * All keys for the rows of the keyboard.
  * NOTE: every row below should have an aspect of 12.6.
  * @type {Array.<Array.<BaseKey>>}
@@ -31,7 +25,7 @@ var KEYS_US = [
     new Key(C('i'), C('I'), C('8'), C('}')),
     new Key(C('o'), C('O'), C('9'), C('\'')),
     new Key(C('p'), C('P'), C('0'), C('|')),
-    backspaceKey = new SvgKey('backspace', 'Backspace')
+    new SvgKey('backspace', 'Backspace', true /* repeat */)
   ],
   [
     new SymbolKey(),
@@ -74,6 +68,3 @@ var KEYS_US = [
     new HideKeyboardKey()
   ]
 ];
-
-// Backspace key should repeat.
-backspaceKey.repeat = true;
