573800b9e41ccecb307ca3fe5bb70121359cdd57
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 03:11:59 +0000

    WebUI TaskManager: Set autoExpands true
    TaskManager expands a height of row when a process has two tasks or more. But sometimes some expanded rows are broken or disappeared.
    To draw rows correctly even if height is expanded, autoExpands must be true.
    
    BUG=chromium-os:13885
    TEST=manual on chromeos-chrome in Linux
    
    Review URL: http://codereview.chromium.org/7432001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/table.js
+++ chrome/browser/resources/shared/js/cr/ui/table.js
@@ -90,6 +90,18 @@ cr.define('cr.ui', function() {
     },
 
     /**
+     * The accessor to "autoExpands" property of the list.
+     *
+     * @type {boolean}
+     */
+    get autoExpands() {
+      return this.list_.autoExpands;
+    },
+    set autoExpands(autoExpands) {
+      this.list_.autoExpands = autoExpands;
+    },
+
+    /**
      * The header of the table.
      *
      * @type {cr.ui.table.TableColumnModel}
--- chrome/browser/resources/task_manager/main.js
+++ chrome/browser/resources/task_manager/main.js
@@ -222,6 +222,9 @@ TaskManager.prototype = {
     this.table_.dataModel = this.dataModel_;
     this.table_.selectionModel = this.selectionModel_;
     this.table_.columnModel = this.columnModel_;
+
+    // Expands height of row when a process has some tasks.
+    this.table_.autoExpands = true;
   },
 
   renderText_: function(entry, columnId, table) {
