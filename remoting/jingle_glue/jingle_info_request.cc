93a003bd7a3ec68356c788c4302025973fbe685a
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 19:40:25 +0000

    Cleanups and bugfixes for JingleInfoRequest.
    BUG=None
    TEST=JinlgeInfoRequest can fetch jingle info on both host and client
    
    Review URL: http://codereview.chromium.org/7495041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94149 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/jingle_glue/jingle_info_request.cc
+++ remoting/jingle_glue/jingle_info_request.cc
@@ -22,18 +22,10 @@ JingleInfoRequest::JingleInfoRequest(IqRequest* request)
 JingleInfoRequest::~JingleInfoRequest() {
 }
 
-void JingleInfoRequest::Run(Task* done) {
-  done_cb_.reset(done);
+void JingleInfoRequest::Send(const OnJingleInfoCallback& callback) {
+  on_jingle_info_cb_ = callback;
   request_->SendIq(buzz::STR_GET, buzz::STR_EMPTY,
                    new buzz::XmlElement(buzz::QN_JINGLE_INFO_QUERY, true));
-}
-
-void JingleInfoRequest::SetCallback(OnJingleInfoCallback* callback) {
-  on_jingle_info_cb_.reset(callback);
-}
-
-void JingleInfoRequest::DetachCallback() {
-  on_jingle_info_cb_.reset();
 }
 
 void JingleInfoRequest::OnResponse(const buzz::XmlElement* stanza) {
@@ -82,14 +74,7 @@ void JingleInfoRequest::OnResponse(const buzz::XmlElement* stanza) {
     }
   }
 
-  if (on_jingle_info_cb_.get()) {
-    on_jingle_info_cb_->Run(relay_token, relay_hosts, stun_hosts);
-  } else {
-    LOG(INFO) << "Iq reply parsed with no callback. Dropping" << stanza->Str();
-  }
-
-  DetachCallback();
-  done_cb_->Run();
+  on_jingle_info_cb_.Run(relay_token, relay_hosts, stun_hosts);
 }
 
 }  // namespace remoting
