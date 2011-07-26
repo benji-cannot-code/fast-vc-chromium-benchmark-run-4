6ffdc9910bd116e9c1d40ba0cc190549c1dcfde4
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 21:45:25 +0000

    Print Preview: Fixing color settings.
    After making lot of changes on the control flow necessary for pdf pipelining, the color
    settings have regressed.
    
    Note: The color settings will soon be refactored to a separate file (as done with all other settings on the page).
    
    BUG=90566
    TEST=Manually and --gtest_filter=PrintPreviewWebUITest.FLAKY_TestColorSettings
    
    Review URL: http://codereview.chromium.org/7514012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94177 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -275,6 +275,7 @@ function updateWithPrinterCapabilities(settingInfo) {
   if (color.checked != setColorAsDefault) {
     color.checked = setColorAsDefault;
     bw.checked = !setColorAsDefault;
+    setColor(color.checked);
   }
 }
 
