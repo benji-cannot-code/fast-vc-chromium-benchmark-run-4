3a442ce08878c45957f93f2360ab5079781e6fc1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-26 14:58:10 +0000

    Unreviewed, rolling out r91746. http://trac.webkit.org/changeset/91746 https://bugs.webkit.org/show_bug.cgi?id=65180
    It broke SL build (Requested by Ossy on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-07-26
    
    Source/JavaScriptCore:
    
    * wtf/text/StringImpl.cpp:
    (WTF::StringImpl::stripWhiteSpace):
    (WTF::StringImpl::simplifyWhiteSpace):
    * wtf/text/StringImpl.h:
    * wtf/text/WTFString.cpp:
    * wtf/text/WTFString.h:
    
    Source/WebCore:
    
    * dom/OptionElement.cpp:
    (WebCore::OptionElement::normalizeText):
    
    LayoutTests:
    
    * fast/forms/option-strip-unicode-spaces-expected.txt: Removed.
    * fast/forms/option-strip-unicode-spaces.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/text/StringImpl.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/text/StringImpl.h
@@ -62,7 +62,6 @@ enum TextCaseSensitivity { TextCaseSensitive, TextCaseInsensitive };
 typedef OwnFastMallocPtr<const UChar> SharableUChar;
 typedef CrossThreadRefCounted<SharableUChar> SharedUChar;
 typedef bool (*CharacterMatchFunctionPtr)(UChar);
-typedef bool (*IsWhiteSpaceFunctionPtr)(UChar);
 
 class StringImpl : public StringImplBase {
     friend struct JSC::IdentifierCStringTranslator;
@@ -290,9 +289,7 @@ public:
     PassRefPtr<StringImpl> foldCase();
 
     PassRefPtr<StringImpl> stripWhiteSpace();
-    PassRefPtr<StringImpl> stripWhiteSpace(IsWhiteSpaceFunctionPtr);
     PassRefPtr<StringImpl> simplifyWhiteSpace();
-    PassRefPtr<StringImpl> simplifyWhiteSpace(IsWhiteSpaceFunctionPtr);
 
     PassRefPtr<StringImpl> removeCharacters(CharacterMatchFunctionPtr);
 
--- third_party/WebKit/Source/WebCore/dom/OptionElement.cpp
+++ third_party/WebKit/Source/WebCore/dom/OptionElement.cpp
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "Element.h"
 #include "HTMLNames.h"
-#include "HTMLParserIdioms.h"
 #include "HTMLOptionElement.h"
 #include "OptionGroupElement.h"
 #include "ScriptElement.h"
@@ -99,11 +98,10 @@ String OptionElement::normalizeText(const Document* document, const String& src)
     String text = document->displayStringModifiedByEncoding(src);
 
     // In WinIE, leading and trailing whitespace is ignored in options and optgroups. We match this behavior.
-    text = text.stripWhiteSpace(isHTMLSpace);
+    text = text.stripWhiteSpace();
 
     // We want to collapse our whitespace too.  This will match other browsers.
-    text = text.simplifyWhiteSpace(isHTMLSpace);
-
+    text = text.simplifyWhiteSpace();
     return text;
 }
 
