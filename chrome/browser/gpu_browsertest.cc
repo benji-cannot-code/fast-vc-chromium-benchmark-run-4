d7c6d7e6a2cc69ce0269b4b72db2ada7890228f5
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 02:59:52 +0000

    Added browser tests to ensure WebGL and 2D canvas can render to a newly opened window. Review URL: http://codereview.chromium.org/7470003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_browsertest.cc
+++ chrome/browser/gpu_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GPUBrowserTest : public InProcessBrowserTest {
  protected:
   GPUBrowserTest() {
+    EnableDOMAutomation();
   }
 
   virtual void SetUpInProcessBrowserTestFixture() {
@@ -36,6 +37,8 @@ class GPUBrowserTest : public InProcessBrowserTest {
     EXPECT_TRUE(test_launcher_utils::OverrideGLImplementation(
         command_line,
         gfx::kGLImplementationOSMesaName));
+
+    command_line->AppendSwitch(switches::kDisablePopupBlocking);
 
 #if defined(OS_MACOSX)
     // Accelerated compositing does not work with OSMesa. AcceleratedSurface
@@ -63,4 +66,28 @@ IN_PROC_BROWSER_TEST_F(GPUBrowserTest, MAYBE_BrowserTestCanLaunchWithOSMesa) {
 
   EXPECT_EQ(ASCIIToUTF16("SUCCESS: Mesa OffScreen"),
             browser()->GetSelectedTabContents()->GetTitle());
+}
+
+IN_PROC_BROWSER_TEST_F(GPUBrowserTest, CanOpenPopupAndRenderWithWebGLCanvas) {
+  ui_test_utils::DOMMessageQueue message_queue;
+
+  ui_test_utils::NavigateToURL(
+      browser(),
+      net::FilePathToFileURL(gpu_test_dir_.AppendASCII("webgl_popup.html")));
+
+  std::string result;
+  ASSERT_TRUE(message_queue.WaitForMessage(&result));
+  EXPECT_EQ("\"SUCCESS\"", result);
+}
+
+IN_PROC_BROWSER_TEST_F(GPUBrowserTest, CanOpenPopupAndRenderWith2DCanvas) {
+  ui_test_utils::DOMMessageQueue message_queue;
+
+  ui_test_utils::NavigateToURL(
+      browser(),
+      net::FilePathToFileURL(gpu_test_dir_.AppendASCII("canvas_popup.html")));
+
+  std::string result;
+  ASSERT_TRUE(message_queue.WaitForMessage(&result));
+  EXPECT_EQ("\"SUCCESS\"", result);
 }
