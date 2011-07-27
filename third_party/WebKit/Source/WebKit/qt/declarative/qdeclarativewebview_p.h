9201cce0fbd919ec3051f4d99a7e0383029fee21
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 15:20:43 +0000

    [Qt] Fix build in Qt 5 of QDeclarativeWebView https://bugs.webkit.org/show_bug.cgi?id=65258
    Reviewed by Andreas Kling.
    
    In Qt 5, the QGraphicsView-based classes for QtQuick are in the
    qtquick1 module, including QDeclarativeItem and QDeclarativeView.
    
    .:
    
    * Source/WebKit.pro:
    
    Source/WebKit/qt:
    
    * declarative/qdeclarativewebview_p.h:
    * tests/qdeclarativewebview/tst_qdeclarativewebview.cpp:
    * tests/tests.pri:
    * tests/tests.pro:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/declarative/qdeclarativewebview_p.h
+++ third_party/WebKit/Source/WebKit/qt/declarative/qdeclarativewebview_p.h
@@ -24,7 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QtCore/QBasicTimer>
 #include <QtCore/QUrl>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#include <QtQuick1/QDeclarativeItem>
+#else
 #include <QtDeclarative/QDeclarativeItem>
+#endif
+
 #include <QtGui/QAction>
 #include <QtNetwork/QNetworkAccessManager>
 #include "qgraphicswebview.h"
