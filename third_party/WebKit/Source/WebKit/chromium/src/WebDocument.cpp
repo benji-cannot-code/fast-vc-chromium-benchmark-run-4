c8d10d79e814d4facf128951dd6c495ee8d3caa1
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 18:34:07 +0000

    [Chromium] Add better WebKit API for chrome.tabs.insertCSS extension API https://bugs.webkit.org/show_bug.cgi?id=65158
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Add per-Document instance user stylesheets (as opposed to the page
    group user styles and the global page stylesheet).
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::CSSStyleSelector):
    * css/CSSStyleSelector.h:
    * dom/Document.cpp:
    (WebCore::Document::~Document):
    (WebCore::Document::createStyleSelector):
    (WebCore::Document::addUserSheet):
    * dom/Document.h:
    
    Source/WebKit/chromium:
    
    Add WebDocument::insertUserStyleSheet which inserts a user stylesheet,
    instead of forcing extensions to use insertStyleText which manipulates
    the DOM of the page and results in compatibility issues (see
    http://crbug.com/82220).
    
    * public/WebDocument.h:
    * src/WebDocument.cpp:
    (WebKit::WebDocument::insertUserStyleSheet):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebDocument.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebDocument.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebDocument.h"
 
 #include "AXObjectCache.h"
+#include "CSSStyleSheet.h"
 #include "Document.h"
 #include "DocumentLoader.h"
 #include "DocumentType.h"
@@ -206,6 +207,16 @@ bool WebDocument::insertStyleText(const WebString& styleText, const WebString& e
     bool success = documentElement->insertBefore(stylesheet, documentElement->firstChild(), err);
     ASSERT(success);
     return success;
+}
+
+void WebDocument::insertUserStyleSheet(const WebString& sourceCode, UserStyleLevel level)
+{
+    RefPtr<Document> document = unwrap<Document>();
+
+    RefPtr<CSSStyleSheet> parsedSheet = CSSStyleSheet::create(document.get());
+    parsedSheet->setIsUserStyleSheet(level == UserStyleUserLevel);
+    parsedSheet->parseString(sourceCode, !document->inQuirksMode());
+    document->addUserSheet(parsedSheet.release());
 }
 
 WebDocument::WebDocument(const PassRefPtr<Document>& elem)
