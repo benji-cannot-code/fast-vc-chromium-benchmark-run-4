9b2fff9d826c03916c8cca10e910bcfc618bba9d
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 22:16:41 +0000

    Allow TestConfigurationConverter report conversion errors. https://bugs.webkit.org/show_bug.cgi?id=65287
    Also, fix a few miscellaneous testing issues.
    
    Reviewed by Adam Barth.
    
    * Scripts/webkitpy/layout_tests/models/test_configuration.py: Added a way to report unknown modifier errors.
    * Scripts/webkitpy/layout_tests/models/test_configuration_unittest.py: Added a test for reporting errors.
    * Scripts/webkitpy/layout_tests/port/chromium_unittest.py: Fixed an error where an invalid TestConfiguration instance
        was created. 'default' is not a valid build type.
    * Scripts/webkitpy/layout_tests/port/test.py: Ditto. '' is not a valid version.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_configuration_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_configuration_unittest.py
@@ -115,6 +115,12 @@ class TestConfigurationConverterTest(unittest.TestCase):
 
         self.assertEquals(converter.to_config_set(set(['xp', 'foo'])), set())
 
+        errors = []
+        self.assertEquals(converter.to_config_set(set(['xp', 'foo']), errors), set())
+        self.assertEquals(errors, ["Unrecognized modifier 'foo'"])
+
+        self.assertEquals(converter.to_config_set(set(['xp', 'x86_64'])), set())
+
         configs_to_match = set([
             TestConfiguration(None, 'xp', 'x86', 'release', 'gpu'),
             TestConfiguration(None, 'xp', 'x86', 'release', 'cpu'),
