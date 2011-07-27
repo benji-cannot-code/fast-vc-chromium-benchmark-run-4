02174c9422c4c70d20c303ec72bfb2aae5f57f05
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-27 12:10:34 +0000

    Use the suggested name from an anchor's 'download' attribute.
    This uses the feature added in WebKit: https://bugs.webkit.org/show_bug.cgi?id=64580
    
    BUG=89346
    TEST=manually
    
    Review URL: http://codereview.chromium.org/7484061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_types.cc
+++ chrome/browser/download/download_types.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,8 @@ DownloadSaveInfo::DownloadSaveInfo() {
 
 DownloadSaveInfo::DownloadSaveInfo(const DownloadSaveInfo& info)
     : file_path(info.file_path),
-      file_stream(info.file_stream) {
+      file_stream(info.file_stream),
+      suggested_name(info.suggested_name) {
 }
 
 DownloadSaveInfo::~DownloadSaveInfo() {
@@ -25,5 +26,6 @@ DownloadSaveInfo::~DownloadSaveInfo() {
 DownloadSaveInfo& DownloadSaveInfo::operator=(const DownloadSaveInfo& info) {
   file_path = info.file_path;
   file_stream = info.file_stream;
+  suggested_name = info.suggested_name;
   return *this;
 }
