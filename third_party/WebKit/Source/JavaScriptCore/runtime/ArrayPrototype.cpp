d5721da8cfc8bcf513781f0ff9bfa591bc96d904
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 06:02:54 +0000

    reduce and reduceRight bind callback's this to null rather than undefined https://bugs.webkit.org/show_bug.cgi?id=62264
    Patch by Mark Hahnenberg <mhahnenberg@apple.com> on 2011-07-26
    Reviewed by Oliver Hunt.
    
    Source/JavaScriptCore:
    
    Fixed Array.prototype.reduce and Array.prototype.reduceRight so that they behave correctly
    when calling the callback function without an argument for this, which means it should
    be undefined according to ES 15.4.4.21 and 15.4.4.22.
    
    * runtime/ArrayPrototype.cpp:
    (JSC::arrayProtoFuncReduce):
    (JSC::arrayProtoFuncReduceRight):
    
    LayoutTests:
    
    Added additional tests to test Array.prototype.reduce and Array.prototype.reduceRight
    when calling the callback function without an argument for this, which means it should
    be undefined according to ES 15.4.4.21 and 15.4.4.22.
    
    * fast/js/array-reduce-expected.txt:
    * fast/js/array-reduceRight-expected.txt:
    * fast/js/script-tests/array-reduce.js:
    * fast/js/script-tests/array-reduceRight.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/ArrayPrototype.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/ArrayPrototype.cpp
@@ -982,7 +982,7 @@ EncodedJSValue JSC_HOST_CALL arrayProtoFuncReduce(ExecState* exec)
     if (callType == CallTypeJS && array) {
         CachedCall cachedCall(exec, asFunction(function), 4);
         for (; i < length && !exec->hadException(); ++i) {
-            cachedCall.setThis(jsNull());
+            cachedCall.setThis(jsUndefined());
             cachedCall.setArgument(0, rv);
             JSValue v;
             if (LIKELY(array->canGetIndex(i)))
@@ -1011,7 +1011,7 @@ EncodedJSValue JSC_HOST_CALL arrayProtoFuncReduce(ExecState* exec)
         eachArguments.append(jsNumber(i));
         eachArguments.append(thisObj);
         
-        rv = call(exec, function, callType, callData, jsNull(), eachArguments);
+        rv = call(exec, function, callType, callData, jsUndefined(), eachArguments);
     }
     return JSValue::encode(rv);
 }
@@ -1058,7 +1058,7 @@ EncodedJSValue JSC_HOST_CALL arrayProtoFuncReduceRight(ExecState* exec)
         CachedCall cachedCall(exec, asFunction(function), 4);
         for (; i < length && !exec->hadException(); ++i) {
             unsigned idx = length - i - 1;
-            cachedCall.setThis(jsNull());
+            cachedCall.setThis(jsUndefined());
             cachedCall.setArgument(0, rv);
             if (UNLIKELY(!array->canGetIndex(idx)))
                 break; // length has been made unsafe while we enumerate fallback to slow path
@@ -1085,7 +1085,7 @@ EncodedJSValue JSC_HOST_CALL arrayProtoFuncReduceRight(ExecState* exec)
         eachArguments.append(jsNumber(idx));
         eachArguments.append(thisObj);
         
-        rv = call(exec, function, callType, callData, jsNull(), eachArguments);
+        rv = call(exec, function, callType, callData, jsUndefined(), eachArguments);
     }
     return JSValue::encode(rv);        
 }
