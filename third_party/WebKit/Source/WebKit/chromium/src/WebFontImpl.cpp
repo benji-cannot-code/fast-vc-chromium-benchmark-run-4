43683f629d29b456b1fdd79bbfe22aa2193be178
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 13:56:16 +0000

    Speculative win build fix after r91835 https://bugs.webkit.org/show_bug.cgi?id=65255
    Unreviewed build fix.
    
    * src/WebFontImpl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFontImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFontImpl.cpp
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebRect.h"
 #include "WebTextRun.h"
 
+#include <skia/ext/platform_canvas.h>
+
 using namespace WebCore;
 
 namespace WebKit {
