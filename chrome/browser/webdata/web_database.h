821e3c5dec399022555949903892ce84cfb12920
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-27 20:51:36 +0000

    First implementation of web intents table.
    BUG=none
    TEST=none
    
    R=dhollowa@chromium.org
    
    Review URL: http://codereview.chromium.org/7461089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_database.h
+++ chrome/browser/webdata/web_database.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webdata/logins_table.h"
 #include "chrome/browser/webdata/token_service_table.h"
 #include "chrome/browser/webdata/web_apps_table.h"
+#include "chrome/browser/webdata/web_intents_table.h"
 #include "sql/connection.h"
 #include "sql/init_status.h"
 #include "sql/meta_table.h"
@@ -39,6 +40,7 @@ class WebDatabase {
   virtual LoginsTable* GetLoginsTable();
   virtual TokenServiceTable* GetTokenServiceTable();
   virtual WebAppsTable* GetWebAppsTable();
+  virtual WebIntentsTable* GetWebIntentsTable();
 
   // Exposed for testing only.
   sql::Connection* GetSQLConnection();
@@ -56,6 +58,7 @@ class WebDatabase {
   scoped_ptr<LoginsTable> logins_table_;
   scoped_ptr<TokenServiceTable> token_service_table_;
   scoped_ptr<WebAppsTable> web_apps_table_;
+  scoped_ptr<WebIntentsTable> web_intents_table_;
 
   scoped_ptr<NotificationService> notification_service_;
 
