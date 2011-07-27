5a32688e130057493a348ebcaa16eb24bf4d54f7
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-27 13:34:41 +0000

    Fix Qt (and possibly Gtk) WebKit2 build after addition of -webkit-pictograph https://bugs.webkit.org/show_bug.cgi?id=65253
    Reviewed by Andreas Kling.
    
    The -webkit-pictograph generic family was added by bug 65197.
    
    * Shared/WebPreferencesStore.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebPreferencesStore.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebPreferencesStore.h
@@ -134,6 +134,7 @@ namespace WebKit {
     macro(FixedFontFamily, fixedFontFamily, String, String, "Courier New") \
     macro(SansSerifFontFamily, sansSerifFontFamily, String, String, "Helvetica") \
     macro(SerifFontFamily, serifFontFamily, String, String, "Times") \
+    macro(PictographFontFamily, pictographFontFamily, String, String, "Times") \
     \
 
 #endif
