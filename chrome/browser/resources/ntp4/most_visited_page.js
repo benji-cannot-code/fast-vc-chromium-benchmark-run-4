eca1e4b5880443d961b187967d239320da9ae003
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-27 22:27:19 +0000

    ntp4: polish
    1. re-layout tile pages when they are shown (see comments for why)
    2. improve nav dots (disallow tabbing to label, improve focus indicator)
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7497004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/most_visited_page.js
+++ chrome/browser/resources/ntp4/most_visited_page.js
@@ -58,7 +58,7 @@ cr.define('ntp4', function() {
           '<div class="color-stripe"></div>' +
           '<span class="title"></span>';
 
-      this.tabIndex = -1;
+      this.removeAttribute('tabIndex');
       this.data_ = null;
       this.removeAttribute('id');
     },
@@ -77,6 +77,7 @@ cr.define('ntp4', function() {
       var id = tileID++;
       this.setAttribute('id', 'tile' + id);
       this.data_ = data;
+      // TODO(estade): this shouldn't be focusable if the page isn't showing.
       this.tabIndex = 0;
       this.classList.remove('filler');
 
