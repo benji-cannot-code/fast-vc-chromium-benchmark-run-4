41432367b570bde4917fe356740e1a775cd9d11f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-27 23:25:28 +0000

    cros: Don't include chromeos_brightness.h in BrightnessLibrary header file.
    BUG=None
    TEST=None
    
    R=stevenjb@chromium.org
    
    Review URL: http://codereview.chromium.org/7524001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/brightness_library.cc
+++ chrome/browser/chromeos/cros/brightness_library.cc
@@ -5,12 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/cros/brightness_library.h"
 
+#include "base/basictypes.h"
+#include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/observer_list.h"
-#include "chrome/browser/chromeos/brightness_bubble.h"
-#include "chrome/browser/chromeos/volume_bubble.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "content/browser/browser_thread.h"
+#include "third_party/cros/chromeos_brightness.h"
 
 namespace chromeos {
 
