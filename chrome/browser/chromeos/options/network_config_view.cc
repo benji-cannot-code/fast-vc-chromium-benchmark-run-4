49eb5411406ab74af4f0890ee9f96399baecc01b
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 22:45:02 +0000

    Revert 94543 - Include non hardware backed certificates in CertList
    CrOS is red.... so we're reverting this probable cause.
    
    All certificates will now be listed in user certficate combo boxes.
    Selecting one that is not hardware-based will display an error message and prevent a login attempt.
    
    BUG=chromium-os:17766
    TEST=Test that all user certificates show up in the combobox in Wifi and VPN connect dialogs. Ensure that connect is disabled when non hardware-backed certs are selected, and that it succeeds with hardware-backed certs.
    
    Review URL: http://codereview.chromium.org/7519022
    
    TBR=stevenjb@google.com
    Review URL: http://codereview.chromium.org/7531011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/network_config_view.cc
+++ chrome/browser/chromeos/options/network_config_view.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 // static
-const int ChildNetworkConfigView::kInputFieldMinWidth = 270;
+const int ChildNetworkConfigView::kPassphraseWidth = 150;
 
 NetworkConfigView::NetworkConfigView(Network* network)
     : delegate_(NULL),
