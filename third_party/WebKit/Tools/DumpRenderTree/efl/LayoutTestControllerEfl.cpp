007f41abfaca6c875fb1a2025c830f81d2ae950a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-28 04:08:40 +0000

    [EFL] Drop WorkQueueItemEfl.h in favour of the general WorkQueueItem.h https://bugs.webkit.org/show_bug.cgi?id=65269
    Patch by Raphael Kubo da Costa <kubo@profusion.mobi> on 2011-07-27
    Reviewed by Kent Tamura.
    
    WorkQueueItemEfl.h was being included by efl's
    LayoutTestControllerEfl.cpp, however the top-level
    LayoutTestController still included WorkQueueItem.h, causing a lot of
    type mismatches when running the WorkQueueItem code.
    
    We can stick to the original WorkQueueItem header by making a few
    changes to EFL's LayoutTestController.
    
    This should make tests such as
    fast/loader/non-deferred-substitute-load.html stop crashing.
    
    * DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
    (LayoutTestController::queueLoad):
    * DumpRenderTree/efl/WorkQueueItemEfl.cpp:
    (LoadItem::invoke):
    (LoadHTMLStringItem::invoke):
    (ScriptItem::invoke):
    * DumpRenderTree/efl/WorkQueueItemEfl.h: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NotImplemented.h"
 #include "OwnFastMallocPtr.h"
 #include "WorkQueue.h"
-#include "WorkQueueItemEfl.h"
+#include "WorkQueueItem.h"
 #include "ewk_private.h"
 #include <EWebKit.h>
 #include <JavaScriptCore/JSRetainPtr.h>
@@ -177,9 +177,11 @@ void LayoutTestController::queueLoad(JSStringRef url, JSStringRef target)
 {
     String absoluteUrl = String::fromUTF8(ewk_frame_uri_get(mainFrame));
     absoluteUrl.append(url->characters(), url->length());
-    String targetString(target->characters(), target->length());
 
-    WorkQueue::shared()->queue(new LoadItem(absoluteUrl, targetString));
+    JSRetainPtr<JSStringRef> jsAbsoluteURL(
+        Adopt, JSStringCreateWithUTF8CString(absoluteUrl.utf8().data()));
+
+    WorkQueue::shared()->queue(new LoadItem(jsAbsoluteURL.get(), target));
 }
 
 void LayoutTestController::setAcceptsEditing(bool acceptsEditing)
