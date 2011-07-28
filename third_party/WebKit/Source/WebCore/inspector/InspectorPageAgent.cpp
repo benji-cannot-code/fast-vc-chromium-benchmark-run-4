b8f852e36a86bf0368d1aac842a2e3a6de86565c
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-28 04:34:10 +0000

    Web Inspector: [REGRESSION] [Chromium] Opening link in new tab does not work properly. https://bugs.webkit.org/show_bug.cgi?id=65279
    Reviewed by Pavel Feldman.
    
    * inspector/InspectorPageAgent.cpp:
    (WebCore::InspectorPageAgent::open):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@91901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -300,6 +300,8 @@ void InspectorPageAgent::reload(ErrorString*, const bool* const optionalIgnoreCa
 
 void InspectorPageAgent::open(ErrorString*, const String& url, const bool* const inNewWindow)
 {
+    UserGestureIndicator indicator(DefinitelyProcessingUserGesture);
+
     Frame* mainFrame = m_page->mainFrame();
     Frame* frame;
     if (inNewWindow && *inNewWindow) {
@@ -316,7 +318,6 @@ void InspectorPageAgent::open(ErrorString*, const String& url, const bool* const
     } else
         frame = mainFrame;
 
-    UserGestureIndicator indicator(DefinitelyProcessingUserGesture);
     // FIXME: Why does one use mainFrame and the other frame?
     frame->loader()->changeLocation(mainFrame->document()->securityOrigin(), frame->document()->completeURL(url), "", false, false);
 }
