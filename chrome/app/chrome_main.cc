3f774d947fa94c165f331425e74e09d4e83d851b
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 12:12:10 +0000

    Remove extrenuous include that snuck in as part of r94279
    BUG=None
    TEST=None
    TBR=mark@chromium.org
    
    
    Review URL: http://codereview.chromium.org/7520025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94446 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/chromium_strings.h"
 #include "third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.h"
 #include "ui/base/l10n/l10n_util_mac.h"
-#include "ui/base/l10n/l10n_util.h"
 #endif
 
 #if defined(OS_POSIX)
