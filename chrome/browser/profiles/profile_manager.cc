e4d2ef7d9bcb57c840d9aed3f8555d27de300f47
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 23:04:34 +0000

    Reenable multi-profiles by default after m14 branch.
    BUG=none
    TEST=return of the cupcake on ToT
    
    Review URL: http://codereview.chromium.org/7492076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -560,5 +560,8 @@ void ProfileManager::ScheduleProfileForDeletion(const FilePath& profile_dir) {
 
 // static
 bool ProfileManager::IsMultipleProfilesEnabled() {
+#if defined(TOOLKIT_VIEWS) && !defined(OS_CHROMEOS)
+  return true;
+#endif
   return CommandLine::ForCurrentProcess()->HasSwitch(switches::kMultiProfiles);
 }
