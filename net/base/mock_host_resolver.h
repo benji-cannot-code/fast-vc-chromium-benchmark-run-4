b2d2606d33c5cd699175caadcc4b0f5adf9b4e8c
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 21:28:18 +0000

    Relanding 94539 - Move MockHostResolver to net_test_support
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7531002
    
    TBR=agayev@chromium.org
    Review URL: http://codereview.chromium.org/7529008
    
    TBR=agayev@chromium.org
    Review URL: http://codereview.chromium.org/7530012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94549 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mock_host_resolver.h
+++ net/base/mock_host_resolver.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "net/base/host_resolver_impl.h"
 #include "net/base/host_resolver_proc.h"
-#include "net/base/net_api.h"
 
 namespace net {
 
@@ -39,7 +38,7 @@ class RuleBasedHostResolverProc;
 // re-map one hostname to another as well.
 
 // Base class shared by MockHostResolver and MockCachingHostResolver.
-class NET_TEST MockHostResolverBase : public HostResolver {
+class MockHostResolverBase : public HostResolver {
  public:
   virtual ~MockHostResolverBase();
 
@@ -103,7 +102,7 @@ class MockCachingHostResolver : public MockHostResolverBase {
 // a replacement host string. It then uses the system host resolver to return
 // a socket address. Generally the replacement should be an IPv4 literal so
 // there is no network dependency.
-class NET_TEST RuleBasedHostResolverProc : public HostResolverProc {
+class RuleBasedHostResolverProc : public HostResolverProc {
  public:
   explicit RuleBasedHostResolverProc(HostResolverProc* previous);
 
@@ -154,7 +153,7 @@ class NET_TEST RuleBasedHostResolverProc : public HostResolverProc {
 };
 
 // Using WaitingHostResolverProc you can simulate very long lookups.
-class NET_TEST WaitingHostResolverProc : public HostResolverProc {
+class WaitingHostResolverProc : public HostResolverProc {
  public:
   explicit WaitingHostResolverProc(HostResolverProc* previous);
 
@@ -182,7 +181,7 @@ class NET_TEST WaitingHostResolverProc : public HostResolverProc {
 //
 // NOTE: Only use this as a catch-all safety net. Individual tests should use
 // MockHostResolver.
-class NET_TEST ScopedDefaultHostResolverProc {
+class ScopedDefaultHostResolverProc {
  public:
   ScopedDefaultHostResolverProc();
   explicit ScopedDefaultHostResolverProc(HostResolverProc* proc);
--- net/net.gyp
+++ net/net.gyp
@@ -127,9 +127,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'base/mime_sniffer.h',
         'base/mime_util.cc',
         'base/mime_util.h',
-        # TODO(eroman): move this into its own test-support target.
-        'base/mock_host_resolver.cc',
-        'base/mock_host_resolver.h',
         'base/net_api.h',
         'base/net_error_list.h',
         'base/net_errors.cc',
@@ -1192,6 +1189,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'base/cookie_monster_store_test.h',
         'base/cookie_store_test_helpers.cc',
         'base/cookie_store_test_helpers.h',
+        'base/mock_host_resolver.cc',
+        'base/mock_host_resolver.h',
         'base/net_test_suite.cc',
         'base/net_test_suite.h',
         'base/test_completion_callback.cc',
