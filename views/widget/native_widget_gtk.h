a7abe55b7ec9ceea2b3f1de8384eb870f19e19b5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-28 16:38:13 +0000

    Fix grab/ungrab in views-desktop world.
    In views-desktop, if you click on a window once, all the subsequent click events
    go to that window until it is closed. This is a fix for that. I have updated the
    relevant test to catch such errors.
    
    BUG=none
    TEST=WidgetTest.GrabUngrab
    
    Review URL: http://codereview.chromium.org/7519006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94479 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_gtk.h
+++ views/widget/native_widget_gtk.h
@@ -291,6 +291,9 @@ class VIEWS_API NativeWidgetGtk : public internal::NativeWidgetPrivate,
   // application.
   virtual void HandleGtkGrabBroke();
 
+  const internal::NativeWidgetDelegate* delegate() const { return delegate_; }
+  internal::NativeWidgetDelegate* delegate() { return delegate_; }
+
  private:
   class DropObserver;
   friend class DropObserver;
