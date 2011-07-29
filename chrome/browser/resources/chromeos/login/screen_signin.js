9cc65eae40b1019d58ebc8df2ea6b2661260bc44
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 16:32:31 +0000

    [ChromeOS] Make button strip visible in login display.
    So that buttons of screens (sign-in and user-image) are still visible.
    
    BUG=chromium-os:18334
    TEST=Verify fix for chromium-os:18334
    
    
    Review URL: http://codereview.chromium.org/7533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_signin.js
+++ chrome/browser/resources/chromeos/login/screen_signin.js
@@ -36,8 +36,6 @@ cr.define('login', function() {
       $('email').addEventListener('keydown', this.handleKeyDown_.bind(this));
       $('email').addEventListener('blur', this.handleEmailBlur_);
       $('password').addEventListener('keydown', this.handleKeyDown_.bind(this));
-      $('signin-button-in-place').addEventListener('click',
-          this.handleSigninClick_.bind(this));
     },
 
     /**
@@ -75,7 +73,6 @@ cr.define('login', function() {
       $('email').hidden = !enable;
       $('password').hidden = !enable;
       $('signin-button').hidden = !enable;
-      $('signin-button-in-place').hidden = !enable;
     },
 
     /**
