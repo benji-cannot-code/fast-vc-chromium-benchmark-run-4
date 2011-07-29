7a63911ceb1bd57c0f95287c1fc69b94ef78e55f
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 02:47:43 +0000

    sync: Disable network change notifications for unauthenticated users.
    BUG=90944
    TEST=none
    
    Review URL: http://codereview.chromium.org/7530023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94624 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncapi.cc
+++ chrome/browser/sync/engine/syncapi.cc
@@ -1202,6 +1202,7 @@ class SyncManager::SyncInternal
         registrar_(NULL),
         initialized_(false),
         setup_for_test_mode_(false),
+        observing_ip_address_changes_(false),
         js_transaction_observer_(ALLOW_THIS_IN_INITIALIZER_LIST(this)) {
     // Pre-fill |notification_info_map_|.
     for (int i = syncable::FIRST_REAL_MODEL_TYPE;
@@ -1628,6 +1629,9 @@ class SyncManager::SyncInternal
   // scheduler actually communicating with the server).
   bool setup_for_test_mode_;
 
+  // Whether we should respond to an IP address change notification.
+  bool observing_ip_address_changes_;
+
   // Map used to store the notification info to be displayed in
   // about:sync page.
   NotificationInfoMap notification_info_map_;
@@ -1772,6 +1776,7 @@ bool SyncManager::SyncInternal::Init(
       sync_server_and_path, port, use_ssl, user_agent, post_factory));
 
   net::NetworkChangeNotifier::AddIPAddressObserver(this);
+  observing_ip_address_changes_ = true;
 
   connection_manager()->AddListener(this);
 
@@ -1943,6 +1948,8 @@ void SyncManager::SyncInternal::UpdateCredentials(
   DCHECK_EQ(credentials.email, share_.name);
   DCHECK(!credentials.email.empty());
   DCHECK(!credentials.sync_token.empty());
+
+  observing_ip_address_changes_ = true;
   connection_manager()->set_auth_token(credentials.sync_token);
   sync_notifier_->UpdateCredentials(
       credentials.email, credentials.sync_token);
@@ -2239,6 +2246,7 @@ void SyncManager::SyncInternal::Shutdown() {
   connection_manager_.reset();
 
   net::NetworkChangeNotifier::RemoveIPAddressObserver(this);
+  observing_ip_address_changes_ = false;
 
   if (dir_manager()) {
     dir_manager()->FinalSaveChangesForAll();
@@ -2262,6 +2270,11 @@ void SyncManager::SyncInternal::Shutdown() {
 
 void SyncManager::SyncInternal::OnIPAddressChanged() {
   VLOG(1) << "IP address change detected";
+  if (!observing_ip_address_changes_) {
+    VLOG(1) << "IP address change dropped.";
+    return;
+  }
+
 #if defined (OS_CHROMEOS)
   // TODO(tim): This is a hack to intentionally lose a race with flimflam at
   // shutdown, so we don't cause shutdown to wait for our http request.
@@ -2294,6 +2307,7 @@ void SyncManager::SyncInternal::OnServerConnectionEvent(
   }
 
   if (event.connection_code == browser_sync::HttpResponse::SYNC_AUTH_ERROR) {
+    observing_ip_address_changes_ = false;
     ObserverList<SyncManager::Observer> temp_obs_list;
     CopyObservers(&temp_obs_list);
     FOR_EACH_OBSERVER(SyncManager::Observer, temp_obs_list,
