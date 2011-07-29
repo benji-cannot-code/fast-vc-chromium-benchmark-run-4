641fb80218a9c11936c0956d97900d78f7c2b2dd
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 03:08:36 +0000

    mac: Components build for net
    BUG=90078
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7529017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94629 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_resolver_mac.h
+++ net/proxy/proxy_resolver_mac.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "googleurl/src/gurl.h"
+#include "net/base/net_api.h"
 #include "net/base/net_errors.h"
 #include "net/proxy/proxy_resolver.h"
 
@@ -16,7 +17,7 @@ namespace net {
 
 // Implementation of ProxyResolver that uses the Mac CFProxySupport to implement
 // proxies.
-class ProxyResolverMac : public ProxyResolver {
+class NET_API ProxyResolverMac : public ProxyResolver {
  public:
   ProxyResolverMac();
   virtual ~ProxyResolverMac();
