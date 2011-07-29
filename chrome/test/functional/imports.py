3a074601c929c65bab6d07892a5bb66dd131bb76
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 21:37:12 +0000

    Disable some failing and flaky pyauto tests. BUG=91033,85307,91030,89768 TEST=none
    Review URL: http://codereview.chromium.org/7538005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/imports.py
+++ chrome/test/functional/imports.py
@@ -225,6 +225,8 @@ class ImportsTest(pyauto.PyUITest):
     if not self._CanRunFirefoxTests():
       logging.warn('Not running firefox import tests.')
       return
+    if self.IsWinVista():  # Broken on vista. crbug.com/89768
+      return
     self._SwapFirefoxProfile()
     self.ImportSettings('Mozilla Firefox', False, self._to_import)
     self._CheckDefaults(bookmarks=True, history=True, passwords=True,
@@ -252,6 +254,8 @@ class ImportsTest(pyauto.PyUITest):
     """
     if not self._CanRunFirefoxTests():
       logging.warn('Not running firefox import tests.')
+      return
+    if self.IsWinVista():  # Broken on vista. crbug.com/89768
       return
     self._SwapFirefoxProfile()
     self.ImportSettings('Mozilla Firefox', False, self._to_import)
