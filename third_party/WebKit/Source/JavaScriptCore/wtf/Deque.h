6aafbfc98ef50ea2746258bc27cb60e2912270c6
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-30 11:43:07 +0000

    MessageQueue::waitForMessageFilteredWithTimeout can triggers an assertion https://bugs.webkit.org/show_bug.cgi?id=65263
    Reviewed by Dmitry Titov.
    
    * wtf/Deque.h:
    (WTF::::operator): Don't check the validity of an iterator
    that will be reassigned right now.
    * wtf/MessageQueue.h:
    (WTF::::removeIf): Revert r51198 as I beleave this is the better
    solution for the problem that was solved by that.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Deque.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Deque.h
@@ -614,7 +614,6 @@ namespace WTF {
     template<typename T, size_t inlineCapacity>
     inline DequeIteratorBase<T, inlineCapacity>& DequeIteratorBase<T, inlineCapacity>::operator=(const Base& other)
     {
-        checkValidity();
         other.checkValidity();
         removeFromIteratorsList();
 
