813401d78827d7c2a1427947d84d31a16e9126f2
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-30 02:17:08 +0000

    Actually import the logging module!
    * Scripts/webkitpy/common/checkout/scm/git.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/git.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/scm/git.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import logging
 import os
 import re
 
@@ -39,6 +40,9 @@ from webkitpy.common.system import ospath
 from .commitmessage import CommitMessage
 from .scm import AuthenticationError, SCM, commit_error_handler
 from .svn import SVN, SVNRepository
+
+
+_log = logging.getLogger(__name__)
 
 
 def run_command(*args, **kwargs):
