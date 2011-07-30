7a952a1fa86d6882d41d3e5c7d58d8238b6ab069
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-30 18:12:47 +0000

    Add missing files to the server.
    * Scripts/webkitpy/tool/servers/gardeningserver.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
@@ -46,8 +46,10 @@ class GardeningHTTPRequestHandler(ReflectionHandler):
         "garden-o-matic.html",
         "main.css",
         "main.js",
+        "model.js",
         "partytime.gif",
         "results.js",
+        "Trac.js",
         "ui.js",
     ])
 
