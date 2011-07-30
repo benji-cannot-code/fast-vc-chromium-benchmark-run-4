4d0b34d6793d5038f9e2f2c2457db048ee3fd108
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-30 09:25:03 +0000

    Revert 94581 - Fix possible memory problems in the ProxyLauncher
    ... introduced by my earlier change. It seems that
    someone tries to access AutomationProxy after
    DisconnectFromRunningBrowser destroys it, and the entire thing crashes.
    
    BUG=90489,91023
    
    Review URL: http://codereview.chromium.org/7533006
    
    TBR=phajdan.jr@chromium.org
    Review URL: http://codereview.chromium.org/7488070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/proxy_launcher.cc
+++ chrome/test/automation/proxy_launcher.cc
@@ -155,8 +155,7 @@ void ProxyLauncher::CloseBrowserAndServer() {
 }
 
 void ProxyLauncher::DisconnectFromRunningBrowser() {
-  if (automation_proxy_.get())
-    automation_proxy_->Disconnect();
+  automation_proxy_.reset();
 }
 
 bool ProxyLauncher::LaunchBrowser(const LaunchState& state) {
