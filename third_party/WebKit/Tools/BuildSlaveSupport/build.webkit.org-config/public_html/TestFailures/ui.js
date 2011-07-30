99fb56e91535fa1261575038fd2eb4629295def7
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-07-30 18:24:13 +0000

    garden-o-matic should strikeout revisions that have been rolled out https://bugs.webkit.org/show_bug.cgi?id=65227
    Reviewed by Dimitri Glazkov.
    
    This helps gardeners keep track of which problems have been solved.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Trac.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.css:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/model.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui.js
@@ -127,6 +127,8 @@ ui.commitEntry = function(commitData)
         $('.reviewer', entry).text(commitData.reviewer);
     else
         $('.reviewer-container', entry).detach();
+    if (commitData.wasReverted)
+        entry.addClass('reverted');
 
     return entry;
 }
