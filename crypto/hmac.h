6a656491d8ae0c78ff65a8c87188f4cc0c4828bd
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-30 05:18:01 +0000

    Add WARN_UNUSED_RESULT to crypto/hmac.h
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7522014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94826 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/hmac.h
+++ crypto/hmac.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/string_piece.h"
 #include "crypto/crypto_api.h"
@@ -39,11 +40,11 @@ class CRYPTO_API HMAC {
   // Initializes this instance using |key| of the length |key_length|. Call Init
   // only once. It returns false on the second or later calls.
   // TODO(abarth): key_length should be a size_t.
-  bool Init(const unsigned char* key, int key_length);
+  bool Init(const unsigned char* key, int key_length) WARN_UNUSED_RESULT;
 
   // Initializes this instance using |key|. Call Init only once. It returns
   // false on the second or later calls.
-  bool Init(const base::StringPiece& key) {
+  bool Init(const base::StringPiece& key) WARN_UNUSED_RESULT {
     return Init(reinterpret_cast<const unsigned char*>(key.data()),
                 static_cast<int>(key.size()));
   }
@@ -53,7 +54,7 @@ class CRYPTO_API HMAC {
   // returned in |digest|, which has |digest_length| bytes of storage available.
   // TODO(abarth): digest_length should be a size_t.
   bool Sign(const base::StringPiece& data, unsigned char* digest,
-            int digest_length) const;
+            int digest_length) const WARN_UNUSED_RESULT;
 
   // Verifies that the HMAC for the message in |data| equals the HMAC provided
   // in |digest|, using the algorithm supplied to the constructor and the key
@@ -63,12 +64,13 @@ class CRYPTO_API HMAC {
   // undermine the cryptographic integrity. |digest| must be exactly
   // |DigestLength()| bytes long.
   bool Verify(const base::StringPiece& data,
-              const base::StringPiece& digest) const;
+              const base::StringPiece& digest) const WARN_UNUSED_RESULT;
 
   // Verifies a truncated HMAC, behaving identical to Verify(), except
   // that |digest| is allowed to be smaller than |DigestLength()|.
-  bool VerifyTruncated(const base::StringPiece& data,
-                       const base::StringPiece& digest) const;
+  bool VerifyTruncated(
+      const base::StringPiece& data,
+      const base::StringPiece& digest) const WARN_UNUSED_RESULT;
 
  private:
   HashAlgorithm hash_alg_;
