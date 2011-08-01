3fbae779c2539b0d396a7567c176e462c9bbf3ec
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 20:51:14 +0000

    GTTF: Clean up UITestSuite:
    - removed cruft
    - actually fixed a bug possibly resulting in error messages gumming up bots
    
    BUG=none
    
    Review URL: http://codereview.chromium.org/7488067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ui_test_suite.h
+++ chrome/test/ui/ui_test_suite.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/process.h"
 #include "chrome/test/base/chrome_test_suite.h"
-#include "chrome/test/ui/ui_test.h"
 
 class UITestSuite : public ChromeTestSuite {
  public:
@@ -17,10 +16,7 @@ class UITestSuite : public ChromeTestSuite {
 
  protected:
   virtual void Initialize();
-
   virtual void Shutdown();
-
-  virtual void SuppressErrorDialogs();
 
  private:
 #if defined(OS_WIN)
@@ -28,13 +24,6 @@ class UITestSuite : public ChromeTestSuite {
 
   base::ProcessHandle crash_service_;
 #endif
-
-  static const char kUseExistingBrowser[];
-  static const char kTestTimeout[];
-  static const char kBatchCount[];
-  static const char kBatchIndex[];
-  static const char kGTestTotalShards[];
-  static const char kGTestShardIndex[];
 };
 
 #endif  // CHROME_TEST_UI_UI_TEST_SUITE_H_
