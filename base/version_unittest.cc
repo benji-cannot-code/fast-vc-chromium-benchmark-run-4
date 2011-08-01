db3a38954305195003c3f71b15ac4bee775ef869
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 17:45:08 +0000

    Add a convenience function to Version
    Came up as in item in a component updater review. It is a common pattern used in extension updater and component updater.
    
    TEST=included
    BUG=none
    Review URL: http://codereview.chromium.org/7541015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94928 0039d316-1c4b-4281-b951-d872f2087c98

--- base/version_unittest.cc
+++ base/version_unittest.cc
@@ -86,5 +86,7 @@ TEST_F(VersionTest, Compare) {
     scoped_ptr<Version> rhs(Version::GetVersionFromString(cases[i].rhs));
     EXPECT_EQ(lhs->CompareTo(*rhs), cases[i].expected) <<
           cases[i].lhs << " ? " << cases[i].rhs;
+
+    EXPECT_EQ(lhs->IsOlderThan(cases[i].rhs), (cases[i].expected == -1));
   }
 }
