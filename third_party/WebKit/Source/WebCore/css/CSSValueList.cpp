a1383632f6a0684bb58a530808227bedbf5a14c8
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-01 08:31:43 +0000

    Add iterator to CSSValueList https://bugs.webkit.org/show_bug.cgi?id=65297
    Reviewed by Darin Adler.
    
    No new tests / refactoring only.
    
    * css/CSSPrimitiveValue.h:
    (WebCore::CSSPrimitiveValue::isLength):
    Add shorthand to determine if this primitive value is a length.
    * css/CSSStyleSelector.cpp:
    Use CSSValueListIterator throughout.
    (WebCore::CSSStyleSelector::applyProperty):
    (WebCore::CSSStyleSelector::applyPageSizeProperty):
    (WebCore::CSSStyleSelector::createTransformOperations):
    * css/CSSValueList.cpp:
    (WebCore::CSSValueList::copy):
    Use itemWithoutBoundsCheck() instead of item().
    * css/CSSValueList.h:
    Add CSSValueListIterator and CSSValueListInspector class definitions.
    (WebCore::CSSValueList::item)
    Provide inline definition of item.
    (WebCore::CSSValueListIterator::CSSValueListIterator):
    (WebCore::CSSValueListIterator::hasMore):
    Return true if there are more values to consume, including the current value.
    (WebCore::CSSValueListIterator::value):
    Return the value at the current position.
    (WebCore::CSSValueListIterator::next):
    Move the iterator forward to the next item.
    (WebCore::CSSValueListIterator::index):
    Return the current position in the list.
    (WebCore::CSSValueListInspector::item):
    Return the item at a given index.
    (WebCore::CSSValueListInspector::first):
    Return the first item in the list.
    (WebCore::CSSValueListInspector::second):
    Return the second item in the list.
    (WebCore::CSSValueListInspector::length):
    Return the size of the underlying list.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSValueList.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSValueList.cpp
@@ -47,13 +47,6 @@ CSSValueList::~CSSValueList()
 {
 }
 
-CSSValue* CSSValueList::item(unsigned index)
-{
-    if (index >= m_values.size())
-        return 0;
-    return m_values[index].get();
-}
-
 unsigned short CSSValueList::cssValueType() const
 {
     return CSS_VALUE_LIST;
@@ -99,7 +92,7 @@ PassRefPtr<CSSValueList> CSSValueList::copy()
 {
     PassRefPtr<CSSValueList> newList = m_isSpaceSeparated ? createSpaceSeparated() : createCommaSeparated();
     for (size_t index = 0; index < m_values.size(); index++)
-        newList->append(item(index));
+        newList->append(m_values[index]);
     return newList;
 }
 
