bd480d42158e854f4e9796a7f26a0ec97aa0fbfa
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 20:35:52 +0000

    [Sync] Prevent switching back to "Sync Everything" view if passphrase needed.
    BUG=90925
    TEST=Sign into account with custom passphrase. Try to click "default view" link.
    
    
    Review URL: http://codereview.chromium.org/7474023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94971 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -245,6 +245,7 @@ cr.define('options', function() {
       // Don't allow the user to tweak the settings once we send the
       // configuration to the backend.
       this.setInputElementsDisabledState_(true);
+      this.animateDisableLink_($('use-default-link'), true, null);
 
       // These values need to be kept in sync with where they are read in
       // SyncSetupFlow::GetDataTypeChoiceData().
@@ -284,10 +285,6 @@ cr.define('options', function() {
       var self = this;
       this.animateDisableLink_($('customize-link'), disabled, function() {
         self.showCustomizePage_(null, true);
-      });
-
-      this.animateDisableLink_($('use-default-link'), disabled, function() {
-        self.showSyncEverythingPage_();
       });
     },
 
@@ -467,6 +464,9 @@ cr.define('options', function() {
      * @private
      */
     showPassphraseContainer_: function(args) {
+      // Once we require a passphrase, we prevent the user from returning to
+      // the Sync Everything pane.
+      $('use-default-link').hidden = true;
       $('sync-custom-passphrase-container').hidden = true;
       $('sync-existing-passphrase-container').hidden = false;
 
@@ -501,8 +501,16 @@ cr.define('options', function() {
       // set focus before that logic.
       $('choose-datatypes-ok').focus();
 
-      if (args && args['show_passphrase'])
+      if (args && args['show_passphrase']) {
         this.showPassphraseContainer_(args);
+      } else {
+        // We only show the "Use Default" link if we're not prompting for an
+        // existing passphrase.
+        var self = this;
+        this.animateDisableLink_($('use-default-link'), false, function() {
+          self.showSyncEverythingPage_();
+        });
+      }
     },
 
     attach_: function() {
