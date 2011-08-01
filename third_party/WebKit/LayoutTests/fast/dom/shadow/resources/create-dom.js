e25f97067db4f81a303fa50a5c29d92ca583c3d5
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-01 12:25:53 +0000

    Add support for getting an element in shadow root by its id into a window.internals object. https://bugs.webkit.org/show_bug.cgi?id=64587
    Reviewed by Hajime Morita.
    
    .:
    
    * Source/autotools/symbols.filter:
    
    Source/WebCore:
    
    Test: fast/dom/shadow/get-element-by-id-in-shadow-root.html
    
    * WebCore.exp.in:
    * testing/Internals.cpp:
    (WebCore::Internals::getElementByIdInShadowRoot):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    LayoutTests:
    
    * fast/dom/shadow/get-element-by-id-in-shadow-root-expected.txt: Added.
    * fast/dom/shadow/get-element-by-id-in-shadow-root.html: Added.
    * fast/dom/shadow/resources/create-dom.js: Added.
    (createShadow):
    (createDom):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/shadow/resources/create-dom.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// This function can take optional child elements as arguments[2:].
+function createShadow(tagName, attributes)
+{
+    var element = document.createElement(tagName);
+    for (var name in attributes)
+        element.setAttribute(name, attributes[name]);
+    var shadow = internals.ensureShadowRoot(element);
+    var childElements = Array.prototype.slice.call(arguments, 2);
+    for (var i = 0; i < childElements.length; ++i)
+        shadow.appendChild(childElements[i]);
+    return element;
+}
+
+// This function can take optional child elements as arguments[2:].
+function createDom(tagName, attributes)
+{
+    var element = document.createElement(tagName);
+    for (var name in attributes)
+        element.setAttribute(name, attributes[name]);
+    var childElements = Array.prototype.slice.call(arguments, 2);
+    for (var i = 0; i < childElements.length; ++i)
+        element.appendChild(childElements[i]);
+    return element;
+}
