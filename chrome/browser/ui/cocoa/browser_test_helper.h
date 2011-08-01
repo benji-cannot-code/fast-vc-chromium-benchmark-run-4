1243dbbf75439a9c2ae80c2b9deb10cd739f4452
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 21:39:04 +0000

    Fix build break caused by bad merge.
    TBR=sky
    
    BUG=61062
    
    Review URL: http://codereview.chromium.org/7544027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94983 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_test_helper.h
+++ chrome/browser/ui/cocoa/browser_test_helper.h
@@ -8,12 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "chrome/browser/ui/browser.h"
-<<<<<<< HEAD
 #include "chrome/test/base/testing_profile.h"
-=======
 #include "chrome/test/testing_browser_process.h"
-#include "chrome/test/testing_profile.h"
->>>>>>> wip
 #include "content/browser/browser_thread.h"
 
 // Base class which contains a valid Browser*.  Lots of boilerplate to
