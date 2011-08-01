fd1e54f8afb1d12148f1542da7471f6edd29e926
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 21:17:58 +0000

    Allow OVDA::Destroy() during Flush() & Reset()
    BUG=none
    TEST=ovdatest, gles2, trybots
    
    Review URL: http://codereview.chromium.org/7517003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94978 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/omx_video_decode_accelerator_unittest.cc
+++ content/common/gpu/media/omx_video_decode_accelerator_unittest.cc
@@ -391,8 +391,10 @@ enum ClientState {
   CS_CREATED,
   CS_DECODER_SET,
   CS_INITIALIZED,
+  CS_FLUSHING,
   CS_FLUSHED,
   CS_DONE,
+  CS_RESETTING,
   CS_RESET,
   CS_ERROR,
   CS_DESTROYED,
@@ -658,6 +660,7 @@ void EglRenderingVDAClient::NotifyFlushDone() {
   if (decoder_deleted())
     return;
   decoder_->Reset();
+  SetState(CS_RESETTING);
 }
 
 void EglRenderingVDAClient::NotifyResetDone() {
@@ -729,6 +732,7 @@ void EglRenderingVDAClient::DecodeNextNALUs() {
     return;
   if (encoded_data_next_pos_to_decode_ == encoded_data_.size()) {
     decoder_->Flush();
+    SetState(CS_FLUSHING);
     return;
   }
   size_t start_pos = encoded_data_next_pos_to_decode_;
@@ -872,8 +876,12 @@ TEST_P(OmxVideoDecodeAcceleratorTest, TestSimpleDecode) {
     // InitializeDone kicks off decoding inside the client, so we just need to
     // wait for Flush.
     ASSERT_NO_FATAL_FAILURE(
+        AssertWaitForStateOrDeleted(note, clients[i], CS_FLUSHING));
+    ASSERT_NO_FATAL_FAILURE(
         AssertWaitForStateOrDeleted(note, clients[i], CS_FLUSHED));
     // FlushDone requests Reset().
+    ASSERT_NO_FATAL_FAILURE(
+        AssertWaitForStateOrDeleted(note, clients[i], CS_RESETTING));
     ASSERT_NO_FATAL_FAILURE(
         AssertWaitForStateOrDeleted(note, clients[i], CS_RESET));
     // ResetDone requests Destroy().
@@ -933,7 +941,9 @@ INSTANTIATE_TEST_CASE_P(
     ::testing::Values(
         MakeTuple(1, 1, END_OF_STREAM_RESET, CS_DECODER_SET),
         MakeTuple(1, 1, END_OF_STREAM_RESET, CS_INITIALIZED),
+        MakeTuple(1, 1, END_OF_STREAM_RESET, CS_FLUSHING),
         MakeTuple(1, 1, END_OF_STREAM_RESET, CS_FLUSHED),
+        MakeTuple(1, 1, END_OF_STREAM_RESET, CS_RESETTING),
         MakeTuple(1, 1, END_OF_STREAM_RESET, CS_RESET),
         MakeTuple(1, 1, END_OF_STREAM_RESET, static_cast<ClientState>(-1)),
         MakeTuple(1, 1, END_OF_STREAM_RESET, static_cast<ClientState>(-10)),
