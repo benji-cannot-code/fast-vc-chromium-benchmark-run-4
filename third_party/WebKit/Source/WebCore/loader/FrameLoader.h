d31ce120cfc5f3757155f5e2af5a8b86bd40d13c
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-01 21:18:49 +0000

    Source/WebCore: Never override the policy URL on form submissions. https://bugs.webkit.org/show_bug.cgi?id=61809
    Reviewed by Adam Barth.
    
    Tests: http/tests/security/cookies/third-party-cookie-blocking-main-frame.html
           http/tests/security/cookies/third-party-cookie-blocking-user-action.html
           http/tests/security/cookies/third-party-cookie-blocking.html
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::loadURL):
    (WebCore::FrameLoader::addExtraFieldsToSubresourceRequest):
    (WebCore::FrameLoader::addExtraFieldsToMainResourceRequest):
    (WebCore::FrameLoader::addExtraFieldsToRequest):
    (WebCore::FrameLoader::loadPostRequest):
    (WebCore::FrameLoader::loadDifferentDocumentItem):
    * loader/FrameLoader.h:
    
    LayoutTests: Require explicit user action to override the policy URL on form submissions.
    https://bugs.webkit.org/show_bug.cgi?id=61809
    
    Reviewed by Adam Barth.
    
    * http/tests/loading/redirect-methods-expected.txt:
    * http/tests/security/cookies/resources/set-a-cookie.php: Added.
    * http/tests/security/cookies/third-party-cookie-blocking-expected.txt: Added.
    * http/tests/security/cookies/third-party-cookie-blocking-main-frame-expected.txt: Added.
    * http/tests/security/cookies/third-party-cookie-blocking-main-frame.html: Added.
    * http/tests/security/cookies/third-party-cookie-blocking-user-action-expected.txt: Added.
    * http/tests/security/cookies/third-party-cookie-blocking-user-action.html: Added.
    * http/tests/security/cookies/third-party-cookie-blocking.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.h
@@ -302,7 +302,7 @@ private:
     void updateFirstPartyForCookies();
     void setFirstPartyForCookies(const KURL&);
     
-    void addExtraFieldsToRequest(ResourceRequest&, FrameLoadType loadType, bool isMainResource, bool cookiePolicyURLFromRequest);
+    void addExtraFieldsToRequest(ResourceRequest&, FrameLoadType, bool isMainResource);
 
     void clearProvisionalLoad();
     void transitionToCommitted(PassRefPtr<CachedPage>);
