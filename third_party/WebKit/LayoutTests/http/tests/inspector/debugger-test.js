32b9b6af50c8a58a9d58dd50bef904a5d264f8a1
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-01 08:17:05 +0000

    Web Inspector: group scripts by folder in the scripts selector. https://bugs.webkit.org/show_bug.cgi?id=65321
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    Test: inspector/debugger/scripts-sorting.html
    
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype._sourceFileAdded.get if):
    (WebInspector.ScriptsPanel.prototype._addOptionToFilesSelect.insertOrdered.optionCompare):
    (WebInspector.ScriptsPanel.prototype._addOptionToFilesSelect.insertOrdered):
    (WebInspector.ScriptsPanel.prototype._addOptionToFilesSelect):
    (WebInspector.ScriptsPanel.prototype._folderAndDisplayNameForScriptURL):
    (WebInspector.ScriptsPanel.prototype.reset):
    (WebInspector.SourceFrameDelegateForScriptsPanel.prototype.suggestedFileName):
    
    LayoutTests:
    
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    * inspector/debugger/scripts-sorting-expected.txt: Added.
    * inspector/debugger/scripts-sorting.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/debugger-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/debugger-test.js
@@ -160,7 +160,7 @@ InspectorTest.showScriptSource = function(scriptName, callback)
 {
     var filesSelect = document.getElementById("scripts-files");
     for (var i = 0; i < filesSelect.length; ++i) {
-        if (filesSelect[i].text === scriptName) {
+        if (filesSelect[i].scriptNameForTest === scriptName) {
             filesSelect.selectedIndex = i;
             WebInspector.panels.scripts._filesSelectChanged();
             var sourceFrame = WebInspector.panels.scripts.visibleView;
