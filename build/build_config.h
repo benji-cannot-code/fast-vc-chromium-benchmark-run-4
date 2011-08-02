8e060163cbe98f98934f8b2b02e1b8eb5bb1304f
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-02 18:53:53 +0000

    Remove USE_BASE_DATA_PACK from build_config.h since it's only used in one file.  Inline the usage instead.
    Review URL: http://codereview.chromium.org/7538030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95123 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -70,8 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     defined(OS_OPENBSD) || defined(OS_SOLARIS) || defined(OS_ANDROID) ||  \
     defined(OS_NACL)
 #define OS_POSIX 1
-// Use base::DataPack for name/value pairs.
-#define USE_BASE_DATA_PACK 1
 #endif
 
 // Use tcmalloc
