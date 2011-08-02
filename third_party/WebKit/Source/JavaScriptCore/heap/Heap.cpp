494a1ab5f804b0d836cf2166e21743773f5f3ab6
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-02 19:57:06 +0000

    JSC does a GC even when the heap still has free pages https://bugs.webkit.org/show_bug.cgi?id=65445
    Reviewed by Oliver Hunt.
    
    If the high watermark is not reached, then we allocate new blocks as
    before.  If the current watermark does reach (or exceed) the high
    watermark, then we check if there is a block on the free block pool.
    If there is, we simply allocation from it.  If there isn't, we
    invoke a collectin as before.  This effectively couples the elastic
    scavenging to the collector's decision function.  That is, if an
    application rapidly varies its heap usage (sometimes using more and
    sometimes less) then the collector will not thrash as it used to.
    But if heap usage drops and stays low then the scavenger thread and
    the GC will eventually reach a kind of consensus: the GC will set
    the watermark low because of low heap usage, and the scavenger thread
    will steadily eliminate pages from the free page pool, until the size
    of the free pool is below the high watermark.
    
    On command-line, this is neutral on SunSpider and Kraken and a 3% win
    on V8.  In browser, this is a 1% win on V8 and neutral on the other
    two.
    
    * heap/Heap.cpp:
    (JSC::Heap::allocateSlowCase):
    (JSC::Heap::allocateBlock):
    * heap/Heap.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
@@ -405,8 +405,16 @@ void* Heap::allocateSlowCase(NewSpace::SizeClass& sizeClass)
     if (LIKELY(result != 0))
         return result;
 
-    if (m_newSpace.waterMark() < m_newSpace.highWaterMark() || !m_isSafeToCollect) {
-        m_newSpace.addBlock(sizeClass, allocateBlock(sizeClass.cellSize));
+    AllocationEffort allocationEffort;
+    
+    if (m_newSpace.waterMark() < m_newSpace.highWaterMark() || !m_isSafeToCollect)
+        allocationEffort = AllocationMustSucceed;
+    else
+        allocationEffort = AllocationCanFail;
+    
+    MarkedBlock* block = allocateBlock(sizeClass.cellSize, allocationEffort);
+    if (block) {
+        m_newSpace.addBlock(sizeClass, block);
         void* result = tryAllocate(sizeClass);
         ASSERT(result);
         return result;
@@ -421,7 +429,7 @@ void* Heap::allocateSlowCase(NewSpace::SizeClass& sizeClass)
     
     ASSERT(m_newSpace.waterMark() < m_newSpace.highWaterMark());
     
-    m_newSpace.addBlock(sizeClass, allocateBlock(sizeClass.cellSize));
+    m_newSpace.addBlock(sizeClass, allocateBlock(sizeClass.cellSize, AllocationMustSucceed));
     
     result = tryAllocate(sizeClass);
     ASSERT(result);
@@ -695,11 +703,14 @@ bool Heap::isValidAllocation(size_t bytes)
     return true;
 }
 
-MarkedBlock* Heap::allocateBlock(size_t cellSize)
+MarkedBlock* Heap::allocateBlock(size_t cellSize, Heap::AllocationEffort allocationEffort)
 {
     MarkedBlock* block;
     
 #if !ENABLE(LAZY_BLOCK_FREEING)
+    if (allocationEffort == AllocationCanFail)
+        return 0;
+    
     block = MarkedBlock::create(this, cellSize);
 #else
     {
@@ -713,6 +724,8 @@ MarkedBlock* Heap::allocateBlock(size_t cellSize)
     }
     if (block)
         block->initForCellSize(cellSize);
+    else if (allocationEffort == AllocationCanFail)
+        return 0;
     else
         block = MarkedBlock::create(this, cellSize);
 #endif
