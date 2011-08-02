29801e8132e4588299297d79c8989837d34271ad
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-02 08:46:48 +0000

    [Qt] tst_QWebElement::style() fails because QWebElement::CascadedStyle doesn't work as expected https://bugs.webkit.org/show_bug.cgi?id=65244
    Patch by Kristóf Kosztyó <Kosztyo.Kristof@stud.u-szeged.hu> on 2011-08-02
    Reviewed by Csaba Osztrogonác.
    
    * tests/qwebelement/tst_qwebelement.cpp: Mark failing test case as expected fail
    (tst_QWebElement::style):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/tests/qwebelement/tst_qwebelement.cpp
+++ third_party/WebKit/Source/WebKit/qt/tests/qwebelement/tst_qwebelement.cpp
@@ -476,6 +476,7 @@ void tst_QWebElement::style()
     p.setStyleProperty("cursor", "auto");
 
     QCOMPARE(p.styleProperty("color", QWebElement::InlineStyle), QLatin1String("red"));
+    QEXPECT_FAIL("", "https://bugs.webkit.org/show_bug.cgi?id=65244", Continue);
     QCOMPARE(p.styleProperty("color", QWebElement::CascadedStyle), QLatin1String("yellow"));
     QCOMPARE(p.styleProperty("cursor", QWebElement::InlineStyle), QLatin1String("auto"));
 
@@ -541,6 +542,7 @@ void tst_QWebElement::style()
     p = m_mainFrame->documentElement().findAll("p").at(0);
 
     QCOMPARE(p.styleProperty("color", QWebElement::InlineStyle), QLatin1String(""));
+    QEXPECT_FAIL("", "https://bugs.webkit.org/show_bug.cgi?id=65244", Continue);
     QCOMPARE(p.styleProperty("color", QWebElement::CascadedStyle), QLatin1String("red"));
 
     QString html6 = "<head>"
@@ -561,6 +563,7 @@ void tst_QWebElement::style()
 
     p = m_mainFrame->documentElement().findAll("p").at(0);
     QCOMPARE(p.styleProperty("color", QWebElement::InlineStyle), QLatin1String("blue"));
+    QEXPECT_FAIL("", "https://bugs.webkit.org/show_bug.cgi?id=65244", Continue);
     QCOMPARE(p.styleProperty("color", QWebElement::CascadedStyle), QLatin1String("black"));
 
     QString html7 = "<head>"
@@ -578,6 +581,7 @@ void tst_QWebElement::style()
     waitForSignal(m_page, SIGNAL(loadFinished(bool)), 200);
 
     p = m_mainFrame->documentElement().findAll("p").at(0);
+    QEXPECT_FAIL("", "https://bugs.webkit.org/show_bug.cgi?id=65244", Continue);
     QCOMPARE(p.styleProperty("color", QWebElement::CascadedStyle), QLatin1String("black"));
 
     QString html8 = "<body><p>some text</p></body>";
