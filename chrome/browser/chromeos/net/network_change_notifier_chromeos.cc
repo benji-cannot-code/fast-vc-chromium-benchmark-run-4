338ddf7804019053ade389b53cac661bc7663654
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-02 01:49:54 +0000

    Added delay for network goining online event handling in order to work around dns resolution issue in ChromeOS conenction manager.
    BUG=chromium-os:18480
    TEST=make sure webui OOBE does not show host not found page again
    Review URL: http://codereview.chromium.org/7453047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/net/network_change_notifier_chromeos.cc
+++ chrome/browser/chromeos/net/network_change_notifier_chromeos.cc
@@ -9,6 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "content/browser/browser_thread.h"
 
+namespace {
+
+// Delay for online change notification reporting.
+const int kOnlineNotificationDelayMS = 500;
+
+}
+
 namespace chromeos {
 
 NetworkChangeNotifierChromeos::NetworkChangeNotifierChromeos()
@@ -23,8 +30,11 @@ NetworkChangeNotifierChromeos::NetworkChangeNotifierChromeos()
       chromeos::CrosLibrary::Get()->GetPowerLibrary();
   power->AddObserver(this);
 
-  connection_state_ = net->active_network() ?
-          net->active_network()->connection_state() : chromeos::STATE_UNKNOWN;
+  BrowserThread::PostDelayedTask(
+      BrowserThread::UI, FROM_HERE,
+      NewRunnableFunction(
+          &NetworkChangeNotifierChromeos::UpdateInitialState, this),
+      kOnlineNotificationDelayMS);
 }
 
 NetworkChangeNotifierChromeos::~NetworkChangeNotifierChromeos() {
@@ -123,11 +133,29 @@ void NetworkChangeNotifierChromeos::UpdateConnectivityState(
   connection_state_ = new_connection_state;
   if (is_online != was_online || is_portal != was_portal ||
       is_unknown != was_unknown) {
-    BrowserThread::PostTask(
-        BrowserThread::IO, FROM_HERE,
-        NewRunnableFunction(
-           &NetworkChangeNotifierChromeos::NotifyObserversOfOnlineStateChange));
+    if (!IsCurrentlyOffline()) {
+      // Delay reporting of edge when we go online. dns resolution service
+      // does not seem to be immediately available.
+      BrowserThread::PostDelayedTask(
+          BrowserThread::IO, FROM_HERE,
+          NewRunnableFunction(
+             &NetworkChangeNotifierChromeos::NotifyObserversOfOnlineStateChange),
+          kOnlineNotificationDelayMS);
+    } else {
+      BrowserThread::PostTask(
+          BrowserThread::IO, FROM_HERE,
+          NewRunnableFunction(
+             &NetworkChangeNotifierChromeos::NotifyObserversOfOnlineStateChange));
+    }
   }
+}
+
+// static
+void NetworkChangeNotifierChromeos::UpdateInitialState(
+    NetworkChangeNotifierChromeos* self) {
+  chromeos::NetworkLibrary* net =
+      chromeos::CrosLibrary::Get()->GetNetworkLibrary();
+  self->UpdateNetworkState(net);
 }
 
 }  // namespace net
