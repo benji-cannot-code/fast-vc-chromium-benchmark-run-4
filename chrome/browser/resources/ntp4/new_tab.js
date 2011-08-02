9985ba2a82c361d0ec6d316b79b1d26b2a0c66f4
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-02 18:26:06 +0000

    ntp4: apps promo
    first cut. It's visible when the promo is on AND the webstore is the only app on the page it's on.
    
    BUG=90560
    TEST=manual with --force-apps-promo-visible
    
    Review URL: http://codereview.chromium.org/7520020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/new_tab.js
+++ chrome/browser/resources/ntp4/new_tab.js
@@ -277,6 +277,8 @@ cr.define('ntp4', function() {
       appsPages[pageIndex].appendApp(app);
     }
 
+    ntp4.AppsPage.setPromo(data.showPromo ? data : null);
+
     // Tell the slider about the pages
     updateSliderCards();
 
