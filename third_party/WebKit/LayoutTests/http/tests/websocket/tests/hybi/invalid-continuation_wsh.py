d60874d4531559050c18ccf77f7408d8d7f7fe67
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-03 04:37:51 +0000

    WebSocket: Should be closed by receiving invalid continuation frame.
    https://bugs.webkit.org/show_bug.cgi?id=65527
    
    Patch by Takashi Toyoshima <toyoshim@chromium.org> on 2011-08-02
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Test: http/tests/websocket/tests/hybi/invalid-continuation.html
    
    * websockets/WebSocketChannel.cpp:
    (WebCore::WebSocketChannel::processFrame):
    
    LayoutTests:
    
    * http/tests/websocket/tests/hybi/invalid-continuation-expected.txt: Added.
    * http/tests/websocket/tests/hybi/invalid-continuation.html: Added.
    * http/tests/websocket/tests/hybi/invalid-continuation_wsh.py: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/websocket/tests/hybi/invalid-continuation_wsh.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from mod_pywebsocket import common
+from mod_pywebsocket import stream
+
+def web_socket_do_extra_handshake(request):
+    pass
+
+
+def web_socket_transfer_data(request):
+    payload1 = 'Invalid continuation frame to be ignored.'
+    payload2 = 'Valid frame after closing should be disposed.'
+    request.connection.write(stream.create_header(common.OPCODE_CONTINUATION,
+                                                  len(payload1),
+                                                  1, 0, 0, 0, 0) + payload1)
+    request.connection.write(stream.create_header(common.OPCODE_TEXT,
+                                                  len(payload2),
+                                                  1, 0, 0, 0, 0) + payload2)
