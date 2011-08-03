c5fd67bb9a563556c6ca3834d5870afda683dccf
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-03 16:48:47 +0000

    Use appropriate macro for CSSPropertyImageRendering. https://bugs.webkit.org/show_bug.cgi?id=65594
    Reviewed by Simon Fraser.
    
    No new tests / trivial code cleanup only.
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    Use HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE macro.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92289 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -4426,9 +4426,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         return;
     }
     case CSSPropertyImageRendering:
-        if (!primitiveValue)
-            return;
-        m_style->setImageRendering(*primitiveValue);
+        HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(imageRendering, ImageRendering);
         return;
     case CSSPropertyTextShadow:
     case CSSPropertyBoxShadow:
