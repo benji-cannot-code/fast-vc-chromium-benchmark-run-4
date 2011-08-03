7ea749a3b2edda50e7ca7dfce4f26e874927c83c
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-03 18:38:26 +0000

    WebCore images backing CG patterns should be released on the main thread https://bugs.webkit.org/show_bug.cgi?id=65630
    Reviewed by Sam Weinig.
    
    * platform/graphics/cg/PatternCG.cpp:
    (WebCore::patternReleaseOnMainThreadCallback):
    Deref the image.
    
    (WebCore::patternReleaseCallback):
    Call callOnMainThread.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cg/PatternCG.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cg/PatternCG.cpp
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AffineTransform.h"
 #include "GraphicsContext.h"
-
 #include <ApplicationServices/ApplicationServices.h>
+#include <wtf/MainThread.h>
 
 namespace WebCore {
 
@@ -46,9 +46,14 @@ static void patternCallback(void* info, CGContextRef context)
     CGContextDrawImage(context, rect, platformImage);
 }
 
-static void patternReleaseCallback(void* info)
+static void patternReleaseOnMainThreadCallback(void* info)
 {
     static_cast<Image*>(info)->deref();
+}
+
+static void patternReleaseCallback(void* info)
+{
+    callOnMainThread(patternReleaseOnMainThreadCallback, info);
 }
 
 CGPatternRef Pattern::createPlatformPattern(const AffineTransform& userSpaceTransformation) const
