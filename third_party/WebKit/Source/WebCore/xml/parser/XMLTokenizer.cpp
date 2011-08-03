40fa94aa2b59fdd27def04930f4dbf77d952aabe
jpfau@apple.com <jpfau@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-03 00:04:06 +0000

    Add namespace prefix support in XML tokenizer https://bugs.webkit.org/show_bug.cgi?id=65578
    Reviewed by Adam Barth.
    
    This also changes markup debug printing to print to stderr instead of stdout
    
    * xml/parser/MarkupTokenBase.h:
    (WebCore::MarkupTokenBase::printString):
    * xml/parser/XMLToken.h:
    (WebCore::XMLToken::XMLDeclarationData::XMLDeclarationData):
    (WebCore::XMLToken::clear):
    (WebCore::XMLToken::appendToName):
    (WebCore::XMLToken::name):
    (WebCore::XMLToken::target):
    (WebCore::XMLToken::data):
    (WebCore::XMLToken::prefix):
    (WebCore::XMLToken::xmlDeclarationData):
    (WebCore::XMLToken::endPrefix):
    (WebCore::XMLToken::hasPrefix):
    (WebCore::XMLToken::endAttributePrefix):
    (WebCore::XMLToken::attributeHasPrefix):
    (WebCore::XMLToken::printAttrs):
    (WebCore::XMLToken::print):
    * xml/parser/XMLTokenizer.cpp:
    (WebCore::XMLTokenizer::nextToken):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/parser/XMLTokenizer.cpp
+++ third_party/WebKit/Source/WebCore/xml/parser/XMLTokenizer.cpp
@@ -115,7 +115,7 @@ inline bool isValidNameChar(UChar cc)
         return true;
     if (cc < 0x30)
         return false;
-    if (cc < 0x3B)
+    if (cc < 0x3A)
         return true;
     if (cc < 0x0300)
         return false;
@@ -302,6 +302,9 @@ bool XMLTokenizer::nextToken(SegmentedString& source, XMLToken& token)
         else if (isValidNameChar(cc)) {
             m_token->appendToName(cc);
             XML_ADVANCE_TO(TagNameState);
+        } else if (cc == ':' && !m_token->hasPrefix()) {
+            m_token->endPrefix();
+            XML_ADVANCE_TO(TagNameState);
         } else {
             parseError();
             return emitEndOfFile(source);
@@ -316,6 +319,9 @@ bool XMLTokenizer::nextToken(SegmentedString& source, XMLToken& token)
             return emitAndResumeIn(source, XMLTokenizerState::DataState);
         else if (isValidNameChar(cc)) {
             m_token->appendToName(cc);
+            XML_ADVANCE_TO(EndTagNameState);
+        } else if (cc == ':' && !m_token->hasPrefix()) {
+            m_token->endPrefix();
             XML_ADVANCE_TO(EndTagNameState);
         } else {
             parseError();
@@ -364,6 +370,9 @@ bool XMLTokenizer::nextToken(SegmentedString& source, XMLToken& token)
             XML_ADVANCE_TO(BeforeAttributeValueState);
         } else if (isValidNameChar(cc)) {
             m_token->appendToAttributeName(cc);
+            XML_ADVANCE_TO(AttributeNameState);
+        } else if (cc == ':' && !m_token->attributeHasPrefix()) {
+            m_token->endAttributePrefix(source.numberOfCharactersConsumed());
             XML_ADVANCE_TO(AttributeNameState);
         } else {
             parseError();
