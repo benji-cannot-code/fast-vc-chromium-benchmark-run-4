538eea7049167c7f25abf15c99480bfae20a9acb
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-03 12:20:12 +0000

    [GTK] Install web and plugin processes in libexecdir instead of bindir https://bugs.webkit.org/show_bug.cgi?id=65600
    Reviewed by Martin Robinson.
    
    .:
    
    * GNUmakefile.am: Initialize libexec_PROGRAMS, only used by
    WebKit2 in this moment.
    
    Source/WebKit2:
    
    Launch processes from libexecdir or WEBKIT_EXEC_PATH
    environment variable when it's set. This variable will be used by
    MiniBrowser and unit tests to find processes without having to
    install them.
    
    * GNUmakefile.am:
    * UIProcess/Launcher/gtk/ProcessLauncherGtk.cpp:
    (WebKit::ProcessLauncher::launchProcess):
    
    Tools:
    
    Set WEBKIT_EXEC_PATH environment variable to use web and plugin
    process from current directory instead of the intalled ones.
    
    * MiniBrowser/gtk/GNUmakefile.am:
    * MiniBrowser/gtk/main.c:
    (main):
    * WebKitTestRunner/GNUmakefile.am:
    * WebKitTestRunner/gtk/main.cpp:
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/gtk/main.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/gtk/main.cpp
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 int main(int argc, char** argv)
 {
     gtk_init(&argc, &argv);
+    // Prefer the not installed web and plugin processes.
+    g_setenv("WEBKIT_EXEC_PATH", WEBKIT_EXEC_PATH, FALSE);
     WTR::TestController controller(argc, const_cast<const char**>(argv));
     return 0;
 }
