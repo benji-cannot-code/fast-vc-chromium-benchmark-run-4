c2fb39255cb87db48b5353a0d669c8fc9d02767c
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-03 19:50:34 +0000

    Revert 95288 - Multi-Profiles: New Icon drop
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7493082
    
    TBR=sail@chromium.org
    Review URL: http://codereview.chromium.org/7562015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu.cc
+++ chrome/browser/ui/views/avatar_menu.cc
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kCellWidth = 38;
-const int kCellHeight = 31;
+const int kCellWidth = 32;
+const int kCellHeight = 32;
 const int kCellPaddingX = 5;
 const int kCellPaddingY = 5;
 const int kGridMaxCol = 3;
