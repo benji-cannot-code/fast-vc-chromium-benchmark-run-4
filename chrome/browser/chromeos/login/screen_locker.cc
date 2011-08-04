a43b333a928c799d7e427938e8d71aa4c3cf650e
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 10:06:49 +0000

    Added version info to WebUI
    R=xiyuan@chromium.org
    BUG=chromium-os:18608
    TEST=Verify that on WebUI OOBE and Login pages there's version info label.
    
    Review URL: http://codereview.chromium.org/7564028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/timer.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
 #include "chrome/browser/chromeos/cros/screen_lock_library.h"
 #include "chrome/browser/chromeos/input_method/input_method_manager.h"
