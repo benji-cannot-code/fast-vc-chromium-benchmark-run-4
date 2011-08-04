3c34c892f5b657263a4754ff1b89fa0957cce1c1
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 06:10:32 +0000

    Fix the issue that keyboard is not rendered properly after login.
    Setting className of the keyboard is deferred until window.onhashchange but early exit in onhashchange caused the class of keyboard to remain nodisplay.
    This change modify the initKeyboard to set the className.
    
    BUG=none
    TEST=Manually checked the keyboard was shown properly.
    
    
    Review URL: http://codereview.chromium.org/7531026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard/main.js
+++ chrome/browser/resources/keyboard/main.js
@@ -126,10 +126,11 @@ function initKeyboard(layout, element) {
   }
   var keyboardDiv = document.createElement('div');
   keyboardDiv.id = getKeyboardId(layout);
-  keyboardDiv.className = 'nodisplay';
+  keyboardDiv.className = 'keyboard';
   initRows(layout, keyboardDiv);
   initHandwritingCanvas(layout, keyboardDiv);
   keyboard['keyboardDiv'] = keyboardDiv;
+  window.onresize();
   element.appendChild(keyboardDiv);
 }
 
@@ -138,8 +139,7 @@ function initKeyboard(layout, element) {
  * @return {void}
  */
 window.onresize = function() {
-  var keyboardDiv = document.getElementById(getKeyboardId(
-      currentKeyboardLayout));
+  var keyboardDiv = KEYBOARDS[currentKeyboardLayout]['keyboardDiv'];
   var height = getKeyboardHeight();
   keyboardDiv.style.height = height + 'px';
   var mainDiv = document.getElementById('main');
