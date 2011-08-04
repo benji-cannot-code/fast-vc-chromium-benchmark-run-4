c96dfd7b52d81292305dd0d6cebb10e1b5ae24d5
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 23:15:17 +0000

    Make the Linux sandbox process initialize WebKit before using the WebKit api.
    TEST=Starting DumpRenderTree will assert without this after I land a change in WebKIt.
    
    Review URL: http://codereview.chromium.org/7578013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95533 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_sandbox_host_linux.cc
+++ content/browser/renderer_host/render_sandbox_host_linux.cc
@@ -32,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/unix_domain_socket_posix.h"
 #include "skia/ext/SkFontHost_fontconfig_direct.h"
 #include "third_party/npapi/bindings/npapi_extensions.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/gtk/WebFontInfo.h"
+#include "webkit/glue/webkitclient_impl.h"
 
 using WebKit::WebCString;
 using WebKit::WebFontInfo;
@@ -69,6 +71,8 @@ class SandboxIPCProcess  {
     }
   }
 
+  ~SandboxIPCProcess();
+
   void Run() {
     struct pollfd pfds[2];
     pfds[0].fd = lifeline_fd_;
@@ -102,6 +106,8 @@ class SandboxIPCProcess  {
   }
 
  private:
+  void EnsureWebKitInitialized();
+
   // ---------------------------------------------------------------------------
   // Requests from the renderer...
 
@@ -246,6 +252,7 @@ class SandboxIPCProcess  {
       return;
     }
 
+    EnsureWebKitInitialized();
     scoped_array<WebUChar> chars(new WebUChar[num_chars]);
 
     for (int i = 0; i < num_chars; ++i) {
@@ -284,6 +291,7 @@ class SandboxIPCProcess  {
       return;
     }
 
+    EnsureWebKitInitialized();
     WebKit::WebFontRenderStyle style;
     WebFontInfo::renderStyleForStrike(family.c_str(), sizeAndStyle, &style);
 
@@ -638,7 +646,20 @@ class SandboxIPCProcess  {
   const int browser_socket_;
   FontConfigDirect* const font_config_;
   std::vector<std::string> sandbox_cmd_;
+  scoped_ptr<webkit_glue::WebKitClientImpl> webkit_client_;
 };
+
+SandboxIPCProcess::~SandboxIPCProcess() {
+  if (webkit_client_.get())
+    WebKit::shutdown();
+}
+
+void SandboxIPCProcess::EnsureWebKitInitialized() {
+  if (webkit_client_.get())
+    return;
+  webkit_client_.reset(new webkit_glue::WebKitClientImpl);
+  WebKit::initialize(webkit_client_.get());
+}
 
 // -----------------------------------------------------------------------------
 
