178727ebfb2b68ad43d1db4e7196af2775949dfb
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 03:11:52 +0000

    Multi-Profiles: Remove new profile setup UI
    After some discussion we decided that we no longer want a new profile setup UI.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7563023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95379 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_factory.cc
@@ -58,8 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/sim_unlock_ui.h"
 #include "chrome/browser/ui/webui/chromeos/system_info_ui.h"
 #include "chrome/browser/ui/webui/active_downloads_ui.h"
-#else
-#include "chrome/browser/ui/webui/new_profile_ui.h"
 #endif
 
 #if defined(TOUCH_UI)
@@ -237,13 +235,6 @@ static WebUIFactoryFunction GetWebUIFactoryFunction(Profile* profile,
 
   if (url.spec() == chrome::kChromeUIConstrainedHTMLTestURL)
     return &NewWebUI<ConstrainedHtmlUI>;
-
-#if !defined(OS_CHROMEOS)
-  if (ProfileManager::IsMultipleProfilesEnabled()) {
-    if (url.host() == chrome::kChromeUINewProfileHost)
-      return &NewWebUI<NewProfileUI>;
-  }
-#endif
 
   return NULL;
 }
