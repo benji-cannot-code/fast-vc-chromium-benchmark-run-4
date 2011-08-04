135c2787d56eb423c24d5e94d7d247c825c65b63
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 03:55:11 +0000

    ntp4: record app launch source
    dispense with anchor pings (these are limiting because you have to use anchor elements) and just use chrome.send apis.
    
    Also note that ntp3 is broken: NTP_RECENTLY_CLOSED app launches will never be recorded because preventDefault prevents the ping from being sent.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7550015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/apps_page.js
+++ chrome/browser/resources/ntp4/apps_page.js
@@ -195,6 +195,7 @@ cr.define('ntp4', function() {
 
       var appSpan = this.ownerDocument.createElement('span');
       appSpan.textContent = this.appData_.name;
+      appSpan.addEventListener('click', this.onClick_.bind(this));
       appContents.appendChild(appSpan);
       this.appendChild(appContents);
 
@@ -437,6 +438,7 @@ cr.define('ntp4', function() {
   };
 
   return {
+    APP_LAUNCH: APP_LAUNCH,
     AppsPage: AppsPage,
   };
 });
