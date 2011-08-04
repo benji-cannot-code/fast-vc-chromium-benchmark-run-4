9b790f2da44f4c545529feb34ff92e8b75075dc7
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 18:13:10 +0000

    Adding UMA stats support for file open events.
    BUG=chromium-os:15175
    TEST=NONE
    Review URL: http://codereview.chromium.org/7465017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/file_manager_util.cc
+++ chrome/browser/extensions/file_manager_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_writer.h"
 #include "base/logging.h"
+#include "base/metrics/histogram.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
@@ -51,6 +52,15 @@ const char* kAVExtensions[] = {
 */
 };
 
+// List of all extensions we want to be shown in histogram that keep track of
+// files that were unsuccessfully tried to be opened.
+// The list has to be synced with histogram values.
+const char* kUMATrackingExtensions[] = {
+  "other", ".doc", ".docx", ".odt", ".rtf", ".pdf", ".ppt", ".pptx", ".odp",
+  ".xls", ".xlsx", ".ods", ".csv", ".odf", ".rar", ".asf", ".wma", ".wmv",
+  ".mov", ".mpg", ".log"
+};
+
 bool IsSupportedBrowserExtension(const char* ext) {
   for (size_t i = 0; i < arraysize(kBrowserSupportedExtensions); i++) {
     if (base::strcasecmp(ext, kBrowserSupportedExtensions[i]) == 0) {
@@ -72,6 +82,19 @@ bool IsSupportedAVExtension(const char* ext) {
 // static
 GURL FileManagerUtil::GetFileBrowserExtensionUrl() {
   return GURL(kFileBrowserExtensionUrl);
+}
+
+// Returns index |ext| has in the |array|. If there is no |ext| in |array|, last
+// element's index is return (last element should have irrelevant value).
+int UMAExtensionIndex(const char *ext,
+                      const char** array,
+                      size_t array_size) {
+  for (size_t i = 0; i < array_size; i++) {
+    if (base::strcasecmp(ext, array[i]) == 0) {
+      return i;
+    }
+  }
+  return 0;
 }
 
 // static
@@ -185,7 +208,14 @@ void FileManagerUtil::ViewItem(const FilePath& full_path, bool enqueue) {
     return;
   }
 
-  // Unknown file type. Show an error message.
+  // Unknown file type. Record UMA and show an error message.
+  size_t extension_index = UMAExtensionIndex(ext.data(),
+                                             kUMATrackingExtensions,
+                                             arraysize(kUMATrackingExtensions));
+  UMA_HISTOGRAM_ENUMERATION("FileBrowser.OpeningFileType",
+                            extension_index,
+                            arraysize(kUMATrackingExtensions) - 1);
+
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       NewRunnableFunction(
