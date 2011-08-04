9c665b786aa15ee8d94861097cf3131a3a1f00fc
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 07:52:01 +0000

    No need to have DevToolsManager reference counted
    Now that ForceReopenWindow method is gone(in r91695), the browser process should own the DevToolsManager.
    
    BUG=None
    TEST=existing DevTools tests
    
    Review URL: http://codereview.chromium.org/7461125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95396 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/debugger/devtools_manager.h
+++ content/browser/debugger/devtools_manager.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
-#include "base/memory/ref_counted.h"
 #include "content/browser/debugger/devtools_client_host.h"
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
@@ -31,12 +30,12 @@ typedef std::map<std::string, std::string> DevToolsRuntimeProperties;
 // This class is a singleton that manages DevToolsClientHost instances and
 // routes messages between developer tools clients and agents.
 class DevToolsManager : public DevToolsClientHost::CloseListener,
-                        public NotificationObserver,
-                        public base::RefCounted<DevToolsManager> {
+                        public NotificationObserver {
  public:
   static DevToolsManager* GetInstance();
 
   DevToolsManager();
+  virtual ~DevToolsManager();
 
   // Returns DevToolsClientHost registered for |inspected_rvh| or NULL if
   // there is no alive DevToolsClientHost registered for |inspected_rvh|.
@@ -82,10 +81,6 @@ class DevToolsManager : public DevToolsClientHost::CloseListener,
   void CloseAllClientHosts();
 
  private:
-  friend class base::RefCounted<DevToolsManager>;
-
-  virtual ~DevToolsManager();
-
   // DevToolsClientHost::CloseListener override.
   // This method will remove all references from the manager to the
   // DevToolsClientHost and unregister all listeners related to the
