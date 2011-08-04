4e2e785cc96f5f7ba0cbe82a16f6468c72ecf312
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-04 04:38:01 +0000

    Fix incorrect checks for HTMLMediaElement https://bugs.webkit.org/show_bug.cgi?id=65590
    Reviewed by Dimitri Glazkov.
    
    <video> and <audio> can be an HTMLElement instance instead of
    HTMLMediaElement if MediaPlayer::isAvailable() returns false or
    the media feature is disabled at runtime.
    
    Source/WebCore:
    
    * html/HTMLSourceElement.cpp:
    (WebCore::HTMLSourceElement::insertedIntoTree):
    (WebCore::HTMLSourceElement::willRemove):
    * html/HTMLTrackElement.cpp:
    (WebCore::HTMLTrackElement::insertedIntoTree):
    (WebCore::HTMLTrackElement::willRemove):
    * html/shadow/MediaControlElements.cpp:
    (WebCore::toParentMediaElement):
    * page/FrameView.cpp:
    (WebCore::FrameView::updateWidget):
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::paintMediaMuteButton):
    * platform/gtk/RenderThemeGtk.cpp:
    (WebCore::getMediaElementFromRenderObject):
    * rendering/RenderThemeMac.mm:
    (WebCore::RenderThemeMac::paintMediaSliderTrack):
    * rendering/RenderThemeWinCE.cpp:
    (WebCore::mediaElementParent):
    
    Source/WebKit/mac:
    
    * WebView/WebFullScreenController.mm:
    (-[WebFullScreenController _isAnyMoviePlaying]):
    
    Source/WebKit/win:
    
    * WebView.cpp:
    (WebView::enterFullscreenForNode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLTrackElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLTrackElement.cpp
@@ -58,7 +58,8 @@ PassRefPtr<HTMLTrackElement> HTMLTrackElement::create(const QualifiedName& tagNa
 void HTMLTrackElement::insertedIntoTree(bool deep)
 {
     HTMLElement::insertedIntoTree(deep);
-    if (parentNode() && (parentNode()->hasTagName(audioTag) || parentNode()->hasTagName(videoTag))) {
+    Element* parent = parentElement();
+    if (parent && parent->isMediaElement()) {
         // TODO(annacc):
         // static_cast<HTMLMediaElement*>(parentNode())->trackWasAdded(this);
     }
@@ -66,7 +67,8 @@ void HTMLTrackElement::insertedIntoTree(bool deep)
 
 void HTMLTrackElement::willRemove()
 {
-    if (parentNode() && (parentNode()->hasTagName(audioTag) || parentNode()->hasTagName(videoTag))) {
+    Element* parent = parentElement();
+    if (parent && parent->isMediaElement()) {
         // TODO(annacc):
         // static_cast<HTMLMediaElement*>(parentNode())->trackWillBeRemoved(this);
     }
