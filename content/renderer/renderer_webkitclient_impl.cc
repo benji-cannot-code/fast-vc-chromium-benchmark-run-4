89974b59d1022ddd37d4ffd1b62a39f012a8e0a1
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 17:30:43 +0000

    [Mac] Remove old version of loadFont()
    Now that the API has changed in WebKit, we can safely remove the old version of loadFont() on the Chromium side.
    
    BUG=72727
    TEST=Chrome should compile.
    TBR=mark
    
    
    Review URL: http://codereview.chromium.org/7566038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95437 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitclient_impl.cc
+++ content/renderer/renderer_webkitclient_impl.cc
@@ -116,8 +116,6 @@ class RendererWebKitClientImpl::SandboxSupport
 #if defined(OS_WIN)
   virtual bool ensureFontLoaded(HFONT);
 #elif defined(OS_MACOSX)
-  // TODO(jeremy): Remove once WebKit side of patch lands - crbug.com/72727 .
-  virtual bool loadFont(NSFont* srcFont, ATSFontContainerRef* out);
   virtual bool loadFont(
       NSFont* srcFont, ATSFontContainerRef* container, uint32* fontID);
 #elif defined(OS_POSIX)
@@ -440,13 +438,6 @@ bool RendererWebKitClientImpl::SandboxSupport::ensureFontLoaded(HFONT font) {
 }
 
 #elif defined(OS_MACOSX)
-
-// TODO(jeremy): Remove once WebKit side of patch lands - crbug.com/72727 .
-bool RendererWebKitClientImpl::SandboxSupport::loadFont(
-    NSFont* srcFont, ATSFontContainerRef* out) {
-  uint32 temp;
-  return loadFont(srcFont, out, &temp);
-}
 
 bool RendererWebKitClientImpl::SandboxSupport::loadFont(
     NSFont* srcFont, ATSFontContainerRef* container, uint32* fontID) {
