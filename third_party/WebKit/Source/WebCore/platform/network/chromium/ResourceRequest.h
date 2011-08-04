10a0bc23070f2afeaa644fe3d593ebe41b79950d
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-04 21:50:03 +0000

    Introduce a new ResourceRequest::TargetType for XHRs https://bugs.webkit.org/show_bug.cgi?id=65544
    Reviewed by Darin Fisher.
    
    Source/WebCore:
    
    * platform/network/chromium/ResourceRequest.h:
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::createRequest):
    
    Source/WebKit/chromium:
    
    * public/WebURLRequest.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceRequest.h
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceRequest.h
@@ -56,6 +56,7 @@ namespace WebCore {
             TargetIsPrefetch,
             TargetIsPrerender,
             TargetIsFavicon,
+            TargetIsXHR,
         };
 
         class ExtraData : public RefCounted<ExtraData> {
