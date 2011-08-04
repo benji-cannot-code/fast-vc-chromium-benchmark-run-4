7227cb27ef159396551536b69aca8aa52d4a84c9
abeera@google.com <abeera@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 22:24:32 +0000

    Installer for Windows cloud print driver changed to use PPD, PPd file added
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7465016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95520 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/win/install/setup.cc
+++ cloud_print/virtual_driver/win/install/setup.cc
@@ -21,12 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-HRESULT GetGpdPath(FilePath* path) {
+HRESULT GetPpdPath(FilePath* path) {
   if (!PathService::Get(base::DIR_EXE, path)) {
     LOG(ERROR) << "Unable to get install path.";
     return ERROR_PATH_NOT_FOUND;
   }
-  *path = path->Append(L"gcp.gpd");
+  *path = path->Append(L"GCP-driver.ppd");
   return S_OK;
 }
 
@@ -144,7 +144,7 @@ DWORDLONG GetVersionNumber() {
   return retval;
 }
 
-HRESULT InstallGpd() {
+HRESULT InstallPpd() {
   DRIVER_INFO_6 driver_info = {0};
   HRESULT result = S_OK;
 
@@ -153,9 +153,9 @@ HRESULT InstallGpd() {
   FilePath driver_dir;
   cloud_print::GetPrinterDriverDir(&driver_dir);
   FilePath xps_path = driver_dir.Append(L"mxdwdrv.dll");
-  FilePath ui_path = driver_dir.Append(L"unidrvui.dll");
+  FilePath ui_path = driver_dir.Append(L"ps5ui.dll");
   FilePath ui_help_path = driver_dir.Append(L"unidrv.hlp");
-  result = GetGpdPath(&source_path);
+  result = GetPpdPath(&source_path);
   if (!SUCCEEDED(result)) {
     return result;
   }
@@ -193,7 +193,7 @@ HRESULT InstallGpd() {
   return S_OK;
 }
 
-HRESULT UninstallGpd() {
+HRESULT UninstallPpd() {
   int tries = 10;
   string16 driver_name = cloud_print::LoadLocalString(IDS_DRIVER_NAME);
   while (!DeletePrinterDriverEx(NULL,
@@ -271,9 +271,9 @@ HRESULT InstallVirtualDriver(void) {
     LOG(ERROR) << "Unable to register port monitor.";
     return result;
   }
-  result = InstallGpd();
+  result = InstallPpd();
   if (!SUCCEEDED(result)) {
-    LOG(ERROR) << "Unable to install gpd.";
+    LOG(ERROR) << "Unable to install Ppd.";
     return result;
   }
   result = InstallPrinter();
@@ -288,12 +288,12 @@ HRESULT UninstallVirtualDriver(void) {
   HRESULT result = S_OK;
   result = UninstallPrinter();
   if (!SUCCEEDED(result)) {
-    LOG(ERROR) << "Unable to uninstall gpd.";
+    LOG(ERROR) << "Unable to uninstall Ppd.";
     return result;
   }
-  result = UninstallGpd();
+  result = UninstallPpd();
   if (!SUCCEEDED(result)) {
-    LOG(ERROR) << "Unable to remove gpd.";
+    LOG(ERROR) << "Unable to remove Ppd.";
     return result;
   }
   result = RegisterPortMonitor(false);
