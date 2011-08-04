581d87d98efc442f044823036e33e318c733583d
abeera@google.com <abeera@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 22:15:55 +0000

    Fix to bug 91320. Function prototype in Cloud Print Virtual Driver changed to void. Gyp file updated.
    BUG=91320
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7542016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95516 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/posix/virtual_driver_posix.cc
+++ cloud_print/virtual_driver/posix/virtual_driver_posix.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cloud_print/virtual_driver/posix/printer_driver_util_posix.h"
 
-int WriteToTemp(FILE* input_pdf, FilePath output_path) {
+void WriteToTemp(FILE* input_pdf, FilePath output_path) {
   FILE* output_pdf;
   char buffer[128];
   output_pdf = fopen(output_path.value().c_str(), "w");
@@ -79,8 +79,6 @@ int main(int argc, const char* argv[]) {
 
   // AtExitManager is necessary to use the path service.
   base::AtExitManager aemanager;
-  // File handler for the temporary PDF we pass onto Chrome.
-  FILE* output_pdf;
   // CommandLine is only setup to enable logging, never used subseqeuently.
   CommandLine::Init(argc, argv);
   // Location for the log file.
