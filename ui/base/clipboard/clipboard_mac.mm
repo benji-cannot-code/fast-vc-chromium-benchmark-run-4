b258dabeb5808bfffa8e1c04610e1ff1ebda598f
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 22:04:08 +0000

    Properly scope ScopedPlatformPaint so that the native drawing layer and Skia are in agreement on the state of the pixels. Also fix Windows to not return completely transparent bitmaps when the source image has color depth < 32 bpp.
    BUG=86085
    TEST=Copy an image from MS Paint and paste it into Gmail using 32bpp for the display color depth.
    
    Review URL: http://codereview.chromium.org/7471008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95668 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/clipboard_mac.mm
+++ ui/base/clipboard/clipboard_mac.mm
@@ -248,12 +248,15 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
 
   scoped_nsobject<NSImage> image(
       [[NSImage alloc] initWithPasteboard:GetPasteboard()]);
-  if (image.get()) {
-    [image setFlipped:YES];
-    int width = [image size].width;
-    int height = [image size].height;
+  if (!image.get())
+    return SkBitmap();
 
-    gfx::CanvasSkia canvas(width, height, false);
+  [image setFlipped:YES];
+  int width = [image size].width;
+  int height = [image size].height;
+
+  gfx::CanvasSkia canvas(width, height, false);
+  {
     skia::ScopedPlatformPaint scoped_platform_paint(&canvas);
     CGContextRef gc = scoped_platform_paint.GetPlatformSurface();
     NSGraphicsContext* cocoa_gc =
@@ -264,9 +267,8 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
             operation:NSCompositeCopy
              fraction:1.0];
     [NSGraphicsContext restoreGraphicsState];
-    return canvas.ExtractBitmap();
   }
-  return SkBitmap();
+  return canvas.ExtractBitmap();
 }
 
 void Clipboard::ReadBookmark(string16* title, std::string* url) const {
