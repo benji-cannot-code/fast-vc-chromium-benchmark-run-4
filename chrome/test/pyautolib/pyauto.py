74f0f8f9f34dbe49cf288f109e19c78e0fc0bf69
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 02:03:59 +0000

    Use --skip-oauth-login when firing chrome using pyauto on chromeos
    This is so that webui based login can be automated.
    
    BUG=chromium-os:18740
    TEST=None
    
    R=zelidrag@chromium.org
    
    Review URL: http://codereview.chromium.org/7575032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/pyauto.py
+++ chrome/test/pyautolib/pyauto.py
@@ -220,6 +220,7 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
         '--allow-file-access-from-files',
         '--enable-file-cookies',
         '--dom-automation',
+        '--skip-oauth-login',
       ]
     else:
       return []
