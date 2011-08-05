8847e4b8c59b0a68342d5d427927d69977e68850
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 21:29:29 +0000

    Options: Disable dragging when the list is disabled.
    BUG=91734
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/7585019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options_startup_page_list.js
+++ chrome/browser/resources/options/browser_options_startup_page_list.js
@@ -84,6 +84,7 @@ cr.define('options.browser_options', function() {
       urlField.addEventListener('blur', function(event) {
         self.parentNode.autocompleteList.detach();
       });
+
       this.draggable = true;
     },
 
@@ -172,6 +173,12 @@ cr.define('options.browser_options', function() {
      * @private
      */
     handleDragStart_: function(e) {
+      // Prevent dragging if the list is disabled.
+      if (this.disabled) {
+        e.preventDefault();
+        return false;
+      }
+
       var target = e.target;
       // StartupPageListItem should be the only draggable element type in the
       // page but let's make sure.
