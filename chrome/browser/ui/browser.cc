05e0e7a8bcb4a75962a7e7b39697b604a13b01bf
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 04:03:39 +0000

    [Sync] [Mac] Show sync menu item in app menu even in incognito mode
    BUG=91682
    TEST=Open Chrome, open incognito window, check app menu to make sure there is an item for sync
    
    
    Review URL: http://codereview.chromium.org/7511024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -3772,7 +3772,7 @@ void Browser::OnStateChanged() {
 #endif
 
   command_updater_.UpdateCommandEnabled(IDC_SYNC_BOOKMARKS,
-      show_main_ui && profile_->IsSyncAccessible());
+      show_main_ui && profile_->GetOriginalProfile()->IsSyncAccessible());
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -4040,7 +4040,7 @@ void Browser::UpdateCommandsForFullscreenMode(bool is_fullscreen) {
       browser_defaults::bookmarks_enabled && show_main_ui);
   command_updater_.UpdateCommandEnabled(IDC_IMPORT_SETTINGS, show_main_ui);
   command_updater_.UpdateCommandEnabled(IDC_SYNC_BOOKMARKS,
-      show_main_ui && profile_->IsSyncAccessible());
+      show_main_ui && profile_->GetOriginalProfile()->IsSyncAccessible());
 
   command_updater_.UpdateCommandEnabled(IDC_OPTIONS, show_main_ui);
   command_updater_.UpdateCommandEnabled(IDC_EDIT_SEARCH_ENGINES, show_main_ui);
