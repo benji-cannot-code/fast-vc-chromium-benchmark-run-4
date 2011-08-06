7cd164ee9e4f2e2a1c716f7e8f8e6d88ed7647ea
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-06 00:58:55 +0000

    Revert 95698 - Implement touch selection for RWHVV.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7570001
    
    TBR=varunjain@chromium.org
    Review URL: http://codereview.chromium.org/7584021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_views.h
+++ chrome/browser/renderer_host/render_widget_host_view_views.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/native/native_view_host.h"
 #include "views/events/event.h"
 #include "views/ime/text_input_client.h"
-#include "views/touchui/touch_selection_controller.h"
 #include "views/view.h"
 #include "webkit/glue/webcursor.h"
 
@@ -37,7 +36,7 @@ struct NativeWebKeyboardEvent;
 // See comments in render_widget_host_view.h about this class and its members.
 // -----------------------------------------------------------------------------
 class RenderWidgetHostViewViews : public RenderWidgetHostView,
-                                  public views::TouchSelectionClientView,
+                                  public views::View,
                                   public views::TextInputClient {
  public:
   // Internal class name.
@@ -81,9 +80,7 @@ class RenderWidgetHostViewViews : public RenderWidgetHostView,
   virtual void Destroy() OVERRIDE;
   virtual void SetTooltipText(const std::wstring& tooltip_text) OVERRIDE;
   virtual void SelectionChanged(const std::string& text,
-                                const ui::Range& range,
-                                const gfx::Point& start,
-                                const gfx::Point& end) OVERRIDE;
+                                const ui::Range& range) OVERRIDE;
   virtual void ShowingContextMenu(bool showing) OVERRIDE;
   virtual BackingStore* AllocBackingStore(const gfx::Size& size) OVERRIDE;
   virtual void SetBackground(const SkBitmap& background) OVERRIDE;
@@ -101,18 +98,6 @@ class RenderWidgetHostViewViews : public RenderWidgetHostView,
   virtual void ShowCompositorHostWindow(bool show) OVERRIDE;
 #endif
   virtual gfx::PluginWindowHandle GetCompositingSurface() OVERRIDE;
-
-  // Overridden from views::TouchSelectionClientView.
-  virtual void SelectRect(const gfx::Point& start,
-                          const gfx::Point& end) OVERRIDE;
-
-  // Overridden from ui::SimpleMenuModel::Delegate.
-  virtual bool IsCommandIdChecked(int command_id) const OVERRIDE;
-  virtual bool IsCommandIdEnabled(int command_id) const OVERRIDE;
-  virtual bool GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) OVERRIDE;
-  virtual void ExecuteCommand(int command_id) OVERRIDE;
 
   // Overridden from views::View.
   virtual std::string GetClassName() const OVERRIDE;
@@ -255,8 +240,6 @@ class RenderWidgetHostViewViews : public RenderWidgetHostView,
   bool has_composition_text_;
 
   string16 tooltip_text_;
-
-  scoped_ptr<views::TouchSelectionController> touch_selection_controller_;
 
 #if defined(TOUCH_UI)
   std::map<uint64, scoped_refptr<AcceleratedSurfaceContainerTouch> >
