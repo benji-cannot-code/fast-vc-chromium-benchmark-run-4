137198ee9281aa0a3f99c93a89eed6a1d685e1fc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-06 01:47:07 +0000

    Unreviewed, rolling out r92330. http://trac.webkit.org/changeset/92330 https://bugs.webkit.org/show_bug.cgi?id=65804
    caused various regressions in paste (Requested by rniwa on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-05
    
    Source/WebCore:
    
    * editing/ReplaceSelectionCommand.cpp:
    (WebCore::isInlineNodeWithStyle):
    (WebCore::ReplaceSelectionCommand::doApply):
    * editing/markup.cpp:
    (WebCore::ancestorToRetainStructureAndAppearance):
    * editing/markup.h:
    
    LayoutTests:
    
    * editing/pasteboard/5065605-expected.txt:
    * editing/pasteboard/copy-paste-text-in-h1-expected.txt: Removed.
    * editing/pasteboard/copy-paste-text-in-h1.html: Removed.
    * editing/pasteboard/display-block-on-spans-expected.txt:
    * editing/pasteboard/paste-pre-001-expected.txt:
    * editing/pasteboard/paste-pre-002-expected.txt:
    * editing/pasteboard/paste-text-011-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/markup.cpp
+++ third_party/WebKit/Source/WebCore/editing/markup.cpp
@@ -351,7 +351,7 @@ Node* StyledMarkupAccumulator::serializeNodes(Node* startNode, Node* pastEnd)
     return lastClosed;
 }
 
-HTMLElement* ancestorToRetainStructureAndAppearance(Node* commonAncestor, ShouldIncludeParagraphSeparators shouldIncludeParagraphSeparators)
+static Node* ancestorToRetainStructureAndAppearance(Node* commonAncestor)
 {
     Node* commonAncestorBlock = enclosingBlock(commonAncestor);
 
@@ -363,7 +363,7 @@ HTMLElement* ancestorToRetainStructureAndAppearance(Node* commonAncestor, Should
         while (table && !table->hasTagName(tableTag))
             table = table->parentNode();
 
-        return toHTMLElement(table);
+        return table;
     }
 
     if (commonAncestorBlock->hasTagName(listingTag)
@@ -377,11 +377,7 @@ HTMLElement* ancestorToRetainStructureAndAppearance(Node* commonAncestor, Should
         || commonAncestorBlock->hasTagName(h3Tag)
         || commonAncestorBlock->hasTagName(h4Tag)
         || commonAncestorBlock->hasTagName(h5Tag))
-        return toHTMLElement(commonAncestorBlock);
-
-    if (shouldIncludeParagraphSeparators == IncludeParagraphSeparators
-        && (commonAncestorBlock->hasTagName(pTag) || commonAncestorBlock->hasTagName(divTag)))
-        return toHTMLElement(commonAncestorBlock);
+        return commonAncestorBlock;
 
     return 0;
 }
