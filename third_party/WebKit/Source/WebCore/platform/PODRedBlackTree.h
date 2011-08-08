b411eb7a050e452b2cb220309f56c772ce7f361e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-08 20:13:48 +0000

    Unreviewed, rolling out r92610. http://trac.webkit.org/changeset/92610 https://bugs.webkit.org/show_bug.cgi?id=65868
    Caused assertion failures when running fast/multicol tests
    (Requested by andersca on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-08
    
    PerformanceTests:
    
    * Layout/floats.html:
    
    Source/WebCore:
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/PODIntervalTree.h:
    (WebCore::PODIntervalTree::allOverlaps):
    (WebCore::PODIntervalTree::searchForOverlapsFrom):
    * platform/PODRedBlackTree.h:
    (WebCore::PODRedBlackTree::add):
    (WebCore::PODRedBlackTree::remove):
    (WebCore::PODRedBlackTree::contains):
    (WebCore::PODRedBlackTree::visitInorder):
    (WebCore::PODRedBlackTree::size):
    (WebCore::PODRedBlackTree::checkInvariants):
    (WebCore::PODRedBlackTree::dump):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::styleDidChange):
    (WebCore::RenderBlock::addOverflowFromFloats):
    (WebCore::RenderBlock::repaintOverhangingFloats):
    (WebCore::RenderBlock::paintFloats):
    (WebCore::RenderBlock::selectionGaps):
    (WebCore::RenderBlock::insertFloatingObject):
    (WebCore::RenderBlock::removeFloatingObject):
    (WebCore::RenderBlock::removeFloatingObjectsBelow):
    (WebCore::RenderBlock::positionNewFloats):
    (WebCore::RenderBlock::logicalLeftOffsetForLine):
    (WebCore::RenderBlock::logicalRightOffsetForLine):
    (WebCore::RenderBlock::nextFloatLogicalBottomBelow):
    (WebCore::RenderBlock::lowestFloatLogicalBottom):
    (WebCore::RenderBlock::addPositionedFloats):
    (WebCore::RenderBlock::clearFloats):
    (WebCore::RenderBlock::addOverhangingFloats):
    (WebCore::RenderBlock::hasOverhangingFloat):
    (WebCore::RenderBlock::addIntrudingFloats):
    (WebCore::RenderBlock::markSiblingsWithFloatsForLayout):
    (WebCore::RenderBlock::hitTestFloats):
    (WebCore::RenderBlock::adjustForBorderFit):
    (WebCore::RenderBlock::FloatingObjects::clear):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::FloatingObject::FloatingObject):
    (WebCore::RenderBlock::FloatingObject::setX):
    (WebCore::RenderBlock::FloatingObject::setY):
    (WebCore::RenderBlock::FloatingObject::setWidth):
    (WebCore::RenderBlock::FloatingObject::setHeight):
    (WebCore::RenderBlock::FloatingObject::setFrameRect):
    (WebCore::RenderBlock::FloatingObjects::FloatingObjects):
    (WebCore::RenderBlock::FloatingObjects::set):
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::layoutRunsAndFloatsInRange):
    (WebCore::RenderBlock::linkToEndLineIfNeeded):
    (WebCore::RenderBlock::matchedEndLine):
    (WebCore::RenderBlock::positionNewFloatOnLine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PODRedBlackTree.h
+++ third_party/WebKit/Source/WebCore/platform/PODRedBlackTree.h
@@ -91,10 +91,6 @@ template<class T>
 struct ValueToString;
 #endif
 
-enum UninitializedTreeEnum {
-    UninitializedTree
-};
-
 template<class T>
 class PODRedBlackTree {
 public:
@@ -105,19 +101,6 @@ public:
     protected:
         virtual ~Visitor() { }
     };
-
-    // Constructs a new red-black tree without allocating an arena.
-    // isInitialized will return false in this case. initIfNeeded can be used
-    // to init the structure. This constructor is usefull for creating
-    // lazy initialized tree.
-    PODRedBlackTree(UninitializedTreeEnum)
-        : m_root(0)
-        , m_needsFullOrderingComparisons(false)
-#ifndef NDEBUG
-        , m_verboseDebugging(false)
-#endif
-    {
-    }
 
     // Constructs a new red-black tree, allocating temporary objects
     // from a newly constructed PODArena.
@@ -145,28 +128,8 @@ public:
 
     virtual ~PODRedBlackTree() { }
 
-    // Clearing will delete the contents of the tree. After this call
-    // isInitialized will return false.
-    void clear()
-    {
-        m_arena = 0;
-        m_root = 0;
-    }
-    
-    bool isInitialized() const
-    {
-        return m_arena;
-    }
-    
-    void initIfNeeded()
-    {
-        if (!m_arena)
-            m_arena = PODArena::create();
-    }
-
     void add(const T& data)
     {
-        ASSERT(isInitialized());
         Node* node = m_arena->allocateObject<Node, T>(data);
         insertNode(node);
     }
@@ -174,7 +137,6 @@ public:
     // Returns true if the datum was found in the tree.
     bool remove(const T& data)
     {
-        ASSERT(isInitialized());
         Node* node = treeSearch(data);
         if (node) {
             deleteNode(node);
@@ -183,15 +145,10 @@ public:
         return false;
     }
 
-    bool contains(const T& data) const
-    {
-        ASSERT(isInitialized());
-        return treeSearch(data);
-    }
+    bool contains(const T& data) const { return treeSearch(data); }
 
     void visitInorder(Visitor* visitor) const
     {
-        ASSERT(isInitialized());
         if (!m_root)
             return;
         visitInorderImpl(m_root, visitor);
@@ -199,7 +156,6 @@ public:
 
     int size() const
     {
-        ASSERT(isInitialized());
         Counter counter;
         visitInorder(&counter);
         return counter.count();
@@ -213,7 +169,6 @@ public:
 
     virtual bool checkInvariants() const
     {
-        ASSERT(isInitialized());
         int blackCount;
         return checkInvariantsFromNode(m_root, &blackCount);
     }
@@ -223,8 +178,7 @@ public:
     // debugging purposes.
     void dump() const
     {
-        if (m_arena)
-            dumpFromNode(m_root, 0);
+        dumpFromNode(m_root, 0);
     }
 
     // Turns on or off verbose debugging of the tree, causing many
