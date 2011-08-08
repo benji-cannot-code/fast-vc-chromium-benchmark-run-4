61c4230a6606535d826b355b5d5b0e54f71899ea
dslomov@google.com <dslomov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-08 19:11:33 +0000

    Source/WebCore: https://bugs.webkit.org/show_bug.cgi?id=65778 [WebWorkers][chromium] Make statics thread-safe and make sure V8 API accesses correct isolates.
    Reviewed by David Levin.
    
    Covered by existing tests.
    
    * bindings/v8/V8Binding.h:
    (WebCore::V8BindingPerIsolateData::lazyEventListenerToStringTemplate):
    (WebCore::V8BindingPerIsolateData::hiddenPropertyName):
    (WebCore::V8BindingPerIsolateData::globalHandleMap):
    (WebCore::AllowAllocation::AllowAllocation): Moving to V8Binding.h from V8Utilities.h to resolve header dependency.
    (WebCore::AllowAllocation::~AllowAllocation):
    (WebCore::AllowAllocation::current):
    (WebCore::SafeAllocation::newInstance):
    * bindings/v8/V8GCController.cpp:
    (WebCore::currentGlobalHandleMap):
    (WebCore::enumerateGlobalHandles):
    (WebCore::V8GCController::registerGlobalHandle):
    (WebCore::V8GCController::unregisterGlobalHandle):
    * bindings/v8/V8HiddenPropertyName.cpp:
    (WebCore::V8HiddenPropertyName::createString):
    * bindings/v8/V8HiddenPropertyName.h:
    * bindings/v8/V8LazyEventListener.cpp:
    (WebCore::V8LazyEventListener::prepareListenerObject):
    * bindings/v8/V8NPObject.cpp:
    * bindings/v8/V8Proxy.cpp:
    (WebCore::V8Proxy::checkNewLegal):
    * bindings/v8/V8Utilities.h:
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::WorkerContextExecutionProxy):
    (WebCore::WorkerContextExecutionProxy::initIsolate):
    * bindings/v8/WorkerContextExecutionProxy.h:
    * bindings/v8/WorkerScriptController.cpp:
    (WebCore::WorkerScriptController::scheduleExecutionTermination):
    
    Source/WebKit/chromium: https://bugs.webkit.org/show_bug.cgi?id=65778
    [WebWorkers][chromium] Make statics thread-safe and make sure V8 API accesses correct isolates
    
    Reviewed by David Levin.
    
    * src/BoundObject.cpp: AllowAllocation moved from V8Utilities.h to V8Binding.h
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/WorkerContextExecutionProxy.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/WorkerContextExecutionProxy.cpp
@@ -84,7 +84,7 @@ WorkerContextExecutionProxy::WorkerContextExecutionProxy(WorkerContext* workerCo
     : m_workerContext(workerContext)
     , m_recursion(0)
 {
-    initV8();
+    initIsolate();
 }
 
 WorkerContextExecutionProxy::~WorkerContextExecutionProxy()
@@ -109,11 +109,14 @@ void WorkerContextExecutionProxy::dispose()
     }
 }
 
-void WorkerContextExecutionProxy::initV8()
+void WorkerContextExecutionProxy::initIsolate()
 {
     // Tell V8 not to call the default OOM handler, binding code will handle it.
     v8::V8::IgnoreOutOfMemoryException();
     v8::V8::SetFatalErrorHandler(reportFatalErrorInV8);
+
+    v8::V8::SetGlobalGCPrologueCallback(&V8GCController::gcPrologue);
+    v8::V8::SetGlobalGCEpilogueCallback(&V8GCController::gcEpilogue);
 
     v8::ResourceConstraints resource_constraints;
     uint32_t here;
