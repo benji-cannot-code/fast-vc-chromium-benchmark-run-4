bd2de9c638d2c67a24bbf88c49cb88c294c581db
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-08 18:03:40 +0000

    views omnibox popup: Setup the parent Widget, instead of the parent NativeView.
    In views-desktop world, the omnibox popup should be parented to the widget the
    location-bar belongs to. This fixes pointer events on the popup in touchui.
    
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/7584024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95838 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_view_win.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_win.h
@@ -70,7 +70,7 @@ class OmniboxViewWin
                  Profile* profile,
                  CommandUpdater* command_updater,
                  bool popup_window_mode,
-                 const views::View* location_bar);
+                 views::View* location_bar);
   ~OmniboxViewWin();
 
   views::View* parent_view() const;
