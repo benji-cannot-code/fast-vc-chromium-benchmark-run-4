129e0d3756918e7b2778104964dd444c6d152888
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-08 07:51:53 +0000

    DevTools: don't show the unresponsive dialog for inspected tabs
    BUG=66458
    TEST=DevToolsManagerTest.NoUnresponsiveDialogInInspectedTab
    
    Review URL: http://codereview.chromium.org/7565008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/testing_browser_process.cc
+++ chrome/test/testing_browser_process.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/prerender_tracker.h"
 #include "chrome/browser/printing/background_printing_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "content/browser/debugger/devtools_manager.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -21,7 +22,8 @@ TestingBrowserProcess::TestingBrowserProcess()
     : module_ref_count_(0),
       app_locale_("en"),
       local_state_(NULL),
-      io_thread_(NULL) {
+      io_thread_(NULL),
+      devtools_manager_(NULL) {
 }
 
 TestingBrowserProcess::~TestingBrowserProcess() {
@@ -97,7 +99,7 @@ ThumbnailGenerator* TestingBrowserProcess::GetThumbnailGenerator() {
 }
 
 DevToolsManager* TestingBrowserProcess::devtools_manager() {
-  return NULL;
+  return devtools_manager_.get();
 }
 
 SidebarManager* TestingBrowserProcess::sidebar_manager() {
@@ -261,6 +263,10 @@ void TestingBrowserProcess::SetGoogleURLTracker(
 
 void TestingBrowserProcess::SetIOThread(IOThread* io_thread) {
   io_thread_ = io_thread;
+}
+
+void TestingBrowserProcess::SetDevToolsManager(DevToolsManager* manager) {
+  devtools_manager_.reset(manager);
 }
 
 ScopedTestingBrowserProcess::ScopedTestingBrowserProcess() {
