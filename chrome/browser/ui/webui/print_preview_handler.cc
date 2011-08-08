1af5b20cc9ca9150d488dce65e92f39c9b7952bb
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-08 23:25:10 +0000

    PrintPreview: Include <cups/ppd.h> in print_preview_handler.h
    This headerfile is required on newer systems.
    
    BUG=none
    TEST=print preview works after including the header file.
    
    
    Review URL: http://codereview.chromium.org/7558008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview_handler.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_CUPS)
 #include <cups/cups.h>
+#include <cups/ppd.h>
 
 #include "base/file_util.h"
 #endif
