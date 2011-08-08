79cf34f9bba82d1187fc61642f8664b60cf9df8d
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-08 15:51:57 +0000

    Speculative fix for crash. Crashes seem to indicate ExtensionsTabModule is asking for the id of a deleted TabContents. My guess is ExtensionWebNavigationTabObserver is adding an entry for a TabContents that is later deleted and ExtensionWebNavigationTabObserver doesn't clean up it's map so that if another TabContents ends up with the same pointer value we get a crash.
    BUG=91385
    TEST=none
    R=jochen@chromium.org
    
    Review URL: http://codereview.chromium.org/7562003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webnavigation_api.cc
+++ chrome/browser/extensions/extension_webnavigation_api.cc
@@ -345,6 +345,9 @@ void ExtensionWebNavigationEventRouter::Init() {
     registrar_.Add(this,
                    content::NOTIFICATION_TAB_ADDED,
                    NotificationService::AllSources());
+    registrar_.Add(this,
+                   content::NOTIFICATION_TAB_CONTENTS_DESTROYED,
+                   NotificationService::AllSources());
   }
 }
 
@@ -359,6 +362,10 @@ void ExtensionWebNavigationEventRouter::Observe(
 
     case content::NOTIFICATION_TAB_ADDED:
       TabAdded(Details<TabContents>(details).ptr());
+      break;
+
+    case content::NOTIFICATION_TAB_CONTENTS_DESTROYED:
+      TabDestroyed(Source<TabContents>(source).ptr());
       break;
 
     default:
@@ -420,6 +427,17 @@ void ExtensionWebNavigationEventRouter::TabAdded(TabContents* tab_contents) {
   pending_tab_contents_.erase(iter);
 }
 
+void ExtensionWebNavigationEventRouter::TabDestroyed(
+    TabContents* tab_contents) {
+  pending_tab_contents_.erase(tab_contents);
+  for (std::map<TabContents*, PendingTabContents>::iterator i =
+           pending_tab_contents_.begin(); i != pending_tab_contents_.end(); ) {
+    if (i->second.source_tab_contents == tab_contents)
+      pending_tab_contents_.erase(i++);
+    else
+      ++i;
+  }
+}
 
 // ExtensionWebNavigationTabObserver ------------------------------------------
 
