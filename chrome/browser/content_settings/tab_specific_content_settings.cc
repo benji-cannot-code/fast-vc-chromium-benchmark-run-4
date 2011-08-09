01e1bdce27c288a7c402c6ca984f2410bfb78e5e
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-09 06:48:30 +0000

    Adding usage entry to chrome://settings/cookies.
    BUG=88644,91816,91836
    TEST='BrowsingDataQuotaHelperTest.*'
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=95607
    Reverted: http://src.chromium.org/viewvc/chrome?view=rev&revision=95779
    
    Review URL: http://codereview.chromium.org/7387007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95959 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/tab_specific_content_settings.cc
+++ chrome/browser/content_settings/tab_specific_content_settings.cc
@@ -527,5 +527,6 @@ TabSpecificContentSettings::LocalSharedObjectsContainer::GetCookiesTreeModel() {
                               appcaches_->Clone(),
                               indexed_dbs_->Clone(),
                               file_systems_->Clone(),
+                              NULL,
                               true);
 }
