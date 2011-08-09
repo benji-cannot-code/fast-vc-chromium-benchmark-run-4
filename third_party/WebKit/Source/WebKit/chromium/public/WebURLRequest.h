91f9c9c44f5eb3d70041e7932ee09e7d7b483847
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-09 13:32:29 +0000

    [chromium] remove temporary #define from WebURLRequest.h https://bugs.webkit.org/show_bug.cgi?id=65813
    Reviewed by Darin Fisher.
    
    * public/WebURLRequest.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebURLRequest.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebURLRequest.h
@@ -39,9 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore { class ResourceRequest; }
 #endif
 
-// FIXME: remove #define once the Chromium side has updated.
-#define WEBKIT_HAS_TARGET_IS_XHR
-
 namespace WebKit {
 
 class WebCString;
