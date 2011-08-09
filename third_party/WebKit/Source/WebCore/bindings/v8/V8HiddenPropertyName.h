279a98aaee68375413f898b55c731e3a780bb7b6
dslomov@google.com <dslomov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-09 19:01:05 +0000

    Source/WebCore: https://bugs.webkit.org/show_bug.cgi?id=65778 [WebWorkers][chromium] Make statics thread-safe and make sure V8 API accesses correct isolates.
    Reviewed by Dmitry Titov.
    
    Covered by existing tests.
    
    * bindings/v8/V8Binding.cpp:
    (WebCore::V8BindingPerIsolateData::V8BindingPerIsolateData):
    * bindings/v8/V8Binding.h:
    (WebCore::V8BindingPerIsolateData::lazyEventListenerToStringTemplate):
    (WebCore::V8BindingPerIsolateData::hiddenPropertyName):
    (WebCore::V8BindingPerIsolateData::globalHandleMap):
    (WebCore::AllowAllocation::AllowAllocation):Moving to V8Binding.h from V8Utilities.h to resolve header dependency.
    (WebCore::AllowAllocation::~AllowAllocation):
    (WebCore::AllowAllocation::current):
    (WebCore::SafeAllocation::newInstance):
    * bindings/v8/V8GCController.cpp:
    (WebCore::currentGlobalHandleMap):
    (WebCore::enumerateGlobalHandles):
    (WebCore::V8GCController::registerGlobalHandle):
    (WebCore::V8GCController::unregisterGlobalHandle):
    * bindings/v8/V8HiddenPropertyName.cpp:
    (WebCore::V8HiddenPropertyName::createString):
    * bindings/v8/V8HiddenPropertyName.h:
    (WebCore::V8HiddenPropertyName::V8HiddenPropertyName):
    * bindings/v8/V8LazyEventListener.cpp:
    (WebCore::V8LazyEventListener::prepareListenerObject):
    * bindings/v8/V8NPObject.cpp:
    * bindings/v8/V8Proxy.cpp:
    (WebCore::V8Proxy::checkNewLegal):
    * bindings/v8/V8Utilities.h:
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::WorkerContextExecutionProxy):
    (WebCore::WorkerContextExecutionProxy::initIsolate):
    * bindings/v8/WorkerContextExecutionProxy.h:
    * bindings/v8/WorkerScriptController.cpp:
    (WebCore::WorkerScriptController::scheduleExecutionTermination):
    
    Source/WebKit/chromium: https://bugs.webkit.org/show_bug.cgi?id=65778
    [WebWorkers][chromium] Make statics thread-safe and make sure V8 API accesses correct isolates
    
    Reviewed by Dmitry Titov.
    
    * src/BoundObject.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
@@ -49,6 +49,7 @@ namespace WebCore {
 
     class V8HiddenPropertyName {
     public:
+        V8HiddenPropertyName() { }
 #define V8_DECLARE_PROPERTY(name) static v8::Handle<v8::String> name();
         V8_HIDDEN_PROPERTIES(V8_DECLARE_PROPERTY);
 #undef V8_DECLARE_PROPERTY
@@ -56,7 +57,10 @@ namespace WebCore {
         static v8::Handle<v8::String> hiddenReferenceName(const char* name);
 
     private:
-        static v8::Persistent<v8::String>* createString(const char* key);
+        static v8::Persistent<v8::String> createString(const char* key);
+#define V8_DECLARE_FIELD(name) v8::Persistent<v8::String> m_##name;
+        V8_HIDDEN_PROPERTIES(V8_DECLARE_FIELD);
+#undef V8_DECLARE_FIELD
     };
 
 }
