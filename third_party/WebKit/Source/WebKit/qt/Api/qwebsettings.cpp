c8a4e97626842cdd9b60eed2d026c80075eda2be
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-09 15:01:45 +0000

    Unreviewed, rolling out r92683. http://trac.webkit.org/changeset/92683 https://bugs.webkit.org/show_bug.cgi?id=65921
     "Breaks Qt --minimal build. Revert pending update of the Qt
    minimal configuration" (Requested by simathur on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-09
    
    .:
    
    * Source/WebKit.pri:
    
    Source/WebKit/qt:
    
    * Api/qwebsettings.cpp:
    (QWebSettings::enablePersistentStorage):
    
    Tools:
    
    * QtTestBrowser/cookiejar.cpp:
    (TestBrowserCookieJar::TestBrowserCookieJar):
    * QtTestBrowser/launcherwindow.cpp:
    (LauncherWindow::createChrome):
    (LauncherWindow::setDiskCache):
    * QtTestBrowser/main.cpp:
    (LauncherApplication::handleUserOptions):
    * QtTestBrowser/webpage.cpp:
    (WebPage::openUrlInDefaultBrowser):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/Api/qwebsettings.cpp
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebsettings.cpp
@@ -1137,6 +1137,7 @@ QString QWebSettings::localStoragePath() const
 */
 void QWebSettings::enablePersistentStorage(const QString& path)
 {
+#ifndef QT_NO_DESKTOPSERVICES
     QString storagePath;
 
     if (path.isEmpty()) {
@@ -1168,6 +1169,7 @@ void QWebSettings::enablePersistentStorage(const QString& path)
         WebCore::PluginDatabase::setPersistentMetadataCacheEnabled(true);
         WebCore::PluginDatabase::setPersistentMetadataCachePath(cachePath);
     }
+#endif
 #endif
 }
 
--- third_party/WebKit/Tools/QtTestBrowser/launcherwindow.cpp
+++ third_party/WebKit/Tools/QtTestBrowser/launcherwindow.cpp
@@ -46,8 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QNetworkReply>
 #endif
 
+#if !defined(QT_NO_NETWORKDISKCACHE) && !defined(QT_NO_DESKTOPSERVICES)
 #include <QtGui/QDesktopServices>
 #include <QtNetwork/QNetworkDiskCache>
+#endif
 
 const int gExitClickArea = 80;
 QVector<int> LauncherWindow::m_zoomLevels;
@@ -434,9 +436,11 @@ void LauncherWindow::createChrome()
 
     QMenu* settingsMenu = menuBar()->addMenu("&Settings");
 
+#if !defined(QT_NO_NETWORKDISKCACHE) && !defined(QT_NO_DESKTOPSERVICES)
     QAction* toggleDiskCache = settingsMenu->addAction("Use Disk Cache", this, SLOT(setDiskCache(bool)));
     toggleDiskCache->setCheckable(true);
     toggleDiskCache->setChecked(m_windowOptions.useDiskCache);
+#endif
 
     QAction* toggleAutoLoadImages = settingsMenu->addAction("Disable Auto Load Images", this, SLOT(toggleAutoLoadImages(bool)));
     toggleAutoLoadImages->setCheckable(true);
@@ -819,6 +823,7 @@ void LauncherWindow::selectElements()
 
 void LauncherWindow::setDiskCache(bool enable)
 {
+#if !defined(QT_NO_NETWORKDISKCACHE) && !defined(QT_NO_DESKTOPSERVICES)
     m_windowOptions.useDiskCache = enable;
     QNetworkDiskCache* cache = 0;
     if (enable) {
@@ -827,6 +832,7 @@ void LauncherWindow::setDiskCache(bool enable)
         cache->setCacheDirectory(cacheLocation);
     }
     page()->networkAccessManager()->setCache(cache);
+#endif
 }
 
 void LauncherWindow::setTouchMocking(bool on)
