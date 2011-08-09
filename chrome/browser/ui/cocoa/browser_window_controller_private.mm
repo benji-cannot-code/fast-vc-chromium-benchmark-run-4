a29599aa0dcfe31a560d781dbefe66df417ece49
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-09 20:39:40 +0000

    Grab bag of bugfixes for Lion fullscreen mode.
    - Removes a DCHECK() in the InstantLoader that fired incorrectly in presentation
      mode.  In presentation mode, the omnibox is entirely enclosed by the web
      contents, so one part of the DCHECK() was incorrect.
    
    - Mark popups and panels as auxiliary fullscreen windows.  This allows them to
      share a space with a fullscreen window, but not to become a fullscreen window.
    
    - Force a relayout in windowDidFailToExitFullScreen.  Despite the name of the
      delegate method, when a window fails to exit fullscreen mode, it actually does
      exit fullscreen mode.  Forcing a relayout gets the window to draw correctly in
      this "failure" case.
    
    - Fixes a DCHECK() when pressing escape in fullscreen mode.
    
    - Pressing escape when the omnibox is focused (in non-presentation fullscreen
      mode) will now drop the window out of fullscreen mode.
    
    BUG=74065
    TEST=None
    
    Review URL: http://codereview.chromium.org/7599029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96056 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -853,6 +853,9 @@ willPositionSheet:(NSWindow*)sheet
 
 - (void)windowDidFailToExitFullScreen:(NSWindow*)window {
   [self deregisterForContentViewResizeNotifications];
+
+  // Force a relayout to try and get the window back into a reasonable state.
+  [self layoutSubviews];
 }
 
 - (void)enableBarVisibilityUpdates {
