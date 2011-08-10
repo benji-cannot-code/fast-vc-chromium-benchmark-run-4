0e0a38d12ec4c08221672ac73a78f52103b60346
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-10 02:14:19 +0000

    Fix to make the log more meaningful when a user script file of an extension cannot be located
    LoadScriptContent outputs meaningful log when |path| is empty
    
    BUG=None
    TEST=compiles successfully
    
    Review URL: http://codereview.chromium.org/7528021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_accessibility_helper.cc
+++ chrome/browser/chromeos/login/wizard_accessibility_helper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/accessibility_util.h"
 #include "chrome/browser/extensions/extension_accessibility_api.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -111,13 +112,7 @@ void WizardAccessibilityHelper::ToggleAccessibility() {
 
 void WizardAccessibilityHelper::SetAccessibilityEnabled(bool enabled) {
   bool doSpeak = (IsAccessibilityEnabled() != enabled);
-  if (g_browser_process) {
-    PrefService* prefService = g_browser_process->local_state();
-    prefService->SetBoolean(prefs::kAccessibilityEnabled, enabled);
-    prefService->ScheduleSavePersistentPrefs();
-  }
-  ExtensionAccessibilityEventRouter::GetInstance()->
-      SetAccessibilityEnabled(enabled);
+  accessibility::EnableAccessibility(enabled, NULL);
   if (doSpeak) {
     accessibility_handler_->Speak(enabled ?
         l10n_util::GetStringUTF8(IDS_CHROMEOS_ACC_ACCESS_ENABLED).c_str() :
