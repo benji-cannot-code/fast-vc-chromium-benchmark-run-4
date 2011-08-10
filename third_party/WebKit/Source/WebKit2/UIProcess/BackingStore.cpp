ca35f830f62055f526fb8c7922a1e9ce7f750284
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-10 18:30:08 +0000

    Accidentally commented out an assertion in BackingStore::incorporateUpdate https://bugs.webkit.org/show_bug.cgi?id=65989
    Reviewed by Anders Carlsson.
    
    I couldn't get this assertion to fire in quick testing, let's bring it back.
    
    * UIProcess/BackingStore.cpp: (WebKit::BackingStore::incorporateUpdate):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92778 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/BackingStore.cpp
@@ -53,7 +53,7 @@ BackingStore::~BackingStore()
 
 void BackingStore::incorporateUpdate(const UpdateInfo& updateInfo)
 {
-//    ASSERT(m_size == updateInfo.viewSize);
+    ASSERT(m_size == updateInfo.viewSize);
     
     RefPtr<ShareableBitmap> bitmap = ShareableBitmap::create(updateInfo.bitmapHandle);
     if (!bitmap)
