dc64f91146183886c38b3d8ce08cb5af1dd57fde
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-10 13:41:30 +0000

    Web Inspector: there should be a way to tell what properties are non-enumerable when expanding objects. https://bugs.webkit.org/show_bug.cgi?id=65518
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * inspector/InjectedScript.cpp:
    (WebCore::InjectedScript::getProperties):
    * inspector/InjectedScript.h:
    * inspector/InjectedScriptSource.js:
    (.):
    * inspector/Inspector.json:
    * inspector/InspectorRuntimeAgent.cpp:
    (WebCore::InspectorRuntimeAgent::getProperties):
    * inspector/InspectorRuntimeAgent.h:
    * inspector/front-end/ObjectPropertiesSection.js:
    (WebInspector.ObjectPropertyTreeElement.prototype.ondblclick):
    (WebInspector.ObjectPropertyTreeElement.prototype.update):
    * inspector/front-end/RemoteObject.js:
    (WebInspector.RemoteObject.prototype.getOwnProperties):
    (WebInspector.RemoteObject.prototype.getAllProperties):
    (WebInspector.RemoteObjectProperty):
    * inspector/front-end/inspector.css:
    (.section .properties .dimmed):
    
    LayoutTests:
    
    * inspector/debugger/debugger-expand-scope-expected.txt:
    * inspector/protocol/runtime-agent-expected.txt:
    * inspector/runtime/runtime-getProperties-expected.txt:
    * inspector/runtime/runtime-getProperties.html:
    * platform/chromium/inspector/runtime/runtime-getProperties-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
@@ -117,15 +117,15 @@ WebInspector.RemoteObject.prototype = {
 
     getOwnProperties: function(callback)
     {
-        this._getProperties(false, callback);
+        this._getProperties(true, callback);
     },
 
     getAllProperties: function(callback)
     {
-        this._getProperties(true, callback);
+        this._getProperties(false, callback);
     },
 
-    _getProperties: function(ignoreHasOwnProperty, callback)
+    _getProperties: function(ownProperties, callback)
     {
         if (!this._objectId) {
             callback([]);
@@ -137,11 +137,20 @@ WebInspector.RemoteObject.prototype = {
                 callback(null);
                 return;
             }
-            for (var i = 0; properties && i < properties.length; ++i)
-                properties[i].value = WebInspector.RemoteObject.fromPayload(properties[i].value);
-            callback(properties);
+            var result = [];
+            for (var i = 0; properties && i < properties.length; ++i) {
+                var property = properties[i];
+                if (property.get || property.set) {
+                    if (property.get)
+                        result.push(new WebInspector.RemoteObjectProperty("get " + property.name, WebInspector.RemoteObject.fromPayload(property.get), property));
+                    if (property.set)
+                        result.push(new WebInspector.RemoteObjectProperty("set " + property.name, WebInspector.RemoteObject.fromPayload(property.set), property));
+                } else
+                    result.push(new WebInspector.RemoteObjectProperty(property.name, WebInspector.RemoteObject.fromPayload(property.value), property));
+            }
+            callback(result);
         }
-        RuntimeAgent.getProperties(this._objectId, !!ignoreHasOwnProperty, remoteObjectBinder);
+        RuntimeAgent.getProperties(this._objectId, ownProperties, remoteObjectBinder);
     },
 
     setPropertyValue: function(name, value, callback)
@@ -214,10 +223,14 @@ WebInspector.RemoteObject.prototype = {
     }
 }
 
-WebInspector.RemoteObjectProperty = function(name, value)
+WebInspector.RemoteObjectProperty = function(name, value, descriptor)
 {
     this.name = name;
     this.value = value;
+    this.enumerable = descriptor ? !!descriptor.enumerable : true;
+    this.writable = descriptor ? !!descriptor.writable : true;
+    if (descriptor && descriptor.wasThrown)
+        this.wasThrown = true; 
 }
 
 WebInspector.RemoteObjectProperty.fromPrimitiveValue = function(name, value)
