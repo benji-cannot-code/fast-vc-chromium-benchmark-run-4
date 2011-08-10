bc6b19f925952dbb4bcf4eb4c204f8bae2a99a9a
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-10 00:51:04 +0000

    Renamed authorized_ to connected_ for p2p sockets host.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7604014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96105 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_host_udp.cc
+++ content/browser/renderer_host/p2p/socket_host_udp.cc
@@ -98,11 +98,11 @@ void P2PSocketHostUdp::DidCompleteRead(int result) {
   if (result > 0) {
     std::vector<char> data(recv_buffer_->data(), recv_buffer_->data() + result);
 
-    if (authorized_peers_.find(recv_address_) == authorized_peers_.end()) {
+    if (connected_peers_.find(recv_address_) == connected_peers_.end()) {
       P2PSocketHost::StunMessageType type;
       bool stun = GetStunPacketType(&*data.begin(), data.size(), &type);
       if (stun && IsRequestOrResponse(type)) {
-        authorized_peers_.insert(recv_address_);
+        connected_peers_.insert(recv_address_);
       } else if (!stun || type == STUN_DATA_INDICATION) {
         LOG(ERROR) << "Received unexpected data packet from "
                    << recv_address_.ToString()
@@ -133,7 +133,7 @@ void P2PSocketHostUdp::Send(const net::IPEndPoint& to,
     return;
   }
 
-  if (authorized_peers_.find(to) == authorized_peers_.end()) {
+  if (connected_peers_.find(to) == connected_peers_.end()) {
     P2PSocketHost::StunMessageType type;
     bool stun = GetStunPacketType(&*data.begin(), data.size(), &type);
     if (!stun || type == STUN_DATA_INDICATION) {
