5ba7cec01dd68eac7be7a8ebdcd5a81bb3c6b9ef
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-10 07:35:13 +0000

    [Chromium] Name an enum used in some template arguments. https://bugs.webkit.org/show_bug.cgi?id=65953
    Patch by Peter Kasting <pkasting@google.com> on 2011-08-10
    Reviewed by Darin Fisher.
    
    * public/WebHTTPBody.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebHTTPBody.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebHTTPBody.h
@@ -49,7 +49,7 @@ class WebHTTPBodyPrivate;
 class WebHTTPBody {
 public:
     struct Element {
-        enum { TypeData, TypeFile, TypeBlob } type;
+        enum Type { TypeData, TypeFile, TypeBlob } type;
         WebData data;
         WebString filePath;
         long long fileStart;
