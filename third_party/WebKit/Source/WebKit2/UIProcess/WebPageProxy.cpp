a46f56e1fe220afd73d074ed7623a4acb5e931a8
sullivan@apple.com <sullivan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-10 00:09:08 +0000

    WKPageCopyPendingAPIRequestURL returns stale result if policy decision set to Ignore https://bugs.webkit.org/show_bug.cgi?id=65950 <rdar://problem/9925931>
    Reviewed by Anders Carlsson.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::receivedPolicyDecision):
    Call clearPendingAPIRequestURL() if the policy decision is PolicyIgnore.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -1022,6 +1022,9 @@ void WebPageProxy::receivedPolicyDecision(PolicyAction action, WebFrameProxy* fr
 {
     if (!isValid())
         return;
+    
+    if (action == PolicyIgnore)
+        clearPendingAPIRequestURL();
 
     uint64_t downloadID = 0;
     if (action == PolicyDownload) {
