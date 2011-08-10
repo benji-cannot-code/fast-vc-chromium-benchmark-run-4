57b10379fffb14f61ecdbb70f7a7962c112bfbde
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-10 12:26:10 +0000

    Remove ibus-hangul support.
    BUG=None
    TEST=try
    
    Review URL: http://codereview.chromium.org/7608019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -126,7 +126,6 @@ extern const char kLanguageChewingSelKeys[];
 extern const char kLanguageChewingHsuSelKeyType[];
 extern const char kLanguageHangulKeyboard[];
 extern const char kLanguageHangulHanjaBindingKeys[];
-extern const char kLanguageHangulHanjaKeys[];
 extern const char kLanguagePinyinCorrectPinyin[];
 extern const char kLanguagePinyinFuzzyPinyin[];
 extern const char kLanguagePinyinLookupTablePageSize[];
