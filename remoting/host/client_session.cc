0a5a1418c57c74247309d56a1df46d2a7ed80144
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-10 03:30:19 +0000

    Make floor-control more robust.
    Check all injected mouse positions, not just the first one, to deal with
    possibility that mouse-injection API might fail.
    
    Also rename recent_remote_mouse_positions_ to injected_mouse_positions_ to
    emphasize these are the events that were (attempted to be) injected into the
    host.
    
    BUG=None
    TEST=Use buggy Mac client to send invalid mouse position to Linux host (move
    mouse off-screen.
    
    
    Review URL: http://codereview.chromium.org/7604007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96124 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/client_session.cc
+++ remoting/host/client_session.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/client_session.h"
 
+#include <algorithm>
+
 #include "base/task.h"
 #include "remoting/host/user_authenticator.h"
 #include "remoting/proto/auth.pb.h"
@@ -95,9 +97,10 @@ void ClientSession::InjectMouseEvent(const protocol::MouseEvent* event,
     }
     if (event->has_x() && event->has_y()) {
       gfx::Point pos(event->x(), event->y());
-      recent_remote_mouse_positions_.push_back(pos);
-      if (recent_remote_mouse_positions_.size() > kNumRemoteMousePositions) {
-        recent_remote_mouse_positions_.pop_front();
+      injected_mouse_positions_.push_back(pos);
+      if (injected_mouse_positions_.size() > kNumRemoteMousePositions) {
+        VLOG(1) << "Injected mouse positions queue full.";
+        injected_mouse_positions_.pop_front();
       }
     }
     input_stub_->InjectMouseEvent(event, done_runner.Release());
@@ -114,9 +117,20 @@ void ClientSession::LocalMouseMoved(const gfx::Point& mouse_pos) {
   // If this is a genuine local input event (rather than an echo of a remote
   // input event that we've just injected), then ignore remote inputs for a
   // short time.
-  if (!recent_remote_mouse_positions_.empty() &&
-      mouse_pos == *recent_remote_mouse_positions_.begin()) {
-    recent_remote_mouse_positions_.pop_front();
+  std::list<gfx::Point>::iterator found_position =
+      std::find(injected_mouse_positions_.begin(),
+                injected_mouse_positions_.end(), mouse_pos);
+  if (found_position != injected_mouse_positions_.end()) {
+    // Remove it from the list, and any positions that were added before it,
+    // if any.  This is because the local input monitor is assumed to receive
+    // injected mouse position events in the order in which they were injected
+    // (if at all).  If the position is found somewhere other than the front of
+    // the queue, this would be because the earlier positions weren't
+    // successfully injected (or the local input monitor might have skipped over
+    // some positions), and not because the events were out-of-sequence.  These
+    // spurious positions should therefore be discarded.
+    injected_mouse_positions_.erase(injected_mouse_positions_.begin(),
+                                    ++found_position);
   } else {
     latest_local_input_time_ = base::Time::Now();
   }
