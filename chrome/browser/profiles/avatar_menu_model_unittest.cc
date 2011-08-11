3b36fdf37d993845bc74886d6bd6c6e26af4f045
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 02:51:35 +0000

    Multi-Profiles: Change avatar menu to bubble view
    BUG=
    TEST=Ran on views and verified that things looked ok.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=96306
    
    Review URL: http://codereview.chromium.org/7566050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96311 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/avatar_menu_model_unittest.cc
+++ chrome/browser/profiles/avatar_menu_model_unittest.cc
@@ -58,7 +58,7 @@ class MockObserver : public AvatarMenuModelObserver {
   MockObserver() : count_(0) {}
   virtual ~MockObserver() {}
 
-  virtual void OnAvatarMenuModelChanged() {
+  virtual void OnAvatarMenuModelChanged(AvatarMenuModel* avatar_menu_model) {
     ++count_;
   }
 
