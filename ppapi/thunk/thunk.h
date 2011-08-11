3c561e994aa997158bc780d6515ac0e7b68c8272
nfullagar@google.com <nfullagar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 20:42:04 +0000

    Add Graphics3DTrusted dev interface for proxy. BUG=none TEST=none Review URL: http://codereview.chromium.org/7587017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96445 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/thunk/thunk.h
+++ ppapi/thunk/thunk.h
@@ -31,6 +31,7 @@ struct PPB_Fullscreen_Dev;
 struct PPB_GLESChromiumTextureMapping_Dev;
 struct PPB_Graphics2D;
 struct PPB_Graphics3D_Dev;
+struct PPB_Graphics3DTrusted_Dev;
 struct PPB_ImageData;
 struct PPB_ImageDataTrusted;
 struct PPB_InputEvent;
@@ -86,6 +87,7 @@ const PPB_GLESChromiumTextureMapping_Dev*
     GetPPB_GLESChromiumTextureMapping_Thunk();
 const PPB_Graphics2D* GetPPB_Graphics2D_Thunk();
 const PPB_Graphics3D_Dev* GetPPB_Graphics3D_Thunk();
+const PPB_Graphics3DTrusted_Dev* GetPPB_Graphics3DTrusted_Thunk();
 const PPB_InputEvent* GetPPB_InputEvent_Thunk();
 const PPB_ImageData* GetPPB_ImageData_Thunk();
 const PPB_ImageDataTrusted* GetPPB_ImageDataTrusted_Thunk();
