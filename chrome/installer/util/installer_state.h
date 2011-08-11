a970346f2ba6467c78b267437617e753f8aa0c59
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 12:37:27 +0000

    Fix DCHECK in GCF quick-enable.
    Make the determination of whether or not Chrome Frame is in use on-demand rather than during initialization.
    
    BUG=92146
    TEST=GCF quick-enable should work again.
    
    
    Review URL: http://codereview.chromium.org/7600021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96370 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/installer_state.h
+++ chrome/installer/util/installer_state.h
@@ -106,11 +106,6 @@ class InstallerState {
   // TODO(grt): Eradicate the bool in favor of the enum.
   bool is_multi_install() const;
 
-  // Returns whether or not there is currently a Chrome Frame instance running.
-  // Note that there isn't a mechanism to lock Chrome Frame in place, so Chrome
-  // Frame may either exit or start up after this is called.
-  bool is_chrome_frame_running() const { return is_chrome_frame_running_; }
-
   // The full path to the place where the operand resides.
   const FilePath& target_path() const { return target_path_; }
 
@@ -149,6 +144,11 @@ class InstallerState {
   // Returns the currently installed version in |target_path|, or NULL if no
   // products are installed.  Ownership is passed to the caller.
   Version* GetCurrentVersion(const InstallationState& machine_state) const;
+
+  // Returns whether or not there is currently a Chrome Frame instance running.
+  // Note that there isn't a mechanism to lock Chrome Frame in place, so Chrome
+  // Frame may either exit or start up after this is called.
+  bool IsChromeFrameRunning(const InstallationState& machine_state) const;
 
   // Returns the path to the installer under Chrome version folder
   // (for example <target_path>\Google\Chrome\Application\<Version>\Installer)
@@ -198,7 +198,6 @@ class InstallerState {
       const InstallationState& machine_state);
   bool IsMultiInstallUpdate(const MasterPreferences& prefs,
                             const InstallationState& machine_state);
-  bool DetectChromeFrameInUse(const InstallationState& machine_state);
 
   // Sets this object's level and updates the root_key_ accordingly.
   void set_level(Level level);
@@ -220,7 +219,6 @@ class InstallerState {
 #endif
   bool msi_;
   bool verbose_logging_;
-  bool is_chrome_frame_running_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(InstallerState);
