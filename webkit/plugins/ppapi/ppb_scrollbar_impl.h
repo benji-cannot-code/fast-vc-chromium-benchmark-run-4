8832928e6c6a6e515834ec766355c9fcb66bfa37
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 01:00:14 +0000

    Fix crash with print preview and the new scrollbar code that triggered an assert in WebKit side. The problem is that WebScrollbar is now expected to have a shorter lifetime than WebPluginContainer. So convert the NewRunnableMethod to use ScopedRunnableMethodFactory to ensure that constraint is not broken. Review URL: http://codereview.chromium.org/7640001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96494 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppb_scrollbar_impl.h
+++ webkit/plugins/ppapi/ppb_scrollbar_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/task.h"
 #include "ppapi/thunk/ppb_scrollbar_api.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebRect.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebScrollbarClient.h"
@@ -76,6 +77,9 @@ class PPB_Scrollbar_Impl : public PPB_Widget_Impl,
   gfx::Rect dirty_;
   std::vector<WebKit::WebRect> tickmarks_;
   scoped_ptr<WebKit::WebScrollbar> scrollbar_;
+
+  // Used so that the post task for Invalidate doesn't keep an extra reference.
+  ScopedRunnableMethodFactory<PPB_Scrollbar_Impl> method_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PPB_Scrollbar_Impl);
 };
