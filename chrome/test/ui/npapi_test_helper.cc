8025b412a2ec3158bd013a89f4e67f35769e1ab1
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 21:33:59 +0000

    Cleanup: Remove old webkit/glue/plugins/plugin_list.h
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/7639009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96625 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/npapi_test_helper.cc
+++ chrome/test/ui/npapi_test_helper.cc
@@ -1,11 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2008, Google Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_file_util.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
-#include "webkit/glue/plugins/plugin_list.h"
+#include "webkit/plugins/npapi/plugin_list.h"
 
 namespace npapi_test {
 const char kTestCompleteCookie[] = "status";
