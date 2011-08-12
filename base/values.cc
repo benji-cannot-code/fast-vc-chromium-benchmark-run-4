cccb21dcb06826b9210fe9620c5a511e6ef8b40f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 20:59:02 +0000

    base: Rename ValueType to something less redundant as _just_ Type.
    So instead of:
    
    base::Value::ValueType
    
    We write a nice:
    
    base::Value::Type
    
    BUG=None
    TEST=None
    
    R=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7634018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96614 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.cc
+++ base/values.cc
@@ -146,7 +146,7 @@ bool Value::Equals(const Value* a, const Value* b) {
   return a->Equals(b);
 }
 
-Value::Value(ValueType type) : type_(type) {
+Value::Value(Type type) : type_(type) {
 }
 
 ///////////////////// FundamentalValue ////////////////////
