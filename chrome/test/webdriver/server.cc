8b3f6e1afa98ea7e09cf333103a8494e5b174b58
alnayeem@google.com <alnayeem@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 00:49:38 +0000

    In chromedriver, integrate SET_ONLINE and APPCACHE_STATUS atoms. Other commands for BrowserConnection and ApplicationCache are not implemented. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/7612017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96488 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/server.cc
+++ chrome/test/webdriver/server.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/webdriver/commands/alert_commands.h"
+#include "chrome/test/webdriver/commands/appcache_status_command.h"
+#include "chrome/test/webdriver/commands/browser_connection_commands.h"
 #include "chrome/test/webdriver/commands/cookie_commands.h"
 #include "chrome/test/webdriver/commands/create_session.h"
 #include "chrome/test/webdriver/commands/execute_async_script_command.h"
@@ -136,6 +138,9 @@ void InitCallbacks(struct mg_context* ctx, Dispatcher* dispatcher,
   // Cookie functions.
   dispatcher->Add<CookieCommand>(     "/session/*/cookie");
   dispatcher->Add<NamedCookieCommand>("/session/*/cookie/*");
+
+  dispatcher->Add<BrowserConnectionCommand>("/session/*/browser_connection");
+  dispatcher->Add<AppCacheStatusCommand>("/session/*/application_cache/status");
 
   // Since the /session/* is a wild card that would match the above URIs, this
   // line MUST be after all other webdriver command callbacks.
