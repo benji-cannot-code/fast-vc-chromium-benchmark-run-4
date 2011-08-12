9e19cd2b2397e6c4e466784351e48c9915867bee
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 22:34:26 +0000

    Implemented nicer battery status
    BUG=chromium-os:17469
    TEST=Click battery icon, confirm it meets spec
    
    Review URL: http://codereview.chromium.org/7491083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96643 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/power_menu_button_browsertest.cc
+++ chrome/browser/chromeos/status/power_menu_button_browsertest.cc
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "grit/theme_resources.h"
 
+namespace {
+const int kNumBatteryStates = 20;
+}
 namespace chromeos {
 using ::testing::AnyNumber;
 using ::testing::InvokeWithoutArgs;
@@ -42,10 +45,10 @@ class PowerMenuButtonTest : public CrosInProcessBrowserTest {
     return power;
   }
 
-  int CallPowerChangedAndGetIconId() {
+  int CallPowerChangedAndGetBatteryIndex() {
     PowerMenuButton* power = GetPowerMenuButton();
     power->PowerChanged(mock_power_library_);
-    return power->icon_id();
+    return power->battery_index();
   }
 };
 
@@ -68,7 +71,7 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryMissingTest) {
   EXPECT_CALL(*mock_power_library_, battery_time_to_full())
       .WillOnce((Return(base::TimeDelta::FromMinutes(24))))
       .RetiresOnSaturation();
-  EXPECT_EQ(IDR_STATUSBAR_BATTERY_MISSING, CallPowerChangedAndGetIconId());
+  EXPECT_EQ(-1, CallPowerChangedAndGetBatteryIndex());
 }
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargedTest) {
@@ -90,7 +93,7 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargedTest) {
   EXPECT_CALL(*mock_power_library_, battery_time_to_full())
       .WillOnce((Return(base::TimeDelta::FromMinutes(0))))
       .RetiresOnSaturation();
-  EXPECT_EQ(IDR_STATUSBAR_BATTERY_CHARGED, CallPowerChangedAndGetIconId());
+  EXPECT_EQ(kNumBatteryStates - 1, CallPowerChangedAndGetBatteryIndex());
 }
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargingTest) {
@@ -116,38 +119,13 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargingTest) {
       .WillRepeatedly((Return(base::TimeDelta::FromMinutes(24))))
       .RetiresOnSaturation();
 
-  // Test the 16 battery charging states.
-  // NOTE: Use an array rather than just calculating a resource number to avoid
-  // creating implicit ordering dependencies on the resource values.
-  static const int kChargingImages[] = {
-    IDR_STATUSBAR_BATTERY_CHARGING_1,
-    IDR_STATUSBAR_BATTERY_CHARGING_2,
-    IDR_STATUSBAR_BATTERY_CHARGING_3,
-    IDR_STATUSBAR_BATTERY_CHARGING_4,
-    IDR_STATUSBAR_BATTERY_CHARGING_5,
-    IDR_STATUSBAR_BATTERY_CHARGING_6,
-    IDR_STATUSBAR_BATTERY_CHARGING_7,
-    IDR_STATUSBAR_BATTERY_CHARGING_8,
-    IDR_STATUSBAR_BATTERY_CHARGING_9,
-    IDR_STATUSBAR_BATTERY_CHARGING_10,
-    IDR_STATUSBAR_BATTERY_CHARGING_11,
-    IDR_STATUSBAR_BATTERY_CHARGING_12,
-    IDR_STATUSBAR_BATTERY_CHARGING_13,
-    IDR_STATUSBAR_BATTERY_CHARGING_14,
-    IDR_STATUSBAR_BATTERY_CHARGING_15,
-    IDR_STATUSBAR_BATTERY_CHARGING_16,
-    IDR_STATUSBAR_BATTERY_CHARGING_17,
-    IDR_STATUSBAR_BATTERY_CHARGING_18,
-    IDR_STATUSBAR_BATTERY_CHARGING_19,
-  };
-  size_t id = 0;
+  int index = 0;
   for (float percent = 5.0; percent < 100.0; percent += 5.0) {
     EXPECT_CALL(*mock_power_library_, battery_percentage())
         .WillOnce((Return(percent)))
         .RetiresOnSaturation();
-    ASSERT_LT(id, arraysize(kChargingImages));
-    EXPECT_EQ(kChargingImages[id], CallPowerChangedAndGetIconId());
-    id++;
+    EXPECT_EQ(index, CallPowerChangedAndGetBatteryIndex());
+    index++;
   }
 }
 
@@ -174,38 +152,13 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryDischargingTest) {
       .WillRepeatedly((Return(base::TimeDelta::FromMinutes(24))))
       .RetiresOnSaturation();
 
-  // Test the 16 battery discharing states.
-  // NOTE: Use an array rather than just calculating a resource number to avoid
-  // creating implicit ordering dependencies on the resource values.
-  static const int kDischargingImages[] = {
-    IDR_STATUSBAR_BATTERY_DISCHARGING_1,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_2,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_3,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_4,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_5,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_6,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_7,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_8,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_9,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_10,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_11,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_12,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_13,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_14,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_15,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_16,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_17,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_18,
-    IDR_STATUSBAR_BATTERY_DISCHARGING_19,
-  };
-  size_t id = 0;
+  int index = 0;
   for (float percent = 5.0; percent < 100.0; percent += 5.0) {
     EXPECT_CALL(*mock_power_library_, battery_percentage())
         .WillOnce((Return(percent)))
         .RetiresOnSaturation();
-    ASSERT_LT(id, arraysize(kDischargingImages));
-    EXPECT_EQ(kDischargingImages[id], CallPowerChangedAndGetIconId());
-    id++;
+    EXPECT_EQ(index, CallPowerChangedAndGetBatteryIndex());
+    index++;
   }
 }
 
