3bd62fd559ca907a6a61ea7f36d8d8becde0c1c6
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 07:49:53 +0000

    Sync Promo: Show profile avatar next to user name
    If the user is signed into sync then we now show the profile avatar next to their user name in the sync promo. We only show the profile avatar icon if the user has created multiple profiles (this matches the behavior of the avatar button in the browser window).
    
    This change also adds hooks to show the profile menu if the user clicks their user name. Currently this doesn't do anything. I'll change it to show the profile menu once the profile menu CL lands.
    
    BUG=91009
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7610008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.h
+++ chrome/browser/ui/webui/ntp/new_tab_sync_setup_handler.h
@@ -40,6 +40,9 @@ class NewTabSyncSetupHandler : public SyncSetupHandler {
   // Javascript callback handler to expand the sync promo.
   void HandleExpandSyncPromo(const ListValue* args);
 
+  // Javascript callback handler to show the profiles menu.
+  void HandleShowProfilesMenu(const ListValue* args);
+
  private:
   // Sends the sync login name to the page.
   void UpdateLogin();
@@ -49,6 +52,9 @@ class NewTabSyncSetupHandler : public SyncSetupHandler {
 
   // Preference for the sync login name.
   StringPrefMember username_pref_;
+
+  // This is used to monitor the profile state.
+  NotificationRegistrar registrar_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_NTP_NEW_TAB_SYNC_SETUP_HANDLER_H_
