7cd76c2f28015ea7d5abdd3591b7e5cab3dd7c5e
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 17:37:07 +0000

    sync: more DEPS fun
    Hopefully this isn't too draconian.
    
    BUG=19878
    TEST=none
    
    Review URL: http://codereview.chromium.org/7634002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/bookmark_model_associator.cc
+++ chrome/browser/sync/glue/bookmark_model_associator.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/api/sync_error.h"
-#include "chrome/browser/sync/engine/nigori_util.h"
 #include "chrome/browser/sync/engine/syncapi.h"
 #include "chrome/browser/sync/glue/bookmark_change_processor.h"
 #include "chrome/browser/sync/util/cryptographer.h"
