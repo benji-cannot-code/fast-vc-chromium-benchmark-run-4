8efa0ad630aa0c7d96c6120f9e30ba8440f09b3c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-12 22:32:43 +0000

    [GTK] Fix non-pthreads build after r91906. https://bugs.webkit.org/show_bug.cgi?id=66151
    Patch by Kalev Lember <kalevlember@gmail.com> on 2011-08-12
    Reviewed by David Levin.
    
    r91906 broke the non-pthreads GTK+ build by including a header which
    doesn't exist. Fix it by including DateMath.h instead of DateMap.h.
    
    * wtf/gtk/ThreadingGtk.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/gtk/ThreadingGtk.cpp
+++ third_party/WebKit/Source/JavaScriptCore/wtf/gtk/ThreadingGtk.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !USE(PTHREADS)
 
 #include "CurrentTime.h"
-#include "DateMap.h"
+#include "DateMath.h"
 #include "dtoa.h"
 #include "HashMap.h"
 #include "MainThread.h"
