c25eeb5384bd9846d304b2c6e9160fdf9bc382ca
fsamuel@chromium.org <fsamuel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-12 18:43:36 +0000

    Chromium's WebKit API should use floats instead of doubles for zoom to match WebCore https://bugs.webkit.org/show_bug.cgi?id=66089
    Reviewed by Darin Fisher.
    
    * public/WebView.h:
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::zoomLevel):
    (WebKit::WebViewImpl::setZoomLevel):
    (WebKit::WebViewImpl::zoomLimitsChanged):
    (WebKit::WebViewImpl::fullFramePluginZoomLevelChanged):
    (WebKit::WebView::zoomLevelToZoomFactor):
    (WebKit::WebView::zoomFactorToZoomLevel):
    * src/WebViewImpl.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -1782,12 +1782,12 @@ void WebViewImpl::scrollFocusedNodeIntoView()
     }
 }
 
-double WebViewImpl::zoomLevel()
+float WebViewImpl::zoomLevel() const
 {
     return m_zoomLevel;
 }
 
-double WebViewImpl::setZoomLevel(bool textOnly, double zoomLevel)
+float WebViewImpl::setZoomLevel(bool textOnly, float zoomLevel)
 {
     if (zoomLevel < m_minimumZoomLevel)
         m_zoomLevel = m_minimumZoomLevel;
@@ -1801,7 +1801,7 @@ double WebViewImpl::setZoomLevel(bool textOnly, double zoomLevel)
     if (pluginContainer)
         pluginContainer->plugin()->setZoomLevel(m_zoomLevel, textOnly);
     else {
-        float zoomFactor = static_cast<float>(zoomLevelToZoomFactor(m_zoomLevel));
+        float zoomFactor = zoomLevelToZoomFactor(m_zoomLevel);
         if (textOnly)
             frame->setPageAndTextZoomFactors(1, zoomFactor);
         else
@@ -1810,15 +1810,15 @@ double WebViewImpl::setZoomLevel(bool textOnly, double zoomLevel)
     return m_zoomLevel;
 }
 
-void WebViewImpl::zoomLimitsChanged(double minimumZoomLevel,
-                                    double maximumZoomLevel)
+void WebViewImpl::zoomLimitsChanged(float minimumZoomLevel,
+                                    float maximumZoomLevel)
 {
     m_minimumZoomLevel = minimumZoomLevel;
     m_maximumZoomLevel = maximumZoomLevel;
     m_client->zoomLimitsChanged(m_minimumZoomLevel, m_maximumZoomLevel);
 }
 
-void WebViewImpl::fullFramePluginZoomLevelChanged(double zoomLevel)
+void WebViewImpl::fullFramePluginZoomLevelChanged(float zoomLevel)
 {
     if (zoomLevel == m_zoomLevel)
         return;
@@ -1827,12 +1827,12 @@ void WebViewImpl::fullFramePluginZoomLevelChanged(double zoomLevel)
     m_client->zoomLevelChanged();
 }
 
-double WebView::zoomLevelToZoomFactor(double zoomLevel)
+float WebView::zoomLevelToZoomFactor(float zoomLevel)
 {
     return pow(textSizeMultiplierRatio, zoomLevel);
 }
 
-double WebView::zoomFactorToZoomLevel(double factor)
+float WebView::zoomFactorToZoomLevel(float factor)
 {
     // Since factor = 1.2^level, level = log(factor) / log(1.2)
     return log(factor) / log(textSizeMultiplierRatio);
