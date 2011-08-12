941249b1a51660c4e1b31eddd995a10b78fdc8c8
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 00:55:55 +0000

    chromium-os:18602 - do not override shared setting on bad password
    BUG=chromium-os:18602
    TEST=See issue; test connection failure with shared option checked / unchecked.
    
    Review URL: http://codereview.chromium.org/7628009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/wifi_config_view.cc
+++ chrome/browser/chromeos/options/wifi_config_view.cc
@@ -536,6 +536,7 @@ void WifiConfigView::OnCertificatesLoaded(bool initial_load) {
 bool WifiConfigView::Login() {
   NetworkLibrary* cros = CrosLibrary::Get()->GetNetworkLibrary();
   if (service_path_.empty()) {
+    const bool share_default = true;  // share networks by default
     if (!eap_method_combobox_) {
       // Hidden ordinary Wi-Fi connection.
       ConnectionSecurity security = SECURITY_UNKNOWN;
@@ -556,7 +557,7 @@ bool WifiConfigView::Login() {
           GetPassphrase(),
           NULL,
           GetSaveCredentials(),
-          GetShareNetwork());
+          GetShareNetwork(share_default));
     } else {
       // Hidden 802.1X EAP Wi-Fi connection.
       chromeos::NetworkLibrary::EAPConfigData config_data;
@@ -573,7 +574,7 @@ bool WifiConfigView::Login() {
           GetPassphrase(),
           &config_data,
           GetSaveCredentials(),
-          GetShareNetwork());
+          GetShareNetwork(share_default));
     }
   } else {
     WifiNetwork* wifi = cros->FindWifiNetworkByPath(service_path_);
@@ -602,8 +603,8 @@ bool WifiConfigView::Login() {
       if (passphrase != wifi->passphrase())
         wifi->SetPassphrase(passphrase);
     }
-
-    cros->ConnectToWifiNetwork(wifi, GetShareNetwork());
+    bool share_default = (wifi->profile_type() == PROFILE_SHARED);
+    cros->ConnectToWifiNetwork(wifi, GetShareNetwork(share_default));
     // Connection failures are responsible for updating the UI, including
     // reopening dialogs.
   }
@@ -632,9 +633,9 @@ bool WifiConfigView::GetSaveCredentials() const {
   return save_credentials_checkbox_->checked();
 }
 
-bool WifiConfigView::GetShareNetwork() const {
-  if (!share_network_checkbox_)
-    return true;  // share networks by default (e.g. non secure network).
+bool WifiConfigView::GetShareNetwork(bool share_default) const {
+  if (!share_network_checkbox_ || !share_network_checkbox_->IsEnabled())
+    return share_default;
   return share_network_checkbox_->checked();
 }
 
