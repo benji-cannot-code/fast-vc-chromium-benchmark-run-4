f5d10124c2a811af76aee8cbe7ee4dce27143f11
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 18:27:57 +0000

    Minor cleanup to UsageTracker.IsWorking(). Review URL: http://codereview.chromium.org/7634013
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96583 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_manager.cc
+++ webkit/quota/quota_manager.cc
@@ -1184,14 +1184,14 @@ void QuotaManager::DeleteOriginData(
 bool QuotaManager::ResetUsageTracker(StorageType type) {
   switch (type) {
     case kStorageTypeTemporary:
-      if (!temporary_usage_tracker_->IsWorking())
+      if (temporary_usage_tracker_->IsWorking())
         return false;
       temporary_usage_tracker_.reset(
           new UsageTracker(clients_, kStorageTypeTemporary,
                            special_storage_policy_));
       return true;
     case kStorageTypePersistent:
-      if (!persistent_usage_tracker_->IsWorking())
+      if (persistent_usage_tracker_->IsWorking())
         return false;
       persistent_usage_tracker_.reset(
           new UsageTracker(clients_, kStorageTypePersistent,
