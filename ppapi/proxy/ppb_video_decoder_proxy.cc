7dce173fb1ef085e69d978364680f6e15c2327f5
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 22:50:19 +0000

    Gracefully handle multiple Flush/Reset/Decode with same id
    The DCHECKs don't trigger in release, and in any case it's a bit rude to have
    the renderer crash on a plugin that did the wrong thing, so send an error
    instead.
    
    BUG=None
    TEST=Pepper Flash
    
    
    Review URL: http://codereview.chromium.org/7628025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96649 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppb_video_decoder_proxy.cc
+++ ppapi/proxy/ppb_video_decoder_proxy.cc
@@ -97,7 +97,8 @@ int32_t VideoDecoder::Decode(
   if (enter_buffer.failed())
     return PP_ERROR_BADRESOURCE;
 
-  SetBitstreamBufferCallback(bitstream_buffer->id, callback);
+  if (!SetBitstreamBufferCallback(bitstream_buffer->id, callback))
+    return PP_ERROR_BADARGUMENT;
 
   Buffer* ppb_buffer =
       static_cast<Buffer*>(enter_buffer.object());
@@ -128,7 +129,8 @@ void VideoDecoder::ReusePictureBuffer(int32_t picture_buffer_id) {
 }
 
 int32_t VideoDecoder::Flush(PP_CompletionCallback callback) {
-  SetFlushCallback(callback);
+  if (!SetFlushCallback(callback))
+    return PP_ERROR_INPROGRESS;
 
   FlushCommandBuffer();
   GetDispatcher()->Send(new PpapiHostMsg_PPBVideoDecoder_Flush(
@@ -137,7 +139,8 @@ int32_t VideoDecoder::Flush(PP_CompletionCallback callback) {
 }
 
 int32_t VideoDecoder::Reset(PP_CompletionCallback callback) {
-  SetResetCallback(callback);
+  if (!SetResetCallback(callback))
+    return PP_ERROR_INPROGRESS;
 
   FlushCommandBuffer();
   GetDispatcher()->Send(new PpapiHostMsg_PPBVideoDecoder_Reset(
