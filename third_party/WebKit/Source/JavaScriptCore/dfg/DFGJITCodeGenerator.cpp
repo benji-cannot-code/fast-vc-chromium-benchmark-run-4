397b8e4d304384770533a9d52eb8ec4e6c8b1980
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-12 20:33:25 +0000

    REGRESSION (r91610?): Bing Maps fail to initialize (InvalidOperation: Matrix3D.invert) https://bugs.webkit.org/show_bug.cgi?id=66038
    Patch by Filip Pizlo <fpizlo@apple.com> on 2011-08-12
    Reviewed by Gavin Barraclough.
            
    Simplest and lowest-impact fix for the case where the spilled format
    of a DFG node differs from the register format: if the format is
    converted then indicate that the spilled value is no longer valid
    ("kill the spill").
    
    * dfg/DFGGenerationInfo.h:
    (JSC::DFG::GenerationInfo::killSpilled):
    * dfg/DFGJITCodeGenerator.cpp:
    (JSC::DFG::JITCodeGenerator::fillDouble):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::fillSpeculateDouble):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@92986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator.cpp
@@ -183,6 +183,7 @@ FPRReg JITCodeGenerator::fillDouble(NodeIndex nodeIndex)
         m_gprs.unlock(tempGpr);
         m_fprs.retain(fpr, virtualRegister, SpillOrderDouble);
         info.fillDouble(fpr);
+        info.killSpilled();
         return fpr;
     }
 
