37a9bdbfecaf11db04a5bd6879bccdeb244c0c96
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 09:47:05 +0000

    Change kNotSignedInProfile to kInitialProfile to better reflect its usage. Change ProfileManager::GetCurrentProfile to ProfileManager::GetInitialProfile as well.
    BUG=none
    TEST=no change in profile behavior
    
    Review URL: http://codereview.chromium.org/7619008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96540 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -51,7 +51,7 @@ std::wstring GetProfileIdFromPath(const FilePath& profile_path) {
   if (chrome::GetDefaultUserDataDirectory(&default_user_data_dir) &&
       profile_path.DirName() == default_user_data_dir &&
       profile_path.BaseName().value() ==
-          ASCIIToUTF16(chrome::kNotSignedInProfile)) {
+          ASCIIToUTF16(chrome::kInitialProfile)) {
     return std::wstring();
   }
 
@@ -224,7 +224,7 @@ bool MigrateChromiumShortcutsTask::GetExpectedAppId(
   if (command_line.HasSwitch(switches::kUserDataDir)) {
     profile_path =
         command_line.GetSwitchValuePath(switches::kUserDataDir).AppendASCII(
-            chrome::kNotSignedInProfile);
+            chrome::kInitialProfile);
   }
 
   std::wstring app_name;
