5d6ddc57a5de2a0bb59d876697cdfe1787cf4482
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-13 20:38:58 +0000

    Changed the value of WebKitShowsToolTipOverTruncatedTextPreferenceKey to match the naming everywhere else.
    Reviewed by Joseph Pecoraro.
    
    ../WebKit/mac: 
    
    * WebView/WebPreferenceKeysPrivate.h:
    
    ../WebKit/win: 
    
    * WebPreferenceKeysPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferenceKeysPrivate.h
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferenceKeysPrivate.h
@@ -116,7 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // cover methods themselves are private.
 #define WebKitRespectStandardStyleKeyEquivalentsPreferenceKey @"WebKitRespectStandardStyleKeyEquivalents"
 #define WebKitShowsURLsInToolTipsPreferenceKey @"WebKitShowsURLsInToolTips"
-#define WebKitShowsToolTipOverTruncatedTextPreferenceKey @"WebKitShowsToolTipForTruncatedText"
+#define WebKitShowsToolTipOverTruncatedTextPreferenceKey @"WebKitShowsToolTipOverTruncatedText"
 #define WebKitPDFDisplayModePreferenceKey @"WebKitPDFDisplayMode"
 #define WebKitPDFScaleFactorPreferenceKey @"WebKitPDFScaleFactor"
 #define WebKitUseSiteSpecificSpoofingPreferenceKey @"WebKitUseSiteSpecificSpoofing"
