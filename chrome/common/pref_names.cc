9c8ec2153b0832f3674f631aefc2c59237a6c04b
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-13 04:29:46 +0000

    ntp4 info bubble
    displays a bubble pointing at "Most Visited"; this is dismissed after 10 displays (try opening the ntp 11 times), or after the user clicks a navigation dot. See bug for more background.
    
    BUG=92434
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7461160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.cc
+++ chrome/common/pref_names.cc
@@ -1122,6 +1122,9 @@ const char kExtensionBlacklistUpdateVersion[] =
 
 const char kExtensionSidebarWidth[] = "extensions.sidebar.width";
 
+// Number of times the NTP4 informational bubble has been shown.
+const char kNTP4IntroDisplayCount[] = "ntp.intro_display_count";
+
 // New Tab Page URLs that should not be shown as most visited thumbnails.
 const char kNTPMostVisitedURLsBlacklist[] = "ntp.most_visited_blacklist";
 
