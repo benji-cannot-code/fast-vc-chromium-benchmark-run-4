c9a2f7470d67e4826f7cf2ef84558854733843f0
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-13 07:00:48 +0000

    Build fix.
    * WebView.cpp:
    (WebView::notifyPreferencesChanged):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/win/WebView.cpp
+++ third_party/WebKit/Source/WebKit/win/WebView.cpp
@@ -4720,12 +4720,6 @@ HRESULT WebView::notifyPreferencesChanged(IWebNotification* notification)
 
     settings->setShowsURLsInToolTips(false);
 
-    hr = preferences->showsToolTipOverTruncatedText(&enabled);
-    if (FAILED(hr))
-        return hr;
-
-    settings->setShowsToolTipOverTruncatedText(enabled);
-
     settings->setForceFTPDirectoryListings(true);
     settings->setDeveloperExtrasEnabled(developerExtrasEnabled());
     settings->setNeedsSiteSpecificQuirks(s_allowSiteSpecificHacks);
@@ -4871,6 +4865,12 @@ HRESULT WebView::notifyPreferencesChanged(IWebNotification* notification)
         return hr;
     settings->setUseHixie76WebSocketProtocol(enabled);
 #endif
+
+    hr = prefsPrivate->showsToolTipOverTruncatedText(&enabled);
+    if (FAILED(hr))
+        return hr;
+
+    settings->setShowsToolTipOverTruncatedText(enabled);
 
     if (!m_closeWindowTimer)
         m_mainFrame->invalidate(); // FIXME
