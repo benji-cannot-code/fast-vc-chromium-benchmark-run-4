88e9dbc1e61d55394e49b176f8f8a2acb884baf7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-13 17:34:31 +0000

    base: Fix the TODO in ListValue::Remove().
    Change the return type of Remove() to bool, and add a size_t output parameter.
    
    BUG=None
    TEST=None
    
    R=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7618021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_notification_provider.cc
+++ chrome/browser/content_settings/content_settings_notification_provider.cc
@@ -282,7 +282,7 @@ void NotificationProvider::PersistPermissionChange(
     // Remove from one list and add to the other.
     if (is_allowed) {
       // Remove from the denied list.
-      if (denied_sites->Remove(*value) != -1)
+      if (denied_sites->Remove(*value, NULL))
         denied_changed = true;
 
       // Add to the allowed list.
@@ -290,7 +290,7 @@ void NotificationProvider::PersistPermissionChange(
         allowed_changed = true;
     } else {
       // Remove from the allowed list.
-      if (allowed_sites->Remove(*value) != -1)
+      if (allowed_sites->Remove(*value, NULL))
         allowed_changed = true;
 
       // Add to the denied list.
@@ -337,8 +337,8 @@ void NotificationProvider::ResetAllowedOrigin(const GURL& origin) {
     ListPrefUpdate update(prefs, prefs::kDesktopNotificationAllowedOrigins);
     ListValue* allowed_sites = update.Get();
     StringValue value(origin.spec());
-    int removed_index = allowed_sites->Remove(value);
-    DCHECK_NE(-1, removed_index) << origin << " was not allowed";
+    bool removed = allowed_sites->Remove(value, NULL);
+    DCHECK_NE(false, removed) << origin << " was not allowed";
   }
   prefs->ScheduleSavePersistentPrefs();
 }
@@ -354,8 +354,8 @@ void NotificationProvider::ResetBlockedOrigin(const GURL& origin) {
     ListPrefUpdate update(prefs, prefs::kDesktopNotificationDeniedOrigins);
     ListValue* denied_sites = update.Get();
     StringValue value(origin.spec());
-    int removed_index = denied_sites->Remove(value);
-    DCHECK_NE(-1, removed_index) << origin << " was not blocked";
+    bool removed = denied_sites->Remove(value, NULL);
+    DCHECK_NE(false, removed) << origin << " was not blocked";
   }
   prefs->ScheduleSavePersistentPrefs();
 }
