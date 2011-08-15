f9c2d7f95ff71769aea39f5ba510f6f77d41a2e3
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-15 03:38:48 +0000

    WebUI TaskManager: Supports background page mode.
    A background-page-moded Task Manager can be lunched by selecting "View Background Pages (xx)" on the menu, which is shown only when any background page is present.
    
    When background-page mode is enabled, the row of background tasks are hi-lighted.
    
    BUG=chromium-os:13885
    TEST=manual on chromeos-chrome in Linux
    
    Review URL: http://codereview.chromium.org/7606029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/task_manager_dialog.h
+++ chrome/browser/ui/webui/task_manager_dialog.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TaskManagerDialog {
  public:
   static void Show();
+  static void ShowBackgroundPages();
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_TASK_MANAGER_DIALOG_H_
