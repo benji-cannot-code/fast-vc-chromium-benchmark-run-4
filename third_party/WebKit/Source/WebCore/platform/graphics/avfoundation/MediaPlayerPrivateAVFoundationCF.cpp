0689faa85d31ab78c42960be148597f514930087
jeffm@apple.com <jeffm@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-15 17:41:34 +0000

    MediaPlayerPrivateAVFoundationCF::playerItemStatus() should return MediaPlayerAVPlayerItemStatusDoesNotExist if there is no AVPlayerItem https://bugs.webkit.org/show_bug.cgi?id=66171
    MediaPlayerPrivateAVFoundationCF::playerItemStatus() should return MediaPlayerAVPlayerItemStatusDoesNotExist if there is no AVPlayerItem
    to match the Mac implementation in MediaPlayerPrivateAVFoundationObjC.  I also added better logging to notificationCallback().
    
    Reviewed by Jon Honeycutt.
    
    No new tests, uses existing media tests.
    
    * platform/graphics/avfoundation/MediaPlayerPrivateAVFoundationCF.cpp:
    (WebCore::MediaPlayerPrivateAVFoundationCF::playerItemStatus): Return MediaPlayerAVPlayerItemStatusDoesNotExist if no AVPlayerItem.
    (WebCore::AVFWrapper::notificationCallback): Log the name of the received notification.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundationCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundationCF.cpp
@@ -389,7 +389,7 @@ void MediaPlayerPrivateAVFoundationCF::beginLoadingMetadata()
 MediaPlayerPrivateAVFoundation::ItemStatus MediaPlayerPrivateAVFoundationCF::playerItemStatus() const
 {
     if (!avPlayerItem(m_avfWrapper))
-        return MediaPlayerPrivateAVFoundation::MediaPlayerAVPlayerItemStatusUnknown;
+        return MediaPlayerPrivateAVFoundation::MediaPlayerAVPlayerItemStatusDoesNotExist;
 
     AVCFPlayerItemStatus status = AVCFPlayerItemGetStatus(avPlayerItem(m_avfWrapper), 0);
     if (status == AVCFPlayerItemStatusUnknown)
@@ -1042,7 +1042,11 @@ void AVFWrapper::notificationCallback(CFNotificationCenterRef, void* observer, C
         return;
     }
 
-    LOG(Media, "AVFWrapper::notificationCallback(%p)", self);
+#if !LOG_DISABLED
+    char notificationName[256];
+    CFStringGetCString(propertyName, notificationName, sizeof(notificationName), kCFStringEncodingASCII);
+    LOG(Media, "AVFWrapper::notificationCallback(%p) %s", self, notificationName);
+#endif
 
     if (CFEqual(propertyName, AVCFPlayerItemDidPlayToEndTimeNotification))
         self->m_owner->scheduleMainThreadNotification(MediaPlayerPrivateAVFoundation::Notification::ItemDidPlayToEndTime);
