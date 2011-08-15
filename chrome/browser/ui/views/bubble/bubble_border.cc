27f2e66f1b6ed1596ddfc87852c4303644462ad6
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-15 22:16:13 +0000

    Revert 96817 - make bubble_border to use resources under ui/resources.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7584007
    
    TBR=saintlou@chromium.org
    Review URL: http://codereview.chromium.org/7648040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bubble/bubble_border.cc
+++ chrome/browser/ui/views/bubble/bubble_border.cc
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/bubble/bubble_border.h"
 
 #include "base/logging.h"
-#include "grit/ui_resources.h"
-#include "grit/ui_resources_standard.h"
+#include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas_skia.h"
