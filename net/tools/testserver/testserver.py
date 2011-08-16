6e5e4f82b7ec02c589419489851e4dc8ee412805
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 03:30:45 +0000

    Prevent DOS attack on UDP echo servers by distinguishing between an echo request and the echo response.
    Client sends <version><checksum><size><payload> data to TCP/UDP echo servers.
    <checksum> is the checksum of the <payload>. For the first cut, we will sum up
    the characters in <payload>.
    
    If checksum of the <payload> is verified, echo servers encrypt the data
    and send back the data as <version><checksum><size><key><encrypted_payload>. 
    <key> is is used to decrypt the <encrypted_payload>. <encrypted_payload> is
    the encrypted <payload>.
    
    BUG=87297
    R=jar
    TEST=network_stats unit tests.
    Review URL: http://codereview.chromium.org/7246021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96890 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/testserver/testserver.py
+++ net/tools/testserver/testserver.py
@@ -4,8 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""This is a simple HTTP/FTP/SYNC/TCP ECHO/UDP ECHO/ server used for testing
-Chrome.
+"""This is a simple HTTP/FTP/SYNC/TCP/UDP/ server used for testing Chrome.
 
 It supports several test URLs, as specified by the handlers in TestPageHandler.
 By default, it listens on an ephemeral port and sends the port number back to
@@ -22,6 +21,7 @@ import cgi
 import errno
 import optparse
 import os
+import random
 import re
 import select
 import simplejson
@@ -37,6 +37,7 @@ import zlib
 # Ignore deprecation warnings, they make our output more cluttered.
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+import echo_message
 import pyftpdlib.ftpserver
 import tlslite
 import tlslite.api
@@ -1522,10 +1523,19 @@ class TCPEchoHandler(SocketServer.BaseRequestHandler):
   """
 
   def handle(self):
-    data = self.request.recv(65536)
-    if not data:
+    """Handles the request from the client and constructs a response."""
+
+    data = self.request.recv(65536).strip()
+    # Verify the "echo request" message received from the client. Send back
+    # "echo response" message if "echo request" message is valid.
+    try:
+      return_data = echo_message.GetEchoResponseData(data)
+      if not return_data:
         return
-    self.request.send(data)
+    except ValueError:
+      return
+
+    self.request.send(return_data)
 
 
 class UDPEchoHandler(SocketServer.BaseRequestHandler):
@@ -1536,9 +1546,19 @@ class UDPEchoHandler(SocketServer.BaseRequestHandler):
   """
 
   def handle(self):
+    """Handles the request from the client and constructs a response."""
+
     data = self.request[0].strip()
     socket = self.request[1]
-    socket.sendto(data, self.client_address)
+    # Verify the "echo request" message received from the client. Send back
+    # "echo response" message if "echo request" message is valid.
+    try:
+      return_data = echo_message.GetEchoResponseData(data)
+      if not return_data:
+        return
+    except ValueError:
+      return
+    socket.sendto(return_data, self.client_address)
 
 
 class FileMultiplexer:
@@ -1605,10 +1625,16 @@ def main(options, args):
     server_data['port'] = server.server_port
     server_data['xmpp_port'] = server.xmpp_port
   elif options.server_type == SERVER_TCP_ECHO:
+    # Used for generating the key (randomly) that encodes the "echo request"
+    # message.
+    random.seed()
     server = TCPEchoServer(('127.0.0.1', port), TCPEchoHandler)
     print 'Echo TCP server started on port %d...' % server.server_port
     server_data['port'] = server.server_port
   elif options.server_type == SERVER_UDP_ECHO:
+    # Used for generating the key (randomly) that encodes the "echo request"
+    # message.
+    random.seed()
     server = UDPEchoServer(('127.0.0.1', port), UDPEchoHandler)
     print 'Echo UDP server started on port %d...' % server.server_port
     server_data['port'] = server.server_port
