77de6d1108857283d49abc5d1cc667965bfc72a3
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 07:51:47 +0000

    Forced cookie removal before sign-in page is shown.
    TEST=none
    BUG=chromium-os:18248
    Review URL: http://codereview.chromium.org/7629016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/power_library.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/login/webui_login_display.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/chromeos/user_cros_settings_provider.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
+#include "content/browser/tab_contents/tab_contents.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -194,6 +196,19 @@ void SigninScreenHandler::ShowError(int login_attempts,
                                   help_id);
 }
 
+void SigninScreenHandler::OnBrowsingDataRemoverDone() {
+  // Show sign in screen as soon as we clear the cookie jar.
+  DictionaryValue params;
+  params.SetString("email", email_);
+  params.SetString("startUrl", kGaiaExtStartPage);
+  params.SetBoolean("createAccount",
+      UserCrosSettingsProvider::cached_allow_new_user());
+  params.SetBoolean("guestSignin",
+      UserCrosSettingsProvider::cached_allow_guest());
+  email_.clear();
+  ShowScreen(kGaiaSigninScreen, &params);
+}
+
 void SigninScreenHandler::HandleCompleteLogin(const base::ListValue* args) {
   std::string username;
   std::string password;
@@ -241,20 +256,12 @@ void SigninScreenHandler::HandleRemoveUser(const base::ListValue* args) {
 
 void SigninScreenHandler::HandleShowAddUser(const base::ListValue* args) {
   if (extension_driven_) {
-    DictionaryValue params;
-    params.SetString("startUrl", kGaiaExtStartPage);
-
-    std::string email;
+    email_.clear();
     // |args| can be null if it's OOBE.
-    if (args && args->GetString(0, &email))
-      params.SetString("email", email);
+    if (args)
+      args->GetString(0, &email_);
 
-    params.SetBoolean("createAccount",
-        UserCrosSettingsProvider::cached_allow_new_user());
-    params.SetBoolean("guestSignin",
-        UserCrosSettingsProvider::cached_allow_guest());
-
-    ShowScreen(kGaiaSigninScreen, &params);
+    StartClearingCookies();
   } else {
     ShowScreen(kSigninScreen, NULL);
   }
@@ -351,6 +358,16 @@ void SigninScreenHandler::SendUserList(bool animated) {
 
 void SigninScreenHandler::HandleCreateAccount(const base::ListValue* args) {
   delegate_->CreateAccount();
+}
+
+void SigninScreenHandler::StartClearingCookies() {
+  BrowsingDataRemover* remover = new BrowsingDataRemover(
+      Profile::FromBrowserContext(web_ui_->tab_contents()->browser_context()),
+      BrowsingDataRemover::EVERYTHING,
+      base::Time());
+  remover->AddObserver(this);
+  remover->Remove(BrowsingDataRemover::REMOVE_COOKIES |
+                  BrowsingDataRemover::REMOVE_LSO_DATA);
 }
 
 }  // namespace chromeos
