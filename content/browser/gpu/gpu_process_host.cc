5ea8b11b9869dc02fb388d3b0491bda5dea1d08a
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 20:53:47 +0000

    Linux build fix.
    BUG=none
    TEST=tree green
    TBR=vangelis
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97017 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/gpu/gpu_process_host.h"
 
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/debug/trace_event.h"
 #include "base/memory/ref_counted.h"
