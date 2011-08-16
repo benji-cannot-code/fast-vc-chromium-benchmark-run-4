d8562ffc1e384e4ee24728cda9065010355ff083
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-16 21:19:11 +0000

    Unreviewed Chromium Windows build fix after r93140. Take 3.
    Include the proper includes!
    
    * platform/graphics/FontCache.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FontCache.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/FontCache.h
@@ -36,7 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Vector.h>
 #include <wtf/unicode/Unicode.h>
 
-#if PLATFORM(WIN)
+#if OS(WINDOWS)
+#include <windows.h>
 #include <objidl.h>
 #include <mlang.h>
 #endif
