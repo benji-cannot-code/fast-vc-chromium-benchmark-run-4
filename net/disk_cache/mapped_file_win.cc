e5fe3b0cb1a676443807b545b74ff562b0352a5d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 17:44:25 +0000

    Disk cache: Detect hardware failures when reading the headers of block mapped files.
    BUG=90087
    TEST=none
    Review URL: http://codereview.chromium.org/7661006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96971 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/mapped_file_win.cc
+++ net/disk_cache/mapped_file_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_path.h"
 #include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "net/disk_cache/disk_cache.h"
 
 namespace disk_cache {
@@ -26,6 +27,12 @@ void* MappedFile::Init(const FilePath& name, size_t size) {
   buffer_ = MapViewOfFile(section_, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, size);
   DCHECK(buffer_);
   view_size_ = size;
+
+  // Make sure we detect hardware failures reading the headers.
+  size_t temp_len = size ? size : 4096;
+  scoped_array<char> temp(new char[temp_len]);
+  if (!Read(temp.get(), temp_len, 0))
+    return NULL;
 
   return buffer_;
 }
