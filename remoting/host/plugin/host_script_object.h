0e458308f057e4765014e039ded55c4485a455a1
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-16 22:52:09 +0000

    Add PluginMessageLoopProxy and use it for Host plugin UI thread.
    The new class will also be used in the client plugin for the main plugin thread.
    
    BUG=None
    TEST=Everything works.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=96981
    
    Review URL: http://codereview.chromium.org/7635030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97050 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/host_script_object.h
+++ remoting/host/plugin/host_script_object.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/platform_thread.h"
 #include "base/time.h"
+#include "remoting/base/plugin_message_loop_proxy.h"
 #include "remoting/host/chromoting_host_context.h"
 #include "remoting/host/host_status_observer.h"
 #include "remoting/host/plugin/host_plugin_utils.h"
@@ -46,7 +47,8 @@ class NatPolicy;
 // destroyed it sychronously shuts down the host and all threads.
 class HostNPScriptObject : public HostStatusObserver {
  public:
-  HostNPScriptObject(NPP plugin, NPObject* parent);
+  HostNPScriptObject(NPP plugin, NPObject* parent,
+                     PluginMessageLoopProxy::Delegate* plugin_thread_delegate);
   virtual ~HostNPScriptObject();
 
   bool Init();
@@ -143,13 +145,6 @@ class HostNPScriptObject : public HostStatusObserver {
                              const NPVariant* args,
                              uint32_t argCount);
 
-  // Posts a task on the main NP thread.
-  void PostTaskToNPThread(
-      const tracked_objects::Location& from_here, const base::Closure& task);
-
-  // Utility function for PostTaskToNPThread.
-  static void NPTaskSpringboard(void* task);
-
   // Set an exception for the current call.
   void SetException(const std::string& exception_string);
 
@@ -163,6 +158,7 @@ class HostNPScriptObject : public HostStatusObserver {
   ScopedRefNPObject log_debug_info_func_;
   ScopedRefNPObject on_state_changed_func_;
   base::PlatformThreadId np_thread_id_;
+  scoped_refptr<PluginMessageLoopProxy> plugin_message_loop_proxy_;
 
   scoped_ptr<RegisterSupportHostRequest> register_request_;
   scoped_refptr<MutableHostConfig> host_config_;
@@ -173,7 +169,6 @@ class HostNPScriptObject : public HostStatusObserver {
   int failed_login_attempts_;
 
   base::WaitableEvent disconnected_event_;
-  base::CancellationFlag destructing_;
 
   scoped_ptr<policy_hack::NatPolicy> nat_policy_;
 
