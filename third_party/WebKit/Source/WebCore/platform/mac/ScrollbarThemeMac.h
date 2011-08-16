eb9ad97bba6ef4905959bd3e10e7a84d093b3777
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-16 16:34:05 +0000

    Chromium Mac: Rubber banding gutter drawing https://bugs.webkit.org/show_bug.cgi?id=66226
    Make ScrollView::paintOverhangAreas() use the ScrollbarTheme::nativeTheme() to
    draw the overhang areas.
    Move default implementation to ScrollbarThemeComposite::paintOverhangAreas().
    Add a different implementation for Chromium Mac.
    
    Patch by Alexei Svitkine <asvitkine@chromium.org> on 2011-08-16
    Reviewed by Dimitri Glazkov.
    
    No new tests since this is just refactoring code and adding a Chromium-specific path for overhang drawing.
    
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::wheelEvent):
    * platform/ScrollbarTheme.h:
    (WebCore::ScrollbarTheme::paintOverhangAreas):
    * platform/ScrollbarThemeComposite.cpp:
    (WebCore::ScrollbarThemeComposite::paintOverhangAreas):
    * platform/ScrollbarThemeComposite.h:
    * platform/chromium/ScrollbarThemeChromiumMac.h:
    * platform/chromium/ScrollbarThemeChromiumMac.mm:
    (WebCore::ScrollbarThemeChromiumMac::ScrollbarThemeChromiumMac):
    (WebCore::scrollbarStateToThemeState):
    (WebCore::ScrollbarThemeChromiumMac::paintTickmarks):
    (WebCore::ScrollbarThemeChromiumMac::paintOverhangAreas):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.h
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollbarThemeMac.h
@@ -40,6 +40,8 @@ public:
     ScrollbarThemeMac();
     virtual ~ScrollbarThemeMac();
 
+    void preferencesChanged();
+
     virtual void updateEnabledState(Scrollbar*);
 
     virtual bool paint(Scrollbar*, GraphicsContext* context, const IntRect& damageRect);
@@ -77,9 +79,6 @@ protected:
     
     virtual bool shouldCenterOnThumb(Scrollbar*, const PlatformMouseEvent&);
     virtual bool shouldDragDocumentInsteadOfThumb(Scrollbar*, const PlatformMouseEvent&);
-    
-public:
-    void preferencesChanged();
 };
 
 }
