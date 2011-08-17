b53acc976ee7f5d698b8be0a8717d8dc574cce59
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-17 03:26:35 +0000

    Fix broken tree
    TBR=brettw
    BUG=none
    Review URL: http://codereview.chromium.org/7671013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/pepper_flash_component_installer.cc
+++ chrome/browser/component_updater/pepper_flash_component_installer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
+#include "base/stringprintf.h"
 #include "base/values.h"
 #include "chrome/browser/component_updater/component_updater_service.h"
 #include "chrome/common/chrome_paths.h"
