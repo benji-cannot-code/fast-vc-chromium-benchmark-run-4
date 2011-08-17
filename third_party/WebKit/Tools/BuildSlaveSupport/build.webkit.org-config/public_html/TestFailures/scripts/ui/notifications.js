7ecc06b5e4b38f474497bcaabb1436664d4e0382
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-17 21:13:29 +0000

    Wire up relative time to garden-o-matic summary view. https://bugs.webkit.org/show_bug.cgi?id=66343
    Reviewed by Adam Barth.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/Trac.js: Add time to the commitData object.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/model_unittests.js: Adjusted unit tests.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications.js: Wired up relative date.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications_unittests.js: Updated unit tests.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/trac_unittests.js: Adjusted unit tests.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications.js
@@ -36,6 +36,7 @@ ui.notifications.Stream = base.extends('ol', {
     },
     push: function(notification)
     {
+        // FIXME: Add in descending time order.
         this.insertBefore(notification, this.firstChild);
         return notification;
     }
@@ -68,11 +69,16 @@ ui.notifications.Info = base.extends(Notification, {
 var Time = base.extends('time', {
     init: function()
     {
-        this.updateTime(new Date());
+        this.date = new Date();
     },
-    updateTime: function(time)
+    date: function()
     {
-        this.textContent = base.relativizeTime(time);
+        return this._date;
+    },
+    setDate: function(date)
+    {
+        this._date = date;
+        this.textContent = base.relativizeTime(date);
     }
 });
 
@@ -134,12 +140,13 @@ ui.notifications.TestFailures = base.extends(Notification, {
     {
         if (this.containsFailureAnalysis(failureAnalysis))
             return;
-        // FIXME: Retrieve date from failureAnalysis and set this._time.
-        // FIXME: Add in order by time.
         return this._tests.appendChild(new ui.notifications.FailingTest(failureAnalysis));
     },
     addCommitData: function(commitData)
     {
+        var commitDataDate = new Date(commitData.time);
+        if (this._time.date > commitDataDate);
+            this._time.date = commitDataDate;
         return this._causes.appendChild(new ui.notifications.SuspiciousCommit(commitData));
     }
 });
