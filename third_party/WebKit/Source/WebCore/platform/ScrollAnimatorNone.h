ebdf2c40ab48637d822ced60e57e760f65f99150
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-17 23:55:37 +0000

    ScrollAnimatorNone: Remove unused field https://bugs.webkit.org/show_bug.cgi?id=66312
    Patch by Scott Byer <scottbyer@chromium.org> on 2011-08-17
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    No functional change; no test needed.
    
    * platform/ScrollAnimatorNone.cpp:
    (WebCore::ScrollAnimatorNone::PerAxisData::updateDataFromParameters):
    (WebCore::ScrollAnimatorNone::scroll):
    * platform/ScrollAnimatorNone.h:
    
    Source/WebKit/chromium:
    
    * tests/ScrollAnimatorNoneTest.cpp:
    (ScrollAnimatorNoneTest::updateDataFromParameters):
    (TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollAnimatorNone.h
+++ third_party/WebKit/Source/WebCore/platform/ScrollAnimatorNone.h
@@ -82,7 +82,7 @@ protected:
     struct PerAxisData {
         PerAxisData(ScrollAnimatorNone* parent, float* currentPos);
         void reset();
-        bool updateDataFromParameters(ScrollbarOrientation, float step, float multiplier, float scrollableSize, double currentTime, Parameters*);
+        bool updateDataFromParameters(float step, float multiplier, float scrollableSize, double currentTime, Parameters*);
         bool animateScroll(double currentTime);
 
         static double curveAt(Curve, double t);
@@ -111,8 +111,6 @@ protected:
         double m_releasePosition;
         double m_releaseTime;
         Curve m_releaseCurve;
-
-        ScrollbarOrientation m_orientation;
     };
 
     void animationTimerFired(Timer<ScrollAnimatorNone>*);
