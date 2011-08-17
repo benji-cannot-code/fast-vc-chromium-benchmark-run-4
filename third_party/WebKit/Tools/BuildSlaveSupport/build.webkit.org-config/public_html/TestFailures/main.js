e1c1a102e68bc4b595b625dbd5a4455a08e58744
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-17 20:15:59 +0000

    garden-o-matic should be able to rebaseline expected failures https://bugs.webkit.org/show_bug.cgi?id=66204
    Reviewed by Dimitri Glazkov.
    
    This patch introduces a new view for displaying expect, actual, and
    differences between results.  The new view also has a fledgling
    controller that handles some basic operations.  The new view is
    exercised by rebaseline.html, which lets you rebaselines expected
    failures, but the new view is not fully integrated into
    garden-o-matic.html
    
    More patches will be required to make this stuff fully working, but
    this patch was already somewhat spiraling out of control.  Hopefully
    this patch will serve as a good starting point for further development.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/controllers: Added.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/controllers.js: Added.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/controllers/rebaseline.js: Added.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/garden-o-matic.html:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.css:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/rebaseline.html: Added.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/results.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/run-unittests.html:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui/results.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/ui/results_unittests.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/main.js
@@ -172,7 +172,7 @@ function failureInfoFromResultsDetail()
     return {
         'builderName': failureDetails.attr(config.kBuilderNameAttr),
         'testName': failureDetails.attr(config.kTestNameAttr),
-        'failureTypeList': failureDetails.attr(config.kFailureTypesAttr).split(' '),
+        'failureTypeList': results.failureTypeList(failureDetails.attr(config.kFailureTypesAttr)),
     }
 }
 
