fbe7271d081eb8fb08f12be85c7d91a30a05d98d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-17 01:17:31 +0000

    Revert 97049 - Trying to see if it fixes sync_integration_tests
    [Mac] Flip the flag for multi-profiles to on by default.
    
    This also changes ProfileManager::GetProfileInfoCache() to be GetProfileInfo()
    and returning the ProfileInfoInterface, making it easier to mock out for testing.
    In the few places that require the full ProfileInfoCache, GetMutableProfileInfo()
    was added.
    
    Because a handful of Mac unittests make calls into g_browser_process for the
    ProfileManager, BrowserTestHelper was expanded to set that up with a
    TestingProfile and a FakeProfileInfo so that tests continue to pass.
    
    BUG=60105
    TEST=Multi-profiles is on by default.
    
    Review URL: http://codereview.chromium.org/7648037
    
    TBR=rsesek@chromium.org
    Review URL: http://codereview.chromium.org/7621031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/manage_profile_handler.cc
+++ chrome/browser/ui/webui/options/manage_profile_handler.cc
@@ -77,7 +77,7 @@ void ManageProfileHandler::InitializeDefaultProfileIcons() {
 
 void ManageProfileHandler::SendProfileNames() {
   ProfileInfoCache& cache =
-      g_browser_process->profile_manager()->GetMutableProfileInfo();
+      g_browser_process->profile_manager()->GetProfileInfoCache();
   DictionaryValue profile_name_dict;
   for (size_t i = 0, e = cache.GetNumberOfProfiles(); i < e; ++i)
     profile_name_dict.SetBoolean(UTF16ToUTF8(cache.GetNameOfProfileAtIndex(i)),
@@ -95,7 +95,7 @@ void ManageProfileHandler::SetProfileNameAndIcon(const ListValue* args) {
     return;
 
   ProfileInfoCache& cache =
-      g_browser_process->profile_manager()->GetMutableProfileInfo();
+      g_browser_process->profile_manager()->GetProfileInfoCache();
   size_t profile_index = cache.GetIndexOfProfileWithPath(profile_file_path);
   if (profile_index == std::string::npos)
     return;
