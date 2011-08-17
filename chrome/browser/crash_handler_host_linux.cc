bde57a23a594fb3e2845a6fe6530ebe9c3aad5e9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-17 00:51:21 +0000

    browser: Fix compiler error related to stringprintf in crash_handler_host_linux.cc.
    Include missing stringprintf.h and add missing base:: to StringPrintf call.
    
    BUG=None
    TEST=None
    
    TBR=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7669008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/crash_handler_host_linux.cc
+++ chrome/browser/crash_handler_host_linux.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/rand_util.h"
 #include "base/string_util.h"
+#include "base/stringprintf.h"
 #include "base/task.h"
 #include "base/threading/thread.h"
 #include "breakpad/src/client/linux/handler/exception_handler.h"
@@ -334,8 +335,10 @@ void CrashHandlerHostLinux::WriteDumpFile(BreakpadInfo* info,
     PathService::Get(chrome::DIR_CRASH_DUMPS, &dumps_path);
   const uint64 rand = base::RandUint64();
   const std::string minidump_filename =
-      StringPrintf("%s/chromium-%s-minidump-%016" PRIx64 ".dmp",
-                   dumps_path.value().c_str(), process_type_.c_str(), rand);
+      base::StringPrintf("%s/chromium-%s-minidump-%016" PRIx64 ".dmp",
+                         dumps_path.value().c_str(),
+                         process_type_.c_str(),
+                         rand);
   if (!google_breakpad::WriteMinidump(minidump_filename.c_str(),
                                       crashing_pid, crash_context,
                                       kCrashContextSize)) {
