8e8af75e5247d87ad94bbe2a2ab17041e8c85fb0
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-17 12:18:16 +0000

    Include base/stringprintf.h in base/debug/profiler.cc.
    This is needed after r97067.
    
    BUG=none
    TEST=building with GYP_DEFINES="profiling=1" works
    TBR=tfarina
    
    Review URL: http://codereview.chromium.org/7624014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97128 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/profiler.cc
+++ base/debug/profiler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/process_util.h"
 #include "base/string_util.h"
+#include "base/stringprintf.h"
 
 #if defined(ENABLE_PROFILING) && !defined(NO_TCMALLOC)
 #include "third_party/tcmalloc/chromium/src/google/profiler.h"
@@ -63,4 +64,3 @@ bool BeingProfiled() {
 
 }  // namespace debug
 }  // namespace base
-
