e6b8aea45f9178360615ade492dd1c81086d94bd
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-17 02:45:44 +0000

    Fix cursor in fullscreen pepper
    BUG=chromium-os:14322
    TEST=Pepper Flash on Youtube, go full screen
    
    
    Review URL: http://codereview.chromium.org/7670008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97088 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget_fullscreen_pepper.cc
+++ content/renderer/render_widget_fullscreen_pepper.cc
@@ -39,8 +39,7 @@ class PepperWidget : public WebWidget {
   PepperWidget(webkit::ppapi::PluginInstance* plugin,
                RenderWidgetFullscreenPepper* widget)
       : plugin_(plugin),
-        widget_(widget),
-        cursor_(WebCursorInfo::TypePointer) {
+        widget_(widget) {
   }
 
   virtual ~PepperWidget() {}
@@ -98,7 +97,10 @@ class PepperWidget : public WebWidget {
   }
 
   virtual bool handleInputEvent(const WebInputEvent& event) {
-    bool result = plugin_->HandleInputEvent(event, &cursor_);
+    // This cursor info is ignored, we always set the cursor directly from
+    // RenderWidgetFullscreenPepper::DidChangeCursor.
+    WebCursorInfo cursor;
+    bool result = plugin_->HandleInputEvent(event, &cursor);
 
     // For normal web pages, WebViewImpl does input event translations and
     // generates context menu events. Since we don't have a WebView, we need to
@@ -127,7 +129,7 @@ class PepperWidget : public WebWidget {
       if (send_context_menu_event) {
         WebMouseEvent context_menu_event(mouse_event);
         context_menu_event.type = WebInputEvent::ContextMenu;
-        plugin_->HandleInputEvent(context_menu_event, &cursor_);
+        plugin_->HandleInputEvent(context_menu_event, &cursor);
       }
     }
     return result;
@@ -189,7 +191,6 @@ class PepperWidget : public WebWidget {
   scoped_refptr<webkit::ppapi::PluginInstance> plugin_;
   RenderWidgetFullscreenPepper* widget_;
   WebSize size_;
-  WebCursorInfo cursor_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperWidget);
 };
@@ -261,6 +262,11 @@ void RenderWidgetFullscreenPepper::Destroy() {
   // plugin_ to NULL to avoid calling into a dangling pointer e.g. on Close().
   plugin_ = NULL;
   Send(new ViewHostMsg_Close(routing_id_));
+}
+
+void RenderWidgetFullscreenPepper::DidChangeCursor(
+    const WebKit::WebCursorInfo& cursor) {
+  didChangeCursor(cursor);
 }
 
 webkit::ppapi::PluginDelegate::PlatformContext3D*
