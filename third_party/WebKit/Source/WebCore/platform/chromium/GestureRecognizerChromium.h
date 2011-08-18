3709e273f53b4a648b89a5b9bdbbf66b3a14a0ff
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-18 22:19:23 +0000

    GestureRecognizer: Update how gesture-scroll works.
    Source/WebCore:
    
    Instead of just sending a series of 'GestureUpdate' events, send a
    single 'GestureBegin' event, followed by a series of 'GestureUpdate' events,
    and end with a 'GestureEnd' event.
    https://bugs.webkit.org/show_bug.cgi?id=66267
    
    Patch by Sadrul Habib Chowdhury <sadrul@chromium.org> on 2011-08-18
    Reviewed by Adam Barth.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::handleGestureEvent):
    * platform/chromium/GestureRecognizerChromium.cpp:
    (WebCore::InnerGestureRecognizer::InnerGestureRecognizer):
    (WebCore::InnerGestureRecognizer::appendScrollGestureBegin):
    (WebCore::InnerGestureRecognizer::appendScrollGestureEnd):
    (WebCore::InnerGestureRecognizer::appendScrollGestureUpdate):
    (WebCore::scrollEnd):
    (WebCore::isClickOrScroll):
    (WebCore::inScroll):
    * platform/chromium/GestureRecognizerChromium.h:
    (WebCore::InnerGestureRecognizer::firstTouchPosition):
    
    Source/WebKit/chromium:
    
    Add a unit-test to make sure the correct gesture events are being
    generated.
    https://bugs.webkit.org/show_bug.cgi?id=66267
    
    Patch by Sadrul Habib Chowdhury <sadrul@chromium.org> on 2011-08-18
    Reviewed by Adam Barth.
    
    * tests/InnerGestureRecognizerTest.cpp:
    (WebCore::BuildablePlatformTouchPoint::BuildablePlatformTouchPoint):
    (WebCore::BuildablePlatformTouchEvent::BuildablePlatformTouchEvent):
    (WebCore::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93358 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/GestureRecognizerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/chromium/GestureRecognizerChromium.h
@@ -60,9 +60,13 @@ public:
     bool isInClickTimeWindow();
     bool isInsideManhattanSquare(const PlatformTouchPoint&);
     virtual PlatformGestureRecognizer::PassGestures  processTouchEventForGestures(const PlatformTouchEvent&, bool defaultPrevented);
-    void appendScrollGesture(const PlatformTouchPoint&, Gestures);
+    void appendScrollGestureBegin(const IntPoint&, Gestures);
+    void appendScrollGestureEnd(const IntPoint&, Gestures);
+    void appendScrollGestureUpdate(const IntPoint&, Gestures);
     void setState(State value) { m_state = value; }
     State state() { return m_state; }
+
+    IntPoint firstTouchPosition() { return m_firstTouchPosition; }
 protected:
     InnerGestureRecognizer();
     void addEdgeFunction(State, unsigned finger, PlatformTouchPoint::State, bool touchHandledByJavaScript, GestureTransitionFunction);
@@ -75,6 +79,7 @@ protected:
     IntPoint m_firstTouchPosition;
     double m_firstTouchTime;
     State m_state;
+    IntPoint m_lastTouchPosition;
     double m_lastTouchTime;
 
     bool m_ctrlKey;
