3ac7f8e5accab5d8d0a652d405fa4bbaf61fcfb3
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 01:51:24 +0000

    Print Preview: Add "print preview" back to about:flags; enable it by default just once.
    BUG=93257
    TEST=none
    
    Review URL: http://codereview.chromium.org/7631025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97264 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -162,7 +162,6 @@ const Experiment kExperiments[] = {
     kOsAll,
     SINGLE_VALUE_TYPE(switches::kDisableGpuVsync)
   },
-#if !defined(GOOGLE_CHROME_BUILD)
   // Only expose this for Chromium builds where users may not have the PDF
   // plugin. Do not give Google Chrome users the option to disable it here.
   {
@@ -172,7 +171,6 @@ const Experiment kExperiments[] = {
     kOsAll,
     SINGLE_VALUE_TYPE(switches::kEnablePrintPreview)
   },
-#endif
   // TODO(dspringer): When NaCl is on by default, remove this flag entry.
   {
     "enable-nacl",  // FLAGS:RECORD_UMA
