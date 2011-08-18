8188df67b9647f6e25c4145e563a4c6089d9aa42
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-18 22:03:11 +0000

    Source/WebCore: Unwarranted DOM Exception when when canvas2D drawImage is called with src rect out of bounds https://bugs.webkit.org/show_bug.cgi?id=65709
    Patch by Justin Novosad <junov@chromium.org> on 2011-08-18
    Reviewed by Stephen White.
    
    Test: fast/canvas/drawImage-clipped-source.html
    
    * html/canvas/CanvasRenderingContext2D.cpp:
    (WebCore::CanvasRenderingContext2D::drawImage):
    Removed the unnecessary dom exceptions for out of bounds source rectangles
    The overloads that receive video and image elements as source images
    now use the normalized versions of the source rectangle, which
    GraphicsContext (and its various platform flavors) can handle correctly.
    The normalized rectangle is the equivalent rectangle with width and height
    greater than 0.  The canvas version of this method, which had better layout
    test coverage, was already correctly using the normalized rectangle. The
    newly added layout test verifies correct behavior with negative
    source rectangle dimensions.
    * platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContext::drawNativeImage):
    Fixed algorithm that adjusts the destination rectangle to match the clipping
    applied to the source rect. The case of scaled filtered images with source
    rectangles that overlap the edge of the image was not being handled
    correctly. This use case was previously unsupported and used to trigger
    a DOM exception.
    
    LayoutTests: Unwarranted DOM Exception when when canvas2D drawImage is called with src
    rect out of bounds
    https://bugs.webkit.org/show_bug.cgi?id=65709
    
    Patch by Justin Novosad <junov@chromium.org> on 2011-08-18
    Reviewed by Stephen White.
    
    * fast/canvas/drawImage-clipped-source-expected.txt: Added.
    * fast/canvas/drawImage-clipped-source.html: Added.
    New test that verifies the clipping behavior when source rectangles
    are partially out of the bounds of the source image
    * fast/canvas/drawImage-clipped-source.js: Added.
    (patternTest.this.testPixel):
    (patternTest.this.testRedSquare):
    (patternTest.this.testPattern):
    (patternTest.this.testAggregatePattern):
    (patternTest):
    (drawTestPattern):
    (executeTest):
    * fast/canvas/drawImage-with-invalid-args-expected.txt:
    * fast/canvas/drawImage-with-invalid-args.html:
    This test covers (among other things) cases where the source rectangle is
    _completely_ outside the bounds of the source image.  It was modified to no
    longer expect DOM exceptions
    * platform/chromium/test_expectations.txt:
    Out-dated test canvas/philip/tests/2d.drawImage.outsidesource.html
    is now expected to fail
    * platform/mac/Skipped:
    Skipping canvas/philip/tests/2d.drawImage.outsidesource.html
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -1270,10 +1270,12 @@ void CanvasRenderingContext2D::drawImage(HTMLImageElement* image, const FloatRec
     FloatRect normalizedDstRect = normalizeRect(dstRect);
 
     FloatRect imageRect = FloatRect(FloatPoint(), size(image));
-    if (!imageRect.contains(normalizedSrcRect) || !srcRect.width() || !srcRect.height()) {
+    if (!srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
+    if (!imageRect.intersects(normalizedSrcRect))
+        return;
 
     GraphicsContext* c = drawingContext();
     if (!c)
@@ -1332,14 +1334,15 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
         return;
     }
 
-    if (!srcCanvasRect.contains(normalizeRect(srcRect)) || !srcRect.width() || !srcRect.height()) {
+    if (!srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
 
     ec = 0;
 
-    if (!dstRect.width() || !dstRect.height())
+    FloatRect normalizedSrcRect = normalizeRect(srcRect);
+    if (!srcCanvasRect.intersects(normalizedSrcRect) || !dstRect.width() || !dstRect.height())
         return;
 
     GraphicsContext* c = drawingContext();
@@ -1366,7 +1369,7 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
     sourceCanvas->makeRenderingResultsAvailable();
 #endif
 
-    c->drawImageBuffer(buffer, ColorSpaceDeviceRGB, dstRect, srcRect, state().m_globalComposite);
+    c->drawImageBuffer(buffer, ColorSpaceDeviceRGB, normalizeRect(dstRect), normalizedSrcRect, state().m_globalComposite);
     didDraw(dstRect);
 }
 
@@ -1413,12 +1416,14 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
         return;
 
     FloatRect videoRect = FloatRect(FloatPoint(), size(video));
-    if (!videoRect.contains(normalizeRect(srcRect)) || !srcRect.width() || !srcRect.height()) {
+    if (!srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
 
-    if (!dstRect.width() || !dstRect.height())
+    FloatRect normalizedSrcRect = normalizeRect(srcRect);
+    FloatRect normalizedDstRect = normalizeRect(dstRect);
+    if (!videoRect.intersects(normalizedSrcRect) || !dstRect.width() || !dstRect.height())
         return;
 
     GraphicsContext* c = drawingContext();
@@ -1430,13 +1435,13 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
     checkOrigin(video);
 
     GraphicsContextStateSaver stateSaver(*c);
-    c->clip(dstRect);
-    c->translate(dstRect.x(), dstRect.y());
-    c->scale(FloatSize(dstRect.width() / srcRect.width(), dstRect.height() / srcRect.height()));
-    c->translate(-srcRect.x(), -srcRect.y());
+    c->clip(normalizedDstRect);
+    c->translate(normalizedDstRect.x(), normalizedDstRect.y());
+    c->scale(FloatSize(normalizedDstRect.width() / normalizedSrcRect.width(), normalizedDstRect.height() / normalizedSrcRect.height()));
+    c->translate(-normalizedSrcRect.x(), -normalizedSrcRect.y());
     video->paintCurrentFrameInContext(c, IntRect(IntPoint(), size(video)));
     stateSaver.restore();
-    didDraw(dstRect);
+    didDraw(normalizedDstRect);
 }
 #endif
 
