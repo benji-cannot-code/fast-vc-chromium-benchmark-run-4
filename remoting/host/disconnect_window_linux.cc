9c0da5feb0548778b9399c2732e716a39263e124
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 22:19:23 +0000

    Make ChromotingHost::ui_strings() immutable. Use string16 for localized strings.
    There were two issues:
     - ui_strings() were mutable which creates possibility for race
       condition. Made it immutable, and added SetUiString() which can be
       called only before session is started.
     - strings were loaded after host is started which sometimes leads to
       non-localized string being shown. Particularly I often saw empty
       text on Disconnect button.
    
    BUG=None
    TEST=Disconnect button text is always localized.
    
    Review URL: http://codereview.chromium.org/7669045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97376 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/disconnect_window_linux.cc
+++ remoting/host/disconnect_window_linux.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/logging.h"
+#include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "remoting/host/chromoting_host.h"
 #include "remoting/host/ui_strings.h"
 #include "ui/base/gtk/gtk_signal.h"
@@ -37,7 +39,7 @@ class DisconnectWindowLinux : public DisconnectWindow {
  private:
   CHROMEGTK_CALLBACK_1(DisconnectWindowLinux, void, OnResponse, int);
 
-  void CreateWindow(UiStrings* ui_strings);
+  void CreateWindow(const UiStrings& ui_strings);
 
   ChromotingHost* host_;
   GtkWidget* disconnect_window_;
@@ -54,14 +56,15 @@ DisconnectWindowLinux::DisconnectWindowLinux()
 DisconnectWindowLinux::~DisconnectWindowLinux() {
 }
 
-void DisconnectWindowLinux::CreateWindow(UiStrings* ui_strings) {
+void DisconnectWindowLinux::CreateWindow(const UiStrings& ui_strings) {
   if (disconnect_window_) return;
 
   disconnect_window_ = gtk_dialog_new_with_buttons(
-      ui_strings->product_name.c_str(),
+      UTF16ToUTF8(ui_strings.product_name).c_str(),
       NULL,
       GTK_DIALOG_NO_SEPARATOR,
-      ui_strings->disconnect_button_text_plus_shortcut.c_str(), GTK_RESPONSE_OK,
+      UTF16ToUTF8(ui_strings.disconnect_button_text_plus_shortcut).c_str(),
+      GTK_RESPONSE_OK,
       NULL);
 
   GtkWindow* window = GTK_WINDOW(disconnect_window_);
@@ -95,11 +98,13 @@ void DisconnectWindowLinux::CreateWindow(UiStrings* ui_strings) {
 }
 
 void DisconnectWindowLinux::Show(ChromotingHost* host,
-                                 const std::string& /* unused */) {
+                                 const std::string& username) {
   host_ = host;
   CreateWindow(host->ui_strings());
-  gtk_label_set_text(GTK_LABEL(message_),
-                     host->ui_strings()->disconnect_message.c_str());
+
+  string16 text = ReplaceStringPlaceholders(
+      host->ui_strings().disconnect_message, UTF8ToUTF16(username), NULL);
+  gtk_label_set_text(GTK_LABEL(message_), UTF16ToUTF8(text).c_str());
   gtk_window_present(GTK_WINDOW(disconnect_window_));
 }
 
--- remoting/host/plugin/host_script_object.h
+++ remoting/host/plugin/host_script_object.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/cancellation_flag.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/string16.h"
 #include "base/threading/platform_thread.h"
 #include "base/time.h"
 #include "remoting/base/plugin_message_loop_proxy.h"
@@ -136,8 +137,7 @@ class HostNPScriptObject : public HostStatusObserver {
   // a string->string mapping with one optional substitution parameter. Stores
   // the translation in |result| and returns true on success, or leaves it
   // unchanged and returns false on failure.
-  bool LocalizeString(const char* tag, const char* substitution,
-                      std::string* result);
+  bool LocalizeString(const char* tag, string16* result);
 
   // Helper function for executing InvokeDefault on an NPObject, and ignoring
   // the return value.
