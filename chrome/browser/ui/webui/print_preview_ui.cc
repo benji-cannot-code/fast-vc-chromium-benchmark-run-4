a58e0aa71400d6d01022ca1fd09749af0cecba32
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 05:49:10 +0000

    Print Preview: Populating print preview tab title earlier.
    BUG=92428
    TEST=See bug description.
    
    
    Review URL: http://codereview.chromium.org/7669035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview_ui.cc
@@ -55,8 +55,11 @@ void PrintPreviewUI::ClearAllPreviewData() {
   print_preview_data_service()->RemoveEntry(preview_ui_addr_str_);
 }
 
-void PrintPreviewUI::SetInitiatorTabURL(const std::string& initiator_url) {
+void PrintPreviewUI::SetInitiatorTabURLAndTitle(
+    const std::string& initiator_url,
+    const string16& job_title) {
   initiator_url_ = initiator_url;
+  initiator_tab_title_ = job_title;
 }
 
 void PrintPreviewUI::OnInitiatorTabCrashed() {
@@ -80,8 +83,9 @@ void PrintPreviewUI::OnDidGetPreviewPageCount(
   base::FundamentalValue count(params.page_count);
   base::FundamentalValue modifiable(params.is_modifiable);
   base::FundamentalValue request_id(params.preview_request_id);
+  StringValue title(initiator_tab_title_);
   CallJavascriptFunction("onDidGetPreviewPageCount", count, modifiable,
-                         request_id);
+                         request_id, title);
 }
 
 void PrintPreviewUI::OnDidPreviewPage(int page_number,
@@ -103,7 +107,6 @@ void PrintPreviewUI::OnReusePreviewData(int preview_request_id) {
 }
 
 void PrintPreviewUI::OnPreviewDataIsAvailable(int expected_pages_count,
-                                              const string16& job_title,
                                               int preview_request_id) {
   VLOG(1) << "Print preview request finished with "
           << expected_pages_count << " pages";
@@ -116,10 +119,9 @@ void PrintPreviewUI::OnPreviewDataIsAvailable(int expected_pages_count,
                          expected_pages_count);
     initial_preview_start_time_ = base::TimeTicks();
   }
-  base::StringValue title(job_title);
   base::StringValue ui_identifier(preview_ui_addr_str_);
   base::FundamentalValue ui_preview_request_id(preview_request_id);
-  CallJavascriptFunction("updatePrintPreview", title, ui_identifier,
+  CallJavascriptFunction("updatePrintPreview", ui_identifier,
                          ui_preview_request_id);
 }
 
