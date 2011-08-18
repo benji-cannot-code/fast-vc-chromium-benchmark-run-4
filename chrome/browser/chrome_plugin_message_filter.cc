1505093e2b61699b3e5c52b385f4dbef1b4f6bb6
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 22:31:56 +0000

    Fix a thread restriction ASSERTION in the plugin download helper class which occurs while handling a url download request from the default plugin. The ASSERTION fires because we attempt to perform file IO on the IO thread.
    Fix is to issue the download request on the file thread. Switched to using the URLFetcher class for the
    download request as it internally proxies the actual HTTP requests to the IO thread.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=93186
    
    BUG=93186
    Review URL: http://codereview.chromium.org/7670038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_plugin_message_filter.cc
+++ chrome/browser/chrome_plugin_message_filter.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_plugin_message_filter.h"
 
 #include "chrome/browser/browser_process.h"
+#include "content/browser/browser_thread.h"
 #include "chrome/browser/plugin_download_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/browser/plugin_process_host.h"
@@ -43,11 +44,21 @@ bool ChromePluginMessageFilter::Send(IPC::Message* message) {
 #if defined(OS_WIN)
 void ChromePluginMessageFilter::OnDownloadUrl(const std::string& url,
                                               gfx::NativeWindow caller_window) {
+  BrowserThread::PostTask(
+      BrowserThread::FILE, FROM_HERE,
+      NewRunnableFunction(OnDownloadUrlOnFileThread, url, caller_window));
+}
+
+void ChromePluginMessageFilter::OnDownloadUrlOnFileThread(
+    const std::string& url,
+    gfx::NativeWindow caller_window) {
   PluginDownloadUrlHelper* download_url_helper =
       new PluginDownloadUrlHelper(url, caller_window, NULL);
   download_url_helper->InitiateDownload(
-      Profile::Deprecated::GetDefaultRequestContext()->GetURLRequestContext());
+      Profile::Deprecated::GetDefaultRequestContext(),
+      BrowserThread::GetMessageLoopProxyForThread(BrowserThread::FILE));
 }
+
 #endif
 
 void ChromePluginMessageFilter::OnGetPluginFinderUrl(
