ab218a0ea4f4acd0ecfbbb4a0fea04c069a93636
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-18 22:23:08 +0000

    Unreviewed, rolling out r93354. http://trac.webkit.org/changeset/93354 https://bugs.webkit.org/show_bug.cgi?id=66503
    "consensus was not reached" (Requested by senorblanco on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-18
    
    Source/WebCore:
    
    * html/canvas/CanvasRenderingContext2D.cpp:
    (WebCore::CanvasRenderingContext2D::drawImage):
    * platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContext::drawNativeImage):
    
    LayoutTests:
    
    * fast/canvas/drawImage-clipped-source-expected.txt: Removed.
    * fast/canvas/drawImage-clipped-source.html: Removed.
    * fast/canvas/drawImage-clipped-source.js: Removed.
    * fast/canvas/drawImage-with-invalid-args-expected.txt:
    * fast/canvas/drawImage-with-invalid-args.html:
    * platform/chromium/test_expectations.txt:
    * platform/mac/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93359 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -1270,12 +1270,10 @@ void CanvasRenderingContext2D::drawImage(HTMLImageElement* image, const FloatRec
     FloatRect normalizedDstRect = normalizeRect(dstRect);
 
     FloatRect imageRect = FloatRect(FloatPoint(), size(image));
-    if (!srcRect.width() || !srcRect.height()) {
+    if (!imageRect.contains(normalizedSrcRect) || !srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
-    if (!imageRect.intersects(normalizedSrcRect))
-        return;
 
     GraphicsContext* c = drawingContext();
     if (!c)
@@ -1334,15 +1332,14 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
         return;
     }
 
-    if (!srcRect.width() || !srcRect.height()) {
+    if (!srcCanvasRect.contains(normalizeRect(srcRect)) || !srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
 
     ec = 0;
 
-    FloatRect normalizedSrcRect = normalizeRect(srcRect);
-    if (!srcCanvasRect.intersects(normalizedSrcRect) || !dstRect.width() || !dstRect.height())
+    if (!dstRect.width() || !dstRect.height())
         return;
 
     GraphicsContext* c = drawingContext();
@@ -1369,7 +1366,7 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
     sourceCanvas->makeRenderingResultsAvailable();
 #endif
 
-    c->drawImageBuffer(buffer, ColorSpaceDeviceRGB, normalizeRect(dstRect), normalizedSrcRect, state().m_globalComposite);
+    c->drawImageBuffer(buffer, ColorSpaceDeviceRGB, dstRect, srcRect, state().m_globalComposite);
     didDraw(dstRect);
 }
 
@@ -1416,14 +1413,12 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
         return;
 
     FloatRect videoRect = FloatRect(FloatPoint(), size(video));
-    if (!srcRect.width() || !srcRect.height()) {
+    if (!videoRect.contains(normalizeRect(srcRect)) || !srcRect.width() || !srcRect.height()) {
         ec = INDEX_SIZE_ERR;
         return;
     }
 
-    FloatRect normalizedSrcRect = normalizeRect(srcRect);
-    FloatRect normalizedDstRect = normalizeRect(dstRect);
-    if (!videoRect.intersects(normalizedSrcRect) || !dstRect.width() || !dstRect.height())
+    if (!dstRect.width() || !dstRect.height())
         return;
 
     GraphicsContext* c = drawingContext();
@@ -1435,13 +1430,13 @@ void CanvasRenderingContext2D::drawImage(HTMLVideoElement* video, const FloatRec
     checkOrigin(video);
 
     GraphicsContextStateSaver stateSaver(*c);
-    c->clip(normalizedDstRect);
-    c->translate(normalizedDstRect.x(), normalizedDstRect.y());
-    c->scale(FloatSize(normalizedDstRect.width() / normalizedSrcRect.width(), normalizedDstRect.height() / normalizedSrcRect.height()));
-    c->translate(-normalizedSrcRect.x(), -normalizedSrcRect.y());
+    c->clip(dstRect);
+    c->translate(dstRect.x(), dstRect.y());
+    c->scale(FloatSize(dstRect.width() / srcRect.width(), dstRect.height() / srcRect.height()));
+    c->translate(-srcRect.x(), -srcRect.y());
     video->paintCurrentFrameInContext(c, IntRect(IntPoint(), size(video)));
     stateSaver.restore();
-    didDraw(normalizedDstRect);
+    didDraw(dstRect);
 }
 #endif
 
