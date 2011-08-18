23a278b1f578d0a71b417100a0d8542b89aaab71
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-18 22:09:13 +0000

    gtk: Convert FindBarGtk::OnActivate() to CHROMEGTK macro.
    BUG=None
    TEST=None
    
    R=evan@chromium.org
    
    Review URL: http://codereview.chromium.org/7670074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97370 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/find_bar_gtk.cc
+++ chrome/browser/ui/gtk/find_bar_gtk.cc
@@ -204,7 +204,7 @@ FindBarGtk::FindBarGtk(BrowserWindowGtk* window)
   g_signal_connect(text_entry_, "button-press-event",
                    G_CALLBACK(OnButtonPress), this);
   g_signal_connect(text_entry_, "move-cursor", G_CALLBACK(OnMoveCursor), this);
-  g_signal_connect(text_entry_, "activate", G_CALLBACK(OnActivate), this);
+  g_signal_connect(text_entry_, "activate", G_CALLBACK(OnActivateThunk), this);
   g_signal_connect(text_entry_, "direction-changed",
                    G_CALLBACK(OnWidgetDirectionChanged), this);
   g_signal_connect(text_entry_, "focus-in-event",
@@ -963,9 +963,8 @@ void FindBarGtk::OnMoveCursor(GtkEntry* entry, GtkMovementStep step, gint count,
   }
 }
 
-// static
-void FindBarGtk::OnActivate(GtkEntry* entry, FindBarGtk* bar) {
-  bar->FindEntryTextInContents(true);
+void FindBarGtk::OnActivate(GtkWidget* entry) {
+  FindEntryTextInContents(true);
 }
 
 // static
