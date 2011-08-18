1da144df03fcd8ef414ef04613e90fba4283cc3c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-18 20:22:47 +0000

    Add a bunch of accessors to WebAccessibilityObject to expose more of AccessibilityObject to Chromium, including range control values, document load state, live region attributes, and a few other ARIA attributes.
    https://bugs.webkit.org/show_bug.cgi?id=66411
    
    Patch by Dominic Mazzoni <dmazzoni@google.com> on 2011-08-18
    Reviewed by Dimitri Glazkov.
    
    * public/WebAccessibilityObject.h:
    * src/WebAccessibilityObject.cpp:
    (WebKit::WebAccessibilityObject::isAriaReadOnly):
    (WebKit::WebAccessibilityObject::isButtonStateMixed):
    (WebKit::WebAccessibilityObject::isControl):
    (WebKit::WebAccessibilityObject::isFocused):
    (WebKit::WebAccessibilityObject::isLoaded):
    (WebKit::WebAccessibilityObject::isRequired):
    (WebKit::WebAccessibilityObject::isVertical):
    (WebKit::WebAccessibilityObject::accessKey):
    (WebKit::WebAccessibilityObject::ariaHasPopup):
    (WebKit::WebAccessibilityObject::ariaLiveRegionAtomic):
    (WebKit::WebAccessibilityObject::ariaLiveRegionBusy):
    (WebKit::WebAccessibilityObject::ariaLiveRegionRelevant):
    (WebKit::WebAccessibilityObject::ariaLiveRegionStatus):
    (WebKit::WebAccessibilityObject::estimatedLoadingProgress):
    (WebKit::WebAccessibilityObject::hierarchicalLevel):
    (WebKit::WebAccessibilityObject::valueDescription):
    (WebKit::WebAccessibilityObject::valueForRange):
    (WebKit::WebAccessibilityObject::maxValueForRange):
    (WebKit::WebAccessibilityObject::minValueForRange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebAccessibilityObject.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebAccessibilityObject.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "EventHandler.h"
 #include "FrameView.h"
+#include "HTMLNames.h"
 #include "Node.h"
 #include "PlatformKeyboardEvent.h"
 #include "RenderStyle.h"
@@ -213,6 +214,24 @@ bool WebAccessibilityObject::isAnchor() const
     return m_private->isAnchor();
 }
 
+bool WebAccessibilityObject::isAriaReadOnly() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return equalIgnoringCase(m_private->getAttribute(HTMLNames::aria_readonlyAttr), "true");
+}
+
+bool WebAccessibilityObject::isButtonStateMixed() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->checkboxOrRadioValue() == ButtonStateMixed;
+}
+
 bool WebAccessibilityObject::isChecked() const
 {
     if (m_private.isNull())
@@ -231,14 +250,13 @@ bool WebAccessibilityObject::isCollapsed() const
     return m_private->isCollapsed();
 }
 
-
-bool WebAccessibilityObject::isFocused() const
+bool WebAccessibilityObject::isControl() const
 {
     if (m_private.isNull())
         return 0;
 
     m_private->updateBackingStore();
-    return m_private->isFocused();
+    return m_private->isControl();
 }
 
 bool WebAccessibilityObject::isEnabled() const
@@ -248,6 +266,15 @@ bool WebAccessibilityObject::isEnabled() const
 
     m_private->updateBackingStore();
     return m_private->isEnabled();
+}
+
+bool WebAccessibilityObject::isFocused() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->isFocused();
 }
 
 bool WebAccessibilityObject::isHovered() const
@@ -275,6 +302,15 @@ bool WebAccessibilityObject::isLinked() const
 
     m_private->updateBackingStore();
     return m_private->isLinked();
+}
+
+bool WebAccessibilityObject::isLoaded() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->isLoaded();
 }
 
 bool WebAccessibilityObject::isMultiSelectable() const
@@ -322,6 +358,15 @@ bool WebAccessibilityObject::isReadOnly() const
     return m_private->isReadOnly();
 }
 
+bool WebAccessibilityObject::isRequired() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->isRequired();
+}
+
 bool WebAccessibilityObject::isSelected() const
 {
     if (m_private.isNull())
@@ -329,6 +374,15 @@ bool WebAccessibilityObject::isSelected() const
 
     m_private->updateBackingStore();
     return m_private->isSelected();
+}
+
+bool WebAccessibilityObject::isVertical() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->orientation() == AccessibilityOrientationVertical;
 }
 
 bool WebAccessibilityObject::isVisible() const
@@ -349,6 +403,60 @@ bool WebAccessibilityObject::isVisited() const
     return m_private->isVisited();
 }
 
+WebString WebAccessibilityObject::accessKey() const
+{
+    if (m_private.isNull())
+        return WebString();
+
+    m_private->updateBackingStore();
+    return WebString(m_private->accessKey());
+}
+
+bool WebAccessibilityObject::ariaHasPopup() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->ariaHasPopup();
+}
+
+bool WebAccessibilityObject::ariaLiveRegionAtomic() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->ariaLiveRegionAtomic();
+}
+
+bool WebAccessibilityObject::ariaLiveRegionBusy() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->ariaLiveRegionBusy();
+}
+
+WebString WebAccessibilityObject::ariaLiveRegionRelevant() const
+{
+    if (m_private.isNull())
+        return WebString();
+
+    m_private->updateBackingStore();
+    return m_private->ariaLiveRegionRelevant();
+}
+
+WebString WebAccessibilityObject::ariaLiveRegionStatus() const
+{
+    if (m_private.isNull())
+        return WebString();
+
+    m_private->updateBackingStore();
+    return m_private->ariaLiveRegionStatus();
+}
+
 WebRect WebAccessibilityObject::boundingBoxRect() const
 {
     if (m_private.isNull())
@@ -356,6 +464,15 @@ WebRect WebAccessibilityObject::boundingBoxRect() const
 
     m_private->updateBackingStore();
     return m_private->boundingBoxRect();
+}
+
+double WebAccessibilityObject::estimatedLoadingProgress() const
+{
+    if (m_private.isNull())
+        return 0.0;
+
+    m_private->updateBackingStore();
+    return m_private->estimatedLoadingProgress();
 }
 
 WebString WebAccessibilityObject::helpText() const
@@ -374,6 +491,15 @@ int WebAccessibilityObject::headingLevel() const
 
     m_private->updateBackingStore();
     return m_private->headingLevel();
+}
+
+int WebAccessibilityObject::hierarchicalLevel() const
+{
+    if (m_private.isNull())
+        return 0;
+
+    m_private->updateBackingStore();
+    return m_private->hierarchicalLevel();
 }
 
 WebAccessibilityObject WebAccessibilityObject::hitTest(const WebPoint& point) const
@@ -492,6 +618,42 @@ WebURL WebAccessibilityObject::url() const
     
     m_private->updateBackingStore();
     return m_private->url();
+}
+
+WebString WebAccessibilityObject::valueDescription() const
+{
+    if (m_private.isNull())
+        return WebString();
+
+    m_private->updateBackingStore();
+    return m_private->valueDescription();
+}
+
+float WebAccessibilityObject::valueForRange() const
+{
+    if (m_private.isNull())
+        return 0.0;
+
+    m_private->updateBackingStore();
+    return m_private->valueForRange();
+}
+
+float WebAccessibilityObject::maxValueForRange() const
+{
+    if (m_private.isNull())
+        return 0.0;
+
+    m_private->updateBackingStore();
+    return m_private->maxValueForRange();
+}
+
+float WebAccessibilityObject::minValueForRange() const
+{
+    if (m_private.isNull())
+        return 0.0;
+
+    m_private->updateBackingStore();
+    return m_private->minValueForRange();
 }
 
 WebNode WebAccessibilityObject::node() const
