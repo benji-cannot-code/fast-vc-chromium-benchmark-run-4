9cc5967fcb7965d5c5d1a3b016567017288ba7c3
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 22:26:37 +0000

    [chromium] Chromium GPU layout tests should use the normal process number selection, not force 1 worker https://bugs.webkit.org/show_bug.cgi?id=66603
    Reviewed by Dirk Pranke.
    
    The various ChromiumGpu ports should use the same child worker
    selection logic as other ports so they can run with full
    parallelization on multicore systems.
    
    * Scripts/webkitpy/layout_tests/port/chromium_gpu.py:
    * Scripts/webkitpy/layout_tests/port/chromium_gpu_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
@@ -96,9 +96,6 @@ class ChromiumGpuLinuxPort(chromium_linux.ChromiumLinuxPort):
         return (map(self._webkit_baseline_path, ['chromium-gpu-linux', 'chromium-gpu-win', 'chromium-gpu']) +
                 chromium_linux.ChromiumLinuxPort.baseline_search_path(self))
 
-    def default_child_processes(self):
-        return 1
-
     def tests(self, paths):
         return _tests(self, paths)
 
@@ -112,9 +109,6 @@ class ChromiumGpuMacPort(chromium_mac.ChromiumMacPort):
         return (map(self._webkit_baseline_path, ['chromium-gpu-mac', 'chromium-gpu']) +
                 chromium_mac.ChromiumMacPort.baseline_search_path(self))
 
-    def default_child_processes(self):
-        return 1
-
     def tests(self, paths):
         return _tests(self, paths)
 
@@ -127,9 +121,6 @@ class ChromiumGpuWinPort(chromium_win.ChromiumWinPort):
     def baseline_search_path(self):
         return (map(self._webkit_baseline_path, ['chromium-gpu-win', 'chromium-gpu']) +
                 chromium_win.ChromiumWinPort.baseline_search_path(self))
-
-    def default_child_processes(self):
-        return 1
 
     def tests(self, paths):
         return _tests(self, paths)
