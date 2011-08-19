3b1924368f230b97a51ec43a7d7d4793d634b8cb
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 19:59:57 +0000

    Fix checkdeps
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97495 0039d316-1c4b-4281-b951-d872f2087c98

--- aura/event.h
+++ aura/event.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/events.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/gfx/point.h"
-#include "views/native_types.h"
-#include "views/views_export.h"
 
 namespace aura {
 
