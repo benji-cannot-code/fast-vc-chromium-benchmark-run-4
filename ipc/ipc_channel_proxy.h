8a388810a066e95614df85a5ce703894627a3867
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 18:05:11 +0000

    Fix IPC OnChannelConnected() to send correct PID on Linux/CrOS
    Sandboxed renderers on Linux/CrOS are in a PID namespace, so they don't know
    their own global PID.  Thus the PID sent in the IPC channel Hello message
    contains an unexpected value, which is used in the OnChannelConnected()
    callback into chrome.
    
    This causes problems like the Task Manager not showing any data for FPS,
    JavaScript memory and image cache memory.  The task manager is attempting to
    use the PID/process handle from BrowserMessageFilter, which got it from
    IPC::Channel::Listener::OnChannelConnected(), and it doesn't match the
    global PID of each renderer.
    
    BUG=70179
    TEST=manual, open a few tabs, then open task manager, right-click to turn on JavaScript memory and image memory. Verify there are non-zero values for FPS, JavaScript memory, image cache memory
    
    Review URL: http://codereview.chromium.org/7661004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97481 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_proxy.h
+++ ipc/ipc_channel_proxy.h
@@ -119,7 +119,8 @@ class IPC_EXPORT ChannelProxy : public Message::Sender {
   ChannelProxy(const IPC::ChannelHandle& channel_handle,
                Channel::Mode mode,
                Channel::Listener* listener,
-               base::MessageLoopProxy* ipc_thread_loop);
+               base::MessageLoopProxy* ipc_thread_loop,
+               bool needs_override_peer_pid);
 
   virtual ~ChannelProxy();
 
@@ -162,6 +163,11 @@ class IPC_EXPORT ChannelProxy : public Message::Sender {
   bool GetClientEuid(uid_t* client_euid) const;
 #endif  // defined(OS_POSIX)
 
+#if defined(OS_LINUX)
+  // Calls through to the underlying channel's method.
+  void OverridePeerPid(int32 peer_pid);
+#endif  // defined(OS_LINUX)
+
  protected:
   class Context;
   // A subclass uses this constructor if it needs to add more information
@@ -170,6 +176,7 @@ class IPC_EXPORT ChannelProxy : public Message::Sender {
   ChannelProxy(const IPC::ChannelHandle& channel_handle,
                Channel::Mode mode,
                base::MessageLoopProxy* ipc_thread_loop,
+               bool needs_override_peer_pid,
                Context* context,
                bool create_pipe_now);
 
@@ -218,12 +225,16 @@ class IPC_EXPORT ChannelProxy : public Message::Sender {
 
     // Create the Channel
     void CreateChannel(const IPC::ChannelHandle& channel_handle,
-                       const Channel::Mode& mode);
+                       const Channel::Mode& mode,
+                       bool needs_override_peer_pid);
 
     // Methods called on the IO thread.
     void OnSendMessage(Message* message_ptr);
     void OnAddFilter();
     void OnRemoveFilter(MessageFilter* filter);
+#if defined(OS_LINUX)
+    void OnOverridePeerPid(int32 peer_pid);
+#endif
 
     // Methods called on the listener thread.
     void AddFilter(MessageFilter* filter);
@@ -258,7 +269,8 @@ class IPC_EXPORT ChannelProxy : public Message::Sender {
   friend class SendTask;
 
   void Init(const IPC::ChannelHandle& channel_handle, Channel::Mode mode,
-            base::MessageLoopProxy* ipc_thread_loop, bool create_pipe_now);
+            base::MessageLoopProxy* ipc_thread_loop, bool create_pipe_now,
+            bool needs_override_peer_pid);
 
   // By maintaining this indirection (ref-counted) to our internal state, we
   // can safely be destroyed while the background thread continues to do stuff
