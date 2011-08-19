ccbf145c3b7ad5b2d6500b9f1395dadd6572f457
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 23:24:51 +0000

    Unreviewed, rolling out r93426. http://trac.webkit.org/changeset/93426 https://bugs.webkit.org/show_bug.cgi?id=66607
    Broke the Mac build by not building gtest.framework anymore
    (Requested by andersca on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-19
    
    Source/ThirdParty:
    
    * gtest/README.WebKit:
    * gtest/include/gtest/internal/gtest-port.h:
    * gtest/msvc/gtest-md.vcproj:
    * gtest/xcode/Config/General.xcconfig:
    * gtest/xcode/gtest.xcodeproj/project.pbxproj:
    
    Tools:
    
    * TestWebKitAPI/Tests/WTF/StringOperators.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/ThirdParty/gtest/include/gtest/internal/gtest-port.h
+++ third_party/WebKit/Source/ThirdParty/gtest/include/gtest/internal/gtest-port.h
@@ -178,24 +178,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/stat.h>
 #endif  // !_WIN32_WCE
 
-#include <wtf/Platform.h>
-
-#if !PLATFORM(CHROMIUM) && OS(WINDOWS) && !defined(BUILDING_WX__) && !COMPILER(GCC)
-#define JS_EXPORTDATA __declspec(dllimport)
-#define WEBKIT_EXPORTDATA __declspec(dllimport)
-#define WTF_EXPORT_PRIVATE
-#define JS_EXPORT_PRIVATE
-#define JS_EXPORTCLASS JS_EXPORTDATA
-#else
-#define JS_EXPORTDATA
-#define JS_EXPORTCLASS
-#define WEBKIT_EXPORTDATA
-#define WTF_EXPORT_PRIVATE
-#define JS_EXPORT_PRIVATE
-#endif
-
-#include <wtf/FastMalloc.h>
-
 #include <iostream>  // NOLINT
 #include <sstream>  // NOLINT
 #include <string>  // NOLINT
