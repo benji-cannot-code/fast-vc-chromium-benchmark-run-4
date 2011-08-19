a1d7977a26343dfdd498000f5c20e660c8701732
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 20:40:52 +0000

    [Sync] Add integration tests for migration cycle
    Add integration tests to make sure that the migration cycle doesn't stall.
    
    Add way for sync integration tests to turn off notifications.
    
    BUG=92928
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7669064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97504 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_harness.cc
+++ chrome/browser/sync/profile_sync_service_harness.cc
@@ -96,7 +96,8 @@ bool StateChangeTimeoutEvent::Abort() {
 ProfileSyncServiceHarness::ProfileSyncServiceHarness(
     Profile* profile,
     const std::string& username,
-    const std::string& password)
+    const std::string& password,
+    bool expected_notifications_enabled)
     : waiting_for_encryption_type_(syncable::UNSPECIFIED),
       wait_state_(INITIAL_WAIT_STATE),
       profile_(profile),
@@ -104,6 +105,7 @@ ProfileSyncServiceHarness::ProfileSyncServiceHarness(
       timestamp_match_partner_(NULL),
       username_(username),
       password_(password),
+      expected_notifications_enabled_(expected_notifications_enabled),
       profile_debug_name_(profile->GetDebugName()) {
   if (IsSyncAlreadySetup()) {
     service_ = profile_->GetProfileSyncService();
@@ -121,7 +123,9 @@ ProfileSyncServiceHarness* ProfileSyncServiceHarness::CreateAndAttach(
     NOTREACHED() << "Profile has never signed into sync.";
     return NULL;
   }
-  return new ProfileSyncServiceHarness(profile, "", "");
+  return new ProfileSyncServiceHarness(
+      profile, "", "",
+      /* expected_notifications_enabled */ true);
 }
 
 void ProfileSyncServiceHarness::SetCredentials(const std::string& username,
@@ -617,7 +621,8 @@ bool ProfileSyncServiceHarness::IsSynced() {
   bool is_synced = snap &&
       snap->num_blocking_conflicting_updates == 0 &&
       ServiceIsPushingChanges() &&
-      GetStatus().notifications_enabled &&
+      (GetStatus().notifications_enabled ==
+       expected_notifications_enabled_) &&
       !service()->HasUnsyncedItems() &&
       !snap->has_more_to_sync &&
       snap->unsynced_count == 0 &&
