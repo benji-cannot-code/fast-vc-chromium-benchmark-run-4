157fbb2ff2ae68d737c931011f83bf39ea2746e1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 14:41:22 +0000

    base/json: Don't use static variables of user-defined type (i.e, std::wstring).
    Instead use plain wchar_t array.
    
    BUG=83856
    TEST=None
    
    R=tony@chromium.org
    
    Review URL: http://codereview.chromium.org/7624057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97458 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_reader.cc
+++ base/json/json_reader.cc
@@ -14,12 +14,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 
+namespace {
+
+const wchar_t kNullString[] = L"null";
+const wchar_t kTrueString[] = L"true";
+const wchar_t kFalseString[] = L"false";
+
+const int kStackLimit = 100;
+
+}  // namespace
+
 namespace base {
 
 static const JSONReader::Token kInvalidToken(JSONReader::Token::INVALID_TOKEN,
                                              0, 0);
-static const int kStackLimit = 100;
-
 namespace {
 
 // A helper method for ParseNumberToken.  It reads an int from the end of
@@ -64,7 +72,7 @@ bool ReadHexDigits(JSONReader::Token& token, int digits) {
   return true;
 }
 
-}  // anonymous namespace
+}  // namespace
 
 const char* JSONReader::kBadRootElementType =
     "Root value must be an array or object.";
@@ -510,10 +518,6 @@ Value* JSONReader::DecodeString(const Token& token) {
 }
 
 JSONReader::Token JSONReader::ParseToken() {
-  static const std::wstring kNullString(L"null");
-  static const std::wstring kTrueString(L"true");
-  static const std::wstring kFalseString(L"false");
-
   EatWhitespaceAndComments();
 
   Token token(Token::INVALID_TOKEN, 0, 0);
@@ -523,17 +527,17 @@ JSONReader::Token JSONReader::ParseToken() {
       break;
 
     case 'n':
-      if (NextStringMatch(kNullString))
+      if (NextStringMatch(kNullString, arraysize(kNullString) - 1))
         token = Token(Token::NULL_TOKEN, json_pos_, 4);
       break;
 
     case 't':
-      if (NextStringMatch(kTrueString))
+      if (NextStringMatch(kTrueString, arraysize(kTrueString) - 1))
         token = Token(Token::BOOL_TRUE, json_pos_, 4);
       break;
 
     case 'f':
-      if (NextStringMatch(kFalseString))
+      if (NextStringMatch(kFalseString, arraysize(kFalseString) - 1))
         token = Token(Token::BOOL_FALSE, json_pos_, 5);
       break;
 
@@ -637,14 +641,8 @@ bool JSONReader::EatComment() {
   return true;
 }
 
-bool JSONReader::NextStringMatch(const std::wstring& str) {
-  for (size_t i = 0; i < str.length(); ++i) {
-    if ('\0' == *json_pos_)
-      return false;
-    if (*(json_pos_ + i) != str[i])
-      return false;
-  }
-  return true;
+bool JSONReader::NextStringMatch(const wchar_t* str, size_t length) {
+  return wcsncmp(json_pos_, str, length) == 0;
 }
 
 void JSONReader::SetErrorCode(JsonParseError error,
