5117792cb068dd53502b5bc06eeed864f349f77f
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 15:11:35 +0000

    Windows build fix after r93385
    * dom/DOMAllInOne.cpp: Added EventDispatcMediator.cpp.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DOMAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/dom/DOMAllInOne.cpp
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ErrorEvent.cpp"
 #include "Event.cpp"
 #include "EventContext.cpp"
+#include "EventDispatcMediator.cpp"
 #include "EventDispatcher.cpp"
 #include "EventNames.cpp"
 #include "EventQueue.cpp"
