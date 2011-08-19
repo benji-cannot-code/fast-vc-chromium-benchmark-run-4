fefbbf4f203ffb2e1d5a28f3cbc1fe7e11f639c3
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 23:56:34 +0000

    Fix Windows build.
    * WebProcess/Plugins/PluginController.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93456 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginController.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginController.h
@@ -27,8 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PluginController_h
 #define PluginController_h
 
-#include "PluginComplexTextInputState.h"
 #include <wtf/Forward.h>
+
+#if PLATFORM(MAC)
+#include "PluginComplexTextInputState.h"
+#endif
 
 struct NPObject;
 typedef struct _NPVariant NPVariant;
