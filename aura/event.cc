f42a48093a9f6de657d0a78c320253b65087195b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 19:51:55 +0000

    Copy over some basic Event code from views.
    Eventually the views::Event type will become a shell that contains or inherits from this type.
    For now, just enough glue to get event stuff going in aura::WM... not hooked up yet. This is just the copy to keep things simple to review.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/7696004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97494 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ aura/event.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "aura/event.h"
+
+namespace aura {
+
+Event::Event(ui::EventType type, int flags)
+    : type_(type),
+      time_stamp_(base::Time::NowFromSystemTime()),
+      flags_(flags) {
+  Init();
+}
+
+Event::Event(NativeEvent native_event, ui::EventType type, int flags)
+    : type_(type),
+      time_stamp_(base::Time::NowFromSystemTime()),
+      flags_(flags) {
+  InitWithNativeEvent(native_event);
+}
+
+Event::Event(const Event& copy)
+    : native_event_(copy.native_event_),
+      type_(copy.type_),
+      time_stamp_(copy.time_stamp_),
+      flags_(copy.flags_) {
+}
+
+}  // namespace aura
+
