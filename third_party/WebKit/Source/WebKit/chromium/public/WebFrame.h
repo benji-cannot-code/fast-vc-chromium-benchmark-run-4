d7fd70d0c315922665f63a86796e44f3f597647f
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 23:18:11 +0000

    [chromium] Remove various unreferenced bits from Chromium API headers https://bugs.webkit.org/show_bug.cgi?id=66575
    Reviewed by Tony Chang.
    
    * public/WebApplicationCacheHost.h:
    (WebKit::WebApplicationCacheHost::willStartMainResourceRequest):
    * public/WebFrame.h:
    * public/WebFrameClient.h:
    (WebKit::WebFrameClient::didReceiveTitle):
    * public/WebVideoFrame.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebNode.h"
 #include "WebURL.h"
 #include "WebURLLoaderOptions.h"
-// FIXME : Remove this file when transient done.
-#include "WebVector.h"
 
 struct NPObject;
 
