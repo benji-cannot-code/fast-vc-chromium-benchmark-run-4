cbfc0dc1c022cf4fbf649a48727f956854af0e02
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 00:40:17 +0000

    Make the core download content ask the delegate whether it needs to generate a hash or not.
    BUG=82782
    Review URL: http://codereview.chromium.org/7671055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97400 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/mock_download_manager_delegate.cc
+++ chrome/browser/download/mock_download_manager_delegate.cc
@@ -28,6 +28,10 @@ bool MockDownloadManagerDelegate::ShouldOpenFileBasedOnExtension(
   return false;
 }
 
+bool MockDownloadManagerDelegate::GenerateFileHash() {
+  return false;
+}
+
 void MockDownloadManagerDelegate::GetSaveDir(
     TabContents* tab_contents,
     FilePath* website_save_dir,
