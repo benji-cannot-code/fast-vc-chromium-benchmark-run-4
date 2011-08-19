a1a90901be19ca191347fabe298dfc2bd33170c3
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 23:50:09 +0000

    [chromium] Regression: Fix clang errors from r93424 https://bugs.webkit.org/show_bug.cgi?id=66609
    Reviewed by James Robinson.
    
    The invalidateRect function does not appear to be used anywhere
    because the setNeedsDisplay function is used instead. Fix the Clang
    error by just removing this unused vestigial base class function.
    
    * platform/graphics/chromium/LayerChromium.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
@@ -109,7 +109,6 @@ public:
 
     void setNeedsDisplay(const FloatRect& dirtyRect);
     void setNeedsDisplay();
-    virtual void invalidateRect(const FloatRect& dirtyRect) {}
     const FloatRect& dirtyRect() const { return m_dirtyRect; }
     void resetNeedsDisplay();
 
