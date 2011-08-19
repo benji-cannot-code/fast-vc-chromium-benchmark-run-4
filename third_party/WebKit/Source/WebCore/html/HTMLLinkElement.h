0b5882d9f88bf8d06b8bc9d16b131e6c471d5c5c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-19 21:37:27 +0000

    Unreviewed, rolling out r93425. http://trac.webkit.org/changeset/93425 https://bugs.webkit.org/show_bug.cgi?id=66591
    "The newly-added ASSERT is being triggered on several bots"
    (Requested by jchaffraix on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-19
    
    Source/WebCore:
    
    * html/HTMLLinkElement.cpp:
    (WebCore::HTMLLinkElement::HTMLLinkElement):
    (WebCore::HTMLLinkElement::setDisabled):
    (WebCore::HTMLLinkElement::sheetLoaded):
    (WebCore::HTMLLinkElement::disabled):
    * html/HTMLLinkElement.h:
    (WebCore::HTMLLinkElement::isEnabledViaScript):
    
    LayoutTests:
    
    * fast/css/link-disabled-attr-expected.txt:
    * fast/css/stylesheet-enable-first-alternate-expected.txt: Removed.
    * fast/css/stylesheet-enable-first-alternate-on-load-expected.txt: Removed.
    * fast/css/stylesheet-enable-first-alternate-on-load.html: Removed.
    * fast/css/stylesheet-enable-first-alternate.html: Removed.
    * fast/css/stylesheet-enable-second-alternate-expected.txt: Removed.
    * fast/css/stylesheet-enable-second-alternate-on-load-expected.txt: Removed.
    * fast/css/stylesheet-enable-second-alternate-on-load.html: Removed.
    * fast/css/stylesheet-enable-second-alternate.html: Removed.
    * http/tests/css/link-css-disabled-value-with-slow-loading-sheet-expected.txt: Removed.
    * http/tests/css/link-css-disabled-value-with-slow-loading-sheet-in-error-expected.txt: Removed.
    * http/tests/css/link-css-disabled-value-with-slow-loading-sheet-in-error.html: Removed.
    * http/tests/css/link-css-disabled-value-with-slow-loading-sheet.html: Removed.
    * http/tests/css/resources/link-css-disabled-value-with-slow-loading-sheet-in-error.js: Removed.
    * http/tests/css/resources/link-css-disabled-value-with-slow-loading-sheet.js: Removed.
    * http/tests/css/resources/slow-loading-sheet-in-error.php: Removed.
    * http/tests/css/resources/slow-loading-sheet.php: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLLinkElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLLinkElement.h
@@ -58,7 +58,7 @@ public:
 
     // FIXME: This should be renamed isStyleSheetLoading as this is only used for stylesheets.
     bool isLoading() const;
-    bool isEnabledViaScript() const { return m_scriptState == EnabledViaScript; }
+    bool isEnabledViaScript() const { return m_isEnabledViaScript; }
     bool disabled() const;
     void setDisabled(bool);
     void setSizes(const String&);
@@ -86,6 +86,7 @@ private:
     
     virtual bool isURLAttribute(Attribute*) const;
 
+private:
     virtual void addSubresourceAttributeURLs(ListHashSet<KURL>&) const;
 
     virtual void finishParsingChildren();
@@ -97,11 +98,6 @@ private:
 private:
     HTMLLinkElement(const QualifiedName&, Document*, bool createdByParser);
 
-    void setIsEnabledViaScript(bool enabled) { m_scriptState = enabled ? EnabledViaScript : DisabledViaScript; }
-#ifndef NDEBUG
-    bool areDisabledAndScriptStatesConsistent() const;
-#endif
-
     LinkLoader m_linkLoader;
     CachedResourceHandle<CachedCSSStyleSheet> m_cachedSheet;
     RefPtr<CSSStyleSheet> m_sheet;
@@ -111,11 +107,10 @@ private:
     RefPtr<DOMSettableTokenList> m_sizes;
     LinkRelAttribute m_relAttribute;
     bool m_loading;
-    enum EnabledViaScriptState { Unset, EnabledViaScript, DisabledViaScript };
-    EnabledViaScriptState m_scriptState;
+    bool m_isEnabledViaScript;
     bool m_createdByParser;
     bool m_isInShadowTree;
-
+    
     PendingSheetType m_pendingSheetType;
 };
 
