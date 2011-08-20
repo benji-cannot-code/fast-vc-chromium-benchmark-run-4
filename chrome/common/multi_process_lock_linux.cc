8e4c95b285ba412ce409e1c9a8256bb19c401c1d
abeera@google.com <abeera@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-20 02:15:22 +0000

    Bring out cloud print proxy from behind a flag on Linux.
    BUG=NONE
    TEST=The cloud print proxy is available under options on Linux without the command line flag.
    
    
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=93683
    
    Review URL: http://codereview.chromium.org/7277018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/multi_process_lock_linux.cc
+++ chrome/common/multi_process_lock_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -78,9 +78,9 @@ class MultiProcessLockLinux : public MultiProcessLock {
       fd_ = socket_fd;
       return true;
     } else {
-      PLOG(ERROR) << "Couldn't bind socket - "
-                  << &(address.sun_path[1])
-                  << " Length: " << length;
+      VLOG(1) << "Couldn't bind socket - "
+              << &(address.sun_path[1])
+              << " Length: " << length;
       if (HANDLE_EINTR(close(socket_fd)) < 0) {
         PLOG(ERROR) << "close";
       }
