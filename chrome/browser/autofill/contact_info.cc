ff9bcd77fc354a90522370da5d22f40259615f2e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-20 01:10:48 +0000

    autofill: Get rid of a static variable of string16 type as it's no longer used.
    BUG=83856
    TEST=None
    
    R=dhollowa@chromium.org,isherman@chromium.org
    
    Review URL: http://codereview.chromium.org/7696006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97541 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/contact_info.cc
+++ chrome/browser/autofill/contact_info.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/autofill_type.h"
 #include "chrome/browser/autofill/field_types.h"
 
-static const string16 kNameSplitChars = ASCIIToUTF16("-'. ");
-
 static const AutofillFieldType kAutofillNameInfoTypes[] = {
   NAME_FIRST,
   NAME_MIDDLE,
