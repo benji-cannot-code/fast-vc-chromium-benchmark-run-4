6a4d7cc79529ed67cbb4a8ae30a897aa73020c0e
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-20 06:38:22 +0000

    Add the functionality to inject JavaScript into ExtensionHosts.
    BUG=chromium-os:17902
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7637020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -783,6 +783,7 @@ void AutomationProvider::InstallExtension(
     // The observer will delete itself when done.
     new ExtensionReadyNotificationObserver(
         manager,
+        service,
         this,
         AutomationMsg_InstallExtension::ID,
         reply_message);
@@ -828,6 +829,7 @@ void AutomationProvider::EnableExtension(int extension_handle,
     // The observer will delete itself when done.
     new ExtensionReadyNotificationObserver(
         manager,
+        service,
         this,
         AutomationMsg_EnableExtension::ID,
         reply_message);
