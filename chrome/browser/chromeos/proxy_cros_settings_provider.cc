873dc4fe3590f7d3754806c0737a2e83bdd8cc6b
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 16:21:15 +0000

    chromeos proxy ui enhancements - implement checking of policy/extension-managed proxy in ui and display reason when network proxy is not configurable. - update instructions on how to disable proxy server when it's is not reachable - update strings for proxy-related settings
    BUG=chromium-os:18010,chromium-os:8513,chromium-os:18108
    TEST=verify per bug reports.
    Review URL: http://codereview.chromium.org/7644001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/proxy_cros_settings_provider.cc
+++ chrome/browser/chromeos/proxy_cros_settings_provider.cc
@@ -5,12 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/proxy_cros_settings_provider.h"
 
-#include "base/command_line.h"
 #include "base/string_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/cros_settings.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/common/chrome_switches.h"
 
 namespace chromeos {
 
