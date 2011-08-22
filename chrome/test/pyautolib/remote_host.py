eb36f6745894d0db12b31a0ed81a0413e422f8ea
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 22:08:25 +0000

    Adds a PyAuto test, chromoting_basic.ChromotingBasic.testChromoting. This test installs and launches the Chromoting extension on two machines, authenticates (with help from a user), establishes a Chromoting connection, and verifies that the Chromoting share started.
    Adds a dependency, ChromotingMixIn, that contains Chromoting-specific methods. A PyUITest can add the MixIn as a base class to gain the additional functionality. ChromotingBasic is an example of this.
    
    Adds support for MixIns to PyAuto Remote. Any MixIns will be automatically shipped over to the RemoteHost, making its functionality available on both machines.
    
    Adds support for multiple RemoteHosts. You can connect to them by doing python test_name.py --remote-host=ip1,ip2,ip3
    
    Additional tweaks to PyAuto Remote to make it more palatable for MixIns.
    
    BUG=chromium:92698,chromium:86890,chromium:92702
    TEST=Run in first terminal/machine: "python chrome/test/functional/remote_host_functional.py". Run in second terminal: "python chrome/test/functional/chromoting_basic.py --remote-host=127.0.0.1"
    
    Review URL: http://codereview.chromium.org/7648046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/remote_host.py
+++ chrome/test/pyautolib/remote_host.py
@@ -5,14 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import cStringIO
+import os
 import pickle
 import socket
 import sys
 
 import pyauto
 
-
-class RemoteHost(pyauto.PyUITest):
+class RemoteHost(object):
   """Class used as a host for tests that use the PyAuto RemoteProxy.
 
   This class fires up a listener which waits for a connection from a RemoteProxy
@@ -21,23 +21,7 @@ class RemoteHost(pyauto.PyUITest):
   can connect to and automate using pyauto.RemoteProxy.
   """
   def __init__(self, *args, **kwargs):
-    pyauto.PyUITest.__init__(self, *args, **kwargs)
     self.StartSocketServer()
-
-  # We give control of setUp and tearDown to the RemoteProxy, so disable them.
-  # The RemoteProxy can call RemoteSetUp() and RemoteTearDown() to do the normal
-  # setUp() and tearDown().
-  def setUp(self):
-    pass
-
-  def tearDown(self):
-    pass
-
-  def RemoteSetUp(self):
-    pyauto.PyUITest.setUp(self)
-
-  def RemoteTearDown(self):
-    pyauto.PyUITest.tearDown(self)
 
   def StartSocketServer(self, port=7410):
     listening_socket = socket.socket()
@@ -45,6 +29,9 @@ class RemoteHost(pyauto.PyUITest):
     listening_socket.bind(('', port))
     listening_socket.listen(1)
     self._socket, _ = listening_socket.accept()
+
+    while self.Connected():
+      self._HandleRPC()
 
   def StopSocketServer(self):
     if self._socket:
@@ -54,6 +41,13 @@ class RemoteHost(pyauto.PyUITest):
 
   def Connected(self):
     return self._socket
+
+  def CreateTarget(self, target_class):
+    """Creates an instance of the specified class to serve as the RPC target.
+
+    RPC calls can be made on the target.
+    """
+    self.target = target_class()
 
   def _HandleRPC(self):
     """Receives a method call request over the socket and executes the method.
@@ -79,7 +73,10 @@ class RemoteHost(pyauto.PyUITest):
     result = None
     exception = None
     try:
-      result = getattr(self, request[0])(*request[1], **request[2])
+      if getattr(self, request[0], None):
+        result = getattr(self, request[0])(*request[1], **request[2])
+      else:
+        result = getattr(self.target, request[0])(*request[1], **request[2])
     except BaseException as e:
       exception = (e.__class__.__name__, str(e))
 
@@ -92,11 +89,3 @@ class RemoteHost(pyauto.PyUITest):
                              exception))
     if self._socket.send(response) != len(response):
       self.StopSocketServer()
-
-  def RunHost(self):
-    while self.Connected():
-      self._HandleRPC()
-
-
-if __name__ == '__main__':
-  pyauto.Main()
