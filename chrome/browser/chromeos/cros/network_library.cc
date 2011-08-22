c3e311f93855b00c3423adbe2dc6fb73f1599102
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 17:00:59 +0000

    Initialize/Shutdown CrosLibrary explicitly to avoid singleton races during shutdown.
    BUG=chromium-os:18912
    TEST=Ensure chrome shuts down cleanly after heavy usage. Include opening Other Wi-F > Advanced.
    
    Review URL: http://codereview.chromium.org/7672005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -120,8 +120,7 @@ static void WipeString(std::string* str) {
 }
 
 static bool EnsureCrosLoaded() {
-  if (!CrosLibrary::Get()->EnsureLoaded() ||
-      !CrosLibrary::Get()->GetNetworkLibrary()->IsCros()) {
+  if (!CrosLibrary::Get()->LibraryLoaded()) {
     return false;
   } else {
     CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI))
@@ -3100,6 +3099,8 @@ NetworkLibraryImplCros::~NetworkLibraryImplCros() {
 }
 
 void NetworkLibraryImplCros::Init() {
+  CHECK(CrosLibrary::Get()->LibraryLoaded())
+      << "libcros must be loaded before NetworkLibraryImplCros::Init()";
   // First, get the currently available networks. This data is cached
   // on the connman side, so the call should be quick.
   VLOG(1) << "Requesting initial network manager info from libcros.";
@@ -4599,18 +4600,23 @@ void NetworkLibraryImplStub::SetIPConfig(const NetworkIPConfig& ipconfig) {
 
 /////////////////////////////////////////////////////////////////////////////
 
+// In NetworkLibrary, rather than check each call to libcros (calls prefixed
+// here with explicit chromeos::), we check to see whether or not libcros was
+// actually loaded. If not, we return a stub implementation instead.
+// This is for UI and Browser tests that require more functionality than the
+// other CrosLibrary modules provide in their stub implementation.
+// TODO(stevenjb): Fix this to be consistent across all CrosLibrary modules.
+
 // static
 NetworkLibrary* NetworkLibrary::GetImpl(bool stub) {
-  // Use static global to avoid recursive GetImpl() call from EnsureCrosLoaded.
-  static NetworkLibrary* network_library = NULL;
-  if (network_library == NULL) {
-    if (stub || !CrosLibrary::Get()->EnsureLoaded())
-      network_library = new NetworkLibraryImplStub();
-    else
-      network_library = new NetworkLibraryImplCros();
-    network_library->Init();
-  }
-  return network_library;
+  NetworkLibrary* impl;
+  if (stub || !CrosLibrary::Get()->LibraryLoaded())
+    impl = new NetworkLibraryImplStub();
+  else
+    impl = new NetworkLibraryImplCros();
+  impl->Init();
+
+  return impl;
 }
 
 /////////////////////////////////////////////////////////////////////////////
