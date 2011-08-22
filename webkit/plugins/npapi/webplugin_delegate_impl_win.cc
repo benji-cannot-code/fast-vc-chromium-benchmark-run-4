22104a109cb527b212d2f9ea9570fdc753289a21
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 23:41:23 +0000

    Cache keyState for windowless Flash plugins.
    BUG=93817
    TEST=Play Robot Unicorn Attack.
    Review URL: http://codereview.chromium.org/7709014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97774 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugin_delegate_impl_win.cc
+++ webkit/plugins/npapi/webplugin_delegate_impl_win.cc
@@ -1184,6 +1184,9 @@ bool WebPluginDelegateImpl::PlatformSetPluginHasFocus(bool focused) {
   focus_event.wParam = 0;
   focus_event.lParam = 0;
 
+  if (GetQuirks() & PLUGIN_QUIRK_PATCH_GETKEYSTATE)
+    ClearSavedKeyState();
+
   instance()->NPP_HandleEvent(&focus_event);
   return true;
 }
@@ -1301,6 +1304,13 @@ bool WebPluginDelegateImpl::PlatformHandleInputEvent(
   NPEvent np_event;
   if (!NPEventFromWebInputEvent(event, &np_event)) {
     return false;
+  }
+
+  if (GetQuirks() & PLUGIN_QUIRK_PATCH_GETKEYSTATE) {
+    if (np_event.event == WM_KEYDOWN)
+      SetSavedKeyState(np_event.wParam);
+    else if (np_event.event == WM_KEYUP)
+      UnsetSavedKeyState(np_event.wParam);
   }
 
   HWND last_focus_window = NULL;
