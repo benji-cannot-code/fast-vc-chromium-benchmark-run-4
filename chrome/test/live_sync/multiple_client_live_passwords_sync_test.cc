68bfa4c5e00013a0c8efb7c3819eafd9b327014f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 03:59:53 +0000

    sync: Convert two FLAKY_ tests to DISABLED_ since they time out.
    Tests that time out turn the bots red, so they need to be disabled.
    See http://build.chromium.org/p/tryserver.chromium/builders/mac_sync/builds/4549/
    
    BUG=77993
    TEST=none
    TBR=rsimha
    
    
    Review URL: http://codereview.chromium.org/7704002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/live_sync/multiple_client_live_passwords_sync_test.cc
+++ chrome/test/live_sync/multiple_client_live_passwords_sync_test.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using webkit_glue::PasswordForm;
 
 // TODO(rsimha): Enable after crbug.com/77993 is fixed.
-IN_PROC_BROWSER_TEST_F(MultipleClientLivePasswordsSyncTest, FLAKY_Sanity) {
+IN_PROC_BROWSER_TEST_F(MultipleClientLivePasswordsSyncTest, DISABLED_Sanity) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   for (int i = 0; i < num_clients(); ++i) {
