811183a3cfa5183d3da2cdf6fc074e825bf144cb
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 22:17:40 +0000

    Add base class for generators
    This CL is the first step towards alowing multiple generators
    with a single driver to simplify the use of the generator.
    It removes old built-in in idl_c_header.py, and replaces it with
    several golden files to verify output is unchanged.
    It renames the old 'version' option to 'release' to disambiguate
    between a RELEASE name such as M14, and an interface VERSION such
    as 1.0.
    
    Removes the 'M14' hack which as added to the source code to force
    release M14 to be generated.
    
    
    TEST= ./generator.py
    BUG= http://code.google.com/p/chromium/issues/detail?id=91607
    R=sehr@google.com
    Review URL: http://codereview.chromium.org/7477056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97752 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_c_proto.py
+++ ppapi/generators/idl_c_proto.py
@@ -106,13 +106,13 @@ class CGen(object):
     self.dbg_depth = 0
     self.vmin = 0.0
     self.vmax = 1e100
-    self.release = 'M14'
+    self.release = GetOption('release')
 
   def SetVersionMap(self, node):
     self.vmin = 0.0
     self.vmax = 1e100
     for version in node.GetListOf('LabelItem'):
-      if version.GetName() == GetOption('version'):
+      if version.GetName() == GetOption('release'):
         self.vmin = float(version.GetProperty('VALUE'))
         self.vmax = float(version.GetProperty('VALUE'))
 
@@ -386,7 +386,6 @@ class CGen(object):
 
   def InterfaceDefs(self, node):
     out = ''
-    release = 'M14'
     name = node.GetName()
     macro = node.GetProperty('macro')
     if not macro:
@@ -397,7 +396,7 @@ class CGen(object):
         strver = str(vers).replace('.', '_')
         out += self.GetDefine('%s_%s' % (macro, strver),
                               '"%s;%s"' % (name, vers))
-        if label.GetRelease(vers) == release:
+        if label.GetRelease(vers) == self.release:
           out += self.GetDefine(macro, '%s_%s' % (macro, strver))
       out += '\n'
     return out
@@ -479,7 +478,7 @@ class CGen(object):
 
       label = node.GetLabel()
       if label:
-        lver = label.GetVersion('M14')
+        lver = label.GetVersion(self.release)
 
         # Verify that we are in a valid version.
         if node_max <= lver: return ''
