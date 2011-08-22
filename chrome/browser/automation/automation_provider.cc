05bc1bfc95e6d00b66af3919c8a83377e2b6e937
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 08:34:17 +0000

    Revert 97557 - [chromedriver] Add chrome.detach option for configuring Chrome not to quit when its automation client disconnects. chrome.detach is currently implemented by using a NamedProxyLauncher. We also add a new switch that accomplishes the same purpose. Also, modify pre-post command execution steps in two ways: 1) wait for loads post execution, in case pyauto issues a non-webdriver command which expects the load to be finished 2) only  return a frame checking error if no alert is present
    Reverting because of chromedriver_tests failure on windows.
    http://build.chromium.org/p/chromium.pyauto/builders/Win%20XP/builds/9846/steps/chromedriver_tests/logs/stdio
    
    ERROR: chromedriver_tests.DetachProcessTest.testDetachProcess: "None"
    ----------------------------------------------------------------------
    Traceback (most recent call last):
        File
        "E:\b\build\slave\Win_XP\build\src\chrome\test\webdriver\test\chromedriver_tests.py",
        line 357, in testDetachProcess
            os.kill(pid, 0)  # Would throw if process no longer exists
        AttributeError: 'module' object has no attribute 'kill'
    
    Feel free to recommit after fix (no review necessary)
    
    
    BUG=87676,93438
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7648053
    
    TBR=kkania@chromium.org
    Review URL: http://codereview.chromium.org/7696022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97611 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/callback.h"
-#include "base/command_line.h"
 #include "base/debug/trace_event.h"
 #include "base/file_path.h"
 #include "base/json/json_reader.h"
@@ -107,9 +106,7 @@ using base::Time;
 AutomationProvider::AutomationProvider(Profile* profile)
     : profile_(profile),
       reply_message_(NULL),
-      reinitialize_on_channel_error_(
-          CommandLine::ForCurrentProcess()->HasSwitch(
-              switches::kAutomationReinitializeOnChannelError)),
+      reinitialize_on_channel_error_(false),
       is_connected_(false),
       initial_tab_loads_complete_(false),
       network_library_initialized_(true) {
