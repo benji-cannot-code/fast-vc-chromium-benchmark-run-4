263dc5540dc6dd0067a43f7c797a1c82a7a91f50
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-22 22:48:18 +0000

    Moved the handling of the IPC message ViewMsg_EnableViewSourceMode to content (RenderView) as all code which handles this message lives in content.
    This CL is the first of many changes to ensure that IPCs don't span across content and chrome.
    
    BUG=87335
    Review URL: http://codereview.chromium.org/7712009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97763 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view.cc
+++ content/renderer/render_view.cc
@@ -712,6 +712,7 @@ bool RenderView::OnMessageReceived(const IPC::Message& message) {
                         OnUpdateRemoteAccessClientFirewallTraversal)
     IPC_MESSAGE_HANDLER(ViewMsg_SetHistoryLengthAndPrune,
                         OnSetHistoryLengthAndPrune)
+    IPC_MESSAGE_HANDLER(ViewMsg_EnableViewSourceMode, OnEnableViewSourceMode)
 
     // Have the super handle all other messages.
     IPC_MESSAGE_UNHANDLED(handled = RenderWidget::OnMessageReceived(message))
@@ -4559,3 +4560,13 @@ void RenderView::OnContextMenuClosed(
   else
     context_menu_node_.reset();
 }
+
+void RenderView::OnEnableViewSourceMode() {
+  if (!webview())
+    return;
+  WebFrame* main_frame = webview()->mainFrame();
+  if (!main_frame)
+    return;
+  main_frame->enableViewSourceMode(true);
+}
+
