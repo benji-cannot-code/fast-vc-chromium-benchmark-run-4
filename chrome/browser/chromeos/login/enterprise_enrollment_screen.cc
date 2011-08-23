096bebf06c4d0dc327eb310351b994e1c51a6621
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 09:58:45 +0000

    Fix editable_user switching for GAIA login.
    BUG=chromium-os:18208
    TEST=Trigger (old-style) enterprise enrollment against a server that errors out on the policy request, verify that the username gets disabled properly.
    
    
    Review URL: http://codereview.chromium.org/7671021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97843 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/enterprise_enrollment_screen.cc
+++ chrome/browser/chromeos/login/enterprise_enrollment_screen.cc
@@ -124,7 +124,6 @@ bool EnterpriseEnrollmentScreen::GetInitialUser(std::string* user) {
         // this means we might only want to reenroll with the DMServer so lock
         // the username to what has been stored in the InstallAttrs already.
         *user = value;
-        actor_->SetEditableUser(false);
         return true;
       }
     }
@@ -282,6 +281,7 @@ void EnterpriseEnrollmentScreen::WriteInstallAttributesData() {
 
   switch (g_browser_process->browser_policy_connector()->LockDevice(user_)) {
     case policy::EnterpriseInstallAttributes::LOCK_SUCCESS: {
+      actor_->SetEditableUser(false);
       // Proceed with policy fetch.
       policy::BrowserPolicyConnector* connector =
           g_browser_process->browser_policy_connector();
