f530eb14c7d45ba197c9d046f31ba8dae8156718
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 13:36:35 +0000

    Unreviewed, rolling out r93579. http://trac.webkit.org/changeset/93579 https://bugs.webkit.org/show_bug.cgi?id=66774
    "ASSERTION FAILED: m_purgePreventCount" (Requested by tonyg-cr
    on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-23
    
    * platform/graphics/chromium/FontCacheLinux.cpp:
    (WebCore::FontCache::getFontDataForCharacters):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/FontCacheLinux.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/FontCacheLinux.cpp
@@ -65,7 +65,7 @@ const SimpleFontData* FontCache::getFontDataForCharacters(const Font& font,
         return 0;
 
     AtomicString atomicFamily(family);
-    return getCachedFontData(getCachedFontPlatformData(font.fontDescription(), atomicFamily, DoNotRetain), DoNotRetain);
+    return getCachedFontData(getCachedFontPlatformData(font.fontDescription(), atomicFamily, DoNotRetain));
 }
 
 SimpleFontData* FontCache::getSimilarFontPlatformData(const Font& font)
