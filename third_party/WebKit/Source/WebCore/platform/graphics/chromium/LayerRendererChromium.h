553207c320307a94cbed409267dfe93bfe4ee335
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 01:35:48 +0000

    [chromium] Remove compositing assertions about creating layers when painting https://bugs.webkit.org/show_bug.cgi?id=66731
    Reviewed by James Robinson.
    
    The trigger for this assertion would cause a real rendering issue, but
    it is both intermittent and in the wrong place in the codebase to
    catch the source of the problem.
    
    * platform/graphics/chromium/LayerChromium.cpp:
    (WebCore::LayerChromium::LayerChromium):
    (WebCore::LayerChromium::~LayerChromium):
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::updateLayers):
    * platform/graphics/chromium/LayerRendererChromium.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerRendererChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerRendererChromium.h
@@ -156,9 +156,6 @@ public:
 
     GC3Denum bestTextureFormat();
 
-#ifndef NDEBUG
-    static bool s_inPaintLayerContents;
-#endif
     typedef Vector<RefPtr<LayerChromium> > LayerList;
     typedef Vector<RefPtr<CCLayerImpl> > CCLayerList;
 
