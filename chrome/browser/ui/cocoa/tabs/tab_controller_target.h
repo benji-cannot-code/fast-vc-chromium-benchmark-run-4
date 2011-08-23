d38cc87f7ef92f7008327ca6ae6b224028846cd3
bashi@chromium.org <bashi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 07:09:32 +0000

    Clicking tab close with option key close the other tabs.
    BUG=91319
    TEST=Open several tabs and click the close button of one of them with option key. Closes other tabs.
    TEST=Open several tabs and click the close button of one of them without option key. Closes the tab.
    
    
    Review URL: http://codereview.chromium.org/7565007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_controller_target.h
+++ chrome/browser/ui/cocoa/tabs/tab_controller_target.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol TabControllerTarget
 - (void)selectTab:(id)sender;
 - (void)closeTab:(id)sender;
+- (void)closeOtherTabs:(id)sender;
 
 // Dispatch context menu commands for the given tab controller.
 - (void)commandDispatch:(TabStripModel::ContextMenuCommand)command
