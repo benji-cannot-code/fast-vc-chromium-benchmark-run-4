a9cc85b35f24503eb7426a11ef03c895530070b5
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 15:35:58 +0000

    Web Inspector: [Extensions API] provide access to page resources https://bugs.webkit.org/show_bug.cgi?id=66767
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Test: inspector/extensions/extensions-resources.html
    
    * inspector/front-end/ExtensionAPI.js:
    (WebInspector.injectedExtensionAPI.EventSinkImpl.prototype.addListener):
    (WebInspector.injectedExtensionAPI):
    (WebInspector.injectedExtensionAPI.RequestImpl.prototype.getContent):
    (WebInspector.injectedExtensionAPI.InspectedWindow.resourceDispatch):
    (WebInspector.injectedExtensionAPI.InspectedWindow.resourceContentDispatch):
    (WebInspector.injectedExtensionAPI.InspectedWindow):
    (WebInspector.injectedExtensionAPI.InspectedWindow.prototype.eval):
    (WebInspector.injectedExtensionAPI.InspectedWindow.prototype.getResources):
    (WebInspector.injectedExtensionAPI.InspectedWindow.prototype.getResources.callbackWrapper):
    (WebInspector.injectedExtensionAPI.ResourceImpl.prototype.get url):
    (WebInspector.injectedExtensionAPI.ResourceImpl.prototype.get type):
    (WebInspector.injectedExtensionAPI.ResourceImpl.prototype.getContent):
    (WebInspector.injectedExtensionAPI.ResourceImpl.prototype.setContent):
    * inspector/front-end/ExtensionServer.js:
    (WebInspector.ExtensionServer):
    (WebInspector.ExtensionServer.prototype._notifyResourceAdded):
    (WebInspector.ExtensionServer.prototype.notifyResourceContentCommitted):
    (WebInspector.ExtensionServer.prototype._makeResource):
    (WebInspector.ExtensionServer.prototype._onGetPageResources):
    (WebInspector.ExtensionServer.prototype._getResourceContent):
    (WebInspector.ExtensionServer.prototype._onGetRequestContent):
    (WebInspector.ExtensionServer.prototype._onGetResourceContent):
    (WebInspector.ExtensionServer.prototype._onSetResourceContent):
    (WebInspector.ExtensionServer.prototype.initExtensions):
    * inspector/front-end/Resource.js:
    (WebInspector.Resource.prototype.addRevision):
    
    LayoutTests:
    
    * http/tests/inspector/resources/extension-main.js:
    (trimURL):
    * inspector/extensions/extensions-api-expected.txt:
    * inspector/extensions/extensions-network-expected.txt:
    * inspector/extensions/extensions-network.html:
    * inspector/extensions/extensions-resources-expected.txt: Added.
    * inspector/extensions/extensions-resources.html: Added.
    * inspector/extensions/resources/Ahem.ttf:
    * inspector/extensions/resources/test-script.js: Added.
    (__test_function):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/resources/extension-main.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/resources/extension-main.js
@@ -1,4 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function trimURL(url)
+{
+    if (/^data:/.test(url))
+        return url.replace(/;.*$/, "...");
+    return url.replace(/.*\//, ".../");
+}
+
 function dumpObject(object, nondeterministicProps, prefix, firstLinePrefix)
 {
     prefix = prefix || "";
@@ -7,9 +14,10 @@ function dumpObject(object, nondeterministicProps, prefix, firstLinePrefix)
     for (var prop in object) {
         var prefixWithName = prefix + "    " + prop + " : ";
         var propValue = object[prop];
-        if (nondeterministicProps && prop in nondeterministicProps)
-            output(prefixWithName + "<" + typeof propValue + ">");
-        else if (propValue === null)
+        if (nondeterministicProps && prop in nondeterministicProps) {
+            var value = nondeterministicProps[prop] === "url" ? trimURL(propValue) : "<" + typeof propValue + ">";
+            output(prefixWithName + value);
+        } else if (propValue === null)
             output(prefixWithName + "null");
         else if (typeof propValue === "object")
             dumpObject(propValue, nondeterministicProps, prefix + "    ", prefixWithName);
--- third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
@@ -834,7 +834,7 @@ WebInspector.Resource.prototype = {
 
         if (!restoringHistory)
             this._persistRevision();
-        WebInspector.extensionServer.notifyResourceContentEdited(this.url, newContent);
+        WebInspector.extensionServer.notifyResourceContentCommitted(this, newContent);
     },
 
     _persistRevision: function()
