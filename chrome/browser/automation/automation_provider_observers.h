b788750081fb45f95f18c48e1c2aca45258de2d5
frankf@google.com <frankf@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 18:42:17 +0000

    Add the functionality to inject JavaScript into ExtensionHosts.
    BUG=chromium-os:17902
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=97569
    
    Review URL: http://codereview.chromium.org/7637020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97893 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_observers.h
+++ chrome/browser/automation/automation_provider_observers.h
@@ -58,6 +58,7 @@ class BalloonCollection;
 class Browser;
 class Extension;
 class ExtensionProcessManager;
+class ExtensionService;
 class NavigationController;
 class Profile;
 class RenderViewHost;
@@ -314,6 +315,7 @@ class ExtensionUninstallObserver : public NotificationObserver {
 class ExtensionReadyNotificationObserver : public NotificationObserver {
  public:
   ExtensionReadyNotificationObserver(ExtensionProcessManager* manager,
+                                     ExtensionService* service,
                                      AutomationProvider* automation,
                                      int id,
                                      IPC::Message* reply_message);
@@ -327,6 +329,7 @@ class ExtensionReadyNotificationObserver : public NotificationObserver {
  private:
   NotificationRegistrar registrar_;
   ExtensionProcessManager* manager_;
+  ExtensionService* service_;
   base::WeakPtr<AutomationProvider> automation_;
   int id_;
   scoped_ptr<IPC::Message> reply_message_;
