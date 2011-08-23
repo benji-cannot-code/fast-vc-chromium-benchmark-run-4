c57c74d47b07f62e74b9fac591fc671e5b24e49a
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 12:14:24 +0000

    Revert 97808 - Make WeakPtr thread-safe, i.e. allow cross-thread copying of WeakPtr and destruction on a thread other than the one where the original reference was created.
    The problem with the current implementation is modifying the flag pointer
    from WeakPtr::~WeakPtr which might happen on a different thread (potentially
    racing with somebody invalidating the flag on the original thread).
    
    For compatibility reasons, creating the initial reference attaches to the
    thread and governs the thread-safety checking logic with respect to checking
    a reference to be valid and invalidating it, which should both only be done
    on the same thread.
    
    BUG=82509
    TEST=added unit tests
    
    Review URL: http://codereview.chromium.org/7677028
    
    TBR=sievers@chromium.org
    Review URL: http://codereview.chromium.org/7685054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97846 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/weak_ptr.cc
+++ base/memory/weak_ptr.cc
@@ -8,28 +8,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
-WeakReference::Flag::Flag() : is_valid_(true) {
+WeakReference::Flag::Flag(Flag** handle) : handle_(handle) {
 }
 
 void WeakReference::Flag::Invalidate() {
-  // The flag being invalidated with a single ref implies that there are no
-  // weak pointers in existence. Allow deletion on other thread in this case.
-  DCHECK(thread_checker_.CalledOnValidThread() || HasOneRef());
-  is_valid_ = false;
+  DCHECK(thread_checker_.CalledOnValidThread());
+  handle_ = NULL;
 }
 
 bool WeakReference::Flag::IsValid() const {
   DCHECK(thread_checker_.CalledOnValidThread());
-  return is_valid_;
+  return handle_ != NULL;
 }
 
 WeakReference::Flag::~Flag() {
+  if (handle_)
+    *handle_ = NULL;
 }
 
 WeakReference::WeakReference() {
 }
 
-WeakReference::WeakReference(const Flag* flag) : flag_(flag) {
+WeakReference::WeakReference(Flag* flag) : flag_(flag) {
 }
 
 WeakReference::~WeakReference() {
@@ -39,7 +39,7 @@ bool WeakReference::is_valid() const {
   return flag_ && flag_->IsValid();
 }
 
-WeakReferenceOwner::WeakReferenceOwner() {
+WeakReferenceOwner::WeakReferenceOwner() : flag_(NULL) {
 }
 
 WeakReferenceOwner::~WeakReferenceOwner() {
@@ -47,10 +47,8 @@ WeakReferenceOwner::~WeakReferenceOwner() {
 }
 
 WeakReference WeakReferenceOwner::GetRef() const {
-  // We also want to reattach to the current thread if all previous references
-  // have gone away.
-  if (!HasRefs())
-    flag_ = new WeakReference::Flag();
+  if (!flag_)
+    flag_ = new WeakReference::Flag(&flag_);
   return WeakReference(flag_);
 }
 
