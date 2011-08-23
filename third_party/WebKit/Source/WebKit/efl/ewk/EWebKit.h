e4a6f46167aa0880aabb00f44e2e2767c36bae8e
demarchi@webkit.org <demarchi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 21:01:52 +0000

    Source/WebKit: Patch by Jonas M. Gastal <jgastal@profusion.mobi> on 2011-08-23 Reviewed by Antonio Gomes.
    [EFL] Exporting JavaScript objects for EFL port
    https://bugs.webkit.org/show_bug.cgi?id=43446
    
    This patch allows to export objects to JavaScript API,
    just like qt does, but it uses the netscape api
    to do the convertion to javascript object.
    
    * CMakeLists.txt:
    Adding necessary files for the patch.
    
    Source/WebKit/efl: [EFL] Exporting JavaScript objects for EFL port
    https://bugs.webkit.org/show_bug.cgi?id=43446
    
    Patch by Jonas M. Gastal <jgastal@profusion.mobi> on 2011-08-23
    Reviewed by Antonio Gomes.
    
    This patch allows to export objects to JavaScript API,
    just like qt does, but it uses the netscape api
    to do the convertion to javascript object.
    
    * CMakeLists.txt:
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    (WebCore::FrameLoaderClientEfl::dispatchDidClearWindowObjectInWorld):
    * ewk/EWebKit.h:
    * ewk/ewk_js.cpp: Added.
    (ewk_js_variant_to_npvariant):
    (ewk_js_npvariant_to_variant):
    (ewk_js_property_has):
    (ewk_js_property_get):
    (ewk_js_property_set):
    (ewk_js_property_remove):
    (ewk_js_properties_enumerate):
    (ewk_js_method_has):
    (ewk_js_method_invoke):
    (ewk_js_class_new):
    (ewk_js_class_free):
    (ewk_js_object_new):
    (ewk_js_object_free):
    (ewk_js_object_properties_get):
    (ewk_js_object_name_get):
    (ewk_js_variant_free):
    * ewk/ewk_js.h: Added.
    * ewk/ewk_main.cpp:
    (ewk_init):
    * ewk/ewk_private.h:
    * ewk/ewk_view.cpp:
    (ewk_view_js_window_object_clear):
    (ewk_view_js_object_add):
    * ewk/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/EWebKit.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/EWebKit.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_cookies.h"
 #include "ewk_frame.h"
 #include "ewk_history.h"
+#include "ewk_js.h"
 #include "ewk_main.h"
 #include "ewk_network.h"
 #include "ewk_settings.h"
