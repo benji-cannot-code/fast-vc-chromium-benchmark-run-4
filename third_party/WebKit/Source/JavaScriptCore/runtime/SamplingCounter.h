f14597abe024eb82eb2ca39a7e40bae83e60d429
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 09:32:42 +0000

    2011-08-23  Ilya Tikhonovsky  <loislo@chromium.org>
            Unreviewed build fix for compile error on Windows for r93560.
    
            * runtime/SamplingCounter.h:
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/SamplingCounter.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/SamplingCounter.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SamplingCounter_h
 #define SamplingCounter_h
 
+#include <stdint.h>
 #include <wtf/Assertions.h>
 
 namespace JSC {
