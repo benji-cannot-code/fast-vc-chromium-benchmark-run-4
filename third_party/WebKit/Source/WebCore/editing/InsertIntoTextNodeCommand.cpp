a95640cefff7babfc36f3526f35430e39b86b56e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-23 23:31:44 +0000

    Added support for momentarily revealing last typed character in password input. Code change was partially based on Apple's iOS code and Samuel Nevala's work. https://bugs.webkit.org/show_bug.cgi?id=32509
    Patch by Chang Shu <cshu@webkit.org> on 2011-08-23
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    * editing/InsertIntoTextNodeCommand.cpp:
    (WebCore::InsertIntoTextNodeCommand::doApply):
    * rendering/RenderText.cpp:
    (WebCore::SecureTextTimer::SecureTextTimer):
    (WebCore::SecureTextTimer::restartWithNewText):
    (WebCore::SecureTextTimer::invalidate):
    (WebCore::SecureTextTimer::lastTypedCharacterOffset):
    (WebCore::SecureTextTimer::fired):
    (WebCore::RenderText::willBeDestroyed):
    (WebCore::RenderText::setTextInternal):
    (WebCore::RenderText::secureText):
    (WebCore::RenderText::momentarilyRevealLastTypedCharacter):
    * rendering/RenderText.h:
    (WebCore::RenderText::isSecure):
    * testing/Internals.cpp:
    (WebCore::Internals::setPasswordEchoEnabled): Fixed some silly coding in Internals.
    (WebCore::Internals::setPasswordEchoDurationInSeconds):
    (WebCore::Internals::reset):
    
    LayoutTests:
    
    Updated failed expected files.
    
    * editing/input/password-echo-passnode-expected.txt:
    * editing/input/password-echo-passnode2-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/InsertIntoTextNodeCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/InsertIntoTextNodeCommand.cpp
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AXObjectCache.h"
 #include "Document.h"
+#include "RenderText.h"
+#include "Settings.h"
 #include "Text.h"
 
 namespace WebCore {
@@ -48,7 +50,13 @@ void InsertIntoTextNodeCommand::doApply()
 {
     if (!m_node->rendererIsEditable())
         return;
-    
+
+    if (document()->settings() && document()->settings()->passwordEchoEnabled()) {
+        RenderText* renderText = toRenderText(m_node->renderer());
+        if (renderText && renderText->isSecure())
+            renderText->momentarilyRevealLastTypedCharacter(m_offset + m_text.length() - 1);
+    }
+
     ExceptionCode ec;
     m_node->insertData(m_offset, m_text, ec);
 
--- third_party/WebKit/Source/WebCore/testing/Internals.cpp
+++ third_party/WebKit/Source/WebCore/testing/Internals.cpp
@@ -227,7 +227,7 @@ void Internals::setPasswordEchoEnabled(Document* document, bool enabled, Excepti
     }
 
     if (!passwordEchoEnabledBackedUp) {
-        passwordEchoEnabledBackup = enabled;
+        passwordEchoEnabledBackup = document->settings()->passwordEchoEnabled();
         passwordEchoEnabledBackedUp = true;
     }
     document->settings()->setPasswordEchoEnabled(enabled);
@@ -241,7 +241,7 @@ void Internals::setPasswordEchoDurationInSeconds(Document* document, double dura
     }
 
     if (!passwordEchoDurationInSecondsBackedUp) {
-        passwordEchoDurationInSecondsBackup = durationInSeconds;
+        passwordEchoDurationInSecondsBackup = document->settings()->passwordEchoDurationInSeconds();
         passwordEchoDurationInSecondsBackedUp = true;
     }
     document->settings()->setPasswordEchoDurationInSeconds(durationInSeconds);
@@ -252,12 +252,16 @@ void Internals::reset(Document* document)
     if (!document || !document->settings())
         return;
 
-    if (passwordEchoDurationInSecondsBackedUp)
+    if (passwordEchoDurationInSecondsBackedUp) {
         document->settings()->setPasswordEchoDurationInSeconds(passwordEchoDurationInSecondsBackup);
+        passwordEchoDurationInSecondsBackedUp = false;
+    }
 
-    if (passwordEchoEnabledBackedUp)
-        document->settings()->setPasswordEchoDurationInSeconds(passwordEchoEnabledBackup);
+    if (passwordEchoEnabledBackedUp) {
+        document->settings()->setPasswordEchoEnabled(passwordEchoEnabledBackup);
+        passwordEchoEnabledBackedUp = false;
+    }
 }
 
-
 }
+
