5b86c3aa49082a97efaa25e4347caf7c75517a3f
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 05:30:49 +0000

    Do not call callback->Run() until we are about to return from this object because callback->Run() may cause this object to be deleted.
    Previously we were calling callback->Run() in DoWaitForDataReadyDone(),
    which returns to the do-while loop in DoLoop().  The do-while loop
    reads the state_ member. If the object was deleted in callback->Run(),
    we would be reading state_ after free.
    
    Miscellaneous cleanups: omit net::, and use the assign() method of
    std::string to avoid constructing a temporary object.
    
    R=agl@chromium.org,rvargas@chromium.org
    BUG=84426
    TEST=net_unittests --gtest_filter=DiskCacheBasedSSLHostInfo.DeleteInCallback
    
    Review URL: http://codereview.chromium.org/7648007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97821 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/disk_cache_based_ssl_host_info.h
+++ net/http/disk_cache_based_ssl_host_info.h
@@ -80,7 +80,9 @@ class DiskCacheBasedSSLHostInfo : public SSLHostInfo,
 
   std::string key() const;
 
-  void DoLoop(int rv);
+  void OnIOComplete(int rv);
+
+  int DoLoop(int rv);
 
   int DoGetBackendComplete(int rv);
   int DoOpenComplete(int rv);
@@ -113,8 +115,8 @@ class DiskCacheBasedSSLHostInfo : public SSLHostInfo,
   disk_cache::Backend* backend_;
   disk_cache::Entry* entry_;
   CompletionCallback* user_callback_;
-  scoped_refptr<net::IOBuffer> read_buffer_;
-  scoped_refptr<net::IOBuffer> write_buffer_;
+  scoped_refptr<IOBuffer> read_buffer_;
+  scoped_refptr<IOBuffer> write_buffer_;
   std::string data_;
 };
 
