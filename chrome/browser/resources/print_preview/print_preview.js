3fe63822b3d275f0111ca231258cdefba895588e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 10:28:20 +0000

    Print Preview: Don't display a reopen button when a print preview fails and there is no initiator tab url.
    BUG=none
    TEST=The following HTML should cause print preview to fail. It should not have a reload button that does not work.
    
    <a href="#foo" onclick="return window.print()">test</a>
    
    Review URL: http://codereview.chromium.org/7711022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97845 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -160,10 +160,14 @@ function launchNativePrintDialog() {
  */
 function onInitiatorTabCrashed(initiatorTabURL) {
   disableInputElementsInSidebar();
-  displayErrorMessageWithButton(
-      localStrings.getString('initiatorTabCrashed'),
-      localStrings.getString('reopenPage'),
-      function() { chrome.send('reloadCrashedInitiatorTab'); });
+  if (initiatorTabURL) {
+    displayErrorMessageWithButton(
+        localStrings.getString('initiatorTabCrashed'),
+        localStrings.getString('reopenPage'),
+        function() { chrome.send('reloadCrashedInitiatorTab'); });
+  } else {
+    displayErrorMessage(localStrings.getString('initiatorTabCrashed'));
+  }
 }
 
 /**
@@ -173,10 +177,14 @@ function onInitiatorTabCrashed(initiatorTabURL) {
  */
 function onInitiatorTabClosed(initiatorTabURL) {
   disableInputElementsInSidebar();
-  displayErrorMessageWithButton(
-      localStrings.getString('initiatorTabClosed'),
-      localStrings.getString('reopenPage'),
-      function() { window.location = initiatorTabURL; });
+  if (initiatorTabURL) {
+    displayErrorMessageWithButton(
+        localStrings.getString('initiatorTabClosed'),
+        localStrings.getString('reopenPage'),
+        function() { window.location = initiatorTabURL; });
+  } else {
+    displayErrorMessage(localStrings.getString('initiatorTabClosed'));
+  }
 }
 
 /**
