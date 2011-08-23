d38836215c94e9f71a56204c158734d919d0d03d
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-23 10:18:10 +0000

    Clean up a comment in ipc_message_macros.h
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7709004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97844 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_macros.h
+++ ipc/ipc_message_macros.h
@@ -151,10 +151,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Once the types have been declared / registered, message definitions follow.
 // "Sync" messages are just synchronous calls, the Send() call doesn't return
-// until a reply comes back.  Input parameters are first (const TYPE&), and
-// To declare a sync message, use the IPC_SYNC_ macros.  The numbers at the
-// end show how many input/output parameters there are (i.e. 1_2 is 1 in, 2
-// out). The caller does a Send([route id, ], in1, &out1, &out2).
+// until a reply comes back.  To declare a sync message, use the IPC_SYNC_
+// macros.  The numbers at the end show how many input/output parameters there
+// are (i.e. 1_2 is 1 in, 2 out).  Input parameters are first, followed by
+// output parameters.  The caller uses Send([route id, ], in1, &out1, &out2).
 // The receiver's handler function will be
 //     void OnSyncMessageName(const type1& in1, type2* out1, type3* out2)
 //
