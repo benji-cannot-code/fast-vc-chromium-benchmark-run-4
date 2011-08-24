75fdc5bf41b7cc0207074c84f7b8114426fc38b3
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 19:58:36 +0000

    Fix variable names and comments in RandGenerator.
    (No semantic change intended).
    
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/7685053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98098 0039d316-1c4b-4281-b951-d872f2087c98

--- base/rand_util_unittest.cc
+++ base/rand_util_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/rand_util.h"
 
+#include <algorithm>
 #include <limits>
 
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,16 +31,14 @@ TEST(RandUtilTest, RandDouble) {
 }
 
 TEST(RandUtilTest, RandBytes) {
-  const size_t buffer_size = 145;
+  const size_t buffer_size = 50;
   char buffer[buffer_size];
   memset(buffer, 0, buffer_size);
   base::RandBytes(buffer, buffer_size);
-  char accumulator = 0;
-  for(size_t i = 0; i < buffer_size; ++i)
-    accumulator |= buffer[i];
-  // In theory this test can fail, but it won't before the universe dies of
-  // heat death.
-  EXPECT_NE(0, accumulator);
+  std::sort(buffer, buffer + buffer_size);
+  // Probability of occurrence of less than 25 unique bytes in 50 random bytes
+  // is below 10^-25.
+  EXPECT_GT(std::unique(buffer, buffer + buffer_size) - buffer, 25);
 }
 
 TEST(RandUtilTest, RandBytesAsString) {
