63b2b4b0652dc507e9e170564c40cda1582c9da8
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 00:26:47 +0000

    Fix the windows build.
    TBR=cpu
    
    Review URL: http://codereview.chromium.org/7701020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97954 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -37,7 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-static const FilePath::CharType kLocaleFileExtension[] = ".pak";
+static const FilePath::CharType kLocaleFileExtension[] =
+    FILE_PATH_LITERAL(".pak");
 
 static const char* const kAcceptLanguageList[] = {
   "af",     // Afrikaans
