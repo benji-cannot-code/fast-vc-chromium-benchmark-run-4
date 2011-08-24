0da6b16888fdeba09e7984a4c058fb165c7423bd
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 23:38:45 +0000

    File Manager: fix date sorting initialization
    BUG=chromium-os:19082
    TEST=manual testing
    
    Review URL: http://codereview.chromium.org/7714008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -140,6 +140,8 @@ function FileManager(dialogDom, filesystem, rootEntries) {
   this.summarizeSelection_();
   this.updatePreview_();
 
+  this.dataModel_.sort('cachedMtime_');
+
   this.refocus();
 
   // Pass all URLs to the metadata reader until we have a correct filter.
@@ -636,7 +638,6 @@ FileManager.prototype = {
                                        this.compareMtime_.bind(this));
     this.dataModel_.setCompareFunction('cachedSize_',
                                        this.compareSize_.bind(this));
-    this.dataModel_.sort('cachedMtime_');
     this.dataModel_.prepareSort = this.prepareSort_.bind(this);
 
     if (this.dialogType_ == FileManager.DialogType.SELECT_OPEN_FILE ||
@@ -662,10 +663,10 @@ FileManager.prototype = {
    * Compare by mtime first, then by name.
    */
   FileManager.prototype.compareMtime_ = function(a, b) {
-    if (a.cachedMtime_ > b.cachedMtime)
+    if (a.cachedMtime_ > b.cachedMtime_)
       return 1;
 
-    if (a.cachedMtime_ < b.cachedMtime)
+    if (a.cachedMtime_ < b.cachedMtime_)
       return -1;
 
     return this.collator_.compare(a.name, b.name);
@@ -675,10 +676,10 @@ FileManager.prototype = {
    * Compare by size first, then by name.
    */
   FileManager.prototype.compareSize_ = function(a, b) {
-    if (a.cachedSize_ > b.cachedSize)
+    if (a.cachedSize_ > b.cachedSize_)
       return 1;
 
-    if (a.cachedSize_ < b.cachedSize)
+    if (a.cachedSize_ < b.cachedSize_)
       return -1;
 
     return this.collator_.compare(a.name, b.name);
