f24381ab73cb7f50a55ae65b66fdf715821415f1
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 21:57:58 +0000

    Add sign in dialog for GCP->print preview integration.
    BUG=http://code.google.com/p/chromium-os/issues/detail?id=16082
    TEST=Make sure you are signed out.  Open print preview. Choose sign in from the printer drop down.  Should be able to sign in with the resulting dialog.
    
    
    Review URL: http://codereview.chromium.org/7518002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/cloud_print/cloud_print_url.cc
+++ chrome/browser/printing/cloud_print/cloud_print_url.cc
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 
 const char kDefaultCloudPrintServiceURL[] = "https://www.google.com/cloudprint";
+const char kDefaultCloudPrintSigninURL[] =
+    "https://www.google.com/accounts/ServiceLogin?"
+    "service=cloudprint&continue=https%3A%2F%2Fwww.google.com%2Fcloudprint";
+
 const char kLearnMoreURL[] =
     "https://www.google.com/support/cloudprint";
 const char kTestPageURL[] =
@@ -24,11 +28,16 @@ const char kTestPageURL[] =
 void CloudPrintURL::RegisterPreferences() {
   DCHECK(profile_);
   PrefService* pref_service = profile_->GetPrefs();
-  if (pref_service->FindPreference(prefs::kCloudPrintServiceURL))
-    return;
-  pref_service->RegisterStringPref(prefs::kCloudPrintServiceURL,
-                                   kDefaultCloudPrintServiceURL,
-                                   PrefService::UNSYNCABLE_PREF);
+  if (!pref_service->FindPreference(prefs::kCloudPrintServiceURL)) {
+    pref_service->RegisterStringPref(prefs::kCloudPrintServiceURL,
+                                     kDefaultCloudPrintServiceURL,
+                                     PrefService::UNSYNCABLE_PREF);
+  }
+  if (!pref_service->FindPreference(prefs::kCloudPrintSigninURL)) {
+    pref_service->RegisterStringPref(prefs::kCloudPrintSigninURL,
+                                     kDefaultCloudPrintSigninURL,
+                                     PrefService::UNSYNCABLE_PREF);
+  }
 }
 
 // Returns the root service URL for the cloud print service.  The default is to
@@ -46,6 +55,15 @@ GURL CloudPrintURL::GetCloudPrintServiceURL() {
         profile_->GetPrefs()->GetString(prefs::kCloudPrintServiceURL));
   }
   return cloud_print_service_url;
+}
+
+GURL CloudPrintURL::GetCloudPrintSigninURL() {
+  DCHECK(profile_);
+  RegisterPreferences();
+
+  GURL cloud_print_signin_url = GURL(
+      profile_->GetPrefs()->GetString(prefs::kCloudPrintSigninURL));
+  return google_util::AppendGoogleLocaleParam(cloud_print_signin_url);
 }
 
 GURL CloudPrintURL::GetCloudPrintServiceDialogURL() {
--- chrome/browser/printing/cloud_print/cloud_print_url.h
+++ chrome/browser/printing/cloud_print/cloud_print_url.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,6 +21,7 @@ class CloudPrintURL {
   GURL GetCloudPrintServiceDialogURL();
   GURL GetCloudPrintServiceManageURL();
   GURL GetCloudPrintServiceEnableURL(const std::string& proxy_id);
+  GURL GetCloudPrintSigninURL();
 
   // These aren't derived from the service, but it makes sense to keep all the
   // URLs together, and this gives the unit tests access for testing.
