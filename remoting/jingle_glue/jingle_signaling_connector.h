208d7977e72399d1d869acb52c76cc05d4fac2a9
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 00:33:01 +0000

    Properly handle errors received in response to session messages.
    BUG=93531
    TEST=Client shows error when host is down.
    
    Review URL: http://codereview.chromium.org/7685045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97956 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/jingle_glue/jingle_signaling_connector.h
+++ remoting/jingle_glue/jingle_signaling_connector.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_JINGLE_GLUE_JINGLE_SIGNALING_CONNECTOR_H_
 #define REMOTING_JINGLE_GLUE_JINGLE_SIGNALING_CONNECTOR_H_
 
+#include <map>
+
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "remoting/jingle_glue/signal_strategy.h"
@@ -46,13 +48,14 @@ class JingleSignalingConnector : public SignalStrategy::Listener,
   virtual bool OnIncomingStanza(const buzz::XmlElement* stanza) OVERRIDE;
 
  private:
-  void OnResponse(const buzz::XmlElement* stanza);
+  typedef std::map<std::string, buzz::XmlElement*> IqRequestsMap;
 
   void OnOutgoingMessage(cricket::SessionManager* manager,
                          const buzz::XmlElement* stanza);
 
   SignalStrategy* signal_strategy_;
   cricket::SessionManager* session_manager_;
+  IqRequestsMap pending_requests_;
 
   DISALLOW_COPY_AND_ASSIGN(JingleSignalingConnector);
 };
