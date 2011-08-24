b5de77ea67db148ed3339cbb22072f984f2e6cef
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 14:59:13 +0000

    Turn off PIE when building for Valgrind.
    BUG=90221, 93551
    TEST=Valgrind
    Review URL: http://codereview.chromium.org/7724002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98046 0039d316-1c4b-4281-b951-d872f2087c98

--- build/mac/change_mach_o_flags_from_xcode.sh
+++ build/mac/change_mach_o_flags_from_xcode.sh
@@ -12,4 +12,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set -e
 
 exec "$(dirname "${0}")/change_mach_o_flags.py" \
+     "${@}" \
      "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_PATH}"
