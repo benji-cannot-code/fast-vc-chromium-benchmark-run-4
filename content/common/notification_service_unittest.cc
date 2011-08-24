205509f77e89ea809a71fb4138fe5d16734101d9
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 17:08:18 +0000

    Move more files to content_unittests.
    BUG=90443
    
    Review URL: http://codereview.chromium.org/7714012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98062 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/notification_service_unittest.cc
+++ content/common/notification_service_unittest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/test/base/testing_browser_process_test.h"
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
 #include "content/common/notification_service.h"
@@ -33,7 +32,7 @@ private:
 }  // namespace
 
 
-class NotificationServiceTest : public TestingBrowserProcessTest {
+class NotificationServiceTest : public testing::Test {
  protected:
   NotificationRegistrar registrar_;
 };
