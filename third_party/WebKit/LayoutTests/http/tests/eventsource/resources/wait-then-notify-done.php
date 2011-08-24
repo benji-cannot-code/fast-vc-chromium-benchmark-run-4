2f9eea18eca9bac66b2ab2fc05af9710f0ae9024
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-24 20:16:45 +0000

    2011-08-24  Nate Chapin  <japhet@chromium.org>
            Fix an intermittent crash in EventSource, when a
            reconnect timer triggers after a navigation
            away from the current page begins.
            https://bugs.webkit.org/show_bug.cgi?id=45202
    
            Reviewed by Alexey Proskuryakov.
    
            Test: http/tests/eventsource/eventsource-reconnect-during-navigate-crash.html
    
            * page/EventSource.cpp:
            (WebCore::EventSource::connect): Don't set m_requestInFlight
                to true if ThreadableLoader::create() failed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/eventsource/resources/wait-then-notify-done.php
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Content-Type: text/html");
+header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Pragma: no-cache");
+sleep(1);
+echo "<script> if (window.layoutTestController) layoutTestController.notifyDone()</script>\n";
+?>
