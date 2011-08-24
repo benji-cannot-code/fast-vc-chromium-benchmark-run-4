d2de38e5c484765da84f44b12ee49dff46a8e731
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 09:20:25 +0000

    Get ListenerLeakTests ready for V8's r9000.
    This change ensures that we skip Objects named after constructors
    when counting leaked objects.
    
    R=sgjesse@chromium.org
    BUG=
    TEST=test_shell_tests/ListenerLeakTest*
    
    Review URL: http://codereview.chromium.org/7714001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98017 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/listener_leak_test.cc
+++ webkit/tools/test_shell/listener_leak_test.cc
@@ -18,6 +18,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ListenerLeakTest : public TestShellTest {
 };
 
+static const v8::HeapGraphNode* GetProperty(const v8::HeapGraphNode* node,
+                                            v8::HeapGraphEdge::Type type,
+                                            const char* name) {
+  for (int i = 0, count = node->GetChildrenCount(); i < count; ++i) {
+    const v8::HeapGraphEdge* prop = node->GetChild(i);
+    if (prop->GetType() == type) {
+      v8::String::AsciiValue prop_name(prop->GetName());
+      if (strcmp(name, *prop_name) == 0) return prop->GetToNode();
+    }
+  }
+  return NULL;
+}
+
 static int GetNumObjects(const char* constructor) {
   v8::HandleScope scope;
   const v8::HeapSnapshot* snapshot =
@@ -27,11 +40,18 @@ static int GetNumObjects(const char* constructor) {
   int count = 0;
   for (int i = 0; i < snapshot->GetNodesCount(); ++i) {
     const v8::HeapGraphNode* node = snapshot->GetNode(i);
-    if (node->GetType() != v8::HeapGraphNode::kObject)
-      continue;
+    if (node->GetType() != v8::HeapGraphNode::kObject) continue;
     v8::String::AsciiValue node_name(node->GetName());
-    if (strcmp(constructor, *node_name) == 0)
+    if (strcmp(constructor, *node_name) == 0) {
+      const v8::HeapGraphNode* constructor_prop =
+        GetProperty(node, v8::HeapGraphEdge::kProperty, "constructor");
+      // Skip an Object instance named after the constructor.
+      if (constructor_prop != NULL) {
+        v8::String::AsciiValue constructor_name(constructor_prop->GetName());
+        if (strcmp(constructor, *constructor_name) == 0) continue;
+      }
       ++count;
+    }
   }
   return count;
 }
