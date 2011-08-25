474a2df01b815009a5ebee51abe6fd470bab367f
ericu@chromium.org <ericu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-25 00:59:51 +0000

    Reviewed by Adam Barth.
    [filesystem/Chromium] Filesystem paths need proper URL escaping
    https://bugs.webkit.org/show_bug.cgi?id=62811
    
    Fix http://code.google.com/p/chromium/issues/detail?id=78860 by making
    KURLChromium.cpp's escaping code actually work.
    
    Source/WebCore:
    
    Make encodeWithURLEscapeSequences call into googleurl to do proper
    escaping.  Tested in WebKit/chromium/tests/KURLTest.cpp.
    * platform/KURLGoogle.cpp:
    (WebCore::encodeWithURLEscapeSequences):
    
    Source/WebKit/chromium:
    
    Here I added the needed calls to encodeWithURLEscapeSequences.
    * src/AsyncFileSystemChromium.cpp:
    (WebCore::AsyncFileSystemChromium::virtualPathToFileSystemURL):
    * src/WorkerAsyncFileSystemChromium.cpp:
    (WebCore::WorkerAsyncFileSystemChromium::virtualPathToFileSystemURL):
    
    Here I updated the test to reflect the new functionality in
    encodeWithURLEscapeSequences.
    * tests/KURLTest.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/SecurityOrigin.cpp
+++ third_party/WebKit/Source/WebCore/page/SecurityOrigin.cpp
@@ -66,7 +66,6 @@ static bool schemeRequiresAuthority(const String& scheme)
     return schemes.contains(scheme);
 }
 
-
 SecurityOrigin::SecurityOrigin(const KURL& url, SandboxFlags sandboxFlags)
     : m_sandboxFlags(sandboxFlags)
     , m_protocol(url.protocol().isNull() ? "" : url.protocol().lower())
@@ -92,7 +91,7 @@ SecurityOrigin::SecurityOrigin(const KURL& url, SandboxFlags sandboxFlags)
         isBlobOrFileSystemProtocol = true;
 #endif
     if (isBlobOrFileSystemProtocol) {
-        KURL originURL(ParsedURLString, url.path());
+        KURL originURL(ParsedURLString, decodeURLEscapeSequences(url.path()));
         if (originURL.isValid()) {
             m_protocol = originURL.protocol().lower();
             m_host = originURL.host().lower();
