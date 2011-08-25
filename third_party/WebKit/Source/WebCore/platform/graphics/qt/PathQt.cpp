bc58a17f0c88329a4ee08a619838c28a30445ad4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-25 15:56:38 +0000

    Unreviewed, rolling out r93774. http://trac.webkit.org/changeset/93774 https://bugs.webkit.org/show_bug.cgi?id=66945
    It broke 26 tests on the Qt bot (Requested by ossy on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-08-25
    
    * html/canvas/CanvasRenderingContext2D.cpp:
    (WebCore::CanvasRenderingContext2D::stroke):
    * platform/graphics/qt/PathQt.cpp:
    (WebCore::Path::boundingRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/qt/PathQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/qt/PathQt.cpp
@@ -1,9 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006 Zack Rusin <zack@kde.org>
- * Copyright (C) 2006 Rob Buis <buis@kde.org>
- * Copyright (C) 2009, 2010 Dirk Schulze <krit@webkit.org>
- * Copyright (C) 2010, 2011 Andreas Kling <kling@webkit.org>
+ * Copyright (C) 2006 Zack Rusin   <zack@kde.org>
+ *               2006 Rob Buis     <buis@kde.org>
+ *               2009, 2010 Dirk Schulze <krit@webkit.org>
  *
  * All rights reserved.
  *
@@ -151,7 +150,7 @@ void Path::translate(const FloatSize& size)
 
 FloatRect Path::boundingRect() const
 {
-    return m_path.controlPointRect();
+    return m_path.boundingRect();
 }
 
 FloatRect Path::strokeBoundingRect(StrokeStyleApplier* applier) const
