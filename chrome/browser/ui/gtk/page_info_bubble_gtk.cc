cab634d684a1b25e2f024ec57f00cb27e40057f1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-25 20:42:41 +0000

    gtk: Change BrowserList::GetLastActive() to GetLastActiveWithProfile() in page_info_bubble_gtk.cc.
    BUG=86361
    TEST=None
    
    R=mirandac@chromium.org,erg@chromium.org
    
    Review URL: http://codereview.chromium.org/7715032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98291 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/page_info_bubble_gtk.cc
+++ chrome/browser/ui/gtk/page_info_bubble_gtk.cc
@@ -82,6 +82,8 @@ class PageInfoBubbleGtk : public PageInfoModelObserver,
 
   BubbleGtk* bubble_;
 
+  Profile* profile_;
+
   DISALLOW_COPY_AND_ASSIGN(PageInfoBubbleGtk);
 };
 
@@ -96,7 +98,8 @@ PageInfoBubbleGtk::PageInfoBubbleGtk(gfx::NativeWindow parent,
       cert_id_(ssl.cert_id()),
       parent_(parent),
       contents_(NULL),
-      theme_service_(GtkThemeService::GetFrom(profile)) {
+      theme_service_(GtkThemeService::GetFrom(profile)),
+      profile_(profile) {
   BrowserWindowGtk* browser_window =
       BrowserWindowGtk::GetBrowserWindowForNativeWindow(parent);
 
@@ -225,7 +228,7 @@ void PageInfoBubbleGtk::OnViewCertLinkClicked(GtkWidget* widget) {
 void PageInfoBubbleGtk::OnHelpLinkClicked(GtkWidget* widget) {
   GURL url = google_util::AppendGoogleLocaleParam(
       GURL(chrome::kPageInfoHelpCenterURL));
-  Browser* browser = BrowserList::GetLastActive();
+  Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
   browser->OpenURL(url, GURL(), NEW_FOREGROUND_TAB, PageTransition::LINK);
   bubble_->Close();
 }
