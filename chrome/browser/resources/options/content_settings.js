6cd77d624f39398edd3db91af1a8c086267fdcea
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-25 23:02:07 +0000

    Revert 97966 : Revert 97955 - First pass on intents options UI.
    Original review URL: http://codereview.chromium.org/7624012
    
    R=groby@chromium.org,vandebo@chromium.org
    BUG=none
    TEST=*Intents*
    
    
    Review URL: http://codereview.chromium.org/7719029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/content_settings.js
+++ chrome/browser/resources/options/content_settings.js
@@ -46,11 +46,17 @@ cr.define('options', function() {
         };
       }
 
-      var manageHandlersButton =
-          this.pageDiv.querySelector('#manage-handlers-button');
+      var manageHandlersButton = $('manage-handlers-button');
       if (manageHandlersButton) {
         manageHandlersButton.onclick = function(event) {
           OptionsPage.navigateToPage('handlers');
+        };
+      }
+
+      var manageIntentsButton = $('manage-intents-button');
+      if (manageIntentsButton) {
+        manageIntentsButton.onclick = function(event) {
+          OptionsPage.navigateToPage('intents');
         };
       }
 
@@ -63,8 +69,8 @@ cr.define('options', function() {
       if (!templateData.enable_click_to_play)
         $('click_to_play').hidden = true;
 
-      if (!templateData.enable_web_intents)
-        $('intent-filter').hidden = true;
+      if (!templateData.enable_web_intents && $('intent-section'))
+        $('intent-section').hidden = true;
     },
   };
 
