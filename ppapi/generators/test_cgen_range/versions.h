99099d69ffe4c3b4afe44d30f88bd214f3caf445
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-25 21:04:26 +0000

    More multi-version support (part 2)
    Add member to Node to return a set of unique releases for that node.
    Updates DefineStruct to iterate across all unique releases for the
    struct, generating first the most recent release as we do today
    and other releases without comments, and the version number 
    appended to the structure name.
    
    NOTE: This is work in progress. This CL adds support for
    Interfaces that change which should be enough for M15.  While this
    same CL creates multiple structures when the structures change, 
    references to structures will not have mangled names which would 
    cause errors.
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=89969
    TEST= python generator.py --cgen --test --diff --range=M13,M14
    Review URL: http://codereview.chromium.org/7751001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98298 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/test_cgen_range/versions.h
+++ ppapi/generators/test_cgen_range/versions.h
@@ -4,12 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From test_cgen_range/versions.idl modified Wed Aug 24 10:35:01 2011. */
+/* From test_cgen_range/versions.idl modified Wed Aug 24 19:49:19 2011. */
 
 #ifndef PPAPI_C_TEST_CGEN_RANGE_VERSIONS_H_
 #define PPAPI_C_TEST_CGEN_RANGE_VERSIONS_H_
 
 #include "ppapi/c/pp_macros.h"
+
+#define BAR_INTERFACE_0_0 "Bar;0.0"
+#define BAR_INTERFACE_1_0 "Bar;1.0"
+#define BAR_INTERFACE_2_0 "Bar;2.0"
+#define BAR_INTERFACE BAR_INTERFACE_2_0
 
 /**
  * @file
@@ -20,20 +25,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @addtogroup Structs
  * @{
  */
-/* Bogus Struct */
-struct PP_Size {
+/* Bogus Struct Foo */
+struct Foo {
   /**
-   * Comment for function
-   */
-  int32_t (*Foo)(int32_t x);
-  /**
-   * Comment for function
-   */
-  int32_t (*Foo)(int32_t x, int32_t y);
-  /**
-   * Comment for function
+   * Comment for function x,y,z
    */
   int32_t (*Foo)(int32_t x, int32_t y, int32_t z);
+};
+struct Foo_0_0 {
+  int32_t (*Foo)(int32_t x);
+};
+struct Foo_1_0 {
+  int32_t (*Foo)(int32_t x, int32_t y);
+};
+/**
+ * @}
+ */
+
+/**
+ * @addtogroup Interfaces
+ * @{
+ */
+/* Inherit revisions thanks to Foo */
+struct Bar {
+  /**
+   * Comment for function
+   */
+  int32_t (*UseFoo)( struct Foo* val);
+};
+struct Bar_0_0 {
+  int32_t (*UseFoo)( struct Foo* val);
+};
+struct Bar_1_0 {
+  int32_t (*UseFoo)( struct Foo* val);
 };
 /**
  * @}
