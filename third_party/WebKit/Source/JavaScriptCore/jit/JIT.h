a0a3661cb77926a681c101d18f1f6baf3d3bcb2b
loki@webkit.org <loki@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-25 23:08:11 +0000

    REGRESSION(r93755): It made 14 jsc test and ~500 layout test fail on Qt-ARM bot https://bugs.webkit.org/show_bug.cgi?id=66956
    Rebaseline constants for patching GetByIdSlowCaseCall on ARM.
    
    Reviewed by Oliver Hunt.
    
    * jit/JIT.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
+++ third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
@@ -392,7 +392,7 @@ namespace JSC {
 #if ENABLE(OPCODE_SAMPLING)
         #error "OPCODE_SAMPLING is not yet supported"
 #else
-        static const int patchOffsetGetByIdSlowCaseCall = 32;
+        static const int patchOffsetGetByIdSlowCaseCall = 40;
 #endif
         static const int patchOffsetOpCallCompareToJump = 12;
 
@@ -411,7 +411,7 @@ namespace JSC {
         static const int sequenceGetByIdHotPathConstantSpace = 4;
         // sequenceGetByIdSlowCase
         static const int sequenceGetByIdSlowCaseInstructionSpace = 56;
-        static const int sequenceGetByIdSlowCaseConstantSpace = 2;
+        static const int sequenceGetByIdSlowCaseConstantSpace = 3;
         // sequencePutById
         static const int sequencePutByIdInstructionSpace = 36;
         static const int sequencePutByIdConstantSpace = 4;
