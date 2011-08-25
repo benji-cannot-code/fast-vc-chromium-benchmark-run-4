56e8a1cf0042c2ba95b680954fafe88319901b1b
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-25 23:39:57 +0000

    Concentrate RenderLayer repaint rects updates https://bugs.webkit.org/show_bug.cgi?id=64993
    Reviewed by David Hyatt.
    
    The change moves the update of the repaint rects to 2 methods
    (computeRepaintRects and clearRepaintRects) to better track
    those changes.
    
    No new test, refactoring covered by the existing test cases.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::updateLayerPositions): Reverse the logic and
    calculate the new bounds upfront. This should have no impact as none
    of the methods rely on the internal value.
    
    (WebCore::RenderLayer::computeRepaintRects): Added the cachedOffset parameter
    to avoid breaking the performance optimization in updateLayerPositions. Added
    some ASSERTS.
    
    (WebCore::RenderLayer::clearRepaintRects): Added some ASSERTs here.
    (WebCore::RenderLayer::updateRepaintRectsAfterScroll): Added a FIXME as this
    method could use the cachedOffset trick too.
    
    (WebCore::RenderLayer::setHasVisibleContent): Changed the code to use
    computeRepaintRects.
    
    * rendering/RenderLayer.h: Updated the signature of computeRepaintRects and moved
    those 2 methods into the private section of the class.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
@@ -404,7 +404,6 @@ public:
     // Return a cached repaint rect, computed relative to the layer renderer's containerForRepaint.
     LayoutRect repaintRect() const { return m_repaintRect; }
     LayoutRect repaintRectIncludingDescendants() const;
-    void computeRepaintRects();
     void updateLayerPositionsAfterScroll(bool fixed = false);
     void setNeedsFullRepaint(bool f = true) { m_needsFullRepaint = f; }
     
@@ -462,6 +461,9 @@ public:
     void setContainsDirtyOverlayScrollbars(bool dirtyScrollbars) { m_containsDirtyOverlayScrollbars = dirtyScrollbars; }
 
 private:
+    void computeRepaintRects(IntPoint* cachedOffset = 0);
+    void clearRepaintRects();
+
     // The normal operator new is disallowed on all render objects.
     void* operator new(size_t) throw();
 
