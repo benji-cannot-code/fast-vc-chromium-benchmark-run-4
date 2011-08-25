8eec72a8a5ebaabddf55bdd8b8f17ba331ca2ce1
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-25 22:33:12 +0000

    Move file internal methods to a class continuation in WKView https://bugs.webkit.org/show_bug.cgi?id=66983
    Reviewed by Dan Bernstein.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _deviceScaleFactor]):
    (-[WKView _setDrawingAreaSize:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -110,7 +110,7 @@ struct WKViewInterpretKeyEventsParameters {
     Vector<KeypressCommand>* commands;
 };
 
-@interface WKView (WKFileInternal)
+@interface WKView ()
 - (float)_deviceScaleFactor;
 - (void)_setDrawingAreaSize:(NSSize)size;
 @end
@@ -2013,6 +2013,29 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
     }
 }
 
+- (float)_deviceScaleFactor
+{
+    NSWindow *window = [self window];
+#if !defined(BUILDING_ON_SNOW_LEOPARD)
+    if (window)
+        return [window backingScaleFactor];
+    return [[NSScreen mainScreen] backingScaleFactor];
+#else
+    if (window)
+        return [window userSpaceScaleFactor];
+    return [[NSScreen mainScreen] userSpaceScaleFactor];
+#endif
+}
+
+- (void)_setDrawingAreaSize:(NSSize)size
+{
+    if (!_data->_page->drawingArea())
+        return;
+    
+    _data->_page->drawingArea()->setSize(IntSize(size), IntSize(_data->_resizeScrollOffset));
+    _data->_resizeScrollOffset = NSZeroSize;
+}
+
 @end
 
 @implementation WKView (Internal)
@@ -2620,33 +2643,6 @@ static void drawPageBackground(CGContextRef context, WebPageProxy* page, const I
 {
     _didReceiveUnhandledCommand = true;
     return YES;
-}
-
-@end
-
-@implementation WKView (WKFileInternal)
-
-- (float)_deviceScaleFactor
-{
-    NSWindow *window = [self window];
-#if !defined(BUILDING_ON_SNOW_LEOPARD)
-    if (window)
-        return [window backingScaleFactor];
-    return [[NSScreen mainScreen] backingScaleFactor];
-#else
-    if (window)
-        return [window userSpaceScaleFactor];
-    return [[NSScreen mainScreen] userSpaceScaleFactor];
-#endif
-}
-
-- (void)_setDrawingAreaSize:(NSSize)size
-{
-    if (!_data->_page->drawingArea())
-        return;
-    
-    _data->_page->drawingArea()->setSize(IntSize(size), IntSize(_data->_resizeScrollOffset));
-    _data->_resizeScrollOffset = NSZeroSize;
 }
 
 @end
