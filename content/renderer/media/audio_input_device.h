dd6514912ab5509ab4eeccdd7929bd7cd74c848b
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-25 07:00:12 +0000

    Refactor AudioInputDevice to remove race conditions and allow more flexible calling sequences. It also ensures that the AudioInputDevice can be destroyed during an active audio session without crashing.
    This CL is a summary of http://codereview.chromium.org/7497025, i.e., it removes potential
    race conditions but in a more "lightweight" manner.
    
    BUG=none
    TEST=trybots
    Review URL: http://codereview.chromium.org/7661017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98198 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/audio_input_device.h
+++ content/renderer/media/audio_input_device.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Low-latency audio capturing unit utilizing audio input stream provided
 // by browser process through IPC.
 //
-// Relationship of classes.
+// Relationship of classes:
 //
 //  AudioInputController                 AudioInputDevice
 //           ^                                  ^
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // an AudioInputDevice::CaptureCallback at construction and will be called
 // by the AudioInputDevice with recorded audio from the underlying audio layers.
 //
-// State sequences.
+// State sequences:
 //
 //            Task [IO thread]                  IPC [IO thread]
 //
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Stop --> ShutDownOnIOThread ------>  AudioInputHostMsg_CloseStream -> Close
 //
 // This class utilizes three threads during its lifetime, namely:
+//
 // 1. Creating thread.
 //    Must be the main render thread. Start and Stop should be called on
 //    this thread.
@@ -43,6 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    Responsible for calling the CaptrureCallback and feed audio samples from
 //    the audio layer in the browser process using sync sockets and shared
 //    memory.
+//
+// Implementation notes:
+//
+// - Start() is asynchronous/non-blocking.
+// - Stop() is synchronous/blocking.
+// - The user must call Stop() before deleting the class instance.
 
 #ifndef CONTENT_RENDERER_MEDIA_AUDIO_INPUT_DEVICE_H_
 #define CONTENT_RENDERER_MEDIA_AUDIO_INPUT_DEVICE_H_
@@ -60,6 +67,9 @@ struct AudioParameters;
 
 // TODO(henrika): This class is based on the AudioDevice class and it has
 // many components in common. Investigate potential for re-factoring.
+// TODO(henrika): Add support for event handling (e.g. OnStateChanged,
+// OnCaptureStopped etc.) and ensure that we can deliver these notifications
+// to any clients using this class.
 class AudioInputDevice
     : public AudioInputMessageFilter::Delegate,
       public base::DelegateSimpleThread::Delegate,
@@ -81,10 +91,13 @@ class AudioInputDevice
                    CaptureCallback* callback);
   virtual ~AudioInputDevice();
 
-  // Starts audio capturing. Returns |true| on success.
-  bool Start();
+  // Starts audio capturing. This method is asynchronous/non-blocking.
+  // TODO(henrika): add support for notification when recording has started.
+  void Start();
 
-  // Stops audio capturing. Returns |true| on success.
+  // Stops audio capturing. This method is synchronous/blocking.
+  // Returns |true| on success.
+  // TODO(henrika): add support for notification when recording has stopped.
   bool Stop();
 
   // Sets the capture volume scaling, with range [0.0, 1.0] inclusive.
@@ -112,7 +125,7 @@ class AudioInputDevice
   // sends IPC messages on that thread.
   void InitializeOnIOThread(const AudioParameters& params);
   void StartOnIOThread();
-  void ShutDownOnIOThread();
+  void ShutDownOnIOThread(base::WaitableEvent* completion);
   void SetVolumeOnIOThread(double volume);
 
   void Send(IPC::Message* message);
