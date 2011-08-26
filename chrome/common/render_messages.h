cd3d1daa293ab9729c3d2ea54dd448510cd5131f
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 21:27:30 +0000

    Add support for exporting IPC messages from component DLLs.
    This removes MessageWithTuple and MessageWithReply since it
    is not easy to export a class that inherits from a template
    specialization.  The functionality of those classes are split
    now between new classes, MessageSchema and SyncMessageSchema,
    and being declared inline via macros.
    
    The key point is that we want to avoid inlining the constructor
    and Read functions for messages.  That avoids code bloat since
    those functions contain all of the parameter serialization and
    deserialization code.  Those are the functions that we really
    want to have contained with component DLLs.
    
    To export IPC messages from a DLL, it is necessary to #define
    IPC_MESSAGE_EXPORT above message declarations.  You can see this
    in action here:
    http://codereview.chromium.org/7687005/diff/41012/ppapi/proxy/ppapi_messages.h
    Review URL: http://codereview.chromium.org/7768001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/common_param_traits.h"
 #include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_platform_file.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebConsoleMessage.h"
 #include "ui/gfx/rect.h"
--- content/common/quota_messages.h
+++ content/common/quota_messages.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Multiply-included message file, hence no include guard.
 
 #include "base/basictypes.h"
+#include "googleurl/src/gurl.h"
 #include "ipc/ipc_message_macros.h"
 #include "webkit/quota/quota_types.h"
 
