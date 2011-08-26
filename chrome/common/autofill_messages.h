ccb66e6055855d00d383dcdd2ad0753c9dda2e55
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 06:02:31 +0000

    Add metrics to track Autofill "user happiness"
    BUG=none
    TEST=TBD
    
    
    Review URL: http://codereview.chromium.org/7747009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98397 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/autofill_messages.h
+++ chrome/common/autofill_messages.h
@@ -108,6 +108,11 @@ IPC_MESSAGE_ROUTED1(AutofillHostMsg_PasswordFormsVisible,
 IPC_MESSAGE_ROUTED1(AutofillHostMsg_FormSubmitted,
                     webkit_glue::FormData /* form */)
 
+// Notification that a form field's value has changed.
+IPC_MESSAGE_ROUTED2(AutofillHostMsg_TextFieldDidChange,
+                    webkit_glue::FormData /* the form */,
+                    webkit_glue::FormField /* the form field */)
+
 // Queries the browser for Autofill suggestions for a form input field.
 IPC_MESSAGE_ROUTED3(AutofillHostMsg_QueryFormFieldAutofill,
                     int /* id of this message */,
@@ -115,7 +120,8 @@ IPC_MESSAGE_ROUTED3(AutofillHostMsg_QueryFormFieldAutofill,
                     webkit_glue::FormField /* the form field */)
 
 // Sent when the popup with Autofill suggestions for a form is shown.
-IPC_MESSAGE_ROUTED0(AutofillHostMsg_DidShowAutofillSuggestions)
+IPC_MESSAGE_ROUTED1(AutofillHostMsg_DidShowAutofillSuggestions,
+                    bool /* is this a new popup? */)
 
 // Instructs the browser to fill in the values for a form using Autofill
 // profile data.
@@ -125,7 +131,10 @@ IPC_MESSAGE_ROUTED4(AutofillHostMsg_FillAutofillFormData,
                     webkit_glue::FormField /* the form field  */,
                     int /* profile unique ID */)
 
-// Sent when a form is previewed or filled with Autofill suggestions.
+// Sent when a form is previewed with Autofill suggestions.
+IPC_MESSAGE_ROUTED0(AutofillHostMsg_DidPreviewAutofillFormData)
+
+// Sent when a form is filled with Autofill suggestions.
 IPC_MESSAGE_ROUTED0(AutofillHostMsg_DidFillAutofillFormData)
 
 // Instructs the browser to remove the specified Autocomplete entry from the
--- chrome/renderer/autofill/autofill_agent.cc
+++ chrome/renderer/autofill/autofill_agent.cc
@@ -180,6 +180,7 @@ void AutofillAgent::removeAutocompleteSuggestion(const WebString& name,
 
 void AutofillAgent::textFieldDidEndEditing(const WebInputElement& element) {
   password_autofill_manager_->TextFieldDidEndEditing(element);
+  has_shown_autofill_popup_for_current_edit_ = false;
 }
 
 void AutofillAgent::textFieldDidChange(const WebInputElement& element) {
@@ -198,6 +199,11 @@ void AutofillAgent::TextFieldDidChangeImpl(const WebInputElement& element) {
     return;
 
   ShowSuggestions(element, false, true, false);
+
+  webkit_glue::FormData form;
+  webkit_glue::FormField field;
+  if (FindFormAndFieldForNode(element, &form, &field))
+    Send(new AutofillHostMsg_TextFieldDidChange(routing_id(), form, field));
 }
 
 void AutofillAgent::textFieldDidReceiveKeyDown(const WebInputElement& element,
@@ -231,6 +237,7 @@ void AutofillAgent::OnSuggestionsReturned(int query_id,
   std::vector<int> ids(unique_ids);
   int separator_index = -1;
 
+  DCHECK_GT(ids.size(), 0U);
   if (!autofill_query_element_.isNull() &&
       !autofill_query_element_.autoComplete()) {
     // If autofill is disabled and we had suggestions, show a warning instead.
@@ -291,7 +298,10 @@ void AutofillAgent::OnSuggestionsReturned(int query_id,
         autofill_query_element_, v, l, i, ids, separator_index);
   }
 
-  Send(new AutofillHostMsg_DidShowAutofillSuggestions(routing_id()));
+  Send(new AutofillHostMsg_DidShowAutofillSuggestions(
+      routing_id(),
+      has_autofill_item && !has_shown_autofill_popup_for_current_edit_));
+  has_shown_autofill_popup_for_current_edit_ |= has_autofill_item;
 }
 
 void AutofillAgent::OnFormDataFilled(int query_id,
@@ -302,15 +312,16 @@ void AutofillAgent::OnFormDataFilled(int query_id,
   switch (autofill_action_) {
     case AUTOFILL_FILL:
       form_manager_.FillForm(form, autofill_query_element_);
+      Send(new AutofillHostMsg_DidFillAutofillFormData(routing_id()));
       break;
     case AUTOFILL_PREVIEW:
       form_manager_.PreviewForm(form, autofill_query_element_);
+      Send(new AutofillHostMsg_DidPreviewAutofillFormData(routing_id()));
       break;
     default:
       NOTREACHED();
   }
   autofill_action_ = AUTOFILL_NONE;
-  Send(new AutofillHostMsg_DidFillAutofillFormData(routing_id()));
 }
 
 void AutofillAgent::OnFieldTypePredictionsAvailable(
