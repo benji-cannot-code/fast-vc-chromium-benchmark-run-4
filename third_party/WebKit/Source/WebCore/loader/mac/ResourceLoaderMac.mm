a1ebdba4902133eeee02e865bf9073e9786f07df
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-26 20:41:03 +0000

    2011-08-26  Nate Chapin  <japhet@chromium.org>
            Fix http/tests/misc/willCacheResponse-delegate-callback.html
            after r93886. We're implicitly converting an enum to a bool.
    
            Rubber-stamped by David Levin.
    
            * loader/mac/ResourceLoaderMac.mm:
            (WebCore::ResourceLoader::willCacheResponse):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/mac/ResourceLoaderMac.mm
+++ third_party/WebKit/Source/WebCore/loader/mac/ResourceLoaderMac.mm
@@ -51,7 +51,7 @@ namespace WebCore {
 
 CFCachedURLResponseRef ResourceLoader::willCacheResponse(ResourceHandle*, CFCachedURLResponseRef cachedResponse)
 {
-    if (!m_sendResourceLoadCallbacks)
+    if (m_options.sendLoadCallbacks == DoNotSendCallbacks)
         return 0;
 
     RetainPtr<NSCachedURLResponse> nsCachedResponse(AdoptNS, [[NSCachedURLResponse alloc] _initWithCFCachedURLResponse:cachedResponse]);
@@ -62,7 +62,7 @@ CFCachedURLResponseRef ResourceLoader::willCacheResponse(ResourceHandle*, CFCach
 
 NSCachedURLResponse* ResourceLoader::willCacheResponse(ResourceHandle*, NSCachedURLResponse* response)
 {
-    if (!m_options.sendLoadCallbacks)
+    if (m_options.sendLoadCallbacks == DoNotSendCallbacks)
         return 0;
     return frameLoader()->client()->willCacheResponse(documentLoader(), identifier(), response);
 }
