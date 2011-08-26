4f00f6076558a34b3764392dd8ae02fb2c934336
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 23:04:48 +0000

    Revert 87592 - Re-enable in-browser thumbnailing for Linux and Chrome OS.
    The in-browser thumbnailing does not work if
    --force-compositing-mode is supplied. I don't know what would be the right way
    to fix this. For now, we should just disable the in-browser thumbnailing,
    until we have the right fix.
    
    BUG=chromium-os:19731
    
    The last valgrind error was fixed by r87590.
    
    BUG=65936
    TEST=confirm that thumbnails in the new tab page are updated properly
    
    Review URL: http://codereview.chromium.org/6969091
    
    TBR=satorux@chromium.org
    
    Review URL: http://codereview.chromium.org/7762006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98512 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1239,13 +1239,8 @@ bool IsPrintPreviewEnabled() {
 }
 
 bool IsInBrowserThumbnailingEnabled() {
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
-  // The in-browser-thumbnailing is enabled for Linux and Chrome OS.
-  return true;
-#else
   return CommandLine::ForCurrentProcess()->HasSwitch(
       kEnableInBrowserThumbnailing);
-#endif
 }
 
 // -----------------------------------------------------------------------------
