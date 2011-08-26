3eb807e899f4e470462a5cae1537d6febeadf8c2
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 00:13:12 +0000

    Put archive file handling support behind a flag for M14.
    BUG=chromium-os:19657
    TEST=make sure 'Open archive' task shows up only when --enable-archives switch is provided
    Review URL: http://codereview.chromium.org/7741020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1648,6 +1648,8 @@ FileManager.prototype = {
           task.iconUrl =
               chrome.extension.getURL('images/icon_mount_archive_16x16.png');
           task.title = str('MOUNT_ARCHIVE');
+          if (str('ENABLE_ARCHIVES') != 'true')
+            continue;
         }
       }
       this.renderTaskButton_(task);
