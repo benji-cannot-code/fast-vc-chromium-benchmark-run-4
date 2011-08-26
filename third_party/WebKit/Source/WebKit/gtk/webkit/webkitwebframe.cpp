15903163d8edde7e57cc1cb0a8200619b1832e43
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-26 12:28:23 +0000

    Minor style fix in the sample code for WebKitWebFrame.
    * webkit/webkitwebframe.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebframe.cpp
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebframe.cpp
@@ -90,7 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * <informalexample><programlisting>
  * /<!-- -->* Get the frame from the #WebKitWebView *<!-- -->/
  * WebKitWebFrame *frame = webkit_web_view_get_main_frame (WEBKIT_WEB_VIEW(my_view));
- * g_print("The URI of this frame is '%s'", webkit_web_frame_get_uri (frame));
+ * g_print ("The URI of this frame is '%s'", webkit_web_frame_get_uri (frame));
  * </programlisting></informalexample>
  */
 
