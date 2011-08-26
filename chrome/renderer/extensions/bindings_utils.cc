d9e0f487439589678c5d8a84f7e99496effcd1c2
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 03:24:44 +0000

    Do not dereference potentially invalid frame pointer in extension event bindings.
    I think this might fix bugs 93768 and 93774.
    
    BUG=93768,93774
    
    Review URL: http://codereview.chromium.org/7717019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/bindings_utils.cc
+++ chrome/renderer/extensions/bindings_utils.cc
@@ -134,7 +134,7 @@ ContextInfo::ContextInfo(v8::Persistent<v8::Context> context,
                          WebFrame* frame)
     : context(context),
       extension_id(extension_id),
-      frame(frame),
+      unsafe_frame(frame),
       num_connected_events(0) {
 }
 
