a720012e9572d8811fb16b95c9f2535cd42765de
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-26 20:47:55 +0000

    Fix MIPS patchOffsetGetByIdSlowCaseCall https://bugs.webkit.org/show_bug.cgi?id=67046
    Patch by Chao-ying Fu <fu@mips.com> on 2011-08-26
    Reviewed by Gavin Barraclough.
    
    * jit/JIT.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
+++ third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
@@ -465,7 +465,7 @@ namespace JSC {
 #if ENABLE(OPCODE_SAMPLING)
         #error "OPCODE_SAMPLING is not yet supported"
 #else
-        static const int patchOffsetGetByIdSlowCaseCall = 44;
+        static const int patchOffsetGetByIdSlowCaseCall = 56;
 #endif
         static const int patchOffsetOpCallCompareToJump = 32;
         static const int patchOffsetMethodCheckProtoObj = 32;
@@ -483,7 +483,7 @@ namespace JSC {
 #if ENABLE(OPCODE_SAMPLING)
         #error "OPCODE_SAMPLING is not yet supported"
 #else
-        static const int patchOffsetGetByIdSlowCaseCall = 44;
+        static const int patchOffsetGetByIdSlowCaseCall = 56;
 #endif
         static const int patchOffsetOpCallCompareToJump = 32;
         static const int patchOffsetMethodCheckProtoObj = 32;
