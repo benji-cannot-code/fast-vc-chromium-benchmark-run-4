60d7738084e6298a8ec5923d91f2bf2f3616fe84
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 09:05:18 +0000

    [cros] Minor WebUI EULA fixes. Serve OEM eula from correct URL.
    BUG=chromium-os:19587
    TEST=Manual. Loaded WebUI EULA with OEM content.
    
    
    Review URL: http://codereview.chromium.org/7753006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98407 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -83,7 +83,7 @@ const char kChromeUIRegisterPageURL[] = "chrome://register/";
 const char kChromeUISlideshowURL[] = "chrome://slideshow/";
 const char kChromeUISimUnlockURL[] = "chrome://sim-unlock/";
 const char kChromeUISystemInfoURL[] = "chrome://system/";
-const char kChromeUITermsOemURL[] = "chrome://terms/oem/";
+const char kChromeUITermsOemURL[] = "chrome://terms/oem";
 const char kChromeUIUserImageURL[] = "chrome://userimage/";
 #endif
 
