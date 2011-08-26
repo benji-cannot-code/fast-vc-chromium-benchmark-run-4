6cf5c3df900afabef850835d2cd4580827bafdfe
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 20:11:53 +0000

    Implement touch selection menu.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7740024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98473 0039d316-1c4b-4281-b951-d872f2087c98

--- views/touchui/touch_selection_controller_impl.h
+++ views/touchui/touch_selection_controller_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VIEWS_TOUCHUI_TOUCH_SELECTION_CONTROLLER_IMPL_H_
 #pragma once
 
+#include "base/timer.h"
 #include "ui/gfx/point.h"
 #include "views/touchui/touch_selection_controller.h"
 #include "views/view.h"
@@ -33,6 +34,7 @@ class VIEWS_EXPORT TouchSelectionControllerImpl
  private:
   friend class TouchSelectionControllerImplTest;
   class SelectionHandleView;
+  class TouchContextMenuView;
 
   // Callback to inform the client view that the selection handle has been
   // dragged, hence selection may need to be updated.
@@ -41,6 +43,21 @@ class VIEWS_EXPORT TouchSelectionControllerImpl
   // Convenience method to convert a point from a selection handle's coordinate
   // system to that of the client view.
   void ConvertPointToClientView(SelectionHandleView* source, gfx::Point* point);
+
+  // Checks if the client view supports a context menu command.
+  bool IsCommandIdEnabled(int command_id) const;
+
+  // Sends a context menu command to the client view.
+  void ExecuteCommand(int command_id);
+
+  // Time to show context menu.
+  void ContextMenuTimerFired();
+
+  // Convenience method to update the position/visibility of the context menu.
+  void UpdateContextMenu(const gfx::Point& p1, const gfx::Point& p2);
+
+  // Convenience method for hiding context menu.
+  void HideContextMenu();
 
   // Convenience methods for testing.
   gfx::Point GetSelectionHandle1Position();
@@ -51,6 +68,12 @@ class VIEWS_EXPORT TouchSelectionControllerImpl
   TouchSelectionClientView* client_view_;
   scoped_ptr<SelectionHandleView> selection_handle_1_;
   scoped_ptr<SelectionHandleView> selection_handle_2_;
+  scoped_ptr<TouchContextMenuView> context_menu_;
+
+  // Timer to trigger |context_menu| (|context_menu| is not shown if the
+  // selection handles are being updated. It appears only when the handles are
+  // stationary for a certain amount of time).
+  base::OneShotTimer<TouchSelectionControllerImpl> context_menu_timer_;
 
   // Pointer to the SelectionHandleView being dragged during a drag session.
   SelectionHandleView* dragging_handle_;
