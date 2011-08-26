a86a6fb0f21b62a4bfcb340f31338ffe4d9c692e
gavinp@chromium.org <gavinp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-26 13:15:00 +0000

    add a state enumeration to track down cause of null CachedScript execution https://bugs.webkit.org/show_bug.cgi?id=66939
    Reviewed by Alexey Proskuryakov.
    
    Over in http://code.google.com/p/chromium/issues/detail?id=75604 I
    have a bug I cannot reproduce.  I added an earlier crash in
    https://bugs.webkit.org/show_bug.cgi?id=65563 , and we've since
    gotten many good stacks, all coming in through a failed request that
    eventually calls notifyFinished() on a ScriptElement with a NULL
    m_cachedScript.
    
    I'd like to know how this got NULL.  This enumeration should let
    me find that in stack dumps from reproductions.
    
    No new tests, as my goal here is to get telemetry on a bug that I cannot reproduce.
    
    * dom/ScriptElement.cpp:
    (WebCore::ScriptElement::ScriptElement):
    (WebCore::ScriptElement::requestScript):
    (WebCore::ScriptElement::stopLoadRequest):
    (WebCore::ScriptElement::notifyFinished):
    * dom/ScriptElement.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ScriptElement.cpp
+++ third_party/WebKit/Source/WebCore/dom/ScriptElement.cpp
@@ -65,6 +65,7 @@ ScriptElement::ScriptElement(Element* element, bool parserInserted, bool already
     , m_willExecuteWhenDocumentFinishedParsing(false)
     , m_forceAsync(!parserInserted)
     , m_willExecuteInOrder(false)
+    , m_cachedScriptState(NeverSet)
 {
     ASSERT(m_element);
 }
@@ -259,8 +260,11 @@ bool ScriptElement::requestScript(const String& sourceUrl)
     m_cachedScript = m_element->document()->cachedResourceLoader()->requestScript(request, scriptCharset());
     m_isExternalScript = true;
 
-    if (m_cachedScript)
+    if (m_cachedScript) {
+        ASSERT(m_cachedScriptState == NeverSet);
+        m_cachedScriptState = Set;
         return true;
+    }
 
     dispatchErrorEvent();
     return false;
@@ -296,6 +300,8 @@ void ScriptElement::stopLoadRequest()
     if (m_cachedScript) {
         if (!m_willBeParserExecuted)
             m_cachedScript->removeClient(this);
+        ASSERT(m_cachedScriptState == Set);
+        m_cachedScriptState = ZeroedInStopLoadRequest;
         m_cachedScript = 0;
     }
 }
@@ -321,6 +327,9 @@ void ScriptElement::notifyFinished(CachedResource* o)
         m_element->document()->scriptRunner()->notifyInOrderScriptReady();
     else
         m_element->document()->scriptRunner()->queueScriptForExecution(this, m_cachedScript, ScriptRunner::ASYNC_EXECUTION);
+
+    ASSERT(m_cachedScriptState == Set);
+    m_cachedScriptState = ZeroedInNotifyFinished;
     m_cachedScript = 0;
 }
 
