4ae58f5bc3ef9e5cb25292a9d1777802ee3dcea4
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-27 02:40:22 +0000

    Deflake PrerenderExcessiveMemory, fixing a race in setting the visibility state of prerendered RenderViews in the process.
    Also add some more visibility API tests that would have
    reliably failed when losing the fixed race.
    
    
    BUG=93081
    TEST=PrerenderBrowserTest.PrerenderExcessiveMemory. PrerenderBrowserTest.PrerenderInfiniteLoop
    
    Review URL: http://codereview.chromium.org/7693029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_contents.h
+++ chrome/browser/prerender/prerender_contents.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/tab_contents/tab_contents_observer.h"
 #include "content/browser/renderer_host/render_view_host_delegate.h"
 #include "content/common/notification_registrar.h"
+#include "ui/gfx/rect.h"
 
 class Profile;
 class RenderViewHost;
@@ -280,8 +281,11 @@ class PrerenderContents : public NotificationObserver,
   // Experiment during which this prerender is performed.
   uint8 experiment_id_;
 
-  // Offset by which to offset prerendered pages
+  // Offset by which to offset prerendered pages.
   static const int32 kPrerenderPageIdOffset = 10;
+
+  // Bounds to use for the prerendered TabContents.
+  gfx::Rect tab_bounds_;
 
   DISALLOW_COPY_AND_ASSIGN(PrerenderContents);
 };
