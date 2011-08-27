53d8c7729ffd934755be04368e764b6f29b822be
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-27 06:53:21 +0000

    [Sync] Add tests for migration triggered by notifications
    Add support for disabling/enabling notifications to the test server, and support for triggering notifications.
    
    BUG=92928
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7753023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/notifier/p2p_notifier.cc
+++ chrome/browser/sync/notifier/p2p_notifier.cc
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sync_notifier {
 
-namespace {
+const char* kSyncP2PNotificationChannel = "http://www.google.com/chrome/sync";
 
-const char kSyncNotificationChannel[] = "http://www.google.com/chrome/sync";
+namespace {
 
 const char kNotifySelf[] = "notifySelf";
 const char kNotifyOthers[] = "notifyOthers";
@@ -197,7 +197,7 @@ void P2PNotifier::UpdateCredentials(
     }
 
     notifier::Subscription subscription;
-    subscription.channel = kSyncNotificationChannel;
+    subscription.channel = kSyncP2PNotificationChannel;
     // There may be some subtle issues around case sensitivity of the
     // from field, but it doesn't matter too much since this is only
     // used in p2p mode (which is only used in testing).
@@ -255,7 +255,7 @@ void P2PNotifier::OnIncomingNotification(
     VLOG(1) << "Notifications not enabled -- not emitting notification";
     return;
   }
-  if (notification.channel != kSyncNotificationChannel) {
+  if (notification.channel != kSyncP2PNotificationChannel) {
     LOG(WARNING) << "Notification from unexpected source "
                  << notification.channel;
   }
@@ -294,7 +294,7 @@ void P2PNotifier::SendNotificationData(
     const P2PNotificationData& notification_data) {
   DCHECK(parent_message_loop_proxy_->BelongsToCurrentThread());
   notifier::Notification notification;
-  notification.channel = kSyncNotificationChannel;
+  notification.channel = kSyncP2PNotificationChannel;
   notification.data = notification_data.ToString();
   VLOG(1) << "Sending XMPP notification: " << notification.ToString();
   talk_mediator_->SendNotification(notification);
