68cd64caf3c87038a32df3aa95b0945041913070
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-27 02:15:30 +0000

    Cleanup resulting text of media/track tests. https://bugs.webkit.org/show_bug.cgi?id=67073
    Patch by Anna Cavender <annacc@chromium.org> on 2011-08-26
    Reviewed by Eric Carlson.
    
    * media/track/track-webvtt-tc001-utf8-expected.txt:
    * media/track/track-webvtt-tc002-bom-expected.txt:
    * media/track/track-webvtt-tc003-newlines-expected.txt:
    * media/track/track-webvtt-tc004-magic-header-expected.txt:
    * media/track/track-webvtt-tc005-header-comment-expected.txt:
    * media/track/track-webvtt-tc006-cue-identifiers-expected.txt:
    * media/track/track-webvtt-tc007-cue-no-id-expected.txt:
    * media/track/track-webvtt-tc008-timings-no-hours-expected.txt:
    * media/track/track-webvtt-tc009-timings-hour-expected.txt:
    * media/track/track-webvtt-tc011-blank-lines-expected.txt:
    * media/track/track-webvtt-tc013-settings-expected.txt:
    * media/track/track-webvtt-tc014-alignment-expected.txt:
    * media/track/track-webvtt-tc015-positioning-expected.txt:
    * media/track/track-webvtt-tc016-align-positioning-expected.txt:
    * media/track/track-webvtt-tc017-line-position-expected.txt:
    * media/track/track-webvtt-tc018-align-text-line-position-expected.txt:
    * media/track/track-webvtt-tc019-cue-size-expected.txt:
    * media/track/track-webvtt-tc020-cue-size-align-expected.txt:
    * media/track/track-webvtt-tc021-valign-expected.txt:
    * media/track/track-webvtt-tc022-entities-expected.txt:
    * media/track/track-webvtt-tc028-unsupported-markup-expected.txt:
    * media/video-test.js:
    (testCues):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93932 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/media/video-test.js
+++ third_party/WebKit/LayoutTests/media/video-test.js
@@ -250,7 +250,7 @@ function testCues(index, expected)
     for (i = 0; i < cues.length; i++) {
         for (j = 0; j < expected.tests.length; j++) {
             var test = expected.tests[j];
-            testExpected("cues[i]." + test.property, test.values[i]);
+            testExpected("cues[" + i + "]." + test.property, test.values[i]);
         }
     }
 }
