e97a8c0c94702b189e7424b0bd96fc4ebf589f3e
piman@google.com <piman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-29 21:47:45 +0000

    Initial checkin of the compositor_model_bench tool
    This tool simulates the GPU demands of Chromium's GPU-accelerated compositor. In
    the current version, we simulate the performance of the existing forward
    rendering model; coming changes will prototype a new model.
    
    BUG=None
    TEST=self
    
    This is http://codereview.chromium.org/7718020/ with trivial fixes
    Original change by Joshua Trask <joshtrask@google.com>
    
    Review URL: http://codereview.chromium.org/7792002
    Patch from Joshua Trask <joshtrask@google.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98706 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ gpu/tools/compositor_model_bench/render_models.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/tools/compositor_model_bench/render_models.h"
+
+#include <string>
+
+#include "gpu/tools/compositor_model_bench/forward_render_model.h"
+
+const char* ModelToString(RenderModel m) {
+  switch (m) {
+    case ForwardRenderModel:
+      return "Forward Rendering";
+    default:
+      return "(unknown render model name)";
+  }
+}
+
+RenderModelSimulator::RenderModelSimulator(RenderNode* root) : root_(root) {
+}
+
+RenderModelSimulator::~RenderModelSimulator() {
+}
+
+RenderModelSimulator* ConstructSimulationModel(RenderModel model,
+                                               RenderNode* render_tree_root,
+                                               int window_width,
+                                               int window_height) {
+  switch (model) {
+    case ForwardRenderModel:
+      return new ForwardRenderSimulator(render_tree_root,
+                                        window_width,
+                                        window_height);
+    default:
+      LOG(ERROR) << "Unrecognized render model. "
+        "If we know its name, then it's..." << ModelToString(model);
+      return 0;
+  }
+}
+
