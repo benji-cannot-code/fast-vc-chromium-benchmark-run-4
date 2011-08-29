aaead26b1feed30b077c745cd4c81ba5c1f96d65
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-29 21:03:43 +0000

    Increased the unresponsive_threshold from 6 to 12 in Beta channel and XP. We will crash the browser if UI/IO threads don't respond within 24 secs (instead of 12 secs) in Beta channel and Windows XP.
    BUG=90095
    R=jar
    TEST=thread_watcher unit tests
    
    Review URL: http://codereview.chromium.org/7744015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/thread_watcher.cc
+++ chrome/browser/metrics/thread_watcher.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/notification_service.h"
 
 #if defined(OS_WIN)
-#include <Objbase.h>
+#include "base/win/windows_version.h"
 #endif
 
 // static
@@ -418,6 +418,22 @@ void ThreadWatcherList::ParseCommandLine(
     uint32* live_threads_threshold) {
   // Determine |unresponsive_threshold| based on switches::kCrashOnHangSeconds.
   *unresponsive_threshold = kUnresponsiveCount;
+
+  if (chrome::VersionInfo::GetChannel() == chrome::VersionInfo::CHANNEL_BETA) {
+    // Increase the unresponsive_threshold in Beta channel to reduce the number
+    // of crashes due to ThreadWatcher.
+    *unresponsive_threshold *= 2;
+  } else {
+    // In Canary and Dev channels, for Windows XP (old systems), double the
+    // unresponsive_threshold to give OS a chance to schedule UI/IO threads a
+    // time slice to respond with a pong message (to get around limitations with
+    // the OS).
+#if defined(OS_WIN)
+    if (base::win::GetVersion() <= base::win::VERSION_XP)
+      *unresponsive_threshold *= 2;
+#endif
+  }
+
   std::string crash_on_hang_seconds =
       command_line.GetSwitchValueASCII(switches::kCrashOnHangSeconds);
   if (!crash_on_hang_seconds.empty()) {
