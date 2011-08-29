ea09ff203cce1ea49a4450aaad805799b3033af3
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-29 17:57:55 +0000

    postMessage raises INVALID_STATE_ERR instead of DATA_CLONE_ERR in many cases. https://bugs.webkit.org/show_bug.cgi?id=66713
    Reviewed by Dmitry Titov.
    
    Source/WebCore:
    
    See http://www.whatwg.org/specs/web-apps/current-work/multipage/comms.html#posting-messages for postMessage
    
    * bindings/js/JSMessagePortCustom.cpp:
    (WebCore::fillMessagePortArray):
    * bindings/v8/custom/V8MessagePortCustom.cpp:
    (WebCore::getMessagePortArray):
    * dom/MessagePort.cpp:
    (WebCore::MessagePort::postMessage):
    (WebCore::MessagePort::disentanglePorts):
    
    LayoutTests:
    
    * fast/events/message-port-clone-expected.txt: Output changed from INVALID_STATE_ERR to DATA_CLONE_ERR.
    * fast/events/message-port-multi-expected.txt: Ditto.
    * fast/workers/worker-context-multi-port-expected.txt:  Ditto.
    * fast/workers/worker-multi-port-expected.txt:  Ditto.
    * platform/chromium-win/fast/events/message-port-multi-expected.txt: Ditto.
    * platform/chromium-win/fast/workers/worker-context-multi-port-expected.txt: Ditto.
    * platform/chromium-win/fast/workers/worker-multi-port-expected.txt: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93986 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/MessagePort.cpp
+++ third_party/WebKit/Source/WebCore/dom/MessagePort.cpp
@@ -84,7 +84,7 @@ void MessagePort::postMessage(PassRefPtr<SerializedScriptValue> message, const M
         for (unsigned int i = 0; i < ports->size(); ++i) {
             MessagePort* dataPort = (*ports)[i].get();
             if (dataPort == this || m_entangledChannel->isConnectedTo(dataPort)) {
-                ec = INVALID_STATE_ERR;
+                ec = DATA_CLONE_ERR;
                 return;
             }
         }
@@ -217,7 +217,7 @@ PassOwnPtr<MessagePortChannelArray> MessagePort::disentanglePorts(const MessageP
     for (unsigned int i = 0; i < ports->size(); ++i) {
         MessagePort* port = (*ports)[i].get();
         if (!port || port->isCloned() || portSet.contains(port)) {
-            ec = INVALID_STATE_ERR;
+            ec = DATA_CLONE_ERR;
             return nullptr;
         }
         portSet.add(port);
