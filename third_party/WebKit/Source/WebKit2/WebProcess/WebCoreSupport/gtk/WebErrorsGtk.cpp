febea67552551ef394e0afea12c3db4cf08db743
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-29 08:22:07 +0000

    [GTK][WK2] Compilation error in WebKit2 GTK - Revision 93953 https://bugs.webkit.org/show_bug.cgi?id=67111
    Patch by Vamshikrishna Yellenki <vamshi@motorola.com> on 2011-08-29
    Reviewed by Martin Robinson.
    
    WebKit2 GTK build error.
    
    * WebProcess/WebCoreSupport/gtk/WebErrorsGtk.cpp:
    Include <WebCore/ResourceError.h> explicitly to remove WebCore::ResourceError
    inclusion error.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@93960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/gtk/WebErrorsGtk.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/gtk/WebErrorsGtk.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebErrors.h"
 
+#include <WebCore/ResourceError.h>
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/ResourceResponse.h>
 #include <glib/gi18n-lib.h>
