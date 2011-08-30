a2d5861a3846e3ea973ce9fe33329e2145089915
noelutz@google.com <noelutz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-30 18:23:25 +0000

    Change the client-side phishing detection hashing function to mimic what we're doing on the server.
    BUG=
    TEST=BrowserFeatureExtractorTest
    
    
    Review URL: http://codereview.chromium.org/7793012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/browser_feature_extractor_unittest.cc
+++ chrome/browser/safe_browsing/browser_feature_extractor_unittest.cc
@@ -489,8 +489,8 @@ TEST_F(BrowserFeatureExtractorTest, URLHashes) {
 
   EXPECT_TRUE(ExtractFeatures(&request));
   EXPECT_EQ(crypto::SHA256HashString("host.com/").substr(
-      0, BrowserFeatureExtractor::kSuffixPrefixHashLength),
-            request.suffix_prefix_hash());
+      0, BrowserFeatureExtractor::kHashPrefixLength),
+            request.hash_prefix());
 
   request.set_url("http://www.host.com/path/");
   history_service()->AddPage(GURL("http://www.host.com/path/"),
@@ -498,9 +498,9 @@ TEST_F(BrowserFeatureExtractorTest, URLHashes) {
   contents()->NavigateAndCommit(GURL("http://www.host.com/path/"));
 
   EXPECT_TRUE(ExtractFeatures(&request));
-  EXPECT_EQ(crypto::SHA256HashString("www.host.com/path/").substr(
-      0, BrowserFeatureExtractor::kSuffixPrefixHashLength),
-            request.suffix_prefix_hash());
+  EXPECT_EQ(crypto::SHA256HashString("host.com/path/").substr(
+      0, BrowserFeatureExtractor::kHashPrefixLength),
+            request.hash_prefix());
 
   request.set_url("http://user@www.host.com:1111/path/123?args");
   history_service()->AddPage(
@@ -510,9 +510,9 @@ TEST_F(BrowserFeatureExtractorTest, URLHashes) {
       GURL("http://user@www.host.com:1111/path/123?args"));
 
   EXPECT_TRUE(ExtractFeatures(&request));
-  EXPECT_EQ(crypto::SHA256HashString("www.host.com/path/123").substr(
-      0, BrowserFeatureExtractor::kSuffixPrefixHashLength),
-            request.suffix_prefix_hash());
+  EXPECT_EQ(crypto::SHA256HashString("host.com/path/").substr(
+      0, BrowserFeatureExtractor::kHashPrefixLength),
+            request.hash_prefix());
 
   // Check that escaping matches the SafeBrowsing specification.
   request.set_url("http://www.host.com/A%21//B");
@@ -521,8 +521,8 @@ TEST_F(BrowserFeatureExtractorTest, URLHashes) {
   contents()->NavigateAndCommit(GURL("http://www.host.com/A%21//B"));
 
   EXPECT_TRUE(ExtractFeatures(&request));
-  EXPECT_EQ(crypto::SHA256HashString("www.host.com/A!/B").substr(
-      0, BrowserFeatureExtractor::kSuffixPrefixHashLength),
-            request.suffix_prefix_hash());
+  EXPECT_EQ(crypto::SHA256HashString("host.com/a!/").substr(
+      0, BrowserFeatureExtractor::kHashPrefixLength),
+            request.hash_prefix());
 }
 }  // namespace safe_browsing
