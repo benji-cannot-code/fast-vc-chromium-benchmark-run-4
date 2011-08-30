5698e2cee258db2a35152a5ab83a332f7b0f2d38
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-30 19:01:19 +0000

    Don't update URL bar or SSL icon for pending history navs until they commit.
    BUG=89564
    TEST=Go back or forward to a slow URL.  No omnibox change until it commits.
    
    
    Review URL: http://codereview.chromium.org/7790018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/toolbar_model.cc
+++ chrome/browser/ui/toolbar/toolbar_model.cc
@@ -44,7 +44,7 @@ string16 ToolbarModel::GetText() const {
     Profile* profile =
         Profile::FromBrowserContext(navigation_controller->browser_context());
     languages = profile->GetPrefs()->GetString(prefs::kAcceptLanguages);
-    NavigationEntry* entry = navigation_controller->GetActiveEntry();
+    NavigationEntry* entry = navigation_controller->GetVisibleEntry();
     if (!navigation_controller->tab_contents()->ShouldDisplayURL()) {
       // Explicitly hide the URL for this tab.
       url = GURL();
@@ -70,7 +70,7 @@ ToolbarModel::SecurityLevel ToolbarModel::GetSecurityLevel() const {
   if (!navigation_controller)  // We might not have a controller on init.
     return NONE;
 
-  NavigationEntry* entry = navigation_controller->GetActiveEntry();
+  NavigationEntry* entry = navigation_controller->GetVisibleEntry();
   if (!entry)
     return NONE;
 
@@ -120,7 +120,7 @@ string16 ToolbarModel::GetEVCertName() const {
   // Note: Navigation controller and active entry are guaranteed non-NULL or
   // the security level would be NONE.
   CertStore::GetInstance()->RetrieveCert(
-      GetNavigationController()->GetActiveEntry()->ssl().cert_id(), &cert);
+      GetNavigationController()->GetVisibleEntry()->ssl().cert_id(), &cert);
   return GetEVCertName(*cert);
 }
 
