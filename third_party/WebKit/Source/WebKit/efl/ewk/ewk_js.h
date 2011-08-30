2e39e1eeeadf1256e2db4f19071adab94ff8cee8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-30 01:29:59 +0000

    [EFL] Add missing API documentation. https://bugs.webkit.org/show_bug.cgi?id=66927
    Patch by Grzegorz Czajkowski <g.czajkowski@samsung.com> on 2011-08-29
    Reviewed by Kent Tamura.
    
    Adds missing documentation for ewk_context_menu_item_checked_get/set.
    Moves API documentation from cpp to the headers.
    Adds brief and file commands for ewk_js.h.
    
    * ewk/ewk_contextmenu.h:
    * ewk/ewk_cookies.cpp:
    * ewk/ewk_cookies.h:
    * ewk/ewk_frame.cpp:
    * ewk/ewk_frame.h:
    * ewk/ewk_js.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_js.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_js.h
@@ -18,6 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Boston, MA 02110-1301, USA.
 */
 
+/**
+ * @file    ewk_js.h
+ * @brief   Allows to export objects to JavaScript API.
+ */
+
 #ifndef ewk_js_h
 #define ewk_js_h
 
