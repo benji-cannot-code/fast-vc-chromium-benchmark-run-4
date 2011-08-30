ebf8de9514269eb331d6aacb90d039d326fb012a
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-30 16:33:40 +0000

    Fix stuck keys problem with Chromoting when host disconnects while keys are down.
    BUG=84544
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7799014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98825 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/input_sender.cc
+++ remoting/protocol/input_sender.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,8 @@ InputSender::~InputSender() {
 }
 
 void InputSender::InjectKeyEvent(const KeyEvent* event, Task* done) {
+  DCHECK(done);
+
   EventMessage message;
   message.set_sequence_number(base::Time::Now().ToInternalValue());
   message.mutable_key_event()->CopyFrom(*event);
@@ -36,6 +38,8 @@ void InputSender::InjectKeyEvent(const KeyEvent* event, Task* done) {
 }
 
 void InputSender::InjectMouseEvent(const MouseEvent* event, Task* done) {
+  DCHECK(done);
+
   EventMessage message;
   message.set_sequence_number(base::Time::Now().ToInternalValue());
   message.mutable_mouse_event()->CopyFrom(*event);
