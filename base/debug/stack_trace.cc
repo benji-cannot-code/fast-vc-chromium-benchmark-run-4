8ad33c633e05c52f8f4d50a9d64c3c00ed2bad35
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-30 19:33:04 +0000

    [Mac] Capture -dealloc backtrace to log with CrZombie messages.
    Logs a subset of the stack backtrace from the point of -dealloc into a
    breakpad key to help debug messages to CrZombie objects.  This
    productionizes the basic approach taken to debug a few recent zombie
    object crashes.
    
    BUG=none
    TEST=monitor crash server.
    
    Review URL: http://codereview.chromium.org/7766013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98858 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/stack_trace.cc
+++ base/debug/stack_trace.cc
@@ -5,8 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/stack_trace.h"
 
+#include "base/basictypes.h"
+
+#include <string.h>
+
+#include <algorithm>
+
 namespace base {
 namespace debug {
+
+StackTrace::StackTrace(const void* const* trace, size_t count) {
+  count = std::min(count, arraysize(trace_));
+  if (count)
+    memcpy(trace_, trace, count * sizeof(trace_[0]));
+  count_ = static_cast<int>(count);
+}
 
 StackTrace::~StackTrace() {
 }
