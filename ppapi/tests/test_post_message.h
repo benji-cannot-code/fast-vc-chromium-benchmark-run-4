b3911472b4bd31c076e75dae3dd4276ec81bc367
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-30 16:39:55 +0000

    Add means for running some tests only o-o-p. Add messaging non-main thread test.
    BUG=92909
    TEST=included
    
    Review URL: http://codereview.chromium.org/7648033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98826 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_post_message.h
+++ ppapi/tests/test_post_message.h
@@ -54,6 +54,9 @@ class TestPostMessage : public TestCase {
   // nothing happens.
   std::string TestExtraParam();
 
+  // Test sending messages off of the main thread.
+  std::string TestNonMainThread();
+
   typedef std::vector<pp::Var> VarVector;
 
   // This is used to store pp::Var objects we receive via a call to
