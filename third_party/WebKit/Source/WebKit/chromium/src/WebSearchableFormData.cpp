5b28697b1ceb570ddad915b511fcd2afd79e9501
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-30 20:23:51 +0000

    [Chromium] Add null checks for document()->loader() https://bugs.webkit.org/show_bug.cgi?id=67077
    Reviewed by Nate Chapin.
    
    Document::loader can return 0 for a number of reasons.  Call sites need
    to null check the result, like we do for Document::frame.  I suspect
    some of these are real crashes, along the lines of
    http://trac.webkit.org/changeset/93692, but null checking this function
    is just good practice.
    
    * src/ContextMenuClientImpl.cpp:
    (WebKit::ContextMenuClientImpl::getCustomMenuFromDefaultItems):
    * src/WebSearchableFormData.cpp:
    (HTMLNames::GetFormEncoding):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::pageEncoding):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSearchableFormData.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSearchableFormData.cpp
@@ -65,7 +65,7 @@ void GetFormEncoding(const HTMLFormElement* form, TextEncoding* encoding)
         if (encoding->isValid())
             return;
     }
-    if (!form->document()->frame())
+    if (!form->document()->loader())
          return;
     *encoding = TextEncoding(form->document()->loader()->writer()->encoding());
 }
