1e93217059b3314783ac769253226d00becc26d1
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 08:21:05 +0000

    Migrate Obsolete NotificationsSettings and remove content_settings::NotificationsProvider.
    BUG=63656
    TEST=host_content_settings_map_unittest.cc,
         content_settings_pref_provider.cc,
         desktop_notifications_service_unittest.cc
    
    Review URL: http://codereview.chromium.org/7655019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notifications_unittest.h
+++ chrome/browser/notifications/desktop_notifications_unittest.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_test_util.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
-#include "chrome/browser/notifications/notifications_prefs_cache.h"
 #include "chrome/test/base/testing_pref_service.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/browser/browser_thread.h"
@@ -90,10 +89,6 @@ class DesktopNotificationsTest : public testing::Test {
 
   void DenyOrigin(const GURL& origin) {
     service_->DenyPermission(origin);
-  }
-
-  int HasPermission(const GURL& origin) {
-    return service_->prefs_cache()->HasPermission(origin);
   }
 
   // Constructs a notification parameter structure for use in tests.
--- chrome/browser/notifications/notification_options_menu_model.cc
+++ chrome/browser/notifications/notification_options_menu_model.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/notifications/notification_options_menu_model.h"
 
+#include <string>
+
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/utf_string_conversions.h"
@@ -15,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/desktop_notification_service_factory.h"
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
-#include "chrome/browser/notifications/notifications_prefs_cache.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/chrome_switches.h"
