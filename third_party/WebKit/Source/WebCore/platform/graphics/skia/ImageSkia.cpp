8ce177d54245d0ecbe421ef440c1f893baabb55a
reed@google.com <reed@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-31 14:03:26 +0000

    [skia] antialias images if we're rotated https://bugs.webkit.org/show_bug.cgi?id=67207
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    * platform/graphics/skia/ImageSkia.cpp:
    (WebCore::hasNon90rotation):
    (WebCore::paintSkBitmap):
    
    LayoutTests:
    
    * platform/chromium/test_expectations.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -206,6 +206,11 @@ static void drawResampledBitmap(SkCanvas& canvas, SkPaint& paint, const NativeIm
     canvas.drawBitmapRect(resampled, 0, destRectVisibleSubset, &paint);
 }
 
+static bool hasNon90rotation(PlatformContextSkia* context)
+{
+    return !context->canvas()->getTotalMatrix().rectStaysRect();
+}
+
 static void paintSkBitmap(PlatformContextSkia* platformContext, const NativeImageSkia& bitmap, const SkIRect& srcRect, const SkRect& destRect, const SkXfermode::Mode& compOp)
 {
     SkPaint paint;
@@ -213,6 +218,8 @@ static void paintSkBitmap(PlatformContextSkia* platformContext, const NativeImag
     paint.setFilterBitmap(true);
     paint.setAlpha(platformContext->getNormalizedAlpha());
     paint.setLooper(platformContext->getDrawLooper());
+    // only antialias if we're rotated or skewed
+    paint.setAntiAlias(hasNon90rotation(platformContext));
 
     SkCanvas* canvas = platformContext->canvas();
 
