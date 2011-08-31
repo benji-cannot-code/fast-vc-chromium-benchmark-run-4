d154983e73a36c5261e94b6a1b6612fca15de89d
noelutz@google.com <noelutz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 02:59:42 +0000

    Parse the whitelist expressions from the phishing verdict response if there are any and check to see whether the current URL matches any of these whitelisted expressions.
    BUG=
    TEST=ClientSideDetectionServiceTest
    
    
    Review URL: http://codereview.chromium.org/7792004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98919 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/client_side_detection_service.h
+++ chrome/browser/safe_browsing/client_side_detection_service.h
@@ -51,6 +51,7 @@ class URLRequestStatus;
 
 namespace safe_browsing {
 class ClientPhishingRequest;
+class ClientPhishingResponse;
 class ClientSideModel;
 
 class ClientSideDetectionService : public URLFetcher::Delegate,
@@ -168,6 +169,8 @@ class ClientSideDetectionService : public URLFetcher::Delegate,
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest, SetEnabled);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest, IsBadIpAddress);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest,
+                           IsFalsePositiveResponse);
+  FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest,
                            ModelHasValidHashIds);
   FRIEND_TEST_ALL_PREFIXES(ClientSideDetectionServiceTest,
                            SanitizeRequestForPingback);
@@ -263,6 +266,12 @@ class ClientSideDetectionService : public URLFetcher::Delegate,
   // Returns true iff all the hash id's in the client-side model point to
   // valid hashes in the model.
   static bool ModelHasValidHashIds(const ClientSideModel& model);
+
+  // Returns true iff the response is phishing (phishy() is true) and if the
+  // given URL matches one of the whitelisted expressions in the given
+  // ClientPhishingResponse.
+  static bool IsFalsePositiveResponse(const GURL& url,
+                                      const ClientPhishingResponse& response);
 
   // Whether the service is running or not.  When the service is not running,
   // it won't download the model nor report detected phishing URLs.
