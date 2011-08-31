dc3ba5ffdbac1fe0970f0846ee4fcdc749d3a6ae
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 01:25:29 +0000

    renderer/extensions: Forward declare a couple of v8 classes in EventBindings header.
    BUG=None
    TEST=None
    
    R=mpcomplete@chromium.org,darin@chromium.org
    
    Review URL: http://codereview.chromium.org/7774012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98907 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/event_bindings.cc
+++ chrome/renderer/extensions/event_bindings.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURL.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebURLRequest.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
+#include "v8/include/v8.h"
 
 using bindings_utils::CallFunctionInContext;
 using bindings_utils::ContextInfo;
--- chrome/renderer/extensions/event_bindings.h
+++ chrome/renderer/extensions/event_bindings.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "v8/include/v8.h"
-
 class ExtensionDispatcher;
 class GURL;
 class RenderThreadBase;
@@ -18,6 +16,12 @@ class RenderView;
 
 namespace base {
 class ListValue;
+}
+
+namespace v8 {
+class Context;
+class Extension;
+template <class T> class Handle;
 }
 
 namespace WebKit {
