5a3c78ba424e883cf296d939a370a87d82166307
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 20:28:27 +0000

    Fix uninitialized variable in BookmarksHandler (valgrind detected)
    BUG=94664
    TEST=Valgrind is happy
    Review URL: http://codereview.chromium.org/7776037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99028 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/bookmarks_handler.cc
+++ chrome/browser/ui/webui/ntp/bookmarks_handler.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace keys = extension_bookmarks_module_constants;
 
-BookmarksHandler::BookmarksHandler() {
+BookmarksHandler::BookmarksHandler() : getBookmarksDataIsPending_(false) {
 }
 
 BookmarksHandler::~BookmarksHandler() {
