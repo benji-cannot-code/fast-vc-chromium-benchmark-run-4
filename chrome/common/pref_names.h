1a897a0a782f6f503d14894c8fb42c67f42fd3a8
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 06:19:23 +0000

    Add default per-script CJK fonts on Windows.
    BUG=94217,2685
    TEST=view a page that uses CSS property --webkit-locale and verify that the font is the desired default
    
    
    Review URL: http://codereview.chromium.org/7748014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -73,6 +73,7 @@ extern const char kWebKitStandardFontFamilyKorean[];
 extern const char kWebKitFixedFontFamilyKorean[];
 extern const char kWebKitSerifFontFamilyKorean[];
 extern const char kWebKitSansSerifFontFamilyKorean[];
+extern const char kWebKitCursiveFontFamilyKorean[];
 extern const char kWebKitStandardFontFamilySimplifiedHan[];
 extern const char kWebKitFixedFontFamilySimplifiedHan[];
 extern const char kWebKitSerifFontFamilySimplifiedHan[];
