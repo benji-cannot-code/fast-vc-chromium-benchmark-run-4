dd112cf396f29de6135aa3169ef1be22feccf7a6
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 17:19:29 +0000

    Trap errors when disconnecting in case the host plugin has crashed.
    BUG=93833
    TEST=Make the host plugin crash while waiting for a connection. Check that an errror is reported when the timeout expires.
    
    Review URL: http://codereview.chromium.org/7792003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98995 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/l10n.js
+++ remoting/webapp/me2mom/l10n.js
@@ -7,6 +7,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var l10n = l10n || {};
 
 /**
+ * Localize an element by setting its innerText according to the specified tag
+ * and an optional set of substitutions.
+ * @param {Element} element The element to localize.
+ * @param {string} tag The localization tag.
+ * @param {{string|Object}=} opt_substitutions An optional set of substitution
+ *     strings corresponding to the "placeholders" attributes in messages.json.
+ * @return {boolean} True if the localization was successful; false otherwise.
+ */
+l10n.localizeElementFromTag = function(element, tag, opt_substitutions) {
+  var translation = chrome.i18n.getMessage(tag, opt_substitutions);
+  if (translation) {
+    element.innerHTML = translation;
+  } else {
+    console.error('Missing translation for "' + tag + '":', element);
+  }
+  return translation != null;
+}
+
+/**
  * Localize an element by setting its innerText according to its i18n-content
  * attribute, and an optional set of substitutions.
  * @param {Element} element The element to localize.
@@ -16,13 +35,7 @@ var l10n = l10n || {};
  */
 l10n.localizeElement = function(element, opt_substitutions) {
   var tag = element.getAttribute('i18n-content');
-  var translation = chrome.i18n.getMessage(tag, opt_substitutions);
-  if (translation) {
-    element.innerHTML = translation;
-  } else {
-    console.error('Missing translation for "' + tag + '":', element);
-  }
-  return translation != null;
+  return l10n.localizeElementFromTag(element, tag, opt_substitutions);
 };
 
 /**
