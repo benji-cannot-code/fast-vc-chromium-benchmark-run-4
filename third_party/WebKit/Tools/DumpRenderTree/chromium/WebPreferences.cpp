07bb17c8d5a56fab137eeee5c28b501c6d6f799b
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-31 19:08:24 +0000

    Make ScrollAnimator(Chromium)?Mac check scrollAnimatorEnabled() https://bugs.webkit.org/show_bug.cgi?id=67297
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    I need this to disable smooth scrolling in chromium tests. I'm making
    the change to ScrollAnimatorMac to make unforking the file easier. I
    changed scrollAnimatorEnabled() to default to true so that this change
    is a no-op for Safari; in chromium the flag is set explicitly to true
    or false anyway.
    
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    * platform/chromium/ScrollAnimatorChromiumMac.mm:
    (WebCore::ScrollAnimatorChromiumMac::scroll):
    * platform/mac/ScrollAnimatorMac.mm:
    (WebCore::ScrollAnimatorMac::scroll):
    
    Tools:
    
    Explicitly disable smooth scrolling in chromium's DRT.
    
    * DumpRenderTree/chromium/WebPreferences.cpp:
    (WebPreferences::applyTo):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebPreferences.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebPreferences.cpp
@@ -177,6 +177,7 @@ void WebPreferences::applyTo(WebView* webView)
     settings->setAllowScriptsToCloseWindows(false);
     settings->setNeedsSiteSpecificQuirks(true);
     settings->setEditableLinkBehaviorNeverLive();
+    settings->setEnableScrollAnimator(false);
     settings->setFontRenderingModeNormal();
     settings->setTextDirectionSubmenuInclusionBehaviorNeverIncluded();
     settings->setUsesEncodingDetector(false);
