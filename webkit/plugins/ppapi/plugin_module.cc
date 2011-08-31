11e802061912fd20978613d348b47c71c84cfd98
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 16:08:56 +0000

    Convert the code that uses the pepper testing switch to use the constant rather than hardcode it.
    TEST=it compiles
    BUG=none
    Review URL: http://codereview.chromium.org/7778027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98975 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/plugin_module.cc
+++ webkit/plugins/ppapi/plugin_module.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/shared_impl/time_conversion.h"
 #include "ppapi/thunk/enter.h"
 #include "ppapi/thunk/thunk.h"
+#include "webkit/plugins/plugin_switches.h"
 #include "webkit/plugins/ppapi/callbacks.h"
 #include "webkit/plugins/ppapi/common.h"
 #include "webkit/plugins/ppapi/ppapi_interface_factory.h"
@@ -371,7 +372,8 @@ const void* GetInterface(const char* name) {
   // specified. This allows us to prevent people from (ab)using this interface
   // in production code.
   if (strcmp(name, PPB_TESTING_DEV_INTERFACE) == 0) {
-    if (CommandLine::ForCurrentProcess()->HasSwitch("enable-pepper-testing"))
+    if (CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnablePepperTesting))
       return &testing_interface;
   }
   return NULL;
