76380e10d1e24a0e32f2e283a20941f290d59db8
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-31 01:41:19 +0000

    Chromium build fix after r94142.
    * src/EditorClientImpl.cpp:
    (WebKit::EditorClientImpl::textFieldDidEndEditing):
    (WebKit::EditorClientImpl::doTextFieldCommandFromEvent):
    * src/WebPasswordFormUtils.cpp:
    (WebKit::findPasswordFormFields):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPasswordFormUtils.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPasswordFormUtils.cpp
@@ -77,7 +77,7 @@ void findPasswordFormFields(HTMLFormElement* form, PasswordFormFields* fields)
         if (!formElement->hasLocalName(HTMLNames::inputTag))
             continue;
 
-        HTMLInputElement* inputElement = toHTMLInputElement(formElement);
+        HTMLInputElement* inputElement = WebCore::toHTMLInputElement(formElement);
         if (!inputElement->isEnabledFormControl())
             continue;
 
@@ -99,7 +99,7 @@ void findPasswordFormFields(HTMLFormElement* form, PasswordFormFields* fields)
             if (!formElement->hasLocalName(HTMLNames::inputTag))
                 continue;
 
-            HTMLInputElement* inputElement = toHTMLInputElement(formElement);
+            HTMLInputElement* inputElement = WebCore::toHTMLInputElement(formElement);
             if (!inputElement->isEnabledFormControl())
                 continue;
 
