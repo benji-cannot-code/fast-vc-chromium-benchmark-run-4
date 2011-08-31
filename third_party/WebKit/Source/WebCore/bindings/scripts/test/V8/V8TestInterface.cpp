ed4809af823473df3efcfa0d5e09f4a8ab4e243f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-31 07:35:47 +0000

    Implement Web IDL Constructor extended attribute in IDLParser.pm and CodeGeneratorV8.pm. https://bugs.webkit.org/show_bug.cgi?id=66536
    Patch by Kentaro Hara <haraken@google.com> on 2011-08-31
    Reviewed by Adam Barth.
    
    The spec is here: http://www.w3.org/TR/WebIDL/#Constructor
    
    [1] Introduced Web IDL 'Constructor' extended attribute.
    
    [2] Added 'ConstructorWith=ScriptExecutionContext' extended attribute.
    - The constructor passes ScriptExecutionContext to a XXX::create() method,
    like XXX::create(..., context, ...).
    
    [3] Added 'V8ConstructorSetsActiveDOMWrapper' extended attribute.
    - The constructor calls V8DOMWrapper::setJSWrapperForActiveDOMObject()
    instead of V8DOMWrapper::setJSWrapperForDOMObject().
    
    [4] Added 'ConstructorRaisesException' extended attribute.
    - A placeholder for ExceptionCode is passed to a XXX::create() method,
    like XXX::create(..., ec).
    - We can use 'ConstructorRaisesException' and 'ConstructorWith=ScriptExecutionContext' together.
    
    Note:
    - Constructor overloading is not yet implemented.
    - We cannot yet specify [Optional] without CallWithDefaultValue.
    
    Test: Source/WebCore/bindings/scripts/test/TestInterface.idl
          Source/WebCore/bindings/scripts/test/TestObj.idl
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader): Generates constructorCallback() header, if 'Constructor' is specified.
    (GenerateFunctionCallback): Uses GenerateArgumentsCountCheck() and GenerateParametersCheck().
    (GenerateArgumentsCountCheck): Splits out of GenerateFunctionCallback() for reuse.
    (GenerateParametersCheck): Ditto.
    (GenerateConstructorCallback): Generates constructorCallback() definition.
    (GenerateImplementation):
    * bindings/scripts/IDLParser.pm:
    (parseExtendedAttributes): Previously, only a string in the "A=B, C, D, E=F" format can be parsed. This patch enables to parse a string in the "A=B, C, D, E=F, G(in DOMString s, in [H, I, J] DOMString t)" format.
    (parseParameters): Splits out of ParseInterface() for reuse.
    (ParseInterface): Uses parseParameters().
    * bindings/scripts/IDLStructure.pm: Added domClass{constructor}. Changed the regular expression to extract the list of extended attributes.
    * bindings/scripts/test/TestInterface.idl: Added all the extended attributes that this patch is going to introduce.
    * bindings/scripts/test/TestObj.idl: Added 'Constructor'.
    * bindings/scripts/test/V8/V8TestInterface.cpp: Updated the test result.
    (WebCore::V8TestInterface::constructorCallback):
    (WebCore::ConfigureV8TestInterfaceTemplate):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp: Ditto.
    * bindings/scripts/test/V8/V8TestObj.cpp: Ditto.
    (WebCore::V8TestObj::constructorCallback):
    * bindings/scripts/test/V8/V8TestObj.h: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestInterface.cpp
@@ -24,8 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(Condition1) || ENABLE(Condition2)
 
+#include "ExceptionCode.h"
 #include "RuntimeEnabledFeatures.h"
 #include "V8Binding.h"
+#include "V8BindingMacros.h"
 #include "V8BindingState.h"
 #include "V8DOMWrapper.h"
 #include "V8IsolatedContext.h"
@@ -44,9 +46,34 @@ template <typename T> void V8_USE(T) { }
 
 v8::Handle<v8::Value> V8TestInterface::constructorCallback(const v8::Arguments& args)
 {
-    INC_STATS("DOM.TestInterface.Contructor");
-    return V8Proxy::constructDOMObjectWithScriptExecutionContext<TestInterface>(args, &info);
+    INC_STATS("DOM.TestInterface.Constructor");
+
+    if (!args.IsConstructCall())
+        return throwError("DOM object constructor cannot be called as a function.", V8Proxy::TypeError);
+
+    if (args.Length() < 1)
+        return throwError("Not enough arguments", V8Proxy::TypeError);
+
+    ExceptionCode ec = 0;
+    STRING_TO_V8PARAMETER_EXCEPTION_BLOCK(V8Parameter<>, str1, args[0]);
+    STRING_TO_V8PARAMETER_EXCEPTION_BLOCK(V8Parameter<>, str2, args[1]);
+
+    ScriptExecutionContext* context = getScriptExecutionContext();
+    if (!context)
+        return throwError("TestInterface constructor's associated context is not available", V8Proxy::ReferenceError);
+
+    RefPtr<TestInterface> obj = TestInterface::create(str1, str2, context, ec);
+    if (ec)
+        goto fail;
+
+    V8DOMWrapper::setDOMWrapper(args.Holder(), &info, obj.get());
+    obj->ref();
+    V8DOMWrapper::setJSWrapperForActiveDOMObject(obj.get(), v8::Persistent<v8::Object>::New(args.Holder()));
+    return args.Holder();
+  fail:
+    return throwError(ec);
 }
+
 static v8::Persistent<v8::FunctionTemplate> ConfigureV8TestInterfaceTemplate(v8::Persistent<v8::FunctionTemplate> desc)
 {
     desc->ReadOnlyPrototype();
@@ -55,7 +82,7 @@ static v8::Persistent<v8::FunctionTemplate> ConfigureV8TestInterfaceTemplate(v8:
         0, 0,
         0, 0);
     UNUSED_PARAM(defaultSignature); // In some cases, it will not be used.
-        desc->SetCallHandler(V8TestInterface::constructorCallback);
+    desc->SetCallHandler(V8TestInterface::constructorCallback);
     
 
     // Custom toString template
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp
@@ -54,6 +54,7 @@ static v8::Handle<v8::Value> methodCallback(const v8::Arguments& args)
 static const BatchedCallback TestMediaQueryListListenerCallbacks[] = {
     {"method", TestMediaQueryListListenerInternal::methodCallback},
 };
+
 static v8::Persistent<v8::FunctionTemplate> ConfigureV8TestMediaQueryListListenerTemplate(v8::Persistent<v8::FunctionTemplate> desc)
 {
     desc->ReadOnlyPrototype();
