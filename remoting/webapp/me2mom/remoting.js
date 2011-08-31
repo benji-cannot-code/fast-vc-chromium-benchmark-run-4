38b4991f20bb250293ad3154526f83e46728f7af
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 22:38:09 +0000

    Fixed bad variable reference.
    BUG=None
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7817010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99063 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -364,7 +364,7 @@ function debugInfoCallback_(msg) {
 function showShareError_(errorTag) {
   var errorDiv = document.getElementById('host-plugin-error');
   l10n.localizeElementFromTag(errorDiv, errorTag);
-  remoting.debug.log('Sharing error: ' + errorCode);
+  remoting.debug.log('Sharing error: ' + errorTag);
   remoting.setMode(remoting.AppMode.HOST_SHARE_FAILED);
 }
 
@@ -486,7 +486,7 @@ function startSession_() {
  * @return {void} Nothing.
  */
 function showConnectError_(errorTag) {
-  remoting.debug.log('Connection failed: ' + errorMsg);
+  remoting.debug.log('Connection failed: ' + errorTag);
   var errorDiv = document.getElementById('connect-error-message');
   l10n.localizeElementFromTag(errorDiv, errorTag);
   remoting.accessCode = '';
