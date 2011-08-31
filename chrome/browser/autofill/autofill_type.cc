20d513c259378438789f92cd8c5549f35a07350b
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 04:26:08 +0000

    Eliminate static initialization in autofill_type.cc
    The code prior to this CL invoked a copy constructor during static initialization.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7785016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_type.cc
+++ chrome/browser/autofill/autofill_type.cc
@@ -11,15 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const AutofillType::AutofillTypeDefinition kUnknownAutofillTypeDefinition = {
-  /* UNKNOWN_TYPE */ AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP
-};
-
 AutofillType::AutofillTypeDefinition kAutofillTypeDefinitions[] = {
   // NO_SERVER_DATA
   { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
   // UNKNOWN_TYPE
-  kUnknownAutofillTypeDefinition,
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
   // EMPTY_TYPE
   { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
 
@@ -51,11 +47,11 @@ AutofillType::AutofillTypeDefinition kAutofillTypeDefinitions[] = {
   { AutofillType::PHONE_HOME, AutofillType::PHONE_WHOLE_NUMBER },
 
   // Work phone numbers (values [15,19]) are deprecated.
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
 
   // PHONE_FAX_NUMBER
   { AutofillType::PHONE_FAX, AutofillType::PHONE_NUMBER },
@@ -69,11 +65,11 @@ AutofillType::AutofillTypeDefinition kAutofillTypeDefinitions[] = {
   { AutofillType::PHONE_FAX, AutofillType::PHONE_WHOLE_NUMBER },
 
   // Cell phone numbers (values [25, 29]) are deprecated.
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
 
   // ADDRESS_HOME_LINE1
   { AutofillType::ADDRESS_HOME, AutofillType::ADDRESS_LINE1 },
@@ -106,13 +102,13 @@ AutofillType::AutofillTypeDefinition kAutofillTypeDefinitions[] = {
   { AutofillType::ADDRESS_BILLING, AutofillType::ADDRESS_COUNTRY },
 
   // ADDRESS_SHIPPING values [44,50] are deprecated.
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
-  kUnknownAutofillTypeDefinition,
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
+  { AutofillType::NO_GROUP, AutofillType::NO_SUBGROUP },
 
   // CREDIT_CARD_NAME
   { AutofillType::CREDIT_CARD, AutofillType::NO_SUBGROUP },
