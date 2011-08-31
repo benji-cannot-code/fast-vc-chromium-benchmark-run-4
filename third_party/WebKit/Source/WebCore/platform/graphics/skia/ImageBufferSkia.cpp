25410c1bbfee379f1efdcb5dd7e834562c7ab56e
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-08-31 17:41:21 +0000

    [chromium] Set the GrContext before SkGpuDevice is destroyed. https://bugs.webkit.org/show_bug.cgi?id=67296
    When SkGpuDevice is destroyed, it frees the FBO used for accelerated
    rendering.  If the wrong context is set (e.g., compositor context),
    a crash may result.
    
    Reviewed by Kenneth Russell.
    
    No new tests.  (Unfortunately, wrong-context bugs are difficult to
    express in a layout test).
    
    * platform/graphics/skia/ImageBufferSkia.cpp:
    (WebCore::ImageBuffer::~ImageBuffer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
@@ -117,6 +117,8 @@ ImageBuffer::ImageBuffer(const IntSize& size, ColorSpace, RenderingMode renderin
 
 ImageBuffer::~ImageBuffer()
 {
+    // This is so that the SkGpuDevice destructor has the correct context.
+    m_context->platformContext()->makeGrContextCurrent();
 }
 
 GraphicsContext* ImageBuffer::context() const
