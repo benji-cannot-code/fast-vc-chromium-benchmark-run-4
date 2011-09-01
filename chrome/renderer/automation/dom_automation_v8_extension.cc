4f66a10809e9f5d1d7fb765922427586c7826ee6
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-01 13:46:05 +0000

    renderer/automation: Forward declare v8::Extension in DomAutomationV8Extension header.
    BUG=None
    TEST=None
    
    R=kkania@chromium.org
    
    Review URL: http://codereview.chromium.org/7809012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99177 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/automation/dom_automation_v8_extension.cc
+++ chrome/renderer/automation/dom_automation_v8_extension.cc
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/extensions/extension_base.h"
 #include "grit/renderer_resources.h"
+#include "v8/include/v8.h"
 
-const char* DomAutomationV8Extension::kName = "chrome/domautomation";
+const char kName[] = "chrome/domautomation";
 
 v8::Extension* DomAutomationV8Extension::Get() {
   static v8::Extension* extension =
