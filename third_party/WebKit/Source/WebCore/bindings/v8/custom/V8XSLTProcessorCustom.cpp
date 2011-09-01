6a550ca8c104c70e1f615a9f1fe88a2852b97e6d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 22:16:23 +0000

    Generate an XSLTProcessor constructor of V8 using the IDL 'Constructor' extended attribute https://bugs.webkit.org/show_bug.cgi?id=67414
    Patch by Kentaro Hara <haraken@google.com> on 2011-09-01
    Reviewed by Dimitri Glazkov.
    
    Test: fast/dom/Window/custom-constructors.html
          fast/xsl/default-html.html
    
    * bindings/v8/custom/V8XSLTProcessorCustom.cpp: Removed constructorCallback().
    * xml/XSLTProcessor.idl: Added the 'Constructor' extended attribute.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8XSLTProcessorCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8XSLTProcessorCustom.cpp
@@ -47,13 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8XSLTProcessor::constructorCallback(const v8::Arguments& args)
-{
-    INC_STATS("DOM.XSLTProcessor.Constructor");
-    return V8Proxy::constructDOMObject<XSLTProcessor>(args, &info);
-}
-
-
 v8::Handle<v8::Value> V8XSLTProcessor::importStylesheetCallback(const v8::Arguments& args)
 {
     INC_STATS("DOM.XSLTProcessor.importStylesheet");
