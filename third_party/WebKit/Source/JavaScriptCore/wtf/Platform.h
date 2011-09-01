71848ddd85a0bfb461f01e941ef24f6c4e7463f5
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 21:06:26 +0000

    QNX GCC distribution doesn't support vasprintf() https://bugs.webkit.org/show_bug.cgi?id=67423
    Reviewed by Antonio Gomes.
    
    * wtf/Platform.h: Don't enable HAVE_VASPRINTF when building with GCC on QNX.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -683,7 +683,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if !defined(HAVE_VASPRINTF)
-#if !COMPILER(MSVC) && !COMPILER(RVCT) && !COMPILER(MINGW)
+#if !COMPILER(MSVC) && !COMPILER(RVCT) && !COMPILER(MINGW) && !(COMPILER(GCC) && OS(QNX))
 #define HAVE_VASPRINTF 1
 #endif
 #endif
