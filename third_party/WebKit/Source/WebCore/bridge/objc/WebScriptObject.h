99df23b203ab72668f97d1d1394ca8bb7263cad4
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 23:53:38 +0000

    <rdar://problem/10063411> WebScriptObject.h declares an Objective-C method without a return type.
    Rubber-stamped by Anders Carlsson.
    
    * bridge/objc/WebScriptObject.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/objc/WebScriptObject.h
+++ third_party/WebKit/Source/WebCore/bridge/objc/WebScriptObject.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (BOOL)isKeyExcludedFromWebScript:(const char *)name;
 
 + (id)_convertValueToObjcValue:(JSC::JSValue)value originRootObject:(JSC::Bindings::RootObject*)originRootObject rootObject:(JSC::Bindings::RootObject*)rootObject;
-- _initWithJSObject:(JSC::JSObject*)imp originRootObject:(PassRefPtr<JSC::Bindings::RootObject>)originRootObject rootObject:(PassRefPtr<JSC::Bindings::RootObject>)rootObject;
+- (id)_initWithJSObject:(JSC::JSObject*)imp originRootObject:(PassRefPtr<JSC::Bindings::RootObject>)originRootObject rootObject:(PassRefPtr<JSC::Bindings::RootObject>)rootObject;
 - (JSC::JSObject *)_imp;
 @end
 
