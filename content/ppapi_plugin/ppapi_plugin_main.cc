48a2ae69b83f59336c965b69c4d81711233e383e
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-01 05:43:55 +0000

    Fix Mac PPAPI (out-of-process).
    Oops, we forgot to initialize the WebCore system interface.
    
    TEST=Mac Flapper runs for pages that display text, e.g., YouTube.
    BUG=none
    
    
    Review URL: http://codereview.chromium.org/7818005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99136 0039d316-1c4b-4281-b951-d872f2087c98

--- content/ppapi_plugin/ppapi_plugin_main.cc
+++ content/ppapi_plugin/ppapi_plugin_main.cc
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/src/sandbox.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.h"
+#endif
+
 #if defined(OS_WIN)
 sandbox::TargetServices* g_target_services = NULL;
 #else
@@ -41,6 +45,12 @@ int PpapiPluginMain(const MainFunctionParams& parameters) {
     else
       ChildProcess::WaitForDebugger("Ppapi");
   }
+
+#if defined(OS_MACOSX)
+  // TODO(viettrungluu): This is called in different places in processes that
+  // will run WebKit. This is stupid and error-prone.
+  InitWebCoreSystemInterface();
+#endif
 
   MessageLoop main_message_loop;
   base::PlatformThread::SetName("CrPPAPIMain");
