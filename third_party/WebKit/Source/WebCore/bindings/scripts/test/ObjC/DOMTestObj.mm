d08494e179a11b7ed14bf860b5d8d134e63037f8
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 22:42:20 +0000

    Consider Conditional attribute in CodeGenerator for methods https://bugs.webkit.org/show_bug.cgi?id=67409
    Reviewed by Darin Adler.
    
    This change adds appropriate #if ENABLE() lines for the preprocessor around the
    generated functions depending on the Conditional attribute in the IDL file.
    
    * bindings/scripts/CodeGeneratorCPP.pm:
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorObjC.pm:
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateImplementation):
    * bindings/scripts/test/CPP/WebDOMTestObj.cpp:
    * bindings/scripts/test/CPP/WebDOMTestObj.h:
    * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
    * bindings/scripts/test/GObject/WebKitDOMTestObj.h:
    * bindings/scripts/test/JS/JSTestObj.cpp:
    * bindings/scripts/test/JS/JSTestObj.h:
    * bindings/scripts/test/ObjC/DOMTestObj.h:
    * bindings/scripts/test/ObjC/DOMTestObj.mm:
    * bindings/scripts/test/TestObj.idl:
    * bindings/scripts/test/V8/V8TestObj.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestObj.mm
@@ -679,6 +679,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     IMPL->methodWithNonOptionalArgAndTwoOptionalArgs(nonOpt, opt1, opt2);
 }
 
+
+#if ENABLE(Condition1)
+- (NSString *)conditionalMethod1
+{
+    WebCore::JSMainThreadNullState state;
+    return IMPL->conditionalMethod1();
+}
+
+#endif
+
+
+#if ENABLE(Condition1) && ENABLE(Condition2)
+- (void)conditionalMethod2
+{
+    WebCore::JSMainThreadNullState state;
+    IMPL->conditionalMethod2();
+}
+
+#endif
+
+
+#if ENABLE(Condition1) || ENABLE(Condition2)
+- (void)conditionalMethod3
+{
+    WebCore::JSMainThreadNullState state;
+    IMPL->conditionalMethod3();
+}
+
+#endif
+
 - (void)classMethod
 {
     WebCore::JSMainThreadNullState state;
