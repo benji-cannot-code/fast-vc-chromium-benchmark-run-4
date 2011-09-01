fb6625d8620dd14902f6a4c40abdafc21ee967ed
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 18:10:51 +0000

    Add 'muted' content attribute to HTMLMediaElement https://bugs.webkit.org/show_bug.cgi?id=58369
    Source/WebCore: 
    
    Reviewed by Darin Adler.
    
    Test: media/video-defaultmuted.html
    
    * html/HTMLAttributeNames.in: Add muted.
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadResource): Deal with the 'muted' attribute.
    * html/HTMLMediaElement.idl: Add defaultMuted.
    
    LayoutTests: 
    
    Reviewed by Darin Adler.
    
    * media/video-defaultmuted-expected.txt: Added.
    * media/video-defaultmuted.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
@@ -160,6 +160,7 @@ public:
     void setVolume(float, ExceptionCode&);
     bool muted() const;
     void setMuted(bool);
+
     void togglePlayState();
     void beginScrubbing();
     void endScrubbing();
