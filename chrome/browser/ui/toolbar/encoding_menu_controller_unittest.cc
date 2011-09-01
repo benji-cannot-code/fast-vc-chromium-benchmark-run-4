7819d95ad5fc9fbe2d36be12e7f9624926f7d8ce
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-01 05:40:30 +0000

    Remove static initializer in character_encoding.cc.
    Also fix the naming of const literals.
    
    BUG=94925
    
    
    Review URL: http://codereview.chromium.org/7816001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/encoding_menu_controller_unittest.cc
+++ chrome/browser/ui/toolbar/encoding_menu_controller_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,6 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 class EncodingMenuControllerTest : public testing::Test {
+ public:
+  EncodingMenuControllerTest()
+      : ui_thread_(BrowserThread::UI, &message_loop_) {}
+ private:
+  MessageLoop message_loop_;
+  BrowserThread ui_thread_;
 };
 
 TEST_F(EncodingMenuControllerTest, EncodingIDsBelongTest) {
