f470f158e1544c5b481e14655321775411011987
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-01 00:02:09 +0000

    Add invalid rect tracking and repaint scheduling to Layer.
    This adds a LayerDelegate interface that can be implemented by an object that wants to know when to repaint.
    
    This is a different model to current layer updating, where code must call SetCanvas when it wishes to push an updated canvas to the layer's texture. 
    
    BUG=none
    TEST=see unittest.
    Review URL: http://codereview.chromium.org/7822002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99080 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/test_compositor_host_win.cc
+++ ui/gfx/compositor/test_compositor_host_win.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/compositor/test_compositor_host.h"
 
+#include "base/compiler_specific.h"
 #include "ui/base/win/window_impl.h"
 #include "ui/gfx/compositor/compositor.h"
 
@@ -13,7 +14,9 @@ namespace ui {
 class TestCompositorHostWin : public TestCompositorHost,
                               public ui::WindowImpl {
  public:
-  explicit TestCompositorHostWin(const gfx::Rect& bounds) {
+  TestCompositorHostWin(const gfx::Rect& bounds,
+                        TestCompositorHostDelegate* delegate)
+      : delegate_(delegate) {
     Init(NULL, bounds);
     compositor_ = ui::Compositor::Create(hwnd(), GetSize());
   }
@@ -43,6 +46,9 @@ class TestCompositorHostWin : public TestCompositorHost,
   END_MSG_MAP()
 
   void OnPaint(HDC dc) {
+    compositor_->NotifyStart();
+    delegate_->Draw();
+    compositor_->NotifyEnd();
     ValidateRect(hwnd(), NULL);
   }
 
@@ -53,12 +59,15 @@ class TestCompositorHostWin : public TestCompositorHost,
   }
 
   scoped_refptr<ui::Compositor> compositor_;
+  TestCompositorHostDelegate* delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(TestCompositorHostWin);
 };
 
-TestCompositorHost* TestCompositorHost::Create(const gfx::Rect& bounds) {
-  return new TestCompositorHostWin(bounds);
+TestCompositorHost* TestCompositorHost::Create(
+    const gfx::Rect& bounds,
+    TestCompositorHostDelegate* delegate) {
+  return new TestCompositorHostWin(bounds, delegate);
 }
 
 }  // namespace ui
