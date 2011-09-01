2ef439234b4cd085f813f371fd592d83748e616b
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-01 23:22:03 +0000

    Rename CFNETWORK_DATA_ARRAY_CALLBACK to NETWORK_CFDATA_ARRAY_CALLBACK https://bugs.webkit.org/show_bug.cgi?id=67348
    Reviewed by Alexey Proskuryakov.
    
    * loader/ResourceLoader.h:
    * loader/SubresourceLoader.h:
    * loader/cf/SubresourceLoaderCF.cpp:
    * loader/mac/ResourceLoaderMac.mm:
    * platform/SharedBuffer.cpp:
    (WebCore::SharedBuffer::clear):
    (WebCore::SharedBuffer::buffer):
    * platform/SharedBuffer.h:
    * platform/cf/SharedBufferCF.cpp:
    * platform/network/ResourceHandle.h:
    * platform/network/ResourceHandleClient.h:
    * platform/network/cf/ResourceHandleCFNet.cpp:
    (WebCore::ResourceHandle::createCFURLConnection):
    * platform/network/mac/ResourceHandleMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94358 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cf/SubresourceLoaderCF.cpp
+++ third_party/WebKit/Source/WebCore/loader/cf/SubresourceLoaderCF.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if HAVE(CFNETWORK_DATA_ARRAY_CALLBACK)
+#if HAVE(NETWORK_CFDATA_ARRAY_CALLBACK)
 void SubresourceLoader::didReceiveDataArray(CFArrayRef dataArray)
 {
     // Reference the object in this method since the additional processing can do
--- third_party/WebKit/Source/WebCore/platform/SharedBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/SharedBuffer.h
@@ -93,7 +93,7 @@ public:
     const char* platformData() const;
     unsigned platformDataSize() const;
 
-#if HAVE(CFNETWORK_DATA_ARRAY_CALLBACK)
+#if HAVE(NETWORK_CFDATA_ARRAY_CALLBACK)
     void append(CFDataRef);
 #endif
 
@@ -138,7 +138,7 @@ private:
     mutable Vector<char> m_buffer;
     mutable Vector<char*> m_segments;
     OwnPtr<PurgeableBuffer> m_purgeableBuffer;
-#if HAVE(CFNETWORK_DATA_ARRAY_CALLBACK)
+#if HAVE(NETWORK_CFDATA_ARRAY_CALLBACK)
     mutable Vector<RetainPtr<CFDataRef> > m_dataArray;
     void copyDataArrayAndClear(char *destination, unsigned bytesToCopy) const;
 #endif
