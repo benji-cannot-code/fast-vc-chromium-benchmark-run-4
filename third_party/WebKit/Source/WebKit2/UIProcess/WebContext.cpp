1fb63e4ed615d09dc09e19c16546c26f751d44bf
adachan@apple.com <adachan@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-02 22:21:07 +0000

    Add WKContextGarbageCollectJavaScriptObjects() which does a garbage collection in the WebProcess https://bugs.webkit.org/show_bug.cgi?id=67526
    Reviewed by Darin Adler.
    
    * UIProcess/API/C/WKContext.cpp:
    (WKContextGarbageCollectJavaScriptObjects):
    * UIProcess/API/C/WKContext.h:
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::garbageCollectJavaScriptObjects): Send a message to WebProcess to garbage collect JS objects.
    * UIProcess/WebContext.h:
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::garbageCollectJavaScriptObjects): Call GCController::garbageCollectNow().
    * WebProcess/WebProcess.h:
    * WebProcess/WebProcess.messages.in: Add GarbageCollectJavaScriptObjects message.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
@@ -830,4 +830,9 @@ void WebContext::didGetWebCoreStatistics(const StatisticsData& statisticsData, u
     callback->performCallbackWithReturnValue(statistics.get());
 }
     
+void WebContext::garbageCollectJavaScriptObjects()
+{
+    process()->send(Messages::WebProcess::GarbageCollectJavaScriptObjects(), 0);
+}
+
 } // namespace WebKit
--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.h
@@ -182,6 +182,7 @@ public:
     bool httpPipeliningEnabled();
     
     void getWebCoreStatistics(PassRefPtr<DictionaryCallback>);
+    void garbageCollectJavaScriptObjects();
 
 private:
     WebContext(ProcessModel, const String& injectedBundlePath);
