bbda40b2ab88315ac0252aac4d0300833e8465a6
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 09:36:51 +0000

    Don't use NaCl helper when run under Valgrind TBR=bradchen,eugenis TEST=Valgrind bots go greener BUG=95171 Review URL: http://codereview.chromium.org/7792096
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_fork_delegate_linux.cc
+++ chrome/nacl/nacl_fork_delegate_linux.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
+#include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "content/common/unix_domain_socket_posix.h"
 #include "content/common/zygote_fork_delegate_linux.h"
 #include "chrome/common/chrome_paths.h"
@@ -50,7 +51,8 @@ void NaClForkDelegate::Init(const bool sandboxed,
   FilePath helper_bootstrap_exe;
   if (PathService::Get(chrome::FILE_NACL_HELPER, &helper_exe) &&
       PathService::Get(chrome::FILE_NACL_HELPER_BOOTSTRAP,
-                       &helper_bootstrap_exe)) {
+                       &helper_bootstrap_exe) &&
+      !RunningOnValgrind()) {
     CommandLine::StringVector argv = CommandLine::ForCurrentProcess()->argv();
     argv[0] = helper_bootstrap_exe.value();
     argv[1] = helper_exe.value();
