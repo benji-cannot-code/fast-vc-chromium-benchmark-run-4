835ee1aa3854d011d5208197a462ea083ea1778a
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 16:08:32 +0000

    This file was omitted from my 'fix views unittests' cl last night. Add it back to avoid gclient errors.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99379 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ views/metrics_aura.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "views/metrics.h"
+
+#include <windows.h>
+
+namespace views {
+
+int GetDoubleClickInterval() {
+#if defined(OS_WIN)
+  return ::GetDoubleClickTime();
+#else
+  return 5;
+#endif
+}
+
+int GetMenuShowDelay() {
+#if defined(OS_WIN)
+  static DWORD delay = 0;
+  if (!delay && !SystemParametersInfo(SPI_GETMENUSHOWDELAY, 0, &delay, 0))
+    delay = kDefaultMenuShowDelay;
+  return delay;
+#else
+  return 0;
+#endif;
+}
+
+}  // namespace views
