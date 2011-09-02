6e4469ca6f4923567a9060885fa718f5e5a9b0ba
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 18:15:50 +0000

    webkit/extensions/v8: Forward declare v8::Extension in GCExtension header.
    BUG=None
    TEST=None
    
    R=asargent@chromium.org
    
    Review URL: http://codereview.chromium.org/7828032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99405 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/extensions/v8/gc_extension.h
+++ webkit/extensions/v8/gc_extension.h
@@ -1,18 +1,20 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// The GCExtension is a v8 extension to expose a method into JS for triggering
-// garbage collection.  This should only be used for debugging.
-
 #ifndef WEBKIT_EXTENSIONS_V8_GC_EXTENSION_H_
 #define WEBKIT_EXTENSIONS_V8_GC_EXTENSION_H_
+#pragma once
 
-#include "v8/include/v8.h"
+namespace v8 {
+class Extension;
+}
 
 namespace extensions_v8 {
 
+// GCExtension is a v8 extension to expose a method into JS for triggering
+// garbage collection. This should only be used for debugging.
 class GCExtension {
  public:
   static v8::Extension* Get();
