97cb00ab1773dcf12c4d42e13027631780c930c4
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 03:06:47 +0000

    Modify the Native Client plugin to report progress events. This CL modifies the FileDownloader::Open method to accept an optional 'progress_callback' parameter which is passed to the URL loader using the trusted interface. The Plugin defines a static 'UpdateNexeDownloadProgress' method which it passes to the file downloader when loading nexes. A 5% progress change threshold is used to avoid spamming the Javascript side with progress updates.
    Review URL: http://codereview.chromium.org/7792018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99303 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/file_downloader.h
+++ ppapi/native_client/src/trusted/plugin/file_downloader.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "native_client/src/include/nacl_macros.h"
 #include "native_client/src/include/nacl_string.h"
 #include "ppapi/c/trusted/ppb_file_io_trusted.h"
+#include "ppapi/c/trusted/ppb_url_loader_trusted.h"
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/file_io.h"
 #include "ppapi/cpp/url_loader.h"
@@ -41,6 +42,7 @@ class FileDownloader {
       : instance_(NULL),
         file_open_notify_callback_(pp::BlockUntilComplete()),
         file_io_trusted_interface_(NULL),
+        url_loader_trusted_interface_(NULL),
         open_time_(-1) {}
   ~FileDownloader() {}
 
@@ -52,9 +54,12 @@ class FileDownloader {
   // Returns true when callback is scheduled to be called on success or failure.
   // Returns false if callback is NULL, Initialize() has not been called or if
   // the PPB_FileIO_Trusted interface is not available.
+  // If |progress_callback| is not NULL, it will be invoked for every progress
+  // update received by the loader.
   bool Open(const nacl::string& url,
             DownloadFlags flags,
-            const pp::CompletionCallback& callback);
+            const pp::CompletionCallback& callback,
+            PP_URLLoaderTrusted_StatusCallback progress_callback);
 
   // If downloading and opening succeeded, this returns a valid read-only
   // POSIX file descriptor.  On failure, the return value is an invalid
@@ -106,6 +111,7 @@ class FileDownloader {
   pp::CompletionCallback file_open_notify_callback_;
   pp::FileIO file_reader_;
   const PPB_FileIOTrusted* file_io_trusted_interface_;
+  const PPB_URLLoaderTrusted* url_loader_trusted_interface_;
   pp::URLLoader url_loader_;
   pp::CompletionCallbackFactory<FileDownloader> callback_factory_;
   int64_t open_time_;
