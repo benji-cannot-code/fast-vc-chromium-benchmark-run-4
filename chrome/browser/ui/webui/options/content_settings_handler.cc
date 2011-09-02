184c69801ff09d565ad9c9baadcfdd143cf3c3e2
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 02:32:52 +0000

    Move web intents switch to content_switches.
    R=jam@chromium.org
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/7821010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99292 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/content_settings_handler.cc
+++ chrome/browser/ui/webui/options/content_settings_handler.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/common/content_notification_types.h"
+#include "content/common/content_switches.h"
 #include "content/common/notification_service.h"
 #include "content/common/notification_source.h"
 #include "grit/generated_resources.h"
