3c28e01bfc7f9cb4cc943d7ed4c5fe4dae6ae899
yutak@chromium.org <yutak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-02 11:16:40 +0000

    WebSocket: Send Blob as WebSocket binary message https://bugs.webkit.org/show_bug.cgi?id=67465
    Reviewed by Kent Tamura.
    
    Re-lands r94399 with a fix for Leopard builds.
    
    Source/WebCore:
    
    * bindings/js/JSWebSocketCustom.cpp:
    (WebCore::JSWebSocket::send):
    * bindings/v8/custom/V8WebSocketCustom.cpp:
    (WebCore::V8WebSocket::sendCallback):
    * websockets/ThreadableWebSocketChannel.h:
    * websockets/WebSocket.cpp:
    (WebCore::WebSocket::send):
    * websockets/WebSocket.h:
    * websockets/WebSocket.idl:
    * websockets/WebSocketChannel.cpp:
    (WebCore::WebSocketChannel::send):
    * websockets/WebSocketChannel.h:
    * websockets/WorkerThreadableWebSocketChannel.cpp:
    (WebCore::WorkerThreadableWebSocketChannel::send):
    (WebCore::WorkerThreadableWebSocketChannel::Peer::send):
    (WebCore::WorkerThreadableWebSocketChannel::mainThreadSendBlob):
    (WebCore::WorkerThreadableWebSocketChannel::Bridge::send):
    * websockets/WorkerThreadableWebSocketChannel.h:
    
    LayoutTests:
    
    * http/tests/websocket/tests/hixie76/send-empty-expected.txt: Added.
    * http/tests/websocket/tests/hixie76/send-empty.html: Added.
    * http/tests/websocket/tests/hixie76/send-object-expected.txt: Added.
    * http/tests/websocket/tests/hixie76/send-object.html: Added.
    * http/tests/websocket/tests/hybi/bufferedAmount-after-close-expected.txt:
    * http/tests/websocket/tests/hybi/bufferedAmount-after-close.html:
    * http/tests/websocket/tests/hybi/send-blob-expected.txt: Added.
    * http/tests/websocket/tests/hybi/send-blob.html: Added.
    * http/tests/websocket/tests/hybi/send-blob_wsh.py: Added.
    * http/tests/websocket/tests/hybi/send-empty-expected.txt: Added.
    * http/tests/websocket/tests/hybi/send-empty.html: Added.
    * http/tests/websocket/tests/hybi/send-file-blob-expected.txt: Added.
    * http/tests/websocket/tests/hybi/send-file-blob-fail-expected.txt: Added.
    * http/tests/websocket/tests/hybi/send-file-blob-fail.html: Added.
    * http/tests/websocket/tests/hybi/send-file-blob.html: Added.
    * http/tests/websocket/tests/hybi/send-file-blob_wsh.py: Added.
    * http/tests/websocket/tests/hybi/workers/resources/send-blob.js: Added.
    * http/tests/websocket/tests/hybi/workers/resources/send-blob_wsh.py: Added.
    * http/tests/websocket/tests/hybi/workers/send-blob-expected.txt: Added.
    * http/tests/websocket/tests/hybi/workers/send-blob.html: Added.
    * platform/gtk/Skipped:
    * platform/mac/Skipped:
    * platform/qt/Skipped:
    * platform/win/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/websocket/tests/hybi/workers/resources/send-blob.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function startsWith(target, prefix)
+{
+    return target.indexOf(prefix) === 0;
+}
+
+function createBlobContainingHelloWorld()
+{
+    var builder = new WebKitBlobBuilder();
+    builder.append("Hello, world!");
+    return builder.getBlob();
+}
+
+function createEmptyBlob()
+{
+    var builder = new WebKitBlobBuilder();
+    return builder.getBlob();
+}
+
+function createBlobContainingAllDistinctBytes()
+{
+    var array = new Uint8Array(256);
+    for (var i = 0; i < 256; ++i)
+        array[i] = i;
+    var builder = new WebKitBlobBuilder();
+    builder.append(array.buffer);
+    return builder.getBlob();
+}
+
+var url = "ws://127.0.0.1:8880/websocket/tests/hybi/workers/resources/send-blob";
+var ws = new WebSocket(url);
+
+ws.onopen = function()
+{
+    ws.send(createBlobContainingHelloWorld());
+    ws.send(createEmptyBlob());
+    ws.send(createBlobContainingAllDistinctBytes());
+};
+
+ws.onmessage = function(event)
+{
+    postMessage(event.data);
+};
+
+ws.onclose = function(closeEvent)
+{
+    if (closeEvent.wasClean === true)
+        postMessage("PASS: closeEvent.wasClean is true.");
+    else
+        postMessage("FAIL: closeEvent.wasClean should be true, but was: " + closeEvent.wasClean);
+    postMessage("DONE");
+};
