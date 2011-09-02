1c50957e59e6dd91aa1876323ac7050c48740459
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 03:52:32 +0000

    Use 16x16 icons so they don't stretch.
    R=estade@chromium.org
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/7785006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/favicon_webui_handler.h
+++ chrome/browser/ui/webui/ntp/favicon_webui_handler.h
@@ -14,11 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/favicon/favicon_service.h"
 #include "content/browser/webui/web_ui.h"
 
+class ExtensionIconColorManager;
 class GURL;
 class Profile;
 
 namespace base {
 class ListValue;
+class StringValue;
 }
 
 class FaviconWebUIHandler : public WebUIMessageHandler {
@@ -30,11 +32,16 @@ class FaviconWebUIHandler : public WebUIMessageHandler {
   virtual void RegisterMessages() OVERRIDE;
 
   void HandleGetFaviconDominantColor(const base::ListValue* args);
+  void HandleGetAppIconDominantColor(const base::ListValue* args);
+
+  // Callback getting signal that an app icon is loaded.
+  void NotifyAppIconReady(const std::string& extension_id);
 
  private:
   // Called when favicon data is available from the history backend.
   void OnFaviconDataAvailable(FaviconService::Handle request_handle,
                               history::FaviconData favicon);
+  base::StringValue* GetDominantColor(scoped_refptr<RefCountedMemory> png);
 
   CancelableRequestConsumerTSimple<int> consumer_;
 
@@ -45,6 +52,12 @@ class FaviconWebUIHandler : public WebUIMessageHandler {
   // A mapping of favicon ID to callback names for requests that are
   // in-progress.
   std::map<int, std::string> callbacks_map_;
+
+  // A mapping of extension ID to callback name for app requests in progress.
+  std::map<std::string, std::string> app_callbacks_map_;
+
+  // Manage retrieval of icons from apps.
+  scoped_ptr<ExtensionIconColorManager> app_icon_color_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(FaviconWebUIHandler);
 };
