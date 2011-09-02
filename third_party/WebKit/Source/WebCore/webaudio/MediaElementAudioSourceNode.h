27f32d9798d7d262a34b5004658f3d49d4d8d795
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-02 07:35:13 +0000

    [WebAudio] Undeclared dependency to VIDEO https://bugs.webkit.org/show_bug.cgi?id=66893
    Reviewed by Kenneth Russell.
    
    Build MediaAudioSourceNode only if VIDEO is enabled
    
    * webaudio/AudioContext.cpp:
    * webaudio/AudioContext.h:
    * webaudio/AudioContext.idl:
    * webaudio/MediaElementAudioSourceNode.cpp:
    * webaudio/MediaElementAudioSourceNode.h:
    * webaudio/MediaElementAudioSourceNode.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/webaudio/MediaElementAudioSourceNode.h
+++ third_party/WebKit/Source/WebCore/webaudio/MediaElementAudioSourceNode.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MediaElementAudioSourceNode_h
 #define MediaElementAudioSourceNode_h
 
+#if ENABLE(VIDEO)
+
 #include "AudioSourceNode.h"
 #include "HTMLMediaElement.h"
 #include <wtf/PassRefPtr.h>
@@ -58,5 +60,7 @@ private:
 };
 
 } // namespace WebCore
+
+#endif // ENABLE(VIDEO)
 
 #endif // MediaElementAudioSourceNode_h
