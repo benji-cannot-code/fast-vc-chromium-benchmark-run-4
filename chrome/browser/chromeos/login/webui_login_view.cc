d66fbc6e7f3a8cdf02c58abe34e4216e6f29a0b7
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 04:50:57 +0000

    [ChromeOS] Make WebUI OOBE/Login rtl friendly.
    - Change how #oobe is centered:
        top/left: 50% - margin ->  -webkit-box-align:center
    - Change how progress dots and button strips are laid:
        float:left/right -> display:-webkit-box and let webkit handle the direction;
    - Add rtl specific rules for left/right transition and image preview element on image screen;
    - Mirrox X position of status area for rtl;
    
    BUG=chromium-os:18696, chromium-os:19798
    TEST=Verify fix for chromium-os:18696, chromium-os:19798.
    
    
    Review URL: http://codereview.chromium.org/7785017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99321 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_view.cc
+++ chrome/browser/chromeos/login/webui_login_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/webui_login_view.h"
 
+#include "base/i18n/rtl.h"
 #include "chrome/browser/chromeos/accessibility_util.h"
 #include "chrome/browser/chromeos/login/proxy_settings_dialog.h"
 #include "chrome/browser/chromeos/login/webui_login_display.h"
@@ -229,10 +230,13 @@ void WebUILoginView::InitStatusArea() {
 
   views::Widget* login_window = WebUILoginDisplay::GetLoginWindow();
   gfx::Size size = status_area_->GetPreferredSize();
-  gfx::Rect bounds(width() - size.width() - kStatusAreaCornerPadding,
-                   kStatusAreaCornerPadding,
-                   size.width(),
-                   size.height());
+  gfx::Rect bounds(
+      base::i18n::IsRTL() ?
+          kStatusAreaCornerPadding :
+          width() - size.width() - kStatusAreaCornerPadding,
+      kStatusAreaCornerPadding,
+      size.width(),
+      size.height());
 #if defined(TOUCH_UI)
   // TODO(oshima): Window manager doesn't know about touch event, hence can't
   // activate the window. Use POPUP for now. This will be non issue
