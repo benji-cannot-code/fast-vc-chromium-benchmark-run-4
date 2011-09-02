c958e5dcadcca087291e5f6453fa9d347084d08b
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 22:42:04 +0000

    Revert 99442 - Add test for calling PostMessage during Init. Deflakify TestPostMessage.
    BUG=93260,91768
    TEST=This test
    
    Review URL: http://codereview.chromium.org/7821001
    
    TBR=dmichael@chromium.org
    Review URL: http://codereview.chromium.org/7737004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99466 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_post_message.h
+++ ppapi/tests/test_post_message.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TestPostMessage : public TestCase {
  public:
-  explicit TestPostMessage(TestingInstance* instance);
-  virtual ~TestPostMessage();
+  explicit TestPostMessage(TestingInstance* instance) : TestCase(instance) {}
 
  private:
   // TestCase implementation.
@@ -39,15 +38,6 @@ class TestPostMessage : public TestCase {
   // calling removeEventListener for each.
   // Returns true on success, false on failure.
   bool ClearListeners();
-
-  // Wait for pending messages; return the number of messages that were pending
-  // at the time of invocation.
-  int WaitForMessages();
-
-  // Test that we can send a message from Instance::Init. Note the actual
-  // message is sent in TestPostMessage::Init, and this test simply makes sure
-  // we got it.
-  std::string TestSendInInit();
 
   // Test some basic functionality;  make sure we can send data successfully
   // in both directions.
