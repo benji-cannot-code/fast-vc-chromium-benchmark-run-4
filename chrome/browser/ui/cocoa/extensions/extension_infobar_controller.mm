5b1fc1cf3b2bf6085a647a0e8290b27293bb2d97
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 00:22:33 +0000

    OSX: Use infobar's self-reported height.
    Always use infobar's self-reported height, rather than dynamically calculated value.
    
    BUG=92656
    TEST=Extensions provided in bug 54288.
    
    
    Review URL: http://codereview.chromium.org/7825032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99489 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/extension_infobar_controller.mm
+++ chrome/browser/ui/cocoa/extensions/extension_infobar_controller.mm
@@ -243,8 +243,8 @@ class InfobarBridge : public ExtensionInfoBarDelegate::DelegateObserver,
 }
 
 - (CGFloat)clampedExtensionViewHeight {
-  return std::max(kToolbarMinHeightPx,
-      std::min(NSHeight([extensionView_ frame]), kToolbarMaxHeightPx));
+  CGFloat height = delegate_->AsExtensionInfoBarDelegate()->height();
+  return std::max(kToolbarMinHeightPx, std::min(height, kToolbarMaxHeightPx));
 }
 
 - (void)adjustExtensionViewSize {
