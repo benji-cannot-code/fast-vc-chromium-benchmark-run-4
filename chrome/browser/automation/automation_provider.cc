d68334f650166ab368c8576cf1ed3355bbe0b777
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 03:14:17 +0000

    [chromeos] Automation should wait for webui login only in login manager mode
    This fixes the use case of pyauto functional tests.
    
    BUG=None
    TEST=desktopui_PyAutoFunctionalTests
    
    R=xiyuan@chromium.org
    
    
    Review URL: http://codereview.chromium.org/7835012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -101,6 +101,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/external_tab_container_win.h"
 #endif  // defined(OS_WIN)
 
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/login/user_manager.h"
+#endif  // defined(OS_CHROMEOS)
+
 using WebKit::WebFindOptions;
 using base::Time;
 
@@ -171,10 +175,12 @@ bool AutomationProvider::InitializeChannel(const std::string& channel_id) {
 #if defined(OS_CHROMEOS)
   // Wait for webui login to be ready.
   // Observer will delete itself.
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kWebUILogin)) {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kWebUILogin) &&
+      !chromeos::UserManager::Get()->user_is_logged_in()) {
     login_webui_ready_ = false;
     new LoginWebuiReadyObserver(this);
   }
+
   // Wait for the network manager to initialize.
   // The observer will delete itself when done.
   network_library_initialized_ = false;
