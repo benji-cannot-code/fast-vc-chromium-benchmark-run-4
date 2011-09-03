032f4533d87179ae9213fd28462b3c5d0a62058e
nfullagar@google.com <nfullagar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 21:52:22 +0000

    Update graphics3d proxy BUG= none TEST= ppb_graphics3d Review URL: http://codereview.chromium.org/7828054
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99577 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/shared/ppapi_proxy/untrusted/srpcgen/ppb_rpc.h
+++ ppapi/native_client/src/shared/ppapi_proxy/untrusted/srpcgen/ppb_rpc.h
@@ -479,6 +479,12 @@ class PpbGraphics2DRpcClient {
 
 class PpbGraphics3DRpcClient {
  public:
+  static NaClSrpcError PPB_Graphics3D_GetAttribMaxValue(
+      NaClSrpcChannel* channel,
+      PP_Instance instance,
+      int32_t attribute,
+      int32_t* value,
+      int32_t* pp_error);
   static NaClSrpcError PPB_Graphics3D_Create(
       NaClSrpcChannel* channel,
       PP_Instance instance,
@@ -495,6 +501,10 @@ class PpbGraphics3DRpcClient {
       NaClSrpcChannel* channel,
       PP_Resource context,
       nacl_abi_size_t attrib_list_bytes, int32_t* attrib_list,
+      int32_t* pp_error);
+  static NaClSrpcError PPB_Graphics3D_GetError(
+      NaClSrpcChannel* channel,
+      PP_Resource context,
       int32_t* pp_error);
   static NaClSrpcError PPB_Graphics3D_SwapBuffers(
       NaClSrpcChannel* channel,
@@ -977,3 +987,4 @@ class PpbZoomRpcClient {
 
 
 #endif  // GEN_PPAPI_PROXY_PPB_RPC_H_
+
