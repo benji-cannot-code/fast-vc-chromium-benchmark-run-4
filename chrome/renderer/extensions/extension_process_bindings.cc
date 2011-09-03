f3b5161889fc38274a157bfe1a81ba59f16bc8d3
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 03:38:47 +0000

    Move binding_utils::GetCurrentRenderView() to ExtensionBase and rename ExtensionBase::GetCurrentExtension() and CheckPermissionsForCurrentContext() to make it clear that they actually test the current RenderView.
    I'm moving GetCurrentRenderView() because bindings_utils.* is going away in a subsequent change and I needed someplace to put it.
    
    It's OK with me to have it protected because we really shouldn't be using this so much. It is pretty bad to assume anything about a context based on its containing RenderView.
    
    Review URL: http://codereview.chromium.org/7824018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_process_bindings.cc
+++ chrome/renderer/extensions/extension_process_bindings.cc
@@ -225,7 +225,7 @@ class ExtensionImpl : public ExtensionBase {
 
     ExtensionImpl* v8_extension = GetFromArguments<ExtensionImpl>(args);
     const ::Extension* extension =
-        v8_extension->GetExtensionForCurrentContext();
+        v8_extension->GetExtensionForCurrentRenderView();
     if (!extension)
       return v8::Undefined();
 
@@ -279,7 +279,7 @@ class ExtensionImpl : public ExtensionBase {
 
     ExtensionImpl* v8_extension = GetFromArguments<ExtensionImpl>(args);
     const ::Extension* extension =
-        v8_extension->GetExtensionForCurrentContext();
+        v8_extension->GetExtensionForCurrentRenderView();
     if (!extension)
       return v8::Undefined();
     if (allowed_ids.end() == std::find(
@@ -340,7 +340,7 @@ class ExtensionImpl : public ExtensionBase {
   static v8::Handle<v8::Value> OpenChannelToTab(const v8::Arguments& args) {
     // Get the current RenderView so that we can send a routed IPC message from
     // the correct source.
-    RenderView* renderview = bindings_utils::GetRenderViewForCurrentContext();
+    RenderView* renderview = GetCurrentRenderView();
     if (!renderview)
       return v8::Undefined();
 
@@ -403,7 +403,7 @@ class ExtensionImpl : public ExtensionBase {
 
     // Get the current RenderView so that we can send a routed IPC message from
     // the correct source.
-    RenderView* renderview = bindings_utils::GetRenderViewForCurrentContext();
+    RenderView* renderview = GetCurrentRenderView();
     if (!renderview)
       return v8::Undefined();
 
@@ -415,7 +415,7 @@ class ExtensionImpl : public ExtensionBase {
       return v8::Undefined();
     }
 
-    if (!v8_extension->CheckPermissionForCurrentContext(name))
+    if (!v8_extension->CheckPermissionForCurrentRenderView(name))
       return v8::Undefined();
 
     GURL source_url;
@@ -540,7 +540,7 @@ class ExtensionImpl : public ExtensionBase {
   }
 
   static v8::Handle<v8::Value> GetRenderViewId(const v8::Arguments& args) {
-    RenderView* renderview = bindings_utils::GetRenderViewForCurrentContext();
+    RenderView* renderview = GetCurrentRenderView();
     if (!renderview)
       return v8::Undefined();
     return v8::Integer::New(renderview->routing_id());
