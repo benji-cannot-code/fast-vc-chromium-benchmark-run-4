1027bde120dab33d3cb83a5eca451b6abcd511d5
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 21:17:31 +0000

    Remove unused g_timeout_ms from PageLoadTest code TEST=trybots
    Review URL: http://codereview.chromium.org/7825029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/reliability/page_load_test.cc
+++ chrome/test/reliability/page_load_test.cc
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // --memoryusage: prints out memory usage when visiting each page.
 // --endurl=url: visits the specified url in the end.
 // --logfile=filepath: saves the visit log to the specified path.
-// --timeout=millisecond: time out as specified in millisecond during each
-//                        page load.
 // --nopagedown: won't simulate page down key presses after page load.
 // --noclearprofile: do not clear profile dir before firing up each time.
 // --savedebuglog: save Chrome, V8, and test debug log for each page loaded.
@@ -87,7 +85,6 @@ const char kContinuousLoadSwitch[] = "continuousload";
 const char kMemoryUsageSwitch[] = "memoryusage";
 const char kEndURLSwitch[] = "endurl";
 const char kLogFileSwitch[] = "logfile";
-const char kTimeoutSwitch[] = "timeout";
 const char kNoPageDownSwitch[] = "nopagedown";
 const char kNoClearProfileSwitch[] = "noclearprofile";
 const char kSaveDebugLogSwitch[] = "savedebuglog";
@@ -120,7 +117,6 @@ bool g_page_down = true;
 bool g_clear_profile = true;
 std::string g_end_url;
 FilePath g_log_file_path;
-int g_timeout_ms = -1;
 bool g_save_debug_log = false;
 FilePath g_chrome_log_path;
 FilePath g_v8_log_path;
@@ -371,9 +367,6 @@ class PageLoadTest : public UITest {
         return;
       // For usage 1
       NavigationMetrics metrics;
-      if (g_timeout_ms == -1)
-        g_timeout_ms = 2000;
-
       // Though it would be nice to test the page down code path in usage 1,
       // enabling page down adds several seconds to the test and does not seem
       // worth the tradeoff. It is also potentially disruptive when running the
@@ -785,14 +778,6 @@ void SetPageRange(const CommandLine& parsed_command_line) {
 
   if (parsed_command_line.HasSwitch(kLogFileSwitch))
     g_log_file_path = parsed_command_line.GetSwitchValuePath(kLogFileSwitch);
-
-  if (parsed_command_line.HasSwitch(kTimeoutSwitch)) {
-    ASSERT_TRUE(
-        base::StringToInt(parsed_command_line.GetSwitchValueASCII(
-                          kTimeoutSwitch),
-                          &g_timeout_ms));
-    ASSERT_GT(g_timeout_ms, 0);
-  }
 
   if (parsed_command_line.HasSwitch(kNoPageDownSwitch))
     g_page_down = false;
