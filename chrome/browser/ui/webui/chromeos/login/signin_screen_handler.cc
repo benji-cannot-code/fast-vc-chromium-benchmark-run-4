6375b78a6f967a459e4fd516721d2534b38e39f1
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-03 17:38:10 +0000

    [ChromeOS] Update Gaia side pane per mock.
    - Add text around the createAccount and guestSignin links per mock;
    - Fix a problem and sign-in error bubble is off position when Gaia side pane is hidden;
    
    BUG=chromium-os:19927
    TEST=Verify fix for chromium-os:19927 and verify whitelist failure error bubble is on Gaia regardless of whether the side pane is visible or not.
    
    
    Review URL: http://codereview.chromium.org/7779021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99565 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -162,9 +162,9 @@ void SigninScreenHandler::GetLocalizedStrings(
   localized_strings->SetString("offlineMessageBody",
       l10n_util::GetStringUTF16(IDS_LOGIN_OFFLINE_MESSAGE));
   localized_strings->SetString("createAccount",
-      l10n_util::GetStringUTF16(IDS_CREATE_ACCOUNT_BUTTON));
+      l10n_util::GetStringUTF16(IDS_CREATE_ACCOUNT_HTML));
   localized_strings->SetString("guestSignin",
-      l10n_util::GetStringUTF16(IDS_BROWSE_WITHOUT_SIGNING_IN_BUTTON));
+      l10n_util::GetStringUTF16(IDS_BROWSE_WITHOUT_SIGNING_IN_HTML));
   localized_strings->SetString("removeUser",
       l10n_util::GetStringUTF16(IDS_LOGIN_REMOVE));
 
@@ -287,6 +287,7 @@ void SigninScreenHandler::ShowSigninScreenForCreds(
   DictionaryValue params;
   params.SetString("startUrl", kGaiaExtStartPage);
   params.SetString("email", email_);
+  email_.clear();
 
   const std::string app_locale = g_browser_process->GetApplicationLocale();
   if (!app_locale.empty())
@@ -296,7 +297,7 @@ void SigninScreenHandler::ShowSigninScreenForCreds(
       UserCrosSettingsProvider::cached_allow_new_user());
   params.SetBoolean("guestSignin",
       UserCrosSettingsProvider::cached_allow_guest());
-  email_.clear();
+
   // Test automation data:
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kAuthExtensionPath)) {
