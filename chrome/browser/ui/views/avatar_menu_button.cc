090895a3b576aded45b7a0698d39c8cf1cc85530
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-04 05:03:42 +0000

    Fix top crasher on the AvatarMenuButton destructor
    like crash/reportdetail?reportid=01290260a34c8b50
    
    
    Because GetTopLevelWidget() is not valid, in the call
    
    GetNativeHandle() {
      
      return GetWidget()->GetTopLevelWidget()->GetNativeWindow();
    
    }
    
    I see no other solution than to test if the whole window is going away.
    
    TBR=ben
    
    BUG=95232
    TEST= see bug
    Review URL: http://codereview.chromium.org/7834013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu_button.cc
+++ chrome/browser/ui/views/avatar_menu_button.cc
@@ -35,8 +35,6 @@ void DrawTaskBarDecoration(const Browser* browser, const SkBitmap* bitmap) {
 #if defined(OS_WIN)
   if (base::win::GetVersion() < base::win::VERSION_WIN7)
     return;
-  // During destruction of the browser frame, we might not have a window
-  // so the taksbar button will be removed by windows anyway.
   BrowserWindow* bw = browser->window();
   if (!bw)
     return;
@@ -76,6 +74,10 @@ AvatarMenuButton::AvatarMenuButton(Browser* browser, bool has_menu)
 }
 
 AvatarMenuButton::~AvatarMenuButton() {
+  // During destruction of the browser frame, we might not have a window
+  // so the taskbar button will be removed by windows anyway.
+  if (browser_->IsAttemptingToCloseBrowser())
+    return;
   DrawTaskBarDecoration(browser_, NULL);
 }
 
