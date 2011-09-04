cfa7950bd6f286d0b3f2a9342febe4005e78ec81
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-04 02:58:46 +0000

    REGRESSION (r86268): Fix for qt_networkAccessAllowed() https://bugs.webkit.org/show_bug.cgi?id=67570
    Reviewed by Noam Rosenthal.
    
    Source/WebCore:
    
    No new tests as this change only removes dead code.
    Support for Qt 4.6 has  been removed a while back.
    
    * WebCore.pri:
    * WebCore.pro:
    * features.pri:
    * platform/network/NetworkStateNotifier.h:
    * platform/network/qt/NetworkStateNotifierQt.cpp:
    
    Source/WebKit/qt:
    
    Replace the ENABLE(QT_BEARER) guard with
    !defined(QT_NO_BEARERMANAGEMENT) which is true by
    default in Qt 4.7 or later versions.
    
    * Api/qwebsettings.cpp:
    (qt_networkAccessAllowed):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/NetworkStateNotifier.h
+++ third_party/WebKit/Source/WebCore/platform/network/NetworkStateNotifier.h
@@ -46,16 +46,11 @@ typedef const struct __SCDynamicStore * SCDynamicStoreRef;
 
 #include <QtCore/qglobal.h>
 
-#ifdef QT_NO_BEARERMANAGEMENT
-#undef WTF_USE_QT_BEARER
-#define WTF_USE_QT_BEARER 0
-#endif
-
 #endif
 
 namespace WebCore {
 
-#if (PLATFORM(QT) && USE(QT_BEARER))
+#if (PLATFORM(QT) && !defined(QT_NO_BEARERMANAGEMENT))
 class NetworkStateNotifierPrivate;
 #endif
 
@@ -67,7 +62,7 @@ public:
 
     bool onLine() const { return m_isOnLine; }
 
-#if (PLATFORM(QT) && USE(QT_BEARER))
+#if (PLATFORM(QT) && !defined(QT_NO_BEARERMANAGEMENT))
     void setNetworkAccessAllowed(bool);
 #elif PLATFORM(CHROMIUM) || PLATFORM(EFL)
     void setOnLine(bool);
@@ -96,13 +91,13 @@ private:
     HANDLE m_waitHandle;
     OVERLAPPED m_overlapped;
 
-#elif PLATFORM(QT) && USE(QT_BEARER)
+#elif (PLATFORM(QT) && !defined(QT_NO_BEARERMANAGEMENT))
     friend class NetworkStateNotifierPrivate;
     NetworkStateNotifierPrivate* p;
 #endif
 };
 
-#if !PLATFORM(MAC) && !PLATFORM(WIN) && !(PLATFORM(QT) && USE(QT_BEARER))
+#if !PLATFORM(MAC) && !PLATFORM(WIN) && !(PLATFORM(QT) && !defined(QT_NO_BEARERMANAGEMENT))
 
 inline NetworkStateNotifier::NetworkStateNotifier()
     : m_isOnLine(true)
--- third_party/WebKit/Source/WebCore/platform/network/qt/NetworkStateNotifierQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/qt/NetworkStateNotifierQt.cpp
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "NetworkStateNotifier.h"
 
-#if PLATFORM(QT) && USE(QT_BEARER)
+#if (PLATFORM(QT) && !defined(QT_NO_BEARERMANAGEMENT))
 
 #include "NetworkStateNotifierPrivate.h"
 #include "qnetworkconfigmanager.h"
