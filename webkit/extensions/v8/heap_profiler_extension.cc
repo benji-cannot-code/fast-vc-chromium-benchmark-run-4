5cf95bd5852a3e94508d7a6fd63fd90c3079f904
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-04 02:04:57 +0000

    webkit/extensions/v8: Forward declare v8::Extension in HeapProfilerExtension header.
    R=willchan@chromium.org
    
    Review URL: http://codereview.chromium.org/7800019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99585 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/extensions/v8/heap_profiler_extension.cc
+++ webkit/extensions/v8/heap_profiler_extension.cc
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "webkit/extensions/v8/heap_profiler_extension.h"
 
 #include "base/basictypes.h"
+#include "v8/include/v8.h"
 
 #if defined(USE_TCMALLOC) && !defined(OS_WIN)
 #include "third_party/tcmalloc/chromium/src/google/heap-profiler.h"
