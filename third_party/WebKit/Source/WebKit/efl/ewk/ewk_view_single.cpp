eea7aca73629ab1a28311d8027d192231ff84ef4
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-05 04:18:04 +0000

    [EFL] Rename ewk_view_(single|tiled).c to ewk_view_(single|tiled).cpp https://bugs.webkit.org/show_bug.cgi?id=66542
    Use a C++ compiler for these files, so that they can include the C++-using
    ewk_private.h.
    
    Reviewed by Daniel Bates.
    
    * CMakeListsEfl.txt:
    * ewk/ewk_view_single.cpp: Renamed from Source/WebKit/efl/ewk/ewk_view_single.c.
    (_ewk_view_single_on_del):
    (_ewk_view_single_smart_add):
    (_ewk_view_single_smart_backing_store_add):
    (_ewk_view_single_smart_resize):
    (_ewk_view_4b_move_region_up):
    (_ewk_view_4b_move_region_down):
    (_ewk_view_4b_move_line_left):
    (_ewk_view_4b_move_line_right):
    (_ewk_view_4b_move_region_left):
    (_ewk_view_4b_move_region_right):
    (_ewk_view_4b_move_region):
    (_ewk_view_single_scroll_process_single):
    (_ewk_view_single_smart_scrolls_process):
    (_ewk_view_single_smart_repaints_process):
    (_ewk_view_single_smart_zoom_weak_set):
    (_ewk_view_single_smart_zoom_weak_smooth_scale_set):
    (_ewk_view_single_smart_bg_color_set):
    (ewk_view_single_smart_set):
    (_ewk_view_single_smart_class_new):
    (ewk_view_single_add):
    * ewk/ewk_view_tiled.cpp: Renamed from Source/WebKit/efl/ewk/ewk_view_tiled.c.
    (_ewk_view_tiled_render_cb):
    (_ewk_view_tiled_updates_process_pre):
    (_ewk_view_tiled_smart_backing_store_add):
    (_ewk_view_tiled_contents_size_changed_cb):
    (_ewk_view_tiled_smart_add):
    (_ewk_view_tiled_smart_scrolls_process):
    (_ewk_view_tiled_smart_repaints_process):
    (_ewk_view_tiled_smart_contents_resize):
    (_ewk_view_tiled_smart_zoom_set):
    (_ewk_view_tiled_smart_zoom_weak_set):
    (_ewk_view_tiled_smart_zoom_weak_smooth_scale_set):
    (_ewk_view_tiled_smart_flush):
    (_ewk_view_tiled_smart_pre_render_region):
    (_ewk_view_tiled_smart_pre_render_relative_radius):
    (_ewk_view_tiled_smart_pre_render_cancel):
    (_ewk_view_tiled_smart_disable_render):
    (_ewk_view_tiled_smart_enable_render):
    (ewk_view_tiled_smart_set):
    (_ewk_view_tiled_smart_class_new):
    (ewk_view_tiled_add):
    (ewk_view_tiled_unused_cache_get):
    (ewk_view_tiled_unused_cache_set):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_single.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_single.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ewk_frame.h"
 #include "ewk_logging.h"
+#include "ewk_private.h"
 
 #include <Evas.h>
 #include <eina_safety_checks.h>
--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_tiled.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_tiled.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_view.h"
 
 #include "ewk_logging.h"
+#include "ewk_private.h"
 
 #include <Evas.h>
 #include <eina_safety_checks.h>
