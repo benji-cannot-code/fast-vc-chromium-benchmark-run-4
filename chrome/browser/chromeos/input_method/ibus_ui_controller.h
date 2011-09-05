125eef5b3d1240b544cd2d0106b015f4e9686e76
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-05 08:36:19 +0000

    Revert "Add "InfoList" for input methods on ChromeOS"
    This reverts commit 698c3c39311a91b7f34bfab8e1523b3d97a7a1ba.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7779031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/ibus_ui_controller.h
+++ chrome/browser/chromeos/input_method/ibus_ui_controller.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/observer_list.h"
-#include "third_party/mozc/session/commands.pb.h"
 
 namespace chromeos {
 namespace input_method {
@@ -84,9 +83,6 @@ struct InputMethodLookupTable {
 
   // Annotation strings in UTF-8 (ex. "Hankaku Katakana").
   std::vector<std::string> annotations;
-
-  // Mozc candidates.
-  mozc::commands::Candidates mozc_candidates;
 };
 
 // IBusUiController is used to interact with the IBus daemon.
