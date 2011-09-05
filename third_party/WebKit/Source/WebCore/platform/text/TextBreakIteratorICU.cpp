63ec83d7408c517294ea1276469cddfd8d86c3de
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-05 12:08:12 +0000

    [Qt] Compiling using system ICU uses QTextBreakIterator in some cases. https://bugs.webkit.org/show_bug.cgi?id=67391
    Patch by Alexander Færøy <alexander.faeroy@nokia.com> on 2011-09-05
    Reviewed by Kenneth Rohde Christiansen.
    
    * platform/text/TextBoundaries.cpp:
    * platform/text/TextBreakIteratorICU.cpp:
    * platform/text/qt/TextBoundariesQt.cpp:
    * platform/text/qt/TextBreakIteratorQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/TextBreakIteratorICU.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/TextBreakIteratorICU.cpp
@@ -30,6 +30,8 @@ using namespace std;
 
 namespace WebCore {
 
+#if USE(ICU_UNICODE)
+
 static TextBreakIterator* setUpIterator(bool& createdIterator, TextBreakIterator*& iterator,
     UBreakIteratorType type, const UChar* string, int length)
 {
@@ -251,5 +253,7 @@ TextBreakIterator* cursorMovementIterator(const UChar* string, int length)
     static TextBreakIterator* staticCursorMovementIterator;
     return setUpIteratorWithRules(createdCursorMovementIterator, staticCursorMovementIterator, kRules, string, length);
 }
+
+#endif
 
 }
--- third_party/WebKit/Source/WebCore/platform/text/qt/TextBoundariesQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/qt/TextBoundariesQt.cpp
@@ -34,12 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QString>
 #include <QChar>
 
-#include <QDebug>
-#include <stdio.h>
-
 #include <qtextboundaryfinder.h>
 
 namespace WebCore {
+
+#if !USE(ICU_UNICODE)
 
 int findNextWordFromIndex(UChar const* buffer, int len, int position, bool forward)
 {
@@ -73,6 +72,8 @@ void findWordBoundary(UChar const* buffer, int len, int position, int* start, in
     *start = position > 0 ? iterator.toPreviousBoundary() : 0;
     *end = position == len ? len : iterator.toNextBoundary();
 }
+
+#endif
 
 }
 
