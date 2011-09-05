a15adf6ab4143a5292b9f74fbb572708bcee951a
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-05 11:03:02 +0000

    DOMUI Prefs: willHidePage() called always before hiding a page.
    willHidePage() now called for preferences pages that are hidden due to Escape press or click on a top-level page, not only due to navigateToPage() call.
    
    BUG=None
    TEST=No observable changes in behaviour
    
    Review URL: http://codereview.chromium.org/7830040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_page.js
+++ chrome/browser/resources/options/options_page.js
@@ -296,8 +296,11 @@ cr.define('options', function() {
    */
   OptionsPage.closeTopSubPage_ = function() {
     var topPage = this.getTopmostVisiblePage();
-    if (topPage && !topPage.isOverlay && topPage.parentPage)
+    if (topPage && !topPage.isOverlay && topPage.parentPage) {
+      if (topPage.willHidePage)
+        topPage.willHidePage();
       topPage.visible = false;
+    }
 
     this.updateHistoryState_();
   };
@@ -309,6 +312,8 @@ cr.define('options', function() {
   OptionsPage.closeSubPagesToLevel = function(level) {
     var topPage = this.getTopmostVisiblePage();
     while (topPage && topPage.nestingLevel > level) {
+      if (topPage.willHidePage)
+        topPage.willHidePage();
       topPage.visible = false;
       topPage = topPage.parentPage;
     }
