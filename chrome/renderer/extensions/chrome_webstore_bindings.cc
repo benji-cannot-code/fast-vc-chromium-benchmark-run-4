a9738fecdfcbe4750ab15930cc016f6b157f22c0
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 04:59:00 +0000

    Reland r99689: Refactor the ContextInfo struct from bindings_utils into a real class. Pull functionality from bindings_utils and event_bindings in as methods. Simplify lifetime management.
    Review URL: http://codereview.chromium.org/7834035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99696 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/chrome_webstore_bindings.cc
+++ chrome/renderer/extensions/chrome_webstore_bindings.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/string_util.h"
 #include "chrome/common/extensions/extension.h"
-#include "chrome/renderer/extensions/bindings_utils.h"
 #include "chrome/renderer/extensions/extension_helper.h"
 #include "content/renderer/render_view.h"
 #include "googleurl/src/gurl.h"
