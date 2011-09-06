31edb24d2a57c365f178b1006b10c6846917b2c2
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 16:11:07 +0000

    For Touch: remove some menu items for experimentation, adjust sizes. Also removed painting keyboard accelerators.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7825024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99745 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/downloads_dom_handler.cc
+++ chrome/browser/ui/webui/downloads_dom_handler.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/fileicon_source.h"
-#include "chrome/browser/ui/webui/fileicon_source_cros.h"
+#include "chrome/browser/ui/webui/fileicon_source_chromeos.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/browser_thread.h"
--- chrome/browser/ui/webui/fileicon_source_chromeos.h
+++ chrome/browser/ui/webui/fileicon_source_chromeos.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CROS_H_
-#define CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CROS_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CHROMEOS_H_
+#define CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CHROMEOS_H_
 #pragma once
 
 #include <string>
@@ -37,5 +37,5 @@ class FileIconSourceCros : public ChromeURLDataManager::DataSource {
   DISALLOW_COPY_AND_ASSIGN(FileIconSourceCros);
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CROS_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_FILEICON_SOURCE_CHROMEOS_H_
 
