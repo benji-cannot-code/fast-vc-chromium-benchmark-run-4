baed672b8892a215e233c00e12e8ed2b104acdd3
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 23:14:47 +0000

    Support for registering arbitrary Tasks with AtExitManager.
    Previously it was limitted to functions taling a single void* argument.
    
    Review URL: http://codereview.chromium.org/7831021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99856 0039d316-1c4b-4281-b951-d872f2087c98

--- base/at_exit.cc
+++ base/at_exit.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <ostream>
 
+#include "base/bind.h"
 #include "base/logging.h"
+#include "base/task.h"
 
 namespace base {
 
@@ -42,15 +44,19 @@ AtExitManager::~AtExitManager() {
 
 // static
 void AtExitManager::RegisterCallback(AtExitCallbackType func, void* param) {
+  DCHECK(func);
+  RegisterTask(base::Bind(func, param));
+}
+
+// static
+void AtExitManager::RegisterTask(base::Closure task) {
   if (!g_top_manager) {
     NOTREACHED() << "Tried to RegisterCallback without an AtExitManager";
     return;
   }
 
-  DCHECK(func);
-
   AutoLock lock(g_top_manager->lock_);
-  g_top_manager->stack_.push(CallbackAndParam(func, param));
+  g_top_manager->stack_.push(task);
 }
 
 // static
@@ -63,10 +69,9 @@ void AtExitManager::ProcessCallbacksNow() {
   AutoLock lock(g_top_manager->lock_);
 
   while (!g_top_manager->stack_.empty()) {
-    CallbackAndParam callback_and_param = g_top_manager->stack_.top();
+    base::Closure task = g_top_manager->stack_.top();
+    task.Run();
     g_top_manager->stack_.pop();
-
-    callback_and_param.func_(callback_and_param.param_);
   }
 }
 
--- base/at_exit_unittest.cc
+++ base/at_exit_unittest.cc
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/at_exit.h"
+#include "base/bind.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -76,5 +77,12 @@ TEST_F(AtExitTest, Param) {
   base::AtExitManager::RegisterCallback(&ExpectParamIsNull, NULL);
   base::AtExitManager::RegisterCallback(&ExpectParamIsCounter,
                                         &g_test_counter_1);
+  base::AtExitManager::ProcessCallbacksNow();
+}
+
+TEST_F(AtExitTest, Task) {
+  ZeroTestCounters();
+  base::AtExitManager::RegisterTask(base::Bind(ExpectParamIsCounter,
+                                               &g_test_counter_1));
   base::AtExitManager::ProcessCallbacksNow();
 }
