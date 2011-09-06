13ca0b87503a1a1ca37eb45d337332254d98ccfc
noel.gordon@gmail.com <noel.gordon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-06 19:50:19 +0000

    [chromium skia] JPEGImageEncoder: hoist constants out of the encoding loop https://bugs.webkit.org/show_bug.cgi?id=67589
    Reviewed by Kenneth Russell.
    
    Change the row converter function signatures to be the same.  Call them via a function
    pointer during the encoding loop.  Minor webkit style cleanup, remove unused include.
    
    No new tests.  Covered by existing canvas 2d and 3d tests.
    canvas/philip/tests/toDataURL.jpeg.alpha.html
    fast/canvas/webgl/premultiplyalpha-test.html
    
    * platform/image-encoders/skia/JPEGImageEncoder.cpp:  Remove SkUnPreMultiply.h (not used).
    (WebCore::preMultipliedBGRAtoRGB):  Use unsigned char* instead of void* for pixels.
    (WebCore::RGBAtoRGB):  pixels & pixelCount to match the preMultipliedBGRAtoRGB() signature.
    (WebCore::encodePixels):  Move constants out of the encoding loop: use a function pointer
    to call preMultipliedBGRAtoRGB or RGBAtoRGB (now they have identical signatures), define
    and use pixelRowStride constant.
    (WebCore::JPEGImageEncoder::encode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-encoders/skia/JPEGImageEncoder.cpp
+++ third_party/WebKit/Source/WebCore/platform/image-encoders/skia/JPEGImageEncoder.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntSize.h"
 #include "SkBitmap.h"
 #include "SkColorPriv.h"
-#include "SkUnPreMultiply.h"
 extern "C" {
 #include <stdio.h> // jpeglib.h needs stdio.h FILE
 #include "jpeglib.h"
@@ -81,9 +80,9 @@ static void handleError(j_common_ptr common)
     longjmp(*jumpBufferPtr, -1);
 }
 
-static void preMultipliedBGRAtoRGB(const void* pixels, unsigned int pixelCount, unsigned char* output)
+static void preMultipliedBGRAtoRGB(const unsigned char* pixels, unsigned int pixelCount, unsigned char* output)
 {
-    const SkPMColor* input = static_cast<const SkPMColor*>(pixels);
+    const SkPMColor* input = reinterpret_cast<const SkPMColor*>(pixels);
     for (; pixelCount-- > 0; ++input) {
         *output++ = SkGetPackedR32(*input);
         *output++ = SkGetPackedG32(*input);
@@ -91,20 +90,17 @@ static void preMultipliedBGRAtoRGB(const void* pixels, unsigned int pixelCount, 
     }
 }
 
-static void RGBAtoRGB(const unsigned char* input, unsigned int pixels, unsigned char* output)
+static void RGBAtoRGB(const unsigned char* pixels, unsigned int pixelCount, unsigned char* output)
 {
-    for (; pixels-- > 0; input += 4) {
-        *output++ = input[0];
-        *output++ = input[1];
-        *output++ = input[2];
+    for (; pixelCount-- > 0; pixels += 4) {
+        *output++ = pixels[0];
+        *output++ = pixels[1];
+        *output++ = pixels[2];
     }
 }
 
-static bool encodePixels(const IntSize& inputSize, unsigned char* inputPixels,
-                         bool premultiplied, int quality, Vector<unsigned char>* output)
+static bool encodePixels(IntSize imageSize, unsigned char* inputPixels, bool premultiplied, int quality, Vector<unsigned char>* output)
 {
-    IntSize imageSize(inputSize);
-    imageSize.clampNegativeToZero();
     JPEGOutputBuffer destination;
     destination.output = output;
     Vector<JSAMPLE> row;
@@ -126,10 +122,17 @@ static bool encodePixels(const IntSize& inputSize, unsigned char* inputPixels,
     cinfo.dest->init_destination = prepareOutput;
     cinfo.dest->empty_output_buffer = writeOutput;
     cinfo.dest->term_destination = finishOutput;
+
+    imageSize.clampNegativeToZero();
     cinfo.image_height = imageSize.height();
     cinfo.image_width = imageSize.width();
     cinfo.in_color_space = JCS_RGB;
     cinfo.input_components = 3;
+
+    void (*extractRowRGB)(const unsigned char*, unsigned int, unsigned char* output);
+    extractRowRGB = &RGBAtoRGB;
+    if (premultiplied)
+        extractRowRGB = &preMultipliedBGRAtoRGB;
 
     jpeg_set_defaults(&cinfo);
     jpeg_set_quality(&cinfo, quality, TRUE);
@@ -137,13 +140,11 @@ static bool encodePixels(const IntSize& inputSize, unsigned char* inputPixels,
 
     unsigned char* pixels = inputPixels;
     row.resize(cinfo.image_width * cinfo.input_components);
+    const size_t pixelRowStride = cinfo.image_width * 4;
     while (cinfo.next_scanline < cinfo.image_height) {
-        if (premultiplied)
-            preMultipliedBGRAtoRGB(pixels, cinfo.image_width, row.data());
-        else 
-            RGBAtoRGB(pixels, cinfo.image_width, row.data());
+        extractRowRGB(pixels, cinfo.image_width, row.data());
         jpeg_write_scanlines(&cinfo, row.dataSlot(), 1);
-        pixels += cinfo.image_width * 4;
+        pixels += pixelRowStride;
     }
 
     jpeg_finish_compress(&cinfo);
@@ -153,20 +154,17 @@ static bool encodePixels(const IntSize& inputSize, unsigned char* inputPixels,
 
 bool JPEGImageEncoder::encode(const SkBitmap& bitmap, int quality, Vector<unsigned char>* output)
 {
-    if (bitmap.config() != SkBitmap::kARGB_8888_Config)
-        return false; // Only support ARGB 32 bpp skia bitmaps.
-
     SkAutoLockPixels bitmapLock(bitmap);
-    IntSize imageSize(bitmap.width(), bitmap.height());
 
-    return encodePixels(imageSize, static_cast<unsigned char *>(bitmap.getPixels()),
-                        true, quality, output);
+    if (bitmap.config() != SkBitmap::kARGB_8888_Config)
+        return false; // Only support 32 bit/pixel skia bitmaps.
+
+    return encodePixels(IntSize(bitmap.width(), bitmap.height()), static_cast<unsigned char *>(bitmap.getPixels()), true, quality, output);
 }
 
 bool JPEGImageEncoder::encode(const ImageData& imageData, int quality, Vector<unsigned char>* output)
 {
-    return encodePixels(imageData.size(), imageData.data()->data()->data(),
-                        false, quality, output);
+    return encodePixels(imageData.size(), imageData.data()->data()->data(), false, quality, output);
 }
 
 } // namespace WebCore
