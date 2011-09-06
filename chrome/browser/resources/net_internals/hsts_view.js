9529d9cdd7855d59c008f6d73559ac8dbde9b21b
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 20:51:47 +0000

    More strict mode fixes for HSTS debug UI.
    BUG=95317
    TEST=Check that querying for mail.google.com in the HSTS debug UI (under about:net-internals), works.
    
    
    Review URL: http://codereview.chromium.org/7739031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99809 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/hsts_view.js
+++ chrome/browser/resources/net_internals/hsts_view.js
@@ -89,7 +89,7 @@ var HSTSView = (function() {
     onHSTSQueryResult: function(result) {
       if (result.error != undefined) {
         this.queryOutputDiv_.innerHTML = '';
-        s = addNode(this.queryOutputDiv_, 'span');
+        var s = addNode(this.queryOutputDiv_, 'span');
         s.textContent = result.error;
         s.style.color = 'red';
         yellowFade(this.queryOutputDiv_);
@@ -104,10 +104,10 @@ var HSTSView = (function() {
 
       this.queryOutputDiv_.innerHTML = '';
 
-      s = addNode(this.queryOutputDiv_, 'span');
+      var s = addNode(this.queryOutputDiv_, 'span');
       s.innerHTML = '<b>Found</b>: mode: ';
 
-      t = addNode(this.queryOutputDiv_, 'tt');
+      var t = addNode(this.queryOutputDiv_, 'tt');
       t.textContent = modeToString(result.mode);
 
       addTextNode(this.queryOutputDiv_, ' include_subdomains:');
