596c19191edd1f71b4a451bca0fefd9d48f2cfe7
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 21:13:38 +0000

    Set label widths and alignment in the views version of the extension install dialog
    These were dropped in r99407, but are still necessary when running on Windows.
    
    Also adds a bit of padding between the ratings stars and the count (Cocoa and GTK
    already do this).
    
    BUG=95532
    TEST=try to install extension or app from the Web Store, dialog layout should be correct
    
    Review URL: http://codereview.chromium.org/7789032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100019 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_install_dialog_view.cc
+++ chrome/browser/ui/views/extensions/extension_install_dialog_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "views/border.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
 #include "views/controls/link.h"
@@ -143,15 +144,15 @@ ExtensionInstallDialogView::ExtensionInstallDialogView(
 
   int column_set_id = 0;
   views::ColumnSet* column_set = layout->AddColumnSet(column_set_id);
+  int left_column_width = prompt.GetPermissionCount() > 0 ?
+      kPermissionsLeftColumnWidth : kNoPermissionsLeftColumnWidth;
 
   column_set->AddColumn(GridLayout::LEADING,
                         GridLayout::FILL,
                         0, // no resizing
                         GridLayout::USE_PREF,
                         0, // no fixed with
-                        prompt.GetPermissionCount() > 0 ?
-                            kNoPermissionsLeftColumnWidth :
-                            kPermissionsLeftColumnWidth);
+                        left_column_width);
   column_set->AddPaddingColumn(0, views::kPanelHorizMargin);
   column_set->AddColumn(GridLayout::LEADING,
                         GridLayout::LEADING,
@@ -167,6 +168,8 @@ ExtensionInstallDialogView::ExtensionInstallDialogView(
   heading->SetFont(heading->font().DeriveFont(kHeadingFontSizeDelta,
                                               gfx::Font::BOLD));
   heading->SetMultiLine(true);
+  heading->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
+  heading->SizeToFit(left_column_width);
   layout->AddView(heading);
 
   // Scale down to icon size, but allow smaller icons (don't scale up).
@@ -201,6 +204,8 @@ ExtensionInstallDialogView::ExtensionInstallDialogView(
         UTF16ToWide(prompt.GetRatingCount()));
     rating_count->SetFont(
         rating_count->font().DeriveFont(kRatingFontSizeDelta));
+    // Add some space between the stars and the rating count.
+    rating_count->set_border(views::Border::CreateEmptyBorder(0, 2, 0, 0));
     rating->AddChildView(rating_count);
 
     layout->StartRow(0, column_set_id);
@@ -232,6 +237,8 @@ ExtensionInstallDialogView::ExtensionInstallDialogView(
     views::Label* permissions_header = new views::Label(UTF16ToWide(
         prompt.GetPermissionsHeader()));
     permissions_header->SetMultiLine(true);
+    permissions_header->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
+    permissions_header->SizeToFit(left_column_width);
     layout->AddView(permissions_header);
 
     for (size_t i = 0; i < prompt.GetPermissionCount(); ++i) {
@@ -241,10 +248,8 @@ ExtensionInstallDialogView::ExtensionInstallDialogView(
           UTF16ToWide(prompt.GetPermission(i)));
       permission_label->SetMultiLine(true);
       permission_label->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
-      layout->AddView(permission_label,
-                      1, 1,
-                      GridLayout::LEADING, GridLayout::FILL,
-                      kPermissionsLeftColumnWidth, 0);
+      permission_label->SizeToFit(left_column_width);
+      layout->AddView(permission_label);
     }
   }
 }
