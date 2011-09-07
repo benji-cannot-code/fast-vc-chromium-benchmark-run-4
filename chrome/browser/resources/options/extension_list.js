3ade443a38665bf998591a6dbe0f16d8011cd6f4
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 19:53:54 +0000

    The checkboxes for the new extension settings page were not shown for the right extensions.
    "Allow in incognito" was erroneously linked to the WantsFileAccess flag and for "Allow file access" the WantsFileAccess bit was accidentally not flipped back during testing.
    
    BUG=87378
    TEST="Allow in incognito" should be available for all extensions and "Allow file access" for all that ask for it.
    Review URL: http://codereview.chromium.org/7789024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99992 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/extension_list.js
+++ chrome/browser/resources/options/extension_list.js
@@ -403,7 +403,7 @@ cr.define('options', function() {
         itemsShown++;
       }
 
-      if (extension.enabled && !extension.wantsFileAccess) {
+      if (extension.enabled) {
         // The 'allow in incognito' checkbox.
         var label = this.ownerDocument.createElement('label');
         label.classList.add('extension-checkbox-label');
@@ -424,7 +424,7 @@ cr.define('options', function() {
         itemsShown++;
       }
 
-      if (extension.enabled && !extension.wantsFileAccess) {
+      if (extension.enabled && extension.wantsFileAccess) {
         // The 'allow access to file URLs' checkbox.
         label = this.ownerDocument.createElement('label');
         label.classList.add('extension-checkbox-label');
