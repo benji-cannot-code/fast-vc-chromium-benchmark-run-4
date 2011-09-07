46a1c942ff0e1c7491ac5593b448fe9ed1b25fd9
mirandac@google.com <mirandac@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 14:47:37 +0000

    Speculative fix for race condition in URLFetcher.
    BUG=94184
    TEST=no more url fetcher related crashes.
    Review URL: http://codereview.chromium.org/7840024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99954 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/url_fetcher.cc
+++ content/common/url_fetcher.cc
@@ -746,7 +746,7 @@ void URLFetcher::Core::RetryOrCompleteUrlFetch() {
   } else {
     backoff_delay = base::TimeDelta();
   }
-
+  request_context_getter_ = NULL;
   bool posted = delegate_loop_proxy_->PostTask(
       FROM_HERE,
       NewRunnableMethod(this,
@@ -756,7 +756,6 @@ void URLFetcher::Core::RetryOrCompleteUrlFetch() {
   // If the delegate message loop does not exist any more, then the delegate
   // should be gone too.
   DCHECK(posted || !delegate_);
-  request_context_getter_ = NULL;
 }
 
 void URLFetcher::Core::ReadResponse() {
