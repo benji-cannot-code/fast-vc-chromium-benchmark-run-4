f28a99b80eda8bc8b6f5ffde66d95a5ae1caacef
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 06:00:52 +0000

    Being less harsh with CHECKs on NTP4 bookmarks WebUI callbacks.
    BUG=93343
    R=csilv@chromium.org
    TEST=chrome.send('removeBookmark',['999999']); doesn't crash the browser.
    
    
    Review URL: http://codereview.chromium.org/7839024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/bookmarks_handler.cc
+++ chrome/browser/ui/webui/ntp/bookmarks_handler.cc
@@ -175,7 +175,7 @@ void BookmarksHandler::HandleCreateBookmark(const ListValue* args) {
   CHECK(base::StringToInt64(parent_id_string, &parent_id));
 
   const BookmarkNode* parent = model_->GetNodeByID(parent_id);
-  CHECK(parent);
+  if (!parent) return;
 
   double index;
   if (!args->GetDouble(1, &index) ||
@@ -266,7 +266,7 @@ void BookmarksHandler::HandleRemoveBookmark(const ListValue* args) {
   CHECK(base::StringToInt64(id_string, &id));
 
   const BookmarkNode* node = model_->GetNodeByID(id);
-  CHECK(node);
+  if (!node) return;
 
   AutoReset<bool> from_page(&from_current_page_, true);
   model_->Remove(node->parent(), node->parent()->GetIndexOf(node));
@@ -289,10 +289,10 @@ void BookmarksHandler::HandleMoveBookmark(const ListValue* args) {
   args->GetDouble(2, &index);
 
   const BookmarkNode* parent = model_->GetNodeByID(parent_id);
-  CHECK(parent);
+  if (!parent) return;
 
   const BookmarkNode* node = model_->GetNodeByID(id);
-  CHECK(node);
+  if (!node) return;
 
   AutoReset<bool> from_page(&from_current_page_, true);
   model_->Move(node, parent, static_cast<int>(index));
