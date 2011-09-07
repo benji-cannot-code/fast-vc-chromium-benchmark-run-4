f7c0f147fd12331addca83cfecdf7f871c58e041
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 20:37:00 +0000

    Step 2 in removing resource strings in chrome.dll.
    Move more strings out of autofill_resources.grd and into string
    literal in code.
    
    BUG=None
    TEST=Covered by unittests.
    
    
    Review URL: http://codereview.chromium.org/7795058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100009 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/form_field.cc
+++ chrome/browser/autofill/form_field.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/form_structure.h"
 #include "chrome/browser/autofill/name_field.h"
 #include "chrome/browser/autofill/phone_field.h"
-#include "grit/autofill_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
