795ec38862f3266ababccd4022468755edebefad
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-07 12:31:11 +0000

    2011-09-05  Andrey Kosyakov  <caseq@chromium.org>
            Web Inspector: disable popover when a mouse button is pressed
            https://bugs.webkit.org/show_bug.cgi?id=67610
    
            Reviewed by Pavel Feldman.
    
            - disable popover when a mouse button is pressed
            - disable popover in a SourceFrame when the source is being edited
    
            * inspector/front-end/Popover.js:
            (WebInspector.PopoverHelper):
            (WebInspector.PopoverHelper.prototype._mouseUp):
            (WebInspector.PopoverHelper.prototype._mouseDown):
            (WebInspector.PopoverHelper.prototype._handleMouseAction):
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame.prototype._onHidePopover):
            (WebInspector.SourceFrame.prototype.doubleClick):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -283,7 +283,7 @@ WebInspector.SourceFrame.prototype = {
         var element = this._textViewer.element;
         if (this._delegate.debuggingSupported()) {
             this._popoverHelper = new WebInspector.PopoverHelper(element,
-                this._getPopoverAnchor.bind(this), this._onShowPopover.bind(this), this._onHidePopover.bind(this));
+                this._getPopoverAnchor.bind(this), this._onShowPopover.bind(this), this._onHidePopover.bind(this), true);
             element.addEventListener("mousedown", this._mouseDown.bind(this), true);
             element.addEventListener("scroll", this._scroll.bind(this), true);
         }
@@ -664,6 +664,8 @@ WebInspector.SourceFrame.prototype = {
     {
         // Replace higlight element with its contents inplace.
         var highlightElement = this._highlightElement;
+        if (!highlightElement)
+            return;
         var parentElement = highlightElement.parentElement;
         var child = highlightElement.firstChild;
         while (child) {
@@ -672,6 +674,7 @@ WebInspector.SourceFrame.prototype = {
             child = nextSibling;
         }
         parentElement.removeChild(highlightElement);
+        delete this._highlightElement;
         this._delegate.releaseEvaluationResult();
     },
 
@@ -719,6 +722,10 @@ WebInspector.SourceFrame.prototype = {
 
     _onShowPopover: function(element, popover)
     {
+        if (!this._textViewer.readOnly) {
+            this._popoverHelper.hidePopover();
+            return;
+        }
         this._highlightElement = this._highlightExpression(element);
 
         function showObjectPopover(result, wasThrown)
@@ -877,6 +884,9 @@ WebInspector.SourceFrame.prototype = {
 
     _setReadOnly: function(readOnly)
     {
+        if (!readOnly && this._popoverHelper)
+            this._popoverHelper.hidePopover();
+
         this._textViewer.readOnly = readOnly;
         WebInspector.markBeingEdited(this._textViewer.element, !readOnly);
         if (readOnly)
