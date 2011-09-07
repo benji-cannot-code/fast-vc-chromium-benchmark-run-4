380b18e2f5c34fb86732440f81875ebb321cf999
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-07 16:43:19 +0000

    [Qt][WK2] The scene graph is rendered continuously even when the page is idle. https://bugs.webkit.org/show_bug.cgi?id=67195
    Reviewed by Tor Arne Vestbø.
    
    Since tiledBackingStorePaintEnd is called even when no tile is dirty, the TiledDrawingArea
    and its proxy are looping over DidRenderFrame and RenderNextFrame messages.
    This causes QSGItem::update() to be called and consumes a lot of useless CPU cycles on the UI process.
    
    Introduce a bool to send DidRenderFrame only when dirty content was rendered.
    
    * WebProcess/WebPage/TiledDrawingArea.cpp:
    (WebKit::TiledDrawingArea::TiledDrawingArea):
    (WebKit::TiledDrawingArea::tiledBackingStorePaint):
    (WebKit::TiledDrawingArea::tiledBackingStorePaintEnd):
    * WebProcess/WebPage/TiledDrawingArea.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/TiledDrawingArea.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/TiledDrawingArea.cpp
@@ -44,6 +44,7 @@ TiledDrawingArea::TiledDrawingArea(WebPage* webPage)
     : DrawingArea(DrawingAreaTypeTiled, webPage)
     , m_suspended(false)
     , m_isWaitingForUIProcess(false)
+    , m_didSendTileUpdate(false)
     , m_mainBackingStore(adoptPtr(new TiledBackingStore(this, TiledBackingStoreRemoteTileBackend::create(this))))
 {
 }
@@ -116,10 +117,13 @@ void TiledDrawingArea::tiledBackingStorePaint(GraphicsContext* graphicsContext, 
 
 void TiledDrawingArea::tiledBackingStorePaintEnd(const Vector<IntRect>& paintedArea)
 {
-    // Since we know that all tile updates following a page invalidate will all be rendered
-    // in one paint pass for all the tiles, we can send the swap tile message here.
-    m_webPage->send(Messages::DrawingAreaProxy::DidRenderFrame());
-    m_isWaitingForUIProcess = true;
+    if (m_didSendTileUpdate) {
+        // Since we know that all tile updates following a page invalidate will all be rendered
+        // in one paint pass for all the tiles, we can send the swap tile message here.
+        m_webPage->send(Messages::DrawingAreaProxy::DidRenderFrame());
+        m_isWaitingForUIProcess = true;
+        m_didSendTileUpdate = false;
+    }
 }
 
 bool TiledDrawingArea::tiledBackingStoreUpdatesAllowed() const
@@ -145,6 +149,7 @@ Color TiledDrawingArea::tiledBackingStoreBackgroundColor() const
 void TiledDrawingArea::createTile(int tileID, const UpdateInfo& updateInfo)
 {
     m_webPage->send(Messages::DrawingAreaProxy::CreateTile(tileID, updateInfo));
+    m_didSendTileUpdate = true;
 
     if (m_previousBackingStore && m_mainBackingStore->coverageRatio(m_visibleContentRect) >= 1.0f)
         m_previousBackingStore.clear();
@@ -152,6 +157,7 @@ void TiledDrawingArea::createTile(int tileID, const UpdateInfo& updateInfo)
 void TiledDrawingArea::updateTile(int tileID, const UpdateInfo& updateInfo)
 {
     m_webPage->send(Messages::DrawingAreaProxy::UpdateTile(tileID, updateInfo));
+    m_didSendTileUpdate = true;
 }
 void TiledDrawingArea::removeTile(int tileID)
 {
