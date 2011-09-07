3300e851a4e2b87e318cc91a76d90f19069afbc2
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 04:01:03 +0000

    Changed OAuth token+secret encryption to use supplemental user key from NSS DB.
    BUG=chromium-os:18633
    TEST=none
    Review URL: http://codereview.chromium.org/7756025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99912 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cert_library.h
+++ chrome/browser/chromeos/cros/cert_library.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/cert_database.h"
 #include "net/base/x509_certificate.h"
 
+namespace crypto {
+class SymmetricKey;
+}
+
 namespace chromeos {
 
 class CertLibrary {
@@ -97,6 +101,9 @@ class CertLibrary {
 
   // Returns the current list of server CA certificates.
   virtual const CertList& GetCACertificates() const = 0;
+
+  // Returns the supplemental user key.
+  virtual crypto::SymmetricKey* GetSupplementalUserKey() const = 0;
 };
 
 }  // namespace chromeos
