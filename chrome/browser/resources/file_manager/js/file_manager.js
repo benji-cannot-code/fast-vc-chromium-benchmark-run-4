50eb0ea8c4add954b933139e4b41fcc6b72322d1
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 08:23:53 +0000

    [filebrowser] Do not move selection in list during rename.
    BUG=chromium-os:18150
    TEST=See bug.
    Review URL: http://codereview.chromium.org/7739033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2901,6 +2901,11 @@ FileManager.prototype = {
         event.preventDefault();
         break;
     }
+
+    // Do not move selection in list during rename.
+    if (event.keyIdentifier == 'Up' || event.keyIdentifier == 'Down') {
+      event.stopPropagation();
+    }
   };
 
   FileManager.prototype.onRenameInputBlur_ = function(event) {
