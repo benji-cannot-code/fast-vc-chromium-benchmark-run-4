14c3c02f881c42d0920c84175c47a1be9314f2e9
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 10:33:01 +0000

    Use the click event instead of mousedown event on the login remove button.
    R=hashimoto@chromium.org
    BUG=chromium-os:20044
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/7851012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100129 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -43,7 +43,7 @@ cr.define('login', function() {
       if (!this.hasAttribute('tabindex'))
         this.tabIndex = 0;
 
-      this.addEventListener('mousedown', this.handleMouseDown_.bind(this));
+      this.addEventListener('click', this.handleClick_.bind(this));
 
       this.enterButtonElement.addEventListener('click',
           this.activate.bind(this));
@@ -210,9 +210,9 @@ cr.define('login', function() {
     },
 
     /**
-     * Handles mousedown on remove user button.
+     * Handles a click event on remove user button.
      */
-    handleRemoevButtonMouseDown_: function(e) {
+    handleRemoveButtonClick_: function(e) {
       if (this.activeRemoveButton)
         chrome.send('removeUser', [this.user.emailAddress]);
       else
@@ -220,11 +220,11 @@ cr.define('login', function() {
     },
 
     /**
-     * Handles mousedown event.
+     * Handles click event.
      */
-    handleMouseDown_: function(e) {
+    handleClick_: function(e) {
       if (e.target == this.removeUserButtonElement) {
-        this.handleRemoevButtonMouseDown_(e);
+        this.handleRemoveButtonClick_(e);
 
         // Prevent default so that we don't trigger 'focus' event.
         e.preventDefault();
