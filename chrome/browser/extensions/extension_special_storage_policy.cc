bef58c5e0803036ff4bf13ab14ccbb391b996eff
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 11:31:40 +0000

    DatabaseTracker: Clearing session-only databases on exit.
    BUG=47049
    TEST=DatabaseTrackerTest.DatabaseTrackerClearSessionOnlyDatabasesOnExit
    
    
    Review URL: http://codereview.chromium.org/7800040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_special_storage_policy.cc
+++ chrome/browser/extensions/extension_special_storage_policy.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/common/content_settings.h"
+#include "chrome/common/content_settings_types.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/browser_thread.h"
@@ -36,6 +37,22 @@ bool ExtensionSpecialStoragePolicy::IsStorageSessionOnly(const GURL& origin) {
   ContentSetting content_setting = host_content_settings_map_->
       GetCookieContentSetting(origin, origin, true);
   return (content_setting == CONTENT_SETTING_SESSION_ONLY);
+}
+
+bool ExtensionSpecialStoragePolicy::HasSessionOnlyOrigins() {
+  if (host_content_settings_map_ == NULL)
+    return false;
+  if (host_content_settings_map_->GetDefaultContentSetting(
+          CONTENT_SETTINGS_TYPE_COOKIES) == CONTENT_SETTING_SESSION_ONLY)
+    return true;
+  HostContentSettingsMap::SettingsForOneType entries;
+  host_content_settings_map_->GetSettingsForOneType(
+      CONTENT_SETTINGS_TYPE_COOKIES, "", &entries);
+  for (size_t i = 0; i < entries.size(); ++i) {
+    if (entries[i].c == CONTENT_SETTING_SESSION_ONLY)
+      return true;
+  }
+  return false;
 }
 
 bool ExtensionSpecialStoragePolicy::IsFileHandler(
