ccc8076601839e437576dc6c11aac5adc47a182a
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 16:08:11 +0000

    Make cookie removal on enterprise enrollment more robust.
    Remove cookies both when entering and leaving the enrollment screen.
    
    BUG=None
    TEST=Enrollment still works.
    
    
    Review URL: http://codereview.chromium.org/7835034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/enterprise_oauth_enrollment_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/enterprise_oauth_enrollment_screen_handler.h
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/values.h"
+#include "chrome/browser/browsing_data_remover.h"
 #include "chrome/browser/chromeos/login/enrollment/enterprise_enrollment_screen_actor.h"
 #include "chrome/browser/net/gaia/gaia_oauth_consumer.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
@@ -26,7 +28,8 @@ namespace chromeos {
 class EnterpriseOAuthEnrollmentScreenHandler
     : public BaseScreenHandler,
       public EnterpriseEnrollmentScreenActor,
-      public GaiaOAuthConsumer {
+      public GaiaOAuthConsumer,
+      public BrowsingDataRemover::Observer {
  public:
   EnterpriseOAuthEnrollmentScreenHandler();
   virtual ~EnterpriseOAuthEnrollmentScreenHandler();
@@ -66,6 +69,9 @@ class EnterpriseOAuthEnrollmentScreenHandler
   virtual void OnUserInfoSuccess(const std::string& email) OVERRIDE;
   virtual void OnUserInfoFailure(const GoogleServiceAuthError& error) OVERRIDE;
 
+  // Implements BrowsingDataRemover::Observer:
+  virtual void OnBrowsingDataRemoverDone() OVERRIDE;
+
  protected:
   // Implements BaseScreenHandler:
   virtual void Initialize() OVERRIDE;
@@ -85,10 +91,15 @@ class EnterpriseOAuthEnrollmentScreenHandler
   // Display the given i18n string as error message.
   void ShowError(int message_id, bool retry);
 
-  // Resets the authentication machinery and clears cookies, so other screens
-  // (like the actual login screen) find a clean slate and don't pick up our
-  // auth state.
+  // Resets the authentication machinery and clears cookies. Will invoke
+  // |action_on_browsing_data_removed_| once cookies are cleared.
   void ResetAuth();
+
+  // Shows the screen.
+  void DoShow();
+
+  // Closes the screen.
+  void DoClose();
 
   bool editable_user_;
   bool show_on_init_;
@@ -99,6 +110,12 @@ class EnterpriseOAuthEnrollmentScreenHandler
   // This intentionally lives here and not in the controller, since it needs to
   // execute requests in the context of the profile that displays the webui.
   scoped_ptr<GaiaOAuthFetcher> oauth_fetcher_;
+
+  // The browsing data remover instance currently active, if any.
+  BrowsingDataRemover* browsing_data_remover_;
+
+  // What to do when browsing data is removed.
+  base::Closure action_on_browsing_data_removed_;
 
   DISALLOW_COPY_AND_ASSIGN(EnterpriseOAuthEnrollmentScreenHandler);
 };
