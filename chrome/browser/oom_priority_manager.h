0dd612da6316cc5d6b5876800545d24ddbd837ab
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 17:06:59 +0000

    CrOS - about:discards shows tab to discard in OOM situations
    Added a simple, non-localized debug page to show the output of
    the OOM-adjust algorithm, in preparation for discarding a tab in
    low memory situations.  See "Out of memory handling" Chrome OS
    design doc for details.
    
    BUG=chromium-os:18375
    TEST=open 3 tabs and navigate to web pages, wait 10 seconds, then open a tab to "about:discards".  It should list 3 tabs and itself.  Order is unimportant for now.
    
    Review URL: http://codereview.chromium.org/7754002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/oom_priority_manager.h
+++ chrome/browser/oom_priority_manager.h
@@ -6,12 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_OOM_PRIORITY_MANAGER_H_
 #define CHROME_BROWSER_OOM_PRIORITY_MANAGER_H_
 
-#include <list>
+#include <vector>
 
-#include "base/timer.h"
-#include "base/process.h"
+#include "base/string16.h"
 
 namespace browser {
+
+class OomPriorityManagerImpl;
 
 // The OomPriorityManager periodically checks (see
 // ADJUSTMENT_INTERVAL_SECONDS in the source) the status of renderers
@@ -27,37 +28,18 @@ namespace browser {
 // them, as no two tabs will have exactly the same idle time.
 class OomPriorityManager {
  public:
-  OomPriorityManager();
-  ~OomPriorityManager();
+  // We need to explicitly manage our destruction, so don't use Singleton.
+  static void Create();
+  static void Destroy();
+
+  // Returns list of tab titles sorted from most interesting (don't kill)
+  // to least interesting (OK to kill).
+  static std::vector<string16> GetTabTitles();
 
  private:
-  struct RendererStats {
-    bool is_pinned;
-    bool is_selected;
-    base::TimeTicks last_selected;
-    size_t memory_used;
-    base::ProcessHandle renderer_handle;
-  };
-  typedef std::list<RendererStats> StatsList;
-
-  void StartTimer();
-  void StopTimer();
-
-  // Posts DoAdjustOomPriorities task to the file thread.  Called when
-  // the timer fires.
-  void AdjustOomPriorities();
-
-  // Called by AdjustOomPriorities.  Runs on the file thread.
-  void DoAdjustOomPriorities(StatsList list);
-
-  static bool CompareRendererStats(RendererStats first, RendererStats second);
-
-  base::RepeatingTimer<OomPriorityManager> timer_;
-
-  DISALLOW_COPY_AND_ASSIGN(OomPriorityManager);
+  static OomPriorityManagerImpl* impl_;
 };
-}  // namespace browser
 
-DISABLE_RUNNABLE_METHOD_REFCOUNT(browser::OomPriorityManager);
+}  // namespace browser
 
 #endif  // CHROME_BROWSER_OOM_PRIORITY_MANAGER_H_
--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -70,6 +70,7 @@ const char kChromeUIActiveDownloadsURL[] = "chrome://active-downloads/";
 const char kChromeUIChooseMobileNetworkURL[] =
     "chrome://choose-mobile-network/";
 const char kChromeUICollectedCookiesURL[] = "chrome://collected-cookies/";
+const char kChromeUIDiscardsURL[] = "chrome://discards/";
 const char kChromeUIEnterpriseEnrollmentURL[] =
     "chrome://enterprise-enrollment/";
 const char kChromeUIHttpAuthURL[] = "chrome://http-auth/";
@@ -177,6 +178,7 @@ const char kChromeUIActiveDownloadsHost[] = "active-downloads";
 const char kChromeUIChooseMobileNetworkHost[] = "choose-mobile-network";
 const char kChromeUICollectedCookiesHost[] = "collected-cookies";
 const char kChromeUICryptohomeHost[] = "cryptohome";
+const char kChromeUIDiscardsHost[] = "discards";
 const char kChromeUIEnterpriseEnrollmentHost[] = "enterprise-enrollment";
 const char kChromeUIHttpAuthHost[] = "http-auth";
 const char kChromeUIImageBurnerHost[] = "imageburner";
