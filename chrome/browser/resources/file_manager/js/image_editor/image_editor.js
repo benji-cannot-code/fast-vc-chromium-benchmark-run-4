57b3bda9bc98b92358ce7ec49123bb32ee741f81
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 23:34:38 +0000

    [filebrowser] First step to gallery (image editor). No sharing, nice UI and animations. No save reminders and feedback. Most effects have preview on the whole image and so are little slow.
    BUG=chromium-os:19534
    TEST=Try out 'Gallery' action for images.
    
    Review URL: http://codereview.chromium.org/7828044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_editor.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_editor.js
@@ -9,8 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {HTMLElement} container
  * @param {function(Blob)} saveCallback
  * @param {function()} closeCallback
+ * @param {HTMLElement} toolbarContainer
  */
-function ImageEditor(container, saveCallback, closeCallback) {
+function ImageEditor(container, toolbarContainer, saveCallback, closeCallback) {
   this.container_ = container;
   this.saveCallback_ = saveCallback;
   this.closeCallback_ = closeCallback;
@@ -30,13 +31,12 @@ function ImageEditor(container, saveCallback, closeCallback) {
 
   this.buffer_ = new ImageBuffer(canvas);
 
-  this.scaleControl_ = new ImageEditor.ScaleControl(
-      this.canvasWrapper_, this.getBuffer().getViewport());
+  // TODO(dgozman): consider adding a ScaleControl in v2.
 
   this.panControl_ = new ImageEditor.MouseControl(canvas, this.getBuffer());
 
-  this.toolbar_ =
-      new ImageEditor.Toolbar(container, this.onOptionsChange.bind(this));
+  this.toolbar_ = new ImageEditor.Toolbar(toolbarContainer,
+      this.onOptionsChange.bind(this));
   this.initToolbar();
 }
 
@@ -53,7 +53,8 @@ function ImageEditor(container, saveCallback, closeCallback) {
  */
 ImageEditor.open = function(saveCallback, closeCallback, source, opt_metadata) {
   var container = document.getElementsByClassName('image-editor')[0];
-  var editor = new ImageEditor(container, saveCallback, closeCallback);
+  var toolbar = document.getElementsByClassName('toolbar-container')[0];
+  var editor = new ImageEditor(container, toolbar, saveCallback, closeCallback);
   if (ImageEditor.resizeListener) {
     // Make sure we do not leak the previous instance.
     window.removeEventListener('resize', ImageEditor.resizeListener, false);
@@ -118,7 +119,6 @@ ImageEditor.prototype.initToolbar = function() {
 
   this.createModeButtons();
   this.toolbar_.addButton('Save', this.save.bind(this, true));
-  this.toolbar_.addButton('Close', this.close.bind(this, false));
 };
 
 /**
