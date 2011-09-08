1a3f686e335774b8d0d3d5f4262264a5810b1389
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 21:47:46 +0000

    Print Preview: Handling pending print to pdf requests.
    BUG=91888
    TEST=Open a large doc in print preview. Select "Print To PDF" and click "Print" immediately. The file dialog should be shown regardless of the pdf file being ready for saving.
    
    Review URL: http://codereview.chromium.org/7792085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100260 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview_animations.js
+++ chrome/browser/resources/print_preview/print_preview_animations.js
@@ -79,15 +79,33 @@ function fadeInOutCleanup(animationName) {
     animEl.parentNode.removeChild(animEl);
 }
 
-function showLoadingAnimation() {
-  $('dancing-dots-text').classList.remove('hidden');
+/**
+ * Displays |message| followed by three dancing dots animation.
+ */
+function showCustomMessage(message) {
+  $('custom-message-with-dots').innerHTML = message +
+      $('dancing-dots-text').innerHTML;
+  $('custom-message-with-dots').hidden = false;
   var pdfViewer = $('pdf-viewer');
   if (pdfViewer)
     pdfViewer.classList.add('invisible');
   $('overlay-layer').classList.remove('invisible');
 }
 
-function hideLoadingAnimation() {
+/**
+ * Displays the "Preview loading..." animation.
+ */
+function showLoadingAnimation() {
+  showCustomMessage(localStrings.getString('loading'));
+}
+
+/**
+ * Hides the |overlay-layer| and any messages currently displayed.
+ */
+function hideOverlayLayer() {
+  if (hasPendingPrintDocumentRequest)
+    return;
+
   var overlayLayer = $('overlay-layer');
   overlayLayer.addEventListener('webkitTransitionEnd', loadingAnimationCleanup);
   var pdfViewer = $('pdf-viewer');
@@ -97,7 +115,7 @@ function hideLoadingAnimation() {
 }
 
 function loadingAnimationCleanup() {
-  $('dancing-dots-text').classList.add('hidden');
+  $('custom-message-with-dots').hidden = true;
   $('overlay-layer').removeEventListener('webkitTransitionEnd',
                                          loadingAnimationCleanup);
 }
