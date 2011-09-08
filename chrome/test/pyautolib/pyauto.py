05db84d478332f2d5401ca1431f7880a7e412451
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 00:55:55 +0000

    Make PyAuto EnrollEnterpriseDevice() return False instead of raising an Exception.
    BUG=chromium-os:19970
    TEST=Make sure Chrome builds. There are no tests that use this automation yet. (They are coming.)
    
    Review URL: http://codereview.chromium.org/7839016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100060 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/pyauto.py
+++ chrome/test/pyautolib/pyauto.py
@@ -3675,8 +3675,12 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
     equivalent to pressing Ctrl-Alt-e to enroll the device from the login
     screen.
 
+    Returns:
+      An error string if the enrollment fails.
+      None otherwise.
+
     Raises:
-      pyauto_errors.JSONInterfaceError if the enrollment fails.
+      pyauto_errors.JSONInterfaceError if the automation call returns an error.
     """
     cmd_dict = {
         'command': 'EnrollEnterpriseDevice',
@@ -3684,7 +3688,8 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
         'password': password,
     }
     time.sleep(5) # TODO(craigdh): Block until Install Attributes is ready.
-    return self._GetResultFromJSONRequest(cmd_dict, windex=-1)
+    result = self._GetResultFromJSONRequest(cmd_dict, windex=-1)
+    return result.get('error_string')
 
   def GetUpdateInfo(self):
     """Gets the status of the ChromeOS updater.
