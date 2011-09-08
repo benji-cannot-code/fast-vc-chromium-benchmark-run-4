954b3499c179e5809c7a6001e0bf006e0f5b0530
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 04:41:59 +0000

    [ChromeOS] Make status area text look the same as WebUI header bar.
    BUG=chromium-os:19961
    TEST=Verify fix for chromium-os:19961.
    
    
    Review URL: http://codereview.chromium.org/7838038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100103 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/keyboard_switch_menu.cc
+++ chrome/browser/chromeos/login/keyboard_switch_menu.cc
@@ -19,7 +19,7 @@ namespace chromeos {
 
 KeyboardSwitchMenu::KeyboardSwitchMenu()
     : InputMethodMenu(NULL /* pref_service */,
-                      StatusAreaHost::kLoginMode,
+                      StatusAreaHost::kViewsLoginMode,
                       true /* for_out_of_box_experience_dialog */) {
 }
 
