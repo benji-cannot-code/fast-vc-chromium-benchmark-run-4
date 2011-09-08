f41ace85248f9fc30c4ce74af29b091c3185f57b
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 16:00:18 +0000

    Move compositor ctor and dtor to .cc file.
    BUG=None
    TEST=Linux clang bot goes green
    TBR=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7848023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100154 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor.cc
+++ ui/gfx/compositor/compositor.cc
@@ -8,6 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+Compositor::Compositor(CompositorDelegate* delegate, const gfx::Size& size)
+    : delegate_(delegate),
+      size_(size) {
+}
+
+Compositor::~Compositor() {
+}
+
 void Compositor::NotifyStart() {
   OnNotifyStart();
 }
