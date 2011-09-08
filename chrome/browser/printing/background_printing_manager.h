9057f8ebc130b5a793a6f533673be27e3edc3726
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-08 20:34:53 +0000

    Print Preview: Hold on to tabs that do window.print(); window.close() until print preview finishes.
    BUG=87362
    TEST=Create html file with window.print(); window.close(); and then print, cancel or close the print tab.
    
    Review URL: http://codereview.chromium.org/7800036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/background_printing_manager.h
+++ chrome/browser/printing/background_printing_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRINTING_BACKGROUND_PRINTING_MANAGER_H_
 #pragma once
 
+#include <map>
 #include <set>
 
 #include "base/compiler_specific.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
 
+class RenderProcessHost;
 class TabContentsWrapper;
 
 namespace printing {
@@ -34,6 +36,13 @@ class BackgroundPrintingManager : public base::NonThreadSafe,
   // hides it from the user.
   void OwnPrintPreviewTab(TabContentsWrapper* preview_tab);
 
+  // Takes ownership of |initiator_tab| and deletes it when its preview tab is
+  // destroyed by either being canceled, closed or finishing printing. This
+  // removes the TabContentsWrapper from its TabStrip and hides it from the
+  // user. Returns true if content has an associated print preview tab,
+  // otherwise, returns false and does not take ownership of |initiator_tab|.
+  bool OwnInitiatorTab(TabContentsWrapper* initiator_tab);
+
   // Let others iterate over the list of background printing tabs.
   TabContentsWrapperSet::const_iterator begin();
   TabContentsWrapperSet::const_iterator end();
@@ -47,8 +56,26 @@ class BackgroundPrintingManager : public base::NonThreadSafe,
                        const NotificationDetails& details) OVERRIDE;
 
  private:
+  typedef std::map<TabContentsWrapper*, TabContentsWrapper*>
+      TabContentsWrapperMap;
+
+  // Notifications handlers.
+  void OnRendererProcessClosed(RenderProcessHost* rph);
+  void OnPrintJobReleased(TabContentsWrapper* preview_tab);
+  void OnTabContentsDestroyed(TabContentsWrapper* preview_tab);
+
+  // Removes |tab| from its tab strip.
+  void RemoveFromTabStrip(TabContentsWrapper* tab);
+
   // The set of print preview tabs managed by BackgroundPrintingManager.
   TabContentsWrapperSet printing_tabs_;
+
+  // 1:1 mapping between an initiator tab managed by BackgroundPrintingManager
+  // and its associated print preview tab. The print preview tab need not be in
+  // |printing_tabs_|.
+  // Key: print preview tab.
+  // Value: initiator tab.
+  TabContentsWrapperMap map_;
 
   NotificationRegistrar registrar_;
 
