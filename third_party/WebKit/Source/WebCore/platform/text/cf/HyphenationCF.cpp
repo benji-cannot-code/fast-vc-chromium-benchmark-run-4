4e5e1be0f8ab016cc0c3acacb4f37a062684b852
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-08 00:55:20 +0000

    Removed hyphenation function implementations for an usupported build configuration.
    Reviewed by Sam Weinig.
    
    * platform/text/cf/HyphenationCF.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/cf/HyphenationCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/cf/HyphenationCF.cpp
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if !PLATFORM(WIN) || (defined(MAC_OS_X_VERSION_10_7) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7)
-
 template<>
 RetainPtr<CFLocaleRef> AtomicStringKeyedMRUCache<RetainPtr<CFLocaleRef> >::createValueForNullKey()
 {
@@ -76,21 +74,6 @@ size_t lastHyphenLocation(const UChar* characters, size_t length, size_t beforeI
     CFIndex result = CFStringGetHyphenationLocationBeforeIndex(string.get(), beforeIndex, CFRangeMake(0, length), 0, locale.get(), 0);
     return result == kCFNotFound ? 0 : result;
 }
-
-#else
-
-bool canHyphenate(const AtomicString&)
-{
-    return false;
-}
-
-size_t lastHyphenLocation(const UChar*, size_t, size_t, const AtomicString&)
-{
-    ASSERT_NOT_REACHED();
-    return 0;
-}
-
-#endif // PLATFORM(WIN) && (!defined(MAC_OS_X_VERSION_10_7) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7)
 
 } // namespace WebCore
 
