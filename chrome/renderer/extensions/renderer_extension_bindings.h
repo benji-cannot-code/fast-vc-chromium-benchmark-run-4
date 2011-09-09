8828b078173918b65f348aba9ae4c174ad1fd220
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 17:04:12 +0000

    Declare a few strings as array to const char in chrome/renderer/extensions/*
    R=mihaip@chromium.org
    
    Review URL: http://codereview.chromium.org/7858021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/renderer_extension_bindings.h
+++ chrome/renderer/extensions/renderer_extension_bindings.h
@@ -7,11 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_EXTENSIONS_RENDERER_EXTENSION_BINDINGS_H_
 #pragma once
 
-#include <string>
-
 class ExtensionDispatcher;
-class GURL;
-class RenderView;
 
 namespace v8 {
 class Extension;
