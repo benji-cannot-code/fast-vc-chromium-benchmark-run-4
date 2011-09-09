30df6d158a29ace1728efef08059c88a3241343c
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 21:06:38 +0000

    Cleanup state transitions for Host plugin.
    BUG=94105
    TEST=See repro steps in the bug.
    
    Review URL: http://codereview.chromium.org/7857011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100489 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/host_script_object.h
+++ remoting/host/plugin/host_script_object.h
@@ -89,10 +89,11 @@ class HostNPScriptObject : public HostStatusObserver {
  private:
   enum State {
     kDisconnected,
+    kStarting,
     kRequestedAccessCode,
     kReceivedAccessCode,
     kConnected,
-    kAffirmingConnection,
+    kDisconnecting,
     kError
   };
 
@@ -110,8 +111,11 @@ class HostNPScriptObject : public HostStatusObserver {
   // No result.
   bool Localize(const NPVariant* args, uint32_t argCount, NPVariant* result);
 
-  // Call OnStateChanged handler if there is one.
-  void OnStateChanged(State state);
+  // Updates state of the host. Can be called only on the main thread.
+  void SetState(State state);
+
+  // Notifies OnStateChanged handler of a state change.
+  void NotifyStateChanged(State state);
 
   // Call LogDebugInfo handler if there is one.
   // This must be called on the correct thread.
@@ -122,6 +126,7 @@ class HostNPScriptObject : public HostStatusObserver {
                            bool success,
                            const std::string& support_id,
                            const base::TimeDelta& lifetime);
+  void NotifyAccessCode(bool success);
 
   // Helper functions that run on main thread. Can be called on any
   // other thread.
@@ -159,10 +164,13 @@ class HostNPScriptObject : public HostStatusObserver {
 
   NPP plugin_;
   NPObject* parent_;
-  int state_;
+  State state_;
+
+  base::Lock access_code_lock_;
   std::string access_code_;
-  std::string client_username_;
   base::TimeDelta access_code_lifetime_;
+
+  std::string client_username_;
   ScopedRefNPObject log_debug_info_func_;
   ScopedRefNPObject on_state_changed_func_;
   base::PlatformThreadId np_thread_id_;
