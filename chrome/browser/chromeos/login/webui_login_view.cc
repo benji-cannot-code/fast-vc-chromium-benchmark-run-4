e38b9826bd42109fed7e208609f1fdadcca0f235
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 06:14:06 +0000

    Another attempt to fix crash in clock menu button.
    BUG=chromium-os:20171
    TEST=no test crashes in chromeos::ClockMenuButton::UpdateText() 
    Review URL: http://codereview.chromium.org/7858022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_view.cc
+++ chrome/browser/chromeos/login/webui_login_view.cc
@@ -124,7 +124,7 @@ WebUILoginView::WebUILoginView()
 
 WebUILoginView::~WebUILoginView() {
   if (status_window_)
-    status_window_->Close();
+    status_window_->CloseNow();
   status_window_ = NULL;
 }
 
@@ -215,7 +215,7 @@ void WebUILoginView::ChildPreferredSizeChanged(View* child) {
 }
 
 Profile* WebUILoginView::GetProfile() const {
-  return ProfileManager::GetDefaultProfile();
+  return NULL;
 }
 
 void WebUILoginView::ExecuteBrowserCommand(int id) const {
