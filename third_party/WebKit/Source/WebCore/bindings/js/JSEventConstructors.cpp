9bbd1914dcc152acaefa31dd2365ad698fca741a
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-09 17:27:50 +0000

    Implement a WebKitAnimationEvent constructor. https://bugs.webkit.org/show_bug.cgi?id=67825
    Source/WebCore:
    
    Patch by Kentaro Hara <haraken@google.com> on 2011-09-08
    Reviewed by Sam Weinig.
    
    There is no spec for the WebKitAnimationEvent constructor
    since it is WebKit-specific. However, based on the current
    IDL of initWebKitAnimationEvent(), the constructor IDL
    should be as follows.
    
    [Constructor(DOMString type, optional WebKitAnimationEventInit eventInitDict)]
    interface WebKitAnimationEvent : Event {
        ...;
    }
    
    dictionary WebKitAnimationEventInit : EventInit {
        DOMString animationName;
        double elapsedTime;
    }
    
    Test: fast/events/constructors/webkit-animation-event-constructor.html
    
    * bindings/generic/EventConstructors.h: Added a definition for the WebKitAnimationEvent constructor.
    * bindings/js/JSEventConstructors.cpp: Added #includes for WebKitAnimationEvent.
    * dom/WebKitAnimationEvent.cpp:
    (WebCore::WebKitAnimationEventInit::WebKitAnimationEventInit):
    (WebCore::WebKitAnimationEvent::WebKitAnimationEvent):
    * dom/WebKitAnimationEvent.h: Added a definition for WebKitAnimationEventInit.
    (WebCore::WebKitAnimationEvent::create):
    * dom/WebKitAnimationEvent.idl: Makes WebKitAnimationEvent constructible.
    
    LayoutTests:
    
    Checks the behavior of the WebKitAnimationEvent constructor.
    
    Patch by Kentaro Hara <haraken@google.com> on 2011-09-08
    Reviewed by Sam Weinig.
    
    * fast/dom/constructed-objects-prototypes-expected.txt:
    * fast/events/constructors/webkit-animation-event-constructor-expected.txt: Added.
    * fast/events/constructors/webkit-animation-event-constructor.html: Added.
    * platform/chromium/test_expectations.txt: Skipped webkit-animation-event-constructor.html, since V8 does not yet have the WebKitAnimationEvent constructor.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSEventConstructors.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSEventConstructors.cpp
@@ -33,7 +33,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSDictionary.h"
 #include "JSEvent.h"
 #include "JSProgressEvent.h"
+#include "JSWebKitAnimationEvent.h"
 #include "ProgressEvent.h"
+#include "WebKitAnimationEvent.h"
 #include <runtime/Error.h>
 
 using namespace JSC;
--- third_party/WebKit/Source/WebCore/dom/WebKitAnimationEvent.cpp
+++ third_party/WebKit/Source/WebCore/dom/WebKitAnimationEvent.cpp
@@ -31,8 +31,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+WebKitAnimationEventInit::WebKitAnimationEventInit()
+    : animationName()
+    , elapsedTime(0.0)
+{
+}
+
 WebKitAnimationEvent::WebKitAnimationEvent()
     : m_elapsedTime(0.0)
+{
+}
+
+WebKitAnimationEvent::WebKitAnimationEvent(const AtomicString& type, const WebKitAnimationEventInit& initializer)
+    : Event(type, initializer)
+    , m_animationName(initializer.animationName)
+    , m_elapsedTime(initializer.elapsedTime)
 {
 }
 
