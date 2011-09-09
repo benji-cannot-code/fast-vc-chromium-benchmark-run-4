b6f7316c7b7a5447cc8192f4175897f3fd3999d9
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 17:55:11 +0000

    content: Move constrained html windows to TabContentsWrapper
    BUG=95257
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/7860008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/collected_cookies_ui_delegate.cc
+++ chrome/browser/ui/webui/collected_cookies_ui_delegate.cc
@@ -147,7 +147,7 @@ void CollectedCookiesUIDelegate::Show(TabContentsWrapper* wrapper) {
   Profile* profile = wrapper->profile();
   ConstrainedHtmlUI::CreateConstrainedHtmlDialog(profile,
                                                  delegate,
-                                                 wrapper->tab_contents());
+                                                 wrapper);
 }
 
 CollectedCookiesUIDelegate::CollectedCookiesUIDelegate(
