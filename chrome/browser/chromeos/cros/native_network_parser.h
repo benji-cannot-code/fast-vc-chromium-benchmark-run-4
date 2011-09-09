53c873303c49732863ad7d98b84e6039fb933abc
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 04:35:13 +0000

    Include "base/values.h" explicitly where it's used.
    We were about to remove base/values.h include from chromeos_network.h
    in libcros: http://gerrit.chromium.org/gerrit/#change,7429
    It should be easy, right? No.
    
    It turned out network_library.h etc. in Chrome are indirectly including
    base/values.h via chromeos_network.h. We should include base/values.h
    explicitly instead.
    
    BUG=chromium-os:19576
    TEST=chrome builds.
    
    
    Review URL: http://codereview.chromium.org/7831064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100346 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/native_network_parser.h
+++ chrome/browser/chromeos/cros/native_network_parser.h
@@ -10,6 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/network_parser.h"
 #include "base/compiler_specific.h"  // for OVERRIDE
 
+namespace base {
+class DictionaryValue;
+class ListValue;
+class Value;
+}
+
 namespace chromeos {
 
 // This is the network device parser that parses the data from the
@@ -20,19 +26,21 @@ class NativeNetworkDeviceParser : public NetworkDeviceParser {
   NativeNetworkDeviceParser();
   virtual ~NativeNetworkDeviceParser();
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           NetworkDevice* device) OVERRIDE;
  protected:
   virtual ConnectionType ParseType(const std::string& type) OVERRIDE;
 
   // Parsing helper routines specific to native network devices.
-  virtual bool ParseApnList(const ListValue& list, CellularApnList* apn_list);
-  virtual bool ParseFoundNetworksFromList(const ListValue& list,
+  virtual bool ParseApnList(const base::ListValue& list,
+                            CellularApnList* apn_list);
+  virtual bool ParseFoundNetworksFromList(const base::ListValue& list,
                                           CellularNetworkList* found_networks);
   virtual SimLockState ParseSimLockState(const std::string& state);
-  virtual bool ParseSimLockStateFromDictionary(const DictionaryValue& info,
-                                               SimLockState* out_state,
-                                               int* out_retries);
+  virtual bool ParseSimLockStateFromDictionary(
+      const base::DictionaryValue& info,
+      SimLockState* out_state,
+      int* out_retries);
   virtual TechnologyFamily ParseTechnologyFamily(
       const std::string& technology_family);
 
@@ -51,11 +59,11 @@ class NativeNetworkParser : public NetworkParser {
   static const ConnectionType ParseConnectionType(const std::string& type);
  protected:
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           Network* network) OVERRIDE;
   virtual ConnectionType ParseType(const std::string& type) OVERRIDE;
   virtual ConnectionType ParseTypeFromDictionary(
-      const DictionaryValue& info) OVERRIDE;
+      const base::DictionaryValue& info) OVERRIDE;
   virtual ConnectionMode ParseMode(const std::string& mode) OVERRIDE;
   virtual ConnectionState ParseState(const std::string& state) OVERRIDE;
   virtual ConnectionError ParseError(const std::string& error) OVERRIDE;
@@ -80,7 +88,7 @@ class NativeWirelessNetworkParser : public NativeNetworkParser {
   NativeWirelessNetworkParser();
   virtual ~NativeWirelessNetworkParser();
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           Network* network) OVERRIDE;
  private:
   DISALLOW_COPY_AND_ASSIGN(NativeWirelessNetworkParser);
@@ -91,7 +99,7 @@ class NativeWifiNetworkParser : public NativeWirelessNetworkParser {
   NativeWifiNetworkParser();
   virtual ~NativeWifiNetworkParser();
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           Network* network) OVERRIDE;
  protected:
   ConnectionSecurity ParseSecurity(const std::string& security);
@@ -106,7 +114,7 @@ class NativeCellularNetworkParser : public NativeWirelessNetworkParser {
   NativeCellularNetworkParser();
   virtual ~NativeCellularNetworkParser();
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           Network* network) OVERRIDE;
  protected:
   ActivationState ParseActivationState(const std::string& state);
@@ -123,13 +131,13 @@ class NativeVirtualNetworkParser : public NativeNetworkParser {
   NativeVirtualNetworkParser();
   virtual ~NativeVirtualNetworkParser();
   virtual bool ParseValue(PropertyIndex index,
-                          const Value& value,
+                          const base::Value& value,
                           Network* network) OVERRIDE;
-  virtual bool UpdateNetworkFromInfo(const DictionaryValue& info,
+  virtual bool UpdateNetworkFromInfo(const base::DictionaryValue& info,
                                      Network* network) OVERRIDE;
  protected:
   bool ParseProviderValue(PropertyIndex index,
-                                  const Value& value,
+                                  const base::Value& value,
                                   VirtualNetwork* network);
   ProviderType ParseProviderType(const std::string& type);
  private:
--- chrome/browser/ui/webui/chromeos/login/network_dropdown.cc
+++ chrome/browser/ui/webui/chromeos/login/network_dropdown.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/values.h"
 #include "content/browser/webui/web_ui.h"
 #include "chrome/browser/chromeos/login/proxy_settings_dialog.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
