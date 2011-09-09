6a85e795fb7408f082b5d174b347dfd2f8563d73
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-09 06:07:22 +0000

    REGRESSION(r94811): Assertion failure in 2 worker tests https://bugs.webkit.org/show_bug.cgi?id=67829
    Patch by Mark Hahnenberg <mhahnenberg@apple.com> on 2011-09-08
    Reviewed by Sam Weinig.
    
    Fixing a couple tests that were broken due to the wrong values being 
    set in the parent class pointers in the ClassInfo structs for 
    TerminatedExecutionError and InterruptedExecutionError.
    
    * runtime/ExceptionHelpers.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
@@ -42,14 +42,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-const ClassInfo InterruptedExecutionError::s_info = { "InterruptedExecutionError", 0, 0, 0 };
+const ClassInfo InterruptedExecutionError::s_info = { "InterruptedExecutionError", &Base::s_info, 0, 0 };
 
 UString InterruptedExecutionError::toString(ExecState*) const
 {
     return "JavaScript execution exceeded timeout.";
 }
 
-const ClassInfo TerminatedExecutionError::s_info = { "TerminatedExecutionError", 0, 0, 0 };
+const ClassInfo TerminatedExecutionError::s_info = { "TerminatedExecutionError", &Base::s_info, 0, 0 };
 
 UString TerminatedExecutionError::toString(ExecState*) const
 {
