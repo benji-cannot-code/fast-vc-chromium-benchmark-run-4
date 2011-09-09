11e3a4cb099a75adb9ca9312dd1f53f7b2509cd0
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 21:10:31 +0000

    Fix memory leak if user of IPC::SyncMessage never calls GetReplyDeserializer().
    This isn't a problem for most real code, because SyncChannel and SyncMessageFilter do it for you. It shows up in ppapi_unittests (and presumably any other tests that rely on IPC::TestSink).
    
    Also removed some code that worked around the problem.
    
    Bonus: Allow running ppapi_unittests in tools/valgrind/chrome_tests.py.
    
    BUG=90240
    TEST=valgrind trybots
    
    Review URL: http://codereview.chromium.org/7831060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100496 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_message.h
+++ ipc/ipc_sync_message.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include <string>
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 #include "ipc/ipc_message.h"
 
 namespace base {
@@ -26,6 +27,7 @@ class IPC_EXPORT SyncMessage : public Message {
  public:
   SyncMessage(int32 routing_id, uint32 type, PriorityValue priority,
               MessageReplyDeserializer* deserializer);
+  virtual ~SyncMessage();
 
   // Call this to get a deserializer for the output parameters.
   // Note that this can only be called once, and the caller is responsible
@@ -77,7 +79,7 @@ class IPC_EXPORT SyncMessage : public Message {
   static bool ReadSyncHeader(const Message& msg, SyncHeader* header);
   static bool WriteSyncHeader(Message* msg, const SyncHeader& header);
 
-  MessageReplyDeserializer* deserializer_;
+  scoped_ptr<MessageReplyDeserializer> deserializer_;
   base::WaitableEvent* pump_messages_event_;
 };
 
