343089c1ba25ea12346756a19702a9af3607211a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-09 14:32:15 +0000

    [Qt] QWebElement::encloseWith doesn't work at all https://bugs.webkit.org/show_bug.cgi?id=62464
    Enclosing an element means wrapping something around an element,
    not inserting something into an element. Therefore we need to check
    if the parent of the element allows insertion rather than checking
    the element itself.
    
    Patch by Zeno Albisser <zeno.albisser@nokia.com> on 2011-09-09
    Reviewed by Andreas Kling.
    
    * Api/qwebelement.cpp:
    (QWebElement::prependOutside):
    (QWebElement::appendOutside):
    (QWebElement::encloseWith):
    * tests/qwebelement/tst_qwebelement.cpp:
    (tst_QWebElement::appendAndPrepend):
    (tst_QWebElement::encloseWith):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94852 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/Api/qwebelement.cpp
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebelement.cpp
@@ -1112,16 +1112,17 @@ void QWebElement::prependOutside(const QString &markup)
     if (!m_element)
         return;
 
-    if (!m_element->parentNode())
+    Node* parent = m_element->parentNode();
+    if (!parent)
         return;
 
-    if (!m_element->isHTMLElement())
+    if (!parent->isHTMLElement())
         return;
 
-    RefPtr<DocumentFragment> fragment =  Range::createDocumentFragmentForElement(markup, toHTMLElement(m_element));
+    RefPtr<DocumentFragment> fragment = Range::createDocumentFragmentForElement(markup, toHTMLElement(parent));
 
     ExceptionCode exception = 0;
-    m_element->parentNode()->insertBefore(fragment, m_element, exception);
+    parent->insertBefore(fragment, m_element, exception);
 }
 
 /*!
@@ -1161,19 +1162,20 @@ void QWebElement::appendOutside(const QString &markup)
     if (!m_element)
         return;
 
-    if (!m_element->parentNode())
+    Node* parent = m_element->parentNode();
+    if (!parent)
         return;
 
-    if (!m_element->isHTMLElement())
+    if (!parent->isHTMLElement())
         return;
 
-    RefPtr<DocumentFragment> fragment =  Range::createDocumentFragmentForElement(markup, toHTMLElement(m_element));
+    RefPtr<DocumentFragment> fragment = Range::createDocumentFragmentForElement(markup, toHTMLElement(parent));
 
     ExceptionCode exception = 0;
     if (!m_element->nextSibling())
-        m_element->parentNode()->appendChild(fragment, exception);
+        parent->appendChild(fragment, exception);
     else
-        m_element->parentNode()->insertBefore(fragment, m_element->nextSibling(), exception);
+        parent->insertBefore(fragment, m_element->nextSibling(), exception);
 }
 
 /*!
@@ -1381,13 +1383,14 @@ void QWebElement::encloseWith(const QString &markup)
     if (!m_element)
         return;
 
-    if (!m_element->parentNode())
+    Node* parent = m_element->parentNode();
+    if (!parent)
         return;
 
-    if (!m_element->isHTMLElement())
+    if (!parent->isHTMLElement())
         return;
 
-    RefPtr<DocumentFragment> fragment =  Range::createDocumentFragmentForElement(markup, toHTMLElement(m_element));
+    RefPtr<DocumentFragment> fragment = Range::createDocumentFragmentForElement(markup, toHTMLElement(parent));
 
     if (!fragment || !fragment->firstChild())
         return;
@@ -1397,11 +1400,10 @@ void QWebElement::encloseWith(const QString &markup)
     if (!insertionPoint)
         return;
 
-    // Keep reference to these two nodes before pulling out this element and
+    // Keep reference to parent & siblingNode before pulling out this element and
     // wrapping it in the fragment. The reason for doing it in this order is
     // that once the fragment has been added to the document it is empty, so
     // we no longer have access to the nodes it contained.
-    Node* parent = m_element->parentNode();
     Node* siblingNode = m_element->nextSibling();
 
     ExceptionCode exception = 0;
