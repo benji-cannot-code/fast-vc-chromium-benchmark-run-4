935b831656660863b524a67ab278802eeb12e028
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 21:52:48 +0000

    Send the views-desktop flag from the login screen to the incognito session when it's set.
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/7867014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -741,6 +741,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       switches::kPpapiFlashInProcess,
       switches::kPpapiFlashPath,
       switches::kPpapiFlashVersion,
+      switches::kViewsDesktop,
 #if defined(TOUCH_UI)
       switches::kTouchDevices,
       // The virtual keyboard extension for TOUCH_UI (chrome://keyboard) highly
