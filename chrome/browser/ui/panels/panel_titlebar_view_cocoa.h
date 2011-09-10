bf1f4d987df5c5f752c4475bc82314710a6e99b9
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 20:54:55 +0000

    Revert 100537, looks like it broke PrintPreviewTabControllerBrowserTest.OpenPreviewTabFromPopupInCorrectPosition on Linux Views:
    http://build.chromium.org/p/chromium/builders/Linux%20Tests%20%28Views%20dbg%29%283%29/builds/5688/steps/browser_tests/logs/OpenPreviewTabFromPopupInCorrectPosition
    chrome/browser/printing/print_preview_tab_controller_browsertest.cc:223: Failure
    Expected: (browser()) != (p.browser), actual: 0x7f8364de6000 vs 0x7f8364de6000
    chrome/browser/printing/print_preview_tab_controller_browsertest.cc:224: Failure
    Value of: p.browser->type()
      Actual: 1
    Expected: Browser::TYPE_POPUP
    Which is: 2
    chrome/browser/printing/print_preview_tab_controller_browsertest.cc:237: Failure
    Value of: browser()->GetTabContentsWrapperAt(kTabCount)
      Actual: 0x7f835d178540
    Expected: preview_tab
    Which is: 0x7f835d1f11c0
    
    - Enable rearrangement of Panels on Mac by dragging.
    TEST=PanelBrowserTest.DragPanels
    
    Review URL: http://codereview.chromium.org/7857002
    
    TBR=dimich@chromium.org
    Review URL: http://codereview.chromium.org/7866031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_titlebar_view_cocoa.h
+++ chrome/browser/ui/panels/panel_titlebar_view_cocoa.h
@@ -25,15 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // differently based on being key window) so it appears easier to simply overlay
 // the standard titlebar.
 
-// When Drag is cancelled by hitting ESC key, we may still receive
-// the mouseDragged events but should ignore them until the mouse button is
-// released. Use these simple states to track this condition.
-enum PanelDragState {
-  PANEL_DRAG_CAN_START,  // Mouse key went down, drag may be started.
-  PANEL_DRAG_IN_PROGRESS,
-  PANEL_DRAG_SUPPRESSED  // Ignore drag events until PANEL_DRAG_CAN_START.
-};
-
 @interface PanelTitlebarViewCocoa : BackgroundGradientView {
  @private
   IBOutlet PanelWindowControllerCocoa* controller_;
@@ -42,7 +33,6 @@ enum PanelDragState {
   NSButton* closeButton_;  // Created explicitly, not from NIB. Weak, destroyed
                            // when view is destroyed, as a subview.
   ScopedCrTrackingArea closeButtonTrackingArea_;
-  PanelDragState dragState_;
 }
 
   // Callback from Close button.
@@ -64,13 +54,6 @@ enum PanelDragState {
 - (void)didChangeTheme:(NSNotification*)notification;
 - (void)didChangeMainWindow:(NSNotification*)notification;
 
-// Helpers to control title drag operation, called from more then one place.
-// TODO(dimich): replace BOOL parameter that we have to explicitly specify at
-// callsites with an enum defined in PanelManager.
-- (void)startDrag;
-- (void)endDrag:(BOOL)cancelled;
-- (void)dragWithDeltaX:(int)deltaX;
-
 @end  // @interface PanelTitlebarView
 
 // Methods which are either only for testing, or only public for testing.
@@ -80,14 +63,6 @@ enum PanelDragState {
 
 // Simulates click on a close button. Used to test panel closing.
 - (void)simulateCloseButtonClick;
-
-// NativePanelTesting support.
-- (void)pressLeftMouseButtonTitlebar;
-- (void)releaseLeftMouseButtonTitlebar;
-- (void)dragTitlebarDeltaX:(double)delta_x
-                    deltaY:(double)delta_y;
-- (void)cancelDragTitlebar;
-- (void)finishDragTitlebar;
 
 @end  // @interface PanelTitlebarViewCocoa(TestingAPI)
 
