51a6e683c248a8b0b37d574b863d1cb1d3c72bc8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-10 13:15:49 +0000

    Source/WebCore: [Qt] QWebSettings::setUserStyleSheetUrl() does not work with windows paths that contain drive letters https://bugs.webkit.org/show_bug.cgi?id=34884
    KURL::path() alone does not handle removing the leading slash from a windows file path.
    Using QUrl::toLocalFile() will turn file:///C:/path into C:/path appropriately.
    
    Patch by Jarred Nicholls <jarred@sencha.com> on 2011-09-10
    Reviewed by Andreas Kling.
    
    * platform/qt/KURLQt.cpp:
    (WebCore::KURL::fileSystemPath):
    
    Source/WebKit/qt: [Qt] QWebSettings::setUserStyleSheetUrl() does not work with windows paths that contain drive letters
    https://bugs.webkit.org/show_bug.cgi?id=34884
    
    KURL::path() alone does not handle removing the leading slash from a windows file path.
    Using QUrl::toLocalFile() will turn file:///C:/path into C:/path appropriately.
    
    New test case that ensures a user stylesheet from the file system will load correctly
    on all platforms.
    
    Patch by Jarred Nicholls <jarred@sencha.com> on 2011-09-10
    Reviewed by Andreas Kling.
    
    * tests/qwebpage/resources/user.css: Added.
    (p):
    * tests/qwebpage/tst_qwebpage.cpp:
    (tst_QWebPage::userStyleSheetFromFile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/KURLQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/KURLQt.cpp
@@ -47,7 +47,7 @@ String KURL::fileSystemPath() const
     if (!isValid() || !protocolIs("file"))
         return String();
 
-    return String(path());
+    return static_cast<QUrl>(*this).toLocalFile();
 }
 
 }
