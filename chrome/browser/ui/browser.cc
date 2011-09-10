32c644eb916c201143c453facad18ba45535564d
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 05:29:37 +0000

    Add UMA logging to track when users show the help tab.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7831074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2148,6 +2148,7 @@ void Browser::OpenUpdateChromeDialog() {
 }
 
 void Browser::ShowHelpTab() {
+  UserMetrics::RecordAction(UserMetricsAction("ShowHelpTab"));
   GURL help_url(kHelpContentUrl);
   GURL localized_help_url = google_util::AppendGoogleLocaleParam(help_url);
   ShowSingletonTab(localized_help_url);
