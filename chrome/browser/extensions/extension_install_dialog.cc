ae953e0b2d70be9f4b90b0707dbaabb88651c16b
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 18:35:11 +0000

    Fix use-after-free in ExtensionWebstorePrivateApiTest.*
    The tests don't actually show the installation confirmation dialog, so the BeginInstallWithManifestFunction object is deleted early.  This only affects tests.  There's a similar problem with the WebstoreInlineInstaller object.
    
    BUG=96064
    TEST=Run AddressSanitizer on browser_tests --gtest_filter=WebstoreInlineInstallTest.FLAKY_Install and --gtest_filter=ExtensionWebstorePrivateApiTest.*.
    
    
    Review URL: http://codereview.chromium.org/7796017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_dialog.cc
+++ chrome/browser/extensions/extension_install_dialog.cc
@@ -18,7 +18,7 @@ enum AutoConfirmForTest {
 };
 AutoConfirmForTest auto_confirm_for_tests = DO_NOT_SKIP;
 
-void ShowExtensionInstallDialogForManifest(
+bool ShowExtensionInstallDialogForManifest(
     Profile *profile,
     ExtensionInstallUI::Delegate* delegate,
     const DictionaryValue* manifest,
@@ -50,7 +50,7 @@ void ShowExtensionInstallDialogForManifest(
       id,
       &init_errors);
   if (!dummy_extension->get()) {
-    return;
+    return false;
   }
 
   if (icon->empty())
@@ -64,7 +64,7 @@ void ShowExtensionInstallDialogForManifest(
       delegate->InstallUIProceed();
     else
       delegate->InstallUIAbort(true);
-    return;
+    return true;
   }
 
   ExtensionInstallUI::Prompt filled_out_prompt = prompt;
@@ -76,6 +76,7 @@ void ShowExtensionInstallDialogForManifest(
                              dummy_extension->get(),
                              icon,
                              filled_out_prompt);
+  return true;
 }
 
 void SetExtensionInstallDialogForManifestAutoConfirmForTests(
--- chrome/browser/extensions/extension_webstore_private_api.cc
+++ chrome/browser/extensions/extension_webstore_private_api.cc
@@ -259,7 +259,7 @@ void BeginInstallWithManifestFunction::OnWebstoreParseSuccess(
 
   ExtensionInstallUI::Prompt prompt(ExtensionInstallUI::INSTALL_PROMPT);
 
-  ShowExtensionInstallDialogForManifest(
+  if (!ShowExtensionInstallDialogForManifest(
       profile(),
       this,
       parsed_manifest,
@@ -268,8 +268,7 @@ void BeginInstallWithManifestFunction::OnWebstoreParseSuccess(
       "", // no localized description
       &icon_,
       prompt,
-      &dummy_extension_);
-  if (!dummy_extension_.get()) {
+      &dummy_extension_)) {
     OnWebstoreParseFailure(WebstoreInstallHelper::Delegate::MANIFEST_ERROR,
                            kInvalidManifestError);
     return;
