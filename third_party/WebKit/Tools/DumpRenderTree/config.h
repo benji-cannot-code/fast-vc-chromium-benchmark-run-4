b69fae90978461d357768c20c5342545db0e7ec9
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-11 00:52:14 +0000

    [wx] Unreviewed build fix. MSW build fixes.
    git-svn-id: svn://svn.chromium.org/blink/trunk@94916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/config.h
+++ third_party/WebKit/Tools/DumpRenderTree/config.h
@@ -54,6 +54,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif /* USE(EXPORT_MACROS) */
 
+// On MSW, wx headers need to be included before windows.h is.
+// The only way we can always ensure this is if we include wx here.
+#if PLATFORM(WX)
+#include <wx/defs.h>
+#endif
+
+
 #ifdef __cplusplus
 #undef new
 #undef delete
@@ -70,6 +77,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif // PLATFORM(MAC)
 
+#if OS(WINDOWS)
+// If we don't define these, they get defined in windef.h. 
+// We want to use std::min and std::max
+#undef max
+#define max max
+#undef min
+#define min min
+#endif
+
 #if PLATFORM(WIN)
 #define WTF_USE_CF 1 
 #if PLATFORM(WIN_CAIRO)
@@ -85,13 +101,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #undef WINVER
 #define WINVER 0x0500
-
-// If we don't define these, they get defined in windef.h. 
-// We want to use std::min and std::max
-#undef max
-#define max max
-#undef min
-#define min min
 
 #undef _WINSOCKAPI_
 #define _WINSOCKAPI_ // Prevent inclusion of winsock.h in windows.h
