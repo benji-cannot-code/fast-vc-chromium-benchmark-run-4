ccdd04e3f5f1a56da90dfe2e50d0d366d311a48f
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-11 18:34:02 +0000

    [Qt] Win32 build broken due to MachineStackMarker.cpp/.o failing to link against pthreads library https://bugs.webkit.org/show_bug.cgi?id=67864
    Qt Win32 is not pthread compatible and cannot participate in multithreaded JSC or it fails to build.
    
    Patch by Jarred Nicholls <jarred@sencha.com> on 2011-09-11
    Reviewed by Csaba Osztrogonác.
    
    * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -510,11 +510,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_PTHREAD_BASED_QT 1
 #endif
 
-#if !defined(ENABLE_JSC_MULTIPLE_THREADS)
+/* FIXME: JSC multithreading currently only supports pthread.
+   Qt-Win cannot do multithreading because it is not pthread-based. */
+#if !defined(ENABLE_JSC_MULTIPLE_THREADS) && (!PLATFORM(QT) || USE(PTHREAD_BASED_QT))
 #define ENABLE_JSC_MULTIPLE_THREADS 1
 #endif
 
-#if !defined(ENABLE_WTF_MULTIPLE_THREADS)
+/* FIXME: JSC multithreading currently only supports pthread.
+   Qt-Win cannot do multithreading because it is not pthread-based. */
+#if !defined(ENABLE_WTF_MULTIPLE_THREADS) && (!PLATFORM(QT) || USE(PTHREAD_BASED_QT))
 #define ENABLE_WTF_MULTIPLE_THREADS 1
 #endif
 
