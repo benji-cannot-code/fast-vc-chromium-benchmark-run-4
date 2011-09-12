e5f663ea2cf04a8b7e0d6216284b23592f8134ea
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-12 05:46:44 +0000

    Implement a ProgressEvent constructor for V8 https://bugs.webkit.org/show_bug.cgi?id=67800
    Patch by Kentaro Hara <haraken@google.com> on 2011-09-11
    Reviewed by Sam Weinig.
    
    Source/WebCore:
    
    Test: fast/events/constructors/progress-event-constructor.html
    
    * bindings/js/JSDictionary.cpp:
    (WebCore::JSDictionary::convertValue): Replaced UnsignedLongLongMax with std::numeric_limits<unsigned long long>::max().
    * bindings/v8/OptionsObject.cpp:
    (WebCore::OptionsObject::getKeyValue): Returns an unsigned long long value corresponding to a given key. Spec: http://www.w3.org/TR/WebIDL/#es-unsigned-long-long
    * bindings/v8/OptionsObject.h:
    * bindings/v8/custom/V8EventConstructors.cpp: Added the ProgressEvent constructor.
    * dom/ProgressEvent.idl: Added a 'V8CustomConstructor' attribute.
    
    LayoutTests:
    
    Enabled fast/events/constructors/progress-event-constructor.html
    
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8EventConstructors.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8EventConstructors.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "DocumentFragment.h"
 #include "Node.h"
+#include "ProgressEvent.h"
 
 #include "OptionsObject.h"
 #include "V8Binding.h"
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8Document.h"
 #include "V8Event.h"
 #include "V8Node.h"
+#include "V8ProgressEvent.h"
 #include "V8Proxy.h"
 
 #include <wtf/RefPtr.h>
@@ -100,6 +102,7 @@ static v8::Handle<v8::Value> constructV8Event(const v8::Arguments& args, bool (*
 
 INSTANTIATE_INITIALIZING_CONSTRUCTOR_FOR_EVENT(DICTIONARY_START, DICTIONARY_END, FILL_PARENT_PROPERTIES, FILL_PROPERTY)
 INSTANTIATE_INITIALIZING_CONSTRUCTOR_FOR_CUSTOM_EVENT(DICTIONARY_START, DICTIONARY_END, FILL_PARENT_PROPERTIES, FILL_PROPERTY)
+INSTANTIATE_INITIALIZING_CONSTRUCTOR_FOR_PROGRESS_EVENT(DICTIONARY_START, DICTIONARY_END, FILL_PARENT_PROPERTIES, FILL_PROPERTY)
 
 
 } // namespace WebCore
