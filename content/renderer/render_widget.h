95278da104a35fd9b95c925deac3d9ae60f493f1
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 23:33:07 +0000

    Revert 100801 - Ensure that compositor does not block the renderer process by issuing too many SwapBuffers.
    This might have regressed with r84372. It caused there to be one additional token queued in the swap buffers token queue than before.
    
    BUG=95649
    Review URL: http://codereview.chromium.org/7867053
    
    TBR=apatrick@chromium.org
    Review URL: http://codereview.chromium.org/7880009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100803 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.h
+++ content/renderer/render_widget.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "content/renderer/paint_aggregator.h"
-#include "gpu/command_buffer/client/gles2_implementation.h"
 #include "ipc/ipc_channel.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCompositionUnderline.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPopupType.h"
@@ -369,8 +368,7 @@ class RenderWidget : public IPC::Channel::Listener,
   // When accelerated rendering is on, is the maximum number of swapbuffers that
   // can be outstanding before we start throttling based on
   // OnSwapBuffersComplete callback.
-  static const int kMaxSwapBuffersPending =
-      gpu::gles2::GLES2Implementation::kMaxSwapBuffers;
+  static const int kMaxSwapBuffersPending = 2;
 
   // Set to true if we should ignore RenderWidget::Show calls.
   bool did_show_;
