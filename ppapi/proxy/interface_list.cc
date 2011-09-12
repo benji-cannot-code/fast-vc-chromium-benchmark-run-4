38a6aac1f4b059cf57ad8bd2591178eb0e37c357
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 20:31:59 +0000

    Revert 100751 - Fix Windows build by renaming interface to iface.
    TEST=it compiles
    Review URL: http://codereview.chromium.org/7871005
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/7872007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100752 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/interface_list.cc
+++ ppapi/proxy/interface_list.cc
@@ -242,7 +242,7 @@ const void* InterfaceList::GetInterfaceForPPB(const std::string& name) const {
       name_to_browser_info_.find(name);
   if (found == name_to_browser_info_.end())
     return NULL;
-  return found->second.iface;
+  return found->second.interface;
 }
 
 const void* InterfaceList::GetInterfaceForPPP(const std::string& name) const {
@@ -250,7 +250,7 @@ const void* InterfaceList::GetInterfaceForPPP(const std::string& name) const {
       name_to_plugin_info_.find(name);
   if (found == name_to_plugin_info_.end())
     return NULL;
-  return found->second.iface;
+  return found->second.interface;
 }
 
 void InterfaceList::AddProxy(InterfaceID id,
@@ -271,16 +271,16 @@ void InterfaceList::AddProxy(InterfaceID id,
 
 void InterfaceList::AddPPB(const char* name,
                            InterfaceID id,
-                           const void* iface) {
+                           const void* interface) {
   DCHECK(name_to_browser_info_.find(name) == name_to_browser_info_.end());
-  name_to_browser_info_[name] = InterfaceInfo(id, iface);
+  name_to_browser_info_[name] = InterfaceInfo(id, interface);
 }
 
 void InterfaceList::AddPPP(const char* name,
                            InterfaceID id,
-                           const void* iface) {
+                           const void* interface) {
   DCHECK(name_to_plugin_info_.find(name) == name_to_plugin_info_.end());
-  name_to_plugin_info_[name] = InterfaceInfo(id, iface);
+  name_to_plugin_info_[name] = InterfaceInfo(id, interface);
 }
 
 void InterfaceList::AddPPB(const InterfaceProxy::Info* info) {
