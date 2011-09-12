8359a34f49a41b49802e08a0b4c4a21197c6d497
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-12 14:20:07 +0000

    [Qt][WK2] WebKitTestRunner does not produce crash logs https://bugs.webkit.org/show_bug.cgi?id=67714
    Added a simple way of generating backtrace on crash
    to the web process. The implementation is similar what
    we have in DRT. It depends on GNU libc functionality
    so it is only enabled where we are running in such an environment.
    
    * WebKitTestRunner/InjectedBundle/qt/InjectedBundleQt.cpp:
    (WTR::printBacktrace):
    (WTR::crashHandler):
    (WTR::InjectedBundle::platformInitialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94955 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/InjectedBundleQt.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/InjectedBundleQt.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011 University of Szeged. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,11 +27,62 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "InjectedBundle.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <wtf/AlwaysInline.h>
+
+#if HAVE(SIGNAL_H)
+#include <signal.h>
+#endif
+
+#if defined(__GLIBC__) && !defined(__UCLIBC__)
+#include <execinfo.h>
+#endif
 
 namespace WTR {
 
+static inline void printBacktrace()
+{
+#if defined(__GLIBC__) && !defined(__UCLIBC__)
+    void* frames[256];
+    size_t size = backtrace(frames, 256);
+    if (!size)
+        return;
+
+    char** symbols = backtrace_symbols(frames, size);
+    if (!symbols)
+        return;
+
+    for (unsigned i = 0; i < size; ++i)
+        fprintf(stderr, "%u: %s\n", i, symbols[i]);
+
+    fflush(stderr);
+    free(symbols);
+#endif
+}
+
+#if HAVE(SIGNAL_H)
+static NO_RETURN void crashHandler(int signal)
+{
+    fprintf(stderr, "%s\n", strsignal(signal));
+    printBacktrace();
+    exit(128 + signal);
+}
+#endif
+
 void InjectedBundle::platformInitialize(WKTypeRef)
 {
+#if HAVE(SIGNAL_H)
+    signal(SIGILL, crashHandler);    /* 4:   illegal instruction (not reset when caught) */
+    signal(SIGTRAP, crashHandler);   /* 5:   trace trap (not reset when caught) */
+    signal(SIGFPE, crashHandler);    /* 8:   floating point exception */
+    signal(SIGBUS, crashHandler);    /* 10:  bus error */
+    signal(SIGSEGV, crashHandler);   /* 11:  segmentation violation */
+    signal(SIGSYS, crashHandler);    /* 12:  bad argument to system call */
+    signal(SIGPIPE, crashHandler);   /* 13:  write on a pipe with no reader */
+    signal(SIGXCPU, crashHandler);   /* 24:  exceeded CPU time limit */
+    signal(SIGXFSZ, crashHandler);   /* 25:  exceeded file size limit */
+#endif
 }
 
 } // namespace WTR
