58c478fcb3e8b801c5c9a16db9e195a7f39d954f
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 17:04:37 +0000

    [cros] Temporarily disable post-auth pods animation. Other WebUI fixes: * Prevent clicking on other pods when authentication is in progress. * Removed irrelevant code in context of WebUI. * Use same font-family for WebUI buttons.
    BUG=chromium-os:11116
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7754023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100707 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -187,6 +187,7 @@ cr.define('login', function() {
         chrome.send('authenticateUser',
             [this.user.emailAddress, this.passwordElement.value]);
       }
+      this.parentNode.rowEnabled = false;
 
       return true;
     },
@@ -196,6 +197,7 @@ cr.define('login', function() {
      * @param {boolean} takeFocus True to take focus.
      */
     reset: function(takeFocus) {
+      this.parentNode.rowEnabled = true;
       this.passwordElement.value = '';
 
       if (takeFocus)
@@ -267,6 +269,15 @@ cr.define('login', function() {
      */
     get pods() {
       return this.children;
+    },
+
+    // True when clicking on pods is enabled.
+    rowEnabled_ : true,
+    get rowEnabled() {
+      return this.rowEnabled_;
+    },
+    set rowEnabled(enabled) {
+      this.rowEnabled_ = enabled;
     },
 
     /**
@@ -456,6 +467,7 @@ cr.define('login', function() {
      * @param {boolean} takeFocus True to take focus.
      */
     reset: function(takeFocus) {
+      this.rowEnabled = true;
       for (var i = 0; i < this.pods.length; ++i)
         this.pods[i].mainInput.disabled = false;
 
@@ -468,6 +480,8 @@ cr.define('login', function() {
      * @private
      */
     handleClick_: function(e) {
+      if (!this.rowEnabled)
+        return;
       // Clears focus if not clicked on a pod.
       if (e.target.parentNode != this &&
           e.target.parentNode.parentNode != this)
@@ -478,6 +492,8 @@ cr.define('login', function() {
      * Handles focus event.
      */
     handleFocus_: function(e) {
+      if (!this.rowEnabled)
+        return;
       if (e.target.parentNode == this) {
         // Focus on a pod
         if (e.target.classList.contains('focused'))
