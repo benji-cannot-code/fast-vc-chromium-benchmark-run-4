924676a2c924716f608d0cf92b49fe6836d16c65
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 09:21:14 +0000

    Make sure we never overwrite the default image.
    BUG=95662 
    TEST=Go to chrome://extensions or chrome://settings/extensionSettings and drag any extension icon to the Omnibox. Then start fiddling with the url parameters, changing the last two numbers. Make sure there is no crash and an image is always returned, even if you ask for absurd values for size (former number) and match properties (latter number).
    Review URL: http://codereview.chromium.org/306028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100664 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/extension_icon_source.cc
+++ chrome/browser/ui/webui/extension_icon_source.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-scoped_refptr<RefCountedMemory> BitmapToMemory(SkBitmap* image) {
+scoped_refptr<RefCountedMemory> BitmapToMemory(const SkBitmap* image) {
   RefCountedBytes* image_bytes = new RefCountedBytes;
   gfx::PNGCodec::EncodeBGRASkBitmap(*image, false, &image_bytes->data());
   return image_bytes;
@@ -51,7 +51,6 @@ SkBitmap* ToBitmap(const unsigned char* data, size_t size) {
 }
 
 }  // namespace
-
 
 ExtensionIconSource::ExtensionIconSource(Profile* profile)
     : DataSource(chrome::kChromeUIExtensionIconHost, MessageLoop::current()),
@@ -150,17 +149,18 @@ void ExtensionIconSource::LoadIconFailed(int request_id) {
     LoadDefaultImage(request_id);
 }
 
-SkBitmap* ExtensionIconSource::GetDefaultAppImage() {
+const SkBitmap* ExtensionIconSource::GetDefaultAppImage() {
   if (!default_app_data_.get())
     default_app_data_.reset(LoadImageByResourceId(IDR_APP_DEFAULT_ICON));
 
   return default_app_data_.get();
 }
 
-SkBitmap* ExtensionIconSource::GetDefaultExtensionImage() {
-  if (!default_extension_data_.get())
+const SkBitmap* ExtensionIconSource::GetDefaultExtensionImage() {
+  if (!default_extension_data_.get()) {
     default_extension_data_.reset(
         LoadImageByResourceId(IDR_EXTENSION_DEFAULT_ICON));
+  }
 
   return default_extension_data_.get();
 }
@@ -176,18 +176,24 @@ void ExtensionIconSource::FinalizeImage(SkBitmap* image,
 
 void ExtensionIconSource::LoadDefaultImage(int request_id) {
   ExtensionIconRequest* request = GetData(request_id);
-  SkBitmap* decoded = NULL;
+  const SkBitmap* default_image = NULL;
 
   if (request->extension->is_app())
-    decoded = GetDefaultAppImage();
+    default_image = GetDefaultAppImage();
   else
-    decoded = GetDefaultExtensionImage();
+    default_image = GetDefaultExtensionImage();
 
-  *decoded = skia::ImageOperations::Resize(
-      *decoded, skia::ImageOperations::RESIZE_LANCZOS3,
-      request->size, request->size);
+  SkBitmap resized_image(skia::ImageOperations::Resize(
+      *default_image, skia::ImageOperations::RESIZE_LANCZOS3,
+      request->size, request->size));
 
-  FinalizeImage(decoded, request_id);
+  // There are cases where Resize returns an empty bitmap, for example if you
+  // ask for an image too large. In this case it is better to return the default
+  // image than returning nothing at all.
+  if (resized_image.empty())
+    resized_image = *default_image;
+
+  FinalizeImage(&resized_image, request_id);
 }
 
 bool ExtensionIconSource::TryLoadingComponentExtensionImage(
