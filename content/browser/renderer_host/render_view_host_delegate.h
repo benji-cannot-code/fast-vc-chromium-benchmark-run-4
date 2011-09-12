4ede8ce37a2c8dc401c7881658598e6ca80374f4
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 18:24:39 +0000

    Move handling of the ViewHostMsg_WebUISend out of Chrome. This message is handled by ChromeOS the balloon notification class which is a RVHD. We now handle this message in RVH and call out the new virtual function in WebUISend in the RVHD interface which the balloon class overrides.
    Continuing changes to get rid of the pattern of IPC messages spanning across
    content and chrome.
    
    BUG=87335
    TEST=No change in functionality
    Review URL: http://codereview.chromium.org/7864016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100725 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_delegate.h
+++ content/browser/renderer_host/render_view_host_delegate.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/process_util.h"
 #include "base/string16.h"
+#include "base/values.h"
 #include "content/common/view_types.h"
 #include "content/common/window_container_type.h"
 #include "ipc/ipc_channel.h"
@@ -376,6 +377,11 @@ class RenderViewHostDelegate : public IPC::Channel::Listener {
   // The contents' preferred size changed.
   virtual void UpdatePreferredSize(const gfx::Size& pref_size) {}
 
+  // Notification message from HTML UI.
+  virtual void WebUISend(RenderViewHost* render_view_host,
+                         const GURL& source_url,
+                         const std::string& name,
+                         const base::ListValue& args) {}
  protected:
   virtual ~RenderViewHostDelegate() {}
 };
