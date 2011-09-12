ded97a6faa634db270e9a8c698952d01264d3bc8
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-12 21:11:32 +0000

    Rubber-stamped by Adam Barth.
    Remove the "the tree is burning!" warning previously shown
    when landing patches.  It was a dream.  But it's just noise now.
    
    Some bots will require a restart to notice the removal of the option
    so I've left the --ignore-builders option in for now.
    
    * Scripts/webkitpy/tool/bot/commitqueuetask_unittest.py:
    * Scripts/webkitpy/tool/bot/patchanalysistask.py:
    * Scripts/webkitpy/tool/commands/download.py:
    * Scripts/webkitpy/tool/commands/download_unittest.py:
    * Scripts/webkitpy/tool/commands/queues_unittest.py:
    * Scripts/webkitpy/tool/steps/__init__.py:
    * Scripts/webkitpy/tool/steps/commit.py:
    * Scripts/webkitpy/tool/steps/ensurebuildersaregreen.py: Removed.
    * Scripts/webkitpy/tool/steps/postdiffforrevert.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/patchanalysistask.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/patchanalysistask.py
@@ -164,7 +164,6 @@ class PatchAnalysisTask(object):
         return self._run_command([
             "land-attachment",
             "--force-clean",
-            "--ignore-builders",
             "--non-interactive",
             "--parent-command=" + self._delegate.parent_command(),
             self._patch.id(),
--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/download.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/download.py
@@ -93,7 +93,6 @@ class Land(AbstractSequencedCommand):
     argument_names = "[BUGID]"
     show_in_main_help = True
     steps = [
-        steps.EnsureBuildersAreGreen,
         steps.UpdateChangeLogsWithReviewer,
         steps.ValidateReviewer,
         steps.ValidateChangeLogs, # We do this after UpdateChangeLogsWithReviewer to avoid not having to cache the diff twice.
@@ -253,9 +252,6 @@ class ApplyFromBug(AbstractPatchApplyingCommand, ProcessBugsMixin):
 
 
 class AbstractPatchLandingCommand(AbstractPatchSequencingCommand):
-    prepare_steps = [
-        steps.EnsureBuildersAreGreen,
-    ]
     main_steps = [
         steps.CleanWorkingDirectory,
         steps.Update,
