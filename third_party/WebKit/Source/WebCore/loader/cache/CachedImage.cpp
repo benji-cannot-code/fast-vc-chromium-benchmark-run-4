0fda12ddf5d7bdbcae266e5d82059fe6b9d00511
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-12 21:52:01 +0000

    https://bugs.webkit.org/show_bug.cgi?id=67898 REGRESSION(r94900): fast/images/support-broken-image-delegate.html fails on Mac
    Reviewed by Simon Fraser.
    
    Source/WebCore: 
    
    New function willPaintBrokenImage() returns true when there has been an error 
    loading the image and the broken image icon will be used in its place. This is 
    necessary since it is possible to have an error loading an image and to NOT use 
    the broken image icon. 
    * loader/cache/CachedImage.cpp:
    (WebCore::CachedImage::willPaintBrokenImage):
    * loader/cache/CachedImage.h:
    * rendering/RenderImage.cpp:
    (WebCore::RenderImage::imageSizeForError):
    
    LayoutTests: 
    
    * platform/mac/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@94980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedImage.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedImage.cpp
@@ -125,6 +125,11 @@ Image* CachedImage::brokenImage(float deviceScaleFactor) const
     return brokenImageLoRes;
 }
 
+bool CachedImage::willPaintBrokenImage() const
+{
+    return errorOccurred() && m_shouldPaintBrokenImage;
+}
+
 Image* CachedImage::image() const
 {
     ASSERT(!isPurgeable());
