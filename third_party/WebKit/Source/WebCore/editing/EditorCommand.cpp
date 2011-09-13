fa28180eadcafdf5df3ae73de318caecdf621104
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-13 22:31:24 +0000

    For compatibility, execCommand should support deprecated 'useCSS' alias for 'styleWithCSS' https://bugs.webkit.org/show_bug.cgi?id=36683
    Patch by Kiyoto Tamura <owenestea@gmail.com> on 2011-09-13
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    In addition to supporting the deprecated 'useCSS', 'styleWithCSS' now accepts any argument other than
    the boolean false or the case-insensitive string "false". This is per
    http://aryeh.name/spec/editing/editing.html#the-stylewithcss-command
    
    Tests: editing/execCommand/style-with-css.html
           editing/execCommand/use-css.html
    
    * editing/EditorCommand.cpp:
    (WebCore::executeStyleWithCSS):
    (WebCore::executeUseCSS):
    (WebCore::createCommandMap):
    
    LayoutTests:
    
    Testing useCSS, the command previously unsupported by WebKit. Also, we are testing
    styleWithCSS accepts anything that is not the boolean false or the string "false" as true.
    Furthermore, we test that queryCommandValue/State('useCSS') return boolean false and
    queryCommandValue/State('useStyleWithCSS') returns booleans (as opposed to 'true'/'false'
    strings)
    
    * editing/execCommand/style-with-css-expected.txt: Added.
    * editing/execCommand/style-with-css.html: Added.
    * editing/execCommand/use-css-expected.txt: Added.
    * editing/execCommand/use-css.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/EditorCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/EditorCommand.cpp
@@ -1018,10 +1018,13 @@ static bool executeStrikethrough(Frame* frame, Event*, EditorCommandSource sourc
 
 static bool executeStyleWithCSS(Frame* frame, Event*, EditorCommandSource, const String& value)
 {
-    if (value != "false" && value != "true")
-        return false;
-    
-    frame->editor()->setShouldStyleWithCSS(value == "true" ? true : false);
+    frame->editor()->setShouldStyleWithCSS(!equalIgnoringCase(value, "false"));
+    return true;
+}
+
+static bool executeUseCSS(Frame* frame, Event*, EditorCommandSource, const String& value)
+{
+    frame->editor()->setShouldStyleWithCSS(equalIgnoringCase(value, "false"));
     return true;
 }
 
@@ -1535,6 +1538,7 @@ static const CommandMap& createCommandMap()
         { "Unlink", { executeUnlink, supported, enabledRangeInRichlyEditableText, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
         { "Unscript", { executeUnscript, supportedFromMenuOrKeyBinding, enabledInRichlyEditableText, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
         { "Unselect", { executeUnselect, supported, enabledVisibleSelection, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
+        { "UseCSS", { executeUseCSS, supported, enabled, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
         { "Yank", { executeYank, supportedFromMenuOrKeyBinding, enabledInEditableText, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
         { "YankAndSelect", { executeYankAndSelect, supportedFromMenuOrKeyBinding, enabledInEditableText, stateNone, valueNull, notTextInsertion, doNotAllowExecutionWhenDisabled } },
 
