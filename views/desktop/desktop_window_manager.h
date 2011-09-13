d4eabb4aab91331221d21433c3ca6300265a89bc
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 02:33:33 +0000

    views desktop wm: Activate a window when it is touched.
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/7796040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100842 0039d316-1c4b-4281-b951-d872f2087c98

--- views/desktop/desktop_window_manager.h
+++ views/desktop/desktop_window_manager.h
@@ -45,6 +45,8 @@ class DesktopWindowManager : public views::WindowManager,
                               const views::KeyEvent& event) OVERRIDE;
   virtual bool HandleMouseEvent(views::Widget* widget,
                                 const views::MouseEvent& event) OVERRIDE;
+  virtual ui::TouchStatus HandleTouchEvent(views::Widget* widget,
+      const views::TouchEvent& event) OVERRIDE;
 
   virtual void Register(Widget* widget) OVERRIDE;
 
@@ -59,6 +61,10 @@ class DesktopWindowManager : public views::WindowManager,
   bool HasMouseCapture() const;
 
   void Activate(Widget* widget);
+
+  // Returns true if a deactivated widget at the location was activated. Returns
+  // false otherwise.
+  bool ActivateWidgetAtLocation(Widget* widget, const gfx::Point& point);
 
   views::Widget* desktop_;
   views::Widget* mouse_capture_;
