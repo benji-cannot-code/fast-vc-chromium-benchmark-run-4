19c58d440a191d149ce1a96c9167cea3fd4f770a
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 04:21:47 +0000

    Switch to the new client-side phishing model that uses Murmurhash for word hashes.
    This gives about a 50% performance improvement on extraction of page term features.
    
    BUG=96060
    TEST=updated existing tests
    
    
    Review URL: http://codereview.chromium.org/7866011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100858 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1807,6 +1807,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'renderer/plugin_uma_unittest.cc',
         'renderer/renderer_about_handler_unittest.cc',
         'renderer/safe_browsing/features_unittest.cc',
+        'renderer/safe_browsing/murmurhash3_util_unittest.cc',
         'renderer/safe_browsing/phishing_term_feature_extractor_unittest.cc',
         'renderer/safe_browsing/phishing_url_feature_extractor_unittest.cc',
         'renderer/safe_browsing/scorer_unittest.cc',
---
+++ chrome/renderer/safe_browsing/murmurhash3_util.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/safe_browsing/murmurhash3_util.h"
+#include "third_party/smhasher/src/MurmurHash3.h"
+
+namespace safe_browsing {
+
+uint32 MurmurHash3String(const std::string& str, uint32 seed) {
+  uint32 output;
+  MurmurHash3_x86_32(str.data(), str.size(), seed, &output);
+  return output;
+}
+
+}  // namespace safe_browsing
