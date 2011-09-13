ec8f14d5dfcf0cdf9014d81a421cf7c46d193551
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-13 03:38:22 +0000

    [EFL] DRT: Add DumpRenderTreeEfl.h https://bugs.webkit.org/show_bug.cgi?id=63993
    Patch by Leandro Pereira <leandro@profusion.mobi> on 2011-09-12
    Reviewed by Eric Seidel.
    
    This header contains some global variables used by EFL's
    DumpRenderTree implementation.
    
    * DumpRenderTree/DumpRenderTree.h: Include DumpRenderTreeEfl.h when
    appropriate.
    * DumpRenderTree/efl/DumpRenderTreeEfl.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.h
+++ third_party/WebKit/Tools/DumpRenderTree/DumpRenderTree.h
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DumpRenderTreeGtk.h"
 #elif PLATFORM(WX)
 #include "DumpRenderTreeWx.h"
+#elif PLATFORM(EFL)
+#include "DumpRenderTreeEfl.h"
 #endif
 
 #include <string>
