d93dbd4ded63e50d95d42b86140da6907a7916a3
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 21:09:30 +0000

    Show or hide settings button depending on whether the panel is on focus or the mouse is over it.
    BUG=none
    TEST=manual test to verify settings button visibility by lauching panels, switching panels and moving mouse
    
    Review URL: http://codereview.chromium.org/7880011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100963 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_window_controller_cocoa.h
+++ chrome/browser/ui/panels/panel_window_controller_cocoa.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/browser_command_executor.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
+#import "chrome/browser/ui/cocoa/tracking_area.h"
 
 @class FindBarCocoaController;
 class PanelBrowserWindowCocoa;
@@ -34,6 +35,7 @@ class PanelBrowserWindowCocoa;
   NSViewAnimation* boundsAnimation_;  // Lifetime controlled manually, needs
                                       // more then just |release| to terminate.
   BOOL animateOnBoundsChange_;
+  ScopedCrTrackingArea windowTrackingArea_;
 }
 
 // Load the browser window nib and do any Cocoa-specific initialization.
