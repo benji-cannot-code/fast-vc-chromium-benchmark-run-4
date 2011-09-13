ca158effb329edb042cb90b46971ce48348b1640
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 04:55:18 +0000

    Don't interpret embeded NULLs in a response header line as a line terminator.
    BUG=95992
    
    Review URL: http://codereview.chromium.org/7796025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100863 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -115,6 +115,13 @@ int MapHttpResponseCode(int code) {
   return 0;
 }
 
+void CheckDoesNotHaveEmbededNulls(const std::string& str) {
+  // Care needs to be taken when adding values to the raw headers string to
+  // make sure it does not contain embeded NULLs. Any embeded '\0' may be
+  // understood as line terminators and change how header lines get tokenized.
+  CHECK(str.find('\0') == std::string::npos);
+}
+
 }  // namespace
 
 struct HttpResponseHeaders::ParsedHeader {
@@ -301,6 +308,7 @@ void HttpResponseHeaders::RemoveHeader(const std::string& name) {
 }
 
 void HttpResponseHeaders::AddHeader(const std::string& header) {
+  CheckDoesNotHaveEmbededNulls(header);
   DCHECK_EQ('\0', raw_headers_[raw_headers_.size() - 2]);
   DCHECK_EQ('\0', raw_headers_[raw_headers_.size() - 1]);
   // Don't copy the last null.
@@ -316,6 +324,7 @@ void HttpResponseHeaders::AddHeader(const std::string& header) {
 }
 
 void HttpResponseHeaders::ReplaceStatusLine(const std::string& new_status) {
+  CheckDoesNotHaveEmbededNulls(new_status);
   // Copy up to the null byte.  This just copies the status line.
   std::string new_raw_headers(new_status);
   new_raw_headers.push_back('\0');
