f52fcb430886dfd717c674c5576c2f487ed3f78e
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 04:40:33 +0000

    Fix URLFetcherTest.CancelAll.
    I don't know what I was smoking before when I wrote this test. It was obviously racy.
    Fix it by moving the EXPECTs and CancelAll call to the IO thread.
    The last EXPECT is fine on the main test thread since it should be guaranteed to be 0.
    
    BUG=89863,88221
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7881001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100861 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/net/url_fetcher_unittest.cc
+++ content/common/net/url_fetcher_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/net/url_fetcher.h"
 
+#include "base/bind.h"
 #include "base/message_loop_proxy.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
@@ -29,24 +30,7 @@ using base::TimeDelta;
 namespace {
 
 const FilePath::CharType kDocRoot[] = FILE_PATH_LITERAL("chrome/test/data");
-const std::string kTestServerFilePrefix = "files/";
-
-class CurriedTask : public Task {
- public:
-  CurriedTask(Task* task, MessageLoop* target_loop)
-      : task_(task),
-        target_loop_(target_loop) {}
-
-  virtual void Run() {
-    target_loop_->PostTask(FROM_HERE, task_);
-  }
-
- private:
-  Task* const task_;
-  MessageLoop* const target_loop_;
-
-  DISALLOW_COPY_AND_ASSIGN(CurriedTask);
-};
+const char kTestServerFilePrefix[] = "files/";
 
 class TestURLRequestContextGetter : public net::URLRequestContextGetter {
  public:
@@ -77,6 +61,10 @@ class TestURLRequestContextGetter : public net::URLRequestContextGetter {
 class URLFetcherTest : public testing::Test, public URLFetcher::Delegate {
  public:
   URLFetcherTest() : fetcher_(NULL) { }
+
+  static int GetNumFetcherCores() {
+    return URLFetcher::GetNumFetcherCores();
+  }
 
   // Creates a URLFetcher, using the program's main thread to do IO.
   virtual void CreateFetcher(const GURL& url);
@@ -109,10 +97,6 @@ class URLFetcherTest : public testing::Test, public URLFetcher::Delegate {
 #if defined(USE_NSS)
     net::ShutdownOCSP();
 #endif
-  }
-
-  int GetNumFetcherCores() const {
-    return URLFetcher::GetNumFetcherCores();
   }
 
   // URLFetcher is designed to run on the main UI thread, but in our tests
@@ -357,7 +341,8 @@ TEST_F(URLFetcherTempFileTest, SmallGet) {
   // Get a small file.
   const char* kFileToFetch = "simple.html";
   expected_file_ = test_server.document_root().AppendASCII(kFileToFetch);
-  CreateFetcher(test_server.GetURL(kTestServerFilePrefix + kFileToFetch));
+  CreateFetcher(
+      test_server.GetURL(std::string(kTestServerFilePrefix) + kFileToFetch));
 
   MessageLoop::current()->Run();  // OnURLFetchComplete() will Quit().
 
@@ -373,7 +358,8 @@ TEST_F(URLFetcherTempFileTest, LargeGet) {
   // URLFetcher::Core's IOBuffer.
   const char* kFileToFetch = "animate1.gif";
   expected_file_ = test_server.document_root().AppendASCII(kFileToFetch);
-  CreateFetcher(test_server.GetURL(kTestServerFilePrefix + kFileToFetch));
+  CreateFetcher(test_server.GetURL(
+      std::string(kTestServerFilePrefix) + kFileToFetch));
 
   MessageLoop::current()->Run();  // OnURLFetchComplete() will Quit().
 }
@@ -385,7 +371,8 @@ TEST_F(URLFetcherTempFileTest, CanTakeOwnershipOfFile) {
   // Get a small file.
   const char* kFileToFetch = "simple.html";
   expected_file_ = test_server.document_root().AppendASCII(kFileToFetch);
-  CreateFetcher(test_server.GetURL(kTestServerFilePrefix + kFileToFetch));
+  CreateFetcher(test_server.GetURL(
+      std::string(kTestServerFilePrefix) + kFileToFetch));
 
   MessageLoop::current()->Run();  // OnURLFetchComplete() will Quit().
 
@@ -888,6 +875,12 @@ TEST_F(URLFetcherMultipleAttemptTest, SameData) {
   MessageLoop::current()->Run();
 }
 
+void CancelAllOnIO() {
+  EXPECT_EQ(1, URLFetcherTest::GetNumFetcherCores());
+  URLFetcher::CancelAll();
+  EXPECT_EQ(0, URLFetcherTest::GetNumFetcherCores());
+}
+
 // Tests to make sure CancelAll() will successfully cancel existing URLFetchers.
 TEST_F(URLFetcherTest, CancelAll) {
   net::TestServer test_server(net::TestServer::TYPE_HTTP, FilePath(kDocRoot));
@@ -895,12 +888,12 @@ TEST_F(URLFetcherTest, CancelAll) {
   EXPECT_EQ(0, GetNumFetcherCores());
 
   CreateFetcher(test_server.GetURL("defaultresponse"));
-  io_message_loop_proxy()->PostTask(
+  io_message_loop_proxy()->PostTaskAndReply(
       FROM_HERE,
-      new CurriedTask(new MessageLoop::QuitTask(), MessageLoop::current()));
+      base::Bind(&CancelAllOnIO),
+      base::Bind(&MessageLoop::Quit,
+                 base::Unretained(MessageLoop::current())));
   MessageLoop::current()->Run();
-  EXPECT_EQ(1, GetNumFetcherCores());
-  URLFetcher::CancelAll();
   EXPECT_EQ(0, GetNumFetcherCores());
   delete fetcher_;
 }
