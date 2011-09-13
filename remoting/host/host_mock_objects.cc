112102a4be6cef0dfe12c937610b7b574a62f37b
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 18:17:30 +0000

    Refactor InputStub interface to pass events by value (const reference).
    This will make it much easier to inject modified key/mouse events, for example,
    restricting mouse-coordinates to the screen area.
    
    BUG=None
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/7803004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100939 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/host_mock_objects.cc
+++ remoting/host/host_mock_objects.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/host_mock_objects.h"
 
 #include "base/message_loop_proxy.h"
+#include "remoting/proto/event.pb.h"
 
 namespace remoting {
 
--- remoting/protocol/input_sender.cc
+++ remoting/protocol/input_sender.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // This stub is thread safe because of the use of BufferedSocketWriter.
-// BufferedSocketWriter buffers messages and send them on them right thread.
+// BufferedSocketWriter buffers messages and send them on the right thread.
 
 #include "remoting/protocol/input_sender.h"
 
@@ -28,22 +28,18 @@ InputSender::InputSender(base::MessageLoopProxy* message_loop,
 InputSender::~InputSender() {
 }
 
-void InputSender::InjectKeyEvent(const KeyEvent* event, Task* done) {
-  DCHECK(done);
-
+void InputSender::InjectKeyEvent(const KeyEvent& event) {
   EventMessage message;
   message.set_sequence_number(base::Time::Now().ToInternalValue());
-  message.mutable_key_event()->CopyFrom(*event);
-  buffered_writer_->Write(SerializeAndFrameMessage(message), done);
+  message.mutable_key_event()->CopyFrom(event);
+  buffered_writer_->Write(SerializeAndFrameMessage(message), NULL);
 }
 
-void InputSender::InjectMouseEvent(const MouseEvent* event, Task* done) {
-  DCHECK(done);
-
+void InputSender::InjectMouseEvent(const MouseEvent& event) {
   EventMessage message;
   message.set_sequence_number(base::Time::Now().ToInternalValue());
-  message.mutable_mouse_event()->CopyFrom(*event);
-  buffered_writer_->Write(SerializeAndFrameMessage(message), done);
+  message.mutable_mouse_event()->CopyFrom(event);
+  buffered_writer_->Write(SerializeAndFrameMessage(message), NULL);
 }
 
 void InputSender::Close() {
