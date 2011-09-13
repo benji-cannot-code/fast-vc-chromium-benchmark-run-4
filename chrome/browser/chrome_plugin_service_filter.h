45f7299785c4c8aeb4e7334963c78154630e1474
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 13:40:49 +0000

    Restrict plugins inside extensions to their profile.
    BUG=80757
    TEST=see bug
    
    
    Review URL: http://codereview.chromium.org/7839032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_plugin_service_filter.h
+++ chrome/browser/chrome_plugin_service_filter.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/plugins/webplugininfo.h"
 
 class PluginPrefs;
+class Profile;
 
 // This class must be created (by calling the |GetInstance| method) on the UI
 // thread, but is safe to use on any thread after that.
@@ -42,9 +43,14 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
                             const GURL& url,
                             const webkit::WebPluginInfo& plugin);
 
-  // Restricts the given plugin to the the scheme and host of the given url.
-  // Call with an empty url to reset this.
-  void RestrictPluginToUrl(const FilePath& plugin_path, const GURL& url);
+  // Restricts the given plugin to the given profile and origin of the given
+  // URL.
+  void RestrictPluginToProfileAndOrigin(const FilePath& plugin_path,
+                                        Profile* profile,
+                                        const GURL& url);
+
+  // Lifts a restriction on a plug-in.
+  void UnrestrictPlugin(const FilePath& plugin_path);
 
   // PluginServiceFilter implementation:
   virtual bool ShouldUsePlugin(
@@ -77,7 +83,8 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
 
   base::Lock lock_;  // Guards access to member variables.
   // Map of plugin paths to the origin they are restricted to.
-  typedef base::hash_map<FilePath, GURL> RestrictedPluginMap;
+  typedef std::pair<const void*, GURL> RestrictedPluginPair;
+  typedef base::hash_map<FilePath, RestrictedPluginPair> RestrictedPluginMap;
   RestrictedPluginMap restricted_plugins_;
   typedef std::map<const void*, scoped_refptr<PluginPrefs> > ResourceContextMap;
   ResourceContextMap resource_context_map_;
--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -1508,9 +1508,14 @@ void ExtensionService::NotifyExtensionLoaded(const Extension* extension) {
     webkit::npapi::PluginList::Singleton()->RefreshPlugins();
     webkit::npapi::PluginList::Singleton()->AddExtraPluginPath(plugin.path);
     plugins_changed = true;
-    if (!plugin.is_public) {
-      ChromePluginServiceFilter::GetInstance()->RestrictPluginToUrl(
-          plugin.path, extension->url());
+    ChromePluginServiceFilter* filter =
+        ChromePluginServiceFilter::GetInstance();
+    if (plugin.is_public) {
+      filter->RestrictPluginToProfileAndOrigin(
+          plugin.path, profile_, GURL());
+    } else {
+      filter->RestrictPluginToProfileAndOrigin(
+          plugin.path, profile_, extension->url());
     }
   }
 
@@ -1604,10 +1609,7 @@ void ExtensionService::NotifyExtensionUnloaded(
     webkit::npapi::PluginList::Singleton()->RemoveExtraPluginPath(
         plugin.path);
     plugins_changed = true;
-    if (!plugin.is_public) {
-      ChromePluginServiceFilter::GetInstance()->RestrictPluginToUrl(
-          plugin.path, GURL());
-    }
+    ChromePluginServiceFilter::GetInstance()->UnrestrictPlugin(plugin.path);
   }
 
   bool nacl_modules_changed = false;
