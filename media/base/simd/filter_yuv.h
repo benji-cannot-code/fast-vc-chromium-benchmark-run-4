6666cf15bb51cdc61d1dc77b55687b16dc4d55af
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 12:40:45 +0000

    Resubmit - Rewrite color space conversions suite using YASM"
    I'll watch the official buildbot this time.
    
    TBR=ajwong, dhollowa
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7891039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101067 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/base/simd/filter_yuv.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_SIMD_FILTER_YUV_H_
+#define MEDIA_BASE_SIMD_FILTER_YUV_H_
+
+#include "base/basictypes.h"
+
+namespace media {
+
+typedef void (*FilterYUVRowsProc)(uint8*,
+                                  const uint8*,
+                                  const uint8*,
+                                  int,
+                                  int);
+
+void FilterYUVRows_C(uint8* ybuf, const uint8* y0_ptr, const uint8* y1_ptr,
+                     int source_width, int source_y_fraction);
+
+void FilterYUVRows_MMX(uint8* ybuf, const uint8* y0_ptr, const uint8* y1_ptr,
+                       int source_width, int source_y_fraction);
+
+void FilterYUVRows_SSE2(uint8* ybuf, const uint8* y0_ptr, const uint8* y1_ptr,
+                        int source_width, int source_y_fraction);
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_SIMD_FILTER_YUV_H_
