b012f0a6af7d77cf121175f1cc11920141df31c0
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-14 19:00:50 +0000

    Implement globalFlag for WebKitTestRunner https://bugs.webkit.org/show_bug.cgi?id=68033 <rdar://problem/10119204>
    Reviewed by Sam Weinig.
    
    * WebKitTestRunner/InjectedBundle/Bindings/LayoutTestController.idl: Porting globalFlag from DRT
    * WebKitTestRunner/InjectedBundle/LayoutTestController.cpp:
    (WTR::LayoutTestController::LayoutTestController): initialize m_globalFlag to false
    * WebKitTestRunner/InjectedBundle/LayoutTestController.h: add m_globalFlag
    (WTR::LayoutTestController::globalFlag): simple getter
    (WTR::LayoutTestController::setGlobalFlag): simple setter
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.cpp
@@ -107,6 +107,7 @@ LayoutTestController::LayoutTestController()
     , m_willSendRequestReturnsNull(false)
     , m_policyDelegateEnabled(false)
     , m_policyDelegatePermissive(false)
+    , m_globalFlag(false)
 {
     platformInitialize();
 }
--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/LayoutTestController.h
@@ -170,6 +170,9 @@ public:
     void setTextDirection(JSStringRef);
 
     void setShouldStayOnPageAfterHandlingBeforeUnload(bool);
+    
+    bool globalFlag() const { return m_globalFlag; }
+    void setGlobalFlag(bool value) { m_globalFlag = value; }
 
 private:
     static const double waitToDumpWatchdogTimerInterval;
@@ -199,6 +202,8 @@ private:
 
     bool m_policyDelegateEnabled;
     bool m_policyDelegatePermissive;
+    
+    bool m_globalFlag;
 
     PlatformTimerRef m_waitToDumpWatchdogTimer;
 };
