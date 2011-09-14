c39b729e9d50d5cede0f9ca98c99a8c0e0c4a724
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-14 01:23:14 +0000

    FailureGrid in garden-o-matic should link to the builder's waterfall page https://bugs.webkit.org/show_bug.cgi?id=68036
    Reviewed by Dimitri Glazkov.
    
    One of the users in the user study thought the configurations names in
    the failure grid would link to the underlying bots.  That makes sense,
    so this patch adds the behavior.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/failures.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/failures_unittests.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications_unittests.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/styles/notifications.css:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/failures.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/failures.js
@@ -31,7 +31,7 @@ ui.failures = ui.failures || {};
 
 var kBuildingResult = 'BUILDING';
 
-ui.failures.Configuration = base.extends('div', {
+ui.failures.Configuration = base.extends('a', {
     init: function(configuration)
     {
         if (configuration.is64bit)
@@ -39,6 +39,7 @@ ui.failures.Configuration = base.extends('div', {
         if (configuration.version)
             this._addSpan('version', configuration.version);
         this._configuration = configuration;
+        this.target = '_blank';
     },
     _addSpan: function(className, text)
     {
@@ -108,7 +109,7 @@ ui.failures.FailureGrid = base.extends('table', {
             var cell = cellByBuildType(row, configuration);
             if (cellContainsConfiguration(cell, configuration))
                 return;
-            cell.appendChild(new ui.failures.Configuration(configuration));
+            cell.appendChild(new ui.failures.Configuration(configuration)).href = ui.displayURLForBuilder(builderName);
         }, this);
     }
 });
