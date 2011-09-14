4300a51cc42fb4685c8d35d1afbeb7c308bb5364
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 21:32:16 +0000

    Fix Checkdeps.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101160 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window_unittest.cc
+++ ui/aura/window_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/keycodes/keyboard_codes.h"
 
 #if !defined(OS_WIN)
-#include "aura/hit_test.h"
+#include "ui/aura/hit_test.h"
 #endif
 
 namespace aura {
