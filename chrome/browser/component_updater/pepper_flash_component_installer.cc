2217862d91718aea8e90c5828c3bb93163837e91
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 00:17:56 +0000

    Flapper installer: add a specific check for the PDF interface
    This is required for flapper 11.0.x.y pushes.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/7887020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/pepper_flash_component_installer.cc
+++ chrome/browser/component_updater/pepper_flash_component_installer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/component_updater/flash_component_installer.h"
 
+#include <string.h>
+
 #include "base/base_paths.h"
 #include "base/compiler_specific.h"
 #include "base/file_path.h"
@@ -18,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "content/browser/browser_thread.h"
 #include "content/common/pepper_plugin_registry.h"
+#include "ppapi/c/private/ppb_pdf.h"
 #include "webkit/plugins/npapi/plugin_list.h"
 #include "webkit/plugins/plugin_constants.h"
 #include "webkit/plugins/ppapi/plugin_module.h"
@@ -93,7 +96,11 @@ bool GetLatestPepperFlashDirectory(FilePath* result, Version* latest) {
 bool SupportsPepperInterface(const char* interface_name) {
   static webkit::ppapi::PluginModule::GetInterfaceFunc get_itf =
       webkit::ppapi::PluginModule::GetLocalGetInterfaceFunc();
-  return get_itf(interface_name) != NULL;
+  if (get_itf(interface_name))
+    return true;
+  // It might be that flapper is using as a temporary hack the PDF interface
+  // so we need to check for that as well. TODO(cpu): make this more sane.
+  return (strcmp(interface_name, PPB_PDF_INTERFACE) == 0);
 }
 
 }  // namespace
