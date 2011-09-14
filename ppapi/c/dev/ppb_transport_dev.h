b6ba4b8b1ef1738120ec37e85fe38429f8936d66
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 23:37:53 +0000

    Use enum instead of string to specify transport type.
    TEST=None
    BUG=41776
    
    Review URL: http://codereview.chromium.org/7891014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101189 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_transport_dev.h
+++ ppapi/c/dev/ppb_transport_dev.h
@@ -14,8 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/c/pp_var.h"
 
-#define PPB_TRANSPORT_DEV_INTERFACE_0_6 "PPB_Transport;0.6"
-#define PPB_TRANSPORT_DEV_INTERFACE PPB_TRANSPORT_DEV_INTERFACE_0_6
+#define PPB_TRANSPORT_DEV_INTERFACE_0_7 "PPB_Transport;0.7"
+#define PPB_TRANSPORT_DEV_INTERFACE PPB_TRANSPORT_DEV_INTERFACE_0_7
+
+typedef enum {
+  PP_TRANSPORTTYPE_DATAGRAM = 0,
+  PP_TRANSPORTTYPE_STREAM = 1
+} PP_TransportType;
 
 typedef enum {
   // STUN server address and port, e.g "stun.example.com:19302".
@@ -51,7 +56,7 @@ struct PPB_Transport_Dev {
   // specified protocol.
   PP_Resource (*CreateTransport)(PP_Instance instance,
                                  const char* name,
-                                 const char* proto);
+                                 PP_TransportType type);
 
   // Returns PP_TRUE if resource is a Transport, PP_FALSE otherwise.
   PP_Bool (*IsTransport)(PP_Resource resource);
--- ppapi/tests/test_transport.h
+++ ppapi/tests/test_transport.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/scoped_ptr.h"
+#include "ppapi/c/dev/ppb_transport_dev.h"
 #include "ppapi/tests/test_case.h"
 
 struct PPB_Transport_Dev;
@@ -26,7 +27,7 @@ class TestTransport : public TestCase {
   virtual void RunTest();
 
  private:
-  std::string InitTargets(const char* proto);
+  std::string InitTargets(PP_TransportType type);
   std::string Connect();
   std::string Clean();
 
