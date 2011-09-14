be80409b884bf339bc7519c2771c6a43ec7ca871
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 21:35:10 +0000

    Fix memory leaks from MessagePumpLibevent::Run() for Chrome OS.
    valgrind detects leaks from MessagePumpLibevent::Run() when we fail to
    launch programs because of their non-existence (valgrind doesn't seem
    to handle cases like "fork -> exec failed -> _exit). For instance, tpcontrol
    and crossystem don't exist on Linux desktop.
    
    This patch to check existence of these tools before executing them.
    This is a good thing to do anyway, as emitting error like
    "LaunchApp: failed to execvp: /opt/google/touchpad/tpcontrol"
    on Linux desktop is noisy and confusing.
    
    BUG=46163
    TEST=run AutomationProxyTest.GetBrowserWindowCount under valgrind locally and confirm that the leak is now gone.
    
    Review URL: http://codereview.chromium.org/7885017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/touchpad_settings.cc
+++ chrome/browser/chromeos/system/touchpad_settings.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/file_path.h"
+#include "base/file_util.h"
 #include "base/message_loop.h"
 #include "base/process_util.h"
 #include "base/stringprintf.h"
@@ -19,6 +21,18 @@ namespace {
 const char* kTpControl = "/opt/google/touchpad/tpcontrol";
 }  // namespace
 
+// Launches the tpcontrol command asynchronously, if it exists.
+void LaunchTpControl(const std::vector<std::string>& argv) {
+  if (!file_util::PathExists(FilePath(argv[0]))) {
+    LOG(WARNING) << argv[0] << " not found. Maybe running on Linux desktop?";
+    return;
+  }
+
+  base::LaunchOptions options;
+  options.wait = false;  // Launch asynchronously.
+
+  base::LaunchProcess(CommandLine(argv), options, NULL);
+}
 
 void SetSensitivity(int value) {
   // Run this on the FILE thread.
@@ -34,10 +48,7 @@ void SetSensitivity(int value) {
   argv.push_back("sensitivity");
   argv.push_back(StringPrintf("%d", value));
 
-  base::LaunchOptions options;
-  options.wait = false;  // Launch asynchronously.
-
-  base::LaunchProcess(CommandLine(argv), options, NULL);
+  LaunchTpControl(argv);
 }
 
 void SetTapToClick(bool enabled) {
@@ -54,10 +65,7 @@ void SetTapToClick(bool enabled) {
   argv.push_back("taptoclick");
   argv.push_back(enabled ? "on" : "off");
 
-  base::LaunchOptions options;
-  options.wait = false;  // Launch asynchronously.
-
-  base::LaunchProcess(CommandLine(argv), options, NULL);
+  LaunchTpControl(argv);
 }
 
 }  // namespace touchpad_settings
