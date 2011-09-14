ee992426c3ed9b87902110364279fedb312243aa
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 22:49:09 +0000

    Reland r100733 - Cancel plugin channel requests when the renderer goes away.
    Logic:
    - Profile shutdown kills all renderer processes
    - When the RMF detects the channel closing, it kills off any plugin channel requests from:
    * PluginService (which needs to check the PluginServiceFilter on the FILE thread)
    * PluginProcessHost (which may need to wait for the channel to get established)
    - RMF uses the new OnPluginProcessHostFound() to cancel at the PluginService or PluginProcessHost
    
    Modified in Reland:
    * PluginProcessHost may be blocked on the browser<=>plugin channel to be established, OR on the plugin<=>renderer channel to be established.
    * Already covered the former case in the previous changelist (pending_requests_) but not the latter case (sent_requests_).
    * Cover both cases and add tests.
    
    BUG=94704
    TEST=New PluginService tests
    
    Review URL: http://codereview.chromium.org/7887027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101181 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_process_host.h
+++ content/browser/plugin_process_host.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#include <queue>
+#include <list>
 #include <set>
 #include <string>
 #include <vector>
@@ -53,6 +53,8 @@ class PluginProcessHost : public BrowserChildProcessHost {
     virtual const content::ResourceContext& GetResourceContext() = 0;
     virtual bool OffTheRecord() = 0;
     virtual void SetPluginInfo(const webkit::WebPluginInfo& info) = 0;
+    virtual void OnFoundPluginProcessHost(PluginProcessHost* host) = 0;
+    virtual void OnSentPluginChannelRequest() = 0;
     // The client should delete itself when one of these methods is called.
     virtual void OnChannelOpened(const IPC::ChannelHandle& handle) = 0;
     virtual void OnError() = 0;
@@ -83,6 +85,12 @@ class PluginProcessHost : public BrowserChildProcessHost {
   // Cancels all pending channel requests for the given resource context.
   static void CancelPendingRequestsForResourceContext(
       const content::ResourceContext* context);
+
+  // This function is called to cancel pending requests to open new channels.
+  void CancelPendingRequest(Client* client);
+
+  // This function is called to cancel sent requests to open new channels.
+  void CancelSentRequest(Client* client);
 
   // This function is called on the IO thread once we receive a reply from the
   // modal HTML dialog (in the form of a JSON string). This function forwards
@@ -139,7 +147,7 @@ class PluginProcessHost : public BrowserChildProcessHost {
 
   // These are the channel requests that we have already sent to
   // the plugin process, but haven't heard back about yet.
-  std::queue<Client*> sent_requests_;
+  std::list<Client*> sent_requests_;
 
   // Information about the plugin.
   webkit::WebPluginInfo info_;
