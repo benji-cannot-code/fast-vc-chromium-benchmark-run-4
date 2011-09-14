895a4896f7ecf335e38113bbffe9ba0f0390dbb0
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-14 19:10:04 +0000

    [Qt] Win32 builds with threads turned off https://bugs.webkit.org/show_bug.cgi?id=67864
    Reviewed by Geoffrey Garen.
    
    * JavaScriptCore.pri: Link pthread library on Windows platform.
    * wtf/Platform.h: Enable multiple threads.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -510,20 +510,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_PTHREAD_BASED_QT 1
 #endif
 
-/* FIXME: JSC multithreading currently only supports pthread.
-   Qt-Win cannot do multithreading because it is not pthread-based. */
 #if !defined(ENABLE_JSC_MULTIPLE_THREADS)
-#if !PLATFORM(QT) || USE(PTHREAD_BASED_QT)
 #define ENABLE_JSC_MULTIPLE_THREADS 1
 #endif
-#endif
 
-/* FIXME: JSC multithreading currently only supports pthread.
-   Qt-Win cannot do multithreading because it is not pthread-based. */
 #if !defined(ENABLE_WTF_MULTIPLE_THREADS)
-#if !PLATFORM(QT) || USE(PTHREAD_BASED_QT)
 #define ENABLE_WTF_MULTIPLE_THREADS 1
-#endif
 #endif
 
 /* On Windows, use QueryPerformanceCounter by default */
