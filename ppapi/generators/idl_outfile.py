58476ca852ce863684090fbb7e5cbf7f1f95ba59
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 16:55:09 +0000

    Add presubmit check to verify PPAPI C headers match IDL files.
    Update outfile to print a better filename on diff.
    Update fix missing 'name' in idl_generator.py
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=93534
    TEST= gcl upload | presubmit (after modifying a .h or .idl file)
    R= brettw@chromium.org
    Review URL: http://codereview.chromium.org/7700001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101092 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_outfile.py
+++ ppapi/generators/idl_outfile.py
@@ -93,7 +93,9 @@ class IDLOutFile(object):
 
     if GetOption('diff'):
       for line in difflib.unified_diff(intext.split('\n'), outtext.split('\n'),
-                                     self.filename, 'NEW', n=1, lineterm=''):
+                                       'OLD ' + self.filename,
+                                       'NEW ' + self.filename,
+                                       n=1, lineterm=''):
         ErrOut.Log(line)
 
     try:
