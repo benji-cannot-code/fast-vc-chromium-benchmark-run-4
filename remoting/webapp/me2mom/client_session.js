145449c6ce5daa1cd6cd5086ccbc2f9d2450b35e
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 23:07:01 +0000

    Persist remoting.useP2pApi
    BUG=None
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/7887054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101186 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/client_session.js
+++ remoting/webapp/me2mom/client_session.js
@@ -17,12 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @suppress {duplicate} */
 var remoting = remoting || {};
 
-/**
- * Whether or not the P2P Transport API should be used.
- * @type {boolean}
- */
-remoting.useP2pApi = false;
-
 (function() {
 /**
  * @param {string} hostJid The jid of the host to connect to.
