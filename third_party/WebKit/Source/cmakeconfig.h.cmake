e4380f449c5b5556bb37bfbb851f9c7c5ba8b667
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-14 18:49:16 +0000

    Remove ENABLE_SVG_USE as <use> is required by HTML5 https://bugs.webkit.org/show_bug.cgi?id=68019
    Reviewed by Ryosuke Niwa.
    
    .:
    
    * Source/cmake/OptionsEfl.cmake:
    * Source/cmake/OptionsWinCE.cmake:
    * Source/cmakeconfig.h.cmake:
    * configure.ac:
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore:
    
    * Configurations/FeatureDefines.xcconfig:
    * DerivedSources.make:
    * GNUmakefile.am:
    * features.pri:
    
    Source/WebKit/chromium:
    
    * features.gypi:
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Tools:
    
    * Scripts/build-webkit:
    * waf/build/settings.py:
    
    WebKitLibraries:
    
    * win/tools/vsprops/FeatureDefines.vsprops:
    * win/tools/vsprops/FeatureDefinesCairo.vsprops:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmakeconfig.h.cmake
+++ third_party/WebKit/Source/cmakeconfig.h.cmake
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #cmakedefine01 ENABLE_SVG
 #cmakedefine01 ENABLE_SVG_ANIMATION
 #cmakedefine01 ENABLE_SVG_FONTS
-#cmakedefine01 ENABLE_SVG_USE
 #cmakedefine01 ENABLE_TOUCH_EVENTS
 #cmakedefine01 ENABLE_TOUCH_ICON_LOADING
 #cmakedefine01 ENABLE_VIDEO
