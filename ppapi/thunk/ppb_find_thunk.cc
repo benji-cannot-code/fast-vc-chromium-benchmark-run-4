c74cfc5948496dbca16100619530a5c60f3440ac
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 22:44:19 +0000

    Convert dev interfaces to use the macro system.
    This converts the non-"weird" dev interfaces to using the macro system. The
    trusted and private ones are still remaining.
    
    This moves the find implementation to the instance API which saves some code
    and routing.
    
    Review URL: http://codereview.chromium.org/7887021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101177 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/thunk/ppb_find_thunk.cc
+++ ppapi/thunk/ppb_find_thunk.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/dev/ppb_find_dev.h"
 #include "ppapi/thunk/thunk.h"
 #include "ppapi/thunk/enter.h"
-#include "ppapi/thunk/ppb_find_api.h"
-#include "ppapi/thunk/resource_creation_api.h"
 
 namespace ppapi {
 namespace thunk {
@@ -17,13 +15,13 @@ namespace {
 void NumberOfFindResultsChanged(PP_Instance instance,
                                 int32_t total,
                                 PP_Bool final) {
-  EnterFunction<PPB_Find_FunctionAPI> enter(instance, true);
+  EnterInstance enter(instance);
   if (enter.succeeded())
     enter.functions()->NumberOfFindResultsChanged(instance, total, final);
 }
 
 void SelectedFindResultChanged(PP_Instance instance, int32_t index) {
-  EnterFunction<PPB_Find_FunctionAPI> enter(instance, true);
+  EnterInstance enter(instance);
   if (enter.succeeded())
     enter.functions()->SelectedFindResultChanged(instance, index);
 }
@@ -35,7 +33,7 @@ const PPB_Find_Dev g_ppb_find_thunk = {
 
 }  // namespace
 
-const PPB_Find_Dev* GetPPB_Find_Thunk() {
+const PPB_Find_Dev* GetPPB_Find_Dev_Thunk() {
   return &g_ppb_find_thunk;
 }
 
