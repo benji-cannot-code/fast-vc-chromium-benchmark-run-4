215e1693c573fe519936767ed8772db0e2a678a0
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 21:38:09 +0000

    Options: Use an edit button for Autofill profile list.
    The previous solution was to activate the editor when the user double-clicked
    a profile list item.
    
    BUG=70294
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/7886030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/autofill_options_list.js
+++ chrome/browser/resources/options/autofill_options_list.js
@@ -9,6 +9,21 @@ cr.define('options.autofillOptions', function() {
   const InlineEditableItem = options.InlineEditableItem;
   const InlineEditableItemList = options.InlineEditableItemList;
 
+  function AutofillEditProfileButton(guid, edit) {
+    var editButtonEl = document.createElement('button');
+    editButtonEl.className = 'raw-button';
+    editButtonEl.textContent =
+        templateData.autofillEditProfileButton;
+    editButtonEl.onclick = function(e) { edit(guid); };
+
+    // Don't select the row when clicking the button.
+    editButtonEl.onmousedown = function(e) {
+      e.stopPropagation();
+    };
+
+    return editButtonEl;
+  }
+
   /**
    * Creates a new address list item.
    * @param {Array} entry An array of the form [guid, label].
@@ -37,6 +52,12 @@ cr.define('options.autofillOptions', function() {
       label.className = 'autofill-list-item';
       label.textContent = this.label;
       this.contentElement.appendChild(label);
+
+      // The 'Edit' button.
+      var editButtonEl = new AutofillEditProfileButton(
+        this.guid,
+        AutofillOptions.loadAddressEditor);
+      this.contentElement.appendChild(editButtonEl);
     },
   };
 
@@ -76,6 +97,12 @@ cr.define('options.autofillOptions', function() {
       icon.src = this.icon;
       icon.alt = this.description;
       this.contentElement.appendChild(icon);
+
+      // The 'Edit' button.
+      var editButtonEl = new AutofillEditProfileButton(
+        this.guid,
+        AutofillOptions.loadCreditCardEditor);
+      this.contentElement.appendChild(editButtonEl);
     },
   };
 
@@ -215,11 +242,6 @@ cr.define('options.autofillOptions', function() {
     },
 
     /** @inheritDoc */
-    activateItemAtIndex: function(index) {
-      AutofillOptions.loadAddressEditor(this.dataModel.item(index)[0]);
-    },
-
-    /** @inheritDoc */
     deleteItemAtIndex: function(index) {
       AutofillOptions.removeAddress(this.dataModel.item(index)[0]);
     },
@@ -252,11 +274,6 @@ cr.define('options.autofillOptions', function() {
     /** @inheritDoc */
     createItem: function(entry) {
       return new CreditCardListItem(entry);
-    },
-
-    /** @inheritDoc */
-    activateItemAtIndex: function(index) {
-      AutofillOptions.loadCreditCardEditor(this.dataModel.item(index)[0]);
     },
 
     /** @inheritDoc */
