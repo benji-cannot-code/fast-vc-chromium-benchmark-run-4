222f270a9d207f2dcd2d4aa05232b1078c03d95a
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-14 19:28:32 +0000

    Check for a crashed plugin before creating resources to send to the plugin
    BUG=95710
    Review URL: http://codereview.chromium.org/7839040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101119 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppapi_webplugin_impl.cc
+++ webkit/plugins/ppapi/ppapi_webplugin_impl.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 #include "ppapi/c/pp_var.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebBindings.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebDocument.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebElement.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginContainer.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginParams.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPoint.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebRect.h"
@@ -154,6 +158,12 @@ bool WebPluginImpl::handleInputEvent(const WebKit::WebInputEvent& event,
 void WebPluginImpl::didReceiveResponse(
     const WebKit::WebURLResponse& response) {
   DCHECK(!document_loader_);
+
+  if (instance_->module()->is_crashed()) {
+    // Don't create a resource for a crashed plugin.
+    instance_->container()->element().document().frame()->stopLoading();
+    return;
+  }
 
   document_loader_ = new PPB_URLLoader_Impl(instance_->pp_instance(), true);
   document_loader_->didReceiveResponse(NULL, response);
