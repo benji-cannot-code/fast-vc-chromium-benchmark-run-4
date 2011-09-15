146dd72f2daebfb13f10687e41d3477bd8c97936
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-15 02:26:42 +0000

    MediaStream API: Change PeerConnection constructor name to webkitPeerConnection https://bugs.webkit.org/show_bug.cgi?id=67843
    Patch by Henrik Grunell <grunell@google.com> on 2011-09-14
    Reviewed by Tony Gentilcore.
    
    Tests for the MediaStream API will be provided by the bug 56587, pending enough landed code.
    
    Source/WebCore:
    
    * bindings/generic/RuntimeEnabledFeatures.h:
    (WebCore::RuntimeEnabledFeatures::webkitPeerConnectionEnabled):
    * page/DOMWindow.idl:
    
    LayoutTests:
    
    * fast/dom/call-a-constructor-as-a-function.html:
    * platform/chromium/fast/dom/call-a-constructor-as-a-function-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
@@ -160,7 +160,7 @@ public:
     static bool mediaStreamEnabled() { return isMediaStreamEnabled; }
     static void setMediaStreamEnabled(bool isEnabled) { isMediaStreamEnabled = isEnabled; }
     static bool webkitGetUserMediaEnabled() { return isMediaStreamEnabled; }
-    static bool peerConnectionEnabled() { return isMediaStreamEnabled; }
+    static bool webkitPeerConnectionEnabled() { return isMediaStreamEnabled; }
 #endif
 
 #if ENABLE(QUOTA)
