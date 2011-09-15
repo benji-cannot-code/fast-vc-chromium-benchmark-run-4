44c09333db4038b6e0336a51aafb2a4bb435b193
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 01:59:06 +0000

    Reland 101111 - Only deliver extension messages to contexts that care.
    BUG=96544,76571
    
    Review URL: http://codereview.chromium.org/7888064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/renderer_extension_bindings.h
+++ chrome/renderer/extensions/renderer_extension_bindings.h
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_EXTENSIONS_RENDERER_EXTENSION_BINDINGS_H_
 #pragma once
 
+#include <string>
+
 class ExtensionDispatcher;
+class RenderView;
 
 namespace v8 {
 class Extension;
@@ -22,6 +25,12 @@ class RendererExtensionBindings {
 
   // Creates an instance of the extension.
   static v8::Extension* Get(ExtensionDispatcher* dispatcher);
+
+  // Delivers a message sent using content script messaging. If
+  // restrict_to_render_view is specified, only contexts in that render view
+  // will receive the message.
+  static void DeliverMessage(int target_port_id, const std::string& message,
+                             RenderView* restrict_to_render_view);
 };
 
 #endif  // CHROME_RENDERER_EXTENSIONS_RENDERER_EXTENSION_BINDINGS_H_
