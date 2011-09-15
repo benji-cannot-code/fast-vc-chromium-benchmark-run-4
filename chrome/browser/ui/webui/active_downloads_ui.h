1311ad44e6b5c9536af21e4c8c559f7720d8cec2
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 04:44:22 +0000

    touchui: Show the active downloads page in a tab in an existing window.
    Panels aren't touch friendly yet. So use a tab in an existing window until we
    have touch friendly panels.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7885045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/active_downloads_ui.h
+++ chrome/browser/ui/webui/active_downloads_ui.h
@@ -15,17 +15,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/html_dialog_ui.h"
 #include "net/base/directory_lister.h"
 
-class Browser;
-class Profile;
-class DownloadItem;
 class ActiveDownloadsHandler;
+class Browser;
+class DownloadItem;
+class Profile;
 
 class ActiveDownloadsUI : public HtmlDialogUI {
  public:
   explicit ActiveDownloadsUI(TabContents* contents);
 
+#if defined(TOUCH_UI)
+  static TabContents* OpenPopup(Profile* profile);
+  static TabContents* GetPopup(Browser** browser);
+#else
   static Browser* OpenPopup(Profile* profile);
   static Browser* GetPopup();
+#endif
 
   // For testing.
   typedef std::vector<DownloadItem*> DownloadList;
