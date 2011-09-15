1dcac82f6afb3e34c2168803b0f4736e85cfea0b
rtoy@google.com <rtoy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 05:29:18 +0000

    Add API to query for the audio buffer size.
    This is essentially identical to the GetAudioHardwareSampleRate API, except
    we get the "optimal" hardware buffer size to be used.
    
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/7837030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101249 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -568,6 +568,10 @@ double RendererWebKitPlatformSupportImpl::audioHardwareSampleRate() {
   return AudioDevice::GetAudioHardwareSampleRate();
 }
 
+size_t RendererWebKitPlatformSupportImpl::audioHardwareBufferSize() {
+  return AudioDevice::GetAudioHardwareBufferSize();
+}
+
 WebAudioDevice*
 RendererWebKitPlatformSupportImpl::createAudioDevice(
     size_t buffer_size,
