c999f754c8571e620a753b9889f331fc155d1ee5
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 06:39:55 +0000

    Simplify GCP integration with print preview. Removes GCP printers from the printer dropdown on platforms other than Chrome OS. Adds an option to the printer dropdown to close print preview and print via the cloud print dialog.
    BUG=
    TEST=Cloud print printers should only appear in print preview in chrome os.
    If cloud print is enabled, a new option should appear in the print preview dropdown that closes print preview and brings up the cloud print dialog.
    
    
    Review URL: http://codereview.chromium.org/7830013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101256 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud.cc
+++ chrome/browser/printing/print_dialog_cloud.cc
@@ -443,8 +443,10 @@ CloudPrintHtmlDialogDelegate::CloudPrintHtmlDialogDelegate(
     const string16& print_job_title,
     const string16& print_ticket,
     const std::string& file_type,
-    bool modal)
-    : flow_handler_(new CloudPrintFlowHandler(path_to_file,
+    bool modal,
+    bool delete_on_close)
+    : delete_on_close_(delete_on_close),
+      flow_handler_(new CloudPrintFlowHandler(path_to_file,
                                               print_job_title,
                                               print_ticket,
                                               file_type)),
@@ -459,8 +461,10 @@ CloudPrintHtmlDialogDelegate::CloudPrintHtmlDialogDelegate(
     CloudPrintFlowHandler* flow_handler,
     int width, int height,
     const std::string& json_arguments,
-    bool modal)
-    : flow_handler_(flow_handler),
+    bool modal,
+    bool delete_on_close)
+    : delete_on_close_(delete_on_close),
+      flow_handler_(flow_handler),
       modal_(modal),
       owns_flow_handler_(true) {
   Init(width, height, json_arguments);
@@ -527,8 +531,7 @@ void CloudPrintHtmlDialogDelegate::OnDialogClosed(
   // Get the final dialog size and store it.
   flow_handler_->StoreDialogClientSize();
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kCloudPrintDeleteFile)) {
+  if (delete_on_close_) {
     BrowserThread::PostTask(BrowserThread::FILE, FROM_HERE,
         NewRunnableFunction(&internal_cloud_print_helpers::Delete,
                             path_to_file_));
@@ -556,12 +559,36 @@ bool CloudPrintHtmlDialogDelegate::HandleContextMenu(
   return true;
 }
 
+void CreatePrintDialogForBytesImpl(scoped_refptr<RefCountedBytes> data,
+                                   const string16& print_job_title,
+                                   const string16& print_ticket,
+                                   const std::string& file_type,
+                                   bool modal) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
+  // TODO(abodenha@chromium.org) Writing the PDF to a file before printing
+  // is wasteful.  Modify the dialog flow to pull PDF data from memory.
+  // See http://code.google.com/p/chromium/issues/detail?id=44093
+  FilePath path;
+  if (file_util::CreateTemporaryFile(&path)) {
+    file_util::WriteFile(path,
+                         reinterpret_cast<const char*>(data->front()),
+                         data->size());
+  }
+  print_dialog_cloud::CreatePrintDialogForFile(path,
+                                               print_job_title,
+                                               print_ticket,
+                                               file_type,
+                                               modal,
+                                               true);
+}
+
 // Called from the UI thread, starts up the dialog.
 void CreateDialogImpl(const FilePath& path_to_file,
                       const string16& print_job_title,
                       const string16& print_ticket,
                       const std::string& file_type,
-                      bool modal) {
+                      bool modal,
+                      bool delete_on_close) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   Browser* browser = BrowserList::GetLastActive();
 
@@ -600,7 +627,7 @@ void CreateDialogImpl(const FilePath& path_to_file,
   HtmlDialogUIDelegate* dialog_delegate =
       new internal_cloud_print_helpers::CloudPrintHtmlDialogDelegate(
           path_to_file, width, height, std::string(), job_title, print_ticket,
-          file_type, modal);
+          file_type, modal, delete_on_close);
   if (modal) {
     DCHECK(browser);
     browser->BrowserShowHtmlDialog(dialog_delegate, NULL);
@@ -628,7 +655,8 @@ void CreatePrintDialogForFile(const FilePath& path_to_file,
                               const string16& print_job_title,
                               const string16& print_ticket,
                               const std::string& file_type,
-                              bool modal) {
+                              bool modal,
+                              bool delete_on_close) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE) ||
          BrowserThread::CurrentlyOn(BrowserThread::UI));
 
@@ -639,7 +667,28 @@ void CreatePrintDialogForFile(const FilePath& path_to_file,
                           print_job_title,
                           print_ticket,
                           file_type,
-                          modal));
+                          modal,
+                          delete_on_close));
+}
+
+void CreatePrintDialogForBytes(scoped_refptr<RefCountedBytes> data,
+                               const string16& print_job_title,
+                               const string16& print_ticket,
+                               const std::string& file_type,
+                               bool modal) {
+  // TODO(abodenha@chromium.org) Avoid cloning the PDF data.  Make use of a
+  // shared memory object instead.
+  // http://code.google.com/p/chromium/issues/detail?id=44093
+  scoped_refptr<RefCountedBytes> cloned_data(new RefCountedBytes(data->data()));
+  BrowserThread::PostTask(
+      BrowserThread::FILE, FROM_HERE,
+      NewRunnableFunction(
+          &internal_cloud_print_helpers::CreatePrintDialogForBytesImpl,
+          cloned_data,
+          print_job_title,
+          print_ticket,
+          file_type,
+          modal));
 }
 
 bool CreatePrintDialogFromCommandLine(const CommandLine& command_line) {
@@ -666,11 +715,15 @@ bool CreatePrintDialogFromCommandLine(const CommandLine& command_line) {
             switches::kCloudPrintFileType);
       }
 
+      bool delete_on_close = CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kCloudPrintDeleteFile);
+
       print_dialog_cloud::CreatePrintDialogForFile(cloud_print_file,
                                                    print_job_title,
                                                    print_job_print_ticket,
                                                    file_type,
-                                                   false);
+                                                   false,
+                                                   delete_on_close);
       return true;
     }
   }
