7aa3e3feb56d5c0c01ed200bbe85d6d160aabb39
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 05:54:14 +0000

    [Sync] Fix encrypting of sync titles.
    We should always ensure we overwrite the NON_UNIQUE_NAME of sync entries
    when encryption is turned on. It's only used for debugging (with a special
    case for bookmarks), and can therefore safely be discarded. This fixes an
    issue wherein we accidentally could resurrect old unencrypted non_unique_names.
    
    BUG=96314
    TEST=Add some extensions/apps/themes to an account with encrypt everything.
    Restart sync. Verify all entries in about:sync's sync node browser list
    as "encrypted".
    
    
    Review URL: http://codereview.chromium.org/7878005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/syncer_unittest.cc
+++ chrome/browser/sync/engine/syncer_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/engine/model_safe_worker.h"
 #include "chrome/browser/sync/engine/net/server_connection_manager.h"
 #include "chrome/browser/sync/engine/process_updates_command.h"
+#include "chrome/browser/sync/engine/nigori_util.h"
 #include "chrome/browser/sync/engine/syncer.h"
 #include "chrome/browser/sync/engine/syncer_proto_util.h"
 #include "chrome/browser/sync/engine/syncer_util.h"
@@ -58,6 +59,7 @@ using syncable::Entry;
 using syncable::GetFirstEntryWithName;
 using syncable::GetOnlyEntryWithName;
 using syncable::Id;
+using syncable::kEncryptedString;
 using syncable::MutableEntry;
 using syncable::ReadTransaction;
 using syncable::ScopedDirLookup;
@@ -567,9 +569,11 @@ TEST_F(SyncerTest, GetCommitIdsFilterEntriesNeedingEncryption) {
     MutableEntry entry_e(&wtrans, GET_BY_HANDLE, handle_e);
     MutableEntry entry_f(&wtrans, GET_BY_HANDLE, handle_f);
     entry_x.Put(SPECIFICS, encrypted_bookmark);
+    entry_x.Put(NON_UNIQUE_NAME, kEncryptedString);
     entry_b.Put(SPECIFICS, DefaultBookmarkSpecifics());
     entry_c.Put(SPECIFICS, DefaultBookmarkSpecifics());
     entry_e.Put(SPECIFICS, encrypted_bookmark);
+    entry_e.Put(NON_UNIQUE_NAME, kEncryptedString);
     entry_f.Put(SPECIFICS, DefaultPreferencesSpecifics());
   }
 
