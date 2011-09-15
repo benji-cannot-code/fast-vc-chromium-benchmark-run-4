d4fd81b8f6499b63bc17c7aa7d5547918c6baa00
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 05:08:25 +0000

    Clamp injected mouse events to within the screen area.
    This is the quick'n'dirty version.  Refactoring stuff out of ClientSession will
    come in later CLs.
    
    BUG=91613
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/7886021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101244 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/client_session.h
+++ remoting/host/client_session.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
+class Capturer;
 class UserAuthenticator;
 
 // A ClientSession keeps a reference to a connection to a client, and maintains
@@ -41,11 +42,12 @@ class ClientSession : public protocol::HostStub,
   };
 
   // Takes ownership of |user_authenticator|. Does not take ownership of
-  // |event_handler| or |input_stub|.
+  // |event_handler|, |input_stub| or |capturer|.
   ClientSession(EventHandler* event_handler,
                 UserAuthenticator* user_authenticator,
                 scoped_refptr<protocol::ConnectionToClient> connection,
-                protocol::InputStub* input_stub);
+                protocol::InputStub* input_stub,
+                Capturer* capturer);
 
   // protocol::HostStub interface.
   virtual void BeginSessionRequest(
@@ -109,6 +111,12 @@ class ClientSession : public protocol::HostStub,
 
   // The input stub to which this object delegates.
   protocol::InputStub* input_stub_;
+
+  // Capturer, used to determine current screen size for ensuring injected
+  // mouse events fall within the screen area.
+  // TODO(lambroslambrou): Move floor-control logic, and clamping to screen
+  // area, out of this class (crbug.com/96508).
+  Capturer* capturer_;
 
   // Whether this client is authenticated.
   bool authenticated_;
