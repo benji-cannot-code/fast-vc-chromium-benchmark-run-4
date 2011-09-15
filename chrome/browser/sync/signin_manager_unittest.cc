94281399950244cfa9d1b98c1442d848335b591a
rickcam@chromium.org <rickcam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 18:38:59 +0000

    Ensure that SigninManager tests restores IsUsing OAuth state
    BUG=96681
    TEST=unit_tests
    
    
    Review URL: http://codereview.chromium.org/7909001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/signin_manager_unittest.cc
+++ chrome/browser/sync/signin_manager_unittest.cc
@@ -84,6 +84,7 @@ class SigninManagerTest : public TokenServiceTestHarness {
 // NOTE: ClientLogin's "StartSignin" is called after collecting credentials
 //       from the user.  See also SigninManagerTest::SignInOAuth.
 TEST_F(SigninManagerTest, SignInClientLogin) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
@@ -101,12 +102,13 @@ TEST_F(SigninManagerTest, SignInClientLogin) {
   manager_.reset(new SigninManager());
   manager_->Initialize(profile_.get());
   EXPECT_EQ("user@gmail.com", manager_->GetUsername());
-  browser_sync::SetIsUsingOAuthForTest(false);
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 // NOTE: OAuth's "StartOAuthSignIn" is called before collecting credentials
 //       from the user.  See also SigninManagerTest::SignInClientLogin.
 TEST_F(SigninManagerTest, SignInOAuth) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(true);
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
@@ -122,10 +124,11 @@ TEST_F(SigninManagerTest, SignInOAuth) {
   manager_.reset(new SigninManager());
   manager_->Initialize(profile_.get());
   EXPECT_EQ("user-xZIuqTKu@gmail.com", manager_->GetUsername());
-  browser_sync::SetIsUsingOAuthForTest(false);
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, SignOutClientLogin) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   manager_->StartSignIn("username", "password", "", "");
@@ -139,9 +142,11 @@ TEST_F(SigninManagerTest, SignOutClientLogin) {
   manager_.reset(new SigninManager());
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, SignOutOAuth) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(true);
   manager_->Initialize(profile_.get());
 
@@ -156,10 +161,11 @@ TEST_F(SigninManagerTest, SignOutOAuth) {
   manager_.reset(new SigninManager());
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
-  browser_sync::SetIsUsingOAuthForTest(false);
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, SignInFailureClientLogin) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   manager_->StartSignIn("username", "password", "", "");
@@ -175,9 +181,11 @@ TEST_F(SigninManagerTest, SignInFailureClientLogin) {
   manager_.reset(new SigninManager());
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, ProvideSecondFactorSuccess) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   manager_->StartSignIn("username", "password", "", "");
@@ -194,9 +202,11 @@ TEST_F(SigninManagerTest, ProvideSecondFactorSuccess) {
 
   EXPECT_EQ(1U, google_login_success_.size());
   EXPECT_EQ(1U, google_login_failure_.size());
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, ProvideSecondFactorFailure) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   manager_->StartSignIn("username", "password", "", "");
@@ -224,9 +234,11 @@ TEST_F(SigninManagerTest, ProvideSecondFactorFailure) {
   EXPECT_EQ(0U, google_login_success_.size());
   EXPECT_EQ(3U, google_login_failure_.size());
   EXPECT_TRUE(manager_->GetUsername().empty());
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, SignOutMidConnect) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(false);
   manager_->Initialize(profile_.get());
   manager_->StartSignIn("username", "password", "", "");
@@ -235,9 +247,11 @@ TEST_F(SigninManagerTest, SignOutMidConnect) {
   EXPECT_EQ(0U, google_login_failure_.size());
 
   EXPECT_TRUE(manager_->GetUsername().empty());
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
 
 TEST_F(SigninManagerTest, SignOutOnUserInfoSucessRaceTest) {
+  bool originally_using_oauth = browser_sync::IsUsingOAuth();
   browser_sync::SetIsUsingOAuthForTest(true);
   manager_->Initialize(profile_.get());
   EXPECT_TRUE(manager_->GetUsername().empty());
@@ -246,5 +260,5 @@ TEST_F(SigninManagerTest, SignOutOnUserInfoSucessRaceTest) {
   manager_->SignOut();
   SimulateOAuthUserInfoSuccess();
   EXPECT_TRUE(manager_->GetUsername().empty());
-  browser_sync::SetIsUsingOAuthForTest(false);
+  browser_sync::SetIsUsingOAuthForTest(originally_using_oauth);
 }
