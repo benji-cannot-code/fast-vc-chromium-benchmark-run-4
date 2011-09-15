8d712b20787f3b993ac6d60aa26f4d706b88863e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 19:18:41 +0000

    Linux: Better detection of SELinux - check for files in /selinux/.
    BUG=none
    TEST=OOM scores get adjusted on Ubuntu machines with an empty /selinux/ directory.
    Review URL: http://codereview.chromium.org/7901007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101353 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/zygote_host_linux.cc
+++ content/browser/zygote_host_linux.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/eintr_wrapper.h"
 #include "base/environment.h"
+#include "base/file_util.h"
 #include "base/linux_util.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
+#include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "content/browser/content_browser_client.h"
 #include "content/browser/renderer_host/render_sandbox_host_linux.h"
@@ -307,14 +309,17 @@ void ZygoteHost::AdjustRendererOOMScore(base::ProcessHandle pid, int score) {
   // don't want to add another library to the build as it's sure to cause
   // problems with other, non-SELinux distros.
   //
-  // So we just check for /selinux. This isn't foolproof, but it's not bad
-  // and it's easy.
+  // So we just check for files in /selinux. This isn't foolproof, but it's not
+  // bad and it's easy.
 
   static bool selinux;
   static bool selinux_valid = false;
 
   if (!selinux_valid) {
-    selinux = access("/selinux", X_OK) == 0;
+    const FilePath kSelinuxPath("/selinux");
+    selinux = access(kSelinuxPath.value().c_str(), X_OK) == 0 &&
+        file_util::CountFilesCreatedAfter(kSelinuxPath,
+                                          base::Time::UnixEpoch()) > 0;
     selinux_valid = true;
   }
 
