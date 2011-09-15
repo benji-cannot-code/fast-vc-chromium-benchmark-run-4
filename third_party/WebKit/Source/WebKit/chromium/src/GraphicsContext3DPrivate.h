cb46a45d107d2ab56ed06a690aae7d09bad5663f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-15 03:04:37 +0000

    [chromium] Return correct value for getContextAttributes().preserveDrawingBuffer https://bugs.webkit.org/show_bug.cgi?id=68082
    Patch by John Bauman <jbauman@chromium.org> on 2011-09-14
    Reviewed by Kenneth Russell.
    
    WebKit::WebGraphicsContext3D::Attributes doesn't save
    preserveDrawingBuffer, so that has to be saving in
    GraphicsContext3DPrivate.
    
    * src/GraphicsContext3DChromium.cpp:
    (WebCore::GraphicsContext3DPrivate::GraphicsContext3DPrivate):
    (WebCore::GraphicsContext3DPrivate::create):
    (WebCore::GraphicsContext3DPrivate::createGraphicsContextFromWebContext):
    (WebCore::GraphicsContext3DPrivate::getContextAttributes):
    * src/GraphicsContext3DPrivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/GraphicsContext3DPrivate.h
+++ third_party/WebKit/Source/WebKit/chromium/src/GraphicsContext3DPrivate.h
@@ -56,7 +56,7 @@ class SwapBuffersCompleteCallbackAdapter;
 
 class GraphicsContext3DPrivate {
 public:
-    static PassOwnPtr<GraphicsContext3DPrivate> create(WebKit::WebViewImpl*, PassOwnPtr<WebKit::WebGraphicsContext3D>);
+    static PassOwnPtr<GraphicsContext3DPrivate> create(WebKit::WebViewImpl*, PassOwnPtr<WebKit::WebGraphicsContext3D>, GraphicsContext3D::Attributes);
 
     enum ThreadUsage {
         ForUseOnThisThread,
@@ -74,8 +74,6 @@ public:
     static PassRefPtr<GraphicsContext3D> createGraphicsContextFromWebContext(PassOwnPtr<WebKit::WebGraphicsContext3D>, GraphicsContext3D::Attributes, HostWindow*, GraphicsContext3D::RenderStyle, ThreadUsage);
 
     ~GraphicsContext3DPrivate();
-
-    bool initialize(GraphicsContext3D::Attributes, HostWindow*, bool renderDirectlyToHostWindow);
 
     // Helper function to provide access to the lower-level WebGraphicsContext3D,
     // which is needed for subordinate contexts like WebGL's to share resources
@@ -307,7 +305,7 @@ public:
     GC3Denum getGraphicsResetStatusARB();
 
 private:
-    GraphicsContext3DPrivate(WebKit::WebViewImpl*, PassOwnPtr<WebKit::WebGraphicsContext3D>);
+    GraphicsContext3DPrivate(WebKit::WebViewImpl*, PassOwnPtr<WebKit::WebGraphicsContext3D>, GraphicsContext3D::Attributes);
 
     OwnPtr<WebKit::WebGraphicsContext3D> m_impl;
     OwnPtr<Extensions3DChromium> m_extensions;
@@ -318,6 +316,7 @@ private:
     HashSet<String> m_enabledExtensions;
     HashSet<String> m_requestableExtensions;
     bool m_layerComposited;
+    bool m_preserveDrawingBuffer;
 
     enum ResourceSafety {
         ResourceSafetyUnknown,
