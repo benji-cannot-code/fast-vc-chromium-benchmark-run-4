749ee7f4f472a9ff07249b227d6372aa788b5e07
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 03:43:17 +0000

    profile.cc should only have static and non virtual methods.
    BUG=89937
    TEST=passes existing
    
    Review URL: http://codereview.chromium.org/7885010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -794,6 +794,10 @@ void TestingProfile::SetDownloadManagerDelegate(
   NOTIMPLEMENTED();
 }
 
+chrome_browser_net::Predictor* TestingProfile::GetNetworkPredictor() {
+  return NULL;
+}
+
 PrefService* TestingProfile::GetOffTheRecordPrefs() {
   return NULL;
 }
