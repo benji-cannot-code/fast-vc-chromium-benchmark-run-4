1bac8b9b73e63875a3804f18e37b8cc0a8e2ca11
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 01:30:16 +0000

    Clear the stale errors when a new account is being setup. This way the past unrecoverable errors dont mess the logic of new account being setup. This avoids an infinite spin.
    BUG=95591
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7860017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -555,6 +555,8 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
 
   void NotifyObservers();
 
+  void ClearStaleErrors();
+
   static const char* GetPrefNameForDataType(syncable::ModelType data_type);
 
   // About-flags experiment names for datatypes that aren't enabled by default
