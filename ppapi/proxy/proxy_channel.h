bd812b80d6f695a95b0d0bd28d11544517c80fef
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 07:36:50 +0000

    Removed the dependency of PepperPluginRegistry on Pepper proxy.
    We would need to move PepperPluginRegistry to webkit/plugins/ppapi so that it can be used by test_shell. But before we can move it, we need to remove all dependencies on content and pepper/proxy.
    TEST=Manually ran pepper 3d demos in-process and out-of-process.
    
    Review URL: http://codereview.chromium.org/7885009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101259 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/proxy_channel.h
+++ ppapi/proxy/proxy_channel.h
@@ -35,6 +35,8 @@ class PPAPI_PROXY_EXPORT ProxyChannel
 
   class Delegate {
    public:
+    virtual ~Delegate() {}
+
     // Returns the dedicated message loop for processing IPC requests.
     virtual base::MessageLoopProxy* GetIPCMessageLoop() = 0;
 
