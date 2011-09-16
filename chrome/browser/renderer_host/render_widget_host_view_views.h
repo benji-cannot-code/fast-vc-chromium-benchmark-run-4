2d55248a9978269a7904d79f0386847b6f4aca1b
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 18:59:19 +0000

    Add notification of virtual keyboard hide/show and change ScrollFocusedNodeToView IPC to Scroll to a specific location.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7903017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_views.h
+++ chrome/browser/renderer_host/render_widget_host_view_views.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/time.h"
 #include "content/browser/renderer_host/render_widget_host_view.h"
+#include "content/common/notification_observer.h"
+#include "content/common/notification_registrar.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebInputEvent.h"
 #include "ui/gfx/native_widget_types.h"
 #include "views/controls/native/native_view_host.h"
@@ -38,7 +40,8 @@ struct NativeWebKeyboardEvent;
 // -----------------------------------------------------------------------------
 class RenderWidgetHostViewViews : public RenderWidgetHostView,
                                   public views::TouchSelectionClientView,
-                                  public views::TextInputClient {
+                                  public views::TextInputClient,
+                                  public NotificationObserver {
  public:
   // Internal class name.
   static const char kViewClassName[];
@@ -116,6 +119,11 @@ class RenderWidgetHostViewViews : public RenderWidgetHostView,
   // Overridden from views::TouchSelectionClientView.
   virtual void SelectRect(const gfx::Point& start,
                           const gfx::Point& end) OVERRIDE;
+
+  // Overriden from NotificationObserver
+  virtual void Observe(int type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details) OVERRIDE;
 
   // Overridden from ui::SimpleMenuModel::Delegate.
   virtual bool IsCommandIdChecked(int command_id) const OVERRIDE;
@@ -279,6 +287,9 @@ class RenderWidgetHostViewViews : public RenderWidgetHostView,
       update_touch_selection_;
 
 #if defined(TOUCH_UI)
+  // used to register for keyboard visiblity notificatons.
+  NotificationRegistrar registrar_;
+  gfx::Rect keyboard_rect_;
   std::map<uint64, scoped_refptr<AcceleratedSurfaceContainerTouch> >
       accelerated_surface_containers_;
 #endif
