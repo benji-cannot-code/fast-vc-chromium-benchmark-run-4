da801545d5a1a3ed7ff230ff7afaabc3ee452d13
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 22:03:21 +0000

    Support Panel auto-resize on Mac.
    BUG=None
    TEST=PanelBrowserTest.AutoResize
    
    Review URL: http://codereview.chromium.org/7890045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101578 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_manager.h
+++ chrome/browser/ui/panels/panel_manager.h
@@ -142,7 +142,7 @@ class PanelManager : public AutoHidingDesktopBar::Observer,
 
   int GetMaxPanelWidth() const;
   int GetMaxPanelHeight() const;
-  int GetRightMostAvaialblePosition() const;
+  int GetRightMostAvailablePosition() const;
 
   // Updates the maximum size of each panel as the result of adding, removing,
   // or sizing panels.
