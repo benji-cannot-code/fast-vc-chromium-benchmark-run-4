0f68b5c4c363df7102553b9ff57b78b4996228da
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-16 20:05:01 +0000

    Unreviewed, rolling out r95309. http://trac.webkit.org/changeset/95309 https://bugs.webkit.org/show_bug.cgi?id=68271
    Broke many chromium gpu tests. (Requested by dave_levin on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-09-16
    
    Source/WebCore:
    
    * platform/graphics/chromium/ContentLayerChromium.cpp:
    (WebCore::ContentLayerChromium::createTextureUpdater):
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::LayerRendererChromium):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.h:
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::context):
    (WebCore::CCThreadProxy::beginFrameAndCommitOnCCThread):
    (WebCore::CCThreadProxy::setNeedsCommitAndRedrawOnCCThread):
    (WebCore::CCThreadProxy::setNeedsRedrawOnCCThread):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostTest.cpp:
    (WTF::CCLayerTreeHostTest::CCLayerTreeHostTest):
    (WTF::CCLayerTreeHostTest::animateAndLayout):
    (WTF::CCLayerTreeHostTest::beginCommitOnCCThread):
    (WTF::CCLayerTreeHostTest::beginCommitOnMainThread):
    (WTF::CCLayerTreeHostTest::commitOnCCThread):
    (WTF::CCLayerTreeHostTest::commitCompleteOnCCThread):
    (WTF::CCLayerTreeHostTest::commitCompleteOnMainThread):
    (WTF::CCLayerTreeHostTest::drawLayersAndPresentOnCCThread):
    (WTF::CCLayerTreeHostTest::updateLayers):
    (WTF::CCLayerTreeHostTest::onBeginTest):
    (WTF::CCLayerTreeHostTest::doEndTest):
    (WTF::CCLayerTreeHostTest::onEndTest):
    (WTF::CCLayerTreeHostTest::runTest):
    (WTF::CCLayerTreeHostTest::testTimeout):
    (WTF::MockLayerTreeHostClient::MockLayerTreeHostClient):
    (WTF::MockLayerTreeHostClient::createLayerTreeHostContext3D):
    (WTF::MockLayerTreeHostClient::animateAndLayout):
    (WTF::MockLayerTreeHostClient::updateLayers):
    (WTF::MockLayerTreeHostCommitter::create):
    (WTF::MockLayerTreeHostCommitter::commit):
    (WTF::MockLayerTreeHostCommitter::MockLayerTreeHostCommitter):
    (WTF::MockLayerTreeHostImpl::create):
    (WTF::MockLayerTreeHostImpl::beginCommit):
    (WTF::MockLayerTreeHostImpl::commitComplete):
    (WTF::MockLayerTreeHostImpl::drawLayersAndPresent):
    (WTF::MockLayerTreeHostImpl::MockLayerTreeHostImpl):
    (WTF::MockLayerTreeHostImplProxy::create):
    (WTF::MockLayerTreeHostImplProxy::createLayerTreeHostImpl):
    (WTF::MockLayerTreeHostImplProxy::MockLayerTreeHostImplProxy):
    (WTF::MockLayerTreeHost::MockLayerTreeHost):
    (WTF::MockLayerTreeHost::createLayerTreeHostImplProxy):
    (WTF::MockLayerTreeHost::updateLayers):
    (WTF::MockLayerTreeHost::createLayerTreeHostCommitter):
    (WTF::MockLayerTreeHost::beginCommit):
    (WTF::MockLayerTreeHost::commitComplete):
    (WTF::CCLayerTreeHostTest::doBeginTest):
    (WTF::CCLayerTreeHostTestShortlived2::beginTest):
    (WTF::CCLayerTreeHostTestShortlived3::beginTest):
    (WTF::CCLayerTreeHostTestCommitingWithContinuousRedraw::beginTest):
    (WTF::CCLayerTreeHostTestCommitingWithContinuousRedraw::commitCompleteOnCCThread):
    (WTF::CCLayerTreeHostTestCommitingWithContinuousRedraw::drawLayersAndPresentOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsCommit1::beginTest):
    (WTF::CCLayerTreeHostTestSetNeedsCommit1::drawLayersAndPresentOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsCommit1::commitOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsCommit2::beginTest):
    (WTF::CCLayerTreeHostTestSetNeedsCommit2::drawLayersAndPresentOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsCommit2::commitOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsRedraw::beginTest):
    (WTF::CCLayerTreeHostTestSetNeedsRedraw::drawLayersAndPresentOnCCThread):
    (WTF::CCLayerTreeHostTestSetNeedsRedraw::commitOnCCThread):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.h
@@ -100,7 +100,7 @@ public:
     void commitTo(CCLayerTreeHostImpl*);
     PassOwnPtr<CCThread> createCompositorThread();
     PassRefPtr<GraphicsContext3D> createLayerTreeHostContext3D();
-    virtual PassOwnPtr<CCLayerTreeHostImpl> createLayerTreeHostImpl();
+    PassOwnPtr<CCLayerTreeHostImpl> createLayerTreeHostImpl();
     void didRecreateGraphicsContext(bool success);
 #if !USE(THREADED_COMPOSITING)
     void scheduleComposite();
@@ -155,7 +155,6 @@ public:
 
 protected:
     CCLayerTreeHost(CCLayerTreeHostClient*, PassRefPtr<LayerChromium> rootLayer, const CCSettings&);
-    bool initialize();
 
 private:
     typedef Vector<RefPtr<LayerChromium> > LayerList;
@@ -165,6 +164,8 @@ private:
     void updateCompositorResources(const LayerList&, GraphicsContext3D*);
     void updateCompositorResources(LayerChromium*, GraphicsContext3D*);
     void clearPendingUpdate();
+
+    bool initialize();
 
     bool m_animating;
 
