ee36ddb6f5e4cd65020af22a32c829604d3fac6a
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-16 21:34:20 +0000

    Prepare JSTypes for more Object subtypes https://bugs.webkit.org/show_bug.cgi?id=68200
    Reviewed by Gavin Barraclough.
    
    * dfg/DFGJITCompiler.h:
    (JSC::DFG::JITCompiler::branchIfNotObject):
    * jit/JITInlineMethods.h:
    (JSC::JIT::emitJumpIfNotObject):
    * runtime/JSGlobalObject.h:
    (JSC::Structure::prototypeForLookup):
    * runtime/JSObject.h:
    (JSC::JSObject::finishCreation):
    * runtime/JSType.h:
    * runtime/JSTypeInfo.h:
    (JSC::TypeInfo::type):
    (JSC::TypeInfo::isObject):
    (JSC::TypeInfo::isFinal):
    (JSC::TypeInfo::prohibitsPropertyCaching):
    * runtime/NativeErrorConstructor.h:
    (JSC::NativeErrorConstructor::finishCreation):
    * runtime/Operations.cpp:
    (JSC::jsIsObjectType):
    * runtime/Structure.cpp:
    (JSC::Structure::addPropertyTransitionToExistingStructure):
    (JSC::Structure::addPropertyTransition):
    * runtime/Structure.h:
    (JSC::Structure::isObject):
    (JSC::JSCell::isObject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalObject.h
@@ -349,7 +349,7 @@ namespace JSC {
 
     inline JSValue Structure::prototypeForLookup(ExecState* exec) const
     {
-        if (typeInfo().type() == ObjectType)
+        if (isObject())
             return m_prototype.get();
 
         ASSERT(typeInfo().type() == StringType);
