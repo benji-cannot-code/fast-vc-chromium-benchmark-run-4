057e5f40c4a90a2df3e7e80646eb12ea38ce4700
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 00:00:09 +0000

    Fix aspect ratio and clarify video frame dimensions
    BUG=18941,94861
    TEST=video-aspect-ratio.html
    
    Review URL: http://codereview.chromium.org/7864009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101418 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base.cc
+++ media/filters/video_renderer_base.cc
@@ -149,7 +149,7 @@ void VideoRendererBase::Initialize(VideoDecoder* decoder,
                  base::Unretained(this)));
 
   // Notify the pipeline of the video dimensions.
-  host()->SetVideoSize(decoder_->width(), decoder_->height());
+  host()->SetNaturalVideoSize(decoder_->natural_size());
 
   // Initialize the subclass.
   // TODO(scherkus): do we trust subclasses not to do something silly while
