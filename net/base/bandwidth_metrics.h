176592844d66b0a9404f68d491a1d6b00343a5c2
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 00:45:36 +0000

    Change LOG(INFO) to VLOG(1) in net/base/bandwidth_metrics.h
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7885004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101423 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/bandwidth_metrics.h
+++ net/base/bandwidth_metrics.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,8 +94,8 @@ class BandwidthMetrics {
         double kbps = static_cast<double>(bytes_since_last_start_) * 8 / ms;
         ++num_data_samples_;
         data_sum_ += kbps;
-        LOG(INFO) << "Bandwidth: " << kbps
-                  << "Kbps (avg " << bandwidth() << "Kbps)";
+        VLOG(1) << "Bandwidth: " << kbps
+                << "Kbps (avg " << bandwidth() << "Kbps)";
         int kbps_int = static_cast<int>(kbps);
         UMA_HISTOGRAM_COUNTS_10000("Net.DownloadBandwidth", kbps_int);
       }
