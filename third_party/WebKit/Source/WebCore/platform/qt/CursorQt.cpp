0aa00e745bde0708cfe904a640be9091b25adbc6
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-16 07:15:57 +0000

    [Qt] Crash when dragging google maps. https://bugs.webkit.org/show_bug.cgi?id=68223
    Source/WebCore: 
    
    The custom image for a cursor might not be ready when WebKit trying
    to use it to construct the cursor in Qt porting. This patch is using
    arrow image to substitute the unready custom image.
    
    Reviewed by Adam Barth.
    
    Test: fast/css/crash-on-custom-cursor-when-loading.html
    
    * platform/qt/CursorQt.cpp:
    (WebCore::createCustomCursor):
    (WebCore::Cursor::ensurePlatformCursor):
    
    LayoutTests: 
    
    Test case to use an unready custom cursor.
    
    Reviewed by Adam Barth.
    
    * fast/css/crash-on-custom-cursor-when-loading-expected.txt: Added.
    * fast/css/crash-on-custom-cursor-when-loading.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/CursorQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/CursorQt.cpp
@@ -76,6 +76,8 @@ Cursor& Cursor::operator=(const Cursor& other)
 #ifndef QT_NO_CURSOR
 static QCursor* createCustomCursor(Image* image, const IntPoint& hotSpot)
 {
+    if (!image->nativeImageForCurrentFrame())
+        return 0;
     IntPoint effectiveHotSpot = determineHotSpot(image, hotSpot);
     return new QCursor(*(image->nativeImageForCurrentFrame()), effectiveHotSpot.x(), effectiveHotSpot.y());
 }
@@ -198,6 +200,8 @@ void Cursor::ensurePlatformCursor() const
         break;
     case Custom:
         m_platformCursor = createCustomCursor(m_image.get(), m_hotSpot);
+        if (!m_platformCursor)
+            m_platformCursor = new QCursor(Qt::ArrowCursor);
         break;
     default:
         ASSERT_NOT_REACHED();
