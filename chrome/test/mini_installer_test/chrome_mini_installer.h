e817651d8be33b3f4dd2d7b93e8be33047c3da00
hnguyen@google.com <hnguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 22:55:55 +0000

    Change the routine to find latest build dir and installer file. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/7818012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/chrome_mini_installer.h
+++ chrome/test/mini_installer_test/chrome_mini_installer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,14 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
-
-class FilePath;
+#include "base/file_path.h"
+#include "base/command_line.h"
 
 // This class has methods to install and uninstall Chrome mini installer.
 class ChromeMiniInstaller {
  public:
   ChromeMiniInstaller(const std::wstring& install_type, bool is_chrome_frame);
-
   ~ChromeMiniInstaller() {}
 
   enum RepairChrome {
@@ -47,8 +46,7 @@ class ChromeMiniInstaller {
   void InstallMetaInstaller();
 
   // Installs Chrome Mini Installer.
-  void InstallMiniInstaller(bool over_install = false,
-                            const std::wstring& path = L"");
+  void InstallMiniInstaller(bool over_install, const FilePath& path);
 
   // This will test the standalone installer.
   void InstallStandaloneInstaller();
@@ -112,7 +110,7 @@ class ChromeMiniInstaller {
   FilePath GetUserDataDirPath();
 
   // Gets the path to launch Chrome.
-  bool GetChromeLaunchPath(std::wstring* launch_path);
+  bool GetChromeLaunchPath(FilePath* launch_path);
 
   // This method will get Chrome.exe path and launch it.
   void VerifyChromeLaunch(bool expected_status);
@@ -132,16 +130,15 @@ class ChromeMiniInstaller {
   void LaunchIE(const std::wstring& navigate_url);
 
   // Launches the chrome installer and waits for it to end.
-  void LaunchInstaller(const std::wstring& install_path,
+  void LaunchInstaller(const FilePath& path,
                        const wchar_t* process_name);
 
   // Verifies if Chrome launches after install.
   void LaunchAndCloseChrome(bool over_install);
 
   // Launches any requested browser.
-  void LaunchBrowser(const std::wstring& launch_path,
-                     const std::wstring& launch_arg,
-                     const std::wstring& process_name,
+  void LaunchBrowser(const FilePath& path,
+                     const std::wstring& args,
                      bool expected_status);
 
   // Compares the registry key values after overinstall.
@@ -151,25 +148,25 @@ class ChromeMiniInstaller {
   // This method will verify if the installed build is correct.
   bool VerifyStandaloneInstall();
 
+  // Get all the latest installers base on last modified date.
+  bool LocateInstallers(const std::wstring& build);
+
+  // This method will create a command line to run apply tag.
+  CommandLine GetCommandForTagging();
+
   // This variable holds the install type.
   // Install type can be either system or user level.
   std::wstring install_type_;
 
   bool is_chrome_frame_;
 
-  // Name of the browser (Chrome or Chromium) and install type (sys or user)
-  std::wstring installer_name_;
+  FilePath full_installer_;
+  FilePath diff_installer_;
+  FilePath previous_installer_;
+  FilePath standalone_installer_;
 
-  // The full path to the various installers.
-  std::wstring full_installer_, diff_installer_, prev_installer_;
-
-  // Whether the path to the associated installer could be found.
-  // This is because we do not want to assert that these paths exist
-  // except in the tests that use them.
-  bool has_full_installer_, has_diff_installer_, has_prev_installer_;
-
-  // The version string of the current and previous builds.
-  std::wstring curr_version_, prev_version_;
+  // Build numbers.
+  std::wstring current_build_, previous_build_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeMiniInstaller);
 };
