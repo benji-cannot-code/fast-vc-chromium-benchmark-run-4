45829e6ec3005dd6c0a3a16376d8224d5b6350cc
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-16 20:32:15 +0000

    Rationalize JSObject::putDirect* methods https://bugs.webkit.org/show_bug.cgi?id=68274
    Reviewed by Sam Weinig.
            
    Delete the *Function variants. These are overall inefficient,
    in the way they get the name back from the function rather
    than just passing it in.
    
    * JavaScriptCore.exp:
    * jsc.cpp:
    (GlobalObject::finishCreation):
    (GlobalObject::addFunction):
    * runtime/FunctionPrototype.cpp:
    (JSC::FunctionPrototype::addFunctionProperties):
    * runtime/JSGlobalObject.cpp:
    (JSC::JSGlobalObject::reset):
    * runtime/JSObject.cpp:
    (JSC::JSObject::put):
    (JSC::JSObject::putWithAttributes):
    (JSC::JSObject::defineGetter):
    (JSC::JSObject::defineSetter):
    * runtime/JSObject.h:
    (JSC::JSObject::putDirect):
    (JSC::JSObject::putDirectWithoutTransition):
    * runtime/Lookup.cpp:
    (JSC::setUpStaticFunctionSlot):
    * runtime/Lookup.h:
    (JSC::lookupPut):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Lookup.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Lookup.h
@@ -334,7 +334,7 @@ namespace JSC {
 
         if (entry->attributes() & Function) { // function: put as override property
             if (LIKELY(value.isCell()))
-                thisObj->putDirectFunction(exec->globalData(), propertyName, value.asCell());
+                thisObj->putDirect(exec->globalData(), propertyName, value.asCell());
             else
                 thisObj->putDirect(exec->globalData(), propertyName, value);
         } else if (!(entry->attributes() & ReadOnly))
