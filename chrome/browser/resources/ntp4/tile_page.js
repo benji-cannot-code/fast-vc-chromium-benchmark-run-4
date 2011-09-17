4c7f3f5fef08602a741d385754fb5d00fb24e56d
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-17 02:17:56 +0000

    [ntp4] Work-around a WebKit that causes a crash, temporary solution until the core issue is fixed.
    BUG=96851
    TEST=Drag app or bookmark tiles without crashing.
    Review URL: http://codereview.chromium.org/7932009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101626 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/tile_page.js
+++ chrome/browser/resources/ntp4/tile_page.js
@@ -710,7 +710,10 @@ cr.define('ntp4', function() {
               'black ' + (this.tileGrid_.clientWidth - leftMargin) + 'px, ' +
               'transparent ' + (this.tileGrid_.clientWidth - leftMargin +
                                 fadeDistance) + 'px, ' +
-              'transparent)';
+              'transparent) ' +
+              // Need to set mask-box-image-slice property due to a WebKit bug.
+              // Details at: http://crbug.com/96851
+              ' 100%';
       this.tileGrid_.style.WebkitMaskBoxImage = gradient;
     },
 
