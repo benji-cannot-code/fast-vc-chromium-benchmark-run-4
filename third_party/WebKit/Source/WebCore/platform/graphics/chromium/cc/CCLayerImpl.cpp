392fcfb9c39c007313b4d4cfa216e68b8f5077c2
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-17 00:33:09 +0000

    [chromium] Move quad drawing code from LayerChromium to LayerRendererChromium https://bugs.webkit.org/show_bug.cgi?id=68272
    Reviewed by James Robinson.
    
    All the OpenGL code is in LayerRendererChromium and the CCLayerImpl
    tree, so move the static drawTexturedQuad/toGLMatrix functions to
    LayerRendererChromium, where they really belong.
    
    Just moving code around. Tested by existing compositor tests.
    
    * platform/graphics/chromium/LayerChromium.cpp:
    * platform/graphics/chromium/LayerChromium.h:
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::toGLMatrix):
    (WebCore::LayerRendererChromium::drawTexturedQuad):
    * platform/graphics/chromium/LayerRendererChromium.h:
    * platform/graphics/chromium/cc/CCCanvasLayerImpl.cpp:
    (WebCore::CCCanvasLayerImpl::draw):
    * platform/graphics/chromium/cc/CCHeadsUpDisplay.cpp:
    (WebCore::CCHeadsUpDisplay::draw):
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
    (WebCore::CCLayerImpl::drawDebugBorder):
    * platform/graphics/chromium/cc/CCPluginLayerImpl.cpp:
    (WebCore::CCPluginLayerImpl::draw):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::drawSurface):
    * platform/graphics/chromium/cc/CCTiledLayerImpl.cpp:
    (WebCore::CCTiledLayerImpl::drawTiles):
    * platform/graphics/chromium/cc/CCVideoLayerImpl.cpp:
    (WebCore::CCVideoLayerImpl::drawYUV):
    (WebCore::CCVideoLayerImpl::drawRGBA):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerImpl.cpp
@@ -36,29 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/CCLayerSorter.h"
 #include <wtf/text/WTFString.h>
 
-namespace {
-void toGLMatrix(float* flattened, const WebCore::TransformationMatrix& m)
-{
-    flattened[0] = m.m11();
-    flattened[1] = m.m12();
-    flattened[2] = m.m13();
-    flattened[3] = m.m14();
-    flattened[4] = m.m21();
-    flattened[5] = m.m22();
-    flattened[6] = m.m23();
-    flattened[7] = m.m24();
-    flattened[8] = m.m31();
-    flattened[9] = m.m32();
-    flattened[10] = m.m33();
-    flattened[11] = m.m34();
-    flattened[12] = m.m41();
-    flattened[13] = m.m42();
-    flattened[14] = m.m43();
-    flattened[15] = m.m44();
-}
-}
-
-
 namespace WebCore {
 
 CCLayerImpl::CCLayerImpl(int id)
@@ -168,7 +145,7 @@ void CCLayerImpl::drawDebugBorder(LayerRendererChromium* layerRenderer)
 
     TransformationMatrix renderMatrix = drawTransform();
     renderMatrix.scale3d(bounds().width(), bounds().height(), 1);
-    toGLMatrix(&glMatrix[0], layerRenderer->projectionMatrix() * renderMatrix);
+    LayerRendererChromium::toGLMatrix(&glMatrix[0], layerRenderer->projectionMatrix() * renderMatrix);
     GLC(context, context->uniformMatrix4fv(program->vertexShader().matrixLocation(), false, &glMatrix[0], 1));
 
     GLC(context, context->uniform4f(program->fragmentShader().colorLocation(), debugBorderColor().red() / 255.0, debugBorderColor().green() / 255.0, debugBorderColor().blue() / 255.0, 1));
