6ba67896b632f4535e28aac7fa485e4a4f69ecdf
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-17 10:57:17 +0000

    Override supportsFocus() for HTMLMediaElement. https://bugs.webkit.org/show_bug.cgi?id=67190
    Patch by Deepak Sherveghar <bpwv64@motorola.com> on 2011-09-17
    Reviewed by Antonio Gomes.
    
    Source/WebCore:
    
    Tests: fast/events/media-element-focus-tab.html
           fast/spatial-navigation/snav-media-elements.html
    
    Return true from supportsFocus() if control's attribute is present or a tabindex is specified.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::supportsFocus):
    * html/HTMLMediaElement.h:
    
    LayoutTests:
    
    For media elements be able to receive focus without tabindex if
    control attribute is present, hence changed
    tabindex-focus-blur-all.js and tabindex-focus-blur-all-expected.txt.
    
    * fast/events/media-element-focus-tab-expected.txt: Added.
    * fast/events/media-element-focus-tab.html: Added.
    * fast/events/resources/tabindex-focus-blur-all.js:
    (test):
    * fast/events/tabindex-focus-blur-all-expected.txt:
    * fast/spatial-navigation/snav-media-elements-expected.txt: Added.
    * fast/spatial-navigation/snav-media-elements.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
@@ -250,6 +250,7 @@ protected:
 private:
     void createMediaPlayer();
 
+    virtual bool supportsFocus() const;
     virtual void attributeChanged(Attribute*, bool preserveDecls);
     virtual bool rendererIsNeeded(const NodeRenderingContext&);
     virtual RenderObject* createRenderer(RenderArena*, RenderStyle*);
