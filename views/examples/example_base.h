8c3d81559f038f9b447a8993111d1d8564438fd6
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-17 00:31:51 +0000

    views/examples: Change the return type of BoolToOnOff() function to char.
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7918007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101605 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/example_base.h
+++ views/examples/example_base.h
@@ -42,8 +42,8 @@ class ExampleBase {
   void PrintStatus(const char* format, ...);
 
   // Converts an boolean value to "on" or "off".
-  const wchar_t* BoolToOnOff(bool value) {
-    return value ? L"on" : L"off";
+  const char* BoolToOnOff(bool value) {
+    return value ? "on" : "off";
   }
 
  private:
