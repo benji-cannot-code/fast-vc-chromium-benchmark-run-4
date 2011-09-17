09323d0edb0240af962c558cb28a8160478df2d6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-17 00:50:28 +0000

    Remove default NULL argument in LayerChromium::create(). https://bugs.webkit.org/show_bug.cgi?id=68211
    Patch by Shawn Singh <shawnsingh@chromium.org> on 2011-09-16
    Reviewed by James Robinson.
    
    Code cleanup related to creating unit testing.
    
    * platform/graphics/chromium/LayerChromium.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95344 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
@@ -70,7 +70,7 @@ public:
 class LayerChromium : public RefCounted<LayerChromium> {
     friend class LayerTilerChromium;
 public:
-    static PassRefPtr<LayerChromium> create(CCLayerDelegate* = 0);
+    static PassRefPtr<LayerChromium> create(CCLayerDelegate*);
 
     virtual ~LayerChromium();
 
