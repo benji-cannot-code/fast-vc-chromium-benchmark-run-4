14dc3fd2e6ece1dc0d1e0e703fad87bb0be72548
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-18 17:25:16 +0000

    views: Remove unnecessary include of combobox.h from native_control_win.h
    While I'm here also remove scoped_ptr.h include (which is also unused) and
    annotate with OVERRIDE the overridden methods.
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7901026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101703 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/native_control_win.cc
+++ views/controls/native_control_win.cc
@@ -12,14 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_win.h"
 #include "ui/base/view_prop.h"
 #include "ui/base/win/hwnd_util.h"
+#include "views/controls/combobox/combobox.h"
 #include "views/focus/focus_manager.h"
 #include "views/widget/widget.h"
 
 using ui::ViewProp;
 
-namespace views {
+const char kNativeControlWinKey[] = "__NATIVE_CONTROL_WIN__";
 
-static const char* const kNativeControlWinKey = "__NATIVE_CONTROL_WIN__";
+namespace views {
 
 ////////////////////////////////////////////////////////////////////////////////
 // NativeControlWin, public:
@@ -109,7 +110,7 @@ void NativeControlWin::OnFocus() {
   // a native win32 control, we don't always send a native (MSAA)
   // focus notification.
   bool send_native_event =
-      parent_view->GetClassName() != views::Combobox::kViewClassName &&
+      parent_view->GetClassName() != Combobox::kViewClassName &&
       parent_view->HasFocus();
 
   // Send the accessibility focus notification.
