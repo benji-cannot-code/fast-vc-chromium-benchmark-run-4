217309562e273a98a2813b887f751eecb3073069
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-19 20:36:13 +0000

    Some checks to narrow in on duplicate history ids from history DB.
    R=benjhayden@chromium.org
    BUG=96627
    
    
    Review URL: http://codereview.chromium.org/7904025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/download_database.h
+++ chrome/browser/history/download_database.h
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_HISTORY_DOWNLOAD_DATABASE_H_
 #pragma once
 
+#include <set>
+
 #include "chrome/browser/history/history_types.h"
+#include "base/threading/platform_thread.h"
 
 struct DownloadPersistentStoreInfo;
 class FilePath;
@@ -64,6 +67,11 @@ class DownloadDatabase {
   bool DropDownloadTable();
 
  private:
+  // TODO(rdsmith): Remove after http://crbug.com/96627 has been resolved.
+  std::set<int64> returned_ids_;
+  bool owning_thread_set_;
+  base::PlatformThreadId owning_thread_;
+
   DISALLOW_COPY_AND_ASSIGN(DownloadDatabase);
 };
 
