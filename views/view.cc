a5d76f10a8d45ab4ce566008ebe30ea6acb72498
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-19 21:04:19 +0000

    Make specifying a texture optional on a Layer.
    This is for Layers that participate in Draw() ordering, transforming and clipping, but do not themselves actually render anything.
    
    Also changes LayerTest to use RunAllPending() rather than using the weird QuitOnComposited test type I had.
    
    BUG=none
    TEST=see unittest
    Review URL: http://codereview.chromium.org/7941016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101829 0039d316-1c4b-4281-b951-d872f2087c98

--- views/view.cc
+++ views/view.cc
@@ -1112,7 +1112,8 @@ void View::SetFillsBoundsOpaquely(bool fills_bounds_opaquely) {
 }
 
 bool View::SetExternalTexture(ui::Texture* texture) {
-  if (!texture && !layer_helper_.get())
+  DCHECK(texture);
+  if (!layer_helper_.get())
     return true;
 
   if (!layer_helper_.get())
@@ -1121,10 +1122,8 @@ bool View::SetExternalTexture(ui::Texture* texture) {
 
   // Child views must not paint into the external texture. So make sure each
   // child view has its own layer to paint into.
-  if (texture) {
-    for (Views::iterator i = children_.begin(); i != children_.end(); ++i)
-      (*i)->SetPaintToLayer(true);
-  }
+  for (Views::iterator i = children_.begin(); i != children_.end(); ++i)
+    (*i)->SetPaintToLayer(true);
 
   SchedulePaintInRect(GetLocalBounds());
 
