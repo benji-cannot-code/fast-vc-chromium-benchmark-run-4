daa77ab889cd16a749877a8bcb238809b795a24b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-19 09:19:33 +0000

    Re-name LayerChromium border functions to reflect that they are only for debug use. https://bugs.webkit.org/show_bug.cgi?id=68212
    Patch by Shawn Singh <shawnsingh@chromium.org> on 2011-09-19
    Reviewed by James Robinson.
    
    Code cleanup towards unit testing.
    
    * platform/graphics/chromium/GraphicsLayerChromium.cpp:
    (WebCore::GraphicsLayerChromium::clearBackgroundColor):
    (WebCore::GraphicsLayerChromium::setDebugBackgroundColor):
    (WebCore::GraphicsLayerChromium::setDebugBorder):
    (WebCore::GraphicsLayerChromium::updateLayerBackgroundColor):
    (WebCore::GraphicsLayerChromium::setupContentsLayer):
    * platform/graphics/chromium/LayerChromium.cpp:
    (WebCore::LayerChromium::setDebugBorderColor):
    (WebCore::LayerChromium::setDebugBorderWidth):
    * platform/graphics/chromium/LayerChromium.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/GraphicsLayerChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/GraphicsLayerChromium.cpp
@@ -64,26 +64,6 @@ using namespace std;
 
 namespace WebCore {
 
-static void setLayerBorderColor(LayerChromium& layer, const Color& color)
-{
-    layer.setBorderColor(color);
-}
-
-static void clearBorderColor(LayerChromium& layer)
-{
-    layer.setBorderColor(static_cast<RGBA32>(0));
-}
-
-static void setLayerBackgroundColor(LayerChromium& layer, const Color& color)
-{
-    layer.setBackgroundColor(color);
-}
-
-static void clearLayerBackgroundColor(LayerChromium& layer)
-{
-    layer.setBackgroundColor(static_cast<RGBA32>(0));
-}
-
 PassOwnPtr<GraphicsLayer> GraphicsLayer::create(GraphicsLayerClient* client)
 {
     return adoptPtr(new GraphicsLayerChromium(client));
@@ -272,7 +252,7 @@ void GraphicsLayerChromium::clearBackgroundColor()
         return;
 
     GraphicsLayer::clearBackgroundColor();
-    clearLayerBackgroundColor(*m_contentsLayer);
+    m_contentsLayer->setBackgroundColor(static_cast<RGBA32>(0));
 }
 
 void GraphicsLayerChromium::setContentsOpaque(bool opaque)
@@ -450,19 +430,19 @@ PlatformLayer* GraphicsLayerChromium::platformLayer() const
 void GraphicsLayerChromium::setDebugBackgroundColor(const Color& color)
 {
     if (color.isValid())
-        setLayerBackgroundColor(*m_layer, color);
+        m_layer->setBackgroundColor(color);
     else
-        clearLayerBackgroundColor(*m_layer);
+        m_layer->setBackgroundColor(static_cast<RGBA32>(0));
 }
 
 void GraphicsLayerChromium::setDebugBorder(const Color& color, float borderWidth)
 {
     if (color.isValid()) {
-        setLayerBorderColor(*m_layer, color);
-        m_layer->setBorderWidth(borderWidth);
+        m_layer->setDebugBorderColor(color);
+        m_layer->setDebugBorderWidth(borderWidth);
     } else {
-        clearBorderColor(*m_layer);
-        m_layer->setBorderWidth(0);
+        m_layer->setDebugBorderColor(static_cast<RGBA32>(0));
+        m_layer->setDebugBorderWidth(0);
     }
 }
 
@@ -632,9 +612,9 @@ void GraphicsLayerChromium::updateLayerBackgroundColor()
 
     // We never create the contents layer just for background color yet.
     if (m_backgroundColorSet)
-        setLayerBackgroundColor(*m_contentsLayer, m_backgroundColor);
+        m_contentsLayer->setBackgroundColor(m_backgroundColor);
     else
-        clearLayerBackgroundColor(*m_contentsLayer);
+        m_contentsLayer->setBackgroundColor(static_cast<RGBA32>(0));
 }
 
 void GraphicsLayerChromium::updateContentsVideo()
@@ -673,8 +653,8 @@ void GraphicsLayerChromium::setupContentsLayer(LayerChromium* contentsLayer)
         updateContentsRect();
 
         if (showDebugBorders()) {
-            setLayerBorderColor(*m_contentsLayer, Color(0, 0, 128, 180));
-            m_contentsLayer->setBorderWidth(1);
+            m_contentsLayer->setDebugBorderColor(Color(0, 0, 128, 180));
+            m_contentsLayer->setDebugBorderWidth(1);
         }
     }
     updateDebugIndicators();
