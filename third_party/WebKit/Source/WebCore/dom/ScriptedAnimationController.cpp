6b7077c3b08e2214dc06ffeec5825b8136875c1f
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-19 07:36:12 +0000

    2011-09-18  Ilya Tikhonovsky  <loislo@chromium.org>
            Web Inspector: requestAnimationFrame callbacks don't show up in the timeline panel.
            https://bugs.webkit.org/show_bug.cgi?id=67986
    
            Reviewed by Pavel Feldman.
    
            Test: inspector/timeline/timeline-animation-frame.html
    
    	WebCore:
    
            * English.lproj/localizedStrings.js:
            * bindings/v8/V8Proxy.cpp:
            (WebCore::V8Proxy::callFunction):
            (WebCore::V8Proxy::callFunctionWithoutFrame):
            (WebCore::V8Proxy::instrumentedCallFunction):
            * bindings/v8/V8Proxy.h:
            * bindings/v8/custom/V8CustomVoidCallback.cpp:
            (WebCore::invokeCallback):
            * dom/ScriptedAnimationController.cpp:
            (WebCore::ScriptedAnimationController::registerCallback):
            (WebCore::ScriptedAnimationController::cancelCallback):
            (WebCore::ScriptedAnimationController::serviceScriptedAnimations):
            * inspector/InspectorInstrumentation.cpp:
            (WebCore::InspectorInstrumentation::didRegisterAnimationFrameCallbackImpl):
            (WebCore::InspectorInstrumentation::didCancelAnimationFrameCallbackImpl):
            (WebCore::InspectorInstrumentation::willFireAnimationFrameEventImpl):
            (WebCore::InspectorInstrumentation::didFireAnimationFrameEventImpl):
            * inspector/InspectorInstrumentation.h:
            (WebCore::InspectorInstrumentation::willCallFunction):
            (WebCore::InspectorInstrumentation::didRegisterAnimationFrameCallback):
            (WebCore::InspectorInstrumentation::didCancelAnimationFrameCallback):
            (WebCore::InspectorInstrumentation::willFireAnimationFrameEvent):
            (WebCore::InspectorInstrumentation::didFireAnimationFrameEvent):
            * inspector/InspectorTimelineAgent.cpp:
            (WebCore::InspectorTimelineAgent::didRegisterAnimationFrameCallback):
            (WebCore::InspectorTimelineAgent::didCancelAnimationFrameCallback):
            (WebCore::InspectorTimelineAgent::willFireAnimationFrameEvent):
            (WebCore::InspectorTimelineAgent::didFireAnimationFrameEvent):
            * inspector/InspectorTimelineAgent.h:
            * inspector/TimelineRecordFactory.cpp:
            (WebCore::TimelineRecordFactory::createAnimationFrameCallbackData):
            * inspector/TimelineRecordFactory.h:
            * inspector/front-end/TimelineAgent.js:
            * inspector/front-end/TimelinePanel.js:
            (WebInspector.TimelinePanel):
            (WebInspector.TimelinePanel.prototype.get _recordStyles):
            (WebInspector.TimelinePanel.prototype._innerAddRecordToTimeline):
            (WebInspector.TimelinePanel.prototype._clearPanel):
            (WebInspector.TimelinePanel.FormattedRecord):
            (WebInspector.TimelinePanel.FormattedRecord.prototype._generatePopupContent):
            (WebInspector.TimelinePanel.FormattedRecord.prototype._getRecordDetails):
    
            LayoutTests:
    
            * inspector/timeline/timeline-animation-frame-expected.txt: Added.
            * inspector/timeline/timeline-animation-frame.html: Added.
            * inspector/timeline/timeline-enum-stability-expected.txt:
            * platform/chromium/inspector/timeline/timeline-enum-stability-expected.txt:
            * platform/qt/Skipped: inspector/timeline/timeline-animation-frame.html was added to the skip list.
            * inspector/timeline/timeline-test.js:
    
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ScriptedAnimationController.cpp
+++ third_party/WebKit/Source/WebCore/dom/ScriptedAnimationController.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "Element.h"
 #include "FrameView.h"
+#include "InspectorInstrumentation.h"
 #include "RequestAnimationFrameCallback.h"
 
 #if USE(REQUEST_ANIMATION_FRAME_TIMER)
@@ -76,6 +77,9 @@ ScriptedAnimationController::CallbackId ScriptedAnimationController::registerCal
     callback->m_id = id;
     callback->m_element = animationElement;
     m_callbacks.append(callback);
+
+    InspectorInstrumentation::didRegisterAnimationFrameCallback(m_document, id);
+
     if (!m_suspendCount)
         scheduleAnimation();
     return id;
@@ -86,6 +90,7 @@ void ScriptedAnimationController::cancelCallback(CallbackId id)
     for (size_t i = 0; i < m_callbacks.size(); ++i) {
         if (m_callbacks[i]->m_id == id) {
             m_callbacks[i]->m_firedOrCancelled = true;
+            InspectorInstrumentation::didCancelAnimationFrameCallback(m_document, id);
             m_callbacks.remove(i);
             return;
         }
@@ -120,7 +125,9 @@ void ScriptedAnimationController::serviceScriptedAnimations(DOMTimeStamp time)
             RequestAnimationFrameCallback* callback = callbacks[i].get();
             if (!callback->m_firedOrCancelled && (!callback->m_element || callback->m_element->renderer())) {
                 callback->m_firedOrCancelled = true;
+                InspectorInstrumentationCookie cookie = InspectorInstrumentation::willFireAnimationFrameEvent(m_document, callback->m_id);
                 callback->handleEvent(time);
+                InspectorInstrumentation::didFireAnimationFrameEvent(cookie);
                 firedCallback = true;
                 callbacks.remove(i);
                 break;
