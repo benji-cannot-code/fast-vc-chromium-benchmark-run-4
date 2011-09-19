42895a6e88745ee2fe3d2448c78f137cde218c59
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-19 23:28:38 +0000

    Fix for Crash in AutofillProfileSyncableService::ActOnChange BUG=96897 TEST=there should be no crash in error conditions. added a unit-test for that Review URL: http://codereview.chromium.org/7912002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101862 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/autofill_profile_syncable_service.cc
+++ chrome/browser/sync/glue/autofill_profile_syncable_service.cc
@@ -398,7 +398,7 @@ void AutofillProfileSyncableService::ActOnChange(
     LOG(WARNING) << "[AUTOFILL SYNC]"
                  << " Failed processing change:"
                  << " Error:" << error.message()
-                 << " Guid:" << change.profile()->guid();
+                 << " Guid:" << change.key();
   }
 }
 
