0a09456e80a8ff97ef2fc2edbc9cd79d8b092f01
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-19 20:32:52 +0000

    Remove toPrimitive from JSCell https://bugs.webkit.org/show_bug.cgi?id=67875
    Reviewed by Geoffrey Garen.
    
    Part of the refactoring process to un-virtualize JSCell.  We move
    all of the implicit functionality provided by the virtual toPrimitive method
    in JSCell to be explicit in JSValue::toPrimitive and JSCell:toPrimitive while
    also de-virtualizing JSCell::toPrimitive.
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
    * runtime/JSCell.cpp:
    (JSC::JSCell::toPrimitive):
    * runtime/JSCell.h:
    
    We replace JSNotAnObject::toPrimitive with defaultValue, which it overrides from
    JSObject.  This pushes the virtual method further down, enabling us to get rid
    of the virtual call in JSCell.  Eventually we'll probably have to deal with this
    again, but we'll cross that bridge when we come to it.
    * runtime/JSNotAnObject.cpp:
    (JSC::JSNotAnObject::defaultValue):
    * runtime/JSNotAnObject.h:
    * runtime/JSObject.h:
    * runtime/JSString.h:
    (JSC::JSValue::toPrimitive):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
@@ -427,6 +427,7 @@ namespace JSC {
         }
         unsigned length() { return m_length; }
 
+        JSValue toPrimitive(ExecState*, PreferredPrimitiveType) const;
         bool getStringPropertySlot(ExecState*, const Identifier& propertyName, PropertySlot&);
         bool getStringPropertySlot(ExecState*, unsigned propertyName, PropertySlot&);
         bool getStringPropertyDescriptor(ExecState*, const Identifier& propertyName, PropertyDescriptor&);
@@ -493,7 +494,6 @@ namespace JSC {
             }
         }
 
-        virtual JSValue toPrimitive(ExecState*, PreferredPrimitiveType) const;
         virtual bool getPrimitiveNumber(ExecState*, double& number, JSValue& value);
         virtual bool toBoolean(ExecState*) const;
         virtual double toNumber(ExecState*) const;
@@ -682,6 +682,13 @@ namespace JSC {
     inline bool isJSString(JSGlobalData* globalData, JSValue v) { return v.isCell() && v.asCell()->vptr() == globalData->jsStringVPtr; }
 
     // --- JSValue inlines ----------------------------
+
+    inline JSValue JSValue::toPrimitive(ExecState* exec, PreferredPrimitiveType preferredType) const
+    {
+        if (!isCell())
+            return asValue();
+        return asCell()->toPrimitive(exec, preferredType);
+    }
 
     inline UString JSValue::toString(ExecState* exec) const
     {
