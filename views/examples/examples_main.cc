1a5818be3466dabc31c9da72ff65698b4a05ada8
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-19 20:47:55 +0000

    views/examples: Fix a typo, do not add widget_example twice.
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/7949003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101820 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/examples_main.cc
+++ views/examples/examples_main.cc
@@ -134,7 +134,7 @@ void ExamplesMain::Init() {
   AddExample(&widget_example);
 
   MenuExample menu_example(this);
-  AddExample(&widget_example);
+  AddExample(&menu_example);
 
   window->Show();
 }
