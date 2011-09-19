e9f1c7c7f57af0a189c5a5e5577592e3386a2fe6
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-19 20:16:31 +0000

    Remove useless include to fix compile on branch 874.
    BUG=14471
    TBR=jeremya@chromium.org
    
    Review URL: http://codereview.chromium.org/7956004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/fullscreen_exit_bubble_controller_unittest.mm
+++ chrome/browser/ui/cocoa/fullscreen_exit_bubble_controller_unittest.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/fullscreen_exit_bubble_controller.h"
 
-#include "chrome/browser/ui/cocoa/cocoa_profile_test.h"
+#include "chrome/browser/ui/cocoa/cocoa_test_helper.h"
 #include "testing/gtest_mac.h"
 #include "ui/base/models/accelerator_cocoa.h"
 
