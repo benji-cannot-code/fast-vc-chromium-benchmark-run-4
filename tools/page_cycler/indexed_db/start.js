617b4feed5424fe2e72c9101b3857e125dd2789c
jnd@chromium.org <jnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 01:48:51 +0000

    Clean variable __navigated_to_report when starting tests.
    BUG=53140
    TEST=page cycler
    Review URL: http://codereview.chromium.org/7935012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101891 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/page_cycler/indexed_db/start.js
+++ tools/page_cycler/indexed_db/start.js
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 document.title = 'page cycler';
 
+document.cookie = '__navigated_to_report=0; path=/';
 document.cookie = '__pc_done=0; path=/';
 document.cookie = '__pc_timings=; path=/';
 
