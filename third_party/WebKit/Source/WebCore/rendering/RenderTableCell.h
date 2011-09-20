66e07ec75bb72498e733a8cb0d252be3d92b9a30
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-20 22:23:41 +0000

    https://bugs.webkit.org/show_bug.cgi?id=68480
    De-virtualize containingBlock() and make RenderView return 0 instead
    of itself to make the construction of normal loops that terminate via
    a null-check possible.
    
    Fix the only two places in the tree that needed null checks.
    
    Eliminating RenderTableCell::containingBlock() is fine since the base class
    does the same thing anyway.
    
    Reviewed by Simon Fraser.
    
    * editing/VisiblePosition.cpp:
    (WebCore::VisiblePosition::lineDirectionPointForBlockDirectionNavigation):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::containingBlock):
    * rendering/RenderObject.h:
    * rendering/RenderTableCell.cpp:
    * rendering/RenderTableCell.h:
    * rendering/RenderTreeAsText.cpp:
    (WebCore::RenderTreeAsText::writeRenderObject):
    * rendering/RenderView.cpp:
    * rendering/RenderView.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
@@ -138,8 +138,6 @@ private:
 
     virtual bool isTableCell() const { return true; }
 
-    virtual RenderBlock* containingBlock() const;
-
     virtual void willBeDestroyed();
 
     virtual void computeLogicalWidth();
