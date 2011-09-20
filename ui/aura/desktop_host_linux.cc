5ba16a16a9460288d5fee86989b644bcaf949d88
jhorwich@chromium.org <jhorwich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 17:55:38 +0000

    Include PointerMotionMask in event mask for linux
    BUG=none
    TEST=manual- drag windows in aura_demo under linux
    
    Review URL: http://codereview.chromium.org/7971005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101980 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/desktop_host_linux.cc
+++ ui/aura/desktop_host_linux.cc
@@ -60,7 +60,8 @@ DesktopHostLinux::DesktopHostLinux(const gfx::Rect& bounds)
   long event_mask = ButtonPressMask | ButtonReleaseMask |
                     KeyPressMask | KeyReleaseMask |
                     ExposureMask | VisibilityChangeMask |
-                    StructureNotifyMask | PropertyChangeMask;
+                    StructureNotifyMask | PropertyChangeMask |
+                    PointerMotionMask;
   XSelectInput(xdisplay_, xwindow_, event_mask);
   XFlush(xdisplay_);
 }
