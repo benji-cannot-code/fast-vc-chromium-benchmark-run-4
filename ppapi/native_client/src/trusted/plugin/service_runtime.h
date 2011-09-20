b7db8549dad78ccbc343a30ee8e98640677aede9
bsy@google.com <bsy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 21:26:16 +0000

    Chrome repo side changes for making crash detection / reporting more reliable.
    This CL depends on NaCl repo side CL being committed:
    
      http://codereview.chromium.org/7942008/
    
    so it also rolls DEPS to NaCl r6727
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2234 95968
    Review URL: http://codereview.chromium.org/7950004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102023 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.h
+++ ppapi/native_client/src/trusted/plugin/service_runtime.h
@@ -93,7 +93,8 @@ class PluginReverseInterface: public nacl::ReverseInterface {
  public:
   PluginReverseInterface(nacl::WeakRefAnchor* anchor,
                          Plugin* plugin,
-                         pp::CompletionCallback init_done_cb);
+                         pp::CompletionCallback init_done_cb,
+                         pp::CompletionCallback crash_cb);
 
   virtual ~PluginReverseInterface();
 
@@ -108,6 +109,8 @@ class PluginReverseInterface: public nacl::ReverseInterface {
   virtual bool OpenManifestEntry(nacl::string url_key, int32_t* out_desc);
 
   virtual bool CloseManifestEntry(int32_t desc);
+
+  virtual void ReportCrash();
 
  protected:
   virtual void Log_MainThreadContinuation(LogToJavaScriptConsoleResource* p,
@@ -134,6 +137,7 @@ class PluginReverseInterface: public nacl::ReverseInterface {
   bool shutting_down_;
 
   pp::CompletionCallback init_done_cb_;
+  pp::CompletionCallback crash_cb_;
 };
 
 //  ServiceRuntime abstracts a NativeClient sel_ldr instance.
@@ -142,7 +146,8 @@ class ServiceRuntime {
   // TODO(sehr): This class should also implement factory methods, using the
   // Start method below.
   ServiceRuntime(Plugin* plugin,
-                 pp::CompletionCallback init_done_cb);
+                 pp::CompletionCallback init_done_cb,
+                 pp::CompletionCallback crash_cb);
   // The destructor terminates the sel_ldr process.
   ~ServiceRuntime();
 
