71bc17faf9d985b1d861cd98de35e5349cf1f611
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 11:06:45 +0000

    Experiment: Add a sync IPC call for every image load.
    This will be reverted as soon as the page cyclers have ran.
    
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/7903003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101944 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/render_view_fake_resources_test.cc
+++ chrome/renderer/safe_browsing/render_view_fake_resources_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/shared_memory.h"
 #include "base/time.h"
 #include "chrome/common/render_messages.h"
+#include "chrome/renderer/content_settings_observer.h"
 #include "chrome/renderer/mock_render_process.h"
 #include "content/common/dom_storage_common.h"
 #include "content/common/main_function_params.h"
@@ -58,6 +59,9 @@ bool RenderViewFakeResourcesTest::Visit(RenderView* render_view) {
 }
 
 void RenderViewFakeResourcesTest::SetUp() {
+  // Make ContentSettingObserver bypass IPC.
+  ContentSettingsObserver::SetAllowAllImages(true);
+
   // Set up the renderer.  This code is largely adapted from
   // render_view_test.cc and renderer_main.cc.  Note that we use a
   // MockRenderProcess (because we don't need to use IPC for painting),
@@ -95,6 +99,8 @@ void RenderViewFakeResourcesTest::SetUp() {
 }
 
 void RenderViewFakeResourcesTest::TearDown() {
+  ContentSettingsObserver::SetAllowAllImages(false);
+
   // Try very hard to collect garbage before shutting down.
   GetMainFrame()->collectGarbage();
   GetMainFrame()->collectGarbage();
