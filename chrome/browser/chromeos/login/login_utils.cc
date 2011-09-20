d5b241051bf840d1f213ef2f3c8bf1262ea7e6f9
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 11:37:05 +0000

    [cros] Don't launch new browser session when browser is already shutting down.
    BUG=chromium-os:20504
    TEST=autotests
    
    
    Review URL: http://codereview.chromium.org/7951004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101946 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
 #include "chrome/browser/chromeos/cros/login_library.h"
 #include "chrome/browser/chromeos/cros/network_library.h"
@@ -1129,6 +1130,9 @@ void LoginUtils::Set(LoginUtils* mock) {
 
 void LoginUtils::DoBrowserLaunch(Profile* profile,
                                  LoginDisplayHost* login_host) {
+  if (browser_shutdown::IsTryingToQuit())
+    return;
+
   BootTimesLoader::Get()->AddLoginTimeMarker("BrowserLaunched", false);
 
   VLOG(1) << "Launching browser...";
