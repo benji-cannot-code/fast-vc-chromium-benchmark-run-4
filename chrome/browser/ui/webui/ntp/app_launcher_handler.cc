benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit d38b3e18642aca5391389c2f7d6f5f8b98ebf29e

ntp4: knock out TODO about not regenerating entire page
when an app page name is renamed (from a different NTP), don't refresh the entire NTP.

this also adds a tooltip to the navdots.

Also, don't jump the gun by calling javascript functions in response to Notifications if the apps haven't loaded yet.

BUG=none
TEST=manual


Review URL: http://codereview.chromium.org/7862018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100781 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -262,7 +262,7 @@ void AppLauncherHandler::RegisterMessages() {
 void AppLauncherHandler::Observe(int type,
                                  const NotificationSource& source,
                                  const NotificationDetails& details) {
-  if (ignore_changes_)
+  if (ignore_changes_ || !has_loaded_apps_)
     return;
 
   switch (type) {
@@ -290,8 +290,7 @@ void AppLauncherHandler::Observe(int type,
         break;
 
       highlight_app_id_ = *Details<const std::string>(details).ptr();
-      if (has_loaded_apps_)
-        SetAppToBeHighlighted();
+      SetAppToBeHighlighted();
       break;
     }
     case chrome::NOTIFICATION_EXTENSION_LOADED: {
@@ -342,23 +341,13 @@ void AppLauncherHandler::Observe(int type,
     // The promo may not load until a couple seconds after the first NTP view,
     // so we listen for the load notification and notify the NTP when ready.
     case chrome::NOTIFICATION_WEB_STORE_PROMO_LOADED:
-      if (web_ui_->tab_contents())
-        HandleGetApps(NULL);
+      // TODO(estade): try to get rid of this inefficient operation.
+      HandleGetApps(NULL);
       break;
     case chrome::NOTIFICATION_PREF_CHANGED: {
-      if (!web_ui_->tab_contents())
-        break;
-      // Handle app page renames.
-      std::string* pref_name = Details<std::string>(details).ptr();
-      if (*pref_name == prefs::kNTPAppPageNames) {
-        // TODO(estade): this doesn't need to regenerate the entire page.
-        HandleGetApps(NULL);
-      } else {
-        // Default prefs change handling.
-        DictionaryValue dictionary;
-        FillAppDictionary(&dictionary);
-        web_ui_->CallJavascriptFunction("appsPrefChangeCallback", dictionary);
-      }
+      DictionaryValue dictionary;
+      FillAppDictionary(&dictionary);
+      web_ui_->CallJavascriptFunction("appsPrefChangeCallback", dictionary);
       break;
     }
     case chrome::NOTIFICATION_EXTENSION_INSTALL_ERROR: {

chrome/browser/resources/ntp4/apps_page.js
@@ -138,7 +138,7 @@ cr.define('ntp4', function() {
         if (launchTypeButton == pressed) {
           chrome.send('setLaunchType', [app.appId, id]);
           // Manually update the launch type. We will only get
-          // appsPrefChangedCallback calls after changes to other NTP instances.
+          // appsPrefChangeCallback calls after changes to other NTP instances.
           app.appData.launch_type = id;
         }
       });
@@ -735,17 +735,6 @@ cr.define('ntp4', function() {
   };
 
   /**
-   * Callback invoked by chrome whenever an app preference changes.
-   * @param {Object} data An object with all the data on available
-   *     applications.
-   */
-  function appsPrefChangeCallback(data) {
-    for (var i = 0; i < data.apps.length; ++i) {
-      $(data.apps[i].id).appData = data.apps[i];
-    }
-  }
-
-  /**
    * Launches the specified app using the APP_LAUNCH_NTP_APP_RE_ENABLE
    * histogram. This should only be invoked from the AppLauncherHandler.
    * @param {String} appID The ID of the app.
@@ -762,7 +751,6 @@ cr.define('ntp4', function() {
     APP_LAUNCH: APP_LAUNCH,
     appNotificationChanged: appNotificationChanged,
     AppsPage: AppsPage,
-    appsPrefChangeCallback: appsPrefChangeCallback,
     launchAppAfterEnable: launchAppAfterEnable,
   };
 });
@@ -770,5 +758,4 @@ cr.define('ntp4', function() {
 // TODO(estade): update the content handlers to use ntp namespace instead of
 // making these global.
 var appNotificationChanged = ntp4.appNotificationChanged;
-var appsPrefChangeCallback = ntp4.appsPrefChangeCallback;
 var launchAppAfterEnable = ntp4.launchAppAfterEnable;

chrome/browser/resources/ntp4/nav_dot.js
@@ -38,8 +38,6 @@ cr.define('ntp4', function() {
       this.setAttribute('role', 'button');
 
       this.page_ = page;
-      this.title_ = title;
-      this.titleIsEditable_ = titleIsEditable;
 
       var selectionBar = this.ownerDocument.createElement('div');
       selectionBar.className = 'selection-bar';
@@ -52,6 +50,9 @@ cr.define('ntp4', function() {
       // Take the input out of the tab-traversal focus order.
       this.input_.disabled = true;
       this.appendChild(this.input_);
+
+      this.displayTitle = title;
+      this.titleIsEditable_ = titleIsEditable;
 
       this.addEventListener('click', this.onClick_);
       this.addEventListener('dblclick', this.onDoubleClick_);
@@ -78,6 +79,17 @@ cr.define('ntp4', function() {
      */
     get page() {
       return this.page_;
+    },
+
+    /**
+     * Sets/gets the display title.
+     * @type {String} title The display name for this nav dot.
+     */
+    get displayTitle() {
+      return this.title;
+    },
+    set displayTitle(title) {
+      this.title = this.input_.value = title;
     },
 
     /**
@@ -141,7 +153,7 @@ cr.define('ntp4', function() {
     onInputKeyDown_: function(e) {
       switch (e.keyIdentifier) {
         case 'U+001B':  // Escape cancels edits.
-          this.input_.value = this.title_;
+          this.input_.value = this.displayTitle;
         case 'Enter':  // Fall through.
           this.input_.blur();
           break;
@@ -155,8 +167,8 @@ cr.define('ntp4', function() {
      */
     onInputBlur_: function(e) {
       window.getSelection().removeAllRanges();
-      this.title_ = this.input_.value;
-      ntp4.saveAppPageName(this.page_, this.title_);
+      this.displayTitle = this.input_.value;
+      ntp4.saveAppPageName(this.page_, this.displayTitle);
       this.input_.disabled = true;
     },
 

chrome/browser/resources/ntp4/new_tab.js
@@ -413,6 +413,13 @@ cr.define('ntp4', function() {
     for (var i = 0; i < data.apps.length; ++i) {
       $(data.apps[i].id).appData = data.apps[i];
     }
+
+    // Set the App dot names. Skip the first and last dots (Most Visited and
+    // Bookmarks).
+    var dots = dotList.getElementsByClassName('dot');
+    for (var i = 1; i < dots.length - 2; ++i) {
+      dots[i].displayTitle = data.appPageNames[i - 1] || '';
+    }
   }
 
   /**

commit 4f9b238c1602606054b54adca44dc9a35f10cba3

ntp4 - fix new app animation regression
regressed by r100781

BUG=96648
TEST=see bug


Review URL: http://codereview.chromium.org/7944006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102025 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -262,6 +262,14 @@ void AppLauncherHandler::RegisterMessages() {
 void AppLauncherHandler::Observe(int type,
                                  const NotificationSource& source,
                                  const NotificationDetails& details) {
+  if (type == chrome::NOTIFICATION_APP_INSTALLED_TO_NTP &&
+          NewTabUI::NTP4Enabled()) {
+    highlight_app_id_ = *Details<const std::string>(details).ptr();
+    if (has_loaded_apps_)
+      SetAppToBeHighlighted();
+    return;
+  }
+
   if (ignore_changes_ || !has_loaded_apps_)
     return;
 
@@ -285,14 +293,7 @@ void AppLauncherHandler::Observe(int type,
       }
       break;
     }
-    case chrome::NOTIFICATION_APP_INSTALLED_TO_NTP: {
-      if (!NewTabUI::NTP4Enabled())
-        break;
 
-      highlight_app_id_ = *Details<const std::string>(details).ptr();
-      SetAppToBeHighlighted();
-      break;
-    }
     case chrome::NOTIFICATION_EXTENSION_LOADED: {
       const Extension* extension = Details<const Extension>(details).ptr();
       if (!extension->is_app())