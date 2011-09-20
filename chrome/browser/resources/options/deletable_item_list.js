7912c14261458bc32c0acb34c0601c06cdf5fbda
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 23:49:23 +0000

    Improve Tab-key navigation for editing Autofill addresses and credit cards.
    Also clean up some of the WebUI list code.
    
    BUG=78773
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7903001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/deletable_item_list.js
+++ chrome/browser/resources/options/deletable_item_list.js
@@ -53,6 +53,8 @@ cr.define('options', function() {
                                                 this.handleMouseDownUpOnClose_);
       this.closeButtonElement_.addEventListener('mouseup',
                                                 this.handleMouseDownUpOnClose_);
+      this.closeButtonElement_.addEventListener('focus',
+                                                this.handleFocus_.bind(this));
       this.appendChild(this.closeButtonElement_);
     },
 
@@ -73,6 +75,18 @@ cr.define('options', function() {
     set deletable(value) {
       this.deletable_ = value;
       this.closeButtonElement_.disabled = !value;
+    },
+
+    /**
+     * Called when a focusable child element receives focus. Selects this item
+     * in the list selection model.
+     * @private
+     */
+    handleFocus_: function() {
+      var list = this.parentNode;
+      var index = list.getIndexOfListItem(this);
+      list.selectionModel.selectedIndex = index;
+      list.selectionModel.anchorIndex = index;
     },
 
     /**
