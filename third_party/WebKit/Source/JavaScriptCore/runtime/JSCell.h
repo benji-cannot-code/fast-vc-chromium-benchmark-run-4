9e3853267fd03a7565ef4c37c733adb80ad40b30
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-20 01:21:51 +0000

    Removed lots of friend declarations from JSCell, so we can more effectively make use of private and protected.
    Reviewed by Sam Weinig.
    
    * runtime/JSCell.h: Removed MSVCBugWorkaround because it was a lot of
    confusion for not much safety.
    (JSC::JSCell::operator new): Made this public because it is used by a
    few clients, and not really dangerous.
    
    * runtime/JSObject.cpp:
    (JSC::JSObject::put):
    (JSC::JSObject::deleteProperty):
    (JSC::JSObject::defineGetter):
    (JSC::JSObject::defineSetter):
    (JSC::JSObject::getPropertySpecificValue):
    (JSC::JSObject::getOwnPropertyNames):
    (JSC::JSObject::seal):
    (JSC::JSObject::freeze):
    (JSC::JSObject::preventExtensions):
    (JSC::JSObject::removeDirect):
    (JSC::JSObject::createInheritorID):
    (JSC::JSObject::allocatePropertyStorage):
    (JSC::JSObject::getOwnPropertyDescriptor):
    * runtime/JSObject.h:
    (JSC::JSObject::getDirect):
    (JSC::JSObject::getDirectLocation):
    (JSC::JSObject::hasCustomProperties):
    (JSC::JSObject::hasGetterSetterProperties):
    (JSC::JSObject::isSealed):
    (JSC::JSObject::isFrozen):
    (JSC::JSObject::isExtensible):
    (JSC::JSObject::flattenDictionaryObject):
    (JSC::JSObject::finishCreation):
    (JSC::JSObject::prototype):
    (JSC::JSObject::setPrototype):
    (JSC::JSObject::inlineGetOwnPropertySlot):
    (JSC::JSCell::fastGetOwnProperty):
    (JSC::JSObject::putDirectInternal):
    (JSC::JSObject::putDirectWithoutTransition):
    (JSC::JSObject::transitionTo):
    (JSC::JSObject::visitChildrenDirect): Changed all use of m_structure to
    structure() / setStructure(), so we don't have to be a friend of JSCell.
    
    * runtime/Structure.h:
    (JSC::JSCell::setStructure): Added, to avoid direct access by JSObject
    to JSCell::m_structure.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
@@ -39,49 +39,23 @@ namespace JSC {
     class JSGlobalObject;
     class Structure;
 
-#if COMPILER(MSVC)
-    // If WTF_MAKE_NONCOPYABLE is applied to JSCell we end up with a bunch of
-    // undefined references to the JSCell copy constructor and assignment operator
-    // when linking JavaScriptCore.
-    class MSVCBugWorkaround {
-        WTF_MAKE_NONCOPYABLE(MSVCBugWorkaround);
-
-    protected:
-        MSVCBugWorkaround() { }
-        ~MSVCBugWorkaround() { }
-    };
-
-    class JSCell : MSVCBugWorkaround {
-#else
     class JSCell {
-        WTF_MAKE_NONCOPYABLE(JSCell);
-#endif
-
-        friend class ExecutableBase;
-        friend class GetterSetter;
-        friend class Heap;
-        friend class JSObject;
-        friend class JSPropertyNameIterator;
-        friend class JSString;
         friend class JSValue;
-        friend class JSAPIValueWrapper;
-        friend class JSGlobalData;
         friend class MarkedBlock;
-        friend class ScopeChainNode;
-        friend class Structure;
-        friend class StructureChain;
-        friend class RegExp;
 
+    public:
         enum CreatingEarlyCellTag { CreatingEarlyCell };
+        JSCell(CreatingEarlyCellTag);
+
+        enum VPtrStealingHackType { VPtrStealingHack };
+        explicit JSCell(VPtrStealingHackType) { }
+
+    public:
+        void* operator new(size_t, void* placementNewDestination) { return placementNewDestination; } // Used for initialization after GC allocation.
 
     protected:
-        enum VPtrStealingHackType { VPtrStealingHack };
-
-    private:
-        explicit JSCell(VPtrStealingHackType) { }
         JSCell(JSGlobalData&, Structure*);
-        JSCell(CreatingEarlyCellTag);
-        virtual ~JSCell();
+        virtual ~JSCell(); // Invoked by GC finalization.
 
     public:
         // Querying the type.
@@ -92,6 +66,7 @@ namespace JSC {
         bool isAPIValueWrapper() const;
 
         Structure* structure() const;
+        void setStructure(JSGlobalData&, Structure*);
 
         // Extracting the value.
         bool getString(ExecState* exec, UString&) const;
@@ -109,9 +84,6 @@ namespace JSC {
         virtual double toNumber(ExecState*) const;
         virtual UString toString(ExecState*) const;
         virtual JSObject* toObject(ExecState*, JSGlobalObject*) const;
-
-        // Garbage collection.
-        void* operator new(size_t, void* placementNewDestination) { return placementNewDestination; }
 
         virtual void visitChildren(SlotVisitor&);
 
@@ -157,11 +129,6 @@ namespace JSC {
         // Base implementation; for non-object classes implements getPropertySlot.
         virtual bool getOwnPropertySlot(ExecState*, const Identifier& propertyName, PropertySlot&);
         virtual bool getOwnPropertySlot(ExecState*, unsigned propertyName, PropertySlot&);
-        
-        // Note that the first two declarations of operator new have no corresponding implementation and 
-        // will cause link errors if you use them.
-        void* operator new(size_t, ExecState*);
-        void* operator new(size_t, JSGlobalData*);
         
         WriteBarrier<Structure> m_structure;
     };
@@ -373,7 +340,7 @@ namespace JSC {
 #endif
         return heap.allocate(sizeof(T));
     }
-        
+
 } // namespace JSC
 
 #endif // JSCell_h
