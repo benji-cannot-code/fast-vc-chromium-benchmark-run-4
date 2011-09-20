5ad956ba3d8c5f40a04fc1deef34da0fa1cdb1c9
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 00:40:50 +0000

    Reland 101545 - Moved code not relating to GPU scheduling out of GpuScheduler and into GpuCommandBufferStub.
    Reason for revert: Compile failure on Arm.
    
    This was mostly a refactor because the code was awkward.
    
    I also deleted the original gles2_demo since we have the gles2 book demos now.
    
    THings still to do are a common way of setting up the few objects involved in initializing a command buffer that is now more-or-less duplicated in the gles2 conformance tests, the gles2 demos, the command buffer stub and the in-process webgl context. I also want to completely remove the reference to the decoder from the scheduler.
    
    I tested WebGL on both windows and mac and saw no regressions. I checked the conformance tests, the gpu tests and am running by the try bots now. The gles2 demos still work.
    Review URL: http://codereview.chromium.org/7782041
    Review URL: http://codereview.chromium.org/7951008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101873 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/image_transport_surface_linux.cc
+++ content/common/gpu/image_transport_surface_linux.cc
@@ -426,11 +426,11 @@ ImageTransportHelper::~ImageTransportHelper() {
 }
 
 bool ImageTransportHelper::Initialize() {
-  gpu::GpuScheduler* scheduler = Scheduler();
-  if (!scheduler)
+  gpu::gles2::GLES2Decoder* decoder = Decoder();
+  if (!decoder)
     return false;
 
-  scheduler->SetResizeCallback(
+  decoder->SetResizeCallback(
        NewCallback(this, &ImageTransportHelper::Resize));
 
   return true;
@@ -506,6 +506,19 @@ gpu::GpuScheduler* ImageTransportHelper::Scheduler() {
     return NULL;
 
   return stub->scheduler();
+}
+
+gpu::gles2::GLES2Decoder* ImageTransportHelper::Decoder() {
+  GpuChannel* channel = manager_->LookupChannel(renderer_id_);
+  if (!channel)
+    return NULL;
+
+  GpuCommandBufferStub* stub =
+      channel->LookupCommandBuffer(command_buffer_id_);
+  if (!stub)
+    return NULL;
+
+  return stub->decoder();
 }
 
 #endif  // defined(USE_GPU)
--- content/common/gpu/image_transport_surface_linux.h
+++ content/common/gpu/image_transport_surface_linux.h
@@ -26,6 +26,10 @@ class GLSurface;
 
 namespace gpu {
 class GpuScheduler;
+
+namespace gles2 {
+class GLES2Decoder;
+}
 }
 
 class ImageTransportSurface {
@@ -72,6 +76,7 @@ class ImageTransportHelper : public IPC::Channel::Listener {
 
  private:
   gpu::GpuScheduler* Scheduler();
+  gpu::gles2::GLES2Decoder* Decoder();
 
   // IPC::Message handlers.
   void OnSetSurfaceACK(uint64 surface_id);
