88d1c6a5c237834cb124f96c4f3b5bb2ed3797f7
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 15:25:43 +0000

    Makes intranet urls autocomplete again. This patch gives the relevance that such sites had in the past. The other key it does it to make sure FixupExactSuggestion returns false so that we don't use the what you typed match.
    BUG=94805
    TEST=see bug
    R=pkasting@chromium.org
    
    Review URL: http://codereview.chromium.org/7756020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101959 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.h
+++ chrome/browser/autocomplete/autocomplete.h
@@ -45,7 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // --------------------------------------------------------------------|-----
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // Extension App (exact match)                                         | 1425
-// HistoryURL (exact or inline autocomplete match)                     | 1400
+// HistoryURL (exact or inline autocomplete match)                     | 1410
+// HistoryURL intranet url never visited                               | 1400
 // Search Primary Provider (past query in history within 2 days)       | 1399**
 // Search Primary Provider (what you typed)                            | 1300
 // HistoryURL (what you typed)                                         | 1200
@@ -70,7 +71,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // --------------------------------------------------------------------|-----
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // Extension App (exact match)                                         | 1425
-// HistoryURL (exact or inline autocomplete match)                     | 1400
+// HistoryURL (exact or inline autocomplete match)                     | 1410
+// HistoryURL intranet url never visited                               | 1400
 // Search Primary Provider (past query in history within 2 days)       | 1399**
 // HistoryURL (what you typed)                                         | 1200
 // Extension App (inexact match)                                       | 1175*~
@@ -95,7 +97,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // --------------------------------------------------------------------|-----
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // Extension App (exact match)                                         | 1425
-// HistoryURL (exact or inline autocomplete match)                     | 1400
+// HistoryURL (exact or inline autocomplete match)                     | 1410
+// HistoryURL intranet url never visited                               | 1400
 // HistoryURL (what you typed)                                         | 1200
 // Extension App (inexact match)                                       | 1175*~
 // Keyword (substituting, exact match)                                 | 1100
@@ -117,7 +120,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Keyword (non-substituting or in keyword UI mode, exact match)       | 1500
 // Keyword (substituting, exact match)                                 | 1450
 // Extension App (exact match)                                         | 1425
-// HistoryURL (exact or inline autocomplete match)                     | 1400
 // Search Primary Provider (past query in history within 2 days)       | 1399**
 // Search Primary Provider (what you typed)                            | 1300
 // Extension App (inexact match)                                       | 1175*~
--- chrome/browser/autocomplete/history_url_provider.h
+++ chrome/browser/autocomplete/history_url_provider.h
@@ -175,6 +175,14 @@ class HistoryURLProvider : public HistoryProvider {
   void QueryComplete(HistoryURLProviderParams* params_gets_deleted);
 
  private:
+  enum MatchType {
+    NORMAL,
+    WHAT_YOU_TYPED,
+    INLINE_AUTOCOMPLETE,
+    UNVISITED_INTRANET,  // An intranet site that has never been visited.
+  };
+  class VisitClassifier;
+
   ~HistoryURLProvider();
 
   // Returns the set of prefixes to use for prefixes_.
@@ -213,6 +221,7 @@ class HistoryURLProvider : public HistoryProvider {
   // when culling redirects to/from it).  Returns whether a match was promoted.
   bool FixupExactSuggestion(history::URLDatabase* db,
                             const AutocompleteInput& input,
+                            const VisitClassifier& classifier,
                             AutocompleteMatch* match,
                             history::HistoryMatches* matches) const;
 
