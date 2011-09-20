97aaecc406b04827be3928d2322752d88833ba5e
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-20 13:37:35 +0000

    Make WebViews in NSPopovers render as they would in active windows.
    The NSWindowDid{Become,Resign}KeyNotifications are not fired when NSPopovers
    are shown or hidden since they share key with the parent window. So WebView
    and WebHTMLView need to also observe the will order on/off screen notifications.
    
    https://webkit.org/b/68402
    rdar://problem/9754099
    
    Reviewed by John Sullivan.
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView _removeWindowObservers]): Remove order on/off screen notification obversers.
    (-[WebHTMLView addWindowObservers]): Add order on/off screen notification obversers.
    (-[WebHTMLView windowWillOrderOnScreen:]): Check if the window is already a key window,
    which can be the case for NSPopovers.
    (-[WebHTMLView windowWillOrderOffScreen:]): Remove the mouse moved observer.
    * WebView/WebView.mm:
    (-[WebView addWindowObserversForWindow:]): Add order off screen notification obverser.
    (-[WebView removeWindowObservers]): Remove order off screen notification obverser.
    (-[WebView _windowWillOrderOnScreen:]): Call _updateActiveState.
    (-[WebView _windowWillOrderOffScreen:]): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
@@ -3195,6 +3195,8 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
             name:NSWindowDidResignKeyNotification object:nil];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowWillOrderOnScreen:)
             name:WKWindowWillOrderOnScreenNotification() object:window];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowWillOrderOffScreen:)
+            name:WKWindowWillOrderOffScreenNotification() object:window];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidChangeResolution:)
             name:windowDidChangeResolutionNotification object:window];
     }
@@ -3210,6 +3212,8 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
             name:NSWindowDidResignKeyNotification object:nil];
         [[NSNotificationCenter defaultCenter] removeObserver:self
             name:WKWindowWillOrderOnScreenNotification() object:window];
+        [[NSNotificationCenter defaultCenter] removeObserver:self
+            name:WKWindowWillOrderOffScreenNotification() object:window];
         [[NSNotificationCenter defaultCenter] removeObserver:self
             name:windowDidChangeResolutionNotification object:window];
     }
@@ -3280,8 +3284,21 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
 
 - (void)_windowWillOrderOnScreen:(NSNotification *)notification
 {
+    // Update the active state here so WebViews in NSPopovers get the active state.
+    // This is needed because the normal NSWindowDidBecomeKeyNotification is not fired
+    // for NSPopover windows since they share key with their parent window.
+    [self _updateActiveState];
+
     if (![self shouldUpdateWhileOffscreen])
         [self setNeedsDisplay:YES];
+}
+
+- (void)_windowWillOrderOffScreen:(NSNotification *)notification
+{
+    // Update the active state here so WebViews in NSPopovers get the inactive state.
+    // This is needed because the normal NSWindowDidResignKeyNotification is not fired
+    // for NSPopover windows since they share key with their parent window.
+    [self _updateActiveState];
 }
 
 - (void)_windowWillClose:(NSNotification *)notification
