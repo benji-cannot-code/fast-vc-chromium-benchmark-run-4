ba0f505fdbf3a87ec1f96e0faa19dc853248b7ec
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 02:25:41 +0000

    Cleanup BugReportUI a little bit.
    R=rkc@chromium.org
    
    Review URL: http://codereview.chromium.org/7935002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101898 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/bug_report_ui.h
+++ chrome/browser/ui/webui/bug_report_ui.h
@@ -8,15 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "chrome/browser/ui/views/window.h"
 #include "chrome/browser/ui/webui/html_dialog_ui.h"
 
-namespace gfx {
-class Rect;
-}  // namespace gfx
-
 class Browser;
-class NSWindow;
 class TabContents;
 
 namespace browser {
