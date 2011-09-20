36130c8b175bc0353c8d6f635ab68d815598d5a4
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 06:07:11 +0000

    Get rid of content::DidEndMainMessageLoop since it was declared in content but defined in chrome, and called by chrome. Review URL: http://codereview.chromium.org/7948006
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101924 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_mac.mm
+++ chrome/browser/chrome_browser_main_mac.mm
@@ -25,15 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/resource/resource_bundle.h"
 
-namespace content {
-
-void DidEndMainMessageLoop() {
-  AppController* appController = [NSApp delegate];
-  [appController didEndMainMessageLoop];
-}
-
-}
-
 void RecordBreakpadStatusUMA(MetricsService* metrics) {
   metrics->RecordBreakpadRegistration(IsCrashReporterEnabled());
   metrics->RecordBreakpadHasDebugger(base::debug::BeingDebugged());
@@ -136,4 +127,9 @@ void ChromeBrowserMainPartsMac::PreMainMessageLoopStart() {
   // |-application:openFiles:|, since we already handle them directly.
   [[NSUserDefaults standardUserDefaults]
       setObject:@"NO" forKey:@"NSTreatUnknownArgumentsAsOpen"];
+}
+
+void ChromeBrowserMainPartsMac::DidEndMainMessageLoop() {
+  AppController* appController = [NSApp delegate];
+  [appController didEndMainMessageLoop];
 }
