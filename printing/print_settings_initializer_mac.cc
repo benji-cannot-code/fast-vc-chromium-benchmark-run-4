4e89483132f4d68e5c3b3f011d8181d3df65ca4f
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 03:59:05 +0000

    printing: Convert some wstrings to string16s.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7967004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101915 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/print_settings_initializer_mac.cc
+++ printing/print_settings_initializer_mac.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,9 +20,9 @@ void PrintSettingsInitializerMac::InitPrintSettings(
   DCHECK(print_settings);
 
   print_settings->set_printer_name(
-      base::SysCFStringRefToWide(PMPrinterGetName(printer)));
+      base::SysCFStringRefToUTF16(PMPrinterGetName(printer)));
   print_settings->set_device_name(
-      base::SysCFStringRefToWide(PMPrinterGetID(printer)));
+      base::SysCFStringRefToUTF16(PMPrinterGetID(printer)));
   print_settings->ranges = new_ranges;
 
   PMOrientation orientation = kPMPortrait;
