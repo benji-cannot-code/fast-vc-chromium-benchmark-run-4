5b74135cb8c6943b04f778c4161fb043e78a44a5
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-20 13:51:06 +0000

    Source/WebCore: Web Inspector: refactor ConsoleView, Drawer, ConsolePanel trio. Remove animation from drawer -> panel transition. https://bugs.webkit.org/show_bug.cgi?id=68155
    ConsoleView, ConsolePanel and Drawer are all Views with custom show/hide implementations
    and weird DOM element containment. As a result, we have a number of cases when switching
    console from full panel to drawer mode and back result in UI glitches.
    For the sake of code clarity and no glitches, I'd like to sacrifice the "grow into full
    screen" console animation.
    
    Reviewed by Tony Gentilcore.
    
    * inspector/front-end/ConsoleModel.js:
    (WebInspector.ConsoleModel):
    * inspector/front-end/ConsolePanel.js:
    (WebInspector.ConsolePanel):
    (WebInspector.ConsolePanel.prototype.get statusBarItems):
    (WebInspector.ConsolePanel.prototype.show):
    (WebInspector.ConsolePanel.prototype.hide):
    * inspector/front-end/ConsoleView.js:
    (WebInspector.ConsoleView):
    (WebInspector.ConsoleView.createFilterElement):
    (WebInspector.ConsoleView.get this):
    (WebInspector.ConsoleView.prototype.get statusBarItems):
    (WebInspector.ConsoleView.prototype.wasShown):
    * inspector/front-end/Drawer.js:
    (WebInspector.Drawer):
    (WebInspector.Drawer.prototype.get visible):
    (WebInspector.Drawer.prototype._constrainHeight):
    (WebInspector.Drawer.prototype.show.animationFinished):
    (WebInspector.Drawer.prototype.show):
    (WebInspector.Drawer.prototype.hide):
    (WebInspector.Drawer.prototype.resize):
    (WebInspector.Drawer.prototype._animationDuration):
    * inspector/front-end/inspector.html:
    * inspector/front-end/inspector.js:
    (WebInspector._createGlobalStatusBarItems):
    (WebInspector._toggleConsoleButtonClicked):
    (WebInspector.set attached):
    (WebInspector.windowResize):
    (WebInspector.documentKeyDown):
    
    LayoutTests: Web Inspector: refactor ConsoleView, Drawer, ConsolePanel trio.
    https://bugs.webkit.org/show_bug.cgi?id=68155
    
    Reviewed by Tony Gentilcore.
    
    * inspector/console/console-dir.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleModel.js
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  */
-WebInspector.ConsoleModel = function(drawer)
+WebInspector.ConsoleModel = function()
 {
     this.messages = [];
     this.warnings = 0;
--- third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
@@ -145,6 +145,8 @@ var WebInspector = {
                 WebInspector.userMetrics.panelShown(panelName);
             }
         }
+
+        this._toggleConsoleButton.disabled = this._currentPanel === WebInspector.panels.console;
     },
 
     _createPanels: function()
@@ -184,10 +186,13 @@ var WebInspector = {
 
         var anchoredStatusBar = document.getElementById("anchored-status-bar-items");
         anchoredStatusBar.appendChild(this._dockToggleButton.element);
-        anchoredStatusBar.appendChild(this.consoleView.toggleConsoleButton.element);
+
+        this._toggleConsoleButton = new WebInspector.StatusBarButton(WebInspector.UIString("Show console."), "console-status-bar-item");
+        this._toggleConsoleButton.addEventListener("click", this._toggleConsoleButtonClicked.bind(this), false);
+        anchoredStatusBar.appendChild(this._toggleConsoleButton.element);
+
         if (this.panels.elements)
             anchoredStatusBar.appendChild(this.panels.elements.nodeSearchButton.element);
-
         anchoredStatusBar.appendChild(this._settingsButton.element);
     },
 
@@ -204,6 +209,22 @@ var WebInspector = {
         } else {
             InspectorFrontendHost.requestDetachWindow();
             WebInspector.userMetrics.WindowUndocked.record();
+        }
+    },
+
+    _toggleConsoleButtonClicked: function()
+    {
+        if (this._toggleConsoleButton.disabled)
+            return;
+
+        this._toggleConsoleButton.toggled = !this._toggleConsoleButton.toggled;
+
+        if (this._toggleConsoleButton.toggled) {
+            this._toggleConsoleButton.title = WebInspector.UIString("Hide console.");
+            this.drawer.show(this.consoleView);
+        } else {
+            this._toggleConsoleButton.title = WebInspector.UIString("Show console.");
+            this.drawer.hide();
         }
     },
 
@@ -287,7 +308,7 @@ var WebInspector = {
             WebInspector.searchController.updateSearchLabel();
 
         if (WebInspector.drawer)
-            WebInspector.drawer.updateHeight();
+            WebInspector.drawer.resize();
     },
 
     _updateErrorAndWarningCounts: function()
@@ -496,8 +517,7 @@ WebInspector.doLoadedDone = function()
     this.console.addEventListener(WebInspector.ConsoleModel.Events.RepeatCountUpdated, this._updateErrorAndWarningCounts, this);
 
     this.drawer = new WebInspector.Drawer();
-    this.consoleView = new WebInspector.ConsoleView(this.drawer);
-    this.drawer.visibleView = this.consoleView;
+    this.consoleView = new WebInspector.ConsoleView();
 
     this.networkManager = new WebInspector.NetworkManager();
     this.resourceTreeModel = new WebInspector.ResourceTreeModel();
@@ -608,7 +628,7 @@ WebInspector.windowResize = function(event)
 {
     if (this.currentPanel())
         this.currentPanel().doResize();
-    this.drawer.doResize();
+    this.drawer.resize();
     this.toolbar.resize();
 }
 
@@ -794,10 +814,7 @@ WebInspector.documentKeyDown = function(event)
 
         case "U+001B": // Escape key
             event.preventDefault();
-            if (this.drawer.fullPanel)
-                return;
-
-            this.drawer.visible = !this.drawer.visible;
+            this._toggleConsoleButtonClicked();
             break;
 
         // Windows and Mac have two different definitions of [, so accept both.
