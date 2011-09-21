462549e25bf89a26c50bb289c61fd18ffa031101
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 15:39:53 +0000

    Web Inspector: add minimal error handling to the DOM breakpoints. https://bugs.webkit.org/show_bug.cgi?id=68537
    Reviewed by Yury Semikhatsky.
    
    * inspector/InspectorDOMAgent.h:
    * inspector/InspectorDOMDebuggerAgent.cpp:
    (WebCore::domTypeForName):
    (WebCore::InspectorDOMDebuggerAgent::setDOMBreakpoint):
    (WebCore::InspectorDOMDebuggerAgent::removeDOMBreakpoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.h
@@ -181,6 +181,8 @@ public:
     static Node* innerParentNode(Node*);
     static bool isWhitespace(Node*);
 
+    Node* assertNode(ErrorString*, int nodeId);
+
 private:
     InspectorDOMAgent(InstrumentingAgents*, InspectorPageAgent*, InspectorClient*, InspectorState*, InjectedScriptManager*);
 
@@ -192,7 +194,6 @@ private:
     typedef HashMap<RefPtr<Node>, int> NodeToIdMap;
     int bind(Node*, NodeToIdMap*);
     void unbind(Node*, NodeToIdMap*);
-    Node* assertNode(ErrorString*, int nodeId);
     Element* assertElement(ErrorString*, int nodeId);
     HTMLElement* assertHTMLElement(ErrorString*, int nodeId);
 
