367ed4930d5d6dbf56f8f83a6b5d2eb2b40d2e6a
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 00:52:52 +0000

    Show bubble and wrench menu item for sync error
    With this change we now show a bubble and menu item for actionable sync errors.
    
    BUG=92803
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7906013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -41,6 +41,7 @@ class NotificationSource;
 class Profile;
 class ProfileSyncFactory;
 class SigninManager;
+class SyncGlobalError;
 struct ChromeCookieDetails;
 
 namespace browser_sync {
@@ -486,6 +487,8 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
   // the user about them any more.
   void AcknowledgeSyncedTypes();
 
+  SyncGlobalError* sync_global_error() { return sync_global_error_.get(); }
+
  protected:
   // Used by test classes that derive from ProfileSyncService.
   virtual browser_sync::SyncBackendHost* GetBackendForTest();
@@ -673,6 +676,9 @@ class ProfileSyncService : public browser_sync::SyncFrontend,
   // This is the last |SyncProtocolError| we received from the server that had
   // an action set on it.
   browser_sync::SyncProtocolError last_actionable_error_;
+
+  // This is used to show sync errors in the wrench menu.
+  scoped_ptr<SyncGlobalError> sync_global_error_;
 
   DISALLOW_COPY_AND_ASSIGN(ProfileSyncService);
 };
--- chrome/browser/ui/toolbar/wrench_menu_model.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sync/sync_ui_util.h"
+#include "chrome/browser/sync/sync_global_error.h"
 #include "chrome/browser/tabs/tab_strip_model.h"
 #include "chrome/browser/task_manager/task_manager.h"
 #include "chrome/browser/ui/browser.h"
@@ -292,6 +293,9 @@ string16 WrenchMenuModel::GetLabelForCommandId(int command_id) const {
     case IDC_SHOW_SYNC_SETUP: {
       ProfileSyncService* service =
           browser_->GetProfile()->GetOriginalProfile()->GetProfileSyncService();
+      SyncGlobalError* error = service->sync_global_error();
+      if (error && error->HasCustomizedSyncMenuItem())
+        return error->MenuItemLabel();
       if (service->HasSyncSetupCompleted()) {
         std::string username = browser_->GetProfile()->GetPrefs()->GetString(
             prefs::kGoogleServicesUsername);
@@ -320,6 +324,20 @@ bool WrenchMenuModel::GetIconForCommandId(int command_id,
                 UpgradeDetector::UPGRADE_ICON_TYPE_MENU_ICON));
         return true;
       }
+      return false;
+    }
+    case IDC_SHOW_SYNC_SETUP: {
+      ProfileSyncService* service =
+          browser_->GetProfile()->GetOriginalProfile()->GetProfileSyncService();
+      SyncGlobalError* error = service->sync_global_error();
+      if (error && error->HasCustomizedSyncMenuItem()) {
+        int icon_id = error->MenuItemIconResourceID();
+        if (icon_id) {
+          *icon = rb.GetNativeImageNamed(icon_id);
+          return true;
+        }
+      }
+      return false;
     }
     default:
       break;
@@ -333,6 +351,16 @@ void WrenchMenuModel::ExecuteCommand(int command_id) {
   if (error) {
     error->ExecuteMenuItem(browser_);
     return;
+  }
+
+  if (command_id == IDC_SHOW_SYNC_SETUP) {
+    ProfileSyncService* service =
+        browser_->GetProfile()->GetOriginalProfile()->GetProfileSyncService();
+    SyncGlobalError* error = service->sync_global_error();
+    if (error && error->HasCustomizedSyncMenuItem()) {
+      error->ExecuteMenuItem(browser_);
+      return;
+    }
   }
 
   if (command_id == IDC_HELP_PAGE)
