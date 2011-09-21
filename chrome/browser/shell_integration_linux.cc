da18e342acd3c14fc03cbe1a69f5d27c1e5ea960
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 03:08:45 +0000

    Fix checking default protocol client on Linux
    When getting the default protocol client using the system xdg-settings tool
    fails, Chrome tries to use its own. The constant script name it looked for
    was mistyped (as xdg_settings) which meant this would always fail.
    
    This code path is not currently used by Chrome but will be soon.
    
    TEST=Check setting Chrome as default browser on Linux works; check registering
         Chrome to handle protocols works.
    
    
    Review URL: http://codereview.chromium.org/7972011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/shell_integration_linux.cc
+++ chrome/browser/shell_integration_linux.cc
@@ -221,9 +221,13 @@ const char* kDesktopKeysToDelete[] = {
   NULL
 };
 
-const char* kDesktopEntry = "Desktop Entry";
+const char kDesktopEntry[] = "Desktop Entry";
 
-const char* kXdgOpenShebang = "#!/usr/bin/env xdg-open";
+const char kXdgOpenShebang[] = "#!/usr/bin/env xdg-open";
+
+const char kXdgSettings[] = "xdg-settings";
+const char kXdgSettingsDefaultBrowser[] = "default-web-browser";
+const char kXdgSettingsDefaultSchemeHandler[] = "default-url-scheme-handler";
 
 }  // namespace
 
@@ -294,12 +298,12 @@ bool SetDefaultWebClient(const std::string& protocol) {
   scoped_ptr<base::Environment> env(base::Environment::Create());
 
   std::vector<std::string> argv;
-  argv.push_back("xdg-settings");
+  argv.push_back(kXdgSettings);
   argv.push_back("set");
   if (protocol.empty()) {
-    argv.push_back("default-web-browser");
+    argv.push_back(kXdgSettingsDefaultBrowser);
   } else {
-    argv.push_back("default-url-scheme-handler");
+    argv.push_back(kXdgSettingsDefaultSchemeHandler);
     argv.push_back(protocol);
   }
   argv.push_back(ShellIntegration::GetDesktopName(env.get()));
@@ -307,7 +311,7 @@ bool SetDefaultWebClient(const std::string& protocol) {
   int exit_code;
   bool ran_ok = LaunchXdgUtility(argv, &exit_code);
   if (ran_ok && exit_code == EXIT_XDG_SETTINGS_SYNTAX_ERROR) {
-    if (GetChromeVersionOfScript("xdg-settings", &argv[0])) {
+    if (GetChromeVersionOfScript(kXdgSettings, &argv[0])) {
       ran_ok = LaunchXdgUtility(argv, &exit_code);
     }
   }
@@ -325,12 +329,12 @@ ShellIntegration::DefaultWebClientState GetIsDefaultWebClient(
   scoped_ptr<base::Environment> env(base::Environment::Create());
 
   std::vector<std::string> argv;
-  argv.push_back("xdg-settings");
+  argv.push_back(kXdgSettings);
   argv.push_back("check");
   if (protocol.empty()) {
-    argv.push_back("default-web-browser");
+    argv.push_back(kXdgSettingsDefaultBrowser);
   } else {
-    argv.push_back("default-url-scheme-handler");
+    argv.push_back(kXdgSettingsDefaultSchemeHandler);
     argv.push_back(protocol);
   }
   argv.push_back(ShellIntegration::GetDesktopName(env.get()));
@@ -340,7 +344,7 @@ ShellIntegration::DefaultWebClientState GetIsDefaultWebClient(
   bool ran_ok = base::GetAppOutputWithExitCode(CommandLine(argv), &reply,
                                                &success_code);
   if (ran_ok && success_code == EXIT_XDG_SETTINGS_SYNTAX_ERROR) {
-    if (GetChromeVersionOfScript("xdg_settings", &argv[0])) {
+    if (GetChromeVersionOfScript(kXdgSettings, &argv[0])) {
       ran_ok = base::GetAppOutputWithExitCode(CommandLine(argv), &reply,
                                               &success_code);
     }
@@ -387,9 +391,9 @@ ShellIntegration::IsDefaultProtocolClient(const std::string& protocol) {
 // static
 bool ShellIntegration::IsFirefoxDefaultBrowser() {
   std::vector<std::string> argv;
-  argv.push_back("xdg-settings");
+  argv.push_back(kXdgSettings);
   argv.push_back("get");
-  argv.push_back("default-web-browser");
+  argv.push_back(kXdgSettingsDefaultBrowser);
 
   std::string browser;
   // We don't care about the return value here.
