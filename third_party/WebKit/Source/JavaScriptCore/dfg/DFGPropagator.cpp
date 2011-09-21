b5e097da7d46ca3492c04a96024587997283612f
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 22:17:06 +0000

    DFG does not support compiling functions as constructors https://bugs.webkit.org/show_bug.cgi?id=68500
    Reviewed by Oliver Hunt.
            
    This adds support for compiling constructors to the DFG. It's a
    1% speed-up on V8, mostly due to a 6% speed-up on early-boyer.
    It's also a 13% win on access-binary-trees, but it's neutral in
    the SunSpider and Kraken averages.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::parseBlock):
    * dfg/DFGCapabilities.h:
    (JSC::DFG::mightCompileFunctionForConstruct):
    (JSC::DFG::canCompileOpcode):
    * dfg/DFGNode.h:
    * dfg/DFGOperations.cpp:
    * dfg/DFGOperations.h:
    * dfg/DFGPropagator.cpp:
    (JSC::DFG::Propagator::propagateNodePredictions):
    (JSC::DFG::Propagator::performNodeCSE):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * runtime/Executable.cpp:
    (JSC::FunctionExecutable::compileOptimizedForConstruct):
    (JSC::FunctionExecutable::compileForConstructInternal):
    * runtime/Executable.h:
    (JSC::FunctionExecutable::compileForConstruct):
    (JSC::FunctionExecutable::compileFor):
    (JSC::FunctionExecutable::compileOptimizedFor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGPropagator.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGPropagator.cpp
@@ -534,6 +534,17 @@ private:
             changed |= mergeUse(node.child1(), PredictObjectUnknown | StrongPredictionTag);
             break;
         }
+            
+        case GetCallee: {
+            changed |= setPrediction(makePrediction(PredictObjectOther, StrongPrediction));
+            break;
+        }
+            
+        case CreateThis: {
+            changed |= mergeUse(node.child1(), PredictObjectUnknown | StrongPredictionTag);
+            changed |= setPrediction(makePrediction(PredictFinalObject, StrongPrediction));
+            break;
+        }
 
 #ifndef NDEBUG
         // These get ignored because they don't return anything.
@@ -1019,6 +1030,7 @@ private:
         case ArithMin:
         case ArithMax:
         case ArithSqrt:
+        case GetCallee:
             setReplacement(pureCSE(node));
             break;
             
