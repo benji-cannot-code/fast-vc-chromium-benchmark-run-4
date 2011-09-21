15828a9961e3e7eb56404e6ea730d29c5f65d6ef
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 23:15:04 +0000

    Fix Chromoting Mac to work with multiple monitors.
    BUG=96742
    TEST=See bug
    
    
    Review URL: http://codereview.chromium.org/7982037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102190 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/capturer_mac.cc
+++ remoting/host/capturer_mac.cc
@@ -378,13 +378,14 @@ void CapturerMac::GlBlitFast(const VideoFrameBuffer& buffer,
     const int y_offset = (buffer_height - 1) * buffer.bytes_per_row();
     for(SkRegion::Iterator i(last_invalid_region_); !i.done(); i.next()) {
       SkIRect copy_rect = i.rect();
-      copy_rect.intersect(clip_rect);
-      CopyRect(last_buffer_ + y_offset,
-               -buffer.bytes_per_row(),
-               buffer.ptr() + y_offset,
-               -buffer.bytes_per_row(),
-               4,  // Bytes for pixel for RGBA.
-               copy_rect);
+      if (copy_rect.intersect(clip_rect)) {
+        CopyRect(last_buffer_ + y_offset,
+                 -buffer.bytes_per_row(),
+                 buffer.ptr() + y_offset,
+                 -buffer.bytes_per_row(),
+                 4,  // Bytes for pixel for RGBA.
+                 copy_rect);
+      }
     }
   }
   last_buffer_ = buffer.ptr();
@@ -405,13 +406,14 @@ void CapturerMac::GlBlitFast(const VideoFrameBuffer& buffer,
     const int y_offset = (buffer_height - 1) * buffer.bytes_per_row();
     for(SkRegion::Iterator i(region); !i.done(); i.next()) {
       SkIRect copy_rect = i.rect();
-      copy_rect.intersect(clip_rect);
-      CopyRect(ptr + y_offset,
-         -buffer.bytes_per_row(),
-         buffer.ptr() + y_offset,
-         -buffer.bytes_per_row(),
-         4,  // Bytes for pixel for RGBA.
-         copy_rect);
+      if (copy_rect.intersect(clip_rect)) {
+        CopyRect(ptr + y_offset,
+           -buffer.bytes_per_row(),
+           buffer.ptr() + y_offset,
+           -buffer.bytes_per_row(),
+           4,  // Bytes for pixel for RGBA.
+           copy_rect);
+      }
     }
   }
   if (!glUnmapBufferARB(GL_PIXEL_PACK_BUFFER_ARB)) {
@@ -461,13 +463,14 @@ void CapturerMac::CgBlit(const VideoFrameBuffer& buffer,
   // http://crbug.com/92354
   for(SkRegion::Iterator i(region); !i.done(); i.next()) {
     SkIRect copy_rect = i.rect();
-    copy_rect.intersect(clip_rect);
-    CopyRect(display_base_address,
-             src_bytes_per_row,
-             buffer.ptr(),
-             buffer.bytes_per_row(),
-             src_bytes_per_pixel,
-             copy_rect);
+    if (copy_rect.intersect(clip_rect)) {
+      CopyRect(display_base_address,
+               src_bytes_per_row,
+               buffer.ptr(),
+               buffer.bytes_per_row(),
+               src_bytes_per_pixel,
+               copy_rect);
+    }
   }
 }
 
