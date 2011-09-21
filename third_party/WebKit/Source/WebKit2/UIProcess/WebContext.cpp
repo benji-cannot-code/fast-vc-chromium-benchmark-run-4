3abf771ce9b631941ab4ba2f63116b78c10ed475
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 23:25:35 +0000

    [WK2] UIProcess should check that WebProcess isn't sending unexpected file: URLs to it         https://bugs.webkit.org/show_bug.cgi?id=68573
            Reviewed by Anders Carlsson.
    
            * UIProcess/API/mac/WKView.mm:
            (maybeCreateSandboxExtensionFromPasteboard): Return a boolean, telling the caller whether
            an extension actually needed to be created
            (-[WKView performDragOperation:]): Tell process proxy when the process is going to get
            universal file read sandbox extension.
    
            * UIProcess/WebContext.cpp:
            (WebKit::WebContext::didPerformClientRedirect): Check the URLs.
            (WebKit::WebContext::didPerformServerRedirect): Ditto.
            (WebKit::WebContext::didUpdateHistoryTitle): Ditto.
            (WebKit::WebContext::getPluginPath): Ditto. Also, properly parse the URL - we can never
            assume that a string coming from WebProcess is a ParsedURLString.
    
            * UIProcess/WebPageProxy.cpp:
            (WebKit::WebPageProxy::reattachToWebProcessWithItem): Tell process proxy when the process
            is going to get universal file read sandbox extension.
            (WebKit::WebPageProxy::maybeInitializeSandboxExtensionHandle): Changed to return a boolean,
            telling the caller whether an extension actually needed to be created.
            (WebKit::WebPageProxy::loadURL): Tell process proxy about extension.
            (WebKit::WebPageProxy::loadURLRequest): Ditto.
            (WebKit::WebPageProxy::loadHTMLString): Tell process proxy if a file URL was used as a base
            one for a string. In this case, WebKit2 assumes that WebProcess has access to a subdirectory,
            (typically, one where error page resources live), and can load from it.
            (WebKit::WebPageProxy::loadAlternateHTMLString): Ditto.
            (WebKit::WebPageProxy::goForward): Tell process proxy about extension.
            (WebKit::WebPageProxy::goBack): Tell process proxy about extension.
            (WebKit::WebPageProxy::goToBackForwardItem): Tell process proxy about extension.
            (WebKit::WebPageProxy::didStartProvisionalLoadForFrame): Check the URL.
            (WebKit::WebPageProxy::didReceiveServerRedirectForProvisionalLoadForFrame): Ditto.
            (WebKit::WebPageProxy::didSameDocumentNavigationForFrame): Ditto.
            (WebKit::WebPageProxy::decidePolicyForNavigationAction): Ditto.
            (WebKit::WebPageProxy::decidePolicyForNewWindowAction): Ditto.
            (WebKit::WebPageProxy::decidePolicyForResponse): Ditto.
            (WebKit::WebPageProxy::didInitiateLoadForResource): Ditto.
            (WebKit::WebPageProxy::didSendRequestForResource): Ditto.
            (WebKit::WebPageProxy::didReceiveResponseForResource): Ditto.
            (WebKit::WebPageProxy::missingPluginButtonClicked): Ditto.
    
            * UIProcess/WebPageProxy.h: Changed initializeSandboxExtensionHandle() to return a bool,
            and renamed to maybeInitializeSandboxExtensionHandle (matching WKView counterpart).
    
            * UIProcess/WebProcessProxy.cpp:
            (WebKit::WebProcessProxy::WebProcessProxy): Initialize m_mayHaveUniversalFileReadSandboxExtension.
            It's going to be true if we ever granted an extension for "/".
            (WebKit::WebProcessProxy::willLoadHTMLStringWithBaseURL): Remember the path, we should expect
            that WebProcess will load subresources from it.
            (WebKit::WebProcessProxy::checkURLReceivedFromWebProcess): Check that it's reasonable to expect
            WebProcess send us a URL like this.
            (WebKit::WebProcessProxy::addBackForwardItem): Check the URLs.
    
            * UIProcess/WebProcessProxy.h: Added data members remembering what to expect from this process.
    
            * UIProcess/cf/WebPageProxyCF.cpp: (WebKit::WebPageProxy::restoreFromSessionStateData):
            Tell process proxy when the process is going to get universal file read sandbox extension.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
@@ -66,7 +66,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/RefCountedLeakCounter.h>
 #endif
 
-#define MESSAGE_CHECK(assertion) MESSAGE_CHECK_BASE(assertion, process()->connection())
+#define MESSAGE_CHECK(assertion) MESSAGE_CHECK_BASE(assertion, m_process->connection())
+#define MESSAGE_CHECK_URL(url) MESSAGE_CHECK_BASE(m_process->checkURLReceivedFromWebProcess(url), m_process->connection())
 
 using namespace WebCore;
 
@@ -440,7 +441,9 @@ void WebContext::didPerformClientRedirect(uint64_t pageID, const String& sourceU
     WebFrameProxy* frame = m_process->webFrame(frameID);
     MESSAGE_CHECK(frame);
     MESSAGE_CHECK(frame->page() == page);
-    
+    MESSAGE_CHECK_URL(sourceURLString);
+    MESSAGE_CHECK_URL(destinationURLString);
+
     m_historyClient.didPerformClientRedirect(this, page, sourceURLString, destinationURLString, frame);
 }
 
@@ -456,7 +459,9 @@ void WebContext::didPerformServerRedirect(uint64_t pageID, const String& sourceU
     WebFrameProxy* frame = m_process->webFrame(frameID);
     MESSAGE_CHECK(frame);
     MESSAGE_CHECK(frame->page() == page);
-    
+    MESSAGE_CHECK_URL(sourceURLString);
+    MESSAGE_CHECK_URL(destinationURLString);
+
     m_historyClient.didPerformServerRedirect(this, page, sourceURLString, destinationURLString, frame);
 }
 
@@ -469,6 +474,7 @@ void WebContext::didUpdateHistoryTitle(uint64_t pageID, const String& title, con
     WebFrameProxy* frame = m_process->webFrame(frameID);
     MESSAGE_CHECK(frame);
     MESSAGE_CHECK(frame->page() == page);
+    MESSAGE_CHECK_URL(url);
 
     m_historyClient.didUpdateHistoryTitle(this, page, title, url, frame);
 }
@@ -554,9 +560,11 @@ void WebContext::getPlugins(bool refresh, Vector<PluginInfo>& pluginInfos)
 
 void WebContext::getPluginPath(const String& mimeType, const String& urlString, String& pluginPath)
 {
+    MESSAGE_CHECK_URL(urlString);
+
     String newMimeType = mimeType.lower();
 
-    PluginModuleInfo plugin = pluginInfoStore().findPlugin(newMimeType, KURL(ParsedURLString, urlString));
+    PluginModuleInfo plugin = pluginInfoStore().findPlugin(newMimeType, KURL(KURL(), urlString));
     if (!plugin.path)
         return;
 
