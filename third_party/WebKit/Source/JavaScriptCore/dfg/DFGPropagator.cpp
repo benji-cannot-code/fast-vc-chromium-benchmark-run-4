34be8e5f13183f8fa22a78c85c1fdaa150663c17
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 23:46:09 +0000

    DFG JIT should be able to compile op_throw https://bugs.webkit.org/show_bug.cgi?id=68571
    Reviewed by Geoffrey Garen.
            
    This compiles op_throw in the simplest way possible: it's an OSR
    point back to the old JIT. This is a good step towards increasing
    coverage, particularly on Kraken, but it's neutral because the
    same functions that do throw also use some other unsupported
    opcodes.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::parseBlock):
    * dfg/DFGCapabilities.h:
    (JSC::DFG::canCompileOpcode):
    * dfg/DFGNode.h:
    * dfg/DFGPropagator.cpp:
    (JSC::DFG::Propagator::propagateNodePredictions):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGPropagator.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGPropagator.cpp
@@ -553,6 +553,8 @@ private:
         case Return:
         case CheckHasInstance:
         case Phi:
+        case Throw:
+        case ThrowReferenceError:
             break;
             
         // These get ignored because we don't have profiling for them, yet.
