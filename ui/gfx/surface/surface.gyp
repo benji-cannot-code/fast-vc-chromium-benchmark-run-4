74a3bb0d0ce6567421294fe57c04de92fad68d69
zhenghao@google.com <zhenghao@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 23:41:39 +0000

    Compile gl/ and surface/ on Android.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7981034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102204 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/surface/surface.gyp
+++ ui/gfx/surface/surface.gyp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'io_surface_support_mac.h',
         'surface_export.h',
         'transport_dib.h',
+        'transport_dib_android.cc',
         'transport_dib_linux.cc',
         'transport_dib_mac.cc',
         'transport_dib_win.cc',
--- ui/gfx/surface/transport_dib.h
+++ ui/gfx/surface/transport_dib.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "ui/gfx/surface/surface_export.h"
 
-#if defined(OS_WIN) || defined(OS_MACOSX)
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_ANDROID)
 #include "base/shared_memory.h"
 #endif
 
@@ -119,6 +119,20 @@ class SURFACE_EXPORT TransportDIB {
     static int fake_handle = 10;
     return fake_handle++;
   }
+#elif defined(OS_ANDROID)
+  typedef base::SharedMemoryHandle Handle;
+  typedef base::SharedMemoryHandle Id;
+
+  // Returns a default, invalid handle, that is meant to indicate a missing
+  // Transport DIB.
+  static Handle DefaultHandleValue() { return Handle(); }
+
+  // Returns a value that is ONLY USEFUL FOR TESTS WHERE IT WON'T BE
+  // ACTUALLY USED AS A REAL HANDLE.
+  static Handle GetFakeHandleForTest() {
+    static int fake_handle = 10;
+    return Handle(fake_handle++, false);
+  }
 #endif
 
   // Create a new TransportDIB, returning NULL on failure.
@@ -186,7 +200,7 @@ class SURFACE_EXPORT TransportDIB {
 
  private:
   TransportDIB();
-#if defined(OS_WIN) || defined(OS_MACOSX)
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_ANDROID)
   explicit TransportDIB(base::SharedMemoryHandle dib);
   base::SharedMemory shared_memory_;
   uint32 sequence_num_;
