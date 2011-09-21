e7a32bda96ea1e34bf13fead7ca0eec15122e54d
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 18:45:56 +0000

    <rdar://problem/9768483> REGRESSION: Crash in RenderBlock::removeFloatingObjectsBelow() https://bugs.webkit.org/show_bug.cgi?id=68550
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    Test: fast/multicol/null-lastFloat-in-removeFloatingObjectsBelow.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::removeFloatingObjectsBelow): Break out of the while() loop when the
    floating object set is empty, which can happen if there were no floats on previous lines
    (in which case, lastFloat is 0).
    
    LayoutTests: 
    
    * fast/multicol/null-lastFloat-in-removeFloatingObjectsBelow-expected.txt: Added.
    * fast/multicol/null-lastFloat-in-removeFloatingObjectsBelow.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95654 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -3291,6 +3291,8 @@ void RenderBlock::removeFloatingObjectsBelow(FloatingObject* lastFloat, int logi
         m_floatingObjects->remove(curr);
         ASSERT(!curr->m_originatingLine);
         delete curr;
+        if (floatingObjectSet.isEmpty())
+            break;
         curr = floatingObjectSet.last();
     }
 }
