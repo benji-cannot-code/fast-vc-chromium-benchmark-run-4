e946352cd31d046cafe12759f75e55d72acc5d98
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 17:00:42 +0000

    Made Authenticator::CompleteLogin, AuthenticateToLogin and AuthenticateToUnlock return void.
    They were anyhow hardcoded to return true and didn't play nicely with base::Bind.
    The result of an authentication is being reported through the callbacks
    OnLoginSuccess and OnLoginFailure.
    
    
    BUG=chromium-os:14054
    TEST=Everything should compile. browser_tests should be still passing.
    
    
    Review URL: http://codereview.chromium.org/7982025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/mock_authenticator.cc
+++ chrome/browser/chromeos/login/mock_authenticator.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-bool MockAuthenticator::AuthenticateToLogin(Profile* profile,
+void MockAuthenticator::AuthenticateToLogin(Profile* profile,
                                  const std::string& username,
                                  const std::string& password,
                                  const std::string& login_token,
@@ -16,26 +16,23 @@ bool MockAuthenticator::AuthenticateToLogin(Profile* profile,
     BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
         NewRunnableMethod(this, &MockAuthenticator::OnLoginSuccess,
                           GaiaAuthConsumer::ClientLoginResult(), false));
-    return true;
   }
   GoogleServiceAuthError error(
       GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS);
   BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
       NewRunnableMethod(this, &MockAuthenticator::OnLoginFailure,
                         LoginFailure::FromNetworkAuthFailure(error)));
-  return false;
 }
 
-bool MockAuthenticator::CompleteLogin(Profile* profile,
+void MockAuthenticator::CompleteLogin(Profile* profile,
                                       const std::string& username,
                                       const std::string& password) {
-  return false;
 }
 
-bool MockAuthenticator::AuthenticateToUnlock(const std::string& username,
+void MockAuthenticator::AuthenticateToUnlock(const std::string& username,
                                   const std::string& password) {
-  return AuthenticateToLogin(NULL /* not used */, username, password,
-                             std::string(), std::string());
+  AuthenticateToLogin(NULL /* not used */, username, password,
+                      std::string(), std::string());
 }
 
 void MockAuthenticator::LoginOffTheRecord() {
--- chrome/browser/chromeos/login/parallel_authenticator.h
+++ chrome/browser/chromeos/login/parallel_authenticator.h
@@ -85,7 +85,7 @@ class ParallelAuthenticator : public Authenticator,
   virtual ~ParallelAuthenticator();
 
   // Authenticator overrides.
-  virtual bool CompleteLogin(Profile* profile,
+  virtual void CompleteLogin(Profile* profile,
                              const std::string& username,
                              const std::string& password) OVERRIDE;
 
@@ -114,9 +114,7 @@ class ParallelAuthenticator : public Authenticator,
   // NOTE: We do not allow HOSTED accounts to log in.  In the event that
   // we are asked to authenticate valid HOSTED account creds, we will
   // call OnLoginFailure() with HOSTED_NOT_ALLOWED.
-  //
-  // Returns true if the attempt gets sent successfully and false if not.
-  virtual bool AuthenticateToLogin(Profile* profile,
+  virtual void AuthenticateToLogin(Profile* profile,
                                    const std::string& username,
                                    const std::string& password,
                                    const std::string& login_token,
@@ -126,7 +124,7 @@ class ParallelAuthenticator : public Authenticator,
   // authenticate to the cached credentials. This will never contact
   // the server even if it's online. The auth result is sent to
   // LoginStatusConsumer in a same way as AuthenticateToLogin does.
-  virtual bool AuthenticateToUnlock(const std::string& username,
+  virtual void AuthenticateToUnlock(const std::string& username,
                                     const std::string& password) OVERRIDE;
 
   // Initiates incognito ("browse without signing in") login.
