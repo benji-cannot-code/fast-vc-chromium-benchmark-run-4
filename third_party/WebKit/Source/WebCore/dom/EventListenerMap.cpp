2a1f207c955b894931e96a49a0b58f23347fb2af
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-21 13:08:19 +0000

    Protect against misuse of EventListenerIterator. https://bugs.webkit.org/show_bug.cgi?id=68364
    Patch by Andreas Kling <kling@webkit.org> on 2011-09-21
    Reviewed by Darin Adler.
    
    In debug mode, keep track of the number of active EventListenerIterators
    on an EventListenerMap, and assert that there are no iterators when the
    map is being modified.
    
    * dom/EventListenerMap.cpp:
    (WebCore::EventListenerMap::EventListenerMap):
    (WebCore::EventListenerMap::clear):
    (WebCore::EventListenerMap::add):
    (WebCore::EventListenerMap::remove):
    (WebCore::EventListenerMap::find):
    (WebCore::EventListenerMap::removeFirstEventListenerCreatedFromMarkup):
    (WebCore::EventListenerMap::copyEventListenersNotCreatedFromMarkupToTarget):
    (WebCore::EventListenerIterator::EventListenerIterator):
    (WebCore::EventListenerIterator::~EventListenerIterator):
    * dom/EventListenerMap.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventListenerMap.cpp
+++ third_party/WebKit/Source/WebCore/dom/EventListenerMap.cpp
@@ -45,6 +45,9 @@ using namespace WTF;
 namespace WebCore {
 
 EventListenerMap::EventListenerMap()
+#ifndef NDEBUG
+    : m_activeIteratorCount(0)
+#endif
 {
 }
 
@@ -69,6 +72,8 @@ bool EventListenerMap::contains(const AtomicString& eventType) const
 
 void EventListenerMap::clear()
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (m_hashMap) {
         deleteAllValues(*m_hashMap);
         m_hashMap.clear();
@@ -106,6 +111,8 @@ static bool addListenerToVector(EventListenerVector* vector, PassRefPtr<EventLis
 
 bool EventListenerMap::add(const AtomicString& eventType, PassRefPtr<EventListener> listener, bool useCapture)
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (m_singleEventListenerVector && m_singleEventListenerType != eventType) {
         // We already have a single (first) listener vector, and this event is not
         // of that type, so create the hash map and move the first listener vector there.
@@ -144,6 +151,8 @@ static bool removeListenerFromVector(EventListenerVector* listenerVector, EventL
 
 bool EventListenerMap::remove(const AtomicString& eventType, EventListener* listener, bool useCapture, size_t& indexOfRemovedListener)
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (!m_hashMap) {
         if (m_singleEventListenerType != eventType)
             return false;
@@ -169,6 +178,8 @@ bool EventListenerMap::remove(const AtomicString& eventType, EventListener* list
 
 EventListenerVector* EventListenerMap::find(const AtomicString& eventType)
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (m_hashMap) {
         EventListenerHashMap::iterator it = m_hashMap->find(eventType);
         if (it == m_hashMap->end())
@@ -201,6 +212,8 @@ static void removeFirstListenerCreatedFromMarkup(EventListenerVector* listenerVe
 
 void EventListenerMap::removeFirstEventListenerCreatedFromMarkup(const AtomicString& eventType)
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (m_hashMap) {
         EventListenerHashMap::iterator result = m_hashMap->find(eventType);
         ASSERT(result != m_hashMap->end());
@@ -240,6 +253,8 @@ static void copyListenersNotCreatedFromMarkupToTarget(const AtomicString& eventT
 
 void EventListenerMap::copyEventListenersNotCreatedFromMarkupToTarget(EventTarget* target)
 {
+    ASSERT(!m_activeIteratorCount);
+
     if (m_hashMap) {
         EventListenerHashMap::iterator end = m_hashMap->end();
         for (EventListenerHashMap::iterator it = m_hashMap->begin(); it != end; ++it)
@@ -273,11 +288,23 @@ EventListenerIterator::EventListenerIterator(EventTarget* target)
 
     m_map = &data->eventListenerMap;
 
+#ifndef NDEBUG
+    m_map->m_activeIteratorCount++;
+#endif
+
     if (m_map->m_hashMap) {
         m_mapIterator = m_map->m_hashMap->begin();
         m_mapEnd = m_map->m_hashMap->end();
     }
 }
+
+#ifndef NDEBUG
+EventListenerIterator::~EventListenerIterator()
+{
+    if (m_map)
+        m_map->m_activeIteratorCount--;
+}
+#endif
 
 EventListener* EventListenerIterator::nextListener()
 {
