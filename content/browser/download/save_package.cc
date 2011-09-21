dea6f300e6f9890911fce8ed26f672faa76e674c
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-21 17:27:49 +0000

    Make cancel remove cancelled download from active queues at time of cancel. Also add various tests required or enabled by this change.
    This changes two aspects of Cancel semantics (for downloads, not save page):
    a) Cancel can now be called anytime during the lifetime of a download, and
    b) if it is called before the history callback occurs, the download will be removed from the system (no where to store it persistently while waiting)
    
    BUG=85408
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=101510
    
    Review URL: http://codereview.chromium.org/7796014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102126 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/save_package.cc
+++ content/browser/download/save_package.cc
@@ -627,7 +627,7 @@ void SavePackage::Stop() {
 
   // Inform the DownloadItem we have canceled whole save page job.
   if (download_) {
-    download_->Cancel(false);
+    download_->Cancel();
     FinalizeDownloadEntry();
   }
 }
