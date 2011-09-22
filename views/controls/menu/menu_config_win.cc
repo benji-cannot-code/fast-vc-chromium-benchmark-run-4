bf93ab920094f002991243cd47e4da187da8f693
yosin@chromium.org <yosin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 05:30:46 +0000

    MenuConfig::Create passes HFONT to Font ctor but this HFONT isn't deleted. PlatformFontWin keeps copied HFONT by InitWIthCopyOfHFONT rather than keeping passed HFONT.
    BUG=96872
    TEST=Manual on Win7
    
    
    Review URL: http://codereview.chromium.org/7978021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102241 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/menu/menu_config_win.cc
+++ views/controls/menu/menu_config_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Vssym32.h>
 
 #include "base/logging.h"
+#include "base/win/scoped_gdi_object.h"
 #include "base/win/win_util.h"
 #include "ui/base/l10n/l10n_util_win.h"
 #include "ui/gfx/native_theme_win.h"
@@ -30,10 +31,11 @@ MenuConfig* MenuConfig::Create() {
   NONCLIENTMETRICS metrics;
   base::win::GetNonClientMetrics(&metrics);
   l10n_util::AdjustUIFont(&(metrics.lfMenuFont));
-  HFONT font = CreateFontIndirect(&metrics.lfMenuFont);
-  DLOG_ASSERT(font);
-  config->font = gfx::Font(font);
-
+  {
+    base::win::ScopedHFONT font(CreateFontIndirect(&metrics.lfMenuFont));
+    DLOG_ASSERT(font.Get());
+    config->font = gfx::Font(font);
+  }
   NativeTheme::ExtraParams extra;
   extra.menu_check.is_radio = false;
   extra.menu_check.is_selected = false;
