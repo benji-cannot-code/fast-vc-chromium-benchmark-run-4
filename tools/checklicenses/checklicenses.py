500c494efc2b9fec3d701ddb8b34ba805908fb09
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 18:00:53 +0000

    Make check_licenses about 5x faster.
    I've used a profiler and removed code that takes long time
    bug we don't use, and optimized several regular expressions.
    
    BUG=28291
    
    Review URL: http://codereview.chromium.org/7988008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102307 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -38,14 +38,12 @@ WHITELISTED_LICENSES = [
     'BSD (2 clause) MIT/X11 (BSD like)',
     'BSD (3 clause)',
     'BSD (3 clause) ISC',
-    'BSD (3 clause) LGPL (v2.1 or later) (with incorrect FSF address)',
+    'BSD (3 clause) LGPL (v2.1 or later)',
     'BSD (3 clause) MIT/X11 (BSD like)',
     'BSD (4 clause)',
     'BSD-like',
 
     # TODO(phajdan.jr): Make licensecheck not print BSD-like twice.
-    'BSD-like BSD (2 clause)',
-    'BSD-like BSD (3 clause)',
     'BSD-like MIT/X11 (BSD like)',
 
     'BSL (v1.0)',
@@ -55,7 +53,6 @@ WHITELISTED_LICENSES = [
     'LGPL',
     'LGPL (v2)',
     'LGPL (v2 or later)',
-    'LGPL (v2 or later) (with incorrect FSF address)',
     'LGPL (v2.1)',
     'LGPL (v3 or later)',
 
@@ -63,8 +60,7 @@ WHITELISTED_LICENSES = [
     'LGPL (v2,1 or later)',
 
     'LGPL (v2.1 or later)',
-    'LGPL (v2.1 or later) (with incorrect FSF address)',
-    'MPL (v1.0) LGPL (v2 or later) (with incorrect FSF address)',
+    'MPL (v1.0) LGPL (v2 or later)',
     'MPL (v1.1)',
     'MPL (v1.1) BSD-like',
     'MPL (v1.1) BSD-like GPL (unversioned/unknown version)',
@@ -130,11 +126,10 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'UNKNOWN',
     ],
     'native_client/toolchain': [
-        'BSD GPL (v2 or later) (with incorrect FSF address)',
+        'BSD GPL (v2 or later)',
         'BSD (2 clause) GPL (v2 or later)',
         'BSL (v1.0) GPL',
         'GPL',
-        'GPL (with incorrect FSF address)',
         'GPL (unversioned/unknown version)',
         'GPL (v2)',
 
@@ -142,7 +137,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'GPL (v2,)',
 
         'GPL (v2 or later)',
-        'GPL (v2 or later) (with incorrect FSF address)',
 
         # TODO(phajdan.jr): Make licensecheck not print the comma after 3.1.
         'GPL (v3.1,)',
@@ -154,7 +148,7 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'UNKNOWN',
     ],
     'net/tools/spdyshark': [
-        'GPL (v2 or later) (with incorrect FSF address)',
+        'GPL (v2 or later)',
         'UNKNOWN',
     ],
     'ppapi/c/documentation/check.sh': [
@@ -182,8 +176,8 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'GPL',
     ],
     'third_party/WebKit/Source/JavaScriptCore/tests/mozilla': [
+        'GPL',
         'GPL (unversioned/unknown version)',
-        'GPL (with incorrect FSF address)',
     ],
     'third_party/active_doc': [
         'UNKNOWN',
@@ -257,7 +251,7 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'UNKNOWN',
     ],
     'third_party/lcov/contrib/galaxy/genflat.pl': [
-        'GPL (v2 or later) (with incorrect FSF address)',
+        'GPL (v2 or later)',
     ],
     'third_party/leveldatabase/src/util/posix_logger.h': [
         'UNKNOWN',
@@ -285,7 +279,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     ],
     'third_party/libvpx/source/libvpx/examples/includes': [
         'GPL (v2 or later)',
-        'GPL (v2 or later) (with incorrect FSF address)',
     ],
     'third_party/libwebp': [
         'UNKNOWN',
