0750fd38417a116fd1c59c216ec22cf50aa6c7f2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-22 04:06:31 +0000

    Implement WebKit side of IDBFactory::getDatabaseNames https://bugs.webkit.org/show_bug.cgi?id=68037
    Patch by Joshua Bell <jsbell@chromium.org> on 2011-09-21
    Reviewed by Tony Chang.
    
    Interface changes and stub implementations for
    new IndexedDB IDBFactory.getDatabaseNames. This is
    part one of a two-sided patch.
    
    * public/WebIDBCallbacks.h:
    (WebKit::WebIDBCallbacks::onSuccess):
    * public/WebIDBFactory.h:
    (WebKit::WebIDBFactory::getDatabaseNames):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebIDBCallbacks.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebIDBCallbacks.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+class WebDOMStringList;
 class WebIDBCursor;
 class WebIDBDatabase;
 class WebIDBDatabaseError;
@@ -47,6 +48,7 @@ public:
     // For classes that follow the PImpl pattern, pass a const reference.
     // For the rest, pass ownership to the callee via a pointer.
     virtual void onError(const WebIDBDatabaseError&) { WEBKIT_ASSERT_NOT_REACHED(); }
+    virtual void onSuccess(const WebDOMStringList&) { WEBKIT_ASSERT_NOT_REACHED(); }
     virtual void onSuccess(WebIDBCursor*) { WEBKIT_ASSERT_NOT_REACHED(); }
     virtual void onSuccess(WebIDBDatabase*) { WEBKIT_ASSERT_NOT_REACHED(); }
     virtual void onSuccess(const WebIDBKey&) { WEBKIT_ASSERT_NOT_REACHED(); }
