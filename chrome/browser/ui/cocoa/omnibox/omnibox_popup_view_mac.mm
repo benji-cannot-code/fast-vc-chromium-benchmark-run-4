c15c84a111f907f77889f14de1d9911b6fd08957
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 00:16:13 +0000

    Prevent VoiceOver from announcing omnibox overlay windows.
    While typing into the omnibox, VoiceOver will announce
    "untitled window created"
    This is fairly disruptive and has been commented on by users.
    We should revisit how to make the underlying NSMatrix accessible, but in the meantime, set the window to NSAccessibilityUnknownRole so VoiceOver will ignore it.
    
    Similarly, status bubbles cause VoiceOver to announce "untitled window created". The content contained therein is exposed in other ways to the user of VoiceOver.
    
    BUG=63483
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7982033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.mm
@@ -345,6 +345,10 @@ void OmniboxPopupViewMac::CreatePopupIfNeeded() {
 
     [contentView addSubview:matrix];
     [popup_ setContentView:contentView];
+
+    // TODO(dtseng): Ignore until we provide NSAccessibility support.
+    [popup_ accessibilitySetOverrideValue:NSAccessibilityUnknownRole
+        forAttribute:NSAccessibilityRoleAttribute];
   }
 }
 
