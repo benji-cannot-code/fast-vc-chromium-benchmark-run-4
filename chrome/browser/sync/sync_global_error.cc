16061a680ead8f0827722018947823bb13c6ef25
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 01:58:02 +0000

    Update global error UI on sync error change
    BUG=92803
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7978013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_global_error.cc
+++ chrome/browser/sync/sync_global_error.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_observer.h"
 #include "chrome/browser/sync/sync_ui_util.h"
+#include "chrome/browser/ui/global_error_service.h"
+#include "chrome/browser/ui/global_error_service_factory.h"
 #include "chrome/common/net/gaia/google_service_auth_error.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -18,7 +20,8 @@ typedef GoogleServiceAuthError AuthError;
 using namespace sync_ui_util;
 
 SyncGlobalError::SyncGlobalError(ProfileSyncService* service)
-    : service_(service) {
+    : has_error_(false),
+      service_(service) {
   OnStateChanged();
 }
 
@@ -94,7 +97,13 @@ void SyncGlobalError::BubbleViewCancelButtonPressed() {
 }
 
 void SyncGlobalError::OnStateChanged() {
-  // TODO(sail): Update the wrench menu button.
+  bool new_has_error = GetStatusLabelsForSyncGlobalError(
+      service_, NULL, NULL, NULL) == SYNC_ERROR;
+  if (new_has_error != has_error_) {
+    has_error_ = new_has_error;
+    GlobalErrorServiceFactory::GetForProfile(
+        service_->profile())->NotifyErrorsChanged(this);
+  }
 }
 
 bool SyncGlobalError::HasCustomizedSyncMenuItem() {
--- chrome/browser/sync/sync_global_error.h
+++ chrome/browser/sync/sync_global_error.h
@@ -46,6 +46,7 @@ class SyncGlobalError : public GlobalError,
   bool HasCustomizedSyncMenuItem();
 
  private:
+  bool has_error_;
   ProfileSyncService* service_;
 
   DISALLOW_COPY_AND_ASSIGN(SyncGlobalError);
