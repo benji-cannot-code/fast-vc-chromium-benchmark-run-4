4f97a807454e2c857c8876fd278f457494db240e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 23:54:36 +0000

    Mark untrusted certificates as such in Linux UI.
    Some certificates are included in the certificate database in order to
    explicitly mark them as untrusted. In our current UI this isn't
    indicated and there's at least one, clearly fraudulent, CA in the list
    that looks like all the rest.
    
    This change causes these explicitly untrusted CAs to be marked with a
    red badge next to them.
    
    BUG=79549
    TEST=Open the certificates dialog on Linux/ChromeOS and look for untrusted CA certificates.
    
    
    Review URL: http://codereview.chromium.org/7272014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/certificate_tree.js
+++ chrome/browser/resources/options/certificate_tree.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,6 +23,14 @@ cr.define('options', function() {
 
     if (data.icon) {
       treeItem.icon = data.icon;
+    }
+
+    if (data.untrusted) {
+      var badge = document.createElement('span');
+      badge.setAttribute('class', 'certUntrusted');
+      badge.textContent = localStrings.getString("badgeCertUntrusted");
+      treeItem.labelElement.insertBefore(
+          badge, treeItem.labelElement.firstChild);
     }
 
     return treeItem;
