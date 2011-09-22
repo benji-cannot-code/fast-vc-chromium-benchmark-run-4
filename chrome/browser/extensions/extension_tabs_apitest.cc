42f4ff92d56d7ee6d824cce0fc1a077b72b8324e
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 03:59:00 +0000

    Make chrome.tabs.update's tabId parameter optional.
    BUG=97330
    TEST=added appropriate browser test
    
    
    Review URL: http://codereview.chromium.org/7981040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -77,6 +77,11 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs2) {
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "crud2.html")) << message_;
 }
 
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabUpdate) {
+  ASSERT_TRUE(StartTestServer());
+  ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "update.html")) << message_;
+}
+
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabPinned) {
   ASSERT_TRUE(StartTestServer());
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "pinned.html")) << message_;
