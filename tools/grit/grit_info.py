cb2ec4681d8b89a977511110c6fe7d7dad0e9f85
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 15:12:51 +0000

    Prepare for moving grit to its own open-source project.
    This involved:
    - Removing a grit->Chrome dependency that had been added in a unit test;
    - Moving the very project-specific resource_ids file to not sit in the same directory as the grit files (albeit in a hacky way; will fix this better later as per TODOs in code); and
    - Removing the hard-coding of "project base dir is two directories up from grit.py" and instead making that a parameter of resource_ids (as per TODO in code, this should later move to be a parameter of the .grd file).
    
    Also fixed a minor bug in relpath in misc.py, and fixed line length in grd_runner.py.
    
    The next steps will be to copy the tools/grit folder into http://code.google.com/p/grit-i18n, then in a single commit delete tools/grit from the Chrome repo and add it back in via DEPS.
    
    Follow-up fixes will address some of the TODOs, and later fixes will generalize a few things and add some features available in a different version of grit.
    
    BUG=97420
    TEST=trybots should catch any breakage here
    
    Review URL: http://codereview.chromium.org/7976026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102276 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/grit/grit_info.py
+++ tools/grit/grit_info.py
@@ -12,6 +12,7 @@ import os
 import posixpath
 import types
 import sys
+
 from grit import grd_reader
 from grit import util
 
@@ -94,7 +95,7 @@ def relpath(path, start=os.path.curdir):
 
     rel_list = [os.path.pardir] * (len(start_list)-i) + path_list[i:]
     if not rel_list:
-        return curdir
+        return os.path.curdir
     return os.path.join(*rel_list)
 ##############################################################################
 
@@ -140,8 +141,16 @@ def GritSourceFiles():
   grit_root_dir = relpath(os.path.dirname(__file__), os.getcwd())
   for root, dirs, filenames in os.walk(grit_root_dir):
     grit_src = [os.path.join(root, f) for f in filenames
-                if f.endswith('.py') or f == 'resource_ids']
+                if f.endswith('.py')]
     files.extend(grit_src)
+  # TODO(joi@chromium.org): Once we switch to specifying the
+  # resource_ids file via a .grd attribute, it should be considered an
+  # input of grit and this bit should no longer be necessary.
+  default_resource_ids = relpath(
+      os.path.join(grit_root_dir, '..', 'gritsettings', 'resource_ids'),
+      os.getcwd())
+  if os.path.exists(default_resource_ids):
+    files.append(default_resource_ids)
   return files
 
 
