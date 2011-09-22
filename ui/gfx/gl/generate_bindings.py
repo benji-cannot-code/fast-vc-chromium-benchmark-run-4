76af527e2c4ecda2f0d9a367520d1f6172c2d4a0
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-22 21:15:26 +0000

    Roll skia to 2303 Review URL: http://codereview.chromium.org/7977031
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102366 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/generate_bindings.py
+++ ui/gfx/gl/generate_bindings.py
@@ -17,6 +17,8 @@ GL_FUNCTIONS = [
 ['void', ['glBindAttribLocation'],
     'GLuint program, GLuint index, const char* name'],
 ['void', ['glBindBuffer'], 'GLenum target, GLuint buffer'],
+['void', ['glBindFragDataLocation'],
+    'GLuint program, GLuint colorNumber, const char* name'],
 ['void', ['glBindFragDataLocationIndexedARB'],
     'GLuint program, GLuint colorNumber, GLuint index, const char* name'],
 ['void', ['glBindFramebufferEXT', 'glBindFramebuffer'],
