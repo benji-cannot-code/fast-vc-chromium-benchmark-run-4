9baeabd9d94d36c93eee4b4c97f069032beeb19b
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 20:48:58 +0000

    Fix in-use updates for Chrome Frame.
    On in-use updates, make a copy of the old chrome launcher's IE low rights elevation policy prior to registering the new npchrome_frame.dll so that running instances of IE can still launch Chrome.
    
    In so doing, I also removed elevation policy addition/removal code from the installer so that npchrome_frame.dll's {un,}registration code is the one and only place where this is done.
    
    BUG=95810
    TEST=Install a previous version of GCF, run IE and visit some page that activates GCF, update to a version of GCF containing this fix, then try to visit another page that will activate GCF.  If all goes well, you won't see an IE security prompt.
    
    Review URL: http://codereview.chromium.org/7976045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/copy_reg_key_work_item_unittest.cc
+++ chrome/installer/util/copy_reg_key_work_item_unittest.cc
@@ -43,7 +43,8 @@ TEST_F(CopyRegKeyWorkItemTest, TestNoKey) {
     const std::wstring& key_path = key_paths[i];
     scoped_ptr<CopyRegKeyWorkItem> item(
         WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(), key_path,
-                                           destination_path_));
+                                           destination_path_,
+                                           WorkItem::ALWAYS));
     EXPECT_TRUE(item->Do());
     EXPECT_NE(ERROR_SUCCESS,
               key.Open(test_data_.root_key(), destination_path_.c_str(),
@@ -62,7 +63,8 @@ TEST_F(CopyRegKeyWorkItemTest, TestEmptyKey) {
   scoped_ptr<CopyRegKeyWorkItem> item(
       WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(),
                                          test_data_.empty_key_path(),
-                                         destination_path_));
+                                         destination_path_,
+                                         WorkItem::ALWAYS));
   EXPECT_TRUE(item->Do());
   RegistryTestData::ExpectEmptyKey(test_data_.root_key(),
                                    destination_path_.c_str());
@@ -80,7 +82,8 @@ TEST_F(CopyRegKeyWorkItemTest, TestNonEmptyKey) {
   scoped_ptr<CopyRegKeyWorkItem> item(
       WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(),
                                          test_data_.non_empty_key_path(),
-                                         destination_path_));
+                                         destination_path_,
+                                         WorkItem::ALWAYS));
   EXPECT_TRUE(item->Do());
   test_data_.ExpectMatchesNonEmptyKey(test_data_.root_key(),
                                       destination_path_.c_str());
@@ -89,6 +92,40 @@ TEST_F(CopyRegKeyWorkItemTest, TestNonEmptyKey) {
   EXPECT_NE(ERROR_SUCCESS,
             key.Open(test_data_.root_key(), destination_path_.c_str(),
                      KEY_READ));
+}
+
+// Test that existing data isn't overwritten.
+TEST_F(CopyRegKeyWorkItemTest, TestNoOverwrite) {
+  RegKey key;
+  // First copy the data into the dest.
+  EXPECT_EQ(ERROR_SUCCESS,
+            key.Create(test_data_.root_key(), destination_path_.c_str(),
+                       KEY_WRITE));
+  EXPECT_EQ(ERROR_SUCCESS,
+            SHCopyKey(test_data_.root_key(),
+                      test_data_.non_empty_key_path().c_str(), key.Handle(),
+                      0));
+  key.Close();
+
+  // Now copy the empty key into the dest, which should do nothing.
+  scoped_ptr<CopyRegKeyWorkItem> item(
+      WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(),
+                                         test_data_.empty_key_path(),
+                                         destination_path_,
+                                         WorkItem::IF_NOT_PRESENT));
+  EXPECT_TRUE(item->Do());
+
+  // Make sure it's all there.
+  test_data_.ExpectMatchesNonEmptyKey(test_data_.root_key(),
+                                      destination_path_.c_str());
+
+  // Rollback should do nothing.
+  item->Rollback();
+  item.reset();
+
+  // Make sure it's still all there.
+  test_data_.ExpectMatchesNonEmptyKey(test_data_.root_key(),
+                                      destination_path_.c_str());
 }
 
 // Test that copying an empty key over a key with data succeeds, and that
@@ -109,7 +146,8 @@ TEST_F(CopyRegKeyWorkItemTest, TestOverwriteAndRestore) {
   scoped_ptr<CopyRegKeyWorkItem> item(
       WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(),
                                          test_data_.empty_key_path(),
-                                         destination_path_));
+                                         destination_path_,
+                                         WorkItem::ALWAYS));
   EXPECT_TRUE(item->Do());
 
   // Make sure the dest is now empty.
@@ -132,7 +170,8 @@ TEST_F(CopyRegKeyWorkItemTest, TestIgnoreFailRollback) {
   scoped_ptr<CopyRegKeyWorkItem> item(
       WorkItem::CreateCopyRegKeyWorkItem(test_data_.root_key(),
                                          test_data_.empty_key_path(),
-                                         test_data_.non_empty_key_path()));
+                                         test_data_.non_empty_key_path(),
+                                         WorkItem::ALWAYS));
   item->set_ignore_failure(true);
   EXPECT_TRUE(item->Do());
   item->Rollback();
--- chrome_frame/chrome_tab.cc
+++ chrome_frame/chrome_tab.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_tab.h"  // NOLINT
 
 #include <atlsecurity.h>
+#include <objbase.h>
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
@@ -172,6 +173,13 @@ class ChromeTabModule : public CAtlDllModuleT<ChromeTabModule> {
         hr = registrar->AddReplacement(L"HIVE", L"HKCU");
       }
       DCHECK(SUCCEEDED(hr));
+    }
+
+    if (SUCCEEDED(hr)) {
+      // Add the Chrome Frame CLSID.
+      wchar_t cf_clsid[64];
+      StringFromGUID2(CLSID_ChromeFrame, &cf_clsid[0], arraysize(cf_clsid));
+      hr = registrar->AddReplacement(L"CHROME_FRAME_CLSID", &cf_clsid[0]);
     }
 
     return hr;
@@ -600,13 +608,10 @@ HRESULT RegisterActiveX(bool reg, bool is_system) {
 
 HRESULT RegisterElevationPolicy(bool reg, bool is_system) {
   HRESULT hr = S_OK;
-  if (reg && base::win::GetVersion() >= base::win::VERSION_VISTA) {
+  if (base::win::GetVersion() >= base::win::VERSION_VISTA) {
     // Register the elevation policy. This must succeed for Chrome Frame to
     // be able launch Chrome when running in low-integrity IE.
-    // Note that this is not done on unregistration, the installer will
-    // explicitly remove the policy on uninstall.
-    hr = _AtlModule.UpdateRegistryFromResourceS(IDR_CHROMEFRAME_ELEVATION,
-                                                reg);
+    hr = _AtlModule.UpdateRegistryFromResourceS(IDR_CHROMEFRAME_ELEVATION, reg);
     if (SUCCEEDED(hr)) {
       hr = RefreshElevationPolicy();
     }
