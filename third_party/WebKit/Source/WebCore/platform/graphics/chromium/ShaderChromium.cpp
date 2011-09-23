8d36cd0c573182c489f5df96be928288a0830e3a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-23 22:28:55 +0000

    [Chromium] Fix CSS 3D corner anti-aliasing. https://bugs.webkit.org/show_bug.cgi?id=68087
    Patch by David Reveman <reveman@chromium.org> on 2011-09-23
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Render sharp corners more correctly by adding bounding box
    edges to anti-aliasing shaders.
    
    Test: platform/chromium/compositing/3d-corners.html
    
    * platform/graphics/chromium/ShaderChromium.cpp:
    (WebCore::FragmentShaderRGBATexAlphaAA::getShaderString):
    (WebCore::FragmentShaderRGBATexClampAlphaAA::getShaderString):
    (WebCore::FragmentShaderRGBATexClampSwizzleAlphaAA::getShaderString):
    (WebCore::FragmentShaderRGBATexAlphaMaskAA::getShaderString):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::drawLayer):
    (WebCore::CCRenderSurface::drawSurface):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    * platform/graphics/chromium/cc/CCTiledLayerImpl.cpp:
    (WebCore::CCTiledLayerImpl::draw):
    (WebCore::CCTiledLayerImpl::drawTiles):
    * platform/graphics/chromium/cc/CCTiledLayerImpl.h:
    
    LayoutTests:
    
    Add a test to check sharp corner rendering of CSS 3D transformed
    elements.
    
    * platform/chromium-gpu-linux/compositing/flat-with-transformed-child-expected.checksum: Removed.
    * platform/chromium-gpu-linux/compositing/flat-with-transformed-child-expected.png: Added.
    * platform/chromium-gpu-linux/platform/chromium/compositing/3d-corners-expected.png: Added.
    * platform/chromium-gpu-linux/platform/chromium/compositing/backface-visibility-transformed-expected.png:
    * platform/chromium-gpu-linux/platform/chromium/compositing/perpendicular-layer-sorting-expected.png:
    * platform/chromium/compositing/3d-corners-expected.txt: Added.
    * platform/chromium/compositing/3d-corners.html: Added.
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ShaderChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ShaderChromium.cpp
@@ -306,7 +306,7 @@ String FragmentShaderRGBATexAlphaAA::getShaderString() const
         varying vec2 v_texCoord;
         uniform sampler2D s_texture;
         uniform float alpha;
-        uniform vec3 edge[4];
+        uniform vec3 edge[8];
         void main()
         {
             vec4 texColor = texture2D(s_texture, v_texCoord);
@@ -315,7 +315,11 @@ String FragmentShaderRGBATexAlphaAA::getShaderString() const
             float a1 = clamp(dot(edge[1], pos), 0.0, 1.0);
             float a2 = clamp(dot(edge[2], pos), 0.0, 1.0);
             float a3 = clamp(dot(edge[3], pos), 0.0, 1.0);
-            gl_FragColor = texColor * alpha * min(a0, a2) * min(a1, a3);
+            float a4 = clamp(dot(edge[4], pos), 0.0, 1.0);
+            float a5 = clamp(dot(edge[5], pos), 0.0, 1.0);
+            float a6 = clamp(dot(edge[6], pos), 0.0, 1.0);
+            float a7 = clamp(dot(edge[7], pos), 0.0, 1.0);
+            gl_FragColor = texColor * alpha * min(min(a0, a2) * min(a1, a3), min(a4, a6) * min(a5, a7));
         }
     );
 }
@@ -346,7 +350,7 @@ String FragmentShaderRGBATexClampAlphaAA::getShaderString() const
         uniform sampler2D s_texture;
         uniform float alpha;
         uniform vec4 fragmentTexTransform;
-        uniform vec3 edge[4];
+        uniform vec3 edge[8];
         void main()
         {
             vec2 texCoord = clamp(v_texCoord, 0.0, 1.0) * fragmentTexTransform.zw + fragmentTexTransform.xy;
@@ -356,7 +360,11 @@ String FragmentShaderRGBATexClampAlphaAA::getShaderString() const
             float a1 = clamp(dot(edge[1], pos), 0.0, 1.0);
             float a2 = clamp(dot(edge[2], pos), 0.0, 1.0);
             float a3 = clamp(dot(edge[3], pos), 0.0, 1.0);
-            gl_FragColor = texColor * alpha * min(a0, a2) * min(a1, a3);
+            float a4 = clamp(dot(edge[4], pos), 0.0, 1.0);
+            float a5 = clamp(dot(edge[5], pos), 0.0, 1.0);
+            float a6 = clamp(dot(edge[6], pos), 0.0, 1.0);
+            float a7 = clamp(dot(edge[7], pos), 0.0, 1.0);
+            gl_FragColor = texColor * alpha * min(min(a0, a2) * min(a1, a3), min(a4, a6) * min(a5, a7));
         }
     );
 }
@@ -369,7 +377,7 @@ String FragmentShaderRGBATexClampSwizzleAlphaAA::getShaderString() const
         uniform sampler2D s_texture;
         uniform float alpha;
         uniform vec4 fragmentTexTransform;
-        uniform vec3 edge[4];
+        uniform vec3 edge[8];
         void main()
         {
             vec2 texCoord = clamp(v_texCoord, 0.0, 1.0) * fragmentTexTransform.zw + fragmentTexTransform.xy;
@@ -379,7 +387,11 @@ String FragmentShaderRGBATexClampSwizzleAlphaAA::getShaderString() const
             float a1 = clamp(dot(edge[1], pos), 0.0, 1.0);
             float a2 = clamp(dot(edge[2], pos), 0.0, 1.0);
             float a3 = clamp(dot(edge[3], pos), 0.0, 1.0);
-            gl_FragColor = vec4(texColor.z, texColor.y, texColor.x, texColor.w) * alpha * min(a0, a2) * min(a1, a3);
+            float a4 = clamp(dot(edge[4], pos), 0.0, 1.0);
+            float a5 = clamp(dot(edge[5], pos), 0.0, 1.0);
+            float a6 = clamp(dot(edge[6], pos), 0.0, 1.0);
+            float a7 = clamp(dot(edge[7], pos), 0.0, 1.0);
+            gl_FragColor = vec4(texColor.z, texColor.y, texColor.x, texColor.w) * alpha * min(min(a0, a2) * min(a1, a3), min(a4, a6) * min(a5, a7));
         }
     );
 }
@@ -441,7 +453,7 @@ String FragmentShaderRGBATexAlphaMaskAA::getShaderString() const
         uniform sampler2D s_texture;
         uniform sampler2D s_mask;
         uniform float alpha;
-        uniform vec3 edge[4];
+        uniform vec3 edge[8];
         void main()
         {
             vec4 texColor = texture2D(s_texture, v_texCoord);
@@ -451,7 +463,11 @@ String FragmentShaderRGBATexAlphaMaskAA::getShaderString() const
             float a1 = clamp(dot(edge[1], pos), 0.0, 1.0);
             float a2 = clamp(dot(edge[2], pos), 0.0, 1.0);
             float a3 = clamp(dot(edge[3], pos), 0.0, 1.0);
-            gl_FragColor = vec4(texColor.x, texColor.y, texColor.z, texColor.w) * alpha * maskColor.w * min(a0, a2) * min(a1, a3);
+            float a4 = clamp(dot(edge[4], pos), 0.0, 1.0);
+            float a5 = clamp(dot(edge[5], pos), 0.0, 1.0);
+            float a6 = clamp(dot(edge[6], pos), 0.0, 1.0);
+            float a7 = clamp(dot(edge[7], pos), 0.0, 1.0);
+            gl_FragColor = vec4(texColor.x, texColor.y, texColor.z, texColor.w) * alpha * maskColor.w * min(min(a0, a2) * min(a1, a3), min(a4, a6) * min(a5, a7));
         }
     );
 }
