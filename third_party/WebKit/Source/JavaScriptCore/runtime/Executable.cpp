03fa23a7406f7f3604c5d6f00be9e46a7fc47e05
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-23 03:27:40 +0000

    PPC build fix, part 3.
    * runtime/Executable.cpp:
    (JSC::FunctionExecutable::compileForConstructInternal):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.cpp
@@ -551,6 +551,8 @@ JSObject* FunctionExecutable::compileForConstructInternal(ExecState* exec, Scope
         
         m_codeBlockForConstruct->setJITCode(m_jitCodeForConstruct, m_jitCodeForConstructWithArityCheck);
     }
+#else
+    UNUSED_PARAM(calleeArgsExec);
 #endif
 
 #if ENABLE(JIT)
