d85040477ff884d284abb9f9c3aab1477ca145d2
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 09:44:11 +0000

    Convert PyAuto hooks to multiprofile.
    Specific changes:
    Add a method to create a new profile to TestingAutomationProvider.
    Add a method to open a browser window with a given Profile.
    Fix methods in TestingAutomationProvider to use the profile from a browser, instead of the instance variable profile_.
    Add a test in ntp.py to make sure ntp pages don't leak between profiles, to test the new methods.
    
    The general idea here is that one can still use profile_ for most tests, but one has the option of creating multiple profiles and opening windows with a specific profile to test multiprofile features.
    
    BUG=87470
    TEST=new ntp.py test in CL.
    
    Review URL: http://codereview.chromium.org/7865026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/pyauto.py
+++ chrome/test/pyautolib/pyauto.py
@@ -1350,6 +1350,7 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
         u'windows': [ { u'index': 0,
                         u'height': 1134,
                         u'incognito': False,
+                        u'profile_path': u'Default',
                         u'fullscreen': False,
                         u'selected_tab': 0,
                         u'tabs': [ {
@@ -3154,6 +3155,18 @@ class PyUITest(pyautolib.PyUITestBase, unittest.TestCase):
         'channel_id': channel_id
     }
     self._GetResultFromJSONRequest(cmd_dict)
+
+  def OpenNewBrowserWindowWithNewProfile(self):
+    """Creates a new multi-profiles user, and then opens and shows a new
+    tabbed browser window with the new profile.
+
+    Raises:
+      pyauto_errors.JSONInterfaceError if the automation call returns an error.
+    """
+    cmd_dict = {  # Prepare command for the json interface
+      'command': 'OpenNewBrowserWindowWithNewProfile'
+    }
+    return self._GetResultFromJSONRequest(cmd_dict, windex=-1)
 
   ## ChromeOS section
 
