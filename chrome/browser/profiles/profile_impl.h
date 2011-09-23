7c0ffd15e28199a82370e9dcb1a1f9537f1089e5
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 09:27:53 +0000

    Fix ProfileImpl::InstallDefaultApps which used to do IO on the wrong thread.
    We first post the file enumeration to the FILE thread then get back to the UI
    thread to use the profile. InstallCrx makes it's own calls to the FILE thread
    to actually install the extensions.
    
    This should also make booting up more responsive as FILE io is done in parallel
    to the rest of the boot process.
    
    BUG=97581
    TEST=Fire up branded chrome in debug build and observe that the crash described in the bug does not appear.
    
    
    Review URL: http://codereview.chromium.org/7990002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -172,8 +172,6 @@ class ProfileImpl : public Profile,
 
   void RegisterComponentExtensions();
 
-  void InstallDefaultApps();
-
   ExtensionPrefValueMap* GetExtensionPrefValueMap();
 
   void CreateQuotaManagerAndClients();
