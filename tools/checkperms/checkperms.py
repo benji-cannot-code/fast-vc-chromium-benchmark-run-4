af217e01c80b7916733894c45d05e0ec53ba75a0
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 12:22:20 +0000

    Fix check_perms false positive introduced in r102477.
    TBR=mark@chromium.org
    Review URL: http://codereview.chromium.org/7966034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102481 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/checkperms/checkperms.py
+++ tools/checkperms/checkperms.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+#!/usr/bin/env python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -67,6 +67,11 @@ WHITELIST_FILES = [
     '/third_party/lcov/bin/genpng',
     '/third_party/lcov/bin/lcov',
     '/third_party/lcov/bin/mcov',
+    '/third_party/lcov-1.9/bin/gendesc',
+    '/third_party/lcov-1.9/bin/genhtml',
+    '/third_party/lcov-1.9/bin/geninfo',
+    '/third_party/lcov-1.9/bin/genpng',
+    '/third_party/lcov-1.9/bin/lcov',
     '/third_party/libxml/linux/xml2-config',
     '/third_party/lzma_sdk/executable/7za.exe',
     '/third_party/swig/linux/swig',
