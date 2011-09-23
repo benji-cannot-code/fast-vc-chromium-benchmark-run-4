1242c916e363b87cd40f9c3a9b45d10068efef15
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 18:27:08 +0000

    Fix lack of initialization for delegate delayed complete flag.
    BUG=94468
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7966016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102535 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_item.cc
+++ content/browser/download/download_item.cc
@@ -185,7 +185,8 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_temporary_(!info.save_info.file_path.empty()),
       all_data_saved_(false),
       opened_(false),
-      open_enabled_(true) {
+      open_enabled_(true),
+      delegate_delayed_complete_(false) {
   Init(true /* actively downloading */);
 }
 
@@ -216,7 +217,8 @@ DownloadItem::DownloadItem(DownloadManager* download_manager,
       is_temporary_(false),
       all_data_saved_(false),
       opened_(false),
-      open_enabled_(true) {
+      open_enabled_(true),
+      delegate_delayed_complete_(false) {
   Init(true /* actively downloading */);
 }
 
