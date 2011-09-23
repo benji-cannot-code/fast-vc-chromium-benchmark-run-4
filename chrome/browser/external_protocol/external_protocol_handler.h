8b41b9336afd3b1d23ced3ba4c3b8d9dca15abbc
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-23 05:04:38 +0000

    Open external application dialog should not show for Chrome.
    This change prevents the external application dialog from coming up if Chrome
    detects that it would just start itself. To determine this it uses the
    shell_integration functionality added for registerProtocolHandler to find out
    if Chrome is the default handler.
    
    BUG=90373
    TEST=Unit tests added.
    
    
    Review URL: http://codereview.chromium.org/7790021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102449 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_protocol/external_protocol_handler.h
+++ chrome/browser/external_protocol/external_protocol_handler.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "chrome/browser/shell_integration.h"
+
 class GURL;
 class PrefService;
 
@@ -22,6 +24,22 @@ class ExternalProtocolHandler {
     DONT_BLOCK,
     BLOCK,
     UNKNOWN,
+  };
+
+  // Delegate to allow unit testing to provide different behavior.
+  class Delegate {
+   public:
+    virtual ShellIntegration::DefaultProtocolClientWorker* CreateShellWorker(
+        ShellIntegration::DefaultWebClientObserver* observer,
+        const std::string& protocol) = 0;
+    virtual BlockState GetBlockState(const std::string& scheme) = 0;
+    virtual void BlockRequest() = 0;
+    virtual void RunExternalProtocolDialog(const GURL& url,
+                                           int render_process_host_id,
+                                           int routing_id) = 0;
+    virtual void LaunchUrlWithoutSecurityCheck(const GURL& url) = 0;
+    virtual void FinishedProcessingCheck() = 0;
+    virtual ~Delegate() {}
   };
 
   // Returns whether we should block a given scheme.
@@ -38,7 +56,14 @@ class ExternalProtocolHandler {
   // application is launched.
   // Must run on the UI thread.
   static void LaunchUrl(const GURL& url, int render_process_host_id,
-                        int tab_contents_id);
+                        int tab_contents_id) {
+      LaunchUrlWithDelegate(url, render_process_host_id, tab_contents_id, NULL);
+  }
+
+  // Version of LaunchUrl allowing use of a delegate to facilitate unit
+  // testing.
+  static void LaunchUrlWithDelegate(const GURL& url, int render_process_host_id,
+                                    int tab_contents_id, Delegate* delegate);
 
   // Creates and runs a External Protocol dialog box.
   // |url| - The url of the request.
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1270,6 +1270,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/sandboxed_extension_unpacker_unittest.cc',
         'browser/extensions/user_script_listener_unittest.cc',
         'browser/extensions/user_script_master_unittest.cc',
+        'browser/external_protocol/external_protocol_handler_unittest.cc',
         'browser/favicon/favicon_handler_unittest.cc',
         'browser/first_run/first_run_unittest.cc',
         'browser/geolocation/chrome_geolocation_permission_context_unittest.cc',
