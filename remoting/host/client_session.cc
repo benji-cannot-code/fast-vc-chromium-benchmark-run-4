593d4d029478a2af58337e0ad124bd7e6724acc3
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-24 02:21:40 +0000

    Call ClientSession::Disconnect when Chromoting client disconnects.
    BUG=84544
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7981046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102638 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/client_session.cc
+++ remoting/host/client_session.cc
@@ -121,8 +121,7 @@ void ClientSession::InjectMouseEvent(const MouseEvent& event) {
   }
 }
 
-void ClientSession::Disconnect() {
-  connection_->Disconnect();
+void ClientSession::OnDisconnected() {
   UnpressKeys();
   authenticated_ = false;
 }
