ae577eccee7197e5359039fe150f00ddbcdabae5
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-24 21:23:24 +0000

    DFG JIT should not eagerly initialize integer tags in the register file https://bugs.webkit.org/show_bug.cgi?id=68763
    Reviewed by Oliver Hunt.
    
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::exitSpeculativeWithOSR):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::ValueRecovery::dump):
    (JSC::DFG::OSRExit::OSRExit):
    (JSC::DFG::SpeculativeJIT::compile):
    (JSC::DFG::SpeculativeJIT::computeValueRecoveryFor):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::ValueRecovery::alreadyInRegisterFileAsUnboxedInt32):
    (JSC::DFG::OSRExit::operandForArgument):
    (JSC::DFG::OSRExit::operandForIndex):
    (JSC::DFG::SpeculativeJIT::computeValueRecoveryFor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
@@ -103,6 +103,8 @@ private:
 enum ValueRecoveryTechnique {
     // It's already in the register file at the right location.
     AlreadyInRegisterFile,
+    // It's already in the register file but unboxed.
+    AlreadyInRegisterFileAsUnboxedInt32,
     // It's in a register.
     InGPR,
     UnboxedInt32InGPR,
@@ -129,6 +131,13 @@ public:
     {
         ValueRecovery result;
         result.m_technique = AlreadyInRegisterFile;
+        return result;
+    }
+    
+    static ValueRecovery alreadyInRegisterFileAsUnboxedInt32()
+    {
+        ValueRecovery result;
+        result.m_technique = AlreadyInRegisterFileAsUnboxedInt32;
         return result;
     }
     
@@ -274,10 +283,14 @@ struct OSRExit {
     {
         return index - m_arguments.size();
     }
+    int operandForArgument(int argument) const
+    {
+        return argument - m_arguments.size() - RegisterFile::CallFrameHeaderSize;
+    }
     int operandForIndex(int index) const
     {
         if (index < (int)m_arguments.size())
-            return index - m_arguments.size() - RegisterFile::CallFrameHeaderSize;
+            return operandForArgument(index);
         return index - m_arguments.size();
     }
     
@@ -337,7 +350,6 @@ private:
     void compile(BasicBlock&);
 
     void checkArgumentTypes();
-    void initializeVariableTypes();
 
     bool isInteger(NodeIndex nodeIndex)
     {
@@ -591,11 +603,11 @@ private:
     int m_lastSetOperand;
     uint32_t m_bytecodeIndexForOSR;
     
-    ValueRecovery computeValueRecoveryFor(const ValueSource&);
+    ValueRecovery computeValueRecoveryFor(int operand, const ValueSource&);
 
     ValueRecovery computeValueRecoveryFor(int operand)
     {
-        return computeValueRecoveryFor(valueSourceForOperand(operand));
+        return computeValueRecoveryFor(operand, valueSourceForOperand(operand));
     }
 };
 
