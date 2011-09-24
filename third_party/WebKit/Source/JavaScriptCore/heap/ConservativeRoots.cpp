4e9e16389bf5e611ae27d68a20de8ea9f2d638d9
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-24 22:53:29 +0000

    More build fixage.
    * heap/ConservativeRoots.cpp: Our system of #includes, it is chaos.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/ConservativeRoots.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/ConservativeRoots.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ConservativeRoots.h"
 
 #include "JSCell.h"
+#include "JSObject.h"
 #include "JettisonedCodeBlocks.h"
 #include "Structure.h"
 
