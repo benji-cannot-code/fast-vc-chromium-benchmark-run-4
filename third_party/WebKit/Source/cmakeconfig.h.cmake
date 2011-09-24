cab28ffe32f8ce4d05ee38179632b0d6fe0e4b63
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-24 05:42:09 +0000

    Add JSVALUE32_64 support to DFG JIT https://bugs.webkit.org/show_bug.cgi?id=67460
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-09-23
    Reviewed by Gavin Barraclough.
    
    Add cmake options to enable DFG JIT compilation for EFL port
    
    * Source/cmake/OptionsEfl.cmake:
    * Source/cmakeconfig.h.cmake:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmakeconfig.h.cmake
+++ third_party/WebKit/Source/cmakeconfig.h.cmake
@@ -45,5 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #cmakedefine01 ENABLE_XHTMLMP
 #cmakedefine01 ENABLE_XPATH
 #cmakedefine01 ENABLE_XSLT
+#cmakedefine01 ENABLE_DFG_JIT
 
 #endif /* CMAKECONFIG_H */
