88aa66e7890070484bc4bc54ae9a47f533950f96
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 23:36:09 +0000

    Expose use-more-webui flag in chrome://flags and tweak cert viewer WebUI
    We're planning on enabling some WebUI dialogs by default on ChromeOS soon, so
    expose in flags to make it easier for people to test.
    
    Also tweak the certificate viewer UI a little to make it look and behave more
    like the original:
     - make it bigger (so it's generally big enough to fit the contents)
     - automatically select the main certificate in the details pane
     - scroll the details windows to the top
     - add a border around the details windows (like the original)
     - shrink hierarchy window a bit - it will rarely have more than 3 lines
     - fix spacing and indendation on the main tab
    
    BUG=98027
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8028024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102831 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/certificate_viewer.cc
+++ chrome/browser/ui/webui/certificate_viewer.cc
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Default width/height of the dialog.
-const int kDefaultWidth = 450;
-const int kDefaultHeight = 450;
+const int kDefaultWidth = 530;
+const int kDefaultHeight = 600;
 
 }  // namespace
 
