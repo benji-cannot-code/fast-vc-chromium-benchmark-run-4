a7a5985ccc7ab836cada16430eee41f2176b5c1a
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 23:00:37 +0000

    Fix WebKitTestRunner builds for Cairo, Windows and Qt.
    * WebKitTestRunner/cairo/TestInvocationCairo.cpp:
    (WTR::TestInvocation::dumpPixelsAndCompareWithExpected):
    * WebKitTestRunner/win/TestInvocationWin.cpp:
    (WTR::TestInvocation::dumpPixelsAndCompareWithExpected):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/win/TestInvocationWin.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/win/TestInvocationWin.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WTR {
 
 #if !USE(CG) && !USE(CAIRO)
-void TestInvocation::dumpPixelsAndCompareWithExpected(WKImageRef)
+void TestInvocation::dumpPixelsAndCompareWithExpected(WKImageRef, WKArrayRef)
 {
 }
 #endif
