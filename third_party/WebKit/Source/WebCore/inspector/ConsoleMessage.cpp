fa7816e267ad6520ae69ce44323845f8ddab4da5
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 20:06:55 +0000

    Web Inspector: XMLHttpRequest console logging messages should link to network panel when possible. https://bugs.webkit.org/show_bug.cgi?id=67399
    Reviewed by Pavel Feldman.
    
    * inspector/ConsoleMessage.cpp:
    (WebCore::ConsoleMessage::ConsoleMessage):
    * inspector/ConsoleMessage.h:
    * inspector/InspectorConsoleAgent.cpp:
    (WebCore::InspectorConsoleAgent::resourceRetrievedByXMLHttpRequest):
    * inspector/InspectorConsoleAgent.h:
    * inspector/InspectorInstrumentation.cpp:
    (WebCore::InspectorInstrumentation::resourceRetrievedByXMLHttpRequestImpl):
    * inspector/front-end/ConsoleMessage.js:
    (WebInspector.ConsoleMessage.prototype._formatMessage.else.else.linkifier):
    (WebInspector.ConsoleMessage.prototype._formatMessage):
    * inspector/front-end/ResourcesPanel.js:
    (WebInspector.ResourcesPanel.prototype.showAnchorLocation):
    * inspector/front-end/inspector.js:
    (WebInspector.linkifyStringAsFragmentWithCustomLinkifier):
    (WebInspector.linkifyStringAsFragment):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
+++ third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-ConsoleMessage::ConsoleMessage(MessageSource s, MessageType t, MessageLevel l, const String& m, unsigned li, const String& u)
+ConsoleMessage::ConsoleMessage(MessageSource s, MessageType t, MessageLevel l, const String& m, unsigned li, const String& u, const String& requestId)
     : m_source(s)
     , m_type(t)
     , m_level(l)
@@ -53,6 +53,7 @@ ConsoleMessage::ConsoleMessage(MessageSource s, MessageType t, MessageLevel l, c
     , m_line(li)
     , m_url(u)
     , m_repeatCount(1)
+    , m_requestId(requestId)
 {
 }
 
