4481a064bbc5b949c9127bee947d653e1305e015
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 22:47:42 +0000

    fix test_shell_tests
    I broke these in r102763 when we stopped having a user agent always
    defined. This change sets the user agent in a couple of test_suites'
    Setup() methods.
    
    R=mihaip@chromium.org
    BUG=90442
    TEST=
    
    Review URL: http://codereview.chromium.org/8048003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102826 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_shell_test.cc
+++ webkit/tools/test_shell/test_shell_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_util.h"
+#include "webkit/glue/user_agent.h"
+#include "webkit/glue/webkit_glue.h"
 
 GURL TestShellTest::GetTestURL(const FilePath& test_case_path,
                                const std::string& test_case) {
@@ -25,6 +27,8 @@ void TestShellTest::SetUp() {
   // Make a test shell for use by the test.
   CreateEmptyWindow();
   test_shell_->Show(WebKit::WebNavigationPolicyNewWindow);
+  webkit_glue::SetUserAgent(webkit_glue::BuildUserAgentFromProduct(
+      "TestShell/0.0.0.0"), false);
 
   // Point data_dir_ to the root of the test case dir
   ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &data_dir_));
