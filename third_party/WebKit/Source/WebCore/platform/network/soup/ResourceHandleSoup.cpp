278e9f3d4a22f259a686f2e574771cf67f7304e0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 09:05:02 +0000

    [GTK] Fix coding style bits in ResourceHandleSoup.cpp https://bugs.webkit.org/show_bug.cgi?id=68634
    Patch by Sergio Villar Senin <svillar@igalia.com> on 2011-09-26
    Reviewed by Martin Robinson.
    
    No new tests needed.
    
    * platform/network/soup/ResourceHandleSoup.cpp:
    (WebCore::ResourceHandle::defaultSession):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -29,9 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandle.h"
 
 #include "Base64.h"
+#include "CachedResourceLoader.h"
 #include "ChromeClient.h"
 #include "CookieJarSoup.h"
-#include "CachedResourceLoader.h"
 #include "FileSystem.h"
 #include "Frame.h"
 #include "GOwnPtrSoup.h"
