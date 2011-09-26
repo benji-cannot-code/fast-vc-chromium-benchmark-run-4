fe2dd0deb287c4a315672c0e35b03603490f7297
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 19:50:20 +0000

    clang plugin test script: Set $? if a test fails.
    I want to run this script automatically on clang rolls, and
    need a working $? to detect failure.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8038011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102782 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/plugins/tests/test.sh
+++ tools/clang/plugins/tests/test.sh
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # and compares the output with golden result files.
 
 E_BADARGS=65
+E_FAILEDTEST=1
+
+failed_any_test=
 
 # Prints usage information.
 usage() {
@@ -28,6 +31,7 @@ do_testcase() {
   if [ "${diffout}" = "" ]; then
     echo "PASS: ${1}"
   else
+    failed_any_test=yes
     echo "FAIL: ${1}"
     echo "Output of compiler:"
     echo "${output}"
@@ -58,3 +62,7 @@ fi
 for input in *.cpp; do
   do_testcase "${input}" "${input%cpp}txt"
 done
+
+if [[ "${failed_any_test}" ]]; then
+  exit ${E_FAILEDTEST}
+fi
