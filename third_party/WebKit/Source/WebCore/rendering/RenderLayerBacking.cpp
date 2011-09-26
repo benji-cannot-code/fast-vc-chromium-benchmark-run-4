fb5368143ed4e130bbe987a6d201ca7c78c7a73b
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 19:51:15 +0000

    White flash when entering full-screen using element.webkitRequestFullScreen() https://bugs.webkit.org/show_bug.cgi?id=68481
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    No new tests; covered by existing full screen tests.
    
    During an animation, renderers may try to paint into the FullScreenRenderer's
    GraphicsLayer, and an optimization we previously added for the FullScreenRenderer's
    background will cause these paints to fail. Remove this optimization in
    containsPaintedContent, and taking non-composited elements into full screen will
    animate correctly.
    
    * rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::containsPaintedContent):
    
    Source/WebKit2:
    
    Do not re-enable screen updates after exiting compositing mode until a repaint has completed.
    
    * UIProcess/mac/WKFullScreenWindowController.mm:
    (-[WKFullScreenWindowController exitAcceleratedCompositingMode]):
    (continueExitCompositingModeAfterRepaintCallback):
    (-[WKFullScreenWindowController _continueExitCompositingModeAfterRepaint]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -911,10 +911,6 @@ bool RenderLayerBacking::containsPaintedContent() const
     if (isAcceleratedCanvas(renderer()))
         return hasBoxDecorationsOrBackground(renderer());
 #endif
-#if ENABLE(FULLSCREEN_API)
-    if (renderer()->isRenderFullScreen())
-        return false;
-#endif
 
     return true;
 }
