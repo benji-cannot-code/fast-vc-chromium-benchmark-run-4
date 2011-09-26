9d101f0c9d299234b2969cdac6699eec10368ae8
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 11:37:54 +0000

    Explicitly include <algorithm> to make std::sort available.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8008011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102719 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_list_posix.cc
+++ webkit/plugins/npapi/plugin_list_posix.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/plugins/npapi/plugin_list.h"
 
+#include <algorithm>
+
 #include "base/file_util.h"
 #include "base/path_service.h"
 #include "base/sha1.h"
