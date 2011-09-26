206b9703e5d6cfacd44c0712a1908ded9e84e1e8
rginda@chromium.org <rginda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 19:58:26 +0000

    sort by name by default
    BUG=20597
    TEST=manual testing
    
    Review URL: http://codereview.chromium.org/8028033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -142,7 +142,7 @@ function FileManager(dialogDom, filesystem, rootEntries) {
   this.summarizeSelection_();
   this.updatePreview_();
 
-  this.dataModel_.sort('cachedMtime_', 'desc');
+  this.dataModel_.sort('name', 'asc');
 
   this.refocus();
 
