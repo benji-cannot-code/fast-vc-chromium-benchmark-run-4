1e08373345059a3071d5978c7658ab5ad39e4b25
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 19:58:00 +0000

    Remove an old DrawingAreaProxy::paint member function https://bugs.webkit.org/show_bug.cgi?id=68825
    Reviewed by Dan Bernstein.
    
    * UIProcess/DrawingAreaProxy.h:
    * UIProcess/DrawingAreaProxyImpl.cpp:
    * UIProcess/DrawingAreaProxyImpl.h:
    * UIProcess/TiledDrawingAreaProxy.cpp:
    * UIProcess/TiledDrawingAreaProxy.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxyImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxyImpl.h
@@ -51,7 +51,6 @@ private:
     explicit DrawingAreaProxyImpl(WebPageProxy*);
 
     // DrawingAreaProxy
-    virtual bool paint(const WebCore::IntRect&, PlatformDrawingContext);
     virtual void sizeDidChange();
     virtual void deviceScaleFactorDidChange();
     virtual void visibilityDidChange();
--- third_party/WebKit/Source/WebKit2/UIProcess/TiledDrawingAreaProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/TiledDrawingAreaProxy.h
@@ -86,7 +86,6 @@ private:
     void updateWebView(const Vector<WebCore::IntRect>& paintedArea);
 
     // DrawingAreaProxy
-    virtual bool paint(const WebCore::IntRect&, PlatformDrawingContext);
     virtual void sizeDidChange();
     virtual void deviceScaleFactorDidChange();
     virtual void setPageIsVisible(bool isVisible);
