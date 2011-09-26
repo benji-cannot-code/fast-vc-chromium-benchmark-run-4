63c303cc85549209f32891d5cc6fe8a9a453c4d6
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 23:02:17 +0000

    Enable requestAnimationFrame on Windows https://bugs.webkit.org/show_bug.cgi?id=68397
    Reviewed by Simon Fraser.
    
    Source/JavaScriptCore: 
    
    Enabled REQUEST_ANIMATION_FRAME_TIMER for Windows
    
    * wtf/Platform.h:
    
    WebKitLibraries: 
    
    Enable REQUEST_ANIMATION_FRAME for Windows
    
    * win/tools/vsprops/FeatureDefines.vsprops:
    
    LayoutTests: 
    
    Unskip requestAnimationFrame tests for Windows. request-animation-frame-during-modal.html
    is still skipped because it uses showModalDialog which is not supported in DRT on Windows
    
    * platform/win/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/DerivedSources.cpp
+++ third_party/WebKit/Source/WebCore/DerivedSources.cpp
@@ -274,6 +274,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSRange.cpp"
 #include "JSRangeException.cpp"
 #include "JSRect.cpp"
+#include "JSRequestAnimationFrameCallback.cpp"
 #include "JSRGBColor.cpp"
 #include "JSScreen.cpp"
 #include "JSScriptProfile.cpp"
--- third_party/WebKit/Source/WebCore/bindings/js/JSBindingsAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSBindingsAllInOne.cpp
@@ -128,6 +128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSPeerConnectionCustom.cpp"
 #include "JSPluginElementFunctions.cpp"
 #include "JSProcessingInstructionCustom.cpp"
+#include "JSRequestAnimationFrameCallbackCustom.cpp"
 #include "JSSQLResultSetRowListCustom.cpp"
 #include "JSSQLTransactionCustom.cpp"
 #include "JSSQLTransactionSyncCustom.cpp"
