9a4f3ef8ff490b7383bfc30f1a2479ce5572ee81
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 17:26:16 +0000

    (Relanding this, exact same CL as the previous one.)
    Adding the first one of the saveAs extension APIs.
    The saveAsMHTML allows saving a tab as MHTML.
    
    BUG=97306
    TEST=Run the browser tests.
    TBR=asargent,ahendrickson,jam
    
    Review URL: http://codereview.chromium.org/8027001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102750 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_processes_api.h"
 #include "chrome/browser/extensions/extension_proxy_api.h"
 #include "chrome/browser/extensions/extension_rlz_module.h"
+#include "chrome/browser/extensions/extension_save_page_api.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_settings_api.h"
 #include "chrome/browser/extensions/extension_sidebar_api.h"
@@ -449,6 +450,9 @@ void FactoryRegistry::ResetFunctions() {
   RegisterFunction<DownloadsAcceptDangerFunction>();
   RegisterFunction<DownloadsShowFunction>();
   RegisterFunction<DownloadsDragFunction>();
+
+  // SavePage
+  RegisterFunction<SavePageAsMHTMLFunction>();
 }
 
 void FactoryRegistry::GetAllNames(std::vector<std::string>* names) {
--- chrome/renderer/resources/extensions/extension_process_bindings.js
+++ chrome/renderer/resources/extensions/extension_process_bindings.js
@@ -23,7 +23,8 @@ var chrome = chrome || {};
   native function SetIconCommon();
   native function GetUniqueSubEventName(eventName);
   native function GetLocalFileSystem(name, path);
-  native function DecodeJPEG(jpeg_image);
+  native function DecodeJPEG(jpegImage);
+  native function CreateBlob(filePath);
 
   var chromeHidden = GetChromeHidden();
 
@@ -740,6 +741,18 @@ var chrome = chrome || {};
         }
       });
     };
+
+    apiFunctions["experimental.savePage.saveAsMHTML"].customCallback =
+      function(name, request, response) {
+        var params = chromeHidden.JSON.parse(response);
+        var path = params.mhtmlFilePath;
+        var size = params.mhtmlFileLength;
+
+        if (request.callback)
+          request.callback(CreateBlob(path, size));
+
+        request.callback = null;
+      };
 
     apiFunctions["fileBrowserPrivate.requestLocalFileSystem"].customCallback =
       function(name, request, response) {
