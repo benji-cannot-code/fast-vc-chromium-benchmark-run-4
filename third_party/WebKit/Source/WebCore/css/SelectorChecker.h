7ea426ca44833235d882e48743c711e278098444
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-26 18:53:48 +0000

    Optimize matching of common pseudo classes https://bugs.webkit.org/show_bug.cgi?id=68633
    Reviewed by Dave Hyatt, Darin Adler, Dimitri Glazkov.
            
    :link, :visited and :focus are quite common. They often used as univeral selectors (including in our
    default stylesheet) so we try to match them for all elements in the document. They take always the 
    slow matching path. In addition we match link styles twice due to visited link pseudo style generation
    so the overhead is doubled. As a result substantial portion of our style matching time is spent 
    dealing with these pseudo classes. 
            
    This patch adds new lists to RuleSet for common pseudo class rules. The rules on the lists are only checked
    if the element has approprate type and stat. ases where the rightmost pseudo class can then be rejected immediately.
    We can also enable the fast path checking for the rest of the selector in many cases.
        
    This seems to be >30% progression in selector matching performance with typical style sheets. It saves ~0.9s
    when loading the full HTML5 spec.
    
    * css/CSSStyleSelector.cpp:
    (WebCore::RuleData::hasRightmostSelectorMatchingHTMLBasedOnRuleHash):
    (WebCore::RuleSet::idRules):
    (WebCore::RuleSet::classRules):
    (WebCore::RuleSet::tagRules):
    (WebCore::RuleSet::shadowPseudoElementRules):
    (WebCore::RuleSet::linkPseudoClassRules):
    (WebCore::RuleSet::visitedPseudoClassRules):
    (WebCore::RuleSet::focusPseudoClassRules):
    (WebCore::RuleSet::universalRules):
    (WebCore::RuleSet::pageRules):
            
        Add a new lists, some stylistic renamings.
        
    (WebCore::CSSStyleSelector::matchRules):
            
        New link and focus checks.
    
    (WebCore::CSSStyleSelector::matchRulesForList):
    (WebCore::CSSStyleSelector::checkSelector):
            
        Inline the rightmost selector tag checking, skip if unnecessary.
            
    (WebCore::isSelectorMatchingHTMLBasedOnRuleHash):
            
        Common pseudo classes now match based on early filtering (though it is not a hash in this case).
    
    (WebCore::RuleData::RuleData):
    (WebCore::RuleSet::~RuleSet):
    (WebCore::RuleSet::addRule):
            
        Sort pseudo classes to new lists.
            
    (WebCore::RuleSet::collectFeatures):
    (WebCore::RuleSet::shrinkToFit):
    (WebCore::CSSStyleSelector::matchPageRules):
    * css/SelectorChecker.cpp:
    (WebCore::SelectorChecker::checkSelector):
            
        Adopt to expanded fast path (this is used by querySelectorAll).
    
    (WebCore::SelectorChecker::fastCheckRightmostSelector):
    (WebCore::SelectorChecker::fastCheckSelector):
            
        Rightmost selector is now checked differently than the rest. RuleSet based selection in CSSStyleSelector
        is equivalent to fastCheckRightmostSelector().
            
    (WebCore::isFastCheckableRelation):
    (WebCore::isFastCheckableMatch):
    (WebCore::isFastCheckableRightmostSelector):
    (WebCore::SelectorChecker::isFastCheckableSelector):
    (WebCore::SelectorChecker::checkOneSelector):
    (WebCore::SelectorChecker::commonPseudoClassSelectorMatches):
    (WebCore::SelectorChecker::isFrameFocused):
    * css/SelectorChecker.h:
    (WebCore::SelectorChecker::isCommonPseudoClassSelector):
    (WebCore::SelectorChecker::linkMatchesVisitedPseudoClass):
    (WebCore::SelectorChecker::matchesFocusPseudoClass):
    (WebCore::SelectorChecker::tagMatches):
            
        Refactor a bunch of shared checks into functions.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@95966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/SelectorChecker.h
+++ third_party/WebKit/Source/WebCore/css/SelectorChecker.h
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SelectorChecker_h
 #define SelectorChecker_h
 
+#include "CSSSelector.h"
 #include "Element.h"
+#include "InspectorInstrumentation.h"
 #include "LinkHash.h"
 #include "RenderStyleConstants.h"
 #include <wtf/BloomFilter.h>
@@ -52,7 +54,7 @@ public:
     bool checkSelector(CSSSelector*, Element*, bool isFastCheckableSelector = false) const;
     SelectorMatch checkSelector(CSSSelector*, Element*, PseudoId& dynamicPseudo, bool isSubSelector, bool encounteredLink, RenderStyle* = 0, RenderStyle* elementParentStyle = 0) const;
     static bool isFastCheckableSelector(const CSSSelector*);
-    static bool fastCheckSelector(const CSSSelector*, const Element*);
+    bool fastCheckSelector(const CSSSelector*, const Element*) const;
 
     template <unsigned maximumIdentifierCount>
     inline bool fastRejectSelector(const unsigned* identifierHashes) const;
@@ -80,10 +82,19 @@ public:
 
     bool hasUnknownPseudoElements() const { return m_hasUnknownPseudoElements; }
     void clearHasUnknownPseudoElements() { m_hasUnknownPseudoElements = false; }
+    
+    static bool tagMatches(const Element*, const CSSSelector*);
+    static bool isCommonPseudoClassSelector(const CSSSelector*);
+    bool commonPseudoClassSelectorMatches(const Element*, const CSSSelector*) const;
+    bool linkMatchesVisitedPseudoClass(const Element*) const;
+    bool matchesFocusPseudoClass(const Element*) const;
 
 private:
     bool checkOneSelector(CSSSelector*, Element*, PseudoId& dynamicPseudo, bool isSubSelector, bool encounteredLink, RenderStyle*, RenderStyle* elementParentStyle) const;
     bool checkScrollbarPseudoClass(CSSSelector*, PseudoId& dynamicPseudo) const;
+    static bool isFrameFocused(const Element*);
+    
+    bool fastCheckRightmostSelector(const CSSSelector*, const Element*) const;
 
     EInsideLink determineLinkStateSlowCase(Element*) const;
 
@@ -128,6 +139,41 @@ inline bool SelectorChecker::fastRejectSelector(const unsigned* identifierHashes
             return true;
     }
     return false;
+}
+    
+inline bool SelectorChecker::isCommonPseudoClassSelector(const CSSSelector* selector)
+{
+    if (selector->m_match != CSSSelector::PseudoClass)
+        return false;
+    CSSSelector::PseudoType pseudoType = selector->pseudoType();
+    return pseudoType == CSSSelector::PseudoLink
+        || pseudoType == CSSSelector::PseudoAnyLink
+        || pseudoType == CSSSelector::PseudoVisited
+        || pseudoType == CSSSelector::PseudoFocus;
+}
+
+inline bool SelectorChecker::linkMatchesVisitedPseudoClass(const Element* element) const
+{
+    ASSERT(element->isLink());
+    return m_isMatchingVisitedPseudoClass || InspectorInstrumentation::forcePseudoState(const_cast<Element*>(element), CSSSelector::PseudoVisited);
+}
+
+inline bool SelectorChecker::matchesFocusPseudoClass(const Element* element) const
+{
+    if (InspectorInstrumentation::forcePseudoState(const_cast<Element*>(element), CSSSelector::PseudoFocus))
+        return true;
+    return element->focused() && isFrameFocused(element);
+}
+    
+inline bool SelectorChecker::tagMatches(const Element* element, const CSSSelector* selector)
+{
+    if (!selector->hasTag())
+        return true;
+    const AtomicString& localName = selector->tag().localName();
+    if (localName != starAtom && localName != element->localName())
+        return false;
+    const AtomicString& namespaceURI = selector->tag().namespaceURI();
+    return namespaceURI == starAtom || namespaceURI == element->namespaceURI();
 }
 
 }
