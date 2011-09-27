c3f94efc87599504509b551b0c5bac43b7ab9cfa
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 11:28:59 +0000

    Leak fix in CrosMountPointProvider
    BUG=97879
    TEST=FileSystemPathManagerTest.*
    
    Review URL: http://codereview.chromium.org/8028023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102913 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/chromeos/fileapi/cros_mount_point_provider.cc
+++ webkit/chromeos/fileapi/cros_mount_point_provider.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-  // Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -85,14 +85,15 @@ void CrosMountPointProvider::ValidateFileSystemRootAndGetURL(
     const GURL& origin_url,
     fileapi::FileSystemType type,
     bool create,
-    fileapi::FileSystemPathManager::GetRootPathCallback* callback_ptr) {
+    fileapi::FileSystemPathManager::GetRootPathCallback* callback) {
   DCHECK(type == fileapi::kFileSystemTypeExternal);
   std::string name(GetOriginIdentifierFromURL(origin_url));
   name += ':';
   name += fileapi::kExternalName;
   FilePath root_path;
   root_path = FilePath(fileapi::kExternalDir);
-  callback_ptr->Run(true, root_path, name);
+  callback->Run(true, root_path, name);
+  delete callback;
 }
 
 FilePath CrosMountPointProvider::ValidateFileSystemRootAndGetPathOnFileThread(
