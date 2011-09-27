a31b3e219441e2e3ffff933eb2468a172fa9acb9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 01:36:08 +0000

    Implement a CloseEvent constructor for V8 https://bugs.webkit.org/show_bug.cgi?id=68793
    Patch by Kentaro Hara <haraken@chromium.org> on 2011-09-26
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Test: fast/events/constructors/close-event-constructor.html
    
    * bindings/v8/OptionsObject.cpp:
    (WebCore::OptionsObject::getKey): Just removed an extra space.
    (WebCore::OptionsObject::getKeyValue): Returns an unsigned short value corresponding to the given key.
    * bindings/v8/OptionsObject.h:
    * bindings/v8/custom/V8EventConstructors.cpp: Added the CloseEvent constructor.
    * websockets/CloseEvent.idl: Added a 'V8CustomConstructor' attribute.
    
    LayoutTests:
    
    Enabled close-event-constructor.html, since now V8 has the CloseEvent constructor.
    
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/OptionsObject.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/OptionsObject.h
@@ -78,6 +78,7 @@ public:
         value = ScriptValue(v8Value);
         return true;
     }
+    bool getKeyValue(const String& key, unsigned short& value) const;
     bool getKeyValue(const String& key, unsigned long long& value) const;
 
 private:
