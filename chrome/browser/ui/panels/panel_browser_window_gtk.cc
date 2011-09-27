226c39a5de2589c3709973fbd316da9297c46ab6
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 18:40:13 +0000

    Properly handle zoomed UI when auto-resizing Mac Panels.
    Refactored to let native panel calculate window<->content sizes.
    
    BUG=None
    TEST=PanelBrowserTest.AutoResize
    
    Review URL: http://codereview.chromium.org/8048009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102981 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_window_gtk.cc
+++ chrome/browser/ui/panels/panel_browser_window_gtk.cc
@@ -30,7 +30,7 @@ PanelBrowserWindowGtk::PanelBrowserWindowGtk(Browser* browser,
       panel_(panel),
       bounds_(bounds),
       restored_height_(bounds.height()),
-      non_client_area_size_known_(false) {
+      window_size_known_(false) {
 }
 
 PanelBrowserWindowGtk::~PanelBrowserWindowGtk() {
@@ -112,9 +112,9 @@ void PanelBrowserWindowGtk::SetBounds(const gfx::Rect& bounds) {
 void PanelBrowserWindowGtk::OnSizeChanged(int width, int height) {
   BrowserWindowGtk::OnSizeChanged(width, height);
 
-  if (!non_client_area_size_known_) {
-    non_client_area_size_known_ = true;
-    panel_->OnNonClientExtentAvailable();
+  if (!window_size_known_) {
+    window_size_known_ = true;
+    panel_->OnWindowSizeAvailable();
   }
 }
 
@@ -260,8 +260,18 @@ void PanelBrowserWindowGtk::DestroyPanelBrowser() {
   DestroyBrowser();
 }
 
-gfx::Size PanelBrowserWindowGtk::GetNonClientAreaExtent() const {
-  return GetNonClientFrameSize();
+gfx::Size PanelBrowserWindowGtk::WindowSizeFromContentSize(
+    const gfx::Size& content_size) const {
+  gfx::Size frame = GetNonClientFrameSize();
+  return gfx::Size(content_size.width() + frame.width(),
+                   content_size.height() + frame.height());
+}
+
+gfx::Size PanelBrowserWindowGtk::ContentSizeFromWindowSize(
+    const gfx::Size& window_size) const {
+  gfx::Size frame = GetNonClientFrameSize();
+  return gfx::Size(window_size.width() - frame.width(),
+                   window_size.height() - frame.height());
 }
 
 int PanelBrowserWindowGtk::GetRestoredHeight() const {
