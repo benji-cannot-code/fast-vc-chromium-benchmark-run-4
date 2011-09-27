9d19b4b1530291dc39c2760de2f0232b4829ffef
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 23:42:12 +0000

    watchlist: Add support for cc and message rules. https://bugs.webkit.org/show_bug.cgi?id=68950
    Reviewed by Adam Barth.
    
    * Scripts/webkitpy/common/watchlist/watchlist.py: Added support to get
    the cc's and messages for a patch.
    * Scripts/webkitpy/common/watchlist/watchlist_unittest.py: Tests for the above.
    * Scripts/webkitpy/common/watchlist/watchlistparser.py: Parsing support
    for the rules.
    * Scripts/webkitpy/common/watchlist/watchlistrule.py: Copied from Tools/Scripts/webkitpy/common/watchlist/watchlist.py.
    A generic encapsulation of either a message list or a cc list.
    * Scripts/webkitpy/common/watchlist/watchlistrule_unittest.py: Copied from Tools/Scripts/webkitpy/common/watchlist/watchlist.py.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96170 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlist.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlist.py
@@ -29,12 +29,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from webkitpy.common.checkout.diff_parser import DiffParser
 
+
 class WatchList(object):
     def __init__(self):
         self._definitions = {}
+        self._cc_rules = set()
+        self._message_rules = set()
 
     def set_definitions(self, definitions):
         self._definitions = definitions
+
+    def set_cc_rules(self, cc_rules):
+        self._cc_rules = cc_rules
+
+    def set_message_rules(self, message_rules):
+        self._message_rules = message_rules
 
     def find_matching_definitions(self, diff):
         matching_definitions = set()
@@ -53,3 +62,23 @@ class WatchList(object):
                 else:
                     matching_definitions.add(definition)
         return matching_definitions
+
+    def _determine_instructions(self, matching_definitions, rules):
+        instructions = set()
+        for rule in rules:
+            if rule.match(matching_definitions):
+                instructions.update(rule.instructions())
+        return instructions
+
+    def determine_cc_set(self, matching_definitions):
+        return self._determine_instructions(matching_definitions, self._cc_rules)
+
+    def determine_messages(self, matching_definitions):
+        return self._determine_instructions(matching_definitions, self._message_rules)
+
+    def determine_cc_set_and_messages(self, diff):
+        definitions = self.find_matching_definitions(diff)
+        return {
+            'cc_set': self.determine_cc_set(definitions),
+            'messages':  self.determine_messages(definitions),
+            }
