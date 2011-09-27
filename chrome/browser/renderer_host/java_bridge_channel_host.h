ebc07392f4bb7bdc093bf61a1d957666b1de400a
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 00:24:21 +0000

    Provide implementations of NPChannelBase to allow script to use NPAPI objects in the browser process
    This functionality is required to implement the Java Bridge on Android.
    JavaBridgeChannel is used with NPObjectProxy in the renderer and
    JavaBridgeChannelHost is used with NPObjectStub in the browser.
    
    Note that this requires a minor change to NPChannelBase to allow JavaBridgeChannelHost to finish setting up the channel handle.
    
    BUG=96703
    
    Review URL: http://codereview.chromium.org/7906005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102842 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/renderer_host/java_bridge_channel_host.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RENDERER_HOST_JAVA_BRIDGE_CHANNEL_HOST_H_
+#define CHROME_BROWSER_RENDERER_HOST_JAVA_BRIDGE_CHANNEL_HOST_H_
+#pragma once
+
+#include "content/common/np_channel_base.h"
+
+class JavaBridgeChannelHost : public NPChannelBase {
+ public:
+  static JavaBridgeChannelHost* GetJavaBridgeChannelHost(
+      int renderer_id, base::MessageLoopProxy*);
+
+  // NPChannelBase implementation:
+  virtual int GenerateRouteID() OVERRIDE;
+
+  // NPChannelBase override:
+  virtual bool Init(base::MessageLoopProxy* ipc_message_loop,
+                    bool create_pipe_now,
+                    base::WaitableEvent* shutdown_event) OVERRIDE;
+  virtual bool Send(IPC::Message* msg) OVERRIDE;
+
+ private:
+  JavaBridgeChannelHost() {}
+  friend class base::RefCountedThreadSafe<JavaBridgeChannelHost>;
+  virtual ~JavaBridgeChannelHost() {}
+
+  static NPChannelBase* ClassFactory() {
+    return new JavaBridgeChannelHost();
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(JavaBridgeChannelHost);
+};
+
+#endif  // CHROME_BROWSER_RENDERER_HOST_JAVA_BRIDGE_CHANNEL_HOST_H_
--- content/common/np_channel_base.h
+++ content/common/np_channel_base.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash_tables.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "content/common/npobject_base.h"
 #include "content/common/message_router.h"
+#include "content/common/npobject_base.h"
 #include "ipc/ipc_channel_handle.h"
 #include "ipc/ipc_sync_channel.h"
 #include "ui/gfx/native_widget_types.h"
@@ -149,10 +149,10 @@ class NPChannelBase : public IPC::Channel::Listener,
                     base::WaitableEvent* shutdown_event);
 
   scoped_ptr<IPC::SyncChannel> channel_;
+  IPC::ChannelHandle channel_handle_;
 
  private:
   IPC::Channel::Mode mode_;
-  IPC::ChannelHandle channel_handle_;
   // This tracks the number of routes registered without an NPObject. It's used
   // to manage the lifetime of this object. See comment for AddRoute() and
   // RemoveRoute().
