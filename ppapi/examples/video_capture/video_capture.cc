07fdce2f01bcd783cda93b5252ef6d8c79284a68
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 16:59:52 +0000

    Replaced opengles.h with opengles2.h Review URL: http://codereview.chromium.org/8056009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102951 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/examples/video_capture/video_capture.cc
+++ ppapi/examples/video_capture/video_capture.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ppapi/c/pp_errors.h"
-#include "ppapi/c/ppb_opengles.h"
+#include "ppapi/c/ppb_opengles2.h"
 #include "ppapi/cpp/dev/buffer_dev.h"
 #include "ppapi/cpp/dev/video_capture_dev.h"
 #include "ppapi/cpp/dev/video_capture_client_dev.h"
