9874855f09d2d7381a76682de3d74e828bce66a3
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 20:05:30 +0000

    ui/gfx: Add ToString() function to Size class.
    So we don't need to override operator<< for Size class.
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8038024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102998 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/size.cc
+++ ui/gfx/size.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,11 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CoreGraphics/CGGeometry.h>
 #endif
 
-#include <ostream>
-
 #include "base/logging.h"
+#include "base/stringprintf.h"
 
 namespace gfx {
+
+Size::Size() : width_(0), height_(0) {}
 
 Size::Size(int width, int height) {
   set_width(width);
@@ -64,8 +65,8 @@ void Size::set_height(int height) {
   height_ = height;
 }
 
-std::ostream& operator<<(std::ostream& out, const gfx::Size& s) {
-  return out << s.width() << "x" << s.height();
+std::string Size::ToString() const {
+  return base::StringPrintf("%dx%d", width_, height_);
 }
 
 }  // namespace gfx
--- ui/gfx/size.h
+++ ui/gfx/size.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GFX_SIZE_H_
 #pragma once
 
-#include <iosfwd>
+#include <string>
 
 #include "build/build_config.h"
 #include "ui/base/ui_export.h"
@@ -23,7 +23,7 @@ namespace gfx {
 // A size has width and height values.
 class UI_EXPORT Size {
  public:
-  Size() : width_(0), height_(0) {}
+  Size();
   Size(int width, int height);
 #if defined(OS_MACOSX)
   explicit Size(const CGSize& s);
@@ -72,12 +72,12 @@ class UI_EXPORT Size {
   CGSize ToCGSize() const;
 #endif
 
+  std::string ToString() const;
+
  private:
   int width_;
   int height_;
 };
-
-UI_EXPORT std::ostream& operator<<(std::ostream& out, const gfx::Size& s);
 
 }  // namespace gfx
 
