e70d6e4170b140094218a94eef3e6b81dfa7e1ce
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 02:38:33 +0000

    Add a license header to net/third_party/nss/patches/applypatches.sh. Remove the suppression from tools/checklicenses/checklicenses.py.
    R=phajdan.jr@chromium.org
    BUG=98106
    TEST=tools/checklicenses/checklicenses.py --ignore-suppressions
    net/third_party/nss/patches/applypatches.sh should say 'SUCCESS'.
    
    Review URL: http://codereview.chromium.org/8028043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102869 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -145,9 +145,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'net/disk_cache/hash.cc': [
         'UNKNOWN',
     ],
-    'net/third_party/nss/patches/applypatches.sh': [
-        'UNKNOWN',
-    ],
     'net/tools/spdyshark': [
         'GPL (v2 or later)',
         'UNKNOWN',
