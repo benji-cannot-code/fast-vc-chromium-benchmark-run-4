513f56f929d15f1fa2263fbe1856d9dcdc03ee2b
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 06:26:14 +0000

    [Qt][WK2] One more unreviewed buildfix after r96005.
    * WebKitTestRunner/qt/TestInvocationQt.cpp: Missing include added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/qt/TestInvocationQt.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/qt/TestInvocationQt.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QCryptographicHash>
 #include <WebKit2/WKImageQt.h>
 #include <stdio.h>
+#include "UnusedParam.h"
 
 namespace WTR {
 
