39121bbbdbcb72e5b6ef5d423a64c53fdf734ce7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 21:19:16 +0000

    Unreviewed, rolling out r96139. http://trac.webkit.org/changeset/96139 https://bugs.webkit.org/show_bug.cgi?id=68933
    Broke table-percent-height.html on Mac bots (Requested by
    mwenge2 on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-09-27
    
    Source/WebCore:
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeReplacedLogicalWidthUsing):
    (WebCore::RenderBox::computeReplacedLogicalHeightUsing):
    * rendering/RenderBox.h:
    
    LayoutTests:
    
    * fast/replaced/table-percent-width-expected.txt: Removed.
    * fast/replaced/table-percent-width.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.h
@@ -291,7 +291,6 @@ public:
 
     virtual LayoutUnit containingBlockLogicalWidthForContent() const;
     LayoutUnit perpendicularContainingBlockLogicalHeight() const;
-    int containingBlockReplacedLogicalWidthForContent() const;
     
     virtual void computeLogicalWidth();
     virtual void computeLogicalHeight();
