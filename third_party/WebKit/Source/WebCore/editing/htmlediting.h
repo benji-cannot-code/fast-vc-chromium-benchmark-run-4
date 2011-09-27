0d0086af36619198a22c32c5b41e4355f7270c38
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 09:00:08 +0000

    Encapsulate m_firstNodeInserted and m_lastLeafInserted in node insertion logic https://bugs.webkit.org/show_bug.cgi?id=68875
    Reviewed by Kent Tamura.
    
    Added InsertedNode class inside ReplaceSelection to encapsulate m_firstNodeInserted and m_lastLeafInserted.
    Deployed it in removeRedundantStylesAndKeepStyleSpanInline, doApply, and insertAsListItems of ReplaceSelectionCommand.
    
    * editing/DeleteSelectionCommand.cpp:
    (WebCore::DeleteSelectionCommand::removeNode):
    (WebCore::DeleteSelectionCommand::handleGeneralDelete):
    * editing/ReplaceSelectionCommand.cpp:
    (WebCore::ReplaceSelectionCommand::removeRedundantStylesAndKeepStyleSpanInline):
    (WebCore::ReplaceSelectionCommand::doApply):
    (WebCore::ReplaceSelectionCommand::insertAsListItems):
    * editing/ReplaceSelectionCommand.h:
    (WebCore::ReplaceSelectionCommand::InsertedNodes::respondToNodeInsertion):
    (WebCore::ReplaceSelectionCommand::InsertedNodes::willRemoveNode):
    (WebCore::ReplaceSelectionCommand::InsertedNodes::firstNodeInserted):
    (WebCore::ReplaceSelectionCommand::InsertedNodes::lastLeafInserted):
    (WebCore::ReplaceSelectionCommand::InsertedNodes::pastLastLeaf):
    * editing/htmlediting.cpp:
    (WebCore::updatePositionForNodeRemoval): Moved from DeleteSelectionCommand.cpp.
    * editing/htmlediting.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/htmlediting.h
+++ third_party/WebKit/Source/WebCore/editing/htmlediting.h
@@ -160,6 +160,7 @@ bool isAtUnsplittableElement(const Position&);
 // miscellaneous functions on Position
 
 unsigned numEnclosingMailBlockquotes(const Position&);
+void updatePositionForNodeRemoval(Position&, Node*);
 
 // -------------------------------------------------------------------------
 // VisiblePosition
