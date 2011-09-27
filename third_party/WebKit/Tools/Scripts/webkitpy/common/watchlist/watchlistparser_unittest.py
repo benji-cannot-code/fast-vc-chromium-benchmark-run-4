7e4db618fb81278339580ed2d68ec1af7c5e1c97
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 15:52:14 +0000

    watchlist: Change watchlistparser.py to be class based. https://bugs.webkit.org/show_bug.cgi?id=68869
    Reviewed by Eric Seidel.
    
    * Scripts/webkitpy/common/watchlist/watchlistparser.py:
    * Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import re
 import unittest
-from webkitpy.common.watchlist.watchlistparser import parse_watch_list
+from webkitpy.common.watchlist.watchlistparser import WatchListParser
 
 
 class WatchListParserTest(unittest.TestCase):
@@ -39,6 +39,7 @@ class WatchListParserTest(unittest.TestCase):
         # For versions of Python before 2.7.
         if not 'assertRaisesRegexp' in dir(self):
             self.assertRaisesRegexp = self._verifyException
+        self._watch_list_parser = WatchListParser()
 
     def _verifyException(self, regex_message, callable, *args):
         try:
@@ -50,7 +51,8 @@ class WatchListParserTest(unittest.TestCase):
 
     def test_bad_section(self):
         watch_list_with_bad_section = ('{"FOO": {}}')
-        self.assertRaisesRegexp('Unknown section "FOO" in watch list.', parse_watch_list, watch_list_with_bad_section)
+        self.assertRaisesRegexp('Unknown section "FOO" in watch list.',
+                                self._watch_list_parser.parse, watch_list_with_bad_section)
 
     def test_bad_definition(self):
         watch_list_with_bad_definition = (
@@ -62,7 +64,8 @@ class WatchListParserTest(unittest.TestCase):
             '     },'
             '}')
 
-        self._verifyException('Invalid character "|" in definition "WatchList1|A".', parse_watch_list, watch_list_with_bad_definition)
+        self._verifyException('Invalid character "|" in definition "WatchList1|A".',
+                              self._watch_list_parser.parse, watch_list_with_bad_definition)
 
     def test_bad_match_type(self):
         watch_list_with_bad_match_type = (
@@ -74,4 +77,5 @@ class WatchListParserTest(unittest.TestCase):
             '     },'
             '}')
 
-        self._verifyException('Invalid pattern type "nothing_matches_this" in definition "WatchList1".', parse_watch_list, watch_list_with_bad_match_type)
+        self._verifyException('Invalid pattern type "nothing_matches_this" in definition "WatchList1".',
+                              self._watch_list_parser.parse, watch_list_with_bad_match_type)
