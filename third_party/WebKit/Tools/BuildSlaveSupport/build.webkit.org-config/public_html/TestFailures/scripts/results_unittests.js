f70ee314414766b8feaa87b33ba988b9608e5cdd
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 21:02:34 +0000

    garden-o-matic examine buttons shows both expected and unexpected failures https://bugs.webkit.org/show_bug.cgi?id=68918
    Reviewed by Dimitri Glazkov.
    
    This was a copy/paste error when I refactored this classes to share
    more code.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/controllers.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96153 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js
@@ -24,11 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+var unittest = unittest || {};
+
 (function () {
 
 module("results");
 
-var kExampleResultsJSON = {
+unittest.kExampleResultsJSON = {
     "tests": {
         "scrollbars": {
             "custom-scrollbar-with-incomplete-style.html": {
@@ -70,7 +72,7 @@ var kExampleResultsJSON = {
 };
 
 test("unexpectedFailures", 1, function() {
-    var unexpectedFailures = results.unexpectedFailures(kExampleResultsJSON);
+    var unexpectedFailures = results.unexpectedFailures(unittest.kExampleResultsJSON);
     deepEqual(unexpectedFailures, {
         "userscripts/another-test.html": {
             "expected": "PASS",
@@ -81,7 +83,7 @@ test("unexpectedFailures", 1, function() {
 
 test("unexpectedFailuresByTest", 1, function() {
     var unexpectedFailuresByTest = results.unexpectedFailuresByTest({
-        "Mock Builder": kExampleResultsJSON
+        "Mock Builder": unittest.kExampleResultsJSON
     });
     deepEqual(unexpectedFailuresByTest, {
         "userscripts/another-test.html": {
@@ -95,7 +97,7 @@ test("unexpectedFailuresByTest", 1, function() {
 
 test("unexpectedSuccessesByTest", 1, function() {
     var unexpectedFailuresByTest = results.unexpectedSuccessesByTest({
-        "Mock Builder": kExampleResultsJSON
+        "Mock Builder": unittest.kExampleResultsJSON
     });
     deepEqual(unexpectedFailuresByTest, {
         "scrollbars/unexpected-pass.html": {
@@ -109,7 +111,7 @@ test("unexpectedSuccessesByTest", 1, function() {
 
 test("failureInfoForTestAndBuilder", 1, function() {
     var unexpectedFailuresByTest = results.unexpectedFailuresByTest({
-        "Mock Builder": kExampleResultsJSON
+        "Mock Builder": unittest.kExampleResultsJSON
     });
     var failureInfo = results.failureInfoForTestAndBuilder(unexpectedFailuresByTest, "userscripts/another-test.html", "Mock Builder");
     deepEqual(failureInfo, {
@@ -150,13 +152,13 @@ test("resultType", 12, function() {
 });
 
 test("resultNodeForTest", 4, function() {
-    deepEqual(results.resultNodeForTest(kExampleResultsJSON, "userscripts/another-test.html"), {
+    deepEqual(results.resultNodeForTest(unittest.kExampleResultsJSON, "userscripts/another-test.html"), {
         "expected": "PASS",
         "actual": "TEXT"
     });
-    equals(results.resultNodeForTest(kExampleResultsJSON, "foo.html"), null);
-    equals(results.resultNodeForTest(kExampleResultsJSON, "userscripts/foo.html"), null);
-    equals(results.resultNodeForTest(kExampleResultsJSON, "userscripts/foo/bar.html"), null);
+    equals(results.resultNodeForTest(unittest.kExampleResultsJSON, "foo.html"), null);
+    equals(results.resultNodeForTest(unittest.kExampleResultsJSON, "userscripts/foo.html"), null);
+    equals(results.resultNodeForTest(unittest.kExampleResultsJSON, "userscripts/foo/bar.html"), null);
 });
 
 test("walkHistory", 6, function() {
