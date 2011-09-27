300ea7003e6c1e5cf96823bb8d658f7166df62a6
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-27 07:55:09 +0000

    [Qt][WK2] Mark FINAL properties which can't be overridden by a subclass. https://bugs.webkit.org/show_bug.cgi?id=68848
    Reviewed by Andreas Kling.
    
    The presence of the FINAL attribute indicates that the property will not be
    overridden by a derived class. This can be used for performance optimizations
    by QML.
    
    * UIProcess/API/qt/qdesktopwebview.h:
    * UIProcess/API/qt/qtouchwebpage.h:
    * UIProcess/API/qt/qtouchwebview.h:
    * UIProcess/API/qt/qwebnavigationcontroller.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qtouchwebview.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qtouchwebview.h
@@ -36,7 +36,7 @@ class TouchViewInterface;
 class QWEBKIT_EXPORT QTouchWebView : public QSGItem
 {
     Q_OBJECT
-    Q_PROPERTY(QTouchWebPage* page READ page CONSTANT)
+    Q_PROPERTY(QTouchWebPage* page READ page CONSTANT FINAL)
 
 public:
     QTouchWebView(QSGItem* parent = 0);
