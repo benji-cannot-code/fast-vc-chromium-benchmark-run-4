5dee5ee5aa009ab51b3ca6ae273237c2090e4e32
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 00:33:59 +0000

    Remove chromeos specific debug code from tuple.h  This should have been removed as a part of 71680, but seems like I overlooked it.
    BUG=7327
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8037050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103063 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tuple.h
+++ base/tuple.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_TUPLE_H__
 #pragma once
 
-#if defined(OS_CHROMEOS)
-// To troubleshoot crosbug.com/7327.
-#include "base/logging.h"
-#endif
 // Traits ----------------------------------------------------------------------
 //
 // A simple traits class for tuple arguments.
