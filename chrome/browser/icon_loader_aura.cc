4ac1206296271db7df788c8d63c1a1ac587f95fa
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 22:45:38 +0000

    Some link errors revealed dup symbols and or missing files.
    BUG=97131
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8054039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/icon_loader_aura.cc
+++ chrome/browser/icon_loader_aura.cc
@@ -5,29 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/icon_loader.h"
 
-IconLoader::IconLoader(const IconGroupID& group,
-                       IconSize size,
-                       Delegate* delegate)  {
-  // TODO(saintlou): stubs for now.
-  NOTIMPLEMENTED();
-}
-
-void IconLoader::Start() {
-  // TODO(saintlou): stubs for now.
-  NOTIMPLEMENTED();
-}
-
-IconLoader::~IconLoader()  {
-   // TODO(saintlou): stubs for now.
-   NOTIMPLEMENTED();
- }
-
 void IconLoader::ReadIcon() {
   // TODO(saintlou): stubs for now.
   NOTIMPLEMENTED();
 }
 
-void IconLoader::NotifyDelegate() {
-  // TODO(saintlou): stubs for now.
-  NOTIMPLEMENTED();
-}
