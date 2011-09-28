37ea959751ce439b5e2faa68989aaa25635d5fa9
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 18:07:43 +0000

    Properly scope the lifetime of the |PPB_VideoCapture_Impl|.
    Since it/its proxy are |media::VideoCapture::EventHandler|s, they must remain
    alive from the |StartCapture()| call until |OnRemoved()| is received.
    
    (Precisely, the |PPB_VideoCapture_Impl| owns a |PlatformVideoCapture(Impl)|,
    which owns a |VideoCaptureHandlerProxy|. Keeping the |PPB_VideoCapture_Impl|
    alive keeps all these things alive.)
    
    BUG=none
    TEST=Closing a page with a PPAPI plugin using the video capture interface
         doesn't cause the renderer to crash. Other similar things should also work
         more or less properly (and at least not crash).
    
    Review URL: http://codereview.chromium.org/8052023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103138 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture_proxy.h
+++ media/video/capture/video_capture_proxy.h
@@ -75,6 +75,9 @@ class MEDIA_EXPORT VideoCaptureHandlerProxy
       VideoCapture* capture,
       const VideoCaptureState& state,
       int error_code);
+  void OnRemovedOnMainThread(
+      VideoCapture* capture,
+      const VideoCaptureState& state);
   void OnBufferReadyOnMainThread(
       VideoCapture* capture,
       const VideoCaptureState& state,
