613e26d92abeac801ef9c2454a7a4af13ebe714d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-28 03:37:23 +0000

    garden-o-matic should load faster and inform the user what is happening https://bugs.webkit.org/show_bug.cgi?id=68954
    Reviewed by Dimitri Glazkov.
    
    This test disables the expected failures tab.  The web inspector showed
    that loading the data for this tab was slowing down the loading time
    for the whole app.  We need to find a way to load that data lazily.
    
    This patch also improves the update message to better describe the
    progress the app is making analyzing data from the buildbot.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/garden-o-matic.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/notifications.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui.js
@@ -74,7 +74,7 @@ ui.onebar = base.extends('div', {
             'failures',
         ]
         this._tabs = $(this).tabs({
-            disabled: [1, 2],
+            disabled: [1, 2, 3],
         });
     },
     attach: function()
