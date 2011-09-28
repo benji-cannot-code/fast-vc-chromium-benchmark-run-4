7269bcba00f00646bdb08e166b8bf3f1bc57ac88
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-28 07:22:38 +0000

    Missing initialization of member in ImageFrameQt class https://bugs.webkit.org/show_bug.cgi?id=68943
    Patch by Adenilson Cavalcanti <adenilson.silva@openbossa.org> on 2011-09-28
    Reviewed by Kenneth Rohde Christiansen.
    
    No new tests, this fixes internal implementation detail.
    
    * platform/image-decoders/qt/ImageFrameQt.cpp:
    (WebCore::ImageFrame::ImageFrame):
    (WebCore::ImageFrame::operator=):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/qt/ImageFrameQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/qt/ImageFrameQt.cpp
@@ -42,6 +42,7 @@ ImageFrame::ImageFrame()
     , m_status(FrameEmpty)
     , m_duration(0)
     , m_disposalMethod(DisposeNotSpecified)
+    , m_premultiplyAlpha(true)
 {
 }
 
@@ -55,6 +56,7 @@ ImageFrame& ImageFrame::operator=(const ImageFrame& other)
     setStatus(other.status());
     setDuration(other.duration());
     setDisposalMethod(other.disposalMethod());
+    setPremultiplyAlpha(other.premultiplyAlpha());
     return *this;
 }
 
