0791fcb82a430a4eaae23ec187943a5a60b67dd7
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 21:54:23 +0000

    Add apps notifications data type to sync. Review URL: http://codereview.chromium.org/8074001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103189 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/protocol/sync_proto.gyp
+++ chrome/browser/sync/protocol/sync_proto.gyp
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'sources': [
         'sync.proto',
         'encryption.proto',
+        'app_notification_specifics.proto',
         'app_specifics.proto',
         'autofill_specifics.proto',
         'bookmark_specifics.proto',
--- chrome/browser/sync/syncable/model_type.cc
+++ chrome/browser/sync/syncable/model_type.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_split.h"
 #include "base/values.h"
 #include "chrome/browser/sync/engine/syncproto.h"
+#include "chrome/browser/sync/protocol/app_notification_specifics.pb.h"
 #include "chrome/browser/sync/protocol/app_specifics.pb.h"
 #include "chrome/browser/sync/protocol/autofill_specifics.pb.h"
 #include "chrome/browser/sync/protocol/bookmark_specifics.pb.h"
@@ -67,6 +68,9 @@ void AddDefaultExtensionValue(syncable::ModelType datatype,
     case EXTENSION_SETTINGS:
       specifics->MutableExtension(sync_pb::extension_setting);
       break;
+    case APP_NOTIFICATIONS:
+      specifics->MutableExtension(sync_pb::app_notification);
+      break;
     default:
       NOTREACHED() << "No known extension for model type.";
   }
@@ -122,6 +126,9 @@ int GetExtensionFieldNumberFromModelType(ModelType model_type) {
       break;
     case EXTENSION_SETTINGS:
       return sync_pb::kExtensionSettingFieldNumber;
+      break;
+    case APP_NOTIFICATIONS:
+      return sync_pb::kAppNotificationFieldNumber;
       break;
     default:
       NOTREACHED() << "No known extension for model type.";
@@ -203,6 +210,9 @@ ModelType GetModelTypeFromSpecifics(const sync_pb::EntitySpecifics& specifics) {
   if (specifics.HasExtension(sync_pb::extension_setting))
     return EXTENSION_SETTINGS;
 
+  if (specifics.HasExtension(sync_pb::app_notification))
+    return APP_NOTIFICATIONS;
+
   return UNSPECIFIED;
 }
 
@@ -245,6 +255,8 @@ std::string ModelTypeToString(ModelType model_type) {
       return "Autofill Profiles";
     case EXTENSION_SETTINGS:
       return "Extension settings";
+    case APP_NOTIFICATIONS:
+      return "App Notifications";
     default:
       break;
   }
@@ -317,6 +329,8 @@ ModelType ModelTypeFromString(const std::string& model_type_string) {
     return APPS;
   else if (model_type_string == "Extension settings")
     return EXTENSION_SETTINGS;
+  else if (model_type_string == "App Notifications")
+    return APP_NOTIFICATIONS;
   else
     NOTREACHED() << "No known model type corresponding to "
                  << model_type_string << ".";
@@ -411,6 +425,8 @@ std::string ModelTypeToRootTag(ModelType type) {
       return "google_chrome_autofill_profiles";
     case EXTENSION_SETTINGS:
       return "google_chrome_extension_settings";
+    case APP_NOTIFICATIONS:
+      return "google_chrome_app_notifications";
     default:
       break;
   }
@@ -478,6 +494,10 @@ void PostTimeToTypeHistogram(ModelType model_type, base::TimeDelta time) {
         SYNC_FREQ_HISTOGRAM("Sync.FreqExtensionSettings", time);
         return;
     }
+    case APP_NOTIFICATIONS: {
+        SYNC_FREQ_HISTOGRAM("Sync.FreqAppNotifications", time);
+        return;
+    }
     default:
       LOG(ERROR) << "No known extension for model type.";
   }
@@ -501,6 +521,7 @@ const char kAppNotificationType[] = "APP";
 const char kSearchEngineNotificationType[] = "SEARCH_ENGINE";
 const char kSessionNotificationType[] = "SESSION";
 const char kAutofillProfileNotificationType[] = "AUTOFILL_PROFILE";
+const char kAppNotificationNotificationType[] = "APP_NOTIFICATION";
 }  // namespace
 
 bool RealModelTypeToNotificationType(ModelType model_type,
@@ -544,6 +565,9 @@ bool RealModelTypeToNotificationType(ModelType model_type,
       return true;
     case EXTENSION_SETTINGS:
       *notification_type = kExtensionSettingNotificationType;
+      return true;
+    case APP_NOTIFICATIONS:
+      *notification_type = kAppNotificationNotificationType;
       return true;
     default:
       break;
@@ -592,6 +616,9 @@ bool NotificationTypeToRealModelType(const std::string& notification_type,
     return true;
   } else if (notification_type == kExtensionSettingNotificationType) {
     *model_type = EXTENSION_SETTINGS;
+    return true;
+  } else if (notification_type == kAppNotificationNotificationType) {
+    *model_type = APP_NOTIFICATIONS;
     return true;
   }
   *model_type = UNSPECIFIED;
