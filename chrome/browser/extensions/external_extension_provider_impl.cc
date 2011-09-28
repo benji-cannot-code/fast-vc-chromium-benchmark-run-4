ad12457dba79ba95f7f15215011d740f92e51ede
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 01:27:23 +0000

    Default apps are only for desktop chrome, not for chromeos.  Make sure not to install them in the latter.
    BUG=94920
    TEST=Make sure that none of the defaults apps installed for desktop chrome
    are installed by default on chromeos.
    Review URL: http://codereview.chromium.org/8060025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_extension_provider_impl.cc
+++ chrome/browser/extensions/external_extension_provider_impl.cc
@@ -34,6 +34,7 @@ const char ExternalExtensionProviderImpl::kExternalVersion[] =
 const char ExternalExtensionProviderImpl::kExternalUpdateUrl[] =
     "external_update_url";
 
+#if !defined(OS_CHROMEOS)
 class DefaultAppsProvider : public ExternalExtensionProviderImpl {
  public:
   DefaultAppsProvider(VisitorInterface* service, Profile* profile)
@@ -72,6 +73,7 @@ void DefaultAppsProvider::VisitRegisteredExtension() const {
 
   ExternalExtensionProviderImpl::VisitRegisteredExtension();
 }
+#endif
 
 ExternalExtensionProviderImpl::ExternalExtensionProviderImpl(
     VisitorInterface* service,
@@ -327,6 +329,7 @@ void ExternalExtensionProviderImpl::CreateExternalProviders(
               Extension::INVALID,
               Extension::EXTERNAL_POLICY_DOWNLOAD)));
 
+#if !defined(OS_CHROMEOS)
   // Install default apps, except for the experimental group of users that are
   // to be excluded.
   static bool install_apps = !base::FieldTrialList::TrialExists(
@@ -338,4 +341,5 @@ void ExternalExtensionProviderImpl::CreateExternalProviders(
         linked_ptr<ExternalExtensionProviderInterface>(
             new DefaultAppsProvider(service, profile)));
   }
+#endif
 }
