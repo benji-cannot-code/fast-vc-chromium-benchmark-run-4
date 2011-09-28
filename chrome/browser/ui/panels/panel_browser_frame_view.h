a56c6589a9a53126c887ca7455eb1aded530ce21
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 17:34:52 +0000

    Animating the visibility change of settings button in Panel for Windows.
    BUG=none
    TEST=manual test by moving mouse around to show or hide settings button
    Review URL: http://codereview.chromium.org/8051002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_frame_view.h
+++ chrome/browser/ui/panels/panel_browser_frame_view.h
@@ -12,15 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/tab_icon_view.h"
+#include "ui/base/animation/animation_delegate.h"
 #include "views/controls/button/button.h"
 #include "views/controls/menu/view_menu_delegate.h"
 
 class Extension;
-class ExtensionUninstallDialog;
 class PanelBrowserView;
 class PanelSettingsMenuModel;
 namespace gfx {
 class Font;
+}
+namespace ui {
+class SlideAnimation;
 }
 namespace views {
 class ImageButton;
@@ -34,7 +37,8 @@ class MenuRunner;
 class PanelBrowserFrameView : public BrowserNonClientFrameView,
                               public views::ButtonListener,
                               public views::ViewMenuDelegate,
-                              public TabIconView::TabIconViewModel {
+                              public TabIconView::TabIconViewModel,
+                              public ui::AnimationDelegate {
  public:
   PanelBrowserFrameView(BrowserFrame* frame, PanelBrowserView* browser_view);
   virtual ~PanelBrowserFrameView();
@@ -88,6 +92,11 @@ class PanelBrowserFrameView : public BrowserNonClientFrameView,
   // Overridden from TabIconView::TabIconViewModel:
   virtual bool ShouldTabIconViewAnimate() const OVERRIDE;
   virtual SkBitmap GetFaviconForTabIconView() OVERRIDE;
+
+  // Overridden from AnimationDelegate:
+  virtual void AnimationEnded(const ui::Animation* animation) OVERRIDE;
+  virtual void AnimationProgressed(const ui::Animation* animation) OVERRIDE;
+  virtual void AnimationCanceled(const ui::Animation* animation) OVERRIDE;
 
  private:
   friend class PanelBrowserViewTest;
@@ -176,7 +185,6 @@ class PanelBrowserFrameView : public BrowserNonClientFrameView,
 
   PaintState paint_state_;
   views::MenuButton* settings_button_;
-  bool is_settings_button_visible_;
   views::ImageButton* close_button_;
   TabIconView* title_icon_;
   views::Label* title_label_;
@@ -186,7 +194,12 @@ class PanelBrowserFrameView : public BrowserNonClientFrameView,
   scoped_ptr<views::MenuModelAdapter> settings_menu_adapter_;
   views::MenuItemView* settings_menu_;  // Owned by |settings_menu_runner_|.
   scoped_ptr<views::MenuRunner> settings_menu_runner_;
-  scoped_ptr<ExtensionUninstallDialog> extension_uninstall_dialog_;
+
+  // Used to animate the visibility change of settings button.
+  scoped_ptr<ui::SlideAnimation> settings_button_animator_;
+  gfx::Rect settings_button_full_bounds_;
+  gfx::Rect settings_button_zero_bounds_;
+  bool is_settings_button_visible_;
 
   DISALLOW_COPY_AND_ASSIGN(PanelBrowserFrameView);
 };
