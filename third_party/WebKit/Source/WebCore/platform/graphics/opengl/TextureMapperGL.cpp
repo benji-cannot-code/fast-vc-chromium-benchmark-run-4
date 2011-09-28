b1e17a8f99644d2c7c472e836a9a27694e859bf6
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-28 09:45:13 +0000

    [Texmap] Allow TextureMapperGL to work without a GraphicsContext https://bugs.webkit.org/show_bug.cgi?id=68980
    Reviewed by Andreas Kling.
    
    Perform a null-check for m_context and initialize that variable.
    If a graphics-context doesn't exist, we don't need to reset it.
    
    No new functionality so no new tests.
    
    * platform/graphics/opengl/TextureMapperGL.cpp:
    (WebCore::TextureMapperGL::TextureMapperGL):
    (WebCore::TextureMapperGL::beginPainting):
    (WebCore::TextureMapperGL::endPainting):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/opengl/TextureMapperGL.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/opengl/TextureMapperGL.cpp
@@ -298,6 +298,7 @@ private:
 
 TextureMapperGL::TextureMapperGL()
     : m_data(new TextureMapperGLData)
+    , m_context(0)
 {
 }
 
@@ -396,9 +397,11 @@ void TextureMapperGL::beginPainting()
         return;
 
     glGetIntegerv(GL_CURRENT_PROGRAM, &m_data->previousProgram);
-    QPainter* painter = m_context->platformContext();
-    painter->save();
-    painter->beginNativePainting();
+    if (m_context) {
+        QPainter* painter = m_context->platformContext();
+        painter->save();
+        painter->beginNativePainting();
+    }
     glClearStencil(0);
     glClear(GL_STENCIL_BUFFER_BIT);
     bindSurface(0);
@@ -411,8 +414,10 @@ void TextureMapperGL::endPainting()
 #if PLATFORM(QT)
     glClearStencil(1);
     glClear(GL_STENCIL_BUFFER_BIT);
-    QPainter* painter = m_context->platformContext();
     glUseProgram(m_data->previousProgram);
+    if (!m_context)
+        return;
+    QPainter* painter = m_context->platformContext();
     painter->endNativePainting();
     painter->restore();
 #endif
