9b726065b447aa8979ce42ea48456050d91cffec
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 16:15:17 +0000

    Remove dead macros after updating _WIN32_WINNT and WINVER BUG=92941
    Review URL: http://codereview.chromium.org/8038027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/cloud_print/print_system_win.cc
+++ chrome/service/cloud_print/print_system_win.cc
@@ -7,10 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <objidl.h>
 #include <winspool.h>
-#if defined(_WIN32_WINNT)
-#undef _WIN32_WINNT
-#endif  // defined(_WIN32_WINNT)
-#define _WIN32_WINNT _WIN32_WINNT_WIN7
 #include <xpsprint.h>
 
 #include "base/file_path.h"
--- content/browser/renderer_host/render_widget_host_view_win.cc
+++ content/browser/renderer_host/render_widget_host_view_win.cc
@@ -3,19 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Need a few defines from Win 7 headers for WM_GESTURE and
-// ChangeWindowMessageFilterEx.
-// TODO(jschuh): See crbug.com/92941 for longterm fix.
-#include <windows.h>
-#if(WINVER < 0x0601)
-typedef struct tagCHANGEFILTERSTRUCT {
-  DWORD cbSize;
-  DWORD ExtStatus;
-} CHANGEFILTERSTRUCT, *PCHANGEFILTERSTRUCT;
-#define MSGFLT_ALLOW (1)
-#define WM_GESTURE 0x0119
-#endif  // WINVER < 0x0601
-
 #include "content/browser/renderer_host/render_widget_host_view_win.h"
 
 #include <algorithm>
