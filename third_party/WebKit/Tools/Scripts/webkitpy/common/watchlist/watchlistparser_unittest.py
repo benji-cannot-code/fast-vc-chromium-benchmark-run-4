c2fdb20628bf2c411aa6f4d2ae6b3427317c82a6
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-28 21:58:34 +0000

    watchlist: Add a way to load the watchlist from config. https://bugs.webkit.org/show_bug.cgi?id=68991
    Reviewed by Eric Seidel.
    
    * Scripts/webkitpy/common/config/watchlist: Added.
    * Scripts/webkitpy/common/watchlist/watchlistloader.py: Added.
    Support for loading the watch list.
    * Scripts/webkitpy/common/watchlist/watchlistloader_unittest.py: Added.
    Tests for the loading.
    * Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py:
    Adjusted to use the common base class.
    * Scripts/webkitpy/common/webkitunittest.py: Added.
    Made a common base class for functionality used in more than one.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/watchlist/watchlistparser_unittest.py
@@ -29,25 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 '''Unit tests for watchlistparser.py.'''
 
-import re
-import unittest
+from webkitpy.common import webkitunittest
 from webkitpy.common.watchlist.watchlistparser import WatchListParser
 
 
-class WatchListParserTest(unittest.TestCase):
+class WatchListParserTest(webkitunittest.TestCase):
     def setUp(self):
-        # For versions of Python before 2.7.
-        if not 'assertRaisesRegexp' in dir(self):
-            self.assertRaisesRegexp = self._verifyException
+        webkitunittest.TestCase.setUp(self)
         self._watch_list_parser = WatchListParser()
-
-    def _verifyException(self, regex_message, callable, *args):
-        try:
-            callable(*args)
-            self.assertTrue(False, 'No assert raised.')
-        except Exception, exception:
-            self.assertTrue(re.match(regex_message, exception.__str__()),
-                            'Expected regex "%s"\nGot "%s"' % (regex_message, exception.__str__()))
 
     def test_bad_section(self):
         watch_list_with_bad_section = ('{"FOO": {}}')
@@ -70,8 +59,8 @@ class WatchListParserTest(unittest.TestCase):
             '     },'
             '}')
 
-        self._verifyException(r'Invalid character "\|" in definition "WatchList1\|A"\.',
-                              self._watch_list_parser.parse, watch_list_with_bad_definition)
+        self.assertRaisesRegexp(r'Invalid character "\|" in definition "WatchList1\|A"\.',
+                                self._watch_list_parser.parse, watch_list_with_bad_definition)
 
     def test_bad_match_type(self):
         watch_list_with_bad_match_type = (
@@ -83,8 +72,8 @@ class WatchListParserTest(unittest.TestCase):
             '     },'
             '}')
 
-        self._verifyException('Unknown pattern type "nothing_matches_this" in definition "WatchList1".',
-                              self._watch_list_parser.parse, watch_list_with_bad_match_type)
+        self.assertRaisesRegexp('Unknown pattern type "nothing_matches_this" in definition "WatchList1".',
+                                self._watch_list_parser.parse, watch_list_with_bad_match_type)
 
     def test_match_type_typo(self):
         watch_list_with_bad_match_type = (
@@ -96,6 +85,6 @@ class WatchListParserTest(unittest.TestCase):
             '     },'
             '}')
 
-        self._verifyException(r'Unknown pattern type "iflename" in definition "WatchList1"\.\s*'
-                              + r'Perhaps it should be filename\.',
-                              self._watch_list_parser.parse, watch_list_with_bad_match_type)
+        self.assertRaisesRegexp(r'Unknown pattern type "iflename" in definition "WatchList1"\.\s*'
+                                + r'Perhaps it should be filename\.',
+                                self._watch_list_parser.parse, watch_list_with_bad_match_type)
