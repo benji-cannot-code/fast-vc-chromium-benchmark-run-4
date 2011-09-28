b57d02f22da0f611083a1487187ee3e5fc3b7e12
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-28 00:25:15 +0000

    chrome.tabs.update now returns null Tab when no tabs permission.
    BUG=97674
    TEST=added a test extension with permissions, enhanced an existing one.
    
    
    Review URL: http://codereview.chromium.org/7967021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103059 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/tabs/basics/update.js
+++ chrome/test/data/extensions/api_test/tabs/basics/update.js
@@ -67,5 +67,12 @@ chrome.test.runTests([
               }));
           }));
       }));
+  },
+
+  function testUpdatingWithPermissionReturnsTabInfo() {
+    chrome.tabs.update(
+      undefined, {"url": pageUrl("neutrinos")}, pass(function(tab) {
+        assertEq(pageUrl("neutrinos"), tab.url);
+    }));
   }
 ]);
