56c8c572600a97587cca9b5840ac0da3b3bbf412
vlaviano@chromium.org <vlaviano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-29 02:49:44 +0000

    Add Chrome OS bluetooth lab to about:flags
    BUG=chromium-os:16752
    TEST=View about:flags page
    
    Change-Id: Ia5850ab7610005a328325facda1a1b5535733120
    
    
    Review URL: http://codereview.chromium.org/8070014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103231 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -286,6 +286,7 @@ extern const char kWinHttpProxyResolver[];
 #if defined(OS_CHROMEOS)
 extern const char kWebUILogin[];
 extern const char kSkipOAuthLogin[];
+extern const char kEnableBluetooth[];
 extern const char kEnableDevicePolicy[];
 extern const char kEnableGView[];
 extern const char kEnableLoginImages[];
