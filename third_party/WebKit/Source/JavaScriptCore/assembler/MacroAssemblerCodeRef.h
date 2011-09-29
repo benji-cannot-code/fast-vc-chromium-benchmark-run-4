05beb629133f29ea1e30a82668fd0ebc16c17911
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-29 02:32:53 +0000

    Build fix attempt for r96286.
    * assembler/MacroAssemblerCodeRef.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerCodeRef.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerCodeRef.h
@@ -72,6 +72,8 @@ public:
     {
     }
 
+#if !CALLING_CONVENTION_IS_CDECL
+
     template<typename returnType>
     FunctionPtr(returnType(*value)())
         : m_value((void*)value)
@@ -107,7 +109,7 @@ public:
         ASSERT_VALID_CODE_POINTER(m_value);
     }
 
-#ifdef CALLING_CONVENTION_IS_CDECL
+#else
 #define STDCALL __attribute__ ((stdcall))
     template<typename returnType>
     FunctionPtr(returnType STDCALL(*value)())
