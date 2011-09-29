dfda6ce5c3746988d78fe07a01c820e726daec53
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-29 00:09:02 +0000

    WinCairo build fix.
    * WebProcess/WebPage/WebFrame.h: Add include of <wtc/RetainPtr.h>
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebFrame.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebFrame.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
+#include <wtf/RetainPtr.h>
 
 namespace WebCore {
     class Frame;
