b3599c73934ccec2fa80fe7940120788ac2db007
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-29 15:12:46 +0000

    Don't allow Chrome to go into background mode when launched by GCF.
    Doing so is broken in at least a few ways:
    - in-use updates don't work as desired (must quit IE for 6h)
    - there's no UX for quitting Chrome
    
    Since extensions aren't really supported in GCF anyway, I don't see a problem with disallowing background mode.
    
    BUG=97740
    TEST=the easy way: fire up IE and visit gcf:about:version, confirm that --disable-background-mode is on the command line.  the hard way: install a background extension via the registry mechanism then launch IE and visit gcf:about:version.  now check to see if you have an extension process.  if so, see that quitting IE makes all chrome.exe processes disappear.
    
    
    Review URL: http://codereview.chromium.org/8070019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_launcher.cc
+++ chrome_frame/chrome_launcher.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,16 +22,17 @@ const wchar_t* kAllowedSwitches[] = {
   L"automation-channel",
   L"chrome-frame",
   L"chrome-version",
+  L"disable-background-mode",
+  L"disable-popup-blocking",
   L"disable-renderer-accessibility",
   L"enable-experimental-extension-apis",
   L"force-renderer-accessibility",
+  L"full-memory-crash-report",
   L"lang",
   L"no-default-browser-check",
-  L"noerrdialogs",
   L"no-first-run",
+  L"noerrdialogs",
   L"user-data-dir",
-  L"disable-popup-blocking",
-  L"full-memory-crash-report",
 };
 
 const wchar_t kWhitespaceChars[] = {
