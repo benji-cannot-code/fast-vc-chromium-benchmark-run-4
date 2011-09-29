15c9374d91fb1fa16951ebba6299ddf7b0e69d14
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-29 22:41:44 +0000

    Fix views_examples Windows Debug build.
    Was missing ICU initialization. Not sure when this broke.
    
    BUG=none
    TEST=Build and run views_examples on Windows in Debug mode. It should run without asserting.
    
    Review URL: http://codereview.chromium.org/8084007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103378 0039d316-1c4b-4281-b951-d872f2087c98

--- views/examples/examples_main.cc
+++ views/examples/examples_main.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/i18n/icu_util.h"
 #include "base/process_util.h"
 #include "base/stl_util.h"
 #include "base/utf_string_conversions.h"
@@ -166,6 +167,8 @@ int main(int argc, char** argv) {
   base::AtExitManager exit_manager;
 
   ui::RegisterPathProvider();
+  bool icu_result = icu_util::Initialize();
+  CHECK(icu_result);
   ui::ResourceBundle::InitSharedInstance("en-US");
 
   MessageLoop main_message_loop(MessageLoop::TYPE_UI);
