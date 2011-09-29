2316a3eaedecafc2cdfc7623904a0e3739c579de
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-29 17:32:26 +0000

    views: Remove utf_string_conversions.h from tabbed_pane as it's no longer necessary.
    Note: This was a TODO for avi@.
    
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8076001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103300 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/tabbed_pane/tabbed_pane.cc
+++ views/controls/tabbed_pane/tabbed_pane.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/base/accessibility/accessible_view_state.h"
-// TODO(avi): remove when not needed
-#include "base/utf_string_conversions.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "views/controls/native/native_view_host.h"
 #include "views/controls/tabbed_pane/native_tabbed_pane_wrapper.h"
@@ -96,8 +94,7 @@ void TabbedPane::ViewHierarchyChanged(bool is_add, View* parent, View* child) {
 
 bool TabbedPane::AcceleratorPressed(const views::Accelerator& accelerator) {
   // We only accept Ctrl+Tab keyboard events.
-  DCHECK(accelerator.key_code() ==
-      ui::VKEY_TAB && accelerator.IsCtrlDown());
+  DCHECK(accelerator.key_code() == ui::VKEY_TAB && accelerator.IsCtrlDown());
 
   int tab_count = GetTabCount();
   if (tab_count <= 1)
@@ -127,10 +124,10 @@ void TabbedPane::OnFocus() {
       selected_tab->GetWidget()->NotifyAccessibilityEvent(
           selected_tab, ui::AccessibilityTypes::EVENT_FOCUS, true);
     }
-  }
-  else
+  } else {
     View::OnFocus();  // Will focus the RootView window (so we still get
                       // keyboard messages).
+  }
 }
 
 void TabbedPane::OnPaintFocusBorder(gfx::Canvas* canvas) {
