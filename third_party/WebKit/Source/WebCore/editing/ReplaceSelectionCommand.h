e94a20ca6c6e80738b763a772c0e25c4885a7c67
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-30 18:28:13 +0000

    Remove unused ReplaceSelectionCommand::copyStyleToChildren https://bugs.webkit.org/show_bug.cgi?id=69153
    Reviewed by Antonio Gomes.
    
    Removed the function because it's no called anywhere.
    
    * editing/ReplaceSelectionCommand.cpp:
    * editing/ReplaceSelectionCommand.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.h
+++ third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.h
@@ -90,7 +90,6 @@ private:
     
     void removeRedundantStylesAndKeepStyleSpanInline(InsertedNodes&);
     void handleStyleSpans(Node* firstNodeInserted);
-    void copyStyleToChildren(Node* parentNode, const CSSMutableStyleDeclaration* parentStyle);
     void handlePasteAsQuotationNode();
     
     virtual void removeNodePreservingChildren(Node*);
