4e752cf93be089dc80a1bfba1d0647e0e3c2cf5e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-30 02:30:15 +0000

    Initial implementation of WebInspector for WebKit2 GTK port. https://bugs.webkit.org/show_bug.cgi?id=68235
    Patch by Ravi Phaneendra Kasibhatla <ravi.kasibhatla@motorola.com> on 2011-09-29
    Reviewed by Martin Robinson.
    
    * UIProcess/WebInspectorProxy.cpp:
    (WebKit::WebInspectorProxy::WebInspectorProxy): Initialize the native window handles of GTK.
    * UIProcess/WebInspectorProxy.h: Add native window handle for inspector window.
    * UIProcess/gtk/WebInspectorGtk.cpp:
    (WebKit::inspectorFilesBasePath): Base folder for inspector resources.
    (WebKit::inspectorWindowDestroyed): Invoked when inspector window is closed by user.
    (WebKit::WebInspectorProxy::windowDestroyed): Resets the native handles to 0 when user closes the window.
    (WebKit::WebInspectorProxy::platformCreateInspectorPage): Create the WebView for inspector.
    (WebKit::WebInspectorProxy::platformOpen): Create the window for inspector.
    (WebKit::WebInspectorProxy::platformDidClose): Close both WebView & window of inspector.
    (WebKit::WebInspectorProxy::platformInspectedURLChanged): Update the title of inspector window.
    (WebKit::WebInspectorProxy::inspectorPageURL): Return the absolute URL of inspector.html.
    (WebKit::WebInspectorProxy::inspectorBaseURL): Return the location where inspector resources are present.
    WebProcess is given read access to this folder only for loading file URLs by sandboxed extension.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Portions Copyright (c) 2011 Motorola Mobility, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,17 +31,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(INSPECTOR)
 
 #include "WebInspectorMessages.h"
-#include "WebPageProxy.h"
 #include "WebPageCreationParameters.h"
+#include "WebPageGroup.h"
+#include "WebPageProxy.h"
 #include "WebPreferences.h"
 #include "WebProcessProxy.h"
-#include "WebPageGroup.h"
 
 #if PLATFORM(WIN)
 #include "WebView.h"
 #endif
-
-#include <WebCore/NotImplemented.h>
 
 using namespace WebCore;
 
@@ -72,6 +71,9 @@ WebInspectorProxy::WebInspectorProxy(WebPageProxy* page)
     , m_isProfilingJavaScript(false)
     , m_isProfilingPage(false)
 #if PLATFORM(WIN)
+    , m_inspectorWindow(0)
+#elif PLATFORM(GTK)
+    , m_inspectorView(0)
     , m_inspectorWindow(0)
 #endif
 {
--- third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Portions Copyright (c) 2011 Motorola Mobility, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -83,6 +84,8 @@ public:
     
 #if PLATFORM(MAC)
     void inspectedViewFrameDidChange();
+#elif PLATFORM(GTK)
+    void windowDestroyed();
 #endif
 
     void showConsole();
@@ -176,6 +179,9 @@ private:
 #elif PLATFORM(WIN)
     HWND m_inspectorWindow;
     RefPtr<WebView> m_inspectorView;
+#elif PLATFORM(GTK)
+    GtkWidget* m_inspectorView;
+    GtkWidget* m_inspectorWindow;
 #endif
 };
 
