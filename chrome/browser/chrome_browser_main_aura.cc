9ccfcedc2fb6ed5464a6f34bf5adc0bf72751581
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-30 23:40:07 +0000

    Fix Aura compile error. My bad.
    TBR=ben@chromium.org
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8098014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_aura.cc
+++ chrome/browser/chrome_browser_main_aura.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 
 ChromeBrowserMainPartsAura::ChromeBrowserMainPartsAura(
-    const MainFunctionParams& parameters) {
+    const MainFunctionParams& parameters)
+  : ChromeBrowserMainParts(parameters) {
   NOTIMPLEMENTED();
 }
 
