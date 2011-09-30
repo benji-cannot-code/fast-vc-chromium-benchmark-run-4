cca3178bf88e4860c701634bf23859bd78d74249
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-30 13:02:33 +0000

    [cros] Notify when login controller is deleted. Fixes crash when login controller is gone but SignInHandler still has JS messages in the loop.
    Ignoring keydown when login is in progress was done in
    http://codereview.chromium.org/8060014
    
    BUG=chromium-os:20822
    TEST=Manual.
    
    Review URL: http://codereview.chromium.org/8085022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/existing_user_controller.h"
 
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/stringprintf.h"
 #include "base/string_util.h"
