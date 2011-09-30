4ea25d9efd13c43b195ba06bdd722be8b11cf01e
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-30 23:01:23 +0000

    [Qt] Add missing include for QMimeData https://bugs.webkit.org/show_bug.cgi?id=69178
    Reviewed by Tor Arne Vestbø.
    
    This was implicitly included by other headers, but due to changes in
    Qt5 we need to include explicitly.
    
    * UIProcess/qt/QtWebPageProxy.cpp:
    * WebProcess/WebPage/WebPage.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageProxy.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QApplication>
 #include <QGraphicsSceneMouseEvent>
 #include <QJSEngine>
+#include <QMimeData>
 #include <QStyle>
 #include <QTouchEvent>
 #include <QUndoStack>
