a1eb8552175e290b77567c48e5031f67ea60a7b2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-09-30 05:15:58 +0000

    DFG JIT - register not unlocked after usage in ArithDiv https://bugs.webkit.org/show_bug.cgi?id=69122
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-09-29
    Reviewed by Geoffrey Garen.
    
    Some allocated register is not unlocked after the usage in ArithDiv.
    Also there's a typo in "ENBALE_DFG_CONSISTENTCY_CHECK".
    
    * dfg/DFGNode.h:
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_DFG_JIT_ASSERT 0
 #endif
 // Consistency check contents compiler data structures.
-#define ENBALE_DFG_CONSISTENCY_CHECK 0
+#define ENABLE_DFG_CONSISTENCY_CHECK 0
 // Emit a breakpoint into the head of every generated function, to aid debugging in GDB.
 #define ENABLE_DFG_JIT_BREAK_ON_EVERY_FUNCTION 0
 // Emit a breakpoint into the head of every generated node, to aid debugging in GDB.
