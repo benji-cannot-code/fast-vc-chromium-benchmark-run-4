1f3fe2cbd211832a6570dbbe45802239b679be3f
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-01 20:46:32 +0000

    Reland r103624 - Add ExtensionFunctionTest base class and a test for GetWindowFunction using it.
    Review URL: http://codereview.chromium.org/8098023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function.cc
+++ chrome/browser/extensions/extension_function.cc
@@ -44,7 +44,7 @@ void UIThreadExtensionFunction::RenderViewHostTracker::Observe(
 
 ExtensionFunction::ExtensionFunction()
     : request_id_(-1),
-      profile_(NULL),
+      profile_id_(NULL),
       has_callback_(false),
       include_incognito_(false),
       user_gesture_(false),
@@ -74,6 +74,10 @@ const std::string ExtensionFunction::GetResult() {
   if (result_.get())
     base::JSONWriter::Write(result_.get(), false, &json);
   return json;
+}
+
+Value* ExtensionFunction::GetResultValue() {
+  return result_.get();
 }
 
 const std::string ExtensionFunction::GetError() {
