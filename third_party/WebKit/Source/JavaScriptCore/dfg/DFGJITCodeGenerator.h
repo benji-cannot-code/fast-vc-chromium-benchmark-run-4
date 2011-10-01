8f611d17bcd354d2034e4ffa4409e64c09c2dd3e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-01 02:14:36 +0000

    DFG operation results are not set correctly in JSVALUE32_64 DFG JIT https://bugs.webkit.org/show_bug.cgi?id=69126
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-09-30
    Reviewed by Gavin Barraclough.
    
    The setupResults routine has the bug of reversing the source and destination.
    Also some other trivial (but stupid) bugs need to be fixed in JSVALUE32_64 DFG JIT.
    
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::setupTwoStubArgs):
    (JSC::DFG::setupResults):
    * dfg/DFGJITCodeGenerator32_64.cpp:
    (JSC::DFG::JITCodeGenerator::fillJSValue):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeValueToInt32):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeCompare):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96451 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator.h
@@ -922,6 +922,7 @@ protected:
         m_generationInfo[node.virtualRegister()].initConstant(nodeIndex, node.refCount());
     }
 
+#if CPU(X86_64)
     // These methods used to sort arguments into the correct registers.
     template<GPRReg destA, GPRReg destB>
     void setupTwoStubArgs(GPRReg srcA, GPRReg srcB)
@@ -951,7 +952,6 @@ protected:
         } else
             m_jit.swap(destA, destB);
     }
-#if CPU(X86_64)
     template<FPRReg destA, FPRReg destB>
     void setupTwoStubArgs(FPRReg srcA, FPRReg srcB)
     {
@@ -1183,7 +1183,21 @@ protected:
 
     void setupResults(GPRReg tag, GPRReg payload)
     {
-        setupTwoStubArgs<GPRInfo::returnValueGPR, GPRInfo::returnValueGPR2>(payload, tag);
+        GPRReg srcA = GPRInfo::returnValueGPR;
+        GPRReg srcB = GPRInfo::returnValueGPR2;
+        GPRReg destA = payload;
+        GPRReg destB = tag;
+
+        if (srcB != destA) {
+            // Handle the easy cases - two simple moves.
+            m_jit.move(srcA, destA);
+            m_jit.move(srcB, destB);
+        } else if (srcA != destB) {
+            // Handle the non-swap case - just put srcB in place first.
+            m_jit.move(srcB, destB);
+            m_jit.move(srcA, destA);
+        } else
+            m_jit.swap(destA, destB);
     }
 
     // These methods add calls to C++ helper functions.
