cd28cde4171a88d60606e976a02544e868bdea82
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 23:18:07 +0000

    Add sample extensions that use the text-to-speech (TTS) API.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8114011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103814 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/extensions/speak_selection/tabs.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/**
+ * Copyright (c) 2011 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+function sendKeyToAllTabs(keyStr) {
+  chrome.windows.getAll({'populate': true}, function(windows) {
+    for (var i = 0; i < windows.length; i++) {
+      var tabs = windows[i].tabs;
+      for (var j = 0; j < tabs.length; j++) {
+        chrome.tabs.sendRequest(
+            tabs[j].id,
+            {'key': keyStr});
+      }
+    }
+  });
+}
+
+function loadContentScriptInAllTabs() {
+  chrome.windows.getAll({'populate': true}, function(windows) {
+    for (var i = 0; i < windows.length; i++) {
+      var tabs = windows[i].tabs;
+      for (var j = 0; j < tabs.length; j++) {
+        chrome.tabs.executeScript(
+            tabs[j].id,
+            {file: 'keycodes.js', allFrames: true});
+        chrome.tabs.executeScript(
+            tabs[j].id,
+            {file: 'content_script.js', allFrames: true});
+      }
+    }
+  });
+}
