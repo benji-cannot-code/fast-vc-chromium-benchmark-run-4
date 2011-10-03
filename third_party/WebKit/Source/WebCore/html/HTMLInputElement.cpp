748740e0084e66c80d639d0c0f30ec9f9b6117a9
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-03 23:02:51 +0000

    REGRESSION(r94274): cloned text input loses value https://bugs.webkit.org/show_bug.cgi?id=69095
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    The bug was caused by copyNonAttributeProperties not updating inner element text after copying
    m_valueIfDirty. Fixed the bug by calling updateInnerTextValue.
    
    Test: fast/forms/clone-input-with-dirty-value.html
    
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::copyNonAttributeProperties):
    
    LayoutTests: 
    
    Added a regression test that clones an input element with a dirty value.
    The cloned input element should have the edited value.
    
    * fast/forms/clone-input-with-dirty-value-expected.txt: Added.
    * fast/forms/clone-input-with-dirty-value.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
@@ -1028,6 +1028,9 @@ void HTMLInputElement::copyNonAttributeProperties(const Element* source)
     m_isIndeterminate = sourceElement->m_isIndeterminate;
 
     HTMLTextFormControlElement::copyNonAttributeProperties(source);
+
+    setFormControlValueMatchesRenderer(false);
+    updateInnerTextValue();
 }
 
 String HTMLInputElement::value() const
