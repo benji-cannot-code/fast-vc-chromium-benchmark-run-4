9ebaa711d0113de28434bb02d6e39b3a850dc0d7
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 20:17:40 +0000

    Language 'add' button still active if spell check is disabled. (Regression fix)
    BUG=98818
    TEST=Go to language/spell checker settings, disable spell check. Add button should still be active
    
    
    Review URL: http://codereview.chromium.org/8118011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103770 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/language_options.js
+++ chrome/browser/resources/options/language_options.js
@@ -590,7 +590,6 @@ cr.define('options', function() {
        var value = !$('enable-spell-check').checked;
 
        $('language-options-spell-check-language-button').disabled = value;
-       $('language-options-add-button').disabled = value;
      },
 
     /**
