6b10234db73bf04261ecbb25d5a30bf73d14608d
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-03 23:54:26 +0000

    [chromium] Tweak WebCompositor API for input event handling to express three possible states https://bugs.webkit.org/show_bug.cgi?id=69304
    Reviewed by Darin Fisher.
    
    * public/WebCompositorClient.h:
    * src/WebCompositorImpl.cpp:
    (WebKit::WebCompositorImpl::~WebCompositorImpl):
    (WebKit::WebCompositorImpl::handleInputEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebCompositorImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebCompositorImpl.cpp
@@ -90,6 +90,9 @@ WebCompositorImpl::WebCompositorImpl()
 
 WebCompositorImpl::~WebCompositorImpl()
 {
+    if (m_client)
+        m_client->willShutdown();
+
     ASSERT(s_compositorsLock);
     MutexLocker lock(*s_compositorsLock);
     ASSERT(s_compositors);
@@ -111,7 +114,7 @@ void WebCompositorImpl::handleInputEvent(const WebInputEvent& event)
 {
     ASSERT(CCProxy::isImplThread());
     // FIXME: Do something interesting with the event here.
-    m_client->didHandleInputEvent(false);
+    m_client->didNotHandleInputEvent(true /* sendToWidget */);
 }
 
 }
