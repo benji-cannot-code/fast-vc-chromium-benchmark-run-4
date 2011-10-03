8a3bb6c484a8121a60d8722465f7e59bc173cb62
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 19:34:38 +0000

    Add a metric to Chrome to measure fragmentation of chrome.dll at startup.
    BUG=98033
    
    TEST=None
    Review URL: http://codereview.chromium.org/8085026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -161,6 +161,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_util_win.h"
 #include "chrome/browser/first_run/try_chrome_dialog_view.h"
 #include "chrome/browser/first_run/upgrade_util_win.h"
+#include "chrome/browser/fragmentation_checker_win.h"
 #include "chrome/browser/net/url_fixer_upper.h"
 #include "chrome/browser/rlz/rlz.h"
 #include "chrome/browser/ui/views/user_data_dir_dialog.h"
@@ -1785,6 +1786,10 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunInternal() {
       profile_->GetPrefs()->GetString(prefs::kAcceptLanguages));
   LanguageUsageMetrics::RecordApplicationLanguage(
       g_browser_process->GetApplicationLocale());
+
+#if defined(OS_WIN)
+  fragmentation_checker::RecordFragmentationMetricForCurrentModule();
+#endif
 
 #if defined(OS_CHROMEOS)
   metrics->StartExternalMetrics();
