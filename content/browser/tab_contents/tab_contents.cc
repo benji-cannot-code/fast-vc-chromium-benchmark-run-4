36b0936f7cfd30db230da58923e54ebb15860ca1
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 10:59:25 +0000

    Remove unused RenderViewHostManager::SwapInRenderViewHost() and TabContents::SwapInRenderViewHost()
    Review URL: http://codereview.chromium.org/8081019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103702 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents.cc
+++ content/browser/tab_contents/tab_contents.cc
@@ -1956,10 +1956,6 @@ void TabContents::set_encoding(const std::string& encoding) {
       GetCanonicalEncodingNameByAliasName(encoding);
 }
 
-void TabContents::SwapInRenderViewHost(RenderViewHost* rvh) {
-  render_manager_.SwapInRenderViewHost(rvh);
-}
-
 void TabContents::CreateViewAndSetSizeForRVH(RenderViewHost* rvh) {
   RenderWidgetHostView* rwh_view = view()->CreateViewForWidget(rvh);
   rwh_view->SetSize(view()->GetContainerSize());
