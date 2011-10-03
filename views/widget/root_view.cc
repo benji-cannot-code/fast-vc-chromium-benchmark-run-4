9deebfa421b75d8d8e81f790c5d48bef060317ac
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 21:21:44 +0000

    Reland the layer creation change since the touchui issues will be resolved in a follow up CL.
    This reverts 103580 - Revert the layer creation change r103338 due to some issues with the keyboard on touch. I'll re-land later once I can get a working linux build.TBR=skyBUG=noneTEST=none
    Review URL: http://codereview.chromium.org/8100015
    
    TBR=ben@chromium.org
    Review URL: http://codereview.chromium.org/8115010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103777 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/root_view.cc
+++ views/widget/root_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/gfx/canvas_skia.h"
+#include "ui/gfx/compositor/layer.h"
 #include "views/focus/view_storage.h"
 #include "views/layout/fill_layout.h"
 #include "views/touchui/gesture_manager.h"
@@ -156,10 +157,14 @@ std::string RootView::GetClassName() const {
 }
 
 void RootView::SchedulePaintInRect(const gfx::Rect& rect) {
-  gfx::Rect xrect = ConvertRectToParent(rect);
-  gfx::Rect invalid_rect = GetLocalBounds().Intersect(xrect);
-  if (!invalid_rect.IsEmpty())
-    widget_->SchedulePaintInRect(invalid_rect);
+  if (layer()) {
+    layer()->SchedulePaint(rect);
+  } else {
+    gfx::Rect xrect = ConvertRectToParent(rect);
+    gfx::Rect invalid_rect = GetLocalBounds().Intersect(xrect);
+    if (!invalid_rect.IsEmpty())
+      widget_->SchedulePaintInRect(invalid_rect);
+  }
 }
 
 bool RootView::OnMousePressed(const MouseEvent& event) {
@@ -416,14 +421,6 @@ void RootView::OnPaint(gfx::Canvas* canvas) {
 #if !defined(TOUCH_UI)
   canvas->AsCanvasSkia()->drawColor(SK_ColorBLACK, SkXfermode::kClear_Mode);
 #endif
-}
-
-const ui::Compositor* RootView::GetCompositor() const {
-  return widget_->GetCompositor();
-}
-
-ui::Compositor* RootView::GetCompositor() {
-  return widget_->GetCompositor();
 }
 
 void RootView::CalculateOffsetToAncestorWithLayer(gfx::Point* offset,
