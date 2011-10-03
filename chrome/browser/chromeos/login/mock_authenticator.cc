07fa432600fe4b13be49bf28e169e390b6fbebd8
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 12:33:35 +0000

    [cros] Transfer proxy HTTP authentication from login screen into new session.
    BUG=chromium-os:20992
    TEST=net_unittests: HttpAuthCacheTest.*; Manual: enable proxies on shared networks, open login screen, set a proxy that requires user authentication, add new user; upon sign-in, no additional username/password prompt should appear.
    
    Review URL: http://codereview.chromium.org/8083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/mock_authenticator.cc
+++ chrome/browser/chromeos/login/mock_authenticator.cc
@@ -125,9 +125,12 @@ std::string MockLoginUtils::GetOffTheRecordCommandLine(
   return std::string();
 }
 
-bool MockLoginUtils::TransferDefaultCookies(Profile* default_profile,
-                                                   Profile* new_profile) {
-  return true;
+void MockLoginUtils::TransferDefaultCookies(Profile* default_profile,
+                                            Profile* new_profile) {
+}
+
+void MockLoginUtils::TransferDefaultAuthCache(Profile* default_profile,
+                                              Profile* new_profile) {
 }
 
 }  // namespace chromeos
