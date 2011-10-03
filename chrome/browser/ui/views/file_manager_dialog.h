490ca6da6f65ccfd5b01dcfd199ce9d2c6ac8072
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 18:29:26 +0000

    CrOS - Fix crash when opening a file from a form file input
    Fixes a memory management problem where the FileManagerDialog object
    can be deleted too early.  Only occurs with <input type='file' />,
    which uses FileSelectHelper.  Adds a unit_test and cleans up some
    of the existing browser_test.
    
    BUG=chromium-os:21035
    TEST=manual, attach a file in Gmail, also unit_test FileManagerDialogTest and browser_test FileManagerDialogBrowserTest
    
    Review URL: http://codereview.chromium.org/8095010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/file_manager_dialog.h
+++ chrome/browser/ui/views/file_manager_dialog.h
@@ -22,9 +22,8 @@ class RenderViewHost;
 class FileManagerDialog
     : public SelectFileDialog,
       public ExtensionDialogObserver {
-
  public:
-  explicit FileManagerDialog(SelectFileDialog::Listener* listener);
+  static FileManagerDialog* Create(SelectFileDialog::Listener* listener);
 
   // BaseShellDialog implementation.
   virtual bool IsRunning(gfx::NativeWindow owner_window) const OVERRIDE;
@@ -55,7 +54,11 @@ class FileManagerDialog
                               void* params) OVERRIDE;
 
  private:
-  // Object is ref-counted, see SelectFileDialog.
+  friend class FileManagerDialogBrowserTest;
+  friend class FileManagerDialogTest;
+
+  // Object is ref-counted, use FileManagerDialog::Create().
+  explicit FileManagerDialog(SelectFileDialog::Listener* listener);
   virtual ~FileManagerDialog();
 
   // Closes the dialog and cleans up callbacks and listeners.
@@ -63,6 +66,9 @@ class FileManagerDialog
 
   // Posts a task to close the dialog.
   void PostTaskToClose();
+
+  // Adds this to the list of pending dialogs, used for testing.
+  void AddPending(int32 tab_id);
 
   void* params_;
 
