3eed783aeaeedaa91200c82e09508db55edf5a5c
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 14:20:49 +0000

    touchui: Ignore notification from keyboard if a RenderWidgetHostViewViews isn't attached to widget.
    Also, do not bother telling the renderer to scroll if there's no place to scroll to.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8073028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103708 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/render_widget_host_view_views.cc
+++ chrome/browser/renderer_host/render_widget_host_view_views.cc
@@ -391,8 +391,10 @@ void RenderWidgetHostViewViews::Observe(int type,
     return;
   }
 
-  gfx::Rect keyboard_rect = *Details<gfx::Rect>(details).ptr();
+  if (!GetWidget())
+    return;
 
+  gfx::Rect keyboard_rect = *Details<gfx::Rect>(details).ptr();
   if (keyboard_rect != keyboard_rect_) {
     keyboard_rect_ = keyboard_rect;
     gfx::Rect screen_bounds = GetScreenBounds();
@@ -405,7 +407,8 @@ void RenderWidgetHostViewViews::Observe(int type,
     origin.SetPoint(origin.x() - r.x(), origin.y() - r.y());
     views::View::ConvertPointFromWidget(this, &origin);
     available_rect.set_origin(origin);
-    host_->ScrollFocusedEditableNodeIntoRect(available_rect);
+    if (!available_rect.IsEmpty())
+      host_->ScrollFocusedEditableNodeIntoRect(available_rect);
   }
 #endif
 }
