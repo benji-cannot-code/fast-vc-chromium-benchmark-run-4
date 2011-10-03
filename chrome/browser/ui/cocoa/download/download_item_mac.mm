719a3b2a9ae7ac4bad9f283656df49705b73d6bd
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 07:59:27 +0000

    Add interface to CancelableRequest that allows use of base::Callback<>.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8068013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/download/download_item_mac.mm
+++ chrome/browser/ui/cocoa/download/download_item_mac.mm
@@ -88,8 +88,8 @@ void DownloadItemMac::LoadIcon() {
 
   // The icon isn't cached, load it asynchronously.
   icon_manager->LoadIcon(file, IconLoader::ALL, &icon_consumer_,
-                         NewCallback(this,
-                                     &DownloadItemMac::OnExtractIconComplete));
+                         base::Bind(&DownloadItemMac::OnExtractIconComplete,
+                                    base::Unretained(this)));
 }
 
 void DownloadItemMac::OnExtractIconComplete(IconManager::Handle handle,
