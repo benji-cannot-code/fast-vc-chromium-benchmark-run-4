591e28812b7337779d2e6843833d5b4651ebb416
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-03 16:24:00 +0000

    Fix flipped boolean state for required attribute. TEST=manual BUG=none
    Review URL: http://codereview.chromium.org/8109003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103722 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -441,7 +441,7 @@ NSDictionary* attributeToMethodNameMap = nil;
 
 - (NSNumber*)required {
   return [NSNumber numberWithBool:
-      !GetState(browserAccessibility_, WebAccessibility::STATE_REQUIRED)];
+      GetState(browserAccessibility_, WebAccessibility::STATE_REQUIRED)];
 }
 
 // Returns a string indicating the role of this object.
