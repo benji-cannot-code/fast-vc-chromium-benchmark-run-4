1d2e936331f68299b956588c451a1850602b12cb
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-04 19:02:44 +0000

    REGRESSION(r94274): setting input.value erroneously triggers focus event https://bugs.webkit.org/show_bug.cgi?id=69315
    Reviewed by Kent Tamura.
    
    Fixed the bug by adding a new flag to setSelection to avoid calling setFocusedNodeIfNeeded
    when called by nodeWillBeRemoved and textWillBeReplaced.
    
    Added a manual test. Unfortunately, the test always passes in DRT.
    
    * editing/FrameSelection.cpp:
    (WebCore::FrameSelection::setSelection):
    (WebCore::FrameSelection::respondToNodeModification):
    (WebCore::FrameSelection::textWillBeReplaced):
    * editing/FrameSelection.h:
    * manual-tests/mutate-unfocused-text-with-selection.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
@@ -275,7 +275,7 @@ void FrameSelection::setSelection(const VisibleSelection& newSelection, SetSelec
     m_selection = s;
     setCaretRectNeedsUpdate();
     
-    if (!s.isNone())
+    if (!s.isNone() && !(options & DoNotSetFocus))
         setFocusedNodeIfNeeded();
     
     updateAppearance();
@@ -394,7 +394,7 @@ void FrameSelection::respondToNodeModification(Node* node, bool baseRemoved, boo
         clearRenderViewSelection(m_selection.start());
 
     if (clearDOMTreeSelection)
-        setSelection(VisibleSelection(), 0);
+        setSelection(VisibleSelection(), DoNotSetFocus);
 }
 
 static void updatePositionAfterAdoptingTextReplacement(Position& position, CharacterData* node, unsigned offset, unsigned oldLength, unsigned newLength)
@@ -434,7 +434,7 @@ void FrameSelection::textWillBeReplaced(CharacterData* node, unsigned offset, un
         VisibleSelection newSelection;
         newSelection.setWithoutValidation(base, extent);
         m_frame->document()->updateLayout();
-        setSelection(newSelection, 0);
+        setSelection(newSelection, DoNotSetFocus);
     }
 }
 
