cd7d87053a47a7dc50853fccac84cb4ea0e0074a
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 15:23:26 +0000

    Fix extra visible->hidden->visible transition while prerendering.  Was due to re-parenting the prerendered TabContents twice while swapping it in.
    BUG=85646
    TEST=PrerenderBrowserTest.PrerenderVisibility
    
    Review URL: http://codereview.chromium.org/8122001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103899 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/prerender/prerender_visibility_shared.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Code shared by some of the visibility tests.  Maintains a list of
+// visibility states, starting with the state when this file was loaded.
+// New states are added on each visibility change event.
+
+// Array of previously observed visibility states.
+var visibilityStates = [document.webkitVisibilityState];
+
+// Array of previously observed hidden values.
+var hiddenValues = [document.webkitHidden];
+
+// Record all visibility changes in corresponding arrays.
+function onVisibilityChange(event) {
+  visibilityStates.push(document.webkitVisibilityState);
+  hiddenValues.push(document.webkitHidden);
+}
+
+document.addEventListener("webkitvisibilitychange",
+                          onVisibilityChange,
+                          false);
+
