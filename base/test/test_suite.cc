bbce1e4224933b6717a7a8f36551df9cb61a84b1
michaelbai@google.com <michaelbai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 16:02:57 +0000

    Upstream: The test stub for Android
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8086004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103912 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_suite.cc
+++ base/test/test_suite.cc
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_chrome_application_mac.h"
 #endif
 
+#if defined(OS_ANDROID)
+#include "base/test/test_stub_android.h"
+#endif
+
 #if defined(TOOLKIT_USES_GTK)
 #include <gtk/gtk.h>
 #endif
@@ -183,6 +187,10 @@ void TestSuite::Initialize() {
   mock_cr_app::RegisterMockCrApp();
 #endif
 
+#if defined(OS_ANDROID)
+  InitAndroidTestStub();
+#endif
+
   // Initialize logging.
   FilePath exe;
   PathService::Get(base::FILE_EXE, &exe);
@@ -212,7 +220,11 @@ void TestSuite::Initialize() {
     logging::SetLogAssertHandler(UnitTestAssertHandler);
   }
 
+#if !defined(OS_ANDROID)
+  // TODO(michaelbai): The icu can not be compiled in Android now, this should
+  // be enabled once icu is ready. http://b/5406077.
   icu_util::Initialize();
+#endif
 
   CatchMaybeTests();
 
