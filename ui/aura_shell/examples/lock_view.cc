d36ca743768e1ec2ff4321ace8f5acf51b39e8b2
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 18:31:07 +0000

    Fix aura_shell_exe build on linux
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8135005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103947 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/examples/lock_view.cc
+++ ui/aura_shell/examples/lock_view.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/utf_string_conversions.h"
 #include "ui/aura/desktop.h"
 #include "ui/aura/window.h"
 #include "ui/aura_shell/examples/example_factory.h"
@@ -30,7 +31,7 @@ class LockView : public views::WidgetDelegateView {
   // Overridden from View:
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE {
     canvas->FillRectInt(SK_ColorYELLOW, 0, 0, width(), height());
-    string16 text = L"LOCKED!";
+    string16 text = ASCIIToUTF16("LOCKED!");
     int string_width = font_.GetStringWidth(text);
     canvas->DrawStringInt(text, font_, SK_ColorRED, (width() - string_width)/ 2,
                           (height() - font_.GetHeight()) / 2,
