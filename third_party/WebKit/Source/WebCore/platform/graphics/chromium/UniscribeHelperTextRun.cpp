189b6498afcf59f07bde323deb7cfbc94c8af592
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-04 10:16:05 +0000

    [Chromium] Implement font shaping with font-feature-settings on Windows https://bugs.webkit.org/show_bug.cgi?id=65904
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    Use Uniscribe's OpenType APIs to shape complex text to support -webkit-font-feature-settings property.
    
    No new tests. css3/font-feature-settings-rendering.html should pass by this patch.
    
    * platform/graphics/chromium/UniscribeHelper.cpp:
    (WebCore::loadOpenTypeFunctions):
    (WebCore::UniscribeHelper::UniscribeHelper): Calls loadOpenTypeFunctions if needed.
    (WebCore::UniscribeHelper::fillRuns): Uses ScriptItemizeOpenType() if possible.
    (WebCore::UniscribeHelper::shape): Uses ScriptShapeOpenType() if possible.
    (WebCore::UniscribeHelper::fillShapes): Adds OpenType script tag as an argument.
    (WebCore::convertFeatureTag): Added.
    (WebCore::UniscribeHelper::setRangeProperties): Added.
    * platform/graphics/chromium/UniscribeHelper.h:
    * platform/graphics/chromium/UniscribeHelperTextRun.cpp:
    (WebCore::UniscribeHelperTextRun::UniscribeHelperTextRun): Calls setRangeProperties().
    
    LayoutTests:
    
    Adds expectations of css3/font-feature-settings-rendering.html for Chromium win port. Since Uniscirbe OpenType APIs are not available on WinXP by default, the test could fail on WinXP.
    
    * platform/chromium-win/css3/font-feature-settings-rendering-expected.png: Added.
    * platform/chromium-win/css3/font-feature-settings-rendering-expected.txt: Added.
    * platform/chromium/test_expectations.txt: VISTA and WIN7 should pass css3/font-feature-settings-rendering.html.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/UniscribeHelperTextRun.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/UniscribeHelperTextRun.cpp
@@ -54,6 +54,7 @@ UniscribeHelperTextRun::UniscribeHelperTextRun(const TextRun& run,
     setSpaceWidth(font.spaceWidth());
     setWordSpacing(font.wordSpacing());
     setAscent(font.fontMetrics().ascent());
+    setRangeProperties(font.fontDescription().featureSettings());
 
     init();
 
