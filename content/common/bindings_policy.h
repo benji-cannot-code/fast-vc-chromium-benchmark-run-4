b057ee5de8dd391173bfdf137a2372173b09e84e
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 19:53:38 +0000

    WebUI: Use the TOOLKIT_VIEWS define to determine the value of isViews.
    This sets isViews correctly for  non-Windows, non-CrOS Views builds.
    
    Fixes test failures from the previous commit which was reverted.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8114027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103966 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/bindings_policy.h
+++ content/common/bindings_policy.h
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BindingsPolicy {
  public:
   enum {
-    // HTML-based UI bindings that allows he js content to send JSON-encoded
+    // HTML-based UI bindings that allows the JS content to send JSON-encoded
     // data back to the browser process.
     WEB_UI = 1 << 0,
-    // DOM automation bindings that allows the js content to send JSON-encoded
+    // DOM automation bindings that allows the JS content to send JSON-encoded
     // data back to automation in the parent process.  (By default this isn't
     // allowed unless the app has been started up with the --dom-automation
     // switch.)
