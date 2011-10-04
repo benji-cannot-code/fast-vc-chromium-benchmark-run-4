9dbe082af98f6bed9d900a83b449295a4d44750e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 22:21:44 +0000

    Get rid of web_ui_bindings.h include from Chrome's ExternalHostBindings. The ChromeFrame object wasn't using anything from that class except for storing an IPC sender and a route.
    BUG=98716
    Review URL: http://codereview.chromium.org/8139004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_view_observer.cc
+++ chrome/renderer/chrome_render_view_observer.cc
@@ -722,8 +722,6 @@ void ChromeRenderViewObserver::DidClearWindowObject(WebFrame* frame) {
 
   if (BindingsPolicy::is_external_host_enabled(
           render_view()->enabled_bindings())) {
-    GetExternalHostBindings()->set_message_sender(render_view());
-    GetExternalHostBindings()->set_routing_id(routing_id());
     GetExternalHostBindings()->BindToJavascript(frame, "externalHost");
   }
 }
@@ -993,8 +991,10 @@ void ChromeRenderViewObserver::BindDOMAutomationController(WebFrame* frame) {
 }
 
 ExternalHostBindings* ChromeRenderViewObserver::GetExternalHostBindings() {
-  if (!external_host_bindings_.get())
-    external_host_bindings_.reset(new ExternalHostBindings());
+  if (!external_host_bindings_.get()) {
+    external_host_bindings_.reset(new ExternalHostBindings(
+        render_view(), routing_id()));
+  }
   return external_host_bindings_.get();
 }
 
