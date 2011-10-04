acce26fb940223f1aa4c63b3829baa20ddf88837
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 15:33:26 +0000

    ntp4: fix app title directionality
    BUG=93339
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7779024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103904 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/rtl.cc
+++ base/i18n/rtl.cc
@@ -115,7 +115,7 @@ void SetICUDefaultLocale(const std::string& locale_string) {
 bool IsRTL() {
 #if defined(TOOLKIT_GTK)
   GtkTextDirection gtk_dir = gtk_widget_get_default_direction();
-  return (gtk_dir == GTK_TEXT_DIR_RTL);
+  return gtk_dir == GTK_TEXT_DIR_RTL;
 #else
   return ICUIsRTL();
 #endif
@@ -181,6 +181,14 @@ bool AdjustStringForLocaleDirection(string16* text) {
 
   return true;
 }
+
+bool UnadjustStringForLocaleDirection(string16* text) {
+  if (!IsRTL() || text->empty())
+    return false;
+
+  *text = StripWrappingBidiControlCharacters(*text);
+  return true;
+}
 #else
 bool AdjustStringForLocaleDirection(string16* text) {
   // On OS X & GTK the directionality of a label is determined by the first
@@ -226,8 +234,34 @@ bool AdjustStringForLocaleDirection(string16* text) {
     WrapStringWithLTRFormatting(text);
     text->insert(0U, 1U, kRightToLeftMark);
     text->push_back(kRightToLeftMark);
+  } else {
+    return false;
   }
 
+  return true;
+}
+
+bool UnadjustStringForLocaleDirection(string16* text) {
+  if (text->empty())
+    return false;
+
+  size_t begin_index = 0;
+  char16 begin = text->at(begin_index);
+  if (begin == kLeftToRightMark ||
+      begin == kRightToLeftMark) {
+    ++begin_index;
+  }
+
+  size_t end_index = text->length() - 1;
+  char16 end = text->at(end_index);
+  if (end == kLeftToRightMark ||
+      end == kRightToLeftMark) {
+    --end_index;
+  }
+
+  string16 unmarked_text =
+      text->substr(begin_index, end_index - begin_index + 1);
+  *text = StripWrappingBidiControlCharacters(unmarked_text);
   return true;
 }
 
@@ -302,7 +336,7 @@ string16 GetDisplayStringInLTRDirectionality(const string16& text) {
   return text_mutable;
 }
 
-const string16 StripWrappingBidiControlCharacters(const string16& text) {
+string16 StripWrappingBidiControlCharacters(const string16& text) {
   if (text.empty())
     return text;
   size_t begin_index = 0;
