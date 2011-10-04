14e26104431d8d171c8e2aca4ab388861dd677db
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 23:53:20 +0000

    Update look of profile menu (Views)
    This change updates the look of the profile menu to match the latest mocks. See bug for more info.
    
    BUG=93837
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8070021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104031 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu_bubble_view.h
+++ chrome/browser/ui/views/avatar_menu_bubble_view.h
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/button/custom_button.h"
 #include "views/controls/link.h"
 #include "views/controls/link_listener.h"
+#include "views/controls/separator.h"
 
 class AvatarMenuModel;
 class Browser;
-class EditProfileButton;
 
 // This bubble view is displayed when the user clicks on the avatar button.
 // It displays a list of profiles and allows users to switch between profiles.
@@ -54,8 +54,8 @@ class AvatarMenuBubbleView : public views::View,
   views::Link* add_profile_link_;
   scoped_ptr<AvatarMenuModel> avatar_menu_model_;
   Browser* browser_;
-  EditProfileButton* edit_profile_button_;
   std::vector<views::CustomButton*> item_views_;
+  views::Separator* separator_;
 
   DISALLOW_COPY_AND_ASSIGN(AvatarMenuBubbleView);
 };
