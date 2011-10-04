28903cb480ea5a529e3d22374232fe781eabd3f5
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-04 03:52:43 +0000

    Build fix after r96568.
    * accessibility/AccessibilityObject.cpp:
    * accessibility/mac/WebAccessibilityObjectWrapper.mm:
    * editing/Editor.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96570 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapper.mm
+++ third_party/WebKit/Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapper.mm
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ScrollView.h"
 #import "SimpleFontData.h"
 #import "TextCheckerClient.h"
+#import "TextCheckingHelper.h"
 #import "TextIterator.h"
 #import "WebCoreFrameView.h"
 #import "WebCoreObjCExtras.h"
