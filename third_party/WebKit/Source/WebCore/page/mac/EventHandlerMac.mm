7741a37654ae360f263187597e7263fc7e220327
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-04 04:16:29 +0000

    Mac release build fix after r96561, and Leopard build fix after r96568.
    * editing/TextCheckingHelper.h:
    (WebCore::TextCheckingParagraph::textCharAt):
    * page/mac/EventHandlerMac.mm:
    (WebCore::EventHandler::wheelEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/mac/EventHandlerMac.mm
+++ third_party/WebKit/Source/WebCore/page/mac/EventHandlerMac.mm
@@ -110,7 +110,7 @@ bool EventHandler::wheelEvent(NSEvent *event)
     bool handled = handleWheelEvent(wheelEvent);
 
     ASSERT(handled == wheelEvent.isAccepted());
-    return wheelEvent.isAccepted();
+    return handled;
 }
 
 bool EventHandler::keyEvent(NSEvent *event)
