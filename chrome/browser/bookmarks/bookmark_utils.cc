1b14279c32598b2b4e0589689efb63e0db7a693e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 03:33:29 +0000

    bookmarks: DoesBookmarkTextContainWords() should not use plain string-match for substring matching
    BUG=98699
    TEST=open bookmark manager, search, it should works as before.
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8110002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_path.h"
 #include "base/i18n/case_conversion.h"
+#include "base/i18n/string_search.h"
 #include "base/string16.h"
 #include "base/string_number_conversions.h"
 #include "base/time.h"
@@ -205,7 +206,7 @@ bool MoreRecentlyModified(const BookmarkNode* n1, const BookmarkNode* n2) {
 bool DoesBookmarkTextContainWords(const string16& text,
                                   const std::vector<string16>& words) {
   for (size_t i = 0; i < words.size(); ++i) {
-    if (text.find(words[i]) == string16::npos)
+    if (!base::i18n::StringSearchIgnoringCaseAndAccents(words[i], text))
       return false;
   }
   return true;
@@ -217,13 +218,11 @@ bool DoesBookmarkContainWords(const BookmarkNode* node,
                               const std::vector<string16>& words,
                               const std::string& languages) {
   return
-      DoesBookmarkTextContainWords(
-          base::i18n::ToLower(node->GetTitle()), words) ||
-      DoesBookmarkTextContainWords(
-          base::i18n::ToLower(UTF8ToUTF16(node->url().spec())), words) ||
-      DoesBookmarkTextContainWords(base::i18n::ToLower(
-          net::FormatUrl(node->url(), languages, net::kFormatUrlOmitNothing,
-                         UnescapeRule::NORMAL, NULL, NULL, NULL)), words);
+      DoesBookmarkTextContainWords(node->GetTitle(), words) ||
+      DoesBookmarkTextContainWords(UTF8ToUTF16(node->url().spec()), words) ||
+      DoesBookmarkTextContainWords(net::FormatUrl(
+          node->url(), languages, net::kFormatUrlOmitNothing,
+          UnescapeRule::NORMAL, NULL, NULL, NULL), words);
 }
 
 }  // namespace
