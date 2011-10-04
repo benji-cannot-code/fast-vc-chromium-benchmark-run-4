a59702326533d1c6215bc518ed227cf439e73435
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-04 21:18:53 +0000

    Add a ScrollElasticityController class and move some members over from ScrollAnimatorMac https://bugs.webkit.org/show_bug.cgi?id=69373
    Reviewed by David Hyatt.
    
    ScrollElasticityController is a new class that will handle the rubber-banding when handling scroll events.
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/mac/ScrollAnimatorMac.h:
    * platform/mac/ScrollAnimatorMac.mm:
    (WebCore::ScrollAnimatorMac::ScrollAnimatorMac):
    (WebCore::ScrollAnimatorMac::handleWheelEvent):
    (WebCore::ScrollAnimatorMac::smoothScrollWithEvent):
    (WebCore::ScrollAnimatorMac::beginScrollGesture):
    (WebCore::ScrollAnimatorMac::snapRubberBand):
    (WebCore::ScrollAnimatorMac::snapRubberBandTimerFired):
    * platform/mac/ScrollElasticityController.h: Added.
    (WebCore::ScrollElasticityControllerClient::~ScrollElasticityControllerClient):
    * platform/mac/ScrollElasticityController.mm: Added.
    (WebCore::ScrollElasticityController::ScrollElasticityController):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatPoint.h"
 #include "FloatSize.h"
 #include "ScrollAnimator.h"
+#include "ScrollElasticityController.h"
 #include "Timer.h"
 #include <wtf/RetainPtr.h>
 
@@ -50,11 +51,16 @@ class WebScrollbarPainterDelegate;
 typedef id ScrollbarPainterController;
 #endif
 
+#if !ENABLE(RUBBER_BANDING)
+class ScrollElasticityControllerClient { };
+#endif
+
 namespace WebCore {
 
 class Scrollbar;
 
-class ScrollAnimatorMac : public ScrollAnimator {
+class ScrollAnimatorMac : public ScrollAnimator, private ScrollElasticityControllerClient {
+
 public:
     ScrollAnimatorMac(ScrollableArea*);
     virtual ~ScrollAnimatorMac();
@@ -142,11 +148,8 @@ private:
     void beginScrollGesture();
     void endScrollGesture();
 
-    bool m_inScrollGesture;
-    bool m_momentumScrollInProgress;
-    bool m_ignoreMomentumScrolls;
-    bool m_scrollerInitiallyPinnedOnLeft;
-    bool m_scrollerInitiallyPinnedOnRight;
+    ScrollElasticityController m_scrollElasticityController;
+
     int m_cumulativeHorizontalScroll;
     bool m_didCumulativeHorizontalScrollEverSwitchToOppositeDirectionOfPin;
     
