e2776f2c8f0712b60db18e9fd45f29c3412600da
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-05 12:21:19 +0000

    Unreviewed build fix for windows bots after r96679.
    * html/NumberInputType.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/NumberInputType.h
+++ third_party/WebKit/Source/WebCore/html/NumberInputType.h
@@ -65,7 +65,7 @@ private:
     virtual double parseToDoubleWithDecimalPlaces(const String&, double, unsigned*) const;
     virtual String serialize(double) const;
     virtual double acceptableError(double) const;
-    virtual void handleBlurEvent() OVERRIDE;
+    virtual void handleBlurEvent();
     virtual String visibleValue() const;
     virtual String convertFromVisibleValue(const String&) const;
     virtual bool isAcceptableValue(const String&);
