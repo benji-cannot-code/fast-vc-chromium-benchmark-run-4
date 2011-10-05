81d972d2ae1e50b7fe283664ffd5d4556f363f8b
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 20:59:38 +0000

    Handle UrlRequests in ChromeFrame for resource types except MAIN_FRAME and SUB_FRAME in a background worker thread.
    We have a new request map in ChromeFrame to keep track of these requests. We synchronize access to this map as it is accessed from multiple threads.
    
    This is an attempt to verify if the performance issues seen on CF are related to handling all HTTP requests on the
    IE UI thread.
    
    This is configurable via a registry DWORD value "BackgroundHTTPWorkerThread" under HKCU\Software\Google\ChromeFrame
    
    Defaults to true.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=98510
    
    BUG=98510
    Review URL: http://codereview.chromium.org/8115018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/urlmon_url_request_private.h
+++ chrome_frame/urlmon_url_request_private.h
@@ -59,6 +59,10 @@ class UrlmonUrlRequest
   // to identify request objects in the log.
   std::string me() const;
 
+  static bool ImplementsThreadSafeReferenceCounting() {
+    return true;
+  }
+
  protected:
   UrlmonUrlRequest();
   ~UrlmonUrlRequest();
