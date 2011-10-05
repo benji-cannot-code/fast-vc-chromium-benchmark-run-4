551a5a2eee3db3e47015315ee494f46649295b41
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 22:46:03 +0000

    Remove unnecessary #include <sys/nacl_syscalls.h>
    This header has never been needed by this code.
    We'd like to be able to remove it in the future.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2335
    TEST= none
    
    R=brettw@chromium.org
    
    Review URL: http://codereview.chromium.org/8162006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104197 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_posix.cc
+++ base/threading/platform_thread_posix.cc
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #endif
 
-#if defined(OS_NACL)
-#include <sys/nacl_syscalls.h>
-#endif
-
 namespace base {
 
 #if defined(OS_MACOSX)
--- ppapi/native_client/src/shared/ppapi_proxy/plugin_globals.cc
+++ ppapi/native_client/src/shared/ppapi_proxy/plugin_globals.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <sys/nacl_imc_api.h>
-#include <sys/nacl_syscalls.h>
 
 #include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/ppapi_proxy/plugin_globals.h"
