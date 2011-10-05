f8922105fd01b5b7c18dc43e8d36aa4b6948849a
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 20:33:03 +0000

    Add more tests for VideoCaptureImpl.
    VideoCapture events are defined more restrictly now:
    Without error, StartCapture will result in one and only one OnStarted event for each client, one and only one OnDeviceInfoReceived event for master client, at least one OnDeviceInfoReceived event for regular client.
    Without error, StopCapture will result in one and only one OnStopped/OnRemoved event pair for each client.
    In case of error, all clients receive OnError/OnRemoved event pair.
    After OnRemoved event is sent to client, VideoCaptureImpl will never call that client.
    
    BUG=none
    TEST=trybots
    Review URL: http://codereview.chromium.org/8138013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104167 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/media/video_capture_messages.h
+++ content/common/media/video_capture_messages.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/shared_memory.h"
 #include "content/common/common_param_traits.h"
 #include "ipc/ipc_message_macros.h"
 #include "media/video/capture/video_capture.h"
