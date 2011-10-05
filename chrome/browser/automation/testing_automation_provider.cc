f857d7bcc1ba1569757916ee1052a61a835cb813
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 14:33:00 +0000

    Fix builds with configuration_policy disabled.
    It broke in  http://src.chromium.org/viewvc/chrome?view=rev&revision=103876
    
    BUG=None
    TEST=http://chromegw.corp.google.com/i/chromium.fyi/builders/Chromium%20Linux%20Redux/ goes green
    
    
    Review URL: http://codereview.chromium.org/8137010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -65,10 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_store_change.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/plugin_prefs.h"
-#include "chrome/browser/policy/browser_policy_connector.h"
-#include "chrome/browser/policy/configuration_policy_pref_store.h"
-#include "chrome/browser/policy/configuration_policy_provider.h"
-#include "chrome/browser/policy/policy_map.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/printing/print_view_manager.h"
 #include "chrome/browser/profiles/profile.h"
@@ -125,6 +121,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/message_box_flags.h"
 #include "webkit/glue/webdropdata.h"
 #include "webkit/plugins/webplugininfo.h"
+
+#if defined(ENABLE_CONFIGURATION_POLICY)
+#include "chrome/browser/policy/browser_policy_connector.h"
+#include "chrome/browser/policy/configuration_policy_pref_store.h"
+#include "chrome/browser/policy/configuration_policy_provider.h"
+#include "chrome/browser/policy/policy_map.h"
+#endif
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/ui/webui/active_downloads_ui.h"
@@ -5843,8 +5846,8 @@ void TestingAutomationProvider::SetPolicies(
     IPC::Message* reply_message) {
   AutomationJSONReply reply(this, reply_message);
 
-#if defined(OFFICIAL_BUILD)
-  reply.SendError("Disabled on official builds.");
+#if !defined(ENABLE_CONFIGURATION_POLICY) || defined(OFFICIAL_BUILD)
+  reply.SendError("Configuration Policy disabled");
 #else
   const policy::ConfigurationPolicyProvider::PolicyDefinitionList* list =
       policy::ConfigurationPolicyPrefStore::GetChromePolicyDefinitionList();
@@ -5879,6 +5882,10 @@ void TestingAutomationProvider::GetPolicyDefinitionList(
     DictionaryValue* args,
     IPC::Message* reply_message) {
   AutomationJSONReply reply(this, reply_message);
+
+#if !defined(ENABLE_CONFIGURATION_POLICY)
+  reply.SendError("Configuration Policy disabled");
+#else
   DictionaryValue response;
 
   const policy::ConfigurationPolicyProvider::PolicyDefinitionList* list =
@@ -5901,6 +5908,7 @@ void TestingAutomationProvider::GetPolicyDefinitionList(
   }
 
   reply.SendSuccess(&response);
+#endif
 }
 
 void TestingAutomationProvider::GetIndicesFromTab(
