09c42960496bab6a755a6dd3fb0c4c76cbe1069b
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 01:06:25 +0000

    Component updater: send extra attributes in the request to facilitate the crx configuration explosion.
    Now the client send os=foo&arch=bar along with the usual request. for example os=win&arch=x86
    
    TEST=see bug
    BUG=61602
    Review URL: http://codereview.chromium.org/8122015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_service_unittest.cc
+++ chrome/browser/component_updater/component_updater_service_unittest.cc
@@ -57,6 +57,8 @@ class TestConfigurator : public ComponentUpdateService::Configurator {
 
   virtual GURL UpdateUrl() OVERRIDE { return GURL("http://localhost/upd"); }
 
+  virtual const char* ExtraRequestParams() OVERRIDE { return "extra=foo"; }
+
   virtual size_t UrlSizeLimit() OVERRIDE { return 256; }
 
   virtual net::URLRequestContextGetter* RequestContext() OVERRIDE {
@@ -239,7 +241,7 @@ TEST_F(ComponentUpdaterTest, CheckCrxSleep) {
   RegisterComponent(&com, kTestComponent_abag, Version("1.1"));
 
   const char expected_update_url[] =
-      "http://localhost/upd?x=id%3D"
+      "http://localhost/upd?extra=foo&x=id%3D"
       "abagagagagagagagagagagagagagagag%26v%3D1.1%26uc";
 
   interceptor->SetResponse(expected_update_url,
@@ -325,12 +327,12 @@ TEST_F(ComponentUpdaterTest, InstallCrx) {
   RegisterComponent(&com2, kTestComponent_abag, Version("2.2"));
   
   const char expected_update_url_1[] =
-      "http://localhost/upd?x=id%3D"
+      "http://localhost/upd?extra=foo&x=id%3D"
       "jebgalgnebhfojomionfpkfelancnnkf%26v%3D0.9%26uc&x=id%3D"
       "abagagagagagagagagagagagagagagag%26v%3D2.2%26uc";
 
   const char expected_update_url_2[] =
-      "http://localhost/upd?x=id%3D"
+      "http://localhost/upd?extra=foo&x=id%3D"
       "abagagagagagagagagagagagagagagag%26v%3D2.2%26uc&x=id%3D"
       "jebgalgnebhfojomionfpkfelancnnkf%26v%3D1.0%26uc";
 
@@ -389,7 +391,7 @@ TEST_F(ComponentUpdaterTest, ProdVersionCheck) {
   RegisterComponent(&com, kTestComponent_jebg, Version("0.9"));
 
   const char expected_update_url[] =
-      "http://localhost/upd?x=id%3D"
+      "http://localhost/upd?extra=foo&x=id%3D"
       "jebgalgnebhfojomionfpkfelancnnkf%26v%3D0.9%26uc";
 
   interceptor->SetResponse(expected_update_url,
