c35758b64c9baa30d2ca66e15905ead7f6f65345
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 22:01:54 +0000

    touchui: Fix a keyboard visibility bug.
    If the keyboard is focused while it's in the process of animating to hide, the
    keyboard does not pop back up. This fixes that.
    
    BUG=none
    TEST=VirtualKeyboardManager.TestVisibility
    
    Review URL: http://codereview.chromium.org/8152002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/virtual_keyboard/virtual_keyboard_manager_browsertest.cc
+++ chrome/browser/ui/virtual_keyboard/virtual_keyboard_manager_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/virtual_keyboard/virtual_keyboard_manager.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -47,6 +48,7 @@ class VirtualKeyboardManagerTest : public InProcessBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(VirtualKeyboardManagerTest, TestVisibility) {
   SetupNotificationListener();
+  browser()->window()->Show();
 
   // Move focus between the omnibox and the wrench menu a few times. Note that
   // it is necessary to RunAllPendingInMessageLoop each time after moving
