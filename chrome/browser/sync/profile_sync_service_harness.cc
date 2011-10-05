c41643b495b12b8e92eb2a4defc669153c5e1b86
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 23:40:01 +0000

    [Sync] Fix OnDataTypesChanged functionality to trigger reconfiguration properly.
    BUG=99214
    TEST=sync_integration_tests, Run a client with --enable-sync-tabs-for-other-clients while another client is running
    
    Review URL: http://codereview.chromium.org/8136039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104205 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_harness.cc
+++ chrome/browser/sync/profile_sync_service_harness.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/task.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/glue/data_type_controller.h"
 #include "chrome/browser/sync/sessions/session_state.h"
 #include "chrome/browser/sync/signin_manager.h"
 #include "chrome/browser/sync/sync_ui_util.h"
@@ -1060,10 +1061,11 @@ bool ProfileSyncServiceHarness::IsTypeEncrypted(syncable::ModelType type) {
   return (encrypted_types.count(type) != 0);
 }
 
-bool ProfileSyncServiceHarness::IsTypeRegistered(syncable::ModelType type) {
-  syncable::ModelTypeSet registered_types;
-  service_->GetRegisteredDataTypes(&registered_types);
-  return (registered_types.count(type) != 0);
+bool ProfileSyncServiceHarness::IsTypeRunning(syncable::ModelType type) {
+  browser_sync::DataTypeController::StateMap state_map;
+  service_->GetDataTypeControllerStates(&state_map);
+  return (state_map.count(type) != 0 &&
+          state_map[type] == browser_sync::DataTypeController::RUNNING);
 }
 
 bool ProfileSyncServiceHarness::IsTypePreferred(syncable::ModelType type) {
--- chrome/browser/sync/test/integration/migration_errors_test.cc
+++ chrome/browser/sync/test/integration/migration_errors_test.cc
@@ -403,7 +403,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabs) {
   }
 
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_FALSE(GetClient(0)->IsTypeRegistered(syncable::SESSIONS));
+  ASSERT_FALSE(GetClient(0)->IsTypeRunning(syncable::SESSIONS));
   ASSERT_FALSE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 
   // Phase 1: Before migrating anything, create & sync a preference.
@@ -420,7 +420,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabs) {
   VerifyPrefSync();
 
   // Phase 5: Verify that sessions are registered and enabled.
-  ASSERT_TRUE(GetClient(0)->IsTypeRegistered(syncable::SESSIONS));
+  ASSERT_TRUE(GetClient(0)->IsTypeRunning(syncable::SESSIONS));
   ASSERT_TRUE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 }
 
@@ -431,7 +431,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabsAndMigrate) {
   }
 
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
-  ASSERT_FALSE(GetClient(0)->IsTypeRegistered(syncable::SESSIONS));
+  ASSERT_FALSE(GetClient(0)->IsTypeRunning(syncable::SESSIONS));
   ASSERT_FALSE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 
   // Phase 1: Before migrating anything, create & sync a preference.
@@ -447,7 +447,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabsAndMigrate) {
   VerifyPrefSync();
 
   // Phase 6: Verify that sessions are registered and enabled.
-  ASSERT_TRUE(GetClient(0)->IsTypeRegistered(syncable::SESSIONS));
+  ASSERT_TRUE(GetClient(0)->IsTypeRunning(syncable::SESSIONS));
   ASSERT_TRUE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 }
 
