6a5309a7a12594a455cf90ee72f98d562c93d20d
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 03:24:48 +0000

    Don't reopen avatar bubble view on second click
    If user opened the avatar bubble view then clicked on the avatar button the avatar menu would close then reopen.
    
    With this change we keep track of the open bubble and prevent it from reopening.
    
    BUG=97146
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8138019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bubble/bubble.h
+++ chrome/browser/ui/views/bubble/bubble.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_BUBBLE_BUBBLE_H_
 #pragma once
 
+#include "base/observer_list.h"
 #include "views/bubble/bubble_border.h"
 #include "ui/base/animation/animation_delegate.h"
 #include "views/accelerator.h"
@@ -87,6 +88,12 @@ class Bubble
       public views::AcceleratorTarget,
       public ui::AnimationDelegate {
  public:
+  class Observer {
+   public:
+    // See BubbleDelegate::BubbleClosing for when this is called.
+    virtual void OnBubbleClosing() = 0;
+  };
+
   // Shows the Bubble.
   // |parent| is set as the parent window.
   // |contents| are the contents shown in the bubble.
@@ -144,6 +151,14 @@ class Bubble
 #ifdef UNIT_TEST
   views::View* contents() const { return contents_; }
 #endif
+
+  void AddObserver(Observer* obs) {
+    observer_list_.AddObserver(obs);
+  }
+
+  void RemoveObserver(Observer* obs) {
+    observer_list_.RemoveObserver(obs);
+  }
 
   static const SkColor kBackgroundColor;
 
@@ -243,6 +258,8 @@ class Bubble
   views::View* contents_;
 
   bool accelerator_registered_;
+
+  ObserverList<Observer> observer_list_;
 
   DISALLOW_COPY_AND_ASSIGN(Bubble);
 };
