0d09f1eb0d5a24482f300019723e5382d959dfb0
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-05 14:30:29 +0000

    Web Inspector: Add support for search in script content. https://bugs.webkit.org/show_bug.cgi?id=69015
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Tests: http/tests/inspector/search/search-in-concatenated-script.html
           http/tests/inspector/search/search-in-script.html
    
    * inspector/Inspector.json:
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::searchInContent):
    (WebCore::InspectorDebuggerAgent::getScriptSource):
    * inspector/InspectorDebuggerAgent.h:
    * inspector/front-end/ContentProviders.js:
    (WebInspector.ScriptContentProvider.prototype.searchInContent):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype._sortedScripts.var):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype._sortedScripts):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype.requestContent):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype.searchInContent.maybeCallback):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype.searchInContent.searchCallback):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype.searchInContent):
    (WebInspector.ConcatenatedScriptsContentProvider.prototype._concatenateScriptsContent):
    * inspector/front-end/Script.js:
    (WebInspector.Script.prototype.requestSource.didGetScriptSource):
    (WebInspector.Script.prototype.requestSource):
    (WebInspector.Script.prototype.searchInContent):
    
    LayoutTests:
    
    * http/tests/inspector/resource-tree/resource-tree-test.js:
    (initialize_ResourceTreeTest):
    * http/tests/inspector/resources-test.js:
    (initialize_ResourceTest.InspectorTest.runAfterResourcesAreFinished):
    (initialize_ResourceTest.InspectorTest._runAfterResourcesAreFinished):
    (initialize_ResourceTest.InspectorTest._runAfterResourcesAreFinished.maybeCallback):
    (initialize_ResourceTest.InspectorTest._runAfterResourcesAreFinished.addSniffer):
    (initialize_ResourceTest.InspectorTest._runAfterResourcesAreFinished.resourceAddedToFrame):
    (initialize_ResourceTest.InspectorTest._runAfterResourcesAreFinished.visit):
    (initialize_ResourceTest):
    * http/tests/inspector/search/resources/search-concatenated.html: Added.
    * http/tests/inspector/search/search-in-concatenated-script-expected.txt: Added.
    * http/tests/inspector/search/search-in-concatenated-script.html: Added.
    * http/tests/inspector/search/search-in-resource.html:
    * http/tests/inspector/search/search-in-script-expected.txt: Added.
    * http/tests/inspector/search/search-in-script.html: Added.
    * http/tests/inspector/search/search-test.js:
    (initialize_SearchTest):
    * inspector/debugger/content-providers.html:
    * platform/qt/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDebuggerAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDebuggerAgent.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorDebuggerAgent.h"
 
 #if ENABLE(JAVASCRIPT_DEBUGGER) && ENABLE(INSPECTOR)
+#include "ContentSearchUtils.h"
 #include "InjectedScript.h"
 #include "InjectedScriptManager.h"
 #include "InspectorFrontend.h"
@@ -321,6 +322,15 @@ static PassRefPtr<InspectorObject> scriptToInspectorObject(ScriptObject scriptOb
     return value->asObject();
 }
 
+void InspectorDebuggerAgent::searchInContent(ErrorString* error, const String& scriptId, const String& query, RefPtr<InspectorArray>* results)
+{
+    ScriptsMap::iterator it = m_scripts.find(scriptId);
+    if (it != m_scripts.end())
+        *results = ContentSearchUtils::searchInTextByLines(query, it->second.source);
+    else
+        *error = "No script for id: " + scriptId;
+}
+
 void InspectorDebuggerAgent::setScriptSource(ErrorString* error, const String& scriptId, const String& newContent, const bool* const preview, RefPtr<InspectorArray>* newCallFrames, RefPtr<InspectorObject>* result)
 {
     bool previewOnly = preview && *preview;
@@ -333,9 +343,13 @@ void InspectorDebuggerAgent::setScriptSource(ErrorString* error, const String& s
         *result = object;
 }
 
-void InspectorDebuggerAgent::getScriptSource(ErrorString*, const String& scriptId, String* scriptSource)
+void InspectorDebuggerAgent::getScriptSource(ErrorString* error, const String& scriptId, String* scriptSource)
 {
-    *scriptSource = m_scripts.get(scriptId).source;
+    ScriptsMap::iterator it = m_scripts.find(scriptId);
+    if (it != m_scripts.end())
+        *scriptSource = it->second.source;
+    else
+        *error = "No script for id: " + scriptId;
 }
 
 void InspectorDebuggerAgent::schedulePauseOnNextStatement(const String& breakReason, PassRefPtr<InspectorObject> data)
