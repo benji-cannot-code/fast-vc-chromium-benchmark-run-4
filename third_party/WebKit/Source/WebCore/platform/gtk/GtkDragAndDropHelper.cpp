bec84539623103db4f5432fb2e8d0e859ceae646
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-05 17:39:01 +0000

    [GTK] Fix compilation with gtk v2.20.1 https://bugs.webkit.org/show_bug.cgi?id=69418
    Unreviewed build fix
    
    Patch by Rémi Duraffort <remi.duraffort@st.com> on 2011-10-05
    
    * platform/gtk/GtkDragAndDropHelper.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96720 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/GtkDragAndDropHelper.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/GtkDragAndDropHelper.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ClipboardUtilitiesGtk.h"
 #include "DragData.h"
 #include "GtkUtilities.h"
+#include "GtkVersioning.h"
 #include "PasteboardHelper.h"
 #include <gtk/gtk.h>
 #include <wtf/PassOwnPtr.h>
