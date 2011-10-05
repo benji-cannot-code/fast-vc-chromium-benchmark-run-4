3343646e7350c30014b804c839ee3a14c3c15bfd
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 19:25:37 +0000

    Don't bother installing default apps in locales where its known that they don't work.
    BUG=94920
    TEST=Make sure default apps are not installed in any chinese locales.
    
    Review URL: http://codereview.chromium.org/8138007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_extension_provider_impl.cc
+++ chrome/browser/extensions/external_extension_provider_impl.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/linked_ptr.h"
 #include "base/metrics/field_trial.h"
 #include "base/path_service.h"
+#include "base/string_util.h"
 #include "base/values.h"
 #include "base/version.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/default_apps_trial.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/external_extension_provider_interface.h"
@@ -337,9 +339,25 @@ void ExternalExtensionProviderImpl::CreateExternalProviders(
           kDefaultAppsTrial_Name)->group_name() !=
               kDefaultAppsTrial_NoAppsGroup);
   if (install_apps) {
-    provider_list->push_back(
-        linked_ptr<ExternalExtensionProviderInterface>(
-            new DefaultAppsProvider(service, profile)));
+    // Don't bother installing default apps in locales where its known that
+    // they don't work.
+    // TODO(rogerta): Do this check dynamically once the webstore can expose
+    // an API.
+    const std::string& locale = g_browser_process->GetApplicationLocale();
+    static const char* unsupported_locales[] = {"CN", "TR", "IR"};
+    bool supported_locale = true;
+    for (size_t i = 0; i < arraysize(unsupported_locales); ++i) {
+      if (EndsWith(locale, unsupported_locales[i], false)) {
+        supported_locale = false;
+        break;
+      }
+    }
+
+    if (supported_locale) {
+      provider_list->push_back(
+          linked_ptr<ExternalExtensionProviderInterface>(
+              new DefaultAppsProvider(service, profile)));
+    }
   }
 #endif
 }
