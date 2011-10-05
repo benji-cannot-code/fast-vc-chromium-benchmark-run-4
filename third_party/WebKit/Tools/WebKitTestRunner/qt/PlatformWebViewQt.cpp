2720b51b2c051eb99fa2d6830f230f8083b70696
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-05 13:08:51 +0000

    [Qt][WK2] Unreviewed build fix.
    Don't use WebCore directly in WTR.
    
    * WebKitTestRunner/qt/PlatformWebViewQt.cpp:
    (WTR::PlatformWebView::addChromeInputField):
    (WTR::PlatformWebView::removeChromeInputField):
    (WTR::PlatformWebView::makeWebViewFirstResponder):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QApplication>
 #include <QDeclarativeProperty>
 #include <QSGView>
-#include <WebCore/NotImplemented.h>
 
 namespace WTR {
 
@@ -122,17 +121,14 @@ void PlatformWebView::postEvent(QEvent* event)
 
 void PlatformWebView::addChromeInputField()
 {
-    notImplemented();
 }
 
 void PlatformWebView::removeChromeInputField()
 {
-    notImplemented();
 }
 
 void PlatformWebView::makeWebViewFirstResponder()
 {
-    notImplemented();
 }
 
 } // namespace WTR
