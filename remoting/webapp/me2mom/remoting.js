9fb32a8ea2358e12b2b5356d196f4a85933a57d3
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 20:08:41 +0000

    Show warning if NAT is disabled.
    BUG=96735
    TEST=Start sharing on a machine on which NAT traversal is disabled. You should (eventually) see an info box at the bottom of the page.
    
    Review URL: http://codereview.chromium.org/8136015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104163 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -247,6 +247,8 @@ remoting.tryShare = function() {
   plugin.width = 0;
   plugin.height = 0;
   div.appendChild(plugin);
+  onNatTraversalPolicyChanged_(true);  // Hide warning by default.
+  plugin.onNatTraversalPolicyChanged = onNatTraversalPolicyChanged_;
   plugin.onStateChanged = onStateChanged_;
   plugin.logDebugInfo = debugInfoCallback_;
   plugin.localize(chrome.i18n.getMessage);
@@ -302,6 +304,11 @@ remoting.updateAccessCodeTimeoutElement_ = function() {
   if (!updateTimeoutStyles_()) {
     disableTimeoutCountdown_();
   }
+}
+
+function onNatTraversalPolicyChanged_(enabled) {
+  var container = document.getElementById('nat-box-container');
+  container.hidden = enabled;
 }
 
 function onStateChanged_() {
