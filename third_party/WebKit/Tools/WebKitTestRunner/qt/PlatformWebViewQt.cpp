467f174425a709a76f8b2bc2ce811b0f7166d012
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-05 09:09:33 +0000

    [Qt][WK2] Unreviewed build fix after r96643.
    * WebKitTestRunner/qt/PlatformWebViewQt.cpp:
    (WTR::PlatformWebView::addChromeInputField):
    (WTR::PlatformWebView::removeChromeInputField):
    (WTR::PlatformWebView::makeWebViewFirstResponder):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QApplication>
 #include <QDeclarativeProperty>
 #include <QSGView>
+#include <WebCore/NotImplemented.h>
 
 namespace WTR {
 
@@ -117,6 +118,21 @@ bool PlatformWebView::sendEvent(QEvent* event)
 void PlatformWebView::postEvent(QEvent* event)
 {
     QCoreApplication::postEvent(m_view, event);
+}
+
+void PlatformWebView::addChromeInputField()
+{
+    notImplemented();
+}
+
+void PlatformWebView::removeChromeInputField()
+{
+    notImplemented();
+}
+
+void PlatformWebView::makeWebViewFirstResponder()
+{
+    notImplemented();
 }
 
 } // namespace WTR
