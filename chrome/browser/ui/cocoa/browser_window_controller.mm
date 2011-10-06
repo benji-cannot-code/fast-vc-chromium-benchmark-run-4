a2c84a203b9bb83174f8f80e26f59ee6833ff664
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 22:32:34 +0000

    [Mac] Properly show and hide the avatar button for the one-profile case.
    This change makes AvatarButton a view controller that is always instantiated
    by the BrowserWindowController, and whose view is selectively hidden.
    
    BUG=99309
    TEST=As in bug.
    
    
    Review URL: http://codereview.chromium.org/8172011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/background_gradient_view.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_editor_controller.h"
-#import "chrome/browser/ui/cocoa/browser/avatar_button.h"
+#import "chrome/browser/ui/cocoa/browser/avatar_button_controller.h"
 #import "chrome/browser/ui/cocoa/browser_window_cocoa.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller_private.h"
 #import "chrome/browser/ui/cocoa/browser_window_utils.h"
@@ -1638,21 +1638,17 @@ enum {
 // view to decorate the window at the upper right. Use the same base y
 // coordinate as the tab strip.
 - (void)installAvatar {
-  // Only install if this browser window is OTR and has a tab strip.
-  if (![self shouldShowAvatar])
-    return;
-
-  // Install the image into the badge view. Hide it for now; positioning,
-  // sizing, and showing will be done by the layout code. The AvatarButton will
-  // choose which image to display based on the browser.
-  avatarButton_.reset([[AvatarButton alloc] initWithBrowser:browser_.get()]);
-  [avatarButton_ setAutoresizingMask:NSViewMinXMargin | NSViewMinYMargin];
-  [avatarButton_ setHidden:YES];
-  // The button shouldn't do anything in incognito.
-  [avatarButton_ setOpenMenuOnClick:!browser_->profile()->IsOffTheRecord()];
+  // Install the image into the badge view. Hide it for now; positioning and
+  // sizing will be done by the layout code. The AvatarButton will choose which
+  // image to display based on the browser.
+  avatarButtonController_.reset(
+      [[AvatarButtonController alloc] initWithBrowser:browser_.get()]);
+  NSView* view = [avatarButtonController_ view];
+  [view setAutoresizingMask:NSViewMinXMargin | NSViewMinYMargin];
+  [view setHidden:![self shouldShowAvatar]];
 
   // Install the view.
-  [[[[self window] contentView] superview] addSubview:avatarButton_];
+  [[[[self window] contentView] superview] addSubview:view];
 }
 
 // Documented in 10.6+, but present starting in 10.5. Called when we get a
