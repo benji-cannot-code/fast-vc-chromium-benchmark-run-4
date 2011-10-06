b7cadb8c56eab20f74e3803a2a9e92ba126002b9
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 09:55:59 +0000

    Update the Dr. Memory info page URL TBR=bruening Review URL: http://codereview.chromium.org/8166018
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104272 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/valgrind_test.py
+++ tools/valgrind/valgrind_test.py
@@ -707,7 +707,7 @@ class DrMemory(BaseTool):
   """Dr.Memory
   Dynamic memory error detector for Windows.
 
-  http://dev.chromium.org/developers/how-tos/using-valgrind/dr-memory
+  http://dev.chromium.org/developers/how-tos/using-drmemory
   It is not very mature at the moment, some things might not work properly.
   """
 
