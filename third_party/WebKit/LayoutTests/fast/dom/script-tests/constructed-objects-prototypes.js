60725b4c55c8d6226376e7d1116622533bee541e
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-06 00:40:39 +0000

    Enable WEB_AUDIO by default in the WebKit/mac port. https://bugs.webkit.org/show_bug.cgi?id=68587
    Reviewed by Simon Fraser.
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig:
    * wtf/Platform.h:
    
    Source/WebCore:
    
    No new tests; existing webaudio/ layout tests cover this.
    
    * Configurations/FeatureDefines.xcconfig:
    * WebCore.xcodeproj/project.pbxproj: Add a build step which copies audio resources to
        the WebCore.framework bundle.
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig:
    
    LayoutTests:
    
    * fast/dom/script-tests/constructed-objects-prototypes.js:
    (constructorPropertiesOnWindow):
    * platform/mac/fast/dom/Window/window-properties-expected.txt:
    * platform/mac/fast/dom/Window/window-property-descriptors-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/constructed-objects-prototypes.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/constructed-objects-prototypes.js
@@ -40,7 +40,8 @@ function constructorPropertiesOnWindow(globalObject)
             type =="Uint32ArrayConstructor" ||
             type == "FileErrorConstructor" ||
             type == "FileReaderConstructor" ||
-            type == "WebKitBlobBuilderConstructor")
+            type == "WebKitBlobBuilderConstructor" ||
+            type == "AudioContextConstructor")
             continue;
         if (!type.match('Constructor$'))
             continue;
