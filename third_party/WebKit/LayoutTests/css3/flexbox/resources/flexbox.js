698701588ba5e8100e7810100750188907eff588
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-06 19:33:43 +0000

    extend flexbox testing to test children and parents of the flexbox https://bugs.webkit.org/show_bug.cgi?id=69495
    Reviewed by Tony Chang.
    
    Checking children of flex items allows for making sure that padding/border
    get properly taken into account. Checking the parent of the flexbox allows
    for checking that margins on the flexbox itself are applied correctly.
    
    Also, make it so that failures print the outerHTML of the failing case to
    make it easier to diagnose.
    
    * css3/flexbox/001.html:
    * css3/flexbox/resources/flexbox.js:
    * css3/flexbox/writing-modes.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96851 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/css3/flexbox/resources/flexbox.js
+++ third_party/WebKit/LayoutTests/css3/flexbox/resources/flexbox.js
@@ -7,6 +7,14 @@ function insertAfter(nodeToAdd, referenceNode)
         referenceNode.parentNode.appendChild(nodeToAdd);
 }
 
+function checkSubtreeExpectedValues(parent, failures)
+{
+    checkExpectedValues(parent, failures);
+    Array.prototype.forEach.call(parent.childNodes, function(node) {
+        checkSubtreeExpectedValues(node, failures);
+    });
+}
+
 function checkExpectedValues(node, failures)
 {
     var expectedWidth = node.getAttribute && node.getAttribute("data-expected-width");
@@ -38,16 +46,12 @@ function checkFlexBoxen()
 {
     var flexboxen = document.getElementsByClassName("flexbox");
     Array.prototype.forEach.call(flexboxen, function(flexbox) {
-      var failures = [];
-      checkExpectedValues(flexbox, failures);
+        var failures = [];
+        checkExpectedValues(flexbox.parentNode, failures);
+        checkSubtreeExpectedValues(flexbox, failures);
 
-      var child = flexbox.firstChild;
-      while (child) {
-          checkExpectedValues(child, failures);
-          child = child.nextSibling;
-      }
-
-      insertAfter(document.createElement("p"), flexbox);
-      insertAfter(document.createTextNode(failures.length ? failures.join('') : "PASS"), flexbox);
+        var pre = document.createElement('pre');
+        pre.appendChild(document.createTextNode(failures.length ? "FAIL:\n" + failures.join('\n') + '\n\n' + flexbox.outerHTML : "PASS"));
+        insertAfter(pre, flexbox);
     });
 }
