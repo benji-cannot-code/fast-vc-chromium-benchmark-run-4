67e5f5048017c0249c13d6529846fa91a6947d70
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 23:50:05 +0000

    Revert 104383 - Remove FieldTypeSubGroups from the AutofillType class.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8169009
    
    TBR=isherman@chromium.org
    Review URL: http://codereview.chromium.org/8192001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_profile.cc
+++ chrome/browser/autofill/autofill_profile.cc
@@ -307,7 +307,7 @@ void AutofillProfile::SetMultiInfo(AutofillFieldType type,
     case AutofillType::EMAIL:
       CopyValuesToItems(type, values, &email_, EmailInfo());
       break;
-    case AutofillType::PHONE:
+    case AutofillType::PHONE_HOME:
       CopyValuesToItems(type,
                         values,
                         &home_number_,
@@ -346,7 +346,7 @@ void AutofillProfile::GetMultiInfoImpl(AutofillFieldType type,
     case AutofillType::EMAIL:
       CopyItemsToValues(type, email_, canonicalize, values);
       break;
-    case AutofillType::PHONE:
+    case AutofillType::PHONE_HOME:
       CopyItemsToValues(type, home_number_, canonicalize, values);
       break;
     default:
@@ -360,7 +360,7 @@ bool AutofillProfile::SupportsMultiValue(AutofillFieldType type) {
   AutofillType::FieldTypeGroup group = AutofillType(type).group();
   return group == AutofillType::NAME ||
          group == AutofillType::EMAIL ||
-         group == AutofillType::PHONE;
+         group == AutofillType::PHONE_HOME;
 }
 
 const string16 AutofillProfile::Label() const {
@@ -536,7 +536,7 @@ bool AutofillProfile::IsSubsetOf(const AutofillProfile& profile) const {
       // name saved, but |profile| lacks one, |profile| could still be a subset
       // of |this|.
       continue;
-    } else if (AutofillType(*iter).group() == AutofillType::PHONE) {
+    } else if (AutofillType(*iter).group() == AutofillType::PHONE_HOME) {
       // Phone numbers should be canonicalized prior to being compared.
       if (*iter != PHONE_HOME_WHOLE_NUMBER) {
         continue;
@@ -579,7 +579,7 @@ void AutofillProfile::OverwriteWithOrAddTo(const AutofillProfile& profile) {
       for (std::vector<string16>::iterator value_iter = new_values.begin();
            value_iter != new_values.end(); ++value_iter) {
         // Don't add duplicates.
-        if (group == AutofillType::PHONE) {
+        if (group == AutofillType::PHONE_HOME) {
           AddPhoneIfUnique(*value_iter, &existing_values);
         } else {
           std::vector<string16>::const_iterator existing_iter = std::find_if(
@@ -740,7 +740,7 @@ FormGroup* AutofillProfile::MutableFormGroupForType(AutofillFieldType type) {
     case AutofillType::COMPANY:
       form_group = &company_;
       break;
-    case AutofillType::PHONE:
+    case AutofillType::PHONE_HOME:
       form_group = &home_number_[0];
       break;
     case AutofillType::ADDRESS_HOME:
