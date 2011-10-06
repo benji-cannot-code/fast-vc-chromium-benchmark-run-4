21ff13c1394c89130839ea7742d23ffc963e7c93
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 18:24:24 +0000

    views: Fix linux touch build by include missing utf_string_conversions.h
    BUG=68267
    TBR=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8173010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104326 0039d316-1c4b-4281-b951-d872f2087c98

--- views/touchui/touch_selection_controller_impl.cc
+++ views/touchui/touch_selection_controller_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/touchui/touch_selection_controller_impl.h"
 
+#include "base/utf_string_conversions.h"
 #include "base/time.h"
 #include "grit/ui_strings.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
