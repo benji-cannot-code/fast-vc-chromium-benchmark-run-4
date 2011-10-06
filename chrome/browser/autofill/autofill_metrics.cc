c9016604bbcf2db6482a6c59a21552b9a43ec4f3
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 23:08:12 +0000

    Remove FieldTypeSubGroups from the AutofillType class.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8169009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_metrics.cc
+++ chrome/browser/autofill/autofill_metrics.cc
@@ -121,7 +121,7 @@ int GetFieldTypeGroupMetric(const AutofillFieldType field_type,
       group = EMAIL;
       break;
 
-    case AutofillType::PHONE_HOME:
+    case AutofillType::PHONE:
       group = PHONE;
       break;
 
