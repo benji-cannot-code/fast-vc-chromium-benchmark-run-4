80a863bd6b7bfce33ab1800b4beaf7c21725c284
bbudge@chromium.org <bbudge@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-07 07:04:45 +0000

    Adds a field, "saveAs", to WebFileChooserParams to present the file chooser dialog in "Save As" mode (allows the user to choose a possibly non-existent file for saving). https://bugs.webkit.org/show_bug.cgi?id=69301
    Reviewed by Darin Fisher.
    
    * public/WebFileChooserParams.h:
    (WebKit::WebFileChooserParams::WebFileChooserParams):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebFileChooserParams.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebFileChooserParams.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Google Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -39,21 +39,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 struct WebFileChooserParams {
-    // If |multiSelect| is true, the dialog allow to select multiple files.
+    // If |multiSelect| is true, the dialog allows the user to select multiple files.
     bool multiSelect;
     // If |directory| is true, the dialog allows the user to select a directory.
     bool directory;
-    // |title| is a title of a file chooser dialog. It can be an empty string.
+    // If |saveAs| is true, the dialog allows the user to select a possibly
+    // non-existent file. This can be used for a "Save As" dialog.
+    bool saveAs;
+    // |title| is the title for a file chooser dialog. It can be an empty string.
     WebString title;
     // |initialValue| is a filename which the dialog should select by default.
     // It can be an empty string.
     WebString initialValue;
     // |acceptTypes| has a comma-separated MIME types such as "audio/*,text/plain".
     // The dialog may restrict selectable files to the specified MIME types.
-    // This value comes from an 'accept' attribute value of an INPUT element.
-    // So it might be a wrong formatted string.
+    // This value comes from an 'accept' attribute value of an INPUT element, so it
+    // might be an incorrectly formatted string.
     WebString acceptTypes;
-    // |selectedFiles| has filenames which a file upload control already select.
+    // |selectedFiles| has filenames which a file upload control already selected.
     // A WebViewClient implementation may ask a user to select
     //  - removing a file from the selected files,
     //  - appending other files, or
@@ -64,6 +67,7 @@ struct WebFileChooserParams {
     WebFileChooserParams()
         : multiSelect(false)
         , directory(false)
+        , saveAs(false)
     {
     }
 };
