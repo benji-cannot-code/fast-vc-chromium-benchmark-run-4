e65e0c7f2855763c72429dda332785ffbf14871e
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 00:28:34 +0000

    Fix non-win exports, exports of v8_value_converter.
    R=jam@chromium.org
    BUG=90442
    TEST=existing stuff stays green
    
    Review URL: http://codereview.chromium.org/8135018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104402 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/v8_value_converter_impl.h
+++ content/renderer/v8_value_converter_impl.h
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_V8_VALUE_CONVERTER_IMPL_H_
 
 #include "base/compiler_specific.h"
+#include "content/common/content_export.h"
 #include "content/public/renderer/v8_value_converter.h"
 
-class V8ValueConverterImpl : public content::V8ValueConverter {
+class CONTENT_EXPORT V8ValueConverterImpl : public content::V8ValueConverter {
  public:
   V8ValueConverterImpl();
 
