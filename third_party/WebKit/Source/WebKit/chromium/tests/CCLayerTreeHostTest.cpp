500caa676287fc577acd0f2b170ca0bc16f6233b
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-07 12:17:00 +0000

    Unreviewed, rolling out r96879. http://trac.webkit.org/changeset/96879 https://bugs.webkit.org/show_bug.cgi?id=69618
    Broke CCLayerTreeHostTestShortlived1 on chromium mac.
    (Requested by podivilov on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-10-07
    
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    
    Source/WebKit/chromium:
    
    * features.gypi:
    * src/WebViewImpl.h:
    * tests/CCLayerTreeHostTest.cpp:
    (WTF::CCLayerTreeHostTest::CCLayerTreeHostTest):
    (WTF::CCLayerTreeHostTest::onEndTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#if USE(THREADED_COMPOSITING)
+
 #include "cc/CCLayerTreeHost.h"
 
 #include "cc/CCLayerImpl.h"
@@ -169,7 +171,9 @@ public:
     {
     }
 
+#if !USE(THREADED_COMPOSITING)
     virtual void scheduleComposite() { }
+#endif
 
 private:
     explicit MockLayerTreeHostClient(TestHooks* testHooks) : m_testHooks(testHooks) { }
@@ -213,7 +217,11 @@ protected:
     {
         m_webThread = adoptPtr(webKitPlatformSupport()->createThread("CCLayerTreeHostTest"));
         WebCompositor::setThread(m_webThread.get());
+#if USE(THREADED_COMPOSITING)
         m_settings.enableCompositorThread = true;
+#else
+        m_settings.enableCompositorThread = false;
+#endif
     }
 
     void doBeginTest();
@@ -226,11 +234,7 @@ protected:
     static void onEndTest(void* self)
     {
         ASSERT(isMainThread());
-        // webkit_support::QuitMessageLoop() simply posts a task on the loop to quit. Any pending messages enqueued
-        // before the Quit task will still run. For this test scenario we want to run all pending messages before
-        // tearing down m_layerTreeHost, so we spin the loop after posting the Quit task.
         webkit_support::QuitMessageLoop();
-        webkit_support::RunAllPendingMessages();
         CCLayerTreeHostTest* test = static_cast<CCLayerTreeHostTest*>(self);
         ASSERT(test);
         test->m_layerTreeHost.clear();
@@ -713,3 +717,5 @@ TEST_F(CCLayerTreeHostTestScrollMultipleRedraw, run)
 }
 
 } // namespace
+
+#endif
