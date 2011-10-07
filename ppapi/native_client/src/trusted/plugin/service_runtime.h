813e735d6fdcb5feb81ddc5ac52c59575858ce0f
bsy@google.com <bsy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 22:24:42 +0000

    chromium side changes to enable nexe exit status to be reported to JavaScript
    this CL contains changes to the plugin to propagate the PPAPI event
    handler nexe's exit status to JavaScript via an integer exitStatus
    property, just like the lastError string property.
    
    this CL depends on NaCl side CL
    http://codereview.chromium.org/8139016/ which was committed as NaCl
    r6851 and requires DEPS's nacl_revision value to be at least 6851.
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=97833
    TEST= exit_status_tests (NaCl-side)
    Review URL: http://codereview.chromium.org/8136022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104578 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.h
+++ ppapi/native_client/src/trusted/plugin/service_runtime.h
@@ -93,6 +93,7 @@ class PluginReverseInterface: public nacl::ReverseInterface {
  public:
   PluginReverseInterface(nacl::WeakRefAnchor* anchor,
                          Plugin* plugin,
+                         ServiceRuntime* service_runtime,
                          pp::CompletionCallback init_done_cb,
                          pp::CompletionCallback crash_cb);
 
@@ -111,6 +112,8 @@ class PluginReverseInterface: public nacl::ReverseInterface {
   virtual bool CloseManifestEntry(int32_t desc);
 
   virtual void ReportCrash();
+
+  virtual void ReportExitStatus(int exit_status);
 
  protected:
   virtual void Log_MainThreadContinuation(LogToJavaScriptConsoleResource* p,
@@ -132,6 +135,7 @@ class PluginReverseInterface: public nacl::ReverseInterface {
   nacl::WeakRefAnchor* anchor_;  // holds a ref
   Plugin* plugin_;  // value may be copied, but should be used only in
                     // main thread in WeakRef-protected callbacks.
+  ServiceRuntime* service_runtime_;
   NaClMutex mu_;
   NaClCondVar cv_;
   bool shutting_down_;
@@ -165,6 +169,13 @@ class ServiceRuntime {
   Plugin* plugin() const { return plugin_; }
   void Shutdown();
 
+  // exit_status is -1 when invalid; when we set it, we will ensure
+  // that it is non-negative (the portion of the exit status from the
+  // nexe that is transferred is the low 8 bits of the argument to the
+  // exit syscall).
+  int exit_status();  // const, but grabs mutex etc.
+  void set_exit_status(int exit_status);
+
   nacl::DescWrapper* async_receive_desc() { return async_receive_desc_.get(); }
   nacl::DescWrapper* async_send_desc() { return async_send_desc_.get(); }
 
@@ -188,6 +199,9 @@ class ServiceRuntime {
   nacl::WeakRefAnchor* anchor_;
 
   PluginReverseInterface* rev_interface_;
+
+  NaClMutex mu_;
+  int exit_status_;
 };
 
 }  // namespace plugin
