ee5cdbd83325bcf2fdd104a0c3933d8d03d8a9f5
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 16:06:03 +0000

    Move kExtensionScheme from content to chrome
    BUG=89642
    TEST=everything still works
    
    Review URL: http://codereview.chromium.org/8159006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_view_host_observer.cc
+++ chrome/browser/renderer_host/chrome_render_view_host_observer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_messages.h"
 #include "chrome/common/render_messages.h"
+#include "chrome/common/url_constants.h"
 #include "content/browser/browsing_instance.h"
 #include "content/browser/child_process_security_policy.h"
 #include "content/browser/renderer_host/render_view_host_delegate.h"
@@ -21,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/site_instance.h"
 #include "content/common/bindings_policy.h"
 #include "content/common/notification_service.h"
-#include "content/common/url_constants.h"
 #include "content/common/view_messages.h"
 
 ChromeRenderViewHostObserver::ChromeRenderViewHostObserver(
--- content/common/url_constants.h
+++ content/common/url_constants.h
@@ -13,8 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-// Null terminated list of schemes that are savable.
-extern const char* kSavableSchemes[];
+// Null terminated list of schemes that are savable. This function can be
+// invoked on any thread.
+const char** GetSavableSchemes();
 
 // Canonical schemes you can use as input to GURL.SchemeIs().
 // TODO(jam): some of these don't below in the content layer, but are accessed
@@ -26,7 +27,6 @@ CONTENT_EXPORT extern const char kChromeInternalScheme[];
 CONTENT_EXPORT extern const char kChromeUIScheme[];  // Used for WebUIs.
 CONTENT_EXPORT extern const char kCrosScheme[];      // Used for ChromeOS.
 CONTENT_EXPORT extern const char kDataScheme[];
-CONTENT_EXPORT extern const char kExtensionScheme[];
 CONTENT_EXPORT extern const char kFileScheme[];
 CONTENT_EXPORT extern const char kFileSystemScheme[];
 CONTENT_EXPORT extern const char kFtpScheme[];
@@ -46,6 +46,13 @@ CONTENT_EXPORT extern const char kAboutCrashURL[];
 
 // Special URL used to start a navigation to an error page.
 extern const char kUnreachableWebDataURL[];
+
+// Call near the beginning of startup to register the content layer's internal
+// URLs that should be parsed as "standard" with the googleurl library. The
+// embedder can pass a 0-terminated list of additional schemes that should be
+// savable, or NULL if the standard list is sufficient.
+void CONTENT_EXPORT RegisterContentSchemes(
+    const char** additional_savable_schemes);
 
 }  // namespace chrome
 
