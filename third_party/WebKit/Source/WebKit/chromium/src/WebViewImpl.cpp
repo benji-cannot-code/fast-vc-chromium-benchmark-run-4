60f16a3a15d45a10f37bd1f4aee822407aed027c
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-07 00:16:26 +0000

    [chromium] Remove obsolete WebWidgetClient::didActivateAcceleratedCompositing call https://bugs.webkit.org/show_bug.cgi?id=69576
    Reviewed by Darin Fisher.
    
    The chromium implementation of this function went away at r104017.
    
    * public/WebWidgetClient.h:
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::setIsAcceleratedCompositingActive):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96878 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -2633,18 +2633,15 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
 
     if (!active) {
         m_isAcceleratedCompositingActive = false;
-        // We need to finish all GL rendering before sending
-        // didActivateAcceleratedCompositing(false) to prevent
+        // We need to finish all GL rendering before sending didDeactivateCompositor() to prevent
         // flickering when compositing turns off.
         if (m_layerTreeHost)
             m_layerTreeHost->finishAllRendering();
-        m_client->didActivateAcceleratedCompositing(false);
         m_client->didDeactivateCompositor();
     } else if (m_layerTreeHost) {
         m_isAcceleratedCompositingActive = true;
         updateLayerTreeViewport();
 
-        m_client->didActivateAcceleratedCompositing(true);
         m_client->didActivateCompositor(m_layerTreeHost->compositorIdentifier());
     } else {
         TRACE_EVENT("WebViewImpl::setIsAcceleratedCompositingActive(true)", this, 0);
@@ -2660,7 +2657,6 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
         m_layerTreeHost = CCLayerTreeHost::create(this, m_nonCompositedContentHost->topLevelRootLayer()->platformLayer(), ccSettings);
         if (m_layerTreeHost) {
             updateLayerTreeViewport();
-            m_client->didActivateAcceleratedCompositing(true);
             m_client->didActivateCompositor(m_layerTreeHost->compositorIdentifier());
             m_isAcceleratedCompositingActive = true;
             m_compositorCreationFailed = false;
@@ -2668,7 +2664,6 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
                 m_pageOverlay->update();
         } else {
             m_isAcceleratedCompositingActive = false;
-            m_client->didActivateAcceleratedCompositing(false);
             m_client->didDeactivateCompositor();
             m_compositorCreationFailed = true;
         }
