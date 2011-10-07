53249c634f9d4e952de3a55f258c635c870db4c4
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 01:42:21 +0000

    [web-ui] Migrate net_internals_ui.cc to base::bind() & base::callback().
    BUG=98478
    Review URL: http://codereview.chromium.org/8138010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/net_internals_ui_browsertest.cc
+++ chrome/browser/ui/webui/net_internals_ui_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/utf_string_conversions.h"
@@ -49,8 +51,10 @@ NetInternalsTestMessageHandler::NetInternalsTestMessageHandler()
 }
 
 void NetInternalsTestMessageHandler::RegisterMessages() {
-  web_ui_->RegisterMessageCallback("openNewTab", NewCallback(
-      this, &NetInternalsTestMessageHandler::OpenNewTab));
+  web_ui_->RegisterMessageCallback(
+      "openNewTab",
+      base::Bind(&NetInternalsTestMessageHandler::OpenNewTab,
+                 base::Unretained(this)));
 }
 
 void NetInternalsTestMessageHandler::OpenNewTab(const ListValue* list_value) {
