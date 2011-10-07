1d7596e750d409d0f2a1a471a493d21f07c2e7d0
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-07 10:12:15 +0000

    2011-10-07  Pavel Podivilov  <podivilov@chromium.org>
            Unreviewed, fix mac build after r96909.
    
            * public/WebContentLayer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@96924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebContentLayer.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebContentLayer.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 class WebContentLayerClient;
 class WebContentLayerImpl;
-class WebFloatRect;
+struct WebFloatRect;
 class WebLayerClient;
 
 class WebContentLayer : public WebLayer {
