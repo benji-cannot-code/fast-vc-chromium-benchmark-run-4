2363d98e65fbab5f3a04f1a5cb7a44cdbd89c4ff
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 17:50:53 +0000

    [web-ui] Migrate metrics_handler.cc to base::Bind().
    BUG=98478
    Review URL: http://codereview.chromium.org/8190006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/metrics_handler.cc
+++ chrome/browser/ui/webui/ntp/metrics_handler.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ntp/metrics_handler.h"
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
 #include "base/utf_string_conversions.h"
@@ -22,12 +24,16 @@ MetricsHandler::MetricsHandler() {}
 MetricsHandler::~MetricsHandler() {}
 
 void MetricsHandler::RegisterMessages() {
-  web_ui_->RegisterMessageCallback("metricsHandler:recordAction",
-      NewCallback(this, &MetricsHandler::HandleRecordAction));
-  web_ui_->RegisterMessageCallback("metricsHandler:recordInHistogram",
-      NewCallback(this, &MetricsHandler::HandleRecordInHistogram));
-  web_ui_->RegisterMessageCallback("metricsHandler:logEventTime",
-      NewCallback(this, &MetricsHandler::HandleLogEventTime));
+  web_ui_->RegisterMessageCallback(
+      "metricsHandler:recordAction",
+      base::Bind(&MetricsHandler::HandleRecordAction, base::Unretained(this)));
+  web_ui_->RegisterMessageCallback(
+      "metricsHandler:recordInHistogram",
+      base::Bind(&MetricsHandler::HandleRecordInHistogram,
+                 base::Unretained(this)));
+  web_ui_->RegisterMessageCallback(
+      "metricsHandler:logEventTime",
+      base::Bind(&MetricsHandler::HandleLogEventTime, base::Unretained(this)));
 }
 
 void MetricsHandler::HandleRecordAction(const ListValue* args) {
