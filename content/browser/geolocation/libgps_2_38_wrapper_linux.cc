abdf3959069dbc089546bfd7409f2d8034f84615
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 15:29:14 +0000

    Revert 104481 - Fix gps.h license issue, by deleting it
    BUG=98132,99326
    TEST=used gpsfake (from lucid package 2.92) and opened maps.google.com/maps/m
    
    Review URL: http://codereview.chromium.org/8171006
    
    TBR=joth@chromium.org
    Review URL: http://codereview.chromium.org/8199006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104482 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/libgps_2_38_wrapper_linux.cc
+++ content/browser/geolocation/libgps_2_38_wrapper_linux.cc
@@ -3,24 +3,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Defines a variant of libgps wrapper for use with the 2.94 release of the API.
+// Defines a variant of libgps wrapper for use with the 2.38 release of the API.
 
 #include "content/browser/geolocation/libgps_wrapper_linux.h"
 
+// This lot needed for DataWaiting()
+#include <sys/socket.h>
+#include <sys/time.h>
+
 #include "base/logging.h"
 #include "content/common/geoposition.h"
-#include "third_party/gpsd/release-2.94/gps.h"
+#include "third_party/gpsd/release-2.38/gps.h"
 
-class LibGpsV294 : public LibGps {
+class LibGpsV238 : public LibGps {
  public:
-  explicit LibGpsV294(LibGpsLibraryWrapper* dl_wrapper) : LibGps(dl_wrapper) {}
+  explicit LibGpsV238(LibGpsLibraryWrapper* dl_wrapper) : LibGps(dl_wrapper) {}
 
   // LibGps
   virtual bool StartStreaming() {
-    return library().stream(WATCH_ENABLE) == 0;
+    return library().query("w+x\n") == 0;
   }
   virtual bool DataWaiting() {
-    return library().waiting();
+    // Unfortunately the 2.38 API has no public method for non-blocking test
+    // for new data arrived, so we must contrive our own by doing a select() on
+    // the underlying struct's socket fd.
+    fd_set fds;
+    struct timeval timeout = {0};
+    int fd = library().data().gps_fd;
+
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+
+    int ret = select(fd + 1, &fds, NULL, NULL, &timeout);
+    if (ret == -1) {
+      LOG(WARNING) << "libgps socket select failed: " << ret;
+      return false;
+    }
+    DCHECK_EQ(!!ret, !!FD_ISSET(fd, &fds));
+    return ret;
   }
   virtual bool GetPositionIfFixed(Geoposition* position) {
     // This function is duplicated between the library versions, however it
@@ -32,7 +52,7 @@ class LibGpsV294 : public LibGps {
       return false;
     position->latitude = gps_data.fix.latitude;
     position->longitude = gps_data.fix.longitude;
-    position->accuracy = std::max(gps_data.fix.epx, gps_data.fix.epy);
+    position->accuracy = gps_data.fix.eph;
     position->altitude = gps_data.fix.altitude;
     position->altitude_accuracy = gps_data.fix.epv;
     position->heading = gps_data.fix.track;
@@ -41,9 +61,9 @@ class LibGpsV294 : public LibGps {
   }
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(LibGpsV294);
+  DISALLOW_COPY_AND_ASSIGN(LibGpsV238);
 };
 
-LibGps* LibGps::NewV294(LibGpsLibraryWrapper* dl_wrapper) {
-  return new LibGpsV294(dl_wrapper);
+LibGps* LibGps::NewV238(LibGpsLibraryWrapper* dl_wrapper) {
+  return new LibGpsV238(dl_wrapper);
 }
