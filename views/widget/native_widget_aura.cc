b497e82b2b7e290a9d5c08c70a3748c0b86cb7e6
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 22:50:51 +0000

    Don't send mouse event to invisible window   added DCHECK in NativeWidgetAura to make sure invisible widget doesn't receive event.
    BUG=none
    TEST=open menu, close menu without choosing item. chrome won't crash.
    
    Review URL: http://codereview.chromium.org/8202020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104591 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_aura.cc
+++ views/widget/native_widget_aura.cc
@@ -474,6 +474,7 @@ void NativeWidgetAura::OnBlur() {
 }
 
 bool NativeWidgetAura::OnKeyEvent(aura::KeyEvent* event) {
+  DCHECK(window_->IsVisible());
   InputMethod* input_method = GetWidget()->GetInputMethod();
   DCHECK(input_method);
   // TODO(oshima): DispatchKeyEvent should return bool?
@@ -490,6 +491,7 @@ int NativeWidgetAura::GetNonClientComponent(const gfx::Point& point) const {
 }
 
 bool NativeWidgetAura::OnMouseEvent(aura::MouseEvent* event) {
+  DCHECK(window_->IsVisible());
   return delegate_->OnMouseEvent(MouseEvent(event));
 }
 
