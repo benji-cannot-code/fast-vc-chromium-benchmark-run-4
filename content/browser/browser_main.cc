89a60f5d814ed2c12fd67c3c334d7ce077974e66
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 17:34:21 +0000

    Initialize glib for all linux because all linux variants uses glib.  aura chrome fails on ubuntu 11.04 without this  remove base::MessagePumpX::DisableGtkMessagePump() from aura as we nolonger has gtk on aura.
    BUG=97131
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8195003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104508 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main.cc
+++ content/browser/browser_main.cc
@@ -39,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util_win.h"
 #endif
 
+#if defined(OS_LINUX)
+#include <glib-object.h>
+#endif
+
 #if defined(OS_CHROMEOS)
 #include <dbus/dbus-glib.h>
 #endif
@@ -105,7 +109,7 @@ void SetupSandbox(const CommandLine& parsed_command_line) {
 }
 #endif
 
-#if defined(TOOLKIT_USES_GTK)
+#if defined(OS_LINUX)
 static void GLibLogHandler(const gchar* log_domain,
                            GLogLevelFlags log_level,
                            const gchar* message,
@@ -277,7 +281,7 @@ void BrowserMainParts::InitializeToolkit() {
   // of intersecting ifdefs we have.  To keep it easy to follow, there
   // are no #else branches on any #ifs.
 
-#if defined(TOOLKIT_USES_GTK)
+#if defined(OS_LINUX)
   // We want to call g_thread_init(), but in some codepaths (tests) it
   // is possible it has already been called.  In older versions of
   // GTK, it is an error to call g_thread_init twice; unfortunately,
@@ -298,10 +302,7 @@ void BrowserMainParts::InitializeToolkit() {
   // TODO(satorux, stevenjb): remove this once it is no longer needed.
   dbus_g_thread_init();
 #endif
-#if defined(USE_AURA)
-  // TODO(oshima|saintlou): Remove this once we remove gtk from build
-  base::MessagePumpX::DisableGtkMessagePump();
-#else
+#if !defined(USE_AURA)
   gfx::GtkInitFromCommandLine(parameters().command_line_);
 #endif
   SetUpGLibLogHandler();
