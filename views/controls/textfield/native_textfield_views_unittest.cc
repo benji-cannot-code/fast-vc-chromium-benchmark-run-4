bf5da9dc9f2f36daa62255e10a2956da520dd8f8
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 08:03:58 +0000

    Simple clipboard support for aura
    BUG=99128
    TEST=more views_unittests tests pass on linux/aura
    
    This include fixes from http://codereview.chromium.org/8150001
    
    Review URL: http://codereview.chromium.org/8165016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104448 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/textfield/native_textfield_views_unittest.cc
+++ views/controls/textfield/native_textfield_views_unittest.cc
@@ -35,49 +35,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/widget/native_widget_private.h"
 #include "views/widget/widget.h"
 
-// Bug http://crbug.com/99128
-#if defined(USE_AURA) && defined(OS_LINUX)
-#define MAYBE_KeyTest FAILS_KeyTest
-#define MAYBE_ControlAndSelectTest FAILS_ControlAndSelectTest
-#define MAYBE_InsertionDeletionTest FAILS_InsertionDeletionTest
-#define MAYBE_OnKeyPressReturnValueTest FAILS_OnKeyPressReturnValueTest
-#define MAYBE_CursorMovement FAILS_CursorMovement
-
 // Drag and drop for aura in linux hasn't been implemented yet.
 // Bug http://crbug.com/97845
+#if defined(USE_AURA) && defined(OS_LINUX)
 #define MAYBE_DragAndDrop_InitiateDrag DISABLED_DragAndDrop_InitiateDrag
 #define MAYBE_DragAndDrop_ToTheLeft DISABLED_DragAndDrop_ToTheLeft
 #define MAYBE_DragAndDrop_ToTheRight DISABLED_DragAndDrop_ToTheRight
 #define MAYBE_DragAndDrop_Canceled DISABLED_DragAndDrop_Canceled
-
-#define MAYBE_ReadOnlyTest FAILS_ReadOnlyTest
-#define MAYBE_TextInputClientTest FAILS_TextInputClientTest
-#define MAYBE_UndoRedoTest FAILS_UndoRedoTest
-#define MAYBE_TextCursorDisplayTest FAILS_TextCursorDisplayTest
-#define MAYBE_TextCursorDisplayInRTLTest FAILS_TextCursorDisplayInRTLTest
-#define MAYBE_HitOutsideTextAreaTest FAILS_HitOutsideTextAreaTest
-#define MAYBE_HitOutsideTextAreaInRTLTest FAILS_HitOutsideTextAreaInRTLTest
-#define MAYBE_OverflowTest FAILS_OverflowTest
-#define MAYBE_OverflowInRTLTest FAILS_OverflowInRTLTest
 #else
-#define MAYBE_KeyTest KeyTest
-#define MAYBE_ControlAndSelectTest ControlAndSelectTest
-#define MAYBE_InsertionDeletionTest InsertionDeletionTest
-#define MAYBE_OnKeyPressReturnValueTest OnKeyPressReturnValueTest
-#define MAYBE_CursorMovement CursorMovement
 #define MAYBE_DragAndDrop_InitiateDrag DragAndDrop_InitiateDrag
 #define MAYBE_DragAndDrop_ToTheLeft DragAndDrop_ToTheLeft
 #define MAYBE_DragAndDrop_ToTheRight DragAndDrop_ToTheRight
 #define MAYBE_DragAndDrop_Canceled DragAndDrop_Canceled
-#define MAYBE_ReadOnlyTest ReadOnlyTest
-#define MAYBE_TextInputClientTest TextInputClientTest
-#define MAYBE_UndoRedoTest UndoRedoTest
-#define MAYBE_TextCursorDisplayTest TextCursorDisplayTest
-#define MAYBE_TextCursorDisplayInRTLTest TextCursorDisplayInRTLTest
-#define MAYBE_HitOutsideTextAreaTest HitOutsideTextAreaTest
-#define MAYBE_HitOutsideTextAreaInRTLTest HitOutsideTextAreaInRTLTest
-#define MAYBE_OverflowTest OverflowTest
-#define MAYBE_OverflowInRTLTest OverflowInRTLTest
 #endif  // OS_LINUX && USE_AURA
 
 namespace {
@@ -380,7 +349,7 @@ TEST_F(NativeTextfieldViewsTest, ModelChangesTest) {
   EXPECT_TRUE(last_contents_.empty());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_KeyTest) {
+TEST_F(NativeTextfieldViewsTest, KeyTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   SendKeyEvent(ui::VKEY_C, true, false);
   EXPECT_STR_EQ("C", textfield_->text());
@@ -401,7 +370,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_KeyTest) {
   EXPECT_STR_EQ("cC1!!", last_contents_);
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_ControlAndSelectTest) {
+TEST_F(NativeTextfieldViewsTest, ControlAndSelectTest) {
   // Insert a test string in a textfield.
   InitTextfield(Textfield::STYLE_DEFAULT);
   textfield_->SetText(ASCIIToUTF16("one two three"));
@@ -436,7 +405,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_ControlAndSelectTest) {
   EXPECT_STR_EQ("ZERO ", textfield_->GetSelectedText());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_InsertionDeletionTest) {
+TEST_F(NativeTextfieldViewsTest, InsertionDeletionTest) {
   // Insert a test string in a textfield.
   InitTextfield(Textfield::STYLE_DEFAULT);
   char test_str[] = "this is a test";
@@ -565,7 +534,7 @@ TEST_F(NativeTextfieldViewsTest, TextInputType) {
   EXPECT_EQ(ui::TEXT_INPUT_TYPE_NONE, GetTextInputType());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_OnKeyPressReturnValueTest) {
+TEST_F(NativeTextfieldViewsTest, OnKeyPressReturnValueTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
 
   // Character keys will be handled by input method.
@@ -596,7 +565,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_OnKeyPressReturnValueTest) {
   EXPECT_FALSE(textfield_->key_handled());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_CursorMovement) {
+TEST_F(NativeTextfieldViewsTest, CursorMovement) {
   InitTextfield(Textfield::STYLE_DEFAULT);
 
   // Test with trailing whitespace.
@@ -991,7 +960,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_DragAndDrop_Canceled) {
 }
 #endif
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_ReadOnlyTest) {
+TEST_F(NativeTextfieldViewsTest, ReadOnlyTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   textfield_->SetText(ASCIIToUTF16(" one two three "));
   textfield_->SetReadOnly(true);
@@ -1048,7 +1017,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_ReadOnlyTest) {
   EXPECT_STR_EQ(" four five six ", textfield_->GetSelectedText());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_TextInputClientTest) {
+TEST_F(NativeTextfieldViewsTest, TextInputClientTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   TextInputClient* client = textfield_->GetTextInputClient();
   EXPECT_TRUE(client);
@@ -1138,7 +1107,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_TextInputClientTest) {
   EXPECT_TRUE(textfield_->GetTextInputClient());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_UndoRedoTest) {
+TEST_F(NativeTextfieldViewsTest, UndoRedoTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   SendKeyEvent(ui::VKEY_A);
   EXPECT_STR_EQ("a", textfield_->text());
@@ -1246,7 +1215,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_UndoRedoTest) {
   EXPECT_STR_EQ("ab3", textfield_->text());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_TextCursorDisplayTest) {
+TEST_F(NativeTextfieldViewsTest, TextCursorDisplayTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   // LTR-RTL string in LTR context.
   SendKeyEvent('a');
@@ -1298,7 +1267,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_TextCursorDisplayTest) {
   EXPECT_LT(prev_x, x);
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_TextCursorDisplayInRTLTest) {
+TEST_F(NativeTextfieldViewsTest, TextCursorDisplayInRTLTest) {
   std::string locale = l10n_util::GetApplicationLocale("");
   base::i18n::SetICUDefaultLocale("he");
 
@@ -1416,7 +1385,7 @@ TEST_F(NativeTextfieldViewsTest, HitInsideTextAreaTest) {
   }
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_HitOutsideTextAreaTest) {
+TEST_F(NativeTextfieldViewsTest, HitOutsideTextAreaTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
 
   // LTR-RTL string in LTR context.
@@ -1456,7 +1425,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_HitOutsideTextAreaTest) {
   EXPECT_EQ(bound, GetCursorBounds());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_HitOutsideTextAreaInRTLTest) {
+TEST_F(NativeTextfieldViewsTest, HitOutsideTextAreaInRTLTest) {
   std::string locale = l10n_util::GetApplicationLocale("");
   base::i18n::SetICUDefaultLocale("he");
 
@@ -1510,7 +1479,7 @@ void OverflowCursorBoundTestVerifier(const gfx::Rect& display,
   EXPECT_GE(display.bottom(), bound.bottom());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_OverflowTest) {
+TEST_F(NativeTextfieldViewsTest, OverflowTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
 
   string16 str;
@@ -1540,7 +1509,7 @@ TEST_F(NativeTextfieldViewsTest, MAYBE_OverflowTest) {
   EXPECT_EQ(501U, textfield_->GetCursorPosition());
 }
 
-TEST_F(NativeTextfieldViewsTest, MAYBE_OverflowInRTLTest) {
+TEST_F(NativeTextfieldViewsTest, OverflowInRTLTest) {
   std::string locale = l10n_util::GetApplicationLocale("");
   base::i18n::SetICUDefaultLocale("he");
 
