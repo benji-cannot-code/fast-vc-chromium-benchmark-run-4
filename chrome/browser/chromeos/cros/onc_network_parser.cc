8c0c9bc73c5132a83c697f940123ee15fc4c4a68
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 19:28:46 +0000

    fix compile breakage caused by moving json_value_serializer to base and browser/chromeis/cros/onc_network_parser.cc getting landed after I had prepared the change.
    TBR=chocobo@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8206011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/onc_network_parser.cc
+++ chrome/browser/chromeos/cros/onc_network_parser.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/native_network_constants.h"
 #include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
-#include "content/common/json_value_serializer.h"
+#include "base/json/json_value_serializer.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace chromeos {
