dd53115b101ffb446696b0a8be50c632e174bbbc
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-07 16:03:17 +0000

    Allow force shared worker termination
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/8198004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/workers/workers.js
+++ chrome/browser/resources/workers/workers.js
@@ -23,6 +23,11 @@ function openDevTools(workerProcessHostId, workerRouteId) {
               [String(workerProcessHostId), String(workerRouteId)]);
 }
 
+function reloadWorker(workerProcessHostId, workerRouteId) {
+  chrome.send("terminateWorker",
+              [String(workerProcessHostId), String(workerRouteId)]);
+}
+
 function populateWorkerList() {
   var data = requestData();
 
@@ -42,6 +47,18 @@ function populateWorkerList() {
     link.addEventListener(
         "click",
         openDevTools.bind(this,
+                          workerData.workerProcessHostId,
+                          workerData.workerRouteId),
+        true);
+    column.appendChild(link);
+    row.appendChild(column);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.textContent = "terminate";
+    link.addEventListener(
+        "click",
+        reloadWorker.bind(this,
                           workerData.workerProcessHostId,
                           workerData.workerRouteId),
         true);
--- chrome/browser/ui/webui/workers_ui.cc
+++ chrome/browser/ui/webui/workers_ui.cc
@@ -21,7 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/debugger/worker_devtools_manager.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/browser/worker_host/worker_process_host.h"
+#include "content/browser/worker_host/worker_service.h"
 #include "content/common/devtools_messages.h"
+#include "content/common/worker_messages.h"
 #include "grit/generated_resources.h"
 #include "grit/workers_resources.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static const char kWorkersDataFile[] = "workers_data.json";
 
 static const char kOpenDevToolsCommand[]  = "openDevTools";
+static const char kTerminateWorkerCommand[]  = "terminateWorker";
 
 static const char kWorkerProcessHostIdField[]  = "workerProcessHostId";
 static const char kWorkerRouteIdField[]  = "workerRouteId";
@@ -104,6 +107,7 @@ class WorkersDOMHandler : public WebUIMessageHandler {
 
   // Callback for "openDevTools" message.
   void HandleOpenDevTools(const ListValue* args);
+  void HandleTerminateWorker(const ListValue* args);
 
   DISALLOW_COPY_AND_ASSIGN(WorkersDOMHandler);
 };
@@ -111,6 +115,9 @@ class WorkersDOMHandler : public WebUIMessageHandler {
 void WorkersDOMHandler::RegisterMessages() {
   web_ui_->RegisterMessageCallback(kOpenDevToolsCommand,
       base::Bind(&WorkersDOMHandler::HandleOpenDevTools,
+                 base::Unretained(this)));
+  web_ui_->RegisterMessageCallback(kTerminateWorkerCommand,
+      base::Bind(&WorkersDOMHandler::HandleTerminateWorker,
                  base::Unretained(this)));
 }
 
@@ -140,6 +147,33 @@ void WorkersDOMHandler::HandleOpenDevTools(const ListValue* args) {
   window->Show(DEVTOOLS_TOGGLE_ACTION_NONE);
   DevToolsManager::GetInstance()->RegisterDevToolsClientHostFor(agent_host,
                                                                 window);
+}
+
+static void TerminateWorker(int worker_process_id, int worker_route_id) {
+  for (BrowserChildProcessHost::Iterator iter(ChildProcessInfo::WORKER_PROCESS);
+       !iter.Done(); ++iter) {
+    if (iter->id() == worker_process_id) {
+      (*iter)->Send(new WorkerMsg_TerminateWorkerContext(worker_route_id));
+      return;
+    }
+  }
+}
+
+void WorkersDOMHandler::HandleTerminateWorker(const ListValue* args) {
+  std::string worker_process_host_id_str;
+  std::string worker_route_id_str;
+  int worker_process_host_id;
+  int worker_route_id;
+  CHECK(args->GetSize() == 2);
+  CHECK(args->GetString(0, &worker_process_host_id_str));
+  CHECK(args->GetString(1, &worker_route_id_str));
+  CHECK(base::StringToInt(worker_process_host_id_str,
+                          &worker_process_host_id));
+  CHECK(base::StringToInt(worker_route_id_str, &worker_route_id));
+
+  BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
+      NewRunnableFunction(&TerminateWorker, worker_process_host_id,
+                          worker_route_id));
 }
 
 }  // namespace
