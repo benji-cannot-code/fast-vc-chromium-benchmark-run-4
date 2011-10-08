0598da97fe50c63c25fa7176e04861f30ceaec8c
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-08 03:09:04 +0000

    [Sync UI] When signed in, choosing the sync wrench menu item should navigate to personal options.
    BUG=99538
    TEST=Sign in to chrome. Choose the wrench menu item "Signed in as...". The browser should navigate to chrome://settings/personal, not chrome://settings/syncSetup.
    
    
    Review URL: http://codereview.chromium.org/8198023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104643 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -5252,7 +5252,7 @@ void Browser::ShowSyncSetup() {
   ProfileSyncService* service =
       profile()->GetOriginalProfile()->GetProfileSyncService();
   if (service->HasSyncSetupCompleted())
-    ShowOptionsTab(chrome::kSyncSetupSubPage);
+    ShowOptionsTab(chrome::kPersonalOptionsSubPage);
   else
     service->ShowLoginDialog();
 }
