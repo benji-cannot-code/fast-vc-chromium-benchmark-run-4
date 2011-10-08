3438a9fd0b69afedd9e076b39f785952aab0e921
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-08 02:25:04 +0000

    Register route with compositor thread whenever compositor is activated if threaded compositing is on
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8188001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104635 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_switches.h"
 #include "content/common/swapped_out_messages.h"
 #include "content/common/view_messages.h"
+#include "content/renderer/gpu/compositor_thread.h"
 #include "content/renderer/render_process.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/renderer_webkitplatformsupport_impl.h"
@@ -916,8 +917,11 @@ void RenderWidget::didScrollRect(int dx, int dy, const WebRect& clip_rect) {
       this, &RenderWidget::InvalidationCallback));
 }
 
-void RenderWidget::didActivateCompositor(int compositorIdentifier) {
+void RenderWidget::didActivateCompositor(int compositor_identifier) {
   TRACE_EVENT0("gpu", "RenderWidget::didActivateCompositor");
+
+  RenderThreadImpl::current()->compositor_thread()->
+      AddCompositor(routing_id_, compositor_identifier);
 
   is_accelerated_compositing_active_ = true;
   Send(new ViewHostMsg_DidActivateAcceleratedCompositing(
