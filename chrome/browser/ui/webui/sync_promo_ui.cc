f30b7cc1e9683bae12683939688c11a42619d22a
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-08 01:19:15 +0000

    Enable sync promo by default
    This CL enables the sync promo by default. This means that users on Canary will see the promo on new installs or when they create a new profile.
    
    For beta and stable users we're planning to throttle visibility using a server side promo flag. (Note, this hasn't been implemented yet).
    
    BUG=89276
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8109006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo_ui.cc
+++ chrome/browser/ui/webui/sync_promo_ui.cc
@@ -89,11 +89,6 @@ bool SyncPromoUI::ShouldShowSyncPromo(Profile* profile) {
   return false;
 #endif
 
-  // Temporarily hide this feature behind a command line flag.
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  if (!command_line->HasSwitch(switches::kSyncShowPromo))
-    return false;
-
   // Honor the sync policies.
   if (!profile->GetOriginalProfile()->IsSyncAccessible())
     return false;
@@ -115,6 +110,10 @@ bool SyncPromoUI::ShouldShowSyncPromoAtStartup(Profile* profile,
                                                bool is_new_profile) {
   if (!ShouldShowSyncPromo(profile))
     return false;
+
+  const CommandLine& command_line  = *CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kNoFirstRun))
+    is_new_profile = false;
 
   RegisterSyncPromoPrefs(profile);
   if (!is_new_profile) {
