75af00f1869d550d33a35e004ffbce2305cd26d4
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-09 23:00:24 +0000

    Fix the issue that PPB_CursorControl_Dev.SetCursor doesn't take effect until the next input event.
    BUG=94981
    TEST= 1) run manual test in ppapi/example;
          2) click on the plugin;
          3) the cursor should change to a rectangle and keep changing its dimensions, even if you don't move/click the mouse.
    
    
    Review URL: http://codereview.chromium.org/8196003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104686 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/mock_plugin_delegate.h
+++ webkit/plugins/ppapi/mock_plugin_delegate.h
@@ -120,6 +120,9 @@ class MockPluginDelegate : public PluginDelegate {
   virtual ::ppapi::Preferences GetPreferences();
   virtual void LockMouse(PluginInstance* instance);
   virtual void UnlockMouse(PluginInstance* instance);
+  virtual void DidChangeCursor(PluginInstance* instance,
+                               const WebKit::WebCursorInfo& cursor);
+  virtual void DidReceiveMouseEvent(PluginInstance* instance);
 };
 
 }  // namespace ppapi
