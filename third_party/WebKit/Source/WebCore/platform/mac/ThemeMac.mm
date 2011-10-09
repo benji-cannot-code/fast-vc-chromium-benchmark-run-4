193327281bbd81f06b336867c49ca1e77e55bb01
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-09 20:15:47 +0000

    <rdar://problem/9484496> REGRESSION (WebKit2): Checkboxes and radio buttons are drawn 1 pixel too high, not repainted properly https://bugs.webkit.org/show_bug.cgi?id=69727
    Reviewed by Anders Carlsson.
    
    * platform/mac/ThemeMac.mm: Made WebCoreFlippedView an NSControl subclass so that the NSButtonCell
    used to draw checkboxes and radio buttons can make it its controlView, and consequently check and
    obey its flippedness.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ThemeMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ThemeMac.mm
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace std;
 
 // This is a view whose sole purpose is to tell AppKit that it's flipped.
-@interface WebCoreFlippedView : NSView
+@interface WebCoreFlippedView : NSControl
 @end
 
 @implementation WebCoreFlippedView
