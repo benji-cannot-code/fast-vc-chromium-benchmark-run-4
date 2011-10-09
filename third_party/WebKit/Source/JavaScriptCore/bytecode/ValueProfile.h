fd36550f114316728468fa06cbcdf5baf8eff856
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-09 10:19:23 +0000

    Fix value profiling in 32_64 JIT https://bugs.webkit.org/show_bug.cgi?id=69717
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-10-09
    Reviewed by Filip Pizlo.
    
    Current value profiling for 32_64 JIT is broken and cannot record
    correct predicated types, which results in many speculation failures
    in the 32_64 DFG JIT, fallbacks to baseline JIT, and re-optimizations
    again and again.
    With this fix 32_64 DFG JIT can demonstrate real performance gains.
    
    * bytecode/ValueProfile.cpp:
    (JSC::ValueProfile::computeStatistics):
    * bytecode/ValueProfile.h:
    (JSC::ValueProfile::classInfo):
    (JSC::ValueProfile::numberOfSamples):
    (JSC::ValueProfile::isLive):
    (JSC::ValueProfile::numberOfInt32s):
    (JSC::ValueProfile::numberOfDoubles):
    (JSC::ValueProfile::numberOfBooleans):
    (JSC::ValueProfile::dump):
        Empty value check should be performed on decoded JSValue,
        as for 32_64 empty value is not identical to encoded 0.
    * jit/JIT.cpp:
    (JSC::JIT::privateCompile):
    * jit/JITInlineMethods.h:
    (JSC::JIT::emitValueProfilingSite):
    * jit/JITStubCall.h:
    (JSC::JITStubCall::callWithValueProfiling):
        Record the right profiling result for 32_64.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97025 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/ValueProfile.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/ValueProfile.h
@@ -56,8 +56,8 @@ struct ValueProfile {
     
     const ClassInfo* classInfo(unsigned bucket) const
     {
-        if (!!m_buckets[bucket]) {
-            JSValue value = JSValue::decode(m_buckets[bucket]);
+        JSValue value = JSValue::decode(m_buckets[bucket]);
+        if (!!value) {
             if (!value.isCell())
                 return 0;
             return value.asCell()->structure()->classInfo();
@@ -69,7 +69,7 @@ struct ValueProfile {
     {
         unsigned result = 0;
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] || !!m_weakBuckets[i])
+            if (!!JSValue::decode(m_buckets[i]) || !!m_weakBuckets[i])
                 result++;
         }
         return result;
@@ -83,7 +83,7 @@ struct ValueProfile {
     bool isLive() const
     {
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] || !!m_weakBuckets[i])
+            if (!!JSValue::decode(m_buckets[i]) || !!m_weakBuckets[i])
                 return true;
         }
         return false;
@@ -100,7 +100,7 @@ struct ValueProfile {
     {
         unsigned result = 0;
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] && JSValue::decode(m_buckets[i]).isInt32())
+            if (JSValue::decode(m_buckets[i]).isInt32())
                 result++;
         }
         return result;
@@ -110,7 +110,7 @@ struct ValueProfile {
     {
         unsigned result = 0;
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] && JSValue::decode(m_buckets[i]).isDouble())
+            if (JSValue::decode(m_buckets[i]).isDouble())
                 result++;
         }
         return result;
@@ -171,7 +171,7 @@ struct ValueProfile {
     {
         unsigned result = 0;
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] && JSValue::decode(m_buckets[i]).isBoolean())
+            if (JSValue::decode(m_buckets[i]).isBoolean())
                 result++;
         }
         return result;
@@ -239,7 +239,8 @@ struct ValueProfile {
                 predictionToString(m_prediction), m_numberOfSamplesInPrediction);
         bool first = true;
         for (unsigned i = 0; i < numberOfBuckets; ++i) {
-            if (!!m_buckets[i] || !!m_weakBuckets[i]) {
+            JSValue value = JSValue::decode(m_buckets[i]);
+            if (!!value || !!m_weakBuckets[i]) {
                 if (first) {
                     fprintf(out, ": ");
                     first = false;
@@ -247,8 +248,8 @@ struct ValueProfile {
                     fprintf(out, ", ");
             }
             
-            if (!!m_buckets[i])
-                fprintf(out, "%s", JSValue::decode(m_buckets[i]).description());
+            if (!!value)
+                fprintf(out, "%s", value.description());
             
             if (!!m_weakBuckets[i])
                 fprintf(out, "DeadCell");
