6ae1c875b192daa929befcf841cbd97f1e789eca
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 22:20:33 +0000

    Cleanup: Remove a static initializer in pickle.cc.
    BUG=94925
    TEST=none
    
    Review URL: http://codereview.chromium.org/8207011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104802 0039d316-1c4b-4281-b951-d872f2087c98

--- base/pickle.cc
+++ base/pickle.cc
@@ -8,15 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 
 #include <algorithm>  // for max()
-#include <limits>
 
 //------------------------------------------------------------------------------
 
 // static
 const int Pickle::kPayloadUnit = 64;
 
-// We mark a read only pickle with a special capacity_.
-static const size_t kCapacityReadOnly = std::numeric_limits<size_t>::max();
+static const size_t kCapacityReadOnly = static_cast<size_t>(-1);
 
 // Payload is uint32 aligned.
 
@@ -387,7 +385,7 @@ char* Pickle::BeginWrite(size_t length) {
     return NULL;
 
 #ifdef ARCH_CPU_64_BITS
-  DCHECK_LE(length, std::numeric_limits<uint32>::max());
+  DCHECK_LE(length, kuint32max);
 #endif
 
   header_->payload_size = static_cast<uint32>(new_size);
