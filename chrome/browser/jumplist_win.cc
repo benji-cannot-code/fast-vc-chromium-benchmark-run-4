8ba200750442aa6816682576ee8e742c87389613
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 17:04:29 +0000

    base::Bind: Final cleanup in jumplist_win.cc.
    BUG=none
    TEST=none
    
    R=csilv@chromium.org
    
    Review URL: http://codereview.chromium.org/8209002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104733 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/jumplist_win.cc
+++ chrome/browser/jumplist_win.cc
@@ -745,7 +745,7 @@ void JumpList::OnFaviconDataAvailable(
   // the file thread.
   BrowserThread::PostTask(
       BrowserThread::FILE, FROM_HERE,
-      NewRunnableMethod(this, &JumpList::RunUpdate));
+      base::Bind(&JumpList::RunUpdate, this));
 }
 
 void JumpList::RunUpdate() {
