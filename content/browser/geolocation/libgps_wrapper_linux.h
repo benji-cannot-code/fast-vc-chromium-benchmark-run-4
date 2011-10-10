991b891991cb489989b8aa00f6f73807074962c1
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 15:12:28 +0000

    Fix gps.h license issue, by deleting it
    BUG=98132,99326
    TEST=used gpsfake (from lucid package 2.92) and opened maps.google.com/maps/m
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=104481
    
    Review URL: http://codereview.chromium.org/8171006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104716 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/libgps_wrapper_linux.h
+++ content/browser/geolocation/libgps_wrapper_linux.h
@@ -54,7 +54,6 @@ class CONTENT_EXPORT LibGps {
   // libgps API versions (v2.38 => libgps.so.17, v2.94 => libgps.so.19).
   // See LibGps::New() for the public API to this.
   // Takes ownership of |dl_wrapper|.
-  static LibGps* NewV238(LibGpsLibraryWrapper* dl_wrapper);
   static LibGps* NewV294(LibGpsLibraryWrapper* dl_wrapper);
 
   scoped_ptr<LibGpsLibraryWrapper> library_;
@@ -72,8 +71,6 @@ class CONTENT_EXPORT LibGpsLibraryWrapper {
   typedef gps_data_t* (*gps_open_fn)(const char*, const char*);
   typedef int (*gps_close_fn)(gps_data_t*);
   typedef int (*gps_poll_fn)(gps_data_t*);
-  // v2.34 only
-  typedef int (*gps_query_fn)(gps_data_t*, const char*, ...);
   // v2.90+
   typedef int (*gps_stream_fn)(gps_data_t*, unsigned int, void*);
   typedef bool (*gps_waiting_fn)(gps_data_t*);
@@ -82,7 +79,6 @@ class CONTENT_EXPORT LibGpsLibraryWrapper {
                        gps_open_fn gps_open,
                        gps_close_fn gps_close,
                        gps_poll_fn gps_poll,
-                       gps_query_fn gps_query,
                        gps_stream_fn gps_stream,
                        gps_waiting_fn gps_waiting);
   ~LibGpsLibraryWrapper();
@@ -91,7 +87,6 @@ class CONTENT_EXPORT LibGpsLibraryWrapper {
   bool open(const char* host, const char* port);
   void close();
   int poll();
-  int query(const char* fmt);
   int stream(int flags);
   bool waiting();
   const gps_data_t& data() const;
@@ -102,7 +97,6 @@ class CONTENT_EXPORT LibGpsLibraryWrapper {
   gps_open_fn gps_open_;
   gps_close_fn gps_close_;
   gps_poll_fn gps_poll_;
-  gps_query_fn gps_query_;
   gps_stream_fn gps_stream_;
   gps_waiting_fn gps_waiting_;
 
