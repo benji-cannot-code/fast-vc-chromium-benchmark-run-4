f4d9b36b8e11912668e0ca7bd89976c8141b7048
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 11:27:59 +0000

    2011-10-10  Pavel Podivilov  <podivilov@chromium.org>
            Unreviewed, mark CCThreadTest.startPostAndWaitOnCondition as flaky on win and mac.
    
            * tests/CCThreadTest.cpp:
            (WebKit::TEST):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCThreadTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCThreadTest.cpp
@@ -87,7 +87,13 @@ public:
     bool hit;
 };
 
-TEST(CCThreadTest, startPostAndWaitOnCondition)
+#if OS(WINDOWS) || OS(MAC_OS_X)
+#define MAYBE_startPostAndWaitOnCondition DISABLED_startPostAndWaitOnCondition
+#else
+#define MAYBE_startPostAndWaitOnCondition startPostAndWaitOnCondition
+#endif
+
+TEST(CCThreadTest, MAYBE_startPostAndWaitOnCondition)
 {
     OwnPtr<WebThread> webThread = adoptPtr(webKitPlatformSupport()->createThread("test"));
 
