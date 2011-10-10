9ab14e2cc587084889bc383493307183640f0b30
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 18:56:48 +0000

    Build fix
    * wtf/MainThread.h: Pull in Platform.h since this file uses PLATFORM() macros.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/MainThread.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/MainThread.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MainThread_h
 #define MainThread_h
 
+#include "Platform.h"
+
 #include <stdint.h>
 
 namespace WTF {
