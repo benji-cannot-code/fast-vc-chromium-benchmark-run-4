588ff1054afd96ccc4da1a003d95c02fa9625716
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 21:17:42 +0000

    Allow a window to be passed to WebGraphicsContext3DInProcessImpl
    BUG=None
    TEST=Webkit compositor in browser process
    
    
    Review URL: http://codereview.chromium.org/8202021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104787 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/webgraphicscontext3d_in_process_impl.h
+++ webkit/gpu/webgraphicscontext3d_in_process_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/angle/include/GLSLANG/ShaderLang.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebGraphicsContext3D.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
+#include "ui/gfx/native_widget_types.h"
 
 #if !defined(OS_MACOSX)
 #define FLIP_FRAMEBUFFER_VERTICALLY
@@ -52,7 +53,9 @@ namespace gpu {
 
 class WebGraphicsContext3DInProcessImpl : public WebGraphicsContext3D {
  public:
-  WebGraphicsContext3DInProcessImpl();
+  // Creates a WebGraphicsContext3DInProcessImpl for a given window. If window
+  // is gfx::kNullPluginWindow, then it creates an offscreen context.
+  WebGraphicsContext3DInProcessImpl(gfx::PluginWindowHandle window);
   virtual ~WebGraphicsContext3DInProcessImpl();
 
   //----------------------------------------------------------------------
@@ -424,6 +427,9 @@ class WebGraphicsContext3DInProcessImpl : public WebGraphicsContext3D {
 
   typedef base::hash_map<WebGLId, ShaderSourceEntry*> ShaderSourceMap;
 
+  bool AllocateOffscreenFrameBuffer(int width, int height);
+  void ClearRenderTarget();
+
 #ifdef FLIP_FRAMEBUFFER_VERTICALLY
   void FlipVertically(unsigned char* framebuffer,
                       unsigned int width,
@@ -485,6 +491,7 @@ class WebGraphicsContext3DInProcessImpl : public WebGraphicsContext3D {
 
   ShHandle fragment_compiler_;
   ShHandle vertex_compiler_;
+  gfx::PluginWindowHandle window_;
 };
 
 }  // namespace gpu
