72990f107e7da431635b91c6d8e1070f3aee82b5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 09:03:28 +0000

    Unreviewed, rolling out r97045. http://trac.webkit.org/changeset/97045 https://bugs.webkit.org/show_bug.cgi?id=69746
    makes apple bots very crashy :( (Requested by kling on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-10-10
    
    Source/JavaScriptCore:
    
    * config.h:
    
    Source/WebCore:
    
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::ApplyPropertyColor::applyInheritValue):
    * page/animation/AnimationBase.cpp:
    (WebCore::PropertyWrapperMaybeInvalidColor::PropertyWrapperMaybeInvalidColor):
    (WebCore::PropertyWrapperSVGPaint::PropertyWrapperSVGPaint):
    (WebCore::AnimationBase::ensurePropertyMap):
    * platform/graphics/Color.h:
    (WebCore::Color::Color):
    * rendering/style/BorderValue.h:
    (WebCore::BorderValue::BorderValue):
    (WebCore::BorderValue::isTransparent):
    (WebCore::BorderValue::operator==):
    (WebCore::BorderValue::color):
    * rendering/style/OutlineValue.h:
    (WebCore::OutlineValue::operator==):
    * rendering/style/RenderStyle.h:
    (WebCore::InheritedFlags::setBorderLeftColor):
    (WebCore::InheritedFlags::setBorderRightColor):
    (WebCore::InheritedFlags::setBorderTopColor):
    (WebCore::InheritedFlags::setBorderBottomColor):
    (WebCore::InheritedFlags::setOutlineColor):
    (WebCore::InheritedFlags::setColumnRuleColor):
    (WebCore::InheritedFlags::fillPaintColor):
    (WebCore::InheritedFlags::strokePaintColor):
    (WebCore::InheritedFlags::borderLeftColor):
    (WebCore::InheritedFlags::borderRightColor):
    (WebCore::InheritedFlags::borderTopColor):
    (WebCore::InheritedFlags::borderBottomColor):
    (WebCore::InheritedFlags::backgroundColor):
    (WebCore::InheritedFlags::color):
    (WebCore::InheritedFlags::columnRuleColor):
    (WebCore::InheritedFlags::outlineColor):
    (WebCore::InheritedFlags::textEmphasisColor):
    (WebCore::InheritedFlags::textFillColor):
    (WebCore::InheritedFlags::textStrokeColor):
    (WebCore::InheritedFlags::stopColor):
    (WebCore::InheritedFlags::floodColor):
    (WebCore::InheritedFlags::lightingColor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/config.h
+++ third_party/WebKit/Source/JavaScriptCore/config.h
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // this breaks compilation of <QFontDatabase>, at least, so turn it off for now
 // Also generates errors on wx on Windows and QNX, because these functions
-// are used from wx and QNX headers.
+// are used from wx and QNX headers. 
 #if !PLATFORM(QT) && !PLATFORM(WX) && !OS(QNX)
 #include <wtf/DisallowCType.h>
 #endif
