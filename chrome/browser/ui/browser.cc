03f2a262e7db087a8c9f1161bcd62b72a64e0cc9
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 04:26:22 +0000

    Exit tabbed fullscreen mode on navigation.
    BUG=97553
    TEST=browsertest provided.
    
    
    Review URL: http://codereview.chromium.org/7966031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -140,6 +140,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/site_instance.h"
 #include "content/browser/tab_contents/interstitial_page.h"
 #include "content/browser/tab_contents/navigation_controller.h"
+#include "content/browser/tab_contents/navigation_details.h"
 #include "content/browser/tab_contents/navigation_entry.h"
 #include "content/browser/tab_contents/tab_contents_view.h"
 #include "content/browser/user_metrics.h"
@@ -1665,8 +1666,11 @@ void Browser::ToggleFullscreenMode() {
 }
 
 void Browser::NotifyTabOfFullscreenExitIfNecessary() {
-  if (fullscreened_tab_)
+  if (fullscreened_tab_) {
     fullscreened_tab_->ExitFullscreenMode();
+    registrar_.Remove(this, content::NOTIFICATION_NAV_ENTRY_COMMITTED,
+        Source<NavigationController>(&fullscreened_tab_->controller()));
+  }
   fullscreened_tab_ = NULL;
   tab_caused_fullscreen_ = false;
 }
@@ -3784,6 +3788,12 @@ void Browser::ToggleFullscreenModeForTab(TabContents* tab,
 #endif
     if (!in_correct_mode_for_tab_fullscreen)
       tab_caused_fullscreen_ = true;
+    registrar_.Add(this, content::NOTIFICATION_NAV_ENTRY_COMMITTED,
+        Source<NavigationController>(&fullscreened_tab_->controller()));
+  } else {
+    registrar_.Remove(this, content::NOTIFICATION_NAV_ENTRY_COMMITTED,
+        Source<NavigationController>(&fullscreened_tab_->controller()));
+    fullscreened_tab_ = NULL;
   }
 
   if (tab_caused_fullscreen_) {
@@ -4120,6 +4130,13 @@ void Browser::Observe(int type,
       if (profile_->IsSameProfile(Source<Profile>(source).ptr()))
         UpdateBookmarkBarState(BOOKMARK_BAR_STATE_CHANGE_PREF_CHANGE);
       break;
+
+    case content::NOTIFICATION_NAV_ENTRY_COMMITTED: {
+      if (Details<content::LoadCommittedDetails>(details)->
+          is_navigation_to_different_page())
+        ExitTabbedFullscreenModeIfNecessary();
+      break;
+    }
 
     default:
       NOTREACHED() << "Got a notification we didn't register for.";
