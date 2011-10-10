bd184b9658d1c2c594b470aa8378dd346533dee3
alicet@chromium.org <alicet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 22:23:45 +0000

    add default plugin stub for aura to get ui_tests to compile.
    BUG=99362
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8226001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104805 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/default_plugin/default_plugin.gyp
+++ chrome/default_plugin/default_plugin.gyp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         '<(DEPTH)/third_party/wtl/include',
       ],
       'sources': [
+        'plugin_impl_aura.cc',
+        'plugin_impl_aura.h',
         'plugin_impl_gtk.cc',
         'plugin_impl_gtk.h',
         'plugin_impl_mac.h',
--- chrome/default_plugin/plugin_impl.h
+++ chrome/default_plugin/plugin_impl.h
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(OS_WIN)
+#if defined(USE_AURA)
+#include "chrome/default_plugin/plugin_impl_aura.h"
+#elif defined(OS_WIN)
 #include "chrome/default_plugin/plugin_impl_win.h"
 #elif defined(OS_MACOSX)
 #include "chrome/default_plugin/plugin_impl_mac.h"
