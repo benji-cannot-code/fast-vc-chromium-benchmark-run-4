3362ab88344b8dfff6a1ffa919a39dda91f0a8c9
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 20:05:16 +0000

    REGRESSION(96070) 25% intl1 PLT regression from scrollbar invalidation https://bugs.webkit.org/show_bug.cgi?id=69238
    Reviewed by Darin Adler.
    
    Fix PLT regression from additional scrollbars invalidations.
    
    Don't invalidate the scrollbar rect from setHasHorizontalScrollbar()
    and setHasVerticalScrollbar(), because this is called during FrameView
    teardown, at which point the invalidation is redundant.
    
    Instead, invalidate from ScrollView::updateScrollbars(), which is
    called only when scrollbars change because of size or layout
    changes.
    
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::setHasHorizontalScrollbar):
    (WebCore::ScrollView::setHasVerticalScrollbar):
    (WebCore::ScrollView::updateScrollbars):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollView.cpp
@@ -96,7 +96,6 @@ void ScrollView::setHasHorizontalScrollbar(bool hasBar)
         didAddHorizontalScrollbar(m_horizontalScrollbar.get());
         m_horizontalScrollbar->styleChanged();
     } else if (!hasBar && m_horizontalScrollbar) {
-        m_horizontalScrollbar->invalidate();
         willRemoveHorizontalScrollbar(m_horizontalScrollbar.get());
         removeChild(m_horizontalScrollbar.get());
         m_horizontalScrollbar = 0;
@@ -115,7 +114,6 @@ void ScrollView::setHasVerticalScrollbar(bool hasBar)
         didAddVerticalScrollbar(m_verticalScrollbar.get());
         m_verticalScrollbar->styleChanged();
     } else if (!hasBar && m_verticalScrollbar) {
-        m_verticalScrollbar->invalidate();
         willRemoveVerticalScrollbar(m_verticalScrollbar.get());
         removeChild(m_verticalScrollbar.get());
         m_verticalScrollbar = 0;
@@ -498,6 +496,8 @@ void ScrollView::updateScrollbars(const IntSize& desiredOffset)
         if (hasHorizontalScrollbar != newHasHorizontalScrollbar && (hasHorizontalScrollbar || !avoidScrollbarCreation())) {
             if (m_scrollOrigin.y() && !newHasHorizontalScrollbar)
                 m_scrollOrigin.setY(m_scrollOrigin.y() - m_horizontalScrollbar->height());
+            if (m_horizontalScrollbar)
+                m_horizontalScrollbar->invalidate();
             setHasHorizontalScrollbar(newHasHorizontalScrollbar);
             sendContentResizedNotification = true;
         }
@@ -505,6 +505,8 @@ void ScrollView::updateScrollbars(const IntSize& desiredOffset)
         if (hasVerticalScrollbar != newHasVerticalScrollbar && (hasVerticalScrollbar || !avoidScrollbarCreation())) {
             if (m_scrollOrigin.x() && !newHasVerticalScrollbar)
                 m_scrollOrigin.setX(m_scrollOrigin.x() - m_verticalScrollbar->width());
+            if (m_verticalScrollbar)
+                m_verticalScrollbar->invalidate();
             setHasVerticalScrollbar(newHasVerticalScrollbar);
             sendContentResizedNotification = true;
         }
