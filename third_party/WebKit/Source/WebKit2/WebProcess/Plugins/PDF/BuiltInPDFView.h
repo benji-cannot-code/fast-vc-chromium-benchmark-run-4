342e6c534c7880480d140d0db8233d55acb0f157
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 04:28:58 +0000

    Fix Lion, Leopard and Snow Leopard builds after changeset 97034 <http://trac.webkit.org/changeset/97034> (https://bugs.webkit.org/show_bug.cgi?id=68035) https://bugs.webkit.org/show_bug.cgi?id=69735
    Patch by Luke Macpherson <macpherson@chromium.org> on 2011-10-09
    Reviewed by Daniel Bates.
    
    Source/WebCore: 
    
    * testing/Internals.cpp:
    Mark unused parameters to prevent compile errors when GESTURE_EVENTS and SMOOTH_SCROLLING are disabled.
    (WebCore::Internals::setEnableScrollAnimator):
    (WebCore::Internals::setZoomAnimatorTransform):
    (WebCore::Internals::setZoomParameters):
    
    Source/WebKit2: 
    
    * WebProcess/Plugins/PDF/BuiltInPDFView.h:
    Add symbols that are now required by ScrollableArea.
    (WebKit::BuiltInPDFView::zoomAnimatorTransformChanged):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PDF/BuiltInPDFView.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PDF/BuiltInPDFView.h
@@ -86,6 +86,8 @@ private:
     virtual void manualStreamDidFinishLoading();
     virtual void manualStreamDidFail(bool wasCancelled);
     
+    virtual void zoomAnimatorTransformChanged(double, double, double, ZoomAnimationState) { }
+
     virtual bool handleMouseEvent(const WebMouseEvent&);
     virtual bool handleWheelEvent(const WebWheelEvent&);
     virtual bool handleMouseEnterEvent(const WebMouseEvent&);
