62409e7d801f7f3d49b02d2613ee9b3873aace15
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-10 21:02:38 +0000

    Switch last calls from DFGSpeculativeJIT to use callOperation. https://bugs.webkit.org/show_bug.cgi?id=69780
    Reviewed by Oliver Hunt.
    
    Also, rename type in operations for booleans from Z to B, since Z is the mathematical symbol for integers.
    
    * dfg/DFGJITCodeGenerator.cpp:
    (JSC::DFG::JITCodeGenerator::nonSpeculativeCompare):
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::callOperation):
    * dfg/DFGJITCodeGenerator32_64.cpp:
    (JSC::DFG::JITCodeGenerator::nonSpeculativePeepholeBranch):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeCompare):
    * dfg/DFGJITCodeGenerator64.cpp:
    (JSC::DFG::JITCodeGenerator::nonSpeculativePeepholeBranch):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeCompare):
    * dfg/DFGOperations.h:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compilePeepHoleBranch):
    * dfg/DFGSpeculativeJIT.h:
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compare):
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compare):
    (JSC::DFG::SpeculativeJIT::compile):
    * wtf/Platform.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97083 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
@@ -475,7 +475,7 @@ void SpeculativeJIT::compileObjectEquality(Node& node, void* vptr)
 }
 
 // Returns true if the compare is fused with a subsequent branch.
-bool SpeculativeJIT::compare(Node& node, MacroAssembler::RelationalCondition condition, MacroAssembler::DoubleCondition doubleCondition, Z_DFGOperation_EJJ operation)
+bool SpeculativeJIT::compare(Node& node, MacroAssembler::RelationalCondition condition, MacroAssembler::DoubleCondition doubleCondition, B_DFGOperation_EJJ operation)
 {
     if (compilePeepHoleBranch(node, condition, doubleCondition, operation))
         return true;
@@ -1485,9 +1485,7 @@ void SpeculativeJIT::compile(Node& node)
 
         // Code to handle put beyond array bounds.
         silentSpillAllRegisters(scratchReg);
-        setupStubArguments(baseReg, propertyReg, valueReg);
-        m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
-        JITCompiler::Call functionCall = appendCallWithExceptionCheck(operationPutByValBeyondArrayBounds);
+        JITCompiler::Call functionCall = callOperation(operationPutByValBeyondArrayBounds, baseReg, propertyReg, valueReg);
         silentFillAllRegisters(scratchReg);
         JITCompiler::Jump wasBeyondArrayBounds = m_jit.jump();
 
@@ -2351,11 +2349,7 @@ void SpeculativeJIT::compile(Node& node)
         JITCompiler::Jump structuresMatch = m_jit.branchPtr(JITCompiler::Equal, resultGPR, JITCompiler::Address(globalObjectGPR, JSCell::structureOffset()));
 
         silentSpillAllRegisters(resultGPR);
-        m_jit.move(resolveInfoGPR, GPRInfo::argumentGPR1);
-        m_jit.move(JITCompiler::TrustedImmPtr(&m_jit.codeBlock()->identifier(data.identifierNumber)), GPRInfo::argumentGPR2);
-        m_jit.move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
-        JITCompiler::Call functionCall = appendCallWithExceptionCheck(operationResolveGlobal);
-        m_jit.move(GPRInfo::returnValueGPR, resultGPR);
+        JITCompiler::Call functionCall = callOperation(operationResolveGlobal, resultGPR, resolveInfoGPR, &m_jit.codeBlock()->identifier(data.identifierNumber));
         silentFillAllRegisters(resultGPR);
 
         JITCompiler::Jump wasSlow = m_jit.jump();
