f82f057aaebcfd29c74cbde449c00b4829c70835
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 04:17:06 +0000

    Prevent reentrant destructors in the PluginModule.
    This makes us not hold a ref to the PluginModule during sending async messages.
    Previously we would cancel callbacks in the PluginModule destructor and this
    would change the ref count from 0 to 1, and then back to 0, calling reentrantly
    into the destructor again.
    
    Not holding a ref during async messages prevent this problem. I also added
    some checking to detect sync messages during shutdown, and also reentrant
    destructors should this happen again.
    
    BUG = http://crosbug.com/21258, http://crbug.com/99398
    Review URL: http://codereview.chromium.org/8222021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104847 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_proxy_private.h
+++ ppapi/c/private/ppb_proxy_private.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_module.h"
 #include "ppapi/c/pp_resource.h"
 
-#define PPB_PROXY_PRIVATE_INTERFACE "PPB_Proxy_Private;4"
+#define PPB_PROXY_PRIVATE_INTERFACE "PPB_Proxy_Private;5"
 
 // Exposes functions needed by the out-of-process proxy to call into the
 // renderer PPAPI implementation.
@@ -45,6 +45,9 @@ struct PPB_Proxy_Private {
   // there must be a corresponding release call.
   void (*AddRefModule)(PP_Module module);
   void (*ReleaseModule)(PP_Module module);
+
+  // Allows asserts to be written for some bad conditions while cleaning up.
+  PP_Bool (*IsInModuleDestructor)(PP_Module module);
 };
 
 #endif  // PPAPI_C_PRIVATE_PROXY_PRIVATE_H_
