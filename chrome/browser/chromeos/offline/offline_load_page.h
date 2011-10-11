98343708a2907a9deb43d94bc1fbe9efc8c69340
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 20:16:43 +0000

    OfflineLoadPage fixes
    * Have OfflineLoadPage hold a scoped_ref_ptr to OfflineResourceHandler to ensure
    that the latter is always destructed after the former. This is necessary because 
    OfflineLoadPage is destructed on the UI thread, and OfflineResourceHandler is 
    destructed on the IO thread at around the same time, but OfflineLoadPage can 
    call into OfflineResourceHandler through OnBlockingPageComplete. 
    * Get rid of OfflineLoadPage::Delegate since we now have a handle to 
    OfflineResourceHandler. Fix unit tests to use OnBlockingPageComplete override.
    * Remove static version of OfflineLoadPage::Show; it's confusing and an overloaded
    method name. Move this logic to OfflineResourceHandler::ShowOfflinePage.
    * Add additional threading DCHECKs.
    * Remove unused EnableTest.
    * Other minor changes to comments, typos, etc.
    
    BUG=chromium-os:21128
    TEST=NONE.
    Review URL: http://codereview.chromium.org/8142017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/offline/offline_load_page.h
+++ chrome/browser/chromeos/offline/offline_load_page.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_change_notifier.h"
 
 class Extension;
+class OfflineResourceHandler;
 class TabContents;
 
 namespace base {
@@ -29,34 +30,15 @@ namespace chromeos {
 class OfflineLoadPage : public ChromeInterstitialPage,
                         public net::NetworkChangeNotifier::OnlineStateObserver {
  public:
-  // A delegate class that is called when the interstitinal page
-  // is closed.
-  class Delegate {
-   public:
-    Delegate() {}
-    virtual ~Delegate() {}
-    // Called when a user selected to proceed or not to proceed
-    // with loading.
-    virtual void OnBlockingPageComplete(bool proceed) = 0;
-
-   private:
-    DISALLOW_COPY_AND_ASSIGN(Delegate);
-  };
-  static void Show(int process_host_id, int render_view_id,
-                   const GURL& url, Delegate* delegate);
-  // Import show here so that overloading works.
-  using ChromeInterstitialPage::Show;
-
- protected:
   // Create a offline load page for the |tab_contents|.
   OfflineLoadPage(TabContents* tab_contents, const GURL& url,
-                  Delegate* delegate);
+                  OfflineResourceHandler* handler);
+
+ protected:
   virtual ~OfflineLoadPage();
 
-  // Only for testing.
-  void EnableTest() {
-    in_test_ = true;
-  }
+  // Overridden by tests.
+  virtual void NotifyBlockingPageComplete(bool proceed);
 
  private:
   // ChromeInterstitialPage implementation.
@@ -80,8 +62,7 @@ class OfflineLoadPage : public ChromeInterstitialPage,
   // has not been activated.
   bool ShowActivationMessage();
 
-  Delegate* delegate_;
-  NotificationRegistrar registrar_;
+  scoped_refptr<OfflineResourceHandler> handler_;
 
   // True if the proceed is chosen.
   bool proceeded_;
--- chrome/browser/chromeos/offline/offline_load_page_unittest.cc
+++ chrome/browser/chromeos/offline/offline_load_page_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/offline/offline_load_page.h"
+#include "chrome/browser/renderer_host/offline_resource_handler.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/browser/browser_thread.h"
 #include "content/browser/tab_contents/navigation_entry.h"
@@ -14,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static const char* kURL1 = "http://www.google.com/";
 static const char* kURL2 = "http://www.gmail.com/";
 
+namespace chromeos {
+
+class OfflineLoadPageTest;
+
 namespace {
 
 // An OfflineLoadPage class that does not create windows.
@@ -21,23 +26,28 @@ class TestOfflineLoadPage :  public chromeos::OfflineLoadPage {
  public:
   TestOfflineLoadPage(TabContents* tab_contents,
                       const GURL& url,
-                      Delegate* delegate)
-    : chromeos::OfflineLoadPage(tab_contents, url, delegate) {
-    EnableTest();
+                      OfflineLoadPageTest* test_page)
+    : chromeos::OfflineLoadPage(tab_contents, url, NULL),
+      test_page_(test_page) {
   }
 
   // Overriden from InterstitialPage.  Don't create a view.
-  virtual TabContentsView* CreateTabContentsView() {
+  virtual TabContentsView* CreateTabContentsView() OVERRIDE {
     return NULL;
   }
+
+  // chromeos::OfflineLoadPage override.
+  virtual void NotifyBlockingPageComplete(bool proceed) OVERRIDE;
+
+ private:
+  OfflineLoadPageTest* test_page_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestOfflineLoadPage);
 };
 
 }  // namespace
 
-namespace chromeos {
-
-class OfflineLoadPageTest : public ChromeRenderViewHostTestHarness,
-                            public OfflineLoadPage::Delegate {
+class OfflineLoadPageTest : public ChromeRenderViewHostTestHarness {
  public:
   // The decision the user made.
   enum UserResponse {
@@ -56,8 +66,7 @@ class OfflineLoadPageTest : public ChromeRenderViewHostTestHarness,
     user_response_ = PENDING;
   }
 
-  // OfflineLoadPage::Delegate implementation.
-  virtual void OnBlockingPageComplete(bool proceed) {
+  void OnBlockingPageComplete(bool proceed) {
     if (proceed)
       user_response_ = OK;
     else
@@ -89,10 +98,16 @@ class OfflineLoadPageTest : public ChromeRenderViewHostTestHarness,
 
   // Initializes / shuts down a stub CrosLibrary.
   chromeos::ScopedStubCrosEnabler stub_cros_enabler_;
+
+  DISALLOW_COPY_AND_ASSIGN(OfflineLoadPageTest);
 };
 
+void TestOfflineLoadPage::NotifyBlockingPageComplete(bool proceed) {
+  test_page_->OnBlockingPageComplete(proceed);
+}
 
-TEST_F(OfflineLoadPageTest, OfflinePaeProceed) {
+
+TEST_F(OfflineLoadPageTest, OfflinePageProceed) {
   // Start a load.
   Navigate(kURL1, 1);
   // Load next page.
