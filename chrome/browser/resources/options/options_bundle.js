4540dc31d6cbfef32812fe49c0aff5a05a87517d
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 18:12:24 +0000

    Add display of available bluetooth devices, and mechanism for retrieving the list.
    BUG=Partial fix for chromium-os:20991.
    TEST=Launch chromeOS with the command-line argument --enable-bluetooth, and switch to the System setttings page.  Press the 'Find devices' button.  A list of bluetooth devices appears.  Currently, the list of devices is faked.
    
    
    Review URL: http://codereview.chromium.org/8137003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104915 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_bundle.js
+++ chrome/browser/resources/options/options_bundle.js
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   <include src="chromeos/internet_options.js"></include>
   <include src="chromeos/internet_detail.js"></include>
   <include src="chromeos/system_options.js"></include>
+  <include src="chromeos/bluetooth_list_element.js"></include>
   <include src="chromeos/accounts_options.js"></include>
   <include src="chromeos/proxy_options.js"></include>
   <include src="chromeos/proxy_rules_list.js"></include>
