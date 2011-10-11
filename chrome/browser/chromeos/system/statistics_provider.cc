b956fe0546007e8360c77c877c736c22e47eaa85
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 19:56:41 +0000

    Fix EnterpriseMetricsTest.*
    Fix to prevent EnterpriseMetricsTest.* tests from failing on VM's. This makes sure that we don't wait endlessly for the statistics to gather since the call can sometimes by blocked on the FILE thread.
    
    R=satorux@chromium.org
    BUG=chromium-os:21529
    TEST=Ran the EnterpriseMetricsTest.* tests on a VM to confirm that they pass.
    
    Review URL: http://codereview.chromium.org/8233020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/statistics_provider.cc
+++ chrome/browser/chromeos/system/statistics_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task.h"
+#include "base/time.h"
 #include "chrome/browser/chromeos/system/name_value_pairs_parser.h"
 #include "chrome/browser/chromeos/system/runtime_environment.h"
 #include "content/browser/browser_thread.h"
@@ -43,6 +44,9 @@ const char kMachineOSInfoDelim[] = "\n";
 const char kVpdFile[] = "/var/log/vpd_2.0.txt";
 const char kVpdEq[] = "=";
 const char kVpdDelim[] = "\n";
+
+// Timeout that we should wait for statistics to get loaded
+const int kTimeoutSecs = 3;
 
 // Gets name-value paris from the file using the parser.
 void GetNameValuePairsFromFile(NameValuePairsParser* parser,
@@ -89,16 +93,17 @@ bool StatisticsProviderImpl::GetMachineStatistic(
   // Block if the statistics are not loaded yet. Per LOG(WARNING) below,
   // the statistics are loaded before requested as of now. For regular
   // sessions (i.e. not OOBE), statistics are first requested when the
-  // user is logging in.
+  // user is logging in so we have plenty of time to load the data
+  // beforehand.
   //
-  // If you see the warning appeared, it probably means that there is
-  // new client code that uses the statistics in the very early stage of
-  // the browser startup. The statistic name should be helpful to identify
-  // the caller.
+  // If you see the warning appeared for regular sessions, it probably
+  // means that there is new client code that uses the statistics in the
+  // very early stage of the browser startup. The statistic name should be
+  // helpful to identify the caller.
   if (!on_statistics_loaded_.IsSignaled()) {
     LOG(WARNING) << "Waiting to load statistics. Requested statistic: "
                  << name;
-    on_statistics_loaded_.Wait();
+    on_statistics_loaded_.TimedWait(base::TimeDelta::FromSeconds(kTimeoutSecs));
   }
 
   NameValuePairsParser::NameValueMap::iterator iter = machine_info_.find(name);
