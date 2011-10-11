71321015006a6c191288fc2ef33d9ef08ebb1a44
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 18:53:36 +0000

    Fix Clang-built ProcessUtilTest.GetTerminationStatusCrash.
    Writing to a NULL pointer is undefined behaviour, and unless that pointer is volatile, Clang will optimize it away, replacing it with an UD2 instruction which causes the process to die by SIGILL.
    
    This patch makes the pointer volatile, so the write occurrs, and the process dies with SIGSEGV as intended.
    
    BUG=68707
    TEST=build with Clang, out/Release/base_unittests --gtest_filter=ProcessUtilTest.GetTerminationStatusCrash
    
    
    Review URL: http://codereview.chromium.org/8233010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104930 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_unittest.cc
+++ base/process_util_unittest.cc
@@ -193,7 +193,7 @@ MULTIPROCESS_TEST_MAIN(CrashingChildProcess) {
   ::signal(SIGSEGV, SIG_DFL);
 #endif
   // Make this process have a segmentation fault.
-  int* oops = NULL;
+  volatile int* oops = NULL;
   *oops = 0xDEAD;
   return 1;
 }
