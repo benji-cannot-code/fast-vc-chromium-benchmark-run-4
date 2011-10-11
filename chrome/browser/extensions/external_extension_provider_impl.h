0bf7872053481b093ebea295ce965d5557bc4952
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 16:31:05 +0000

    Add supported_locales attribute to external_extensions.json
    This CL supports backward compatibility. If supported_locales is
    missing, it is supposed that extension supports all locales. 
    supported_locales is a list of locale strings. Locale may specify
    parent locale like "en", in this case the extension will be
    installed for all "en*" locales like "en-US", "en-GB", etc. If
    browser locale is changed and installed default extension is not
    suitable for new locale it will be uninstalled.
    
    BUG=chromium-os:21211
    TEST=ExtensionServiceTest.ExternalPrefProvider
    
    
    Review URL: http://codereview.chromium.org/8188008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_extension_provider_impl.h
+++ chrome/browser/extensions/external_extension_provider_impl.h
@@ -68,6 +68,7 @@ class ExternalExtensionProviderImpl
   static const char kExternalCrx[];
   static const char kExternalVersion[];
   static const char kExternalUpdateUrl[];
+  static const char kSupportedLocales[];
 
  protected:
   VisitorInterface* service() const { return service_; }
