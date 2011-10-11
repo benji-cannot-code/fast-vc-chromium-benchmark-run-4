2e882628d9c2f772afd2d7997ba663f81a14d80f
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 20:03:00 +0000

    get rid of clearing/sync issues with osmesa
    We are seeing clearing artifacts using OSMesa since the shared memory buffer is
    being written to in the Renderer while also being put on screen by the GPU.
    
    This makes all Renderer drawing happen in a local buffer which is then blitted
    to the shared memory buffer.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8139006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104951 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/image_transport_surface_linux.cc
+++ content/common/gpu/image_transport_surface_linux.cc
@@ -133,7 +133,6 @@ class OSMesaImageTransportSurface : public ImageTransportSurface,
   virtual bool IsOffscreen() OVERRIDE;
   virtual bool SwapBuffers() OVERRIDE;
   virtual gfx::Size GetSize() OVERRIDE;
-  virtual void* GetHandle() OVERRIDE;
 
  protected:
   // ImageTransportSurface implementation:
@@ -476,11 +475,10 @@ void OSMesaImageTransportSurface::OnResize(gfx::Size size) {
   if (shared_mem_.get())
     ReleaseSurface();
 
-  // Now that the shared memory buffer is gone, we need to change OSMesa to
-  // point at something valid, so we'll let it point to the buffer in the super
-  // class.  This would be really bad since that buffer has a different size,
-  // but we don't allow any drawing to take place until we reset the surface
-  // back to a new buffer of shared memory.
+  GLSurfaceOSMesa::Resize(size);
+
+  // Now that we resized/reallocated the memory buffer, we need to change
+  // what OSMesa is pointing at to the new buffer.
   helper_->MakeCurrent();
 
   size_ = size;
@@ -500,10 +498,6 @@ void OSMesaImageTransportSurface::OnNewSurfaceACK(
   shared_mem_.reset(TransportDIB::Map(surface_handle));
   DCHECK_NE(shared_mem_.get(), static_cast<void*>(NULL));
 
-  // When we get the shared memory buffer back we can use that for OSMesa to
-  // write in, so we give it to OSMesa.
-  helper_->MakeCurrent();
-
   helper_->SetScheduled(true);
 }
 
@@ -511,6 +505,7 @@ bool OSMesaImageTransportSurface::SwapBuffers() {
   DCHECK_NE(shared_mem_.get(), static_cast<void*>(NULL));
 
   // Copy the OSMesa buffer to the shared memory
+  glFinish();
   memcpy(shared_mem_->memory(), GetHandle(), size_.GetArea() * 4);
 
   GpuHostMsg_AcceleratedSurfaceBuffersSwapped_Params params;
@@ -527,13 +522,6 @@ void OSMesaImageTransportSurface::OnBuffersSwappedACK() {
 
 gfx::Size OSMesaImageTransportSurface::GetSize() {
   return size_;
-}
-
-void* OSMesaImageTransportSurface::GetHandle() {
-  if (shared_mem_.get())
-    return shared_mem_->memory();
-  else
-    return GLSurfaceOSMesa::GetHandle();
 }
 
 }  // namespace
