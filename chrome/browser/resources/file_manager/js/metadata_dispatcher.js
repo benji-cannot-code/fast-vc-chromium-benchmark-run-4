fcb21c0f5e70e7502ff8e063b75dd587323911af
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-11 15:31:46 +0000

    [filebrowser] Supported save for non-JPEG images
    BUG=chromium-os:21457
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8230003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/metadata_dispatcher.js
+++ chrome/browser/resources/file_manager/js/metadata_dispatcher.js
@@ -190,7 +190,7 @@ MetadataDispatcher.prototype.processOneFile = function(fileURL, callback) {
             return;
           }
         }
-        callback({mimeType: mimeType});  // Unrecognized mime type.
+        callback({});  // Unrecognized mime type.
       },
 
       // Reuse the last step from the standard sequence.
