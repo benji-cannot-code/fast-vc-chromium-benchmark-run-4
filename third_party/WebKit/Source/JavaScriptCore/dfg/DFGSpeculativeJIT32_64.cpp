84eec129303129368eaffd584a46d9610bddb38f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-11 07:40:22 +0000

    Add missing copyright notice in DFG JIT files https://bugs.webkit.org/show_bug.cgi?id=69809
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-10-11
    Reviewed by Gavin Barraclough.
    
    * dfg/DFGJITCodeGenerator32_64.cpp:
    * dfg/DFGJITCompiler32_64.cpp:
    * dfg/DFGJITCompilerInlineMethods.h:
    * dfg/DFGSpeculativeJIT32_64.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
