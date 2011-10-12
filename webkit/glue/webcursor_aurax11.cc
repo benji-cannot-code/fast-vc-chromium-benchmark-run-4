a25101d14cda8275ac7407126fd5c619ad8d82cc
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 03:03:59 +0000

    aura: Fix 'typo' to fix cursor.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8231019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104982 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webcursor_aurax11.cc
+++ webkit/glue/webcursor_aurax11.cc
@@ -30,7 +30,7 @@ class CursorCache {
   }
 
   Cursor GetCursor(int web_cursor_info_type) {
-    Cursor cursor = static_cast<Cursor>(NULL);
+    Cursor cursor = gfx::kNullCursor;
     std::pair<std::map<int, Cursor>::iterator, bool> it = cache_.insert(
         std::make_pair(web_cursor_info_type, cursor));
     if (it.second) {
@@ -38,7 +38,7 @@ class CursorCache {
           GetXCursorType(web_cursor_info_type));
       it.first->second = cursor;
     }
-    return cursor;
+    return it.first->second;
   }
 
  private:
