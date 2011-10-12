9af085d4508e88f7e3e4303e0708086f5dabe542
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-12 17:05:41 +0000

    ScriptController::executeIfJavaScriptURL gets confused by synchronous frame loads https://bugs.webkit.org/show_bug.cgi?id=69777
    Patch by Sergey Glazunov <serg.glazunov@gmail.com> on 2011-10-12
    Reviewed by Adam Barth.
    
    Source/WebCore: 
    
    Test: http/tests/security/xss-DENIED-synchronous-frame-load-in-javascript-url.html
    
    * bindings/ScriptControllerBase.cpp:
    (WebCore::ScriptController::executeIfJavaScriptURL):
    * loader/DocumentWriter.cpp:
    (WebCore::DocumentWriter::replaceDocument):
    (WebCore::DocumentWriter::begin):
    * loader/DocumentWriter.h:
    
    Source/WebKit/chromium: 
    
    * src/WebFrameImpl.cpp:
    (WebKit::WebFrameImpl::loadJavaScriptURL):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -2274,6 +2274,8 @@ void WebFrameImpl::loadJavaScriptURL(const KURL& url)
     if (!m_frame->document() || !m_frame->page())
         return;
 
+    RefPtr<Document> ownerDocument(m_frame->document());
+
     // Protect privileged pages against bookmarklets and other javascript manipulations.
     if (SchemeRegistry::shouldTreatURLSchemeAsNotAllowingJavascriptURLs(m_frame->document()->url().protocol()))
         return;
@@ -2286,7 +2288,7 @@ void WebFrameImpl::loadJavaScriptURL(const KURL& url)
         return;
 
     if (!m_frame->navigationScheduler()->locationChangePending())
-        m_frame->document()->loader()->writer()->replaceDocument(scriptResult);
+        m_frame->document()->loader()->writer()->replaceDocument(scriptResult, ownerDocument.get());
 }
 
 } // namespace WebKit
