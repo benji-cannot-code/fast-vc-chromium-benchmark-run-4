adde5f6b5221e3127251a48980d87ef7e7ca8a6f
alicet@chromium.org <alicet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 16:54:47 +0000

    fix win_aura build.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8232031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105101 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/first_run_bubble.cc
+++ chrome/browser/ui/views/first_run_bubble.cc
@@ -535,6 +535,7 @@ void FirstRunBubble::EnableParent() {
                SWP_NOSIZE | SWP_NOMOVE | SWP_NOREDRAW | SWP_SHOWWINDOW);
 }
 
+#if defined(OS_WIN) && !defined(USE_AURA)
 void FirstRunBubble::OnActivate(UINT action, BOOL minimized, HWND window) {
   // Keep the bubble around for kLingerTime milliseconds, to prevent accidental
   // closure.
@@ -559,6 +560,7 @@ void FirstRunBubble::OnActivate(UINT action, BOOL minimized, HWND window) {
   if (::IsWindowEnabled(GetParent()))
     Bubble::OnActivate(action, minimized, window);
 }
+#endif
 
 void FirstRunBubble::BubbleClosing(Bubble* bubble, bool closed_by_escape) {
   // Make sure our parent window is re-enabled.
