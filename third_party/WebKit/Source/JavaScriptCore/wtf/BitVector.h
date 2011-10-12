cba0c8c432ff0171bcb6e0fdd917e3cd3eb2a919
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-12 01:37:00 +0000

    GTK build fix attempt after r97197.
    * wtf/BitVector.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/BitVector.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/BitVector.h
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Assertions.h>
 #include <wtf/StdLibExtras.h>
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace WTF {
 
 // This is a space-efficient, resizeable bitvector class. In the common case it
