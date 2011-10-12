4edbc3e2982ddfc3697c6daf3d85ccc7174903ed
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 17:59:55 +0000

    [Mac] Override -[HyperlinkButtonCell cellSize] so that |-sizeToFit| does not truncate the contents.
    BUG=100019
    TEST=Visual
    
    
    Review URL: http://codereview.chromium.org/8226020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/hyperlink_button_cell.mm
+++ chrome/browser/ui/cocoa/hyperlink_button_cell.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -111,6 +111,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setTextColor:(NSColor*)color {
   textColor_.reset([color retain]);
+}
+
+// Override so that |-sizeToFit| works better with this type of cell.
+- (NSSize)cellSize {
+  NSSize size = [super cellSize];
+  size.width += 2;
+  return size;
 }
 
 @end
