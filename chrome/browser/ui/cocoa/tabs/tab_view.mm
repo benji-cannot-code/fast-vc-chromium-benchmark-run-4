9aee6148e065b71eae114afc202a4f2e3efed81d
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 07:24:58 +0000

    [Mac] Plug some leaks in accessibility code.
    BUG=99820
    TEST=Valgrind on Mac.
    
    
    Review URL: http://codereview.chromium.org/8229011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_view.mm
+++ chrome/browser/ui/cocoa/tabs/tab_view.mm
@@ -458,7 +458,7 @@ const CGFloat kRapidCloseDist = 2.5;
   [attributes addObject:NSAccessibilityTitleAttribute];
   [attributes addObject:NSAccessibilityEnabledAttribute];
 
-  return attributes;
+  return [attributes autorelease];
 }
 
 - (BOOL)accessibilityIsAttributeSettable:(NSString*)attribute {
