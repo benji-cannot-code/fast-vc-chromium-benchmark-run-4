921da51047fb8fe09ba05d0760eb2a4542eaf689
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 14:37:17 +0000

    Add DispatchToMethod to the list of 'boring' callers TBR=thestig,glider Review URL: http://codereview.chromium.org/8208010
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105078 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/memcheck_analyze.py
+++ tools/valgrind/memcheck_analyze.py
@@ -43,6 +43,7 @@ _BORING_CALLERS = [
   "_ZN11MessageLoop3RunEv",
   "_ZN14RunnableMethod.*",
   "_ZN16RunnableFunction.*",
+  "_ZN16DispatchToMethod.*",
 
   # Also don't show the internals of libc/pthread.
   "start_thread"
--- tools/valgrind/valgrind_test.py
+++ tools/valgrind/valgrind_test.py
@@ -644,6 +644,7 @@ class ThreadSanitizerBase(object):
     ret += ["--cut_stack_below=MessageLoop*Run"]
     ret += ["--cut_stack_below=RunnableMethod*"]
     ret += ["--cut_stack_below=RunnableFunction*"]
+    ret += ["--cut_stack_below=DispatchToMethod*"]
 
     return ret
 
@@ -825,7 +826,8 @@ class DrMemory(BaseTool):
     proc += ["-callstack_truncate_below",
              "main,BaseThreadInitThunk,"+
              "testing*Test*Run*,testing::internal::Handle*Exceptions*,"+
-             "MessageLoop::Run,RunnableMethod*,RunnableFunction*"]
+             "MessageLoop::Run,"+
+             "RunnableMethod*,RunnableFunction*,DispatchToMethod*"]
     proc += ["-callstack_modname_hide",
              "*.exe,chrome.dll"]
 
