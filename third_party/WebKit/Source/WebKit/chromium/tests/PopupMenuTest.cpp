ef7feb43f4ebf2b78d85e47cab1410195f3e4a04
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-12 01:12:57 +0000

    Chromium build fix attempt after r97202.
    * tests/PopupMenuTest.cpp:
    (WebKit::SelectPopupMenuTest::boundingBoxRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97207 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
@@ -273,6 +273,11 @@ protected:
         frame->loadRequest(urlRequest);
     }
 
+    virtual LayoutRect boundingBoxRect() const
+    {
+        return LayoutRect();
+    }
+
 protected:
     TestWebViewClient m_webviewClient;
     WebViewImpl* m_webView;
