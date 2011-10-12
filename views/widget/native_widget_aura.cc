ee486bb9179afe02c9421b2a52e9fa7680bac4b5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 14:55:22 +0000

    views: Make sure layer visibility is updated properly.
    If a View without a layer is hidden, and one of its descendant views has a layer, then the layer also needs to be hidden. This also needs to be reversed when such a View is redisplayed.
    
    BUG=none
    TEST=ViewLayerTreesInSync, VisibilityChildLayers
    
    Review URL: http://codereview.chromium.org/8216027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105080 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/native_widget_aura.cc
+++ views/widget/native_widget_aura.cc
@@ -170,7 +170,7 @@ void NativeWidgetAura::ReorderLayers() {
 }
 
 void NativeWidgetAura::ViewRemoved(View* view) {
-  NOTIMPLEMENTED();
+//  NOTIMPLEMENTED();
 }
 
 void NativeWidgetAura::SetNativeWindowProperty(const char* name, void* value) {
