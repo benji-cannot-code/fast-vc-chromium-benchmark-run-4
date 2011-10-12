3462309903f63506a6df914839e055d3adfd714b
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 16:26:17 +0000

    PrintPreview: Fix printer color settings issues based on printer ppd/schema information.
    Show/Hide the color options based on printer ppd/schema information. Some printers does not provide sufficient information in the printer schema/ppd regarding the color settings and they use custom advance settings to print in black & white/greyscale. In those cases, users need to print using native dialog in order to set these advance color settings.
    
    BUG=93811, 93490, 87344, 96658, 98768
    TEST= Please refer to bug description.
    
    Review URL: http://codereview.chromium.org/8138020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105087 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -250,7 +250,7 @@ function updateControlsWithSelectedPrinterCapabilities() {
         'disableColorOption': true,
         'setColorAsDefault': true,
         'setDuplexAsDefault': false,
-        'printerColorModelForColor': colorSettings.COLOR,
+        'printerColorModelForColor': print_preview.ColorSettings.COLOR,
         'printerDefaultDuplexValue': copiesSettings.UNKNOWN_DUPLEX_MODE,
         'disableCopiesOption': true});
   } else {
@@ -976,7 +976,8 @@ function createPDFPlugin(srcDataIndex) {
     pdfViewer.goToPage('0');
     pdfViewer.resetPrintPreviewUrl(srcURL);
     pdfViewer.reload();
-    pdfViewer.grayscale(colorSettings.colorMode == colorSettings.GRAY);
+    pdfViewer.grayscale(
+        colorSettings.colorMode == print_preview.ColorSettings.GRAY);
     return;
   }
 
--- chrome/browser/ui/webui/print_preview_handler.h
+++ chrome/browser/ui/webui/print_preview_handler.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/printing/print_view_manager_observer.h"
 #include "chrome/browser/ui/shell_dialogs.h"
 #include "content/browser/webui/web_ui.h"
+#include "printing/print_job_constants.h"
 
 class FilePath;
 class PrintSystemTaskProxy;
@@ -30,21 +31,6 @@ class StringValue;
 namespace printing {
 class PrintBackend;
 }
-
-#if defined(UNIT_TEST) && defined(USE_CUPS) && !defined(OS_MACOSX)
-typedef struct cups_option_s cups_option_t;
-
-namespace printing_internal {
-
-// Helper function to parse the lpoptions custom settings. |num_options| and
-// |options| will be updated if the custom settings for |printer_name| are
-// found, otherwise nothing is done.
-// NOTE: This function is declared here so it can be exposed for unit testing.
-void parse_lpoptions(const FilePath& filepath, const std::string& printer_name,
-                     int* num_options, cups_option_t** options);
-
-}  // namespace printing_internal
-#endif
 
 // The handler for Javascript messages related to the print preview dialog.
 class PrintPreviewHandler : public WebUIMessageHandler,
@@ -199,7 +185,7 @@ class PrintPreviewHandler : public WebUIMessageHandler,
   static FilePath* last_saved_path_;
   static std::string* last_used_printer_cloud_print_data_;
   static std::string* last_used_printer_name_;
-  static bool last_used_color_setting_;
+  static printing::ColorModels last_used_color_model_;
 
   // A count of how many requests received to regenerate preview data.
   // Initialized to 0 then incremented and emitted to a histogram.
