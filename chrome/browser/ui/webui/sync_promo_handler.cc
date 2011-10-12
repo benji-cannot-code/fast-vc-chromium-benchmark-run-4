5f1176bd2fe262a9a63d34b3486a037d78050955
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 05:50:42 +0000

    Sync Promo: Suppress promo further
    This change suppresses the sync promo if:
      - user clicks on "Skip for now"
      - user has ever signed into sync (even if they are currently signed out)
    
    BUG=99649, 98842
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8229027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo_handler.cc
+++ chrome/browser/ui/webui/sync_promo_handler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_setup_flow.h"
 #include "chrome/browser/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/webui/sync_promo_ui.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
@@ -159,6 +160,9 @@ void SyncPromoHandler::HandleUserFlowAction(const base::ListValue* args) {
   } else {
     NOTREACHED() << "Attempt to record invalid user flow action on sync promo.";
   }
+
+  if (action == extension_misc::SYNC_PROMO_SKIP_CLICKED)
+    SyncPromoUI::SetUserSkippedSyncPromo(Profile::FromWebUI(web_ui_));
 }
 
 int SyncPromoHandler::GetViewCount() const {
--- chrome/browser/ui/webui/sync_setup_handler.cc
+++ chrome/browser/ui/webui/sync_setup_handler.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_setup_flow.h"
 #include "chrome/browser/sync/util/oauth.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/webui/sync_promo_ui.h"
 #include "chrome/common/net/gaia/gaia_constants.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/tab_contents.h"
@@ -416,6 +417,10 @@ void SyncSetupHandler::ShowSetupDone(const std::wstring& user) {
   StringValue page("done");
   web_ui_->CallJavascriptFunction(
       "SyncSetupOverlay.showSyncSetupPage", page);
+
+  // Suppress the sync promo once the user signs into sync. This way the user
+  // doesn't see the sync promo even if they sign out of sync later on.
+  SyncPromoUI::SetUserSkippedSyncPromo(Profile::FromWebUI(web_ui_));
 }
 
 void SyncSetupHandler::SetFlow(SyncSetupFlow* flow) {
