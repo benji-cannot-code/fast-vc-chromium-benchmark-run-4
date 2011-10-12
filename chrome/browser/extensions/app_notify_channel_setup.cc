19c070d497d2b88f2e3a4f1094b475c73caf6548
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-12 01:37:22 +0000

    Add a per-context "Handler" concept to ChromeV8Extension and use it to update the chrome.app bidings.
    Review URL: http://codereview.chromium.org/8218004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104975 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_notify_channel_setup.cc
+++ chrome/browser/extensions/app_notify_channel_setup.cc
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_thread.h"
 
 AppNotifyChannelSetup::AppNotifyChannelSetup(
-    int request_id,
     const std::string& client_id,
     const GURL& requestor_url,
+    int return_route_id,
+    int callback_id,
     base::WeakPtr<AppNotifyChannelSetup::Delegate> delegate)
-    : request_id_(request_id),
-      client_id_(client_id),
+    : client_id_(client_id),
       requestor_url_(requestor_url),
+      return_route_id_(return_route_id),
+      callback_id_(callback_id),
       delegate_(delegate) {}
 
 AppNotifyChannelSetup::~AppNotifyChannelSetup() {}
@@ -45,6 +47,7 @@ void AppNotifyChannelSetup::ReportResult(
     const std::string& error) {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   if (delegate_.get())
-    delegate_->AppNotifyChannelSetupComplete(request_id_, channel_id, error);
+    delegate_->AppNotifyChannelSetupComplete(
+        channel_id, error, return_route_id_, callback_id_);
   Release(); // Matches AddRef in Start.
 }
---
+++ chrome/renderer/resources/extensions/app.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var chrome = chrome || {};
+(function() {
+  native function GetChromeHidden();
+  native function GetIsInstalled();
+  native function Install();
+  native function GetDetails();
+  native function GetDetailsForFrame();
+  native function GetAppNotifyChannel();
+
+  var chromeHidden = GetChromeHidden();
+  var callbacks = {};
+  var nextCallbackId = 1;
+
+  chrome.app = new function() {
+    this.__defineGetter__('isInstalled', GetIsInstalled);
+    this.install = Install;
+    this.getDetails = GetDetails;
+    this.getDetailsForFrame = GetDetailsForFrame;
+    this.experimental = {
+      getNotificationChannel: function(clientId, callback) {
+        var callbackId = 0;
+        if (callback) {
+          callbackId = nextCallbackId++;
+          callbacks[callbackId] = callback;
+        }
+        GetAppNotifyChannel(clientId, callbackId);
+      }
+    };
+  }();
+
+  chromeHidden.app = {};
+  chromeHidden.app.onGetAppNotifyChannelResponse =
+      function(channelId, error, callbackId) {
+    if (callbackId) {
+      callbacks[callbackId](channelId, error);
+      delete callbacks[callbackId];
+    }
+  };
+})();
