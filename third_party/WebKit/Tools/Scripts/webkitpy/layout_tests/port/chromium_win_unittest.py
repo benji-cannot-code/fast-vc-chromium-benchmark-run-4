f0331e1be672236776de6398f6a5cbf6514e7315
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-12 22:20:54 +0000

    Fix test-webkitpy after r97307 and r97293.
    * Scripts/webkitpy/layout_tests/port/chromium.py: options.time_out_ms
      is None during tests so add back the check.
    * Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py: The
      output directory is now build/Debug.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py
@@ -145,7 +145,7 @@ class ChromiumWinTest(port_testcase.PortTestCase):
             '/mock-checkout/Source/WebKit/chromium/build/Release/DumpRenderTree.exe',
             port._path_to_driver('Release'))
         self.assertEquals(
-            '/mock-checkout/Source/WebKit/chromium/Debug/DumpRenderTree.exe',
+            '/mock-checkout/Source/WebKit/chromium/build/Debug/DumpRenderTree.exe',
             port._path_to_driver('Debug'))
 
 
