390134a36bf0db207f97d5b707f3c5134f546f5b
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 02:06:06 +0000

    Change the default of the SSL_ENABLE_OB_CERTS option to PR_FALSE.
    This change does not affect Chromium because Chromium doesn't
    depend on the default value of this SSL option.
    
    R=mattm@chromium.org
    BUG=88782
    TEST=none
    
    Review URL: http://codereview.chromium.org/8249012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105241 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/sslsock.c
+++ net/third_party/nss/ssl/sslsock.c
@@ -188,7 +188,7 @@ static sslOptions ssl_defaults = {
     PR_FALSE,   /* enableFalseStart   */
     PR_FALSE,   /* enableOCSPStapling */
     PR_FALSE,   /* enableCachedInfo */
-    PR_TRUE,    /* enableOBCerts */
+    PR_FALSE,   /* enableOBCerts */
 };
 
 sslSessionIDLookupFunc  ssl_sid_lookup;
