d4bbdbd7984e968c5b73d6b738fc928e040c0f73
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-13 20:03:36 +0000

    Move XSTL to script-src in Content-Security-Policy https://bugs.webkit.org/show_bug.cgi?id=63637
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    The spec was unclear about how to treat XSLT.  My reading was that they
    should fall under style-src, but further discussion on the mailing list
    settled on using script-src because an XSLT can inject arbitrary DOM
    and script into a page, which is more risky than including style.
    
    Tests: http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-1.php
           http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-2.php
    
    * loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::canRequest):
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/xsl-allowed.php:
    * http/tests/security/contentSecurityPolicy/xsl-blocked-expected.txt:
    * http/tests/security/contentSecurityPolicy/xsl-blocked.php:
    * http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-1-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-1.php: Added.
    * http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-2-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-2.php: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-2.php
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/xsl-unaffected-by-style-src-2.php
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
 header("Content-Type: application/xhtml+xml");
-header("X-WebKit-CSP: script-src 'unsafe-inline'");
+header("X-WebKit-CSP: style-src 'none'; script-src * 'unsafe-inline'");
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 echo '<?xml-stylesheet type="text/xsl" href="resources/style.xsl"?>';
@@ -18,7 +18,7 @@ if (window.layoutTestController)
 </script>
 </head>
 <body>
-This test should render as a blank page because the style sheet will fail to load!
+The text below should indicate that the style sheet was applied.
 <div />
 </body>
 </html>
