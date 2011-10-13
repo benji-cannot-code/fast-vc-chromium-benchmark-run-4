bedf7bea3139dfde4a772aaecabf0a26a030246b
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 17:13:46 +0000

    Get chromeos aura build to run
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8258003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105320 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/about_page_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/about_page_handler.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8.h"
 #include "webkit/glue/user_agent.h"
 #include "webkit/glue/webkit_glue.h"
+#include "webkit/glue/user_agent.h"
 
 namespace {
 
