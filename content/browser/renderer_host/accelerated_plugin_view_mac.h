6e7f5e14be761625d20da60684f90eeffe90c6a6
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 18:41:43 +0000

    Prevent recursive calls to globalFrameDidChange.
    TEST=ran Google+ Hangout with Extras, resized window smaller; works correctly
    BUG=98262
    Review URL: http://codereview.chromium.org/8261004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105339 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/accelerated_plugin_view_mac.h
+++ content/browser/renderer_host/accelerated_plugin_view_mac.h
@@ -34,6 +34,8 @@ class RenderWidgetHostViewMac;
 
   // Rects that should show web content rather than plugin content.
   scoped_nsobject<NSArray> cutoutRects_;
+
+  BOOL handlingGlobalFrameDidChange_;
 }
 
 - (id)initWithRenderWidgetHostViewMac:(RenderWidgetHostViewMac*)r
--- content/browser/renderer_host/accelerated_plugin_view_mac.mm
+++ content/browser/renderer_host/accelerated_plugin_view_mac.mm
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       swapInterval = 1;
     [glContext_ setValues:&swapInterval forParameter:NSOpenGLCPSwapInterval];
 
+    handlingGlobalFrameDidChange_ = NO;
     [[NSNotificationCenter defaultCenter]
          addObserver:self
             selector:@selector(globalFrameDidChange:)
@@ -152,6 +153,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)globalFrameDidChange:(NSNotification*)notification {
+  // Short-circuit recursive calls.
+  if (handlingGlobalFrameDidChange_)
+    return;
+
+  handlingGlobalFrameDidChange_ = YES;
+
   // This call to -update can call -globalFrameDidChange: again, see
   // http://crbug.com/55754 comments 22 and 24.
   [glContext_ update];
@@ -161,6 +168,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSSize size = [self frame].size;
   glViewport(0, 0, size.width, size.height);
   CGLSetCurrentContext(0);
+
+  handlingGlobalFrameDidChange_ = NO;
 }
 
 - (void)prepareForGLRendering {
