2395eb59477491cb1b0fd0be2ce93f94cd666a45
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 18:04:46 +0000

    Revert 105176 - [Sync] Disable sync tabs automation.
    BUG=94693
    TEST=sync_integration_tests
    
    
    Review URL: http://codereview.chromium.org/8194014
    
    TBR=zea@chromium.org
    Review URL: http://codereview.chromium.org/8278003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/migration_errors_test.cc
+++ chrome/browser/sync/test/integration/migration_errors_test.cc
@@ -396,7 +396,7 @@ class MigrationReconfigureTest : public MigrationTwoClientTest {
   DISALLOW_COPY_AND_ASSIGN(MigrationReconfigureTest);
 };
 
-IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, FAILS_SetSyncTabs) {
+IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabs) {
   if (!ServerSupportsErrorTriggering()) {
     LOG(WARNING) << "Test skipped in this server environment.";
     return;
@@ -424,7 +424,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, FAILS_SetSyncTabs) {
   ASSERT_TRUE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 }
 
-IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, FAILS_SetSyncTabsAndMigrate) {
+IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabsAndMigrate) {
   if (!ServerSupportsErrorTriggering()) {
     LOG(WARNING) << "Test skipped in this server environment.";
     return;
