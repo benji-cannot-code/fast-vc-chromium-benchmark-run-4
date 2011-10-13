74a0837f53a5cbcf9aa1a31dc92a6351e8d1db4a
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 23:51:47 +0000

    Add screen power save block level.
    BUG=100054
    TEST=no change
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=105401
    
    Review URL: http://codereview.chromium.org/8251008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105415 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/power_save_blocker_stub.cc
+++ content/browser/power_save_blocker_stub.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Default, stub implementation, for platforms that don't have their own yet.
 
-// Called only from UI thread.
-// static
-void PowerSaveBlocker::ApplyBlock(PowerSaveBlockerType type) {
+void PowerSaveBlocker::ApplyBlock(bool blocking) {
   // http://code.google.com/p/chromium/issues/detail?id=33605
 }
