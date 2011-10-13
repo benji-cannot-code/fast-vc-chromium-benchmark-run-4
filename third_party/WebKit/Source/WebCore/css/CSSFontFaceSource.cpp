908c58d8081bd9d8d00e2fa1bbbd06eaf361cdda
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-13 21:52:12 +0000

    Unreviewed. Qt compile fix.
    * css/CSSFontFaceSource.cpp: include Document.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSFontFaceSource.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSFontFaceSource.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSFontFace.h"
 #include "CSSFontSelector.h"
 #include "CachedResourceLoader.h"
+#include "Document.h"
 #include "FontCache.h"
 #include "FontDescription.h"
 #include "GlyphPageTreeNode.h"
