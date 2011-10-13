1c5fbfcbcfe062b4c0a1804641915eefad65d4b3
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 02:39:47 +0000

    Define id_ to have the ID type for better readability.
    R=eroman@chromium.org
    BUG=none
    TEST=no compilation error
    
    Review URL: http://codereview.chromium.org/8251007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105245 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_config.h
+++ net/proxy/proxy_config.h
@@ -120,7 +120,7 @@ class NET_EXPORT ProxyConfig {
 
   // Used to numerically identify this configuration.
   ID id() const { return id_; }
-  void set_id(int id) { id_ = id; }
+  void set_id(ID id) { id_ = id; }
   bool is_valid() const { return id_ != kInvalidConfigID; }
 
   // Returns true if the given config is equivalent to this config.
@@ -206,7 +206,7 @@ class NET_EXPORT ProxyConfig {
   // Manual proxy settings.
   ProxyRules proxy_rules_;
 
-  int id_;
+  ID id_;
 };
 
 }  // namespace net
