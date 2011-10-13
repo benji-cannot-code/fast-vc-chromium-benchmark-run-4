27623a509daa025b446de766e1b0ba3257cfc293
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 02:48:50 +0000

    Implement 'SAVEAS' mode for PPB_FileChooser_Impl. Also, fix the Save File dialog when thesuggested file name is a root directory, such as "C:\". BUG=73070 TEST=manual
    Review URL: http://codereview.chromium.org/8142018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/file_select_helper.cc
+++ chrome/browser/file_select_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/file_util.h"
+#include "base/platform_file.h"
 #include "base/string_split.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
@@ -32,7 +33,28 @@ namespace {
 // There is only one file-selection happening at any given time,
 // so we allocate an enumeration ID for that purpose.  All IDs from
 // the renderer must start at 0 and increase.
-static const int kFileSelectEnumerationId = -1;
+const int kFileSelectEnumerationId = -1;
+
+void NotifyRenderViewHost(RenderViewHost* render_view_host,
+                          const std::vector<FilePath>& files,
+                          SelectFileDialog::Type dialog_type) {
+  const int kReadFilePermissions =
+      base::PLATFORM_FILE_OPEN |
+      base::PLATFORM_FILE_READ |
+      base::PLATFORM_FILE_EXCLUSIVE_READ |
+      base::PLATFORM_FILE_ASYNC;
+
+  const int kWriteFilePermissions =
+      base::PLATFORM_FILE_OPEN_ALWAYS |
+      base::PLATFORM_FILE_WRITE |
+      base::PLATFORM_FILE_WRITE_ATTRIBUTES |
+      base::PLATFORM_FILE_ASYNC;
+
+  int permissions = kReadFilePermissions;
+  if (dialog_type == SelectFileDialog::SELECT_SAVEAS_FILE)
+    permissions = kWriteFilePermissions;
+  render_view_host->FilesSelectedInChooser(files, permissions);
+}
 }
 
 struct FileSelectHelper::ActiveDirectoryEnumeration {
@@ -84,7 +106,7 @@ void FileSelectHelper::FileSelected(const FilePath& path,
 
   std::vector<FilePath> files;
   files.push_back(path);
-  render_view_host_->FilesSelectedInChooser(files);
+  NotifyRenderViewHost(render_view_host_, files, dialog_type_);
 
   // No members should be accessed from here on.
   RunFileChooserEnd();
@@ -97,7 +119,7 @@ void FileSelectHelper::MultiFilesSelected(const std::vector<FilePath>& files,
   if (!render_view_host_)
     return;
 
-  render_view_host_->FilesSelectedInChooser(files);
+  NotifyRenderViewHost(render_view_host_, files, dialog_type_);
 
   // No members should be accessed from here on.
   RunFileChooserEnd();
@@ -109,7 +131,8 @@ void FileSelectHelper::FileSelectionCanceled(void* params) {
 
   // If the user cancels choosing a file to upload we pass back an
   // empty vector.
-  render_view_host_->FilesSelectedInChooser(std::vector<FilePath>());
+  NotifyRenderViewHost(
+      render_view_host_, std::vector<FilePath>(), dialog_type_);
 
   // No members should be accessed from here on.
   RunFileChooserEnd();
@@ -161,7 +184,7 @@ void FileSelectHelper::OnListDone(int id, int error) {
     return;
   }
   if (id == kFileSelectEnumerationId)
-    entry->rvh_->FilesSelectedInChooser(entry->results_);
+    NotifyRenderViewHost(entry->rvh_, entry->results_, dialog_type_);
   else
     entry->rvh_->DirectoryEnumerationFinished(id, entry->results_);
 
