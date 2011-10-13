44e1c726bdba1deecad05479c85e07b4c94afcab
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 18:34:59 +0000

    Flash white instead of black when the URL is changed in the omnibox for the same tab
    This is separate from the previous fix, when a new tab is opened.
    When the URL of a tab is changed, the RWHV is deleted and another one is recreated. There is a period of time for which the root view contains no RWHV and is the top layer
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/8208009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/native_tab_contents_view_views.cc
+++ chrome/browser/ui/views/tab_contents/native_tab_contents_view_views.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/browser/tab_contents/tab_contents_view.h"
+#include "views/background.h"
 #include "views/widget/widget.h"
 
 // TODO(beng): HiddenTabHostWindow??
@@ -60,6 +61,10 @@ void NativeTabContentsViewViews::InitNativeTabContentsView() {
   params.native_widget = this;
   params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   GetWidget()->Init(params);
+  // Set root view to be white such that the user sees white instead of black
+  // when the RWHV is destroyed as a result of navigating to a new URL.
+  GetWidget()->GetRootView()->set_background(
+      views::Background::CreateSolidBackground(SK_ColorWHITE));
 }
 
 void NativeTabContentsViewViews::Unparent() {
