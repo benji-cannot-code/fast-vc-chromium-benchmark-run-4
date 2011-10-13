d9e571b7146bb18ea2fce1b1105b4f7970b1e436
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 00:45:23 +0000

    Pull FileBrowserPrivate API out of EventBindings.
    Review URL: http://codereview.chromium.org/8229040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_dispatcher.cc
+++ chrome/renderer/extensions/extension_dispatcher.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/extensions/event_bindings.h"
 #include "chrome/renderer/extensions/extension_groups.h"
 #include "chrome/renderer/extensions/extension_process_bindings.h"
+#include "chrome/renderer/extensions/file_browser_private_bindings.h"
 #include "chrome/renderer/extensions/renderer_extension_bindings.h"
 #include "chrome/renderer/extensions/user_script_slave.h"
 #include "content/public/renderer/render_thread.h"
@@ -97,6 +98,7 @@ void ExtensionDispatcher::WebKitInitialized() {
   RegisterExtension(new ChromeV8Extension(
       "extensions/json_schema.js", IDR_JSON_SCHEMA_JS, NULL), true);
   RegisterExtension(EventBindings::Get(this), true);
+  RegisterExtension(new FileBrowserPrivateBindings(), true);
   RegisterExtension(RendererExtensionBindings::Get(this), true);
   RegisterExtension(ExtensionProcessBindings::Get(this), true);
   RegisterExtension(new ChromeV8Extension(
