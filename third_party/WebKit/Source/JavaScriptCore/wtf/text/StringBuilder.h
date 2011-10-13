1f48be8a401939a3a2f6e26529e51ee0dd0a8318
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-13 17:00:59 +0000

    Use realloc() to expand/shrink StringBuilder buffer https://bugs.webkit.org/show_bug.cgi?id=69913
    Patch by Xianzhu Wang <wangxianzhu@chromium.org> on 2011-10-13
    Reviewed by Darin Adler.
    
    * wtf/text/StringBuilder.cpp:
    (WTF::StringBuilder::reserveCapacity):
    (WTF::StringBuilder::reallocateBuffer):
    (WTF::StringBuilder::appendUninitialized):
    (WTF::StringBuilder::shrinkToFit):
    * wtf/text/StringBuilder.h:
    * wtf/text/StringImpl.cpp:
    (WTF::StringImpl::reallocate): Added to allow StringBuilder to reallocate the buffer.
    * wtf/text/StringImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/text/StringBuilder.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/text/StringBuilder.h
@@ -131,6 +131,7 @@ public:
 
 private:
     void allocateBuffer(const UChar* currentCharacters, unsigned requiredLength);
+    void reallocateBuffer(unsigned requiredLength);
     UChar* appendUninitialized(unsigned length);
     void reifyString();
 
