36dd959ea82fe1a7ee212aba9f000d1f7752afdf
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-13 16:12:48 +0000

    [chromium] Add a selectionBounds() method to WebWidget. https://bugs.webkit.org/show_bug.cgi?id=69028
    Patch by Peng Huang <shawn.p.huang@gmail.com> on 2011-10-13
    Reviewed by Darin Fisher.
    
    * public/WebWidget.h:
    (WebKit::WebWidget::selectionBounds):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::selectionRange):
    (WebKit::WebViewImpl::selectionBounds):
    * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97368 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebWidget.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebWidget.h
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebInputEvent;
-class WebRange;
 class WebString;
 struct WebPoint;
 template <typename T> class WebVector;
@@ -150,14 +149,22 @@ public:
     // otherwise the anchor index will be the same value of the focus index.
     virtual bool getSelectionOffsetsAndTextInEditableContent(WebString&, size_t& focus, size_t& anchor) const { return false; }
 
+    // FIXME: It has been replaced by selectionBounds. Remove it when chromium
+    // switches to selectionBounds.
     // Returns the current caret bounds of this WebWidget. The selection bounds
     // will be returned if a selection range is available.
     virtual WebRect caretOrSelectionBounds() { return WebRect(); }
 
+    // FIXME: It has been replaced by selectionBounds. Remove it when chromium
+    // switches to selectionBounds.
     // Returns the start and end point for the current selection, aligned to the
     // bottom of the selected line. start and end are the logical beginning and
     // ending positions of the selection. Visually, start may lie after end.
     virtual bool selectionRange(WebPoint& start, WebPoint& end) const { return false; }
+
+    // Returns the start and end bounds of the current selection.
+    // If the selection range is empty, it returns the caret bounds.
+    virtual bool selectionBounds(WebRect& start, WebRect& end) const { return false; }
 
     // Fetch the current selection range of this WebWidget. If there is no
     // selection, it will output a 0-length range with the location at the
