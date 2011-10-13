a58a0add9e3c55380f44bde1a2be77abb89453d2
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 17:43:02 +0000

    Add Content-security-policy to chrome://imageburner Review URL: http://codereview.chromium.org/8221023
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_url_data_manager_backend.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_backend.cc
@@ -65,7 +65,6 @@ class ChromeURLContentSecurityPolicyExceptionSet
 #if defined(OS_CHROMEOS)
     insert(chrome::kChromeUIActiveDownloadsHost);
     insert(chrome::kChromeUIEnterpriseEnrollmentHost);
-    insert(chrome::kChromeUIImageBurnerHost);
     insert(chrome::kChromeUIKeyboardOverlayHost);
     insert(chrome::kChromeUIOobeHost);
     insert(chrome::kChromeUIMobileSetupHost);
