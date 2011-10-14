2dab4e94db953ef240383589bd07f5df4cb022d1
leandro@webkit.org <leandro@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-14 18:34:41 +0000

    Unreviewed; add missing #includes.
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    * DumpRenderTree/efl/EventSender.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97484 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GCController.h"
 #include "LayoutTestController.h"
 #include "NotImplemented.h"
+#include "WebCoreSupport/DumpRenderTreeSupportEfl.h"
 #include "WorkQueue.h"
 #include "ewk_private.h" // FIXME: create some WebCoreSupport/DumpRenderTree.cpp instead
 
--- third_party/WebKit/Tools/DumpRenderTree/efl/EventSender.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/EventSender.cpp
@@ -36,8 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventSender.h"
 
 #include "DumpRenderTree.h"
+#include "DumpRenderTreeChrome.h"
 #include "JSStringUtils.h"
 #include "NotImplemented.h"
+#include "WebCoreSupport/DumpRenderTreeSupportEfl.h"
 #include "ewk_private.h"
 #include <EWebKit.h>
 #include <JavaScriptCore/JSObjectRef.h>
