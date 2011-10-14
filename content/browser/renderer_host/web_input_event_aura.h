2e5e2b78588f7542896d4fc35b95da534e200669
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 17:25:10 +0000

    Adds factories for WebInputEvents for Aura.
    Explanation:
    
    WebKit::WebInputEvent uses more state from the original native event than aura::Event or views::Event expose. I'd rather not expose all of this in platform-neutral ways on aura::Event, since we don't really need any of it. So instead my approach was to create an appropriate WebInputEvent, initialize it directly from the underlying platform event, and then replace the position values with the translated ones on the aura::Event, since that's the only really Aura-specific bit.
    
    For Windows, we can just use the factory method in the chromium API. For X11, we need to write the factory ourselves since there isn't one in WebKit.
    
    This code isn't used by anything yet. I'll start using it in RenderWidgetHostViewAura next.
    
    http://crbug.com/99757
    TEST=none
    Review URL: http://codereview.chromium.org/8277027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105509 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/browser/renderer_host/web_input_event_aura.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_WEB_INPUT_EVENT_AURA_H_
+#define CONTENT_BROWSER_RENDERER_HOST_WEB_INPUT_EVENT_AURA_H_
+#pragma once
+
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebInputEvent.h"
+
+namespace aura {
+class KeyEvent;
+class MouseEvent;
+}
+
+namespace content {
+
+WebKit::WebMouseEvent MakeWebMouseEvent(aura::MouseEvent* event);
+WebKit::WebKeyboardEvent MakeWebKeyboardEvent(aura::KeyEvent* event);
+
+// TODO(beng): WebKit::WebMouseWheelEvent
+
+}
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_WEB_INPUT_EVENT_AURA_H_
