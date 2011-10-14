ae30132327914ddc3304cabf9e674e161d459be3
jeffm@apple.com <jeffm@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-14 22:11:12 +0000

    ../WebCore: InjectedBundleHitTestResult::imageRect() should return rect in WKView coordinates https://bugs.webkit.org/show_bug.cgi?id=69963
    Add infrastructure to convert from any frame view's coordinate system to the
    root view's coordinate system.
    
    Reviewed by Simon Fraser.
    
    No new tests (yet), this is covered by <https://bugs.webkit.org/show_bug.cgi?id=70136>.
    
    * WebCore.exp.in: Exported WebCore::ScrollView::contentsToRootView(), used by InjectedBundleHitTestResult.cpp.
            
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::rootViewToContents): Added (both point and rect versions).
    (WebCore::ScrollView::contentsToRootView): Ditto.
            
    * platform/ScrollView.h: Added member functions to convert to/from root view coordinates.
            
    * platform/Widget.cpp:
    (WebCore::Widget::convertFromRootView): Added (both point and rect versions).
    (WebCore::Widget::convertToRootView): Ditto.
            
    * platform/Widget.h: Added member functions to convert to/from root view coordinates.
    
    ../WebKit2: InjectedBundleHitTestResult::imageRect() should return rect in WKView coordinates
    https://bugs.webkit.org/show_bug.cgi?id=69963
            
    WebKit2 clients only have knowledge of the WKView's coordinate system, they have no way to
    convert from subframe view coordinates , so any rect that we expose through WK2 APIs should
    be in WKView coordinates.
            
    Reviewed by Simon Fraser.
    
    * WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp:
    (WebKit::InjectedBundleHitTestResult::imageRect): Use WebCore::FrameView::contentsToRootView() to convert the image rect to WKView coordinates.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundleHitTestResult.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/Document.h>
 #include <WebCore/Frame.h>
 #include <WebCore/FrameLoader.h>
+#include <WebCore/FrameView.h>
 #include <WebCore/KURL.h>
 #include <wtf/text/WTFString.h>
 
@@ -103,7 +104,25 @@ String InjectedBundleHitTestResult::linkTitle() const
 
 WebCore::IntRect InjectedBundleHitTestResult::imageRect() const
 {
-    return m_hitTestResult.imageRect();
+    WebCore::IntRect imageRect = m_hitTestResult.imageRect();
+    if (imageRect.isEmpty())
+        return imageRect;
+        
+    // The image rect in WebCore::HitTestResult is in frame coordinates, but we need it in WKView
+    // coordinates since WebKit2 clients don't have enough context to do the conversion themselves.
+    WebFrame* webFrame = frame();
+    if (!webFrame)
+        return imageRect;
+    
+    WebCore::Frame* coreFrame = webFrame->coreFrame();
+    if (!coreFrame)
+        return imageRect;
+    
+    WebCore::FrameView* view = coreFrame->view();
+    if (!view)
+        return imageRect;
+    
+    return view->contentsToRootView(imageRect);
 }
 
 bool InjectedBundleHitTestResult::isSelected() const
