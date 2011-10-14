ca4d5fd6bc8eceaac2287110fb7d55b042535746
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-14 17:23:39 +0000

    DFG JIT 32_64 - Performance fix for ResolveGlobal https://bugs.webkit.org/show_bug.cgi?id=70096
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-10-14
    Reviewed by Gavin Barraclough.
    
    Structure check of global object should be a pointer comparison
    instead of a tag and payload pair comparison. This fix improves
    SunSpider by 7% on Linux 32, with bitops-bitwise-and improved by 4.75X.
    Also two trivial fixes for successful 32-bit build are included.
    
    * dfg/DFGSpeculativeJIT.cpp:
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(DFG_JIT)
 
+#if USE(JSVALUE32_64)
+#include "DFGJITCompilerInlineMethods.h"
+#endif
+
 namespace JSC { namespace DFG {
 
 #ifndef NDEBUG
