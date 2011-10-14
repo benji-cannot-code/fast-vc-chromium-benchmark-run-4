1dfb6243313888fce50370c7528eee2159f4c2d6
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-14 00:42:06 +0000

    Another SnowLeopard build fix.
    * WebProcess/WebCoreSupport/mac/WebDragClientMac.mm:
    (WebKit::cachedImage):
    (WebKit::WebDragClient::declareAndWriteDragImage):
    (-[WKPasteboardOwner initWithImage:WebCore::]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/mac/WebDragClientMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/mac/WebDragClientMac.mm
@@ -103,14 +103,14 @@ void WebDragClient::startDrag(RetainPtr<NSImage> image, const IntPoint& point, c
     m_page->send(Messages::WebPageProxy::SetDragImage(frame->view()->contentsToWindow(point), handle, linkDrag));
 }
 
-static CachedImage* cachedImage(Element* element)
+static WebCore::CachedImage* cachedImage(Element* element)
 {
     RenderObject* renderer = element->renderer();
     if (!renderer)
         return 0;
     if (!renderer->isRenderImage())
         return 0;
-    CachedImage* image = toRenderImage(renderer)->cachedImage();
+    WebCore::CachedImage* image = toRenderImage(renderer)->cachedImage();
     if (!image || image->errorOccurred()) 
         return 0;
     return image;
@@ -129,7 +129,7 @@ void WebDragClient::declareAndWriteDragImage(NSPasteboard *pasteboard, DOMElemen
 
     Element* coreElement = core(element);
 
-    CachedImage* image = cachedImage(coreElement);
+    WebCore::CachedImage* image = cachedImage(coreElement);
 
     NSString *extension = @"";
     if (image) {
@@ -226,7 +226,7 @@ static CachedImageClient* promisedDataClient()
     _image = 0;
 }
 
-- (id)initWithImage:(CachedImage*)image
+- (id)initWithImage:(WebCore::CachedImage*)image
 {
     self = [super init];
     if (!self)
