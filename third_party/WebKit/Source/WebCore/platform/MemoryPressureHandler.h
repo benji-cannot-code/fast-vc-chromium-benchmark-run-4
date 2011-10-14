bcbc9e72e024c3fd88bd0825846d6288e12a5a75
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-14 06:14:54 +0000

    [Qt][Win] Unreviewed buildfix after r97433.
    * platform/MemoryPressureHandler.h: Add a missing include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/MemoryPressureHandler.h
+++ third_party/WebKit/Source/WebCore/platform/MemoryPressureHandler.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MemoryPressureHandler_h
 #define MemoryPressureHandler_h
 
+#include <time.h>
 #include <wtf/Platform.h>
 
 namespace WebCore {
