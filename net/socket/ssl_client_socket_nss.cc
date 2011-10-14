0496e84c1e185d6dfcc9d1521253f0fcc364f395
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 21:45:32 +0000

    DoGetOBCertComplete should reset client_auth_cert_needed_ to false to prevent from going from STATE_HANDSHAKE back to the STATE_GET_OB_CERT_COMPLETE by mistake.
    R=mattm@chromium.org
    BUG=88782
    TEST=Chrome should not time out when connecting to a site requesting
    an origin-bound certificate for the first time.
    Review URL: http://codereview.chromium.org/8305008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105585 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -1580,6 +1580,7 @@ int SSLClientSocketNSS::ImportOBCertAndKey(CERTCertificate** cert,
 }
 
 int SSLClientSocketNSS::DoGetOBCertComplete(int result) {
+  client_auth_cert_needed_ = false;
   ob_cert_request_handle_ = NULL;
 
   if (result != OK)
