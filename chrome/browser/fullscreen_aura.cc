02ca93a707de298f18051c473841fa8da1a93478
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 19:20:33 +0000

    aura: Implement IsFullscreenMode().
    We walk down the tree looking for windows that have
    delegates, are visible, and are fullscreen.
    
    BUG=99710
    TEST=added
    
    Review URL: http://codereview.chromium.org/8233023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/fullscreen_aura.cc
+++ chrome/browser/fullscreen_aura.cc
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/fullscreen.h"
 
 #include "base/logging.h"
+#include "ui/aura/desktop.h"
+#include "ui/aura/window.h"
 
 bool IsFullScreenMode() {
-  NOTIMPLEMENTED();
-  return false;
+  return aura::Desktop::GetInstance()->window()->IsOrContainsFullscreenWindow();
 }
