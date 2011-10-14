958ca00b607514db08695e04b0648777f852a1c7
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 21:10:00 +0000

    Fix RENDERER_PROCESS_CREATED AllSources listeners in UserScriptMaster and VisitedLinkEventListener.
    BUG=99391
    TEST=updated unit tests
    
    Review URL: http://codereview.chromium.org/8286015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/visitedlink/visitedlink_event_listener.h
+++ chrome/browser/visitedlink/visitedlink_event_listener.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/notification_observer.h"
 #include "content/common/notification_registrar.h"
 
+class Profile;
 class VisitedLinkUpdater;
 
 namespace base {
@@ -28,7 +29,7 @@ class SharedMemory;
 class VisitedLinkEventListener : public VisitedLinkMaster::Listener,
                                  public NotificationObserver {
  public:
-  VisitedLinkEventListener();
+  explicit VisitedLinkEventListener(Profile* profile);
   virtual ~VisitedLinkEventListener();
 
   virtual void NewTable(base::SharedMemory* table_memory);
@@ -51,6 +52,8 @@ class VisitedLinkEventListener : public VisitedLinkMaster::Listener,
   // Map between renderer child ids and their VisitedLinkUpdater.
   typedef std::map<int, linked_ptr<VisitedLinkUpdater> > Updaters;
   Updaters updaters_;
+
+  Profile* profile_;
 
   DISALLOW_COPY_AND_ASSIGN(VisitedLinkEventListener);
 };
