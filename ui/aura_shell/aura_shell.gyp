c17963d540b9f5b057a233f219386deb0d512a3b
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 03:44:34 +0000

    Shrinks the launcher down to 48 pixels high, changes chromium image appropriately, and gets browser window entry to match mocks. For aura_shell I'm randomly adding 3 images to test that it works.
    BUG=97262
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8274019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105455 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/aura_shell.gyp
+++ ui/aura_shell/aura_shell.gyp
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'launcher/launcher_types.h',
         'launcher/launcher_view.cc',
         'launcher/launcher_view.h',
+        'launcher/tabbed_launcher_button.cc',
+        'launcher/tabbed_launcher_button.h',
         'shell.cc',
         'shell.h',
         'shell_delegate.h',
--- ui/aura_shell/launcher/launcher_view.cc
+++ ui/aura_shell/launcher/launcher_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "grit/ui_resources.h"
 #include "ui/aura_shell/launcher/launcher_model.h"
+#include "ui/aura_shell/launcher/tabbed_launcher_button.h"
 #include "ui/aura_shell/shell.h"
 #include "ui/aura_shell/shell_delegate.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -23,9 +24,12 @@ namespace internal {
 // Padding between each view.
 static const int kHorizontalPadding = 12;
 
+// Amount content is inset on the left edge.
+static const int kLeadingInset = 8;
+
 // Height of the LauncherView. Hard coded to avoid resizing as items are
 // added/removed.
-static const int kPreferredHeight = 64;
+static const int kPreferredHeight = 48;
 
 LauncherView::LauncherView(LauncherModel* model)
     : model_(model),
@@ -59,7 +63,11 @@ void LauncherView::Init() {
 }
 
 views::View* LauncherView::CreateViewForItem(const LauncherItem& item) {
-  // TODO: need to support images.
+  if (item.type == TYPE_TABBED) {
+    TabbedLauncherButton* button = new TabbedLauncherButton(this);
+    button->SetImages(item.tab_images);
+    return button;
+  }
   views::ImageButton* button = new views::ImageButton(this);
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
   button->SetImage(
@@ -75,8 +83,7 @@ void LauncherView::Resize() {
 }
 
 void LauncherView::Layout() {
-  // TODO: need to deal with overflow.
-  int x = kHorizontalPadding;
+  int x = kLeadingInset;
   for (int i = 0; i < child_count(); ++i) {
     View* child = child_at(i);
     if (child->IsVisible()) {
@@ -86,10 +93,12 @@ void LauncherView::Layout() {
       x += child->width() + kHorizontalPadding;
     }
   }
+  // TODO: remove this when we get a better image.
+  show_apps_button_->SetX(show_apps_button_->x() - 8);
 }
 
 gfx::Size LauncherView::GetPreferredSize() {
-  int x = kHorizontalPadding;
+  int x = kLeadingInset;
   for (int i = 0; i < child_count(); ++i) {
     View* child = child_at(i);
     if (child->IsVisible()) {
@@ -97,6 +106,8 @@ gfx::Size LauncherView::GetPreferredSize() {
       x += pref_size.width() + kHorizontalPadding;
     }
   }
+  // TODO: remove this when we get a better image.
+  x -= 10;
   return gfx::Size(x, kPreferredHeight);
 }
 
