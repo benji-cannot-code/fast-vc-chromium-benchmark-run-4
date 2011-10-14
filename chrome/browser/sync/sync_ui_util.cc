5e10a2b450035b6ee9248ea19cf38806005b4e39
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 00:11:17 +0000

    Revert 105010 - [Sync] Use IsPassphraseRequiredForDecryption() for sync error bubble
    This prevents the bubble from showing up if there are no encrypted
    types.
    
    BUG=99875
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8231026
    
    TBR=akalin@chromium.org
    Review URL: http://codereview.chromium.org/8273025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -390,7 +390,7 @@ bool ShouldShowSyncErrorButton(ProfileSyncService* service) {
          ((!service->IsManaged() &&
            service->HasSyncSetupCompleted()) &&
          (GetStatus(service) == sync_ui_util::SYNC_ERROR ||
-          service->IsPassphraseRequiredForDecryption()));
+          service->IsPassphraseRequired()));
 }
 
 string16 GetSyncMenuLabel(ProfileSyncService* service) {
