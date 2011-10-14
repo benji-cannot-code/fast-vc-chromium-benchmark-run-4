2eb06c022195ee9d07ee93b24a23e2ed62dd73ca
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 23:08:20 +0000

    Grant the GL bindings permission to do file I/O during their initialization. This one-time initialization takes very little time and is impractical to move to another thread when the bindings are needed.
    Re-enable NoStartupWindowTest.NoStartupWindowBasicTest on Mac.
    
    BUG=100298
    TEST=NoStartupWindowTest.NoStartupWindowBasicTest
    TBR=atwilson
    Review URL: http://codereview.chromium.org/8301014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105605 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_implementation_mac.cc
+++ ui/gfx/gl/gl_implementation_mac.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/file_path.h"
 #include "base/logging.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/mac/foundation_util.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
@@ -24,6 +25,12 @@ bool InitializeGLBindings(GLImplementation implementation) {
   // later switch to another GL implementation.
   if (GetGLImplementation() != kGLImplementationNone)
     return true;
+
+  // Allow the main thread or another to initialize these bindings
+  // after instituting restrictions on I/O. Going forward they will
+  // likely be used in the browser process on most platforms. The
+  // one-time initialization cost is small, between 2 and 5 ms.
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
 
   switch (implementation) {
     case kGLImplementationOSMesaGL: {
