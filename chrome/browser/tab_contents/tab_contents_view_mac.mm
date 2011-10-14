7da6a0813d9fa6c09577452df91f8e3219f4a38d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 23:44:45 +0000

    Mac: Fix a crash in TabContentsViewCocoa::mouseEvent.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8293005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105612 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/tab_contents_view_mac.mm
+++ chrome/browser/tab_contents/tab_contents_view_mac.mm
@@ -57,6 +57,7 @@ COMPILE_ASSERT_MATCHING_ENUM(DragOperationEvery);
                         image:(NSImage*)image
                        offset:(NSPoint)offset;
 - (void)cancelDeferredClose;
+- (void)clearTabContentsView;
 - (void)closeTabAfterEvent;
 - (void)viewDidBecomeFirstResponder:(NSNotification*)notification;
 @end
@@ -80,6 +81,7 @@ TabContentsViewMac::~TabContentsViewMac() {
   // close.  In that case, the Cocoa view outlives the
   // TabContentsViewMac instance due to Cocoa retain count.
   [cocoa_view_ cancelDeferredClose];
+  [cocoa_view_ clearTabContentsView];
 }
 
 void TabContentsViewMac::CreateView(const gfx::Size& initial_size) {
@@ -460,12 +462,14 @@ void TabContentsViewMac::Observe(int type,
 }
 
 - (TabContents*)tabContents {
+  if (tabContentsView_ == NULL)
+    return NULL;
   return tabContentsView_->tab_contents();
 }
 
 - (void)mouseEvent:(NSEvent *)theEvent {
   TabContents* tabContents = [self tabContents];
-  if (tabContents->delegate()) {
+  if (tabContents && tabContents->delegate()) {
     NSPoint location = [NSEvent mouseLocation];
     if ([theEvent type] == NSMouseMoved)
       tabContents->delegate()->ContentsMouseEvent(
@@ -569,6 +573,10 @@ void TabContentsViewMac::Observe(int type,
   [NSObject cancelPreviousPerformRequestsWithTarget:self
                                            selector:aSel
                                              object:nil];
+}
+
+- (void)clearTabContentsView {
+  tabContentsView_ = NULL;
 }
 
 - (void)closeTabAfterEvent {
