ca33dfb299386057b98ecdc6b493a9146a288fd1
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-15 00:19:38 +0000

    Revert 105607 - Revert 105602 - Ensure GLES2CmdDecoder is not destroyed if it has not been initialized.^ The culprit appears to have been 105595, now reverted. If the above happens, the accounting of the number of contexts in the share group gets out of sync.BUG=97775Review URL: http://codereview.chromium.org/8298008TBR=apatrick@chromium.orgReview URL: http://codereview.chromium.org/8301015TBR=jar@chromium.org Review URL: http://codereview.chromium.org/8295020
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105618 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -203,6 +203,9 @@ void GpuCommandBufferStub::OnInitialize(
   }
 
   if (!surface_.get()) {
+    // Ensure the decoder is not destroyed if it is not initialized.
+    decoder_.reset();
+
     LOG(ERROR) << "Failed to create surface.\n";
     OnInitializeFailed(reply_message);
     return;
@@ -217,6 +220,9 @@ void GpuCommandBufferStub::OnInitialize(
       surface_.get(),
       gpu_preference);
   if (!context_.get()) {
+    // Ensure the decoder is not destroyed if it is not initialized.
+    decoder_.reset();
+
     LOG(ERROR) << "Failed to create context.\n";
     OnInitializeFailed(reply_message);
     return;
