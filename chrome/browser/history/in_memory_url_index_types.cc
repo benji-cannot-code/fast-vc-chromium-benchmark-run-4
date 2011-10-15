9f431da1000dcf2e08219ba98622547555b54e9d
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-15 19:23:47 +0000

    Fix MSVC 2010 compile following http://crrev.com/105678
    R=mrossetti@chromium.org
    BUG=96731, 92718
    TEST=none
    
    Review URL: http://codereview.chromium.org/8310004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_url_index_types.cc
+++ chrome/browser/history/in_memory_url_index_types.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/in_memory_url_index_types.h"
 
 #include <algorithm>
+#include <iterator>
 
 #include "base/i18n/break_iterator.h"
 #include "base/i18n/case_conversion.h"
