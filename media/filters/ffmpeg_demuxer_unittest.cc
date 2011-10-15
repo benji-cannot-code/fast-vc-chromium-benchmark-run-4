7f0cf2434f0a62571a3f495f980a4042b68fc636
ddorwin@chromium.org <ddorwin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-15 06:55:18 +0000

    Fix Valgrind warnings from r105646.
    TBR=scherkus@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8304020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105647 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_demuxer_unittest.cc
+++ media/filters/ffmpeg_demuxer_unittest.cc
@@ -37,6 +37,10 @@ MATCHER(IsEndOfStreamBuffer,
   return arg->IsEndOfStream();
 }
 
+ACTION(DeleteArg0Buffer) {
+  scoped_refptr<Buffer> buffer(arg0);
+}
+
 // Fixture class to facilitate writing tests.  Takes care of setting up the
 // FFmpeg, pipeline and filter host mocks.
 class FFmpegDemuxerTest : public testing::Test {
@@ -404,7 +408,8 @@ TEST_F(FFmpegDemuxerTest, Stop) {
 
   // The callback should be immediately deleted.  We'll use a checkpoint to
   // verify that it has indeed been deleted.
-  EXPECT_CALL(*callback, Run(NotNull()));
+  EXPECT_CALL(*callback, Run(IsEndOfStreamBuffer()))
+      .WillOnce(DeleteArg0Buffer());
   EXPECT_CALL(*callback, OnDelete());
   EXPECT_CALL(*this, CheckPoint(1));
 
@@ -443,7 +448,8 @@ TEST_F(FFmpegDemuxerTest, StreamReadAfterStopAndDemuxerDestruction) {
 
   // The callback should be immediately deleted.  We'll use a checkpoint to
   // verify that it has indeed been deleted.
-  EXPECT_CALL(*callback, Run(IsEndOfStreamBuffer()));
+  EXPECT_CALL(*callback, Run(IsEndOfStreamBuffer()))
+      .WillOnce(DeleteArg0Buffer());
   EXPECT_CALL(*callback, OnDelete());
   EXPECT_CALL(*this, CheckPoint(1));
 
