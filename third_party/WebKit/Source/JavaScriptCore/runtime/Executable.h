7d5274476f640419475df540f5f42f7b1d1e1ea0
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-15 21:44:50 +0000

    FunctionExecutable should expose the ability to create unattached FunctionCodeBlocks https://bugs.webkit.org/show_bug.cgi?id=70157
    Reviewed by Geoff Garen.
            
    Added FunctionExecutable::produceCodeBlockFor() and rewired compileForCallInternal()
    and compileForConstructInternal() to use this method. This required more cleanly
    exposing some of CodeBlock's tiering functionality and moving the CompilationKind
    enum to Executable.h, as this was the easiest way to make it available to the
    declarations/definitions of CodeBlock, FunctionExecutable, and BytecodeGenerator.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::copyDataFrom):
    (JSC::CodeBlock::copyDataFromAlternative):
    * bytecode/CodeBlock.h:
    (JSC::CodeBlock::setAlternative):
    * bytecompiler/BytecodeGenerator.h:
    * runtime/Executable.cpp:
    (JSC::EvalExecutable::compileInternal):
    (JSC::ProgramExecutable::compileInternal):
    (JSC::FunctionExecutable::produceCodeBlockFor):
    (JSC::FunctionExecutable::compileForCallInternal):
    (JSC::FunctionExecutable::compileForConstructInternal):
    * runtime/Executable.h:
    (JSC::FunctionExecutable::codeBlockFor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
@@ -46,6 +46,7 @@ namespace JSC {
     struct ExceptionInfo;
     
     enum CodeSpecializationKind { CodeForCall, CodeForConstruct };
+    enum CompilationKind { FirstCompilation, OptimizingCompilation };
 
     class ExecutableBase : public JSCell {
         friend class JIT;
@@ -461,6 +462,8 @@ namespace JSC {
             ASSERT(m_codeBlockForConstruct);
             return *m_codeBlockForConstruct;
         }
+        
+        PassOwnPtr<FunctionCodeBlock> produceCodeBlockFor(ExecState*, ScopeChainNode*, CompilationKind, CodeSpecializationKind, JSObject*& exception);
 
         JSObject* compileForCall(ExecState* exec, ScopeChainNode* scopeChainNode, ExecState* calleeArgsExec = 0)
         {
@@ -606,6 +609,14 @@ namespace JSC {
 
         JSObject* compileForCallInternal(ExecState*, ScopeChainNode*, ExecState* calleeArgsExec, JITCode::JITType);
         JSObject* compileForConstructInternal(ExecState*, ScopeChainNode*, ExecState* calleeArgsExec, JITCode::JITType);
+        
+        OwnPtr<FunctionCodeBlock>& codeBlockFor(CodeSpecializationKind kind)
+        {
+            if (kind == CodeForCall)
+                return m_codeBlockForCall;
+            ASSERT(kind == CodeForConstruct);
+            return m_codeBlockForConstruct;
+        }
         
         static const unsigned StructureFlags = OverridesVisitChildren | ScriptExecutable::StructureFlags;
         unsigned m_numCapturedVariables : 31;
