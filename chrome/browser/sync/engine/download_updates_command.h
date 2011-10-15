7cb881ea9545743d232b2ffdcef1a450f704f8f7
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-15 20:19:06 +0000

    Send important client side event information to the server. We create a class called DebugInfoEventListener which implements 2 interfaces. one being the syncmanager::observer to observe events from syncmanager. The other being DebugInfoGetter so that syncer can call this class to give the debug information in protobuf format. The implementation of this class uses a queue. And it limits the number of events to 6. if more than 6 events are in the queue we delete the oldest event. Also we send this information to the server only once per sync cycle during the first getudpdates command.
    also includes the unit test.
    
    TBR=cmp@chromium.org for the SI relaxation.
    BUG=100058
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=105667
    
    Review URL: http://codereview.chromium.org/8189003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/download_updates_command.h
+++ chrome/browser/sync/engine/download_updates_command.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "chrome/browser/sync/engine/model_safe_worker.h"
 #include "chrome/browser/sync/engine/syncer_command.h"
+#include "chrome/browser/sync/protocol/sync.pb.h"
 #include "chrome/browser/sync/syncable/model_type.h"
 
 namespace sync_pb {
@@ -48,6 +50,9 @@ class DownloadUpdatesCommand : public SyncerCommand {
                          sync_pb::EntitySpecifics* filter_protobuf);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(DownloadUpdatesCommandTest, VerifyAppendDebugInfo);
+  void AppendClientDebugInfoIfNeeded(sessions::SyncSession* session,
+      sync_pb::DebugInfo* debug_info);
   DISALLOW_COPY_AND_ASSIGN(DownloadUpdatesCommand);
 };
 
--- chrome/browser/sync/sessions/sync_session_context.h
+++ chrome/browser/sync/sessions/sync_session_context.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/sync/engine/model_safe_worker.h"
 #include "chrome/browser/sync/engine/syncer_types.h"
+#include "chrome/browser/sync/sessions/debug_info_getter.h"
 
 namespace syncable {
 class DirectoryManager;
@@ -50,7 +51,8 @@ class SyncSessionContext {
   SyncSessionContext(ServerConnectionManager* connection_manager,
                      syncable::DirectoryManager* directory_manager,
                      ModelSafeWorkerRegistrar* model_safe_worker_registrar,
-                     const std::vector<SyncEngineEventListener*>& listeners);
+                     const std::vector<SyncEngineEventListener*>& listeners,
+                     DebugInfoGetter* debug_info_getter);
   ~SyncSessionContext();
 
   ConflictResolver* resolver() { return resolver_; }
@@ -65,6 +67,10 @@ class SyncSessionContext {
   }
   ExtensionsActivityMonitor* extensions_monitor() {
     return extensions_activity_monitor_;
+  }
+
+  DebugInfoGetter* debug_info_getter() {
+    return debug_info_getter_;
   }
 
   // Talk notification status.
@@ -137,6 +143,10 @@ class SyncSessionContext {
 
   // Cache of last session snapshot information.
   scoped_ptr<sessions::SyncSessionSnapshot> previous_session_snapshot_;
+
+  // We use this to get debug info to send to the server for debugging
+  // client behavior on server side.
+  DebugInfoGetter* const debug_info_getter_;
 
   DISALLOW_COPY_AND_ASSIGN(SyncSessionContext);
 };
