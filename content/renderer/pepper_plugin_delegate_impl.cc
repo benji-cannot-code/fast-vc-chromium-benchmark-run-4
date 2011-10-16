d58565684c71c7d2d9dabee2a3060b49384fa4ad
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-16 02:33:08 +0000

    Fix use-after-free in PepperPluginDelegateImpl::GetTextInputType.
    BUG=100153
    TEST=manual
    
    This patch is to correctly nullify a dangling reference to a deleted plugin instance.
    
    
    Review URL: http://codereview.chromium.org/8298020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105708 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper_plugin_delegate_impl.cc
+++ content/renderer/pepper_plugin_delegate_impl.cc
@@ -985,6 +985,8 @@ void PepperPluginDelegateImpl::InstanceDeleted(
   }
   if (last_mouse_event_target_ == instance)
     last_mouse_event_target_ = NULL;
+  if (focused_plugin_ == instance)
+    PluginFocusChanged(instance, false);
 }
 
 SkBitmap* PepperPluginDelegateImpl::GetSadPluginBitmap() {
