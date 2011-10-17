c822715130e7efa291864ede0a61c4f63b82e103
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 16:12:40 +0000

    Added global error bubble for unwanted settings changes.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8302006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -173,6 +173,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_SHOW_SYNC_SETUP             40030
 #define IDC_EXTENSION_ERRORS            40031
 #define IDC_SHOW_SYNC_ERROR             40032
+#define IDC_SHOW_SETTINGS_CHANGES       40033
 
 // Spell-check
 // Insert any additional suggestions before _LAST; these have to be consecutive.
