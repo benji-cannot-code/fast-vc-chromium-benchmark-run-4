0eb4c2de517c358125bcfea51b5938d688f022ed
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-17 14:03:19 +0000

    Interpreter build fix after r97564
    * runtime/Executable.cpp:
    (JSC::FunctionExecutable::compileForCallInternal):
    (JSC::FunctionExecutable::compileForConstructInternal):
    Moved declaration of globalData variable into ENABLE(JIT) blocks, since it is only used
    there.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.cpp
@@ -441,10 +441,8 @@ JSObject* FunctionExecutable::compileForCallInternal(ExecState* exec, ScopeChain
 #if !ENABLE(JIT)
     UNUSED_PARAM(jitType);
 #endif
-    JSObject* exception;
-    JSGlobalData* globalData = scopeChainNode->globalData;
-    
     ASSERT((jitType == JITCode::bottomTierJIT()) == !m_codeBlockForCall);
+    JSObject* exception;
     OwnPtr<FunctionCodeBlock> newCodeBlock = produceCodeBlockFor(exec, scopeChainNode, !!m_codeBlockForCall ? OptimizingCompilation : FirstCompilation, CodeForCall, exception);
     if (!newCodeBlock)
         return exception;
@@ -458,6 +456,7 @@ JSObject* FunctionExecutable::compileForCallInternal(ExecState* exec, ScopeChain
     m_symbolTable = m_codeBlockForCall->sharedSymbolTable();
 
 #if ENABLE(JIT)
+    JSGlobalData* globalData = scopeChainNode->globalData;
     if (globalData->canUseJIT()) {
         bool dfgCompiled = false;
         if (jitType == JITCode::DFGJIT)
@@ -502,10 +501,8 @@ JSObject* FunctionExecutable::compileForConstructInternal(ExecState* exec, Scope
 {
     UNUSED_PARAM(jitType);
     
-    JSObject* exception;
-    JSGlobalData* globalData = scopeChainNode->globalData;
-    
     ASSERT((jitType == JITCode::bottomTierJIT()) == !m_codeBlockForConstruct);
+    JSObject* exception;
     OwnPtr<FunctionCodeBlock> newCodeBlock = produceCodeBlockFor(exec, scopeChainNode, !!m_codeBlockForConstruct ? OptimizingCompilation : FirstCompilation, CodeForConstruct, exception);
     if (!newCodeBlock)
         return exception;
@@ -519,6 +516,7 @@ JSObject* FunctionExecutable::compileForConstructInternal(ExecState* exec, Scope
     m_symbolTable = m_codeBlockForConstruct->sharedSymbolTable();
 
 #if ENABLE(JIT)
+    JSGlobalData* globalData = scopeChainNode->globalData;
     if (globalData->canUseJIT()) {
         bool dfgCompiled = false;
         if (jitType == JITCode::DFGJIT)
