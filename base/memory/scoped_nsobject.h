746f5b761f503eff264f20aa4837f4bde1032045
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 04:25:20 +0000

    Remove an unused typedef in scoped_nsobject.h
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8283019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105764 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/scoped_nsobject.h
+++ base/memory/scoped_nsobject.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 template<typename NST>
 class scoped_nsobject {
  public:
-  typedef NST* element_type;
-
   explicit scoped_nsobject(NST* object = nil)
       : object_(object) {
   }
@@ -103,8 +101,6 @@ bool operator!=(C* p1, const scoped_nsobject<C>& p2) {
 template<>
 class scoped_nsobject<id> {
  public:
-  typedef id element_type;
-
   explicit scoped_nsobject(id object = nil)
       : object_(object) {
   }
