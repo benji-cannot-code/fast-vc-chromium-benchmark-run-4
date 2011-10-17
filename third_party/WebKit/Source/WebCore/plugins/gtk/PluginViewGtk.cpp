b56ed87c3921802ef7a03982285a77ee9fd10d96
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-17 07:51:55 +0000

    Fix clang WebKitGTK+ build.
    Patch by Andrei Lavreniyuk <andy.lavr@gmail.com> on 2011-10-17
    Reviewed by Martin Robinson.
    
    * plugins/gtk/PluginViewGtk.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/gtk/PluginViewGtk.cpp
+++ third_party/WebKit/Source/WebCore/plugins/gtk/PluginViewGtk.cpp
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gtk/gtk.h>
 
 #if defined(XP_UNIX)
+#define String XtStringType
 #include "RefPtrCairo.h"
 #include "gtk2xtbin.h"
 #define Bool int // this got undefined somewhere
@@ -82,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(GDK_WINDOWING_WIN32)
 #include "PluginMessageThrottlerWin.h"
 #include <gdk/gdkwin32.h>
+#undef String
 #endif
 
 using JSC::ExecState;
