4ff0338757b7ed399d0020cb0ba8bc80f68168c6
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 23:02:35 +0000

    ntp4: UMA for app dragging
    BUG=95198
    TEST=none
    
    Review URL: http://codereview.chromium.org/8233035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/metrics_handler.h
+++ chrome/browser/ui/webui/metrics_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_NTP_METRICS_HANDLER_H_
-#define CHROME_BROWSER_UI_WEBUI_NTP_METRICS_HANDLER_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_METRICS_HANDLER_H_
+#define CHROME_BROWSER_UI_WEBUI_METRICS_HANDLER_H_
 
 #include "content/browser/webui/web_ui.h"
 
@@ -48,4 +48,4 @@ class MetricsHandler : public WebUIMessageHandler {
   DISALLOW_COPY_AND_ASSIGN(MetricsHandler);
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_NTP_METRICS_HANDLER_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_METRICS_HANDLER_H_
