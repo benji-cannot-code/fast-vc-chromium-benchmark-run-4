3860b5f151c84c382e85015874857c9263dcedf4
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-17 11:46:06 +0000

    Web Inspector: Editing CSS selector doesn't fire onResourceContentCommitted https://bugs.webkit.org/show_bug.cgi?id=70018
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    Test: inspector/styles/commit-selector.html
    
    * inspector/front-end/CSSStyleModel.js:
    (WebInspector.CSSStyleModel.prototype.setRuleSelector.callback):
    (WebInspector.CSSStyleModel.prototype.setRuleSelector):
    
    LayoutTests:
    
    * inspector/styles/commit-selector-expected.txt: Added.
    * inspector/styles/commit-selector.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/CSSStyleModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/CSSStyleModel.js
@@ -51,8 +51,18 @@ WebInspector.CSSStyleModel.Events = {
 }
 
 WebInspector.CSSStyleModel.prototype = {
+    /**
+     * @param {DOMAgent.NodeId} nodeId
+     * @param {?Array.<string>|undefined} forcedPseudoClasses
+     * @param {function(?*)} userCallback
+     */
     getStylesAsync: function(nodeId, forcedPseudoClasses, userCallback)
     {
+        /**
+         * @param {function(?*)} userCallback
+         * @param {?Protocol.Error} error
+         * @param {?CSSAgent.CSSNodeStyles} payload
+         */
         function callback(userCallback, error, payload)
         {
             if (error) {
@@ -99,8 +109,17 @@ WebInspector.CSSStyleModel.prototype = {
         CSSAgent.getStylesForNode(nodeId, forcedPseudoClasses || [], callback.bind(null, userCallback));
     },
 
+    /**
+     * @param {DOMAgent.NodeId} nodeId
+     * @param {function(?WebInspector.CSSStyleDeclaration)} userCallback
+     */
     getComputedStyleAsync: function(nodeId, userCallback)
     {
+        /**
+         * @param {function(?WebInspector.CSSStyleDeclaration)} userCallback
+         * @param {?Protocol.Error} error
+         * @param {?CSSAgent.CSSStyle} stylePayload
+         */
         function callback(userCallback, error, stylePayload)
         {
             if (error)
@@ -112,8 +131,17 @@ WebInspector.CSSStyleModel.prototype = {
         CSSAgent.getComputedStyleForNode(nodeId, callback.bind(null, userCallback));
     },
 
+    /**
+     * @param {DOMAgent.NodeId} nodeId
+     * @param {function(?WebInspector.CSSStyleDeclaration)} userCallback
+     */
     getInlineStyleAsync: function(nodeId, userCallback)
     {
+        /**
+         * @param {function(?WebInspector.CSSStyleDeclaration)} userCallback
+         * @param {?Protocol.Error} error
+         * @param {?CSSAgent.CSSStyle} stylePayload
+         */
         function callback(userCallback, error, stylePayload)
         {
             if (error)
@@ -125,8 +153,21 @@ WebInspector.CSSStyleModel.prototype = {
         CSSAgent.getInlineStyleForNode(nodeId, callback.bind(null, userCallback));
     },
 
+    /**
+     * @param {CSSAgent.CSSRuleId} ruleId
+     * @param {DOMAgent.NodeId} nodeId
+     * @param {string} newSelector
+     * @param {function(WebInspector.CSSRule, boolean)} successCallback
+     * @param {function()} failureCallback
+     */
     setRuleSelector: function(ruleId, nodeId, newSelector, successCallback, failureCallback)
     {
+        /**
+         * @param {DOMAgent.NodeId} nodeId
+         * @param {function(WebInspector.CSSRule, boolean)} successCallback
+         * @param {*} rulePayload
+         * @param {?Array.<DOMAgent.NodeId>} selectedNodeIds
+         */
         function checkAffectsCallback(nodeId, successCallback, rulePayload, selectedNodeIds)
         {
             if (!selectedNodeIds)
@@ -137,9 +178,16 @@ WebInspector.CSSStyleModel.prototype = {
             this._fireStyleSheetChanged(rule.id.styleSheetId, true);
         }
 
-        function callback(nodeId, successCallback, failureCallback, error, newSelector, rulePayload)
+        /**
+         * @param {DOMAgent.NodeId} nodeId
+         * @param {function(WebInspector.CSSRule, boolean)} successCallback
+         * @param {function()} failureCallback
+         * @param {?Protocol.Error} error
+         * @param {string} newSelector
+         * @param {*=} rulePayload
+         */
+        function callback(nodeId, successCallback, failureCallback, newSelector, error, rulePayload)
         {
-            // FIXME: looks like rulePayload is always null.
             if (error)
                 failureCallback();
             else {
@@ -154,6 +202,12 @@ WebInspector.CSSStyleModel.prototype = {
         CSSAgent.setRuleSelector(ruleId, newSelector, callback.bind(this, nodeId, successCallback, failureCallback, newSelector));
     },
 
+    /**
+     * @param {DOMAgent.NodeId} nodeId
+     * @param {string} selector
+     * @param {function(WebInspector.CSSRule, boolean)} successCallback
+     * @param {function()} failureCallback
+     */
     addRule: function(nodeId, selector, successCallback, failureCallback)
     {
         function checkAffectsCallback(nodeId, successCallback, rulePayload, selectedNodeIds)
@@ -167,6 +221,13 @@ WebInspector.CSSStyleModel.prototype = {
             this._fireStyleSheetChanged(rule.id.styleSheetId, true);
         }
 
+        /**
+         * @param {function(WebInspector.CSSRule, boolean)} successCallback
+         * @param {function()} failureCallback
+         * @param {string} selector
+         * @param {?Protocol.Error} error
+         * @param {?CSSAgent.CSSRule} rulePayload
+         */
         function callback(successCallback, failureCallback, selector, error, rulePayload)
         {
             if (error) {
