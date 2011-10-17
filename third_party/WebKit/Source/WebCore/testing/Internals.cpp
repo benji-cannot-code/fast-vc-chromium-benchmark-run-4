5363c235a38444dde17ca27b507c964d32887d9a
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-17 06:05:00 +0000

    Expose rangeFromLocationAndLength and locationAndLengthFromRange via internals object https://bugs.webkit.org/show_bug.cgi?id=68330
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    Add rangeFromLocationAndLength, locationFromRange, and lengthFromRange to window.internals.
    
    Test: editing/text-iterator/range-to-from-location-and-length.html
    
    * WebCore.exp.in:
    * testing/Internals.cpp:
    (WebCore::Internals::scrollElementToRect):
    (WebCore::Internals::rangeFromLocationAndLength):
    (WebCore::Internals::locationFromRange):
    (WebCore::Internals::lengthFromRange):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    LayoutTests:
    
    Add a basic test that uses internals.rangeFromLocationAndLength, internals.locationFromRange,
    and internals.lengthFromRange.
    
    * editing/text-iterator/range-to-from-location-and-length-expected.txt: Added.
    * editing/text-iterator/range-to-from-location-and-length.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97587 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.cpp
+++ third_party/WebKit/Source/WebCore/testing/Internals.cpp
@@ -42,18 +42,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntRect.h"
 #include "NodeRenderingContext.h"
 #include "Page.h"
-#if ENABLE(GESTURE_EVENTS)
-#include "PlatformGestureEvent.h"
-#endif
 #include "Range.h"
 #include "RenderObject.h"
 #include "RenderTreeAsText.h"
-#if ENABLE(SMOOTH_SCROLLING)
-#include "ScrollAnimator.h"
-#endif
 #include "Settings.h"
 #include "ShadowContentElement.h"
 #include "ShadowRoot.h"
+#include "TextIterator.h"
+
+#if ENABLE(GESTURE_EVENTS)
+#include "PlatformGestureEvent.h"
+#endif
+
+#if ENABLE(SMOOTH_SCROLLING)
+#include "ScrollAnimator.h"
+#endif
 
 #if ENABLE(INPUT_COLOR)
 #include "ColorChooser.h"
@@ -415,6 +418,16 @@ void Internals::setSuggestedValue(Element* element, const String& value, Excepti
     inputElement->setSuggestedValue(value);
 }
 
+void Internals::scrollElementToRect(Element* element, long x, long y, long w, long h, ExceptionCode& ec)
+{
+    if (!element || !element->document() || !element->document()->view()) {
+        ec = INVALID_ACCESS_ERR;
+        return;
+    }
+    FrameView* frameView = element->document()->view();
+    frameView->scrollElementToRect(element, IntRect(x, y, w, h));
+}
+
 void Internals::paintControlTints(Document* document, ExceptionCode& ec)
 {
     if (!document || !document->view()) {
@@ -426,14 +439,39 @@ void Internals::paintControlTints(Document* document, ExceptionCode& ec)
     frameView->paintControlTints();
 }
 
-void Internals::scrollElementToRect(Element* element, long x, long y, long w, long h, ExceptionCode& ec)
+PassRefPtr<Range> Internals::rangeFromLocationAndLength(Element* scope, int rangeLocation, int rangeLength, ExceptionCode& ec)
 {
-    if (!element || !element->document() || !element->document()->view()) {
+    if (!scope) {
         ec = INVALID_ACCESS_ERR;
-        return;
+        return 0;
     }
-    FrameView* frameView = element->document()->view();
-    frameView->scrollElementToRect(element, IntRect(x, y, w, h));
+
+    return TextIterator::rangeFromLocationAndLength(scope, rangeLocation, rangeLength);
 }
 
+unsigned Internals::locationFromRange(Element* scope, const Range* range, ExceptionCode& ec)
+{
+    if (!scope || !range) {
+        ec = INVALID_ACCESS_ERR;
+        return 0;
+    }
+
+    size_t location = 0;
+    size_t unusedLength = 0;
+    TextIterator::getLocationAndLengthFromRange(scope, range, location, unusedLength);
+    return location;
+}
+
+unsigned Internals::lengthFromRange(Element* scope, const Range* range, ExceptionCode& ec)
+{
+    if (!scope || !range) {
+        ec = INVALID_ACCESS_ERR;
+        return 0;
+    }
+
+    size_t unusedLocation = 0;
+    size_t length = 0;
+    TextIterator::getLocationAndLengthFromRange(scope, range, unusedLocation, length);
+    return length;
+}
 }
