319d536a8ecfd257389d878eca35f48d3c3ceac3
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 20:52:29 +0000

    [Sync] Add assertions in migration tests.
    BUG=100382
    TEST=sync_integration_tests --gtest_filter="*Migrate*"
    
    
    Review URL: http://codereview.chromium.org/8298010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/migration_errors_test.cc
+++ chrome/browser/sync/test/integration/migration_errors_test.cc
@@ -178,7 +178,7 @@ class MigrationTest : public SyncTest {
     }
 
     // Phase 3: Wait for all clients to catch up.
-    AwaitQuiescence();
+    ASSERT_TRUE(AwaitQuiescence());
 
     // Re-enable notifications if we disabled it.
     if (do_test_without_notifications) {
@@ -341,8 +341,9 @@ class MigrationTwoClientTest : public MigrationTest {
 
 // Easiest possible test of migration errors: triggers a server
 // migration on one datatype, then modifies some other datatype.
+// Flaky. crbug.com/100382.
 IN_PROC_BROWSER_TEST_F(MigrationTwoClientTest,
-                       MigratePrefsThenModifyBookmark) {
+                       FLAKY_MigratePrefsThenModifyBookmark) {
   RunTwoClientMigrationTest(MakeList(syncable::PREFERENCES),
                             MODIFY_BOOKMARK);
 }
@@ -424,7 +425,8 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabs) {
   ASSERT_TRUE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 }
 
-IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabsAndMigrate) {
+// Flaky. crbug.com/100382.
+IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, FLAKY_SetSyncTabsAndMigrate) {
   if (!ServerSupportsErrorTriggering()) {
     LOG(WARNING) << "Test skipped in this server environment.";
     return;
