538616c1816b29dd024291ed86d716e94a6bd74a
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 14:15:17 +0000

    Revert 105791 (to see if it fixes browser_tests) - Change default global temporary quota limit to 50% of available space
    BUG=94363
    TEST=test_shell_tests:Quota*
    
    Review URL: http://codereview.chromium.org/7806014
    
    TBR=kinuko@chromium.org
    Review URL: http://codereview.chromium.org/8319004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105800 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_temporary_storage_evictor.h
+++ webkit/quota/quota_temporary_storage_evictor.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/scoped_callback_factory.h"
-#include "base/memory/weak_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "base/timer.h"
 #include "webkit/quota/quota_types.h"
@@ -25,7 +24,6 @@ class MessageLoopProxy;
 namespace quota {
 
 class QuotaEvictionHandler;
-struct QuotaAndUsage;
 
 class QuotaTemporaryStorageEvictor : public base::NonThreadSafe {
  public:
@@ -103,7 +101,10 @@ class QuotaTemporaryStorageEvictor : public base::NonThreadSafe {
   void ConsiderEviction();
   void OnGotUsageAndQuotaForEviction(
       QuotaStatusCode status,
-      const QuotaAndUsage& quota_and_usage);
+      int64 usage,
+      int64 unlimited_usage,
+      int64 quota,
+      int64 available_disk_space);
   void OnGotLRUOrigin(const GURL& origin);
   void OnEvictionComplete(QuotaStatusCode status);
 
@@ -115,7 +116,10 @@ class QuotaTemporaryStorageEvictor : public base::NonThreadSafe {
     repeated_eviction_ = repeated_eviction;
   }
 
+  static const double kUsageRatioToStartEviction;
   static const int kMinAvailableDiskSpaceToStartEvictionNotSpecified;
+  static const int kThresholdOfErrorsToStopEviction;
+  static const base::TimeDelta kHistogramReportInterval;
 
   int64 min_available_disk_space_to_start_eviction_;
 
@@ -135,7 +139,6 @@ class QuotaTemporaryStorageEvictor : public base::NonThreadSafe {
   base::RepeatingTimer<QuotaTemporaryStorageEvictor> histogram_timer_;
 
   base::ScopedCallbackFactory<QuotaTemporaryStorageEvictor> callback_factory_;
-  base::WeakPtrFactory<QuotaTemporaryStorageEvictor> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(QuotaTemporaryStorageEvictor);
 };
