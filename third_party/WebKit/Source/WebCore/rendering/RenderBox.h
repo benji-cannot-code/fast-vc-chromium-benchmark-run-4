a2897b4519869844721330b4cde4ee4ea831c63f
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-17 18:20:38 +0000

    <rdar://problem/10293929> REGRESSION (r97525): iChat transcript has horizontal scroll bar when the conversation includes a wide image https://bugs.webkit.org/show_bug.cgi?id=70204
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    Reverted r97525, which was supposed to fix <http://webkit.org/b/29447>.
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeReplacedLogicalWidthUsing):
    (WebCore::RenderBox::computeReplacedLogicalHeightUsing):
    * rendering/RenderBox.h:
    
    LayoutTests: 
    
    * fast/replaced/table-percent-width-expected.txt: Updated the results to
    show that this test is failing after reverting r97525.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.h
@@ -298,7 +298,6 @@ public:
     virtual LayoutUnit containingBlockLogicalWidthForContent() const;
     LayoutUnit containingBlockLogicalWidthForContentInRegion(RenderRegion*, LayoutUnit offsetFromLogicalTopOfFirstPage) const;
     LayoutUnit perpendicularContainingBlockLogicalHeight() const;
-    LayoutUnit containingBlockReplacedLogicalWidthForContent() const;
     
     virtual void computeLogicalWidth();
     virtual void computeLogicalHeight();
