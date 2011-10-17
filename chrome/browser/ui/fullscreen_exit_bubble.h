d4c93d8921d0036b94743140dd925c008f832d34
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 21:39:16 +0000

    views: Implementation of the new fullscreen bubble.
    Due to the base class changing, this also touches the GTK UI, but the GTK
    changes should be mostly no-ops.
    
    Based on a patch by Yuzhu Shen <yzshen@chromium.org>
    
    BUG=95136, 100266
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=105922
    
    Review URL: http://codereview.chromium.org/8321016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105927 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/fullscreen_exit_bubble.h
+++ chrome/browser/ui/fullscreen_exit_bubble.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/timer.h"
 #include "chrome/browser/command_updater.h"
+#include "chrome/browser/ui/fullscreen_exit_bubble_type.h"
 #include "googleurl/src/gurl.h"
 #include "ui/base/animation/animation_delegate.h"
 #include "ui/gfx/point.h"
@@ -26,7 +27,9 @@ class Rect;
 
 class FullscreenExitBubble : public ui::AnimationDelegate {
  public:
-  explicit FullscreenExitBubble(Browser* browser);
+  explicit FullscreenExitBubble(Browser* browser,
+                                const GURL& url,
+                                FullscreenExitBubbleType bubble_type);
   virtual ~FullscreenExitBubble();
 
  protected:
@@ -34,8 +37,9 @@ class FullscreenExitBubble : public ui::AnimationDelegate {
   static const int kInitialDelayMs;      // Initial time bubble remains onscreen
   static const int kIdleTimeMs;          // Time before mouse idle triggers hide
   static const int kPositionCheckHz;     // How fast to check the mouse position
-  static const int kSlideInRegionHeightPx; // Height of region triggering
-                                           // slide-in
+  static const int kSlideInRegionHeightPx;
+                                         // Height of region triggering
+                                         // slide-in
   static const int kPopupTopPx;          // Space between the popup and the top
                                          // of the screen.
   static const int kSlideInDurationMs;   // Duration of slide-in animation
@@ -65,10 +69,19 @@ class FullscreenExitBubble : public ui::AnimationDelegate {
   void CheckMousePosition();
 
   void StartWatchingMouse();
+  void StopWatchingMouse();
 
   void ToggleFullscreen();
-  void AcceptFullscreen(const GURL& url);
-  void CancelFullscreen();
+  void Accept();
+  void Cancel();
+
+  // The following strings may change according to the content type and URL.
+  string16 GetCurrentMessageText() const;
+  string16 GetCurrentDenyButtonText() const;
+
+  // The following strings never change.
+  string16 GetAllowButtonText() const;
+  string16 GetInstructionText() const;
 
   // The browser this bubble is in.
   Browser* browser_;
@@ -90,6 +103,13 @@ class FullscreenExitBubble : public ui::AnimationDelegate {
   // The most recently seen mouse position, in screen coordinates.  Used to see
   // if the mouse has moved since our last check.
   gfx::Point last_mouse_pos_;
+
+ protected:
+  // The host the bubble is for, can be empty.
+  GURL url_;
+
+  // The type of the bubble; controls e.g. which buttons to show.
+  FullscreenExitBubbleType bubble_type_;
 };
 
 #endif  // CHROME_BROWSER_UI_FULLSCREEN_EXIT_BUBBLE_H_
