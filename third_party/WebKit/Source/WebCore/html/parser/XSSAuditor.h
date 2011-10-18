b7d6b7ccb2289c51d8e88234dec788f5dc5b448f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 04:37:22 +0000

    XSSAuditor bypass with remote script ending in ? character https://bugs.webkit.org/show_bug.cgi?id=70255
    Patch by Tom Sepez <tsepez@chromium.org> on 2011-10-17
    Reviewed by Daniel Bates.
    
    Fix XSSAuditor bypass where unterminated src="" attribute could pick up
    text from page causing failed XSS detection.  Constrain match to domain
    portions of src attribute only.
    
    Source/WebCore:
    
    Test: http/tests/security/xssAuditor/script-tag-with-source-unterminated.html
    
    * html/parser/XSSAuditor.cpp:
    (WebCore::XSSAuditor::filterScriptToken):
    (WebCore::XSSAuditor::filterObjectToken):
    (WebCore::XSSAuditor::filterParamToken):
    (WebCore::XSSAuditor::filterEmbedToken):
    (WebCore::XSSAuditor::filterAppletToken):
    (WebCore::XSSAuditor::filterIframeToken):
    (WebCore::XSSAuditor::eraseAttributeIfInjected):
    (WebCore::XSSAuditor::decodedSnippetForAttribute):
    * html/parser/XSSAuditor.h:
    
    LayoutTests:
    
    * http/tests/security/xssAuditor/script-tag-with-source-unterminated-expected.txt: Added.
    * http/tests/security/xssAuditor/script-tag-with-source-unterminated.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
+++ third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
@@ -49,6 +49,11 @@ private:
         AfterScriptStartTag,
     };
 
+    enum AttributeKind {
+        NormalAttribute,
+        SrcLikeAttribute
+    };
+
     void init();
 
     bool filterTokenInitial(HTMLToken&);
@@ -65,11 +70,11 @@ private:
     bool filterFormToken(HTMLToken&);
 
     bool eraseDangerousAttributesIfInjected(HTMLToken&);
-    bool eraseAttributeIfInjected(HTMLToken&, const QualifiedName&, const String& replacementValue = String());
+    bool eraseAttributeIfInjected(HTMLToken&, const QualifiedName&, const String& replacementValue = String(), AttributeKind treatment = NormalAttribute);
 
     String snippetForRange(const HTMLToken&, int start, int end);
     String snippetForJavaScript(const String&);
-    String decodedSnippetForAttribute(const HTMLToken&, const HTMLToken::Attribute&);
+    String decodedSnippetForAttribute(const HTMLToken&, const HTMLToken::Attribute&, AttributeKind treatment = NormalAttribute);
 
     bool isContainedInRequest(const String&);
     bool isSameOriginResource(const String& url);
