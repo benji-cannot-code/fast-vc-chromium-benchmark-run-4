4b1e06c7febe4c795f766785077d2e1580778f13
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 11:52:37 +0000

    [EFL][WK2] Fix compilation error by include WebErrors.h file. https://bugs.webkit.org/show_bug.cgi?id=70309
    Unreviewed build fix
    
    Added a header file to fix compilation error.
    
    Patch by Tomasz Morawski <t.morawski@samsung.com> on 2011-10-18
    
    * WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebErrors.h"
 
 #include <WebCore/NotImplemented.h>
+#include <WebCore/ResourceError.h>
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/ResourceResponse.h>
 
