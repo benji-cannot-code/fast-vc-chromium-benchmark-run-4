ae860a2d3bc6504351e3fb47c57b736f0839cb29
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 02:12:40 +0000

    Revert 105998 - views: Change TextButtonBase API to string16.
    BUG=68267
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8262003
    
    TBR=tfarina@chromium.org
    Review URL: http://codereview.chromium.org/8329010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/input_method_menu_button.cc
+++ chrome/browser/chromeos/status/input_method_menu_button.cc
@@ -121,7 +121,7 @@ void InputMethodMenuButton::UpdateUI(const std::string& input_method_id,
       input_method::InputMethodUtil::IsKeyboardLayout(input_method_id) &&
       host_->GetScreenMode() == StatusAreaHost::kBrowserMode;
   SetVisible(!hide_button);
-  SetText(name);
+  SetText(UTF16ToWideHack(name));
   SetTooltipText(tooltip);
   SetAccessibleName(tooltip);
 
--- chrome/browser/chromeos/views/dropdown_button.cc
+++ chrome/browser/chromeos/views/dropdown_button.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/views/dropdown_button.h"
 
+#include "base/utf_string_conversions.h"
 #include "grit/theme_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas_skia.h"
@@ -78,8 +79,8 @@ void DropDownButton::OnPaintFocusBorder(gfx::Canvas* canvas) {
                           height() - kFocusFrameBottomOffset);
 }
 
-void DropDownButton::SetText(const string16& text) {
-  text_ = text;
+void DropDownButton::SetText(const std::wstring& text) {
+  text_ = WideToUTF16Hack(text);
   UpdateTextSize();
 }
 
