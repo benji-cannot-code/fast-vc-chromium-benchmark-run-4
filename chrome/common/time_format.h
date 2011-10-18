630dbe86244f752ac2c586e6bc0ddf3da343c537
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 18:12:38 +0000

    Wrap TimeFormat unittests into unnamed namespace.
    Plus some other clean ups around.
    
    R=phajdan.jr@chromium.org
    
    Review URL: http://codereview.chromium.org/8313003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/time_format.h
+++ chrome/common/time_format.h
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_COMMON_TIME_FORMAT_H__
-#define CHROME_COMMON_TIME_FORMAT_H__
+#ifndef CHROME_COMMON_TIME_FORMAT_H_
+#define CHROME_COMMON_TIME_FORMAT_H_
 #pragma once
 
-// This file defines methods to format time values as strings.
-
+#include "base/basictypes.h"
 #include "base/string16.h"
 
 namespace base {
@@ -16,6 +15,7 @@ class Time;
 class TimeDelta;
 }
 
+// Methods to format time values as strings.
 class TimeFormat {
  public:
   // TimeElapsed, TimeRemaining and TimeRemainingShort functions:
@@ -47,6 +47,9 @@ class TimeFormat {
   // time once at the beginning and pass it for each computation.
   static string16 RelativeDate(const base::Time& time,
                                const base::Time* optional_midnight_today);
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(TimeFormat);
 };
 
-#endif  // CHROME_COMMON_TIME_FORMAT_H__
+#endif  // CHROME_COMMON_TIME_FORMAT_H_
