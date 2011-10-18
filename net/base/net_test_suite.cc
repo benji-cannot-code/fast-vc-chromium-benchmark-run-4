2568e251d16bd38fde015948f3f8c5bf9f152f94
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 20:03:08 +0000

    Reset static HttpStreamFactory settings before each test in a NetTestSuite.
    This improves test isolation. 
    
    BUG=
    TEST=./net_unittests --gtest_shuffle=1 --gtest_random_seed=56044
    More specifically:
    TEST=./net_unittests --gtest_shuffle=1 --gtest_random_seed=56044 --gtest_filter=Spdy/SpdyNetworkTransactionTest.ThreeGets/1:HttpStreamFactoryTest.PreconnectHttpProxy
    
    
    Review URL: http://codereview.chromium.org/8295009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106106 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_test_suite.cc
+++ net/base/net_test_suite.cc
@@ -6,9 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_test_suite.h"
 
 #include "base/message_loop.h"
+#include "net/http/http_stream_factory.h"
 #if defined(USE_NSS)
 #include "net/ocsp/nss_ocsp.h"
 #endif
+#include "testing/gtest/include/gtest/gtest.h"
+
+class StaticReset : public ::testing::EmptyTestEventListener {
+  virtual void OnTestStart(const ::testing::TestInfo& test_info) OVERRIDE {
+    net::HttpStreamFactory::ResetStaticSettingsToInit();
+  }
+};
 
 NetTestSuite::NetTestSuite(int argc, char** argv)
     : TestSuite(argc, argv) {
@@ -18,6 +26,7 @@ NetTestSuite::~NetTestSuite() {}
 
 void NetTestSuite::Initialize() {
   TestSuite::Initialize();
+  ::testing::UnitTest::GetInstance()->listeners().Append(new StaticReset());
   InitializeTestThread();
 }
 
