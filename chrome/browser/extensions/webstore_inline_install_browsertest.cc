52e1553d6914ab5c25864325ce878206f51f53b8
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 22:42:39 +0000

    Revert 106140 - Make inline installation callbacks fire when the item is downloaded and installed.
    By having WebstoreInlineInstaller implement WebstoreInstaller::Delegate, we can
    respond to the callback when the item is actually installed, instead of as soon
    as the download is initiated.
    
    R=jstritar@chromium.org
    
    Review URL: http://codereview.chromium.org/8348006
    
    TBR=mihaip@chromium.org
    
    Logs:
    http://build.chromium.org/p/chromium/builders/Mac10.6%20Tests%20%281%29/builds/13843/steps/browser_tests/logs/stdio
    http://build.chromium.org/p/chromium/builders/Vista%20Tests%20%283%29/builds/6860/steps/browser_tests/logs/stdio
    Review URL: http://codereview.chromium.org/8345024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106154 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/webstore_inline_install_browsertest.cc
+++ chrome/browser/extensions/webstore_inline_install_browsertest.cc
@@ -82,14 +82,20 @@ class WebstoreInlineInstallTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(WebstoreInlineInstallTest, Install) {
   SetExtensionInstallDialogForManifestAutoConfirmForTests(true);
 
+  ui_test_utils::WindowedNotificationObserver load_signal(
+        chrome::NOTIFICATION_EXTENSION_LOADED,
+        Source<Profile>(browser()->profile()));
+
   ui_test_utils::NavigateToURL(
       browser(), GenerateTestServerUrl(kAppDomain, "install.html"));
 
   RunInlineInstallTest("runTest");
 
+  load_signal.Wait();
+
   const Extension* extension = browser()->profile()->GetExtensionService()->
       GetExtensionById("ecglahbcnmdpdciemllbhojghbkagdje", false);
-  EXPECT_TRUE(extension);
+  EXPECT_TRUE(extension != NULL);
 }
 
 IN_PROC_BROWSER_TEST_F(
@@ -97,7 +103,7 @@ IN_PROC_BROWSER_TEST_F(
   SetExtensionInstallDialogForManifestAutoConfirmForTests(false);
   ui_test_utils::NavigateToURL(
       browser(),
-      GenerateTestServerUrl(kNonAppDomain, "install_non_verified_domain.html"));
+      GenerateTestServerUrl(kNonAppDomain, "install-non-verified-domain.html"));
 
   RunInlineInstallTest("runTest1");
   RunInlineInstallTest("runTest2");
@@ -122,7 +128,7 @@ IN_PROC_BROWSER_TEST_F(WebstoreInlineInstallTest, InstallNotSupported) {
   SetExtensionInstallDialogForManifestAutoConfirmForTests(false);
   ui_test_utils::NavigateToURL(
       browser(),
-      GenerateTestServerUrl(kAppDomain, "install_not_supported.html"));
+      GenerateTestServerUrl(kAppDomain, "install-not-supported.html"));
 
   RunInlineInstallTest("runTest");
 
@@ -133,28 +139,4 @@ IN_PROC_BROWSER_TEST_F(WebstoreInlineInstallTest, InstallNotSupported) {
   }
   TabContents* tab_contents = browser()->GetSelectedTabContents();
   EXPECT_EQ(GURL("http://cws.com/show-me-the-money"), tab_contents->GetURL());
-}
-
-// The unpack failure test needs to use a different install .crx, which is
-// specified via a command-line flag, so it needs its own test subclass.
-class WebstoreInlineInstallUnpackFailureTest
-    : public WebstoreInlineInstallTest {
- public:
-  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
-    WebstoreInlineInstallTest::SetUpCommandLine(command_line);
-
-    GURL crx_url = GenerateTestServerUrl(
-        kWebstoreDomain, "malformed_extension.crx");
-    CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-        switches::kAppsGalleryUpdateURL, crx_url.spec());
-  }
-};
-
-IN_PROC_BROWSER_TEST_F(WebstoreInlineInstallUnpackFailureTest, Test) {
-  SetExtensionInstallDialogForManifestAutoConfirmForTests(true);
-
-  ui_test_utils::NavigateToURL(browser(),
-      GenerateTestServerUrl(kAppDomain, "install_unpack_failure.html"));
-
-  RunInlineInstallTest("runTest");
 }
