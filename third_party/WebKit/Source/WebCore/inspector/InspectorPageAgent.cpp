d2002c8984f8b94ba504a8afca98ebddc50f1137
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 02:19:36 +0000

    Web Inspector: Search in resource crashes when there is no resource with given url. https://bugs.webkit.org/show_bug.cgi?id=69767
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Test: http/tests/inspector/search/search-in-non-existing-resource.html
    
    * inspector/InspectorPageAgent.cpp:
    (WebCore::InspectorPageAgent::searchInResource):
    
    LayoutTests:
    
    * http/tests/inspector/search/search-in-non-existing-resource-expected.txt: Added.
    * http/tests/inspector/search/search-in-non-existing-resource.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -504,7 +504,8 @@ void InspectorPageAgent::searchInResource(ErrorString*, const String& frameId, c
 
     if (!success) {
         CachedResource* resource = cachedResource(frame, kurl);
-        success = textContentForCachedResource(resource, &content);
+        if (resource)
+            success = textContentForCachedResource(resource, &content);
     }
 
     if (!success)
