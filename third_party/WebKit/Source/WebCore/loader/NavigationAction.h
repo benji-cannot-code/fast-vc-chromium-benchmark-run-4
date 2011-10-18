071b1503c12eae235c77c41e054728797ea967d5
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 04:47:05 +0000

    Source/WebCore: Make NavigationAction wrap a ResourceRequest instead of a KURL. https://bugs.webkit.org/show_bug.cgi?id=68803
    With this, FrameLoaderClient::dispatchCreatePage knows about the
    request that will be used for the initial navigaion of the to be
    created page.
    
    Reviewed by Nate Chapin.
    
    Test: platform/chromium/fast/loader/create-view-target-blank.html
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::loadURL):
    (WebCore::FrameLoader::load):
    (WebCore::FrameLoader::loadWithDocumentLoader):
    (WebCore::FrameLoader::reload):
    (WebCore::FrameLoader::loadPostRequest):
    (WebCore::FrameLoader::continueLoadAfterNewWindowPolicy):
    (WebCore::FrameLoader::loadDifferentDocumentItem):
    (WebCore::createWindow):
    * loader/NavigationAction.cpp:
    (WebCore::NavigationAction::NavigationAction):
    * loader/NavigationAction.h:
    (WebCore::NavigationAction::isEmpty):
    (WebCore::NavigationAction::url):
    (WebCore::NavigationAction::resourceRequest):
    * loader/PolicyChecker.cpp:
    (WebCore::PolicyChecker::checkNavigationPolicy):
    * page/ContextMenuController.cpp:
    (WebCore::openNewWindow):
    * WebCore.exp: updated
    
    Source/WebKit/chromium: Make NavigationAction wrap a ResourceRequest instead of a KURL.
    https://bugs.webkit.org/show_bug.cgi?id=68803
    
    Use the request in the NavigationAction, if the FrameLoadRequest
    doesn't already contain a request.
    
    Reviewed by Nate Chapin.
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::createWindow):
    
    Tools: [chromium] add LayoutTestController.dumpCreateView() to monitor calls to WebViewClient::createView
    https://bugs.webkit.org/show_bug.cgi?id=68803
    
    Reviewed by Nate Chapin.
    
    * DumpRenderTree/chromium/LayoutTestController.cpp:
    (LayoutTestController::LayoutTestController):
    (LayoutTestController::dumpCreateView):
    * DumpRenderTree/chromium/LayoutTestController.h:
    (LayoutTestController::shouldDumpCreateView):
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::createView):
    
    LayoutTests: Verify that the URL to be opened in the new window is passed by dispatchCreateWindow down to createView.
    https://bugs.webkit.org/show_bug.cgi?id=68803
    
    Reviewed by Nate Chapin.
    
    * platform/chromium/fast/loader/create-view-target-blank-expected.txt: Added.
    * platform/chromium/fast/loader/create-view-target-blank.html: Added.
    * platform/chromium/fast/loader/resources/done.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97716 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/NavigationAction.h
+++ third_party/WebKit/Source/WebCore/loader/NavigationAction.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Event.h"
 #include "FrameLoaderTypes.h"
 #include "KURL.h"
+#include "ResourceRequest.h"
 #include <wtf/Forward.h>
 
 namespace WebCore {
@@ -40,19 +41,22 @@ namespace WebCore {
     class NavigationAction {
     public:
         NavigationAction();
-        NavigationAction(const KURL&, NavigationType);
-        NavigationAction(const KURL&, FrameLoadType, bool isFormSubmission);
-        NavigationAction(const KURL&, NavigationType, PassRefPtr<Event>);
-        NavigationAction(const KURL&, FrameLoadType, bool isFormSubmission, PassRefPtr<Event>);
+        NavigationAction(const ResourceRequest&);
+        NavigationAction(const ResourceRequest&, NavigationType);
+        NavigationAction(const ResourceRequest&, FrameLoadType, bool isFormSubmission);
+        NavigationAction(const ResourceRequest&, NavigationType, PassRefPtr<Event>);
+        NavigationAction(const ResourceRequest&, FrameLoadType, bool isFormSubmission, PassRefPtr<Event>);
 
-        bool isEmpty() const { return m_URL.isEmpty(); }
+        bool isEmpty() const { return m_resourceRequest.url().isEmpty(); }
 
-        KURL url() const { return m_URL; }
+        KURL url() const { return m_resourceRequest.url(); }
+        const ResourceRequest& resourceRequest() const { return m_resourceRequest; }
+
         NavigationType type() const { return m_type; }
         const Event* event() const { return m_event.get(); }
 
     private:
-        KURL m_URL;
+        ResourceRequest m_resourceRequest;
         NavigationType m_type;
         RefPtr<Event> m_event;
     };
--- third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.cpp
@@ -113,6 +113,7 @@ LayoutTestController::LayoutTestController(TestShell* shell)
     bindMethod("dumpStatusCallbacks", &LayoutTestController::dumpWindowStatusChanges);
     bindMethod("dumpTitleChanges", &LayoutTestController::dumpTitleChanges);
     bindMethod("dumpPermissionClientCallbacks", &LayoutTestController::dumpPermissionClientCallbacks);
+    bindMethod("dumpCreateView", &LayoutTestController::dumpCreateView);
     bindMethod("elementDoesAutoCompleteForElementWithId", &LayoutTestController::elementDoesAutoCompleteForElementWithId);
     bindMethod("evaluateInWebInspector", &LayoutTestController::evaluateInWebInspector);
     bindMethod("evaluateScriptInIsolatedWorld", &LayoutTestController::evaluateScriptInIsolatedWorld);
@@ -382,6 +383,12 @@ void LayoutTestController::dumpTitleChanges(const CppArgumentList&, CppVariant* 
 void LayoutTestController::dumpPermissionClientCallbacks(const CppArgumentList&, CppVariant* result)
 {
     m_dumpPermissionClientCallbacks = true;
+    result->setNull();
+}
+
+void LayoutTestController::dumpCreateView(const CppArgumentList&, CppVariant* result)
+{
+    m_dumpCreateView = true;
     result->setNull();
 }
 
