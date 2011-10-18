a31eed2cdf1cbf3b9f05fdfd79d5130831748f47
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 16:59:44 +0000

    [Qt] Merge QML plugin for WebKit2 with the original WebKit1 plugin
    .: 
    
    We don't need a sepearate plugin, as we no longer expose the WK2
    APIs under QtWebKit.experimental, but now use QtWebKit. The version
    was also changed to 3.0 instead of 5.0, as the original plugin was
    versioned 1.0, and bumping to 3.0 fits the scheme used by QtWebKit releases.
    
    Patch by Tor Arne Vestbø <tor.arne.vestbo@nokia.com> on 2011-10-18
    Reviewed by Andreas Kling.
    
    * Source/WebKit.pro:
    
    Source/WebKit/qt: 
    
    We don't need a separate plugin, as we no longer expose the WK2
    APIs under QtWebKit.experimental, but now use QtWebKit. The version
    was also changed to 3.0 instead of 5.0, as the original plugin was
    versioned 1.0, and bumping to 3.0 fits the scheme used by QtWebKit releases.
    
    Patch by Tor Arne Vestbø <tor.arne.vestbo@nokia.com> on 2011-10-18
    Reviewed by Andreas Kling.
    
    * declarative/declarative.pro:
    * declarative/plugin.cpp:
    (WebKitQmlPlugin::registerTypes):
    
    Source/WebKit2: 
    
    We don't need a separate plugin, as we no longer expose the WK2
    APIs under QtWebKit.experimental, but now use QtWebKit. The version
    was also changed to 3.0 instead of 5.0, as the original plugin was
    versioned 1.0, and bumping to 3.0 fits the scheme used by QtWebKit releases.
    
    Patch by Tor Arne Vestbø <tor.arne.vestbo@nokia.com> on 2011-10-18
    Reviewed by Andreas Kling.
    
    * UIProcess/API/qt/qmlplugin/plugin.cpp: Removed.
    * UIProcess/API/qt/qmlplugin/qmldir: Removed.
    * UIProcess/API/qt/qmlplugin/qmlplugin.pro: Removed.
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_linkHovered.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_loadFail.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_loadProgress.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_loadProgressSignal.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_navigationPolicyForUrl.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_preferences.qml:
    * UIProcess/API/qt/tests/qmltests/DesktopWebView/tst_properties.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_load.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadFail.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadNegativeSizeView.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadProgress.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadProgressSignal.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadZeroSizeView.qml:
    * UIProcess/API/qt/tests/qmltests/TouchWebView/tst_properties.qml:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/declarative/plugin.cpp
+++ third_party/WebKit/Source/WebKit/qt/declarative/plugin.cpp
@@ -23,6 +23,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtDeclarative/qdeclarative.h>
 #include <QtDeclarative/qdeclarativeextensionplugin.h>
 
+#if defined(HAVE_WEBKIT2)
+#include "qdesktopwebview.h"
+#include "qtouchwebpage.h"
+#include "qtouchwebview.h"
+#include "qwebnavigationcontroller.h"
+#include "qwebpreferences.h"
+
+#include <QtNetwork/qnetworkreply.h>
+#endif
+
 QT_BEGIN_NAMESPACE
 
 class WebKitQmlPlugin : public QDeclarativeExtensionPlugin {
@@ -37,6 +47,15 @@ public:
         qmlRegisterType<QDeclarativeWebView>(uri, 1, 1, "WebView");
         qmlRegisterRevision<QDeclarativeWebView, 0>("QtWebKit", 1, 0);
         qmlRegisterRevision<QDeclarativeWebView, 1>("QtWebKit", 1, 1);
+#endif
+
+#if defined(HAVE_WEBKIT2)
+        qmlRegisterType<QDesktopWebView>(uri, 3, 0, "DesktopWebView");
+        qmlRegisterType<QTouchWebView>(uri, 3, 0, "TouchWebView");
+        qmlRegisterUncreatableType<QWebNavigationController>(uri, 3, 0, "NavigationController", QObject::tr("Cannot create separate instance of NavigationController"));
+        qmlRegisterUncreatableType<QWebPreferences>(uri, 3, 0, "WebPreferences", QObject::tr("Cannot create separate instance of WebPreferences"));
+        qmlRegisterUncreatableType<QTouchWebPage>(uri, 3, 0, "TouchWebPage", QObject::tr("Cannot create separate instance of TouchWebPage, use TouchWebView"));
+        qmlRegisterUncreatableType<QNetworkReply>(uri, 3, 0, "NetworkReply", QObject::tr("Cannot create separate instance of NetworkReply"));
 #endif
     }
 };
--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadProgress.qml
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/qmltests/TouchWebView/tst_loadProgress.qml
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import QtQuick 2.0
 import QtTest 1.0
-import QtWebKit.experimental 5.0
+import QtWebKit 3.0
 
 TouchWebView {
     id: webView
