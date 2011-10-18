5eaffbf26daa8a5d3dee540e6165d02ee5e5ed90
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 02:32:13 +0000

    Implement the final UI for the CAPS LOCK indicator.
    - Remove the tooltip
    - Insted, add a drop-down menu which looks like http://crosbug.com/11690#c18
    - Split the string resource for the indicator into two since gfx canvas can not handle a string containing "\n" correctly.
    - The bubble in the UI mock is NOT implemented yet.
    
    BUG=chromium-os:11690
    TEST=ran browser_tests
    
    
    Review URL: http://codereview.chromium.org/8249014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/caps_lock_menu_button.h
+++ chrome/browser/chromeos/status/caps_lock_menu_button.h
@@ -13,7 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/system_key_event_listener.h"
 #include "chrome/browser/prefs/pref_member.h"
 #include "content/common/notification_observer.h"
+#include "views/controls/menu/menu_delegate.h"
 #include "views/controls/menu/view_menu_delegate.h"
+
+namespace views {
+class MenuRunner;
+}
 
 namespace chromeos {
 
@@ -23,6 +28,7 @@ class StatusAreaHost;
 // lock is active.
 class CapsLockMenuButton : public NotificationObserver,
                            public StatusAreaButton,
+                           public views::MenuDelegate,
                            public views::ViewMenuDelegate,
                            public SystemKeyEventListener::CapsLockObserver {
  public:
@@ -31,6 +37,10 @@ class CapsLockMenuButton : public NotificationObserver,
 
   // views::View implementation.
   virtual void OnLocaleChanged();
+
+  // views::MenuDelegate implementation.
+  virtual string16 GetLabel(int id) const;
+  virtual bool IsCommandEnabled(int id) const;
 
   // views::ViewMenuDelegate implementation.
   virtual void RunMenu(views::View* unused_source, const gfx::Point& pt);
@@ -43,14 +53,26 @@ class CapsLockMenuButton : public NotificationObserver,
                        const NotificationSource& source,
                        const NotificationDetails& details);
 
-  // Updates the tooltip text and the accessible name.
-  void UpdateTooltip();
+  // Updates the accessible name.
+  void UpdateAccessibleName();
+
+  // Gets the text for the drop-down menu.
+  string16 GetText() const;
+
   // Updates the UI from the current state.
   void UpdateUIFromCurrentCapsLock(bool enabled);
 
  private:
+  class StatusView;
+
   PrefService* prefs_;
   IntegerPrefMember remap_search_key_to_;
+
+  // The currently showing status view. NULL if menu is not being displayed.
+  StatusView* status_;
+
+  // If non-null the menu is showing.
+  scoped_ptr<views::MenuRunner> menu_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(CapsLockMenuButton);
 };
