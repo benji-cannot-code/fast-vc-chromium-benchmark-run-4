8f909127095d866d5f073d7f04402dbc25f9f6d1
kdecker@apple.com <kdecker@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 20:50:34 +0000

    2011-10-18  Damian Kaleta  <dkaleta@apple.com>
            Add WebKit API to hide the definition window
            https://bugs.webkit.org/show_bug.cgi?id=70345
    
            Reviewed by Anders Carlsson.
    
            * UIProcess/API/mac/WKView.mm:
            (-[WKView hideWordDefinitionWindow]): Hides the definition window.
            * UIProcess/API/mac/WKViewPrivate.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKViewPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKViewPrivate.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (BOOL)frameSizeUpdatesDisabled;
 
 - (void)performDictionaryLookupAtCurrentMouseLocation;
+- (void)hideWordDefinitionWindow;
 
 - (NSInteger)spellCheckerDocumentTag;
 - (void)handleCorrectionPanelResult:(NSString*)result;
