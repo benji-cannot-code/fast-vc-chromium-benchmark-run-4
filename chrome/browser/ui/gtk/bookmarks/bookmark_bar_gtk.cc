48b98d0a8a0e44d5b451f11397911ddaeca3f90e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 20:02:57 +0000

    gtk: fix bulge in custom frame
    problem: when there is no Other Bookmarks button showing (because the folder is empty), you can see a small beige bulge in the custom frame at the level of the bookmark bar
    cause: the other_padding GtkAlignment is still showing although the button itself is hidden (I'm not sure why this alignment extends outside the bounds of the bookmark bar, but it does)
    solution: show/hide that widget instead of the button widget.
    
    problem 2: the Other Bookmarks button is not shown/hidden when elements are added to or removed from it.
    solution: call UpdateOtherBookmarksVisibility more aggressively
    
    BUG=100724
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/8336011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106105 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/bookmarks/bookmark_bar_gtk.cc
+++ chrome/browser/ui/gtk/bookmarks/bookmark_bar_gtk.cc
@@ -256,16 +256,18 @@ void BookmarkBarGtk::Init() {
   // We pack the button manually (rather than using gtk_button_set_*) so that
   // we can have finer control over its label.
   other_bookmarks_button_ = theme_service_->BuildChromeButton();
+  gtk_widget_show_all(other_bookmarks_button_);
   ConnectFolderButtonEvents(other_bookmarks_button_, false);
-  GtkWidget* other_padding = gtk_alignment_new(0, 0, 1, 1);
-  gtk_alignment_set_padding(GTK_ALIGNMENT(other_padding),
+  other_padding_ = gtk_alignment_new(0, 0, 1, 1);
+  gtk_alignment_set_padding(GTK_ALIGNMENT(other_padding_),
                             kOtherBookmarksPaddingVertical,
                             kOtherBookmarksPaddingVertical,
                             kOtherBookmarksPaddingHorizontal,
                             kOtherBookmarksPaddingHorizontal);
-  gtk_container_add(GTK_CONTAINER(other_padding), other_bookmarks_button_);
-  gtk_box_pack_start(GTK_BOX(bookmark_hbox_), other_padding,
+  gtk_container_add(GTK_CONTAINER(other_padding_), other_bookmarks_button_);
+  gtk_box_pack_start(GTK_BOX(bookmark_hbox_), other_padding_,
                      FALSE, FALSE, 0);
+  gtk_widget_set_no_show_all(other_padding_, TRUE);
 
   sync_error_button_ = theme_service_->BuildChromeButton();
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
@@ -528,10 +530,8 @@ void BookmarkBarGtk::SetChevronState() {
 
 void BookmarkBarGtk::UpdateOtherBookmarksVisibility() {
   bool has_other_children = !model_->other_node()->empty();
-  if (has_other_children == gtk_widget_get_visible(other_bookmarks_button_))
-    return;
 
-  gtk_widget_set_visible(other_bookmarks_button_, has_other_children);
+  gtk_widget_set_visible(other_padding_, has_other_children);
   gtk_widget_set_visible(other_bookmarks_separator_, has_other_children);
 }
 
@@ -881,6 +881,8 @@ void BookmarkBarGtk::BookmarkNodeMoved(BookmarkModel* model,
 void BookmarkBarGtk::BookmarkNodeAdded(BookmarkModel* model,
                                        const BookmarkNode* parent,
                                        int index) {
+  UpdateOtherBookmarksVisibility();
+
   const BookmarkNode* node = parent->GetChild(index);
   if (parent != model_->bookmark_bar_node()) {
     StartThrobbing(node);
@@ -894,7 +896,6 @@ void BookmarkBarGtk::BookmarkNodeAdded(BookmarkModel* model,
   if (node->is_folder())
     menu_bar_helper_.Add(gtk_bin_get_child(GTK_BIN(item)));
 
-  UpdateOtherBookmarksVisibility();
   SetInstructionState();
   SetChevronState();
 
@@ -905,6 +906,8 @@ void BookmarkBarGtk::BookmarkNodeRemoved(BookmarkModel* model,
                                          const BookmarkNode* parent,
                                          int old_index,
                                          const BookmarkNode* node) {
+  UpdateOtherBookmarksVisibility();
+
   if (parent != model_->bookmark_bar_node()) {
     // We only care about nodes on the bookmark bar.
     return;
@@ -918,7 +921,6 @@ void BookmarkBarGtk::BookmarkNodeRemoved(BookmarkModel* model,
   gtk_container_remove(GTK_CONTAINER(bookmark_toolbar_.get()),
                        to_remove);
 
-  UpdateOtherBookmarksVisibility();
   SetInstructionState();
   SetChevronState();
 }
