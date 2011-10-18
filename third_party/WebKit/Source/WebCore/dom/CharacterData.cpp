108bb1ea557d8125850157aacb77f7e16bf2455a
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 23:06:48 +0000

    [MutationObservers] Implement WebKitMutationObserver.observe for characterData changes https://bugs.webkit.org/show_bug.cgi?id=68957
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Test: fast/mutation/observe-characterdata.html
    
    * dom/CharacterData.cpp:
    (WebCore::CharacterData::dispatchModifiedEvent):
    
    LayoutTests:
    
    * fast/mutation/observe-characterdata-expected.txt: Added.
    * fast/mutation/observe-characterdata.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/CharacterData.cpp
+++ third_party/WebKit/Source/WebCore/dom/CharacterData.cpp
@@ -28,9 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExceptionCode.h"
 #include "InspectorInstrumentation.h"
 #include "MutationEvent.h"
+#include "MutationRecord.h"
 #include "NodeRenderingContext.h"
 #include "RenderText.h"
 #include "TextBreakIterator.h"
+#include "WebKitMutationObserver.h"
 
 using namespace std;
 
@@ -191,6 +193,15 @@ void CharacterData::updateRenderer(unsigned offsetOfReplacedData, unsigned lengt
 
 void CharacterData::dispatchModifiedEvent(StringImpl* oldData)
 {
+#if ENABLE(MUTATION_OBSERVERS)
+    Vector<WebKitMutationObserver*> observers;
+    registeredMutationObserversOfType(observers, WebKitMutationObserver::CharacterData);
+    if (!observers.isEmpty()) {
+        RefPtr<MutationRecord> mutation = MutationRecord::createCharacterData(this);
+        for (size_t i = 0; i < observers.size(); ++i)
+            observers[i]->enqueueMutationRecord(mutation);
+    }
+#endif
     if (parentNode())
         parentNode()->childrenChanged();
     if (document()->hasListenerType(Document::DOMCHARACTERDATAMODIFIED_LISTENER))
