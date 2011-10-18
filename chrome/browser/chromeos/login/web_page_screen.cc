8dca4ff4815772594a9196d7fb7255e1cd7f0ee2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 16:35:08 +0000

    Move navigation_types.h from content/common to content/public/browser, and put it in the content namespace. There was no reason for it to be in common since it's only used by browser. I also updated the enum names to match the rest of the enums in the Content API.
    BUG=98716
    TBR=joi
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=106029
    Review URL: http://codereview.chromium.org/8339006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/web_page_screen.cc
+++ chrome/browser/chromeos/login/web_page_screen.cc
@@ -31,7 +31,7 @@ bool WebPageScreen::IsPopup(TabContents* source) {
 
 bool WebPageScreen::ShouldAddNavigationToHistory(
     const history::HistoryAddPageArgs& add_page_args,
-    NavigationType::Type navigation_type) {
+    content::NavigationType navigation_type) {
   return false;
 }
 
