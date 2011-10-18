5a5a26bfe571b2a9665be06ef8d445a1577d6019
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-18 15:05:52 +0000

    MediaList: Remove inheritance from StyleBase. https://bugs.webkit.org/show_bug.cgi?id=70203
    Reviewed by Darin Adler.
    
    * css/MediaList.h:
    (WebCore::MediaList::parentStyleSheet):
    (WebCore::MediaList::setParentStyleSheet):
    * css/MediaList.cpp:
    (WebCore::MediaList::MediaList):
    (WebCore::MediaList::notifyChanged):
    
        Change MediaList to no longer inherit from StyleBase. The parent()/setParent()
        mechanism was replaced by explicit parentStyleSheet()/setParentStyleSheet().
    
    * css/CSSImportRule.cpp:
    (WebCore::CSSImportRule::CSSImportRule):
    (WebCore::CSSImportRule::~CSSImportRule):
    * css/CSSMediaRule.cpp:
    (WebCore::CSSMediaRule::CSSMediaRule):
    (WebCore::CSSMediaRule::~CSSMediaRule):
    
        Change StyleBase::setParent() calls to MediaList::setParentStyleSheet().
        CSSRules are never moved between stylesheets, so the parent style sheet pointer
        is valid for the MediaList's lifetime.
    
    * css/StyleSheet.cpp:
    (WebCore::StyleSheet::~StyleSheet):
    
        Change StyleBase::setParent() calls to MediaList::setParentStyleSheet().
    
    (WebCore::StyleSheet::setMedia):
    
        Change StyleBase::setParent() calls to MediaList::setParentStyleSheet().
        Assert that the parent sheet is either null, or this, as MediaLists should never
        be reparented to other sheets.
        Assert that the StyleSheet is indeed a CSSStyleSheet.  XSLStyleSheet inherits from
        StyleSheet, but it shouldn't be using the MediaList.
    
    * bindings/js/JSDOMBinding.h:
    (WebCore::root):
    
        Add MediaList* specific overload for root() since it can't fall back
        to root(StyleBase*) anymore.
    
    * WebCore.xcodeproj/project.pbxproj:
    
        Add some PrivateHeaders to accomodate the JSDOMBinding.h changes.
    
    * css/CSSMediaRule.h:
    
        Remove unnecessary forward-declaration of MediaList.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSImportRule.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSImportRule.cpp
@@ -40,7 +40,7 @@ CSSImportRule::CSSImportRule(CSSStyleSheet* parent, const String& href, PassRefP
     , m_loading(false)
 {
     if (m_lstMedia)
-        m_lstMedia->setParent(this);
+        m_lstMedia->setParentStyleSheet(parent);
     else
         m_lstMedia = MediaList::create(this, String());
 }
@@ -48,7 +48,7 @@ CSSImportRule::CSSImportRule(CSSStyleSheet* parent, const String& href, PassRefP
 CSSImportRule::~CSSImportRule()
 {
     if (m_lstMedia)
-        m_lstMedia->setParent(0);
+        m_lstMedia->setParentStyleSheet(0);
     if (m_styleSheet)
         m_styleSheet->setParent(0);
     if (m_cachedSheet)
