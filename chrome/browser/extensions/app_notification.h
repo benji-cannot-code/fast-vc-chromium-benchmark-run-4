e28d1ac167feb056c260e94d801630dcfefd05f5
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 20:11:42 +0000

    Implement SyncableService in AppNotificationsManager: - Implement all methods of SyncableService - Modify existing methods that change the model (Add and ClearAll) to push changes to sync. - Add some extra properties to AppNotification: guid and extension id - Disallow operations on model until storage is loaded. - Ton of unit tests for sync methods
    There is anotehr part for the full notifications sync to work that will be done in a separate CL.
    Review URL: http://codereview.chromium.org/8263002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_notification.h
+++ chrome/browser/extensions/app_notification.h
@@ -18,7 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // displayed on the New Tab Page.
 class AppNotification {
  public:
-  AppNotification(const std::string& title, const std::string& body);
+  // Creates an instance with the given properties.
+  // If |is_local| is true, notification is not synced.
+  // If |guid| is empty, a new guid is automatically created.
+  AppNotification(bool is_local,
+                  const std::string& guid,
+                  const std::string& extension_id,
+                  const std::string& title,
+                  const std::string& body);
   ~AppNotification();
 
   // Setters for optional properties.
@@ -26,6 +33,9 @@ class AppNotification {
   void set_link_text(const std::string& text) { link_text_ = text; }
 
   // Accessors.
+  bool is_local() const { return is_local_; }
+  const std::string& guid() const { return guid_; }
+  const std::string& extension_id() const { return extension_id_; }
   const std::string& title() const { return title_; }
   const std::string& body() const { return body_; }
   const GURL& link_url() const { return link_url_; }
@@ -42,6 +52,12 @@ class AppNotification {
   // If you add to the list of data members, make sure to add appropriate checks
   // to the Equals and {To,From}DictionaryValue methods, keeping in mind
   // backwards compatibility.
+
+  // Whether notification is local only, which means it is not synced
+  // across machines.
+  bool is_local_;
+  std::string guid_;
+  std::string extension_id_;
   std::string title_;
   std::string body_;
   GURL link_url_;
