79cf24beb9ebb034b4ab12e94bf438bb5e66da81
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-19 01:49:13 +0000

    Date.prototype.toISOString fails to throw exception https://bugs.webkit.org/show_bug.cgi?id=70394
    Reviewed by Sam Weinig.
    
    Source/JavaScriptCore: 
    
    * runtime/DatePrototype.cpp:
    (JSC::dateProtoFuncToISOString):
        - Should throw a range error if the internal value is not finite.
    
    LayoutTests: 
    
    * fast/js/date-toisostring-expected.txt:
    * fast/js/script-tests/date-toisostring.js:
    (throwsRangeError):
        - fixed test case.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/date-toisostring.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/date-toisostring.js
@@ -1,6 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Tests for Date.toISOString");
 
+function throwsRangeError(str)
+{
+    try {
+        eval(str);
+    } catch (e) {
+        return e instanceof RangeError;
+    }
+    return false;
+}
+
 shouldThrow("Date.toISOString.call({})");
 shouldThrow("Date.toISOString.call(0)");
 
@@ -11,6 +21,6 @@ shouldBe("new Date('1 January 4000 UTC').toISOString()", "'4000-01-01T00:00:00.0
 shouldBe("new Date('1 January 100000 UTC').toISOString()", "'+100000-01-01T00:00:00.000Z'");
 shouldBe("new Date('1 January -1 UTC').toISOString()", "'-000001-01-01T00:00:00.000Z'");
 shouldBe("new Date('10 March 2000 UTC').toISOString()", "'2000-03-10T00:00:00.000Z'");
-shouldBe("new Date(NaN).toISOString()", "'Invalid Date'");
+shouldBeTrue('throwsRangeError("new Date(NaN).toISOString()")');
 
 successfullyParsed = true;
