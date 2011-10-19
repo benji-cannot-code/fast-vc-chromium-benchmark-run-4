1af17955595e211edc0dd52be0a7e3c9b53381f0
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 05:27:44 +0000

    NTP4, Sync promo: Adding UMA to view/clicks on NTP4 sync promo.
    R=estade@chromium.org,isherman@chromium.org
    BUG=99540
    TEST=We get data in our new histogram.
    
    
    Review URL: http://codereview.chromium.org/8334016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/ntp_login_handler.cc
+++ chrome/browser/ui/webui/ntp/ntp_login_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/metrics/histogram.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/prefs/pref_notifier.h"
@@ -16,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/sync_setup_flow.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/ui/webui/sync_promo_ui.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
@@ -63,6 +64,9 @@ void NTPLoginHandler::Observe(int type,
 
 void NTPLoginHandler::HandleInitializeSyncLogin(const ListValue* args) {
   UpdateLogin();
+  UMA_HISTOGRAM_ENUMERATION("SyncPromo.NTPPromo",
+                            SYNC_PROMO_NTP_PROMO_VIEWED,
+                            SYNC_PROMO_NTP_PROMO_BUCKET_BOUNDARY);
 }
 
 void NTPLoginHandler::HandleShowSyncLoginUI(const ListValue* args) {
@@ -73,9 +77,12 @@ void NTPLoginHandler::HandleShowSyncLoginUI(const ListValue* args) {
   if (username.empty()) {
     // The user isn't signed in, show the sync promo.
     if (SyncPromoUI::ShouldShowSyncPromo(profile)) {
-        web_ui_->tab_contents()->OpenURL(GURL(chrome::kChromeUISyncPromoURL),
-                                         GURL(), CURRENT_TAB,
-                                         content::PAGE_TRANSITION_LINK);
+      UMA_HISTOGRAM_ENUMERATION("SyncPromo.NTPPromo",
+                                SYNC_PROMO_NTP_PROMO_CLICKED,
+                                SYNC_PROMO_NTP_PROMO_BUCKET_BOUNDARY);
+      web_ui_->tab_contents()->OpenURL(GURL(chrome::kChromeUISyncPromoURL),
+                                       GURL(), CURRENT_TAB,
+                                       content::PAGE_TRANSITION_LINK);
     }
   } else if (args->GetSize() == 4) {
     // The user is signed in, show the profiles menu.
