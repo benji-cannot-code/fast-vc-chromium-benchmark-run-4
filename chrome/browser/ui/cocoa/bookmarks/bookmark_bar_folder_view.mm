53bf6a8aeb3bdabba43e79d5cc58a6e95707a7ca
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 05:23:36 +0000

    Fix bookmark drag&drop across profiles.
    When dragging from one profile to another, we should always copy the bookmark, and never try to move it instead.
    
    BUG=99933
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8233043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106217 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_view.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_view.mm
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_view.h"
 
 #include "chrome/browser/bookmarks/bookmark_pasteboard_helper_mac.h"
+#include "chrome/browser/profiles/profile.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
 #import "chrome/browser/ui/cocoa/bookmarks/bookmark_folder_target.h"
+#import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #include "content/browser/user_metrics.h"
 
 #import "third_party/mozilla/NSPasteboard+Utils.h"
@@ -183,7 +185,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (data && [info draggingSource]) {
     BookmarkButton* button = nil;
     [data getBytes:&button length:sizeof(button)];
-    BOOL copy = !([info draggingSourceOperationMask] & NSDragOperationMove);
+
+    // If we're dragging from one profile to another, disallow moving (only
+    // allow copying). Note that we need to call |GetOriginalProfile()| to make
+    // sure that Incognito profiles are handled correctly.
+    NSWindow* source_window = [[button delegate] browserWindow];
+    BrowserWindowController* source_window_controller =
+        [BrowserWindowController
+         browserWindowControllerForWindow:source_window];
+    const Profile* source_profile =
+        [source_window_controller profile]->GetOriginalProfile();
+    const Profile* target_profile =
+        [controller_ bookmarkModel]->profile()->GetOriginalProfile();
+
+    BOOL copy =
+        !([info draggingSourceOperationMask] & NSDragOperationMove) ||
+        source_profile != target_profile;
     doDrag = [[self controller] dragButton:button
                                         to:[info draggingLocation]
                                       copy:copy];
