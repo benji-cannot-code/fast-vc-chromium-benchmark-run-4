8f800e7598b80eee85f49b22b83e9121a0fdbfcb
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 15:27:37 +0000

    Refactor net-internals TablePrinter.
    BUG=90857
    TEST=none
    
    Review URL: http://codereview.chromium.org/8342012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/index.js
+++ chrome/browser/resources/net_internals/index.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 <include src="util.js"/>
+<include src="table_printer.js"/>
 <include src="view.js"/>
 <include src="tab_switcher_view.js"/>
 <include src="import_view.js"/>
@@ -36,6 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="logs_view.js"/>
 <include src="prerender_view.js"/>
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   MainView.getInstance();  // from main.js
 });
