ee4b0f82d4be750524d38b86017640a985044037
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 12:16:12 +0000

    Swap 'Make changes' and 'Keep settings' buttons on 'Verify Settings Change' error bubble.
    BUG=100585
    TEST=manual
    
    Review URL: http://codereview.chromium.org/8321015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/global_error_bubble_view.cc
+++ chrome/browser/ui/views/global_error_bubble_view.cc
@@ -54,10 +54,11 @@ GlobalErrorBubbleView::GlobalErrorBubbleView(Browser* browser,
   message_label_->SetMultiLine(true);
   message_label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
 
+  bool accept_default = error_->IsAcceptButtonDefault();
   string16 accept_string(error_->GetBubbleViewAcceptButtonLabel());
   scoped_ptr<views::TextButton> accept_button(
       new views::NativeTextButton(this, UTF16ToWideHack(accept_string)));
-  accept_button->SetIsDefault(true);
+  accept_button->SetIsDefault(accept_default);
   accept_button->set_tag(TAG_ACCEPT_BUTTON);
 
   string16 cancel_string(error_->GetBubbleViewCancelButtonLabel());
@@ -65,6 +66,7 @@ GlobalErrorBubbleView::GlobalErrorBubbleView(Browser* browser,
   if (!cancel_string.empty()) {
     cancel_button.reset(
         new views::NativeTextButton(this, UTF16ToWideHack(cancel_string)));
+    cancel_button->SetIsDefault(!accept_default);
     cancel_button->set_tag(TAG_CANCEL_BUTTON);
   }
 
