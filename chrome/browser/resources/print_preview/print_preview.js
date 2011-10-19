cac4728c140281ae0103352fc306c25dcf5b4222
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 20:15:22 +0000

    Print Preview: Fixing Uncaught TypeError in the margins code.
    BUG=100896
    TEST=See bug description.
    
    
    Review URL: http://codereview.chromium.org/8353013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106365 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -818,6 +818,7 @@ function reloadPreviewPages(previewUid, previewResponseId) {
 
   hasPendingPreviewRequest = false;
   isPrintReadyMetafileReady = true;
+  previewArea.pdfLoaded = true;
   sendPrintDocumentRequestIfNeeded();
 }
 
