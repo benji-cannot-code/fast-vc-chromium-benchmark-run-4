f51b7fc9451d17bac27828f1213275c754439d78
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-19 07:18:01 +0000

    [EFL] Added missing dummy platformDescription function https://bugs.webkit.org/show_bug.cgi?id=70310
    Patch by Tomasz Morawski <t.morawski@samsung.com> on 2011-10-19
    Reviewed by Ryosuke Niwa.
    
    Added dummy platformDescription function needed to compile WK2.
    
    * platform/efl/ContextMenuEfl.cpp:
    (WebCore::ContextMenu::platformDescription):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/ContextMenuEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/ContextMenuEfl.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContextMenu.h"
 
 #include "NotImplemented.h"
+#include "PlatformMenuDescription.h"
 
 namespace WebCore {
 
@@ -52,6 +53,12 @@ void* ContextMenu::nativeMenu() const
 ContextMenu::ContextMenu()
 {
     notImplemented();
+}
+
+PlatformMenuDescription ContextMenu::platformDescription() const
+{
+    notImplemented();
+    return 0;
 }
 #endif
 
