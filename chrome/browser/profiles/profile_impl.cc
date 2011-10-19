b22eb2c94f0f8e728efe7ea4fcd60e56f0500038
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 20:39:54 +0000

    Fix ordering dependencies for #if OS_CHROMEOS and OS_LINUX
    BUG=none
    TEST=Chrome compiles and passes tests.
    
    
    Review URL: http://codereview.chromium.org/8296015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106376 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -121,7 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_store_mac.h"
 #elif defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/enterprise_extension_observer.h"
-#elif defined(OS_POSIX) && !defined(OS_CHROMEOS)
+#elif defined(OS_POSIX)
 #include "base/nix/xdg_util.h"
 #if defined(USE_GNOME_KEYRING)
 #include "chrome/browser/password_manager/native_backend_gnome_x.h"
