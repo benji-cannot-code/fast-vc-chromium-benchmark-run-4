cf7ceddf8e9c376a327a1e6bae2e81feed04fe37
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 04:10:23 +0000

    Fix touch build try 3
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106208 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/virtual_keyboard/virtual_keyboard_manager_browsertest.cc
+++ chrome/browser/ui/virtual_keyboard/virtual_keyboard_manager_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+  // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,10 +36,10 @@ class VirtualKeyboardManagerTest : public InProcessBrowserTest,
   }
 
   virtual void Observe(int type,
-                       const NotificationSource& source,
-                       const NotificationDetails& details) OVERRIDE {
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE {
     DCHECK_EQ(chrome::NOTIFICATION_KEYBOARD_VISIBILITY_CHANGED, type);
-    keyboard_visible_ = *Details<bool>(details).ptr();
+    keyboard_visible_ = *content::Details<bool>(details).ptr();
   }
 
   bool keyboard_visible_;
