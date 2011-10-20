e80c21900a26ec7d0cb50c04c929a9970c5df8af
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-20 19:54:34 +0000

    Move BubbleView logic into BubbleDelegateView. Add customization ctor; override GetInitiallyFocusedView.
    BUG=97248,98322,98323
    TEST=views_examples bubbles work as before.
    
    Review URL: http://codereview.chromium.org/8349015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106546 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/examples/bubble.cc
+++ ui/aura_shell/examples/bubble.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "views/bubble/bubble_border.h"
 #include "views/bubble/bubble_delegate.h"
-#include "views/bubble/bubble_view.h"
 #include "views/controls/label.h"
 #include "views/layout/fill_layout.h"
 #include "views/widget/widget.h"
@@ -24,29 +23,18 @@ struct BubbleConfig {
 class ExampleBubbleDelegateView : public views::BubbleDelegateView {
  public:
   ExampleBubbleDelegateView(const BubbleConfig& config)
-      : config_(config) {}
-
-  virtual gfx::Point GetAnchorPoint() const OVERRIDE {
-    return config_.anchor_point;
-  }
-
-  views::BubbleBorder::ArrowLocation GetArrowLocation() const OVERRIDE {
-    return config_.arrow;
-  }
-
-  SkColor GetColor() const OVERRIDE {
-    return config_.color;
-}
+      : BubbleDelegateView(config.anchor_point, config.arrow, config.color),
+        label_(config.label) {}
 
   virtual void Init() OVERRIDE {
     SetLayoutManager(new views::FillLayout());
-    views::Label* label = new views::Label(config_.label);
-    label->set_border(views::Border::CreateSolidBorder(10, config_.color));
+    views::Label* label = new views::Label(label_);
+    label->set_border(views::Border::CreateSolidBorder(10, GetColor()));
     AddChildView(label);
   }
 
  private:
-  const BubbleConfig config_;
+  string16 label_;
 };
 
 void CreatePointyBubble(views::Widget* parent, const gfx::Point& origin) {
--- views/views.gyp
+++ views/views.gyp
@@ -68,8 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'bubble/bubble_delegate.h',
         'bubble/bubble_frame_view.cc',
         'bubble/bubble_frame_view.h',
-        'bubble/bubble_view.cc',
-        'bubble/bubble_view.h',
         'context_menu_controller.h',
         'controls/button/button.cc',
         'controls/button/button.h',
