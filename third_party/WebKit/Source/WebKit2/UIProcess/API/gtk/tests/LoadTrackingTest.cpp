cc71c1c5187d6466edd671da30171f67c5ec178c
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-20 02:14:56 +0000

    [GTK] [WebKit2] Allow sharing page clients between WebViews https://bugs.webkit.org/show_bug.cgi?id=69814
    No longer associate WebPage client wrappers with a WebKitWebView. This allows the
    same client to be used with more than one WebKitWebView.
    
    Reviewed by Xan Lopez.
    
    * GNUmakefile.am: Add the new WebKitWebViewPrivate header.
    * UIProcess/API/gtk/WebKitDefines.h: Added some forward declarations for API classes.
    * UIProcess/API/gtk/WebKitWebLoaderClient.cpp: C API callbacks now convert their WKPageRef argument
    into a WebKitWebView directly and pass it as a parameter to signals. Remove the web-view property.
    * UIProcess/API/gtk/WebKitWebLoaderClient.h: Move the estimated-progress property to WebKitWebView.
    * UIProcess/API/gtk/WebKitWebView.cpp: Add an estimated-load-progress property.
    (webkitWebViewConstructed):Only allocate one default WebKitWebLoaderClient.
    (webkitWebViewGetProperty): Add support for the new property.
    (webkit_web_view_class_init): Ditto.
    (webkitWebViewSetEstimatedLoadProgress): Added this private method so that the WebKitWebLoaderClient
    can update the appropriate WebKitWebView.
    (webkit_web_view_set_loader_client): Attach the C API callbacks here now.
    (webkit_web_view_get_estimated_load_progress): Moved from WebKitWebLoaderClient.
    * UIProcess/API/gtk/WebKitWebView.h: Added new method definition.
    * UIProcess/API/gtk/WebKitWebViewPrivate.h: Copied from Source/WebKit2/UIProcess/API/gtk/WebKitDefines.h.
    * UIProcess/API/gtk/tests/LoadTrackingTest.cpp: Updated the load tracking tests callbacks to take the new parameter.
    (LoadTrackingTest::LoadTrackingTest): Listen to the WebKitWebView property now.
    (LoadTrackingTest::estimatedProgressChanged):
    * UIProcess/API/gtk/tests/TestWebKitWebView.cpp: Added a test to verify that two WebViews share the same default client.
    * UIProcess/API/gtk/webkit2marshal.list: Updated the marshallers here.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/LoadTrackingTest.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/LoadTrackingTest.cpp
@@ -23,37 +23,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <webkit2/webkit2.h>
 
-static gboolean provisionalLoadStartedCallback(WebKitWebLoaderClient* client, LoadTrackingTest* test)
+static gboolean provisionalLoadStartedCallback(WebKitWebLoaderClient* client, WebKitWebView*, LoadTrackingTest* test)
 {
     test->provisionalLoadStarted(client);
     return TRUE;
 }
 
-static gboolean provisionalLoadReceivedServerRedirectCallback(WebKitWebLoaderClient* client, LoadTrackingTest* test)
+static gboolean provisionalLoadReceivedServerRedirectCallback(WebKitWebLoaderClient* client, WebKitWebView*, LoadTrackingTest* test)
 {
     test->provisionalLoadReceivedServerRedirect(client);
     return TRUE;
 }
 
-static gboolean provisionalLoadFailedCallback(WebKitWebLoaderClient* client, const gchar* failingURI, GError* error, LoadTrackingTest* test)
+static gboolean provisionalLoadFailedCallback(WebKitWebLoaderClient* client, WebKitWebView*, const gchar* failingURI, GError* error, LoadTrackingTest* test)
 {
     test->provisionalLoadFailed(client, failingURI, error);
     return TRUE;
 }
 
-static gboolean loadCommittedCallback(WebKitWebLoaderClient* client, LoadTrackingTest* test)
+static gboolean loadCommittedCallback(WebKitWebLoaderClient* client, WebKitWebView*, LoadTrackingTest* test)
 {
     test->loadCommitted(client);
     return TRUE;
 }
 
-static gboolean loadFinishedCallback(WebKitWebLoaderClient* client, LoadTrackingTest* test)
+static gboolean loadFinishedCallback(WebKitWebLoaderClient* client, WebKitWebView*, LoadTrackingTest* test)
 {
     test->loadFinished(client);
     return TRUE;
 }
 
-static gboolean loadFailedCallback(WebKitWebLoaderClient* client, const gchar* failingURI, GError* error, LoadTrackingTest* test)
+static gboolean loadFailedCallback(WebKitWebLoaderClient* client, WebKitWebView*, const gchar* failingURI, GError* error, LoadTrackingTest* test)
 {
     test->loadFailed(client, failingURI, error);
     return TRUE;
@@ -74,13 +74,14 @@ LoadTrackingTest::LoadTrackingTest()
     g_signal_connect(client, "load-committed", G_CALLBACK(loadCommittedCallback), this);
     g_signal_connect(client, "load-finished", G_CALLBACK(loadFinishedCallback), this);
     g_signal_connect(client, "load-failed", G_CALLBACK(loadFailedCallback), this);
-    g_signal_connect(client, "notify::estimated-progress", G_CALLBACK(estimatedProgressChangedCallback), this);
+    g_signal_connect(m_webView, "notify::estimated-load-progress", G_CALLBACK(estimatedProgressChangedCallback), this);
 }
 
 LoadTrackingTest::~LoadTrackingTest()
 {
     WebKitWebLoaderClient* client = webkit_web_view_get_loader_client(m_webView);
     g_signal_handlers_disconnect_matched(client, G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, this);
+    g_signal_handlers_disconnect_matched(m_webView, G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, this);
 }
 
 void LoadTrackingTest::waitUntilLoadFinished()
@@ -128,8 +129,8 @@ void LoadTrackingTest::loadFailed(WebKitWebLoaderClient* client, const gchar* fa
 
 void LoadTrackingTest::estimatedProgressChanged()
 {
-    WebKitWebLoaderClient* client = webkit_web_view_get_loader_client(m_webView);
-    double progress = webkit_web_loader_client_get_estimated_progress(client);
+    double progress = webkit_web_view_get_estimated_load_progress(m_webView);
     g_assert_cmpfloat(m_estimatedProgress, <, progress);
     m_estimatedProgress = progress;
 }
+
--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitWebView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitWebView.cpp
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebViewTest.h"
 #include "LoadTrackingTest.h"
+#include <wtf/gobject/GRefPtr.h>
 
 static void testWebViewDefaultContext(WebViewTest* test, gconstpointer)
 {
@@ -37,11 +38,21 @@ static void testWebViewCustomCharset(WebViewTest* test, gconstpointer)
     g_assert(!webkit_web_view_get_custom_charset(test->m_webView));
 }
 
+static void testWebViewsShareClients(Test* test, gconstpointer)
+{
+    GRefPtr<GtkWidget> webView1 = webkit_web_view_new();
+    GRefPtr<GtkWidget> webView2 = webkit_web_view_new();
+    WebKitWebLoaderClient* client1 = webkit_web_view_get_loader_client(WEBKIT_WEB_VIEW(webView1.get()));
+    WebKitWebLoaderClient* client2 = webkit_web_view_get_loader_client(WEBKIT_WEB_VIEW(webView2.get()));
+    g_assert(client1);
+    g_assert(client1 == client2);
+}
 
 void beforeAll()
 {
     WebViewTest::add("WebKitWebView", "default-context", testWebViewDefaultContext);
     WebViewTest::add("WebKitWebView", "custom-charset", testWebViewCustomCharset);
+    Test::add("WebKitWebView", "webviews-share-clients", testWebViewsShareClients);
 }
 
 void afterAll()
