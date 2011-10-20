6a95076a594e93cecad61f834f6a99757fd56963
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-20 19:55:33 +0000

    Added missing policy to policy_prefs_ui.py
    This was removed by mistake on http://crrev.com/106521
    
    TBR=nirnimesh@chromium.org
    TEST=policy_prefs_ui.py passes on pyautos
    
    Review URL: http://codereview.chromium.org/8366010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/policy_prefs_ui.py
+++ chrome/test/functional/policy_prefs_ui.py
@@ -85,6 +85,7 @@ class PolicyPrefsUITest(pyauto.PyUITest):
     'IncognitoEnabled': (False, []),
     'IncognitoModeAvailability': (1, []),
     'SavingBrowserHistoryDisabled': (True, []),
+    'RemoteAccessClientFirewallTraversal': (True, []),
     'RemoteAccessHostFirewallTraversal': (True, []),
     'PrintingEnabled': (False, []),
     # Note: supported_on is empty for this policy.
