4addc424e1ed20a19a5341f7cb63353c1269a4fc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-20 18:37:14 +0000

    Remove StyleBase::cssText(). https://bugs.webkit.org/show_bug.cgi?id=70521
    Patch by Andreas Kling <kling@webkit.org> on 2011-10-20
    Reviewed by Antti Koivisto.
    
    * css/StyleBase.cpp:
    * css/StyleBase.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleBase.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleBase.cpp
@@ -31,11 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-String StyleBase::cssText() const
-{
-    return "";
-}
-
 void StyleBase::checkLoaded()
 {
     if (parent())
