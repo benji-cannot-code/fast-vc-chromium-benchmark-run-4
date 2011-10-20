a511e91752bcb329d7e8c978421d0da1a5bd9d9c
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-20 16:48:35 +0000

    When user is panning with the tiled backing store, the page isn't notified about the scroll position change https://bugs.webkit.org/show_bug.cgi?id=70495
    Reviewed by Simon Hausmann.
    
    When using the tiled backing store the UI handles scrolling,
    and sends setFixedVisibleContentRect after panning/scale ends.
    
    If we actually changed position we need to send the scroll DOM event.
    
    Covered by existing tests, though we are not testing the tiled backing store yet.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::setFixedVisibleContentRect):
    * page/FrameView.h:
    * platform/ScrollView.h:
    (WebCore::ScrollView::setFixedVisibleContentRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.h
+++ third_party/WebKit/Source/WebCore/page/FrameView.h
@@ -166,6 +166,7 @@ public:
 
     virtual LayoutRect windowResizerRect() const;
 
+    virtual void setFixedVisibleContentRect(const IntRect&) OVERRIDE;
     void setScrollPosition(const LayoutPoint&);
     void scrollPositionChangedViaPlatformWidget();
     virtual void repaintFixedElementsAfterScrolling();
