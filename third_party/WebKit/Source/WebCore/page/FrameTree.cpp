beee79df5004671dcefbb5f2de0001e0edad919d
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-20 20:57:47 +0000

    Fix build breakage on some platforms after r98008.
    * page/FrameTree.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameTree.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameTree.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameView.h"
 #include "Page.h"
 #include "PageGroup.h"
+#include "Document.h"
 #include <stdarg.h>
 #include <wtf/StringExtras.h>
 #include <wtf/Vector.h>
