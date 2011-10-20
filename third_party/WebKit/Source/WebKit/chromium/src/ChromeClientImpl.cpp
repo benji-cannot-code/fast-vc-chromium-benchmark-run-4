b1fda9fa55965e483bbc84c1241c38db448e2625
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-20 01:47:45 +0000

    [Chromium]: Move from FileChooserSettings deprecatedAcceptType to acceptMIMETypes https://bugs.webkit.org/show_bug.cgi?id=70003
    Reviewed by Darin Fisher.
    
    * public/WebFileChooserParams.h:
    Add acceptMIMETypes, and make acceptTypes deprecated.
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::runOpenPanel):
    - Set WebFileChooserParams::acceptMIMETypes
    - Set a joined string to WebFileChooserParams::acceptTypes until we remove acceptTypes.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@97910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/ChromeClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/ChromeClientImpl.cpp
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebWindowFeatures.h"
 #include "WindowFeatures.h"
 #include "WrappedResourceRequest.h"
+#include <wtf/text/StringBuilder.h>
 #include <wtf/text/StringConcatenate.h>
 #include <wtf/unicode/CharacterNames.h>
 
@@ -671,7 +672,16 @@ void ChromeClientImpl::runOpenPanel(Frame* frame, PassRefPtr<FileChooser> fileCh
 #else
     params.directory = false;
 #endif
-    params.acceptTypes = fileChooser->settings().deprecatedAcceptTypes;
+    params.acceptMIMETypes = fileChooser->settings().acceptMIMETypes;
+    // FIXME: Remove WebFileChooserParams::acceptTypes.
+    StringBuilder builder;
+    const Vector<String>& acceptTypeList = fileChooser->settings().acceptMIMETypes;
+    for (unsigned i = 0; i < acceptTypeList.size(); ++i) {
+        if (i > 0)
+            builder.append(',');
+        builder.append(acceptTypeList[i]);
+    }
+    params.acceptTypes = builder.toString();
     params.selectedFiles = fileChooser->settings().selectedFiles;
     if (params.selectedFiles.size() > 0)
         params.initialValue = params.selectedFiles[0];
