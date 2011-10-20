515f96a11e9a5ce9ec0b652300fbc41f09f46a8c
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-20 00:43:03 +0000

    Print Preview: Hiding margins UI if an error occurs.
    BUG=100818
    TEST=Select custom margins, close the initiator tab, the margins UI should be hidden.
    
    
    Review URL: http://codereview.chromium.org/8343017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/preview_area.js
+++ chrome/browser/resources/print_preview/preview_area.js
@@ -199,8 +199,7 @@ cr.define('print_preview', function() {
      * Display an error message in the center of the preview area.
      * @param {string} errorMessage The error message to be displayed.
      */
-    displayErrorMessage: function(errorMessage) {
-      $('print-button').disabled = true;
+    displayErrorMessageAndNotify: function(errorMessage) {
       this.overlayLayer.classList.remove('invisible');
       this.customMessage_.textContent = errorMessage;
       this.customMessage_.hidden = false;
@@ -210,6 +209,7 @@ cr.define('print_preview', function() {
         $('mainview').removeChild(this.pdfPlugin_);
         this.pdfPlugin_ = null;
       }
+      cr.dispatchSimpleEvent(document, 'PDFGenerationError');
     },
 
     /**
@@ -220,7 +220,7 @@ cr.define('print_preview', function() {
      * @param {string} buttonListener The listener to be executed when the
      *     button is clicked.
      */
-    displayErrorMessageWithButton: function(
+    displayErrorMessageWithButtonAndNotify: function(
         errorMessage, buttonText, buttonListener) {
       this.errorButton.disabled = false;
       this.errorButton.textContent = buttonText;
@@ -228,7 +228,7 @@ cr.define('print_preview', function() {
       this.errorButton.hidden = false;
       $('system-dialog-throbber').hidden = true;
       $('native-print-dialog-throbber').hidden = true;
-      this.displayErrorMessage(errorMessage);
+      this.displayErrorMessageAndNotify(errorMessage);
     }
   };
 
--- chrome/browser/resources/print_preview/print_preview.js
+++ chrome/browser/resources/print_preview/print_preview.js
@@ -115,14 +115,14 @@ function onLoad() {
 
   previewArea = print_preview.PreviewArea.getInstance();
   printHeader = print_preview.PrintHeader.getInstance();
+  document.addEventListener('PDFGenerationError', cancelPendingPrintRequest);
 
   if (!checkCompatiblePluginExists()) {
     disableInputElementsInSidebar();
-    previewArea.displayErrorMessageWithButton(
+    previewArea.displayErrorMessageWithButtonAndNotify(
         localStrings.getString('noPlugin'),
         localStrings.getString('launchNativeDialog'),
         launchNativePrintDialog);
-    cancelPendingPrintRequest();
     $('mainview').parentElement.removeChild($('dummy-viewer'));
     return;
   }
@@ -192,15 +192,13 @@ function launchNativePrintDialog() {
 function onInitiatorTabCrashed(initiatorTabURL) {
   disableInputElementsInSidebar();
   if (initiatorTabURL) {
-    previewArea.displayErrorMessageWithButton(
+    previewArea.displayErrorMessageWithButtonAndNotify(
         localStrings.getString('initiatorTabCrashed'),
         localStrings.getString('reopenPage'),
         function() { chrome.send('reloadCrashedInitiatorTab'); });
-    cancelPendingPrintRequest();
   } else {
-    previewArea.displayErrorMessage(
+    previewArea.displayErrorMessageAndNotify(
         localStrings.getString('initiatorTabCrashed'));
-    cancelPendingPrintRequest();
   }
 }
 
@@ -212,15 +210,13 @@ function onInitiatorTabCrashed(initiatorTabURL) {
 function onInitiatorTabClosed(initiatorTabURL) {
   disableInputElementsInSidebar();
   if (initiatorTabURL) {
-    previewArea.displayErrorMessageWithButton(
+    previewArea.displayErrorMessageWithButtonAndNotify(
         localStrings.getString('initiatorTabClosed'),
         localStrings.getString('reopenPage'),
         function() { window.location = initiatorTabURL; });
-    cancelPendingPrintRequest();
   } else {
-    previewArea.displayErrorMessage(
+    previewArea.displayErrorMessageAndNotify(
         localStrings.getString('initiatorTabClosed'));
-    cancelPendingPrintRequest();
   }
 }
 
@@ -715,11 +711,10 @@ function setColor(color) {
  * Called from PrintPreviewMessageHandler::OnPrintPreviewFailed().
  */
 function printPreviewFailed() {
-  previewArea.displayErrorMessageWithButton(
+  previewArea.displayErrorMessageWithButtonAndNotify(
       localStrings.getString('previewFailed'),
       localStrings.getString('launchNativeDialog'),
       launchNativePrintDialog);
-  cancelPendingPrintRequest();
 }
 
 /**
@@ -727,9 +722,8 @@ function printPreviewFailed() {
  * Called from PrintPreviewMessageHandler::OnInvalidPrinterSettings().
  */
 function invalidPrinterSettings() {
-  previewArea.displayErrorMessage(
+  previewArea.displayErrorMessageAndNotify(
       localStrings.getString('invalidPrinterSettings'));
-  cancelPendingPrintRequest();
 }
 
 /**
