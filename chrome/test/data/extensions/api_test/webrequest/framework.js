03309971c9e126ad459e60a494f6e871a6a4e23b
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-20 19:35:27 +0000

    Add asyncBlocking support to webRequest event listeners.
    If an event listener is registered with "asyncBlocking" listed in the extraInfoSpec, then a callback is passed into the listener in addition to the details argument. The callback expects a BlockingResponse argument, and MUST be invoked by the listener at some point so the request can proceed.
    
    This is supported for the onAuthRequired listener only at this point.
    
    BUG=32056
    TEST=Write an extension with an onAuthRequired listener set with "asyncBlocking" in the extra parameters, validate that it works.
    
    
    Review URL: http://codereview.chromium.org/8354014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/webrequest/framework.js
+++ chrome/test/data/extensions/api_test/webrequest/framework.js
@@ -128,7 +128,7 @@ function checkUserAgent(headers) {
   return false;
 }
 
-function captureEvent(name, details) {
+function captureEvent(name, details, callback) {
   // Ignore system-level requests like safebrowsing updates and favicon fetches
   // since they are unpredictable.
   if (details.tabId == -1 || details.type == "other" ||
@@ -194,7 +194,12 @@ function captureEvent(name, details) {
 
   capturedEventData.push({label: label, event: name, details: details});
   checkExpectations();
-  return retval;
+
+  if (callback) {
+    window.setTimeout(callback, 0, retval);
+  } else {
+    return retval;
+  }
 }
 
 // Simple array intersection. We use this to filter extraInfoSpec so
@@ -221,9 +226,10 @@ function initListeners(filter, extraInfoSpec) {
     return captureEvent("onHeadersReceived", details);
   }, filter, intersect(extraInfoSpec, ["blocking", "responseHeaders"]));
   chrome.experimental.webRequest.onAuthRequired.addListener(
-      function(details) {
-    return captureEvent("onAuthRequired", details);
-  }, filter, intersect(extraInfoSpec, ["blocking", "responseHeaders"]));
+      function(details, callback) {
+    return captureEvent("onAuthRequired", details, callback);
+  }, filter, intersect(extraInfoSpec, ["asyncBlocking", "blocking",
+                                       "responseHeaders"]));
   chrome.experimental.webRequest.onResponseStarted.addListener(
       function(details) {
     return captureEvent("onResponseStarted", details);
