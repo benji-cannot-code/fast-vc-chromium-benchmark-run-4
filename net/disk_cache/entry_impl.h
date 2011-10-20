efca9b253a3e02df04a3964c52caa1e5de3eb5ee
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-20 21:23:50 +0000

    Disk cache: Add another timming histogram.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8357021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106581 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/entry_impl.h
+++ net/disk_cache/entry_impl.h
@@ -31,7 +31,9 @@ class NET_EXPORT_PRIVATE EntryImpl
     kWrite,
     kSparseRead,
     kSparseWrite,
-    kAsyncIO
+    kAsyncIO,
+    kReadAsync1,
+    kWriteAsync1
   };
 
   EntryImpl(BackendImpl* backend, Addr address, bool read_only);
