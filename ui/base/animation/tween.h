8eacc262a4d9fc1c96f24c072ddbf545e5648725
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 20:36:32 +0000

    Revert 106768 - Explicit animation support
    High level description:
     - LayerPropertySetter is now LayerAnimator since it manages implicit/explicit animations and the animation queue.
     - LayerAnimationElement represents an animation curve.
     - LayerAnimationSequence owns a collection of elements.
     - The animator works as follows:
       o Has a queue of sequences and a collection of running sequences.
       o It knows the start time of each running sequence.
       o While there are running sequences, LayerAnimator::Step(base::TimeTicks now) is called periodically, and each of the running sequences are updated.
    
    BUG=None
    TEST=compositor_unittests, base_unittests
    
    Review URL: http://codereview.chromium.org/8247009
    
    TBR=vollick@chromium.org
    Review URL: http://codereview.chromium.org/8375008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106774 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/animation/tween.h
+++ ui/base/animation/tween.h
@@ -9,11 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "ui/base/ui_export.h"
-#include "ui/gfx/rect.h"
 
-#if !defined(OS_MACOSX)
-#include "ui/gfx/transform.h"
-#endif
+namespace gfx {
+class Rect;
+}
 
 namespace ui {
 
@@ -38,11 +37,6 @@ class UI_EXPORT Tween {
   static gfx::Rect ValueBetween(double value,
                                 const gfx::Rect& start_bounds,
                                 const gfx::Rect& target_bounds);
-#if !defined(OS_MACOSX)
-  static Transform ValueBetween(double value,
-                                const Transform& start_transform,
-                                const Transform& target_transform);
-#endif
 
  private:
   Tween();
--- ui/gfx/compositor/layer.cc
+++ ui/gfx/compositor/layer.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "ui/base/animation/animation.h"
-#include "ui/gfx/compositor/layer_animation_manager.h"
+#include "ui/gfx/compositor/layer_animator.h"
 #include "ui/gfx/canvas_skia.h"
 #include "ui/gfx/interpolated_transform.h"
 #include "ui/gfx/point3.h"
@@ -110,7 +110,7 @@ bool Layer::Contains(const Layer* other) const {
 void Layer::SetAnimation(Animation* animation) {
   if (animation) {
     if (!animator_.get())
-      animator_.reset(new LayerAnimationManager(this));
+      animator_.reset(new LayerAnimator(this));
     animation->Start();
     animator_->SetAnimation(animation);
   } else {
@@ -119,7 +119,7 @@ void Layer::SetAnimation(Animation* animation) {
 }
 
 void Layer::SetTransform(const ui::Transform& transform) {
-  StopAnimatingIfNecessary(LayerAnimationManager::TRANSFORM);
+  StopAnimatingIfNecessary(LayerAnimator::TRANSFORM);
   if (animator_.get() && animator_->IsRunning()) {
     animator_->AnimateTransform(transform);
     return;
@@ -128,7 +128,7 @@ void Layer::SetTransform(const ui::Transform& transform) {
 }
 
 void Layer::SetBounds(const gfx::Rect& bounds) {
-  StopAnimatingIfNecessary(LayerAnimationManager::LOCATION);
+  StopAnimatingIfNecessary(LayerAnimator::LOCATION);
   if (animator_.get() && animator_->IsRunning() &&
       bounds.size() == bounds_.size()) {
     animator_->AnimateToPoint(bounds.origin());
@@ -144,7 +144,7 @@ gfx::Rect Layer::GetTargetBounds() const {
 }
 
 void Layer::SetOpacity(float opacity) {
-  StopAnimatingIfNecessary(LayerAnimationManager::OPACITY);
+  StopAnimatingIfNecessary(LayerAnimator::OPACITY);
   if (animator_.get() && animator_->IsRunning()) {
     animator_->AnimateOpacity(opacity);
     return;
@@ -440,23 +440,23 @@ bool Layer::GetTransformRelativeTo(const Layer* ancestor,
 }
 
 void Layer::StopAnimatingIfNecessary(
-    LayerAnimationManager::AnimationProperty property) {
+    LayerAnimator::AnimationProperty property) {
   if (!animator_.get() || !animator_->IsRunning() ||
       !animator_->got_initial_tick()) {
     return;
   }
 
-  if (property != LayerAnimationManager::LOCATION &&
-      animator_->IsAnimating(LayerAnimationManager::LOCATION)) {
+  if (property != LayerAnimator::LOCATION &&
+      animator_->IsAnimating(LayerAnimator::LOCATION)) {
     SetBoundsImmediately(
         gfx::Rect(animator_->GetTargetPoint(), bounds_.size()));
   }
-  if (property != LayerAnimationManager::OPACITY &&
-      animator_->IsAnimating(LayerAnimationManager::OPACITY)) {
+  if (property != LayerAnimator::OPACITY &&
+      animator_->IsAnimating(LayerAnimator::OPACITY)) {
     SetOpacityImmediately(animator_->GetTargetOpacity());
   }
-  if (property != LayerAnimationManager::TRANSFORM &&
-      animator_->IsAnimating(LayerAnimationManager::TRANSFORM)) {
+  if (property != LayerAnimator::TRANSFORM &&
+      animator_->IsAnimating(LayerAnimator::TRANSFORM)) {
     SetTransformImmediately(animator_->GetTargetTransform());
   }
   animator_.reset();
