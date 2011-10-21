65087f343bc59c477f2d80203c8b7c02fe142825
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-21 16:58:58 +0000

    Web Inspector: Advanced search is working very slowly and does not show searching progress. https://bugs.webkit.org/show_bug.cgi?id=70611
    Search implementation changed so that we do not start searching in the next file unless
    the previous one was already searched. This allows to interrupt search.
    Not all search matches are added on UI by default now, only first 20 for each file.
    Search progress information and search stop button were added to drawer status bar.
    
    Reviewed by Pavel Feldman.
    
    * English.lproj/localizedStrings.js:
    * inspector/ContentSearchUtils.cpp:
    (WebCore::ContentSearchUtils::getRegularExpressionMatchesByLines):
    (WebCore::ContentSearchUtils::countRegularExpressionMatches):
    * inspector/front-end/AdvancedSearchController.js:
    (WebInspector.AdvancedSearchController.prototype._onSearchResult):
    (WebInspector.AdvancedSearchController.prototype._onSearchFinished):
    (WebInspector.AdvancedSearchController.prototype.startSearch):
    (WebInspector.AdvancedSearchController.prototype.resetSearch):
    (WebInspector.AdvancedSearchController.prototype.stopSearch):
    (WebInspector.SearchView):
    (WebInspector.SearchView.prototype.get statusBarItems):
    (WebInspector.SearchView.prototype.get counterElement):
    (WebInspector.SearchView.prototype.set resultsPane):
    (WebInspector.SearchView.prototype.searchStarted):
    (WebInspector.SearchView.prototype._updateSearchResultsMessage):
    (WebInspector.SearchView.prototype._updateSearchProgress):
    (WebInspector.SearchView.prototype.resetResults):
    (WebInspector.SearchView.prototype._resetCounters):
    (WebInspector.SearchView.prototype.nothingFound):
    (WebInspector.SearchView.prototype.addSearchResult):
    (WebInspector.SearchView.prototype.searchFinished):
    (WebInspector.SearchView.prototype._searchStopButtonPressed):
    (WebInspector.SearchResultsPane):
    (WebInspector.FileBasedSearchResultsPane):
    (WebInspector.FileBasedSearchResultsPane.prototype.addSearchResult):
    (WebInspector.FileBasedSearchResultsPane.prototype._fileTreeElementExpanded):
    (WebInspector.FileBasedSearchResultsPane.prototype._appendSearchMatches):
    (WebInspector.FileBasedSearchResultsPane.prototype._appendShowMoreMatchesElement):
    (WebInspector.FileBasedSearchResultsPane.prototype._showMoreMatchesElementSelected):
    (WebInspector.FileBasedSearchResultsPane.prototype._addFileTreeElement):
    (WebInspector.FileBasedSearchResultsPane.prototype._regexMatchRanges):
    * inspector/front-end/Drawer.js:
    (WebInspector.Drawer.prototype.hide):
    * inspector/front-end/Images/statusbarButtonGlyphs.png:
    * inspector/front-end/ScriptsSearchScope.js:
    (WebInspector.ScriptsSearchScope):
    (WebInspector.ScriptsSearchScope.prototype.performSearch.filterOutContentScripts):
    (WebInspector.ScriptsSearchScope.prototype.performSearch.continueSearch):
    (WebInspector.ScriptsSearchScope.prototype.performSearch.searchCallbackWrapper):
    (WebInspector.ScriptsSearchScope.prototype.performSearch):
    (WebInspector.ScriptsSearchScope.prototype.stopSearch):
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._registerShortcuts):
    * inspector/front-end/inspector.css:
    (#drawer-status-bar .search-status-bar-item):
    (#drawer-status-bar .search-status-bar-message):
    (#drawer-status-bar .search-status-bar-progress):
    (#drawer-status-bar .search-status-bar-stop-button-item):
    (#drawer-status-bar .search-status-bar-stop-button .glyph):
    (#drawer-status-bar .search-results-status-bar-message):
    (.search-view .search-results):
    (#search-results-pane-file-based .search-results-outline-disclosure):
    (#search-results-pane-file-based .search-result):
    (#search-results-pane-file-based .search-result:hover):
    (#search-results-pane-file-based .show-more-matches):
    (#search-results-pane-file-based .show-more-matches:hover):
    (#search-results-pane-file-based .search-match:hover):
    * inspector/front-end/utilities.js:
    ():
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.cpp
+++ third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.cpp
@@ -63,6 +63,8 @@ static String createSearchRegexSource(const String& text)
 static Vector<pair<int, String> > getRegularExpressionMatchesByLines(const RegularExpression& regex, const String& text)
 {
     Vector<pair<int, String> > result;
+    if (text.isEmpty())
+        return result;
 
     int lineNumber = 0;
     unsigned start = 0;
@@ -106,6 +108,9 @@ RegularExpression createSearchRegex(const String& query, bool caseSensitive, boo
 
 int countRegularExpressionMatches(const RegularExpression& regex, const String& content)
 {
+    if (content.isEmpty())
+        return 0;
+
     int result = 0;
     int position;
     unsigned start = 0;
--- third_party/WebKit/Source/WebCore/inspector/front-end/Drawer.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Drawer.js
@@ -75,6 +75,8 @@ WebInspector.Drawer.prototype = {
     show: function(view, animationType)
     {
         this.immediatelyFinishAnimation();
+        if (this._view && this._view.counterElement)
+            this._view.counterElement.parentNode.removeChild(this._view.counterElement);
 
         var drawerWasVisible = this.visible;
 
@@ -87,6 +89,9 @@ WebInspector.Drawer.prototype = {
         this._viewStatusBar.removeChildren();
         for (var i = 0; i < statusBarItems.length; ++i)
             this._viewStatusBar.appendChild(statusBarItems[i]);
+
+        if (this._view.counterElement)
+            this._counters.insertBefore(this._view.counterElement, this._counters.firstChild);
 
         document.body.addStyleClass("drawer-visible");
         this._view.show(this._drawerContentsElement);
@@ -113,7 +118,7 @@ WebInspector.Drawer.prototype = {
             this._currentPanelCounters.parentNode.removeChild(this._currentPanelCounters);
             this._mainStatusBar.appendChild(this._currentPanelCounters);
         }
-
+        
         function animationFinished()
         {
             WebInspector.currentPanel().statusBarResized();
@@ -167,6 +172,9 @@ WebInspector.Drawer.prototype = {
             WebInspector.currentPanel().doResize();
             this._mainStatusBar.insertBefore(anchoredItems, this._mainStatusBar.firstChild);
             this._mainStatusBar.style.removeProperty("padding-left");
+
+            if (this._view.counterElement)
+                this._view.counterElement.parentNode.removeChild(this._view.counterElement);
 
             if (this._currentPanelCounters) {
                 this._currentPanelCounters.setAttribute("style", null);
