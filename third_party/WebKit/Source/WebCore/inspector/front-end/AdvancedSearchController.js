ba90c049c49f606341abd017e0c425138b2ab4ff
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-21 19:38:12 +0000

    Web Inspector: Advanced search results should be cleared on navigation. https://bugs.webkit.org/show_bug.cgi?id=70627
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/AdvancedSearchController.js:
    (WebInspector.AdvancedSearchController):
    (WebInspector.AdvancedSearchController.prototype._frameNavigated):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/AdvancedSearchController.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/AdvancedSearchController.js
@@ -36,6 +36,8 @@ WebInspector.AdvancedSearchController = function()
     this._searchId = 0;
     
     WebInspector.settings.advancedSearchConfig = WebInspector.settings.createSetting("advancedSearchConfig", new WebInspector.SearchConfig("", true, false));
+    
+    WebInspector.resourceTreeModel.addEventListener(WebInspector.ResourceTreeModel.EventTypes.FrameNavigated, this._frameNavigated, this);
 }
 
 WebInspector.AdvancedSearchController.createShortcut = function()
@@ -53,6 +55,11 @@ WebInspector.AdvancedSearchController.prototype = {
             this.show();
             event.handled = true;
         }
+    },
+
+    _frameNavigated: function()
+    {
+        this.resetSearch();
     },
 
     /**
@@ -370,6 +377,9 @@ WebInspector.SearchView.prototype = {
     
     _onAction: function()
     {
+        if (!this.searchConfig.query || !this.searchConfig.query.length)
+            return;
+        
         this._save();
         this._controller.startSearch(this.searchConfig);
     }
