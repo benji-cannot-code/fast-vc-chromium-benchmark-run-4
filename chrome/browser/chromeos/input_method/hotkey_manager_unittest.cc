457d14544f0e2e6aecf16ba92635436e984c5785
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 04:57:50 +0000

    Ignore NumLock modifier so that IME could be switched even if NumLock is enabled.
    Remove Super and Hyper modifiers support for now. I thought Mod2Mask in X corresponds to Super and Mod3Mask does Hyper, but I was wrong. In the current X configuration for Chrome OS, Mod2Mask is for NumLock.
    
    BUG=chromium-os:21842
    TEST=ran unit_tests
    
    
    Review URL: http://codereview.chromium.org/8356009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/hotkey_manager_unittest.cc
+++ chrome/browser/chromeos/input_method/hotkey_manager_unittest.cc
@@ -72,13 +72,15 @@ TEST(HotkeyManagerTest, TestIsModifier) {
   EXPECT_TRUE(manager.IsModifier(XK_Alt_R));
   EXPECT_TRUE(manager.IsModifier(XK_Meta_L));
   EXPECT_TRUE(manager.IsModifier(XK_Meta_R));
-  EXPECT_TRUE(manager.IsModifier(XK_Super_L));
-  EXPECT_TRUE(manager.IsModifier(XK_Super_R));
-  EXPECT_TRUE(manager.IsModifier(XK_Hyper_L));
-  EXPECT_TRUE(manager.IsModifier(XK_Hyper_R));
   EXPECT_FALSE(manager.IsModifier(XK_a));
   EXPECT_FALSE(manager.IsModifier(XK_A));
   EXPECT_FALSE(manager.IsModifier(XK_Tab));
+
+  // For now, we don't support Super and Hyper. See crosbug.com/21842.
+  EXPECT_FALSE(manager.IsModifier(XK_Super_L));
+  EXPECT_FALSE(manager.IsModifier(XK_Super_R));
+  EXPECT_FALSE(manager.IsModifier(XK_Hyper_L));
+  EXPECT_FALSE(manager.IsModifier(XK_Hyper_R));
 }
 
 TEST(HotkeyManagerTest, TestNormalizeModifiers) {
@@ -101,14 +103,6 @@ TEST(HotkeyManagerTest, TestNormalizeModifiers) {
             manager.NormalizeModifiers(XK_Meta_L, ShiftMask, true));
   EXPECT_EQ(Mod1Mask | ShiftMask | kReleaseMask,
             manager.NormalizeModifiers(XK_Meta_R, ShiftMask, false));
-  EXPECT_EQ(ShiftMask | Mod2Mask | 0x0U,
-            manager.NormalizeModifiers(XK_Super_L, ShiftMask, true));
-  EXPECT_EQ(Mod1Mask | Mod2Mask | kReleaseMask,
-            manager.NormalizeModifiers(XK_Super_R, Mod1Mask, false));
-  EXPECT_EQ(ShiftMask | Mod3Mask | kReleaseMask,
-            manager.NormalizeModifiers(XK_Hyper_L, ShiftMask, false));
-  EXPECT_EQ(Mod1Mask | Mod3Mask | 0x0U,
-            manager.NormalizeModifiers(XK_Hyper_R, Mod1Mask, true));
 
   // Test non modifier keys like XK_a.
   EXPECT_EQ(Mod1Mask | 0x0U,
@@ -121,6 +115,16 @@ TEST(HotkeyManagerTest, TestNormalizeModifiers) {
   EXPECT_EQ(Mod1Mask | ControlMask | kReleaseMask,
             manager.NormalizeModifiers(
                 XK_D, Mod1Mask | ControlMask, false));
+
+  // For now, we don't support Super and Hyper. See crosbug.com/21842.
+  EXPECT_EQ(ShiftMask | 0x0U,
+            manager.NormalizeModifiers(XK_Super_L, ShiftMask, true));
+  EXPECT_EQ(Mod1Mask | kReleaseMask,
+            manager.NormalizeModifiers(XK_Super_R, Mod1Mask, false));
+  EXPECT_EQ(ShiftMask | kReleaseMask,
+            manager.NormalizeModifiers(XK_Hyper_L, ShiftMask, false));
+  EXPECT_EQ(Mod1Mask | 0x0U,
+            manager.NormalizeModifiers(XK_Hyper_R, Mod1Mask, true));
 }
 
 TEST(HotkeyManagerTest, TestAddBasic) {
