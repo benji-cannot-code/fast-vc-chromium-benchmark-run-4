6d4c9699aa7d863f1fa7ebd7ad1ff20414080abd
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 09:28:23 +0000

    Fix for a memory leak in views/controls/native/native_view_host_gtk.cc
    Landing a patch for Parag Radke, originally reviewed at
    http://codereview.chromium.org/8294008/
    
    BUG=96368
    TBR=sky
    Review URL: http://codereview.chromium.org/8353034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106696 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/native/native_view_host_gtk.cc
+++ views/controls/native/native_view_host_gtk.cc
@@ -113,7 +113,7 @@ NativeViewHostGtk::NativeViewHostGtk(NativeViewHost* host)
 NativeViewHostGtk::~NativeViewHostGtk() {
   if (fixed_) {
     gtk_container_foreach(GTK_CONTAINER(fixed_), RemoveFromParent, fixed_);
-    gtk_widget_destroy(fixed_);
+    DestroyFixed();
   }
 }
 
