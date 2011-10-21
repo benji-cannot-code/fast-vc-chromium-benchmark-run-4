4807dc090b3f7002114faed68f409d521bfd9c9f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 17:57:04 +0000

    Fix build break from bad merge
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/all_messages.h
+++ chrome/common/all_messages.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // up-to-date when adding a new value to enum IPCMessageStart in
 // ipc/ipc_message_utils.h to include the corresponding message file.
 #include "chrome/browser/importer/profile_import_process_messages.h"
-#include "chrome/common/automation_messages.h"
 #include "chrome/common/common_message_generator.h"
 #include "chrome/common/nacl_messages.h"
 #include "content/common/content_message_generator.h"
+#include "content/common/pepper_messages.h"
 #include "ppapi/proxy/ppapi_messages.h"
