10e03366c6330e7af71e88b4f781817b2c254342
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-21 07:51:42 +0000

    Web Inspector: make extension tests pass on chromium https://bugs.webkit.org/show_bug.cgi?id=70334
    Reviewed by Pavel Feldman.
    
    LayoutTests:
    
    * http/tests/inspector/extensions-headers.html:
    * http/tests/inspector/resources/extension-main.js:
    ():
    * inspector/extensions/extensions-audits.html:
    * inspector/extensions/extensions-console.html:
    * inspector/extensions/extensions-resources.html:
    * platform/chromium/inspector/extensions/extensions-api-expected.txt: Added.
    * platform/chromium/inspector/extensions/extensions-eval-expected.txt: Added.
    * platform/chromium/test_expectations.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionAPI.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionAPI.js
@@ -716,5 +716,6 @@ function buildExtensionAPIInjectedScript(platformAPI)
         injectedExtensionAPI.toString() + ";" +
         "injectedExtensionAPI(injectedScriptId);" +
         (platformAPI || "") +
+        "return {};" +
         "})";
 }
