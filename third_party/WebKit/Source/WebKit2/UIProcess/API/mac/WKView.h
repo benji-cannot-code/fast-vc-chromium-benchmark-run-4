e9e127275bdf2ab4cecb642e7063e4f34974bb91
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-21 22:30:29 +0000

    Mark member variables of WebKit2 API @private https://bugs.webkit.org/show_bug.cgi?id=70655
    Reviewed by Simon Fraser.
    
    * UIProcess/API/mac/WKBrowsingContextController.h:
    * UIProcess/API/mac/WKView.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WK_EXPORT
 @interface WKView : NSView <NSTextInputClient> {
+@private
     WKViewData *_data;
     unsigned _unused;
 }
