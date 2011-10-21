cf132dc92f62156cf63168b248c9b9578197c451
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 01:20:31 +0000

    Move kFraudulentCertificateUploadEndpoint to its proper home in the .cc.
    BUG=99185
    Review URL: http://codereview.chromium.org/8317022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/chrome_fraudulent_certificate_reporter.cc
+++ chrome/browser/net/chrome_fraudulent_certificate_reporter.cc
@@ -18,10 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_browser_net {
 
+// TODO(palmer): Switch to HTTPS when the error handling delegate is more
+// sophisticated. Ultimately we plan to attempt the report on many transports.
+static const char kFraudulentCertificateUploadEndpoint[] =
+    "http://clients3.google.com/log_cert_error";
+
 ChromeFraudulentCertificateReporter::ChromeFraudulentCertificateReporter(
     net::URLRequestContext* request_context)
     : request_context_(request_context),
-      upload_url_(FRAUDULENT_CERTIFICATE_UPLOAD_ENDPOINT) {
+      upload_url_(kFraudulentCertificateUploadEndpoint) {
 }
 
 ChromeFraudulentCertificateReporter::~ChromeFraudulentCertificateReporter() {
--- chrome/browser/net/chrome_fraudulent_certificate_reporter.h
+++ chrome/browser/net/chrome_fraudulent_certificate_reporter.h
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_browser_net {
 
-// TODO(palmer): Switch to HTTPS when the error handling delegate is more
-// sophisticated. Ultimately we plan to attempt the report on many transports.
-const char FRAUDULENT_CERTIFICATE_UPLOAD_ENDPOINT[] =
-    "http://clients3.google.com/log_cert_error";
-
 class ChromeFraudulentCertificateReporter
     : public net::FraudulentCertificateReporter,
       public net::URLRequest::Delegate {
