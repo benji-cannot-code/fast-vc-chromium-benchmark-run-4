f37f31da6da29f502cc0a0ecdcc37ce5c0ce254d
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 00:09:22 +0000

    aura: Improve initial host window size.
    This adds a --aura-host-window-size switch (taking an
    argument of the form "1024x768") that can be used to set the
    initial size for the host window.
    
    It also makes the Linux implementation automatically size
    the window to cover the entire screen if an X window manager
    isn't present (i.e. we're running on a Chrome OS device).
    
    BUG=100979
    TEST=tried it on a chrome os device
    
    Review URL: http://codereview.chromium.org/8362002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106639 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/aura.gyp
+++ ui/aura/aura.gyp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'AURA_IMPLEMENTATION',
       ],
       'sources': [
+        'aura_switches.cc',
+        'aura_switches.h',
         'cursor.h',
         'desktop_host.h',
         'desktop_host_linux.cc',
---
+++ ui/aura/aura_switches.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_AURA_AURA_SWITCHES_H_
+#define UI_AURA_AURA_SWITCHES_H_
+#pragma once
+
+namespace switches {
+
+extern const char kAuraHostWindowSize[];
+
+}  // namespace switches
+
+#endif  // UI_AURA_AURA_SWITCHES_H_
