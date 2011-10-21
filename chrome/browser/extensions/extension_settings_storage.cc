e82649c079a52206144b77cc97de0201a77e5e41
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 02:12:06 +0000

    Add onChanged events to the extension settings API, both from sync and between split mode background pages.
    BUG=97545
    TEST=Updated ExtensionSettingsStorageUnittest, ExtensionSettingsSyncUnittest, ExtensionSettingsApitest
    
    
    Review URL: http://codereview.chromium.org/8177022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106660 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_settings_storage.cc
+++ chrome/browser/extensions/extension_settings_storage.cc
@@ -9,25 +9,55 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Implementation of ExtensionSettingsStorage::Result
 
 ExtensionSettingsStorage::Result::Result(
-    DictionaryValue* settings, std::set<std::string>* changed_keys)
-    : inner_(new Inner(settings, changed_keys, std::string())) {}
+    DictionaryValue* settings,
+    DictionaryValue* old_settings,
+    std::set<std::string>* changed_keys)
+    : inner_(new Inner(settings, old_settings, changed_keys, std::string())) {}
 
 ExtensionSettingsStorage::Result::Result(const std::string& error)
-    : inner_(new Inner(NULL, new std::set<std::string>(), error)) {
+    : inner_(new Inner(NULL, NULL, new std::set<std::string>(), error)) {
   DCHECK(!error.empty());
 }
 
 ExtensionSettingsStorage::Result::~Result() {}
 
-DictionaryValue* ExtensionSettingsStorage::Result::GetSettings() const {
+const DictionaryValue* ExtensionSettingsStorage::Result::GetSettings() const {
   DCHECK(!HasError());
   return inner_->settings_.get();
 }
 
-std::set<std::string>*
+const std::set<std::string>*
 ExtensionSettingsStorage::Result::GetChangedKeys() const {
   DCHECK(!HasError());
   return inner_->changed_keys_.get();
+}
+
+const Value* ExtensionSettingsStorage::Result::GetOldValue(
+    const std::string& key) const {
+  DCHECK(!HasError());
+  if (!inner_->changed_keys_.get()) {
+    return NULL;
+  }
+  Value* old_value = NULL;
+  if (inner_->changed_keys_->count(key)) {
+    inner_->old_settings_->GetWithoutPathExpansion(key, &old_value);
+  } else if (inner_->settings_.get()) {
+    inner_->settings_->GetWithoutPathExpansion(key, &old_value);
+  }
+  return old_value;
+}
+
+const Value* ExtensionSettingsStorage::Result::GetNewValue(
+    const std::string& key) const {
+  DCHECK(!HasError());
+  if (!inner_->changed_keys_.get()) {
+    return NULL;
+  }
+  Value* new_value = NULL;
+  if (inner_->settings_.get()) {
+    inner_->settings_->GetWithoutPathExpansion(key, &new_value);
+  }
+  return new_value;
 }
 
 bool ExtensionSettingsStorage::Result::HasError() const {
@@ -41,8 +71,12 @@ const std::string& ExtensionSettingsStorage::Result::GetError() const {
 
 ExtensionSettingsStorage::Result::Inner::Inner(
     DictionaryValue* settings,
+    DictionaryValue* old_settings,
     std::set<std::string>* changed_keys,
     const std::string& error)
-    : settings_(settings), changed_keys_(changed_keys), error_(error) {}
+    : settings_(settings),
+      old_settings_(old_settings),
+      changed_keys_(changed_keys),
+      error_(error) {}
 
 ExtensionSettingsStorage::Result::Inner::~Inner() {}
