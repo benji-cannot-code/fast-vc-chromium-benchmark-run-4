1caf5ea5767375d63afce0dfa2281c0d258a7001
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-21 12:43:45 +0000

    2011-10-21  Alejandro G. Castro  <alex@igalia.com>
            [cairo] Forward declaration of GraphicsContextState uses class instead of struct
            https://bugs.webkit.org/show_bug.cgi?id=70522
    
            The type is a struct not a class, replace the definition in the
            forward declaration.
    
            Reviewed by Martin Robinson.
    
            * platform/graphics/cairo/PlatformContextCairo.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class GraphicsContextState;
+struct GraphicsContextState;
 
 // Much like PlatformContextSkia in the Skia port, this class holds information that
 // would normally be private to GraphicsContext, except that we want to allow access
