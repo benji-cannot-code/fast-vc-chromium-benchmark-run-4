79b475716174d0bb0a06d2e8c647bb869637d727
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 18:31:39 +0000

    Add Content-Security-Policy to chrome://keyboardoverlay page, and remove it from the whitelist of entry point not yet converted. Review URL: http://codereview.chromium.org/8362020
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/keyboard_overlay.js
+++ chrome/browser/resources/keyboard_overlay.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+<include src="keyboard_overlay_data.js"/>
+<include src="keyboard_overlay_accessibility_helper.js"/>
+
 var BASE_KEYBOARD = {
   top: 0,
   left: 0,
--- chrome/browser/ui/webui/chrome_url_data_manager_backend.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_backend.cc
@@ -63,7 +63,6 @@ class ChromeURLContentSecurityPolicyExceptionSet
     insert(chrome::kChromeUINewTabHost);
 #if defined(OS_CHROMEOS)
     insert(chrome::kChromeUIEnterpriseEnrollmentHost);
-    insert(chrome::kChromeUIKeyboardOverlayHost);
     insert(chrome::kChromeUIOobeHost);
     insert(chrome::kChromeUIMobileSetupHost);
     insert(chrome::kChromeUIProxySettingsHost);
