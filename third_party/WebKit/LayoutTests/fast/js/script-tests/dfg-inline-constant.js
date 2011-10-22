1bde12e2983ef2059465918ef1cccb9877701f79
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-22 03:58:55 +0000

    DFG inlining sometimes fails to reset constant references https://bugs.webkit.org/show_bug.cgi?id=70668
    Source/JavaScriptCore: 
    
    Reviewed by Anders Carlsson.
            
    Reset constant references when we need to (new block created) and not
    when we don't (change of inlining depth).
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::handleInlining):
    (JSC::DFG::ByteCodeParser::prepareToParseBlock):
    (JSC::DFG::ByteCodeParser::parseBlock):
    (JSC::DFG::ByteCodeParser::parseCodeBlock):
    
    LayoutTests: 
    
    Reviewed by Anders Carlsson.
            
    Added a new test that covers this specific bug as well as the general
    ability to perform inlining, and the ability to OSR out of an inline
    stack.
    
    * fast/js/dfg-inline-constant-expected.txt: Added.
    * fast/js/dfg-inline-constant.html: Added.
    * fast/js/script-tests/dfg-inline-constant.js: Added.
    (foo):
    (bar):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-inline-constant.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that function inlining in the DFG JIT doesn't get confused by constants being reused between inliner and inlinee."
+);
+
+function foo(a, b) {
+    if (b)
+        return a + 4;
+    return b + 5;
+}
+
+function bar(a, b) {
+    return foo(a, b) + 5;
+}
+
+for (var i = 0; i < 1000; ++i)
+    bar(i, i + 1);
+
+shouldBe("bar(6, 0)", "10");
+shouldBe("bar(6, 1)", "15");
+shouldBe("bar(6, false)", "10");
+shouldBe("bar(6, true)", "15");
+
+var successfullyParsed = true;
