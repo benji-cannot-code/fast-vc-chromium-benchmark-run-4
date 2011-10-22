2b14468440d187b9f98655c2a5511ed9aa5b272b
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-22 03:25:07 +0000

    Fix crash below CoreIPC::MessageSender<WebKit::WebPage>::send<Messages::WebPageProxy::DidPerformDictionaryLookup> https://bugs.webkit.org/show_bug.cgi?id=70667 <rdar://problem/9622186>
    Reviewed by Anders Carlsson.
    
    * Shared/DictionaryPopupInfo.cpp:
    (WebKit::DictionaryPopupInfo::encode):
    (WebKit::DictionaryPopupInfo::decode):
    Account for the fact that the options dictionary can be null. This is due to Lookup passing
    back a null, rather than empty, dictionary when it could not find any interesting characteristics.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98186 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/DictionaryPopupInfo.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/DictionaryPopupInfo.cpp
@@ -42,7 +42,10 @@ void DictionaryPopupInfo::encode(CoreIPC::ArgumentEncoder* encoder) const
     encoder->encodeEnum(type);
 
 #if PLATFORM(MAC) && !defined(BUILDING_ON_SNOW_LEOPARD)
-    CoreIPC::encode(encoder, options.get());
+    bool hadOptions = options;
+    encoder->encodeBool(hadOptions);
+    if (hadOptions)
+        CoreIPC::encode(encoder, options.get());
 #endif
 }
 
@@ -55,8 +58,13 @@ bool DictionaryPopupInfo::decode(CoreIPC::ArgumentDecoder* decoder, DictionaryPo
     if (!decoder->decodeEnum(result.type))
         return false;
 #if PLATFORM(MAC) && !defined(BUILDING_ON_SNOW_LEOPARD)
-    if (!CoreIPC::decode(decoder, result.options))
+    bool hadOptions;
+    if (!decoder->decodeBool(hadOptions))
         return false;
+    if (hadOptions) {
+        if (!CoreIPC::decode(decoder, result.options))
+            return false;
+    }
 #endif
     return true;
 }
