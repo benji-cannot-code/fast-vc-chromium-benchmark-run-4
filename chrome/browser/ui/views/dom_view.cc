9aea67bd2e5fae48b33455952ef487a575fd949a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-22 21:36:05 +0000

    aura: Move the keyboard correctly in the proper container.
    BUG=none
    TEST=manually
    
    Review URL: http://codereview.chromium.org/8375018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/dom_view.cc
+++ chrome/browser/ui/views/dom_view.cc
@@ -81,7 +81,7 @@ void DOMView::ViewHierarchyChanged(bool is_add, views::View* parent,
 }
 
 void DOMView::AttachTabContents() {
-#if !defined(USE_AURA)
+#if defined(TOUCH_UI) || !defined(USE_AURA)
   if (views::Widget::IsPureViews()) {
     TabContentsViewViews* widget = static_cast<TabContentsViewViews*>(
         dom_contents_->tab_contents()->view());
@@ -91,7 +91,7 @@ void DOMView::AttachTabContents() {
   } else {
 #endif
     Attach(dom_contents_->tab_contents()->GetNativeView());
-#if !defined(USE_AURA)
+#if defined(TOUCH_UI) || !defined(USE_AURA)
   }
 #endif
 }
