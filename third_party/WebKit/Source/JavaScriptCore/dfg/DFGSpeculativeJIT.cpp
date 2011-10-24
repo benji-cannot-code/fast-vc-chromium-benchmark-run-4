d0e61d0bec324ccc236bc5ea49440fbf81e469f8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-24 22:51:25 +0000

    Add boolean speculations to DFG JIT 32_64 https://bugs.webkit.org/show_bug.cgi?id=70706
    Patch by Yuqiang Xian <yuqiang.xian@intel.com> on 2011-10-24
    Reviewed by Filip Pizlo.
    
    Different from the boolean speculations in DFG 64, the boolean
    speculations in DFG 32_64 will use a 32bit GPR to hold the primitive
    boolean instead of a JSBoolean. This choice is not only for
    performance, but also to save a register as we're short of registers on
    X86.
    To accomplish this we make use of DataFormatBoolean, allow a value to
    be represented as a primitive boolean and converted from/to a
    JSBoolean.
    This patch also fixes SpillOrder in 32_64, which should be different
    from 64, and fixes needDataFormatConversion logic in 32_64.
    
    * assembler/MacroAssemblerX86Common.h:
    (JSC::MacroAssemblerX86Common::branchTest32):
        We don't expect byte test actually as it doesn't work for registers
        esp..edi on X86.
    * dfg/DFGGenerationInfo.h:
    (JSC::DFG::needDataFormatConversion):
    (JSC::DFG::GenerationInfo::initBoolean):
    (JSC::DFG::GenerationInfo::gpr):
    (JSC::DFG::GenerationInfo::fillInteger):
    (JSC::DFG::GenerationInfo::fillBoolean):
    * dfg/DFGJITCodeGenerator.cpp:
    (JSC::DFG::JITCodeGenerator::checkConsistency):
    * dfg/DFGJITCodeGenerator.h:
    (JSC::DFG::JITCodeGenerator::use):
    (JSC::DFG::JITCodeGenerator::silentSpillGPR):
    (JSC::DFG::JITCodeGenerator::silentFillGPR):
    (JSC::DFG::JITCodeGenerator::spill):
    (JSC::DFG::cellResult):
    (JSC::DFG::booleanResult):
    * dfg/DFGJITCodeGenerator32_64.cpp:
    (JSC::DFG::JITCodeGenerator::fillJSValue):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeCompareNull):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeCompare):
    (JSC::DFG::JITCodeGenerator::nonSpeculativeNonPeepholeStrictEq):
    * dfg/DFGJITCompiler32_64.cpp:
    (JSC::DFG::JITCompiler::exitSpeculativeWithOSR):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::ValueSource::dump):
    (JSC::DFG::ValueRecovery::dump):
    (JSC::DFG::SpeculativeJIT::checkArgumentTypes):
    (JSC::DFG::SpeculativeJIT::computeValueRecoveryFor):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::ValueSource::forPrediction):
    (JSC::DFG::ValueRecovery::alreadyInRegisterFileAsUnboxedBoolean):
    (JSC::DFG::ValueRecovery::inGPR):
    (JSC::DFG::ValueRecovery::gpr):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::fillSpeculateIntInternal):
    (JSC::DFG::SpeculativeJIT::fillSpeculateDouble):
    (JSC::DFG::SpeculativeJIT::fillSpeculateBoolean):
    (JSC::DFG::SpeculativeJIT::compileObjectEquality):
    (JSC::DFG::SpeculativeJIT::compare):
    (JSC::DFG::SpeculativeJIT::compileObjectOrOtherLogicalNot):
    (JSC::DFG::SpeculativeJIT::compileLogicalNot):
    (JSC::DFG::SpeculativeJIT::emitBranch):
    (JSC::DFG::SpeculativeJIT::compile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -53,6 +53,9 @@ void ValueSource::dump(FILE* out) const
     case CellInRegisterFile:
         fprintf(out, "Cell");
         break;
+    case BooleanInRegisterFile:
+        fprintf(out, "Bool");
+        break;
     case HaveNode:
         fprintf(out, "Node(%d)", m_nodeIndex);
         break;
@@ -71,11 +74,17 @@ void ValueRecovery::dump(FILE* out) const
     case AlreadyInRegisterFileAsUnboxedCell:
         fprintf(out, "(cell)");
         break;
+    case AlreadyInRegisterFileAsUnboxedBoolean:
+        fprintf(out, "(bool)");
+        break;
     case InGPR:
         fprintf(out, "%%%s", GPRInfo::debugName(gpr()));
         break;
     case UnboxedInt32InGPR:
         fprintf(out, "int32(%%%s)", GPRInfo::debugName(gpr()));
+        break;
+    case UnboxedBooleanInGPR:
+        fprintf(out, "bool(%%%s)", GPRInfo::debugName(gpr()));
         break;
     case InFPR:
         fprintf(out, "%%%s", FPRInfo::debugName(fpr()));
@@ -416,7 +425,8 @@ void SpeculativeJIT::checkArgumentTypes()
             speculationCheck(m_jit.branch32(MacroAssembler::NotEqual, temp.gpr(), TrustedImm32(JSValue::CellTag)));
             m_jit.load32(JITCompiler::payloadFor(virtualRegister), temp.gpr());
             speculationCheck(m_jit.branchPtr(MacroAssembler::NotEqual, MacroAssembler::Address(temp.gpr()), MacroAssembler::TrustedImmPtr(m_jit.globalData()->jsByteArrayVPtr)));
-        } // FIXME: need boolean predictions, but we currently don't have that support.
+        } else if (isBooleanPrediction(predictedType))
+            speculationCheck(m_jit.branch32(MacroAssembler::NotEqual, JITCompiler::tagFor(virtualRegister), TrustedImm32(JSValue::BooleanTag)));
 #endif
     }
 }
@@ -443,6 +453,9 @@ ValueRecovery SpeculativeJIT::computeValueRecoveryFor(const ValueSource& valueSo
 
     case CellInRegisterFile:
         return ValueRecovery::alreadyInRegisterFileAsUnboxedCell();
+
+    case BooleanInRegisterFile:
+        return ValueRecovery::alreadyInRegisterFileAsUnboxedBoolean();
 
     case HaveNode: {
         if (m_jit.isConstant(valueSource.nodeIndex()))
--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
@@ -73,6 +73,7 @@ enum ValueSourceKind {
     ValueInRegisterFile,
     Int32InRegisterFile,
     CellInRegisterFile,
+    BooleanInRegisterFile,
     HaveNode
 };
 
@@ -102,6 +103,8 @@ public:
             return ValueSource(Int32InRegisterFile);
         if (isArrayPrediction(prediction) || isByteArrayPrediction(prediction))
             return ValueSource(CellInRegisterFile);
+        if (isBooleanPrediction(prediction))
+            return ValueSource(BooleanInRegisterFile);
         return ValueSource(ValueInRegisterFile);
     }
     
@@ -151,9 +154,11 @@ enum ValueRecoveryTechnique {
     // It's already in the register file but unboxed.
     AlreadyInRegisterFileAsUnboxedInt32,
     AlreadyInRegisterFileAsUnboxedCell,
+    AlreadyInRegisterFileAsUnboxedBoolean,
     // It's in a register.
     InGPR,
     UnboxedInt32InGPR,
+    UnboxedBooleanInGPR,
 #if USE(JSVALUE32_64)
     InPair,
 #endif
@@ -197,15 +202,24 @@ public:
         return result;
     }
     
+    static ValueRecovery alreadyInRegisterFileAsUnboxedBoolean()
+    {
+        ValueRecovery result;
+        result.m_technique = AlreadyInRegisterFileAsUnboxedBoolean;
+        return result;
+    }
+    
     static ValueRecovery inGPR(GPRReg gpr, DataFormat dataFormat)
     {
         ASSERT(dataFormat != DataFormatNone);
 #if USE(JSVALUE32_64)
-        ASSERT(dataFormat == DataFormatInteger || dataFormat == DataFormatCell);
+        ASSERT(dataFormat == DataFormatInteger || dataFormat == DataFormatCell || dataFormat == DataFormatBoolean);
 #endif
         ValueRecovery result;
         if (dataFormat == DataFormatInteger)
             result.m_technique = UnboxedInt32InGPR;
+        else if (dataFormat == DataFormatBoolean)
+            result.m_technique = UnboxedBooleanInGPR;
         else
             result.m_technique = InGPR;
         result.m_source.gpr = gpr;
@@ -264,7 +278,7 @@ public:
     
     GPRReg gpr() const
     {
-        ASSERT(m_technique == InGPR || m_technique == UnboxedInt32InGPR);
+        ASSERT(m_technique == InGPR || m_technique == UnboxedInt32InGPR || m_technique == UnboxedBooleanInGPR);
         return m_source.gpr;
     }
     
