9fafc677a43c7cb487a24fada4b7085d976dfda0
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-24 19:58:10 +0000

    Optionally show component extensions in options.
    Add a new switch (--show-component-extension-options) that causes
    the built-in component extensions to be displayed on
    chrome://settings/extensions.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8332006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106960 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/extension_settings_handler.cc
+++ chrome/browser/ui/webui/options/extension_settings_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
@@ -24,10 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_contents/background_contents.h"
 #include "chrome/browser/ui/webui/extension_icon_source.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/common/chrome_view_types.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/common/chrome_view_types.h"
 #include "content/browser/browsing_instance.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/browser/tab_contents/tab_contents.h"
@@ -50,7 +52,9 @@ bool ShouldShowExtension(const Extension* extension) {
 
   // Don't show component extensions because they are only extensions as an
   // implementation detail of Chrome.
-  if (extension->location() == Extension::COMPONENT)
+  if (extension->location() == Extension::COMPONENT &&
+      !CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kShowComponentExtensionOptions))
     return false;
 
   // Always show unpacked extensions and apps.
