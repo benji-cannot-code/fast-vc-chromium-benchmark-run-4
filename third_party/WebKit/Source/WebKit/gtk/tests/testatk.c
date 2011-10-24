d5c933caf54d461136a1c84360dfb3ed386279a7
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-24 14:56:39 +0000

    [GTK] Remove g_thread_init() calls https://bugs.webkit.org/show_bug.cgi?id=70717
    Reviewed by Xan Lopez.
    
    With newer glib it's not needed anymore and with previous versions
    it was already called by g_type_init().
    
    Source/WebKit/gtk:
    
    * tests/testapplicationcache.c:
    (main):
    * tests/testatk.c:
    (main):
    * tests/testatkroles.c:
    (main):
    * tests/testcopyandpaste.c:
    (main):
    * tests/testdomdocument.c:
    (main):
    * tests/testdomdomwindow.c:
    (main):
    * tests/testdomnode.c:
    (main):
    * tests/testdownload.c:
    (main):
    * tests/testglobals.c:
    (main):
    * tests/testhittestresult.c:
    (main):
    * tests/testhttpbackend.c:
    (main):
    * tests/testkeyevents.c:
    (main):
    * tests/testloading.c:
    (main):
    * tests/testmimehandling.c:
    (main):
    * tests/testnetworkrequest.c:
    (main):
    * tests/testnetworkresponse.c:
    (main):
    * tests/testwebbackforwardlist.c:
    (main):
    * tests/testwebdatasource.c:
    (main):
    * tests/testwebframe.c:
    (main):
    * tests/testwebhistoryitem.c:
    (main):
    * tests/testwebplugindatabase.c:
    (main):
    * tests/testwebresource.c:
    (main):
    * tests/testwebsettings.c:
    (main):
    * tests/testwebview.c:
    * tests/testwindow.c:
    (main):
    
    Source/WebKit2:
    
    * UIProcess/API/gtk/tests/TestMain.cpp:
    (main):
    
    Tools:
    
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (main):
    * GtkLauncher/main.c:
    (main):
    * MiniBrowser/gtk/main.c:
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/tests/testatk.c
+++ third_party/WebKit/Source/WebKit/gtk/tests/testatk.c
@@ -1661,7 +1661,6 @@ static void testWebkitAtkTextChangedNotifications()
 
 int main(int argc, char** argv)
 {
-    g_thread_init(0);
     gtk_test_init(&argc, &argv, 0);
 
     g_test_bug_base("https://bugs.webkit.org/");
