d978fe863438d4edc07344ac28033bae625869f9
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-24 17:03:52 +0000

    Move infobar extension API implementation to infobars dir.
    We're moving all the extension api implementations out of
    browser/extensions into the directory with the code they are
    automating.
    
    BUG=101244
    
    
    Review URL: http://codereview.chromium.org/8374024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/infobars/infobar_extension_api.h
+++ chrome/browser/infobars/infobar_extension_api.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_INFOBAR_MODULE_H_
-#define CHROME_BROWSER_EXTENSIONS_EXTENSION_INFOBAR_MODULE_H_
+#ifndef CHROME_BROWSER_INFOBARS_INFOBAR_EXTENSION_API_H_
+#define CHROME_BROWSER_INFOBARS_INFOBAR_EXTENSION_API_H_
 #pragma once
 
 #include "chrome/browser/extensions/extension_function.h"
@@ -15,4 +15,4 @@ class ShowInfoBarFunction : public SyncExtensionFunction {
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.infobars.show")
 };
 
-#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_INFOBAR_MODULE_H_
+#endif  // CHROME_BROWSER_INFOBARS_INFOBAR_EXTENSION_API_H_
