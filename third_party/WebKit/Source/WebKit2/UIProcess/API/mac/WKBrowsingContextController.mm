770b4557ed98be6f054098d3b5c64f41e6994916
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-24 19:55:26 +0000

    Add URL getters for the WKBrowsingContextController https://bugs.webkit.org/show_bug.cgi?id=70752
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/mac/WKBrowsingContextController.h:
    * UIProcess/API/mac/WKBrowsingContextController.mm:
    (autoreleased):
    Added helpers for autoreleasing.
    
    (-[WKBrowsingContextController activeURL]):
    Added buy not implemented yet.
    
    (-[WKBrowsingContextController pendingURL]):
    (-[WKBrowsingContextController provisionalURL]):
    (-[WKBrowsingContextController commitedURL]):
    (-[WKBrowsingContextController unreachableURL]):
    Added getters.
    
    (-[WKBrowsingContextController title]):
    Convert to new autoreleased helper.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKBrowsingContextController.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKBrowsingContextController.mm
@@ -28,11 +28,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WKBrowsingContextController.h"
 #import "WKBrowsingContextControllerInternal.h"
 
+#import "WKFrame.h"
 #import "WKPage.h"
 #import "WKRetainPtr.h"
+#import "WKStringCF.h"
+#import "WKURLCF.h"
 #import "WKURLRequest.h"
 #import "WKURLRequestNS.h"
-#import "WKStringCF.h"
+
+
+static inline NSString *autoreleased(WKStringRef string)
+{
+    WKRetainPtr<WKStringRef> wkString = adoptWK(string);
+    return [(NSString *)WKStringCopyCFString(kCFAllocatorDefault, wkString.get()) autorelease];
+}
+
+static inline NSURL *autoreleased(WKURLRef url)
+{
+    WKRetainPtr<WKURLRef> wkURL = adoptWK(url);
+    return [(NSURL *)WKURLCopyCFURL(kCFAllocatorDefault, wkURL.get()) autorelease];
+}
+
 
 @interface WKBrowsingContextControllerData : NSObject {
 @public
@@ -106,12 +122,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return WKPageCanGoBack(self.pageRef);
 }
 
+
+#pragma mark Active Load Introspection
+
+- (NSURL *)activeURL
+{
+    /* FIXME: Implement. */
+    return nil;
+}
+
+- (NSURL *)pendingURL
+{
+    return autoreleased(WKPageCopyPendingAPIRequestURL(self.pageRef));
+}
+
+- (NSURL *)provisionalURL
+{
+    return autoreleased(WKFrameCopyProvisionalURL(WKPageGetMainFrame(self.pageRef)));
+}
+
+- (NSURL *)commitedURL
+{
+    return autoreleased(WKFrameCopyURL(WKPageGetMainFrame(self.pageRef)));
+}
+
+- (NSURL *)unreachableURL
+{
+    return autoreleased(WKFrameCopyUnreachableURL(WKPageGetMainFrame(self.pageRef)));
+}
+
+
 #pragma mark Active Document Introspection
 
 - (NSString *)title
 {
-    WKRetainPtr<WKStringRef> wkTitle = adoptWK(WKPageCopyTitle(self.pageRef));
-    return [(NSString *)WKStringCopyCFString(kCFAllocatorDefault, wkTitle.get()) autorelease];
+    return autoreleased(WKPageCopyTitle(self.pageRef));
 }
 
 #pragma mark Zoom
