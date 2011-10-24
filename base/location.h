c558672b6354fcf77fde90fc659348c21ffcb274
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-24 18:55:16 +0000

    Support JSON encoding of data for about:tracking information
    I also fought a terrible (but educational) fight with Thread Local Store, and its ability to do cleanup (call destructors) at thread exit (notably applicable to Worker Threads). Thta is why there were soooo many test bot runs and tiny checkins. I now have a plan in mind that won't rely on that functionality.  The code seems to work cross-platform, but if I have trouble with Linux, I'll repeatedly leak ThreadData contexts temporarily on that platform.  Given that the code is only enabled under Debug, this is not yet a real problem.
    
    Each CL I write for this code also includes a bunch of cleanup.  In this case, I changed the Write() methods to WriteHTML(), since I didn't want any confusion with JSON writing etc.  I also did a bunch of tiny cleanups which should not have  changed what the code does.
    
    r=rtenneti
    Review URL: http://codereview.chromium.org/8313013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106952 0039d316-1c4b-4281-b951-d872f2087c98

--- base/location.h
+++ base/location.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/base_export.h"
+#include "base/values.h"
 
 #ifndef NDEBUG
 #ifndef TRACK_ALL_TASK_OBJECTS
@@ -54,11 +55,19 @@ class BASE_EXPORT Location {
 
   std::string ToString() const;
 
+  // Translate the some of the state in this instance into a human readable
+  // string with HTML characters in the function names escaped, and append that
+  // string to |output|.  Inclusion of the file_name_ and function_name_ are
+  // optional, and controlled by the boolean arguments.
   void Write(bool display_filename, bool display_function_name,
              std::string* output) const;
 
   // Write function_name_ in HTML with '<' and '>' properly encoded.
   void WriteFunctionName(std::string* output) const;
+
+  // Construct a Value* representation.  The caller assumes ownership of the
+  // memory in the returned instance.
+  base::DictionaryValue* ToValue() const;
 
  private:
   const char* function_name_;
