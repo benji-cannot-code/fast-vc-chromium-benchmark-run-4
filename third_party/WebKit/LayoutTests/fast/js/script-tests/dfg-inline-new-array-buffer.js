a64ff88404c3f04f275b2fa842d03f2bcf8b7a77
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-24 23:22:39 +0000

    op_new_array_buffer is not inlined correctly https://bugs.webkit.org/show_bug.cgi?id=70770
    Source/JavaScriptCore: 
    
    Reviewed by Oliver Hunt.
            
    Disabled inlining of op_new_array_buffer, for now.
    
    * dfg/DFGCapabilities.h:
    (JSC::DFG::canInlineOpcode):
    
    LayoutTests: 
    
    Reviewed by Oliver Hunt.
    
    * fast/js/dfg-inline-new-array-buffer-expected.txt: Added.
    * fast/js/dfg-inline-new-array-buffer.html: Added.
    * fast/js/script-tests/dfg-inline-new-array-buffer.js: Added.
    (foo):
    (bar):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-inline-new-array-buffer.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that inlining correctly handles constant buffers."
+);
+
+function foo() {
+    return [1, 2, 3, 4];
+}
+
+function bar() {
+    return foo();
+}
+
+for (var i = 0; i < 1000; ++i) {
+    bar();
+}
+
+for (var i = 0; i < 10; ++i) {
+    shouldBe("bar()[0]", "1")
+    shouldBe("bar()[1]", "2")
+    shouldBe("bar()[2]", "3")
+    shouldBe("bar()[3]", "4")
+}
+
+var successfullyParsed = true;
