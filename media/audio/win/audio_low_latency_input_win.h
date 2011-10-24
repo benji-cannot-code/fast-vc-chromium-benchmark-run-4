ec4fc578a4aa25abbcda107d9c76b907428f29eb
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-24 10:26:47 +0000

    Fixes yet another build error on Windows shared bot.
    TBR=tommi
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106901 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_low_latency_input_win.h
+++ media/audio/win/audio_low_latency_input_win.h
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <MMDeviceAPI.h>
 
 #include "base/compiler_specific.h"
-#include "base/media_export.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/simple_thread.h"
 #include "base/win/scoped_co_mem.h"
@@ -67,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_handle.h"
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_parameters.h"
+#include "media/base/media_export.h"
 
 class AudioManagerWin;
 
