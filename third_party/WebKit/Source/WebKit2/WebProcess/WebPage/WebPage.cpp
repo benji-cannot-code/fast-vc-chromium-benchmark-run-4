4003c9557fd1f4af052f177341c80a97632d30b0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 08:19:54 +0000

    [WK2]Critical warning while building WebKit2 GTK+ https://bugs.webkit.org/show_bug.cgi?id=70591
    Patch by Vamshikrishna Yellenki <vamshi@motorola.com> on 2011-10-25
    Reviewed by Martin Robinson.
    
    * WebProcess/WebPage/WebPage.cpp: Included DataObjectGtk.h to avoid warnings for GTK
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -129,6 +129,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QMimeData>
 #endif
 
+#if PLATFORM(GTK)
+#include "DataObjectGtk.h"
+#endif
+
 #ifndef NDEBUG
 #include <wtf/RefCountedLeakCounter.h>
 #endif
