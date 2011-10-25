429594395674e4119a17add6c4ecb4546a9deb35
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 21:23:00 +0000

    Fix a notreached assertion and linker error in RWHVA
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8394009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107207 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/win/events_win.cc
+++ ui/base/win/events_win.cc
@@ -184,7 +184,9 @@ EventType EventTypeFromNative(const base::NativeEvent& native_event) {
     case WM_NCMOUSELEAVE:
       return ET_MOUSE_EXITED;
     default:
-      NOTREACHED();
+      // We can't NOTREACHED() here, since this function can be called for any
+      // message.
+      break;
   }
   return ET_UNKNOWN;
 }
