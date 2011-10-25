454c56d49eb13b68ce88dd0e8862d4aba167277e
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 02:12:09 +0000

    Replace most LOG/CHECK statements with DLOG/DCHECK statements in base.
    I tried hard not to change CHECKs that had side effects. I kept fatal checks
    that seemed security or debugging-info (in crash reports) sensitive, and ones
    that seems particularly well-conceived.
    Review URL: http://codereview.chromium.org/8368009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107042 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.h
+++ base/file_util.h
@@ -427,7 +427,7 @@ class ScopedFDClose {
   inline void operator()(int* x) const {
     if (x && *x >= 0) {
       if (HANDLE_EINTR(close(*x)) < 0)
-        PLOG(ERROR) << "close";
+        DPLOG(ERROR) << "close";
     }
   }
 };
--- base/scoped_temp_dir.cc
+++ base/scoped_temp_dir.cc
@@ -13,7 +13,7 @@ ScopedTempDir::ScopedTempDir() {
 
 ScopedTempDir::~ScopedTempDir() {
   if (!path_.empty() && !Delete())
-    LOG(WARNING) << "Could not delete temp dir in dtor.";
+    DLOG(WARNING) << "Could not delete temp dir in dtor.";
 }
 
 bool ScopedTempDir::CreateUniqueTempDir() {
@@ -68,7 +68,7 @@ bool ScopedTempDir::Delete() {
     // We only clear the path if deleted the directory.
     path_.clear();
   } else {
-    LOG(ERROR) << "ScopedTempDir unable to delete " << path_.value();
+    DLOG(ERROR) << "ScopedTempDir unable to delete " << path_.value();
   }
 
   return ret;
