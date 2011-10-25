e5bfafed84edf8b34d235daa322ca3cf0dcf78f6
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 22:52:24 +0000

    Aura: Non-client frame view approximately like the mocks
    + Adds Aura-look window close and maximize buttons.
    + Adds a transparent window background at the caption, visible on mouse over.
    + Adds transparent resize handles on window edges, also visible on mouse over.
    
    BUG=97254
    TEST=manual
    
    Review URL: http://codereview.chromium.org/8366014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107220 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_aura.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/frame/browser_non_client_frame_view_aura.h"
+
+#include "chrome/browser/ui/panels/panel_browser_frame_view.h"
+#include "chrome/browser/ui/panels/panel_browser_view.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+
+namespace browser {
+
+BrowserNonClientFrameView* CreateBrowserNonClientFrameView(
+    BrowserFrame* frame, BrowserView* browser_view) {
+  if (browser_view->IsBrowserTypePanel()) {
+    return new PanelBrowserFrameView(
+        frame, static_cast<PanelBrowserView*>(browser_view));
+  }
+  return new BrowserNonClientFrameViewAura(frame, browser_view);
+}
+
+}  // namespace browser
