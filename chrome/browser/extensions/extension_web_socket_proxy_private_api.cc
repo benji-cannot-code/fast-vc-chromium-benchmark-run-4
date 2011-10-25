24b577d032cc7afb0195857aac19ac401bf098fb
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 16:04:43 +0000

    websocket-to-TCP proxy: observe value of listening port in right place.
    Also minor change for new API: increase timeout for waiting WEB_SOCKET_PROXY_STARTED notification.  New API (GetURLForTCP as opposed to GetPassportForTCP) runs on ephemeral port so if port is not known then we cannot construct URL at all.  3 seconds may be too little to launch proxy on slow overloaded machine.
    
    BUG=chromium-os:21942
    TEST=Manual+Apitest
    
    Review URL: http://codereview.chromium.org/8362027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107125 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_web_socket_proxy_private_api.cc
+++ chrome/browser/extensions/extension_web_socket_proxy_private_api.cc
@@ -42,7 +42,7 @@ void WebSocketProxyPrivate::Finalize() {
   if (is_finalized_)
     return;
   is_finalized_ = true;
-  SendResponse(true);
+  SendResponse(listening_port_ > 0);
   Release();
 }
 
@@ -60,10 +60,14 @@ void WebSocketProxyPrivateGetURLForTCPFunction::Observe(
 }
 
 void WebSocketProxyPrivateGetURLForTCPFunction::Finalize() {
+#if defined(OS_CHROMEOS)
   StringValue* url = Value::CreateStringValue(std::string(
       "ws://127.0.0.1:" + base::IntToString(listening_port_) +
       "/tcpproxy?" + query_));
   result_.reset(url);
+  if (listening_port_ < 1)
+    listening_port_ = chromeos::WebSocketProxyController::GetPort();
+#endif
   WebSocketProxyPrivate::Finalize();
 }
 
@@ -81,12 +85,12 @@ bool WebSocketProxyPrivateGetPassportForTCPFunction::RunImpl() {
   if (chromeos::WebSocketProxyController::CheckCredentials(
           extension_id(), hostname, port,
           chromeos::WebSocketProxyController::PLAIN_TCP)) {
-    if (!chromeos::WebSocketProxyController::IsInitiated()) {
+    listening_port_ = chromeos::WebSocketProxyController::GetPort();
+    if (listening_port_ < 1) {
       delay_response = true;
       registrar_.Add(
           this, chrome::NOTIFICATION_WEB_SOCKET_PROXY_STARTED,
           content::NotificationService::AllSources());
-      chromeos::WebSocketProxyController::Initiate();
     }
 
     std::map<std::string, std::string> map;
@@ -133,12 +137,12 @@ bool WebSocketProxyPrivateGetURLForTCPFunction::RunImpl() {
           extension_id(), hostname, port,
           do_tls ? chromeos::WebSocketProxyController::TLS_OVER_TCP :
               chromeos::WebSocketProxyController::PLAIN_TCP)) {
-    if (!chromeos::WebSocketProxyController::IsInitiated()) {
+    listening_port_ = chromeos::WebSocketProxyController::GetPort();
+    if (listening_port_ < 1) {
       delay_response = true;
       registrar_.Add(
           this, chrome::NOTIFICATION_WEB_SOCKET_PROXY_STARTED,
           content::NotificationService::AllSources());
-      chromeos::WebSocketProxyController::Initiate();
     }
 
     std::map<std::string, std::string> map;
@@ -156,7 +160,7 @@ bool WebSocketProxyPrivateGetURLForTCPFunction::RunImpl() {
 #endif  // defined(OS_CHROMEOS)
 
   if (delay_response) {
-    const int kTimeout = 3;
+    const int kTimeout = 12;
     timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(kTimeout),
         this, &WebSocketProxyPrivate::Finalize);
   } else {
--- chrome/browser/extensions/extension_web_socket_proxy_private_api.h
+++ chrome/browser/extensions/extension_web_socket_proxy_private_api.h
@@ -36,7 +36,7 @@ class WebSocketProxyPrivate
 
   content::NotificationRegistrar registrar_;
 
-  // Proxy listens incoming websocket connection on this port.
+  // Proxy accepts websocket connections on this port.
   int listening_port_;
 };
 
