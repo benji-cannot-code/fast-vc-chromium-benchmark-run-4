f5732b4eb463f57d8f2fe72ae504926058278ddd
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 19:26:04 +0000

    avoid unnecessary layouts of flex items during the flex pass https://bugs.webkit.org/show_bug.cgi?id=70557
    Reviewed by Ojan Vafai.
    
    If the preferred size of a flex item is provided, we don't need to
    layout the flex item when computing the preferred size.  This allows
    us to only call layout on each flex item once in the common case.
    
    No new tests, covered by existing tests.
    
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::flowAwareLogicalWidthLengthForChild):
    (WebCore::RenderFlexibleBox::preferredLogicalContentWidthForFlexItem):
    (WebCore::RenderFlexibleBox::computePreferredLogicalWidth):
    (WebCore::RenderFlexibleBox::layoutAndPlaceChildrenInlineDirection):
    * rendering/RenderFlexibleBox.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.h
@@ -59,6 +59,7 @@ private:
     bool isHorizontalFlow() const;
     bool isLeftToRightFlow() const;
     bool isFlowAwareLogicalHeightAuto() const;
+    Length flowAwareLogicalWidthLengthForChild(RenderBox* child) const;
     void setFlowAwareLogicalHeight(LayoutUnit);
     LayoutUnit flowAwareLogicalHeightForChild(RenderBox* child);
     LayoutUnit flowAwareLogicalWidthForChild(RenderBox* child);
