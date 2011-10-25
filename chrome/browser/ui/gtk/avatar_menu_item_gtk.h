1c62cca216baf1e75c20b747464a1fbedcb0a61d
ncj674@motorola.com <ncj674@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 18:08:49 +0000

    Gtk:AvatarMenu:Destroy AvatarMenuBubble object properly when bubble closes.
    BUG=101243
    TEST=As described in the bug.
    
    Review URL: http://codereview.chromium.org/8370021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/avatar_menu_item_gtk.h
+++ chrome/browser/ui/gtk/avatar_menu_item_gtk.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <gtk/gtk.h>
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/avatar_menu_model.h"
 #include "ui/base/gtk/gtk_signal.h"
 #include "ui/base/gtk/owned_widget_gtk.h"
@@ -36,6 +37,9 @@ class AvatarMenuItemGtk {
                     size_t item_index,
                     GtkThemeService* theme_service);
   virtual ~AvatarMenuItemGtk();
+
+  void OpenProfile();
+  void EditProfile();
 
   // Returns the root widget for this menu item.
   GtkWidget* widget() { return widget_.get(); }
@@ -73,6 +77,8 @@ class AvatarMenuItemGtk {
   // A weak pointer to a link button to edit the given profile. It is shown only
   // when the user is hovering over the active profile.
   GtkWidget* link_alignment_;
+
+  base::WeakPtrFactory<AvatarMenuItemGtk> weak_factory_;
 };
 
 #endif  // CHROME_BROWSER_UI_GTK_AVATAR_MENU_ITEM_GTK_H_
