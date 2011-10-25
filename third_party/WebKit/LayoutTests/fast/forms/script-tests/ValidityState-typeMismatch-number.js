ff6e378021dd9f6b08a0669a781df014d2a60452
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 14:04:45 +0000

    Double number step=.5 should be considered valid in <input type="number" step=".5"> https://bugs.webkit.org/show_bug.cgi?id=70320
    Patch by Sachin Puranik <sachin.puranik@motorola.com> on 2011-10-25
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Test: fast/forms/input-step-as-double.html
    
    * html/parser/HTMLParserIdioms.cpp:
    (WebCore::parseToDoubleForNumberType): Changed the function to consider
    step as valid even if it starts with "."
    
    LayoutTests:
    
    * fast/forms/ValidityState-typeMismatch-number-expected.txt: Change in result of layout test.
    * fast/forms/input-step-as-double-expected.txt: Added, expected output for new test case.
    * fast/forms/input-step-as-double.html: Added, testcase to make sure that all ports
    consider .5 as a valid double value for step attribute of input element.
    * fast/forms/input-valueasnumber-number-expected.txt: Change in result of layout test.
    * fast/forms/range/input-valueasnumber-range-expected.txt: Change in result of layout test.
    * fast/forms/range/input-valueasnumber-range.html: Consider .2 as valid real number now.
    * fast/forms/script-tests/ValidityState-typeMismatch-number.js: Consider
    .2 as valid real number now.
    * fast/forms/script-tests/input-valueasnumber-number.js: Consider .2 as valid real number now.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/forms/script-tests/ValidityState-typeMismatch-number.js
+++ third_party/WebKit/LayoutTests/fast/forms/script-tests/ValidityState-typeMismatch-number.js
@@ -32,6 +32,8 @@ function checkSanitization(value, expectedValue)
 check('0');
 check('10');
 check('01');
+check('0.2');
+check('.2');
 check('-0');
 check('-1.2');
 check('1.2E10');
@@ -48,7 +50,6 @@ checkSanitization('+1', '');
 checkSanitization(' 10', '');
 checkSanitization('10 ', '');
 checkSanitization('1,2', '');
-checkSanitization('.2', '');
 checkSanitization('1E', '');
 checkSanitization('NaN', '');
 checkSanitization('nan', '');
