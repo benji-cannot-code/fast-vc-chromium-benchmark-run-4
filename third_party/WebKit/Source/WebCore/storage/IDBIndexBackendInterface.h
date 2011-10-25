cc25be57b54f2f8590a4dfee0285eb615012ffaa
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 04:49:18 +0000

    Include ThreadSafeRefCounted.h since the class declared in this header file inherits from ThreadSafeRefCounted.
    * storage/IDBIndexBackendInterface.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98319 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBIndexBackendInterface.h
+++ third_party/WebKit/Source/WebCore/storage/IDBIndexBackendInterface.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExceptionCode.h"
 #include "PlatformString.h"
 #include <wtf/Forward.h>
+#include <wtf/ThreadSafeRefCounted.h>
 
 #if ENABLE(INDEXED_DATABASE)
 
