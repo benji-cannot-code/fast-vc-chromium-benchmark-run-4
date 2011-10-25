1ad9a64aea997be0b1314bbd6666867e5ae87f84
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 05:01:07 +0000

    Move RLZ extension api implementation to rlz directory.
    We're moving all the extension api implementations out of
    browser/extensions into the directory with the code they are
    automating.
    
    BUG=101244
    
    Review URL: http://codereview.chromium.org/8374025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/rlz/rlz_extension_api.cc
+++ chrome/browser/rlz/rlz_extension_api.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/extension_rlz_module.h"
+#include "chrome/browser/rlz/rlz_extension_api.h"
 
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/thread_restrictions.h"
