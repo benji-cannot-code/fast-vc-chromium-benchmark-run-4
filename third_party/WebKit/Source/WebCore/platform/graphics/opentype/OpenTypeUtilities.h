d6d4083b4263a357a42a017694707308d1123615
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 04:57:36 +0000

    Chromium build fix: #include window.h for HANDLE.
    * platform/graphics/opentype/OpenTypeUtilities.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/opentype/OpenTypeUtilities.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/opentype/OpenTypeUtilities.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define OpenTypeUtilities_h
 
 #include "PlatformString.h"
+#include <windows.h>
 #include <wtf/Forward.h>
 
 namespace WebCore {
