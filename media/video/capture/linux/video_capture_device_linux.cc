6529d7f5606d9ef4b6469fa3aea3c93a6312bc42
robert.nagy@gmail.com <robert.nagy@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 23:01:50 +0000

    fix v4l support on openbsd
    media/video/capture/linux/video_capture_device_linux.cc:
      OpenBSD has V4L support but the header's filename is different
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8380018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107227 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/linux/video_capture_device_linux.cc
+++ media/video/capture/linux/video_capture_device_linux.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <fcntl.h>
+#if defined(OS_OPENBSD)
+#include <sys/videoio.h>
+#else
 #include <linux/videodev2.h>
+#endif
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
