950ce280a69184ca4c7326f1b161e6fffb7c6f06
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 04:26:38 +0000

    Source/WebCore: Fix a typo.
    * bindings/v8/JavaScriptCallFrame.h:
    
    Source/WebKit/win: threadsafeCopy has been renamed to isolatedCopy.
    
    * WebDatabaseManager.cpp:
    (DidModifyOriginData::dispatchToMainThread):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/win/WebDatabaseManager.cpp
+++ third_party/WebKit/Source/WebKit/win/WebDatabaseManager.cpp
@@ -335,7 +335,7 @@ class DidModifyOriginData {
 public:
     static void dispatchToMainThread(WebDatabaseManager* databaseManager, SecurityOrigin* origin)
     {
-        DidModifyOriginData* context = new DidModifyOriginData(databaseManager, origin->threadsafeCopy());
+        DidModifyOriginData* context = new DidModifyOriginData(databaseManager, origin->isolatedCopy());
         callOnMainThread(&DidModifyOriginData::dispatchDidModifyOriginOnMainThread, context);
     }
 
