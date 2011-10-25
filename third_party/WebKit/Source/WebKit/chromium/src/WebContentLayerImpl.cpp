78c99f51ae85f389fae91c2cdfe2c87101f7ec4f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 21:10:12 +0000

    Fix teardown in Web*LayerImpl, and add tests for Web*Layer https://bugs.webkit.org/show_bug.cgi?id=70431
    Patch by Antoine Labour <piman@chromium.org> on 2011-10-25
    Reviewed by James Robinson.
    
    * src/WebContentLayerImpl.cpp:
    (WebKit::WebContentLayerImpl::~WebContentLayerImpl):
    * src/WebExternalTextureLayerImpl.cpp:
    (WebKit::WebExternalTextureLayerImpl::~WebExternalTextureLayerImpl):
    * src/WebLayerImpl.cpp:
    (WebKit::WebLayerImpl::~WebLayerImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebContentLayerImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebContentLayerImpl.cpp
@@ -55,11 +55,13 @@ WebContentLayerImpl::WebContentLayerImpl(WebLayerClient* client, WebContentLayer
 
 WebContentLayerImpl::~WebContentLayerImpl()
 {
+    setDelegate(0);
 }
 
 void WebContentLayerImpl::setDrawsContent(bool drawsContent)
 {
     m_drawsContent = drawsContent;
+    setNeedsCommit();
 }
 
 bool WebContentLayerImpl::drawsContent() const
