eb15caeef1508bf22846291d4bbfa7fa690d8b0a
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-25 08:37:35 +0000

    [GTK] Add go_to_back_forward_list_item() to WebKit2 GTK+ API https://bugs.webkit.org/show_bug.cgi?id=69825
    Reviewed by Martin Robinson.
    
    * UIProcess/API/gtk/WebKitBackForwardListItem.cpp:
    (webkitBackForwardListItemGetWKItem):
    * UIProcess/API/gtk/WebKitBackForwardListPrivate.h:
    * UIProcess/API/gtk/WebKitWebView.cpp:
    (webkit_web_view_go_to_back_forward_list_item):
    * UIProcess/API/gtk/WebKitWebView.h:
    * UIProcess/API/gtk/tests/TestBackForwardList.cpp:
    (testBackForwardListNavigation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitBackForwardListPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitBackForwardListPrivate.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 WebKitBackForwardList* webkitBackForwardListCreate(WKBackForwardListRef);
 WebKitBackForwardListItem* webkitBackForwardListItemGetOrCreate(WKBackForwardListItemRef);
+WKBackForwardListItemRef webkitBackForwardListItemGetWKItem(WebKitBackForwardListItem*);
 void webkitBackForwardListChanged(WebKitBackForwardList*, WKBackForwardListItemRef wkAddedItem, WKArrayRef wkRemovedItems);
 
 #endif // WebKitBackForwardListPrivate_h
