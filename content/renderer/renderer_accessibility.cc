a8557dd343cf1b8be220c7dd4cc8d56865b124d9
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 06:56:24 +0000

    Pass the right notification enum to WebAccessibility::DebugString.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8351010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107074 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_accessibility.cc
+++ content/renderer/renderer_accessibility.cc
@@ -284,7 +284,7 @@ void RendererAccessibility::SendPendingAccessibilityNotifications() {
       LOG(INFO) << "Accessibility update: "
                 << param.acc_tree.DebugString(true,
                                               routing_id(),
-                                              param.notification_type);
+                                              notification.type);
     }
 #endif
   }
