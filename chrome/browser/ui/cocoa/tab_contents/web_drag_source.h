137a748bb1b78b7abd4b70d7f935129b6c9e7a05
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 20:50:14 +0000

    Break Mac's WebDragSource's dependency on chrome.
    BUG=95573
    TEST=no change
    
    Review URL: http://codereview.chromium.org/8390003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/web_drag_source.h
+++ chrome/browser/ui/cocoa/tab_contents/web_drag_source.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "googleurl/src/gurl.h"
 
-@class TabContentsViewCocoa;
+class TabContents;
 struct WebDropData;
 
 // A class that handles tracking and event processing for a drag and drop
@@ -18,7 +18,10 @@ struct WebDropData;
 @interface WebDragSource : NSObject {
  @private
   // Our tab. Weak reference (owns or co-owns us).
-  TabContentsViewCocoa* contentsView_;
+  TabContents* contents_;
+
+  // The view from which the drag was initiated. Weak reference.
+  NSView* contentsView_;
 
   // Our drop data. Should only be initialized once.
   scoped_ptr<WebDropData> dropData_;
@@ -45,12 +48,13 @@ struct WebDropData;
 // Initialize a WebDragSource object for a drag (originating on the given
 // contentsView and with the given dropData and pboard). Fill the pasteboard
 // with data types appropriate for dropData.
-- (id)initWithContentsView:(TabContentsViewCocoa*)contentsView
-                  dropData:(const WebDropData*)dropData
-                     image:(NSImage*)image
-                    offset:(NSPoint)offset
-                pasteboard:(NSPasteboard*)pboard
-         dragOperationMask:(NSDragOperation)dragOperationMask;
+- (id)initWithContents:(TabContents*)contents
+                  view:(NSView*)contentsView
+              dropData:(const WebDropData*)dropData
+                 image:(NSImage*)image
+                offset:(NSPoint)offset
+            pasteboard:(NSPasteboard*)pboard
+     dragOperationMask:(NSDragOperation)dragOperationMask;
 
 // Returns a mask of the allowed drag operations.
 - (NSDragOperation)draggingSourceOperationMaskForLocal:(BOOL)isLocal;
--- chrome/browser/ui/cocoa/tab_contents/web_drag_source.mm
+++ chrome/browser/ui/cocoa/tab_contents/web_drag_source.mm
@@ -14,8 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/tab_contents/tab_contents_view_mac.h"
+#include "content/browser/browser_thread.h"
 #include "content/browser/download/drag_download_file.h"
 #include "content/browser/download/drag_download_util.h"
 #include "content/browser/renderer_host/render_view_host.h"
@@ -124,13 +123,17 @@ void PromiseWriterTask::Run() {
 
 @implementation WebDragSource
 
-- (id)initWithContentsView:(TabContentsViewCocoa*)contentsView
-                  dropData:(const WebDropData*)dropData
-                     image:(NSImage*)image
-                    offset:(NSPoint)offset
-                pasteboard:(NSPasteboard*)pboard
-         dragOperationMask:(NSDragOperation)dragOperationMask {
+- (id)initWithContents:(TabContents*)contents
+                  view:(NSView*)contentsView
+              dropData:(const WebDropData*)dropData
+                 image:(NSImage*)image
+                offset:(NSPoint)offset
+            pasteboard:(NSPasteboard*)pboard
+     dragOperationMask:(NSDragOperation)dragOperationMask {
   if ((self = [super init])) {
+    contents_ = contents;
+    DCHECK(contents_);
+
     contentsView_ = contentsView;
     DCHECK(contentsView_);
 
@@ -274,9 +277,9 @@ void PromiseWriterTask::Run() {
 
 - (void)endDragAt:(NSPoint)screenPoint
         operation:(NSDragOperation)operation {
-  [contentsView_ tabContents]->SystemDragEnded();
+  contents_->SystemDragEnded();
 
-  RenderViewHost* rvh = [contentsView_ tabContents]->render_view_host();
+  RenderViewHost* rvh = contents_->render_view_host();
   if (rvh) {
     // Convert |screenPoint| to view coordinates and flip it.
     NSPoint localPoint = NSMakePoint(0, 0);
@@ -304,7 +307,7 @@ void PromiseWriterTask::Run() {
 }
 
 - (void)moveDragTo:(NSPoint)screenPoint {
-  RenderViewHost* rvh = [contentsView_ tabContents]->render_view_host();
+  RenderViewHost* rvh = contents_->render_view_host();
   if (rvh) {
     // Convert |screenPoint| to view coordinates and flip it.
     NSPoint localPoint = NSMakePoint(0, 0);
@@ -343,22 +346,22 @@ void PromiseWriterTask::Run() {
     return nil;
 
   if (downloadURL_.is_valid()) {
-    TabContents* tabContents = [contentsView_ tabContents];
     scoped_refptr<DragDownloadFile> dragFileDownloader(new DragDownloadFile(
         filePath,
         linked_ptr<net::FileStream>(fileStream),
         downloadURL_,
-        tabContents->GetURL(),
-        tabContents->encoding(),
-        tabContents));
+        contents_->GetURL(),
+        contents_->encoding(),
+        contents_));
 
     // The finalizer will take care of closing and deletion.
     dragFileDownloader->Start(
         new drag_download_util::PromiseFileFinalizer(dragFileDownloader));
   } else {
     // The writer will take care of closing and deletion.
-    g_browser_process->file_thread()->message_loop()->PostTask(FROM_HERE,
-        new PromiseWriterTask(*dropData_, fileStream));
+    BrowserThread::PostTask(BrowserThread::FILE,
+                            FROM_HERE,
+                            new PromiseWriterTask(*dropData_, fileStream));
   }
 
   // Once we've created the file, we should return the file name.
