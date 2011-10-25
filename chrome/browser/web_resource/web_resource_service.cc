6bd96ba7e3b3afa9beb2994bf3d77aba346d8f17
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 13:43:44 +0000

    Fix crasher due to const-ref to non-existing variable
    TBR=jam@chromium.org
    BUG=101476
    TEST=no
    
    Review URL: http://codereview.chromium.org/8392001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/web_resource_service.cc
+++ chrome/browser/web_resource/web_resource_service.cc
@@ -147,7 +147,7 @@ class WebResourceService::UnpackerClient : public UtilityProcessHost::Client {
   }
 
  private:
-  ~UnpackerClient() {}
+  virtual ~UnpackerClient() {}
 
   // UtilityProcessHost::Client
   virtual bool OnMessageReceived(const IPC::Message& message) {
@@ -202,7 +202,7 @@ class WebResourceService::UnpackerClient : public UtilityProcessHost::Client {
   scoped_refptr<WebResourceService> web_resource_service_;
 
   // Holds raw JSON string.
-  const std::string& json_data_;
+  std::string json_data_;
 
   // True if we got a response from the utility process and have cleaned up
   // already.
