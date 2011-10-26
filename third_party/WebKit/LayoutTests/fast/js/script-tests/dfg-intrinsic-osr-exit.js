c69c292bc419f681859958dd1796d5a5bbcc199e
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-26 21:13:52 +0000

    REGRESSION (r97030): Cannot log in to progressive.com https://bugs.webkit.org/show_bug.cgi?id=70094
    Reviewed by Oliver Hunt.
    
    Source/JavaScriptCore: 
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::handleCall):
    
    LayoutTests: 
    
    * fast/js/dfg-intrinsic-osr-exit-expected.html: Added.
    * fast/js/dfg-intrinsic-osr-exit.html: Added.
    * fast/js/script-tests/dfg-intrinsic-osr-exit.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-intrinsic-osr-exit.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"This tests that an OSR exit inside of an intrinsic that was not loaded with a method check works correctly."
+);
+
+function foo(a,b) {
+    return a[0](b.f);
+}
+
+for (var i = 0; i < 100; ++i)
+    foo([Math.abs], {f:5});
+
+shouldBe("foo([Math.abs], {f:5})", "5");
+
+for (var i = 0; i < 10; ++i)
+    shouldBe("foo([Math.abs], {f:5.5})", "5.5");
+
+var successfullyParsed = true;
