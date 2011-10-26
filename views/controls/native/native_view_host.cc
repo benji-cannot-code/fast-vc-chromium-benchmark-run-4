830ee6ef95b733225cb772cdd664b41a949c4fd8
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 11:18:47 +0000

    Use LockScreen container for login screen. Use NativeViewHostAura in NativeViewHost.
    Using TYPE_WINDOW_FRAMELESS instead of TYPE_CONTROL since latter has issues with keyboard/focus (both not working). In a result, launcher is seen on login screen even though its container is below LockScreen container.
    
    Tested with http://codereview.chromium.org/8373004/ and
    temporary workaround with old WM present (http://gerrit.chromium.org/gerrit/#change,10552).
    
    This CL is a take-over for http://codereview.chromium.org/8361001/
    
    BUG=100874
    TEST=Manual: Navigated through user pods, signed in with new/existing users, launched guest mode, shutdown, create account flow, removed user. Tested with non-Aura build that there were no regressions.
    
    Review URL: http://codereview.chromium.org/8382004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107332 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/native/native_view_host.cc
+++ views/controls/native/native_view_host.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "ui/gfx/canvas.h"
-#include "views/controls/native/native_view_host_wrapper.h"
 #include "views/controls/native/native_view_host_views.h"
+#include "views/controls/native/native_view_host_wrapper.h"
 #include "views/widget/widget.h"
 
 namespace views {
