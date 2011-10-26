b27f069e9bde363b6eb8d26ebf53af8a44c7edcd
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 01:08:46 +0000

    Pass current state to callback.
    BUG=92433
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/8395022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107266 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+'use strict';
+
 /** @suppress {duplicate} */
 var remoting = remoting || {};
-
-'use strict';
 
 /**
  * Whether or not the plugin should scale itself.
@@ -354,10 +354,13 @@ function onNatTraversalPolicyChanged_(enabled) {
   container.hidden = enabled;
 }
 
-function onStateChanged_() {
+/**
+ * Callback for the host plugin to notify the web app of state changes.
+ * @param {number} state The new state of the plugin.
+ */
+function onStateChanged_(state) {
   var plugin = /** @type {remoting.HostPlugin} */
       document.getElementById(remoting.HOST_PLUGIN_ID);
-  var state = plugin.state;
   if (state == plugin.STARTING) {
     // Nothing to do here.
     remoting.debug.log('Host plugin state: STARTING');
