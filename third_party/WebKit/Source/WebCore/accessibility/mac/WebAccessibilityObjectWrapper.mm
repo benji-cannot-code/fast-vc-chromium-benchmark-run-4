b832379e9746e5bbe677d17811e78b2e32e60236
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-26 01:45:40 +0000

    AX: WebKit does not expose HTML label for slider elements https://bugs.webkit.org/show_bug.cgi?id=70856
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Test: platform/mac/accessibility/slider-allows-title-ui-element.html
    
    * accessibility/AccessibilitySlider.h:
    (WebCore::AccessibilitySlider::isControl):
    * accessibility/mac/WebAccessibilityObjectWrapper.mm:
    (-[WebAccessibilityObjectWrapper accessibilityAttributeNames]):
    
    LayoutTests:
    
    * platform/mac/accessibility/slider-allows-title-ui-element-expected.txt: Added.
    * platform/mac/accessibility/slider-allows-title-ui-element.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapper.mm
+++ third_party/WebKit/Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapper.mm
@@ -1079,6 +1079,7 @@ static id textMarkerRangeFromVisiblePositions(AXObjectCache *cache, VisiblePosit
         [tempArray addObject:NSAccessibilityMaxValueAttribute];
         [tempArray addObject:NSAccessibilityOrientationAttribute];
         [tempArray addObject:NSAccessibilityValueDescriptionAttribute];
+        [tempArray addObject:NSAccessibilityTitleUIElementAttribute];
         rangeAttrs = [[NSArray alloc] initWithArray:tempArray];
         [tempArray release];
     }
