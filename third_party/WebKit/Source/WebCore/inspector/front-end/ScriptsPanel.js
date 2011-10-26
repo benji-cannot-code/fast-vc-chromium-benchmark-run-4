c084999a67f2698a6e1b58a101668f7e35d1b114
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-26 06:25:39 +0000

    Unreviewed, rolling out r98346. http://trac.webkit.org/changeset/98346 https://bugs.webkit.org/show_bug.cgi?id=70874
    Breaks JSC-based bots. (Requested by pfeldman on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-10-25
    
    Source/WebCore:
    
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype.evaluateInSelectedCallFrame):
    
    LayoutTests:
    
    * inspector/debugger/error-in-watch-expressions.html:
    * inspector/debugger/watch-expressions-panel-switch-expected.txt: Removed.
    * inspector/debugger/watch-expressions-panel-switch.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -468,8 +468,7 @@ WebInspector.ScriptsPanel.prototype = {
                 callback(WebInspector.RemoteObject.fromPayload(result), wasThrown);
         }
         var selectedCallFrame = this._presentationModel.selectedCallFrame;
-        if (selectedCallFrame)
-            selectedCallFrame.evaluate(code, objectGroup, includeCommandLineAPI, returnByValue, didEvaluate.bind(this));
+        selectedCallFrame.evaluate(code, objectGroup, includeCommandLineAPI, returnByValue, didEvaluate.bind(this));
     },
 
     getSelectedCallFrameVariables: function(callback)
