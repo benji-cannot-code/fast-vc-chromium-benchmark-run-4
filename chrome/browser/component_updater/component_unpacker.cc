1604dd12d0b36869524e8f890467d2dbadecef88
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 06:59:34 +0000

    Put zip functions into 'zip' namespace.
    Along the way, rename one of the two versions of Zip() to
    ZipWithFilterCallback() per the C++ style guide that prohibits
    function overloading like this.
    
    TEST=chrome and tests build
    BUG=none
    
    Review URL: http://codereview.chromium.org/8381027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_unpacker.cc
+++ chrome/browser/component_updater/component_unpacker.cc
@@ -187,7 +187,7 @@ ComponentUnpacker::ComponentUnpacker(const std::vector<uint8>& pk_hash,
     error_ = kUzipPathError;
     return;
   }
-  if (!Unzip(path, unpack_path_)) {
+  if (!zip::Unzip(path, unpack_path_)) {
     error_ = kUnzipFailed;
     return;
   }
--- chrome/common/zip.h
+++ chrome/common/zip.h
@@ -11,13 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class FilePath;
 
+namespace zip {
+
 // Zip the contents of src_dir into dest_file. src_path must be a directory.
 // An entry will *not* be created in the zip for the root folder -- children
 // of src_dir will be at the root level of the created zip. For each file in
 // src_dir, include it only if the callback |filter_cb| returns true. Otherwise
 // omit it.
-bool Zip(const FilePath& src_dir, const FilePath& dest_file,
-         const base::Callback<bool(const FilePath&)>& filter_cb);
+typedef base::Callback<bool(const FilePath&)> FilterCallback;
+bool ZipWithFilterCallback(const FilePath& src_dir, const FilePath& dest_file,
+                           const FilterCallback& filter_cb);
 
 // Convenience method for callers who don't need to set up the filter callback.
 // If |include_hidden_files| is true, files starting with "." are included.
@@ -27,5 +30,7 @@ bool Zip(const FilePath& src_dir, const FilePath& dest_file,
 
 // Unzip the contents of zip_file into dest_dir.
 bool Unzip(const FilePath& zip_file, const FilePath& dest_dir);
+
+}  // namespace zip
 
 #endif  // CHROME_COMMON_ZIP_H_
