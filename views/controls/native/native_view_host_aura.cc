3708501601a96c364fdf8e51ff5ecd6569d2ecf0
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 19:28:45 +0000

    [Aura] Fix NativeViewHostAura::RemovedFromWidget crash.
    Make NativeViewHostAura detach the native view when notified OnWindowDestroyed.
    
    BUG=101526
    TEST=Verify closing a window with DOMView inside (e.g. AppList window) does not crash per 101526.
    
    
    Review URL: http://codereview.chromium.org/8390052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107409 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/native/native_view_host_aura.cc
+++ views/controls/native/native_view_host_aura.cc
@@ -28,11 +28,16 @@ void NativeViewHostAura::NativeViewAttached() {
     host_->native_view()->parent()->RemoveChild(host_->native_view());
   host_->GetWidget()->GetNativeView()->AddChild(host_->native_view());
   host_->Layout();
+
+  host_->native_view()->AddObserver(this);
 }
 
 void NativeViewHostAura::NativeViewDetaching(bool destroyed) {
-  host_->native_view()->Hide();
-  host_->GetWidget()->GetNativeView()->RemoveChild(host_->native_view());
+  if (!destroyed) {
+    host_->native_view()->RemoveObserver(this);
+    host_->native_view()->Hide();
+    host_->GetWidget()->GetNativeView()->RemoveChild(host_->native_view());
+  }
 }
 
 void NativeViewHostAura::AddedToWidget() {
@@ -84,6 +89,11 @@ void NativeViewHostAura::SetFocus() {
 
 gfx::NativeViewAccessible NativeViewHostAura::GetNativeViewAccessible() {
   return NULL;
+}
+
+void NativeViewHostAura::OnWindowDestroyed(aura::Window* window) {
+  DCHECK(window == host_->native_view());
+  host_->NativeViewDestroyed();
 }
 
 // static
