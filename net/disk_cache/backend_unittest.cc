4bbca287b232fc6b3e88dd3ab8770445fd074493
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 22:13:42 +0000

    Disk cache: Fix the synchronization of BackendTrimInvalidEntry2.
    BUG=none
    TEST=Tsan should be happy.
    TBR=gavinp@chromium.org
    Review URL: http://codereview.chromium.org/8400004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107446 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -813,6 +813,7 @@ void DiskCacheBackendTest::BackendTrimInvalidEntry2() {
   ASSERT_EQ(net::OK, CreateEntry("Something else", &entry));
   EXPECT_EQ(kSize, WriteData(entry, 0, 0, buffer, kSize, false));
 
+  FlushQueueForTest();
   EXPECT_EQ(33, cache_->GetEntryCount());
   SetMaxSize(kSize);
 
@@ -827,6 +828,7 @@ void DiskCacheBackendTest::BackendTrimInvalidEntry2() {
 
   // We may abort the eviction before cleaning up everything.
   MessageLoop::current()->RunAllPending();
+  FlushQueueForTest();
   EXPECT_GE(30, cache_->GetEntryCount());
 }
 
