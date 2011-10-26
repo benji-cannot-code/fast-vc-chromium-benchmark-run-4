26d96cb7945219360b8774159a1f6118243ce970
zhenghao@google.com <zhenghao@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 18:00:34 +0000

    Remove an include which has been upstreamed to ImageDiff.cpp.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8392031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107382 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/webkit_support_gfx.h
+++ webkit/support/webkit_support_gfx.h
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-// TODO(darin): Remove once this #include has been upstreamed to ImageDiff.cpp.
-// ImageDiff.cpp expects that PATH_MAX has already been defined :-/
-#include <limits.h>
-
 namespace webkit_support {
 
 // Decode a PNG into an RGBA pixel array.
