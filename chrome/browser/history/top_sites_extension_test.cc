9f256770f50ec5670528d7240879fd7d62cf1250
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 19:12:40 +0000

    Disable GetTopSites.
    BUG=101783
    TEST=green tree
    
    Review URL: http://codereview.chromium.org/8390063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/top_sites_extension_test.cc
+++ chrome/browser/history/top_sites_extension_test.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/history/top_sites_extension_api.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -17,7 +18,16 @@ class TopSitesExtensionTest : public InProcessBrowserTest {
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(TopSitesExtensionTest, GetTopSites) {
+// Test started failing soon after commit. http://crbug.com/101783
+#if defined(OS_WIN)
+#define MAYBE_GetTopSites FAILS_GetTopSites
+#if defined(OS_LINUX)
+#define MAYBE_GetTopSites FLAKY_GetTopSites
+#else
+#define MAYBE_GetTopSites GetTopSites
+#endif
+
+IN_PROC_BROWSER_TEST_F(TopSitesExtensionTest, MAYBE_GetTopSites) {
   scoped_refptr<GetTopSitesFunction> get_top_sites_function(
       new GetTopSitesFunction());
   // Without a callback the function will not generate a result.
