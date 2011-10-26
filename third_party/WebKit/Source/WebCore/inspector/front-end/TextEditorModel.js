40fb94a1c063aca7a7402e0eb11867485de177b1
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-26 14:32:04 +0000

    2011-10-26  Pavel Feldman  <pfeldman@google.com>
            Not reviewed: follow up to 98236 - moved inspector settings initialization earlier to unbreak settings panel.
    
            * inspector/front-end/TextEditorModel.js:
            (WebInspector.TextEditorModel):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TextEditorModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TextEditorModel.js
@@ -67,8 +67,6 @@ WebInspector.TextEditorModel = function()
     this._undoStack = [];
     this._noPunctuationRegex = /[^ !%&()*+,-.:;<=>?\[\]\^{|}~]+/;
     this._lineBreak = "\n";
-
-    WebInspector.settings.textEditorIndent = WebInspector.settings.createSetting("indent", WebInspector.TextEditorModel.Indent.FourSpaces);
 }
 
 WebInspector.TextEditorModel.Indent = {
@@ -314,3 +312,5 @@ WebInspector.TextEditorModel.prototype = {
         this._undoStack = [];
     }
 }
+
+WebInspector.settings.textEditorIndent = WebInspector.settings.createSetting("textEditorIndent", WebInspector.TextEditorModel.Indent.FourSpaces);
