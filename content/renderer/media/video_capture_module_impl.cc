c635598213e34a4cc0f3ee2320a5808138a0563f
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 17:06:27 +0000

    Use base::Unretained() around VideoCaptureModuleImpl since it's not ref counted by Chrome.
    The creator of VideoCaptureModuleImpl has exclusive ownership. When it tries to release the
    object, it will wait for the VCMImplThread to be stopped, which means all pending tasks will
    be executed before the object is deleted.
    
    Chrome is not supposed to add reference of it. If Chrome adds reference and the creator calls
    release before some task is executed on VCMImplThread, then the VCMImpl dtor will be called on
    the VCMImplThread. But in dtor, it will try to stop VCMImplThread. That's wrong.
     
    BUG=none
    TEST=trybots
    Review URL: http://codereview.chromium.org/8394013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107370 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/video_capture_module_impl.cc
+++ content/renderer/media/video_capture_module_impl.cc
@@ -60,14 +60,15 @@ WebRtc_Word32 VideoCaptureModuleImpl::StartCapture(
   message_loop_proxy_->PostTask(
       FROM_HERE,
       base::Bind(&VideoCaptureModuleImpl::StartCaptureOnCaptureThread,
-                 this, capability));
+                 base::Unretained(this), capability));
   return 0;
 }
 
 WebRtc_Word32 VideoCaptureModuleImpl::StopCapture() {
   message_loop_proxy_->PostTask(
       FROM_HERE,
-      base::Bind(&VideoCaptureModuleImpl::StopCaptureOnCaptureThread, this));
+      base::Bind(&VideoCaptureModuleImpl::StopCaptureOnCaptureThread,
+                 base::Unretained(this)));
   return 0;
 }
 
@@ -92,7 +93,8 @@ void VideoCaptureModuleImpl::OnStarted(media::VideoCapture* capture) {
 void VideoCaptureModuleImpl::OnStopped(media::VideoCapture* capture) {
   message_loop_proxy_->PostTask(
       FROM_HERE,
-      base::Bind(&VideoCaptureModuleImpl::OnStoppedOnCaptureThread, this,
+      base::Bind(&VideoCaptureModuleImpl::OnStoppedOnCaptureThread,
+                 base::Unretained(this),
                  capture));
 }
 
@@ -115,7 +117,7 @@ void VideoCaptureModuleImpl::OnBufferReady(
   message_loop_proxy_->PostTask(
       FROM_HERE,
       base::Bind(&VideoCaptureModuleImpl::OnBufferReadyOnCaptureThread,
-                 this, capture, buf));
+                 base::Unretained(this), capture, buf));
 }
 
 void VideoCaptureModuleImpl::OnDeviceInfoReceived(
