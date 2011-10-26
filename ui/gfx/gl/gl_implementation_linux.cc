cd9547155e73acf4088136306645be6ef550bb0d
robert.nagy@gmail.com <robert.nagy@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 20:09:45 +0000

    on OpenBSD libGL.so has to be loaded instead of libGL.so.1
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8393049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107419 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_implementation_linux.cc
+++ ui/gfx/gl/gl_implementation_linux.cc
@@ -104,7 +104,11 @@ bool InitializeGLBindings(GLImplementation implementation) {
       break;
     }
     case kGLImplementationDesktopGL: {
+#if defined(OS_OPENBSD)
+      base::NativeLibrary library = LoadLibrary("libGL.so");
+#else
       base::NativeLibrary library = LoadLibrary("libGL.so.1");
+#endif
       if (!library)
         return false;
 
