2fa8c18a59be877653e357f91fc682702d73e563
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-26 21:13:37 +0000

    Make single item bookmark drags look nicer.
    BUG=37264
    TEST=no change to location bar icon drags, single item drags in the bookmark manager look the same as location bar icon drags.
    
    Review URL: http://codereview.chromium.org/8368010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107431 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_pasteboard_helper_mac.h
+++ chrome/browser/bookmarks/bookmark_pasteboard_helper_mac.h
@@ -11,6 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_node_data.h"
 #include "ui/gfx/native_widget_types.h"
 
+#if defined(__OBJC__)
+@class NSImage;
+@class NSString;
+#else  // __OBJC__
+class NSImage;
+#endif  // __OBJC__
+
 // This set of functions lets C++ code interact with the cocoa pasteboard
 // and dragging methods.
 namespace bookmark_pasteboard_helper_mac {
@@ -35,6 +42,9 @@ bool ReadFromPasteboard(PasteboardType type,
 // valid bookmark.
 bool PasteboardContainsBookmarks(PasteboardType type);
 
+// Returns a drag image for a bookmark.
+NSImage* DragImageForBookmark(NSImage* favicon, const string16& title);
+
 // Copies the bookmark nodes to the dragging pasteboard and initiates a
 // drag from the specified view.  |view| must be a |TabContentsViewCocoa*|.
 void StartDrag(Profile* profile, const std::vector<const BookmarkNode*>& nodes,
@@ -42,8 +52,7 @@ void StartDrag(Profile* profile, const std::vector<const BookmarkNode*>& nodes,
 
 }  // namespace bookmark_pasteboard_helper_mac
 
-#ifdef __OBJC__
-@class NSString;
+#if defined(__OBJC__)
 // Pasteboard type for dictionary containing bookmark structure consisting
 // of individual bookmark nodes and/or bookmark folders.
 extern "C" NSString* const kBookmarkDictionaryListPboardType;
