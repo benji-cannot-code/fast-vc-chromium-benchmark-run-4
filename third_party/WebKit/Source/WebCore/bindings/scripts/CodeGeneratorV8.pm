7c603a35e8b4c26c5449f857fa1959bf39cb687a
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 19:22:31 +0000

    Get rid of optional parameters in the middle in IDLs. https://bugs.webkit.org/show_bug.cgi?id=70816
    Reviewed by Adam Barth.
    
    Optional parameters in the middle are prohibited by WebIDL spec.
    
    Source/WebCore:
    
    * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateHeader):
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader):
    (GenerateArgumentsCountCheck):
    (GenerateImplementation):
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore::jsTestObjPrototypeFunctionMethodWithNonOptionalArgAndTwoOptionalArgs):
    * bindings/scripts/test/TestObj.idl:
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjInternal::methodWithNonOptionalArgAndTwoOptionalArgsCallback):
    * html/canvas/CanvasRenderingContext2D.idl:
    * page/DOMWindow.idl:
    
    LayoutTests:
    
    * fast/canvas/canvas-putImageData-expected.txt: expectation was wrong, context.putImageData({}, 0, 0) should throw Type Error.
    * fast/canvas/canvas-putImageData.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -265,6 +265,7 @@ sub GenerateHeader
     # Copy contents of parent classes except the first parent or if it is
     # EventTarget.
     $codeGenerator->AddMethodsConstantsAndAttributesFromParentClasses($dataNode, \@allParents, 1);
+    $codeGenerator->LinkOverloadedFunctions($dataNode);
 
     my $hasDependentLifetime = $dataNode->extendedAttributes->{"V8DependentLifetime"} || $dataNode->extendedAttributes->{"ActiveDOMObject"} || $className =~ /SVG/;
     if (!$hasDependentLifetime) {
@@ -372,7 +373,7 @@ END
         my $name = $function->signature->name;
         my $attrExt = $function->signature->extendedAttributes;
 
-        if ($attrExt->{"Custom"} || $attrExt->{"V8Custom"}) {
+        if (($attrExt->{"Custom"} || $attrExt->{"V8Custom"}) && $function->{overloadIndex} == 1) {
             push(@headerContent, <<END);
     static v8::Handle<v8::Value> ${name}Callback(const v8::Arguments&);
 END
@@ -1385,6 +1386,17 @@ sub GenerateArgumentsCountCheck
     my $function = shift;
     my $dataNode = shift;
 
+    my $numMandatoryParams = 0;
+    my $optionalSeen = 0;
+    foreach my $param (@{$function->parameters}) {
+        if ($param->extendedAttributes->{"Optional"}) {
+            $optionalSeen = 1;
+        } else {
+            die "An argument must not be declared to be optional unless all subsequent arguments to the operation are also optional." if $optionalSeen;
+            $numMandatoryParams++;
+        }
+    }
+
     my $argumentsCountCheckString = "";
     my $requiresAllArguments;
     my $requiresAllArgumentsDefault = "";
@@ -1392,22 +1404,12 @@ sub GenerateArgumentsCountCheck
         $requiresAllArgumentsDefault = "Raise";
     }
     $requiresAllArguments = $function->signature->extendedAttributes->{"RequiresAllArguments"} || $requiresAllArgumentsDefault;
-    if ($requiresAllArguments) {
-        my $numMandatoryParams = @{$function->parameters};
-        foreach my $param (reverse(@{$function->parameters})) {
-            if ($param->extendedAttributes->{"Optional"}) {
-                $numMandatoryParams--;
-            } else {
-                last;
-            }
-        }
-        if ($numMandatoryParams >= 1) {
-            $argumentsCountCheckString .= "    if (args.Length() < $numMandatoryParams)\n";
-            if ($requiresAllArguments eq "Raise") {
-                $argumentsCountCheckString .= "        return throwError(\"Not enough arguments\", V8Proxy::TypeError);\n";
-            } else {
-                $argumentsCountCheckString .= "        return v8::Handle<v8::Value>();\n";
-            }
+    if ($requiresAllArguments && $numMandatoryParams >= 1) {
+        $argumentsCountCheckString .= "    if (args.Length() < $numMandatoryParams)\n";
+        if ($requiresAllArguments eq "Raise") {
+            $argumentsCountCheckString .= "        return throwError(\"Not enough arguments\", V8Proxy::TypeError);\n";
+        } else {
+            $argumentsCountCheckString .= "        return v8::Handle<v8::Value>();\n";
         }
     }
     return $argumentsCountCheckString;
@@ -2006,13 +2008,12 @@ sub GenerateImplementation
         GenerateConstructorGetter($implClassName);
     }
 
-    $codeGenerator->LinkOverloadedFunctions($dataNode);
-
     my $indexer;
     my $namedPropertyGetter;
     # Generate methods for functions.
     foreach my $function (@{$dataNode->functions}) {
-        if (!($function->signature->extendedAttributes->{"Custom"} || $function->signature->extendedAttributes->{"V8Custom"})) {
+        my $isCustom = $function->signature->extendedAttributes->{"Custom"} || $function->signature->extendedAttributes->{"V8Custom"};
+        if (!$isCustom) {
             GenerateFunctionCallback($function, $dataNode, $implClassName);
             if ($function->{overloadIndex} > 1 && $function->{overloadIndex} == @{$function->{overloads}}) {
                 GenerateOverloadedFunctionCallback($function, $dataNode, $implClassName);
@@ -2031,7 +2032,9 @@ sub GenerateImplementation
         # generate an access getter that returns different function objects
         # for different calling context.
         if (($dataNode->extendedAttributes->{"CheckDomainSecurity"} || ($interfaceName eq "DOMWindow")) && $function->signature->extendedAttributes->{"DoNotCheckDomainSecurity"}) {
-            GenerateDomainSafeFunctionGetter($function, $implClassName);
+            if (!$isCustom || $function->{overloadIndex} == 1) {
+                GenerateDomainSafeFunctionGetter($function, $implClassName);
+            }
         }
     }
 
