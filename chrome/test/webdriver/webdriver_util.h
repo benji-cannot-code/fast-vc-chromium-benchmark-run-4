51608080937b0519e7d35343d891cde788d488c3
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 21:39:25 +0000

    Enhance and refactor ChromeDriver's capability handling. Log warning for non-recognized capabilities, return errors on incorrect input types, and allow new format where all options are placed under a chromeOptions dictionary. The latter is needed to simplify handling capabilities on the WebDriver client side. See http://codereview.appspot.com/5307047/. BUG=82895,94721 TEST=none
    Review URL: http://codereview.chromium.org/8341044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107629 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/webdriver_util.h
+++ chrome/test/webdriver/webdriver_util.h
@@ -12,13 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/values.h"
 #include "chrome/test/automation/value_conversion_traits.h"
 
 class FilePath;
-
-namespace base {
-class Value;
-}
 
 namespace webdriver {
 
@@ -27,6 +24,13 @@ std::string GenerateRandomID();
 
 // Returns the equivalent JSON string for the given value.
 std::string JsonStringify(const base::Value* value);
+
+// Returns the JSON string for the given value, with the exception that
+// long strings are shortened for easier display.
+std::string JsonStringifyForDisplay(const base::Value* value);
+
+// Returns the string representation of the given type, for display purposes.
+const char* GetJsonTypeName(base::Value::Type type);
 
 #if defined(OS_MACOSX)
 // Gets the paths to the user and local application directory.
