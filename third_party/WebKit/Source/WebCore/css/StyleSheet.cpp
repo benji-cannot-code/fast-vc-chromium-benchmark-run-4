87317f25be2d95de14be416f82c2073353fa6e38
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 14:26:04 +0000

    StyleSheet.parentStyleSheet does not work. https://bugs.webkit.org/show_bug.cgi?id=70956
    Patch by Andreas Kling <kling@webkit.org> on 2011-10-27
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Since StyleSheet is only ever parented by a CSSRule, we need
    to go through that rule when resolving the StyleSheet's parent.
    
    Test: fast/css/stylesheet-parentStyleSheet.html
    
    * css/StyleSheet.cpp:
    (WebCore::StyleSheet::parentStyleSheet):
    
    LayoutTests:
    
    * fast/css/stylesheet-parentStyleSheet-expected.txt: Added.
    * fast/css/stylesheet-parentStyleSheet.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleSheet.cpp
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "StyleSheet.h"
 
+#include "CSSRule.h"
 #include "CSSStyleSheet.h"
 #include "MediaList.h"
 #include "Node.h"
@@ -53,7 +54,10 @@ StyleSheet::~StyleSheet()
 
 StyleSheet* StyleSheet::parentStyleSheet() const
 {
-    return (parent() && parent()->isStyleSheet()) ? static_cast<StyleSheet*>(parent()) : 0;
+    if (!parent())
+        return 0;
+    ASSERT(parent()->isRule());
+    return static_cast<CSSRule*>(parent())->parentStyleSheet();
 }
 
 void StyleSheet::setMedia(PassRefPtr<MediaList> media)
