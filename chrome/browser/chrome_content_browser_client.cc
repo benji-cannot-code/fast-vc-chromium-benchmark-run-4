bfd12b55e349e9959380d901a15515d4c94ee84c
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 15:09:48 +0000

    Aura: fix crash when clicking on a SELECT element.
    This fix doesn't display selection popups properly, though (because RenderWidgetHostViewAura::InitAsPopup is not implemented).
    
    BUG=101899,99757
    TEST=Manual: click on any SELECT element
    
    Review URL: http://codereview.chromium.org/8363047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -114,7 +114,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/crypto_module_password_dialog.h"
 #endif
 
-#if defined(USE_AURA) || defined(TOUCH_UI)
+#if defined(USE_AURA)
+#include "content/browser/renderer_host/render_widget_host_view_aura.h"
+#elif defined(TOUCH_UI)
 #include "chrome/browser/renderer_host/render_widget_host_view_views.h"
 #elif defined(OS_WIN)
 #include "chrome/browser/renderer_host/render_widget_host_view_views.h"
@@ -230,7 +232,9 @@ void ChromeContentBrowserClient::CreateBrowserMainParts(
 
 RenderWidgetHostView* ChromeContentBrowserClient::CreateViewForWidget(
     RenderWidgetHost* widget) {
-#if defined(USE_AURA) || defined(TOUCH_UI)
+#if defined(USE_AURA)
+  return new RenderWidgetHostViewAura(widget);
+#elif defined(TOUCH_UI)
   return new RenderWidgetHostViewViews(widget);
 #elif defined(OS_WIN)
   if (views::Widget::IsPureViews())
