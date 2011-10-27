4a6e637ca2b91d3e77226afc597b9c0b6213f7e1
vangelis@chromium.org <vangelis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 20:28:40 +0000

    Source/WebCore: Add support for creating compositing layers for fixed position elements. https://bugs.webkit.org/show_bug.cgi?id=69796
    position:fixed elements will get a compositing layer if:
    1. Settings:acceleratedCompositingForFixedPositionEnabled() returns true, 
    2. The element has its own stacking context (e.g. has a z-index), and
    3. The element's container is the RenderView (e.g. not a descendant of a transformed element)
    
    Reviewed by Simon Fraser.
    
    
    Test: compositing/geometry/fixed-position-composited-switch.html
    
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    * page/Settings.h:
    (WebCore::Settings::setAcceleratedCompositingForFixedPositionEnabled):
    (WebCore::Settings::acceleratedCompositingForFixedPositionEnabled):
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::RenderLayerCompositor):
    (WebCore::RenderLayerCompositor::updateCompositingLayers):
    (WebCore::RenderLayerCompositor::requiresCompositingLayer):
    (WebCore::RenderLayerCompositor::requiresCompositingForPosition):
    * rendering/RenderLayerCompositor.h:
    * testing/Internals.cpp:
    (WebCore::Internals::setEnableCompositingForFixedPosition):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    LayoutTests: Adding tests for fixed positioned elements getting their own composited layer.
    https://bugs.webkit.org/show_bug.cgi?id=69796
    
    Reviewed by Simon Fraser.
    
    
    * compositing/geometry/fixed-position-composited-switch-expected.txt: Added.
    * compositing/geometry/fixed-position-composited-switch.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.h
@@ -283,6 +283,7 @@ private:
     bool requiresCompositingWhenDescendantsAreCompositing(RenderObject*) const;
     bool requiresCompositingForFullScreen(RenderObject*) const;
     bool requiresCompositingForScrollableFrame() const;
+    bool requiresCompositingForPosition(RenderObject*, const RenderLayer*) const;
 
     bool requiresScrollLayer(RootLayerAttachment) const;
     bool requiresHorizontalScrollbarLayer() const;
@@ -310,10 +311,13 @@ private:
     // FIXME: once set, this is never cleared.
     mutable bool m_compositingDependsOnGeometry;
 
+    mutable bool m_compositingNeedsUpdate;
+
     bool m_compositing;
     bool m_compositingLayersNeedRebuild;
     bool m_flushingLayers;
     bool m_forceCompositingMode;
+    bool m_compositeForFixedPosition;
 
     RootLayerAttachment m_rootLayerAttachment;
 
