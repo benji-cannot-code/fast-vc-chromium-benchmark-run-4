4531fa510f00f8c63baa0e9e1d57517e5104ba57
sque@chromium.org <sque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 09:05:55 +0000

    chromeos: initializer for power supply status struct
    This eliminates the need to explicitly initialize it with "= {}"
    everywhere.
    
    BUG=chromium:101867
    TEST=Valgrind error should not happen (see bug)
    
    Signed-off-by: Simon Que <sque@chromium.org>
    
    R=satorux@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8402020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107548 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/power_menu_button_browsertest.cc
+++ chrome/browser/chromeos/status/power_menu_button_browsertest.cc
@@ -49,7 +49,7 @@ class PowerMenuButtonTest : public CrosInProcessBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryMissingTest) {
-  PowerSupplyStatus status = {};
+  PowerSupplyStatus status;
   // No battery present.
   status.battery_is_present    = false;
   status.battery_percentage    = 42.0;
@@ -62,7 +62,7 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryMissingTest) {
 }
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargedTest) {
-  PowerSupplyStatus status = {};
+  PowerSupplyStatus status;
   // The battery is fully charged, and line power is plugged in.
   status.battery_is_present    = true;
   status.battery_percentage    = 42.0;
@@ -75,7 +75,7 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargedTest) {
 }
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargingTest) {
-  PowerSupplyStatus status = {};
+  PowerSupplyStatus status;
   status.battery_is_present    = true;
   status.battery_is_full       = false;
   status.line_power_on         = true;
@@ -92,7 +92,7 @@ IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryChargingTest) {
 }
 
 IN_PROC_BROWSER_TEST_F(PowerMenuButtonTest, BatteryDischargingTest) {
-  PowerSupplyStatus status = {};
+  PowerSupplyStatus status;
   status.battery_is_present    = true;
   status.battery_is_full       = false;
   status.line_power_on         = false;
