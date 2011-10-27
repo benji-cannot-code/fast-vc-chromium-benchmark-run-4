a92bb912595468f7669dc5ae65a4a2236fd0b556
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 11:30:44 +0000

    Web Inspector: Add support for advanced search in content scripts. https://bugs.webkit.org/show_bug.cgi?id=70923
    Reviewed by Yury Semikhatsky.
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/ScriptsSearchScope.js:
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.SettingsScreen):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsSearchScope.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsSearchScope.js
@@ -56,8 +56,8 @@ WebInspector.ScriptsSearchScope.prototype = {
             return !uiSourceCode.isContentScript;
         }
         
-        // FIXME: Add setting to search in content scripts as well.
-        uiSourceCodes.filter(filterOutContentScripts);
+        if (!WebInspector.settings.searchInContentScripts.get())
+            uiSourceCodes = uiSourceCodes.filter(filterOutContentScripts);
 
         function continueSearch()
         {
@@ -187,3 +187,5 @@ WebInspector.ScriptsSearchResultsPane.LinkifierFormatter.prototype = {
 }
 
 WebInspector.ScriptsSearchResultsPane.LinkifierFormatter.prototype.__proto__ = WebInspector.DebuggerPresentationModel.LinkifierFormatter.prototype;
+
+WebInspector.settings.searchInContentScripts = WebInspector.settings.createSetting("searchInContentScripts", false);
