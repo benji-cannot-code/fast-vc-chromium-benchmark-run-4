58d8b754d1f6262d07577e5c40fba7326b9f9e3f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 22:05:17 +0000

    Unreviewed, rolling out r98626. http://trac.webkit.org/changeset/98626 https://bugs.webkit.org/show_bug.cgi?id=71066
    Does not compile on cr-win (Requested by jamesr_ on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-10-27
    
    * platform/graphics/chromium/FontChromiumWin.cpp:
    (WebCore::Font::drawGlyphs):
    * platform/graphics/skia/SkiaFontWin.cpp:
    (WebCore::paintSkiaText):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/SkiaFontWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/SkiaFontWin.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformContextSkia.h"
 #include "Gradient.h"
 #include "Pattern.h"
-#include "PlatformSupport.h"
 #include "SkCanvas.h"
 #include "SkPaint.h"
 #include "SkShader.h"
@@ -238,9 +237,6 @@ void paintSkiaText(GraphicsContext* context,
     PlatformContextSkia* platformContext = context->platformContext();
     SkCanvas* canvas = platformContext->canvas();
     TextDrawingModeFlags textMode = platformContext->getTextDrawingMode();
-    // Ensure font load for printing, because PDF device needs it.
-    if (canvas->getTopDevice()->getDeviceCapabilities() & SkDevice::kVector_Capability)
-        PlatformSupport::ensureFontLoaded(hfont);
 
     // Filling (if necessary). This is the common case.
     SkPaint paint;
