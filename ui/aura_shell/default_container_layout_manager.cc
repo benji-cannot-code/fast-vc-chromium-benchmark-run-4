ec8c67ad46ebdbb3b699eab2591fafe7efeb3c77
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 22:49:02 +0000

    Drag and rotate windows between/within workspaces
    BUG=none
    TEST=new tests in workspace_manager_unittests
    
    Review URL: http://codereview.chromium.org/8391035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107643 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/default_container_layout_manager.cc
+++ ui/aura_shell/default_container_layout_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "ui/aura/desktop.h"
+#include "ui/aura/event.h"
 #include "ui/aura/window.h"
 #include "ui/aura/screen_aura.h"
 #include "ui/aura/window_types.h"
@@ -45,6 +46,29 @@ void DefaultContainerLayoutManager::CancelMoveOrResize(
   drag_window_ = NULL;
 }
 
+void DefaultContainerLayoutManager::ProcessMove(
+    aura::Window* drag,
+    aura::MouseEvent* event) {
+  AutoReset<bool> reset(&ignore_calculate_bounds_, true);
+
+  // TODO(oshima): Just zooming out may (and will) move/swap window without
+  // a users's intent. We probably should scroll viewport, but that may not
+  // be enough. See crbug.com/101826 for more discussion.
+  workspace_manager_->SetOverview(true);
+
+  gfx::Point point_in_owner = event->location();
+  aura::Window::ConvertPointToWindow(
+      drag,
+      owner_,
+      &point_in_owner);
+  // TODO(oshima): We should support simply moving to another
+  // workspace when the destination workspace has enough room to accomodate.
+  aura::Window* rotate_target =
+      workspace_manager_->FindRotateWindowForLocation(point_in_owner);
+  if (rotate_target)
+    workspace_manager_->RotateWindows(drag, rotate_target);
+}
+
 void DefaultContainerLayoutManager::EndMove(
     aura::Window* drag,
     aura::MouseEvent* evnet) {
@@ -53,7 +77,8 @@ void DefaultContainerLayoutManager::EndMove(
   drag_window_ = NULL;
   Workspace* workspace = workspace_manager_->GetActiveWorkspace();
   if (workspace)
-    workspace->Layout(NULL);
+    workspace->Layout(NULL, NULL);
+  workspace_manager_->SetOverview(false);
 }
 
 void DefaultContainerLayoutManager::EndResize(
@@ -63,7 +88,8 @@ void DefaultContainerLayoutManager::EndResize(
   drag_window_ = NULL;
   Workspace* workspace = workspace_manager_->GetActiveWorkspace();
   if (workspace)
-    workspace->Layout(NULL);
+    workspace->Layout(NULL, NULL);
+  workspace_manager_->SetOverview(false);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
