840b36b90593d1f77266bb71699955e991d53534
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 19:17:17 +0000

    Added logic that will retry to reload+reconnect the mobile payment portal page 5 times before it eventually gives up and shows service unavailable error.
    Expanded frame sniffer to send an event when a frame which is being watched gets finally loaded. From the sent error (or its absence) and this new event, we can finally reconstruct on the browser side if a given iframe was loaded successfully or not.  
    
    BUG=chromium-os:20525, chromium-os:21738
    TEST=tricky one to test since it repros on 3g network flakyness. one can edit /usr/lib/cromo/config/madison.ini and put a bad value for olp_root there to trigger this.
    Review URL: http://codereview.chromium.org/8341037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107613 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/mobile_setup_dialog.h
+++ chrome/browser/ui/webui/chromeos/mobile_setup_dialog.h
@@ -12,36 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "chrome/browser/ui/webui/html_dialog_ui.h"
 
-class MobileSetupDialog : public HtmlDialogUIDelegate {
+class MobileSetupDialog {
  public:
-  MobileSetupDialog();
-
   static void Show();
-  static MobileSetupDialog* GetInstance();
-
- protected:
-  friend struct DefaultSingletonTraits<MobileSetupDialog>;
-  virtual ~MobileSetupDialog();
-
-  void OnCloseDialog();
-
-  // HtmlDialogUIDelegate overrides.
-  virtual bool IsDialogModal() const OVERRIDE;
-  virtual string16 GetDialogTitle() const OVERRIDE;
-  virtual GURL GetDialogContentURL() const OVERRIDE;
-  virtual void GetWebUIMessageHandlers(
-      std::vector<WebUIMessageHandler*>* handlers) const OVERRIDE;
-  virtual void GetDialogSize(gfx::Size* size) const OVERRIDE;
-  virtual std::string GetDialogArgs() const OVERRIDE;
-  virtual void OnDialogClosed(const std::string& json_retval) OVERRIDE;
-  virtual void OnCloseContents(TabContents* source,
-                               bool* out_close_dialog) OVERRIDE;
-  virtual bool ShouldShowDialogTitle() const OVERRIDE;
-  virtual bool HandleContextMenu(const ContextMenuParams& params) OVERRIDE;
 
  private:
-  void ShowDialog();
-
   DISALLOW_COPY_AND_ASSIGN(MobileSetupDialog);
 };
 
