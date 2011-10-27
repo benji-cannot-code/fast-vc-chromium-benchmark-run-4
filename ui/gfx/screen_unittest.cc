d191f54eac2a6853469dc9a53633784ed6747d86
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 17:18:00 +0000

    aura: Remove failing ScreenTest for aura.
    BUG=none
    TEST=gfx_unittets on linux_aura should go green
    
    Review URL: http://codereview.chromium.org/8391006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107592 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/screen_unittest.cc
+++ ui/gfx/screen_unittest.cc
@@ -6,30 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/screen.h"
 
-#if defined(USE_AURA)
-#include "base/message_loop.h"
-#include "ui/aura/desktop.h"
-#endif
-
 namespace {
 
-#if defined(USE_AURA)
-class ScreenTest : public testing::Test {
- public:
-  ScreenTest() {
-    aura::Desktop::GetInstance()->ShowDesktop();
-  }
-
-  virtual ~ScreenTest() {
-    aura::Desktop::GetInstance()->DeleteInstanceForTesting();
-  }
-
- private:
-  MessageLoopForUI message_loop_;
-};
-#else
 typedef testing::Test ScreenTest;
-#endif
 
 TEST_F(ScreenTest, GetPrimaryMonitorSize) {
   // We aren't actually testing that it's correct, just that it's sane.
--- ui/ui_unittests.gypi
+++ ui/ui_unittests.gypi
@@ -161,8 +161,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ],
         }],
         ['use_aura==1', {
-          'dependencies': [
-            'aura/aura.gyp:aura',
+          'sources!': [
+            'gfx/screen_unittest.cc',
           ],
         }],
       ],
