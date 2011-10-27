bb5d4c963fc71c85df4733f11ba0096519e870ad
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 00:39:05 +0000

    Disable sync promo
    BUG=101251
    TEST=
    TBR=dbeam@chromium.org
    
    Review URL: http://codereview.chromium.org/8404015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo_ui.cc
+++ chrome/browser/ui/webui/sync_promo_ui.cc
@@ -189,8 +189,7 @@ bool SyncPromoUI::ShouldShowSyncPromoAtStartup(Profile* profile,
   if (!AllowPromoAtStartupForCurrentBrand())
     return false;
 
-  // Default to show the promo.
-  return true;
+  return false;
 }
 
 void SyncPromoUI::DidShowSyncPromoAtStartup(Profile* profile) {
