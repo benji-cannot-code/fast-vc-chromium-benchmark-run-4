a7a57cd6001ec9db7dbd9faf1a520b4eb7717c48
vlaviano@chromium.org <vlaviano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 22:46:13 +0000

    Chrome OS: Attach bluetooth UI to device discovery API
    BUG=chromium-os:22140
    TEST=Visit system settings page, click "Find devices" button, observe UI and read chrome logs.
    
    Change-Id: Idb0aa1650663f9c95d4c44194d6fa3b7e660d668
    
    
    Review URL: http://codereview.chromium.org/8233042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/bluetooth_list_element.js
+++ chrome/browser/resources/options/chromeos/bluetooth_list_element.js
@@ -15,9 +15,11 @@ cr.define('options.system.bluetooth', function() {
    * @enum {string}
    */
   Constants.DEVICE_TYPE = {
+    COMPUTER: 'computer',
     HEADSET: 'headset',
     KEYBOARD: 'keyboard',
-    MOUSE: 'mouse'
+    MOUSE: 'mouse',
+    PHONE: 'phone',
   };
 
   /**
--- chrome/browser/ui/webui/options/chromeos/bluetooth_options_handler.h
+++ chrome/browser/ui/webui/options/chromeos/bluetooth_options_handler.h
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_OPTIONS_CHROMEOS_BLUETOOTH_OPTIONS_HANDLER_H_
 #pragma once
 
+#include <string>
+
+#include "chrome/browser/chromeos/bluetooth/bluetooth_adapter.h"
+#include "chrome/browser/chromeos/bluetooth/bluetooth_manager.h"
 #include "chrome/browser/ui/webui/options/chromeos/cros_options_page_ui_handler.h"
 
 namespace base {
@@ -16,7 +20,9 @@ class DictionaryValue;
 namespace chromeos {
 
 // Handler for Bluetooth options on the system options page.
-class BluetoothOptionsHandler : public chromeos::CrosOptionsPageUIHandler {
+class BluetoothOptionsHandler : public chromeos::CrosOptionsPageUIHandler,
+                                public chromeos::BluetoothManager::Observer,
+                                public chromeos::BluetoothAdapter::Observer {
  public:
   BluetoothOptionsHandler();
   virtual ~BluetoothOptionsHandler();
@@ -47,14 +53,35 @@ class BluetoothOptionsHandler : public chromeos::CrosOptionsPageUIHandler {
   // for device are "deviceName", "deviceId", "deviceType" and "deviceStatus".
   void DeviceNotification(const base::DictionaryValue& device);
 
+  // chromeos::BluetoothManager::Observer override.
+  virtual void DefaultAdapterChanged(chromeos::BluetoothAdapter* adapter);
+
+  // chromeos::BluetoothAdapter::Observer override.
+  virtual void DiscoveryStarted(const std::string& adapter_id);
+
+  // chromeos::BluetoothAdapter::Observer override.
+  virtual void DiscoveryEnded(const std::string& adapter_id);
+
+  // chromeos::BluetoothAdapter::Observer override.
+  virtual void DeviceFound(const std::string& adapter_id,
+                           chromeos::BluetoothDevice* device);
+
  private:
+  // Compares |adapter| with our cached default adapter ID and calls
+  // DefaultAdapterChanged if there has been an unexpected change.
+  void ValidateDefaultAdapter(chromeos::BluetoothAdapter* adapter);
+
   // Simulates extracting a list of available bluetooth devices.
   // Called when emulating ChromeOS from a desktop environment.
   void GenerateFakeDeviceList();
 
+  // The id of the current default bluetooth adapter.
+  // The empty string represents "none".
+  std::string default_adapter_id_;
+
   DISALLOW_COPY_AND_ASSIGN(BluetoothOptionsHandler);
 };
 
-}
+}  // namespace chromeos
 
 #endif  // CHROME_BROWSER_UI_WEBUI_OPTIONS_CHROMEOS_BLUETOOTH_OPTIONS_HANDLER_H_
