ce2c7a14c716352f071e2683ac0d4661df963e27
nduca@chromium.org <nduca@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 20:42:54 +0000

    Unreviewed, fix CCSchedulerStateMachine build errors.
    * platform/graphics/chromium/cc/CCSchedulerStateMachine.cpp:
    * platform/graphics/chromium/cc/CCSchedulerStateMachine.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98631 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSchedulerStateMachine.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSchedulerStateMachine.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CCSchedulerStateMachine_h
 #define CCSchedulerStateMachine_h
 
+#include <wtf/Noncopyable.h>
+
 namespace WebCore {
 
 // The CCSchedulerStateMachine decides how to coordinates main thread activites
@@ -41,8 +43,6 @@ namespace WebCore {
 class CCSchedulerStateMachine {
 public:
     CCSchedulerStateMachine();
-    CCSchedulerStateMachine(const CCSchedulerStateMachine& that);
-    CCSchedulerStateMachine& operator=(const CCSchedulerStateMachine& that);
 
     enum CommitState {
         COMMIT_STATE_IDLE,
