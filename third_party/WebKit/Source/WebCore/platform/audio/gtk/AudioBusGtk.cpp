a6462f785024d0fd16975cba4fce624c8ab1ba90
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-27 12:31:44 +0000

    [GStreamer] WebAudio AudioFileReader implementation https://bugs.webkit.org/show_bug.cgi?id=69834
    .:
    
    Patch by Philippe Normand  <pnormand@igalia.com> and Zan Dobersek <zandobersek@gmail.com> on 2011-10-27
    Reviewed by Martin Robinson.
    
    * configure.ac: Check for libfftw and gstreamer-audio if WebAudio
    build is enabled.
    
    Source/WebCore:
    
    Patch by Philippe Normand  <pnormand@igalia.com> and Zan Dobersek <zandobersek@gmail.com> on 2011-10-27
    Reviewed by Martin Robinson.
    
    Basic FileReader implementation, supporting one or 2 audio
    channels. An empty AudioDestination is also provided, its complete
    implementation is handled in bug 69835.
    
    * GNUmakefile.am:
    * GNUmakefile.list.am:
    * platform/audio/gstreamer/AudioDestinationGStreamer.cpp: Added.
    (WebCore::AudioDestination::create):
    (WebCore::AudioDestination::hardwareSampleRate):
    (WebCore::AudioDestinationGStreamer::AudioDestinationGStreamer):
    (WebCore::AudioDestinationGStreamer::~AudioDestinationGStreamer):
    (WebCore::AudioDestinationGStreamer::start):
    (WebCore::AudioDestinationGStreamer::stop):
    * platform/audio/gstreamer/AudioDestinationGStreamer.h: Added.
    (WebCore::AudioDestinationGStreamer::isPlaying):
    (WebCore::AudioDestinationGStreamer::sampleRate):
    (WebCore::AudioDestinationGStreamer::sourceProvider):
    * platform/audio/gstreamer/AudioFileReaderGStreamer.cpp: Added.
    (WebCore::getGStreamerAudioCaps):
    (WebCore::getFloatFromByteReader):
    (WebCore::copyGstreamerBuffersToAudioChannel):
    (WebCore::onAppsinkNewBufferCallback):
    (WebCore::messageCallback):
    (WebCore::onGStreamerDeinterleavePadAddedCallback):
    (WebCore::onGStreamerDeinterleaveReadyCallback):
    (WebCore::onGStreamerDecodebinPadAddedCallback):
    (WebCore::AudioFileReader::AudioFileReader):
    (WebCore::AudioFileReader::~AudioFileReader):
    (WebCore::AudioFileReader::handleBuffer):
    (WebCore::AudioFileReader::handleMessage):
    (WebCore::AudioFileReader::handleNewDeinterleavePad):
    (WebCore::AudioFileReader::deinterleavePadsConfigured):
    (WebCore::AudioFileReader::plugDeinterleave):
    (WebCore::AudioFileReader::createBus):
    (WebCore::createBusFromAudioFile):
    (WebCore::createBusFromInMemoryAudioFile):
    * platform/audio/gtk/AudioBusGtk.cpp: Added.
    (WebCore::AudioBus::loadPlatformResource):
    
    Source/WebKit/gtk:
    
    Reviewed by Martin Robinson.
    
    * GNUmakefile.am: Link against libfftw.
    
    Source/WebKit2:
    
    Reviewed by Martin Robinson.
    
    * GNUmakefile.am: Link against libfftw.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/WebCore/platform/audio/gtk/AudioBusGtk.cpp
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+ *  Copyright (C) 2011 Igalia S.L
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+
+#if ENABLE(WEB_AUDIO)
+
+#include "AudioBus.h"
+
+#include "AudioFileReader.h"
+#include "GOwnPtr.h"
+
+#include <gio/gio.h>
+#include <glib.h>
+
+namespace WebCore {
+
+PassOwnPtr<AudioBus> AudioBus::loadPlatformResource(const char* name, float sampleRate)
+{
+    GOwnPtr<gchar> filename(g_strdup_printf("%s.wav", name));
+    GOwnPtr<gchar> absoluteFilename(g_build_filename(DATA_DIR, "webkitgtk-"WEBKITGTK_API_VERSION_STRING, "resources", "audio", filename.get(), NULL));
+    return createBusFromAudioFile(absoluteFilename.get(), false, sampleRate);
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(WEB_AUDIO)
