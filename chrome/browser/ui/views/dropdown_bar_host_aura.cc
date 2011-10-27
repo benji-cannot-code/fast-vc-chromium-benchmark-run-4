c3477012af5784126d6a21c2d2b893b7a200ebf5
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-27 15:43:58 +0000

    Gets the findbar to at least show up.
    We need to do some more work to make it clip properly.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8401004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107575 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/dropdown_bar_host_aura.cc
+++ chrome/browser/ui/views/dropdown_bar_host_aura.cc
@@ -6,17 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/dropdown_bar_host.h"
 
 #include "base/logging.h"
+#include "ui/aura/window.h"
+#include "views/widget/widget.h"
 
 NativeWebKeyboardEvent DropdownBarHost::GetKeyboardEvent(
      const TabContents* contents,
      const views::KeyEvent& key_event) {
-  // TODO(beng):
-  NOTIMPLEMENTED();
-  return NativeWebKeyboardEvent();
+  return NativeWebKeyboardEvent(key_event.native_event());
 }
 
 void DropdownBarHost::SetWidgetPositionNative(const gfx::Rect& new_pos,
                                               bool no_redraw) {
-  // TODO(beng):
-  NOTIMPLEMENTED();
+  if (!host_->IsVisible())
+    host_->GetNativeView()->Show();
+  host_->GetNativeView()->SetBounds(new_pos);
 }
