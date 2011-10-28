f7dedefd11aa9968b5f2b43277a7a0f5a8a7d468
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-28 23:18:08 +0000

    If visibility changes while an accelerated animation is running, element jumps around https://bugs.webkit.org/show_bug.cgi?id=29984
    Source/WebCore:
    
    Reviewed by Chris Marrin.
    
    Compositing now affects whether RenderLayers for visibility:hidden elements
    are included in z-order lists. So we have to dirty those lists when we enter
    compopsiting mode.
    
    Test: compositing/visibility/animation-visibility.html
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::updateVisibilityStatus): Removed trailing whitespace.
    (WebCore::RenderLayer::dirtyZOrderLists): Call dirtyZOrderListsInternal(), which doesn't have
    to ping the compositor.
    (WebCore::RenderLayer::dirtyZOrderListsInternal):
    (WebCore::RenderLayer::dirtyZOrderListsIncludingDescendants): Recursively dirty z-order
    lists.
    * rendering/RenderLayer.h:
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::enableCompositingMode): When going into compositing mode,
    dirty all z-order lists.
    
    LayoutTests:
    
    Reviewed by Chris Marrin.
    
    Add testcase for visibility changing in the middle of an accelerated animation.
    
    * animations/resources/animation-test-helpers.js: Add some constants for readability.
    (checkExpectedValue): Add support for testing 'visibility'.
    * compositing/visibility/animation-visibility-expected.png: Added.
    * compositing/visibility/animation-visibility-expected.txt: Added.
    * compositing/visibility/animation-visibility.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
@@ -377,6 +377,7 @@ public:
     bool isStackingContext() const { return !hasAutoZIndex() || renderer()->isRenderView(); }
 
     void dirtyZOrderLists();
+    void dirtyZOrderListsIncludingDescendants();
     void dirtyStackingContextZOrderLists();
     void updateZOrderLists();
     Vector<RenderLayer*>* posZOrderList() const { return m_posZOrderList; }
@@ -541,6 +542,8 @@ private:
     void setParent(RenderLayer* parent);
     void setFirstChild(RenderLayer* first) { m_first = first; }
     void setLastChild(RenderLayer* last) { m_last = last; }
+    
+    void dirtyZOrderListsInternal();
 
     LayoutPoint renderBoxLocation() const { return renderer()->isBox() ? toRenderBox(renderer())->location() : LayoutPoint(); }
     LayoutUnit renderBoxX() const { return renderBoxLocation().x(); }
