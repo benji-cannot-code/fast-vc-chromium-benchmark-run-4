cf2525f5b2efc06f79444d276fed78fc438eb259
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 19:46:37 +0000

    Fix a flaky GViewRequestInterceptorTest by refreshing the plugins list before every test case.
    BUG=none
    TEST=unit_tests --gtest_filter=GViewRequestInterceptorTest.* and unit_tests --gtest_filter=GViewRequestInterceptorTest.InterceptPdfWithNoPlugins should both pass; Official CrOS bot goes green.
    Review URL: http://codereview.chromium.org/8417015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gview_request_interceptor_unittest.cc
+++ chrome/browser/chromeos/gview_request_interceptor_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <string>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/message_loop.h"
@@ -102,6 +103,10 @@ class GViewRequestInterceptorTest : public testing::Test {
     ASSERT_TRUE(PathService::Get(chrome::FILE_PDF_PLUGIN, &pdf_path_));
 
     handler_ = new content::DummyResourceHandler();
+
+    PluginService::GetInstance()->RefreshPluginList();
+    PluginService::GetInstance()->GetPlugins(base::Bind(&QuitMessageLoop));
+    MessageLoop::current()->Run();
   }
 
   virtual void TearDown() {
