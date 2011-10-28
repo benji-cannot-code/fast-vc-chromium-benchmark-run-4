ba76996b3b4181d039e6de952a3217d8b7f3fccf
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 18:21:26 +0000

    Sync Promo: Correctly setup sync
    With r107251 the sync promo page stopped loading.
    
    The problem was due to a difference between the sync promo and the options Sign In UI. The options UI calls ShowSetupUI() when showing the sign in overlay. The promo wasn't calling this causing sync not to be setup correctly.
    
    Fix was to match the behavior of the options UI and call ShowSetupUI() when showing the sync promo page.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8416033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107757 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_promo.js
+++ chrome/browser/resources/sync_promo.js
@@ -60,6 +60,7 @@ cr.define('sync_promo', function() {
       $('sync-setup-login-promo-column').hidden = false;
       $('promo-skip').hidden = false;
 
+      this.showSetupUI_();
       chrome.send('SyncPromo:Initialize');
 
       var self = this;
