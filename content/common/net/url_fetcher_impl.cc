4a0d022fe917f57e30dcdeea6fbfc132807bd8db
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 20:10:39 +0000

    Add histogram to measure string response size of URLFetcher.
    BUG=101929
    TEST=URLFetcher*Test.*
    
    
    Review URL: http://codereview.chromium.org/8416001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107780 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/net/url_fetcher_impl.cc
+++ content/common/net/url_fetcher_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop_proxy.h"
+#include "base/metrics/histogram.h"
 #include "base/platform_file.h"
 #include "base/stl_util.h"
 #include "base/string_util.h"
@@ -1067,6 +1068,9 @@ bool URLFetcherImpl::GetResponseAsString(
     return false;
 
   *out_response_string = core_->data_;
+  UMA_HISTOGRAM_MEMORY_KB("UrlFetcher.StringResponseSize",
+                          (core_->data_.length() / 1024));
+
   return true;
 }
 
