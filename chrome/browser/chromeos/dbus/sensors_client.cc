c2e27c16fb10619115d11359f9e395417da0e473
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 14:37:13 +0000

    Restore chrome/browser/ui/touch/animation/* Fix BrowserThread bug.
    BUG=None
    TEST=Linux touch build succeeds.
    
    Review URL: http://codereview.chromium.org/8400062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107728 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/sensors_client.cc
+++ chrome/browser/chromeos/dbus/sensors_client.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "chrome/browser/chromeos/system/runtime_environment.h"
-#include "content/browser/browser_thread.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/browser/sensors/sensors_provider.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
