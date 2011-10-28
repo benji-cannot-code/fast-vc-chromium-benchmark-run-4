5c530237da99d3b90320bf4f59af016a6765a23b
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 00:04:17 +0000

    Build fix for win_aura
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8343040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107661 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/aura_constants.cc
+++ ui/aura/aura_constants.cc
@@ -3,16 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_AURA_AURA_CONSTANTS_H_
-#define UI_AURA_AURA_CONSTANTS_H_
-#pragma once
-
-#include "ui/aura/aura_export.h"
+#include "ui/aura/aura_constants.h"
 
 namespace aura {
 
-AURA_EXPORT extern const char* kTooltipTextKey;
+const char* kTooltipTextKey = "TooltipTextKey";
 
-}  // namespace aura
-
-#endif  // UI_AURA_AURA_CONSTANTS_H_
+}
