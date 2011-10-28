cda40dfd3c506743e8a3ecf63838caebc0b8679c
droger@google.com <droger@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 16:52:41 +0000

    Removed useless header include.
    BUG=NONE
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/8413030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107742 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_contents.h
+++ chrome/browser/prerender/prerender_contents.h
@@ -16,11 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/prerender/prerender_final_status.h"
 #include "content/browser/tab_contents/tab_contents_observer.h"
-#include "content/browser/renderer_host/render_view_host_delegate.h"
 #include "content/public/browser/notification_registrar.h"
 
 class Profile;
 class RenderViewHost;
+class RenderViewHostDelegate;
 class TabContents;
 class TabContentsWrapper;
 struct FaviconURL;
