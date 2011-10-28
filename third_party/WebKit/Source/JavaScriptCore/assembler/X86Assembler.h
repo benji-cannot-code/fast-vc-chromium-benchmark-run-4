5e31090663e7bcff2e4b3b513df457f430ba6949
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-28 01:09:53 +0000

    ENH: Add 8 bit string support to JSC JIT https://bugs.webkit.org/show_bug.cgi?id=71073
    Changed the JIT String character access generation to create code
    to check the character size and load8() or load16() as approriate.
    
    Reviewed by Gavin Barraclough.
    
    * assembler/MacroAssemblerX86Common.h:
    (JSC::MacroAssemblerX86Common::load8):
    * assembler/X86Assembler.h:
    (JSC::X86Assembler::movzbl_mr):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileGetCharCodeAt):
    (JSC::DFG::SpeculativeJIT::compileGetByValOnString):
    * jit/JITInlineMethods.h:
    (JSC::JIT::emitLoadCharacterString):
    * jit/JITPropertyAccess.cpp:
    (JSC::JIT::stringGetByValStubGenerator):
    * jit/JITPropertyAccess32_64.cpp:
    (JSC::JIT::stringGetByValStubGenerator):
    * jit/JSInterfaceJIT.h:
    (JSC::ThunkHelpers::stringImplFlagsOffset):
    (JSC::ThunkHelpers::stringImpl8BitFlag):
    * jit/ThunkGenerators.cpp:
    (JSC::stringCharLoad):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/X86Assembler.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/X86Assembler.h
@@ -1232,6 +1232,11 @@ public:
         m_formatter.twoByteOp(OP2_MOVZX_GvEw, dst, base, index, scale, offset);
     }
 
+    void movzbl_mr(int offset, RegisterID base, RegisterID dst)
+    {
+        m_formatter.twoByteOp(OP2_MOVZX_GvEb, dst, base, offset);
+    }
+    
     void movzbl_mr(int offset, RegisterID base, RegisterID index, int scale, RegisterID dst)
     {
         m_formatter.twoByteOp(OP2_MOVZX_GvEb, dst, base, index, scale, offset);
