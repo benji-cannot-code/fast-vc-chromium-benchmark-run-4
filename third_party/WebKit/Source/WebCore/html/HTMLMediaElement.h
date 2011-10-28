67977a0d6f0a70c19efe336d1cbfd7f63d168ba5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-28 18:43:36 +0000

    Unreviewed, rolling out r98736. http://trac.webkit.org/changeset/98736 https://bugs.webkit.org/show_bug.cgi?id=71125
    This patch broke perf/array-binary-search.html test (Requested
    by annacc on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-10-28
    
    Source/WebCore:
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadTextTracks):
    (WebCore::HTMLMediaElement::addTrack):
    * html/HTMLMediaElement.h:
    * html/HTMLTrackElement.cpp:
    (WebCore::HTMLTrackElement::insertedIntoTree):
    (WebCore::HTMLTrackElement::willRemove):
    * html/HTMLTrackElement.h:
    * html/LoadableTextTrack.cpp:
    (WebCore::LoadableTextTrack::cueLoadingCompleted):
    
    LayoutTests:
    
    * media/track/track-load-error-readyState-expected.txt: Removed.
    * media/track/track-load-error-readyState.html: Removed.
    * media/track/track-load-from-element-readyState-expected.txt: Removed.
    * media/track/track-load-from-element-readyState.html: Removed.
    * media/track/track-load-from-src-readyState-expected.txt: Removed.
    * media/track/track-load-from-src-readyState.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
@@ -58,9 +58,6 @@ class Uint8Array;
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
 class Widget;
 #endif
-#if ENABLE(VIDEO_TRACK)
-class HTMLTrackElement;
-#endif
 
 // FIXME: The inheritance from MediaPlayerClient here should be private inheritance.
 // But it can't be until the Chromium WebMediaPlayerClientImpl class is fixed so it
@@ -184,9 +181,6 @@ public:
 
 #if ENABLE(VIDEO_TRACK)
     PassRefPtr<TextTrack> addTrack(const String& kind, const String& label = "", const String& language = "");
-    virtual void trackWasAdded(HTMLTrackElement*);
-    virtual void trackWillBeRemoved(HTMLTrackElement*);
-    virtual void trackSourceChanged(HTMLTrackElement*);
 #endif
 
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
@@ -359,7 +353,6 @@ private:
 
 #if ENABLE(VIDEO_TRACK)
     void loadTextTracks();
-    void loadNextTextTrack(HTMLTrackElement*);
 
     // TextTrackClient
     virtual void textTrackReadyStateChanged(TextTrack*);
