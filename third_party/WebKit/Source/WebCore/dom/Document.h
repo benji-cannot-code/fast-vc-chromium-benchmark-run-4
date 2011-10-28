e1be22198515b6b7659d975a9c1da16630dd88e5
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-28 13:27:32 +0000

    Reset line numbers for scripts generated with document.write. https://bugs.webkit.org/show_bug.cgi?id=71099
    Reviewed by Yury Semikhatsky.
    
    Source/JavaScriptCore:
    
    * wtf/text/TextPosition.h:
    (WTF::OrdinalNumber::OrdinalNumber):
    
    Source/WebCore:
    
    Test: http/tests/inspector-enabled/document-write.html
    
    * dom/Document.h:
    (WebCore::Document::isInDocumentWrite):
    * dom/ScriptElement.cpp:
    (WebCore::ScriptElement::prepareScript):
    
    LayoutTests:
    
    * http/tests/inspector-enabled/document-write-expected.txt: Added.
    * http/tests/inspector-enabled/document-write.html: Added.
    * platform/chromium-win/fast/dom/nested-script-exceptions-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -1106,6 +1106,8 @@ public:
     void removeCachedMicroDataItemList(MicroDataItemList*, const String&);
 #endif
     
+    bool isInDocumentWrite() { return m_writeRecursionDepth > 0; }
+
 protected:
     Document(Frame*, const KURL&, bool isXHTML, bool isHTML);
 
