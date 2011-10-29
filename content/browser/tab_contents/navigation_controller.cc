cbd71264c80a94dfd97d2917d4cb4ea2a1bc72bc
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-29 02:51:52 +0000

    Temporary code to track down increase in renderer crashes.
    Review URL: http://codereview.chromium.org/8418043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107858 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/navigation_controller.cc
+++ content/browser/tab_contents/navigation_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/logging.h"
+#include "base/string_number_conversions.h"  // Temporary
 #include "base/string_util.h"
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browser_url_handler.h"
 #include "content/browser/child_process_security_policy.h"
 #include "content/browser/in_process_webkit/session_storage_namespace.h"
+#include "content/browser/renderer_host/render_view_host.h"  // Temporary
 #include "content/browser/site_instance.h"
 #include "content/browser/tab_contents/interstitial_page.h"
 #include "content/browser/tab_contents/navigation_details.h"
@@ -672,6 +674,24 @@ content::NavigationType NavigationController::ClassifyNavigation(
     // release builds. Instead, we'll kill the renderer process to be safe.
     LOG(ERROR) << "terminating renderer for bad navigation: " << params.url;
     UserMetrics::RecordAction(UserMetricsAction("BadMessageTerminate_NC"));
+
+    // Temporary code so we can get more information:
+    std::string temp = params.url.spec();;
+    temp.append("#");
+    temp.append(params.referrer.spec());
+    temp.append("#");
+    temp.append(base::IntToString(params.page_id));
+    for (int i = static_cast<int>(entries_.size()) - 1; i >= 0; --i) {
+      if (entries_[i]->site_instance() == tab_contents_->GetSiteInstance()) {
+        temp.append("#");
+        temp.append(base::IntToString(entries_[i]->page_id()));
+      }
+    }
+    GURL url(temp);
+    tab_contents_->render_view_host()->Send(new ViewMsg_TempCrashWithData(url));
+    return content::NAVIGATION_TYPE_NAV_IGNORE;
+
+
     if (tab_contents_->GetSiteInstance()->HasProcess())
       tab_contents_->GetSiteInstance()->GetProcess()->ReceivedBadMessage();
     return content::NAVIGATION_TYPE_NAV_IGNORE;
