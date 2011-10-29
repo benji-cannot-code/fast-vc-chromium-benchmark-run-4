09aebf9f523e3a8f21cc794251c70a6af0ff1bd3
noelutz@google.com <noelutz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-29 22:32:01 +0000

    Measure how often downloaded executables match the download whitelist. This CL also tells all Chrome users who have SafeBrowsing turned on to download this new whitelist.
    BUG=
    TEST=SafeBrowsingProtocolParsingTest
    
    
    Review URL: http://codereview.chromium.org/8417040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/protocol_parser.cc
+++ chrome/browser/safe_browsing/protocol_parser.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -336,8 +336,9 @@ bool SafeBrowsingProtocolParser::ParseAddChunk(const std::string& list_name,
   SBEntry::Type type = hash_len == sizeof(SBPrefix) ?
       SBEntry::ADD_PREFIX : SBEntry::ADD_FULL_HASH;
 
-  if (list_name == safe_browsing_util::kBinHashList) {
-    // kBinHashList only contains prefixes, no HOSTKEY and COUNT.
+  if (list_name == safe_browsing_util::kBinHashList ||
+      list_name == safe_browsing_util::kDownloadWhiteList) {
+    // These lists only contain prefixes, no HOSTKEY and COUNT.
     DCHECK_EQ(0, remaining % hash_len);
     prefix_count = remaining / hash_len;
     SBChunkHost chunk_host;
