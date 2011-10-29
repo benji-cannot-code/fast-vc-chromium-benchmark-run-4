859a8de9f9c9e85d2b32888bbf36353d68b40895
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-29 02:16:24 +0000

    Convert some non-debug logging on content/common to debug logging.
    [ Re-land of 107815 ]
    Review URL: http://codereview.chromium.org/8416055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107852 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/net/url_fetcher_impl.cc
+++ content/common/net/url_fetcher_impl.cc
@@ -335,7 +335,7 @@ URLFetcherImpl::Core::TempFileWriter::~TempFileWriter() {
 
 void URLFetcherImpl::Core::TempFileWriter::CreateTempFile() {
   DCHECK(core_->io_message_loop_proxy_->BelongsToCurrentThread());
-  CHECK(file_message_loop_proxy_.get());
+  DCHECK(file_message_loop_proxy_.get());
   base::FileUtilProxy::CreateTemporary(
       file_message_loop_proxy_,
       0,  // No additional file flags.
@@ -539,14 +539,14 @@ URLFetcherImpl::Core::~Core() {
 
 void URLFetcherImpl::Core::Start() {
   DCHECK(delegate_loop_proxy_);
-  CHECK(request_context_getter_) << "We need an URLRequestContext!";
+  DCHECK(request_context_getter_) << "We need an URLRequestContext!";
   if (io_message_loop_proxy_) {
     DCHECK_EQ(io_message_loop_proxy_,
               request_context_getter_->GetIOMessageLoopProxy());
   } else {
     io_message_loop_proxy_ = request_context_getter_->GetIOMessageLoopProxy();
   }
-  CHECK(io_message_loop_proxy_.get()) << "We need an IO message loop proxy";
+  DCHECK(io_message_loop_proxy_.get()) << "We need an IO message loop proxy";
 
   io_message_loop_proxy_->PostTask(
       FROM_HERE, base::Bind(&Core::StartOnIOThread, this));
@@ -627,7 +627,7 @@ void URLFetcherImpl::Core::CompleteAddingUploadDataChunk(
 void URLFetcherImpl::Core::AppendChunkToUpload(const std::string& content,
                                            bool is_last_chunk) {
   DCHECK(delegate_loop_proxy_);
-  CHECK(io_message_loop_proxy_.get());
+  DCHECK(io_message_loop_proxy_.get());
   io_message_loop_proxy_->PostTask(
       FROM_HERE,
       base::Bind(&Core::CompleteAddingUploadDataChunk, this, content,
@@ -763,7 +763,7 @@ void URLFetcherImpl::Core::StartURLRequest() {
     return;
   }
 
-  CHECK(request_context_getter_);
+  DCHECK(request_context_getter_);
   DCHECK(!request_.get());
 
   g_registry.Get().AddURLFetcherCore(this);
@@ -811,8 +811,8 @@ void URLFetcherImpl::Core::StartURLRequest() {
 
   // If we are writing the response to a file, the only caller
   // of this function should have created it and not written yet.
-  CHECK(!temp_file_writer_.get() ||
-        temp_file_writer_->total_bytes_written() == 0);
+  DCHECK(!temp_file_writer_.get() ||
+         temp_file_writer_->total_bytes_written() == 0);
 
   request_->Start();
 }
@@ -867,7 +867,7 @@ void URLFetcherImpl::Core::OnCompletedURLRequest(
 }
 
 void URLFetcherImpl::Core::InformDelegateFetchIsComplete() {
-  CHECK(delegate_loop_proxy_->BelongsToCurrentThread());
+  DCHECK(delegate_loop_proxy_->BelongsToCurrentThread());
   if (delegate_) {
     delegate_->OnURLFetchComplete(fetcher_);
   }
--- content/common/process_watcher_posix.cc
+++ content/common/process_watcher_posix.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static bool IsChildDead(pid_t child) {
   const pid_t result = HANDLE_EINTR(waitpid(child, NULL, WNOHANG));
   if (result == -1) {
-    PLOG(ERROR) << "waitpid(" << child << ")";
+    DPLOG(ERROR) << "waitpid(" << child << ")";
     NOTREACHED();
   } else if (result > 0) {
     // The child has died.
@@ -48,8 +48,8 @@ class BackgroundReaper : public base::PlatformThread::Delegate {
     if (timeout_ == 0) {
       pid_t r = HANDLE_EINTR(waitpid(child_, NULL, 0));
       if (r != child_) {
-        PLOG(ERROR) << "While waiting for " << child_
-                    << " to terminate, we got the following result: " << r;
+        DPLOG(ERROR) << "While waiting for " << child_
+                     << " to terminate, we got the following result: " << r;
       }
       return;
     }
@@ -68,10 +68,10 @@ class BackgroundReaper : public base::PlatformThread::Delegate {
       // SIGKILL is uncatchable. Since the signal was delivered, we can
       // just wait for the process to die now in a blocking manner.
       if (HANDLE_EINTR(waitpid(child_, NULL, 0)) < 0)
-        PLOG(WARNING) << "waitpid";
+        DPLOG(WARNING) << "waitpid";
     } else {
-      LOG(ERROR) << "While waiting for " << child_ << " to terminate we"
-                 << " failed to deliver a SIGKILL signal (" << errno << ").";
+      DLOG(ERROR) << "While waiting for " << child_ << " to terminate we"
+                  << " failed to deliver a SIGKILL signal (" << errno << ").";
     }
   }
 
