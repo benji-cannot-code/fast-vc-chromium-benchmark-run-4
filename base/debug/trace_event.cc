9ad1499ccae8ae215900bdae8dd16fe0ea11fa5d
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-29 03:16:52 +0000

    add classes trace_analyzer::Query and TraceAnalyzer to improve testing (retry)
    http://codereview.chromium.org/7981004/
    
    BUG=95714
    TEST=base_unittests
    
    Review URL: http://codereview.chromium.org/8418044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107861 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event.cc
+++ base/debug/trace_event.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "base/bind.h"
 #include "base/format_macros.h"
-#include "base/memory/ref_counted_memory.h"
 #include "base/process_util.h"
 #include "base/stringprintf.h"
 #include "base/threading/thread_local.h"
@@ -120,22 +119,6 @@ void TraceValue::AppendAsJSON(std::string* out) const {
 
 namespace {
 
-const char* GetPhaseStr(TraceEventPhase phase) {
-  switch(phase) {
-    case TRACE_EVENT_PHASE_BEGIN:
-      return "B";
-    case TRACE_EVENT_PHASE_INSTANT:
-      return "I";
-    case TRACE_EVENT_PHASE_END:
-      return "E";
-    case TRACE_EVENT_PHASE_METADATA:
-      return "M";
-    default:
-      NOTREACHED() << "Invalid phase argument";
-      return "?";
-  }
-}
-
 size_t GetAllocLength(const char* str) { return str ? strlen(str) + 1 : 0; }
 
 // Copies |*member| into |*buffer|, sets |*member| to point to this new
@@ -222,6 +205,38 @@ TraceEvent::TraceEvent(unsigned long process_id,
 TraceEvent::~TraceEvent() {
 }
 
+const char* TraceEvent::GetPhaseString(TraceEventPhase phase) {
+  switch(phase) {
+    case TRACE_EVENT_PHASE_BEGIN:
+      return "B";
+    case TRACE_EVENT_PHASE_INSTANT:
+      return "I";
+    case TRACE_EVENT_PHASE_END:
+      return "E";
+    case TRACE_EVENT_PHASE_METADATA:
+      return "M";
+    default:
+      NOTREACHED() << "Invalid phase argument";
+      return "?";
+  }
+}
+
+TraceEventPhase TraceEvent::GetPhase(const char* phase) {
+  switch(*phase) {
+    case 'B':
+      return TRACE_EVENT_PHASE_BEGIN;
+    case 'I':
+      return TRACE_EVENT_PHASE_INSTANT;
+    case 'E':
+      return TRACE_EVENT_PHASE_END;
+    case 'M':
+      return TRACE_EVENT_PHASE_METADATA;
+    default:
+      NOTREACHED() << "Invalid phase name";
+      return TRACE_EVENT_PHASE_METADATA;
+  }
+}
+
 void TraceEvent::AppendEventsAsJSON(const std::vector<TraceEvent>& events,
                                     size_t start,
                                     size_t count,
@@ -234,7 +249,7 @@ void TraceEvent::AppendEventsAsJSON(const std::vector<TraceEvent>& events,
 }
 
 void TraceEvent::AppendAsJSON(std::string* out) const {
-  const char* phase_str = GetPhaseStr(phase_);
+  const char* phase_str = GetPhaseString(phase_);
   int64 time_int64 = timestamp_.ToInternalValue();
   // Category name checked at category creation time.
   DCHECK(!strchr(name_, '"'));
