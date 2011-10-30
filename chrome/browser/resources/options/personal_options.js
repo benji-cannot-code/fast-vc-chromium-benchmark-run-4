1428f0586b49930bb5090a0a51b6044a0c96b9a8
qghc36@motorola.com <qghc36@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-30 03:33:00 +0000

    Options: Disable the delete button when no profile is selected in personal settings. According to this change, Delete button will be disabled when there are multiple profiles and none of the profiles is selected. If profile list is hidden i.e if there is only one default profile then Delete Button is also disabled.
    BUG=100625
    TEST='Delete...' button should be disabled when no profile is selected in chrome://settings/personal page.
    
    
    Review URL: http://codereview.chromium.org/8339014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/personal_options.js
+++ chrome/browser/resources/options/personal_options.js
@@ -58,14 +58,7 @@ cr.define('options', function() {
       options.personal_options.ProfileList.decorate(profilesList);
       profilesList.autoExpands = true;
 
-      profilesList.onchange = function(event) {
-        var selectedProfile = profilesList.selectedItem;
-        var hasSelection = selectedProfile != null;
-        var hasSingleProfile = profilesList.dataModel.length == 1;
-        $('profiles-manage').disabled = !hasSelection ||
-            !selectedProfile.isCurrentProfile;
-        $('profiles-delete').disabled = !hasSingleProfile && !hasSelection;
-      };
+      profilesList.onchange = self.setProfileViewButtonsStatus_;
       $('profiles-create').onclick = function(event) {
         chrome.send('createProfile');
       };
@@ -222,6 +215,20 @@ cr.define('options', function() {
     },
 
     /**
+     * Helper function to set the status of profile view buttons to disabled or
+     * enabled, depending on the number of profiles and selection status of the
+     * profiles list.
+     */
+    setProfileViewButtonsStatus_: function() {
+      var profilesList = $('profiles-list');
+      var selectedProfile = profilesList.selectedItem;
+      var hasSelection = selectedProfile != null;
+      var hasSingleProfile = profilesList.dataModel.length == 1;
+      $('profiles-manage').disabled = !hasSelection;
+      $('profiles-delete').disabled = !hasSelection || hasSingleProfile;
+    },
+
+    /**
      * Display the correct dialog layout, depending on how many profiles are
      * available.
      * @param {number} numProfiles The number of profiles to display.
@@ -252,6 +259,7 @@ cr.define('options', function() {
       // add it to the list, even if the list is hidden so we can access it
       // later.
       $('profiles-list').dataModel = new ArrayDataModel(profiles);
+      this.setProfileViewButtonsStatus_();
     },
 
     setStartStopButtonVisible_: function(visible) {
