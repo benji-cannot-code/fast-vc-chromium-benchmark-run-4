48b16bbfb494a33464e64bba242908c194e0f077
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-31 11:55:52 +0000

    [GTK] Add webkit_settings_new_with_settings() to WebKit2 GTK+ API https://bugs.webkit.org/show_bug.cgi?id=71111
    Reviewed by Martin Robinson.
    
    Convenient method to create a WebKitSettings object with initial
    settings.
    
    * UIProcess/API/gtk/WebKitSettings.cpp:
    (webkit_settings_new_with_settings):
    * UIProcess/API/gtk/WebKitSettings.h:
    * UIProcess/API/gtk/docs/webkit2gtk-sections.txt:
    * UIProcess/API/gtk/tests/TestWebKitSettings.cpp:
    (testWebKitSettingsNewWithSettings):
    (beforeAll):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitSettings.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitSettings.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 
 #include "TestMain.h"
+#include <JavaScriptCore/GRefPtr.h>
 #include <gtk/gtk.h>
 #include <webkit2/webkit2.h>
 
@@ -106,9 +107,22 @@ static void testWebKitSettings(Test*, gconstpointer)
 #endif
 }
 
+void testWebKitSettingsNewWithSettings(Test* test, gconstpointer)
+{
+    GRefPtr<WebKitSettings> settings = adoptGRef(webkit_settings_new_with_settings("enable-javascript", FALSE,
+                                                                                   "auto-load-images", FALSE,
+                                                                                   "load-icons-ignoring-image-load-setting", TRUE,
+                                                                                   NULL));
+    test->assertObjectIsDeletedWhenTestFinishes(G_OBJECT(settings.get()));
+    g_assert(!webkit_settings_get_enable_javascript(settings.get()));
+    g_assert(!webkit_settings_get_auto_load_images(settings.get()));
+    g_assert(webkit_settings_get_load_icons_ignoring_image_load_setting(settings.get()));
+}
+
 void beforeAll()
 {
     Test::add("WebKitSettings", "webkit-settings", testWebKitSettings);
+    Test::add("WebKitSettings", "new-with-settings", testWebKitSettingsNewWithSettings);
 }
 
 void afterAll()
