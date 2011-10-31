f293d21243be678654dc70041cc41ff34ef52747
jeffm@apple.com <jeffm@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-31 22:55:01 +0000

    Add more APIs to WKUserContentURLPattern.h https://bugs.webkit.org/show_bug.cgi?id=71245
    Add additional WKUserContentURLPattern APIs to match what was provided by
    the equivalent WebKit1 API.
    
    Reviewed by Darin Adler.
    
    * Shared/API/c/WKUserContentURLPattern.cpp:
    (WKUserContentURLPatternCopyHost): Added.
    (WKUserContentURLPatternCopyScheme): Added.
    (WKUserContentURLPatternIsValid): Added.
    (WKUserContentURLPatternMatchesSubdomains): Added.
            
    * Shared/API/c/WKUserContentURLPattern.h: Added new APIs.
            
    * Shared/WebUserContentURLPattern.h:
    (WebKit::WebUserContentURLPattern::host): Added.
    (WebKit::WebUserContentURLPattern::scheme): Added.
    (WebKit::WebUserContentURLPattern::isValid): Added.
    (WebKit::WebUserContentURLPattern::matchesSubdomains): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/API/c/WKUserContentURLPattern.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/API/c/WKUserContentURLPattern.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKUserContentURLPattern.h"
 
 #include "WKAPICast.h"
+#include "WKString.h"
 #include "WebUserContentURLPattern.h"
 
 using namespace WebKit;
@@ -43,7 +44,27 @@ WKUserContentURLPatternRef WKUserContentURLPatternCreate(WKStringRef patternRef)
     return toAPI(userContentURLPattern.release().leakRef());
 }
 
+WKStringRef WKUserContentURLPatternCopyHost(WKUserContentURLPatternRef urlPatternRef)
+{
+    return toCopiedAPI(toImpl(urlPatternRef)->host());
+}
+
+WKStringRef WKUserContentURLPatternCopyScheme(WKUserContentURLPatternRef urlPatternRef)
+{
+    return toCopiedAPI(toImpl(urlPatternRef)->scheme());
+}
+
+bool WKUserContentURLPatternIsValid(WKUserContentURLPatternRef urlPatternRef)
+{
+    return toImpl(urlPatternRef)->isValid();
+}
+
 bool WKUserContentURLPatternMatchesURL(WKUserContentURLPatternRef urlPatternRef, WKURLRef urlRef)
 {
     return toImpl(urlPatternRef)->matchesURL(toWTFString(urlRef));
+}
+
+bool WKUserContentURLPatternMatchesSubdomains(WKUserContentURLPatternRef urlPatternRef)
+{
+    return toImpl(urlPatternRef)->matchesSubdomains();
 }
