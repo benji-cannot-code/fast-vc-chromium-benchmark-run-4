70d6c186f4c28eac9d89579ce363dca76c2b8a29
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-31 22:51:19 +0000

    More work on making plug-ins work better with transforms https://bugs.webkit.org/show_bug.cgi?id=71241
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Export symbols used by WebKit2.
    
    * WebCore.exp.in:
    
    Source/WebKit2:
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.cpp:
    (WebKit::NetscapePlugin::geometryDidChange):
    Implement NetscapePlugin::geometryDidChange and store the plug-in size,
    the clip rect and the root view transform. Use the transform to compute the window
    relative frame and clip rects.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.h:
    Add new member variables.
    
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::viewGeometryDidChange):
    Always call the new Plugin::geometryDidChange.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98892 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NetscapePluginModule.h"
 #include "Plugin.h"
 #include "RunLoop.h"
+#include <WebCore/AffineTransform.h>
 #include <WebCore/GraphicsLayer.h>
 #include <WebCore/IntRect.h>
 #include <wtf/HashMap.h>
@@ -233,6 +234,14 @@ private:
     RefPtr<NetscapePluginModule> m_pluginModule;
     NPP_t m_npp;
     NPWindow m_npWindow;
+
+    WebCore::IntSize m_pluginSize;
+
+    // The clip rect in plug-in coordinates.
+    WebCore::IntRect m_clipRect;
+
+    // A transform that can be used to convert from root view coordinates to plug-in coordinates.
+    WebCore::AffineTransform m_pluginToRootViewTransform;
 
     // FIXME: Get rid of these.
     WebCore::IntRect m_frameRectInWindowCoordinates;
