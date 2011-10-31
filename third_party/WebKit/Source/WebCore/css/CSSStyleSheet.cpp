ea14cf688e9aadc3df934d6611fa07935f01acde
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-10-31 17:02:49 +0000

    CSSRule: Devirtualize insertedIntoParent() https://bugs.webkit.org/show_bug.cgi?id=71223
    Reviewed by Antti Koivisto.
    
    Moved insertedIntoParent() down into CSSImportRule and renamed it to
    requestStyleSheet(). CSSImportRule is the only user of this function.
    
    * css/CSSImportRule.cpp:
    (WebCore::CSSImportRule::requestStyleSheet):
    * css/CSSImportRule.h:
    * css/CSSRule.h:
    * css/CSSStyleSheet.cpp:
    (WebCore::CSSStyleSheet::append):
    (WebCore::CSSStyleSheet::insertRule):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSheet.cpp
@@ -89,7 +89,8 @@ void CSSStyleSheet::append(PassRefPtr<CSSRule> child)
 {
     CSSRule* c = child.get();
     m_children.append(child);
-    c->insertedIntoParent();
+    if (c->isImportRule())
+        static_cast<CSSImportRule*>(c)->requestStyleSheet();
 }
 
 void CSSStyleSheet::remove(unsigned index)
@@ -137,7 +138,8 @@ unsigned CSSStyleSheet::insertRule(const String& rule, unsigned index, Exception
 
     CSSRule* c = r.get();
     m_children.insert(index, r.release());
-    c->insertedIntoParent();
+    if (c->isImportRule())
+        static_cast<CSSImportRule*>(c)->requestStyleSheet();
 
     styleSheetChanged();
 
