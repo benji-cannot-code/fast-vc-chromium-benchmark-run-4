50c1ac2e4a72fe8187cef47d9be3f37a81f82e56
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-01 23:02:17 +0000

    Switch RenderObject to layout abstraction https://bugs.webkit.org/show_bug.cgi?id=71249
    Switch RenderObject to LayoutRect/Size/Point abstraction.
    
    Reviewed by Darin Adler.
    
    No new tests.
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::drawLineForBoxSide):
    (WebCore::RenderObject::addPDFURLRect):
    (WebCore::RenderObject::absoluteBoundingBoxRect):
    (WebCore::RenderObject::absoluteFocusRingQuads):
    (WebCore::RenderObject::addAbsoluteRectForLayer):
    (WebCore::RenderObject::repaintAfterLayoutIfNeeded):
    (WebCore::RenderObject::computeRectForRepaint):
    (WebCore::RenderObject::viewRect):
    (WebCore::RenderObject::mapLocalToContainer):
    (WebCore::RenderObject::localCaretRect):
    (WebCore::RenderObject::addDashboardRegions):
    (WebCore::RenderObject::maximalOutlineSize):
    (WebCore::RenderObject::adjustRectForOutlineAndShadow):
    * rendering/RenderObject.h:
    (WebCore::RenderObject::absoluteBoundingBoxRectIgnoringTransforms):
    (WebCore::RenderObject::absoluteClippedOverflowRect):
    (WebCore::RenderObject::computeAbsoluteRepaintRect):
    (WebCore::RenderObject::absoluteOutlineBounds):
    (WebCore::RenderObject::outlineBoundsForRepaint):
    (WebCore::adjustForAbsoluteZoom):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.h
@@ -110,8 +110,8 @@ struct DashboardRegionValue {
     }
 
     String label;
-    IntRect bounds;
-    IntRect clip;
+    LayoutRect bounds;
+    LayoutRect clip;
     int type;
 };
 #endif
@@ -245,7 +245,7 @@ protected:
     }
     //////////////////////////////////////////
 private:
-    void addAbsoluteRectForLayer(IntRect& result);
+    void addAbsoluteRectForLayer(LayoutRect& result);
     void setLayerNeedsFullRepaint();
 
 public:
@@ -647,8 +647,8 @@ public:
     virtual void absoluteRects(Vector<LayoutRect>&, const LayoutPoint&) const { }
 
     // FIXME: useTransforms should go away eventually
-    IntRect absoluteBoundingBoxRect(bool useTransform = true) const;
-    IntRect absoluteBoundingBoxRectIgnoringTransforms() const { return absoluteBoundingBoxRect(false); }
+    LayoutRect absoluteBoundingBoxRect(bool useTransform = true) const;
+    LayoutRect absoluteBoundingBoxRectIgnoringTransforms() const { return absoluteBoundingBoxRect(false); }
 
     // Build an array of quads in absolute coords for line boxes
     virtual void absoluteQuads(Vector<FloatQuad>&, bool* /*wasFixed*/ = 0) const { }
@@ -705,7 +705,7 @@ public:
 
     // Returns the rect that should be repainted whenever this object changes.  The rect is in the view's
     // coordinate space.  This method deals with outlines and overflow.
-    IntRect absoluteClippedOverflowRect() const
+    LayoutRect absoluteClippedOverflowRect() const
     {
         return clippedOverflowRectForRepaint(0);
     }
@@ -714,13 +714,13 @@ public:
 
     // Given a rect in the object's coordinate space, compute a rect suitable for repainting
     // that rect in view coordinates.
-    void computeAbsoluteRepaintRect(IntRect& r, bool fixed = false) const
+    void computeAbsoluteRepaintRect(LayoutRect& r, bool fixed = false) const
     {
         return computeRectForRepaint(0, r, fixed);
     }
     // Given a rect in the object's coordinate space, compute a rect suitable for repainting
     // that rect in the coordinate space of repaintContainer.
-    virtual void computeRectForRepaint(RenderBoxModelObject* repaintContainer, IntRect&, bool fixed = false) const;
+    virtual void computeRectForRepaint(RenderBoxModelObject* repaintContainer, LayoutRect&, bool fixed = false) const;
 
     // If multiple-column layout results in applying an offset to the given point, add the same
     // offset to the given size.
@@ -736,7 +736,7 @@ public:
     bool hasReflection() const { return m_hasReflection; }
 
     // Applied as a "slop" to dirty rect checks during the outline painting phase's dirty-rect checks.
-    int maximalOutlineSize(PaintPhase) const;
+    LayoutUnit maximalOutlineSize(PaintPhase) const;
 
     void setHasMarkupTruncation(bool b = true) { m_hasMarkupTruncation = b; }
     bool hasMarkupTruncation() const { return m_hasMarkupTruncation; }
@@ -844,7 +844,7 @@ public:
     
     virtual void addFocusRingRects(Vector<LayoutRect>&, const LayoutPoint&) { };
 
-    IntRect absoluteOutlineBounds() const
+    LayoutRect absoluteOutlineBounds() const
     {
         return outlineBoundsForRepaint(0);
     }
@@ -856,21 +856,21 @@ protected:
     virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle);
     void propagateStyleToAnonymousChildren(bool blockChildrenOnly = false);
 
-    void drawLineForBoxSide(GraphicsContext*, int x1, int y1, int x2, int y2, BoxSide,
+    void drawLineForBoxSide(GraphicsContext*, LayoutUnit x1, LayoutUnit y1, LayoutUnit x2, LayoutUnit y2, BoxSide,
                             Color, EBorderStyle, int adjbw1, int adjbw2, bool antialias = false);
 
     void paintFocusRing(GraphicsContext*, const LayoutPoint&, RenderStyle*);
     void paintOutline(GraphicsContext*, const LayoutRect&);
-    void addPDFURLRect(GraphicsContext*, const IntRect&);
+    void addPDFURLRect(GraphicsContext*, const LayoutRect&);
 
-    virtual IntRect viewRect() const;
+    virtual LayoutRect viewRect() const;
 
-    void adjustRectForOutlineAndShadow(IntRect&) const;
+    void adjustRectForOutlineAndShadow(LayoutRect&) const;
 
     virtual void willBeDestroyed();
     void arenaDelete(RenderArena*, void* objectBase);
 
-    virtual LayoutRect outlineBoundsForRepaint(RenderBoxModelObject* /*repaintContainer*/, IntPoint* /*cachedOffsetToRepaintContainer*/ = 0) const { return LayoutRect(); }
+    virtual LayoutRect outlineBoundsForRepaint(RenderBoxModelObject* /*repaintContainer*/, LayoutPoint* /*cachedOffsetToRepaintContainer*/ = 0) const { return LayoutRect(); }
 
 private:
     RenderStyle* firstLineStyleSlowCase() const;
