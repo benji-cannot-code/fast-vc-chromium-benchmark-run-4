15222396f2a629777eefddb4cb8aa240730642cc
alokp@chromium.org <alokp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-01 19:18:58 +0000

    [chromium] Add testing for --enable-accelerated-drawing https://bugs.webkit.org/show_bug.cgi?id=70822
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Test: platform/chromium/compositing/accelerated-drawing/alpha.html
    
    * WebCore.exp.in:
    * page/Settings.cpp:
    * page/Settings.h:
    (WebCore::Settings::setAcceleratedDrawingEnabled):
    * testing/Internals.cpp:
    (WebCore::Internals::setAcceleratedDrawingEnabled):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    LayoutTests:
    
    * platform/chromium/compositing/accelerated-drawing: Added.
    * platform/chromium/compositing/accelerated-drawing/alpha-expected.png: Added.
    * platform/chromium/compositing/accelerated-drawing/alpha-expected.txt: Added.
    * platform/chromium/compositing/accelerated-drawing/alpha.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@98990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.h
+++ third_party/WebKit/Source/WebCore/testing/Internals.h
@@ -78,6 +78,7 @@ public:
 
     void setForceCompositingMode(Document*, bool enabled, ExceptionCode&);
     void setEnableCompositingForFixedPosition(Document*, bool enabled, ExceptionCode&);
+    void setAcceleratedDrawingEnabled(Document*, bool enabled, ExceptionCode&);
 
     void setEnableScrollAnimator(Document*, bool enabled, ExceptionCode&);
     void setZoomAnimatorTransform(Document*, float scale, float tx, float ty, ExceptionCode&);
