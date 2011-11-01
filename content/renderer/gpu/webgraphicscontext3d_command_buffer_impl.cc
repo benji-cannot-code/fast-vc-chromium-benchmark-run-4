ea0e31a8476ffe30a22d6cc6e62662bde4b8d1cf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-01 20:27:37 +0000

    Make LazyInstance managed Locks leaky.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8430019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108159 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/webgraphicscontext3d_command_buffer_impl.cc
+++ content/renderer/gpu/webgraphicscontext3d_command_buffer_impl.cc
@@ -36,7 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "webkit/glue/gl_bindings_skia_cmd_buffer.h"
 
-static base::LazyInstance<base::Lock>
+static base::LazyInstance<base::Lock,
+                          base::LeakyLazyInstanceTraits<base::Lock> >
     g_all_shared_contexts_lock(base::LINKER_INITIALIZED);
 static base::LazyInstance<std::set<WebGraphicsContext3DCommandBufferImpl*> >
     g_all_shared_contexts(base::LINKER_INITIALIZED);
@@ -348,8 +349,7 @@ bool WebGraphicsContext3DCommandBufferImpl::readBackFramebuffer(
   if (mustRestoreFBO) {
     gl_->BindFramebuffer(GL_FRAMEBUFFER, buffer);
   }
-   gl_->ReadPixels(0, 0, width, height,
-                   GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+  gl_->ReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
   // Swizzle red and blue channels
   // TODO(kbr): expose GL_BGRA as extension
--- webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
+++ webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
@@ -168,7 +168,7 @@ class GLInProcessContext : public base::SupportsWeakPtr<GLInProcessContext> {
   CommandBufferService* GetCommandBufferService();
 
  private:
-  GLInProcessContext(GLInProcessContext* parent);
+  explicit GLInProcessContext(GLInProcessContext* parent);
 
   bool Initialize(bool onscreen,
                   gfx::PluginWindowHandle render_surface,
@@ -210,7 +210,8 @@ const int32 kTransferBufferSize = 1024 * 1024;
 static base::LazyInstance<
     std::set<WebGraphicsContext3DInProcessCommandBufferImpl*> >
         g_all_shared_contexts(base::LINKER_INITIALIZED);
-static base::LazyInstance<base::Lock>
+static base::LazyInstance<base::Lock,
+                          base::LeakyLazyInstanceTraits<base::Lock> >
     g_all_shared_contexts_lock(base::LINKER_INITIALIZED);
 
 // Singleton used to initialize and terminate the gles2 library.
@@ -838,8 +839,7 @@ bool WebGraphicsContext3DInProcessCommandBufferImpl::readBackFramebuffer(
   if (mustRestoreFBO) {
     gl_->BindFramebuffer(GL_FRAMEBUFFER, framebuffer);
   }
-   gl_->ReadPixels(0, 0, width, height,
-                   GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+  gl_->ReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
   // Swizzle red and blue channels
   // TODO(kbr): expose GL_BGRA as extension
@@ -1638,8 +1638,7 @@ void WebGraphicsContext3DInProcessCommandBufferImpl::OnSwapBuffersComplete() {
 }
 
 void WebGraphicsContext3DInProcessCommandBufferImpl::setContextLostCallback(
-    WebGraphicsContext3D::WebGraphicsContextLostCallback* cb)
-{
+    WebGraphicsContext3D::WebGraphicsContextLostCallback* cb) {
   context_lost_callback_ = cb;
 }
 
