1481ba4c81987cd1a03b48c903bbd371b49b379a
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-01 17:40:06 +0000

    refactor video capture in renderer process This patch is a split of http://codereview.chromium.org/8304017/ . 1. Since all clients of VideoCaptureImpl accept pixel frames with different dimension than requested, there is no need to distinguish them. Remove "resolution_fixed" in media::VideoCapture::VideoCaptureCapability. 2. VideoCaptureImpl is now taking the largested dimension from client requests and send it to browser process. 3. remove some unnecessary code in VideoCaptureModuleImpl since webrtc ViE can handle different frame size.
    BUG=none
    TEST=trybots
    Review URL: http://codereview.chromium.org/8400084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108126 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/video_capture_module_impl.cc
+++ content/renderer/media/video_capture_module_impl.cc
@@ -17,7 +17,6 @@ VideoCaptureModuleImpl::VideoCaptureModuleImpl(
       thread_("VideoCaptureModuleImpl"),
       vc_manager_(vc_manager),
       state_(media::VideoCapture::kStopped),
-      got_first_frame_(false),
       width_(-1),
       height_(-1),
       frame_rate_(-1),
@@ -161,7 +160,6 @@ void VideoCaptureModuleImpl::StartCaptureInternal(
   cap.height = capability.height;
   cap.max_fps = capability.maxFPS;
   cap.raw_type = media::VideoFrame::I420;
-  cap.resolution_fixed = true;
   capture_engine_->StartCapture(this, cap);
 }
 
@@ -192,7 +190,6 @@ void VideoCaptureModuleImpl::OnStoppedOnCaptureThread(
 
   VLOG(1) << "Capture Stopped!!! ";
   state_ = media::VideoCapture::kStopped;
-  got_first_frame_ = false;
   width_ = -1;
   height_ = -1;
   frame_rate_ = -1;
@@ -211,11 +208,6 @@ void VideoCaptureModuleImpl::OnBufferReadyOnCaptureThread(
 
   if (state_ != media::VideoCapture::kStarted)
     return;
-
-  if (!got_first_frame_) {
-    got_first_frame_ = true;
-    start_time_ = buf->timestamp;
-  }
 
   frameInfo_.width = buf->width;
   frameInfo_.height = buf->height;
