ea3a4fc2e41c543d40564b214e267f373eb91dac
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-01 18:18:34 +0000

    Add warning to Mac that passwords are shared between profiles through the underlying keychain.
    BUG=102176
    TEST=on Mac, with more than one profile existing, a warning is shown on the settings/personal page about profile password sharing.
    
    Review URL: http://codereview.chromium.org/8430017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/personal_options.js
+++ chrome/browser/resources/options/personal_options.js
@@ -135,6 +135,9 @@ cr.define('options', function() {
         $('manage-passwords').disabled = true;
       }
 
+      $('mac-passwords-warning').hidden =
+          !(localStrings.getString('macPasswordsWarning'));
+
       if (PersonalOptions.disableAutofillManagement()) {
         $('autofill-settings').disabled = true;
 
