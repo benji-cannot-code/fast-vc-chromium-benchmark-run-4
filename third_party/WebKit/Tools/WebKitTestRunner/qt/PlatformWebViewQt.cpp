09b63e7e3d6013009b49ee0b23c8d8cf6d5f2487
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-02 18:09:37 +0000

    [Qt] REGRESSION: 5 editing tests fail https://bugs.webkit.org/show_bug.cgi?id=71117
    Reviewed by Andreas Kling.
    
    Tools:
    
    Make sure that the view is properly focused, in order to get the
    didBeginEditing DRT calls.
    
    This requires activating the window (the call will result in a FocusIn
    event to the window as well as setting QGuiApplication::focusWindow())
    as well as enabling the focus on the view item. The signature of
    setFocus has changed to take a boolean.
    
    * WebKitTestRunner/qt/PlatformWebViewQt.cpp:
    (WTR::WrapperWindow::handleStatusChanged):
    
    LayoutTests:
    
    * platform/qt-5.0/Skipped: Unskip tests that now pass.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QApplication>
 #include <QDeclarativeProperty>
 #include <QQuickView>
+#include <qwindowsysteminterface_qpa.h>
 
 namespace WTR {
 
@@ -58,9 +59,8 @@ private slots:
         m_view->setParentItem(rootObject());
         QDeclarativeProperty::write(m_view, "anchors.fill", qVariantFromValue(rootObject()));
 
-        QFocusEvent ev(QEvent::WindowActivate);
-        QApplication::sendEvent(m_view, &ev);
-        m_view->setFocus(Qt::OtherFocusReason);
+        QWindowSystemInterface::handleWindowActivated(this);
+        m_view->setFocus(true);
     }
 
 private:
