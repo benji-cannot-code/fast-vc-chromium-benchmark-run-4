9fe6919fc7f5fdd3b4efe9b14c4d6439ad1152e0
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 23:52:14 +0000

    Makes TabContentsViewViews not create a texture for the widget it created. To get this right required moving SadTabView into its own widget.
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8437020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/tab_contents_view_views.cc
+++ chrome/browser/ui/views/tab_contents/tab_contents_view_views.cc
@@ -40,7 +40,7 @@ using WebKit::WebInputEvent;
 TabContentsViewViews::TabContentsViewViews(TabContents* tab_contents)
     : tab_contents_(tab_contents),
       native_tab_contents_view_(NULL),
-      sad_tab_(NULL),
+      sad_tab_widget_(NULL),
       close_tab_after_drag_ends_(false),
       focus_manager_(NULL) {
   last_focused_view_storage_id_ =
@@ -91,9 +91,9 @@ RenderWidgetHostView* TabContentsViewViews::CreateViewForWidget(
   }
 
   // If we were showing sad tab, remove it now.
-  if (sad_tab_) {
-    SetContentsView(new views::View());
-    sad_tab_ = NULL;
+  if (sad_tab_widget_) {
+    sad_tab_widget_->Close();
+    sad_tab_widget_ = NULL;
   }
 
   return native_tab_contents_view_->CreateRenderWidgetHostView(
@@ -137,15 +137,19 @@ void TabContentsViewViews::OnTabCrashed(base::TerminationStatus status,
   if (browser_shutdown::GetShutdownType() != browser_shutdown::NOT_VALID)
     return;
 
-  // Force an invalidation to render sad tab.
   // Note that it's possible to get this message after the window was destroyed.
   if (GetNativeView()) {
     SadTabView::Kind kind =
         status == base::TERMINATION_STATUS_PROCESS_WAS_KILLED ?
         SadTabView::KILLED : SadTabView::CRASHED;
-    sad_tab_ = new SadTabView(tab_contents_, kind);
-    SetContentsView(sad_tab_);
-    sad_tab_->SchedulePaint();
+    views::Widget::InitParams sad_tab_widget_params(
+        views::Widget::InitParams::TYPE_CONTROL);
+    sad_tab_widget_params.parent_widget = this;
+    sad_tab_widget_params.bounds =
+        gfx::Rect(GetClientAreaScreenBounds().size());
+    sad_tab_widget_ = new views::Widget;
+    sad_tab_widget_->Init(sad_tab_widget_params);
+    sad_tab_widget_->SetContentsView(new SadTabView(tab_contents_, kind));
   }
 }
 
@@ -170,8 +174,8 @@ void TabContentsViewViews::Focus() {
     return;
   }
 
-  if (tab_contents_->is_crashed() && sad_tab_ != NULL) {
-    sad_tab_->RequestFocus();
+  if (tab_contents_->is_crashed() && sad_tab_widget_ != NULL) {
+    sad_tab_widget_->GetContentsView()->RequestFocus();
     return;
   }
 
@@ -382,7 +386,7 @@ TabContents* TabContentsViewViews::GetTabContents() {
 }
 
 bool TabContentsViewViews::IsShowingSadTab() const {
-  return tab_contents_->is_crashed() && sad_tab_;
+  return tab_contents_->is_crashed() && sad_tab_widget_;
 }
 
 void TabContentsViewViews::OnNativeTabContentsViewShown() {
@@ -465,4 +469,11 @@ void TabContentsViewViews::OnNativeWidgetVisibilityChanged(bool visible) {
   } else {
     tab_contents_->HideContents();
   }
+}
+
+void TabContentsViewViews::OnNativeWidgetSizeChanged(
+    const gfx::Size& new_size) {
+  if (sad_tab_widget_)
+    sad_tab_widget_->SetBounds(gfx::Rect(new_size));
+  views::Widget::OnNativeWidgetSizeChanged(new_size);
 }
