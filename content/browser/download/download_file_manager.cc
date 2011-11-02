d0a3f872f6f500779e58ef2b596a3b23af126537
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 17:09:32 +0000

    First pass at some Downloads UMA to get a handle on downloads performance.
    BUG=None
    
    
    Review URL: http://codereview.chromium.org/8135032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108309 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_file_manager.cc
+++ content/browser/download/download_file_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/download/download_file.h"
 #include "content/browser/download/download_manager.h"
 #include "content/browser/download/download_request_handle.h"
+#include "content/browser/download/download_stats.h"
 #include "content/browser/renderer_host/resource_dispatcher_host.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "content/public/browser/browser_thread.h"
@@ -156,6 +157,8 @@ void DownloadFileManager::UpdateDownload(
     DownloadId global_id, content::DownloadBuffer* buffer) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
   scoped_ptr<content::ContentVector> contents(buffer->ReleaseContents());
+
+  download_stats::RecordFileThreadReceiveBuffers(contents->size());
 
   DownloadFile* download_file = GetDownloadFile(global_id);
   bool had_error = false;
