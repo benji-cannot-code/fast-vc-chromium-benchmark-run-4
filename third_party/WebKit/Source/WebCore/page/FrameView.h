09033db011f0b8ba3b06bdd07b95399944749cd8
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-02 06:26:16 +0000

    Fix uses of LayoutUnit in Frame-, Scroll-, and RenderView https://bugs.webkit.org/show_bug.cgi?id=71321
    Reviewed by Darin Adler.
    
    Updating the usage of LayoutUnits in the *View classes to mirror the proper use
    as derived in the subpixellayout branch.
    
    This entails scrolling only with integers (and rounding once we've made the switch)
    and using integers for window coordinates, with LayoutUnits for content coordinates.
    
    No new tests -- no change in behavior.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::invalidateRect):
    (WebCore::FrameView::setFrameRect):
    (WebCore::FrameView::zoomAnimatorTransformChanged):
    (WebCore::FrameView::scrollContentsFastPath):
    (WebCore::FrameView::scrollContentsSlowPath):
    (WebCore::FrameView::scrollElementToRect):
    (WebCore::FrameView::setScrollPosition):
    (WebCore::FrameView::repaintContentRectangle):
    (WebCore::FrameView::scrollTo):
    (WebCore::FrameView::updateScrollCorner):
    * page/FrameView.h:
    (WebCore::FrameView::trackedRepaintRects):
    * platform/ScrollView.cpp:
    (WebCore::ScrollView::visibleContentRect):
    (WebCore::ScrollView::layoutWidth):
    (WebCore::ScrollView::layoutHeight):
    (WebCore::ScrollView::fixedLayoutSize):
    (WebCore::ScrollView::setFixedLayoutSize):
    (WebCore::ScrollView::contentsSize):
    (WebCore::ScrollView::setContentsSize):
    (WebCore::ScrollView::overhangAmount):
    (WebCore::ScrollView::updateScrollbars):
    (WebCore::ScrollView::rectToCopyOnScroll):
    (WebCore::ScrollView::scrollContents):
    (WebCore::ScrollView::windowToContents):
    (WebCore::ScrollView::screenToContents):
    (WebCore::ScrollView::scrollbarAtPoint):
    (WebCore::ScrollView::wheelEvent):
    * platform/ScrollView.h:
    (WebCore::ScrollView::visibleWidth):
    (WebCore::ScrollView::visibleHeight):
    (WebCore::ScrollView::contentsWidth):
    (WebCore::ScrollView::contentsHeight):
    (WebCore::ScrollView::adjustScrollPositionWithinRange):
    * rendering/RenderView.cpp:
    (WebCore::RenderView::paint):
    (WebCore::RenderView::shouldRepaint):
    (WebCore::RenderView::repaintViewRectangle):
    (WebCore::RenderView::repaintRectangleInViewAndCompositedLayers):
    (WebCore::RenderView::computeRectForRepaint):
    (WebCore::RenderView::selectionBounds):
    (WebCore::RenderView::viewRect):
    (WebCore::RenderView::unscaledDocumentRect):
    (WebCore::RenderView::documentRect):
    * rendering/RenderView.h:
    (WebCore::RenderView::printRect):
    (WebCore::RenderView::setPrintRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.h
+++ third_party/WebKit/Source/WebCore/page/FrameView.h
@@ -65,8 +65,8 @@ public:
 
     virtual HostWindow* hostWindow() const;
     
-    virtual void invalidateRect(const IntRect&);
-    virtual void setFrameRect(const IntRect&);
+    virtual void invalidateRect(const LayoutRect&);
+    virtual void setFrameRect(const LayoutRect&);
 
 #if ENABLE(REQUEST_ANIMATION_FRAME)
     void scheduleAnimation();
@@ -302,11 +302,11 @@ public:
     void setTracksRepaints(bool);
     bool isTrackingRepaints() const { return m_isTrackingRepaints; }
     void resetTrackedRepaints() { m_trackedRepaintRects.clear(); }
-    const Vector<IntRect>& trackedRepaintRects() const { return m_trackedRepaintRects; }
+    const Vector<LayoutRect>& trackedRepaintRects() const { return m_trackedRepaintRects; }
 
 protected:
-    virtual bool scrollContentsFastPath(const IntSize& scrollDelta, const LayoutRect& rectToScroll, const LayoutRect& clipRect);
-    virtual void scrollContentsSlowPath(const LayoutRect& updateRect);
+    virtual bool scrollContentsFastPath(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect);
+    virtual void scrollContentsSlowPath(const IntRect& updateRect);
 
     virtual bool isVerticalDocument() const;
     virtual bool isFlippedDocument() const;
@@ -351,7 +351,7 @@ private:
     virtual void invalidateScrollbarRect(Scrollbar*, const LayoutRect&);
     virtual bool isActive() const;
     virtual void getTickmarks(Vector<LayoutRect>&) const;
-    virtual void scrollTo(const LayoutSize&);
+    virtual void scrollTo(const IntSize&);
     virtual void didStartRubberBand(const IntSize&) const;
     virtual void didCompleteRubberBand(const IntSize&) const;
     virtual void didStartAnimatedScroll() const;
@@ -457,7 +457,7 @@ private:
     double m_lastPaintTime;
     
     bool m_isTrackingRepaints; // Used for testing.
-    Vector<IntRect> m_trackedRepaintRects;
+    Vector<LayoutRect> m_trackedRepaintRects;
 
     bool m_shouldUpdateWhileOffscreen;
 
