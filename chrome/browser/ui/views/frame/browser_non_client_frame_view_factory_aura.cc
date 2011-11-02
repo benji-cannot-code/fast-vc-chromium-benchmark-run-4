a7631678cb30d492512433549caf40cbeb876402
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 03:23:40 +0000

    Aura: Use more traditional window frame, add flag for new look
    Added --aura-windows to trigger new-style transparent window frames.
    Fixed window frame art for top border.
    
    BUG=102499
    TEST=none
    
    Review URL: http://codereview.chromium.org/8423021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108239 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_aura.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_aura.cc
@@ -5,9 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view_aura.h"
 
+#include "base/command_line.h"
 #include "chrome/browser/ui/panels/panel_browser_frame_view.h"
 #include "chrome/browser/ui/panels/panel_browser_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/opaque_browser_frame_view.h"
+#include "chrome/browser/ui/views/frame/popup_non_client_frame_view.h"
+#include "ui/aura/aura_switches.h"
 
 namespace browser {
 
@@ -17,7 +21,9 @@ BrowserNonClientFrameView* CreateBrowserNonClientFrameView(
     return new PanelBrowserFrameView(
         frame, static_cast<PanelBrowserView*>(browser_view));
   }
-  return new BrowserNonClientFrameViewAura(frame, browser_view);
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kAuraWindows))
+    return new BrowserNonClientFrameViewAura(frame, browser_view);
+  return new OpaqueBrowserFrameView(frame, browser_view);
 }
 
 }  // namespace browser
