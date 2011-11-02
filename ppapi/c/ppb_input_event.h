9e595f8f0dbcf0d1c7a77e951b848888fe71406e
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 18:24:32 +0000

    Update comments of PPB_MouseInputEvent.GetMovement.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8438025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108321 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_input_event.h
+++ ppapi/c/ppb_input_event.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_input_event.idl modified Wed Sep 21 12:32:06 2011. */
+/* From ppb_input_event.idl modified Wed Nov  2 09:59:40 2011. */
 
 #ifndef PPAPI_C_PPB_INPUT_EVENT_H_
 #define PPAPI_C_PPB_INPUT_EVENT_H_
@@ -501,9 +501,9 @@ struct PPB_MouseInputEvent {
    * @return The change in position of the mouse, relative to the previous
    * position.
    *
-   * TODO(yzshen): This feature hasn't been supported yet. The returned value is
-   * always (0, 0) for system-generated mouse events (which are passed through
-   * the <code>HandleInputEvent</code>).
+   * TODO(yzshen): This feature hasn't been fully supported yet. For now,
+   * movement information is provided only if the mouse is locked. If the mouse
+   * is not locked, the returned value is (0, 0).
    */
   struct PP_Point (*GetMovement)(PP_Resource mouse_event);
 };
