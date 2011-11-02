8c68c6aa234fdfcbf4149dcb256c744f9a93d5a2
glotov@google.com <glotov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 14:41:33 +0000

    Revert 108276 - Revert 108206 - Share TabFirstRenderWatcher with HtmlDialogView.
    - Move TabFirstRenderWatcher out of chromeos and combine the logic with HTMLDialgoView.
    - Update HtmlDialogBrowserTest.TestStateTransition and move state transition test into TabFirstRenderWatcherTest.TestStateTransition;
    - HtmlDialogBrowserTest.TestStateTransition -> WebContentRendered as it only tests OnTabMainFrameFirstRender is callled now;
    - Conslidate two TestHtmlDialogUIDelegate into one;
    
    Will share it with the auro app list window to avoid initial jankiness.
    
    BUG=98308,86059
    TEST=Hold until all app list changes are in.
    
    
    Review URL: http://codereview.chromium.org/8417005
    
    TBR=xiyuan@chromium.org
    Review URL: http://codereview.chromium.org/8351070
    
    TBR=glotov@google.com
    Review URL: http://codereview.chromium.org/8437052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108288 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_first_render_watcher.cc
+++ chrome/browser/tab_first_render_watcher.cc
@@ -3,15 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/tab_first_render_watcher.h"
+#include "chrome/browser/tab_first_render_watcher.h"
 
 #include "content/browser/renderer_host/render_widget_host.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
-
-namespace chromeos {
 
 TabFirstRenderWatcher::TabFirstRenderWatcher(TabContents* tab,
                                              Delegate* delegate)
@@ -55,5 +53,3 @@ void TabFirstRenderWatcher::Observe(int type,
       NOTREACHED() << "unknown type" << type;
   }
 }
-
-}  // namespace chromeos
