5a2e59542892563961c1ad9e3458eaeab409d038
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-02 02:52:05 +0000

    Cut down on malloc/free a bit in the parser arena https://bugs.webkit.org/show_bug.cgi?id=71343
    Reviewed by Oliver Hunt.
    
    * parser/ParserArena.cpp:
    (JSC::ParserArena::deallocateObjects): Call the destructors of
    the deletable objects before freeing the pools. Don't call
    fastFree on the deletable objects any more.
    
    * parser/ParserArena.h:
    (JSC::ParserArena::allocateDeletable): Use allocateFreeable
    instead of fastMalloc here.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/parser/ParserArena.h
+++ third_party/WebKit/Source/JavaScriptCore/parser/ParserArena.h
@@ -124,7 +124,7 @@ namespace JSC {
 
         void* allocateDeletable(size_t size)
         {
-            ParserArenaDeletable* deletable = static_cast<ParserArenaDeletable*>(fastMalloc(size));
+            ParserArenaDeletable* deletable = static_cast<ParserArenaDeletable*>(allocateFreeable(size));
             m_deletableObjects.append(deletable);
             return deletable;
         }
