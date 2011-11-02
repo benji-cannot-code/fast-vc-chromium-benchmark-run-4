ece3a93e6410366b53a4f5989f5221aaaa13eef1
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 11:53:10 +0000

    Use scoped_refptr to manage TaskManagerResourceProvider lifetime
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/8439038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager.cc
+++ chrome/browser/task_manager/task_manager.cc
@@ -103,10 +103,6 @@ TaskManagerModel::TaskManagerModel(TaskManager* task_manager)
 }
 
 TaskManagerModel::~TaskManagerModel() {
-  for (ResourceProviderList::iterator iter = providers_.begin();
-       iter != providers_.end(); ++iter) {
-    (*iter)->Release();
-  }
 }
 
 int TaskManagerModel::ResourceCount() const {
@@ -650,8 +646,6 @@ void TaskManagerModel::StopUpdating() {
 void TaskManagerModel::AddResourceProvider(
     TaskManager::ResourceProvider* provider) {
   DCHECK(provider);
-  // AddRef matched with Release in destructor.
-  provider->AddRef();
   providers_.push_back(provider);
 }
 
