13f7675aee7e1845bcb36f0f57c6bb9a2f6f8771
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 03:53:47 +0000

    Sync Promo: Show sign in confirmation bubble in NTP
    To make it clear what's happening we want to show a "signed into sync" confirmation bubble in NTP. We only show this bubble for users who signed in using the sync promo page.
    
    Screenshots of the bubble:
    http://www.dropmocks.com/mbiqd
    
    BUG=101057
    TEST=
    
    Review URL: http://codereview.chromium.org/8396024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_page_handler.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
+#include "chrome/browser/ui/webui/ntp/ntp_login_handler.h"
 #include "chrome/browser/ui/webui/ntp/shown_sections_handler.h"
 #include "chrome/browser/ui/webui/sync_promo_ui.h"
 #include "chrome/browser/ui/webui/sync_setup_handler.h"
@@ -378,6 +379,7 @@ void NTPResourceCache::CreateNewTabHTML() {
 #endif
 
   NewTabPageHandler::GetLocalizedValues(profile_, &localized_strings);
+  NTPLoginHandler::GetLocalizedValues(profile_, &localized_strings);
 
   SyncSetupHandler::GetStaticLocalizedValues(&localized_strings);
 
