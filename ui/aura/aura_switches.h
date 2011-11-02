f0db912713a5b147db4b22bc6113b9eabc7defe5
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 15:44:15 +0000

    Fix shared library build for aura.
    TBR=ben@chromium.org,derat@chromium.org
    R=ben@chromium.org,derat@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8438039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108299 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/aura_switches.h
+++ ui/aura/aura_switches.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_AURA_AURA_SWITCHES_H_
 #pragma once
 
+#include "ui/aura/aura_export.h"
+
 namespace switches {
 
-extern const char kAuraHostWindowSize[];
-extern const char kAuraWindows[];
+AURA_EXPORT extern const char kAuraHostWindowSize[];
+AURA_EXPORT extern const char kAuraWindows[];
 
 }  // namespace switches
 
