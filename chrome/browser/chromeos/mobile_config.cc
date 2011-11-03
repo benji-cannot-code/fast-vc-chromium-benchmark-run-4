eed4aea648e8c2665497d3ab4d8e7f24f9318e54
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 19:38:28 +0000

    Replace NewRunnableMethod with Bind in MobileConfig
    BUG=chromium-os:22024
    TEST=compiles, tests pass.
    Review URL: http://codereview.chromium.org/8439020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/mobile_config.cc
+++ chrome/browser/chromeos/mobile_config.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/bind.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/json/json_reader.h"
@@ -287,10 +288,10 @@ MobileConfig::~MobileConfig() {
 
 void MobileConfig::LoadConfig() {
   BrowserThread::PostTask(BrowserThread::FILE, FROM_HERE,
-      NewRunnableMethod(this,
-          &MobileConfig::ReadConfigInBackground,
-          FilePath(kGlobalCarrierConfigPath),
-          FilePath(kLocalCarrierConfigPath)));
+      base::Bind(&MobileConfig::ReadConfigInBackground,
+                 base::Unretained(this),  // this class is a singleton.
+                 FilePath(kGlobalCarrierConfigPath),
+                 FilePath(kLocalCarrierConfigPath)));
 }
 
 void MobileConfig::ProcessConfig(const std::string& global_config,
@@ -333,11 +334,10 @@ void MobileConfig::ReadConfigInBackground(const FilePath& global_config_file,
             << local_config_file.value();
   }
   BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      NewRunnableMethod(
-          this,
-          &MobileConfig::ProcessConfig,
-          global_config,
-          local_config));
+                          base::Bind(&MobileConfig::ProcessConfig,
+                                     base::Unretained(this),  // singleton.
+                                     global_config,
+                                     local_config));
 }
 
 }  // namespace chromeos
