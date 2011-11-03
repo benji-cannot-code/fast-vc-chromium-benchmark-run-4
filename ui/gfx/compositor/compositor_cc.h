af355c30712f57dd89dbee48a290b48adacbddc1
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 15:46:33 +0000

    Fix build issues with CompositorCC & component build.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8438053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108470 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor_cc.h
+++ ui/gfx/compositor/compositor_cc.h
@@ -26,7 +26,8 @@ class GLShareGroup;
 
 namespace ui {
 
-class COMPOSITOR_EXPORT SharedResourcesCC : public SharedResources {
+class COMPOSITOR_EXPORT SharedResourcesCC
+    : NON_EXPORTED_BASE(public SharedResources) {
  public:
   static SharedResourcesCC* GetInstance();
 
@@ -74,9 +75,10 @@ class COMPOSITOR_EXPORT TextureCC : public Texture {
   DISALLOW_COPY_AND_ASSIGN(TextureCC);
 };
 
-class COMPOSITOR_EXPORT CompositorCC : public Compositor,
-                                       public WebKit::WebLayerTreeViewClient,
-                                       public WebKit::WebLayerClient {
+class COMPOSITOR_EXPORT CompositorCC
+    : public Compositor,
+      NON_EXPORTED_BASE(public WebKit::WebLayerTreeViewClient),
+      NON_EXPORTED_BASE(public WebKit::WebLayerClient) {
  public:
   CompositorCC(CompositorDelegate* delegate,
                gfx::AcceleratedWidget widget,
