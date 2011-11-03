d900d8be5f5755512ac2359dc667902965b42e4b
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 18:04:00 +0000

    GLES2CmdDecoder uses LOG instead of DLOG to output GL commands. Review URL: http://codereview.chromium.org/8438050
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108500 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2766,7 +2766,7 @@ error::Error GLES2DecoderImpl::DoCommand(
   error::Error result = error::kNoError;
   if (debug()) {
     // TODO(gman): Change output to something useful for NaCl.
-    DLOG(INFO) << "[" << this << "]" << "cmd: " << GetCommandName(command);
+    LOG(INFO) << "[" << this << "]" << "cmd: " << GetCommandName(command);
   }
   unsigned int command_index = command - kStartPoint - 1;
   if (command_index < arraysize(g_command_info)) {
