28e3c62592a75fee32f783d1d7e5855c26fb1799
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 14:42:28 +0000

    Unreviewed build fix, sigh.
    * css/CSSFontFaceRule.h:
    * css/CSSMutableStyleDeclaration.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSFontFaceRule.h
+++ third_party/WebKit/Source/WebCore/css/CSSFontFaceRule.h
@@ -23,13 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CSSFontFaceRule_h
 #define CSSFontFaceRule_h
 
+#include "CSSMutableStyleDeclaration.h"
 #include "CSSRule.h"
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
-
-class CSSMutableStyleDeclaration;
 
 class CSSFontFaceRule : public CSSRule {
 public:
--- third_party/WebKit/Source/WebCore/css/CSSStyleRule.h
+++ third_party/WebKit/Source/WebCore/css/CSSStyleRule.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CSSStyleRule_h
 #define CSSStyleRule_h
 
+#include "CSSMutableStyleDeclaration.h"
 #include "CSSRule.h"
 #include "CSSSelectorList.h"
 #include <wtf/PassRefPtr.h>
@@ -30,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSSMutableStyleDeclaration;
 class CSSSelector;
 
 class CSSStyleRule : public CSSRule {
