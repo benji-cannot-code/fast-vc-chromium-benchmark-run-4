d36728cce32fb54de85f0036c3c0dce4971a8ae8
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 00:53:02 +0000

    An ugly but working host-list implementation for the Chromoting app.
    Hosts will have a Connect button if online, and the text Offline otherwise.
    
    The host list will display only once one or more hosts are registered, for now.
    
    Connection setup code still needs to be rationalized between the two modes.
    
    The necessary host-side changes will arrive in a separate CL.
    
    
    Review URL: http://codereview.chromium.org/8432021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108375 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/ui_mode.js
+++ remoting/webapp/me2mom/ui_mode.js
@@ -68,6 +68,9 @@ remoting.setMode = function(mode) {
   } else {
     document.addEventListener('keydown', remoting.DebugLog.onKeydown, false);
   }
+  if (mode == remoting.AppMode.HOME) {
+    remoting.refreshHostList();
+  }
 };
 
 /**
