59b9c10f1831a08267c571de9ae586cac9d3ffd9
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 04:39:32 +0000

    Implement allow-popups for iframe@sandbox https://bugs.webkit.org/show_bug.cgi?id=66505
    Reviewed by Eric Seidel.
    
    Source/WebCore: 
    
    There's been some discussion in the HTML working group about adding an
    allow-popups directive to the iframe sandbox.  Microsoft has added it
    to IE10 platform preview and is fairly adamant about this feature
    because it's needed by one or their products that's planning to use
    iframe sandbox.  Hixie says he'll add it to the spec once we implement
    it, so here's our implementation.  (See discussion in the W3C linked in
    the bug for more details.)
    
    Tests: http/tests/security/popup-allowed-by-sandbox-is-sandboxed-control.html
           http/tests/security/popup-allowed-by-sandbox-is-sandboxed.html
           http/tests/security/popup-allowed-by-sandbox-when-allowed.html
    
    * html/HTMLIFrameElement.cpp:
    (WebCore::HTMLIFrameElement::parseMappedAttribute):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::setOpener):
    (WebCore::createWindow):
    * loader/FrameLoader.h:
    (WebCore::FrameLoader::forceSandboxFlags):
    * loader/FrameLoaderTypes.h:
    * loader/PolicyChecker.cpp:
    (WebCore::PolicyChecker::checkNewWindowPolicy):
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::parseSandboxPolicy):
    * page/SecurityOrigin.h:
    (WebCore::SecurityOrigin::sandboxFlags):
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::dataChanged):
    
    LayoutTests: 
    
    Test that the allow-popups directive works as expected.  Note:
    no-popup-from-sandbox.html verifies that we still block popups without
    the directive.
    
    * http/tests/security/popup-allowed-by-sandbox-is-sandboxed-control-expected.txt: Added.
    * http/tests/security/popup-allowed-by-sandbox-is-sandboxed-control.html: Added.
    * http/tests/security/popup-allowed-by-sandbox-is-sandboxed-expected.txt: Added.
    * http/tests/security/popup-allowed-by-sandbox-is-sandboxed.html: Added.
    * http/tests/security/popup-allowed-by-sandbox-when-allowed-expected.txt: Added.
    * http/tests/security/popup-allowed-by-sandbox-when-allowed.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLIFrameElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLIFrameElement.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "NodeRenderingContext.h"
 #include "RenderIFrame.h"
+#include "SecurityOrigin.h"
 
 namespace WebCore {
 
@@ -69,42 +70,6 @@ bool HTMLIFrameElement::mapToEntry(const QualifiedName& attrName, MappedAttribut
     return HTMLFrameElementBase::mapToEntry(attrName, result);
 }
 
-static SandboxFlags parseSandboxAttribute(Attribute* attribute)
-{
-    if (attribute->isNull())
-        return SandboxNone;
-
-    // Parse the unordered set of unique space-separated tokens.
-    SandboxFlags flags = SandboxAll;
-    const UChar* characters = attribute->value().characters();
-    unsigned length = attribute->value().length();
-    unsigned start = 0;
-    while (true) {
-        while (start < length && isASCIISpace(characters[start]))
-            ++start;
-        if (start >= length)
-            break;
-        unsigned end = start + 1;
-        while (end < length && !isASCIISpace(characters[end]))
-            ++end;
-
-        // Turn off the corresponding sandbox flag if it's set as "allowed".
-        String sandboxToken = String(characters + start, end - start);
-        if (equalIgnoringCase(sandboxToken, "allow-same-origin"))
-            flags &= ~SandboxOrigin;
-        else if (equalIgnoringCase(sandboxToken, "allow-forms"))
-            flags &= ~SandboxForms;
-        else if (equalIgnoringCase(sandboxToken, "allow-scripts"))
-            flags &= ~SandboxScripts;
-        else if (equalIgnoringCase(sandboxToken, "allow-top-navigation"))
-            flags &= ~SandboxTopNavigation;
-
-        start = end + 1;
-    }
-
-    return flags;
-}
-
 void HTMLIFrameElement::parseMappedAttribute(Attribute* attr)
 {
     if (attr->name() == widthAttr)
@@ -128,7 +93,7 @@ void HTMLIFrameElement::parseMappedAttribute(Attribute* attr)
             // Add a rule that nulls out our border width.
             addCSSLength(attr, CSSPropertyBorderWidth, "0");
     } else if (attr->name() == sandboxAttr)
-        setSandboxFlags(parseSandboxAttribute(attr));
+        setSandboxFlags(attr->isNull() ? SandboxNone : SecurityOrigin::parseSandboxPolicy(attr->value()));
     else
         HTMLFrameElementBase::parseMappedAttribute(attr);
 }
