dac05f92858b796fe81730d7e6eec5027122f26f
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 16:39:15 +0000

    HQP Refactoring (in Preparation for SQLite Cache)
    1. Move ownership of the InMemoryURLIndex from the InMemoryHistoryBackend to the HistoryBackend where it truly belongs.
    2. Handle (by notification) URL visits, updates and deletes. Refactor use of NOTIFICATION_HISTORY_URLS_DELETED to provide the deleted URLRow so that row ID is available.
    3. Correctly handle the adding and removing of page title words when a URL change is detected.
    4. Other small cleanups.
    
    BUG=96731, 92718
    TEST=Unit tests updated.
    TBR=atwilson (for profile_sync_service_typed_url_unittest.cc)
    
    Previously reviewed as: http://codereview.chromium.org/8384024/
    Review URL: http://codereview.chromium.org/8437042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108478 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_quick_provider_unittest.cc
+++ chrome/browser/autocomplete/history_quick_provider_unittest.cc
@@ -147,7 +147,7 @@ void HistoryQuickProviderTest::FillData() {
   ASSERT_TRUE(db != NULL);
 
   history::InMemoryURLIndex* index =
-      new history::InMemoryURLIndex(FilePath());
+      new history::InMemoryURLIndex(profile_.get(), FilePath());
   PrefService* prefs = profile_->GetPrefs();
   std::string languages(prefs->GetString(prefs::kAcceptLanguages));
   index->Init(db, languages);
@@ -162,7 +162,8 @@ void HistoryQuickProviderTest::FillData() {
     url_info.set_typed_count(cur.typed_count);
     url_info.set_last_visit(visit_time);
     url_info.set_hidden(false);
-    index->UpdateURL(i, url_info);
+    url_info.set_id(i);
+    index->UpdateURL(url_info);
   }
 
   provider_->set_index(index);
