7c559634423f7eb92265588b57fc8aabe0094811
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 23:17:27 +0000

    NetscapePlugin::wantsWindowRelativeCoordinates should return false for all ports https://bugs.webkit.org/show_bug.cgi?id=71501
    Reviewed by Darin Adler.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.cpp:
    (WebKit::NetscapePlugin::wantsWindowRelativeCoordinates):
    Return false.
    
    * WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp:
    (WebKit::NetscapePlugin::platformGeometryDidChange):
    Use m_pluginSize instead of m_frameRectInWindowCoordinates.size().
    
    (WebKit::NetscapePlugin::platformPaint):
    No need to translate back and forth between window and plug-in relative coordinates anymore.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.cpp
@@ -936,11 +936,7 @@ bool NetscapePlugin::handleScroll(ScrollDirection, ScrollGranularity)
 
 bool NetscapePlugin::wantsWindowRelativeCoordinates()
 {
-#if PLUGIN_ARCHITECTURE(MAC)
     return false;
-#else
-    return true;
-#endif
 }
 
 Scrollbar* NetscapePlugin::horizontalScrollbar()
