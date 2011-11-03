87cdd988b606ef4f41b63ddd827e990266826092
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 19:15:42 +0000

    [EFL] DRT: Allow choosing which backing store to use. https://bugs.webkit.org/show_bug.cgi?id=70532
    Patch by Raphael Kubo da Costa <kubo@profusion.mobi> on 2011-11-03
    Reviewed by Antonio Gomes.
    
    This change makes it possible to change the backing store used by DRT
    from Tiled Backing Store (TBS) to Single Backing Store (SBS) by
    setting the environment variable DRT_USE_SINGLE_BACKING_STORE to 1.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::createView):
    * DumpRenderTree/efl/DumpRenderTreeView.cpp:
    (shouldUseSingleBackingStore):
    (chooseAndInitializeAppropriateSmartClass):
    (drtViewAdd):
    * DumpRenderTree/efl/DumpRenderTreeView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeView.h
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeView.h
@@ -28,6 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Evas.h>
 
-Evas_Object* drtViewTiledAdd(Evas*);
+Evas_Object* drtViewAdd(Evas*);
 
 #endif // DumpRenderTreeView_h
