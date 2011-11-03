9a1c79fa1c211d8a68fbd4f0705c32a2fd2088b8
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 13:40:03 +0000

    [Qt] Add pageScaleFactor to the serialized history item https://bugs.webkit.org/show_bug.cgi?id=71463
    Reviewed by Tor Arne Vestbø.
    
    r75758 added the pageScaleFactor member, which we need to
    serialize.
    
    * history/qt/HistoryItemQt.cpp:
    (WebCore::HistoryItem::restoreState):
    (WebCore::HistoryItem::saveState):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99182 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/history/qt/HistoryItemQt.cpp
+++ third_party/WebKit/Source/WebCore/history/qt/HistoryItemQt.cpp
@@ -47,6 +47,7 @@ bool WebCore::HistoryItem::restoreState(QDataStream& in, int version)
     int visitCount;
     WTF::Vector<WTF::String> documentState;
     WebCore::IntPoint scrollPoint;
+    qreal pageScaleFactor;
     WTF::Vector<int> weeklyVisitCounts;
     WTF::Vector<int> dailyVisitCounts;
     // bool loadFormdata;
@@ -55,7 +56,7 @@ bool WebCore::HistoryItem::restoreState(QDataStream& in, int version)
 
     in >> url >> title >> altTitle >> lastVisitedTime >> orginalUrl >> referrer >> target >> parent;
     in >> lastVisitWasHTTPNonGet >> lastVisitWasFailure >> isTargetItem >> visitCount >> documentState;
-    in >> scrollPoint >> dailyVisitCounts >> weeklyVisitCounts;
+    in >> scrollPoint >> pageScaleFactor >> dailyVisitCounts >> weeklyVisitCounts;
     /*in >> loadFormdata;
     if (loadFormdata) {
         in >> formContentType >> formData;
@@ -66,6 +67,7 @@ bool WebCore::HistoryItem::restoreState(QDataStream& in, int version)
     // use setters
     adoptVisitCounts(dailyVisitCounts, weeklyVisitCounts);
     setScrollPoint(scrollPoint);
+    setPageScaleFactor(pageScaleFactor);
     setDocumentState(documentState);
     setVisitCount(visitCount);
     setIsTargetItem(isTargetItem);
@@ -101,7 +103,7 @@ QDataStream& WebCore::HistoryItem::saveState(QDataStream& out, int version) cons
     out << originalURLString() << referrer() << target() << parent();
     out << lastVisitWasHTTPNonGet() << lastVisitWasFailure() << isTargetItem();
     out << visitCount() << documentState() << scrollPoint();
-    out << dailyVisitCounts() << weeklyVisitCounts();
+    out << qreal(pageScaleFactor()) << dailyVisitCounts() << weeklyVisitCounts();
     /*if (m_formData) {
         out << true;
         out << formContentType();
