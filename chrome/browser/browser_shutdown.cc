008d16e1812b5e216ac656ebda409208ddfcb529
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 19:38:06 +0000

    Replace NewRunnableFunction with Bind in BrowserShutdown.
    BUG=chromium-os:22024.
    TEST=compiles, tests pass.
    Review URL: http://codereview.chromium.org/8437023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_shutdown.cc
+++ chrome/browser/browser_shutdown.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
@@ -301,8 +302,7 @@ void ReadLastShutdownInfo() {
   // Read and delete the file on the file thread.
   BrowserThread::PostTask(
       BrowserThread::FILE, FROM_HERE,
-      NewRunnableFunction(
-          &ReadLastShutdownFile, type, num_procs, num_procs_slow));
+      base::Bind(&ReadLastShutdownFile, type, num_procs, num_procs_slow));
 }
 
 void SetTryingToQuit(bool quitting) {
