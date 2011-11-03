39c24e04cbe98a09ad718d58e506b5361983e1fa
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 18:05:15 +0000

    Add Clock class and platform-specific implementations. https://bugs.webkit.org/show_bug.cgi?id=71341
    Reviewed by Sam Weinig.
    
    Source/JavaScriptCore:
    
    Add WTF_USE_COREAUDIO macro for use by PlatformClockCA.
    
    * wtf/Platform.h:
    
    Source/WebCore:
    
    No new tests; these classes will be used and tested by the MediaController feature.
    
    Adds a abstract Clock class and two concrete, platform-specific subclasses.
    
    Clock provides a basic interface for starting, stopping, and querying a generic timeline.
    PlatformClockCA implements this interface using a CoreAudio CAClock object, which uses
    the default audio device's hardware clock as a timing source. PlatformClockPOSIX
    implements this interface using gettimeofday() as its timing source.
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/Clock.cpp: Added.
    (Clock::create): Creates a platform-specific clock.
    * platform/Clock.h: Added.
    (WebCore::Clock::~Clock):
    (WebCore::Clock::Clock):
    * platform/mac/PlatformClockCA.cpp: Added.
    (PlatformClockCA::PlatformClockCA):
    (PlatformClockCA::~PlatformClockCA):
    (PlatformClockCA::setCurrentTime):
    (PlatformClockCA::currentTime):
    (PlatformClockCA::setPlayRate):
    (PlatformClockCA::PlatformClockCA::playRate):
    (PlatformClockCA::start):
    (PlatformClockCA::stop):
    * platform/mac/PlatformClockCA.h: Added.
    (WebCore::PlatformClockCA::isRunning):
    * platform/posix/PlatformClockPOSIX.cpp: Added.
    (timevalToFloat):
    (timevalDelta):
    (PlatformClockPOSIX::PlatformClockPOSIX):
    (PlatformClockPOSIX::setCurrentTime):
    (PlatformClockPOSIX::currentTime):
    (PlatformClockPOSIX::setPlayRate):
    (PlatformClockPOSIX::PlatformClockPOSIX::playRate):
    (PlatformClockPOSIX::start):
    (PlatformClockPOSIX::stop):
    (PlatformClockPOSIX::now):
    * platform/posix/PlatformClockPOSIX.h: Added.
    (WebCore::PlatformClockPOSIX::isRunning):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -1105,4 +1105,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_INVERTED_WHEEL_EVENTS 1
 #endif
 
+#if PLATFORM(MAC) || (PLATFORM(WIN) && !OS(WINCE) && !PLATFORM(WIN_CAIRO))
+#define WTF_USE_COREAUDIO 1
+#endif
+
 #endif /* WTF_Platform_h */
