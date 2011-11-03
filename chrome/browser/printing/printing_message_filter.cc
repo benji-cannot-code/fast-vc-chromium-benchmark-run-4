2d232997f386dd10bae6809b11bc7ee6cef7eca2
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 00:56:01 +0000

    Printing: Remove an unused variable, update some obsolete comments, and remove an unneeded prefs check.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/8386028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/printing_message_filter.cc
+++ chrome/browser/printing/printing_message_filter.cc
@@ -244,7 +244,6 @@ void PrintingMessageFilter::OnScriptedPrint(
       this,
       &PrintingMessageFilter::OnScriptedPrintReply,
       printer_query,
-      params.routing_id,
       reply_msg);
 
   printer_query->GetSettings(printing::PrinterQuery::ASK_USER,
@@ -257,7 +256,6 @@ void PrintingMessageFilter::OnScriptedPrint(
 
 void PrintingMessageFilter::OnScriptedPrintReply(
     scoped_refptr<printing::PrinterQuery> printer_query,
-    int routing_id,
     IPC::Message* reply_msg) {
   PrintMsg_PrintPages_Params params;
   if (printer_query->last_status() != printing::PrintingContext::OK ||
