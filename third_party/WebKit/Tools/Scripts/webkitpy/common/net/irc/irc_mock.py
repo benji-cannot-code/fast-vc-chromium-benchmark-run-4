40692fea928f901f872cffeef3a9a0b0f500359e
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 06:16:11 +0000

    Move Mocks into _mock files near their implementations https://bugs.webkit.org/show_bug.cgi?id=71425
    Reviewed by Adam Barth.
    
    This entire change is splitting mocktool.py into separate _mock.py files
    and placing them next to their real implementations.
    
    I also deleted MockPortFactory (since it was wrong) and was just hiding
    the fact that the "skipped-ports" command was broken (and has been for a long time).
    So I made MockHost use a real PortFactory (passed a MockHost) and changed
    the skipped-ports implementation to use modern PortFactory methods.
    
    * Scripts/webkitpy/common/checkout/baselineoptimizer_unittest.py:
    * Scripts/webkitpy/common/checkout/checkout_mock.py: Added.
    * Scripts/webkitpy/common/checkout/checkout_unittest.py:
    * Scripts/webkitpy/common/checkout/deps_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/__init__.py.
    * Scripts/webkitpy/common/checkout/scm/scm_mock.py: Added.
    * Scripts/webkitpy/common/checkout/scm/scm_unittest.py:
    * Scripts/webkitpy/common/config/ports_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/gtk_unittest.py.
    * Scripts/webkitpy/common/host.py:
    * Scripts/webkitpy/common/host_mock.py: Added.
    * Scripts/webkitpy/common/net/bugzilla/bugzilla_mock.py: Added.
    * Scripts/webkitpy/common/net/bugzilla/bugzilla_unittest.py:
    * Scripts/webkitpy/common/net/buildbot/buildbot_mock.py: Added.
    * Scripts/webkitpy/common/net/credentials_unittest.py:
    * Scripts/webkitpy/common/net/failuremap_unittest.py:
    * Scripts/webkitpy/common/net/irc/irc_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/__init__.py.
    * Scripts/webkitpy/common/net/statusserver_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/gtk_unittest.py.
    * Scripts/webkitpy/common/net/statusserver_unittest.py:
    * Scripts/webkitpy/common/net/web_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/gtk_unittest.py.
    * Scripts/webkitpy/common/system/executive.py:
    * Scripts/webkitpy/common/system/executive_mock.py:
    * Scripts/webkitpy/common/system/platforminfo_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/__init__.py.
    * Scripts/webkitpy/common/system/user_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/gtk_unittest.py.
    * Scripts/webkitpy/common/system/workspace_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/__init__.py.
    * Scripts/webkitpy/common/system/workspace_unittest.py:
    * Scripts/webkitpy/common/watchlist/watchlist_mock.py: Copied from Tools/Scripts/webkitpy/layout_tests/port/__init__.py.
    * Scripts/webkitpy/layout_tests/controllers/manager_unittest.py:
    * Scripts/webkitpy/layout_tests/controllers/test_expectations_editor_unittest.py:
    * Scripts/webkitpy/layout_tests/port/__init__.py:
    * Scripts/webkitpy/layout_tests/port/base_unittest.py:
    * Scripts/webkitpy/layout_tests/port/efl_unittest.py:
    * Scripts/webkitpy/layout_tests/port/factory.py:
    * Scripts/webkitpy/layout_tests/port/factory_unittest.py:
    * Scripts/webkitpy/layout_tests/port/gtk_unittest.py:
    * Scripts/webkitpy/layout_tests/port/http_lock_unittest.py:
    * Scripts/webkitpy/layout_tests/port/leakdetector_unittest.py:
    * Scripts/webkitpy/layout_tests/port/mac_unittest.py:
    * Scripts/webkitpy/layout_tests/port/port_testcase.py:
    * Scripts/webkitpy/layout_tests/port/qt_unittest.py:
    * Scripts/webkitpy/layout_tests/port/webkit_unittest.py:
    * Scripts/webkitpy/layout_tests/port/win_unittest.py:
    * Scripts/webkitpy/test/main.py:
    * Scripts/webkitpy/tool/bot/botinfo_unittest.py:
    * Scripts/webkitpy/tool/bot/flakytestreporter_unittest.py:
    * Scripts/webkitpy/tool/bot/irc_command_unittest.py:
    * Scripts/webkitpy/tool/commands/queries_unittest.py:
    * Scripts/webkitpy/tool/commands/rebaseline_unittest.py:
    * Scripts/webkitpy/tool/mocktool.py:
    * Scripts/webkitpy/tool/servers/gardeningserver_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/irc/irc_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/irc/irc_mock.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2010 Google Inc. All rights reserved.
+# Copyright (c) 2011 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -27,18 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import unittest
-
-from webkitpy.common.net.statusserver import StatusServer
-from webkitpy.common.system.outputcapture import OutputCaptureTestCaseBase
-from webkitpy.common.net.web_mock import MockBrowser
+from webkitpy.common.system.deprecated_logging import log
 
 
-class StatusServerTest(OutputCaptureTestCaseBase):
-    def test_url_for_issue(self):
-        mock_browser = MockBrowser()
-        status_server = StatusServer(browser=mock_browser, bot_id='123')
-        status_server.update_status('queue name', 'the status')
-        self.assertEqual('queue name', mock_browser.params['queue_name'])
-        self.assertEqual('the status', mock_browser.params['status'])
-        self.assertEqual('123', mock_browser.params['bot_id'])
+class MockIRC(object):
+    def post(self, message):
+        log("MOCK: irc.post: %s" % message)
+
+    def disconnect(self):
+        log("MOCK: irc.disconnect")
--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/botinfo_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/botinfo_unittest.py
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.tool.bot.botinfo import BotInfo
-from webkitpy.tool.mocktool import MockTool, MockStatusServer
+from webkitpy.tool.mocktool import MockTool
+from webkitpy.common.net.statusserver_mock import MockStatusServer
 
 
 class BotInfoTest(unittest.TestCase):
