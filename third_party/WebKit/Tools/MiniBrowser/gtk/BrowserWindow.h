5901b1ed53928ff0052fe078c68672533e0a5964
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 16:10:42 +0000

    [GTK] Remove WebKit2 C API from MiniBrowser https://bugs.webkit.org/show_bug.cgi?id=71459
    Reviewed by Martin Robinson.
    
    Use the GTK+ API instead to add minimum functionality. The other
    features will be ported to GTK+ API in following patches.
    
    * MiniBrowser/gtk/BrowserWindow.c:
    (activateUriEntryCallback): Use webkit_web_view_load_uri().
    (goBackCallback): Use webkit_web_view_go_back().
    (goForwardCallback): Use webkit_web_view_go_forward().
    (webViewURIChanged): Update location entry with current uri using
    webkit_web_view_get_uri().
    (browserWindowFinalize):
    (browserWindowGetProperty):
    (browserWindowSetProperty):
    (browser_window_init):
    (browserWindowConstructed): Connect to notify::uri signal of
    WebView to be notified when the URI changes.
    (browser_window_class_init):
    (browser_window_new): Use WebKitWebView.
    (browser_window_get_view): Use WebKitWebView
    * MiniBrowser/gtk/BrowserWindow.h:
    * MiniBrowser/gtk/GNUmakefile.am:
    * MiniBrowser/gtk/WebBundle/WebBundleMain.c: Removed.
    * MiniBrowser/gtk/main.c:
    (loadURI): Use webkit_web_view_new().
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/gtk/BrowserWindow.h
+++ third_party/WebKit/Tools/MiniBrowser/gtk/BrowserWindow.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BrowserWindow_h
 #define BrowserWindow_h
 
-#include <WebKit2/WebKit2.h>
+#include <webkit2/webkit2.h>
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
@@ -44,8 +44,8 @@ typedef struct _BrowserWindowClass   BrowserWindowClass;
 
 GType browser_window_get_type(void);
 
-GtkWidget* browser_window_new(WKViewRef);
-WKViewRef browser_window_get_view(BrowserWindow*);
+GtkWidget* browser_window_new(WebKitWebView*);
+WebKitWebView* browser_window_get_view(BrowserWindow*);
 
 G_END_DECLS
 
