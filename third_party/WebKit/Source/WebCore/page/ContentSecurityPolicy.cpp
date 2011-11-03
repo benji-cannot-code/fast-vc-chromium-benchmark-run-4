f2cdd15f9803e440ce0bbc6c4a4d4f8d63e58f8d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-03 06:46:45 +0000

    CSP should handle empty URLs as agreed at TPAC https://bugs.webkit.org/show_bug.cgi?id=71426
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    It was somewhat unclear how CSP should treat plugins that lacked a URL
    because most of the CSP rules are URL-based.  At TPAC, we decided to
    treat "empty" URLs as if there were the URL of the document.  That
    means you can use plugins with no URL if you've included 'self' in
    object-src, but you can also block them by using 'none' as your
    object-src.
    
    Tests: http/tests/security/contentSecurityPolicy/object-src-no-url-allowed.html
           http/tests/security/contentSecurityPolicy/object-src-no-url-blocked.html
           http/tests/security/contentSecurityPolicy/object-src-none-allowed.html
           http/tests/security/contentSecurityPolicy/object-src-none-blocked.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::CSPDirective::CSPDirective):
    (WebCore::CSPDirective::allows):
    (WebCore::ContentSecurityPolicy::createCSPDirective):
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/object-src-no-url-allowed-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/object-src-no-url-allowed.html: Added.
    * http/tests/security/contentSecurityPolicy/object-src-no-url-blocked-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/object-src-no-url-blocked.html: Added.
        - Test the allow and block cases for plugins with no URL.
    * http/tests/security/contentSecurityPolicy/object-src-none-allowed.html: Added.
    * http/tests/security/contentSecurityPolicy/object-src-none-blocked.html: Added.
        - Somehow these tests got deleted from the repository.  This patch just re-adds them.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
@@ -459,16 +459,17 @@ void CSPSourceList::addSourceUnsafeEval()
 
 class CSPDirective {
 public:
-    CSPDirective(const String& name, const String& value, SecurityOrigin* origin)
-        : m_sourceList(origin)
+    CSPDirective(const String& name, const String& value, ScriptExecutionContext* context)
+        : m_sourceList(context->securityOrigin())
         , m_text(name + ' ' + value)
+        , m_selfURL(context->url())
     {
         m_sourceList.parse(value);
     }
 
     bool allows(const KURL& url)
     {
-        return m_sourceList.matches(url);
+        return m_sourceList.matches(url.isEmpty() ? m_selfURL : url);
     }
 
     bool allowInline() const { return m_sourceList.allowInline(); }
@@ -479,6 +480,7 @@ public:
 private:
     CSPSourceList m_sourceList;
     String m_text;
+    KURL m_selfURL;
 };
 
 ContentSecurityPolicy::ContentSecurityPolicy(ScriptExecutionContext* scriptExecutionContext)
@@ -760,7 +762,7 @@ void ContentSecurityPolicy::parseReportURI(const String& value)
 
 PassOwnPtr<CSPDirective> ContentSecurityPolicy::createCSPDirective(const String& name, const String& value)
 {
-    return adoptPtr(new CSPDirective(name, value, m_scriptExecutionContext->securityOrigin()));
+    return adoptPtr(new CSPDirective(name, value, m_scriptExecutionContext));
 }
 
 void ContentSecurityPolicy::addDirective(const String& name, const String& value)
