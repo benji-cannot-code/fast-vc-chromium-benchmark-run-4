c7d279fb746872dd89afc29e4a127c0b2a12ab31
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 17:22:10 +0000

    Publish the source for CFInstall.min.js .
    BUG=None
    TEST=./build.sh and then load file:///path/to/local_testing.html in IE (without CF). Click the "This site requires Chrome Frame" link and observe the dialog that pops up.
    
    Review URL: http://codereview.chromium.org/8467005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108675 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome_frame/cfinstall/src/stub/cf-activex-externs.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Describes the Chrome Frame ActiveX control.
+ **/
+
+// Defined by chrome_tab.idl
+var ChromeTab;
+/**
+ * @constructor
+ * @extends ActiveXObject
+ */
+ChromeTab.ChromeFrame;
+ChromeTab.ChromeFrame.prototype.registerBhoIfNeeded;
