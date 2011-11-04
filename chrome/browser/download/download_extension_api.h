f99bc2cc7b30bc366a705ec790804ac7272385a5
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 15:53:43 +0000

    Move download extension api implementation to download/ dir.
    We're moving all the extension api implementations out of browser/extensions
    into the directory with the code they are automating.
    
    BUG=101244
    R=aa@chromium.org,rdsmith@chromium.org
    
    Review URL: http://codereview.chromium.org/8451019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108660 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extension_api.h
+++ chrome/browser/download/download_extension_api.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_DOWNLOADS_API_H_
-#define CHROME_BROWSER_EXTENSIONS_EXTENSION_DOWNLOADS_API_H_
+#ifndef CHROME_BROWSER_DOWNLOAD_DOWNLOAD_EXTENSION_API_H_
+#define CHROME_BROWSER_DOWNLOAD_DOWNLOAD_EXTENSION_API_H_
 #pragma once
 
 #include <map>
@@ -275,4 +275,4 @@ class ExtensionDownloadsEventRouter : public DownloadManager::Observer {
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionDownloadsEventRouter);
 };
-#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_DOWNLOADS_API_H_
+#endif  // CHROME_BROWSER_DOWNLOAD_DOWNLOAD_EXTENSION_API_H_
