302db3f4ecb27fd07d4bd0d502fdf39565e574fd
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 21:52:43 +0000

    Fix build breakage
    TBR=flackr@chromium.org
    BUG=none
    TEST=non
    
    Review URL: http://codereview.chromium.org/8476009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.cc
+++ chrome/browser/ui/views/constrained_window_views.cc
@@ -645,7 +645,7 @@ void ConstrainedWindowViews::FocusConstrainedWindow() {
   }
 }
 
-gfx::NativeView ConstrainedWindowViews::GetNativeWindow() {
+gfx::NativeWindow ConstrainedWindowViews::GetNativeWindow() {
   return Widget::GetNativeWindow();
 }
 
