45010c22accc955a1a373e987d75c79265c8e438
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 19:24:31 +0000

    Disable chromedriver frame switching tests which are broken after 108661. BUG=none TEST=none TBR=nirnimesh
    Review URL: http://codereview.chromium.org/8478009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108693 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/test/chromedriver_tests.py
+++ chrome/test/webdriver/test/chromedriver_tests.py
@@ -741,6 +741,10 @@ class FileUploadControlTest(ChromeDriverTest):
       self.assertTrue(f['name'] in filenames)
 
 
+def Skip(arg):
+  pass
+
+@Skip
 class FrameSwitchingTest(ChromeDriverTest):
 
   def testGetWindowHandles(self):
