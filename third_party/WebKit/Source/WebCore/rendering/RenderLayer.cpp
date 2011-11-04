080d76110ee7f578aba8b04f96fcad60e6324a64
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-04 18:52:53 +0000

    RenderLayer::styleChanged invalidates the GraphicsLayer needlessly https://bugs.webkit.org/show_bug.cgi?id=71517
    Reviewed by Simon Fraser.
    
    Unused code removal covered by existing tests.
    
    The current code is invalidating the whole RenderLayerBacking as part of
    a style change. However this is redundant with the repainting logic which
    would invalidate only the necessary bits.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::styleChanged):
    Remove the invalidation code as it duplicates and defeats the repaint logic.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99307 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -4173,7 +4173,7 @@ bool RenderLayer::isSelfPaintingLayer() const
         || renderer()->isRenderIFrame();
 }
 
-void RenderLayer::styleChanged(StyleDifference diff, const RenderStyle* oldStyle)
+void RenderLayer::styleChanged(StyleDifference, const RenderStyle* oldStyle)
 {
     bool isNormalFlowOnly = shouldBeNormalFlowOnly();
     if (isNormalFlowOnly != m_isNormalFlowOnly) {
@@ -4236,11 +4236,6 @@ void RenderLayer::styleChanged(StyleDifference diff, const RenderStyle* oldStyle
         if (stackingContext()->hasCompositingDescendant())
             compositor()->setCompositingLayersNeedRebuild();
     }
-    
-    if (m_backing && diff >= StyleDifferenceRepaint)
-        m_backing->setContentsNeedDisplay();
-#else
-    UNUSED_PARAM(diff);
 #endif
 }
 
