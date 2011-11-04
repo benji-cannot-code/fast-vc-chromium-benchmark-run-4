15c9175d4b3f465d72d11a4e349631ab84a732fc
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-04 18:27:35 +0000

    Correct usage of LayoutUnits in RenderImage and SVGImage https://bugs.webkit.org/show_bug.cgi?id=71525
    Reviewed by Darin Adler.
    
    Correcting incorrect usage of LayoutUnits for image sizes that should be integers and fixing overloaded functions that
    should be using LayoutUnits.
    
    No new tests -- no change in behavior.
    
    * rendering/RenderImage.cpp:
    (WebCore::RenderImage::imageDimensionsChanged):
    (WebCore::RenderImage::paintIntoRect):
    (WebCore::RenderImage::nodeAtPoint):
    (WebCore::RenderImage::computeReplacedLogicalWidth):
    * rendering/RenderImage.h:
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImageChromeClient::invalidateContentsAndWindow):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99303 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
@@ -74,7 +74,7 @@ private:
         m_image = 0;
     }
 
-    virtual void invalidateContentsAndWindow(const LayoutRect& r, bool)
+    virtual void invalidateContentsAndWindow(const IntRect& r, bool)
     {
         if (m_image && m_image->imageObserver())
             m_image->imageObserver()->changedInRect(m_image, r);
