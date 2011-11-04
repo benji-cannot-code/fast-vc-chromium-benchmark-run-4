60dad13dbbdda9d792e9d15748f78b2d5eddec8a
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 17:28:34 +0000

    Implements maximize and fullscreen for default window manager.
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8477002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108677 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/shell.cc
+++ ui/aura_shell/shell.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura_shell/shell_delegate.h"
 #include "ui/aura_shell/shell_factory.h"
 #include "ui/aura_shell/shell_window_ids.h"
+#include "ui/aura_shell/toplevel_layout_manager.h"
 #include "ui/aura_shell/workspace/workspace_controller.h"
 #include "ui/gfx/compositor/layer.h"
 #include "ui/gfx/compositor/layer_animator.h"
@@ -29,9 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace aura_shell {
 
 namespace {
-
-// The right/left margin of work area in the screen.
-const int kWorkAreaHorizontalMargin = 15;
 
 using views::Widget;
 
@@ -121,14 +119,13 @@ void Shell::Init() {
   desktop_layout->set_launcher_widget(launcher_->widget());
   desktop_layout->set_status_area_widget(internal::CreateStatusArea());
 
-  desktop_window->screen()->set_work_area_insets(
-      gfx::Insets(
-          0, kWorkAreaHorizontalMargin,
-          launcher_->widget()->GetWindowScreenBounds().height(),
-          kWorkAreaHorizontalMargin));
+  desktop_window->screen()->set_work_area_insets(gfx::Insets(
+      0, 0, launcher_->widget()->GetWindowScreenBounds().height(), 0));
 
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kAuraWindows))
     EnableWorkspaceManager();
+  else
+    toplevel_container->SetLayoutManager(new internal::ToplevelLayoutManager());
 
   // Force a layout.
   desktop_layout->OnWindowResized();
