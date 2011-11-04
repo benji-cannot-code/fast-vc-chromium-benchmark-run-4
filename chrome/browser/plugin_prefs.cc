f782f0c1fb222d852c35e74d9b4cc28d21957f34
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 23:53:17 +0000

    Modify code in chrome to call wrapper functions on PluginService rather than calling into webkit::npapi::PluginList directly.
    This change is necessary to ensure we only have one list when we switch
    content to the component build.
    
    R=jam@chromium.org
    TBR=cpu@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8440058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108744 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugin_prefs.cc
+++ chrome/browser/plugin_prefs.cc
@@ -481,7 +481,7 @@ void PluginPrefs::SetPolicyEnforcedPluginPatterns(
 webkit::npapi::PluginList* PluginPrefs::GetPluginList() {
   if (plugin_list_)
     return plugin_list_;
-  return webkit::npapi::PluginList::Singleton();
+  return PluginService::GetInstance()->GetPluginList();
 }
 
 void PluginPrefs::GetPreferencesDataOnFileThread() {
