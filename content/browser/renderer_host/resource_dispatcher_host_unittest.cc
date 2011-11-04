4827852da033d8e6140f56672b0fae036e1f7a05
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 16:41:19 +0000

    Added parentFrameId to webRequest API calls
    BUG=98048
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/8429007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108666 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host_unittest.cc
+++ content/browser/renderer_host/resource_dispatcher_host_unittest.cc
@@ -88,6 +88,8 @@ static ResourceHostMsg_Request CreateResourceRequest(
   request.download_to_file = false;
   request.is_main_frame = true;
   request.frame_id = 0;
+  request.parent_is_main_frame = false;
+  request.parent_frame_id = -1;
   request.transition_type = content::PAGE_TRANSITION_LINK;
   return request;
 }
--- content/common/request_extra_data.cc
+++ content/common/request_extra_data.cc
@@ -7,9 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 RequestExtraData::RequestExtraData(bool is_main_frame,
                                    int64 frame_id,
+                                   bool parent_is_main_frame,
+                                   int64 parent_frame_id,
                                    content::PageTransition transition_type)
     : is_main_frame_(is_main_frame),
       frame_id_(frame_id),
+      parent_is_main_frame_(parent_is_main_frame),
+      parent_frame_id_(parent_frame_id),
       transition_type_(transition_type) {
 }
 
