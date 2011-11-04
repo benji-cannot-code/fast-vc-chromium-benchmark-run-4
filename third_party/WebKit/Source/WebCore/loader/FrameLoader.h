77c69f1844f273d1c3f1303b9b104fb41a86ec16
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-04 22:50:54 +0000

    Delete FrameLoader::isSandboxed https://bugs.webkit.org/show_bug.cgi?id=71591
    Reviewed by Eric Seidel.
    
    Source/WebCore: 
    
    We should always use document->securityOrigin()->isSandboxed because
    that picks up the sandbox bits that are frozen on the document rather
    than the ones that could change on the Frame.
    
    This patch starts preparing us to implement the CSP sandbox directive,
    which will cause use to have document sandbox bits without any attribute.
    
    * bindings/ScriptControllerBase.cpp:
    (WebCore::ScriptController::canExecuteScripts):
        - This call site was the only functional site left where these two
          could be different. This patch causes one progression and one
          regression. The progression is that we now correctly freeze the
          allow-scripts bit when a document is created, but the regression
          is we now allow the execution of JavaScript URLs, as noted in 
          fast/frames/sandboxed-iframe-scripting.html. That's even more of
          an edge case, so I think it's a win overall.
    * loader/DocumentWriter.cpp:
    (WebCore::DocumentWriter::begin):
        - I don't think this part of the change is testable. There's no
          time to execute script between when the bits get copied off the
          Frame and when they're checked, so there's no time to change them.
    * loader/FrameLoader.cpp:
        - Update FIXME comment that is now fixed.
    * loader/FrameLoader.h:
        - Remove wrong API.
    
    LayoutTests: 
    
    This patch updates this test to check a few more cases and notes a bug
    in our current implementation.
    
    * fast/frames/resources/sandboxed-iframe-script-dynamic.html:
    * fast/frames/sandboxed-iframe-scripting-expected.txt:
    * fast/frames/sandboxed-iframe-scripting.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99330 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.h
@@ -212,7 +212,6 @@ public:
 
     void ownerElementSandboxFlagsChanged() { updateSandboxFlags(); }
 
-    bool isSandboxed(SandboxFlags mask) const { return m_sandboxFlags & mask; }
     SandboxFlags sandboxFlags() const { return m_sandboxFlags; }
     // The following sandbox flags will be forced, regardless of changes to
     // the sandbox attribute of any parent frames.
