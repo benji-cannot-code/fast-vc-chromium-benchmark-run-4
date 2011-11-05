0abea0443ca97f2bc52dd2b80f7d050427bad773
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-05 18:07:42 +0000

    Introduced the ConfigurationPolicyHandlerList class, and refactored some of the policy code to use it.
    Review URL: http://codereview.chromium.org/8479003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store.cc
+++ chrome/browser/policy/configuration_policy_pref_store.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/policy/browser_policy_connector.h"
+#include "chrome/browser/policy/configuration_policy_handler_list.h"
 #include "chrome/browser/policy/policy_error_map.h"
 #include "chrome/browser/policy/policy_map.h"
 #include "chrome/browser/prefs/pref_value_map.h"
@@ -162,8 +163,9 @@ PrefValueMap* ConfigurationPolicyPrefStore::CreatePreferencesFromPolicies() {
   scoped_ptr<PrefValueMap> prefs(new PrefValueMap);
   scoped_ptr<PolicyErrorMap> errors(new PolicyErrorMap);
 
-  g_browser_process->browser_policy_connector()->GetPoliciesAsPreferences(
-      policies, prefs.get(), errors.get());
+  const ConfigurationPolicyHandlerList* handler_list =
+      g_browser_process->browser_policy_connector()->GetHandlerList();
+  handler_list->ApplyPolicySettings(policies, prefs.get(), errors.get());
 
   // Retrieve and log the errors once the UI loop is ready. This is only an
   // issue during startup.
