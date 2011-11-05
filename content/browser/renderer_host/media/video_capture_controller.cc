ef16373e4b4fc025e3fc2aedcf56c89329dd6925
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-05 03:41:51 +0000

    fix racing condition by setting frame_info_ on caller thread. BUG=103004 TEST=trybots
    Review URL: http://codereview.chromium.org/8477006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108772 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/video_capture_controller.cc
+++ content/browser/renderer_host/media/video_capture_controller.cc
@@ -331,6 +331,7 @@ void VideoCaptureController::OnError() {
 
 void VideoCaptureController::OnFrameInfo(
     const media::VideoCaptureDevice::Capability& info) {
+  frame_info_= info;
   BrowserThread::PostTask(BrowserThread::IO,
       FROM_HERE,
       base::Bind(&VideoCaptureController::DoFrameInfoOnIOThread,
@@ -403,7 +404,6 @@ void VideoCaptureController::DoFrameInfoOnIOThread(
     }
     return;
   }
-  frame_info_= info;
   frame_info_available_ = true;
 
   for (ControllerClients::iterator client_it = controller_clients_.begin();
