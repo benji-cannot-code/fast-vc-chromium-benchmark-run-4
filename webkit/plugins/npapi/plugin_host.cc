53a721c7d55090f7562026116c21818767602605
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-05 02:40:54 +0000

    Copy WebKit's DEFINE_STATIC_LOCAL to base/basictypes.h
    Use it to remove 3 exit time destructors and 1 static initializer.
    
    BUG=101600,94925
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8474010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108765 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_host.cc
+++ webkit/plugins/npapi/plugin_host.cc
@@ -74,8 +74,6 @@ static bool SupportsSharingAcceleratedSurfaces() {
 }
 #endif
 
-scoped_refptr<PluginHost> PluginHost::singleton_;
-
 PluginHost::PluginHost() {
   InitializeHostFuncs();
 }
@@ -84,12 +82,13 @@ PluginHost::~PluginHost() {
 }
 
 PluginHost *PluginHost::Singleton() {
-  if (singleton_.get() == NULL) {
-    singleton_ = new PluginHost();
+  CR_DEFINE_STATIC_LOCAL(scoped_refptr<PluginHost>, singleton, ());
+  if (singleton.get() == NULL) {
+    singleton = new PluginHost();
   }
 
-  DCHECK(singleton_.get() != NULL);
-  return singleton_;
+  DCHECK(singleton.get() != NULL);
+  return singleton;
 }
 
 void PluginHost::InitializeHostFuncs() {
--- webkit/plugins/npapi/plugin_host.h
+++ webkit/plugins/npapi/plugin_host.h
@@ -55,7 +55,6 @@ class PluginHost : public base::RefCounted<PluginHost> {
 
   PluginHost();
   void InitializeHostFuncs();
-  static scoped_refptr<PluginHost> singleton_;
   NPNetscapeFuncs host_funcs_;
   DISALLOW_COPY_AND_ASSIGN(PluginHost);
 };
