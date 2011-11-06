e2169a86e3f20d439c9206df0231c7c729869914
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-06 19:14:48 +0000

    Minor cleanup: Make method private that is only used for testing
    r=rtenneti
    Review URL: http://codereview.chromium.org/8484003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108817 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tracked_objects_unittest.cc
+++ base/tracked_objects_unittest.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace tracked_objects {
 
 class TrackedObjectsTest : public testing::Test {
- public:
+ protected:
   TrackedObjectsTest() {
     // On entry, leak any database structures in case they are still in use by
     // prior threads.
@@ -26,6 +26,11 @@ class TrackedObjectsTest : public testing::Test {
     // We should not need to leak any structures we create, since we are
     // single threaded, and carefully accounting for items.
     ThreadData::ShutdownSingleThreadedCleanup(false);
+  }
+
+  // Provide access, since this class is a friend of ThreadData.
+  void ShutdownSingleThreadedCleanup(bool leak) {
+    ThreadData::ShutdownSingleThreadedCleanup(leak);
   }
 };
 
@@ -47,7 +52,7 @@ TEST_F(TrackedObjectsTest, MinimalStartupShutdown) {
   data->SnapshotDeathMap(&death_map);
   EXPECT_EQ(0u, death_map.size());
   // Cleanup with no leaking.
-  ThreadData::ShutdownSingleThreadedCleanup(false);
+  ShutdownSingleThreadedCleanup(false);
 
   // Do it again, just to be sure we reset state completely.
   ThreadData::InitializeAndSetTrackingStatus(true);
