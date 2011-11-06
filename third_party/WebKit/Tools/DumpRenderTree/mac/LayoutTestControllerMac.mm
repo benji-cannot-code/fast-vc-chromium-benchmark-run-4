9fdea860284b8037a353945e4bbfff89ba17b788
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-06 01:07:26 +0000

    Fix a small leak in Mac version of LayoutTestController https://bugs.webkit.org/show_bug.cgi?id=71615
    Reviewed by Mark Rowe.
    
    * DumpRenderTree/mac/LayoutTestControllerMac.mm:
    (LayoutTestController::applicationCacheDiskUsageForOrigin): Release the origin.
    (LayoutTestController::localStorageDiskUsageForOrigin): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/LayoutTestControllerMac.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/LayoutTestControllerMac.mm
@@ -140,7 +140,9 @@ long long LayoutTestController::applicationCacheDiskUsageForOrigin(JSStringRef u
 {
     RetainPtr<CFStringRef> urlCF(AdoptCF, JSStringCopyCFString(kCFAllocatorDefault, url));
     WebSecurityOrigin *origin = [[WebSecurityOrigin alloc] initWithURL:[NSURL URLWithString:(NSString *)urlCF.get()]];
-    return [WebApplicationCache diskUsageForOrigin:origin];
+    long long usage = [WebApplicationCache diskUsageForOrigin:origin];
+    [origin release];
+    return usage;
 }
 
 void LayoutTestController::syncLocalStorage()
@@ -152,7 +154,9 @@ long long LayoutTestController::localStorageDiskUsageForOrigin(JSStringRef url)
 {
     RetainPtr<CFStringRef> urlCF(AdoptCF, JSStringCopyCFString(kCFAllocatorDefault, url));
     WebSecurityOrigin *origin = [[WebSecurityOrigin alloc] initWithURL:[NSURL URLWithString:(NSString *)urlCF.get()]];
-    return [[WebStorageManager sharedWebStorageManager] diskUsageForOrigin:origin];
+    long long usage = [[WebStorageManager sharedWebStorageManager] diskUsageForOrigin:origin];
+    [origin release];
+    return usage;
 }
 
 void LayoutTestController::observeStorageTrackerNotifications(unsigned number)
