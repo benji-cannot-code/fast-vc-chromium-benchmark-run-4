f4f62733357c9894f1072c8c550c836bf19e636e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-06 21:31:45 +0000

    Implement the sandbox directive for CSP https://bugs.webkit.org/show_bug.cgi?id=71604
    Reviewed by Sam Weinig.
    
    Source/WebCore:
    
    At TPAC, Microsoft was pushing pretty hard to add the sandbox directive
    to CSP.  There's a question about whether it's going to be in CSP 1.0
    or CSP 1.1, but it seems to be clearly headed into the spec.
    
    This patch implements the sandbox directive for CSP.  It's built on the
    same machinery we use for the sandbox attribute for iframe.  Now that
    I've done the implementation, I'm going to write up some concrete text
    for the spec.
    
    Tests: http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-subframe.html
           http/tests/security/contentSecurityPolicy/sandbox-allow-scripts.html
           http/tests/security/contentSecurityPolicy/sandbox-empty-subframe.html
           http/tests/security/contentSecurityPolicy/sandbox-empty.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::ContentSecurityPolicy::ContentSecurityPolicy):
    (WebCore::ContentSecurityPolicy::applySandboxPolicy):
    (WebCore::ContentSecurityPolicy::addDirective):
    * page/ContentSecurityPolicy.h:
    
    LayoutTests:
    
    Test that the sandbox directive correctly wires up to the sandbox
    flags.  Also, test that the policy inherits into subframes.  These are
    essentially integration tests between the CSP and sandbox subsystems,
    which is why they don't cover all the permutations of the sandbox
    flags.  Those are covered in more detail via tests of the sandbox
    attribute.
    
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-subframe-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-subframe.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-empty-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-empty-subframe-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-empty-subframe.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-empty.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
@@ -74,6 +74,7 @@ private:
     bool parseDirective(const UChar* begin, const UChar* end, String& name, String& value);
     void parseReportURI(const String&);
     void addDirective(const String& name, const String& value);
+    void applySandboxPolicy(const String& sandboxPolicy);
 
     PassOwnPtr<CSPDirective> createCSPDirective(const String& name, const String& value);
 
@@ -101,6 +102,7 @@ private:
     OwnPtr<CSPDirective> m_fontSrc;
     OwnPtr<CSPDirective> m_mediaSrc;
     OwnPtr<CSPDirective> m_connectSrc;
+    bool m_haveSandboxPolicy;
     Vector<KURL> m_reportURLs;
 };
 
