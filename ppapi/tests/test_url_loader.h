f9e6a91a7e1e32722c1d67eec56045a6f66974ef
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-06 00:00:54 +0000

    Remove redundant XHR method/header validation. This is done byAssociatedURLLoader now. BUG=none TEST=ui_tests,nacl_integration
    Review URL: http://codereview.chromium.org/8359009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108801 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_url_loader.h
+++ ppapi/tests/test_url_loader.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/tests/test_case.h"
 
 struct PPB_FileIOTrusted;
+struct PPB_URLLoaderTrusted;
 
 namespace pp {
 class FileIO;
@@ -38,6 +39,15 @@ class TestURLLoader : public TestCase {
   int32_t PrepareFileForPost(const pp::FileRef& file_ref,
                              const std::string& data,
                              std::string* message);
+  std::string GetReachableCrossOriginURL();
+  int32_t OpenUntrusted(const pp::URLRequestInfo& request);
+  int32_t OpenTrusted(const pp::URLRequestInfo& request);
+  int32_t OpenUntrusted(const std::string& method,
+                        const std::string& header);
+  int32_t OpenTrusted(const std::string& method,
+                      const std::string& header);
+  int32_t Open(const pp::URLRequestInfo& request,
+               bool with_universal_access);
 
   std::string TestBasicGET();
   std::string TestBasicPOST();
@@ -50,13 +60,18 @@ class TestURLLoader : public TestCase {
   std::string TestFailsBogusContentLength();
   std::string TestStreamToFile();
   std::string TestSameOriginRestriction();
-  std::string TestJavascriptURLRestriction();
   std::string TestCrossOriginRequest();
+  std::string TestJavascriptURLRestriction();
+  std::string TestMethodRestriction();
+  std::string TestHeaderRestriction();
+  std::string TestCustomReferrer();
+  std::string TestCustomContentTransferEncoding();
   std::string TestAuditURLRedirect();
   std::string TestAbortCalls();
   std::string TestUntendedLoad();
 
   const PPB_FileIOTrusted* file_io_trusted_interface_;
+  const PPB_URLLoaderTrusted* url_loader_trusted_interface_;
 };
 
 #endif  // PAPPI_TESTS_TEST_URL_LOADER_H_
