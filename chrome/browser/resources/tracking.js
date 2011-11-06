b618bedc404591897dd238cc56f86404a947106a
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-06 19:42:24 +0000

    Change default for profiler page to count with no grouping
    The motivation is that the first thing to do in optimization is to see what tasks we can do less of (not merely make faster), so showing the largest counts helps.  In addition, by not grouping, the first page gives a broad overview of what is "happening" in the browser, especially of there are a wild number of any task on any thread.
    
    Also pushed aggregation row to above the header row.
    
    r=eroman
    Review URL: http://codereview.chromium.org/8474017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/tracking.js
+++ chrome/browser/resources/tracking.js
@@ -423,7 +423,7 @@ var MainView = (function() {
   /**
    * The initial keys to sort by when loading the page (can be changed later).
    */
-  var INITIAL_SORT_KEYS = ['-' + KEY_AVG_RUN_TIME];
+  var INITIAL_SORT_KEYS = ['-' + KEY_COUNT];
 
   /**
    * The default sort keys to use when nothing has been specified.
@@ -433,7 +433,7 @@ var MainView = (function() {
   /**
    * The initial keys to group by when loading the page (can be changed later).
    */
-  var INITIAL_GROUP_KEYS = [KEY_DEATH_THREAD];
+  var INITIAL_GROUP_KEYS = [];
 
   // --------------------------------------------------------------------------
   // General utility functions
@@ -751,8 +751,8 @@ var MainView = (function() {
     var thead = addNode(table, 'thead');
     var tbody = addNode(table, 'tbody');
 
-    drawTableHeader(thead, columns, columnOnClickHandler, currentSortKeys);
     drawAggregateRow(thead, data.aggregates, columns);
+    drawTableHeader(thead, columns, columnOnClickHandler, currentSortKeys);
     drawTableBody(tbody, data.rows, columns);
   }
 
