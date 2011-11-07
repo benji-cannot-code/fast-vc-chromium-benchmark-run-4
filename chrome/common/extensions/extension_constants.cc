af4e13e343f1a0ce39c42fe98466bc9642c6b6b9
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 19:13:05 +0000

    Revert 108885 - Add platform_app flag to manifest.
    Enforce constraint that a platform app must declare a panel as its
    launch container.
    
    BUG=none
    TEST=unit tests added.
    
    
    Review URL: http://codereview.chromium.org/8429023
    
    TBR=miket@chromium.org
    Review URL: http://codereview.chromium.org/8467022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108887 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -76,7 +76,6 @@ const char* kPageActionPopupHeight = "height";
 const char* kPageActionPopupPath = "path";
 const char* kPageActions = "page_actions";
 const char* kPermissions = "permissions";
-const char* kPlatformApp = "platform_app";
 const char* kPlugins = "plugins";
 const char* kPluginsPath = "path";
 const char* kPluginsPublic = "public";
@@ -256,8 +255,6 @@ const char* kInvalidKey =
     "Value 'key' is missing or invalid.";
 const char* kInvalidLaunchContainer =
     "Invalid value for 'app.launch.container'.";
-const char* kInvalidLaunchContainerForPlatform =
-    "platform_app requires 'app.launch.container' == 'panel'.";
 const char* kInvalidLaunchHeight =
     "Invalid value for 'app.launch.height'.";
 const char* kInvalidLaunchHeightContainer =
