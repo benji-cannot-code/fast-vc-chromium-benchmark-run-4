f79ac3e99274727f252418cc01901e3b1dca36a2
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 13:41:33 +0000

    Hotfix the hotfix for SyncedSessionTrackerTest.ManyGetTabs (r108858) to compile on Windows.
    TBR=zea
    Review URL: http://codereview.chromium.org/8491015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108859 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/synced_session_tracker_unittest.cc
+++ chrome/browser/sync/glue/synced_session_tracker_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/rand_util.h"
+#include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/sessions/session_types.h"
 #include "chrome/browser/sync/glue/synced_session_tracker.h"
