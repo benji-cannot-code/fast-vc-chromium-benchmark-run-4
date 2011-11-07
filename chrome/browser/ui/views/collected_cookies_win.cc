899582782c91b2a72307364bd1a483f06e153cb8
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 16:13:03 +0000

    views: Remove unneeded includes of separator.h.
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8476012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108873 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/collected_cookies_win.cc
+++ chrome/browser/ui/views/collected_cookies_win.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/button/text_button.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
-#include "views/controls/separator.h"
 #include "views/controls/tabbed_pane/tabbed_pane.h"
 #include "views/layout/box_layout.h"
 #include "views/layout/grid_layout.h"
--- chrome/browser/ui/views/first_run_search_engine_view.cc
+++ chrome/browser/ui/views/first_run_search_engine_view.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "views/controls/button/button.h"
 #include "views/controls/image_view.h"
 #include "views/controls/label.h"
-#include "views/controls/separator.h"
 #include "views/focus/accelerator_handler.h"
 #include "views/layout/layout_constants.h"
 #include "views/view_text_utils.h"
