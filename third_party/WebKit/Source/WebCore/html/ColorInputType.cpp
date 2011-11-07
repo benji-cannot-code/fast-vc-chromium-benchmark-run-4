8c72824dbef0f3d30d24ea5076f1bf368221178b
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-07 11:39:32 +0000

    Change ColorChooser from singleton to ordinary object https://bugs.webkit.org/show_bug.cgi?id=71644
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Changing WebCore::ColorChooser from a singleton to an ordinary object can broaden how browsers implement the color chooser interface.
    
    * WebCore.exp.in:
    * html/ColorInputType.cpp:
    (WebCore::ColorInputType::~ColorInputType):
    (WebCore::ColorInputType::setValue): If a chooser exists, calls Chrome::setSelectedColorInColorChooser
    (WebCore::ColorInputType::handleDOMActivateEvent):
    (WebCore::ColorInputType::detach):
    (WebCore::ColorInputType::didCleanup): Called after cleanup is complete.
    (WebCore::ColorInputType::cleanupColorChooser): Renamed from cleanupColorChooserIfCurrentClient.
    * html/ColorInputType.h:
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::selectColorInColorChooser):
    * html/HTMLInputElement.h:
    * loader/EmptyClients.h:
    (WebCore::EmptyChromeClient::cleanupColorChooser): Added colorChooser argument because there are many WebCore::ColorChoosers now.
    (WebCore::EmptyChromeClient::setSelectedColorInColorChooser): Ditto.
    * page/Chrome.cpp:
    (WebCore::Chrome::cleanupColorChooser): Added colorChooser argument because there are many WebCore::ColorChoosers now.
    (WebCore::Chrome::setSelectedColorInColorChooser): Ditto.
    * page/Chrome.h:
    * page/ChromeClient.h:
    * platform/ColorChooser.cpp:
    (WebCore::ColorChooserClient::~ColorChooserClient):
    (WebCore::ColorChooserClient::newColorChooser): Creates a new color chooser that is connected to itself.
    (WebCore::ColorChooserClient::discardChooser): Discards the connected color chooser.
    (WebCore::ColorChooser::ColorChooser): ColorChooser is RefCounted.
    (WebCore::ColorChooser::create): Creates a ColorChooser that is connected to the given ColorChooserClient.
    (WebCore::ColorChooser::~ColorChooser):
    (WebCore::ColorChooser::didChooseColor): Called from WebKit side when user chose a color. Calls ColorChooserClient::didChooseColor
    (WebCore::ColorChooser::didCleanup): Called from WebKit side when user color chooser was cleaned up. Calls ColorChooserClient::didCleanup
    * platform/ColorChooser.h:
    (WebCore::ColorChooserClient::chooser): Returns the current ColorChooser.
    (WebCore::ColorChooser::disconnectClient): Disconnects the ColorChooserClient.
    * testing/Internals.cpp:
    (WebCore::Internals::selectColorInColorChooser): Added element argument. This calls didChooseColor on the ColorChooser of that element.
    * testing/Internals.h:
    * testing/Internals.idl: Removed connectColorChooserClient and updated selectColorInColorChooser.
    
    LayoutTests:
    
    * fast/forms/color/input-color-onchange-event.html: Changed to match the changes to window.internals object
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
@@ -67,7 +67,7 @@ PassOwnPtr<InputType> ColorInputType::create(HTMLInputElement* element)
 
 ColorInputType::~ColorInputType()
 {
-    cleanupColorChooserIfCurrentClient();
+    cleanupColorChooser();
 }
 
 bool ColorInputType::isColorControl() const
@@ -127,10 +127,9 @@ void ColorInputType::setValue(const String& value, bool valueChanged, bool sendC
         return;
 
     updateColorSwatch();
-    if (ColorChooser::chooser()->client() == this) {
-        if (Chrome* chrome = this->chrome())
-            chrome->setSelectedColorInColorChooser(valueAsColor());
-    }
+    Chrome* chrome = this->chrome();
+    if (chrome && chooser())
+        chrome->setSelectedColorInColorChooser(chooser(), valueAsColor());
 }
 
 void ColorInputType::handleDOMActivateEvent(Event* event)
@@ -141,16 +140,14 @@ void ColorInputType::handleDOMActivateEvent(Event* event)
     if (!ScriptController::processingUserGesture())
         return;
 
-    if (Chrome* chrome = this->chrome()) {
-        ColorChooser::chooser()->connectClient(this);
-        chrome->openColorChooser(ColorChooser::chooser(), valueAsColor());
-    }
+    if (Chrome* chrome = this->chrome())
+        chrome->openColorChooser(newColorChooser(), valueAsColor());
     event->setDefaultHandled();
 }
 
 void ColorInputType::detach()
 {
-    cleanupColorChooserIfCurrentClient();
+    cleanupColorChooser();
 }
 
 void ColorInputType::didChooseColor(const Color& color)
@@ -162,17 +159,17 @@ void ColorInputType::didChooseColor(const Color& color)
     element()->dispatchFormControlChangeEvent();
 }
 
-bool ColorInputType::isColorInputType() const
+void ColorInputType::didCleanup()
 {
-    return true;
+    discardChooser();
 }
 
-void ColorInputType::cleanupColorChooserIfCurrentClient() const
+void ColorInputType::cleanupColorChooser()
 {
-    if (ColorChooser::chooser()->client() != this)
-        return;
-    if (Chrome* chrome = this->chrome())
-        chrome->cleanupColorChooser();
+    Chrome* chrome = this->chrome();
+    if (chrome && chooser())
+        chrome->cleanupColorChooser(chooser());
+    discardChooser();
 }
 
 void ColorInputType::updateColorSwatch()
