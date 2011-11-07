65b4e6ae4a46cb65c32b7fb54c2f893caf8a0547
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-07 14:10:56 +0000

    CSSImageValue: Remove inheritance from CachedImageClient. <http://webkit.org/b/71675>
    Reviewed by Antti Koivisto.
    
    CSSImageValue doesn't actually use any of the CachedImageClient
    functionality, and holds a reference to the resource via the
    CachedResourceHandle in StyleCachedImage.
    
    So we can safely remove the multiple inheritance and simplify
    the class.
    
    * css/CSSImageValue.cpp:
    (WebCore::CSSImageValue::~CSSImageValue):
    (WebCore::CSSImageValue::cachedImage):
    (WebCore::CSSImageValue::clearCachedImage):
    * css/CSSImageValue.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSImageValue.h
+++ third_party/WebKit/Source/WebCore/css/CSSImageValue.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSImageValue_h
 
 #include "CSSPrimitiveValue.h"
-#include "CachedImage.h"
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
@@ -32,7 +31,7 @@ class CachedResourceLoader;
 class StyleCachedImage;
 class StyleImage;
 
-class CSSImageValue : public CSSPrimitiveValue, private CachedImageClient {
+class CSSImageValue : public CSSPrimitiveValue {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     static PassRefPtr<CSSImageValue> create() { return adoptRef(new CSSImageValue); }
