8644ab6da02d85a07bcdabefcf45c1447a6c9833
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 21:22:47 +0000

    Remove HostResolverImpl from Mock*HostResolver.
    MockHostResolver should not depend on the job dispatch machinery of
    HostResolverImpl. This implementation aims to be more readable/reliable
    while providing the same testing utility.
    
    MockHostResolver does not use extra threads hence cannot support 
    WaitingHostResolverProc. To mock slow resolution, HangingHostResolver is
    provided for unit_tests:PredictorTest.
    
    Updated host_resolver_impl_unittest to test HostResolverImpl directly
    (rather than through MockHostResolver).
    
    BUG=99889
    TEST=./net_unittests ; ./unit_tests --gtest_filter='PredictorTest.*' ; ./jingle_unittests
    
    
    Review URL: http://codereview.chromium.org/8267001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108910 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mock_host_resolver.h
+++ net/base/mock_host_resolver.h
@@ -8,14 +8,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <list>
+#include <map>
 
+#include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "base/synchronization/waitable_event.h"
-#include "net/base/host_resolver_impl.h"
+#include "base/threading/non_thread_safe.h"
+#include "net/base/host_resolver.h"
 #include "net/base/host_resolver_proc.h"
 
 namespace net {
 
+class HostCache;
 class RuleBasedHostResolverProc;
+
+// Fills |*addrlist| with a socket address for |host_list| which should be a
+// comma-separated list of IPv4 or IPv6 literal(s) without enclosing brackets.
+// If |canonical_name| is non-empty it is used as the DNS canonical name for
+// the host. Returns OK on success, ERR_UNEXPECTED otherwise.
+int ParseAddressList(const std::string& host_list,
+                     const std::string& canonical_name,
+                     AddressList* addrlist);
 
 // In most cases, it is important that unit tests avoid relying on making actual
 // DNS queries since the resulting tests can be flaky, especially if the network
@@ -38,7 +51,9 @@ class RuleBasedHostResolverProc;
 // re-map one hostname to another as well.
 
 // Base class shared by MockHostResolver and MockCachingHostResolver.
-class MockHostResolverBase : public HostResolver {
+class MockHostResolverBase : public HostResolver,
+                             public base::SupportsWeakPtr<MockHostResolverBase>,
+                             public base::NonThreadSafe {
  public:
   virtual ~MockHostResolverBase();
 
@@ -52,13 +67,6 @@ class MockHostResolverBase : public HostResolver {
   // Resets the mock.
   void Reset(HostResolverProc* interceptor);
 
-  void SetPoolConstraints(HostResolverImpl::JobPoolIndex pool_index,
-                          size_t max_outstanding_jobs,
-                          size_t max_pending_requests) {
-    impl_->SetPoolConstraints(
-        pool_index, max_outstanding_jobs, max_pending_requests);
-  }
-
   // HostResolver methods:
   virtual int Resolve(const RequestInfo& info,
                       AddressList* addresses,
@@ -71,16 +79,32 @@ class MockHostResolverBase : public HostResolver {
   virtual void CancelRequest(RequestHandle req) OVERRIDE;
   virtual void AddObserver(Observer* observer) OVERRIDE;
   virtual void RemoveObserver(Observer* observer) OVERRIDE;
+  virtual HostCache* GetHostCache() OVERRIDE;
 
  protected:
-  MockHostResolverBase(bool use_caching);
-
-  scoped_ptr<HostResolverImpl> impl_;
-  scoped_refptr<RuleBasedHostResolverProc> rules_;
-  bool synchronous_mode_;
-  bool use_caching_;
+  explicit MockHostResolverBase(bool use_caching);
 
  private:
+  struct Request;
+  typedef std::map<size_t, Request*> RequestMap;
+
+  // Resolve as IP or from |cache_| return cached error or
+  // DNS_CACHE_MISS if failed.
+  int ResolveFromIPLiteralOrCache(const RequestInfo& info,
+                                  AddressList* addresses);
+  // Resolve via |proc_|.
+  int ResolveProc(size_t id, const RequestInfo& info, AddressList* addresses);
+  // Resolve request stored in |requests_|. Pass rv to callback.
+  void ResolveNow(size_t id);
+
+  bool synchronous_mode_;
+  ObserverList<Observer> observers_;
+  scoped_refptr<RuleBasedHostResolverProc> rules_;
+  scoped_refptr<HostResolverProc> proc_;
+  scoped_ptr<HostCache> cache_;
+  RequestMap requests_;
+  size_t next_request_id_;
+
   DISALLOW_COPY_AND_ASSIGN(MockHostResolverBase);
 };
 
@@ -155,24 +179,23 @@ class RuleBasedHostResolverProc : public HostResolverProc {
   RuleList rules_;
 };
 
-// Using WaitingHostResolverProc you can simulate very long lookups.
-class WaitingHostResolverProc : public HostResolverProc {
+// Create rules that map all requests to localhost.
+RuleBasedHostResolverProc* CreateCatchAllHostResolverProc();
+
+// HangingHostResolver never completes its |Resolve| request.
+class HangingHostResolver : public HostResolver {
  public:
-  explicit WaitingHostResolverProc(HostResolverProc* previous);
-
-  void Signal();
-
-  // HostResolverProc methods:
-  virtual int Resolve(const std::string& host,
-                      AddressFamily address_family,
-                      HostResolverFlags host_resolver_flags,
-                      AddressList* addrlist,
-                      int* os_error);
-
- private:
-  virtual ~WaitingHostResolverProc();
-
-  base::WaitableEvent event_;
+  virtual int Resolve(const RequestInfo& info,
+                      AddressList* addresses,
+                      OldCompletionCallback* callback,
+                      RequestHandle* out_req,
+                      const BoundNetLog& net_log) OVERRIDE;
+  virtual int ResolveFromCache(const RequestInfo& info,
+                               AddressList* addresses,
+                               const BoundNetLog& net_log) OVERRIDE;
+  virtual void CancelRequest(RequestHandle req) OVERRIDE {}
+  virtual void AddObserver(Observer* observer) OVERRIDE {}
+  virtual void RemoveObserver(Observer* observer) OVERRIDE {}
 };
 
 // This class sets the default HostResolverProc for a particular scope.  The
--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/auth.h"
 #include "net/base/capturing_net_log.h"
 #include "net/base/completion_callback.h"
+#include "net/base/host_cache.h"
 #include "net/base/mock_host_resolver.h"
 #include "net/base/net_log.h"
 #include "net/base/net_log_unittest.h"
@@ -9120,7 +9121,7 @@ class OneTimeCachingHostResolver : public net::HostResolver {
                                const BoundNetLog& net_log) OVERRIDE {
     int rv = host_resolver_.ResolveFromCache(info, addresses, net_log);
     if (rv == OK && info.host_port_pair().Equals(host_port_))
-      host_resolver_.Reset(NULL);
+      host_resolver_.GetHostCache()->clear();
     return rv;
   }
 
