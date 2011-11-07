af07dc8ac0618458b884bc61b79990fed6c5d165
robert.nagy@gmail.com <robert.nagy@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 22:50:18 +0000

    OpenBSD patches for skia, split from CR #8275005
    bitmap_platform_device_linux.cc:
      - according to pkg-config cairo include dir is
        /usr/local/include/cairo (even on linux),
        so we have to include cairo.h and not cairo/cairo.h
    
      - Define DrawToNativeContext on OpenBSD too
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8339020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108930 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/bitmap_platform_device_linux.cc
+++ skia/ext/bitmap_platform_device_linux.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "skia/ext/bitmap_platform_device_data.h"
 
+#if defined(OS_OPENBSD)
+#include <cairo.h>
+#else
 #include <cairo/cairo.h>
+#endif
 
 namespace skia {
 
