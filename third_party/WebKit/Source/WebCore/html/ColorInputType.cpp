5b670d5181c0fa919a765e8d1d04e898e1a0f0f2
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-07 02:25:46 +0000

    Remove extra semicolon in ColorInputType https://bugs.webkit.org/show_bug.cgi?id=71639
    Reviewed by Kent Tamura.
    
    * html/ColorInputType.cpp:
    (WebCore::ColorInputType::setValue):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
@@ -119,7 +119,7 @@ void ColorInputType::createShadowSubtree()
     updateColorSwatch();
 }
 
-void ColorInputType::setValue(const String& value, bool valueChanged, bool sendChangeEvent);
+void ColorInputType::setValue(const String& value, bool valueChanged, bool sendChangeEvent)
 {
     InputType::setValue(value, valueChanged, sendChangeEvent);
 
