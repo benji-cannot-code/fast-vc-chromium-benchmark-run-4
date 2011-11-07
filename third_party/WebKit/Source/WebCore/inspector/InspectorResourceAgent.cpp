cd617323693f0c5fa1df900c9056625c5d567be2
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-07 13:22:09 +0000

    Web Inspector: introduce UserAgent override setting. https://bugs.webkit.org/show_bug.cgi?id=71627
    Patch by Pavel Feldman <pfeldman@chromium.org> on 2011-11-07
    Reviewed by Yury Semikhatsky.
    
    * English.lproj/localizedStrings.js:
    * inspector/InspectorResourceAgent.cpp:
    (WebCore::InspectorResourceAgent::clearFrontend):
    (WebCore::InspectorResourceAgent::applyUserAgentOverride):
    (WebCore::InspectorResourceAgent::setUserAgentOverride):
    * inspector/front-end/HelpScreen.js:
    (WebInspector.HelpScreen):
    (WebInspector.HelpScreen.prototype.show):
    (WebInspector.HelpScreen.prototype._onBlur):
    * inspector/front-end/NetworkManager.js:
    (WebInspector.NetworkManager.prototype._cacheDisabledSettingChanged):
    (WebInspector.NetworkManager.prototype._userAgentSettingChanged):
    * inspector/front-end/Settings.js:
    (WebInspector.Settings):
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.SettingsScreen):
    (WebInspector.SettingsScreen.prototype._createSelectSetting.get for):
    (WebInspector.SettingsScreen.prototype._createCustomSetting):
    (WebInspector.SettingsScreen.prototype._createUserActionControl.checkboxClicked):
    (WebInspector.SettingsScreen.prototype._createUserAgentSelectRowElement.get const):
    (WebInspector.SettingsScreen.prototype._createUserAgentSelectRowElement.textDoubleClicked):
    (WebInspector.SettingsScreen.prototype._createUserAgentSelectRowElement.textChanged):
    * inspector/front-end/helpScreen.css:
    (.help-table td):
    (.help-content fieldset label):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorResourceAgent.cpp
@@ -74,6 +74,7 @@ namespace ResourceAgentState {
 static const char resourceAgentEnabled[] = "resourceAgentEnabled";
 static const char extraRequestHeaders[] = "extraRequestHeaders";
 static const char cacheDisabled[] = "cacheDisabled";
+static const char userAgentOverride[] = "userAgentOverride";
 }
 
 void InspectorResourceAgent::setFrontend(InspectorFrontend* frontend)
@@ -86,7 +87,6 @@ void InspectorResourceAgent::clearFrontend()
     m_frontend = 0;
     ErrorString error;
     disable(&error);
-    m_userAgentOverride = "";
 }
 
 void InspectorResourceAgent::restore()
@@ -349,8 +349,9 @@ void InspectorResourceAgent::didLoadXHRSynchronously()
 
 void InspectorResourceAgent::applyUserAgentOverride(String* userAgent)
 {
-    if (!m_userAgentOverride.isEmpty())
-        *userAgent = m_userAgentOverride;
+    String userAgentOverride = m_state->getString(ResourceAgentState::userAgentOverride);
+    if (!userAgentOverride.isEmpty())
+        *userAgent = userAgentOverride;
 }
 
 void InspectorResourceAgent::willRecalculateStyle()
@@ -470,7 +471,7 @@ void InspectorResourceAgent::disable(ErrorString*)
 
 void InspectorResourceAgent::setUserAgentOverride(ErrorString*, const String& userAgent)
 {
-    m_userAgentOverride = userAgent;
+    m_state->setString(ResourceAgentState::userAgentOverride, userAgent);
 }
 
 void InspectorResourceAgent::setExtraHTTPHeaders(ErrorString*, PassRefPtr<InspectorObject> headers)
--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkManager.js
@@ -41,7 +41,11 @@ WebInspector.NetworkManager = function()
         NetworkAgent.setCacheDisabled(true);
     NetworkAgent.enable();
 
-    WebInspector.settings.cacheDisabled.addChangeListener(this._cacheDisabledSettingChanged.bind(this));
+    WebInspector.settings.cacheDisabled.addChangeListener(this._cacheDisabledSettingChanged, this);
+
+    if (WebInspector.settings.userAgent.get())
+        this._userAgentSettingChanged();
+    WebInspector.settings.userAgent.addChangeListener(this._userAgentSettingChanged, this);
 }
 
 WebInspector.NetworkManager.EventTypes = {
@@ -76,6 +80,11 @@ WebInspector.NetworkManager.prototype = {
     _cacheDisabledSettingChanged: function(event)
     {
         NetworkAgent.setCacheDisabled(event.data);
+    },
+
+    _userAgentSettingChanged: function()
+    {
+        NetworkAgent.setUserAgentOverride(WebInspector.settings.userAgent.get());
     }
 }
 
