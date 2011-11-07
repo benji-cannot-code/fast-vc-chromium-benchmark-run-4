8b54a2cc3f6006c134d7a49ce9e2d93fa94a3fde
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-07 21:51:24 +0000

    Change LOG(INFO) to DVLOG(1) in gl_implementation.cc
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8475022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108914 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_implementation.cc
+++ ui/gfx/gl/gl_implementation.cc
@@ -121,9 +121,9 @@ bool InitializeRequestedGLBindings(
     LOG(ERROR) << "Could not initialize GL.";
     return false;
   } else {
-    LOG(INFO) << "Using "
-              << GetGLImplementationName(GetGLImplementation())
-              << " GL implementation.";
+    DVLOG(1) << "Using "
+             << GetGLImplementationName(GetGLImplementation())
+             << " GL implementation.";
     return true;
   }
 }
