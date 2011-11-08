1abb7d4514efd3027cbadb8b163c576fe3494f0f
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 20:20:08 +0000

    Fix more build bustage.
    git-svn-id: svn://svn.chromium.org/blink/trunk@99605 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/mac/BrowserWindowController.m
+++ third_party/WebKit/Tools/MiniBrowser/mac/BrowserWindowController.m
@@ -326,11 +326,6 @@ static void didRunInsecureContentForFrame(WKPageRef page, WKFrameRef frame, WKTy
     LOG(@"didRunInsecureContentForFrame");
 }
 
-static void didDetectXSSForFrame(WKPageRef page, WKFrameRef frame, WKTypeRef userData, const void *clientInfo)
-{
-    LOG(@"didDetectXSSForFrame");
-}
-
 static void didStartProgress(WKPageRef page, const void *clientInfo)
 {
     [(BrowserWindowController *)clientInfo didStartProgress];
@@ -610,7 +605,6 @@ static void runOpenPanel(WKPageRef page, WKFrameRef frame, WKOpenPanelParameters
         didChangeBackForwardList,
         0, // shouldGoToBackForwardItem
         0,  // didFailToInitializePlugin
-        didDetectXSSForFrame,
     };
     WKPageSetPageLoaderClient(_webView.pageRef, &loadClient);
     
