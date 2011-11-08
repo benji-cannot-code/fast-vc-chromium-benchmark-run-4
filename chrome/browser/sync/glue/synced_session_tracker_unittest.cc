948970443a63d9144bddd122c3c1d58524f5a721
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 09:27:43 +0000

    Use base::StringPrintf to fix SyncedSessionTrackerTest.ManyGetTabs under AddressSanitizer Review URL: http://codereview.chromium.org/8492009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/synced_session_tracker_unittest.cc
+++ chrome/browser/sync/glue/synced_session_tracker_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/rand_util.h"
+#include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/sessions/session_types.h"
 #include "chrome/browser/sync/glue/synced_session_tracker.h"
@@ -172,7 +173,7 @@ TEST_F(SyncedSessionTrackerTest, ManyGetTabs) {
   const int kMaxTabs = 1000;
   const int kMaxAttempts = 10000;
   for (int j=0; j<kMaxSessions; ++j) {
-    std::string tag = "tag" + j;
+    std::string tag = base::StringPrintf("tag%d", j);
     for (int i=0; i<kMaxAttempts; ++i) {
       // More attempts than tabs means we'll sometimes get the same tabs,
       // sometimes have to allocate new tabs.
