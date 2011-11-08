65aacb4716696f07121a38aa8d0c9d5f03cf8b06
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 20:10:44 +0000

    [WK2][GTK] The default database & local storage directory names should note they exist for Gtk port's needs https://bugs.webkit.org/show_bug.cgi?id=71837
    Patch by Zan Dobersek <zandobersek@gmail.com> on 2011-11-08
    Reviewed by Martin Robinson.
    
    Ensure that directories for databases and local storage are in a 'webkitgtk'
    directory in current user's data directory, noting they originate from the Gtk port.
    
    * UIProcess/gtk/WebContextGtk.cpp:
    (WebKit::WebContext::platformDefaultDatabaseDirectory):
    (WebKit::WebContext::platformDefaultLocalStorageDirectory):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebContextGtk.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebContextGtk.cpp
@@ -48,7 +48,7 @@ void WebContext::platformInvalidateContext()
 
 String WebContext::platformDefaultDatabaseDirectory() const
 {
-    GOwnPtr<gchar> databaseDirectory(g_build_filename(g_get_user_data_dir(), "webkit", "databases", NULL));
+    GOwnPtr<gchar> databaseDirectory(g_build_filename(g_get_user_data_dir(), "webkitgtk", "databases", NULL));
     return WebCore::filenameToString(databaseDirectory.get());
 }
 
@@ -60,7 +60,7 @@ String WebContext::platformDefaultIconDatabasePath() const
 
 String WebContext::platformDefaultLocalStorageDirectory() const
 {
-    GOwnPtr<gchar> storageDirectory(g_build_filename(g_get_user_data_dir(), "webkit", "localstorage", NULL));
+    GOwnPtr<gchar> storageDirectory(g_build_filename(g_get_user_data_dir(), "webkitgtk", "localstorage", NULL));
     return WebCore::filenameToString(storageDirectory.get());
 }
 
