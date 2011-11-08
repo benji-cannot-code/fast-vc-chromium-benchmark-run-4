41f5e2e16464d0843ff33c789896d1c4ea7662ea
jschuh@chromium.org <jschuh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 17:42:29 +0000

    Document::loader should use documentLoader(), not activeDocumentLoader() https://bugs.webkit.org/show_bug.cgi?id=65895
    Reviewed by Brady Eidson.
    
    Source/WebCore:
    
    Test: fast/loader/stateobjects/replacestate-in-onunload.html
    
    * dom/Document.cpp:
    (WebCore::Document::loader):
    
    LayoutTests:
    
    * fast/loader/stateobjects/replacestate-in-onunload-expected.txt: Added.
    * fast/loader/stateobjects/replacestate-in-onunload.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99579 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -5067,7 +5067,7 @@ DocumentLoader* Document::loader() const
     if (!m_frame)
         return 0;
     
-    DocumentLoader* loader = m_frame->loader()->activeDocumentLoader();
+    DocumentLoader* loader = m_frame->loader()->documentLoader();
     if (!loader)
         return 0;
     
