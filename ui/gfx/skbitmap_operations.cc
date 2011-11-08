9b10f332cabfe043108223f13d0a5b9f836a20d3
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 20:06:36 +0000

    Clean up CreateTransposedBtmap.
    The function started to show up in valgrind reports. It's unlikely this
    change will improve this.
    
    BUG=103391
    TEST=none
    TBR=sky
    
    Review URL: http://codereview.chromium.org/8501013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109081 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/skbitmap_operations.cc
+++ ui/gfx/skbitmap_operations.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -719,13 +719,13 @@ SkBitmap SkBitmapOperations::UnPreMultiply(const SkBitmap& bitmap) {
 SkBitmap SkBitmapOperations::CreateTransposedBtmap(const SkBitmap& image) {
   DCHECK(image.config() == SkBitmap::kARGB_8888_Config);
 
-  SkAutoLockPixels lock_image(image);
-
   SkBitmap transposed;
   transposed.setConfig(
       SkBitmap::kARGB_8888_Config, image.height(), image.width(), 0);
   transposed.allocPixels();
-  transposed.eraseARGB(0, 0, 0, 0);
+
+  SkAutoLockPixels lock_image(image);
+  SkAutoLockPixels lock_transposed(transposed);
 
   for (int y = 0; y < image.height(); ++y) {
     uint32* image_row = image.getAddr32(0, y);
