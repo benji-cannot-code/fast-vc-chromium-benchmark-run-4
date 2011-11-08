b89dd19d97bc4f98d20448c9ed5282bd4ad3aa77
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 11:58:34 +0000

    Polish the extension app notifications a bit:
    1) Add the handleCloseEvent setter so we can get rid of the bubble (munjal, can you take a look at this piece)?
    2) Get rid of error message when uninstalling apps.
    3) Make sure the bubble doesn't pop up in the top left corner while the icons for the apps are loading.
    
    BUG=88067, 102606
    TEST=When loading the NTP, notification bubbles should not flash briefly in the top left corner. The inspector should have no error message when removing (uninstalling apps) and bubbles you close should not come back.
    Review URL: http://codereview.chromium.org/8430035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/apps_page.js
+++ chrome/browser/resources/ntp4/apps_page.js
@@ -338,6 +338,11 @@ cr.define('ntp4', function() {
           // Create a new bubble.
           infoBubble = new cr.ui.ExpandableBubble;
           infoBubble.anchorNode = this;
+          infoBubble.appId = this.appData_.id;
+          infoBubble.handleCloseEvent = function() {
+            chrome.send('closeNotification', [this.appId]);
+            infoBubble.hide();
+          };
         } else {
           // Reuse the old bubble instead of popping up a new bubble over
           // the old one.
@@ -778,7 +783,9 @@ cr.define('ntp4', function() {
   };
 
   function appNotificationChanged(id, notification) {
-    $(id).setupNotification_(notification);
+    var app = $(id);
+    if (app)  // The app might have been uninstalled.
+      app.setupNotification_(notification);
   };
 
   return {
