2ef57a39a76ddf536e5233ad82f7b52807365e93
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 12:40:54 +0000

    [Qt] Improve the animation time constant https://bugs.webkit.org/show_bug.cgi?id=71790
    Reviewed by Simon Hausmann.
    
    Use 250 milliseconds instead of 400, which feels a lot more natural
    and makes the mini browser feel more responsive.
    
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.cpp
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-static const int kScaleAnimationDurationMillis = 400;
+static const int kScaleAnimationDurationMillis = 250;
 
 // Updating content properties cause the notify signals to be sent by the content item itself.
 // We manage these differently, as we do not want to act on them when we are the ones changing the content.
