b652bea9ef18d255333a87ed25b9fc8bcb5f70a2
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 01:03:43 +0000

    Revert "Have ExtensionHost use TabContents instead of RenderViewHost. This is part"
    It broke a browser test on Mac.
    
    BUG=84146
    TEST=no
    
    Review URL: http://codereview.chromium.org/8467039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108957 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_dialog.cc
+++ chrome/browser/ui/views/extensions/extension_dialog.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_notification_types.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/browser/renderer_host/render_widget_host_view.h"
-#include "content/browser/tab_contents/tab_contents.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
 #include "googleurl/src/gurl.h"
@@ -75,7 +74,7 @@ ExtensionDialog* ExtensionDialog::Show(
   ExtensionDialog* dialog = new ExtensionDialog(host, observer);
   dialog->InitWindow(browser, width, height);
   // Ensure the DOM JavaScript can respond immediately to keyboard shortcuts.
-  host->host_contents()->Focus();
+  host->render_view_host()->view()->Focus();
   return dialog;
 }
 
--- chrome/browser/ui/views/extensions/extension_popup.cc
+++ chrome/browser/ui/views/extensions/extension_popup.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/browser/renderer_host/render_widget_host_view.h"
-#include "content/browser/tab_contents/tab_contents.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
 #include "views/widget/root_view.h"
@@ -97,7 +96,7 @@ void ExtensionPopup::BubbleBrowserWindowClosing(BrowserBubble* bubble) {
 
 void ExtensionPopup::BubbleGotFocus(BrowserBubble* bubble) {
   // Forward the focus to the renderer.
-  host()->host_contents()->Focus();
+  host()->render_view_host()->view()->Focus();
 }
 
 void ExtensionPopup::BubbleLostFocus(BrowserBubble* bubble,
