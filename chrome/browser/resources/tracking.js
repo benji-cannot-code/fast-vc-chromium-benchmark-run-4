ea878f6027a16f0440e69a133a61e760f81a3943
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 22:38:07 +0000

    Add support for process ID and process type to about:tracking2.
    BUG=100992
    Review URL: http://codereview.chromium.org/8503027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/tracking.js
+++ chrome/browser/resources/tracking.js
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var g_browserBridge;
 var g_mainView;
 
+// TODO(eroman): Don't comma-separate PID numbers (since they aren't
+//               really quantities).
+
 /**
  * Main entry point called once the page has loaded.
  */
@@ -119,6 +122,8 @@ var MainView = (function() {
   var KEY_MAX_QUEUE_TIME = 'death_data.queue_ms_max';
   var KEY_RUN_TIME = 'death_data.run_ms';
   var KEY_MAX_RUN_TIME = 'death_data.run_ms_max';
+  var KEY_PROCESS_ID = 'process_id';
+  var KEY_PROCESS_TYPE = 'process_type';
 
   // The following are computed properties which we add to each row. They
   // are not present in the original JSON stream.
@@ -270,6 +275,18 @@ var MainView = (function() {
    */
   var KEY_PROPERTIES = {};
 
+  KEY_PROPERTIES[KEY_PROCESS_ID] = {
+    name: 'PID',
+    type: 'number',
+    aggregator: UniquifyAggregator,
+  };
+
+  KEY_PROPERTIES[KEY_PROCESS_TYPE] = {
+    name: 'Process type',
+    type: 'string',
+    aggregator: UniquifyAggregator,
+  };
+
   KEY_PROPERTIES[KEY_BIRTH_THREAD] = {
     name: 'Birth thread',
     type: 'string',
@@ -374,6 +391,8 @@ var MainView = (function() {
     KEY_MAX_QUEUE_TIME,
     KEY_BIRTH_THREAD,
     KEY_DEATH_THREAD,
+    KEY_PROCESS_TYPE,
+    KEY_PROCESS_ID,
     KEY_FUNCTION_NAME,
     KEY_SOURCE_LOCATION,
     KEY_FILE_NAME,
@@ -400,6 +419,8 @@ var MainView = (function() {
    * leads to awkward bucketing.
    */
   var GROUPING_DROPDOWN_CHOICES = [
+    KEY_PROCESS_TYPE,
+    KEY_PROCESS_ID,
     KEY_BIRTH_THREAD,
     KEY_DEATH_THREAD,
     KEY_FUNCTION_NAME,
@@ -935,14 +956,18 @@ var MainView = (function() {
 
   MainView.prototype = {
     addData: function(data) {
-      if (this.allData_) {
-        // TODO(eroman): once the browser is calling receivedData() multiple
-        // times, will need to concatenate it. For now disregard any call after
-        // the first. This way when the C++ side change lands to call multiple
-        // times, the javascript will keep working until I upgrade it.
-        return;
+      var pid = data[KEY_PROCESS_ID];
+      var ptype = data[KEY_PROCESS_TYPE];
+
+      // Augment each data row with the process information.
+      var rows = data.list;
+      for (var i = 0; i < rows.length; ++i) {
+        var e = rows[i];
+        e[KEY_PROCESS_ID] = pid;
+        e[KEY_PROCESS_TYPE] = ptype;
       }
-      this.allData_ = data;
+
+      this.allData_ = this.allData_.concat(rows);
       this.redrawData_();
     },
 
@@ -960,7 +985,7 @@ var MainView = (function() {
 
       // Group, aggregate, filter, and sort the data.
       var groupedData = prepareData(
-          data.list, this.getGroupingFunction_(), this.getFilterFunction_(),
+          data, this.getGroupingFunction_(), this.getFilterFunction_(),
           this.getSortingFunction_());
 
       // Figure out a display order for the groups.
@@ -999,6 +1024,7 @@ var MainView = (function() {
     },
 
     init_: function() {
+      this.allData_ = [];
       this.fillSelectionCheckboxes_($(COLUMN_TOGGLES_CONTAINER_ID));
 
       $(FILTER_SEARCH_ID).onsearch = this.onChangedFilter_.bind(this);
