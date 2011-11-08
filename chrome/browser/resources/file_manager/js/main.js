f688347c591cb1507e62b0c030f9147ecc6e11fc
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 13:53:50 +0000

    Revert 109019 - Revert 109017 - [filebrowser] Histogram for the file browser's open time.
    BUG=chromeos:20553
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8490014
    
    TBR=kaznacheev@chromium.org
    Review URL: http://codereview.chromium.org/8502018
    
    TBR=joi@chromium.org
    Review URL: http://codereview.chromium.org/8503012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main.js
+++ chrome/browser/resources/file_manager/js/main.js
@@ -17,8 +17,12 @@ function init() {
   var rootPaths = ['Downloads', 'removable', 'archive', 'tmp'];
 
   function onEntriesFound(filesystem, entries) {
+    metrics.recordTime('EnumerateRoots');
     FileManager.initStrings(function () {
+      metrics.startInterval('Construct');
       fileManager = new FileManager(document.body, filesystem, entries);
+      metrics.recordTime('Construct');
+      metrics.recordTime('TotalLoad');
       // We're ready to run.  Tests can monitor for this state with
       // ExtensionTestMessageListener listener("ready");
       // ASSERT_TRUE(listener.WaitUntilSatisfied());
@@ -27,6 +31,7 @@ function init() {
   }
 
   function onFileSystemFound(filesystem) {
+    metrics.recordTime('RequestLocalFileSystem');
     console.log('Found filesystem: ' + filesystem.name, filesystem);
 
     var entries = [];
@@ -43,6 +48,7 @@ function init() {
       }
     }
 
+    metrics.startInterval('EnumerateRoots');
     if (filesystem.name.match(/^chrome-extension_\S+:external/i)) {
       // We've been handed the local filesystem, whose root directory
       // cannot be enumerated.
@@ -55,6 +61,7 @@ function init() {
 
   util.installFileErrorToString();
 
-  console.log('Requesting filesystem.');
-  chrome.fileBrowserPrivate.requestLocalFileSystem(onFileSystemFound);
+    console.log('Requesting filesystem.');
+    metrics.startInterval('RequestLocalFileSystem');
+    chrome.fileBrowserPrivate.requestLocalFileSystem(onFileSystemFound);
 }
