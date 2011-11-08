77a51e68e11e90641cef9ba5ef93623d11296b6f
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 17:51:06 +0000

    IndexedDB: Start using the onSuccessWithContinuation() callback https://bugs.webkit.org/show_bug.cgi?id=71565
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    This is a follow-up to r99169 where we start using this new
    callback for cursors.
    
    * storage/IDBCursorBackendImpl.cpp:
    (WebCore::IDBCursorBackendImpl::continueFunctionInternal):
    * storage/IDBRequest.cpp:
    (WebCore::IDBRequest::setCursor):
    
    LayoutTests:
    
    The cursor-inconsistency.html test now passes completely.
    
    * storage/indexeddb/cursor-inconsistency-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBCursorBackendImpl.cpp
+++ third_party/WebKit/Source/WebCore/storage/IDBCursorBackendImpl.cpp
@@ -111,7 +111,7 @@ void IDBCursorBackendImpl::continueFunctionInternal(ScriptExecutionContext*, Pas
         return;
     }
 
-    callbacks->onSuccess(cursor.get()); // FIXME: After Chromium side is ready, this should be callbacks->onSuccessWithContinuation();
+    callbacks->onSuccessWithContinuation();
 }
 
 void IDBCursorBackendImpl::deleteFunction(PassRefPtr<IDBCallbacks> prpCallbacks, ExceptionCode& ec)
