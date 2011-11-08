e56537bac697f89d76d1bb1d9231553901f8a822
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 23:30:48 +0000

    Properly batch cookie tree model updates on initial fetch.
    This avoids sending each cookie one by one to the cookie manager UI, which
    makes it really janky. This behavior appears to have regressed in r95534.
    
    This fix should be merged to M16 and possibly also M15.
    BUG=100391
    Review URL: http://codereview.chromium.org/8483002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cookies_tree_model.cc
+++ chrome/browser/cookies_tree_model.cc
@@ -852,6 +852,7 @@ void CookiesTreeModel::PopulateCookieInfoWithFilter(
   // mmargh mmargh mmargh! delicious!
 
   CookieTreeRootNode* root = static_cast<CookieTreeRootNode*>(GetRoot());
+  NotifyObserverBeginBatch();
   for (CookieList::iterator it = cookie_list_.begin();
        it != cookie_list_.end(); ++it) {
     std::string source_string = it->Source();
@@ -877,6 +878,8 @@ void CookiesTreeModel::PopulateCookieInfoWithFilter(
       cookies_node->AddCookieNode(new_cookie);
     }
   }
+  NotifyObserverTreeNodeChanged(root);
+  NotifyObserverEndBatch();
 }
 
 void CookiesTreeModel::OnDatabaseModelInfoLoaded(
