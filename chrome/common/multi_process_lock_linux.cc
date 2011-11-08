727929e84ec21441b97b1a1dc1eec99a1a713f10
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 20:04:48 +0000

    Pull back some CHECK calls which were removed.
    Original CL http://codereview.chromium.org/8368018/ was sort of
    aggressive in some areas.  I reveiewed it and this pulls back the
    cases I'm pretty sure should be kept.  My basic approach was to keep
    things which would probably never happen in development, and which
    signal a problem which is unlikely to be transient.
    
     - important_file_writer.cc getting impossible amounts of data.
     - cfbundle_blocker.mm logs blocked bundles so user can see.
     - objc_zombie.mm crashes if the objc runtime is unexpectedly bizarre.
     - multi-process lock logs failure.
    
    I've actually seen that last before when a test nested a profile too
    deeply, in which case knowing the filename was the key hint.
    
    
    Review URL: http://codereview.chromium.org/8477018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/multi_process_lock_linux.cc
+++ chrome/common/multi_process_lock_linux.cc
@@ -31,9 +31,8 @@ class MultiProcessLockLinux : public MultiProcessLock {
     }
 
     if (name_.length() > MULTI_PROCESS_LOCK_NAME_MAX_LEN) {
-      DLOG(ERROR) << "Socket name too long (" << name_.length()
-                  << " > " << MULTI_PROCESS_LOCK_NAME_MAX_LEN << ") - "
-                  << name_;
+      LOG(ERROR) << "Socket name too long (" << name_.length()
+                 << " > " << MULTI_PROCESS_LOCK_NAME_MAX_LEN << ") - " << name_;
       return false;
     }
 
@@ -53,7 +52,7 @@ class MultiProcessLockLinux : public MultiProcessLock {
 
     if (print_length < 0 ||
         print_length > static_cast<int>(MULTI_PROCESS_LOCK_NAME_MAX_LEN)) {
-      DPLOG(ERROR) << "Couldn't create sun_path - " << name_;
+      PLOG(ERROR) << "Couldn't create sun_path - " << name_;
       return false;
     }
 
@@ -69,7 +68,7 @@ class MultiProcessLockLinux : public MultiProcessLock {
 
     int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
     if (socket_fd < 0) {
-      DPLOG(ERROR) << "Couldn't create socket - " << name_;
+      PLOG(ERROR) << "Couldn't create socket - " << name_;
       return false;
     }
 
@@ -83,7 +82,7 @@ class MultiProcessLockLinux : public MultiProcessLock {
                << &(address.sun_path[1])
                << " Length: " << length;
       if (HANDLE_EINTR(close(socket_fd)) < 0) {
-        DPLOG(ERROR) << "close";
+        PLOG(ERROR) << "close";
       }
       return false;
     }
--- chrome/common/multi_process_lock_mac.cc
+++ chrome/common/multi_process_lock_mac.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,9 +26,8 @@ class MultiProcessLockMac : public MultiProcessLock {
     }
 
     if (name_.length() > MULTI_PROCESS_LOCK_NAME_MAX_LEN) {
-      DLOG(ERROR) << "Socket name too long (" << name_.length()
-                  << " > " << MULTI_PROCESS_LOCK_NAME_MAX_LEN << ") - "
-                  << name_;
+      LOG(ERROR) << "Socket name too long (" << name_.length()
+                 << " > " << MULTI_PROCESS_LOCK_NAME_MAX_LEN << ") - " << name_;
       return false;
     }
 
