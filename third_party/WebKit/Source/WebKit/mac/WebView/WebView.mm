abf2d7e53b27ec614f6a0036bc24da7c5effa3fa
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 22:44:17 +0000

    https://bugs.webkit.org/show_bug.cgi?id=71856 WebKit should use new NSWindowDidChangeBackingPropertiesNotification instead of old NSWindowDidChangeResolutionNotification -and corresponding- <rdar://problem/10317253>
    Reviewed by Tim Hatcher.
    
    Source/WebKit/mac: 
    
    * WebView/WebView.mm:
    (-[WebView addWindowObserversForWindow:]):
    (-[WebView removeWindowObservers]):
    
    We have to check that the backing scale factor actually changed since this 
    notification can also be used to signify other changes. 
    (-[WebView _windowDidChangeBackingProperties:]):
    
    Source/WebKit2: 
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView addWindowObserversForWindow:]):
    (-[WKView removeWindowObservers]):
    
    We have to check that the backing scale factor actually changed since this 
    notification can also be used to signify other changes. 
    (-[WKView _windowDidChangeBackingProperties:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
@@ -3224,8 +3224,9 @@ static bool needsWebViewInitThreadWorkaround()
     return _private->shouldCloseWithWindow;
 }
 
-// FIXME: Use an AppKit constant for this once one is available.
-static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidChangeResolutionNotification";
+// FIXME: Use AppKit constants for these when they are available.
+static NSString * const windowDidChangeBackingPropertiesNotification = @"NSWindowDidChangeBackingPropertiesNotification";
+static NSString * const backingPropertyOldScaleFactorKey = @"NSBackingPropertyOldScaleFactorKey"; 
 
 - (void)addWindowObserversForWindow:(NSWindow *)window
 {
@@ -3238,8 +3239,8 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
             name:WKWindowWillOrderOnScreenNotification() object:window];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowWillOrderOffScreen:)
             name:WKWindowWillOrderOffScreenNotification() object:window];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidChangeResolution:)
-            name:windowDidChangeResolutionNotification object:window];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidChangeBackingProperties:)
+            name:windowDidChangeBackingPropertiesNotification object:window];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidChangeScreen:)
             name:NSWindowDidChangeScreenNotification object:window];
     }
@@ -3258,7 +3259,7 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
         [[NSNotificationCenter defaultCenter] removeObserver:self
             name:WKWindowWillOrderOffScreenNotification() object:window];
         [[NSNotificationCenter defaultCenter] removeObserver:self
-            name:windowDidChangeResolutionNotification object:window];
+            name:windowDidChangeBackingPropertiesNotification object:window];
         [[NSNotificationCenter defaultCenter] removeObserver:self
             name:NSWindowDidChangeScreenNotification object:window];
     }
@@ -3372,9 +3373,14 @@ static NSString * const windowDidChangeResolutionNotification = @"NSWindowDidCha
         [self close];
 }
 
-- (void)_windowDidChangeResolution:(NSNotification *)notification
+- (void)_windowDidChangeBackingProperties:(NSNotification *)notification
 {
-    _private->page->setDeviceScaleFactor([self _deviceScaleFactor]);
+    CGFloat oldBackingScaleFactor = [[notification.userInfo objectForKey:backingPropertyOldScaleFactorKey] doubleValue]; 
+    CGFloat newBackingScaleFactor = [self _deviceScaleFactor];
+    if (oldBackingScaleFactor == newBackingScaleFactor) 
+        return; 
+
+    _private->page->setDeviceScaleFactor(newBackingScaleFactor);
 }
 
 - (void)setPreferences:(WebPreferences *)prefs
