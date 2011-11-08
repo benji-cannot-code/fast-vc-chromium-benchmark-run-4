faa6254b08dd12f1996f049dea7d81f38a198886
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-08 09:07:29 +0000

    [WK2][GTK] Application cache directory is incorrectly set https://bugs.webkit.org/show_bug.cgi?id=70680
    Patch by Zan Dobersek <zandobersek@gmail.com> on 2011-11-08
    Reviewed by Philippe Normand.
    
    Correctly set application cache directory to be in current
    user's default cache directory.
    
    * UIProcess/gtk/WebContextGtk.cpp:
    (WebKit::WebContext::applicationCacheDirectory):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebContextGtk.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebContextGtk.cpp
@@ -30,13 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FileSystem.h>
 #include <wtf/gobject/GOwnPtr.h>
 
-#include "ApplicationCacheStorage.h"
-
 namespace WebKit {
 
 WTF::String WebContext::applicationCacheDirectory()
 {
-    return WebCore::cacheStorage().cacheDirectory();
+    GOwnPtr<gchar> cacheDirectory(g_build_filename(g_get_user_cache_dir(), "webkitgtk", "applications", NULL));
+    return WebCore::filenameToString(cacheDirectory.get());
 }
 
 void WebContext::platformInitializeWebProcess(WebProcessCreationParameters&)
