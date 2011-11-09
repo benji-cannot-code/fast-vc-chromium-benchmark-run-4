1903bab3f755143d4250e6b7ef022b9e99d3eb39
rgabor@webkit.org <rgabor@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-09 14:07:24 +0000

    Buildfix for 32bit debug mode.
    Reviewed by Csaba Osztrogonác.
    
    * dfg/DFGAbstractState.cpp:
    (JSC::DFG::AbstractState::dump):
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::dump):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGGraph.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGGraph.cpp
@@ -210,7 +210,7 @@ void Graph::dump(NodeIndex nodeIndex, CodeBlock* codeBlock)
         else
             printf("%sid%u", hasPrinted ? ", " : "", storageAccessData.identifierNumber);
         
-        printf(", %lu", storageAccessData.offset);
+        printf(", %lu", static_cast<unsigned long>(storageAccessData.offset));
         hasPrinted = true;
     }
     ASSERT(node.hasVariableAccessData() == node.hasLocal());
