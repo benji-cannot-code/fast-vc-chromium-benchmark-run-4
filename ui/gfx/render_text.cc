5d5dc7ffd757a9b0210053129951c8083732d704
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 19:11:25 +0000

    Update layout when selection is updated.
    BUG=103452
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8502032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109279 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text.cc
+++ ui/gfx/render_text.cc
@@ -134,6 +134,7 @@ void RenderText::ToggleInsertMode() {
 void RenderText::SetDisplayRect(const Rect& r) {
   display_rect_ = r;
   cached_bounds_and_offset_valid_ = false;
+  UpdateLayout();
 }
 
 size_t RenderText::GetCursorPosition() const {
@@ -306,6 +307,7 @@ void RenderText::SetCompositionRange(const ui::Range& composition_range) {
         ui::Range(0, text_.length()).Contains(composition_range));
   composition_range_.set_end(composition_range.end());
   composition_range_.set_start(composition_range.start());
+  UpdateLayout();
 }
 
 void RenderText::ApplyStyleRange(StyleRange style_range) {
@@ -320,6 +322,7 @@ void RenderText::ApplyStyleRange(StyleRange style_range) {
 #endif
   // TODO(xji): only invalidate if font or underline changes.
   cached_bounds_and_offset_valid_ = false;
+  UpdateLayout();
 }
 
 void RenderText::ApplyDefaultStyle() {
@@ -328,6 +331,7 @@ void RenderText::ApplyDefaultStyle() {
   style.range.set_end(text_.length());
   style_ranges_.push_back(style);
   cached_bounds_and_offset_valid_ = false;
+  UpdateLayout();
 }
 
 base::i18n::TextDirection RenderText::GetTextDirection() {
@@ -613,6 +617,7 @@ void RenderText::SetSelectionModel(const SelectionModel& selection_model) {
   selection_model_.set_caret_placement(selection_model.caret_placement());
 
   cached_bounds_and_offset_valid_ = false;
+  UpdateLayout();
 }
 
 void RenderText::MoveCursorTo(size_t position, bool select) {
