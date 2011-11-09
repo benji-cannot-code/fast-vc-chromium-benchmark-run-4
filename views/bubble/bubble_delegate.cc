9888c095462b38d7ed9c3aae69cc3c9f891c6949
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 01:20:39 +0000

    Rebase CriticalNotificationBubble on the new views bubble. Use a LayoutManager instead of manual sizing/layout. Don't allow bubbles off-screen by default (like old bubbles). Don't let CriticalNotificationBubble close on deactivate.
    BUG=98323
    TEST=CriticalNotificationBubble works/acts as before.
    
    Review URL: http://codereview.chromium.org/8334005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109158 0039d316-1c4b-4281-b951-d872f2087c98

--- views/bubble/bubble_delegate.cc
+++ views/bubble/bubble_delegate.cc
@@ -77,7 +77,7 @@ Widget* CreateBorderWidget(BubbleDelegateView* bubble, Widget* parent) {
 BubbleDelegateView::BubbleDelegateView()
     : close_on_esc_(true),
       close_on_deactivate_(true),
-      allow_bubble_offscreen_(true),
+      allow_bubble_offscreen_(false),
       arrow_location_(BubbleBorder::TOP_LEFT),
       color_(SK_ColorWHITE),
       border_widget_(NULL),
@@ -92,7 +92,7 @@ BubbleDelegateView::BubbleDelegateView(
     const SkColor& color)
     : close_on_esc_(true),
       close_on_deactivate_(true),
-      allow_bubble_offscreen_(true),
+      allow_bubble_offscreen_(false),
       anchor_point_(anchor_point),
       arrow_location_(arrow_location),
       color_(color),
