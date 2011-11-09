f0f7c6b36dec0237ae5d6641988da839aaf38b71
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 19:35:10 +0000

    Add a tray notification UI for speech input recording in the extension API.
    BUG=97388
    TEST=existing apitests + status icon unittests
    
    Review URL: http://codereview.chromium.org/8386074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_input_extension_api.h
+++ chrome/browser/speech/speech_input_extension_api.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
-#define CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
+#ifndef CHROME_BROWSER_SPEECH_SPEECH_INPUT_EXTENSION_API_H_
+#define CHROME_BROWSER_SPEECH_SPEECH_INPUT_EXTENSION_API_H_
 #pragma once
 
 #include "chrome/browser/extensions/extension_function.h"
 #include "content/public/browser/notification_observer.h"
 
-class ExtensionSpeechInputManager;
+class SpeechInputExtensionManager;
 
 // Handles asynchronous operations such as starting or stopping speech
 // recognition in the framework of the extension API state machine.
@@ -70,4 +70,4 @@ class IsRecordingSpeechInputFunction : public SyncExtensionFunction {
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.speechInput.isRecording");
 };
 
-#endif  // CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
+#endif  // CHROME_BROWSER_SPEECH_SPEECH_INPUT_EXTENSION_API_H_
