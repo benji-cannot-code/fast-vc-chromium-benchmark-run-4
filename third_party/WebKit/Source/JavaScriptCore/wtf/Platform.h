d4e30428248b87508f2805b7735522e737fcbe78
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-09 11:03:41 +0000

    Enable the DFG JIT on X86-64 Linux platforms https://bugs.webkit.org/show_bug.cgi?id=71373
    Patch by Andy Wingo <wingo@igalia.com> on 2011-11-09
    Reviewed by Filip Pizlo.
    
    * wtf/Platform.h (ENABLE_DFG_JIT): Enable the DFG JIT on the
    x86-64 GNU/Linux platform.
    * CMakeListsEfl.txt: Add JSValue64 implementations to EFL build.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -883,13 +883,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_JIT 1
 #endif
 
-/* Currently for JSVALUE64, only tested on PLATFORM(MAC) */
-#if !defined(ENABLE_DFG_JIT) && ENABLE(JIT) && USE(JSVALUE64) && PLATFORM(MAC)
-#define ENABLE_DFG_JIT 1
-#endif
-
-/* Currently DFG for X86 are only tested on Linux OS and Mac Platform */
-#if !defined(ENABLE_DFG_JIT) && ENABLE(JIT) && CPU(X86) && (PLATFORM(MAC) || OS(LINUX))
+/* Enable the DFG JIT on X86 and X86_64.  Only tested on Mac and GNU/Linux.  */
+#if !defined(ENABLE_DFG_JIT) && ENABLE(JIT) \
+    && (CPU(X86) || CPU(X86_64)) \
+    && (PLATFORM(MAC) || OS(LINUX))
 #define ENABLE_DFG_JIT 1
 #endif
 
