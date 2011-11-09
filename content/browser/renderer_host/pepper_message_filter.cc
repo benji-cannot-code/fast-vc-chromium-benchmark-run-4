23beeb37f627086d85d8f145f9d176b6e447583c
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 01:36:02 +0000

    Revert 109160 - Revert 109153 - Revert 109086 - Add private Pepper API for dealing with PP_Flash_NetAddress.
    [The Win components breakage should be fixed by r109156.]
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8357030
    
    TBR=viettrungluu@chromium.org
    Review URL: http://codereview.chromium.org/8499045
    
    TBR=thakis@chromium.org
    Review URL: http://codereview.chromium.org/8496043
    
    TBR=viettrungluu@chromium.org
    Review URL: http://codereview.chromium.org/8498037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109161 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/pepper_message_filter.cc
+++ content/browser/renderer_host/pepper_message_filter.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/private/ppb_flash_net_connector.h"
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/proxy/ppb_flash_tcp_socket_proxy.h"
-#include "ppapi/shared_impl/private/flash_net_address_impl.h"
 #include "webkit/plugins/ppapi/ppb_flash_net_connector_impl.h"
 
 #if defined(ENABLE_FLAPPER_HACKS)
@@ -56,7 +55,25 @@ using content::BrowserThread;
 
 namespace {
 
-// TODO(viettrungluu): Move (some of) these to flash_net_address_impl.h.
+bool ValidateNetAddress(const PP_Flash_NetAddress& addr) {
+  if (addr.size < sizeof(reinterpret_cast<sockaddr*>(0)->sa_family))
+    return false;
+
+  // TODO(viettrungluu): more careful validation?
+  // Just do a size check for AF_INET.
+  if (reinterpret_cast<const sockaddr*>(addr.data)->sa_family == AF_INET &&
+      addr.size >= sizeof(sockaddr_in))
+    return true;
+
+  // Ditto for AF_INET6.
+  if (reinterpret_cast<const sockaddr*>(addr.data)->sa_family == AF_INET6 &&
+      addr.size >= sizeof(sockaddr_in6))
+    return true;
+
+  // Reject everything else.
+  return false;
+}
+
 bool SockaddrToNetAddress(const sockaddr* sa,
                           socklen_t sa_length,
                           PP_Flash_NetAddress* net_addr) {
@@ -89,8 +106,7 @@ bool AddressListToNetAddress(const net::AddressList& address_list,
 
 bool NetAddressToIPEndPoint(const PP_Flash_NetAddress& net_addr,
                             net::IPEndPoint* ip_end_point) {
-  if (!ip_end_point ||
-      !ppapi::FlashNetAddressImpl::ValidateNetAddress(net_addr))
+  if (!ip_end_point || !ValidateNetAddress(net_addr))
     return false;
 
   if (!ip_end_point->FromSockAddr(
@@ -1090,7 +1106,7 @@ void PepperMessageFilter::OnConnectTcpAddress(int routing_id,
 
   // Validate the address and then continue (doing |connect()|) on a worker
   // thread.
-  if (!ppapi::FlashNetAddressImpl::ValidateNetAddress(addr) ||
+  if (!ValidateNetAddress(addr) ||
       !base::WorkerPool::PostTask(FROM_HERE,
            NewRunnableMethod(
                this,
