bdd278f15942c04f7b1146277f51960f9a36547e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 01:21:35 +0000

    Fix chromeos clang build.
    BUG=none
    TEST=none
    TBR=ben
    
    Review URL: http://codereview.chromium.org/8495041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/omnibox/omnibox_view_gtk.cc
+++ chrome/browser/ui/gtk/omnibox/omnibox_view_gtk.cc
@@ -857,7 +857,7 @@ bool OmniboxViewGtk::IsImeComposing() const {
 }
 
 #if defined(TOOLKIT_VIEWS)
-int OmniboxViewGtk::GetMaxEditWidth(int entry_width) const OVERRIDE {
+int OmniboxViewGtk::GetMaxEditWidth(int entry_width) const {
   return entry_width;
 }
 
