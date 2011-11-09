1fabe0ae833b2512adda739c3dc7071e48544a48
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 08:00:33 +0000

    Remove gradient_button.css
    gradient_button.css duplicates button.css.
    
    BUG=101057
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8351065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109201 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/menu_item.js
+++ chrome/browser/resources/shared/js/cr/ui/menu_item.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,6 +36,10 @@ cr.define('cr.ui', function() {
         this.command = commandId;
 
       this.addEventListener('mouseup', this.handleMouseUp_);
+
+      // Adding the 'custom-appearance' class prevents button.css from changing
+      // the appearance of this element.
+      this.classList.add('custom-appearance');
     },
 
     /**
