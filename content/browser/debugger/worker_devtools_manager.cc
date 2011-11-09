d3d106c51d63303fa19128480d4abf18602e0d6b
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 06:51:47 +0000

    DevTools: notify devtools front-end when inspected worker terminates
    This change depends on a WebKit change: https://bugs.webkit.org/show_bug.cgi?id=71770
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/8496010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109196 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/debugger/worker_devtools_manager.cc
+++ content/browser/debugger/worker_devtools_manager.cc
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebCString.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebDevToolsAgent.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebString.h"
 
 using content::BrowserThread;
 
@@ -162,6 +165,9 @@ class WorkerDevToolsManager::DetachedClientHosts {
       RemovePendingWorkerData(id);
       return;
     }
+    DevToolsManager::GetInstance()->ForwardToDevToolsClient(agent,
+        DevToolsClientMsg_DispatchOnInspectorFrontend(MSG_ROUTING_NONE,
+            WebKit::WebDevToolsAgent::disconnectEventAsText().utf8()));
     int cookie = DevToolsManager::GetInstance()->DetachClientHost(agent);
     if (cookie == -1) {
       RemovePendingWorkerData(id);
