33e1cfefe66832c4a97a5af0c2822479eb6b215f
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-10 18:05:24 +0000

    Fix crash in compositor_unittests when using webkit compositor.
    BUG=None
    TEST=compositor_unittests
    
    Review URL: http://codereview.chromium.org/8507023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109453 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/layer_unittest.cc
+++ ui/gfx/compositor/layer_unittest.cc
@@ -758,6 +758,7 @@ TEST_F(LayerWithRealCompositorTest, MAYBE_DrawPixels) {
 
   SkBitmap bitmap;
   GetCompositor()->ReadPixels(&bitmap);
+  ASSERT_FALSE(bitmap.empty());
 
   SkAutoLockPixels lock(bitmap);
   bool is_all_red = true;
