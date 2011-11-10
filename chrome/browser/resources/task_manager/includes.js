24b4a829d03120becff92f2f9e3c7228b32d952d
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-10 06:20:24 +0000

    WebUI TaskManager: small UI fixes
    - Use global chrome style by including CSS.
    - Prevent page entire scrolling on Mac.
    - Adjust margin.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8509033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/task_manager/includes.js
+++ chrome/browser/resources/task_manager/includes.js
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // reference the resources.
 
 var css = [
+  'chrome_shared.css',
   'list.css',
   'table.css',
   'menu.css',
+  'button.css',
 ];
 
 var script = [
