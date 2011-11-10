2da2908b1e1e2ad491983e38e9617e8cb784a75c
sergio@webkit.org <sergio@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-10 12:38:39 +0000

    [WK2] [GTK] Implement a MouseDown/MouseUp/MouseMoveTo/MouseScrollBy/LeapForward functions for WebKit2 EventSender https://bugs.webkit.org/show_bug.cgi?id=69411
    Reviewed by Martin Robinson.
    
    Tools:
    
    * WebKitTestRunner/EventSenderProxy.h:
    * WebKitTestRunner/InjectedBundle/EventSendingController.h:
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::didReceiveSynchronousMessageFromInjectedBundle):
    use the EventSender instead of WebProcess Event Simulation.
    * WebKitTestRunner/gtk/EventSenderProxyGtk.cpp:
    (WTR::WTREventQueueItem::WTREventQueueItem):
    (WTR::EventSenderProxy::EventSenderProxy):
    (WTR::getMouseButtonModifiers):
    (WTR::eventSenderButtonToGDKButton):
    (WTR::EventSenderProxy::createMouseButtonEvent):
    (WTR::EventSenderProxy::updateClickCountForButton):
    (WTR::EventSenderProxy::replaySavedEvents):
    (WTR::EventSenderProxy::sendOrQueueEvent):
    (WTR::webkitModifiersToGDKModifiers):
    (WTR::getGDKKeySymForKeyRef):
    (WTR::EventSenderProxy::keyDown):
    (WTR::EventSenderProxy::mouseDown):
    (WTR::EventSenderProxy::mouseUp):
    (WTR::EventSenderProxy::mouseMoveTo):
    (WTR::EventSenderProxy::mouseScrollBy):
    (WTR::EventSenderProxy::leapForward):
    
    LayoutTests:
    
    Unskip tests that were previously failing due to the lack of
    MouseDown/MouseUp/MouseMoveTo/MouseScrollBy/LeapForward. Also
    moved around some other tests to their proper section.
    
    * platform/gtk-wk2/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/EventSendingController.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/EventSendingController.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit2/WKGeometry.h>
 #include <wtf/PassRefPtr.h>
 
-#if !PLATFORM(MAC) && !PLATFORM(QT)
+#if !PLATFORM(MAC) && !PLATFORM(QT) && !PLATFORM(GTK)
 #define USE_WEBPROCESS_EVENT_SIMULATION
 #endif
 
