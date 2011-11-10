713f2a6f6c0f6f092313a3c73e2a30b43f49d50a
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-10 17:07:02 +0000

    Web Inspector: [SuggestBox] Correctly position the box under the first character of the suggestion prefix. https://bugs.webkit.org/show_bug.cgi?id=72022
    Reviewed by Pavel Feldman.
    
    On any suggest box position update (updateSuggestions invocation) an anchor is created at the beginning
    of the user-entered word, and its box is ultimately passed into _updateBoxPosition to be used as the
    actual anchor box (instead of the input element box).
    
    * inspector/front-end/TextPrompt.js:
    (WebInspector.TextPrompt.prototype._boxForAnchorAtStart):
    (WebInspector.TextPrompt.prototype._completionsReady):
    (WebInspector.TextPrompt.SuggestBox):
    (WebInspector.TextPrompt.SuggestBox.prototype._onscrollresize):
    (WebInspector.TextPrompt.SuggestBox.prototype._updateBoxPositionWithExistingAnchor):
    (WebInspector.TextPrompt.SuggestBox.prototype._updateBoxPosition):
    (WebInspector.TextPrompt.SuggestBox.prototype.updateSuggestions):
    (WebInspector.TextPrompt.SuggestBox.prototype._completionsReady):
    * inspector/front-end/utilities.js:
    (AnchorBox):
    (Element.prototype.offsetRelativeToWindow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * http://ejohn.org/files/jsdiff.js (released under the MIT license).
  */
 
-function setupPrototypeUtilities() {
-
 Function.prototype.bind = function(thisObject)
 {
     var func = this;
@@ -284,9 +282,27 @@ Element.prototype.totalOffsetTop = function()
     return total;
 }
 
+/**
+ * @constructor
+ * @param {number=} x
+ * @param {number=} y
+ * @param {number=} width
+ * @param {number=} height
+ */
+function AnchorBox(x, y, width, height)
+{
+    this.x = x || 0;
+    this.y = y || 0;
+    this.width = width || 0;
+    this.height = height || 0;
+}
+
+/**
+ * @return {AnchorBox}
+ */
 Element.prototype.offsetRelativeToWindow = function(targetWindow)
 {
-    var elementOffset = {x: 0, y: 0};
+    var elementOffset = new AnchorBox();
     var curElement = this;
     var curWindow = this.ownerDocument.defaultView;
     while (curWindow && curElement) {
@@ -302,6 +318,9 @@ Element.prototype.offsetRelativeToWindow = function(targetWindow)
     return elementOffset;
 }
 
+/**
+ * @return {AnchorBox}
+ */
 Element.prototype.boxInWindow = function(targetWindow, relativeParent)
 {
     targetWindow = targetWindow || this.ownerDocument.defaultView;
@@ -853,10 +872,6 @@ String.format = function(format, substitutions, formatters, initialValue, append
 
     return { formattedResult: result, unusedSubstitutions: unusedSubstitutions };
 }
-
-} // setupPrototypeUtilities()
-
-setupPrototypeUtilities();
 
 function isEnterKey(event) {
     // Check if in IME.
