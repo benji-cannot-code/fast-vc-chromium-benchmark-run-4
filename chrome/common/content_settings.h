3c5466a1268d9e90151a77247beb3cdc60245a3b
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-10 01:56:15 +0000

    Revert 109213 - Take script URLs into account when applying script content settings.
    <cmp> We suspect this CL caused a performance regression on chromium.perf.
          Reverting to see if this is the source of the regression.
    
    Transmit script content settings to the renderer. Use the script URL as the secondary URL
    for the content setting rules.
    
    BUG=90840
    TEST=ChromeRenderViewTest.ContentSettings(Allow|Block)Scripts
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=109005
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=109036
    
    Review URL: http://codereview.chromium.org/8409006
    
    TBR=marja@chromium.org
    Review URL: http://codereview.chromium.org/8510040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109362 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/content_settings.h
+++ chrome/common/content_settings.h
@@ -77,11 +77,4 @@ struct SettingInfo {
 
 }  // namespace content_settings
 
-struct RendererContentSettingRules {
-  RendererContentSettingRules();
-  ~RendererContentSettingRules();
-  ContentSettingsForOneType image_rules;
-  ContentSettingsForOneType script_rules;
-};
-
 #endif  // CHROME_COMMON_CONTENT_SETTINGS_H_
