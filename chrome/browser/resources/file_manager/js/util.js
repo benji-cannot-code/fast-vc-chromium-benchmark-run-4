473c4641c71657658a9bbb004a7a420ca9d5d730
serya@google.com <serya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-10 14:49:44 +0000

    Showing warning of low disk space.
    TEST=None
    BUG=chromium-os:19552
    
    Review URL: http://codereview.chromium.org/8497043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109424 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/util.js
+++ chrome/browser/resources/file_manager/js/util.js
@@ -153,6 +153,10 @@ var util = {
   forEachDirEntry: function(dirEntry, callback) {
     var reader;
 
+    function onError(err) {
+      console.error('Failed to read  dir entries at ' + dirEntry.fullPath);
+    }
+
     function onReadSome(results) {
       if (results.length == 0)
         return callback(null);
@@ -160,11 +164,11 @@ var util = {
       for (var i = 0; i < results.length; i++)
         callback(results[i]);
 
-      reader.readEntries(onReadSome);
+      reader.readEntries(onReadSome, onError);
     };
 
     reader = dirEntry.createReader();
-    reader.readEntries(onReadSome);
+    reader.readEntries(onReadSome, onError);
   },
 
   /**
@@ -245,9 +249,9 @@ var util = {
                 path, {create: false},
                 resultCallback,
                 errorCallback);
-            } else  {
-              errorCallback(err);
-            }
+          } else  {
+            errorCallback(err);
+          }
         });
   },
 
