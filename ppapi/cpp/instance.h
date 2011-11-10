d3d464a16a289bb872ddd17c3c805fa14a46e1cf
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-10 18:35:10 +0000

    Some description changes and documentation for logging.h Review URL: http://codereview.chromium.org/8438005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109463 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/instance.h
+++ ppapi/cpp/instance.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PPAPI_CPP_INSTANCE_H_
 
 /// @file
-/// Defines the C++ wrapper for an instance.
+/// This file defines the C++ wrapper for an instance.
 
 #include <map>
 #include <string>
