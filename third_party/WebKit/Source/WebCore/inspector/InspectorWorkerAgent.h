cf5345ecd6d6a6d383226c426eb4e2d074b48328
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-11 22:41:35 +0000

    Unreviewed, rolling out r99953. Caused 4 tests to hit an assert. https://bugs.webkit.org/show_bug.cgi?id=72163
    Source/WebCore:
    
    * inspector/InspectorInstrumentation.h:
    (WebCore::InspectorInstrumentation::didStartWorkerContext):
    * inspector/InspectorWorkerAgent.cpp:
    (WebCore::InspectorWorkerAgent::InspectorWorkerAgent):
    (WebCore::InspectorWorkerAgent::~InspectorWorkerAgent):
    (WebCore::InspectorWorkerAgent::setFrontend):
    (WebCore::InspectorWorkerAgent::restore):
    (WebCore::InspectorWorkerAgent::clearFrontend):
    (WebCore::InspectorWorkerAgent::setWorkerInspectionEnabled):
    (WebCore::InspectorWorkerAgent::didStartWorkerContext):
    (WebCore::InspectorWorkerAgent::workerContextTerminated):
    * inspector/InspectorWorkerAgent.h:
    
    LayoutTests:
    
    * http/tests/inspector-enabled/dedicated-workers-list-expected.txt: Removed.
    * http/tests/inspector-enabled/dedicated-workers-list.html: Removed.
    * http/tests/inspector-enabled/resources/dedicated-worker.js: Removed.
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorWorkerAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorWorkerAgent.h
@@ -70,9 +70,6 @@ public:
 
 private:
     InspectorWorkerAgent(InstrumentingAgents*, InspectorState*);
-    void createWorkerFrontendChannelsForExistingWorkers();
-    void createWorkerFrontendChannel(WorkerContextProxy*, const String& url);
-    void destroyWorkerFrontendChannels();
 
     InstrumentingAgents* m_instrumentingAgents;
     InspectorFrontend* m_inspectorFrontend;
@@ -81,8 +78,6 @@ private:
     class WorkerFrontendChannel;
     typedef HashMap<int, WorkerFrontendChannel*> WorkerChannels;
     WorkerChannels m_idToChannel;
-    typedef HashMap<WorkerContextProxy*, String> DedicatedWorkers;
-    DedicatedWorkers m_dedicatedWorkers;
 };
 
 } // namespace WebCore
