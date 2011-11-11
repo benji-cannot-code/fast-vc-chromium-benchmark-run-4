3e953d20425230fa5475da4f1ed9674a0badecf0
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 14:11:09 +0000

    Revert 109622 - Export symbols from content.dll that are depended upon by chrome.dll.
    BUG=103896
    TBR=dpranke
    Review URL: http://codereview.chromium.org/8511058
    
    TBR=tommi@chromium.org
    Review URL: http://codereview.chromium.org/8463015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109623 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/websocketstreamhandle_bridge.h
+++ webkit/glue/websocketstreamhandle_bridge.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
-#include "content/common/content_export.h"
 
 class GURL;
 
@@ -25,7 +24,7 @@ class WebSocketStreamHandleDelegate;
 class WebSocketStreamHandleBridge
     : public base::RefCountedThreadSafe<WebSocketStreamHandleBridge> {
  public:
-  CONTENT_EXPORT static WebSocketStreamHandleBridge* Create(
+  static WebSocketStreamHandleBridge* Create(
       WebKit::WebSocketStreamHandle* handle,
       WebSocketStreamHandleDelegate* delegate);
 
