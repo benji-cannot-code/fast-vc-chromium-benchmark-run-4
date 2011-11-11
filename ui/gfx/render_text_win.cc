ecfce41ac9f3101568b2c77a674015cd37ad882c
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 23:22:40 +0000

    Avoid unnecessary work in |RenderTextWin::UpdateLayout()| if there are no runs.
    Avoids calling unnecessary Uniscribe functions until there's text to process.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8515012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109723 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text_win.cc
+++ ui/gfx/render_text_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/string_util.h"
+#include "base/win/scoped_hdc.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/canvas_skia.h"
@@ -317,9 +318,8 @@ bool RenderTextWin::IsCursorablePosition(size_t position) {
 void RenderTextWin::UpdateLayout() {
   // TODO(msw): Skip complex processing if ScriptIsComplex returns false.
   ItemizeLogicalText();
-  HDC hdc = CreateCompatibleDC(NULL);
-  LayoutVisualText(hdc);
-  DeleteDC(hdc);
+  if (!runs_.empty())
+    LayoutVisualText();
 }
 
 size_t RenderTextWin::IndexOfAdjacentGrapheme(size_t index, bool next) {
@@ -399,8 +399,9 @@ void RenderTextWin::ItemizeLogicalText() {
   }
 }
 
-void RenderTextWin::LayoutVisualText(HDC hdc) {
+void RenderTextWin::LayoutVisualText() {
   HRESULT hr = E_FAIL;
+  base::win::ScopedCreateDC hdc(CreateCompatibleDC(NULL));
   std::vector<internal::TextRun*>::const_iterator run_iter;
   for (run_iter = runs_.begin(); run_iter < runs_.end(); ++run_iter) {
     internal::TextRun* run = *run_iter;
