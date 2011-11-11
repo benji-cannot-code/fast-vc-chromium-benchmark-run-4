04b0b7200fb9eaf777fde9233d74e3aa677815c8
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 00:42:16 +0000

    Don't start speech synthesis until after the audio mixer is initialized.
    Merges all Chrome OS audio code to use a single codepath, and
    notify the TTS interface when the audio mixer is initialized so
    that any queued speech can be played at that time.
    
    BUG=103629
    BUG=chromium-os:19067
    TEST=No visible change yet; this will make it possible to eliminate the 3-second startup delay for speech synthesis.
    
    Review URL: http://codereview.chromium.org/8511026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109548 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/extension_tts_api_chromeos.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_TTS_API_CHROMEOS_H_
+#define CHROME_BROWSER_EXTENSIONS_EXTENSION_TTS_API_CHROMEOS_H_
+#pragma once
+
+// This is called by AudioMixerAlsa to signal that it's safe to begin
+// using TTS on ChromeOS - this must happen after AudioMixerAlsa has
+// finished its initialization. NotificationService would be cleaner,
+// but it's not available at the point when this code needs to be called.
+void EnableChromeOsTts();
+
+#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_TTS_API_CHROMEOS_H_
