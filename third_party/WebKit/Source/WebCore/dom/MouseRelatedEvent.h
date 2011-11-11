f39e8582ddc43ea982b76c4beb41d4a67dbadc79
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-11 22:37:13 +0000

    Unreviewed, rolling out r100024. http://trac.webkit.org/changeset/100024 https://bugs.webkit.org/show_bug.cgi?id=72158
    Broke chromium build
    
    * dom/MouseEvent.cpp:
    (WebCore::MouseEvent::initMouseEvent):
    (WebCore::MouseEventDispatchMediator::dispatchEvent):
    * dom/MouseEvent.h:
    * dom/MouseEvent.idl:
    * dom/MouseRelatedEvent.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/MouseRelatedEvent.h
+++ third_party/WebKit/Source/WebCore/dom/MouseRelatedEvent.h
@@ -40,10 +40,6 @@ namespace WebCore {
         const IntPoint& screenLocation() const { return m_screenLocation; }
         int clientX() const { return m_clientLocation.x(); }
         int clientY() const { return m_clientLocation.y(); }
-#if ENABLE(MOUSE_LOCK_API)
-        int movementX() const { return m_movementDelta.x(); }
-        int movementY() const { return m_movementDelta.y(); }
-#endif
         const LayoutPoint& clientLocation() const { return m_clientLocation; }
         int layerX();
         int layerY();
@@ -60,7 +56,7 @@ namespace WebCore {
         // usable with RenderObject::absoluteToLocal).
         const LayoutPoint& absoluteLocation() const { return m_absoluteLocation; }
         void setAbsoluteLocation(const LayoutPoint& p) { m_absoluteLocation = p; }
-
+    
     protected:
         MouseRelatedEvent();
         MouseRelatedEvent(const AtomicString& type, bool canBubble, bool cancelable, PassRefPtr<AbstractView>,
@@ -73,13 +69,10 @@ namespace WebCore {
 
         void computePageLocation();
         void computeRelativePosition();
-
+        
         // Expose these so MouseEvent::initMouseEvent can set them.
         IntPoint m_screenLocation;
         LayoutPoint m_clientLocation;
-#if ENABLE(MOUSE_LOCK_API)
-        LayoutPoint m_movementDelta;
-#endif
 
     private:
         LayoutPoint m_pageLocation;
