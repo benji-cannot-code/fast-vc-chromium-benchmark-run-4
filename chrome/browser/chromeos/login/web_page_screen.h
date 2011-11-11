f00d4c859c0342849be246d83defebd4a518208f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 05:34:49 +0000

    chromeos: Add missing OVERRIDE annotation to classes implementing TabContentsDelegate::IsPopup
    BUG=100276
    R=thestig@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8505016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/web_page_screen.h
+++ chrome/browser/chromeos/login/web_page_screen.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_WEB_PAGE_SCREEN_H_
 #pragma once
 
+#include "base/basictypes.h"
+#include "base/compiler_specific.h"
 #include "base/timer.h"
 #include "chrome/browser/chromeos/login/screen_observer.h"
 #include "content/browser/tab_contents/tab_contents_delegate.h"
@@ -26,11 +28,10 @@ class WebPageScreen : public TabContentsDelegate {
 
  protected:
   // TabContentsDelegate implementation:
-  virtual bool IsPopup(TabContents* source);
   virtual bool ShouldAddNavigationToHistory(
       const history::HistoryAddPageArgs& add_page_args,
-      content::NavigationType navigation_type);
-  virtual bool HandleContextMenu(const ContextMenuParams& params);
+      content::NavigationType navigation_type) OVERRIDE;
+  virtual bool HandleContextMenu(const ContextMenuParams& params) OVERRIDE;
 
   // Called by |timeout_timer_|. Stops page fetching and closes screen.
   virtual void OnNetworkTimeout();
