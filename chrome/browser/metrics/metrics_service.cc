7e18e5386cd148b1205e03412b89768ca34edf8a
petkov@chromium.org <petkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 14:30:41 +0000

    Include PPAPI plugin crashes in the plugin crash data.
    This should enable us to see PPAPI plugin crashes in the plugin crashes column
    on the stability dashboard. Note that each plugin (PPAPI or regular) is included
    separately in the metrics log and aggregated at the server side. This allows us
    to slice and dice the data later, if necessary.
    
    Also:
    
    - Remove a couple of obsolete FRIEND_TEST declarations.
    
    - Use base file name as plugin name when the name of the PPAPI plugin to be
      launched is empty. This is mostly so that the OutOfProcessPPAPITests can pass
      but also matches the logic in PepperPluginInfo::ToWebPluginInfo().
    
    BUG=chromium-os:22465
    TEST=git try, unit tests, kill -SEGV ppapi process, check that it's counted in
    the plugin prefs and transmitted in the metrics log:
    <pluginstability filename="nfGm7uk2q0xBpC3X5CA95A==" launchcount="3"
                     instancecount="1" crashcount="2"/>
    
    
    Review URL: http://codereview.chromium.org/8501005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109627 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -180,7 +180,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/load_notification_details.h"
 #include "content/browser/plugin_service.h"
 #include "content/browser/renderer_host/render_process_host.h"
-#include "content/common/child_process_info.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/common/url_fetcher.h"
 #include "webkit/plugins/webplugininfo.h"
@@ -1357,7 +1356,7 @@ void MetricsService::LogChildProcessChange(
       stats.process_crashes++;
       // Exclude plugin crashes from the count below because we report them via
       // a separate UMA metric.
-      if (child_details->type() != ChildProcessInfo::PLUGIN_PROCESS) {
+      if (!IsPluginProcess(child_details->type())) {
         IncrementPrefValue(prefs::kStabilityChildProcessCrashCount);
       }
       break;
@@ -1474,7 +1473,7 @@ void MetricsService::RecordPluginChanges(PrefService* pref) {
     ChildProcessStats stats = cache_iter->second;
 
     // Insert only plugins information into the plugins list.
-    if (ChildProcessInfo::PLUGIN_PROCESS != stats.process_type)
+    if (!IsPluginProcess(stats.process_type))
       continue;
 
     // TODO(viettrungluu): remove conversion
@@ -1518,6 +1517,12 @@ void MetricsService::RecordCurrentState(PrefService* pref) {
   pref->SetInt64(prefs::kStabilityLastTimestampSec, Time::Now().ToTimeT());
 
   RecordPluginChanges(pref);
+}
+
+// static
+bool MetricsService::IsPluginProcess(ChildProcessInfo::ProcessType type) {
+  return (type == ChildProcessInfo::PLUGIN_PROCESS ||
+          type == ChildProcessInfo::PPAPI_PLUGIN_PROCESS);
 }
 
 static bool IsSingleThreaded() {
