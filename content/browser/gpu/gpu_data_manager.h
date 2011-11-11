f87860a407dcf361a545af9ba4c3a75aff48c5ce
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 21:04:40 +0000

    Allow switching to SwiftShader when GPU is blacklisted
    When a path to swiftshader is specified and the current GPU is blacklisted, tell the gpu process to use swiftshader to render it. If there already is a gpu process, kill it and make a new one that uses swiftshader
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8480015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109695 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager.h
+++ content/browser/gpu/gpu_data_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/observer_list_threadsafe.h"
@@ -128,6 +129,12 @@ class CONTENT_EXPORT GpuDataManager {
   // Returns the Gpu Info as a DictionaryValue.
   DictionaryValue* GpuInfoAsDictionaryValue() const;
 
+  // Returns true if the software rendering should currently be used.
+  bool software_rendering();
+
+  // Register a path to the SwiftShader software renderer.
+  void RegisterSwiftShaderPath(FilePath path);
+
  private:
   class UserFlags {
    public:
@@ -212,6 +219,9 @@ class CONTENT_EXPORT GpuDataManager {
   // lose_context could happen and whether skia is the backend.
   bool supportsAccelerated2dCanvas() const;
 
+  // Try to switch to software rendering, if possible and necessary.
+  void EnableSoftwareRenderingIfNecessary();
+
   bool complete_gpu_info_already_requested_;
 
   GpuFeatureFlags gpu_feature_flags_;
@@ -228,6 +238,9 @@ class CONTENT_EXPORT GpuDataManager {
   const scoped_refptr<GpuDataManagerObserverList> observer_list_;
 
   ListValue log_messages_;
+  bool software_rendering_;
+
+  FilePath swiftshader_path_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuDataManager);
 };
