a8327939bdea8ca28956dbfb22f0ad3a4628e2a5
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 19:08:33 +0000

    [cros, Aura] Fix login screen crash on boot.
    Crash was caused by the uninitialized ScreenAura instance used in
    chromeos::CalculateScreenBounds chrome/browser/chromeos/login/helper.cc:164
    Moved initialization of login screen classes right before message loop start so that Desktop/Screen instances are initialized.
    
    Also fixed crash during sign in.
    
    TBR=nkostylev@chromium.org
    BUG=103892
    TEST=Boot with Aura build, sign in. No crashes
    
    Original CL: http://codereview.chromium.org/8511059/
    
    landing for nkostylev@chromium.org,
    
    Review URL: http://codereview.chromium.org/8540014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1575,11 +1575,6 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
   }
 #endif
 
-  // Tests should be able to tune login manager before showing it.
-  // Thus only show login manager in normal (non-testing) mode.
-  if (!parameters().ui_task)
-    OptionallyRunChromeOSLoginManager(parsed_command_line(), profile_);
-
 #if !defined(OS_MACOSX)
   // Importing other browser settings is done in a browser-like process
   // that exits when this task has finished.
@@ -1860,6 +1855,11 @@ void ChromeBrowserMainParts::StartBrowserOrUITask() {
   // PreMainMessageLoopRun() is complete, and before starting the browser.
   DesktopNotificationServiceFactory::GetForProfile(profile_)->SetUIManager(
       g_browser_process->notification_ui_manager());
+
+  // Tests should be able to tune login manager before showing it.
+  // Thus only show login manager in normal (non-testing) mode.
+  if (!parameters().ui_task)
+    OptionallyRunChromeOSLoginManager(parsed_command_line(), profile_);
 
   if (parameters().ui_task) {
     // We are in test mode. Run one task and enter the main message loop.
