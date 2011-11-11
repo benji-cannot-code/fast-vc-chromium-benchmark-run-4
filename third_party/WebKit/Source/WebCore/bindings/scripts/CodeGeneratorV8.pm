be9beba980179a8d1a0a5c6fa57f90d8dc7566fd
andreas.kling@nokia.com <andreas.kling@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-11 14:37:58 +0000

    Remove ability to call HTMLCollections in V8 bindings as well. <http://webkit.org/b/72045>
    Reviewed by Anders Carlsson.
    
    Source/WebCore: 
    
    Follow the JSC bindings and remove the ability to call HTMLCollections
    in the V8 bindings (excluding HTMLAllCollection.)
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeaderCustomCall):
    (GenerateImplementationCustomCall):
    * dom/NodeList.idl:
    * html/HTMLCollection.idl:
    
        Remove the V8CustomCall attribute as it's no longer needed.
    
    * bindings/v8/custom/V8HTMLCollectionCustom.cpp:
    * bindings/v8/custom/V8NodeListCustom.cpp:
    
    LayoutTests: 
    
    Add a test verifying that calling collections other than document.all
    will now throw.
    
    * fast/dom/collection-calling-expected.txt: Added.
    * fast/dom/collection-calling.html: Added.
    * fast/dom/script-tests/collection-calling.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@99958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -649,7 +649,7 @@ sub GenerateHeaderCustomCall
 {
     my $dataNode = shift;
 
-    if ($dataNode->extendedAttributes->{"CustomCall"} || $dataNode->extendedAttributes->{"V8CustomCall"}) {
+    if ($dataNode->extendedAttributes->{"CustomCall"}) {
         push(@headerContent, "    static v8::Handle<v8::Value> callAsFunctionCallback(const v8::Arguments&);\n");
     }
     if ($dataNode->name eq "Event") {
@@ -2116,7 +2116,7 @@ sub GenerateImplementationCustomCall
 {
     my $dataNode = shift;
     my $interfaceName = $dataNode->name;
-    my $hasCustomCall = $dataNode->extendedAttributes->{"CustomCall"} || $dataNode->extendedAttributes->{"V8CustomCall"};
+    my $hasCustomCall = $dataNode->extendedAttributes->{"CustomCall"};
 
     # FIXME: Remove hard-coded HTMLOptionsCollection reference.
     if ($interfaceName eq "HTMLOptionsCollection") {
