677ab3fd4985a297e0dc0c2053252ebc10d7b898
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 18:24:56 +0000

    Revert 109534 since this change broke clobbered component build.
    Original CL:
    reenable component build w/ fix to test_support_content
    
    TBR=jam@chromium.org
    BUG=90442
    TEST=the win shared debug builder compiles
    
    Review URL: http://codereview.chromium.org/8501025
    
    TBR=dpranke@chromium.org
    Review URL: http://codereview.chromium.org/8538016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109654 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/net/url_request_abort_on_end_job.h
+++ content/browser/net/url_request_abort_on_end_job.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
+#include "content/common/content_export.h"
 #include "net/url_request/url_request_job.h"
 
 // This url request simulates a network error which occurs immediately after
@@ -33,7 +34,7 @@ class URLRequestAbortOnEndJob : public net::URLRequestJob {
   static net::URLRequestJob* Factory(net::URLRequest* request,
                                      const std::string& scheme);
 
-  static void AddUrlHandler();
+  CONTENT_EXPORT static void AddUrlHandler();
 
  private:
   explicit URLRequestAbortOnEndJob(net::URLRequest* request);
