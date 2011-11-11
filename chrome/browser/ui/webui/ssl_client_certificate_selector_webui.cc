816d910e219886435599c4cbabafa9d2568dfeb7
wyck@chromium.org <wyck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-11 02:14:58 +0000

    WebUI SSL Client Certificate Selector Polish
    Adjusts margin around dialog +10px.  Sets width of drop-down selector 100%.
    
    BUG=103240
    TEST=UI Review
    
    
    Review URL: http://codereview.chromium.org/8511016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ssl_client_certificate_selector_webui.cc
+++ chrome/browser/ui/webui/ssl_client_certificate_selector_webui.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kDialogWidth = 442;
+const int kDialogWidth = 460;
 const int kDialogHeight = 382;
 
 // Formats a certificate into details text.
