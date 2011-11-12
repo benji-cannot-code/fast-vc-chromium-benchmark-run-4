07a39baebac3dad621605a1e6cd6603355bcd86a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-12 02:05:39 +0000

    [chromium] Fix CCThreadProxy::setVisible https://bugs.webkit.org/show_bug.cgi?id=71903
    The behavior is different depending on whether we're showing
    or hiding the compositor. This patch fixes both code paths.
    
    Patch by Iain Merrick <husky@google.com> on 2011-11-11
    Reviewed by James Robinson.
    
    * platform/graphics/chromium/cc/CCCompletionEvent.h:
    (WebCore::CCCompletionEvent::CCCompletionEvent):
    (WebCore::CCCompletionEvent::~CCCompletionEvent):
    (WebCore::CCCompletionEvent::wait):
    (WebCore::CCCompletionEvent::signal):
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::setVisible):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100055 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCCompletionEvent.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCCompletionEvent.h
@@ -31,35 +31,54 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 // Used for making blocking calls from one thread to another. Use only when
-// absolutely certain that doing-so will not lead to a livelock.
+// absolutely certain that doing-so will not lead to a deadlock.
 //
 // It is safe to destroy this object as soon as wait() returns.
 class CCCompletionEvent {
 public:
     CCCompletionEvent()
     {
+#ifndef NDEBUG
+        m_waited = false;
+        m_signaled = false;
+#endif
         m_mutex.lock();
     }
 
     ~CCCompletionEvent()
     {
         m_mutex.unlock();
+        ASSERT(m_waited);
+        ASSERT(m_signaled);
     }
 
     void wait()
     {
+        ASSERT(!m_waited);
+#ifndef NDEBUG
+        m_waited = true;
+#endif
         m_condition.wait(m_mutex);
     }
 
     void signal()
     {
         MutexLocker lock(m_mutex);
+        ASSERT(!m_signaled);
+#ifndef NDEBUG
+        m_signaled = true;
+#endif
         m_condition.signal();
     }
 
 private:
     Mutex m_mutex;
     ThreadCondition m_condition;
+#ifndef NDEBUG
+    // Used to assert that wait() and signal() are each called exactly once.
+    bool m_waited;
+    bool m_signaled;
+#endif
 };
 
 }
