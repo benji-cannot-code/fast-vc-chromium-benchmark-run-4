04e4056130356abcc30e9d5befff2ac842cbc310
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-12 01:26:22 +0000

    Crash in styleDidChange when changing a table cell's height. <http://webkit.org/b/72004>
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    Tests: fast/table/crash-table-cell-change-height.html
           fast/table/table-cell-change-height-with-needsCellRecalc-section.html
           fast/table/table-cell-change-last-row-with-needsCellRecalc-section.html
           fast/table/table-row-change-height-with-needsCellRecalc-section.html
    
    r99212 removed our call to recalcCells when the logical height changed. This
    means that we could end up with a cells without having set its row() properly
    which would crash.
    
    The change exposes the was-the-row-properly-set information so that we don't
    try to access a bogus index. This should work properly even if the section
    needs a cell recalc as the current section's row structure would still be
    accurate in this case.
    
    * rendering/RenderTableCell.cpp:
    (WebCore::RenderTableCell::styleDidChange): Check that our row index
    was properly set before calling
    
    * rendering/RenderTableCell.h:
    (WebCore::RenderTableCell::rowWasSet): Helper function to detect
    if we have set up our a row.
    (WebCore::RenderTableCell::row): Use the previous function in the
    ASSERT.
    
    LayoutTests: 
    
    * fast/table/crash-table-cell-change-height-expected.txt: Added.
    * fast/table/crash-table-cell-change-height.html: Added.
    
    * fast/table/table-cell-change-height-with-needsCellRecalc-section-expected.txt: Added.
    * fast/table/table-cell-change-height-with-needsCellRecalc-section.html: Added.
    * fast/table/table-cell-change-last-row-with-needsCellRecalc-section-expected.txt: Added.
    * fast/table/table-cell-change-last-row-with-needsCellRecalc-section.html: Added.
    * fast/table/table-row-change-height-with-needsCellRecalc-section-expected.txt: Added.
    * fast/table/table-row-change-height-with-needsCellRecalc-section.html: Added.
    Those tests checks that we don't try to access bad rows when the section needs a recalc.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100051 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderTableCell.h
@@ -72,9 +72,10 @@ public:
         m_row = row;
     }
 
+    bool rowWasSet() const { return m_row != unsetRowIndex; }
     unsigned row() const
     {
-        ASSERT(m_row != unsetRowIndex);
+        ASSERT(rowWasSet());
         return m_row;
     }
 
