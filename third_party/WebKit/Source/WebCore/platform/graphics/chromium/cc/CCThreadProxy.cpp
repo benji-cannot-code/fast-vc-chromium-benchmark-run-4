808a1c54b62c3cea0feb92b699be65f87a060c8d
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-12 02:56:27 +0000

    Rollout http://trac.webkit.org/changeset/99844 Solution is more complicated than the problem it was intended to solve.
    Source/WebCore:
    
    * platform/graphics/chromium/LayerChromium.cpp:
    * platform/graphics/chromium/LayerChromium.h:
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
    * platform/graphics/chromium/cc/CCLayerImpl.h:
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::applyScrollAndScale):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp:
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.h:
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::processScrollDeltas):
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::beginFrameAndCommit):
    (WebCore::CCThreadProxy::beginFrameCompleteOnImplThread):
    * platform/graphics/chromium/cc/CCThreadProxy.h:
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostImplTest.cpp:
    (WebKit::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/CCFrameRateController.h"
 #include "cc/CCInputHandler.h"
 #include "cc/CCLayerTreeHost.h"
-#include "cc/CCLayerTreeHostCommon.h"
 #include "cc/CCMainThreadTask.h"
 #include "cc/CCScheduler.h"
 #include "cc/CCScopedMainThreadProxy.h"
@@ -356,7 +355,7 @@ void CCThreadProxy::beginFrameAndCommit(int sequenceNumber, double frameBeginTim
         return;
 
     // Scroll deltas need to be applied even if the commit will be dropped.
-    CCLayerTreeHostCommon::applyScrollAndScale(m_layerTreeHost->rootLayer(), *scrollInfo);
+    m_layerTreeHost->applyScrollAndScale(*scrollInfo.get());
 
     // Drop beginFrameAndCommit calls that occur out of sequence. See createBeginFrameAndCommitTaskOnImplThread for
     // an explanation of how out-of-sequence beginFrameAndCommit tasks can occur.
@@ -388,7 +387,7 @@ void CCThreadProxy::beginFrameAndCommit(int sequenceNumber, double frameBeginTim
         // coordinated by the CCScheduler.
         TRACE_EVENT("commit", this, 0);
         CCCompletionEvent completion;
-        s_ccThread->postTask(createCCThreadTask(this, &CCThreadProxy::beginFrameCompleteOnImplThread, AllowCrossThreadAccess(&completion), scrollInfo));
+        s_ccThread->postTask(createCCThreadTask(this, &CCThreadProxy::beginFrameCompleteOnImplThread, AllowCrossThreadAccess(&completion)));
         completion.wait();
     }
 
@@ -397,7 +396,7 @@ void CCThreadProxy::beginFrameAndCommit(int sequenceNumber, double frameBeginTim
     ASSERT(m_lastExecutedBeginFrameAndCommitSequenceNumber == sequenceNumber);
 }
 
-void CCThreadProxy::beginFrameCompleteOnImplThread(CCCompletionEvent* completion, PassOwnPtr<CCScrollAndScaleSet> scrollInfo)
+void CCThreadProxy::beginFrameCompleteOnImplThread(CCCompletionEvent* completion)
 {
     TRACE_EVENT("CCThreadProxy::beginFrameCompleteOnImplThread", this, 0);
     ASSERT(!m_commitCompletionEventOnImplThread);
@@ -414,7 +413,6 @@ void CCThreadProxy::beginFrameCompleteOnImplThread(CCCompletionEvent* completion
     ASSERT(!m_currentTextureUpdaterOnImplThread);
     m_currentTextureUpdaterOnImplThread = adoptPtr(new CCTextureUpdater(m_layerTreeHostImpl->contentsTextureAllocator()));
     m_layerTreeHost->updateCompositorResources(m_layerTreeHostImpl->context(), *m_currentTextureUpdaterOnImplThread);
-    CCLayerTreeHostCommon::applyScrollAndScale(m_layerTreeHostImpl->scrollLayer(), *scrollInfo);
 
     m_schedulerOnImplThread->beginFrameComplete();
 }
--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerRendererChromium.h"
 #include "MockWebGraphicsContext3D.h"
 #include "cc/CCLayerImpl.h"
-#include "cc/CCLayerTreeHostCommon.h"
 #include "cc/CCSingleThreadProxy.h"
 #include <gtest/gtest.h>
 
@@ -127,22 +126,22 @@ TEST_F(CCLayerTreeHostImplTest, scrollDeltaRepeatedScrolls)
     OwnPtr<CCScrollAndScaleSet> scrollInfo;
 
     scrollInfo = m_hostImpl->processScrollDeltas();
-    ASSERT_EQ(root->scrollPosition(), scrollPosition);
+    ASSERT_EQ(root->scrollPosition(), scrollPosition + scrollDelta);
     ASSERT_EQ(scrollInfo->scrolls.size(), 1u);
     expectContains(*scrollInfo.get(), root->id(), scrollDelta);
+    expectClearedScrollDeltasRecursive(root.get());
 
     IntSize scrollDelta2(-5, 27);
     root->scrollBy(scrollDelta2);
     scrollInfo = m_hostImpl->processScrollDeltas();
-    ASSERT_EQ(root->scrollPosition(), scrollPosition);
+    ASSERT_EQ(root->scrollPosition(), scrollPosition + scrollDelta + scrollDelta2);
     ASSERT_EQ(scrollInfo->scrolls.size(), 1u);
-    expectContains(*scrollInfo, root->id(), scrollDelta + scrollDelta2);
-    EXPECT_EQ(root->scrollDelta(), scrollDelta + scrollDelta2);
+    expectContains(*scrollInfo.get(), root->id(), scrollDelta2);
+    expectClearedScrollDeltasRecursive(root.get());
 
-    CCLayerTreeHostCommon::applyScrollAndScale(m_hostImpl->rootLayer(), *scrollInfo);
     root->scrollBy(IntSize());
     scrollInfo = m_hostImpl->processScrollDeltas();
-    ASSERT_EQ(root->scrollPosition(), scrollPosition);
+    ASSERT_EQ(root->scrollPosition(), scrollPosition + scrollDelta + scrollDelta2);
     ASSERT_EQ(scrollInfo->scrolls.size(), 0u);
     expectClearedScrollDeltasRecursive(root.get());
 }
