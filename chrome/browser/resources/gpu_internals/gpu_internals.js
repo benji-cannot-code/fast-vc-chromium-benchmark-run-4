166bb57452d712e4f964d2f1eaae2a089922b0a9
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-12 03:10:51 +0000

    deprecate webui.css
    This file is really only useful to downloads and history. All other includes of the file are senseless copy-pasta as far as I can tell. There is also the problem that due to its poor name, it became a dumping ground for generic stuff like displaytable, and then was included into chrome://settings because of it. Move out the bits that should live elsewhere, remove the rules that are not used anywhere, rename the file to head off re-use.
    
    Also move gpu_internals stuff into gpu_internals/, and remove completely duplicated file webui2.css.
    
    BUG=103688
    TEST=manually checking gpu internals, media internals, policy, tracing, options, history, downloads
    
    Review URL: http://codereview.chromium.org/8511046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109765 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gpu_internals/gpu_internals.js
+++ chrome/browser/resources/gpu_internals/gpu_internals.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-<include src="gpu_internals/browser_bridge.js"/>
-<include src="gpu_internals/info_view.js"/>
+<include src="browser_bridge.js"/>
+<include src="info_view.js"/>
 
 var browserBridge;
 
