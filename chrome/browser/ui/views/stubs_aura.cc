1748bedadd0b81b0dcda3a8748e9411908941611
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-12 01:01:40 +0000

    More stub cleanup. Removed unused #include and stubs for Linux.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8536034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/stubs_aura.cc
+++ chrome/browser/ui/views/stubs_aura.cc
@@ -5,15 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
-#include "chrome/browser/bookmarks/bookmark_editor.h"
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
-#include "chrome/browser/first_run/first_run.h"
-#include "chrome/browser/first_run/first_run_import_observer.h"
 #include "chrome/browser/importer/importer_progress_dialog.h"
-#include "chrome/browser/ui/views/first_run_bubble.h"
 #include "ui/gfx/native_widget_types.h"
 
-#if !defined(OS_WIN)
+#if defined(OS_WIN)
+#include "chrome/browser/first_run/first_run.h"
+#include "chrome/browser/first_run/first_run_import_observer.h"
+#include "chrome/browser/ui/views/first_run_bubble.h"
+#else
 #include "chrome/browser/ui/gtk/certificate_dialogs.h"
 #endif
 
@@ -24,9 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SSLClientAuthHandler;
 class TabContents;
 class TabContentsWrapper;
-namespace crypto {
-class CryptoModuleBlockingPasswordDelegate;
-}
+
 namespace net {
 class SSLCertRequestInfo;
 class X509Certificate;
@@ -61,9 +59,7 @@ crypto::CryptoModuleBlockingPasswordDelegate*
   NOTIMPLEMENTED();
   return NULL;
 }
-#endif
 
-#if !defined(OS_WIN)
 void ShowCryptoModulePasswordDialog(
     const std::string& module_name,
     bool retry,
@@ -82,7 +78,6 @@ void ShowCertificateViewer(gfx::NativeWindow parent,
                            net::X509Certificate* cert) {
   // No certificate viewer on Windows.
 }
-#endif
 
 // static
 FirstRunBubble* FirstRunBubble::Show(
@@ -95,8 +90,7 @@ FirstRunBubble* FirstRunBubble::Show(
   NOTIMPLEMENTED();
   return NULL;
 }
-
-#if !defined(OS_WIN)
+#else
 void ShowCertSelectFileDialog(SelectFileDialog* select_file_dialog,
                               SelectFileDialog::Type type,
                               const FilePath& suggested_path,
@@ -113,7 +107,7 @@ void ShowCertExportDialog(TabContents* tab_contents,
   // TODO(saintlou);
   NOTIMPLEMENTED();
 }
-#endif
+#endif // OS_WIN
 
 namespace importer {
 
