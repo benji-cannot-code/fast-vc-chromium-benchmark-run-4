7b07e154674735e1527ebe1a81ce2c1f8c7e76bf
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-12 16:14:34 +0000

    Fix "unused variable" warning in JSLock https://bugs.webkit.org/show_bug.cgi?id=72213
    Reviewed by Anders Carlsson.
    
    Use ASSERT_UNUSED() instead of ASSERT() to make sure
    that the variable is also used in the release build.
    
    * runtime/JSLock.cpp:
    (JSC::JSLock::lock):
    (JSC::JSLock::unlock):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSLock.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSLock.cpp
@@ -89,9 +89,8 @@ void JSLock::lock(JSLockBehavior lockBehavior)
 
     intptr_t currentLockCount = lockCount();
     if (!currentLockCount && lockBehavior == LockForReal) {
-        int result;
-        result = pthread_mutex_lock(&JSMutex);
-        ASSERT(!result);
+        int result = pthread_mutex_lock(&JSMutex);
+        ASSERT_UNUSED(result, !result);
     }
     setLockCount(currentLockCount + 1);
 }
@@ -109,9 +108,8 @@ void JSLock::unlock(JSLockBehavior lockBehavior)
     intptr_t newLockCount = lockCount() - 1;
     setLockCount(newLockCount);
     if (!newLockCount && lockBehavior == LockForReal) {
-        int result;
-        result = pthread_mutex_unlock(&JSMutex);
-        ASSERT(!result);
+        int result = pthread_mutex_unlock(&JSMutex);
+        ASSERT_UNUSED(result, !result);
     }
 }
 
