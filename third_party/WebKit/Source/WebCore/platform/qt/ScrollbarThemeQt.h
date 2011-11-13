cdafc79ebf8984dfe7d5d74e41986273d071d69f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-13 07:40:46 +0000

    Unreviewed, rolling out r100038. http://trac.webkit.org/changeset/100038 https://bugs.webkit.org/show_bug.cgi?id=72225
    It broke more than 500 layout tests on the Qt bots (Requested
    by Ossy_night on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-11-12
    
    * platform/qt/ScrollbarThemeQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/ScrollbarThemeQt.h
+++ third_party/WebKit/Source/WebCore/platform/qt/ScrollbarThemeQt.h
@@ -57,8 +57,6 @@ public:
 
     virtual int scrollbarThickness(ScrollbarControlSize = RegularScrollbar);
 
-    virtual bool usesOverlayScrollbars() const { return true; }
-
     QStyle* style() const;
 };
 
