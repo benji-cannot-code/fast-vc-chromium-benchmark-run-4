63d0b1a396b1c49dc1b0f250541f6744f5589d74
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 23:51:06 +0000

    Towards 8 bit strings - Add 8 bit handling to JSString Ropes https://bugs.webkit.org/show_bug.cgi?id=72317
    Added bit to track that a rope is made up of all 8 bit fibers.
    Created an 8 bit path (fast and slow cases) to handle 8 bit 
    only ropes.
    
    Reviewed by Oliver Hunt.
    
    * runtime/JSString.cpp:
    (JSC::JSString::resolveRope):
    (JSC::JSString::resolveRopeSlowCase8):
    (JSC::JSString::resolveRopeSlowCase16):
    * runtime/JSString.h:
    (JSC::RopeBuilder::finishCreation):
    (JSC::RopeBuilder::is8Bit):
    (JSC::jsSubstring8):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef JSString_h
 #define JSString_h
-
 #include "CallFrame.h"
 #include "CommonIdentifiers.h"
 #include "Identifier.h"
@@ -86,6 +85,7 @@ namespace JSC {
                     expand();
                 m_jsString->m_fibers[m_index++].set(m_globalData, m_jsString, jsString);
                 m_jsString->m_length += jsString->m_length;
+                m_jsString->m_is8Bit = m_jsString->m_is8Bit && jsString->m_is8Bit;
             }
 
             JSString* release()
@@ -128,6 +128,7 @@ namespace JSC {
             ASSERT(!m_value.isNull());
             Base::finishCreation(globalData);
             m_length = length;
+            m_is8Bit = m_value.impl()->is8Bit();
         }
 
         void finishCreation(JSGlobalData& globalData, size_t length, size_t cost)
@@ -135,6 +136,7 @@ namespace JSC {
             ASSERT(!m_value.isNull());
             Base::finishCreation(globalData);
             m_length = length;
+            m_is8Bit = m_value.impl()->is8Bit();
             Heap::heap(this)->reportExtraMemoryCost(cost);
         }
 
@@ -142,6 +144,7 @@ namespace JSC {
         {
             Base::finishCreation(globalData);
             m_length = s1->length() + s2->length();
+            m_is8Bit = (s1->is8Bit() && s2->is8Bit());
             m_fibers[0].set(globalData, this, s1);
             m_fibers[1].set(globalData, this, s2);
         }
@@ -150,6 +153,7 @@ namespace JSC {
         {
             Base::finishCreation(globalData);
             m_length = s1->length() + s2->length() + s3->length();
+            m_is8Bit = (s1->is8Bit() && s2->is8Bit() &&  s3->is8Bit());
             m_fibers[0].set(globalData, this, s1);
             m_fibers[1].set(globalData, this, s2);
             m_fibers[2].set(globalData, this, s3);
@@ -245,6 +249,7 @@ namespace JSC {
         }
 
         void resolveRope(ExecState*) const;
+        void resolveRopeSlowCase(ExecState*, LChar*) const;
         void resolveRopeSlowCase(ExecState*, UChar*) const;
         void outOfMemory(ExecState*) const;
 
@@ -257,11 +262,13 @@ namespace JSC {
         static const unsigned s_maxInternalRopeLength = 3;
 
         // A string is represented either by a UString or a rope of fibers.
+        bool m_is8Bit : 1;
         unsigned m_length;
         mutable UString m_value;
         mutable FixedArray<WriteBarrier<JSString>, s_maxInternalRopeLength> m_fibers;
 
         bool isRope() const { return m_value.isNull(); }
+        bool is8Bit() const { return m_is8Bit; }
         UString& string() { ASSERT(!isRope()); return m_value; }
 
         friend JSValue jsString(ExecState*, JSString*, JSString*);
