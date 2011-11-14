2922e97bc41ede646ee6976585642401d9f657fa
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 19:25:40 +0000

    Towards 8 Bit Strings: Templatize YARR Parser https://bugs.webkit.org/show_bug.cgi?id=72288
    Changed Yarr::Parser to be a template based on character type.
    
    Reviewed by Oliver Hunt.
    
    * yarr/YarrParser.h:
    (JSC::Yarr::Parser::Parser):
    (JSC::Yarr::parse):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100167 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/yarr/YarrParser.h
+++ third_party/WebKit/Source/JavaScriptCore/yarr/YarrParser.h
@@ -44,7 +44,7 @@ enum BuiltInCharacterClassID {
 };
 
 // The Parser class should not be used directly - only via the Yarr::parse() method.
-template<class Delegate>
+template<class Delegate, typename CharType>
 class Parser {
 private:
     template<class FriendDelegate>
@@ -232,13 +232,13 @@ private:
         : m_delegate(delegate)
         , m_backReferenceLimit(backReferenceLimit)
         , m_err(NoError)
-        , m_data(pattern.characters16())
+        , m_data(pattern.getCharacters<CharType>())
         , m_size(pattern.length())
         , m_index(0)
         , m_parenthesesNestingDepth(0)
     {
     }
-    
+
     /*
      * parseEscape():
      *
@@ -794,7 +794,7 @@ private:
     Delegate& m_delegate;
     unsigned m_backReferenceLimit;
     ErrorCode m_err;
-    const UChar* m_data;
+    const CharType* m_data;
     unsigned m_size;
     unsigned m_index;
     unsigned m_parenthesesNestingDepth;
@@ -865,7 +865,9 @@ private:
 template<class Delegate>
 const char* parse(Delegate& delegate, const UString& pattern, unsigned backReferenceLimit = quantifyInfinite)
 {
-    return Parser<Delegate>(delegate, pattern, backReferenceLimit).parse();
+    if (pattern.is8Bit())
+        return Parser<Delegate, LChar>(delegate, pattern, backReferenceLimit).parse();
+    return Parser<Delegate, UChar>(delegate, pattern, backReferenceLimit).parse();
 }
 
 } } // namespace JSC::Yarr
