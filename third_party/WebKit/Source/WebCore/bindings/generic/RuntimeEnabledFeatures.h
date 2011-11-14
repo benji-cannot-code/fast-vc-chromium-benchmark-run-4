435cffd1bc699e93ab87217e05472ac8d64fce52
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 22:39:48 +0000

    Mouse Lock: Renaming to 'Pointer Lock': Runtime Enable Flags https://bugs.webkit.org/show_bug.cgi?id=72303
    Reviewed by Darin Fisher.
    
    Source/WebCore:
    
    * bindings/generic/RuntimeEnabledFeatures.cpp:
    * bindings/generic/RuntimeEnabledFeatures.h:
    (WebCore::RuntimeEnabledFeatures::webkitPointerLockEnabled):
    (WebCore::RuntimeEnabledFeatures::setWebkitPointerLockEnabled):
    (WebCore::RuntimeEnabledFeatures::webkitPointerEnabled):
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    * page/Settings.h:
    (WebCore::Settings::setPointerLockEnabled):
    (WebCore::Settings::PointerLockEnabled):
    
    Source/WebKit/chromium:
    
    * public/WebRuntimeFeatures.h:
    * public/WebSettings.h:
    * src/WebRuntimeFeatures.cpp:
    (WebKit::WebRuntimeFeatures::enablePointerLock):
    (WebKit::WebRuntimeFeatures::isPointerLockEnabled):
    * src/WebSettingsImpl.cpp:
    (WebKit::WebSettingsImpl::setPointerLockEnabled):
    * src/WebSettingsImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100194 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
@@ -82,9 +82,9 @@ public:
 #endif
 
 #if ENABLE(POINTER_LOCK)
-    static bool webkitMouseLockAPIEnabled() { return isMouseLockAPIEnabled; }
-    static void setWebkitMouseLockAPIEnabled(bool isEnabled) { isMouseLockAPIEnabled = isEnabled; }
-    static bool webkitPointerEnabled() { return isMouseLockAPIEnabled; }
+    static bool webkitPointerLockEnabled() { return isPointerLockEnabled; }
+    static void setWebkitPointerLockEnabled(bool isEnabled) { isPointerLockEnabled = isEnabled; }
+    static bool webkitPointerEnabled() { return isPointerLockEnabled; }
 #endif
 
 #if ENABLE(VIDEO)
@@ -236,7 +236,7 @@ private:
 #endif
 
 #if ENABLE(POINTER_LOCK)
-    static bool isMouseLockAPIEnabled;
+    static bool isPointerLockEnabled;
 #endif
 
 #if ENABLE(MEDIA_SOURCE)
