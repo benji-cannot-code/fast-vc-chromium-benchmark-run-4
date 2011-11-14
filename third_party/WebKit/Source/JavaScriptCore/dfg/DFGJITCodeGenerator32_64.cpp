e0e9e1ff4e208ad44ebbe1251c98adcf84760e34
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 19:23:36 +0000

    32-bit build fix: Removed unused declaration.
    * dfg/DFGJITCodeGenerator32_64.cpp:
    (JSC::DFG::JITCodeGenerator::emitCall):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator32_64.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGJITCodeGenerator32_64.cpp
@@ -1270,7 +1270,6 @@ void JITCodeGenerator::nonSpeculativeNonPeepholeStrictEq(Node& node, bool invert
 void JITCodeGenerator::emitCall(Node& node)
 {
     P_DFGOperation_E slowCallFunction;
-    bool isCall;
 
     if (node.op == Call) {
         slowCallFunction = operationLinkCall;
