5aee5074271918edfc0b385e91a54b9de44e3ab6
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 12:46:56 +0000

    [Qt] Remove reference to QWebNavigationController https://bugs.webkit.org/show_bug.cgi?id=72257
    Reviewed by Simon Hausmann.
    
    QWebNavigationController was removed in r99946, this is just a leftover.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::QQuickWebViewPrivate):
    * UIProcess/API/qt/qquickwebview_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
 
-class QWebNavigationController;
 class QtWebPageProxy;
 
 QT_BEGIN_NAMESPACE
@@ -91,7 +90,6 @@ private:
     QQuickWebView* q_ptr;
     QScopedPointer<QtWebPageProxy> pageProxy;
 
-    QWebNavigationController* navigationController;
     bool useTraditionalDesktopBehaviour;
     QFileDialog* fileDialog;
     WKOpenPanelResultListenerRef openPanelResultListener;
