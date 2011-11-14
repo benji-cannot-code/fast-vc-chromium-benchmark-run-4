6da2ae9b828bbb625ac7c586f8d994a60ca84ce9
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-14 17:01:39 +0000

    [WIN] Remove dependency on pthread from FastMalloc https://bugs.webkit.org/show_bug.cgi?id=72098
    Reviewed by Adam Roben.
    
    All pthread calls are already ported to native Windows calls.
    Use the native version for all OS(WINDOWS) to remove the
    runtime dependency on the pthread dll.
    
    * wtf/FastMalloc.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/FastMalloc.cpp
+++ third_party/WebKit/Source/JavaScriptCore/wtf/FastMalloc.cpp
@@ -80,11 +80,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Assertions.h"
 #include <limits>
-#if OS(WINDOWS) && PLATFORM(CHROMIUM) || OS(WINCE) && PLATFORM(WIN)
+#if OS(WINDOWS)
 #include <windows.h>
 #else
 #include <pthread.h>
-#endif // OS(WINDOWS) && PLATFORM(CHROMIUM) || OS(WINCE) && PLATFORM(WIN)
+#endif
 #include <wtf/StdLibExtras.h>
 #include <string.h>
 
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NDEBUG
 namespace WTF {
 
-#if OS(WINDOWS) && PLATFORM(CHROMIUM) || OS(WINCE) && PLATFORM(WIN)
+#if OS(WINDOWS)
 
 // TLS_OUT_OF_INDEXES is not defined on WinCE.
 #ifndef TLS_OUT_OF_INDEXES
@@ -145,7 +145,7 @@ void fastMallocAllow()
     TlsSetValue(isForibiddenTlsIndex, kTlsAllowValue);
 }
 
-#else // !OS(WINDOWS) || !PLATFORM(CHROMIUM)
+#else // !OS(WINDOWS)
 
 static pthread_key_t isForbiddenKey;
 static pthread_once_t isForbiddenKeyOnce = PTHREAD_ONCE_INIT;
@@ -173,7 +173,7 @@ void fastMallocAllow()
     pthread_once(&isForbiddenKeyOnce, initializeIsForbiddenKey);
     pthread_setspecific(isForbiddenKey, 0);
 }
-#endif // OS(WINDOWS) && PLATFORM(CHROMIUM) || OS(WINCE) && PLATFORM(WIN)
+#endif // OS(WINDOWS)
 
 } // namespace WTF
 #endif // NDEBUG
