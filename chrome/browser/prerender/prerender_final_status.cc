4de2c1467ee7b5264352bb2b795a4787b1c090e8
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 23:56:35 +0000

    Prerendering: Add MatchComplete PPLT R=cbentzel, dominich Review URL: http://codereview.chromium.org/8540025
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109983 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_final_status.cc
+++ chrome/browser/prerender/prerender_final_status.cc
@@ -50,6 +50,7 @@ const char* kFinalStatusNames[] = {
   "DevTools Attached To The Tab",
   "Session Storage Namespace Mismatch",
   "No Use Group",
+  "Match Complete Dummy",
   "Max",
 };
 COMPILE_ASSERT(arraysize(kFinalStatusNames) == FINAL_STATUS_MAX + 1,
