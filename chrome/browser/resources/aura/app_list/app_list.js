6e4907fcb913ebceb5bb83ba1c7f823b3c2909d8
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 21:04:41 +0000

    [Aura] Apply NTP fix to app list webui.
    Fix potential app list crash similar to NTP crash bug http://crbug.com/104059.
    
    BUG=none.
    TEST=After drag-n-drop is supported in aura, drag an app in app list window should not crash.
    
    
    Review URL: http://codereview.chromium.org/8555015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109952 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/aura/app_list/app_list.js
+++ chrome/browser/resources/aura/app_list/app_list.js
@@ -50,7 +50,7 @@ cr.define('appList', function() {
   }
 
   function getAppsPageIndex(page) {
-    appsView.getAppsPageIndex(page);
+    return appsView.getAppsPageIndex(page);
   }
 
   function getCardSlider() {
