2acf39b04cd7ac33f3b5e398b151195243c2b7d2
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 05:38:56 +0000

    Refactor ChannelAuthenticator so that it can be used with Authenticator.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8527018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110051 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/channel_authenticator.h
+++ remoting/protocol/channel_authenticator.h
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 class DrainableIOBuffer;
 class GrowableIOBuffer;
-class SSLClientSocket;
-class SSLServerSocket;
+class SSLSocket;
 }  // namespace net
 
 namespace remoting {
@@ -39,7 +38,7 @@ class ChannelAuthenticator : public base::NonThreadSafe {
   // when authentication is finished. Caller retains ownership of
   // |socket|. |shared_secret| is a shared secret that we use to
   // authenticate the channel.
-  virtual void Authenticate(const std::string& shared_secret,
+  virtual void Authenticate(net::SSLSocket* socket,
                             const DoneCallback& done_callback) = 0;
 
  private:
@@ -48,11 +47,11 @@ class ChannelAuthenticator : public base::NonThreadSafe {
 
 class HostChannelAuthenticator : public ChannelAuthenticator {
  public:
-  HostChannelAuthenticator(net::SSLServerSocket* socket);
+  HostChannelAuthenticator(const std::string& shared_secret);
   virtual ~HostChannelAuthenticator();
 
   // ChannelAuthenticator overrides.
-  virtual void Authenticate(const std::string& shared_secret,
+  virtual void Authenticate(net::SSLSocket* socket,
                             const DoneCallback& done_callback) OVERRIDE;
 
  private:
@@ -61,8 +60,9 @@ class HostChannelAuthenticator : public ChannelAuthenticator {
   bool HandleAuthBytesRead(int result);
   bool VerifyAuthBytes(const std::string& received_auth_bytes);
 
+  std::string shared_secret_;
   std::string auth_bytes_;
-  net::SSLServerSocket* socket_;
+  net::SSLSocket* socket_;
   DoneCallback done_callback_;
 
   scoped_refptr<net::GrowableIOBuffer> auth_read_buf_;
@@ -74,11 +74,11 @@ class HostChannelAuthenticator : public ChannelAuthenticator {
 
 class ClientChannelAuthenticator : public ChannelAuthenticator {
  public:
-  ClientChannelAuthenticator(net::SSLClientSocket* socket);
+  ClientChannelAuthenticator(const std::string& shared_secret);
   virtual ~ClientChannelAuthenticator();
 
   // ChannelAuthenticator overrides.
-  virtual void Authenticate(const std::string& shared_secret,
+  virtual void Authenticate(net::SSLSocket* socket,
                             const DoneCallback& done_callback);
 
  private:
@@ -86,12 +86,14 @@ class ClientChannelAuthenticator : public ChannelAuthenticator {
   void OnAuthBytesWritten(int result);
   bool HandleAuthBytesWritten(int result);
 
-  net::SSLClientSocket* socket_;
+  std::string shared_secret_;
+  net::SSLSocket* socket_;
   DoneCallback done_callback_;
 
   scoped_refptr<net::DrainableIOBuffer> auth_write_buf_;
 
-  net::OldCompletionCallbackImpl<ClientChannelAuthenticator> auth_write_callback_;
+  net::OldCompletionCallbackImpl<ClientChannelAuthenticator>
+      auth_write_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(ClientChannelAuthenticator);
 };
