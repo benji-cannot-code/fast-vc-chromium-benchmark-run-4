25e3a115a325915a3de8d44b8771220bb316e575
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 01:22:59 +0000

    Set the profiler's thread name at a central place
    This will automatically catch thread names in several of the
    child processes we are after, and it also catches the name
    for a webkcore thread.
    
    The underlying function uses a try/catch clause, which
    contaminates the entire function.  As a result, I needed
    to factor out that section, so that I could still call
    to set the profiler thread name.  Without this, the
    shared dbg build will fail to compile.
    
    r=rtenneti
    Review URL: http://codereview.chromium.org/8565036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110003 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/threading/thread_local.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/tracked_objects.h"
 
 #include "base/win/windows_version.h"
 
@@ -28,6 +29,21 @@ typedef struct tagTHREADNAME_INFO {
   DWORD dwThreadID;  // Thread ID (-1=caller thread).
   DWORD dwFlags;  // Reserved for future use, must be zero.
 } THREADNAME_INFO;
+
+// This function has try handling, so it is separated out of its caller.
+void SetNameInternal(PlatformThreadId thread_id, const char* name) {
+  THREADNAME_INFO info;
+  info.dwType = 0x1000;
+  info.szName = name;
+  info.dwThreadID = thread_id;
+  info.dwFlags = 0;
+
+  __try {
+    RaiseException(kVCThreadNameException, 0, sizeof(info)/sizeof(DWORD),
+                   reinterpret_cast<DWORD_PTR*>(&info));
+  } __except(EXCEPTION_CONTINUE_EXECUTION) {
+  }
+}
 
 struct ThreadParams {
   PlatformThread::Delegate* delegate;
@@ -101,23 +117,14 @@ void PlatformThread::Sleep(int duration_ms) {
 // static
 void PlatformThread::SetName(const char* name) {
   current_thread_name.Set(const_cast<char*>(name));
+  tracked_objects::ThreadData::InitializeThreadContext(name);
 
   // The debugger needs to be around to catch the name in the exception.  If
   // there isn't a debugger, we are just needlessly throwing an exception.
   if (!::IsDebuggerPresent())
     return;
 
-  THREADNAME_INFO info;
-  info.dwType = 0x1000;
-  info.szName = name;
-  info.dwThreadID = CurrentId();
-  info.dwFlags = 0;
-
-  __try {
-    RaiseException(kVCThreadNameException, 0, sizeof(info)/sizeof(DWORD),
-                   reinterpret_cast<DWORD_PTR*>(&info));
-  } __except(EXCEPTION_CONTINUE_EXECUTION) {
-  }
+  SetNameInternal(CurrentId(), name);
 }
 
 // static
