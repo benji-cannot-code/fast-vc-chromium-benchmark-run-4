3ec6e03e5e256c2da5c29081737a883223adcf7d
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 18:55:26 +0000

    Point out that reverting clang rolls is safe.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8468008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110126 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # http://code.google.com/p/chromium/wiki/UpdatingClang
+# Reverting problematic clang rolls is safe, though.
 CLANG_REVISION=143497
 
 THIS_DIR="$(dirname "${0}")"
