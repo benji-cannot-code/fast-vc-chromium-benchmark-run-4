77b8de14ff6ca47c6dc0e63b307c459999e2681c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 02:56:30 +0000

    views: Move examples/ directory to ui/views/.
    BUG=104039
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8555013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110023 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/views/examples/native_theme_checkbox_example.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/examples/native_theme_checkbox_example.h"
+
+#include "base/stringprintf.h"
+#include "base/utf_string_conversions.h"
+#include "views/controls/button/checkbox.h"
+#include "views/controls/button/radio_button.h"
+#include "views/layout/fill_layout.h"
+
+namespace examples {
+
+NativeThemeCheckboxExample::NativeThemeCheckboxExample(ExamplesMain* main)
+    : ExampleBase(main, "Native Theme Checkbox"),
+      count_(0) {
+}
+
+NativeThemeCheckboxExample::~NativeThemeCheckboxExample() {
+}
+
+void NativeThemeCheckboxExample::CreateExampleView(views::View* container) {
+  button_ = new views::Checkbox(ASCIIToUTF16("Checkbox"));
+  button_->set_listener(this);
+  container->SetLayoutManager(new views::FillLayout);
+  container->AddChildView(button_);
+}
+
+void NativeThemeCheckboxExample::ButtonPressed(views::Button* sender,
+                                               const views::Event& event) {
+  PrintStatus(base::StringPrintf("Pressed! count: %d", ++count_).c_str());
+}
+
+}  // namespace examples
