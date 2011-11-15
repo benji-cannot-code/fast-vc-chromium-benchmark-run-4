2ff630fed0608658fbacc1fa72d78da08db05819
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 19:15:03 +0000

    sync: Use a single transaction in ProcessUpdates rather than one per entry.
    BUG=104203
    TEST=compare 'Entries' count on about:sync to number of SyncThread>SyncThread Notify tasks in tracker2/profiler.  The entries count should be much smaller.
    
    
    Review URL: http://codereview.chromium.org/8533024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110138 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/process_updates_command.h
+++ chrome/browser/sync/engine/process_updates_command.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncable {
 class ScopedDirLookup;
+class WriteTransaction;
 }
 
 namespace sync_pb {
@@ -40,7 +41,8 @@ class ProcessUpdatesCommand : public ModelChangingSyncerCommand {
  private:
   ServerUpdateProcessingResult ProcessUpdate(
       const syncable::ScopedDirLookup& dir,
-      const sync_pb::SyncEntity& proto_update);
+      const sync_pb::SyncEntity& proto_update,
+      syncable::WriteTransaction* const trans);
   DISALLOW_COPY_AND_ASSIGN(ProcessUpdatesCommand);
 };
 
