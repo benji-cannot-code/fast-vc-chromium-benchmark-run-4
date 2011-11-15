844c138f297343086d290f1af30f791a10cbc5e3
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-15 20:34:59 +0000

    Result of Error.prototype.toString not ES5 conformant https://bugs.webkit.org/show_bug.cgi?id=70889
    Reviewed by Oliver Hunt.
    
    Source/JavaScriptCore: 
    
    * runtime/ErrorPrototype.cpp:
    (JSC::errorProtoFuncToString):
    
    LayoutTests: 
    
    * fast/js/native-error-prototype-expected.txt:
    * fast/js/script-tests/native-error-prototype.js:
    * fast/js/script-tests/toString-recursion.js:
    * fast/js/toString-recursion-expected.txt:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100310 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/native-error-prototype.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/native-error-prototype.js
@@ -1,7 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description(
-'This is a test case for <a href="https://bugs.webkit.org/show_bug.cgi?id=55346">bug 55346</a>.'
+'This is a test case for <a href="https://bugs.webkit.org/show_bug.cgi?id=55346">bug 55346</a> and <a href="https://bugs.webkit.org/show_bug.cgi?id=70889">bug 70889</a>.'
 );
 
 shouldBe("({}).toString.call(Error.prototype)", '"[object Error]"');
 shouldBe("({}).toString.call(RangeError.prototype)", '"[object Error]"');
+
+var err = new Error("message");
+err.name = "";
+shouldBe("err.toString()", '"message"');
