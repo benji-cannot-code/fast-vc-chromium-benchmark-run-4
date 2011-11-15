f6b7051210aeb282cb25e9a8e40e9d60ef0928ac
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 01:19:16 +0000

    Write the install language to the registry for user-level installs.
    Google Update (Omaha) will send the value up in app pings.
    
    BUG=100751
    TEST=install user-level Chrome.  check HKCU\Software\Google\Update\Clients\{8A69D345-D564-463c-AFF1-A69D9E530F96} for a "lang" value.
    
    Review URL: http://codereview.chromium.org/8555010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/l10n_string_util.cc
+++ chrome/installer/util/l10n_string_util.cc
@@ -79,4 +79,8 @@ std::wstring GetLocalizedEulaResource() {
   return url_path;
 }
 
+std::wstring GetCurrentTranslation() {
+  return GetLanguageSelector().selected_translation();
+}
+
 }  // namespace installer
