3216be2c61d9271aeab85e55a0674124090ae51c
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-15 02:06:08 +0000

    Fix V8 build again.
    git-svn-id: svn://svn.chromium.org/blink/trunk@100229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -2706,7 +2706,7 @@ END
     }
     namespace WTF {
     void ${nativeType}::neuterBinding(WebCore::ScriptExecutionContext*) {
-        v8::Handle<v8::Value> bound = toV8(this);
+        v8::Handle<v8::Value> bound = WebCore::toV8(this);
         v8::Handle<v8::Object> object(bound.As<v8::Object>());
         object->SetIndexedPropertiesToExternalArrayData(0, v8::kExternalByteArray, 0);
     }
