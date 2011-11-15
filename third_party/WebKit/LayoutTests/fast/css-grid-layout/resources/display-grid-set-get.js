645a5591589ea5f7d6b51ec222262308b6271255
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-15 23:05:28 +0000

    Add the needed plumbing to parse display: -webkit-grid https://bugs.webkit.org/show_bug.cgi?id=72331
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Test: fast/css-grid-layout/display-grid-set-get.html
    
    Added parsing support for display: -webkit-grid. From a rendering perspective,
    the value is equivalent to display: none until we properly implement it.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseValue):
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
    * css/CSSValueKeywords.in:
    * rendering/style/RenderStyleConstants.h:
    Added the new CSS value and plumbed the parsing and style application of
    the new value.
    
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::operator EDisplay):
    Added an ASSERT here as I bumped into some non-trivial issues due to bug 72296.
    -wap-marquee was offsetting the new value and was wrongly casted by the CSSPrimitiveValueMapping
    logic outside the EDisplay range which would lead to crashes.
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::createObject):
    Fixed the indentation to follow our coding rules.
    
    LayoutTests:
    
    * fast/css-grid-layout/display-grid-set-get-expected.txt: Added.
    * fast/css-grid-layout/display-grid-set-get.html: Added.
    * fast/css-grid-layout/resources/display-grid-set-get.js: Added.
    Test that setting the display to -webkit-grid (from JS or CSS) works
    and we get the right value back.
    
    * platform/chromium/test_expectations.txt:
    * platform/efl/Skipped:
    * platform/gtk/Skipped:
    * platform/mac/Skipped:
    * platform/qt/Skipped:
    * platform/win/Skipped:
    * platform/wincairo/Skipped:
    SKIP the css-grid-layout tests on all platforms for now.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100338 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/css-grid-layout/resources/display-grid-set-get.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test that setting and getting display: grid works as expected');
+
+debug("Test getting |display| set through CSS");
+var gridElement = document.getElementById("gridElement");
+shouldBe("getComputedStyle(gridElement, '').getPropertyValue('display')", "'-webkit-grid'");
+
+debug("");
+debug("Test getting and setting display through JS");
+var element = document.createElement("div");
+document.body.appendChild(element);
+shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'block'");
+element.style.display = "-webkit-grid";
+shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'-webkit-grid'");
