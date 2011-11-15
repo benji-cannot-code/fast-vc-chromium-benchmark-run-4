787b9464954705d33b50de980cc4abf3b577f5b8
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 19:18:13 +0000

    Fix DoubleClickInterval and DragThreshold on aura.
    BUG=101774
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/8564015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110140 0039d316-1c4b-4281-b951-d872f2087c98

--- views/metrics_aura.cc
+++ views/metrics_aura.cc
@@ -9,13 +9,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
+namespace {
+
+// Default double click interval in milliseconds.
+// Same as what gtk uses.
+const int kDefaultDoubleClickInterval = 500;
+
+}  // namespace
+
 namespace views {
 
 int GetDoubleClickInterval() {
 #if defined(OS_WIN)
   return ::GetDoubleClickTime();
 #else
-  return 5;
+  // TODO(jennyz): This value may need to be adjusted on different platforms.
+  return kDefaultDoubleClickInterval;
 #endif
 }
 
--- views/view_aura.cc
+++ views/view_aura.cc
@@ -5,6 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "views/view.h"
 
+namespace {
+
+// Default horizontal drag threshold in pixels.
+// Same as what gtk uses.
+const int kDefaultHorizontalDragThreshold = 8;
+
+// Default vertical drag threshold in pixels.
+// Same as what gtk uses.
+const int kDefaultVerticalDragThreshold = 8;
+
+}  // namespace
+
 namespace views {
 
 gfx::NativeViewAccessible View::GetNativeViewAccessible() {
@@ -12,11 +24,15 @@ gfx::NativeViewAccessible View::GetNativeViewAccessible() {
 }
 
 int View::GetHorizontalDragThreshold() {
-  return 0;
+  // TODO(jennyz): This value may need to be adjusted for different platforms
+  // and for different display density.
+  return kDefaultHorizontalDragThreshold;
 }
 
 int View::GetVerticalDragThreshold() {
-  return 0;
+  // TODO(jennyz): This value may need to be adjusted for different platforms
+  // and for different display density.
+  return kDefaultVerticalDragThreshold;
 }
 
 }  // namespace views
