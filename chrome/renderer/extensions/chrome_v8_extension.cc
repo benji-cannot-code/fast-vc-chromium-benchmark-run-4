efbb67b928e23c60f408999b48fe7ad1ad929d39
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 22:50:46 +0000

    Stop copying extension bindings scripts.
    After improving v8 API to allow us to pass in an extension source string
    length field, use that to refer directly to the in-image source rather
    than copying it and holding it in memory, thus saving memory.
    
    It bums me out that I have to call GetRawDataResource() twice, but I don't s
    a way around that without abandoning inheritance from v8::Extension and
    switching to composition/delegation.
    
    Moved DEPS for v8 to r9534, the newest to include r9365. I don't know whethe
    this matches our convention; perhaps we always move DEPS to the latest.
    
    BUG=95147
    TEST=relying on existing extensions unit tests, which all still pass.
    
    
    Review URL: http://codereview.chromium.org/8277020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110197 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/chrome_v8_extension.cc
+++ chrome/renderer/extensions/chrome_v8_extension.cc
@@ -32,9 +32,6 @@ const char kChromeHidden[] = "chromeHidden";
 const char kValidateCallbacks[] = "validateCallbacks";
 #endif
 
-typedef std::map<int, std::string> StringMap;
-static base::LazyInstance<StringMap> g_string_map = LAZY_INSTANCE_INITIALIZER;
-
 static base::LazyInstance<ChromeV8Extension::InstanceSet> g_instances =
     LAZY_INSTANCE_INITIALIZER;
 
@@ -42,16 +39,8 @@ static base::LazyInstance<ChromeV8Extension::InstanceSet> g_instances =
 
 
 // static
-const char* ChromeV8Extension::GetStringResource(int resource_id) {
-  StringMap* strings = g_string_map.Pointer();
-  StringMap::iterator it = strings->find(resource_id);
-  if (it == strings->end()) {
-    it = strings->insert(std::make_pair(
-        resource_id,
-        ResourceBundle::GetSharedInstance().GetRawDataResource(
-            resource_id).as_string())).first;
-  }
-  return it->second.c_str();
+base::StringPiece ChromeV8Extension::GetStringResource(int resource_id) {
+  return ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
 }
 
 // static
@@ -73,9 +62,10 @@ content::RenderView* ChromeV8Extension::GetCurrentRenderView() {
 ChromeV8Extension::ChromeV8Extension(const char* name, int resource_id,
                                      ExtensionDispatcher* extension_dispatcher)
     : v8::Extension(name,
-                    GetStringResource(resource_id),
-                    0,  // num dependencies
-                    NULL),  // dependencies array
+                    GetStringResource(resource_id).data(),
+                    0,     // num dependencies
+                    NULL,  // dependencies array
+                    GetStringResource(resource_id).size()),  // source length
       extension_dispatcher_(extension_dispatcher) {
   g_instances.Get().insert(this);
 }
@@ -85,9 +75,10 @@ ChromeV8Extension::ChromeV8Extension(const char* name, int resource_id,
                                      const char** dependencies,
                                      ExtensionDispatcher* extension_dispatcher)
     : v8::Extension(name,
-                    GetStringResource(resource_id),
+                    GetStringResource(resource_id).data(),
                     dependency_count,
-                    dependencies),
+                    dependencies,
+                    GetStringResource(resource_id).size()),
       extension_dispatcher_(extension_dispatcher) {
   g_instances.Get().insert(this);
 }
