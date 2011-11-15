9f4d25a11e3af0cfb9ff394f9e778ae94351c7cd
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-15 13:38:23 +0000

    Web Inspector: move generic code from DevTools.js into the WebCore. https://bugs.webkit.org/show_bug.cgi?id=72377
    This includes support for themed toolbar, remote debugging routines,
    removes a couple of obsolete overrides from the DevTools.js
    
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/InspectorFrontendHostStub.js:
    (.WebInspector.InspectorFrontendHostStub.prototype.inspectedURLChanged):
    * inspector/front-end/ProfilesPanel.js:
    * inspector/front-end/Resource.js:
    * inspector/front-end/Settings.js:
    * inspector/front-end/UIUtils.js:
    (WebInspector.setToolbarColors):
    (WebInspector.resetToolbarColors):
    * inspector/front-end/inspector.js:
    
    Source/WebKit/chromium:
    
    * src/js/DevTools.js:
    (WebInspector.setInspectedTabId):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/inspector.js
@@ -320,6 +320,9 @@ WebInspector.doLoadedDone = function()
     if (WebInspector.socket)
         document.body.addStyleClass("remote");
 
+    if (WebInspector.queryParamsObject.toolbarColor && WebInspector.queryParamsObject.textColor)
+        WebInspector.setToolbarColors(WebInspector.queryParamsObject.toolbarColor, WebInspector.queryParamsObject.textColor);
+
     WebInspector.shortcutsScreen = new WebInspector.ShortcutsScreen();
     this._registerShortcuts();
 
