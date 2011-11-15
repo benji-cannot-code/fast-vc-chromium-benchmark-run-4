5dd040c346d0df4287a3b8fd61879643f67fd4a0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-15 01:23:35 +0000

    [MutationObservers] Add histogram collection for usage of DOM Mutation Events https://bugs.webkit.org/show_bug.cgi?id=72316
    Patch by Rafael Weinstein <rafaelw@chromium.org> on 2011-11-14
    Reviewed by Ryosuke Niwa.
    
    This patch adds six calls in ~Document() which simply pipe-out to the embedder
    the (already-collected) bits of whether varous DOM Mutation Events were registered
    on the document.
    
    No tests needed. No functional changes.
    
    * dom/Document.cpp:
    (WebCore::histogramMutationEventUsage):
    (WebCore::Document::~Document):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -158,6 +158,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/StdLibExtras.h>
 #include <wtf/text/StringBuffer.h>
 
+#if PLATFORM(CHROMIUM)
+#include "PlatformSupport.h"
+#endif
+
 #if ENABLE(SHARED_WORKERS)
 #include "SharedWorkerRepository.h"
 #endif
@@ -494,6 +498,18 @@ Document::Document(Frame* frame, const KURL& url, bool isXHTML, bool isHTML)
     m_docID = docID++;
 }
 
+#if PLATFORM(CHROMIUM)
+static void histogramMutationEventUsage(const unsigned short& listenerTypes)
+{
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMSubtreeModified", static_cast<bool>(listenerTypes & Document::DOMSUBTREEMODIFIED_LISTENER), 2);
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMNodeInserted", static_cast<bool>(listenerTypes & Document::DOMNODEINSERTED_LISTENER), 2);
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMNodeRemoved", static_cast<bool>(listenerTypes & Document::DOMNODEREMOVED_LISTENER), 2);
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMNodeRemovedFromDocument", static_cast<bool>(listenerTypes & Document::DOMNODEREMOVEDFROMDOCUMENT_LISTENER), 2);
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMNodeInsertedIntoDocument", static_cast<bool>(listenerTypes & Document::DOMNODEINSERTEDINTODOCUMENT_LISTENER), 2);
+    PlatformSupport::histogramEnumeration("DOMAPI.PerDocumentMutationEventUsage.DOMCharacterDataModified", static_cast<bool>(listenerTypes & Document::DOMCHARACTERDATAMODIFIED_LISTENER), 2);
+}
+#endif
+
 Document::~Document()
 {
     ASSERT(!renderer());
@@ -505,6 +521,10 @@ Document::~Document()
     ASSERT(!m_guardRefCount);
 
     m_scriptRunner.clear();
+
+#if PLATFORM(CHROMIUM)
+    histogramMutationEventUsage(m_listenerTypes);
+#endif
 
     removeAllEventListeners();
 
