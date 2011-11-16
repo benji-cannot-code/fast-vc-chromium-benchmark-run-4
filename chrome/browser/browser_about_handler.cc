6ea8caa0d20f8255272d3c983c8f702c99b77509
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 04:31:06 +0000

    Remove about:tracking. It has been subsumed by about:profiler.
    I also added a button to about:profiler that matches the behavior of "about:tracking/reset" (nukes the tracked objects). That way there shouldn't be any functionality loss in deleting about:tracking.
    
    BUG=100992
    
    Review URL: http://codereview.chromium.org/8568036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110252 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/threading/thread.h"
-#include "base/tracked_objects.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/about_flags.h"
@@ -147,7 +146,6 @@ const char* const kChromePaths[] = {
   chrome::kChromeUITCMallocHost,
   chrome::kChromeUITermsHost,
   chrome::kChromeUITracingHost,
-  chrome::kChromeUITrackingHost,
   chrome::kChromeUIVersionHost,
   chrome::kChromeUIWorkersHost,
 #if defined(OS_WIN)
@@ -184,7 +182,6 @@ const char* const kAboutSourceNames[] = {
   chrome::kChromeUIStatsHost,
   chrome::kChromeUITaskManagerHost,
   chrome::kChromeUITermsHost,
-  chrome::kChromeUITrackingHost,
   chrome::kChromeUIVersionHost,
 #if defined(USE_TCMALLOC)
   chrome::kChromeUITCMallocHost,
@@ -888,21 +885,6 @@ void AboutMemory(const std::string& path, AboutSource* source, int request_id) {
   }
 }
 
-static std::string AboutTracking(const std::string& query) {
-  std::string unescaped_title("About Tracking");
-  if (!query.empty()) {
-    unescaped_title += " - ";
-    unescaped_title += net::UnescapeURLComponent(query,
-                                                 net::UnescapeRule::NORMAL);
-  }
-  std::string data;
-  AppendHeader(&data, 0, unescaped_title);
-  AppendBody(&data);
-  tracked_objects::ThreadData::WriteHTML(query, &data);
-  AppendFooter(&data);
-  return data;
-}
-
 // Handler for filling in the "about:stats" page, as called by the browser's
 // About handler processing.
 // |query| is roughly the query string of the about:stats URL.
@@ -1472,8 +1454,6 @@ void AboutSource::StartDataRequest(const std::string& path,
 #endif
   } else if (host == chrome::kChromeUIStatsHost) {
     response = AboutStats(path);
-  } else if (host == chrome::kChromeUITrackingHost) {
-    response = AboutTracking(path);
 #if defined(USE_TCMALLOC)
   } else if (host == chrome::kChromeUITCMallocHost) {
     response = AboutTcmalloc();
