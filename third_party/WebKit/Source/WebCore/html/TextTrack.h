25bf3e0ee73f9566d51b7754e8bfa10696230694
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-16 16:05:45 +0000

    addTrack() must throw an exception if 'kind' is unknown https://bugs.webkit.org/show_bug.cgi?id=71915
    Reviewed by Philippe Normand.
    
    Source/WebCore: 
    
    Tests: media/track/track-addtrack-kind.html
           media/track/track-kind.html
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::textTrackKindChanged): New. Will be implemented for 62885.
    (WebCore::HTMLMediaElement::addTrack): Throw if  'kind' is not a known value.
    (WebCore::HTMLMediaElement::addTextTrack): Call textTracks(), it will allocate the track
        list object if necessary.
    (WebCore::HTMLMediaElement::textTracks): Never return NULL, a TextTrackList with no tracks
        is allowed.
    * html/HTMLMediaElement.h:
    (WebCore::HTMLMediaElement::addTrack): Add variants to deal with optional parameters plus
        a mandatory ExceptionCode parameter.
    * html/HTMLMediaElement.idl: addTrack can generate an exception.
    
    * html/HTMLTrackElement.cpp:
    (WebCore::HTMLTrackElement::attributeChanged): kind, label, and srclang attribute changes should
        percolate down the the TextTrack.
    (WebCore::HTMLTrackElement::kind): Return the TextTrack kind because it is not necessarily the
        same as the attribute value.
    (WebCore::HTMLTrackElement::ensureTrack): Only pass legal 'kind' keywords to create a TextTrack.
    (WebCore::HTMLTrackElement::textTrackKindChanged): Notify parent element, if any.
    * html/HTMLTrackElement.h:
    
    * html/TextTrack.cpp:
    (WebCore::TextTrack::subtitlesKeyword): New, return legal kind attribute value.
    (WebCore::TextTrack::captionsKeyword): Ditto.
    (WebCore::TextTrack::descriptionsKeyword): Ditto.
    (WebCore::TextTrack::chaptersKeyword): Ditto.
    (WebCore::TextTrack::metadataKeyword): Ditto.
    (WebCore::TextTrack::TextTrack): Call setKind to make sure m_kind is always set to legal value.
    (WebCore::TextTrack::isValidKindKeyword): New, validate 'kind' value.
    (WebCore::TextTrack::setKind): Only allow legal values.
    * html/TextTrack.h:
    (WebCore::TextTrack::kind):
    (WebCore::TextTrack::label):
    (WebCore::TextTrack::setLabel):
    (WebCore::TextTrack::language):
    (WebCore::TextTrack::setLanguage):
    (WebCore::TextTrack::readyState):
    (WebCore::TextTrack::mode):
    
    LayoutTests: 
    
    * media/track/track-addtrack-kind-expected.txt: Added.
    * media/track/track-addtrack-kind.html: Added.
    * media/track/track-kind-expected.txt: Added.
    * media/track/track-kind.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/TextTrack.h
+++ third_party/WebKit/Source/WebCore/html/TextTrack.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(VIDEO_TRACK)
 
+#include "ExceptionCode.h"
 #include "TrackBase.h"
 #include <wtf/PassOwnPtr.h>
 #include <wtf/RefCounted.h>
@@ -40,12 +41,11 @@ class TextTrack;
 class TextTrackCue;
 class TextTrackCueList;
 
-typedef int ExceptionCode;
-
 class TextTrackClient {
 public:
     virtual ~TextTrackClient() { }
     virtual void textTrackReadyStateChanged(TextTrack*) = 0;
+    virtual void textTrackKindChanged(TextTrack*) = 0;
     virtual void textTrackModeChanged(TextTrack*) = 0;
     virtual void textTrackAddCues(TextTrack*, const TextTrackCueList*) = 0;
     virtual void textTrackRemoveCues(TextTrack*, const TextTrackCueList*) = 0;
@@ -61,15 +61,27 @@ public:
     }
     virtual ~TextTrack();
 
-    String kind() const;
-    String label() const;
-    String language() const;
+    String kind() const { return m_kind; }
+    void setKind(const String&);
+
+    static const AtomicString& subtitlesKeyword();
+    static const AtomicString& captionsKeyword();
+    static const AtomicString& descriptionsKeyword();
+    static const AtomicString& chaptersKeyword();
+    static const AtomicString& metadataKeyword();
+    static bool isValidKindKeyword(const String&);
+
+    String label() const { return m_label; }
+    void setLabel(const String& label) { m_label = label; }
+
+    String language() const { return m_language; }
+    void setLanguage(const String& language) { m_language = language; }
 
     enum ReadyState { NONE = 0, LOADING = 1, LOADED = 2, HTML_ERROR = 3 };
-    ReadyState readyState() const;
+    ReadyState readyState() const { return m_readyState; }
 
     enum Mode { DISABLED = 0, HIDDEN = 1, SHOWING = 2 };
-    Mode mode() const;
+    Mode mode() const { return m_mode; }
     void setMode(unsigned short, ExceptionCode&);
 
     TextTrackCueList* cues();
