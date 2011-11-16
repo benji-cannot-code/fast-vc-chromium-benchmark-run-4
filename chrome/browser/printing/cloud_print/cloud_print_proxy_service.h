72edf686aea374467cb930bc2f8b5e79b134a71d
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 18:40:55 +0000

    Cloud print connector policy.
    This implements the policy inside the browser process, shutting down the cloud
    print connector if the policy is set to disabled. This isn't a complete
    solution, as the browser needs to be running or be launched for the policy to
    take effect. (It will take a lot more refactoring for the service process to
    use the policy code). The hole without the refactoring is that if you enable
    the connector, quit Chromium, and set the policy, the connector will stay alive
    until the next launch of Chromium.
    
    The browser process checks the policy on startup, and listens for it changing
    as long as it is up. You can sit on the Under the Hood page and watch the
    button change state on Windows as you fiddle with the policy.
    
    BUG=59769
    TEST=Set Cloud Print Proxy policy to disabled, bring up browser, Options/Under
    the Hood - Sign in to Google Cloud print will be disabled.  Unset policy, wait,
    button becomes active.  Log in to cloud print.  Quit Chromium, note service
    process hangs around for more than a minute.  Set policy, launch and quit
    Chromium. Note that the service process quits within a minute.
    
    
    Review URL: http://codereview.chromium.org/8438020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/cloud_print/cloud_print_proxy_service.h
+++ chrome/browser/printing/cloud_print/cloud_print_proxy_service.h
@@ -13,10 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/task.h"
+#include "chrome/browser/prefs/pref_change_registrar.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_setup_handler.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
+#include "content/public/browser/notification_observer.h"
 
 class Profile;
+class ServiceProcessControl;
 
 namespace cloud_print {
 struct CloudPrintProxyInfo;
@@ -26,7 +29,8 @@ struct CloudPrintProxyInfo;
 // running in the service process.
 class CloudPrintProxyService
     : public CloudPrintSetupHandlerDelegate,
-      public ProfileKeyedService {
+      public ProfileKeyedService,
+      public content::NotificationObserver {
  public:
   explicit CloudPrintProxyService(Profile* profile);
   virtual ~CloudPrintProxyService();
@@ -51,6 +55,11 @@ class CloudPrintProxyService
 
   // CloudPrintSetupHandler::Delegate implementation.
   virtual void OnCloudPrintSetupClosed();
+
+  // content::NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
  private:
   // NotificationDelegate implementation for the token expired notification.
@@ -83,8 +92,15 @@ class CloudPrintProxyService
   void OnTokenExpiredNotificationClosed(bool by_user);
   void OnTokenExpiredNotificationClick();
   void TokenExpiredNotificationDone(bool keep_alive);
+  void ApplyCloudPrintConnectorPolicy();
+
+  // Virtual for testing.
+  virtual ServiceProcessControl* GetServiceProcessControl();
 
   base::WeakPtrFactory<CloudPrintProxyService> weak_factory_;
+
+  // For watching for connector enablement policy changes.
+  PrefChangeRegistrar pref_change_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(CloudPrintProxyService);
 };
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1530,6 +1530,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/prerender/prerender_manager_unittest.cc',
         'browser/prerender/prerender_tracker_unittest.cc',
         'browser/prerender/prerender_util_unittest.cc',
+        'browser/printing/cloud_print/cloud_print_proxy_service_unittest.cc',
         'browser/printing/cloud_print/cloud_print_setup_source_unittest.cc',
         'browser/printing/print_dialog_cloud_unittest.cc',
         'browser/printing/print_job_unittest.cc',
