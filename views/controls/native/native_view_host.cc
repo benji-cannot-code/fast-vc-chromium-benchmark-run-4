c8e89cc5517bb0be8f4a16758e31a89b66a20624
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 17:46:53 +0000

    Fixes regression where showing bookmark bar (or download shelf) wouldn't do a final layout resulting in the content not laying out correctly.
    BUG=104082
    TEST=none
    R=ben@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8573029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110320 0039d316-1c4b-4281-b951-d872f2087c98

--- views/controls/native/native_view_host.cc
+++ views/controls/native/native_view_host.cc
@@ -32,6 +32,7 @@ NativeViewHost::NativeViewHost()
     : native_view_(NULL),
       views_view_(NULL),
       fast_resize_(false),
+      fast_resize_at_last_layout_(false),
       focus_view_(NULL) {
 }
 
@@ -121,6 +122,7 @@ void NativeViewHost::Layout() {
   } else {
     native_wrapper_->HideWidget();
   }
+  fast_resize_at_last_layout_ = visible && fast_resize_;
 }
 
 void NativeViewHost::OnPaint(gfx::Canvas* canvas) {
