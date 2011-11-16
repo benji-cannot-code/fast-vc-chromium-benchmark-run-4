7310a3a698438969f3ca8b3da03710997fdc562a
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-16 02:49:07 +0000

    Add the needed plumbing to parse display: -webkit-inline-grid https://bugs.webkit.org/show_bug.cgi?id=72438
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Test: fast/css-grid-layout/display-grid-set-get.html
    
    Added the needed constants and plugged everything together.
    Again we treat display: -webkit-inline-grid like display: none
    for the moment.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseValue):
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
    * css/CSSValueKeywords.in:
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::createObject):
    * rendering/style/RenderStyleConstants.h:
    
    LayoutTests:
    
    Integrated testing for display: -webkit-inline-grid into the existing test.
    
    * fast/css-grid-layout/display-grid-set-get-expected.txt:
    * fast/css-grid-layout/display-grid-set-get.html:
    * fast/css-grid-layout/resources/display-grid-set-get.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/css-grid-layout/resources/display-grid-set-get.js
+++ third_party/WebKit/LayoutTests/fast/css-grid-layout/resources/display-grid-set-get.js
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description('Test that setting and getting display: grid works as expected');
+description('Test that setting and getting display: grid and inline-grid works as expected');
 
 debug("Test getting |display| set through CSS");
 var gridElement = document.getElementById("gridElement");
+var inlineGridElement = document.getElementById("inlineGridElement");
 shouldBe("getComputedStyle(gridElement, '').getPropertyValue('display')", "'-webkit-grid'");
+shouldBe("getComputedStyle(inlineGridElement, '').getPropertyValue('display')", "'-webkit-inline-grid'");
 
 debug("");
 debug("Test getting and setting display through JS");
@@ -12,3 +14,9 @@ document.body.appendChild(element);
 shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'block'");
 element.style.display = "-webkit-grid";
 shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'-webkit-grid'");
+
+element = document.createElement("div");
+document.body.appendChild(element);
+shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'block'");
+element.style.display = "-webkit-inline-grid";
+shouldBe("getComputedStyle(element, '').getPropertyValue('display')", "'-webkit-inline-grid'");
--- third_party/WebKit/Source/WebCore/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/Source/WebCore/css/CSSPrimitiveValueMappings.h
@@ -1071,6 +1071,9 @@ template<> inline CSSPrimitiveValue::CSSPrimitiveValue(EDisplay e)
         case GRID:
             m_value.ident = CSSValueWebkitGrid;
             break;
+        case INLINE_GRID:
+            m_value.ident = CSSValueWebkitInlineGrid;
+            break;
 #endif
         case NONE:
             m_value.ident = CSSValueNone;
