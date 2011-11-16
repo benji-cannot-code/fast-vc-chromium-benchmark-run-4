ff6e965f44177817b48a493247cde6a7be6bb6ba
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-16 12:56:21 +0000

    Web Inspector: [Extensions API] drop ExtensionSidebarPane.onUpdated, use callbacks instead https://bugs.webkit.org/show_bug.cgi?id=72388
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Tests: inspector/extensions/extensions-panel.html
           inspector/extensions/extensions-sidebar.html
    
    - fire ExtensionSidebarPane.on{Hidden,Shown} for non-iframe content (experssions/objects);
    - drop ExtensionsSidebarPane.onUpdated, provide callback for setObject()/setExpression() instead;
    - fix an exception when a page is replaced with object/expression.
    
     inspector/front-end/ExtensionAPI.js:
    (injectedExtensionAPI.ExtensionSidebarPaneImpl):
    (injectedExtensionAPI.ExtensionSidebarPaneImpl.prototype.setExpression):
    (injectedExtensionAPI.ExtensionSidebarPaneImpl.prototype.setObject):
    * inspector/front-end/ExtensionPanel.js:
    (WebInspector.ExtensionNotifierView):
    (WebInspector.ExtensionNotifierView.prototype.wasShown):
    (WebInspector.ExtensionNotifierView.prototype.willHide):
    (WebInspector.ExtensionSidebarPane.prototype.setObject):
    (WebInspector.ExtensionSidebarPane.prototype.setExpression):
    (WebInspector.ExtensionSidebarPane.prototype.setPage):
    (WebInspector.ExtensionSidebarPane.prototype._onEvaluate):
    (WebInspector.ExtensionSidebarPane.prototype._makeObjectPropertiesView):
    (WebInspector.ExtensionSidebarPane.prototype._setObject):
    * inspector/front-end/ExtensionServer.js:
    (WebInspector.ExtensionServer.prototype._onSetSidebarContent.callback):
    (WebInspector.ExtensionServer.prototype._onSetSidebarContent):
    (WebInspector.ExtensionServer.prototype._dispatchCallback):
    * inspector/front-end/View.js:
    (WebInspector.View.prototype.detach):
    
    LayoutTests:
    
    - split LayoutTests/inspector/extensions.html into extensions-panel.html & extensions-sidebar.html.
    
    * inspector/extensions/extensions-panel-expected.txt: Added.
    * inspector/extensions/extensions-panel.html: Added.
    * inspector/extensions/extensions-sidebar-expected.txt: Renamed from LayoutTests/inspector/extensions/extensions-expected.txt.
    * inspector/extensions/extensions-sidebar.html: Renamed from LayoutTests/inspector/extensions/extensions.html.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/View.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/View.js
@@ -168,7 +168,10 @@ WebInspector.View.prototype = {
             this._processWasShown();
     },
 
-    detach: function()
+    /**
+     * @param {boolean=} overrideHideOnDetach
+     */
+    detach: function(overrideHideOnDetach)
     {
         var parentElement = this.element.parentElement;
         if (!parentElement)
@@ -177,7 +180,7 @@ WebInspector.View.prototype = {
         if (this._parentIsShowing())
             this._processWillHide();
 
-        if (this._hideOnDetach) {
+        if (this._hideOnDetach && !overrideHideOnDetach) {
             this.element.removeStyleClass("visible");
             this._visible = false;
             if (this._parentIsShowing())
