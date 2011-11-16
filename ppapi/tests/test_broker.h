38f8211d461cabfe9f13da3711ee5f04d63d294f
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 01:58:32 +0000

    Add TestConnectFailure, TestGetHandleFailure and TestConnectAndPipe to PPAPI Broker UI test.
    Also fix wrong sandbox options for broker process on Windows and Mac.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8400024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110241 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_broker.h
+++ ppapi/tests/test_broker.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,6 +23,9 @@ class TestBroker : public TestCase {
 
  private:
   std::string TestCreate();
+  std::string TestConnectFailure();
+  std::string TestGetHandleFailure();
+  std::string TestConnectAndPipe();
 
   const struct PPB_BrokerTrusted* broker_interface_;
 };
