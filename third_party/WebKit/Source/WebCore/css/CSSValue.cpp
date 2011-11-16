4a984272fff0fda321d71c52b4105dd73eb8fd1e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-16 15:49:12 +0000

    CSSValue: Clean up initial value construction. <http://webkit.org/b/72502>
    Patch by Andreas Kling <kling@webkit.org> on 2011-11-16
    Reviewed by Antti Koivisto.
    
    Instead of determining whether a given CSSInitialValue is 'implicit' or not by
    querying the CSSValue::ClassType (InitialClass vs. ImplicitInitialClass),
    add a protected CSSValue member and set it from the CSSInitialValue constructor.
    
    Also get rid of the CSSValue::m_isInitial bit since we can now replace the
    checks by classType() == InitialClass.
    
    No new tests, this is a cleanup.
    
    * css/CSSInitialValue.h:
    (WebCore::CSSInitialValue::CSSInitialValue):
    
        Poke 'implicit' constructor argument into CSSValue::m_isImplicit.
    
    * css/CSSValue.cpp:
    (WebCore::CSSValue::cssText):
    (WebCore::CSSValue::destroy):
    
        Remove ImplicitInitialClass cases.
    
    * css/CSSValue.h:
    (WebCore::CSSValue::isImplicitInitialValue):
    (WebCore::CSSValue::isInitialValue):
    (WebCore::CSSValue::CSSValue):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSValue.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSValue.cpp
@@ -121,7 +121,6 @@ String CSSValue::cssText() const
     case InheritedClass:
         return static_cast<const CSSInheritedValue*>(this)->customCssText();
     case InitialClass:
-    case ImplicitInitialClass:
         return static_cast<const CSSInitialValue*>(this)->customCssText();
     case PrimitiveClass:
         return static_cast<const CSSPrimitiveValue*>(this)->customCssText();
@@ -213,7 +212,6 @@ void CSSValue::destroy()
         delete static_cast<CSSInheritedValue*>(this);
         return;
     case InitialClass:
-    case ImplicitInitialClass:
         delete static_cast<CSSInitialValue*>(this);
         return;
     case PrimitiveClass:
