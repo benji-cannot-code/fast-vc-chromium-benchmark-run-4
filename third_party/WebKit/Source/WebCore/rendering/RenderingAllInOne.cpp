d5c4fb65c9f2e15c4b936890083e2614dfff7229
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-16 20:25:19 +0000

    Unreviewed, rolling out r100479. http://trac.webkit.org/changeset/100479 https://bugs.webkit.org/show_bug.cgi?id=72533
    "Broke the Mac Build" (Requested by mwenge2 on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-11-16
    
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * rendering/RenderingAllInOne.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderingAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderingAllInOne.cpp
@@ -93,7 +93,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderSlider.cpp"
 #include "RenderSummary.cpp"
 #include "RenderTable.cpp"
-#include "RenderTableCaption.cpp"
 #include "RenderTableCell.cpp"
 #include "RenderTableCol.cpp"
 #include "RenderTableRow.cpp"
