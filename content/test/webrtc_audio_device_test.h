c8f33e7e5339b28a99c135c8041b26dace5a1008
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 11:57:47 +0000

    Adds more unit tests for WebRTC. These tests verifies that rendering and capturing starts as they should. It is also possible to enable a full-duplex audio test.
    BUG=none
    TEST=content_unittests with --gtest_filter=*WebRTC*
    Review URL: http://codereview.chromium.org/8528026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110289 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/webrtc_audio_device_test.h
+++ content/test/webrtc_audio_device_test.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include <string>
-
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
