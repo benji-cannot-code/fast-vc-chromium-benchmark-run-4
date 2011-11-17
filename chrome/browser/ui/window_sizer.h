cf47f55060978be745b604e3ac645a5456a3ca7d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 23:18:13 +0000

    Use scoped_ptr for memory management rather than calling delete through WindowSizer.
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8585035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110593 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/window_sizer.h
+++ chrome/browser/ui/window_sizer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 #include "ui/gfx/rect.h"
 
 class Browser;
@@ -158,8 +159,8 @@ class WindowSizer {
       gfx::Rect* bounds) const;
 
   // Providers for persistent storage and monitor metrics.
-  StateProvider* state_provider_;
-  MonitorInfoProvider* monitor_info_provider_;
+  scoped_ptr<StateProvider> state_provider_;
+  scoped_ptr<MonitorInfoProvider> monitor_info_provider_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowSizer);
 };
