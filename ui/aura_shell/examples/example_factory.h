f9f163ed88658f51d908b302f1a866209439b6f7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 18:20:33 +0000

    aura_shell: Remove unused native_widget_types.h include from example_factory.h
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/8587013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110525 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/examples/example_factory.h
+++ ui/aura_shell/examples/example_factory.h
@@ -3,11 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_AURA_SHELL_SHELL_FACTORY_H_
-#define UI_AURA_SHELL_SHELL_FACTORY_H_
+#ifndef UI_AURA_SHELL_EXAMPLES_EXAMPLE_FACTORY_H_
+#define UI_AURA_SHELL_EXAMPLES_EXAMPLE_FACTORY_H_
 #pragma once
-
-#include "ui/gfx/native_widget_types.h"
 
 namespace views {
 class View;
@@ -18,7 +16,7 @@ namespace examples {
 
 void CreatePointyBubble(views::View* anchor_view);
 
-void CreateLock();
+void CreateLockScreen();
 
 // Creates a window showing samples of commonly used widgets.
 void CreateWidgetsWindow();
@@ -26,4 +24,4 @@ void CreateWidgetsWindow();
 }  // namespace examples
 }  // namespace aura_shell
 
-#endif  // UI_AURA_SHELL_SHELL_FACTORY_H_
+#endif  // UI_AURA_SHELL_EXAMPLES_EXAMPLE_FACTORY_H_
--- ui/aura_shell/examples/lock_view.cc
+++ ui/aura_shell/examples/lock_view.cc
@@ -49,7 +49,7 @@ class LockView : public views::WidgetDelegateView {
   DISALLOW_COPY_AND_ASSIGN(LockView);
 };
 
-void CreateLock() {
+void CreateLockScreen() {
   LockView* lock_view = new LockView;
   views::Widget* widget = new views::Widget;
   views::Widget::InitParams params(views::Widget::InitParams::TYPE_CONTROL);
