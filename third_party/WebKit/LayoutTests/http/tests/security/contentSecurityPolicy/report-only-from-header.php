6537ca8aedead513a6bb3ce6c92f08ba15efcc3b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-17 08:25:40 +0000

    CSP report-only mode doesn't work from an HTTP header https://bugs.webkit.org/show_bug.cgi?id=71958
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    "It's tested or it's broken." -- Adam Leventhal
    
    Test: http/tests/security/contentSecurityPolicy/report-only-from-header.php
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::didBeginDocument):
    
    LayoutTests:
    
    Test that X-WebKit-CSP-Report-Only actually sends reports.
    
    * http/tests/security/contentSecurityPolicy/report-only-from-header-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/report-only-from-header.php: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-only-from-header.php
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("X-WebKit-CSP-Report-Only: script-src 'self'; report-uri resources/save-report.php");
+?>
+<script>
+// This script block will trigger a violation report but shouldn't be blocked.
+alert('PASS');
+</script>
+<script src="resources/go-to-echo-report.js"></script>
