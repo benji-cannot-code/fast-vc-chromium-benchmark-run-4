ce791b8ca9b6d113fe5ce6f7cfde34e1eb2e188b
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-17 08:16:44 +0000

    Web Inspector: introduce Debugger domain capabilities concept. https://bugs.webkit.org/show_bug.cgi?id=72393
    Many of the Preferences that we have in Settings.js and override in DevTools.js
    are really not preferences, but capabilities. Protocol clients should have a way
    of figuring out whether some capability is present before using it.
    
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * bindings/js/ScriptDebugServer.cpp:
    (WebCore::ScriptDebugServer::canSetScriptSource):
    * bindings/js/ScriptDebugServer.h:
    * bindings/v8/ScriptDebugServer.cpp:
    (WebCore::ScriptDebugServer::canSetScriptSource):
    * bindings/v8/ScriptDebugServer.h:
    * inspector/CodeGeneratorInspector.py:
    * inspector/Inspector.json:
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::getCapabilities):
    * inspector/InspectorDebuggerAgent.h:
    * inspector/front-end/DebuggerModel.js:
    (WebInspector.DebuggerModel):
    (WebInspector.DebuggerModel.prototype.enableDebugger):
    (WebInspector.DebuggerModel.prototype.canSetScriptSource):
    * inspector/front-end/DebuggerPresentationModel.js:
    (WebInspector.DebuggerPresentationModel.prototype.canEditScriptSource):
    * inspector/front-end/Settings.js:
    
    Source/WebKit/chromium:
    
    * src/js/DevTools.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptDebugServer.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptDebugServer.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/HashMap.h>
 #include <wtf/Noncopyable.h>
 #include <wtf/PassOwnPtr.h>
+#include <wtf/Vector.h>
 #include <wtf/text/StringHash.h>
 
 namespace WebCore {
@@ -75,6 +76,7 @@ public:
     void stepOverStatement();
     void stepOutOfFunction();
 
+    bool canSetScriptSource();
     bool setScriptSource(const String& sourceID, const String& newContent, bool preview, String* error, ScriptValue* newCallFrames, ScriptObject* result);
 
     void recompileAllJSFunctionsSoon() { }
