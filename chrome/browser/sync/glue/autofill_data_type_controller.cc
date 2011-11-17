e10822ff9eebea58915242566101418821f7931c
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 22:53:11 +0000

    sync: remove includes from ProfileSyncFactory + add to AutofillDataTypeController
    Probably the awesomest patch evar
    
    BUG=none
    TEST=compiles!
    
    
    Review URL: http://codereview.chromium.org/8590041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110586 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/autofill_data_type_controller.cc
+++ chrome/browser/sync/glue/autofill_data_type_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sync/glue/autofill_data_type_controller.h"
 
+#include "base/bind.h"
 #include "base/metrics/histogram.h"
 #include "base/task.h"
 #include "chrome/browser/profiles/profile.h"
--- chrome/browser/sync/profile_sync_factory.h
+++ chrome/browser/sync/profile_sync_factory.h
@@ -8,12 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
-#include <utility>
 
-#include "base/task.h"
-#include "chrome/browser/sync/glue/change_processor.h"
 #include "chrome/browser/sync/glue/data_type_controller.h"
-#include "chrome/browser/sync/glue/model_associator.h"
 #include "chrome/browser/sync/unrecoverable_error_handler.h"
 
 class PasswordStore;
@@ -22,6 +18,8 @@ class SyncableService;
 class WebDataService;
 
 namespace browser_sync {
+class AssociatorInterface;
+class ChangeProcessor;
 class DataTypeManager;
 class GenericChangeProcessor;
 class SharedChangeProcessor;
