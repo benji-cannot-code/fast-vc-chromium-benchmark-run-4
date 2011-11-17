80e4b0c18eb52ed35d3f85c9ab3a272dc3e7d066
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-17 22:13:39 +0000

    new-run-webkit-tests is locale dependent https://bugs.webkit.org/show_bug.cgi?id=68691
    Reviewed by Adam Barth.
    
    Make NRWT use a clean environment, just like ORWT did.
    
    * Scripts/webkitpy/layout_tests/port/base.py:
    * Scripts/webkitpy/layout_tests/port/chromium.py:
    * Scripts/webkitpy/layout_tests/port/chromium_unittest.py:
    * Scripts/webkitpy/layout_tests/port/chromium_win.py:
    * Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py:
    * Scripts/webkitpy/layout_tests/port/efl.py:
    * Scripts/webkitpy/layout_tests/port/google_chrome_unittest.py:
    * Scripts/webkitpy/layout_tests/port/mac.py:
    * Scripts/webkitpy/layout_tests/port/qt.py:
    * Scripts/webkitpy/layout_tests/port/qt_unittest.py:
    * Scripts/webkitpy/layout_tests/port/webkit.py:
    * Scripts/webkitpy/layout_tests/port/win.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100674 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_win.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_win.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Chromium Win implementation of the Port interface."""
 
+import os
 import logging
 import sys
 
@@ -99,13 +100,22 @@ class ChromiumWinPort(chromium.ChromiumPort):
 
 
     def setup_environ_for_server(self, server_name=None):
-        env = chromium.ChromiumPort.setup_environ_for_server(self)
+        env = chromium.ChromiumPort.setup_environ_for_server(self, server_name)
+
+        # FIXME: lighttpd depends on some environment variable we're not whitelisting.
+        # We should add the variable to an explicit whitelist in base.Port.
+        # FIXME: This is a temporary hack to get the cr-win bot online until
+        # someone from the cr-win port can take a look.
+        for key, value in os.environ.items():
+            if key not in env:
+                env[key] = value
+
         # Put the cygwin directory first in the path to find cygwin1.dll.
         env["PATH"] = "%s;%s" % (self.path_from_chromium_base("third_party", "cygwin", "bin"), env["PATH"])
         # Configure the cygwin directory so that pywebsocket finds proper
         # python executable to run cgi program.
         env["CYGWIN_PATH"] = self.path_from_chromium_base("third_party", "cygwin", "bin")
-        if (sys.platform in ("cygwin", "win32") and self.get_option('register_cygwin')):
+        if self.get_option('register_cygwin'):
             setup_mount = self.path_from_chromium_base("third_party", "cygwin", "setup_mount.bat")
             self._executive.run_command([setup_mount])  # Paths are all absolute, so this does not require a cwd.
         return env
