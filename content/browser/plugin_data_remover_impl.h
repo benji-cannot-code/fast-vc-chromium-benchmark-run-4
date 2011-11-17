a6edc6c7a000d2e0c111922314d9214ee84458c8
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 19:22:59 +0000

    Fix shared compile on windows
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110535 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_data_remover_impl.h
+++ content/browser/plugin_data_remover_impl.h
@@ -9,13 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "content/browser/plugin_process_host.h"
 #include "content/public/browser/plugin_data_remover.h"
 
-class CONTENT_EXPORT PluginDataRemoverImpl : public content::PluginDataRemover,
-                                             public PluginProcessHost::Client,
-                                             public IPC::Channel::Listener {
+class CONTENT_EXPORT PluginDataRemoverImpl
+    : public content::PluginDataRemover,
+      public NON_EXPORTED_BASE(PluginProcessHost::Client),
+      public IPC::Channel::Listener {
  public:
   explicit PluginDataRemoverImpl(
       const content::ResourceContext& resource_context);
