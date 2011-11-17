0a302293fed9d23695a9a76dc78ab6fc7120cb39
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-17 00:59:48 +0000

    [chromium] Track property changes for render surfaces. https://bugs.webkit.org/show_bug.cgi?id=72521
    Patch by Shawn Singh <shawnsingh@chromium.org> on 2011-11-16
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Created CCRenderSurfaceTest for testing.
    
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::drawLayersOntoRenderSurfaces):
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
    (WebCore::CCLayerImpl::resetPropertyChangedFlagForSubtree):
    * platform/graphics/chromium/cc/CCLayerImpl.h:
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::CCRenderSurface):
    (WebCore::CCRenderSurface::setClipRect):
    (WebCore::CCRenderSurface::setContentRect):
    (WebCore::CCRenderSurface::surfacePropertyChanged):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    (WebCore::CCRenderSurface::resetPropertyChangedFlag):
    
    Source/WebKit/chromium:
    
    * WebKit.gypi:
    * tests/CCLayerImplTest.cpp:
    (WebCore::TEST):
    * tests/CCRenderSurfaceTest.cpp: Added.
    (WebCore::TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100530 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
@@ -76,10 +76,10 @@ public:
     void setOriginTransform(const TransformationMatrix& originTransform) { m_originTransform = originTransform; }
     const TransformationMatrix& originTransform() const { return m_originTransform; }
 
-    void setClipRect(const IntRect& clipRect) { m_clipRect = clipRect; }
+    void setClipRect(const IntRect&);
     const IntRect& clipRect() const { return m_clipRect; }
 
-    void setContentRect(const IntRect& contentRect) { m_contentRect = contentRect; }
+    void setContentRect(const IntRect&);
     const IntRect& contentRect() const { return m_contentRect; }
 
     void setSkipsDraw(bool skipsDraw) { m_skipsDraw = skipsDraw; }
@@ -98,6 +98,10 @@ public:
     ManagedTexture* contentsTexture() const { return m_contentsTexture.get(); }
 
     int owningLayerId() const;
+
+    void resetPropertyChangedFlag() { m_surfacePropertyChanged = false; }
+    bool surfacePropertyChanged() const;
+
 private:
     void drawLayer(LayerRendererChromium*, CCLayerImpl*, const TransformationMatrix&);
     template <class T>
@@ -108,6 +112,7 @@ private:
 
     IntRect m_contentRect;
     bool m_skipsDraw;
+    bool m_surfacePropertyChanged;
 
     OwnPtr<ManagedTexture> m_contentsTexture;
     float m_drawOpacity;
--- third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
+++ third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'tests/CCLayerTreeHostCommonTest.cpp',
             'tests/CCLayerTreeHostImplTest.cpp',
             'tests/CCLayerTreeHostTest.cpp',
+            'tests/CCRenderSurfaceTest.cpp',
             'tests/CCSchedulerTest.cpp',
             'tests/CCSchedulerStateMachineTest.cpp',
             'tests/CCSchedulerTestCommon.h',
