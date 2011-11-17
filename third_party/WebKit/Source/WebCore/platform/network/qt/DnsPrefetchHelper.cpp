fff9bab81c2ae99d16abb65284f468b0f3cfb475
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-17 14:23:49 +0000

    [Qt] Layer violation: WebCore::dnsPrefetch uses QWebSettings::globalSettings() https://bugs.webkit.org/show_bug.cgi?id=72596
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    We don't need to use QWebSettings here, because we now propagate the DNS prefetch
    setting to WebCore::Settings and the rest of WebCore checks the setting before calling
    WebCore::prefetchDNS.
    
    * platform/network/qt/DnsPrefetchHelper.cpp:
    (WebCore::prefetchDNS): Removed the use of QWebSettings.
    * platform/network/qt/DnsPrefetchHelper.h: Remove offending qwebsettings.h inclusion.
    
    Source/WebKit/qt:
    
    * Api/qwebsettings.cpp:
    (QWebSettingsPrivate::apply): Propagate the DNS prefetch setting to WebCore::Settings.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/qt/DnsPrefetchHelper.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/qt/DnsPrefetchHelper.cpp
@@ -27,10 +27,8 @@ namespace WebCore {
 // this is called on mouse over a href and on page loading
 void prefetchDNS(const String& hostname)
 {
-    if (QWebSettings::globalSettings()->testAttribute(QWebSettings::DnsPrefetchEnabled)) {
-        static DnsPrefetchHelper dnsPrefetchHelper;
-        dnsPrefetchHelper.lookup(QString(hostname));
-    }
+    static DnsPrefetchHelper dnsPrefetchHelper;
+    dnsPrefetchHelper.lookup(QString(hostname));
 }
 
 }
--- third_party/WebKit/Source/WebCore/platform/network/qt/DnsPrefetchHelper.h
+++ third_party/WebKit/Source/WebCore/platform/network/qt/DnsPrefetchHelper.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QSet>
 #include <QString>
 #include <QTime>
-#include "qwebsettings.h"
 
 namespace WebCore {
 
