d68cc2b8c5bddf654f175acf702d1684116393ab
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 23:55:42 +0000

    Add buttons to clear the cache and flush socket pools to net-internals events view.  This can make reproducing some issues much less painful.
    Review URL: http://codereview.chromium.org/8590037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals_ui.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/browsing_data_remover.h"
 #include "chrome/browser/io_thread.h"
 #include "chrome/browser/net/chrome_net_log.h"
 #include "chrome/browser/net/connection_tester.h"
@@ -175,11 +176,12 @@ class NetInternalsMessageHandler
   // Javascript message handlers.
   void OnRendererReady(const ListValue* list);
   void OnEnableHttpThrottling(const ListValue* list);
+  void OnClearBrowserCache(const ListValue* list);
+  void OnGetPrerenderInfo(const ListValue* list);
 #ifdef OS_CHROMEOS
   void OnRefreshSystemLogs(const ListValue* list);
   void OnGetSystemLog(const ListValue* list);
 #endif
-  void OnGetPrerenderInfo(const ListValue* list);
 
  private:
   class IOThreadImpl;
@@ -533,6 +535,23 @@ void NetInternalsMessageHandler::RegisterMessages() {
       base::Bind(&IOThreadImpl::CallbackHelper,
                  &IOThreadImpl::OnGetServiceProviders, proxy_));
 #endif
+
+  web_ui_->RegisterMessageCallback(
+      "setLogLevel",
+      base::Bind(&IOThreadImpl::CallbackHelper,
+                 &IOThreadImpl::OnSetLogLevel, proxy_));
+  web_ui_->RegisterMessageCallback(
+      "enableHttpThrottling",
+      base::Bind(&NetInternalsMessageHandler::OnEnableHttpThrottling,
+                 base::Unretained(this)));
+  web_ui_->RegisterMessageCallback(
+      "clearBrowserCache",
+      base::Bind(&NetInternalsMessageHandler::OnClearBrowserCache,
+                 base::Unretained(this)));
+  web_ui_->RegisterMessageCallback(
+      "getPrerenderInfo",
+      base::Bind(&NetInternalsMessageHandler::OnGetPrerenderInfo,
+                 base::Unretained(this)));
 #ifdef OS_CHROMEOS
   web_ui_->RegisterMessageCallback(
       "refreshSystemLogs",
@@ -543,18 +562,6 @@ void NetInternalsMessageHandler::RegisterMessages() {
       base::Bind(&NetInternalsMessageHandler::OnGetSystemLog,
                  base::Unretained(this)));
 #endif
-  web_ui_->RegisterMessageCallback(
-      "setLogLevel",
-      base::Bind(&IOThreadImpl::CallbackHelper,
-                 &IOThreadImpl::OnSetLogLevel, proxy_));
-  web_ui_->RegisterMessageCallback(
-      "enableHttpThrottling",
-      base::Bind(&NetInternalsMessageHandler::OnEnableHttpThrottling,
-                 base::Unretained(this)));
-  web_ui_->RegisterMessageCallback(
-      "getPrerenderInfo",
-      base::Bind(&NetInternalsMessageHandler::OnGetPrerenderInfo,
-                 base::Unretained(this)));
 }
 
 void NetInternalsMessageHandler::SendJavascriptCommand(
@@ -614,6 +621,15 @@ void NetInternalsMessageHandler::OnEnableHttpThrottling(const ListValue* list) {
   if (http_throttling_may_experiment_.GetValue()) {
     http_throttling_may_experiment_.SetValue(false);
   }
+}
+
+void NetInternalsMessageHandler::OnClearBrowserCache(const ListValue* list) {
+  BrowsingDataRemover* remover =
+      new BrowsingDataRemover(Profile::FromWebUI(web_ui()),
+                              BrowsingDataRemover::EVERYTHING,
+                              base::Time());
+  remover->Remove(BrowsingDataRemover::REMOVE_CACHE);
+  // BrowsingDataRemover deletes itself.
 }
 
 void NetInternalsMessageHandler::OnGetPrerenderInfo(const ListValue* list) {
