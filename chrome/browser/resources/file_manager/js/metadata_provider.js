1ffeffa5f093bb1cad182045f66ffe9a3013353e
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 16:17:01 +0000

    Moving checkboxes to a separate column.
    In the File Manager list view icons are moved to the name column. Checkbox column made unsortable.
    
    +bugfixes.
    
    BUG=chromium-os:20156
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8585027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110501 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/metadata_provider.js
+++ chrome/browser/resources/file_manager/js/metadata_provider.js
@@ -14,7 +14,8 @@ function MetadataProvider(opt_workerPath) {
 
   if (!opt_workerPath) {
     var path = document.location.pathname;
-    opt_workerPath = path.substring(0, path.lastIndexOf('/') + 1) +
+    opt_workerPath = document.location.origin +
+        path.substring(0, path.lastIndexOf('/') + 1) +
         'js/metadata_dispatcher.js';
   }
 
