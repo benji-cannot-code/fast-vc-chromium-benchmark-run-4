9fab9f72dacc48f2d8f6407b41ddbc99961a95d7
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-18 06:09:25 +0000

    [GTK] The process freezes when you right click on windowless Flash https://bugs.webkit.org/show_bug.cgi?id=69123
    Reviewed by Xan Lopez.
    
    Source/WebCore:
    
    No new tests. I tried to create a layout test that exercised this
    issue, but it appears that EventSender clicks do not trigger
    it. This is covered by the manual tests containing Flash.
    
    * plugins/PluginPackage.cpp:
    (WebCore::PluginPackage::determineQuirks): Always activate the
    windowless Flash quirk if on x86_64 and X11.
    * plugins/gtk/PluginViewGtk.cpp:
    (WebCore::PluginView::handleMouseEvent): Avoid sending right-click
    events if we have the quirk.
    
    Source/WebKit2:
    
    Add a new plugin quirk for dealing with right-clicking on
    windowless Flash on x86_64 machines. This already exists for
    WebKit1.
    
    * Shared/Plugins/Netscape/x11/NetscapePluginModuleX11.cpp:
    (WebKit::NetscapePluginModule::determineQuirks): If the plugin is
    Flash and we are on x86_64, then disable sending right-clicking
    events while in windowless mode.
    * Shared/Plugins/PluginQuirks.h: Add the new quirk.
    * WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp:
    (WebKit::NetscapePlugin::platformHandleMouseEvent): If the quirk
    is active  don't send right click events.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
@@ -410,6 +410,11 @@ bool NetscapePlugin::platformHandleMouseEvent(const WebMouseEvent& event)
     if (m_isWindowed)
         return false;
 
+    if ((event.type() == WebEvent::MouseDown || event.type() == WebEvent::MouseUp)
+         && event.button() == WebMouseEvent::RightButton
+         && quirks().contains(PluginQuirks::IgnoreRightClickInWindowlessMode))
+        return false;
+
     XEvent xEvent;
     initializeXEvent(xEvent);
 
