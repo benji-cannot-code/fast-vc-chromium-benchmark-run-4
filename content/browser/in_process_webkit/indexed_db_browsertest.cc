da624d20305ff0b26787b13d8e0200d155068802
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 17:54:53 +0000

    Add FAIL to IndexedDBBrowserTestWithLowQuota.QuotaTest
    This fails after roll to WebKit r100780
    
    BUG=104748
    
    Review URL: http://codereview.chromium.org/8600003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110715 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_browsertest.cc
@@ -244,7 +244,7 @@ class IndexedDBBrowserTestWithLowQuota : public IndexedDBBrowserTest {
 
 };
 
-IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithLowQuota, QuotaTest) {
+IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithLowQuota, FAILS_QuotaTest) {
   SimpleTest(testUrl(FilePath(FILE_PATH_LITERAL("quota_test.html"))));
 }
 
