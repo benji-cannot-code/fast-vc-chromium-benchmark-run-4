7b2a8e1755da0438529f0b8b667723a6f9a370c3
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-18 00:20:41 +0000

    Remove cargo-cult copy/pasting of ScriptExecutionContext namespace https://bugs.webkit.org/show_bug.cgi?id=72676
    Reviewed by Eric Seidel.
    
    It looks like this cargo-cult started with initDNSPrefetch being hacked
    into setSecurityOrigin.  I've removed this hack along with the
    copy/paste code.
    
    * dom/DOMImplementation.cpp:
    * dom/Document.cpp:
    (WebCore::Document::setIsViewSource):
    (WebCore::Document::open):
    (WebCore::Document::initSecurityContext):
    (WebCore::Document::setSecurityOrigin):
    * dom/Document.h:
    * xml/XSLTProcessor.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100696 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DOMImplementation.cpp
+++ third_party/WebKit/Source/WebCore/dom/DOMImplementation.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PluginData.h"
 #include "PluginDocument.h"
 #include "RegularExpression.h"
+#include "SecurityOrigin.h"
 #include "Settings.h"
 #include "TextDocument.h"
 #include "ThreadGlobalData.h"
--- third_party/WebKit/Source/WebCore/xml/XSLTProcessor.cpp
+++ third_party/WebKit/Source/WebCore/xml/XSLTProcessor.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLBodyElement.h"
 #include "HTMLDocument.h"
 #include "Page.h"
+#include "SecurityOrigin.h"
 #include "Text.h"
 #include "TextResourceDecoder.h"
 #include "markup.h"
