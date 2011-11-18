22bdad7f96e9556281c2b7b6a33af528228fcbfc
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 02:06:34 +0000

    Revert 110613 - Implement the status tray/icon API for ChromeOS.
    BUG=74970
    TEST=status_tray_chromeos_unittest
    
    Review URL: http://codereview.chromium.org/8476003
    
    TBR=leandrogracia@chromium.org
    Review URL: http://codereview.chromium.org/8596003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/status_icons/status_tray_linux.cc
+++ chrome/browser/ui/views/status_icons/status_tray_linux.cc
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/status_icons/status_tray.h"
 
-#if !defined(OS_CHROMEOS)
-
 // Status icons are not currently supported on linux/views or Aura.
 StatusTray* StatusTray::Create() {
   return NULL;
 }
-
-#endif
