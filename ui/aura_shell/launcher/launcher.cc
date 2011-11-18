bdfda04afc742fa3dfb7cdd8fdd7348f81197042
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-18 01:35:32 +0000

    [Aura] Fix aura_shell_unittest ASAN errors.
    Synchronously close launcher widget so that it will not access LauncherModel after Launcher is destructed.
    
    BUG=104672
    TEST=Verify aura_shell_unittest should pass ASAN.
    
    
    Review URL: http://codereview.chromium.org/8589035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110628 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/launcher/launcher.cc
+++ ui/aura_shell/launcher/launcher.cc
@@ -43,6 +43,7 @@ Launcher::Launcher(aura::Window* window_container)
 }
 
 Launcher::~Launcher() {
+  widget_->CloseNow();
   window_container_->RemoveObserver(this);
   for (WindowMap::iterator i = known_windows_.begin();
        i != known_windows_.end(); ++i) {
