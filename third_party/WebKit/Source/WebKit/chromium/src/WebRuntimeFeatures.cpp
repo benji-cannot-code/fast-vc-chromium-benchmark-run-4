0785ffe63059c41b10bcd815a8e6c2420e8a1085
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-19 07:19:57 +0000

    [Chromium] [WebSocket] export WebSocketChannel interface for plugins https://bugs.webkit.org/show_bug.cgi?id=72016
    Source/WebCore:
    
    Patch by Takashi Toyoshima <toyoshim@chromium.org> on 2011-11-18
    Reviewed by Darin Fisher.
    
    Add a interface to send raw binary data.
    This interface is used by WebWebSocketChannel implementation
    in WebKit API.
    
    No new tests because just export a interface.
    
    * websockets/WebSocketChannel.cpp:
    (WebCore::WebSocketChannel::send):
    * websockets/WebSocketChannel.h:
    
    Source/WebKit/chromium:
    
    Add WebSocket and WebSocketClient to WebKit API.
    
    Currently, only WebSocketStreamHandle class is exported to WebKit API.
    WebSocketStreamHandle implements bi-directional communication ports.
    If plugins handles the WebSocket protocol, WebSocketChannel and
    WebSocketChannelClient must be exported as WebSocket because they are
    the classes which implement the WebSocket protocol stack.
    
    Normally, we use WebWebSocketChannel and WebWebSocketChannelClient as
    their class names. But here we use just WebSocket and WebSocketClient
    because of avoiding WebWeb prefix and class name confliction on
    header include.
    
    Patch by Takashi Toyoshima <toyoshim@chromium.org> on 2011-11-18
    Reviewed by Darin Fisher.
    
    * WebKit.gyp:
    * public/WebSocket.h: Added.
    (WebKit::WebSocket::~WebSocket):
    * public/WebSocketClient.h: Added.
    (WebKit::WebSocketClient::~WebSocketClient):
    * src/WebRuntimeFeatures.cpp: Insert websockets directory to include path in order to avoid filename confliction.
    (WebKit::WebRuntimeFeatures::enableSockets): Use WebCore namespace explicitly.
    * src/WebSocket.cpp: Added.
    (WebKit::WebSocket::create):
    * src/WebSocketImpl.cpp: Added.
    (WebKit::WebSocketImpl::WebSocketImpl):
    (WebKit::WebSocketImpl::~WebSocketImpl):
    (WebKit::WebSocketImpl::connect):
    (WebKit::WebSocketImpl::subprotocol):
    (WebKit::WebSocketImpl::sendText):
    (WebKit::WebSocketImpl::sendBinary):
    (WebKit::WebSocketImpl::bufferedAmount):
    (WebKit::WebSocketImpl::close):
    (WebKit::WebSocketImpl::fail):
    (WebKit::WebSocketImpl::disconnect):
    (WebKit::WebSocketImpl::didConnect):
    (WebKit::WebSocketImpl::didReceiveMessage):
    (WebKit::WebSocketImpl::didReceiveBinaryData):
    (WebKit::WebSocketImpl::didReceiveMessageError):
    (WebKit::WebSocketImpl::didStartClosingHandshake):
    (WebKit::WebSocketImpl::didClose):
    * src/WebSocketImpl.h: Added.
    (WebKit::WebSocketImpl::isNull):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100849 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AbstractDatabase.h"
 #include "RuntimeEnabledFeatures.h"
 #include "WebMediaPlayerClientImpl.h"
-#include "WebSocket.h"
+#include "websockets/WebSocket.h"
 
 #include <wtf/UnusedParam.h>
 
@@ -102,7 +102,7 @@ bool WebRuntimeFeatures::isMediaPlayerEnabled()
 void WebRuntimeFeatures::enableSockets(bool enable)
 {
 #if ENABLE(WEB_SOCKETS)
-    WebSocket::setIsAvailable(enable);
+    WebCore::WebSocket::setIsAvailable(enable);
 #endif
 }
 
