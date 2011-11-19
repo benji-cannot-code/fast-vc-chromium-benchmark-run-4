75c919c1eb267de3e75a88b8741096b595cb1829
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 01:21:58 +0000

    Fixes bug where clock button wasn't updating in response to a timezone change.
    BUG=104667
    TEST=none
    R=stevenjb@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8600001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/clock_menu_button_browsertest.cc
+++ chrome/browser/chromeos/status/clock_menu_button_browsertest.cc
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "unicode/calendar.h"
 #include "unicode/timezone.h"
 
+#if defined(USE_AURA)
+#include "chrome/browser/ui/views/aura/chrome_shell_delegate.h"
+#endif
+
 namespace chromeos {
 
 class ClockMenuButtonTest : public InProcessBrowserTest {
@@ -28,15 +32,20 @@ class ClockMenuButtonTest : public InProcessBrowserTest {
     // to use stub, so reset it here.
     CrosLibrary::Get()->GetTestApi()->ResetUseStubImpl();
   }
-  ClockMenuButton* GetClockMenuButton() {
-    BrowserView* view = static_cast<BrowserView*>(browser()->window());
-    return static_cast<ClockMenuButton*>(view->GetViewByID(
+  const ClockMenuButton* GetClockMenuButton() {
+    const views::View* parent = NULL;
+#if defined(USE_AURA)
+    parent = ChromeShellDelegate::instance()->GetStatusArea();
+#else
+    parent = static_cast<const BrowserView*>(browser()->window());
+#endif
+    return static_cast<const ClockMenuButton*>(parent->GetViewByID(
         VIEW_ID_STATUS_BUTTON_CLOCK));
   }
 };
 
 IN_PROC_BROWSER_TEST_F(ClockMenuButtonTest, TimezoneTest) {
-  ClockMenuButton* clock = GetClockMenuButton();
+  const ClockMenuButton* clock = GetClockMenuButton();
   ASSERT_TRUE(clock != NULL);
 
   // Update timezone and make sure clock text changes.
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -767,6 +767,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/status/status_area_view.h',
         'browser/chromeos/status/status_area_view_chromeos.cc',
         'browser/chromeos/status/status_area_view_chromeos.h',
+        'browser/chromeos/status/timezone_clock_updater.cc',
+        'browser/chromeos/status/timezone_clock_updater.h',
         'browser/chromeos/system/name_value_pairs_parser.cc',
         'browser/chromeos/system/name_value_pairs_parser.h',
         'browser/chromeos/system/runtime_environment.cc',
