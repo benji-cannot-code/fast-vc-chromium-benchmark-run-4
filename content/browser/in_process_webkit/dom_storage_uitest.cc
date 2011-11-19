41c3712aeb79c2bb48a720fa0a4beb0ac9b4d9e5
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 19:44:21 +0000

    Mark two tests as fails, to unblock the commit queue.
    BUG=104872
    TEST=none
    TBR=shess
    
    Review URL: http://codereview.chromium.org/8500011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110843 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/dom_storage_uitest.cc
+++ content/browser/in_process_webkit/dom_storage_uitest.cc
@@ -120,7 +120,14 @@ TEST_F(DOMStorageTest, FLAKY_EventLayoutTests) {
   RunTests(kEventsFiles);
 }
 
-TEST_F(DOMStorageTest, LocalStorageLayoutTests) {
+#if defined(OS_LINUX)
+// http://crbug.com/104872
+#define MAYBE_LocalStorageLayoutTests FAILS_LocalStorageLayoutTests
+#else
+#define MAYBE_LocalStorageLayoutTests LocalStorageLayoutTests
+#endif
+
+TEST_F(DOMStorageTest, MAYBE_LocalStorageLayoutTests) {
   InitializeForLayoutTest(test_dir_, FilePath().AppendASCII("localstorage"),
                           kNoHttpPort);
   AddJSTestResources();
@@ -129,7 +136,14 @@ TEST_F(DOMStorageTest, LocalStorageLayoutTests) {
   RunTests(kStorageFiles);
 }
 
-TEST_F(DOMStorageTest, SessionStorageLayoutTests) {
+#if defined(OS_LINUX)
+// http://crbug.com/104872
+#define MAYBE_SessionStorageLayoutTests FAILS_SessionStorageLayoutTests
+#else
+#define MAYBE_SessionStorageLayoutTests SessionStorageLayoutTests
+#endif
+
+TEST_F(DOMStorageTest, MAYBE_SessionStorageLayoutTests) {
   InitializeForLayoutTest(test_dir_, FilePath().AppendASCII("sessionstorage"),
                           kNoHttpPort);
   AddJSTestResources();
