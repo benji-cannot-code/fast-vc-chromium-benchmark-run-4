6ddf70fb2ccf108418dee611b018a9d9118a88d7
cevans@google.com <cevans@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-19 00:43:23 +0000

    Crash with ranges across a detached, reparented node tree https://bugs.webkit.org/show_bug.cgi?id=72757
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Test: fast/dom/move-detached-child-in-range.html
    
    * dom/RangeBoundaryPoint.h:
    (WebCore::RangeBoundaryPoint::childBefore): protect the raw child node from getting pulled from under us.
    
    LayoutTests:
    
    * fast/dom/move-detached-child-in-range-expected.txt: Added.
    * fast/dom/move-detached-child-in-range.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/RangeBoundaryPoint.h
+++ third_party/WebKit/Source/WebCore/dom/RangeBoundaryPoint.h
@@ -60,7 +60,7 @@ private:
     
     RefPtr<Node> m_containerNode;
     mutable int m_offsetInContainer;
-    Node* m_childBeforeBoundary;
+    RefPtr<Node> m_childBeforeBoundary;
 };
 
 inline RangeBoundaryPoint::RangeBoundaryPoint(PassRefPtr<Node> container)
@@ -78,7 +78,7 @@ inline Node* RangeBoundaryPoint::container() const
 
 inline Node* RangeBoundaryPoint::childBefore() const
 {
-    return m_childBeforeBoundary;
+    return m_childBeforeBoundary.get();
 }
 
 inline void RangeBoundaryPoint::ensureOffsetIsValid() const
