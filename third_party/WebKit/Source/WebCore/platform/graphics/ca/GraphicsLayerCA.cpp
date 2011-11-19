8ec9593efd926e64bad09eb316f8d92853235ab8
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-19 01:17:12 +0000

    Reflection on composited element doesn't update if the element changes to show no content https://bugs.webkit.org/show_bug.cgi?id=72774
    Source/WebCore:
    
    Reviewed by Chris Marrin.
    
    When a style change results in a reflected element losing its backing store,
    we need to clear the backing stores on the layer clones as well.
    
    Test: compositing/reflections/become-simple-composited-reflection.html
    
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::updateLayerDrawsContent):
    
    LayoutTests:
    
    Reviewed by Chris Marrin.
    
    Test that removes the background-color of a reflected, composited element.
    
    * compositing/reflections/become-simple-composited-reflection-expected.png: Added.
    * compositing/reflections/become-simple-composited-reflection-expected.txt: Added.
    * compositing/reflections/become-simple-composited-reflection.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100831 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -1339,9 +1339,14 @@ void GraphicsLayerCA::updateLayerDrawsContent(float pageScaleFactor, const Float
 
     if (m_drawsContent)
         m_layer->setNeedsDisplay();
-    else
+    else {
         m_layer->setContents(0);
-
+        if (m_layerClones) {
+            LayerMap::const_iterator end = m_layerClones->end();
+            for (LayerMap::const_iterator it = m_layerClones->begin(); it != end; ++it)
+                it->second->setContents(0);
+        }
+    }
     updateDebugIndicators();
 }
 
