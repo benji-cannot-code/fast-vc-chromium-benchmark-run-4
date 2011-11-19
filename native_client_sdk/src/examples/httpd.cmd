6b01acf832163fe5d0b4474adcad1b4fb3dd9359
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 04:05:02 +0000

    Revert 110822 - Breaks check_deps and check_perms
    Adding reduced version of sdk at 1387 to chrome tree.
    
    BUG=None
    TEST=None
    R=noelallen@google.com
    
    TBR
    
    BUG=None
    TBR=bradnelson@google.com
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110830 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/httpd.cmd
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-@echo off
-setlocal
-
-PATH=%CYGWIN%;%PATH%
-REM Use the path to this file (httpd.cmd) to get the
-REM path to httpd.py, so that we can run httpd.cmd from
-REM any directory.  Pass up to 9 arguments to httpd.py.
-python %~dp0\httpd.py %1 %2 %3 %4 %5 %6 %7 %8 %9
