3759f9ea7b21e1db1e8348fa12755381786dda60
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-21 12:48:15 +0000

    Add .dir-locals.el file for better Emacs defaults https://bugs.webkit.org/show_bug.cgi?id=72483
    Patch by Andy Wingo <wingo@igalia.com> on 2011-11-21
    Reviewed by Xan Lopez.
    
    * .dir-locals.el: Set appropriate directory-local variables for Emacs.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/.dir-locals.el
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+;; Per-directory local variables for GNU Emacs 23 and later.
+
+;; Syntax: ((MODE (VAR . VAL) ...) ...)
+;; MODE is a symbol like `c-mode', or `nil' for all modes.
+((nil
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 4))
+ (c-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 4))
+ (c++-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 4))
+ (change-log-mode
+  (indent-tabs-mode . nil)))
