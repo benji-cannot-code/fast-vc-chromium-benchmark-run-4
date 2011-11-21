30b765cc7d7f30ab9573357e129ee10c0744ab22
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 16:55:00 +0000

    Added "select all/none" checkbox.
    BUG=chromium-os:20156
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8548012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/table/table_column.js
+++ chrome/browser/resources/shared/js/cr/ui/table/table_column.js
@@ -32,8 +32,6 @@ cr.define('cr.ui.table', function() {
 
     width_: null,
 
-    renderFunction_: null,
-
     /**
      * Clones column.
      * @return {cr.ui.table.TableColumn} Clone of the given column.
@@ -41,6 +39,7 @@ cr.define('cr.ui.table', function() {
     clone: function() {
       var tableColumn = new TableColumn(this.id_, this.name_, this.width_);
       tableColumn.renderFunction = this.renderFunction_;
+      tableColumn.headerRenderFunction = this.headerRenderFunction_;
       return tableColumn;
     },
 
@@ -55,6 +54,15 @@ cr.define('cr.ui.table', function() {
       var div = table.ownerDocument.createElement('div');
       div.textContent = dataItem[columnId];
       return div;
+    },
+
+    /**
+     * Renders table header. This is the default render function.
+     * @param {cr.ui.Table} table The table.
+     * @return {HTMLElement} Rendered element.
+     */
+    headerRenderFunction_: function(table) {
+      return table.ownerDocument.createTextNode(this.name);
     },
   };
 
@@ -81,6 +89,12 @@ cr.define('cr.ui.table', function() {
    * @type {Function(*, string, cr.ui.Table): HTMLElement}
    */
   cr.defineProperty(TableColumn, 'renderFunction');
+
+  /**
+   * The column header render function.
+   * @type {Function(cr.ui.Table): HTMLElement}
+   */
+  cr.defineProperty(TableColumn, 'headerRenderFunction');
 
   return {
     TableColumn: TableColumn
