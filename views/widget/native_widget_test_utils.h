b3e00c2912ace99a0c9bf31fd3b95031af0b2809
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 18:17:35 +0000

    Revert "views: Move widget/ directory to ui/views." properly.
    Review URL: http://codereview.chromium.org/8562003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110960 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ views/widget/native_widget_test_utils.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VIEWS_WIDGET_NATIVE_WIDGET_TEST_UTILS_H_
+#define VIEWS_WIDGET_NATIVE_WIDGET_TEST_UTILS_H_
+#pragma once
+
+namespace views {
+namespace internal {
+
+class NativeWidgetPrivate;
+
+// Create dummy widgets for use in testing.  Caller owns the returned
+// |NativeWidgetPrivate| object which, in turn, owns the associated Widget.
+NativeWidgetPrivate* CreateNativeWidget();
+NativeWidgetPrivate* CreateNativeSubWidget();
+
+}  // namespace internal
+}  // namespace views
+
+#endif  // VIEWS_WIDGET_NATIVE_WIDGET_TEST_UTILS_H_
