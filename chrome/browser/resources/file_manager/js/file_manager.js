2f4961d4e5db6e1f4c2b18777b1d614f3518aacc
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 15:51:06 +0000

    Fixing exit on 'X' icon in Photo Editor
    BUG=chromium-os:23239
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8461009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -10,8 +10,6 @@ const EMPTY_IMAGE_URI = 'data:image/gif;base64,'
 
 var g_slideshow_data = null;
 
-const GALLERY_ENABLED = true;
-
 // If directory files changes too often, don't rescan directory more than once
 // per specified interval
 const SIMULTANEOUS_RESCAN_INTERVAL = 1000;
@@ -2270,7 +2268,6 @@ FileManager.prototype = {
           selectedUrl,
           function () {
             // TODO(kaznacheev): keep selection.
-            self.rescanDirectoryNow_();  // Make sure new files show up.
             self.dialogDom_.removeChild(galleryFrame);
             self.document_.title = self.currentDirEntry_.fullPath;
             self.refocus();
@@ -3065,6 +3062,11 @@ FileManager.prototype = {
 
       function onReadSome(entries) {
         if (entries.length == 0) {
+          metrics.recordTime('DirectoryScan');
+          if (self.currentDirEntry_.fullPath == DOWNLOADS_DIRECTORY) {
+            metrics.reportCount("DownloadsCount", self.dataModel_.length);
+          }
+
           if (self.pendingRescanQueue_.length > 0) {
             setTimeout(self.rescanDirectory_.bind(self),
                 SIMULTANEOUS_RESCAN_INTERVAL);
@@ -3099,6 +3101,8 @@ FileManager.prototype = {
 
         spliceArgs.unshift(0, 0);  // index, deleteCount
         self.dataModel_.splice.apply(self.dataModel_, spliceArgs);
+
+        metrics.startInterval('DirectoryScan');
 
         // Keep reading until entries.length is 0.
         reader.readEntries(onReadSome, onError);
