6715f8177f72e0c38d82efba7abd02b9ba4bfda0
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 13:36:01 +0000

    Protector strings and bubble/SettingsChange code refactoring.
    BUG=102765, 103317
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/8558020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110910 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/protector/base_setting_change.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/protector/base_setting_change.h"
+
+namespace protector {
+
+BaseSettingChange::BaseSettingChange() {
+}
+
+BaseSettingChange::~BaseSettingChange() {
+}
+
+bool BaseSettingChange::Init(Protector* protector) {
+  return true;
+}
+
+void BaseSettingChange::Apply(Protector* protector) {
+}
+
+void BaseSettingChange::Discard(Protector* protector) {
+}
+
+}  // namespace protector
