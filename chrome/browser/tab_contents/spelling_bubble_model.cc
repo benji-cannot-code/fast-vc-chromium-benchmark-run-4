116354bc17709e7accae82e10074371dae13fb7a
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 13:12:14 +0000

    Add a "Learn More" link to the Spelling bubble. This change adds a "Learn More" link to the Spelling bubble so we can open the "Exploring Chrome's privacy features" page as our "Under the Hood" page does.
    BUG=103840
    TEST=right click on a misspelled word.
    
    Review URL: http://codereview.chromium.org/8561024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/spelling_bubble_model.cc
+++ chrome/browser/tab_contents/spelling_bubble_model.cc
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/spelling_bubble_model.h"
 
 #include "base/logging.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/url_constants.h"
+#include "googleurl/src/gurl.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -48,4 +52,15 @@ void SpellingBubbleModel::Accept() {
 }
 
 void SpellingBubbleModel::Cancel() {
+}
+
+string16 SpellingBubbleModel::GetLinkText() const {
+  return l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_SPELLING_BUBBLE_LINK);
+}
+
+void SpellingBubbleModel::LinkClicked() {
+  Browser* browser = BrowserList::GetLastActiveWithProfile(profile_);
+  browser->OpenURL(
+      google_util::AppendGoogleLocaleParam(GURL(chrome::kPrivacyLearnMoreURL)),
+      GURL(), NEW_FOREGROUND_TAB, content::PAGE_TRANSITION_LINK);
 }
