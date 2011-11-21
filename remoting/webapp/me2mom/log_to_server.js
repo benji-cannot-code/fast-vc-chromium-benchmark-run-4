ce370c9b102bf6d983696c45cb4f5e094140499a
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 18:03:55 +0000

    Disable logging from the chromoting client to the server.
    R=jamiewalch@chromium.org
    
    Review URL: http://codereview.chromium.org/8558034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110956 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/me2mom/log_to_server.js
+++ remoting/webapp/me2mom/log_to_server.js
@@ -18,6 +18,7 @@ var remoting = remoting || {};
  */
 remoting.LogToServer = function() {
   /** @type Array.<string> */ this.pendingEntries = [];
+  /** @type boolean */ this.enabled = false;
 };
 
 /**
@@ -43,6 +44,9 @@ remoting.LogToServer.prototype.logClientSessionStateChange =
  * @param {remoting.ServerLogEntry} entry
  */
 remoting.LogToServer.prototype.log = function(entry) {
+  if (!this.enabled) {
+    return;
+  }
   // Store a stanza for the entry
   this.pendingEntries.push(entry.toStanza());
   // Stop if there's no connection to the server.
