49f847dd67a7106be87e10f573d88fac773918eb
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 01:09:05 +0000

    Adding in (mostly) verbatim landing of nacl sdk. Dropping large binaries. Redo of http://crrev.com/110822 checkdeps + checkperms have now beep updated in preparation.
    BUG=None
    TEST=None
    R=noelallen@google.com
    TBR
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110872 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/examples/hello_world/helper_functions.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "examples/hello_world/helper_functions.h"
+
+#include <algorithm>
+
+namespace hello_world {
+
+int32_t FortyTwo() {
+  return 42;
+}
+
+std::string ReverseText(const std::string& text) {
+  std::string reversed_string(text);
+  // Use reverse to reverse |reversed_string| in place.
+  std::reverse(reversed_string.begin(), reversed_string.end());
+  return reversed_string;
+}
+}  // namespace hello_world
+
