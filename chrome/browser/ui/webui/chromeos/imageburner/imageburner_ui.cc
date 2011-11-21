00f75b87c419f82cb37d651e6b016a170f08597b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 00:21:43 +0000

    CrOs: Remove 30 static initializers and 30 exit time destructors
    Also remove a few unused strings.
    
    BUG=101600,94925
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8608001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110867 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.cc
+++ chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.cc
@@ -29,15 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::BrowserThread;
 
-namespace {
-
 const char kPropertyDevicePath[] = "devicePath";
 const char kPropertyFilePath[] = "filePath";
 const char kPropertyLabel[] = "label";
 const char kPropertyPath[] = "path";
 
 // Name for hwid in machine statistics.
-const std::string kHwidStatistic = "hardware_class";
+const char kHwidStatistic[] = "hardware_class";
 
 const char kImageZipFileName[] = "chromeos_image.bin.zip";
 
@@ -45,8 +43,10 @@ const char kImageZipFileName[] = "chromeos_image.bin.zip";
 const uint64 kMinDeviceSize = static_cast<uint64>(3.9) * 1000 * 1000 * 1000;
 
 // Link displayed on imageburner ui.
-const std::string kMoreInfoLink =
+const char kMoreInfoLink[] =
     "http://www.chromium.org/chromium-os/chromiumos-design-docs/recovery-mode";
+
+namespace {
 
 ChromeWebUIDataSource *CreateImageburnerUIHTMLSource() {
   ChromeWebUIDataSource *source =
