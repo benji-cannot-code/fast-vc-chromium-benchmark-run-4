494f5a971a9673231d53b47f14a72b8a6b4ca69a
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-21 21:06:41 +0000

    [Qt] Remove unnecessary initialize method from QQuickWebView. https://bugs.webkit.org/show_bug.cgi?id=72905
    Reviewed by Kenneth Rohde Christiansen.
    
    We don't need this level of indirection.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::QQuickWebViewPrivate):
    (QQuickWebView::QQuickWebView):
    * UIProcess/API/qt/qquickwebview_p_p.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100950 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtWidgets/QInputDialog>
 #include <WKOpenPanelResultListener.h>
 
-QQuickWebViewPrivate::QQuickWebViewPrivate()
-    : q_ptr(0)
+QQuickWebViewPrivate::QQuickWebViewPrivate(QQuickWebView* viewport, WKContextRef contextRef, WKPageGroupRef pageGroupRef)
+    : q_ptr(viewport)
     , alertDialog(0)
     , confirmDialog(0)
     , promptDialog(0)
@@ -46,6 +46,19 @@ QQuickWebViewPrivate::QQuickWebViewPrivate()
     , transitioningToNewPage(false)
     , useTraditionalDesktopBehaviour(false)
 {
+    viewport->setFlags(QQuickItem::ItemClipsChildrenToShape);
+
+    QObject::connect(viewport, SIGNAL(visibleChanged()), viewport, SLOT(_q_onVisibleChanged()));
+    pageView.reset(new QQuickWebPage(viewport));
+
+    QQuickWebPagePrivate* const pageViewPrivate = pageView.data()->d;
+    setPageProxy(new QtWebPageProxy(pageView.data(), q_ptr, 0, this, contextRef, pageGroupRef));
+    pageViewPrivate->setPageProxy(pageProxy.data());
+
+    QWebPreferencesPrivate::get(pageProxy->preferences())->setAttribute(QWebPreferencesPrivate::AcceleratedCompositingEnabled, true);
+    pageProxy->init();
+
+    QObject::connect(pageProxy.data(), SIGNAL(updateNavigationState()), q_ptr, SIGNAL(navigationStateChanged()));
 }
 
 void QQuickWebViewPrivate::enableMouseEvents()
@@ -64,24 +77,6 @@ void QQuickWebViewPrivate::disableMouseEvents()
     q->setAcceptHoverEvents(false);
     pageView->setAcceptedMouseButtons(Qt::NoButton);
     pageView->setAcceptHoverEvents(false);
-}
-
-void QQuickWebViewPrivate::initialize(QQuickWebView* viewport, WKContextRef contextRef, WKPageGroupRef pageGroupRef)
-{
-    q_ptr = viewport;
-    viewport->setFlags(QQuickItem::ItemClipsChildrenToShape);
-
-    QObject::connect(viewport, SIGNAL(visibleChanged()), viewport, SLOT(_q_onVisibleChanged()));
-    pageView.reset(new QQuickWebPage(viewport));
-
-    QQuickWebPagePrivate* const pageViewPrivate = pageView.data()->d;
-    setPageProxy(new QtWebPageProxy(pageView.data(), q_ptr, 0, this, contextRef, pageGroupRef));
-    pageViewPrivate->setPageProxy(pageProxy.data());
-
-    QWebPreferencesPrivate::get(pageProxy->preferences())->setAttribute(QWebPreferencesPrivate::AcceleratedCompositingEnabled, true);
-    pageProxy->init();
-
-    QObject::connect(pageProxy.data(), SIGNAL(updateNavigationState()), q_ptr, SIGNAL(navigationStateChanged()));
 }
 
 void QQuickWebViewPrivate::initializeDesktop(QQuickWebView* viewport)
@@ -475,21 +470,19 @@ void QQuickWebViewExperimental::setPromptDialog(QDeclarativeComponent* promptDia
 
 QQuickWebView::QQuickWebView(QQuickItem* parent)
     : QQuickItem(parent)
-    , d_ptr(new QQuickWebViewPrivate)
+    , d_ptr(new QQuickWebViewPrivate(this))
     , m_experimental(new QQuickWebViewExperimental(this))
 {
     Q_D(QQuickWebView);
-    d->initialize(this);
     d->initializeTouch(this);
 }
 
 QQuickWebView::QQuickWebView(WKContextRef contextRef, WKPageGroupRef pageGroupRef, QQuickItem* parent)
     : QQuickItem(parent)
-    , d_ptr(new QQuickWebViewPrivate)
+    , d_ptr(new QQuickWebViewPrivate(this, contextRef, pageGroupRef))
     , m_experimental(new QQuickWebViewExperimental(this))
 {
     Q_D(QQuickWebView);
-    d->initialize(this, contextRef, pageGroupRef);
     // Used by WebKitTestRunner.
     d->setUseTraditionalDesktopBehaviour(true);
 }
