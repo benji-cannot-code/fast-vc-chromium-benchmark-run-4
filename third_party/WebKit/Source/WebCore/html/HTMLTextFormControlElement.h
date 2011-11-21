76c9124e121ee36aee0a2645a29d455a3f7d5416
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-21 23:13:00 +0000

    Need support for dirname attribute https://bugs.webkit.org/show_bug.cgi?id=65542
    Patch by Rakesh KN <rakesh.kn@motorola.com> on 2011-11-21
    Reviewed by Eric Seidel.
    
    Implemented 'dirname' form attribute.
    
    Source/WebCore:
    
    Tests: fast/forms/form-dirname-attribute.html
           fast/forms/submit-form-with-dirname-attribute-with-ancestor-dir-attribute.html
           fast/forms/submit-form-with-dirname-attribute-with-nonhtml-ancestor.html
           fast/forms/submit-form-with-dirname-attribute.html
    
    * html/HTMLAttributeNames.in:
    Added "dirname" attribute.
    * html/HTMLInputElement.idl:
    Add "dirName" property to HTMLInputElement interface.
    * html/HTMLTextAreaElement.cpp:
    (WebCore::HTMLTextAreaElement::appendFormData):
    Append dirname form data.
    * html/HTMLTextAreaElement.idl:
    Add "dirName" property to HTMLTextAreaElement interface.
    * html/HTMLTextFormControlElement.cpp:
    (WebCore::parentHTMLElement):
    Helper function which returns only HTML parent element.
    (WebCore::HTMLTextFormControlElement::directionForFormData):
    Helper function for finding directionality of the Element.
    * html/HTMLTextFormControlElement.h:
    Helper function for finding directionality of the Element.
    * html/TextFieldInputType.cpp:
    (WebCore::TextFieldInputType::appendFormData):
    Append dirname form data.
    * html/TextFieldInputType.h:
    Append dirname form data.
    
    LayoutTests:
    
    * fast/forms/form-dirname-attribute-expected.txt: Added.
    * fast/forms/form-dirname-attribute.html: Added.
    * fast/forms/submit-form-with-dirname-attribute-expected.txt: Added.
    * fast/forms/submit-form-with-dirname-attribute-with-ancestor-dir-attribute-expected.txt: Added.
    * fast/forms/submit-form-with-dirname-attribute-with-ancestor-dir-attribute.html: Added.
    * fast/forms/submit-form-with-dirname-attribute-with-nonhtml-ancestor-expected.txt: Added.
    * fast/forms/submit-form-with-dirname-attribute-with-nonhtml-ancestor.html: Added.
    * fast/forms/submit-form-with-dirname-attribute.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@100965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLTextFormControlElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLTextFormControlElement.h
@@ -80,6 +80,8 @@ public:
     void setInnerTextValue(const String&);
     String innerTextValue() const;
 
+    String directionForFormData() const;
+
 protected:
     HTMLTextFormControlElement(const QualifiedName&, Document*, HTMLFormElement*);
     virtual void updatePlaceholderText() = 0;
