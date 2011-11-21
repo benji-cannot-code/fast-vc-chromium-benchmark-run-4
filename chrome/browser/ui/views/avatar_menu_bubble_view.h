1122580a0a9d9a3beee120cf096b8ff5ddfc23a0
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 23:01:22 +0000

    Revert 111018 - Rebase AvatarMenuBubbleView on the new views bubble. Added an std::max to prevent negative width calculation.
    TODO(sail/msw): BubbleBorder::ALIGN_EDGE_TO_ANCHOR_EDGE.
    I filed crbug.com/105014 for the placement regression on NTP.
    
    BUG=98323
    TEST=The avatar menu bubble works as before.
    
    Review URL: http://codereview.chromium.org/8500004
    
    TBR=msw@chromium.org
    Review URL: http://codereview.chromium.org/8621004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu_bubble_view.h
+++ chrome/browser/ui/views/avatar_menu_bubble_view.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "chrome/browser/profiles/avatar_menu_model_observer.h"
-#include "ui/views/bubble/bubble_delegate.h"
+#include "chrome/browser/ui/views/bubble/bubble.h"
 #include "views/controls/button/button.h"
 #include "views/controls/link_listener.h"
 
@@ -27,15 +27,13 @@ class Separator;
 
 // This bubble view is displayed when the user clicks on the avatar button.
 // It displays a list of profiles and allows users to switch between profiles.
-class AvatarMenuBubbleView : public views::BubbleDelegateView,
+class AvatarMenuBubbleView : public views::View,
                              public views::ButtonListener,
                              public views::LinkListener,
+                             public BubbleDelegate,
                              public AvatarMenuModelObserver {
  public:
-  AvatarMenuBubbleView(views::View* anchor_view,
-                       views::BubbleBorder::ArrowLocation arrow_location,
-                       const gfx::Rect& anchor_rect,
-                       Browser* browser);
+  explicit AvatarMenuBubbleView(Browser* browser);
   virtual ~AvatarMenuBubbleView();
 
   // views::View implementation.
@@ -51,8 +49,10 @@ class AvatarMenuBubbleView : public views::BubbleDelegateView,
   virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
 
   // BubbleDelegate implementation.
-  virtual gfx::Point GetAnchorPoint() OVERRIDE;
-  virtual void Init() OVERRIDE;
+  virtual void BubbleShown() OVERRIDE;
+  virtual void BubbleClosing(Bubble* bubble, bool closed_by_escape) OVERRIDE;
+  virtual bool CloseOnEscape() OVERRIDE;
+  virtual bool FadeInOnShow() OVERRIDE;
 
   // AvatarMenuModelObserver implementation.
   virtual void OnAvatarMenuModelChanged(
@@ -61,7 +61,6 @@ class AvatarMenuBubbleView : public views::BubbleDelegateView,
  private:
   views::Link* add_profile_link_;
   scoped_ptr<AvatarMenuModel> avatar_menu_model_;
-  gfx::Rect anchor_rect_;
   Browser* browser_;
   std::vector<views::CustomButton*> item_views_;
   views::Separator* separator_;
