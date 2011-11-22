c355baa198215e3721ffa4b98ae7eb7d84473d82
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-22 18:35:41 +0000

    Fix .dir-locals.el to only apply to specific modes https://bugs.webkit.org/show_bug.cgi?id=72963
    Patch by Andy Wingo <wingo@igalia.com> on 2011-11-22
    Reviewed by Xan Lopez.
    
    * .dir-locals.el: Remove `nil' block, as it was causing
    makefile-mode not to insert tabs.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/.dir-locals.el
+++ third_party/WebKit/.dir-locals.el
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ;; Syntax: ((MODE (VAR . VAL) ...) ...)
 ;; MODE is a symbol like `c-mode', or `nil' for all modes.
-((nil
-  (indent-tabs-mode . nil)
-  (c-basic-offset . 4))
- (c-mode
+((c-mode
   (indent-tabs-mode . nil)
   (c-basic-offset . 4))
  (c++-mode
