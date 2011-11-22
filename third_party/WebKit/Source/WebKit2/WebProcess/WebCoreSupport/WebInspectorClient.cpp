95183ee5b1497485461c2168ac83eaae8a5140cc
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-22 15:18:02 +0000

    cvel Feldman  <pfeldman@google.com>
            Not reviewed: moving inspector frontend creation out of assert in WebKit2.
    
            * WebProcess/WebCoreSupport/WebInspectorClient.cpp:
            (WebKit::WebInspectorClient::openInspectorFrontend):lean
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebInspectorClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebInspectorClient.cpp
@@ -45,7 +45,8 @@ void WebInspectorClient::inspectorDestroyed()
 
 void WebInspectorClient::openInspectorFrontend(InspectorController*)
 {
-    ASSERT(m_page->inspector()->createInspectorPage());
+    WebPage* inspectorPage = m_page->inspector()->createInspectorPage();
+    ASSERT(inspectorPage);
 }
 
 void WebInspectorClient::bringFrontendToFront()
