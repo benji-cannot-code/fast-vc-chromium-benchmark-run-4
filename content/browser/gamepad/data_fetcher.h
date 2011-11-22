c99798c822abc6451483f2d54103374551ed809b
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 19:10:40 +0000

    Add gamepad hardware data fetcher, and higher level thread container
    DataFetcher is the hardware and platform-specific data getter that talks to OS
    level interfaces for each platform and fill in a structure provided to it.
    Currently only Windows is here.
    
    Provider is a manager for the background thread that the DataFetcher runs on,
    and interacts with the rest of browser.
    
    This is part of a larger patch, the remainder of which is at
    http://codereview.chromium.org/8345027/
    
    BUG=79050
    
    
    Review URL: http://codereview.chromium.org/8568029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111189 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/browser/gamepad/data_fetcher.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_GAMEPAD_DATA_FETCHER_H_
+#define CONTENT_BROWSER_GAMEPAD_DATA_FETCHER_H_
+
+namespace WebKit {
+class WebGamepads;
+}
+
+namespace gamepad {
+
+class DataFetcher {
+ public:
+  virtual ~DataFetcher() {}
+  virtual void GetGamepadData(WebKit::WebGamepads* pads,
+                              bool devices_changed_hint) = 0;
+};
+
+} // namespace gamepad
+
+#endif  // CONTENT_BROWSER_GAMEPAD_DATA_FETCHER_H_
