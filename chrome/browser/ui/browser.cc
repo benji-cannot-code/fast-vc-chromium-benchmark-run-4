445fb5da42f171e4efa42e4618de760285c9143c
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 03:05:56 +0000

    Don't show first run bubble for non-tabbed windows
    BUG=104685
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8622006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -5286,6 +5286,10 @@ void Browser::OnWindowDidShow() {
     return;
   window_has_shown_ = true;
 
+  // Nothing to do for non-tabbed windows.
+  if (!is_type_tabbed())
+    return;
+
   // Suppress the first run bubble if we're showing the sync promo.
   TabContents* contents = GetSelectedTabContents();
   bool is_showing_promo = contents &&
@@ -5310,7 +5314,7 @@ void Browser::OnWindowDidShow() {
     // windows.
     local_state->SetBoolean(prefs::kShouldShowFirstRunBubble, false);
     window_->GetLocationBar()->ShowFirstRunBubble(bubble_type);
-  } else if (is_type_tabbed()) {
+  } else {
     GlobalErrorService* service =
         GlobalErrorServiceFactory::GetForProfile(profile());
     GlobalError* error = service->GetFirstGlobalErrorWithBubbleView();
