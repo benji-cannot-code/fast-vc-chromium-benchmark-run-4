850441283b476b47f850b17ce63e98befdc9aabe
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 15:43:05 +0000

    Add reload link.
    BUG=chromium:101618
    TEST=Load a page.  Open Tools -> Task manager and kill the process for rendering the page.  On the killed process page, click on the "Reload" button.
    
    
    Review URL: http://codereview.chromium.org/8574074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/sad_tab_view.h
+++ chrome/browser/ui/views/sad_tab_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "views/controls/button/button.h"
 #include "views/controls/link_listener.h"
 #include "views/view.h"
 
@@ -20,6 +21,7 @@ class Font;
 
 namespace views {
 class Label;
+class TextButton;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -31,7 +33,8 @@ class Label;
 //
 ///////////////////////////////////////////////////////////////////////////////
 class SadTabView : public views::View,
-                   public views::LinkListener {
+                   public views::LinkListener,
+                   public views::ButtonListener {
  public:
   // NOTE: Do not remove or reorder the elements in this enum, and only add new
   // items at the end. We depend on these specific values in a histogram.
@@ -48,6 +51,10 @@ class SadTabView : public views::View,
 
   // Overridden from views::LinkListener:
   virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
+
+  // Overridden from views::ButtonListener:
+  virtual void ButtonPressed(views::Button* source,
+                             const views::Event& event) OVERRIDE;
 
  protected:
   // Overridden from views::View:
@@ -67,6 +74,7 @@ class SadTabView : public views::View,
   views::Label* message_;
   views::Link* help_link_;
   views::Link* feedback_link_;
+  views::TextButton* reload_button_;
 
   DISALLOW_COPY_AND_ASSIGN(SadTabView);
 };
