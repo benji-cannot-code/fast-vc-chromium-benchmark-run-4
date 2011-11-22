e46d25cfb5cb7aa0217b166be1a6e41023735c12
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 12:08:10 +0000

    Revert "DevTools: remove obsolete methods from DevToolsClientHost"
    This reverts commit 111135
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8636010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111137 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/debugger/devtools_client_host.h
+++ content/browser/debugger/devtools_client_host.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "content/common/content_export.h"
@@ -16,6 +17,7 @@ namespace IPC {
 class Message;
 }
 
+class RenderViewHost;
 class TabContents;
 
 // Describes interface for managing devtools clients from browser process. There
@@ -31,6 +33,8 @@ class CONTENT_EXPORT DevToolsClientHost {
    private:
     DISALLOW_COPY_AND_ASSIGN(CloseListener);
   };
+
+  static DevToolsClientHost* FindOwnerClientHost(RenderViewHost* client_rvh);
 
   virtual ~DevToolsClientHost();
 
@@ -52,6 +56,10 @@ class CONTENT_EXPORT DevToolsClientHost {
   // Invoked when a tab is replaced by another tab. This is triggered by
   // TabStripModel::ReplaceTabContentsAt.
   virtual void TabReplaced(TabContents* new_tab) = 0;
+
+  // Returns client (front-end) RenderViewHost implementation of this
+  // client host if applicable. NULL otherwise.
+  virtual RenderViewHost* GetClientRenderViewHost();
 
  protected:
   DevToolsClientHost();
