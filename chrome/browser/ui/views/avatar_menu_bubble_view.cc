1dbffefbe75e4ce416ff9a118059fa6aa538197b
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 05:43:46 +0000

    Rebase AvatarMenuBubbleView on the new views bubble. Added an std::max to prevent negative width calculation.
    TODO(sail/msw): BubbleBorder::ALIGN_EDGE_TO_ANCHOR_EDGE.
    I filed crbug.com/105014 for the placement regression on NTP.
    
    BUG=98323
    TEST=The avatar menu bubble works as before.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=111018
    
    Review URL: http://codereview.chromium.org/8500004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111102 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu_bubble_view.cc
+++ chrome/browser/ui/views/avatar_menu_bubble_view.cc
@@ -30,6 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// TODO(msw): Get color from theme/window color.
+const SkColor kColor = SK_ColorWHITE;
+
 const int kItemHeight = 44;
 const int kItemMarginY = 4;
 const int kIconWidth = 38;
@@ -235,7 +238,7 @@ void ProfileItemView::Layout() {
   image_view_->SetBoundsRect(icon_rect);
 
   int label_x = icon_rect.right() + kIconMarginX;
-  int max_label_width = width() - label_x;
+  int max_label_width = std::max(width() - label_x, 0);
   gfx::Size name_size = name_label_->GetPreferredSize();
   name_size.set_width(std::min(name_size.width(), max_label_width));
   gfx::Size state_size = sync_state_label_->GetPreferredSize();
@@ -280,11 +283,6 @@ void ProfileItemView::OnBlur() {
 void ProfileItemView::OnHighlightStateChanged() {
   set_background(IsHighlighted() ? views::Background::CreateSolidBackground(
       SkColorSetRGB(0xe3, 0xed, 0xf6)) : NULL);
-  SkColor background_color = background() ?
-      background()->get_color() : Bubble::kBackgroundColor;
-  name_label_->SetBackgroundColor(background_color);
-  sync_state_label_->SetBackgroundColor(background_color);
-  edit_link_->SetBackgroundColor(background_color);
 
   bool show_edit = IsHighlighted() && item_.active;
   sync_state_label_->SetVisible(!show_edit);
@@ -331,8 +329,14 @@ bool ProfileItemView::IsHighlighted() {
 
 // AvatarMenuBubbleView -------------------------------------------------------
 
-AvatarMenuBubbleView::AvatarMenuBubbleView(Browser* browser)
-    : add_profile_link_(NULL),
+AvatarMenuBubbleView::AvatarMenuBubbleView(
+    views::View* anchor_view,
+    views::BubbleBorder::ArrowLocation arrow_location,
+    const gfx::Rect& anchor_rect,
+    Browser* browser)
+    : BubbleDelegateView(anchor_view, arrow_location, kColor),
+      add_profile_link_(NULL),
+      anchor_rect_(anchor_rect),
       browser_(browser) {
   avatar_menu_model_.reset(new AvatarMenuModel(
       &g_browser_process->profile_manager()->GetProfileInfoCache(),
@@ -445,23 +449,13 @@ void AvatarMenuBubbleView::LinkClicked(views::Link* source, int event_flags) {
   }
 }
 
-void AvatarMenuBubbleView::BubbleShown() {
-  GetFocusManager()->RegisterAccelerator(
-      ui::Accelerator(ui::VKEY_DOWN, false, false, false), this);
-  GetFocusManager()->RegisterAccelerator(
-      ui::Accelerator(ui::VKEY_UP, false, false, false), this);
+gfx::Point AvatarMenuBubbleView::GetAnchorPoint() {
+  return gfx::Point(anchor_rect_.CenterPoint().x(), anchor_rect_.bottom());
 }
 
-void AvatarMenuBubbleView::BubbleClosing(Bubble* bubble,
-                                         bool closed_by_escape) {
-}
-
-bool AvatarMenuBubbleView::CloseOnEscape() {
-  return true;
-}
-
-bool AvatarMenuBubbleView::FadeInOnShow() {
-  return false;
+void AvatarMenuBubbleView::Init() {
+  AddAccelerator(ui::Accelerator(ui::VKEY_DOWN, 0));
+  AddAccelerator(ui::Accelerator(ui::VKEY_UP, 0));
 }
 
 void AvatarMenuBubbleView::OnAvatarMenuModelChanged(
@@ -489,7 +483,6 @@ void AvatarMenuBubbleView::OnAvatarMenuModelChanged(
       l10n_util::GetStringUTF16(IDS_PROFILES_CREATE_NEW_PROFILE_LINK));
   add_profile_link_->set_listener(this);
   add_profile_link_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
-  add_profile_link_->SetBackgroundColor(Bubble::kBackgroundColor);
   add_profile_link_->SetEnabledColor(SkColorSetRGB(0xe3, 0xed, 0xf6));
   AddChildView(add_profile_link_);
 
