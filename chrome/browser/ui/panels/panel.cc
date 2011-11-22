72f31bf096af581b9b6191509f339bab650d51d5
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 01:31:10 +0000

    Add extension API to change window show state using chrome.windows.update().
    Valid states are normal, minimized and maximized. Actual behavior depends on OS and window type.
    
    BUG=10828
    TEST=ExtensionApiTest.UpdateWindowShowState
    
    
    Review URL: http://codereview.chromium.org/8586045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel.cc
+++ chrome/browser/ui/panels/panel.cc
@@ -257,6 +257,18 @@ bool Panel::IsMinimized() const {
   return expansion_state_ != EXPANDED;
 }
 
+void Panel::Maximize() {
+  Restore();
+}
+
+void Panel::Minimize() {
+  SetExpansionState(MINIMIZED);
+}
+
+void Panel::Restore() {
+  SetExpansionState(EXPANDED);
+}
+
 void Panel::EnterFullscreen(
       const GURL& url, FullscreenExitBubbleType type) {
   NOTIMPLEMENTED();
