f8b5e9e2f303149fb119b99e7bb41864aabb2cd6
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 22:48:55 +0000

    The host sends simple log entries to the server.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8468015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111239 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/server_log_entry.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_SERVER_LOG_ENTRY_H_
+#define REMOTING_HOST_SERVER_LOG_ENTRY_H_
+
+#include <map>
+#include <string>
+
+namespace buzz {
+class XmlElement;
+}  // namespace buzz
+
+namespace remoting {
+
+class ServerLogEntry {
+ public:
+  // Constructs a log entry for a session state change.
+  // Currently this is either connection or disconnection.
+  static ServerLogEntry* MakeSessionStateChange(bool connection);
+
+  ~ServerLogEntry();
+
+  // Adds fields describing the host to this log entry.
+  void AddHostFields();
+
+  // Converts this object to an XML stanza.
+  // The caller takes ownership of the stanza.
+  buzz::XmlElement* ToStanza() const;
+
+ private:
+  typedef std::map<std::string, std::string> ValuesMap;
+
+  ServerLogEntry();
+  void Set(const char* key, const char* value);
+
+  static const char* GetValueSessionState(bool connected);
+
+  ValuesMap values_map_;
+};
+
+}  // namespace remoting
+
+#endif
