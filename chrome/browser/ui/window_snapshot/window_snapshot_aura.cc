10945e6a53706cfa02e7460f307822fae1d6a41e
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 14:47:08 +0000

    This replaces the snapshotting code in the pixel tests with the code used to get screenshots for reporting issues. Added a bounds parameter to window_snapshot.cc so that we can take a picture of just the tab contents and not have to rebaseline with chrome ui changes.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/8523022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
+++ chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser {
 
-gfx::Rect GrabWindowSnapshot(gfx::NativeWindow window_handle,
-                             std::vector<unsigned char>* png_representation) {
+bool GrabWindowSnapshot(gfx::NativeWindow window_handle,
+                             std::vector<unsigned char>* png_representation,
+                             const gfx::Rect& snapshot_bounds) {
   // TODO(saintlou): Stub for Aura.
   NOTIMPLEMENTED();
-  return gfx::Rect();
+  return false;
 }
 
 }  // namespace browser
