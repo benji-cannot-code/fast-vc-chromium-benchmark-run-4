87a4a72554783c8bed809bdda8374674da2370ff
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 19:48:06 +0000

    Move GPU preliminary info collection back to UI thread.
    Apparently doing it on FILE thread causes a race, which causes crbug 83153.
    
    In order to move back to UI thread, we have to drop chrome channel support in GPU blacklist (it's a FILE operation); also, we have to move the collection of ATI driver version in linux to GPU process.
    
    Note that this is the minimum change required for the move.  Further cleanup will follow once this is merged safely back to M16.
    
    BUG=97535
    TEST=unittest
    Review URL: http://codereview.chromium.org/8670004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/gpu_blacklist_updater.h
+++ chrome/browser/web_resource/gpu_blacklist_updater.h
@@ -13,8 +13,8 @@ class GpuBlacklistUpdater : public WebResourceService {
  public:
   GpuBlacklistUpdater();
 
-  // Initialize GpuDataManager, and post a SetupOnUIThread task.
-  static void SetupOnFileThread();
+  // Initialize GpuDataManager.
+  static void Setup();
 
   // URL of the up-to-date gpu_blacklist.json file.
   static const char* kDefaultGpuBlacklistURL;
@@ -23,9 +23,6 @@ class GpuBlacklistUpdater : public WebResourceService {
   virtual ~GpuBlacklistUpdater();
 
   virtual void Unpack(const base::DictionaryValue& parsed_json) OVERRIDE;
-
-  // Initialize GpuBlacklistUpdater and schedule an auto update.
-  static void SetupOnUIThread();
 
   void InitializeGpuBlacklist();
 
