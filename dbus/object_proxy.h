c5713d3bfdfbe62c2a66147030203306d86e9bbb
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 22:08:38 +0000

    dbus: Fix a bug where we were emitting spurious error messages.
    Error messages like "Requested to remove an unknown match rule:
    type='signal', interface='...'" were emitted at shutdown of Bus
    object if an object proxy was connected to more than one signal of
    the same interface.
    
    TEST=changed the end_to_end_async_unittest to reproduce this bug, and confirm that the error messages were gone after fixing object_proxy.{cc,h}
    BUG=chromium-os:23382
    
    Review URL: http://codereview.chromium.org/8681002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111423 0039d316-1c4b-4281-b951-d872f2087c98

--- dbus/object_proxy.h
+++ dbus/object_proxy.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dbus/dbus.h>
 
 #include <map>
+#include <set>
 #include <string>
-#include <vector>
 
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
@@ -193,7 +193,7 @@ class ObjectProxy : public base::RefCountedThreadSafe<ObjectProxy> {
   typedef std::map<std::string, SignalCallback> MethodTable;
   MethodTable method_table_;
 
-  std::vector<std::string> match_rules_;
+  std::set<std::string> match_rules_;
 
   DISALLOW_COPY_AND_ASSIGN(ObjectProxy);
 };
