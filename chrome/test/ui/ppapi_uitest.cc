555392e9c30a9d74f07345300df7cc3f0d7a5952
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 02:32:51 +0000

    Add RunTestWithWebSocketServer for subsequent Pepper WebSocket API tests.
    BUG=87310
    TEST=n/a
    
    
    Review URL: http://codereview.chromium.org/8642001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ui/ppapi_uitest.cc
+++ chrome/test/ui/ppapi_uitest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/automation/tab_proxy.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/ui/ui_test.h"
 #include "net/base/net_util.h"
 #include "net/test/test_server.h"
@@ -105,6 +106,16 @@ class PPAPITestBase : public UITest {
     ASSERT_TRUE(test_server.Start());
     std::string query = BuildQuery("files/test_case.html?", test_case);
     RunTestURL(test_server.GetURL(query));
+  }
+
+  void RunTestWithWebSocketServer(const std::string& test_case) {
+    FilePath websocket_root_dir;
+    ASSERT_TRUE(
+        PathService::Get(chrome::DIR_LAYOUT_TESTS, &websocket_root_dir));
+
+    ui_test_utils::TestWebSocketServer server;
+    ASSERT_TRUE(server.Start(websocket_root_dir));
+    RunTest(test_case);
   }
 
  private:
