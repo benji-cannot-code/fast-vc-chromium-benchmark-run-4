41c176865a9eccbb86958314d236ca65e07458ba
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-23 13:15:41 +0000

    Add new Chromium tests to the flakiness dashboard https://bugs.webkit.org/show_bug.cgi?id=72997
    Patch by Ryan Sleevi <rsleevi@chromium.org> on 2011-11-23
    Reviewed by Ojan Vafai.
    
    * TestResultServer/static-dashboards/dashboard_base.js:
    ():
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
@@ -137,8 +137,8 @@ var TEST_TYPES = [
     'net_unittests',
     'printing_unittests',
     'remoting_unittests',
-    'sql_unittests',
     'safe_browsing_tests',
+    'sql_unittests',
     'sync_unit_tests',
     'sync_integration_tests',
     'test_shell_tests',
@@ -426,13 +426,17 @@ function currentBuilderGroup(opt_state)
     case 'layout-tests':
         return LAYOUT_TESTS_BUILDER_GROUPS[state.group]
         break;
-    case 'app_unittests':
+    case 'aura_unittests':
+    case 'aura_shell_unittests':
     case 'base_unittests':
     case 'browser_tests':
     case 'cacheinvalidation_unittests':
+    case 'compositor_unittests':
+    case 'content_unittests':
     case 'courgette_unittests':
     case 'crypto_unittests':
     case 'googleurl_unittests':
+    case 'gfx_unittests':
     case 'gpu_unittests':
     case 'installer_util_unittests':
     case 'interactive_ui_tests':
@@ -445,11 +449,13 @@ function currentBuilderGroup(opt_state)
     case 'printing_unittests':
     case 'remoting_unittests':
     case 'safe_browsing_tests':
+    case 'sql_unittests':
     case 'sync_unit_tests':
     case 'sync_integration_tests':
     case 'test_shell_tests':
     case 'ui_tests':
     case 'unit_tests':
+    case 'views_unittests':
         return G_TESTS_BUILDER_GROUP;
         break;
     default:
