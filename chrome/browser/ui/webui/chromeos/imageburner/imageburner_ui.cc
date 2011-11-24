5df3b2b15a9fcf755b5aa6f06b8c1a6acd9c4b2c
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-24 01:10:57 +0000

    Remove NewRunnableFunction in imageburner.
    BUG=chromium-os:22024
    TEST=compiles, tests run.
    
    Review URL: http://codereview.chromium.org/8667001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.cc
+++ chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.cc
@@ -4,17 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/webui/chromeos/imageburner/imageburner_ui.h"
-#include "chrome/browser/ui/webui/chromeos/imageburner/webui_handler.h"
+
+#include <string>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/i18n/rtl.h"
 #include "base/message_loop.h"
-#include "base/task.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/system/statistics_provider.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/chromeos/imageburner/webui_handler.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/time_format.h"
@@ -48,8 +50,8 @@ const char kMoreInfoLink[] =
 
 namespace {
 
-ChromeWebUIDataSource *CreateImageburnerUIHTMLSource() {
-  ChromeWebUIDataSource *source =
+ChromeWebUIDataSource* CreateImageburnerUIHTMLSource() {
+  ChromeWebUIDataSource* source =
       new ChromeWebUIDataSource(chrome::kChromeUIImageBurnerHost);
 
     source->AddLocalizedString("headerTitle", IDS_IMAGEBURN_HEADER_TITLE);
@@ -360,7 +362,7 @@ void WebUIHandler::HandleBurnImage(const ListValue* args) {
     scoped_refptr<WebUIHandlerTaskProxy> task = new WebUIHandlerTaskProxy(this);
     BrowserThread::PostTask(
         BrowserThread::FILE, FROM_HERE,
-        NewRunnableMethod(task.get(), &WebUIHandlerTaskProxy::CreateImageDir));
+        base::Bind(&WebUIHandlerTaskProxy::CreateImageDir, task.get()));
   } else {
     ImageDirCreatedOnUIThread(true);
   }
@@ -377,8 +379,8 @@ void WebUIHandler::OnImageDirCreated(bool success) {
   scoped_refptr<WebUIHandlerTaskProxy> task = new WebUIHandlerTaskProxy(this);
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
-      NewRunnableMethod(task.get(), &WebUIHandlerTaskProxy::OnImageDirCreated,
-                        success));
+      base::Bind(&WebUIHandlerTaskProxy::OnImageDirCreated,
+                 task.get(), success));
 }
 
 void WebUIHandler::ImageDirCreatedOnUIThread(bool success) {
