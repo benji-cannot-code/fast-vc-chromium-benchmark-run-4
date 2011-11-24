022e529405e5d64aebd356d76ec8d4d6e286ca00
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-24 01:40:52 +0000

    Kill orphaned WebSocker server processes before launching new one in POSIX, or use JobObject to kill child servers on quitting in Win32.
    Sometime, unexpected existing WebSocket servers cause continuous test failures
    because they prevent to launch new WebSocket server using the same port.
    
    This change kills orphaned WebSocket servers before launching new one in POSIX,
    or use JobObject to register the server processes must be killed after parent process quit.
    This mechanism works fine in net/test/test_server_{posix|win}.cc for HTTP servers.
    
    BUG=91058
    TEST=third_party/WebKit/Tools/Script/new-run-webkit-websocketserver --server=start; ui_tests --gtest_filter="*WorkerWebSocket*" --gtest_also_run_disabled_tests (ui_tests must run by killing existing WebSocket server)
    
    
    Review URL: http://codereview.chromium.org/8633004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/ui_test_utils.h"
 
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 #include <vector>
 
 #include "base/bind.h"
@@ -17,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
+#include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/automation/ui_controls.h"
@@ -62,6 +67,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_AURA)
 #include "ui/aura/desktop.h"
 #endif
+
+static const int kDefaultWsPort = 8880;
 
 namespace ui_test_utils {
 
@@ -728,7 +735,11 @@ void AppendToPythonPath(const FilePath& dir) {
 
 }  // anonymous namespace
 
-TestWebSocketServer::TestWebSocketServer() : started_(false) {
+TestWebSocketServer::TestWebSocketServer()
+    : started_(false) {
+#if defined(OS_POSIX)
+  process_handle_ = base::kNullProcessHandle;
+#endif
 }
 
 bool TestWebSocketServer::Start(const FilePath& root_directory) {
@@ -741,6 +752,7 @@ bool TestWebSocketServer::Start(const FilePath& root_directory) {
   cmd_line->AppendArg("--register_cygwin");
   cmd_line->AppendArgNative(FILE_PATH_LITERAL("--root=") +
                             root_directory.value());
+  cmd_line->AppendArg("--port=" + base::IntToString(kDefaultWsPort));
   if (!temp_dir_.CreateUniqueTempDir()) {
     LOG(ERROR) << "Unable to create a temporary directory.";
     return false;
@@ -749,9 +761,32 @@ bool TestWebSocketServer::Start(const FilePath& root_directory) {
   cmd_line->AppendArgNative(FILE_PATH_LITERAL("--pidfile=") +
                             websocket_pid_file_.value());
   SetPythonPath();
+
   base::LaunchOptions options;
+  base::ProcessHandle* process_handle = NULL;
+
+#if defined(OS_POSIX)
+  options.new_process_group = true;
+  process_handle = &process_handle_;
+#elif defined(OS_WIN)
+  job_handle_.Set(CreateJobObject(NULL, NULL));
+  if (!job_handle_.IsValid()) {
+    LOG(ERROR) << "Could not create JobObject.";
+    return false;
+  }
+
+  if (!base::SetJobObjectAsKillOnJobClose(job_handle_.Get())) {
+    LOG(ERROR) << "Could not SetInformationJobObject.";
+    return false;
+  }
+
+  options.inherit_handles = true;
+  options.job_handle = job_handle_.Get();
+#endif
+
+  // Launch a new WebSocket server process.
   options.wait = true;
-  if (!base::LaunchProcess(*cmd_line.get(), options, NULL)) {
+  if (!base::LaunchProcess(*cmd_line.get(), options, process_handle)) {
     LOG(ERROR) << "Unable to launch websocket server.";
     return false;
   }
@@ -806,6 +841,11 @@ TestWebSocketServer::~TestWebSocketServer() {
   base::LaunchOptions options;
   options.wait = true;
   base::LaunchProcess(*cmd_line.get(), options, NULL);
+
+#if defined(OS_POSIX)
+  // Just to make sure that the server process terminates certainly.
+  base::KillProcessGroup(process_handle_);
+#endif
 }
 
 TestNotificationObserver::TestNotificationObserver()
--- chrome/test/base/ui_test_utils.h
+++ chrome/test/base/ui_test_utils.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/message_loop.h"
+#include "base/process.h"
 #include "base/scoped_temp_dir.h"
 #include "base/string16.h"
 #include "chrome/browser/ui/view_ids.h"
@@ -28,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/gfx/native_widget_types.h"
 #include "webkit/glue/window_open_disposition.h"
+
+#if defined(OS_WIN)
+#include "base/win/scoped_handle.h"
+#endif
 
 class AppModalDialog;
 class BookmarkModel;
@@ -336,6 +341,14 @@ class TestWebSocketServer {
   // Used to close the same python interpreter when server falls out
   // scope.
   FilePath websocket_pid_file_;
+
+#if defined(OS_POSIX)
+  // ProcessHandle used to terminate child process.
+  base::ProcessHandle process_handle_;
+#elif defined(OS_WIN)
+  // JobObject used to clean up orphaned child process.
+  base::win::ScopedHandle job_handle_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(TestWebSocketServer);
 };
