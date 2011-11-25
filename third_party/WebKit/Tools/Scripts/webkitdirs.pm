a88ff553bd302fc6ed9c57dc3cb1baa781e84054
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-25 16:19:44 +0000

    [GTK] Integrate build-gtkdoc into build-webkit and make https://bugs.webkit.org/show_bug.cgi?id=72626
    Reviewed by Philippe Normand.
    
    .:
    
    * GNUmakefile.am: Remove references to old gtkdoc files. Update
    the dist-hook to include copying documentation into the distribution.
    This also means that the build will fail if documentation hasn't been
    generated before 'make dist.'
    * configure.ac: Ditto.
    
    Source/WebKit/gtk:
    
    * docs/GNUmakefile.am: Removed.
    * docs/version.xml.in: Removed.
    
    Source/WebKit2:
    
    * GNUmakefile.am: Removed references to build-gtkdoc and gtkdoc.py.
    * UIProcess/API/gtk/docs/build-gtkdoc: Removed.
    
    Tools:
    
    * GNUmakefile.am: Added support for new scripts and "make docs".
    * Scripts/webkitdirs.pm: Call generate-gtkdoc when building.
    (buildAutotoolsProject):
    * gtk/generate-gtkdoc: Added.
    * gtk/gtkdoc.py: Renamed from Source/WebKit2/UIProcess/API/gtk/docs/gtkdoc.py.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1680,6 +1680,14 @@ sub buildAutotoolsProject($@)
     }
 
     chdir ".." or die;
+
+    if (isGtk()) {
+        my $relativeScriptsPath = relativeScriptsDir();
+        if (system("$relativeScriptsPath/../gtk/generate-gtkdoc --skip-html")) {
+            die "\n gtkdoc did not build without warnings\n";
+        }
+    }
+
     return 0;
 }
 
