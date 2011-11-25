ab14e841f1073ae0bfacec1b130893587fc0e150
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-25 18:08:38 +0000

    StyleGeneratedImage should ref CSSImageGeneratorValue https://bugs.webkit.org/show_bug.cgi?id=73074
    Reviewed by Andreas Kling and Nikolas Zimmermann.
            
    RenderStyle owns a bunch of StyleImage objects. However StyleGeneratedImage does not ref the
    CSSImageGeneratorValue it holds so we currently rely on the stylesheet to keep the CSSImageGeneratorValues
    alive as long as RenderStyle stays alive. While this works (RenderStyles are thrown away if stylesheets
    change) it is not particularly robust or nice.
    
    - Use RefPtr<CSSImageGeneratorValue> in StyleGeneratedImage
    - Remove the RefPtr<StyleGeneratedImage> from CSSImageGeneratorValue.
      There is no good reason to cache StyleGeneratedImage as it is a small and
      uncommon object. With that the whole back-reference becomes unnecessary.
    - Switch more places to use (Pass)RefPtr<StyleImage> for consistency.
    
    * css/CSSImageGeneratorValue.cpp:
    (WebCore::CSSImageGeneratorValue::CSSImageGeneratorValue):
    * css/CSSImageGeneratorValue.h:
    * css/CSSStyleApplyProperty.cpp:
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    (WebCore::CSSStyleSelector::styleImage):
    (WebCore::CSSStyleSelector::cachedOrPendingFromValue):
    (WebCore::CSSStyleSelector::generatedOrPendingFromValue):
    (WebCore::CSSStyleSelector::loadPendingImage):
    (WebCore::CSSStyleSelector::loadPendingImages):
    * css/CSSStyleSelector.h:
    * rendering/style/FillLayer.h:
    (WebCore::FillLayer::setImage):
    * rendering/style/NinePieceImage.h:
    (WebCore::NinePieceImage::NinePieceImage):
    * rendering/style/StyleGeneratedImage.cpp:
    (WebCore::StyleGeneratedImage::StyleGeneratedImage):
    (WebCore::StyleGeneratedImage::cssValue):
    (WebCore::StyleGeneratedImage::imageSize):
    (WebCore::StyleGeneratedImage::addClient):
    (WebCore::StyleGeneratedImage::removeClient):
    (WebCore::StyleGeneratedImage::image):
    * rendering/style/StyleGeneratedImage.h:
    (WebCore::StyleGeneratedImage::create):
    (WebCore::StyleGeneratedImage::data):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSImageGeneratorValue.h
+++ third_party/WebKit/Source/WebCore/css/CSSImageGeneratorValue.h
@@ -37,8 +37,6 @@ namespace WebCore {
 class CachedResourceLoader;
 class Image;
 class RenderObject;
-class StyleGeneratedImage;
-class StyleImage;
 
 struct SizeAndCount {
     SizeAndCount(IntSize newSize = IntSize(), int newCount = 0)
@@ -61,9 +59,6 @@ public:
     void removeClient(RenderObject*);
     PassRefPtr<Image> image(RenderObject*, const IntSize&);
 
-    StyleImage* generatedOrPendingImage();
-    StyleGeneratedImage* generatedImage();
-
     bool isFixedSize() const;
     IntSize fixedSize(const RenderObject*);
 
@@ -77,9 +72,6 @@ protected:
     Image* getImage(RenderObject*, const IntSize&);
     void putImage(const IntSize&, PassRefPtr<Image>);
     const RenderObjectSizeCountMap& clients() const { return m_clients; }
-
-    RefPtr<StyleImage> m_image;
-    bool m_accessedImage;
 
     HashCountedSet<IntSize> m_sizes; // A count of how many times a given image size is in use.
     RenderObjectSizeCountMap m_clients; // A map from RenderObjects (with entry count) to image sizes.
--- third_party/WebKit/Source/WebCore/rendering/style/StyleGeneratedImage.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleGeneratedImage.cpp
@@ -30,16 +30,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderObject.h"
 
 namespace WebCore {
+    
+StyleGeneratedImage::StyleGeneratedImage(PassRefPtr<CSSImageGeneratorValue> value)
+    : m_imageGeneratorValue(value)  
+    , m_fixedSize(m_imageGeneratorValue->isFixedSize())
+{
+    m_isGeneratedImage = true;
+}
 
 PassRefPtr<CSSValue> StyleGeneratedImage::cssValue() const
 {
-    return m_generator;
+    return m_imageGeneratorValue;
 }
 
 IntSize StyleGeneratedImage::imageSize(const RenderObject* renderer, float multiplier) const
 {
     if (m_fixedSize) {
-        IntSize fixedSize = m_generator->fixedSize(renderer);
+        IntSize fixedSize = m_imageGeneratorValue->fixedSize(renderer);
         if (multiplier == 1.0f)
             return fixedSize;
 
@@ -69,18 +76,18 @@ void StyleGeneratedImage::computeIntrinsicDimensions(const RenderObject* rendere
 
 void StyleGeneratedImage::addClient(RenderObject* renderer)
 {
-    m_generator->addClient(renderer, IntSize());
+    m_imageGeneratorValue->addClient(renderer, IntSize());
 }
 
 void StyleGeneratedImage::removeClient(RenderObject* renderer)
 {
-    m_generator->removeClient(renderer);
+    m_imageGeneratorValue->removeClient(renderer);
 }
 
 PassRefPtr<Image> StyleGeneratedImage::image(RenderObject* renderer, const IntSize& size) const
 {
     renderer->document()->styleSelector()->setStyle(renderer->style());
-    return m_generator->image(renderer, size);
+    return m_imageGeneratorValue->image(renderer, size);
 }
 
 }
