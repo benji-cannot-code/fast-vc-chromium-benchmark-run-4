3e67d4154846a5bedff15b3f2c969ad57b59a940
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-25 15:08:35 +0000

    Revert r111596 "WebSocket Pepper API: in process API implementation"
    Broke compile on Linux Builder (dbg)(shared)
    
    TBR=toyoshim@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8698007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111598 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_websocket.h
+++ ppapi/tests/test_websocket.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/tests/test_case.h"
 
-struct PPB_Core;
-struct PPB_Var;
 struct PPB_WebSocket_Dev;
 
 class TestWebSocket : public TestCase {
@@ -23,21 +21,11 @@ class TestWebSocket : public TestCase {
   virtual void RunTests(const std::string& filter);
 
  private:
-  PP_Var CreateVar(const char* string);
-  void ReleaseVar(const PP_Var& var);
-  bool AreEqual(const PP_Var& var, const char* string);
-
-  PP_Resource Connect();
-
+  std::string TestCreate();
   std::string TestIsWebSocket();
-  std::string TestInvalidConnect();
-  std::string TestValidConnect();
-  std::string TestTextSendReceive();
 
   // Used by the tests that access the C API directly.
   const PPB_WebSocket_Dev* websocket_interface_;
-  const PPB_Var* var_interface_;
-  const PPB_Core* core_interface_;
 };
 
 #endif  // PAPPI_TESTS_TEST_WEBSOCKET_H_
