86dff6083a5cde5c6bf7425a8c7e5d5d65ba002e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-27 01:26:19 +0000

    Needs WTF_INLINE and JS_INLINE https://bugs.webkit.org/show_bug.cgi?id=72853
    Patch by Hajime Morrita <morrita@chromium.org> on 2011-11-26
    Reviewed by Kevin Ollivier.
    
    Added WTF_HIDDEN, WTF_INLINE and JS_INLINE which
    indirect __attribute__((visibility("hidden"))
    
    * config.h:
    * wtf/ExportMacros.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/ExportMacros.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/ExportMacros.h
@@ -36,12 +36,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !PLATFORM(CHROMIUM) && OS(WINDOWS) && !COMPILER(GCC)
 #define WTF_EXPORT __declspec(dllexport)
 #define WTF_IMPORT __declspec(dllimport)
+#define WTF_HIDDEN
 #elif defined(__GNUC__) && !defined(__CC_ARM) && !defined(__ARMCC__)
 #define WTF_EXPORT __attribute__((visibility("default")))
 #define WTF_IMPORT WTF_EXPORT
+#define WTF_HIDDEN __attribute__((visibility("hidden")))
 #else
 #define WTF_EXPORT
 #define WTF_IMPORT
+#define WTF_HIDDEN
 #endif
 
 #endif /* ExportMacros_h */
