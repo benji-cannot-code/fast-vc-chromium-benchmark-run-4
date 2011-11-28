6f6b6d96744ace96f0e614fc1ec1885bb8a912f6
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-28 20:04:45 +0000

    Share ShowPageInfoBubble() function declaration between cocoa and gtk.
    R=erg@chromium.org,rsesek@chromium.org
    
    Review URL: http://codereview.chromium.org/8682020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/page_info_bubble.h
+++ chrome/browser/ui/page_info_bubble.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_COCOA_PAGE_INFO_WINDOW_H_
-#define CHROME_BROWSER_UI_COCOA_PAGE_INFO_WINDOW_H_
+#ifndef CHROME_BROWSER_UI_PAGE_INFO_BUBBLE_H_
+#define CHROME_BROWSER_UI_PAGE_INFO_BUBBLE_H_
 #pragma once
 
 #include "content/browser/tab_contents/navigation_entry.h"
@@ -23,4 +23,4 @@ void ShowPageInfoBubble(gfx::NativeWindow parent,
 
 }  // namespace browser
 
-#endif  // CHROME_BROWSER_UI_COCOA_PAGE_INFO_WINDOW_H_
+#endif  // CHROME_BROWSER_UI_PAGE_INFO_BUBBLE_H_
