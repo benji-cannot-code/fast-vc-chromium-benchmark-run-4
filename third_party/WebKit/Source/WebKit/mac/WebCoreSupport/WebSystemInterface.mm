1c548aa4d3d486f00ee3c3509dce41978e280b0a
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-28 11:01:26 +0000

    Fix chromium canary build after r101215. Unreviewed. Build fix.
    * WebCoreSupport/WebSystemInterface.mm:
    (InitWebCoreSystemInterface):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
+++ third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
@@ -167,7 +167,7 @@ void InitWebCoreSystemInterface(void)
     INIT(CreateVMPressureDispatchOnMainQueue);
 #endif
 
-#if !defined(BUILDING_ON_SNOW_LEOPARD) && !defined(BUILDING_ON_LION)
+#if PLATFORM(MAC) && !defined(BUILDING_ON_SNOW_LEOPARD) && !defined(BUILDING_ON_LION)
     INIT(GetMacOSXVersionString);
 #endif
 
