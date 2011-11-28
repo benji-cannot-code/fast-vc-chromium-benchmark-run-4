c6c4b4481809b7fea3b8afa483827d6b39aaf01c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-28 23:07:26 +0000

    Don't try to optimize huge code blocks https://bugs.webkit.org/show_bug.cgi?id=73187
    Reviewed by Oliver Hunt.
            
    This unifies the heuristics used for deciding if a code block is too big
    to optimize, and sets this heuristic to 1000, which is intuitively better
    than numeric_limits<unsigned>::max(). It also results in what looks like
    a speed-up on both SunSpider and V8 (in Tools/Scripts/bencher).
    
    * dfg/DFGCapabilities.h:
    (JSC::DFG::mightCompileEval):
    (JSC::DFG::mightCompileProgram):
    (JSC::DFG::mightCompileFunctionForCall):
    (JSC::DFG::mightCompileFunctionForConstruct):
    * runtime/Heuristics.cpp:
    (JSC::Heuristics::initializeHeuristics):
    * runtime/Heuristics.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Heuristics.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Heuristics.cpp
@@ -45,10 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC { namespace Heuristics {
 
-unsigned maximumEvalOptimizationCandidateInstructionCount;
-unsigned maximumProgramOptimizationCandidateInstructionCount;
-unsigned maximumFunctionForCallOptimizationCandidateInstructionCount;
-unsigned maximumFunctionForConstructOptimizationCandidateInstructionCount;
+unsigned maximumOptimizationCandidateInstructionCount;
 
 unsigned maximumFunctionForCallInlineCandidateInstructionCount;
 unsigned maximumFunctionForConstructInlineCandidateInstructionCount;
@@ -127,13 +124,7 @@ void setHeuristic(T& variable, const char* name, U value)
 
 void initializeHeuristics()
 {
-    // FIXME: Must revisit these heuristics! The DFG, being an optimizing compiler, may
-    // take a long time for pathologically huge code blocks. The best way to cope with
-    // this is to refuse to optimize them.
-    SET(maximumEvalOptimizationCandidateInstructionCount,                 std::numeric_limits<unsigned>::max());
-    SET(maximumProgramOptimizationCandidateInstructionCount,              std::numeric_limits<unsigned>::max());
-    SET(maximumFunctionForCallOptimizationCandidateInstructionCount,      std::numeric_limits<unsigned>::max());
-    SET(maximumFunctionForConstructOptimizationCandidateInstructionCount, std::numeric_limits<unsigned>::max());
+    SET(maximumOptimizationCandidateInstructionCount, 1000);
     
     SET(maximumFunctionForCallInlineCandidateInstructionCount, 150);
     SET(maximumFunctionForConstructInlineCandidateInstructionCount, 80);
--- third_party/WebKit/Source/JavaScriptCore/runtime/Heuristics.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Heuristics.h
@@ -31,10 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC { namespace Heuristics {
 
-extern unsigned maximumEvalOptimizationCandidateInstructionCount;
-extern unsigned maximumProgramOptimizationCandidateInstructionCount;
-extern unsigned maximumFunctionForCallOptimizationCandidateInstructionCount;
-extern unsigned maximumFunctionForConstructOptimizationCandidateInstructionCount;
+extern unsigned maximumOptimizationCandidateInstructionCount;
 
 extern unsigned maximumFunctionForCallInlineCandidateInstructionCount;
 extern unsigned maximumFunctionForConstructInlineCandidateInstructionCount;
