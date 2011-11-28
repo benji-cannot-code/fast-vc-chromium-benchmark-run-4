3154a765a114dc501ff98fc2ec3aed2d88b37f71
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-28 15:44:51 +0000

    Web Inspector: resource status image is mis-aligned in the network headers view https://bugs.webkit.org/show_bug.cgi?id=73211
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/ResourceHeadersView.js:
    (WebInspector.ResourceHeadersView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourceHeadersView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourceHeadersView.js
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ResourceHeadersView = function(resource)
 {
     WebInspector.View.call(this);
+    this.registerRequiredCSS("resourceView.css");
     this.element.addStyleClass("resource-headers-view");
 
     this._resource = resource;
