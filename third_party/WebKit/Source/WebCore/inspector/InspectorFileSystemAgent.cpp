9732ec4302ae38efe4302d4a2b63715616e8f64e
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-28 12:04:28 +0000

    Fix build error on Chromium/Windows. https://bugs.webkit.org/show_bug.cgi?id=73200
    Unreviewed build fix.
    
    Patch by Taiju TSUIKI <tzik@chromium.org> on 2011-11-28
    
    * inspector/InspectorFileSystemAgent.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorFileSystemAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorFileSystemAgent.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(INSPECTOR) && ENABLE(FILE_SYSTEM)
 
+#include "DOMFileSystem.h"
 #include "InspectorState.h"
 #include "InstrumentingAgents.h"
 
