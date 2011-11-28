da0f18f571cf1584e8f5d4887bfb2f05f06544d1
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-28 17:55:28 +0000

    Add OVERRIDE to webkit/.
    BUG=104314
    TEST=no change
    
    Review URL: http://codereview.chromium.org/8662021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111719 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/chromeos/fileapi/cros_mount_point_provider.h
+++ webkit/chromeos/fileapi/cros_mount_point_provider.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/file_path.h"
 #include "base/synchronization/lock.h"
 #include "webkit/fileapi/file_system_mount_point_provider.h"
@@ -46,7 +47,7 @@ class CrosMountPointProvider
       const GURL& origin_url,
       fileapi::FileSystemType type,
       const FilePath& virtual_path,
-      bool create);
+      bool create) OVERRIDE;
   virtual bool IsRestrictedFileName(const FilePath& filename) const OVERRIDE;
   virtual std::vector<FilePath> GetRootDirectories() const OVERRIDE;
   virtual fileapi::FileSystemFileUtil* GetFileUtil() OVERRIDE;
