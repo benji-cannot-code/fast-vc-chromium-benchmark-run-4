4b263779301b844827b5238530d4d0bb020df0c9
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-28 18:02:02 +0000

    Fix a regression in chrome://gpucrash etc.
    So we won't have a GpuUIHost with id 0.  It starts from 1.
    
    BUG=105044
    TEST=GPUCrashTest.Kill on GPU bots
    Review URL: http://codereview.chromium.org/8680035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111721 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host_ui_shim.cc
+++ content/browser/gpu/gpu_process_host_ui_shim.cc
@@ -132,6 +132,15 @@ GpuProcessHostUIShim* GpuProcessHostUIShim::FromID(int host_id) {
   return g_hosts_by_id.Pointer()->Lookup(host_id);
 }
 
+// static
+GpuProcessHostUIShim* GpuProcessHostUIShim::GetOneInstance() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  if (g_hosts_by_id.Pointer()->IsEmpty())
+    return NULL;
+  IDMap<GpuProcessHostUIShim>::iterator it(g_hosts_by_id.Pointer());
+  return it.GetCurrentValue();
+}
+
 bool GpuProcessHostUIShim::Send(IPC::Message* msg) {
   DCHECK(CalledOnValidThread());
   return BrowserThread::PostTask(BrowserThread::IO,
