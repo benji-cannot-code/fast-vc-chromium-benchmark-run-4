dc665d6773700b223881de9e2c5675ebf58987d0
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 04:40:20 +0000

    Revert 111865 - Remove several more custom Task implementations from base/
    BUG=none
    TEST=trybots
    
    Review URL: http://codereview.chromium.org/8702016
    
    TBR=dcheng@chromium.org
    Review URL: http://codereview.chromium.org/8729020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111868 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/object_watcher.h
+++ base/win/object_watcher.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/base_export.h"
-#include "base/memory/weak_ptr.h"
 #include "base/message_loop.h"
 
 namespace base {
@@ -81,17 +80,12 @@ class BASE_EXPORT ObjectWatcher : public MessageLoop::DestructionObserver {
   // Called on a background thread when done waiting.
   static void CALLBACK DoneWaiting(void* param, BOOLEAN timed_out);
 
-  void Signal();
-
   // MessageLoop::DestructionObserver implementation:
   virtual void WillDestroyCurrentMessageLoop();
 
   // Internal state.
-  base::WeakPtrFactory<ObjectWatcher> weak_factory_;
-  HANDLE object_;             // The object being watched
-  HANDLE wait_object_;        // Returned by RegisterWaitForSingleObject
-  MessageLoop* origin_loop_;  // Used to get back to the origin thread
-  Delegate* delegate_;        // Delegate to notify when signaled
+  struct Watch;
+  Watch* watch_;
 
   DISALLOW_COPY_AND_ASSIGN(ObjectWatcher);
 };
