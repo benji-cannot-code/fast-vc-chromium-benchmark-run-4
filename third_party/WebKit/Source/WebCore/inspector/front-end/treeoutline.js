bd6d07f8b021a497083f4777093ef7661b0463cf
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-29 02:34:22 +0000

    Skip selecting TreeElements that are hidden when keyboard navigating.
    We already skipped non-selectable tree elements in the common cases, this just makes selectable
    take the hidden flag into account.
    
    Reviewed by Brian Weinstein.
    
    * inspector/front-end/treeoutline.js:
    (TreeOutline.prototype._treeKeyDown): Skip non-selectable elements when pressing Left or Right,
    similar to what we already do when pressing Up and Down.
    (TreeElement): Set _selectable to true.
    (TreeElement.prototype.get selectable): Added. Return false when hidden.
    (TreeElement.prototype.set selectable): Added. Set _selectable.
    (TreeElement.prototype.traverseNextTreeElement): Renamed skipHidden to skipUnrevealed to prevent confusion with
    the hidden property of TreeElement.
    (TreeElement.prototype.traversePreviousTreeElement): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/treeoutline.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/treeoutline.js
@@ -379,6 +379,8 @@ TreeOutline.prototype._treeKeyDown = function(event)
             handled = true;
             if (this.selectedTreeElement.parent.selectable) {
                 nextSelectedElement = this.selectedTreeElement.parent;
+                while (nextSelectedElement && !nextSelectedElement.selectable)
+                    nextSelectedElement = nextSelectedElement.parent;
                 handled = nextSelectedElement ? true : false;
             } else if (this.selectedTreeElement.parent)
                 this.selectedTreeElement.parent.collapse();
@@ -391,6 +393,8 @@ TreeOutline.prototype._treeKeyDown = function(event)
             handled = true;
             if (this.selectedTreeElement.expanded) {
                 nextSelectedElement = this.selectedTreeElement.children[0];
+                while (nextSelectedElement && !nextSelectedElement.selectable)
+                    nextSelectedElement = nextSelectedElement.nextSibling;
                 handled = nextSelectedElement ? true : false;
             } else {
                 if (event.altKey)
@@ -469,6 +473,7 @@ function TreeElement(title, representedObject, hasChildren)
     }
 
     this._hidden = false;
+    this._selectable = true;
     this.expanded = false;
     this.selected = false;
     this.hasChildren = hasChildren;
@@ -481,8 +486,17 @@ function TreeElement(title, representedObject, hasChildren)
 }
 
 TreeElement.prototype = {
-    selectable: true,
     arrowToggleWidth: 10,
+
+    get selectable() {
+        if (this._hidden)
+            return false;
+        return this._selectable;
+    },
+
+    set selectable(x) {
+        this._selectable = x;
+    },
 
     get listItemElement() {
         return this._listItemNode;
@@ -889,13 +903,13 @@ TreeElement.prototype.onpopulate = function()
 }
 
 /**
- * @param {boolean} skipHidden
+ * @param {boolean} skipUnrevealed
  * @param {(TreeOutline|TreeElement)=} stayWithin
  * @param {boolean=} dontPopulate
  * @param {Object=} info
  * @return {TreeElement}
  */
-TreeElement.prototype.traverseNextTreeElement = function(skipHidden, stayWithin, dontPopulate, info)
+TreeElement.prototype.traverseNextTreeElement = function(skipUnrevealed, stayWithin, dontPopulate, info)
 {
     if (!dontPopulate && this.hasChildren)
         this.onpopulate();
@@ -903,8 +917,8 @@ TreeElement.prototype.traverseNextTreeElement = function(skipHidden, stayWithin,
     if (info)
         info.depthChange = 0;
 
-    var element = skipHidden ? (this.revealed() ? this.children[0] : null) : this.children[0];
-    if (element && (!skipHidden || (skipHidden && this.expanded))) {
+    var element = skipUnrevealed ? (this.revealed() ? this.children[0] : null) : this.children[0];
+    if (element && (!skipUnrevealed || (skipUnrevealed && this.expanded))) {
         if (info)
             info.depthChange = 1;
         return element;
@@ -913,12 +927,12 @@ TreeElement.prototype.traverseNextTreeElement = function(skipHidden, stayWithin,
     if (this === stayWithin)
         return null;
 
-    element = skipHidden ? (this.revealed() ? this.nextSibling : null) : this.nextSibling;
+    element = skipUnrevealed ? (this.revealed() ? this.nextSibling : null) : this.nextSibling;
     if (element)
         return element;
 
     element = this;
-    while (element && !element.root && !(skipHidden ? (element.revealed() ? element.nextSibling : null) : element.nextSibling) && element.parent !== stayWithin) {
+    while (element && !element.root && !(skipUnrevealed ? (element.revealed() ? element.nextSibling : null) : element.nextSibling) && element.parent !== stayWithin) {
         if (info)
             info.depthChange -= 1;
         element = element.parent;
@@ -927,24 +941,24 @@ TreeElement.prototype.traverseNextTreeElement = function(skipHidden, stayWithin,
     if (!element)
         return null;
 
-    return (skipHidden ? (element.revealed() ? element.nextSibling : null) : element.nextSibling);
+    return (skipUnrevealed ? (element.revealed() ? element.nextSibling : null) : element.nextSibling);
 }
 
 /**
- * @param {boolean} skipHidden
+ * @param {boolean} skipUnrevealed
  * @param {boolean=} dontPopulate
  * @return {TreeElement}
  */
-TreeElement.prototype.traversePreviousTreeElement = function(skipHidden, dontPopulate)
+TreeElement.prototype.traversePreviousTreeElement = function(skipUnrevealed, dontPopulate)
 {
-    var element = skipHidden ? (this.revealed() ? this.previousSibling : null) : this.previousSibling;
+    var element = skipUnrevealed ? (this.revealed() ? this.previousSibling : null) : this.previousSibling;
     if (!dontPopulate && element && element.hasChildren)
         element.onpopulate();
 
-    while (element && (skipHidden ? (element.revealed() && element.expanded ? element.children[element.children.length - 1] : null) : element.children[element.children.length - 1])) {
+    while (element && (skipUnrevealed ? (element.revealed() && element.expanded ? element.children[element.children.length - 1] : null) : element.children[element.children.length - 1])) {
         if (!dontPopulate && element.hasChildren)
             element.onpopulate();
-        element = (skipHidden ? (element.revealed() && element.expanded ? element.children[element.children.length - 1] : null) : element.children[element.children.length - 1]);
+        element = (skipUnrevealed ? (element.revealed() && element.expanded ? element.children[element.children.length - 1] : null) : element.children[element.children.length - 1]);
     }
 
     if (element)
