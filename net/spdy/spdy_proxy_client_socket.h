33f412e2cfddb422800df4c43ab19c156bf68f3f
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 23:39:18 +0000

    Change SpdyProxyClientSocket::OnClose to not call the write callback if this has been deleted.
    BUG=105285
    TEST=SpdyProxyClientSocket.Rst\*
    
    Review URL: http://codereview.chromium.org/8729007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112068 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_proxy_client_socket.h
+++ net/spdy/spdy_proxy_client_socket.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "net/base/completion_callback.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_log.h"
@@ -160,6 +161,8 @@ class NET_EXPORT_PRIVATE SpdyProxyClientSocket : public ProxyClientSocket,
   bool was_ever_used_;
 
   scoped_ptr<SpdyHttpStream> response_stream_;
+
+  base::WeakPtrFactory<SpdyProxyClientSocket> weak_factory_;
 
   const BoundNetLog net_log_;
 
