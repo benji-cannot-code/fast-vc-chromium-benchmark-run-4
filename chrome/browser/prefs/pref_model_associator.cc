99dc44cc4bfe1e60758770f45ebc3126138e6eb3
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 12:56:56 +0000

    The dictionary preference prefs::kContentSettingsPatternPairs should be merged when it is synced.
    This prevents local content settings patterns (exceptions) from being overwritten when sync is turned on and a previously synced version of the preferences is fetched.
    
    BUG=105695
    TEST=syncing content settings works
    
    Review URL: http://codereview.chromium.org/8718016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/pref_model_associator.cc
+++ chrome/browser/prefs/pref_model_associator.cc
@@ -176,7 +176,8 @@ Value* PrefModelAssociator::MergePreference(
   }
 
   if (name == prefs::kContentSettingsPatterns ||
-      name == prefs::kGeolocationContentSettings) {
+      name == prefs::kGeolocationContentSettings ||
+      name == prefs::kContentSettingsPatternPairs) {
     return MergeDictionaryValues(*local_pref.GetValue(), server_value);
   }
 
