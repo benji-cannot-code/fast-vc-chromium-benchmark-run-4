e3c8e63091317cab35924b2e3247899b45093dff
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-29 20:37:24 +0000

    Crash in IsolateTracker::addFakeRunIfNecessary(), preceded by assertion failure (m_nestedIsolateCount >= 1) in IsolateTracker::exitIsolate() https://bugs.webkit.org/show_bug.cgi?id=69275
    Reviewed by Eric Seidel.
    
    Source/WebCore: 
    
    The crash was caused by our false assumption that at most one isolated container exists between the start
    and the root when appending a new run. Fixed the crash by computing the actual number of isolated containers
    between the start and the root.
    
    Test: fast/text/nested-bidi-isolate-crash.html
    
    * rendering/InlineIterator.h:
    (WebCore::numberOfIsolateAncestors):
    (WebCore::IsolateTracker::IsolateTracker):
    (WebCore::InlineBidiResolver::appendRun):
    
    LayoutTests: 
    
    Add a regression test.
    
    * fast/text/nested-bidi-isolate-crash-expected.txt: Added.
    * fast/text/nested-bidi-isolate-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/InlineIterator.h
+++ third_party/WebKit/Source/WebCore/rendering/InlineIterator.h
@@ -407,6 +407,18 @@ static inline RenderObject* containingIsolate(RenderObject* object, RenderObject
     return 0;
 }
 
+static inline unsigned numberOfIsolateAncestors(RenderObject* object, RenderObject* root)
+{
+    ASSERT(object);
+    unsigned count = 0;
+    while (object && object != root) {
+        if (isIsolatedInline(object))
+            count++;
+        object = object->parent();
+    }
+    return count;
+}
+
 // FIXME: This belongs on InlineBidiResolver, except it's a template specialization
 // of BidiResolver which knows nothing about RenderObjects.
 static inline void addPlaceholderRunForIsolatedInline(InlineBidiResolver& resolver, RenderObject* isolatedInline)
@@ -421,8 +433,8 @@ static inline void addPlaceholderRunForIsolatedInline(InlineBidiResolver& resolv
 
 class IsolateTracker {
 public:
-    explicit IsolateTracker(bool inIsolate)
-        : m_nestedIsolateCount(inIsolate ? 1 : 0)
+    explicit IsolateTracker(unsigned nestedIsolateCount)
+        : m_nestedIsolateCount(nestedIsolateCount)
         , m_haveAddedFakeRunForRootIsolate(false)
     {
     }
@@ -471,7 +483,7 @@ inline void InlineBidiResolver::appendRun()
         // Keep track of when we enter/leave "unicode-bidi: isolate" inlines.
         // Initialize our state depending on if we're starting in the middle of such an inline.
         // FIXME: Could this initialize from this->inIsolate() instead of walking up the render tree?
-        IsolateTracker isolateTracker(containingIsolate(m_sor.m_obj, m_sor.root()));
+        IsolateTracker isolateTracker(numberOfIsolateAncestors(m_sor.m_obj, m_sor.root()));
         int start = m_sor.m_pos;
         RenderObject* obj = m_sor.m_obj;
         while (obj && obj != m_eor.m_obj && obj != endOfLine.m_obj) {
