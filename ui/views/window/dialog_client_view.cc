1dccad3f5b816e4d95314240266ea746c684b6f0
benrg@chromium.org <benrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 02:30:27 +0000

    Quick, hopefully temporary, fix for chromium issue 105609 and chromium-os issue 23461.
    BUG=105609
    TEST=dialog colors should be correct again
    
    
    Review URL: http://codereview.chromium.org/8725010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111842 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/window/dialog_client_view.cc
+++ ui/views/window/dialog_client_view.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <uxtheme.h>
 #include <vsstyle.h>
+#elif defined(TOOLKIT_USES_GTK)
+#include <gtk/gtk.h>
 #endif
 
 #include <algorithm>
@@ -305,8 +307,19 @@ const DialogClientView* DialogClientView::AsDialogClientView() const {
 // DialogClientView, View overrides:
 
 void DialogClientView::OnPaint(gfx::Canvas* canvas) {
+#if defined(TOOLKIT_USES_GTK)
+  // TODO(benrg): Unfortunately, GetSystemColor often returns the wrong color
+  // under GTK right now. This is meant to be a temporary fix. See related TODO
+  // in ui/gfx/native_theme_gtk.cc.
+  GtkWidget* widget = GetWidget()->GetNativeView();
+  if (!GTK_IS_WINDOW(widget))
+    return;
+  SkColor bg_color = gfx::GdkColorToSkColor(
+                         gtk_widget_get_style(widget)->bg[GTK_STATE_NORMAL]);
+#else
   SkColor bg_color = gfx::NativeTheme::instance()->GetSystemColor(
                          gfx::NativeTheme::kColorId_DialogBackground);
+#endif
   canvas->FillRect(bg_color, GetLocalBounds());
 }
 
