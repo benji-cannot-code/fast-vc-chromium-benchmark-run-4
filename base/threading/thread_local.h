526d134fde26e1f493475d75c4f01a52f4def189
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 20:59:25 +0000

    Allow const pointers to be stored in ThreadLocalPointer.
    Review URL: http://codereview.chromium.org/8666010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112021 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread_local.h
+++ base/threading/thread_local.h
@@ -94,7 +94,8 @@ class ThreadLocalPointer {
   }
 
   void Set(Type* ptr) {
-    internal::ThreadLocalPlatform::SetValueInSlot(slot_, ptr);
+    internal::ThreadLocalPlatform::SetValueInSlot(
+        slot_, const_cast<void*>(static_cast<const void*>(ptr)));
   }
 
  private:
