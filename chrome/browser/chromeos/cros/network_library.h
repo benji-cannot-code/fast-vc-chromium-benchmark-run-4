8f67a4be629dd056a3d05f51c0baab3972e97ce8
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 14:14:45 +0000

    Add ui data property in network library.
    BUG=chromium-os:21992
    TEST=compiles and passes tests.
    
    
    Review URL: http://codereview.chromium.org/8488008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111931 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.h
+++ chrome/browser/chromeos/cros/network_library.h
@@ -136,6 +136,7 @@ enum PropertyIndex {
   PROPERTY_INDEX_SUPPORT_NETWORK_SCAN,
   PROPERTY_INDEX_TECHNOLOGY_FAMILY,
   PROPERTY_INDEX_TYPE,
+  PROPERTY_INDEX_UI_DATA,
   PROPERTY_INDEX_UNKNOWN,
   PROPERTY_INDEX_USAGE_URL,
   PROPERTY_INDEX_OLP,
@@ -699,6 +700,7 @@ class Network {
                                         std::string* dest);
 
   void set_unique_id(const std::string& unique_id) { unique_id_ = unique_id; }
+  DictionaryValue* ui_data() { return &ui_data_; }
 
  private:
   // This allows NetworkParser and its subclasses access to device
@@ -785,6 +787,11 @@ class Network {
   // These must not be modified after construction.
   std::string service_path_;
   ConnectionType type_;
+
+  // UI-level state that is opaque to the connection manager. It's kept in a
+  // DictionaryValue to allow for simple addition of new settings. The value is
+  // stored in JSON-serialized from in the connection manager.
+  DictionaryValue ui_data_;
 
   // This is the parser we use to parse messages from the native
   // network layer.
