27d4f15bcb442990acce3d1c619e2d8ec934374f
rnk@chromium.org <rnk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 20:37:10 +0000

    Fix an uninitialized memory read in PanelMouseWatcherTest.StartStopWatching.
    BUG=105722
    
    Review URL: http://codereview.chromium.org/8664034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112019 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_mouse_watcher_unittest.cc
+++ chrome/browser/ui/panels/panel_mouse_watcher_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TestMouseObserver : public PanelMouseWatcherObserver {
  public:
+  TestMouseObserver() : mouse_movements_(0) {}
   // Overridden from PanelMouseWatcherObserver:
   virtual void OnMouseMove(const gfx::Point& mouse_position) OVERRIDE {
     ++mouse_movements_;
