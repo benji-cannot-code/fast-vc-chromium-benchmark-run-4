9664ba49d2fcdcc66b5dcba86b02e2e35c57cc84
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 18:18:14 +0000

    Remove JingleSessionManager::set_allow_local_ips().
    Currently allow_local_ips is always set to true, and there is no reason
    not to allow local IP addresses.
    
    Review URL: http://codereview.chromium.org/8728011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111973 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host.cc
+++ remoting/host/chromoting_host.cc
@@ -258,8 +258,6 @@ void ChromotingHost::OnStateChange(
     // Create and start session manager.
     protocol::JingleSessionManager* server =
         new protocol::JingleSessionManager(context_->network_message_loop());
-    // TODO(ajwong): Make this a command switch when we're more stable.
-    server->set_allow_local_ips(true);
 
     // Assign key and certificate to server.
     HostKeyPair key_pair;
