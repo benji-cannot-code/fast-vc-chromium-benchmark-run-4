aeb064b3c8cedd11162a59c9fffedc2d4ab9c810
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-29 21:29:18 +0000

    Use contentsToRootView when converting the mouse coordinates for the context menu key event https://bugs.webkit.org/show_bug.cgi?id=73352
    Reviewed by Adam Roben.
    
    No new tests: Already covered by existing tests.
    
    This is another step towards fixing https://bugs.webkit.org/show_bug.cgi?id=71945, by getting
    rid of a call to ScrollView::contentsToWindow.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::sendContextMenuEventForKey):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -2344,8 +2344,8 @@ bool EventHandler::sendContextMenuEventForKey()
 
     m_frame->view()->setCursor(pointerCursor());
 
-    IntPoint position = view->contentsToWindow(location);
-    IntPoint globalPosition = view->contentsToScreen(IntRect(location, IntSize())).location();
+    IntPoint position = view->contentsToRootView(location);
+    IntPoint globalPosition = view->hostWindow()->rootViewToScreen(IntRect(position, IntSize())).location();
 
     Node* targetNode = doc->focusedNode();
     if (!targetNode)
