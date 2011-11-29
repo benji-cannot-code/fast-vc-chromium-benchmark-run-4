4a91528a24068b2682a2267a92e166c4212d9faf
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 16:29:15 +0000

    The root of problem is x button or alt+f4 didn't close the window completely. There are still something in the pendingdialog list need to be cleaned.
    BUG=105311
    TEST=settings->under the hood->Manage certificates->import; close the dialog
    by x button or alt+f4; click import button again; verify if the dialog shows
    up again.
    
    
    Review URL: http://codereview.chromium.org/8692006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_dialog.cc
+++ chrome/browser/ui/views/extensions/extension_dialog.cc
@@ -114,9 +114,6 @@ void ExtensionDialog::Close() {
   if (!window_)
     return;
 
-  if (observer_)
-    observer_->ExtensionDialogIsClosing(this);
-
   window_->Close();
   window_ = NULL;
 }
@@ -138,6 +135,11 @@ bool ExtensionDialog::ShouldShowWindowTitle() const {
 
 string16 ExtensionDialog::GetWindowTitle() const {
   return window_title_;
+}
+
+void ExtensionDialog::WindowClosing() {
+  if (observer_)
+    observer_->ExtensionDialogIsClosing(this);
 }
 
 void ExtensionDialog::DeleteDelegate() {
