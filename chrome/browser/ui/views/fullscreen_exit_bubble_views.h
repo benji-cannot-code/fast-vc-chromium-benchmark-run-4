613f5baa961877c0d15015cb7dd79b9c187c4893
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 06:55:04 +0000

    Move fullscreen bubble (the version that uses views) from manual layout to a layout manager.
    BUG=100653
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8539031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/fullscreen_exit_bubble_views.h
+++ chrome/browser/ui/views/fullscreen_exit_bubble_views.h
@@ -8,14 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/ui/fullscreen_exit_bubble.h"
-#include "googleurl/src/gurl.h"
-#include "views/controls/link_listener.h"
 
+class GURL;
 namespace ui {
 class SlideAnimation;
 }
-
 namespace views {
 class View;
 class Widget;
@@ -25,8 +24,7 @@ class Widget;
 // screen in fullscreen mode, telling users how to exit and providing a click
 // target. The bubble auto-hides, and re-shows when the user moves to the
 // screen top.
-class FullscreenExitBubbleViews : public views::LinkListener,
-                                  public FullscreenExitBubble {
+class FullscreenExitBubbleViews : public FullscreenExitBubble {
  public:
   FullscreenExitBubbleViews(views::Widget* frame,
                             Browser* browser,
@@ -36,8 +34,12 @@ class FullscreenExitBubbleViews : public views::LinkListener,
 
   void UpdateContent(const GURL& url, FullscreenExitBubbleType bubble_type);
 
- protected:
+ private:
+  class FullscreenExitView;
+
   // FullScreenExitBubble
+  virtual void AnimationProgressed(const ui::Animation* animation) OVERRIDE;
+  virtual void AnimationEnded(const ui::Animation* animation) OVERRIDE;
   virtual gfx::Rect GetPopupRect(bool ignore_animation_state) const OVERRIDE;
   virtual gfx::Point GetCursorScreenPoint() OVERRIDE;
   virtual bool WindowContainsPoint(gfx::Point pos) OVERRIDE;
@@ -46,17 +48,7 @@ class FullscreenExitBubbleViews : public views::LinkListener,
   virtual void Show() OVERRIDE;
   virtual bool IsAnimating() OVERRIDE;
 
- private:
-  class FullscreenExitView;
-
   void StartWatchingMouseIfNecessary();
-
-  // views::LinkListener:
-  virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
-
-  // ui::AnimationDelegate:
-  virtual void AnimationProgressed(const ui::Animation* animation) OVERRIDE;
-  virtual void AnimationEnded(const ui::Animation* animation) OVERRIDE;
 
   // The root view containing us.
   views::View* root_view_;
