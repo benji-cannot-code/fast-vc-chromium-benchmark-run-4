1d5dcdc607345c49eb84c9635dcc8657bba2b229
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 15:55:03 +0000

    Use callback_forward.h instead of callback.h where possible.
    callback_forward.h will be committed separately, before this commit, and is included here only to assist in running try jobs. It will be removed before the final commit.
    
    You are asked to review this as you have OWNERS for these files.
    
    BUG=None
    TEST=Compiles
    R=ajwong@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8697005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111947 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/base/plugin_message_loop_proxy.h
+++ remoting/base/plugin_message_loop_proxy.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_BASE_PLUGIN_MESSAGE_LOOP_H_
 #define REMOTING_BASE_PLUGIN_MESSAGE_LOOP_H_
 
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "base/compiler_specific.h"
 #include "base/message_loop_proxy.h"
 #include "base/synchronization/lock.h"
--- remoting/protocol/client_stub.h
+++ remoting/protocol/client_stub.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define REMOTING_PROTOCOL_CLIENT_STUB_H_
 
 #include "base/basictypes.h"
-#include "base/callback.h"
 
 namespace remoting {
 namespace protocol {
