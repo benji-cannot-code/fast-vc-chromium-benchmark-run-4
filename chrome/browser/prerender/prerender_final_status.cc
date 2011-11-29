fa6a3abb03a90176d1405c41c36725595ec53665
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 21:46:19 +0000

    Fix recording of Omnibox prerenders so counts are more aligned.
    BUG=105733
    
    
    Review URL: http://codereview.chromium.org/8729039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_final_status.cc
+++ chrome/browser/prerender/prerender_final_status.cc
@@ -51,6 +51,7 @@ const char* kFinalStatusNames[] = {
   "Session Storage Namespace Mismatch",
   "No Use Group",
   "Match Complete Dummy",
+  "Duplicate",
   "Max",
 };
 COMPILE_ASSERT(arraysize(kFinalStatusNames) == FINAL_STATUS_MAX + 1,
--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -358,9 +358,6 @@ bool PrerenderManager::AddPrerenderFromOmnibox(
       NOTREACHED();
       break;
   };
-
-  histograms_->RecordPrerenderFromOmnibox();
-
   return AddPrerender(origin, std::make_pair(-1, -1), url, GURL(),
                       session_storage_namespace);
 }
@@ -375,8 +372,6 @@ bool PrerenderManager::AddPrerender(
 
   if (origin == ORIGIN_LINK_REL_PRERENDER && IsGoogleSearchResultURL(referrer))
     origin = ORIGIN_GWS_PRERENDER;
-
-  histograms_->RecordPrerender(origin, url_arg);
 
   // If the referring page is prerendering, defer the prerender.
   std::list<PrerenderContentsData>::iterator source_prerender =
@@ -395,10 +390,16 @@ bool PrerenderManager::AddPrerender(
   if (IsControlGroup() && MaybeGetQueryStringBasedAliasURL(url, &alias_url))
     url = alias_url;
 
-  if (FindEntry(url))
-    return false;
+  // From here on, we will record a FinalStatus so we need to register with the
+  // histogram tracking.
+  histograms_->RecordPrerender(origin, url_arg);
 
   uint8 experiment = GetQueryStringBasedExperiment(url_arg);
+
+  if (FindEntry(url)) {
+    RecordFinalStatus(origin, experiment, FINAL_STATUS_DUPLICATE);
+    return false;
+  }
 
   // Do not prerender if there are too many render processes, and we would
   // have to use an existing one.  We do not want prerendering to happen in
@@ -647,9 +648,8 @@ bool PrerenderManager::MaybeUsePrerenderedPage(TabContents* tab_contents,
   }
 
   histograms_->RecordPerSessionCount(++prerenders_per_session_count_);
+  histograms_->RecordUsedPrerender(prerender_contents->origin());
   prerender_contents->set_final_status(FINAL_STATUS_USED);
-
-  histograms_->RecordOmniboxUsedPrerender(prerender_contents->origin());
 
   new_render_view_host->Send(
       new ChromeViewMsg_SetIsPrerendering(new_render_view_host->routing_id(),
