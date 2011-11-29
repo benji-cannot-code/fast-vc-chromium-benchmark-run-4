2d84ab5b0ca7a1a6c42a436a9f4f525c26cac99d
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 16:10:23 +0000

    Use callback_forward.h instead of callback.h where possible.
    callback_forward.h will be committed separately, before this commit, and is included here only to assist in running try jobs. It will be removed before the final commit.
    
    You are asked to review this as you have OWNERS for these files.
    
    BUG=None
    TEST=Compiles
    R=mnissler@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8700012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_provider_test.h
+++ chrome/browser/policy/configuration_policy_provider_test.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "chrome/browser/policy/asynchronous_policy_test_base.h"
--- chrome/browser/policy/url_blacklist_manager.h
+++ chrome/browser/policy/url_blacklist_manager.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "base/compiler_specific.h"
 #include "base/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
