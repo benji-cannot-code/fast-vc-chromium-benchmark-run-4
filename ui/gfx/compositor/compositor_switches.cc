a31c01d63d44fde1f4e5db3858e8c1ca10e45f4d
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 13:37:47 +0000

    Enable vsync on CompositorCC.
    This is relying on the fact that the WebGraphicsContext3DInProcessImpl has a gfx::GLContext under the hood. The alternative is to push SetSwapInterval into the initialization of WebGraphicsContext3DInProcessImpl. I've preferred to do it here, because I think that they other clients of WebGraphicsContext3DInProcessImpl don't need vsync enabled.
    
    BUG=none
    TEST=Build chrome using GYP_DEFINES="use_aura=1 use_webkit_compositor=1". Run chrome with proprietary Nvidia driver (supports vsync). See no tearing when dragging the window around.
    
    Review URL: http://codereview.chromium.org/8700007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111928 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor_switches.cc
+++ ui/gfx/compositor/compositor_switches.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
+const char kDisableUIVsync[] = "disable-ui-vsync";
+
 const char kEnableCompositorOverdrawDebugging[] =
     "enable-compositor-overdraw-debugging";
 
