a8662ee987d6ff5406e0893fcc2ff637f6630ccb
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 06:37:40 +0000

    base::Bind fixes
    R=jhawkins@chromium.org
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8735014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112153 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/watching_file_reader_unittest.cc
+++ net/dns/watching_file_reader_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/watching_file_reader.h"
 
+#include "base/bind.h"
 #include "base/message_loop.h"
 #include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
@@ -106,23 +107,15 @@ class WatchingFileReaderTest : public testing::Test {
   }
 
  protected:
-  friend class BreakTask;
-  class BreakTask : public Task {
-   public:
-    BreakTask(WatchingFileReaderTest* test, std::string breakpoint)
-      : test_(test), breakpoint_(breakpoint) {}
-    virtual ~BreakTask() {}
-    virtual void Run() OVERRIDE {
-      test_->breakpoint_ = breakpoint_;
-      MessageLoop::current()->QuitNow();
-    }
-   private:
-    WatchingFileReaderTest* test_;
-    std::string breakpoint_;
-  };
+  void BreakCallback(std::string breakpoint) {
+    breakpoint_ = breakpoint;
+    MessageLoop::current()->QuitNow();
+  }
 
   void BreakNow(std::string b) {
-    message_loop_->PostTask(FROM_HERE, new BreakTask(this, b));
+    message_loop_->PostTask(FROM_HERE,
+        base::Bind(&WatchingFileReaderTest::BreakCallback,
+                   base::Unretained(this), b));
   }
 
   void RunUntilBreak(std::string b) {
