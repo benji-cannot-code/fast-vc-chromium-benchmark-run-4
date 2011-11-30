04b7ed7528008124f70bec49cfc4c434fc7c82ad
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-30 23:25:40 +0000

    AX: Searching mechanism gets stuck when searching tables https://bugs.webkit.org/show_bug.cgi?id=72519
    Source/WebCore: 
    
    When searching through the elements within a data table, the children() method should not
    be used, since that contains elements (like a table header column) which have the same children
    as the table itself. Instead the cells() should be searched.
    
    Reviewed by Beth Dakin.
    
    Test: platform/mac/accessibility/search-when-element-starts-in-table.html
    
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::findMatchingObjects):
    
    LayoutTests: 
    
    Reviewed by Beth Dakin.
    
    * platform/mac/accessibility/search-when-element-starts-in-table-expected.txt: Added.
    * platform/mac/accessibility/search-when-element-starts-in-table.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AXObjectCache.h"
 #include "AccessibilityRenderObject.h"
+#include "AccessibilityTable.h"
 #include "FloatRect.h"
 #include "FocusController.h"
 #include "Frame.h"
@@ -385,7 +386,14 @@ void AccessibilityObject::findMatchingObjects(AccessibilitySearchCriteria* crite
         } else if (searchObject == startObject)
             didFindStartObject = true;
         
-        AccessibilityChildrenVector searchChildren = searchObject->children();
+        AccessibilityChildrenVector searchChildren;
+        // A table's children includes elements whose own children are also the table's children (due to the way the Mac exposes tables).
+        // The table's cells are what are desired in this case, since that's where the content resides.
+        if (searchObject->isAccessibilityTable())
+            toAccessibilityTable(searchObject)->cells(searchChildren);
+        else
+            searchChildren = searchObject->children();
+        
         size_t childrenSize = searchChildren.size();
         for (size_t i = isForward ? childrenSize : 0; isForward ? i > 0 : i < childrenSize; isForward ? i-- : i++) {
             // FIXME: Handle attachments.
