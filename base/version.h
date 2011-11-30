f62130e3599b19d1aab239492e373136b5342582
ensonic@google.com <ensonic@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 13:27:05 +0000

    base: remove unused header and include the right one instead
    Don't include "base/gtest_prod_util.h". It defines one macro and that is not used here. Include the needed testing header in two places instead.
    
    BUG=105855
    
    
    Review URL: http://codereview.chromium.org/8555003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112209 0039d316-1c4b-4281-b951-d872f2087c98

--- base/version.h
+++ base/version.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/basictypes.h"
-#include "base/gtest_prod_util.h"
 
 // Version represents a dotted version number, like "1.2.3.4", supporting
 // parsing and comparison.
