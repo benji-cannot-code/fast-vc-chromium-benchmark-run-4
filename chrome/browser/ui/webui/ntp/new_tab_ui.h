a82e36eceec859c5fbe441b3d0311af30bab287a
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 15:43:26 +0000

    Remove TOUCH_UI specific new tab page.
    Also remove and clean-up the remaining vestiges of NTP3 support (they were left n the code just for touch_ntp which was built against them).
    
    BUG=105046
    TEST=NTP4WebUITest.TestBrowsePages
    
    Review URL: http://codereview.chromium.org/8689013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_ui.h
+++ chrome/browser/ui/webui/ntp/new_tab_ui.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/time.h"
 #include "base/timer.h"
-#include "chrome/browser/prefs/pref_change_registrar.h"
 #include "chrome/browser/sessions/tab_restore_service.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/chrome_web_ui.h"
@@ -45,9 +44,6 @@ class NewTabUI : public ChromeWebUI,
 
   // The current preference version.
   static int current_pref_version() { return current_pref_version_; }
-
-  // Returns whether NTP4 is enabled.
-  static bool NTP4Enabled();
 
   // Returns whether NTP4 bookmark features are enabled.
   static bool NTP4BookmarkFeaturesEnabled();
@@ -94,8 +90,6 @@ class NewTabUI : public ChromeWebUI,
 
   content::NotificationRegistrar registrar_;
 
-  // Tracks updates of the kShowBookmarkBar preference.
-  PrefChangeRegistrar pref_change_registrar_;
   // The time when we started benchmarking.
   base::TimeTicks start_;
   // The last time we got a paint notification.
