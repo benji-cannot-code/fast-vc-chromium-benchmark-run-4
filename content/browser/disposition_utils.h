44819e5770312a6562ce5b58920241bc6688677e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 00:19:51 +0000

    recently closed menu: respect middle click
    (and other disposition modifiers)
    
    BUG=7678
    TEST=manual
    
    Review URL: http://codereview.chromium.org/8678002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112077 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/disposition_utils.h
+++ content/browser/disposition_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_DISPOSITION_UTILS_H_
 #pragma once
 
+#include "base/logging.h"
 #include "content/common/content_export.h"
 #include "webkit/glue/window_open_disposition.h"
 
