129cb6bd35c33e9b9d64f218f7142ad13ab0b189
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 09:14:30 +0000

    Fix chromeos link error due to web_ui_util.cc
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8747007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/dialog_style.h"
 #include "chrome/browser/ui/views/window.h"
-#include "chrome/browser/ui/webui/web_ui_util.cc"
+#include "chrome/browser/ui/webui/web_ui_util.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
