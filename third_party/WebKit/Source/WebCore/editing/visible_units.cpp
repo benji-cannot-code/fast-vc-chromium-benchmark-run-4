353c638dacfc6fa1b3acd29a5f8c17acf83fcd67
xji@chromium.org <xji@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-30 00:23:23 +0000

    --webkit-visual-word should be able to reach end of text, instead of end of line https://bugs.webkit.org/show_bug.cgi?id=72048
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore: 
    
    Revert r92223 -- webkit-visual-word should reach boundary of line.
    When there is no more left or right words in the same editing boundary and
    current position is an editable position, return start or end position in this
    editable content.
    
    Test: editing/selection/move-by-word-visually-textarea.html
    
    * editing/visible_units.cpp:
    (WebCore::collectWordBreaksInBoxInsideBlockWithSameDirectionality):
    (WebCore::collectWordBreaksInBoxInsideBlockWithDifferntDirectionality):
    (WebCore::leftWordPosition):
    (WebCore::rightWordPosition):
    
    LayoutTests: 
    
    * editing/selection/move-by-word-visually-inline-block-positioned-element-expected.txt:
    * editing/selection/move-by-word-visually-inline-block-positioned-element.html:
    * editing/selection/move-by-word-visually-multi-line-expected.txt:
    * editing/selection/move-by-word-visually-multi-line.html:
    * editing/selection/move-by-word-visually-single-space-inline-element-expected.txt:
    * editing/selection/move-by-word-visually-textarea-expected.txt: Added.
    * editing/selection/move-by-word-visually-textarea.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/visible_units.cpp
+++ third_party/WebKit/Source/WebCore/editing/visible_units.cpp
@@ -1101,12 +1101,6 @@ VisiblePosition rightBoundaryOfLine(const VisiblePosition& c, TextDirection dire
 static const int invalidOffset = -1;
 static const int offsetNotFound = -1;
 
-static bool isBoxVisuallyLastInLine(const InlineBox* box, TextDirection blockDirection)
-{
-    return blockDirection == LTR ? !box->nextLeafChild() || box->nextLeafChild()->renderer()->isBR()
-        : !box->prevLeafChild() || box->prevLeafChild()->renderer()->isBR();
-}
-
 static bool positionIsInBox(const VisiblePosition& wordBreak, const InlineBox* box, int& offsetOfWordBreak)
 {
     if (wordBreak.isNull())
@@ -1293,22 +1287,10 @@ struct WordBoundaryEntry {
     
 typedef Vector<WordBoundaryEntry, 50> WordBoundaryVector;
     
-static void appendPositionAtLogicalEndOfLine(const InlineBox* box, WordBoundaryVector& orderedWordBoundaries)
-{
-    VisiblePosition endOfBlock = logicalEndOfLine(createLegacyEditingPosition(box->renderer()->node(), box->caretMaxOffset()));
-
-    int offsetOfEndOfBlock;
-    if (positionIsInBox(endOfBlock, box, offsetOfEndOfBlock))
-        orderedWordBoundaries.append(WordBoundaryEntry(endOfBlock, offsetOfEndOfBlock));
-}
-    
 static void collectWordBreaksInBoxInsideBlockWithSameDirectionality(const InlineBox* box, WordBoundaryVector& orderedWordBoundaries)
 {
     orderedWordBoundaries.clear();
 
-    if (!box->renderer()->isBR() && isBoxVisuallyLastInLine(box, box->direction()))
-        appendPositionAtLogicalEndOfLine(box, orderedWordBoundaries);
-    
     VisiblePosition wordBreak;
     int offsetOfWordBreak = invalidOffset;
     while (1) {
@@ -1323,9 +1305,6 @@ static void collectWordBreaksInBoxInsideBlockWithSameDirectionality(const Inline
 static void collectWordBreaksInBoxInsideBlockWithDifferntDirectionality(const InlineBox* box, WordBoundaryVector& orderedWordBoundaries)
 {
     orderedWordBoundaries.clear();
-    
-    if (!box->renderer()->isBR() && isBoxVisuallyLastInLine(box, box->direction() == LTR ? RTL : LTR))
-        appendPositionAtLogicalEndOfLine(box, orderedWordBoundaries);
     
     VisiblePosition wordBreak;
     int offsetOfWordBreak = invalidOffset;
@@ -1632,7 +1611,14 @@ VisiblePosition leftWordPosition(const VisiblePosition& visiblePosition)
         return VisiblePosition();
 
     VisiblePosition leftWordBreak = leftWordPositionIgnoringEditingBoundary(visiblePosition);
-    return visiblePosition.honorEditingBoundaryAtOrBefore(leftWordBreak);
+    leftWordBreak = visiblePosition.honorEditingBoundaryAtOrBefore(leftWordBreak);
+    
+    // FIXME: How should we handle a non-editable position?
+    if (leftWordBreak.isNull() && isEditablePosition(visiblePosition.deepEquivalent())) {
+        TextDirection blockDirection = directionOfEnclosingBlock(visiblePosition.deepEquivalent());
+        leftWordBreak = blockDirection == LTR ? startOfEditableContent(visiblePosition) : endOfEditableContent(visiblePosition);
+    }
+    return leftWordBreak;
 }
 
 VisiblePosition rightWordPosition(const VisiblePosition& visiblePosition)
@@ -1641,7 +1627,14 @@ VisiblePosition rightWordPosition(const VisiblePosition& visiblePosition)
         return VisiblePosition();
 
     VisiblePosition rightWordBreak = rightWordPositionIgnoringEditingBoundary(visiblePosition);
-    return visiblePosition.honorEditingBoundaryAtOrBefore(rightWordBreak);
+    rightWordBreak = visiblePosition.honorEditingBoundaryAtOrBefore(rightWordBreak);
+
+    // FIXME: How should we handle a non-editable position?
+    if (rightWordBreak.isNull() && isEditablePosition(visiblePosition.deepEquivalent())) {
+        TextDirection blockDirection = directionOfEnclosingBlock(visiblePosition.deepEquivalent());
+        rightWordBreak = blockDirection == LTR ? endOfEditableContent(visiblePosition) : startOfEditableContent(visiblePosition);
+    }
+    return rightWordBreak;
 }
 
 }
