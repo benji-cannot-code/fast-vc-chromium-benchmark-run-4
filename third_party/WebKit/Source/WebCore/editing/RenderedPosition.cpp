f60b70a5e46ceb9baef90ae46d0dfa21f21ecb85
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-11-30 23:27:07 +0000

    Cannot select RTL text inside LTR text from right to left by a mouse drag https://bugs.webkit.org/show_bug.cgi?id=73056
    Reviewed by Eric Seidel.
    
    Source/WebCore: 
    
    The bug was caused by positionAtRightBoundaryOfBiDiRun using current inline box's offset
    even when creating a position with previous inline box. Fixed the bug by using the correct offset.
    
    * editing/RenderedPosition.cpp:
    (WebCore::RenderedPosition::positionAtLeftBoundaryOfBiDiRun):
    (WebCore::RenderedPosition::positionAtRightBoundaryOfBiDiRun):
    
    LayoutTests: 
    
    Added a test case to ensure WebKit can select "A" in "aCBAb" when selecting text by a mouse drag
    from the position between "A" and "b" to the position between "B" and "A".
    
    * editing/selection/select-bidi-run-expected.txt:
    * editing/selection/select-bidi-run.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/RenderedPosition.cpp
+++ third_party/WebKit/Source/WebCore/editing/RenderedPosition.cpp
@@ -212,7 +212,7 @@ Position RenderedPosition::positionAtLeftBoundaryOfBiDiRun() const
     if (atLeftmostOffsetInBox())
         return createLegacyEditingPosition(m_renderer->node(), m_offset);
 
-    return createLegacyEditingPosition(nextLeafChild()->renderer()->node(), m_offset);
+    return createLegacyEditingPosition(nextLeafChild()->renderer()->node(), nextLeafChild()->caretLeftmostOffset());
 }
 
 Position RenderedPosition::positionAtRightBoundaryOfBiDiRun() const
@@ -222,7 +222,7 @@ Position RenderedPosition::positionAtRightBoundaryOfBiDiRun() const
     if (atRightmostOffsetInBox())
         return createLegacyEditingPosition(m_renderer->node(), m_offset);
 
-    return createLegacyEditingPosition(prevLeafChild()->renderer()->node(), m_offset);
+    return createLegacyEditingPosition(prevLeafChild()->renderer()->node(), prevLeafChild()->caretRightmostOffset());
 }
 
 LayoutRect RenderedPosition::absoluteRect(int* extraWidthToEndOfLine) const
