12c6bd09452f10426adbfb3aec4faa44b97863f5
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 19:52:37 +0000

    Move a bunch of extension API tests to manifest_version 2.
    These extensions are loaded as unpacked extensions, so they need to be updated
    to version 2 before we can change every unpacked extension to use version 2.
    
    BUG=62897
    Review URL: http://codereview.chromium.org/8729014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112269 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/all_urls/content_script/background.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+    // Let the extension know where the script ran.
+    var url = sender.tab ? sender.tab.url : 'about:blank';
+    chrome.test.sendMessage('content script: ' + url);
+  });
