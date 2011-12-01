a7c3e1fb57f6e229dd0f2655a88bb0ca84fc3dcd
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 20:03:31 +0000

    Implement Accelerator::GetShortcutText for non-Windows Aura.
    BUG=103986
    TEST=Checked all the accelerator text shown in the menu equal to the ones on non-Aura Chrome OS.
    
    
    Review URL: http://codereview.chromium.org/8773003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112506 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/accelerators/accelerator.cc
+++ ui/base/accelerators/accelerator.cc
@@ -18,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/ui_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if !defined(OS_WIN) && defined(USE_AURA)
+#include "ui/base/keycodes/keyboard_code_conversion.h"
+#endif
+
 namespace ui {
 
 string16 Accelerator::GetShortcutText() const {
@@ -84,6 +88,11 @@ string16 Accelerator::GetShortcutText() const {
     else
       key = LOWORD(::MapVirtualKeyW(key_code_, MAPVK_VK_TO_CHAR));
     shortcut += key;
+#elif defined(USE_AURA)
+    const uint16 c = GetCharacterFromKeyCode(key_code_, false);
+    if (c != 0) {
+      shortcut += static_cast<string16::value_type>(base::ToUpperASCII(c));
+    }
 #elif defined(TOOLKIT_USES_GTK)
     const gchar* name = NULL;
     switch (key_code_) {
