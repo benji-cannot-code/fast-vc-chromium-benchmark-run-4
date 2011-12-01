b6489ed95b9a4e52161ad7a0441753d3d4b0fcbb
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 23:17:04 +0000

    Add histogram to measure failure reasons for SSL Search
    This change adds a histogram that measures the reasons why loads of
    https://www.google.com fail in the hopes of getting some visibility into the
    recent SSL search launch.
    
    Since the users were navigating to a Google property in any case, and we would
    have a log of that, this doesn't have any additional privacy impact.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8585038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112569 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host.cc
+++ content/browser/renderer_host/resource_dispatcher_host.cc
@@ -1782,6 +1782,15 @@ void ResourceDispatcherHost::OnResponseCompleted(net::URLRequest* request) {
         -request->status().error(),
         base::CustomHistogram::ArrayToCustomRanges(
             kAllNetErrorCodes, arraysize(kAllNetErrorCodes)));
+
+    if (request->url().SchemeIsSecure() &&
+        request->url().host() == "www.google.com") {
+      UMA_HISTOGRAM_CUSTOM_ENUMERATION(
+          "Net.ErrorCodesForHTTPSGoogleMainFrame",
+          -request->status().error(),
+          base::CustomHistogram::ArrayToCustomRanges(
+              kAllNetErrorCodes, arraysize(kAllNetErrorCodes)));
+    }
   }
 
   std::string security_info;
