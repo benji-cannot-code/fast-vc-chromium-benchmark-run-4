863b3ff5acb1796ec60c528a7e0e7c6e12d373ee
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 06:38:02 +0000

    XHR 'progress' event code assumes wrongly that expectedLength >= 0 https://bugs.webkit.org/show_bug.cgi?id=36156
    Source/WebCore:
    
    Patch by Hans Muller <hmuller@adobe.com> on 2011-11-30
    Reviewed by Alexey Proskuryakov
    
    Avoid passing a negative value as the dispatchProgressEvent's total parameter and always use 0 when lengthComputable is false.
    
    Test: http/tests/xmlhttprequest/chunked-progress-event-expectedLength.html
    
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::didReceiveData):
    
    LayoutTests:
    
    Patch by Hans Muller <hmuller@adobe.com> on 2011-11-30
    Reviewed by Alexey Proskuryakov
    
    Verify that XMLHttpRequest ProgressEvent's total is zero when the expectedLength of the
    (chunked transfer mode) response can't be computed.
    
    * http/tests/xmlhttprequest/chunked-progress-event-expectedLength-expected.txt: Added.
    * http/tests/xmlhttprequest/chunked-progress-event-expectedLength.html: Added.
    * http/tests/xmlhttprequest/resources/chunked-transfer.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/chunked-transfer.php
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php 
+header("Transfer-encoding: chunked");
+flush();
+printf("4\r\n<a/>\r\n");
+flush();
+printf("0\r\n\r\n"); 
+?>
