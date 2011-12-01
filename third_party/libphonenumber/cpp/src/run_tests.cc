ede84736f3b6579f1f5fb7f3e2dbda2b3dc8b43c
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 02:43:55 +0000

    Revert 112305 - Pull the phone library directly. Delete old version. TEST=unit-tests BUG=105577 Review URL: http://codereview.chromium.org/8736001
    TBR=georgey@chromium.org
    
    Review URL: http://codereview.chromium.org/8762016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112381 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/libphonenumber/cpp/src/run_tests.cc
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
