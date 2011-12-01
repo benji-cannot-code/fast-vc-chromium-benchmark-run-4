aecc55f7b65603d2f066de5812b2c50c8371d9ae
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 12:10:48 +0000

    Web Inspector: totalOffsetLeft and totalOffsetTop should take scroll into account. https://bugs.webkit.org/show_bug.cgi?id=73443
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/TextPrompt.js:
    (WebInspector.TextPrompt.prototype._boxForAnchorAtStart):
    (WebInspector.TextPrompt.SuggestBox):
    * inspector/front-end/utilities.js:
    (Element.prototype.totalOffsetLeft):
    (Element.prototype.totalOffsetTop):
    (Element.prototype.boxInWindow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TextPrompt.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TextPrompt.js
@@ -375,7 +375,7 @@ WebInspector.TextPrompt.prototype = {
         var anchorElement = document.createElement("span");
         anchorElement.textContent = "\u200B";
         textRange.insertNode(anchorElement);
-        var box = anchorElement.boxInWindow(window, this._suggestBox._parentElement);
+        var box = anchorElement.boxInWindow(window);
         anchorElement.parentElement.removeChild(anchorElement);
         selection.removeAllRanges();
         selection.addRange(rangeCopy);
@@ -845,7 +845,6 @@ WebInspector.TextPromptWithHistory.prototype.__proto__ = WebInspector.TextPrompt
 WebInspector.TextPrompt.SuggestBox = function(textPrompt, inputElement, className)
 {
     this._textPrompt = textPrompt;
-    this._parentElement = inputElement.ownerDocument.body;
     this._inputElement = inputElement;
     this._selectedElement = null;
     this._boundOnScroll = this._onscrollresize.bind(this, true);
@@ -853,7 +852,8 @@ WebInspector.TextPrompt.SuggestBox = function(textPrompt, inputElement, classNam
     window.addEventListener("scroll", this._boundOnScroll, true);
     window.addEventListener("resize", this._boundOnResize, true);
 
-    this._element = this._parentElement.createChild("div", "suggest-box " + (className || ""));
+    var bodyElement = inputElement.ownerDocument.body;
+    this._element = bodyElement.createChild("div", "suggest-box " + (className || ""));
     this._element.addEventListener("mousedown", this._onboxmousedown.bind(this), true);
     this.containerElement = this._element.createChild("div", "container");
     this.contentElement = this.containerElement.createChild("div", "content");
