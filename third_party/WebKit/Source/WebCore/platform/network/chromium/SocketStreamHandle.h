c56de344ebb1fa4f77806b1a957e8bf1b8c51685
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 03:44:55 +0000

    Add OVERRIDE to WebSocket related sources for safe inheritances. https://bugs.webkit.org/show_bug.cgi?id=73308
    Patch by Takashi Toyoshima <toyoshim@chromium.org> on 2011-11-30
    Reviewed by Kent Tamura.
    
    No new tests because this change contains no functional change.
    
    * platform/network/chromium/SocketStreamHandle.h:
    * websockets/CloseEvent.h:
    * websockets/WebSocket.h:
    * websockets/WebSocketChannel.h:
    * websockets/WorkerThreadableWebSocketChannel.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/SocketStreamHandle.h
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/SocketStreamHandle.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2009 Apple Inc. All rights reserved.
- * Copyright (C) 2009 Google Inc.  All rights reserved.
+ * Copyright (C) 2009, 2011 Google Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -52,8 +52,9 @@ namespace WebCore {
         virtual ~SocketStreamHandle();
 
     protected:
-        virtual int platformSend(const char* data, int length);
-        virtual void platformClose();
+        // SocketStreamHandleBase functions.
+        virtual int platformSend(const char* data, int length) OVERRIDE;
+        virtual void platformClose() OVERRIDE;
 
     private:
         SocketStreamHandle(const KURL&, SocketStreamHandleClient*);
