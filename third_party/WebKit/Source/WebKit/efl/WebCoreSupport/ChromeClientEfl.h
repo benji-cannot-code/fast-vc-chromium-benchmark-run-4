2b87afc9c5d44b756d2127b0e466d13d9b627e32
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 06:30:11 +0000

    Unreviewed. Fix build error when NOTIFICATIONS feature is enabled.
    Source/WebCore:
    
    * CMakeLists.txt:
    
    Source/WebKit/efl:
    
    * WebCoreSupport/ChromeClientEfl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/WebCoreSupport/ChromeClientEfl.h
+++ third_party/WebKit/Source/WebKit/efl/WebCoreSupport/ChromeClientEfl.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "PopupMenu.h"
 
+#if ENABLE(NOTIFICATIONS)
+#include "NotificationPresenter.h"
+#endif
+
 typedef struct _Evas_Object Evas_Object;
 
 namespace WebCore {
@@ -112,7 +116,7 @@ public:
 #endif
 
 #if ENABLE(NOTIFICATIONS)
-    virtual NotificationPresenter* notificationPresenter() const;
+    virtual WebCore::NotificationPresenter* notificationPresenter() const;
 #endif
 
     virtual void reachedMaxAppCacheSize(int64_t spaceNeeded);
