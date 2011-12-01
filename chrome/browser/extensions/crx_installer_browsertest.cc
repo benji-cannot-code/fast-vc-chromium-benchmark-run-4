aaa6588ba0eee4f43da47397fe084790ae5f22ea
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 01:22:24 +0000

    Fix broken ExtensionCrxInstallerTest.GalleryInstallGetsExperimental test.
    Not sure what happened here, but I felt dirty when I checked that
    crx in the first time, so just removed it.
    
    BUG=105728
    
    Review URL: http://codereview.chromium.org/8741004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112367 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/crx_installer_browsertest.cc
+++ chrome/browser/extensions/crx_installer_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 
 class SkBitmap;
@@ -78,9 +79,20 @@ IN_PROC_BROWSER_TEST_F(ExtensionCrxInstallerTest, Whitelisting) {
 
 // crbug.com/105728: Fails because the CRX is invalid.
 IN_PROC_BROWSER_TEST_F(ExtensionCrxInstallerTest,
-                       FAILS_GalleryInstallGetsExperimental) {
-  ASSERT_FALSE(InstallExtension(
-      test_data_dir_.AppendASCII("experimental.crx"), 0));
-  ASSERT_TRUE(InstallExtensionFromWebstore(
-      test_data_dir_.AppendASCII("experimental.crx"), 1));
+                       GalleryInstallGetsExperimental) {
+  // We must modify the command line temporarily in order to pack an extension
+  // that requests the experimental permission.
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  CommandLine old_command_line = *command_line;
+  command_line->AppendSwitch(switches::kEnableExperimentalExtensionApis);
+  FilePath crx_path = PackExtension(
+      test_data_dir_.AppendASCII("experimental"));
+  ASSERT_FALSE(crx_path.empty());
+
+  // Now reset the command line so that we are testing specifically whether
+  // installing from webstore enables experimental permissions.
+  *(CommandLine::ForCurrentProcess()) = old_command_line;
+
+  EXPECT_FALSE(InstallExtension(crx_path, 0));
+  EXPECT_TRUE(InstallExtensionFromWebstore(crx_path, 1));
 }
