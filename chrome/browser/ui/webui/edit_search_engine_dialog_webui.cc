ea48598616729970c6982feb0c55d783453d9026
kgr@chromium.org <kgr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 19:58:56 +0000

    Dialog boxes now wider and deeper and buttons bottom justified.
    BUG=103949
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/8729042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112501 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/edit_search_engine_dialog_webui.cc
+++ chrome/browser/ui/webui/edit_search_engine_dialog_webui.cc
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
-const int kEditSearchEngineDialogWidth = 394;
-const int kEditSearchEngineDialogHeight = 180;
+const int kEditSearchEngineDialogWidth = 440;
+const int kEditSearchEngineDialogHeight = 196;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
