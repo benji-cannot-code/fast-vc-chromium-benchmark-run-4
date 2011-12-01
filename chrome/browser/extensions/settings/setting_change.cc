52974514642305eb646133c950d38af130c99fb7
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 08:55:23 +0000

    Extension Storage API: make the onChanged event pass in a map of changes rather than a list.
    BUG=
    TEST=*ExtensionSetting* (unit_tests, browser_tests)
    
    
    Review URL: http://codereview.chromium.org/8744004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/settings/setting_change.cc
+++ chrome/browser/extensions/settings/setting_change.cc
@@ -12,20 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 /* static */
-std::string SettingChange::GetEventJson(
-    const SettingChangeList& changes) {
-  ListValue changes_value;
+std::string SettingChange::GetEventJson(const SettingChangeList& changes) {
+  DictionaryValue changes_value;
   for (SettingChangeList::const_iterator it = changes.begin();
       it != changes.end(); ++it) {
     DictionaryValue* change_value = new DictionaryValue();
-    change_value->SetString("key", it->key());
     if (it->old_value()) {
       change_value->Set("oldValue", it->old_value()->DeepCopy());
     }
     if (it->new_value()) {
       change_value->Set("newValue", it->new_value()->DeepCopy());
     }
-    changes_value.Append(change_value);
+    changes_value.Set(it->key(), change_value);
   }
   std::string json;
   base::JSONWriter::Write(&changes_value, false, &json);
