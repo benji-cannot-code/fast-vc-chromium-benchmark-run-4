44f7a06ad31d70c9f208779ba5b68f35034cb348
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 06:49:26 +0000

    Move DevTools front-end IPC message handling to content
    BUG=104625
    TEST=Existing tests
    
    Review URL: http://codereview.chromium.org/8729024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112415 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/browser/devtools_frontend_window.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_DEVTOOLS_FRONTEND_WINDOW_H_
+#define CONTENT_PUBLIC_BROWSER_DEVTOOLS_FRONTEND_WINDOW_H_
+#pragma once
+
+class TabContents;
+
+namespace content {
+
+class DevToolsFrontendWindowDelegate;
+
+// Installs delegate for DevTools front-end loaded into |client_tab_contents|.
+void SetupDevToolsFrontendDelegate(
+    TabContents* client_tab_contents,
+    DevToolsFrontendWindowDelegate* delegate);
+
+}
+
+#endif  // CONTENT_PUBLIC_BROWSER_DEVTOOLS_FRONTEND_WINDOW_H_
