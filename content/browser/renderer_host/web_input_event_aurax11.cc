73041611ca0aec2c23b2ea3c587d4e44888c39e6
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 18:25:58 +0000

    aura: Detect double clicks and set ui::EF_IS_DOUBLE_CLICK appropriately.
    BUG=105958
    TEST=manually, double clicked title bar of a window in aura_shell. The window
    maximized.
    
    Review URL: http://codereview.chromium.org/8763020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112486 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/web_input_event_aurax11.cc
+++ content/browser/renderer_host/web_input_event_aurax11.cc
@@ -316,9 +316,6 @@ WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
     case KeyRelease:
       webkit_event.type = WebKit::WebInputEvent::KeyUp;
       break;
-    case GenericEvent:
-      // TODO(sadrul): touch!
-      break;
     default:
       NOTREACHED();
   }
