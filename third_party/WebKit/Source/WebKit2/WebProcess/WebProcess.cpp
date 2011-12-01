0f49cb3092beeec3ff597a134486775b189997be
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 05:55:33 +0000

    Original page URL is not set in quarantine information when downloading using context menu Save Linked File https://bugs.webkit.org/show_bug.cgi?id=73475 <rdar://problem/10500337>
    Reviewed by Dan Bernstein.
    
    Source/WebCore:
    
    * WebCore.exp.in: Exported FrameLoader::setOriginalURLForDownloadRequest.
    
    Source/WebKit2:
    
    * WebProcess/WebProcess.cpp: (WebKit::WebProcess::downloadRequest): Set main document URL
    for the request, so that CFNetwork would know where te file was downloaded. Note that this
    is an easier case than downloading due to clicks, since we don't really need to build a host
    only URL, and could use full page URL. However, the same function is used for consistency.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/CrossOriginPreflightResultCache.h>
 #include <WebCore/Font.h>
 #include <WebCore/FontCache.h>
+#include <WebCore/Frame.h>
 #include <WebCore/GCController.h>
 #include <WebCore/GlyphPageTreeNode.h>
 #include <WebCore/IconDatabase.h>
@@ -952,7 +953,10 @@ void WebProcess::downloadRequest(uint64_t downloadID, uint64_t initiatingPageID,
 {
     WebPage* initiatingPage = initiatingPageID ? webPage(initiatingPageID) : 0;
 
-    DownloadManager::shared().startDownload(downloadID, initiatingPage, request);
+    ResourceRequest requestWithOriginalURL = request;
+    initiatingPage->mainFrame()->loader()->setOriginalURLForDownloadRequest(requestWithOriginalURL);
+
+    DownloadManager::shared().startDownload(downloadID, initiatingPage, requestWithOriginalURL);
 }
 
 void WebProcess::cancelDownload(uint64_t downloadID)
