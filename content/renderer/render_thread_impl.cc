6cee3435d599ee1bcec1d92b051b6961569b2f5c
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 21:39:01 +0000

    Call WebCompositor::shutdown only when we have called WebCompositor::initialize TBR=piman@chromium.org
    Review URL: http://codereview.chromium.org/8769018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112539 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -194,6 +194,7 @@ void RenderThreadImpl::Init() {
   hidden_widget_count_ = 0;
   idle_notification_delay_in_ms_ = kInitialIdleHandlerDelayMs;
   idle_notifications_to_skip_ = 0;
+  compositor_initialized_ = false;
   task_factory_.reset(new ScopedRunnableMethodFactory<RenderThreadImpl>(this));
 
   appcache_dispatcher_.reset(new AppCacheDispatcher(Get()));
@@ -247,7 +248,10 @@ RenderThreadImpl::~RenderThreadImpl() {
     file_thread_->Stop();
 
 #ifdef WEBCOMPOSITOR_HAS_INITIALIZE
-  WebKit::WebCompositor::shutdown();
+  if (compositor_initialized_) {
+    WebKit::WebCompositor::shutdown();
+    compositor_initialized_ = false;
+  }
 #endif
   if (compositor_thread_.get()) {
     RemoveFilter(compositor_thread_->GetMessageFilter());
@@ -447,6 +451,7 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
     WebKit::WebCompositor::initialize(NULL);
 #endif
   }
+  compositor_initialized_ = true;
 
   WebScriptController::enableV8SingleThreadMode();
 
