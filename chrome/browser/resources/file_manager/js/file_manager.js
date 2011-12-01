ca3a5b40745974e5f882a3e615cf6e1daf344ad2
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 21:09:28 +0000

    Moving file selection checkbox to the field name.
    BUG=chromium-os:20156,chromium-os:22832
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8720006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1165,10 +1165,8 @@ FileManager.prototype = {
     columns[3].renderFunction = this.renderDate_.bind(this);
 
     if (this.showCheckboxes_) {
-      columns.unshift(new cr.ui.table.TableColumn('checkbox_', '', 3.6));
-      columns[0].renderFunction = this.renderCheckbox_.bind(this);
       columns[0].headerRenderFunction =
-          this.renderCheckboxColumnHeader_.bind(this);
+          this.renderNameColumnHeader_.bind(this, columns[0].name);
     }
 
     this.table_ = this.dialogDom_.querySelector('.detail-table');
@@ -1554,7 +1552,7 @@ FileManager.prototype = {
     return input;
   };
 
-  FileManager.prototype.renderCheckboxColumnHeader_ = function(table) {
+  FileManager.prototype.renderNameColumnHeader_ = function(name, table) {
     var input = this.document_.createElement('input');
     input.setAttribute('type', 'checkbox');
     input.setAttribute('tabindex', -1);
@@ -1568,9 +1566,13 @@ FileManager.prototype = {
       else
         table.selectionModel.selectAll();
       event.preventDefault();
+      event.stopPropagation();
     });
 
-    return input;
+    var fragment = this.document_.createDocumentFragment();
+    fragment.appendChild(input);
+    fragment.appendChild(this.document_.createTextNode(name));
+    return fragment;
   };
 
   /**
@@ -1578,7 +1580,7 @@ FileManager.prototype = {
    * @return {boolean} True if all items are selected.
    */
   FileManager.prototype.areAllItemsSelected = function() {
-    return this.selection &&
+    return this.selection && this.dataModel_.length > 0 &&
            this.dataModel_.length == this.selection.totalCount;
   };
 
@@ -1687,16 +1689,11 @@ FileManager.prototype = {
    * @param {cr.ui.Table} table The table doing the rendering.
    */
   FileManager.prototype.renderIconType_ = function(entry, columnId, table) {
-    var div = this.document_.createElement('div');
-    div.className = 'detail-icon-container';
-
     var icon = this.document_.createElement('div');
     icon.className = 'detail-icon';
     this.getIconType(entry);
     icon.setAttribute('iconType', entry.cachedIconType_);
-    div.appendChild(icon);
-
-    return div;
+    return icon;
   };
 
   FileManager.prototype.getLabelForRootPath_ = function(path) {
@@ -1724,9 +1721,11 @@ FileManager.prototype = {
    */
   FileManager.prototype.renderName_ = function(entry, columnId, table) {
     var label = this.document_.createElement('div');
+    if (this.showCheckboxes_)
+      label.appendChild(this.renderCheckbox_(entry));
     label.appendChild(this.renderIconType_(entry, columnId, table));
     label.entry = entry;
-    label.className = 'detail-name filename-label';
+    label.className = 'detail-name';
     if (this.currentDirEntry_.name == '') {
       label.appendChild(this.document_.createTextNode(
           this.getLabelForRootPath_(entry.name)));
@@ -1746,7 +1745,6 @@ FileManager.prototype = {
    */
   FileManager.prototype.renderSize_ = function(entry, columnId, table) {
     var div = this.document_.createElement('div');
-    div.className = 'detail-size';
 
     div.textContent = '...';
     cacheEntrySize(entry, function(entry) {
@@ -1769,7 +1767,6 @@ FileManager.prototype = {
    */
   FileManager.prototype.renderType_ = function(entry, columnId, table) {
     var div = this.document_.createElement('div');
-    div.className = 'detail-type';
 
     this.cacheEntryFileType(entry, function(entry) {
       var info = entry.cachedFileType_;
@@ -1794,7 +1791,6 @@ FileManager.prototype = {
    */
   FileManager.prototype.renderDate_ = function(entry, columnId, table) {
     var div = this.document_.createElement('div');
-    div.className = 'detail-date';
 
     div.textContent = '...';
 
