eefa717ea5c78d6ccb71a634d12793530c125c04
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 00:37:16 +0000

    add missing CONTENT_EXPORT
    BUG=build fix
    
    Review URL: http://codereview.chromium.org/8762013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112352 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gamepad_seqlock.h
+++ content/common/gamepad_seqlock.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomicops.h"
 #include "base/threading/platform_thread.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
@@ -28,7 +29,7 @@ namespace content {
 // garbage, or indices could be out of range. Probably the only suitable thing
 // to do during the read loop is to make a copy of the data, and operate on it
 // only after the read was found to be consistent.
-class GamepadSeqLock {
+class CONTENT_EXPORT GamepadSeqLock {
  public:
   GamepadSeqLock();
   base::subtle::Atomic32 ReadBegin();
