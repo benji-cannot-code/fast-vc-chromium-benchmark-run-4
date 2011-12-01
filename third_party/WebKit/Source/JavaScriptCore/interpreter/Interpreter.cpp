b9635458472b75c3544af86d646dd32a80d31523
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-01 05:38:15 +0000

    Removed ArgList iterators.
    Reviewed by Gavin Barraclough.
            
    Another step toward reversing the argument order.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::executeCall):
    (JSC::Interpreter::executeConstruct): Switched from iterator to int.
    
    * runtime/ArgList.h:
    (JSC::ArgList::ArgList):
    (JSC::ArgList::isEmpty): Removed iterators.
    
    * runtime/JSArray.cpp:
    (JSC::JSArray::finishCreation): Switched from iterator to int.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.cpp
+++ third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.cpp
@@ -1037,9 +1037,8 @@ JSValue Interpreter::executeCall(CallFrame* callFrame, JSObject* function, CallT
     CallFrame* newCallFrame = CallFrame::create(oldEnd);
     size_t dst = 0;
     newCallFrame->uncheckedR(0) = thisValue;
-    ArgList::const_iterator end = args.end();
-    for (ArgList::const_iterator it = args.begin(); it != end; ++it)
-        newCallFrame->uncheckedR(++dst) = *it;
+    for (size_t i = 0; i < args.size(); ++i)
+        newCallFrame->uncheckedR(++dst) = args.at(i);
 
     if (callType == CallTypeJS) {
         ScopeChainNode* callDataScopeChain = callData.js.scopeChain;
@@ -1135,9 +1134,8 @@ JSObject* Interpreter::executeConstruct(CallFrame* callFrame, JSObject* construc
 
     CallFrame* newCallFrame = CallFrame::create(oldEnd);
     size_t dst = 0;
-    ArgList::const_iterator end = args.end();
-    for (ArgList::const_iterator it = args.begin(); it != end; ++it)
-        newCallFrame->uncheckedR(++dst) = *it;
+    for (size_t i = 0; i < args.size(); ++i)
+        newCallFrame->uncheckedR(++dst) = args.at(i);
 
     if (constructType == ConstructTypeJS) {
         ScopeChainNode* constructDataScopeChain = constructData.js.scopeChain;
