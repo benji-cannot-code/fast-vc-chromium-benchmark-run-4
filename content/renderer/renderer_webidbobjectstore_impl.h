dd35371752af2c28087ac5fea4ce13eb392f851f
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 20:53:30 +0000

    IndexedDB: Add support for IDBIndex multientry flag
    WebKit side (which needs to land first) is: https://bugs.webkit.org/show_bug.cgi?id=73232 which landed as WebKit r101602
    
    BUG=103578
    TEST=
    
    Review URL: http://codereview.chromium.org/8743001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112777 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webidbobjectstore_impl.h
+++ content/renderer/renderer_webidbobjectstore_impl.h
@@ -51,6 +51,7 @@ class RendererWebIDBObjectStoreImpl : public WebKit::WebIDBObjectStore {
       const WebKit::WebString& name,
       const WebKit::WebString& key_path,
       bool unique,
+      bool multi_entry,
       const WebKit::WebIDBTransaction& transaction,
       WebKit::WebExceptionCode& ec);
   // Transfers ownership of the WebIDBIndex to the caller.
