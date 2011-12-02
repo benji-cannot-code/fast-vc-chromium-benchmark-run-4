b6b84eed8cb01ef8276f7c02cfe6a0dee572421f
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 14:10:02 +0000

    Preventing files from being resorted after load.
    Scanning a directory we add files to the data model, sort them by name (if the sorting field needs async call like date) and then resort againg when data available.
    To avoid flacking lets fetch the data before adding data to the model.
    
    BUG=chromium-os:19082
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8774008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1483,6 +1483,11 @@ FileManager.prototype = {
    * go fetch data for the sort field that we may not have yet.
    */
   FileManager.prototype.prepareSort_ = function(field, callback) {
+    this.prepareSortEntries_(this.dataModel_.slice(), field, callback);
+  };
+
+  FileManager.prototype.prepareSortEntries_ = function(entries, field,
+                                                       callback) {
     var cacheFunction;
 
     if (field == 'name' || field == 'cachedMtime_') {
@@ -1508,11 +1513,10 @@ FileManager.prototype = {
       }
     }
 
-    var dataModel = this.dataModel_;
-    var uncachedCount = dataModel.length;
+    var uncachedCount = entries.length;
 
     for (var i = uncachedCount - 1; i >= 0 ; i--) {
-      var entry = dataModel.item(i);
+      var entry = entries[i];
       if (field in entry) {
         uncachedCount--;
       } else {
@@ -3213,11 +3217,13 @@ FileManager.prototype = {
             });
         }
 
-        spliceArgs.unshift(0, 0);  // index, deleteCount
-        self.dataModel_.splice.apply(self.dataModel_, spliceArgs);
+        self.prefetchCacheForSorting_(spliceArgs, function() {
+          spliceArgs.unshift(0, 0);  // index, deleteCount
+          self.dataModel_.splice.apply(self.dataModel_, spliceArgs);
 
-        // Keep reading until entries.length is 0.
-        reader.readEntries(onReadSome, onError);
+          // Keep reading until entries.length is 0.
+          reader.readEntries(onReadSome, onError);
+        });
       };
 
       metrics.startInterval('DirectoryScan');
@@ -3237,6 +3243,16 @@ FileManager.prototype = {
 
     if (opt_callback)
       opt_callback();
+  };
+
+  FileManager.prototype.prefetchCacheForSorting_ = function(entries, callback) {
+    var field = this.dataModel_.sortStatus.field;
+    if (field) {
+      this.prepareSortEntries_(entries, field, callback);
+    } else {
+      callback();
+      return;
+    }
   };
 
   FileManager.prototype.findListItem_ = function(event) {
