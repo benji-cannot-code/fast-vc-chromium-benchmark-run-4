7c10be15e944e07e53d12c6e3df64aa247258380
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-02 22:49:53 +0000

    <rdar://problem/10520670> REGRESSION (r91738): didFinishLoad is called before custom fonts have finished loading https://bugs.webkit.org/show_bug.cgi?id=73688
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    The problem was that after CSSFontFaceSource::getFontData() had scheduled a 0-delay timer to
    begin loading the font, but before that timer fired, the subresource loader appeared to have
    had no resources waiting to be loaded, and therefore didFinishLoad could be called. This change
    reworks the fix for <http://webkit.org/b/65123> so that while the load is still started on a
    0-dealy timer, the subresource loader’s request count is incremented immediately, preventing
    it from hitting 0 while the font load is scheduled to begin. The delayed load mechanism is
    moved from CSSFontFaceSource into CSSFontSelector in order to safely handle the possibility of
    the latter being decommissioned while waiting for font loading to begin.
    
    * css/CSSFontFaceSource.cpp:
    (WebCore::CSSFontFaceSource::CSSFontFaceSource): Removed initializer for m_loadStartTimer.
    (WebCore::CSSFontFaceSource::~CSSFontFaceSource): Removed stopping of m_loadStartTimer.
    (WebCore::CSSFontFaceSource::getFontData): Replaced code to schedule loading on a timer with
    a call to CSSFontSelector::beginLoadingFontSoon.
    * css/CSSFontFaceSource.h: Removed m_loadStartTimer and m_fontSelector member variables.
    * css/CSSFontSelector.cpp:
    (WebCore::CSSFontSelector::CSSFontSelector): Added initialized for m_beginLoadingTimer.
    (WebCore::CSSFontSelector::~CSSFontSelector): Added call to clearDocument(), to deal with
    anything remaining in m_fontsToBeginLoading at this time.
    (WebCore::CSSFontSelector::clearDocument): Now stops m_beginLoadingTimer and balances
    incrementRequestCount() calls for anything remaining in m_fontsToBeginLoading.
    (WebCore::CSSFontSelector::beginLoadingFontSoon): Added. Schedules the actual call to
    CachedFont::beginLoadingIfNeeded on a 0-delay timer, and meanwhile increments the request count
    on the CachedResourceLoader, which ensures that didFinishLoad will not be called while waiting
    for the timer to fire.
    (WebCore::CSSFontSelector::beginLoadTimerFired): Added. Actually calls
    CachedFont::beginLoadIfNeeded and balances the incrementRequestCount() made when the timer was
    scheduled.
    * css/CSSFontSelector.h:
    
    LayoutTests: 
    
    Reverted test chnages from r91738. These tests were made to wait for the fonts to finish loading
    using a DOM. Now again they do not need to.
    
    * fast/css/color-leakage.html:
    * fast/css/custom-font-xheight.html:
    * fast/css/font-face-multiple-faces.html:
    * fast/css/font-face-multiple-remote-sources.html:
    * fast/css/font-face-remote.html:
    * fast/css/font-face-woff.html:
    * fast/writing-mode/broken-ideograph-small-caps.html:
    * svg/W3C-SVG-1.1-SE/text-intro-09-b.svg:
    * svg/W3C-SVG-1.1/fonts-elem-07-b.svg:
    * svg/custom/svg-fonts-fallback.xhtml:
    * svg/custom/svg-fonts-in-html.html:
    * svg/custom/svg-fonts-segmented.xhtml:
    * svg/custom/svg-fonts-with-no-element-reference.html:
    * svg/custom/svg-fonts-without-missing-glyph.xhtml:
    * svg/text/text-overflow-ellipsis-svgfont.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSFontFaceSource.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSFontFaceSource.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2008, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -52,7 +52,6 @@ CSSFontFaceSource::CSSFontFaceSource(const String& str, CachedFont* font)
     : m_string(str)
     , m_font(font)
     , m_face(0)
-    , m_loadStartTimer(this, &CSSFontFaceSource::startLoadingTimerFired)
 #if ENABLE(SVG_FONTS)
     , m_hasExternalSVGFont(false)
 #endif
@@ -63,7 +62,6 @@ CSSFontFaceSource::CSSFontFaceSource(const String& str, CachedFont* font)
 
 CSSFontFaceSource::~CSSFontFaceSource()
 {
-    m_loadStartTimer.stop();
     if (m_font)
         m_font->removeClient(this);
     pruneTable();
@@ -178,11 +176,9 @@ SimpleFontData* CSSFontFaceSource::getFontData(const FontDescription& fontDescri
 #endif
         }
     } else {
-        // Kick off the load now. Do it on a zero-delay timer rather than synchronously, because we may be in
-        // the middle of layout, and the loader may invoke arbitrary delegate or event handler code.
-        m_fontSelector = fontSelector;
-        if (!m_loadStartTimer.isActive())
-            m_loadStartTimer.startOneShot(0);
+        // Kick off the load. Do it soon rather than now, because we may be in the middle of layout,
+        // and the loader may invoke arbitrary delegate or event handler code.
+        fontSelector->beginLoadingFontSoon(m_font.get());
 
         // This temporary font is not retained and should not be returned.
         FontCachePurgePreventer fontCachePurgePreventer;
@@ -197,17 +193,6 @@ SimpleFontData* CSSFontFaceSource::getFontData(const FontDescription& fontDescri
         doc->registerCustomFont(fontDataRawPtr);
 
     return fontDataRawPtr;
-}
-
-void CSSFontFaceSource::startLoadingTimerFired(Timer<WebCore::CSSFontFaceSource>*)
-{
-    ASSERT(m_font);
-    ASSERT(m_fontSelector);
-
-    if (CachedResourceLoader* cachedResourceLoader = m_fontSelector->cachedResourceLoader())
-        m_font->beginLoadIfNeeded(cachedResourceLoader);
-
-    m_fontSelector = nullptr;
 }
 
 #if ENABLE(SVG_FONTS)
--- third_party/WebKit/Source/WebCore/css/CSSFontSelector.h
+++ third_party/WebKit/Source/WebCore/css/CSSFontSelector.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CSSFontSelector_h
 #define CSSFontSelector_h
 
+#include "CachedResourceHandle.h"
 #include "FontSelector.h"
+#include "Timer.h"
 #include <wtf/Forward.h>
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
@@ -39,8 +41,8 @@ namespace WebCore {
 class CSSFontFace;
 class CSSFontFaceRule;
 class CSSSegmentedFontFace;
+class CachedFont;
 class Document;
-class CachedResourceLoader;
 class FontDescription;
 
 class CSSFontSelector : public FontSelector {
@@ -53,7 +55,7 @@ public:
 
     virtual FontData* getFontData(const FontDescription& fontDescription, const AtomicString& familyName);
 
-    void clearDocument() { m_document = 0; }
+    void clearDocument();
 
     void addFontFaceRule(const CSSFontFaceRule*);
 
@@ -62,23 +64,28 @@ public:
 
     bool isEmpty() const;
 
-    CachedResourceLoader* cachedResourceLoader() const;
-
     virtual void registerForInvalidationCallbacks(FontSelectorClient*);
     virtual void unregisterForInvalidationCallbacks(FontSelectorClient*);
 
     Document* document() const { return m_document; }
+
+    void beginLoadingFontSoon(CachedFont*);
 
 private:
     CSSFontSelector(Document*);
 
     void dispatchInvalidationCallbacks();
 
+    void beginLoadTimerFired(Timer<CSSFontSelector>*);
+
     Document* m_document;
     HashMap<String, Vector<RefPtr<CSSFontFace> >*, CaseFoldingHash> m_fontFaces;
     HashMap<String, Vector<RefPtr<CSSFontFace> >*, CaseFoldingHash> m_locallyInstalledFontFaces;
     HashMap<String, HashMap<unsigned, RefPtr<CSSSegmentedFontFace> >*, CaseFoldingHash> m_fonts;
     HashSet<FontSelectorClient*> m_clients;
+
+    Vector<CachedResourceHandle<CachedFont> > m_fontsToBeginLoading;
+    Timer<CSSFontSelector> m_beginLoadingTimer;
 };
 
 } // namespace WebCore
