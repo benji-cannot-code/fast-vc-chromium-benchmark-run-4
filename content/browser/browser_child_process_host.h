31d08f92b6047284ba86952ef4d7b3bb21fab0f5
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 22:32:25 +0000

    fix shared library build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112807 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_child_process_host.h
+++ content/browser/browser_child_process_host.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <list>
 
+#include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/process.h"
@@ -31,7 +32,7 @@ class WaitableEvent;
 // [Browser]RenderProcessHost is the main exception that doesn't derive from
 // this class. That project lives on the UI thread.
 class CONTENT_EXPORT BrowserChildProcessHost :
-    public content::ChildProcessHostDelegate,
+    public NON_EXPORTED_BASE(content::ChildProcessHostDelegate),
     public ChildProcessLauncher::Client,
     public base::WaitableEventWatcher::Delegate,
     public IPC::Message::Sender {
