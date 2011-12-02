a8babcd709bdb61a5e6c3337ea5d8553d4ba2a2e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 15:09:21 +0000

    importer: Convert two wstrings to string16 in GetFirefoxInstallPathFromRegistry().
    BUG=43460
    R=mirandac@chromium.org
    
    Review URL: http://codereview.chromium.org/8772026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112698 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox_importer_utils_win.cc
+++ chrome/browser/importer/firefox_importer_utils_win.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shlobj.h>
 
 #include "base/file_util.h"
+#include "base/string16.h"
 #include "base/win/registry.h"
 
 // NOTE: Keep these in order since we need test all those paths according
@@ -43,7 +44,7 @@ int GetCurrentFirefoxMajorVersionFromRegistry() {
 
 FilePath GetFirefoxInstallPathFromRegistry() {
   // Detects the path that Firefox is installed in.
-  std::wstring registry_path = L"Software\\Mozilla\\Mozilla Firefox";
+  string16 registry_path = L"Software\\Mozilla\\Mozilla Firefox";
   wchar_t buffer[MAX_PATH];
   DWORD buffer_length = sizeof(buffer);
   base::win::RegKey reg_key(HKEY_LOCAL_MACHINE, registry_path.c_str(),
@@ -53,7 +54,7 @@ FilePath GetFirefoxInstallPathFromRegistry() {
   if (result != ERROR_SUCCESS)
     return FilePath();
 
-  registry_path += L"\\" + std::wstring(buffer) + L"\\Main";
+  registry_path += L"\\" + string16(buffer) + L"\\Main";
   buffer_length = sizeof(buffer);
   base::win::RegKey reg_key_directory(HKEY_LOCAL_MACHINE,
                                       registry_path.c_str(), KEY_READ);
