0d16d05187ef22ab3aeccf790973572d6ae2231d
frankf@google.com <frankf@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 20:58:24 +0000

    Revert "Add PyAuto tests for triggering browser/page action."
    Dues to link issues on chromeos clang.
    
    This reverts commit 2c6c8fc139bae1b64a184c13c979f408721316a3.
    
    TBR=dennisjeffrey@chromium.org
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8774053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112779 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/browser_action_test_util_views.cc
+++ chrome/browser/extensions/browser_action_test_util_views.cc
@@ -11,12 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/browser_actions_container.h"
 #include "chrome/browser/ui/views/extensions/extension_popup.h"
 #include "chrome/browser/ui/views/toolbar_view.h"
-
-#ifdef UNIT_TEST
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#endif  // UNIT_TEST
-
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
 
@@ -39,13 +35,11 @@ int BrowserActionTestUtil::VisibleBrowserActions() {
   return GetContainer(browser_)->VisibleBrowserActions();
 }
 
-#ifdef UNIT_TEST
 void BrowserActionTestUtil::WaitForBrowserActionUpdated(int index) {
   ui_test_utils::WaitForBrowserActionUpdated(
       GetContainer(browser_)->GetBrowserActionViewAt(index)->
           button()->extension()->browser_action());
 }
-#endif  // UNIT_TEST
 
 bool BrowserActionTestUtil::HasIcon(int index) {
   return GetContainer(browser_)->GetBrowserActionViewAt(index)->button()->
