d6caa82285747d0ccf557678fd7da8a9e1306d92
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-02 14:45:44 +0000

    [Qt] [WK2] Use input event timestamps in WebEvents if available https://bugs.webkit.org/show_bug.cgi?id=73647
    Reviewed by Simon Hausmann.
    
    Qt5 input events already have a native timestamp, use this timestamp
    in WebEventFactory instead of WTF::currentTime if it is available.
    
    * Shared/qt/WebEventFactoryQt.cpp:
    (WebKit::currentTimeForEvent):
    (WebKit::WebEventFactory::createWebMouseEvent):
    (WebKit::WebEventFactory::createWebWheelEvent):
    (WebKit::WebEventFactory::createWebKeyboardEvent):
    (WebKit::WebEventFactory::createWebTouchEvent):
    * Shared/qt/WebEventFactoryQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/qt/WebEventFactoryQt.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/qt/WebEventFactoryQt.cpp
@@ -40,6 +40,18 @@ using namespace WebCore;
 
 namespace WebKit {
 
+static inline double currentTimeForEvent(const QInputEvent* event)
+{
+    ASSERT(event);
+
+    // Use the input event timestamps if they are available.
+    // These timestamps are in milliseconds, thus convert them to seconds.
+    if (event->timestamp())
+        return static_cast<double>(event->timestamp()) / 1000;
+
+    return WTF::currentTime();
+}
+
 static WebMouseEvent::Button mouseButtonForEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton || (event->buttons() & Qt::LeftButton))
@@ -105,7 +117,7 @@ WebMouseEvent WebEventFactory::createWebMouseEvent(QMouseEvent* event, int event
     float deltaY                    = event->pos().y() - lastPos.y();
     int clickCount                  = eventClickCount;
     WebEvent::Modifiers modifiers   = modifiersForEvent(event->modifiers());
-    double timestamp                = WTF::currentTime();
+    double timestamp                = currentTimeForEvent(event);
     lastPos.set(event->localPos().x(), event->localPos().y());
 
     return WebMouseEvent(type, button, event->localPos().toPoint(), event->screenPos().toPoint(), deltaX, deltaY, 0.0f, clickCount, modifiers, timestamp);
@@ -119,7 +131,7 @@ WebWheelEvent WebEventFactory::createWebWheelEvent(QWheelEvent* e)
     float wheelTicksY                       = 0;
     WebWheelEvent::Granularity granularity  = WebWheelEvent::ScrollByPixelWheelEvent;
     WebEvent::Modifiers modifiers           = modifiersForEvent(e->modifiers());
-    double timestamp                        = WTF::currentTime();
+    double timestamp                        = currentTimeForEvent(e);
 
     // A delta that is not mod 120 indicates a device that is sending
     // fine-resolution scroll events, so use the delta as number of wheel ticks
@@ -157,7 +169,7 @@ WebKeyboardEvent WebEventFactory::createWebKeyboardEvent(QKeyEvent* event)
     int nativeVirtualKeyCode        = event->nativeVirtualKey();
     int macCharCode                 = 0;
     WebEvent::Modifiers modifiers   = modifiersForEvent(event->modifiers());
-    double timestamp                = WTF::currentTime();
+    double timestamp                = currentTimeForEvent(event);
 
     return WebKeyboardEvent(type, text, unmodifiedText, keyIdentifier, windowsVirtualKeyCode, nativeVirtualKeyCode, macCharCode, isAutoRepeat, isKeypad, isSystemKey, modifiers, timestamp);
 }
@@ -169,7 +181,7 @@ WebTouchEvent WebEventFactory::createWebTouchEvent(const QTouchEvent* event)
     WebPlatformTouchPoint::TouchPointState state = static_cast<WebPlatformTouchPoint::TouchPointState>(0);
     unsigned int id;
     WebEvent::Modifiers modifiers   = modifiersForEvent(event->modifiers());
-    double timestamp                = WTF::currentTime();
+    double timestamp                = currentTimeForEvent(event);
 
     const QList<QTouchEvent::TouchPoint>& points = event->touchPoints();
     
