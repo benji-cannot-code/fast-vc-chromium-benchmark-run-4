0bd2f61c2ea2e40ee2d5f94f723c955ea72722f8
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 17:05:25 +0000

    importer: Convert ResolveInternetShortcut() to string16.
    BUG=43460
    R=mirandac@chromium.org
    
    Review URL: http://codereview.chromium.org/8775053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112720 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/ie_importer.h
+++ chrome/browser/importer/ie_importer.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_IMPORTER_IE_IMPORTER_H_
 #pragma once
 
-#include <string>
 #include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/file_path.h"
+#include "base/string16.h"
 #include "chrome/browser/importer/importer.h"
 #include "chrome/browser/importer/profile_writer.h"
 
@@ -32,7 +32,7 @@ class IEImporter : public Importer {
   // A struct that hosts the information of IE Favorite folder.
   struct FavoritesInfo {
     FilePath path;
-    std::wstring links_folder;
+    string16 links_folder;
   };
 
   // IE PStore subkey GUID: AutoComplete password & form data.
@@ -66,7 +66,7 @@ class IEImporter : public Importer {
 
   // Resolves what's the .url file actually targets.
   // Returns empty string if failed.
-  std::wstring ResolveInternetShortcut(const std::wstring& file);
+  string16 ResolveInternetShortcut(const string16& file);
 
   // Gets the information of Favorites folder. Returns true if successful.
   bool GetFavoritesInfo(FavoritesInfo* info);
