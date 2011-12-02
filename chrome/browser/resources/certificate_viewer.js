49dd4ea44da91f7a01be03bd2880a3fe7b89ba61
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 16:48:14 +0000

    UI polish for certificate viewer.
    Identical to this CL http://codereview.chromium.org/8479042/
    Fix base URL problem. All comments about the changes are in the above URL.
    
    This CL makes certificate viewer for chromeos looks like the screen shot posted
    in issue 102511. It hacks bubble_frame_view.cc to get rid of the body padding
    and add a x button in the title area.
    
    It should not affect the appearance of any other html dialogs.
    
    BUG=102511
    TEST=Go to https://www.google.com; Click the locker icon on the left side of URL in the omnibox and click "certificate information". For screenshots, see http://code.google.com/p/chromium/issues/detail?id=102511
    
    
    Review URL: http://codereview.chromium.org/8461015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112717 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/certificate_viewer.js
+++ chrome/browser/resources/certificate_viewer.js
@@ -11,9 +11,6 @@ cr.define('cert_viewer', function() {
    * substituting in translated strings and requesting certificate details.
    */
   function initialize() {
-    $('close').onclick = function() {
-      window.close();
-    }
     $('export').onclick = exportCertificate;
     cr.ui.decorate('tabbox', cr.ui.TabBox);
 
