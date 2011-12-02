c2a39ef329f3324e4ed96681608e6b77ad443006
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 18:27:37 +0000

    Upstream: Media implementation for Android.
    - This CL makes media_unittests linked.
    - The audio_track_output_stub_android.cc is in place of
      audio_track_output_android.cc because the Java environment is not
      avaliable yet.
    - Also changed to dependence on '../third_party/ffmpeg/ffmpeg.gyp'
      only if os is not Android in media.gyp.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8718014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112739 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/base/media_android.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/media.h"
+#include "base/logging.h"
+
+namespace media {
+
+bool InitializeMediaLibrary(const FilePath& module_dir) {
+  // Android doesn't require any additional media libraries.
+  return true;
+}
+
+void InitializeMediaLibraryForTesting() {}
+
+bool IsMediaLibraryInitialized() {
+  return true;
+}
+
+bool InitializeOpenMaxLibrary(const FilePath& module_dir) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace media
