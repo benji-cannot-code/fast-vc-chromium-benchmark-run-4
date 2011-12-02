f0eac149a33874908b2f8fea2fbc90b96130e553
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-02 11:50:49 +0000

    Not reviewed: remove console.timeStamp from the inspector backend dispatcher.
    * inspector/front-end/InspectorBackend.js:
    (InspectorBackendClass.prototype.sendMessageObjectToBackend):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/InspectorBackend.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/InspectorBackend.js
@@ -146,7 +146,6 @@ InspectorBackendClass.prototype = {
 
     sendMessageObjectToBackend: function(messageObject)
     {
-        console.timeStamp(messageObject.method);
         var message = JSON.stringify(messageObject);
         InspectorFrontendHost.sendMessageToBackend(message);
     },
