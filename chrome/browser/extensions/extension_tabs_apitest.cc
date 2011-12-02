1f64dea17df348d5dcb88dcba393cb1a2e06d6ef
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 23:34:53 +0000

    Re-enable ExtensionApiTest.UpdateWindowShowState on Windows.
    The fixed window size was too small for a tabbed browser window,
    causing the window to grow after being restored.
    
    BUG=105356
    TEST=ExtensionApiTest.UpdateWindowShowState
    
    Review URL: http://codereview.chromium.org/8762006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -32,14 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #define MAYBE_FocusWindowDoesNotExitFullscreen FocusWindowDoesNotExitFullscreen
 #define MAYBE_UpdateWindowSizeExitsFullscreen UpdateWindowSizeExitsFullscreen
-
-// http://crbug.com/105356 , this test is failing browser_tests on Windows.
-#if defined(OS_WIN)
-#define MAYBE_UpdateWindowShowState FAILS_UpdateWindowShowState
-#else
 #define MAYBE_UpdateWindowShowState UpdateWindowShowState
-#endif
-
 #endif
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
