f21bf5727b065393db2e62d865adf0176ce34c50
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 02:26:26 +0000

    Remove another unnecessary include that got added in-between my try and commit
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112607 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/tracking_synchronizer.cc
+++ chrome/browser/metrics/tracking_synchronizer.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 #include "base/threading/thread.h"
 #include "base/tracked_objects.h"
-#include "content/common/child_process_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/profiler_controller.h"
 #include "content/public/common/process_type.h"
