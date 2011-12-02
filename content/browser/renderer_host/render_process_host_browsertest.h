c80e886c39acb8bd5b3f90ec03502e9b4f267126
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 16:14:46 +0000

    Fix tab backgrounding
    This broke because an incompatible change was made to Process, and because there was no test that would catch it. I've fixed the underlying problem for both Linux and ChromeOS and made it testable.
    
    BUG=102726
    TEST=Added new RenderProcessHostTest.Backgrounding
    
    Review URL: http://codereview.chromium.org/8506036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112712 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_browsertest.h
+++ content/browser/renderer_host/render_process_host_browsertest.h
@@ -7,13 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_RENDER_PROCESS_HOST_BROWSERTEST_H_
 #pragma once
 
+#include "base/process.h"
 #include "chrome/test/base/in_process_browser_test.h"
+
+
+class GURL;
 
 class RenderProcessHostTest : public InProcessBrowserTest {
  public:
   RenderProcessHostTest();
 
   int RenderProcessHostCount();
+  base::ProcessHandle ShowSingletonTab(const GURL& page);
 };
 
 #endif  // CONTENT_BROWSER_RENDERER_HOST_RENDER_PROCESS_HOST_BROWSERTEST_H_
