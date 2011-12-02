51b5dfb7dfefd20acb306e0ff3c98ed6454213df
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-02 19:49:47 +0000

    Restrict extension features based on the extension type.
    BUG=101992, 104103
    TEST=existing, ManifestTest
    
    Review URL: http://codereview.chromium.org/8654001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112764 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/chrome_app_api_browsertest.cc
+++ chrome/browser/extensions/chrome_app_api_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/manifest.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "googleurl/src/gurl.h"
@@ -99,10 +100,10 @@ IN_PROC_BROWSER_TEST_F(ChromeAppAPITest, IsInstalled) {
   scoped_ptr<DictionaryValue> app_details(
       static_cast<DictionaryValue*>(
           base::JSONReader::Read(result, false /* allow trailing comma */)));
-  // extension->manifest_value() does not contain the id.
+  // extension->manifest() does not contain the id.
   app_details->Remove("id", NULL);
   EXPECT_TRUE(app_details.get());
-  EXPECT_TRUE(app_details->Equals(extension->manifest_value()));
+  EXPECT_TRUE(app_details->Equals(extension->manifest()->value()));
 
   // Try to change app.isInstalled.  Should silently fail, so
   // that isInstalled should have the initial value.
@@ -178,8 +179,8 @@ IN_PROC_BROWSER_TEST_F(ChromeAppAPITest, GetDetailsForFrame) {
   scoped_ptr<DictionaryValue> app_details(
       static_cast<DictionaryValue*>(
           base::JSONReader::Read(json, false /* allow trailing comma */)));
-  // extension->manifest_value() does not contain the id.
+  // extension->manifest() does not contain the id.
   app_details->Remove("id", NULL);
   EXPECT_TRUE(app_details.get());
-  EXPECT_TRUE(app_details->Equals(extension->manifest_value()));
+  EXPECT_TRUE(app_details->Equals(extension->manifest()->value()));
 }
