312354790d9b2ab7ca4982ebf455044e60cab08e
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-03 06:55:37 +0000

    Show GAIA profile info in NTP
    Screenshots: http://imgur.com/6owxK
    
    BUG=91241
    TEST=
    
    Review URL: http://codereview.chromium.org/8680033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112876 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/ntp_login_handler.h
+++ chrome/browser/ui/webui/ntp/ntp_login_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_member.h"
 #include "content/browser/webui/web_ui.h"
 #include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 
 class Profile;
 
@@ -72,6 +73,7 @@ class NTPLoginHandler : public WebUIMessageHandler,
   void UpdateLogin();
 
   StringPrefMember username_pref_;
+  content::NotificationRegistrar registrar_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_NTP_NTP_LOGIN_HANDLER_H_
