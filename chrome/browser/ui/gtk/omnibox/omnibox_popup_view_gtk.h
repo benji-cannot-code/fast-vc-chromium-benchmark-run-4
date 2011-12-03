02d48e9e582a3af59ad4d49f090236cc3bb9abca
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-03 21:52:41 +0000

    Revert 112839 (it probably caused http://crbug.com/106299 ) - GTK: Port omnibox drawing from GdkGC interface to cairo.
    GdkGC is deprecated and will be removed in gtk3 and cairo should be faster anyway. This uses the new CairoCachedSurface in gfx::Image.
    
    BUG=79722
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8781001
    
    TBR=erg@chromium.org
    Review URL: http://codereview.chromium.org/8789016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
+++ chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
@@ -29,10 +29,6 @@ class GtkThemeService;
 class OmniboxView;
 class SkBitmap;
 
-namespace gfx {
-class Image;
-}
-
 class OmniboxPopupViewGtk : public AutocompletePopupView,
                             public content::NotificationObserver {
  public:
@@ -80,7 +76,7 @@ class OmniboxPopupViewGtk : public AutocompletePopupView,
   // Accept a line of the results, for example, when the user clicks a line.
   void AcceptLine(size_t line, WindowOpenDisposition disposition);
 
-  const gfx::Image* IconForMatch(const AutocompleteMatch& match, bool selected);
+  GdkPixbuf* IconForMatch(const AutocompleteMatch& match, bool selected);
 
   CHROMEGTK_CALLBACK_1(OmniboxPopupViewGtk, gboolean, HandleMotion,
                        GdkEventMotion*);
@@ -113,8 +109,8 @@ class OmniboxPopupViewGtk : public AutocompletePopupView,
 
   // Used to cache GdkPixbufs and map them from the SkBitmaps they were created
   // from.
-  typedef std::map<const SkBitmap*, gfx::Image*> ImageMap;
-  ImageMap images_;
+  typedef std::map<const SkBitmap*, GdkPixbuf*> PixbufMap;
+  PixbufMap pixbufs_;
 
   // A list of colors which we should use for drawing the popup. These change
   // between gtk and normal mode.
