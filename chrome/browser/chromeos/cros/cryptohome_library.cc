9e18bcdb9bb16cb0c6ce3bb1aad0fa9fb969fa40
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-03 00:57:01 +0000

    Shaving parallel authenticator yak to remove unnecessary dependency on this class from OAuth specific code. Moved system salt and supplemental key related methods to cryptohome and cert libraries instead.
    BUG=chromium-os:23301
    TEST=none
    
    Review URL: http://codereview.chromium.org/8761016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cryptohome_library.cc
+++ chrome/browser/chromeos/cros/cryptohome_library.cc
@@ -9,14 +9,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/hash_tables.h"
 #include "base/message_loop.h"
+#include "base/string_number_conversions.h"
+#include "base/string_util.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
+#include "crypto/encryptor.h"
+#include "crypto/sha2.h"
 
 using content::BrowserThread;
 
 namespace {
-  const char kStubSystemSalt[] = "stub_system_salt";
+
+const char kStubSystemSalt[] = "stub_system_salt";
+const int kPassHashLen = 32;
+
 }
 
 namespace chromeos {
@@ -83,23 +90,6 @@ class CryptohomeLibraryImpl : public CryptohomeLibrary {
 
   virtual bool IsMounted() OVERRIDE {
     return chromeos::CryptohomeIsMounted();
-  }
-
-  virtual CryptohomeBlob GetSystemSalt() OVERRIDE {
-    CryptohomeBlob system_salt;
-    char* salt_buf;
-    int salt_len;
-    bool result = chromeos::CryptohomeGetSystemSaltSafe(&salt_buf, &salt_len);
-    if (result) {
-      system_salt.resize(salt_len);
-      if ((int)system_salt.size() == salt_len) {
-        memcpy(&system_salt[0], static_cast<const void*>(salt_buf),
-               salt_len);
-      } else {
-        system_salt.clear();
-      }
-    }
-    return system_salt;
   }
 
   virtual bool AsyncSetOwnerUser(
@@ -185,7 +175,31 @@ class CryptohomeLibraryImpl : public CryptohomeLibrary {
     return chromeos::CryptohomePkcs11IsTpmTokenReady();
   }
 
+  virtual std::string HashPassword(const std::string& password) OVERRIDE {
+    // Get salt, ascii encode, update sha with that, then update with ascii
+    // of password, then end.
+    std::string ascii_salt = GetSystemSalt();
+    char passhash_buf[kPassHashLen];
+
+    // Hash salt and password
+    crypto::SHA256HashString(ascii_salt + password,
+                             &passhash_buf, sizeof(passhash_buf));
+
+    return StringToLowerASCII(base::HexEncode(
+        reinterpret_cast<const void*>(passhash_buf),
+        sizeof(passhash_buf) / 2));
+  }
+
+  virtual std::string GetSystemSalt() OVERRIDE {
+    LoadSystemSalt();  // no-op if it's already loaded.
+    return StringToLowerASCII(base::HexEncode(
+        reinterpret_cast<const void*>(system_salt_.data()),
+        system_salt_.size()));
+  }
+
  private:
+  typedef base::hash_map<int, Delegate*> CallbackMap;
+
   static void Handler(const chromeos::CryptohomeAsyncCallStatus& event,
                       void* cryptohome_library) {
     CryptohomeLibraryImpl* library =
@@ -214,7 +228,25 @@ class CryptohomeLibraryImpl : public CryptohomeLibrary {
     return true;
   }
 
-  typedef base::hash_map<int, Delegate*> CallbackMap;
+  void LoadSystemSalt() {
+    if (!system_salt_.empty())
+      return;
+
+    char* salt_buf;
+    int salt_len;
+    bool result = chromeos::CryptohomeGetSystemSaltSafe(&salt_buf, &salt_len);
+    if (result) {
+      system_salt_.resize(salt_len);
+      if (static_cast<int>(system_salt_.size()) == salt_len)
+        memcpy(&system_salt_[0], static_cast<const void*>(salt_buf), salt_len);
+      else
+        system_salt_.clear();
+    }
+    CHECK(!system_salt_.empty());
+    CHECK_EQ(system_salt_.size() % 2, 0U);
+  }
+
+  chromeos::CryptohomeBlob system_salt_;
   mutable CallbackMap callback_map_;
 
   void* cryptohome_connection_;
@@ -276,14 +308,6 @@ class CryptohomeLibraryStubImpl : public CryptohomeLibrary {
 
   virtual bool IsMounted() OVERRIDE {
     return true;
-  }
-
-  virtual CryptohomeBlob GetSystemSalt() OVERRIDE {
-    CryptohomeBlob salt = CryptohomeBlob();
-    for (size_t i = 0; i < strlen(kStubSystemSalt); i++)
-      salt.push_back(static_cast<unsigned char>(kStubSystemSalt[i]));
-
-    return salt;
   }
 
   virtual bool AsyncSetOwnerUser(
@@ -360,6 +384,16 @@ class CryptohomeLibraryStubImpl : public CryptohomeLibrary {
   }
 
   virtual bool Pkcs11IsTpmTokenReady() OVERRIDE { return true; }
+
+  virtual std::string HashPassword(const std::string& password) OVERRIDE {
+    return StringToLowerASCII(base::HexEncode(
+            reinterpret_cast<const void*>(password.data()),
+            password.length()));
+  }
+
+  virtual std::string GetSystemSalt() OVERRIDE {
+    return kStubSystemSalt;
+  }
 
  private:
   static void DoStubCallback(Delegate* callback) {
--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
+#include "chrome/browser/chromeos/cros/cert_library.h"
 #include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/chromeos/dbus/dbus_thread_manager.h"
 #include "chrome/browser/chromeos/dbus/session_manager_client.h"
@@ -104,6 +105,11 @@ const char kServiceScopeChromeOS[] =
 
 const char kServiceScopeChromeOSDeviceManagement[] =
     "https://www.googleapis.com/auth/chromeosdevicemanagement";
+
+const char kServiceScopeChromeOSDocuments[] =
+    "https://docs.google.com/feeds/ "
+    "https://spreadsheets.google.com/feeds/ "
+    "https://docs.googleusercontent.com/";
 
 class InitializeCookieMonsterHelper {
  public:
@@ -1115,14 +1121,10 @@ void LoginUtilsImpl::RestoreAuthenticationSession(const std::string& username,
 }
 
 void LoginUtilsImpl::KickStartAuthentication(Profile* user_profile) {
-  if (!authenticator_.get())
-    CreateAuthenticator(NULL);
   std::string oauth1_token;
   std::string oauth1_secret;
   if (ReadOAuth1AccessToken(user_profile, &oauth1_token, &oauth1_secret))
     VerifyOAuth1AccessToken(user_profile, oauth1_token, oauth1_secret);
-
-  authenticator_ = NULL;
 }
 
 void LoginUtilsImpl::SetBackgroundView(BackgroundView* background_view) {
@@ -1202,9 +1204,10 @@ bool LoginUtilsImpl::ReadOAuth1AccessToken(Profile* user_profile,
   if (!encoded_token.length() || !encoded_secret.length())
     return false;
 
-  DCHECK(authenticator_.get());
-  std::string decoded_token = authenticator_->DecryptToken(encoded_token);
-  std::string decoded_secret = authenticator_->DecryptToken(encoded_secret);
+  std::string decoded_token =
+      CrosLibrary::Get()->GetCertLibrary()->DecryptToken(encoded_token);
+  std::string decoded_secret =
+      CrosLibrary::Get()->GetCertLibrary()->DecryptToken(encoded_secret);
   if (!decoded_token.length() || !decoded_secret.length())
     return false;
 
@@ -1219,9 +1222,9 @@ void LoginUtilsImpl::StoreOAuth1AccessToken(Profile* user_profile,
   // First store OAuth1 token + service for the current user profile...
   PrefService* pref_service = user_profile->GetPrefs();
   pref_service->SetString(prefs::kOAuth1Token,
-                          authenticator_->EncryptToken(token));
+      CrosLibrary::Get()->GetCertLibrary()->EncryptToken(token));
   pref_service->SetString(prefs::kOAuth1Secret,
-                          authenticator_->EncryptToken(secret));
+      CrosLibrary::Get()->GetCertLibrary()->EncryptToken(secret));
 
   // ...then record the presence of valid OAuth token for this account in local
   // state as well.
