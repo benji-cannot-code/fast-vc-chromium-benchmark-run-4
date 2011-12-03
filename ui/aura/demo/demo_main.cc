4efb7d852afd8a70b3e6a444a27bb1e894cb6438
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-03 22:17:40 +0000

    aura: Ask the WindowDelegate before a Window is Focus()ed.
    . In aura, before a window is focused, ask the delegate if it can be focused.
    . Make a popup RWHVA non-focusable.
    
    This is to prevent the focus from moving from the main web-page to the combobox (which causes the combobox to get destroyed). This fixes an issue where comboboxes in the login screen could not be used properly.
    
    BUG=105786
    TEST=manually
    
    Review URL: http://codereview.chromium.org/8770011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112909 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/demo/demo_main.cc
+++ ui/aura/demo/demo_main.cc
@@ -54,6 +54,7 @@ class DemoWindowDelegate : public aura::WindowDelegate {
   virtual ui::TouchStatus OnTouchEvent(aura::TouchEvent* event) OVERRIDE {
     return ui::TOUCH_STATUS_END;
   }
+  virtual bool CanFocus() OVERRIDE { return true; }
   virtual bool ShouldActivate(aura::Event* event) OVERRIDE { return true; }
   virtual void OnActivated() OVERRIDE {}
   virtual void OnLostActive() OVERRIDE {}
