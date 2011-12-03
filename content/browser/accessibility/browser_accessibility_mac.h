845f7cbf36a0ca0e16d0d990b6a6443a9b9ffca5
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-03 08:48:21 +0000

    Relanding http://codereview.chromium.org/8416034 TBR=dtseng@chromium.org Review URL: http://codereview.chromium.org/8775059
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112885 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_mac.h
+++ content/browser/accessibility/browser_accessibility_mac.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BrowserAccessibilityMac : public BrowserAccessibility {
  public:
   // Implementation of BrowserAccessibility.
-  virtual void Initialize() OVERRIDE;
+  virtual void PreInitialize() OVERRIDE;
   virtual void NativeReleaseReference() OVERRIDE;
 
   // Overrides from BrowserAccessibility.
