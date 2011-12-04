4948afa3d3cbf740aafa14ea9fe3b19d1c63b896
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-04 21:52:56 +0000

    HIERARCHY_REQUEST_ERR check in checkAcceptChild should be optimized for newChild without children https://bugs.webkit.org/show_bug.cgi?id=73737
    Reviewed by Darin Adler.
    
    It turned out that 50-70% of nodes inserted by DOM APIs such as insertBefore and appendChild
    don't have any descendent nodes. Optimize isDescendantOf which is used by checkAcceptChild for this case.
    On a test case attached on the bug, we see a 40% improvement.
    
    Also optimize for cases where either new child or new parent but not both are in document as suggested
    by Erik Arvidsson. This appears to happen about 40-70% of the time, and the symmetric difference between
    the two cases is about 50% so it's worth implementing both optimizations.
    
    Unfortunately no tests because we still have a O(n) algorithm somewhere.
    
    * dom/Node.cpp:
    (WebCore::Node::isDescendantOf):
    (WebCore::Node::contains):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@101967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -1349,8 +1349,10 @@ void Node::checkAddChild(Node *newChild, ExceptionCode& ec)
 bool Node::isDescendantOf(const Node *other) const
 {
     // Return true if other is an ancestor of this, otherwise false
-    if (!other)
+    if (!other || !other->firstChild() || inDocument() != other->inDocument())
         return false;
+    if (other == other->document())
+        return document() == other && this != document() && inDocument();
     for (const ContainerNode* n = parentNode(); n; n = n->parentNode()) {
         if (n == other)
             return true;
@@ -1362,8 +1364,6 @@ bool Node::contains(const Node* node) const
 {
     if (!node)
         return false;
-    if (document() == this)
-        return node->document() == this && node->inDocument();
     return this == node || node->isDescendantOf(this);
 }
 
