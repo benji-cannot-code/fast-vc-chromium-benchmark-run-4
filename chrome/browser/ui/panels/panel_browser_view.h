c591fb70f51d9d9f2117fa0218717008a85a0789
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-04 07:41:18 +0000

    Add 3-stage animation for minimization of Panels on Win. BUG=104645
    Review URL: http://codereview.chromium.org/8787009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112931 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_view.h
+++ chrome/browser/ui/panels/panel_browser_view.h
@@ -13,14 +13,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/panels/native_panel.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "ui/base/animation/animation_delegate.h"
+#include "ui/base/animation/slide_animation.h"
 
 class Browser;
 class Panel;
 class NativePanelTestingWin;
 class PanelBrowserFrameView;
-namespace ui {
-class SlideAnimation;
-}
+
+class PanelSlideAnimation : public ui::SlideAnimation {
+ public:
+  PanelSlideAnimation(ui::AnimationDelegate* target,
+                       bool for_minimize,
+                       double animation_stop_to_show_titlebar)
+    : ui::SlideAnimation(target),
+      for_minimize_(for_minimize),
+      animation_stop_to_show_titlebar_(animation_stop_to_show_titlebar) { }
+  virtual ~PanelSlideAnimation() { }
+  virtual double GetCurrentValue() const OVERRIDE;
+
+ private:
+  bool for_minimize_;
+  double animation_stop_to_show_titlebar_;
+};
 
 // A browser view that implements Panel specific behavior.
 class PanelBrowserView : public BrowserView,
@@ -149,7 +163,7 @@ class PanelBrowserView : public BrowserView,
   MouseDraggingState mouse_dragging_state_;
 
   // Used to animate the bounds change.
-  scoped_ptr<ui::SlideAnimation> bounds_animator_;
+  scoped_ptr<PanelSlideAnimation> bounds_animator_;
   gfx::Rect animation_start_bounds_;
 
   // Is the panel in highlighted state to draw people's attention?
