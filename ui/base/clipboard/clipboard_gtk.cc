9ef7f2b3af001c502a849945b90c64a126c5e3bf
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 21:59:11 +0000

    Enable custom MIME types in web copy/paste.
    BUG=31037
    TEST=manual testing
    
    Review URL: http://codereview.chromium.org/8802004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113040 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/clipboard_gtk.cc
+++ ui/base/clipboard/clipboard_gtk.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/utf_string_conversions.h"
 #include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/gtk/gtk_signal.h"
 #include "ui/base/x/x11_util.h"
 #include "ui/gfx/canvas_skia.h"
@@ -107,6 +108,8 @@ GdkFilterReturn SelectionChangeObserver::OnXEvent(GdkXEvent* xevent,
 const char kMimeTypeBitmap[] = "image/bmp";
 const char kMimeTypeMozillaURL[] = "text/x-moz-url";
 const char kMimeTypeWebkitSmartPaste[] = "chromium/x-webkit-paste";
+// TODO(dcheng): This name is temporary. See crbug.com/106449
+const char kMimeTypeWebCustomData[] = "chromium/x-web-custom-data";
 
 std::string GdkAtomToString(const GdkAtom& atom) {
   gchar* name = gdk_atom_name(atom);
@@ -391,6 +394,17 @@ void Clipboard::ReadAvailableTypes(Clipboard::Buffer buffer,
   if (IsFormatAvailable(GetBitmapFormatType(), buffer))
     types->push_back(UTF8ToUTF16(kMimeTypePNG));
   *contains_filenames = false;
+
+  GtkClipboard* clipboard = LookupBackingClipboard(buffer);
+  if (!clipboard)
+    return;
+
+  GtkSelectionData* data = gtk_clipboard_wait_for_contents(
+      clipboard, StringToGdkAtom(GetWebCustomDataFormatType()));
+  if (!data)
+    return;
+  ReadCustomDataTypes(data->data, data->length, types);
+  gtk_selection_data_free(data);
 }
 
 
@@ -492,8 +506,16 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
 void Clipboard::ReadCustomData(Buffer buffer,
                                const string16& type,
                                string16* result) const {
-  // TODO(dcheng): Implement this.
-  NOTIMPLEMENTED();
+  GtkClipboard* clipboard = LookupBackingClipboard(buffer);
+  if (!clipboard)
+    return;
+
+  GtkSelectionData* data = gtk_clipboard_wait_for_contents(
+      clipboard, StringToGdkAtom(GetWebCustomDataFormatType()));
+  if (!data)
+    return;
+  ReadCustomDataForType(data->data, data->length, type, result);
+  gtk_selection_data_free(data);
 }
 
 void Clipboard::ReadBookmark(string16* title, std::string* url) const {
@@ -540,6 +562,11 @@ Clipboard::FormatType Clipboard::GetBitmapFormatType() {
 // static
 Clipboard::FormatType Clipboard::GetWebKitSmartPasteFormatType() {
   return std::string(kMimeTypeWebkitSmartPaste);
+}
+
+// static
+Clipboard::FormatType Clipboard::GetWebCustomDataFormatType() {
+  return std::string(kMimeTypeWebCustomData);
 }
 
 void Clipboard::InsertMapping(const char* key,
