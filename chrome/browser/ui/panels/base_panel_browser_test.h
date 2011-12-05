b39e735e67e80206d7b464c495c5447027add0bf
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 23:12:47 +0000

    Fix a couple panel overflow related bugs. 1) Moving mouse to left screen edge causes the expanded overflow panels to shrink back. 2) Overflow panel is brought to normal area cannot be dragged correctly.
    BUG=106434
    TEST=Enable overflow test plus manual test by moving mouse to left screen edge or clicking on overflow panel
    
    Review URL: http://codereview.chromium.org/8802021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113053 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/base_panel_browser_test.h
+++ chrome/browser/ui/panels/base_panel_browser_test.h
@@ -11,11 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/ui/panels/auto_hiding_desktop_bar.h"
+#include "chrome/browser/ui/panels/panel.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "ui/gfx/rect.h"
-
-class Panel;
 
 class BasePanelBrowserTest : public InProcessBrowserTest {
  public:
@@ -64,6 +63,8 @@ class BasePanelBrowserTest : public InProcessBrowserTest {
   void WaitForPanelActiveState(Panel* panel, ActiveState state);
   void WaitForWindowSizeAvailable(Panel* panel);
   void WaitForBoundsAnimationFinished(Panel* panel);
+  void WaitForExpansionStateChanged(Panel* panel,
+                                    Panel::ExpansionState expansion_state);
 
   void CreateTestTabContents(Browser* browser);
 
