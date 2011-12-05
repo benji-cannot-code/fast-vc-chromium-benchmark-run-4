e8d2cd21344ffb03acf1da34c09cf23b9b1d55e7
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-05 22:43:24 +0000

    Another attempted build fix.
    * src/linux/WebFontInfo.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontInfo.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontInfo.cpp
@@ -32,9 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebFontInfo.h"
 
-#include "WebFontFamily.h"
 #include "WebFontRenderStyle.h"
-
+#include "platform/linux/WebFontFamily.h"
 #include <fontconfig/fontconfig.h>
 #include <string.h>
 #include <unicode/utf16.h>
