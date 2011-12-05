c6227e9fc3a151b9c6f3145357a1d774c75e342d
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 17:52:15 +0000

    Revert 112986 - Implement Clipboard::ReadData and Clipboard::WriteData for Mac.
    BUG=31037
    TEST=ui_unittests --gtest_filter=ClipboardTest.*
    
    Review URL: http://codereview.chromium.org/8803006
    
    TBR=dcheng@chromium.org
    Review URL: http://codereview.chromium.org/8801015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112993 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/clipboard_unittest.cc
+++ ui/base/clipboard/clipboard_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/pickle.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -22,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/message_loop.h"
 #include "ui/base/clipboard/clipboard_util_win.h"
+#endif
+
+#if defined(OS_WIN) || (defined(OS_POSIX) && !defined(OS_MACOSX))
+#include "base/pickle.h"
 #endif
 
 namespace ui {
@@ -378,13 +381,10 @@ TEST_F(ClipboardTest, MultipleBitmapReadWriteTest) {
 }
 #endif
 
+#if defined(OS_WIN) || (defined(OS_POSIX) && !defined(OS_MACOSX))
 TEST_F(ClipboardTest, DataTest) {
   Clipboard clipboard;
-#if !defined(OS_MACOSX)
   const char* kFormat = "chromium/x-test-format";
-#else
-  const char* kFormat = "org.chromium.test.format";
-#endif  // !defined(OS_MACOSX)
   std::string payload("test string");
   Pickle write_pickle;
   write_pickle.WriteString(payload);
@@ -461,6 +461,7 @@ TEST_F(ClipboardTest, MultipleDataTest) {
   ASSERT_TRUE(read_pickle1.ReadString(&iter1, &unpickled_string1));
   EXPECT_EQ(payload1, unpickled_string1);
 }
+#endif
 
 #if defined(OS_WIN)  // Windows only tests.
 TEST_F(ClipboardTest, HyperlinkTest) {
