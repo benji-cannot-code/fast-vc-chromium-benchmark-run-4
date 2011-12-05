80d607fdfadd3baa7e3a2121bea4cc93cfc2276e
laszlo.1.gombos@nokia.com <laszlo.1.gombos@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-05 16:17:51 +0000

    [Qt] Remove viewModeChanged() from QQuickWebView https://bugs.webkit.org/show_bug.cgi?id=73828
    Reviewed by Kenneth Rohde Christiansen.
    
    * UIProcess/API/qt/qquickwebview_p.h: viewModeChanged is not used
    and we should not expose it.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
@@ -112,7 +112,6 @@ Q_SIGNALS:
     void urlChanged(const QUrl& url);
     void messageReceived(const QVariantMap& message);
     void linkHovered(const QUrl& url, const QString& title);
-    void viewModeChanged();
     void navigationStateChanged();
     void navigationRequested(QObject* request);
 
