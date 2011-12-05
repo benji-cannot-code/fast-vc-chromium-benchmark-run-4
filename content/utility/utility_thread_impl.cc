5699cbf9c87a44ae239b689d04f15c4d71f899f4
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 21:05:13 +0000

    [Linux] Call g_thread_init() in UtilityThreadImpl::OnLoadPlugins().
    This allows plugins that call into glib for NP_GetMIMEDescription to load
    without crashing.
    
    BUG=71661,105252
    TEST=Have libTotem plugins installed, go to about:plugins and they are present.
    
    
    Review URL: http://codereview.chromium.org/8774048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113021 0039d316-1c4b-4281-b951-d872f2087c98

--- content/utility/utility_thread_impl.cc
+++ content/utility/utility_thread_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/memory/scoped_vector.h"
 #include "content/common/child_process.h"
@@ -20,6 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebSerializedScriptValue.h"
 #include "webkit/glue/idb_bindings.h"
 #include "webkit/plugins/npapi/plugin_list.h"
+
+#if defined(TOOLKIT_USES_GTK)
+#include <gtk/gtk.h>
+
+#include "ui/gfx/gtk_util.h"
+#endif
 
 namespace {
 
@@ -38,6 +45,13 @@ UtilityThreadImpl::UtilityThreadImpl()
   webkit_platform_support_.reset(new content::WebKitPlatformSupportImpl);
   WebKit::initialize(webkit_platform_support_.get());
   content::GetContentClient()->utility()->UtilityThreadStarted();
+
+  // On Linux, some plugins expect the browser to have loaded glib/gtk. Do that
+  // before attempting to call into the plugin.
+#if defined(TOOLKIT_USES_GTK)
+  g_thread_init(NULL);
+  gfx::GtkInitFromCommandLine(*CommandLine::ForCurrentProcess());
+#endif
 }
 
 UtilityThreadImpl::~UtilityThreadImpl() {
