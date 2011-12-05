73fef79ca7e2caadc2b076434404a85c09caf6bb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-05 21:55:56 +0000

    [chromium] Set opaque flag for ImageLayerChromium https://bugs.webkit.org/show_bug.cgi?id=72964
    Patch by Dana Jansens <danakj@chromium.org> on 2011-12-05
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    New unit test in tests/ImageLayerChromiumTest.cpp.
    
    * platform/graphics/chromium/GraphicsLayerChromium.cpp:
    (WebCore::GraphicsLayerChromium::setContentsToImage):
    * platform/graphics/chromium/GraphicsLayerChromium.h:
    (WebCore::GraphicsLayerChromium::contentsLayer):
    * platform/graphics/chromium/ImageLayerChromium.cpp:
    (WebCore::ImageLayerChromium::setContents):
    
    Source/WebKit/chromium:
    
    * WebKit.gypi:
    * tests/ImageLayerChromiumTest.cpp: Added.
    (WebCore::MockGraphicsLayerClient::notifyAnimationStarted):
    (WebCore::MockGraphicsLayerClient::notifySyncRequired):
    (WebCore::MockGraphicsLayerClient::paintContents):
    (WebCore::MockGraphicsLayerClient::showDebugBorders):
    (WebCore::MockGraphicsLayerClient::showRepaintCounter):
    (WebCore::TestImage::create):
    (WebCore::TestImage::TestImage):
    (WebCore::TestImage::~TestImage):
    (WebCore::TestImage::isBitmapImage):
    (WebCore::TestImage::currentFrameHasAlpha):
    (WebCore::TestImage::size):
    (WebCore::TestImage::nativeImageForCurrentFrame):
    (WebCore::TestImage::destroyDecodedData):
    (WebCore::TestImage::decodedSize):
    (WebCore::TestImage::draw):
    (WebCore::TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/GraphicsLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/GraphicsLayerChromium.h
@@ -102,6 +102,9 @@ public:
     virtual void paintContents(GraphicsContext&, const IntRect& clip);
     virtual void notifySyncRequired();
 
+    // Exposed for tests.
+    LayerChromium* contentsLayer() const { return m_contentsLayer.get(); }
+
 private:
     void updateOpacityOnLayer();
 
@@ -129,7 +132,6 @@ private:
     void updateContentsScale();
 
     void setupContentsLayer(LayerChromium*);
-    LayerChromium* contentsLayer() const { return m_contentsLayer.get(); }
     float contentsScale() const;
 
     String m_nameBase;
