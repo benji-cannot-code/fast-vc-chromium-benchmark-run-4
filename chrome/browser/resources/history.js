56aebc0944373140fca39049aeb2ac6bfde10d2a
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 22:12:24 +0000

    History: Fix regression - unable to remove entries from search results.
    In the switchover from history2, the ability to select multiple history search results
    (using the checkboxes) was lost. This CL restores that ability, and fixes some style
    issues with the search results page.
    
    BUG=106394
    TEST=Manual: open chrome://history, do a search, and ensure that you can select and
    delete multiple items.
    
    Review URL: http://codereview.chromium.org/8807001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/history.js
+++ chrome/browser/resources/history.js
@@ -164,7 +164,7 @@ Page.prototype.getResultDOM = function(searchResultFlag) {
   node.appendChild(entryBox);
 
   if (searchResultFlag) {
-    time.textContent = this.dateShort;
+    time.appendChild(document.createTextNode(this.dateShort));
     var snippet = createElementWithClassName('div', 'snippet');
     this.addHighlightedText_(snippet,
                              this.snippet_,
