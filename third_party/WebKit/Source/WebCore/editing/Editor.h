1c164441478103b2334f6f5f9c0b096fa743462e
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-06 11:38:16 +0000

    Unreviewed, rolling out r102111. http://trac.webkit.org/changeset/102111 https://bugs.webkit.org/show_bug.cgi?id=73902
    Breaks compilation (Requested by vsevik on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-12-06
    
    * editing/Editor.cpp:
    (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges):
    * editing/Editor.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.h
+++ third_party/WebKit/Source/WebCore/editing/Editor.h
@@ -63,8 +63,6 @@ class SimpleFontData;
 class SpellChecker;
 class Text;
 class TextCheckerClient;
-class TextCheckingParagraph;
-class TextCheckingResult;
 class TextEvent;
 
 struct CompositionUnderline {
@@ -406,7 +404,6 @@ private:
     void writeSelectionToPasteboard(Pasteboard*);
     void revealSelectionAfterEditingOperation();
     void markMisspellingsOrBadGrammar(const VisibleSelection&, bool checkSpelling, RefPtr<Range>& firstMisspellingRange);
-    void markAndReplaceFor(TextCheckingTypeMask, const Vector<TextCheckingResult>&, const TextCheckingParagraph& spellingParagraph, const TextCheckingParagraph& grammarParagraph);
     TextCheckingTypeMask resolveTextCheckingTypeMask(TextCheckingTypeMask);
 
     void selectComposition();
