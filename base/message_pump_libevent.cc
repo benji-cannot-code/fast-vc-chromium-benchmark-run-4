cd841109ef0909d6fda6f597fac38c3886d523f0
ensonic@google.com <ensonic@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 15:09:56 +0000

    mac: use the preprocessor for the mac header
    The mac header is using conditional compilation to become neutral for other platforms.
    Unfortunately it won't work for all gyp backends, as the gyp file is removing the header
    from the sources for non mac platforms. Better use conditionals in the few places
    where we use it. That also spares us to explain the hack.
    
    
    Review URL: http://codereview.chromium.org/8822010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113202 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_libevent.cc
+++ base/message_pump_libevent.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/eintr_wrapper.h"
 #include "base/logging.h"
+#if defined(OS_MACOSX)
+#include "base/mac/scoped_nsautorelease_pool.h"
+#endif
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/time.h"
--- base/shared_memory_unittest.cc
+++ base/shared_memory_unittest.cc
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/basictypes.h"
+#if defined(OS_MACOSX)
+#include "base/mac/scoped_nsautorelease_pool.h"
+#endif
 #include "base/memory/scoped_ptr.h"
 #include "base/shared_memory.h"
 #include "base/test/multiprocess_test.h"
