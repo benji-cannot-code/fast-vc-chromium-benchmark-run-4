6db3bfa4947d5c87a5ee425c96182baad5980ace
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 02:37:21 +0000

    [Sync] Add framework for avoid posting tasks on threads with no work to do
    Add abstract GetGroupsToChange() method to ModelChangingSyncerCommand.
    Use that to figure out which worker threads to post work on (instead of
    posting on all of them).
    
    Implement GetGroupsToChange() for each ModelChangingSyncerCommand.
    
    Add GetEnabledGroups() and GetEnabledGroupsWithConflicts() functions to SyncSession.
    
    Key unapplied updates index by type for ApplyUpdatesCommand.
    
    Make the abstract methods of ModelChangingSyncerCommand protected.
    
    Add HasCustomGroupsToChange() method to ModelChangingSyncerCommand and have all
    commands default to returning false to track down the perf regression introduced
    by the last couple of times this was landed (112178, 112815).
    
    BUG=97832
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=112178
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=112743
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=112815
    
    Review URL: http://codereview.chromium.org/8637006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113090 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/engine/resolve_conflicts_command.h
+++ chrome/browser/sync/engine/resolve_conflicts_command.h
@@ -18,7 +18,11 @@ class ResolveConflictsCommand : public ModelChangingSyncerCommand {
   ResolveConflictsCommand();
   virtual ~ResolveConflictsCommand();
 
+ protected:
   // ModelChangingSyncerCommand implementation.
+  virtual bool HasCustomGroupsToChange() const OVERRIDE;
+  virtual std::set<ModelSafeGroup> GetGroupsToChange(
+      const sessions::SyncSession& session) const OVERRIDE;
   virtual void ModelChangingExecuteImpl(
       sessions::SyncSession* session) OVERRIDE;
 
