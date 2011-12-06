2494a9f20949adc4b2979b9bc85f9cf09ec9d68a
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 23:41:27 +0000

    Fix TabRestoreServiceTest and TranslateManagerTest when using use_webkit_compositor=1
    BUG=106157
    TEST=browser_tests
    
    Review URL: http://codereview.chromium.org/8790012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113288 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager_browsertest.cc
+++ chrome/browser/translate/translate_manager_browsertest.cc
@@ -40,6 +40,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebContextMenuData.h"
 #include "third_party/cld/languages/public/languages.h"
 
+#if defined(USE_WEBKIT_COMPOSITOR)
+#include "content/test/render_view_test.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
+#endif
+
 using content::BrowserThread;
 using testing::_;
 using testing::Pointee;
@@ -154,6 +159,9 @@ class TranslateManagerTest : public TabContentsWrapperTestHarness,
 
  protected:
   virtual void SetUp() {
+#if defined(USE_WEBKIT_COMPOSITOR)
+    WebKit::initialize(&webkit_platform_support_);
+#endif
     // Access the TranslateManager singleton so it is created before we call
     // TabContentsWrapperTestHarness::SetUp() to match what's done in Chrome,
     // where the TranslateManager is created before the TabContents.  This
@@ -183,6 +191,9 @@ class TranslateManagerTest : public TabContentsWrapperTestHarness,
             contents_wrapper()->infobar_tab_helper()));
 
     TabContentsWrapperTestHarness::TearDown();
+#if defined(USE_WEBKIT_COMPOSITOR)
+    WebKit::shutdown();
+#endif
   }
 
   void SimulateTranslateScriptURLFetch(bool success) {
@@ -240,6 +251,10 @@ class TranslateManagerTest : public TabContentsWrapperTestHarness,
   content::NotificationRegistrar notification_registrar_;
   TestURLFetcherFactory url_fetcher_factory_;
   content::TestBrowserThread ui_thread_;
+#if defined(USE_WEBKIT_COMPOSITOR)
+  content::RenderViewTest::RendererWebKitPlatformSupportImplNoSandbox
+      webkit_platform_support_;
+#endif
 
   // The infobars that have been removed.
   // WARNING: the pointers point to deleted objects, use only for comparison.
