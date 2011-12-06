24949fcb2b7fc4e9dddc82d4832d2a9328d70081
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 23:52:43 +0000

    Call util function instead of duplicate the code in gpu_feature_browsertest.cc
    BUG=
    TEST=GpuFeatureBrowsertest
    R=jbates
    Review URL: http://codereview.chromium.org/8816027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113298 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/gpu/gpu_feature_browsertest.cc
+++ chrome/test/gpu/gpu_feature_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/test_launcher_utils.h"
 #include "chrome/test/base/tracing.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/browser/gpu/gpu_blacklist.h"
@@ -40,9 +41,9 @@ class GpuFeatureTest : public InProcessBrowserTest {
     // This enables DOM automation for tab contents.
     EnableDOMAutomation();
 #if !defined(OS_MACOSX)
-    CHECK(!command_line->HasSwitch(switches::kUseGL)) <<
+    CHECK(test_launcher_utils::OverrideGLImplementation(
+        command_line, gfx::kGLImplementationOSMesaName)) <<
         "kUseGL must not be set by test framework code!";
-    command_line->AppendSwitchASCII(switches::kUseGL, "osmesa");
 #endif
   }
 
