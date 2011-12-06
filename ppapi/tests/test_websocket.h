07edccb4a90e67ff63e2ae4efbf963263ade76bb
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 18:06:34 +0000

    Pepper WebSocket API: Add unit test to access uninitialized properties
    BUG=87310
    TEST=ui_tests --gtest_filter='PPAPITest.WebSocket_UninitializedPropertiesAccess'
    
    
    Review URL: http://codereview.chromium.org/8816005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113227 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_websocket.h
+++ ppapi/tests/test_websocket.h
@@ -30,6 +30,7 @@ class TestWebSocket : public TestCase {
   PP_Resource Connect(const char* url, int32_t* result, const char* protocol);
 
   std::string TestIsWebSocket();
+  std::string TestUninitializedPropertiesAccess();
   std::string TestInvalidConnect();
   std::string TestGetURL();
   std::string TestValidConnect();
