e6eb6fb76fad34758232e234f3744d15068e9f0f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-06 08:38:54 +0000

    [Qt] [WK2] MiniBrowser assert on startup in debug build after r101713 https://bugs.webkit.org/show_bug.cgi?id=73874
    This change partially reverts r101713 restoring original behaviour for
    KUrl creation from empty string and fixes asserts in debug build.
    
    Patch by Viatcheslav Ostapenko <ostapenko.viatcheslav@nokia.com> on 2011-12-06
    Reviewed by Alexey Proskuryakov.
    
    No new tests. Tests from r101713 pass.
    
    * platform/KURL.cpp:
    (WebCore::KURL::init):
    (WebCore::KURL::parse):
    * platform/KURL.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/KURL.cpp
+++ third_party/WebKit/Source/WebCore/platform/KURL.cpp
@@ -428,14 +428,14 @@ void KURL::init(const KURL& base, const String& relative, const TextEncoding& en
     CharBuffer parseBuffer;
 
     if (absolute) {
-        parse(str, relative);
+        parse(str, &relative);
     } else {
         // If the base is empty or opaque (e.g. data: or javascript:), then the URL is invalid
         // unless the relative URL is a single fragment.
         if (!base.isHierarchical()) {
             if (str[0] == '#') {
                 appendASCII(base.m_string.left(base.m_queryEnd), str, len, parseBuffer);
-                parse(parseBuffer.data(), relative);
+                parse(parseBuffer.data(), &relative);
             } else {
                 m_string = relative;
                 invalidate();
@@ -452,13 +452,13 @@ void KURL::init(const KURL& base, const String& relative, const TextEncoding& en
         case '#': {
             // must be fragment-only reference
             appendASCII(base.m_string.left(base.m_queryEnd), str, len, parseBuffer);
-            parse(parseBuffer.data(), relative);
+            parse(parseBuffer.data(), &relative);
             break;
         }
         case '?': {
             // query-only reference, special case needed for non-URL results
             appendASCII(base.m_string.left(base.m_pathEnd), str, len, parseBuffer);
-            parse(parseBuffer.data(), relative);
+            parse(parseBuffer.data(), &relative);
             break;
         }
         case '/':
@@ -466,11 +466,11 @@ void KURL::init(const KURL& base, const String& relative, const TextEncoding& en
             if (str[1] == '/') {
                 // net-path
                 appendASCII(base.m_string.left(base.m_schemeEnd + 1), str, len, parseBuffer);
-                parse(parseBuffer.data(), relative);
+                parse(parseBuffer.data(), &relative);
             } else {
                 // abs-path
                 appendASCII(base.m_string.left(base.m_portEnd), str, len, parseBuffer);
-                parse(parseBuffer.data(), relative);
+                parse(parseBuffer.data(), &relative);
             }
             break;
         default:
@@ -545,7 +545,7 @@ void KURL::init(const KURL& base, const String& relative, const TextEncoding& en
                 // of the relative reference; this will also add a null terminator
                 strncpy(bufferPos, relStringPos, bufferSize - (bufferPos - bufferStart));
 
-                parse(parseBuffer.data(), relative);
+                parse(parseBuffer.data(), &relative);
 
                 ASSERT(strlen(parseBuffer.data()) + 1 <= parseBuffer.size());
                 break;
@@ -1055,7 +1055,7 @@ void KURL::parse(const String& string)
     CharBuffer buffer(string.length() + 1);
     copyASCII(string, buffer.data());
     buffer[string.length()] = '\0';
-    parse(buffer.data(), string);
+    parse(buffer.data(), &string);
 }
 
 static inline bool equal(const char* a, size_t lenA, const char* b, size_t lenB)
@@ -1112,18 +1112,18 @@ static bool isNonFileHierarchicalScheme(const char* scheme, size_t schemeLength)
     return false;
 }
 
-void KURL::parse(const char* url, const String& originalString)
+void KURL::parse(const char* url, const String* originalString)
 {
     if (!url || url[0] == '\0') {
         // valid URL must be non-empty
-        m_string = !originalString.isNull() ? originalString : url;
+        m_string = originalString ? *originalString : url;
         invalidate();
         return;
     }
 
     if (!isSchemeFirstChar(url[0])) {
         // scheme must start with an alphabetic character
-        m_string = !originalString.isNull() ? originalString : url;
+        m_string = originalString ? *originalString : url;
         invalidate();
         return;
     }
@@ -1133,7 +1133,7 @@ void KURL::parse(const char* url, const String& originalString)
         schemeEnd++;
 
     if (url[schemeEnd] != ':') {
-        m_string = !originalString.isNull() ? originalString : url;
+        m_string = originalString ? *originalString : url;
         invalidate();
         return;
     }
@@ -1199,7 +1199,7 @@ void KURL::parse(const char* url, const String& originalString)
             hostStart = userStart;
         } else {
             // invalid character
-            m_string = !originalString.isNull() ? originalString : url;
+            m_string = originalString ? *originalString : url;
             invalidate();
             return;
         }
@@ -1215,7 +1215,7 @@ void KURL::parse(const char* url, const String& originalString)
                 hostEnd++;
             else {
                 // invalid character
-                m_string = !originalString.isNull() ? originalString : url;
+                m_string = originalString ? *originalString : url;
                 invalidate();
                 return;
             }
@@ -1236,7 +1236,7 @@ void KURL::parse(const char* url, const String& originalString)
 
         if (!isPathSegmentEndChar(url[portEnd])) {
             // invalid character
-            m_string = !originalString.isNull() ? originalString : url;
+            m_string = originalString ? *originalString : url;
             invalidate();
             return;
         }
@@ -1415,8 +1415,8 @@ void KURL::parse(const char* url, const String& originalString)
 
     // If we didn't end up actually changing the original string and
     // it was already in a String, reuse it to avoid extra allocation.
-    if (originalString == buffer)
-        m_string = originalString;
+    if (originalString && *originalString == buffer)
+        m_string = *originalString;
     else
         m_string = String(buffer.data(), m_fragmentEnd);
 
