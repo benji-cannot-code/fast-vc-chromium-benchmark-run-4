1c4d60b955966e7717a5ea4c459b64a906efd9ab
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-06 05:55:18 +0000

    [Qt] MiniBrowser should default to touch behavior https://bugs.webkit.org/show_bug.cgi?id=73863
    Patch by Alexander Færøy <alexander.faeroy@nokia.com> on 2011-12-05
    Reviewed by Chang Shu.
    
    MiniBrowser currently defaults to the traditional desktop behaviour
    instead of touch behaviour which is prefered.
    
    * MiniBrowser/qt/BrowserWindow.cpp:
    (BrowserWindow::BrowserWindow):
    * MiniBrowser/qt/MiniBrowserApplication.cpp:
    (printHelp):
    (MiniBrowserApplication::handleUserOptions):
    * MiniBrowser/qt/MiniBrowserApplication.h:
    (WindowOptions::WindowOptions):
    (WindowOptions::setUseTraditionalDesktopBehavior):
    (WindowOptions::useTraditionalDesktopBehavior):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102093 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/qt/MiniBrowserApplication.cpp
+++ third_party/WebKit/Tools/MiniBrowser/qt/MiniBrowserApplication.cpp
@@ -169,7 +169,7 @@ void MiniBrowserApplication::sendTouchEvent(QWindow* targetWindow)
 static void printHelp(const QString& programName)
 {
     qDebug() << "Usage:" << programName.toLatin1().data()
-         << "[--touch]"
+         << "[--desktop]"
          << "[-r list]"
          << "[--robot-timeout seconds]"
          << "[--robot-extra-time seconds]"
@@ -193,7 +193,7 @@ void MiniBrowserApplication::handleUserOptions()
         appQuit(0);
     }
 
-    m_windowOptions.setUseTouchWebView(takeOptionFlag(&args, "--touch"));
+    m_windowOptions.setUseTraditionalDesktopBehavior(takeOptionFlag(&args, "--desktop"));
     m_windowOptions.setPrintLoadedUrls(takeOptionFlag(&args, "-v"));
     m_windowOptions.setStartMaximized(takeOptionFlag(&args, "--maximize"));
     m_windowOptions.setStartFullScreen(takeOptionFlag(&args, "-f"));
