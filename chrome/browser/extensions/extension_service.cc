2baaf5bef7bc1ab3e94d330a3547a330b22de9ef
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 23:36:32 +0000

    Start the IME Daemon when adding an extension IME Cleanup the connection member of the engine object when destructing. Properly unregister IMEs when unloading extensions.
    BUG=chromium-os:21357,21514,21355
    TEST=See bugs
    
    
    Review URL: http://codereview.chromium.org/8761018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -1061,6 +1061,11 @@ void ExtensionService::NotifyExtensionUnloaded(
     profile_->GetFileSystemContext()->path_manager()->external_provider()->
         RevokeAccessForExtension(extension->id());
   }
+
+  if (extension->input_components().size() > 0) {
+    ExtensionInputImeEventRouter::GetInstance()->UnregisterAllImes(
+        profile_, extension->id());
+  }
 #endif
 
   UpdateActiveExtensionsInCrashReporter();
