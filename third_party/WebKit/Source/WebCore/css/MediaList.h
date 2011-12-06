afd09c01f9991d3043ad63b999545339b78c8ea2
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-06 18:19:38 +0000

    MediaList: Remove constructor that takes a CSSImportRule*. <http://webkit.org/b/73833>
    Reviewed by Antti Koivisto.
    
    * css/MediaList.h:
    * css/MediaList.cpp:
    
        Remove MediaList(CSSImportRule*, ...) constructor.
    
    * css/CSSImportRule.cpp:
    (WebCore::CSSImportRule::CSSImportRule):
    
        Have CSSImportRule construct its MediaList by passing the parent
        style sheet, which is exactly what the old constructor accomplished.
        Also assert that we're always created with a non-null parent sheet.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102152 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/MediaList.h
+++ third_party/WebKit/Source/WebCore/css/MediaList.h
@@ -42,10 +42,6 @@ public:
     {
         return adoptRef(new MediaList(0, false));
     }
-    static PassRefPtr<MediaList> create(CSSImportRule* parentRule, const String& media)
-    {
-        return adoptRef(new MediaList(parentRule, media));
-    }
     static PassRefPtr<MediaList> create(CSSStyleSheet* parentSheet, const String& media)
     {
         return adoptRef(new MediaList(parentSheet, media, false));
@@ -92,7 +88,6 @@ public:
 private:
     MediaList(CSSStyleSheet* parentSheet, bool fallbackToDescription);
     MediaList(CSSStyleSheet* parentSheet, const String& media, bool fallbackToDescription);
-    MediaList(CSSImportRule* parentRule, const String& media);
 
     void notifyChanged();
 
