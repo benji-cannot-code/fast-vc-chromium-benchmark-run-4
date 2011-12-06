7dde16124b9482c8afc3ebdf3dc44a823d4ed7ae
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 18:07:05 +0000

    Adaptively use temp dir instead of /dev/shm for executable shmem file on Linux
    On some Linux systems, files from /dev/shm cannot have PROT_EXEC applied to
    their mappings.  This depends on picayune setup details that vary between
    distributions and kernels and could vary between installations.  So just
    use an empirical test of whether it works or not, and fall back to using
    generic temporary space instead of /dev/shm for the executable case if needed.
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=103377
    TEST= SharedMemory.AnonymousExecutable with /dev/shm mounted noexec
    
    R=mark@chromium.org
    
    Review URL: http://codereview.chromium.org/8800025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113228 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_android.cc
+++ base/file_util_android.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_util {
 
-bool GetShmemTempDir(FilePath* path) {
+bool GetShmemTempDir(FilePath* path, bool executable) {
   *path = FilePath("/data/local/tmp");
   return true;
 }
--- base/file_util_win.cc
+++ base/file_util_win.cc
@@ -557,7 +557,7 @@ bool GetTempDir(FilePath* path) {
   return true;
 }
 
-bool GetShmemTempDir(FilePath* path) {
+bool GetShmemTempDir(FilePath* path, bool executable) {
   return GetTempDir(path);
 }
 
@@ -577,7 +577,7 @@ bool CreateTemporaryFile(FilePath* path) {
   return false;
 }
 
-FILE* CreateAndOpenTemporaryShmemFile(FilePath* path) {
+FILE* CreateAndOpenTemporaryShmemFile(FilePath* path, bool executable) {
   base::ThreadRestrictions::AssertIOAllowed();
   return CreateAndOpenTemporaryFile(path);
 }
