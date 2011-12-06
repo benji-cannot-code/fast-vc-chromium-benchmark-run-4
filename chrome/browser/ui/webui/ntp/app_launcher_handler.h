3ddbdeb04d874a2798efafa4ce8ad3959d564bdf
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 04:35:42 +0000

    Add menu option for disabling app notifications
    This should only show up for apps which have the appNotifications permission
    in their manifest.
    
    BUG=106268
    TEST=Install an app with the appNotifications permission - the icon on the
    NTP should get a 'Disable Notifications' context menu entry which if checked
    will keep the notification bubbles from appearing over the app's icon.
    
    Review URL: http://codereview.chromium.org/8785016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113111 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/app_launcher_handler.h
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.h
@@ -107,6 +107,9 @@ class AppLauncherHandler : public WebUIMessageHandler,
   // Callback for "closeNotification" message.
   void HandleNotificationClose(const base::ListValue* args);
 
+  // Callback for "setNotificationsDisabled" message.
+  void HandleSetNotificationsDisabled(const base::ListValue* args);
+
   // Register app launcher preferences.
   static void RegisterUserPrefs(PrefService* pref_service);
 
