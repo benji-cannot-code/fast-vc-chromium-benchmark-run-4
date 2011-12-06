c521f16fe881b03efd2e3bbd7b2e6cb2254be3a1
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 02:51:36 +0000

    Fix build break from the future.
    TBR=pfeldman
    Review URL: http://codereview.chromium.org/8801036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113098 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/webview_color_overlay.h
+++ chrome/renderer/webview_color_overlay.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPageOverlay.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebRect.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebRect.h"
 
 namespace content {
 class RenderView;
