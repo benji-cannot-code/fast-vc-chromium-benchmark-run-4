149ce6c22b0752490973c21aaa5f9830519da3a1
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 07:38:25 +0000

    Wait for tpcontrol to exit so we don't leave zombie processes hanging around.
    BUG=chromium-os:23388
    TEST=Go to chrome://settings/system. Play with touchpad settings. top -u chronos should not show any defunct tpcontrol processes.
    
    Review URL: http://codereview.chromium.org/8813001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113150 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/touchpad_settings.cc
+++ chrome/browser/chromeos/system/touchpad_settings.cc
@@ -32,7 +32,9 @@ void LaunchTpControl(const std::vector<std::string>& argv) {
   if (!TPCtrlExists())
     return;
 
-  base::LaunchProcess(CommandLine(argv), base::LaunchOptions(), NULL);
+  base::LaunchOptions options;
+  options.wait = true;
+  base::LaunchProcess(CommandLine(argv), options, NULL);
 }
 
 }  // namespace
