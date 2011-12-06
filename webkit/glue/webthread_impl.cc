dcad50da289431d49c8991c04cb6cbf47208ec12
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 00:26:04 +0000

    Implement WebThread::{add,remove}TaskObserver.
    Re-landing r113010 with clang style fixes.
    
    R=tony@chromium.org
    
    Review URL: http://codereview.chromium.org/8805027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113073 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webthread_impl.cc
+++ webkit/glue/webthread_impl.cc
@@ -11,8 +11,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/message_loop.h"
+#include "base/threading/platform_thread.h"
 
 namespace webkit_glue {
+
+WebThreadBase::WebThreadBase() { }
+WebThreadBase::~WebThreadBase() { }
+
+class WebThreadBase::TaskObserverAdapter : public MessageLoop::TaskObserver {
+public:
+  TaskObserverAdapter(WebThread::TaskObserver* observer)
+      : observer_(observer) { }
+
+  // WebThread::TaskObserver does not have a willProcessTask method.
+  virtual void WillProcessTask(base::TimeTicks) OVERRIDE { }
+
+  virtual void DidProcessTask(base::TimeTicks) OVERRIDE {
+    observer_->didProcessTask();
+  }
+
+private:
+  WebThread::TaskObserver* observer_;
+};
+
+void WebThreadBase::addTaskObserver(TaskObserver* observer) {
+  CHECK(IsCurrentThread());
+  std::pair<TaskObserverMap::iterator, bool> result = task_observer_map_.insert(
+      std::make_pair(observer, static_cast<TaskObserverAdapter*>(NULL)));
+  if (result.second)
+    result.first->second = new TaskObserverAdapter(observer);
+  MessageLoop::current()->AddTaskObserver(result.first->second);
+}
+
+void WebThreadBase::removeTaskObserver(TaskObserver* observer) {
+  CHECK(IsCurrentThread());
+  TaskObserverMap::iterator iter = task_observer_map_.find(observer);
+  if (iter == task_observer_map_.end())
+    return;
+  MessageLoop::current()->RemoveTaskObserver(iter->second);
+  delete iter->second;
+  task_observer_map_.erase(iter);
+}
 
 WebThreadImpl::WebThreadImpl(const char* name)
     : thread_(new base::Thread(name)) {
@@ -23,12 +62,17 @@ void WebThreadImpl::postTask(Task* task) {
   thread_->message_loop()->PostTask(
       FROM_HERE, base::Bind(&WebKit::WebThread::Task::run, base::Owned(task)));
 }
+
 void WebThreadImpl::postDelayedTask(
     Task* task, long long delay_ms) {
   thread_->message_loop()->PostDelayedTask(
       FROM_HERE,
       base::Bind(&WebKit::WebThread::Task::run, base::Owned(task)),
       delay_ms);
+}
+
+bool WebThreadImpl::IsCurrentThread() const {
+  return thread_->thread_id() == base::PlatformThread::CurrentId();
 }
 
 WebThreadImpl::~WebThreadImpl() {
@@ -51,6 +95,10 @@ void WebThreadImplForMessageLoop::postDelayedTask(
       FROM_HERE,
       base::Bind(&WebKit::WebThread::Task::run, base::Owned(task)),
       delay_ms);
+}
+
+bool WebThreadImplForMessageLoop::IsCurrentThread() const {
+  return message_loop_->BelongsToCurrentThread();
 }
 
 WebThreadImplForMessageLoop::~WebThreadImplForMessageLoop() {
