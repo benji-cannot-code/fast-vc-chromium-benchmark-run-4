7bb24cfcb3f94c1db561f2599f920d9fc8fc4afb
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-06 06:31:37 +0000

    Upstream the Cursor implementation of iOS https://bugs.webkit.org/show_bug.cgi?id=73724
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2011-12-05
    Reviewed by David Kilzer.
    
    iOS does not need to support the Cursor of WebKit. For compatibility, Cursor
    is implemented as an empty class on the platform.
    
    * Configurations/WebCore.xcconfig:
    * WebCore.xcodeproj/project.pbxproj:
    * platform/Cursor.h:
    (WebCore::Cursor::Cursor):
    * platform/ios/CursorIOS.cpp: Added.
    (WebCore::cursor):
    (WebCore::pointerCursor):
    (WebCore::crossCursor):
    (WebCore::handCursor):
    (WebCore::moveCursor):
    (WebCore::iBeamCursor):
    (WebCore::waitCursor):
    (WebCore::helpCursor):
    (WebCore::eastResizeCursor):
    (WebCore::northResizeCursor):
    (WebCore::northEastResizeCursor):
    (WebCore::northWestResizeCursor):
    (WebCore::southResizeCursor):
    (WebCore::southEastResizeCursor):
    (WebCore::southWestResizeCursor):
    (WebCore::westResizeCursor):
    (WebCore::northSouthResizeCursor):
    (WebCore::eastWestResizeCursor):
    (WebCore::northEastSouthWestResizeCursor):
    (WebCore::northWestSouthEastResizeCursor):
    (WebCore::columnResizeCursor):
    (WebCore::rowResizeCursor):
    (WebCore::middlePanningCursor):
    (WebCore::eastPanningCursor):
    (WebCore::northPanningCursor):
    (WebCore::northEastPanningCursor):
    (WebCore::northWestPanningCursor):
    (WebCore::southPanningCursor):
    (WebCore::southEastPanningCursor):
    (WebCore::southWestPanningCursor):
    (WebCore::westPanningCursor):
    (WebCore::verticalTextCursor):
    (WebCore::cellCursor):
    (WebCore::contextMenuCursor):
    (WebCore::noDropCursor):
    (WebCore::notAllowedCursor):
    (WebCore::progressCursor):
    (WebCore::aliasCursor):
    (WebCore::zoomInCursor):
    (WebCore::zoomOutCursor):
    (WebCore::copyCursor):
    (WebCore::noneCursor):
    (WebCore::grabCursor):
    (WebCore::grabbingCursor):
    (WebCore::determineHotSpot):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/Cursor.h
+++ third_party/WebKit/Source/WebCore/platform/Cursor.h
@@ -46,7 +46,7 @@ typedef HICON HCURSOR;
 #include "PlatformCursor.h"
 #endif
 
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) && !PLATFORM(IOS)
 #ifdef __OBJC__
 @class NSCursor;
 #else
@@ -82,7 +82,7 @@ namespace WebCore {
         HCURSOR m_nativeCursor;
     };
     typedef RefPtr<SharedCursor> PlatformCursor;
-#elif PLATFORM(MAC)
+#elif PLATFORM(MAC) && !PLATFORM(IOS)
     typedef NSCursor *PlatformCursor;
 #elif PLATFORM(GTK)
     typedef GRefPtr<GdkCursor> PlatformCursor;
@@ -151,10 +151,13 @@ namespace WebCore {
         static const Cursor& fromType(Cursor::Type);
 
         Cursor()
+#if !PLATFORM(IOS)
             : m_platformCursor(0)
+#endif
         {
         }
 
+#if !PLATFORM(IOS)
         Cursor(Image*, const IntPoint& hotSpot);
         Cursor(const Cursor&);
         ~Cursor();
@@ -185,6 +188,7 @@ namespace WebCore {
 #else
         mutable RetainPtr<NSCursor> m_platformCursor;
 #endif
+#endif // !PLATFORM(IOS)
     };
 
     IntPoint determineHotSpot(Image*, const IntPoint& specifiedHotSpot);
