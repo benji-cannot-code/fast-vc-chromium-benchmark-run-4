f0b4eedfa8b7498f4fe6643dd5a1af3b3d05de7b
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 06:40:40 +0000

    Add OAuth2 revocation fetcher that is right now used for notifications. Add unit tests. Review URL: http://codereview.chromium.org/8803029
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/http_return.h
+++ chrome/common/net/http_return.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // contains a few HTTP return codes. Add more HTTP return codes.
 enum HTTPReturnCode {
   RC_REQUEST_OK = 200,
+  RC_REQUEST_OK_EMPTY_BODY = 204,
   RC_BAD_REQUEST = 400,
   RC_UNAUTHORIZED = 401,
   RC_FORBIDDEN = 403,
