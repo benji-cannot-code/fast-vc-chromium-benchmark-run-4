91a900daf94c06be98f69374c31b7c45fc2ae6d5
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 14:15:17 +0000

    importer: Convert the wstrings in importer_unittest to string16.
    BUG=43460
    R=mirandac@chromium.org
    
    Review URL: http://codereview.chromium.org/8816001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_unittest.cc
+++ chrome/browser/importer/importer_unittest.cc
@@ -270,7 +270,7 @@ class TestObserver : public ProfileWriter,
   size_t password_count_;
 };
 
-bool CreateUrlFile(std::wstring file, std::wstring url) {
+bool CreateUrlFile(const string16& file, const string16& url) {
   base::win::ScopedComPtr<IUniformResourceLocator> locator;
   HRESULT result = locator.CreateInstance(CLSID_InternetShortcut, NULL,
                                           CLSCTX_INPROC_SERVER);
@@ -332,7 +332,7 @@ void WritePStore(IPStore* pstore, const GUID* type, const GUID* subtype) {
 TEST_F(ImporterTest, IEImporter) {
   // Sets up a favorites folder.
   base::win::ScopedCOMInitializer com_init;
-  std::wstring path = temp_dir_.path().AppendASCII("Favorites").value();
+  string16 path = temp_dir_.path().AppendASCII("Favorites").value();
   CreateDirectory(path.c_str(), NULL);
   CreateDirectory((path + L"\\SubFolder").c_str(), NULL);
   CreateDirectory((path + L"\\Links").c_str(), NULL);
@@ -425,8 +425,8 @@ TEST_F(ImporterTest, IE7Importer) {
   decrypted_data2.resize(arraysize(data2));
   memcpy(&decrypted_data2.front(), data2, sizeof(data2));
 
-  std::wstring password;
-  std::wstring username;
+  string16 password;
+  string16 username;
   ASSERT_TRUE(ie7_password::GetUserPassFromData(decrypted_data1, &username,
                                                 &password));
   EXPECT_EQ(L"abcdefgh", username);
@@ -543,7 +543,7 @@ class FirefoxObserver : public ProfileWriter,
     EXPECT_EQ(arraysize(kFirefox2Passwords), password_count_);
     EXPECT_EQ(arraysize(kFirefox2Keywords), keyword_count_);
     EXPECT_EQ(kFirefox2Keywords[kDefaultFirefox2KeywordIndex].keyword,
-              default_keyword_);
+              UTF16ToWideHack(default_keyword_));
     EXPECT_EQ(kFirefox2Keywords[kDefaultFirefox2KeywordIndex].url,
               default_keyword_url_);
   }
@@ -611,7 +611,7 @@ class FirefoxObserver : public ProfileWriter,
     if (default_keyword_index != -1) {
       EXPECT_LT(default_keyword_index, static_cast<int>(template_urls.size()));
       TemplateURL* default_turl = template_urls[default_keyword_index];
-      default_keyword_ = UTF16ToWideHack(default_turl->keyword());
+      default_keyword_ = default_turl->keyword();
       default_keyword_url_ = default_turl->url()->url();
     }
 
@@ -628,7 +628,7 @@ class FirefoxObserver : public ProfileWriter,
   size_t history_count_;
   size_t password_count_;
   size_t keyword_count_;
-  std::wstring default_keyword_;
+  string16 default_keyword_;
   std::string default_keyword_url_;
 };
 
@@ -746,7 +746,7 @@ class Firefox3Observer : public ProfileWriter,
     if (import_search_engines_) {
       EXPECT_EQ(arraysize(kFirefox3Keywords), keyword_count_);
       EXPECT_EQ(kFirefox3Keywords[kDefaultFirefox3KeywordIndex].keyword,
-                default_keyword_);
+                UTF16ToWideHack(default_keyword_));
       EXPECT_EQ(kFirefox3Keywords[kDefaultFirefox3KeywordIndex].url,
                 default_keyword_url_);
     }
@@ -820,7 +820,7 @@ class Firefox3Observer : public ProfileWriter,
     if (default_keyword_index != -1) {
       EXPECT_LT(default_keyword_index, static_cast<int>(template_urls.size()));
       TemplateURL* default_turl = template_urls[default_keyword_index];
-      default_keyword_ = UTF16ToWideHack(default_turl->keyword());
+      default_keyword_ = default_turl->keyword();
       default_keyword_url_ = default_turl->url()->url();
     }
 
@@ -838,7 +838,7 @@ class Firefox3Observer : public ProfileWriter,
   size_t password_count_;
   size_t keyword_count_;
   bool import_search_engines_;
-  std::wstring default_keyword_;
+  string16 default_keyword_;
   std::string default_keyword_url_;
 };
 
