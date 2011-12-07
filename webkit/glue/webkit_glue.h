6166f4b358b1e0d75b387811ed9a4dd6b6ef7dcf
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 21:07:38 +0000

    Don't attempt to forward declare StringPiece. #include the declaration, or remove the forward declaration where it was not actually used.
    1) Forward declaring StringPiece is generally discouraged because it prevents callers from benefiting from automatic coersion from string/char* types.
    2) A follow-up CL (http://codereview.chromium.org/8659047/) will make StringPiece a template, and thus awkward to forward declare. The very small number of places that were appropriately forward declaring it do not justify writing a 'string_piece_forward.h'.
    
    This particular CL would be one of the rare ones where a forward declaration would be 'appropriate', but it does not warrant forward-declaring both the template and the typedef, and the number of similar cases does not justify creating a _forward.h header.
    
    BUG=87634
    R=tony@chromium.org
    
    Review URL: http://codereview.chromium.org/8819022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113465 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkit_glue.h
+++ webkit/glue/webkit_glue.h
@@ -25,10 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 class SkBitmap;
 
-namespace base {
-class StringPiece;
-}
-
 namespace skia {
 class PlatformCanvas;
 }
--- webkit/tools/test_shell/test_shell.h
+++ webkit/tools/test_shell/test_shell.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_path.h"
 #include "base/scoped_temp_dir.h"
+#include "base/string_piece.h"
 #if defined(OS_MACOSX)
 #include "base/lazy_instance.h"
 #endif
@@ -57,10 +58,6 @@ class TestNotificationPresenter;
 class TestShellDevToolsAgent;
 class TestShellDevToolsClient;
 class TestWebViewDelegate;
-
-namespace base {
-class StringPiece;
-}
 
 namespace WebKit {
 class WebDeviceOrientationClientMock;
