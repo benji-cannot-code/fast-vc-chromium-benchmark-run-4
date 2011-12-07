972cc4906986dc800c8ac4be1b0ff051d29ef344
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 20:35:49 +0000

    Convert keysym to keycode for modifiers Set mask after sending modifier keys. Print unknown keysym in hex and add warning for unknown keycode.
    BUG=106630
    TEST=All BrowserKeyEventsTest except for AccessKey will pass
    
    
    Review URL: http://codereview.chromium.org/8836007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113456 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/keycodes/keyboard_code_conversion_x.cc
+++ ui/base/keycodes/keyboard_code_conversion_x.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 
 namespace ui {
@@ -44,6 +45,7 @@ KeyboardCode KeyboardCodeFromXKeysym(unsigned int keysym) {
       return VKEY_DELETE;
     case XK_Tab:
     case XK_KP_Tab:
+    case XK_ISO_Left_Tab:
       return VKEY_TAB;
     case XK_Linefeed:
     case XK_Return:
@@ -314,8 +316,7 @@ KeyboardCode KeyboardCodeFromXKeysym(unsigned int keysym) {
 
     // TODO(sad): some keycodes are still missing.
   }
-
-  DLOG(WARNING) << "Unknown keycode: " << keysym;
+  DLOG(WARNING) << "Unknown keysym: " << StringPrintf("0x%x", keysym);
   return VKEY_UNKNOWN;
 }
 
@@ -617,6 +618,7 @@ int XKeysymForWindowsKeyCode(KeyboardCode keycode, bool shift) {
       return XK_F1 + (keycode - VKEY_F1);
 
     default:
+      LOG(WARNING) << "Unknown keycode:" << keycode;
       return 0;
     }
 }
