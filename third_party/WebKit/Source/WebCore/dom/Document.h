5dc7e2a9f3921229eb6434f81fe91deb46953678
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-07 23:41:21 +0000

    Defer ScriptExecutionContext::Task's in Document when page loading is deferred. Schedule them with timer when page loading is resumed. The tasks will be performed in the original order. This fixes the problem that database callbacks could be missed when page loading was deferred. https://bugs.webkit.org/show_bug.cgi?id=49401
    Patch by Yong Li <yoli@rim.com> on 2011-12-07
    Reviewed by Darin Adler.
    
    Manual test added: ManualTests/database-callback-deferred.html.
    
    * dom/Document.cpp:
    (WebCore::Document::Document):
    (WebCore::Document::didReceiveTask):
    (WebCore::Document::postTask):
    (WebCore::Document::pendingTasksTimerFired):
    (WebCore::Document::suspendScheduledTasks):
    (WebCore::Document::resumeScheduledTasks):
    * dom/Document.h:
    * page/PageGroupLoadDeferrer.cpp:
    (WebCore::PageGroupLoadDeferrer::PageGroupLoadDeferrer):
    (WebCore::PageGroupLoadDeferrer::~PageGroupLoadDeferrer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -1115,7 +1115,7 @@ public:
     unsigned wheelEventHandlerCount() const { return m_wheelEventHandlerCount; }
     void didAddWheelEventHandler();
     void didRemoveWheelEventHandler();
-    
+
     bool visualUpdatesAllowed() const;
 
 #if ENABLE(MICRODATA)
@@ -1124,6 +1124,9 @@ public:
 #endif
     
     bool isInDocumentWrite() { return m_writeRecursionDepth > 0; }
+
+    void suspendScheduledTasks();
+    void resumeScheduledTasks();
 
 protected:
     Document(Frame*, const KURL&, bool isXHTML, bool isHTML);
@@ -1171,6 +1174,10 @@ private:
     PassRefPtr<NodeList> handleZeroPadding(const HitTestRequest&, HitTestResult&) const;
 
     void loadEventDelayTimerFired(Timer<Document>*);
+
+    void pendingTasksTimerFired(Timer<Document>*);
+
+    static void didReceiveTask(void*);
 
 #if ENABLE(PAGE_VISIBILITY_API)
     PageVisibilityState visibilityState() const;
@@ -1434,6 +1441,9 @@ private:
 #if ENABLE(REQUEST_ANIMATION_FRAME)
     OwnPtr<ScriptedAnimationController> m_scriptedAnimationController;
 #endif
+
+    Timer<Document> m_pendingTasksTimer;
+    Vector<OwnPtr<Task> > m_pendingTasks;
 };
 
 // Put these methods here, because they require the Document definition, but we really want to inline them.
