643c30a3b0f0c2a2dc0489cc8832efcc93bf02f2
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 18:27:28 +0000

    Revert 113409 - Add a new method to SSLClientSocket:
      was_origin_cert_sent()
    
    This will help SpdySession decide to send a CREDENTIAL frame.
    
    BUG=106103
    TEST=none
    
    Review URL: http://codereview.chromium.org/8817021
    
    TBR=rch@chromium.org
    Review URL: http://codereview.chromium.org/8850003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113419 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_nss.cc
+++ net/socket/ssl_client_socket_nss.cc
@@ -1718,7 +1718,6 @@ int SSLClientSocketNSS::DoGetOBCertComplete(int result) {
     return MapNSSError(PORT_GetError());
 
   GotoState(STATE_HANDSHAKE);
-  set_was_origin_bound_cert_sent(true);
   return OK;
 }
 
@@ -2274,9 +2273,7 @@ SECStatus SSLClientSocketNSS::OriginBoundClientAuthHandler(
     // Synchronous success.
     int result = ImportOBCertAndKey(result_certificate,
                                     result_private_key);
-    if (result == OK)
-      set_was_origin_bound_cert_sent(true);
-    else
+    if (result != OK)
       rv = SECFailure;
   } else {
     rv = SECFailure;  // Synchronous failure.
