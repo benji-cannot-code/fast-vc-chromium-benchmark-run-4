4b237687310f9c8a1228810a18124b015523c61b
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 18:54:17 +0000

    Replace media::Limits struct with media::limits namespace and update documentation.
    Review URL: http://codereview.chromium.org/8786013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113425 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/buffers.cc
+++ media/base/buffers.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,9 @@ namespace media {
 
 const base::TimeDelta kNoTimestamp =
     base::TimeDelta::FromMicroseconds(kint64min);
+
+const base::TimeDelta kInfiniteDuration =
+    base::TimeDelta::FromMicroseconds(kint64max);
 
 StreamSample::StreamSample() {}
 
