81da216831b898277d8ef060b0067b8e63ab20af
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 19:23:16 +0000

    Setup TestShellPlatformDelegate before MessageLoop.
    MessagePumpMac wants NSApp to be initialized before the UI MessageLoop
    is created.
    
    BUG=102224
    
    
    Review URL: http://codereview.chromium.org/8844001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113437 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/run_all_tests.cc
+++ webkit/tools/test_shell/run_all_tests.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,8 +45,8 @@ class TestShellTestSuite : public base::TestSuite {
  public:
   TestShellTestSuite(int argc, char** argv)
       : base::TestSuite(argc, argv),
-        test_shell_webkit_init_(true),
-        platform_delegate_(*CommandLine::ForCurrentProcess()) {
+        platform_delegate_(*CommandLine::ForCurrentProcess()),
+        test_shell_webkit_init_(true) {
   }
 
   virtual void Initialize() {
@@ -94,14 +94,14 @@ class TestShellTestSuite : public base::TestSuite {
   }
 
  private:
+  TestShellPlatformDelegate platform_delegate_;
+
   // Allocate a message loop for this thread.  Although it is not used
   // directly, its constructor sets up some necessary state.
   MessageLoopForUI main_message_loop_;
 
   // Initialize WebKit for this scope.
   TestShellWebKitInit test_shell_webkit_init_;
-
-  TestShellPlatformDelegate platform_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(TestShellTestSuite);
 };
