4e5cbfeaceab5cf6905e58277024846572cf9587
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-07 09:56:41 +0000

    [Qt][WK2] Enable page cache. https://bugs.webkit.org/show_bug.cgi?id=73938
    Enable WebCore's page cache for Qt port by setting its capacity to 20. This is temporary
    and will be replaced by a computed value based on CacheMode. See bug#73918.
    
    Patch by Zalan Bujtas <zbujtas@gmail.com> on 2011-12-07
    Reviewed by Simon Hausmann.
    
    * WebProcess/qt/WebProcessQt.cpp:
    (WebKit::WebProcess::platformSetCacheModel):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/qt/WebProcessQt.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/qt/WebProcessQt.cpp
@@ -36,17 +36,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QNetworkAccessManager>
 #include <QNetworkCookieJar>
 #include <WebCore/CookieJarQt.h>
+#include <WebCore/PageCache.h>
 #include <WebCore/RuntimeEnabledFeatures.h>
 
 #if defined(Q_OS_MACX)
 #include <dispatch/dispatch.h>
 #endif
 
+using namespace WebCore;
+
 namespace WebKit {
+
+static const int DefaultPageCacheCapacity = 20;
 
 void WebProcess::platformSetCacheModel(CacheModel)
 {
-    // FIXME: Implement.
+    // FIXME: see bug 73918
+    pageCache()->setCapacity(DefaultPageCacheCapacity);
 }
 
 void WebProcess::platformClearResourceCaches(ResourceCachesToClear)
