b96677fc3b3bbda9921a34dd79502e2860c31bd7
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-07 15:32:58 +0000

    [Qt] V8 debug build fixes.
    Reviewed by Tor Arne Vestbø.
    
    * Target.pri: Add missing files to the build.
    * loader/SubresourceLoader.cpp: Add missing CString.h header file inclusion,
    that is implicitly included with Chromium builds.
    * loader/cache/CachedResource.cpp: Ditto.
    * page/FrameTree.cpp: Ditto.
    * platform/graphics/MediaPlayer.cpp: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/MediaPlayer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/MediaPlayer.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MediaPlayerPrivate.h"
 #include "Settings.h"
 #include "TimeRanges.h"
+#include <wtf/text/CString.h>
 
 #if PLATFORM(QT)
 #include <QtGlobal>
