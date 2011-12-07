b06762f9830e085ec024d892496b43fdbe3346a4
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-07 22:06:18 +0000

    Revert "Revert 113250 - Add CommandBuffer::SetGetBuffer"
    This reverts commit bd45bd252aeb8babac62547a5c605fbf64287cd3.
    
    TEST=ran webkit tests in DRT and webgl tests in chrome
    BUG=103989
    
    Review URL: http://codereview.chromium.org/8758026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113479 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/demos/framework/window.cc
+++ gpu/demos/framework/window.cc
@@ -55,7 +55,7 @@ void Window::OnPaint() {
 
 bool Window::CreateRenderContext(gfx::PluginWindowHandle hwnd) {
   command_buffer_.reset(new CommandBufferService);
-  if (!command_buffer_->Initialize(kCommandBufferSize)) {
+  if (!command_buffer_->Initialize()) {
     return false;
   }
 
