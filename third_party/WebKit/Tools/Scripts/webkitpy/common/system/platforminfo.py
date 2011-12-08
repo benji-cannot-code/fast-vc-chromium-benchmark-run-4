4b10204608f06526460e1554355b6c995979ca9c
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 01:50:51 +0000

    Use free memory to determine if we have space for DRT instances instead of total memory https://bugs.webkit.org/show_bug.cgi?id=74021
    Reviewed by Adam Barth.
    
    This will hopefully make the Mac buildbots behave better.
    
    I also reduced the expected memory needed per DRT now that
    we're properly accounting for the system memory in our calculations.
    
    * Scripts/webkitpy/common/system/platforminfo.py:
    (PlatformInfo.total_bytes_memory):
    (PlatformInfo._compute_free_bytes_from_vm_stat_output):
    * Scripts/webkitpy/common/system/platforminfo_mock.py:
    (MockPlatformInfo.total_bytes_memory):
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.default_child_processes):
    * Scripts/webkitpy/layout_tests/port/base_unittest.py:
    (PortTest.test_default_child_processes):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/platforminfo.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/platforminfo.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import platform
+import re
 
 
 # We use this instead of calls to platform directly to allow mocking.
@@ -49,4 +50,20 @@ class PlatformInfo(object):
         system_name = platform.system()
         if system_name == "Darwin":
             return int(self._executive.run_command(["sysctl", "-n", "hw.memsize"]))
+        return None
+
+    def _compute_free_bytes_from_vm_stat_output(self, vm_stat_output):
+        page_size_match = re.search(r"page size of (\d+) bytes", vm_stat_output)
+        free_pages_match = re.search(r"Pages free:\s+(\d+).", vm_stat_output)
+        if not page_size_match or not free_pages_match:
+            return None
+        free_page_count = int(free_pages_match.group(1))
+        page_size = int(page_size_match.group(1))
+        return free_page_count * page_size
+
+    def free_bytes_memory(self):
+        system_name = platform.system()
+        if system_name == "Darwin":
+            vm_stat_output = self._executive.run_command(["vm_stat"])
+            return self._compute_free_bytes_from_vm_stat_output(vm_stat_output)
         return None
