d0ca57de40e7a27b57021ec068a418985636901e
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 21:16:06 +0000

    Revert 113656 - Implement a proxy for Pepper FileIO.
    This splits apart the old in-process implementation into a new object in shared_impl that does most of the general tracking. This alllows that code to be shared by the proxy.
    
    BUG=http://crbug.com/101154
    
    Review URL: http://codereview.chromium.org/8764003
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/8890037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113659 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/file_type_conversions.h
+++ webkit/plugins/ppapi/file_type_conversions.h
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef PPAPI_SHARED_IMPL_FILE_TYPE_CONVERSION_H_
-#define PPAPI_SHARED_IMPL_FILE_TYPE_CONVERSION_H_
+#ifndef WEBKIT_PLUGINS_PPAPI_FILE_TYPE_CONVERSIONS_H_
+#define WEBKIT_PLUGINS_PPAPI_FILE_TYPE_CONVERSIONS_H_
 
 #include "base/platform_file.h"
 #include "ppapi/c/pp_stdint.h"
 
+namespace webkit {
 namespace ppapi {
 
 int PlatformFileErrorToPepperError(base::PlatformFileError error_code);
@@ -20,5 +21,6 @@ bool PepperFileOpenFlagsToPlatformFileFlags(int32_t pp_open_flags,
                                             int* flags_out);
 
 }  // namespace ppapi
+}  // namespace webkit
 
-#endif  // PPAPI_SHARED_IMPL_FILE_TYPE_CONVERSION_H_
+#endif  // WEBKIT_PLUGINS_PPAPI_FILE_TYPE_CONVERSIONS_H_
