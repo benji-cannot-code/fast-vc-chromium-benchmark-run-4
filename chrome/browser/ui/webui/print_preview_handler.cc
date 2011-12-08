1a5fc63f54f761834a4a8b9839a8b09ccb6d4a43
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 07:10:06 +0000

    Print preview: Delete metafiles on the right thread.
    BUG=106665
    TEST=see bug.
    
    Review URL: http://codereview.chromium.org/8872008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview_handler.cc
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using content::BrowserThread;
+using printing::Metafile;
 
 namespace {
 
@@ -174,12 +175,15 @@ void ReportPrintSettingsStats(const DictionaryValue& settings) {
   }
 }
 
-}  // namespace
-
 // Callback that stores a PDF file on disk.
-void PrintToPdfCallback(printing::Metafile* metafile, const FilePath& path) {
+void PrintToPdfCallback(Metafile* metafile, const FilePath& path) {
   metafile->SaveTo(path);
+  // |metafile| must be deleted on the UI thread.
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
+                          base::Bind(&DeletePointer<Metafile>, metafile));
 }
+
+}  // namespace
 
 // static
 FilePath* PrintPreviewHandler::last_saved_path_ = NULL;
@@ -815,8 +819,9 @@ void PrintPreviewHandler::PostPrintToPdfTask() {
   DCHECK(data.get());
   printing::PreviewMetafile* metafile = new printing::PreviewMetafile;
   metafile->InitFromData(static_cast<const void*>(data->front()), data->size());
+  // PrintToPdfCallback takes ownership of |metafile|.
   BrowserThread::PostTask(BrowserThread::FILE, FROM_HERE,
-                          base::Bind(&PrintToPdfCallback, base::Owned(metafile),
+                          base::Bind(&PrintToPdfCallback, metafile,
                                      *print_to_pdf_path_));
   print_to_pdf_path_.reset();
   ActivateInitiatorTabAndClosePreviewTab();
