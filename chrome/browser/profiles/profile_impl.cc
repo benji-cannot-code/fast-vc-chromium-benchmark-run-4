081afd1c7692dfb9ea38ea8885bc5e8456f21dbd
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 08:52:10 +0000

    Removed flag --expose-private-extension-api. Added --load-component-extension.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8659002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113578 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/path_service.h"
+#include "base/string_tokenizer.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
@@ -437,6 +438,21 @@ void ProfileImpl::InitExtensions(bool extensions_enabled) {
       extensions_enabled));
 
   extension_service_->component_loader()->AddDefaultComponentExtensions();
+  if (command_line->HasSwitch(switches::kLoadComponentExtension)) {
+    CommandLine::StringType path_list = command_line->GetSwitchValueNative(
+        switches::kLoadComponentExtension);
+    StringTokenizerT<CommandLine::StringType,
+        CommandLine::StringType::const_iterator> t(path_list,
+                                                   FILE_PATH_LITERAL(","));
+    while (t.GetNext()) {
+      // Load the component extension manifest synchronously.
+      // Blocking the UI thread is acceptable here since
+      // this flag designated for developers.
+      base::ThreadRestrictions::ScopedAllowIO allow_io;
+      extension_service_->component_loader()->AddOrReplace(
+          FilePath(t.token()));
+    }
+  }
   extension_service_->Init();
 
   if (extensions_enabled) {
