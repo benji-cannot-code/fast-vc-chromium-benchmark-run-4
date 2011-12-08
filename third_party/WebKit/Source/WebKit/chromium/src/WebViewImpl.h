43ee198692ed0eb784f24915b65b2c4d5e0efd9f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 01:53:42 +0000

    [chromium] Preserve original pageScale limits in WebViewImpl https://bugs.webkit.org/show_bug.cgi?id=72983
    Patch by Alexandre Elias <aelias@google.com> on 2011-12-07
    Reviewed by Darin Fisher.
    
    Preserve the originally viewport-tag specified page scale limits, and
    compute the final ones in a new private method
    computePageScaleFactorLimits().  This is then called when contents
    size change (which can affect the minimum).
    
    Also clean up several minor bugs, namely clamping issues (reorder
    clampNegativeToZero to avoid negative scroll offsets, and force max to
    be greater than min), and incorrect behavior in the presence of
    "solid" scrollbars.
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::contentsSizeChanged):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::WebViewImpl):
    (WebKit::WebViewImpl::clampPageScaleFactorToLimits):
    (WebKit::WebViewImpl::clampOffsetAtScale):
    (WebKit::WebViewImpl::setPageScaleFactorPreservingScrollOffset):
    (WebKit::WebViewImpl::setPageScaleFactor):
    (WebKit::WebViewImpl::setPageScaleFactorLimits):
    (WebKit::WebViewImpl::computePageScaleFactorLimits):
    (WebKit::WebViewImpl::didChangeContentsSize):
    * src/WebViewImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
@@ -336,6 +336,8 @@ public:
     // unless the view did not need a layout.
     void layoutUpdated(WebFrameImpl*);
 
+    void didChangeContentsSize();
+
     // Returns true if popup menus should be rendered by the browser, false if
     // they should be rendered by WebKit (which is the default).
     static bool useExternalPopupMenus();
@@ -462,7 +464,8 @@ public:
     bool hasVerticalScrollbar();
 
 private:
-    float computePageScaleFactorWithinLimits(float scale);
+    bool computePageScaleFactorLimits();
+    float clampPageScaleFactorToLimits(float scale);
     WebPoint clampOffsetAtScale(const WebPoint& offset, float scale);
 
     friend class WebView;  // So WebView::Create can call our constructor
@@ -569,8 +572,9 @@ private:
 
     double m_maximumZoomLevel;
 
+    float m_pageDefinedMinimumPageScaleFactor;
+    float m_pageDefinedMaximumPageScaleFactor;
     float m_minimumPageScaleFactor;
-
     float m_maximumPageScaleFactor;
 
     bool m_contextMenuAllowed;
