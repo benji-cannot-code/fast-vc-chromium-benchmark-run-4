52f266d4caf3c45f9405c5eb00ca6ff5c197acc1
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 18:12:40 +0000

    Reverting 8833008 which breaks CandidateWindowViewTest.MozcUpdateCandidateTest
    TBR=nona@chromium.org
    BUG=106862
    TEST=CandidateWindowViewTest.MozcUpdateCandidateTest
    
    Review URL: http://codereview.chromium.org/8879023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113624 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window_view.h
+++ chrome/browser/chromeos/input_method/candidate_window_view.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_INPUT_METHOD_CANDIDATE_WINDOW_VIEW_H_
 #define CHROME_BROWSER_CHROMEOS_INPUT_METHOD_CANDIDATE_WINDOW_VIEW_H_
 
-#include "base/gtest_prod_util.h"
 #include "chrome/browser/chromeos/input_method/ibus_ui_controller.h"
 #include "ui/views/view.h"
 
@@ -135,8 +134,6 @@ class CandidateWindowView : public views::View {
   virtual void OnBoundsChanged(const gfx::Rect& previous_bounds) OVERRIDE;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(CandidateWindowViewTest, MozcUpdateCandidateTest);
-
   // Initializes the candidate views if needed.
   void MaybeInitializeCandidateViews(
       const InputMethodLookupTable& lookup_table);
