fe5d74407c3c7eff8af62a562899004e15d80351
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 02:36:39 +0000

    Remove some custom Task implementations from chrome/.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8866007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/worker_thread_ticker.cc
+++ chrome/common/worker_thread_ticker.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,31 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/task.h"
 #include "base/threading/thread.h"
-
-class WorkerThreadTicker::TimerTask : public Task {
- public:
-  explicit TimerTask(WorkerThreadTicker* ticker) : ticker_(ticker) {
-  }
-
-  virtual void Run() {
-    // When the ticker is running, the handler list CANNOT be modified.
-    // So we can do the enumeration safely without a lock
-    TickHandlerListType* handlers = &ticker_->tick_handler_list_;
-    for (TickHandlerListType::const_iterator i = handlers->begin();
-         i != handlers->end(); ++i) {
-      (*i)->OnTick();
-    }
-
-    ticker_->ScheduleTimerTask();
-  }
-
- private:
-  WorkerThreadTicker* ticker_;
-};
 
 WorkerThreadTicker::WorkerThreadTicker(int tick_interval)
     : timer_thread_("worker_thread_ticker"),
@@ -97,6 +78,21 @@ bool WorkerThreadTicker::Stop() {
 }
 
 void WorkerThreadTicker::ScheduleTimerTask() {
-  timer_thread_.message_loop()->PostDelayedTask(FROM_HERE, new TimerTask(this),
-                                                tick_interval_);
+  timer_thread_.message_loop()->PostDelayedTask(
+      FROM_HERE,
+      base::Bind(&WorkerThreadTicker::TimerTask, base::Unretained(this)),
+      tick_interval_);
 }
+
+void WorkerThreadTicker::TimerTask() {
+  // When the ticker is running, the handler list CANNOT be modified.
+  // So we can do the enumeration safely without a lock
+  const TickHandlerListType& handlers = tick_handler_list_;
+  for (TickHandlerListType::const_iterator i = handlers.begin();
+       i != handlers.end(); ++i) {
+    (*i)->OnTick();
+  }
+
+  ScheduleTimerTask();
+}
+
