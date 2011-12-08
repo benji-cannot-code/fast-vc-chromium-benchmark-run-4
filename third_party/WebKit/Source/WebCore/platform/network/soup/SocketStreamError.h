29440bbb3c4042268206c0b114eafa2980cc4918
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 15:45:39 +0000

    Provide more specific error description for SocketStreamError. https://bugs.webkit.org/show_bug.cgi?id=74066
    Patch by Takashi Toyoshima <toyoshim@chromium.org> on 2011-12-08
    Reviewed by Martin Robinson.
    
    No new tests because this change just improve error messages for unexpected failures.
    
    * platform/network/soup/SocketStreamError.h: Add an argument for passing error description.
    (WebCore::SocketStreamError::SocketStreamError):
    * platform/network/soup/SocketStreamHandleSoup.cpp: Add error description for SocketStreamError.
    (WebCore::SocketStreamHandle::connected):
    (WebCore::SocketStreamHandle::readBytes):
    (WebCore::SocketStreamHandle::platformSend):
    (WebCore::SocketStreamHandle::platformClose):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/soup/SocketStreamError.h
+++ third_party/WebKit/Source/WebCore/platform/network/soup/SocketStreamError.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Google Inc.  All rights reserved.
+ * Copyright (C) 2009, 2011 Google Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -39,8 +39,8 @@ namespace WebCore {
     class SocketStreamError : public SocketStreamErrorBase {
     public:
         SocketStreamError() { }
-        explicit SocketStreamError(int errorCode)
-            : SocketStreamErrorBase(errorCode)
+        explicit SocketStreamError(int errorCode, const gchar* description)
+            : SocketStreamErrorBase(errorCode, String(), String(description))
         {
         }
 
