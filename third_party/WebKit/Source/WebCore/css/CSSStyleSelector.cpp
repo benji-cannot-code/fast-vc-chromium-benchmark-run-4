8abe3f65ff1b46f2b2559ee02c9261ab57b246de
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 05:08:32 +0000

    Implement border image source properties in CSSStyleApplyProperty. https://bugs.webkit.org/show_bug.cgi?id=73981
    Reviewed by Andreas Kling.
    
    No new tests / refactoring only.
    
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::ApplyPropertyBorderImageSource::applyValue):
    (WebCore::ApplyPropertyBorderImageSource::createHandler):
    (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -2709,18 +2709,6 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(whiteSpace, WhiteSpace)
         return;
 // uri || inherit
-    case CSSPropertyBorderImageSource:
-    {
-        HANDLE_INHERIT_AND_INITIAL(borderImageSource, BorderImageSource)
-        m_style->setBorderImageSource(styleImage(CSSPropertyBorderImageSource, value));
-        return;
-    }
-    case CSSPropertyWebkitMaskBoxImageSource:
-    {
-        HANDLE_INHERIT_AND_INITIAL(maskBoxImageSource, MaskBoxImageSource)
-        m_style->setMaskBoxImageSource(styleImage(CSSPropertyWebkitMaskBoxImageSource, value));
-        return;
-    }
     case CSSPropertyWordBreak:
         HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(wordBreak, WordBreak)
         return;
@@ -3870,6 +3858,8 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWebkitMaskBoxImageSlice:
     case CSSPropertyBorderImageWidth:
     case CSSPropertyWebkitMaskBoxImageWidth:
+    case CSSPropertyBorderImageSource:
+    case CSSPropertyWebkitMaskBoxImageSource:
     case CSSPropertyBorderTop:
     case CSSPropertyBorderRight:
     case CSSPropertyBorderBottom:
