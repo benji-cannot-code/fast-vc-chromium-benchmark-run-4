8fb60bf96c0f78050e9353e8266a4edbe3de471a
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 22:29:39 +0000

    Use Skia's implementation of Gaussian blur when accelerated filters are enabled. https://bugs.webkit.org/show_bug.cgi?id=73949
    Reviewed by Zoltan Herczeg.
    
    In the future, this will be covered by the SVG tests run in GPU mode.
    
    * WebCore.gypi:
    Add FEGaussianBlurSkia.cpp to the build.
    * platform/graphics/filters/FEGaussianBlur.cpp:
    (WebCore::FEGaussianBlur::platformApplySoftware):
    Call out to platformApplySkia() when USE_SKIA is enabled.
    * platform/graphics/filters/FEGaussianBlur.h:
    platformApplySkia() declaration.
    * platform/graphics/filters/skia: Added.
    * platform/graphics/filters/skia/FEGaussianBlurSkia.cpp: Added.
    (WebCore::FEGaussianBlur::platformApplySkia):
    On the Skia port, use SkBlurImageFilter for drawing
    Gaussian blurs in accelerated mode.
    * platform/graphics/skia/ImageBufferSkia.cpp:
    (WebCore::ImageBuffer::copyImage):
    Implement ImageBuffer::copyImage() with DontCopyBackingStore semantics.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102385 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
@@ -144,8 +144,7 @@ size_t ImageBuffer::dataSize() const
 
 PassRefPtr<Image> ImageBuffer::copyImage(BackingStoreCopy copyBehavior) const
 {
-    ASSERT(copyBehavior == CopyBackingStore);
-    return BitmapImageSingleFrameSkia::create(*m_data.m_platformContext.bitmap(), true);
+    return BitmapImageSingleFrameSkia::create(*m_data.m_platformContext.bitmap(), copyBehavior == CopyBackingStore);
 }
 
 PlatformLayer* ImageBuffer::platformLayer() const
