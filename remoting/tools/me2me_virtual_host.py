e6bad1f5a795a71a277702b8c6628e6350742c9c
lambroslambrou@google.com <lambroslambrou@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 22:37:39 +0000

    Handle SIGHUP in me2me_virtual_host.py
    BUG=106805
    TEST=Follow repro steps
    
    Review URL: http://codereview.chromium.org/8888001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113680 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/tools/me2me_virtual_host.py
+++ remoting/tools/me2me_virtual_host.py
@@ -272,7 +272,7 @@ class Desktop:
 def main():
   atexit.register(cleanup)
 
-  for s in [signal.SIGINT, signal.SIGTERM]:
+  for s in [signal.SIGHUP, signal.SIGINT, signal.SIGTERM]:
     signal.signal(s, signal_handler)
 
   # Ensure full path to config directory exists.
