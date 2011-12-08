a92b3535840c2dc752c0de54ca8f99932bca61a0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 10:46:32 +0000

    Refactoring: Editor::requestCheckingFor should take SpellCheckRequest object. https://bugs.webkit.org/show_bug.cgi?id=74033
    Patch by Shinya Kawanaka <shinyak@google.com> on 2011-12-08
    Reviewed by Hajime Morita.
    
    SpellChecker::requestCheckingFor takes SpellCheckRequest object in order to make it easy to
    pass necessary information to requestCheckingFor.
    
    No new tests. Covered by existing tests.
    
    * editing/Editor.cpp:
    (WebCore::Editor::replaceSelectionWithFragment):
      Uses the new interface.
    (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges): ditto.
    * editing/SpellChecker.cpp:
    (WebCore::SpellCheckRequest::SpellCheckRequest):
    (WebCore::SpellCheckRequest::~SpellCheckRequest):
    (WebCore::SpellCheckRequest::create):
      Creates a new SpellCheckRequest object.
    (WebCore::SpellChecker::requestCheckingFor):
      Uses the new interface.
    (WebCore::SpellChecker::didCheck):
    * editing/SpellChecker.h:
    (WebCore::SpellCheckRequest::setSequence):
    (WebCore::SpellCheckRequest::sequence):
    (WebCore::SpellCheckRequest::checkingRange):
    (WebCore::SpellCheckRequest::paragraphRange):
    (WebCore::SpellCheckRequest::text):
    (WebCore::SpellCheckRequest::mask):
    (WebCore::SpellCheckRequest::rootEditableElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102328 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/SpellChecker.h
+++ third_party/WebKit/Source/WebCore/editing/SpellChecker.h
@@ -27,10 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SpellChecker_h
 #define SpellChecker_h
 
+#include "Element.h"
 #include "PlatformString.h"
+#include "Range.h"
 #include "TextChecking.h"
 #include "Timer.h"
 #include <wtf/Deque.h>
+#include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
 #include <wtf/Noncopyable.h>
 #include <wtf/Vector.h>
@@ -41,7 +44,30 @@ class Frame;
 class Node;
 class TextCheckerClient;
 struct TextCheckingResult;
-class Range;
+
+class SpellCheckRequest : public RefCounted<SpellCheckRequest> {
+public:
+    SpellCheckRequest(int sequence, PassRefPtr<Range> checkingRange, PassRefPtr<Range> paragraphRange, const String&, TextCheckingTypeMask);
+    ~SpellCheckRequest();
+
+    static PassRefPtr<SpellCheckRequest> create(TextCheckingTypeMask, PassRefPtr<Range> checkingRange, PassRefPtr<Range> paragraphRange);
+
+    void setSequence(int sequence) { m_sequence = sequence; }
+    int sequence() const { return m_sequence; }
+    PassRefPtr<Range> checkingRange() const { return m_checkingRange; }
+    PassRefPtr<Range> paragraphRange() const { return m_paragraphRange; }
+    const String& text() const { return m_text; }
+    TextCheckingTypeMask mask() const { return m_mask; }
+    PassRefPtr<Element> rootEditableElement() const { return m_rootEditableElement; }
+private:
+
+    int m_sequence;
+    RefPtr<Range> m_checkingRange;
+    RefPtr<Range> m_paragraphRange;
+    String m_text;
+    TextCheckingTypeMask m_mask;
+    RefPtr<Element> m_rootEditableElement;
+};
 
 class SpellChecker {
     WTF_MAKE_NONCOPYABLE(SpellChecker);
@@ -51,7 +77,8 @@ public:
 
     bool isAsynchronousEnabled() const;
     bool isCheckable(Range*) const;
-    void requestCheckingFor(TextCheckingTypeMask, PassRefPtr<Range>);
+
+    void requestCheckingFor(PassRefPtr<SpellCheckRequest>);
     void didCheck(int sequence, const Vector<TextCheckingResult>&);
 
     int lastRequestSequence() const
@@ -65,11 +92,9 @@ public:
     }
 
 private:
-    class SpellCheckRequest;
     typedef Deque<RefPtr<SpellCheckRequest> > RequestQueue;
 
     bool canCheckAsynchronously(Range*) const;
-    PassRefPtr<SpellCheckRequest> createRequest(TextCheckingTypeMask, PassRefPtr<Range>);
     TextCheckerClient* client() const;
     void timerFiredToProcessQueuedRequest(Timer<SpellChecker>*);
     void invokeRequest(PassRefPtr<SpellCheckRequest>);
