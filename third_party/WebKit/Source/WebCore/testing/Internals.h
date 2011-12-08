c761a46c25a08eb23d5c651b92d14a17068a7b09
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 10:12:22 +0000

    Unreviewed, rolling out r102323. http://trac.webkit.org/changeset/102323 https://bugs.webkit.org/show_bug.cgi?id=74069
    Caused Chromium and GTK build failure (Requested by bashi on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-12-08
    
    .:
    
    * Source/autotools/symbols.filter:
    
    Source/WebCore:
    
    * testing/Internals.cpp:
    (WebCore::Internals::getPageScaleFactor):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    LayoutTests:
    
    * compositing/geometry/fixed-position-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-composited-page-scale.html:
    * compositing/geometry/fixed-position-iframe-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-iframe-composited-page-scale.html:
    * compositing/geometry/fixed-position-transform-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-transform-composited-page-scale.html:
    * compositing/scaling/tiled-layer-recursion.html:
    * fast/dom/Element/scale-page-bounding-client-rect.html:
    * fast/dom/Element/scale-page-client-rects.html:
    * fast/dom/Range/scale-page-bounding-client-rect.html:
    * fast/dom/Range/scale-page-client-rects.html:
    * fast/events/scale-and-scroll-body.html:
    * fast/events/scale-and-scroll-iframe-body.html:
    * fast/events/scale-and-scroll-iframe-window.html:
    * fast/events/scale-and-scroll-window.html:
    * fast/events/script-tests/page-scaled-mouse-click-iframe.js:
    (iframeLoaded):
    * fast/events/script-tests/page-scaled-mouse-click.js:
    * fast/events/scroll-in-scaled-page-with-overflow-hidden.html:
    * fast/events/touch/page-scaled-touch-gesture-click.html:
    * fast/frames/frame-set-rotation-hit.html:
    * fast/frames/frame-set-scaling-hit.html:
    * fast/frames/iframe-double-scale-contents.html:
    * fast/repaint/background-scaling.html:
    * fast/repaint/scale-page-shrink.html:
    * fast/transforms/selection-bounds-in-transformed-view.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102324 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.h
+++ third_party/WebKit/Source/WebCore/testing/Internals.h
@@ -84,6 +84,7 @@ public:
 
     void setEnableScrollAnimator(Document*, bool enabled, ExceptionCode&);
     void setZoomAnimatorTransform(Document*, float scale, float tx, float ty, ExceptionCode&);
+    float getPageScaleFactor(Document*,  ExceptionCode&);
     void setZoomParameters(Document*, float scale, float x, float y, ExceptionCode&);
 
     void setMockScrollbarsEnabled(Document*, bool enabled, ExceptionCode&);
@@ -112,9 +113,6 @@ public:
 
     int lastSpellCheckRequestSequence(Document*, ExceptionCode&);
     int lastSpellCheckProcessedSequence(Document*, ExceptionCode&);
-    
-    float pageScaleFactor(Document*,  ExceptionCode&);
-    void setPageScaleFactor(Document*, float scaleFactor, int x, int y, ExceptionCode&);
 
     static const char* internalsId;
 
