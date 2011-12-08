0e5dde95d55bbdb2377aed07b1048e7b79f933a1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-08 23:16:54 +0000

    https://bugs.webkit.org/show_bug.cgi?id=74005 fix unaligned access memory in generatePatternCharacterOnce function for SH4 platforms.
    Patch by Thouraya ANDOLSI <thouraya.andolsi@st.com> on 2011-12-08
    Reviewed by Gavin Barraclough.
    
    * assembler/MacroAssemblerARM.h:
    (JSC::MacroAssemblerARM::load16Unaligned):
    * assembler/MacroAssemblerARMv7.h:
    (JSC::MacroAssemblerARMv7::load16Unaligned):
    * assembler/MacroAssemblerMIPS.h:
    (JSC::MacroAssemblerMIPS::load16Unaligned):
    * assembler/MacroAssemblerSH4.h:
    (JSC::MacroAssemblerSH4::lshift32):
    (JSC::MacroAssemblerSH4::load8):
    (JSC::MacroAssemblerSH4::load16):
    (JSC::MacroAssemblerSH4::load16Unaligned):
    (JSC::MacroAssemblerSH4::branch8):
    * assembler/MacroAssemblerX86Common.h:
    (JSC::MacroAssemblerX86Common::load16Unaligned):
    * jit/JIT.h:
    * yarr/YarrJIT.cpp:
    (JSC::Yarr::YarrGenerator::generatePatternCharacterOnce):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102389 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerX86Common.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerX86Common.h
@@ -447,6 +447,11 @@ public:
         load32(address, dest);
     }
 
+    void load16Unaligned(BaseIndex address, RegisterID dest)
+    {
+        load16(address, dest);
+    }
+
     DataLabel32 load32WithAddressOffsetPatch(Address address, RegisterID dest)
     {
         m_assembler.movl_mr_disp32(address.offset, address.base, dest);
