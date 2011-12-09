6093aceb07e320925dfc0505668a47486aa28f49
kmixter@chromium.org <kmixter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 22:14:56 +0000

    Reflect ONC parse status correctly
    BUG=chromium-os:23931
    TEST=import incorrect and correctly formatted ONC files
    
    
    Review URL: http://codereview.chromium.org/8879029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113856 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -2868,7 +2868,8 @@ bool NetworkLibraryImplBase::LoadOncNetworks(const std::string& onc_blob,
 
     CallConfigureService(network->unique_id(), &dict);
   }
-  return parser.GetNetworkConfigsSize() != 0;
+  return (parser.GetNetworkConfigsSize() != 0 ||
+          parser.GetCertificatesSize() != 0);
 }
 
 ////////////////////////////////////////////////////////////////////////////
