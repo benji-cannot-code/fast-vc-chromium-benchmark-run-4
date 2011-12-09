3bcf6411b22fa45b3e28c073924db2b3a078c4b1
shinyak@google.com <shinyak@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 11:56:02 +0000

    Added a switch to enable a unified text checker and asynchronous text checking.
    It will enable us to use asynchronous spelling text checking
    feature which will be implemeneted in WebKit. 
    
    BUG=106968
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/8554002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113792 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_host_delegate_helper.cc
+++ chrome/browser/tab_contents/render_view_host_delegate_helper.cc
@@ -506,6 +506,9 @@ WebPreferences RenderViewHostDelegateHelper::GetWebkitPrefs(
 
     web_prefs.visual_word_movement_enabled =
         command_line.HasSwitch(switches::kEnableVisualWordMovement);
+
+    web_prefs.unified_textchecker_enabled =
+        command_line.HasSwitch(switches::kExperimentalSpellcheckerFeatures);
   }
 
   {  // Certain GPU features might have been blacklisted.
