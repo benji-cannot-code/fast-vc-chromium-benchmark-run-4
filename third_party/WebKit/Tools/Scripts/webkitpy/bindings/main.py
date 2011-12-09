28c8b8960e88e3f0d4c55bbdcc7958b669763bf8
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-09 09:15:59 +0000

    REGRESSION(r102105): run-bindings-tests should output full diff results https://bugs.webkit.org/show_bug.cgi?id=74166
    Reviewed by Adam Barth.
    
    Since r102105, the output of run-bindings-tests is truncated to 500 characters.
    run-bindings-tests should output full diff results.
    
    * Scripts/webkitpy/bindings/main.py: Replaced e.message_with_output() with e.output.
    (BindingsTests.generate_from_idl):
    (BindingsTests.generate_supplemental_dependency):
    (BindingsTests.detect_changes):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/bindings/main.py
@@ -59,7 +59,7 @@ class BindingsTests:
             if output:
                 print output
         except ScriptError, e:
-            print e.message_with_output()
+            print e.output
             exit_code = e.exit_code
         return exit_code
 
@@ -85,7 +85,7 @@ class BindingsTests:
             if output:
                 print output
         except ScriptError, e:
-            print e.message_with_output()
+            print e.output
             exit_code = e.exit_code
         os.remove(idl_files_list[1])
         return exit_code
@@ -103,7 +103,7 @@ class BindingsTests:
             try:
                 output = self.executive.run_command(cmd)
             except ScriptError, e:
-                output = e.message_with_output()
+                output = e.output
                 exit_code = e.exit_code
 
             if exit_code or output:
