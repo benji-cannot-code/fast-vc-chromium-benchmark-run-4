5e141c245dd3d6adb51f2c7186793cbeb17bf8bc
dspringer@chromium.org <dspringer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 19:45:56 +0000

    Fix test that broke in earlier CL.
    BUG=none
    TEST=run_all_tests
    
    Review URL: http://codereview.chromium.org/8893007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113839 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/tests/sdk_update_test.py
+++ native_client_sdk/src/build_tools/tests/sdk_update_test.py
@@ -85,7 +85,7 @@ class TestSDKUpdate(unittest.TestCase):
     self.assertEqual(bundle_list.count('sdk_tools'), 2)
     self.assertEqual(bundle_list.count('test_1'), 2)
     self.assertEqual(bundle_list.count('test_2'), 1)
-    self.assertEqual(bundle_list.count('description:'), 7)
+    self.assertEqual(bundle_list.count('description:'), 8)
 
   def testUpdateHelp(self):
     '''Test the help for the update command'''
@@ -116,7 +116,7 @@ class TestSDKUpdate(unittest.TestCase):
     tools = sdk_update.ManifestTools(self._options)
     tools.LoadManifest()
     bundles = tools.GetBundles()
-    self.assertEqual(4, len(bundles))
+    self.assertEqual(5, len(bundles))
     local_manifest = sdk_update.SDKManifestFile(
         os.path.join(self._options.user_data_dir,
                      sdk_update.MANIFEST_FILENAME))
@@ -124,7 +124,7 @@ class TestSDKUpdate(unittest.TestCase):
     for bundle in bundles:
       bundle_name = bundle['name']
       self.assertTrue('revision' in bundle)
-      if bundle_name in ['test_1', 'test_2', 'pepper_1']:
+      if bundle_name in ['test_1', 'test_2', 'pepper_1', 'pepper_phony']:
         self.assertTrue(local_manifest.BundleNeedsUpdate(bundle))
       else:
         self.assertFalse(local_manifest.BundleNeedsUpdate(bundle))
@@ -134,7 +134,7 @@ class TestSDKUpdate(unittest.TestCase):
     tools = sdk_update.ManifestTools(self._options)
     tools.LoadManifest()
     bundles = tools.GetBundles()
-    self.assertEqual(4, len(bundles))
+    self.assertEqual(5, len(bundles))
     local_manifest = sdk_update.SDKManifestFile(
         os.path.join(self._options.user_data_dir,
                      sdk_update.MANIFEST_FILENAME))
@@ -150,7 +150,7 @@ class TestSDKUpdate(unittest.TestCase):
     tools = sdk_update.ManifestTools(self._options)
     tools.LoadManifest()
     bundles = tools.GetBundles()
-    self.assertEqual(4, len(bundles))
+    self.assertEqual(5, len(bundles))
     local_manifest = sdk_update.SDKManifestFile(
         os.path.join(self._options.user_data_dir,
                      sdk_update.MANIFEST_FILENAME))
@@ -158,7 +158,7 @@ class TestSDKUpdate(unittest.TestCase):
     # Test the | operator.
     for bundle in bundles:
       bundle_name = bundle['name']
-      if bundle_name in ['pepper_1', 'test_2']:
+      if bundle_name in ['pepper_1', 'test_2', 'pepper_phony']:
         continue
       local_test_bundle = local_manifest.GetBundleNamed(bundle_name)
       merged_bundle = local_test_bundle.MergeWithBundle(bundle)
