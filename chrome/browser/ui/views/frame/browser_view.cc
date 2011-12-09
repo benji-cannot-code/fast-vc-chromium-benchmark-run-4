6bede0175b17f19a6c88c12b5bb3a0bf610d758b
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 11:14:56 +0000

    [cros] Use a distinct icon for the guest session.
    BUG=chromium-os:22840
    TEST=Manual: see bug description
    
    Review URL: http://codereview.chromium.org/8872015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -460,6 +460,10 @@ bool BrowserView::IsOffTheRecord() const {
   return browser_->profile()->IsOffTheRecord();
 }
 
+bool BrowserView::IsGuestSession() const {
+  return browser_->profile()->IsGuestSession();
+}
+
 bool BrowserView::ShouldShowAvatar() const {
   if (!IsBrowserTypeNormal())
     return false;
@@ -546,7 +550,7 @@ TabContentsWrapper* BrowserView::GetSelectedTabContentsWrapper() const {
   return browser_->GetSelectedTabContentsWrapper();
 }
 
-SkBitmap BrowserView::GetOTRAvatarIcon() {
+SkBitmap BrowserView::GetOTRAvatarIcon() const {
   static SkBitmap* otr_avatar_ = new SkBitmap();
 
   if (otr_avatar_->isNull()) {
@@ -554,6 +558,21 @@ SkBitmap BrowserView::GetOTRAvatarIcon() {
     *otr_avatar_ = *rb.GetBitmapNamed(IDR_OTR_ICON);
   }
   return *otr_avatar_;
+}
+
+SkBitmap BrowserView::GetGuestAvatarIcon() const {
+#if defined(OS_CHROMEOS)
+  static SkBitmap* guest_avatar_ = new SkBitmap();
+
+  if (guest_avatar_->isNull()) {
+    ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+    *guest_avatar_ = *rb.GetBitmapNamed(IDR_GUEST_ICON);
+  }
+  return *guest_avatar_;
+#else
+  NOTREACHED();
+  return SkBitmap();
+#endif
 }
 
 #if defined(OS_WIN) && !defined(USE_AURA)
