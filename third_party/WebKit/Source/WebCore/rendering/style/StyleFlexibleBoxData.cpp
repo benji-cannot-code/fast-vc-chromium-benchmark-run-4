d8ff7d1e0c325379f318904cb6b9503dcc7df0a6
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-09 23:50:16 +0000

    add css parsing for flex-flow: wrap and wrap-reverse https://bugs.webkit.org/show_bug.cgi?id=74008
    Patch by Tony Chang <tony@chromium.org> on 2011-12-09
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Also save 2 bits in StyleFlexibleBoxData by changing the size of m_flexFlow (there are only 4 enum values).
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue): Print the wrap value if it exists.
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseValue): Parse a second token and put the values into a CSSValueList.
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
    (WebCore::CSSPrimitiveValue::operator EFlexWrap):
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::ApplyPropertyFlexFlow::applyInheritValue): Does not inherit.
    (WebCore::ApplyPropertyFlexFlow::applyInitialValue):
    (WebCore::ApplyPropertyFlexFlow::applyValue): Special handler for setting two render style values from one
    CSS property.
    (WebCore::ApplyPropertyFlexFlow::createHandler):
    (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
    * css/CSSValueKeywords.in:
    * rendering/style/RenderStyle.h:
    (WebCore::InheritedFlags::flexWrap):
    (WebCore::InheritedFlags::setFlexWrap):
    (WebCore::InheritedFlags::initialFlexWrap):
    * rendering/style/RenderStyleConstants.h: EFlexWrap to hold flex wrap values.
    * rendering/style/StyleFlexibleBoxData.cpp:
    (WebCore::StyleFlexibleBoxData::StyleFlexibleBoxData):
    (WebCore::StyleFlexibleBoxData::operator==):
    * rendering/style/StyleFlexibleBoxData.h: 2 bits is enough to hold the 4 flexFlow values.
    
    LayoutTests:
    
    * css3/flexbox/css-properties-expected.txt:
    * css3/flexbox/script-tests/css-properties.js: Test call combinations of flex-flow values.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/StyleFlexibleBoxData.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleFlexibleBoxData.cpp
@@ -40,6 +40,7 @@ StyleFlexibleBoxData::StyleFlexibleBoxData()
     , m_flexPack(RenderStyle::initialFlexPack())
     , m_flexAlign(RenderStyle::initialFlexAlign())
     , m_flexFlow(RenderStyle::initialFlexFlow())
+    , m_flexWrap(RenderStyle::initialFlexWrap())
 {
 }
 
@@ -53,6 +54,7 @@ StyleFlexibleBoxData::StyleFlexibleBoxData(const StyleFlexibleBoxData& o)
     , m_flexPack(o.m_flexPack)
     , m_flexAlign(o.m_flexAlign)
     , m_flexFlow(o.m_flexFlow)
+    , m_flexWrap(o.m_flexWrap)
 {
 }
 
@@ -61,7 +63,7 @@ bool StyleFlexibleBoxData::operator==(const StyleFlexibleBoxData& o) const
     return m_widthPositiveFlex == o.m_widthPositiveFlex && m_widthNegativeFlex == o.m_widthNegativeFlex
         && m_heightPositiveFlex == o.m_heightPositiveFlex && m_heightNegativeFlex == o.m_heightNegativeFlex
         && m_flexOrder == o.m_flexOrder && m_flexPack == o.m_flexPack && m_flexAlign == o.m_flexAlign
-        && m_flexFlow == o.m_flexFlow;
+        && m_flexFlow == o.m_flexFlow && m_flexWrap == o.m_flexWrap;
 }
 
 }
