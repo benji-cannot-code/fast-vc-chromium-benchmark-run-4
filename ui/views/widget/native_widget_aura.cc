764906a31f7b02086d368ac679f505f2a952d2b2
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 02:45:24 +0000

    Move {Restore,Store}FocusedView to NativeWidgetAura::On{Activated,LostActive}.
    BUG=104361
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/8821004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113741 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -556,9 +556,6 @@ void NativeWidgetAura::OnFocus() {
   if (widget->is_top_level()) {
     InputMethod* input_method = widget->GetInputMethod();
     input_method->OnFocus();
-    // See description of got_initial_focus_in_ for details on this.
-    // TODO(mazda): Investigate this is actually necessary.
-    // widget->GetFocusManager()->RestoreFocusedView();
   }
   delegate_->OnNativeFocus(window_);
 }
@@ -568,7 +565,6 @@ void NativeWidgetAura::OnBlur() {
   if (widget->is_top_level()) {
     InputMethod* input_method = widget->GetInputMethod();
     input_method->OnBlur();
-    widget->GetFocusManager()->StoreFocusedView();
   }
   delegate_->OnNativeBlur(NULL);
 }
@@ -623,12 +619,14 @@ bool NativeWidgetAura::ShouldActivate(aura::Event* event) {
 }
 
 void NativeWidgetAura::OnActivated() {
+  GetWidget()->GetFocusManager()->RestoreFocusedView();
   delegate_->OnNativeWidgetActivationChanged(true);
   if (IsVisible() && GetWidget()->non_client_view())
     GetWidget()->non_client_view()->SchedulePaint();
 }
 
 void NativeWidgetAura::OnLostActive() {
+  GetWidget()->GetFocusManager()->StoreFocusedView();
   delegate_->OnNativeWidgetActivationChanged(false);
   if (IsVisible() && GetWidget()->non_client_view())
     GetWidget()->non_client_view()->SchedulePaint();
