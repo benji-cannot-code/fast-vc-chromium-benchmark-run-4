41a40003bf1e557eb84861a2b5e61a4a800a1218
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 01:49:56 +0000

    Refactor generete_bindings.py so per function logging code and be added.
    TEST=none
    BUG=none
    
    
    Review URL: http://codereview.chromium.org/8872001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113732 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_bindings.h
+++ ui/gfx/gl/gl_bindings.h
@@ -42,6 +42,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #define GL_SERVICE_LOG(args) DLOG(INFO) << args;
+#if !defined(NDEBUG)
+  #define GL_SERVICE_LOG_CODE_BLOCK(code)
+#else
+  #define GL_SERVICE_LOG_CODE_BLOCK(code) code
+#endif
 
 // Forward declare OSMesa types.
 typedef struct osmesa_context *OSMesaContext;
