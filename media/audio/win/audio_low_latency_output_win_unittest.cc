fe94832129f3e4a6b10fc74c1ab38179f38e431d
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 02:45:16 +0000

    Removing MessageLoop::QuitTask() from media/
    base/task.h is going away and being replaced by base/bind.h and base/callback.h.
    This CL was automatically generated by sed.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8879032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113740 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_low_latency_output_win_unittest.cc
+++ media/audio/win/audio_low_latency_output_win_unittest.cc
@@ -225,7 +225,7 @@ static AudioOutputStream* CreateDefaultAudioOutputStream() {
 }
 
 static void QuitMessageLoop(base::MessageLoopProxy* proxy) {
-  proxy->PostTask(FROM_HERE, new MessageLoop::QuitTask());
+  proxy->PostTask(FROM_HERE, MessageLoop::QuitClosure());
 }
 
 // Verify that we can retrieve the current hardware/mixing sample rate
@@ -397,7 +397,7 @@ TEST(WinAudioOutputTest, WASAPIAudioOutputStreamTestPacketSizeInMilliseconds) {
               Return(bytes_per_packet)));
 
   aos->Start(&source);
-  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask(),
+  loop.PostDelayedTask(FROM_HERE, MessageLoop::QuitClosure(),
                        TestTimeouts::action_timeout_ms());
   loop.Run();
   aos->Stop();
@@ -438,7 +438,7 @@ TEST(WinAudioOutputTest, WASAPIAudioOutputStreamTestPacketSizeInSamples) {
               Return(bytes_per_packet)));
 
   aos->Start(&source);
-  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask(),
+  loop.PostDelayedTask(FROM_HERE, MessageLoop::QuitClosure(),
                        TestTimeouts::action_timeout_ms());
   loop.Run();
   aos->Stop();
@@ -476,7 +476,7 @@ TEST(WinAudioOutputTest, WASAPIAudioOutputStreamTestMono) {
               Return(bytes_per_packet)));
 
   aos->Start(&source);
-  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask(),
+  loop.PostDelayedTask(FROM_HERE, MessageLoop::QuitClosure(),
                        TestTimeouts::action_timeout_ms());
   loop.Run();
   aos->Stop();
