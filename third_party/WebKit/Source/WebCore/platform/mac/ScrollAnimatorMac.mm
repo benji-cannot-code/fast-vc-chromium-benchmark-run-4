76aa136b4bbc64759fc75966d99f796de246a006
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-09 23:21:10 +0000

    Fix assertion failure in ScrollAnimatorMac https://bugs.webkit.org/show_bug.cgi?id=74222
    Reviewed by Andreas Kling.
    
    * platform/mac/ScrollAnimatorMac.mm:
    (-[WebScrollbarPainterControllerDelegate scrollerImpPair:convertContentPoint:toScrollerImp:]):
    This can be called with a nil scrollerImp, just return NSZeroPoint when that happens.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
@@ -230,7 +230,8 @@ static NSSize abs(NSSize size)
 - (NSPoint)scrollerImpPair:(id)scrollerImpPair convertContentPoint:(NSPoint)pointInContentArea toScrollerImp:(id)scrollerImp
 {
     UNUSED_PARAM(scrollerImpPair);
-    if (!_scrollableArea)
+
+    if (!_scrollableArea || !scrollerImp)
         return NSZeroPoint;
 
     WebCore::Scrollbar* scrollbar = 0;
@@ -245,7 +246,7 @@ static NSSize abs(NSSize size)
     // of the clean-up work in ScrollbarThemeMac::unregisterScrollbar() to avoid this
     // issue.
     if (!scrollbar)
-        return WebCore::IntPoint();
+        return NSZeroPoint;
 
     ASSERT(scrollerImp == scrollbarPainterForScrollbar(scrollbar));
 
