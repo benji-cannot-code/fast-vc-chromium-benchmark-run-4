37cfdcde1d0fe431e08505e75c5971bc21db232c
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-10 03:57:32 +0000

    Don't close ExtensionPopups on child window focus (Win, non-Aura). Fixes crash on JS dialog popups from the extension bubbles.
    Check for child HWND in WidgetFocusChangeListener::OnNativeFocusChange.
    Code from BubbleWidget in browser_bubble_win.cc (crrev.com/112278)
    
    See Issue 106958 for Aura, the dialog&bubble close early without crashing.
    
    BUG=106723,106958
    TEST=The js alert extension attached to the issue doesn't crash, popup works on non-aura.
    
    Review URL: http://codereview.chromium.org/8879045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113936 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_popup.h
+++ chrome/browser/ui/views/extensions/extension_popup.h
@@ -13,12 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "googleurl/src/gurl.h"
 #include "ui/views/bubble/bubble_delegate.h"
+#include "ui/views/focus/widget_focus_manager.h"
 
 class Browser;
 
 class ExtensionPopup : public views::BubbleDelegateView,
                        public ExtensionView::Container,
-                       public content::NotificationObserver {
+                       public content::NotificationObserver,
+                       public views::WidgetFocusChangeListener {
  public:
   virtual ~ExtensionPopup();
 
@@ -51,8 +53,12 @@ class ExtensionPopup : public views::BubbleDelegateView,
   // ExtensionView::Container overrides.
   virtual void OnExtensionPreferredSizeChanged(ExtensionView* view) OVERRIDE;
 
-  // view::View overrides.
+  // views::View overrides.
   virtual gfx::Size GetPreferredSize() OVERRIDE;
+
+  // views::WidgetFocusChangeListener overrides.
+  virtual void OnNativeFocusChange(gfx::NativeView focused_before,
+                                   gfx::NativeView focused_now) OVERRIDE;
 
   // The min/max height of popups.
   static const int kMinWidth;
