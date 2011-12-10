5e07a0dec34de68a5456b37505a9a9732f8142fb
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-10 21:48:30 +0000

    Mac build fix. Remove an erroneous OVERRIDE.
    * editing/CompositeEditCommand.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.h
+++ third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.h
@@ -48,7 +48,7 @@ public:
     void append(SimpleEditCommand*);
 
 #ifndef NDEBUG
-    virtual void getNodesInCommand(HashSet<Node*>&) OVERRIDE;
+    virtual void getNodesInCommand(HashSet<Node*>&);
 #endif
 
 private:
