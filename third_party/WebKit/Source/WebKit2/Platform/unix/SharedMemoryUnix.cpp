209f0353165e222389be370f2c5ed82e2a4fffb0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-10 01:01:12 +0000

    [Qt] Open shared memory files with shm_open. https://bugs.webkit.org/show_bug.cgi?id=74078
    Original patch by Kimmo Kinnunen
    
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2011-12-09
    Reviewed by Kenneth Rohde Christiansen.
    
    Open shared memory files with shm_open. This uses mount point that is
    intended to host shared memory files. Typically this is /dev/shm.
    
    This fixes crashes when filesystem that hosts QDir::temp() is full.
    
    This is also more well-defined with respect to question whether SHM
    writes to temp dir would cause unintended wear if hosted on flash drives.
    
    This also fixes performance problems regarding QDir::temp() and
    mkostemp(), both of which appear to be long operations.
    
    * Platform/unix/SharedMemoryUnix.cpp:
    (WebKit::SharedMemory::create):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/unix/SharedMemoryUnix.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/unix/SharedMemoryUnix.cpp
@@ -106,8 +106,8 @@ void SharedMemory::Handle::adoptFromAttachment(int fileDescriptor, size_t size)
 PassRefPtr<SharedMemory> SharedMemory::create(size_t size)
 {
 #if PLATFORM(QT)
-    QString tempName = QDir::temp().filePath(QLatin1String("qwkshm.XXXXXX"));
-    QByteArray tempNameCSTR = tempName.toLocal8Bit();
+    QByteArray tempNameCSTR("/qwkshm.");
+    tempNameCSTR += QByteArray::number(qrand(), 36);
     char* tempNameC = tempNameCSTR.data();
 #elif PLATFORM(GTK)
     GOwnPtr<gchar> tempName(g_build_filename(g_get_tmp_dir(), "WK2SharedMemoryXXXXXX", NULL));
@@ -115,6 +115,12 @@ PassRefPtr<SharedMemory> SharedMemory::create(size_t size)
 #endif
 
     int fileDescriptor;
+#if PLATFORM(QT)
+    while ((fileDescriptor = shm_open(tempNameC, O_CREAT | O_CLOEXEC | O_RDWR, S_IRUSR | S_IWUSR)) == -1) {
+        if (errno != EINTR)
+            return 0;
+    }
+#else
     while ((fileDescriptor = mkstemp(tempNameC)) == -1) {
         if (errno != EINTR)
             return 0;
@@ -126,11 +132,15 @@ PassRefPtr<SharedMemory> SharedMemory::create(size_t size)
             return 0;
         }
     }
-
+#endif
     while (ftruncate(fileDescriptor, size) == -1) {
         if (errno != EINTR) {
             while (close(fileDescriptor) == -1 && errno == EINTR) { }
+#if PLATFORM(QT)
+            shm_unlink(tempNameC);
+#else
             unlink(tempNameC);
+#endif
             return 0;
         }
     }
@@ -138,11 +148,19 @@ PassRefPtr<SharedMemory> SharedMemory::create(size_t size)
     void* data = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fileDescriptor, 0);
     if (data == MAP_FAILED) {
         while (close(fileDescriptor) == -1 && errno == EINTR) { }
+#if PLATFORM(QT)
+        shm_unlink(tempNameC);
+#else
         unlink(tempNameC);
+#endif
         return 0;
     }
 
+#if PLATFORM(QT)
+    shm_unlink(tempNameC);
+#else
     unlink(tempNameC);
+#endif
 
     RefPtr<SharedMemory> instance = adoptRef(new SharedMemory());
     instance->m_data = data;
