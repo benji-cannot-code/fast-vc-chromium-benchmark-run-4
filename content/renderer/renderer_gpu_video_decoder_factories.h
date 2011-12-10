237d7b59406b462faf5d7c325b46c87dc6944665
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-10 00:46:28 +0000

    Add missing 'virtual'.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8893020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113899 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_gpu_video_decoder_factories.h
+++ content/renderer/renderer_gpu_video_decoder_factories.h
@@ -30,7 +30,7 @@ class RendererGpuVideoDecoderFactories
 
   virtual ~RendererGpuVideoDecoderFactories();
 
-  media::VideoDecodeAccelerator* CreateVideoDecodeAccelerator(
+  virtual media::VideoDecodeAccelerator* CreateVideoDecodeAccelerator(
       media::VideoDecodeAccelerator::Profile profile,
       media::VideoDecodeAccelerator::Client* client) OVERRIDE;
 
