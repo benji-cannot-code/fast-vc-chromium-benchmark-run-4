a23b8ed4fd9bbd758ac08619499a7efa1f59a94f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-10 01:44:06 +0000

    Fix Lion release build.
    * platform/mac/ScrollAnimatorMac.mm:
    (-[WebScrollbarPainterDelegate mouseLocationInScrollerForScrollerImp:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
@@ -428,7 +428,7 @@ enum FeatureToAnimate {
     if (!_scrollbar)
         return NSZeroPoint;
 
-    ASSERT(scrollerImp == scrollbarPainterForScrollbar(_scrollbar));
+    ASSERT_UNUSED(scrollerImp, scrollerImp == scrollbarPainterForScrollbar(_scrollbar));
 
     return _scrollbar->convertFromContainingView(_scrollbar->scrollableArea()->currentMousePosition());
 }
