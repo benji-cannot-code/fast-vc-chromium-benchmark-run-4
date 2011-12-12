e6a2b0a94d71e6584e7dd58c81ab0327d5d3728e
ncj674@motorola.com <ncj674@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-12 21:52:53 +0000

    Linux/Gtk: Drawing a focus-ring around the avatar menu items to highlight selection.
    BUG=None.
    TEST=Click avatar menu and press Tab key to navigate through the menu.
    Selected menu items should have a focus ring.
    
    Review URL: http://codereview.chromium.org/8921003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114086 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/avatar_menu_item_gtk.cc
+++ chrome/browser/ui/gtk/avatar_menu_item_gtk.cc
@@ -121,7 +121,6 @@ void AvatarMenuItemGtk::ShowEditLink() {
 
 gboolean AvatarMenuItemGtk::OnProfileFocusIn(GtkWidget* widget,
                                              GdkEventFocus* event) {
-  gtk_widget_modify_bg(widget, GTK_STATE_NORMAL, &highlighted_color_);
   if (item_.active)
     ShowEditLink();
 
@@ -130,7 +129,6 @@ gboolean AvatarMenuItemGtk::OnProfileFocusIn(GtkWidget* widget,
 
 gboolean AvatarMenuItemGtk::OnProfileFocusOut(GtkWidget* widget,
                                               GdkEventFocus* event) {
-  gtk_widget_modify_bg(widget, GTK_STATE_NORMAL, unhighlighted_color_);
   if (item_.active)
     ShowStatusLabel();
 
@@ -202,6 +200,22 @@ void AvatarMenuItemGtk::OnEditProfileLinkClicked(GtkWidget* link) {
                  weak_factory_.GetWeakPtr()));
 }
 
+gboolean AvatarMenuItemGtk::OnEventBoxExpose(GtkWidget* widget,
+                                             GdkEventExpose* event) {
+  // Draw the focus rectangle.
+  if (gtk_widget_has_focus(widget)) {
+    GtkAllocation allocation;
+    gtk_widget_get_allocation(widget, &allocation);
+    gtk_paint_focus(widget->style, widget->window,
+                    gtk_widget_get_state(widget),
+                    &event->area, widget, NULL,
+                    0, 0,
+                    allocation.width, allocation.height);
+  }
+
+  return TRUE;
+}
+
 void AvatarMenuItemGtk::Init(GtkThemeService* theme_service) {
   widget_.Own(gtk_event_box_new());
 
@@ -217,6 +231,8 @@ void AvatarMenuItemGtk::Init(GtkThemeService* theme_service) {
       G_CALLBACK(OnProfileFocusOutThunk), this);
   g_signal_connect(widget_.get(), "key-press-event",
       G_CALLBACK(OnProfileKeyPressThunk), this);
+  g_signal_connect_after(widget_.get(), "expose-event",
+      G_CALLBACK(OnEventBoxExposeThunk), this);
 
   GtkWidget* item_hbox = gtk_hbox_new(FALSE, ui::kControlSpacing);
   GdkPixbuf* avatar_pixbuf = NULL;
