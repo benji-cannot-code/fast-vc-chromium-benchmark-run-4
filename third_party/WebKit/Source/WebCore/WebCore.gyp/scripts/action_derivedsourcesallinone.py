fdea599825cba45ce5d290041bbb13537998ab2a
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-12 13:41:10 +0000

    Unreviewed, rolling out r102556. http://trac.webkit.org/changeset/102556 https://bugs.webkit.org/show_bug.cgi?id=73394
    clobber build failure
    
    * WebCore.gyp/WebCore.gyp:
    * WebCore.gyp/scripts/action_derivedsourcesallinone.py:
    (main):
    * WebCore.gypi:
    * bindings/scripts/generate-bindings.pl:
    * page/DOMWindow.idl:
    * webaudio/DOMWindowWebAudio.idl: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.gyp/scripts/action_derivedsourcesallinone.py
+++ third_party/WebKit/Source/WebCore/WebCore.gyp/scripts/action_derivedsourcesallinone.py
@@ -33,12 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# action_derivedsourcesallinone.py generates a single cpp file that includes
+# action_derivedsourceslist.py generates a single cpp file that includes
 # all v8 bindings cpp files generated from idls. Files can be assigned into
 # multiple output files, to reduce maximum compilation unit size and allow
 # parallel compilation.
 #
-# usage: action_derivedsourcesallinone.py IDL_FILES_LIST -- OUTPUT_FILE1 OUTPUT_FILE2 ...
+# usage: action_derivedsourceslist.py IDL_FILES_LIST -- OUTPUT_FILE1 OUTPUT_FILE2 ...
 #
 # Note that IDL_FILES_LIST is a text file containing the IDL file paths.
 
@@ -48,9 +48,6 @@ import os.path
 import re
 import subprocess
 import sys
-
-sys.path.append("../../../Tools/Scripts/")
-from webkitpy.common.system import path
 
 # A regexp for finding Conditional attributes in interface definitions.
 conditionalPattern = re.compile('interface[\s]*\[[^\]]*Conditional=([\_0-9a-zA-Z&|]*)')
@@ -190,12 +187,7 @@ def main(args):
     outputFileNames = args[inOutBreakIndex+1:]
 
     inputFile = open(inputFileName, 'r')
-    idlFileNames = []
-    for line in inputFile:
-        idlFileName = line[:-1].split(' ')[0]
-        if idlFileName.find("/cygdrive") == 0:
-            idlFileName = path.cygpath(idlFileName)
-        idlFileNames.append(idlFileName)
+    idlFileNames = inputFile.read().split('\n')
     inputFile.close()
 
     filesMetaData = extractMetaData(idlFileNames)
