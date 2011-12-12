e98d2fc51ab0cb4929d2e78979cb32e975e0d9c7
vangelis@chromium.org <vangelis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-12 19:46:36 +0000

    Fixed service side implementation of glTexStorage2DEXT to only initialize the number of levels requirested. Also ClearLevel now uses glTexSubImage2D to clear the level if the texture is immutable.  Finally, rearranged the code in doTexSubImage2D to correctly bypass clearing a level if the bitmaps supplied matches the dimensions of the texture level. BUG=106894 Review URL: http://codereview.chromium.org/8872061
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114059 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/texture_manager.cc
+++ gpu/command_buffer/service/texture_manager.cc
@@ -107,8 +107,7 @@ bool TextureManager::TextureInfo::CanRender(
 }
 
 bool TextureManager::TextureInfo::MarkMipmapsGenerated(
-    const FeatureInfo* feature_info,
-    bool cleared) {
+    const FeatureInfo* feature_info) {
   if (!CanGenerateMipmaps(feature_info)) {
     return false;
   }
@@ -134,7 +133,7 @@ bool TextureManager::TextureInfo::MarkMipmapsGenerated(
                    info1.border,
                    info1.format,
                    info1.type,
-                   cleared);
+                   true);
     }
   }
 
@@ -525,7 +524,7 @@ bool TextureManager::TextureInfo::ClearLevel(
   // needed to be able to call GL correctly.
   info.cleared = decoder->ClearLevel(
       service_id_, target_, info.target, info.level, info.format, info.type,
-      info.width, info.height);
+      info.width, info.height, immutable_);
   if (!info.cleared) {
     ++num_uncleared_mips_;
   }
@@ -792,8 +791,7 @@ bool TextureManager::SetParameter(
 
 bool TextureManager::MarkMipmapsGenerated(
     const FeatureInfo* feature_info,
-    TextureManager::TextureInfo* info,
-    bool cleared) {
+    TextureManager::TextureInfo* info) {
   DCHECK(info);
   if (!info->CanRender(feature_info)) {
     DCHECK_NE(0, num_unrenderable_textures_);
@@ -805,7 +803,7 @@ bool TextureManager::MarkMipmapsGenerated(
   }
   num_uncleared_mips_ -= info->num_uncleared_mips();
   DCHECK_GE(num_uncleared_mips_, 0);
-  bool result = info->MarkMipmapsGenerated(feature_info, cleared);
+  bool result = info->MarkMipmapsGenerated(feature_info);
   num_uncleared_mips_ += info->num_uncleared_mips();
   if (!info->CanRender(feature_info)) {
     ++num_unrenderable_textures_;
