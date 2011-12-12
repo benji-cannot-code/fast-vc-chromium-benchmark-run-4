96cf433cae0f8d235ec79cebb771202d20be6988
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-12 00:24:29 +0000

    Implement CSS resize property in CSSStyleApplyProperty. https://bugs.webkit.org/show_bug.cgi?id=74162
    Reviewed by Julien Chaffraix.
    
    No new tests / refactoring only.
    
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::ApplyPropertyResize::applyValue):
    (WebCore::ApplyPropertyResize::createHandler):
    (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Pair.h"
 #include "RenderObject.h"
 #include "RenderStyle.h"
+#include "Settings.h"
 #include <wtf/StdLibExtras.h>
 #include <wtf/UnusedParam.h>
 
@@ -1170,6 +1171,36 @@ public:
     static PropertyHandler createHandler() { return PropertyHandler(&applyInheritValue, &applyInitialValue, &applyValue); }
 };
 
+class ApplyPropertyResize {
+public:
+    static void applyValue(CSSStyleSelector* selector, CSSValue* value)
+    {
+        if (!value->isPrimitiveValue())
+            return;
+
+        CSSPrimitiveValue* primitiveValue = static_cast<CSSPrimitiveValue*>(value);
+
+        EResize r = RESIZE_NONE;
+        switch (primitiveValue->getIdent()) {
+        case 0:
+            return;
+        case CSSValueAuto:
+            if (Settings* settings = selector->document()->settings())
+                r = settings->textAreasAreResizable() ? RESIZE_BOTH : RESIZE_NONE;
+            break;
+        default:
+            r = *primitiveValue;
+        }
+        selector->style()->setResize(r);
+    }
+
+    static PropertyHandler createHandler()
+    {
+        PropertyHandler handler = ApplyPropertyDefaultBase<EResize, &RenderStyle::resize, EResize, &RenderStyle::setResize, EResize, &RenderStyle::initialResize>::createHandler();
+        return PropertyHandler(handler.inheritFunction(), handler.initialFunction(), &applyValue);
+    }
+};
+
 class ApplyPropertyVerticalAlign {
 public:
     static void applyValue(CSSStyleSelector* selector, CSSValue* value)
@@ -1520,6 +1551,8 @@ CSSStyleApplyProperty::CSSStyleApplyProperty()
     setPropertyHandler(CSSPropertyPaddingBottom, ApplyPropertyLength<&RenderStyle::paddingBottom, &RenderStyle::setPaddingBottom, &RenderStyle::initialPadding>::createHandler());
     setPropertyHandler(CSSPropertyPaddingLeft, ApplyPropertyLength<&RenderStyle::paddingLeft, &RenderStyle::setPaddingLeft, &RenderStyle::initialPadding>::createHandler());
     setPropertyHandler(CSSPropertyPadding, ApplyPropertyExpanding<SuppressValue, CSSPropertyPaddingTop, CSSPropertyPaddingRight, CSSPropertyPaddingBottom, CSSPropertyPaddingLeft>::createHandler());
+
+    setPropertyHandler(CSSPropertyResize, ApplyPropertyResize::createHandler());
 
     setPropertyHandler(CSSPropertyVerticalAlign, ApplyPropertyVerticalAlign::createHandler());
 
--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -2725,24 +2725,6 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWebkitMatchNearestMailBlockquoteColor:
         HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(matchNearestMailBlockquoteColor, MatchNearestMailBlockquoteColor)
         return;
-
-    case CSSPropertyResize:
-    {
-        HANDLE_INHERIT_AND_INITIAL(resize, Resize)
-
-        if (!primitiveValue->getIdent())
-            return;
-
-        EResize r = RESIZE_NONE;
-        if (primitiveValue->getIdent() == CSSValueAuto) {
-            if (Settings* settings = m_checker.document()->settings())
-                r = settings->textAreasAreResizable() ? RESIZE_BOTH : RESIZE_NONE;
-        } else
-            r = *primitiveValue;
-
-        m_style->setResize(r);
-        return;
-    }
     case CSSPropertyFontSize:
     {
         FontDescription fontDescription = m_style->fontDescription();
@@ -3933,6 +3915,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyPaddingBottom:
     case CSSPropertyPaddingLeft:
     case CSSPropertyPadding:
+    case CSSPropertyResize:
     case CSSPropertySize:
     case CSSPropertyTextAlign:
     case CSSPropertyTextIndent:
