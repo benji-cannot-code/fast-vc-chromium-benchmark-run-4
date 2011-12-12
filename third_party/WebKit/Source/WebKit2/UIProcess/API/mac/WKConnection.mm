2f3952800b7650f64fc3ca03f5f8e7960c79ceeb
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-12 22:41:31 +0000

    Remove unused getter on WKConnection.
    Reviewed by Dan Bernstein.
    
    * UIProcess/API/mac/WKConnection.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102625 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKConnection.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKConnection.mm
@@ -54,11 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [super dealloc];
 }
 
-- (WKConnectionRef)connectionRef
-{
-    return _data->_connectionRef.get();
-}
-
 #pragma mark Delegates
 
 - (id<WKConnectionDelegate>)delegate
