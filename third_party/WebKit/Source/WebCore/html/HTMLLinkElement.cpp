72ceca2c4d8af97b77ff8af94fe8e4a76eba05d4
reni@webkit.org <reni@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-12 15:13:18 +0000

    Fulfill FIXME in  HTMLLinkElement.h. https://bugs.webkit.org/show_bug.cgi?id=74278
    Rename HTMLLinkElement::isLoading() to isStyleSheetLoading().
    
    Reviewed by Andreas Kling.
    
    No new tests because the functionality remains the same.
    
    * dom/Document.cpp:
    (WebCore::Document::recalcStyleSelector):
    * html/HTMLLinkElement.cpp:
    (WebCore::HTMLLinkElement::setDisabledState):
    (WebCore::HTMLLinkElement::isStyleSheetLoading):
    (WebCore::HTMLLinkElement::sheetLoaded):
    * html/HTMLLinkElement.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLLinkElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLLinkElement.cpp
@@ -91,7 +91,7 @@ void HTMLLinkElement::setDisabledState(bool disabled)
     if (oldDisabledState != m_disabledState) {
         // If we change the disabled state while the sheet is still loading, then we have to
         // perform three checks:
-        if (isLoading()) {
+        if (isStyleSheetLoading()) {
             // Check #1: The sheet becomes disabled while loading.
             if (m_disabledState == Disabled)
                 removePendingSheet();
@@ -334,7 +334,7 @@ void HTMLLinkElement::setCSSStyleSheet(const String& href, const KURL& baseURL, 
     m_sheet->checkLoaded();
 }
 
-bool HTMLLinkElement::isLoading() const
+bool HTMLLinkElement::isStyleSheetLoading() const
 {
     if (m_loading)
         return true;
@@ -355,7 +355,7 @@ void HTMLLinkElement::linkLoadingErrored()
 
 bool HTMLLinkElement::sheetLoaded()
 {
-    if (!isLoading()) {
+    if (!isStyleSheetLoading()) {
         removePendingSheet();
         return true;
     }
