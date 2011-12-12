f4a35d1cd72389f6387ab11c3dd98874244996b8
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-12 23:10:21 +0000

    Revert "Force tabs to be integer widths."
    This reverts commit r112107.
    
    BUG=107269
    TEST=as in bug
    
    Review URL: http://codereview.chromium.org/8928001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <QuartzCore/QuartzCore.h>
 
-#include <cmath>
 #include <limits>
 #include <string>
 
@@ -860,8 +859,7 @@ private:
     availableSpaceForNonMini += (numberOfOpenNonMiniTabs - 1) * kTabOverlap;
 
     // Divide up the space between the non-mini-tabs.
-    nonMiniTabWidth =
-        std::floor(availableSpaceForNonMini / numberOfOpenNonMiniTabs);
+    nonMiniTabWidth = availableSpaceForNonMini / numberOfOpenNonMiniTabs;
 
     // Clamp the width between the max and min.
     nonMiniTabWidth = MAX(MIN(nonMiniTabWidth, kMaxTabWidth), kMinTabWidth);
