fe61ee2616cbc6ce3e81354c1d691d8de83bd462
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-12 16:04:30 +0000

    Add a policy pref for the --enable-memory-info flag.
    BUG=43281
    TEST=Setting pref has same effect as running with --enable-memory-info.
    
    
    Review URL: http://codereview.chromium.org/8896018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114022 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/policy_prefs_ui.py
+++ chrome/test/functional/policy_prefs_ui.py
@@ -197,6 +197,7 @@ class PolicyPrefsUITest(policy_base.PolicyTestBase):
     'URLWhitelist': ([ 'google.com' ], []),
     'EnterpriseWebStoreURL': ('', []),
     'EnterpriseWebStoreName': ('', []),
+    'EnableMemoryInfo': (True, []),
 
     # ChromeOS-only policies:
     'ChromeOsLockOnIdleSuspend': (True, [ PERSONAL ], [ 'chromeos' ]),
