be9f9d5040e6fbd6d276a3cf99d177772ce9799f
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 00:30:18 +0000

    Clear focused view if the focused view is in a window that is being destroyed.
    Original CL (http://codereview.chromium.org/8821004/) was causing crash on some of browser_tests due to this.
    
    BUG=104361
    TEST=manual, and browser_tests now pass without crash
    
    
    Review URL: http://codereview.chromium.org/8892025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114131 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -575,9 +575,6 @@ void NativeWidgetAura::OnFocus() {
   if (widget->is_top_level()) {
     InputMethod* input_method = widget->GetInputMethod();
     input_method->OnFocus();
-    // See description of got_initial_focus_in_ for details on this.
-    // TODO(mazda): Investigate this is actually necessary.
-    // widget->GetFocusManager()->RestoreFocusedView();
   }
   delegate_->OnNativeFocus(window_);
 }
@@ -587,7 +584,6 @@ void NativeWidgetAura::OnBlur() {
   if (widget->is_top_level()) {
     InputMethod* input_method = widget->GetInputMethod();
     input_method->OnBlur();
-    widget->GetFocusManager()->StoreFocusedView();
   }
   delegate_->OnNativeBlur(NULL);
 }
@@ -673,12 +669,14 @@ bool NativeWidgetAura::ShouldActivate(aura::Event* event) {
 }
 
 void NativeWidgetAura::OnActivated() {
+  GetWidget()->GetFocusManager()->RestoreFocusedView();
   delegate_->OnNativeWidgetActivationChanged(true);
   if (IsVisible() && GetWidget()->non_client_view())
     GetWidget()->non_client_view()->SchedulePaint();
 }
 
 void NativeWidgetAura::OnLostActive() {
+  GetWidget()->GetFocusManager()->StoreFocusedView();
   delegate_->OnNativeWidgetActivationChanged(false);
   if (IsVisible() && GetWidget()->non_client_view())
     GetWidget()->non_client_view()->SchedulePaint();
