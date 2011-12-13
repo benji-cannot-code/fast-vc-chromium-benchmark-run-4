9fa54a21b9d322348fff809e15bbed6fdb6e28f3
alokp@chromium.org <alokp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-13 21:12:40 +0000

    [chromium] compositing/masks layout tests fail with accelerated drawing https://bugs.webkit.org/show_bug.cgi?id=72760
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    Accelerated drawing path used to render bottom-up upright textures, which was opposite of what the software path rendered.
    The textures produced by the accelerated path was flipped along Y in the shader to make it upside down as expected by the compositor.
    This strategy does not work in case of masks which do not go through a shader and hence do not get flipped,
    which results in a case where texture in the render surface is top-down, while that in the mask is bottom-up.
    This patch makes accelerated drawing path render textures in the same orientation as the software path.
    LayerTextureUpdater::Orientation was added to support the difference in texture orientation between software and accelerated paths.
    Now that both paths produce textures in the same orientation, there is no need for it.
    
    No new tests needed. Covered by existing compositing tests.
    
    * platform/graphics/chromium/BitmapCanvasLayerTextureUpdater.h:
    * platform/graphics/chromium/BitmapSkPictureCanvasLayerTextureUpdater.h:
    * platform/graphics/chromium/FrameBufferSkPictureCanvasLayerTextureUpdater.cpp:
    (WebCore::FrameBufferSkPictureCanvasLayerTextureUpdater::updateTextureRect):
    * platform/graphics/chromium/FrameBufferSkPictureCanvasLayerTextureUpdater.h:
    * platform/graphics/chromium/ImageLayerChromium.cpp:
    * platform/graphics/chromium/LayerTextureUpdater.h:
    * platform/graphics/chromium/TiledLayerChromium.cpp:
    (WebCore::TiledLayerChromium::TiledLayerChromium):
    (WebCore::TiledLayerChromium::setLayerTreeHost):
    (WebCore::TiledLayerChromium::pushPropertiesTo):
    * platform/graphics/chromium/TiledLayerChromium.h:
    * platform/graphics/chromium/cc/CCTiledLayerImpl.cpp:
    (WebCore::CCTiledLayerImpl::drawTiles):
    * platform/graphics/chromium/cc/CCTiledLayerImpl.h:
    (WebCore::CCTiledLayerImpl::setSkipsDraw):
    
    Source/WebKit/chromium:
    
    * tests/TiledLayerChromiumTest.cpp:
    (WTF::FakeLayerTextureUpdater::createTexture):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCTiledLayerImpl.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCTiledLayerImpl.h
@@ -50,7 +50,6 @@ public:
     virtual void dumpLayerProperties(TextStream&, int indent) const;
 
     void setSkipsDraw(bool skipsDraw) { m_skipsDraw = skipsDraw; }
-    void setTextureOrientation(LayerTextureUpdater::Orientation textureOrientation) { m_textureOrientation = textureOrientation; }
     void setSampledTexelFormat(LayerTextureUpdater::SampledTexelFormat sampledTexelFormat) { m_sampledTexelFormat = sampledTexelFormat; }
     void setTilingData(const CCLayerTilingData& tiler);
     void syncTextureId(int, int, Platform3DObject textureId);
@@ -90,7 +89,6 @@ private:
     void drawTiles(LayerRendererChromium*, const IntRect& contentRect, const TransformationMatrix& globalTransform, const TransformationMatrix& deviceTransform, const CCLayerQuad& deviceRect, const CCLayerQuad& contentQuad, float opacity, const T* program, int fragmentTexTransformLocation, int edgeLocation);
 
     bool m_skipsDraw;
-    LayerTextureUpdater::Orientation m_textureOrientation;
     LayerTextureUpdater::SampledTexelFormat m_sampledTexelFormat;
 
     OwnPtr<CCLayerTilingData> m_tiler;
