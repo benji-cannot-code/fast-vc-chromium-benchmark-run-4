856c142d4b04fe6c9226c78d53167b13ad9f9721
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 23:39:55 +0000

    Clean up WebResourceService: * Use base::Bind instead of NewRunnableMethod. * Inline WebResourceFetcher into WebResourceService, as they have the same lifetime. * Move stuff specific to PromoResourceService into that class.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8905022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114306 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/promo_resource_service.h
+++ chrome/browser/web_resource/promo_resource_service.h
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/web_resource/notification_promo.h"
 #include "chrome/browser/web_resource/web_resource_service.h"
 #include "chrome/common/chrome_version_info.h"
 
 namespace base {
-  class DictionaryValue;
+class DictionaryValue;
 }
 
 class AppsPromoLogoFetcher;
@@ -82,8 +83,6 @@ class PromoResourceService
   // so we can fetch new data if the locale changes.
   std::string GetPromoLocale();
 
-  void Init();
-
   // Returns true if |builds_targeted| includes the release channel Chrome
   // belongs to. For testing purposes, you can override the current channel
   // with set_channel.
@@ -98,6 +97,14 @@ class PromoResourceService
   // checks than ScheduleNotification, namely it schedules updates immediately
   // if the promo service or Chrome locale has changed.
   void ScheduleNotificationOnInit();
+
+  // If delay_ms is positive, schedule notification with the delay.
+  // If delay_ms is 0, notify immediately by calling WebResourceStateChange().
+  // If delay_ms is negative, do nothing.
+  void PostNotification(int64 delay_ms);
+
+  // Notify listeners that the state of a web resource has changed.
+  void PromoResourceStateChange();
 
   // Overrides the current Chrome release channel for testing purposes.
   void set_channel(chrome::VersionInfo::Channel channel) { channel_ = channel; }
@@ -239,6 +246,18 @@ class PromoResourceService
 
   // Overrides the current Chrome release channel for testing purposes.
   chrome::VersionInfo::Channel channel_;
+
+  // Allows the creation of tasks to send a notification.
+  // This allows the PromoResourceService to notify the New Tab Page immediately
+  // when a new web resource should be shown or removed.
+  base::WeakPtrFactory<PromoResourceService> weak_ptr_factory_;
+
+  // Notification type when an update is done.
+  int notification_type_;
+
+  // True if a task has been set to update the cache when a new web resource
+  // becomes available.
+  bool web_resource_update_scheduled_;
 
   // A helper that downloads the promo logo.
   scoped_ptr<AppsPromoLogoFetcher> apps_promo_logo_fetcher_;
--- chrome/browser/web_resource/promo_resource_service_unittest.cc
+++ chrome/browser/web_resource/promo_resource_service_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/json/json_reader.h"
+#include "base/message_loop.h"
 #include "base/string_number_conversions.h"
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
