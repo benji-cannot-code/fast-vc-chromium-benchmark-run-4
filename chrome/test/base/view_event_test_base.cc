e768639d79f23987c9c20a889c4f6f105e957113
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 22:03:28 +0000

    Initialize shell and browser thread for views event tests.  This is necessary as widget now needs shell to activate.
    BUG=none
    TEST=interactive_ui_tests no longer crashes.
    
    Review URL: http://codereview.chromium.org/8937007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/view_event_test_base.cc
+++ chrome/test/base/view_event_test_base.cc
@@ -16,12 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "chrome/browser/automation/ui_controls.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "content/public/browser/browser_thread.h"
 #include "ui/gfx/compositor/test/compositor_test_support.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/root_window.h"
+#include "ui/aura_shell/shell.h"
 #endif
 
 namespace {
@@ -61,6 +63,7 @@ const int kMouseMoveDelayMS = 200;
 ViewEventTestBase::ViewEventTestBase()
   : window_(NULL),
     content_view_(NULL),
+    ui_thread_(content::BrowserThread::UI, &message_loop_),
     ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {
 }
 
@@ -90,6 +93,7 @@ void ViewEventTestBase::SetUp() {
   ui::CompositorTestSupport::Initialize();
 #if defined(USE_AURA)
   aura::RootWindow::GetInstance();
+  aura_shell::Shell::CreateInstance(NULL);
 #endif
   window_ = views::Widget::CreateWindow(this);
 }
@@ -105,6 +109,7 @@ void ViewEventTestBase::TearDown() {
     window_ = NULL;
   }
 #if defined(USE_AURA)
+  aura_shell::Shell::DeleteInstance();
   aura::RootWindow::DeleteInstance();
 #endif
   ui::CompositorTestSupport::Terminate();
--- chrome/test/base/view_event_test_base.h
+++ chrome/test/base/view_event_test_base.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/message_loop.h"
 #include "base/threading/thread.h"
+#include "content/test/test_browser_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/widget/widget_delegate.h"
 
@@ -134,6 +135,8 @@ class ViewEventTestBase : public views::WidgetDelegate,
   scoped_ptr<base::Thread> dnd_thread_;
 
   MessageLoopForUI message_loop_;
+
+  content::TestBrowserThread ui_thread_;
 
   // Method factory used for time-outs.
   ScopedRunnableMethodFactory<ViewEventTestBase> method_factory_;
