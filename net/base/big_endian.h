c6b073cfd8bacea40e238e4381345e40cde429da
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 16:27:07 +0000

    Don't attempt to forward declare StringPiece.
    1) This is discouraged because it prevents callers from benefiting from automatic coersion from string/char* types.
    2) A follow-up CL (http://codereview.chromium.org/8659047/) will make StringPiece a template, and thus awkward to forward declare. The very small number of places that were appropriately forward declaring it do not justify writing a 'string_piece_forward.h'.
    
    BUG=87634
    R=cbentzel@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8929002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114219 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/big_endian.h
+++ net/base/big_endian.h
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
+#include "base/string_piece.h"
 #include "net/base/net_export.h"
-
-namespace base {
-class StringPiece;
-}
 
 namespace net {
 
