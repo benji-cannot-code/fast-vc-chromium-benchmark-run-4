d4415d96102b43f78028f3f3c8b10ea757050939
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 23:59:01 +0000

    Fix lion build.
    clang started warning about deprecated methods for receivers of
    type `id`. NSApp is type id in -arch i386, and apparently some
    stop: method of some type is marked as deprecated. Use
    -[NSApplication sharedInstance], which doesn't return type id.
    
    BUG=none
    TEST=build on lion
    
    
    Review URL: http://codereview.chromium.org/8890083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114312 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_mac.mm
+++ base/message_pump_mac.mm
@@ -578,7 +578,7 @@ void MessagePumpNSApplication::DoRun(Delegate* delegate) {
 
 void MessagePumpNSApplication::Quit() {
   if (!running_own_loop_) {
-    [NSApp stop:nil];
+    [[NSApplication sharedApplication] stop:nil];
   } else {
     keep_running_ = false;
   }
