6888d056db144a8f75c8e3712c0598fffa4fab01
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 19:19:36 +0000

    Extract common code from StringPiece and StringPiece16 into a templated base class. Convert copy-and-pasted unit tests into TYPED_TESTs.
    The motivation is that I wish to add constructors for string::iterator ranges and I don't want to do it twice.
    
    The motivation for adding the string::iterator range constructors is to reduce the number of overloads in string_number_conversions.h .
    
    BUG=87634
    TEST=
    
    Review URL: http://codereview.chromium.org/8659047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114242 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_piece.cc
+++ base/string_piece.cc
@@ -11,6 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+// MSVC doesn't like complex extern templates and DLLs.
+#if !defined(COMPILER_MSVC)
+namespace internal {
+template class StringPieceDetail<std::string>;
+template class StringPieceDetail<string16>;
+}  // namespace internal
+
+template class BasicStringPiece<std::string>;
+template class BasicStringPiece<string16>;
+#endif
+
 typedef StringPiece::size_type size_type;
 
 bool operator==(const StringPiece& x, const StringPiece& y) {
@@ -20,22 +31,25 @@ bool operator==(const StringPiece& x, const StringPiece& y) {
   return StringPiece::wordmemcmp(x.data(), y.data(), x.size()) == 0;
 }
 
-void StringPiece::CopyToString(std::string* target) const {
+void BasicStringPiece<std::string>::CopyToString(std::string* target) const {
   target->assign(!empty() ? data() : "", size());
 }
 
-void StringPiece::AppendToString(std::string* target) const {
+void BasicStringPiece<std::string>::AppendToString(std::string* target) const {
   if (!empty())
     target->append(data(), size());
 }
 
-size_type StringPiece::copy(char* buf, size_type n, size_type pos) const {
+size_type BasicStringPiece<std::string>::copy(char* buf,
+                                              size_type n,
+                                              size_type pos) const {
   size_type ret = std::min(length_ - pos, n);
   memcpy(buf, ptr_ + pos, ret);
   return ret;
 }
 
-size_type StringPiece::find(const StringPiece& s, size_type pos) const {
+size_type BasicStringPiece<std::string>::find(const BasicStringPiece& s,
+                                              size_type pos) const {
   if (pos > length_)
     return npos;
 
@@ -45,7 +59,7 @@ size_type StringPiece::find(const StringPiece& s, size_type pos) const {
   return xpos + s.length_ <= length_ ? xpos : npos;
 }
 
-size_type StringPiece::find(char c, size_type pos) const {
+size_type BasicStringPiece<std::string>::find(char c, size_type pos) const {
   if (pos >= length_)
     return npos;
 
@@ -53,7 +67,8 @@ size_type StringPiece::find(char c, size_type pos) const {
   return result != ptr_ + length_ ? static_cast<size_t>(result - ptr_) : npos;
 }
 
-size_type StringPiece::rfind(const StringPiece& s, size_type pos) const {
+size_type BasicStringPiece<std::string>::rfind(const BasicStringPiece& s,
+                                               size_type pos) const {
   if (length_ < s.length_)
     return npos;
 
@@ -65,7 +80,7 @@ size_type StringPiece::rfind(const StringPiece& s, size_type pos) const {
   return result != last ? static_cast<size_t>(result - ptr_) : npos;
 }
 
-size_type StringPiece::rfind(char c, size_type pos) const {
+size_type BasicStringPiece<std::string>::rfind(char c, size_type pos) const {
   if (length_ == 0)
     return npos;
 
@@ -95,8 +110,8 @@ static inline void BuildLookupTable(const StringPiece& characters_wanted,
   }
 }
 
-size_type StringPiece::find_first_of(const StringPiece& s,
-                                     size_type pos) const {
+size_type BasicStringPiece<std::string>::find_first_of(
+    const BasicStringPiece& s, size_type pos) const {
   if (length_ == 0 || s.length_ == 0)
     return npos;
 
@@ -114,8 +129,8 @@ size_type StringPiece::find_first_of(const StringPiece& s,
   return npos;
 }
 
-size_type StringPiece::find_first_not_of(const StringPiece& s,
-                                         size_type pos) const {
+size_type BasicStringPiece<std::string>::find_first_not_of(
+    const BasicStringPiece& s, size_type pos) const {
   if (length_ == 0)
     return npos;
 
@@ -136,7 +151,8 @@ size_type StringPiece::find_first_not_of(const StringPiece& s,
   return npos;
 }
 
-size_type StringPiece::find_first_not_of(char c, size_type pos) const {
+size_type BasicStringPiece<std::string>::find_first_not_of(
+    char c, size_type pos) const {
   if (length_ == 0)
     return npos;
 
@@ -148,7 +164,8 @@ size_type StringPiece::find_first_not_of(char c, size_type pos) const {
   return npos;
 }
 
-size_type StringPiece::find_last_of(const StringPiece& s, size_type pos) const {
+size_type BasicStringPiece<std::string>::find_last_of(const StringPiece& s,
+                                                      size_type pos) const {
   if (length_ == 0 || s.length_ == 0)
     return npos;
 
@@ -167,8 +184,8 @@ size_type StringPiece::find_last_of(const StringPiece& s, size_type pos) const {
   return npos;
 }
 
-size_type StringPiece::find_last_not_of(const StringPiece& s,
-                                        size_type pos) const {
+size_type BasicStringPiece<std::string>::find_last_not_of(
+    const BasicStringPiece& s, size_type pos) const {
   if (length_ == 0)
     return npos;
 
@@ -191,7 +208,8 @@ size_type StringPiece::find_last_not_of(const StringPiece& s,
   return npos;
 }
 
-size_type StringPiece::find_last_not_of(char c, size_type pos) const {
+size_type BasicStringPiece<std::string>::find_last_not_of(char c,
+                                                          size_type pos) const {
   if (length_ == 0)
     return npos;
 
@@ -204,12 +222,11 @@ size_type StringPiece::find_last_not_of(char c, size_type pos) const {
   return npos;
 }
 
-StringPiece StringPiece::substr(size_type pos, size_type n) const {
+BasicStringPiece<std::string> BasicStringPiece<std::string>::substr(
+    size_type pos, size_type n) const {
   if (pos > length_) pos = length_;
   if (n > length_ - pos) n = length_ - pos;
   return StringPiece(ptr_ + pos, n);
 }
-
-const StringPiece::size_type StringPiece::npos = size_type(-1);
 
 }  // namespace base
--- base/utf_offset_string_conversions.h
+++ base/utf_offset_string_conversions.h
@@ -12,11 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/string16.h"
-
-namespace base {
-class StringPiece;
-class StringPiece16;
-}
+#include "base/string_piece.h"
 
 // Like the conversions in utf_string_conversions.h, but also takes one or more
 // offsets (|offset[s]_for_adjustment|) into the source strings, each offset
