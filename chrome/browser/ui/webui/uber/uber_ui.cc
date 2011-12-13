7b42547357c0a4a510690f3830f7087ff3ba2449
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 21:58:42 +0000

    Revert "Options2: Pull the trigger."
    This reverts commit 11079a4772dacf7865c3d650b7aa911242bb782e.
    
    Revert "Fix build after code changed from underneath."
    
    This reverts commit 93b43a2cd246c85ee6159a70223966f5efe2af86.
    
    Revert "Moar build fix."
    
    This reverts commit bf84b5766d5d05466da641685a1f554786598310.
    
    Revert "Fix the fix."
    
    This reverts commit 4aecf275ac5c7163483f48ffa2abcfa1481ffd53.
    
    R=csilv@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8937011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114276 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/uber/uber_ui.cc
+++ chrome/browser/ui/webui/uber/uber_ui.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
-#include "chrome/browser/ui/webui/options2/options_ui.h"
+#include "chrome/browser/ui/webui/options/options_ui.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "grit/browser_resources.h"
@@ -31,7 +31,7 @@ UberUI::UberUI(TabContents* contents) : ChromeWebUI(contents) {
   Profile* profile = Profile::FromBrowserContext(contents->browser_context());
   profile->GetChromeURLDataManager()->AddDataSource(CreateUberHTMLSource());
 
-  ChromeWebUI* options = new Options2UI(contents);
+  ChromeWebUI* options = new OptionsUI(contents);
   options->set_frame_xpath("//iframe[@id='settings']");
   sub_uis_.push_back(options);
 }
--- chrome/chrome_repack_resources.gypi
+++ chrome/chrome_repack_resources.gypi
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       '<(grit_out_dir)/devtools_resources.pak',
       '<(grit_out_dir)/net_internals_resources.pak',
       '<(grit_out_dir)/options_resources.pak',
-      '<(grit_out_dir)/options2_resources.pak',
       '<(grit_out_dir)/quota_internals_resources.pak',
       '<(grit_out_dir)/shared_resources.pak',
       '<(grit_out_dir)/sync_internals_resources.pak',
