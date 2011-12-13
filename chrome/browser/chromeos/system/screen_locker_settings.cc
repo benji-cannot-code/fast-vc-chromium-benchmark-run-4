87f97e0c2d28abad508cf4ccd7e23b4d85a418e8
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 01:00:34 +0000

    Fix erroneous include in screen_locker_settings.
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/8923011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/screen_locker_settings.cc
+++ chrome/browser/chromeos/system/screen_locker_settings.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/system/touchpad_settings.h"
+#include "chrome/browser/chromeos/system/screen_locker_settings.h"
 
 #include "base/bind.h"
 #include "base/file_path.h"
