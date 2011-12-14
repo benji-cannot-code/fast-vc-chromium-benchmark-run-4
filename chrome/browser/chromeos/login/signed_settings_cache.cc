64e22e3702ce34708fbb36478faa989d240f6110
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-14 12:19:40 +0000

    Only serialize the signed settings cache to the policy blob if there is a local owner.
    Until now the cache attempted serialization every time the owner key was loaded.
    However this also happens on enterprise managed devices and leads to an error
    message in the log files which this patch tries to avoid.
    
    BUG=24007
    TEST=Enroll device in a domain and check that the "Couldn't save temp store to the policy blob. code: 2"  error message is not present.
    
    Review URL: http://codereview.chromium.org/8890077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114411 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/signed_settings_cache.cc
+++ chrome/browser/chromeos/login/signed_settings_cache.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/cros_settings.h"
 #include "chrome/browser/chromeos/login/ownership_service.h"
+#include "chrome/browser/chromeos/login/ownership_status_checker.h"
 #include "chrome/browser/chromeos/login/signed_settings_helper.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/prefs/scoped_user_pref_update.h"
@@ -68,6 +69,20 @@ void FinishFinalize(PrefService* local_state,
   }
 }
 
+// Reload the initial policy blob, and if successful apply the settings from
+// temp storage, and write them back the blob in FinishFinalize.
+void ReloadSignedSettingsAndFinalize(
+    PrefService* local_state,
+    OwnershipStatusChecker* ownership_checker,
+    OwnershipService::Status status,
+    bool current_user_is_owner) {
+  if (current_user_is_owner) {
+    SignedSettingsHelper::Get()->StartRetrievePolicyOp(
+        base::Bind(FinishFinalize, local_state));
+  }
+  delete ownership_checker;
+}
+
 }  // namespace
 
 namespace signed_settings_cache {
@@ -107,10 +122,11 @@ bool Retrieve(em::PolicyData *policy, PrefService* local_state) {
 }
 
 void Finalize(PrefService* local_state) {
-  // Reload the initial policy blob, apply settings from temp storage,
-  // and write back the blob.
-  SignedSettingsHelper::Get()->StartRetrievePolicyOp(
-      base::Bind(FinishFinalize, local_state));
+  // First we have to make sure the owner is really logged in because the key
+  // notification is generated on every cloud policy key rotation too.
+  OwnershipStatusChecker* ownership_checker = new OwnershipStatusChecker();
+  ownership_checker->Check(base::Bind(&ReloadSignedSettingsAndFinalize,
+                                      local_state, ownership_checker));
 }
 
 }  // namespace signed_settings_cache
