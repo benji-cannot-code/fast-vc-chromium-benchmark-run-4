4386d6e6c257ebb756619dd97b3cc9327347288c
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-14 01:52:40 +0000

    [chromium] Use HashMap<..., OwnPtr<Tile>> for compositor tilemap https://bugs.webkit.org/show_bug.cgi?id=74154
    Reviewed by James Robinson.
    
    After r102410 landed, it's now possible to properly use an OwnPtr to
    store tiles rather than hackily use a RefPtr.
    
    Covered by the compositing/ layout tests.
    
    * platform/graphics/chromium/TiledLayerChromium.cpp:
    (WebCore::UpdatableTile::create):
    (WebCore::UpdatableTile::UpdatableTile):
    (WebCore::TiledLayerChromium::createTile):
    * platform/graphics/chromium/cc/CCLayerTilingData.cpp:
    (WebCore::CCLayerTilingData::addTile):
    (WebCore::CCLayerTilingData::takeTile):
    (WebCore::CCLayerTilingData::tileAt):
    * platform/graphics/chromium/cc/CCLayerTilingData.h:
    * platform/graphics/chromium/cc/CCTiledLayerImpl.cpp:
    (WebCore::DrawableTile::create):
    (WebCore::DrawableTile::DrawableTile):
    (WebCore::CCTiledLayerImpl::createTile):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102726 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTilingData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTilingData.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/HashMap.h>
 #include <wtf/HashTraits.h>
 #include <wtf/PassOwnPtr.h>
-#include <wtf/RefCounted.h>
 
 namespace WebCore {
 
@@ -63,7 +62,7 @@ public:
 
     const CCLayerTilingData& operator=(const CCLayerTilingData&);
 
-    class Tile: public RefCounted<Tile> {
+    class Tile {
         WTF_MAKE_NONCOPYABLE(Tile);
     public:
         Tile() : m_i(-1), m_j(-1) { }
@@ -86,13 +85,10 @@ public:
         static void constructDeletedValue(TileMapKey& slot) { slot = std::make_pair(-2, -2); }
         static bool isDeletedValue(TileMapKey value) { return value.first == -2 && value.second == -2; }
     };
-    // FIXME: The mapped value in TileMap should really be an OwnPtr, as the
-    // refcount of a Tile should never be more than 1. However, HashMap
-    // doesn't easily support OwnPtr as a value.
-    typedef HashMap<TileMapKey, RefPtr<Tile>, DefaultHash<TileMapKey>::Hash, TileMapKeyTraits> TileMap;
+    typedef HashMap<TileMapKey, OwnPtr<Tile>, DefaultHash<TileMapKey>::Hash, TileMapKeyTraits> TileMap;
 
-    void addTile(PassRefPtr<Tile>, int, int);
-    PassRefPtr<Tile> takeTile(int, int);
+    void addTile(PassOwnPtr<Tile>, int, int);
+    PassOwnPtr<Tile> takeTile(int, int);
     Tile* tileAt(int, int) const;
     const TileMap& tiles() const { return m_tiles; }
 
