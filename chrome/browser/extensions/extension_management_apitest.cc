adb6c9eb9c418b854e111a75a2bd157768f87315
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-14 06:11:21 +0000

    Enable panels flag for all tests that use panels.
    Tests on the trunk don't need the flag, but tests on beta/stable do.
    Flag can be removed when panels are enabled by default.
    
    BUG=107422
    TEST=tests updated
    
    TBR=dimich@chromium.org
    
    Review URL: http://codereview.chromium.org/8890093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_management_apitest.cc
+++ chrome/browser/extensions/extension_management_apitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 
 namespace {
@@ -31,6 +32,11 @@ Browser* FindOtherBrowser(Browser* browser) {
 
 class ExtensionManagementApiTest : public ExtensionApiTest {
  public:
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kEnablePanels);
+  }
+
   virtual void InstallExtensions() {
     FilePath basedir = test_data_dir_.AppendASCII("management");
 
--- chrome/browser/ui/browser_init_browsertest.cc
+++ chrome/browser/ui/browser_init_browsertest.cc
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class BrowserInitTest : public ExtensionBrowserTest {
  protected:
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    ExtensionBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kEnablePanels);
+  }
+
   // Helper functions return void so that we can ASSERT*().
   // Use ASSERT_NO_FATAL_FAILURE around calls to these functions to stop the
   // test if an assert fails.
