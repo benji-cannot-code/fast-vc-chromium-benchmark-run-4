52434668dcf52500d481bc8e109e99715f5a9815
arthurhsu@chromium.org <arthurhsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-14 05:40:30 +0000

    Push subsetter code upstream
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8921027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114354 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -133,7 +133,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*  Define this to provide font subsetter for font subsetting when generating
     PDF documents.
  */
-#define SK_SFNTLY_SUBSETTER "third_party/sfntly/src/subsetter/font_subsetter.h"
+#define SK_SFNTLY_SUBSETTER \
+    "third_party/sfntly/cpp/src/sample/chromium/font_subsetter.h"
 
 /*  Define this to remove dimension checks on bitmaps. Not all blits will be
     correct yet, so this is mostly for debugging the implementation.
