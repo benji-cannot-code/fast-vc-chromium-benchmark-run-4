90135340d0d1d0eb7af2256465845330370d55fc
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-14 05:07:42 +0000

    Unreviewed, rolling out r102732. http://trac.webkit.org/changeset/102732
    The last fix makes no sense...
    
    * platform/graphics/chromium/cc/CCLayerTilingData.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTilingData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTilingData.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TilingData.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashTraits.h>
-#include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
 namespace WebCore {
