67cb771589da3c7d12098429aa9a982e9b823a6b
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 20:23:16 +0000

    Implement RunFileChooser for ExtensionHost, so that file input elements work in extension popups.
    Also fix a leak that happens if the renderer that initiated a file chooser is
    closed before the file chooser appears.
    
    BUG=104222
    TEST=see bug for repro
    
    Review URL: http://codereview.chromium.org/8909015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_host.cc
+++ chrome/browser/extensions/extension_host.cc
@@ -464,6 +464,11 @@ content::JavaScriptDialogCreator* ExtensionHost::GetJavaScriptDialogCreator() {
   return GetJavaScriptDialogCreatorInstance();
 }
 
+void ExtensionHost::RunFileChooser(TabContents* tab,
+                                   const content::FileChooserParams& params) {
+  Browser::RunFileChooserHelper(tab, params);
+}
+
 void ExtensionHost::AddNewContents(TabContents* source,
                                    TabContents* new_contents,
                                    WindowOpenDisposition disposition,
