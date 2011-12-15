c42ff4374fda844e230ce79cbb88b8d96a2dd9ff
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 20:18:17 +0000

    Do not show downloads panel for temporary downloads.
    BUG=106856
    TEST=DownloadTest.DownloadUrlToFile should pass on ChromeOS.
    
    Review URL: http://codereview.chromium.org/8929026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/active_downloads_ui.h
+++ chrome/browser/ui/webui/active_downloads_ui.h
@@ -24,6 +24,7 @@ class ActiveDownloadsUI : public HtmlDialogUI {
  public:
   explicit ActiveDownloadsUI(TabContents* contents);
 
+  static bool ShouldShowPopup(Profile* profile, DownloadItem* download);
   static Browser* OpenPopup(Profile* profile);
   static Browser* GetPopup();
 
