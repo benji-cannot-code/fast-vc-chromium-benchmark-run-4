80524a667f458caae491fcc4d11c758d87d9220b
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 13:36:48 +0000

    Add presubmit checks for TSan suppressions TEST=`gclient presubmit cl` with different errors in tools/valgrind/tsan/suppressions.txt Review URL: http://codereview.chromium.org/8951011
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114629 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/tsan/PRESUBMIT.py
+++ tools/valgrind/tsan/PRESUBMIT.py
@@ -3,10 +3,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+import re
+import sys
+
 """
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
+
+def CheckChange(input_api, output_api):
+  """Checks the TSan suppressions files for bad suppressions."""
+
+  # TODO(timurrrr): find out how to do relative imports
+  # and remove this ugly hack. Also, the CheckChange function won't be needed.
+  tools_vg_path = os.path.join(input_api.PresubmitLocalPath(), '..')
+  sys.path.append(tools_vg_path)
+  import suppressions
+
+  return suppressions.PresubmitCheck(input_api, output_api)
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api)
 
 def GetPreferredTrySlaves():
   return ['linux_tsan']
