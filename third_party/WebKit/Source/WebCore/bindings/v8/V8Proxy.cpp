3313ea67ebda47a7d869f4103577a9ac628272ee
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-15 03:22:03 +0000

    Unreviewed, rolling out r102794. http://trac.webkit.org/changeset/102794 https://bugs.webkit.org/show_bug.cgi?id=74220
    Reapplying patch since it is not the cause of the problems
    described in bug 74220.
    
    * bindings/v8/V8Proxy.cpp:
    (WebCore::V8Proxy::reportUnsafeAccessTo):
    * bindings/v8/V8Proxy.h:
    * bindings/v8/custom/V8CustomXPathNSResolver.cpp:
    (WebCore::V8CustomXPathNSResolver::lookupNamespaceURI):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CSSMutableStyleDeclaration.h"
 #include "CachedMetadata.h"
-#include "Console.h"
 #include "DateExtension.h"
 #include "Document.h"
 #include "DocumentLoader.h"
@@ -44,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClient.h"
 #include "IDBFactoryBackendInterface.h"
 #include "InspectorInstrumentation.h"
-#include "Page.h"
 #include "PlatformSupport.h"
 #include "ScriptSourceCode.h"
 #include "SecurityOrigin.h"
@@ -120,21 +118,6 @@ typedef HashMap<Node*, v8::Object*> DOMNodeMap;
 typedef HashMap<void*, v8::Object*> DOMObjectMap;
 typedef HashMap<int, v8::FunctionTemplate*> FunctionTemplateMap;
 
-static void addMessageToConsole(Page* page, const String& message, const String& sourceID, unsigned lineNumber)
-{
-    ASSERT(page);
-    Console* console = page->mainFrame()->domWindow()->console();
-    console->addMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, message, lineNumber, sourceID);
-}
-
-void logInfo(Frame* frame, const String& message, const String& url)
-{
-    Page* page = frame->page();
-    if (!page)
-        return;
-    addMessageToConsole(page, message, url, 0);
-}
-
 void V8Proxy::reportUnsafeAccessTo(Frame* target)
 {
     ASSERT(target);
@@ -144,9 +127,6 @@ void V8Proxy::reportUnsafeAccessTo(Frame* target)
 
     Frame* source = V8Proxy::retrieveFrameForEnteredContext();
     if (!source)
-        return;
-    Page* page = source->page();
-    if (!page)
         return;
 
     Document* sourceDocument = source->document();
@@ -158,14 +138,10 @@ void V8Proxy::reportUnsafeAccessTo(Frame* target)
     String str = "Unsafe JavaScript attempt to access frame with URL " + targetDocument->url().string() +
                  " from frame with URL " + sourceDocument->url().string() + ". Domains, protocols and ports must match.\n";
 
-    // Build a console message with fake source ID and line number.
-    const String kSourceID = "";
-    const int kLineNumber = 1;
-
     // NOTE: Safari prints the message in the target page, but it seems like
     // it should be in the source page. Even for delayed messages, we put it in
     // the source page.
-    addMessageToConsole(page, str, kSourceID, kLineNumber);
+    sourceDocument->addConsoleMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, str);
 }
 
 static void handleFatalErrorInV8()
