436bdafe6fd7828b5b3d99fe7366823a5778fffd
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-15 23:10:33 +0000

    REGRESSION(r102663): generate-bindings.pl runs every time https://bugs.webkit.org/show_bug.cgi?id=74481
    Reviewed by Adam Barth.
    
    See the comment #1 of bug 74481 for the cause of this bug.
    
    This patch fixes generate-bindings.pl so that it generates .h and .cpp files
    even for IDL files that do not need .h and .cpp files. This is just to prevent
    build scripts from trying to generate .h and .cpp files at every build.
    
    No new tests. No change in behavior.
    
    * bindings/scripts/generate-bindings.pl:
    (generateEmptyHeaderAndCpp): Generates .h and .cpp files for IDL files
    that do not need .h and .cpp files.
    * bindings/scripts/test/CPP/CPPTestSupplemental.cpp: Added.
    * bindings/scripts/test/CPP/CPPTestSupplemental.h: Added.
    * bindings/scripts/test/GObject/GObjectTestSupplemental.cpp: Added.
    * bindings/scripts/test/GObject/GObjectTestSupplemental.h: Added.
    * bindings/scripts/test/JS/JSTestSupplemental.cpp: Added.
    * bindings/scripts/test/JS/JSTestSupplemental.h: Added.
    * bindings/scripts/test/ObjC/ObjCTestSupplemental.cpp: Added.
    * bindings/scripts/test/ObjC/ObjCTestSupplemental.h: Added.
    * bindings/scripts/test/V8/V8TestSupplemental.cpp: Added.
    * bindings/scripts/test/V8/V8TestSupplemental.h: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/ObjCTestSupplemental.cpp
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+    This file is generated just to tell build scripts that ObjCTestSupplemental.h and
+    ObjCTestSupplemental.cpp are created for TestSupplemental.idl, and thus
+    prevent the build scripts from trying to generate ObjCTestSupplemental.h and
+    ObjCTestSupplemental.cpp at every build. This file must not be tried to compile.
+*/
