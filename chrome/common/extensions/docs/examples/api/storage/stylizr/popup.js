7a5f64d6a581c68c357f7f32a5024b8df0e7a0c0
smus@chromium.org <smus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 00:02:15 +0000

    Added storage sample.
    BUG=98591
    TEST=
    
    Review URL: http://codereview.chromium.org/8916013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114531 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/api/storage/stylizr/popup.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Store settings in the synchronized repository.
+var storage = chrome.experimental.storage.sync;
+var message = document.querySelector('#message');
+
+// Check if there is CSS specified.
+storage.get('css', function(items) {
+  console.log(items);
+  // If there is CSS specified, inject it into the page.
+  if (items.css) {
+    chrome.tabs.insertCSS(null, {code: items.css}, function() {
+      if (chrome.extension.lastError) {
+        message.innerText = 'Not allowed to inject CSS into special page.';
+      } else {
+        message.innerText = 'Injected style!';
+      }
+    });
+  } else {
+    var optionsUrl = chrome.extension.getURL('options.html');
+    message.innerHTML = 'Set a style in the <a target="_blank" href="' +
+        optionsUrl + '">options page</a> first.';
+  }
+});
+
