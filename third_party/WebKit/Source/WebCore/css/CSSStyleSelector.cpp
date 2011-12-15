2b8d52950053fa9db46afbbd90e30f59d78e56ae
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-15 01:30:35 +0000

    Filter amounts should accept percentages https://bugs.webkit.org/show_bug.cgi?id=74531
    Source/WebCore:
    
    Reviewed by Chris Marrin.
    
    Support percentage arguments for some filter functions, using the FPercent flag
    to validUnit(). Fix CSSStyleSelector::createFilterOperations() to do the divide by 100
    for percentages.
    
    Replaced isValidFilterArgument(), which just tested arguments one by one for validity, with
    parseBuiltinFilterArguments() which tests and creates the CSSValues at the same time, which
    is a little more efficient. It also allows filter-specific behavior to be more localized in this
    method.
    
    Covered by existing tests.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseBuiltinFilterArguments):
    (WebCore::CSSParser::parseFilter):
    * css/CSSParser.h:
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::createFilterOperations):
    
    LayoutTests:
    
    Reviewed by Chris Marrin.
    
    Adjust test results now that percentages are allowed for some
    filter arguments, and add testing of percentage arguments to filter-property-computed-style.js
    
    * css3/filters/filter-property-computed-style-expected.txt:
    * css3/filters/filter-property-parsing-invalid-expected.txt:
    * css3/filters/script-tests/filter-property-computed-style.js:
    * css3/filters/script-tests/filter-property-parsing-invalid.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -5325,8 +5325,11 @@ bool CSSStyleSelector::createFilterOperations(CSSValue* inValue, RenderStyle* st
         case WebKitCSSFilterValue::SepiaFilterOperation:
         case WebKitCSSFilterValue::SaturateFilterOperation: {
             double amount = 1;
-            if (filterValue->length() == 1)
+            if (filterValue->length() == 1) {
                 amount = firstValue->getDoubleValue();
+                if (firstValue->primitiveType() == CSSPrimitiveValue::CSS_PERCENTAGE)
+                    amount /= 100;
+            }
 
             operations.operations().append(BasicColorMatrixFilterOperation::create(amount, operationType));
             break;
@@ -5349,8 +5352,11 @@ bool CSSStyleSelector::createFilterOperations(CSSValue* inValue, RenderStyle* st
         case WebKitCSSFilterValue::InvertFilterOperation:
         case WebKitCSSFilterValue::OpacityFilterOperation: {
             double amount = 1;
-            if (filterValue->length() == 1)
+            if (filterValue->length() == 1) {
                 amount = firstValue->getDoubleValue();
+                if (firstValue->primitiveType() == CSSPrimitiveValue::CSS_PERCENTAGE)
+                    amount /= 100;
+            }
 
             operations.operations().append(BasicComponentTransferFilterOperation::create(amount, operationType));
             break;
