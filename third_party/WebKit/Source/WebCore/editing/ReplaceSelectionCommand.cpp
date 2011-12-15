2cf2ff9310e858dafe19983bd4b136aa5fdb6e0c
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-15 01:24:18 +0000

    Build fix.
    Unreviewed.
    
    * editing/ReplaceSelectionCommand.cpp: Added include RenderStyle.h
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NodeList.h"
 #include "RenderInline.h"
 #include "RenderObject.h"
+#include "RenderStyle.h"
 #include "RenderText.h"
 #include "SmartReplace.h"
 #include "TextIterator.h"
