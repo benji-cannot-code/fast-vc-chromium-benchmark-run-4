a6546ff40f4782c9c517f70b675f0daa54165519
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 10:15:40 +0000

    Reverting due to compile failure on multiple bots.
    Revert 114615 - chrome.clear: Increasing granularity of public API
    
    http://codereview.chromium.org/7717023 added more granular options to
    BrowsingDataRemover. This CL exposes those options to the chrome.clear
    extension API. Among other things, this means that chrome.clear.cookies()
    will _only_ clear cookies, not cookies and site data.
    
    At the moment, clearing any quota managed data type will clear them all.
    That is being addressed in http://codereview.chromium.org/7839029/
    but is independent from changing the public interface.
    
    BUG=94334
    TEST=browser_tests
    
    Review URL: http://codereview.chromium.org/8008012
    
    TBR=mkwst@chromium.org
    Review URL: http://codereview.chromium.org/8949014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114619 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/extension_clear_apitest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "chrome/browser/browsing_data_remover.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/common/chrome_switches.h"
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Clear) {
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kEnableExperimentalExtensionApis);
+
+  ASSERT_TRUE(RunExtensionTest("clear/api")) << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, ClearOneAtATime) {
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+            switches::kEnableExperimentalExtensionApis);
+  BrowsingDataRemover::set_removing(true);
+  ASSERT_TRUE(RunExtensionTest("clear/one_at_a_time")) << message_;
+  BrowsingDataRemover::set_removing(false);
+}
