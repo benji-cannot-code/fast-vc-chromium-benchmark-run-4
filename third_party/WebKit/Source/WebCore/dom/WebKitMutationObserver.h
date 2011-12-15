cffcc0c6d1efd44fad83d55bf80b8613eb2528e2
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-15 05:26:55 +0000

    Optimize MutationObserverInterestGroup construction https://bugs.webkit.org/show_bug.cgi?id=74563
    Reviewed by Ojan Vafai.
    
    Inline MutationObserverInterestGroup's creation methods and fix an
    accidental pass-by-value in an attempt to further reduce the CPU
    footprint of MutationObservers.
    
    No new tests, refactor only.
    
    * dom/WebKitMutationObserver.cpp:
    (WebCore::MutationObserverInterestGroup::MutationObserverInterestGroup): Pass observers HashMap by reference.
    * dom/WebKitMutationObserver.h:
    (WebCore::MutationObserverInterestGroup::createForChildListMutation): Inline.
    (WebCore::MutationObserverInterestGroup::createForCharacterDataMutation): Inline.
    (WebCore::MutationObserverInterestGroup::createForAttributesMutation): Inline.
    (WebCore::MutationObserverInterestGroup::hasOldValue): Remove spurious inline keyword.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/WebKitMutationObserver.h
+++ third_party/WebKit/Source/WebCore/dom/WebKitMutationObserver.h
@@ -34,8 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(MUTATION_OBSERVERS)
 
+#include "QualifiedName.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
+#include <wtf/PassOwnPtr.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
@@ -99,17 +101,28 @@ private:
 
 class MutationObserverInterestGroup {
 public:
-    static PassOwnPtr<MutationObserverInterestGroup> createForChildListMutation(Node* target);
-    static PassOwnPtr<MutationObserverInterestGroup> createForCharacterDataMutation(Node* target);
-    static PassOwnPtr<MutationObserverInterestGroup> createForAttributesMutation(Node* target, const QualifiedName& attributeName);
+    static PassOwnPtr<MutationObserverInterestGroup> createForChildListMutation(Node* target)
+    {
+        MutationRecordDeliveryOptions oldValueFlag = 0;
+        return createIfNeeded(target, WebKitMutationObserver::ChildList, nullAtom, oldValueFlag);
+    }
+    static PassOwnPtr<MutationObserverInterestGroup> createForCharacterDataMutation(Node* target)
+    {
+        return createIfNeeded(target, WebKitMutationObserver::CharacterData, nullAtom, WebKitMutationObserver::CharacterDataOldValue);
+    }
+    static PassOwnPtr<MutationObserverInterestGroup> createForAttributesMutation(Node* target, const QualifiedName& attributeName)
+    {
+        return createIfNeeded(target, WebKitMutationObserver::Attributes, attributeName.localName(), WebKitMutationObserver::AttributeOldValue);
+    }
 
     bool isOldValueRequested();
     void enqueueMutationRecord(PassRefPtr<MutationRecord>);
+
 private:
     static PassOwnPtr<MutationObserverInterestGroup> createIfNeeded(Node* target, WebKitMutationObserver::MutationType, const AtomicString& attributeName, MutationRecordDeliveryOptions oldValueFlag);
-    MutationObserverInterestGroup(HashMap<WebKitMutationObserver*, MutationRecordDeliveryOptions> observers, MutationRecordDeliveryOptions oldValueFlag);
+    MutationObserverInterestGroup(HashMap<WebKitMutationObserver*, MutationRecordDeliveryOptions>& observers, MutationRecordDeliveryOptions oldValueFlag);
 
-    inline bool hasOldValue(MutationRecordDeliveryOptions options) { return options & m_oldValueFlag; }
+    bool hasOldValue(MutationRecordDeliveryOptions options) { return options & m_oldValueFlag; }
 
     HashMap<WebKitMutationObserver*, MutationRecordDeliveryOptions> m_observers;
     MutationRecordDeliveryOptions m_oldValueFlag;
