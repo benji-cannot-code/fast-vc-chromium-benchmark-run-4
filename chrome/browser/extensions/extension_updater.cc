f8e517afe699e89a8f14cc1cc7f41e7a28b6ec42
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 00:42:46 +0000

    Add some VLOG debugging info for extensions updating.
    Eventually it would be great to have something more user friendly like 
    chrome:net-internals, but this is at least a start.
    
    BUG=30812
    TEST=Run chrome with --vmodule=extension_updater=2, then try doing an 
    extension update check (eg from chrome://extensions page, turn on development
    mode and then hit the update now button). You should see some debugging output
    on the console.
    
    
    Review URL: http://codereview.chromium.org/8929019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_updater.cc
+++ chrome/browser/extensions/extension_updater.cc
@@ -598,6 +598,8 @@ void ExtensionUpdater::OnURLFetchComplete(const content::URLFetcher* source) {
   // called after Stop() is called.
   DCHECK(alive_);
 
+  VLOG(2) << source->GetResponseCode() << " " << source->GetURL();
+
   if (source == manifest_fetcher_.get()) {
     std::string data;
     source->GetResponseAsString(&data);
@@ -690,6 +692,8 @@ class SafeManifestParser : public UtilityProcessHost::Client {
 
   void OnParseUpdateManifestSucceeded(
       const UpdateManifest::Results& results) {
+    VLOG(2) << "parsing manifest succeeded (" << fetch_data_->full_url()
+            << ")";
     DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
     if (!updater_) {
       return;
@@ -698,6 +702,7 @@ class SafeManifestParser : public UtilityProcessHost::Client {
   }
 
   void OnParseUpdateManifestFailed(const std::string& error_message) {
+    VLOG(2) << "parsing manifest failed (" << fetch_data_->full_url() << ")";
     DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
     if (!updater_) {
       return;
@@ -729,6 +734,7 @@ void ExtensionUpdater::OnManifestFetchComplete(
   // available, we want to fire off requests to fetch those updates.
   if (status.status() == net::URLRequestStatus::SUCCESS &&
       (response_code == 200 || (url.SchemeIsFile() && data.length() > 0))) {
+    VLOG(2) << "beginning manifest parse for " << url;
     scoped_refptr<SafeManifestParser> safe_parser(
         new SafeManifestParser(data, current_manifest_fetch_.release(),
                                weak_ptr_factory_.GetWeakPtr()));
@@ -880,6 +886,8 @@ void ExtensionUpdater::OnCRXFileWritten(const std::string& id,
                                         const GURL& download_url) {
   DCHECK(alive_);
 
+  VLOG(2) << download_url << " written to " << path.value();
+
   FetchedCRXFile fetched(id, path, download_url);
   fetched_crx_files_.push(fetched);
 
@@ -892,6 +900,8 @@ bool ExtensionUpdater::MaybeInstallCRXFile() {
 
   while (!fetched_crx_files_.empty() && !crx_install_is_running_) {
     const FetchedCRXFile& crx_file = fetched_crx_files_.top();
+
+    VLOG(2) << "updating " << crx_file.id << " with " << crx_file.path.value();
 
     // The ExtensionService is now responsible for cleaning up the temp file
     // at |extension_file.path|.
@@ -1006,6 +1016,7 @@ void ExtensionUpdater::DoCheckSoon() {
 }
 
 void ExtensionUpdater::CheckNow() {
+  VLOG(2) << "Starting update check";
   DCHECK(alive_);
   NotifyStarted();
   ManifestFetchesBuilder fetches_builder(service_, extension_prefs_);
@@ -1102,16 +1113,31 @@ std::vector<int> ExtensionUpdater::DetermineUpdates(
   for (size_t i = 0; i < possible_updates.list.size(); i++) {
     const UpdateManifest::Result* update = &possible_updates.list[i];
 
-    if (!fetch_data.Includes(update->extension_id))
+    if (!fetch_data.Includes(update->extension_id)) {
+      VLOG(2) << "Ignoring " << update->extension_id << " from this manifest";
       continue;
+    }
+
+    if (VLOG_IS_ON(2)) {
+      if (update->version.empty())
+        VLOG(2) << "manifest indicates " << update->extension_id
+                << " has no update";
+      else
+        VLOG(2) << "manifest indicates " << update->extension_id
+                << " latest version is '" << update->version << "'";
+    }
 
     if (!pending_extension_manager->IsIdPending(update->extension_id)) {
       // If we're not installing pending extension, and the update
       // version is the same or older than what's already installed,
       // we don't want it.
       std::string version;
-      if (!GetExistingVersion(update->extension_id, &version))
+      if (!GetExistingVersion(update->extension_id, &version)) {
+        VLOG(2) << update->extension_id << " is not installed";
         continue;
+      }
+
+      VLOG(2) << update->extension_id << " is at '" << version << "'";
 
       Version existing_version(version);
       Version update_version(update->version);
@@ -1141,6 +1167,7 @@ std::vector<int> ExtensionUpdater::DetermineUpdates(
         continue;
       }
     }
+    VLOG(2) << "will try to update " << update->extension_id;
     result.push_back(i);
   }
   return result;
@@ -1169,6 +1196,14 @@ void ExtensionUpdater::StartUpdateCheck(ManifestFetchData* fetch_data) {
   } else {
     UMA_HISTOGRAM_COUNTS("Extensions.UpdateCheckUrlLength",
         fetch_data->full_url().possibly_invalid_spec().length());
+
+    if (VLOG_IS_ON(2)) {
+      const std::set<std::string>& id_set = fetch_data->extension_ids();
+      std::vector<std::string> id_vector(id_set.begin(), id_set.end());
+      std::string id_list = JoinString(id_vector, ',');
+      VLOG(2) << "Fetching " << fetch_data->full_url() << " for "
+              << id_list;
+    }
 
     current_manifest_fetch_.swap(scoped_fetch_data);
     manifest_fetcher_.reset(content::URLFetcher::Create(
@@ -1212,6 +1247,8 @@ void ExtensionUpdater::FetchUpdatedExtension(const std::string& id,
       extension_fetcher_->SaveResponseToTemporaryFile(
           BrowserThread::GetMessageLoopProxyForThread(BrowserThread::FILE));
     }
+
+    VLOG(2) << "Starting fetch of " << url << " for " << id;
 
     extension_fetcher_->Start();
     current_extension_fetch_ = ExtensionFetch(id, url, hash, version);
