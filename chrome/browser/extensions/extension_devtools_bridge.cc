44f9297412d43fb9379344e8a8df6de63aa79178
knorton@google.com <knorton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 22:20:02 +0000

    Automatically turn on Page events in the soon-to-go-away devtools API.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8965002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114708 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_devtools_bridge.cc
+++ chrome/browser/extensions/extension_devtools_bridge.cc
@@ -81,6 +81,11 @@ bool ExtensionDevToolsBridge::RegisterAsDevToolsClientHost() {
         this,
         FormatDevToolsMessage(3, "Network.enable"));
 
+    // 3. Enable page events.
+    devtools_manager->DispatchOnInspectorBackend(
+        this,
+        FormatDevToolsMessage(4, "Page.enable"));
+
     return true;
   }
   return false;
