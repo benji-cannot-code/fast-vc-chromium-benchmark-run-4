a5a613ac072c7bc8fa9f64128d303dfdb3f55b4b
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 03:18:13 +0000

    Fix timegm behavior for android.
    time_t on android is 32 bit.
    We should detect overflow and return -1.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8976004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114762 0039d316-1c4b-4281-b951-d872f2087c98

--- base/os_compat_android.cc
+++ base/os_compat_android.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/os_compat_android.h"
 
+#include <time64.h>
+
 #include "base/stringprintf.h"
 
 // There is no futimes() avaiable in Bionic, so we provide our own
@@ -14,6 +16,18 @@ extern "C" {
 int futimes(int fd, const struct timeval tv[2]) {
   const std::string fd_path = StringPrintf("/proc/self/fd/%d", fd);
   return utimes(fd_path.c_str(), tv);
+}
+
+// Android has only timegm64() and no timegm().
+// We replicate the behaviour of timegm() when the result overflows time_t.
+time_t timegm(struct tm* const t) {
+  // time_t is signed on Android.
+  static const time_t kTimeMax = ~(1 << (sizeof(time_t) * CHAR_BIT - 1));
+  static const time_t kTimeMin = (1 << (sizeof(time_t) * CHAR_BIT - 1));
+  time64_t result = timegm64(t);
+  if (result < kTimeMin || result > kTimeMax)
+    return -1;
+  return result;
 }
 
 }  // extern "C"
