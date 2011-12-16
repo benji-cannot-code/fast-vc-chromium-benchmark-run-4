772eb3043b59af1355b914ceb66784396580f780
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 07:28:21 +0000

    WebSocket Pepper API: C++ bindings implementation.
    This change contains simple straightforward C++ interfaces.
    JS like bindings will be implemented as a helper class in another change.
    
    BUG=87310
    TEST=ui_tests --gtest_filter='PPAPITest.WebSocket_CcInterfaces'
    
    
    Review URL: http://codereview.chromium.org/8821010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114779 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/ppapi_sources.gypi
+++ ppapi/ppapi_sources.gypi
@@ -212,6 +212,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'cpp/dev/video_decoder_client_dev.h',
       'cpp/dev/video_decoder_dev.cc',
       'cpp/dev/video_decoder_dev.h',
+      'cpp/dev/websocket_dev.cc',
+      'cpp/dev/websocket_dev.h',
       'cpp/dev/widget_client_dev.cc',
       'cpp/dev/widget_client_dev.h',
       'cpp/dev/widget_dev.cc',
--- ppapi/tests/test_websocket.cc
+++ ppapi/tests/test_websocket.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/tests/test_websocket.h"
 
 #include <string.h>
+#include <vector>
 
 #include "ppapi/c/dev/ppb_testing_dev.h"
 #include "ppapi/c/dev/ppb_websocket_dev.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_completion_callback.h"
 #include "ppapi/c/ppb_core.h"
 #include "ppapi/c/ppb_var.h"
+#include "ppapi/cpp/dev/websocket_dev.h"
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/module.h"
 #include "ppapi/tests/test_utils.h"
@@ -21,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const char kEchoServerURL[] =
     "ws://localhost:8880/websocket/tests/hybi/echo";
+
+const char kCloseServerURL[] =
+    "ws://localhost:8880/websocket/tests/hybi/close";
 
 const char kProtocolTestServerURL[] =
     "ws://localhost:8880/websocket/tests/hybi/protocol-test?protocol=";
@@ -63,6 +68,8 @@ void TestWebSocket::RunTests(const std::string& filter) {
   RUN_TEST_WITH_REFERENCE_CHECK(ValidClose, filter);
   RUN_TEST_WITH_REFERENCE_CHECK(GetProtocol, filter);
   RUN_TEST_WITH_REFERENCE_CHECK(TextSendReceive, filter);
+
+  RUN_TEST_WITH_REFERENCE_CHECK(CcInterfaces, filter);
 }
 
 PP_Var TestWebSocket::CreateVar(const char* string) {
@@ -415,3 +422,58 @@ std::string TestWebSocket::TestTextSendReceive() {
 // TODO(toyoshim): Add tests for didReceiveMessageError().
 
 // TODO(toyoshim): Add other function tests.
+
+std::string TestWebSocket::TestCcInterfaces() {
+  // C++ bindings is simple straightforward, then just verifies interfaces work
+  // as a interface bridge fine.
+  pp::WebSocket_Dev ws(instance_);
+
+  // Check uninitialized properties access.
+  ASSERT_EQ(0, ws.GetBufferedAmount());
+  ASSERT_EQ(0, ws.GetCloseCode());
+  ASSERT_TRUE(AreEqual(ws.GetCloseReason().pp_var(), ""));
+  ASSERT_EQ(false, ws.GetCloseWasClean());
+  ASSERT_TRUE(AreEqual(ws.GetExtensions().pp_var(), ""));
+  ASSERT_TRUE(AreEqual(ws.GetProtocol().pp_var(), ""));
+  ASSERT_EQ(PP_WEBSOCKETREADYSTATE_INVALID_DEV, ws.GetReadyState());
+  ASSERT_TRUE(AreEqual(ws.GetURL().pp_var(), ""));
+
+  // Check communication interfaces (connect, send, receive, and close).
+  TestCompletionCallback connect_callback(instance_->pp_instance());
+  int32_t result = ws.Connect(pp::Var(std::string(kCloseServerURL)), NULL, 0U,
+      connect_callback);
+  ASSERT_EQ(PP_OK_COMPLETIONPENDING, result);
+  result = connect_callback.WaitForResult();
+  ASSERT_EQ(PP_OK, result);
+
+  std::string message("hello C++");
+  result = ws.SendMessage(pp::Var(message));
+  ASSERT_EQ(PP_OK, result);
+
+  pp::Var receive_var;
+  TestCompletionCallback receive_callback(instance_->pp_instance());
+  result = ws.ReceiveMessage(&receive_var, receive_callback);
+  if (result == PP_OK_COMPLETIONPENDING)
+    result = receive_callback.WaitForResult();
+  ASSERT_EQ(PP_OK, result);
+  ASSERT_TRUE(AreEqual(receive_var.pp_var(), message.c_str()));
+
+  TestCompletionCallback close_callback(instance_->pp_instance());
+  std::string reason("bye");
+  result = ws.Close(kCloseCodeNormalClosure, pp::Var(reason), close_callback);
+  ASSERT_EQ(PP_OK_COMPLETIONPENDING, result);
+  result = close_callback.WaitForResult();
+  ASSERT_EQ(PP_OK, result);
+
+  // Check initialized properties access.
+  ASSERT_EQ(0, ws.GetBufferedAmount());
+  ASSERT_EQ(kCloseCodeNormalClosure, ws.GetCloseCode());
+  ASSERT_TRUE(AreEqual(ws.GetCloseReason().pp_var(), reason.c_str()));
+  ASSERT_EQ(true, ws.GetCloseWasClean());
+  ASSERT_TRUE(AreEqual(ws.GetExtensions().pp_var(), ""));
+  ASSERT_TRUE(AreEqual(ws.GetProtocol().pp_var(), ""));
+  ASSERT_EQ(PP_WEBSOCKETREADYSTATE_CLOSED_DEV, ws.GetReadyState());
+  ASSERT_TRUE(AreEqual(ws.GetURL().pp_var(), kCloseServerURL));
+
+  PASS();
+}
