ff899979ec167a9eb047f453ada229a61d0de62d
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-16 23:43:40 +0000

    Windows test fix
    Source/WebCore: 
    
    No new tests.
    
    Unreviewed test fix. All Windows tests were crashing when objects who were pointing to 
    static data members across DLL boundaries were getting garbage in their pointers.
    
    * WebCore.exp.in:
    * bindings/js/JSDOMWrapper.cpp:
    * bindings/js/JSDOMWrapper.h:
    
    Source/WebKit2: 
    
    Unreviewed test fix. All Windows tests were crashing when objects who were pointing to 
    static data members across DLL boundaries were getting garbage in their pointers.
    
    * win/WebKit2.def:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMWrapper.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMWrapper.cpp
@@ -35,6 +35,4 @@ namespace WebCore {
 
 ASSERT_HAS_TRIVIAL_DESTRUCTOR(JSDOMWrapper);
 
-const ClassInfo JSDOMWrapper::s_info = { "JSDOMWrapper", &Base::s_info, 0, 0, CREATE_METHOD_TABLE(JSDOMWrapper) };
-
 } // namespace WebCore
