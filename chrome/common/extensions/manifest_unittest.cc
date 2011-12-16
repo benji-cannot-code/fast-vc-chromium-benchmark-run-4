a201dd29b34813788921f88e6ca5b2894c7257a9
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 02:31:13 +0000

    Update the manifest key restrictions.
    Do not let platform apps use the following features:
      - background pages
      - tts engines
      - npapi plugins
      - input components
      - file browser handlers
    
    BUG=none
    TEST=ManifestTest.PlatformApp
    
    Review URL: http://codereview.chromium.org/8914013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/manifest_unittest.cc
+++ chrome/common/extensions/manifest_unittest.cc
@@ -96,6 +96,10 @@ const char* kNotPlatformAppKeys[] = {
   keys::kDevToolsPage,
   keys::kSidebar,
   keys::kHomepageURL,
+  keys::kPlugins,
+  keys::kInputComponents,
+  keys::kTtsEngine,
+  keys::kFileBrowserHandlers
 };
 
 // Returns all the manifest keys not including those in |filtered| or kTypeKeys.
