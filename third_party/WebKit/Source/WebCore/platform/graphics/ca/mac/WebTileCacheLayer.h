72710d52f957ed48ec5ec8050b24e321cdc65141
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-16 23:05:23 +0000

    Add a pretty dumb tile cache to WebTileCacheLayer https://bugs.webkit.org/show_bug.cgi?id=74753
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::requiresTiledLayer):
    If a layer is a tile cache layer, we never want to swap it out for a tiled layer.
    
    (WebCore::GraphicsLayerCA::swapFromOrToTiledLayer):
    Assert that we don't have a tile cache layer.
    
    * platform/graphics/ca/mac/PlatformCALayerMac.mm:
    (PlatformCALayer::PlatformCALayer):
    If we have a tile cache layer, add its tile container to the list of custom sublayers.
    
    * platform/graphics/ca/mac/TileCache.h: Added.
    (WebCore::TileCache::tileContainerLayer):
    Return the tile container layer.
    
    * platform/graphics/ca/mac/TileCache.mm: Added.
    (WebCore::TileCache::tileCacheLayerBoundsChanged):
    Resize the tile grid if necessary.
    
    (WebCore::TileCache::setNeedsDisplayInRect):
    Invalidate the necessary tiles.
    
    (WebCore::TileCache::drawLayer):
    Set up the transform and draw the layer.
    
    (WebCore::TileCache::getTileRangeForRect):
    Given a rect, return the range of tiles that it covers.
    
    (WebCore::TileCache::numTilesForGridSize):
    Given a size, return how many tiles are needed to completely cover it.
    
    (WebCore::TileCache::resizeTileGrid):
    Create new tile layers if needed, or reuse already existing ones.
    
    (WebCore::TileCache::tileLayerAtPosition):
    Given a position in the grid, return the tile layer.
    
    (WebCore::TileCache::createTileLayer):
    Create a WebTileLayer and set it up.
    
    * platform/graphics/ca/mac/WebTileCacheLayer.h:
    * platform/graphics/ca/mac/WebTileCacheLayer.mm:
    (-[WebTileCacheLayer setBounds:]):
    (-[WebTileCacheLayer setNeedsDisplayInRect:]):
    (-[WebTileCacheLayer tileContainerLayer]):
    Call down to the tile cache object.
    
    * platform/graphics/ca/mac/WebTileLayer.h: Added.
    * platform/graphics/ca/mac/WebTileLayer.mm: Added.
    
    (-[WebTileLayer drawInContext:]):
    Ask the tile cache to draw the given layer.
    
    (-[WebTileLayer setTileCache:WebCore::]):
    
    Source/WebKit2:
    
    * WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    (WebKit::TiledCoreAnimationDrawingArea::flushLayers):
    Always do a layout here, to prevent an ASSERT(!needsLayout()) when painting.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103114 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.h
@@ -24,8 +24,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#import "WebTiledLayer.h"
+#import <QuartzCore/QuartzCore.h>
+#import <wtf/OwnPtr.h>
 
-@interface WebTileCacheLayer : WebTiledLayer
+namespace WebCore {
+    class TileCache;
+}
 
+@interface WebTileCacheLayer : CALayer {
+    OwnPtr<WebCore::TileCache> _tileCache;
+}
+
+- (CALayer *)tileContainerLayer;
 @end
--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.mm
@@ -25,10 +25,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #import "config.h"
-
 #import "WebTileCacheLayer.h"
+
+#import "IntRect.h"
+#import "TileCache.h"
+
+using namespace WebCore;
 
 @implementation WebTileCacheLayer
 
+- (id)init
+{
+    self = [super init];
+    if (!self)
+        return nil;
+
+    // FIXME: The tile size should be configurable.
+    _tileCache = TileCache::create(self, IntSize(512, 512));
+
+    return self;
+}
+
+- (void)setBounds:(CGRect)bounds
+{
+    [super setBounds:bounds];
+
+    _tileCache->tileCacheLayerBoundsChanged();
+}
+
+- (void)setNeedsDisplayInRect:(CGRect)rect
+{
+    _tileCache->setNeedsDisplayInRect(enclosingIntRect(rect));
+}
+
+- (CALayer *)tileContainerLayer
+{
+    return _tileCache->tileContainerLayer();
+}
 
 @end
