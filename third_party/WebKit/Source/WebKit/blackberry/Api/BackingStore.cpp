d44fb39084e2947eb24a6963d225517f6bde2c9e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-16 00:41:00 +0000

    [BlackBerry] Regression: lots of checkerboarding https://bugs.webkit.org/show_bug.cgi?id=74611
    Patch by Jacky Jiang <zhajiang@rim.com> on 2011-12-15
    Reviewed by Daniel Bates.
    
    Fix a regression caused by r102843 which introduced a lot of unnecessary
    checkerboarding. R102843 changed behavior by removing a check for blit
    based on whether the render actually took place and whether the
    backingstore was in the midst of suspended updates.
    The initial anthor is Adam Treat <atreat@rim.com>.
    
    * blackberry/Api/BackingStore.cpp:
    (BlackBerry::WebKit::BackingStorePrivate::repaint):
    (BlackBerry::WebKit::BackingStorePrivate::slowScroll):
    (BlackBerry::WebKit::BackingStorePrivate::renderVisibleContents):
    (BlackBerry::WebKit::BackingStorePrivate::renderBackingStore):
    * blackberry/Api/BackingStore_p.h:
    (BlackBerry::WebKit::BackingStorePrivate::isSuspended):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102998 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -334,8 +334,8 @@ void BackingStorePrivate::repaint(const Platform::IntRect& windowRect,
 #endif
 
         if (immediate) {
-            render(rect);
-            blitVisibleContents();
+            if (render(rect))
+                blitVisibleContents();
         } else
             m_renderQueue->addToQueue(RenderQueue::RegularRender, rect);
     }
@@ -355,8 +355,8 @@ void BackingStorePrivate::slowScroll(const Platform::IntSize& delta, const Platf
     Platform::IntRect rect = m_webPage->d->mapToTransformed(m_client->mapFromViewportToContents(windowRect));
 
     if (immediate) {
-        render(rect);
-        blitVisibleContents();
+        if (render(rect) && !isSuspended())
+            blitVisibleContents();
     } else {
         m_renderQueue->addToQueue(RenderQueue::VisibleScroll, rect);
         // We only blit here if the client did not generate the scroll as the client
@@ -1088,16 +1088,19 @@ void BackingStorePrivate::requestLayoutIfNeeded() const
     m_webPage->d->requestLayoutIfNeeded();
 }
 
-void BackingStorePrivate::renderVisibleContents()
+bool BackingStorePrivate::renderVisibleContents()
 {
     Platform::IntRect renderRect = shouldDirectRenderingToWindow() ? visibleContentsRect() : visibleTilesRect();
-    render(renderRect);
-    m_renderQueue->clear(renderRect, true /*clearRegularRenderJobs*/);
+    if (render(renderRect)) {
+        m_renderQueue->clear(renderRect, true /*clearRegularRenderJobs*/);
+        return true;
+    }
+    return false;
 }
 
-void BackingStorePrivate::renderBackingStore()
+bool BackingStorePrivate::renderBackingStore()
 {
-    render(frontState()->backingStoreRect());
+    return render(frontState()->backingStoreRect());
 }
 
 void BackingStorePrivate::blitVisibleContents(bool force)
--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore_p.h
@@ -111,6 +111,8 @@ public:
     // the minimum number of tiles required to scroll.
     bool shouldDirectRenderingToWindow() const;
 
+    bool isSuspended() const { return m_suspendBackingStoreUpdates; }
+
     // Suspends all screen updates so that 'blitContents' is disabled.
     void suspendScreenAndBackingStoreUpdates();
 
@@ -181,8 +183,8 @@ public:
     void requestLayoutIfNeeded() const;
 
     // Helper render methods.
-    void renderVisibleContents();
-    void renderBackingStore();
+    bool renderVisibleContents();
+    bool renderBackingStore();
     void blitVisibleContents(bool force = false);
 
     // Assumes the rect to be in window/viewport coordinates.
