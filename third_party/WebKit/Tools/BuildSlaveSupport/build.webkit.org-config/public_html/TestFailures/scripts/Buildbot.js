bb323b4dbeb8f7d781cafce16eec69a55701e83a
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-16 00:18:35 +0000

    TestFailures: store the name of the buildbot results directory in a property https://bugs.webkit.org/show_bug.cgi?id=74663
    Reviewed by Adam Barth.
    
    Store the name of the results directory as a property on Buildbot's
    prototype. This allows subclasses of Buildbot to customize the results
    directory.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/Buildbot.js:
    (Buildbot.prototype.resultsDirectoryURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@102994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/Buildbot.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/Buildbot.js
@@ -31,6 +31,7 @@ function Buildbot(baseURL) {
 
 Buildbot.prototype = {
     _builders: {},
+    _resultsDirectory: 'results/',
 
     buildURL: function(builderName, buildName) {
         return this.baseURL + 'builders/' + builderName + '/builds/' + buildName;
@@ -50,7 +51,7 @@ Buildbot.prototype = {
         }
 
         var self = this;
-        getResource(this.baseURL + 'results/', function(xhr) {
+        getResource(this.baseURL + this._resultsDirectory, function(xhr) {
             var root = document.createElement('html');
             root.innerHTML = xhr.responseText;
             var names = Array.prototype.map.call(root.querySelectorAll('td:first-child > a > b'), function(elem) {
@@ -70,7 +71,7 @@ Buildbot.prototype = {
     },
 
     resultsDirectoryURL: function(builderName, buildName) {
-        return this.baseURL + 'results/' + encodeURIComponent(builderName) + '/' + encodeURIComponent(buildName) + '/';
+        return this.baseURL + this._resultsDirectory + encodeURIComponent(builderName) + '/' + encodeURIComponent(buildName) + '/';
     },
 
     _buildersForNames: function(names) {
