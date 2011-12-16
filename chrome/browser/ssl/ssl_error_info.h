5686046d1ccea91ce05fa71b0b07848711a70ec4
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 23:27:35 +0000

    UI strings for the SSL roadblock/error pages for the CERT_WEAK_KEY case.
    BUG=107450
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/8965026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114879 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ssl/ssl_error_info.h
+++ chrome/browser/ssl/ssl_error_info.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,6 +33,7 @@ class SSLErrorInfo {
     CERT_INVALID,
     CERT_WEAK_SIGNATURE_ALGORITHM,
     CERT_NOT_IN_DNS,
+    CERT_WEAK_KEY,
     UNKNOWN
   };
 
