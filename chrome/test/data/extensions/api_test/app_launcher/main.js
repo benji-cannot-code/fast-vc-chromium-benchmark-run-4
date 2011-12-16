90c81fab21c2913ff96a0645df34b58de6de0996
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 01:14:18 +0000

    Make hosted apps launched via chrome.tabs.create with an URL redirect have the right process type.
    (This affected apps like Google Docs when launched from the bookmark manager)
    
    The check for UseWebUIForURL in RenderViewHostManager::GetSiteInstanceForEntry
    was not enough, since it only returns true for URLs that need extension
    bindings, which is not true for hosted apps.
    
    This only affected chrome.tabs.create, and not regular <a target="_blank"> links
    since those are first created in the opening tab's process, thus the current
    site instance is the launching extension's, so we would consider them a
    WebUI -> non-WebUI transition, which needs a process swap.
    
    BUG=99349
    R=creis@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8773050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114733 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/app_launcher/main.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var appUrl = 'http://localhost:' + config.testServer.port +
+        '/files/extensions/api_test/app_process/path1/empty.html';
+  var redirectUrl = 'http://localhost:' + config.testServer.port +
+      '/server-redirect?' + appUrl;
+  chrome.tabs.create({
+    url: redirectUrl
+  });
+});
--- content/browser/tab_contents/render_view_host_manager_unittest.cc
+++ content/browser/tab_contents/render_view_host_manager_unittest.cc
@@ -58,6 +58,11 @@ class RenderViewHostManagerTestWebUIFactory
     return HasWebUIScheme(url);
   }
 
+  virtual bool UseWebUIBindingsForURL(content::BrowserContext* browser_context,
+                                      const GURL& url) const OVERRIDE {
+    return HasWebUIScheme(url);
+  }
+
   virtual bool HasWebUIScheme(const GURL& url) const OVERRIDE {
     return url.SchemeIs(chrome::kChromeUIScheme) &&
         url.spec() != kChromeUISchemeButNotWebUIURL;
