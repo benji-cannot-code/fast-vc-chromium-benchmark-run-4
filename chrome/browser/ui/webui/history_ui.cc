afd30c68b4a1fcb20bf487fd5d615f54ea42664c
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 02:04:31 +0000

    Make history refresh after clearing all browsing data.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8970014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/history_ui.cc
+++ chrome/browser/ui/webui/history_ui.cc
@@ -410,7 +410,8 @@ void BrowsingHistoryHandler::Observe(
   }
   history::URLsDeletedDetails* deletedDetails =
       content::Details<history::URLsDeletedDetails>(details).ptr();
-  if (deletedDetails->urls != urls_to_be_deleted_) {
+  if (deletedDetails->urls != urls_to_be_deleted_ ||
+      deletedDetails->all_history) {
     // Notify the page that someone else deleted from the history.
     web_ui_->CallJavascriptFunction("historyDeleted");
   }
