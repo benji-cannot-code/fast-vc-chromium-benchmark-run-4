d25ad59b82f9105016066f7681e0d15fb3298f45
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 03:22:48 +0000

    ChromeOS Clang build break fix.
    BUG=none
    TEST=none
    TBR=satorux
    Review URL: http://codereview.chromium.org/8966017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114764 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata.cc
+++ chrome/browser/chromeos/gdata/gdata.cc
@@ -298,7 +298,7 @@ DeleteDocumentOperation::DeleteDocumentOperation(Profile* profile,
 }
 
 content::URLFetcher::RequestType
-DeleteDocumentOperation::GetRequestType() const OVERRIDE {
+DeleteDocumentOperation::GetRequestType() const {
   return content::URLFetcher::DELETE_REQUEST;
 }
 
