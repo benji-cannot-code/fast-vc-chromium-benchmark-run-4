7aa9ab0eb1f0671d5fb66eccaa99d4184f523226
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 01:42:10 +0000

    Build fix for 114736. Add missing exports.
    BUG=None
    TEST=Build shared.
    TBR=kbr
    
    Review URL: http://codereview.chromium.org/8972008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114745 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/inter_process_time_ticks_converter.h
+++ content/common/inter_process_time_ticks_converter.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_COMMON_INTER_PROCESS_TIME_TICKS_CONVERTER_H_
 
 #include "base/time.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
@@ -46,7 +47,7 @@ class RemoteTimeTicks;
 //
 // 2. The remote's range is shifted so that it is centered within the
 //    local's range. Any values converted will be shifted the same amount.
-class InterProcessTimeTicksConverter {
+class CONTENT_EXPORT InterProcessTimeTicksConverter {
  public:
   InterProcessTimeTicksConverter(const LocalTimeTicks& local_lower_bound,
                                  const LocalTimeTicks& local_upper_bound,
@@ -72,7 +73,7 @@ class InterProcessTimeTicksConverter {
   int64 remote_upper_bound_;
 };
 
-class LocalTimeDelta {
+class CONTENT_EXPORT LocalTimeDelta {
  public:
   int ToInt32() const { return value_; }
 
@@ -85,7 +86,7 @@ class LocalTimeDelta {
   int value_;
 };
 
-class LocalTimeTicks {
+class CONTENT_EXPORT LocalTimeTicks {
  public:
   static LocalTimeTicks FromTimeTicks(const base::TimeTicks& value) {
     return LocalTimeTicks(value.ToInternalValue());
@@ -107,7 +108,7 @@ class LocalTimeTicks {
   int64 value_;
 };
 
-class RemoteTimeDelta {
+class CONTENT_EXPORT RemoteTimeDelta {
  public:
   static RemoteTimeDelta FromRawDelta(int delta) {
     return RemoteTimeDelta(delta);
@@ -122,7 +123,7 @@ class RemoteTimeDelta {
   int value_;
 };
 
-class RemoteTimeTicks {
+class CONTENT_EXPORT RemoteTimeTicks {
  public:
   static RemoteTimeTicks FromTimeTicks(const base::TimeTicks& ticks) {
     return RemoteTimeTicks(ticks.ToInternalValue());
