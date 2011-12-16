762b6bd56334b7ba725e63a420f16fc64bc099c8
petermayo@chromium.org <petermayo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 20:18:22 +0000

    Add aura compile testing by default to likely areas (3).
    BUG=chromium:107599
    TEST=None
    NOTRY=true
    
    Review URL: http://codereview.chromium.org/8914018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/PRESUBMIT.py
+++ chrome/browser/ui/webui/PRESUBMIT.py
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Chromium presubmit script for src/chrome/browser/view.
+"""Chromium presubmit script for src/chrome/browser/externsions.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
 
 def GetPreferredTrySlaves():
-  return ['linux_chromeos_aura:compile', 'linux_chromeos']
+  return ['linux_chromeos_aura:compile']
