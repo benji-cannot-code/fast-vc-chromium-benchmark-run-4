2df97357e8af99d30511911684c055b1d0c4bdf3
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 17:02:06 +0000

    Badge the original profile's desktop shortcut and ensure it is unbadged if it is the last profile left.
    We do this by fetching the default profile and avatar when the shortcut manager needs to make changes to the default shortcut.
    
    We also correct a few places in the original ProfileShortcutManagerWin code to ensure it uses the last remaining profile's details rather than default details.
    
    TEST=On a fresh install of Chrome, create a new profile. Ensure that both profiles have the correct badges.
    BUG=106513,107435
    
    Review URL: http://codereview.chromium.org/8931015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/background/background_mode_manager.cc
+++ chrome/browser/background/background_mode_manager.cc
@@ -406,7 +406,8 @@ void BackgroundModeManager::OnProfileAdded(const string16& profile_name,
   }
 }
 
-void BackgroundModeManager::OnProfileRemoved(const string16& profile_name) {
+void BackgroundModeManager::OnProfileWillBeRemoved(
+    const string16& profile_name) {
   // Remove the profile from our map of profiles.
   BackgroundModeInfoMap::iterator it =
       GetBackgroundModeIterator(profile_name);
@@ -415,6 +416,9 @@ void BackgroundModeManager::OnProfileRemoved(const string16& profile_name) {
     background_mode_data_.erase(it);
     UpdateStatusTrayIconContextMenu();
   }
+}
+
+void BackgroundModeManager::OnProfileWasRemoved(const string16& profile_name) {
 }
 
 void BackgroundModeManager::OnProfileNameChanged(
