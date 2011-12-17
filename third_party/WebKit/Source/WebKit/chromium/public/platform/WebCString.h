1212cdaec2f8654e6b2ab1b1b8cf93d325442e1a
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-17 00:01:15 +0000

    [chromium] Remove WebCString's dependency on WebCore https://bugs.webkit.org/show_bug.cgi?id=74761
    Reviewed by Darin Fisher.
    
    Remove WebCString::fromUTF16(), which are never called, and implement WebCString::utf16() using WTF instead of
    WebCore/platform/text/TextEncoding.
    
    * public/platform/WebCString.h:
    * src/WebCString.cpp:
    (WebKit::WebCString::utf16):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/platform/WebCString.h
+++ third_party/WebKit/Source/WebKit/chromium/public/platform/WebCString.h
@@ -90,9 +90,6 @@ public:
 
     WEBKIT_EXPORT WebString utf16() const;
 
-    WEBKIT_EXPORT static WebCString fromUTF16(const WebUChar* data, size_t length);
-    WEBKIT_EXPORT static WebCString fromUTF16(const WebUChar* data);
-
 #if WEBKIT_IMPLEMENTATION
     WebCString(const WTF::CString&);
     WebCString& operator=(const WTF::CString&);
@@ -113,12 +110,6 @@ public:
     {
         size_t len = length();
         return len ? std::string(data(), len) : std::string();
-    }
-
-    template <class UTF16String>
-    static WebCString fromUTF16(const UTF16String& s)
-    {
-        return fromUTF16(s.data(), s.length());
     }
 #endif
 
--- third_party/WebKit/Source/WebKit/chromium/src/WebCString.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebCString.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/WebCString.h"
 
-#include "TextEncoding.h"
 #include "platform/WebString.h"
 #include <wtf/text/CString.h>
 
@@ -92,21 +91,7 @@ const char* WebCString::data() const
 
 WebString WebCString::utf16() const
 {
-    return WebCore::UTF8Encoding().decode(data(), length());
-}
-
-WebCString WebCString::fromUTF16(const WebUChar* data, size_t length)
-{
-    return WebCore::UTF8Encoding().encode(
-        data, length, WebCore::QuestionMarksForUnencodables);
-}
-
-WebCString WebCString::fromUTF16(const WebUChar* data)
-{
-    size_t len = 0;
-    while (data[len] != WebUChar(0))
-        len++;
-    return fromUTF16(data, len);
+    return WebString::fromUTF8(data(), length());
 }
 
 WebCString::WebCString(const WTF::CString& s)
