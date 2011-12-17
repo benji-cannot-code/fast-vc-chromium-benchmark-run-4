db74461eeded8b82403e6a6651d03c70ef3d38ef
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 03:37:55 +0000

    Fix Chrome Frame crash when automation server dies.
    GCF was crashing if the automation server dies while there are outstanding sub-resource requests being serviced on the new bg thread.  The reason was that AutomationServerDied posts a task to InitializeComplete(AUTOMATION_SERVER_CRASHED) and then a task to Uninitialize().  The former will call ReleaseAutomationServer() without first joining on the bg resource thread.  The fix is to wait and release the server in Uninitialize.
    
    BUG=107811
    TEST=Prior to fix, the following would usually trigger the crash: chrome_frame_tests.exe --gtest_repeat=20 --gtest_filter=CF/FullTabUITest.TabCrashReload/0
    
    
    Review URL: http://codereview.chromium.org/8965031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114906 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_frame_automation.cc
+++ chrome_frame/chrome_frame_automation.cc
@@ -624,7 +624,7 @@ void ChromeFrameAutomationClient::Uninitialize() {
     tab_ = NULL;    // scoped_refptr::Release
   }
 
-  // Wait for the background thread to exit.
+  // Wait for the automation proxy's worker thread to exit.
   ReleaseAutomationServer();
 
   // We must destroy the window, since if there are pending tasks
@@ -847,7 +847,7 @@ AutomationLaunchResult ChromeFrameAutomationClient::CreateExternalTabComplete(
   return launch_result;
 }
 
-// Invoked in launch background thread.
+// Invoked in the automation proxy's worker thread.
 void ChromeFrameAutomationClient::LaunchComplete(
     ChromeFrameAutomationProxy* proxy,
     AutomationLaunchResult result) {
@@ -890,6 +890,7 @@ void ChromeFrameAutomationClient::LaunchComplete(
   }
 }
 
+// Invoked in the automation proxy's worker thread.
 void ChromeFrameAutomationClient::AutomationServerDied() {
   // Make sure we notify our delegate.
   PostTask(
@@ -906,7 +907,6 @@ void ChromeFrameAutomationClient::InitializeComplete(
   DCHECK_EQ(base::PlatformThread::CurrentId(), ui_thread_id_);
   if (result != AUTOMATION_SUCCESS) {
     DLOG(WARNING) << "InitializeComplete: failure " << result;
-    ReleaseAutomationServer();
   } else {
     init_state_ = INITIALIZED;
 
