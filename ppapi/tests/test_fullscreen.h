468d788787274c3d1c90468d0b0f30792e3ad5cf
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 01:33:01 +0000

    Revert 114881 - PPB_Fullscreen test: Merge the NaCl version of the test into ppapi/tests compiled with gyp. The two tests were written at around the same time and using the same test cases, so the only piece missing was the binding and paintig of the graphics device.
    BUG=none
    TEST=ui_tests
    Review URL: http://codereview.chromium.org/8954008
    
    TBR=polina@google.com
    Review URL: http://codereview.chromium.org/8976016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114889 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_fullscreen.h
+++ ppapi/tests/test_fullscreen.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/logging.h"
 #include "ppapi/cpp/fullscreen.h"
 #include "ppapi/cpp/graphics_2d.h"
 #include "ppapi/cpp/rect.h"
@@ -19,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace pp {
 class InputEvent;
 }  // namespace pp
-
-struct ColorPremul { uint32_t A, R, G, B; };  // Use premultipled Alpha.
 
 class TestFullscreen : public TestCase {
  public:
@@ -37,11 +34,8 @@ class TestFullscreen : public TestCase {
   std::string TestNormalToFullscreenToNormal();
 
   void SimulateUserGesture();
+
   void FailFullscreenTest(const std::string& error);
-  void FailNormalTest(const std::string& error);
-  void PassFullscreenTest();
-  void PassNormalTest();
-  bool PaintPlugin(pp::Size size, ColorPremul color);
 
   bool GotError();
   std::string Error();
@@ -55,7 +49,8 @@ class TestFullscreen : public TestCase {
   bool fullscreen_pending_;
   bool normal_pending_;
   bool saw_first_fullscreen_didchangeview;
-  pp::Graphics2D graphics2d_;
+  pp::Graphics2D graphics2d_fullscreen_;
+  pp::Graphics2D graphics2d_normal_;
   TestCompletionCallback set_fullscreen_true_callback_;
   TestCompletionCallback fullscreen_callback_;
   TestCompletionCallback normal_callback_;
