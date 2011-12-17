5f2ab58b068adf37e49981680fe59a73c8259b8b
bashi@chromium.org <bashi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 10:34:52 +0000

    Use references instead of pointers for WebViewPlugin::getFormValue().
    In additin to r114338, I should have changed the argument of
    WebViewPlugin::getFormValue().
    
    TEST=compiled.
    BUG=88896
    
    
    Review URL: http://codereview.chromium.org/8947014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114920 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/webview_plugin.cc
+++ webkit/plugins/webview_plugin.cc
@@ -117,7 +117,7 @@ NPObject* WebViewPlugin::scriptableObject() {
   return NULL;
 }
 
-bool WebViewPlugin::getFormValue(WebString* value) {
+bool WebViewPlugin::getFormValue(WebString& value) {
   return false;
 }
 
