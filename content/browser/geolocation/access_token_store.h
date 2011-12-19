51cb16a80eec51c7546ecbbb7c658ff99a7166d1
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-19 16:55:12 +0000

    Revert 114992 - Switch to PostTaskAndReply for AccessTokenStore.
    This removes the only usage of the CancelableRequest class in
    content/, and is in preparation for moving the class back to
    chrome/browser.
    
    The ability to cancel requests to the AccessTokenStore was never used in production, so the interface now changes to a simpler non-cancelable one, and the browsertest GeolocationAccessTokenStoreTest.CancelRequest which tested the functionality is removed.
    
    BUG=98716
    
    Review URL: http://codereview.chromium.org/8996006
    
    TBR=joi@chromium.org
    Review URL: http://codereview.chromium.org/8992018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114997 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/access_token_store.h
+++ content/browser/geolocation/access_token_store.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/string16.h"
+#include "content/browser/cancelable_request.h"
 #include "content/common/content_export.h"
 #include "googleurl/src/gurl.h"
 
@@ -30,8 +31,10 @@ class URLRequestContextGetter;
 }
 
 // Provides storage for the access token used in the network request.
-class AccessTokenStore : public base::RefCountedThreadSafe<AccessTokenStore> {
+class AccessTokenStore : public base::RefCountedThreadSafe<AccessTokenStore>,
+                         public CancelableRequestProvider {
  public:
+
   // Map of server URLs to associated access token.
   typedef std::map<GURL, string16> AccessTokenSet;
   typedef base::Callback<void(AccessTokenSet, net::URLRequestContextGetter*)>
@@ -43,8 +46,10 @@ class AccessTokenStore : public base::RefCountedThreadSafe<AccessTokenStore> {
   // in Chrome the call to obtain this must also be performed on the UI thread
   // so it is efficient to piggyback it onto this request.
   // Takes ownership of |callback|.
-  CONTENT_EXPORT virtual void LoadAccessTokens(
-      const LoadAccessTokensCallbackType& callback) = 0;
+  // Returns a handle which can subsequently be used with CancelRequest().
+  CONTENT_EXPORT Handle LoadAccessTokens(
+      CancelableRequestConsumerBase* consumer,
+      const LoadAccessTokensCallbackType& callback);
 
   virtual void SaveAccessToken(
       const GURL& server_url, const string16& access_token) = 0;
@@ -53,6 +58,9 @@ class AccessTokenStore : public base::RefCountedThreadSafe<AccessTokenStore> {
   friend class base::RefCountedThreadSafe<AccessTokenStore>;
   CONTENT_EXPORT AccessTokenStore();
   CONTENT_EXPORT virtual ~AccessTokenStore();
+
+  virtual void DoLoadAccessTokens(
+      scoped_refptr<CancelableRequest<LoadAccessTokensCallbackType> > req) = 0;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(AccessTokenStore);
--- content/browser/geolocation/fake_access_token_store.cc
+++ content/browser/geolocation/fake_access_token_store.cc
@@ -5,28 +5,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/geolocation/fake_access_token_store.h"
 
-#include "base/logging.h"
-
 using testing::_;
 using testing::Invoke;
 
 FakeAccessTokenStore::FakeAccessTokenStore() {
-  ON_CALL(*this, LoadAccessTokens(_))
+  ON_CALL(*this, DoLoadAccessTokens(_))
       .WillByDefault(Invoke(this,
-                            &FakeAccessTokenStore::DefaultLoadAccessTokens));
+                            &FakeAccessTokenStore::DefaultDoLoadAccessTokens));
   ON_CALL(*this, SaveAccessToken(_, _))
       .WillByDefault(Invoke(this,
                             &FakeAccessTokenStore::DefaultSaveAccessToken));
 }
 
 void FakeAccessTokenStore::NotifyDelegateTokensLoaded() {
+  CHECK(request_ != NULL);
   net::URLRequestContextGetter* context_getter = NULL;
-  callback_.Run(access_token_set_, context_getter);
+  request_->ForwardResult(access_token_set_, context_getter);
+  request_ = NULL;
 }
 
-void FakeAccessTokenStore::DefaultLoadAccessTokens(
-    const LoadAccessTokensCallbackType& callback) {
-  callback_ = callback;
+void FakeAccessTokenStore::DefaultDoLoadAccessTokens(
+    scoped_refptr<CancelableRequest<LoadAccessTokensCallbackType> > request) {
+  DCHECK(request_ == NULL)
+      << "Fake token store currently only allows one request at a time";
+  request_ = request;
 }
 
 void FakeAccessTokenStore::DefaultSaveAccessToken(
