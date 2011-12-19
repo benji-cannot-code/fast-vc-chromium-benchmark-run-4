8de148dafc4344d052a955d0a7fced2e377afaa8
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-19 07:15:58 +0000

    REGRESSION(r101445): [JSC] Generated code for custom getters and setters with the [Supplemental] IDL is wrong https://bugs.webkit.org/show_bug.cgi?id=74837
    Reviewed by Darin Adler.
    
    In bug 73162, we implemented the [Supplemental] IDL, but the generated code
    for custom getters and setters was wrong in JSC. This patch fixes CodeGeneratorJS.pm
    so that the result of WebCore/bindings/scripts/test/TestInterface.idl becomes as follows:
    
    Wrong:
        JSValue jsTestInterfaceStr3(ExecState* exec, JSValue slotBase, const Identifier&)
        {
            JSTestInterface* castedThis = static_cast<JSTestInterface*>(asObject(slotBase));
            return JSTestSupplemental::str3(castedThis, exec);
        }
    
    Correct:
        JSValue jsTestInterfaceStr3(ExecState* exec, JSValue slotBase, const Identifier&)
        {
            JSTestInterface* castedThis = static_cast<JSTestInterface*>(asObject(slotBase));
            TestInterface* imp = static_cast<TestInterface*>(castedThis->impl());
            return castedThis->str3(imp, exec);
        }
    
    Tests: bindings/scripts/test/JS/TestInterface.idl
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateHeader):
    (GenerateImplementation):
    * bindings/scripts/test/JS/JSTestInterface.cpp: Updated run-bindings-tests result.
    (WebCore::jsTestInterfaceStr3):
    (WebCore::setJSTestInterfaceStr3):
    * bindings/scripts/test/JS/JSTestInterface.h: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ExceptionCode.h"
 #include "JSDOMBinding.h"
-#include "JSTestSupplemental.h"
 #include "TestInterface.h"
 #include "TestSupplemental.h"
 #include <runtime/Error.h>
@@ -202,7 +201,8 @@ JSValue jsTestInterfaceStr2(ExecState* exec, JSValue slotBase, const Identifier&
 JSValue jsTestInterfaceStr3(ExecState* exec, JSValue slotBase, const Identifier&)
 {
     JSTestInterface* castedThis = static_cast<JSTestInterface*>(asObject(slotBase));
-    return JSTestSupplemental::str3(castedThis, exec);
+    TestInterface* imp = static_cast<TestInterface*>(castedThis->impl());
+    return castedThis->str3(imp, exec);
 }
 
 #endif
@@ -233,7 +233,9 @@ void setJSTestInterfaceStr2(ExecState* exec, JSObject* thisObject, JSValue value
 #if ENABLE(Condition11) || ENABLE(Condition12)
 void setJSTestInterfaceStr3(ExecState* exec, JSObject* thisObject, JSValue value)
 {
-    JSTestSupplemental::setStr3(static_cast<JSTestInterface*>(thisObject), exec, value);
+    JSTestInterface* castedThis = static_cast<JSTestInterface*>(thisObject);
+    TestInterface* imp = static_cast<TestInterface*>(castedThis->impl());
+    castedThis->setStr3(imp, exec, value);
 }
 
 #endif
