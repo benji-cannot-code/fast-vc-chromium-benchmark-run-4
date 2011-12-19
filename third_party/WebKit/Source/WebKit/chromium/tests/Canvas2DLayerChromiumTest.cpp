fd536f8c7de640bca542b2b59d213c4d1ae50095
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-19 22:51:36 +0000

    [chromium] Fix win builder due to more NotNull ambiguity errors.
    Unreviewed gardening.
    
    * tests/Canvas2DLayerChromiumTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/Canvas2DLayerChromiumTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/Canvas2DLayerChromiumTest.cpp
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 using namespace WebKit;
-using namespace testing;
+using testing::Return;
 
 namespace WebCore {
 
