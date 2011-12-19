3dce432c3e4756b55657cd6ff2e163ac8f8e1238
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-19 01:40:15 +0000

    KURL::protocolIs() should handle 8-bit strings. <http://webkit.org/b/74827>
    Reviewed by Antti Koivisto.
    
    * platform/KURL.cpp:
    (WebCore::isLetterMatchIgnoringCase):
    
        Turned this into a template method so it can be used for both UChar and LChar.
    
    (WebCore::charactersAreProtocol):
    (WebCore::protocolIs):
    
        Handle 8/16 bit strings separately to avoid conversion.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollableArea.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollableArea.cpp
@@ -128,6 +128,7 @@ bool ScrollableArea::scroll(ScrollDirection direction, ScrollGranularity granula
 
 void ScrollableArea::scrollToOffsetWithoutAnimation(const FloatPoint& offset)
 {
+    printf("scrollToOffsetWithoutAnimation: %g, %g\n", offset.x(), offset.y());
     scrollAnimator()->scrollToOffsetWithoutAnimation(offset);
 }
 
