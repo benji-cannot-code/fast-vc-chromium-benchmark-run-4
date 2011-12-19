7805c08a0b6b71e39830b366d9ad6c60cc62dfae
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-19 16:09:43 +0000

    Switch to PostTaskAndReply for AccessTokenStore.
    This removes the only usage of the CancelableRequest class in
    content/, and is in preparation for moving the class back to
    chrome/browser.
    
    The ability to cancel requests to the AccessTokenStore was never used in production, so the interface now changes to a simpler non-cancelable one, and the browsertest GeolocationAccessTokenStoreTest.CancelRequest which tested the functionality is removed.
    
    BUG=98716
    
    Review URL: http://codereview.chromium.org/8996006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114992 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/fake_access_token_store.cc
+++ content/browser/geolocation/fake_access_token_store.cc
@@ -5,30 +5,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/geolocation/fake_access_token_store.h"
 
+#include "base/logging.h"
+
 using testing::_;
 using testing::Invoke;
 
 FakeAccessTokenStore::FakeAccessTokenStore() {
-  ON_CALL(*this, DoLoadAccessTokens(_))
+  ON_CALL(*this, LoadAccessTokens(_))
       .WillByDefault(Invoke(this,
-                            &FakeAccessTokenStore::DefaultDoLoadAccessTokens));
+                            &FakeAccessTokenStore::DefaultLoadAccessTokens));
   ON_CALL(*this, SaveAccessToken(_, _))
       .WillByDefault(Invoke(this,
                             &FakeAccessTokenStore::DefaultSaveAccessToken));
 }
 
 void FakeAccessTokenStore::NotifyDelegateTokensLoaded() {
-  CHECK(request_ != NULL);
   net::URLRequestContextGetter* context_getter = NULL;
-  request_->ForwardResult(access_token_set_, context_getter);
-  request_ = NULL;
+  callback_.Run(access_token_set_, context_getter);
 }
 
-void FakeAccessTokenStore::DefaultDoLoadAccessTokens(
-    scoped_refptr<CancelableRequest<LoadAccessTokensCallbackType> > request) {
-  DCHECK(request_ == NULL)
-      << "Fake token store currently only allows one request at a time";
-  request_ = request;
+void FakeAccessTokenStore::DefaultLoadAccessTokens(
+    const LoadAccessTokensCallbackType& callback) {
+  callback_ = callback;
 }
 
 void FakeAccessTokenStore::DefaultSaveAccessToken(
