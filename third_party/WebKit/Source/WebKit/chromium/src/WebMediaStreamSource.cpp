600b4bc04fa023fc3b8476746ffb000e7a7b2b67
tommyw@google.com <tommyw@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-19 21:01:59 +0000

    [chromium] MediaStream API: Fixing memory leak in WebMediaStreamSource https://bugs.webkit.org/show_bug.cgi?id=74714
    Reviewed by Eric Seidel.
    
    Missed that PassRefPtr does a ref if constructed with a raw pointer.
    Also removed an extra space in WebMediaStreamDescriptor.cpp.
    
    * src/WebMediaStreamDescriptor.cpp:
    (WebKit::WebMediaStreamDescriptor::sources):
    * src/WebMediaStreamSource.cpp:
    (WebKit::WebMediaStreamSource::operator=):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103258 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebMediaStreamSource.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebMediaStreamSource.cpp
@@ -50,11 +50,7 @@ WebMediaStreamSource::WebMediaStreamSource(const PassRefPtr<MediaStreamSource>& 
 
 WebMediaStreamSource& WebMediaStreamSource::operator=(WebCore::MediaStreamSource* mediaStreamSource)
 {
-    m_private.reset();
-    if (mediaStreamSource) {
-        mediaStreamSource->ref();
-        m_private = mediaStreamSource;
-    }
+    m_private = mediaStreamSource;
     return *this;
 }
 
