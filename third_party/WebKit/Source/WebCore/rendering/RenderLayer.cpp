ce0f08108b7d4e7998569c0d60ddee4118f40ded
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-19 18:11:20 +0000

    Avoid instantiating ScrollAnimators when possible. <http://webkit.org/b/74830>
    Reviewed by Beth Dakin.
    
    Have RenderLayer::scrollToOffset() check if we're scrolling to the already
    current offset. In that case, don't call down to scrollToOffsetWithoutAnimation(),
    avoiding the instantiation of a ScrollAnimator.
    
    This reduces memory consumption by 400 kB (on 32-bit) when viewing the full HTML5
    spec on <http://whatwg.org/c>, since we were creating a ScrollAnimator for every
    single RenderLayer.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::scrollToOffset):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -1413,8 +1413,10 @@ void RenderLayer::scrollToOffset(LayoutUnit x, LayoutUnit y, ScrollOffsetClampin
         x = min(max<LayoutUnit>(x, 0), maxX);
         y = min(max<LayoutUnit>(y, 0), maxY);
     }
-    
-    ScrollableArea::scrollToOffsetWithoutAnimation(LayoutPoint(x, y));
+
+    LayoutPoint newScrollOffset(x, y);
+    if (newScrollOffset != LayoutPoint(scrollXOffset(), scrollYOffset()))
+        scrollToOffsetWithoutAnimation(newScrollOffset);
 }
 
 void RenderLayer::scrollTo(int x, int y)
