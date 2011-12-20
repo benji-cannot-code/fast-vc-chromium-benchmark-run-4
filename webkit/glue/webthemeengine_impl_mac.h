a515f6d7f357a39795461065ea3ed1d685ffce6d
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 20:24:32 +0000

    Clean up the Linux WebThemeEngine impl according to the coding style rules
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8961013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115175 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webthemeengine_impl_mac.h
+++ webkit/glue/webthemeengine_impl_mac.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,11 +14,11 @@ class WebThemeEngineImpl : public WebKit::WebThemeEngine {
  public:
   // WebKit::WebThemeEngine implementation.
   virtual void paintScrollbarThumb(
-      WebKit::WebCanvas*,
-      WebKit::WebThemeEngine::State,
-      WebKit::WebThemeEngine::Size,
-      const WebKit::WebRect&,
-      const WebKit::WebThemeEngine::ScrollbarInfo&);
+      WebKit::WebCanvas* canvas,
+      WebKit::WebThemeEngine::State part,
+      WebKit::WebThemeEngine::Size state,
+      const WebKit::WebRect& rect,
+      const WebKit::WebThemeEngine::ScrollbarInfo& extra_params);
 };
 
 }  // namespace webkit_glue
