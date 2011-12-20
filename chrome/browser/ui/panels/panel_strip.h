5ea238244e9e5abf246a6b08e8a9ef970351818d
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 01:24:56 +0000

    Fix bug creating a Panel when a delayed overflow panel exists.
    Add overflow test for resizing a panel.
    
    BUG=107230
    TEST=PanelOverflowBrowserTests added/enabled
    
    Review URL: http://codereview.chromium.org/8980010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115058 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_strip.h
+++ chrome/browser/ui/panels/panel_strip.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_PANELS_PANEL_STRIP_H_
 #pragma once
 
+#include <set>
 #include <vector>
 #include "base/basictypes.h"
 #include "base/memory/weak_ptr.h"
@@ -68,9 +69,12 @@ class PanelStrip : public PanelMouseWatcherObserver {
   int GetBottomPositionForExpansionState(
       Panel::ExpansionState expansion_state) const;
 
+  // num_panels() and panels() only includes panels in the panel strip that
+  // do NOT have a temporary layout.
   int num_panels() const { return panels_.size(); }
-  bool is_dragging_panel() const;
   const Panels& panels() const { return panels_; }
+
+  bool is_dragging_panel() const;
   gfx::Rect display_area() const { return display_area_; }
 
   int GetMaxPanelWidth() const;
@@ -82,6 +86,12 @@ class PanelStrip : public PanelMouseWatcherObserver {
       AutoHidingDesktopBar::Visibility visibility);
 
   void OnFullScreenModeChanged(bool is_full_screen);
+
+#ifdef UNIT_TEST
+  int num_temporary_layout_panels() const {
+    return panels_in_temporary_layout_.size();
+  }
+#endif
 
  private:
   enum TitlebarAction {
@@ -140,6 +150,10 @@ class PanelStrip : public PanelMouseWatcherObserver {
   // Stores the panels that are pending to remove. We want to delay the removal
   // when we're in the process of the dragging.
   Panels panels_pending_to_remove_;
+
+  // Stores newly created panels that have a temporary layout until they
+  // are moved to overflow after a delay.
+  std::set<Panel*> panels_in_temporary_layout_;
 
   int minimized_panel_count_;
   bool are_titlebars_up_;
