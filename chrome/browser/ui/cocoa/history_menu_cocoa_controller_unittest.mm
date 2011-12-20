478bd3c49d60d797f9895cb223e726984ec35853
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 20:03:21 +0000

    [Mac] Show navigational history in the History menu, rather than most visited.
    XIB changes:
    * Move visited below recently closed.
    * Update menu item tags to match .h.
    
    BUG=62111
    TEST=Navigate to three sites and they all appear in the History menu under "Recently Visited". Selecting one navigates to that site.
    
    
    Review URL: http://codereview.chromium.org/9005012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115167 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/history_menu_cocoa_controller_unittest.mm
+++ chrome/browser/ui/cocoa/history_menu_cocoa_controller_unittest.mm
@@ -37,8 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class HistoryMenuCocoaControllerTest : public CocoaProfileTest {
  public:
-
-  virtual void SetUp() {
+  virtual void SetUp() OVERRIDE {
     CocoaProfileTest::SetUp();
     ASSERT_TRUE(profile());
 
@@ -52,12 +51,12 @@ class HistoryMenuCocoaControllerTest : public CocoaProfileTest {
     HistoryMenuBridge::HistoryItem* item = new HistoryMenuBridge::HistoryItem();
     item->url = GURL("http://google.com");
     item->session_id = 0;
-    bridge_->AddItemToMenu(item, menu, HistoryMenuBridge::kMostVisited, 0);
+    bridge_->AddItemToMenu(item, menu, HistoryMenuBridge::kVisited, 0);
 
     item = new HistoryMenuBridge::HistoryItem();
     item->url = GURL("http://apple.com");
     item->session_id = 1;
-    bridge_->AddItemToMenu(item, menu, HistoryMenuBridge::kMostVisited, 1);
+    bridge_->AddItemToMenu(item, menu, HistoryMenuBridge::kVisited, 1);
   }
 
   std::map<NSMenuItem*, HistoryMenuBridge::HistoryItem*>& menu_item_map() {
@@ -73,7 +72,6 @@ class HistoryMenuCocoaControllerTest : public CocoaProfileTest {
 };
 
 TEST_F(HistoryMenuCocoaControllerTest, OpenURLForItem) {
-
   scoped_nsobject<NSMenu> menu([[NSMenu alloc] initWithTitle:@"History"]);
   CreateItems(menu.get());
 
