1f0099184cd5dbb4970f4ea544a2e256b8550cc2
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-20 01:35:53 +0000

    [chromium] Set the CCLayerTreeHost pointer on LayerChromium instances eagerly https://bugs.webkit.org/show_bug.cgi?id=74477
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    This enforces that the m_layerTreeHost pointer on LayerChromium instances is always up to date, instead of
    lazily setting it in the paintContents loop. There are two invariants:
    1.) If a LayerChromium is the root layer of a CCLayerTreeHost, or is reachable via the children, mask, or
    replica pointers from the root layer of a CCLayerTreeHost, then that LayerChromium's m_layerTreeHost pointer
    refers to that CCLayerTreeHost
    2.) If a LayerChromium is not a root layer or reachable from a root layer of any CCLayerTreeHost, its
    CCLayerTreeHost pointer is nil.
    
    Covered by several new layout tests in LayerChromiumTest
    
    * platform/graphics/chromium/LayerChromium.cpp:
    (WebCore::LayerChromium::setLayerTreeHost):
    (WebCore::LayerChromium::setParent):
    (WebCore::LayerChromium::setMaskLayer):
    (WebCore::LayerChromium::setReplicaLayer):
    * platform/graphics/chromium/LayerChromium.h:
    * platform/graphics/chromium/TiledLayerChromium.cpp:
    (WebCore::TiledLayerChromium::createTile):
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::setRootLayer):
    (WebCore::CCLayerTreeHost::paintMaskAndReplicaForRenderSurface):
    (WebCore::CCLayerTreeHost::paintLayerContents):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    
    Source/WebKit/chromium:
    
    Add some new tests for LayerChromium::m_layerTreeHost behavior.
    
    * tests/CCLayerTreeHostTest.cpp:
    (::MockLayerTreeHost::create):
    (::MockLayerTreeHost::MockLayerTreeHost):
    (::CCLayerTreeHostTestShortlived1::beginTest):
    (::CCLayerTreeHostTestShortlived2::beginTest):
    (::CCLayerTreeHostTestShortlived3::beginTest):
    * tests/LayerChromiumTest.cpp:
    
    LayoutTests:
    
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.cpp
@@ -176,8 +176,10 @@ PassOwnPtr<CCLayerTreeHostImpl> CCLayerTreeHost::createLayerTreeHostImpl(CCLayer
 
 void CCLayerTreeHost::didRecreateGraphicsContext(bool success)
 {
-    if (rootLayer())
-        rootLayer()->cleanupResourcesRecursive();
+    if (m_rootLayer) {
+        m_rootLayer->setLayerTreeHost(0);
+        m_rootLayer->setLayerTreeHost(this);
+    }
     m_client->didRecreateGraphicsContext(success);
 }
 
@@ -235,6 +237,18 @@ void CCLayerTreeHost::setNeedsRedraw()
         m_proxy->setNeedsRedraw();
     else
         m_client->scheduleComposite();
+}
+
+void CCLayerTreeHost::setRootLayer(PassRefPtr<LayerChromium> rootLayer)
+{
+    if (m_rootLayer == rootLayer)
+        return;
+
+    if (m_rootLayer)
+        m_rootLayer->setLayerTreeHost(0);
+    m_rootLayer = rootLayer;
+    if (m_rootLayer)
+        m_rootLayer->setLayerTreeHost(this);
 }
 
 void CCLayerTreeHost::setViewport(const IntSize& viewportSize)
@@ -401,18 +415,15 @@ void CCLayerTreeHost::paintMaskAndReplicaForRenderSurface(LayerChromium* renderS
     // mask and replica should be painted.
 
     if (renderSurfaceLayer->maskLayer()) {
-        renderSurfaceLayer->maskLayer()->setLayerTreeHost(this);
         renderSurfaceLayer->maskLayer()->setVisibleLayerRect(IntRect(IntPoint(), renderSurfaceLayer->contentBounds()));
         paintContentsIfDirty(renderSurfaceLayer->maskLayer(), paintType);
     }
 
     LayerChromium* replicaLayer = renderSurfaceLayer->replicaLayer();
     if (replicaLayer) {
-        replicaLayer->setLayerTreeHost(this);
         paintContentsIfDirty(replicaLayer, paintType);
 
         if (replicaLayer->maskLayer()) {
-            replicaLayer->maskLayer()->setLayerTreeHost(this);
             replicaLayer->maskLayer()->setVisibleLayerRect(IntRect(IntPoint(), replicaLayer->maskLayer()->contentBounds()));
             paintContentsIfDirty(replicaLayer->maskLayer(), paintType);
         }
@@ -427,7 +438,6 @@ void CCLayerTreeHost::paintLayerContents(const LayerList& renderSurfaceLayerList
         ASSERT(renderSurface);
         ASSERT(renderSurface->drawOpacity());
 
-        renderSurfaceLayer->setLayerTreeHost(this);
         paintMaskAndReplicaForRenderSurface(renderSurfaceLayer, paintType);
 
         const LayerList& layerList = renderSurface->layerList();
@@ -439,7 +449,6 @@ void CCLayerTreeHost::paintLayerContents(const LayerList& renderSurfaceLayerList
             if (CCLayerTreeHostCommon::renderSurfaceContributesToTarget<LayerChromium>(layer, renderSurfaceLayer->id()))
                 continue;
 
-            layer->setLayerTreeHost(this);
             ASSERT(!layer->bounds().isEmpty());
 
             paintContentsIfDirty(layer, paintType);
--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
@@ -104,12 +104,15 @@ class MockLayerTreeHost : public CCLayerTreeHost {
 public:
     static PassRefPtr<MockLayerTreeHost> create(TestHooks* testHooks, CCLayerTreeHostClient* client, PassRefPtr<LayerChromium> rootLayer, const CCSettings& settings)
     {
-        RefPtr<MockLayerTreeHost> layerTreeHost = adoptRef(new MockLayerTreeHost(testHooks, client, rootLayer, settings));
+        RefPtr<MockLayerTreeHost> layerTreeHost = adoptRef(new MockLayerTreeHost(testHooks, client, settings));
+        layerTreeHost->setRootLayer(rootLayer);
+        bool success = layerTreeHost->initialize();
+        EXPECT_TRUE(success);
 
         // LayerTreeHostImpl won't draw if it has 1x1 viewport.
         layerTreeHost->setViewport(IntSize(1, 1));
 
-        return layerTreeHost;
+        return layerTreeHost.release();
     }
 
     virtual PassOwnPtr<CCLayerTreeHostImpl> createLayerTreeHostImpl(CCLayerTreeHostImplClient* client)
@@ -118,13 +121,10 @@ public:
     }
 
 private:
-    MockLayerTreeHost(TestHooks* testHooks, CCLayerTreeHostClient* client, PassRefPtr<LayerChromium> rootLayer, const CCSettings& settings)
+    MockLayerTreeHost(TestHooks* testHooks, CCLayerTreeHostClient* client, const CCSettings& settings)
         : CCLayerTreeHost(client, settings)
         , m_testHooks(testHooks)
     {
-        setRootLayer(rootLayer);
-        bool success = initialize();
-        EXPECT_TRUE(success);
     }
 
     TestHooks* m_testHooks;
@@ -454,7 +454,7 @@ public:
     virtual void beginTest()
     {
         // Kill the layerTreeHost immediately.
-        m_layerTreeHost->rootLayer()->setLayerTreeHost(0);
+        m_layerTreeHost->setRootLayer(0);
         m_layerTreeHost.clear();
 
         endTest();
@@ -487,7 +487,7 @@ public:
         postSetNeedsCommitToMainThread();
 
         // Kill the layerTreeHost immediately.
-        m_layerTreeHost->rootLayer()->setLayerTreeHost(0);
+        m_layerTreeHost->setRootLayer(0);
         m_layerTreeHost.clear();
 
         endTest();
@@ -510,7 +510,7 @@ public:
         postSetNeedsRedrawToMainThread();
 
         // Kill the layerTreeHost immediately.
-        m_layerTreeHost->rootLayer()->setLayerTreeHost(0);
+        m_layerTreeHost->setRootLayer(0);
         m_layerTreeHost.clear();
 
         endTest();
