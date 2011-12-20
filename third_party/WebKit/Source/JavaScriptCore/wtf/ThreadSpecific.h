587bae5cc09369bf90f45508456d11b1cf9c77bb
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-20 01:13:08 +0000

    Try to fix the Qt build.
    Unreviewed.
    
    * wtf/ThreadSpecific.h: #include!
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/ThreadSpecific.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/ThreadSpecific.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_ThreadSpecific_h
 
 #include <wtf/Noncopyable.h>
+#include <wtf/StdLibExtras.h>
 
 #if USE(PTHREADS)
 #include <pthread.h>
