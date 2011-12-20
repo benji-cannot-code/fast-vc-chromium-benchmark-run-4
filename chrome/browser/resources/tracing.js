7cc0d2bb68c85acfac3058d4a84b089cc086e514
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 20:13:28 +0000

    Refactor about:tracing to support linux kernel traces.
    Review URL: http://codereview.chromium.org/8968024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115171 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/tracing.js
+++ chrome/browser/resources/tracing.js
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="tracing/overlay.js">
 <include src="tracing/tracing_controller.js">
 <include src="tracing/timeline_model.js">
+<include src="tracing/linux_perf_importer.js">
+<include src="tracing/trace_event_importer.js">
 <include src="tracing/sorted_array_utils.js">
 <include src="tracing/measuring_stick.js">
 <include src="tracing/timeline.js">
