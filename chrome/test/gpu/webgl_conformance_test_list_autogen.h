4ef5f9b29f2d27fb133d65da816597af196e70fe
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 00:38:15 +0000

    Update webgl conformance tests test expectations to turn linux gpu debug bot green.
    After we install the new AMD driver, the bot began to run fine, except with two very slow tests.
    
    BUG=
    TEST=GPU Linux Tests (dbg)(1)
    TBR=kbr
    Review URL: http://codereview.chromium.org/8974031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/gpu/webgl_conformance_test_list_autogen.h
+++ chrome/test/gpu/webgl_conformance_test_list_autogen.h
@@ -10,6 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_GPU_WEBGL_CONFORMANCE_TEST_LIST_AUTOGEN_H_
 #define CHROME_TEST_GPU_WEBGL_CONFORMANCE_TEST_LIST_AUTOGEN_H_
 
+#if (defined(OS_LINUX)) && !defined(NDEBUG)
+#define MAYBE_conformance_more_conformance_quickCheckAPI_B1 DISABLED_conformance_more_conformance_quickCheckAPI_B1
+#elif !defined(MAYBE_conformance_more_conformance_quickCheckAPI_B1)
+#define MAYBE_conformance_more_conformance_quickCheckAPI_B1 conformance_more_conformance_quickCheckAPI_B1
+#endif
+#if (defined(OS_LINUX)) && !defined(NDEBUG)
+#define MAYBE_conformance_more_conformance_quickCheckAPI_G_I DISABLED_conformance_more_conformance_quickCheckAPI_G_I
+#elif !defined(MAYBE_conformance_more_conformance_quickCheckAPI_G_I)
+#define MAYBE_conformance_more_conformance_quickCheckAPI_G_I conformance_more_conformance_quickCheckAPI_G_I
+#endif
 #if defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_LINUX)
 #define MAYBE_conformance_more_functions_readPixelsBadArgs FAILS_conformance_more_functions_readPixelsBadArgs
 #elif !defined(MAYBE_conformance_more_functions_readPixelsBadArgs)
@@ -33,7 +43,7 @@ CONFORMANCE_TEST(conformance_more_conformance_methods,
   "conformance/more/conformance/methods.html");
 CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_A,
   "conformance/more/conformance/quickCheckAPI-A.html");
-CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_B1,
+CONFORMANCE_TEST(MAYBE_conformance_more_conformance_quickCheckAPI_B1,
   "conformance/more/conformance/quickCheckAPI-B1.html");
 CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_B2,
   "conformance/more/conformance/quickCheckAPI-B2.html");
@@ -45,7 +55,7 @@ CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_C,
   "conformance/more/conformance/quickCheckAPI-C.html");
 CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_D_G,
   "conformance/more/conformance/quickCheckAPI-D_G.html");
-CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_G_I,
+CONFORMANCE_TEST(MAYBE_conformance_more_conformance_quickCheckAPI_G_I,
   "conformance/more/conformance/quickCheckAPI-G_I.html");
 CONFORMANCE_TEST(conformance_more_conformance_quickCheckAPI_L_S,
   "conformance/more/conformance/quickCheckAPI-L_S.html");
