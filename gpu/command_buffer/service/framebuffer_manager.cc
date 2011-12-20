95a9141d21ec8b7c5759da8cc71627761ecaa92d
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 08:26:51 +0000

    Optimize glCheckFramebuffer checks.
    This CL basically tries to cache the "framebuffer complete"
    state of framebuffers in the command buffer.
    
    TEST=unit tests
    BUG=102230
    
    
    Review URL: http://codereview.chromium.org/9008001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115096 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/framebuffer_manager.cc
+++ gpu/command_buffer/service/framebuffer_manager.cc
@@ -159,7 +159,9 @@ class TextureAttachment
   DISALLOW_COPY_AND_ASSIGN(TextureAttachment);
 };
 
-FramebufferManager::FramebufferManager() {}
+FramebufferManager::FramebufferManager()
+    : framebuffer_state_change_count_(1) {
+}
 
 FramebufferManager::~FramebufferManager() {
   DCHECK(framebuffer_infos_.empty());
@@ -200,7 +202,8 @@ void FramebufferManager::CreateFramebufferInfo(
 
 FramebufferManager::FramebufferInfo::FramebufferInfo(GLuint service_id)
     : service_id_(service_id),
-      has_been_bound_(false) {
+      has_been_bound_(false),
+      framebuffer_complete_state_count_id_(0) {
 }
 
 FramebufferManager::FramebufferInfo::~FramebufferInfo() {}
@@ -359,6 +362,7 @@ void FramebufferManager::FramebufferInfo::AttachRenderbuffer(
   } else {
     attachments_.erase(attachment);
   }
+  framebuffer_complete_state_count_id_ = 0;
 }
 
 void FramebufferManager::FramebufferInfo::AttachTexture(
@@ -379,6 +383,7 @@ void FramebufferManager::FramebufferInfo::AttachTexture(
   } else {
     attachments_.erase(attachment);
   }
+  framebuffer_complete_state_count_id_ = 0;
 }
 
 const FramebufferManager::FramebufferInfo::Attachment*
@@ -402,6 +407,28 @@ bool FramebufferManager::GetClientId(
     }
   }
   return false;
+}
+
+void FramebufferManager::MarkAttachmentsAsCleared(
+    FramebufferManager::FramebufferInfo* framebuffer,
+    RenderbufferManager* renderbuffer_manager,
+    TextureManager* texture_manager) {
+  DCHECK(framebuffer);
+  framebuffer->MarkAttachmentsAsCleared(renderbuffer_manager, texture_manager);
+  MarkAsComplete(framebuffer);
+}
+
+void FramebufferManager::MarkAsComplete(
+    FramebufferManager::FramebufferInfo* framebuffer) {
+  DCHECK(framebuffer);
+  framebuffer->MarkAsComplete(framebuffer_state_change_count_);
+}
+
+bool FramebufferManager::IsComplete(
+    FramebufferManager::FramebufferInfo* framebuffer) {
+  DCHECK(framebuffer);
+  return framebuffer->framebuffer_complete_state_count_id() ==
+      framebuffer_state_change_count_;
 }
 
 }  // namespace gles2
--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2459,6 +2459,10 @@ bool GLES2DecoderImpl::CheckFramebufferValid(
     return true;
   }
 
+  if (framebuffer_manager()->IsComplete(framebuffer)) {
+    return true;
+  }
+
   GLenum completeness = framebuffer->IsPossiblyComplete();
   if (completeness != GL_FRAMEBUFFER_COMPLETE) {
     SetGLError(
@@ -2481,6 +2485,17 @@ bool GLES2DecoderImpl::CheckFramebufferValid(
       }
       ClearUnclearedAttachments(target, framebuffer);
     }
+  }
+
+  if (!framebuffer_manager()->IsComplete(framebuffer)) {
+    if (glCheckFramebufferStatusEXT(target) != GL_FRAMEBUFFER_COMPLETE) {
+      SetGLError(
+          GL_INVALID_FRAMEBUFFER_OPERATION,
+          (std::string(func_name) +
+          " framebuffer incomplete (check)").c_str());
+      return false;
+    }
+    framebuffer_manager()->MarkAsComplete(framebuffer);
   }
 
   // NOTE: At this point we don't know if the framebuffer is complete but
@@ -3990,7 +4005,8 @@ void GLES2DecoderImpl::ClearUnclearedAttachments(
   glDisable(GL_SCISSOR_TEST);
   glClear(clear_bits);
 
-  info->MarkAttachmentsAsCleared(renderbuffer_manager(), texture_manager());
+  framebuffer_manager()->MarkAttachmentsAsCleared(
+      info, renderbuffer_manager(), texture_manager());
 
   RestoreClearState();
 
@@ -4196,6 +4212,9 @@ void GLES2DecoderImpl::DoRenderbufferStorageMultisample(
   }
   GLenum error = PeekGLError();
   if (error == GL_NO_ERROR) {
+    // TODO(gman): If renderbuffers tracked which framebuffers they were
+    // attached to we could just mark those framebuffers as not complete.
+    framebuffer_manager()->IncFramebufferStateChangeCount();
     renderbuffer_manager()->SetInfo(
         renderbuffer, samples, internalformat, width, height);
   }
@@ -4238,6 +4257,9 @@ void GLES2DecoderImpl::DoRenderbufferStorage(
   glRenderbufferStorageEXT(target, impl_format, width, height);
   GLenum error = PeekGLError();
   if (error == GL_NO_ERROR) {
+    // TODO(gman): If tetxures tracked which framebuffers they were attached to
+    // we could just mark those framebuffers as not complete.
+    framebuffer_manager()->IncFramebufferStateChangeCount();
     renderbuffer_manager()->SetInfo(
         renderbuffer, 0, internalformat, width, height);
   }
@@ -6219,6 +6241,14 @@ error::Error GLES2DecoderImpl::DoCompressedTexImage2D(
                "glCompressedTexImage2D: texture is immutable");
     return error::kNoError;
   }
+
+  if (info->IsAttachedToFramebuffer()) {
+    state_dirty_ = true;
+    // TODO(gman): If textures tracked which framebuffers they were attached to
+    // we could just mark those framebuffers as not complete.
+    framebuffer_manager()->IncFramebufferStateChangeCount();
+  }
+
   scoped_array<int8> zero;
   if (!data) {
     zero.reset(new int8[image_size]);
@@ -6417,6 +6447,9 @@ error::Error GLES2DecoderImpl::DoTexImage2D(
 
   if (info->IsAttachedToFramebuffer()) {
     state_dirty_ = true;
+    // TODO(gman): If textures tracked which framebuffers they were attached to
+    // we could just mark those framebuffers as not complete.
+    framebuffer_manager()->IncFramebufferStateChangeCount();
   }
 
   if (!teximage2d_faster_than_texsubimage2d_ && level_is_same) {
@@ -6605,6 +6638,9 @@ void GLES2DecoderImpl::DoCopyTexImage2D(
 
   if (info->IsAttachedToFramebuffer()) {
     state_dirty_ = true;
+    // TODO(gman): If textures tracked which framebuffers they were attached to
+    // we could just mark those framebuffers as not complete.
+    framebuffer_manager()->IncFramebufferStateChangeCount();
   }
 
   // Clip to size to source dimensions
