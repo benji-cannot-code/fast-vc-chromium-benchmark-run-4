b6bac519d7260e61c1649724f3d4a8c043addecd
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 22:49:12 +0000

    Make sure the PDB path in Syzygy-optimized Chrome is absolute.
    R=rogerm@chromium.org,mmoss@chromium.org
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8989031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/build/win/syzygy_reorder.py
+++ chrome/tools/build/win/syzygy_reorder.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -47,10 +47,10 @@ def _ReorderBinary(relink_exe, executable, symbol, destination_dir):
          '--verbose',
          '--input-dll=%s' % executable,
          '--input-pdb=%s' % symbol,
-         '--output-dll=%s' % os.path.join(destination_dir,
-                                          os.path.basename(executable)),
-         '--output-pdb=%s' % os.path.join(destination_dir,
-                                          os.path.basename(symbol)),]
+         '--output-dll=%s' % os.path.abspath(
+             os.path.join(destination_dir, os.path.basename(executable))),
+         '--output-pdb=%s' % os.path.abspath(
+             os.path.join(destination_dir, os.path.basename(symbol))),]
 
   # Check whether there's an order file available for the executable.
   order_file = '%s-order.json' % executable
