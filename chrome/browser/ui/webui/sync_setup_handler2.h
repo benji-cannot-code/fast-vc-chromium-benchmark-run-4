7736ffae447d53b40a31397696c63e58e440252b
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 01:17:49 +0000

    Moved all options2 files to the options2 namespace.
    BUG=100885
    TEST=everything compiles
    
    Review URL: http://codereview.chromium.org/8947010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_setup_handler2.h
+++ chrome/browser/ui/webui/sync_setup_handler2.h
@@ -15,8 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SyncSetupFlow;
 class ProfileManager;
 
+namespace options2 {
+
 class SyncSetupHandler2 : public GaiaOAuthConsumer,
-                          public OptionsPage2UIHandler,
+                          public OptionsPageUIHandler,
                           public SyncSetupFlowHandler {
  public:
   // Constructs a new SyncSetupHandler. |profile_manager| may be NULL.
@@ -114,5 +116,7 @@ class SyncSetupHandler2 : public GaiaOAuthConsumer,
 
   DISALLOW_COPY_AND_ASSIGN(SyncSetupHandler2);
 };
+
+}  // namespace options2
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SYNC_SETUP_HANDLER2_H_
