d9b5c536d7e1c259824a13c8d93610706915c612
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 23:22:09 +0000

    extensions: change remove button to trash icon
    this also moves reusable CSS and HTML into shared/ so more places can continue using the trash code.
    
    BUG=107323
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9005003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115218 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extension_list.js
+++ chrome/browser/resources/extensions/extension_list.js
@@ -90,9 +90,9 @@ cr.define('options', function() {
       * @private
       */
      createNode_: function(extension) {
-      var template = document.querySelector('#extension-item-template');
+      var template = $('template-collection').querySelector(
+          '.extension-list-item-wrapper');
       var node = template.cloneNode(true);
-      node.hidden = false;
       node.id = extension.id;
 
       if (!extension.enabled)
@@ -182,10 +182,12 @@ cr.define('options', function() {
       }
 
       // 'Remove' button.
-      var button = node.querySelector('.remove-button');
-      button.addEventListener('click', function(e) {
+      var trashTemplate = $('template-collection').querySelector('.trash');
+      var trash = trashTemplate.cloneNode(true);
+      trash.addEventListener('click', function(e) {
         chrome.send('extensionSettingsUninstall', [extension.id]);
       });
+      node.querySelector('.enable-controls').appendChild(trash);
 
       this.appendChild(node);
     },
--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
 #include "chrome/browser/ui/webui/options/extension_settings_handler.h"
+#include "chrome/browser/ui/webui/shared_resources_data_source.h"
 #include "chrome/common/url_constants.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "grit/browser_resources.h"
@@ -32,6 +33,8 @@ ExtensionsUI::ExtensionsUI(TabContents* contents) : ChromeWebUI(contents) {
   Profile* profile = Profile::FromBrowserContext(contents->browser_context());
   ChromeWebUIDataSource* source = CreateExtensionsHTMLSource();
   profile->GetChromeURLDataManager()->AddDataSource(source);
+  profile->GetChromeURLDataManager()->AddDataSource(
+      new SharedResourcesDataSource());
 
   ExtensionSettingsHandler* handler = new ExtensionSettingsHandler();
   handler->GetLocalizedValues(source->localized_strings());
