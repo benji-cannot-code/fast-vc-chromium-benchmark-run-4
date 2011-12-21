1c34319f40a3e690b51c20da11e55f12992458f2
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 03:22:35 +0000

    Add 'running glint' animation to 'draw attention' state of Panels on Mac.
    Experimental feature, to adjust later. The motivation is to increase visibility and ability to actually 'draw attention'. Simply changing the color of a static object is often missed by the users.
    
    
    Review URL: http://codereview.chromium.org/9008026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115270 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_titlebar_view_cocoa.h
+++ chrome/browser/ui/panels/panel_titlebar_view_cocoa.h
@@ -33,6 +33,14 @@ enum PanelDragState {
   PANEL_DRAG_SUPPRESSED  // Ignore drag events until PANEL_DRAG_CAN_START.
 };
 
+@interface RepaintAnimation : NSAnimation {
+ @private
+  NSView* targetView_;
+}
+- (id)initWithView:(NSView*)targetView duration:(double) duration;
+- (void)setCurrentProgress:(NSAnimationProgress)progress;
+@end
+
 @interface PanelTitlebarViewCocoa : NSView {
  @private
   IBOutlet PanelWindowControllerCocoa* controller_;
@@ -51,6 +59,10 @@ enum PanelDragState {
   PanelDragState dragState_;
   BOOL isDrawingAttention_;
   NSPoint dragStartLocation_;
+  // "Glint" animation is used in "Draw Attention" mode.
+  scoped_nsobject<RepaintAnimation> glintAnimation_;
+  scoped_nsobject<NSTimer> glintAnimationTimer_;
+  double glintInterval_;
 }
 
   // Callback from Close button.
@@ -89,6 +101,10 @@ enum PanelDragState {
 - (void)drawAttention;
 - (void)stopDrawingAttention;
 - (BOOL)isDrawingAttention;
+- (void)startGlintAnimation;
+- (void)restartGlintAnimation:(NSTimer*)timer;
+- (void)stopGlintAnimation;
+
 @end  // @interface PanelTitlebarView
 
 // Methods which are either only for testing, or only public for testing.
