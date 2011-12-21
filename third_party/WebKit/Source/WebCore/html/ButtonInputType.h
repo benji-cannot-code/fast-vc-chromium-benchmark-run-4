b94d7bc18732ac013fbb54a219b2cbeef769d849
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-21 09:47:45 +0000

    [Forms] Add OVERRIDE to WebCore/html/*InputType.h https://bugs.webkit.org/show_bug.cgi?id=74996
    Patch by Yosifumi Inoue <yosin@chromium.org> on 2011-12-21
    Reviewed by Kent Tamura.
    
    No new tests. Changes are just for compilation.
    
    * html/BaseButtonInputType.h: Add OVERRIDE.
    * html/BaseCheckableInputType.h: Add OVERRIDE.
    * html/BaseDateAndTimeInputType.h: Add OVERRIDE.
    * html/BaseTextInputType.h: Add OVERRIDE.
    * html/ButtonInputType.h: Add OVERRIDE.
    * html/CheckboxInputType.h: Add OVERRIDE.
    * html/ColorInputType.h: Add OVERRIDE.
    * html/DateInputType.h: Add OVERRIDE.
    * html/DateTimeInputType.h: Add OVERRIDE.
    * html/DateTimeLocalInputType.h: Add OVERRIDE.
    * html/EmailInputType.h: Add OVERRIDE.
    * html/FileInputType.h: Add OVERRIDE.
    * html/HiddenInputType.h: Add OVERRIDE.
    * html/ImageInputType.h: Add OVERRIDE.
    * html/IsIndexInputType.h: Add OVERRIDE.
    * html/MonthInputType.h: Add OVERRIDE.
    * html/NumberInputType.h: Add OVERRIDE.
    * html/PasswordInputType.h: Add OVERRIDE.
    * html/RadioInputType.h: Add OVERRIDE.
    * html/RangeInputType.h: Add OVERRIDE.
    * html/ResetInputType.h: Add OVERRIDE.
    * html/SearchInputType.h: Add OVERRIDE.
    * html/SubmitInputType.h: Add OVERRIDE.
    * html/TelephoneInputType.h: Add OVERRIDE.
    * html/TextFieldInputType.h: Add OVERRIDE.
    * html/TextInputType.h: Add OVERRIDE.
    * html/TimeInputType.h: Add OVERRIDE.
    * html/URLInputType.h: Add OVERRIDE.
    * html/WeekInputType.h: Add OVERRIDE.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/ButtonInputType.h
+++ third_party/WebKit/Source/WebCore/html/ButtonInputType.h
@@ -42,9 +42,9 @@ public:
 
 private:
     ButtonInputType(HTMLInputElement* element) : BaseButtonInputType(element) { }
-    virtual const AtomicString& formControlType() const;
-    virtual bool supportsValidation() const;
-    virtual bool isTextButton() const;
+    virtual const AtomicString& formControlType() const OVERRIDE;
+    virtual bool supportsValidation() const OVERRIDE;
+    virtual bool isTextButton() const OVERRIDE;
 };
 
 } // namespace WebCore
