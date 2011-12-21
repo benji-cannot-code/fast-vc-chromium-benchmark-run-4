1daa8becadece41ded2fbddd2a69aaaa505e2729
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 02:00:48 +0000

    Coverity: Fixing pass by value. CID=101462, 101458, 101437, 101471, 101467
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/9006023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -239,7 +239,7 @@ class ScopedExtensionControlledPrefUpdate : public DictionaryPrefUpdate {
   DISALLOW_COPY_AND_ASSIGN(ScopedExtensionControlledPrefUpdate);
 };
 
-std::string JoinPrefs(std::string parent, const char* child) {
+std::string JoinPrefs(const std::string& parent, const char* child) {
   return parent + "." + child;
 }
 
