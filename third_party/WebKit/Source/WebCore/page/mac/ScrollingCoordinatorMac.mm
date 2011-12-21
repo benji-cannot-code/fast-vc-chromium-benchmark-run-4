dd0a19fe9c813a5b8302e586ae4b9adf31a529d6
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-21 23:02:33 +0000

    Make it possible to use contents layers in scrollbars https://bugs.webkit.org/show_bug.cgi?id=75044
    Reviewed by Simon Fraser.
    
    * page/ScrollingCoordinator.h:
    * page/mac/ScrollingCoordinatorMac.mm:
    (WebCore::ScrollingCoordinator::frameViewHorizontalScrollbarLayerDidChange):
    (WebCore::ScrollingCoordinator::frameViewVerticalScrollbarLayerDidChange):
    Don't pass const GraphicsLayers to these member functions; we need to be able to mutate them.
    
    * platform/ScrollView.cpp:
    (positionScrollbarLayer):
    If the scrollbar layer has a contents layer, just update its contents rect. Otherwise,
    invalidate the scrollbar layer.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103462 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/mac/ScrollingCoordinatorMac.mm
+++ third_party/WebKit/Source/WebCore/page/mac/ScrollingCoordinatorMac.mm
@@ -162,7 +162,7 @@ static ScrollingThread& scrollingThread()
     return scrollingThread;
 }
 
-void ScrollingCoordinator::frameViewHorizontalScrollbarLayerDidChange(FrameView* frameView, const GraphicsLayer*)
+void ScrollingCoordinator::frameViewHorizontalScrollbarLayerDidChange(FrameView* frameView, GraphicsLayer*)
 {
     ASSERT(isMainThread());
     ASSERT(m_page);
@@ -173,7 +173,7 @@ void ScrollingCoordinator::frameViewHorizontalScrollbarLayerDidChange(FrameView*
     // FIXME: Implement.
 }
 
-void ScrollingCoordinator::frameViewVerticalScrollbarLayerDidChange(FrameView* frameView, const GraphicsLayer*)
+void ScrollingCoordinator::frameViewVerticalScrollbarLayerDidChange(FrameView* frameView, GraphicsLayer*)
 {
     ASSERT(isMainThread());
     ASSERT(m_page);
