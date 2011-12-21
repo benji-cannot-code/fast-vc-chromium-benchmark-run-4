92414c58c5d8683b82a789b4bbe84771e154a8ef
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 07:59:34 +0000

    Moved all options2 files to the options2 namespace.
    Reland of http://codereview.chromium.org/8947010
    
    BUG=100885
    TEST=everything compiles
    
    Review URL: http://codereview.chromium.org/8999008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo/sync_promo_handler2.cc
+++ chrome/browser/ui/webui/sync_promo/sync_promo_handler2.cc
@@ -67,6 +67,8 @@ static void RecordExperimentOutcomesOnSignIn() {
 
 }  // namespace
 
+namespace options2 {
+
 SyncPromoHandler2::SyncPromoHandler2(ProfileManager* profile_manager)
     : SyncSetupHandler2(profile_manager),
       window_already_closed_(false) {
@@ -291,3 +293,5 @@ void SyncPromoHandler2::RecordUserFlowAction(int action) {
   UMA_HISTOGRAM_ENUMERATION("SyncPromo.UserFlow", action,
                             SYNC_PROMO_BUCKET_BOUNDARY);
 }
+
+}  // namespace options2
