6b238c7edfa24159446f2eb9a5134dbc4217c545
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 00:01:55 +0000

    Revert 115227 - Coverity: Initialize member variables.
    CID=6462,16392,16996,17268,100987,101018,102401,102417,102476,102484,102513
        102546,102552,102567,102617,102674,102677,102678,102679,102681,102685,
        102714,102750,102751,102781
    BUG=none
    TEST=none
    R=groby
    
    Review URL: http://codereview.chromium.org/9005015
    
    TBR=jhawkins@chromium.org
    Review URL: http://codereview.chromium.org/8965062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/snapshot_tab_helper.cc
+++ chrome/browser/ui/snapshot_tab_helper.cc
@@ -11,8 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 
 SnapshotTabHelper::SnapshotTabHelper(TabContentsWrapper* wrapper)
-    : TabContentsObserver(wrapper->tab_contents()),
-      wrapper_(wrapper) {
+    : TabContentsObserver(wrapper->tab_contents()) {
 }
 
 SnapshotTabHelper::~SnapshotTabHelper() {
