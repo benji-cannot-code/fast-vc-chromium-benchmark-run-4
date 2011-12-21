8baf409638612b323d0b2f383e7790b53230cebe
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 10:54:32 +0000

    Revert 115297 - Add overflow indicator count on Windows.
    Broke compilation on "Linux ChromiumOS Builder (Aura dbg)"
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8953040
    
    TBR=jianli@chromium.org
    Review URL: http://codereview.chromium.org/8949035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115301 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel.cc
+++ chrome/browser/ui/panels/panel.cc
@@ -195,7 +195,6 @@ bool Panel::IsActive() const {
 
 void Panel::FlashFrame() {
   native_panel_->DrawAttention();
-  manager()->OnPanelAttentionStateChanged(this);
 }
 
 gfx::NativeWindow Panel::GetNativeHandle() {
