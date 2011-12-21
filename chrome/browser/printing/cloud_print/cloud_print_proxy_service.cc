3884b3ef9221e6ed34ec1efedbec74ff2d1df2eb
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 20:56:54 +0000

    Delete me Localize Google Cloud Print in strings. Change UI interaction points to reference connector instead of proxy. Fix strings for connector in options to be more clearly about the connector.
    BUG=107252,107592
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8936013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115396 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/cloud_print/cloud_print_proxy_service.cc
+++ chrome/browser/printing/cloud_print/cloud_print_proxy_service.cc
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::BrowserThread;
 
-// TODO(sanjeevr): Localize the product name?
-const char kCloudPrintProductName[] = "Google Cloud Print";
-
 class CloudPrintProxyService::TokenExpiredNotificationDelegate
     : public NotificationDelegate {
  public:
@@ -123,9 +120,9 @@ bool CloudPrintProxyService::ShowTokenExpiredNotification() {
   // TODO(sanjeevr): Get icon for this notification.
   GURL icon_url;
 
-  string16 title = UTF8ToUTF16(kCloudPrintProductName);
+  string16 title = l10n_util::GetStringUTF16(IDS_GOOGLE_CLOUD_PRINT);
   string16 message =
-      l10n_util::GetStringUTF16(IDS_CLOUD_PRINT_TOKEN_EXPIRED_MESSAGE);
+      l10n_util::GetStringFUTF16(IDS_CLOUD_PRINT_TOKEN_EXPIRED_MESSAGE, title);
   string16 content_url = DesktopNotificationService::CreateDataUrl(
       icon_url, title, message, WebKit::WebTextDirectionDefault);
   token_expired_delegate_ = new TokenExpiredNotificationDelegate(this);
