1b5ef363571bfab305244564076da84083f8fb55
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 13:29:06 +0000

    Add an out-of-line destructor to this class.
    They are needed because StringPiece is going to become a template instatiation, and according to the Chromium style-checking Clang plugins, that makes any class with a member StringPiece 'complex'.
    
    See http://codereview.chromium.org/8659047/ for the changes to StringPiece.
    
    R=aa@chromium.org
    BUG=87634
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9021001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115323 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/static_v8_external_string_resource.cc
+++ chrome/renderer/static_v8_external_string_resource.cc
@@ -10,6 +10,9 @@ StaticV8ExternalAsciiStringResource::StaticV8ExternalAsciiStringResource(
     : buffer_(buffer) {
 }
 
+StaticV8ExternalAsciiStringResource::~StaticV8ExternalAsciiStringResource() {
+}
+
 const char* StaticV8ExternalAsciiStringResource::data() const {
   return buffer_.data();
 }
