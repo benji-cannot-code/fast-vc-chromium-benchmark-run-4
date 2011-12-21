8d189f4a4bbbdbb0b7681e41acd965bed2246241
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-21 21:54:43 +0000

    Always send a reply to nacl launch requests.
    Previously there were many return paths where the reply message would not get sent, which would indefinitely hang the renderer. I also found a leak where if the FILE thread is gone when we try to make the IRT available, the object will be leaked (the the reply message would not be sent either). I tried to find all of the failure cases and ensure the reply message is sent.
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8994010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115413 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_process_host.cc
+++ chrome/browser/nacl_host/nacl_process_host.cc
@@ -226,9 +226,11 @@ bool NaClProcessHost::Launch(
   if (!LaunchSelLdr()) {
     return false;
   }
-  chrome_render_message_filter_ = chrome_render_message_filter;
-  reply_msg_ = reply_msg;
 
+  chrome_render_message_filter_ = chrome_render_message_filter;
+
+  // On success, we take responsibility for sending the reply.
+  reply_msg_ = reply_msg;
   return true;
 }
 
@@ -385,8 +387,10 @@ void NaClProcessHost::OnProcessLaunched() {
     SendStart(nacl_browser->IrtFile());
   } else {
     // We're waiting for the IRT to be open.
-    nacl_browser->MakeIrtAvailable(base::Bind(&NaClProcessHost::IrtReady,
-                                              weak_factory_.GetWeakPtr()));
+    if (!nacl_browser->MakeIrtAvailable(
+            base::Bind(&NaClProcessHost::IrtReady,
+                       weak_factory_.GetWeakPtr())))
+      delete this;
   }
 }
 
