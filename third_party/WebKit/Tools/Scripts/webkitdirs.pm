1ddf9eb33f92609f22b0f884661c80c6aa99f0ab
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-21 23:22:05 +0000

    Teach build-jsc how to build JavaScriptCore for the EFL, BlackBerry, and WinCE ports https://bugs.webkit.org/show_bug.cgi?id=74985
    Patch by Daniel Bates <dbates@rim.com> on 2011-12-21
    Reviewed by Eric Seidel.
    
    * Scripts/build-jsc:
    * Scripts/webkitdirs.pm:
    (cmakeBasedPortName): Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -54,6 +54,7 @@ BEGIN {
        &chdirWebKit
        &checkFrameworks
        &cmakeBasedPortArguments
+       &cmakeBasedPortName
        &currentSVNRevision
        &debugSafari
        &passedConfiguration
@@ -1939,6 +1940,14 @@ sub cmakeBasedPortArguments()
     return blackberryCMakeArguments() if isBlackBerry();
     return ('-DCMAKE_WINCE_SDK="STANDARDSDK_500 (ARMV4I)"') if isWinCE();
     return ();
+}
+
+sub cmakeBasedPortName()
+{
+    return "BlackBerry" if isBlackBerry();
+    return "Efl" if isEfl();
+    return "WinCE" if isWinCE();
+    return "";
 }
 
 sub promptUser
