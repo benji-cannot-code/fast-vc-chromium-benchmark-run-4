2e29a9c2db011c7c8f349249656a86f727efa4b9
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 17:23:58 +0000

    Aura: Window should be dragged by touch move event on title bar.
    Move window when touch move event(single touch) triggered on title bar.
    
    Patch by Junmin Zhu <junmin.zhu@intel.com>
    
    BUG=106500
    TEST=manually
    
    Review URL: http://codereview.chromium.org/8885008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115550 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/toplevel_window_event_filter.h
+++ ui/aura_shell/toplevel_window_event_filter.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_AURA_SHELL_TOPLEVEL_WINDOW_EVENT_FILTER_H_
 #pragma once
 
+#include <set>
+
 #include "base/compiler_specific.h"
 #include "ui/aura/event_filter.h"
 #include "ui/aura_shell/aura_shell_export.h"
@@ -14,8 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 
 namespace aura {
-class Window;
+class LocatedEvent;
 class MouseEvent;
+class Window;
 }
 
 namespace aura_shell {
@@ -46,11 +49,15 @@ class AURA_SHELL_EXPORT ToplevelWindowEventFilter : public aura::EventFilter {
 
   // Called during a drag to resize/position the window.
   // The return value is returned by OnMouseEvent() above.
-  bool HandleDrag(aura::Window* target, aura::MouseEvent* event);
+  bool HandleDrag(aura::Window* target, aura::LocatedEvent* event);
+
+  // Updates the event location to window.
+  void UpdateLocationFromEvent(aura::Window* target,
+                               aura::LocatedEvent* event);
 
   // Updates the |window_component_| using the |event|'s location.
   void UpdateWindowComponentForEvent(aura::Window* window,
-                                     aura::MouseEvent* event);
+                                     aura::LocatedEvent* event);
 
   // Calculates the new origin of the window during a drag.
   gfx::Point GetOriginForDrag(int bounds_change,
@@ -87,6 +94,9 @@ class AURA_SHELL_EXPORT ToplevelWindowEventFilter : public aura::EventFilter {
 
   // The window component (hit-test code) the mouse is currently over.
   int window_component_;
+
+  // Set of touch ids currently pressed.
+  std::set<int> pressed_touch_ids_;
 
   DISALLOW_COPY_AND_ASSIGN(ToplevelWindowEventFilter);
 };
