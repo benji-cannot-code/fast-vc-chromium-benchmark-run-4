73eea75f0e6a4b56ae28164d89789318a47108be
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 15:12:04 +0000

    Prevent Windows AcceleratedSurface from positioning the compositing window above plugin windows.
    SetWindowPos moves the window to the top by default.
    
    This broke chromium windowed flash plugins (not official chrome), e.g. youtube videos, when the accelerated compositor was active.
    
    Review URL: http://codereview.chromium.org/9020034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115538 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/surface/accelerated_surface_win.cc
+++ ui/gfx/surface/accelerated_surface_win.cc
@@ -366,7 +366,7 @@ void AcceleratedSurface::DoPresentAndAcknowledge(
 
   ::SetWindowPos(
       window_,
-      NULL,
+      HWND_BOTTOM,
       0, 0,
       size.width(), size.height(),
       SWP_NOACTIVATE | SWP_NOCOPYBITS | SWP_NOMOVE |SWP_NOOWNERZORDER |
