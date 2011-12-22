4d1f2b94ca85e56123d731f7c6c90dce6e47f048
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-22 07:00:23 +0000

    Plumbs refresh rate through WebLayerTreeView::Settings https://bugs.webkit.org/show_bug.cgi?id=74884
    Patch by Peter Kotwicz <pkotwicz@google.com> on 2011-12-21
    Reviewed by James Robinson.
    
    * public/platform/WebLayerTreeView.h:
    (WebKit::WebLayerTreeView::Settings::Settings):
    * src/WebLayerTreeView.cpp:
    (WebKit::WebLayerTreeView::Settings::operator CCSettings):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103496 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebLayerTreeView.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebLayerTreeView.cpp
@@ -43,6 +43,7 @@ WebLayerTreeView::Settings::operator CCSettings() const
     settings.compositeOffscreen = compositeOffscreen;
     settings.showFPSCounter = showFPSCounter;
     settings.showPlatformLayerTree = showPlatformLayerTree;
+    settings.refreshRate = refreshRate;
     settings.partialSwapEnabled = partialSwapEnabled;
 
     // FIXME: showFPSCounter / showPlatformLayerTree aren't supported currently.
