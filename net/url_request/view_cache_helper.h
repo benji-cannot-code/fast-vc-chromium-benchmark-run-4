054b931354fc85f5c4c1a411dc92517b0ea0f473
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 05:18:12 +0000

    base::Bind: Finish up net/url_request.
    BUG=none
    TEST=none
    R=csilv
    
    Review URL: http://codereview.chromium.org/9017041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115487 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/view_cache_helper.h
+++ net/url_request/view_cache_helper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/weak_ptr.h"
 #include "net/base/completion_callback.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_export.h"
@@ -115,7 +116,7 @@ class NET_EXPORT ViewCacheHelper {
 
   State next_state_;
 
-  scoped_refptr<CancelableOldCompletionCallback<ViewCacheHelper> > entry_callback_;
+  base::WeakPtrFactory<ViewCacheHelper> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ViewCacheHelper);
 };
