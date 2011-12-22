433f9c3dad9e0f4b67ddb8fd376e038744c52f66
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-22 22:52:22 +0000

    WebMediaPlayerClientImpl::AudioClientImpl wrapper object should exist only if wrapped object exists https://bugs.webkit.org/show_bug.cgi?id=75113
    Reviewed by Kenneth Russell.
    
    * src/WebMediaPlayerClientImpl.cpp:
    (WebKit::WebMediaPlayerClientImpl::AudioSourceProviderImpl::wrap):
    (WebKit::WebMediaPlayerClientImpl::AudioSourceProviderImpl::setClient):
    * src/WebMediaPlayerClientImpl.h:
    (WebKit::WebMediaPlayerClientImpl::AudioClientImpl::AudioClientImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103584 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.h
@@ -184,8 +184,8 @@ private:
 
     class AudioClientImpl : public WebKit::WebAudioSourceProviderClient {
     public:
-        AudioClientImpl()
-            : m_client(0)
+        AudioClientImpl(WebCore::AudioSourceProviderClient* client)
+            : m_client(client)
         {
         }
 
@@ -193,8 +193,6 @@ private:
 
         // WebAudioSourceProviderClient
         virtual void setFormat(size_t numberOfChannels, float sampleRate);
-
-        void wrap(WebCore::AudioSourceProviderClient* client) { m_client = client; }
 
     private:
         WebCore::AudioSourceProviderClient* m_client;
@@ -221,7 +219,7 @@ private:
 
     private:
         WebAudioSourceProvider* m_webAudioSourceProvider;
-        AudioClientImpl m_client;
+        OwnPtr<AudioClientImpl> m_client;
     };
 
     AudioSourceProviderImpl m_audioSourceProvider;
