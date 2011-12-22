0800f63a9f424d7c80a3ddf5f2f8651b64a08f7d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 03:26:33 +0000

    gtk: Include SkColor.h instead of SkPaint.h in infobar_gtk.h
    The infobar_gtk.h header file does not need SkPaint as only SkColor is used.
    
    R=erg@chromium.org,thestig@chromium.org
    
    Review URL: http://codereview.chromium.org/8992017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115476 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/infobar_gtk.h
+++ chrome/browser/ui/gtk/infobars/infobar_gtk.h
@@ -16,13 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/menu_gtk.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/gtk/gtk_signal.h"
 #include "ui/base/gtk/owned_widget_gtk.h"
 
 class CustomDrawButton;
 class GtkThemeService;
-class MenuGtk;
 
 namespace ui {
 class GtkSignalRegistrar;
