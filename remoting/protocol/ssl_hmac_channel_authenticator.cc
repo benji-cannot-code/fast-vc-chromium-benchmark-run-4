benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 794a76650fb9813bd9b0786bdb4a08af0b3a3840

Replace V1*ChannelAuthenticator with SslHmacChannelAuthenticator.
The new class will be used for both V1 and V2 authentication

BUG=105214


Review URL: http://codereview.chromium.org/8963005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115519 0039d316-1c4b-4281-b951-d872f2087c98

remoting/protocol/ssl_hmac_channel_authenticator.cc
@@ -1,0 +1,273 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/protocol/ssl_hmac_channel_authenticator.h"
+
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "crypto/secure_util.h"
+#include "net/base/cert_verifier.h"
+#include "net/base/host_port_pair.h"
+#include "net/base/io_buffer.h"
+#include "net/base/net_errors.h"
+#include "net/base/ssl_config_service.h"
+#include "net/base/x509_certificate.h"
+#include "net/socket/client_socket_factory.h"
+#include "net/socket/ssl_client_socket.h"
+#include "net/socket/ssl_server_socket.h"
+#include "remoting/protocol/auth_util.h"
+
+namespace remoting {
+namespace protocol {
+
+// static
+SslHmacChannelAuthenticator* SslHmacChannelAuthenticator::CreateForClient(
+      const std::string& remote_cert,
+      const std::string& auth_key) {
+  SslHmacChannelAuthenticator* result =
+      new SslHmacChannelAuthenticator(auth_key);
+  result->remote_cert_ = remote_cert;
+  return result;
+}
+
+SslHmacChannelAuthenticator* SslHmacChannelAuthenticator::CreateForHost(
+    const std::string& local_cert,
+    crypto::RSAPrivateKey* local_private_key,
+    const std::string& auth_key) {
+  SslHmacChannelAuthenticator* result =
+      new SslHmacChannelAuthenticator(auth_key);
+  result->local_cert_ = local_cert;
+  result->local_private_key_ = local_private_key;
+  return result;
+}
+
+SslHmacChannelAuthenticator::SslHmacChannelAuthenticator(
+    const std::string& auth_key)
+    : auth_key_(auth_key),
+      local_private_key_(NULL),
+      legacy_mode_(NONE) {
+}
+
+SslHmacChannelAuthenticator::~SslHmacChannelAuthenticator() {
+}
+
+void SslHmacChannelAuthenticator::SetLegacyOneWayMode(LegacyMode legacy_mode) {
+  // Must be called before SecureAndAuthenticate().
+  DCHECK(done_callback_.is_null());
+  legacy_mode_ = legacy_mode;
+}
+
+void SslHmacChannelAuthenticator::SecureAndAuthenticate(
+    net::StreamSocket* socket, const DoneCallback& done_callback) {
+  DCHECK(CalledOnValidThread());
+  DCHECK(socket->IsConnected());
+
+  scoped_ptr<net::StreamSocket> channel_socket(socket);
+  done_callback_ = done_callback;
+
+  int result;
+  if (is_ssl_server()) {
+    scoped_refptr<net::X509Certificate> cert =
+        net::X509Certificate::CreateFromBytes(
+            local_cert_.data(), local_cert_.length());
+    if (!cert) {
+      LOG(ERROR) << "Failed to parse X509Certificate";
+      done_callback.Run(net::ERR_FAILED, NULL);
+      return;
+    }
+
+    net::SSLConfig ssl_config;
+    net::SSLServerSocket* server_socket = net::CreateSSLServerSocket(
+        channel_socket.release(), cert, local_private_key_, ssl_config);
+    socket_.reset(server_socket);
+
+    result = server_socket->Handshake(base::Bind(
+        &SslHmacChannelAuthenticator::OnConnected, base::Unretained(this)));
+  } else {
+    cert_verifier_.reset(new net::CertVerifier());
+
+    net::SSLConfig::CertAndStatus cert_and_status;
+    cert_and_status.cert_status = net::CERT_STATUS_AUTHORITY_INVALID;
+    cert_and_status.der_cert = remote_cert_;
+
+    net::SSLConfig ssl_config;
+    // Certificate verification and revocation checking are not needed
+    // because we use self-signed certs. Disable it so that the SSL
+    // layer doesn't try to initialize OCSP (OCSP works only on the IO
+    // thread).
+    ssl_config.allowed_bad_certs.push_back(cert_and_status);
+    ssl_config.rev_checking_enabled = false;
+
+    net::HostPortPair host_and_port(kSslFakeHostName, 0);
+    net::SSLClientSocketContext context;
+    context.cert_verifier = cert_verifier_.get();
+    socket_.reset(
+        net::ClientSocketFactory::GetDefaultFactory()->CreateSSLClientSocket(
+            channel_socket.release(), host_and_port,
+            ssl_config, NULL, context));
+
+    result = socket_->Connect(
+        base::Bind(&SslHmacChannelAuthenticator::OnConnected,
+                   base::Unretained(this)));
+  }
+
+  if (result == net::ERR_IO_PENDING)
+    return;
+
+  OnConnected(result);
+}
+
+bool SslHmacChannelAuthenticator::is_ssl_server() {
+  return local_private_key_ != NULL;
+}
+
+void SslHmacChannelAuthenticator::OnConnected(int result) {
+  if (result != net::OK) {
+    LOG(WARNING) << "Failed to establish SSL connection";
+    done_callback_.Run(static_cast<net::Error>(result), NULL);
+    return;
+  }
+
+  if (legacy_mode_ != RECEIVE_ONLY) {
+    // Generate authentication digest to write to the socket.
+    std::string auth_bytes = GetAuthBytes(
+        socket_.get(), is_ssl_server() ?
+        kClientAuthSslExporterLabel : kHostAuthSslExporterLabel, auth_key_);
+    if (auth_bytes.empty()) {
+      done_callback_.Run(net::ERR_FAILED, NULL);
+      return;
+    }
+
+    // Allocate a buffer to write the digest.
+    auth_write_buf_ = new net::DrainableIOBuffer(
+        new net::StringIOBuffer(auth_bytes), auth_bytes.size());
+  }
+
+  if (legacy_mode_ != SEND_ONLY) {
+    // Read an incoming token.
+    auth_read_buf_ = new net::GrowableIOBuffer();
+    auth_read_buf_->SetCapacity(kAuthDigestLength);
+  }
+
+  // If WriteAuthenticationBytes() results in |done_callback_| being
+  // called then we must not do anything else because this object may
+  // be destroyed at that point.
+  bool callback_called = false;
+  if (legacy_mode_ != RECEIVE_ONLY)
+    WriteAuthenticationBytes(&callback_called);
+  if (!callback_called && legacy_mode_ != SEND_ONLY)
+    ReadAuthenticationBytes();
+}
+
+void SslHmacChannelAuthenticator::WriteAuthenticationBytes(
+    bool* callback_called) {
+  while (true) {
+    int result = socket_->Write(
+        auth_write_buf_, auth_write_buf_->BytesRemaining(),
+        base::Bind(&SslHmacChannelAuthenticator::OnAuthBytesWritten,
+                   base::Unretained(this)));
+    if (result == net::ERR_IO_PENDING)
+      break;
+    if (!HandleAuthBytesWritten(result, callback_called))
+      break;
+  }
+}
+
+void SslHmacChannelAuthenticator::OnAuthBytesWritten(int result) {
+  DCHECK(CalledOnValidThread());
+
+  if (HandleAuthBytesWritten(result, NULL))
+    WriteAuthenticationBytes(NULL);
+}
+
+bool SslHmacChannelAuthenticator::HandleAuthBytesWritten(
+    int result, bool* callback_called) {
+  if (result <= 0) {
+    LOG(ERROR) << "Error writing authentication: " << result;
+    if (callback_called)
+      *callback_called = false;
+    done_callback_.Run(static_cast<net::Error>(result), NULL);
+    return false;
+  }
+
+  auth_write_buf_->DidConsume(result);
+  if (auth_write_buf_->BytesRemaining() > 0)
+    return true;
+
+  auth_write_buf_ = NULL;
+  CheckDone(callback_called);
+  return false;
+}
+
+void SslHmacChannelAuthenticator::ReadAuthenticationBytes() {
+  while (true) {
+    int result = socket_->Read(
+        auth_read_buf_,
+        auth_read_buf_->RemainingCapacity(),
+        base::Bind(&SslHmacChannelAuthenticator::OnAuthBytesRead,
+                   base::Unretained(this)));
+    if (result == net::ERR_IO_PENDING)
+      break;
+    if (!HandleAuthBytesRead(result))
+      break;
+  }
+}
+
+void SslHmacChannelAuthenticator::OnAuthBytesRead(int result) {
+  DCHECK(CalledOnValidThread());
+
+  if (HandleAuthBytesRead(result))
+    ReadAuthenticationBytes();
+}
+
+bool SslHmacChannelAuthenticator::HandleAuthBytesRead(int read_result) {
+  if (read_result <= 0) {
+    done_callback_.Run(static_cast<net::Error>(read_result), NULL);
+    return false;
+  }
+
+  auth_read_buf_->set_offset(auth_read_buf_->offset() + read_result);
+  if (auth_read_buf_->RemainingCapacity() > 0)
+    return true;
+
+  if (!VerifyAuthBytes(std::string(
+          auth_read_buf_->StartOfBuffer(),
+          auth_read_buf_->StartOfBuffer() + kAuthDigestLength))) {
+    LOG(WARNING) << "Mismatched authentication";
+    done_callback_.Run(net::ERR_FAILED, NULL);
+    return false;
+  }
+
+  auth_read_buf_ = NULL;
+  CheckDone(NULL);
+  return false;
+}
+
+bool SslHmacChannelAuthenticator::VerifyAuthBytes(
+    const std::string& received_auth_bytes) {
+  DCHECK(received_auth_bytes.length() == kAuthDigestLength);
+
+  // Compute expected auth bytes.
+  std::string auth_bytes = GetAuthBytes(
+      socket_.get(), is_ssl_server() ?
+      kHostAuthSslExporterLabel : kClientAuthSslExporterLabel, auth_key_);
+  if (auth_bytes.empty())
+    return false;
+
+  return crypto::SecureMemEqual(received_auth_bytes.data(),
+                                &(auth_bytes[0]), kAuthDigestLength);
+}
+
+void SslHmacChannelAuthenticator::CheckDone(bool* callback_called) {
+  if (auth_write_buf_ == NULL && auth_read_buf_ == NULL) {
+    DCHECK(socket_.get() != NULL);
+    if (callback_called)
+      *callback_called = true;
+    done_callback_.Run(net::OK, socket_.release());
+  }
+}
+
+}  // namespace protocol
+}  // namespace remoting

remoting/protocol/auth_util.cc
@@ -10,12 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "crypto/hmac.h"
 #include "crypto/sha2.h"
+#include "net/base/net_errors.h"
+#include "net/socket/ssl_socket.h"
 
 namespace remoting {
 namespace protocol {
 
 const char kClientAuthSslExporterLabel[] =
     "EXPORTER-remoting-channel-auth-client";
+const char kHostAuthSslExporterLabel[] =
+    "EXPORTER-remoting-channel-auth-host";
 
 const char kSslFakeHostName[] = "chromoting";
 
@@ -38,23 +42,31 @@ bool VerifySupportAuthToken(const std::string& jid,
 }
 
 // static
-bool GetAuthBytes(const std::string& shared_secret,
-                  const std::string& key_material,
-                  std::string* auth_bytes) {
+std::string GetAuthBytes(net::SSLSocket* socket,
+                         const base::StringPiece& label,
+                         const base::StringPiece& shared_secret) {
+  // Get keying material from SSL.
+  unsigned char key_material[kAuthDigestLength];
+  int export_result = socket->ExportKeyingMaterial(
+      label, "", key_material, kAuthDigestLength);
+  if (export_result != net::OK) {
+    LOG(ERROR) << "Error fetching keying material: " << export_result;
+    return std::string();
+  }
+
   // Generate auth digest based on the keying material and shared secret.
   crypto::HMAC response(crypto::HMAC::SHA256);
-  if (!response.Init(key_material)) {
+  if (!response.Init(key_material, kAuthDigestLength)) {
     NOTREACHED() << "HMAC::Init failed";
-    return false;
+    return std::string();
   }
   unsigned char out_bytes[kAuthDigestLength];
   if (!response.Sign(shared_secret, out_bytes, kAuthDigestLength)) {
     NOTREACHED() << "HMAC::Sign failed";
-    return false;
+    return std::string();
   }
 
-  auth_bytes->assign(out_bytes, out_bytes + kAuthDigestLength);
-  return true;
+  return std::string(out_bytes, out_bytes + kAuthDigestLength);
 }
 
 }  // namespace protocol

remoting/protocol/auth_util.h
@@ -8,11 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/string_piece.h"
+
+namespace net {
+class SSLSocket;
+}  // namespace net
+
 namespace remoting {
 namespace protocol {
 
 // Labels for use when exporting the SSL master keys.
 extern const char kClientAuthSslExporterLabel[];
+extern const char kHostAuthSslExporterLabel[];
 
 // Fake hostname used for SSL connections.
 extern const char kSslFakeHostName[];
@@ -29,10 +36,11 @@ bool VerifySupportAuthToken(const std::string& jid,
                             const std::string& access_code,
                             const std::string& auth_token);
 
-// Returns hash used for channel authentication.
-bool GetAuthBytes(const std::string& shared_secret,
-                  const std::string& key_material,
-                  std::string* auth_bytes);
+// Returns authentication bytes that must be used for the given
+// |socket|. Empty string is returned in case of failure.
+std::string GetAuthBytes(net::SSLSocket* socket,
+                         const base::StringPiece& label,
+                         const base::StringPiece& shared_secret);
 
 }  // namespace protocol
 }  // namespace remoting

remoting/protocol/channel_authenticator.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/callback.h"
+#include "base/callback_forward.h"
 #include "net/base/net_errors.h"
 
 namespace net {

remoting/protocol/pepper_session.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/jingle_messages.h"
 #include "remoting/protocol/pepper_session_manager.h"
 #include "remoting/protocol/pepper_stream_channel.h"
-#include "remoting/protocol/v1_client_channel_authenticator.h"
 #include "third_party/libjingle/source/talk/p2p/base/candidate.h"
 #include "third_party/libjingle/source/talk/xmllite/xmlelement.h"
 

remoting/protocol/ssl_hmac_channel_authenticator.h
@@ -1,0 +1,111 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_PROTOCOL_SSL_HMAC_CHANNEL_AUTHENTICATOR_H_
+#define REMOTING_PROTOCOL_SSL_HMAC_CHANNEL_AUTHENTICATOR_H_
+
+#include <string>
+
+#include "base/callback.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/threading/non_thread_safe.h"
+#include "remoting/protocol/channel_authenticator.h"
+
+namespace crypto {
+class RSAPrivateKey;
+}  // namespace crypto
+
+namespace net {
+class CertVerifier;
+class DrainableIOBuffer;
+class GrowableIOBuffer;
+class SSLSocket;
+}  // namespace net
+
+namespace remoting {
+namespace protocol {
+
+// SslHmacChannelAuthenticator implements ChannelAuthenticator that
+// secures channels using SSL and authenticates them with a shared
+// secret HMAC.
+class SslHmacChannelAuthenticator : public ChannelAuthenticator,
+                                    public base::NonThreadSafe {
+ public:
+  enum LegacyMode {
+    NONE,
+    SEND_ONLY,
+    RECEIVE_ONLY,
+  };
+
+  // CreateForClient() and CreateForHost() create an authenticator
+  // instances for client and host. |auth_key| specifies shared key
+  // known by both host and client. In case of V1Authenticator the
+  // |auth_key| is set to access code. For EKE-based authentication
+  // |auth_key| is the key established using EKE over the signaling
+  // channel.
+  static SslHmacChannelAuthenticator* CreateForClient(
+      const std::string& remote_cert,
+      const std::string& auth_key);
+
+  static SslHmacChannelAuthenticator* CreateForHost(
+      const std::string& local_cert,
+      crypto::RSAPrivateKey* local_private_key,
+      const std::string& auth_key);
+
+  // TODO(sergeyu): This method is used only for the legacy
+  // V1Authenticator. Remove it when V1Authenticator is removed.
+  void SetLegacyOneWayMode(LegacyMode legacy_mode);
+
+  virtual ~SslHmacChannelAuthenticator();
+
+  // ChannelAuthenticator interface.
+  virtual void SecureAndAuthenticate(
+      net::StreamSocket* socket, const DoneCallback& done_callback) OVERRIDE;
+
+ private:
+  SslHmacChannelAuthenticator(const std::string& auth_key);
+
+  bool is_ssl_server();
+
+  void OnConnected(int result);
+
+  void WriteAuthenticationBytes(bool* callback_called);
+  void OnAuthBytesWritten(int result);
+  bool HandleAuthBytesWritten(int result, bool* callback_called);
+
+  void ReadAuthenticationBytes();
+  void OnAuthBytesRead(int result);
+  bool HandleAuthBytesRead(int result);
+  bool VerifyAuthBytes(const std::string& received_auth_bytes);
+
+  void CheckDone(bool* callback_called);
+
+  // The mutual secret used for authentication.
+  std::string auth_key_;
+
+  // Used in the SERVER mode only.
+  std::string local_cert_;
+  crypto::RSAPrivateKey* local_private_key_;
+
+  // Used in the CLIENT mode only.
+  std::string remote_cert_;
+  scoped_ptr<net::CertVerifier> cert_verifier_;
+
+  LegacyMode legacy_mode_;
+
+  scoped_ptr<net::SSLSocket> socket_;
+  DoneCallback done_callback_;
+
+  scoped_refptr<net::DrainableIOBuffer> auth_write_buf_;
+  scoped_refptr<net::GrowableIOBuffer> auth_read_buf_;
+
+  DISALLOW_COPY_AND_ASSIGN(SslHmacChannelAuthenticator);
+};
+
+}  // namespace protocol
+}  // namespace remoting
+
+#endif  // REMOTING_PROTOCOL_SSL_HMAC_CHANNEL_AUTHENTICATOR_H_

remoting/protocol/ssl_hmac_channel_authenticator_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "remoting/protocol/v1_authenticator.h"
+#include "remoting/protocol/ssl_hmac_channel_authenticator.h"
 
 #include "base/bind.h"
 #include "base/file_path.h"
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "crypto/rsa_private_key.h"
 #include "net/base/net_errors.h"
-#include "remoting/protocol/authenticator.h"
-#include "remoting/protocol/channel_authenticator.h"
 #include "remoting/protocol/connection_tester.h"
 #include "remoting/protocol/fake_session.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -21,19 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libjingle/source/talk/xmllite/xmlelement.h"
 
 using testing::_;
-using testing::DeleteArg;
-using testing::Mock;
 using testing::SaveArg;
 
 namespace remoting {
 namespace protocol {
 
 namespace {
-
-const int kMessageSize = 100;
-const int kMessages = 1;
-
-const char kClientJid[] = "host2@gmail.com/321";
 
 const char kTestSharedSecret[] = "1234-1234-5678";
 const char kTestSharedSecretBad[] = "0000-0000-0001";
@@ -45,11 +36,11 @@ class MockChannelDoneCallback {
 
 }  // namespace
 
-class V1AuthenticatorTest : public testing::Test {
+class SslHmacChannelAuthenticatorTest : public testing::Test {
  public:
-  V1AuthenticatorTest() {
+  SslHmacChannelAuthenticatorTest() {
   }
-  virtual ~V1AuthenticatorTest() {
+  virtual ~SslHmacChannelAuthenticatorTest() {
   }
 
  protected:
@@ -75,46 +66,6 @@ class V1AuthenticatorTest : public testing::Test {
         crypto::RSAPrivateKey::CreateFromPrivateKeyInfo(key_vector));
   }
 
-  void InitAuthenticators(const std::string& client_secret,
-                          const std::string& host_secret) {
-    host_.reset(new V1HostAuthenticator(
-        host_cert_, private_key_.get(), host_secret, kClientJid));
-    client_.reset(new V1ClientAuthenticator(kClientJid, client_secret));
-  }
-
-  void RunAuthExchange() {
-    do {
-      scoped_ptr<buzz::XmlElement> message;
-
-      // Pass message from client to host.
-      ASSERT_EQ(Authenticator::MESSAGE_READY, client_->state());
-      message.reset(client_->GetNextMessage());
-      ASSERT_TRUE(message.get());
-      ASSERT_NE(Authenticator::MESSAGE_READY, client_->state());
-
-      ASSERT_EQ(Authenticator::WAITING_MESSAGE, host_->state());
-      host_->ProcessMessage(message.get());
-      ASSERT_NE(Authenticator::WAITING_MESSAGE, host_->state());
-
-      // Are we done yet?
-      if (host_->state() == Authenticator::ACCEPTED ||
-          host_->state() == Authenticator::REJECTED) {
-        break;
-      }
-
-      // Pass message from host to client.
-      ASSERT_EQ(Authenticator::MESSAGE_READY, host_->state());
-      message.reset(host_->GetNextMessage());
-      ASSERT_TRUE(message.get());
-      ASSERT_NE(Authenticator::MESSAGE_READY, host_->state());
-
-      ASSERT_EQ(Authenticator::WAITING_MESSAGE, client_->state());
-      client_->ProcessMessage(message.get());
-      ASSERT_NE(Authenticator::WAITING_MESSAGE, client_->state());
-    } while (host_->state() != Authenticator::ACCEPTED &&
-             host_->state() != Authenticator::REJECTED);
-  }
-
   void RunChannelAuth(bool expected_fail) {
     client_fake_socket_.reset(new FakeSocket());
     host_fake_socket_.reset(new FakeSocket());
@@ -133,19 +84,17 @@ class V1AuthenticatorTest : public testing::Test {
     net::StreamSocket* client_socket = NULL;
     net::StreamSocket* host_socket = NULL;
 
-    EXPECT_CALL(client_callback_, OnDone(net::OK, _))
-        .WillOnce(SaveArg<1>(&client_socket));
     if (expected_fail) {
+      EXPECT_CALL(client_callback_, OnDone(net::ERR_FAILED, NULL));
       EXPECT_CALL(host_callback_, OnDone(net::ERR_FAILED, NULL));
     } else {
+      EXPECT_CALL(client_callback_, OnDone(net::OK, _))
+          .WillOnce(SaveArg<1>(&client_socket));
       EXPECT_CALL(host_callback_, OnDone(net::OK, _))
           .WillOnce(SaveArg<1>(&host_socket));
     }
 
     message_loop_.RunAllPending();
-
-    Mock::VerifyAndClearExpectations(&client_callback_);
-    Mock::VerifyAndClearExpectations(&host_callback_);
 
     client_socket_.reset(client_socket);
     host_socket_.reset(host_socket);
@@ -155,8 +104,6 @@ class V1AuthenticatorTest : public testing::Test {
 
   scoped_ptr<crypto::RSAPrivateKey> private_key_;
   std::string host_cert_;
-  scoped_ptr<V1HostAuthenticator> host_;
-  scoped_ptr<V1ClientAuthenticator> client_;
   scoped_ptr<FakeSocket> client_fake_socket_;
   scoped_ptr<FakeSocket> host_fake_socket_;
   scoped_ptr<ChannelAuthenticator> client_auth_;
@@ -166,41 +113,39 @@ class V1AuthenticatorTest : public testing::Test {
   scoped_ptr<net::StreamSocket> client_socket_;
   scoped_ptr<net::StreamSocket> host_socket_;
 
-  DISALLOW_COPY_AND_ASSIGN(V1AuthenticatorTest);
+  DISALLOW_COPY_AND_ASSIGN(SslHmacChannelAuthenticatorTest);
 };
 
-TEST_F(V1AuthenticatorTest, SuccessfulAuth) {
-  {
-    SCOPED_TRACE("RunAuthExchange");
-    InitAuthenticators(kTestSharedSecret, kTestSharedSecret);
-    RunAuthExchange();
-  }
-  ASSERT_EQ(Authenticator::ACCEPTED, host_->state());
-  ASSERT_EQ(Authenticator::ACCEPTED, client_->state());
+// Verify that a channel can be connected using a valid shared secret.
+TEST_F(SslHmacChannelAuthenticatorTest, SuccessfulAuth) {
+  client_auth_.reset(SslHmacChannelAuthenticator::CreateForClient(
+      host_cert_, kTestSharedSecret));
+  host_auth_.reset(SslHmacChannelAuthenticator::CreateForHost(
+      host_cert_, private_key_.get(), kTestSharedSecret));
 
-  client_auth_.reset(client_->CreateChannelAuthenticator());
-  host_auth_.reset(host_->CreateChannelAuthenticator());
   RunChannelAuth(false);
 
   EXPECT_TRUE(client_socket_.get() != NULL);
   EXPECT_TRUE(host_socket_.get() != NULL);
 
   StreamConnectionTester tester(host_socket_.get(), client_socket_.get(),
-                                kMessageSize, kMessages);
+                                100, 2);
 
   tester.Start();
   message_loop_.Run();
   tester.CheckResults();
 }
 
-// Verify that connection is rejected when secrets don't match.
-TEST_F(V1AuthenticatorTest, InvalidSecret) {
-  {
-    SCOPED_TRACE("RunAuthExchange");
-    InitAuthenticators(kTestSharedSecretBad, kTestSharedSecret);
-    RunAuthExchange();
-  }
-  ASSERT_EQ(Authenticator::REJECTED, host_->state());
+// Verify that channels cannot be using invalid shared secret.
+TEST_F(SslHmacChannelAuthenticatorTest, InvalidChannelSecret) {
+  client_auth_.reset(SslHmacChannelAuthenticator::CreateForClient(
+      host_cert_, kTestSharedSecretBad));
+  host_auth_.reset(SslHmacChannelAuthenticator::CreateForHost(
+      host_cert_, private_key_.get(), kTestSharedSecret));
+
+  RunChannelAuth(true);
+
+  EXPECT_TRUE(host_socket_.get() == NULL);
 }
 
 }  // namespace protocol

remoting/protocol/v1_authenticator.cc
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/rsa_private_key.h"
 #include "remoting/base/constants.h"
 #include "remoting/protocol/auth_util.h"
-#include "remoting/protocol/v1_client_channel_authenticator.h"
-#include "remoting/protocol/v1_host_channel_authenticator.h"
+#include "remoting/protocol/ssl_hmac_channel_authenticator.h"
 #include "third_party/libjingle/source/talk/xmllite/xmlelement.h"
 
 using buzz::QName;
@@ -79,7 +78,11 @@ XmlElement* V1ClientAuthenticator::GetNextMessage() {
 ChannelAuthenticator*
 V1ClientAuthenticator::CreateChannelAuthenticator() const {
   DCHECK_EQ(state_, ACCEPTED);
-  return new V1ClientChannelAuthenticator(remote_cert_, shared_secret_);
+  SslHmacChannelAuthenticator* result =
+      SslHmacChannelAuthenticator::CreateForClient(
+          remote_cert_, shared_secret_);
+  result->SetLegacyOneWayMode(SslHmacChannelAuthenticator::SEND_ONLY);
+  return result;
 };
 
 V1HostAuthenticator::V1HostAuthenticator(
@@ -135,8 +138,11 @@ XmlElement* V1HostAuthenticator::GetNextMessage() {
 ChannelAuthenticator*
 V1HostAuthenticator::CreateChannelAuthenticator() const {
   DCHECK_EQ(state_, ACCEPTED);
-  return new V1HostChannelAuthenticator(
-      local_cert_, local_private_key_.get(), shared_secret_);
+  SslHmacChannelAuthenticator* result =
+      SslHmacChannelAuthenticator::CreateForHost(
+          local_cert_, local_private_key_.get(), shared_secret_);
+  result->SetLegacyOneWayMode(SslHmacChannelAuthenticator::RECEIVE_ONLY);
+  return result;
 };
 
 V1HostAuthenticatorFactory::V1HostAuthenticatorFactory(

commit 017d6cc0bb33fad91eeedd05c20c1c5983118a73

Fix backward compatibility issue introduced in r115519.
BUG=105214

Review URL: http://codereview.chromium.org/9026015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115584 0039d316-1c4b-4281-b951-d872f2087c98

remoting/protocol/ssl_hmac_channel_authenticator.cc
@@ -134,7 +134,7 @@ void SslHmacChannelAuthenticator::OnConnected(int result) {
     // Generate authentication digest to write to the socket.
     std::string auth_bytes = GetAuthBytes(
         socket_.get(), is_ssl_server() ?
-        kClientAuthSslExporterLabel : kHostAuthSslExporterLabel, auth_key_);
+        kHostAuthSslExporterLabel : kClientAuthSslExporterLabel, auth_key_);
     if (auth_bytes.empty()) {
       done_callback_.Run(net::ERR_FAILED, NULL);
       return;
@@ -252,7 +252,7 @@ bool SslHmacChannelAuthenticator::VerifyAuthBytes(
   // Compute expected auth bytes.
   std::string auth_bytes = GetAuthBytes(
       socket_.get(), is_ssl_server() ?
-      kHostAuthSslExporterLabel : kClientAuthSslExporterLabel, auth_key_);
+      kClientAuthSslExporterLabel : kHostAuthSslExporterLabel, auth_key_);
   if (auth_bytes.empty())
     return false;
 