4d6b8e85c5dae5a337ab0d9f0033d9ad92b553b6
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 21:12:27 +0000

    Aura: Automatically use compact window mode on narrow displays
    We default to compact mode on displays with a width of 1366 (ZGB) and narrower.  Only do this when we're not running in a window, to avoid changing the default for Aura developers running on desktop workstations.
    
    BUG=108187
    TEST=added unit test
    
    Review URL: http://codereview.chromium.org/9018022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115606 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura_shell/shell.h
+++ ui/aura_shell/shell.h
@@ -11,11 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/task.h"
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/aura_shell/aura_shell_export.h"
+
+class CommandLine;
 
 namespace aura {
 class EventFilter;
@@ -24,6 +27,7 @@ class Window;
 }
 namespace gfx {
 class Rect;
+class Size;
 }
 
 namespace aura_shell {
@@ -94,12 +98,20 @@ class AURA_SHELL_EXPORT Shell {
   }
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ShellTest, DefaultToCompactWindowMode);
+
   typedef std::pair<aura::Window*, gfx::Rect> WindowAndBoundsPair;
 
   explicit Shell(ShellDelegate* delegate);
   virtual ~Shell();
 
   void Init();
+
+  // Returns true if the |monitor_size| is narrow and the user has not set
+  // an explicit window mode flag on the |command_line|.
+  bool DefaultToCompactWindowMode(const gfx::Size& monitor_size,
+                                  CommandLine* command_line) const;
+
   void InitLayoutManagers(aura::RootWindow* root_window);
 
   // Enables WorkspaceManager.
