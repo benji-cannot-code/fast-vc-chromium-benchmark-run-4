7a380279f45dfe2856bc0c874fb6c695e24d5e33
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 19:00:04 +0000

    Disk cache: Make sure that the index file is closed on the cache thread.
    BUG=108354
    TEST=net_unittests
    Review URL: http://codereview.chromium.org/9016029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115569 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.cc
+++ net/disk_cache/backend_impl.cc
@@ -561,6 +561,7 @@ void BackendImpl::CleanupCache() {
     }
   }
   block_files_.CloseFiles();
+  index_ = NULL;
   ptr_factory_.InvalidateWeakPtrs();
   done_.Signal();
 }
--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
 #include "base/threading/platform_thread.h"
+#include "base/threading/thread_restrictions.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 #include "net/base/test_completion_callback.h"
@@ -234,6 +235,19 @@ TEST_F(DiskCacheTest, CreateBackend) {
   }
 
   MessageLoop::current()->RunAllPending();
+}
+
+// Testst that re-creating the cache performs the expected cleanup.
+TEST_F(DiskCacheBackendTest, CreateBackend_MissingFile) {
+  ASSERT_TRUE(CopyTestCache("bad_entry"));
+  FilePath filename = cache_path_.AppendASCII("data_1");
+  file_util::Delete(filename, false);
+  DisableFirstCleanup();
+  SetForceCreation();
+
+  bool prev = base::ThreadRestrictions::SetIOAllowed(false);
+  InitCache();
+  base::ThreadRestrictions::SetIOAllowed(prev);
 }
 
 TEST_F(DiskCacheBackendTest, ExternalFiles) {
