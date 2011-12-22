134e8167739cd95f361a1e4dde717dbd3e306e0b
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 22:05:16 +0000

    Refactor ProfileInfoCacheObserver interface and usage thereof.
    We want to simplify OnProfileAdded and OnProfileAvatarChanged so decouple the Observer interface a bit away from ProfileShortcutManagerWin. Instead, classes that implement the interface should fetch the global cache object and retrieve any additional information it needs from there when they are notified.
    
    BUG=none
    TEST=Ensure that ProfileInfoCacheTest and BackgroundModeManagerTest unit tests all pass.
    
    Review URL: http://codereview.chromium.org/9020013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/background/background_mode_manager.cc
+++ chrome/browser/background/background_mode_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_info_cache.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/status_icons/status_icon.h"
 #include "chrome/browser/status_icons/status_tray.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -387,10 +388,11 @@ void BackgroundModeManager::OnApplicationListChanged(Profile* profile) {
 
 ///////////////////////////////////////////////////////////////////////////////
 //  BackgroundModeManager, ProfileInfoCacheObserver overrides
-void BackgroundModeManager::OnProfileAdded(const string16& profile_name,
-                                           const string16& profile_base_dir,
-                                           const FilePath& profile_path,
-                                           const gfx::Image* avatar_image) {
+void BackgroundModeManager::OnProfileAdded(const FilePath& profile_path) {
+  ProfileInfoCache& cache =
+      g_browser_process->profile_manager()->GetProfileInfoCache();
+  string16 profile_name = cache.GetNameOfProfileAtIndex(
+      cache.GetIndexOfProfileWithPath(profile_path));
   // At this point, the profile should be registered with the background mode
   // manager, but when it's actually added to the cache is when its name is
   // set so we need up to update that with the background_mode_data.
@@ -407,7 +409,11 @@ void BackgroundModeManager::OnProfileAdded(const string16& profile_name,
 }
 
 void BackgroundModeManager::OnProfileWillBeRemoved(
-    const string16& profile_name) {
+    const FilePath& profile_path) {
+  ProfileInfoCache& cache =
+      g_browser_process->profile_manager()->GetProfileInfoCache();
+  string16 profile_name = cache.GetNameOfProfileAtIndex(
+      cache.GetIndexOfProfileWithPath(profile_path));
   // Remove the profile from our map of profiles.
   BackgroundModeInfoMap::iterator it =
       GetBackgroundModeIterator(profile_name);
@@ -418,12 +424,18 @@ void BackgroundModeManager::OnProfileWillBeRemoved(
   }
 }
 
-void BackgroundModeManager::OnProfileWasRemoved(const string16& profile_name) {
+void BackgroundModeManager::OnProfileWasRemoved(
+    const FilePath& profile_path,
+    const string16& profile_name) {
 }
 
 void BackgroundModeManager::OnProfileNameChanged(
-    const string16& old_profile_name,
-    const string16& new_profile_name) {
+    const FilePath& profile_path,
+    const string16& old_profile_name) {
+  ProfileInfoCache& cache =
+      g_browser_process->profile_manager()->GetProfileInfoCache();
+  string16 new_profile_name = cache.GetNameOfProfileAtIndex(
+      cache.GetIndexOfProfileWithPath(profile_path));
   BackgroundModeInfoMap::const_iterator it =
       GetBackgroundModeIterator(old_profile_name);
   // We check that the returned iterator is valid due to unittests, but really
@@ -436,10 +448,7 @@ void BackgroundModeManager::OnProfileNameChanged(
 }
 
 void BackgroundModeManager::OnProfileAvatarChanged(
-    const string16& profile_name,
-    const string16& profile_base_dir,
-    const FilePath& profile_path,
-    const gfx::Image* avatar_image) {
+    const FilePath& profile_path) {
 
 }
 ///////////////////////////////////////////////////////////////////////////////
--- chrome/browser/profiles/profile_info_cache.cc
+++ chrome/browser/profiles/profile_info_cache.cc
@@ -207,14 +207,9 @@ void ProfileInfoCache::AddProfileToCache(const FilePath& profile_path,
 
   sorted_keys_.insert(FindPositionForProfile(key, name), key);
 
-  gfx::Image& avatar_img =
-      ResourceBundle::GetSharedInstance().GetNativeImageNamed(
-          GetDefaultAvatarIconResourceIDAtIndex(icon_index));
-
   FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                     observer_list_,
-                    OnProfileAdded(name, UTF8ToUTF16(key),
-                                   profile_path, &avatar_img));
+                    OnProfileAdded(profile_path));
 
   content::NotificationService::current()->Notify(
       chrome::NOTIFICATION_PROFILE_CACHED_INFO_CHANGED,
@@ -236,7 +231,7 @@ void ProfileInfoCache::DeleteProfileFromCache(const FilePath& profile_path) {
 
   FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                     observer_list_,
-                    OnProfileWillBeRemoved(name));
+                    OnProfileWillBeRemoved(profile_path));
 
   DictionaryPrefUpdate update(prefs_, prefs::kProfileInfoCache);
   DictionaryValue* cache = update.Get();
@@ -246,7 +241,7 @@ void ProfileInfoCache::DeleteProfileFromCache(const FilePath& profile_path) {
 
   FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                     observer_list_,
-                    OnProfileWasRemoved(name));
+                    OnProfileWasRemoved(profile_path, name));
 
   content::NotificationService::current()->Notify(
       chrome::NOTIFICATION_PROFILE_CACHED_INFO_CHANGED,
@@ -415,12 +410,13 @@ void ProfileInfoCache::SetNameOfProfileAtIndex(size_t index,
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
   string16 new_display_name = GetNameOfProfileAtIndex(index);
+  FilePath profile_path = GetPathOfProfileAtIndex(index);
   UpdateSortForProfileIndex(index);
 
   if (old_display_name != new_display_name) {
     FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                       observer_list_,
-                      OnProfileNameChanged(old_display_name, new_display_name));
+                      OnProfileNameChanged(profile_path, old_display_name));
   }
 }
 
@@ -442,17 +438,10 @@ void ProfileInfoCache::SetAvatarIconOfProfileAtIndex(size_t index,
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
 
-  string16 name = GetNameOfProfileAtIndex(index);
   FilePath profile_path = GetPathOfProfileAtIndex(index);
-  std::string key = CacheKeyFromProfilePath(profile_path);
-  gfx::Image& avatar_img =
-      ResourceBundle::GetSharedInstance().GetNativeImageNamed(
-          GetDefaultAvatarIconResourceIDAtIndex(icon_index));
-
   FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                     observer_list_,
-                    OnProfileAvatarChanged(name, UTF8ToUTF16(key),
-                                           profile_path, &avatar_img));
+                    OnProfileAvatarChanged(profile_path));
 }
 
 void ProfileInfoCache::SetBackgroundStatusOfProfileAtIndex(
@@ -477,12 +466,13 @@ void ProfileInfoCache::SetGAIANameOfProfileAtIndex(size_t index,
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
   string16 new_display_name = GetNameOfProfileAtIndex(index);
+  FilePath profile_path = GetPathOfProfileAtIndex(index);
   UpdateSortForProfileIndex(index);
 
   if (old_display_name != new_display_name) {
     FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                       observer_list_,
-                      OnProfileNameChanged(old_display_name, new_display_name));
+                      OnProfileNameChanged(profile_path, old_display_name));
   }
 }
 
@@ -497,12 +487,13 @@ void ProfileInfoCache::SetIsUsingGAIANameOfProfileAtIndex(size_t index,
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
   string16 new_display_name = GetNameOfProfileAtIndex(index);
+  FilePath profile_path = GetPathOfProfileAtIndex(index);
   UpdateSortForProfileIndex(index);
 
   if (old_display_name != new_display_name) {
     FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                       observer_list_,
-                      OnProfileNameChanged(old_display_name, new_display_name));
+                      OnProfileNameChanged(profile_path, old_display_name));
   }
 }
 
@@ -553,34 +544,23 @@ void ProfileInfoCache::SetGAIAPictureOfProfileAtIndex(size_t index,
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
 
-  string16 name = GetNameOfProfileAtIndex(index);
-  const gfx::Image& avatar_image = GetAvatarIconOfProfileAtIndex(index);
   FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
                     observer_list_,
-                    OnProfileAvatarChanged(name, UTF8ToUTF16(key),
-                                           path, &avatar_image));
+                    OnProfileAvatarChanged(path));
 }
 
 void ProfileInfoCache::SetIsUsingGAIAPictureOfProfileAtIndex(size_t index,
                                                              bool value) {
   scoped_ptr<DictionaryValue> info(GetInfoForProfileAtIndex(index)->DeepCopy());
-  string16 name = GetNameOfProfileAtIndex(index);
   info->SetBoolean(kUseGAIAPictureKey, value);
   // This takes ownership of |info|.
   SetInfoForProfileAtIndex(index, info.release());
 
   // Retrieve some info to update observers who care about avatar changes.
-  if (value) {
-    FilePath profile_path = GetPathOfProfileAtIndex(index);
-    std::string key = CacheKeyFromProfilePath(profile_path);
-    if (gaia_pictures_.find(key) != gaia_pictures_.end()) {
-      FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
-                        observer_list_,
-                        OnProfileAvatarChanged(name, UTF8ToUTF16(key),
-                                               profile_path,
-                                               gaia_pictures_[key]));
-    }
-  }
+  FilePath profile_path = GetPathOfProfileAtIndex(index);
+  FOR_EACH_OBSERVER(ProfileInfoCacheObserver,
+                    observer_list_,
+                    OnProfileAvatarChanged(profile_path));
 }
 
 string16 ProfileInfoCache::ChooseNameForNewProfile(size_t icon_index) {
