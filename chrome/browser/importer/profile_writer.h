40428afe72c4e07d813ab307c0a5e670b77b3a94
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-22 05:45:46 +0000

    Bookmark import from IE preserving the order.
    This change is to read the order information of IE Favorites from registry and sort the imported bookmarks in that order. If it failed to read the information from registry, it falls back to the current implementation: alphabetical sorting.
    
    BUG=4962
    TEST=unit_tests --gtest_filter=*Importer*
    
    Review URL: http://codereview.chromium.org/8970022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115489 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/profile_writer.h
+++ chrome/browser/importer/profile_writer.h
@@ -37,6 +37,7 @@ class ProfileWriter : public base::RefCountedThreadSafe<ProfileWriter> {
   struct BookmarkEntry {
     BookmarkEntry();
     ~BookmarkEntry();
+    bool operator==(const BookmarkEntry& other) const;
 
     bool in_toolbar;
     bool is_folder;
