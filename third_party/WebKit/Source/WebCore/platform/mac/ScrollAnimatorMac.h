ee8e20d823f3d151ca313dc3d319042b51609478
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-22 21:08:42 +0000

    Move some member variables out of ScrollElasticityController https://bugs.webkit.org/show_bug.cgi?id=75115
    Reviewed by Adam Roben.
    
    Move a couple of member variables that aren't related to rubberbanding out from
    ScrollElasticityController and back into ScrollAnimatorMac and remove now unneeded
    ScrollElasticityControllerClient member functions as well.
    
    * platform/mac/ScrollAnimatorMac.h:
    * platform/mac/ScrollAnimatorMac.mm:
    (WebCore::ScrollAnimatorMac::ScrollAnimatorMac):
    (WebCore::ScrollAnimatorMac::handleWheelEvent):
    (WebCore::ScrollAnimatorMac::beginScrollGesture):
    * platform/mac/ScrollElasticityController.h:
    * platform/mac/ScrollElasticityController.mm:
    (WebCore::ScrollElasticityController::ScrollElasticityController):
    (WebCore::ScrollElasticityController::beginScrollGesture):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.h
@@ -134,8 +134,6 @@ private:
 
 #if ENABLE(RUBBER_BANDING)
     /// ScrollElasticityControllerClient member functions.
-    virtual bool isHorizontalScrollerPinnedToMinimumPosition() OVERRIDE;
-    virtual bool isHorizontalScrollerPinnedToMaximumPosition() OVERRIDE;
     virtual IntSize stretchAmount() OVERRIDE;
     virtual void startSnapRubberbandTimer() OVERRIDE;
     virtual void stopSnapRubberbandTimer() OVERRIDE;
@@ -151,6 +149,11 @@ private:
 
     ScrollElasticityController m_scrollElasticityController;
     Timer<ScrollAnimatorMac> m_snapRubberBandTimer;
+
+    bool m_scrollerInitiallyPinnedOnLeft;
+    bool m_scrollerInitiallyPinnedOnRight;
+    int m_cumulativeHorizontalScroll;
+    bool m_didCumulativeHorizontalScrollEverSwitchToOppositeDirectionOfPin;
 #endif
 
     bool m_haveScrolledSincePageLoad;
