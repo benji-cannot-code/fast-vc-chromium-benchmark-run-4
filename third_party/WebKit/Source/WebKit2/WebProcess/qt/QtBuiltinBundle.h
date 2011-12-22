b30d2044a81e6cf217bcf18a65e5bf5557d0a06d
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-22 21:17:49 +0000

    [Qt] [WK2] Use HashMap instead of Vector in the builtin bundle https://bugs.webkit.org/show_bug.cgi?id=75112
    Reviewed by Andreas Kling.
    
    Fix my own FIXME now that HashMap have support to hold OwnPtr as value.
    
    * WebProcess/qt/QtBuiltinBundle.cpp:
    (WebKit::QtBuiltinBundle::didCreatePage):
    (WebKit::QtBuiltinBundle::willDestroyPage):
    (WebKit::QtBuiltinBundle::handleMessageToNavigatorQtObject):
    (WebKit::QtBuiltinBundle::handleSetNavigatorQtObjectEnabled):
    * WebProcess/qt/QtBuiltinBundle.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/qt/QtBuiltinBundle.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/qt/QtBuiltinBundle.h
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WKBundle.h"
 #include "WKBundlePage.h"
+#include <wtf/HashMap.h>
 #include <wtf/OwnPtr.h>
-#include <wtf/Vector.h>
 
 namespace WebKit {
 
@@ -56,13 +56,10 @@ public:
     void didReceiveMessage(WKStringRef messageName, WKTypeRef messageBody);
 
 private:
-    QtBuiltinBundlePage* bundlePageForPageRef(const WKBundlePageRef) const;
-
     void handleMessageToNavigatorQtObject(WKTypeRef messageBody);
     void handleSetNavigatorQtObjectEnabled(WKTypeRef messageBody);
 
-    // FIXME: Can we use HashMap of OwnPtrs? Or something equivalent?
-    Vector<OwnPtr<QtBuiltinBundlePage> > m_pages;
+    HashMap<WKBundlePageRef, OwnPtr<QtBuiltinBundlePage> > m_pages;
     WKBundleRef m_bundle;
 };
 
