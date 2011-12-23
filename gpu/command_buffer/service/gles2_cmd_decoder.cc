e59342eb311105e40a54ddad71f5249526ba71a5
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-23 13:12:14 +0000

    Add tracking of backbuffer memory to command buffer
    TEST=none
    BUG=79762
    
    R=apatrick@chromium.org
    
    
    Review URL: http://codereview.chromium.org/8958027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115715 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops.h"
 #include "base/at_exit.h"
 #include "base/bind.h"
+#include "base/debug/trace_event.h"
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_cftyperef.h"
 #endif
@@ -26,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/gles2_cmd_format.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
 #include "gpu/command_buffer/common/id_allocator.h"
-#include "gpu/command_buffer/common/trace_event.h"
 #include "gpu/command_buffer/service/buffer_manager.h"
 #include "gpu/command_buffer/service/cmd_buffer_engine.h"
 #include "gpu/command_buffer/service/context_group.h"
@@ -307,10 +307,15 @@ class Texture {
     return size_;
   }
 
+  size_t estimated_size() const {
+    return estimated_size_;
+  }
+
  private:
   GLES2DecoderImpl* decoder_;
   GLuint id_;
   gfx::Size size_;
+  size_t estimated_size_;
   DISALLOW_COPY_AND_ASSIGN(Texture);
 };
 
@@ -338,9 +343,14 @@ class RenderBuffer {
     return id_;
   }
 
+  size_t estimated_size() const {
+    return estimated_size_;
+  }
+
  private:
   GLES2DecoderImpl* decoder_;
   GLuint id_;
+  size_t estimated_size_;
   DISALLOW_COPY_AND_ASSIGN(RenderBuffer);
 };
 
@@ -547,6 +557,7 @@ class GLES2DecoderImpl : public base::SupportsWeakPtr<GLES2DecoderImpl>,
  private:
   friend class ScopedGLErrorSuppressor;
   friend class ScopedResolvedFrameBufferBinder;
+  friend class Texture;
   friend class RenderBuffer;
   friend class FrameBuffer;
 
@@ -1290,6 +1301,10 @@ class GLES2DecoderImpl : public base::SupportsWeakPtr<GLES2DecoderImpl>,
       error::Error* error, GLuint* service_id, void** result,
       GLenum* result_type);
 
+  // Computes the estimated memory used for the backbuffer and passes it to
+  // the tracing system.
+  void UpdateBackbufferMemoryAccounting();
+
   // Returns true if the context was just lost due to e.g. GL_ARB_robustness.
   bool WasContextLost();
 
@@ -1557,7 +1572,6 @@ ScopedResolvedFrameBufferBinder::ScopedResolvedFrameBufferBinder(
       DCHECK(decoder_->offscreen_saved_color_format_);
       decoder_->offscreen_resolved_color_texture_->AllocateStorage(
           decoder_->offscreen_size_, decoder_->offscreen_saved_color_format_);
-
       decoder_->offscreen_resolved_frame_buffer_->AttachRenderTexture(
           decoder_->offscreen_resolved_color_texture_.get());
       if (decoder_->offscreen_resolved_frame_buffer_->CheckStatus() !=
@@ -1598,7 +1612,8 @@ ScopedResolvedFrameBufferBinder::~ScopedResolvedFrameBufferBinder() {
 
 Texture::Texture(GLES2DecoderImpl* decoder)
     : decoder_(decoder),
-      id_(0) {
+      id_(0),
+      estimated_size_(0) {
 }
 
 Texture::~Texture() {
@@ -1626,6 +1641,8 @@ void Texture::Create() {
   // crash.
   glTexImage2D(
       GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+  estimated_size_ = 16u * 16u * 4u;
+  decoder_->UpdateBackbufferMemoryAccounting();
 }
 
 bool Texture::AllocateStorage(const gfx::Size& size, GLenum format) {
@@ -1645,7 +1662,15 @@ bool Texture::AllocateStorage(const gfx::Size& size, GLenum format) {
 
   size_ = size;
 
-  return glGetError() == GL_NO_ERROR;
+  bool success = glGetError() == GL_NO_ERROR;
+  if (success) {
+    uint32 image_size = 0;
+    GLES2Util::ComputeImageDataSize(
+        size.width(), size.height(), format, GL_UNSIGNED_BYTE, 4, &image_size);
+    estimated_size_ = image_size;
+    decoder_->UpdateBackbufferMemoryAccounting();
+  }
+  return success;
 }
 
 void Texture::Copy(const gfx::Size& size, GLenum format) {
@@ -1666,6 +1691,8 @@ void Texture::Destroy() {
     ScopedGLErrorSuppressor suppressor(decoder_);
     glDeleteTextures(1, &id_);
     id_ = 0;
+    estimated_size_ = 0;
+    decoder_->UpdateBackbufferMemoryAccounting();
   }
 }
 
@@ -1675,7 +1702,8 @@ void Texture::Invalidate() {
 
 RenderBuffer::RenderBuffer(GLES2DecoderImpl* decoder)
     : decoder_(decoder),
-      id_(0) {
+      id_(0),
+      estimated_size_(0) {
 }
 
 RenderBuffer::~RenderBuffer() {
@@ -1715,7 +1743,13 @@ bool RenderBuffer::AllocateStorage(const gfx::Size& size, GLenum format,
                                           size.height());
     }
   }
-  return glGetError() == GL_NO_ERROR;
+  bool success = glGetError() == GL_NO_ERROR;
+  if (success) {
+    estimated_size_ = size.width() * size.height() * samples *
+                      GLES2Util::RenderbufferBytesPerPixel(format);
+    decoder_->UpdateBackbufferMemoryAccounting();
+  }
+  return success;
 }
 
 void RenderBuffer::Destroy() {
@@ -1723,6 +1757,8 @@ void RenderBuffer::Destroy() {
     ScopedGLErrorSuppressor suppressor(decoder_);
     glDeleteRenderbuffersEXT(1, &id_);
     id_ = 0;
+    estimated_size_ = 0;
+    decoder_->UpdateBackbufferMemoryAccounting();
   }
 }
 
@@ -2764,6 +2800,36 @@ bool GLES2DecoderImpl::SetParent(GLES2Decoder* new_parent,
   return true;
 }
 
+void GLES2DecoderImpl::UpdateBackbufferMemoryAccounting() {
+  size_t total = 0;
+  if (offscreen_target_frame_buffer_.get()) {
+    if (offscreen_target_color_texture_.get()) {
+        total += offscreen_target_color_texture_->estimated_size();
+    }
+    if (offscreen_target_color_render_buffer_.get()) {
+        total += offscreen_target_color_render_buffer_->estimated_size();
+    }
+    if (offscreen_target_depth_render_buffer_.get()) {
+        total += offscreen_target_depth_render_buffer_->estimated_size();
+    }
+    if (offscreen_target_stencil_render_buffer_.get()) {
+        total += offscreen_target_stencil_render_buffer_->estimated_size();
+    }
+    if (offscreen_saved_color_texture_.get()) {
+        total += offscreen_saved_color_texture_->estimated_size();
+    }
+    if (offscreen_resolved_color_texture_.get()) {
+        total += offscreen_resolved_color_texture_->estimated_size();
+    }
+  } else {
+    gfx::Size size = surface_->GetSize();
+    total += size.width() * size.height() *
+        GLES2Util::RenderbufferBytesPerPixel(back_buffer_color_format_);
+  }
+  TRACE_COUNTER_ID1(
+      "GLES2DecoderImpl", "BackbufferMemory", this, total);
+}
+
 bool GLES2DecoderImpl::ResizeOffscreenFrameBuffer(const gfx::Size& size) {
   bool is_offscreen = !!offscreen_target_frame_buffer_.get();
   if (!is_offscreen) {
@@ -2818,6 +2884,7 @@ bool GLES2DecoderImpl::ResizeOffscreenFrameBuffer(const gfx::Size& size) {
                << "to allocate storage for offscreen target stencil buffer.";
     return false;
   }
+  UpdateBackbufferMemoryAccounting();
 
   // Attach the offscreen target buffers to the target frame buffer.
   if (IsOffscreenBufferMultisampled()) {
@@ -2901,6 +2968,8 @@ error::Error GLES2DecoderImpl::HandleResizeCHROMIUM(
     if (!context_->IsCurrent(surface_.get()))
       return error::kLostContext;
   }
+
+  UpdateBackbufferMemoryAccounting();
 
   return error::kNoError;
 }
@@ -7291,6 +7360,7 @@ error::Error GLES2DecoderImpl::HandleSwapBuffers(
       DCHECK(offscreen_saved_color_format_);
       offscreen_saved_color_texture_->AllocateStorage(
           offscreen_size_, offscreen_saved_color_format_);
+      UpdateBackbufferMemoryAccounting();
 
       offscreen_saved_frame_buffer_->AttachRenderTexture(
           offscreen_saved_color_texture_.get());
