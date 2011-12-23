949461349df059b03a14721ce001b04f448f3cb1
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-23 21:34:34 +0000

    base::Bind: Remove an unused using statement.
    BUG=none
    TEST=none
    R=groby
    
    Review URL: http://codereview.chromium.org/9035013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115738 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/test_completion_callback_unittest.cc
+++ net/base/test_completion_callback_unittest.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 typedef PlatformTest TestCompletionCallbackTest;
 
-using net::OldCompletionCallback;
-
 const int kMagicResult = 8888;
 
 // ExampleEmployer is a toy version of HostResolver
