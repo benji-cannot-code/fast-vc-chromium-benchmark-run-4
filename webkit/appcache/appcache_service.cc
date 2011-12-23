6985bd9d1104cc42b6ca638f0d23f3ca386ce31e
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-23 17:06:06 +0000

    Cleanup: Remove unused DeferredCallCallback.
    BUG=none
    TEST=none
    R=michaeln
    
    Review URL: http://codereview.chromium.org/9023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115724 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_service.cc
+++ webkit/appcache/appcache_service.cc
@@ -66,11 +66,6 @@ class AppCacheService::NewAsyncHelper
     callback_.Reset();
   }
 
-  static void DeferredCallCallback(const net::CompletionCallback& callback,
-                                   int rv) {
-    callback.Run(rv);
-  }
-
   AppCacheService* service_;
   net::CompletionCallback callback_;
 };
