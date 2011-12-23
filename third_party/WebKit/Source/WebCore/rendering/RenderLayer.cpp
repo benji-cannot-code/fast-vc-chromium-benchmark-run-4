f4e8b9c55920259ce387fdcb0f9491aeb8948f90
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-23 01:08:56 +0000

    Crash and incorrect behavior when switching between hardware and software CSS filters https://bugs.webkit.org/show_bug.cgi?id=75130
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Test: css3/filters/crash-hw-sw-switch.html
    
    Backing store on layer gets fixed up (added or removed) after style change is evaluated
    so the state of the m_filter variable might not match the current filter state. Added
    updateOrRemoveFilterEffect() call to ensureBacking() and clearBacking() to get the
    m_filter property in the right state. Also added an ASSERT() where the crash was
    occuring.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::paintLayer):
    (WebCore::RenderLayer::ensureBacking):
    (WebCore::RenderLayer::clearBacking):
    
    LayoutTests:
    
    * css3/filters/crash-hw-sw-switch-expected.txt: Added.
    * css3/filters/crash-hw-sw-switch.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -2783,6 +2783,8 @@ void RenderLayer::paintLayer(RenderLayer* rootLayer, GraphicsContext* p,
     
 #if ENABLE(CSS_FILTERS)
     if (paintsWithFilters() && !(paintFlags & PaintLayerAppliedFilters)) {
+        ASSERT(m_filter);
+        
         // Update the filter's image if necessary.
         // The filter is always built at this point.
         LayoutRect filterRect = transparencyClipBox(this, rootLayer, paintBehavior);
@@ -3938,6 +3940,10 @@ RenderLayerBacking* RenderLayer::ensureBacking()
     if (!m_backing) {
         m_backing = adoptPtr(new RenderLayerBacking(this));
         compositor()->layerBecameComposited(this);
+
+#if ENABLE(CSS_FILTERS)
+        updateOrRemoveFilterEffect();
+#endif
     }
     return m_backing.get();
 }
@@ -3947,6 +3953,11 @@ void RenderLayer::clearBacking()
     if (m_backing && !renderer()->documentBeingDestroyed())
         compositor()->layerBecameNonComposited(this);
     m_backing.clear();
+
+#if ENABLE(CSS_FILTERS)
+    updateOrRemoveFilterEffect();
+#endif
+    
 }
 
 bool RenderLayer::hasCompositedMask() const
