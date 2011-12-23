d7a1d8d077ba69a5a0d24008baca3a25fdfa7291
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-23 22:33:58 +0000

    CSSParser: Avoid creating dummy declaration in parseColor() slow path. <http://webkit.org/b/75104>
    Patch by Andreas Kling <kling@webkit.org> on 2011-12-22
    Reviewed by Darin Adler.
    
    We only needed the dummy declaration to trigger the instantiation of
    a CSSValuePool. Added an ensureCSSValuePool() method and have parseColor()
    call that instead.
    
    Also renamed the fast-path parseColor() to fastParseColor() and reordered
    the arguments for consistency with the slow-path parseColor().
    
    * css/CSSParser.cpp:
    (WebCore::parseColorValue):
    (WebCore::CSSParser::parseColor):
    (WebCore::CSSParser::ensureCSSValuePool):
    (WebCore::CSSParser::fastParseColor):
    (WebCore::CSSParser::parseColorFromValue):
    * css/CSSParser.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103639 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.h
+++ third_party/WebKit/Source/WebCore/css/CSSParser.h
@@ -70,7 +70,6 @@ public:
     static bool parseColor(RGBA32& color, const String&, bool strict = false);
     static bool parseSystemColor(RGBA32& color, const String&, Document*);
     PassRefPtr<CSSPrimitiveValue> parseValidPrimitive(int propId, CSSParserValue*);
-    bool parseColor(CSSMutableStyleDeclaration*, const String&);
     bool parseDeclaration(CSSMutableStyleDeclaration*, const String&, RefPtr<CSSStyleSourceData>* = 0, CSSStyleSheet* contextStyleSheet = 0);
     bool parseMediaQuery(MediaList*, const String&);
 
@@ -152,7 +151,7 @@ public:
     bool parseColorFromValue(CSSParserValue*, RGBA32&);
     void parseSelector(const String&, Document* doc, CSSSelectorList&);
 
-    static bool parseColor(const String&, RGBA32& rgb, bool strict);
+    static bool fastParseColor(RGBA32&, const String&, bool strict);
 
     bool parseFontStyle(bool important);
     bool parseFontVariant(bool important);
@@ -318,6 +317,7 @@ public:
 
 private:
     void setStyleSheet(CSSStyleSheet*);
+    void ensureCSSValuePool();
 
     void recheckAtKeyword(const UChar* str, int len);
 
@@ -348,6 +348,8 @@ private:
 
     bool parseFontFaceSrcURI(CSSValueList*);
     bool parseFontFaceSrcLocal(CSSValueList*);
+
+    bool parseColor(const String&);
 
     UChar* m_data;
     UChar* yytext;
