0b00245dc462aa7c790a2e3fbc59b881c72eca7a
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-23 23:47:00 +0000

    Provide option to disable Mac OS 10.7 application resume when using {debug, run}-{safari, minibrowser, test-runner, test-webkit-api}, and run-webkit-app https://bugs.webkit.org/show_bug.cgi?id=66902
    Reviewed by David Kilzer.
    
    Add an optional command line argument --no-saved-state to disable
    application resume (state restoration) for the session on Mac OS 10.7.
    
    For instance, when debugging an issue in Safari it is useful to be able
    to temporarily disable application resume. Currently you can disable
    application resume when launching Safari from within Xcode. We should
    expose a similar option in our command line tools.
    
    * Scripts/debug-minibrowser: Call printHelpAndExitForRunAndDebugWebKitAppIfNeeded()
    to print a help message and exit() if the command line argument --help was given.
    * Scripts/debug-safari: Ditto.
    * Scripts/debug-test-runner: Ditto.
    * Scripts/run-minibrowser: Ditto.
    * Scripts/run-safari: Ditto.
    * Scripts/run-test-runner: Ditto.
    * Scripts/run-test-webkit-api: Ditto.
    * Scripts/run-webkit-app: Ditto.
    * Scripts/webkitdirs.pm:
    (printHelpAndExitForRunAndDebugWebKitAppIfNeeded): Added.
    (argumentsForRunAndDebugMacWebKitApp): Added.
    (runMacWebKitApp): If the --no-saved-state command line argument was given then
    disable application resume for the session.
    (execMacWebKitAppForDebugging): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -58,6 +58,7 @@ BEGIN {
        &currentSVNRevision
        &debugSafari
        &passedConfiguration
+       &printHelpAndExitForRunAndDebugWebKitAppIfNeeded
        &productDir
        &runMacWebKitApp
        &safariPath
@@ -2252,6 +2253,24 @@ sub setPathForRunningWebKitApp
     }
 }
 
+sub printHelpAndExitForRunAndDebugWebKitAppIfNeeded()
+{
+    return unless checkForArgumentAndRemoveFromARGV("--help");
+    print STDERR <<EOF;
+Usage: @{[basename($0)]} [options] [args ...]
+  --help                Show this help message
+  --no-saved-state      Disable application resume for the session on Mac OS 10.7
+EOF
+    exit(1);
+}
+
+sub argumentsForRunAndDebugMacWebKitApp()
+{
+    my @args = @ARGV;
+    push @args, ("-ApplePersistenceIgnoreState", "YES") if isLion() && checkForArgumentAndRemoveFromArrayRef("--no-saved-state", \@args);
+    return @args;
+}
+
 sub runMacWebKitApp($;$)
 {
     my ($appPath, $useOpenCommand) = @_;
@@ -2260,12 +2279,12 @@ sub runMacWebKitApp($;$)
     $ENV{DYLD_FRAMEWORK_PATH} = $productDir;
     $ENV{WEBKIT_UNSET_DYLD_FRAMEWORK_PATH} = "YES";
     if (defined($useOpenCommand) && $useOpenCommand == USE_OPEN_COMMAND) {
-        return system("open", "-W", "-a", $appPath, "--args", @ARGV);
+        return system("open", "-W", "-a", $appPath, "--args", argumentsForRunAndDebugMacWebKitApp());
     }
     if (architecture()) {
-        return system "arch", "-" . architecture(), $appPath, @ARGV;
+        return system "arch", "-" . architecture(), $appPath, argumentsForRunAndDebugMacWebKitApp();
     }
-    return system { $appPath } $appPath, @ARGV;
+    return system { $appPath } $appPath, argumentsForRunAndDebugMacWebKitApp();
 }
 
 sub execMacWebKitAppForDebugging($)
@@ -2280,7 +2299,7 @@ sub execMacWebKitAppForDebugging($)
     $ENV{DYLD_FRAMEWORK_PATH} = $productDir;
     $ENV{WEBKIT_UNSET_DYLD_FRAMEWORK_PATH} = "YES";
     my @architectureFlags = ("-arch", architecture());
-    exec { $gdbPath } $gdbPath, @architectureFlags, $appPath, @ARGV or die;
+    exec { $gdbPath } $gdbPath, @architectureFlags, $appPath, argumentsForRunAndDebugMacWebKitApp() or die;
 }
 
 sub debugSafari
