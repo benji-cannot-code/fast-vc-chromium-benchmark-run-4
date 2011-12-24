e32195c1998e5bf4fec8125bd0e23535c6888d04
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-24 02:14:39 +0000

    views: Fix comments refering to Window instead of Widget.
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/9020003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/native_dialog_window.h
+++ chrome/browser/chromeos/native_dialog_window.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,7 +23,7 @@ enum NativeDialogFlags {
   DIALOG_FLAG_MODAL      = 0x02,  // For modal dialog.
 };
 
-// Shows a |native_dialog| hosted in a views::Window. |flags| are combinations
+// Shows a |native_dialog| hosted in a views::Widget. |flags| are combinations
 // of the NativeDialogFlags. |size| is a default size. Zero width/height of
 // |size| means let gtk choose a proper size for that dimension. |min_size| is
 // the minimum size of the final host Window.
