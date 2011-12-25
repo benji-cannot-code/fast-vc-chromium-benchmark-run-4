129080c479fea61f7200d8ad8174572d4cfc37cf
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-25 04:39:30 +0000

    Fix windows component build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115768 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents.h
+++ content/browser/tab_contents/tab_contents.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
@@ -49,11 +50,12 @@ namespace webkit_glue {
 struct WebIntentData;
 }
 
-class CONTENT_EXPORT TabContents : public content::WebContents,
-                                   public PageNavigator,
-                                   public RenderViewHostDelegate,
-                                   public RenderViewHostManager::Delegate,
-                                   public content::JavaScriptDialogDelegate {
+class CONTENT_EXPORT TabContents
+    : public NON_EXPORTED_BASE(content::WebContents),
+      public PageNavigator,
+      public RenderViewHostDelegate,
+      public RenderViewHostManager::Delegate,
+      public content::JavaScriptDialogDelegate {
  public:
   // Flags passed to the WebContentsDelegate.NavigationStateChanged to tell it
   // what has changed. Combine them to update more than one thing.
