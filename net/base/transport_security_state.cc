8f7239b20d71add935975acac3d6bc708681e345
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-27 19:38:03 +0000

    Simple change to simplify TransportSecurityState.
    Remove two no-op (i.e. always true) boolean functions, each of which was
    only used once. Having a DomainState at all is equivalent to having a
    DomainState and ShouldCertificateErrorsBeFatal /
    ShouldMixedScriptingBeBlocked.
    
    The reason to simplify TransportSecurityState is that agl and I want to
    re-factor this class because it is getting too confusing. Doing some
    straightforward clean-up now will help accelerate the real re-factoring.
    Review URL: http://codereview.chromium.org/9019034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115839 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/transport_security_state.cc
+++ net/base/transport_security_state.cc
@@ -1390,19 +1390,9 @@ bool TransportSecurityState::DomainState::IsChainOfPublicKeysPermitted(
   return false;
 }
 
-bool TransportSecurityState::DomainState::ShouldCertificateErrorsBeFatal()
-    const {
-  return true;
-}
-
 bool TransportSecurityState::DomainState::ShouldRedirectHTTPToHTTPS()
     const {
   return mode == MODE_STRICT;
-}
-
-bool TransportSecurityState::DomainState::ShouldMixedScriptingBeBlocked()
-    const {
-  return true;
 }
 
 }  // namespace
