d3b7cf2c5846e406e94fdb41204e962e6459a4ad
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-27 17:44:43 +0000

    Link rel=prefetch was disabled during prerender development; for now let's turn it back on and see if it's a useful intermediate between preconnect and prerendering.
    This is the first of two CLs, this one renames the existing field trial, the next CL will actually flip the switch and add prefetch historgrams.
    
    BUG=107897
    
    
    Review URL: http://codereview.chromium.org/8965032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115832 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/prerender/prerender_helper.cc
+++ chrome/renderer/prerender/prerender_helper.cc
@@ -19,7 +19,7 @@ using content::DocumentState;
 #define RECORD_PLT(tag, perceived_page_load_time) { \
     UMA_HISTOGRAM_CUSTOM_TIMES( \
         base::FieldTrial::MakeName(std::string("Prerender.") + tag, \
-                                   "Prefetch"), \
+                                   "Prerender"), \
         perceived_page_load_time, \
         base::TimeDelta::FromMilliseconds(10), \
         base::TimeDelta::FromSeconds(60), \
@@ -51,7 +51,7 @@ void PrerenderHelper::RecordHistograms(
     const base::Time& finish_all_loads,
     const base::TimeDelta& begin_to_finish_all_loads) {
   static bool use_prerender_histogram =
-      base::FieldTrialList::TrialExists("Prefetch");
+      base::FieldTrialList::TrialExists("Prerender");
   if (!use_prerender_histogram)
     return;
 
