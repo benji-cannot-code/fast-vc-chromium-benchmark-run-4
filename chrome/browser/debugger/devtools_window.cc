99f7c69fb1da331b573af9c3406eb26733bfe6d5
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-27 19:37:36 +0000

    Added command line flag for enabling DevTools experiments.
    BUG=108691
    
    
    Review URL: http://codereview.chromium.org/9047004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115838 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window.cc
+++ chrome/browser/debugger/devtools_window.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/common/url_constants.h"
@@ -531,13 +532,18 @@ GURL DevToolsWindow::GetDevToolsUrl(Profile* profile, bool docked,
   SkColor color_tab_text =
       tp->GetColor(ThemeService::COLOR_BOOKMARK_TEXT);
 
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  bool experiments_enabled =
+      command_line.HasSwitch(switches::kEnableDevToolsExperiments);
+
   std::string url_string = StringPrintf(
-      "%sdevtools.html?docked=%s&toolbarColor=%s&textColor=%s%s",
+      "%sdevtools.html?docked=%s&toolbarColor=%s&textColor=%s%s%s",
       chrome::kChromeUIDevToolsURL,
       docked ? "true" : "false",
       SkColorToRGBAString(color_toolbar).c_str(),
       SkColorToRGBAString(color_tab_text).c_str(),
-      shared_worker_frontend ? "&isSharedWorker=true" : "");
+      shared_worker_frontend ? "&isSharedWorker=true" : "",
+      experiments_enabled ? "&experiments=true" : "");
   return GURL(url_string);
 }
 
