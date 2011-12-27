7f6f5317280faac6a921059a91343d09e436d652
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-27 18:43:06 +0000

    Add support to SpdyFramer for the CREDENTIAL frame.
    BUG=106103
    TEST=None
    
    Review URL: http://codereview.chromium.org/8893003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115835 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -125,6 +125,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  |   Delta-Window-Size (32 bits)    |
 //  +----------------------------------+
 //
+//  Control Frame: CREDENTIAL
+//  +----------------------------------+
+//  |1|000000000000001|0000000000001010|
+//  +----------------------------------+
+//  | flags (8)  |  Length (24 bits)   | >= 12
+//  +----------------------------------+
+//  |  Slot (16 bits) | Origin Len (16)|
+//  +----------------------------------+
+//  |              Origin              |
+//  +----------------------------------+
+//  |      Proof Length (32 bits)      |
+//  +----------------------------------+
+//  |               Proof              |
+//  +----------------------------------+ <+
+//  |   Certificate Length (32 bits)   |  |
+//  +----------------------------------+  | Repeated until end of frame
+//  |            Certificate           |  |
+//  +----------------------------------+ <+
+//
 namespace spdy {
 
 // This implementation of Spdy is version 2; It's like version 1, with some
@@ -161,6 +180,7 @@ enum SpdyControlType {
   GOAWAY,
   HEADERS,
   WINDOW_UPDATE,
+  CREDENTIAL,
   NUM_CONTROL_FRAME_TYPES
 };
 
@@ -290,6 +310,17 @@ struct SpdyNoopControlFrameBlock : SpdyFrameBlock {
 // A PING Control Frame structure.
 struct SpdyPingControlFrameBlock : SpdyFrameBlock {
   uint32 unique_id_;
+};
+
+// A CREDENTIAL Control Frame structure.
+struct SpdyCredentialControlFrameBlock : SpdyFrameBlock {
+  uint16 slot_;
+  uint16 origin_len_;
+  uint32 proof_len_;
+  // Variable data here.
+  // origin data
+  // proof data
+  // for each certificate: unit32 certificate_len + certificate_data[i]
 };
 
 // A GOAWAY Control Frame structure.
@@ -686,6 +717,25 @@ class SpdyPingControlFrame : public SpdyControlFrame {
   struct SpdyPingControlFrameBlock* mutable_block() {
     return static_cast<SpdyPingControlFrameBlock*>(frame_);
   }
+};
+
+class SpdyCredentialControlFrame : public SpdyControlFrame {
+ public:
+  SpdyCredentialControlFrame() : SpdyControlFrame(size()) {}
+  SpdyCredentialControlFrame(char* data, bool owns_buffer)
+      : SpdyControlFrame(data, owns_buffer) {}
+
+  const char* payload() const {
+    return reinterpret_cast<const char*>(block()) + SpdyFrame::kHeaderSize;
+  }
+
+  static size_t size() { return sizeof(SpdyCredentialControlFrameBlock); }
+
+ private:
+  const struct SpdyCredentialControlFrameBlock* block() const {
+    return static_cast<SpdyCredentialControlFrameBlock*>(frame_);
+  }
+  DISALLOW_COPY_AND_ASSIGN(SpdyCredentialControlFrame);
 };
 
 class SpdyGoAwayControlFrame : public SpdyControlFrame {
--- net/tools/flip_server/spdy_interface.cc
+++ net/tools/flip_server/spdy_interface.cc
@@ -310,6 +310,11 @@ void SpdySM::OnStreamFrameData(SpdyStreamId stream_id,
     interface->ProcessWriteInput(data, len);
 }
 
+bool SpdySM::OnCredentialFrameData(const char* frame_data,
+                                     size_t len) {
+  return false;
+}
+
 size_t SpdySM::ProcessReadInput(const char* data, size_t len) {
   return spdy_framer_->ProcessInput(data, len);
 }
@@ -612,4 +617,3 @@ void SpdySM::GetOutput() {
 }
 
 }  // namespace net
-
