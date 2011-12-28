4a5e5708316e28af003b0404ed41913d76f74fd7
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-28 04:08:42 +0000

    [mac] Stop using bootstrap_look_up2 https://bugs.webkit.org/show_bug.cgi?id=75280
    Reviewed by Mark Rowe.
    
    * PluginProcess/mac/PluginProcessMainMac.mm:
    (WebKit::PluginProcessMain): Changed to use bootstrap_look_up() instead of
    bootstrap_look_up2().
    * WebProcess/mac/WebProcessMainMac.mm:
    (WebKit::WebProcessMain): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
@@ -46,9 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/text/CString.h>
 #import <wtf/text/StringBuilder.h>
 
-// FIXME: We should be doing this another way.
-extern "C" kern_return_t bootstrap_look_up2(mach_port_t, const name_t, mach_port_t*, pid_t, uint64_t);
-
 @interface NSApplication (WebNSApplicationDetails)
 -(void)_installAutoreleasePoolsOnCurrentThreadIfNecessary;
 @end
@@ -75,9 +72,9 @@ int WebProcessMain(const CommandLine& commandLine)
 
     // Get the server port.
     mach_port_t serverPort;
-    kern_return_t kr = bootstrap_look_up2(bootstrap_port, serviceName.utf8().data(), &serverPort, 0, 0);
+    kern_return_t kr = bootstrap_look_up(bootstrap_port, serviceName.utf8().data(), &serverPort);
     if (kr) {
-        printf("bootstrap_look_up2 result: %x", kr);
+        printf("bootstrap_look_up result: %x", kr);
         return 2;
     }
 
