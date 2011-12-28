c4ff91f3c34017074b021eb945a6093a9788a7cc
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-28 00:13:10 +0000

    Call LayoutStatusArea() in CompactStatusAreaLayoutManager::SetChildBounds
    BUG=108501
    TEST=See issue. Also generally test status area in compact mode.
    
    
    Review URL: http://codereview.chromium.org/9027022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115856 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/compact_status_area_layout_manager.cc
+++ ash/wm/compact_status_area_layout_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/compact_status_area_layout_manager.h"
 
+#include "base/auto_reset.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/screen.h"
 #include "ui/views/widget/widget.h"
@@ -22,7 +23,8 @@ namespace internal {
 
 CompactStatusAreaLayoutManager::CompactStatusAreaLayoutManager(
     views::Widget* status_widget)
-    : status_widget_(status_widget) {
+    : in_layout_(false),
+      status_widget_(status_widget) {
 }
 
 CompactStatusAreaLayoutManager::~CompactStatusAreaLayoutManager() {
@@ -51,12 +53,15 @@ void CompactStatusAreaLayoutManager::OnChildWindowVisibilityChanged(
 void CompactStatusAreaLayoutManager::SetChildBounds(
     aura::Window* child, const gfx::Rect& requested_bounds) {
   SetChildBoundsDirect(child, requested_bounds);
+  if (!in_layout_)
+    LayoutStatusArea();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // CompactStatusAreaLayoutManager, private:
 
 void CompactStatusAreaLayoutManager::LayoutStatusArea() {
+  AutoReset<bool> auto_reset_in_layout(&in_layout_, true);
   // Place the widget in the top-right corner of the screen.
   gfx::Rect monitor_bounds = gfx::Screen::GetPrimaryMonitorBounds();
   gfx::Rect widget_bounds = status_widget_->GetRestoredBounds();
