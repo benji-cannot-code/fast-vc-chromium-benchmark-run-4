f59de1f59d5a67f1ebe07a321fd4d1f629283f16
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-28 20:18:03 +0000

    getComputedStyle for border-style is not implemented. https://bugs.webkit.org/show_bug.cgi?id=75312
    Reviewed by Andreas Kling.
    
    Source/WebCore: 
    
    Implement getComputedStyle for border-style.
    
    Test: fast/css/getComputedStyle/getComputedStyle-border-style-shorthand.html
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    
    LayoutTests: 
    
    Add test to cover that we return correct values.
    
    * fast/css/getComputedStyle/getComputedStyle-border-style-shorthand-expected.txt: Added.
    * fast/css/getComputedStyle/getComputedStyle-border-style-shorthand.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103775 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -2160,8 +2160,11 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
                                         CSSPropertyBorderRightColor };
             return getCSSPropertyValuesForShorthandProperties(properties, WTF_ARRAY_LENGTH(properties));
         }
-        case CSSPropertyBorderStyle:
-            break;
+        case CSSPropertyBorderStyle: {
+            const int properties[4] = { CSSPropertyBorderTopStyle, CSSPropertyBorderRightStyle,
+                                        CSSPropertyBorderBottomStyle, CSSPropertyBorderLeftStyle };
+            return getCSSPropertyValuesForShorthandProperties(properties, WTF_ARRAY_LENGTH(properties));
+        }
         case CSSPropertyBorderTop: {
             const int properties[3] = { CSSPropertyBorderTopWidth, CSSPropertyBorderTopStyle,
                                         CSSPropertyBorderTopColor };
