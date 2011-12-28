6e296c6338270243513cf785990d3fe100afe948
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-28 23:42:40 +0000

    Unreviewed, rolling out r103782. http://trac.webkit.org/changeset/103782 https://bugs.webkit.org/show_bug.cgi?id=75328
    broke origin-whitelisting-removal.html (Requested by kling on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2011-12-28
    
    * page/SecurityPolicy.cpp:
    (WebCore::SecurityPolicy::addOriginAccessWhitelistEntry):
    (WebCore::SecurityPolicy::removeOriginAccessWhitelistEntry):
    (WebCore::SecurityPolicy::resetOriginAccessWhitelists):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/SecurityPolicy.cpp
+++ third_party/WebKit/Source/WebCore/page/SecurityPolicy.cpp
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/MainThread.h>
 #include "OriginAccessEntry.h"
 #include "SecurityOrigin.h"
-#include <wtf/OwnPtr.h>
-#include <wtf/PassOwnPtr.h>
 #include <wtf/text/StringHash.h>
 
 namespace WebCore {
@@ -43,7 +41,7 @@ namespace WebCore {
 static SecurityPolicy::LocalLoadPolicy localLoadPolicy = SecurityPolicy::AllowLocalLoadsForLocalOnly;
 
 typedef Vector<OriginAccessEntry> OriginAccessWhiteList;
-typedef HashMap<String, OwnPtr<OriginAccessWhiteList> > OriginAccessMap;
+typedef HashMap<String, OriginAccessWhiteList*> OriginAccessMap;
 
 static OriginAccessMap& originAccessMap()
 {
@@ -132,11 +130,11 @@ void SecurityPolicy::addOriginAccessWhitelistEntry(const SecurityOrigin& sourceO
         return;
 
     String sourceString = sourceOrigin.toString();
-    pair<OriginAccessMap::iterator, bool> result = originAccessMap().add(sourceString, nullptr);
+    pair<OriginAccessMap::iterator, bool> result = originAccessMap().add(sourceString, 0);
     if (result.second)
-        result.first->second = adoptPtr(new OriginAccessWhiteList);
+        result.first->second = new OriginAccessWhiteList;
 
-    OriginAccessWhiteList* list = result.first->second.get();
+    OriginAccessWhiteList* list = result.first->second;
     list->append(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains));
 }
 
@@ -153,7 +151,7 @@ void SecurityPolicy::removeOriginAccessWhitelistEntry(const SecurityOrigin& sour
     if (it == map.end())
         return;
 
-    OriginAccessWhiteList* list = it->second.get();
+    OriginAccessWhiteList* list = it->second;
     size_t index = list->find(OriginAccessEntry(destinationProtocol, destinationDomain, allowDestinationSubdomains ? OriginAccessEntry::AllowSubdomains : OriginAccessEntry::DisallowSubdomains));
     if (index == notFound)
         return;
@@ -170,7 +168,9 @@ void SecurityPolicy::removeOriginAccessWhitelistEntry(const SecurityOrigin& sour
 void SecurityPolicy::resetOriginAccessWhitelists()
 {
     ASSERT(isMainThread());
-    originAccessMap().clear();
+    OriginAccessMap& map = originAccessMap();
+    deleteAllValues(map);
+    map.clear();
 }
 
 } // namespace WebCore
