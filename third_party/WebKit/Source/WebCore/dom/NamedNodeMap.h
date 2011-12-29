f2ccfe7ea0307a1b99fc7811874feb6361d87cf7
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-29 03:56:25 +0000

    Reduce memory used by NamedNodeMap. <http://webkit.org/b/75333>
    Reviewed by Sam Weinig.
    
    Give NamedNodeMap's attribute vector an inline capacity of 4. The vast majority
    of elements have <= 4 attributes, and if they don't have any we normally don't
    allocate an attribute map at all.
    
    This reduces memory consumption by 1.2MB (on 64-bit) when loading the full HTML5
    spec at <http://whatwg.org/c>.
    
    * dom/NamedNodeMap.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103791 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/NamedNodeMap.h
+++ third_party/WebKit/Source/WebCore/dom/NamedNodeMap.h
@@ -123,7 +123,7 @@ private:
     int m_mappedAttributeCount;
     SpaceSplitString m_classNames;
     Element* m_element;
-    Vector<RefPtr<Attribute> > m_attributes;
+    Vector<RefPtr<Attribute>, 4> m_attributes;
     AtomicString m_idForStyleResolution;
 };
 
