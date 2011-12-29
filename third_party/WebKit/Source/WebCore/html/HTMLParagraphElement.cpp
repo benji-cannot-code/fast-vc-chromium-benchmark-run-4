33d46838dd9bd04ced77011901ade411eab8148b
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-29 23:27:28 +0000

    HTMLParagraphElement: Remove unnecessary variable in parseMappedAttribute(). <http://webkit.org/b/75362>
    Reviewed by Daniel Bates.
    
    * html/HTMLParagraphElement.cpp:
    (WebCore::HTMLParagraphElement::parseMappedAttribute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLParagraphElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLParagraphElement.cpp
@@ -57,7 +57,6 @@ bool HTMLParagraphElement::mapToEntry(const QualifiedName& attrName, MappedAttri
 void HTMLParagraphElement::parseMappedAttribute(Attribute* attr)
 {
     if (attr->name() == alignAttr) {
-        String v = attr->value();
         if (equalIgnoringCase(attr->value(), "middle") || equalIgnoringCase(attr->value(), "center"))
             addCSSProperty(attr, CSSPropertyTextAlign, CSSValueWebkitCenter);
         else if (equalIgnoringCase(attr->value(), "left"))
@@ -65,7 +64,7 @@ void HTMLParagraphElement::parseMappedAttribute(Attribute* attr)
         else if (equalIgnoringCase(attr->value(), "right"))
             addCSSProperty(attr, CSSPropertyTextAlign, CSSValueWebkitRight);
         else
-            addCSSProperty(attr, CSSPropertyTextAlign, v);
+            addCSSProperty(attr, CSSPropertyTextAlign, attr->value());
     } else
         HTMLElement::parseMappedAttribute(attr);
 }
