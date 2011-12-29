8ca88e8579b818961481defb75d983dd3522eee1
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-29 11:34:40 +0000

    Fix a memory leak introduced in r115955.
    Forgot to delete the Preference object in PrefService::UnregisterPreference.
    
    TBR=bauerb@chromium.org
    BUG=none
    TEST=no leaks from MigratePreferences
    
    Review URL: http://codereview.chromium.org/9034018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115958 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/pref_service.cc
+++ chrome/browser/prefs/pref_service.cc
@@ -756,6 +756,7 @@ void PrefService::UnregisterPreference(const char* path) {
     return;
   }
 
+  delete *it;
   prefs_.erase(it);
   default_store_->RemoveDefaultValue(path);
   if (pref_sync_associator_.get() &&
