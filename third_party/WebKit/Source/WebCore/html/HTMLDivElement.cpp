63a9438cfae00e7171001ffd6ced33fdf7811de4
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-29 23:30:17 +0000

    HTMLDivElement: Remove unnecessary variable in parseMappedAttribute(). <http://webkit.org/b/75363>
    Reviewed by Daniel Bates.
    
    * html/HTMLDivElement.cpp:
    (WebCore::HTMLDivElement::parseMappedAttribute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLDivElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLDivElement.cpp
@@ -61,7 +61,6 @@ bool HTMLDivElement::mapToEntry(const QualifiedName& attrName, MappedAttributeEn
 void HTMLDivElement::parseMappedAttribute(Attribute* attr)
 {
     if (attr->name() == alignAttr) {
-        String v = attr->value();
         if (equalIgnoringCase(attr->value(), "middle") || equalIgnoringCase(attr->value(), "center"))
            addCSSProperty(attr, CSSPropertyTextAlign, CSSValueWebkitCenter);
         else if (equalIgnoringCase(attr->value(), "left"))
@@ -69,7 +68,7 @@ void HTMLDivElement::parseMappedAttribute(Attribute* attr)
         else if (equalIgnoringCase(attr->value(), "right"))
             addCSSProperty(attr, CSSPropertyTextAlign, CSSValueWebkitRight);
         else
-            addCSSProperty(attr, CSSPropertyTextAlign, v);
+            addCSSProperty(attr, CSSPropertyTextAlign, attr->value());
     } else
         HTMLElement::parseMappedAttribute(attr);
 }
