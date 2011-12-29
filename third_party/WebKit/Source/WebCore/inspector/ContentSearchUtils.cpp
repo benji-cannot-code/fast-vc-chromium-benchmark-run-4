be229a4cb3ef81a762bb51903dda525850a7e132
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-29 17:12:45 +0000

    Web Inspector: support sourceMappingURL magic comment. https://bugs.webkit.org/show_bug.cgi?id=75356
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    * inspector/ContentSearchUtils.cpp:
    (WebCore::ContentSearchUtils::findSourceMapURL):
    * inspector/ContentSearchUtils.h:
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::sourceMapURLForScript):
    
    LayoutTests:
    
    * http/tests/inspector/compiler-source-mapping-debug.html:
    * http/tests/inspector/resources/compiled.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.cpp
+++ third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.cpp
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorValues.h"
 #include "RegularExpression.h"
 
+#include <wtf/BumpPointerAllocator.h>
+#include <yarr/Yarr.h>
+
 using namespace std;
 
 namespace WebCore {
@@ -136,6 +139,26 @@ PassRefPtr<InspectorArray> searchInTextByLines(const String& text, const String&
         result->pushValue(buildObjectForSearchMatch(it->first, it->second));
 
     return result;
+}
+
+String findSourceMapURL(const String& content)
+{
+    DEFINE_STATIC_LOCAL(String, patternString, ("//@[\040\t]sourceMappingURL=[\040\t]*([^\\s\'\"]*)"));
+    const char* error;
+    JSC::Yarr::YarrPattern pattern(JSC::UString(patternString.impl()), false, false, &error);
+    ASSERT(!error);
+    BumpPointerAllocator regexAllocator;
+    OwnPtr<JSC::Yarr::BytecodePattern> bytecodePattern = JSC::Yarr::byteCompile(pattern, &regexAllocator);
+    ASSERT(bytecodePattern);
+
+    ASSERT(pattern.m_numSubpatterns == 1);
+    Vector<int, 4> matches;
+    matches.resize(4);
+    int result = JSC::Yarr::interpret(bytecodePattern.get(), JSC::UString(content.impl()), 0, content.length(), matches.data());
+    if (result < 0)
+        return String();
+    ASSERT(matches[2] > 0 && matches[3] > 0);
+    return content.substring(matches[2], matches[3]);
 }
 
 } // namespace ContentSearchUtils
