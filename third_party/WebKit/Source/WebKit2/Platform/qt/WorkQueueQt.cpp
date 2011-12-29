c3e4f0c2ed23296902e6dad6bc64f8b1fbd263be
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-29 17:22:51 +0000

    [Qt] Remove unused m_signalListeners from WorkQueue https://bugs.webkit.org/show_bug.cgi?id=75354
    Reviewed by Andreas Kling.
    
    This is unused since r76507.
    
    * Platform/WorkQueue.h:
    * Platform/qt/WorkQueueQt.cpp:
    (WorkQueue::platformInvalidate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/qt/WorkQueueQt.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/qt/WorkQueueQt.cpp
@@ -106,7 +106,6 @@ void WorkQueue::platformInvalidate()
     m_workThread->exit();
     m_workThread->wait();
     delete m_workThread;
-    deleteAllValues(m_signalListeners);
 }
 
 void WorkQueue::dispatch(const Function<void()>& function)
