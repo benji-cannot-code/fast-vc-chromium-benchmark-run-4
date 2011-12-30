ec9074309a491c633f089a749884fca7c2b8372a
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-30 06:01:06 +0000

    Web Inspector: enable dock-to-right view (still need embedders to support it). https://bugs.webkit.org/show_bug.cgi?id=75360
    Patch by Pavel Feldman <pfeldman@google.com> on 2011-12-29
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/Settings.js:
    (WebInspector.Settings):
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.SettingsScreen):
    * inspector/front-end/inspector.js:
    (WebInspector.set attached):
    (WebInspector._renderAsAttached.get return):
    (WebInspector._renderAsAttached):
    (WebInspector._installDockToRight.updateToolbar):
    
    Source/WebKit/chromium:
    
    * src/js/DevTools.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/js/DevTools.js
+++ third_party/WebKit/Source/WebKit/chromium/src/js/DevTools.js
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Preferences.applicationTitle = "Developer Tools - %s";
     Preferences.exposeDisableCache = true;
     Preferences.exposeWorkersInspection = true;
+    Preferences.showDockToRight = true;
 })();}
 
 function buildPlatformExtensionAPI(extensionInfo)
