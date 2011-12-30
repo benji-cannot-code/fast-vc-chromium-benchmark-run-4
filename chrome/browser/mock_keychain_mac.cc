45894d0897ce6aca8ad39460d7ab26f132994c09
kerz@chromium.org <kerz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-30 02:53:41 +0000

    Revert 116002 - Fix and re-enable passwords sync perf test
    This appears to have broken a sync test on windows perf.
    
    http://build.chromium.org/p/chromium.perf/builders/Vista%20Perf%20%282%29/builds/15149
    
    The password sync perf test has been disabled on Mac for quite a while, because of the keychain prompts that used to pop up. The tests were made to use a MockKeychain implementation with http://codereview.chromium.org/9021004.
    
    Yet, the tests were failing when several passwords were added because of a bug in the keychain, that would treat two usernames / domains / paths with the same prefix as equal, and therefore, treat them like duplicates in the keychain.
    
    This patch fixes the bug by adding more stringent checks to the logic in MockKeychain that determines whether a password being added is in fact a duplicate or not. It also re-enables the perf test that was previously disabled.
    
    BUG=105999
    TEST=PasswordsSyncPerfTest.P0
    
    Review URL: http://codereview.chromium.org/9052006
    
    TBR=rsimha@chromium.org
    Review URL: http://codereview.chromium.org/8968037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/mock_keychain_mac.cc
+++ chrome/browser/mock_keychain_mac.cc
@@ -291,8 +291,7 @@ bool MockKeychain::AlreadyContainsInternetPassword(
        keychain_attr_list_.begin(); it != keychain_attr_list_.end(); ++it) {
     SecKeychainAttribute* attribute;
     attribute = AttributeWithTag(it->second, kSecServerItemAttr);
-    if ((attribute->length != serverNameLength) ||
-        (attribute->data == NULL && *serverName != '\0') ||
+    if ((attribute->data == NULL && *serverName != '\0') ||
         (attribute->data != NULL && *serverName == '\0') ||
         strncmp(serverName,
                 (const char*) attribute->data,
@@ -300,8 +299,7 @@ bool MockKeychain::AlreadyContainsInternetPassword(
       continue;
     }
     attribute = AttributeWithTag(it->second, kSecSecurityDomainItemAttr);
-    if ((attribute->length != securityDomainLength) ||
-        (attribute->data == NULL && *securityDomain != '\0') ||
+    if ((attribute->data == NULL && *securityDomain != '\0') ||
         (attribute->data != NULL && *securityDomain == '\0') ||
         strncmp(securityDomain,
                 (const char*) attribute->data,
@@ -309,8 +307,7 @@ bool MockKeychain::AlreadyContainsInternetPassword(
       continue;
     }
     attribute = AttributeWithTag(it->second, kSecAccountItemAttr);
-    if ((attribute->length != accountNameLength) ||
-        (attribute->data == NULL && *accountName != '\0') ||
+    if ((attribute->data == NULL && *accountName != '\0') ||
         (attribute->data != NULL && *accountName == '\0') ||
         strncmp(accountName,
                 (const char*) attribute->data,
@@ -318,8 +315,7 @@ bool MockKeychain::AlreadyContainsInternetPassword(
       continue;
     }
     attribute = AttributeWithTag(it->second, kSecPathItemAttr);
-    if ((attribute->length != pathLength) ||
-        (attribute->data == NULL && *path != '\0') ||
+    if ((attribute->data == NULL && *path != '\0') ||
         (attribute->data != NULL && *path == '\0') ||
         strncmp(path,
                 (const char*) attribute->data,
