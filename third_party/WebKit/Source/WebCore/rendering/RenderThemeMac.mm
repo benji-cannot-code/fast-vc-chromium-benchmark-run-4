9a688de8e612b55323c2adf65589237e140b67eb
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2011-12-31 23:41:55 +0000

    RenderThemeMac: Avoid double hash lookup in systemColor(). <http://webkit.org/b/75409>
    Reviewed by Anders Carlsson.
    
    * rendering/RenderThemeMac.mm:
    (WebCore::RenderThemeMac::systemColor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.mm
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.mm
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -356,9 +356,12 @@ void RenderThemeMac::platformColorsDidChange()
 
 Color RenderThemeMac::systemColor(int cssValueId) const
 {
-    if (m_systemColorCache.contains(cssValueId))
-        return m_systemColorCache.get(cssValueId);
-    
+    {
+        HashMap<int, RGBA32>::iterator it = m_systemColorCache.find(cssValueId);
+        if (it != m_systemColorCache.end())
+            return it->second;
+    }
+
     Color color;
     switch (cssValueId) {
         case CSSValueActiveborder:
