b03b9d674ebb9c63afc53db5533cbe34a11587b8
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-01 09:43:42 +0000

    Unreviewed. Rebaselined run-bindings-tests results.
    * bindings/scripts/test/CPP/WebDOMTestSupplemental.cpp: Added.
    * bindings/scripts/test/CPP/WebDOMTestSupplemental.h: Added.
    * bindings/scripts/test/GObject/WebKitDOMTestSupplemental.cpp: Added.
    * bindings/scripts/test/GObject/WebKitDOMTestSupplemental.h: Added.
    * bindings/scripts/test/ObjC/DOMTestSupplemental.cpp: Added.
    * bindings/scripts/test/ObjC/DOMTestSupplemental.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestSupplemental.cpp
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+    This file is generated just to tell build scripts that DOMTestSupplemental.h and
+    DOMTestSupplemental.cpp are created for TestSupplemental.idl, and thus
+    prevent the build scripts from trying to generate DOMTestSupplemental.h and
+    DOMTestSupplemental.cpp at every build. This file must not be tried to compile.
+*/
