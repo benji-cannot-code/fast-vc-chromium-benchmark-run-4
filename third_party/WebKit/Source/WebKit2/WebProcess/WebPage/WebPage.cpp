5a1278cad693cb053e2cb6f34923a2fee3743505
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-01 01:59:47 +0000

    Start trying to fix the Windows build after r103858.
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::drawPDFPage):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -2682,7 +2682,7 @@ static void drawPDFPage(CGPDFDocumentRef pdfDocument, CFIndex pageIndex, CGConte
         CGContextTranslateCTM(context, roundCGFloat(widthDifference / 2), roundCGFloat(heightDifference / 2));
 
     if (shouldRotate) {
-        CGContextRotateCTM(context, M_PI_2);
+        CGContextRotateCTM(context, static_cast<CGFloat>(piOverTwoDouble));
         CGContextTranslateCTM(context, 0, -cropBox.size.width);
     }
 
