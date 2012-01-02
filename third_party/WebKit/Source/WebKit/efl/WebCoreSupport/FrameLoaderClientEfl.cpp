2e3ab74999ddb858323ac749f76cbe745ed7298d
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-02 21:09:32 +0000

    [EFL] Remove whitespace/declaration style exception https://bugs.webkit.org/show_bug.cgi?id=75424
    Reviewed by David Levin.
    
    Source/WebKit/efl:
    
    EFL port doesn't need to avoid *whitespace/declaration* exception because of coding
    style change. However, EWebLauncher and MiniBrowser need to use EFL coding style because
    they are EFL application.
    
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    * ewk/ewk_frame.cpp:
    (ewk_frame_feed_touch_event):
    * ewk/ewk_tiled_matrix.cpp:
    (ewk_tile_matrix_invalidate):
    * ewk/ewk_view.cpp:
    
    Tools:
    
    Remove *whitespace/eclaration* style rule. However, EWebLauncher and MiniBrowser are EFL simple
    application. So, they still need to use EFL coding style.
    
    * Scripts/webkitpy/style/checker.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103911 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.cpp
+++ third_party/WebKit/Source/WebKit/efl/WebCoreSupport/FrameLoaderClientEfl.cpp
@@ -55,9 +55,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitVersion.h"
 #include "ewk_logging.h"
 #include "ewk_private.h"
+#include <Ecore_Evas.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/StringConcatenate.h>
-#include <Ecore_Evas.h>
 
 using namespace WebCore;
 
