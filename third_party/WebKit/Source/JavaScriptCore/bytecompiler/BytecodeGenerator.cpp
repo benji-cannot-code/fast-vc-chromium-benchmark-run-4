2609e98bb849d668ee58d8bd82849102fd7655a0
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-02 00:33:07 +0000

    Call instructions should leave room for linking information https://bugs.webkit.org/show_bug.cgi?id=75422 <rdar://problem/10633985>
    Reviewed by Oliver Hunt.
    
    * bytecode/Opcode.h:
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::emitCall):
    (JSC::BytecodeGenerator::emitConstruct):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ third_party/WebKit/Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -1822,6 +1822,8 @@ RegisterID* BytecodeGenerator::emitCall(OpcodeID opcodeID, RegisterID* dst, Regi
     instructions().append(func->index()); // func
     instructions().append(callArguments.argumentCountIncludingThis()); // argCount
     instructions().append(callArguments.registerOffset()); // registerOffset
+    instructions().append(0);
+    instructions().append(0);
     if (dst != ignoredResult()) {
         emitOpcode(op_call_put_result);
         instructions().append(dst->index()); // dst
@@ -1922,6 +1924,8 @@ RegisterID* BytecodeGenerator::emitConstruct(RegisterID* dst, RegisterID* func, 
     instructions().append(func->index()); // func
     instructions().append(callArguments.argumentCountIncludingThis()); // argCount
     instructions().append(callArguments.registerOffset()); // registerOffset
+    instructions().append(0);
+    instructions().append(0);
     if (dst != ignoredResult()) {
         emitOpcode(op_call_put_result);
         instructions().append(dst->index()); // dst
