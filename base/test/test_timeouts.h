f53e877b2df23a4663306ee7db7b8f9975051f28
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 21:04:04 +0000

    Add TimeDelay interfaces to TestTimeouts.
    R=brettw@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8952022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116194 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_timeouts.h
+++ base/test/test_timeouts.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "base/time.h"
 
 // Returns common timeouts to use in tests. Makes it possible to adjust
 // the timeouts for different environments (like Valgrind).
@@ -42,6 +43,33 @@ class TestTimeouts {
   static int large_test_timeout_ms() {
     DCHECK(initialized_);
     return large_test_timeout_ms_;
+  }
+
+  // Timeout for actions that are expected to finish "almost instantly".
+  static base::TimeDelta tiny_timeout() {
+    DCHECK(initialized_);
+    return base::TimeDelta::FromMilliseconds(tiny_timeout_ms_);
+  }
+
+  // Timeout to wait for something to happen. If you are not sure
+  // which timeout to use, this is the one you want.
+  static base::TimeDelta action_timeout() {
+    DCHECK(initialized_);
+    return base::TimeDelta::FromMilliseconds(action_timeout_ms_);
+  }
+
+  // Timeout longer than the above, but still suitable to use
+  // multiple times in a single test. Use if the timeout above
+  // is not sufficient.
+  static base::TimeDelta action_max_timeout() {
+    DCHECK(initialized_);
+    return base::TimeDelta::FromMilliseconds(action_max_timeout_ms_);
+  }
+
+  // Timeout for a large test that may take a few minutes to run.
+  static base::TimeDelta large_test_timeout() {
+    DCHECK(initialized_);
+    return base::TimeDelta::FromMilliseconds(large_test_timeout_ms_);
   }
 
  private:
