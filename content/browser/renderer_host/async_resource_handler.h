f183593d33c7cc37d6c6eb94a1ce9cb639f69223
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 21:09:53 +0000

    Store URL in AsyncResourceHandler to debug crash of issue 107692
    BUG=107692
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9064008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116197 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/async_resource_handler.h
+++ content/browser/renderer_host/async_resource_handler.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "content/browser/renderer_host/resource_handler.h"
+#include "googleurl/src/gurl.h"
 
 class HostZoomMap;
 class ResourceDispatcherHost;
@@ -68,6 +69,10 @@ class AsyncResourceHandler : public ResourceHandler {
   // allocate a buffer of 32k and double it in OnReadCompleted() if the buffer
   // was filled, up to a maximum size of 512k.
   int next_buffer_size_;
+
+  // TODO(battre): Remove url. This is only for debugging
+  // http://crbug.com/107692.
+  GURL url_;
 
   DISALLOW_COPY_AND_ASSIGN(AsyncResourceHandler);
 };
