17d1352b0de8cf688b05b7175744f9e560dfed22
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 22:17:59 +0000

    Fix deletion of entries from ResourceDispatcherHost::transferred_navigations_
    Before the web request API, only downloads were URLRequests that did not get
    canceled if a renderer requested the cancelling. Now requests that are
    transferred to a new renderer have the same behavior. This CL adapts a DCHECK
    in ResourceDispatcherHost::CancelRequestsForContext to reflect the new
    invariance.
    
    Furthermore, it fixes a DCHECK in
    ResourceDispatcherHost::~ResourceDispatcherHost that tests that
    transferred_navigations_.empty() is true.
    
    BUG=108970
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9014011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116222 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host.h
+++ content/browser/renderer_host/resource_dispatcher_host.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -437,6 +437,13 @@ class CONTENT_EXPORT ResourceDispatcherHost : public net::URLRequest::Delegate {
   };
 
   HttpAuthResourceType HttpAuthResourceTypeOf(net::URLRequest* request);
+
+  // Returns whether the URLRequest identified by |transferred_request_id| is
+  // currently in the process of being transferred to a different renderer.
+  // This happens if a request is redirected cross-site and needs to be resumed
+  // by a new render view.
+  bool IsTransferredNavigation(
+      const content::GlobalRequestID& transferred_request_id) const;
 
   PendingRequestList pending_requests_;
 
