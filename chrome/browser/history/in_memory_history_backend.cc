3d540dc8cd1505df39ba76c482d6130756c2d282
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 19:58:30 +0000

    Refactor InMemoryURLIndex and its URLIndexPrivateData Classes--Part 3
    Move all private data-related member functions from the InMemoryURLIndex class into its URLIndexPrivateData class. 
    
    BUG=NONE 
    TEST=Updated all related unit tests.
    Review URL: http://codereview.chromium.org/9036004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116179 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_history_backend.cc
+++ chrome/browser/history/in_memory_history_backend.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -147,7 +147,7 @@ void InMemoryHistoryBackend::OnURLsDeleted(const URLsDeletedDetails& details) {
     if (!db_->InitFromScratch())
       db_.reset();
     if (index_.get())
-      index_->ReloadFromHistory(db_.get(), true);
+      index_->ReloadFromHistory(db_.get());
     return;
   }
 
