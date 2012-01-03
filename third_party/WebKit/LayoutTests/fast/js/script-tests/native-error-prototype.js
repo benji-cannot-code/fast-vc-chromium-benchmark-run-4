cf04918514fad7cc9b7a420f77cb0131ad3694ed
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-03 06:56:22 +0000

    https://bugs.webkit.org/show_bug.cgi?id=75452 If argument to Error is undefined, message is not set
    Reviewed by Sam Weinig.
    
    Source/JavaScriptCore: 
    
    Per section 15.11.1.1 of the spec.
    
    * runtime/ErrorInstance.h:
    (JSC::ErrorInstance::create):
    (JSC::ErrorInstance::finishCreation):
    
    LayoutTests: 
    
    Added test case.
    
    * fast/js/native-error-prototype-expected.txt:
    * fast/js/script-tests/native-error-prototype.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/native-error-prototype.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/native-error-prototype.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description(
-'This is a test case for <a href="https://bugs.webkit.org/show_bug.cgi?id=55346">bug 55346</a> and <a href="https://bugs.webkit.org/show_bug.cgi?id=70889">bug 70889</a>.'
+'This is a test case for bugs <a href="https://bugs.webkit.org/show_bug.cgi?id=55346">55346</a>, <a href="https://bugs.webkit.org/show_bug.cgi?id=70889">70889</a>, and <a href="https://bugs.webkit.org/show_bug.cgi?id=75452">75452</a>.'
 );
 
 shouldBe("({}).toString.call(Error.prototype)", '"[object Error]"');
@@ -9,3 +9,9 @@ shouldBe("({}).toString.call(RangeError.prototype)", '"[object Error]"');
 var err = new Error("message");
 err.name = "";
 shouldBe("err.toString()", '"message"');
+
+var err = new Error();
+shouldBeFalse("err.hasOwnProperty('message')");
+
+var err = new Error(undefined);
+shouldBeFalse("err.hasOwnProperty('message')");
