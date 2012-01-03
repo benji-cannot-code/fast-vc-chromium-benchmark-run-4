43a4d1911831974b1793dc5d5080d0fccdb95ca2
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 18:08:46 +0000

    aura: Windows that cannot activate should not be able to grab focus.
    BUG=105980
    TEST=manually
    
    
    Review URL: http://codereview.chromium.org/9035014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116147 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/native_tab_contents_view_aura.cc
+++ chrome/browser/ui/views/tab_contents/native_tab_contents_view_aura.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -150,6 +150,7 @@ void NativeTabContentsViewAura::InitNativeTabContentsView() {
   params.create_texture_for_layer = false;
   params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   params.parent = NULL;
+  params.can_activate = true;
   GetWidget()->Init(params);
 
   // Hide the widget to prevent it from showing up on the root window. This is
--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -681,7 +681,7 @@ ui::TouchStatus NativeWidgetAura::OnTouchEvent(aura::TouchEvent* event) {
 }
 
 bool NativeWidgetAura::CanFocus() {
-  return true;
+  return can_activate_;
 }
 
 void NativeWidgetAura::OnCaptureLost() {
