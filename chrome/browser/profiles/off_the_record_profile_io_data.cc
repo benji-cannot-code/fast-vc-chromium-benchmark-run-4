0ca2c504b222e8df9fb66a84feef76be1ec90836
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 21:55:28 +0000

    Create a new SSL session cache for each Incognito profile.
    At the moment, closing all the Incogito windows and opening a new one gives you
    the same SSL session cache. This this change, we create a new session cache
    shard for each Incogito profile.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8995016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116210 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/off_the_record_profile_io_data.cc
+++ chrome/browser/profiles/off_the_record_profile_io_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/stringprintf.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/io_thread.h"
@@ -135,14 +136,21 @@ void OffTheRecordProfileIOData::Handle::LazyInitialize() const {
   }
 }
 
-// The SSL session cache is partitioned by setting a string. By choosing a
-// different string here, we ensure that incognito connections don't resume
-// sessions from non-incognito connections and vice-versa.
-static const char kIncognitoSSLCacheShard[] = "incognito";
-
 OffTheRecordProfileIOData::OffTheRecordProfileIOData()
     : ProfileIOData(true) {}
 OffTheRecordProfileIOData::~OffTheRecordProfileIOData() {}
+
+unsigned OffTheRecordProfileIOData::ssl_session_cache_instance_ = 0;
+
+// static
+std::string OffTheRecordProfileIOData::GetSSLSessionCacheShard() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  // The SSL session cache is partitioned by setting a string. This returns a
+  // unique string to partition the SSL session cache. Each time we create a
+  // new Incognito profile, we'll get a fresh SSL session cache which is also
+  // separate from the non-incognito ones.
+  return StringPrintf("incognito/%u", ssl_session_cache_instance_++);
+}
 
 void OffTheRecordProfileIOData::LazyInitializeInternal(
     ProfileParams* profile_params) const {
@@ -205,7 +213,7 @@ void OffTheRecordProfileIOData::LazyInitializeInternal(
                          main_context->origin_bound_cert_service(),
                          main_context->transport_security_state(),
                          main_context->proxy_service(),
-                         kIncognitoSSLCacheShard,
+                         GetSSLSessionCacheShard(),
                          main_context->ssl_config_service(),
                          main_context->http_auth_handler_factory(),
                          main_context->network_delegate(),
--- chrome/browser/profiles/off_the_record_profile_io_data.h
+++ chrome/browser/profiles/off_the_record_profile_io_data.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,6 +94,7 @@ class OffTheRecordProfileIOData : public ProfileIOData {
   OffTheRecordProfileIOData();
   virtual ~OffTheRecordProfileIOData();
 
+  static std::string GetSSLSessionCacheShard();
   virtual void LazyInitializeInternal(
       ProfileParams* profile_params) const OVERRIDE;
   virtual scoped_refptr<ChromeURLRequestContext> InitializeAppRequestContext(
@@ -110,6 +111,7 @@ class OffTheRecordProfileIOData : public ProfileIOData {
 
   mutable scoped_ptr<net::HttpTransactionFactory> main_http_factory_;
   mutable scoped_ptr<net::FtpTransactionFactory> ftp_factory_;
+  static unsigned ssl_session_cache_instance_;  // See GetSSLSessionCacheShard.
 
   DISALLOW_COPY_AND_ASSIGN(OffTheRecordProfileIOData);
 };
