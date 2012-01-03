63cb66b48ba4d3d2dfae6d4168d09be043591a0d
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 18:52:10 +0000

    base::Bind: Remove OldCompletionCallback.
    BUG=none
    TEST=none
    R=groby,awong,csilv
    
    Review URL: http://codereview.chromium.org/9064007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116158 0039d316-1c4b-4281-b951-d872f2087c98

--- net/net.gyp
+++ net/net.gyp
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'base/cert_verifier.h',
         'base/cert_verify_result.cc',
         'base/cert_verify_result.h',
-        'base/completion_callback.cc',
         'base/completion_callback.h',
         'base/connection_type_histograms.cc',
         'base/connection_type_histograms.h',
--- webkit/appcache/appcache_disk_cache.cc
+++ webkit/appcache/appcache_disk_cache.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,6 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 
 namespace appcache {
+
+// TODO(ajwong): Change disk_cache's API to return results via Callback.
+struct AppCacheDiskCache::CreateBackendDataShim {
+  CreateBackendDataShim() : backend(NULL) {}
+  disk_cache::Backend* backend;
+};
 
 // An implementation of AppCacheDiskCacheInterface::Entry that's a thin
 // wrapper around disk_cache::Entry.
@@ -124,10 +130,9 @@ AppCacheDiskCache::AppCacheDiskCache()
 }
 
 AppCacheDiskCache::~AppCacheDiskCache() {
-  if (create_backend_callback_) {
-    create_backend_callback_->Cancel();
-    create_backend_callback_.release();
-    OnCreateBackendComplete(net::ERR_ABORTED);
+  if (!create_backend_callback_.IsCancelled()) {
+    create_backend_callback_.Cancel();
+    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
   }
   disk_cache_.reset();
   STLDeleteElements(&active_calls_);
@@ -153,10 +158,9 @@ void AppCacheDiskCache::Disable() {
 
   is_disabled_ = true;
 
-  if (create_backend_callback_) {
-    create_backend_callback_->Cancel();
-    create_backend_callback_.release();
-    OnCreateBackendComplete(net::ERR_ABORTED);
+  if (!create_backend_callback_.IsCancelled()) {
+    create_backend_callback_.Cancel();
+    OnCreateBackendComplete(NULL, net::ERR_ABORTED);
   }
 }
 
@@ -222,26 +226,31 @@ int AppCacheDiskCache::Init(net::CacheType cache_type,
                             const net::CompletionCallback& callback) {
   DCHECK(!is_initializing() && !disk_cache_.get());
   is_disabled_ = false;
-  create_backend_callback_ = new CreateBackendCallback(
-      this, &AppCacheDiskCache::OnCreateBackendComplete);
 
+  // TODO(ajwong): Change disk_cache's API to return results via Callback.
+  disk_cache::Backend** backend_ptr = new(disk_cache::Backend*);
+  create_backend_callback_.Reset(
+      base::Bind(&AppCacheDiskCache::OnCreateBackendComplete,
+                 base::Unretained(this), base::Owned(backend_ptr)));
   int rv = disk_cache::CreateCacheBackend(
       cache_type, cache_directory, cache_size, force, cache_thread, NULL,
-      &(create_backend_callback_->backend_ptr_),
-      base::Bind(&net::OldCompletionCallbackAdapter, create_backend_callback_));
+      backend_ptr, create_backend_callback_.callback());
   if (rv == net::ERR_IO_PENDING)
     init_callback_ = callback;
   else
-    OnCreateBackendComplete(rv);
+    OnCreateBackendComplete(backend_ptr, rv);
+
   return rv;
 }
 
-void AppCacheDiskCache::OnCreateBackendComplete(int rv) {
+void AppCacheDiskCache::OnCreateBackendComplete(
+    disk_cache::Backend** backend, int rv) {
   if (rv == net::OK) {
-    disk_cache_.reset(create_backend_callback_->backend_ptr_);
-    create_backend_callback_->backend_ptr_ = NULL;
+    DCHECK(backend);
+    disk_cache_.reset(*backend);
   }
-  create_backend_callback_ = NULL;
+
+  create_backend_callback_.Cancel();
 
   // Invoke our clients callback function.
   if (!init_callback_.is_null()) {
