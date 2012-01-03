0614c2a29d0a206fa45349975bb3b51c34729b07
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-03 12:59:24 +0000

    Move the post processing of the Pack message down into the web page.
    Showin an alert asynchronously interferes with the overlay closing animation.
    
    BUG=108304
    TEST=Repro steps in bug work reliably, so it should be easy to verify.
    Review URL: http://codereview.chromium.org/9044009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/pack_extension_overlay.js
+++ chrome/browser/resources/options/pack_extension_overlay.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -83,6 +83,16 @@ cr.define('options', function() {
       });
     },
   };
+
+  /**
+   * Wrap up the pack process by showing the success |message| and closing
+   * the overlay.
+   * @param {String} message The message to show to the user.
+   */
+  PackExtensionOverlay.showSuccessMessage = function(message) {
+    alert(message);
+    OptionsPage.closeOverlay();
+  }
 
   // Export
   return {
