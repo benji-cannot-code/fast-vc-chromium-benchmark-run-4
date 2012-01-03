a28688d52b39fd3874196c69ed0418de09d4fe6f
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-03 19:56:48 +0000

    Source/WebCore: getComputedStyle should return shorthands property with the minimum number of sides possible. https://bugs.webkit.org/show_bug.cgi?id=75434
    Reviewed by Tony Chang.
    
    As stated in http://www.w3.org/TR/DOM-Level-2-Style/css.html#CSS-CSS2Properties we should
    return shorthands properties with the minimum number of sides possible.
    
    Existing tests have been updated accordingly.
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    (WebCore::CSSComputedStyleDeclaration::getCSSPropertyValuesForSidesShorthand):
    * css/CSSComputedStyleDeclaration.h:
    
    LayoutTests: getComputedStyle should return shorthands properties with the minimum number of sides possible.
    https://bugs.webkit.org/show_bug.cgi?id=75434
    
    Reviewed by Tony Chang.
    
    As stated in http://www.w3.org/TR/DOM-Level-2-Style/css.html#CSS-CSS2Properties we should
    return shorthands properties with the minimum number of sides possible.
    
    * fast/css/getComputedStyle/getComputedStyle-border-color-shorthand-expected.txt:
    * fast/css/getComputedStyle/getComputedStyle-border-color-shorthand.html:
    * fast/css/getComputedStyle/getComputedStyle-border-style-shorthand-expected.txt:
    * fast/css/getComputedStyle/getComputedStyle-border-style-shorthand.html:
    * fast/css/getComputedStyle/getComputedStyle-border-width-expected.txt:
    * fast/css/getComputedStyle/getComputedStyle-border-width.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103953 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.h
+++ third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.h
@@ -89,6 +89,7 @@ private:
 #endif
 
     PassRefPtr<CSSValueList> getCSSPropertyValuesForShorthandProperties(const int* properties, size_t) const;
+    PassRefPtr<CSSValueList> getCSSPropertyValuesForSidesShorthand(const int* properties) const;
 
     RefPtr<Node> m_node;
     PseudoId m_pseudoElementSpecifier;
