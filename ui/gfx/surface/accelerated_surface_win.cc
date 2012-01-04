643769579f85194cdbf55186bdf6e64207e0abfe
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 00:53:18 +0000

    Win AcceleratedSurface does not change z-order when changing compositing window size. Review URL: http://codereview.chromium.org/9076003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116248 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/surface/accelerated_surface_win.cc
+++ ui/gfx/surface/accelerated_surface_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -371,7 +371,7 @@ void AcceleratedSurface::DoPresentAndAcknowledge(
       size.width(), size.height(),
       SWP_NOACTIVATE | SWP_NOCOPYBITS | SWP_NOMOVE |SWP_NOOWNERZORDER |
           SWP_NOREDRAW | SWP_NOSENDCHANGING | SWP_NOSENDCHANGING |
-          SWP_ASYNCWINDOWPOS);
+          SWP_ASYNCWINDOWPOS | SWP_NOZORDER);
 
   // Wait for the StretchRect to complete before notifying the GPU process
   // that it is safe to write to its backing store again.
