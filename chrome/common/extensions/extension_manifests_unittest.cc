e9a670612e6c05f668001c7095604105a3c765b6
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 23:16:53 +0000

    Remove deprecated page action features from manifest v2.
    BUG=106977
    
    Review URL: http://codereview.chromium.org/8989055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_manifests_unittest.cc
+++ chrome/common/extensions/extension_manifests_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_action.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_error_utils.h"
 #include "chrome/common/extensions/extension_sidebar_defaults.h"
@@ -1030,4 +1031,21 @@ TEST_F(ExtensionManifestTest, BackgroundPage) {
   extension = LoadAndExpectSuccess(Manifest(manifest.get(), ""));
   ASSERT_TRUE(extension);
   EXPECT_FALSE(extension->background_url().is_valid());
+}
+
+TEST_F(ExtensionManifestTest, PageActionManifestVersion2) {
+  scoped_refptr<Extension> extension(
+      LoadAndExpectSuccess("page_action_manifest_version_2.json"));
+  ASSERT_TRUE(extension.get());
+  ASSERT_TRUE(extension->page_action());
+
+  EXPECT_EQ("", extension->page_action()->id());
+  EXPECT_EQ(0u, extension->page_action()->icon_paths()->size());
+  EXPECT_EQ("", extension->page_action()->GetTitle(
+      ExtensionAction::kDefaultTabId));
+  EXPECT_FALSE(extension->page_action()->HasPopup(
+      ExtensionAction::kDefaultTabId));
+
+  LoadAndExpectError("page_action_manifest_version_2b.json",
+                     errors::kInvalidPageActionPopup);
 }
