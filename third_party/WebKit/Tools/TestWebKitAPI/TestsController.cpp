b0d48ba5b128771a925e0cc9d91f111cc2d86360
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-04 19:25:19 +0000

    Revert fix for Bug 75064: TestWebKitAPI: initialize the main thread before running tests
    This reverts commit r103546.
    
    * TestWebKitAPI/TestsController.cpp:
    (TestWebKitAPI::TestsController::TestsController):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/TestsController.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/TestsController.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "TestsController.h"
 
-#include <wtf/MainThread.h>
 #include <wtf/Threading.h>
 
 namespace TestWebKitAPI {
@@ -40,8 +39,6 @@ TestsController& TestsController::shared()
 
 TestsController::TestsController()
 {
-    WTF::initializeMainThread();
-
     // FIXME: We currently initialize threading here to avoid assertion failures from
     // the ThreadRestrictionVerifier - https://bugs.webkit.org/show_bug.cgi?id=66112
     // We should make sure that all objects tested either initialize threading or inherit from
