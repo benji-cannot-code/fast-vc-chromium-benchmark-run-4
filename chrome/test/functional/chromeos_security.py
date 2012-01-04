cd278cc2310c007a730c3d575158447c98cf4bb2
dennisjeffrey@chromium.org <dennisjeffrey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 21:43:45 +0000

    Exclude the Bookmark Manager extension from the pyauto ChromeOS extension security test.
    BUG=chromium-os:24637
    TEST=None
    
    Review URL: http://codereview.chromium.org/9008072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/chromeos_security.py
+++ chrome/test/functional/chromeos_security.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -99,10 +99,11 @@ class ChromeosSecurity(pyauto.PyUITest):
       full_actual_info: A list of dictionaries describing the actual information
                         for all extensions.
     """
-    # Skip the Web Store; it's integral to Chrome and is redundant to check on
-    # ChromeOS.  This can reduce the number of times we have to update the
-    # baseline for this test.
-    actual_set.discard('Chrome Web Store')
+    # Skip the Web Store and Bookmark Manager; they are integral to Chrome and
+    # are redundant to check on ChromeOS.  This can reduce the number of times
+    # we have to update the baseline for this test.
+    for extension_name in ['Chrome Web Store', 'Bookmark Manager']:
+      actual_set.discard(extension_name)
 
     def _GetSetDifferenceMessage(expected_set, actual_set):
       strings = []
