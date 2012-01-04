4d23b736487b5b4f252e21e696b84c55fa4fbd42
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 09:41:05 +0000

    Revert 116260 - [Aura] Exiting from full screen should restore previous show state.
    BUG=108773
    TEST=Verify fix for bug 108773.
    
    Review URL: http://codereview.chromium.org/9015039
    
    TBR=xiyuan@chromium.org
    
    Review URL: http://codereview.chromium.org/9085009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116290 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -149,15 +149,6 @@ Widget* CreateChildNativeWidget() {
 bool WidgetHasMouseCapture(const Widget* widget) {
   return static_cast<const internal::NativeWidgetPrivate*>(widget->
       native_widget())->HasMouseCapture();
-}
-
-ui::WindowShowState GetWidgetShowState(const Widget* widget) {
-  gfx::Rect bounds;
-  ui::WindowShowState show_state;
-
-  static_cast<const internal::NativeWidgetPrivate*>(widget->
-      native_widget())->GetWindowPlacement(&bounds, &show_state);
-  return show_state;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -773,42 +764,6 @@ TEST_F(WidgetTest, GetRestoredBounds) {
   EXPECT_GT(toplevel->GetRestoredBounds().height(), 0);
 }
 #endif
-
-// Test that window state is not changed after getting out of full screen.
-TEST_F(WidgetTest, ExitFullscreenRestoreState) {
-  Widget* toplevel = CreateTopLevelPlatformWidget();
-
-  toplevel->Show();
-  RunPendingMessages();
-
-  // This should be a normal state window.
-  EXPECT_EQ(ui::SHOW_STATE_NORMAL, GetWidgetShowState(toplevel));
-
-  toplevel->SetFullscreen(true);
-  RunPendingMessages();
-  toplevel->SetFullscreen(false);
-  RunPendingMessages();
-
-  // And it should still be in normal state after getting out of full screen.
-  EXPECT_EQ(ui::SHOW_STATE_NORMAL, GetWidgetShowState(toplevel));
-
-  // Now, make it maximized.
-  toplevel->Maximize();
-  RunPendingMessages();
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, GetWidgetShowState(toplevel));
-
-  toplevel->SetFullscreen(true);
-  RunPendingMessages();
-  toplevel->SetFullscreen(false);
-  RunPendingMessages();
-
-  // And it stays maximized after getting out of full screen.
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, GetWidgetShowState(toplevel));
-
-  // Clean up.
-  toplevel->Close();
-  RunPendingMessages();
-}
 
 }  // namespace
 }  // namespace views
