be1db8ae156845e3444d372fadbc3fed57c3407c
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-04 02:25:56 +0000

    [chromium] LocalFileSystemChromium needs some thread safety fixes. https://bugs.webkit.org/show_bug.cgi?id=75494
    Reviewed by Dmitry Titov.
    
    This is essentially the same fix as r102894 except in slightly different code.
    
    The important part of this fix is the removal of AllowCrossThreadAccess so
    that ref counting happens appropriately.
    
    Minor clean up throughout: Removed unnecessary WTF prefix in many
    of these places and unnecessary String().
    
    * src/LocalFileSystemChromium.cpp: See r102894 since this
    is basically the same thing.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@103996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/LocalFileSystemChromium.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/LocalFileSystemChromium.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Google Inc. All rights reserved.
+ * Copyright (C) 2010, 2012 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -80,7 +80,7 @@ static const char openFileSystemMode[] = "openFileSystemMode";
 // call back to the worker context.
 class AllowFileSystemMainThreadBridge : public ThreadSafeRefCounted<AllowFileSystemMainThreadBridge> {
 public:
-    static PassRefPtr<AllowFileSystemMainThreadBridge> create(WebCore::WorkerLoaderProxy* workerLoaderProxy, const WTF::String& mode, WebCommonWorkerClient* commonClient)
+    static PassRefPtr<AllowFileSystemMainThreadBridge> create(WebCore::WorkerLoaderProxy* workerLoaderProxy, const String& mode, WebCommonWorkerClient* commonClient)
     {
         return adoptRef(new AllowFileSystemMainThreadBridge(workerLoaderProxy, mode, commonClient));
     }
@@ -98,30 +98,26 @@ public:
     }
 
     // This method is invoked on the main thread.
-    void signalCompleted(bool result)
+    void signalCompleted(const String& mode, bool result)
     {
         MutexLocker locker(m_mutex);
-        if (m_workerLoaderProxy)
-            m_workerLoaderProxy->postTaskForModeToWorkerContext(
-                createCallbackTask(&didComplete, AllowCrossThreadAccess(this), result), m_mode);
+        if (!m_workerLoaderProxy)
+            return;
+        m_workerLoaderProxy->postTaskForModeToWorkerContext(
+            createCallbackTask(&didComplete, AllowCrossThreadAccess(this), result), mode);
     }
 
 private:
-    AllowFileSystemMainThreadBridge(WebCore::WorkerLoaderProxy* workerLoaderProxy, const WTF::String& mode, WebCommonWorkerClient* commonClient)
+    AllowFileSystemMainThreadBridge(WebCore::WorkerLoaderProxy* workerLoaderProxy, const String& mode, WebCommonWorkerClient* commonClient)
         : m_workerLoaderProxy(workerLoaderProxy)
-        , m_mode(mode)
     {
-        WebWorkerBase::dispatchTaskToMainThread(
-            createCallbackTask(&allowFileSystemTask, AllowCrossThreadAccess(commonClient),
-                               AllowCrossThreadAccess(this)));
+        WebWorkerBase::dispatchTaskToMainThread(createCallbackTask(&allowFileSystemTask, mode, AllowCrossThreadAccess(commonClient), this));
     }
 
-    static void allowFileSystemTask(WebCore::ScriptExecutionContext* context, WebCommonWorkerClient* commonClient, PassRefPtr<AllowFileSystemMainThreadBridge> bridge)
+    static void allowFileSystemTask(WebCore::ScriptExecutionContext* context, const String& mode, WebCommonWorkerClient* commonClient, PassRefPtr<AllowFileSystemMainThreadBridge> bridge)
     {
-        if (commonClient)
-            bridge->signalCompleted(commonClient->allowFileSystem());
-        else
-            bridge->signalCompleted(false);
+        bool allowFileSystem = commonClient ? commonClient->allowFileSystem() : false;
+        bridge->signalCompleted(mode, allowFileSystem);
     }
 
     static void didComplete(WebCore::ScriptExecutionContext* context, PassRefPtr<AllowFileSystemMainThreadBridge> bridge, bool result)
@@ -132,7 +128,6 @@ private:
     bool m_result;
     Mutex m_mutex;
     WebCore::WorkerLoaderProxy* m_workerLoaderProxy;
-    WTF::String m_mode;
 };
 
 bool allowFileSystemForWorker(WebCommonWorkerClient* commonClient)
