9c8f6c6ae6efb15f8d7d59082859919f73b97bc4
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 00:06:20 +0000

    Enabling the following ChromeFrame tests which were failing because of a crash in the Chrome renderer process in the WebKit accessibility code. The WebKit bug https://bugs.webkit.org/show_bug.cgi?id=75279 was fixed last week.
    KeyboardInput
    WidgetModeIE_InitializeHidden
    WidgetModeIE_ObjectFocus
    FormPostBackForward
    Core
    
    TBR=robertshield
    Review URL: http://codereview.chromium.org/9084001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116233 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/test_with_web_server.cc
+++ chrome_frame/test/test_with_web_server.cc
@@ -407,8 +407,7 @@ TEST_F(ChromeFrameTestWithWebServer, WidgetModeIE_NavigateURLRelative) {
 
 const wchar_t kNavigateSimpleObjectFocus[] = L"simple_object_focus.html";
 
-// Disabled, http://crbug.com/108508
-TEST_F(ChromeFrameTestWithWebServer, DISABLED_WidgetModeIE_ObjectFocus) {
+TEST_F(ChromeFrameTestWithWebServer, WidgetModeIE_ObjectFocus) {
   SimpleBrowserTest(IE, kNavigateSimpleObjectFocus);
 }
 
@@ -557,8 +556,7 @@ TEST_F(ChromeFrameTestWithWebServer, FullTabIE_CFInstallDismiss) {
 
 const wchar_t kInitializeHiddenPage[] = L"initialize_hidden.html";
 
-// Disabled, http://crbug.com/108508
-TEST_F(ChromeFrameTestWithWebServer, DISABLED_WidgetModeIE_InitializeHidden) {
+TEST_F(ChromeFrameTestWithWebServer, WidgetModeIE_InitializeHidden) {
   SimpleBrowserTest(IE, kInitializeHiddenPage);
 }
 
