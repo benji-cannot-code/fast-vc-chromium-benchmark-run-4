3e7816e504cc6ff1cc778406011bcb5f30bf9238
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 18:01:13 +0000

    Fix Sleep() calls to use TimeDelta in portions of chrome/browser.
    R=jar@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9088015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116334 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/connect_interceptor_unittest.cc
+++ chrome/browser/net/connect_interceptor_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -66,7 +66,7 @@ TEST(ConnectInterceptorTest, TimedCacheEviction) {
   cache.SetRecentlySeen(url);
 
   // Sleep at least long enough to cause an eviction.
-  base::PlatformThread::Sleep(30);
+  base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(30));
 
   EXPECT_FALSE(cache.WasRecentlySeen(url));
 }
