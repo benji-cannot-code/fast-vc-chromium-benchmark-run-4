209c8f96b27b29da81241653cd0e0c7d8e71dd4d
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 18:24:38 +0000

    Make ./tools/sort-headers.py order C++ system headers before C system headers.
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9109013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116513 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/sort-headers.py
+++ tools/sort-headers.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -51,7 +51,14 @@ def IncludeCompareKey(line):
   if line.startswith('<unknwn.h>'):  # Must be before e.g. intshcut.h
     return '1'
 
-  return line
+  # C++ system headers should come after C system headers.
+  if line.startswith('<'):
+    if line.find('.h>') != -1:
+      return '2' + line
+    else:
+      return '3' + line
+
+  return '4' + line
 
 
 def IsInclude(line):
