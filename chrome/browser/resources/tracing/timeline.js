1dd566203108ef5d014dd2b0407a4f60b8516c2c
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 22:08:07 +0000

    Create ruler for about:tracing
    BUG=108085
    
    
    Review URL: http://codereview.chromium.org/9014008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116568 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/tracing/timeline.js
+++ chrome/browser/resources/tracing/timeline.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -334,6 +334,12 @@ cr.define('tracing', function() {
       for (var i = 0; i < this.tracks_.children.length; i++)
         this.tracks_.children[i].detach();
       this.tracks_.textContent = '';
+
+      // Add the viewport track
+      var viewportTrack = new tracing.TimelineViewportTrack();
+      viewportTrack.headingWidth = maxHeadingWidth;
+      viewportTrack.viewport = this.viewport_;
+      this.tracks_.appendChild(viewportTrack);
 
       // Get a sorted list of CPUs
       var cpus = model.getAllCpus();
