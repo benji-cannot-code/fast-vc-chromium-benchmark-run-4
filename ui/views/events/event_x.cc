f90926a212e8a2834e32932ba41e4b10893843ec
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 04:09:23 +0000

    Do not use ui::GetCharacterFromXEvent when Control is pressed.
    The function does not take into account the modifier.
    
    BUG=107837
    TEST=see the bug
    
    
    Review URL: http://codereview.chromium.org/8968011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116451 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/events/event_x.cc
+++ ui/views/events/event_x.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -64,7 +64,9 @@ uint16 KeyEvent::GetCharacter() const {
   DCHECK(native_event()->type == KeyPress ||
          native_event()->type == KeyRelease);
 
-  uint16 ch = ui::GetCharacterFromXEvent(native_event());
+  uint16 ch = 0;
+  if (!IsControlDown())
+    ch = ui::GetCharacterFromXEvent(native_event());
   return ch ? ch : ui::GetCharacterFromKeyCode(key_code_, flags());
 }
 
