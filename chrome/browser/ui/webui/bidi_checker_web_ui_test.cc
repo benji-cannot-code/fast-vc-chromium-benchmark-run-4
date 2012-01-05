0ce3ab4cbe1e41a67fe677e256642b309b0b69cc
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 15:29:12 +0000

    Mark WebUIBidiCheckerBrowserTestRTL.TestDownloadsPageRTL as flaky on Linux.
    Also, disambiguate test names of TestDownloadsPageRTL and TestDownloadsPageLTR.
    
    TBR=ofri@chromium.org
    BUG=109262
    Review URL: http://codereview.chromium.org/9116005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116489 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/bidi_checker_web_ui_test.cc
+++ chrome/browser/ui/webui/bidi_checker_web_ui_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -177,13 +177,23 @@ IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestRTL, TestCrashesPage) {
 
 #if defined(OS_WIN) || defined(OS_LINUX)
 // http://crbug.com/104129
-#define TestDownloadsPageLTR FLAKY_TestDownloadsPage
+#define MAYBE_TestDownloadsPageLTR FLAKY_TestDownloadsPageLTR
+#else
+#define MAYBE_TestDownloadsPageLTR TestDownloadsPageLTR
 #endif
-IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestLTR, TestDownloadsPageLTR) {
+IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestLTR,
+                       MAYBE_TestDownloadsPageLTR) {
   RunBidiCheckerOnPage(chrome::kChromeUIDownloadsURL);
 }
 
-IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestRTL, TestDownloadsPage) {
+#if defined(OS_LINUX)
+// http://crbug.com/109262
+#define MAYBE_TestDownloadsPageRTL FLAKY_TestDownloadsPageRTL
+#else
+#define MAYBE_TestDownloadsPageRTL TestDownloadsPageRTL
+#endif
+IN_PROC_BROWSER_TEST_F(WebUIBidiCheckerBrowserTestRTL,
+                       MAYBE_TestDownloadsPageRTL) {
   RunBidiCheckerOnPage(chrome::kChromeUIDownloadsURL);
 }
 
