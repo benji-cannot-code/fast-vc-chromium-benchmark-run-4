d8011cee092c1ec52039c6969d39f94934fe3dd8
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 12:55:50 +0000

    getComputedStyle for border-image is not implemented. https://bugs.webkit.org/show_bug.cgi?id=75347
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Implement getComputedStyle for border-color.
    
    fast/css/getComputedStyle/computed-style-border-image.html was extended to cover the
    new feature.
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    
    LayoutTests:
    
    Extend existing test to cover that we return correct values for border-image.
    
    * fast/css/getComputedStyle/computed-style-border-image-expected.txt:
    * fast/css/getComputedStyle/computed-style-border-image.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -2165,6 +2165,7 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
             return getCSSPropertyValuesForShorthandProperties(properties, WTF_ARRAY_LENGTH(properties));
         }
         case CSSPropertyBorderImage:
+            return valueForNinePieceImage(style->borderImage(), cssValuePool);
         case CSSPropertyBorderRadius:
             break;
         case CSSPropertyBorderRight: {
