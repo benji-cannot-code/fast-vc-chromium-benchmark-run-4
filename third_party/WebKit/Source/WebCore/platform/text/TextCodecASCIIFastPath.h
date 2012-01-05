d04bd726441bfafe59e93a6a7e2eed2695088d51
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 12:51:35 +0000

    Unreviewed. Build fix for Chromium Mac Clang build.
    Push using namespace WTF to .cpp file from .h file.
    
    * platform/text/TextCodecASCIIFastPath.h:
    * platform/text/TextCodecLatin1.cpp:
    * platform/text/TextCodecUTF8.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/TextCodecASCIIFastPath.h
+++ third_party/WebKit/Source/WebCore/platform/text/TextCodecASCIIFastPath.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/text/ASCIIFastPath.h>
 
-using namespace WTF;
-
 namespace WebCore {
 
 template<size_t size> struct UCharByteFiller;
--- third_party/WebKit/Source/WebCore/platform/text/TextCodecLatin1.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/TextCodecLatin1.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/text/StringBuffer.h>
 #include <wtf/PassOwnPtr.h>
 
+using namespace WTF;
+
 namespace WebCore {
 
 static const UChar table[256] = {
