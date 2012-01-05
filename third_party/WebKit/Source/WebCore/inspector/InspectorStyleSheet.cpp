e7d6c8fd63dd6240501a64fd6a706d2a68597edb
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 10:20:21 +0000

    InspectorStyleSheet: Avoid cloning CSSRuleLists. <http://webkit.org/b/75603>
    Reviewed by Ryosuke Niwa.
    
    Don't filter out @charset rules from CSSRuleLists. This was forcing us to clone
    the stylesheet rule list, and is unnecessary since InspectorStyleSheet disregards
    any rule that isn't either a style rule or a rule with an internal rule list.
    
    * inspector/InspectorStyleSheet.cpp:
    (WebCore::asCSSRuleList):
    (WebCore::InspectorStyleSheet::buildObjectForStyleSheet):
    
        Don't pass omitCharsetRules=true to the CSSRuleList constructor.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104131 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
@@ -150,7 +150,7 @@ static PassRefPtr<CSSRuleList> asCSSRuleList(CSSStyleSheet* styleSheet)
     if (!styleSheet)
         return 0;
 
-    return CSSRuleList::create(styleSheet, true);
+    return CSSRuleList::create(styleSheet);
 }
 
 static PassRefPtr<CSSRuleList> asCSSRuleList(CSSRule* rule)
@@ -766,7 +766,7 @@ PassRefPtr<InspectorObject> InspectorStyleSheet::buildObjectForStyleSheet()
 
     RefPtr<InspectorObject> result = InspectorObject::create();
     result->setString("styleSheetId", id());
-    RefPtr<CSSRuleList> cssRuleList = CSSRuleList::create(styleSheet, true);
+    RefPtr<CSSRuleList> cssRuleList = CSSRuleList::create(styleSheet);
     RefPtr<InspectorArray> cssRules = buildArrayForRuleList(cssRuleList.get());
     result->setArray("rules", cssRules.release());
 
