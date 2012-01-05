23a5b701a4c16230fa44cef39b551c704a7ddf6e
prasadt@chromium.org <prasadt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 21:45:57 +0000

    Settings button not switching visibility on enter/leave on panels in Aura.
    BUG=109222
    TEST=Follow steps in bug report.
    
    
    Review URL: http://codereview.chromium.org/9115001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116563 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/x/x11_util.h
+++ ui/base/x/x11_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -229,6 +229,9 @@ UI_EXPORT void SetDefaultX11ErrorHandlers();
 
 // Return true if a given window is in full-screen mode.
 UI_EXPORT bool IsX11WindowFullScreen(XID window);
+
+// Return true if event type is MotionNotify.
+UI_EXPORT bool IsMotionEvent(XEvent* event);
 
 // Returns the mapped button.
 int GetMappedButton(int button);
