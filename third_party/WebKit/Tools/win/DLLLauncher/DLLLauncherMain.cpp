aead11a470ebadb05c45274aa72c069400694699
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 14:57:46 +0000

    Turn MiniBrowser into a stub .exe launcher and a .dll that contains all the real code
    This allows us to add the Apple Application Support directory to PATH before trying to load
    WebKit so that CoreGraphics.dll and friends can be found.
    
    Fixes <http://webkit.org/b/68576> <rdar://problem/10163853> MiniBrowser fails to launch when
    double-clicked
    
    Reviewed by Anders Carlsson.
    
    Tools:
    
    * MiniBrowser/MiniBrowser.vcproj:
    * MiniBrowser/Configurations/MiniBrowserCommon.vsprops:
    Changed to build MiniBrowser.dll instead of MiniBrowser.exe.
    
    * MiniBrowser/Configurations/MiniBrowserLauncherCommon.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherDebug.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherDebugAll.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherDebugCairoCFLite.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherProduction.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherRelease.vsprops: Added.
    * MiniBrowser/Configurations/MiniBrowserLauncherReleaseCairoCFLite.vsprops: Added.
    
    * MiniBrowser/MiniBrowserLauncher.vcproj: Copied from Tools/MiniBrowser/MiniBrowser.vcproj
    but changed to build the stub launcher.
    
    * MiniBrowser/win/main.cpp: Moved the /manifestdependency code to DLLLauncherMain.cpp, since
    it needs to be compiled into the .exe instead of the .dll.
    (DllMain): Added. We use this to store the DLL's HINSTANCE.
    (dllLauncherEntryPoint): Renamed from _tWinMain. This is what gets called by the
    DLLLauncherMain code.
    
    * win/DLLLauncher/DLLLauncherMain.cpp: Moved the /manifestdependency code from MiniBrowser
    to here.
    
    Source/WebKit/win:
    
    Add MiniBrowserLauncher to WebKit.sln
    
    It builds just after MiniBrowser.
    
    * WebKit.vcproj/WebKit.sln: Added MiniBrowserLauncher.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104155 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/win/DLLLauncher/DLLLauncherMain.cpp
+++ third_party/WebKit/Tools/win/DLLLauncher/DLLLauncherMain.cpp
@@ -36,6 +36,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace std;
 
+#if defined _M_IX86
+#define PROCESSORARCHITECTURE "x86"
+#elif defined _M_IA64
+#define PROCESSORARCHITECTURE "ia64"
+#elif defined _M_X64
+#define PROCESSORARCHITECTURE "amd64"
+#else
+#define PROCESSORARCHITECTURE "*"
+#endif
+
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='" PROCESSORARCHITECTURE "' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 static void enableTerminationOnHeapCorruption()
 {
     // Enable termination on heap corruption on OSes that support it (Vista and XPSP3).
