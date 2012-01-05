21abeacdde6e9cc28a46a52148173da7c043814d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 20:41:16 +0000

    [v8] Null pointer exception if a typed array constructor set to a primitive value. https://bugs.webkit.org/show_bug.cgi?id=75532
    Make sure that V8ArrayBufferViewCustomScript.js does not throw exception.
    
    Patch by Ulan Degenbaev <ulan@chromium.org> on 2012-01-05
    Reviewed by Kenneth Russell.
    
    * LayoutTests/fast/canvas/webgl/array-buffer-view-crash-when-reassigned-expected.txt: Added.
    * LayoutTests/fast/canvas/webgl/array-buffer-view-crash-when-reassigned.html: Added.
    * Source/WebCore/bindings/v8/custom/V8ArrayBufferViewCustom.cpp:
    * Source/WebCore/bindings/v8/custom/V8ArrayBufferViewCustomScript.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ArrayBufferViewCustomScript.js
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ArrayBufferViewCustomScript.js
@@ -27,24 +27,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var optimizeSetMethod = function(type)
 {
-    type.prototype.set = (function() {
-        var nativeSet = type.prototype.set;
-        var f = function(source, offset)
-        {
-            if (source.constructor === Array) {
-                var length = source.length;
-                offset = offset || 0;
-                if (offset < 0 || offset + length > this.length) {
+    if (typeof type === 'function' &&
+        typeof type.prototype !== 'undefined' && 
+        typeof type.prototype.set === 'function') {
+        type.prototype.set = (function() {
+            var nativeSet = type.prototype.set;
+            var f = function(source, offset)
+            {
+                if (source.constructor === Array) {
+                    var length = source.length;
+                    offset = offset || 0;
+                    if (offset < 0 || offset + length > this.length) {
+                        return nativeSet.call(this, source, offset);
+                    }
+                    for (var i = 0; i < length; i++)
+                        this[i + offset] = source[i];
+                } else
                     return nativeSet.call(this, source, offset);
-                }
-                for (var i = 0; i < length; i++)
-                    this[i + offset] = source[i];
-            } else
-                return nativeSet.call(this, source, offset);
-        }
-        f.name = "set";
-        return f;
-    })();
+            }
+            f.name = "set";
+            return f;
+        })();
+    }
 };
 
 optimizeSetMethod(Float32Array);
