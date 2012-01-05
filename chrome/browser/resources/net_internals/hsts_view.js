f821231ec126b385788c1e71d8961e11a813b9ac
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 02:26:15 +0000

    Implement HTTP header-based public key pinning.
    Upon receipt of the Public-Key-Pins header, check the syntax and the pins, and
    set the pins in the TransportSecurityState. From then on, use these new
    dynamic pins to validate TLS connections: as with preloaded pins, refuse to
    connect to TLS servers that fail the pin check.
    
    The Public-Key-Pins header is defined in an IETF Internet-Draft, and
    discussed on the websec@ietf.org mailing list.
    
    Clarified TransportSecurityState member function and field documentation.
    
    Also: Minor "gcl lint" repairs, and a new typedef
    std::vector<net::SHA1Fingerprint> FingerprintVector used everywhere relevant.
    
    BUG=78369
    TEST=net_unittests TransportSecurityStateTest.*, browser_tests NetInternalsTest.*
    
    Review URL: http://codereview.chromium.org/8082016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116443 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/hsts_view.js
+++ chrome/browser/resources/net_internals/hsts_view.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * use HTTPS. See http://dev.chromium.org/sts
  *
  * This UI allows a user to query and update the browser's list of HSTS domains.
+ * It also allows users to query and update the browser's list of public key
+ * pins.
  */
 
 var HSTSView = (function() {
@@ -133,8 +135,25 @@ var HSTSView = (function() {
       addTextNode(this.queryOutputDiv_, ' pubkey_hashes:');
 
       t = addNode(this.queryOutputDiv_, 'tt');
-      t.textContent = result.public_key_hashes;
+      // |public_key_hashes| is an old synonym for what is now
+      // |preloaded_spki_hashes|. Look for both, and also for
+      // |dynamic_spki_hashes|.
+      if (typeof result.public_key_hashes === 'undefined')
+        result.public_key_hashes = '';
+      if (typeof result.preloaded_spki_hashes === 'undefined')
+        result.preloaded_spki_hashes = '';
+      if (typeof result.dynamic_spki_hashes === 'undefined')
+        result.dynamic_spki_hashes = '';
 
+      var hashes = [];
+      if (result.public_key_hashes)
+        hashes.push(result.public_key_hashes);
+      if (result.preloaded_spki_hashes)
+        hashes.push(result.preloaded_spki_hashes);
+      if (result.dynamic_spki_hashes)
+        hashes.push(result.dynamic_spki_hashes);
+
+      t.textContent = hashes.join(",");
       yellowFade(this.queryOutputDiv_);
     }
   };
