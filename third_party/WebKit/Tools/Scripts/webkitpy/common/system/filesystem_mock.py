a620913e2df2f149a22a3f155c57e21e8f78a6b1
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 21:25:23 +0000

    Replace webkitpy.common.system.filesystem.file_path_as_url with webkitpy.common.system.path.abspath_to_uri https://bugs.webkit.org/show_bug.cgi?id=75617
    Reviewed by Dirk Pranke.
    
    * Scripts/webkitpy/common/system/filesystem.py:
    (FileSystem.exists):
    * Scripts/webkitpy/common/system/filesystem_mock.py:
    (MockFileSystem.exists):
    * Scripts/webkitpy/common/system/filesystem_unittest.py:
    (GenericFileSystemTests.teardown_generic_test_dir):
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.to.show_results_html_file):
    * Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py:
    (MainTest.test_results_directory_absolute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/filesystem_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/filesystem_mock.py
@@ -32,7 +32,6 @@ import errno
 import hashlib
 import os
 import re
-from urllib import pathname2url
 
 from webkitpy.common.system import path
 from webkitpy.common.system import ospath
@@ -118,9 +117,6 @@ class MockFileSystem(object):
 
     def exists(self, path):
         return self.isfile(path) or self.isdir(path)
-
-    def file_path_as_url(self, path):
-        return 'file://' + pathname2url(path)
 
     def files_under(self, path, dirs_to_skip=[], file_filter=None):
         def filter_all(fs, dirpath, basename):
