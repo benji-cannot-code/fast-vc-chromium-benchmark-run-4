4afb61a3b55e986781e4bfc3299dfe6582293069
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 09:46:42 +0000

    Returns a bool for JSONValueConverter::Convert()
    As I see a real use scenario in chrome/browser/chromeos/gdata/
    gdata_parser.cc, I realized that sometimes structural validity
    matters.
    
    It also introduce string16 whose code is almost equivalent to
    std::string.
    
    BUG=none
    TEST=passed
    
    
    Review URL: http://codereview.chromium.org/8952029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116473 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_value_converter_unittest.cc
+++ base/json/json_value_converter_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -63,7 +63,8 @@ TEST(JSONValueConverterTest, ParseSimpleMessage) {
   scoped_ptr<Value> value(base::JSONReader::Read(normal_data, false));
   SimpleMessage message;
   base::JSONValueConverter<SimpleMessage> converter;
-  converter.Convert(*value.get(), &message);
+  EXPECT_TRUE(converter.Convert(*value.get(), &message));
+
   EXPECT_EQ(1, message.foo);
   EXPECT_EQ("bar", message.bar);
   EXPECT_TRUE(message.baz);
@@ -96,7 +97,8 @@ TEST(JSONValueConverterTest, ParseNestedMessage) {
   scoped_ptr<Value> value(base::JSONReader::Read(normal_data, false));
   NestedMessage message;
   base::JSONValueConverter<NestedMessage> converter;
-  converter.Convert(*value.get(), &message);
+  EXPECT_TRUE(converter.Convert(*value.get(), &message));
+
   EXPECT_EQ(1.0, message.foo);
   EXPECT_EQ(1, message.child.foo);
   EXPECT_EQ("bar", message.child.bar);
@@ -112,6 +114,44 @@ TEST(JSONValueConverterTest, ParseNestedMessage) {
   EXPECT_EQ(3, second_child.foo);
   EXPECT_EQ("barbaz", second_child.bar);
   EXPECT_FALSE(second_child.baz);
+}
+
+TEST(JSONValueConverterTest, ParseFailures) {
+  const char normal_data[] =
+      "{\n"
+      "  \"foo\": 1,\n"
+      "  \"bar\": 2,\n" // "bar" is an integer here.
+      "  \"baz\": true,\n"
+      "  \"ints\": [1, 2]"
+      "}\n";
+
+  scoped_ptr<Value> value(base::JSONReader::Read(normal_data, false));
+  SimpleMessage message;
+  base::JSONValueConverter<SimpleMessage> converter;
+  EXPECT_FALSE(converter.Convert(*value.get(), &message));
+  // Do not check the values below.  |message| may be modified during
+  // Convert() even it fails.
+}
+
+TEST(JSONValueConverterTest, ParseWithMissingFields) {
+  const char normal_data[] =
+      "{\n"
+      "  \"foo\": 1,\n"
+      "  \"baz\": true,\n"
+      "  \"ints\": [1, 2]"
+      "}\n";
+
+  scoped_ptr<Value> value(base::JSONReader::Read(normal_data, false));
+  SimpleMessage message;
+  base::JSONValueConverter<SimpleMessage> converter;
+  // Convert() still succeeds even if the input doesn't have "bar" field.
+  EXPECT_TRUE(converter.Convert(*value.get(), &message));
+
+  EXPECT_EQ(1, message.foo);
+  EXPECT_TRUE(message.baz);
+  EXPECT_EQ(2, static_cast<int>(message.ints.size()));
+  EXPECT_EQ(1, message.ints[0]);
+  EXPECT_EQ(2, message.ints[1]);
 }
 
 }  // namespace base
