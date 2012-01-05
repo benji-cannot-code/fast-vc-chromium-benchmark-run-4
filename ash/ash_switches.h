a780ed79b05763c121810066672eac573a8afcfa
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 02:10:16 +0000

    Aura: Toggling window mode in about:flags works on CrOS devices
    The problem was that programmatically modifying the command line doesn't change switches visible in about:flags -- they are in a special section.  Reworked the code to:
    * Not modify the command line when automatically setting the mode.  We now store the mode in ash::Shell.
    * Add an "automatic" option for window mode, equivalent to not specifying anything on the command line.
    
    BUG=109002
    TEST=aura_shell_unittests for Shell, manual on device modifying about:flags
    
    
    Review URL: http://codereview.chromium.org/9093002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116438 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.h
+++ ash/ash_switches.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,9 +20,6 @@ ASH_EXPORT extern const char kAuraWindowMode[];
 ASH_EXPORT extern const char kAuraWindowModeCompact[];
 ASH_EXPORT extern const char kAuraWindowModeNormal[];
 ASH_EXPORT extern const char kAuraWorkspaceManager[];
-
-// Utilities for testing multi-valued switches.
-ASH_EXPORT bool IsAuraWindowModeCompact();
 
 }  // namespace switches
 }  // namespace ash
