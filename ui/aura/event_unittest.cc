f320f677ee208cb2c77a65721c07316394c95e32
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 05:21:32 +0000

    Fix Aura compile.
    R=hbono@chromium.org
    
    BUG=None
    TEST=Win Aura compiles.
    
    Review URL: http://codereview.chromium.org/9111005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116456 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/event_unittest.cc
+++ ui/aura/event_unittest.cc
@@ -32,6 +32,7 @@ TEST(EventTest, NativeEvent) {
                               native_event.get());
   KeyEvent keyev(native_event.get(), false);
   EXPECT_TRUE(keyev.HasNativeEvent());
+#endif
 }
 
 TEST(EventTest, GetCharacter) {
