5357982132d77a1458046bd95f7d8792ca2303b9
guohui@google.com <guohui@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 16:51:31 +0000

    BUG=103026 TEST=Please refer to the repro steps in 103026
    The line that increments counter for ntp4 message shown was deleted in CL 8396024, thus chrome always think the ntp4 bubble is never shown. This has two effects, first the bubble will never be dismissed unless the user explicitly clicks the close button, while it used to be automatically dismissed after shown 10 times. Second double registration of the preference that records the number of times ntp4 bubble is shown may occur. For more details please refer to bug 103026.
    Review URL: http://codereview.chromium.org/9085023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116498 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/new_tab.js
+++ chrome/browser/resources/ntp4/new_tab.js
@@ -153,6 +153,7 @@ cr.define('ntp4', function() {
       learnMoreLink.onclick = infoBubble.hide.bind(infoBubble);
 
       infoBubble.show();
+      chrome.send('introMessageSeen');
     }
 
     var serverpromo = localStrings.getString('serverpromo');
