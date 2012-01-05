a6e8cd86772e7a67f9e85a15118dfe986d448ae3
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 17:50:57 +0000

    Add missing unversioned interface-name macro for PPP_Pdf.
    BUG=107398
    
    Review URL: http://codereview.chromium.org/9114010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116504 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppp_pdf.h
+++ webkit/plugins/ppapi/ppp_pdf.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_var.h"
 
 #define PPP_PDF_INTERFACE_1 "PPP_Pdf;1"
+#define PPP_PDF_INTERFACE PPP_PDF_INTERFACE_1
 
 struct PPP_Pdf_1 {
   // Returns an absolute URL if the position is over a link.
