c3a141704c42cca1ba06a2fb22ac72a0f5d09dce
dslomov@chromium.org <dslomov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 06:05:14 +0000

    Marking ExtensionUnpackerTest.EmptyDefaultLocale as flake on Windows. See http://crbug.com/109238
    TBR=cira@chromium.org,RDevlin.Cronin@gmail.com
    
    Review URL: http://codereview.chromium.org/9108006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116463 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_unpacker_unittest.cc
+++ chrome/common/extensions/extension_unpacker_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -47,7 +47,14 @@ public:
   scoped_ptr<ExtensionUnpacker> unpacker_;
 };
 
-TEST_F(ExtensionUnpackerTest, EmptyDefaultLocale) {
+// Flaky on Windows, http://crbug.com/109238
+#if defined(OS_WINDOWS)
+#define MAYBE_EmptyDefaultLocale FLAKY_EmptyDefaultLocale
+#else
+#define MAYBE_EmptyDefaultLocale EmptyDefaultLocale
+#endif
+
+TEST_F(ExtensionUnpackerTest, MAYBE_EmptyDefaultLocale) {
   SetupUnpacker("empty_default_locale.crx");
   EXPECT_FALSE(unpacker_->Run());
   EXPECT_EQ(ASCIIToUTF16(errors::kInvalidDefaultLocale),
