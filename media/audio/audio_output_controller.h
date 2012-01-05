d7ec9fd9f2349b2b30b1d0693c51386eda2046a8
enal@chromium.org <enal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 02:25:02 +0000

    Fix race condition (bug 108685).
    In the ~AudioOutputController() we were calling method supposed to be run
    on the audio manager message loop only.
    
    BUG=108685
    TEST=Should not be noticeable, crash should go away.
    
    Review URL: http://codereview.chromium.org/9085030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116441 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_controller.h
+++ media/audio/audio_output_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_manager.h"
 #include "media/audio/simple_sources.h"
+
+namespace base {
+class WaitableEvent;
+}  // namespace base
 
 class MessageLoop;
 
@@ -221,7 +225,8 @@ class MEDIA_EXPORT AudioOutputController
   void StartStream();
 
   // Helper method that stops, closes, and NULLs |*stream_|.
-  void StopCloseAndClearStream();
+  // Signals event when done if it is not NULL.
+  void DoStopCloseAndClearStream(base::WaitableEvent *done);
 
   scoped_refptr<AudioManager> audio_manager_;
   // |handler_| may be called only if |state_| is not kClosed.
