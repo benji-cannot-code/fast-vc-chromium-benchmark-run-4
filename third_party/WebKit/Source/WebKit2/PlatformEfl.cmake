83b84aae7e44e79b108b09220c96c0b0e4902080
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-05 08:51:02 +0000

    [WK2][EFL] creating dummy functions for Download class on Efl port. https://bugs.webkit.org/show_bug.cgi?id=75246
    dummy functions for Download class on Efl port
    
    Patch by Keunsoon Lee <keunsoon.lee@samsung.com> on 2012-01-05
    Reviewed by Hajime Morita.
    
    * PlatformEfl.cmake: add new file to compile
    * WebProcess/Downloads/efl: Added.
    * WebProcess/Downloads/efl/DownloadEfl.cpp: Added.
    (WebKit::Download::start):
    (WebKit::Download::startWithHandle):
    (WebKit::Download::cancel):
    (WebKit::Download::platformInvalidate):
    (WebKit::Download::didDecideDestination):
    (WebKit::Download::platformDidFinish):
    (WebKit::Download::receivedCredential):
    (WebKit::Download::receivedRequestToContinueWithoutCredential):
    (WebKit::Download::receivedCancellation):
    (WebKit::Download::useCredential):
    (WebKit::Download::continueWithoutCredential):
    (WebKit::Download::cancelAuthenticationChallenge):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
+++ third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
@@ -45,6 +45,8 @@ LIST(APPEND WebKit2_SOURCES
     UIProcess/Plugins/efl/PluginInfoStoreEfl.cpp
     UIProcess/Plugins/efl/PluginProcessProxyEfl.cpp
 
+    WebProcess/Downloads/efl/DownloadEfl.cpp
+
     WebProcess/efl/WebProcessEfl.cpp
     WebProcess/efl/WebProcessMainEfl.cpp
 
@@ -117,7 +119,6 @@ IF (WTF_USE_SOUP)
     LIST(APPEND WebKit2_LIBRARIES ${LIBSOUP24_LIBRARIES})
     LIST(APPEND WebKit2_SOURCES
         WebProcess/Cookies/soup/WebCookieManagerSoup.cpp
-        WebProcess/Downloads/soup/DownloadSoup.cpp
     )
 
     ADD_CUSTOM_TARGET(forwarding-headerSoup
@@ -134,7 +135,6 @@ IF (WTF_USE_CURL)
     LIST(APPEND WebKit2_LIBRARIES ${CURL_LIBRARIES})
     LIST(APPEND WebKit2_SOURCES
         WebProcess/Cookies/curl/WebCookieManagerCurl.cpp
-        WebProcess/Downloads/curl/DownloadCurl.cpp
     )
 ENDIF ()
 
