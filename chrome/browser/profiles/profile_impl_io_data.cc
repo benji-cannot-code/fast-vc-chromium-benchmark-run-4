9db9b40851ed4c89405e0b84a292057a430b4262
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 18:20:58 +0000

    Make incognito windows not inherit HSTS state from the main profile.
    The dynamic HSTS state is loaded by the TransportSecurityPersister. By moving it up into profile_impl (which isn't shared by Incognito profiles), the Incognito profiles don't ever load dynamic state.
    
    BUG=104935
    TEST=Inject an HSTS entry for a non-HTTPS site in the main profile using about:net-internals. Open an incognito window and verify that you can still access the HTTP site.
    
    
    Review URL: http://codereview.chromium.org/8997012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116685 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl_io_data.cc
+++ chrome/browser/profiles/profile_impl_io_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/sqlite_persistent_cookie_store.h"
 #include "chrome/browser/prefs/pref_member.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/transport_security_persister.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -262,6 +263,11 @@ void ProfileImplIOData::LazyInitializeInternal(
 
   if (http_server_properties_manager_.get())
     http_server_properties_manager_->InitializeOnIOThread();
+
+  transport_security_persister_.reset(
+      new TransportSecurityPersister(transport_security_state(),
+                                     profile_params->path,
+                                     profile_params->is_incognito));
 
   main_context->set_transport_security_state(transport_security_state());
   media_request_context_->set_transport_security_state(
