664e709b76ba76b9cba6b15566920f5ef4403bf1
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 20:50:02 +0000

    aura: On Linux, avoid CHAR events for Tab, Escape, and Back.
    RootWindowLinux previously synthesized CHAR events for the
    Tab, Escape, and Backspace keys in an attempt to match
    Windows, but it sounds like these events are discarded on
    Windows.  This change also removes a workaround in the Aura
    implementation of UnhandledKeyboardEventHandler.
    
    BUG=108480
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9108031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116731 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_linux.cc
+++ ui/aura/root_window_host_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -236,7 +236,9 @@ int CoalescePendingXIMotionEvents(const XEvent* xev, XEvent* last_event) {
 
 // We emulate Windows' WM_KEYDOWN and WM_CHAR messages.  WM_CHAR events are only
 // generated for certain keys; see
-// http://msdn.microsoft.com/en-us/library/windows/desktop/ms646268.aspx.
+// http://msdn.microsoft.com/en-us/library/windows/desktop/ms646268.aspx.  Per
+// discussion on http://crbug.com/108480, char events should furthermore not be
+// generated for Tab, Escape, and Backspace.
 bool ShouldSendCharEventForKeyboardCode(ui::KeyboardCode keycode) {
   if ((keycode >= ui::VKEY_0 && keycode <= ui::VKEY_9) ||
       (keycode >= ui::VKEY_A && keycode <= ui::VKEY_Z) ||
@@ -245,11 +247,8 @@ bool ShouldSendCharEventForKeyboardCode(ui::KeyboardCode keycode) {
   }
 
   switch (keycode) {
-    case ui::VKEY_BACK:
     case ui::VKEY_RETURN:
-    case ui::VKEY_ESCAPE:
     case ui::VKEY_SPACE:
-    case ui::VKEY_TAB:
     // In addition to the keys listed at MSDN, we include other
     // graphic-character and numpad keys.
     case ui::VKEY_MULTIPLY:
