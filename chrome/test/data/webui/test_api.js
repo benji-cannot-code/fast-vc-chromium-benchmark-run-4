564c6853cf2c64c181bc5eace11b74e10820ace0
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 20:46:31 +0000

    Catch addEventListener as early as possible for WebUI test framework.
    R= kevers@chromium.org, sky@chromium.org
    BUG=109227
    TEST=browser_tests
    
    
    Review URL: http://codereview.chromium.org/9113001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/test_api.js
+++ chrome/test/data/webui/test_api.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -925,6 +925,9 @@ var testing = {};
   function preloadJavascriptLibraries(testFixture, testName) {
     deferGlobalOverrides = true;
 
+    // The document seems to change from the point of preloading to the point of
+    // events (and doesn't fire), whereas the window does not. Listening to the
+    // capture phase allows this event to fire first.
     window.addEventListener('DOMContentLoaded', function() {
       overrideChrome();
 
@@ -933,7 +936,7 @@ var testing = {};
       deferGlobalOverrides = false;
       for (var funcName in globalOverrides)
         overrideGlobal(funcName);
-    });
+    }, true);
     currentTestCase = createTestCase(testFixture, testName);
     currentTestCase.preLoad();
   }
