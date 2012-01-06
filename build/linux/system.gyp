b067f1b4bdada57f901b192131998887fe653d4f
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 20:34:35 +0000

    Revert 116724 - Add gamepad data fetcher for Linux
    Broke ASAN builder:
    http://build.chromium.org/p/chromium.memory/builders/ASAN%20Builder/builds/8480/steps/update/logs/stdio
    
    BUG=79050
    
    Review URL: http://codereview.chromium.org/8899017
    
    TBR=scottmg@google.com
    Review URL: http://codereview.chromium.org/9110039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116726 0039d316-1c4b-4281-b951-d872f2087c98

--- build/linux/system.gyp
+++ build/linux/system.gyp
@@ -626,26 +626,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }],
       ],
     },
-    {
-      'target_name': 'udev',
-      'type': 'none',
-      'conditions': [
-        ['_toolset=="target"', {
-          'direct_dependent_settings': {
-            'cflags': [
-              '<!@(<(pkg-config) --cflags libudev)'
-            ],
-          },
-          'link_settings': {
-            'ldflags': [
-              '<!@(<(pkg-config) --libs-only-L --libs-only-other libudev)',
-            ],
-            'libraries': [
-              '<!@(<(pkg-config) --libs-only-l libudev)',
-            ],
-          },
-        }],
-      ],
-    },
   ],
 }
--- content/browser/gamepad/gamepad_provider.h
+++ content/browser/gamepad/gamepad_provider.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_GAMEPAD_GAMEPAD_PROVIDER_H_
-#define CONTENT_BROWSER_GAMEPAD_GAMEPAD_PROVIDER_H_
+#ifndef CONTENT_BROWSER_GAMEPAD_PROVIDER_H_
+#define CONTENT_BROWSER_GAMEPAD_PROVIDER_H_
 
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -90,4 +90,4 @@ class CONTENT_EXPORT GamepadProvider :
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_GAMEPAD_GAMEPAD_PROVIDER_H_
+#endif  // CONTENT_BROWSER_GAMEPAD_PROVIDER_H_
