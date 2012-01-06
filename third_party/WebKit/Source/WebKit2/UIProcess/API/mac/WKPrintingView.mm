e0c5114c88f4dde7cb58806acba67cb9a0480490
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-06 22:01:00 +0000

    [Mac] Thread verifier assertions when printing         https://bugs.webkit.org/show_bug.cgi?id=75738
            Reviewed by Sam Weinig.
    
            * UIProcess/API/mac/WKPrintingView.mm:
            (-[WKPrintingView dealloc]): Dealloc in main thread, so that WebFrameProxy doesn't get
            deref'ed from a secondary one.
            (-[WKPrintingView _adjustPrintingMarginsForHeaderAndFooter]): This method calls out to client,
            which only expects that on main thread.
            (-[WKPrintingView knowsPageRange:]): Call _adjustPrintingMarginsForHeaderAndFooter on main
            thread.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104334 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKPrintingView.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKPrintingView.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "PrintInfo.h"
 #import "WebData.h"
 #import "WebPageProxy.h"
+#import <WebCore/WebCoreObjCExtras.h>
 #import <wtf/MainThread.h>
 
 using namespace WebKit;
@@ -55,6 +56,14 @@ static BOOL isForcingPreviewUpdate;
     _wkView = wkView;
 
     return self;
+}
+
+- (void)dealloc
+{
+    if (WebCoreObjCScheduleDeallocateOnMainThread([WKPrintingView class], self))
+        return;
+
+    [super dealloc];
 }
 
 - (BOOL)isFlipped
@@ -111,6 +120,8 @@ static BOOL isForcingPreviewUpdate;
 
 - (void)_adjustPrintingMarginsForHeaderAndFooter
 {
+    ASSERT(isMainThread()); // This funciton calls the client, which should only be done on main thread.
+
     NSPrintInfo *info = [_printOperation printInfo];
     NSMutableDictionary *infoDictionary = [info dictionary];
 
@@ -365,7 +376,7 @@ static void prepareDataForPrintingOnSecondaryThread(void* untypedContext)
 
     [self _suspendAutodisplay];
     
-    [self _adjustPrintingMarginsForHeaderAndFooter];
+    [self performSelectorOnMainThread:@selector(_adjustPrintingMarginsForHeaderAndFooter) withObject:nil waitUntilDone:YES];
 
     if ([self _hasPageRects])
         *range = NSMakeRange(1, _printingPageRects.size());
