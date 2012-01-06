9e6a308c6965b7b53bb500d391647249dd8cb348
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 23:06:30 +0000

    Don't pass WebContents* between threads, since it can go away in the meantime. Instead pass the render process and render view IDs, which we can get a WebContents from later if it's valid. Review URL: http://codereview.chromium.org/9123013
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116764 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/cloud_print_signin_dialog.cc
+++ chrome/browser/ui/webui/cloud_print_signin_dialog.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_url.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/dialog_style.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
@@ -187,8 +189,14 @@ bool CloudPrintSigninDelegate::ShouldShowDialogTitle() const {
   return false;
 }
 
-void CreateCloudPrintSigninDialogImpl(WebContents* parent_tab) {
+void CreateCloudPrintSigninDialogImpl(int render_process_id,
+                                      int render_view_id) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  WebContents* parent_tab =
+      tab_util::GetWebContentsByID(render_process_id, render_view_id);
+  if (!parent_tab)
+    return;
   HtmlDialogUIDelegate* dialog_delegate =
       new CloudPrintSigninDelegate(parent_tab);
   BrowserList::GetLastActive()->BrowserShowHtmlDialog(dialog_delegate,
@@ -201,6 +209,8 @@ void CreateCloudPrintSigninDialog(WebContents* parent_tab) {
 
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
-      base::Bind(&CreateCloudPrintSigninDialogImpl, parent_tab));
+      base::Bind(&CreateCloudPrintSigninDialogImpl,
+                 parent_tab->GetRenderProcessHost()->GetID(),
+                 parent_tab->GetRenderViewHost()->routing_id()));
 }
 }  // namespace cloud_print_signin_dialog
