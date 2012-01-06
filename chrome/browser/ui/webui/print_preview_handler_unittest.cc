a4242b9c5c62c5a5ee27b05c49d87317a1d94695
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 03:05:04 +0000

    Print Preview: Fixing tests that were failing on official builds only.
    BUG=NONE
    TEST=unit_tests --gtest_filter=PrintPreviewHandlerTest.* should pass on official builds
    
    
    Review URL: http://codereview.chromium.org/9111038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview_handler_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,10 +39,8 @@ class PrintPreviewHandlerTest : public BrowserWithTestWindowTest {
  protected:
   void SetUp() {
     BrowserWithTestWindowTest::SetUp();
-#if !defined(GOOGLE_CHROME_BUILD) || defined(OS_CHROMEOS)
     CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnablePrintPreview);
-#endif
     ASSERT_TRUE(browser());
     BrowserList::SetLastActive(browser());
     ASSERT_TRUE(BrowserList::GetLastActive());
