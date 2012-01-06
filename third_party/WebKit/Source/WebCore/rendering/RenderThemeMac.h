14f60cac6a8efb881376c455121d9553fec8312e
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-06 00:30:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=75654 Text fields should draw using NSTextFieldCell instead of WebKitSystemInterface
    Reviewed by John Sullivan.
    
    This change should not have any affect on tests or real web sites. It just changed 
    the implementation under the hood to the more modern NSCell approach.
    * rendering/RenderThemeMac.h:
    * rendering/RenderThemeMac.mm:
    (WebCore::RenderThemeMac::paintTextField):
    (WebCore::RenderThemeMac::textField):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeMac.h
@@ -220,6 +220,7 @@ private:
     NSMenu* searchMenuTemplate() const;
     NSSliderCell* sliderThumbHorizontal() const;
     NSSliderCell* sliderThumbVertical() const;
+    NSTextFieldCell* textField() const;
 
 #if ENABLE(METER_TAG)
     NSLevelIndicatorStyle levelIndicatorStyleFor(ControlPart) const;
@@ -239,6 +240,7 @@ private:
     mutable RetainPtr<NSSliderCell> m_sliderThumbHorizontal;
     mutable RetainPtr<NSSliderCell> m_sliderThumbVertical;
     mutable RetainPtr<NSLevelIndicatorCell> m_levelIndicator;
+    mutable RetainPtr<NSTextFieldCell> m_textField;
 
     bool m_isSliderThumbHorizontalPressed;
     bool m_isSliderThumbVerticalPressed;
