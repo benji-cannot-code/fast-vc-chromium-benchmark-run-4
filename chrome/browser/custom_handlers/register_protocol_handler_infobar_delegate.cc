2c9a77438aa8cbd91f67a74513c2da7b9edf85fe
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 02:51:17 +0000

    Make registerProtocolHandler() infobars replace existing infobars for equivalent handlers.
    BUG=109097
    
    
    Review URL: http://codereview.chromium.org/9116003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116632 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/custom_handlers/register_protocol_handler_infobar_delegate.cc
+++ chrome/browser/custom_handlers/register_protocol_handler_infobar_delegate.cc
@@ -98,3 +98,14 @@ bool RegisterProtocolHandlerInfoBarDelegate::LinkClicked(
   owner()->web_contents()->OpenURL(params);
   return false;
 }
+
+bool RegisterProtocolHandlerInfoBarDelegate::IsReplacedBy(
+    RegisterProtocolHandlerInfoBarDelegate* delegate) {
+  return handler_.IsEquivalent(delegate->handler_);
+}
+
+RegisterProtocolHandlerInfoBarDelegate*
+    RegisterProtocolHandlerInfoBarDelegate::
+        AsRegisterProtocolHandlerInfoBarDelegate() {
+  return this;
+}
