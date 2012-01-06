1bd71f0930a06559ef059078855484cea29e2433
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-06 13:34:27 +0000

    [Qt][WK2] QQuickWebView breaks when an empty url is loaded https://bugs.webkit.org/show_bug.cgi?id=75445
    Patch by Rafael Brandao <rafael.lobo@openbossa.org> on 2012-01-06
    Reviewed by Tor Arne Vestbø.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebView::load):
    * UIProcess/API/qt/tests/qmltests/WebView/tst_loadUrl.qml: Check if a request to load
    an empty url is ignored. 'about:blank' should be used instead to load an empty page.
    * UIProcess/API/qt/tests/qmltests/qmltests.pro:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -815,6 +815,9 @@ QQuickWebPage* QQuickWebView::page()
 
 void QQuickWebView::load(const QUrl& url)
 {
+    if (url.isEmpty())
+        return;
+
     Q_D(QQuickWebView);
     d->webPageProxy->loadURL(url.toString());
 }
