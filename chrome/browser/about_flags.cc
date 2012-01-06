0fb441803d5013d9b37283765c09c83c57ada8f5
reveman@google.com <reveman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 20:44:37 +0000

    Disable per-tile painting support on Windows.
    BUG=chromium:107461
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9127006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -491,7 +491,7 @@ const Experiment kExperiments[] = {
     IDS_FLAGS_PER_TILE_PAINTING_NAME,
     IDS_FLAGS_PER_TILE_PAINTING_DESCRIPTION,
 #if defined(USE_SKIA)
-    kOsAll,
+    kOsMac | kOsLinux | kOsCrOS,
 #else
     0,
 #endif
