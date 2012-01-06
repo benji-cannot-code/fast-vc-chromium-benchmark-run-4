42ae5c60b0b26627848dcbcea549639ad3d7d5b3
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-06 21:12:40 +0000

    Make TextTrackCue more mutable https://bugs.webkit.org/show_bug.cgi?id=72555
    Reviewed by Anders Carlsson.
    
    Source/WebCore: 
    
    Test: media/track/track-cue-mutable.html
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::updateActiveTextTrackCues): Do nothing if the "ignore cue updates"
        flag is set
    (WebCore::HTMLMediaElement::textTrackAddCues): Block cue updates until all of the new cues have
        been added, then call updateActiveTextTrackCues so update the display if necessary.
    (WebCore::HTMLMediaElement::textTrackRemoveCues): Block cue updates until all of the new cues have
        been removed, then call updateActiveTextTrackCues so update the display if necessary.
    (WebCore::HTMLMediaElement::textTrackAddCue): Call updateActiveTextTrackCues so update the display if necessary.
    (WebCore::HTMLMediaElement::textTrackRemoveCue): Ditto.
    * html/HTMLMediaElement.h:
    (WebCore::HTMLMediaElement::ignoreTrackDisplayUpdateRequests):
    (WebCore::HTMLMediaElement::beginIgnoringTrackDisplayUpdateRequests):
    (WebCore::HTMLMediaElement::endIgnoringTrackDisplayUpdateRequests):
    
    * html/TextTrack.cpp:
    (WebCore::TextTrack::cueWillChange): New, remove the cue from the media element because its
        position in the interval tree is based on start and end times.
    (WebCore::TextTrack::cueDidChange): Add the cue to the media element.
    * html/TextTrack.h:
    
    * html/TextTrackCue.cpp:
    (WebCore::startKeyword): New, use a static String for the constant.
    (WebCore::middleKeyword): Ditto.
    (WebCore::endKeyword): Ditto.
    (WebCore::horizontalKeyword): Ditto.
    (WebCore::verticalKeyword): Ditto.
    (WebCore::verticallrKeyword): Ditto.
    (WebCore::TextTrackCue::cueWillChange): New, tell the track the cue is about to change.
    (WebCore::TextTrackCue::cueDidChange): New, tell the track the cue has changed.
    (WebCore::TextTrackCue::setId): New, attribute is mutable.
    (WebCore::TextTrackCue::setStartTime): Ditto.
    (WebCore::TextTrackCue::setEndTime): Ditto.
    (WebCore::TextTrackCue::setPauseOnExit): Ditto.
    (WebCore::TextTrackCue::direction): Ditto.
    (WebCore::TextTrackCue::setDirection): Ditto.
    (WebCore::TextTrackCue::setSnapToLines): Ditto.
    (WebCore::TextTrackCue::setLinePosition): Ditto.
    (WebCore::TextTrackCue::setTextPosition): Ditto.
    (WebCore::TextTrackCue::setSize): Ditto.
    (WebCore::TextTrackCue::alignment): Ditto.
    (WebCore::TextTrackCue::setAlignment): Ditto.
    (WebCore::TextTrackCue::parseSettings): Use the static strings.
    * html/TextTrackCue.h:
    (WebCore::TextTrackCue::id):
    (WebCore::TextTrackCue::startTime):
    (WebCore::TextTrackCue::endTime):
    (WebCore::TextTrackCue::pauseOnExit):
    * html/TextTrackCue.idl:
    
    LayoutTests: 
    
    * media/track/track-cue-mutable-expected.txt: Added.
    * media/track/track-cue-mutable.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/TextTrack.h
+++ third_party/WebKit/Source/WebCore/html/TextTrack.h
@@ -99,6 +99,9 @@ public:
 
     void addCue(PassRefPtr<TextTrackCue>, ExceptionCode&);
     void removeCue(TextTrackCue*, ExceptionCode&);
+
+    void cueWillChange(TextTrackCue*);
+    void cueDidChange(TextTrackCue*);
     
     virtual void fireCueChangeEvent();
     DEFINE_ATTRIBUTE_EVENT_LISTENER(cuechange);
