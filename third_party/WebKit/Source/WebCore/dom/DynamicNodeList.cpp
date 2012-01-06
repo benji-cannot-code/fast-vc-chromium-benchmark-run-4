7d9c43ad0f1c442c20c473b1148ea654a78cdc90
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-06 03:14:05 +0000

    REGRESSION(r104210): Dromaeo DOM test score is lower https://bugs.webkit.org/show_bug.cgi?id=75679
    Reviewed by Andreas Kling.
    
    The regression was caused by isDomVersionConsistent not being able to obtain the tree version
    inside isLengthCacheValid when m_cachedItem is null. Fix the regression by always setting
    m_cachedItem to some node when caching the length so that we can obtain the tree version later.
    
    Also address Antti's review comment to fit m_cachedLength, m_isLengthCacheValid, and
    m_isItemCacheValid all in 32-bit.
    
    * dom/DynamicNodeList.cpp:
    (WebCore::DynamicSubtreeNodeList::SubtreeCaches::setLengthCache):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DynamicNodeList.cpp
+++ third_party/WebKit/Source/WebCore/dom/DynamicNodeList.cpp
@@ -42,7 +42,8 @@ void DynamicSubtreeNodeList::SubtreeCaches::setLengthCache(Node* node, unsigned 
     if (m_isItemCacheValid && !domVersionIsConsistent()) {
         m_cachedItem = node;
         m_isItemCacheValid = false;
-    }
+    } else if (!m_isItemCacheValid)
+        m_cachedItem = node; // Used in domVersionIsConsistent.
     m_cachedLength = length;
     m_isLengthCacheValid = true;
     m_domTreeVersionAtTimeOfCaching = node->document()->domTreeVersion();
