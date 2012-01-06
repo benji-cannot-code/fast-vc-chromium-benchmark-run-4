67cd4088a7fdff735ee31fa55adc8fec94b91c99
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 20:51:49 +0000

    Move setting the touchOptimized webui property to WebUI from ChromeWebUI. This is temporary, and like the other toolkit properties it's only used in chrome. Taking it out of ChromeWebUI allows us to remove that class in a followup cl. Review URL: http://codereview.chromium.org/9121009
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116732 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/webui/web_ui.cc
+++ content/browser/webui/web_ui.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/webui/web_ui.h"
 
+#include "base/command_line.h"
 #include "base/json/json_writer.h"
 #include "base/stl_util.h"
 #include "base/utf_string_conversions.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/webui/generic_handler.h"
 #include "content/common/view_messages.h"
 #include "content/public/common/bindings_policy.h"
+#include "content/public/common/content_switches.h"
 
 using content::WebContents;
 using content::WebUIMessageHandler;
@@ -96,6 +98,13 @@ void WebUI::RenderViewCreated(RenderViewHost* render_view_host) {
 #elif defined(TOOLKIT_GTK)
   render_view_host->SetWebUIProperty("toolkit", "GTK");
 #endif  // defined(TOOLKIT_VIEWS)
+
+  // Let the WebUI know that we're looking for UI that's optimized for touch
+  // input.
+  // TODO(rbyers) Figure out the right model for enabling touch-optimized UI
+  // (http://crbug.com/105380).
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kTouchOptimizedUI))
+    render_view_host->SetWebUIProperty("touchOptimized", "true");
 }
 
 void WebUI::CallJavascriptFunction(const std::string& function_name) {
--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -153,6 +153,7 @@ extern const char kSimpleDataSource[];
 CONTENT_EXPORT extern const char kSingleProcess[];
 CONTENT_EXPORT extern const char kSkipGpuDataLoading[];
 CONTENT_EXPORT extern const char kTestSandbox[];
+extern const char kTouchOptimizedUI[];
 extern const char kTraceStartup[];
 extern const char kTraceStartupFile[];
 extern const char kTraceStartupDuration[];
