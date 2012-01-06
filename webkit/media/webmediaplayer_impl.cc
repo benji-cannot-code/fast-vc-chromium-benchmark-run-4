9c811a72a0c67f6a71a97c1b823e3c3a83342e21
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 18:59:27 +0000

    Disable accelerated compositing for audio-only <video> tags. Uses the new API introduced in http://trac.webkit.org/changeset/103857.
    BUG=102329
    TEST=manually tested w/ MediaDocument and mp3 & mp4 files.
    
    Review URL: http://codereview.chromium.org/9041016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116702 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_impl.cc
+++ webkit/media/webmediaplayer_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -759,6 +759,8 @@ void WebMediaPlayerImpl::OnPipelineInitialize(PipelineStatus status) {
     if (hasVideo()) {
       UMA_HISTOGRAM_BOOLEAN("Media.AcceleratedCompositingActive",
                             is_accelerated_compositing_active_);
+    } else {
+      GetClient()->disableAcceleratedCompositing();
     }
 
     if (pipeline_->IsLocalSource())
