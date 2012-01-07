7c89d31735e4875d1608950246902d70ba50e340
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-07 01:23:41 +0000

    Use HashMap<OwnPtr> in SVGAttributeToPropertyMap https://bugs.webkit.org/show_bug.cgi?id=75734
    Reviewed by Daniel Bates.
    
    * svg/properties/SVGAttributeToPropertyMap.cpp:
    (WebCore::SVGAttributeToPropertyMap::addProperties):
    (WebCore::SVGAttributeToPropertyMap::addProperty): use HashMap::add() to avoid an
    extra hash lookup to set a new PropertiesVector.
    (WebCore::SVGAttributeToPropertyMap::synchronizeProperties):
    * svg/properties/SVGAttributeToPropertyMap.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/properties/SVGAttributeToPropertyMap.h
+++ third_party/WebKit/Source/WebCore/svg/properties/SVGAttributeToPropertyMap.h
@@ -34,9 +34,6 @@ struct SVGPropertyInfo;
 
 class SVGAttributeToPropertyMap {
 public:
-    SVGAttributeToPropertyMap() { }
-    ~SVGAttributeToPropertyMap() { deleteAllValues(m_map); }
-
     bool isEmpty() const { return m_map.isEmpty(); }
 
     void addProperties(SVGAttributeToPropertyMap&);
@@ -53,7 +50,7 @@ private:
     PassRefPtr<SVGAnimatedProperty> animatedProperty(SVGElement* contextElement, const QualifiedName& attributeName, const SVGPropertyInfo*);
 
     typedef Vector<const SVGPropertyInfo*> PropertiesVector;
-    typedef HashMap<QualifiedName, PropertiesVector*> AttributeToPropertiesMap;
+    typedef HashMap<QualifiedName, OwnPtr<PropertiesVector> > AttributeToPropertiesMap;
     AttributeToPropertiesMap m_map;
 };
 
