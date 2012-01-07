a0aa8cb0d3a099c75bb1d97475f876319a97fa34
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 00:47:04 +0000

    Revert 116782 - Almost fixes the multithread compositor for accelerated content. This will be fully fixed once the browser compositor does not use the in_process_impl anymore.
    WebKit patch: https://bugs.webkit.org/show_bug.cgi?id=75491
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/9014030
    
    TBR=pkotwicz@chromium.org
    Review URL: http://codereview.chromium.org/9124026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116786 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor.cc
+++ ui/gfx/compositor/compositor.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,9 +52,7 @@ void Compositor::Draw(bool force_clear) {
 
   NotifyStart(force_clear);
   DrawTree();
-  if (!CompositesAsynchronously()) {
-    NotifyEnd();
-  }
+  NotifyEnd();
 }
 
 void Compositor::AddObserver(CompositorObserver* observer) {
@@ -75,10 +73,6 @@ void Compositor::OnRootLayerChanged() {
 
 void Compositor::DrawTree() {
   root_layer_->DrawTree();
-}
-
-bool Compositor::CompositesAsynchronously() {
-  return false;
 }
 
 void Compositor::SwizzleRGBAToBGRAAndFlip(unsigned char* pixels,
@@ -104,15 +98,15 @@ void Compositor::SwizzleRGBAToBGRAAndFlip(unsigned char* pixels,
   }
 }
 
+void Compositor::NotifyStart(bool clear) {
+  OnNotifyStart(clear);
+}
+
 void Compositor::NotifyEnd() {
   OnNotifyEnd();
   FOR_EACH_OBSERVER(CompositorObserver,
                     observer_list_,
                     OnCompositingEnded(this));
-}
-
-void Compositor::NotifyStart(bool clear) {
-  OnNotifyStart(clear);
 }
 
 }  // namespace ui
