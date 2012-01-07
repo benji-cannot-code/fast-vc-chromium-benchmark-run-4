fe3fa817bc1af115d4f442d8db546409ab84c031
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 23:51:17 +0000

    Add a commandline flag for auto confirm/deny of webstore install confirm dialog
    The webstore testing folks asked me for this to use in their webdriver tests.
    
    BUG=none
    TEST=Run chrome with --apps-gallery-install-auto-confirm-for-tests=accept and
    notice that all installs from the webstore immediately complete without the
    confirmation prompt. Then run with =cancel instead and notice that all installs
    fail to complete.
    
    
    Review URL: http://codereview.chromium.org/9108032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webstore_private_apitest.cc
+++ chrome/browser/extensions/extension_webstore_private_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -91,8 +91,10 @@ class ExtensionWebstorePrivateApiTest : public ExtensionApiTest {
  public:
   void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
     ExtensionApiTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitchASCII(switches::kAppsGalleryURL,
-        "http://www.example.com");
+    command_line->AppendSwitchASCII(
+        switches::kAppsGalleryURL, "http://www.example.com");
+    command_line->AppendSwitchASCII(
+        switches::kAppsGalleryInstallAutoConfirmForTests, "accept");
   }
 
   void SetUpInProcessBrowserTestFixture() OVERRIDE {
@@ -100,7 +102,6 @@ class ExtensionWebstorePrivateApiTest : public ExtensionApiTest {
     // API functions.
     host_resolver()->AddRule("www.example.com", "127.0.0.1");
     ASSERT_TRUE(test_server()->Start());
-    SetExtensionInstallDialogAutoConfirmForTests(true);
     ExtensionInstallUI::DisableFailureUIForTests();
   }
 
@@ -234,7 +235,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebstorePrivateApiTest, InstallLocalized) {
 
 // Now test the case where the user cancels the confirmation dialog.
 IN_PROC_BROWSER_TEST_F(ExtensionWebstorePrivateApiTest, InstallCancelled) {
-  SetExtensionInstallDialogAutoConfirmForTests(false);
+  CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kAppsGalleryInstallAutoConfirmForTests, "cancel");
   ASSERT_TRUE(RunInstallTest("cancelled.html", "extension.crx"));
 }
 
