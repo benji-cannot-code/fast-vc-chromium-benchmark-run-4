61209da5708aadd6df167a2d50254c5cf16c7be9
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 05:39:31 +0000

    Revert 116816 - Hook up the SequencedWorkerPool to the browser thread.
    This does some refactoring of the static data in the browser thread so we only have one global object instead of a bunch fo separate arrays.
    
    It also hooks up the visited link master's I/O to use this new system as a proof of concept.
    Review URL: http://codereview.chromium.org/9065009
    
    TBR=brettw@chromium.org
    Review URL: http://codereview.chromium.org/9122022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/visitedlink/visitedlink_master.cc
+++ chrome/browser/visitedlink/visitedlink_master.cc
@@ -192,7 +192,6 @@ void VisitedLinkMaster::InitMembers(Listener* listener, Profile* profile) {
   history_service_override_ = NULL;
   suppress_rebuild_ = false;
   profile_ = profile;
-  sequence_token_ = BrowserThread::GetBlockingPool()->GetSequenceToken();
 
 #ifndef NDEBUG
   posted_asynchronous_operation_ = false;
@@ -242,12 +241,6 @@ VisitedLinkMaster::Hash VisitedLinkMaster::TryToAddURL(const GURL& url) {
     return null_hash_;  // Table is more than 80% full.
 
   return AddFingerprint(fingerprint, true);
-}
-
-void VisitedLinkMaster::PostIOTask(const tracked_objects::Location& from_here,
-                                   const base::Closure& task) {
-  BrowserThread::GetBlockingPool()->PostSequencedWorkerTask(sequence_token_,
-                                                            from_here, task);
 }
 
 void VisitedLinkMaster::AddURL(const GURL& url) {
@@ -488,7 +481,10 @@ bool VisitedLinkMaster::WriteFullTable() {
               hash_table_, table_length_ * sizeof(Fingerprint));
 
   // The hash table may have shrunk, so make sure this is the end.
-  PostIOTask(FROM_HERE, base::Bind(base::IgnoreResult(&TruncateFile), file_));
+  BrowserThread::PostTask(
+      BrowserThread::FILE,
+      FROM_HERE,
+      base::Bind(base::IgnoreResult(&TruncateFile), file_));
   return true;
 }
 
@@ -678,7 +674,11 @@ void VisitedLinkMaster::FreeURLTable() {
   }
   if (!file_)
     return;
-  PostIOTask(FROM_HERE, base::Bind(base::IgnoreResult(&fclose), file_));
+
+  BrowserThread::PostTask(
+      BrowserThread::FILE,
+      FROM_HERE,
+      base::Bind(base::IgnoreResult(&fclose), file_));
 }
 
 bool VisitedLinkMaster::ResizeTableIfNecessary() {
@@ -861,7 +861,9 @@ void VisitedLinkMaster::WriteToFile(FILE* file,
 #ifndef NDEBUG
   posted_asynchronous_operation_ = true;
 #endif
-  PostIOTask(FROM_HERE,
+
+  BrowserThread::PostTask(
+      BrowserThread::FILE, FROM_HERE,
       base::Bind(&AsyncWrite, file, offset,
                  std::string(static_cast<const char*>(data), data_size)));
 }
