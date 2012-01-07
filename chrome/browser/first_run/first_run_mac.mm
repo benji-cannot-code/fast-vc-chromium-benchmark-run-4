fa30174c9126e512dfe903e9bf2caf666a3055d4
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 03:09:15 +0000

    Refactor FirstRun class to first_run namespace progressively(part 3). This cl refactors: ImportFromBrowser ImportBookmarks ImportFromFile ImportNow
    Remove GetFirstRunSentinelFilePath from first_run.h, which was missed from the previous refactoring cl.
    
    BUG=108137
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9108026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run_mac.mm
+++ chrome/browser/first_run/first_run_mac.mm
@@ -14,6 +14,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/process_singleton.h"
 
 namespace first_run {
+namespace internal {
+
+bool ImportBookmarks(const FilePath& import_bookmarks_path) {
+  // http://crbug.com/48880
+  return false;
+}
+
+}  // namespace internal
+}  // namespace first_run
+
+namespace first_run {
 
 void AutoImport(
     Profile* profile,
@@ -52,11 +63,6 @@ void AutoImport(
 }
 
 }  //namespace first_run
-
-bool FirstRun::ImportBookmarks(const FilePath& import_bookmarks_path) {
-  // http://crbug.com/48880
-  return false;
-}
 
 // static
 FilePath FirstRun::MasterPrefsPath() {
