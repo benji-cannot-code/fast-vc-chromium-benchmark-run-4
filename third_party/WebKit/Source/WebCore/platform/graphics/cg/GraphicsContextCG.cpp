4844f2ffdd95d8751ad41b67f287f8722d731e35
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-07 00:29:37 +0000

    [cg] userSpaceOnUse SVG Patterns have the wrong origin https://bugs.webkit.org/show_bug.cgi?id=75741 <rdar://problem/9383222>
    Reviewed by Simon Fraser.
    
    The transformation from pattern space to user space should use the userToBase CTM,
    not the current CTM.
    
    Test: svg/custom/pattern-userSpaceOnUse-userToBaseTransform.xhtml
    
    * platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContext::applyStrokePattern):
    (WebCore::GraphicsContext::applyFillPattern):
    (WebCore::GraphicsContext::getCTM):
    * platform/graphics/cg/TransformationMatrixCG.cpp:
    (WebCore::AffineTransform::AffineTransform): Add a AffineTransform(CGAffineTransform) constructor
    * platform/graphics/transforms/AffineTransform.h:
    
    Add a test that ensures that userSpaceOnUse patterns are rendered in the
    correct orientation and location in both the accelerated compositing and
    regular cases, as well as with SVG translation.
    
    * platform/mac/svg/custom/pattern-userSpaceOnUse-userToBaseTransform-expected.png: Added.
    * platform/mac/svg/custom/pattern-userSpaceOnUse-userToBaseTransform-expected.txt: Added.
    * svg/custom/pattern-userSpaceOnUse-userToBaseTransform.xhtml: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -528,8 +528,9 @@ void GraphicsContext::clipConvexPolygon(size_t numberOfPoints, const FloatPoint*
 void GraphicsContext::applyStrokePattern()
 {
     CGContextRef cgContext = platformContext();
+    AffineTransform userToBaseCTM = AffineTransform(wkGetUserToBaseCTM(cgContext));
 
-    RetainPtr<CGPatternRef> platformPattern(AdoptCF, m_state.strokePattern->createPlatformPattern(getCTM()));
+    RetainPtr<CGPatternRef> platformPattern(AdoptCF, m_state.strokePattern->createPlatformPattern(userToBaseCTM));
     if (!platformPattern)
         return;
 
@@ -543,8 +544,9 @@ void GraphicsContext::applyStrokePattern()
 void GraphicsContext::applyFillPattern()
 {
     CGContextRef cgContext = platformContext();
+    AffineTransform userToBaseCTM = AffineTransform(wkGetUserToBaseCTM(cgContext));
 
-    RetainPtr<CGPatternRef> platformPattern(AdoptCF, m_state.fillPattern->createPlatformPattern(getCTM()));
+    RetainPtr<CGPatternRef> platformPattern(AdoptCF, m_state.fillPattern->createPlatformPattern(userToBaseCTM));
     if (!platformPattern)
         return;
 
@@ -1254,8 +1256,7 @@ AffineTransform GraphicsContext::getCTM() const
     if (paintingDisabled())
         return AffineTransform();
 
-    CGAffineTransform t = CGContextGetCTM(platformContext());
-    return AffineTransform(t.a, t.b, t.c, t.d, t.tx, t.ty);
+    return AffineTransform(CGContextGetCTM(platformContext()));
 }
 
 FloatRect GraphicsContext::roundToDevicePixels(const FloatRect& rect, RoundingMode roundingMode)
