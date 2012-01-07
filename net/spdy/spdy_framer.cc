8b5cf87f47535e1a88ecd7f0d145678d4545006b
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 00:00:15 +0000

    Revert 116772 - Enabled SpdyFramerTest's UnclosedStreamDataCompressors.
    Wasn't able to duplicate ASAN tests failing in 
    UnclosedStreamDataCompressors on my linux build.
    Enabling it to see if the failure was flaky.
    
    BUG=108596
    R=rch
    TEST=network unittests.
    
    Review URL: http://codereview.chromium.org/8982009
    
    TBR=rtenneti@chromium.org
    Review URL: http://codereview.chromium.org/9141002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116777 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_framer.cc
+++ net/spdy/spdy_framer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -700,6 +700,14 @@ size_t SpdyFramer::ProcessControlFramePayload(const char* data, size_t len) {
       DCHECK(!control_frame.has_header_block());
       visitor_->OnControl(&control_frame);
 
+      // If this is a FIN, tell the caller.
+      if (control_frame.type() == SYN_REPLY &&
+          control_frame.flags() & CONTROL_FLAG_FIN) {
+        visitor_->OnStreamFrameData(reinterpret_cast<SpdySynReplyControlFrame*>(
+                                        &control_frame)->stream_id(),
+                                    NULL, 0);
+      }
+
       CHANGE_STATE(SPDY_IGNORE_REMAINING_PAYLOAD);
     }
   }
@@ -742,7 +750,7 @@ size_t SpdyFramer::ProcessDataFramePayload(const char* data, size_t len) {
 
         int rv = inflate(decompressor, Z_SYNC_FLUSH);
         if (rv != Z_OK) {
-          LOG(WARNING) << "ProcessDataFramePayload inflate failure: " << rv;
+          LOG(WARNING) << "inflate failure: " << rv;
           set_error(SPDY_DECOMPRESS_FAILURE);
           return 0;
         }
@@ -1535,7 +1543,7 @@ SpdyFrame* SpdyFramer::DecompressFrameWithZStream(const SpdyFrame& frame,
     }
   }
   if (rv != Z_OK) {  // How can we know that it decompressed everything?
-    LOG(WARNING) << "DecompressFrameWithZStream inflate failure: " << rv;
+    LOG(WARNING) << "inflate failure: " << rv;
     return NULL;
   }
 
@@ -1641,8 +1649,7 @@ bool SpdyFramer::IncrementallyDecompressControlFrameHeaderData(
     int rv = DecompressHeaderBlockInZStream(decomp);
     if (rv != Z_OK) {
       set_error(SPDY_DECOMPRESS_FAILURE);
-      LOG(WARNING) << "IncrementallyDecompressControlFrameHeaderData "
-                      "inflate failure: " << rv;
+      DLOG(WARNING) << "inflate failure: " << rv;
       processed_successfully = false;
     } else {
       size_t decompressed_len = arraysize(buffer) - decomp->avail_out;
