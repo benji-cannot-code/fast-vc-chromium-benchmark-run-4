397e24816c017c25db8f6dfa7a91e45e5f2bebfc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 03:44:00 +0000

    Revert 116803 - Don't use browser windows for platform app shell windows.
    This frees platform apps from having to disable browser behaviors they don't
    want (e.g. keyboard shortcuts).
    
    Reverts part of r112378, which add a "shell" browser window type, and redoes
    part of r114162 to not depend on browser windows when checking if the context
    menu is being displayed for a platform app.
    
    
    Review URL: http://codereview.chromium.org/8985008
    
    TBR=mihaip@chromium.org
    Review URL: http://codereview.chromium.org/9125021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_window_gtk.cc
+++ chrome/browser/ui/gtk/browser_window_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1982,7 +1982,8 @@ void BrowserWindowGtk::ConnectAccelerators() {
 }
 
 void BrowserWindowGtk::UpdateCustomFrame() {
-  gtk_window_set_decorated(window_, !UseCustomFrame());
+  gtk_window_set_decorated(
+      window_, !UseCustomFrame() && browser_->type() != Browser::TYPE_SHELL);
   titlebar_->UpdateCustomFrame(UseCustomFrame() && !IsFullscreen());
   UpdateWindowShape(bounds_.width(), bounds_.height());
 }
