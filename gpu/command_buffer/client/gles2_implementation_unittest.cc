e4935a7e7c5e4e2b54c88de6a1bf31b53ed02e26
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 08:25:57 +0000

    Make StrictSharedIdHandler free its unused ids.
    TEST=none
    BUG=109330
    
    
    Review URL: http://codereview.chromium.org/9109030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116821 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_unittest.cc
+++ gpu/command_buffer/client/gles2_implementation_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using testing::_;
+using testing::AnyNumber;
 using testing::DoAll;
 using testing::InSequence;
 using testing::Invoke;
@@ -448,6 +449,9 @@ class GLES2ImplementationTest : public GLES2CommandBufferTestBase {
   }
 
   virtual void TearDown() {
+    Mock::VerifyAndClear(gl_.get());
+    EXPECT_CALL(*command_buffer_, OnFlush(_)).Times(AnyNumber());
+    gl_.reset();
   }
 
   void Initialize(bool shared_resources, bool bind_generates_resource) {
