5f2b809aad5e8b31f4e2d5baf0696b74320b8fb9
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 03:09:03 +0000

    Don't use browser windows for platform app shell windows.
    This frees platform apps from having to disable browser behaviors they don't
    want (e.g. keyboard shortcuts).
    
    Reverts part of r112378, which add a "shell" browser window type, and redoes
    part of r114162 to not depend on browser windows when checking if the context
    menu is being displayed for a platform app.
    
    
    Review URL: http://codereview.chromium.org/8985008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -103,8 +103,7 @@ class Browser : public TabHandlerDelegate,
     // BrowserTest.StartMaximized.
     TYPE_TABBED = 1,
     TYPE_POPUP = 2,
-    TYPE_PANEL = 3,
-    TYPE_SHELL = 4
+    TYPE_PANEL = 3
   };
 
   // Possible elements of the Browser window.
@@ -852,7 +851,6 @@ class Browser : public TabHandlerDelegate,
   bool is_type_tabbed() const { return type_ == TYPE_TABBED; }
   bool is_type_popup() const { return type_ == TYPE_POPUP; }
   bool is_type_panel() const { return type_ == TYPE_PANEL; }
-  bool is_type_shell() const { return type_ == TYPE_SHELL; }
 
   bool is_app() const;
   bool is_devtools() const;
