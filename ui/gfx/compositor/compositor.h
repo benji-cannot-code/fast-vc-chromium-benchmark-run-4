444d64b1368450d3a9b4727bde535eabee0ccca5
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-07 00:20:18 +0000

    Almost fixes the multithread compositor for accelerated content. This will be fully fixed once the browser compositor does not use the in_process_impl anymore.
    WebKit patch: https://bugs.webkit.org/show_bug.cgi?id=75491
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/9014030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116782 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor.h
+++ ui/gfx/compositor/compositor.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -183,6 +183,7 @@ class COMPOSITOR_EXPORT Compositor : public base::RefCounted<Compositor> {
   virtual void OnWidgetSizeChanged() = 0;
   virtual void OnRootLayerChanged();
   virtual void DrawTree();
+  virtual bool CompositesAsynchronously();
 
   CompositorDelegate* delegate() { return delegate_; }
 
@@ -191,13 +192,13 @@ class COMPOSITOR_EXPORT Compositor : public base::RefCounted<Compositor> {
   static void SwizzleRGBAToBGRAAndFlip(unsigned char* pixels,
                                        const gfx::Size& image_size);
 
+  // Notifies the compositor that compositing is complete.
+  void NotifyEnd();
+
  private:
   // Notifies the compositor that compositing is about to start. See Draw() for
   // notes about |force_clear|.
   void NotifyStart(bool force_clear);
-
-  // Notifies the compositor that compositing is complete.
-  void NotifyEnd();
 
   CompositorDelegate* delegate_;
   gfx::Size size_;
