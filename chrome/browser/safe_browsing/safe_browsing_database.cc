b08c21472c83bbbbd25eecb9ed654030d33444b0
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-08 23:56:43 +0000

    Stop updating SafeBrowsing download hash list.
    BUG=108130
    TEST=wireshark
    
    
    Review URL: http://codereview.chromium.org/9138003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_database.cc
+++ chrome/browser/safe_browsing/safe_browsing_database.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1070,6 +1070,14 @@ bool SafeBrowsingDatabaseNew::UpdateStarted(
     download_listnames.push_back(safe_browsing_util::kBinUrlList);
     download_listnames.push_back(safe_browsing_util::kBinHashList);
     UpdateChunkRanges(download_store_.get(), download_listnames, lists);
+    DCHECK_EQ(lists->back().name,
+              std::string(safe_browsing_util::kBinHashList));
+    // Remove kBinHashList entry so that we do not request updates for it from
+    // the server.  The existing data will still be retained by
+    // SafeBrowsingStoreFile::DoUpdate.
+    // TODO(mattm): write some code to remove the kBinHashList data from the
+    // file?
+    lists->pop_back();
   }
 
   if (csd_whitelist_store_.get()) {
