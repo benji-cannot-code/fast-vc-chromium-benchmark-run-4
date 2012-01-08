915c6818d6f555f6d4daa099afe08872828762e5
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-08 11:45:58 +0000

    Source/WebCore: REGRESSION (r104060): Layout Test fast/media/viewport-media-query.html is occasionally failing https://bugs.webkit.org/show_bug.cgi?id=75633
    Reviewed by Andreas Kling.
            
    If something triggers CSSStyleSelector construction very early, before documentElement is known,
    it won't be able to resolve viewport-related media queries. In the included test case
    the attribute on <html> element triggers the style selector creation. I can't repro
    the fast/media/viewport-media-query.html failure but I suspect it is the same issue with
    a different mechanism for early CSSStyleSelector construction.
            
    - Reset style selector on documentElement change.
    - Remove the code for lazy documentElement initialization. It is not an useful optimization,
      the children of Document rarely change.
    
    Test: fast/media/viewport-media-query-synchronous.html
    
    * WebCore.exp.in:
    * dom/Document.cpp:
    (WebCore::Document::childrenChanged):
    * dom/Document.h:
    (WebCore::Document::documentElement):
    
    LayoutTests: Layout Test fast/media/viewport-media-query.html is occasionally failing
    https://bugs.webkit.org/show_bug.cgi?id=75633
    
    Reviewed by Andreas Kling.
    
    * fast/media/viewport-media-query-synchronous-expected.txt: Added.
    * fast/media/viewport-media-query-synchronous.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -332,8 +332,6 @@ public:
     
     Element* documentElement() const
     {
-        if (!m_documentElement)
-            cacheDocumentElement();
         return m_documentElement.get();
     }
     
@@ -1156,8 +1154,6 @@ private:
     void updateFocusAppearanceTimerFired(Timer<Document>*);
     void updateBaseURL();
 
-    void cacheDocumentElement() const;
-
     void buildAccessKeyMap(TreeScope* root);
 
     void createStyleSelector();
@@ -1256,7 +1252,7 @@ private:
     RefPtr<Node> m_focusedNode;
     RefPtr<Node> m_hoverNode;
     RefPtr<Node> m_activeNode;
-    mutable RefPtr<Element> m_documentElement;
+    RefPtr<Element> m_documentElement;
 
     uint64_t m_domTreeVersion;
     static uint64_t s_globalTreeVersion;
