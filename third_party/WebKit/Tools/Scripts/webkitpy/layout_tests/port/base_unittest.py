99adfe77a984438c16630b70dd1567fbf2de7600
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-09 18:38:49 +0000

    Print an error message when NRWT can't run Apache
    Fixes <http://webkit.org/b/75712> NRWT mysteriously exits when Apache returns an error code
    
    Reviewed by Dirk Pranke.
    
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.check_httpd): Print an error message when Apache returns an error code instead of
    failing silently.
    
    * Scripts/webkitpy/layout_tests/port/base_unittest.py:
    (PortTest.test_check_httpd_success): Test that we don't print anything when we successfully
    run Apache.
    (PortTest.test_httpd_returns_error_code): Test that we print an error message when Apache
    fails.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base_unittest.py
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import logging
 import optparse
 import sys
 import tempfile
@@ -39,7 +40,7 @@ from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.common.system.path import abspath_to_uri
 from webkitpy.thirdparty.mock import Mock
 from webkitpy.tool.mocktool import MockOptions
-from webkitpy.common.system.executive_mock import MockExecutive
+from webkitpy.common.system.executive_mock import MockExecutive, MockExecutive2
 from webkitpy.common.host_mock import MockHost
 
 from webkitpy.layout_tests.port import Port, Driver, DriverOutput
@@ -341,6 +342,24 @@ class PortTest(unittest.TestCase):
 
     def test_operating_system(self):
         self.assertEqual('mac', self.make_port().operating_system())
+
+    def test_check_httpd_success(self):
+        port = self.make_port(executive=MockExecutive2())
+        port._path_to_apache = lambda: '/usr/sbin/httpd'
+        capture = OutputCapture()
+        capture.capture_output()
+        self.assertTrue(port.check_httpd())
+        _, _, logs = capture.restore_output()
+        self.assertEqual('', logs)
+
+    def test_httpd_returns_error_code(self):
+        port = self.make_port(executive=MockExecutive2(exit_code=1))
+        port._path_to_apache = lambda: '/usr/sbin/httpd'
+        capture = OutputCapture()
+        capture.capture_output()
+        self.assertFalse(port.check_httpd())
+        _, _, logs = capture.restore_output()
+        self.assertEqual('httpd seems broken. Cannot run http tests.\n', logs)
 
 
 class VirtualTest(unittest.TestCase):
