d132e66fcbfed3968a1c507d0c750ebb25232742
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 22:43:47 +0000

    Mac + Breakpad + NaCl: Filter out crashes that occur in untrusted code
    We don't want Breakpad to report crashes in NaCl untrusted code
    because it does not produce useful crash dumps in this case and
    because such crashes are typically not caused by a bug in Chromium.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2097
    TEST=covered by NaCl's tests/inbrowser_crash_test, which will be enabled later
    
    
    Review URL: http://codereview.chromium.org/8955001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_mac.mm
+++ chrome/app/breakpad_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/env_vars.h"
 #include "chrome/installer/util/google_update_settings.h"
+#include "native_client/src/trusted/service_runtime/osx/crash_filter.h"
 #include "policy/policy_constants.h"
 
 namespace {
@@ -85,6 +86,15 @@ bool FatalMessageHandler(int severity, const char* file, int line,
   // caller to do it.
   return false;
 }
+
+#if !defined(DISABLE_NACL)
+bool NaClBreakpadCrashFilter(int exception_type,
+                             int exception_code,
+                             mach_port_t crashing_thread,
+                             void* context) {
+  return !NaClMachThreadIsInUntrusted(crashing_thread);
+}
+#endif
 
 }  // namespace
 
@@ -230,6 +240,12 @@ void InitCrashProcessInfo() {
   if (!process_type_switch.empty()) {
     process_type = base::SysUTF8ToNSString(process_type_switch);
   }
+
+#if !defined(DISABLE_NACL)
+  if (process_type_switch == switches::kNaClLoaderProcess) {
+    BreakpadSetFilterCallback(gBreakpadRef, NaClBreakpadCrashFilter, NULL);
+  }
+#endif
 
   // Store process type in crash dump.
   SetCrashKeyValue(@"ptype", process_type);
