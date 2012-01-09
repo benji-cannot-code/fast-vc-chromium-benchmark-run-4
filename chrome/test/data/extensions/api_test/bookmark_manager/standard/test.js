07eee60cc4982d19b810fa25bd7142cd150356d3
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 22:36:27 +0000

    Update BookmarkManager extension API test to use document.execCommand()
    This can help catch breakages where the clipboard is accidentally overwritten
    by nested ScopedClipboardWriters. There is also some minor logic consolidation
    in ScopedClipboardWriterGlue.
    
    BUG=108293
    TEST=browser_tests --gtest_filter=ExtensionApiTest.BookmarkManager
    
    
    Review URL: http://codereview.chromium.org/9125019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/bookmark_manager/standard/test.js
+++ chrome/test/data/extensions/api_test/bookmark_manager/standard/test.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,6 +15,32 @@ const bookmarkManager = chrome.experimental.bookmarkManager;
 var fooNode, fooNode2, barNode, gooNode, count, emptyFolder, emptyFolder2;
 var folder, nodeA, nodeB;
 var childFolder, grandChildFolder, childNodeA, childNodeB;
+
+var clipboardArguments;
+function doCopy() {
+  clipboardArguments = arguments;
+  document.execCommand('copy');
+}
+function doCut() {
+  clipboardArguments = arguments;
+  document.execCommand('cut');
+}
+function doPaste() {
+  clipboardArguments = arguments;
+  document.execCommand('paste');
+}
+document.addEventListener('copy', function (event) {
+  bookmarkManager.copy.apply(null, clipboardArguments);
+  event.preventDefault();
+});
+document.addEventListener('cut', function (event) {
+  bookmarkManager.cut.apply(null, clipboardArguments);
+  event.preventDefault();
+});
+document.addEventListener('paste', function (event) {
+  bookmarkManager.paste.apply(null, clipboardArguments);
+  event.preventDefault();
+});
 
 var tests = [
   function getStrings() {
@@ -167,7 +193,7 @@ var tests = [
 
   function clipboard2() {
     // Copy the fooNode.
-    bookmarkManager.copy([fooNode.id]);
+    doCopy([fooNode.id]);
 
     // Ensure canPaste is now true.
     bookmarkManager.canPaste('1', pass(function(result) {
@@ -175,7 +201,7 @@ var tests = [
     }));
 
     // Paste it.
-    bookmarkManager.paste('1');
+    doPaste('1');
 
     // Ensure it got added at the end.
     bookmarks.getChildren('1', pass(function(result) {
@@ -192,7 +218,7 @@ var tests = [
 
   function clipboard3() {
     // Cut fooNode bookmarks.
-    bookmarkManager.cut([fooNode.id, fooNode2.id]);
+    doCut([fooNode.id, fooNode2.id]);
 
     // Ensure count decreased by 2.
     bookmarks.getChildren('1', pass(function(result) {
@@ -208,7 +234,7 @@ var tests = [
 
   function clipboard4() {
     // Paste the cut bookmarks at a specific position between bar and goo.
-    bookmarkManager.paste('1', [barNode.id]);
+    doPaste('1', [barNode.id]);
 
     // Check that the two bookmarks were pasted after bar.
     bookmarks.getChildren('1', pass(function(result) {
@@ -239,7 +265,7 @@ var tests = [
   // Ensure we can copy empty folders
   function clipboard5() {
     // Copy it.
-    bookmarkManager.copy([emptyFolder.id]);
+    doCopy([emptyFolder.id]);
 
     // Ensure canPaste is now true.
     bookmarkManager.canPaste('1', pass(function(result) {
@@ -247,7 +273,7 @@ var tests = [
     }));
 
     // Paste it at the end of a multiple selection.
-    bookmarkManager.paste('1', [barNode.id, fooNode2.id]);
+    doPaste('1', [barNode.id, fooNode2.id]);
 
     // Ensure it got added at the right place.
     bookmarks.getChildren('1', pass(function(result) {
