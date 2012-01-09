32aee1298e65158b8b6c4e6246a154e4027c6aa7
petkov@chromium.org <petkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 12:26:04 +0000

    Look for the first valid GPU process host in the host-id map.
    This is an alternative fix for the race between destroying a dead host and
    looking up a host that may lead to unnecessary spawning of new GPU processes.
    
    BUG=chromium-os:107491
    TEST=see issue
    
    
    Review URL: http://codereview.chromium.org/9030021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116862 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -166,12 +166,8 @@ static bool HostIsValid(GpuProcessHost* host) {
     return false;
 
   // Check if the GPU process has died and the host is about to be destroyed.
-  if (host->disconnect_was_alive()) {
-    // Ensure that this GPU process host is not used again by removing it from
-    // the host-id map.
-    g_hosts_by_id.Pointer()->Remove(host->host_id());
+  if (host->disconnect_was_alive())
     return false;
-  }
 
   // The Gpu process is invalid if it's not using software, the card is
   // blacklisted, and we can kill it and start over.
@@ -197,10 +193,10 @@ GpuProcessHost* GpuProcessHost::GetForRenderer(
     return NULL;
 
   // The current policy is to ignore the renderer ID and use a single GPU
-  // process for all renderers. Later this will be extended to allow the
-  // use of multiple GPU processes.
-  if (!g_hosts_by_id.Pointer()->IsEmpty()) {
-    IDMap<GpuProcessHost>::iterator it(g_hosts_by_id.Pointer());
+  // process (the first valid host in the host-id map) for all renderers. Later
+  // this will be extended to allow the use of multiple GPU processes.
+  for (IDMap<GpuProcessHost>::iterator it(g_hosts_by_id.Pointer());
+       !it.IsAtEnd(); it.Advance()) {
     GpuProcessHost *host = it.GetCurrentValue();
 
     if (HostIsValid(host))
