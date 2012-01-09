e3dba964e2c7dee73f33f21563ee47d15ca3a710
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 19:30:03 +0000

    Suppress critical error dialogs on Windows
    BUG=100295
    TEST=Manual. Download a file to a USB stick. Disconnect USB stick, restart Chrome and visit chrome://downloads. There should be no "There is no disk in drive..." error and the download should be marked as "Removed."
    
    
    Review URL: http://codereview.chromium.org/9022044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116897 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -181,6 +181,17 @@ void CheckUserDataDirPolicy(FilePath* user_data_dir) {
     if (is_chrome_frame)
       *user_data_dir = user_data_dir->Append(cf_host_dir);
   }
+}
+
+// If we try to access a path that is not currently available, we want the call
+// to fail rather than show an error dialog.
+void SuppressWindowsErrorDialogs() {
+  UINT new_flags = SEM_FAILCRITICALERRORS |
+                   SEM_NOOPENFILEERRORBOX;
+
+  // Preserve existing error mode.
+  UINT existing_flags = SetErrorMode(new_flags);
+  SetErrorMode(existing_flags | new_flags);
 }
 
 #endif  // defined(OS_WIN)
@@ -664,6 +675,9 @@ void ChromeMainDelegate::SandboxInitialized(const std::string& process_type) {
   // AdjustLinuxOOMScore function too.
 #if defined(OS_LINUX)
   AdjustLinuxOOMScore(process_type);
+#endif
+#if defined(OS_WIN)
+  SuppressWindowsErrorDialogs();
 #endif
 }
 
