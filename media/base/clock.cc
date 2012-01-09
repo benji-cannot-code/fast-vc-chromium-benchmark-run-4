94d832b4a368b19070731e63920e8ce211b73b9d
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 19:39:10 +0000

    Revert 116889 - Creating integration tests for media pipeline that use real decoders and demuxers.
    Reverting because Windows TSAN is failing.
    
    
    TEST=PipelineIntegrationTest
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=116349
    
    Review URL: http://codereview.chromium.org/8968035
    
    TBR=acolwell@chromium.org
    Review URL: http://codereview.chromium.org/9146009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116899 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/clock.cc
+++ media/base/clock.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,10 +17,6 @@ Clock::Clock(TimeProvider* time_provider)
 }
 
 Clock::~Clock() {}
-
-bool Clock::IsPlaying() const {
-  return playing_;
-}
 
 base::TimeDelta Clock::Play() {
   DCHECK(!playing_);
--- media/base/clock.h
+++ media/base/clock.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,9 +33,6 @@ class MEDIA_EXPORT Clock {
 
   Clock(TimeProvider* time_provider);
   ~Clock();
-
-  // Returns true if the clock is running.
-  bool IsPlaying() const;
 
   // Starts the clock and returns the current media time, which will increase
   // with respect to the current playback rate.
