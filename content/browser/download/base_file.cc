55c9cf28bc1ad5049036c64c9ec2239be3bca2b0
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 22:24:04 +0000

    Using VLOG instead of DVLOG to display BaseFile errors.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9109040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116930 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/base_file.cc
+++ content/browser/download/base_file.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -61,8 +61,8 @@ net::Error LogError(const char* file, int line, const char* func,
 
 #undef NET_ERROR
 
-  DVLOG(1) << " " << func << "(): " << operation
-           << "() returned error " << error << " (" << err_string << ")";
+  VLOG(1) << " " << func << "(): " << operation
+          << "() returned error " << error << " (" << err_string << ")";
 
   return net_error;
 }
