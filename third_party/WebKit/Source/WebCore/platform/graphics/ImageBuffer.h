734c41cbd19b1bbf09c8509af3a8b467aa37aa0b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-09 23:53:36 +0000

    [chromium win] Creating lots of temporary canvas contexts will crash. https://bugs.webkit.org/show_bug.cgi?id=68420
    When using the skia port, the allocation of 2d canvas backing stores
    no longer needs to be done through a platform API (GDI/CG) because
    canvases now use skia for drawing text.  Removing the allocation through
    GDI on windows prevents resource exhaustion due to unreferenced canvases
    that are awaiting garbage collection.
    
    Patch by Justin Novosad <junov@chromium.org> on 2012-01-09
    Reviewed by Stephen White.
    
    No new tests: Relying on existing canvas layout tests.
    
    * html/HTMLCanvasElement.cpp:
    (WebCore::HTMLCanvasElement::createImageBuffer):
    * platform/graphics/ImageBuffer.h:
    * platform/graphics/skia/ImageBufferSkia.cpp:
    (WebCore::createNonPlatformCanvas):
    (WebCore::ImageBuffer::ImageBuffer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104501 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.h
@@ -60,6 +60,7 @@ namespace WebCore {
 
     enum RenderingMode {
         Unaccelerated,
+        UnacceleratedNonPlatformBuffer, // Use plain memory allocation rather than platform API to allocate backing store.
         Accelerated
     };
     
--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageBufferSkia.cpp
@@ -94,6 +94,13 @@ static SkCanvas* createAcceleratedCanvas(const IntSize& size, ImageBufferData* d
     return canvas;
 }
 
+static SkCanvas* createNonPlatformCanvas(const IntSize& size)
+{
+    SkCanvas* canvas = new SkCanvas();
+    canvas->setDevice(new SkDevice(SkBitmap::kARGB_8888_Config, size.width(), size.height()))->unref();
+    return canvas;
+}
+
 ImageBuffer::ImageBuffer(const IntSize& size, ColorSpace, RenderingMode renderingMode, bool& success)
     : m_data(size)
     , m_size(size)
@@ -102,6 +109,8 @@ ImageBuffer::ImageBuffer(const IntSize& size, ColorSpace, RenderingMode renderin
 
     if (renderingMode == Accelerated)
         canvas = adoptPtr(createAcceleratedCanvas(size, &m_data));
+    else if (renderingMode == UnacceleratedNonPlatformBuffer)
+        canvas = adoptPtr(createNonPlatformCanvas(size));
 
     if (!canvas)
         canvas = adoptPtr(skia::TryCreateBitmapCanvas(size.width(), size.height(), false));
