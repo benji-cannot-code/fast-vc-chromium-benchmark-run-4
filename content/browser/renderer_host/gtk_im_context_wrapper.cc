ba72845aa9a06f2b043e0da1d8cf06676de72c39
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 19:12:53 +0000

    GTK: Seal up GSEALs, focusing on GtkSelectionData.
    BUG=79722
    TEST=compiles
    First Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=116956
    
    Review URL: http://codereview.chromium.org/9151007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117070 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/gtk_im_context_wrapper.cc
+++ content/browser/renderer_host/gtk_im_context_wrapper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -590,9 +590,10 @@ void GtkIMContextWrapper::HandleHostViewRealize(GtkWidget* widget) {
   // We should only set im context's client window once, because when setting
   // client window.im context may destroy and recreate its internal states and
   // objects.
-  if (widget->window) {
-    gtk_im_context_set_client_window(context_, widget->window);
-    gtk_im_context_set_client_window(context_simple_, widget->window);
+  GdkWindow* gdk_window = gtk_widget_get_window(widget);
+  if (gdk_window) {
+    gtk_im_context_set_client_window(context_, gdk_window);
+    gtk_im_context_set_client_window(context_simple_, gdk_window);
   }
 }
 
