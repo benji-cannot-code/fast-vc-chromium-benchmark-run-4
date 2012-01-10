adbbebdd3fe311aaa02dd584960bf17de7500919
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 04:25:57 +0000

    [uber page] The UI doesn't update when profiles are added/removed.
    BUG=NONE
    TEST=Add and remove profiles from chrome://chrome
    
    Review URL: http://codereview.chromium.org/9149016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117003 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/browser_options_handler2.cc
+++ chrome/browser/ui/webui/options2/browser_options_handler2.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_details.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
@@ -289,6 +290,9 @@ void BrowserOptionsHandler::Initialize() {
 
   pref_change_registrar_.Init(profile->GetPrefs());
   pref_change_registrar_.Add(prefs::kHomePage, this);
+
+  registrar_.Add(this, chrome::NOTIFICATION_PROFILE_CACHED_INFO_CHANGED,
+                 content::NotificationService::AllSources());
 
   UpdateSearchEngines();
   UpdateHomePageLabel();
