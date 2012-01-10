37c58ba5ec131b20068ec554480402e467119c82
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 23:26:07 +0000

    Move WebM specific code from ChunkDemuxer to WebMStreamParser.
    BUG=108329 
    TEST=Existing ChunkDemuxer unittests
    TBR=acolwell@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9170002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117113 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_decoder_config.h
+++ media/base/video_decoder_config.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -83,7 +83,11 @@ class MEDIA_EXPORT VideoDecoderConfig {
                   const gfx::Rect& visible_rect,
                   int frame_rate_numerator, int frame_rate_denominator,
                   int aspect_ratio_numerator, int aspect_ratio_denominator,
-                  const uint8* extra_data, size_t extra_data_size);
+                  const uint8* extra_data, size_t extra_data_size,
+                  bool record_stats);
+
+  // Deep copies |video_config|.
+  void CopyFrom(const VideoDecoderConfig& video_config);
 
   // Returns true if this object has appropriate configuration values, false
   // otherwise.
--- media/ffmpeg/ffmpeg_common.cc
+++ media/ffmpeg/ffmpeg_common.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -191,7 +191,8 @@ void AVCodecContextToAudioDecoderConfig(
                      channel_layout,
                      samples_per_second,
                      codec_context->extradata,
-                     codec_context->extradata_size);
+                     codec_context->extradata_size,
+                     true);
 }
 
 void AudioDecoderConfigToAVCodecContext(const AudioDecoderConfig& config,
@@ -259,7 +260,8 @@ void AVStreamToVideoDecoderConfig(
                      aspect_ratio.num,
                      aspect_ratio.den,
                      stream->codec->extradata,
-                     stream->codec->extradata_size);
+                     stream->codec->extradata_size,
+                     true);
 }
 
 void VideoDecoderConfigToAVCodecContext(
