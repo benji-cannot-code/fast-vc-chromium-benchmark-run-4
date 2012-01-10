07ff1a72eed7faafcf130750294629830fbd906d
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 19:23:52 +0000

    Delete the AudioOutputStream correctly (by using Close and not delete).
    TEST=Fixes a crash in the WASAPIAudioOutputStreamTestMono test when mono isn't supported.
    
    Review URL: http://codereview.chromium.org/9169011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117075 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_low_latency_output_win_unittest.cc
+++ media/audio/win/audio_low_latency_output_win_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -469,7 +469,7 @@ TEST(WinAudioOutputTest, WASAPIAudioOutputStreamTestMono) {
   bool opened;
   EXPECT_TRUE(opened = aos->Open());
   if (!opened) {
-    delete aos;
+    aos->Close();
     return;
   }
   // Derive the expected size in bytes of each packet.
