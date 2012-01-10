bcc4697d0a1a57995cfc3b47aff5f3947c53e592
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-10 22:08:47 +0000

    CodeBlock::m_numParameters should be encapsulated https://bugs.webkit.org/show_bug.cgi?id=75985 <rdar://problem/10671020>
    Reviewed by Oliver Hunt.
            
    Encapsulated CodeBlock::m_numParameters and hooked argument profile creation
    into it.  This appears to be performance neutral.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::CodeBlock):
    (JSC::CodeBlock::setNumParameters):
    (JSC::CodeBlock::addParameter):
    * bytecode/CodeBlock.h:
    (JSC::CodeBlock::numParameters):
    (JSC::CodeBlock::addressOfNumParameters):
    (JSC::CodeBlock::offsetOfNumParameters):
    (JSC::CodeBlock::numberOfArgumentValueProfiles):
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::BytecodeGenerator):
    (JSC::BytecodeGenerator::addParameter):
    (JSC::BytecodeGenerator::emitReturn):
    * dfg/DFGAbstractState.cpp:
    (JSC::DFG::AbstractState::AbstractState):
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::ByteCodeParser):
    (JSC::DFG::ByteCodeParser::InlineStackEntry::InlineStackEntry):
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::predictArgumentTypes):
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::compileFunction):
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::checkArgumentTypes):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::SpeculativeJIT::SpeculativeJIT):
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::slideRegisterWindowForCall):
    (JSC::Interpreter::dumpRegisters):
    (JSC::Interpreter::execute):
    (JSC::Interpreter::prepareForRepeatCall):
    * jit/JIT.cpp:
    (JSC::JIT::privateCompile):
    * jit/JITStubs.cpp:
    (JSC::arityCheckFor):
    (JSC::lazyLinkFor):
    * runtime/Executable.cpp:
    (JSC::FunctionExecutable::compileForCallInternal):
    (JSC::FunctionExecutable::compileForConstructInternal):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
@@ -269,6 +269,13 @@ namespace JSC {
 
     public:
         virtual ~CodeBlock();
+        
+        int numParameters() const { return m_numParameters; }
+        void setNumParameters(int newValue);
+        void addParameter();
+        
+        int* addressOfNumParameters() { return &m_numParameters; }
+        static ptrdiff_t offsetOfNumParameters() { return OBJECT_OFFSETOF(CodeBlock, m_numParameters); }
 
         CodeBlock* alternative() { return m_alternative.get(); }
         PassOwnPtr<CodeBlock> releaseAlternative() { return m_alternative.release(); }
@@ -658,12 +665,10 @@ namespace JSC {
 #endif
         
 #if ENABLE(VALUE_PROFILER)
-        void setArgumentValueProfileSize(unsigned size)
-        {
-            m_argumentValueProfiles.resize(size);
-        }
         unsigned numberOfArgumentValueProfiles()
         {
+            ASSERT(m_numParameters >= 0);
+            ASSERT(m_argumentValueProfiles.size() == static_cast<unsigned>(m_numParameters));
             return m_argumentValueProfiles.size();
         }
         ValueProfile* valueProfileForArgument(unsigned argumentIndex)
@@ -1132,7 +1137,6 @@ namespace JSC {
         int m_numCalleeRegisters;
         int m_numVars;
         int m_numCapturedVars;
-        int m_numParameters;
         bool m_isConstructor;
 
         // This is public because otherwise we would have many friends.
@@ -1196,6 +1200,8 @@ namespace JSC {
                 m_rareData = adoptPtr(new RareData);
         }
         
+        int m_numParameters;
+
         WriteBarrier<ScriptExecutable> m_ownerExecutable;
         JSGlobalData* m_globalData;
 
