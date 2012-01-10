c732cc1a806d2a520a3e2adfa02a3c8e83104907
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 17:55:07 +0000

    autocomplete in webui pages: Construct a new event instead of reusing an old one
    BUG=109341
    TEST=use arrow keys on list of autocomplete suggestions on settings page
    
    Review URL: http://codereview.chromium.org/9116034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117047 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/autocomplete_list.js
+++ chrome/browser/resources/options2/autocomplete_list.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -214,14 +214,16 @@ cr.define('options', function() {
           handled = true;
           break;
         case 'Enter':
-          var hadSelection = this.selectedItem != null;
+          // If the user has already selected an item using the arrow keys, then
+          // presses Enter, clear the suggestions, but also keep
+          // |handled| = false, so the input field can handle the event as well.
           this.suggestions = [];
-          // Only count the event as handled if a selection is being commited.
-          handled = hadSelection;
           break;
         case 'Up':
         case 'Down':
-          this.dispatchEvent(event);
+          var newEvent = new cr.Event(event.type);
+          newEvent.keyIdentifier = event.keyIdentifier;
+          this.dispatchEvent(newEvent);
           handled = true;
           break;
       }
