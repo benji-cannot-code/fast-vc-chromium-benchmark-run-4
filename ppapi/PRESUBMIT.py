e3470df7903411e8ea04364ccd90a1fa69dae97e
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 23:18:24 +0000

    ppapi: Add presubmit check for .srpc file changes
    If someone changes a .srpc file, they must include the regenerated
    files from srpcgen in the same commit.
    
    BUG= none
    TEST= none
    
    R=noelallen@google.com,noelallen@chromium.org
    
    Review URL: http://codereview.chromium.org/9152017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117103 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/PRESUBMIT.py
+++ ppapi/PRESUBMIT.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,9 +7,9 @@ import os
 import sys
 import subprocess
 
-def RunCmdAndCheck(cmd, ppapi_dir, err_string, output_api):
+def RunCmdAndCheck(cmd, err_string, output_api, cwd=None):
   results = []
-  p = subprocess.Popen(cmd, cwd=os.path.join(ppapi_dir, 'generators'),
+  p = subprocess.Popen(cmd, cwd=cwd,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
   (p_stdout, p_stderr) = p.communicate()
@@ -33,14 +33,30 @@ def RunUnittests(input_api, output_api):
     cmd = [ sys.executable, 'idl_gen_pnacl.py', '--wnone', '--test']
     ppapi_dir = input_api.PresubmitLocalPath()
     results.extend(RunCmdAndCheck(cmd,
-                                  ppapi_dir,
                                   'PPAPI IDL Pnacl unittest failed.',
-                                  output_api))
+                                  output_api,
+                                  os.path.join(ppapi_dir, 'generators')))
   return results
 
 
+# If any .srpc files were changed, run run_srpcgen.py --diff_mode.
+def CheckSrpcChange(input_api, output_api):
+  if [True for filename in input_api.LocalPaths() if
+      os.path.splitext(filename)[1] == '.srpc']:
+    return RunCmdAndCheck([sys.executable,
+                           os.path.join(input_api.PresubmitLocalPath(),
+                                        'native_client', 'src',
+                                        'shared', 'ppapi_proxy',
+                                        'run_srpcgen.py'),
+                           '--diff_mode'],
+                          'PPAPI SRPC Diff detected: Run run_srpcgen.py.',
+                          output_api)
+  return []
+
 def CheckChange(input_api, output_api):
   results = []
+
+  results.extend(CheckSrpcChange(input_api, output_api))
 
   results.extend(RunUnittests(input_api, output_api))
 
@@ -87,9 +103,9 @@ def CheckChange(input_api, output_api):
   # Only generate output for IDL files references (as *.h or *.idl) in this CL
   cmd.append('--out=' + ','.join([name + '.idl' for name in both]))
   results.extend(RunCmdAndCheck(cmd,
-                                ppapi_dir,
                                 'PPAPI IDL Diff detected: Run the generator.',
-                                output_api))
+                                output_api,
+                                os.path.join(ppapi_dir, 'generators')))
   return results
 
 def CheckChangeOnUpload(input_api, output_api):
