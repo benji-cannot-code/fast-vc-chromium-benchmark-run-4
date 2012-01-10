7d39894f2ec17f584bf449b8a08c98d51d90d3ad
bdibello@google.com <bdibello@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 14:15:42 +0000

    chrome/common/render_messages.h isn't needed by content_settings_utils.cc.
    Review URL: http://codereview.chromium.org/9151020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_utils.cc
+++ chrome/browser/content_settings/content_settings_utils.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/content_settings_pattern.h"
-#include "chrome/common/render_messages.h"
 #include "googleurl/src/gurl.h"
 
 namespace {
