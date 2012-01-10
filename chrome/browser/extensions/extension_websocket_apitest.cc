c4f72f9ec0b2219fd5cb82eb535214192ef41350
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 12:02:09 +0000

    Logging detected LayoutTests path.
    Some trybots fails all layout tests.
    Wrong directory detection is one of possible reasons.
    This change is to log the detected directory for investigation.
    
    BUG=107836
    TEST=ui_tests
    
    
    Review URL: http://codereview.chromium.org/8961010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117023 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "base/path_service.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_paths.h"
@@ -12,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
   FilePath websocket_root_dir;
   ASSERT_TRUE(PathService::Get(chrome::DIR_LAYOUT_TESTS, &websocket_root_dir));
+  // TODO(toyoshim): Remove following logging after a bug investigation.
+  // http://crbug.com/107836 .
+  LOG(INFO) << "Assume LayoutTests in " << websocket_root_dir.MaybeAsASCII();
 
   ui_test_utils::TestWebSocketServer server;
   ASSERT_TRUE(server.Start(websocket_root_dir));
