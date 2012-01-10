3df7232f9164c4143f909954750aab8a50fe8168
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-10 01:32:54 +0000

    webkitpy.layout_tests.port.mock_drt_unittest.MockChromiumDRTTest has been failing on cr-win https://bugs.webkit.org/show_bug.cgi?id=75884
    Reviewed by Ryosuke Niwa.
    
    Fix regression introduced (revealed?) in r104340. Chromium file
    url syntax is different on win32 and the test results weren't
    expecting that.
    
    * Scripts/webkitpy/layout_tests/models/test_expectations.py:
    (TestExpectations._report_errors):
    * Scripts/webkitpy/layout_tests/models/test_expectations_unittest.py:
    (test_parse_error_nonfatal):
    * Scripts/webkitpy/layout_tests/port/mock_drt_unittest.py:
    (MockDRTTest.input_line):
    (MockChromiumDRTTest.test_pixeltest__fails):
    * Scripts/webkitpy/layout_tests/port/test.py:
    * Scripts/webkitpy/to_be_moved/rebaseline_chromium_webkit_tests_unittest.py:
    (TestRebaseliner.make_rebaseliner):
    (TestRebaseliner.test_one_platform):
    (TestRebaseliner.test_all_platforms):
    (TestRebaseliner.test_png_file_with_comment):
    (TestRebaseliner.test_png_file_with_comment_remove_old_checksum):
    (TestRebaseliner.test_png_file_with_comment_as_duplicate):
    (TestRealMain.test_all_platforms):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from __future__ import with_statement
 
 import base64
+import sys
 import time
 
 from webkitpy.layout_tests.port import Port, Driver, DriverOutput
@@ -221,7 +222,10 @@ layer at (0,0) size 800x34
 # this works. The path contains a '.' in the name because we've seen bugs
 # related to this before.
 
-LAYOUT_TEST_DIR = '/test.checkout/LayoutTests'
+if sys.platform == 'win32':
+    LAYOUT_TEST_DIR = 'c:/test.checkout/LayoutTests'
+else:
+    LAYOUT_TEST_DIR = '/test.checkout/LayoutTests'
 
 
 # Here we synthesize an in-memory filesystem from the test list
