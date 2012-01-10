04f54ac3dd0c23aec8ce7142c4818053691a5f4c
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 23:17:10 +0000

    fix parsing of gdata document feed and title.
    BUG=none.
    TEST=none.
    
    Review URL: http://codereview.chromium.org/9166011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117100 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_parser.cc
+++ chrome/browser/chromeos/gdata/gdata_parser.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -557,7 +557,7 @@ bool DocumentFeed::Parse(const DictionaryValue* value_dict) {
     return false;
   }
 
-  if (!value_dict->GetString(kUpdatedField, &title_)) {
+  if (!value_dict->GetString(kTitleField, &title_)) {
     DVLOG(1) << "Feed with no title!";
     return false;
   }
