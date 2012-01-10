4a378b325a3ba414d3bdaa94fba52437d9acb701
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-10 00:19:06 +0000

    Disable V2 authenticator temporarily until it is finished.
    BUG=105214
    
    
    Review URL: http://codereview.chromium.org/9164001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116967 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/it2me_host_authenticator_factory.cc
+++ remoting/protocol/it2me_host_authenticator_factory.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,10 +28,13 @@ It2MeHostAuthenticatorFactory::~It2MeHostAuthenticatorFactory() {
 Authenticator* It2MeHostAuthenticatorFactory::CreateAuthenticator(
     const std::string& remote_jid,
     const buzz::XmlElement* first_message) {
-  if (V2Authenticator::IsEkeMessage(first_message)) {
-    return V2Authenticator::CreateForHost(
-        local_cert_, local_private_key_.get(), shared_secret_);
-  }
+  // TODO(sergeyu): V2 authenticator is not finished yet. Enable it
+  // here when it is finished. crbug.com/105214
+  //
+  // if (V2Authenticator::IsEkeMessage(first_message)) {
+  //   return V2Authenticator::CreateForHost(
+  //       local_cert_, local_private_key_.get(), shared_secret_);
+  // }
 
   return new V1HostAuthenticator(local_cert_, local_private_key_.get(),
                                  shared_secret_, remote_jid);
