e4fa514e4df32f68c8a921bcd6f40012eb9469d0
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 23:39:18 +0000

    Convert use of int ms to TimeDelta in files owned by johnnyg.
    R=johnnyg@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9186020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/balloon_collection_impl.cc
+++ chrome/browser/notifications/balloon_collection_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,7 +33,8 @@ const int kMinAllowedBalloonCount = 2;
 
 // Delay from the mouse leaving the balloon collection before
 // there is a relayout, in milliseconds.
-const int kRepositionDelay = 300;
+const base::TimeDelta kRepositionDelay =
+    base::TimeDelta::FromMilliseconds(300);
 
 // The spacing between the balloon and the panel.
 const int kVerticalSpacingBetweenBalloonAndPanel = 5;
