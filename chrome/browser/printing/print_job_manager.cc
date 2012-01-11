89a179715c791f5ba13cabf941f75fde16bb9063
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 00:15:50 +0000

    Cleanup: Make PrintJobManager::printing_enabled() const.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9114048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job_manager.cc
+++ chrome/browser/printing/print_job_manager.cc
@@ -1,16 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/printing/print_job_manager.h"
 
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/printing/print_job.h"
 #include "chrome/browser/printing/printer_query.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "printing/printed_document.h"
 #include "printing/printed_page.h"
@@ -165,6 +165,10 @@ void PrintJobManager::OnPrintJobEvent(
       break;
     }
   }
+}
+
+bool PrintJobManager::printing_enabled() const {
+  return *printing_enabled_;
 }
 
 }  // namespace printing
--- chrome/browser/printing/print_job_manager.h
+++ chrome/browser/printing/print_job_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -55,9 +55,9 @@ class PrintJobManager : public content::NotificationObserver {
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  bool printing_enabled() {
-    return *printing_enabled_;
-  }
+  // Only accessed on the IO thread. UI thread can query
+  // prefs::kPrintingEnabled via g_browser_process->local_state() directly.
+  bool printing_enabled() const;
 
  private:
   typedef std::vector<scoped_refptr<PrintJob> > PrintJobs;
@@ -69,9 +69,6 @@ class PrintJobManager : public content::NotificationObserver {
 
   content::NotificationRegistrar registrar_;
 
-  // Used to serialize access to queued_workers_.
-  base::Lock lock_;
-
   // Printing is enabled/disabled. For printing with the native print dialog,
   // this variable is checked at only one place, by
   // PrintingMessageFilter::OnGetDefaultPrintSettings. If its value is true
@@ -81,6 +78,9 @@ class PrintJobManager : public content::NotificationObserver {
   // PrintingMessageFilter::OnUpdatePrintSettings, which gets called multiple
   // times in the print preview workflow.
   BooleanPrefMember printing_enabled_;
+
+  // Used to serialize access to queued_workers_.
+  base::Lock lock_;
 
   PrinterQueries queued_queries_;
 
