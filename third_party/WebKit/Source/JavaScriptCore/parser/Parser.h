f96cc4235cec8480c1d168d244b77246df5aa067
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-11 23:15:20 +0000

    Typo in error message: Unexpected token 'defualt' https://bugs.webkit.org/show_bug.cgi?id=75105
    Patch by Eugene Girard <girard@google.com> on 2012-01-11
    Reviewed by Simon Fraser.
    
    Source/JavaScriptCore:
    
    * parser/Parser.h:
    (JSC::Parser::getTokenName):
    
    LayoutTests:
    
    * fast/js/reserved-words-as-property-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/parser/Parser.h
+++ third_party/WebKit/Source/JavaScriptCore/parser/Parser.h
@@ -560,7 +560,7 @@ private:
         case CASE: 
             return "case";
         case DEFAULT: 
-            return "defualt";
+            return "default";
         case FOR: 
             return "for";
         case NEW: 
