9a2ab3c7736fa6456891eeba9befe776d9084a7d
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 21:36:14 +0000

    debug::Alias GetErrorCode in download_database.cc
    BUG=96627
    
    Review URL: http://codereview.chromium.org/9187008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/download_database.cc
+++ chrome/browser/history/download_database.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/debug/alias.h"
 #include "base/file_path.h"
 #include "base/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -284,6 +285,8 @@ void DownloadDatabase::RemoveDownloadsBetween(base::Time delete_begin,
       int64 id_to_delete = dbg_statement.ColumnInt64(0);
       returned_ids_.erase(id_to_delete);
     }
+    int last_error_code = GetDB().GetErrorCode();
+    base::debug::Alias(&last_error_code);
     CHECK_96627(dbg_statement.Succeeded());
   }
 
