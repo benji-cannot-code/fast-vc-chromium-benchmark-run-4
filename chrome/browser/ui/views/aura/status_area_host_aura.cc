d3c62c1603dff321cca1757d9ab8c09be60c31ba
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 19:41:53 +0000

    Makes StatusAuraHostAura get the right profile when displaying various options pages.
    BUG=106209
    TEST=none
    R=ben@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9139019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/aura/status_area_host_aura.cc
+++ chrome/browser/ui/views/aura/status_area_host_aura.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/status/clock_menu_button.h"
 #include "chrome/browser/chromeos/status/memory_menu_button.h"
 #include "chrome/browser/chromeos/status/status_area_view.h"
+#include "chrome/browser/defaults.h"
+#include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/view_ids.h"
@@ -105,6 +107,12 @@ void StatusAreaHostAura::ExecuteStatusAreaCommand(
 #if defined(OS_CHROMEOS)
   if (chromeos::StatusAreaViewChromeos::IsBrowserMode()) {
     Profile* profile = ProfileManager::GetDefaultProfile();
+    if (browser_defaults::kAlwaysOpenIncognitoWindow &&
+        IncognitoModePrefs::ShouldLaunchIncognito(
+            *CommandLine::ForCurrentProcess(),
+            profile->GetPrefs())) {
+      profile = profile->GetOffTheRecordProfile();
+    }
     Browser* browser = BrowserList::FindBrowserWithProfile(profile);
     if (!browser)
       browser = Browser::Create(profile);
