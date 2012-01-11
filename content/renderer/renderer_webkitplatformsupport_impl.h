144398cbd7d2b1d03af102888d77efdb47522d7b
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 10:58:50 +0000

    Adding support for MediaStream and PeerConnection functionality.
    BUG=
    TEST=content_unittests
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=117187
    
    Review URL: http://codereview.chromium.org/8060055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117201 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.h
+++ content/renderer/renderer_webkitplatformsupport_impl.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -93,6 +93,8 @@ class CONTENT_EXPORT RendererWebKitPlatformSupportImpl
   virtual WebKit::WebString userAgent(const WebKit::WebURL& url) OVERRIDE;
   virtual void GetPlugins(bool refresh,
                           std::vector<webkit::WebPluginInfo>* plugins) OVERRIDE;
+  virtual WebKit::WebPeerConnectionHandler* createPeerConnectionHandler(
+      WebKit::WebPeerConnectionHandlerClient* client) OVERRIDE;
 
  private:
   bool CheckPreparsedJsCachingEnabled() const;
