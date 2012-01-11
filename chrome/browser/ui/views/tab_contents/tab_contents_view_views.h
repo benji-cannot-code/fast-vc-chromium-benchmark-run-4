41f9509877a831d4f91e428c4f0c35349188eeb6
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 15:53:27 +0000

    Revert 117220 - Move RenderViewHostDelegateViewHelper to content.
    Rename to be something less of a tongue-twister.
    
    BUG=93804, 95573
    TEST=no visible change
    
    Review URL: http://codereview.chromium.org/9114047
    
    TBR=avi@chromium.org
    Review URL: http://codereview.chromium.org/9186006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/tab_contents_view_views.h
+++ chrome/browser/ui/views/tab_contents/tab_contents_view_views.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/timer.h"
+#include "chrome/browser/tab_contents/render_view_host_delegate_helper.h"
 #include "chrome/browser/ui/views/tab_contents/native_tab_contents_view_delegate.h"
 #include "content/browser/tab_contents/tab_contents_view.h"
-#include "content/browser/tab_contents/tab_contents_view_helper.h"
 #include "ui/views/widget/widget.h"
 
 class ConstrainedWindowGtk;
@@ -148,8 +148,8 @@ class TabContentsViewViews : public views::Widget,
   // The WebContents whose contents we display.
   content::WebContents* web_contents_;
 
-  // Common implementations of some TabContentsView methods.
-  TabContentsViewHelper tab_contents_view_helper_;
+  // Common implementations of some RenderViewHostDelegate::View methods.
+  RenderViewHostDelegateViewHelper delegate_view_helper_;
 
   NativeTabContentsView* native_tab_contents_view_;
 
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2366,6 +2366,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/tab_contents/render_view_context_menu_mac.mm',
         'browser/tab_contents/render_view_context_menu_observer.cc',
         'browser/tab_contents/render_view_context_menu_observer.h',
+        'browser/tab_contents/render_view_host_delegate_helper.cc',
+        'browser/tab_contents/render_view_host_delegate_helper.h',
         'browser/tab_contents/retargeting_details.h',
         'browser/tab_contents/tab_contents_ssl_helper.cc',
         'browser/tab_contents/tab_contents_ssl_helper.h',
