b28541497aaf128181d6d217069c0a5e5fb36348
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 03:11:33 +0000

    revert 117139 - Revert 117136 separate about page into its own page (included in chrome://chrome)
    for now it's only enabled in cros, but we plan to have a version for desktop chrome as well, which is why I took it out of the chromeos namespace.
    
    BUG=109203
    TEST=manual
    
    reason for the failure was conflict with r117025 as well as lack of cq coverage of chromeos (my local branch was behind r117025)
    
    TBR=dbeam
    
    Review URL: http://codereview.chromium.org/9114061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117160 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options_bundle.js
+++ chrome/browser/resources/options2/options_bundle.js
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="controlled_setting.js"></include>
 <include src="options_page.js"></include>
 <if expr="pp_ifdef('chromeos')">
-  <include src="about_page.js"></include>
   <include src="../chromeos/user_images_grid.js"></include>
   <include src="chromeos/cellular_plan_element.js"></include>
   <include src="chromeos/change_picture_options.js"></include>
@@ -32,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   <include src="chromeos/accounts_user_name_edit.js"></include>
   <include src="chromeos/virtual_keyboard.js"></include>
   <include src="chromeos/virtual_keyboard_list.js"></include>
-  var AboutPage = options.AboutPage;
   var AccountsOptions = options.AccountsOptions;
   var ChangePictureOptions = options.ChangePictureOptions;
   var InternetOptions = options.InternetOptions;
