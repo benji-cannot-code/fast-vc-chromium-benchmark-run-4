673423dee8546687871adb94a9f99a65a9d31c8b
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 00:11:24 +0000

    Return correct result from XmppSignalStrategy::HandleStanza().
    This code was broken in revision 116192.
    
    BUG=109661
    
    
    Review URL: http://codereview.chromium.org/9168021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117123 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/jingle_glue/xmpp_signal_strategy.cc
+++ remoting/jingle_glue/xmpp_signal_strategy.cc
@@ -123,7 +123,7 @@ bool XmppSignalStrategy::HandleStanza(const buzz::XmlElement* stanza) {
   Listener* listener;
   while ((listener = it.GetNext()) != NULL) {
     if (listener->OnSignalStrategyIncomingStanza(stanza))
-      break;
+      return true;
   }
   return false;
 }
