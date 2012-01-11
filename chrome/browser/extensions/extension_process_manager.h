acfea93ec81e4695bb6312f868616fc0a9b922d5
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 23:33:49 +0000

    More correct ShellWindow/ExtensionHost lifetime
    We now close shell windows (and dispose of their extension hosts) when the
    browser shuts down and when their extension is unloaded.
    
    BUG=109639
    R=mpcomplete@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9167037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117323 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.h
+++ chrome/browser/extensions/extension_process_manager.h
@@ -34,8 +34,6 @@ class ExtensionProcessManager : public content::NotificationObserver {
   static ExtensionProcessManager* Create(Profile* profile);
   virtual ~ExtensionProcessManager();
 
-  ExtensionHost* CreateShellHost(const Extension* extension, const GURL& url);
-
   // Creates a new ExtensionHost with its associated view, grouping it in the
   // appropriate SiteInstance (and therefore process) based on the URL and
   // profile.
@@ -56,8 +54,7 @@ class ExtensionProcessManager : public content::NotificationObserver {
                                    Browser* browser);
   ExtensionHost* CreateInfobarHost(const GURL& url,
                                    Browser* browser);
-
-  ExtensionHost* CreateShellHost(const GURL& url, Browser* browser);
+  ExtensionHost* CreateShellHost(const Extension* extension, const GURL& url);
 
   // Open the extension's options page.
   void OpenOptionsPage(const Extension* extension, Browser* browser);
