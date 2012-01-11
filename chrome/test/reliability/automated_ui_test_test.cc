207bc8f33d14c3c156918c0c6f2629251fec1c12
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 07:17:48 +0000

    Re-enable AutomatedUITestBase.OpenCloseBrowserWindowWithAccelerator on Aura.
    This test does not seem to fail any longer.
    
    BUG=108496
    TEST=Locally and try bot
    
    
    Review URL: http://codereview.chromium.org/9155009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/reliability/automated_ui_test_test.cc
+++ chrome/test/reliability/automated_ui_test_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -257,10 +257,6 @@ TEST_F(AutomatedUITestBase, IncognitoWindow) {
 #if defined(OS_WIN)
 #define MAYBE_OpenCloseBrowserWindowWithAccelerator \
         FLAKY_OpenCloseBrowserWindowWithAccelerator
-#elif defined(USE_AURA)
-// See crbug.com/108496.
-#define MAYBE_OpenCloseBrowserWindowWithAccelerator \
-        DISABLED_OpenCloseBrowserWindowWithAccelerator
 #else
 #define MAYBE_OpenCloseBrowserWindowWithAccelerator \
         OpenCloseBrowserWindowWithAccelerator
