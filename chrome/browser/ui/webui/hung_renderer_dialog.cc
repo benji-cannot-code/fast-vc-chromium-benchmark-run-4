32fb5fea42ba3a6bbcd38f18a1bfcaf2b41ea3ed
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 16:46:52 +0000

    Adds a trivial views based table implementation (only supports single selection, even for grouptableview) and wires it up for hung renderer and ssl client certificate.
    BUG=109665
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/9181019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/hung_renderer_dialog.cc
+++ chrome/browser/ui/webui/hung_renderer_dialog.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,7 +42,10 @@ const int kHungRendererDialogHeight = 200;
 namespace browser {
 
 void ShowHungRendererDialog(WebContents* contents) {
-#if defined(OS_CHROMEOS) || defined(USE_AURA)
+  // TODO(jhawkins): move this into a non-webui location.
+#if defined(USE_AURA)
+  ShowNativeHungRendererDialog(contents);
+#elif defined(OS_CHROMEOS)
   HungRendererDialog::ShowHungRendererDialog(contents);
 #else
   // TODO(rbyers): Remove IsMoreWebUI check once we decide for sure which
@@ -55,7 +58,9 @@ void ShowHungRendererDialog(WebContents* contents) {
 }
 
 void HideHungRendererDialog(WebContents* contents) {
-#if defined(OS_CHROMEOS) || defined(USE_AURA)
+#if defined(USE_AURA)
+  HideNativeHungRendererDialog(contents);
+#elif defined(OS_CHROMEOS)
   HungRendererDialog::HideHungRendererDialog(contents);
 #else
   if (chrome_web_ui::IsMoreWebUI())
