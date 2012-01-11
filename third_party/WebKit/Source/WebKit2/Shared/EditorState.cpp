de81ee782b2581b578267613d10106a8ea28693f
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-11 14:58:22 +0000

    [Qt] Add the editor rect to the EditorState https://bugs.webkit.org/show_bug.cgi?id=76053
    Reviewed by Simon Hausmann.
    
    Also rename some members to match the new Qt naming scheme.
    
    * Shared/EditorState.cpp:
    (WebKit::EditorState::encode):
    (WebKit::EditorState::decode):
    * Shared/EditorState.h:
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebView::inputMethodQuery):
    * WebProcess/WebCoreSupport/WebEditorClient.cpp:
    (WebKit::WebEditorClient::respondToChangedSelection):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::editorState):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/EditorState.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/EditorState.cpp
@@ -45,7 +45,8 @@ void EditorState::encode(CoreIPC::ArgumentEncoder* encoder) const
 #if PLATFORM(QT)
     encoder->encode(cursorPosition);
     encoder->encode(anchorPosition);
-    encoder->encode(microFocus);
+    encoder->encode(editorRect);
+    encoder->encode(cursorRect);
     encoder->encode(compositionRect);
     encoder->encode(selectedText);
     encoder->encode(surroundingText);
@@ -82,7 +83,10 @@ bool EditorState::decode(CoreIPC::ArgumentDecoder* decoder, EditorState& result)
     if (!decoder->decode(result.anchorPosition))
         return false;
 
-    if (!decoder->decode(result.microFocus))
+    if (!decoder->decode(result.editorRect))
+        return false;
+
+    if (!decoder->decode(result.cursorRect))
         return false;
 
     if (!decoder->decode(result.compositionRect))
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -399,6 +399,9 @@ EditorState WebPage::editorState() const
     if (!scope)
         return result;
 
+    if (selectionRoot)
+        result.editorRect = frame->view()->contentsToWindow(selectionRoot->getRect());
+
     RefPtr<Range> range;
     if (result.hasComposition && (range = frame->editor()->compositionRange())) {
         frame->editor()->getCompositionSelection(result.anchorPosition, result.cursorPosition);
@@ -416,7 +419,7 @@ EditorState WebPage::editorState() const
     }
 
     if (range)
-        result.microFocus = frame->view()->contentsToWindow(frame->editor()->firstRectForRange(range.get()));
+        result.cursorRect = frame->view()->contentsToWindow(frame->editor()->firstRectForRange(range.get()));
 
     // FIXME: We should only transfer innerText when it changes and do this on the UI side.
     if (result.isContentEditable && !result.isInPasswordField) {
