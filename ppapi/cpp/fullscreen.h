f4e9ceabeab7009a1a9e7a2200c7eca81a6fc9a6
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 22:57:10 +0000

    Fixed lint errors
    Review URL: http://codereview.chromium.org/9111026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117315 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/fullscreen.h
+++ ppapi/cpp/fullscreen.h
@@ -31,7 +31,7 @@ class Fullscreen {
   /// fullscreen mode.
   ///
   /// @return <code>true</code> if the module instance is in fullscreen mode,
-  ///<code>false</code> if the module instance is not in fullscreen mode.
+  /// <code>false</code> if the module instance is not in fullscreen mode.
   bool IsFullscreen();
 
   /// SetFullscreen() switches the module instance to and from fullscreen
@@ -49,7 +49,7 @@ class Fullscreen {
   /// @param[in] fullscreen <code>true</code> to enter fullscreen mode, or
   /// <code>false</code> to exit fullscreen mode.
   ///
-  ///@return <code>true</code> on success or <code>false</code> on
+  /// @return <code>true</code> on success or <code>false</code> on
   /// failure.
   bool SetFullscreen(bool fullscreen);
 
