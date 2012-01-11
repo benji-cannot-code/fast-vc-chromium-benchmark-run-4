f2c4f71c369fd8a1ed86d7c7afe70c409ffe39b7
dslomov@google.com <dslomov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-11 17:40:58 +0000

    [Chromium] Remove obsolete references to WebWorker class. https://bugs.webkit.org/show_bug.cgi?id=76020
    Reviewed by David Levin.
    
    Source/WebKit/chromium:
    
    * public/WebFrameClient.h:
    * public/WebSharedWorkerClient.h:
    
    Tools:
    
    * DumpRenderTree/chromium/TestWebWorker.h: Removed.
    * DumpRenderTree/chromium/WebViewHost.cpp:
    * DumpRenderTree/chromium/WebViewHost.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TestNavigationController.h"
 #include "TestShell.h"
 #include "TestWebPlugin.h"
-#include "TestWebWorker.h"
 #include "platform/WebCString.h"
 #include "WebConsoleMessage.h"
 #include "WebContextMenuData.h"
@@ -843,11 +842,6 @@ WebPlugin* WebViewHost::createPlugin(WebFrame* frame, const WebPluginParams& par
         return new TestWebPlugin(frame, params);
 
     return webkit_support::CreateWebPlugin(frame, params);
-}
-
-WebWorker* WebViewHost::createWorker(WebFrame*, WebSharedWorkerClient*)
-{
-    return new TestWebWorker();
 }
 
 WebMediaPlayer* WebViewHost::createMediaPlayer(WebFrame* frame, WebMediaPlayerClient* client)
