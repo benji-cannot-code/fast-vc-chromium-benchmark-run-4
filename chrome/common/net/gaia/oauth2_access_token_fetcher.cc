190aa31937e1859a8dd8d12e8c5d0b959a95264b
arthurhsu@chromium.org <arthurhsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 22:27:38 +0000

    Coverity fix: Big parameter passed by value
    CID=102492
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9007025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/gaia/oauth2_access_token_fetcher.cc
+++ chrome/common/net/gaia/oauth2_access_token_fetcher.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -153,7 +153,8 @@ void OAuth2AccessTokenFetcher::OnGetTokenSuccess(
   consumer_->OnGetTokenSuccess(access_token);
 }
 
-void OAuth2AccessTokenFetcher::OnGetTokenFailure(GoogleServiceAuthError error) {
+void OAuth2AccessTokenFetcher::OnGetTokenFailure(
+    const GoogleServiceAuthError& error) {
   state_ = ERROR_STATE;
   consumer_->OnGetTokenFailure(error);
 }
--- chrome/common/net/gaia/oauth2_access_token_fetcher.h
+++ chrome/common/net/gaia/oauth2_access_token_fetcher.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -77,7 +77,7 @@ class OAuth2AccessTokenFetcher : public content::URLFetcherDelegate {
 
   // Helper mehtods for reporting back results.
   void OnGetTokenSuccess(const std::string& access_token);
-  void OnGetTokenFailure(GoogleServiceAuthError error);
+  void OnGetTokenFailure(const GoogleServiceAuthError& error);
 
   // Other helpers.
   static GURL MakeGetAccessTokenUrl();
