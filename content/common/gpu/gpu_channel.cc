530e7a59250f4c85cb5a204b33b99645294e3740
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 23:38:01 +0000

    Convert use of int ms to TimeDelta in files owned by apatrick.
    R=apatrick@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9188017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117324 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.cc
+++ content/common/gpu/gpu_channel.cc
@@ -27,7 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-const int64 kHandleMoreWorkPeriod = 1;
+const base::TimeDelta kHandleMoreWorkPeriod =
+    base::TimeDelta::FromMilliseconds(1);
 }
 
 GpuChannel::GpuChannel(GpuChannelManager* gpu_channel_manager,
--- content/gpu/gpu_watchdog_thread.h
+++ content/gpu/gpu_watchdog_thread.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -56,16 +56,16 @@ class GpuWatchdogThread : public base::Thread,
   void DeliberatelyTerminateToRecoverFromHang();
   void Disable();
 
-  int64 GetWatchedThreadTime();
+  base::TimeDelta GetWatchedThreadTime();
 
   MessageLoop* watched_message_loop_;
-  int timeout_;
+  base::TimeDelta timeout_;
   volatile bool armed_;
   GpuWatchdogTaskObserver task_observer_;
 
 #if defined(OS_WIN)
   void* watched_thread_handle_;
-  int64 arm_cpu_time_;
+  base::TimeDelta arm_cpu_time_;
 #endif
 
   base::Time arm_absolute_time_;
