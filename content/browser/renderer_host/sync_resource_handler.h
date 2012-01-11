746e007e2f5a432e611d8336c370169905997f21
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 19:28:09 +0000

    Fix shutdown crash by retaining references to ResourceMessageFilter.
    BUG=109762
    
    Review URL: http://codereview.chromium.org/9167025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117255 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/sync_resource_handler.h
+++ content/browser/renderer_host/sync_resource_handler.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -63,7 +63,7 @@ class SyncResourceHandler : public ResourceHandler {
   scoped_refptr<net::IOBuffer> read_buffer_;
 
   content::SyncLoadResult result_;
-  ResourceMessageFilter* filter_;
+  scoped_refptr<ResourceMessageFilter> filter_;
   IPC::Message* result_message_;
   ResourceDispatcherHost* rdh_;
 };
