7aa6672bd69bbcf82f7c63645b9bb8f325db6b9c
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-11 13:38:03 +0000

    2012-01-11  Alexandru Chiculita  <achicu@adobe.com>
            Unreviewd build fix after http://trac.webkit.org/changeset/104702.
    
    	Moving the FilterEffectRenderer::updateBackingStore outside the ENABLE(CSS_SHADERS) block.
    
            * rendering/FilterEffectRenderer.cpp:
            (WebCore::FilterEffectRenderer::notifyCustomFilterProgramLoaded):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
@@ -293,12 +293,6 @@ void FilterEffectRenderer::build(Document* document, const FilterOperations& ope
 #endif
 }
 
-#if ENABLE(CSS_SHADERS)
-void FilterEffectRenderer::notifyCustomFilterProgramLoaded(CustomFilterProgram*)
-{
-    m_observer->filterNeedsRepaint();
-}
-
 void FilterEffectRenderer::updateBackingStore(const FloatRect& filterRect)
 {
     if (!filterRect.isZero()) {
@@ -306,6 +300,12 @@ void FilterEffectRenderer::updateBackingStore(const FloatRect& filterRect)
         if (filterRect != currentSourceRect)
             setSourceImageRect(filterRect);
     }
+}
+
+#if ENABLE(CSS_SHADERS)
+void FilterEffectRenderer::notifyCustomFilterProgramLoaded(CustomFilterProgram*)
+{
+    m_observer->filterNeedsRepaint();
 }
 
 void FilterEffectRenderer::removeCustomFilterClients()
