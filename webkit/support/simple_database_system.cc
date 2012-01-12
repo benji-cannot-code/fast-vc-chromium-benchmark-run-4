bde9ac501202e3e152f2d2e18a93d4604b2d450b
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 17:11:37 +0000

    Convert use of int ms to TimeDelta in files owned by tkent.
    R=tkent@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9139033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117442 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/simple_database_system.cc
+++ webkit/support/simple_database_system.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -247,7 +247,9 @@ void SimpleDatabaseSystem::VfsDeleteFile(
     *result = VfsBackend::DeleteFile(file_name, sync_dir);
   } while ((++num_retries < kNumDeleteRetries) &&
            (*result == SQLITE_IOERR_DELETE) &&
-           (base::PlatformThread::Sleep(10), 1));
+           (base::PlatformThread::Sleep(
+               base::TimeDelta::FromMilliseconds(10)),
+            1));
 
   done_event->Signal();
 }
