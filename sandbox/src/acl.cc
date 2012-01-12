09da8261e8ec03e2d360e5a5475166553ff20c37
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 19:39:55 +0000

    Fix memory dealocatiom mismatch by using scoped_ptr_malloc
    - Flagged by almost all tools
    
    BUG=101717
    TEST= sandbox tests in the waterfall are green.
    
    Review URL: http://codereview.chromium.org/9107029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117476 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/acl.cc
+++ sandbox/src/acl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sandbox {
 
 bool GetDefaultDacl(HANDLE token,
-                    scoped_ptr<TOKEN_DEFAULT_DACL>* default_dacl) {
+                    scoped_ptr_malloc<TOKEN_DEFAULT_DACL>* default_dacl) {
   if (token == NULL)
     return false;
 
@@ -27,7 +27,7 @@ bool GetDefaultDacl(HANDLE token,
   }
 
   TOKEN_DEFAULT_DACL* acl =
-      reinterpret_cast<TOKEN_DEFAULT_DACL*>(new char[length]);
+      reinterpret_cast<TOKEN_DEFAULT_DACL*>(malloc(length));
   default_dacl->reset(acl);
 
   if (!::GetTokenInformation(token, TokenDefaultDacl, default_dacl->get(),
@@ -60,7 +60,7 @@ bool AddSidToDefaultDacl(HANDLE token, const Sid& sid, ACCESS_MASK access) {
   if (token == NULL)
     return false;
 
-  scoped_ptr<TOKEN_DEFAULT_DACL> default_dacl;
+  scoped_ptr_malloc<TOKEN_DEFAULT_DACL> default_dacl;
   if (!GetDefaultDacl(token, &default_dacl))
     return false;
 
@@ -79,9 +79,9 @@ bool AddSidToDefaultDacl(HANDLE token, const Sid& sid, ACCESS_MASK access) {
 
 bool AddUserSidToDefaultDacl(HANDLE token, ACCESS_MASK access) {
   DWORD size = sizeof(TOKEN_USER) + SECURITY_MAX_SID_SIZE;
-  TOKEN_USER* token_user = reinterpret_cast<TOKEN_USER*>(new BYTE[size]);
+  TOKEN_USER* token_user = reinterpret_cast<TOKEN_USER*>(malloc(size));
 
-  scoped_ptr<TOKEN_USER> token_user_ptr(token_user);
+  scoped_ptr_malloc<TOKEN_USER> token_user_ptr(token_user);
 
   if (!::GetTokenInformation(token, TokenUser, token_user, size, &size))
     return false;
