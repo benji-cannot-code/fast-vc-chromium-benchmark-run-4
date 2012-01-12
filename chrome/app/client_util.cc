b447da10d2a785882df3912353a3db2e0b0b03c5
chrisha@chromium.org <chrisha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 19:23:20 +0000

    Extend the PreRead experiment expiry date.
    The PreRead experiment expired as of Jan 1, 2012. This CL extends it for another 6 months until July 1, 2012.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9107041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/client_util.cc
+++ chrome/app/client_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -92,7 +92,7 @@ bool EnvQueryStr(const wchar_t* key_name, std::wstring* value) {
 // These constants are used by the PreRead experiment.
 const wchar_t kPreReadRegistryValue[] = L"PreReadExperimentGroup";
 const int kPreReadExpiryYear = 2012;
-const int kPreReadExpiryMonth = 1;
+const int kPreReadExpiryMonth = 7;
 const int kPreReadExpiryDay = 1;
 
 bool PreReadShouldRun() {
