8bd541d977e1ab13a0fdf484ae4203b1bb0b4463
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-12 00:56:24 +0000

    Implement the [Supplemental] IDL for custom methods https://bugs.webkit.org/show_bug.cgi?id=76036
    Reviewed by Adam Barth.
    
    We have implemented the [Supplemental] IDL for non-custom methods in bug 75944.
    This patch implements it for custom methods. This patch modifies only CodeGeneratorV8.pm,
    since CodeGeneratorJS.pm requires no change and other code generators
    do not support custom methods.
    
    Test: bindings/scripts/test/TestSupplemental.idl
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader):
    
    * bindings/scripts/test/TestSupplemental.idl: Added a custom method with the [Supplemental] IDL.
    
    * bindings/scripts/test/JS/JSTestInterface.cpp: Updated the test result.
    (WebCore::jsTestInterfacePrototypeFunctionSupplementalMethod3):
    * bindings/scripts/test/JS/JSTestInterface.h: Ditto.
    * bindings/scripts/test/ObjC/DOMTestInterface.h: Ditto.
    * bindings/scripts/test/ObjC/DOMTestInterface.mm: Ditto.
    (-[DOMTestInterface supplementalMethod3]):
    * bindings/scripts/test/V8/V8TestInterface.cpp: Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104767 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestInterface.h
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestInterface.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 - (void)supplementalMethod1;
 - (DOMTestObj *)supplementalMethod2:(NSString *)strArg objArg:(DOMTestObj *)objArg;
+- (void)supplementalMethod3;
 @end
 
 #endif
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestInterface.mm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/ObjC/DOMTestInterface.mm
@@ -132,6 +132,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #endif
 
+
+#if ENABLE(Condition11) || ENABLE(Condition12)
+- (void)supplementalMethod3
+{
+    WebCore::JSMainThreadNullState state;
+    TestSupplemental::supplementalMethod3(IMPL);
+}
+
+#endif
+
 @end
 
 WebCore::TestInterface* core(DOMTestInterface *wrapper)
