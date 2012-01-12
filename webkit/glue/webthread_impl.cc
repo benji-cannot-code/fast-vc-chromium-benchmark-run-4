02e3a5c2c125e4c2ae2fe36c028113a613772ebf
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 08:05:02 +0000

    Convert use of int ms to TimeDelta in files owned by darin.
    R=darin@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9190022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117397 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webthread_impl.cc
+++ webkit/glue/webthread_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -68,7 +68,7 @@ void WebThreadImpl::postDelayedTask(
   thread_->message_loop()->PostDelayedTask(
       FROM_HERE,
       base::Bind(&WebKit::WebThread::Task::run, base::Owned(task)),
-      delay_ms);
+      base::TimeDelta::FromMilliseconds(delay_ms));
 }
 
 bool WebThreadImpl::IsCurrentThread() const {
