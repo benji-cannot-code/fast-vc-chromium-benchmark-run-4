5ec42f45cb9394ce5c36c60ffb34a4b354fbb50c
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 20:08:23 +0000

    Moving Ordinal Specific Code out of ExtensionPrefs
    Moved all of the ordinal specific code in ExtensionPrefs to a new class
    ExtensionSorting, also moved the related unit tests to an ExtensionSorting
    unit test file.
    
    There are no logic changes in this patch.
    
    
    BUG=104973
    TEST=The icons on the NTP should behave the same as before, with users able to
    move them around and remember position.
    
    
    Review URL: http://codereview.chromium.org/9021034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117490 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_preference_api.h"
 #include "chrome/browser/extensions/extension_process_manager.h"
 #include "chrome/browser/extensions/extension_processes_api.h"
+#include "chrome/browser/extensions/extension_sorting.h"
 #include "chrome/browser/extensions/extension_special_storage_policy.h"
 #include "chrome/browser/extensions/extension_sync_data.h"
 #include "chrome/browser/extensions/extension_updater.h"
@@ -1610,7 +1611,8 @@ bool ExtensionService::CanLoadInIncognito(const Extension* extension) const {
 
 StringOrdinal ExtensionService::GetAppLaunchOrdinal(
     const std::string& extension_id) const {
-  return extension_prefs_->GetAppLaunchOrdinal(extension_id);
+  return
+      extension_prefs_->extension_sorting()->GetAppLaunchOrdinal(extension_id);
 }
 
 void ExtensionService::SetAppLaunchOrdinal(
@@ -1619,7 +1621,8 @@ void ExtensionService::SetAppLaunchOrdinal(
   const Extension* ext = GetExtensionById(extension_id, true);
   DCHECK(ext->is_app());
 
-  extension_prefs_->SetAppLaunchOrdinal(extension_id, app_launch_index);
+  extension_prefs_->extension_sorting()->SetAppLaunchOrdinal(
+      extension_id, app_launch_index);
 
   const Extension* extension = GetInstalledExtension(extension_id);
   if (extension)
@@ -1628,7 +1631,7 @@ void ExtensionService::SetAppLaunchOrdinal(
 
 StringOrdinal ExtensionService::GetPageOrdinal(
     const std::string& extension_id) const {
-  return extension_prefs_->GetPageOrdinal(extension_id);
+  return extension_prefs_->extension_sorting()->GetPageOrdinal(extension_id);
 }
 
 void ExtensionService::SetPageOrdinal(const std::string& extension_id,
@@ -1636,7 +1639,8 @@ void ExtensionService::SetPageOrdinal(const std::string& extension_id,
   const Extension* ext = GetExtensionById(extension_id, true);
   DCHECK(ext->is_app());
 
-  extension_prefs_->SetPageOrdinal(extension_id, page_ordinal);
+  extension_prefs_->extension_sorting()->SetPageOrdinal(
+      extension_id, page_ordinal);
 
   const Extension* extension = GetInstalledExtension(extension_id);
   if (extension)
@@ -1647,9 +1651,10 @@ void ExtensionService::OnExtensionMoved(
     const std::string& moved_extension_id,
     const std::string& predecessor_extension_id,
     const std::string& successor_extension_id) {
-  extension_prefs_->OnExtensionMoved(moved_extension_id,
-                                     predecessor_extension_id,
-                                     successor_extension_id);
+  extension_prefs_->extension_sorting()->OnExtensionMoved(
+      moved_extension_id,
+      predecessor_extension_id,
+      successor_extension_id);
 
   const Extension* extension = GetInstalledExtension(moved_extension_id);
   if (extension)
