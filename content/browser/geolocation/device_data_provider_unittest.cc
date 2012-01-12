6f3be96097213045bb3e02785af170cd82a679e8
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 22:54:34 +0000

    Convert use of int ms to TimeDelta in files owned by bulach.
    R=bulach@chromium.org
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9185028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117541 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/device_data_provider_unittest.cc
+++ content/browser/geolocation/device_data_provider_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,12 +28,12 @@ TEST(GeolocationDeviceDataProviderWifiData, CreateDestroy) {
   for (int i = 0; i < 10; i++) {
     DeviceDataProvider<WifiData>::Register(&listener);
     for (int j = 0; j < 10; j++) {
-      base::PlatformThread::Sleep(0);
+      base::PlatformThread::Sleep(base::TimeDelta());
       main_message_loop.RunAllPending();  // See comment above
     }
     DeviceDataProvider<WifiData>::Unregister(&listener);
     for (int j = 0; j < 10; j++) {
-      base::PlatformThread::Sleep(0);
+      base::PlatformThread::Sleep(base::TimeDelta());
       main_message_loop.RunAllPending();  // See comment above
     }
   }
