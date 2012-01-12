a52d677b60cd0df2e7c8dad519e4d12512c18543
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 03:10:55 +0000

    Extends chromedriver to support HTML5 storage commands. Original patch from Abdullah Al-Nayeem <alnayeem@google.com>. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/9149013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117378 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1007,6 +1007,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test/webdriver/commands/execute_command.h',
         'test/webdriver/commands/find_element_commands.cc',
         'test/webdriver/commands/find_element_commands.h',
+        'test/webdriver/commands/html5_storage_commands.cc',
+        'test/webdriver/commands/html5_storage_commands.h',
         'test/webdriver/commands/keys_command.cc',
         'test/webdriver/commands/keys_command.h',
         'test/webdriver/commands/log_command.cc',
--- chrome/test/webdriver/commands/webelement_commands.cc
+++ chrome/test/webdriver/commands/webelement_commands.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -430,7 +430,7 @@ bool ElementToggleCommand::DoesPost() {
 void ElementToggleCommand::ExecutePost(Response* const response) {
   std::string script = base::StringPrintf(
       "return (%s).apply(null, arguments);",
-      atoms::asString(atoms::TOGGLE).c_str());
+      atoms::asString(atoms::SET_SELECTED).c_str());
 
   ListValue args;
   args.Append(element.ToValue());
