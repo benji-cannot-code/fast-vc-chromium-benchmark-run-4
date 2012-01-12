6ccd882e2b55162135b7e0be2af86faf895669d4
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 07:03:50 +0000

    WebSocket Pepper API: add interfaces to handle binaryType attribute
    Add interfaces to handle binaryType attribute for various binary types supporting
    in API version 0.9. Keeps the old interface as 0.1 for compatibility, and add
    simple unit test just to instansiate the old interface.
    Adding new interfaces are based on the WebSocket API.
    
    BUG=87310
    TEST=ui_test --gtest_filter='PPAPITest.WebSocket_*'
    
    Review URL: http://codereview.chromium.org/8989046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117391 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_websocket.cc
+++ ppapi/tests/test_websocket.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -129,6 +129,8 @@ PP_Resource TestWebSocket::Connect(
     protocols[0] = CreateVarString(protocol);
     protocol_count = 1U;
   }
+  websocket_interface_->SetBinaryType(
+      ws, PP_WEBSOCKETBINARYTYPE_ARRAYBUFFER_DEV);
   *result = websocket_interface_->Connect(
       ws, url_var, protocols, protocol_count,
       static_cast<pp::CompletionCallback>(callback).pp_completion_callback());
--- webkit/plugins/ppapi/ppb_websocket_impl.cc
+++ webkit/plugins/ppapi/ppb_websocket_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -76,9 +76,12 @@ bool InValidStateToReceive(PP_WebSocketReadyState_Dev state) {
 namespace webkit {
 namespace ppapi {
 
+// TODO(toyoshim): Default value of binary_type_ must be
+// PP_WEBSOCKETBINARYTYPE_BLOB_DEV after supporting Blob.
 PPB_WebSocket_Impl::PPB_WebSocket_Impl(PP_Instance instance)
     : Resource(instance),
       state_(PP_WEBSOCKETREADYSTATE_INVALID_DEV),
+      binary_type_(PP_WEBSOCKETBINARYTYPE_ARRAYBUFFER_DEV),
       error_was_received_(false),
       receive_callback_var_(NULL),
       wait_for_receive_(false),
@@ -418,6 +421,16 @@ PP_Var PPB_WebSocket_Impl::GetURL() {
   if (!url_)
     return empty_string_->GetPPVar();
   return url_->GetPPVar();
+}
+
+PP_Bool PPB_WebSocket_Impl::SetBinaryType(
+    PP_WebSocketBinaryType_Dev binary_type) {
+  // TODO(toyoshim): Use WebKit new API to set the receiving binary type.
+  return PP_FALSE;
+}
+
+PP_WebSocketBinaryType_Dev PPB_WebSocket_Impl::GetBinaryType() {
+  return binary_type_;
 }
 
 void PPB_WebSocket_Impl::didConnect() {
