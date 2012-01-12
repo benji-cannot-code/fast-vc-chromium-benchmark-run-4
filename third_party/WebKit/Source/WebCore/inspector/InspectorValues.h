c5fbec1cea75bb60d117f164151fb8d51a0a23df
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-12 17:33:30 +0000

    Unreviewed, rolling out r104829. http://trac.webkit.org/changeset/104829 https://bugs.webkit.org/show_bug.cgi?id=76188
    it broke qt-minimal (Requested by loislo1 on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-01-12
    
    * inspector/CodeGeneratorInspector.py:
    (CodeGenerator):
    (CodeGenerator.generate_type_builder):
    (CodeGenerator.generate_type_builder.AdHocTypeContext):
    (CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix):
    (CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix.NameFix):
    (CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix.NameFix.output_comment):
    (CodeGenerator.generate_type_builder.AdHocTypeContext.call_generate_type_builder):
    (CodeGenerator.register_use):
    (RawTypesBinding):
    (RawTypesBinding.get_code_generator):
    (RawTypesBinding.get_in_c_type_text):
    (RawTypesBinding.get_setter_value_expression_pattern):
    (RawTypesBinding.reduce_to_raw_type):
    (TypeData.__init__):
    * inspector/InspectorValues.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorValues.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorValues.h
@@ -245,10 +245,8 @@ public:
     const_iterator begin() const { return m_data.begin(); }
     const_iterator end() const { return m_data.end(); }
 
-protected:
-    InspectorArray();
-
 private:
+    InspectorArray();
     Vector<RefPtr<InspectorValue> > m_data;
 };
 
