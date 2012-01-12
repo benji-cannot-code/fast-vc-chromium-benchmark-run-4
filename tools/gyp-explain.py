1842b6733d1c9033153b91ee988935cf6ccf0fbc
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 18:16:29 +0000

    gyp-explain does not work on windows. Tell the user and exit early
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9107042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117456 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gyp-explain.py
+++ tools/gyp-explain.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -53,6 +53,11 @@ def MatchNode(graph, substring):
 
 
 def Main(argv):
+  if sys.platform in ['win32', 'cygwin']:
+    print 'The dump_dependency_json gyp generator required for gyp-explain.py'
+    print 'does not support Windows.'
+    sys.exit(1)
+
   # Check that dump.json exists and that it's not too old.
   dump_json_dirty = False
   try:
