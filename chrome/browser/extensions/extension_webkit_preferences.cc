fab7d51b101775348b04d489e98f4301c9f48b3d
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 18:00:42 +0000

    Expose priviledged webgl extensions to hosted apps.
    BUG=
    TEST=
    R=mihaip
    Review URL: http://codereview.chromium.org/9186025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webkit_preferences.cc
+++ chrome/browser/extensions/extension_webkit_preferences.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,9 +23,6 @@ void SetPreferences(const Extension* extension,
     // be subject to that.
     webkit_prefs->allow_scripts_to_close_windows = true;
 
-    // Enable privileged WebGL extensions.
-    webkit_prefs->privileged_webgl_extensions_enabled = true;
-
     // Disable anything that requires the GPU process for background pages.
     // See http://crbug.com/64512 and http://crbug.com/64841.
     if (render_view_type == chrome::VIEW_TYPE_EXTENSION_BACKGROUND_PAGE) {
@@ -33,6 +30,11 @@ void SetPreferences(const Extension* extension,
       webkit_prefs->accelerated_compositing_enabled = false;
       webkit_prefs->accelerated_2d_canvas_enabled = false;
     }
+  }
+  if (extension) {
+    // Enable WebGL features that regular pages can't access, since they add
+    // more risk of fingerprinting.
+    webkit_prefs->privileged_webgl_extensions_enabled = true;
   }
 }
 
