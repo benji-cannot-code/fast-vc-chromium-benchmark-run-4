fe9b391ea962ca44cd620e3a5666595ff46e5c43
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-12 19:06:35 +0000

    https://bugs.webkit.org/show_bug.cgi?id=76133 ScrollAnimatorMac::mouseEnteredScrollbar() and mouseExitedScrollbar() should only do stuff for legacy scrollbars -and corresponding- <rdar://problem/10603290>
    Reviewed by Sam Weinig.
    
    * platform/mac/ScrollAnimatorMac.mm:
    (WebCore::ScrollAnimatorMac::mouseEnteredScrollbar):
    (WebCore::ScrollAnimatorMac::mouseExitedScrollbar):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
@@ -743,8 +743,13 @@ void ScrollAnimatorMac::mouseMovedInContentArea() const
 
 void ScrollAnimatorMac::mouseEnteredScrollbar(Scrollbar* scrollbar) const
 {
+    // At this time, only legacy scrollbars needs to send notifications here.
+    if (recommendedScrollerStyle() != NSScrollerStyleLegacy)
+        return;
+
     if (!scrollableArea()->isOnActivePage())
         return;
+
     if (isScrollbarOverlayAPIAvailable()) {
         if (!supportsUIStateTransitionProgress())
             return;
@@ -755,8 +760,13 @@ void ScrollAnimatorMac::mouseEnteredScrollbar(Scrollbar* scrollbar) const
 
 void ScrollAnimatorMac::mouseExitedScrollbar(Scrollbar* scrollbar) const
 {
+    // At this time, only legacy scrollbars needs to send notifications here.
+    if (recommendedScrollerStyle() != NSScrollerStyleLegacy)
+        return;
+
     if (!scrollableArea()->isOnActivePage())
         return;
+
     if (isScrollbarOverlayAPIAvailable()) {
         if (!supportsUIStateTransitionProgress())
             return;
