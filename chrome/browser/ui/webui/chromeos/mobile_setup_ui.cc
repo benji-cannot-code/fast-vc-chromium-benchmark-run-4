a479b7cc78f3f58573c9e0e18e803d6413f06744
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 21:35:48 +0000

    Crash fix for mobile activation page refresh request.
    BUG=chrome-os-partner:7427
    TEST=make sure you can refresh chrome://mobilesetup without crashing the browser
    Review URL: http://codereview.chromium.org/9188027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117507 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/mobile_setup_ui.cc
+++ chrome/browser/ui/webui/chromeos/mobile_setup_ui.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/logging.h"
-#include "base/memory/weak_ptr.h"
 #include "base/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/string_piece.h"
@@ -133,14 +132,12 @@ chromeos::CellularNetwork* GetCellularNetwork(
 // appears.
 class PortalFrameLoadObserver : public content::RenderViewHostObserver {
  public:
-  PortalFrameLoadObserver(RenderViewHost* host, WebUI* webui)
-      : content::RenderViewHostObserver(host), webui_(webui) {
-    DCHECK(webui_);
+  PortalFrameLoadObserver(const base::WeakPtr<MobileSetupUI>& parent,
+                          RenderViewHost* host)
+      : content::RenderViewHostObserver(host), parent_(parent) {
     Send(new ChromeViewMsg_StartFrameSniffer(routing_id(),
                                              UTF8ToUTF16("paymentForm")));
   }
-
-  virtual ~PortalFrameLoadObserver() {}
 
   // IPC::Channel::Listener implementation.
   virtual bool OnMessageReceived(const IPC::Message& message) {
@@ -156,16 +153,22 @@ class PortalFrameLoadObserver : public content::RenderViewHostObserver {
 
  private:
   void OnFrameLoadError(int error) {
+    if (!parent_.get())
+      return;
+
     base::FundamentalValue result_value(error);
-    webui_->CallJavascriptFunction(
-        kJsPortalFrameLoadFailedCallback, result_value);
+    parent_->CallJavascriptFunction(kJsPortalFrameLoadFailedCallback,
+                                    result_value);
   }
 
   void OnFrameLoadCompleted() {
-    webui_->CallJavascriptFunction(kJsPortalFrameLoadCompletedCallback);
+    if (!parent_.get())
+      return;
+
+    parent_->CallJavascriptFunction(kJsPortalFrameLoadCompletedCallback);
   }
 
-  WebUI* webui_;
+  base::WeakPtr<MobileSetupUI> parent_;
   DISALLOW_COPY_AND_ASSIGN(PortalFrameLoadObserver);
 };
 
@@ -1387,7 +1390,9 @@ void MobileSetupHandler::StartActivationOnUIThread() {
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-MobileSetupUI::MobileSetupUI(WebContents* contents) : WebUI(contents) {
+MobileSetupUI::MobileSetupUI(WebContents* contents)
+    : WebUI(contents),
+      frame_load_observer_(NULL) {
   chromeos::CellularNetwork* network = GetCellularNetwork();
   std::string service_path = network ? network->service_path() : std::string();
   MobileSetupHandler* handler = new MobileSetupHandler(service_path);
@@ -1403,5 +1408,6 @@ MobileSetupUI::MobileSetupUI(WebContents* contents) : WebUI(contents) {
 void MobileSetupUI::RenderViewCreated(RenderViewHost* host) {
   WebUI::RenderViewCreated(host);
   // Destroyed by the corresponding RenderViewHost.
-  new PortalFrameLoadObserver(host, web_contents()->GetWebUI());
+  frame_load_observer_ =
+      new PortalFrameLoadObserver(AsWeakPtr(), host);
 }
