e11ba82445547684a2c1da430a25c3d9eaa4f779
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 19:53:23 +0000

    Fixed HW video decode EOS/Flush-related bugs. - VideoRendererBase needs to watch out for an EOS ready_frame_   not at the front of the queue (since HW video decoder can   generate multiple frames per Read() call). - VideoRendererBase needs account for outstanding textures in   current_frame_ & last_available_frame_ when deciding whether to   Decoder::Read() some more. - Made GpuVideoDecoder's implementation of EOS and Flush()   handling more straightforward/explicit/robust.
    BUG=109625
    TEST=chrome play-to-EOS & replay works
    
    Review URL: http://codereview.chromium.org/9185017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117480 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base.h
+++ media/filters/video_renderer_base.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,6 +94,9 @@ class MEDIA_EXPORT VideoRendererBase
 
   // Helper function that flushes the buffers when a Stop() or error occurs.
   void DoStopOrError_Locked();
+
+  // Return the number of frames currently held by this class.
+  int NumFrames_Locked() const;
 
   // Used for accessing data members.
   base::Lock lock_;
--- webkit/media/video_renderer_impl_unittest.cc
+++ webkit/media/video_renderer_impl_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,7 +27,7 @@ using testing::ReturnRef;
 
 namespace webkit_media {
 
-static const int64 kFrameDuration = 10;
+static const int64 kFrameDurationMs = 10;
 static const int kWidth = 320;
 static const int kHeight = 240;
 static const gfx::Size kNaturalSize(kWidth, kHeight);
@@ -97,9 +97,9 @@ class VideoRendererImplTest : public testing::Test {
     for (int i = 0; i < media::limits::kMaxVideoFrames; ++i) {
       WaitForRead();
       if (i == 0)
-        deliver_first_frame.Run(i * kFrameDuration);
+        deliver_first_frame.Run(i * kFrameDurationMs);
       else
-        deliver_remaining_frames.Run(i * kFrameDuration);
+        deliver_remaining_frames.Run(i * kFrameDurationMs);
     }
 
     // Wait for prerolling to complete and paint first frame.
@@ -108,7 +108,10 @@ class VideoRendererImplTest : public testing::Test {
 
     // Advance to remaining frames and paint again.
     Play();
-    SetTime(kFrameDuration);
+    for (int i = 1; i < media::limits::kMaxVideoFrames; ++i) {
+      SetTime(i * kFrameDurationMs);
+      paint.Run();
+    }
     WaitForRead();
     paint.Run();
   }
@@ -128,7 +131,7 @@ class VideoRendererImplTest : public testing::Test {
     EXPECT_CALL(*decoder_, Read(_))
         .WillRepeatedly(Invoke(this, &VideoRendererImplTest::FrameRequested));
 
-    renderer_->Seek(base::TimeDelta::FromMicroseconds(0),
+    renderer_->Seek(base::TimeDelta::FromMilliseconds(0),
                     base::Bind(&VideoRendererImplTest::PrerollDone,
                                base::Unretained(this),
                                media::PIPELINE_OK));
@@ -143,7 +146,7 @@ class VideoRendererImplTest : public testing::Test {
 
   void SetTime(int64 timestamp) {
     base::AutoLock l(lock_);
-    time_ = base::TimeDelta::FromMicroseconds(timestamp);
+    time_ = base::TimeDelta::FromMilliseconds(timestamp);
   }
 
   base::TimeDelta GetTime() {
@@ -187,8 +190,9 @@ class VideoRendererImplTest : public testing::Test {
   void DeliverFrame(int width, int height, int64 timestamp) {
     scoped_refptr<VideoFrame> video_frame =
         VideoFrame::CreateBlackFrame(width, height);
-    video_frame->SetTimestamp(base::TimeDelta::FromMicroseconds(timestamp));
-    video_frame->SetDuration(base::TimeDelta::FromMicroseconds(kFrameDuration));
+    video_frame->SetTimestamp(base::TimeDelta::FromMilliseconds(timestamp));
+    video_frame->SetDuration(
+        base::TimeDelta::FromMilliseconds(kFrameDurationMs));
 
     VideoDecoder::ReadCB read_cb;
     {
