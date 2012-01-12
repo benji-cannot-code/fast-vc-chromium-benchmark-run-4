911ab7b394718675929eeecb92b2d7e0976083f9
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 20:55:38 +0000

    Revert 116954 - Pass Content-security-policy and header type from WebSharedWorkerProxy to WebSharedWorkerStub. WebKit side has changed, and need to start from scratch to avoid conflicts on rolls.
    BUG=105482
    
    Review URL: http://codereview.chromium.org/9154003
    
    TBR=jam@chromium.org
    Review URL: http://codereview.chromium.org/9188046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117496 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/websharedworker_proxy.h
+++ content/renderer/websharedworker_proxy.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,23 +38,11 @@ class WebSharedWorkerProxy : public WebKit::WebSharedWorker,
   virtual bool isStarted();
   virtual void connect(WebKit::WebMessagePortChannel* channel,
                        ConnectListener* listener);
-
-  // API is evolving from first form to the second form.
   virtual void startWorkerContext(const WebKit::WebURL& script_url,
                                   const WebKit::WebString& name,
                                   const WebKit::WebString& user_agent,
                                   const WebKit::WebString& source_code,
                                   long long script_resource_appcache_id);
-
-  virtual void startWorkerContext(
-      const WebKit::WebURL& script_url,
-      const WebKit::WebString& name,
-      const WebKit::WebString& user_agent,
-      const WebKit::WebString& source_code,
-      const WebKit::WebString& content_security_policy,
-      bool  report_only,
-      long long script_resource_appcache_id);
-
   virtual void terminateWorkerContext();
   virtual void clientDestroyed();
 
@@ -83,8 +71,6 @@ class WebSharedWorkerProxy : public WebKit::WebSharedWorker,
                            const string16& name,
                            const string16& user_agent,
                            const string16& source_code,
-                           const string16& content_security_policy,
-                           bool report_only,
                            int pending_route_id,
                            int64 script_resource_appcache_id);
   void OnWorkerCreated();
--- content/worker/websharedworker_stub.h
+++ content/worker/websharedworker_stub.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -52,9 +52,7 @@ class WebSharedWorkerStub : public IPC::Channel::Listener {
 
   void OnConnect(int sent_message_port_id, int routing_id);
   void OnStartWorkerContext(
-      const GURL& url, const string16& user_agent, const string16& source_code,
-      const string16& content_security_policy, bool report_only);
-
+      const GURL& url, const string16& user_agent, const string16& source_code);
   void OnTerminateWorkerContext();
 
   int route_id_;
