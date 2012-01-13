b74cb8ebb1c10327f36a71f41d4813b7a01ca264
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 05:07:07 +0000

    The Me2Me chromoting host logs connection data to the cloud.
    BUG=106208
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9148042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117612 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/heartbeat_sender.h"
 #include "remoting/host/host_config.h"
 #include "remoting/host/json_host_config.h"
+#include "remoting/host/log_to_server.h"
 #include "remoting/host/signaling_connector.h"
 #include "remoting/jingle_glue/xmpp_signal_strategy.h"
 #include "remoting/protocol/me2me_host_authenticator_factory.h"
@@ -160,6 +161,9 @@ class HostProcess {
     heartbeat_sender_.reset(
         new HeartbeatSender(host_id_, signal_strategy_.get(), &key_pair_));
 
+    log_to_server_.reset(new LogToServer(signal_strategy_.get()));
+    host_->AddStatusObserver(log_to_server_.get());
+
     host_->Start();
 
     // Create authenticator factory.
@@ -191,6 +195,7 @@ class HostProcess {
   scoped_ptr<SignalingConnector> signaling_connector_;
   scoped_ptr<DesktopEnvironment> desktop_environment_;
   scoped_ptr<remoting::HeartbeatSender> heartbeat_sender_;
+  scoped_ptr<LogToServer> log_to_server_;
   scoped_refptr<ChromotingHost> host_;
 };
 
