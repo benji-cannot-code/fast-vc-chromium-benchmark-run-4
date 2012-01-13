8498ce64f67e2d37ca15c2a7bc27729bd9526265
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 22:03:49 +0000

    Revert 117696 - Created DownloadFileManager unit test.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8978001
    
    TBR=ahendrickson@chromium.org
    Review URL: http://codereview.chromium.org/9112041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117706 0039d316-1c4b-4281-b951-d872f2087c98

--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -199,7 +199,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/device_orientation/provider_unittest.cc',
         'browser/download/base_file_unittest.cc',
         'browser/download/download_buffer_unittest.cc',
-        'browser/download/download_file_manager_unittest.cc',
         'browser/download/download_file_unittest.cc',
         'browser/download/download_id_unittest.cc',
         'browser/download/download_query_unittest.cc',
--- content/public/browser/download_manager.h
+++ content/public/browser/download_manager.h
@@ -154,13 +154,13 @@ class CONTENT_EXPORT DownloadManager
   // Remove downloads after remove_begin (inclusive) and before remove_end
   // (exclusive). You may pass in null Time values to do an unbounded delete
   // in either direction.
-  virtual int RemoveDownloadsBetween(base::Time remove_begin,
-                                     base::Time remove_end) = 0;
+  virtual int RemoveDownloadsBetween(const base::Time remove_begin,
+                                     const base::Time remove_end) = 0;
 
   // Remove downloads will delete all downloads that have a timestamp that is
   // the same or more recent than |remove_begin|. The number of downloads
   // deleted is returned back to the caller.
-  virtual int RemoveDownloads(base::Time remove_begin) = 0;
+  virtual int RemoveDownloads(const base::Time remove_begin) = 0;
 
   // Remove all downloads will delete all downloads. The number of downloads
   // deleted is returned back to the caller.
@@ -244,8 +244,6 @@ class CONTENT_EXPORT DownloadManager
   // Get the download item from the active map.  Useful when the item is not
   // yet in the history map.
   virtual DownloadItem* GetActiveDownloadItem(int id) = 0;
-
-  virtual bool GenerateFileHash() = 0;
 
   virtual content::DownloadManagerDelegate* delegate() const = 0;
 
