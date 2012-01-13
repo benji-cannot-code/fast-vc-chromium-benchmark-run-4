d56835c08bbe1eee25d489c5e3ba699ae3fa4445
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 00:45:51 +0000

    Revert 117561 - Add NaCl proxy for pepper gamepad
    BUG=79098
    
    
    Review URL: http://codereview.chromium.org/9148044
    
    TBR=scottmg@chromium.org
    Review URL: http://codereview.chromium.org/9186060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117572 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/shared/ppapi_proxy/browser_globals.h
+++ ppapi/native_client/src/shared/ppapi_proxy/browser_globals.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/dev/ppb_cursor_control_dev.h"
 #include "ppapi/c/dev/ppb_find_dev.h"
 #include "ppapi/c/dev/ppb_font_dev.h"
-#include "ppapi/c/dev/ppb_gamepad_dev.h"
 #include "ppapi/c/dev/ppb_memory_dev.h"
 #include "ppapi/c/dev/ppb_scrollbar_dev.h"
 #include "ppapi/c/dev/ppb_testing_dev.h"
@@ -106,7 +105,6 @@ const PPB_FileSystem* PPBFileSystemInterface();
 const PPB_Find_Dev* PPBFindInterface();
 const PPB_Font_Dev* PPBFontInterface();
 const PPB_Fullscreen* PPBFullscreenInterface();
-const PPB_Gamepad_Dev* PPBGamepadInterface();
 const PPB_Graphics2D* PPBGraphics2DInterface();
 const PPB_Graphics3D* PPBGraphics3DInterface();
 const PPB_Graphics3DTrusted* PPBGraphics3DTrustedInterface();
--- ppapi/native_client/src/shared/ppapi_proxy/ppapi_proxy_untrusted.gyp
+++ ppapi/native_client/src/shared/ppapi_proxy/ppapi_proxy_untrusted.gyp
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'plugin_ppb_find.cc',
           'plugin_ppb_font.cc',
           'plugin_ppb_fullscreen.cc',
-          'plugin_ppb_gamepad.cc',
           'plugin_ppb_graphics_2d.cc',
           'plugin_ppb_graphics_3d.cc',
           'plugin_ppb_image_data.cc',
