e8d7e81ee3ee6bffc122364ed34935c33589801e
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 08:59:05 +0000

    Revert 117342 - Prevent callbacks after URLRequest/URLRequestJob have been canceled/completed/killed.
    With this CL, URLRequestJobs notify their NetworkDelegate when they are being destroyed to insure that the NetworkDelegate does not access any of their variables or call callbacks.
    
    Also the WebRequest API does not execute any callbacks if they URLRequest has informed the API that the Request has been completed or canceled.
    
    Both cases could happen in rare race conditions.
    
    BUG=108968
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9147018
    
    TBR=battre@chromium.org
    Review URL: http://codereview.chromium.org/9173005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117620 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_webrequest_api.cc
+++ chrome/browser/extensions/extension_webrequest_api.cc
@@ -862,8 +862,6 @@ void ExtensionWebRequestEventRouter::OnCompleted(
 
   DCHECK(!GetAndSetSignaled(request->identifier(), kOnCompleted));
 
-  ClearPendingCallbacks(request);
-
   int extra_info_spec = 0;
   std::vector<const EventListener*> listeners =
       GetMatchingListeners(profile, extension_info_map,
@@ -911,8 +909,6 @@ void ExtensionWebRequestEventRouter::OnErrorOccurred(
 
   DCHECK(!GetAndSetSignaled(request->identifier(), kOnErrorOccurred));
 
-  ClearPendingCallbacks(request);
-
   int extra_info_spec = 0;
   std::vector<const EventListener*> listeners =
       GetMatchingListeners(profile, extension_info_map,
@@ -937,17 +933,11 @@ void ExtensionWebRequestEventRouter::OnErrorOccurred(
 
 void ExtensionWebRequestEventRouter::OnURLRequestDestroyed(
     void* profile, net::URLRequest* request) {
-  ClearPendingCallbacks(request);
-
+  blocked_requests_.erase(request->identifier());
   signaled_requests_.erase(request->identifier());
 
   request_time_tracker_->LogRequestEndTime(request->identifier(),
                                            base::Time::Now());
-}
-
-void ExtensionWebRequestEventRouter::ClearPendingCallbacks(
-    net::URLRequest* request) {
-  blocked_requests_.erase(request->identifier());
 }
 
 bool ExtensionWebRequestEventRouter::DispatchEvent(
