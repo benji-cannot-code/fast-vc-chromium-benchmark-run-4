d93751bbc4f1ebea07a084c5a2a6cc5cc0311fc9
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 23:50:51 +0000

    Fix startup pages overlay: Add a Cancel button and inherit from SettingsDialog
    BUG=NONE
    TEST=Click 'Cancel' on the startup pages overlay on chrome://chrome
    
    
    Review URL: http://codereview.chromium.org/9174014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/startup_overlay.js
+++ chrome/browser/resources/options2/startup_overlay.js
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('options', function() {
-  var OptionsPage = options.OptionsPage;
-  var ArrayDataModel = cr.ui.ArrayDataModel;
-
-  const localStrings = new LocalStrings();
+  const ArrayDataModel = cr.ui.ArrayDataModel;
+  const OptionsPage = options.OptionsPage;
+  const SettingsDialog = options.SettingsDialog;
 
   /**
    * StartupOverlay class
@@ -16,35 +15,47 @@ cr.define('options', function() {
    * @class
    */
   function StartupOverlay() {
-    OptionsPage.call(this, 'startup',
-                     templateData.startupOverlayTabTitle,
-                     'startup-overlay');
+    SettingsDialog.call(this, 'startup', templateData.startupPagesDialogTitle,
+      'startup-overlay',
+      $('startup-overlay-confirm'), $('startup-overlay-cancel'));
   };
 
   cr.addSingletonGetter(StartupOverlay);
 
   StartupOverlay.prototype = {
-    // Inherit from OptionsPage.
-    __proto__: OptionsPage.prototype,
-
-    startup_pages_pref_: {
-      'name': 'session.urls_to_restore_on_startup',
-      'disabled': false
-    },
+    __proto__: SettingsDialog.prototype,
 
     /**
      * An autocomplete list that can be attached to a text field during editing.
      * @type {HTMLElement}
      * @private
      */
-    autocompleteList_ : null,
+    autocompleteList_: null,
+
+    // TODO(tbreisacher): Work with jhawkins to refactor this so that we're not
+    // overriding private handle* methods in SettingsDialog.
+
+    /**
+     * @override
+     */
+    handleConfirm_: function() {
+      OptionsPage.closeOverlay();
+      chrome.send('commitStartupPrefChanges');
+    },
+
+    /**
+     * @override
+     */
+    handleCancel_: function() {
+      OptionsPage.closeOverlay();
+      chrome.send('cancelStartupPrefChanges');
+    },
 
     /**
      * Initialize the page.
      */
     initializePage: function() {
-      // Call base class implementation to start preference initialization.
-      OptionsPage.prototype.initializePage.call(this);
+      SettingsDialog.prototype.initializePage.call(this);
 
       var self = this;
 
@@ -55,15 +66,6 @@ cr.define('options', function() {
       $('startupUseCurrentButton').onclick = function(event) {
         chrome.send('setStartupPagesToCurrentPages');
       };
-
-      $('startup-overlay-ok').onclick = function() {
-        OptionsPage.closeOverlay();
-      }
-
-      // Initialize control enabled states.
-      Preferences.getInstance().addEventListener(
-          this.startup_pages_pref_.name,
-          this.handleStartupPageListChange_.bind(this));
 
       var suggestionList = new cr.ui.AutocompleteList();
       suggestionList.autoExpands = true;
@@ -113,16 +115,6 @@ cr.define('options', function() {
         return;
       }
       list.suggestions = suggestions;
-    },
-
-    /**
-     * Handle change events of the preference
-     * 'session.urls_to_restore_on_startup'.
-     * @param {event} preference changed event.
-     * @private
-     */
-    handleStartupPageListChange_: function(event) {
-      this.startup_pages_pref_.disabled = event.value['disabled'];
     },
   };
 
--- chrome/browser/ui/webui/options2/startup_pages_handler2.cc
+++ chrome/browser/ui/webui/options2/startup_pages_handler2.cc
@@ -65,6 +65,12 @@ void StartupPagesHandler::RegisterMessages() {
       "requestAutocompleteSuggestionsForStartupPages",
       base::Bind(&StartupPagesHandler::RequestAutocompleteSuggestions,
                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback("commitStartupPrefChanges",
+      base::Bind(&StartupPagesHandler::CommitChanges,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback("cancelStartupPrefChanges",
+      base::Bind(&StartupPagesHandler::CancelChanges,
+                 base::Unretained(this)));
 }
 
 void StartupPagesHandler::UpdateStartupPages() {
@@ -137,7 +143,6 @@ void StartupPagesHandler::Observe(
 void StartupPagesHandler::SetStartupPagesToCurrentPages(
     const ListValue* args) {
   startup_custom_pages_table_model_->SetToCurrentlyOpenPages();
-  SaveStartupPagesPref();
 }
 
 void StartupPagesHandler::RemoveStartupPages(const ListValue* args) {
@@ -154,8 +159,6 @@ void StartupPagesHandler::RemoveStartupPages(const ListValue* args) {
     }
     startup_custom_pages_table_model_->Remove(selected_index);
   }
-
-  SaveStartupPagesPref();
 }
 
 void StartupPagesHandler::AddStartupPage(const ListValue* args) {
@@ -168,7 +171,6 @@ void StartupPagesHandler::AddStartupPage(const ListValue* args) {
     return;
   int index = startup_custom_pages_table_model_->RowCount();
   startup_custom_pages_table_model_->Add(index, url);
-  SaveStartupPagesPref();
 }
 
 void StartupPagesHandler::EditStartupPage(const ListValue* args) {
@@ -188,7 +190,6 @@ void StartupPagesHandler::EditStartupPage(const ListValue* args) {
   std::vector<GURL> urls = startup_custom_pages_table_model_->GetURLs();
   urls[index] = URLFixerUpper::FixupURL(url_string, std::string());
   startup_custom_pages_table_model_->SetURLs(urls);
-  SaveStartupPagesPref();
 }
 
 void StartupPagesHandler::DragDropStartupPage(const ListValue* args) {
@@ -212,7 +213,6 @@ void StartupPagesHandler::DragDropStartupPage(const ListValue* args) {
   }
 
   startup_custom_pages_table_model_->MoveURLs(to_index, index_list);
-  SaveStartupPagesPref();
 }
 
 void StartupPagesHandler::SaveStartupPagesPref() {
@@ -222,6 +222,14 @@ void StartupPagesHandler::SaveStartupPagesPref() {
   pref.urls = startup_custom_pages_table_model_->GetURLs();
 
   SessionStartupPref::SetStartupPref(prefs, pref);
+}
+
+void StartupPagesHandler::CommitChanges(const ListValue* args) {
+  SaveStartupPagesPref();
+}
+
+void StartupPagesHandler::CancelChanges(const ListValue* args) {
+  UpdateStartupPages();
 }
 
 void StartupPagesHandler::RequestAutocompleteSuggestions(
