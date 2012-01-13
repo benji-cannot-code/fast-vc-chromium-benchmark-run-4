caf3a756a4f5338f70c042fb8eda82ffc171fa47
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 21:19:47 +0000

    Created DownloadFileManager unit test.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8978001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117696 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_file_manager.h
+++ content/browser/download/download_file_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -140,6 +140,7 @@ class CONTENT_EXPORT DownloadFileManager
 
  private:
   friend class base::RefCountedThreadSafe<DownloadFileManager>;
+  friend class DownloadFileManagerTest;
   friend class DownloadManagerTest;
   FRIEND_TEST_ALL_PREFIXES(DownloadManagerTest, StartDownload);
 
