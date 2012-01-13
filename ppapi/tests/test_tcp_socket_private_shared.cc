8a3875130e3778ea59fbc72f81f9f846ae38e8f4
asharif@chromium.org <asharif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 12:02:09 +0000

    Initialized rv to 0 to fix -Wmaybe-uninitialized warning.
    BUG=none
    TEST=try bots.
    
    
    Review URL: http://codereview.chromium.org/9148054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117634 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_tcp_socket_private_shared.cc
+++ ppapi/tests/test_tcp_socket_private_shared.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,7 +137,7 @@ std::string TestTCPSocketPrivateShared::CheckHTTPResponse(
     PP_Resource socket,
     const std::string& request,
     const std::string& response) {
-  int32_t rv;
+  int32_t rv = 0;
   std::string error_message;
 
   error_message = SyncWrite(socket, request.c_str(), request.size(), &rv);
