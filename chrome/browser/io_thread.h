3bd213788b6ff95bbd692d67b1219c087037ac0d
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 20:46:49 +0000

    Fix typos of member names in a comment.
    R=eroman@chromium.org
    BUG=none
    TEST=no compilation errors
    
    Review URL: http://codereview.chromium.org/9214006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/io_thread.h
+++ chrome/browser/io_thread.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,8 +94,8 @@ class IOThread : public content::BrowserThreadDelegate {
     scoped_ptr<net::HttpTransactionFactory> system_http_transaction_factory;
     scoped_ptr<net::FtpTransactionFactory> system_ftp_transaction_factory;
     scoped_refptr<net::URLRequestContext> system_request_context;
-    // |cookie_store| and |origin_bound_cert_service| are shared between
-    // |proxy_script_fetcher_context| and |system_request_context|.
+    // |system_cookie_store| and |system_origin_bound_cert_service| are shared
+    // between |proxy_script_fetcher_context| and |system_request_context|.
     scoped_refptr<net::CookieStore> system_cookie_store;
     scoped_ptr<net::OriginBoundCertService> system_origin_bound_cert_service;
     scoped_refptr<ExtensionEventRouterForwarder>
