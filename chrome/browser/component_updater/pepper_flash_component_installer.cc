b298778934af40353ed426c714bb014996d2c8b9
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 23:20:44 +0000

    More drastic measures to disable flapper on windows canary
    Somehow having the bundled 11.2 flash and my previous change[1] does not make bundled flash the default. If you have any better ideas I am all ears
    
    Note: In debug builds it will be the existing behavior.
    
    [1] http://codereview.chromium.org/9147020
    
    
    BUG=none
    TEST=canary build uses flash 11.2 as default.
    
    Review URL: http://codereview.chromium.org/9162003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117719 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/pepper_flash_component_installer.cc
+++ chrome/browser/component_updater/pepper_flash_component_installer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -175,10 +175,8 @@ void RegisterPepperFlashWithChrome(const FilePath& path,
   content::PepperPluginInfo plugin_info;
   if (!MakePepperFlashPluginInfo(path, version, true, &plugin_info))
     return;
-  if (fresh_install) {
-    PluginPrefs::EnablePluginGlobally(IsPepperFlashEnabledByDefault(),
-                                      plugin_info.path);
-  }
+  PluginPrefs::EnablePluginGlobally(IsPepperFlashEnabledByDefault(),
+                                    plugin_info.path);
   PluginService::GetInstance()->RegisterInternalPlugin(
       plugin_info.ToWebPluginInfo());
   PluginService::GetInstance()->RefreshPlugins();
