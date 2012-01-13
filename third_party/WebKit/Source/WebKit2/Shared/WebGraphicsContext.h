56b2e627489b30d83950d6274922dd7304fc85f4
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-13 19:19:17 +0000

    [WinCairo] Build correction.
    * Shared/WebGraphicsContext.h: Use proper include path for RefPtrCairo.h.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@104958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebGraphicsContext.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebGraphicsContext.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(CG)
 #include <wtf/RetainPtr.h>
 #elif USE(CAIRO)
-#include "RefPtrCairo.h"
+#include <WebCore/RefPtrCairo.h>
 #endif
 
 namespace WebKit {
