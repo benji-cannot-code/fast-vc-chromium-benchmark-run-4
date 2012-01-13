fb267f5bc1fc5249d6dedafdf66561be98405cdd
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 04:40:24 +0000

    Move ProfileInfoCache + GAIAInfoUpdateService init to DoFinalInit.
    This is a more fitting resting place than tying it to sync, as it
    really only depends on kGoogleServicesUsername.
    
    BUG=93922
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9139063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117610 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/off_the_record_profile_io_data.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 #include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/browser/sync/sync_prefs.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/find_bar/find_bar_state.h"
@@ -219,6 +220,6 @@ bool Profile::IsGuestSession() {
 }
 
 bool Profile::IsSyncAccessible() {
-  ProfileSyncService* syncService = GetProfileSyncService();
-  return syncService && !syncService->IsManaged();
+  browser_sync::SyncPrefs prefs(GetPrefs());
+  return ProfileSyncService::IsSyncEnabled() && !prefs.IsManaged();
 }
