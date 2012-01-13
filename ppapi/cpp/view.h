259d158c29ae581bf54aee60669e64092c4f30ac
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 17:52:47 +0000

    Needed to add an @file introduction and a class introduction.
    Review URL: http://codereview.chromium.org/9124014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117651 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/view.h
+++ ppapi/cpp/view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,8 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/rect.h"
 #include "ppapi/cpp/size.h"
 
+/// @file
+/// This file defines the API for getting the state of a the view for an
+/// instance.
+
 namespace pp {
 
+/// This class represents the state of the view for an instance and contains
+/// functions for retrieving the current state of that view.
 class View : public Resource {
  public:
   /// Default constructor for creating an is_null() <code>View</code> object.
