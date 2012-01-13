4e974f0c88d5eff1eb6be589336cb89c94c697f7
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 22:04:16 +0000

    [i18n-fixlet] Make strings branding specific in Download code.
    BUG=NONE
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9186050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117708 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/download/download_in_progress_dialog_gtk.cc
+++ chrome/browser/ui/gtk/download/download_in_progress_dialog_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,15 +39,12 @@ DownloadInProgressDialogGtk::DownloadInProgressDialogGtk(Browser* browser)
   std::string explanation_text;
   std::string ok_button_text;
   std::string cancel_button_text;
-  string16 product_name = l10n_util::GetStringUTF16(IDS_PRODUCT_NAME);
   if (download_count == 1) {
     warning_text =
-        l10n_util::GetStringFUTF8(IDS_SINGLE_DOWNLOAD_REMOVE_CONFIRM_WARNING,
-                                  product_name);
+        l10n_util::GetStringUTF8(IDS_SINGLE_DOWNLOAD_REMOVE_CONFIRM_WARNING);
     explanation_text =
-        l10n_util::GetStringFUTF8(
-            IDS_SINGLE_DOWNLOAD_REMOVE_CONFIRM_EXPLANATION,
-            product_name);
+        l10n_util::GetStringUTF8(
+            IDS_SINGLE_DOWNLOAD_REMOVE_CONFIRM_EXPLANATION);
     ok_button_text = l10n_util::GetStringUTF8(
         IDS_SINGLE_DOWNLOAD_REMOVE_CONFIRM_OK_BUTTON_LABEL);
     cancel_button_text = l10n_util::GetStringUTF8(
@@ -55,11 +52,10 @@ DownloadInProgressDialogGtk::DownloadInProgressDialogGtk(Browser* browser)
   } else {
     warning_text =
         l10n_util::GetStringFUTF8(IDS_MULTIPLE_DOWNLOADS_REMOVE_CONFIRM_WARNING,
-                                  product_name,
                                   base::IntToString16(download_count));
     explanation_text =
-        l10n_util::GetStringFUTF8(
-            IDS_MULTIPLE_DOWNLOADS_REMOVE_CONFIRM_EXPLANATION, product_name);
+        l10n_util::GetStringUTF8(
+            IDS_MULTIPLE_DOWNLOADS_REMOVE_CONFIRM_EXPLANATION);
     ok_button_text = l10n_util::GetStringUTF8(
         IDS_MULTIPLE_DOWNLOADS_REMOVE_CONFIRM_OK_BUTTON_LABEL);
     cancel_button_text = l10n_util::GetStringUTF8(
