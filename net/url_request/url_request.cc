f2a6a6b70bfe890ff1c5ec984df42393c40fa75a
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 15:53:50 +0000

    Revert "Debug checks that URLRequest::set_method arguments match the contract." This change broke WebKit test http/tests/xmlhttprequest/methods-lower-case.html
    This reverts commit a964df83606a31b3674e20989e7dc6db9f3d4e86.
    
    TBR=cbentzel@chromium.org
    
    Review URL: http://codereview.chromium.org/9207003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117641 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/message_loop.h"
 #include "base/metrics/stats_counters.h"
-#include "base/string_util.h"
 #include "base/synchronization/lock.h"
 #include "net/base/auth.h"
 #include "net/base/host_port_pair.h"
@@ -378,8 +377,6 @@ void URLRequest::set_first_party_for_cookies(
 
 void URLRequest::set_method(const std::string& method) {
   DCHECK(!is_pending_);
-  DCHECK(!method.empty());
-  DCHECK_EQ(StringToUpperASCII(method), method);
   method_ = method;
 }
 
