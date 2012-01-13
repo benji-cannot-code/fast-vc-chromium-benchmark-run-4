e88dda0bb8997fa03af6ae4bd82e26345d9278ca
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 00:27:41 +0000

    CanvasSkia::Transform should be enabled on the Mac
    Enables CanvasSkia::Transform on Mac.  It works fine and is
    needed for the Mac/Aura work.
    
    BUG=109946
    TEST=Compiles, manual testing with Mac/Aura
    R=ben@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9148066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117566 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas_skia.h
+++ ui/gfx/canvas_skia.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -173,9 +173,7 @@ class UI_EXPORT CanvasSkia : public Canvas {
                             int dest_x, int dest_y, int w, int h) OVERRIDE;
   virtual gfx::NativeDrawingContext BeginPlatformPaint() OVERRIDE;
   virtual void EndPlatformPaint() OVERRIDE;
-#if !defined(OS_MACOSX)
   virtual void Transform(const ui::Transform& transform) OVERRIDE;
-#endif
   virtual ui::TextureID GetTextureID() OVERRIDE;
   virtual CanvasSkia* AsCanvasSkia() OVERRIDE;
   virtual const CanvasSkia* AsCanvasSkia() const OVERRIDE;
