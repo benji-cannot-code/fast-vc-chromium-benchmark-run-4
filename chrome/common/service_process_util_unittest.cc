23e594c11b6708f9f09d1cc61a7abdf97297db59
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-13 23:44:31 +0000

    Mark ServiceProcessStateTest.ForceShutdown as flaky on Windows.
    BUG=84854
    TEST=none
    
    Review URL: http://codereview.chromium.org/9174018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117727 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/service_process_util_unittest.cc
+++ chrome/common/service_process_util_unittest.cc
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // somewhat flaky on other Linux.
 #define MAYBE_ForceShutdown FAILS_ForceShutdown
 #else
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_WIN)
 #define MAYBE_ForceShutdown FLAKY_ForceShutdown
 #else
 #define MAYBE_ForceShutdown ForceShutdown
