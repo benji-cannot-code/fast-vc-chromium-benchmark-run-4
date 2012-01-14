ae32818a6e43b7080467030e1c7af432b75fa3c7
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-14 03:03:08 +0000

    Implement flex-align https://bugs.webkit.org/show_bug.cgi?id=75782
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    Intentially gets computed style for flex-item-align:auto wrong.
    Will fix this in a followup patch.
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseValue):
    * css/CSSProperty.cpp:
    (WebCore::CSSProperty::isInheritedProperty):
    * css/CSSPropertyNames.in:
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::applyProperty):
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::sizesToIntrinsicLogicalWidth):
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::flexAlignForChild):
    (WebCore::RenderFlexibleBox::layoutAndPlaceChildren):
    (WebCore::RenderFlexibleBox::alignChildren):
    * rendering/style/RenderStyle.h:
    (WebCore::RenderStyleBitfields::flexAlign):
    (WebCore::RenderStyleBitfields::setFlexAlign):
    (WebCore::RenderStyleBitfields::initialFlexAlign):
    (WebCore::RenderStyleBitfields::initialFlexItemAlign):
    * rendering/style/StyleFlexibleBoxData.cpp:
    (WebCore::StyleFlexibleBoxData::StyleFlexibleBoxData):
    (WebCore::StyleFlexibleBoxData::operator==):
    * rendering/style/StyleFlexibleBoxData.h:
    
    LayoutTests:
    
    * css3/flexbox/css-properties-expected.txt:
    * css3/flexbox/flex-align-expected.txt:
    * css3/flexbox/flex-align.html:
    * css3/flexbox/script-tests/css-properties.js:
    * fast/css/getComputedStyle/computed-style-expected.txt:
    * platform/chromium-mac-snowleopard/svg/css/getComputedStyle-basic-expected.txt:
    * platform/chromium-win/fast/css/getComputedStyle/computed-style-expected.txt:
    * platform/chromium-win/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
    * platform/chromium-win/svg/css/getComputedStyle-basic-expected.txt:
    * platform/gtk/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
    * platform/mac/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
    * platform/qt/fast/css/getComputedStyle/computed-style-without-renderer-expected.txt:
    * svg/css/getComputedStyle-basic-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -3787,6 +3787,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWordSpacing:
     case CSSPropertyWebkitFlexOrder:
     case CSSPropertyWebkitFlexPack:
+    case CSSPropertyWebkitFlexAlign:
     case CSSPropertyWebkitFlexItemAlign:
     case CSSPropertyWebkitFlexDirection:
     case CSSPropertyWebkitFlexFlow:
--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -619,6 +619,14 @@ void RenderFlexibleBox::prepareChildForPositionedLayout(RenderBox* child, Layout
     }
 }
 
+static EFlexAlign flexAlignForChild(RenderBox* child)
+{
+    EFlexAlign align = child->style()->flexItemAlign();
+    if (align == AlignAuto)
+        return child->parent()->style()->flexAlign();
+    return align;
+}
+
 void RenderFlexibleBox::layoutAndPlaceChildren(FlexOrderIterator& iterator, const WTF::Vector<LayoutUnit>& childSizes, LayoutUnit availableFreeSpace, float totalPositiveFlexibility)
 {
     LayoutUnit mainAxisOffset = flowAwareBorderStart() + flowAwarePaddingStart();
@@ -640,7 +648,7 @@ void RenderFlexibleBox::layoutAndPlaceChildren(FlexOrderIterator& iterator, cons
         child->setChildNeedsLayout(true);
         child->layoutIfNeeded();
 
-        if (child->style()->flexItemAlign() == AlignBaseline) {
+        if (flexAlignForChild(child) == AlignBaseline) {
             LayoutUnit ascent = marginBoxAscent(child);
             LayoutUnit descent = (crossAxisMarginExtentForChild(child) + crossAxisExtentForChild(child)) - ascent;
 
@@ -733,9 +741,9 @@ void RenderFlexibleBox::alignChildren(FlexOrderIterator& iterator, LayoutUnit ma
         }
 
         // FIXME: Make sure this does the right thing with column flows.
-        switch (child->style()->flexItemAlign()) {
+        switch (flexAlignForChild(child)) {
         case AlignAuto:
-            // FIXME: Handle this once we add flex-align.
+            ASSERT_NOT_REACHED();
             break;
         case AlignStretch: {
             if (!isColumnFlow() && child->style()->logicalHeight().isAuto()) {
