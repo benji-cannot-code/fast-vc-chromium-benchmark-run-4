5a1bd24b74f529995a92d672af258d7fc5615449
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-14 01:39:34 +0000

    [uber page] Add a 'Pointer settings' overlay for ChromeOS.
    The dialog is currently not accessible, but this is in preparation for migrating the System page contents.
    
    BUG=109214
    TEST=None
    Review URL: http://codereview.chromium.org/9110050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117755 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options_bundle.js
+++ chrome/browser/resources/options2/options_bundle.js
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   <include src="chromeos/accounts_user_list.js"></include>
   <include src="chromeos/accounts_user_name_edit.js"></include>
   <include src="chromeos/keyboard_overlay.js"></include>
+  <include src="chromeos/pointer_overlay.js"></include>
   <include src="chromeos/virtual_keyboard.js"></include>
   <include src="chromeos/virtual_keyboard_list.js"></include>
   var AccountsOptions = options.AccountsOptions;
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var BluetoothOptions = options.BluetoothOptions;
   var BluetoothPairing = options.BluetoothPairing;
   var KeyboardOverlay = options.KeyboardOverlay;
+  var PointerOverlay = options.PointerOverlay;
 </if>
 <if expr="not pp_ifdef('win32') and not pp_ifdef('darwin')">
   <include src="certificate_tree.js"></include>
--- chrome/browser/ui/webui/options2/options_ui2.cc
+++ chrome/browser/ui/webui/options2/options_ui2.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/options2/chromeos/language_hangul_handler2.h"
 #include "chrome/browser/ui/webui/options2/chromeos/language_mozc_handler2.h"
 #include "chrome/browser/ui/webui/options2/chromeos/language_pinyin_handler2.h"
+#include "chrome/browser/ui/webui/options2/chromeos/pointer_handler2.h"
 #include "chrome/browser/ui/webui/options2/chromeos/proxy_handler2.h"
 #include "chrome/browser/ui/webui/options2/chromeos/stats_options_handler2.h"
 #include "chrome/browser/ui/webui/options2/chromeos/system_options_handler2.h"
@@ -248,6 +249,8 @@ OptionsUI::OptionsUI(WebContents* contents)
                           new chromeos::options2::LanguageMozcHandler());
   AddOptionsPageUIHandler(localized_strings,
                           new chromeos::options2::LanguagePinyinHandler());
+  AddOptionsPageUIHandler(localized_strings,
+                          new chromeos::options2::PointerHandler());
   AddOptionsPageUIHandler(
       localized_strings,
       new chromeos::options2::VirtualKeyboardManagerHandler());
