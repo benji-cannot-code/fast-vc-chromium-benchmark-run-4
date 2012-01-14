0409c6776070eae4c33611f2ce61d20cc13b1017
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-14 02:34:52 +0000

    [NTP4] Some general refactor bits as part of splitting my CLs.
    R=estade@chromium.org,csilv@chromium.org
    BUG=97762
    TEST=Nothing breaks, things are easier to read or follow the style guide.
    
    Review URL: http://codereview.chromium.org/9123027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117766 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/page_list_view.js
+++ chrome/browser/resources/ntp4/page_list_view.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -36,7 +36,7 @@ cr.define('ntp4', function() {
     cardSlider: undefined,
 
     /**
-     * The frame div for cardSlider.
+     * The frame div for this.cardSlider.
      * @type {!Element|undefined}
      */
     sliderFrame: undefined,
@@ -145,8 +145,8 @@ cr.define('ntp4', function() {
       if (this.pageSwitcherEnd)
         ntp4.initializePageSwitcher(this.pageSwitcherEnd);
 
-      this.shownPage = templateData['shown_page_type'];
-      this.shownPageIndex = templateData['shown_page_index'];
+      this.shownPage = templateData.shown_page_type;
+      this.shownPageIndex = templateData.shown_page_index;
 
       // Request data on the apps so we can fill them in.
       // Note that this is kicked off asynchronously.  'getAppsCallback' will be
@@ -190,7 +190,7 @@ cr.define('ntp4', function() {
      * @param {string} title The title of the tile page.
      * @param {bool} titleIsEditable If true, the title can be changed.
      * @param {TilePage} opt_refNode Optional reference node to insert in front
-     * of.
+     *     of.
      * When opt_refNode is falsey, |page| will just be appended to the end of
      * the page list.
      */
@@ -490,9 +490,10 @@ cr.define('ntp4', function() {
     },
 
     /**
-     * Returns the index of the given page.
-     * @param {AppsPage} page The AppsPage for we wish to find.
-     * @return {number} The index of |page|, or -1 if it is not here.
+     * Returns the index of the given apps page.
+     * @param {AppsPage} page The AppsPage we wish to find.
+     * @return {number} The index of |page| or -1 if it is not in the
+     *    collection.
      */
     getAppsPageIndex: function(page) {
       return Array.prototype.indexOf.call(this.appsPages, page);
@@ -510,10 +511,10 @@ cr.define('ntp4', function() {
       // reflect user actions).
       if (!document.documentElement.classList.contains('starting-up')) {
         if (page.classList.contains('apps-page')) {
-          this.shownPage = templateData['apps_page_id'];
+          this.shownPage = templateData.apps_page_id;
           this.shownPageIndex = this.getAppsPageIndex(page);
         } else if (page.classList.contains('most-visited-page')) {
-          this.shownPage = templateData['most_visited_page_id'];
+          this.shownPage = templateData.most_visited_page_id;
           this.shownPageIndex = 0;
         } else {
           console.error('unknown page selected');
--- chrome/browser/resources/ntp4/tile_page.js
+++ chrome/browser/resources/ntp4/tile_page.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1157,7 +1157,7 @@ cr.define('ntp4', function() {
 
     /**
      * Reposition all the tiles (possibly ignoring one).
-     * @param {Node} ignoreNode An optional node to ignore.
+     * @param {?Node} ignoreNode An optional node to ignore.
      * @private
      */
     repositionTiles_: function(ignoreNode) {
