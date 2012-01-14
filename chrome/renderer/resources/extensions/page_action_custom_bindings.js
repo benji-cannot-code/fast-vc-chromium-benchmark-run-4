e06c9a114508b2d1ae529cac8253736cd3708fae
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-14 02:29:09 +0000

    Extensions cleanup: pull the browserAction and pageAction API customisations out of schema_generated_bindings.
    BUG=80310
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9203003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117765 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/renderer/resources/extensions/page_action_custom_bindings.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Custom bindings for the pageAction API.
+
+(function() {
+
+native function GetChromeHidden();
+
+GetChromeHidden().registerCustomHook('pageAction', function(bindingsAPI) {
+  var apiFunctions = bindingsAPI.apiFunctions;
+  var setIcon = bindingsAPI.setIcon;
+
+  apiFunctions.setHandleRequest('pageAction.setIcon', function(details) {
+    setIcon(details, this.name, this.definition.parameters, 'page action');
+  });
+});
+
+})();
