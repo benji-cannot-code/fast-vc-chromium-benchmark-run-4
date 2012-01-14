c91e8534d4b88aa4c1e14c392c6459aede845ca1
dslomov@google.com <dslomov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-14 19:11:21 +0000

    [Chromium] Remove WebKit::WebWorker class. https://bugs.webkit.org/show_bug.cgi?id=76327
    Reviewed by Darin Fisher.
    
    * public/WebWorker.h: Removed.
    * src/WebWorkerClientImpl.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebWorkerClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebWorkerClientImpl.cpp
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/WebString.h"
 #include "platform/WebURL.h"
 #include "WebViewImpl.h"
-#include "WebWorker.h"
 
 using namespace WebCore;
 
