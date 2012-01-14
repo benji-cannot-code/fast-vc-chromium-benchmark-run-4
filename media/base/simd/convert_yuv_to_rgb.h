ad3c04dc98d8a931cd10372039e0ae9f12937396
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-14 01:21:18 +0000

    Linear sub-rectangle scaler for use in Chromoting.
    This implementation re-uses the common row filter procedures, but is currently limited to a C horizontal interpolation procedure.
    
    There's also plenty of scope for optimizing the new sub-rectangle scaler routine.
    
    BUG=93451
    TEST=media_unittests, remoting_unittests and manual verification of image quality of Chromoting sessions using fit-to-screen.
    
    
    Review URL: http://codereview.chromium.org/8954003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117748 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/simd/convert_yuv_to_rgb.h
+++ media/base/simd/convert_yuv_to_rgb.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -124,6 +124,14 @@ void LinearScaleYUVToRGB32Row_C(const uint8* y_buf,
                                 uint8* rgb_buf,
                                 int width,
                                 int source_dx);
+
+void LinearScaleYUVToRGB32RowWithRange_C(const uint8* y_buf,
+                                         const uint8* u_buf,
+                                         const uint8* v_buf,
+                                         uint8* rgb_buf,
+                                         int dest_width,
+                                         int source_x,
+                                         int source_dx);
 
 void LinearScaleYUVToRGB32Row_MMX(const uint8* y_buf,
                                   const uint8* u_buf,
