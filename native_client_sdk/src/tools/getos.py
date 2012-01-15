95c039d2abdbd675be35104d62aa9bab799dc6c7
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-15 04:57:34 +0000

    Update SDK build scripts to green tree.
    Replace multiple SDK scripts with updated buildbot_run.py
    Remove Windows installer, use tgz/bz2 on all platforms.
    Remove scons, replace all example build components with Make.
    Add missing NMF files.
    Add oshelper to make a platform independant mv, rm, cp, mkdir
    This CL should only affect SDK bots, not Chrome.
    Commiting TBR to green SDK tree.
    
    BUG= 109207
    TBR= bradnelson@chromium.org
    Review URL: http://codereview.chromium.org/9090001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117813 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/tools/getos.py
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2012 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Determine OS
+
+Determine the name of the platform used to determine the correct Toolchain to
+invoke.
+"""
+
+import sys
+
+def GetPlatform():
+  if sys.platform.startswith('cygwin') or sys.platform.startswith('win'):
+    return 'win'
+
+  if sys.platform.startswith('darwin'):
+    return 'mac'
+
+  if sys.platform.startswith('linux'):
+    return 'linux'
+  return None
+
+if __name__ == '__main__':
+  platform = GetPlatform()
+  if platform is None:
+    print 'Unknown platform.'
+    sys.exit(1)
+
+  print platform
+  sys.exit(0)
+
