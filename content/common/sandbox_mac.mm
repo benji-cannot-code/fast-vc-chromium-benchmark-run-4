d02c6d8f74a674893c6b38024492be882c0e2850
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-16 06:27:43 +0000

    Fix leak in LoadSandboxTemplate found by Valgrind
    BUG=109901
    TEST=waterfall.sh
    
    
    Review URL: http://codereview.chromium.org/9224003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117834 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_mac.mm
+++ content/common/sandbox_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,6 +21,7 @@ extern "C" {
 #include "base/rand_util_c.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
+#include "base/memory/scoped_nsobject.h"
 #include "base/string16.h"
 #include "base/string_piece.h"
 #include "base/string_util.h"
@@ -377,15 +378,15 @@ NSString* LoadSandboxTemplate(int sandbox_type) {
     return nil;
   }
 
-  NSString* common_sandbox_prefix_data =
+  scoped_nsobject<NSString> common_sandbox_prefix_data(
       [[NSString alloc] initWithBytes:common_sandbox_definition.data()
                                length:common_sandbox_definition.length()
-                             encoding:NSUTF8StringEncoding];
+                             encoding:NSUTF8StringEncoding]);
 
-  NSString* sandbox_data =
+  scoped_nsobject<NSString> sandbox_data(
       [[NSString alloc] initWithBytes:sandbox_definition.data()
                                length:sandbox_definition.length()
-                             encoding:NSUTF8StringEncoding];
+                             encoding:NSUTF8StringEncoding]);
 
   // Prefix sandbox_data with common_sandbox_prefix_data.
   return [common_sandbox_prefix_data stringByAppendingString:sandbox_data];
