88250119514c1ee8cd90fa73eac5549e98586e3d
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-16 06:24:23 +0000

    Re-enable PrintDialogCloudTest.HandlersRegistered on non-Windows and move it back to browser_tests.
    This is not failing any more.
    
    BUG=103497
    TEST=Trybot
    
    
    Review URL: http://codereview.chromium.org/9139008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117833 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud_uitest.cc
+++ chrome/browser/printing/print_dialog_cloud_uitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -228,9 +228,8 @@ net::URLRequestJob* PrintDialogCloudTest::Factory(net::URLRequest* request,
   return new SimpleTestJob(request);
 }
 
-#if defined(OS_WIN) || defined(USE_AURA)
+#if defined(OS_WIN)
 // http://crbug.com/94864 for OS_WIN issue
-// http://crbug.com/103497 for USE_AURA issue
 #define MAYBE_HandlersRegistered DISABLED_HandlersRegistered
 #else
 #define MAYBE_HandlersRegistered HandlersRegistered
