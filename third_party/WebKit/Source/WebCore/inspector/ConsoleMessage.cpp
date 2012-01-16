46e7ae6d4370255952dc9f69865bc5ca436b7c14
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-16 13:44:14 +0000

    Web Inspector: CodeGeneratorInspector.py: generate array types. https://bugs.webkit.org/show_bug.cgi?id=75284
    Patch by Peter Rybin <peter.rybin@gmail.com> on 2012-01-16
    Reviewed by Yury Semikhatsky.
    
    New classes are generated for ecah array type instance. Some includes
    and ifdefs are fixed.
    
    * inspector/CodeGeneratorInspector.py:
    (ArrayBinding):
    (ArrayBinding.get_code_generator):
    (ArrayBinding.get_code_generator.CodeGenerator):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder.AdHocTypeContext):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix.NameFix):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder.AdHocTypeContext.get_type_name_fix.NameFix.output_comment):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_type_builder.AdHocTypeContext.call_generate_type_builder):
    (ArrayBinding.get_code_generator.CodeGenerator.generate_forward_declaration):
    (ArrayBinding.get_code_generator.CodeGenerator.register_use):
    (ArrayBinding.get_in_c_type_text):
    (ArrayBinding.get_setter_value_expression_pattern):
    (ArrayBinding.reduce_to_raw_type):
    (RawTypeBinding):
    (RawTypeBinding.__init__):
    (RawTypeBinding.get_code_generator):
    (RawTypeBinding.get_in_c_type_text):
    (RawTypeBinding.get_setter_value_expression_pattern):
    (RawTypeBinding.reduce_to_raw_type):
    (TypeData.__init__):
    (TypeData.get_binding):
    * inspector/ConsoleMessage.cpp:
    * inspector/InspectorValues.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
+++ third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
@@ -30,9 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "ConsoleMessage.h"
 
 #if ENABLE(INSPECTOR)
+
+#include "ConsoleMessage.h"
 
 #include "Console.h"
 #include "InjectedScript.h"
