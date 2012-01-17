e51979e3242104bf1e60a8f4c56b02a86b30d7cf
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 15:24:53 +0000

    Revert r117895 "Convert use of int ms to TimeDelta in files owned by bulach."
    This broke compilation on Windows debug.
    
    R=bulach@chromium.org
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9242010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117897 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/wifi_data_provider_common.cc
+++ content/browser/geolocation/wifi_data_provider_common.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -106,5 +106,5 @@ void WifiDataProviderCommon::ScheduleNextScan(int interval) {
       FROM_HERE,
       base::Bind(&WifiDataProviderCommon::DoWifiScanTask,
                  weak_factory_.GetWeakPtr()),
-      base::TimeDelta::FromMilliseconds(interval));
+      interval);
 }
