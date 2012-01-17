8d3f11a84063d06548a845936252f0432c32e69e
tpayne@chromium.org <tpayne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 20:00:09 +0000

    [i18n fixlet]
    fix RTL on version page
    
    Ignore the extension_setting.css change, it is from another CL. I'll figure out how to revert it.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9186056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117941 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/about_ui.cc
+++ chrome/browser/ui/webui/about_ui.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
+#include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
 #include "chrome/common/about_handler.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_version_info.h"
@@ -1101,6 +1102,7 @@ std::string AboutVersionStrings(DictionaryValue* localized_strings,
     localized_strings->SetString("profile_path",
         l10n_util::GetStringUTF16(IDS_ABOUT_VERSION_PATH_NOTFOUND));
   }
+  ChromeWebUIDataSource::SetFontAndTextDirection(localized_strings);
 
   std::string data;
   jstemplate_builder::AppendJsonJS(localized_strings, &data);
