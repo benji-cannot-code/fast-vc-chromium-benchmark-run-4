7c92a851a3ad4a4ab93fc5e62de80f8cca20e4c3
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 13:23:44 +0000

    Fix Win Aura trybots
    TBR=sky@chromium.org
    
    BUG=none
    TEST=win_aura:compile works
    
    
    Review URL: http://codereview.chromium.org/9243005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117891 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_listener.h
+++ ui/views/controls/menu/menu_listener.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,11 +14,13 @@ namespace views {
 // An interface for clients that want a notification when a menu is opened.
 class VIEWS_EXPORT MenuListener {
  public:
+  MenuListener();
+
   // This will be called after the menu has actually opened.
   virtual void OnMenuOpened() = 0;
 
  protected:
-  virtual ~MenuListener() {}
+  virtual ~MenuListener();
 };
 
 }  // namespace views
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -123,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'controls/menu/menu_item_view.h',
         'controls/menu/menu_item_view_views.cc',
         'controls/menu/menu_item_view_win.cc',
+        'controls/menu/menu_listener.cc',
         'controls/menu/menu_listener.h',
         'controls/menu/menu_model_adapter.cc',
         'controls/menu/menu_model_adapter.h',
