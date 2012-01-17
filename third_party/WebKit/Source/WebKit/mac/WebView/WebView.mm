6603cacf72315c9ad24e591b23b0836cd08ae611
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-17 23:26:12 +0000

    REGRESSION (r101413): Correction panel overlaps misspelled text. https://bugs.webkit.org/show_bug.cgi?id=74589 <rdar://problem/10585115>
    When converting the rectangle from the root view, we need
    to take into account the height of the rectangle.
            
    Reviewed by Anders Carlsson.
    
    * WebView/WebView.mm:
    (-[WebView _convertRectFromRootView:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
@@ -6307,7 +6307,7 @@ static void glibContextIterationCallback(CFRunLoopObserverRef, CFRunLoopActivity
 
 - (NSRect)_convertRectFromRootView:(NSRect)rect
 {
-    return NSMakeRect(rect.origin.x, [self bounds].size.height - rect.origin.y, rect.size.width, rect.size.height);
+    return NSMakeRect(rect.origin.x, [self bounds].size.height - rect.origin.y - rect.size.height, rect.size.width, rect.size.height);
 }
 
 @end
