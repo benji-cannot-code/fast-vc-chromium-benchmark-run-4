d6c6cbbf074bac5bd0d4f9e4c6b67906d0333bd8
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-17 15:29:37 +0000

    Web Inspector: ConsoleMessage.cpp and InspectorResourceAgent.cpp doesn't conform to Inspector.json https://bugs.webkit.org/show_bug.cgi?id=76403
    A couple of protocol fixes.
    
    Reviewed by Pavel Feldman.
    
    * inspector/ConsoleMessage.cpp: return 'log' in case some unsupported value is passed.
    We cannot omit that return statement as GCC would complain on missing return statement
    despite all enum values are listed.
    (WebCore::messageTypeValue):
    * inspector/Inspector-0.1.json: fixed v0.1 protocol definition.
    * inspector/Inspector.json: made CachedResource.response field optional.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
+++ third_party/WebKit/Source/WebCore/inspector/ConsoleMessage.cpp
@@ -119,7 +119,7 @@ static String messageTypeValue(MessageType type)
     case EndGroupMessageType: return "endGroup";
     case AssertMessageType: return "assert";
     }
-    return "other";
+    return "log";
 }
 
 static String messageLevelValue(MessageLevel level)
