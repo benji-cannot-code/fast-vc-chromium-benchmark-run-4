a84c471cd446e5586ae0715b5896a093a8fb02e4
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 21:53:12 +0000

    aura: Add method to the RootWindow to warp the mouse pointer.
    BUG=102563
    TEST=manually
    
    
    Review URL: http://codereview.chromium.org/9179013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117967 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_win.h
+++ ui/aura/root_window_host_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -32,6 +32,7 @@ class RootWindowHostWin : public RootWindowHost, public ui::WindowImpl {
   virtual void SetCursor(gfx::NativeCursor cursor) OVERRIDE;
   virtual void ShowCursor(bool show) OVERRIDE;
   virtual gfx::Point QueryMouseLocation() OVERRIDE;
+  virtual void MoveCursorTo(const gfx::Point& location) OVERRIDE;
   virtual void PostNativeEvent(const base::NativeEvent& native_event) OVERRIDE;
 
  private:
