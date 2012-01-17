7e8433830f4e0d5b10f4d0de79cb6316a816c9f6
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-17 15:34:35 +0000

    Web Inspector: Dialogs style and DialogDelegate interface fixes. https://bugs.webkit.org/show_bug.cgi?id=76449
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/Dialog.js:
    (WebInspector.Dialog):
    (WebInspector.Dialog.currentInstance):
    (WebInspector.Dialog.show):
    (WebInspector.Dialog.hide):
    (WebInspector.Dialog.prototype._hide):
    (WebInspector.Dialog.prototype._onGlassPaneFocus):
    (WebInspector.Dialog.prototype._onFocus):
    (WebInspector.Dialog.prototype._position):
    (WebInspector.Dialog.prototype._onKeyDown):
    (WebInspector.DialogDelegate.prototype.wasShown):
    (WebInspector.DialogDelegate.prototype.position):
    (WebInspector.DialogDelegate.prototype.focus):
    (WebInspector.DialogDelegate.prototype.onEnter):
    (WebInspector.DialogDelegate.prototype.willHide):
    * inspector/front-end/GoToLineDialog.js:
    (WebInspector.GoToLineDialog):
    (WebInspector.GoToLineDialog.prototype.focus):
    (WebInspector.GoToLineDialog.prototype._onGoClick):
    (WebInspector.GoToLineDialog.prototype._applyLineNumber):
    (WebInspector.GoToLineDialog.prototype.onEnter):
    * inspector/front-end/dialog.css:
    (.dialog):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/GoToLineDialog.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/GoToLineDialog.js
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @constructor
- * @implements {WebInspector.DialogDelegate}
+ * @extends {WebInspector.DialogDelegate}
  */
 WebInspector.GoToLineDialog = function(view)
 {
@@ -48,10 +48,14 @@ WebInspector.GoToLineDialog = function(view)
 
     this._goButton = this.element.createChild("button");
     this._goButton.textContent = WebInspector.UIString("Go");
+    this._goButton.addEventListener("click", this._onGoClick.bind(this), false);
 
     this._view = view;
 }
 
+/**
+ * @param {WebInspector.Panel} panel
+ */
 WebInspector.GoToLineDialog.install = function(panel, viewGetter)
 {
     function showGoToLineDialog()
@@ -82,22 +86,29 @@ WebInspector.GoToLineDialog.createShortcut = function()
 }
 
 WebInspector.GoToLineDialog.prototype = {
-    get defaultFocusedElement()
+    focus: function()
     {
-        return this._input;
-    },
-
-    get okButton()
-    {
-        return this._goButton;
+        WebInspector.setCurrentFocusElement(this._input);
+        this._input.select();
     },
     
-    onAction: function()
+    _onGoClick: function()
+    {
+        this._applyLineNumber();
+        WebInspector.Dialog.hide();
+    },
+    
+    _applyLineNumber: function()
     {
         var value = this._input.value;
         var lineNumber = parseInt(value, 10) - 1;
         if (!isNaN(lineNumber) && lineNumber >= 0)
             this._view.highlightLine(lineNumber);
+    },
+    
+    onEnter: function()
+    {
+        this._applyLineNumber();
     }
 }
 
