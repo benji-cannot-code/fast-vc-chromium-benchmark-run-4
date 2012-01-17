9064007516e7a4f5e83e16a0197c7012ca8b9752
kgr@chromium.org <kgr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 21:52:02 +0000

    Captured cookies WEBUI improved button spacing and layout. Sets dialog title.
    Rather than bottom-justifing the buttons so that there's no ugly space after
    them when the info-area isn't expanded, I've made it so that the two tree
    components are slightly expanded when the info-area isn't expanded so that
    the bottons are still justified along the bottom.  This approach doesn't 
    waste any screen space.  All buttons are left-justified, except for the close
    button which appears on the right.  These changes work both RTL and LTR.
    
    This is a replacement for CL 8968029.
    
    BUG=104494
    TEST=manually
    
    Review URL: http://codereview.chromium.org/9207010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/collected_cookies.js
+++ chrome/browser/resources/collected_cookies.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -64,10 +64,6 @@ function load() {
       handleAllowThisSessionButtonClick);
 
   $('close-button').addEventListener('click', closeDialog);
-
-  document.oncontextmenu = function(e) {
-    e.preventDefault();
-  }
 
   document.onkeydown = function(e) {
     if (e.keyCode == 27)  // Esc
