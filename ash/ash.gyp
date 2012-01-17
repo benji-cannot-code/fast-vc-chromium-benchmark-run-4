8d111812b52434d9b79498243c6642337e186237
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 19:12:04 +0000

    Aura: Resize windows when screen/host window size changes
    * Monitor root window for size changes and resize maximized windows when it changes. This also ensures windows in compact window mode continue to fill the screen.
    * Merged CompactLayoutManager and ToplevelLayoutManager.  I expected them to diverge, but they ended up being nearly identical.
    * Expanded unit test coverage of ToplevelLayoutManager.
    * Put a few window_util functions into a namespace.  I didn't do them all because the patch would be too big.
    
    BUG=109952
    TEST=Run Aura, maximize an Aura window, change the size of the host window (or plug an external monitor into your ChromeOS device).  Window should still fill the screen.  Run Aura in compact window mode (default on small screen laptops).  Plug in an external monitor.  Windows should still fill the screen.
    
    Review URL: https://chromiumcodereview.appspot.com/9214009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117931 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash.gyp
+++ ash/ash.gyp
@@ -101,8 +101,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'wm/activation_controller.h',
         'wm/always_on_top_controller.cc',
         'wm/always_on_top_controller.h',
-        'wm/compact_layout_manager.cc',
-        'wm/compact_layout_manager.h',
         'wm/compact_status_area_layout_manager.cc',
         'wm/compact_status_area_layout_manager.h',
         'wm/default_container_event_filter.cc',
--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -1,12 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "ash/wm/window_util.h"
 
 #include "ash/wm/activation_controller.h"
-#include "ash/wm/property_util.h"
 #include "ui/aura/client/activation_client.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/root_window.h"
@@ -15,11 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/screen.h"
 
 namespace ash {
-
-bool IsWindowMaximized(aura::Window* window) {
-  return window->GetIntProperty(aura::client::kShowStateKey) ==
-      ui::SHOW_STATE_MAXIMIZED;
-}
 
 void ActivateWindow(aura::Window* window) {
   aura::client::GetActivationClient()->ActivateWindow(window);
@@ -41,30 +35,16 @@ aura::Window* GetActivatableWindow(aura::Window* window) {
   return internal::ActivationController::GetActivatableWindow(window);
 }
 
-void UpdateBoundsFromShowState(aura::Window* window) {
-  switch (window->GetIntProperty(aura::client::kShowStateKey)) {
-    case ui::SHOW_STATE_NORMAL: {
-      const gfx::Rect* restore = GetRestoreBounds(window);
-      window->SetProperty(aura::client::kRestoreBoundsKey, NULL);
-      if (restore)
-        window->SetBounds(*restore);
-      delete restore;
-      break;
-    }
+namespace window_util {
 
-    case ui::SHOW_STATE_MAXIMIZED:
-      SetRestoreBoundsIfNotSet(window);
-      window->SetBounds(gfx::Screen::GetMonitorWorkAreaNearestWindow(window));
-      break;
+bool IsWindowMaximized(aura::Window* window) {
+  return window->GetIntProperty(aura::client::kShowStateKey) ==
+      ui::SHOW_STATE_MAXIMIZED;
+}
 
-    case ui::SHOW_STATE_FULLSCREEN:
-      SetRestoreBoundsIfNotSet(window);
-      window->SetBounds(gfx::Screen::GetMonitorAreaNearestWindow(window));
-      break;
-
-    default:
-      break;
-  }
+bool IsWindowFullscreen(aura::Window* window) {
+  return window->GetIntProperty(aura::client::kShowStateKey) ==
+      ui::SHOW_STATE_FULLSCREEN;
 }
 
 bool HasFullscreenWindow(const WindowSet& windows) {
@@ -77,4 +57,5 @@ bool HasFullscreenWindow(const WindowSet& windows) {
   return false;
 }
 
+}  // namespace window_util
 }  // namespace ash
