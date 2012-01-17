2da846836c0e953076d913ad9f7a06c895562d1f
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 20:35:59 +0000

    Update expectations.
    Minor regression:
    xp-release-dual-core/moz/total_op_b/IO_op_b: http://build.chromium.org/f/chromium/perf/xp-release-dual-core/moz/report.html?history=150&rev=117931&graph=total_op_b
    
    Also update test output to be more clear.
    
    TBR=thakis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9240017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117949 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf_expectations/tests/perf_expectations_unittest.py
+++ tools/perf_expectations/tests/perf_expectations_unittest.py
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-#
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -152,7 +151,8 @@ class PerfExpectationsUnittest(unittest.TestCase):
   def testNoUpdatesNeeded(self):
     p = subprocess.Popen([MAKE_EXPECTATIONS, '-s'], stdout=subprocess.PIPE)
     p.wait();
-    self.assertEqual(p.returncode, 0, msg='Expectations has pending updates!')
+    self.assertEqual(p.returncode, 0,
+        msg='Update expectations first by running ./make_expectations.py')
 
   def testConfigFile(self):
     # Test that the config file can be parsed as JSON.
