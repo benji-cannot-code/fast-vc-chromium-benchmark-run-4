391ae279bacb51f3a142a5369cac31cbd1470121
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 17:47:16 +0000

    Some style cleanups for text_elider.
    These were brought up during my C++ readability review.
    
    BUG=none
    TEST=existing unit tests
    
    Review URL: http://codereview.chromium.org/9226009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117912 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/text/text_elider.h
+++ ui/base/text/text_elider.h
@@ -1,7 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// This file defines utility functions for eliding and formatting UI text.
 
 #ifndef UI_BASE_TEXT_TEXT_ELIDER_H_
 #define UI_BASE_TEXT_TEXT_ELIDER_H_
@@ -99,6 +101,8 @@ class UI_EXPORT SortedDisplayURL {
   size_t prefix_end_;
 
   string16 display_url_;
+
+  DISALLOW_COPY_AND_ASSIGN(SortedDisplayURL);
 };
 
 // Functions to elide strings when the font information is unknown.  As
