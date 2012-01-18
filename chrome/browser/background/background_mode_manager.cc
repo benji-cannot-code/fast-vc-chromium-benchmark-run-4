ae2b8db029bfd27f5c4388a24bc352b1de737322
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 20:22:16 +0000

    [i18n-fixlet] Make strings branding specific in Wrench menu.
    BUG=NONE
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9148072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118133 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/background/background_mode_manager.cc
+++ chrome/browser/background/background_mode_manager.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -669,8 +669,7 @@ void BackgroundModeManager::UpdateStatusTrayIconContextMenu() {
   // Create a context menu item for Chrome.
   ui::SimpleMenuModel* menu = new ui::SimpleMenuModel(this);
   // Add About item
-  menu->AddItem(IDC_ABOUT, l10n_util::GetStringFUTF16(IDS_ABOUT,
-      l10n_util::GetStringUTF16(IDS_PRODUCT_NAME)));
+  menu->AddItem(IDC_ABOUT, l10n_util::GetStringUTF16(IDS_ABOUT));
   menu->AddItemWithStringId(IDC_TASK_MANAGER, IDS_TASK_MANAGER);
   menu->AddSeparator();
 
