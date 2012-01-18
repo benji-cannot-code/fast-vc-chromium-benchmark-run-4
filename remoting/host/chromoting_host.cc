9c429757b899ed770683f49f92bf53e6df7f83c0
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 03:33:36 +0000

    Remove set_it2me() from ChromotingHost.
    Review URL: http://codereview.chromium.org/9187077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118028 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host.cc
+++ remoting/host/chromoting_host.cc
@@ -44,7 +44,8 @@ ChromotingHost::ChromotingHost(
       stopping_recorders_(0),
       state_(kInitial),
       protocol_config_(protocol::CandidateSessionConfig::CreateDefault()),
-      is_it2me_(false) {
+      authenticating_client_(false),
+      reject_authenticating_client_(false) {
   DCHECK(context_);
   DCHECK(signal_strategy);
   DCHECK(desktop_environment_);
@@ -127,6 +128,11 @@ void ChromotingHost::RemoveStatusObserver(HostStatusObserver* observer) {
   status_observers_.RemoveObserver(observer);
 }
 
+void ChromotingHost::RejectAuthenticatingClient() {
+  DCHECK(authenticating_client_);
+  reject_authenticating_client_ = true;
+}
+
 void ChromotingHost::SetAuthenticatorFactory(
     scoped_ptr<protocol::AuthenticatorFactory> authenticator_factory) {
   DCHECK(context_->network_message_loop()->BelongsToCurrentThread());
@@ -169,8 +175,17 @@ void ChromotingHost::OnSessionAuthenticated(ClientSession* client) {
 
   // Notify observers that there is at least one authenticated client.
   const std::string& jid = client->connection()->session()->jid();
+
+  reject_authenticating_client_ = false;
+
+  authenticating_client_ = true;
   FOR_EACH_OBSERVER(HostStatusObserver, status_observers_,
                     OnClientAuthenticated(jid));
+  authenticating_client_ = false;
+
+  if (reject_authenticating_client_) {
+    client->Disconnect();
+  }
 }
 
 void ChromotingHost::OnSessionAuthenticationFailed(ClientSession* client) {
@@ -224,16 +239,6 @@ void ChromotingHost::OnIncomingSession(
 
   if (state_ != kStarted) {
     *response = protocol::SessionManager::DECLINE;
-    return;
-  }
-
-  // If we are running Me2Mom and already have an authenticated client then
-  // one of the connections may be an attacker, so both are suspect.
-  if (is_it2me_ && AuthenticatedClientsCount() > 0) {
-    *response = protocol::SessionManager::DECLINE;
-
-    // Close existing sessions and shutdown the host.
-    Shutdown(base::Closure());
     return;
   }
 
@@ -315,18 +320,6 @@ Encoder* ChromotingHost::CreateEncoder(const protocol::SessionConfig& config) {
   }
 
   return NULL;
-}
-
-int ChromotingHost::AuthenticatedClientsCount() const {
-  DCHECK(context_->network_message_loop()->BelongsToCurrentThread());
-
-  int authenticated_clients = 0;
-  for (ClientList::const_iterator it = clients_.begin(); it != clients_.end();
-       ++it) {
-    if ((*it)->authenticated())
-      ++authenticated_clients;
-  }
-  return authenticated_clients;
 }
 
 void ChromotingHost::StopScreenRecorder() {
