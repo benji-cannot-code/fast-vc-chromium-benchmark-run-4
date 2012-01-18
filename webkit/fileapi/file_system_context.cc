7b9392c9b9eef9eb3c14e7bdcf9492d34d894e1a
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 05:25:04 +0000

    Use WebKit API's filesystem types directly under webkit/fileapi
    BUG=none
    TEST=no funcational changes
    
    
    Review URL: http://codereview.chromium.org/9241013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118064 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_context.cc
+++ webkit/fileapi/file_system_context.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/message_loop_proxy.h"
 #include "googleurl/src/gurl.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebFileSystem.h"
 #include "webkit/fileapi/file_system_callback_dispatcher.h"
 #include "webkit/fileapi/file_system_file_util.h"
 #include "webkit/fileapi/file_system_operation_interface.h"
@@ -197,10 +196,3 @@ FileSystemOperationInterface* FileSystemContext::CreateFileSystemOperation(
 }
 
 }  // namespace fileapi
-
-COMPILE_ASSERT(int(WebKit::WebFileSystem::TypeTemporary) == \
-               int(fileapi::kFileSystemTypeTemporary), mismatching_enums);
-COMPILE_ASSERT(int(WebKit::WebFileSystem::TypePersistent) == \
-               int(fileapi::kFileSystemTypePersistent), mismatching_enums);
-COMPILE_ASSERT(int(WebKit::WebFileSystem::TypeExternal) == \
-               int(fileapi::kFileSystemTypeExternal), mismatching_enums);
