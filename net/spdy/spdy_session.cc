9de46e72f21208800cba094cf2c6dd092908560f
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 04:12:22 +0000

    SPDY - bug fix for WINDOW_UPDATE logging
    BUG=108356
    R=wtc
    TEST=network unit tests
    
    Review URL: http://codereview.chromium.org/9242021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118036 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1480,7 +1480,7 @@ void SpdySession::OnWindowUpdate(
     stream->IncreaseSendWindowSize(delta_window_size);
 
   net_log_.AddEvent(
-      NetLog::TYPE_SPDY_SESSION_SEND_WINDOW_UPDATE,
+      NetLog::TYPE_SPDY_SESSION_RECV_WINDOW_UPDATE,
       make_scoped_refptr(new NetLogSpdyWindowUpdateParameter(
           stream_id, delta_window_size, stream->send_window_size())));
 }
@@ -1492,7 +1492,7 @@ void SpdySession::SendWindowUpdate(spdy::SpdyStreamId stream_id,
   CHECK_EQ(stream->stream_id(), stream_id);
 
   net_log_.AddEvent(
-      NetLog::TYPE_SPDY_SESSION_RECV_WINDOW_UPDATE,
+      NetLog::TYPE_SPDY_SESSION_SEND_WINDOW_UPDATE,
       make_scoped_refptr(new NetLogSpdyWindowUpdateParameter(
           stream_id, delta_window_size, stream->recv_window_size())));
 
