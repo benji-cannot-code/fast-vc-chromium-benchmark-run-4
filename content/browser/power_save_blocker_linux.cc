d1d4df5add353530d547db9947f2a3d5cf3448bc
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 03:25:21 +0000

    Linux: use the binary name rather than the (debugging-only) thread name when contacting the power save inhibitor daemon over DBus.
    Note that this only solves part of the problem for the unit tests, but we should do this for the browser itself.
    
    BUG=110022
    
    Review URL: http://codereview.chromium.org/9112047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118023 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/power_save_blocker_linux.cc
+++ content/browser/power_save_blocker_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/environment.h"
+#include "base/file_path.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -153,7 +155,8 @@ class KDEPowerSaveBlocker: public DBusPowerSaveBlocker::Delegate {
         // the reason of the power save block request.
         // The method returns a cookie (an int), which we must pass back to the
         // UnInhibit method when we cancel our request.
-        message_writer.AppendString(base::PlatformThread::GetName());
+        message_writer.AppendString(
+            CommandLine::ForCurrentProcess()->GetProgram().value());
         message_writer.AppendString(DBusPowerSaveBlocker::kPowerSaveReason);
         bus_callback = base::Bind(&KDEPowerSaveBlocker::OnInhibitResponse,
                                   this);
@@ -317,7 +320,8 @@ class GnomePowerSaveBlocker: public DBusPowerSaveBlocker::Delegate {
         // The method returns and inhibit_cookie, used to uniquely identify
         // this request. It should be used as an argument to Uninhibit()
         // in order to remove the request.
-        message_writer.AppendString(base::PlatformThread::GetName());
+        message_writer.AppendString(
+            CommandLine::ForCurrentProcess()->GetProgram().value());
         message_writer.AppendUint32(0);  // should be toplevel_xid
         message_writer.AppendString(DBusPowerSaveBlocker::kPowerSaveReason);
         message_writer.AppendUint32(flags);
