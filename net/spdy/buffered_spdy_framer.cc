3778c669107203b42f978c66eac98bb2e6ba6d64
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 20:46:11 +0000

    SPDY - FlipServer - changes to use BufferedSpdyFramer.
    TEST=network unittests
    R=wtc
    
    Review URL: http://codereview.chromium.org/9174026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118139 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/buffered_spdy_framer.cc
+++ net/spdy/buffered_spdy_framer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -108,6 +108,19 @@ SpdyFramer::SpdyError BufferedSpdyFramer::error_code() const {
 
 SpdyFramer::SpdyState BufferedSpdyFramer::state() const {
   return spdy_framer_.state();
+}
+
+bool BufferedSpdyFramer::MessageFullyRead() {
+  return spdy_framer_.MessageFullyRead();
+}
+
+bool BufferedSpdyFramer::HasError() {
+  return spdy_framer_.HasError();
+}
+
+bool BufferedSpdyFramer::ParseHeaderBlock(const SpdyFrame* frame,
+                                          SpdyHeaderBlock* block) {
+  return spdy_framer_.ParseHeaderBlock(frame, block);
 }
 
 SpdySynStreamControlFrame* BufferedSpdyFramer::CreateSynStream(
