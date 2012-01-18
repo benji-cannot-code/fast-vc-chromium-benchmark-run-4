cc70030a7bccdc6230327a6db1444a37691ad6e6
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 04:12:31 +0000

    Change autofill profile commit behavior to commit instantly. Enable the perf test case that was failing because of the fact that this was not instant.
    BUG=109037
    TEST=sync_integration_tests.exe, sync_performance_tests.exe, sync_unit_tests.exe, unit_tests.exe and manual test cases.
    
    
    Review URL: http://codereview.chromium.org/9169030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/performance/autofill_sync_perf_test.cc
+++ chrome/browser/sync/test/integration/performance/autofill_sync_perf_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -165,10 +165,7 @@ const std::string AutofillSyncPerfTest::IntToValue(int n) {
   return StringPrintf("Value%d", n);
 }
 
-
-// TODO(lipalani): Bug 108321. Make a bookmark change so it syncs right away.
-IN_PROC_BROWSER_TEST_F(AutofillSyncPerfTest,
-                       DISABLED_AutofillProfiles_P0) {
+IN_PROC_BROWSER_TEST_F(AutofillSyncPerfTest, AutofillProfiles_P0) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   // TCM ID - 7557873.
