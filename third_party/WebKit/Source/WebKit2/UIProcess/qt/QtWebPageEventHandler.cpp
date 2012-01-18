bb06bc2d5cafc50a430277c3c557000695dfda09
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-18 17:35:27 +0000

    [Qt][WK2] Fix the debug build.
    Unreviewed.
    
    * UIProcess/qt/QtWebPageEventHandler.cpp:
    (setInputPanelVisible): Remove assertion from non-member function.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageEventHandler.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageEventHandler.cpp
@@ -428,8 +428,6 @@ void QtWebPageEventHandler::resetGestureRecognizers()
 
 static void setInputPanelVisible(bool visible)
 {
-    Q_ASSERT(m_webView->hasFocus());
-
     if (qApp->inputPanel()->visible() == visible)
         return;
 
