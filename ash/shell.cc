b047e380f8f5b52686e5a7afa3d2c2521afa7cf7
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 23:57:05 +0000

    Aura: Split out CompactLayoutManager from ToplevelLayoutManager
    Move the shared code to BaseLayoutManager.  Adjust the unit tests.  Alice needs this for some compact mode work she's doing right now.
    
    BUG=110648
    TEST=Run Aura in both --aura-window-mode=normal and --aura-window-mode=compact, both still start correctly.  Also unit test coverage from CompactLayoutManagerTest and ToplevelLayoutManagerTest
    
    Review URL: https://chromiumcodereview.appspot.com/9255024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118186 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_window_ids.h"
 #include "ash/tooltips/tooltip_controller.h"
 #include "ash/wm/activation_controller.h"
+#include "ash/wm/compact_layout_manager.h"
 #include "ash/wm/compact_status_area_layout_manager.h"
 #include "ash/wm/default_container_event_filter.h"
 #include "ash/wm/default_container_layout_manager.h"
@@ -316,10 +317,10 @@ void Shell::InitLayoutManagers(aura::RootWindow* root_window) {
   // desktop background, etc.  The launcher still exists so we can use its
   // data model and list of open windows, but we hide the UI to save space.
   if (IsWindowModeCompact()) {
-    internal::ToplevelLayoutManager* toplevel_layout_manager =
-        new internal::ToplevelLayoutManager();
-    default_container->SetLayoutManager(toplevel_layout_manager);
-    toplevel_layout_manager->set_status_area_widget(status_widget);
+    internal::CompactLayoutManager* compact_layout_manager =
+        new internal::CompactLayoutManager();
+    default_container->SetLayoutManager(compact_layout_manager);
+    compact_layout_manager->set_status_area_widget(status_widget);
     internal::CompactStatusAreaLayoutManager* status_area_layout_manager =
         new internal::CompactStatusAreaLayoutManager(status_widget);
     GetContainer(internal::kShellWindowId_StatusContainer)->
