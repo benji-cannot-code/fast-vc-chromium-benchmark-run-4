dfece487bac3357710edada080a274021d42c611
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 18:12:59 +0000

    Drop obsolete earth tests from nacl_integration disabled tests
    These tests are gone, so no need to have them in the disabled list.
    
    BUG= 108379
    TEST= nacl_integration
    
    R=noelallen@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9107020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -32,10 +32,6 @@ def Main(args):
     # TODO(ncbray): Reenable when this issue is resolved.
     # http://code.google.com/p/nativeclient/issues/detail?id=2091
     tests_to_disable.append('run_ppapi_bad_browser_test')
-    # TODO(wez): Reenable when he duplicate copy of the test has been removed.
-    # http://code.google.com/p/chromium/issues/detail?id=108379
-    tests_to_disable.append('earth_browser_test_c')
-    tests_to_disable.append('earth_browser_test_cc')
     # This thread safety stress test is flaky on at least Windows.
     # See http://code.google.com/p/nativeclient/issues/detail?id=2124
     # TODO(mseaborn): Reenable when this issue is resolved.
