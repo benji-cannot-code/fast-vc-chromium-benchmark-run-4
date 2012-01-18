3e023f0fe5b85241d982829194a0b49e17fc8ee1
ostapenko.viatcheslav@nokia.com <ostapenko.viatcheslav@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-18 00:10:53 +0000

    [Qt] Debug build fails with debug qt5 https://bugs.webkit.org/show_bug.cgi?id=76463
    Reviewed by Simon Hausmann.
    
    Add QNetworkCookie include in order to satisfy sizeof in QTypeInfo in debug builds.
    
    Source/WebKit/qt:
    
    * WebCoreSupport/FrameNetworkingContextQt.cpp:
    
    Tools:
    
    * QtTestBrowser/cookiejar.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameNetworkingContextQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameNetworkingContextQt.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qwebframe_p.h"
 #include "qwebpage.h"
 #include <QNetworkAccessManager>
+#include <QNetworkCookie>
 #include <QNetworkCookieJar>
 
 namespace WebCore {
--- third_party/WebKit/Tools/QtTestBrowser/cookiejar.h
+++ third_party/WebKit/Tools/QtTestBrowser/cookiejar.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define cookiejar_h
 
 #include <QFile>
+#include <QNetworkCookie>
 #include <QNetworkCookieJar>
 #include <QTimer>
 
