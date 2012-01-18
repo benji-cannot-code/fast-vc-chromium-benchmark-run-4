1b7e59192187e6b0c6e93757455925aac303a63a
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 20:06:35 +0000

    Roll deps for filesystem URL changes, take 3.
    BUG=none
    TEST=existing googleurl unit tests continue to pass
    
    Review URL: https://chromiumcodereview.appspot.com/9104001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118128 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_resource_providers.cc
+++ chrome/browser/task_manager/task_manager_resource_providers.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -114,11 +114,7 @@ TaskManagerRendererResource::TaskManagerRendererResource(
   // We cache the process and pid as when a Tab/BackgroundContents is closed the
   // process reference becomes NULL and the TaskManager still needs it.
   pid_ = base::GetProcId(process_);
-  stats_.images.size = 0;
-  stats_.cssStyleSheets.size = 0;
-  stats_.scripts.size = 0;
-  stats_.xslStyleSheets.size = 0;
-  stats_.fonts.size = 0;
+  memset(&stats_, 0, sizeof(stats_));
 }
 
 TaskManagerRendererResource::~TaskManagerRendererResource() {
--- chrome/installer/util/master_preferences_dummy.cc
+++ chrome/installer/util/master_preferences_dummy.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -56,4 +56,18 @@ const MasterPreferences& MasterPreferences::ForCurrentProcess() {
   static MasterPreferences prefs(*CommandLine::ForCurrentProcess());
   return prefs;
 }
+}
+
+// The use of std::vector<GURL>() above requires us to have a destructor for
+// GURL.  GURL contains a member of type url_parse::Parsed, which declares (but
+// does not implement) an explicit destructor in its header file.  We're missing
+// the real implementation by not depending on the googleurl library.  However,
+// we don't really need it, so we just replace it here rather than building a
+// 64-bit version of the googleurl library with all its dependencies.
+namespace url_parse {
+
+Parsed::~Parsed() {
+  NOTREACHED();
+}
+
 }
