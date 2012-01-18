f712e348a1dd022ed0f3f4cb5a783319bf47db33
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 22:26:40 +0000

    aura/chromeos: Avoid suspending while video is playing.
    This adds an ash::VideoDetector class that watches for layer
    updates and attempts to detect the playback of video.  A
    Chrome OS-specific observer updates a _CHROME_VIDEO_TIME
    property on the X root window, which is used by the power
    manager to defer screen dimming or other power management
    features.  This matches the implementation currently in use
    on Chrome OS, with Chrome taking the X window manager's role
    for detecting video and setting the property.
    
    BUG=110114
    TEST=added; also manually checked that the property is updated
    
    
    Review URL: http://codereview.chromium.org/9249004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118171 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/legacy_window_manager/initial_browser_window_observer.h"
 #include "chrome/browser/chromeos/power/power_button_controller_delegate_chromeos.h"
 #include "chrome/browser/chromeos/power/power_button_observer.h"
+#include "chrome/browser/chromeos/power/video_property_writer.h"
 #endif
 
 class MessageLoopObserver : public MessageLoopForUI::Observer {
@@ -456,11 +457,13 @@ void ChromeBrowserMainPartsChromeos::PostBrowserStart() {
   }
 
 #if defined(USE_AURA)
-  // This is dependent on the ash::Shell singleton already having been
+  // These are dependent on the ash::Shell singleton already having been
   // initialized.
   power_button_observer_.reset(new chromeos::PowerButtonObserver);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(
       power_button_observer_.get());
+
+  video_property_writer_.reset(new chromeos::VideoPropertyWriter);
 #endif
 
   ChromeBrowserMainPartsLinux::PostBrowserStart();
