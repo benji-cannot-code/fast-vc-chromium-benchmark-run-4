ba4a17f246cab8fc2086e92886ab4373e3c4eac6
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-18 02:43:13 +0000

    Fix media handling in compositing tests after r105219.
    Reviewed by James Robinson.
    
    Seeking seems to have issues on Chromium, and cause these tests to
    timeout, so change the tests to play, then seek back to time 0.
    
    New image results now show the first frame of the movie.
    
    * compositing/resources/media-testing.js:
    (ensureFrameVisible):
    * platform/mac/compositing/geometry/video-fixed-scrolling-expected.png:
    * platform/mac/compositing/geometry/video-opacity-overlay-expected.png:
    * platform/mac/compositing/layers-inside-overflow-scroll-expected.png:
    * platform/mac/compositing/reflections/load-video-in-reflection-expected.png:
    * platform/mac/compositing/self-painting-layers-expected.png:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/compositing/resources/media-testing.js
+++ third_party/WebKit/LayoutTests/compositing/resources/media-testing.js
@@ -3,19 +3,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if (window.layoutTestController)
     layoutTestController.waitUntilDone();
 
-function ensureFrameVisible(video, videoRenderedCallbac)
+function ensureFrameVisible(video, videoRenderedCallback)
 {
-    video.addEventListener('seeked', videoRenderedCallbac, false);
-    video.currentTime = 0.5;
+    video.addEventListener('timeupdate', function() {
+        video.pause();
+        video.currentTime = 0;
+        if (videoRenderedCallback)
+            videoRenderedCallback();
+    }, false);
+    
+    video.play();
 }
 
-function setupVideo(videoElement, videoPath, canPlayThroughCallback, videoRenderedCallbac)
+function setupVideo(videoElement, videoPath, canPlayThroughCallback, videoRenderedCallback)
 {
     var mediaFile = findMediaFile("video", videoPath);
     videoElement.addEventListener("canplaythrough", function () {
         if (canPlayThroughCallback)
             canPlayThroughCallback();
-        ensureFrameVisible(this, videoRenderedCallbac);
+        ensureFrameVisible(this, videoRenderedCallback);
     }, false);
     videoElement.src = mediaFile;
 }
