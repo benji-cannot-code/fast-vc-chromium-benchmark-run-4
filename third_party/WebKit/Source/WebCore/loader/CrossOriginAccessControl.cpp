a2f06f2fec694d037a373683915104121d5ac73f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-18 06:06:09 +0000

    https://bugs.webkit.org/show_bug.cgi?id=75089 Access-Control-Request-Headers value should be lowercase
    Source/WebCore:
    
    Access-Control-Request-Headers is used when issuing a preflight request to let the server know
    the HTTP headers that will be used when the actual request is made.
    As per the W3C specification, Access-Control-Request-Headers value should be set in lowercase.
    
    Patch by Joe Thomas <joethomas@motorola.com> on 2012-01-17
    Reviewed by Alexey Proskuryakov.
    
    Test: http/tests/xmlhttprequest/access-control-preflight-request-header-lowercase.html
    
    * loader/CrossOriginAccessControl.cpp:
    (WebCore::createAccessControlPreflightRequest): setting Access-Control-Request-Headers value to lowercase
    
    LayoutTests:
    
    Added test to verify that the Access-Control-Request-Headers value is sent in lowercase.
    
    Patch by Joe Thomas <joethomas@motorola.com> on 2012-01-17
    Reviewed by Alexey Proskuryakov.
    
    * http/tests/xmlhttprequest/access-control-preflight-request-header-lowercase-expected.txt: Added.
    * http/tests/xmlhttprequest/access-control-preflight-request-header-lowercase.html: Added.
    * http/tests/xmlhttprequest/resources/access-control-preflight-request-header-lowercase.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/CrossOriginAccessControl.cpp
+++ third_party/WebKit/Source/WebCore/loader/CrossOriginAccessControl.cpp
@@ -127,7 +127,7 @@ ResourceRequest createAccessControlPreflightRequest(const ResourceRequest& reque
             headerBuffer.append(it->first);
         }
 
-        preflightRequest.setHTTPHeaderField("Access-Control-Request-Headers", headerBuffer.toString());
+        preflightRequest.setHTTPHeaderField("Access-Control-Request-Headers", headerBuffer.toString().lower());
     }
 
     return preflightRequest;
