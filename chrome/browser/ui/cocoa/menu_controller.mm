796828ba501fa861edbb3e1617af83b46bce0e47
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 20:59:47 +0000

    [Mac] Support command-clicking in context menus.
    BUG=110473
    TEST=Right click on some text on the web. Command-click the "Search Google for ..." option in the menu. The Google search tab should open in the background.
    
    Review URL: http://codereview.chromium.org/9159015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/menu_controller.mm
+++ chrome/browser/ui/cocoa/menu_controller.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/sys_string_conversions.h"
+#import "chrome/browser/ui/cocoa/event_utils.h"
 #include "skia/ext/skia_utils_mac.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/accelerators/accelerator_cocoa.h"
@@ -180,8 +181,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       static_cast<ui::MenuModel*>(
           [[sender representedObject] pointerValue]);
   DCHECK(model);
-  if (model)
-    model->ActivatedAt(modelIndex);
+  if (model) {
+    int event_flags = event_utils::EventFlagsFromNSEvent([NSApp currentEvent]);
+    model->ActivatedAt(modelIndex, event_flags);
+  }
 }
 
 - (NSMenu*)menu {
