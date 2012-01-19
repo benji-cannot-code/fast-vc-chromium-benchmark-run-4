692a4e5031b9691de30575c725468523dab40425
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 21:43:19 +0000

    views: Make sure the native-wrapper for the combobox inherits the enabled-ness of the parent.
    BUG=110683
    TEST=views_unittests:NativeComboboxViewsTest.DisabilityTest
    
    Review URL: https://chromiumcodereview.appspot.com/9159021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118365 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -133,6 +133,7 @@ void Combobox::ViewHierarchyChanged(bool is_add, View* parent, View* child) {
     // The native wrapper's lifetime will be managed by the view hierarchy after
     // we call AddChildView.
     native_wrapper_ = NativeComboboxWrapper::CreateWrapper(this);
+    native_wrapper_->UpdateEnabled();
     AddChildView(native_wrapper_->GetView());
   }
 }
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -584,7 +584,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             '../aura/aura.gyp:test_support_aura',
           ],
           'sources/': [
-            ['exclude', 'controls/combobox/native_combobox_views_unittest.cc'],
             ['exclude', 'controls/tabbed_pane/tabbed_pane_unittest.cc'],
             ['exclude', 'controls/table/table_view_unittest.cc'],
             ['exclude', 'widget/native_widget_win_unittest.cc'],
