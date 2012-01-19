3155e124eb4296846b79fbe18caa71d99e6bb068
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 05:24:53 +0000

    Output generated headers using Python's "wb" mode, to prevent EOL-munging.
    This results in PPAPI headers always being generated with Unix line-endings.
    
    BUG=109116
    TEST=Re-generate PPAPI headers on a Windows system, using a Windows-native version of Python, and verify that the generated headers have Unix line-endings.
    
    
    Review URL: http://codereview.chromium.org/9088010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118245 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_outfile.py
+++ ppapi/generators/idl_outfile.py
@@ -82,7 +82,7 @@ class IDLOutFile(object):
     outtext = ''.join(self.outlist)
     if not self.always_write:
       if os.path.isfile(filename):
-        intext = open(filename, 'r').read()
+        intext = open(filename, 'rb').read()
       else:
         intext = ''
 
@@ -107,7 +107,7 @@ class IDLOutFile(object):
         os.makedirs(basepath)
 
       if not GetOption('test'):
-        outfile = open(filename, 'w')
+        outfile = open(filename, 'wb')
         outfile.write(outtext)
         InfoOut.Log('Output %s written.' % self.filename)
       return True
