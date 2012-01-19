a8ba80c489a42886c99631cedce041d0579cebd9
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 10:33:24 +0000

    Fix JavaBridgeDispatcherHost to use WebContents rather than TabContents
    BUG=110637
    
    Review URL: https://chromiumcodereview.appspot.com/9251022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118281 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/java/java_bridge_dispatcher_host_manager.h
+++ content/browser/renderer_host/java/java_bridge_dispatcher_host_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,11 +19,11 @@ class RenderViewHost;
 struct NPObject;
 
 // This class handles injecting Java objects into all of the RenderViews
-// associated with a TabContents. It manages a set of JavaBridgeDispatcherHost
+// associated with a WebContents. It manages a set of JavaBridgeDispatcherHost
 // objects, one per RenderViewHost.
 class JavaBridgeDispatcherHostManager : public content::WebContentsObserver {
  public:
-  JavaBridgeDispatcherHostManager(TabContents* tab_contents);
+  JavaBridgeDispatcherHostManager(content::WebContents* web_contents);
   virtual ~JavaBridgeDispatcherHostManager();
 
   // These methods add or remove the object to each JavaBridgeDispatcherHost.
@@ -36,7 +36,7 @@ class JavaBridgeDispatcherHostManager : public content::WebContentsObserver {
   virtual void RenderViewCreated(RenderViewHost* render_view_host) OVERRIDE;
   virtual void RenderViewDeleted(RenderViewHost* render_view_host) OVERRIDE;
   virtual void WebContentsDestroyed(
-      content::WebContents* tab_contents) OVERRIDE;
+      content::WebContents* web_contents) OVERRIDE;
 
  private:
   typedef std::map<RenderViewHost*, scoped_refptr<JavaBridgeDispatcherHost> >
