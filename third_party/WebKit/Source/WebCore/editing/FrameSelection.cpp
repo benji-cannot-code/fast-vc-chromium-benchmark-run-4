85b2004f883a822b2ba0285621fc8305649a5328
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-19 21:41:08 +0000

    Crash in CompositeEditCommand::ensureComposition https://bugs.webkit.org/show_bug.cgi?id=76207
    Reviewed by Chang Shu.
    
    Source/WebCore: 
    
    The crash was caused by TypingCommand not kept alive when new editing commands are executed
    during adding more typings to the open last typing command since m_lastEditCommand is replaced
    by the new command. Fixed the bug by keeping them alive a little longer with RefPtr.
    
    Test: editing/execCommand/editing-command-while-executing-typing-command-crash.html
    
    * editing/FrameSelection.cpp:
    (WebCore::shouldStopBlinkingDueToTypingCommand):
    (WebCore::FrameSelection::updateAppearance):
    * editing/TypingCommand.cpp:
    (WebCore::TypingCommand::deleteSelection):
    (WebCore::TypingCommand::deleteKeyPressed):
    (WebCore::TypingCommand::forwardDeleteKeyPressed):
    (WebCore::TypingCommand::insertText):
    (WebCore::TypingCommand::insertLineBreak):
    (WebCore::TypingCommand::insertParagraphSeparatorInQuotedContent):
    (WebCore::TypingCommand::insertParagraphSeparator):
    (WebCore::TypingCommand::lastTypingCommandIfStillOpenForTyping):
    (WebCore::TypingCommand::closeTyping):
    * editing/TypingCommand.h:
    
    LayoutTests: 
    
    Add a regression test.
    
    * editing/execCommand/editing-command-while-executing-typing-command-crash-expected.txt: Added.
    * editing/execCommand/editing-command-while-executing-typing-command-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
@@ -1655,6 +1655,11 @@ bool FrameSelection::isFocusedAndActive() const
     return m_focused && m_frame->page() && m_frame->page()->focusController()->isActive();
 }
 
+inline static bool shouldStopBlinkingDueToTypingCommand(Frame* frame)
+{
+    return frame->editor()->lastEditCommand() && frame->editor()->lastEditCommand()->shouldStopCaretBlinking();
+}
+
 void FrameSelection::updateAppearance()
 {
 #if ENABLE(TEXT_CARET)
@@ -1662,13 +1667,10 @@ void FrameSelection::updateAppearance()
 
     bool caretBrowsing = m_frame->settings() && m_frame->settings()->caretBrowsingEnabled();
     bool shouldBlink = caretIsVisible() && isCaret() && (isContentEditable() || caretBrowsing);
-    
-    CompositeEditCommand* lastEditCommand = m_frame ? m_frame->editor()->lastEditCommand() : 0;
-    bool shouldStopBlinkingDueToTypingCommand = lastEditCommand && lastEditCommand->shouldStopCaretBlinking();
 
     // If the caret moved, stop the blink timer so we can restart with a
     // black caret in the new location.
-    if (caretRectChanged || !shouldBlink || shouldStopBlinkingDueToTypingCommand)
+    if (caretRectChanged || !shouldBlink || shouldStopBlinkingDueToTypingCommand(m_frame))
         m_caretBlinkTimer.stop();
 
     // Start blinking with a black caret. Be sure not to restart if we're
