d155d32ddad2634ec32e6a962d10d47093941e95
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 05:29:21 +0000

    Fix a crash in WebURLLoaderMockFactory caused by removing a loader that has been canceled. BUG=NONE TEST=webkit_unit_tests AssociatedURLLoaderTest
    Review URL: http://codereview.chromium.org/9259004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118250 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/weburl_loader_mock_factory.cc
+++ webkit/support/weburl_loader_mock_factory.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -82,7 +82,8 @@ void WebURLLoaderMockFactory::ServeAsynchronousRequests() {
     }
     if (!loader->isDeferred())
       loader->ServeAsynchronousRequest(response, data, error);
-    pending_loaders_.erase(iter);
+    // If the load has been canceled, the loader may not be in the map.
+    pending_loaders_.erase(loader);
   }
 }
 
