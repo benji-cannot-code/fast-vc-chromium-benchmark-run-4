76e7dce677bf4601aa39cf6e28d5f076ed9aa476
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 17:11:01 +0000

    Improve inline matching against standard prefixes.
    The HQP was not properly matching partial URLs typed into the omnibox against the standard ftp://, http:// or https:// prefixes.
    
    BUG=110547
    TEST=Added a prefix unit test. Manual testing should include visiting sites with prefixes of "ftp://ftp.", "ftp://www.", "ftp://", "https://www.", "https://", "http://www.", and "http://" and then typing only the TLD into the omnibox to insure the visited sites are presented as inline autocompletions.
    Review URL: https://chromiumcodereview.appspot.com/9254002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_url_index_types.cc
+++ chrome/browser/history/in_memory_url_index_types.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <iterator>
+#include <set>
 
 #include "base/i18n/break_iterator.h"
 #include "base/i18n/case_conversion.h"
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 
 namespace history {
 
@@ -130,6 +132,20 @@ Char16Set Char16SetFromString16(const string16& term) {
   for (string16::const_iterator iter = term.begin(); iter != term.end(); ++iter)
     characters.insert(*iter);
   return characters;
+}
+
+bool IsInlineablePrefix(const string16& prefix) {
+  CR_DEFINE_STATIC_LOCAL(std::set<string16>, prefixes, ());
+  if (prefixes.empty()) {
+    prefixes.insert(ASCIIToUTF16("ftp://ftp."));
+    prefixes.insert(ASCIIToUTF16("ftp://www."));
+    prefixes.insert(ASCIIToUTF16("ftp://"));
+    prefixes.insert(ASCIIToUTF16("https://www."));
+    prefixes.insert(ASCIIToUTF16("https://"));
+    prefixes.insert(ASCIIToUTF16("http://www."));
+    prefixes.insert(ASCIIToUTF16("http://"));
+  }
+  return prefixes.count(prefix) != 0;
 }
 
 }  // namespace history
--- chrome/browser/history/in_memory_url_index_types.h
+++ chrome/browser/history/in_memory_url_index_types.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -82,7 +82,7 @@ typedef std::set<string16> String16Set;
 typedef std::set<char16> Char16Set;
 typedef std::vector<char16> Char16Vector;
 
-// Utility Functions Relates to Convenience Types ------------------------------
+// Utility Functions -----------------------------------------------------------
 
 // Breaks a string down into individual words.
 String16Set String16SetFromString16(const string16& uni_string);
@@ -114,6 +114,9 @@ String16Vector String16VectorFromString16(const string16& uni_string,
 // by score. Also, provide the metrics for where the matches occur so that
 // the UI can highlight the matched sections.
 Char16Set Char16SetFromString16(const string16& uni_word);
+
+// Determine if |prefix| is any of the standard 'ftp' or 'http[s]' prefixes.
+bool IsInlineablePrefix(const string16& prefix);
 
 // Support for InMemoryURLIndex Private Data -----------------------------------
 
