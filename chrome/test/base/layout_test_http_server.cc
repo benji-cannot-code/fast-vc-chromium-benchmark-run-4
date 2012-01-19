36d0c6e4c93e6765fe7031d7c7283406b3ec5eec
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 20:02:43 +0000

    Make layout_test_http_server.cc use PathService::Get(chrome::DIR_LAYOUT_TESTS) instead of hardcoding chrome/test/data/layout_tests/LayoutTests.
    The http layout tests fail on my machine (new git workflow) without this.
    
    BUG=105104
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9252038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118339 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/layout_test_http_server.cc
+++ chrome/test/base/layout_test_http_server.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/process_util.h"
 #include "base/string_number_conversions.h"
+#include "chrome/common/chrome_paths.h"
 
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
@@ -65,13 +66,8 @@ bool LayoutTestHttpServer::Start() {
   cmd_line.AppendArg("--port=" + base::IntToString(port_));
 
   FilePath layout_tests_dir;
-  if (!PathService::Get(base::DIR_SOURCE_ROOT, &layout_tests_dir))
+  if (!PathService::Get(chrome::DIR_LAYOUT_TESTS, &layout_tests_dir))
     return false;
-  layout_tests_dir = layout_tests_dir.AppendASCII("chrome")
-                                     .AppendASCII("test")
-                                     .AppendASCII("data")
-                                     .AppendASCII("layout_tests")
-                                     .AppendASCII("LayoutTests");
   cmd_line.AppendArgNative(FILE_PATH_LITERAL("--layout_tests_dir=") +
                            layout_tests_dir.value());
 
