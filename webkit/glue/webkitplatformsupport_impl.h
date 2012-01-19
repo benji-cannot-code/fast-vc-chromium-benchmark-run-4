d254a401df59bcba144afb490447895f04fbe46a
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 05:29:07 +0000

    Add overloaded WebKitPlatformSupportImpl::createGraphicsContext3D
    BUG=99516
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9249048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118249 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitplatformsupport_impl.h
+++ webkit/glue/webkitplatformsupport_impl.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/platform_file.h"
 #include "base/threading/thread_local_storage.h"
 #include "base/timer.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h"
 #include "webkit/glue/resource_loader_bridge.h"
 #include "webkit/glue/webkit_glue_export.h"
@@ -98,6 +99,11 @@ class WEBKIT_GLUE_EXPORT WebKitPlatformSupportImpl :
   virtual void callOnMainThread(void (*func)(void*), void* context);
   virtual WebKit::WebThread* createThread(const char* name);
   virtual WebKit::WebThread* currentThread();
+  virtual WebKit::WebGraphicsContext3D* createGraphicsContext3D();
+  virtual WebKit::WebGraphicsContext3D* createGraphicsContext3D(
+      WebKit::WebGraphicsContext3D::Attributes attributes,
+      WebKit::WebView* web_view,
+      bool render_to_web_view);
 
 
   // Embedder functions. The following are not implemented by the glue layer and
