777da609645656dd9a86ba9b7520bf21d6feff2c
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 17:29:42 +0000

    Consider any type of browser as the current window in extension APIs.
    This fixes a regression so that extension APIs consider any type of browser (rather than only tabbed ones) when searching for the current window.
    
    BUG=53077
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9251018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118317 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/tabs/current_window/background.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function getSelectedPopup() {
+    chrome.windows.create(
+        {type: 'popup', url: 'about:blank', focused: true},
+        chrome.test.callbackPass(function(win) {
+      chrome.tabs.query(
+          {active: true, windowId: chrome.windows.WINDOW_ID_CURRENT},
+          chrome.test.callbackPass(function(tabs) {
+        chrome.test.assertEq(1, tabs.length);
+        chrome.test.assertTrue(tabs[0].windowId == win.id);
+      }));
+    }));
+  }
+]);
