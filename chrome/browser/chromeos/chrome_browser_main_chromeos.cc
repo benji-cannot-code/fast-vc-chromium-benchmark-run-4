f30ca7ca5fe4e8c124add2078fe2a482a1e174ec
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 22:13:38 +0000

    Reland "aura/chromeos: Avoid suspending while video is playing."
    This reverts r118199, which was a revert of r118171.
    aura::VideoPropertyWriter (not ash::VideoDetector -- made a
    typo in the revert's description) was outliving the
    ash::Shell singleton.  Now we explicitly destroy the
    VideoPropertyWriter before the shell.
    
    The only differences from r118171 are the addition of the
    final chunk in chrome_browser_main_chromeos.cc and a change
    to reparent a window in VideoDetectorTest.WindowNotVisible
    needed in response to r118259.
    
    BUG=110114
    TEST=avoided missing DCHECK on shutdown in trybot logs this time
    
    Review URL: http://codereview.chromium.org/9254018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/legacy_window_manager/initial_browser_window_observer.h"
 #include "chrome/browser/chromeos/power/power_button_controller_delegate_chromeos.h"
 #include "chrome/browser/chromeos/power/power_button_observer.h"
+#include "chrome/browser/chromeos/power/video_property_writer.h"
 #endif
 
 class MessageLoopObserver : public MessageLoopForUI::Observer {
@@ -458,11 +459,13 @@ void ChromeBrowserMainPartsChromeos::PostBrowserStart() {
   }
 
 #if defined(USE_AURA)
-  // This is dependent on the ash::Shell singleton already having been
+  // These are dependent on the ash::Shell singleton already having been
   // initialized.
   power_button_observer_.reset(new chromeos::PowerButtonObserver);
   chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(
       power_button_observer_.get());
+
+  video_property_writer_.reset(new chromeos::VideoPropertyWriter);
 #endif
 
   ChromeBrowserMainPartsLinux::PostBrowserStart();
@@ -513,6 +516,12 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   chromeos::AudioHandler::Shutdown();
 
   chromeos::WebSocketProxyController::Shutdown();
+
+#if defined(USE_AURA)
+  // Let VideoPropertyWriter unregister itself as an observer of the ash::Shell
+  // singleton before the shell is destroyed.
+  video_property_writer_.reset();
+#endif
 
   ChromeBrowserMainPartsLinux::PostMainMessageLoopRun();
 }
