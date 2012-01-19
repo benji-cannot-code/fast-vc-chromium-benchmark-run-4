a4a7155ba5c77faa4586f1e3f06d24b6b97240fa
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 17:23:10 +0000

    Add a startup check to create profile shortcuts for legacy Chrome profiles.
    We do this by adding a per-profile pref that tracks whether or not a shortcut has been created for it. We check that flag at Profile startup and create a new shortcut if needed (while setting the flag to avoid creating future shortcuts).
    
    BUG=109447
    TEST=Start Chrome in v16 or v17 and create a new profile. Update Chrome to the latest version (including this patch) and ensure that starting a window with one of the two original profiles creates a desktop shortcut with that Profile's data. Ensure that if we then delete the shortcut for the profile, but start a new window for the profile, another shortcut is not created.
    
    Review URL: http://codereview.chromium.org/9253016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118316 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_shortcut_manager_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_win.cc
@@ -214,7 +214,7 @@ ProfileShortcutManagerWin::ProfileShortcutManagerWin() {
 ProfileShortcutManagerWin::~ProfileShortcutManagerWin() {
 }
 
-void ProfileShortcutManagerWin::OnProfileAdded(
+void ProfileShortcutManagerWin::AddProfileShortcut(
     const FilePath& profile_path) {
   ProfileInfoCache& cache =
       g_browser_process->profile_manager()->GetProfileInfoCache();
@@ -285,6 +285,10 @@ void ProfileShortcutManagerWin::OnProfileAdded(
                    string16(), string16(), FilePath(),
                    static_cast<gfx::Image*>(NULL), true));
   }
+}
+
+void ProfileShortcutManagerWin::OnProfileAdded(
+    const FilePath& profile_path) {
 }
 
 void ProfileShortcutManagerWin::OnProfileWillBeRemoved(
--- chrome/browser/profiles/profile_shortcut_manager_win.h
+++ chrome/browser/profiles/profile_shortcut_manager_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,6 +18,11 @@ class ProfileShortcutManagerWin : public ProfileInfoCacheObserver {
  public:
   ProfileShortcutManagerWin();
   virtual ~ProfileShortcutManagerWin();
+
+  // Create a profile shortcut for the profile with path |profile_path|, plus
+  // update the original profile shortcut if |profile_path| is the second
+  // profile created.
+  virtual void AddProfileShortcut(const FilePath& profile_path);
 
   // ProfileInfoCacheObserver:
   virtual void OnProfileAdded(const FilePath& profile_path) OVERRIDE;
