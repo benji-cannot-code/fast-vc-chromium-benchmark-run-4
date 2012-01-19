a81616e2f3bf84843d55da86f52ec8578faa0317
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 02:02:31 +0000

    Revert 118198 - [Sync] Cleanup Sessions code and make tab syncability stricter.
    We now only consider a tab syncable if it has at least one valid entry, where
    valid is true iff the url is valid and the scheme is neither chrome or file.
    This avoids syncing tabs with nothing but chrome:// or file:// navigations.
    
    BUG=109301
    TEST=unit_tests
    
    
    Review URL: http://codereview.chromium.org/9114015
    
    TBR=zea@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118212 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,8 +29,8 @@ class TwoClientSessionsSyncTest : public SyncTest {
 };
 
 static const char* kValidPassphrase = "passphrase!";
-static const char* kURL1 = "http://127.0.0.1/bubba1";
-static const char* kURL2 = "http://127.0.0.1/bubba2";
+static const char* kURL1 = "chrome://sync";
+static const char* kURL2 = "chrome://version";
 
 // TODO(zea): Test each individual session command we care about separately.
 // (as well as multi-window). We're currently only checking basic single-window/
