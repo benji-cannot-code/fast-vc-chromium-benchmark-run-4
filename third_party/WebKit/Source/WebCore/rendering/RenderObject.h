2472071ff2e62e2c1b45eee286ed17c3b131b3c3
mihnea@adobe.com <mihnea@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-19 18:55:20 +0000

    [CSSRegions]Add support for background-color in region styling https://bugs.webkit.org/show_bug.cgi?id=71488
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    This patch enables region styling again. The region styling tests were also added back.
    With the improvements from https://bugs.webkit.org/show_bug.cgi?id=76265, hopefully we will not see the same 3% regressions
    in performance.
    
    * rendering/RenderObject.h:
    (WebCore::RenderObject::style):
    
    LayoutTests:
    
    Add back the region styling tests.
    
    * platform/mac-snowleopard/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.h
@@ -1111,7 +1111,9 @@ inline bool RenderObject::preservesNewline() const
 
 inline RenderStyle* RenderObject::style() const
 {
-    return m_style.get();
+    if (!inRenderFlowThread())
+        return m_style.get();
+    return styleInRegion();
 }
 
 inline void makeMatrixRenderable(TransformationMatrix& matrix, bool has3DRendering)
