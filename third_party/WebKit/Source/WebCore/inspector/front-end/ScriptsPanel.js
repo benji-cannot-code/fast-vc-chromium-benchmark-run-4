e1e5b6c3bb898cf236f04a6008ea65dc2ea76ad7
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-19 19:15:58 +0000

    Unreviewed, inspector closure compilation fix.
    * inspector/front-end/ScriptsPanel.js:
    * inspector/front-end/TabbedEditorContainer.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105430 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -1089,7 +1089,7 @@ WebInspector.ScriptsPanel.FileSelector.prototype = {
 WebInspector.ScriptsPanel.EditorContainer = function() { }
 
 WebInspector.ScriptsPanel.EditorContainer.Events = {
-    FileSelected: "FileSelected"
+    EditorSelected: "EditorSelected"
 }
 
 WebInspector.ScriptsPanel.EditorContainer.prototype = {
