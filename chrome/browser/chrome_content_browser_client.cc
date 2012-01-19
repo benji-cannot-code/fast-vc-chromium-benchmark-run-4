9367631af256d5c1272e963fff3e86046d81b0e2
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 18:56:31 +0000

    Print Preview: Don't copy a renderer specific command line switch from the browser's command line.
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9150042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118328 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -669,7 +669,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kProfilingAtStart,
       switches::kProfilingFile,
       switches::kProfilingFlush,
-      switches::kRendererPrintPreview,
       switches::kSilentDumpOnDCHECK,
     };
 
