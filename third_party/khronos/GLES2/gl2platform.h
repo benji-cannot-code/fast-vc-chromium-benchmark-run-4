23ac5633f486e504c161e114fdb4c6bc6a409268
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 06:35:21 +0000

    Revert r118525 / Re-land r118240 - the build failure was a flake.
    This change re-enables building gles2_c_lib as a component.
    
    TBR=gman@chromium.org
    BUG=107735
    TEST=waterfall stays green
    
    Review URL: https://chromiumcodereview.appspot.com/9150022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118263 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/khronos/GLES2/gl2platform.h
+++ third_party/khronos/GLES2/gl2platform.h
@@ -20,8 +20,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <KHR/khrplatform.h>
 
-#ifndef GL_APICALL
-#define GL_APICALL  KHRONOS_APICALL
+#if defined(COMPONENT_BUILD)
+#if defined(WIN32)
+#if defined(GLES2_C_LIB_IMPLEMENTATION)
+#define GL_APICALL __declspec(dllexport)
+#else
+#define GL_APICALL __declspec(dllimport)
+#endif  /* defined(GLES2_C_LIB_IMPLEMENTATION) */
+#else /* defined(WIN32) */
+#define GL_APICALL __attribute__((visibility("default")))
+#endif
+#else
+#   define GL_APICALL
 #endif
 
 #ifndef GL_APIENTRY
