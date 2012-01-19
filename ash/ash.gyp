d664a731bb9029384d90c173dcf49ba1941fe139
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 06:23:04 +0000

    Add a simple animation when showing/hiding windows.
    http://crbug.com/110024
    TEST=none
    
    Review URL: http://codereview.chromium.org/9167032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118259 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash.gyp
+++ ash/ash.gyp
@@ -153,6 +153,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'wm/toplevel_layout_manager.h',
         'wm/toplevel_window_event_filter.cc',
         'wm/toplevel_window_event_filter.h',
+        'wm/window_animations.cc',
+        'wm/window_animations.h',        
         'wm/window_cycle_controller.cc',
         'wm/window_cycle_controller.h',
         'wm/window_cycle_list.cc',
--- ui/aura/window_observer.h
+++ ui/aura/window_observer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -47,7 +47,12 @@ class AURA_EXPORT WindowObserver {
   // has changed.
   virtual void OnWindowStackingChanged(Window* window) {}
 
-  // Invoked when the Window has been destroyed (i.e. from its destructor).
+  // Invoked when the Window is being destroyed (i.e. from the start of its
+  // destructor).
+  virtual void OnWindowDestroying(Window* window) {}
+
+  // Invoked when the Window has been destroyed (i.e. at the end of its
+  // destructor).
   virtual void OnWindowDestroyed(Window* window) {}
 
  protected:
