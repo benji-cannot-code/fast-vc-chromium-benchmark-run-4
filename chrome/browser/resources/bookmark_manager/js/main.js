90e878c34f3a2a1f3cd9cf2323109205854d6a6e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 06:26:37 +0000

    [i18n fixlet] Align the icons in the bookmark manager in RTL mode.
    BUG=none
    TEST=manual, folders and recent/search icons should line up vertically.
    Review URL: https://chromiumcodereview.appspot.com/9199003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/main.js
+++ chrome/browser/resources/bookmark_manager/js/main.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,6 +38,10 @@ chrome.experimental.bookmarkManager.getStrings(function(data) {
 
   recentTreeItem.label = localStrings.getString('recent');
   searchTreeItem.label = localStrings.getString('search');
+  if (!isRTL())
+    searchTreeItem.icon = 'images/bookmark_manager_search.png'
+  else
+    searchTreeItem.icon = 'images/bookmark_manager_search_rtl.png'
 });
 
 /**
@@ -59,7 +63,6 @@ splitter.addEventListener('resize', function(e) {
 BookmarkList.decorate(list);
 
 var searchTreeItem = new TreeItem({
-  icon: 'images/bookmark_manager_search.png',
   bookmarkId: 'q='
 });
 bmm.treeLookup[searchTreeItem.bookmarkId] = searchTreeItem;
