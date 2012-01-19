0a64fb9260b66bc8baf40d127d1e75e1af06d548
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 01:41:41 +0000

    Fix process_count test flakiness on win
    Ensure that all plugins are registered properly before attempting to load
    flash files.
    
    BUG=94123
    TEST=green pyauto win bots.
    
    R=dennisjeffrey@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9252036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/process_count.py
+++ chrome/test/functional/process_count.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -53,6 +53,12 @@ class ProcessCountTest(pyauto.PyUITest):
       self.chrome_proc_name = self.CHROME_PROCESS_NAME['linux']
 
     pyauto.PyUITest.setUp(self)
+
+    # Forcibly trigger all plugins to get registered.  crbug.com/94123
+    # Sometimes flash files loaded too quickly after firing browser
+    # ends up getting downloaded, which seems to indicate that the plugin
+    # hasn't been registered yet.
+    self.GetPluginsInfo()
 
   def _VerifyProcessCount(self, num_expected):
     """Verifies the number of Chrome-related processes is as expected.
