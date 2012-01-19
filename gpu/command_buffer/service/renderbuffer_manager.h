8d90a58966f3d1714f644066b65ce4d9e170e0db
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 05:20:56 +0000

    Fix RenderbufferManager to handle deleted objects
    TEST=unit tests
    BUG=110198
    
    R=apatrick@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9253027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118243 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/renderbuffer_manager.h
+++ gpu/command_buffer/service/renderbuffer_manager.h
@@ -24,8 +24,9 @@ class RenderbufferManager {
    public:
     typedef scoped_refptr<RenderbufferInfo> Ref;
 
-    explicit RenderbufferInfo(GLuint service_id)
-        : service_id_(service_id),
+    RenderbufferInfo(RenderbufferManager* manager, GLuint service_id)
+        : manager_(manager),
+          service_id_(service_id),
           cleared_(true),
           has_been_bound_(false),
           samples_(0),
@@ -76,7 +77,7 @@ class RenderbufferManager {
     friend class RenderbufferManager;
     friend class base::RefCounted<RenderbufferInfo>;
 
-    ~RenderbufferInfo() { }
+    ~RenderbufferInfo();
 
     void set_cleared() {
       cleared_ = true;
@@ -94,6 +95,9 @@ class RenderbufferManager {
     void MarkAsDeleted() {
       service_id_ = 0;
     }
+
+    // RenderbufferManager that owns this RenderbufferInfo.
+    RenderbufferManager* manager_;
 
     // Service side renderbuffer id.
     GLuint service_id_;
@@ -153,6 +157,8 @@ class RenderbufferManager {
 
  private:
   void UpdateMemRepresented();
+
+  void StopTracking(RenderbufferInfo* renderbuffer);
 
   GLint max_renderbuffer_size_;
   GLint max_samples_;
