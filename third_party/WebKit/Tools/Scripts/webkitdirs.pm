a89136666cd43af7e2057362f4083bc7cbd897bc
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-19 11:06:31 +0000

    Fix the Qt build
    Unreviewed build fix.
    
    * Scripts/webkitdirs.pm:
    (buildQMakeProjects):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105399 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -2172,7 +2172,7 @@ sub buildQMakeProjects
 
     chdir ".." or die;
 
-    unlink($pathToBuildHint) || die "Could not delete $pathToBuildHint: $!" if $result eq 0;
+    unlink($pathToBuildHint) || die "Could not delete $pathToBuildHint: $!" if -e $pathToBuildHint && $result eq 0;
 
     return $result;
 }
