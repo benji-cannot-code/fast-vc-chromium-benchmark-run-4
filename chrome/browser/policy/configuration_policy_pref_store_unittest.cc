469785c520f8ed435a1cbb92b9f5605243f73a29
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 13:26:24 +0000

    Print Preview: Add an enterprise policy to toggle print preview.
    BUG=102863
    TEST=none
    
    Review URL: http://codereview.chromium.org/9250024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118287 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store_unittest.cc
+++ chrome/browser/policy/configuration_policy_pref_store_unittest.cc
@@ -1,7 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+#include <string>
 
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
@@ -258,6 +260,8 @@ INSTANTIATE_TEST_CASE_P(
                     prefs::kImportSavedPasswords),
         TypeAndName(kPolicyEnableMemoryInfo,
                     prefs::kEnableMemoryInfo),
+        TypeAndName(kPolicyDisablePrintPreview,
+                    prefs::kPrintPreviewDisabled),
         TypeAndName(kPolicyDeveloperToolsDisabled,
                     prefs::kDevToolsDisabled)));
 
