ccd57c2cba92bc1f121680195dea69756d656d07
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-19 10:16:35 +0000

    drop event isn't fired for contentEditable in edit drag https://bugs.webkit.org/show_bug.cgi?id=57185
    Reviewed by Adam Barth.
    
    Source/WebCore: 
    
    Dispatch drop and dragend events after edit drag per HTML5 spec:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/dnd.html#drag-and-drop-processing-model
    
    There are two major differences between the spec and WebKit's new behavior:
    
    While the spec says we have to insert the dragged contents immediately after dispatching drop event
    and delete the source in the default event handler of dragend event, doing so in WebKit is extremely
    difficult because of the way we manage the selection. Instead, we continue to delete the source
    and insert the dragged contents immediately after the drop event; this behavior matches that of Firefox 9.
    
    When the dragged contents and the destination of the move is in the same text node, ReplaceSelectionCommand
    may end up replacing it with a new text node. But this removal causes a problem when EventHandler uses
    the node to dispatch dragend event because the node is "orphaned" from its parent at that point. To mitigate
    this issue, we update the dragState's m_dragSrc when the node is orphaned by the edit drag. While this behavior
    may differ from the spec and other browsers, not delivering dragend to the editing host seems strictly worse than
    dispatching it at the slightly wrong target.
    
    Tests: fast/events/moving-text-should-fire-drop-and-dragend-events-2.html
           fast/events/moving-text-should-fire-drop-and-dragend-events.html
    
    * page/DragController.cpp:
    (WebCore::DragController::performDrag): Dispatch drop event even when m_isHandlingDrag is true as long
    as DragDestinationActionDHTML is an acceptable action.
    (WebCore::DragController::concludeEditDrag): Call updateDragStateAfterEditDragIfNeeded after inserting
    the dragged contents. This is necessary when ReplaceSelectionCommand or MoveSelectionCommand modifies
    the source node while inserting the dragged contents.
    * page/EventHandler.cpp:
    (WebCore::EventHandler::performDragAndDrop): Clear the drag state only if drop event's default action
    was prevented so that we dispatch dragevent event later.
    (WebCore::EventHandler::updateDragStateAfterEditDragIfNeeded): Update dragState's m_dragSrc when the node
    is orphaned. See above for the rationale.
    * page/EventHandler.h:
    
    LayoutTests: 
    
    Added tests ensure moving text in contenteditable regions fire dragstart, drop, and dragend events.
    
    * fast/events/moving-text-should-fire-drop-and-dragend-events-2-expected.txt: Added.
    * fast/events/moving-text-should-fire-drop-and-dragend-events-2.html: Added.
    * fast/events/moving-text-should-fire-drop-and-dragend-events-expected.txt: Added.
    * fast/events/moving-text-should-fire-drop-and-dragend-events.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -1912,15 +1912,18 @@ void EventHandler::cancelDragAndDrop(const PlatformMouseEvent& event, Clipboard*
     clearDragState();
 }
 
-void EventHandler::performDragAndDrop(const PlatformMouseEvent& event, Clipboard* clipboard)
+bool EventHandler::performDragAndDrop(const PlatformMouseEvent& event, Clipboard* clipboard)
 {
     Frame* targetFrame;
+    bool preventedDefault = false;
     if (targetIsFrame(m_dragTarget.get(), targetFrame)) {
         if (targetFrame)
-            targetFrame->eventHandler()->performDragAndDrop(event, clipboard);
+            preventedDefault = targetFrame->eventHandler()->performDragAndDrop(event, clipboard);
     } else if (m_dragTarget.get())
-        dispatchDragEvent(eventNames().dropEvent, m_dragTarget.get(), event, clipboard);
-    clearDragState();
+        preventedDefault = dispatchDragEvent(eventNames().dropEvent, m_dragTarget.get(), event, clipboard);
+    if (preventedDefault)
+        clearDragState();
+    return preventedDefault;
 }
 
 void EventHandler::clearDragState()
@@ -2784,7 +2787,14 @@ void EventHandler::dragSourceEndedAt(const PlatformMouseEvent& event, DragOperat
     // that consecutive mousemove events don't reinitiate the drag and drop.
     m_mouseDownMayStartDrag = false;
 }
-    
+
+void EventHandler::updateDragStateAfterEditDragIfNeeded(Element* rootEditableElement)
+{
+    // If inserting the dragged contents removed the drag source, we still want to fire dragend at the root editble element.
+    if (dragState().m_dragSrc && !dragState().m_dragSrc->inDocument())
+        dragState().m_dragSrc = rootEditableElement;
+}
+
 // returns if we should continue "default processing", i.e., whether eventhandler canceled
 bool EventHandler::dispatchDragSrcEvent(const AtomicString& eventType, const PlatformMouseEvent& event)
 {
