d659f801369c1e2926bc905ae30ae9fcf0782289
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 16:24:29 +0000

    [Qt] Trigger forcing accelerated compositing from the UI process side. https://bugs.webkit.org/show_bug.cgi?id=76296
    Reviewed by Noam Rosenthal.
    
    Some messages can be sent from the web view to the LayerTreeHost before
    accelerated compositing is entered on the web process and signaled back.
    By letting the UI process decide if AC has to be forced, we can create
    the LayerTreeHostProxy earlier to send messages to the web process while
    AC is being entered there.
    
    This patch also fixes the flow of DidRenderFrame and RenderNextFrame messages
    by setting the m_waitingForUIProcess flag properly and only send the
    RenderNextFrame once the painting thread on the UI process is ready to paint.
    
    This fixes the first visible content rect message not being received when
    loading pages from the disk.
    
    * Shared/WebPreferencesStore.h:
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::initialize):
    * UIProcess/DrawingAreaProxyImpl.cpp:
    (WebKit::DrawingAreaProxyImpl::DrawingAreaProxyImpl):
    * UIProcess/qt/LayerTreeHostProxyQt.cpp:
    (WebKit::LayerTreeHostProxy::flushLayerChanges):
    (WebKit::LayerTreeHostProxy::didRenderFrame):
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit::DrawingAreaImpl::DrawingAreaImpl):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences):
    * WebProcess/WebPage/qt/LayerTreeHostQt.cpp:
    (WebKit::LayerTreeHostQt::performScheduledLayerFlush):
    * WebProcess/WebPage/qt/WebPageQt.cpp:
    (WebKit::WebPage::platformInitialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/LayerTreeHostQt.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/LayerTreeHostQt.cpp
@@ -241,15 +241,15 @@ void LayerTreeHostQt::didDeleteLayer(WebLayerID id)
 
 void LayerTreeHostQt::performScheduledLayerFlush()
 {
-    m_webPage->layoutIfNeeded();
-
-    if (!m_isValid)
-        return;
-
 #if USE(TILED_BACKING_STORE)
     if (m_isSuspended || m_waitingForUIProcess)
         return;
 #endif
+
+    m_webPage->layoutIfNeeded();
+
+    if (!m_isValid)
+        return;
 
     m_shouldSyncFrame = false;
     flushPendingLayerChanges();
@@ -262,6 +262,7 @@ void LayerTreeHostQt::performScheduledLayerFlush()
     }
 
     m_webPage->send(Messages::LayerTreeHostProxy::DidRenderFrame());
+    m_waitingForUIProcess = true;
 
     if (!m_notifyAfterScheduledLayerFlush)
         return;
