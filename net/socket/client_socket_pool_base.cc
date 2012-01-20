9b03a256a0c4694a86986b982a21cb51c47215b6
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 21:49:14 +0000

    Revert 118074 - Disable FPO throughout client_socket_pool_base.cc.
    This will help track down the origin of a use-after-free bug.
    
    BUG=109876
    
    
    Review URL: http://codereview.chromium.org/9240036
    
    TBR=eroman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9271013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118510 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_pool_base.cc
+++ net/socket/client_socket_pool_base.cc
@@ -23,14 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::TimeDelta;
 
-// Disable FPO for all the functions in this file. This is to help track down
-// http://crbug.com/109876. With FPO enabled, the stacktrace capture is
-// unreliable, and we can't see the specific deletion point of the Group.
-// TODO(eroman): Remove it when done investigating.
-#if defined(COMPILER_MSVC)
-#pragma optimize("y", off)
-#endif
-
 namespace {
 
 // Indicate whether we should enable idle socket cleanup timer. When timer is
@@ -1264,8 +1256,3 @@ void ClientSocketPoolBaseHelper::Group::RemoveAllJobs() {
 }  // namespace internal
 
 }  // namespace net
-
-// Restore default optimization settings.
-#if defined(COMPILER_MSVC)
-#pragma optimize("", on)
-#endif
