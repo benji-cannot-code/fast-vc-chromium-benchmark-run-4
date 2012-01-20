d164d95ea5a2a973d674376489d68ea0ebde7573
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 03:22:39 +0000

    Replace improper #import with an #include.
    * UIProcess/Plugins/PluginProcessProxy.cpp:
    Fixes warnings.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginProcessProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Plugins/PluginProcessProxy.cpp
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebProcessMessages.h"
 #include "WebProcessProxy.h"
 #include <WebCore/NotImplemented.h>
-#import <WebCore/RunLoop.h>
+#include <WebCore/RunLoop.h>
 
 #if PLATFORM(MAC)
 #include "MachPort.h"
