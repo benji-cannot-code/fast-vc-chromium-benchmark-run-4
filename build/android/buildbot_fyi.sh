1a70bedfa6a0a2bf5574e63b2604e858a60c3cf9
jrg@google.com <jrg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 19:59:10 +0000

    Android buildbot refactor.
    Add distinct entry points for each type of chromium android buildbot.
    Make it super-ez for a sheriff to disable (force green) any of them.
    
    BUG=None
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9249030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118471 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/buildbot_fyi.sh
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash -ex
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Buildbot annotator script for the FYI waterfall.  Compile,
+# experimental compile, run tests, ...
+
+# SHERIFF: there should be no need to disable this bot.
+# The FYI waterfall does not close the tree.
+
+ROOT=$(cd "$(dirname $0)"; pwd)
+. "${ROOT}"/buildbot_functions.sh
+
+bb_baseline_setup "${ROOT}"/../..
+bb_compile
+bb_compile_experimental
+bb_run_tests_emulator
+
+
