72b3029150c3e95805571f796e0a289e33a6f434
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 02:41:06 +0000

    Regression (r98735): Video chat moles in Gmail render incorrectly on Mac OS https://bugs.webkit.org/show_bug.cgi?id=75682
    Source/WebCore:
    
    Reviewed by James Robinson.
    
    RenderLayerBacking::isSimpleContainerCompositingLayer() gave incorret
    results in the case where the layer itself was visibility:hidden, but
    where it had visible, non-composited descendant layers.
    
    Fix by breaking RenderLayerBacking::hasVisibleNonCompositingDescendants()
    into two methods, one that tests for renderers in this layer which
    render stuff (and are thus affected by visibility on this layer), and
    another which walks descendant, non-composited layers looking for those
    which are visible.
    
    Removed an early return in the "renderObject->node()->isDocumentNode()"
    clause, because we want to run the same code that we run for non-document
    nodes.
    
    Test: compositing/visibility/layer-visible-content.html
    
    * rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::isSimpleContainerCompositingLayer):
    (WebCore::RenderLayerBacking::containsNonEmptyRenderers):
    (WebCore::RenderLayerBacking::hasVisibleNonCompositingDescendantLayers):
    * rendering/RenderLayerBacking.h:
    
    LayoutTests:
    
    Reviewed by James Robinson.
    
    Testcase with a combination of visibility:hidden, overflow, positioning and
    transforms.
    
    * compositing/visibility/layer-visible-content-expected.png: Added.
    * compositing/visibility/layer-visible-content-expected.txt: Added.
    * compositing/visibility/layer-visible-content.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105471 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.h
@@ -198,7 +198,8 @@ private:
     Color rendererBackgroundColor() const;
     void updateBackgroundColor();
 
-    bool hasVisibleNonCompositingDescendants() const;
+    bool containsNonEmptyRenderers() const;
+    bool hasVisibleNonCompositingDescendantLayers() const;
     
     void paintIntoLayer(RenderLayer* rootLayer, GraphicsContext*, const LayoutRect& paintDirtyRect, PaintBehavior, GraphicsLayerPaintingPhase, RenderObject* paintingRoot);
 
