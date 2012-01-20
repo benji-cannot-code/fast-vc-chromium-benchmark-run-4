89f97aaa24d8421553814275d65624bf9cc97d76
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 16:19:34 +0000

    Fix the fix. Added missing return type.
    TBR=varunjain@chromium.org
    TEST=ChromiumOS clang bots build
    
    Review URL: https://chromiumcodereview.appspot.com/9233010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118450 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/textfield_controller.cc
+++ ui/views/controls/textfield/textfield_controller.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
-TextfieldController::IsCommandIdEnabled(int command_id) const {
+bool TextfieldController::IsCommandIdEnabled(int command_id) const {
   return false;
 }
 
