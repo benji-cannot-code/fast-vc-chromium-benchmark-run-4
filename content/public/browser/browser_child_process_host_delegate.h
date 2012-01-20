9d074024dcad749144bd40fc0267d330ec180727
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 06:55:23 +0000

    Try to fix Linux components build.
    TBR=erg
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118417 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/browser_child_process_host_delegate.h
+++ content/public/browser/browser_child_process_host_delegate.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 // Interface that all users of BrowserChildProcessHost need to provide.
-class BrowserChildProcessHostDelegate : public IPC::Channel::Listener {
+class CONTENT_EXPORT BrowserChildProcessHostDelegate
+    : public IPC::Channel::Listener {
  public:
   virtual ~BrowserChildProcessHostDelegate() {}
 
@@ -21,7 +22,7 @@ class BrowserChildProcessHostDelegate : public IPC::Channel::Listener {
   // the default return value). The exception is if the host is in the middle of
   // sending a request to the process, in which case the other side might think
   // it's ok to shutdown, when really it's not.
-  CONTENT_EXPORT virtual bool CanShutdown();
+  virtual bool CanShutdown();
 
   // Called when the process has been started.
   virtual void OnProcessLaunched() {}
