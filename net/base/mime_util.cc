15fe54abb6703ff771b7ae20c582a6df7117c413
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 03:31:07 +0000

    [uber page] make references to chrome_shared2.css relative
    otherwise open_sans.woff doesn't get loaded (not sure why)
    
    BUG=none
    TEST=open sans is used
    
    Review URL: https://chromiumcodereview.appspot.com/9254001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118402 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -113,6 +113,7 @@ static const MimeInfo secondary_mappings[] = {
   { "application/pdf", "pdf" },
   { "application/postscript", "ps,eps,ai" },
   { "application/x-javascript", "js" },
+  { "application/x-woff", "woff" },
   { "image/bmp", "bmp" },
   { "image/x-icon", "ico" },
   { "image/jpeg", "jfif,pjpeg,pjp" },
