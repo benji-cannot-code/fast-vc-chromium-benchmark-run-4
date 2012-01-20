faf58e6339da958df78ac7273ecb0a7200e25117
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 20:43:20 +0000

    [FileBrowser] Fix eject button
    The problem was we were sending devices full path instead of its URL to removeMount method.
    
    TEST=manual
    BUG=chromium-os:24589,chromium-os:23752
    
    Review URL: https://chromiumcodereview.appspot.com/9190033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118491 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1639,7 +1639,7 @@ FileManager.prototype = {
    */
   FileManager.prototype.onEjectClick_ = function(entry, event) {
     this.unmountRequests_.push(entry.toURL());
-    chrome.fileBrowserPrivate.removeMount(entry.fullPath);
+    chrome.fileBrowserPrivate.removeMount(entry.toURL());
   };
 
   /**
