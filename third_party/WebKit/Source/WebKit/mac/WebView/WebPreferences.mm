7746fb1c61ee1e59d50cbb541666c6b4cb61390e
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 21:19:34 +0000

    https://bugs.webkit.org/show_bug.cgi?id=76444
    This breaks builds.
    
    * WebView/WebPreferences.mm:
    * WebView/WebPreferencesPrivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
@@ -1329,6 +1329,7 @@ static NSString *classIBCreatorID = nil;
 {
     return [self _boolValueForKey:WebKitCSSCustomFilterEnabledPreferenceKey];
 }
+
 - (void)setCSSCustomFilterEnabled:(BOOL)enabled
 {
     [self _setBoolValue:enabled forKey:WebKitCSSCustomFilterEnabledPreferenceKey];
