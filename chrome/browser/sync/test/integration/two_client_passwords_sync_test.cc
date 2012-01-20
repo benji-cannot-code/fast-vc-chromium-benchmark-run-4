99b4ff720437cf57415d15ac95413c81dcd32cc8
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 00:01:29 +0000

    Fix sync passphrase integration test reliability
    This is an attempt to fix the flakiness of sync integration tests using
    explicit passphrases that are affected by the race described in
    crbug.com/110529.
    
    As far as I know, this issue is not very common, though I suspect it is
    related to the much more common crbug.com/95269.  The reason I am
    attempting to fix this is that I have some patches that change the
    behaviour of the sync cycle and tend to make this issue more common.
    
    The actual change is simple: allow clients to be considered "fully
    synced" even when they require a passphrase to decrypt their contents.
    Tests that require a certain cryptographer state can use the
    AwaitPassphraseRequired() and AwaitPassphraseAccepted() functions to
    make assertions about or wait on password state.  It seems most tests
    that need to make these kinds of assertions are already using those
    functions, so this change does not require any changes to existing
    integration tests.
    
    BUG=110529
    TEST=sync_integration_tests
    
    Review URL: https://chromiumcodereview.appspot.com/9249047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/two_client_passwords_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_passwords_sync_test.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -273,7 +273,6 @@ IN_PROC_BROWSER_TEST_F(TwoClientPasswordsSyncTest,
   ASSERT_FALSE(GetClient(1)->SetupSync());
   SetPassphrase(1, kAnotherValidPassphrase);
   ASSERT_TRUE(GetClient(1)->AwaitPassphraseRequired());
-  ASSERT_FALSE(GetClient(1)->AwaitFullSyncCompletion("Initial sync."));
 
   // Add a password on 0 while clients have different passphrases.
   PasswordForm form0 = CreateTestPasswordForm(0);
