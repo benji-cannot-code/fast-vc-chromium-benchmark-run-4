0aa1e20320f0b5ddc25e86cf3eea02cc04cace92
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 20:22:24 +0000

    Follow-up to previous patch: don't produce NaN when the revision number is missing.
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model_unittests.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model_unittests.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model_unittests.js
@@ -243,7 +243,7 @@ test("buildersInFlightForRevision", 3, function() {
 test("latestRevisionWithNoBuildersInFlight", 1, function() {
     var unmock = model.state.resultsByBuilder;
     model.state.resultsByBuilder = {
-        'Mr. Beasley': {revision: '5'},
+        'Mr. Beasley': { },
         'Mr Dixon': {revision: '2'},
         'Mr. Sabatini': {revision: '4'},
         'Bob': {revision: '6'}
