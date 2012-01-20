adfa5aa81294996e1e1924135ca6722147fd7797
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 02:00:46 +0000

    Fix a potential race in OnSwapBuffersComplete().
    It seems to be possible that we delete (and recreate) the graphics
    context from
    WebGraphicsContext3DCommandBufferImpl::OnSwapBuffersComplete() calling
    renderview->OnViewContextSwapBuffersComplete().
    This seems to be possible if the updated state already contains
    a cmdbuffer error while the context did not receive OnContextLost yet.
    
    BUG=110115
    
    Review URL: https://chromiumcodereview.appspot.com/9159026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118392 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/webgraphicscontext3d_command_buffer_impl.cc
+++ content/renderer/gpu/webgraphicscontext3d_command_buffer_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
+#include "base/message_loop.h"
 #include "base/metrics/histogram.h"
 #include "base/synchronization/lock.h"
 #include "content/common/child_process.h"
@@ -1135,8 +1136,12 @@ void WebGraphicsContext3DCommandBufferImpl::OnSwapBuffersComplete() {
   // This may be called after tear-down of the RenderView.
   RenderViewImpl* renderview =
       web_view_ ? RenderViewImpl::FromWebView(web_view_) : NULL;
-  if (renderview)
-    renderview->OnViewContextSwapBuffersComplete();
+  if (renderview) {
+    MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(&RenderViewImpl::OnViewContextSwapBuffersComplete,
+                   renderview));
+  }
 
   if (swapbuffers_complete_callback_)
     swapbuffers_complete_callback_->onSwapBuffersComplete();
