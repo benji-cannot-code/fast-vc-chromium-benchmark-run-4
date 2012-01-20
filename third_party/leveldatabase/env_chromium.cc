fe0ceb7a6ab2258466ce9de5bc9bd4d9b08acc6a
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 23:39:59 +0000

    Make leveldb use TimeTicks::Now instead of TimeTicks::HighResNow
    BUG=108506
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9192033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118544 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -444,7 +444,7 @@ class ChromiumEnv : public Env {
   }
 
   virtual uint64_t NowMicros() {
-    return ::base::TimeTicks::HighResNow().ToInternalValue();
+    return ::base::TimeTicks::Now().ToInternalValue();
   }
 
   virtual void SleepForMicroseconds(int micros) {
