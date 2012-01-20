c53579f493a58095c24d9d28162658e210d5772c
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 19:56:53 +0000

    Fix a couple of drag drop bugs: 1. When dragging over BrowserRootView, we must sanitize the dragged string before querying Autocomplete. 2. We may receive un-expected events during drag drop session if the user moves out of the root window.
    BUG=110733
    TEST=chrome no longer crashes on drag drop.
    
    
    Review URL: http://codereview.chromium.org/9270001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_root_view.cc
+++ chrome/browser/ui/views/frame/browser_root_view.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -158,6 +158,7 @@ bool BrowserRootView::GetPasteAndGoURL(const ui::OSExchangeData& data,
   string16 text;
   if (!data.GetString(&text) || text.empty())
     return false;
+  text = AutocompleteMatch::SanitizeString(text);
 
   AutocompleteMatch match;
   browser_view_->browser()->profile()->GetAutocompleteClassifier()->Classify(
