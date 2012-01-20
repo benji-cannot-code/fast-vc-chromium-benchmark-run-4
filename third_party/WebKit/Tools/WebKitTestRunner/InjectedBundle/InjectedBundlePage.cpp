6b8a60af821c12e07fc5c7513f0221ea0dacef72
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-20 01:10:47 +0000

    [WK2] DumpRenderTree converts "file:///" to a path differently https://bugs.webkit.org/show_bug.cgi?id=76653
    Reviewed by John Sullivan.
    
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp: (WTR::lastFileURLPathComponent):
    Do print a slash if there are no path components.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -839,7 +839,7 @@ static string lastFileURLPathComponent(const string& path)
     ASSERT(string::npos != pos);
 
     string tmpPath = path.substr(pos + 7);
-    if (tmpPath.empty())
+    if (tmpPath.length() < 2) // Keep the lone slash to avoid empty output.
         return tmpPath;
 
     // Remove the trailing delimiter
