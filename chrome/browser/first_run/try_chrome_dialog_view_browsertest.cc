b78810aaf9c7f781abb4b452b2df01d1730a13b5
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 10:25:12 +0000

    Converting Toast crasher test from UI test to browser test.
    The old UI test would just hang due to limitations in our test framework, but the browser test catches the crash that we currently have in our codebase (a crash due to recent refactoring).
    
    BUG=45115
    TEST=None, this is to add a new test for a disabled one.
    Review URL: https://chromiumcodereview.appspot.com/9250010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118430 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/first_run/try_chrome_dialog_view_browsertest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/test/base/in_process_browser_test.h"
+
+// By passing kTryChromeAgain with a magic value > 10000 we cause Chrome
+// to exit fairly early.
+// Quickly exiting Chrome (regardless of this particular flag -- it
+// doesn't do anything other than cause Chrome to quit on startup on
+// non-Windows) was a cause of crashes (see bug 34799 for example) so
+// this is a useful test of the startup/quick-shutdown cycle.
+class TryChromeDialogBrowserTest : public InProcessBrowserTest {
+public:
+  TryChromeDialogBrowserTest() {}
+
+protected:
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    command_line->AppendSwitchASCII(switches::kTryChromeAgain, "10001");
+  }
+};
+IN_PROC_BROWSER_TEST_F(TryChromeDialogBrowserTest, ToastCrasher) {}
