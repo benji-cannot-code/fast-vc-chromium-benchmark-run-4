444a0608b562f7e8779e9490b1616ec015f7f08d
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 17:31:25 +0000

    Fix Android compile error following r118272
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9233011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118456 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/android/audio_manager_android.cc
+++ media/audio/android/audio_manager_android.cc
@@ -18,7 +18,7 @@ AudioManager* CreateAudioManager() {
 AudioManagerAndroid::AudioManagerAndroid() {}
 
 AudioManagerAndroid::~AudioManagerAndroid() {
-  audio_thread_.Stop();
+  audio_thread_->Stop();
 }
 
 bool AudioManagerAndroid::HasAudioOutputDevices() {
