67c4ca68f81d6adeed726cbf6f3bb2de45516292
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 23:08:58 +0000

    Stack un-parented control windows in their own container. This prevents them from interfering in existing window layout heuristics/animations.
    BUG=none
    TEST=existing
    Review URL: https://chromiumcodereview.appspot.com/9138026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118541 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/toplevel_layout_manager.cc
+++ ash/wm/toplevel_layout_manager.cc
@@ -54,6 +54,8 @@ void ToplevelLayoutManager::OnWindowAddedToLayout(aura::Window* child) {
 }
 
 void ToplevelLayoutManager::OnWillRemoveWindowFromLayout(aura::Window* child) {
+  DCHECK(child->type() == aura::client::WINDOW_TYPE_NORMAL ||
+         child->type() == aura::client::WINDOW_TYPE_POPUP);
   BaseLayoutManager::OnWillRemoveWindowFromLayout(child);
   UpdateShelfVisibility();
 }
@@ -61,15 +63,12 @@ void ToplevelLayoutManager::OnWillRemoveWindowFromLayout(aura::Window* child) {
 void ToplevelLayoutManager::OnChildWindowVisibilityChanged(aura::Window* child,
                                                            bool visible) {
   BaseLayoutManager::OnChildWindowVisibilityChanged(child, visible);
-  if (child->type() == aura::client::WINDOW_TYPE_NORMAL ||
-      child->type() == aura::client::WINDOW_TYPE_POPUP) {
-    if (visible) {
-      AnimateShowWindow(child);
-    } else {
-      // Don't start hiding the window again if it's already being hidden.
-      if (child->layer()->GetTargetOpacity() != 0.0f)
-        AnimateHideWindow(child);
-    }
+  if (visible) {
+    AnimateShowWindow(child);
+  } else {
+    // Don't start hiding the window again if it's already being hidden.
+    if (child->layer()->GetTargetOpacity() != 0.0f)
+      AnimateHideWindow(child);
   }
   UpdateShelfVisibility();
 }
