f0a5a75c23792b602b4114a0dfb6a4a53cefe552
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-21 16:41:43 +0000

    Move the pnacl translation nexes fully off the main thread. BUG=none TEST=run_pnacl_example_browser_test Review URL: https://chromiumcodereview.appspot.com/9138047
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118621 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/plugin.h
+++ ppapi/native_client/src/trusted/plugin/plugin.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // -*- c++ -*-
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -383,6 +383,7 @@ class Plugin : public pp::InstancePrivate {
   bool LoadNaClModuleCommon(nacl::DescWrapper* wrapper,
                             NaClSubprocess* subprocess,
                             const Manifest* manifest,
+                            bool should_report_uma,
                             ErrorInfo* error_info,
                             pp::CompletionCallback init_done_cb,
                             pp::CompletionCallback crash_cb);
