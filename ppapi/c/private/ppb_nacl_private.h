a6a21b7ce4afd62b8cb601c100bf39ab9bd0fb7f
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-21 00:41:22 +0000

    Revert 118553 - Add nacl private interface and state for process creation. After invoking an interface method this will allow creation from other than RenderThread. BUG=none TEST=none Review URL: https://chromiumcodereview.appspot.com/9265027
    TBR=sehr@google.com
    Review URL: https://chromiumcodereview.appspot.com/9131027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118565 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/private/ppb_nacl_private.h
+++ ppapi/c/private/ppb_nacl_private.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,8 +17,7 @@ struct PPB_NaCl_Private {
   // write |socket_count| nacl::Handles to imc_handles and will write the
   // nacl::Handle of the created process to |nacl_process_handle|.  Finally,
   // the function will write the process ID of the created process to
-  // |nacl_process_id|.  Unless EnableBackgroundSelLdrLaunch is called, this
-  // method must be invoked from the main thread.
+  // |nacl_process_id|.
   bool (*LaunchSelLdr)(const char* alleged_url, int socket_count,
                        void* imc_handles, void* nacl_process_handle,
                        int* nacl_process_id);
@@ -31,9 +30,6 @@ struct PPB_NaCl_Private {
   // proxy. This is so paranoid admins can effectively prevent untrusted shader
   // code to be processed by the graphics stack.
   bool (*Are3DInterfacesDisabled)();
-
-  // Enables the creation of sel_ldr processes from other than the main thread.
-  void (*EnableBackgroundSelLdrLaunch)();
 };
 
 #endif  // PPAPI_C_PRIVATE_PPB_NACL_PRIVATE_H_
