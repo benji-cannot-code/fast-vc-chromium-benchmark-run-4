9c7fdc4d6e09d6de5121b8c36cd76e0805696eb6
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-21 02:01:53 +0000

    Expand suppression for testGeolocationPref to include vista chromium pyauto bot.
    BUG=89000
    TEST=green pyauto chromium vista bot
    
    R=dennisjeffrey@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9270029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/prefs.py
+++ chrome/test/functional/prefs.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -159,10 +159,6 @@ class PrefsTest(pyauto.PyUITest):
 
     Checks for the geolocation infobar.
     """
-    # Fails on Win7 Chromium bot only.  crbug.com/89000
-    if (self.IsWin7() and
-        self.GetBrowserInfo()['properties']['branding'] == 'Chromium'):
-      return
     url = self.GetFileURLForPath(os.path.join(  # triggers geolocation
         self.DataDir(), 'geolocation', 'geolocation_on_load.html'))
     self.assertEqual(3,  # default state
@@ -175,6 +171,10 @@ class PrefsTest(pyauto.PyUITest):
     self.assertEqual(2,
         self.GetPrefsInfo().Prefs(pyauto.kGeolocationDefaultContentSetting))
     self.GetBrowserWindow(0).GetTab(0).Reload()
+    # Fails on Win7/Vista Chromium bots.  crbug.com/89000
+    if ((self.IsWin7() or self.IsWinVista()) and
+        self.GetBrowserInfo()['properties']['branding'] == 'Chromium'):
+      return
     self.assertTrue(self.WaitForInfobarCount(0))
     self.assertFalse(self.GetBrowserInfo()['windows'][0]['tabs'][0]['infobars'])
 
