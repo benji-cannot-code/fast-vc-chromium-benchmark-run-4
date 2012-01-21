23ed282e4eea47542732a961190ad6ee5b43cd43
cmarrin@apple.com <cmarrin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-21 15:40:03 +0000

    Implement hardware accelerated Brightness and contrast filters https://bugs.webkit.org/show_bug.cgi?id=75521 https://bugs.webkit.org/show_bug.cgi?id=76719
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Implemented hardware accelerated brightness and contrast filters. This also fixes
    the bug where grayscale filter was accidentally never getting hardware accelerated.
    It also complies with proposed spec changes for the brightness filter to be additive
    rather than multiplicative, according to https://bugs.webkit.org/show_bug.cgi?id=76719.
    Had to make both fixes in the same patch because I had to change the allowed brightness
    values for the hardware version, so I had to change the software version as well.
    
    Tests: css3/filters/effect-brightness-hw.html
           css3/filters/effect-contrast-hw.html
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseBuiltinFilterArguments):
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::createFilterOperations):
    * platform/graphics/ca/mac/PlatformCALayerMac.mm:
    (PlatformCALayer::setFilters):
    (PlatformCALayer::filtersCanBeComposited):
    * rendering/FilterEffectRenderer.cpp:
    (WebCore::FilterEffectRenderer::build):
    
    LayoutTests:
    
    New tests for hardware accelerated brightness and contrast filters. Also
    added brightness and contrast values to effect-combined-hw test. And changed
    brightness values in effect-brightness.html to reflect new spec.
    Also fixed parsing and computed style tests to reflect new brightness spec.
    
    * css3/filters/effect-brightness-expected.png:
    * css3/filters/effect-brightness-hw-expected.png: Added.
    * css3/filters/effect-brightness-hw-expected.txt: Added.
    * css3/filters/effect-brightness-hw.html: Added.
    * css3/filters/effect-brightness.html:
    * css3/filters/effect-combined-expected.png:
    * css3/filters/effect-combined-hw-expected.png:
    * css3/filters/effect-combined-hw-expected.txt:
    * css3/filters/effect-combined-hw.html:
    * css3/filters/effect-combined.html:
    * css3/filters/effect-contrast-hw-expected.png: Added.
    * css3/filters/effect-contrast-hw-expected.txt: Added.
    * css3/filters/effect-contrast-hw.html: Added.
    * css3/filters/filter-property-computed-style-expected.txt:
    * css3/filters/filter-property-parsing-expected.txt:
    * css3/filters/filter-property-parsing-invalid-expected.txt:
    * css3/filters/script-tests/filter-property-computed-style.js:
    * css3/filters/script-tests/filter-property-parsing-invalid.js:
    * css3/filters/script-tests/filter-property-parsing.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/PlatformCALayerMac.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/PlatformCALayerMac.mm
@@ -721,6 +721,8 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
     RetainPtr<NSMutableArray> array(AdoptNS, [[NSMutableArray alloc] init]);
     
     for (unsigned i = 0; i < filters.size(); ++i) {
+        String filterName = String::format("filter_%d", i);
+        
         const FilterOperation* filterOperation = filters.at(i);
         switch(filterOperation->getOperationType()) {
         case FilterOperation::DROP_SHADOW: {
@@ -745,7 +747,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             [caFilter setDefaults];
             [caFilter setValue:[NSNumber numberWithFloat:op->amount()] forKey:@"inputIntensity"];
             [caFilter setValue:[CIColor colorWithRed:1 green:1 blue:1] forKey:@"inputColor"];
-            [caFilter setName:@"grayscaleFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -763,7 +765,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             [caFilter setValue:[CIVector vectorWithX:WebCore::blend(0.0, 0.272, t) Y:WebCore::blend(0.0, 0.534, t) Z:WebCore::blend(1.0, 0.131, t) W:0] forKey:@"inputBVector"];
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:1] forKey:@"inputAVector"];
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:0] forKey:@"inputBiasVector"];
-            [caFilter setName:@"sepiaFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -772,7 +774,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             CIFilter* caFilter = [CIFilter filterWithName:@"CIColorControls"];
             [caFilter setDefaults];
             [caFilter setValue:[NSNumber numberWithFloat:op->amount()] forKey:@"inputSaturation"];
-            [caFilter setName:@"saturateFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -783,7 +785,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             
             // The CIHueAdjust value is in radians
             [caFilter setValue:[NSNumber numberWithFloat:op->amount() * M_PI * 2 / 360] forKey:@"inputAngle"];
-            [caFilter setName:@"hueRotateFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -799,7 +801,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:multiplier W:0] forKey:@"inputBVector"];
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:1] forKey:@"inputAVector"];
             [caFilter setValue:[CIVector vectorWithX:op->amount() Y:op->amount() Z:op->amount() W:0] forKey:@"inputBiasVector"];
-            [caFilter setName:@"invertFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -813,7 +815,7 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:1 W:0] forKey:@"inputBVector"];
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:op->amount()] forKey:@"inputAVector"];
             [caFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:0] forKey:@"inputBiasVector"];
-            [caFilter setName:@"opacityFilter"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -823,7 +825,25 @@ void PlatformCALayer::setFilters(const FilterOperations& filters)
             CIFilter* caFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
             [caFilter setDefaults];
             [caFilter setValue:[NSNumber numberWithFloat:op->stdDeviation().calcFloatValue(0)] forKey:@"inputRadius"];
-            [caFilter setName:@"blurFilter"];
+            [caFilter setName:filterName];
+            [array.get() addObject:caFilter];
+            break;
+        }
+        case FilterOperation::CONTRAST: {
+            const BasicComponentTransferFilterOperation* op = static_cast<const BasicComponentTransferFilterOperation*>(filterOperation);
+            CIFilter* caFilter = [CIFilter filterWithName:@"CIColorControls"];
+            [caFilter setDefaults];
+            [caFilter setValue:[NSNumber numberWithFloat:op->amount()] forKey:@"inputContrast"];
+            [caFilter setName:filterName];
+            [array.get() addObject:caFilter];
+            break;
+        }
+        case FilterOperation::BRIGHTNESS: {
+            const BasicComponentTransferFilterOperation* op = static_cast<const BasicComponentTransferFilterOperation*>(filterOperation);
+            CIFilter* caFilter = [CIFilter filterWithName:@"CIColorControls"];
+            [caFilter setDefaults];
+            [caFilter setValue:[NSNumber numberWithFloat:op->amount()] forKey:@"inputBrightness"];
+            [caFilter setName:filterName];
             [array.get() addObject:caFilter];
             break;
         }
@@ -851,9 +871,6 @@ bool PlatformCALayer::filtersCanBeComposited(const FilterOperations& filters)
         const FilterOperation* filterOperation = filters.at(i);
         switch(filterOperation->getOperationType()) {
         case FilterOperation::REFERENCE:
-        case FilterOperation::GRAYSCALE:
-        case FilterOperation::BRIGHTNESS:
-        case FilterOperation::CONTRAST:
 #if ENABLE(CSS_SHADERS)
         case FilterOperation::CUSTOM:
 #endif
