c3ef12472edbf7d2aef50c8c150fb4f8fc052469
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-22 09:11:49 +0000

    Build fix for non-DFG platforms that error out on warn-unused-parameter.
    * bytecode/CallLinkStatus.cpp:
    (JSC::CallLinkStatus::computeFor):
    * bytecode/GetByIdStatus.cpp:
    (JSC::GetByIdStatus::computeFor):
    * bytecode/MethodCallLinkStatus.cpp:
    (JSC::MethodCallLinkStatus::computeFor):
    * bytecode/PutByIdStatus.cpp:
    (JSC::PutByIdStatus::computeFor):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105586 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/PutByIdStatus.cpp
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/PutByIdStatus.cpp
@@ -35,6 +35,9 @@ namespace JSC {
 
 PutByIdStatus PutByIdStatus::computeFor(CodeBlock* profiledBlock, unsigned bytecodeIndex, Identifier& ident)
 {
+    UNUSED_PARAM(profiledBlock);
+    UNUSED_PARAM(bytecodeIndex);
+    UNUSED_PARAM(ident);
 #if ENABLE(JIT) && ENABLE(VALUE_PROFILER)
     if (profiledBlock->likelyToTakeSlowCase(bytecodeIndex))
         return PutByIdStatus(TakesSlowPath, 0, 0, 0, notFound);
