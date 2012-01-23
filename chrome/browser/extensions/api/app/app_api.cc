0cf66a4421b11a7f051f8ac0ea550026270b552a
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 16:03:51 +0000

    Move app_api out of chrome/browser/extensions
    BUG=101244
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9187025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118687 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/app/app_api.cc
+++ chrome/browser/extensions/api/app/app_api.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/extension_app_api.h"
+#include "chrome/browser/extensions/api/app/app_api.h"
 
 #include "base/values.h"
 #include "base/time.h"
