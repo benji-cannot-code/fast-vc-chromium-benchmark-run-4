5b3a099ec3c379c2b0da48f38ffe0f048775f537
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 18:36:57 +0000

    Mark FirstRunSearchEngineViewTest.ClosingBeforeServiceLoadedAbortsClose as Flaky
    BUG=111062
    TEST=unit_tests
    TBR=jam
    
    Review URL: https://chromiumcodereview.appspot.com/9270050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118704 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/first_run_search_engine_view_unittest.cc
+++ chrome/browser/ui/views/first_run_search_engine_view_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -56,7 +56,9 @@ TEST_F(FirstRunSearchEngineViewTest, ClosingSelectsFirstEngine) {
   EXPECT_EQ(template_urls.front(), service->GetDefaultSearchProvider());
 }
 
-TEST_F(FirstRunSearchEngineViewTest, ClosingBeforeServiceLoadedAbortsClose) {
+// http://code.google.com/p/chromium/issues/detail?id=111062
+TEST_F(FirstRunSearchEngineViewTest,
+       FLAKY_ClosingBeforeServiceLoadedAbortsClose) {
   // This ensures the current thread is named the UI thread, so code that checks
   // that this is the UI thread doesn't assert.
   base::Thread db_thread("tempdbthread");
