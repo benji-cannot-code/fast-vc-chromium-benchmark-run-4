460921e1a79cdf0bbed58bcb52ec3d2242275c67
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-23 21:36:18 +0000

    Remove StackBounds from JSGlobalData https://bugs.webkit.org/show_bug.cgi?id=76310
    Reviewed by Sam Weinig.
    
    Removed StackBounds and the stack() function from JSGlobalData since it no 
    longer accessed any members of JSGlobalData.
    
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::BytecodeGenerator):
    * heap/MachineStackMarker.cpp:
    (JSC::MachineThreads::addCurrentThread):
    (JSC::MachineThreads::gatherFromCurrentThread):
    * parser/Parser.cpp:
    (JSC::::Parser):
    * runtime/JSGlobalData.cpp:
    (JSC::JSGlobalData::JSGlobalData):
    * runtime/JSGlobalData.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/MachineStackMarker.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/MachineStackMarker.cpp
@@ -194,7 +194,7 @@ void MachineThreads::addCurrentThread()
         return;
 
     pthread_setspecific(m_threadSpecific, this);
-    Thread* thread = new Thread(getCurrentPlatformThread(), m_heap->globalData()->stack().origin());
+    Thread* thread = new Thread(getCurrentPlatformThread(), wtfThreadData().stack().origin());
 
     MutexLocker lock(m_registeredThreadsMutex);
 
@@ -258,7 +258,7 @@ void MachineThreads::gatherFromCurrentThread(ConservativeRoots& conservativeRoot
     conservativeRoots.add(registersBegin, registersEnd);
 
     void* stackBegin = stackCurrent;
-    void* stackEnd = m_heap->globalData()->stack().origin();
+    void* stackEnd = wtfThreadData().stack().origin();
     swapIfBackwards(stackBegin, stackEnd);
     conservativeRoots.add(stackBegin, stackEnd);
 }
