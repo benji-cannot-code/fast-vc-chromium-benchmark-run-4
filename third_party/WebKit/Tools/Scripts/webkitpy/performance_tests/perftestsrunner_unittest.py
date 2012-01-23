a02261ad27004d270ab3beb7adf127972e21253b
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-23 23:49:00 +0000

    run-perf-tests should report server-side errors https://bugs.webkit.org/show_bug.cgi?id=76802
    Reviewed by Tony Chang.
    
    Report errors whenever server's response doesn't match "OK".
    
    * Scripts/webkitpy/common/net/file_uploader.py:
    (FileUploader.upload_single_text_file):
    (FileUploader.upload_as_multipart_form_data):
    (FileUploader._upload_data.callback):
    (FileUploader):
    (FileUploader._upload_data):
    * Scripts/webkitpy/performance_tests/perftestsrunner.py:
    (PerfTestsRunner._upload_json):
    * Scripts/webkitpy/performance_tests/perftestsrunner_unittest.py:
    (test_upload_json):
    (test_upload_json.MockFileUploader.upload_single_text_file):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105650 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/performance_tests/perftestsrunner_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/performance_tests/perftestsrunner_unittest.py
@@ -30,12 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Unit tests for run_perf_tests."""
 
+import StringIO
 import json
 import unittest
 
 from webkitpy.common import array_stream
 from webkitpy.common.host_mock import MockHost
 from webkitpy.common.system.filesystem_mock import MockFileSystem
+from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.layout_tests.port.driver import DriverInput, DriverOutput
 from webkitpy.layout_tests.port.test import TestPort
 from webkitpy.layout_tests.views import printing
@@ -241,11 +243,12 @@ max 1120
 
     def test_upload_json(self):
         regular_output = array_stream.ArrayStream()
-        runner = self.create_runner(buildbot_output=regular_output)
+        runner = self.create_runner(regular_output=regular_output)
         runner._host.filesystem.files['/mock-checkout/some.json'] = 'some content'
 
         called = []
         upload_single_text_file_throws = False
+        upload_single_text_file_return_value = StringIO.StringIO('OK')
 
         class MockFileUploader:
             def __init__(mock, url, timeout):
@@ -260,9 +263,17 @@ max 1120
                 called.append('upload_single_text_file')
                 if upload_single_text_file_throws:
                     raise "Some exception"
+                return upload_single_text_file_return_value
 
         runner._upload_json('some.host', 'some.json', MockFileUploader)
         self.assertEqual(called, ['FileUploader', 'upload_single_text_file'])
+
+        output = OutputCapture()
+        output.capture_output()
+        upload_single_text_file_return_value = StringIO.StringIO('Some error')
+        runner._upload_json('some.host', 'some.json', MockFileUploader)
+        _, _, logs = output.restore_output()
+        self.assertEqual(logs, 'Uploaded JSON but got a bad response:\nSome error\n')
 
         # Throwing an exception upload_single_text_file shouldn't blow up _upload_json
         called = []
