e82e5cf69de40725d5dfb38d90896722b3add270
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 11:19:04 +0000

    Added pyauto hooks and basic functional tests for Protector.
    BUG=104539
    TEST=functional/protector.py
    
    Review URL: https://chromiumcodereview.appspot.com/9150037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118668 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/keyword_table.cc
+++ chrome/browser/webdata/keyword_table.cc
@@ -283,7 +283,7 @@ bool KeywordTable::DidDefaultSearchProviderChange() {
       protector::kProtectorHistogramDefaultSearchProvider,
       protector::kProtectorErrorValueChanged,
       protector::kProtectorErrorCount);
-  LOG(WARNING) << "Default Search Provider is changed.";
+  LOG(WARNING) << "Default Search Provider has changed.";
   return true;
 }
 
