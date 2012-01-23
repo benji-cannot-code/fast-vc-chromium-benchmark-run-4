1cef0e0656930cfa670553501fee349c2b91f981
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 20:27:18 +0000

    Disable crashing unit tests for linux/aura
    BUG=110886
    TEST=Run unit tests on all platforms
    
    
    Review URL: http://codereview.chromium.org/9178003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_manager_unittest.cc
+++ chrome/browser/download/download_manager_unittest.cc
@@ -51,10 +51,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAYBE_StartDownload DISABLED_StartDownload
 #define MAYBE_DownloadOverwriteTest DISABLED_DownloadOverwriteTest
 #define MAYBE_DownloadRemoveTest DISABLED_DownloadRemoveTest
+#define MAYBE_DownloadFileErrorTest DownloadFileErrorTest
+#elif defined(OS_LINUX)
+// http://crbug.com/110886 for Linux
+#define MAYBE_StartDownload DISABLED_StartDownload
+#define MAYBE_DownloadOverwriteTest DISABLED_DownloadOverwriteTest
+#define MAYBE_DownloadRemoveTest DISABLED_DownloadRemoveTest
+#define MAYBE_DownloadFileErrorTest DISABLED_DownloadFileErrorTest
 #else
 #define MAYBE_StartDownload StartDownload
 #define MAYBE_DownloadOverwriteTest DownloadOverwriteTest
 #define MAYBE_DownloadRemoveTest DownloadRemoveTest
+#define MAYBE_DownloadFileErrorTest DownloadFileErrorTest
 #endif
 
 using content::BrowserThread;
@@ -983,7 +991,7 @@ TEST_F(DownloadManagerTest, DownloadInterruptTest) {
 
 // Test the behavior of DownloadFileManager and DownloadManager in the event
 // of a file error while writing the download to disk.
-TEST_F(DownloadManagerTest, DownloadFileErrorTest) {
+TEST_F(DownloadManagerTest, MAYBE_DownloadFileErrorTest) {
   // Create a temporary file and a mock stream.
   FilePath path;
   ASSERT_TRUE(file_util::CreateTemporaryFile(&path));
