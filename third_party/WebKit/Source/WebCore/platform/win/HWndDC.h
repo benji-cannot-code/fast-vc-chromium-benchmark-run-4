1c3c30d40926959d8b56c06af6e0b65fcb7b45f5
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-24 00:35:29 +0000

    Allow delayed DC allocation in HWndDC. https://bugs.webkit.org/show_bug.cgi?id=76737
    Reviewed by Adam Roben.
    
    No new functionality exposed so no new tests.
    
    * platform/win/HWndDC.h: Changed this slightly to allow
    for allocating a window DC after the initial creation since
    this pattern occurrs in several places so this makes it easy to
    replace them in an upcoming change.
    (WebCore::HWndDC::HWndDC):
    (WebCore::HWndDC::~HWndDC):
    (WebCore::HWndDC::setHWnd):
    (WebCore::HWndDC::clear):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105656 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/HWndDC.h
+++ third_party/WebKit/Source/WebCore/platform/win/HWndDC.h
@@ -35,6 +35,12 @@ namespace WebCore {
 class HWndDC {
     WTF_MAKE_NONCOPYABLE(HWndDC);
 public:
+    HWndDC()
+        : m_hwnd(0)
+        , m_hdc(0)
+    {
+    }
+
     explicit HWndDC(HWND hwnd)
         : m_hwnd(hwnd)
         , m_hdc(::GetDC(hwnd))
@@ -49,8 +55,24 @@ public:
 
     ~HWndDC()
     {
-        if (m_hdc)
-            ::ReleaseDC(m_hwnd, m_hdc);
+        clear();
+    }
+
+    HDC setHWnd(HWND hwnd)
+    {
+        clear();
+        m_hwnd = hwnd;
+        m_hdc = ::GetDC(hwnd);
+        return m_hdc;
+    }
+
+    void clear()
+    {
+        if (!m_hdc)
+            return;
+        ::ReleaseDC(m_hwnd, m_hdc);
+        m_hwnd = 0;
+        m_hdc = 0;
     }
 
     operator HDC()
