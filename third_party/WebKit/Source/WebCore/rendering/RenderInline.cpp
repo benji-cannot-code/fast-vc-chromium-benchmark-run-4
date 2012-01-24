852d346a2880da56ca8045311801d1014d6ecf20
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-24 20:22:00 +0000

    RenderInline: Skip caching the computed line height. <http://webkit.org/b/76929>
    Reviewed by David Hyatt.
    
    Stop caching the computed line height on RenderInline and make retrieving it from
    RenderStyle slightly cheaper, freeing up 4 bytes per RenderInline instance.
    This appears to be mostly performance neutral, I don't get more than the occasional
    sample hit when instrumenting heavier web pages.
    
    This reduces memory consumption by 228 kB (both 32/64-bit) when viewing the full
    HTML5 spec at <http://whatwg.org/c>.
    
    * rendering/RenderInline.cpp:
    (WebCore::RenderInline::RenderInline):
    (WebCore::RenderInline::styleDidChange):
    (WebCore::RenderInline::lineHeight):
    * rendering/RenderInline.h:
    * rendering/style/RenderStyle.h:
    (WebCore::RenderStyle::computedLineHeight):
    
        Optimize computedLineHeight() to mitigate some of the damage of calling
        it more often.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderInline.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderInline.cpp
@@ -48,7 +48,6 @@ namespace WebCore {
 
 RenderInline::RenderInline(Node* node)
     : RenderBoxModelObject(node)
-    , m_lineHeight(-1)
     , m_alwaysCreateLineBoxes(false)
 {
     setChildrenInline(true);
@@ -145,8 +144,6 @@ void RenderInline::styleDidChange(StyleDifference diff, const RenderStyle* oldSt
         currCont->setStyle(newStyle);
         currCont->setContinuation(nextCont);
     }
-
-    m_lineHeight = -1;
 
     if (!m_alwaysCreateLineBoxes) {
         bool alwaysCreateLineBoxes = hasSelfPaintingLayer() || hasBoxDecorations() || newStyle->hasPadding() || newStyle->hasMargin() || hasOutline();
@@ -1270,11 +1267,8 @@ LayoutUnit RenderInline::lineHeight(bool firstLine, LineDirectionMode /*directio
         if (s != style())
             return s->computedLineHeight();
     }
-    
-    if (m_lineHeight == -1)
-        m_lineHeight = style()->computedLineHeight();
-    
-    return m_lineHeight;
+
+    return style()->computedLineHeight();
 }
 
 LayoutUnit RenderInline::baselinePosition(FontBaseline baselineType, bool firstLine, LineDirectionMode direction, LinePositionMode linePositionMode) const
