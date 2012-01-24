22c9095aa42999f8bf2236a97ff43f0dc3a372d5
vollick@google.com <vollick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 15:01:06 +0000

    Revert 118834 - Support touch scroll gestures in menus.
    Depends on http://codereview.chromium.org/8364039/
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/8508024
    
    TBR=vollick@google.com
    Review URL: https://chromiumcodereview.appspot.com/9284024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118835 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_host_root_view.cc
+++ ui/views/controls/menu/menu_host_root_view.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -61,14 +61,6 @@ bool MenuHostRootView::OnMouseWheel(const MouseWheelEvent& event) {
   // focused view, so we need to override this then forward to the menu.
   return submenu_->OnMouseWheel(event);
 #endif
-}
-
-ui::GestureStatus MenuHostRootView::OnGestureEvent(const GestureEvent& event) {
-  // ChromeOS uses MenuController to forward events like other
-  // mouse events.
-  if (!GetMenuController())
-    return ui::GESTURE_STATUS_UNKNOWN;
-  return GetMenuController()->OnGestureEvent(submenu_, event);
 }
 
 MenuController* MenuHostRootView::GetMenuController() {
