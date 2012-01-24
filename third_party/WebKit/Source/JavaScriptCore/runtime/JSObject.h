f7b809a60a7343287e8e26b962375db926a8cd03
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-24 03:55:17 +0000

    Unreviewed, rolling out r105676. http://trac.webkit.org/changeset/105676 https://bugs.webkit.org/show_bug.cgi?id=76665
    Breaks build on max due to compile warnings.
    
    * runtime/JSObject.cpp:
    (JSC::JSObject::finalize):
    (JSC::JSObject::visitChildren):
    (JSC::JSObject::allocatePropertyStorage):
    * runtime/JSObject.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105682 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
@@ -91,6 +91,8 @@ namespace JSC {
 
         JS_EXPORT_PRIVATE static UString className(const JSObject*);
 
+        static void finalize(JSCell*);
+
         JSValue prototype() const;
         void setPrototype(JSGlobalData&, JSValue prototype);
         bool setPrototypeWithCycleCheck(JSGlobalData&, JSValue prototype);
