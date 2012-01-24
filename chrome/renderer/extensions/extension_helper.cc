578d352d5f0d21d3fabd2044ea2921db15ceea97
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 02:30:56 +0000

    Replace lazy-background switch with a manifest entry controlled by the extension.
    BUG=81752
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/9233016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_helper.cc
+++ chrome/renderer/extensions/extension_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -209,9 +209,7 @@ void ExtensionHelper::OnExtensionResponse(int request_id,
       extension_dispatcher_->v8_context_set(), request_id, success,
       response, error, &extension_id);
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableLazyBackgroundPages))
-    extension_dispatcher_->CheckIdleStatus(extension_id);
+  extension_dispatcher_->CheckIdleStatus(extension_id);
 }
 
 void ExtensionHelper::OnExtensionMessageInvoke(const std::string& extension_id,
