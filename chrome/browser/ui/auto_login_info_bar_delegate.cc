876c986412b219555c03fe97c0e4f65618d6f682
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 17:00:33 +0000

    Fix string resources for reverse auto-login. Make reverse auto-login on all the time, no need to hide behind about:flag.
    BUG=104695,110454
    TEST=Make sure strings appear as specified in last comments of both bugs.
    Make sure new auto-login about:flag does not enable/disable reverse auto-login.
    
    Review URL: http://codereview.chromium.org/9131017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118847 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/auto_login_info_bar_delegate.cc
+++ chrome/browser/ui/auto_login_info_bar_delegate.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/common/referrer.h"
+#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources_standard.h"
 #include "net/base/escape.h"
@@ -202,7 +203,9 @@ InfoBarDelegate::Type ReverseAutoLoginInfoBarDelegate::GetInfoBarType() const {
 }
 
 string16 ReverseAutoLoginInfoBarDelegate::GetMessageText() const {
-  return l10n_util::GetStringUTF16(IDS_REVERSE_AUTOLOGIN_INFOBAR_MESSAGE);
+  return l10n_util::GetStringFUTF16(
+      IDS_REVERSE_AUTOLOGIN_INFOBAR_MESSAGE,
+      l10n_util::GetStringUTF16(IDS_SHORT_PRODUCT_NAME));
 }
 
 string16 ReverseAutoLoginInfoBarDelegate::GetButtonLabel(
