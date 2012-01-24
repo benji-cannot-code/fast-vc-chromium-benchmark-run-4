8e04d4098981daf3a55b41253fc5ad07b1f5165c
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 18:24:26 +0000

    Enable V2 authentication for Me2Me host.
    Updated both Me2me host and simple host to accept spake2_hmac auth. They still
    accept V1 authentication. Also the corresponding scripts are updated to ask
    for PIN
    
    BUG=107777
    
    
    Review URL: http://codereview.chromium.org/9270031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118860 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/host_config.cc
+++ remoting/host/host_config.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,7 @@ const char kXmppAuthTokenConfigPath[] = "xmpp_auth_token";
 const char kXmppAuthServiceConfigPath[] = "xmpp_auth_service";
 const char kHostIdConfigPath[] = "host_id";
 const char kHostNameConfigPath[] = "host_name";
+const char kHostSecretHashConfigPath[] = "host_secret_hash";
 const char kPrivateKeyConfigPath[] = "private_key";
 
 }  // namespace remoting
--- remoting/host/host_config.h
+++ remoting/host/host_config.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,6 +27,8 @@ extern const char kXmppAuthServiceConfigPath[];
 extern const char kHostIdConfigPath[];
 // Readable host name.
 extern const char kHostNameConfigPath[];
+// Hash of the host secret used for authentication.
+extern const char kHostSecretHashConfigPath[];
 // Private keys used for host authentication.
 extern const char kPrivateKeyConfigPath[];
 
