e0c66d09d2e7b52c2e2daa7c26b20898a54f612e
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-24 13:58:04 +0000

    [Refactoring] Make finish() of CodeGeneratorJS.pm empty https://bugs.webkit.org/show_bug.cgi?id=76846
    Reviewed by Tor Arne Vestbø.
    
    * bindings/scripts/CodeGeneratorJS.pm: Warning fix after r105683.
    (WriteData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -3288,7 +3288,7 @@ sub WriteData
     # Write content to file.
     print HEADER @headerContentHeader;
 
-    my @includes = ();
+    @includes = ();
     foreach my $include (keys %headerIncludes) {
         $include = "\"$include\"" unless $include =~ /^["<]/; # "
         push @includes, $include;
