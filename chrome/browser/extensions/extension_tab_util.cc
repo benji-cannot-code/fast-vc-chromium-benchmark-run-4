c26d12c21306df877e54eac8e438a0fd367094fa
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 21:57:59 +0000

    Add openerTabId to the extension API's Tab object.
    BUG=29698
    TEST=ExtensionApiTest.Tabs2
    
    Review URL: http://codereview.chromium.org/9250015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tab_util.cc
+++ chrome/browser/extensions/extension_tab_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -119,6 +119,15 @@ DictionaryValue* ExtensionTabUtil::CreateTabValue(const WebContents* contents,
   result->SetString(keys::kTitleKey, contents->GetTitle());
   result->SetBoolean(keys::kIncognitoKey,
                      contents->GetBrowserContext()->IsOffTheRecord());
+
+  if (tab_strip) {
+    content::NavigationController* opener =
+        tab_strip->GetOpenerOfTabContentsAt(tab_index);
+    if (opener) {
+      result->SetInteger(keys::kOpenerTabIdKey,
+                         ExtensionTabUtil::GetTabId(opener->GetWebContents()));
+    }
+  }
 
   if (!is_loading) {
     NavigationEntry* entry = contents->GetController().GetActiveEntry();
--- chrome/browser/extensions/extension_tabs_module_constants.cc
+++ chrome/browser/extensions/extension_tabs_module_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,6 +26,7 @@ const char kNewPositionKey[] = "newPosition";
 const char kNewWindowIdKey[] = "newWindowId";
 const char kOldPositionKey[] = "oldPosition";
 const char kOldWindowIdKey[] = "oldWindowId";
+const char kOpenerTabIdKey[] = "openerTabId";
 const char kPinnedKey[] = "pinned";
 const char kPopulateKey[] = "populate";
 const char kQualityKey[] = "quality";
