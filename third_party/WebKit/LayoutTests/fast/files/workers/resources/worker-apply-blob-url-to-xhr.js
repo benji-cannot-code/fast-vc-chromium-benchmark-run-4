0f0d30baf39dbe76bb708f6cb8045b59268760ab
levin@chromium.org <levin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-24 22:39:21 +0000

    Change reference of revokeObjectURL in worker-apply-blob-url-to-xhr.html to use webkitURL. https://bugs.webkit.org/show_bug.cgi?id=76942
    Reviewed by Jian Li.
    
    * fast/files/workers/resources/worker-apply-blob-url-to-xhr.js:
    (onmessage):
    * platform/chromium/test_expectations.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/files/workers/resources/worker-apply-blob-url-to-xhr.js
+++ third_party/WebKit/LayoutTests/fast/files/workers/resources/worker-apply-blob-url-to-xhr.js
@@ -30,7 +30,7 @@ onmessage = function(event)
     sendXMLHttpRequest("POST", fileURL);
 
     log("Test that XMLHttpRequest GET fails after the blob URL is revoked.");
-    revokeObjectURL(fileURL);
+    webkitURL.revokeObjectURL(fileURL);
     sendXMLHttpRequest("GET", fileURL);
 
     log("DONE");
