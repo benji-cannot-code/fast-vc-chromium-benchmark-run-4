66f7b086129fba381c1ce549bd38ee3aa8110168
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 16:19:36 +0000

    Fix some license headers in tools/
    Similar fixes were made to other files in tools/ as part of
    http://codereview.chromium.org/8678023.
    
    Review URL: https://chromiumcodereview.appspot.com/9187051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118844 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/playback_benchmark/common.js
+++ tools/playback_benchmark/common.js
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2010 Google Inc. All Rights Reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 /**
  * @fileoverview Classes and functions used during recording and playback.
