ba6ae2aad05da0464f72dd75ed82ddad76642c1c
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 04:33:38 +0000

    Print Preview: Changing "Print to PDF" to "Save to PDF".
    BUG=104780
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9255004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118798 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/print_header.js
+++ chrome/browser/resources/print_preview/print_header.js
@@ -51,6 +51,8 @@ cr.define('print_preview', function() {
                                 this.updatePrintButton_.bind(this));
       document.addEventListener(customEvents.PDF_GENERATION_ERROR,
                                 this.onPDFGenerationError_.bind(this));
+      document.addEventListener(customEvents.PRINTER_CAPABILITIES_UPDATED,
+                                this.onPrinterCapabilitiesUpdated_.bind(this));
     },
 
     /**
@@ -67,6 +69,16 @@ cr.define('print_preview', function() {
      */
     onPDFGenerationError_: function() {
       this.printButton_.disabled = true;
+    },
+
+    /**
+     * Executes when a |customEvents.PRINTER_CAPABILITIES_UPDATED| event occurs.
+     * @private
+     */
+    onPrinterCapabilitiesUpdated_: function() {
+      getSelectedPrinterName() == PRINT_TO_PDF ?
+          this.printButton.textContent = localStrings.getString('saveButton') :
+          this.printButton.textContent = localStrings.getString('printButton');
     },
 
     /**
