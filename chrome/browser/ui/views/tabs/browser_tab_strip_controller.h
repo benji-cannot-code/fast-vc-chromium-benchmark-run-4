7aa1829ef89b8276acca6634663bee176ccdc431
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 06:09:03 +0000

    Revert 119018 - Draw new tab button with tab background
    Currently the new tab button is drawn with a semi-transparent image blended with the window background color.
    
    This CL changes the drawing code so that we now use the inactive tab background image as the button's background. I've removed the fill from the newtab button images so that they just contain the border and shadow.
    
    The main benefit of this approach is that the button now matches the look of inactive tabs.
    
    Note, I'm only implementing this on views for now. Once this is implemented on all platforms we can remove the old button images.
    
    Screenshots: http://imgur.com/a/iez8r
    
    BUG=100775
    TEST=
    Review URL: https://chromiumcodereview.appspot.com/9252026
    
    TBR=sail@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9288015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -69,7 +69,6 @@ class BrowserTabStripController : public TabStripController,
   virtual bool IsCompatibleWith(TabStrip* other) const OVERRIDE;
   virtual void CreateNewTab() OVERRIDE;
   virtual void ClickActiveTab(int index) OVERRIDE;
-  virtual bool IsIncognito() OVERRIDE;
 
   // TabStripModelObserver implementation:
   virtual void TabInsertedAt(TabContentsWrapper* contents,
