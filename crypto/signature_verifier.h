3544c350a222079bfdb0b0115a54255a9a49fb00
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 00:56:43 +0000

    Create a new ECSignatureCreator class for signing content using EC crypto.
    Review URL: http://codereview.chromium.org/9240029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118964 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/signature_verifier.h
+++ crypto/signature_verifier.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,19 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(USE_NSS)
-#include <cryptoht.h>
-#elif defined(OS_MACOSX)
-#include <Security/cssm.h>
-#endif
-
 #include <vector>
 
 #include "base/basictypes.h"
 #include "crypto/crypto_export.h"
 
-#if defined(OS_WIN)
-#include "crypto/scoped_capi_types.h"
+#if !defined(USE_OPENSSL)
+typedef struct VFYContextStr VFYContext;
 #endif
 
 namespace crypto {
@@ -88,20 +82,8 @@ class CRYPTO_EXPORT SignatureVerifier {
 #if defined(USE_OPENSSL)
   struct VerifyContext;
   VerifyContext* verify_context_;
-#elif defined(USE_NSS)
+#else
   VFYContext* vfy_context_;
-#elif defined(OS_MACOSX)
-  std::vector<uint8> public_key_info_;
-
-  CSSM_CC_HANDLE sig_handle_;
-
-  CSSM_KEY public_key_;
-#elif defined(OS_WIN)
-  ScopedHCRYPTPROV provider_;
-
-  ScopedHCRYPTHASH hash_object_;
-
-  ScopedHCRYPTKEY public_key_;
 #endif
 };
 
