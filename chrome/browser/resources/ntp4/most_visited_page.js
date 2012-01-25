f674d3ba27829a79ed45bd74d86a0c42ddf7e2b3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 05:50:30 +0000

    [ntp] fix RTL positioning of favicons on most visited tiles
    BUG=none
    TEST=immanuel
    
    
    Review URL: http://codereview.chromium.org/9160036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119017 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/most_visited_page.js
+++ chrome/browser/resources/ntp4/most_visited_page.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -97,7 +97,6 @@ cr.define('ntp4', function() {
       var faviconDiv = this.querySelector('.favicon');
       var faviconUrl = 'chrome://favicon/size/16/' + data.url;
       faviconDiv.style.backgroundImage = url(faviconUrl);
-      faviconDiv.dir = data.direction;
       chrome.send('getFaviconDominantColor', [faviconUrl, this.id]);
 
       var title = this.querySelector('.title');
