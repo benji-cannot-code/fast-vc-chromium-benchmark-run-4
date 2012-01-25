8bb355a49f2a8f4583a284ed6a369191509254bc
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 14:29:11 +0000

    Read tsan/suppressions_win32.txt too when matching TSan/Win reports BUG=109991 TBR=glider TEST=manually crafted a "report" file in tools/valgrind/waterfall.tmp, then      ./tools/valgrind/waterfall.sh match with and without a suppression in      tools/valgrind/tsan/suppressions_win32.txt Review URL: https://chromiumcodereview.appspot.com/9235030
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119065 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/test_suppressions.py
+++ tools/valgrind/test_suppressions.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -64,6 +64,10 @@ def main(argv):
   tsan_mac = suppressions.ReadSuppressionsFromFile(supp_filename)
   mac_suppressions = vg_mac + tsan_mac
 
+  supp_filename = JOIN(suppressions_root, "tsan", "suppressions_win32.txt")
+  tsan_win = suppressions.ReadSuppressionsFromFile(supp_filename)
+  win_suppressions = tsan_win
+
   supp_filename = JOIN(suppressions_root, "..", "heapcheck", "suppressions.txt")
   heapcheck_suppressions = suppressions.ReadSuppressionsFromFile(supp_filename)
 
@@ -89,6 +93,9 @@ def main(argv):
             for url in all_reports[r]]):
       # Include mac suppressions if the report is only present on Mac
       cur_supp += mac_suppressions
+    elif all([re.search("Windows%20", url) for url in all_reports[r]]):
+      # Include win32 suppressions if the report is only present on Windows
+      cur_supp += win_suppressions
     elif all([re.search("%20Heapcheck", url)
               for url in all_reports[r]]):
       cur_supp += heapcheck_suppressions
