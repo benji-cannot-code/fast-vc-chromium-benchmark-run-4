66d818b1d0003eaae0efc76e72a8a4f0a875c99a
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 18:18:07 +0000

    Fix license info for undoview
    Gtk+ and GtkSourceView, on which this code is based, both use LGPL 2.1.
    
    Also fix some header include guards.
    
    BUG=111334
    
    
    Review URL: http://codereview.chromium.org/9288024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119089 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/undoview/undo_manager.h
+++ third_party/undoview/undo_manager.h
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef CHROME_BROWSER_AUTOCOMPLETE_UNDO_MANAGER_H_
-#define CHROME_BROWSER_AUTOCOMPLETE_UNDO_MANAGER_H_
+#ifndef UNDOVIEW_UNDO_MANAGER_H_
+#define UNDOVIEW_UNDO_MANAGER_H_
 
 #include <gtk/gtk.h>
 
@@ -83,5 +83,5 @@ void      gtk_source_undo_manager_set_max_undo_levels(GtkSourceUndoManager   *um
 
 G_END_DECLS
 
-#endif  // CHROME_BROWSER_AUTOCOMPLETE_UNDO_MANAGER_H_
+#endif  // UNDOVIEW_UNDO_MANAGER_H_
 
