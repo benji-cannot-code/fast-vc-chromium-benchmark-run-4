641495084f8f48d3642b48a46ea31a878502be9d
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-25 22:33:33 +0000

    [chromium] Refactor Clipboard invalidate for DataTransferItem/DataTransferItemList into a wrapper https://bugs.webkit.org/show_bug.cgi?id=76993
    We want to unify the backing data store for ClipboardChromium and DataTransferItems. For
    that, we want use a similar representation to DataTransferItem list inside
    ChromiumDataObject.  However, since ChromiumDataObject should be valid in scopes where
    Clipboard is not (e.g. default drag processing), we need to separate the clipboard
    invalidation logic into a wrapper class.
    
    Reviewed by Tony Chang.
    
    Covered by existing tests.
    
    * platform/chromium/ClipboardChromium.cpp:
    ():
    (WebCore::ClipboardChromium::items):
    * platform/chromium/DataTransferItemChromium.cpp:
    (WebCore::DataTransferItemChromium::getAsString):
    * platform/chromium/DataTransferItemListChromium.cpp:
    (WebCore::DataTransferItemListChromium::length):
    (WebCore::DataTransferItemListChromium::item):
    (WebCore::DataTransferItemListChromium::deleteItem):
    (WebCore::DataTransferItemListChromium::clear):
    (WebCore::DataTransferItemListChromium::add):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/DataTransferItemListChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/DataTransferItemListChromium.cpp
@@ -61,17 +61,12 @@ DataTransferItemListChromium::DataTransferItemListChromium(PassRefPtr<Clipboard>
 
 size_t DataTransferItemListChromium::length() const
 {
-    if (m_owner->policy() == ClipboardNumb)
-        return 0;
     clipboardChromium()->mayUpdateItems(m_items);
     return m_items.size();
 }
 
 PassRefPtr<DataTransferItem> DataTransferItemListChromium::item(unsigned long index)
 {
-    if (m_owner->policy() == ClipboardNumb)
-        return 0;
-
     clipboardChromium()->mayUpdateItems(m_items);
     if (index >= length())
         return 0;
@@ -80,11 +75,6 @@ PassRefPtr<DataTransferItem> DataTransferItemListChromium::item(unsigned long in
 
 void DataTransferItemListChromium::deleteItem(unsigned long index, ExceptionCode& ec)
 {
-    if (m_owner->policy() != ClipboardWritable) {
-        ec = INVALID_STATE_ERR;
-        return;
-    }
-
     clipboardChromium()->mayUpdateItems(m_items);
     if (index >= length())
         return;
@@ -117,18 +107,12 @@ void DataTransferItemListChromium::deleteItem(unsigned long index, ExceptionCode
 
 void DataTransferItemListChromium::clear()
 {
-    if (m_owner->policy() != ClipboardWritable)
-        return;
-
     m_items.clear();
     clipboardChromium()->clearAllData();
 }
 
 void DataTransferItemListChromium::add(const String& data, const String& type, ExceptionCode& ec)
 {
-    if (m_owner->policy() != ClipboardWritable)
-        return;
-
     RefPtr<ChromiumDataObject> dataObject = clipboardChromium()->dataObject();
     if (!dataObject)
         return;
@@ -147,7 +131,7 @@ void DataTransferItemListChromium::add(const String& data, const String& type, E
 
 void DataTransferItemListChromium::add(PassRefPtr<File> file)
 {
-    if (m_owner->policy() != ClipboardWritable || !file)
+    if (!file)
         return;
 
     RefPtr<ChromiumDataObject> dataObject = clipboardChromium()->dataObject();
