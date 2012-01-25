8fffe030d84ebc880530481afa27e01bb4809e78
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 01:54:40 +0000

    [Sync setup] Add "learn more" link to "Encryption Passphrase" section when passphrase is required.
    BUG=111018
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9288003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -472,10 +472,9 @@ cr.define('options', function() {
       else
         $('normal-body').hidden = false;
 
+      $('passphrase-learn-more').hidden = false;
       $('incorrect-passphrase').hidden = !args["passphrase_setting_rejected"];
-
       $('sync-passphrase-warning').hidden = false;
-
       $('passphrase').focus();
     },
 
