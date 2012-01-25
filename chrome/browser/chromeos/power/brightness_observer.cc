0284346224350682ce8fd72719299be0fcfe0883
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 10:09:46 +0000

    Add an extension API event about screen brightness change.
    BUG=chromium-os:20007
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9193005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119042 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/power/brightness_observer.cc
+++ chrome/browser/chromeos/power/brightness_observer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/ui/brightness_bubble.h"
 #include "chrome/browser/chromeos/ui/volume_bubble.h"
+#include "chrome/browser/extensions/system/system_api.h"
 
 namespace chromeos {
 
@@ -15,6 +16,8 @@ void BrightnessObserver::BrightnessChanged(int level, bool user_initiated) {
     BrightnessBubble::GetInstance()->ShowBubble(level, true);
   else
     BrightnessBubble::GetInstance()->UpdateWithoutShowingBubble(level, true);
+
+  extensions::DispatchBrightnessChangedEvent(level, user_initiated);
 
   VolumeBubble::GetInstance()->HideBubble();
 }
