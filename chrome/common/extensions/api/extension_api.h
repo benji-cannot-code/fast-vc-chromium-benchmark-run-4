f78a7fd1b9e8cc1d97ca328fad20bfa82026444e
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 04:40:15 +0000

    Add the ability to express dependencies between JSON schema files for the extension APIs by setting a "dependencies" attribute, and add a dependency for "tts" from "ttsEngine".
    BUG=110944
    TEST=unit_tests --gtest_filter=ExtensionAPI*, and as in bug
    
    Review URL: http://codereview.chromium.org/9280006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/api/extension_api.h
+++ chrome/common/extensions/api/extension_api.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,6 +79,10 @@ class ExtensionAPI {
   // Gets the schemas for the APIs that are allowed by a permission set.
   void GetSchemasForPermissions(
       const ExtensionPermissionSet& permissions, SchemaMap* out) const;
+
+  // Adds dependent schemas to |out| as determined by the "dependencies"
+  // property.
+  void ResolveDependencies(SchemaMap* out) const;
 
   static ExtensionAPI* instance_;
 
