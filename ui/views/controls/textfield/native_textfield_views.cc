77575a31dd7082536148876f84aa12222c2e3b38
benrg@chromium.org <benrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 02:07:01 +0000

    Fix tests of TextInputType in NativeTextfieldViews
    The test GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT was added when there
    were only three input types (NONE, TEXT, PASSWORD). When more input types were
    added the test should have been updated but wasn't.
    
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9146041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118980 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/native_textfield_views.cc
+++ ui/views/controls/textfield/native_textfield_views.cc
@@ -718,9 +718,7 @@ bool NativeTextfieldViews::HasCompositionText() {
 }
 
 bool NativeTextfieldViews::GetTextRange(ui::Range* range) {
-  // We don't allow the input method to retrieve or delete content from an
-  // obscured textfield.
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT)
+  if (!ImeEditingAllowed())
     return false;
 
   model_->GetTextRange(range);
@@ -728,7 +726,7 @@ bool NativeTextfieldViews::GetTextRange(ui::Range* range) {
 }
 
 bool NativeTextfieldViews::GetCompositionTextRange(ui::Range* range) {
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT)
+  if (!ImeEditingAllowed())
     return false;
 
   model_->GetCompositionTextRange(range);
@@ -736,7 +734,7 @@ bool NativeTextfieldViews::GetCompositionTextRange(ui::Range* range) {
 }
 
 bool NativeTextfieldViews::GetSelectionRange(ui::Range* range) {
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT)
+  if (!ImeEditingAllowed())
     return false;
 
   gfx::SelectionModel sel;
@@ -747,7 +745,7 @@ bool NativeTextfieldViews::GetSelectionRange(ui::Range* range) {
 }
 
 bool NativeTextfieldViews::SetSelectionRange(const ui::Range& range) {
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT || !range.IsValid())
+  if (!ImeEditingAllowed() || !range.IsValid())
     return false;
 
   OnBeforeUserAction();
@@ -757,7 +755,7 @@ bool NativeTextfieldViews::SetSelectionRange(const ui::Range& range) {
 }
 
 bool NativeTextfieldViews::DeleteRange(const ui::Range& range) {
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT || range.is_empty())
+  if (!ImeEditingAllowed() || range.is_empty())
     return false;
 
   OnBeforeUserAction();
@@ -773,7 +771,7 @@ bool NativeTextfieldViews::DeleteRange(const ui::Range& range) {
 bool NativeTextfieldViews::GetTextFromRange(
     const ui::Range& range,
     string16* text) {
-  if (GetTextInputType() != ui::TEXT_INPUT_TYPE_TEXT || !range.IsValid())
+  if (!ImeEditingAllowed() || !range.IsValid())
     return false;
 
   ui::Range text_range;
@@ -1098,6 +1096,13 @@ void NativeTextfieldViews::HandleMousePressEvent(const MouseEvent& event) {
     }
     SchedulePaint();
   }
+}
+
+bool NativeTextfieldViews::ImeEditingAllowed() const {
+  // We don't allow the input method to retrieve or delete content from a
+  // password field.
+  ui::TextInputType t = GetTextInputType();
+  return (t != ui::TEXT_INPUT_TYPE_NONE && t != ui::TEXT_INPUT_TYPE_PASSWORD);
 }
 
 // static
