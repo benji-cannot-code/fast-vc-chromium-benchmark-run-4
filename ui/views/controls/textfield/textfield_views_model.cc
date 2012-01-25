f7fbcf1783277d067bd774523f42437967be9b5c
benrg@chromium.org <benrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 00:06:05 +0000

    Remove GetVisibleText method and obscured/password bit from TextfieldViewsModel
    This code has been unused since r93855, and is going to be replaced by password
    support in RenderText. This is split off from issue 8747001.
    
    
    BUG=none
    TEST=compiles
    
    
    Review URL: http://codereview.chromium.org/9283011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118949 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/textfield_views_model.cc
+++ ui/views/controls/textfield/textfield_views_model.cc
@@ -282,7 +282,6 @@ TextfieldViewsModel::Delegate::~Delegate() {
 TextfieldViewsModel::TextfieldViewsModel(Delegate* delegate)
     : delegate_(delegate),
       render_text_(gfx::RenderText::CreateRenderText()),
-      is_obscured_(false),
       current_edit_(edit_history_.end()) {
 }
 
@@ -499,10 +498,6 @@ bool TextfieldViewsModel::Redo() {
   return old != GetText() || old_cursor != GetCursorPosition();
 }
 
-string16 TextfieldViewsModel::GetVisibleText() const {
-  return GetVisibleText(0U, GetText().length());
-}
-
 bool TextfieldViewsModel::Cut() {
   if (!HasCompositionText() && HasSelection()) {
     ui::ScopedClipboardWriter(views::ViewsDelegate::views_delegate
@@ -638,13 +633,6 @@ bool TextfieldViewsModel::HasCompositionText() const {
 
 /////////////////////////////////////////////////////////////////
 // TextfieldViewsModel: private
-
-string16 TextfieldViewsModel::GetVisibleText(size_t begin, size_t end) const {
-  DCHECK(end >= begin);
-  if (is_obscured_)
-    return string16(end - begin, '*');
-  return GetText().substr(begin, end - begin);
-}
 
 void TextfieldViewsModel::InsertTextInternal(const string16& text,
                                              bool mergeable) {
