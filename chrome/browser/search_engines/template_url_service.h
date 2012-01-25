0009f58df59f06244a3b28003392628ef4a7ec65
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 20:08:45 +0000

    Avoid inappropriately setting the safe_for_autoreplace flag to false in TemplateURL sync code.
    Added a helper method to ensure that we leave unsynced fields alone.
    
    Also made a small update to TemplateURLServiceSyncTest to ensure that this this field is now safe from the Sync code.
    
    BUG=111095
    TEST=Ensure that unit test TemplateURLServiceSyncTest.ResolveSyncKeywordConflict passes.
    
    Review URL: http://codereview.chromium.org/9160003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119107 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_service.h
+++ chrome/browser/search_engines/template_url_service.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -507,6 +507,11 @@ class TemplateURLService : public WebDataServiceConsumer,
   // and database copies have valid sync_guids. This is to fix crbug.com/102038,
   // where old entries were being pushed to Sync without a sync_guid.
   void PatchMissingSyncGUIDs(std::vector<TemplateURL*>* template_urls);
+
+  // Overwrites |dst|'s synced fields with values from |sync_data|. This does
+  // commit |dst| to the TemplateURLService.
+  static void UpdateTemplateURLWithSyncData(TemplateURL* dst,
+                                            const SyncData& sync_data);
 
   content::NotificationRegistrar registrar_;
 
