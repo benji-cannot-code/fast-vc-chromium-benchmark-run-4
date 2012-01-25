231838491bcc59da4e7b45523747af740b341c36
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-25 17:18:29 +0000

    Remove unnecessary member variable from PluginView. https://bugs.webkit.org/show_bug.cgi?id=76909
    Patch by Kaustubh Atrawalkar <kaustubh@motorola.com> on 2012-01-25
    Reviewed by Adam Roben.
    
    No new tests needed.
    
    * plugins/PluginView.cpp:
    (WebCore::PluginView::getURLNotify):
    (WebCore::PluginView::getURL):
    (WebCore::PluginView::PluginView):
    (WebCore::PluginView::handlePost):
    (WebCore::PluginView::getValueForURL):
    (WebCore::PluginView::setValueForURL):
    * plugins/PluginView.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/PluginView.cpp
+++ third_party/WebKit/Source/WebCore/plugins/PluginView.cpp
@@ -563,7 +563,7 @@ NPError PluginView::getURLNotify(const char* url, const char* target, void* noti
 
     frameLoadRequest.setFrameName(target);
     frameLoadRequest.resourceRequest().setHTTPMethod("GET");
-    frameLoadRequest.resourceRequest().setURL(makeURL(m_baseURL, url));
+    frameLoadRequest.resourceRequest().setURL(makeURL(m_parentFrame->document()->baseURL(), url));
 
     return load(frameLoadRequest, true, notifyData);
 }
@@ -574,7 +574,7 @@ NPError PluginView::getURL(const char* url, const char* target)
 
     frameLoadRequest.setFrameName(target);
     frameLoadRequest.resourceRequest().setHTTPMethod("GET");
-    frameLoadRequest.resourceRequest().setURL(makeURL(m_baseURL, url));
+    frameLoadRequest.resourceRequest().setURL(makeURL(m_parentFrame->document()->baseURL(), url));
 
     return load(frameLoadRequest, false, 0);
 }
@@ -820,7 +820,6 @@ PluginView::PluginView(Frame* parentFrame, const IntSize& size, PluginPackage* p
     , m_element(element)
     , m_isStarted(false)
     , m_url(url)
-    , m_baseURL(m_parentFrame->document()->baseURL()) // FIXME: No need for this member variable!
     , m_status(PluginStatusLoadedSuccessfully)
     , m_requestTimer(this, &PluginView::requestTimerFired)
     , m_invalidateTimer(this, &PluginView::invalidateTimerFired)
@@ -1201,7 +1200,7 @@ NPError PluginView::handlePost(const char* url, const char* target, uint32_t len
     }
 
     frameLoadRequest.resourceRequest().setHTTPMethod("POST");
-    frameLoadRequest.resourceRequest().setURL(makeURL(m_baseURL, url));
+    frameLoadRequest.resourceRequest().setURL(makeURL(m_parentFrame->document()->baseURL(), url));
     frameLoadRequest.resourceRequest().addHTTPHeaderFields(headerFields);
     frameLoadRequest.resourceRequest().setHTTPBody(FormData::create(postData, postDataLength));
     frameLoadRequest.setFrameName(target);
@@ -1396,7 +1395,7 @@ NPError PluginView::getValueForURL(NPNURLVariable variable, const char* url, cha
 
     switch (variable) {
     case NPNURLVCookie: {
-        KURL u(m_baseURL, url);
+        KURL u(m_parentFrame->document()->baseURL(), url);
         if (u.isValid()) {
             Frame* frame = getFrame(parentFrame(), m_element);
             if (frame) {
@@ -1418,7 +1417,7 @@ NPError PluginView::getValueForURL(NPNURLVariable variable, const char* url, cha
         break;
     }
     case NPNURLVProxy: {
-        KURL u(m_baseURL, url);
+        KURL u(m_parentFrame->document()->baseURL(), url);
         if (u.isValid()) {
             Frame* frame = getFrame(parentFrame(), m_element);
             const FrameLoader* frameLoader = frame ? frame->loader() : 0;
@@ -1457,7 +1456,7 @@ NPError PluginView::setValueForURL(NPNURLVariable variable, const char* url, con
 
     switch (variable) {
     case NPNURLVCookie: {
-        KURL u(m_baseURL, url);
+        KURL u(m_parentFrame->document()->baseURL(), url);
         if (u.isValid()) {
             const String cookieStr = String::fromUTF8(value, len);
             Frame* frame = getFrame(parentFrame(), m_element);
