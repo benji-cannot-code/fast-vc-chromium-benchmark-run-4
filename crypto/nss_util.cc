d60a196fd8653b67879c3aa2389f3a727f3f917b
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 04:37:51 +0000

    CertDatabaseNSSTest: Don't delete test DB dir since we don't close the DB (broke in r108543).
    Refactor test DB code so that nss_util owns the test DB dir.
    
    Keeping the test DB dir until exit prevents later tests that would use the test DB from failing.  The dir will still be deleted by the LazyInstance atexit handler.
    
    BUG=108748
    TEST=see bug
    
    
    Review URL: http://codereview.chromium.org/9255034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119003 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/nss_util.cc
+++ crypto/nss_util.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/native_library.h"
+#include "base/scoped_temp_dir.h"
 #include "base/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
@@ -221,6 +222,11 @@ class NSPRInitSingleton {
 base::LazyInstance<NSPRInitSingleton>::Leaky
     g_nspr_singleton = LAZY_INSTANCE_INITIALIZER;
 
+// This is a LazyInstance so that it will be deleted automatically when the
+// unittest exits.  NSSInitSingleton is a LeakySingleton, so it would not be
+// deleted if it were a regular member.
+base::LazyInstance<ScopedTempDir> g_test_nss_db_dir = LAZY_INSTANCE_INITIALIZER;
+
 class NSSInitSingleton {
  public:
 #if defined(OS_CHROMEOS)
@@ -344,8 +350,12 @@ class NSSInitSingleton {
 #endif  // defined(OS_CHROMEOS)
 
 
-  bool OpenTestNSSDB(const FilePath& path, const char* description) {
-    test_slot_ = OpenUserDB(path, description);
+  bool OpenTestNSSDB() {
+    if (test_slot_)
+      return true;
+    if (!g_test_nss_db_dir.Get().CreateUniqueTempDir())
+      return false;
+    test_slot_ = OpenUserDB(g_test_nss_db_dir.Get().path(), "Test DB");
     return !!test_slot_;
   }
 
@@ -356,6 +366,7 @@ class NSSInitSingleton {
         PLOG(ERROR) << "SECMOD_CloseUserDB failed: " << PORT_GetError();
       PK11_FreeSlot(test_slot_);
       test_slot_ = NULL;
+      ignore_result(g_test_nss_db_dir.Get().Delete());
     }
   }
 
@@ -699,12 +710,8 @@ bool CheckNSSVersion(const char* version) {
 }
 
 #if defined(USE_NSS)
-bool OpenTestNSSDB(const FilePath& path, const char* description) {
-  return g_nss_singleton.Get().OpenTestNSSDB(path, description);
-}
-
-void CloseTestNSSDB() {
-  g_nss_singleton.Get().CloseTestNSSDB();
+bool OpenTestNSSDB() {
+  return g_nss_singleton.Get().OpenTestNSSDB();
 }
 
 base::Lock* GetNSSWriteLock() {
