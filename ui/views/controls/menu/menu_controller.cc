747e6ae27bf4b11023fb2a9e146e2975d27f2117
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 04:29:25 +0000

    [Coverity] Fixed uninit member vars
    CID=1367,4395
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9148080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118996 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -147,7 +147,7 @@ static View* GetNextFocusableView(View* ancestor,
 
 class MenuController::MenuScrollTask {
  public:
-  MenuScrollTask() : submenu_(NULL) {
+  MenuScrollTask() : submenu_(NULL), is_scrolling_up_(false), start_y_(0) {
     pixels_per_second_ = MenuItemView::pref_menu_height() * 20;
   }
 
@@ -245,7 +245,10 @@ struct MenuController::SelectByCharDetails {
 
 // MenuController:State ------------------------------------------------------
 
-MenuController::State::State() : item(NULL), submenu_open(false) {}
+MenuController::State::State()
+    : item(NULL),
+      submenu_open(false),
+      anchor(views::MenuItemView::TOPLEFT) {}
 
 MenuController::State::~State() {}
 
