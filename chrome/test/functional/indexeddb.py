3d9e941be15f8f9f1d2911aa7c7febc2f51765d5
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 19:57:46 +0000

    IndexedDB: Ensure database connections are closed on tab close.
    BUG=80111
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9121016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119104 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/indexeddb.py
+++ chrome/test/functional/indexeddb.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -59,6 +59,28 @@ class IndexedDBTest(pyauto.PyUITest):
     self.assertTrue(self.WaitUntil(self.GetActiveTabTitle,
                                    expect_retval='pass - part3 - rolled back'),
                     msg='VERSION_CHANGE not completely aborted')
+
+  def testConnectionsClosedOnTabClose(self):
+    """Verify that open DB connections are closed when a tab is destroyed."""
+
+    url = self.GetHttpURLForDataPath('indexeddb', 'version_change_blocked.html')
+
+    self.NavigateToURL(url + '#tab1')
+    pid = self.GetBrowserInfo()['windows'][0]['tabs'][0]['renderer_pid']
+    self.assertTrue(self.WaitUntil(self.GetActiveTabTitle,
+                                   expect_retval='setVersion(1) complete'),
+                    msg='Version change failed')
+
+    # Start to a different URL to force a new renderer process
+    self.AppendTab(pyauto.GURL('about:blank'))
+    self.NavigateToURL(url + '#tab2')
+    self.assertTrue(self.WaitUntil(self.GetActiveTabTitle,
+                                   expect_retval='setVersion(2) blocked'),
+                    msg='Version change not blocked as expected')
+    self.KillRendererProcess(pid)
+    self.assertTrue(self.WaitUntil(self.GetActiveTabTitle,
+                                    expect_retval='setVersion(2) complete'),
+                     msg='Version change never unblocked')
 
 if __name__ == '__main__':
   pyauto_functional.Main()
