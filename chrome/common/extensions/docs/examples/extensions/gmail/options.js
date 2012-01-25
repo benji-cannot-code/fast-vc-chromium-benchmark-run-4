7ed9df2cfcf81bbde4c8a8903f711eed9451b2e1
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 21:19:17 +0000

    Updating the Gmail sample extension to manifest version 2.
    BUG=111049
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9146040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119117 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/extensions/gmail/options.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var customDomainsTextbox;
+var saveButton;
+
+function init() {
+  customDomainsTextbox = document.getElementById("custom-domain");
+  saveButton = document.getElementById("save-button");
+
+  customDomainsTextbox.value = localStorage.customDomain || "";
+  markClean();
+}
+
+function save() {
+  localStorage.customDomain = customDomainsTextbox.value;
+  markClean();
+
+  chrome.extension.getBackgroundPage().init();
+}
+
+function markDirty() {
+  saveButton.disabled = false;
+}
+
+function markClean() {
+  saveButton.disabled = true;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
+  document.querySelector('#cancel-button').addEventListener('DOMContentLoaded',
+                                                            init);
+  document.querySelector('#save-button').addEventListener('DOMContentLoaded',
+                                                          save);
+});
