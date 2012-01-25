521086b84330ea5cae57a30e60c23807167313d6
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 18:05:11 +0000

    Disallow WebSQL and localStorage in platform apps.
    BUG=none
    TEST=PlatformAppBrowserTest.DisallowStorage
    
    Review URL: http://codereview.chromium.org/9192021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119088 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/storage/main.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function testOpenDatabase() {
+    chrome.test.assertTrue(!window.openDatabase);
+    chrome.test.succeed();
+  },
+
+  function testOpenDatabaseSync() {
+    chrome.test.assertTrue(!window.openDatabaseSync);
+    chrome.test.succeed();
+  },
+
+  function testLocalStorage() {
+    chrome.test.assertTrue(!window.localStorage);
+    chrome.test.succeed();
+  }
+]);
