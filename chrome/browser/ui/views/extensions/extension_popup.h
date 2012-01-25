049d49aa7918cbc25e1e8b1796c33e44857fa827
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 00:55:08 +0000

    Tweak Win ExtensionPopup::OnNativeFocusChange logic. Close even if focused_before is not the popup window. Fixes non-aura Windows ExtensionPopup close logic added in crrev.com/113936.
    BUG=106723,106958,110600
    TEST=Open, click on, then click away from an extension popup, expect to close.
    
    Review URL: http://codereview.chromium.org/9193018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118963 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_popup.h
+++ chrome/browser/ui/views/extensions/extension_popup.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -72,6 +72,9 @@ class ExtensionPopup : public views::BubbleDelegateView,
                  views::View* anchor_view,
                  views::BubbleBorder::ArrowLocation arrow_location,
                  bool inspect_with_devtools);
+
+  // Show the bubble, focus on its content, and register listeners.
+  void ShowBubble();
 
   // The contained host for the view.
   scoped_ptr<ExtensionHost> extension_host_;
