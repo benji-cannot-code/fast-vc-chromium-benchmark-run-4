8f54e9594a83e40884a64556401e7930c24c9628
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 05:44:08 +0000

    Reenable tests that were crashing due to a bad Vista Tests (3) bot.
    BUG=108724,106381,108429,108721,109238,109385,109738
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9289010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119014 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_file_util_unittest.cc
+++ chrome/common/extensions/extension_file_util_unittest.cc
@@ -20,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace keys = extension_manifest_keys;
 
-#if defined(OS_WIN)
-// http://crbug.com/106381
-#define InstallUninstallGarbageCollect DISABLED_InstallUninstallGarbageCollect
-#endif
 TEST(ExtensionFileUtil, InstallUninstallGarbageCollect) {
   ScopedTempDir temp;
   ASSERT_TRUE(temp.CreateUniqueTempDir());
@@ -109,11 +105,6 @@ TEST(ExtensionFileUtil, LoadExtensionWithoutLocalesFolder) {
   EXPECT_TRUE(error.empty());
 }
 
-#if defined(OS_WIN)
-// http://crbug.com/106381
-#define CheckIllegalFilenamesNoUnderscores \
-    DISABLED_CheckIllegalFilenamesNoUnderscores
-#endif
 TEST(ExtensionFileUtil, CheckIllegalFilenamesNoUnderscores) {
   ScopedTempDir temp;
   ASSERT_TRUE(temp.CreateUniqueTempDir());
@@ -129,11 +120,6 @@ TEST(ExtensionFileUtil, CheckIllegalFilenamesNoUnderscores) {
                                                             &error));
 }
 
-#if defined(OS_WIN)
-// http://crbug.com/106381
-#define CheckIllegalFilenamesOnlyReserved \
-    DISABLED_CheckIllegalFilenamesOnlyReserved
-#endif
 TEST(ExtensionFileUtil, CheckIllegalFilenamesOnlyReserved) {
   ScopedTempDir temp;
   ASSERT_TRUE(temp.CreateUniqueTempDir());
@@ -146,11 +132,6 @@ TEST(ExtensionFileUtil, CheckIllegalFilenamesOnlyReserved) {
                                                             &error));
 }
 
-#if defined(OS_WIN)
-// http://crbug.com/106381
-#define CheckIllegalFilenamesReservedAndIllegal \
-    DISABLED_CheckIllegalFilenamesReservedAndIllegal
-#endif
 TEST(ExtensionFileUtil, CheckIllegalFilenamesReservedAndIllegal) {
   ScopedTempDir temp;
   ASSERT_TRUE(temp.CreateUniqueTempDir());
