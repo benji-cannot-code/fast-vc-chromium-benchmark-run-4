5e64972c21691f25dd8896617ce4324386edd3cd
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 21:22:58 +0000

    Coverity: pass string by const-ref
    CID=102845
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9292001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_helper.h
+++ chrome/renderer/extensions/extension_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,7 +49,7 @@ class ExtensionHelper
                                                 string16* error);
 
   void InlineWebstoreInstall(int install_id,
-                             std::string webstore_item_id,
+                             const std::string& webstore_item_id,
                              const GURL& requestor_url);
 
   int browser_window_id() const { return browser_window_id_; }
