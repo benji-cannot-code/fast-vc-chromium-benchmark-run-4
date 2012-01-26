e8020f59895523afd26ccf65d47d8515069b15b4
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 00:32:05 +0000

    [Coverity] Another UNINIT_CTOR...
    CID=102809
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9289026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119154 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/in_flight_backend_io.cc
+++ net/disk_cache/in_flight_backend_io.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,7 +21,12 @@ BackendIO::BackendIO(InFlightIO* controller, BackendImpl* backend,
     : BackgroundIO(controller),
       backend_(backend),
       callback_(callback),
-      operation_(OP_NONE) {
+      operation_(OP_NONE),
+      entry_ptr_(NULL),
+      iter_ptr_(NULL),
+      iter_(NULL),
+      entry_(NULL),
+      start_(NULL) {
   start_time_ = base::TimeTicks::Now();
 }
 
