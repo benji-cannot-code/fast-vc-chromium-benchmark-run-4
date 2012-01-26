f12cab2baf263a485622d430ca6dde3ba9e54bbb
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 19:51:30 +0000

    Animation observers must opt in to get notified when the animator is destroyed.
    With this change, animation observers will not care about getting NotifyAborted notifications when the animator is destroyed, unless they explicitly opt into this behavior. If an observer does decide to override RequiresNotificationWhenAnimatorDestroyed to return true, it must also ensure that it removes itself as an observer before it is destroyed. There is a strongly worded comment to this effect over RequiresNotificationWhenAnimatorDestroyed.
    
    BUG=111362
    TEST=Verify fix for issue 111362
    
    Review URL: http://codereview.chromium.org/9169087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119271 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/test/test_layer_animation_observer.cc
+++ ui/gfx/compositor/test/test_layer_animation_observer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,7 +12,8 @@ namespace ui {
 TestLayerAnimationObserver::TestLayerAnimationObserver()
     : last_ended_sequence_(NULL),
       last_scheduled_sequence_(NULL),
-      last_aborted_sequence_(NULL) {
+      last_aborted_sequence_(NULL),
+      requires_notification_when_animator_destroyed_(false) {
 }
 
 TestLayerAnimationObserver::~TestLayerAnimationObserver() {
@@ -23,14 +24,19 @@ void TestLayerAnimationObserver::OnLayerAnimationEnded(
   last_ended_sequence_ = sequence;
 }
 
+void TestLayerAnimationObserver::OnLayerAnimationAborted(
+    const LayerAnimationSequence* sequence) {
+  last_aborted_sequence_ = sequence;
+}
+
 void TestLayerAnimationObserver::OnLayerAnimationScheduled(
     const LayerAnimationSequence* sequence) {
   last_scheduled_sequence_ = sequence;
 }
 
-void TestLayerAnimationObserver::OnLayerAnimationAborted(
-    const LayerAnimationSequence* sequence) {
-  last_aborted_sequence_ = sequence;
+bool
+TestLayerAnimationObserver::RequiresNotificationWhenAnimatorDestroyed() const {
+  return requires_notification_when_animator_destroyed_;
 }
 
 }  // namespace ui
