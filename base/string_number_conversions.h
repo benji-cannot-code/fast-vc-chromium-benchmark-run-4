bfc8fb2aad9fb657dcb184a9b092175901aaeeaf
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 00:27:29 +0000

    Add StringToUint and StringToUint64 methods to base/string_number_conversions.h.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9288012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119150 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string_number_conversions.h
+++ base/string_number_conversions.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -62,8 +62,15 @@ BASE_EXPORT std::string DoubleToString(double value);
 //  - Empty string.  |*output| will be set to 0.
 BASE_EXPORT bool StringToInt(const StringPiece& input, int* output);
 BASE_EXPORT bool StringToInt(const StringPiece16& input, int* output);
+
+BASE_EXPORT bool StringToUint(const StringPiece& input, unsigned* output);
+BASE_EXPORT bool StringToUint(const StringPiece16& input, unsigned* output);
+
 BASE_EXPORT bool StringToInt64(const StringPiece& input, int64* output);
 BASE_EXPORT bool StringToInt64(const StringPiece16& input, int64* output);
+
+BASE_EXPORT bool StringToUint64(const StringPiece& input, uint64* output);
+BASE_EXPORT bool StringToUint64(const StringPiece16& input, uint64* output);
 
 // For floating-point conversions, only conversions of input strings in decimal
 // form are defined to work.  Behavior with strings representing floating-point
