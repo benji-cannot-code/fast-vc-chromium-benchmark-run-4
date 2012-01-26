9965f87154cc717ad73cf7708443531e23b5c074
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 09:00:09 +0000

    Disable MouseLeaveTest.TestOnMouseOut on Chrome OS Aura.
    The test is failing on Chrome OS Aura build, so this CL disables it for now.
    
    BUG=111086
    TEST=interactive_ui_tests succeeds
    
    
    Review URL: http://codereview.chromium.org/9234057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/mouseleave_interactive_uitest.cc
+++ chrome/browser/mouseleave_interactive_uitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,8 +27,9 @@ class MouseLeaveTest : public UITest {
   DISALLOW_COPY_AND_ASSIGN(MouseLeaveTest);
 };
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || (defined(OS_CHROMEOS) && defined(USE_AURA))
 // Missing automation provider support: http://crbug.com/45892
+// Failing on Chrome OS Aura: http://crbug.com/111086
 #define MAYBE_TestOnMouseOut DISABLED_TestOnMouseOut
 #else
 #define MAYBE_TestOnMouseOut TestOnMouseOut
