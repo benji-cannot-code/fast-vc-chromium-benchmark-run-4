fd13542e35d4d0e06d47f137c22a5e3b08c4b3e1
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-26 00:48:04 +0000

    Attempted Mac build fix after r105939.
    * runtime/DatePrototype.cpp: Don't #include unicode/udat.h on Mac or iOS.
    It isn't used on these platforms and isn't available in the ICU headers
    for Mac.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/DatePrototype.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/DatePrototype.cpp
@@ -63,9 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(MAC) || PLATFORM(IOS)
 #include <CoreFoundation/CoreFoundation.h>
-#endif
-
-#if USE(ICU_UNICODE)
+#elif USE(ICU_UNICODE)
 #include <unicode/udat.h>
 #endif
 
