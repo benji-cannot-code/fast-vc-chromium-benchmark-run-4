87da26bb55b51270adf9c029b8bf9d99a69798f7
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 08:40:29 +0000

    Serve sample extension code as `text/plain` for all branches.
    BUG=107548
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9234044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119212 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server/chromeextensionsdocs.py
+++ chrome/common/extensions/docs/server/chromeextensionsdocs.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -80,7 +80,11 @@ class MainPage(webapp.RequestHandler):
         # Files inside of samples should be rendered with content-type
         # text/plain so that their source is visible when linked to. The only
         # types we should serve as-is are images.
-        if (path.startswith("/examples") and
+        if ((path.startswith("/examples") or
+             path.startswith("/stable/examples") or
+             path.startswith("/beta/examples") or
+             path.startswith("/dev/examples") or
+             path.startswith("/trunk/examples")) and
             not (result.headers['content-type'].startswith('image/') or
                  result.headers['Content-Type'].startswith('image/'))):
           result.headers['content-type'] = 'text/plain'
