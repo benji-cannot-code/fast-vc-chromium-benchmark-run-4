05c1c6a1b8e4b37e1bbf68ccd336c8830b8c7ee9
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-26 08:17:47 +0000

    REGRESSION (r105555): Incorrect use of OS() macro breaks OwnPtr when used with Win32 data types https://bugs.webkit.org/show_bug.cgi?id=77073
    Reviewed by Ryosuke Niwa.
    
    r105555 changed PLATFORM(WIN) to OS(WIN), but WTF_OS_WIN isn't defined.
    This should have been changed to OS(WINDOWS). This causes the
    preprocessor to strip out Win32 data type overrides for deleteOwnedPtr,
    causing allocations made by Win32 to be deleted by fastmalloc.
    
    * wtf/OwnPtrCommon.h:
    (WTF): Use OS(WINDOWS) instead of OS(WIN).
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@105970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/OwnPtrCommon.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/OwnPtrCommon.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_OwnPtrCommon_h
 #define WTF_OwnPtrCommon_h
 
-#if OS(WIN)
+#if OS(WINDOWS)
 typedef struct HBITMAP__* HBITMAP;
 typedef struct HBRUSH__* HBRUSH;
 typedef struct HDC__* HDC;
@@ -55,7 +55,7 @@ namespace WTF {
             delete ptr;
     }
 
-#if OS(WIN)
+#if OS(WINDOWS)
     void deleteOwnedPtr(HBITMAP);
     void deleteOwnedPtr(HBRUSH);
     void deleteOwnedPtr(HDC);
