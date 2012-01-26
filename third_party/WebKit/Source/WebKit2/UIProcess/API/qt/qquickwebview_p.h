83fd35b31d322e4ccc7ffe33a833ded816de5a75
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-26 19:47:53 +0000

    [Qt][WK2] Add the componentComplete method to WebView https://bugs.webkit.org/show_bug.cgi?id=77111
    Reviewed by Kenneth Rohde Christiansen.
    
    Move the touch/desktop initialization to componentComplete.
    Also return valid default values in the viewport info functions
    if the interaction engine is not yet initialized because the
    viewport info component could finish initialization earlier
    than the WebView, which results in QML warnings during MiniBrowser
    startup.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::setUseTraditionalDesktopBehaviour):
    (QQuickWebView::geometryChanged):
    (QQuickWebView::componentComplete):
    * UIProcess/API/qt/qquickwebview_p.h:
    * UIProcess/API/qt/qwebviewportinfo.cpp:
    (QWebViewportInfo::currentScale):
    (QWebViewportInfo::devicePixelRatio):
    (QWebViewportInfo::initialScale):
    (QWebViewportInfo::minimumScale):
    (QWebViewportInfo::maximumScale):
    (QWebViewportInfo::isScalable):
    (QWebViewportInfo::layoutSize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106022 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
@@ -136,7 +136,7 @@ Q_SIGNALS:
 
 protected:
     virtual void geometryChanged(const QRectF&, const QRectF&);
-
+    virtual void componentComplete();
     virtual void keyPressEvent(QKeyEvent*);
     virtual void keyReleaseEvent(QKeyEvent*);
     virtual void inputMethodEvent(QInputMethodEvent*);
