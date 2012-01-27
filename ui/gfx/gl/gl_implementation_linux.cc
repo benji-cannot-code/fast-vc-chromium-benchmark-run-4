4ef298332f9a3d834adebd141642b0484102ec47
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 18:46:22 +0000

    Revert 119457 - --test-gl-lib for GpuPixelBrowserTests
    This CL causes the GpuPixelBrowserTests to load libllvmpipe.so (if it is available) and fallback to standard libGL.so otherwise. This is so that we can (ultimately) do a smoke test of the GPU accelerated display path on the Aura dbg bot on the Chromium waterfall.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9169100
    
    TBR=backer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9225029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119468 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/gl/gl_implementation_linux.cc
+++ ui/gfx/gl/gl_implementation_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "ui/gfx/gl/gl_bindings.h"
 #include "ui/gfx/gl/gl_implementation.h"
-#include "ui/gfx/gl/gl_switches.h"
 
 namespace gfx {
 namespace {
@@ -71,8 +70,6 @@ bool InitializeGLBindings(GLImplementation implementation) {
   // one-time initialization cost is small, between 2 and 5 ms.
   base::ThreadRestrictions::ScopedAllowIO allow_io;
 
-  const CommandLine* command_line = CommandLine::ForCurrentProcess();
-
   switch (implementation) {
 #if !defined(USE_WAYLAND)
     case kGLImplementationOSMesaGL: {
@@ -106,19 +103,11 @@ bool InitializeGLBindings(GLImplementation implementation) {
       break;
     }
     case kGLImplementationDesktopGL: {
-      base::NativeLibrary library = NULL;
-      if (command_line->HasSwitch(switches::kTestGLLib))
-        library = LoadLibrary(command_line->GetSwitchValueASCII(
-            switches::kTestGLLib).c_str());
-
-      if (!library) {
 #if defined(OS_OPENBSD)
-        library = LoadLibrary("libGL.so");
+      base::NativeLibrary library = LoadLibrary("libGL.so");
 #else
-        library = LoadLibrary("libGL.so.1");
+      base::NativeLibrary library = LoadLibrary("libGL.so.1");
 #endif
-      }
-
       if (!library)
         return false;
 
