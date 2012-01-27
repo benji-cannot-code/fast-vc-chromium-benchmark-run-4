09cf6556d9dfd67bc3d7bb308e46c133a6aa8dc9
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 06:11:51 +0000

    Toward an android build of sync_unit_tests: OWNERS=dhollowa
    BUG=None
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9169064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/personal_data_manager.cc
+++ chrome/browser/autofill/personal_data_manager.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -229,7 +229,6 @@ bool PersonalDataManager::ImportFormData(
   // Parse the form and construct a profile based on the information that is
   // possible to import.
   int importable_credit_card_fields = 0;
-  std::vector<const FormStructure*>::const_iterator iter;
 
   // Detect and discard forms with multiple fields of the same type.
   std::set<AutofillFieldType> types_seen;
