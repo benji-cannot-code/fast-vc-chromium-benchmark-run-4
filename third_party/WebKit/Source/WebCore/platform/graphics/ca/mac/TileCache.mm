b5e78769bf10e5c1ca8b52365632c6e988f9a041
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 23:34:01 +0000

    WebTileLayers should honor the acceleratesDrawing flag https://bugs.webkit.org/show_bug.cgi?id=77242 <rdar://problem/10622128>
    Reviewed by Dan Bernstein.
    
    * platform/graphics/ca/mac/TileCache.h:
    (WebCore::TileCache::acceleratesDrawing):
    Add getter.
    
    * platform/graphics/ca/mac/TileCache.mm:
    (WebCore::TileCache::TileCache):
    Initialize m_acceleratesDrawing.
    
    (WebCore::TileCache::setAcceleratesDrawing):
    Set m_acceleratesDrawing and go through all tile layers and update the flag.
    
    (WebCore::TileCache::createTileLayer):
    Call -[CALayer setAcceleratesDrawing:] on the newly created layer.
    
    * platform/graphics/ca/mac/WebTileCacheLayer.mm:
    (-[WebTileCacheLayer setAcceleratesDrawing:]):
    (-[WebTileCacheLayer acceleratesDrawing]):
    Call through to the TileCache object.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.mm
@@ -36,6 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace std;
 
+#if !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+@interface CALayer (WebCALayerDetails)
+- (void)setAcceleratesDrawing:(BOOL)flag;
+@end
+#endif
+
 namespace WebCore {
 
 PassOwnPtr<TileCache> TileCache::create(WebTileCacheLayer* tileCacheLayer, const IntSize& tileSize)
@@ -47,6 +53,7 @@ TileCache::TileCache(WebTileCacheLayer* tileCacheLayer, const IntSize& tileSize)
     : m_tileCacheLayer(tileCacheLayer)
     , m_tileSize(tileSize)
     , m_tileContainerLayer(adoptCF([[CALayer alloc] init]))
+    , m_acceleratesDrawing(false)
 {
     [CATransaction begin];
     [CATransaction setDisableActions:YES];
@@ -103,6 +110,21 @@ void TileCache::drawLayer(WebTileLayer* layer, CGContextRef context)
     drawLayerContents(context, layer, platformLayer);
 
     CGContextRestoreGState(context);
+}
+
+void TileCache::setAcceleratesDrawing(bool acceleratesDrawing)
+{
+#if !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+    if (acceleratesDrawing == m_acceleratesDrawing)
+        return;
+
+    m_acceleratesDrawing = acceleratesDrawing;
+
+    for (WebTileLayer* tileLayer in [m_tileContainerLayer.get() sublayers])
+        [tileLayer setAcceleratesDrawing:m_acceleratesDrawing];
+#else
+    UNUSED_PARAM(acceleratesDrawing);
+#endif
 }
 
 IntRect TileCache::bounds() const
@@ -172,6 +194,10 @@ RetainPtr<WebTileLayer> TileCache::createTileLayer()
     [layer.get() setBounds:CGRectMake(0, 0, m_tileSize.width(), m_tileSize.height())];
     [layer.get() setAnchorPoint:CGPointZero];
     [layer.get() setTileCache:this];
+
+#if !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+    [layer.get() setAcceleratesDrawing:m_acceleratesDrawing];
+#endif
 
     return layer;
 }
