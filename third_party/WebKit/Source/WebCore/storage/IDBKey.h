09864753fffe4e0d95f8eee6e0c636251694fcca
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 00:07:11 +0000

    IndexedDB: WebCore::IDBKey objects are leaked https://bugs.webkit.org/show_bug.cgi?id=77114
    Reviewed by Tony Chang.
    
    Fixing memory leaks, no new tests.
    
    * storage/IDBKey.h:
    (WebCore::IDBKey::createInvalid):
    (WebCore::IDBKey::createNumber):
    (WebCore::IDBKey::createString):
    (WebCore::IDBKey::createDate):
    (WebCore::IDBKey::createArray):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/IDBKey.h
+++ third_party/WebKit/Source/WebCore/storage/IDBKey.h
@@ -42,14 +42,14 @@ public:
 
     static PassRefPtr<IDBKey> createInvalid()
     {
-        RefPtr<IDBKey> idbKey(new IDBKey());
+        RefPtr<IDBKey> idbKey = adoptRef(new IDBKey());
         idbKey->m_type = InvalidType;
         return idbKey.release();
     }
 
     static PassRefPtr<IDBKey> createNumber(double number)
     {
-        RefPtr<IDBKey> idbKey(new IDBKey());
+        RefPtr<IDBKey> idbKey = adoptRef(new IDBKey());
         idbKey->m_type = NumberType;
         idbKey->m_number = number;
         idbKey->m_sizeEstimate += sizeof(double);
@@ -58,7 +58,7 @@ public:
 
     static PassRefPtr<IDBKey> createString(const String& string)
     {
-        RefPtr<IDBKey> idbKey(new IDBKey());
+        RefPtr<IDBKey> idbKey = adoptRef(new IDBKey());
         idbKey->m_type = StringType;
         idbKey->m_string = string;
         idbKey->m_sizeEstimate += string.length() * sizeof(UChar);
@@ -67,7 +67,7 @@ public:
 
     static PassRefPtr<IDBKey> createDate(double date)
     {
-        RefPtr<IDBKey> idbKey(new IDBKey());
+        RefPtr<IDBKey> idbKey = adoptRef(new IDBKey());
         idbKey->m_type = DateType;
         idbKey->m_date = date;
         idbKey->m_sizeEstimate += sizeof(double);
@@ -76,7 +76,7 @@ public:
 
     static PassRefPtr<IDBKey> createArray(const KeyArray& array)
     {
-        RefPtr<IDBKey> idbKey(new IDBKey());
+        RefPtr<IDBKey> idbKey = adoptRef(new IDBKey());
         idbKey->m_type = ArrayType;
         idbKey->m_array = array;
 
