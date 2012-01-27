acaaf2787fccafc9cd2359a8d9b19400c3b0f158
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 06:07:25 +0000

    WebSocket Pepper API: Remove binary type handling interfaces
    In an API discussion, we decide to remove binary type handling interfaces
    from the first release. This change just fix the interface. The change to
    make the interface out of dev will follow this change.
    
    BUG=87310
    TEST=ui_test --gtest_filter='*WebSocket*'
    
    Review URL: https://chromiumcodereview.appspot.com/9296001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119382 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/thunk/ppb_websocket_api.h
+++ ppapi/thunk/ppb_websocket_api.h
@@ -34,8 +34,6 @@ class PPB_WebSocket_API {
   virtual PP_Var GetProtocol() = 0;
   virtual PP_WebSocketReadyState_Dev GetReadyState() = 0;
   virtual PP_Var GetURL() = 0;
-  virtual PP_Bool SetBinaryType(PP_WebSocketBinaryType_Dev) = 0;
-  virtual PP_WebSocketBinaryType_Dev GetBinaryType() = 0;
 };
 
 }  // namespace thunk
