197d453511f339f33df62664b43d91e8e0605e2c
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 23:06:33 +0000

    [wx] Unreviewed. Build fix, add missing header.
    git-svn-id: svn://svn.chromium.org/blink/trunk@106157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/SVGImageBufferTools.h
+++ third_party/WebKit/Source/WebCore/rendering/svg/SVGImageBufferTools.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(SVG)
 #include "ImageBuffer.h"
+#include "IntRect.h"
 #include <wtf/Noncopyable.h>
 
 namespace WebCore {
