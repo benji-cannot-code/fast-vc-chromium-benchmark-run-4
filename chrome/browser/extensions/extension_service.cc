84869a8870132a75987b7dbf3fde0c5b67a4f238
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 23:10:37 +0000

    Don't check for alertable extensions on extension installation.
    BUG=111504
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9298020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119518 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -2064,7 +2064,6 @@ void ExtensionService::AddExtension(const Extension* extension) {
   extensions_.Insert(scoped_extension);
   SyncExtensionChangeIfNeeded(*extension);
   NotifyExtensionLoaded(extension);
-  IdentifyAlertableExtensions();
 }
 
 void ExtensionService::InitializePermissions(const Extension* extension) {
