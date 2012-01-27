4d4c79f1eb167a9017d79767d92024f415885570
noel.gordon@gmail.com <noel.gordon@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 07:52:38 +0000

    Remove FIXME: ColorProfile encapsulation as a Vector<char> works well https://bugs.webkit.org/show_bug.cgi?id=77176
    Reviewed by Adam Barth.
    
    No new tests. No change in behavior.
    
    * platform/image-decoders/ImageDecoder.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/ImageDecoder.h
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    // FIXME: Do we want better encapsulation?
     typedef Vector<char> ColorProfile;
 
     // ImageFrame represents the decoded image data.  This buffer is what all
