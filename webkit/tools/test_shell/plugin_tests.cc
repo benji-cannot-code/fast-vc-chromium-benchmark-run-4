26a8bc4fc60ce2553e0faa33a3e5a88f662c83cc
yuzo@chromium.org <yuzo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 10:44:51 +0000

    Mark PluginTest.PluginVisibilty as FAILS.
    The test fails for WIN.
    
    BUG=111601
    TEST=none
    TBR=jam@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9232042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119435 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/plugin_tests.cc
+++ webkit/tools/test_shell/plugin_tests.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -167,7 +167,14 @@ BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lparam) {
 }
 
 // Tests that hiding/showing the parent frame hides/shows the plugin.
-TEST_F(PluginTest, PluginVisibilty) {
+// Fails for WIN. http://crbug.com/111601
+#if defined(OS_WIN)
+#define MAYBE_PluginVisibilty FAILS_PluginVisibilty
+#else
+#define MAYBE_PluginVisibilty PluginVisibilty
+#endif
+
+TEST_F(PluginTest, MAYBE_PluginVisibilty) {
   FilePath test_html = data_dir_;
   test_html = test_html.AppendASCII(kPluginsDir);
   test_html = test_html.AppendASCII("plugin_visibility.html");
