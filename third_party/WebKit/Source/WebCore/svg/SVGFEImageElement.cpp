da3539b1c0b1ed168e73aa74f20ca111abd5a419
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 12:56:21 +0000

    <feImage> DOM mutation problems https://bugs.webkit.org/show_bug.cgi?id=77198
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Consider <feImage xlink:href="#rect"/>, where <rect> gets dynamically added to the tree.
    Currently <feImage> doesn't notice this, as it doesn't register itself pending on "#rect".
    
    Integrate <feImage> properly within the pending resources concept, fixing the bug.
    
    Tests: svg/filters/feImage-target-add-to-document.svg
           svg/filters/feImage-target-changes-id.svg
           svg/filters/feImage-target-id-change.svg
           svg/filters/feImage-target-reappend-to-document.svg
           svg/filters/feImage-target-remove-from-document.svg
    
    * svg/SVGFEImageElement.cpp: Rename invalidateImageResource to clearResourceReferences.
    (WebCore::SVGFEImageElement::~SVGFEImageElement): Call clearResourceReferences.
    (WebCore::SVGFEImageElement::clearResourceReferences): Remove any occurence of m_targetImage, it's no longer used.
    (WebCore::SVGFEImageElement::requestImageResource): requestImageResource is now called by buildPendingResource.
    (WebCore::SVGFEImageElement::buildPendingResource): Called whenever any element that we depend on gets resolved - now invalidates the <filter>.
    (WebCore::SVGFEImageElement::parseMappedAttribute): Don't start loading from parseMappedAttribute.
    (WebCore::SVGFEImageElement::svgAttributeChanged): Start loading from here, for consistency with SVGImageElement.
    (WebCore::SVGFEImageElement::insertedIntoDocument): Invoke buildPendingResource(), if we enter the tree.
    (WebCore::SVGFEImageElement::removedFromDocument): Clear m_cachedImage as soon as we get removed from the tree.
    (WebCore::SVGFEImageElement::build): Clean up this function, m_targetImage is no longer exist.
    * svg/SVGFEImageElement.h: Remove OwnPtr<ImageBuffer> m_targetImage, which is no longer used.
    
    LayoutTests:
    
    Add new test cases covering <feImage> + DOM mutations.
    
    * platform/chromium/test_expectations.txt: Updated expectations.
    * svg/filters/feImage-target-add-to-document-expected.png: Added.
    * svg/filters/feImage-target-add-to-document-expected.txt: Added.
    * svg/filters/feImage-target-add-to-document.svg: Added.
    * svg/filters/feImage-target-changes-id-expected.png: Added.
    * svg/filters/feImage-target-changes-id-expected.txt: Added.
    * svg/filters/feImage-target-changes-id.svg: Added.
    * svg/filters/feImage-target-id-change-expected.png: Added.
    * svg/filters/feImage-target-id-change-expected.txt: Added.
    * svg/filters/feImage-target-id-change.svg: Added.
    * svg/filters/feImage-target-reappend-to-document-expected.png: Added.
    * svg/filters/feImage-target-reappend-to-document-expected.txt: Added.
    * svg/filters/feImage-target-reappend-to-document.svg: Added.
    * svg/filters/feImage-target-remove-from-document-expected.png: Added.
    * svg/filters/feImage-target-remove-from-document-expected.txt: Added.
    * svg/filters/feImage-target-remove-from-document.svg: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGFEImageElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGFEImageElement.cpp
@@ -65,39 +65,47 @@ PassRefPtr<SVGFEImageElement> SVGFEImageElement::create(const QualifiedName& tag
 
 SVGFEImageElement::~SVGFEImageElement()
 {
-    if (m_cachedImage)
-        m_cachedImage->removeClient(this);
+    clearResourceReferences();
 }
 
-void SVGFEImageElement::invalidateImageResource()
+void SVGFEImageElement::clearResourceReferences()
 {
     if (m_cachedImage) {
         m_cachedImage->removeClient(this);
         m_cachedImage = 0;
     }
-
-    m_targetImage.clear();
 }
 
 void SVGFEImageElement::requestImageResource()
 {
-    invalidateImageResource();
-
-    String fragmentIdentifier;
-    Element* hrefElement = SVGURIReference::targetElementFromIRIString(href(), document(), &fragmentIdentifier);
-
-    if (hrefElement && hrefElement->isSVGElement() && hrefElement->renderer())
-        return;
-
-    // We have what appears to be a local fragment identifier, but it didn't resolve yet.
-    if (!fragmentIdentifier.isEmpty())
-        return;
-
     ResourceRequest request(ownerDocument()->completeURL(href()));
-    m_cachedImage = ownerDocument()->cachedResourceLoader()->requestImage(request);
+    m_cachedImage = document()->cachedResourceLoader()->requestImage(request);
 
     if (m_cachedImage)
         m_cachedImage->addClient(this);
+}
+
+void SVGFEImageElement::buildPendingResource()
+{
+    clearResourceReferences();
+    if (!inDocument())
+        return;
+
+    String id;
+    Element* target = SVGURIReference::targetElementFromIRIString(href(), document(), &id);
+    if (!target) {
+        if (hasPendingResources())
+            return;
+
+        if (id.isEmpty())
+            requestImageResource();
+        else {
+            document()->accessSVGExtensions()->addPendingResource(id, this);
+            ASSERT(hasPendingResources());
+        }
+    }
+
+    invalidate();
 }
 
 bool SVGFEImageElement::isSupportedAttribute(const QualifiedName& attrName)
@@ -125,11 +133,8 @@ void SVGFEImageElement::parseMappedAttribute(Attribute* attr)
         return;
     }
 
-    if (SVGURIReference::parseMappedAttribute(attr)) {
-        requestImageResource();
+    if (SVGURIReference::parseMappedAttribute(attr))
         return;
-    }
-
     if (SVGLangSpace::parseMappedAttribute(attr))
         return;
     if (SVGExternalResourcesRequired::parseMappedAttribute(attr))
@@ -153,8 +158,7 @@ void SVGFEImageElement::svgAttributeChanged(const QualifiedName& attrName)
     }
 
     if (SVGURIReference::isKnownAttribute(attrName)) {
-        invalidateImageResource();
-        invalidate();
+        buildPendingResource();
         return;
     }
 
@@ -162,6 +166,18 @@ void SVGFEImageElement::svgAttributeChanged(const QualifiedName& attrName)
         return;
 
     ASSERT_NOT_REACHED();
+}
+
+void SVGFEImageElement::insertedIntoDocument()
+{
+    SVGFilterPrimitiveStandardAttributes::insertedIntoDocument();
+    buildPendingResource();
+}
+
+void SVGFEImageElement::removedFromDocument()
+{
+    SVGFilterPrimitiveStandardAttributes::removedFromDocument();
+    clearResourceReferences();
 }
 
 void SVGFEImageElement::notifyFinished(CachedResource*)
@@ -180,12 +196,9 @@ void SVGFEImageElement::notifyFinished(CachedResource*)
 
 PassRefPtr<FilterEffect> SVGFEImageElement::build(SVGFilterBuilder*, Filter* filter)
 {
-    if (!m_cachedImage && !m_targetImage)
-        requestImageResource();
-
-    if (!m_cachedImage && !m_targetImage)
-        return FEImage::createWithIRIReference(filter, document(), href(), preserveAspectRatio());
-    return FEImage::createWithImage(filter, m_cachedImage->imageForRenderer(renderer()), preserveAspectRatio());
+    if (m_cachedImage)
+        return FEImage::createWithImage(filter, m_cachedImage->imageForRenderer(renderer()), preserveAspectRatio());
+    return FEImage::createWithIRIReference(filter, document(), href(), preserveAspectRatio());
 }
 
 void SVGFEImageElement::addSubresourceAttributeURLs(ListHashSet<KURL>& urls) const
