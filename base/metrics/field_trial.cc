9df37921021fdadb12e94c0825a427c6bb3d93e7
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 13:50:26 +0000

    Added a Unique ID for a Field Trial containing it's hashed name and the selected group ID. Also returns the list of UIDs of all currently running Field Trials
    BUG=None
    TEST=FieldTrialTest.UIDs
    Review URL: https://chromiumcodereview.appspot.com/9117037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119444 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -15,23 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-// static
-const int FieldTrial::kNotFinalized = -1;
-
-// static
-const int FieldTrial::kDefaultGroupNumber = 0;
-
-// static
-bool FieldTrial::enable_benchmarking_ = false;
-
-// static
-const char FieldTrialList::kPersistentStringSeparator('/');
-
 static const char kHistogramFieldTrialSeparator('_');
 
-// static
-int FieldTrialList::kExpirationYearInFuture = 0;
+// statics
+const int FieldTrial::kNotFinalized = -1;
+const int FieldTrial::kDefaultGroupNumber = 0;
+const uint32 FieldTrial::kReservedHashValue = 0;
+bool FieldTrial::enable_benchmarking_ = false;
 
+const char FieldTrialList::kPersistentStringSeparator('/');
+int FieldTrialList::kExpirationYearInFuture = 0;
 
 //------------------------------------------------------------------------------
 // FieldTrial methods and members.
@@ -43,12 +36,14 @@ FieldTrial::FieldTrial(const std::string& name,
                        const int month,
                        const int day_of_month)
   : name_(name),
+    name_hash_(HashName(name)),
     divisor_(total_probability),
     default_group_name_(default_group_name),
     random_(static_cast<Probability>(divisor_ * RandDouble())),
     accumulated_group_probability_(0),
     next_group_number_(kDefaultGroupNumber + 1),
     group_(kNotFinalized),
+    group_name_hash_(kReservedHashValue),
     enable_field_trial_(true) {
   DCHECK_GT(total_probability, 0);
   DCHECK(!name_.empty());
@@ -97,6 +92,7 @@ void FieldTrial::Disable() {
   if (group_ != kNotFinalized) {
     group_ = kDefaultGroupNumber;
     group_name_ = default_group_name_;
+    group_name_hash_ = HashName(group_name_);
   }
 }
 
@@ -118,6 +114,7 @@ int FieldTrial::AppendGroup(const std::string& name,
       StringAppendF(&group_name_, "%d", group_);
     else
       group_name_ = name;
+    group_name_hash_ = HashName(group_name_);
     FieldTrialList::NotifyFieldTrialGroupSelection(name_, group_name_);
   }
   return next_group_number_++;
@@ -128,6 +125,7 @@ int FieldTrial::group() {
     accumulated_group_probability_ = divisor_;
     group_ = kDefaultGroupNumber;
     group_name_ = default_group_name_;
+    group_name_hash_ = HashName(group_name_);
     FieldTrialList::NotifyFieldTrialGroupSelection(name_, group_name_);
   }
   return group_;
@@ -137,6 +135,14 @@ std::string FieldTrial::group_name() {
   group();  // call group() to make sure group assignment was done.
   DCHECK(!group_name_.empty());
   return group_name_;
+}
+
+bool FieldTrial::GetNameGroupId(NameGroupId* name_group_id) {
+  if (group_name_hash_ == kReservedHashValue)
+    return false;
+  name_group_id->name = name_hash_;
+  name_group_id->group = group_name_hash_;
+  return true;
 }
 
 // static
@@ -172,6 +178,25 @@ double FieldTrial::HashClientId(const std::string& client_id,
   uint64* bits = reinterpret_cast<uint64*>(&sha1_hash[0]);
 
   return BitsToOpenEndedUnitInterval(*bits);
+}
+
+// static
+uint32 FieldTrial::HashName(const std::string& name) {
+  // SHA-1 is designed to produce a uniformly random spread in its output space,
+  // even for nearly-identical inputs.
+  unsigned char sha1_hash[kSHA1Length];
+  SHA1HashBytes(reinterpret_cast<const unsigned char*>(name.c_str()),
+                name.size(),
+                sha1_hash);
+
+  COMPILE_ASSERT(sizeof(uint32) < sizeof(sha1_hash), need_more_data);
+  uint32* bits = reinterpret_cast<uint32*>(&sha1_hash[0]);
+
+  // We only DCHECK, since this should not happen because the registration
+  // of the experiment on the server should have already warn the developer.
+  // If this ever happen, we'll ignore this experiment/group when reporting.
+  DCHECK(*bits != kReservedHashValue);
+  return *bits;
 }
 
 //------------------------------------------------------------------------------
@@ -252,9 +277,9 @@ bool FieldTrialList::TrialExists(const std::string& name) {
 
 // static
 void FieldTrialList::StatesToString(std::string* output) {
+  DCHECK(output->empty());
   if (!global_)
     return;
-  DCHECK(output->empty());
   AutoLock auto_lock(global_->lock_);
 
   for (RegistrationList::iterator it = global_->registered_.begin();
@@ -269,6 +294,22 @@ void FieldTrialList::StatesToString(std::string* output) {
     output->append(1, kPersistentStringSeparator);
     output->append(group_name);
     output->append(1, kPersistentStringSeparator);
+  }
+}
+
+// static
+void FieldTrialList::GetFieldTrialNameGroupIds(
+    std::vector<FieldTrial::NameGroupId>* name_group_ids) {
+  DCHECK(name_group_ids->empty());
+  if (!global_)
+    return;
+  AutoLock auto_lock(global_->lock_);
+
+  for (RegistrationList::iterator it = global_->registered_.begin();
+       it != global_->registered_.end(); ++it) {
+    FieldTrial::NameGroupId name_group_id;
+    if (it->second->GetNameGroupId(&name_group_id))
+      name_group_ids->push_back(name_group_id);
   }
 }
 
