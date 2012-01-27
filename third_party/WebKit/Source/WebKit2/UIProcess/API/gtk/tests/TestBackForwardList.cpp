9ac4a39b34903d01cf4086f725012fc68d156144
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 21:47:51 +0000

    Fix a compilation warning in a WebKit2 GTK+ API test.
    Reviewed by Gustavo Noronha Silva.
    
    * UIProcess/API/gtk/tests/TestBackForwardList.cpp:
    (testBackForwardListLimitAndCache): Use an int instead of size_t
    because we want to print them as ints and do subtraction on them.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestBackForwardList.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestBackForwardList.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <webkit2/webkit2.h>
 
 // Back forward list limit is 100 by default.
-static const size_t kBackForwardListLimit = 100;
+static const int kBackForwardListLimit = 100;
 
 static WebKitTestServer* kServer;
 
@@ -241,7 +241,7 @@ static void testBackForwardListNavigation(BackForwardListTest* test, gconstpoint
 
 static void testBackForwardListLimitAndCache(BackForwardListTest* test, gconstpointer)
 {
-    for (size_t i = 0; i < kBackForwardListLimit; i++) {
+    for (int i = 0; i < kBackForwardListLimit; i++) {
         GOwnPtr<char> path(g_strdup_printf("/Page%d", i));
         test->m_changedFlags = BackForwardListTest::CurrentItem | BackForwardListTest::AddedItem;
         test->loadURI(kServer->getURIForPath(path.get()).data());
