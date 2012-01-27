cf57950a8fb72a933eaab9dbda2fd9ac35262018
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 01:55:36 +0000

    NULL ptr in WebCore::ContainerNode::parserAddChild https://bugs.webkit.org/show_bug.cgi?id=76258
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Test: fast/parser/nested-fragment-parser-crash.html
    
    We always need a parent element to attach to.  In crazy cases, we can
    have elements in the stack of open elements that are already detached
    from the DOM.  In those cases, they don't have a parent, so we aren't
    able to enforce the maximum DOM depth.  (Fortunately, they're not
    attached to the DOM anymore so we don't need to enforce the maximum DOM
    depth!)
    
    * html/parser/HTMLConstructionSite.cpp:
    (WebCore::HTMLConstructionSite::attachLater):
    (WebCore::HTMLConstructionSite::fosterParent):
    
    LayoutTests:
    
    This crazy test case causes the parser to do all manner of crazy
    things, which is good times for testing.
    
    * fast/parser/nested-fragment-parser-crash-expected.txt: Added.
    * fast/parser/nested-fragment-parser-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106072 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/HTMLConstructionSite.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLConstructionSite.cpp
@@ -119,9 +119,10 @@ void HTMLConstructionSite::attachLater(ContainerNode* parent, PassRefPtr<Node> p
     }
 
     // Add as a sibling of the parent if we have reached the maximum depth allowed.
-    if (m_openElements.stackDepth() > m_maximumDOMTreeDepth)
+    if (m_openElements.stackDepth() > m_maximumDOMTreeDepth && task.parent->parentNode())
         task.parent = task.parent->parentNode();
 
+    ASSERT(task.parent);
     m_attachmentQueue.append(task);
 }
 
@@ -529,6 +530,7 @@ void HTMLConstructionSite::fosterParent(PassRefPtr<Node> node)
     HTMLConstructionSiteTask task;
     findFosterSite(task);
     task.child = node;
+    ASSERT(task.parent);
     m_attachmentQueue.append(task);
 }
 
