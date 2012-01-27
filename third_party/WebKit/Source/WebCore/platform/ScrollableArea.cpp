988f02161aaee7f73fd81cae0502833acab39b79
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-27 00:31:29 +0000

    We don't need to always repaint overlay scrollbars if they're in layers https://bugs.webkit.org/show_bug.cgi?id=77159
    Reviewed by Beth Dakin.
    
    If a scrollable area has overlay scrollbars we'll always invalidate both scrollbars, regardless of
    whether both scroll offsets actually change since they're translucent and we want to draw the new page
    contents underneath.
    
    However, if the scrollbars are painted into GraphicsLayers they'll be composited by the GPU, and so we don't
    need to repaint them unless the corresponding scroll offset actually changes (which is handled in Scrollbar::updateThumb).
    
    * platform/ScrollableArea.cpp:
    (WebCore::ScrollableArea::setScrollOffsetFromAnimation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollableArea.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollableArea.cpp
@@ -182,7 +182,7 @@ void ScrollableArea::setScrollOffsetFromAnimation(const IntPoint& offset)
     // Tell the scrollbars to update their thumb postions.
     if (Scrollbar* horizontalScrollbar = this->horizontalScrollbar()) {
         horizontalScrollbar->offsetDidChange();
-        if (horizontalScrollbar->isOverlayScrollbar()) {
+        if (horizontalScrollbar->isOverlayScrollbar() && !hasLayerForHorizontalScrollbar()) {
             if (!verticalScrollbar)
                 horizontalScrollbar->invalidate();
             else {
@@ -196,7 +196,7 @@ void ScrollableArea::setScrollOffsetFromAnimation(const IntPoint& offset)
     }
     if (verticalScrollbar) {
         verticalScrollbar->offsetDidChange();
-        if (verticalScrollbar->isOverlayScrollbar())
+        if (verticalScrollbar->isOverlayScrollbar() && !hasLayerForVerticalScrollbar())
             verticalScrollbar->invalidate();
     }
 }
