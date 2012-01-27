9512ff65f230d8edc7ec1b6f4ec53dc2610f31c3
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 23:06:19 +0000

    Revert "Revert 119430 - Make transferbuffer increase in size dynamically"
    This reverts commit 2f38c45427c68fe731c1f5c05256b6c141a6a590.
    
    BUG=101431
    TEST=
    TBR=apatrick@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9121057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119509 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/gles2_conform_support/egl/display.cc
+++ gpu/gles2_conform_support/egl/display.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "gpu/command_buffer/client/gles2_lib.h"
+#include "gpu/command_buffer/client/transfer_buffer.h"
 #include "gpu/command_buffer/service/context_group.h"
 #include "gpu/gles2_conform_support/egl/config.h"
 #include "gpu/gles2_conform_support/egl/surface.h"
@@ -22,8 +23,7 @@ namespace egl {
 
 Display::Display(EGLNativeDisplayType display_id)
     : display_id_(display_id),
-      is_initialized_(false),
-      transfer_buffer_id_(-1) {
+      is_initialized_(false) {
 }
 
 Display::~Display() {
@@ -132,15 +132,11 @@ EGLSurface Display::CreateWindowSurface(EGLConfig config,
   if (!cmd_helper->Initialize(kCommandBufferSize))
     return false;
 
-  int32 transfer_buffer_id =
-      command_buffer->CreateTransferBuffer(kTransferBufferSize, -1);
-  gpu::Buffer transfer_buffer =
-      command_buffer->GetTransferBuffer(transfer_buffer_id);
-  if (transfer_buffer.ptr == NULL)
-    return false;
+  scoped_ptr<gpu::TransferBuffer> transfer_buffer(new gpu::TransferBuffer(
+      cmd_helper.get()));
 
   command_buffer_.reset(command_buffer.release());
-  transfer_buffer_id_ = transfer_buffer_id;
+  transfer_buffer_.reset(transfer_buffer.release());
   gles2_cmd_helper_.reset(cmd_helper.release());
   surface_.reset(new Surface(win));
 
@@ -172,22 +168,25 @@ EGLContext Display::CreateContext(EGLConfig config,
                                   EGLContext share_ctx,
                                   const EGLint* attrib_list) {
   DCHECK(IsValidConfig(config));
-  // TODO(alokp): Command buffer does not support shared contexts.
+  // TODO(alokp): Add support for shared contexts.
   if (share_ctx != NULL)
     return EGL_NO_CONTEXT;
 
   DCHECK(command_buffer_ != NULL);
-  DCHECK(transfer_buffer_id_ != -1);
-  gpu::Buffer buffer = command_buffer_->GetTransferBuffer(transfer_buffer_id_);
-  DCHECK(buffer.ptr != NULL);
+  DCHECK(transfer_buffer_.get());
   bool share_resources = share_ctx != NULL;
   context_.reset(new gpu::gles2::GLES2Implementation(
       gles2_cmd_helper_.get(),
-      buffer.size,
-      buffer.ptr,
-      transfer_buffer_id_,
+      transfer_buffer_.get(),
       share_resources,
       true));
+
+  if (!context_->Initialize(
+      kTransferBufferSize / 2,
+      kTransferBufferSize,
+      kTransferBufferSize * 2)) {
+    return EGL_NO_CONTEXT;
+  }
 
   context_->EnableFeatureCHROMIUM("pepper3d_allow_buffers_on_multiple_targets");
   context_->EnableFeatureCHROMIUM("pepper3d_support_fixed_attribs");
