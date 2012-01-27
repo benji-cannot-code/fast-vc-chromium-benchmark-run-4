47a5e1346b9bf459b27ab2df19126bb61ff9def7
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 07:12:21 +0000

    Remove reinterpret_cast on JSONValueConverter
    BUG=None
    TEST='JSONValueConverterTest.*'
    
    
    Review URL: http://codereview.chromium.org/9289040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119393 0039d316-1c4b-4281-b951-d872f2087c98

--- base/json/json_value_converter.h
+++ base/json/json_value_converter.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/base_export.h"
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -93,11 +92,13 @@ class JSONValueConverter;
 
 namespace internal {
 
+template<typename StructType>
 class FieldConverterBase {
  public:
-  BASE_EXPORT explicit FieldConverterBase(const std::string& path);
-  BASE_EXPORT virtual ~FieldConverterBase();
-  virtual bool ConvertField(const base::Value& value, void* obj) const = 0;
+  explicit FieldConverterBase(const std::string& path) : field_path_(path) {}
+  virtual ~FieldConverterBase() {}
+  virtual bool ConvertField(const base::Value& value, StructType* obj)
+      const = 0;
   const std::string& field_path() const { return field_path_; }
 
  private:
@@ -113,19 +114,18 @@ class ValueConverter {
 };
 
 template <typename StructType, typename FieldType>
-class FieldConverter : public FieldConverterBase {
+class FieldConverter : public FieldConverterBase<StructType> {
  public:
   explicit FieldConverter(const std::string& path,
                           FieldType StructType::* field,
                           ValueConverter<FieldType>* converter)
-      : FieldConverterBase(path),
+      : FieldConverterBase<StructType>(path),
         field_pointer_(field),
         value_converter_(converter) {
   }
 
   virtual bool ConvertField(
-      const base::Value& value, void* obj) const OVERRIDE {
-    StructType* dst = reinterpret_cast<StructType*>(obj);
+      const base::Value& value, StructType* dst) const OVERRIDE {
     return value_converter_->Convert(value, &(dst->*field_pointer_));
   }
 
@@ -424,7 +424,8 @@ class JSONValueConverter {
       return false;
 
     for(size_t i = 0; i < fields_.size(); ++i) {
-      const internal::FieldConverterBase* field_converter = fields_[i];
+      const internal::FieldConverterBase<StructType>* field_converter =
+          fields_[i];
       base::Value* field = NULL;
       if (dictionary_value->Get(field_converter->field_path(), &field)) {
         if (!field_converter->ConvertField(*field, output)) {
@@ -437,7 +438,7 @@ class JSONValueConverter {
   }
 
  private:
-  ScopedVector<internal::FieldConverterBase> fields_;
+  ScopedVector<internal::FieldConverterBase<StructType> > fields_;
 
   DISALLOW_COPY_AND_ASSIGN(JSONValueConverter);
 };
