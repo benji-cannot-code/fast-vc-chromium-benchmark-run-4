b7f7019ac167a83ddc8f576b7df2b22b771066f5
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 12:25:57 +0000

    Introduced UMA counters for auto-enrollment.
    Also reintroduced the missing counters for token and policy fetching.
    
    BUG=chromium-os:23063
    TEST=about:histograms shows collected metrics for Enterprise.Enrollment with values from 9 to 13 after enrolling a device
    
    
    Review URL: http://codereview.chromium.org/9160027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/enterprise_metrics.h
+++ chrome/browser/policy/enterprise_metrics.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,7 @@ namespace policy {
 // These metrics are collected both for device and user tokens.
 enum MetricToken {
   // A cached token was successfully loaded from disk.
-  kMetricTokenLoadSucceeded = 0,
+  kMetricTokenLoadSucceeded,
   // Reading a cached token from disk failed.
   kMetricTokenLoadFailed,
 
@@ -53,7 +53,7 @@ enum MetricToken {
 // Events related to fetching, saving and loading user and device policies.
 enum MetricPolicy {
   // A cached policy was successfully loaded from disk.
-  kMetricPolicyLoadSucceeded = 0,
+  kMetricPolicyLoadSucceeded,
   // Reading a cached policy from disk failed.
   kMetricPolicyLoadFailed,
 
@@ -105,7 +105,7 @@ enum MetricPolicy {
 enum MetricEnrollment {
   // The enrollment screen was closed without completing the enrollment
   // process.
-  kMetricEnrollmentCancelled = 0,
+  kMetricEnrollmentCancelled,
   // The user submitted credentials and started the enrollment process.
   kMetricEnrollmentStarted,
   // Enrollment failed due to a network error.
@@ -126,6 +126,16 @@ enum MetricEnrollment {
   // Enrollment failed because the serial number we try to register is not
   // assigned to the domain used.
   kMetricEnrollmentInvalidSerialNumber,
+  // Auto-enrollment started automatically after the user signed in.
+  kMetricEnrollmentAutoStarted,
+  // Auto-enrollment failed.
+  kMetricEnrollmentAutoFailed,
+  // Auto-enrollment was retried after having failed before.
+  kMetricEnrollmentAutoRetried,
+  // Auto-enrollment was cancelled through the opt-out dialog.
+  kMetricEnrollmentAutoCancelled,
+  // Auto-enrollment succeeded.
+  kMetricEnrollmentAutoOK,
 
   kMetricEnrollmentSize  // Must be the last.
 };
