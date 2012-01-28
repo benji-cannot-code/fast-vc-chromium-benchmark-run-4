fa32391275f710b4fb3563f27817e17c845e27c2
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 02:12:14 +0000

    Add GPU info collection to GPUTestConfig.
    We didn't add it the first place due to a shared build failure. This should be added so the GPUTestConfig can be easier to use.
    
    BUG=101216
    TEST=content_unittests
    R=kbr
    
    Review URL: http://codereview.chromium.org/9288073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119553 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_test_config.cc
+++ content/test/gpu/gpu_test_config.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/sys_info.h"
+#include "content/gpu/gpu_info_collector.h"
 #include "content/public/common/gpu_info.h"
 
 namespace {
@@ -189,8 +190,15 @@ bool GPUTestBotConfig::Matches(const GPUTestConfig& config) const {
   return true;
 }
 
-bool GPUTestBotConfig::LoadCurrentConfig(const content::GPUInfo& gpu_info) {
-  bool rt = SetGPUInfo(gpu_info);
+bool GPUTestBotConfig::LoadCurrentConfig(const content::GPUInfo* gpu_info) {
+  bool rt;
+  if (gpu_info == NULL) {
+    content::GPUInfo my_gpu_info;
+    gpu_info_collector::CollectPreliminaryGraphicsInfo(&my_gpu_info);
+    rt = SetGPUInfo(my_gpu_info);
+  } else {
+    rt = SetGPUInfo(*gpu_info);
+  }
   set_os(GetCurrentOS());
   if (os() == kOsUnknown)
     rt = false;
