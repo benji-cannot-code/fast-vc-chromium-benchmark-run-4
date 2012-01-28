025b35b8edaeb1f7f72c8db13d8c1360ed9048fa
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 08:46:48 +0000

    [uber page] Style update for sub-pages & headers
    This CL also adds target="_blank" to fix "Learn More" opening in the same tab (and hence fixes bug 11155).
    
    R=csilv@chromium.org,estade@chromium.org
    BUG=110658,111555
    TEST=Uber page looks like mocks, "Learn more" opens in new tab, sub-pages look snazzy, scrolling works, and RTL functions (but isn't polished to the max).
    
    Review URL: http://codereview.chromium.org/9283013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/search_page.js
+++ chrome/browser/resources/options2/search_page.js
@@ -351,10 +351,7 @@ cr.define('options', function() {
       }
 
       // Configure elements on the search results page based on search results.
-      if (foundMatches)
-        $('searchPageNoMatches').classList.add('search-hidden');
-      else
-        $('searchPageNoMatches').classList.remove('search-hidden');
+      $('searchPageNoMatches').hidden = foundMatches;
 
       // Create search balloons for sub-page results.
       length = bubbleControls.length;
