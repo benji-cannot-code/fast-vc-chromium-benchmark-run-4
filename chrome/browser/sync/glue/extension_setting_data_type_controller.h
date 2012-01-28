8d3ef0878031ecbfa43b6e15487ce2a92ff4b34b
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 14:54:37 +0000

    Enable syncing of extension settings in integration tests.
    So that this doesn't crash, make ExtensionSettingDataTypeController call
    InitExtensions on Profile (like ExtensionDataTypeController already does).
    
    
    BUG=
    TEST=sync_integration_tests
    
    
    Review URL: http://codereview.chromium.org/9121068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/extension_setting_data_type_controller.h
+++ chrome/browser/sync/glue/extension_setting_data_type_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -59,7 +59,7 @@ class ExtensionSettingDataTypeController
   syncable::ModelType type_;
 
   // These only used on the UI thread.
-  extensions::SettingsFrontend* settings_frontend_;
+  Profile* profile_;
   ProfileSyncService* profile_sync_service_;
 
   // Only used on the FILE thread.
