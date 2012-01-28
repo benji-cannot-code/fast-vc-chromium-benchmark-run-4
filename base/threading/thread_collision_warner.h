2bb1a22fc32d1483e16b373bda754aaa894dfe8b
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 03:09:53 +0000

    Make DFAKE_MUTEX a typedef when using NDEBUG, so that pedantic compilers don't warn about an extra ; in a class definition
    BUG=111583
    
    
    Review URL: http://codereview.chromium.org/9121034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119574 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/thread_collision_warner.h
+++ base/threading/thread_collision_warner.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -120,7 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #else
 
-#define DFAKE_MUTEX(obj)
+#define DFAKE_MUTEX(obj) typedef void InternalFakeMutexType##obj
 #define DFAKE_SCOPED_LOCK(obj) ((void)0)
 #define DFAKE_SCOPED_RECURSIVE_LOCK(obj) ((void)0)
 #define DFAKE_SCOPED_LOCK_THREAD_LOCKED(obj) ((void)0)
