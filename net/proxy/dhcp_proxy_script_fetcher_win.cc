35f7d1966d8f4be91825f271aa272c7f7b8f3d96
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 03:23:11 +0000

    [Coverity] Fixed uninitialized member
    CID=102816
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9298034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119584 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/dhcp_proxy_script_fetcher_win.cc
+++ net/proxy/dhcp_proxy_script_fetcher_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,6 +40,7 @@ DhcpProxyScriptFetcherWin::DhcpProxyScriptFetcherWin(
     URLRequestContext* url_request_context)
     : state_(STATE_START),
       num_pending_fetchers_(0),
+      destination_string_(NULL),
       url_request_context_(url_request_context) {
   DCHECK(url_request_context_);
 }
