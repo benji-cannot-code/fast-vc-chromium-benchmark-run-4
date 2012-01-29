5cc16c31c1189eee7bfc064d7d1ceb86e1db6ce3
cdn@chromium.org <cdn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-29 21:43:24 +0000

    Add Access-Control-Allow-Origin: * header for extension resources specifically listed as web accessible.
    BUG=109686
    
    Review URL: http://codereview.chromium.org/9152022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_resource_request_policy_apitest.cc
+++ chrome/browser/extensions/extension_resource_request_policy_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -142,6 +142,30 @@ IN_PROC_BROWSER_TEST_F(ExtensionResourceRequestPolicyTest,
       L"window.domAutomationController.send(document.title)",
     &result));
   EXPECT_EQ("Loaded", result);
+
+  GURL xhr_accessible_resource(
+      test_server()->GetURL(
+          "files/extensions/api_test/extension_resource_request_policy/"
+          "web_accessible/xhr_accessible_resource.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), xhr_accessible_resource);
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+    browser()->GetSelectedWebContents()->GetRenderViewHost(), L"",
+      L"window.domAutomationController.send(document.title)",
+    &result));
+  EXPECT_EQ("XHR completed with status: 200", result);
+
+  GURL xhr_inaccessible_resource(
+      test_server()->GetURL(
+          "files/extensions/api_test/extension_resource_request_policy/"
+          "web_accessible/xhr_inaccessible_resource.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), xhr_inaccessible_resource);
+  ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+    browser()->GetSelectedWebContents()->GetRenderViewHost(), L"",
+      L"window.domAutomationController.send(document.title)",
+    &result));
+  EXPECT_EQ("XHR failed to load resource", result);
 
   GURL nonaccessible_resource(
       test_server()->GetURL(
