f762e46afc6a77c891aa74f7169d9aacecb3f60a
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-30 21:19:03 +0000

    Aura: Resize login screen if resolution changes
    The lock screen container needs a layout manager to respond to resolution changes caused by an external monitor being attached or detached.  Marking the login screen widget as fullscreen makes it reshape to track the size change.
    
    BUG=110942
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/9121063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119732 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_window_ids.h"
 #include "ash/tooltips/tooltip_controller.h"
 #include "ash/wm/activation_controller.h"
+#include "ash/wm/base_layout_manager.h"
 #include "ash/wm/compact_layout_manager.h"
 #include "ash/wm/compact_status_area_layout_manager.h"
 #include "ash/wm/dialog_frame_view.h"
@@ -113,6 +114,7 @@ void CreateSpecialContainers(aura::Window::Windows* containers) {
   // TODO(beng): Figure out if we can make this use
   // SystemModalContainerEventFilter instead of stops_event_propagation.
   aura::Window* lock_container = new aura::Window(NULL);
+  lock_container->SetLayoutManager(new internal::BaseLayoutManager);
   lock_container->set_stops_event_propagation(true);
   lock_container->set_id(internal::kShellWindowId_LockScreenContainer);
   containers->push_back(lock_container);
