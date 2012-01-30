1f9450530ee32ecc42716f5a56af526f23bb14d4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-30 17:31:52 +0000

    REGRESSION (r82580): Reproducible crash in CSSPrimitiveValue::computeLengthDouble https://bugs.webkit.org/show_bug.cgi?id=61989
    Patch by Parag Radke <nrqv63@motorola.com> on 2012-01-30
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    According to css3 specs when font-size is specified in 'rems' for an element implies the font-size
    of the root element. In this case as HTML element has a property 'display:none' and hence renderer
    is NULL causes this crash.
    
    Test: fast/css/fontsize-unit-rems-crash.html
    
    * css/CSSPrimitiveValue.cpp:
    (WebCore::CSSPrimitiveValue::computeLengthDouble):
    Added a null check for the root element's RenderStyle as it can be null in case of html has a property
    hidden or display:none.
    
    LayoutTests:
    
    Added a test case to check rems unit (css3) with html property display:none.
    
    * fast/css/fontsize-unit-rems-crash-expected.txt: Added.
    * fast/css/fontsize-unit-rems-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
@@ -417,7 +417,8 @@ double CSSPrimitiveValue::computeLengthDouble(RenderStyle* style, RenderStyle* r
             break;
         case CSS_REMS:
             applyZoomMultiplier = false;
-            factor = computingFontSize ? rootStyle->fontDescription().specifiedSize() : rootStyle->fontDescription().computedSize();
+            if (rootStyle)
+                factor = computingFontSize ? rootStyle->fontDescription().specifiedSize() : rootStyle->fontDescription().computedSize();
             break;
         case CSS_PX:
             break;
