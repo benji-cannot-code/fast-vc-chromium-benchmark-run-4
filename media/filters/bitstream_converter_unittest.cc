1033680be3c0c6e3ff251acb54e68ae5bc0e2813
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-30 20:32:52 +0000

    Don't register an ffmpeg bitstream filter more than once.
    The previous code was buggy in that it would register twice (when --gtest_repeat
    was >1) and create an infinite loop inside ffmpeg (with a circular linked
    list).  IWBN to *un*register the filter in a dtor or TearDownTestCase, but sadly
    libavcodec doesn't offer that API.  Instead, cheese by storing a bool.
    
    BUG=105236
    TEST=CHROME_HEADLESS=1 ./ninja/Debug/media_unittests --gtest_shuffle --gtest_repeat=-1 doesn't hang.
    
    Review URL: https://chromiumcodereview.appspot.com/9223036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119721 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/bitstream_converter_unittest.cc
+++ media/filters/bitstream_converter_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -51,6 +51,7 @@ static int DoFilter(AVBitStreamFilterContext* bsfc,
 
 static void DoClose(AVBitStreamFilterContext* bsfc) {}
 
+static bool g_stream_filter_registered = false;
 static AVBitStreamFilter g_stream_filter = {
   kTestFilterName,
   0, // Private Data Size
@@ -74,8 +75,10 @@ class BitstreamConverterTest : public testing::Test {
 
   static void SetUpTestCase() {
     // Register g_stream_filter if it isn't already registered.
-    if (!g_stream_filter.next)
+    if (!g_stream_filter_registered) {
       av_register_bitstream_filter(&g_stream_filter);
+      g_stream_filter_registered = true;
+    }
   }
 
   AVCodecContext test_stream_context_;
