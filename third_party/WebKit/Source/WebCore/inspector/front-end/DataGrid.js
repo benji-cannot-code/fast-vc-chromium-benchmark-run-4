222c5a97f4a009c43731cf5981ee01a00562edb6
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-30 16:17:01 +0000

    Web Inspector: keyboard navigation through comparison view in heap profiler should update retainers view https://bugs.webkit.org/show_bug.cgi?id=77326
    Keyboard navigation in the detailed heap snapshot view now updates retainers view.
    
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/DataGrid.js: Added SelectedNode/DeselectedNode events to DataGrid.
    (WebInspector.DataGridNode.prototype.select):
    (WebInspector.DataGridNode.prototype.deselect):
    * inspector/front-end/DetailedHeapshotView.js:
    (WebInspector.DetailedHeapshotView.prototype._selectionChanged):
    (WebInspector.DetailedHeapshotView.prototype._setRetainmentDataGridSource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106245 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DataGrid.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DataGrid.js
@@ -163,6 +163,11 @@ WebInspector.DataGrid = function(columns, editCallback, deleteCallback)
     this._columnWidthsInitialized = false;
 }
 
+WebInspector.DataGrid.Events = {
+    SelectedNode: "SelectedNode",
+    DeselectedNode: "DeselectedNode"
+}
+
 /**
  * @param {Array.<string>} columnNames
  * @param {Array.<string>} values
@@ -1465,8 +1470,10 @@ WebInspector.DataGridNode.prototype = {
         if (this._element)
             this._element.addStyleClass("selected");
 
-        if (!supressSelectedEvent)
+        if (!supressSelectedEvent) {
             this.dispatchEventToListeners("selected");
+            this.dataGrid.dispatchEventToListeners(WebInspector.DataGrid.Events.SelectedNode);
+        }
     },
 
     revealAndSelect: function()
@@ -1489,8 +1496,10 @@ WebInspector.DataGridNode.prototype = {
         if (this._element)
             this._element.removeStyleClass("selected");
 
-        if (!supressDeselectedEvent)
+        if (!supressDeselectedEvent) {
             this.dispatchEventToListeners("deselected");
+            this.dataGrid.dispatchEventToListeners(WebInspector.DataGrid.Events.DeselectedNode);
+        }
     },
 
     traverseNextNode: function(skipHidden, stayWithin, dontPopulate, info)
