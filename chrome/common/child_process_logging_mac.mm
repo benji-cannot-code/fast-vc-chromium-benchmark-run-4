6aa94e6405e9ffae10aafb48cc28dced8f10f0c5
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-30 17:50:36 +0000

    mac: Fix 2 harmless -Wformat issues found by clang.
    BUG=111861
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9232073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119689 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_logging_mac.mm
+++ chrome/common/child_process_logging_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -188,7 +188,7 @@ void SetCommandLine(const CommandLine* command_line) {
   // Note the total number of switches, not including the exec path.
   const CommandLine::StringVector& argv = command_line->argv();
   SetCrashKeyValue(kNumSwitchesKey,
-                   [NSString stringWithFormat:@"%d", argv.size() - 1]);
+                   [NSString stringWithFormat:@"%zu", argv.size() - 1]);
 
   size_t key_i = 0;
   for (size_t i = 1; i < argv.size() && key_i < kMaxSwitches; ++i) {
