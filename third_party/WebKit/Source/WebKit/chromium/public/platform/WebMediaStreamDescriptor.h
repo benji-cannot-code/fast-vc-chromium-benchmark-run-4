e5eb3515e7f117d284bf6ba0a1e02c24c44f7578
tommyw@google.com <tommyw@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-30 18:36:18 +0000

    [chromium] MediaStream API: Fix a few memory leaks https://bugs.webkit.org/show_bug.cgi?id=77334
    Reviewed by Darin Fisher.
    
    It seems that either the behaviour of WebPrivatePtr has changed or I misunderstood
    something, but I fixed a memory leak in WebMediaStreamDescriptor and safeguarded
    WebMediaStreamSource.
    
    * public/platform/WebMediaStreamDescriptor.h:
    (WebKit::WebMediaStreamDescriptor::WebMediaStreamDescriptor):
    * public/platform/WebMediaStreamSource.h:
    (WebKit::WebMediaStreamSource::WebMediaStreamSource):
    (WebKit::WebMediaStreamSource::operator=):
    (WebMediaStreamSource):
    * src/WebMediaStreamDescriptor.cpp:
    (WebKit::WebMediaStreamDescriptor::assign):
    * src/WebMediaStreamSource.cpp:
    (WebKit::WebMediaStreamSource::assign):
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamDescriptor.h
+++ third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamDescriptor.h
@@ -43,7 +43,7 @@ class WebString;
 class WebMediaStreamDescriptor {
 public:
     WebMediaStreamDescriptor() { }
-    WebMediaStreamDescriptor(const WebMediaStreamDescriptor& request) { assign(request); }
+    WebMediaStreamDescriptor(const WebMediaStreamDescriptor& other) { assign(other); }
     ~WebMediaStreamDescriptor() { reset(); }
 
     WebMediaStreamDescriptor& operator=(const WebMediaStreamDescriptor& other)
