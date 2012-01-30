12826f19136aff4380e1457507d5908b71b56cb2
dominich@google.com <dominich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-30 20:23:21 +0000

    Revert 119699 - Cancel prerenders from Omnibox if we navigate to a different URL than predicted.
    I also took the opportunity to align the order of methods in prerender_manager.cc to that of the header (as it should be) and make a couple of methods const.
    
    BUG=110799,111350
    
    
    Review URL: http://codereview.chromium.org/9226037
    
    TBR=dominich@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9301012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119720 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/network_action_predictor.cc
+++ chrome/browser/autocomplete/network_action_predictor.cc
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_notifications.h"
 #include "chrome/browser/history/in_memory_database.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
-#include "chrome/browser/prerender/prerender_manager.h"
-#include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/guid.h"
@@ -238,15 +236,6 @@ void NetworkActionPredictor::OnOmniboxOpenedUrl(const AutocompleteLog& log) {
 
   const GURL& opened_url = match.destination_url;
 
-  // If the Omnibox triggered a prerender but the URL doesn't match the one the
-  // user is navigating to, cancel the prerender.
-  prerender::PrerenderManager* prerender_manager =
-      prerender::PrerenderManagerFactory::GetForProfile(profile_);
-  // |prerender_manager| can be NULL in incognito mode or if prerendering is
-  // otherwise disabled.
-  if (prerender_manager && !prerender_manager->IsPrerendering(opened_url))
-    prerender_manager->CancelOmniboxPrerenders();
-
   const string16 lower_user_text(base::i18n::ToLower(log.text));
 
   BeginTransaction();
@@ -302,6 +291,7 @@ void NetworkActionPredictor::OnOmniboxOpenedUrl(const AutocompleteLog& log) {
   }
   tracked_urls_.clear();
 }
+
 
 void NetworkActionPredictor::DeleteOldIdsFromCaches(
     history::URLDatabase* url_db,
