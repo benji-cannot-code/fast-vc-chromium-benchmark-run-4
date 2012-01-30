675cf1877d4e9d6abc77401cf8996d97e79377b9
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-30 15:16:02 +0000

    Mark BrowserCloseTest.DownloadsCloseCheck as FLAKY
    These tests are repeatedly timing out. They often run in the 42-43 second range on XP debug, which is too close to the global 45 second timeout.
    
    BUG=111914
    TEST=none
    TBR=rdsmith
    
    Review URL: https://chromiumcodereview.appspot.com/9264035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_close_browsertest.cc
+++ chrome/browser/ui/browser_close_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -517,7 +517,14 @@ IN_PROC_BROWSER_TEST_F(BrowserCloseTest, DownloadsCloseCheck_1) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserCloseTest, DownloadsCloseCheck_2) {
+// Timing out on XP debug. http://crbug.com/111914
+#if defined(OS_WIN)
+#define MAYBE_DownloadsCloseCheck_2 FLAKY_DownloadsCloseCheck_2
+#else
+#define MAYBE_DownloadsCloseCheck_2 DownloadsCloseCheck_2
+#endif
+
+IN_PROC_BROWSER_TEST_F(BrowserCloseTest, MAYBE_DownloadsCloseCheck_2) {
   ASSERT_TRUE(SetupForDownloadCloseCheck());
   for (size_t i = 2 * arraysize(download_close_check_cases) / 6;
        i < 3 * arraysize(download_close_check_cases) / 6; ++i) {
@@ -541,7 +548,14 @@ IN_PROC_BROWSER_TEST_F(BrowserCloseTest, DownloadsCloseCheck_4) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserCloseTest, DownloadsCloseCheck_5) {
+// Timing out on XP debug. http://crbug.com/111914
+#if defined(OS_WIN)
+#define MAYBE_DownloadsCloseCheck_5 FLAKY_DownloadsCloseCheck_5
+#else
+#define MAYBE_DownloadsCloseCheck_5 DownloadsCloseCheck_5
+#endif
+
+IN_PROC_BROWSER_TEST_F(BrowserCloseTest, MAYBE_DownloadsCloseCheck_5) {
   ASSERT_TRUE(SetupForDownloadCloseCheck());
   for (size_t i = 5 * arraysize(download_close_check_cases) / 6;
        i < 6 * arraysize(download_close_check_cases) / 6; ++i) {
