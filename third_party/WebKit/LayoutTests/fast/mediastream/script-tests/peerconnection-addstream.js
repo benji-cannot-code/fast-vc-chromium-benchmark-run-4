28600887999dad45e8fe8023efff633d6cb82cd6
tommyw@google.com <tommyw@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-30 20:25:15 +0000

    Memory leak caused by PeerConnection add a NULL media stream https://bugs.webkit.org/show_bug.cgi?id=76150
    Source/WebCore:
    
    It was my missunderstanding that the IDL keyword [StrictTypeChecking] also protects against
    null or undefined arguments, it doesn't. Added checks for null pointers.
    
    Reviewed by Adam Barth.
    
    Test: fast/mediastream/peerconnection-addstream.html
    
    * mediastream/PeerConnection.cpp:
    (WebCore::PeerConnection::addStream):
    (WebCore::PeerConnection::removeStream):
    
    LayoutTests:
    
    Added a basic LayoutTest for null/undefined arguments to PeerConnection::addStream().
    
    Reviewed by Adam Barth.
    
    * fast/mediastream/peerconnection-addstream-expected.txt: Added.
    * fast/mediastream/peerconnection-addstream.html: Added.
    * fast/mediastream/script-tests/peerconnection-addstream.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/mediastream/script-tests/peerconnection-addstream.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Tests PeerConnection::addStream().");
+
+var pc = new webkitPeerConnection("STUN some.server.com", function() {});
+
+try {
+    pc.addStream();
+} catch(e) {
+    testPassed('pc.addStream() threw ' + e);
+}
+
+try {
+    pc.addStream(undefined);
+} catch(e) {
+    testPassed('pc.addStream(undefined) threw ' + e);
+}
+
+try {
+    pc.addStream(null);
+} catch(e) {
+    testPassed('pc.addStream(null) threw ' + e);
+}
+
+try {
+    pc.addStream(new Array());
+} catch(e) {
+    testPassed('pc.addStream(new Array()) threw ' + e);
+}
+
+finishJSTest();
+
+window.successfullyParsed = true;
+
