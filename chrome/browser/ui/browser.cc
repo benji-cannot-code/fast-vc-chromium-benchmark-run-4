14fdf8cd131e1552c8917ebd6afbe8421857fe3b
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 17:10:49 +0000

    Revert r119897 "Fixed 'singleton' behavior of filebrowser"
    TBR=zvorygin@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9309002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2328,8 +2328,7 @@ void Browser::OpenSearchEngineOptionsDialog() {
 #if defined(FILE_MANAGER_EXTENSION)
 void Browser::OpenFileManager() {
   content::RecordAction(UserMetricsAction("OpenFileManager"));
-  OpenURL(GURL(chrome::kChromeUIFileManagerURL), GURL(),
-            NEW_FOREGROUND_TAB, content::PAGE_TRANSITION_LINK);
+  ShowSingletonTabRespectRef(GURL(chrome::kChromeUIFileManagerURL));
 }
 #endif
 
