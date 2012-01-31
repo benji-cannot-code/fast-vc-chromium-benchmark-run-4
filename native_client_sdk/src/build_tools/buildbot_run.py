571d71f193dfcfa0325ff441257cd4339a82de42
elijahtaylor@google.com <elijahtaylor@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 09:08:54 +0000

    Fix typo in nacl_dyncode.h path which prevented it from installing. Install toolchain headers to glibc toolchain instead of newlib twice.
    BUG=none
    TEST=manual (build sdk, verify headers in place)
    
    Review URL: http://codereview.chromium.org/9293021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119871 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/buildbot_run.py
+++ native_client_sdk/src/build_tools/buildbot_run.py
@@ -182,7 +182,7 @@ header_map = {
       'semaphore.h': 'src/untrusted/pthread/semaphore.h',
       'dynamic_annotations.h': 'src/untrusted/valgrind/dynamic_annotations.h',
       'dynamic_annotations.h': 'src/untrusted/valgrind/dynamic_annotations.h',
-      'nacl_dyncode.h': ' src/untrusted/nacl/nacl_dyncode.h',
+      'nacl_dyncode.h': 'src/untrusted/nacl/nacl_dyncode.h',
       'nacl_startup.h': 'src/untrusted/nacl/nacl_startup.h',
       'nacl_thread.h': 'src/untrusted/nacl/nacl_thread.h',
       'pnacl.h': 'src/untrusted/nacl/pnacl.h',
@@ -192,7 +192,7 @@ header_map = {
   'glibc': {
       'dynamic_annotations.h': 'src/untrusted/valgrind/dynamic_annotations.h',
       'dynamic_annotations.h': 'src/untrusted/valgrind/dynamic_annotations.h',
-      'nacl_dyncode.h': ' src/untrusted/nacl/nacl_dyncode.h',
+      'nacl_dyncode.h': 'src/untrusted/nacl/nacl_dyncode.h',
       'nacl_startup.h': 'src/untrusted/nacl/nacl_startup.h',
       'nacl_thread.h': 'src/untrusted/nacl/nacl_thread.h',
       'pnacl.h': 'src/untrusted/nacl/pnacl.h',
@@ -304,7 +304,7 @@ def main():
     # Then rename/move it to the pepper toolchain directory
     srcdir = os.path.join(tmpdir, 'sdk', 'nacl-sdk')
     newlibdir = os.path.join(pepperdir, 'toolchain', tcname + '_newlib')
-    print "BUildbot mv %s to %s" % (srcdir, newlibdir)
+    print "Buildbot mv %s to %s" % (srcdir, newlibdir)
     MoveDir(srcdir, newlibdir)
     print "Done with buildbot move"
     # Untar the glibc toolchains
@@ -336,7 +336,7 @@ def main():
       InstallHeaders(GetToolchainNaClInclude(newlibdir, 'x86'),
                      pepper_ver, 
                      'newlib')
-      InstallHeaders(GetToolchainNaClInclude(newlibdir, 'x86'),
+      InstallHeaders(GetToolchainNaClInclude(glibcdir, 'x86'),
                      pepper_ver,
                      'glibc')
     else:
