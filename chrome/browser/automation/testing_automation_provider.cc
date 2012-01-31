2c680d39ec37c78646d44876d90ba468abc73d60
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 06:09:28 +0000

    [pyauto] GetBrowserInfo() returns info about aura
    BUG=None
    TEST=None
    
    R=dtu@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9293024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119864 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -2842,6 +2842,11 @@ void TestingAutomationProvider::GetBrowserInfo(
   FilePath dumps_path;
   PathService::Get(chrome::DIR_CRASH_DUMPS, &dumps_path);
   properties->SetString("DIR_CRASH_DUMPS", dumps_path.value());
+#if defined(USE_AURA)
+  properties->SetBoolean("aura", true);
+#else
+  properties->SetBoolean("aura", false);
+#endif
 
   std::string branding;
 #if defined(GOOGLE_CHROME_BUILD)
