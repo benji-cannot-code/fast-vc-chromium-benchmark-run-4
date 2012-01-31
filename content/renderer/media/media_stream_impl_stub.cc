f5118d1b001fa299e60d37b13466aa0594c6f8b6
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 12:12:37 +0000

    Fix build when webrtc is disabled; i.e. when enable_webrtc is set to 0 in build/common.gypi.
    Building with webrtc disabled is currently not tested by any build bots. Landing the support for PeerConnection v2 (instead of v1) in 119568 broke the build when webrtc is disabled.
    
    The stub implementations must be updated to reflect the new interfaces.
    
    TEST=Set enable_webrtc to 0 in build/common.gypi; build chrome.
    
    Review URL: https://chromiumcodereview.appspot.com/9302030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119888 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_impl_stub.cc
+++ content/renderer/media/media_stream_impl_stub.cc
@@ -17,7 +17,6 @@ MediaStreamImpl::MediaStreamImpl(
     MediaStreamDependencyFactory* dependency_factory)
     : dependency_factory_(dependency_factory),
       media_stream_dispatcher_(media_stream_dispatcher),
-      media_engine_(NULL),
       p2p_socket_dispatcher_(p2p_socket_dispatcher),
       network_manager_(NULL),
       vc_manager_(vc_manager),
@@ -25,15 +24,10 @@ MediaStreamImpl::MediaStreamImpl(
       message_loop_proxy_(base::MessageLoopProxy::current()),
       signaling_thread_(NULL),
       worker_thread_(NULL),
-      chrome_worker_thread_("Chrome_libJingle_WorkerThread"),
-      vcm_created_(false) {
+      chrome_worker_thread_("Chrome_libJingle_WorkerThread") {
 }
 
-MediaStreamImpl::~MediaStreamImpl() {
-  DCHECK(!peer_connection_handler_);
-  if (dependency_factory_.get())
-    dependency_factory_->DeletePeerConnectionFactory();
-}
+MediaStreamImpl::~MediaStreamImpl() {}
 
 WebKit::WebPeerConnectionHandler* MediaStreamImpl::CreatePeerConnectionHandler(
     WebKit::WebPeerConnectionHandlerClient* client) {
@@ -43,8 +37,9 @@ WebKit::WebPeerConnectionHandler* MediaStreamImpl::CreatePeerConnectionHandler(
 void MediaStreamImpl::ClosePeerConnection() {
 }
 
-bool MediaStreamImpl::SetVideoCaptureModule(const std::string& label) {
-  return false;
+webrtc::MediaStreamTrackInterface* MediaStreamImpl::GetLocalMediaStreamTrack(
+    const std::string& label) {
+  return NULL;
 }
 
 void MediaStreamImpl::requestUserMedia(
@@ -70,8 +65,7 @@ void MediaStreamImpl::OnStreamGenerated(
     const media_stream::StreamDeviceInfoArray& video_array) {
 }
 
-void MediaStreamImpl::OnStreamGenerationFailed(int request_id) {
-}
+void MediaStreamImpl::OnStreamGenerationFailed(int request_id) {}
 
 void MediaStreamImpl::OnVideoDeviceFailed(const std::string& label,
                                           int index) {
@@ -86,8 +80,7 @@ void MediaStreamImpl::OnDevicesEnumerated(
     const media_stream::StreamDeviceInfoArray& device_array) {
 }
 
-void MediaStreamImpl::OnDevicesEnumerationFailed(int request_id) {
-}
+void MediaStreamImpl::OnDevicesEnumerationFailed(int request_id) {}
 
 void MediaStreamImpl::OnDeviceOpened(
     int request_id,
@@ -95,5 +88,4 @@ void MediaStreamImpl::OnDeviceOpened(
     const media_stream::StreamDeviceInfo& video_device) {
 }
 
-void MediaStreamImpl::OnDeviceOpenFailed(int request_id) {
-}
+void MediaStreamImpl::OnDeviceOpenFailed(int request_id) {}
