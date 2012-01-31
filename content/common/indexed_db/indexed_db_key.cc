6e911511a46cd6d7fe2e821ce427c747ca67f1e5
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 21:13:36 +0000

    Iterate an array in O(n) rather than O(n^2) time during IDBKey conversion.
    BUG=112016
    TEST=third_party/WebKit/LayoutTests/storage/indexeddb/key-type-array.html
    
    
    Review URL: http://codereview.chromium.org/9104026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119956 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/indexed_db/indexed_db_key.cc
+++ content/common/indexed_db/indexed_db_key.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebVector.h"
 
 using WebKit::WebIDBKey;
+using WebKit::WebVector;
 
 IndexedDBKey::IndexedDBKey()
     : type_(WebIDBKey::NullType),
@@ -56,8 +57,9 @@ void IndexedDBKey::Set(const WebIDBKey& key) {
   type_ = key.type();
   array_.clear();
   if (key.type() == WebIDBKey::ArrayType) {
-    for (size_t i = 0; i < key.array().size(); ++i) {
-      array_.push_back(IndexedDBKey(key.array()[i]));
+    WebVector<WebIDBKey> array = key.array();
+    for (size_t i = 0; i < array.size(); ++i) {
+      array_.push_back(IndexedDBKey(array[i]));
     }
   }
   string_ = key.type() == WebIDBKey::StringType ?
