e7bda4fb2f24ef88e14794b337c7d13304132748
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 00:24:07 +0000

    Set the ShortcutCreated pref for when shortcuts are created on first run.
    BUG=111519
    TEST=Verify 111519.
    
    Review URL: http://codereview.chromium.org/9288077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run_win.cc
+++ chrome/browser/first_run/first_run_win.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/worker_thread_ticker.h"
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/install_util.h"
@@ -137,8 +138,10 @@ bool CreateChromeQuickLaunchShortcut() {
       true);  // create if doesn't exist.
 }
 
-void PlatformSetup() {
-  CreateChromeDesktopShortcut();
+void PlatformSetup(Profile* profile) {
+  if (CreateChromeDesktopShortcut())
+    profile->GetPrefs()->SetBoolean(prefs::kProfileShortcutCreated, true);
+
   // Windows 7 has deprecated the quick launch bar.
   if (base::win::GetVersion() < base::win::VERSION_WIN7)
     CreateChromeQuickLaunchShortcut();
@@ -455,7 +458,7 @@ void AutoImport(
   // discarded, which is the correct behavior during the import process.
   process_singleton->Lock(NULL);
 
-  PlatformSetup();
+  PlatformSetup(profile);
 
   scoped_refptr<ImporterHost> importer_host;
   importer_host = new ImporterHost;
