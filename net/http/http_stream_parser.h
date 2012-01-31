52d1625376680db9d4c94646250a7c2bf2ba56ee
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 09:08:45 +0000

    net: Replace use of memmove() with DrainableIOBuffer.
    This is yet another cleanup patch for HttpStreamParser.
    
    BUG=72001
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9289004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119870 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_parser.h
+++ net/http/http_stream_parser.h
@@ -26,6 +26,7 @@ struct HttpRequestInfo;
 class HttpRequestHeaders;
 class HttpResponseInfo;
 class IOBuffer;
+class IOBufferWithSize;
 class SSLCertRequestInfo;
 class SSLInfo;
 
@@ -223,11 +224,9 @@ class NET_EXPORT_PRIVATE HttpStreamParser  : public ChunkCallback {
 
   // Stores an encoded chunk for chunked uploads.
   // Note: This should perhaps be improved to not create copies of the data.
-  scoped_refptr<IOBuffer> chunk_buf_;
-  // The size of the chunk buffer (chunk_buf_). The chunk buffer is
-  // guaranteed to be large enough to hold the encoded chunk.
-  const size_t chunk_buffer_size_;
-  size_t chunk_length_;
+  scoped_refptr<IOBufferWithSize> raw_chunk_buf_;
+  // Wraps raw_chunk_buf_ to read the remaining data progressively.
+  scoped_refptr<DrainableIOBuffer> chunk_buf_;
   size_t chunk_length_without_encoding_;
   bool sent_last_chunk_;
 
