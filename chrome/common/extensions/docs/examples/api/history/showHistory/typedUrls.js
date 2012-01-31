4d2f407e444158aeb10797c0fcf8c36ec80c1009
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 10:39:21 +0000

    Changing manifest to v2 in extension samples
    Patch from Ernest Delgado <ernestd@chromium.org>.
    
    BUG=111049
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9289057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/api/history/showHistory/typedUrls.js
+++ chrome/common/extensions/docs/examples/api/history/showHistory/typedUrls.js
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Event listner for clicks on links in a browser action popup.
 // Open the link in a new tab of the current window.
 function onAnchorClick(event) {
@@ -111,4 +115,6 @@ function buildTypedUrlList(divName) {
   };
 }
 
-
+document.addEventListener('DOMContentLoaded', function () {
+  buildTypedUrlList("typedUrl_div");
+});
