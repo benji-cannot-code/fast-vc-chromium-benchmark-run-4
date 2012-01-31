9fae72e0fb343c4cbc5415fab46f7daec266897f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-31 08:22:31 +0000

    Fix compilation errors on build-webkit --debug --no-workers on mac. https://bugs.webkit.org/show_bug.cgi?id=75869
    Patch by Pablo Flouret <pablof@motorola.com> on 2012-01-31
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    * WebCore.exp.in:
    
    Source/WebKit/mac:
    
    * Workers/WebWorkersPrivate.mm:
    (+[WebWorkersPrivate workerThreadCount]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/Workers/WebWorkersPrivate.mm
+++ third_party/WebKit/Source/WebKit/mac/Workers/WebWorkersPrivate.mm
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 + (unsigned) workerThreadCount
 {
-#if ENABLE_WORKERS
+#if defined(ENABLE_WORKERS) && ENABLE_WORKERS
     return WebCore::WorkerThread::workerThreadCount();
 #else
     return 0;
