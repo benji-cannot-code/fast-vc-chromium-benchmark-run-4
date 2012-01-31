ffb375bff72330177e5f67dc1f0563e274a8056c
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 04:37:52 +0000

    [uber] lazy load subpages
    depends on csilv's http://codereview.chromium.org/9234010/
    
    BUG=109183
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/9113098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/uber/uber.js
+++ chrome/browser/resources/uber/uber.js
@@ -40,6 +40,9 @@ cr.define('uber', function() {
     showPage(params.id, HISTORY_STATE_OPTION.REPLACE);
 
     window.addEventListener('message', handleWindowMessage);
+    window.setTimeout(function() {
+      document.documentElement.classList.remove('loading');
+    }, 0);
   }
 
   /**
@@ -194,6 +197,11 @@ cr.define('uber', function() {
     var lastSelected = document.querySelector('.iframe-container.selected');
     if (lastSelected === container)
       return;
+
+    // Lazy load of iframe contents.
+    var frame = container.querySelector('iframe');
+    if (!frame.getAttribute('src'))
+      frame.setAttribute('src', frame.dataset.url);
 
     if (lastSelected)
       lastSelected.classList.remove('selected');
