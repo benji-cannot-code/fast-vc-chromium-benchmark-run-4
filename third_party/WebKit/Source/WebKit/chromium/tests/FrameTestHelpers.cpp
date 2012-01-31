4a0072ac61c96fbb23ab49d23788fc57217636df
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-31 23:55:39 +0000

    [chromium] enable -Wexit-time-destructors for webkit unit tests https://bugs.webkit.org/show_bug.cgi?id=77300 Fix the only extant violation (verified by building webkit_unit_tests in static build).
    Patch by Ami Fischman <fischman@chromium.org> on 2012-01-31
    Reviewed by Tony Chang.
    
    * WebKitUnitTests.gyp:
    * tests/FrameTestHelpers.cpp:
    (WebKit::FrameTestHelpers::defaultWebFrameClient):
    (WebKit::FrameTestHelpers::defaultWebViewClient):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/FrameTestHelpers.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/FrameTestHelpers.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "FrameTestHelpers.h"
 
+#include "StdLibExtras.h"
 #include "WebFrame.h"
 #include "WebFrameClient.h"
 #include "WebSettings.h"
@@ -72,7 +73,7 @@ class TestWebFrameClient : public WebFrameClient {
 
 static WebFrameClient* defaultWebFrameClient()
 {
-    static TestWebFrameClient client;
+    DEFINE_STATIC_LOCAL(TestWebFrameClient, client, ());
     return &client;
 }
 
@@ -81,7 +82,7 @@ class TestWebViewClient : public WebViewClient {
 
 static WebViewClient* defaultWebViewClient()
 {
-    static TestWebViewClient client;
+    DEFINE_STATIC_LOCAL(TestWebViewClient,  client, ());
     return &client;
 }
 
