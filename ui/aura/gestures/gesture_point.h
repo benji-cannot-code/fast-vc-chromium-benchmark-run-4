ce8c14261520574d2cf38bd978610b0a539449a4
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 23:54:51 +0000

    Tweak the aura gesture recognizer slightly.
    * Reset the state of a GestureSequence after a tap/double-tap gesture.
    * Do not send scroll-update gestures when both delta-x/delta-y are zero.
    
    BUG=none
    TEST=aura_unittests:GestureRecognizerTest.GestureTapFollowedByScroll
    
    Review URL: https://chromiumcodereview.appspot.com/9318010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119989 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_point.h
+++ ui/aura/gestures/gesture_point.h
@@ -46,6 +46,7 @@ class GesturePoint {
   bool IsInDoubleClickWindow(const TouchEvent& event) const;
   bool IsInScrollWindow(const TouchEvent& event) const;
   bool IsInFlickWindow(const TouchEvent& event) const;
+  bool DidScroll(const TouchEvent& event) const;
 
   const gfx::Point& first_touch_position() const {
     return first_touch_position_;
