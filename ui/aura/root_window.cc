5f017aac0c5bb2ce791c4add3ec5201aeb168990
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 21:07:26 +0000

    Makes GetRootWindow() public on Window so we can begin to convert uses of RootWindow::GetInstance().
    http://crbug.com/112131
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9318002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119955 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window.cc
+++ ui/aura/root_window.cc
@@ -408,6 +408,16 @@ void RootWindow::SetGestureRecognizerForTesting(GestureRecognizer* gr) {
   gesture_recognizer_.reset(gr);
 }
 
+#if !defined(NDEBUG)
+void RootWindow::ToggleFullScreen() {
+  host_->ToggleFullScreen();
+}
+#endif
+
+RootWindow* RootWindow::GetRootWindow() {
+  return this;
+}
+
 void RootWindow::SetTransform(const ui::Transform& transform) {
   Window::SetTransform(transform);
 
@@ -416,12 +426,6 @@ void RootWindow::SetTransform(const ui::Transform& transform) {
   if (!layer()->GetAnimator()->is_animating())
     OnHostResized(host_->GetSize());
 }
-
-#if !defined(NDEBUG)
-void RootWindow::ToggleFullScreen() {
-  host_->ToggleFullScreen();
-}
-#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 // RootWindow, private:
@@ -629,10 +633,6 @@ bool RootWindow::CanReceiveEvents() const {
 }
 
 internal::FocusManager* RootWindow::GetFocusManager() {
-  return this;
-}
-
-RootWindow* RootWindow::GetRootWindow() {
   return this;
 }
 
--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -145,6 +145,15 @@ ui::Layer* Window::AcquireLayer() {
   return layer_owner_.release();
 }
 
+RootWindow* Window::GetRootWindow() {
+  return const_cast<RootWindow*>(
+      static_cast<const Window*>(this)->GetRootWindow());
+}
+
+const RootWindow* Window::GetRootWindow() const {
+  return parent_ ? parent_->GetRootWindow() : NULL;
+}
+
 void Window::Show() {
   SetVisible(true);
 }
@@ -509,10 +518,6 @@ bool Window::StopsEventPropagation() const {
       std::find_if(children_.begin(), children_.end(),
                    std::mem_fun(&aura::Window::IsVisible));
   return it != children_.end();
-}
-
-RootWindow* Window::GetRootWindow() {
-  return parent_ ? parent_->GetRootWindow() : NULL;
 }
 
 void Window::OnWindowDetachingFromRootWindow(aura::Window* window) {
