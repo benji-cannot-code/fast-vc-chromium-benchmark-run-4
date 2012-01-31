3d3dfd1ccf6d76114d628e8efa6c08a44fb051d0
mihnea@adobe.com <mihnea@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-31 22:55:00 +0000

    [CSSRegions]Reduce the cases when the box style in region is computed https://bugs.webkit.org/show_bug.cgi?id=77446
    Reviewed by David Hyatt.
    
    Covered by existing region style tests.
    
    * rendering/RenderRegion.cpp:
    (WebCore::RenderRegion::setRegionBoxesRegionStyle):
    (WebCore::RenderRegion::restoreRegionBoxesOriginalStyle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderRegion.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderRegion.cpp
@@ -99,6 +99,9 @@ bool RenderRegion::isLastRegion() const
 
 void RenderRegion::setRegionBoxesRegionStyle()
 {
+    if (!hasCustomRegionStyle())
+        return;
+
     for (RenderBoxRegionInfoMap::iterator iter = m_renderBoxRegionInfo.begin(), end = m_renderBoxRegionInfo.end(); iter != end; ++iter) {
         const RenderBox* box = iter->first;
         if (!box->canHaveRegionStyle())
@@ -116,6 +119,9 @@ void RenderRegion::setRegionBoxesRegionStyle()
 
 void RenderRegion::restoreRegionBoxesOriginalStyle()
 {
+    if (!hasCustomRegionStyle())
+        return;
+
     for (RenderBoxRegionInfoMap::iterator iter = m_renderBoxRegionInfo.begin(), end = m_renderBoxRegionInfo.end(); iter != end; ++iter) {
         const RenderBox* box = iter->first;
         RenderBoxRegionStyleMap::iterator it = m_renderBoxRegionStyle.find(box);
