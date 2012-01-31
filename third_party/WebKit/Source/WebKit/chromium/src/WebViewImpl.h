710b2de08a2377d2489de2593b54707eedfca02d
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-01-31 23:02:14 +0000

    [chromium] Expose windowResizerRectChanged() on WebWidget. https://bugs.webkit.org/show_bug.cgi?id=77453
    Needed for http://crbug.com/111266
    
    Reviewed by Darin Fisher.
    
    * public/WebWidget.h:
    (WebWidget):
    (WebKit::WebWidget::didChangeWindowResizerRect):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::didChangeWindowResizerRect):
    (WebKit):
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106401 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
@@ -133,6 +133,7 @@ public:
     virtual void didAcquirePointerLock();
     virtual void didNotAcquirePointerLock();
     virtual void didLosePointerLock();
+    virtual void didChangeWindowResizerRect();
 
     // WebView methods:
     virtual void initializeMainFrame(WebFrameClient*);
