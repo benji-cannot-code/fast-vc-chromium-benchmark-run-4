4d4178099c8592e2a0a3c90dd38107f5b669634b
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 23:54:16 +0000

    Field trial to investigate higher weightings for hits when calculating Omnibox prerender confidence.
    BUG=111069
    
    
    Review URL: http://codereview.chromium.org/9298032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120133 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_histograms.cc
+++ chrome/browser/prerender/prerender_histograms.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
+#include "base/stringprintf.h"
+#include "chrome/browser/autocomplete/network_action_predictor.h"
 #include "chrome/browser/prerender/prerender_util.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
 
@@ -44,7 +46,10 @@ std::string GetHistogramName(Origin origin, uint8 experiment_id,
 
   switch (origin) {
     case ORIGIN_OMNIBOX:
-      return ComposeHistogramName("omnibox", name);
+      return ComposeHistogramName(
+          StringPrintf("omnibox_%.1f",
+                       NetworkActionPredictor::get_hit_weight()).c_str(),
+          name);
     case ORIGIN_LINK_REL_PRERENDER:
       return ComposeHistogramName("web", name);
     case ORIGIN_GWS_PRERENDER:  // Handled above.
