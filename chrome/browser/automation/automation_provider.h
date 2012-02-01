2d92452f95e88a1efd2694065745f12c5ca76ae9
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 02:34:22 +0000

    Refactor AutomationProvider::InitialLoadsComplete.
    BUG=None.
    TEST=Run any test.
    
    
    Review URL: http://codereview.chromium.org/9301025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120016 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider.h
+++ chrome/browser/automation/automation_provider.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -117,6 +117,10 @@ class AutomationProvider
 
   // Called when the chromeos WebUI login is ready.
   void OnLoginWebuiReady();
+
+  // Checks all of the initial load conditions, then sends the
+  // InitialLoadsComplete message over the automation channel.
+  void SendInitialLoadMessage();
 
   // Get the index of a particular NavigationController object
   // in the given parent window.  This method uses
