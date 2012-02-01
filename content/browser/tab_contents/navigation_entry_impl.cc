6569efe4f741174a91826c17feee025e431e41e4
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 01:54:22 +0000

    Reloading page after installing app should bring it into correct process.
    BUG=80621
    TEST=Open a page which is part of an app, in another tab install the app, reload the original page. The renderer process for the tab should be switched.
    
    
    Review URL: http://codereview.chromium.org/9169065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120007 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/navigation_entry_impl.cc
+++ content/browser/tab_contents/navigation_entry_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,7 +45,8 @@ NavigationEntryImpl::NavigationEntryImpl()
       transition_type_(PAGE_TRANSITION_LINK),
       has_post_data_(false),
       restore_type_(RESTORE_NONE),
-      is_renderer_initiated_(false) {
+      is_renderer_initiated_(false),
+      is_cross_site_reload_(false) {
 }
 
 NavigationEntryImpl::NavigationEntryImpl(SiteInstanceImpl* instance,
@@ -66,7 +67,8 @@ NavigationEntryImpl::NavigationEntryImpl(SiteInstanceImpl* instance,
       transition_type_(transition_type),
       has_post_data_(false),
       restore_type_(RESTORE_NONE),
-      is_renderer_initiated_(is_renderer_initiated) {
+      is_renderer_initiated_(is_renderer_initiated),
+      is_cross_site_reload_(false) {
 }
 
 NavigationEntryImpl::~NavigationEntryImpl() {
