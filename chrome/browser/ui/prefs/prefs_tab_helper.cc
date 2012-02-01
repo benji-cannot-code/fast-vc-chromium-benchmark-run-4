ee097cbfd13cfb46b5aa5845fc5ae06993b5ffca
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 22:29:35 +0000

    Add per-tab XSSAuditorEnabled setting.
    There is no corresponding "global" setting, only a command-line flag.
    
    BUG=105537
    
    TBR=pkasting
    
    Review URL: https://chromiumcodereview.appspot.com/9264071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120115 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -47,7 +47,8 @@ const char* kPerTabPrefsToObserve[] = {
   prefs::kWebKitDefaultFontSize,
   prefs::kWebKitDefaultFixedFontSize,
   prefs::kWebKitMinimumFontSize,
-  prefs::kWebKitMinimumLogicalFontSize
+  prefs::kWebKitMinimumLogicalFontSize,
+  prefs::kWebKitXSSAuditorEnabled
 };
 
 const int kPerTabPrefsToObserveLength = arraysize(kPerTabPrefsToObserve);
@@ -108,6 +109,9 @@ static void RegisterPerTabUserPrefs(PrefService* prefs) {
                              PrefService::UNSYNCABLE_PREF);
   prefs->RegisterBooleanPref(prefs::kWebKitPluginsEnabled,
                              pref_defaults.plugins_enabled,
+                             PrefService::UNSYNCABLE_PREF);
+  prefs->RegisterBooleanPref(prefs::kWebKitXSSAuditorEnabled,
+                             pref_defaults.xss_auditor_enabled,
                              PrefService::UNSYNCABLE_PREF);
   RegisterFontsAndCharsetPrefs(prefs);
 }
@@ -435,6 +439,8 @@ void PrefsTabHelper::InitPerTabUserPrefStore(
   pref_store->RegisterOverlayPref(
       prefs::kWebKitMinimumLogicalFontSize,
       prefs::kWebKitGlobalMinimumLogicalFontSize);
+  pref_store->RegisterOverlayPref(
+      prefs::kWebKitXSSAuditorEnabled);
 }
 
 // static
@@ -642,6 +648,8 @@ void PrefsTabHelper::UpdateWebPreferences() {
       per_tab_prefs_->GetInteger(prefs::kWebKitMinimumFontSize);
   prefs.minimum_logical_font_size =
       per_tab_prefs_->GetInteger(prefs::kWebKitMinimumLogicalFontSize);
+  prefs.xss_auditor_enabled =
+      per_tab_prefs_->GetBoolean(prefs::kWebKitXSSAuditorEnabled);
   prefs.default_encoding =
       per_tab_prefs_->GetString(prefs::kDefaultCharset);
 
