d0f07acfd7455432641a938f96359c9e517cab59
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 20:07:07 +0000

    Make chrome://omnibox/ be able to report per-provider matches.
    This will be very useful for those of us working
    on improving the scoring/ranking of autocomplete
    suggestions.
    
    The presentation method could probably be improved, but I'd prefer
    to check this in as-is, and see how us omnibox developers use it in
    practice.  Then we'll have a better idea of how chrome://omnibox/
    should convey the information we most need.
    
    BUG=111791
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9289028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120090 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete.h
+++ chrome/browser/autocomplete/autocomplete.h
@@ -178,6 +178,7 @@ struct AutocompleteMatch;
 class AutocompleteProvider;
 class AutocompleteResult;
 class KeywordProvider;
+class OmniboxUIHandler;
 class Profile;
 class SearchProvider;
 class TemplateURL;
@@ -685,6 +686,7 @@ class AutocompleteController : public ACProviderListener {
   const AutocompleteInput& input() const { return input_; }
   const AutocompleteResult& result() const { return result_; }
   bool done() const { return done_; }
+  const ACProviders* providers() const { return &providers_; }
 
   // From AutocompleteProvider::Listener
   virtual void OnProviderUpdate(bool updated_matches);
