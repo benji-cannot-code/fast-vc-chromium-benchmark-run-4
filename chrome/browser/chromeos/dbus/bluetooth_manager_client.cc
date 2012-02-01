bed3f748cdfe3d53d95d8ead069a3a2dd27c52ad
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 04:02:36 +0000

    chrome: bluetooth: hook up the AdapterAdded signal
    The manager sends signals when additional adapters (including the
    initial) are added, we may need these to obtain properties before
    the default adapter is switched so hook up that signal so the
    observer can take it.
    
    BUG=chromium-os:22086
    TEST=verified signals are received
    
    Change-Id: I895c699eaef2f77f70ad855601b16e9a7c731ebb
    
    
    Review URL: http://codereview.chromium.org/9302027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/bluetooth_manager_client.cc
+++ chrome/browser/chromeos/dbus/bluetooth_manager_client.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,6 +28,14 @@ class BluetoothManagerClientImpl : public BluetoothManagerClient {
     bluetooth_manager_proxy_ = bus->GetObjectProxy(
         bluetooth_manager::kBluetoothManagerServiceName,
         bluetooth_manager::kBluetoothManagerServicePath);
+
+    bluetooth_manager_proxy_->ConnectToSignal(
+        bluetooth_manager::kBluetoothManagerInterface,
+        bluetooth_manager::kAdapterAddedSignal,
+        base::Bind(&BluetoothManagerClientImpl::AdapterAddedReceived,
+                   weak_ptr_factory_.GetWeakPtr()),
+        base::Bind(&BluetoothManagerClientImpl::AdapterAddedConnected,
+                   weak_ptr_factory_.GetWeakPtr()));
 
     bluetooth_manager_proxy_->ConnectToSignal(
         bluetooth_manager::kBluetoothManagerInterface,
@@ -80,18 +88,39 @@ class BluetoothManagerClientImpl : public BluetoothManagerClient {
   }
 
  private:
+  // Called by dbus:: when an AdapterAdded signal is received.
+  void AdapterAddedReceived(dbus::Signal* signal) {
+    DCHECK(signal);
+    dbus::MessageReader reader(signal);
+    std::string object_path;
+    if (!reader.PopObjectPath(&object_path)) {
+      LOG(ERROR) << "AdapterAdded signal has incorrect parameters: "
+          << signal->ToString();
+      return;
+    }
+    VLOG(1) << "Adapter added: " << object_path;
+    FOR_EACH_OBSERVER(Observer, observers_, AdapterAdded(object_path));
+  }
+
+  // Called by dbus:: when the AdapterAdded signal is initially connected.
+  void AdapterAddedConnected(const std::string& interface_name,
+                             const std::string& signal_name,
+                             bool success) {
+    LOG_IF(WARNING, !success) << "Failed to connect to AdapterAdded signal.";
+  }
+
   // Called by dbus:: when an AdapterRemoved signal is received.
   void AdapterRemovedReceived(dbus::Signal* signal) {
     DCHECK(signal);
     dbus::MessageReader reader(signal);
-    std::string adapter;
-    if (!reader.PopObjectPath(&adapter)) {
+    std::string object_path;
+    if (!reader.PopObjectPath(&object_path)) {
       LOG(ERROR) << "AdapterRemoved signal has incorrect parameters: "
           << signal->ToString();
       return;
     }
-    VLOG(1) << "Adapter removed: " << adapter;
-    FOR_EACH_OBSERVER(Observer, observers_, AdapterRemoved(adapter));
+    VLOG(1) << "Adapter removed: " << object_path;
+    FOR_EACH_OBSERVER(Observer, observers_, AdapterRemoved(object_path));
   }
 
   // Called by dbus:: when the AdapterRemoved signal is initially connected.
