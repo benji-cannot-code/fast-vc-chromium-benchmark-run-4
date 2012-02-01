7c4af9c29b34747388d27ffc2fd9a3b0024d2dc2
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 20:26:58 +0000

    Removing unnecessary includes of the content\browser\child_process_security_policy.h file in preparation for creating a public content API around the ChildProcessSecurityPolicy class.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9307023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120096 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_message_service.cc
+++ chrome/browser/extensions/extension_message_service.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_messages.h"
-#include "content/browser/child_process_security_policy.h"
 #include "content/browser/renderer_host/render_view_host.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
