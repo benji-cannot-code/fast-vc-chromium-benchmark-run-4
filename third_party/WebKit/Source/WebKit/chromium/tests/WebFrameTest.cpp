c1a2ce60d827d049584673a5b7b6ea1ace2dc9b6
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-01 09:01:30 +0000

    [Chromium] Disable WebFrameTest.FindInPage https://bugs.webkit.org/show_bug.cgi?id=77186
    Disable it because PlatformContextSkiaTest.trackOpaqueOvalTest has
    been failing since FindInPage test was committed.
    
    * tests/WebFrameTest.cpp:
    (WebKit::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -382,7 +382,7 @@ TEST_F(WebFrameTest, ContextNotificationsIsolatedWorlds)
     EXPECT_EQ(1, matchCount);
 }
 
-TEST_F(WebFrameTest, FindInPage)
+TEST_F(WebFrameTest, DISABLED_FindInPage)
 {
     registerMockedHttpURLLoad("find.html");
     WebView* webView = FrameTestHelpers::createWebViewAndLoad(m_baseURL + "find.html");
