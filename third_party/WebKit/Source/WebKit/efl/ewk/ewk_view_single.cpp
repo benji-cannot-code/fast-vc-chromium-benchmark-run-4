2d5d480b2376273782b28c6b76f0b693f2c9cc4a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-01 03:38:27 +0000

    [EFL] Set content hint information for ewk_view_single. https://bugs.webkit.org/show_bug.cgi?id=77319
    Patch by KwangHyuk Kim <hyuki.kim@samsung.com> on 2012-01-31
    Reviewed by Ryosuke Niwa.
    
    The content hint information corresponding to opengl_x11 engine is set
    for the image object which ewk_view_single owns when evas is based on
    opengl_x11 engine.
    
    * ewk/ewk_view_single.cpp:
    (_ewk_view_single_smart_backing_store_add):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_single.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view_single.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_logging.h"
 #include "ewk_private.h"
 
+#include <Ecore_Evas.h>
 #include <Evas.h>
 #include <eina_safety_checks.h>
 #include <string.h>
@@ -58,6 +59,12 @@ static void _ewk_view_single_smart_add(Evas_Object* ewkView)
 static Evas_Object* _ewk_view_single_smart_backing_store_add(Ewk_View_Smart_Data* smartData)
 {
     Evas_Object* bs = evas_object_image_add(smartData->base.evas);
+    EINA_SAFETY_ON_NULL_RETURN_VAL(bs, 0);
+    const Ecore_Evas* ecoreEvas = ecore_evas_ecore_evas_get(smartData->base.evas);
+    const char* engine = ecore_evas_engine_name_get(ecoreEvas);
+    if (!strncmp(engine, "opengl_x11", strlen("opengl_x11")))
+        evas_object_image_content_hint_set(bs, EVAS_IMAGE_CONTENT_HINT_DYNAMIC);
+
     evas_object_image_alpha_set(bs, false);
     evas_object_image_smooth_scale_set(bs, smartData->zoom_weak_smooth_scale);
 
