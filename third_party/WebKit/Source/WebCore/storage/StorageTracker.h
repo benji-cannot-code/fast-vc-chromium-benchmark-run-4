cfd417a9fa5c955b45ff71ecb97dfce3e6f402b6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-01 23:32:37 +0000

    Make one able to set the local storage (tracker) database dir's path https://bugs.webkit.org/show_bug.cgi?id=77006
    Patch by Gustavo Lima Chaves <glima@profusion.mobi> on 2012-02-01
    Reviewed by Darin Adler.
    
    There are no behavior changes with the diff, so no need for new tests.
    
    * storage/StorageTracker.cpp:
    (WebCore::StorageTracker::setDatabaseDirectoryPath):
    (WebCore):
    (WebCore::StorageTracker::databaseDirectoryPath):
    * storage/StorageTracker.h:
    (StorageTracker):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/storage/StorageTracker.h
+++ third_party/WebKit/Source/WebCore/storage/StorageTracker.h
@@ -48,6 +48,9 @@ public:
     static void initializeTracker(const String& storagePath, StorageTrackerClient*);
     static StorageTracker& tracker();
 
+    void setDatabaseDirectoryPath(const String&);
+    String databaseDirectoryPath() const;
+
     void setOriginDetails(const String& originIdentifier, const String& databaseFile);
     
     void deleteAllOrigins();
