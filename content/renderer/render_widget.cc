bdd056bf6bc3fec39c24f262ba535c1d3741eab7
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 23:29:37 +0000

    Revert 120111 - Defer render_widget draw until host window is available
    In accelerated compositing mode we can't start rendering until the
    native window (HWND/gtk widget) are created on Windows and GTK without
    aura. For most render_widgets the code today is racy, but for
    window.open() and similar creation paths this is a race that we pretty
    much always lose. This defers the first DoDeferredUpdate on a render_widget
    until its host_window_ is available.
    
    Also defers querying for GL_CHROMIUM_supports_swapbuffers_callback
    extension until the first draw, since the context isn't ready until the
    host window is available.
    
    BUG=106815
    TEST=start with --force-compositing-mode and --show-fps-counter, load up
    a page that uses window.open() to open a popup, and confirm that the
    popup is in compositing mode
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=119977
    
    Review URL: http://codereview.chromium.org/9225050
    
    TBR=jamesr@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9307043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120130 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -176,8 +176,6 @@ void RenderWidget::CompleteInit(gfx::NativeViewId parent_hwnd) {
 
   host_window_ = parent_hwnd;
 
-  DoDeferredUpdate();
-
   Send(new ViewHostMsg_RenderViewReady(routing_id_));
 }
 
@@ -740,11 +738,6 @@ void RenderWidget::DoDeferredUpdate() {
 
   if (!webwidget_)
     return;
-
-  if (!host_window_) {
-    TRACE_EVENT0("renderer", "EarlyOut_NoHostWindow");
-    return;
-  }
   if (update_reply_pending_) {
     TRACE_EVENT0("renderer", "EarlyOut_UpdateReplyPending");
     return;
@@ -762,9 +755,6 @@ void RenderWidget::DoDeferredUpdate() {
     TRACE_EVENT0("renderer", "EarlyOut_NotVisible");
     return;
   }
-
-  if (is_accelerated_compositing_active_)
-    using_asynchronous_swapbuffers_ = SupportsAsynchronousSwapBuffers();
 
   // Tracking of frame rate jitter
   base::TimeTicks frame_begin_ticks = base::TimeTicks::Now();
@@ -1021,6 +1011,10 @@ void RenderWidget::didActivateCompositor(int compositor_identifier) {
   is_accelerated_compositing_active_ = true;
   Send(new ViewHostMsg_DidActivateAcceleratedCompositing(
       routing_id_, is_accelerated_compositing_active_));
+
+  // Note: asynchronous swapbuffer support currently only matters if
+  // compositing scheduling happens on the RenderWidget.
+  using_asynchronous_swapbuffers_ = SupportsAsynchronousSwapBuffers();
 }
 
 void RenderWidget::didDeactivateCompositor() {
