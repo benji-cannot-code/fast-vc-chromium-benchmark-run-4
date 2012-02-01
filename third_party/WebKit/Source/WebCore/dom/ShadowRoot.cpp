cf6300850a81021505b1622e00ee34e6f2bf5072
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-01 06:39:11 +0000

    Content element should be able to be dynamically added/removed/replaced in a shadow tree. https://bugs.webkit.org/show_bug.cgi?id=76611
    Patch by Shinya Kawanaka <shinyak@google.com> on 2012-01-31
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    When a content element is added/removed/replaced in a shadow tree, we have to recreate
    the shadow tree to recalculate inclusions of content elements. Currently we didn't recalculate it
    when content element is removed. (When added, it is recalculated.)
    This patch enables us to recalcurate the shadow tree when content element is removed.
    
    Test: fast/dom/shadow/content-element-move.html
    
    * dom/Element.cpp:
    (WebCore::Element::attach):
      If a shadow root exists, attaches shadow tree before attaching child elements.
    * dom/ShadowRoot.cpp:
    (WebCore::ShadowRoot::recalcShadowTreeStyle):
      Recalculates light children and shadow tree.
    (WebCore::ShadowRoot::reattachHostChildrenAndShadow):
      Detaches shadow tree and host light children, and attaches them again.
    * dom/ShadowRoot.h:
      Added a flag to recalculate shadow tree.
    (WebCore::ShadowRoot::setNeedsShadowTreeStyleRecalc):
    (WebCore::ShadowRoot::clearNeedsShadowTreeStyleRecalc):
    (WebCore::ShadowRoot::needsShadowTreeStyleRecalc):
    * html/shadow/HTMLContentElement.cpp:
    (WebCore::HTMLContentElement::attach):
      Does not need to detach included elements, because they are not attached in ContainerNode anymore.
    (WebCore::HTMLContentElement::detach):
      When a content element detached, reattaches a shadow tree.
    
    LayoutTests:
    
    Test cases for appending/removing/replacing content element in a shadow tree.
    
    * fast/dom/shadow/content-element-move-expected.txt: Added.
    * fast/dom/shadow/content-element-move.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
@@ -100,8 +100,8 @@ bool ShadowRoot::childTypeAllowed(NodeType type) const
 
 void ShadowRoot::recalcShadowTreeStyle(StyleChange change)
 {
-    if (hasContentElement())
-        reattach();
+    if (needsShadowTreeStyleRecalc() || hasContentElement())
+        reattachHostChildrenAndShadow();
     else {
         for (Node* n = firstChild(); n; n = n->nextSibling()) {
             if (n->isElementNode())
@@ -111,6 +111,7 @@ void ShadowRoot::recalcShadowTreeStyle(StyleChange change)
         }
     }
 
+    clearNeedsShadowTreeStyleRecalc();
     clearNeedsStyleRecalc();
     clearChildNeedsStyleRecalc();
 }
@@ -167,6 +168,24 @@ void ShadowRoot::attach()
     TreeScope::attach();
     if (m_inclusions)
         m_inclusions->didSelect();
+}
+
+void ShadowRoot::reattachHostChildrenAndShadow()
+{
+    if (!host())
+        return;
+
+    for (Node* child = host()->firstChild(); child; child = child->nextSibling()) {
+        if (child->attached())
+            child->detach();
+    }
+
+    reattach();
+
+    for (Node* child = host()->firstChild(); child; child = child->nextSibling()) {
+        if (!child->attached())
+            child->attach();
+    }
 }
 
 ContentInclusionSelector* ShadowRoot::inclusions() const
