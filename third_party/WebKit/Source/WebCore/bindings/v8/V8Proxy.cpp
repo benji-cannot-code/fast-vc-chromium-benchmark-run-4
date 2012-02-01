0102d98c032ff9c6726b3744c520c8eb99d1c0f3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-01 21:18:23 +0000

    Unreviewed, rolling out r106382. http://trac.webkit.org/changeset/106382 https://bugs.webkit.org/show_bug.cgi?id=77571
    Causing chromium crashes in PNGImageDecoder (Requested by
    japhet on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-02-01
    
    Source/WebCore:
    
    * bindings/v8/V8Proxy.cpp:
    (WebCore::V8Proxy::evaluate):
    * page/Console.cpp:
    (WebCore::Console::time):
    (WebCore::Console::timeEnd):
    * platform/chromium/PlatformSupport.h:
    * platform/chromium/TraceEvent.h:
    (internal):
    (ScopeTracer):
    (WebCore::internal::ScopeTracer::ScopeTracer):
    (WebCore::internal::ScopeTracer::~ScopeTracer):
    
    Source/WebKit/chromium:
    
    * public/platform/WebKitPlatformSupport.h:
    (WebKitPlatformSupport):
    (WebKit::WebKitPlatformSupport::isTraceEventEnabled):
    (WebKit::WebKitPlatformSupport::traceEventBegin):
    (WebKit::WebKitPlatformSupport::traceEventEnd):
    * src/PlatformSupport.cpp:
    (WebCore::PlatformSupport::isTraceEventEnabled):
    (WebCore):
    (WebCore::PlatformSupport::traceEventBegin):
    (WebCore::PlatformSupport::traceEventEnd):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106487 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.cpp
@@ -72,10 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/UnusedParam.h>
 #include <wtf/text/WTFString.h>
 
-#if PLATFORM(CHROMIUM)
-#include "TraceEvent.h"
-#endif
-
 namespace WebCore {
 
 static V8Extensions& staticExtensionsList()
@@ -340,7 +336,7 @@ v8::Local<v8::Value> V8Proxy::evaluate(const ScriptSourceCode& source, Node* nod
         // Compile the script.
         v8::Local<v8::String> code = v8ExternalString(source.source());
 #if PLATFORM(CHROMIUM)
-        TRACE_EVENT_BEGIN0("v8", "v8.compile");
+        PlatformSupport::traceEventBegin("v8.compile", node, "");
 #endif
         OwnPtr<v8::ScriptData> scriptData = precompileScript(code, source.cachedScript());
 
@@ -348,11 +344,15 @@ v8::Local<v8::Value> V8Proxy::evaluate(const ScriptSourceCode& source, Node* nod
         // 1, whereas v8 starts at 0.
         v8::Handle<v8::Script> script = compileScript(code, source.url(), source.startPosition(), scriptData.get());
 #if PLATFORM(CHROMIUM)
-        TRACE_EVENT_END0("v8", "v8.compile");
-        TRACE_EVENT0("v8", "v8.run");
+        PlatformSupport::traceEventEnd("v8.compile", node, "");
+
+        PlatformSupport::traceEventBegin("v8.run", node, "");
 #endif
         result = runScript(script);
     }
+#if PLATFORM(CHROMIUM)
+    PlatformSupport::traceEventEnd("v8.run", node, "");
+#endif
 
     InspectorInstrumentation::didEvaluateScript(cookie);
 
