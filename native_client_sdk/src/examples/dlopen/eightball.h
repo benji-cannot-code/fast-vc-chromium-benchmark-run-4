32db11ea85a5e11f6663e1f08741f833f3487ce4
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-01 22:34:58 +0000

    Support GLBIC example in SDK
    This CL added two new examples for the NaCl SDK.  This only affects SDK bots since
    the code is completely contained within native_client_sdk subtree.
    
    Fix incorrectly placed ppapi headers.
    Remove TODOs and cleanup create_nmf.py.
    Add hello_world_glibc example
    Add dlopen example.
    
    BUG= 111224
    Review URL: https://chromiumcodereview.appspot.com/9234043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120117 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/examples/dlopen/eightball.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef __EIGHTBALL_H__
+#define __EIGHTBALL_H__
+
+/* Return an answer. Question not required */
+typedef char* (*TYPE_eightball)(void);
+extern "C" const char* Magic8Ball();
+
+#endif  /* __EIGHTBALL_H__ */
+
---
+++ native_client_sdk/src/examples/hello_world_glibc/helper_functions.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <algorithm>
+
+#include "helper_functions.h"
+
+namespace hello_world {
+
+int32_t FortyTwo() {
+  return 42;
+}
+
+std::string ReverseText(const std::string& text) {
+  std::string reversed_string(text);
+  // Use reverse to reverse |reversed_string| in place.
+  std::reverse(reversed_string.begin(), reversed_string.end());
+  return reversed_string;
+}
+}  // namespace hello_world
+
