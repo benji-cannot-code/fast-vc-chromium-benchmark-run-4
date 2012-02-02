d20c20a39a9d06337bff593e087b3f24c61198f9
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-02 09:56:20 +0000

    Fix attrition experiment toast regression due to startup refactoring.
    Make sure early return from PreCreateThreads aborts the startup sequence.
    
    BUG=110671
    TEST=Launch Chrome with --try-chrome-again=0, select "Dont bug me" and Chrome should not launch.
    Review URL: https://chromiumcodereview.appspot.com/9150033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120186 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.h
+++ chrome_frame/test/net/fake_external_tab.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -105,7 +105,7 @@ class CFUrlRequestUnittestRunner
   virtual void PreMainMessageLoopStart() OVERRIDE {}
   virtual void PostMainMessageLoopStart() OVERRIDE {}
   virtual void ToolkitInitialized() OVERRIDE {}
-  virtual void PreCreateThreads() OVERRIDE;
+  virtual int PreCreateThreads() OVERRIDE;
   virtual void PreMainMessageLoopRun() OVERRIDE;
   virtual bool MainMessageLoopRun(int* result_code) OVERRIDE;
   virtual void PostMainMessageLoopRun() OVERRIDE;
