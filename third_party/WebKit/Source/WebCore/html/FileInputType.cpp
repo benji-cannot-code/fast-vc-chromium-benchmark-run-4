ccd550d524b4e37862069cc1b7cdcff2dfa47beb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-02 08:09:07 +0000

    hidden attribute on <input type=file /> suppresses the file selection dialog https://bugs.webkit.org/show_bug.cgi?id=58208
    Patch by Rakesh KN <rakesh.kn@motorola.com> on 2012-02-02
    Reviewed by Ryosuke Niwa.
    
    .:
    
    * ManualTests/input-file-hidden-open-dialog-on-click.html: Added.
    Adding manual test as the file dialog opens only in User gesture.
    
    Source/WebCore:
    
    We want to allow opening file dialog on hidden element when click() is called
    on the element. The behaviour is similar to Firefox and IE.
    
    Added manual test as the file dialog opens only on user gesture.
    
    * html/FileInputType.cpp:
    (WebCore::FileInputType::handleDOMActivateEvent):
    Removed renderer check as input can be hidden.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/FileInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/FileInputType.cpp
@@ -140,7 +140,7 @@ String FileInputType::valueMissingText() const
 
 void FileInputType::handleDOMActivateEvent(Event* event)
 {
-    if (element()->disabled() || !element()->renderer())
+    if (element()->disabled())
         return;
 
     if (!ScriptController::processingUserGesture())
