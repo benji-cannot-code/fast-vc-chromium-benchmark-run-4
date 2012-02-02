0b78cbabe49463b977035fa3bbc2e3e05e69cc13
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-02 15:27:31 +0000

    Disabling ExtensionUnpackerTest.EmptyDefaultLocale
    Crashed twice today, most recently at http://build.chromium.org/p/chromium/builders/Vista%20Tests%20%283%29/builds/11178/steps/unit_tests/logs/stdio
    
    BUG=108724
    TBR=aa@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9310062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_unpacker_unittest.cc
+++ chrome/common/extensions/extension_unpacker_unittest.cc
@@ -55,7 +55,7 @@ public:
 
 // Crashes intermittently on Windows, see http://crbug.com/109238
 #if defined(OS_WIN)
-#define MAYBE_EmptyDefaultLocale EmptyDefaultLocale
+#define MAYBE_EmptyDefaultLocale DISABLED_EmptyDefaultLocale
 #else
 #define MAYBE_EmptyDefaultLocale EmptyDefaultLocale
 #endif
