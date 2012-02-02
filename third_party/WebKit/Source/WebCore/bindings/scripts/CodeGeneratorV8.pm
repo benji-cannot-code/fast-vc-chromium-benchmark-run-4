810b88900af515feec8fe4cd63cc45bf98de3cbb
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-02 07:07:50 +0000

    Rename [ConvertNullToNullString] to [TreatNullAs=EmptyString] https://bugs.webkit.org/show_bug.cgi?id=77602
    Reviewed by Adam Barth.
    
    This patch renames [ConvertNullToNullString] to [TreatNullAs=EmptyString],
    according to the spec (http://dev.w3.org/2006/webapi/WebIDL/#TreatNullAs).
    
    No tests. No changes in behavior.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (JSValueToNative):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GetNativeTypeFromSignature):
    
    * css/CSSCharsetRule.idl:
    * css/CSSPageRule.idl:
    * css/CSSRule.idl:
    * css/CSSStyleDeclaration.idl:
    * css/CSSStyleRule.idl:
    * css/CSSValue.idl:
    * css/MediaList.idl:
    * css/WebKitCSSKeyframesRule.idl:
    * dom/Attr.idl:
    * dom/CharacterData.idl:
    * dom/DOMImplementation.idl:
    * dom/Document.idl:
    * dom/Element.idl:
    * dom/NamedNodeMap.idl:
    * dom/Node.idl:
    * dom/ProcessingInstruction.idl:
    * dom/ShadowRoot.idl:
    * html/HTMLAnchorElement.idl:
    * html/HTMLButtonElement.idl:
    * html/HTMLDocument.idl:
    * html/HTMLElement.idl:
    * html/HTMLFormElement.idl:
    * html/HTMLFrameElement.idl:
    * html/HTMLInputElement.idl:
    * html/HTMLMediaElement.idl:
    * html/HTMLOutputElement.idl:
    * html/HTMLScriptElement.idl:
    * html/HTMLSelectElement.idl:
    * html/HTMLTextAreaElement.idl:
    * html/HTMLTitleElement.idl:
    * html/canvas/CanvasRenderingContext2D.idl:
    * page/DOMWindow.idl:
    * storage/StorageEvent.idl:
    * svg/SVGAngle.idl:
    * svg/SVGElement.idl:
    * svg/SVGLength.idl:
    * svg/SVGScriptElement.idl:
    
    * bindings/scripts/test/TestObj.idl: No change in the run-bindings-tests results.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -3337,7 +3337,7 @@ sub GetNativeTypeFromSignature
         my $mode = "";
         if ($signature->extendedAttributes->{"ConvertUndefinedOrNullToNullString"}) {
             $mode = "WithUndefinedOrNullCheck";
-        } elsif ($signature->extendedAttributes->{"ConvertNullToNullString"} || $signature->extendedAttributes->{"Reflect"}) {
+        } elsif (($signature->extendedAttributes->{"TreatNullAs"} and $signature->extendedAttributes->{"TreatNullAs"} eq "EmptyString") or $signature->extendedAttributes->{"Reflect"}) {
             $mode = "WithNullCheck";
         }
         $type .= "<$mode>";
