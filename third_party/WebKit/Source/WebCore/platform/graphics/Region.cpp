2c573b39124c1b93a6f0c3ebd123e7c391a9cad0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-02 02:46:10 +0000

    Unreviewed, rolling out r106408. http://trac.webkit.org/changeset/106408 https://bugs.webkit.org/show_bug.cgi?id=77592
    crashes in chromium mac release tests (Requested by japhet on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-02-01
    
    * platform/graphics/Region.cpp:
    * platform/graphics/Region.h:
    (Region):
    (Shape):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/Region.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/Region.cpp
@@ -66,11 +66,6 @@ Vector<IntRect> Region::rects() const
     return rects;
 }
 
-bool Region::contains(const Region& region) const
-{
-    return WebCore::intersect(region, *this) == region;
-}
-
 Region::Shape::Shape()
 {
 }
