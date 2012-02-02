062107e9a7963d2045eacb06a0697dd6d6a86baa
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-02 01:56:58 +0000

    Mac-ews logs are huge https://bugs.webkit.org/show_bug.cgi?id=77045
    Reviewed by Eric Seidel.
    
    In order to get the cr-linux-ews bot to run tests on EC2, we created
    the chromium-xvfb port, which contained a number of tweaks to the
    Chromium port's behavior.  This patch refactors those tweaks so they
    can be shared with the mac-ews, mostly by moving them into the
    non-interactive mode of RunTests.
    
    * Scripts/webkitpy/common/config/ports.py:
    (WebKitPort):
    (WebKitPort.layout_tests_results_path):
    (ChromiumPort.run_javascriptcore_tests_command):
    (ChromiumXVFBPort):
    (ChromiumXVFBPort.run_webkit_tests_command):
    * Scripts/webkitpy/common/config/ports_mock.py:
    (MockPort):
    (MockPort.layout_tests_results_path):
    * Scripts/webkitpy/common/config/ports_unittest.py:
    (WebKitPortTest.test_chromium_xvfb_port):
    * Scripts/webkitpy/tool/bot/layouttestresultsreader_unittest.py:
    (LayoutTestResultsReaderTest.test_missing_layout_test_results):
    * Scripts/webkitpy/tool/commands/queues_unittest.py:
    (CommitQueueTest.test_commit_queue):
    (test_rollout):
    (test_manual_reject_during_processing):
    * Scripts/webkitpy/tool/steps/runtests.py:
    (RunTests.run):
    * Scripts/webkitpy/tool/steps/runtests_unittest.py:
    (RunTestsTest.test_no_unit_tests):
    * Scripts/webkitpy/tool/steps/steps_unittest.py:
    (StepsTest.test_runtests_args):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/config/ports_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/config/ports_mock.py
@@ -29,11 +29,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 class MockPort(object):
+    results_directory = "/mock-results"
+
     def name(self):
         return "MockPort"
 
     def layout_tests_results_path(self):
-        return "/mock-results/results.html"
+        return "/mock-results/full_results.json"
 
     def check_webkit_style_command(self):
         return ["mock-check-webkit-style"]
