e25183fd5ebb1ff76039683805af2681c6cf7907
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-02 02:27:18 +0000

    Remove [ObjCPrefix], [V8ConstructorTemplate], [allowAccessToNode], [v8implname] and [v8referenceattr] from code generators https://bugs.webkit.org/show_bug.cgi?id=77588
    Reviewed by Adam Barth.
    
    This patch removes [ObjCPrefix], [V8ConstructorTemplate], [allowAccessToNode],
    [v8implname] and [v8referenceattr] from code generators, since they are not used
    by any IDL files.
    
    No tests. No changes in behavior.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorObjC.pm:
    (GenerateHeader):
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorV8.pm:
    (IsConstructable):
    (IsConstructorTemplate):
    (GenerateNormalAttrGetter):
    (GenerateFunctionCallString):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorObjC.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorObjC.pm
@@ -878,10 +878,8 @@ sub GenerateHeader
                 $typesToForwardDeclare{$param->type} = 1;
 
                 if ($parameterIndex >= 1) {
-                    my $paramPrefix = $param->extendedAttributes->{"ObjCPrefix"};
-                    $paramPrefix = $paramName unless defined($paramPrefix);
-                    $functionSig .= " $paramPrefix";
-                    $methodName .= $paramPrefix;
+                    $functionSig .= " $paramName";
+                    $methodName .= $paramName;
                 }
 
                 $functionSig .= ":($paramType)$paramName";
@@ -1505,9 +1503,7 @@ sub GenerateImplementation
                 }
 
                 if ($parameterIndex >= 1) {
-                    my $paramPrefix = $param->extendedAttributes->{"ObjCPrefix"};
-                    $paramPrefix = $param->name unless defined($paramPrefix);
-                    $functionSig .= " $paramPrefix";
+                    $functionSig .= " " . $param->name;
                 }
 
                 $functionSig .= ":($paramType)$paramName";
