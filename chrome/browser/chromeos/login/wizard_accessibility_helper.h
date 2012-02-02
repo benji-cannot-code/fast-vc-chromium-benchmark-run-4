af33735a6245a4bcc6941b79d535da13541e6dc0
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-02 04:42:56 +0000

    Remove unused chromeos/login/wizard_accessibility_* code
    Many code are not used since we switched to WebUI login.
    Only MaybeSpeak is not removed because it's still used.
    
    BUG=None
    TEST=try -b linux_chromeos
    
    
    Review URL: http://codereview.chromium.org/9307029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120163 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_accessibility_helper.h
+++ chrome/browser/chromeos/login/wizard_accessibility_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,17 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_WIZARD_ACCESSIBILITY_HELPER_H_
 #pragma once
 
-#include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
-#include "chrome/browser/chromeos/login/wizard_accessibility_handler.h"
-#include "content/public/browser/notification_registrar.h"
-#include "ui/base/accelerators/accelerator.h"
-#include "ui/base/keycodes/keyboard_codes.h"
-
-class Profile;
-namespace views {
-class View;
-}
 
 namespace chromeos {
 
@@ -28,24 +18,12 @@ class WizardAccessibilityHelper {
   // Get Singleton instance of WizardAccessibilityHelper.
   static WizardAccessibilityHelper* GetInstance();
 
-  // Get accelerator for enabling accessibility.
-  static ui::Accelerator GetAccelerator();
-
   // Speak the given text if the accessibility pref is already set. |queue|
   // specifies whether this utterance will be queued or spoken immediately.
   // |interruptible| specified whether this utterance can be flushed by a
   // subsequent utterance.
   // TODO (chaitanyag): Change API to use string16 instead of char*.
   void MaybeSpeak(const char* str, bool queue, bool interruptible);
-
-  // Unregisters all accessibility notifications
-  void UnregisterNotifications();
-
-  // Toggles accessibility support.
-  void ToggleAccessibility();
-
-  // Initialize accessibility
-  void Init();
 
  private:
   friend struct DefaultSingletonTraits<WizardAccessibilityHelper>;
@@ -54,20 +32,7 @@ class WizardAccessibilityHelper {
 
   virtual ~WizardAccessibilityHelper();
 
-  void RegisterNotifications();
-
   bool IsAccessibilityEnabled();
-
-  void SetAccessibilityEnabled(bool);
-
-  scoped_ptr<WizardAccessibilityHandler> accessibility_handler_;
-
-  Profile* profile_;
-
-  // Used for tracking registrations to accessibility notifications.
-  content::NotificationRegistrar registrar_;
-
-  bool registered_notifications_;
 
   DISALLOW_COPY_AND_ASSIGN(WizardAccessibilityHelper);
 };
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1356,7 +1356,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/login/signed_settings_helper_unittest.cc',
         'browser/chromeos/login/signed_settings_cache_unittest.cc',
         'browser/chromeos/login/signed_settings_unittest.cc',
-        'browser/chromeos/login/wizard_accessibility_handler_unittest.cc',
         'browser/chromeos/mobile_config_unittest.cc',
         'browser/chromeos/network_message_observer_unittest.cc',
         'browser/chromeos/notifications/desktop_notifications_unittest.cc',
