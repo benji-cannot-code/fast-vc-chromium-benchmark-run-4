f8368eaf3d5f259f5b98779a89162c582a3eb309
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 04:23:34 +0000

    [Sync] Fix DCHECK() following AwaitQuiescence() failure
    Basically WaitUntilTimestampMatches() should be the one that sets
    timestamp_match_partner_ to NULL, as AwaitStatusChangeWithTimeout()
    only does so when the status change happens (i.e., not when it times
    out).
    
    Re-enable migration tests that were running into this DCHECK. (The
    original problem might still be there, but I was unable to repro
    locally, so it may be fixed.  If it still fails, I will investigate
    further.)
    
    BUG=100382
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9309071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/migration_errors_test.cc
+++ chrome/browser/sync/test/integration/migration_errors_test.cc
@@ -345,18 +345,16 @@ class MigrationTwoClientTest : public MigrationTest {
 
 // Easiest possible test of migration errors: triggers a server
 // migration on one datatype, then modifies some other datatype.
-// Crashy. crbug.com/100382.
 IN_PROC_BROWSER_TEST_F(MigrationTwoClientTest,
-                       DISABLED_MigratePrefsThenModifyBookmark) {
+                       MigratePrefsThenModifyBookmark) {
   RunTwoClientMigrationTest(MakeList(syncable::PREFERENCES),
                             MODIFY_BOOKMARK);
 }
 
 // Triggers a server migration on two datatypes, then makes a local
 // modification to one of them.
-// Crashy. crbug.com/100382.
 IN_PROC_BROWSER_TEST_F(MigrationTwoClientTest,
-                       DISABLED_MigratePrefsAndBookmarksThenModifyBookmark) {
+                       MigratePrefsAndBookmarksThenModifyBookmark) {
   RunTwoClientMigrationTest(
       MakeList(syncable::PREFERENCES, syncable::BOOKMARKS),
       MODIFY_BOOKMARK);
@@ -436,9 +434,7 @@ IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabs) {
   ASSERT_TRUE(GetClient(0)->IsTypePreferred(syncable::SESSIONS));
 }
 
-// Crashy. crbug.com/100382.
-IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest,
-                       DISABLED_SetSyncTabsAndMigrate) {
+IN_PROC_BROWSER_TEST_F(MigrationReconfigureTest, SetSyncTabsAndMigrate) {
   if (!ServerSupportsErrorTriggering()) {
     LOG(WARNING) << "Test skipped in this server environment.";
     return;
