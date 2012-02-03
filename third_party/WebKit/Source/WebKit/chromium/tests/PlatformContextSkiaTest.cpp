fe53e3f5ca2d9465331f341f7fea43b87b28e833
jknotten@chromium.org <jknotten@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-03 17:31:20 +0000

    [skia] Fix unit test PlatformContextSkiaTest.trackOpaqueOvalTest https://bugs.webkit.org/show_bug.cgi?id=77738
    We need to clear the bitmap before the test runs, otherwise the
    contents of the bitmap will be uninitialised. This causes an
    unpredicatable result as the canvas draw operations blend the
    destination bitmap with the source colour.
    
    Similarly, valgrind discovered that
    PlatformContextSkiaTest.trackOpaqueTest and
    PlatformContextSkiaTest.trackOpaqueLineTest suffers from the same
    problem.
    
    Also, re-enable WebFrameTest.FindInPage which was falsely
    blamed for making this test fail.
    
    Reviewed by Stephen White.
    
    * tests/PlatformContextSkiaTest.cpp:
    (WebCore::TEST):
    * tests/WebFrameTest.cpp:
    (WebKit::TEST_F):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/PlatformContextSkiaTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/PlatformContextSkiaTest.cpp
@@ -59,6 +59,7 @@ TEST(PlatformContextSkiaTest, trackOpaqueTest)
     SkBitmap bitmap;
     bitmap.setConfig(SkBitmap::kARGB_8888_Config, 400, 400);
     bitmap.allocPixels();
+    bitmap.eraseColor(0);
     SkCanvas canvas(bitmap);
 
     PlatformContextSkia platformContext(&canvas);
@@ -260,6 +261,7 @@ TEST(PlatformContextSkiaTest, trackOpaqueLineTest)
     SkBitmap bitmap;
     bitmap.setConfig(SkBitmap::kARGB_8888_Config, 200, 200);
     bitmap.allocPixels();
+    bitmap.eraseColor(0);
     SkCanvas canvas(bitmap);
 
     PlatformContextSkia platformContext(&canvas);
@@ -499,6 +501,7 @@ TEST(PlatformContextSkiaTest, trackOpaqueOvalTest)
     SkBitmap bitmap;
     bitmap.setConfig(SkBitmap::kARGB_8888_Config, 200, 200);
     bitmap.allocPixels();
+    bitmap.eraseColor(0);
     SkCanvas canvas(bitmap);
 
     PlatformContextSkia platformContext(&canvas);
