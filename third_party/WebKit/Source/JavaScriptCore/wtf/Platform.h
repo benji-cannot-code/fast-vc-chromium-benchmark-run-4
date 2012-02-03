18da03f9673041e46919e5b6dc125440b476c040
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-03 12:51:42 +0000

    [Qt] Replace GNU linker script for exports with export macros in WTF/JSC https://bugs.webkit.org/show_bug.cgi?id=77723
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2012-02-03
    Reviewed by Tor Arne Vestbø.
    
    .:
    
    * Source/api.pri: Remove use of GNU linker script on Linux.
    * Source/qtwebkit-export.map: Removed.
    
    Source/JavaScriptCore:
    
    * wtf/Platform.h: Enable use of export macros.
    
    Tools:
    
    * Tools.pro: Compile WTR again on all platforms with WK2.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106650 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -1114,7 +1114,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    since most ports try to support sub-project independence, adding new headers
    to WTF causes many ports to break, and so this way we can address the build
    breakages one port at a time. */
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || PLATFORM(QT)
 #define WTF_USE_EXPORT_MACROS 1
 #else
 #define WTF_USE_EXPORT_MACROS 0
