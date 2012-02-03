df7f4991c29ee7d6fe2ee5411e638d0e4dbb64bd
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 00:57:52 +0000

    [Sync] Ensure we always re-encrypt with newest gaia password if available.
    Previously we would only re-encrypt an account that had implicit passphrases if,
    on a password change, the client was already signed in. Otherwise, for example
    if the user signs in to a new client, we would disregard the current credentials
    because they are unable to decrypt the existing encrypted data (based on the old
    gaia password).
    
    This change ensures we do not throw away the old credentials. If we have pending
    keys, we go ahead and generate new encryption keys based on the new credentials
    and set those keys as the default. In addition, we persist the keys in the
    bootstrap token for the cryptographer. In this way, the bootstrap token for
    accounts using implicit passphrases will always correspond to the encryption
    keys derived from the most recent gaia password.
    
    Once the user supplies the old gaia password (which they will be prompted for
    due to the cryptographer having pending keys), we will install it, but then
    restore the encryption keys based on the current gaia password as the default.
    This decision is made based on whether the pending keybag contains the current
    default key or not. If it does, then the keybag must be newer than our data,
    and we let it overwrite the default. If it does not, then our current key
    is the newest, and we persist the current default.
    
    In order to support this change, we plumb from the setup flow whether a
    passphrase originates from the user or from an internal source (the
    signin successful notification). In this way we can differentiate the current
    implicit passphrase from an older user provided one.
    
    BUG=104508
    TEST=sync_unit_tests. All existing passphrase tests. Manually signing into
    a clean account, setting up sync, signing out, changing the password, then
    signing in to a new client. The new client should prompt for the old password,
    but once provided should re-encrypt all data with the new password.
    Subsequent new clients should not prompt for any old password.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=120157
    
    Review URL: https://chromiumcodereview.appspot.com/9309022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/passwords_helper.cc
+++ chrome/browser/sync/test/integration/passwords_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -108,7 +108,9 @@ void RemoveLogins(PasswordStore* store) {
 
 void SetPassphrase(int index, const std::string& passphrase) {
   test()->GetProfile(index)->GetProfileSyncService()->SetPassphrase(
-      passphrase, true);
+      passphrase,
+      ProfileSyncService::EXPLICIT,
+      ProfileSyncService::USER_PROVIDED);
 }
 
 PasswordStore* GetPasswordStore(int index) {
