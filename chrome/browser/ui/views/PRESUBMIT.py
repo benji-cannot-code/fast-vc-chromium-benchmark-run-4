04c0363506c52ae3d9314eee8d8632bab5e8a74d
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 01:05:49 +0000

    Renamed old bot to existing trybot.
    TBR=ben@chromium.org
    BUG=112154
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9325016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120259 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/PRESUBMIT.py
+++ chrome/browser/ui/views/PRESUBMIT.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -10,4 +10,4 @@ for more details on the presubmit API built into gcl.
 """
 
 def GetPreferredTrySlaves():
-  return ['linux_chromeos_aura:compile', 'linux_chromeos']
+  return ['linux_chromeos']
--- ui/resources/aura/PRESUBMIT.py
+++ ui/resources/aura/PRESUBMIT.py
@@ -10,4 +10,4 @@ for more details on the presubmit API built into gcl.
 """
 
 def GetPreferredTrySlaves():
-  return ['linux_chromeos_aura:compile']
+  return ['linux_chromeos']
