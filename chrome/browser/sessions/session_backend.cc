7526839cf36b1c239e29d31a3160af814ecf2dca
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 06:44:58 +0000

    Revert 120283 - net: Use ClosePlatformFile() instead of close/CloseHandle().
    Review URL: http://codereview.chromium.org/9315062
    
    TBR=satorux@chromium.org
    
    We're guessing that this is causing timeouts on the Windows browser tests,
    see http://build.chromium.org/p/chromium/builders/Win7%20Tests%20%28dbg%29%286%29/builds/2695/steps/browser_tests/logs/stdio
    for example. :(
    Review URL: https://chromiumcodereview.appspot.com/9307081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_backend.cc
+++ chrome/browser/sessions/session_backend.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/memory/scoped_vector.h"
 #include "base/metrics/histogram.h"
-#include "base/threading/thread_restrictions.h"
 #include "net/base/file_stream.h"
 #include "net/base/net_errors.h"
 
@@ -345,11 +344,6 @@ bool SessionBackend::AppendCommandsToFile(net::FileStream* file,
 }
 
 SessionBackend::~SessionBackend() {
-  if (current_session_file_.get()) {
-    // Close() performs file IO. crbug.com/112512.
-    base::ThreadRestrictions::ScopedAllowIO allow_io;
-    current_session_file_->Close();
-  }
 }
 
 void SessionBackend::ResetFile() {
