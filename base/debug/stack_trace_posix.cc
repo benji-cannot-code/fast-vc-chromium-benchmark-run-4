00585cfaa20051d05b1d55f3559d50eb8a2aee4a
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 04:02:18 +0000

    Get rid of <iostream> include in stack_trace_posix (forces a static initializer)
    BUG=94925
    TEST=sizes reports one less SI
    
    
    Review URL: http://codereview.chromium.org/9315044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120278 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/stack_trace_posix.cc
+++ base/debug/stack_trace_posix.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include <AvailabilityMacros.h>
 #endif
-
-#include <iostream>
 
 #include "base/basictypes.h"
 #include "base/eintr_wrapper.h"
@@ -171,7 +169,7 @@ void StackTrace::PrintBacktrace() const {
   std::vector<std::string> trace_strings;
   GetBacktraceStrings(trace_, count_, &trace_strings, NULL);
   for (size_t i = 0; i < trace_strings.size(); ++i) {
-    std::cerr << "\t" << trace_strings[i] << "\n";
+    fprintf(stderr, "\t%s\n", trace_strings[i].c_str());
   }
 }
 
