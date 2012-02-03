4082e4ad5a5e80be14b0cf4736e3ef6589e42e0f
marshall@chromium.org <marshall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 15:41:43 +0000

    Add a source_frame_id member to OpenURLParams for passage to WebContentsDelegate::OpenURLFromTab. This is necessary in order for the browser to open the request in the correct frame.
    BUG=112289
    TEST=NONE
    Review URL: https://chromiumcodereview.appspot.com/9283033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120333 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/page_navigator.cc
+++ content/public/browser/page_navigator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,13 +15,30 @@ OpenURLParams::OpenURLParams(
     bool is_renderer_initiated)
     : url(url),
       referrer(referrer),
+      source_frame_id(-1),
+      disposition(disposition),
+      transition(transition),
+      is_renderer_initiated(is_renderer_initiated) {
+}
+
+OpenURLParams::OpenURLParams(
+    const GURL& url,
+    const Referrer& referrer,
+    int64 source_frame_id,
+    WindowOpenDisposition disposition,
+    PageTransition transition,
+    bool is_renderer_initiated)
+    : url(url),
+      referrer(referrer),
+      source_frame_id(source_frame_id),
       disposition(disposition),
       transition(transition),
       is_renderer_initiated(is_renderer_initiated) {
 }
 
 OpenURLParams::OpenURLParams()
-    : disposition(UNKNOWN),
+    : source_frame_id(-1),
+      disposition(UNKNOWN),
       transition(PageTransitionFromInt(0)),
       is_renderer_initiated(false) {
 }
--- content/public/browser/page_navigator.h
+++ content/public/browser/page_navigator.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,11 +30,20 @@ struct CONTENT_EXPORT OpenURLParams {
                 WindowOpenDisposition disposition,
                 PageTransition transition,
                 bool is_renderer_initiated);
+  OpenURLParams(const GURL& url,
+                const Referrer& referrer,
+                int64 source_frame_id,
+                WindowOpenDisposition disposition,
+                PageTransition transition,
+                bool is_renderer_initiated);
   ~OpenURLParams();
 
   // The URL/referrer to be opened.
   GURL url;
   Referrer referrer;
+
+  // The source frame id or -1 to indicate the main frame.
+  int64 source_frame_id;
 
   // The disposition requested by the navigation source.
   WindowOpenDisposition disposition;
