f34fafb5f385b946f47c74a377b04964fd020d9f
alicet@chromium.org <alicet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 21:53:24 +0000

    Update widget initialization for screen lock, settings bubble and menus to set up parent container before widget initialization. This allows widget to be initiated with proper parent bounds taken into initial bounds calculation.
    In particular, in compact mode, the default container layer have different bounds and transformation than all other layers. If windows are initialized with the default container and then added to the intended containers, bounds and transformation of the new windows may need to be updated after this move between layers.
    
    BUG=112322
    TEST=tested on alex for screenlock/volume/brightness bubbles to show up even when the user was on a new window in compact mode.
    
    
    Review URL: http://codereview.chromium.org/9314041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120413 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_display_host.cc
+++ chrome/browser/chromeos/login/webui_login_display_host.cc
@@ -143,6 +143,9 @@ void WebUILoginDisplayHost::LoadURL(const GURL& url) {
     params.bounds = background_bounds();
 #if defined(USE_AURA)
     params.show_state = ui::SHOW_STATE_FULLSCREEN;
+    params.parent =
+        ash::Shell::GetInstance()->GetContainer(
+            ash::internal::kShellWindowId_LockScreenContainer);
 #endif
 
     login_window_ = new views::Widget;
@@ -152,9 +155,6 @@ void WebUILoginDisplayHost::LoadURL(const GURL& url) {
     login_view_->Init(login_window_);
 
 #if defined(USE_AURA)
-    ash::Shell::GetInstance()->GetContainer(
-        ash::internal::kShellWindowId_LockScreenContainer)->
-        AddChild(login_window_->GetNativeView());
     ash::SetWindowVisibilityAnimationDuration(
         login_window_->GetNativeView(),
         base::TimeDelta::FromMilliseconds(kLoginFadeoutTransitionDurationMs));
