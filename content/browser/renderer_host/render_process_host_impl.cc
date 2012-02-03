15139acd829e2fd4c67017c753c63889477905d3
antonm@chromium.org <antonm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 13:39:45 +0000

    Do not use setDartFlags API as we're now passing flags by other means.
    Review URL: http://codereview.chromium.org/9307062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120323 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -626,7 +626,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kAuditAllHandles,
     switches::kAuditHandles,
     switches::kChromeFrame,
-    switches::kDartFlags,
     switches::kDisable3DAPIs,
     switches::kDisableAcceleratedCompositing,
     switches::kDisableApplicationCache,
--- webkit/tools/test_shell/test_shell_main.cc
+++ webkit/tools/test_shell/test_shell_main.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -285,11 +285,6 @@ int main(int argc, char* argv[]) {
   if (parsed_command_line.HasSwitch(test_shell::kHeapProfiler)) {
     WebScriptController::registerExtension(
         extensions_v8::HeapProfilerExtension::Get());
-  }
-
-  if (parsed_command_line.HasSwitch(test_shell::kDartFlags)) {
-    webkit_glue::SetDartFlags(
-        parsed_command_line.GetSwitchValueASCII(test_shell::kDartFlags));
   }
 
   // Load and initialize the stats table.  Attempt to construct a somewhat
