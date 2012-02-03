28b97a7fb3569fa90605f8ec602e95e8c39299ed
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-03 04:51:18 +0000

    Support 'disabled' attribute on SVGStyleElement https://bugs.webkit.org/show_bug.cgi?id=52130
    Source/WebCore:
    
    Adding disabled property to SVGStyleElement, which
    mirrors the functionality added to HTMLStyleElement
    for DOM1. The disabled property reflects and sets
    the disabled state of its style sheet.
    
    Based off of patch for bug 25287
    
    Patch by Bear Travis <betravis@adobe.com> on 2012-02-02
    Reviewed by Dirk Schulze.
    
    Test: svg/dom/SVGStyleElement/disable-svg-style-element.html
    
    * svg/SVGStyleElement.cpp:
    (WebCore::SVGStyleElement::disabled):
    (WebCore):
    (WebCore::SVGStyleElement::setDisabled):
    * svg/SVGStyleElement.h:
    (SVGStyleElement):
    * svg/SVGStyleElement.idl:
    
    LayoutTests:
    
    Test the disabled property of an svg style element.
    Based off of the patch for bug 25287.
    
    Patch by Bear Travis <betravis@adobe.com> on 2012-02-02
    Reviewed by Dirk Schulze.
    
    * svg/dom/SVGStyleElement/disable-svg-style-element-expected.txt: Added.
    * svg/dom/SVGStyleElement/disable-svg-style-element.html: Added.
    * svg/dom/SVGStyleElement/script-tests/disable-svg-style-element.js: Added.
    (createStyle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106623 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/svg/dom/SVGStyleElement/script-tests/disable-svg-style-element.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test the disabled property on an svg style element.');
+
+var console = document.getElementById('console');
+
+// Setup
+function createStyle(ns, type, ruletext) {
+    var style = document.createElementNS(ns, "style");
+    var rules = document.createTextNode(ruletext);
+    style.appendChild(rules);
+    style.type = type;
+    return style;
+}
+
+var xmlns = "http://www.w3.org/2000/svg";
+var svg = document.createElementNS(xmlns, "svg");
+svg.style.display = "block";
+
+var defs = document.createElementNS(xmlns, "defs");
+var styleElement = createStyle(xmlns, "text/css", "rect { fill: #0000ff; }");
+var otherStyleElement = createStyle(xmlns, "foo/bar", "");
+defs.appendChild(styleElement);
+defs.appendChild(otherStyleElement);
+svg.appendChild(defs);
+
+var rect = document.createElementNS(xmlns, "rect");
+rect.setAttribute("width", 100);
+rect.setAttribute("height", 100);
+svg.appendChild(rect);
+
+document.body.appendChild(svg);
+
+// Simple case
+shouldBeFalse('styleElement.disabled');
+shouldBe('window.getComputedStyle(rect).fill', '"#0000ff"');
+
+styleElement.disabled = true
+shouldBeTrue('styleElement.disabled');
+shouldBe('window.getComputedStyle(rect).fill', '"#ff0000"');
+
+// Test disconnected element
+var newStyleElement = document.createElementNS(xmlns, 'style');
+shouldBeFalse('newStyleElement.disabled');
+newStyleElement.disabled = true
+shouldBeFalse('newStyleElement.disabled');
+
+// Test non-CSS element
+shouldBeFalse('otherStyleElement.disabled');
+otherStyleElement.disabled = true
+shouldBeFalse('otherStyleElement.disabled');
+
+document.body.removeChild(svg);
