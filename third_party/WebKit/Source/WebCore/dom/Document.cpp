840987e38e93e237f980fa2bf956372f25914cf8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-03 07:30:20 +0000

    Use content-language from http-equiv to set document locale and font https://bugs.webkit.org/show_bug.cgi?id=76701
    Patch by Matt Falkenhagen <falken@chromium.org> on 2012-02-02
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    So far, only content-language set through http-equiv is used; the HTTP Content-Language
    header is not yet supported.
    
    Tests: fast/text/content-language-case-insensitivity.html
           fast/text/content-language-mapped-to-webkit-locale.html
           fast/text/international/content-language-font-selection-expected.html
           fast/text/international/content-language-font-selection.html
           fast/text/international/content-language-with-subtags-expected.html
           fast/text/international/content-language-with-subtags.html
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::styleForDocument): Use content language when selecting initial font.
    * dom/Document.cpp:
    (WebCore::Document::setContentLanguage): Recalculate style so language is taken into account.
    (WebCore):
    * dom/Document.h:
    (Document):
    
    LayoutTests:
    
    Add tests that content-language from http-equiv sets document locale and is used in font selection.
    
    * fast/text/content-language-case-insensitivity-expected.txt: Added.
    * fast/text/content-language-case-insensitivity.html: Added.
    * fast/text/content-language-mapped-to-webkit-locale-expected.txt: Added.
    * fast/text/content-language-mapped-to-webkit-locale.html: Added.
    * fast/text/international/content-language-font-selection-expected.html: Added.
    * fast/text/international/content-language-font-selection.html: Added.
    * fast/text/international/content-language-with-subtags-expected.html: Added.
    * fast/text/international/content-language-with-subtags.html: Added.
    * fast/text/lang-mapped-to-webkit-locale-expected.txt:
    * fast/text/lang-mapped-to-webkit-locale.xhtml: Fix some style issues.
    * fast/text/xml-lang-ignored-in-html-expected.txt:
    * fast/text/xml-lang-ignored-in-html.html: Fix some style issues.
    * platform/efl/Skipped: Skip tests using overridePreference("WebKit*FontMap"...) on non-Chromium ports.
    * platform/gtk-wk2/Skipped: Ditto.
    * platform/gtk/Skipped: Ditto.
    * platform/mac/Skipped: Ditto.
    * platform/qt/Skipped: Ditto.
    * platform/win/Skipped: Ditto.
    * platform/wincairo/Skipped: Ditto.
    * platform/wk2/Skipped: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106632 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -1103,6 +1103,16 @@ void Document::setCharset(const String& charset)
     decoder()->setEncoding(charset, TextResourceDecoder::UserChosenEncoding);
 }
 
+void Document::setContentLanguage(const String& language)
+{
+    if (m_contentLanguage == language)
+        return;
+    m_contentLanguage = language;
+
+    // Recalculate style so language is used when selecting the initial font.
+    styleSelectorChanged(DeferRecalcStyle);
+}
+
 void Document::setXMLVersion(const String& version, ExceptionCode& ec)
 {
     if (!implementation()->hasFeature("XML", String())) {
