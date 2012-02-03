970cd73b9a913537a16b8ec49a950be4569628fe
droger@google.com <droger@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 18:19:15 +0000

    Move CookieStore unittests to a separate file.
    This is a follow up from http://codereview.chromium.org/8953028/.
    This allows other CookieStore implementations to benefit from the tests.
    
    BUG=NONE
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9310051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120355 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/base/cookie_util_unittest.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/basictypes.h"
+#include "net/base/cookie_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(CookieUtilTest, TestDomainIsHostOnly) {
+  const struct {
+    const char* str;
+    const bool is_host_only;
+  } tests[] = {
+    { "",               true },
+    { "www.google.com", true },
+    { ".google.com",    false }
+  };
+
+  for (size_t i = 0; i < ARRAYSIZE_UNSAFE(tests); ++i) {
+    EXPECT_EQ(tests[i].is_host_only,
+              net::cookie_util::DomainIsHostOnly(tests[i].str));
+  }
+}
