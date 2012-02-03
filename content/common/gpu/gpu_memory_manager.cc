f28adbae0471e03f1bf9817527b2b86ec4ef9207
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 17:49:30 +0000

    Replacing static initializer with enum in header.
    TBR=mmocny
    BUG=None
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9309084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120347 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_memory_manager.cc
+++ content/common/gpu/gpu_memory_manager.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/gpu_command_buffer_stub.h"
 #include "content/common/gpu/gpu_memory_allocation.h"
 
-const size_t GpuMemoryManager::kDefaultMaxSurfacesWithFrontbufferSoftLimit = 8;
-
 namespace {
 
 // These are predefined values (in bytes) for
