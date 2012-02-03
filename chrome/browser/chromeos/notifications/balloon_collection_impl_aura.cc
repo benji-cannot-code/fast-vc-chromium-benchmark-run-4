4e9f5025a721315f82dbdd5476b50623744ebcc7
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 18:16:20 +0000

    Fix remaining Clang errors.
    TBR=ben@chromium.org
    BUG=112464
    TEST=clang happy
    
    Review URL: https://chromiumcodereview.appspot.com/9317083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120354 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/notifications/balloon_collection_impl_aura.cc
+++ chrome/browser/chromeos/notifications/balloon_collection_impl_aura.cc
@@ -15,6 +15,9 @@ namespace chromeos {
 BalloonCollectionImplAura::BalloonCollectionImplAura() {
 }
 
+BalloonCollectionImplAura::~BalloonCollectionImplAura() {
+}
+
 bool BalloonCollectionImplAura::AddWebUIMessageCallback(
     const Notification& notification,
     const std::string& message,
--- chrome/browser/ui/views/tab_contents/native_tab_contents_container_aura.h
+++ chrome/browser/ui/views/tab_contents/native_tab_contents_container_aura.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,7 +34,7 @@ class NativeTabContentsContainerAura : public NativeTabContentsContainer,
   virtual void RequestFocus() OVERRIDE;
   virtual void AboutToRequestFocusFromTabTraversal(bool reverse) OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
-  virtual gfx::NativeViewAccessible GetNativeViewAccessible();
+  virtual gfx::NativeViewAccessible GetNativeViewAccessible() OVERRIDE;
 
  private:
   TabContentsContainer* container_;
