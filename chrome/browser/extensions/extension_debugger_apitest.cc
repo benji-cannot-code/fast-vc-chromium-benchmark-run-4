387c676be461c39ae5d93a0845c617dcc6eda2c3
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 08:53:03 +0000

    DevTools: re-enable chrome.debugger tests after the version number bump.
    TBR=yurys
    
    Review URL: https://chromiumcodereview.appspot.com/9309076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120300 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_debugger_apitest.cc
+++ chrome/browser/extensions/extension_debugger_apitest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-// TODO(pfeldman): uncomment after the WebKit roll.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Debugger) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Debugger) {
   ASSERT_TRUE(RunExtensionTest("debugger")) << message_;
 }
