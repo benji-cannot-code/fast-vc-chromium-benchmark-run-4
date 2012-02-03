5a295a235de0a9367983baf200c751556ac9e1f5
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 09:06:49 +0000

    [cros] Tries online login in background to detect password change.
    If pasword change is detected, user is redirected to GAIA login flow. Also note about password change is shown.
    
    BUG=chromium-os:24984
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9225044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120301 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_display.h
+++ chrome/browser/chromeos/login/login_display.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -104,6 +104,9 @@ class LoginDisplay : public RemoveUserDelegate {
   virtual void ShowError(int error_msg_id,
                          int login_attempts,
                          HelpAppLauncher::HelpTopic help_topic_id) = 0;
+
+  // Proceed with Gaia flow because password has changed.
+  virtual void ShowGaiaPasswordChanged(const std::string& username) = 0;
 
   gfx::Rect background_bounds() const { return background_bounds_; }
   void set_background_bounds(const gfx::Rect background_bounds){
