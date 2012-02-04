787f9aabd40b8e24ad9ef790fc868981f5110c69
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 04:30:25 +0000

    mark SOCKS4_HTTP_GET and SOCKS4_SSL_GET as flaky on mac.
    BUG=112682
    TEST=none
    TBR=satorux
    
    
    Review URL: http://codereview.chromium.org/9325050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120467 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -5357,7 +5357,13 @@ TEST_F(HttpNetworkTransactionTest, BuildRequest_ExtraHeadersStripped) {
   EXPECT_EQ(OK, rv);
 }
 
-TEST_F(HttpNetworkTransactionTest, SOCKS4_HTTP_GET) {
+#if defined(OS_MACOSX)
+#define MAYBE_SOCKS4_HTTP_GET FLAKY_SOCKS4_HTTP_GET
+#else
+#define MAYBE_SOCKS4_HTTP_GET SOCKS4_HTTP_GET
+#endif
+
+TEST_F(HttpNetworkTransactionTest, MAYBE_SOCKS4_HTTP_GET) {
   HttpRequestInfo request;
   request.method = "GET";
   request.url = GURL("http://www.google.com/");
@@ -5408,7 +5414,13 @@ TEST_F(HttpNetworkTransactionTest, SOCKS4_HTTP_GET) {
   EXPECT_EQ("Payload", response_text);
 }
 
-TEST_F(HttpNetworkTransactionTest, SOCKS4_SSL_GET) {
+#if defined(OS_MACOSX)
+#define MAYBE_SOCKS4_SSL_GET FLAKY_SOCKS4_SSL_GET
+#else
+#define MAYBE_SOCKS4_SSL_GET SOCKS4_SSL_GET
+#endif
+
+TEST_F(HttpNetworkTransactionTest, MAYBE_SOCKS4_SSL_GET) {
   HttpRequestInfo request;
   request.method = "GET";
   request.url = GURL("https://www.google.com/");
