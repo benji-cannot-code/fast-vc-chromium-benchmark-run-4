4d8e6f46460d30c66f991dc82f812bfdf0c12338
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 02:01:22 +0000

    chromeos: bluetooth: tie Proxy lifetime to object, not observer
    Change the lifetime of Object Proxies to the remote object, not any
    Observer; one of the problems with the previous method was adding
    a second observer would treat every new attempt to add an observer
    as a new proxy so reconnect signals, etc.
    
    The other main problem is that you couldn't call methods unless there
    was an observer that had created the object proxy.
    
    BUG=chromium-os:22086
    TEST=verified methods still work
    
    Change-Id: Ic58b88bbad84c7728abd16ddc48d11174f59f59f
    
    
    Review URL: http://codereview.chromium.org/9314013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/bluetooth/bluetooth_adapter.cc
+++ chrome/browser/chromeos/bluetooth/bluetooth_adapter.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,12 +23,12 @@ class BluetoothAdapterImpl : public BluetoothAdapter,
     bluetooth_adapter_client_ =
         dbus_thread_manager->GetBluetoothAdapterClient();
     DCHECK(bluetooth_adapter_client_);
-    bluetooth_adapter_client_->AddObserver(this, id_);
+    bluetooth_adapter_client_->AddObserver(this);
   }
 
   virtual ~BluetoothAdapterImpl() {
     DCHECK(bluetooth_adapter_client_);
-    bluetooth_adapter_client_->RemoveObserver(this, id_);
+    bluetooth_adapter_client_->RemoveObserver(this);
   }
 
   virtual void AddObserver(BluetoothAdapter::Observer* observer) {
