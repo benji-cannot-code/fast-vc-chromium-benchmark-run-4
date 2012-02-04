16ca8d8299fc7c3135cd3557ac2a0839ac94d01f
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 02:11:32 +0000

    Revert 120448 - Revert 120018 - Revert 120005 - Enabled pressing TAB to traverse through the Omnibox results, removed moving the caret to the end of the line with TAB, and filtered out redundant URLs.
    This adds the ability to move through Omnibox result matches using TAB in addition to the arrow keys. To enable this, pressing TAB to move the caret to the end of the line was removed and the keyword hint/shortcut logic has been modified.
    
    The Omnibox popup now shows keyword markers on the right side of matches that have associated keywords (represented by a right arrow). When this kind of match is selected, and the keyword is accepted, the match changes appearance by animating in the associated keyword match from the right to display the "Search X for <>" message. 
    
    If multiple matches have the same keyword then only the most relevant match will display the keyword marker and hint. Pressing TAB while a keyword hint is shown will enter keyword mode in place; the results will no longer change when keyword mode is entered. Additionally, substituting keyword provider matches will only be shown if a keyword substitution
    is available.  
    
    Finally, results with redundant destination URLs (e.g., "foo.com", "www.foo.com") will have the less relevant URLs filtered out.
    
    This also addresses some GTK omnibox browsertest flakiness; see bug 112041.
    
    Contributed by aaron.randolph@gmail.com
    
    BUG=57748,76278,77662,80934,84420
    TEST=Press TAB to move the selection down the list of results, SHIFT+TAB to move up.
    
    Review URL: http://codereview.chromium.org/6731036
    
    TBR=aaron.randolph@gmail.com
    Review URL: https://chromiumcodereview.appspot.com/9310017
    
    TBR=kinuko@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9309097
    
    TBR=pkasting@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120457 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/keycodes/keyboard_code_conversion_mac.mm
+++ ui/base/keycodes/keyboard_code_conversion_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -32,7 +32,6 @@ bool operator<(const KeyCodeMap& a, const KeyCodeMap& b) {
 const KeyCodeMap kKeyCodesMap[] = {
   { VKEY_BACK /* 0x08 */, kVK_Delete, kBackspaceCharCode },
   { VKEY_TAB /* 0x09 */, kVK_Tab, kTabCharCode },
-  { VKEY_BACKTAB /* 0x0A */, 0x21E4, '\031' },
   { VKEY_CLEAR /* 0x0C */, kVK_ANSI_KeypadClear, kClearCharCode },
   { VKEY_RETURN /* 0x0D */, kVK_Return, kReturnCharCode },
   { VKEY_SHIFT /* 0x10 */, kVK_Shift, 0 },
