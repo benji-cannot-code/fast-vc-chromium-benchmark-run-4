d105b4343266c42d3daa87ea4a888b2b5a0b6202
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 16:49:16 +0000

    Fix Autofill hang when interacting with large forms (i.e. forms containing many fields).
    BUG=111319
    TEST=browser_tests --gtest_filter=FormAutofillTest.*
    
    
    Review URL: http://codereview.chromium.org/9225042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120483 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/form_cache.cc
+++ chrome/renderer/autofill/form_cache.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -41,13 +41,6 @@ namespace {
 // label for each field is a costly operation and we can't spare the cycles if
 // it's not necessary.
 const size_t kRequiredAutofillFields = 3;
-
-// The maximum number of form fields we are willing to parse, due to
-// computational costs.  Several examples of forms with lots of fields that are
-// not relevant to Autofill: (1) the Netflix queue; (2) the Amazon wishlist;
-// (3) router configuration pages; and (4) other configuration pages, e.g. for
-// Google code project settings.
-const size_t kMaxParseableFields = 100;
 
 }  // namespace
 
@@ -93,15 +86,17 @@ void FormCache::ExtractForms(const WebFrame& frame,
       }
     }
 
-    // To avoid overly expensive computation, we impose both a minimum and a
-    // maximum number of allowable fields.
-    if (control_elements.size() < kRequiredAutofillFields ||
-        control_elements.size() > kMaxParseableFields)
+    // To avoid overly expensive computation, we impose a minimum number of
+    // allowable fields.  The corresponding maximum number of allowable fields
+    // is imposed by WebFormElementToFormData().
+    if (control_elements.size() < kRequiredAutofillFields)
       continue;
 
     FormData form;
-    WebFormElementToFormData(form_element, WebFormControlElement(),
-                             REQUIRE_NONE, EXTRACT_VALUE, &form, NULL);
+    if (!WebFormElementToFormData(form_element, WebFormControlElement(),
+                                  REQUIRE_NONE, EXTRACT_VALUE, &form, NULL)) {
+      continue;
+    }
 
     num_fields_seen += form.fields.size();
     if (num_fields_seen > kMaxParseableFields)
