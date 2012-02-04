5f055343a96c675d959f9a0dea020b165c52f5d7
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 16:49:00 +0000

    Fix a tiny typo in the comment on pp::Instance::Init().
    Review URL: http://codereview.chromium.org/9310068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120481 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/instance.h
+++ ppapi/cpp/instance.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -86,7 +86,7 @@ class Instance {
   /// match the indices of the corresponding names in <code>argn</code>.
   ///
   /// @return true on success. Returning false causes the instance to be
-  /// instance to be deleted and no other functions to be called.
+  /// deleted and no other functions to be called.
   virtual bool Init(uint32_t argc, const char* argn[], const char* argv[]);
 
   /// @{
