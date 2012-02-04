9cbffd9a199fea812c223c15e3e6fd1518740197
dslomov@google.com <dslomov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-04 01:45:14 +0000

    [Chromium] WebCore::toV8Context crashes if DomWindow::frame() returns null. https://bugs.webkit.org/show_bug.cgi?id=77686.
    Reviewed by Adam Barth.
    
    * bindings/v8/V8Helpers.cpp:
    (WebCore::toV8Context):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Helpers.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Helpers.cpp
@@ -43,7 +43,7 @@ v8::Local<v8::Context> toV8Context(NPP npp, NPObject* npObject)
 {
     V8NPObject* object = reinterpret_cast<V8NPObject*>(npObject);
     DOMWindow* domWindow = object->rootObject;
-    if (!domWindow || domWindow != domWindow->frame()->domWindow())
+    if (!domWindow || !domWindow->frame() || domWindow != domWindow->frame()->domWindow())
         return v8::Local<v8::Context>();
     return V8Proxy::mainWorldContext(object->rootObject->frame());
 }
