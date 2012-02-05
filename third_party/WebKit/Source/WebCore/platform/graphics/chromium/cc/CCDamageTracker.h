693853277042ae971e4ac14dc7500a5e6049e510
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-05 06:13:47 +0000

    [chromium] Add support to force full damage in CCDamageTracker https://bugs.webkit.org/show_bug.cgi?id=76805
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Unit test added to CCDamageTrackerTest.cpp.
    
    This feature is needed for events that should cause the entire
    surface to be damaged, even if layers themselves had only partial
    damage or no damage at all. For example, tab-switching is one such
    event.
    
    * platform/graphics/chromium/cc/CCDamageTracker.cpp:
    (WebCore::CCDamageTracker::CCDamageTracker):
    (WebCore::CCDamageTracker::updateDamageRectForNextFrame):
    * platform/graphics/chromium/cc/CCDamageTracker.h:
    (WebCore::CCDamageTracker::forceFullDamageNextUpdate):
    (CCDamageTracker):
    
    Source/WebKit/chromium:
    
    * tests/CCDamageTrackerTest.cpp:
    (WebKitTests::TEST_F):
    (WebKitTests):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCDamageTracker.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCDamageTracker.h
@@ -44,6 +44,7 @@ public:
     static PassOwnPtr<CCDamageTracker> create();
     ~CCDamageTracker();
 
+    void forceFullDamageNextUpdate() { m_forceFullDamageNextUpdate = true; }
     void updateDamageRectForNextFrame(const Vector<RefPtr<CCLayerImpl> >& layerList, int targetSurfaceLayerID, CCLayerImpl* targetSurfaceMaskLayer);
     const FloatRect& currentDamageRect() { return m_currentDamageRect; }
 
@@ -69,6 +70,7 @@ private:
     OwnPtr<RectMap> m_nextRectHistory;
 
     FloatRect m_currentDamageRect;
+    bool m_forceFullDamageNextUpdate;
 };
 
 } // namespace WebCore
