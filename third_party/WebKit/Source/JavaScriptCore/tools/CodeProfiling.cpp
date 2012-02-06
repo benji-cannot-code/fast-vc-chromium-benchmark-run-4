a3f40332d305f252bd9f4c7cca86c1ce743e8525
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-06 12:33:47 +0000

    Unreviewed WinCE build fix after r106197.
    * tools/CodeProfiling.cpp:
    (JSC::CodeProfiling::notifyAllocator): getenv() isn't supported by WinCE. Don't call it.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106796 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/tools/CodeProfiling.cpp
+++ third_party/WebKit/Source/JavaScriptCore/tools/CodeProfiling.cpp
@@ -29,7 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CodeProfile.h"
 #include "MetaAllocator.h"
-#include "signal.h"
+
+#if HAVE(SIGNAL_H)
+#include <signal.h>
+#endif
 
 namespace JSC {
 
@@ -79,6 +82,7 @@ void CodeProfiling::sample(void* pc, void** framePointer)
 
 void CodeProfiling::notifyAllocator(WTF::MetaAllocator* allocator)
 {
+#if !OS(WINCE)
     // Check for JSC_CODE_PROFILING.
     const char* codeProfilingMode = getenv("JSC_CODE_PROFILING");
     if (!codeProfilingMode)
@@ -105,6 +109,7 @@ void CodeProfiling::notifyAllocator(WTF::MetaAllocator* allocator)
     ASSERT(!s_tracker);
     s_tracker = new WTF::MetaAllocatorTracker();
     allocator->trackAllocations(s_tracker);
+#endif
 }
 
 void* CodeProfiling::getOwnerUIDForPC(void* address)
