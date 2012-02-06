e0e94f103714a764f1a3b4ada448589908b74389
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-06 19:37:12 +0000

    gcc build fix after r106749.
    * UIProcess/WebPreferences.cpp:
    (WebKit::WebPreferences::WebPreferences):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPreferences.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPreferences.cpp
@@ -43,7 +43,8 @@ WebPreferences::WebPreferences(const String& identifier)
 }
 
 WebPreferences::WebPreferences(const WebPreferences& other)
-    : m_store(other.m_store)
+    : APIObject()
+    , m_store(other.m_store)
 {
     platformInitializeStore();
 }
