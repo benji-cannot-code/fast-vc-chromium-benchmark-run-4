06c8a8626733cfdb080736fdf88dd8e6e76a867d
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-06 22:08:48 +0000

    [Sync] Don't commit items with predecessors/parents in conflict.
    We now only consider an item committable if it and all it's immediate predecessors
     are not in conflict, and if it has a valid parent. This finishes the work
    to prevent conflicting entries from committing and allows us to preserve
    positional changes when in conflict.
    
    BUG=82236
    TEST=
    
    Review URL: http://codereview.chromium.org/8922015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120629 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sessions/ordered_commit_set.cc
+++ chrome/browser/sync/sessions/ordered_commit_set.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,13 @@ void OrderedCommitSet::AddCommitItem(const int64 metahandle,
     projections_[GetGroupForModelType(type, routes_)].push_back(
         commit_ids_.size() - 1);
     types_.push_back(type);
+  }
+}
+
+void OrderedCommitSet::Append(const OrderedCommitSet& other) {
+  for (int i = 0; i < other.Size(); ++i) {
+    CommitItem item = other.GetCommitItemAt(i);
+    AddCommitItem(item.meta, item.id, item.group);
   }
 }
 
