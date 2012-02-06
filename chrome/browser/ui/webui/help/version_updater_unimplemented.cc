33d48f9934f77f034e66b5aac80af809c8328505
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-06 01:46:57 +0000

    VersionUpdater: Remove a superfluous cast.
    BUG=none
    TEST=none
    R=thakis
    
    Review URL: https://chromiumcodereview.appspot.com/9309107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120531 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/help/version_updater_unimplemented.cc
+++ chrome/browser/ui/webui/help/version_updater_unimplemented.cc
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/help/version_updater_unimplemented.h"
 
 VersionUpdater* VersionUpdater::Create() {
-  return static_cast<VersionUpdater*>(new VersionUpdaterUnimplemented);
+  return new VersionUpdaterUnimplemented;
 }
