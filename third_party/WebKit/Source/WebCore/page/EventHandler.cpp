25d5060a9bd31d34628fb6b86aadf0935263d519
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 10:29:57 +0000

    [CRASH] bool EventHandler::dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent& event) references a NULL pointer https://bugs.webkit.org/show_bug.cgi?id=77953
    Reviewed by Andreas Kling.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::dispatchSyntheticTouchEventIfEnabled):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106916 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -3457,7 +3457,7 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
 
 bool EventHandler::dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent& event)
 {
-    if (!m_frame->document()->settings()->isTouchEventEmulationEnabled())
+    if (!m_frame || !m_frame->settings() || !m_frame->settings()->isTouchEventEmulationEnabled())
         return false;
 
     PlatformEvent::Type eventType = event.type();
