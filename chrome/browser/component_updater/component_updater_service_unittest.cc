a53182912cd80909f25f3dddc10b1802f3754037
rnk@chromium.org <rnk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 15:12:45 +0000

    Fix leak in ComponentUpdaterTest.ProdVersionCheck.
    R=cpu@chromium.org,timurrrr@chromium.org
    BUG=96295
    TEST=Ran ComponentUpdaterTest.* under drmemory, RegisterComponent leak gone
    
    Review URL: https://chromiumcodereview.appspot.com/9332004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_service_unittest.cc
+++ chrome/browser/component_updater/component_updater_service_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
+#include "base/memory/scoped_vector.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/values.h"
@@ -165,6 +166,10 @@ class ComponentUpdaterTest : public testing::Test {
     content::URLFetcher::SetEnableInterceptionForTests(false);
   }
 
+  void TearDown() {
+    xmlCleanupGlobals();
+  }
+
   ComponentUpdateService* component_updater() {
     return component_updater_.get();
   }
@@ -194,6 +199,7 @@ class ComponentUpdaterTest : public testing::Test {
     }
     com->version = version;
     com->installer = new TestInstaller;
+    test_installers_.push_back(com->installer);
     component_updater_->RegisterComponent(*com);
   }
 
@@ -202,6 +208,8 @@ class ComponentUpdaterTest : public testing::Test {
   FilePath test_data_dir_;
   TestNotificationTracker notification_tracker_;
   TestConfigurator* test_config_;
+  // ComponentInstaller objects to delete after each test.
+  ScopedVector<ComponentInstaller> test_installers_;
 };
 
 // Verify that our test fixture work and the component updater can
@@ -299,9 +307,6 @@ TEST_F(ComponentUpdaterTest, CheckCrxSleep) {
   EXPECT_EQ(0, static_cast<TestInstaller*>(com.installer)->install_count());
 
   component_updater()->Stop();
-
-  delete com.installer;
-  xmlCleanupGlobals();
 }
 
 // Verify that we can check for updates and install one component. Besides
@@ -370,9 +375,6 @@ TEST_F(ComponentUpdaterTest, InstallCrx) {
   EXPECT_EQ(chrome::NOTIFICATION_COMPONENT_UPDATER_SLEEPING, ev3.type);
 
   component_updater()->Stop();
-  delete com1.installer;
-  delete com2.installer;
-  xmlCleanupGlobals();
 }
 
 // This test checks that the "prodversionmin" value is handled correctly. In
