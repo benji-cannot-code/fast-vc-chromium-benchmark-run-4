9bb4a904df90928aae88ec28268d488347b75653
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 22:52:47 +0000

    Destroy OTR/incognito profile earlier in ProfileImpl destructor.
    Also allow SetTestingFactory to be used for incognito TestingProfiles.
    
    BUG=104095
    TEST=No crashing at normal profile shutdown.
    
    Review URL: https://chromiumcodereview.appspot.com/9344006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -573,6 +573,9 @@ ProfileImpl::~ProfileImpl() {
     default_request_context_ = NULL;
   }
 
+  // Destroy OTR profile and its profile services first.
+  DestroyOffTheRecordProfile();
+
   ProfileDependencyManager::GetInstance()->DestroyProfileServices(this);
 
   if (db_tracker_) {
