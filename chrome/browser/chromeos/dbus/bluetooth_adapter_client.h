50ba8b4fd26bd569809348663d06b01c57c292bb
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 23:35:31 +0000

    chromeos: bluetooth: tie Proxy lifetime to object, not observer
    Change the lifetime of Object Proxies to the remote object, not any
    Observer; one of the problems with the previous method was adding
    a second observer would treat every new attempt to add an observer
    as a new proxy so reconnect signals, etc.
    
    The other main problem is that you couldn't call methods unless there
    was an observer that had created the object proxy.
    
    Originally landed in I9317012, reverted in I9316113; unfortunately CQ
    was letting it through without the fixes to the Mock code.
    
    BUG=chromium-os:22086
    TEST=verified methods still work, built tests with USE=aura
    
    Change-Id: Icc86c03d00dabc27f2b31f9549da3a163f4b18e8
    
    
    Review URL: http://codereview.chromium.org/9309122
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120853 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/bluetooth_adapter_client.h
+++ chrome/browser/chromeos/dbus/bluetooth_adapter_client.h
@@ -19,6 +19,8 @@ class Bus;
 
 namespace chromeos {
 
+class BluetoothManagerClient;
+
 // BluetoothAdapterClient is used to communicate with a bluetooth Adapter
 // interface.
 class BluetoothAdapterClient {
@@ -55,10 +57,8 @@ class BluetoothAdapterClient {
 
   // Adds and removes observers for events on the adapter with object path
   // |object_path|.
-  virtual void AddObserver(Observer* observer,
-                           const std::string& object_path) = 0;
-  virtual void RemoveObserver(Observer* observer,
-                              const std::string& object_path) = 0;
+  virtual void AddObserver(Observer* observer) = 0;
+  virtual void RemoveObserver(Observer* observer) = 0;
 
   // Starts a device discovery on the adapter with object path |object_path|.
   virtual void StartDiscovery(const std::string& object_path) = 0;
@@ -68,7 +68,8 @@ class BluetoothAdapterClient {
   virtual void StopDiscovery(const std::string& object_path) = 0;
 
   // Creates the instance.
-  static BluetoothAdapterClient* Create(dbus::Bus* bus);
+  static BluetoothAdapterClient* Create(dbus::Bus* bus,
+                                        BluetoothManagerClient* manager_client);
 
  protected:
   BluetoothAdapterClient();
