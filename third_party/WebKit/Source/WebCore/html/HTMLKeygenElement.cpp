bfb1b76cd2f62100ac85823a4769c9a1b8aced7e
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 12:43:10 +0000

    Unreviewed, rolling out r106909. http://trac.webkit.org/changeset/106909 https://bugs.webkit.org/show_bug.cgi?id=77965
    ~20 tests are crashing on chromium win release bot. (Requested
    by loislo on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-02-07
    
    * html/HTMLDetailsElement.cpp:
    (WebCore::HTMLDetailsElement::createShadowSubtree):
    * html/HTMLKeygenElement.cpp:
    (WebCore::HTMLKeygenElement::HTMLKeygenElement):
    * html/HTMLMeterElement.cpp:
    (WebCore::HTMLMeterElement::createShadowSubtree):
    * html/HTMLProgressElement.cpp:
    (WebCore::HTMLProgressElement::createShadowSubtree):
    * html/HTMLSummaryElement.cpp:
    (WebCore::HTMLSummaryElement::createShadowSubtree):
    * html/HTMLTextAreaElement.cpp:
    (WebCore::HTMLTextAreaElement::createShadowSubtree):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106926 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLKeygenElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLKeygenElement.cpp
@@ -86,9 +86,7 @@ inline HTMLKeygenElement::HTMLKeygenElement(const QualifiedName& tagName, Docume
         option->appendChild(Text::create(document, keys[i]), ec);
     }
 
-    ASSERT(!shadowRoot());
-    RefPtr<ShadowRoot> root = ShadowRoot::create(this, ASSERT_NO_EXCEPTION);
-    root->appendChild(select, ec);
+    ensureShadowRoot()->appendChild(select, ec);
 }
 
 PassRefPtr<HTMLKeygenElement> HTMLKeygenElement::create(const QualifiedName& tagName, Document* document, HTMLFormElement* form)
