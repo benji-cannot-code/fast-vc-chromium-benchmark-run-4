d53f351794c5179c71f4e5adaeda4a2d321e1e1a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 11:05:56 +0000

    Removed CHECKs used for bug investigation.
    CHECKs was added when investigating issue 103957 and issue 95732. Since these two bugs have been fixed, remove CHECKs or replace with normal error handling.
    
    BUG=95732,103957
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9307009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120763 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper_plugin_delegate_impl.cc
+++ content/renderer/pepper_plugin_delegate_impl.cc
@@ -518,9 +518,7 @@ class HostDispatcherWrapper
       return false;
 
 #if defined(OS_POSIX)
-    // Check the validity of fd for bug investigation. Remove after fixed.
-    // See for details: crbug.com/103957.
-    CHECK_NE(-1, channel_handle.socket.fd);
+    DCHECK_NE(-1, channel_handle.socket.fd);
     if (channel_handle.socket.fd == -1)
       return false;
 #endif
@@ -670,6 +668,7 @@ bool BrokerDispatcherWrapper::Init(
     return false;
 
 #if defined(OS_POSIX)
+  DCHECK_NE(-1, channel_handle.socket.fd);
   if (channel_handle.socket.fd == -1)
     return false;
 #endif
