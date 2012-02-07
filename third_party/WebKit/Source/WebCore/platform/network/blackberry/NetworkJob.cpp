5a308d95b75b5a56e8437725af495e8a54050977
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 13:41:30 +0000

    [Blackberry] Clean up Networkjob and Networkmanger: remove unused variables in release build and change some public functions into be private ones https://bugs.webkit.org/show_bug.cgi?id=77926
    Patch by Chris Guan <chris.guan@torchmobile.com.cn> on 2012-02-07
    Reviewed by Rob Buis.
    
    1. rename clientIsOk to isClientAvailable.
    2. m_isRunning is only for an ASSERT in NetWorkManager, So move out from
    release build but keep available in debug build.
    
    No changes in behavior, so no new tests.
    
    * platform/network/blackberry/NetworkJob.cpp:
    (WebCore::NetworkJob::NetworkJob):
    (WebCore::NetworkJob::handleNotifyDataReceived):
    (WebCore::NetworkJob::handleNotifyDataSent):
    (WebCore::NetworkJob::handleNotifyClose):
    (WebCore::NetworkJob::startNewJobWithRequest):
    (WebCore::NetworkJob::sendResponseIfNeeded):
    (WebCore::NetworkJob::sendMultipartResponseIfNeeded):
    * platform/network/blackberry/NetworkJob.h:
    (NetworkJob):
    (WebCore::NetworkJob::isClientAvailable):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106931 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/NetworkJob.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/NetworkJob.cpp
@@ -98,7 +98,9 @@ NetworkJob::NetworkJob()
     , m_isAbout(false)
     , m_isFTP(false)
     , m_isFTPDir(true)
+#ifndef NDEBUG
     , m_isRunning(true) // Always started immediately after creation.
+#endif
     , m_cancelled(false)
     , m_statusReceived(false)
     , m_dataReceived(false)
@@ -380,7 +382,7 @@ void NetworkJob::handleNotifyDataReceived(const char* buf, size_t len)
     if (shouldSendClientData()) {
         sendResponseIfNeeded();
         sendMultipartResponseIfNeeded();
-        if (clientIsOk()) {
+        if (isClientAvailable()) {
             RecursionGuard guard(m_callingClient);
             m_handle->client()->didReceiveData(m_handle.get(), buf, len, len);
         }
@@ -405,7 +407,7 @@ void NetworkJob::handleNotifyDataSent(unsigned long long bytesSent, unsigned lon
     // Protect against reentrancy.
     updateDeferLoadingCount(1);
 
-    if (clientIsOk()) {
+    if (isClientAvailable()) {
         RecursionGuard guard(m_callingClient);
         m_handle->client()->didSendData(m_handle.get(), bytesSent, totalBytesToBeSent);
     }
@@ -423,8 +425,9 @@ void NetworkJob::notifyClose(int status)
 
 void NetworkJob::handleNotifyClose(int status)
 {
+#ifndef NDEBUG
     m_isRunning = false;
-
+#endif
     if (!m_cancelled) {
         if (!m_statusReceived) {
             // Connection failed before sending notifyStatusReceived: use generic NetworkError.
@@ -444,7 +447,7 @@ void NetworkJob::handleNotifyClose(int status)
                 m_extendedStatusCode = BlackBerry::Platform::FilterStream::StatusTooManyRedirects;
 
             sendResponseIfNeeded();
-            if (clientIsOk()) {
+            if (isClientAvailable()) {
 
                 RecursionGuard guard(m_callingClient);
                 if (isError(m_extendedStatusCode) && !m_dataReceived) {
@@ -499,7 +502,7 @@ bool NetworkJob::retryAsFTPDirectory()
 
 bool NetworkJob::startNewJobWithRequest(ResourceRequest& newRequest, bool increasRedirectCount)
 {
-    if (clientIsOk()) {
+    if (isClientAvailable()) {
         RecursionGuard guard(m_callingClient);
         m_handle->client()->willSendRequest(m_handle.get(), newRequest, m_response);
 
@@ -617,7 +620,7 @@ void NetworkJob::sendResponseIfNeeded()
     if (m_isFile || m_isData || m_isAbout)
         m_response.setHTTPHeaderField("Cache-Control", "no-cache");
 
-    if (clientIsOk()) {
+    if (isClientAvailable()) {
         RecursionGuard guard(m_callingClient);
         m_handle->client()->didReceiveResponse(m_handle.get(), m_response);
     }
@@ -625,7 +628,7 @@ void NetworkJob::sendResponseIfNeeded()
 
 void NetworkJob::sendMultipartResponseIfNeeded()
 {
-    if (m_multipartResponse && clientIsOk()) {
+    if (m_multipartResponse && isClientAvailable()) {
         m_handle->client()->didReceiveResponse(m_handle.get(), *m_multipartResponse);
         m_multipartResponse = nullptr;
     }
