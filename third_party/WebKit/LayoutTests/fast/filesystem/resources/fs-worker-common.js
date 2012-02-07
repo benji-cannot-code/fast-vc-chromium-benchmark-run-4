dc0bf967128f44d752aa5a51e11ab5b8dbfb40a6
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 04:05:21 +0000

    js-test-pre.js: relay calls to testPassed, testFailed, debug, etc from worker to document. https://bugs.webkit.org/show_bug.cgi?id=76762
    Reviewed by Ojan Vafai.
    
    * fast/filesystem/resources/async-operations.js:
    * fast/filesystem/resources/directory-entry-to-uri.js:
    * fast/filesystem/resources/file-entry-to-uri-sync.js:
    * fast/filesystem/resources/file-entry-to-uri.js:
    * fast/filesystem/resources/file-from-file-entry-sync.js:
    * fast/filesystem/resources/file-from-file-entry.js:
    * fast/filesystem/resources/file-writer-abort-continue.js:
    * fast/filesystem/resources/file-writer-abort-depth.js:
    * fast/filesystem/resources/file-writer-abort.js:
    * fast/filesystem/resources/file-writer-events.js:
    * fast/filesystem/resources/file-writer-gc-blob.js:
    * fast/filesystem/resources/file-writer-sync-truncate-extend.js:
    * fast/filesystem/resources/file-writer-sync-write-overlapped.js:
    * fast/filesystem/resources/file-writer-truncate-extend.js:
    * fast/filesystem/resources/file-writer-write-overlapped.js:
    * fast/filesystem/resources/fs-worker-common.js:
    * fast/filesystem/resources/fs-worker-test-util.js: Removed.
    * fast/filesystem/resources/simple-persistent-sync.js:
    * fast/filesystem/resources/simple-persistent.js:
    * fast/filesystem/resources/simple-readonly.js:
    * fast/filesystem/resources/simple-required-arguments-getdirectory.js:
    * fast/filesystem/resources/simple-required-arguments-getfile.js:
    * fast/filesystem/resources/simple-required-arguments-getmetadata.js:
    * fast/filesystem/resources/simple-required-arguments-remove.js:
    * fast/filesystem/resources/simple-temporary-sync.js:
    * fast/filesystem/resources/simple-temporary.js:
    * fast/filesystem/resources/sync-operations.js:
    * fast/filesystem/workers/async-operations.html:
    * fast/filesystem/workers/file-entry-to-uri-sync.html:
    * fast/filesystem/workers/file-from-file-entry-sync.html:
    * fast/filesystem/workers/file-from-file-entry.html:
    * fast/filesystem/workers/file-writer-events.html:
    * fast/filesystem/workers/file-writer-gc-blob.html:
    * fast/filesystem/workers/file-writer-sync-truncate-extend.html:
    * fast/filesystem/workers/file-writer-sync-write-overlapped.html:
    * fast/filesystem/workers/file-writer-truncate-extend.html:
    * fast/filesystem/workers/file-writer-write-overlapped.html:
    * fast/filesystem/workers/simple-persistent-sync.html:
    * fast/filesystem/workers/simple-persistent.html:
    * fast/filesystem/workers/simple-temporary-sync.html:
    * fast/filesystem/workers/simple-temporary.html:
    * fast/filesystem/workers/sync-operations.html:
    * fast/js/resources/js-test-pre.js:
    (testPassed):
    (testFailed):
    (finishJSTest):
    (startWorker.worker.onmessage):
    (startWorker.worker.onerror):
    (startWorker):
    * storage/indexeddb/basics-workers.html:
    * storage/indexeddb/resources/basics.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106898 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/filesystem/resources/fs-worker-common.js
+++ third_party/WebKit/LayoutTests/fast/filesystem/resources/fs-worker-common.js
@@ -1,41 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function debug(message)
-{
-    postMessage("MESG:" + message);
-}
-
-function finishJSTest()
-{
-    postMessage("DONE:");
-}
-
-function description(message)
-{
-    postMessage('DESC:' + message);
-}
-
-function testPassed(msg) {
-    postMessage("PASS:" + msg);
-}
-
-function testFailed(msg) {
-    postMessage("FAIL:" + msg);
-}
-
-function shouldBe(_a, _b)
-{
-  if (typeof _a != "string" || typeof _b != "string")
-    debug("WARN: shouldBe() expects string arguments");
-  var _av = eval(_a);
-  var _bv = eval(_b);
-  if (_av === _bv)
-    testPassed(_a + " is " + _b);
-  else
-    testFailed(_a + " should be " + _bv + " (of type " + typeof _bv + "). Was " + _av + " (of type " + typeof _av + ").");
-}
-
-function shouldBeTrue(_a) { shouldBe(_a, "true"); }
-
 function removeAllInDirectorySync(directory) {
     if (!directory)
         return;
