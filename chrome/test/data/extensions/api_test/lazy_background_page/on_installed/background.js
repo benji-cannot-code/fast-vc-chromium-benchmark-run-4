338bd08c080eee861d0ac77c139deffe78f7ae96
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 19:41:27 +0000

    Unrevert again r119770 - "Event pages: remember events that the page registered for,""
    Original CL:
    Add support for broadcasted events.
    
    Add an extension.onInstalled event that is run when the event page first
    starts."
    
    Fixed ASAN bug with previous incarnation.
    
    Previous revert did not fix failing tests, so it's going back in.
    
    TBR=aa
    BUG=81752, 112391
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/9350006
    
    Review URL: https://chromiumcodereview.appspot.com/9350024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/lazy_background_page/on_installed/background.js
+++ chrome/test/data/extensions/api_test/lazy_background_page/on_installed/background.js
@@ -3,9 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.search("test_file.html") > -1)
-    chrome.pageAction.show(tabId);
-  else
-    chrome.pageAction.hide(tabId);
+chrome.experimental.extension.onInstalled.addListener(function() {
+  chrome.test.notifyPass();
 });
