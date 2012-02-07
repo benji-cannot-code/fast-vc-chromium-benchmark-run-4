a5cf5dfcc6facff7dd662bb31f5a08509f832fe8
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 02:41:05 +0000

    Switch to using WebIntentPickerModel, and bring picker closer to mocks.
    BUG=90458,112925
    TEST=WebIntentBubbleControllerTest.*
    
    Review URL: http://codereview.chromium.org/9310074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120691 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/web_intent_picker_cocoa.h
+++ chrome/browser/ui/cocoa/web_intent_picker_cocoa.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/intents/web_intent_picker.h"
 #include "chrome/browser/ui/intents/web_intent_picker_model.h"
 #include "chrome/browser/ui/intents/web_intent_picker_model_observer.h"
@@ -56,11 +57,20 @@ class WebIntentPickerCocoa : public WebIntentPicker,
 
   WebIntentBubbleController* controller_;  // Weak reference.
 
+  // Factory for weak ptrs, used for delayed callbacks.
+  base::WeakPtrFactory<WebIntentPickerCocoa> weak_ptr_factory_;
+
   // Tab contents wrapper to hold intent page if inline disposition is used.
   scoped_ptr<TabContentsWrapper> inline_disposition_tab_contents_;
 
   // Delegate for inline disposition tab contents.
   scoped_ptr<WebIntentInlineDispositionDelegate> inline_disposition_delegate_;
+
+  // Post a delayed task to do layout, if it isn't already pending.
+  void PerformDelayedLayout();
+
+  // Re-layout the intent picker.
+  void PerformLayout();
 
   // Default constructor, for testing only.
   WebIntentPickerCocoa();
