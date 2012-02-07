ebc28455e0a11d79922cb02f6c0fe7e8aed4fccc
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 23:36:09 +0000

    SPDY - Adjust the send window size of all streams (new and current) based on initial window size sent in settings frame from the server.
    R=willchan
    BUG=112778
    TEST=network unit tests
    
    Review URL: http://codereview.chromium.org/9346003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120855 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.h
+++ net/spdy/spdy_stream.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,6 +137,9 @@ class NET_EXPORT_PRIVATE SpdyStream
   void set_stalled_by_flow_control(bool stalled) {
     stalled_by_flow_control_ = stalled;
   }
+
+  // Adjust the |send_window_size_| by |delta_window_size|.
+  void AdjustSendWindowSize(int delta_window_size);
 
   // Increases |send_window_size_| with delta extracted from a WINDOW_UPDATE
   // frame; sends a RST_STREAM if delta overflows |send_window_size_| and
