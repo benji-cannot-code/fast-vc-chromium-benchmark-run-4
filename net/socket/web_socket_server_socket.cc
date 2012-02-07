benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit ae705e008b2b6ba3c321321c72aa6782054d88f0

Coverity: Fix leaks.
CID=2168,8197,8198,100251,100256,100820,101011,101982,101983,101984,101988,
    102256
BUG=none
TEST=none
R=dpapad

Review URL: http://codereview.chromium.org/8989036

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115422 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/web_socket_server_socket.cc
@@ -732,13 +732,13 @@ class WebSocketServerSocketImpl : public net::WebSocketServerSocket {
       }
 
       operator net::DrainableIOBuffer*() {
-        return new net::DrainableIOBuffer(io_buf_, bytes_written_);
+        return new net::DrainableIOBuffer(io_buf_.release(), bytes_written_);
       }
 
       bool is_ok() { return is_ok_; }
 
      private:
-      net::IOBuffer* io_buf_;
+      scoped_refptr<net::IOBuffer> io_buf_;
       size_t bytes_written_;
       bool is_ok_;
     } buffer;

chrome/browser/themes/browser_theme_pack.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "base/string_util.h"
 #include "base/threading/thread_restrictions.h"
@@ -339,7 +340,7 @@ BrowserThemePack* BrowserThemePack::BuildFromExtension(
   DCHECK(extension);
   DCHECK(extension->is_theme());
 
-  BrowserThemePack* pack = new BrowserThemePack;
+  scoped_refptr<BrowserThemePack> pack = new BrowserThemePack;
   pack->BuildHeader(extension);
   pack->BuildTintsFromJSON(extension->GetThemeTints());
   pack->BuildColorsFromJSON(extension->GetThemeColors());
@@ -364,7 +365,7 @@ BrowserThemePack* BrowserThemePack::BuildFromExtension(
   pack->GenerateTabBackgroundImages(&pack->prepared_images_);
 
   // The BrowserThemePack is now in a consistent state.
-  return pack;
+  return pack.release();
 }
 
 // static

chrome/installer/util/google_chrome_distribution.cc
@@ -101,8 +101,12 @@ int GetDirectoryWriteTimeInHours(const wchar_t* path) {
   if (INVALID_HANDLE_VALUE == file)
     return -1;
   FILETIME time;
-  if (!::GetFileTime(file, NULL, NULL, &time))
+  if (!::GetFileTime(file, NULL, NULL, &time)) {
+    ::CloseHandle(file);
     return -1;
+  }
+
+  ::CloseHandle(file);
   return FileTimeToHours(time);
 }
 

content/browser/renderer_host/media/video_capture_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/media/video_capture_controller.h"
 
 #include "base/bind.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "content/browser/renderer_host/media/media_stream_manager.h"
 #include "content/browser/renderer_host/media/video_capture_manager.h"
@@ -394,12 +395,12 @@ void VideoCaptureController::DoFrameInfoOnIOThread(
   {
     base::AutoLock lock(lock_);
     for (size_t i = 1; i <= kNoOfDIBS; ++i) {
-      base::SharedMemory* shared_memory = new base::SharedMemory();
+      scoped_ptr<base::SharedMemory> shared_memory(new base::SharedMemory());
       if (!shared_memory->CreateAndMapAnonymous(needed_size)) {
         frames_created = false;
         break;
       }
-      SharedDIB* dib = new SharedDIB(shared_memory);
+      SharedDIB* dib = new SharedDIB(shared_memory.release());
       owned_dibs_.insert(std::make_pair(i, dib));
     }
   }

content/browser/renderer_host/render_sandbox_host_linux.cc
@@ -644,7 +644,7 @@ class SandboxIPCProcess  {
 
   const int lifeline_fd_;
   const int browser_socket_;
-  FontConfigDirect* const font_config_;
+  scoped_ptr<FontConfigDirect> font_config_;
   std::vector<std::string> sandbox_cmd_;
   scoped_ptr<content::WebKitPlatformSupportImpl> webkit_platform_support_;
 };

content/common/npobject_stub.cc
@@ -150,6 +150,7 @@ void NPObjectStub::OnInvoke(bool is_default,
       NPObjectMsg_Invoke::WriteReplyParams(reply_msg, result_param,
                                            return_value);
       channel_->Send(reply_msg);
+      delete[] args_var;
       return;
     }
   }
@@ -346,6 +347,7 @@ void NPObjectStub::OnConstruct(const std::vector<NPVariant_Param>& args,
       NPObjectMsg_Invoke::WriteReplyParams(reply_msg, result_param,
                                            return_value);
       channel_->Send(reply_msg);
+      delete[] args_var;
       return;
     }
   }

ipc/ipc_channel_posix.cc
@@ -171,6 +171,8 @@ bool CreateServerUnixDomainSocket(const std::string& pipe_name,
   FilePath path(pipe_name);
   FilePath dir_path = path.DirName();
   if (!file_util::CreateDirectory(dir_path)) {
+    if (HANDLE_EINTR(close(fd)) < 0)
+      PLOG(ERROR) << "close " << pipe_name;
     return false;
   }
 

ppapi/native_client/src/trusted/plugin/plugin.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/memory/scoped_ptr.h"
 #include "native_client/src/include/nacl_base.h"
 #include "native_client/src/include/nacl_macros.h"
 #include "native_client/src/include/nacl_scoped_ptr.h"
@@ -901,7 +902,8 @@ bool Plugin::Init(uint32_t argc, const char* argn[], const char* argv[]) {
   HistogramEnumerateOsArch(GetSandboxISA());
   init_time_ = NaClGetTimeOfDayMicroseconds();
 
-  BrowserInterface* browser_interface = new(std::nothrow) BrowserInterface;
+  scoped_ptr<BrowserInterface> browser_interface(
+      new(std::nothrow) BrowserInterface);
   if (browser_interface == NULL) {
     return false;
   }
@@ -920,7 +922,7 @@ bool Plugin::Init(uint32_t argc, const char* argn[], const char* argv[]) {
                  static_cast<const void*>(url_util_)));
 
   bool status = Plugin::Init(
-      browser_interface,
+      browser_interface.release(),
       static_cast<int>(argc),
       // TODO(polina): Can we change the args on our end to be const to
       // avoid these ugly casts?

commit 1be07df5efd150abf2ce180906bd69ffd076f03a

Fix ref leak in WebSocketServerSocketImpl introduced in r115422.
Also remove the heapcheck and memcheck suppressions.

Added cbentzel and timurrrr for OWNERS approval.

R=jhawkins@chromium.org,cbentzel@chromium.org,timurrrr@chromium.org
BUG=108399
TEST=ran WebSocketServerSocketTest.* under drmemory

Review URL: https://chromiumcodereview.appspot.com/9309090

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120775 0039d316-1c4b-4281-b951-d872f2087c98

net/socket/web_socket_server_socket.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -727,7 +727,7 @@ class WebSocketServerSocketImpl : public net::WebSocketServerSocket {
       }
 
       operator net::DrainableIOBuffer*() {
-        return new net::DrainableIOBuffer(io_buf_.release(), bytes_written_);
+        return new net::DrainableIOBuffer(io_buf_.get(), bytes_written_);
       }
 
       bool is_ok() { return is_ok_; }