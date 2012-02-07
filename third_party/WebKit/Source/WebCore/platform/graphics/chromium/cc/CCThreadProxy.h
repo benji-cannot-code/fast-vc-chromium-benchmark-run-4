190cdf5e133f887b8944d21bd66c8a3c92f1cda4
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 16:43:23 +0000

    Unreviewed, rolling out r106935. http://trac.webkit.org/changeset/106935 https://bugs.webkit.org/show_bug.cgi?id=77994
    "Crashes runMultiThread webkit_unit_test" (Requested by tonyg-
    cr on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-02-07
    
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::setNeedsRedraw):
    * platform/graphics/chromium/cc/CCSingleThreadProxy.cpp:
    (WebCore::CCSingleThreadProxy::setNeedsRedraw):
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::setNeedsRedraw):
    * platform/graphics/chromium/cc/CCThreadProxy.h:
    (CCThreadProxy):
    
    Source/WebKit/chromium:
    
    * public/WebWidget.h:
    (WebWidget):
    * src/WebViewImpl.cpp:
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.h
@@ -104,7 +104,6 @@ private:
     void initializeLayerRendererOnImplThread(CCCompletionEvent*, bool* initializeSucceeded, LayerRendererCapabilities*);
     void setVisibleOnImplThread(CCCompletionEvent*, bool visible);
     void layerTreeHostClosedOnImplThread(CCCompletionEvent*);
-    void resetDamageTrackerOnImplThread();
 
     // Accessed on main thread only.
     bool m_animateRequested;
