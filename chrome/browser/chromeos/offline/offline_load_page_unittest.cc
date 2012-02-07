440427f091377d969199eb20f662e5b503809c70
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 23:42:38 +0000

    fix cros again
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/offline/offline_load_page_unittest.cc
+++ chrome/browser/chromeos/offline/offline_load_page_unittest.cc
@@ -19,8 +19,6 @@ namespace chromeos {
 
 class OfflineLoadPageTest;
 
-namespace {
-
 // An OfflineLoadPage class that does not create windows.
 class TestOfflineLoadPage :  public chromeos::OfflineLoadPage {
  public:
@@ -40,8 +38,6 @@ class TestOfflineLoadPage :  public chromeos::OfflineLoadPage {
 
   DISALLOW_COPY_AND_ASSIGN(TestOfflineLoadPage);
 };
-
-}  // namespace
 
 class OfflineLoadPageTest : public ChromeRenderViewHostTestHarness {
  public:
