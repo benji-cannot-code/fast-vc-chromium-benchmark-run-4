0d75371c8ef0045d47da8595d7b6f85db97ab83a
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-07 05:09:11 +0000

    [BlackBerry] Use WebPagePrivate instead of WebPage in GeolocationControllerClientBlackBerry https://bugs.webkit.org/show_bug.cgi?id=77927
    Reviewed by Antonio Gomes.
    
    WebKit/blackberry/Api/WebPage which has a d pointer is designed for public API.
    GeolocationControllerClientBlackBerry in WebKit/blackberry/WebCoreSupport should
    use the d pointer directly because the client is internal implementation.
    
    No functionalities changed, no new tests.
    
    * blackberry/WebCoreSupport/GeolocationControllerClientBlackBerry.cpp:
    (GeolocationControllerClientBlackBerry::GeolocationControllerClientBlackBerry):
    (GeolocationControllerClientBlackBerry::requestPermission):
    (GeolocationControllerClientBlackBerry::cancelPermissionRequest):
    (GeolocationControllerClientBlackBerry::onLocationUpdate):
    (GeolocationControllerClientBlackBerry::onLocationError):
    * blackberry/WebCoreSupport/GeolocationControllerClientBlackBerry.h:
    (WebKit):
    (GeolocationControllerClientBlackBerry):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@106903 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationControllerClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationControllerClientBlackBerry.cpp
@@ -25,13 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GeolocationController.h"
 #include "GeolocationError.h"
 #include "Page.h"
-#include "WebPage.h"
 #include "WebPage_p.h"
 
 using namespace WebCore;
 
-GeolocationControllerClientBlackBerry::GeolocationControllerClientBlackBerry(BlackBerry::WebKit::WebPage* webPage)
-    : m_webPage(webPage)
+GeolocationControllerClientBlackBerry::GeolocationControllerClientBlackBerry(BlackBerry::WebKit::WebPagePrivate* webPagePrivate)
+    : m_webPagePrivate(webPagePrivate)
     , m_tracker(0)
     , m_accuracy(false)
 {
@@ -68,7 +67,7 @@ void GeolocationControllerClientBlackBerry::requestPermission(Geolocation* locat
     Frame* frame = location->frame();
     if (!frame)
         return;
-    m_webPage->d->m_page->chrome()->requestGeolocationPermissionForFrame(frame, location);
+    m_webPagePrivate->m_page->chrome()->requestGeolocationPermissionForFrame(frame, location);
 }
 
 void GeolocationControllerClientBlackBerry::cancelPermissionRequest(Geolocation* location)
@@ -76,7 +75,7 @@ void GeolocationControllerClientBlackBerry::cancelPermissionRequest(Geolocation*
     Frame* frame = location->frame();
     if (!frame)
         return;
-    m_webPage->d->m_page->chrome()->cancelGeolocationPermissionRequestForFrame(frame, location);
+    m_webPagePrivate->m_page->chrome()->cancelGeolocationPermissionRequestForFrame(frame, location);
 }
 
 void GeolocationControllerClientBlackBerry::onLocationUpdate(double timestamp, double latitude, double longitude, double accuracy, double altitude, bool altitudeValid,
@@ -84,13 +83,13 @@ void GeolocationControllerClientBlackBerry::onLocationUpdate(double timestamp, d
 {
     m_lastPosition = GeolocationPosition::create(timestamp, latitude, longitude, accuracy, altitudeValid, altitude, altitudeAccuracyValid,
                                                  altitudeAccuracy, headingValid, heading, speedValid, speed);
-    m_webPage->d->m_page->geolocationController()->positionChanged(m_lastPosition.get());
+    m_webPagePrivate->m_page->geolocationController()->positionChanged(m_lastPosition.get());
 }
 
 void GeolocationControllerClientBlackBerry::onLocationError(const char* errorStr)
 {
     RefPtr<GeolocationError> error = GeolocationError::create(GeolocationError::PositionUnavailable, String::fromUTF8(errorStr));
-    m_webPage->d->m_page->geolocationController()->errorOccurred(error.get());
+    m_webPagePrivate->m_page->geolocationController()->errorOccurred(error.get());
 }
 
 void GeolocationControllerClientBlackBerry::onPermission(void* context, bool isAllowed)
