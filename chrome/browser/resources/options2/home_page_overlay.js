1410c4d32ac526e4c41eae930e750103671af76b
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 07:01:57 +0000

    [uber page] Improve titles for homePage and StartupPages overlays for consistency.
    BUG=112232
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/9349003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/home_page_overlay.js
+++ chrome/browser/resources/options2/home_page_overlay.js
@@ -13,7 +13,9 @@ cr.define('options', function() {
    * @extends {SettingsDialog}
    */
   function HomePageOverlay() {
-    SettingsDialog.call(this, 'homePageOverlay', '', 'home-page-overlay',
+    SettingsDialog.call(this, 'homePageOverlay',
+                        templateData.homePageOverlayTabTitle,
+                        'home-page-overlay',
                         $('home-page-confirm'), $('home-page-cancel'));
   }
 
