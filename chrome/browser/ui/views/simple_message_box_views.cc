3f58aaa306798aa5af1f44233ab164c438de3b38
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 15:27:51 +0000

    Have 15 or more bookmarks Right click on bookmark bar, select "Open All Bookmarks"
    This patch makes it possible to interact with dialog which shows up
    
    BUG=None
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9317086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120975 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/simple_message_box_views.cc
+++ chrome/browser/ui/views/simple_message_box_views.cc
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 #if defined(USE_AURA)
-#include "ui/views/focus/accelerator_handler.h"
+#include "ui/aura/client/dispatcher_client.h"
+#include "ui/aura/root_window.h"
 #endif
 
 namespace browser {
@@ -59,10 +60,14 @@ bool SimpleMessageBoxViews::ShowYesNoBox(gfx::NativeWindow parent_window,
   // Make sure Chrome doesn't attempt to shut down with the dialog up.
   g_browser_process->AddRefModule();
 
+#if defined(USE_AURA)
+  aura::client::GetDispatcherClient()->RunWithDispatcher(dialog, true);
+#else
   bool old_state = MessageLoopForUI::current()->NestableTasksAllowed();
   MessageLoopForUI::current()->SetNestableTasksAllowed(true);
   MessageLoopForUI::current()->RunWithDispatcher(dialog);
   MessageLoopForUI::current()->SetNestableTasksAllowed(old_state);
+#endif
 
   g_browser_process->ReleaseModule();
 
@@ -151,7 +156,7 @@ bool SimpleMessageBoxViews::Dispatch(const MSG& msg) {
 #elif defined(USE_AURA)
 base::MessagePumpDispatcher::DispatchStatus
     SimpleMessageBoxViews::Dispatch(XEvent* xev) {
-  if (!views::DispatchXEvent(xev))
+  if (!aura::RootWindow::GetInstance()->GetDispatcher()->Dispatch(xev))
     return EVENT_IGNORED;
 
   if (disposition_ == DISPOSITION_UNKNOWN)
