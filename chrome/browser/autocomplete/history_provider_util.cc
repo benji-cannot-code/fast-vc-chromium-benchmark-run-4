542a481ceea85a6c9ddfc4df5d9ecbe44b6b15cd
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 14:54:38 +0000

    Fix history_url_provider when building with stlport
    Use the predicate-supplying version of find_first_of since stlport uses equal_to<> and find_first_of is trying to compare a non-const HistoryMatch with a const GURL
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/9352004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_provider_util.cc
+++ chrome/browser/autocomplete/history_provider_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,8 +24,8 @@ HistoryMatch::HistoryMatch(const URLRow& url_info,
       innermost_match(innermost_match) {
 }
 
-bool HistoryMatch::operator==(const GURL& url) const {
-  return url_info.url() == url;
+bool HistoryMatch::EqualsGURL(const HistoryMatch& h, const GURL& url) {
+  return h.url_info.url() == url;
 }
 
 }  // namespace history
