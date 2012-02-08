363b58f71960892a14df3d966c059a2ef2ee83dd
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 21:44:05 +0000

    Do not override the default search engine during import. IE/FF import was previously setting the default search engine. This is not the intended behavior of the import process. Remove unused GetFirefoxDefaultSearchEngineIndex(), etc.
    BUG=113130
    TEST=Chrome first run doesn't use the previously default browser's default search engine.
    
    Review URL: http://codereview.chromium.org/9363011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox_importer_utils.h
+++ chrome/browser/importer/firefox_importer_utils.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -76,12 +76,6 @@ bool CanImportURL(const GURL& url);
 // with the resulting TemplateURLs.
 void ParseSearchEnginesFromXMLFiles(const std::vector<FilePath>& xml_files,
                                     std::vector<TemplateURL*>* search_engines);
-
-// Returns the index of the default search engine in the |search_engines| list.
-// If none is found, -1 is returned.
-int GetFirefoxDefaultSearchEngineIndex(
-    const std::vector<TemplateURL*>& search_engines,
-    const FilePath& profile_path);
 
 // Returns the home page set in Firefox in a particular profile.
 GURL GetHomepage(const FilePath& profile_path);
