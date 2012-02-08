f9ef647a3a0b0d027ddb6876ecd0ebc01950e2b6
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-08 17:35:44 +0000

    REGRESSION (r106681): Null check missing in [WebFrame(WebInternal) _typingStyle] https://bugs.webkit.org/show_bug.cgi?id=78080
    Source/WebKit/mac: 
    
    Reviewed by Reviewed by Andreas Kling, Darin Adler and Dan Bernstein.
    
    * WebView/WebFrame.mm:
    (-[WebFrame _typingStyle]):
    
    Tools: 
    
    Reviewed by Andreas Kling, Darin Adler and Dan Bernstein.
            
    Add API test.
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/mac/TypingStyleCrash.mm: Added.
    (TestWebKitAPI):
    (TestWebKitAPI::TEST):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebFrame.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebFrame.mm
@@ -792,10 +792,10 @@ static inline WebDataSource *dataSource(DocumentLoader* loader)
 {
     if (!_private->coreFrame)
         return nil;
-    RefPtr<CSSStyleDeclaration> typingStyle = _private->coreFrame->selection()->copyTypingStyle()->ensureCSSStyleDeclaration();
+    RefPtr<StylePropertySet> typingStyle = _private->coreFrame->selection()->copyTypingStyle();
     if (!typingStyle)
         return nil;
-    return kit(typingStyle.get());
+    return kit(typingStyle->ensureCSSStyleDeclaration());
 }
 
 - (void)_setTypingStyle:(DOMCSSStyleDeclaration *)style withUndoAction:(EditAction)undoAction
