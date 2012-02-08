3cf046a45b2673257fd5313a4d3b2fe21c2cbe15
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 22:52:15 +0000

    add NON_EXPORTED_BASE
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121078 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/interstitial_page_impl.h
+++ content/browser/tab_contents/interstitial_page_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_TAB_CONTENTS_INTERSTITIAL_PAGE_IMPL_H_
 #pragma once
 
+#include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/process_util.h"
 #include "content/public/browser/interstitial_page.h"
@@ -30,7 +31,7 @@ enum ResourceRequestAction {
 };
 
 class CONTENT_EXPORT InterstitialPageImpl
-    : public content::InterstitialPage,
+    : public NON_EXPORTED_BASE(content::InterstitialPage),
       public content::NotificationObserver,
       public content::RenderViewHostDelegate {
  public:
