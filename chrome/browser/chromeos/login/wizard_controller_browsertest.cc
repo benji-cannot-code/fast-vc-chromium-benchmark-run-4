584e85b9c96b77387f8af01efce35fd37a900786
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 19:18:52 +0000

    [cros] Mark WizardControllerFlowTest.{ControlFlowMain|ControlFlowErrorUpdate} as flaky.
    BUG=chromium:110544
    TEST=Tests do not make bot red.
    
    
    Review URL: http://codereview.chromium.org/9351011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121014 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller_browsertest.cc
+++ chrome/browser/chromeos/login/wizard_controller_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -142,7 +142,8 @@ class WizardControllerFlowTest : public WizardControllerTest {
   DISALLOW_COPY_AND_ASSIGN(WizardControllerFlowTest);
 };
 
-IN_PROC_BROWSER_TEST_F(WizardControllerFlowTest, ControlFlowMain) {
+// Seems to be flaky http://crbug.com/110544.
+IN_PROC_BROWSER_TEST_F(WizardControllerFlowTest, DISABLED_ControlFlowMain) {
   EXPECT_TRUE(ExistingUserController::current_controller() == NULL);
   EXPECT_EQ(controller()->GetNetworkScreen(), controller()->current_screen());
   EXPECT_CALL(*mock_network_screen_, Hide()).Times(1);
@@ -166,7 +167,9 @@ IN_PROC_BROWSER_TEST_F(WizardControllerFlowTest, ControlFlowMain) {
   set_controller(NULL);
 }
 
-IN_PROC_BROWSER_TEST_F(WizardControllerFlowTest, ControlFlowErrorUpdate) {
+// Seems to be flaky http://crbug.com/110544.
+IN_PROC_BROWSER_TEST_F(WizardControllerFlowTest,
+                       DISABLED_ControlFlowErrorUpdate) {
   EXPECT_EQ(controller()->GetNetworkScreen(), controller()->current_screen());
   EXPECT_CALL(*mock_update_screen_, StartUpdate()).Times(0);
   EXPECT_CALL(*mock_eula_screen_, Show()).Times(1);
