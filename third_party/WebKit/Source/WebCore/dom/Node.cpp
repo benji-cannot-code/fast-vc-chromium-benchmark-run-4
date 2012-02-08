3a385ee9f6d79aafe8efbca5412052b4ec6ac070
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-08 20:53:54 +0000

    Crash in Node::normalize. https://bugs.webkit.org/show_bug.cgi?id=78135
    Reviewed by Ryosuke Niwa.
    
    No new tests. Original testcase does not reduce to manageable
    extent.
    
    * dom/Node.cpp:
    (WebCore::Node::normalize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107123 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -633,7 +633,7 @@ void Node::normalize()
             continue;
         }
 
-        Text* text = static_cast<Text*>(node.get());
+        RefPtr<Text> text = static_cast<Text*>(node.get());
 
         // Remove empty text nodes.
         if (!text->length()) {
