099f898b07edeff1f73e3dfc20ee43e3e6c64f97
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-08 02:21:04 +0000

    Resolve crash in FrameLoader::checkTimerFired. https://bugs.webkit.org/show_bug.cgi?id=77907
    Patch by Chris Palmer <palmer@google.com> on 2012-02-07
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Test is LayoutTests/http/tests/appcache/deferred-events-delete-while-raising-timer.html.
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::checkTimerFired):
    
    LayoutTests:
    
    * http/tests/appcache/deferred-events-delete-while-raising-timer-expected.txt: Added.
    * http/tests/appcache/deferred-events-delete-while-raising-timer.html: Added.
    * http/tests/appcache/resources/deferred-events-delete-while-raising-timer-1.html: Added.
    * http/tests/appcache/resources/deferred-events-delete-while-raising-timer-2.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -751,6 +751,8 @@ void FrameLoader::checkCompleted()
 
 void FrameLoader::checkTimerFired(Timer<FrameLoader>*)
 {
+    RefPtr<Frame> protect(m_frame);
+
     if (Page* page = m_frame->page()) {
         if (page->defersLoading())
             return;
