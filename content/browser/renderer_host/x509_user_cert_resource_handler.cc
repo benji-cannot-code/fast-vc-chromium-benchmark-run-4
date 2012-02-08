871dbb4d0769fbc96b16785d182b08d728d69cc3
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 17:24:26 +0000

    Fix a crash when downloading invalid client certificates.
    Additionally, remove some outdated comments/TODOs that no longer apply.
    
    BUG=112451
    
    
    Review URL: http://codereview.chromium.org/9361007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120991 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/x509_user_cert_resource_handler.cc
+++ content/browser/renderer_host/x509_user_cert_resource_handler.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -62,6 +62,8 @@ bool X509UserCertResourceHandler::OnWillRead(int request_id,
                                              net::IOBuffer** buf,
                                              int* buf_size,
                                              int min_size) {
+  static const int kReadBufSize = 32768;
+
   // TODO(gauravsh): Should we use 'min_size' here?
   DCHECK(buf && buf_size);
   if (!read_buffer_) {
@@ -99,12 +101,12 @@ bool X509UserCertResourceHandler::OnResponseCompleted(
   if (urs.status() != net::URLRequestStatus::SUCCESS)
     return false;
 
-  // TODO(gauravsh): Verify that 'request_id' was actually a keygen form post
-  // and only then import the certificate.
   AssembleResource();
-  scoped_refptr<net::X509Certificate> cert(
-      net::X509Certificate::CreateFromBytes(resource_buffer_->data(),
-                                            content_length_));
+  scoped_refptr<net::X509Certificate> cert;
+  if (resource_buffer_) {
+      cert = net::X509Certificate::CreateFromBytes(resource_buffer_->data(),
+                                                   content_length_);
+  }
   content::GetContentClient()->browser()->AddNewCertificate(
       request_, cert, render_process_host_id_, render_view_id_);
   return true;
