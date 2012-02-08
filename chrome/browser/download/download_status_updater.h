8d7f43a6df6900e16bf58ad19eb910a292fd9657
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 20:59:51 +0000

    Revert 121031 - Eliminate DownloadProgressUpdated.
    content::DownloadManagerDelegate::DownloadProgressUpdated was called out of sync with the content::DownloadItem::Observer calls. Eliminate it, as the notification that it provides is anti-useful.
    
    BUG=112841
    TEST=as in bug
    
    Review URL: https://chromiumcodereview.appspot.com/9363001
    
    TBR=avi@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9361046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121041 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_status_updater.h
+++ chrome/browser/download/download_status_updater.h
@@ -44,9 +44,6 @@ class DownloadStatusUpdater
   virtual void OnDownloadOpened(content::DownloadItem* download) OVERRIDE;
 
  private:
-  // Update the app icon.
-  void UpdateAppIconDownloadProgress();
-
   // Update the internal state tracking an item.
   void UpdateItem(content::DownloadItem* download);
 
