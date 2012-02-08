d7ee1daa7de0b78b5f03cd7184405070c3818f67
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 05:26:15 +0000

    [Sync] Fix thread-ordering-dependent NULL dereference in NewNonFrontendDTC
    Re-enable migration tests that were affected.
    
    Rewrite autofill DTC tests.
    
    Add basic unit test for SharedChangeProcessor.
    
    Fix bug in SharedChangeProcessor where its GenericChangeProcessor could be deleted on the wrong thread.
    
    Renamed some tests to have 'Sync' in their names.
    
    BUG=107743,111552
    TEST=
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=120698
    
    Review URL: https://chromiumcodereview.appspot.com/9307079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/shared_change_processor.h
+++ chrome/browser/sync/glue/shared_change_processor.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,10 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/message_loop_proxy.h"
 #include "base/synchronization/lock.h"
-#include "base/threading/thread_checker.h"
 #include "chrome/browser/sync/api/sync_change_processor.h"
 #include "chrome/browser/sync/api/sync_error.h"
 #include "chrome/browser/sync/engine/model_safe_worker.h"
@@ -47,8 +46,7 @@ class UnrecoverableErrorHandler;
 //
 // We use virtual methods so that we can use mock's in testing.
 class SharedChangeProcessor
-    : public base::RefCountedThreadSafe<SharedChangeProcessor>,
-      public base::ThreadChecker {
+    : public base::RefCountedThreadSafe<SharedChangeProcessor> {
  public:
   // Create an uninitialized SharedChangeProcessor (to be later connected).
   SharedChangeProcessor();
@@ -104,7 +102,12 @@ class SharedChangeProcessor
   mutable base::Lock monitor_lock_;
   bool disconnected_;
 
-  scoped_ptr<GenericChangeProcessor> generic_change_processor_;
+  // The loop that all methods except the constructor, destructor, and
+  // Disconnect() should be called on.  Set in Connect().
+  scoped_refptr<base::MessageLoopProxy> backend_loop_;
+
+  // Used only on |backend_loop_|.
+  GenericChangeProcessor* generic_change_processor_;
 
   DISALLOW_COPY_AND_ASSIGN(SharedChangeProcessor);
 };
--- chrome/browser/sync/glue/shared_change_processor_mock.h
+++ chrome/browser/sync/glue/shared_change_processor_mock.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,21 +19,24 @@ class SharedChangeProcessorMock : public SharedChangeProcessor {
   SharedChangeProcessorMock();
 
   MOCK_METHOD4(Connect, bool(
-      ProfileSyncComponentsFactory* sync_factory,
-      ProfileSyncService* sync_service,
-      UnrecoverableErrorHandler* error_handler,
-      const base::WeakPtr<SyncableService>& local_service));
+      ProfileSyncComponentsFactory*,
+      ProfileSyncService*,
+      UnrecoverableErrorHandler*,
+      const base::WeakPtr<SyncableService>&));
   MOCK_METHOD0(Disconnect, bool());
   MOCK_METHOD2(ProcessSyncChanges,
-               SyncError(const tracked_objects::Location& from_here,
-                         const SyncChangeList& change_list));
+               SyncError(const tracked_objects::Location&,
+                         const SyncChangeList&));
   MOCK_METHOD2(GetSyncDataForType,
-               SyncError(syncable::ModelType type,
-                         SyncDataList* current_sync_data));
+               SyncError(syncable::ModelType,
+                         SyncDataList*));
   MOCK_METHOD2(SyncModelHasUserCreatedNodes,
-               bool(syncable::ModelType type,
-                    bool* has_nodes));
-  MOCK_METHOD1(CryptoReadyIfNecessary, bool(syncable::ModelType type));
+               bool(syncable::ModelType,
+                    bool*));
+  MOCK_METHOD1(CryptoReadyIfNecessary, bool(syncable::ModelType));
+  MOCK_METHOD3(ActivateDataType,
+               void(ProfileSyncService*, syncable::ModelType,
+                    browser_sync::ModelSafeGroup));
 
  protected:
   virtual ~SharedChangeProcessorMock();
