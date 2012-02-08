622db5e4184016c69ce823b441b7d74f6a27c54a
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-08 00:14:42 +0000

    [SVG] Use element disappears after scripted change https://bugs.webkit.org/show_bug.cgi?id=74392
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Solution uncovered by Nikolas Zimmermann. Removing an early return that caused
    SVGUseElements to not update the shadow root's style, and therefor not render
    correctly.
    
    Test: svg/custom/use-disappears-after-style-update.svg
    
    * svg/SVGUseElement.cpp:
    (WebCore::SVGUseElement::didRecalcStyle):
    
    LayoutTests:
    
    * platform/mac-snowleopard/svg/custom/use-disappears-after-style-update-expected.png: Added.
    * platform/mac-snowleopard/svg/custom/use-disappears-after-style-update-expected.txt: Added.
    * svg/custom/use-disappears-after-style-update.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGUseElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGUseElement.cpp
@@ -359,17 +359,12 @@ void SVGUseElement::didRecalcStyle(StyleChange change)
     if (!shadowRoot)
         return;
     
-    bool needsStyleUpdate = !m_needsShadowTreeRecreation;
     if (m_needsShadowTreeRecreation) {
         shadowRoot->markShadowTreeForRecreation();
         m_needsShadowTreeRecreation = false;
     }
 
     shadowRoot->updateFromElement();
-
-    if (!needsStyleUpdate)
-        return;
-
     shadowRoot->updateStyle(change);
 }
 
