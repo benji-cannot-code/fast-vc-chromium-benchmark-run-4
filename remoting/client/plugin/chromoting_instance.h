5dcec0e2b59bee818dfbf7aeaf92d621a75ac336
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 01:01:52 +0000

    Switch Chromoting client to use new mime type.
    Old mimetype is still supported in Chrome for time-being. crbug.com/112532 is tracking the removal of this mimetype from Chrome once we ship updated webapp.
    
    BUG=none
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9309067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120886 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_instance.h
+++ remoting/client/plugin/chromoting_instance.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,9 +49,6 @@ struct ClientConfig;
 
 class ChromotingInstance : public pp::InstancePrivate {
  public:
-  // The mimetype for which this plugin is registered.
-  static const char kMimeType[];
-
   explicit ChromotingInstance(PP_Instance instance);
   virtual ~ChromotingInstance();
 
