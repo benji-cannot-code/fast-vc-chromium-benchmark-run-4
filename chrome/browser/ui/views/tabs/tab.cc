acf92e5f37bb05703a6f3d66df0a3dfc037a4737
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 01:01:36 +0000

    ui/gfx: Make the first version of Canvas::TileImageInt take a gfx::Rect.
    BUG=100898
    R=pkasting@chromium.org
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9332006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120885 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -487,13 +487,11 @@ void Tab::PaintInactiveTabBackground(gfx::Canvas* canvas) {
 
   // Now draw the highlights/shadows around the tab edge.
   canvas->DrawBitmapInt(*tab_inactive_image->image_l, 0, 0);
-  canvas->TileImageInt(*tab_inactive_image->image_c,
-                       tab_inactive_image->l_width, 0,
-                       width() - tab_inactive_image->l_width -
-                           tab_inactive_image->r_width,
-                       height());
-  canvas->DrawBitmapInt(*tab_inactive_image->image_r,
-                        width() - tab_inactive_image->r_width, 0);
+  gfx::Rect tile_rect(GetLocalBounds());
+  tile_rect.Inset(tab_inactive_image->l_width, 0,
+                  tab_inactive_image->r_width, 0);
+  canvas->TileImage(*tab_inactive_image->image_c, tile_rect);
+  canvas->DrawBitmapInt(*tab_inactive_image->image_r, tile_rect.right(), 0);
 }
 
 void Tab::PaintActiveTabBackground(gfx::Canvas* canvas) {
@@ -532,9 +530,10 @@ void Tab::PaintActiveTabBackground(gfx::Canvas* canvas) {
 
   // Now draw the highlights/shadows around the tab edge.
   canvas->DrawBitmapInt(*tab_image->image_l, 0, 0);
-  canvas->TileImageInt(*tab_image->image_c, tab_image->l_width, 0,
-      width() - tab_image->l_width - tab_image->r_width, height());
-  canvas->DrawBitmapInt(*tab_image->image_r, width() - tab_image->r_width, 0);
+  gfx::Rect tile_rect(GetLocalBounds());
+  tile_rect.Inset(tab_image->l_width, 0, tab_image->r_width, 0);
+  canvas->TileImage(*tab_image->image_c, tile_rect);
+  canvas->DrawBitmapInt(*tab_image->image_r, tile_rect.right(), 0);
 }
 
 int Tab::IconCapacity() const {
