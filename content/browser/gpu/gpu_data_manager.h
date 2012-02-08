5eead30980708ff00cd7aad2cc03abda8f12963a
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 00:42:42 +0000

    Fallback to software renderer if GPU process crashes often enough.
    This lets us use a software renderer on a card with a broken 3d implementation even if we don't have it blacklisted.
    
    BUG=111502
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9303014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120882 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager.h
+++ content/browser/gpu/gpu_data_manager.h
@@ -135,6 +135,10 @@ class CONTENT_EXPORT GpuDataManager {
   // Register a path to the SwiftShader software renderer.
   void RegisterSwiftShaderPath(FilePath path);
 
+  // Force the current card to be blacklisted (usually due to GPU process
+  // crashes).
+  void BlacklistCard();
+
  private:
   class UserFlags {
    public:
@@ -258,6 +262,9 @@ class CONTENT_EXPORT GpuDataManager {
   bool software_rendering_;
 
   FilePath swiftshader_path_;
+
+  // Current card force-blacklisted due to GPU crashes.
+  bool card_blacklisted_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuDataManager);
 };
