b11bd863a039b21aac15e15968bb1c1837e05f5d
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 19:31:47 +0000

    Revert trunk r121013. It broke the ChromiumOS build. See
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20%28Clang%20dbg%29/builds/7058/steps/compile/logs/stdio
    
    Intergrate progress bar into butter bar.
    
    This CL implements the following features:
    1. Showing an animated progress bar when pasting files/folders. Currently, the progress bar only shows when the copied files are larger than 100MB or there are more than 25 files to copy.
    2. Removing texts for cut and copy operations in butter bar. Use a blinking effect (decrease opacity to 0.8 in 100ms and restore it) to give user visual feedback for these operations.
    3. Using a x button to replace "cancel" button in butter bar
    4. Changing the error butter bar to more Kennedy style.
    5. Removing "pasting" text when pasting large files and replace it with an animated progress bar.
    
    
    BUG=chromium-os:24704
    TEST=Copy and paste files and/or folders in file manager. Verify the following:
    1. Copy and paste a large file(>100MB), verify if a 100% progress bar with no cancel button appears in butter bar.
    2. Copy and paste a few files (more than 25), verify if a progress bar appears.
    3. Copy and paste small files, when the total size is less than 100MB and the total number is less than 25, there shouldn't be a progress bar.
    4. Copy and paste same file more than 10 times. The 10th time should have show an error-bar. Verify the style of it.
    5. When copy or cut, verify if the selected items blink.
    
    
    Review URL: http://codereview.chromium.org/9307022
    
    TBR=bshe@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9363028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_copy_manager.js
+++ chrome/browser/resources/file_manager/js/file_copy_manager.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -311,9 +311,6 @@ FileCopyManager.prototype.serviceNextTask_ = function(
     }, 10);
   }
 
-  var event = new cr.Event('copy-progress');
-  event.reason = 'BEGIN';
-  this.dispatchEvent(event);
   this.serviceNextTaskEntry_(task, onEntryServiced, errorCallback);
 }
 
