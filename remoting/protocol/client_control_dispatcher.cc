76e6858a7a358f6ded5c29e7f37b9d901d3e6184
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-08 21:51:45 +0000

    Remove BeginSessionDeprecated.
    Once M17 is released no clients will need BeginSession
    message from the host, so it's safe to remove it.
    
    BUG=104670
    
    Review URL: https://chromiumcodereview.appspot.com/9243021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121055 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/client_control_dispatcher.cc
+++ remoting/protocol/client_control_dispatcher.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,15 +38,8 @@ void ClientControlDispatcher::OnInitialized() {
 void ClientControlDispatcher::OnMessageReceived(
     ControlMessage* message, const base::Closure& done_task) {
   DCHECK(client_stub_);
-
   base::ScopedClosureRunner done_runner(done_task);
-
-  if (message->has_begin_session_deprecated()) {
-    // Host sends legacy BeginSession message for compatibility with
-    // older clients. Ignore it without warning.
-  } else {
-    LOG(WARNING) << "Unknown control message received.";
-  }
+  LOG(WARNING) << "Unknown control message received.";
 }
 
 }  // namespace protocol
