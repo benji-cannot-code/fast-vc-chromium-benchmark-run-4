230bfea67ff255aa15d3dc64e6f712a49d04476e
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 01:45:54 +0000

    Add android component updater configuration.
    Android won't actually use this but it's required to build.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9346025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_configurator.cc
+++ chrome/browser/component_updater/component_updater_configurator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -55,6 +55,14 @@ const char kExtraInfo[] =
     "os=win&arch=x86&prod=chrome&prodversion=";
   #else
     #error "unknown windows architecture"
+  #endif
+#elif defined(OS_ANDROID)
+  #if defined(__i386__)
+    "os=android&arch=x86&prod=chrome&prodversion=";
+  #elif defined(__arm__)
+    "os=android&arch=arm&prod=chrome&prodversion=";
+  #else
+    "os=android&arch=unknown&prod=chrome&prodversion=";
   #endif
 #elif defined(OS_CHROMEOS)
   #if defined(__i386__)
