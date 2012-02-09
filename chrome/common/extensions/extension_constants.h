fddee7154c58996636e11b769d1b63f1aeff3069
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 05:30:13 +0000

    Support hosted app URLs for intents. Verify URL permissions.
    BUG=112602,90458
    TEST=ExtensionManifestTest.*
    
    
    Review URL: http://codereview.chromium.org/9362008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -194,6 +194,7 @@ namespace extension_manifest_errors {
   extern const char kInvalidIntent[];
   extern const char kInvalidIntentDisposition[];
   extern const char kInvalidIntentPath[];
+  extern const char kInvalidIntentPageInHostedApp[];
   extern const char kInvalidIntents[];
   extern const char kInvalidIntentType[];
   extern const char kInvalidIntentTypeElement[];
