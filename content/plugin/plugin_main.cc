b155f991182858da90d0a83315fdbd11e27f8b99
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 21:03:03 +0000

    Tighten Flash sandbox to USER_LIMITED Review URL: https://chromiumcodereview.appspot.com/9372013
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121286 0039d316-1c4b-4281-b951-d872f2087c98

--- content/plugin/plugin_main.cc
+++ content/plugin/plugin_main.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include <objbase.h>
 #include <windows.h>
+// Some directx includes can trigger deprecation warnings.
+#pragma warning(disable:4995)
+#include <dshow.h>
 #endif
 
 #include "base/command_line.h"
@@ -22,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/main_function_params.h"
 
 #if defined(OS_WIN)
+#include "base/win/scoped_comptr.h"
 #include "content/common/injection_test_dll.h"
 #include "sandbox/src/sandbox.h"
 #elif defined(OS_POSIX) && !defined(OS_MACOSX)
@@ -135,6 +139,11 @@ int PluginMain(const content::MainFunctionParams& parameters) {
         DVLOG(1) << "Sandboxing flash";
         if (!PreloadIMEForFlash())
           DVLOG(1) << "IME preload failed";
+
+        // Warm up the device enumerator for webcam and microphone.
+        base::win::ScopedComPtr<ICreateDevEnum> device_enumerator;
+        device_enumerator.CreateInstance(CLSID_SystemDeviceEnum);
+
         DelayedLowerToken(target_services);
       } else {
         target_services->LowerToken();
