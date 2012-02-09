6c49224842f5463ff628668c3e85fddb3742a5d4
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 18:28:37 +0000

    Fix an indent issue in VideoCaptureManager::GetAvailableDevices BUG=NONE TEST=NONE Review URL: https://chromiumcodereview.appspot.com/9368023
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121254 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/video_capture_manager.cc
+++ content/browser/renderer_host/media/video_capture_manager.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -356,7 +356,7 @@ void VideoCaptureManager::GetAvailableDevices(
   DCHECK(IsOnCaptureDeviceThread());
 
   if (!use_fake_device_) {
-     media::VideoCaptureDevice::GetDeviceNames(device_names);
+    media::VideoCaptureDevice::GetDeviceNames(device_names);
   } else {
     media::FakeVideoCaptureDevice::GetDeviceNames(device_names);
   }
