7f86c82dd6d55fdff437353fe87ac81f32b9da9c
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 01:54:13 +0000

    Verifies that the AutocompleteEditModel goes through the right intermediate UI calls before seeing results / opening a suggestion.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9358036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121143 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_browsertest.cc
+++ chrome/browser/autocomplete/autocomplete_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -278,7 +278,6 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, ExtensionAppProvider) {
   // loaded so that the autocomplete results are consistent.
   ui_test_utils::WaitForHistoryToLoad(browser());
 
-  LocationBar* location_bar = GetLocationBar();
   AutocompleteController* autocomplete_controller = GetAutocompleteController();
 
   // Try out the packaged app.
@@ -288,16 +287,12 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, ExtensionAppProvider) {
         AutocompleteInput::SYNCHRONOUS_MATCHES);
 
     EXPECT_TRUE(autocomplete_controller->done());
-    EXPECT_TRUE(location_bar->GetInputString().empty());
-    EXPECT_TRUE(location_bar->location_entry()->GetText().empty());
-    EXPECT_TRUE(location_bar->location_entry()->IsSelectAll());
     const AutocompleteResult& result = autocomplete_controller->result();
     EXPECT_GT(result.size(), 1U) << AutocompleteResultAsString(result);
     AutocompleteMatch match = result.match_at(0);
     EXPECT_EQ(ASCIIToUTF16("Packaged App Test"), match.contents);
     EXPECT_EQ(AutocompleteMatch::EXTENSION_APP, match.type);
     EXPECT_FALSE(match.deletable);
-    location_bar->AcceptInput();
   }
 
   browser()->NewTab();
@@ -309,9 +304,6 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, ExtensionAppProvider) {
         AutocompleteInput::SYNCHRONOUS_MATCHES);
 
     EXPECT_TRUE(autocomplete_controller->done());
-    EXPECT_TRUE(location_bar->GetInputString().empty());
-    EXPECT_TRUE(location_bar->location_entry()->GetText().empty());
-    EXPECT_TRUE(location_bar->location_entry()->IsSelectAll());
     const AutocompleteResult& result = autocomplete_controller->result();
     // 'App test' is also a substring of extension 'Packaged App Test'.
     EXPECT_GT(result.size(), 2U) << AutocompleteResultAsString(result);
@@ -319,6 +311,5 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, ExtensionAppProvider) {
     EXPECT_EQ(ASCIIToUTF16("App Test"), match.contents);
     EXPECT_EQ(AutocompleteMatch::EXTENSION_APP, match.type);
     EXPECT_FALSE(match.deletable);
-    location_bar->AcceptInput();
   }
 }
