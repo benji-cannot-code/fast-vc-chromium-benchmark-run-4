1f9f8125886505b90e9494306c1ae581fca20c05
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 00:06:54 +0000

    views: Get rid of unused ExamplesMain class.
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9362019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121109 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/views/examples/examples_main.h"
-
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
