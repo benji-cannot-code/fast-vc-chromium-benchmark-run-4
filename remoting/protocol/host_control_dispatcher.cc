acc60418093ebd846fc2e3db84293e827554a61a
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 19:08:08 +0000

    Revert 121055 - Remove BeginSessionDeprecated.
    Once M17 is released no clients will need BeginSession
    message from the host, so it's safe to remove it.
    
    BUG=104670
    
    Review URL: https://chromiumcodereview.appspot.com/9243021
    
    TBR=sergeyu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9369037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121261 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/host_control_dispatcher.cc
+++ remoting/protocol/host_control_dispatcher.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,6 +31,13 @@ void HostControlDispatcher::OnInitialized() {
   reader_.Init(channel(), base::Bind(
       &HostControlDispatcher::OnMessageReceived, base::Unretained(this)));
   writer_->Init(channel(), BufferedSocketWriter::WriteFailedCallback());
+
+  // Write legacy BeginSession message.
+  // TODO(sergeyu): Remove it. See http://crbug.com/104670 .
+  protocol::ControlMessage message;
+  message.mutable_begin_session_deprecated()->mutable_login_status()->
+      set_success(true);
+  writer_->Write(SerializeAndFrameMessage(message), base::Closure());
 }
 
 void HostControlDispatcher::OnMessageReceived(
