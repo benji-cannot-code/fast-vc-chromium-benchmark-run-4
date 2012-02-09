be04cb8fd351833e1d16e7e85e4505ddde90bc0b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 05:59:40 +0000

    net: Rename FileStream::Open/Close with OpenSync/CloseSync.
    This is in preparation for implementing async versions of Open()
    and Close().
    
    The existing clients are changed to use OpenSync/CloseSync.
    No logic is changed.
    
    The original patch was reverted as it broke linux_chromeos build.
    This patch includes fixes for the build.
    
    TEST=git try -r 121075 --bot=win,linux,mac,linux_chromeos,linux_chromeos_gtk,win_aura to confirm everything is built as before.
    BUG=72001
    
    Review URL: https://chromiumcodereview.appspot.com/9373001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121202 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/web_drag_source_mac.mm
+++ content/browser/tab_contents/web_drag_source_mac.mm
@@ -83,7 +83,7 @@ void PromiseWriterHelper(const WebDropData& drop_data,
                      net::CompletionCallback());
 
   if (file_stream)
-    file_stream->Close();
+    file_stream->CloseSync();
 }
 
 }  // namespace
--- webkit/tools/test_shell/simple_resource_loader_bridge.cc
+++ webkit/tools/test_shell/simple_resource_loader_bridge.cc
@@ -333,7 +333,7 @@ class RequestProxy : public net::URLRequest::Delegate,
       if (file_util::CreateTemporaryFile(&path)) {
         downloaded_file_ = DeletableFileReference::GetOrCreate(
             path, base::MessageLoopProxy::current());
-        file_stream_.Open(
+        file_stream_.OpenSync(
             path, base::PLATFORM_FILE_OPEN | base::PLATFORM_FILE_WRITE);
       }
     }
@@ -427,7 +427,7 @@ class RequestProxy : public net::URLRequest::Delegate,
                                   const std::string& security_info,
                                   const base::TimeTicks& complete_time) {
     if (download_to_file_)
-      file_stream_.Close();
+      file_stream_.CloseSync();
     owner_loop_->PostTask(
         FROM_HERE,
         base::Bind(&RequestProxy::NotifyCompletedRequest, this, status,
@@ -733,7 +733,7 @@ class SyncRequestProxy : public RequestProxy {
                                   const std::string& security_info,
                                   const base::TimeTicks& complete_time) {
     if (download_to_file_)
-      file_stream_.Close();
+      file_stream_.CloseSync();
     result_->status = status;
     event_.Signal();
   }
