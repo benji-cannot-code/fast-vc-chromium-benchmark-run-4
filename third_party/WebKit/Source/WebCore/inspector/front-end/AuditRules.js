07374db5a4d1b6553fbbb6b659262121d2e762e1
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-09 12:41:37 +0000

    Web Inspector: Update protocol and UI to follow bug 77204 (Kill per-Attribute style declarations) https://bugs.webkit.org/show_bug.cgi?id=77962
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    * English.lproj/localizedStrings.js:
    * inspector/Inspector.json:
    * inspector/InspectorCSSAgent.cpp:
    (WebCore::InspectorCSSAgent::getInlineStylesForNode):
    (WebCore::InspectorCSSAgent::buildObjectForAttributesStyle):
    * inspector/InspectorCSSAgent.h:
    (InspectorCSSAgent):
    * inspector/front-end/AuditRules.js:
    (WebInspector.AuditRules.ImageDimensionsRule.prototype.doRun):
    (WebInspector.AuditRules.ImageDimensionsRule.prototype.doRun.getStyles):
    * inspector/front-end/CSSStyleModel.js:
    (WebInspector.CSSStyleModel.prototype.getInlineStylesAsync):
    * inspector/front-end/StylesSidebarPane.js:
    (WebInspector.StylesSidebarPane.prototype._executeRebuildUpdate.inlineCallback):
    (WebInspector.StylesSidebarPane.prototype._rebuildStyleRules):
    (WebInspector.StylesSidebarPane.prototype._rebuildStyleRules.get continue):
    
    LayoutTests:
    
    * inspector/elements/elements-panel-styles-expected.txt:
    * inspector/styles/styles-computed-trace-expected.txt:
    * inspector/styles/styles-new-API-expected.txt:
    * inspector/styles/styles-new-API.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/AuditRules.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/AuditRules.js
@@ -730,8 +730,10 @@ WebInspector.AuditRules.ImageDimensionsRule.prototype = {
                 return;
             }
 
-            var widthFound = "width" in styles.styleAttributes;
-            var heightFound = "height" in styles.styleAttributes;
+            if (styles.attributesStyle) {
+                var widthFound = !!styles.attributesStyle.getLiveProperty("width");
+                var heightFound = !!styles.attributesStyle.getLiveProperty("height");
+            }
 
             var inlineStyle = styles.inlineStyle;
             if (inlineStyle) {
@@ -766,10 +768,10 @@ WebInspector.AuditRules.ImageDimensionsRule.prototype = {
                 return;
             var targetResult = {};
 
-            function inlineCallback(inlineStyle, styleAttributes)
+            function inlineCallback(inlineStyle, attributesStyle)
             {
                 targetResult.inlineStyle = inlineStyle;
-                targetResult.styleAttributes = styleAttributes;
+                targetResult.attributesStyle = attributesStyle;
             }
 
             function matchedCallback(result)
