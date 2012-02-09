8faf60cec49a1008fcb8555c7ac509ca2d36e54a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-09 09:54:04 +0000

    Disable adding a shadow root to elements having a dynamic built-in shadow root. https://bugs.webkit.org/show_bug.cgi?id=77935
    Patch by Shinya Kawanaka <shinyak@google.com> on 2012-02-09
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    We temporarily disable adding a shadow root in elements having a dynamic user agent shadow root.
    These shadow roots are currently created using Element::ensureShadowRoot.
    So we don't check the condition if a shadow root is created eaither using Element::ensureShadowRoot
    or ShadowRoot::CreatingUserAgentShadowRoot is specified for ShadowRoot::create.
    
    Test: fast/dom/shadow/shadow-disable.html
    
    * dom/Element.cpp:
    (WebCore::Element::ensureShadowRoot):
    * dom/ShadowRoot.cpp:
    (WebCore::allowsUserShadowRoot):
    (WebCore):
    (WebCore::ShadowRoot::create):
    * dom/ShadowRoot.h:
    (ShadowRoot):
    * html/HTMLDetailsElement.cpp:
    (WebCore::HTMLDetailsElement::createShadowSubtree):
      ShadowRoot::CreatingUserAgentShadowRoot is specified.
    * html/HTMLKeygenElement.cpp:
    (WebCore::HTMLKeygenElement::HTMLKeygenElement): ditto.
    * html/HTMLMeterElement.cpp:
    (WebCore::HTMLMeterElement::createShadowSubtree): ditto.
    * html/HTMLProgressElement.cpp:
    (WebCore::HTMLProgressElement::createShadowSubtree): ditto.
    * html/HTMLSummaryElement.cpp:
    (WebCore::HTMLSummaryElement::createShadowSubtree): ditto.
    * html/HTMLTextAreaElement.cpp:
    (WebCore::HTMLTextAreaElement::createShadowSubtree): ditto.
    
    LayoutTests:
    
    * fast/dom/shadow/shadow-disable-expected.txt: Added.
    * fast/dom/shadow/shadow-disable.html: Added.
    * platform/efl/Skipped:
    * platform/gtk/Skipped:
    * platform/mac/Skipped:
    * platform/qt/Skipped:
    * platform/win/Skipped:
    * platform/wincairo/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLKeygenElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLKeygenElement.cpp
@@ -87,7 +87,7 @@ inline HTMLKeygenElement::HTMLKeygenElement(const QualifiedName& tagName, Docume
     }
 
     ASSERT(!shadowRoot());
-    RefPtr<ShadowRoot> root = ShadowRoot::create(this, ASSERT_NO_EXCEPTION);
+    RefPtr<ShadowRoot> root = ShadowRoot::create(this, ShadowRoot::CreatingUserAgentShadowRoot);
     root->appendChild(select, ec);
 }
 
--- third_party/WebKit/Source/WebCore/html/HTMLProgressElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLProgressElement.cpp
@@ -160,7 +160,7 @@ void HTMLProgressElement::createShadowSubtree()
     m_value = ProgressValueElement::create(document());
     bar->appendChild(m_value, ASSERT_NO_EXCEPTION);
 
-    RefPtr<ShadowRoot> root = ShadowRoot::create(this, ASSERT_NO_EXCEPTION);
+    RefPtr<ShadowRoot> root = ShadowRoot::create(this, ShadowRoot::CreatingUserAgentShadowRoot, ASSERT_NO_EXCEPTION);
     root->appendChild(bar, ASSERT_NO_EXCEPTION);
 }
 
