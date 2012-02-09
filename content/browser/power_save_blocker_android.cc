c2fb2816d120a82dc7c2301e5752fa60c8f09211
satish@chromium.org <satish@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 01:55:23 +0000

    Adds a couple of stub classes in content/browser to get past linker errors with content_unittests. The real implementation will follow in subsequent CLs.
    BUG=None
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9358029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121144 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/browser/power_save_blocker_android.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/power_save_blocker.h"
+
+#include "base/logging.h"
+
+// Called only from UI thread.
+// static
+void PowerSaveBlocker::ApplyBlock(PowerSaveBlockerType type) {
+  // TODO(wangxianzhu): Implement it.
+  NOTIMPLEMENTED();
+}
---
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/render_widget_host_view.h"
+
+#include "base/logging.h"
+
+// static
+void RenderWidgetHostView::GetDefaultScreenInfo(
+    WebKit::WebScreenInfo* results) {
+  NOTIMPLEMENTED();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// RenderWidgetHostView, public:
+
+// static
+RenderWidgetHostView* RenderWidgetHostView::CreateViewForWidget(
+    RenderWidgetHost* widget) {
+  NOTIMPLEMENTED();
+  return NULL;
+}
+
