be85b035b6a4469e5b9c63b58ed9506d6924669c
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 08:49:35 +0000

    Removes usage of the term low-latency in AudioInputRendererHost.
    BUG=none
    TEST=content_unittests and WebRTC demo applications
    
    Review URL: https://chromiumcodereview.appspot.com/9332002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121213 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/audio_input_device.cc
+++ content/renderer/media/audio_input_device.cc
@@ -111,8 +111,7 @@ void AudioInputDevice::InitializeOnIOThread() {
   // and create the stream when getting a OnDeviceReady() callback.
   if (!session_id_) {
     Send(new AudioInputHostMsg_CreateStream(
-        stream_id_, audio_parameters_, true,
-        AudioManagerBase::kDefaultDeviceId));
+        stream_id_, audio_parameters_, AudioManagerBase::kDefaultDeviceId));
   } else {
     Send(new AudioInputHostMsg_StartDevice(stream_id_, session_id_));
     pending_device_ready_ = true;
@@ -155,7 +154,7 @@ void AudioInputDevice::SetVolumeOnIOThread(double volume) {
     Send(new AudioInputHostMsg_SetVolume(stream_id_, volume));
 }
 
-void AudioInputDevice::OnLowLatencyCreated(
+void AudioInputDevice::OnStreamCreated(
     base::SharedMemoryHandle handle,
     base::SyncSocket::Handle socket_handle,
     uint32 length) {
@@ -169,9 +168,9 @@ void AudioInputDevice::OnLowLatencyCreated(
 #endif
   DCHECK(length);
   DCHECK(!audio_thread_.get());
+  DVLOG(1) << "OnStreamCreated (stream_id=" << stream_id_ << ")";
 
-  DVLOG(1) << "OnLowLatencyCreated (stream_id=" << stream_id_ << ")";
-  // Takes care of the case when Stop() is called before OnLowLatencyCreated().
+  // Takes care of the case when Stop() is called before OnStreamCreated().
   if (!stream_id_) {
     base::SharedMemory::CloseHandle(handle);
     // Close the socket handler.
@@ -243,7 +242,7 @@ void AudioInputDevice::OnDeviceReady(const std::string& device_id) {
     stream_id_ = 0;
   } else {
     Send(new AudioInputHostMsg_CreateStream(stream_id_, audio_parameters_,
-                                            true, device_id));
+                                            device_id));
   }
 
   pending_device_ready_ = false;
--- content/renderer/media/audio_input_message_filter.cc
+++ content/renderer/media/audio_input_message_filter.cc
@@ -43,8 +43,8 @@ bool AudioInputMessageFilter::Send(IPC::Message* message) {
 bool AudioInputMessageFilter::OnMessageReceived(const IPC::Message& message) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(AudioInputMessageFilter, message)
-    IPC_MESSAGE_HANDLER(AudioInputMsg_NotifyLowLatencyStreamCreated,
-                        OnLowLatencyStreamCreated)
+    IPC_MESSAGE_HANDLER(AudioInputMsg_NotifyStreamCreated,
+                        OnStreamCreated)
     IPC_MESSAGE_HANDLER(AudioInputMsg_NotifyStreamVolume, OnStreamVolume)
     IPC_MESSAGE_HANDLER(AudioInputMsg_NotifyStreamStateChanged,
                         OnStreamStateChanged)
@@ -69,7 +69,7 @@ void AudioInputMessageFilter::OnChannelClosing() {
   channel_ = NULL;
 }
 
-void AudioInputMessageFilter::OnLowLatencyStreamCreated(
+void AudioInputMessageFilter::OnStreamCreated(
     int stream_id,
     base::SharedMemoryHandle handle,
 #if defined(OS_WIN)
@@ -90,7 +90,7 @@ void AudioInputMessageFilter::OnLowLatencyStreamCreated(
     return;
   }
   // Forward message to the stream delegate.
-  delegate->OnLowLatencyCreated(handle, socket_handle, length);
+  delegate->OnStreamCreated(handle, socket_handle, length);
 }
 
 void AudioInputMessageFilter::OnStreamVolume(int stream_id, double volume) {
