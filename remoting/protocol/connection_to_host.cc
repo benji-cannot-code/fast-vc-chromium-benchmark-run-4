84c94b6d0eb244afbec26b3ca0804cd3518fbcea
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 01:01:44 +0000

    Revert 121094 - Add abstract interfaces for the transport layer.
    Renamed PepperChannel to Transport and added TransportFactory. The new 
    interfaces abstract transport layer for the signaling layer.
    
    BUG=110485,109630
    
    
    Review URL: http://codereview.chromium.org/9325036
    
    TBR=sergeyu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9373011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121126 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/connection_to_host.cc
+++ remoting/protocol/connection_to_host.cc
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/client_control_dispatcher.h"
 #include "remoting/protocol/client_event_dispatcher.h"
 #include "remoting/protocol/client_stub.h"
+#include "remoting/protocol/jingle_session_manager.h"
 #include "remoting/protocol/pepper_session_manager.h"
-#include "remoting/protocol/pepper_transport_factory.h"
 #include "remoting/protocol/video_reader.h"
 #include "remoting/protocol/video_stub.h"
 #include "remoting/protocol/util.h"
@@ -71,9 +71,7 @@ void ConnectionToHost::Connect(scoped_refptr<XmppProxy> xmpp_proxy,
   signal_strategy_->AddListener(this);
   signal_strategy_->Connect();
 
-  scoped_ptr<TransportFactory> transport_factory(
-      new PepperTransportFactory(pp_instance_));
-  session_manager_.reset(new PepperSessionManager(transport_factory.Pass()));
+  session_manager_.reset(new PepperSessionManager(pp_instance_));
   session_manager_->Init(signal_strategy_.get(), this,
                          NetworkSettings(allow_nat_traversal_));
 }
