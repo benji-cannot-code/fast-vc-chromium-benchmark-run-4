ffa8ad879a119fe597bbca1c4c0d5ee4e4206147
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-10 20:49:45 +0000

    Minor ScrollableArea cleanup https://bugs.webkit.org/show_bug.cgi?id=78372
    Reviewed by Beth Dakin.
    
    Get rid of scrollToXOffsetWithoutAnimation and scrollToYOffsetWithoutAnimation.
    
    * platform/ScrollableArea.cpp:
    (WebCore::ScrollableArea::scrollToOffsetWithoutAnimation):
    * platform/ScrollableArea.h:
    * rendering/RenderListBox.cpp:
    (WebCore::RenderListBox::computeLogicalHeight):
    (WebCore::RenderListBox::scrollToRevealElementAtListIndex):
    (WebCore::RenderListBox::setScrollTop):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderListBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderListBox.cpp
@@ -256,7 +256,7 @@ void RenderListBox::computeLogicalHeight()
         m_vBar->setSteps(1, max(1, numVisibleItems() - 1), itemHeight);
         m_vBar->setProportion(numVisibleItems(), numItems());
         if (!enabled) {
-            scrollToYOffsetWithoutAnimation(0);
+            scrollToOffsetWithoutAnimation(VerticalScrollbar, 0);
             m_indexOffset = 0;
         }
     }
@@ -580,7 +580,7 @@ bool RenderListBox::scrollToRevealElementAtListIndex(int index)
     else
         newOffset = index - numVisibleItems() + 1;
 
-    ScrollableArea::scrollToYOffsetWithoutAnimation(newOffset);
+    scrollToOffsetWithoutAnimation(VerticalScrollbar, newOffset);
 
     return true;
 }
@@ -676,7 +676,7 @@ void RenderListBox::setScrollTop(int newTop)
     if (index < 0 || index >= numItems() || index == m_indexOffset)
         return;
     
-    ScrollableArea::scrollToYOffsetWithoutAnimation(index);
+    scrollToOffsetWithoutAnimation(VerticalScrollbar, index);
 }
 
 bool RenderListBox::nodeAtPoint(const HitTestRequest& request, HitTestResult& result, const LayoutPoint& pointInContainer, const LayoutPoint& accumulatedOffset, HitTestAction hitTestAction)
