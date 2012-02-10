52e47f8f7191f6d1a9776830ba5677989cf9a73b
ihf@chromium.org <ihf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 19:58:12 +0000

    Fix pepper flash fullscreen.
    Fullscreen video was broken by
    http://src.chromium.org/viewvc/chrome?view=rev&revision=120490
    After going fullscreen painting stopped. It seems the reason is that the
    change above forget hooking up DidInitiatePaint.
    
    BUG=111639
    TEST=Ran youtube videos in fullsceen. Ran streetview in fullscreen. 
    
    
    Review URL: http://codereview.chromium.org/9370051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121498 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget_fullscreen_pepper.cc
+++ content/renderer/render_widget_fullscreen_pepper.cc
@@ -290,6 +290,11 @@ void RenderWidgetFullscreenPepper::WillInitiatePaint() {
     plugin_->ViewWillInitiatePaint();
 }
 
+void RenderWidgetFullscreenPepper::DidInitiatePaint() {
+  if (plugin_)
+    plugin_->ViewInitiatedPaint();
+}
+
 void RenderWidgetFullscreenPepper::DidFlushPaint() {
   if (plugin_)
     plugin_->ViewFlushedPaint();
