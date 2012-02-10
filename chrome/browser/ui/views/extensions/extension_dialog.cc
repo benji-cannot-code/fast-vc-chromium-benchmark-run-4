f229670e2e67a4fa966baf96467b1a3de64379b1
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 02:51:51 +0000

    Add fullscreen window support for extension dialog boxes. Add the ability to open a fullscreen, always on top window via an extension dialog box. This ability is needed for the screensaver for retail mode.
    R=jamescook@chromium.org,sky@chromium.org
    BUG=chromium-os:26042
    TEST=Tested this with changing the dimensions of the select file extension dialog window.
    
    
    Review URL: http://codereview.chromium.org/9374007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_dialog.cc
+++ chrome/browser/ui/views/extensions/extension_dialog.cc
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/background.h"
 #include "ui/views/widget/widget.h"
 
+#if defined(USE_AURA)
+#include "ui/aura/root_window.h"
+#include "ui/aura/window.h"
+#endif
+
 using content::WebContents;
 
 ExtensionDialog::ExtensionDialog(ExtensionHost* host,
@@ -51,6 +56,32 @@ ExtensionDialog* ExtensionDialog::Show(
     int height,
     const string16& title,
     ExtensionDialogObserver* observer) {
+  return ExtensionDialog::ShowInternal(url, browser, web_contents, width,
+                                       height, false, title, observer);
+}
+
+#if defined(USE_AURA)
+// static
+ExtensionDialog* ExtensionDialog::ShowFullscreen(
+    const GURL& url,
+    Browser* browser,
+    WebContents* web_contents,
+    const string16& title,
+    ExtensionDialogObserver* observer) {
+  return ExtensionDialog::ShowInternal(url, browser, web_contents, 0, 0,
+                                       true, title, observer);
+}
+#endif
+
+// static
+ExtensionDialog* ExtensionDialog::ShowInternal(const GURL& url,
+    Browser* browser,
+    content::WebContents* web_contents,
+    int width,
+    int height,
+    bool fullscreen,
+    const string16& title,
+    ExtensionDialogObserver* observer) {
   CHECK(browser);
   ExtensionHost* host = CreateExtensionHost(url, browser);
   if (!host)
@@ -59,7 +90,10 @@ ExtensionDialog* ExtensionDialog::Show(
 
   ExtensionDialog* dialog = new ExtensionDialog(host, observer);
   dialog->set_title(title);
-  dialog->InitWindow(browser, width, height);
+  if (fullscreen)
+    dialog->InitWindowFullscreen(browser);
+  else
+    dialog->InitWindow(browser, width, height);
 
   // Show a white background while the extension loads.  This is prettier than
   // flashing a black unfilled window frame.
@@ -82,6 +116,32 @@ ExtensionHost* ExtensionDialog::CreateExtensionHost(const GURL& url,
     return NULL;
   return manager->CreateDialogHost(url, browser);
 }
+
+#if defined(USE_AURA)
+void ExtensionDialog::InitWindowFullscreen(Browser* browser) {
+  gfx::NativeWindow parent = browser->window()->GetNativeHandle();
+
+  // Create the window as a child of the root window.
+  window_ = browser::CreateFramelessViewsWindow(
+      parent->GetRootWindow(), this);
+  // Make sure we're always on top by putting ourselves at the top
+  // of the z-order of the child windows of the root window.
+  parent->GetRootWindow()->StackChildAtTop(window_->GetNativeWindow());
+
+  int width = parent->GetRootWindow()->GetHostSize().width();
+  int height = parent->GetRootWindow()->GetHostSize().height();
+  window_->SetBounds(gfx::Rect(0, 0, width, height));
+
+  window_->Show();
+  // TODO(jamescook): Remove redundant call to Activate()?
+  window_->Activate();
+}
+#else
+void ExtensionDialog::InitWindowFullscreen(Browser* browser) {
+  NOTIMPLEMENTED();
+}
+#endif
+
 
 void ExtensionDialog::InitWindow(Browser* browser, int width, int height) {
   gfx::NativeWindow parent = browser->window()->GetNativeHandle();
