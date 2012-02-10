8929289e0829a5ff6e748f272dc8b4aea26326a7
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 16:26:13 +0000

    Change geolocaiton browser name in official builds
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9123009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121457 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/network_location_request.cc
+++ content/browser/geolocation/network_location_request.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -151,7 +151,12 @@ GURL FormRequestURL(const std::string& url,
   }
 
   std::vector<std::string> params;
+#if defined(GOOGLE_CHROME_BUILD)
+  params.push_back("browser=googlechrome");
+#else
   params.push_back("browser=chromium");
+#endif
+
   params.push_back("sensor=true");
   if (!access_token.empty())
     params.push_back("token=" + UTF16ToUTF8(access_token));
