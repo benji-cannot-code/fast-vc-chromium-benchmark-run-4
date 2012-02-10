53273e67f644912935a41d12c4a2d82d7fdee63c
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 21:47:09 +0000

    Add status area buttons after Profile has been initialized.
    BUG=113108
    TEST=See issue. Ensure status area buttons show up as expected on startup.
    
    
    Review URL: http://codereview.chromium.org/9371027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_extra_parts_aura.cc
+++ chrome/browser/chrome_browser_main_extra_parts_aura.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/aura/chrome_shell_delegate.h"
 #include "chrome/browser/ui/views/aura/screen_orientation_listener.h"
 #include "chrome/browser/ui/views/aura/screenshot_taker.h"
+#include "chrome/browser/ui/views/aura/status_area_host_aura.h"
 #include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
 #include "ui/gfx/compositor/compositor_setup.h"
@@ -56,6 +57,11 @@ void ChromeBrowserMainExtraPartsAura::PreProfileInit() {
 
   // Make sure the singleton ScreenOrientationListener object is created.
   ScreenOrientationListener::GetInstance();
+}
+
+void ChromeBrowserMainExtraPartsAura::PostProfileInit() {
+  // Add the status area buttons after Profile has been initialized.
+  ChromeShellDelegate::instance()->status_area_host()->AddButtons();
 }
 
 void ChromeBrowserMainExtraPartsAura::PostMainMessageLoopRun() {
