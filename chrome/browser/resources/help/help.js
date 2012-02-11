805866efc0d807e7bcac655b1af3a4fce6c8944e
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-11 00:16:47 +0000

    Help: Set the title of the help page.
    BUG=none
    TEST=none
    R=csilv
    
    Review URL: https://chromiumcodereview.appspot.com/9383026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121578 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/help/help.js
+++ chrome/browser/resources/help/help.js
@@ -24,6 +24,10 @@ cr.define('help_page', function() {
     initialize: function() {
       uber.onContentFrameLoaded();
 
+      // Set the title.
+      var title = localStrings.getString('helpTitle');
+      uber.invokeMethodOnParent('setTitle', {title: title});
+
       $('product-license').innerHTML = localStrings.getString('productLicense');
 
       var productTOS = $('product-tos');
