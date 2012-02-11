aed8425394cea5a388768c17a3d4b49cdadd9b6f
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-11 00:06:53 +0000

    Enable MUTATION_OBSERVERS by default on all platforms https://bugs.webkit.org/show_bug.cgi?id=78196
    Reviewed by Ojan Vafai.
    
    .:
    
    * configure.ac:
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore:
    
    * Configurations/FeatureDefines.xcconfig:
    * UseV8.cmake: Add some previously-missing files.
    * WebCore.vcproj/WebCore.vcproj: ditto.
    * bindings/js/JSBindingsAllInOne.cpp: ditto.
    * dom/DOMAllInOne.cpp: ditto.
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Tools:
    
    * Scripts/build-webkit:
    
    WebKitLibraries:
    
    * win/tools/vsprops/FeatureDefines.vsprops:
    * win/tools/vsprops/FeatureDefinesCairo.vsprops:
    
    LayoutTests:
    
    * platform/efl/Skipped: Stop skipping the tests except for those known to fail.
    * platform/gtk/Skipped: ditto.
    * platform/mac/Skipped: ditto.
    * platform/qt/Skipped: ditto.
    * platform/win/Skipped: ditto.
    * platform/wincairo/Skipped: ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/UseV8.cmake
+++ third_party/WebKit/Source/WebCore/UseV8.cmake
@@ -130,6 +130,7 @@ LIST(APPEND WebCore_SOURCES
     bindings/v8/custom/V8MessageChannelConstructor.cpp
     bindings/v8/custom/V8MessageEventCustom.cpp
     bindings/v8/custom/V8MessagePortCustom.cpp
+    bindings/v8/custom/V8MutationCallbackCustom.cpp
     bindings/v8/custom/V8NamedNodeMapCustom.cpp
     bindings/v8/custom/V8NamedNodesCollection.cpp
     bindings/v8/custom/V8NavigatorCustom.cpp
@@ -149,6 +150,7 @@ LIST(APPEND WebCore_SOURCES
     bindings/v8/custom/V8Uint8ArrayCustom.cpp
     bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
     bindings/v8/custom/V8WebKitAnimationCustom.cpp
+    bindings/v8/custom/V8WebKitMutationObserverCustom.cpp
     bindings/v8/custom/V8WebKitPointConstructor.cpp
     bindings/v8/custom/V8WebSocketCustom.cpp
     bindings/v8/custom/V8WorkerContextCustom.cpp
