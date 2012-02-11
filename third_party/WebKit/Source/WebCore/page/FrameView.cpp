8c12b1e4c51b74ad69cd954f8a6ae31cccafd895
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-11 04:00:10 +0000

    Non-threaded scrolling build fix.
    * page/FrameView.cpp:
    (WebCore::FrameView::requestScrollPositionUpdate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameView.cpp
@@ -1767,6 +1767,8 @@ bool FrameView::requestScrollPositionUpdate(const IntPoint& position)
         if (ScrollingCoordinator* scrollingCoordinator = page->scrollingCoordinator())
             return scrollingCoordinator->requestScrollPositionUpdate(this, position);
     }
+#else
+    UNUSED_PARAM(position);
 #endif
 
     return false;
