9bda45ef6db433b001ef842224c7c95a67a4305c
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-11 01:41:32 +0000

    Unreviewed build fix.
    Fixing the build by removing a duplicate definition of pixelSnappedIntRect in LayoutTypes.h and
    removing conflict markers from the changelog.
    
    * rendering/LayoutTypes.h:
    (pixelSnappedIntRect):
    * ChangeLog:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/LayoutTypes.h
+++ third_party/WebKit/Source/WebCore/rendering/LayoutTypes.h
@@ -57,11 +57,6 @@ inline IntRect pixelSnappedIntRect(const LayoutRect& rect)
     return rect;
 }
 
-inline IntRect pixelSnappedIntRect(const LayoutRect& rect)
-{
-    return rect;
-}
-
 inline LayoutSize roundedLayoutSize(const FloatSize& s)
 {
     return roundedIntSize(s);
