4379628a26ea1dda09487d7703c2677b7d023845
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-11 23:02:59 +0000

    Implement more ScrollElasticityControllerClient member functions https://bugs.webkit.org/show_bug.cgi?id=78425 <rdar://problem/10710727>
    Reviewed by Sam Weinig.
    
    * page/scrolling/ScrollingTreeNode.h:
    (ScrollingTreeNode):
    (WebCore::ScrollingTreeNode::horizontalScrollElasticity):
    (WebCore::ScrollingTreeNode::verticalScrollElasticity):
    (WebCore::ScrollingTreeNode::hasEnabledHorizontalScrollbar):
    (WebCore::ScrollingTreeNode::hasEnabledVerticalScrollbar):
    Add new getters.
    
    * page/scrolling/mac/ScrollingTreeNodeMac.h:
    * page/scrolling/mac/ScrollingTreeNodeMac.mm:
    (WebCore::ScrollingTreeNodeMac::allowsHorizontalStretching):
    (WebCore::ScrollingTreeNodeMac::allowsVerticalStretching):
    (WebCore::ScrollingTreeNodeMac::stretchAmount):
    (WebCore::ScrollingTreeNodeMac::pinnedInDirection):
    (WebCore::ScrollingTreeNodeMac::canScrollHorizontally):
    (WebCore::ScrollingTreeNodeMac::canScrollVertically):
    (WebCore::ScrollingTreeNodeMac::absoluteScrollPosition):
    (WebCore::ScrollingTreeNodeMac::immediateScrollByWithoutContentEdgeConstraints):
    (WebCore::ScrollingTreeNodeMac::startSnapRubberbandTimer):
    (WebCore::ScrollingTreeNodeMac::stopSnapRubberbandTimer):
    (WebCore::ScrollingTreeNodeMac::scrollByWithoutContentEdgeConstraints):
    Implement ScrollElasticityControllerClient member functions.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107488 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/scrolling/mac/ScrollingTreeNodeMac.h
+++ third_party/WebKit/Source/WebCore/page/scrolling/mac/ScrollingTreeNodeMac.h
@@ -66,9 +66,13 @@ private:
 
     IntPoint minimumScrollPosition() const;
     IntPoint maximumScrollPosition() const;
+
     void scrollBy(const IntSize&);
+    void scrollByWithoutContentEdgeConstraints(const IntSize&);
 
     ScrollElasticityController m_scrollElasticityController;
+    RetainPtr<CFRunLoopTimerRef> m_snapRubberbandTimer;
+
     RetainPtr<CALayer> m_scrollLayer;
 };
 
