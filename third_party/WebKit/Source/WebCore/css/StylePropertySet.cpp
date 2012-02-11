e8b5a651f57dad9c9cfe34078b5d127ab3041f4a
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-11 07:52:11 +0000

    Move CSSOM wrapper pointer out of StylePropertySet https://bugs.webkit.org/show_bug.cgi?id=78406
    Reviewed by Andreas Kling.
            
    Most StylePropertySet instances never have CSSOM wrappers so having a pointer to one in
    each and and every object makes no sense.
    
    Move the PropertySetCSSStyleDeclaration instances to a global HashMap. This shrinks
    StylePropertySet by a pointer.
            
    Added COMPILE_ASSERT for StylePropertySet size.
    
    * css/StylePropertySet.cpp:
    (WebCore):
    (WebCore::StylePropertySet::StylePropertySet):
    (WebCore::StylePropertySet::~StylePropertySet):
    (WebCore::StylePropertySet::ensureCSSStyleDeclaration):
    * css/StylePropertySet.h:
    (StylePropertySet):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
@@ -46,6 +46,13 @@ using namespace std;
 
 namespace WebCore {
 
+typedef HashMap<const StylePropertySet*, OwnPtr<PropertySetCSSStyleDeclaration> > PropertySetCSSOMWrapperMap;
+static PropertySetCSSOMWrapperMap& propertySetCSSOMWrapperMap()
+{
+    DEFINE_STATIC_LOCAL(PropertySetCSSOMWrapperMap, propertySetCSSOMWrapperMapInstance, ());
+    return propertySetCSSOMWrapperMapInstance;
+}
+
 class PropertySetCSSStyleDeclaration : public CSSStyleDeclaration {
 public:
     PropertySetCSSStyleDeclaration(StylePropertySet* propertySet) : m_propertySet(propertySet) { }
@@ -170,6 +177,7 @@ StylePropertySet::StylePropertySet()
     : m_strictParsing(false)
     , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
+    , m_hasCSSOMWrapper(false)
     , m_parent(static_cast<CSSRule*>(0))
 {
 }
@@ -178,6 +186,7 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule)
     : m_strictParsing(!parentRule || parentRule->useStrictParsing())
     , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
+    , m_hasCSSOMWrapper(false)
     , m_parent(parentRule)
 {
 }
@@ -187,6 +196,7 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule, const Vector<CSSProperty
     , m_strictParsing(!parentRule || parentRule->useStrictParsing())
     , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
+    , m_hasCSSOMWrapper(false)
     , m_parent(parentRule)
 {
     m_properties.shrinkToFit();
@@ -196,6 +206,7 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule, const CSSProperty* const
     : m_strictParsing(!parentRule || parentRule->useStrictParsing())
     , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
+    , m_hasCSSOMWrapper(false)
     , m_parent(parentRule)
 {
     m_properties.reserveInitialCapacity(numProperties);
@@ -221,13 +232,16 @@ StylePropertySet::StylePropertySet(StyledElement* parentElement, bool isInlineSt
     : m_strictParsing(false)
     , m_parentIsElement(true)
     , m_isInlineStyleDeclaration(isInlineStyle)
+    , m_hasCSSOMWrapper(false)
     , m_parent(parentElement)
 {
 }
 
 StylePropertySet::~StylePropertySet()
 {
-    
+    ASSERT(!m_hasCSSOMWrapper || propertySetCSSOMWrapperMap().contains(this));
+    if (m_hasCSSOMWrapper)
+        propertySetCSSOMWrapperMap().remove(this);
 }
 
 CSSStyleSheet* StylePropertySet::contextStyleSheet() const
@@ -1117,9 +1131,12 @@ PassRefPtr<StylePropertySet> StylePropertySet::copyPropertiesInSet(const int* se
 
 CSSStyleDeclaration* StylePropertySet::ensureCSSStyleDeclaration() const
 {
-    if (!m_cssStyleDeclaration)
-        m_cssStyleDeclaration = adoptPtr(new PropertySetCSSStyleDeclaration(const_cast<StylePropertySet*>(this)));
-    return m_cssStyleDeclaration.get();
+    if (m_hasCSSOMWrapper)
+        return propertySetCSSOMWrapperMap().get(this);
+    m_hasCSSOMWrapper = true;
+    PropertySetCSSStyleDeclaration* cssomWrapper = new PropertySetCSSStyleDeclaration(const_cast<StylePropertySet*>(this));
+    propertySetCSSOMWrapperMap().add(this, adoptPtr(cssomWrapper));
+    return cssomWrapper;
 }
 
 unsigned PropertySetCSSStyleDeclaration::length() const
@@ -1248,5 +1265,12 @@ bool PropertySetCSSStyleDeclaration::cssPropertyMatches(const CSSProperty* prope
 {
     return m_propertySet->propertyMatches(property);
 }
+
+class SameSizeAsStylePropertySet : public RefCounted<SameSizeAsStylePropertySet> {
+    Vector<CSSProperty, 4> properties;
+    unsigned bitfield;
+    void* parent;
+};
+COMPILE_ASSERT(sizeof(StylePropertySet) == sizeof(SameSizeAsStylePropertySet), style_property_set_should_stay_small);
 
 } // namespace WebCore
