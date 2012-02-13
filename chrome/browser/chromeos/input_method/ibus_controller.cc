9de4d80d1d24a346ceb85ae3359893609aae266f
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-13 17:06:40 +0000

    Remove ibus-engine-xkb-layouts from Chrome OS [Chrome part].
    Since Aura version of Chrome doesn't send a key event to the dummy engine (r119012), now we can remove the dummy engine completely. This CL modifies Chrome so it does not preload an IBus engine whose ID starts with "xkb:".
    
    BUG=chromium-os:26167
    TEST=Enable "xkb:us::eng" and "mozc", type something, switch the current IME from xkb:us::eng to mozc via the IME menu, type something, switch back to xkb:us::eng, type something. Open a terminal and verify ibus-engine-xkb-layouts is not running.
    
    
    Review URL: http://codereview.chromium.org/9350032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121703 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/ibus_controller.cc
+++ chrome/browser/chromeos/input_method/ibus_controller.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/ibus_input_methods.h"
 #include "chrome/browser/chromeos/input_method/input_method_engine.h"
 #include "chrome/browser/chromeos/input_method/input_method_manager.h"
+#include "chrome/browser/chromeos/input_method/input_method_util.h"
 
 namespace chromeos {
 namespace input_method {
@@ -631,6 +632,10 @@ class IBusControllerImpl : public IBusController {
 
   // IBusController override.
   virtual bool ChangeInputMethod(const std::string& name) {
+#if defined(USE_AURA)
+    DCHECK(!InputMethodUtil::IsKeyboardLayout(name));
+#endif
+
     if (!IBusConnectionsAreAlive()) {
       LOG(ERROR) << "ChangeInputMethod: IBus connection is not alive";
       return false;
@@ -659,6 +664,14 @@ class IBusControllerImpl : public IBusController {
                                      NULL,  // cancellable
                                      NULL,  // callback
                                      NULL);  // user_data
+
+#if defined(USE_AURA)
+    // Since we don't have an IME for XKB layouts, when a global engine is
+    // switched from A (IME) to B (XKB) then back to A, "global-engine-changed"
+    // signal will not be emitted (note that on ibus-daemon side, A is always
+    // in use). To update the UI correctly, call the function manually.
+    IBusBusGlobalEngineChanged(ibus_, name.c_str());
+#endif
     return true;
   }
 
@@ -700,6 +713,7 @@ class IBusControllerImpl : public IBusController {
       case ImeConfigValue::kValueTypeStringList:
         GVariantBuilder variant_builder;
         g_variant_builder_init(&variant_builder, G_VARIANT_TYPE("as"));
+        DCHECK(!string_list.empty());
         const size_t size = string_list.size();  // don't use string_list_value.
         for (size_t i = 0; i < size; ++i) {
           g_variant_builder_add(&variant_builder, "s", string_list[i].c_str());
@@ -1193,7 +1207,13 @@ class IBusControllerImpl : public IBusController {
     for (size_t i = 0; i < requested_input_methods.size(); ++i) {
       const std::string& input_method = requested_input_methods[i];
       if (whitelist_.InputMethodIdIsWhitelisted(input_method.c_str())) {
+#if defined(USE_AURA)
+        if (!InputMethodUtil::IsKeyboardLayout(input_method)) {
+          out_filtered_input_methods->push_back(input_method);
+        }
+#else
         out_filtered_input_methods->push_back(input_method);
+#endif
       } else {
         LOG(ERROR) << "Unsupported input method: " << input_method;
       }
