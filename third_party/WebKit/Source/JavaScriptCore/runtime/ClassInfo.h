9f2da5d91b742de27aa8638e941d425aa99a6ded
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-13 09:28:44 +0000

    https://bugs.webkit.org/show_bug.cgi?id=78434 Unreviewed - temporarily reverting r107498 will I fix a couple of testcases.
    Source/JavaScriptCore: 
    
    * parser/Parser.cpp:
    (JSC::::parseFunctionInfo):
    * runtime/ClassInfo.h:
    (MethodTable):
    (JSC):
    * runtime/JSCell.cpp:
    (JSC):
    * runtime/JSCell.h:
    (JSCell):
    * runtime/JSGlobalObject.cpp:
    (JSC::JSGlobalObject::reset):
    * runtime/JSGlobalObjectFunctions.cpp:
    (JSC):
    * runtime/JSGlobalObjectFunctions.h:
    (JSC):
    * runtime/JSObject.cpp:
    (JSC::JSObject::put):
    (JSC):
    (JSC::JSObject::putDirectAccessor):
    (JSC::JSObject::defineOwnProperty):
    * runtime/JSObject.h:
    (JSC::JSObject::inlineGetOwnPropertySlot):
    (JSC::JSValue::get):
    * runtime/JSString.cpp:
    (JSC::JSString::getOwnPropertySlot):
    * runtime/JSValue.h:
    (JSValue):
    * runtime/ObjectConstructor.cpp:
    (JSC::objectConstructorGetPrototypeOf):
    * runtime/Structure.cpp:
    (JSC::Structure::Structure):
    * runtime/Structure.h:
    (JSC::Structure::setHasGetterSetterProperties):
    (Structure):
    
    Source/WebCore: 
    
    * bindings/js/JSDOMWindowBase.cpp:
    (WebCore):
    * bindings/js/JSDOMWindowBase.h:
    (JSDOMWindowBase):
    
    LayoutTests: 
    
    * fast/js/Object-getOwnPropertyNames-expected.txt:
    * fast/js/cyclic-prototypes-expected.txt:
    * fast/js/parser-syntax-check-expected.txt:
    * fast/js/preventExtensions-expected.txt:
    * fast/js/prototypes-expected.txt:
    * fast/js/script-tests/Object-getOwnPropertyNames.js:
    * fast/js/script-tests/cyclic-prototypes.js:
    * fast/js/script-tests/parser-syntax-check.js:
    * fast/js/script-tests/preventExtensions.js:
    * fast/js/script-tests/prototypes.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/ClassInfo.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/ClassInfo.h
@@ -90,9 +90,6 @@ namespace JSC {
 
         typedef bool (*GetOwnPropertyDescriptorFunctionPtr)(JSObject*, ExecState*, const Identifier&, PropertyDescriptor&);
         GetOwnPropertyDescriptorFunctionPtr getOwnPropertyDescriptor;
-
-        typedef bool (*AllowsAccessFromFunctionPtr)(JSObject*, ExecState*);
-        AllowsAccessFromFunctionPtr allowsAccessFrom;
     };
 
 #define CREATE_MEMBER_CHECKER(member) \
@@ -134,7 +131,6 @@ struct MemberCheck##member { \
         &ClassName::putDirectVirtual, \
         &ClassName::defineOwnProperty, \
         &ClassName::getOwnPropertyDescriptor, \
-        &ClassName::allowsAccessFrom, \
     }, \
     sizeof(ClassName), \
     ClassName::TypedArrayStorageType
