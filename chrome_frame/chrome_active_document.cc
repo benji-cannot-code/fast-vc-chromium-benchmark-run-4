7b47087762e551a485a3576a71f32cc5858760d2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-13 22:10:07 +0000

    Enforce that chrome_frame only uses content through its API.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9382039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_active_document.cc
+++ chrome_frame/chrome_active_document.cc
@@ -35,8 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome_frame/buggy_bho_handling.h"
 #include "chrome_frame/crash_reporting/crash_metrics.h"
 #include "chrome_frame/utils.h"
-#include "content/browser/tab_contents/tab_contents.h"
 #include "content/public/browser/invalidate_type.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/page_zoom.h"
 #include "grit/generated_resources.h"
 
