631c85e50ec832374249fb5b2f525a55171eb25d
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-13 23:37:55 +0000

    nesting horizontal flexboxes is broken https://bugs.webkit.org/show_bug.cgi?id=76867
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    This is copied from RenderDeprecatedFlexibleBox and updated
    for RenderFlexibleBox and to handle vertical writing mode.
    
    Tests: css3/flexbox/preferred-widths-orthogonal.html
           css3/flexbox/preferred-widths.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::computePreferredLogicalWidths):
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::marginWidthForChild):
    (WebCore):
    (WebCore::RenderFlexibleBox::computePreferredLogicalWidths):
    * rendering/RenderFlexibleBox.h:
    (RenderFlexibleBox):
    
    LayoutTests:
    
    * css3/flexbox/floated-flexbox-expected.txt:
    * css3/flexbox/line-wrapping.html:
    The old results here were wrong because they wrapped the contents of each flex item.
    
    * css3/flexbox/preferred-widths-expected.txt: Added.
    * css3/flexbox/preferred-widths-orthogonal-expected.txt: Added.
    * css3/flexbox/preferred-widths-orthogonal.html: Added.
    * css3/flexbox/preferred-widths.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -5024,6 +5024,8 @@ void RenderBlock::computePreferredLogicalWidths()
         }
 
         int scrollbarWidth = 0;
+        // FIXME: This should only be done for horizontal writing mode.
+        // For vertical writing mode, this should check overflowX and use the horizontalScrollbarHeight.
         if (hasOverflowClip() && styleToUse->overflowY() == OSCROLL) {
             layer()->setHasVerticalScrollbar(true);
             scrollbarWidth = verticalScrollbarWidth();
