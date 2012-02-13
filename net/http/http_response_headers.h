0fbe85c9d05983520c721759ef6da6d0ed3f7395
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-13 22:41:27 +0000

    Do not cache Strict-Transport-Security and Public-Key-Pins headers.
    This stops them from being honored (since they are absent) when loading
    pages from cache. These headers should only take effect on live, error-free
    HTTPS connections.
    
    BUG=110817
    TEST=net_unittests, HTTPSRequestTest.HTTPSErrorsNoClobberTSSTest
    
    Review URL: http://codereview.chromium.org/9233026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121747 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_headers.h
+++ net/http/http_response_headers.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,6 +38,7 @@ class NET_EXPORT HttpResponseHeaders
   static const PersistOptions PERSIST_SANS_HOP_BY_HOP = 1 << 2;
   static const PersistOptions PERSIST_SANS_NON_CACHEABLE = 1 << 3;
   static const PersistOptions PERSIST_SANS_RANGES = 1 << 4;
+  static const PersistOptions PERSIST_SANS_SECURITY_STATE = 1 << 5;
 
   // Parses the given raw_headers.  raw_headers should be formatted thus:
   // includes the http status response line, each line is \0-terminated, and
@@ -335,6 +336,9 @@ class NET_EXPORT HttpResponseHeaders
 
   // Adds the set of content range response headers.
   static void AddHopContentRangeHeaders(HeaderSet* header_names);
+
+  // Adds the set of transport security state headers.
+  static void AddSecurityStateHeaders(HeaderSet* header_names);
 
   // We keep a list of ParsedHeader objects.  These tell us where to locate the
   // header-value pairs within raw_headers_.
