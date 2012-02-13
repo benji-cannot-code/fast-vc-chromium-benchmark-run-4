1bed3e8f70899227a25795c6f54c2a14791453b6
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-13 22:55:46 +0000

    Unreviewed, rolling out r107582. http://trac.webkit.org/changeset/107582 https://bugs.webkit.org/show_bug.cgi?id=78349
    Broke three inspector interactive_ui_tests
    
    Source/WebCore:
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/DebuggerPresentationModel.js:
    (WebInspector.DebuggerPresentationModel.prototype.uiSourceCodes):
    * inspector/front-end/ScriptsNavigator.js:
    (WebInspector.ScriptsNavigator.prototype._removeUISourceCode.get while):
    (WebInspector.ScriptsNavigator.prototype._removeUISourceCode):
    (WebInspector.ScriptsNavigator.prototype._showScriptFoldersSettingChanged):
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype._uiSourceCodeAdded):
    (WebInspector.ScriptsPanel.prototype._updateExecutionLine):
    * inspector/front-end/Settings.js:
    (WebInspector.ExperimentsSettings):
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.SettingsScreen):
    
    LayoutTests:
    
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    * inspector/debugger/scripts-combobox-file-selector-history-expected.txt: Removed.
    * inspector/debugger/scripts-combobox-file-selector-history.html: Removed.
    * inspector/debugger/scripts-file-selector-expected.txt: Removed.
    * inspector/debugger/scripts-file-selector.html: Removed.
    * inspector/debugger/scripts-panel-expected.txt:
    * inspector/debugger/scripts-panel.html:
    * inspector/debugger/scripts-sorting-expected.txt:
    * inspector/debugger/scripts-sorting.html:
    * inspector/debugger/source-frame-count.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsNavigator.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsNavigator.js
@@ -218,7 +218,6 @@ WebInspector.ScriptsNavigator.prototype = {
             }
             treeElement = parent;
         }
-        this._scriptTreeElementsByUISourceCode.remove(uiSourceCode);
     },
     
     _showScriptFoldersSettingChanged: function()
@@ -229,8 +228,7 @@ WebInspector.ScriptsNavigator.prototype = {
         for (var i = 0; i < uiSourceCodes.length; ++i)
             this.addUISourceCode(uiSourceCodes[i]);
         
-        if (this._lastSelectedUISourceCode)
-            this.revealUISourceCode(this._lastSelectedUISourceCode);
+        this.revealUISourceCode(this._lastSelectedUISourceCode);
     },
     
     reset: function()
--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -61,8 +61,7 @@ WebInspector.ScriptsPanel = function(presentationModel)
     this.debugSidebarResizeWidgetElement.id = "scripts-debug-sidebar-resizer-widget";
     this.splitView.installResizer(this.debugSidebarResizeWidgetElement);
 
-    WebInspector.settings.useScriptsNavigator = WebInspector.settings.createSetting("useScriptsNavigator", true);
-    if (WebInspector.settings.useScriptsNavigator.get()) {
+    if (WebInspector.experimentsSettings.useScriptsNavigator.isEnabled()) {
         const initialNavigatorWidth = 225;
         const minimalViewsContainerWidthPercent = 50;
         this.editorView = new WebInspector.SplitView(WebInspector.SplitView.SidebarPosition.Left, "scriptsPanelNavigatorSidebarWidth", initialNavigatorWidth);
@@ -260,15 +259,6 @@ WebInspector.ScriptsPanel.prototype = {
             // Anonymous sources are shown only when stepping.
             return;
         }
-
-        this._addUISourceCode(uiSourceCode);
-    },
-
-    /**
-     * @param {WebInspector.UISourceCode} uiSourceCode
-     */
-    _addUISourceCode: function(uiSourceCode)
-    {
         this._fileSelector.addUISourceCode(uiSourceCode);
         this._editorContainer.uiSourceCodeAdded(uiSourceCode);
     },
@@ -615,7 +605,7 @@ WebInspector.ScriptsPanel.prototype = {
             return;
 
         // Anonymous scripts are not added to files select by default.
-        this._addUISourceCode(uiLocation.uiSourceCode);
+        this._fileSelector.addUISourceCode(uiLocation.uiSourceCode);
 
         var sourceFrame = this._showFile(uiLocation.uiSourceCode);
         sourceFrame.setExecutionLine(uiLocation.lineNumber);
