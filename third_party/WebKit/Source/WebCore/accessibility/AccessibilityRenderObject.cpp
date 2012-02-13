7196d1b9f78535180707d29ce60395efb910a4c1
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-13 19:23:52 +0000

    AX: the web area should report that focus can be set to itself https://bugs.webkit.org/show_bug.cgi?id=78272
    Reviewed by Beth Dakin.
    
    Source/WebCore: 
    
    Test: platform/mac/accessibility/webarea-can-set-focus.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::canSetFocusAttribute):
    * accessibility/AccessibilityTable.cpp:
    (WebCore::AccessibilityTable::isDataTable):
       Fixed erroneous comment.
    
    LayoutTests: 
    
    * platform/mac/accessibility/webarea-can-set-focus-expected.txt: Added.
    * platform/mac/accessibility/webarea-can-set-focus.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -3349,6 +3349,9 @@ bool AccessibilityRenderObject::canSetFocusAttribute() const
 {
     Node* node = this->node();
 
+    if (isWebArea())
+        return true;
+    
     // NOTE: It would be more accurate to ask the document whether setFocusedNode() would
     // do anything.  For example, setFocusedNode() will do nothing if the current focused
     // node will not relinquish the focus.
