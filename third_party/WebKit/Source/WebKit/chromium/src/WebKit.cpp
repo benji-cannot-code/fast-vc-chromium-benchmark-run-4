cbc30359f1ba53b95fb3b1b3016644532f951fb5
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-13 23:10:37 +0000

    [chromium] Let WebKit::initialize call InitWebCoreSystemInterface on mac. https://bugs.webkit.org/show_bug.cgi?id=78246
    This allows removing a bunch of code in chromium land (https://chromiumcodereview.appspot.com/9374020/),
    and helps with the component build on mac (InitWebCoreSystemInterface is not an
    exported function, and it shouldn't be).
    
    Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm early-returns when it's
    called twice, so it's safe to land this change, and remove the calls in client
    code in a later CL.
    
    Reviewed by Darin Fisher.
    
    * src/WebKit.cpp:
    (WebKit::initializeWithoutV8):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107618 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkerContextExecutionProxy.h"
 #include "v8.h"
 
+#if OS(DARWIN)
+#include "WebSystemInterface.h"
+#endif
+
 #include <wtf/Assertions.h>
 #include <wtf/MainThread.h>
 #include <wtf/Threading.h>
@@ -107,6 +111,10 @@ void initializeWithoutV8(WebKitPlatformSupport* webKitPlatformSupport)
 {
     ASSERT(!s_webKitInitialized);
     s_webKitInitialized = true;
+
+#if OS(DARWIN)
+    InitWebCoreSystemInterface();
+#endif
 
     ASSERT(webKitPlatformSupport);
     ASSERT(!s_webKitPlatformSupport);
