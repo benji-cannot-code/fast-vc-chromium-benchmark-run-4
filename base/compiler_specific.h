5f16bdad0623fafd32472d66f7e75f0e03ecaffb
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 01:12:12 +0000

    Add a stackframe named 'CrashIntentionally' to crashes from about:crash.
    This should make it easier to categorize on the crash server.
    
    BUG=113231
    
    Review URL: http://codereview.chromium.org/9380026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121798 0039d316-1c4b-4281-b951-d872f2087c98

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -91,9 +91,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   int x ALLOW_UNUSED = ...;
 #if defined(COMPILER_GCC)
 #define ALLOW_UNUSED __attribute__((unused))
-#define NOINLINE __attribute__((noinline))
 #else
 #define ALLOW_UNUSED
+#endif
+
+// Annotate a function indicating it should not be inlined.
+// Use like:
+//   NOINLINE void DoStuff() { ... }
+#if defined(COMPILER_GCC)
+#define NOINLINE __attribute__((noinline))
+#elif defined(COMPILER_MSVC)
+#define NOINLINE __declspec(noinline)
+#else
 #define NOINLINE
 #endif
 
