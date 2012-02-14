e8fbd07a8fba4a55110456b5d1650db82690ea70
kubo@profusion.mobi <kubo@profusion.mobi@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-14 23:26:09 +0000

    [CMake] Merge WebKitEfl.cmake into FindEFL.cmake. https://bugs.webkit.org/show_bug.cgi?id=78566
    Reviewed by Daniel Bates.
    
    .:
    
    * Source/cmake/FindEFL.cmake: Move the code to find edje_cc here from
    WebKitEfl.cmake.
    * Source/cmake/WebKitEfl.cmake: Removed.
    
    Source/WebKit/efl:
    
    * CMakeListsEfl.txt: Do not include WebKitEfl.cmake.
    
    Source/WebKit2:
    
    * PlatformEfl.cmake: Do not include WebKitEfl.cmake.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmake/WebKitEfl.cmake
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# EFL port specific macros and definitions
-
-FIND_PROGRAM(EDJE_CC_EXECUTABLE edje_cc)
-IF (NOT EDJE_CC_EXECUTABLE)
-  MESSAGE(FATAL_ERROR "Missing edje_cc")
-ENDIF ()
