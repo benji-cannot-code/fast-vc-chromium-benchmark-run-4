5b04ecfd24c4f6a8fbb381b208c210913870ae44
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 00:14:50 +0000

    Fix how we warm up the device enumerator for Flash
    Change the warmup flags so we don't cause DCOM initialization.
    
    BUG=113891
    
    Review URL: http://codereview.chromium.org/9350048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121772 0039d316-1c4b-4281-b951-d872f2087c98

--- content/plugin/plugin_main.cc
+++ content/plugin/plugin_main.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -142,7 +142,9 @@ int PluginMain(const content::MainFunctionParams& parameters) {
 
         // Warm up the device enumerator for webcam and microphone.
         base::win::ScopedComPtr<ICreateDevEnum> device_enumerator;
-        device_enumerator.CreateInstance(CLSID_SystemDeviceEnum);
+        device_enumerator.CreateInstance(CLSID_SystemDeviceEnum, NULL,
+                                         CLSCTX_INPROC_SERVER |
+                                         CLSCTX_INPROC_HANDLER);
 
         DelayedLowerToken(target_services);
       } else {
