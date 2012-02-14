baf2f3a9085a4f3b6e58f412f2416e6eb03089bb
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 19:51:42 +0000

    Instrument how the download file chooser is used.
    This lets us see how frequently users override the default filename,.
    
    BUG=114025
    TEST=Look at about:histograms after doing a few actions, make sure Download.FilePickerResult is sane.
    
    
    Review URL: http://codereview.chromium.org/9389012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121916 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_file_picker.cc
+++ chrome/browser/download/download_file_picker.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/download/download_file_picker.h"
 
+#include "base/metrics/histogram.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -15,12 +16,51 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::DownloadManager;
 using content::WebContents;
 
+namespace {
+
+enum FilePickerResult {
+  FILE_PICKER_SAME,
+  FILE_PICKER_DIFFERENT_DIR,
+  FILE_PICKER_DIFFERENT_NAME,
+  FILE_PICKER_CANCEL,
+  FILE_PICKER_MAX,
+};
+
+// Record how the File Chooser was used during a download. Only record this
+// for profiles that do not always prompt for save locations on downloads.
+void RecordFilePickerResult(DownloadManager* manager,
+                            FilePickerResult result) {
+  if (!manager)
+    return;
+  const DownloadPrefs* prefs = DownloadPrefs::FromDownloadManager(manager);
+  if (!prefs)
+    return;
+  if (prefs->PromptForDownload())
+    return;
+  UMA_HISTOGRAM_ENUMERATION("Download.FilePickerResult",
+                            result,
+                            FILE_PICKER_MAX);
+}
+
+FilePickerResult ComparePaths(const FilePath& suggested_path,
+                              const FilePath& actual_path) {
+  if (suggested_path == actual_path)
+    return FILE_PICKER_SAME;
+  if (suggested_path.DirName() != actual_path.DirName())
+    return FILE_PICKER_DIFFERENT_DIR;
+  return FILE_PICKER_DIFFERENT_NAME;
+}
+
+}  // namespace
+
 DownloadFilePicker::DownloadFilePicker(
     DownloadManager* download_manager,
     WebContents* web_contents,
     const FilePath& suggested_path,
     void* params)
-    : download_manager_(download_manager) {
+    : download_manager_(download_manager),
+      suggested_path_(suggested_path) {
+  DCHECK(download_manager_);
   select_file_dialog_ = SelectFileDialog::Create(this);
   SelectFileDialog::FileTypeInfo file_type_info;
   FilePath::StringType extension = suggested_path.Extension();
@@ -54,12 +94,15 @@ void DownloadFilePicker::ManagerGoingDown(DownloadManager* manager) {
 void DownloadFilePicker::FileSelected(const FilePath& path,
                                       int index,
                                       void* params) {
+  FilePickerResult result = ComparePaths(suggested_path_, path);
+  RecordFilePickerResult(download_manager_, result);
   if (download_manager_)
     download_manager_->FileSelected(path, params);
   delete this;
 }
 
 void DownloadFilePicker::FileSelectionCanceled(void* params) {
+  RecordFilePickerResult(download_manager_, FILE_PICKER_CANCEL);
   if (download_manager_)
     download_manager_->FileSelectionCanceled(params);
   delete this;
