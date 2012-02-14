69c99aefb5a0c350dadf150a371f5948b732d92c
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 18:56:34 +0000

    LocationBarViewGtk already has a pointer to Browser, so no need to get it from the controller.
    This is a follow up comment to r121736.
    
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9348091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121907 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/location_bar_view_gtk.cc
+++ chrome/browser/ui/gtk/location_bar_view_gtk.cc
@@ -71,7 +71,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "webkit/glue/window_open_disposition.h"
 
-using content::NavigationController;
 using content::NavigationEntry;
 using content::OpenURLParams;
 using content::WebContents;
@@ -1043,14 +1042,12 @@ gboolean LocationBarViewGtk::OnIconReleased(GtkWidget* sender,
     if (event->x == 0 && event->y == 0)
       return FALSE;
 
-    const NavigationController& controller = tab->GetController();
-    NavigationEntry* nav_entry = controller.GetActiveEntry();
+    NavigationEntry* nav_entry = tab->GetController().GetActiveEntry();
     if (!nav_entry) {
       NOTREACHED();
       return FALSE;
     }
-    Browser* browser = Browser::GetBrowserForController(&controller, NULL);
-    browser->ShowPageInfo(nav_entry->GetURL(), nav_entry->GetSSL(), true);
+    browser_->ShowPageInfo(nav_entry->GetURL(), nav_entry->GetSSL(), true);
     return TRUE;
   } else if (event->button == 2) {
     // When the user middle clicks on the location icon, try to open the
