9b0aa610e241dd9c83349c2bd53673b0aebc9c78
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 18:05:39 +0000

    PPAPI: Add unlocking for PPP calls and callbacks. Add more locking.
    With this patch, ppapi_tests pass locally when building with enable_pepper_threading=1. (They didn't before).
    
    TODO: Test more calls off the main thread, make sync completion callbacks work.
    
    BUG=92909
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9391006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121901 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppp_graphics_3d_proxy.cc
+++ ppapi/proxy/ppp_graphics_3d_proxy.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/host_dispatcher.h"
 #include "ppapi/proxy/plugin_dispatcher.h"
 #include "ppapi/proxy/ppapi_messages.h"
+#include "ppapi/shared_impl/proxy_lock.h"
 
 namespace ppapi {
 namespace proxy {
@@ -66,7 +67,7 @@ bool PPP_Graphics3D_Proxy::OnMessageReceived(const IPC::Message& msg) {
 
 void PPP_Graphics3D_Proxy::OnMsgContextLost(PP_Instance instance) {
   if (ppp_graphics_3d_impl_)
-    ppp_graphics_3d_impl_->Graphics3DContextLost(instance);
+    CallWhileUnlocked(ppp_graphics_3d_impl_->Graphics3DContextLost, instance);
 }
 
 }  // namespace proxy
