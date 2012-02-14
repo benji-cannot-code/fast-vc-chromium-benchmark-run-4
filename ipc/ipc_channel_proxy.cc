155823d7ba332557b7565476270e1f594d8227c8
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 16:19:57 +0000

    Revert 121889 - Add simple trace logging of received IPC messages
    Somehow triggered a crash in the Windows linker
    
    Adds an entry to chrome://tracing for processing IPC messages that includes the IPC message type ID (which can be matched to a name with the ipclist tool), and in DEBUG builds (or other builds where IPC_MESSAGE_LOG_ENABLED has been set), the message name.
    
    Also adds shell_messages.h to the message generator headers (per the rules in ipc_message_macros.h) to fix broken IPC message logging.
    
    BUG=79942
    TEST=
    
    Review URL: http://codereview.chromium.org/9389020
    
    TBR=rbyers@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9365058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121890 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_proxy.cc
+++ ipc/ipc_channel_proxy.cc
@@ -1,11 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
-#include "base/debug/trace_event.h"
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
@@ -240,23 +239,13 @@ void ChannelProxy::Context::AddFilter(MessageFilter* filter) {
 
 // Called on the listener's thread
 void ChannelProxy::Context::OnDispatchMessage(const Message& message) {
-#ifdef IPC_MESSAGE_LOG_ENABLED
-  Logging* logger = Logging::GetInstance();
-  std::string name;
-  logger->GetMessageText(message.type(), &name, &message, NULL);
-  TRACE_EVENT1("task", "ChannelProxy::Context::OnDispatchMessage",
-               "name", name);
-#else
-  TRACE_EVENT1("task", "ChannelProxy::Context::OnDispatchMessage",
-               "type", message.type());
-#endif
-
   if (!listener_)
     return;
 
   OnDispatchConnected();
 
 #ifdef IPC_MESSAGE_LOG_ENABLED
+  Logging* logger = Logging::GetInstance();
   if (message.type() == IPC_LOGGING_ID) {
     logger->OnReceivedLoggingMessage(message);
     return;
