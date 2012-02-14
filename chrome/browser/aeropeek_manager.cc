c18efa216988511095cd66a5072d3764a5225afa
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 03:37:14 +0000

    Make scoped dc objects smarter
    So we don't destroy the dc with gdi objects selected.
    Also remove implicit conversion to HDC.
    
    BUG=110113,113683
    TEST=chrome runs, base unittests green.
    Review URL: https://chromiumcodereview.appspot.com/9212020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/aeropeek_manager.cc
+++ chrome/browser/aeropeek_manager.cc
@@ -212,7 +212,7 @@ void SendThumbnailCallback(
   // We can delete this DIB after sending it to Windows since Windows creates
   // a copy of the DIB and use it.
   base::win::ScopedCreateDC hdc(CreateCompatibleDC(NULL));
-  if (!hdc.Get()) {
+  if (!hdc.get()) {
     LOG(ERROR) << "cannot create a memory DC: " << GetLastError();
     return;
   }
@@ -223,7 +223,7 @@ void SendThumbnailCallback(
 
   void* bitmap_data = NULL;
   base::win::ScopedBitmap bitmap(
-      CreateDIBSection(hdc, reinterpret_cast<BITMAPINFO*>(&header),
+      CreateDIBSection(hdc.get(), reinterpret_cast<BITMAPINFO*>(&header),
                        DIB_RGB_COLORS, &bitmap_data, NULL, 0));
 
   if (!bitmap.Get() || !bitmap_data) {
@@ -275,7 +275,7 @@ void SendLivePreviewCallback(
   // We don't need to paste this tab image onto the frame image since Windows
   // automatically pastes it for us.
   base::win::ScopedCreateDC hdc(CreateCompatibleDC(NULL));
-  if (!hdc.Get()) {
+  if (!hdc.get()) {
     LOG(ERROR) << "cannot create a memory DC: " << GetLastError();
     return;
   }
@@ -286,7 +286,7 @@ void SendLivePreviewCallback(
 
   void* bitmap_data = NULL;
   base::win::ScopedBitmap bitmap(
-      CreateDIBSection(hdc.Get(), reinterpret_cast<BITMAPINFO*>(&header),
+      CreateDIBSection(hdc.get(), reinterpret_cast<BITMAPINFO*>(&header),
                        DIB_RGB_COLORS, &bitmap_data, NULL, 0));
   if (!bitmap.Get() || !bitmap_data) {
     LOG(ERROR) << "cannot create a bitmap: " << GetLastError();
