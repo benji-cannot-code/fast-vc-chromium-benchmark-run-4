30ebd9fd626701b8a274bb1f95ef021cd648e7d9
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 01:08:54 +0000

    SafeBrowsing should ensure MAC is present in response if it was requested.
    BUG=112814
    TEST=SafeBrowsingProtocolParsingTest
    
    
    Review URL: http://codereview.chromium.org/9369021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/protocol_parser.cc
+++ chrome/browser/safe_browsing/protocol_parser.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -141,6 +141,12 @@ bool SafeBrowsingProtocolParser::ParseUpdate(
 
   // Populated below.
   std::string list_name;
+
+  // If we requested the MAC, the response must start with a MAC command.
+  // This test ensures it is present, the value will be verified in the
+  // switch statement below.
+  if (!key.empty() && (length < 1 || data[0] != 'm'))
+    return false;
 
   while (length > 0) {
     std::string cmd_line;
