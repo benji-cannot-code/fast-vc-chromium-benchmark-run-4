2632783c84e8a57dad3050ea9590be6f9a5c384b
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 03:23:38 +0000

    Preload setupapi.dll in GPU process.
    It can't be loaded on some systems once the sandbox is enabled.
    BUG=114011,112205
    
    Review URL: http://codereview.chromium.org/9389013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121837 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -83,6 +83,9 @@ int GpuMain(const content::MainFunctionParams& parameters) {
   base::win::ScopedCOMInitializer com_initializer;
 
 #if defined(OS_WIN)
+  // Preload this DLL because the sandbox prevents it from loading.
+  LoadLibrary(L"setupapi.dll");
+
   sandbox::TargetServices* target_services =
       parameters.sandbox_info->target_services;
   // Initialize H/W video decoding stuff which fails in the sandbox.
