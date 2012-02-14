371718da0dc33a2ef5e9ab2313cec8ce5b4229bb
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-14 12:55:56 +0000

    Web Inspector: Make ScriptsNavigator default file selector. https://bugs.webkit.org/show_bug.cgi?id=78349
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Moved ScriptsNavigator out of experiments.
    Introduced new setting "useScriptsNavigator" with true as default value.
    Updated scripts panel tests related to file selector.
    
    Tests: inspector/debugger/scripts-combobox-file-selector-history.html
           inspector/debugger/scripts-file-selector.html
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/DebuggerPresentationModel.js:
    (WebInspector.DebuggerPresentationModel.prototype.uiSourceCodes):
    * inspector/front-end/ScriptsNavigator.js:
    (WebInspector.ScriptsNavigator.prototype._removeUISourceCode):
    (WebInspector.ScriptsNavigator.prototype._showScriptFoldersSettingChanged):
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype._uiSourceCodeAdded):
    (WebInspector.ScriptsPanel.prototype._addUISourceCode):
    (WebInspector.ScriptsPanel.prototype._updateExecutionLine):
    * inspector/front-end/Settings.js:
    (WebInspector.ExperimentsSettings):
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.SettingsScreen):
    
    Source/WebKit/chromium:
    
    * src/js/Tests.js:
    (.TestSuite.prototype.testNoScriptDuplicatesOnPanelSwitch.checkNoDuplicates):
    (.TestSuite.prototype.uiSourceCodesToString_):
    (.TestSuite.prototype.nonAnonymousUISourceCodes_):
    (.TestSuite.prototype._scriptsAreParsed):
    (.TestSuite.prototype._waitUntilScriptsAreParsed.waitForAllScripts):
    (.TestSuite.prototype._waitUntilScriptsAreParsed):
    
    LayoutTests:
    
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    (initialize_DebuggerTest.):
    * inspector/debugger/scripts-combobox-file-selector-history-expected.txt: Added.
    * inspector/debugger/scripts-combobox-file-selector-history.html: Added.
    * inspector/debugger/scripts-file-selector-expected.txt: Copied from LayoutTests/inspector/debugger/scripts-panel-expected.txt.
    * inspector/debugger/scripts-file-selector.html: Added.
    * inspector/debugger/scripts-panel-expected.txt:
    * inspector/debugger/scripts-panel.html:
    * inspector/debugger/scripts-sorting-expected.txt:
    * inspector/debugger/scripts-sorting.html:
    * inspector/debugger/source-frame-count.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsNavigator.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsNavigator.js
@@ -218,6 +218,7 @@ WebInspector.ScriptsNavigator.prototype = {
             }
             treeElement = parent;
         }
+        this._scriptTreeElementsByUISourceCode.remove(uiSourceCode);
     },
     
     _showScriptFoldersSettingChanged: function()
@@ -228,7 +229,8 @@ WebInspector.ScriptsNavigator.prototype = {
         for (var i = 0; i < uiSourceCodes.length; ++i)
             this.addUISourceCode(uiSourceCodes[i]);
         
-        this.revealUISourceCode(this._lastSelectedUISourceCode);
+        if (this._lastSelectedUISourceCode)
+            this.revealUISourceCode(this._lastSelectedUISourceCode);
     },
     
     reset: function()
