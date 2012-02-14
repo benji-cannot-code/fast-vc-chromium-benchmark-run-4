84ff24490774bc97c6227448a0988f770f422af9
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 00:29:14 +0000

    Flakiness cleanup: disable flaky tests under base/
    See https://groups.google.com/a/chromium.org/group/chromium-dev/browse_thread/thread/fcec09fc659f39a6
    
    BUG=46246,85930,10611,86494,95058,61589,25038
    
    Review URL: https://chromiumcodereview.appspot.com/9386014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121782 0039d316-1c4b-4281-b951-d872f2087c98

--- base/timer_unittest.cc
+++ base/timer_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -321,7 +321,7 @@ TEST(TimerTest, DelayTimer_OneCall) {
 }
 
 // It's flaky on the buildbot, http://crbug.com/25038.
-TEST(TimerTest, FLAKY_DelayTimer_Reset) {
+TEST(TimerTest, DISABLED_DelayTimer_Reset) {
   RunTest_DelayTimer_Reset(MessageLoop::TYPE_DEFAULT);
   RunTest_DelayTimer_Reset(MessageLoop::TYPE_UI);
   RunTest_DelayTimer_Reset(MessageLoop::TYPE_IO);
