078ab08e6ed1e969ec9d1d10ee9dc6529f3ee5df
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 19:11:26 +0000

    Remove TODO in PPAPI input event interface files. This issue has been fixed. (https://bugs.webkit.org/show_bug.cgi?id=78014)
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9348095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121910 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_input_event.h
+++ ppapi/c/ppb_input_event.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_input_event.idl modified Mon Nov 14 10:36:01 2011. */
+/* From ppb_input_event.idl modified Tue Feb 14 10:47:45 2012. */
 
 #ifndef PPAPI_C_PPB_INPUT_EVENT_H_
 #define PPAPI_C_PPB_INPUT_EVENT_H_
@@ -502,10 +502,6 @@ struct PPB_MouseInputEvent_1_1 {
    *
    * @return The change in position of the mouse, relative to the previous
    * position.
-   *
-   * TODO(yzshen): This feature hasn't been fully supported yet. For now,
-   * movement information is provided only if the mouse is locked. If the mouse
-   * is not locked, the returned value is (0, 0).
    */
   struct PP_Point (*GetMovement)(PP_Resource mouse_event);
 };
--- ppapi/cpp/input_event.h
+++ ppapi/cpp/input_event.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -160,10 +160,6 @@ class MouseInputEvent : public InputEvent {
   ///
   /// @return The change in position of the mouse, relative to the previous
   /// position.
-  ///
-  /// TODO(yzshen): This feature hasn't been fully supported yet. For now,
-  /// movement information is provided only if the mouse is locked. If the mouse
-  /// is not locked, the returned value is (0, 0).
   Point GetMovement() const;
 };
 
