313cdfdf85d1acc27ed985aa04a37fa6ce29c9ee
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 17:59:37 +0000

    Flakiness cleanup: disable flaky tests under webkit/
    See https://groups.google.com/a/chromium.org/group/chromium-dev/browse_thread/thread/fcec09fc659f39a6
    
    BUG=51622
    
    Review URL: https://chromiumcodereview.appspot.com/9387011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121899 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/resource_fetcher_unittest.cc
+++ webkit/glue/resource_fetcher_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -101,7 +101,7 @@ FetcherDelegate* FetcherDelegate::instance_ = NULL;
 
 // Test a fetch from the test server.
 // Flaky, http://crbug.com/51622.
-TEST_F(ResourceFetcherTests, FLAKY_ResourceFetcherDownload) {
+TEST_F(ResourceFetcherTests, DISABLED_ResourceFetcherDownload) {
   ASSERT_TRUE(test_server_.Start());
 
   WebFrame* frame = test_shell_->webView()->mainFrame();
@@ -133,7 +133,7 @@ TEST_F(ResourceFetcherTests, FLAKY_ResourceFetcherDownload) {
 }
 
 // Flaky, http://crbug.com/51622.
-TEST_F(ResourceFetcherTests, FLAKY_ResourceFetcherDidFail) {
+TEST_F(ResourceFetcherTests, DISABLED_ResourceFetcherDidFail) {
   ASSERT_TRUE(test_server_.Start());
 
   WebFrame* frame = test_shell_->webView()->mainFrame();
