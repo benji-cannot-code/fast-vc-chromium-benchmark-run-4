90f5cb5831f7005768bd8f4866b6f59cf3d6c6e1
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 05:08:24 +0000

    Protector bubble is re-displayed when browser window focus is lost and returned.
    BUG=111174
    TEST=Manual: see bug description.
    
    
    Review URL: http://codereview.chromium.org/9379009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121850 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/protector/settings_change_global_error.h
+++ chrome/browser/protector/settings_change_global_error.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/protector/base_setting_change.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/global_error.h"
 
 class Browser;
@@ -23,7 +24,8 @@ class BaseSettingChange;
 class SettingsChangeGlobalErrorDelegate;
 
 // Global error about unwanted settings changes.
-class SettingsChangeGlobalError : public GlobalError {
+class SettingsChangeGlobalError : public GlobalError,
+                                  public BrowserList::Observer {
  public:
   // Creates new global error about setting changes |change| which must not be
   // deleted until |delegate->OnRemovedFromProfile| is called. Uses |delegate|
@@ -39,6 +41,7 @@ class SettingsChangeGlobalError : public GlobalError {
   // Removes global error from its profile.
   void RemoveFromProfile();
 
+ private:
   // GlobalError implementation.
   virtual bool HasBadge() OVERRIDE;
   virtual int GetBadgeResourceID() OVERRIDE;
@@ -57,13 +60,22 @@ class SettingsChangeGlobalError : public GlobalError {
   virtual void BubbleViewAcceptButtonPressed(Browser* browser) OVERRIDE;
   virtual void BubbleViewCancelButtonPressed(Browser* browser) OVERRIDE;
 
- private:
+  // BrowserList::Observer implementation.
+  virtual void OnBrowserAdded(const Browser* browser) OVERRIDE {}
+  virtual void OnBrowserRemoved(const Browser* browser) OVERRIDE {}
+  virtual void OnBrowserSetLastActive(const Browser* browser) OVERRIDE;
+
   // Helper called on the UI thread to add this global error to the default
   // profile (stored in |profile_|).
   void AddToProfile(Profile* profile);
 
-  // Displays global error bubble. Must be called on the UI thread.
+  // Displays the bubble in the last active tabbed browser. Must be called
+  // on the UI thread.
   void Show();
+
+  // Displays the bubble in |browser|'s window. Must be called
+  // on the UI thread.
+  void ShowInBrowser(Browser* browser);
 
   // Called when the wrench menu item has been displayed for enough time
   // without user interaction.
@@ -80,6 +92,9 @@ class SettingsChangeGlobalError : public GlobalError {
 
   // True if user has dismissed the bubble by clicking on one of the buttons.
   bool closed_by_button_;
+
+  // True if the bubble has to be shown on the next browser window activation.
+  bool show_on_browser_activation_;
 
   base::WeakPtrFactory<SettingsChangeGlobalError> weak_factory_;
 
