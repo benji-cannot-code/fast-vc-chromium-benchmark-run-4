4546fcf60e7b018afa8281bb70c9d76dae54b389
mflodman@chromium.org <mflodman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 15:07:22 +0000

    Changed VideoCaptureController to use correct session id when stopping the capture device.
    Review URL: http://codereview.chromium.org/9389006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121886 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/video_capture_controller.cc
+++ content/browser/renderer_host/media/video_capture_controller.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -191,12 +191,13 @@ void VideoCaptureController::StopCapture(
     client->event_handler->OnReadyToDelete(client->controller_id);
   }
 
+  int session_id = client->parameters.session_id;
   delete client;
   controller_clients_.remove(client);
 
   // No more clients. Stop device.
   if (controller_clients_.empty()) {
-    video_capture_manager_->Stop(current_params_.session_id,
+    video_capture_manager_->Stop(session_id,
         base::Bind(&VideoCaptureController::OnDeviceStopped, this));
     frame_info_available_ = false;
     state_ = video_capture::kStopping;
