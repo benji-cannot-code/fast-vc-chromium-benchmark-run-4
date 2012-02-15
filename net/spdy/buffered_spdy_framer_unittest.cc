738cfe58b5907d4c881ed52bdfc3b820abd03341
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 04:03:40 +0000

    SPDY - pass stream_id instread contol frame to OnControlFrameHeaderData. In OnControl method cache the control_frame which would be used in OnControlFrameHeaderData.
    TEST=network unit tests
    BUG=108609
    R=willchan
    
    Review URL: http://codereview.chromium.org/9242034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122025 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/buffered_spdy_framer_unittest.cc
+++ net/spdy/buffered_spdy_framer_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -81,14 +81,13 @@ class TestBufferedSpdyVisitor : public BufferedSpdyFramerVisitorInterface {
     }
   }
 
-  bool OnControlFrameHeaderData(const SpdyControlFrame* control_frame,
+  bool OnControlFrameHeaderData(SpdyStreamId stream_id,
                                 const char* header_data,
                                 size_t len) {
-    SpdyStreamId stream_id = SpdyFramer::GetControlFrameStreamId(control_frame);
     EXPECT_EQ(header_stream_id_, stream_id);
 
     bool result = buffered_spdy_framer_.OnControlFrameHeaderData(
-        control_frame, header_data, len);
+        stream_id, header_data, len);
     EXPECT_TRUE(result);
 
     ++control_frame_header_data_count_;
