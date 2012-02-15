99e8022f53481a12f88424d800942298834709f7
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 06:46:26 +0000

    Valgrind: Fix conditional jump in ProfileManager::AutoloadProfiles.
    BUG=114156
    TEST=none
    
    Review URL: http://codereview.chromium.org/9360036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_info_cache.cc
+++ chrome/browser/profiles/profile_info_cache.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -300,8 +300,10 @@ const gfx::Image& ProfileInfoCache::GetAvatarIconOfProfileAtIndex(
 bool ProfileInfoCache::GetBackgroundStatusOfProfileAtIndex(
     size_t index) const {
   bool background_app_status;
-  GetInfoForProfileAtIndex(index)->GetBoolean(kBackgroundAppsKey,
-                                              &background_app_status);
+  if (!GetInfoForProfileAtIndex(index)->GetBoolean(kBackgroundAppsKey,
+                                                   &background_app_status)) {
+    return false;
+  }
   return background_app_status;
 }
 
