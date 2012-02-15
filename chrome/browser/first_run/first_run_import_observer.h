7ed3fa13b3eecdd1197b80befb074563930872dd
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 03:59:49 +0000

    Progressively refactor FirstRun class to first_run namespace(part 7). This cl refactors the rest of FirstRun class to first_run namespace, majorly ProcessMasterPreferences and its related functions. It includes the change to the following functions: ProcessMasterPreferences SkipFirstRunUI SetImportPreferencesAndLaunchImport ShowPostInstallEULAIfNeeded DoDelayedInstallExtensionsIfNeeded
    BUG=108137
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/9339003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122023 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run_import_observer.h
+++ chrome/browser/first_run/first_run_import_observer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "chrome/browser/importer/importer_observer.h"
 
-// This class is used by FirstRun::ImportNow to get notified of the outcome of
+// This class is used by first_run::ImportNow to get notified of the outcome of
 // the import operation. It differs from ImportProcessRunner in that this
 // class executes in the context of the importing child process.
 // The values that it handles are meant to be used as the process exit code.
