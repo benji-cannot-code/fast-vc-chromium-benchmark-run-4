f81f1388fe0d3a27862289584de8258c01e42c9b
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-15 15:56:59 +0000

    Unreviewed Qt 4.x build fix.
    * WebCoreSupport/PageClientQt.cpp: Fix the QT_VERSION_CHECK pre-processor foo.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PageClientQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PageClientQt.cpp
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(WEBGL)
 #include <QGLWidget>
 
-#if QT_VERSION_CHECK(5, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 #include <QWindow>
 #endif
 
