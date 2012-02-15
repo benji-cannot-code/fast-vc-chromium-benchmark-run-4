232f63e7aa272ec168ea995f85491ea01db0cf77
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 18:20:28 +0000

    Change sync promo URL
    This CL changes the sync promo URL from chrome://syncpromo to chrome://signin
    
    BUG=113746
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9350071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo/sync_promo_ui.h
+++ chrome/browser/ui/webui/sync_promo/sync_promo_ui.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 class PrefService;
 
-// The Web UI handler for chrome://syncpromo.
+// The Web UI handler for chrome://signin.
 class SyncPromoUI : public content::WebUIController {
  public:
   enum Version {
