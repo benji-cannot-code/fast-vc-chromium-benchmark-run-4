fb1c3b465d2b577206f4d455ed8f6677a9afb2a8
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 01:44:56 +0000

    Remove TypedUrlModelAssociator association map.
    Removed the association map in TypedUrlModelAssociator because it has been a source
    of bugs and the information is redundant.
    
    TypedUrlModelAssociator no longer implements PerDataTypeAssociatorInterface, since
    many of the "standard" API implementations in that class are not needed (like
    Associate()) or result in inefficient code (such as code that looks up nodes by
    their client tag, returns the ID, then looks up the node again by the ID).
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9350069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/model_associator.h
+++ chrome/browser/sync/glue/model_associator.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -89,31 +89,6 @@ class PerDataTypeAssociatorInterface : public AssociatorInterface {
 
   // Remove the association that corresponds to the given sync id.
   virtual void Disassociate(int64 sync_id) = 0;
-};
-
-template <class Node, class IDType>
-class AbortablePerDataTypeAssociatorInterface
-    : public PerDataTypeAssociatorInterface<Node, IDType> {
- public:
-  AbortablePerDataTypeAssociatorInterface() : pending_abort_(false) {}
-
-  // Implementation of AssociatorInterface methods.
-  virtual void AbortAssociation() {
-    base::AutoLock lock(pending_abort_lock_);
-    pending_abort_ = true;
-  }
-
- protected:
-  // Overridable by tests.
-  virtual bool IsAbortPending() {
-    base::AutoLock lock(pending_abort_lock_);
-    return pending_abort_;
-  }
-
-  // Lock to ensure exclusive access to the pending_abort_ flag.
-  base::Lock pending_abort_lock_;
-  // Set to true if there's a pending abort.
-  bool pending_abort_;
 };
 
 }  // namespace browser_sync
