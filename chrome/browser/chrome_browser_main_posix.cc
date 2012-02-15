183e5d4286a67790816d88166bc356f7300b14ed
robert.nagy@gmail.com <robert.nagy@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 23:13:07 +0000

    use a correct value for kShutdownDetectorThreadStackSize
    Previously a hard coded value of 4096 was used as the stack size,
    which is wrong because POSIX mandates that pthread_attr_setstacksize()
    fails with EINVAL if the stacksize is less than PTHREAD_STACK_MIN.
    On Linux it is smaller than PTHREAD_STACK_MIN, then it is set to RLIMIT_STACK,
    which is huge compared to the expected 4096 bytes.
    By using PTHREAD_STACK_MIN it is ensured that the smallest and still
    correct stack size is used for the ShutdownDetectorThread.
    
    BUG=
    TEST=compile
    
    
    Review URL: http://codereview.chromium.org/9405005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_posix.cc
+++ chrome/browser/chrome_browser_main_posix.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <limits.h>
+#include <pthread.h>
 #include <signal.h>
 #include <sys/resource.h>
 #include <unistd.h>
@@ -232,7 +233,7 @@ void ChromeBrowserMainPartsPosix::PostMainMessageLoopStart() {
   } else {
     g_shutdown_pipe_read_fd = pipefd[0];
     g_shutdown_pipe_write_fd = pipefd[1];
-    const size_t kShutdownDetectorThreadStackSize = 4096;
+    const size_t kShutdownDetectorThreadStackSize = PTHREAD_STACK_MIN;
     // TODO(viettrungluu,willchan): crbug.com/29675 - This currently leaks, so
     // if you change this, you'll probably need to change the suppression.
     if (!base::PlatformThread::CreateNonJoinable(
