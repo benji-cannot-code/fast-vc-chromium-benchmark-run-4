97ccec9f7faf1b0e5cc3af1e50eb1b98bf1c758a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 14:56:28 +0000

    views: Fixes leak in ComboboxModelExampleList.
    - Use a ScopedVector to delete the examples, otherwise we never release the
    memory allocated.
    
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9395009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122084 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -8,11 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
+#include "base/memory/scoped_vector.h"
 #include "base/process_util.h"
 #include "base/stl_util.h"
 #include "base/utf_string_conversions.h"
-#include "ui/base/ui_base_paths.h"
 #include "ui/base/models/combobox_model.h"
+#include "ui/base/ui_base_paths.h"
 #include "ui/views/controls/button/text_button.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/controls/label.h"
@@ -70,7 +71,7 @@ class ComboboxModelExampleList : public ui::ComboboxModel {
   }
 
  private:
-  std::vector<ExampleBase*> example_list_;
+  ScopedVector<ExampleBase> example_list_;
 
   DISALLOW_COPY_AND_ASSIGN(ComboboxModelExampleList);
 };
