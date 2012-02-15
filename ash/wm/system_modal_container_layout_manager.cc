9cbf46b2c8fa1091227147a8b9b4088cb88bf2b3
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 04:07:57 +0000

    Fix two crash and potential leak&crash * closing window while menu is open  This only fix part of this. There is another place that crashes and i'll address it in separate CL. * closing window while modal dialog is open * release resources early and use window::Close in BallonViewImpl  This was causing crash if the widget gets closed earlier than  DelayedClose task is executed.
    BUG=114123
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9348113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122028 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/system_modal_container_layout_manager.cc
+++ ash/wm/system_modal_container_layout_manager.cc
@@ -122,6 +122,13 @@ void SystemModalContainerLayoutManager::OnWindowPropertyChanged(
   }
 }
 
+void SystemModalContainerLayoutManager::OnWindowDestroying(
+    aura::Window* window) {
+  if (modal_screen_ && modal_screen_->GetNativeView() == window)
+    modal_screen_ = NULL;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // SystemModalContainerLayoutManager,
 //     ui::ImplicitAnimationObserver implementation:
