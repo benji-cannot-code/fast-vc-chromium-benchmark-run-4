9c7555515eadbf5a010a0034dfd9c12402ea26e1
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 21:04:18 +0000

    Settings: Fix setting the title when loading a specific page.
    There are three places we need to set the title:
    * Showing a page.
    * Showing an overlay.
    * Updating history, .e.g., after closing an overlay.
    
    BUG=113940
    TEST=none
    R=csilv
    
    Review URL: https://chromiumcodereview.appspot.com/9405023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options_page.js
+++ chrome/browser/resources/options2/options_page.js
@@ -134,6 +134,9 @@ cr.define('options', function() {
     if (updateHistory)
       this.updateHistoryState_();
 
+    // Update tab title.
+    this.setTitle_(targetPage.title);
+
     // Notify pages if they were shown.
     for (var name in this.registeredPages) {
       var page = this.registeredPages[name];
@@ -255,6 +258,7 @@ cr.define('options', function() {
                          {pageName: page.name},
                          page.title,
                          '/' + page.name);
+
     // Update tab title.
     this.setTitle_(page.title);
   };
@@ -277,6 +281,9 @@ cr.define('options', function() {
       overlay.visible = true;
       if (overlay.didShowPage) overlay.didShowPage();
     }
+
+    // Update tab title.
+    this.setTitle_(overlay.title);
 
     return true;
   };
