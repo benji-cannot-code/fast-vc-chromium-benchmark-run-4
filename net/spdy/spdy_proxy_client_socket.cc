0465c66a63c7f1348b7e9321b1edcfe009707e34
rtenneti@google.com <rtenneti@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 23:31:22 +0000

    SPDY Proxy - Bug fix to handle flow control (spdy/2.1) Increase receive window size with bytes read.
    BUG=114064
    R=willchan
    TEST=spdy tests. Accessing mail with SPDY proxy enabled.
    Review URL: https://chromiumcodereview.appspot.com/9392030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122395 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_proxy_client_socket.cc
+++ net/spdy/spdy_proxy_client_socket.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -173,12 +173,14 @@ int SpdyProxyClientSocket::PopulateUserReadBuffer() {
   if (!user_buffer_)
     return ERR_IO_PENDING;
 
+  int bytes_read = 0;
   while (!read_buffer_.empty() && user_buffer_->BytesRemaining() > 0) {
     scoped_refptr<DrainableIOBuffer> data = read_buffer_.front();
     const int bytes_to_copy = std::min(user_buffer_->BytesRemaining(),
                                        data->BytesRemaining());
     memcpy(user_buffer_->data(), data->data(), bytes_to_copy);
     user_buffer_->DidConsume(bytes_to_copy);
+    bytes_read += bytes_to_copy;
     if (data->BytesRemaining() == bytes_to_copy) {
       // Consumed all data from this buffer
       read_buffer_.pop_front();
@@ -186,6 +188,9 @@ int SpdyProxyClientSocket::PopulateUserReadBuffer() {
       data->DidConsume(bytes_to_copy);
     }
   }
+
+  if (bytes_read > 0 && spdy_stream_)
+    spdy_stream_->IncreaseRecvWindowSize(bytes_read);
 
   return user_buffer_->BytesConsumed();
 }
