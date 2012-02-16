3ea485de62a9f4c7a025e0de4bb6b13cdd2f7fb3
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 21:29:43 +0000

    Settings: Fix the title of the 'manage profile' overlay.
    BUG=114616
    TEST=none
    R=csilv
    
    Review URL: https://chromiumcodereview.appspot.com/9416034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/manage_profile_overlay.js
+++ chrome/browser/resources/options2/manage_profile_overlay.js
@@ -16,9 +16,7 @@ cr.define('options', function() {
    * @class
    */
   function ManageProfileOverlay() {
-    OptionsPage.call(this,
-                     'manageProfile',
-                     templateData.manageProfileOverlayTabTitle,
+    OptionsPage.call(this, 'manageProfile', templateData.manageProfileTabTitle,
                      'manage-profile-overlay');
   };
 
@@ -220,9 +218,7 @@ cr.define('options', function() {
       $('manage-profile-overlay-delete').hidden = true;
       ManageProfileOverlay.getInstance().hideErrorBubble_();
 
-      // Intentionally don't show the URL in the location bar as we don't want
-      // people trying to navigate here by hand.
-      OptionsPage.showPageByName('manageProfile', false);
+      OptionsPage.navigateToPage('manageProfile');
     },
 
     /**
@@ -239,9 +235,7 @@ cr.define('options', function() {
       $('delete-profile-message').style.backgroundImage = 'url("' +
           profileInfo.iconURL + '")';
 
-      // Intentionally don't show the URL in the location bar as we don't want
-      // people trying to navigate here by hand.
-      OptionsPage.showPageByName('manageProfile', false);
+      OptionsPage.navigateToPage('manageProfile');
     },
   };
 
