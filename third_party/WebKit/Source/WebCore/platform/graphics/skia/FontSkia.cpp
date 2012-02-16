d0700e0648e81b146b5f3a127ab926b8e466c60a
knorton@google.com <knorton@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-16 02:15:47 +0000

    fill-opacity does not render properly only on Chromium Mac. https://bugs.webkit.org/show_bug.cgi?id=78624
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    Test: svg/text/text-fill-opacity.svg
    
    * platform/graphics/skia/FontSkia.cpp:
    (WebCore::Font::drawGlyphs):
    
    LayoutTests:
    
    * platform/chromium-linux/svg/text/text-fill-opacity-expected.png: Added.
    * platform/chromium-linux/svg/text/text-fill-opacity-expected.txt: Added.
    * platform/chromium/test_expectations.txt:
    * svg/text/text-fill-opacity.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/FontSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/FontSkia.cpp
@@ -160,7 +160,6 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
         setupPaint(&paint, font, this, shouldAntialias, shouldSmoothFonts);
         adjustTextRenderMode(&paint, gc->platformContext());
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
-        paint.setColor(gc->fillColor().rgb());
 
         canvas->drawPosText(glyphs, numGlyphs * sizeof(uint16_t), pos, paint);
     }
@@ -174,7 +173,6 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
         setupPaint(&paint, font, this, shouldAntialias, shouldSmoothFonts);
         adjustTextRenderMode(&paint, gc->platformContext());
         paint.setTextEncoding(SkPaint::kGlyphID_TextEncoding);
-        paint.setColor(gc->strokeColor().rgb());
 
         if (textMode & TextModeFill) {
             // If we also filled, we don't want to draw shadows twice.
