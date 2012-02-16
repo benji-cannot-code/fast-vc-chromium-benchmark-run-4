87873f1896cfa1de337c688d576244e422526745
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 00:21:22 +0000

    GTK: Address GSEALs in TaskManager.
    BUG=79722
    
    
    Review URL: http://codereview.chromium.org/9404016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/task_manager_gtk.h
+++ chrome/browser/ui/gtk/task_manager_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -85,9 +85,6 @@ class TaskManagerGtk : public TaskManagerModelObserver {
 
   // Response signal handler that notifies us of dialog responses.
   CHROMEGTK_CALLBACK_1(TaskManagerGtk, void, OnResponse, int);
-
-  // Realize signal handler to set the page column's initial size.
-  CHROMEG_CALLBACK_0(TaskManagerGtk, void, OnTreeViewRealize, GtkTreeView*);
 
   // Changed signal handler that is sent when the treeview selection changes.
   CHROMEG_CALLBACK_0(TaskManagerGtk, void, OnSelectionChanged,
