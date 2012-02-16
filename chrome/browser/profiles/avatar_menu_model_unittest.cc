539d81722b11c070ebe3994e4f9465d763c43f7a
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 19:23:58 +0000

    Reland 122003 - chromeos: Use default profile as last used profile in login screen.
    Otherwise, we might load a profile from /home/chronos/user before
    cryptohome does its job.
    
    BUG=chromium-os:25789
    TEST=Verify fix for chromium-os:25789.
    
    Review URL: http://codereview.chromium.org/9350063
    TBR=davemoore@chromium.org
    
    Review URL: http://codereview.chromium.org/9405015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122333 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/avatar_menu_model_unittest.cc
+++ chrome/browser/profiles/avatar_menu_model_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,6 +39,10 @@ class AvatarMenuModelTest : public testing::Test {
 
   virtual void SetUp() {
     ASSERT_TRUE(manager_.SetUp());
+#if defined(OS_CHROMEOS)
+    // AvatarMenu and multiple profiles works after user logged in.
+    manager_.SetLoggedIn(true);
+#endif
   }
 
   Browser* browser() { return NULL; }
--- chrome/test/base/testing_profile_manager.h
+++ chrome/test/base/testing_profile_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -56,6 +56,9 @@ class TestingProfileManager {
   // Deletes the cache instance. This is useful for testing that the cache is
   // properly persisting data.
   void DeleteProfileInfoCache();
+
+  // Sets ProfileManager's logged_in state. This is only useful on ChromeOS.
+  void SetLoggedIn(bool logged_in);
 
   // Helper accessors.
   ProfileManager* profile_manager();
