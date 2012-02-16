5b507c33ab766c57b616e5f79a3b51a97c87c169
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-16 19:36:26 +0000

    [chromium] Add method to WebPluginContainer to check if a rectangle is topmost https://bugs.webkit.org/show_bug.cgi?id=78166
    Patch by Viet-Trung Luu <viettrungluu@chromium.org> on 2012-02-16
    Reviewed by Darin Fisher.
    
    * public/WebPluginContainer.h:
    (WebPluginContainer):
    * src/WebPluginContainerImpl.cpp:
    * src/WebPluginContainerImpl.h:
    (WebPluginContainerImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@107960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.cpp
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoadRequest.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
+#include "HitTestResult.h"
 #include "HostWindow.h"
 #include "HTMLFormElement.h"
 #include "HTMLNames.h"
@@ -433,6 +434,26 @@ void WebPluginContainerImpl::zoomLevelChanged(double zoomLevel)
 {
     WebViewImpl* view = WebViewImpl::fromPage(m_element->document()->frame()->page());
     view->fullFramePluginZoomLevelChanged(zoomLevel);
+}
+ 
+bool WebPluginContainerImpl::isRectTopmost(const WebRect& rect)
+{
+    Page* page = m_element->document()->page();
+    if (!page)
+        return false;
+
+    // hitTestResultAtPoint() takes a padding rectangle.
+    // FIXME: We'll be off by 1 when the width or height is even.
+    IntRect windowRect = convertToContainingWindow(static_cast<IntRect>(rect));
+    LayoutPoint center = windowRect.center();
+    // Make the rect we're checking (the point surrounded by padding rects) contained inside the requested rect. (Note that -1/2 is 0.)
+    LayoutSize padding((windowRect.width() - 1) / 2, (windowRect.height() - 1) / 2);
+    HitTestResult result =
+        page->mainFrame()->eventHandler()->hitTestResultAtPoint(center, false, false, DontHitTestScrollbars, HitTestRequest::ReadOnly | HitTestRequest::Active, padding);
+    const HitTestResult::NodeSet& nodes = result.rectBasedTestResult();
+    if (nodes.size() != 1)
+        return false;
+    return (nodes.first().get() == m_element);
 }
 
 void WebPluginContainerImpl::didReceiveResponse(const ResourceResponse& response)
