3969ddb67241dbf783db5571256281c8f4f7c4cd
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 19:13:13 +0000

    Disk cache: Handle clearing the memory cache when it is already empty.
    BUG=114258
    TEST=net_unittests
    Review URL: https://chromiumcodereview.appspot.com/9359047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122329 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/mem_backend_impl.cc
+++ net/disk_cache/mem_backend_impl.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -297,8 +297,8 @@ bool MemBackendImpl::OpenNextEntry(void** iter, Entry** next_entry) {
 
 void MemBackendImpl::TrimCache(bool empty) {
   MemEntryImpl* next = rankings_.GetPrev(NULL);
-
-  DCHECK(next);
+  if (!next)
+    return;
 
   int target_size = empty ? 0 : LowWaterAdjust(max_size_);
   while (current_size_ > target_size && next) {
