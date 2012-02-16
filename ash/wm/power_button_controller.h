bf7dc8a72e4e7c681c1abf664d974704c6ce3b47
alicet@chromium.org <alicet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 16:58:18 +0000

    If there are existing transformation on the layer before screen locking, we should restore it after screen lock is dismissed.
    BUG=113756
    TEST=verified on alex.
    
    
    Review URL: http://codereview.chromium.org/9348089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122307 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/power_button_controller.h
+++ ash/wm/power_button_controller.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_POWER_BUTTON_CONTROLLER_H_
 #pragma once
 
+#include <map>
+
 #include "ash/ash_export.h"
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "base/timer.h"
 #include "ui/aura/root_window_observer.h"
+#include "ui/aura/window.h"
 
 namespace gfx {
 class Size;
@@ -20,6 +23,7 @@ class Size;
 
 namespace ui {
 class Layer;
+class Transform;
 }
 
 namespace ash {
@@ -156,6 +160,10 @@ class ASH_EXPORT PowerButtonController : public aura::RootWindowObserver {
   // aura::RootWindowObserver overrides:
   virtual void OnRootWindowResized(const gfx::Size& new_size) OVERRIDE;
 
+  // Fills |containers| with the containers described by |group|.
+  void GetContainers(ContainerGroup group,
+                     aura::Window::Windows* containers);
+
  private:
   // Requests that the screen be locked and starts |lock_fail_timer_|.
   void OnLockTimeout();
@@ -180,6 +188,12 @@ class ASH_EXPORT PowerButtonController : public aura::RootWindowObserver {
   // initializes the layer if it doesn't already exist.
   void ShowBackgroundLayer();
   void HideBackgroundLayer();
+
+  // Apply animation |type| to all containers described by |group|.
+  void StartAnimation(ContainerGroup group, AnimationType type);
+
+  // Retrieve original transform for |window| stored before animation started.
+  ui::Transform RetrieveOriginalTransform(aura::Window* window);
 
   scoped_ptr<PowerButtonControllerDelegate> delegate_;
 
@@ -238,6 +252,10 @@ class ASH_EXPORT PowerButtonController : public aura::RootWindowObserver {
   // |background_layer_|, as the desktop background is now covering the whole
   // screen.
   base::OneShotTimer<PowerButtonController> hide_background_layer_timer_;
+
+  // Map from containers to their original layer transforms before animation.
+  typedef std::map<aura::Window*, ui::Transform> WindowTransformsMap;
+  WindowTransformsMap container_transforms_;
 
   DISALLOW_COPY_AND_ASSIGN(PowerButtonController);
 };
