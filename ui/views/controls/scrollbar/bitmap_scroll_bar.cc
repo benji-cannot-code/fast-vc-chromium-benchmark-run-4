b2168b2d50b956bf6ed12b1e8f33cceda39f4b82
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 20:19:42 +0000

    views: Pass GetMessagePos() directly to gfx::Point.
    gfx::Point has a ctor that takes DWORD and does the conversion for us, so this
    patch uses it.
    
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9418023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122566 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/scrollbar/bitmap_scroll_bar.cc
+++ ui/views/controls/scrollbar/bitmap_scroll_bar.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -75,11 +75,9 @@ class AutorepeatButton : public ImageButton {
  private:
   void NotifyClick() {
 #if defined(OS_WIN)
-    DWORD pos = GetMessagePos();
-    POINTS points = MAKEPOINTS(pos);
-    gfx::Point cursor_point(points.x, points.y);
+    gfx::Point cursor_point(GetMessagePos());
 #elif defined(OS_LINUX)
-    gfx::Point cursor_point = Screen::GetCursorScreenPoint();
+    gfx::Point cursor_point = gfx::Screen::GetCursorScreenPoint();
 #endif
     views::MouseEvent event(ui::ET_MOUSE_RELEASED,
                             cursor_point.x(), cursor_point.y(),
