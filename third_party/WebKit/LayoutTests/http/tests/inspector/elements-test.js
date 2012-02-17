b19d0a24eae0387a5aa9373d1fda98372d5c9a5f
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 11:40:45 +0000

    Web Inspector: split innerUpdate into rebuildUpdate and refreshUpdate, make computed styles load lazily. https://bugs.webkit.org/show_bug.cgi?id=78827
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    * inspector/front-end/StylesSidebarPane.js:
    (WebInspector.StylesSidebarPane.prototype.update):
    (WebInspector.StylesSidebarPane.prototype._refreshUpdate.computedStyleCallback):
    (WebInspector.StylesSidebarPane.prototype._refreshUpdate):
    (WebInspector.StylesSidebarPane.prototype._rebuildUpdate):
    (WebInspector.StylesSidebarPane.prototype._validateNode):
    (WebInspector.StylesSidebarPane.prototype._styleSheetOrMediaQueryResultChanged):
    (WebInspector.StylesSidebarPane.prototype._attributesModified):
    (WebInspector.StylesSidebarPane.prototype._attributesRemoved):
    (WebInspector.StylesSidebarPane.prototype._styleInvalidated):
    (WebInspector.StylesSidebarPane.prototype._innerRefreshUpdate):
    (WebInspector.StylesSidebarPane.prototype._innerRebuildUpdate):
    (WebInspector.StylesSidebarPane.prototype._nodeStylesUpdatedForTest):
    (WebInspector.StylesSidebarPane.prototype._toggleElementStatePane):
    (WebInspector.StylesSidebarPane.prototype._createElementStatePane.clickListener):
    (WebInspector.StylesSidebarPane.prototype._showUserAgentStylesSettingChanged):
    (WebInspector.ComputedStyleSidebarPane.prototype.expand):
    (WebInspector.StylePropertyTreeElement.prototype):
    
    LayoutTests:
    
    * http/tests/inspector/elements-test.js:
    (initialize_ElementTest.InspectorTest.waitForStyles):
    * inspector/styles/undo-add-property.html:
    * inspector/styles/undo-change-property.html:
    * inspector/styles/updates-during-dom-traversal.html:
    * inspector/styles/updates-throttled.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108058 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/elements-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/elements-test.js
@@ -70,13 +70,13 @@ InspectorTest.selectNodeWithId = function(idValue, callback)
     InspectorTest.nodeWithId(idValue, onNodeFound);
 }
 
-InspectorTest.waitForStyles = function(idValue, callback)
+InspectorTest.waitForStyles = function(idValue, callback, requireRebuild)
 {
     callback = InspectorTest.safeWrap(callback);
 
-    (function sniff(node)
+    (function sniff(node, rebuild)
     {
-        if (node && node.getAttribute("id") === idValue) {
+        if ((rebuild || !requireRebuild) && node && node.getAttribute("id") === idValue) {
             callback();
             return;
         }
@@ -92,7 +92,7 @@ InspectorTest.selectNodeAndWaitForStyles = function(idValue, callback)
 
     var targetNode;
 
-    InspectorTest.waitForStyles(idValue, stylesUpdated);
+    InspectorTest.waitForStyles(idValue, stylesUpdated, true);
     InspectorTest.selectNodeWithId(idValue, nodeSelected);
 
     function nodeSelected(node)
