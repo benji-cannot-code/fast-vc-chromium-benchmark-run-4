8c318c857b1b8fef2581cfd54ee47bae70c712a0
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 16:16:54 +0000

    Removing code that is never compiled under any config.
    Class NativeBrowserFrameDelegate is abstract, with no derivation. 
    browser_frame_gtk.cc has 
    NativeBrowserFrameDelegate* delegate_ 
    which is never set.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9418034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122518 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame.h
+++ chrome/browser/ui/views/frame/browser_frame.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "build/build_config.h"
-#include "chrome/browser/ui/views/frame/native_browser_frame_delegate.h"
 #include "ui/views/widget/widget.h"
 
 class AvatarMenuButton;
