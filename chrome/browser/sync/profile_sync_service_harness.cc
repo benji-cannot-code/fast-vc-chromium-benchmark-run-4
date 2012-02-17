dd8cc2d540b8dc8025a1b16aa2d58bc489e06505
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 19:19:58 +0000

    Fix a compile warning.
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9423008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122553 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_harness.cc
+++ chrome/browser/sync/profile_sync_service_harness.cc
@@ -540,7 +540,6 @@ bool ProfileSyncServiceHarness::AwaitDataSyncCompletion(
   CHECK_NE(wait_state_, SYNC_DISABLED);
   CHECK_NE(wait_state_, SERVER_UNREACHABLE);
 
-  const SyncSessionSnapshot* snap = GetLastSessionSnapshot();
   if (IsDataSynced()) {
     // Client is already synced; don't wait.
     return true;
@@ -548,7 +547,6 @@ bool ProfileSyncServiceHarness::AwaitDataSyncCompletion(
 
   wait_state_ = WAITING_FOR_DATA_SYNC;
   AwaitStatusChangeWithTimeout(kLiveSyncOperationTimeoutMs, reason);
-  snap = GetLastSessionSnapshot();
   if (wait_state_ == FULLY_SYNCED) {
     return true;
   } else {
