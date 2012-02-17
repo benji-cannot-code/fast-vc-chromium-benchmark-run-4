be1c161c38ed5f3d70b6e08ac241c45cb390e463
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 22:27:55 +0000

    [Sync] Fix sync behavior when token service is cleared.
    We now trigger an unrecoverable error if the token service has lost the sync
    token.
    
    In addition, we ensure any implicit passphrase that was cached gets
    consumed after we finishing configuring datatypes.
    
    BUG=112631
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9348072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122606 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/token_service_unittest.cc
+++ chrome/browser/signin/token_service_unittest.cc
@@ -145,6 +145,7 @@ TEST_F(TokenServiceTest, SanityCheck) {
   EXPECT_TRUE(service_->HasOAuthCredentials());
   EXPECT_EQ(service_->GetOAuthToken(), "oauth");
   EXPECT_EQ(service_->GetOAuthSecret(), "secret");
+  EXPECT_FALSE(service_->TokensLoadedFromDB());
 }
 
 TEST_F(TokenServiceTest, NoToken) {
@@ -388,6 +389,7 @@ TEST_F(TokenServiceTest, LoadTokensIntoMemoryBasic) {
   std::map<std::string, std::string> db_tokens;
   std::map<std::string, std::string> memory_tokens;
 
+  EXPECT_FALSE(service_->TokensLoadedFromDB());
   service_->LoadTokensIntoMemory(db_tokens, &memory_tokens);
   EXPECT_TRUE(db_tokens.empty());
   EXPECT_TRUE(memory_tokens.empty());
@@ -444,6 +446,7 @@ TEST_F(TokenServiceTest, LoadTokensIntoMemoryAdvanced) {
 TEST_F(TokenServiceTest, WebDBLoadIntegration) {
   service_->LoadTokensFromDB();
   WaitForDBLoadCompletion();
+  EXPECT_TRUE(service_->TokensLoadedFromDB());
   EXPECT_EQ(0U, success_tracker_.size());
 
   // Should result in DB write.
@@ -489,11 +492,14 @@ TEST_F(TokenServiceTest, MultipleLoadResetIntegration) {
   EXPECT_FALSE(service_->HasTokenForService(GaiaConstants::kSyncServiceOAuth));
   EXPECT_FALSE(service_->HasOAuthCredentials());
 
+  EXPECT_FALSE(service_->TokensLoadedFromDB());
   service_->LoadTokensFromDB();
   WaitForDBLoadCompletion();
+  EXPECT_TRUE(service_->TokensLoadedFromDB());
 
   service_->LoadTokensFromDB();  // Should do nothing.
   WaitForDBLoadCompletion();
+  EXPECT_TRUE(service_->TokensLoadedFromDB());
 
   EXPECT_EQ(2U, success_tracker_.size());
   EXPECT_TRUE(service_->HasTokenForService(GaiaConstants::kSyncService));
@@ -504,10 +510,12 @@ TEST_F(TokenServiceTest, MultipleLoadResetIntegration) {
 
   // Reset it one more time so there's no surprises.
   service_->ResetCredentialsInMemory();
+  EXPECT_FALSE(service_->TokensLoadedFromDB());
   success_tracker_.Reset();
 
   service_->LoadTokensFromDB();
   WaitForDBLoadCompletion();
+  EXPECT_TRUE(service_->TokensLoadedFromDB());
 
   EXPECT_EQ(2U, success_tracker_.size());
   EXPECT_TRUE(service_->HasTokenForService(GaiaConstants::kSyncService));
