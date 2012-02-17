5809a9dd5ecac117916e3faa8b5181b61d7e625a
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 07:20:12 +0000

    Add ENABLE(STYLE_SCOPED) around HTMLStyleElement::m_isRegisteredWithScopingNode
    The instance variable HTMLStyleElement::m_isRegisteredWithScopingNode is only
    referenced from within ENABLE(STYLE_SCOPED)-guarded code. We should add this
    guard around its declaration.
    
    * html/HTMLStyleElement.h:
    (HTMLStyleElement):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLStyleElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLStyleElement.h
@@ -75,7 +75,9 @@ private:
     void registerWithScopingNode();
     void unregisterWithScopingNode();
 
+#if ENABLE(STYLE_SCOPED)
     bool m_isRegisteredWithScopingNode;
+#endif
 };
 
 } //namespace
