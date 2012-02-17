bf194aea076a0a0cd935ce999fca5c20603d63c8
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 06:43:23 +0000

    Unreviewed. Rolling out r107980, because it broke 32 bit platforms.
    Source/JavaScriptCore:
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::throwException):
    (JSC::Interpreter::privateExecute):
    * interpreter/Interpreter.h:
    (JSC):
    (Interpreter):
    * jsc.cpp:
    (GlobalObject::finishCreation):
    * parser/Nodes.h:
    (JSC::FunctionBodyNode::setInferredName):
    * parser/Parser.h:
    (JSC::::parse):
    * runtime/CommonIdentifiers.h:
    * runtime/Error.cpp:
    (JSC::addErrorInfo):
    * runtime/Error.h:
    (JSC):
    
    LayoutTests:
    
    * fast/js/exception-properties-expected.txt:
    * fast/js/script-tests/exception-properties.js:
    * fast/js/script-tests/stack-trace.js: Removed.
    * fast/js/stack-trace-expected.txt: Removed.
    * fast/js/stack-trace.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/exception-properties.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/exception-properties.js
@@ -17,7 +17,7 @@ try {
     var error = new Error("message");
 
     shouldBe('enumerableProperties(error)', '[]');
-    shouldBe('enumerableProperties(nativeError)', '["line", "sourceURL", "stack"]');
+    shouldBe('enumerableProperties(nativeError)', '["line", "sourceURL"]');
 
     shouldBe('Object.getPrototypeOf(nativeError).name', '"RangeError"');
     shouldBe('Object.getPrototypeOf(nativeError).message', '""');
