8e8ea38f73c245200a0b9dc8812da318b46ab68c
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 14:32:17 +0000

    [GTK] Rename WebKitPrintOperation::done signal to ::finished in WebKit2 https://bugs.webkit.org/show_bug.cgi?id=78893
    Reviewed by Gustavo Noronha Silva.
    
    And it's not emitted anynmore when the print dialog has been
    cancelled. Since it's not possible to know whether the print
    dialog was cancelled or not, the enum WebKitPrintOperationResponse
    has been added, with Print and Cancel values, and it's used as
    return value of webkit_print_operation_run_dialog().
    
    * UIProcess/API/gtk/WebKitPrintOperation.cpp:
    (webkit_print_operation_class_init): Rename done as finished.
    (webkitPrintOperationRunDialogUnix): Return a
    WebKitPrintOperationResponse instead of bool.
    (webkitPrintOperationRunDialogWin32): Ditto.
    (drawPagesForPrintingCompleted): Emit finished instead of done.
    (webkitPrintOperationRunDialogForFrame): Do not emit finished if
    the dialog was cancelled and return a WebKitPrintOperationResponse.
    (webkit_print_operation_run_dialog): Return
    WebKitPrintOperationResponse.
    * UIProcess/API/gtk/WebKitPrintOperation.h: Add
    WebKitPrintOperationResponse enum.
    * UIProcess/API/gtk/WebKitPrintOperationPrivate.h:
    * UIProcess/API/gtk/WebKitWebView.cpp:
    (webkitWebViewPrintFrame): Check whether the dialog has been
    cancelled or not and connect to finish when the print operation is
    in progress to release the print operation object when printing
    finishes.
    * UIProcess/API/gtk/docs/webkit2gtk-sections.txt: Add
    WebKitPrintOperationResponse symbol.
    * UIProcess/API/gtk/tests/TestPrinting.cpp:
    (testPrintOperationPrintFinished): Use finished instead of done.
    (testPrintOperationPrint): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108069 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitPrintOperationPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitPrintOperationPrivate.h
@@ -24,6 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrameProxy.h"
 #include "WebKitPrintOperation.h"
 
-void webkitPrintOperationRunDialogForFrame(WebKitPrintOperation*, GtkWindow* parent, WebKit::WebFrameProxy*);
+WebKitPrintOperationResponse webkitPrintOperationRunDialogForFrame(WebKitPrintOperation*, GtkWindow* parent, WebKit::WebFrameProxy*);
 
 #endif // WebKitPrintOperationPrivate_h
