a56b176557b997614fb35b6d99bae7c39f79c329
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 23:57:08 +0000

    Move textDirectionForSelection from Editor to EditingStyle https://bugs.webkit.org/show_bug.cgi?id=78868
    Reviewed by Enrica Casucci.
    
    Move textDirectionForSelection from Editor to EditingStyle to centralize the editing code's
    dependency on CSSStyleDeclaration.
    
    * editing/EditingStyle.cpp:
    (WebCore::EditingStyle::textDirectionForSelection):
    (WebCore):
    * editing/EditingStyle.h:
    (EditingStyle):
    * editing/Editor.cpp:
    (WebCore):
    * editing/Editor.h:
    (Editor):
    * editing/EditorCommand.cpp:
    (WebCore::stateTextWritingDirection):
    * editing/ReplaceSelectionCommand.cpp:
    * editing/markup.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ApplyStyleCommand.h"
 #include "BeforeTextInsertedEvent.h"
 #include "BreakBlockquoteCommand.h"
-#include "CSSComputedStyleDeclaration.h"
 #include "CSSPropertyNames.h"
 #include "CSSValueKeywords.h"
 #include "Document.h"
--- third_party/WebKit/Source/WebCore/editing/markup.cpp
+++ third_party/WebKit/Source/WebCore/editing/markup.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "markup.h"
 
 #include "CDATASection.h"
-#include "CSSComputedStyleDeclaration.h"
 #include "CSSPrimitiveValue.h"
 #include "CSSProperty.h"
 #include "CSSPropertyNames.h"
