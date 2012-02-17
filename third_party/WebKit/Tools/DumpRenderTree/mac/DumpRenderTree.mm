ef09087ae847dd35c216c99cd604724a4878c9eb
aroben@apple.com <aroben@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 22:31:14 +0000

    Clang/Mac build fix after r108119
    Source/WebKit2:
    
    * WebProcess/WebProcess.cpp:
    (WebKit::randomCrashThread): Annotate this function with NO_RETURN_DUE_TO_CRASH.
    
    Tools:
    
    Mac build fix after r108119
    
    * DumpRenderTree/mac/DumpRenderTree.mm:
    (runThread):
    (runPthread):
    (testThreadIdentifierMap):
    Updated to match current WTF threading APIs.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/DumpRenderTree.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/DumpRenderTree.mm
@@ -682,13 +682,18 @@ static void setDefaultsToConsistentValuesForTesting()
     [WebPreferences _switchNetworkLoaderToNewTestingSession];
 }
 
-static void* runThread(void* arg)
+static void runThread(void* arg)
 {
     static ThreadIdentifier previousId = 0;
     ThreadIdentifier currentId = currentThread();
     // Verify 2 successive threads do not get the same Id.
     ASSERT(previousId != currentId);
     previousId = currentId;
+}
+
+static void* runPthread(void* arg)
+{
+    runThread(arg);
     return 0;
 }
 
@@ -696,10 +701,10 @@ static void testThreadIdentifierMap()
 {
     // Imitate 'foreign' threads that are not created by WTF.
     pthread_t pthread;
-    pthread_create(&pthread, 0, &runThread, 0);
+    pthread_create(&pthread, 0, &runPthread, 0);
     pthread_join(pthread, 0);
 
-    pthread_create(&pthread, 0, &runThread, 0);
+    pthread_create(&pthread, 0, &runPthread, 0);
     pthread_join(pthread, 0);
 
     // Now create another thread using WTF. On OSX, it will have the same pthread handle
