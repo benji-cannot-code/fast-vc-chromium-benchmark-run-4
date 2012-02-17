1df174e191bca39ccaf63d43195b67b94171fecb
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 02:14:20 +0000

    Revert 122422 - aura: ExtensionPopup bubbles should not close when they open JS Dialogs.
    BUG=106958
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9347051
    
    TBR=varunjain@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9418032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/extension_popup.h
+++ chrome/browser/ui/views/extensions/extension_popup.h
@@ -76,8 +76,6 @@ class ExtensionPopup : public views::BubbleDelegateView,
   // Show the bubble, focus on its content, and register listeners.
   void ShowBubble();
 
-  void CloseBubble();
-
   // The contained host for the view.
   scoped_ptr<ExtensionHost> extension_host_;
 
@@ -86,8 +84,6 @@ class ExtensionPopup : public views::BubbleDelegateView,
   bool inspect_with_devtools_;
 
   content::NotificationRegistrar registrar_;
-
-  base::WeakPtrFactory<ExtensionPopup> close_bubble_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionPopup);
 };
