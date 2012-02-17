b531d861de9643108915e51724f5946e179aa4cb
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 16:25:15 +0000

    Web Inspector: Switch Debugger agent to TypeBuilder https://bugs.webkit.org/show_bug.cgi?id=78390
    Patch by Peter Rybin <peter.rybin@gmail.com> on 2012-02-17
    Reviewed by Vsevolod Vlasov.
    
    Client code is switched to TypeBuilder.
    
    * inspector/CodeGeneratorInspector.py:
    * inspector/InjectedScript.cpp:
    (WebCore::InjectedScript::evaluateOnCallFrame):
    (WebCore::InjectedScript::getFunctionDetails):
    (WebCore::InjectedScript::getProperties):
    (WebCore::InjectedScript::wrapCallFrames):
    * inspector/InjectedScript.h:
    (InjectedScript):
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::setBreakpointByUrl):
    (WebCore::InspectorDebuggerAgent::resolveBreakpoint):
    (WebCore::InspectorDebuggerAgent::getFunctionDetails):
    (WebCore::InspectorDebuggerAgent::evaluateOnCallFrame):
    (WebCore::InspectorDebuggerAgent::currentCallFrames):
    (WebCore::InspectorDebuggerAgent::didParseSource):
    * inspector/InspectorDebuggerAgent.h:
    (InspectorDebuggerAgent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/CodeGeneratorInspector.py
+++ third_party/WebKit/Source/WebCore/inspector/CodeGeneratorInspector.py
@@ -57,7 +57,8 @@ TYPE_NAME_FIX_MAP = {
 }
 
 
-TYPES_WITH_RUNTIME_CAST_SET = frozenset([])
+TYPES_WITH_RUNTIME_CAST_SET = frozenset(["Runtime.RemoteObject", "Runtime.PropertyDescriptor",
+                                         "Debugger.FunctionDetails", "Debugger.CallFrame"])
 
 
 cmdline_parser = optparse.OptionParser()
