645915058c12b9b58f0a28c47a903d49c8f78839
morrita@chromium.org <morrita@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 04:13:59 +0000

    Unmark FAIL on DeviceOrientationBrowserTest.BasicTest The breakage is fixed WebKit r107799.
    TEST=DeviceOrientationBrowserTest
    TBR=hans@chromium.org
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/9413036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122453 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/device_orientation/device_orientation_browsertest.cc
+++ content/browser/device_orientation/device_orientation_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,7 +49,7 @@ class DeviceOrientationBrowserTest : public InProcessBrowserTest {
 };
 
 // crbug.com/113952
-IN_PROC_BROWSER_TEST_F(DeviceOrientationBrowserTest, FAILS_BasicTest) {
+IN_PROC_BROWSER_TEST_F(DeviceOrientationBrowserTest, BasicTest) {
   const Orientation kTestOrientation(true, 1, true, 2, true, 3);
   scoped_refptr<MockProvider> provider(new MockProvider(kTestOrientation));
   Provider::SetInstanceForTests(provider.get());
