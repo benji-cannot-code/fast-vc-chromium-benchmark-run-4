baef481012369b50f25f0ee85dcba3e0a9d3684b
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 23:56:48 +0000

    Make the gpu pixel test container size 400x300
    This is because on Mac, chrome window has a minimum size.  400x300 definitely is bigger than that.
    
    BUG=
    TEST=GpuPixelBrowserTest on gpu bots
    R=jbates
    Review URL: https://chromiumcodereview.appspot.com/9426010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/gpu/gpu_pixel_browsertest.cc
+++ chrome/test/gpu/gpu_pixel_browsertest.cc
@@ -354,7 +354,7 @@ IN_PROC_BROWSER_TEST_F(GpuPixelBrowserTest, MAYBE_WebGLGreenTriangle) {
   // following number. If no revision requirement, then 0.
   const int64 ref_img_revision_update = 118461;
 
-  gfx::Size container_size(250, 250);
+  gfx::Size container_size(400, 300);
   FilePath url =
       test_data_dir().AppendASCII("pixel_webgl.html");
   RunPixelTest(container_size, url, ref_img_revision_update);
@@ -370,7 +370,7 @@ IN_PROC_BROWSER_TEST_F(GpuPixelBrowserTest, MAYBE_CSS3DBlueBox) {
   // following number. If no revision requirement, then 0.
   const int64 ref_img_revision_update = 118461;
 
-  gfx::Size container_size(250, 250);
+  gfx::Size container_size(400, 300);
   FilePath url =
       test_data_dir().AppendASCII("pixel_css3d.html");
   RunPixelTest(container_size, url, ref_img_revision_update);
@@ -394,7 +394,7 @@ IN_PROC_BROWSER_TEST_F(Canvas2DPixelTestHD, MAYBE_Canvas2DRedBoxHD) {
   // following number. If no revision requirement, then 0.
   const int64 ref_img_revision_update = 121581;
 
-  gfx::Size container_size(250, 250);
+  gfx::Size container_size(400, 300);
   FilePath url =
       test_data_dir().AppendASCII("pixel_canvas2d.html");
   RunPixelTest(container_size, url, ref_img_revision_update);
@@ -418,7 +418,7 @@ IN_PROC_BROWSER_TEST_F(Canvas2DPixelTestSD, MAYBE_Canvas2DRedBoxSD) {
   // following number. If no revision requirement, then 0.
   const int64 ref_img_revision_update = 121581;
 
-  gfx::Size container_size(250, 250);
+  gfx::Size container_size(400, 300);
   FilePath url =
       test_data_dir().AppendASCII("pixel_canvas2d.html");
   RunPixelTest(container_size, url, ref_img_revision_update);
