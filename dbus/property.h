7999f4635c10db542f3befdc2f0d029e468bbb23
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 16:50:42 +0000

    dbus: slight cleanup to Property code
    Correct comments based on practice that worked best for the bluetooth
    adapter and device clients, in particular adding the virtual destructor
    to the struct and cleaning up the return values of GetObject() in the
    documentation.
    
    Drop VLOG() calls, there's no need to debug at this level, you can use
    dbus-monitor to see the incoming and outgoing calls to Chromium and can
    add logging in upper layers when there are real values to log.
    
    BUG=none
    TEST=built on try bots
    
    Change-Id: I2ac6b55e288626b5f9cd843935887e261d386dec
    
    
    Review URL: http://codereview.chromium.org/9418035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122522 0039d316-1c4b-4281-b951-d872f2087c98

--- dbus/property.h
+++ dbus/property.h
@@ -41,12 +41,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //       Properties(dbus::ObjectProxy* object_proxy,
 //                  PropertyChangedCallback callback)
-//           : dbus::PropertySet(object_proxy, kExampleInterface, callback) {
+//           : dbus::PropertySet(object_proxy, "com.example.DBus", callback) {
 //         RegisterProperty("Name", &name);
 //         RegisterProperty("Version", &version);
 //         RegisterProperty("Parent", &parent);
 //         RegisterProperty("Children", &children);
 //       }
+//       virtual ~Properties() {}
 //     };
 //
 // The Properties structure requires a pointer to the object proxy of the
@@ -57,8 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Example (continued):
 //
-//     typedef std::map<std::pair<dbus::ObjectProxy*,Properties*> > Object;
-//     typedef std::map<dbus::ObjectPath,Object> ObjectMap;
+//     typedef std::map<std::pair<dbus::ObjectProxy*, Properties*> > Object;
+//     typedef std::map<dbus::ObjectPath, Object> ObjectMap;
 //     ObjectMap object_map_;
 //
 //     dbus::ObjectProxy* GetObjectProxy(const dbus::ObjectPath& object_path) {
@@ -75,7 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //         return it->second;
 //
 //       dbus::ObjectProxy* object_proxy = bus->GetObjectProxy(...);
-//       // connect signals, ect.
+//       // connect signals, etc.
 //
 //       Properties* properties = new Properties(
 //           object_proxy,
@@ -85,8 +86,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       properties->ConnectSignals();
 //       properties->GetAll();
 //
-//       return object_map_.insert(std::make_pair(object_proxy,
-//                                                properties).first;
+//       Object object = std::make_pair(object_proxy, properties);
+//       object_map_[object_path] = object;
+//       return object;
 //     }
 //  };
 //
@@ -218,7 +220,7 @@ class PropertySet {
                                 const std::string& signal_name,
                                 bool success);
 
-  // Queries the remove object for values of all properties and updates
+  // Queries the remote object for values of all properties and updates
   // initial values. Sub-classes may override to use a different D-Bus
   // method, or if the remote object does not support retrieving all
   // properties, either ignore or obtain each property value individually.
@@ -326,14 +328,12 @@ class Property : public PropertyBase {
   // This may not be implemented by some interfaces, and may be overriden by
   // sub-classes if interfaces use different method calls.
   void Get(GetCallback callback) {
-    DVLOG(1) << "Get: " << name();
-
     MethodCall method_call(kPropertiesInterface, kPropertiesGet);
     MessageWriter writer(&method_call);
     writer.AppendString(property_set()->interface());
     writer.AppendString(name());
 
-    ObjectProxy *object_proxy = property_set()->object_proxy();
+    ObjectProxy* object_proxy = property_set()->object_proxy();
     DCHECK(object_proxy);
     object_proxy->CallMethod(&method_call,
                              ObjectProxy::TIMEOUT_USE_DEFAULT,
@@ -345,7 +345,6 @@ class Property : public PropertyBase {
   // Callback for Get(), may be overriden by sub-classes if interfaces
   // use different response arguments.
   virtual void OnGet(SetCallback callback, Response* response) {
-    DVLOG(1) << "OnGet: " << name();
     if (!response) {
       LOG(WARNING) << name() << ": Get: failed.";
       return;
@@ -365,15 +364,13 @@ class Property : public PropertyBase {
   // remote object. This method may be overriden by sub-classes if
   // interfaces use different method calls.
   void Set(const T& value, SetCallback callback) {
-    DVLOG(1) << "Set: " << name();
-
     MethodCall method_call(kPropertiesInterface, kPropertiesSet);
     MessageWriter writer(&method_call);
     writer.AppendString(property_set()->interface());
     writer.AppendString(name());
     AppendToWriter(&writer, value);
 
-    ObjectProxy *object_proxy = property_set()->object_proxy();
+    ObjectProxy* object_proxy = property_set()->object_proxy();
     DCHECK(object_proxy);
     object_proxy->CallMethod(&method_call,
                              ObjectProxy::TIMEOUT_USE_DEFAULT,
@@ -385,7 +382,6 @@ class Property : public PropertyBase {
   // Callback for Set(), may be overriden by sub-classes if interfaces
   // use different response arguments.
   virtual void OnSet(SetCallback callback, Response* response) {
-    DVLOG(1) << "OnSet: " << name();
     LOG_IF(WARNING, !response) << name() << ": Set: failed.";
     if (!callback.is_null())
       callback.Run(response);
