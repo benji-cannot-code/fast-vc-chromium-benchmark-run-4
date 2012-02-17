7d66b75357c8d7fe24b7fbddfb70f1749ebf6158
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 04:43:26 +0000

    [uber] Redoing the homepage selection UI
    Resubmit of http://codereview.chromium.org/9296038
    
    BUG=111139
    TEST=SessionStartupPrefTest.HomePageMigration
    
    Review URL: https://chromiumcodereview.appspot.com/9387019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/session_startup_pref.h
+++ chrome/browser/prefs/session_startup_pref.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,8 +18,11 @@ class Profile;
 // StartupPref is stored in the preferences for a particular profile.
 struct SessionStartupPref {
   enum Type {
-    // Indicates the user doesn't want to restore a previous session.
+    // Indicates the user wants to open the New Tab page.
     DEFAULT,
+
+    // Deprecated. See comment in session_startup_pref.cc.
+    HOMEPAGE,
 
     // Indicates the user wants to restore the last session.
     LAST,
@@ -41,8 +44,6 @@ struct SessionStartupPref {
   // Whether the startup type and URLs are managed via policy.
   static bool TypeIsManaged(PrefService* prefs);
   static bool URLsAreManaged(PrefService* prefs);
-
-  SessionStartupPref();
 
   explicit SessionStartupPref(Type type);
 
