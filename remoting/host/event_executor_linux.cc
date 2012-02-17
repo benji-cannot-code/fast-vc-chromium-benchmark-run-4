34a0299d241cc37cf828f5efe4fe5b6879536c1c
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 02:57:42 +0000

    Fix VK -> XK mapping for VK_LSHIFT -> XK_Shift_L (and _R versions). Add a few missing entries.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9416046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122439 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/event_executor_linux.cc
+++ remoting/host/event_executor_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -93,7 +93,7 @@ int VerticalScrollWheelToX11ButtonNumber(int dy) {
 // host's keyboard layout (see http://crbug.com/74550 ).
 const int kUsVkeyToKeysym[256] = {
   // 0x00 - 0x07
-  -1, -1, -1, -1,
+  -1, -1, -1, XK_Cancel,
   // 0x04 - 0x07
   -1, -1, -1, -1,
   // 0x08 - 0x0B
@@ -104,9 +104,9 @@ const int kUsVkeyToKeysym[256] = {
   // 0x10 - 0x13
   XK_Shift_L, XK_Control_L, XK_Alt_L, XK_Pause,
   // 0x14 - 0x17
-  XK_Caps_Lock, XK_Kana_Shift, -1, /* VKEY_JUNJA */ -1,
+  XK_Caps_Lock, XK_Kana_Shift, -1, XK_Hangul_Jeonja,
   // 0x18 - 0x1B
-  /* VKEY_FINAL */ -1, XK_Kanji, -1, XK_Escape,
+  XK_Hangul_End, XK_Kanji, -1, XK_Escape,
   // 0x1C - 0x1F
   XK_Henkan, XK_Muhenkan, /* VKEY_ACCEPT */ -1, XK_Mode_switch,
 
@@ -183,7 +183,7 @@ const int kUsVkeyToKeysym[256] = {
   -1, -1, -1, -1,
 
   // 0xA0 - 0xA3
-  XK_Num_Lock, XK_Scroll_Lock, XK_Control_L, XK_Control_R,
+  XK_Shift_L, XK_Shift_R, XK_Control_L, XK_Control_R,
   // 0xA4 - 0xA7
   XK_Meta_L, XK_Meta_R, XF86XK_Back, XF86XK_Forward,
   // 0xA8 - 0xAB
