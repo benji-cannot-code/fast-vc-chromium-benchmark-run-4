3420f9d33beab9e58a7b7d567b4153f30d0432b1
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 23:45:41 +0000

    Revert 122617 - Adds dump accessibility tree support in Windows.
    This largely adds human readable strings for IAccessible2 roles and states.
    This is in part to make it possible to hand-edit the expected files we generate.
    Sidenote: switch to string16 usage from std::string as much as possible (despite the incompatibility with API's provided by FilePath).
    
    Review URL: https://chromiumcodereview.appspot.com/9295031
    
    TBR=dtseng@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9425021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122626 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_mac.mm
+++ content/browser/accessibility/browser_accessibility_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -57,4 +57,14 @@ void BrowserAccessibilityMac::DetachTree(
 BrowserAccessibilityCocoa* BrowserAccessibility::toBrowserAccessibilityCocoa() {
   return static_cast<BrowserAccessibilityMac*>(this)->
       native_view();
+}
+
+std::string BrowserAccessibility::ToString() {
+  BrowserAccessibilityCocoa* cocoa_node = this->toBrowserAccessibilityCocoa();
+  NSString* dump = [NSString stringWithFormat:@"%@|%@|%@|%@",
+      [cocoa_node role],
+      [cocoa_node subrole],
+      [cocoa_node title],
+      [cocoa_node value]];
+  return [dump cStringUsingEncoding:NSUTF8StringEncoding];
 }
