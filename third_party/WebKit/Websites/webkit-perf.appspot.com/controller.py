e7aa9f21d8620fe77ace483d88660298963ab5e2
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 18:47:05 +0000

    perf-o-matic needs model unit tests https://bugs.webkit.org/show_bug.cgi?id=78885
    Reviewed by Adam Barth.
    
    Added unit tests for models.py.
    
    Also renamed modelFromNumericId to model_from_numeric_id and moved set_persitent_cache and get_persistent_cache
    from controller to PersistentCache.set_cache and PersistentCahce.set_cache respectively.
    
    * Websites/webkit-perf.appspot.com/controller.py:
    (cache_manifest):
    (CachedManifestHandler.get):
    (cache_dashboard):
    (CachedDashboardHandler.get):
    (cache_runs):
    (CachedRunsHandler.get):
    * Websites/webkit-perf.appspot.com/create_handler.py:
    (CreateHandler._create_builder):
    (CreateHandler._create_builder.execute):
    * Websites/webkit-perf.appspot.com/models.py:
    (create_in_transaction_with_numeric_id_holder):
    (model_from_numeric_id):
    (Builder):
    (Builder.create):
    (Builder.update_password):
    (Builder._hashed_password):
    (TestResult.key_name):
    (ReportLog.get_value):
    (ReportLog._integer_in_payload):
    (ReportLog):
    (ReportLog.timestamp):
    (PersistentCache):
    (PersistentCache.set_cache):
    (PersistentCache.set_cache.execute):
    (PersistentCache.get_cache):
    * Websites/webkit-perf.appspot.com/models_unittest.py: Added.
    (HelperTests):
    (HelperTests.setUp):
    (HelperTests.tearDown):
    (HelperTests._assert_there_is_exactly_one_id_holder_and_matches):
    (HelperTests.test_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_create_in_transaction_with_numeric_id_holder.execute):
    (HelperTests.test_failing_in_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_failing_in_create_in_transaction_with_numeric_id_holder.execute):
    (HelperTests.test_raising_in_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_raising_in_create_in_transaction_with_numeric_id_holder.execute):
    (HelperTests.test_delete_model_with_numeric_id_holder):
    (HelperTests.test_delete_model_with_numeric_id_holder.execute):
    (HelperTests.test_model_from_numeric_id):
    (HelperTests.test_model_from_numeric_id.execute):
    (BuilderTests):
    (BuilderTests.setUp):
    (BuilderTests.tearDown):
    (BuilderTests.test_create):
    (BuilderTests.test_update_password):
    (BuilderTests.test_hashed_password):
    (BuilderTests.test_authenticate):
    (ReportLog):
    (ReportLog.setUp):
    (ReportLog.tearDown):
    (ReportLog._create_log_with_payload):
    (ReportLog.test_parsed_payload):
    (ReportLog.test_get_value):
    (ReportLog.test_results):
    (ReportLog.test_builder):
    (ReportLog.test_build_number):
    (ReportLog.test_webkit_revision):
    (ReportLog.chromium_revision):
    (PersistentCacheTests):
    (PersistentCacheTests.setUp):
    (PersistentCacheTests.tearDown):
    (PersistentCacheTests._assert_persistent_cache):
    (PersistentCacheTests.test_set):
    (PersistentCacheTests.test_get):
    * Websites/webkit-perf.appspot.com/runs_handler.py:
    (RunsHandler.post):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/webkit-perf.appspot.com/controller.py
+++ third_party/WebKit/Websites/webkit-perf.appspot.com/controller.py
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import webapp2
-from google.appengine.api import memcache
 from google.appengine.api import taskqueue
 from google.appengine.ext import db
 
@@ -37,31 +36,8 @@ from models import Test
 from models import PersistentCache
 
 
-def set_persistent_cache(name, value):
-    memcache.set(name, value)
-
-    def execute():
-        cache = PersistentCache.get_by_key_name(name)
-        if cache:
-            cache.value = value
-            cache.put()
-        else:
-            PersistentCache(key_name=name, value=value).put()
-
-    db.run_in_transaction(execute)
-
-
-def get_persistent_cache(name):
-    value = memcache.get(name)
-    if value:
-        return value
-    cache = PersistentCache.get_by_key_name(name)
-    memcache.set(name, cache.value)
-    return cache.value
-
-
 def cache_manifest(cache):
-    set_persistent_cache('manifest', cache)
+    PersistentCache.set_cache('manifest', cache)
 
 
 def schedule_manifest_update():
@@ -71,7 +47,7 @@ def schedule_manifest_update():
 class CachedManifestHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        manifest = get_persistent_cache('manifest')
+        manifest = PersistentCache.get_cache('manifest')
         if manifest:
             self.response.out.write(manifest)
         else:
@@ -79,7 +55,7 @@ class CachedManifestHandler(webapp2.RequestHandler):
 
 
 def cache_dashboard(cache):
-    set_persistent_cache('dashboard', cache)
+    PersistentCache.set_cache('dashboard', cache)
 
 
 def schedule_dashboard_update():
@@ -89,7 +65,7 @@ def schedule_dashboard_update():
 class CachedDashboardHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        dashboard = get_persistent_cache('dashboard')
+        dashboard = PersistentCache.get_cache('dashboard')
         if dashboard:
             self.response.out.write(dashboard)
         else:
@@ -97,7 +73,7 @@ class CachedDashboardHandler(webapp2.RequestHandler):
 
 
 def cache_runs(test_id, branch_id, platform_id, cache):
-    set_persistent_cache(Test.cache_key(test_id, branch_id, platform_id), cache)
+    PersistentCache.set_cache(Test.cache_key(test_id, branch_id, platform_id), cache)
 
 
 def schedule_runs_update(test_id, branch_id, platform_id):
@@ -118,7 +94,7 @@ class CachedRunsHandler(webapp2.RequestHandler):
             branch_id = 0
             platform_id = 0
 
-        runs = get_persistent_cache(Test.cache_key(test_id, branch_id, platform_id))
+        runs = PersistentCache.get_cache(Test.cache_key(test_id, branch_id, platform_id))
         if runs:
             self.response.out.write(runs)
         else:
