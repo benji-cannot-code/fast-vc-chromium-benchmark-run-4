4dd7b3bcefd31f292ae65264a08d4a8641de34ec
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-17 06:42:02 +0000

    [v8] v8 doesn't assume to do 'new WebKitShadowRoot(host)' https://bugs.webkit.org/show_bug.cgi?id=78875
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    Since v8 does not assume that we do 'new WebkitShadowRoot(host)', a wrapper object for new WebKitShadowRoot(host)
    was saved in DOMObject storage instead of DOMNode storage.
    
    CodeGenerator should handle with DOMNode correctly to solve the problem.
    
    Test: fast/dom/shadow/shadow-root-new.html
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateConstructorCallback):
    
    LayoutTests:
    
    Skip non-chromium ports since SHADOW_DOM flag is not enabled in their ports.
    
    * fast/dom/shadow/shadow-root-new.html: Added.
    * fast/dom/shadow/shadow-root-new-expected.txt: Added.
    * platform/efl/Skipped:
    * platform/gtk/Skipped:
    * platform/mac/Skipped:
    * platform/qt/Skipped:
    * platform/win/Skipped:
    * platform/wincairo/Skipped:
    * platform/wk2/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1683,7 +1683,13 @@ END
         push(@implContent, "        goto fail;\n");
     }
 
-    my $DOMObject = $dataNode->extendedAttributes->{"ActiveDOMObject"} ? "ActiveDOMObject" : "DOMObject";
+    my $DOMObject = "DOMObject";
+    if (IsNodeSubType($dataNode)) {
+        $DOMObject = "DOMNode";
+    } elsif ($dataNode->extendedAttributes->{"ActiveDOMObject"}) {
+        $DOMObject = "ActiveDOMObject";
+    }
+
     push(@implContent, <<END);
 
     V8DOMWrapper::setDOMWrapper(wrapper, &info, impl.get());
