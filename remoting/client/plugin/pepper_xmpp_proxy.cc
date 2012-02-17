74a76384c5eca88ae79eb6bc6159a365d634b2c0
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 18:50:52 +0000

    Implement support for messaging interface in the client plugin.
    This adds support for messaging interface in the plugin. The old
    interface (based on ScriptableObject) is still supported.
    
    BUG=88353
    
    Review URL: https://chromiumcodereview.appspot.com/9365067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122540 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_xmpp_proxy.cc
+++ remoting/client/plugin/pepper_xmpp_proxy.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,15 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/message_loop_proxy.h"
-#include "remoting/client/plugin/chromoting_scriptable_object.h"
 
 namespace remoting {
 
 PepperXmppProxy::PepperXmppProxy(
-    base::WeakPtr<ChromotingScriptableObject> scriptable_object,
+    const SendIqCallback& send_iq_callback,
     base::MessageLoopProxy* plugin_message_loop,
     base::MessageLoopProxy* callback_message_loop)
-    : scriptable_object_(scriptable_object),
+    : send_iq_callback_(send_iq_callback),
       plugin_message_loop_(plugin_message_loop),
       callback_message_loop_(callback_message_loop) {
   DCHECK(plugin_message_loop_->BelongsToCurrentThread());
@@ -41,8 +40,7 @@ void PepperXmppProxy::SendIq(const std::string& request_xml) {
     return;
   }
 
-  if (scriptable_object_)
-    scriptable_object_->SendIq(request_xml);
+  send_iq_callback_.Run(request_xml);
 }
 
 void PepperXmppProxy::OnIq(const std::string& response_xml) {
--- remoting/client/plugin/pepper_xmpp_proxy.h
+++ remoting/client/plugin/pepper_xmpp_proxy.h
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef REMOTING_CLIENT_PLUGIN_PEPPER_XMPP_PROXY_H_
 #define REMOTING_CLIENT_PLUGIN_PEPPER_XMPP_PROXY_H_
 
+#include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "remoting/jingle_glue/xmpp_proxy.h"
@@ -16,12 +17,12 @@ class MessageLoopProxy;
 
 namespace remoting {
 
-class ChromotingScriptableObject;
-
 class PepperXmppProxy : public XmppProxy {
  public:
+  typedef base::Callback<void(const std::string&)> SendIqCallback;
+
   PepperXmppProxy(
-      base::WeakPtr<ChromotingScriptableObject> scriptable_object,
+      const SendIqCallback& send_iq_callback,
       base::MessageLoopProxy* plugin_message_loop,
       base::MessageLoopProxy* callback_message_loop);
 
@@ -44,7 +45,7 @@ class PepperXmppProxy : public XmppProxy {
  private:
   virtual ~PepperXmppProxy();
 
-  base::WeakPtr<ChromotingScriptableObject> scriptable_object_;
+  SendIqCallback send_iq_callback_;
 
   scoped_refptr<base::MessageLoopProxy> plugin_message_loop_;
   scoped_refptr<base::MessageLoopProxy> callback_message_loop_;
