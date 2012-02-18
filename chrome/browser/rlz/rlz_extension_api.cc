896fe6cc47a730be02d9a9b714e18d00e213c07f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-18 01:47:25 +0000

    build fix attempt
    BUG=none
    TEST=none
    TBR=apatrick@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9424017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122661 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/rlz/rlz_extension_api.cc
+++ chrome/browser/rlz/rlz_extension_api.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/values.h"
 #include "chrome/common/extensions/extension.h"
-#include "rlz/win/lib/lib_values.h"
+#include "rlz/lib/lib_values.h"
 
 namespace {
 
