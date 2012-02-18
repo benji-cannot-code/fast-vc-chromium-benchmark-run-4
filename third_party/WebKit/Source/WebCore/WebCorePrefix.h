d386df8d7b4ae5b787932a326bf4048c0f757e85
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-18 23:42:03 +0000

    Make WebCore compile with libc++ (Part 1) https://bugs.webkit.org/show_bug.cgi?id=78974
    Reviewed by Anders Carlsson.
    
    Add a workaround for <rdar://problem/10858112>, which cause the standard heap functions
    not to work when using an iterator with proxy objects for reference and pointer types.
    
    * WebCorePrefix.h:
    (move):
    Add an overload of std::move that the heap functions can call successfully.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCorePrefix.h
+++ third_party/WebKit/Source/WebCore/WebCorePrefix.h
@@ -90,6 +90,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifdef __cplusplus
 
+
+#include <ciso646>
+
+#if defined(_LIBCPP_VERSION)
+
+// Add work around for a bug in libc++ that caused standard heap
+// APIs to not compile <rdar://problem/10858112>.
+
+#include <type_traits>
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+template <class _Tp>
+inline _LIBCPP_INLINE_VISIBILITY
+typename enable_if
+<
+    !is_convertible<_Tp, __rv<_Tp> >::value,
+    const _Tp&
+>::type
+move(const _Tp& __t)
+{
+    return __t;
+}
+
+_LIBCPP_END_NAMESPACE_STD
+
+#endif // defined(_LIBCPP_VERSION)
+
 #include <algorithm>
 #include <cstddef>
 #include <new>
