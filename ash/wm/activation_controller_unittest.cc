341d887632954153cec65b0f9fcea6769944f25e
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-18 01:03:06 +0000

    Deactivate active window even if there is no next window to activate
     Deactivated/deleted window was staying active at shutdown, and window being deleted didn't receive OnLostActive.
    
    BUG=114123
    TEST=tbd
    
    
    Review URL: http://codereview.chromium.org/9348115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122650 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/activation_controller_unittest.cc
+++ ash/wm/activation_controller_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -195,19 +195,30 @@ TEST_F(ActivationControllerTest, NotActiveInLostActive) {
   // Activate w1.
   ActivateWindow(w1.get());
   EXPECT_TRUE(IsActiveWindow(w1.get()));
-
+  EXPECT_EQ(1, ad1.activated_count());
   // Should not have gotten a OnLostActive yet.
   EXPECT_EQ(0, ad1.lost_active_count());
 
-  // ActivateWindow(NULL) should not change the active window.
+  // ActivateWindow(NULL) should deactivate the active window.
   ActivateWindow(NULL);
-  EXPECT_TRUE(IsActiveWindow(w1.get()));
+  EXPECT_FALSE(IsActiveWindow(w1.get()));
+  EXPECT_EQ(1, ad1.lost_active_count());
+  EXPECT_FALSE(ad1.window_was_active());
+
+  // Activate w1 again. w1 should have gotten OnActivated.
+  ActivateWindow(w1.get());
+  EXPECT_EQ(2, ad1.activated_count());
+  EXPECT_EQ(1, ad1.lost_active_count());
+
+  // Reset the delegate.
+  ad1.Clear();
 
   // Now activate another window.
   ActivateWindow(w2.get());
 
-  // Should have gotten OnLostActive and w1 should not have been active at that
-  // time.
+  // Should have gotten OnLostActive and w1 shoouldn't have been
+  // active window in OnLostActive.
+  EXPECT_EQ(0, ad1.activated_count());
   EXPECT_EQ(1, ad1.lost_active_count());
   EXPECT_FALSE(ad1.window_was_active());
 }
