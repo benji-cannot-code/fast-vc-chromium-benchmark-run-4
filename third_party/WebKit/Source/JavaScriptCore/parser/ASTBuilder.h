532d3a6a90b3966dc5779d38656d6485f3d4c766
wingo@igalia.com <wingo@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-20 12:49:41 +0000

    2012-02-120  Andy Wingo  <wingo@pobox.com>
    Remove unused features from CodeFeatures
    https://bugs.webkit.org/show_bug.cgi?id=78804
    
    Reviewed by Gavin Barraclough.
    
    * parser/Nodes.h:
    * parser/ASTBuilder.h:
    (JSC::ClosureFeature):
    (JSC::ASTBuilder::createFunctionBody):
    (JSC::ASTBuilder::usesClosures):
    Remove "ClosureFeature".  Since we track captured variables more
    precisely, this bit doesn't do us any good.
    
    (JSC::AssignFeature):
    (JSC::ASTBuilder::makeAssignNode):
    (JSC::ASTBuilder::makePrefixNode):
    (JSC::ASTBuilder::makePostfixNode):
    (JSC::ASTBuilder::usesAssignment):
    Similarly, remove AssignFeature.  It is unused.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/parser/ASTBuilder.h
+++ third_party/WebKit/Source/JavaScriptCore/parser/ASTBuilder.h
@@ -266,7 +266,6 @@ public:
 
     FunctionBodyNode* createFunctionBody(int lineNumber, bool inStrictContext)
     {
-        usesClosures();
         return FunctionBodyNode::create(m_globalData, lineNumber, inStrictContext);
     }
     
@@ -625,9 +624,7 @@ private:
     void incConstants() { m_scope.m_numConstants++; }
     void usesThis() { m_scope.m_features |= ThisFeature; }
     void usesCatch() { m_scope.m_features |= CatchFeature; }
-    void usesClosures() { m_scope.m_features |= ClosureFeature; }
     void usesArguments() { m_scope.m_features |= ArgumentsFeature; }
-    void usesAssignment() { m_scope.m_features |= AssignFeature; }
     void usesWith() { m_scope.m_features |= WithFeature; }
     void usesEval() 
     {
@@ -905,7 +902,6 @@ ExpressionNode* ASTBuilder::makeBinaryNode(int lineNumber, int token, pair<Expre
 
 ExpressionNode* ASTBuilder::makeAssignNode(int lineNumber, ExpressionNode* loc, Operator op, ExpressionNode* expr, bool locHasAssignments, bool exprHasAssignments, int start, int divot, int end)
 {
-    usesAssignment();
     if (!loc->isLocation())
         return new (m_globalData) AssignErrorNode(lineNumber, loc, op, expr, divot, divot - start, end - divot);
 
@@ -943,7 +939,6 @@ ExpressionNode* ASTBuilder::makeAssignNode(int lineNumber, ExpressionNode* loc, 
 
 ExpressionNode* ASTBuilder::makePrefixNode(int lineNumber, ExpressionNode* expr, Operator op, int start, int divot, int end)
 {
-    usesAssignment();
     if (!expr->isLocation())
         return new (m_globalData) PrefixErrorNode(lineNumber, expr, op, divot, divot - start, end - divot);
 
@@ -966,7 +961,6 @@ ExpressionNode* ASTBuilder::makePrefixNode(int lineNumber, ExpressionNode* expr,
 
 ExpressionNode* ASTBuilder::makePostfixNode(int lineNumber, ExpressionNode* expr, Operator op, int start, int divot, int end)
 {
-    usesAssignment();
     if (!expr->isLocation())
         return new (m_globalData) PostfixErrorNode(lineNumber, expr, op, divot, divot - start, end - divot);
 
