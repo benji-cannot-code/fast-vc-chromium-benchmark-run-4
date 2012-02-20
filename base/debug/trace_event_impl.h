8c4ab41cae3e3876c02c7645ab83419eee13f9be
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-20 06:35:15 +0000

    Add EnabledStateObserver to TraceLog as prelude to hooking V8 GC events.
    Review URL: http://codereview.chromium.org/9307002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122730 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.h
+++ base/debug/trace_event_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/hash_tables.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/observer_list.h"
 #include "base/string_util.h"
 #include "base/synchronization/lock.h"
 #include "base/timer.h"
@@ -195,6 +196,25 @@ class BASE_EXPORT TraceLog {
   void SetEnabled(bool enabled);
   bool IsEnabled() { return enabled_; }
 
+  // Enabled state listeners give a callback when tracing is enabled or
+  // disabled. This can be used to tie into other library's tracing systems
+  // on-demand.
+  class EnabledStateChangedObserver {
+   public:
+    // Called just before the tracing system becomes
+    // enabled. TraceLog::IsEnabled will return false at this point and trace
+    // macros and methods called within the observer will deadlock.
+    virtual void OnTraceLogWillEnable() { }
+
+    // Called just before the tracing system disables. TraceLog::IsEnabled is
+    // still false at this point TRACE macros will still be capturing
+    // data. However, trace macros and methods called within the observer will
+    // deadlock.
+    virtual void OnTraceLogWillDisable() { }
+  };
+  void AddEnabledStateObserver(EnabledStateChangedObserver* listener);
+  void RemoveEnabledStateObserver(EnabledStateChangedObserver* listener);
+
   float GetBufferPercentFull() const;
 
   // When enough events are collected, they are handed (in bulk) to
@@ -287,6 +307,8 @@ class BASE_EXPORT TraceLog {
   std::vector<TraceEvent> logged_events_;
   std::vector<std::string> included_categories_;
   std::vector<std::string> excluded_categories_;
+  bool dispatching_to_observer_list_;
+  ObserverList<EnabledStateChangedObserver> enabled_state_observer_list_;
 
   base::hash_map<int, std::string> thread_names_;
 
