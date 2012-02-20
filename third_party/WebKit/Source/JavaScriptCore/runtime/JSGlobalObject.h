6c0be06a9043caed4c6f7dbd0fc7c3c5e0d120c6
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-20 21:14:48 +0000

    Move special __proto__ property to Object.prototype https://bugs.webkit.org/show_bug.cgi?id=78409
    Reviewed by Oliver Hunt.
    
    Re-implement this as a regular accessor property.  This has three key benefits:
    1) It makes it possible for objects to be given properties named __proto__.
    2) Object.prototype.__proto__ can be deleted, preventing object prototypes from being changed.
    3) This largely removes the magic used the implement __proto__, it can just be made a regular accessor property.
    
    Source/JavaScriptCore: 
    
    * parser/Parser.cpp:
    (JSC::::parseFunctionInfo):
        - No need to prohibit functions named __proto__.
    * runtime/JSGlobalObject.cpp:
    (JSC::JSGlobalObject::reset):
        - Add __proto__ accessor to Object.prototype.
    * runtime/JSGlobalObjectFunctions.cpp:
    (JSC::globalFuncProtoGetter):
    (JSC::globalFuncProtoSetter):
        - Definition of the __proto__ accessor functions.
    * runtime/JSGlobalObjectFunctions.h:
        - Declaration of the __proto__ accessor functions.
    * runtime/JSObject.cpp:
    (JSC::JSObject::put):
        - Remove the special handling for __proto__, there is still a check to allow for a fast guard for accessors excluding __proto__.
    (JSC::JSObject::putDirectAccessor):
        - Track on the structure whether an object contains accessors other than one for __proto__.
    (JSC::JSObject::defineOwnProperty):
        - No need to prohibit definition of own properties named __proto__.
    * runtime/JSObject.h:
    (JSC::JSObject::inlineGetOwnPropertySlot):
        - Remove the special handling for __proto__.
    (JSC::JSValue::get):
        - Remove the special handling for __proto__.
    * runtime/JSString.cpp:
    (JSC::JSString::getOwnPropertySlot):
        - Remove the special handling for __proto__.
    * runtime/JSValue.h:
    (JSValue):
        - Made synthesizePrototype public (this may be needed by the __proto__ getter).
    * runtime/ObjectConstructor.cpp:
    (JSC::objectConstructorGetPrototypeOf):
        - Perform the security check & call prototype() directly.
    * runtime/Structure.cpp:
    (JSC::Structure::Structure):
        - Added 'ExcludingProto' variant of the 'hasGetterSetterProperties' state.
    * runtime/Structure.h:
    (JSC::Structure::hasGetterSetterPropertiesExcludingProto):
    (JSC::Structure::setHasGetterSetterProperties):
    (Structure):
        - Added 'ExcludingProto' variant of the 'hasGetterSetterProperties' state.
    
    Source/WebCore: 
    
    * bindings/js/JSDOMWindowBase.cpp:
    (WebCore::JSDOMWindowBase::allowsAccessFrom):
    (WebCore):
        - expose allowsAccessFrom check to JSC.
    * bindings/js/JSDOMWindowBase.h:
    (JSDOMWindowBase):
        - expose allowsAccessFrom check to JSC.
    
    LayoutTests: 
    
    * fast/js/Object-getOwnPropertyNames-expected.txt:
    * fast/js/cyclic-prototypes-expected.txt:
    * fast/js/parser-syntax-check-expected.txt:
    * fast/js/preventExtensions-expected.txt:
    * fast/js/prototypes-expected.txt:
        - Update results
    * fast/js/script-tests/Object-getOwnPropertyNames.js:
        - __proto__ is now a property of Object Prototype.
    * fast/js/script-tests/cyclic-prototypes.js:
        - setting an object's prototype to null removes __proto__ setter, future usage won't set prototype.
    * fast/js/script-tests/parser-syntax-check.js:
        - Allow functions named __proto__
    * fast/js/script-tests/preventExtensions.js:
        - Setting __proto__ should not throw.
    * fast/js/script-tests/prototypes.js:
        - Objects may contained own properties named __proto__, add new test cases.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalObject.h
@@ -57,6 +57,9 @@ namespace JSC {
     typedef Vector<ExecState*, 16> ExecStateStack;
     
     struct GlobalObjectMethodTable {
+        typedef bool (*AllowsAccessFromFunctionPtr)(const JSGlobalObject*, ExecState*);
+        AllowsAccessFromFunctionPtr allowsAccessFrom;
+
         typedef bool (*SupportsProfilingFunctionPtr)(const JSGlobalObject*); 
         SupportsProfilingFunctionPtr supportsProfiling;
 
@@ -280,6 +283,7 @@ namespace JSC {
 
         const GlobalObjectMethodTable* globalObjectMethodTable() const { return m_globalObjectMethodTable; }
 
+        static bool allowsAccessFrom(const JSGlobalObject*, ExecState*) { return true; }
         static bool supportsProfiling(const JSGlobalObject*) { return false; }
         static bool supportsRichSourceInfo(const JSGlobalObject*) { return true; }
 
--- third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSValue.h
@@ -235,6 +235,8 @@ namespace JSC {
 
         char* description();
 
+        JS_EXPORT_PRIVATE JSObject* synthesizePrototype(ExecState*) const;
+
     private:
         template <class T> JSValue(WriteBarrierBase<T>);
 
@@ -247,7 +249,6 @@ namespace JSC {
         JS_EXPORT_PRIVATE JSObject* toObjectSlowCase(ExecState*, JSGlobalObject*) const;
         JS_EXPORT_PRIVATE JSObject* toThisObjectSlowCase(ExecState*) const;
 
-        JS_EXPORT_PRIVATE JSObject* synthesizePrototype(ExecState*) const;
         JSObject* synthesizeObject(ExecState*) const;
 
 #if USE(JSVALUE32_64)
