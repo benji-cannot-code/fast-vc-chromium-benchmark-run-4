3f934bafe0d540dacd788b9259239324b4df8e89
francoisk777@gmail.com <francoisk777@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 11:01:03 +0000

    Removed unused data member from chrome/browser/browser_process_impl.h.
    Review URL: http://codereview.chromium.org/9416008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122813 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.h
+++ chrome/browser/browser_process_impl.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_member.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "ipc/ipc_message.h"
 
 class BrowserOnlineStateObserver;
@@ -241,8 +240,6 @@ class BrowserProcessImpl : public BrowserProcess,
 
   scoped_ptr<ChromeResourceDispatcherHostDelegate>
       resource_dispatcher_host_delegate_;
-
-  content::NotificationRegistrar notification_registrar_;
 
   scoped_refptr<MHTMLGenerationManager> mhtml_generation_manager_;
 
