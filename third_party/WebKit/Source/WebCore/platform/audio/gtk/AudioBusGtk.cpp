6ffa1e10df95c9af78ad1f836d118e01fdb98485
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-21 18:20:06 +0000

    [GTK] WebAudio build is broken https://bugs.webkit.org/show_bug.cgi?id=79113
    Patch by Philippe Normand <pnormand@igalia.com> on 2012-02-21
    Reviewed by Martin Robinson.
    
    * platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp:
    (webKitWebAudioGStreamerChannelPosition): Set a default value for
    the position variable.
    * platform/audio/gtk/AudioBusGtk.cpp: Include CString.h, needed
    for sharedResourcesPath().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/gtk/AudioBusGtk.cpp
+++ third_party/WebKit/Source/WebCore/platform/audio/gtk/AudioBusGtk.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AudioBus.h"
 
 #include "AudioFileReader.h"
+#include "CString.h"
 #include "FileSystem.h"
 #include "GOwnPtr.h"
 
