844fd0d90964ddd69ba1d1d307bb452cc1f5dda0
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 16:53:37 +0000

    Don't tell the compositor that the widget has resized in compact WM
    If the compositor is told that the widget has resized to something quite large, the ui is scaled down to fit. It looks like this is no longer necessary.
    
    BUG=113729
    TEST=start chrome with --aura-window-mode=compact and notice that the ui does not shrink when you open many windows.
    
    
    Review URL: http://codereview.chromium.org/9420030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122832 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/compact_layout_manager.cc
+++ ash/wm/compact_layout_manager.cc
@@ -45,9 +45,8 @@ void AdjustContainerLayerWidth(int delta_width) {
   gfx::Rect bounds(GetDefaultContainerLayer()->bounds());
   bounds.set_width(bounds.width() + delta_width);
   GetDefaultContainerLayer()->SetBounds(bounds);
-  GetDefaultContainerLayer()->GetCompositor()->WidgetSizeChanged(
-      GetDefaultContainerLayer()->bounds().size());
 }
+
 }  // namespace
 
 /////////////////////////////////////////////////////////////////////////////
