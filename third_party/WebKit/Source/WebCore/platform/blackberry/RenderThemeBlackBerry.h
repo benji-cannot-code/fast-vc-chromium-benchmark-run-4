28f9fbe1a659a5eb9bb88cff45a641b8d1b9e6c0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-21 04:17:26 +0000

    [BlackBerry] Need to override active/inactive text search highlight color for RenderThemeBlackBerry https://bugs.webkit.org/show_bug.cgi?id=78920
    Override two functions for text search highlight colors so that we can
    provide platform specified colors.
    
    Patch by Andy Chen <andchen@rim.com> on 2012-02-17
    Reviewed by Antonio Gomes.
    
    No function change so no new tests.
    
    * platform/blackberry/RenderThemeBlackBerry.cpp:
    (WebCore::RenderThemeBlackBerry::platformActiveTextSearchHighlightColor):
    (WebCore::RenderThemeBlackBerry::platformInactiveTextSearchHighlightColor):
    * platform/blackberry/RenderThemeBlackBerry.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/blackberry/RenderThemeBlackBerry.h
+++ third_party/WebKit/Source/WebCore/platform/blackberry/RenderThemeBlackBerry.h
@@ -89,6 +89,10 @@ public:
 
     virtual Color platformActiveSelectionBackgroundColor() const;
 
+    // Highlighting colors for TextMatches.
+    virtual Color platformActiveTextSearchHighlightColor() const;
+    virtual Color platformInactiveTextSearchHighlightColor() const;
+
 private:
     static const String& defaultGUIFont();
 
