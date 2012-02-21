2723d10c7190008c12daa2315285530859a933c2
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 22:00:23 +0000

    Make GetNetworkList on Windows filter out 'down' interfaces, as on POSIX.
    The chrome_frame_net_tests wish to identify a non-loopback adapter to use during testing. Calling GetNetworkList is very convenient but currently on Windows it returns disconnected adapters without providing a means to detect the state of adapters. The POSIX version only returns connected adapters; this CL ports that behaviour to Windows.
    
    BUG=114369
    TEST=existing tests in net_unittests
    
    
    Review URL: http://codereview.chromium.org/9419053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122883 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_util_win.cc
+++ net/base/net_util_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -103,6 +103,10 @@ bool GetNetworkList(NetworkInterfaceList* networks) {
        adapter = adapter->Next) {
     // Ignore the loopback device.
     if (adapter->IfType == IF_TYPE_SOFTWARE_LOOPBACK) {
+      continue;
+    }
+
+    if (adapter->OperStatus != IfOperStatusUp) {
       continue;
     }
 
