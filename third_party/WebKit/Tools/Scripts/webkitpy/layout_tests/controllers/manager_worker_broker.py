966ab4e84673b99e531a2f70b9a65c3865a7600e
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-21 19:28:03 +0000

    webkitpy: prepare to delete message_broker.py https://bugs.webkit.org/show_bug.cgi?id=78997
    Reviewed by Eric Seidel.
    
    This is part 1 of 3 changes to fix bug 78187. This change
    prepares the code for merging message_broker.py into
    manager_worker_broker.py; the tests in message_broker_unittest
    are merged into manager_worker_broker_unittest, the symbols
    needed by the tests are exported from manager_worker_broker.py,
    and manager_worker_broker itself is updated to refer to the
    symbols directly (without the module prefix).
    
    * Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py:
    (get):
    (AbstractWorker):
    (AbstractWorker.__init__):
    (_ManagerConnection):
    (_ManagerConnection.__init__):
    (_WorkerConnection):
    (_WorkerConnection.__init__):
    * Scripts/webkitpy/layout_tests/controllers/manager_worker_broker_unittest.py:
    (InterfaceTest.test_brokerclient_is_abstract):
    (InterfaceTest.test_brokerclient_is_abstract.methods):
    (MessageTest):
    (MessageTest.test__no_body):
    (MessageTest.test__body):
    * Scripts/webkitpy/layout_tests/controllers/message_broker_unittest.py: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py
@@ -39,7 +39,7 @@ They interact more or less like:
   Manager  -->  _InlineManager ---> _InlineWorker <-> Worker
      ^                    \               /              ^
      |                     v             v               |
-     \--------------------  MessageBroker   -------------/
+     \-----------------------  Broker   ----------------/
 """
 
 import logging
@@ -48,10 +48,11 @@ import optparse
 import Queue
 import sys
 
-from webkitpy.layout_tests.controllers import message_broker
+from webkitpy.layout_tests.controllers.message_broker import Broker, BrokerClient, BrokerConnection, _Message
 
 
 _log = logging.getLogger(__name__)
+
 
 #
 # Topic names for Manager <-> Worker messaging
@@ -65,7 +66,7 @@ def get(worker_model, client, worker_class):
 
     Args:
         worker_model - concurrency model to use (inline/processes)
-        client - message_broker.BrokerClient implementation to dispatch
+        client - BrokerClient implementation to dispatch
             replies to.
         worker_class - type of workers to create. This class should override
             the methods in AbstractWorker.
@@ -81,11 +82,11 @@ def get(worker_model, client, worker_class):
     else:
         raise ValueError("unsupported value for --worker-model: %s" % worker_model)
 
-    broker = message_broker.Broker(queue_class)
+    broker = Broker(queue_class)
     return manager_class(broker, client, worker_class)
 
 
-class AbstractWorker(message_broker.BrokerClient):
+class AbstractWorker(BrokerClient):
     def __init__(self, worker_connection, worker_arguments=None):
         """The constructor should be used to do any simple initialization
         necessary, but should not do anything that creates data structures
@@ -97,7 +98,7 @@ class AbstractWorker(message_broker.BrokerClient):
             worker_connection - handle to the BrokerConnection object creating
                 the worker and that can be used for messaging.
             worker_arguments - (optional, Picklable) object passed to the worker from the manager"""
-        message_broker.BrokerClient.__init__(self)
+        BrokerClient.__init__(self)
         self._worker_connection = worker_connection
         self._name = 'worker'
         self._done = False
@@ -139,7 +140,7 @@ class AbstractWorker(message_broker.BrokerClient):
         self._canceled = True
 
 
-class _ManagerConnection(message_broker.BrokerConnection):
+class _ManagerConnection(BrokerConnection):
     def __init__(self, broker, client, worker_class):
         """Base initialization for all Manager objects.
 
@@ -148,8 +149,7 @@ class _ManagerConnection(message_broker.BrokerConnection):
             client: callback object (the caller)
             worker_class: class object to use to create workers.
         """
-        message_broker.BrokerConnection.__init__(self, broker, client,
-            MANAGER_TOPIC, ANY_WORKER_TOPIC)
+        BrokerConnection.__init__(self, broker, client, MANAGER_TOPIC, ANY_WORKER_TOPIC)
         self._worker_class = worker_class
 
     def start_worker(self, worker_arguments=None):
@@ -192,11 +192,10 @@ class _MultiProcessManager(_ManagerConnection):
         return worker_connection
 
 
-class _WorkerConnection(message_broker.BrokerConnection):
+class _WorkerConnection(BrokerConnection):
     def __init__(self, broker, worker_class, worker_arguments=None):
         self._client = worker_class(self, worker_arguments)
-        message_broker.BrokerConnection.__init__(self, broker, self._client,
-                                                 ANY_WORKER_TOPIC, MANAGER_TOPIC)
+        BrokerConnection.__init__(self, broker, self._client, ANY_WORKER_TOPIC, MANAGER_TOPIC)
 
     def name(self):
         return self._client.name()
