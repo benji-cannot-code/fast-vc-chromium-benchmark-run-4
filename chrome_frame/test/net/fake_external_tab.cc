786f585afce7d7a79f050a4bd90e573a834f79b7
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 16:46:57 +0000

    These tests cannot run under chrome_frame_net_tests by design.
    BUG=None
    TEST=chrome_frame_net_tests pass on IE8
    
    
    Review URL: http://codereview.chromium.org/9432001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/net/fake_external_tab.cc
+++ chrome_frame/test/net/fake_external_tab.cc
@@ -285,6 +285,8 @@ void FilterDisabledTests() {
     "HTTPSRequestTest.HTTPSPreloadedHSTSTest",
     "HTTPSRequestTest.ResumeTest",
     "HTTPSRequestTest.SSLSessionCacheShardTest",
+    "HTTPSRequestTest.SSLSessionCacheShardTest",
+    "HTTPSRequestTest.HTTPSErrorsNoClobberTSSTest",
   };
 
   const char* ie9_disabled_tests[] = {
