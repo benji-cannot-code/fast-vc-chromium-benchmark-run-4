5d9149d6855346a4f0277e73b19d633c6a7a646b
marshall@chromium.org <marshall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 20:04:01 +0000

    Forward declare FilePath in gpu_data_manager.h to avoid compile errors. This problem was introduced by revision 122211 changes.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9433001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122860 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/gpu_data_manager.h
+++ content/public/browser/gpu_data_manager.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/common/gpu_feature_type.h"
 
+class FilePath;
+
 namespace base {
 class ListValue;
 }
