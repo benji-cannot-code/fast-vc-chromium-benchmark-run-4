d1f094de88253aabcac63fdc9877ffd9827e9071
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-21 17:17:43 +0000

    Source/WebCore: https://bugs.webkit.org/show_bug.cgi?id=79046
    width of overflow:hidden blocks is wrong when their margins overlap floats.
            
    Relevant discussion is here: http://lists.w3.org/Archives/Public/www-style/2012Feb/0954.html
            
    This patch makes the following changes to how objects shrink when avoiding floats:
    (1) The containing block width used for percentage calculations is now constant. This means that
    an overflow:hidden block with a margin-left of 10% will no longer change what 10% means depending
    on the available line width. From a code perspective, containingBlockLogicalWidthForContent and
    containingBlockLogicalWidthForContentInRegion no longer contain any special-case code for
    shrinkToAvoidFloats() objects.
    
    (2) Now the computation of the logical width for the shrinking object itself handles the adjustments.
    Two kinds of adjustments are made for both the start and end margins. Conceptually what we do is shrink
    the object to the available line width minus our margins, and then expand the width back out to the content edge
    or to the float edge as necessary. If the float fully fits in the margin, then we expand back out to the
    content edge. Otherwise if the float doesn't fit, then we expand only to the float edge, making sure our
    margin overlaps with the float. This expansion is not performed for negative margins, since our result is already
    correct in that case.
            
    Reviewed by Dan Bernstein.
    
    Added the new tests from the thread on www-style about this issue (from Alan Gresley and George Talbot)
    in fast/block/float.
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::containsFloat):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::containsFloats):
    Changed to be const.
    
    (WebCore::RenderBlock::endOffsetForLine):
    (WebCore::RenderBlock::endOffsetForContent):
    Added endOffsetForXXX methods to mirror their startOffset counterparts. Used by the width adjustment code.
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::shrinkLogicalWidthToAvoidFloats):
    New method that handles shrinking the logical width to the line width (and then re-expanding back out to the
    float and/or content edges as needed).
    
    (WebCore::RenderBox::containingBlockLogicalWidthForContent):
    (WebCore::RenderBox::containingBlockLogicalWidthForContentInRegion):
    Patched to no longer do the shrinking of objects to avoid floats.
    
    (WebCore::RenderBox::computeLogicalWidthInRegion):
    (WebCore::RenderBox::computeLogicalWidthInRegionUsing):
    Patched to handle calling the new shrinking code to adjust our logical width.
    
    * rendering/RenderBox.h:
    (RenderBox):
    The signature of computeLogicalWithUsing changed to be region-specific and to carry along enough information
    to efficiently perform the shrinking.
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::computeLogicalWidth):
    Patched to handle calling the new shrinking code to adjust our logical width.
    
    LayoutTests: https://bugs.webkit.org/show_bug.cgi?id=79046
            
    width of overflow:hidden blocks is wrong when their margins overlap floats.
    
    Added new tests from www-style thread: http://lists.w3.org/Archives/Public/www-style/2012Feb/0954.html
    
    Reviewed by Dan Bernstein.
    
    * fast/block/float/centered-float-avoidance-complexity.html: Added.
    * fast/block/float/shrink-to-avoid-float-complexity.html: Added.
    * platform/mac/fast/block/float/centered-float-avoidance-complexity-expected.png: Added.
    * platform/mac/fast/block/float/centered-float-avoidance-complexity-expected.txt: Added.
    * platform/mac/fast/block/float/shrink-to-avoid-float-complexity-expected.png: Added.
    * platform/mac/fast/block/float/shrink-to-avoid-float-complexity-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
@@ -123,8 +123,8 @@ public:
     void markPositionedObjectsForLayout();
     virtual void markForPaginationRelayoutIfNeeded();
     
-    bool containsFloats() { return m_floatingObjects && !m_floatingObjects->set().isEmpty(); }
-    bool containsFloat(RenderBox*);
+    bool containsFloats() const { return m_floatingObjects && !m_floatingObjects->set().isEmpty(); }
+    bool containsFloat(RenderBox*) const;
 
     // Versions that can compute line offsets with the region and page offset passed in. Used for speed to avoid having to
     // compute the region all over again when you already know it.
@@ -146,6 +146,11 @@ public:
         return style()->isLeftToRightDirection() ? logicalLeftOffsetForLine(position, firstLine, region, offsetFromLogicalTopOfFirstPage)
             : logicalWidth() - logicalRightOffsetForLine(position, firstLine, region, offsetFromLogicalTopOfFirstPage);
     }
+    LayoutUnit endOffsetForLine(LayoutUnit position, bool firstLine, RenderRegion* region, LayoutUnit offsetFromLogicalTopOfFirstPage) const
+    {
+        return !style()->isLeftToRightDirection() ? logicalLeftOffsetForLine(position, firstLine, region, offsetFromLogicalTopOfFirstPage)
+            : logicalWidth() - logicalRightOffsetForLine(position, firstLine, region, offsetFromLogicalTopOfFirstPage);
+    }
 
     LayoutUnit availableLogicalWidthForLine(LayoutUnit position, bool firstLine) const
     {
@@ -162,6 +167,11 @@ public:
     LayoutUnit startOffsetForLine(LayoutUnit position, bool firstLine) const
     {
         return style()->isLeftToRightDirection() ? logicalLeftOffsetForLine(position, firstLine)
+            : logicalWidth() - logicalRightOffsetForLine(position, firstLine);
+    }
+    LayoutUnit endOffsetForLine(LayoutUnit position, bool firstLine) const
+    {
+        return !style()->isLeftToRightDirection() ? logicalLeftOffsetForLine(position, firstLine)
             : logicalWidth() - logicalRightOffsetForLine(position, firstLine);
     }
 
@@ -327,6 +337,11 @@ public:
         return style()->isLeftToRightDirection() ? logicalLeftOffsetForContent(region, offsetFromLogicalTopOfFirstPage)
             : logicalWidth() - logicalRightOffsetForContent(region, offsetFromLogicalTopOfFirstPage);
     }
+    LayoutUnit endOffsetForContent(RenderRegion* region, LayoutUnit offsetFromLogicalTopOfFirstPage) const
+    {
+        return !style()->isLeftToRightDirection() ? logicalLeftOffsetForContent(region, offsetFromLogicalTopOfFirstPage)
+            : logicalWidth() - logicalRightOffsetForContent(region, offsetFromLogicalTopOfFirstPage);
+    }
     LayoutUnit logicalLeftOffsetForContent(LayoutUnit blockOffset) const
     {
         return logicalLeftOffsetForContent(regionAtBlockOffset(blockOffset), offsetFromLogicalTopOfFirstPage());
@@ -343,9 +358,14 @@ public:
     {
         return startOffsetForContent(regionAtBlockOffset(blockOffset), offsetFromLogicalTopOfFirstPage());
     }
+    LayoutUnit endOffsetForContent(LayoutUnit blockOffset) const
+    {
+        return endOffsetForContent(regionAtBlockOffset(blockOffset), offsetFromLogicalTopOfFirstPage());
+    }
     LayoutUnit logicalLeftOffsetForContent() const { return isHorizontalWritingMode() ? borderLeft() + paddingLeft() : borderTop() + paddingTop(); }
     LayoutUnit logicalRightOffsetForContent() const { return logicalLeftOffsetForContent() + availableLogicalWidth(); }
     LayoutUnit startOffsetForContent() const { return style()->isLeftToRightDirection() ? logicalLeftOffsetForContent() : logicalWidth() - logicalRightOffsetForContent(); }
+    LayoutUnit endOffsetForContent() const { return !style()->isLeftToRightDirection() ? logicalLeftOffsetForContent() : logicalWidth() - logicalRightOffsetForContent(); }
     
     void setStaticInlinePositionForChild(RenderBox*, LayoutUnit blockOffset, LayoutUnit inlinePosition);
 
