ad4070e8b5cfdd4ccc4ee15f2fd9d68f2ce83e81
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 22:37:02 +0000

    Help: Also disable the new help page on Mac until we check in the implementation.
    BUG=115123
    TEST=none
    R=csilv
    
    Review URL: https://chromiumcodereview.appspot.com/9372073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2387,7 +2387,7 @@ void Browser::OpenAboutChromeDialog() {
     window_->ShowAboutChromeDialog();
 #endif
   } else {
-#if !defined(OS_WIN)
+#if !defined(OS_WIN) && !defined(OS_MACOSX)
     ShowSingletonTab(GURL(chrome::kChromeUIUberURL));
 #else
     // crbug.com/115123.
