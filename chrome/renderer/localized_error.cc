3e5c3c8a4cf61db913233c1d5a0001112a4e7a0d
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 17:45:33 +0000

    net: remove special case for Kaspersky HTTPS
    Kaspersky released an updated version some months ago and hopefully most users
    have upgraded by now.
    
    BUG=86208
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9419014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/localized_error.cc
+++ chrome/renderer/localized_error.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,9 +38,6 @@ static const char kWeakDHKeyLearnMoreUrl[] =
     "err_ssl_weak_server_ephemeral_dh_key";
 static const char kESETLearnMoreUrl[] =
     "http://kb.eset.com/esetkb/index?page=content&id=SOLN2588";
-static const char kKasperskyLearnMoreUrl[] =
-    "http://support.kaspersky.com/kav2012/settings/options"
-    "?print=true&qid=208284701";
 #if defined(OS_CHROMEOS)
 static const char kAppWarningLearnMoreUrl[] =
     "chrome-extension://honijodknafkokifofgiaalefdiedpko/main.html"
@@ -246,13 +243,6 @@ const LocalizedErrorMap net_error_options[] = {
    IDS_ERRORPAGES_TITLE_LOAD_FAILED,
    IDS_ERRORPAGES_HEADING_ESET_ANTI_VIRUS_SSL_INTERCEPTION,
    IDS_ERRORPAGES_SUMMARY_ESET_ANTI_VIRUS_SSL_INTERCEPTION,
-   IDS_ERRORPAGES_DETAILS_SSL_PROTOCOL_ERROR,
-   SUGGEST_LEARNMORE,
-  },
-  {net::ERR_KASPERSKY_ANTI_VIRUS_SSL_INTERCEPTION,
-   IDS_ERRORPAGES_TITLE_LOAD_FAILED,
-   IDS_ERRORPAGES_HEADING_KASPERSKY_ANTI_VIRUS_SSL_INTERCEPTION,
-   IDS_ERRORPAGES_SUMMARY_KASPERSKY_ANTI_VIRUS_SSL_INTERCEPTION,
    IDS_ERRORPAGES_DETAILS_SSL_PROTOCOL_ERROR,
    SUGGEST_LEARNMORE,
   },
@@ -628,9 +618,6 @@ void LocalizedError::GetStrings(const WebKit::WebURLError& error,
         break;
       case net::ERR_ESET_ANTI_VIRUS_SSL_INTERCEPTION:
         learn_more_url = GURL(kESETLearnMoreUrl);
-        break;
-      case net::ERR_KASPERSKY_ANTI_VIRUS_SSL_INTERCEPTION:
-        learn_more_url = GURL(kKasperskyLearnMoreUrl);
         break;
       default:
         break;
