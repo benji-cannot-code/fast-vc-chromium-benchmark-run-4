7758a58c356a5dbdaabac9a1a33f8302b12c95de
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-21 15:52:06 +0000

    Use WTF::cryptographicallyRandomValues in FileSystemWin.cpp https://bugs.webkit.org/show_bug.cgi?id=79089
    Reviewed by Adam Roben.
    
    Avoid loading and unloading of the crypto library during every
    call to openTemporaryFile() and make the code easier.
    
    * platform/win/FileSystemWin.cpp:
    (WebCore::openTemporaryFile):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/FileSystemWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/win/FileSystemWin.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "NotImplemented.h"
 #include "PathWalker.h"
+#include <wtf/CryptographicallyRandomNumber.h>
 #include <wtf/HashMap.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/WTFString.h>
@@ -221,16 +222,11 @@ String openTemporaryFile(const String&, PlatformFileHandle& handle)
     if (tempPathLength <= 0 || tempPathLength > WTF_ARRAY_LENGTH(tempPath))
         return String();
 
-    HCRYPTPROV hCryptProv = 0;
-    if (!CryptAcquireContext(&hCryptProv, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
-        return String();
-
     String proposedPath;
     do {
         wchar_t tempFile[] = L"XXXXXXXX.tmp"; // Use 8.3 style name (more characters aren't helpful due to 8.3 short file names)
         const int randomPartLength = 8;
-        if (!CryptGenRandom(hCryptProv, randomPartLength * sizeof(wchar_t), reinterpret_cast<BYTE*>(tempFile)))
-            break;
+        cryptographicallyRandomValues(tempFile, randomPartLength * sizeof(wchar_t));
 
         // Limit to valid filesystem characters, also excluding others that could be problematic, like punctuation.
         // don't include both upper and lowercase since Windows file systems are typically not case sensitive.
@@ -247,8 +243,6 @@ String openTemporaryFile(const String&, PlatformFileHandle& handle)
         // use CREATE_NEW to avoid overwriting an existing file with the same name
         handle = ::CreateFileW(proposedPath.charactersWithNullTermination(), GENERIC_READ | GENERIC_WRITE, 0, 0, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, 0);
     } while (!isHandleValid(handle) && GetLastError() == ERROR_ALREADY_EXISTS);
-
-    CryptReleaseContext(hCryptProv, 0);
 
     if (!isHandleValid(handle))
         return String();
