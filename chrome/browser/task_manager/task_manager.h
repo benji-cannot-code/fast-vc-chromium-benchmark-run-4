a76d69acf06cc12e4e90cb4d003f0ab6e67b6d23
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-21 04:14:17 +0000

    WebUI TaskManager: Make the initialization of model (in browser process) and UI (in render process) paralleled.
    - Moves the sending of 'enableTaskManager' command to the first of the loading.
    - Adds the "onReadyPeriodicalUpdated" callback. It is fired when the last of initialize of task manager model. On JS side, the list is refreshed forcibly by this event.
    - Separates the code of initialization of task manager from main.js into preload.js and defines.js. 
    - Moves the code of sending command from main.js to commands.js.
    
    BUG=113939
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9388001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122794 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager.h
+++ chrome/browser/task_manager/task_manager.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -254,6 +254,11 @@ class TaskManagerModelObserver {
 
   // Invoked when a range of items has been removed.
   virtual void OnItemsRemoved(int start, int length) = 0;
+
+  // Invoked when the initialization of the model has been finished and
+  // periodical updates is started. The first periodical update will be done
+  // in a few seconds. (depending on platform)
+  virtual void OnReadyPeriodicalUpdate() {}
 };
 
 // The model that the TaskManager is using.
