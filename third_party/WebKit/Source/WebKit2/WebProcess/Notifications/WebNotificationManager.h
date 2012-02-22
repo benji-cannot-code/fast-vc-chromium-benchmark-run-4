c903268268894d2e3aaebe90e059eee6c78ef2a6
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 23:45:15 +0000

    [WK2] Clearing notifications does not clean up internal state https://bugs.webkit.org/show_bug.cgi?id=78861 <rdar://problem/10881167>
    Reviewed by Anders Carlsson.
    
    * WebProcess/Notifications/WebNotificationManager.cpp:
    (WebKit::WebNotificationManager::clearNotifications): This function was missing removing the notification
    entries from the other maps.
    (WebKit::WebNotificationManager::didDestroyNotification): When the notification is destroyed, it should
    also be removed from the context map.
    (WebKit::WebNotificationManager::didCloseNotifications): Refactor to pull out the code that removes
    a provided notification from the context map.
    (WebKit::WebNotificationManager::removeNotificationFromContextMap): Find the notification in the map, and
    remove it. If the map is empty, get rid of the entry.
    * WebProcess/Notifications/WebNotificationManager.h:
    (WebNotificationManager):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108566 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Notifications/WebNotificationManager.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Notifications/WebNotificationManager.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -78,6 +78,10 @@ private:
     void didCloseNotifications(const Vector<uint64_t>& notificationIDs);
     void didUpdateNotificationDecision(const String& originString, bool allowed);
     void didRemoveNotificationDecisions(const Vector<String>& originStrings);
+    
+#if ENABLE(NOTIFICATIONS)
+    void removeNotificationFromContextMap(uint64_t notificationID, WebCore::Notification*);
+#endif
 
     WebProcess* m_process;
 
