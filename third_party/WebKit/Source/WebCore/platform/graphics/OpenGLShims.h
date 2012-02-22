b39d573d48b88ac0efe5e841cf6aadf910c12bec
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 12:56:57 +0000

    [Qt][WK2] Fix the N9 build https://bugs.webkit.org/show_bug.cgi?id=79101
    Reviewed by Simon Hausmann.
    
    Fixed the include order of the rolled out r108359 not to break
    the build with CONFIG+=force_static_libs_as_shared.
    
    * platform/graphics/OpenGLShims.h: Add missing include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/OpenGLShims.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/OpenGLShims.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if PLATFORM(QT)
 #include <qglobal.h>
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#include <qopenglfunctions.h>
 #include <QOpenGLContext>
 #include <QSurface>
 #else
