6869af73dca372c031baacd63bc33ac3c1829e07
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 09:36:29 +0000

    Adds support for 96kHz output sample rate in WebRTC.
    BUG=114396
    TEST=content_unittests.exe
    
    
    Review URL: http://codereview.chromium.org/9355040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123011 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_device_unittest.cc
+++ content/renderer/media/webrtc_audio_device_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,7 +79,6 @@ bool IsRunningHeadless() {
   return false;
 }
 
-// The WebRTC audio client only supports 44.1 and 48.0 kHz.
 // This method returns false if a non-supported rate is detected on the
 // input or output side.
 // TODO(henrika): add support for automatic fallback to Windows Wave audio
@@ -92,9 +91,10 @@ bool HardwareSampleRatesAreValid() {
   int input_sample_rate =
       static_cast<int>(audio_hardware::GetInputSampleRate());
   bool rates_are_valid =
-      ((output_sample_rate == 44100 || output_sample_rate == 48000) &&
-       (input_sample_rate == 44100 || input_sample_rate == 48000 ||
-        input_sample_rate == 16000 || input_sample_rate == 32000));
+      ((output_sample_rate == 96000 || output_sample_rate == 48000 ||
+        output_sample_rate == 44100) &&
+       (input_sample_rate == 48000 || input_sample_rate == 44100 ||
+        input_sample_rate == 32000 || input_sample_rate == 16000));
   DLOG_IF(WARNING, !rates_are_valid) << "Non-supported sample rate detected.";
   return rates_are_valid;
 }
