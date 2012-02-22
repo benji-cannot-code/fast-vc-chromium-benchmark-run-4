d1bda28ea2bec4d89cb6e00e7d2d2b189560e9c7
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 08:04:18 +0000

    While calling IO methods on accepted TCP sockets we run into problem that AssertEventNotSignaled is triggered in TCPClientSocketWin::{Read|Write} methods. This CL fixes this issue and adds test.
    BUG=99602
    TEST=TCPServerSocketTest
    
    Review URL: https://chromiumcodereview.appspot.com/9413007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122998 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/tcp_client_socket_win.cc
+++ net/socket/tcp_client_socket_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -257,6 +257,9 @@ TCPClientSocketWin::Core::Core(
       slow_start_throttle_(kInitialSlowStartThrottle) {
   memset(&read_overlapped_, 0, sizeof(read_overlapped_));
   memset(&write_overlapped_, 0, sizeof(write_overlapped_));
+
+  read_overlapped_.hEvent = WSACreateEvent();
+  write_overlapped_.hEvent = WSACreateEvent();
 }
 
 TCPClientSocketWin::Core::~Core() {
@@ -343,7 +346,10 @@ int TCPClientSocketWin::AdoptSocket(SOCKET socket) {
     return MapSystemError(error);
 
   socket_ = socket;
+  SetNonBlocking(socket_);
+
   core_ = new Core(this);
+
   current_ai_ = addresses_.head();
   use_history_.set_was_ever_connected();
 
@@ -475,14 +481,9 @@ int TCPClientSocketWin::DoConnect() {
 
   DCHECK(!core_);
   core_ = new Core(this);
-
-  // WSACreateEvent creates a manual-reset event object.
-  core_->read_overlapped_.hEvent = WSACreateEvent();
   // WSAEventSelect sets the socket to non-blocking mode as a side effect.
   // Our connect() and recv() calls require that the socket be non-blocking.
   WSAEventSelect(socket_, core_->read_overlapped_.hEvent, FD_CONNECT);
-
-  core_->write_overlapped_.hEvent = WSACreateEvent();
 
   connect_start_time_ = base::TimeTicks::Now();
   if (!connect(socket_, ai->ai_addr, static_cast<int>(ai->ai_addrlen))) {
