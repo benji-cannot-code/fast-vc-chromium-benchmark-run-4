6e2882e266367955b79cf34d88360f608de4d879
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 23:05:54 +0000

    Updated home screen to match new UI mocks.
    BUG=115350
    TEST=Home screen looks shinier. Rest of UI still works, but may look a bit funky for a while.
    
    Review URL: https://chromiumcodereview.appspot.com/9385040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123122 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_list.js
+++ remoting/webapp/host_list.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -198,7 +198,6 @@ remoting.HostList.prototype.showOrHide_ = function(show) {
   var parent = /** @type {Element} */ (this.table_.parentNode);
   parent.hidden = !show;
   if (show) {
-    parent.style.height = parent.scrollHeight + 'px';
     removeClass(parent, remoting.HostList.COLLAPSED_);
   } else {
     addClass(parent, remoting.HostList.COLLAPSED_);
