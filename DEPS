benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit d42fb40ddaae2b58216d51bb03bbf3c59ad0e9a2

Pull in latest version of V8 to fix issues with dependencies on v8_snapshot and preparsing.
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1220 0039d316-1c4b-4281-b951-d872f2087c98

DEPS
@@ -16,7 +16,7 @@ deps = {
     "svn://chrome-svn/chrome/trunk/deps/third_party/icu38@1197",
 
   "src/v8":
-    "https://svn/r/googleclient/v8/trunk@131081",
+    "https://svn/r/googleclient/v8/trunk@131747",
 
   "src/webkit/data/layout_tests/LayoutTests":
     "http://svn.webkit.org/repository/webkit/branches/Safari-3-1-branch/LayoutTests@31256",

commit 2ff67737bcf18be65d289c5f8b14b1a7e81cbeff

roll gyp 1212:1223
1213: ninja: Add a normpath() call, fix copy rules on windows
1214: ninja: Fix test/product broken at r1211 (also fixes crbug.com/115049)
1215: ninja: Fix quoting shell args on Windows
1216: make action_dependencies test win32 and c89 friendly (test-only)
1217: Revert normpath() part of r1213
1218: Quote arguments properly in rules for cmd on Windows ninja.
1219: ninja/win: Fix obj extension for test/ninja/chained-dependency (test-only)
1220: Exclude test/assembly on Windows (test-only)
1221: ninja/win: Allow override of OS variable on Windows ninja.
1222: Fix indent on test code for test/assembly broken in r1220 (test-only)
1223: mac ninja&make: Filter out useless 'This view is clipping its content' message.

BUG=115049
TEST=`ninja All` starts building
TBR=scottmg

Review URL: http://codereview.chromium.org/9431006

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122922 0039d316-1c4b-4281-b951-d872f2087c98

DEPS
@@ -113,7 +113,7 @@ deps = {
     (Var("googlecode_url") % "grit-i18n") + "/trunk@9",
 
   "src/tools/gyp":
-    (Var("googlecode_url") % "gyp") + "/trunk@1212",
+    (Var("googlecode_url") % "gyp") + "/trunk@1222",
 
   "src/v8":
     (Var("googlecode_url") % "v8") + "/trunk@" + Var("v8_revision"),