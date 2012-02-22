462960aff526ae717d0ba35a2e2c793797c07eb9
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 13:10:09 +0000

    Remove unused class declaration in WebCore/page/scrolling/ScrollingCoordinator.h https://bugs.webkit.org/show_bug.cgi?id=79175
    Patch by Changhun Kang <temoochin@company100.net> on 2012-02-22
    Reviewed by Kentaro Hara.
    
    * page/scrolling/ScrollingCoordinator.h:
    Remove PlatformGestureEvent class declaration.
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/scrolling/ScrollingCoordinator.h
+++ third_party/WebKit/Source/WebCore/page/scrolling/ScrollingCoordinator.h
@@ -55,10 +55,6 @@ class ScrollingTreeState;
 class ScrollingTree;
 #endif
 
-#if ENABLE(GESTURE_EVENTS)
-class PlatformGestureEvent;
-#endif
-
 class ScrollingCoordinator : public ThreadSafeRefCounted<ScrollingCoordinator> {
 public:
     static PassRefPtr<ScrollingCoordinator> create(Page*);
