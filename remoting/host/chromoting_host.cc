a73389fd887b2ded7dcd270023685f9f564b8370
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 01:44:25 +0000

    Switch host to the new signaling code.
    BUG=110485
    
    
    Review URL: http://codereview.chromium.org/9365025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122943 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host.cc
+++ remoting/host/chromoting_host.cc
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/client_stub.h"
 #include "remoting/protocol/host_stub.h"
 #include "remoting/protocol/input_stub.h"
-#include "remoting/protocol/jingle_session_manager.h"
+#include "remoting/protocol/libjingle_transport_factory.h"
+#include "remoting/protocol/pepper_session_manager.h"
 #include "remoting/protocol/session_config.h"
 
 using remoting::protocol::ConnectionToClient;
@@ -68,8 +69,10 @@ void ChromotingHost::Start() {
   state_ = kStarted;
 
   // Create and start session manager.
+  scoped_ptr<protocol::TransportFactory> transport_factory(
+      new protocol::LibjingleTransportFactory());
   session_manager_.reset(
-      new protocol::JingleSessionManager(context_->network_message_loop()));
+      new protocol::PepperSessionManager(transport_factory.Pass()));
   session_manager_->Init(signal_strategy_, this, network_settings_);
 }
 
