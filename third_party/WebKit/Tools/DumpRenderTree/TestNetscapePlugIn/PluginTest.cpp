d6d6d178161fa3951feec92148083a940186d6cf
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 20:51:32 +0000

    Explicitly include unistd.h in TestNetscapePlugIn for GCC 4.7 https://bugs.webkit.org/show_bug.cgi?id=77759
    Patch by Kalev Lember <kalevlember@gmail.com> on 2012-02-22
    Reviewed by Tony Chang.
    
    Fixes build with gcc 4.7.
    
    r100432 added the include only for ANDROID; this changes the ifdef to
    also cover other unix platforms. On Fedora 17 the gcc 4.7 compiler no
    longer implicitly includes unistd.h from standard headers and other
    unix platforms are likely to get gcc 4.7 as well in the future.
    
    * DumpRenderTree/TestNetscapePlugIn/PluginTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108540 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/TestNetscapePlugIn/PluginTest.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/TestNetscapePlugIn/PluginTest.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <assert.h>
 #include <string.h>
 
-#if defined(ANDROID)
+#if defined(XP_UNIX) || defined(ANDROID)
 #include <unistd.h>
 #endif
 
