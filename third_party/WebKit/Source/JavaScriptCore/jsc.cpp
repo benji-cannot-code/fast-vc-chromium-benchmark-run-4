e65669c81b51c081e8db07d9078fcf45338c2660
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 00:49:48 +0000

    Make TypedArrays be available in commandline jsc https://bugs.webkit.org/show_bug.cgi?id=79163
    Reviewed by Gavin Barraclough.
    
    Adds a compile time option to have jsc support a basic implementation
    of the TypedArrays available in WebCore.  This lets us test the typed
    array logic in the JIT witout having to build webcore.
    
    * JSCTypedArrayStubs.h: Added.
    (JSC):
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * jsc.cpp:
    (GlobalObject::finishCreation):
    (GlobalObject):
    (GlobalObject::addConstructableFunction):
    * runtime/JSGlobalData.h:
    (JSGlobalData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jsc.cpp
+++ third_party/WebKit/Source/JavaScriptCore/jsc.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InitializeThreading.h"
 #include "Interpreter.h"
 #include "JSArray.h"
+#include "JSCTypedArrayStubs.h"
 #include "JSFunction.h"
 #include "JSLock.h"
 #include "JSString.h"
@@ -194,6 +195,17 @@ protected:
         addFunction(globalData, "setSamplingFlags", functionSetSamplingFlags, 1);
         addFunction(globalData, "clearSamplingFlags", functionClearSamplingFlags, 1);
 #endif
+        
+#if ENABLE(COMMANDLINE_TYPEDARRAYS)
+        addConstructableFunction(globalData, "Uint8Array", constructJSUint8Array, 1);
+        addConstructableFunction(globalData, "Uint16Array", constructJSUint16Array, 1);
+        addConstructableFunction(globalData, "Uint32Array", constructJSUint32Array, 1);
+        addConstructableFunction(globalData, "Int8Array", constructJSInt8Array, 1);
+        addConstructableFunction(globalData, "Int16Array", constructJSInt16Array, 1);
+        addConstructableFunction(globalData, "Int32Array", constructJSInt32Array, 1);
+        addConstructableFunction(globalData, "Float32Array", constructJSFloat32Array, 1);
+        addConstructableFunction(globalData, "Float64Array", constructJSFloat64Array, 1);
+#endif
 
         JSObject* array = constructEmptyArray(globalExec());
         for (size_t i = 0; i < arguments.size(); ++i)
@@ -205,6 +217,12 @@ protected:
     {
         Identifier identifier(globalExec(), name);
         putDirect(globalData, identifier, JSFunction::create(globalExec(), this, arguments, identifier, function));
+    }
+    
+    void addConstructableFunction(JSGlobalData& globalData, const char* name, NativeFunction function, unsigned arguments)
+    {
+        Identifier identifier(globalExec(), name);
+        putDirect(globalData, identifier, JSFunction::create(globalExec(), this, arguments, identifier, function, function));
     }
 };
 COMPILE_ASSERT(!IsInteger<GlobalObject>::value, WTF_IsInteger_GlobalObject_false);
