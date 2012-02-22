dea3f1cc12927796f139b4262226b45973e67a98
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 16:37:55 +0000

    Remove hard-coded source ids from net_log_source_type_list.h, as they're not needed.
    R=eroman@chromium.org
    BUG=114611
    
    Review URL: http://codereview.chromium.org/9317059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123039 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_log.h
+++ net/base/net_log.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -37,6 +36,7 @@ class NET_EXPORT NetLog {
 #define EVENT_TYPE(label) TYPE_ ## label,
 #include "net/base/net_log_event_type_list.h"
 #undef EVENT_TYPE
+    EVENT_COUNT
   };
 
   // The 'phase' of an event trace (whether it marks the beginning or end
@@ -49,9 +49,10 @@ class NET_EXPORT NetLog {
 
   // The "source" identifies the entity that generated the log message.
   enum SourceType {
-#define SOURCE_TYPE(label, value) SOURCE_ ## label = value,
+#define SOURCE_TYPE(label) SOURCE_ ## label,
 #include "net/base/net_log_source_type_list.h"
 #undef SOURCE_TYPE
+    SOURCE_COUNT
   };
 
   // Identifies the entity that generated this log. The |id| field should
@@ -188,11 +189,16 @@ class NET_EXPORT NetLog {
   // Returns a C-String symbolic name for |event_type|.
   static const char* EventTypeToString(EventType event_type);
 
-  // Returns a list of all the available EventTypes.
-  static std::vector<EventType> GetAllEventTypes();
+  // Returns a dictionary that maps event type symbolic names to their enum
+  // values.  Caller takes ownership of the returned Value.
+  static base::Value* GetEventTypesAsValue();
 
   // Returns a C-String symbolic name for |source_type|.
   static const char* SourceTypeToString(SourceType source_type);
+
+  // Returns a dictionary that maps source type symbolic names to their enum
+  // values.  Caller takes ownership of the returned Value.
+  static base::Value* GetSourceTypesAsValue();
 
   // Returns a C-String symbolic name for |event_phase|.
   static const char* EventPhaseToString(EventPhase event_phase);
