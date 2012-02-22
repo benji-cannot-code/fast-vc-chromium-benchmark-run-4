d8f832a26feab20c5b84351062c10a47bc81b211
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 20:04:59 +0000

    Web Inspector: [Regression] network worker tests crash on qt. https://bugs.webkit.org/show_bug.cgi?id=79263
    Reviewed by Pavel Feldman.
    
    * inspector/InspectorPageAgent.cpp:
    (WebCore::InspectorPageAgent::createDecoder):
    (WebCore::InspectorPageAgent::cachedResourceContent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -145,7 +145,8 @@ PassRefPtr<TextResourceDecoder> InspectorPageAgent::createDecoder(const String& 
     else if (DOMImplementation::isXMLMIMEType(mimeType)) {
         decoder = TextResourceDecoder::create("application/xml");
         decoder->useLenientXMLDecoding();
-    }
+    } else
+        decoder = TextResourceDecoder::create("text/plain", "UTF-8");
     return decoder;
 }
 
@@ -182,6 +183,8 @@ bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, S
             return true;
         case CachedResource::RawResource: {
             SharedBuffer* buffer = cachedResource->data();
+            if (!buffer)
+                return false;
             RefPtr<TextResourceDecoder> decoder = InspectorPageAgent::createDecoder(cachedResource->response().mimeType(), cachedResource->response().textEncodingName());
             String content = decoder->decode(buffer->data(), buffer->size());
             content += decoder->flush();
