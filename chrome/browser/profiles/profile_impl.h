7a2105fe0e7cfd4b9df96b37b0f0b95481f6892f
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 17:41:11 +0000

    Restore multiple profiles after a crash.
    All profiles display a crash infobar. The user can restore the
    profiles independently by clicking "Restore" on the info bar.
    
    BUG=112261,107495
    TEST=BrowserInitTest.ProfilesLaunchedAfterCrash
    
    Review URL: http://codereview.chromium.org/9373024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/timer.h"
@@ -142,6 +143,7 @@ class ProfileImpl : public Profile,
 
  private:
   friend class Profile;
+  FRIEND_TEST_ALL_PREFIXES(BrowserInitTest, ProfilesLaunchedAfterCrash);
 
   ProfileImpl(const FilePath& path,
               Profile::Delegate* delegate);
