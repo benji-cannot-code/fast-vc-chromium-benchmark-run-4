3fe9b9b73cda431369e5b32400eaf79afc30368d
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 00:46:01 +0000

    Fix references to HistoryExtensionApiTest in comments (the test was renamed at some point).
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9429016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/history/search_after_add.js
+++ chrome/test/data/extensions/api_test/history/search_after_add.js
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // History api test for Chrome.
-// browser_tests.exe --gtest_filter=ExtensionHistoryApiTest.SearchAfterAdd
+// browser_tests.exe --gtest_filter=HistoryExtensionApiTest.SearchAfterAdd
 
 // runHistoryTestFns is defined in ./common.js .
 runHistoryTestFns([
