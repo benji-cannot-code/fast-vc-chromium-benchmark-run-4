13967c8b76543fea71ada96dc7c6f61e96fe4fee
yael.aharon@nokia.com <yael.aharon@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 04:02:26 +0000

    [Texmap] Move all TextureMapper related files to graphics/texmap https://bugs.webkit.org/show_bug.cgi?id=79141
    Reviewed by Martin Robinson.
    
    Move TextureMaooerGL and TextureMapperShaderManager files to graphics/texmap.
    No new tests.
    
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * platform/graphics/qt/GraphicsContext3DQt.cpp
    * platform/graphics/opengl/TextureMapperGL.cpp: Removed.
    * platform/graphics/opengl/TextureMapperGL.h: Removed.
    * platform/graphics/opengl/TextureMapperShaderManager.cpp: Removed.
    * platform/graphics/opengl/TextureMapperShaderManager.h: Removed.
    * platform/graphics/texmap/TextureMapperGL.cpp: Copied from Source/WebCore/platform/graphics/opengl/TextureMapperGL.cpp.
    * platform/graphics/texmap/TextureMapperGL.h: Copied from Source/WebCore/platform/graphics/opengl/TextureMapperGL.h.
    * platform/graphics/texmap/TextureMapperShaderManager.cpp: Copied from Source/WebCore/platform/graphics/opengl/TextureMapperShaderManager.cpp.
    * platform/graphics/texmap/TextureMapperShaderManager.h: Copied from Source/WebCore/platform/graphics/opengl/TextureMapperShaderManager.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/text/CString.h>
 
 #if USE(ACCELERATED_COMPOSITING) && USE(TEXTURE_MAPPER) && USE(TEXTURE_MAPPER_GL)
-#include <opengl/TextureMapperGL.h>
+#include <texmap/TextureMapperGL.h>
 #endif
 
 #if ENABLE(WEBGL)
