60494ba5d319aa5332533caf2d8c916fa2cbce33
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 17:04:42 +0000

    cleanup: Remove InputMethodManager::defer_ime_startup_.
    The latest InputMethodManager does not start ibus-daemon on login screen even when defer_ime_startup_ is false.
    
    BUG=None
    TEST=try
    
    
    Review URL: http://codereview.chromium.org/9395083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123044 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_manager_browsertest.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_browsertest.cc
@@ -18,7 +18,6 @@ class InputMethodManagerTest : public InProcessBrowserTest {
 
  protected:
   virtual void SetUpOnMainThread() OVERRIDE {
-    manager_->SetDeferImeStartup(true);
     manager_->SetEnableAutoImeShutdown(true);
   }
   virtual void CleanUpOnMainThread() OVERRIDE {
