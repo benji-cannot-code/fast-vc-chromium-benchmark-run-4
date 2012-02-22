ad3ebf4f4a2d8f3ddc9d64b344764d5b60dc9d7c
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 01:00:31 +0000

    Mark HistoryExtensionApiTest.Delete as non-flaky on Windows
    BUG=88318
    TEST=HistoryExtensionApiTest.Delete
    
    
    Review URL: http://codereview.chromium.org/9428011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_extension_apitest.cc
+++ chrome/browser/history/history_extension_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,13 +30,7 @@ IN_PROC_BROWSER_TEST_F(HistoryExtensionApiTest, TimedSearch) {
   ASSERT_TRUE(RunExtensionSubtest("history", "timed_search.html")) << message_;
 }
 
-#if defined(OS_WIN)
-// Flakily times out on Win - See http://crbug.com/88318
-#define MAYBE_Delete DISABLED_Delete
-#else
-#define MAYBE_Delete Delete
-#endif
-IN_PROC_BROWSER_TEST_F(HistoryExtensionApiTest, MAYBE_Delete) {
+IN_PROC_BROWSER_TEST_F(HistoryExtensionApiTest, Delete) {
   ASSERT_TRUE(RunExtensionSubtest("history", "delete.html")) << message_;
 }
 
