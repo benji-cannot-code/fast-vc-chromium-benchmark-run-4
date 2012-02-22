254dd4144b82998ade8cd03611e051ba570ef7d8
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 01:56:41 +0000

    Disable DetachedPanelBrowserTest.CheckDetachedPanelProperties. Will fix in another patch.
    BUG=none
    TEST=none
    TBR=jennb
    
    Review URL: https://chromiumcodereview.appspot.com/9430029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122946 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/detached_panel_browsertest.cc
+++ chrome/browser/ui/panels/detached_panel_browsertest.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class DetachedPanelBrowserTest : public BasePanelBrowserTest {
 };
 
-IN_PROC_BROWSER_TEST_F(DetachedPanelBrowserTest, CheckDetachedPanelProperties) {
+IN_PROC_BROWSER_TEST_F(DetachedPanelBrowserTest,
+                       DISABLED_CheckDetachedPanelProperties) {
   PanelManager* panel_manager = PanelManager::GetInstance();
   DetachedPanelStrip* detached_strip = panel_manager->detached_strip();
 
