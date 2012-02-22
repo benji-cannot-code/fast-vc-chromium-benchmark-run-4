edb092516c4e3f426636e21cf4d3652541ef3ea8
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 18:29:57 +0000

    [cros] Potential fix for weird screen transition loop.
    From the video it seems that screen transition is in a loop when it constantly
    add/removes left/right classes to the screen. Not sure how we could end up in such state.
    
    e.stopPropagation() is nice to have on button handlers.
    I've also tried calling chrome.send() and toggle_step_ multple times but could not trigger same behavior.
    
    BUG=chrome-os-partner:6439
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9430003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/oobe_screen_eula.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_eula.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -54,6 +54,7 @@ cr.define('oobe', function() {
       backButton.textContent = localStrings.getString('back');
       backButton.addEventListener('click', function(e) {
         chrome.send('eulaOnExit', [false, $('usage-stats').checked]);
+        e.stopPropagation();
       });
       buttons.push(backButton);
 
@@ -62,6 +63,7 @@ cr.define('oobe', function() {
       acceptButton.textContent = localStrings.getString('acceptAgreement');
       acceptButton.addEventListener('click', function(e) {
         chrome.send('eulaOnExit', [true, $('usage-stats').checked]);
+        e.stopPropagation();
       });
       buttons.push(acceptButton);
 
--- chrome/browser/resources/chromeos/login/oobe_screen_network.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_network.js
@@ -74,6 +74,7 @@ cr.define('oobe', function() {
       continueButton.textContent = localStrings.getString('continueButton');
       continueButton.addEventListener('click', function(e) {
         chrome.send('networkOnExit', []);
+        e.stopPropagation();
       });
       buttons.push(continueButton);
 
