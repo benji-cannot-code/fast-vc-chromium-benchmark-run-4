e8b3331daa859a208398a55c36c12821a8b96e25
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-22 22:21:14 +0000

    XSSAuditor bypass with <svg> tags and html-entities. https://bugs.webkit.org/show_bug.cgi?id=78836
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Tests: http/tests/security/xssAuditor/iframe-onload-in-svg-tag.html
           http/tests/security/xssAuditor/script-tag-inside-svg-tag.html
           http/tests/security/xssAuditor/script-tag-inside-svg-tag2.html
           http/tests/security/xssAuditor/script-tag-inside-svg-tag3.html
    
    * html/parser/XSSAuditor.cpp:
    (WebCore::isNonCanonicalCharacter):
    (WebCore::isTerminatingCharacter):
    (WebCore):
    (WebCore::startsHTMLCommentAt):
    (WebCore::startsSingleLineCommentAt):
    (WebCore::fullyDecodeString):
    (WebCore::XSSAuditor::XSSAuditor):
    (WebCore::XSSAuditor::init):
    (WebCore::XSSAuditor::filterToken):
    (WebCore::XSSAuditor::filterStartToken):
    (WebCore::XSSAuditor::filterEndToken):
    (WebCore::XSSAuditor::filterCharacterToken):
    (WebCore::XSSAuditor::filterScriptToken):
    (WebCore::XSSAuditor::filterObjectToken):
    (WebCore::XSSAuditor::filterParamToken):
    (WebCore::XSSAuditor::filterEmbedToken):
    (WebCore::XSSAuditor::filterAppletToken):
    (WebCore::XSSAuditor::filterIframeToken):
    (WebCore::XSSAuditor::filterMetaToken):
    (WebCore::XSSAuditor::filterBaseToken):
    (WebCore::XSSAuditor::filterFormToken):
    (WebCore::XSSAuditor::decodedSnippetForAttribute):
    (WebCore::XSSAuditor::snippetForJavaScript):
    * html/parser/XSSAuditor.h:
    (XSSAuditor):
    
    LayoutTests:
    
    * http/tests/security/xssAuditor/iframe-onload-in-svg-tag-expected.txt: Added.
    * http/tests/security/xssAuditor/iframe-onload-in-svg-tag.html: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag-expected.txt: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag.html: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag2-expected.txt: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag2.html: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag3-expected.txt: Added.
    * http/tests/security/xssAuditor/script-tag-inside-svg-tag3.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
+++ third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
@@ -43,10 +43,11 @@ public:
     void filterToken(HTMLToken&);
 
 private:
+    static const size_t kMaximumFragmentLengthTarget = 100;
+
     enum State {
         Uninitialized,
-        Initial,
-        AfterScriptStartTag,
+        Initialized
     };
 
     enum AttributeKind {
@@ -56,9 +57,9 @@ private:
 
     void init();
 
-    bool filterTokenInitial(HTMLToken&);
-    bool filterTokenAfterScriptStartTag(HTMLToken&);
-
+    bool filterStartToken(HTMLToken&);
+    void filterEndToken(HTMLToken&);
+    bool filterCharacterToken(HTMLToken&);
     bool filterScriptToken(HTMLToken&);
     bool filterObjectToken(HTMLToken&);
     bool filterParamToken(HTMLToken&);
@@ -89,6 +90,8 @@ private:
 
     State m_state;
     String m_cachedSnippet;
+    bool m_shouldAllowCDATA;
+    unsigned m_scriptTagNestingLevel;
     bool m_notifiedClient;
 };
 
