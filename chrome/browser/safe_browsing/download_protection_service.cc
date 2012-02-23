cfbc253ee749898539617649e775812689114804
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 20:49:55 +0000

    Change SBClientDownload.CheckDownloadStats REASON_HTTPS_URL histogram meaning.
    Now applies only to binaries from HTTPS, and REASON_NOT_BINARY_FILE applies to
    any non-binary files, regardless of scheme.
    
    BUG=none
    TEST=download binary and non-binary file from https url, check about:histograms
    
    Review URL: https://chromiumcodereview.appspot.com/9453003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123318 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection_service.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -376,8 +376,8 @@ class DownloadProtectionService::CheckClientDownloadRequest
     RecordFileExtensionType(info_.target_file);
 
     if (final_url.SchemeIs("https") || !IsBinaryFile(info_.target_file)) {
-      RecordImprovedProtectionStats(final_url.SchemeIs("https") ?
-                  REASON_HTTPS_URL : REASON_NOT_BINARY_FILE);
+      RecordImprovedProtectionStats(!IsBinaryFile(info_.target_file) ?
+                  REASON_NOT_BINARY_FILE : REASON_HTTPS_URL);
       BrowserThread::PostTask(
           BrowserThread::IO,
           FROM_HERE,
