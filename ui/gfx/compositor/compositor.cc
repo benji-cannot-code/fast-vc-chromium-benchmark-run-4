062be6be9b6e4614aa44a4f484be6aa36f886659
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 18:39:04 +0000

    Suppress painting when closing window * Send damaged rect only when compositor is ready to paint and composite. * Layer::SuppressPaint to discard damaged rect and ignore new paint requests. * Test compositor for linux now use async Draw to avoid infinite loop. I believe this is closer to real environment.
    BUG=111531
    TEST=none
    
    Review URL: http://codereview.chromium.org/9401017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123284 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/compositor/compositor.cc
+++ ui/gfx/compositor/compositor.cc
@@ -171,10 +171,14 @@ void Compositor::Terminate() {
 }
 
 void Compositor::ScheduleDraw() {
-  if (g_compositor_thread)
+  if (g_compositor_thread) {
+    // TODO(nduca): Temporary while compositor calls
+    // compositeImmediately() directly.
+    layout();
     host_.composite();
-  else
+  } else {
     delegate_->ScheduleDraw();
+  }
 }
 
 void Compositor::SetRootLayer(Layer* root_layer) {
@@ -194,6 +198,9 @@ void Compositor::Draw(bool force_clear) {
   if (!root_layer_)
     return;
 
+  // TODO(nduca): Temporary while compositor calls
+  // compositeImmediately() directly.
+  layout();
   host_.composite();
   if (!g_compositor_thread)
     NotifyEnd();
@@ -248,6 +255,8 @@ void Compositor::updateAnimations(double frameBeginTime) {
 }
 
 void Compositor::layout() {
+  if (root_layer_)
+    root_layer_->SendDamagedRect();
 }
 
 void Compositor::applyScrollAndScale(const WebKit::WebSize& scrollDelta,
