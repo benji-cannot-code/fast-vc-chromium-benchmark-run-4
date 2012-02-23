08e492cb51d433b82294b4db6906ace1fb454150
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 14:01:34 +0000

    Only set the Name property for VPN networks.
    Flimflam will complain if we do so for non-VPN networks. While at it,
    simplify base::Value handling in onc_network_parser.cc.
    
    BUG=chromium-os:26371
    TEST=Unit tests, WiFi ONC containing the Name property actually works :)
    
    
    Review URL: http://codereview.chromium.org/9414042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123237 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_parser.cc
+++ chrome/browser/chromeos/cros/network_parser.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -130,7 +130,7 @@ bool NetworkParser::UpdateStatus(const std::string& key,
   PropertyIndex found_index = mapper().Get(key);
   if (index)
     *index = found_index;
-  network->UpdatePropertyMap(found_index, value);
+  network->UpdatePropertyMap(found_index, &value);
   if (!ParseValue(found_index, value, network)) {
     VLOG(1) << "Unhandled key '" << key << "' in Network: " << network->name()
             << " ID: " << network->unique_id()
