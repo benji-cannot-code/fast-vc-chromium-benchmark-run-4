9b59114dcd7875e458c0d54fabf9d75188e7088b
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-23 06:26:07 +0000

    Enable Geolocation bindings for GObject https://bugs.webkit.org/show_bug.cgi?id=79293
    Reviewed by Adam Barth.
    
    This patch adds "WebCore::" to supplemental method calls.
    This will solve the GTK build failure we have observed in the Geolocation API,
    and thus this patch enables the Geolocation API.
    
    * Modules/geolocation/NavigatorGeolocation.idl:
    * bindings/scripts/CodeGeneratorGObject.pm:
    (GenerateProperty):
    (GenerateFunction):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
@@ -390,8 +390,8 @@ sub GenerateProperty {
         push(@setterArguments, "${convertFunction}(g_value_get_$gtype(value))");
         unshift(@getterArguments, "coreSelf");
         unshift(@setterArguments, "coreSelf");
-        $getterFunctionName = "${implementedBy}::$getterFunctionName";
-        $setterFunctionName = "${implementedBy}::$setterFunctionName";
+        $getterFunctionName = "WebCore::${implementedBy}::$getterFunctionName";
+        $setterFunctionName = "WebCore::${implementedBy}::$setterFunctionName";
     } else {
         push(@setterArguments, "${convertFunction}(g_value_get_$gtype(value))");
         $getterFunctionName = "coreSelf->$getterFunctionName";
@@ -927,7 +927,7 @@ EOF
                 my $implementedBy = $function->signature->extendedAttributes->{"ImplementedBy"};
                 $implIncludes{"${implementedBy}.h"} = 1;
                 unshift(@arguments, "item");
-                $functionName = "${implementedBy}::${functionName}";
+                $functionName = "WebCore::${implementedBy}::${functionName}";
             } else {
                 $functionName = "item->${functionName}";
             }
@@ -938,7 +938,7 @@ EOF
                 my $implementedBy = $function->signature->extendedAttributes->{"ImplementedBy"};
                 $implIncludes{"${implementedBy}.h"} = 1;
                 unshift(@arguments, "item");
-                $getterContentHead = "${assign}convertToUTF8String(${implementedBy}::${functionSigName}(" . join(", ", @arguments) . "));\n";
+                $getterContentHead = "${assign}convertToUTF8String(WebCore::${implementedBy}::${functionSigName}(" . join(", ", @arguments) . "));\n";
             } else {
                 $getterContentHead = "${assign}convertToUTF8String(item->${functionSigName}(" . join(", ", @arguments) . "));\n";
             }
@@ -953,7 +953,7 @@ EOF
                 my $implementedBy = $function->signature->extendedAttributes->{"ImplementedBy"};
                 $implIncludes{"${implementedBy}.h"} = 1;
                 unshift(@arguments, "item");
-                $functionName = "${implementedBy}::${functionName}";
+                $functionName = "WebCore::${implementedBy}::${functionName}";
             } else {
                 $functionName = "item->${functionName}";
             }
@@ -965,7 +965,7 @@ EOF
                 my $implementedBy = $function->signature->extendedAttributes->{"ImplementedBy"};
                 $implIncludes{"${implementedBy}.h"} = 1;
                 unshift(@arguments, "item");
-                $functionName = "${implementedBy}::${functionName}";
+                $functionName = "WebCore::${implementedBy}::${functionName}";
                 $contentHead = "${assign}${assignPre}${functionName}(" . join(", ", @arguments) . "${assignPost});\n";
             } else {
                 $functionName = "item->${functionName}";
@@ -977,7 +977,7 @@ EOF
                 my $implementedBy = $function->signature->extendedAttributes->{"ImplementedBy"};
                 $implIncludes{"${implementedBy}.h"} = 1;
                 unshift(@arguments, "item");
-                $contentHead = "${assign}${assignPre}${implementedBy}::${functionSigName}(" . join(", ", @arguments) . "${assignPost});\n";
+                $contentHead = "${assign}${assignPre}WebCore::${implementedBy}::${functionSigName}(" . join(", ", @arguments) . "${assignPost});\n";
             } else {
                 $contentHead = "${assign}${assignPre}item->${functionSigName}(" . join(", ", @arguments) . "${assignPost});\n";
             }
