4c23a54651fd04b68a32b4c1eca29322825aadb4
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 00:27:12 +0000

    [flaky fixlet] Re-enabling SafeBrowsingDatabaseTest.CrashOnceOnly
    BUG=69918,15288
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/9420042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123148 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/web_cache_manager_browsertest.cc
+++ chrome/browser/renderer_host/web_cache_manager_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,8 +23,7 @@ class WebCacheManagerBrowserTest : public InProcessBrowserTest {
 
 // Regression test for http://crbug.com/12362.  If a renderer crashes and the
 // user navigates to another tab and back, the browser doesn't crash.
-// Flaky, http://crbug.com/15288. Disabled, http://crbug.com/69918.
-IN_PROC_BROWSER_TEST_F(WebCacheManagerBrowserTest, DISABLED_CrashOnceOnly) {
+IN_PROC_BROWSER_TEST_F(WebCacheManagerBrowserTest, CrashOnceOnly) {
   const FilePath kTestDir(FILE_PATH_LITERAL("google"));
   const FilePath kTestFile(FILE_PATH_LITERAL("google.html"));
   GURL url(ui_test_utils::GetTestUrl(kTestDir, kTestFile));
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -3045,7 +3045,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'app/chrome_version.rc.version',
             'browser/rlz/rlz_extension_apitest.cc',
             # http://crbug.com/15101 These tests fail on Linux and Mac.
-            'browser/renderer_host/web_cache_manager_browsertest.cc',
             '../content/browser/child_process_security_policy_browsertest.cc',
           ],
         }],
