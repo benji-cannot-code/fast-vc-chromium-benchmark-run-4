df7735911994506cbed4f0637a1b0c4850c6d29d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 18:27:46 +0000

    Disk Cache: Remove the deleted list experiment.
    BUG=79186
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9430030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123279 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/eviction.h
+++ net/disk_cache/eviction.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -76,13 +76,13 @@ class Eviction {
   IndexHeader* header_;
   int max_size_;
   int trim_delays_;
+  int index_size_;
   bool new_eviction_;
   bool first_trim_;
   bool trimming_;
   bool delay_trim_;
   bool init_;
   bool test_mode_;
-  bool in_experiment_;
   base::WeakPtrFactory<Eviction> ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(Eviction);
