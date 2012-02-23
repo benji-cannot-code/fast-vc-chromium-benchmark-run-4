572da089de2b2435f9c9badf1fd8fce66c94901f
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 05:04:50 +0000

    [uber page] Fix subpage Esc bug
    R=estade@chromium.org
    BUG=None (yet), don't believe
    TEST=Press Esc while on advanced page.
    
    Review URL: https://chromiumcodereview.appspot.com/9442012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123194 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options_page.js
+++ chrome/browser/resources/options2/options_page.js
@@ -844,7 +844,7 @@ cr.define('options', function() {
    */
   OptionsPage.keyDownEventHandler_ = function(event) {
     if (event.keyCode == 27 &&  // Esc
-        !this.isOverlayVisible_) {
+        !this.isOverlayVisible_()) {
       this.closeTopSubPage_();
     }
   };
