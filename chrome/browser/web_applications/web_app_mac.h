9853a9f0c1828d96121e4e879d4b9b498c332169
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 21:23:07 +0000

    Add icons support for Mac platform apps
    This change adds support for settings icons for Mac platform apps.
    
    BUG=112651
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9374046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123335 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_mac.h
+++ chrome/browser/web_applications/web_app_mac.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "chrome/browser/shell_integration.h"
 
 namespace web_app {
@@ -38,6 +39,8 @@ class WebAppShortcutCreator {
   bool UpdateIcon(const FilePath& app_path) const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(WebAppShortcutCreatorTest, UpdateIcon);
+
   // Information about the app.
   ShellIntegration::ShortcutInfo info_;
 };
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -4799,6 +4799,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           },
           'dependencies': [
             '../content/content.gyp:closure_blocks_leopard_compat',
+            '../third_party/icon_family/icon_family.gyp:icon_family',
           ],
           'actions': [
             {
