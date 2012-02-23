6b80d34e6f5169e65ccda9ec02d00a1ac48b005a
dslomov@chromium.org <dslomov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 21:56:49 +0000

    Minimize for panels in Aura compact mode
    Review URL: http://codereview.chromium.org/9427024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123340 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/panel_layout_manager.cc
+++ ash/wm/panel_layout_manager.cc
@@ -9,15 +9,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/launcher/launcher.h"
 #include "ash/shell.h"
+#include "ash/wm/property_util.h"
 #include "base/auto_reset.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/size.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
 const int kPanelMarginEdge = 4;
 const int kPanelMarginMiddle = 8;
+
+const int kMinimizedHeight = 24;
+
 const float kMaxHeightFactor = .80f;
 const float kMaxWidthFactor = .50f;
 }
@@ -47,6 +53,34 @@ void PanelLayoutManager::StartDragging(aura::Window* panel) {
 void PanelLayoutManager::FinishDragging() {
   DCHECK(dragged_panel_ != NULL);
   dragged_panel_ = NULL;
+  Relayout();
+}
+
+void PanelLayoutManager::ToggleMinimize(aura::Window* panel) {
+  DCHECK(panel->parent() == panel_container_);
+  if (panel->GetProperty(aura::client::kShowStateKey) ==
+      ui::SHOW_STATE_MINIMIZED) {
+    const gfx::Rect& old_bounds = panel->bounds();
+    panel->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_NORMAL);
+
+    gfx::Rect new_bounds(old_bounds);
+    const gfx::Rect* restore_bounds = GetRestoreBounds(panel);
+    if (restore_bounds != NULL) {
+      new_bounds.set_height(restore_bounds->height());
+      new_bounds.set_y(old_bounds.bottom() - restore_bounds->height());
+      SetChildBounds(panel, new_bounds);
+      ClearRestoreBounds(panel);
+    }
+  } else {
+    const gfx::Rect& old_bounds = panel->bounds();
+    panel->SetProperty(aura::client::kShowStateKey, ui::SHOW_STATE_MINIMIZED);
+    SetRestoreBounds(panel, old_bounds);
+    SetChildBounds(panel,
+                   gfx::Rect(old_bounds.x(),
+                             old_bounds.bottom() - kMinimizedHeight,
+                             old_bounds.width(),
+                             kMinimizedHeight));
+  }
   Relayout();
 }
 
