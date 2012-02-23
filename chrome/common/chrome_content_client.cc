dbb81aa0cf0bf8d8098ee8f376071dad4cb38872
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 00:27:04 +0000

    Grant Flash read access to Talk application directory.
    This makes the Talk plugin work with sandboxed Flash.
    Copied from https://chromiumcodereview.appspot.com/9433016/
    
    BUG=113891
    
    Review URL: http://codereview.chromium.org/9429031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123147 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_split.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
-#include "base/win/windows_version.h"
 #include "chrome/common/child_process_logging.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -32,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "flapper_version.h"  // In <(SHARED_INTERMEDIATE_DIR).
 
 #if defined(OS_WIN)
+#include "base/win/registry.h"
+#include "base/win/windows_version.h"
 #include "sandbox/src/sandbox.h"
 #elif defined(OS_MACOSX)
 #include "chrome/common/chrome_sandbox_type_mac.h"
@@ -393,13 +394,31 @@ bool ChromeContentClient::SandboxPlugin(CommandLine* command_line,
   }
 
   // Add the policy for the pipes.
-  sandbox::ResultCode result = sandbox::SBOX_ALL_OK;
-  result = policy->AddRule(sandbox::TargetPolicy::SUBSYS_NAMED_PIPES,
-                           sandbox::TargetPolicy::NAMEDPIPES_ALLOW_ANY,
-                           L"\\\\.\\pipe\\chrome.*");
-  if (result != sandbox::SBOX_ALL_OK) {
+  if (policy->AddRule(sandbox::TargetPolicy::SUBSYS_NAMED_PIPES,
+                      sandbox::TargetPolicy::NAMEDPIPES_ALLOW_ANY,
+                      L"\\\\.\\pipe\\chrome.*") != sandbox::SBOX_ALL_OK) {
     NOTREACHED();
     return false;
+  }
+
+  // Allow Talk's camera control.
+  base::win::RegKey talk_key(HKEY_CURRENT_USER,
+                             L"Software\\Google\\Google Talk Plugin",
+                             KEY_READ);
+  if (talk_key.Valid()) {
+    string16 install_dir;
+    if (talk_key.ReadValue(L"install_dir", &install_dir) == ERROR_SUCCESS) {
+      if (install_dir[install_dir.size() - 1] != '\\')
+        install_dir.append(L"\\*");
+      else
+        install_dir.append(L"*");
+      // This is not a hard failure because a reparse point in the path can
+      // cause the rule to fail, but we should not abort sandboxing.
+      DCHECK_EQ(policy->AddRule(sandbox::TargetPolicy::SUBSYS_FILES,
+                                sandbox::TargetPolicy::FILES_ALLOW_READONLY,
+                                install_dir.c_str()), sandbox::SBOX_ALL_OK);
+    }
+    talk_key.Close();
   }
 
   // Spawn the flash broker and apply sandbox policy.
