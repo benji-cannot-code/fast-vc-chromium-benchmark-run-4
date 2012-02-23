734ddadde9b22f3838d069e817b47305a6756df6
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 21:56:10 +0000

    Test for DownloadResourceThrottle cancellation of downloads.
    BUG=99083
    TEST=DownloadTest.DownloadResourceThrottleCancels
    
    Review URL: http://codereview.chromium.org/9421007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123338 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_request_limiter.h
+++ chrome/browser/download/download_request_limiter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -184,6 +185,7 @@ class DownloadRequestLimiter
   void OnUserGesture(content::WebContents* tab);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(DownloadTest, DownloadResourceThrottleCancels);
   friend class base::RefCountedThreadSafe<DownloadRequestLimiter>;
   friend class DownloadRequestLimiterTest;
   friend class TabDownloadState;
