1507865e461eed8c20d00315c190a05c8131f4a0
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-23 20:48:35 +0000

    [wx] Build fix, disable LLINT for now and fix ENABLE defines for it.
    git-svn-id: svn://svn.chromium.org/blink/trunk@108661 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/llint/LLIntOffsetsExtractor.cpp
+++ third_party/WebKit/Source/JavaScriptCore/llint/LLIntOffsetsExtractor.cpp
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#if ENABLE(LLLINT)
+
 #include "CodeBlock.h"
 #include "Executable.h"
 #include "Heap.h"
@@ -81,3 +83,5 @@ int main(int, char**)
     printf("%p\n", JSC::LLIntOffsetsExtractor::dummy());
     return 0;
 }
+
+#endif // ENABLE(LLINT)
--- third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
+++ third_party/WebKit/Source/JavaScriptCore/wtf/Platform.h
@@ -643,6 +643,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_JIT 1
 #endif
 #define ENABLE_GLOBAL_FASTMALLOC_NEW 0
+#define ENABLE_LLINT 0
 #if OS(DARWIN)
 #define WTF_USE_CF 1
 #define WTF_USE_CORE_TEXT 1
