a006ff4ce06fd9ec69923136a55b25b97d90b6b1
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 20:18:56 +0000

    Fix the gesture-recognizer to emit scroll-start event at the location of the press.
    BUG=none
    TEST=GestureEventScroll
    
    Review URL: https://chromiumcodereview.appspot.com/9452026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123309 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_recognizer_unittest.cc
+++ ui/aura/gestures/gesture_recognizer_unittest.cc
@@ -53,6 +53,8 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
     pinch_end_ = false;
     long_press_ = false;
 
+    scroll_begin_position_.SetPoint(0, 0);
+
     scroll_x_ = 0;
     scroll_y_ = 0;
   }
@@ -67,6 +69,10 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
   bool pinch_update() const { return pinch_update_; }
   bool pinch_end() const { return pinch_end_; }
   bool long_press() const { return long_press_; }
+
+  const gfx::Point scroll_begin_position() const {
+    return scroll_begin_position_;
+  }
 
   float scroll_x() const { return scroll_x_; }
   float scroll_y() const { return scroll_y_; }
@@ -85,6 +91,7 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
         break;
       case ui::ET_GESTURE_SCROLL_BEGIN:
         scroll_begin_ = true;
+        scroll_begin_position_ = gesture->location();
         break;
       case ui::ET_GESTURE_SCROLL_UPDATE:
         scroll_update_ = true;
@@ -124,6 +131,8 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
   bool pinch_update_;
   bool pinch_end_;
   bool long_press_;
+
+  gfx::Point scroll_begin_position_;
 
   float scroll_x_;
   float scroll_y_;
@@ -367,6 +376,8 @@ TEST_F(GestureRecognizerTest, GestureEventScroll) {
   EXPECT_FALSE(delegate->scroll_end());
   EXPECT_EQ(29, delegate->scroll_x());
   EXPECT_EQ(29, delegate->scroll_y());
+  EXPECT_EQ(gfx::Point(1, 1).ToString(),
+            delegate->scroll_begin_position().ToString());
 
   // Move some more to generate a few more scroll updates.
   SendScrollEvent(root_window(), 110, 211, delegate.get());
