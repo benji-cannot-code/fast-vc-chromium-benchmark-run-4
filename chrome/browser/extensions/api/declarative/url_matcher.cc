973170783bd895612568987c25795fac569674dd
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 22:36:53 +0000

    Fixing "error C2039: 'back_inserter' : is not a member of 'std'" on Windows.
    Review URL: http://codereview.chromium.org/9428057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative/url_matcher.cc
+++ chrome/browser/extensions/api/declarative/url_matcher.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/declarative/url_matcher.h"
 
 #include <algorithm>
+#include <iterator>
 
 #include "base/logging.h"
 #include "googleurl/src/gurl.h"
