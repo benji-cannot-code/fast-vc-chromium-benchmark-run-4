0910c6b876aece491db94149ada079587b8fa7c1
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 17:05:43 +0000

    Don't repaint if plus animation progressed on active tab  This only mitigates the issue. See the bug for more detail.
    BUG=114651
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9429062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123263 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/base_tab.cc
+++ chrome/browser/ui/views/tabs/base_tab.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -488,6 +488,10 @@ void BaseTab::PaintTitle(gfx::Canvas* canvas, SkColor title_color) {
 }
 
 void BaseTab::AnimationProgressed(const ui::Animation* animation) {
+  // Ignore if the pulse animation is being performed on active tab because
+  // it repaints the same image. See |Tab::PaintTabBackground()|.
+  if (animation == pulse_animation_.get() && IsActive())
+    return;
   SchedulePaint();
 }
 
