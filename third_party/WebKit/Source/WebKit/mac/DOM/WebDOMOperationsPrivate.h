006122d3db5273d89ee84409a1dad0bb7c647cd6
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-24 22:46:35 +0000

    <rdar://problem/10805709> and https://bugs.webkit.org/show_bug.cgi?id=79421 Need a WK1 Mac API to filter which subframes go into WebArchives as they are created
    Reviewed by Sam Weinig (with additional comments by Adam Roben)
    
    Add webArchiveByFilteringSubframes: which takes a callback block:
    * DOM/WebDOMOperationsPrivate.h:
    
    Add FrameFilter that adapts the block, and use it to implement the new SPI:
    * DOM/WebDOMOperations.mm:
    (WebFrameFilter):
    (WebFrameFilter::WebFrameFilter):
    (WebFrameFilter::~WebFrameFilter):
    (WebFrameFilter::shouldIncludeSubframe):
    (-[DOMNode webArchiveByFilteringSubframes:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/DOM/WebDOMOperationsPrivate.h
+++ third_party/WebKit/Source/WebKit/mac/DOM/WebDOMOperationsPrivate.h
@@ -48,3 +48,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (NSString *)markupString;
 - (NSRect)_renderRect:(bool *)isReplaced;
 @end
+
+typedef BOOL (^WebArchiveSubframeFilter)(WebFrame* subframe);
+
+@interface DOMNode (WebDOMNodeOperationsPrivate)
+- (WebArchive *)webArchiveByFilteringSubframes:(WebArchiveSubframeFilter)webArchiveSubframeFilter;
+@end
