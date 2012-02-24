21b4f57d82f5cdb0dd440038d1dc57c8fb55c0c2
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 17:35:14 +0000

    Fix emulation of offline mode.
    BUG=chromium:110750
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9454016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_library.cc
+++ chrome/browser/chromeos/cros/network_library.cc
@@ -5116,9 +5116,7 @@ class NetworkLibraryImplStub : public NetworkLibraryImplBase {
 
   virtual void CallRemoveNetwork(const Network* network) OVERRIDE {}
 
-  virtual void EnableOfflineMode(bool enable) OVERRIDE {
-    offline_mode_ = enable;
-  }
+  virtual void EnableOfflineMode(bool enable) OVERRIDE;
 
   virtual NetworkIPConfigVector GetIPConfigs(
       const std::string& device_path,
@@ -5674,6 +5672,14 @@ void NetworkLibraryImplStub::DisconnectFromNetwork(const Network* network) {
     active_virtual_ = NULL;
   SignalNetworkManagerObservers();
   NotifyNetworkChanged(network);
+}
+
+void NetworkLibraryImplStub::EnableOfflineMode(bool enable) {
+  if (enable != offline_mode_) {
+    offline_mode_ = enable;
+    CallEnableNetworkDeviceType(TYPE_WIFI, !enable);
+    CallEnableNetworkDeviceType(TYPE_CELLULAR, !enable);
+  }
 }
 
 NetworkIPConfigVector NetworkLibraryImplStub::GetIPConfigs(
