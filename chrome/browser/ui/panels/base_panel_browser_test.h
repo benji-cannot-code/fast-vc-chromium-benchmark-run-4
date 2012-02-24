deb07523b9bc7c7318a1b8b730f87bd14b1611fd
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 03:46:51 +0000

    Prevent a new Panel that will go directly to overflow from becoming active.
    BUG=None
    TEST=added test code to CreateOverflowPanels
    
    
    Review URL: http://codereview.chromium.org/9455027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/base_panel_browser_test.h
+++ chrome/browser/ui/panels/base_panel_browser_test.h
@@ -57,6 +57,7 @@ class BasePanelBrowserTest : public InProcessBrowserTest {
     ActiveState show_flag;
     GURL url;
     bool wait_for_fully_created;
+    ActiveState expected_active_state;
 
     CreatePanelParams(const std::string& name,
                       const gfx::Rect& bounds,
@@ -64,7 +65,8 @@ class BasePanelBrowserTest : public InProcessBrowserTest {
         : name(name),
           bounds(bounds),
           show_flag(show_flag),
-          wait_for_fully_created(true) {
+          wait_for_fully_created(true),
+          expected_active_state(show_flag) {
     }
   };
 
