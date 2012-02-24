fa82cf53fdd145a00b0c5a55ca02e567c5cc67e1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-24 03:03:52 +0000

    [Chromium] Add profiling trace for deferred canvas rendering https://bugs.webkit.org/show_bug.cgi?id=79376
    Patch by Justin Novosad <junov@chromium.org> on 2012-02-23
    Reviewed by Stephen White.
    
    No new tests.
    
    * platform/graphics/chromium/Canvas2DLayerChromium.cpp:
    (WebCore::Canvas2DLayerChromium::paintContentsIfDirty):
    Profiling trace for the entire method, as well as for
    calls to canvas flush and context flush, both of which may
    cause deferred operation to be executed.
    * platform/graphics/skia/PlatformContextSkia.cpp:
    (WebCore::PlatformContextSkia::bitmap):
    Inserting a profiling trace in this method because it may cause
    deferred draw commands to be executed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/MathExtras.h>
 #include <wtf/Vector.h>
 
+#if PLATFORM(CHROMIUM)
+#include "TraceEvent.h"
+#endif
+
 namespace WebCore {
 
 // State -----------------------------------------------------------------------
@@ -552,6 +556,9 @@ void PlatformContextSkia::paintSkPaint(const SkRect& rect,
 
 const SkBitmap* PlatformContextSkia::bitmap() const
 {
+#if PLATFORM(CHROMIUM)
+    TRACE_EVENT("PlatformContextSkia::bitmap", this, 0);
+#endif
     return &m_canvas->getDevice()->accessBitmap(false);
 }
 
