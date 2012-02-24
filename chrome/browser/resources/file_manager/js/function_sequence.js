5de7f24ab89c5b807f2a721222a42e3a6edcb2de
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 12:46:30 +0000

    ID3 parser fix
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9467004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/function_sequence.js
+++ chrome/browser/resources/file_manager/js/function_sequence.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,7 +94,11 @@ FunctionSequence.prototype.nextStep_ = function(var_args) {
     this.logger.vlog('Attempting to start step ['
                       + this.steps_[this.currentStepIdx_].name
                       + ']');
-    this.steps_[this.currentStepIdx_].apply(this, arguments);
+    try {
+      this.steps_[this.currentStepIdx_].apply(this, arguments);
+    } catch(e) {
+      this.onError(e.toString());
+    }
   }
 };
 
