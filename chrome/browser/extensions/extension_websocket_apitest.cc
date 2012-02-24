48ad366bcc494e59bf36573306b58465f9640fd8
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 23:48:38 +0000

    Disable browser tests that WebKit r108729 breaks
    Once this lands, then WebKit can roll past this revision, and the tests can be fixed and re-enabled.
    
    TEST=none
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/9460024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "net/base/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WebSocket) {
   FilePath websocket_root_dir;
   ASSERT_TRUE(PathService::Get(chrome::DIR_LAYOUT_TESTS, &websocket_root_dir));
 
--- chrome/browser/extensions/window_open_apitest.cc
+++ chrome/browser/extensions/window_open_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -246,6 +246,6 @@ IN_PROC_BROWSER_TEST_F(WindowOpenPanelTest,
   WaitForTabsAndPopups(browser(), 1, 1, 0);
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WindowOpener) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WindowOpener) {
   ASSERT_TRUE(RunExtensionTest("window_open/opener")) << message_;
 }
