fc62186339e17b25410be7bb613cab30d48390ac
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 03:51:09 +0000

    Select location bar text when focused via F6.
    BUG=106520
    TEST=Manually test by pressing F6 to focus the location bar, make sure it selects all.
    
    
    Review URL: http://codereview.chromium.org/9455023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/toolbar_view.h
+++ chrome/browser/ui/views/toolbar_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -55,11 +55,6 @@ class ToolbarView : public views::AccessiblePaneView,
   // (back?) to this tab and should restore any previous location bar state
   // (such as user editing) as well.
   void Update(content::WebContents* tab, bool should_restore_state);
-
-  // Set focus to the toolbar with complete keyboard access, with the
-  // focus initially set to the location bar. Focus will be restored
-  // to the last focused view if the user escapes.
-  void SetPaneFocusAndFocusLocationBar();
 
   // Set focus to the toolbar with complete keyboard access, with the
   // focus initially set to the app menu. Focus will be restored
@@ -139,7 +134,7 @@ class ToolbarView : public views::AccessiblePaneView,
 
  protected:
   // Overridden from AccessiblePaneView
-  virtual views::View* GetDefaultFocusableChild() OVERRIDE;
+  virtual bool SetPaneFocusAndFocusDefault() OVERRIDE;
   virtual void RemovePaneFocus() OVERRIDE;
 
  private:
