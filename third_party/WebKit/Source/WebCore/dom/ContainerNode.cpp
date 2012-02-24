25939d809f01379bc703fb786f1f549fcec857b4
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-24 18:04:35 +0000

    Don't notify the inspector of Node insertions initiated by the parser https://bugs.webkit.org/show_bug.cgi?id=79388
    Reviewed by Adam Barth.
    
    This is part of a series of changes to make ContainerNode's
    implementation simpler and more internally consistent.
    
    I don't know of a way to break on parser-initiated insertions,
    and the code already doesn't notify on parser-initiated removes
    (in ContainerNode::parserRemoveChild).
    
    No new tests, should already be covered by
    inspector/debugger/dom-breakpoints.html.
    
    * dom/ContainerNode.cpp:
    (WebCore::ContainerNode::parserInsertBefore):
    (WebCore::ContainerNode::parserAddChild):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108817 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -241,10 +241,6 @@ void ContainerNode::parserInsertBefore(PassRefPtr<Node> newChild, Node* nextChil
     for (NodeVector::const_iterator it = targets.begin(); it != targets.end(); ++it) {
         Node* child = it->get();
 
-#if ENABLE(INSPECTOR)
-        InspectorInstrumentation::willInsertDOMNode(document(), child, this);
-#endif
-
         insertBeforeCommon(next.get(), child);
 
         childrenChanged(true, nextChildPreviousSibling.get(), nextChild, 1);
@@ -682,10 +678,6 @@ void ContainerNode::parserAddChild(PassRefPtr<Node> newChild)
 {
     ASSERT(newChild);
     ASSERT(!newChild->parentNode()); // Use appendChild if you need to handle reparenting (and want DOM mutation events).
-
-#if ENABLE(INSPECTOR)
-    InspectorInstrumentation::willInsertDOMNode(document(), newChild.get(), this);
-#endif
 
     forbidEventDispatch();
     Node* last = m_lastChild;
