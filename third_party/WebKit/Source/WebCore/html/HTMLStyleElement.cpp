8e4df4e97552d3f353163fe7d9456eeafdcb9393
dbates@webkit.org <dbates@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-24 17:26:10 +0000

    Attempt to fix the Windows and WinCE build after changeset r108809 <http://trac.webkit.org/changeset/108809> (https://bugs.webkit.org/show_bug.cgi?id=38995)
    Substitute styleLoadEventSender() for loadEventSender() in HTMLStyleElement.cpp
    and substitute linkLoadEventSender() for loadEventSender() in HTMLLinkElement.cpp. 
    
    * html/HTMLLinkElement.cpp:
    (WebCore::linkLoadEventSender):
    (WebCore::HTMLLinkElement::~HTMLLinkElement):
    (WebCore::HTMLLinkElement::dispatchPendingLoadEvents):
    (WebCore::HTMLLinkElement::dispatchPendingEvent):
    (WebCore::HTMLLinkElement::notifyLoadedSheetAndAllCriticalSubresources):
    * html/HTMLStyleElement.cpp:
    (WebCore::styleLoadEventSender):
    (WebCore::HTMLStyleElement::~HTMLStyleElement):
    (WebCore::HTMLStyleElement::dispatchPendingLoadEvents):
    (WebCore::HTMLStyleElement::dispatchPendingEvent):
    (WebCore::HTMLStyleElement::notifyLoadedSheetAndAllCriticalSubresources):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLStyleElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLStyleElement.cpp
@@ -39,7 +39,7 @@ namespace WebCore {
 
 using namespace HTMLNames;
 
-static StyleEventSender& loadEventSender()
+static StyleEventSender& styleLoadEventSender()
 {
     DEFINE_STATIC_LOCAL(StyleEventSender, sharedLoadEventSender, (eventNames().loadEvent));
     return sharedLoadEventSender;
@@ -63,7 +63,7 @@ HTMLStyleElement::~HTMLStyleElement()
     // Therefore we can't ASSERT(!m_isRegisteredWithScopingNode).
     StyleElement::clearDocumentData(document(), this);
 
-    loadEventSender().cancelEvent(this);
+    styleLoadEventSender().cancelEvent(this);
 }
 
 PassRefPtr<HTMLStyleElement> HTMLStyleElement::create(const QualifiedName& tagName, Document* document, bool createdByParser)
@@ -231,12 +231,12 @@ Element* HTMLStyleElement::scopingElement() const
 
 void HTMLStyleElement::dispatchPendingLoadEvents()
 {
-    loadEventSender().dispatchPendingEvents();
+    styleLoadEventSender().dispatchPendingEvents();
 }
 
 void HTMLStyleElement::dispatchPendingEvent(StyleEventSender* eventSender)
 {
-    ASSERT_UNUSED(eventSender, eventSender == &loadEventSender());
+    ASSERT_UNUSED(eventSender, eventSender == &styleLoadEventSender());
     if (m_loadedSheet)
         dispatchEvent(Event::create(eventNames().loadEvent, false, false));
     else
@@ -248,7 +248,7 @@ void HTMLStyleElement::notifyLoadedSheetAndAllCriticalSubresources(bool errorOcc
     if (m_firedLoad)
         return;
     m_loadedSheet = !errorOccurred;
-    loadEventSender().dispatchEventSoon(this);
+    styleLoadEventSender().dispatchEventSoon(this);
     m_firedLoad = true;
 }
 
