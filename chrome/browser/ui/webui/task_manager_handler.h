719ef8fe33b509a0ea70df88bdfbe9bef010f73b
kgr@chromium.org <kgr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 02:53:05 +0000

    Removed delta-updates, task queueing and batching. The delta-events were redundant because a full list of all tasks are re-sent every second and delta-events were batched on 900 millisecond intervals anyway to cut down on screen redraws. Also fixes small issue causing exceptions when opening context-menus.
    BUG=114880,114615,115134
    TEST=manually
    
    
    Review URL: http://codereview.chromium.org/9432006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/task_manager_handler.h
+++ chrome/browser/ui/webui/task_manager_handler.h
@@ -66,9 +66,6 @@ class TaskManagerHandler : public content::WebUIMessageHandler,
 
   bool is_enabled_;
 
-  // Table to cache the group index of the resource index.
-  std::vector<int> resource_to_group_table_;
-
   // Set to store the enabled columns.
   std::set<std::string> enabled_columns_;
 
@@ -77,9 +74,6 @@ class TaskManagerHandler : public content::WebUIMessageHandler,
   void OnGroupAdded(int start, int length);
   void OnGroupChanged(int start, int length);
   void OnGroupRemoved(int start, int length);
-
-  // Updates |resource_to_group_table_|.
-  void UpdateResourceGroupTable(int start, int length);
 
   DISALLOW_COPY_AND_ASSIGN(TaskManagerHandler);
 };
