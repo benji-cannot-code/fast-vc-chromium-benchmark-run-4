d57f7b1303bc7759d58c5dc866e32bad63e4cce7
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-25 02:39:10 +0000

    XSS Auditor targeting legitimate frames as false positives. https://bugs.webkit.org/show_bug.cgi?id=79397
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Test: http/tests/security/xssAuditor/script-tag-safe4.html
    
    * html/parser/XSSAuditor.cpp:
    (WebCore::XSSAuditor::filterCharacterToken):
    (WebCore::XSSAuditor::filterScriptToken):
    (WebCore::XSSAuditor::filterObjectToken):
    (WebCore::XSSAuditor::filterEmbedToken):
    (WebCore::XSSAuditor::filterAppletToken):
    (WebCore::XSSAuditor::filterIframeToken):
    (WebCore::XSSAuditor::decodedSnippetForToken):
    (WebCore):
    (WebCore::XSSAuditor::decodedSnippetForName):
    (WebCore::XSSAuditor::decodedSnippetForAttribute):
    (WebCore::XSSAuditor::decodedSnippetForJavascript):
    (WebCore::XSSAuditor::isContainedInRequest):
    (WebCore::XSSAuditor::isSameOriginResource):
    * html/parser/XSSAuditor.h:
    
    LayoutTests:
    
    * http/tests/security/xssAuditor/resources/script-tag-safe4-frame.html: Added.
    * http/tests/security/xssAuditor/script-tag-safe4-expected.txt: Added.
    * http/tests/security/xssAuditor/script-tag-safe4.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
+++ third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
@@ -73,9 +73,10 @@ private:
     bool eraseDangerousAttributesIfInjected(HTMLToken&);
     bool eraseAttributeIfInjected(HTMLToken&, const QualifiedName&, const String& replacementValue = String(), AttributeKind treatment = NormalAttribute);
 
-    String snippetForRange(const HTMLToken&, int start, int end);
-    String snippetForJavaScript(const String&);
+    String decodedSnippetForToken(const HTMLToken&);
+    String decodedSnippetForName(const HTMLToken&);
     String decodedSnippetForAttribute(const HTMLToken&, const HTMLToken::Attribute&, AttributeKind treatment = NormalAttribute);
+    String decodedSnippetForJavaScript(const HTMLToken&);
 
     bool isContainedInRequest(const String&);
     bool isSameOriginResource(const String& url);
@@ -89,7 +90,7 @@ private:
     OwnPtr<SuffixTree<ASCIICodebook> > m_decodedHTTPBodySuffixTree;
 
     State m_state;
-    String m_cachedSnippet;
+    String m_cachedDecodedSnippet;
     bool m_shouldAllowCDATA;
     unsigned m_scriptTagNestingLevel;
     bool m_notifiedClient;
