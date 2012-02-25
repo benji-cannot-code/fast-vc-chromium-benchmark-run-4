a49a6a0ae33f2e7809bc91240845260ab574a239
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 00:19:50 +0000

    Enable bundled Flapper by default on Linux x64 (if available, e.g., for Official builds).
    Add a flag to *disable* the use of the bundled Flapper.
    
    TEST=On Official builds of Chrome on Linux x64, Flapper is enabled and used by
         default. Everywhere else, things should remain the same (e.g., on other
         Official builds, the bundled NPAPI Flash should continue to be used).
    
    
    Review URL: http://codereview.chromium.org/9466019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -726,6 +726,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kAppsGalleryURL,
       switches::kCloudPrintServiceURL,
       switches::kDebugPrint,
+      switches::kDisableBundledPpapiFlash,
       switches::kDumpHistogramsOnExit,
       switches::kEnableAsynchronousSpellChecking,
       switches::kEnableBenchmarking,
@@ -779,6 +780,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
     static const char* const kSwitchNames[] = {
       switches::kUserDataDir,  // Make logs go to the right file.
       // Load (in-process) Pepper plugins in-process in the zygote pre-sandbox.
+      switches::kDisableBundledPpapiFlash,
       switches::kEnableBundledPpapiFlash,
       switches::kPpapiFlashInProcess,
       switches::kPpapiFlashPath,
--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -77,6 +77,7 @@ extern const char kDiagnostics[];
 extern const char kDisableAuthNegotiateCnameLookup[];
 extern const char kDisableBackgroundMode[];
 extern const char kDisableBackgroundNetworking[];
+extern const char kDisableBundledPpapiFlash[];
 extern const char kDisableClientSidePhishingDetection[];
 extern const char kDisableConnectBackupJobs[];
 extern const char kDisableCRLSets[];
