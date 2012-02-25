dc038dd5522f2c115b8b436375037b7fc5eef555
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 18:27:36 +0000

    Remove "is_content_initiated" argument to ContentRendererClient::ShouldFork
    The argument was always set as |true| in practice, and is probably needed to prevent infinite navigation loops.
    
    This also simplifies the logic in ChromeContentRendererClient::ShouldFork.
    
    
    Review URL: http://codereview.chromium.org/9465006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -641,7 +641,6 @@ bool ChromeContentRendererClient::AllowPopup(const GURL& creator) {
 
 bool ChromeContentRendererClient::ShouldFork(WebFrame* frame,
                                              const GURL& url,
-                                             bool is_content_initiated,
                                              bool is_initial_navigation,
                                              bool* send_referrer) {
   const ExtensionSet* extensions = extension_dispatcher_->extensions();
@@ -663,16 +662,14 @@ bool ChromeContentRendererClient::ShouldFork(WebFrame* frame,
     // navigation test)
     *send_referrer = true;
 
-    if (is_content_initiated) {
-      const Extension* extension =
-          extension_dispatcher_->extensions()->GetExtensionOrAppByURL(
-              ExtensionURLInfo(url));
-      if (extension && extension->is_app()) {
-        UMA_HISTOGRAM_ENUMERATION(
-            extension_misc::kAppLaunchHistogram,
-            extension_misc::APP_LAUNCH_CONTENT_NAVIGATION,
-            extension_misc::APP_LAUNCH_BUCKET_BOUNDARY);
-      }
+    const Extension* extension =
+        extension_dispatcher_->extensions()->GetExtensionOrAppByURL(
+            ExtensionURLInfo(url));
+    if (extension && extension->is_app()) {
+      UMA_HISTOGRAM_ENUMERATION(
+          extension_misc::kAppLaunchHistogram,
+          extension_misc::APP_LAUNCH_CONTENT_NAVIGATION,
+          extension_misc::APP_LAUNCH_BUCKET_BOUNDARY);
     }
     return true;
   }
@@ -689,7 +686,7 @@ bool ChromeContentRendererClient::ShouldFork(WebFrame* frame,
   // Navigating to a new chrome:// scheme (in a new tab) from within a
   // chrome:// page must be a browser navigation so that the browser can
   // register the new associated data source.
-  if (is_content_initiated && url.SchemeIs(kChromeUIScheme))
+  if (url.SchemeIs(kChromeUIScheme))
     return true;
 
   return false;
