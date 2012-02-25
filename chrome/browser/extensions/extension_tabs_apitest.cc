910a497c5bf6e2a357e2b5d2efd670720fe5f23c
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 01:42:30 +0000

    Set minimize property and re-enable tests on Aura
    Currently we disable several important tests on Aura because Minimize() is unimplemented.
    Most significant is ExtensionApiTest.UpdateWindowResize and ExtensionApiTest.UpdateWindowState, which test a lot of the chrome.windows api.
    
    Setting the minimized property but not doing anything satisfies the tests with minimal change, until we determine what the expected minimize behavior is on Aura, and how this affects the extensions API.
    
    BUG=104571
    TEST=Tests pass (esp. browser_tests)
    
    
    Review URL: http://codereview.chromium.org/9372126
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // Window resizes are not completed by the time the callback happens,
-// so these tests fail on linux. http://crbug.com/72369
-#if defined(OS_LINUX)
+// so these tests fail on linux/gtk. http://crbug.com/72369
+#if defined(OS_LINUX) && !defined(USE_AURA)
 #define MAYBE_FocusWindowDoesNotExitFullscreen \
   DISABLED_FocusWindowDoesNotExitFullscreen
 #define MAYBE_UpdateWindowSizeExitsFullscreen \
