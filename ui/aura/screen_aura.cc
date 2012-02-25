ee02bbf14960bb1e442e38ceff9c4502027a5833
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 22:30:50 +0000

    Attempt 4: Makes managed mode constrain the height of windows so they don't overlap the launcher.
    BUG=115581
    TEST=see bug, covered by unit tests too
    R=ben@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9466032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123674 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/screen_aura.cc
+++ ui/aura/screen_aura.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -47,7 +47,7 @@ gfx::NativeWindow ScreenAura::GetWindowAtCursorScreenPointImpl() {
 }
 
 gfx::Rect ScreenAura::GetBounds() {
-  return gfx::Rect(root_window_->GetHostSize());
+  return gfx::Rect(root_window_->bounds().size());
 }
 
 gfx::Rect ScreenAura::GetWorkAreaBounds() {
