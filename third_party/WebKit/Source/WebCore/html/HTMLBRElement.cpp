62dd7fde4a3ed1db7a3b132f56fa9544bf4af426
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-25 03:27:12 +0000

    Don't pass constant strings to CSSParser for presentation attributes. <http://webkit.org/b/79530>
    Reviewed by Anders Carlsson.
    
    "both" -> CSSValueBoth.
    "center" -> CSSValueCenter.
    
    * html/HTMLBRElement.cpp:
    (WebCore::HTMLBRElement::collectStyleForAttribute):
    * html/HTMLElement.cpp:
    (WebCore::HTMLElement::collectStyleForAttribute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLBRElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLBRElement.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Attribute.h"
 #include "CSSPropertyNames.h"
+#include "CSSValueKeywords.h"
 #include "HTMLNames.h"
 #include "RenderBR.h"
 
@@ -63,7 +64,7 @@ void HTMLBRElement::collectStyleForAttribute(Attribute* attr, StylePropertySet* 
         // <br clear> and <br clear=""> are just treated like <br> by Gecko, Mac IE, etc. -dwh
         if (!attr->isEmpty()) {
             if (equalIgnoringCase(attr->value(), "all"))
-                addPropertyToAttributeStyle(style, CSSPropertyClear, "both");
+                addPropertyToAttributeStyle(style, CSSPropertyClear, CSSValueBoth);
             else
                 addPropertyToAttributeStyle(style, CSSPropertyClear, attr->value());
         }
