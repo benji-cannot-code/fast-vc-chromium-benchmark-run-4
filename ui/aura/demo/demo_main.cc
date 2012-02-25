85b99bd4bc7ad0424574df3ec2294c99d138711a
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 00:06:31 +0000

    Revert 123593 - Remove RootWindow::Run(). This removes one reason people will call RootWindow::GetInstance().
    http://crbug.com/112131
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9466029
    
    TBR=ben@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9447080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123594 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/demo/demo_main.cc
+++ ui/aura/demo/demo_main.cc
@@ -116,8 +116,7 @@ int main(int argc, char** argv) {
   window3.Show();
   window3.SetParent(&window2);
 
-  aura::RootWindow::GetInstance()->ShowRootWindow();
-  MessageLoopForUI::current()->Run();
+  aura::RootWindow::GetInstance()->Run();
 
   ui::CompositorTestSupport::Terminate();
 
