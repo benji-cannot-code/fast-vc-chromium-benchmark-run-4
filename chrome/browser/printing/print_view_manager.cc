f15b78608913d8aeb8cc153722d6dd42cfee54d1
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 00:08:43 +0000

    Printing: Release the PrinterQuery when a renderer crashes.
    BUG=none
    TEST=In a debug build, crash the renderer in the middle of printing. Reload the page and repeat. Then quit the browser and make sure there's no worker leaks in ~PrinterQuery. (DCHECK)
    In a debug build, try printing chrome://setting repeatedly without print preview. The quit and look for the same DCHECK.
    
    Review URL: http://codereview.chromium.org/9456034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123595 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -37,23 +37,6 @@ using content::BrowserThread;
 
 namespace {
 
-// Release the PrinterQuery identified by |cookie|.
-void ReleasePrinterQuery(int cookie) {
-  printing::PrintJobManager* print_job_manager =
-      g_browser_process->print_job_manager();
-  // May be NULL in tests.
-  if (!print_job_manager)
-    return;
-
-  scoped_refptr<printing::PrinterQuery> printer_query;
-  print_job_manager->PopPrinterQuery(cookie, &printer_query);
-  if (printer_query.get()) {
-    BrowserThread::PostTask(
-        BrowserThread::IO, FROM_HERE,
-        base::Bind(&printing::PrinterQuery::StopWorker, printer_query.get()));
-  }
-}
-
 // Keeps track of pending scripted print preview closures.
 // No locking, only access on the UI thread.
 typedef std::map<content::RenderProcessHost*, base::Closure>
@@ -82,7 +65,7 @@ PrintViewManager::PrintViewManager(TabContentsWrapper* tab)
 
 PrintViewManager::~PrintViewManager() {
   DCHECK_EQ(NOT_PREVIEWING, print_preview_state_);
-  ReleasePrinterQuery(cookie_);
+  ReleasePrinterQuery();
   DisconnectFromCurrentPrintJob();
 }
 
@@ -167,6 +150,7 @@ void PrintViewManager::StopNavigation() {
 
 void PrintViewManager::RenderViewGone(base::TerminationStatus status) {
   print_preview_state_ = NOT_PREVIEWING;
+  ReleasePrinterQuery();
 
   if (!print_job_.get())
     return;
@@ -263,7 +247,11 @@ void PrintViewManager::OnDidPrintPage(
 }
 
 void PrintViewManager::OnPrintingFailed(int cookie) {
-  ReleasePrinterQuery(cookie);
+  if (cookie != cookie_) {
+    NOTREACHED();
+    return;
+  }
+  ReleasePrinterQuery();
 
   content::NotificationService::current()->Notify(
       chrome::NOTIFICATION_PRINT_JOB_RELEASED,
@@ -607,6 +595,28 @@ bool PrintViewManager::PrintNowInternal(IPC::Message* message) {
   if (web_contents()->ShowingInterstitialPage())
     return false;
   return Send(message);
+}
+
+void PrintViewManager::ReleasePrinterQuery() {
+  if (!cookie_)
+    return;
+
+  int cookie = cookie_;
+  cookie_ = 0;
+
+  printing::PrintJobManager* print_job_manager =
+      g_browser_process->print_job_manager();
+  // May be NULL in tests.
+  if (!print_job_manager)
+    return;
+
+  scoped_refptr<printing::PrinterQuery> printer_query;
+  print_job_manager->PopPrinterQuery(cookie, &printer_query);
+  if (!printer_query.get())
+    return;
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&PrinterQuery::StopWorker, printer_query.get()));
 }
 
 }  // namespace printing
