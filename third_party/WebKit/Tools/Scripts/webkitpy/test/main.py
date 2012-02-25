99b36f20c5e8d5f39a61170d531d626c84c74a4e
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-25 00:12:38 +0000

    webkitpy: should autoinstall coverage https://bugs.webkit.org/show_bug.cgi?id=79535
    Reviewed by Eric Seidel.
    
    We should automatically print the coverage report after the run, too.
    
    * Scripts/webkitpy/test/main.py:
    (Tester._run_tests):
    * Scripts/webkitpy/thirdparty/__init__.py:
    (AutoinstallImportHook.find_module):
    (AutoinstallImportHook._install_coverage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/test/main.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/test/main.py
@@ -199,7 +199,7 @@ class Tester(object):
     def _run_tests(self, dirs, args):
         if self._options.coverage:
             try:
-                import coverage
+                import webkitpy.thirdparty.autoinstalled.coverage as coverage
             except ImportError, e:
                 _log.error("Failed to import 'coverage'; can't generate coverage numbers.")
                 return False
@@ -234,6 +234,7 @@ class Tester(object):
         if self._options.coverage:
             cov.stop()
             cov.save()
+            cov.report(show_missing=False)
         return result.wasSuccessful()
 
     def _is_module(self, dirs, name):
