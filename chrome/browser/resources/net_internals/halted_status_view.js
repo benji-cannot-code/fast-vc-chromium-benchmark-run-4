568572e7f7fe73bcdcdaa4fada44829cafc75664
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 02:19:05 +0000

    Cleanup about:net-internals capture tab.
    - Remove captured events count (this is now displayed in status bar)
    - Removed delete all events button (this was already in status bar)
    
    Also made some aesthetic changes to the status bar, so the "capture" functionality is grouped together.
    
    BUG=115564,115217
    
    Review URL: http://codereview.chromium.org/9443043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123626 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/resources/net_internals/halted_status_view.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * The status view at the top of the page after stopping capturing.
+ */
+var HaltedStatusView = (function() {
+  'use strict';
+
+  // We inherit from DivView.
+  var superClass = DivView;
+
+  function HaltedStatusView() {
+    superClass.call(this, HaltedStatusView.MAIN_BOX_ID);
+  }
+
+  HaltedStatusView.MAIN_BOX_ID = 'halted-status-view';
+
+  HaltedStatusView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype
+  };
+
+  return HaltedStatusView;
+})();
