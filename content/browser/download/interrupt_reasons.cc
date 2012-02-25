2acdbe29b69136d72dbc6872d5e214b7daec4dee
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-25 07:11:22 +0000

    Fixed issues with interrupting a download at the start.
    Added unit tests for direct and navigated download 404 errors.
    
    BUG= 114020
    TEST=Set up a server with a web page that has an invalid link.  Right click on the link and select Save As to download the file.
    
    
    
    Review URL: http://codereview.chromium.org/9378035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123648 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/interrupt_reasons.cc
+++ content/browser/download/interrupt_reasons.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,7 +29,7 @@ InterruptReason ConvertNetErrorToInterruptReason(
     FILE_ERROR_TO_INTERRUPT_REASON(ACCESS_DENIED, ACCESS_DENIED)
 
     // There were not enough resources to complete the operation.
-     FILE_ERROR_TO_INTERRUPT_REASON(INSUFFICIENT_RESOURCES, TRANSIENT_ERROR)
+    FILE_ERROR_TO_INTERRUPT_REASON(INSUFFICIENT_RESOURCES, TRANSIENT_ERROR)
 
     // Memory allocation failed.
     FILE_ERROR_TO_INTERRUPT_REASON(OUT_OF_MEMORY, TRANSIENT_ERROR)
