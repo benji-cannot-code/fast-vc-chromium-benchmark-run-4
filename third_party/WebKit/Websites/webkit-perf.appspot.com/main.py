9dc25e94e6031fabbed2ee7ab26e2087933e966c
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-26 01:22:39 +0000

    perf-o-matic needs a better admin page https://bugs.webkit.org/show_bug.cgi?id=79585
    Reviewed by Sam Weinig.
            
    Add admin/ to replace admin/create-models.html and admin/merge-tests.
    
    Also update MergeTestHandler to accept JSON requests and add "Admin" navigation link on all pages.
    
    * Websites/webkit-perf.appspot.com/admin_handlers.py: Added.
    * Websites/webkit-perf.appspot.com/controller.py:
    (RunsUpdateHandler.post): Fix a regression from r108399.
    * Websites/webkit-perf.appspot.com/css/admin.css: Added.
    * Websites/webkit-perf.appspot.com/js/admin.js: Added.
    * Websites/webkit-perf.appspot.com/js/config.js:
    * Websites/webkit-perf.appspot.com/main.py:
    * Websites/webkit-perf.appspot.com/merge_tests.html: Removed.
    * Websites/webkit-perf.appspot.com/merge_tests_handler.py:
    (MergeTestsHandler):
    (MergeTestsHandler.post):
    * Websites/webkit-perf.appspot.com/static: Removed.
    * Websites/webkit-perf.appspot.com/static/create-models.html: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/webkit-perf.appspot.com/main.py
+++ third_party/WebKit/Websites/webkit-perf.appspot.com/main.py
@@ -21,6 +21,8 @@ from google.appengine.ext.webapp import util
 
 import json
 
+from admin_handlers import IsAdminHandler
+from admin_handlers import AdminDashboardHandler
 from controller import CachedDashboardHandler
 from controller import CachedManifestHandler
 from controller import CachedRunsHandler
@@ -39,6 +41,8 @@ routes = [
     ('/admin/merge-tests/?', MergeTestsHandler),
     ('/admin/report-logs/?', ReportLogsHandler),
     ('/admin/create/(.*)', CreateHandler),
+    (r'/admin/([A-Za-z\-]*)', AdminDashboardHandler),
+    ('/api/user/is-admin', IsAdminHandler),
     ('/api/test/?', CachedManifestHandler),
     ('/api/test/update', ManifestUpdateHandler),
     ('/api/test/report/?', ReportHandler),
