28238a841af754639f8745d026550e7ed5a388f0
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-26 04:21:49 +0000

    Clean up some RootWindow::GetInstance() use in views unittests.
    http://crbug.com/112131
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9465032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123682 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/test/views_test_base.h
+++ ui/views/test/views_test_base.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/test_views_delegate.h"
 
 namespace aura {
+class RootWindow;
 namespace test {
 class TestActivationClient;
 }
@@ -54,6 +55,7 @@ class ViewsTestBase : public testing::Test {
 #if defined(USE_AURA)
   scoped_ptr<aura::test::TestActivationClient> test_activation_client_;
   scoped_ptr<ui::InputMethod> test_input_method_;
+  aura::RootWindow* root_window_;
 #endif
   bool setup_called_;
   bool teardown_called_;
