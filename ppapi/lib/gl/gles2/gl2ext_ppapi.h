a08ca9ac036ce7b9792ff98d13a0c56d40feaa5a
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-26 19:05:20 +0000

    Revert 123696 - Add Pepper support for several GL extensions
    Add support to the command buffer code generator to generate multiple pepper interfaces, and use that to generate interfaces for ANGLE_instanced_arrays, ANGLE_framebuffer_blit, ANGLE_framebuffer_multisample, CHROMIUM_enable_feature, and CHROMIUM_map_sub.
    
    BUG=93148
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9420017
    
    TBR=jbauman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9466042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123697 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/lib/gl/gles2/gl2ext_ppapi.h
+++ ppapi/lib/gl/gles2/gl2ext_ppapi.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_resource.h"
 #include "ppapi/c/ppb.h"
 #include "ppapi/c/ppb_opengles2.h"
-#include "ppapi/c/dev/ppb_opengles2ext_dev.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,16 +37,6 @@ GL_APICALL PP_Resource GL_APIENTRY glGetCurrentContextPPAPI();
 
 // Returns OpenGL ES 2.0 interface.
 GL_APICALL const struct PPB_OpenGLES2* GL_APIENTRY glGetInterfacePPAPI();
-GL_APICALL const struct PPB_OpenGLES2InstancedArrays_Dev* GL_APIENTRY
-    glGetInstancedArraysInterfacePPAPI();
-GL_APICALL const struct PPB_OpenGLES2FramebufferBlit_Dev* GL_APIENTRY
-    glGetFramebufferBlitInterfacePPAPI();
-GL_APICALL const struct PPB_OpenGLES2FramebufferMultisample_Dev* GL_APIENTRY
-    glGetFramebufferMultisampleInterfacePPAPI();
-GL_APICALL const struct PPB_OpenGLES2ChromiumEnableFeature_Dev* GL_APIENTRY
-    glGetChromiumEnableFeatureInterfacePPAPI();
-GL_APICALL const struct PPB_OpenGLES2ChromiumMapSub_Dev* GL_APIENTRY
-    glGetChromiumMapSubInterfacePPAPI();
 
 #ifdef __cplusplus
 }
--- ppapi/native_client/src/shared/ppapi_proxy/plugin_ppb_graphics_3d.h
+++ ppapi/native_client/src/shared/ppapi_proxy/plugin_ppb_graphics_3d.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_graphics_3d.h"
 #include "ppapi/c/ppb_graphics_3d.h"
 #include "ppapi/c/ppb_opengles2.h"
-#include "ppapi/c/dev/ppb_opengles2ext_dev.h"
 #include "ppapi/c/pp_instance.h"
 
 namespace gpu {
@@ -34,16 +33,6 @@ class PluginGraphics3D : public PluginResource {
 
   static const PPB_Graphics3D* GetInterface();
   static const PPB_OpenGLES2* GetOpenGLESInterface();
-  static const PPB_OpenGLES2InstancedArrays_Dev*
-      GetOpenGLESInstancedArraysInterface();
-  static const PPB_OpenGLES2FramebufferBlit_Dev*
-      GetOpenGLESFramebufferBlitInterface();
-  static const PPB_OpenGLES2FramebufferMultisample_Dev*
-      GetOpenGLESFramebufferMultisampleInterface();
-  static const PPB_OpenGLES2ChromiumEnableFeature_Dev*
-      GetOpenGLESChromiumEnableFeatureInterface();
-  static const PPB_OpenGLES2ChromiumMapSub_Dev*
-      GetOpenGLESChromiumMapSubInterface();
 
   virtual bool InitFromBrowserResource(PP_Resource graphics3d_id);
 
