881a9c751fc8f33c904ddd008b36f99c9d7cca2c
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-26 06:16:00 +0000

    Fix typo in comment.  This #endif is for a different ENABLE macro.
    * page/NavigatorRegisterProtocolHandler.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108926 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/NavigatorRegisterProtocolHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/NavigatorRegisterProtocolHandler.cpp
@@ -143,4 +143,4 @@ void NavigatorRegisterProtocolHandler::registerProtocolHandler(Navigator* naviga
 
 } // namespace WebCore
 
-#endif // ENABLE(WEB_INTENTS)
+#endif // ENABLE(REGISTER_PROTOCOL_HANDLER)
