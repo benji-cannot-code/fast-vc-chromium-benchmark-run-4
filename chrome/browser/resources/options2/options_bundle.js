b34886ce42726d7b524ae69a37c06fa7a3f83c62
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 19:59:49 +0000

    Cleanup platform checks in grit conditionals.
    BUG=none
    Review URL: https://chromiumcodereview.appspot.com/9460034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123789 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options_bundle.js
+++ chrome/browser/resources/options2/options_bundle.js
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var KeyboardOverlay = options.KeyboardOverlay;
   var PointerOverlay = options.PointerOverlay;
 </if>
-<if expr="not pp_ifdef('win32') and not pp_ifdef('darwin')">
+<if expr="not is_win and not is_macosx">
   <include src="certificate_tree.js"></include>
   <include src="certificate_manager.js"></include>
   <include src="certificate_restore_overlay.js"></include>
