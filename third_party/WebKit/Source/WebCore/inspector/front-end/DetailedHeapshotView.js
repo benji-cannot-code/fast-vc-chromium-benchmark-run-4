0bcf7e62b87e733ce8873b11f34721bc032400d9
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-27 14:43:34 +0000

    Unreviewed single line fix for r108983.
    * inspector/front-end/DetailedHeapshotView.js:
    (WebInspector.DetailedHeapshotView.prototype.willHide):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DetailedHeapshotView.js
@@ -624,7 +624,7 @@ WebInspector.DetailedHeapshotView.prototype = {
     {
         this._currentSearchResultIndex = -1;
         this._popoverHelper.hidePopover();
-        if (this.helpPopover.visible)
+        if (this.helpPopover && this.helpPopover.visible)
             this.helpPopover.hide();
     },
 
