bcbdd2d6deb1c2ffe66c83a3e6eb7be73f5d6986
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-27 13:47:26 +0000

    Web Inspector: repaint counter graphs when timeline splitter moves https://bugs.webkit.org/show_bug.cgi?id=79644
    Immediately refresh timeline panel on splitter move.
    
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._splitterDragging):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
@@ -155,6 +155,7 @@ WebInspector.TimelinePanel.prototype = {
         var top = event.pageY + this._dragOffset
         this._setSplitterPosition(top);
         event.preventDefault();
+        this._refresh();
     },
 
     /**
