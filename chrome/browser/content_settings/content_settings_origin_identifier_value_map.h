4cdef0f6955905c1f3252e936530a81dc8d984d0
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 22:03:27 +0000

    [Coverity 101670] Pass ResourceIdentifier by const-ref
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9463034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123812 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_origin_identifier_value_map.h
+++ chrome/browser/content_settings/content_settings_origin_identifier_value_map.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,7 +31,7 @@ class OriginIdentifierValueMap {
     ContentSettingsType content_type;
     ResourceIdentifier resource_identifier;
     EntryMapKey(ContentSettingsType content_type,
-                ResourceIdentifier resource_identifier);
+                const ResourceIdentifier& resource_identifier);
     bool operator<(const OriginIdentifierValueMap::EntryMapKey& other) const;
   };
 
