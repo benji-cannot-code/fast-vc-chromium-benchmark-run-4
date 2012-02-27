2d4ae7bef57015c86ab40d228bd650ea150ca365
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-27 10:21:06 +0000

    Add missing include to ColorInputType.cpp https://bugs.webkit.org/show_bug.cgi?id=79632
    Reviewed by Kent Tamura.
    
    * html/ColorInputType.cpp: Include ShadowTree.h
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108975 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/ColorInputType.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MouseEvent.h"
 #include "ScriptController.h"
 #include "ShadowRoot.h"
+#include "ShadowTree.h"
 
 #include <wtf/PassOwnPtr.h>
 #include <wtf/text/WTFString.h>
