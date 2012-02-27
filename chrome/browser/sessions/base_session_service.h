1929db19056eb633761477247a03b87398eb26d7
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 12:50:41 +0000

    Metrics for measuring how much overhead reading compressed content states adds.
    BUG=104293
    TEST=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/9426039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123733 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/base_session_service.h
+++ chrome/browser/sessions/base_session_service.h
@@ -161,6 +161,10 @@ class BaseSessionService : public CancelableRequestProvider,
   bool RunTaskOnBackendThread(const tracked_objects::Location& from_here,
                               const base::Closure& task);
 
+  // For metrics
+  void ResetContentStateReadingMetrics();
+  void WriteContentStateReadingMetrics();
+
   // Max number of navigation entries in each direction we'll persist.
   static const int max_persist_navigation_count;
 
@@ -192,6 +196,9 @@ class BaseSessionService : public CancelableRequestProvider,
 
   // Whether to save the HTTP bodies of the POST requests.
   bool save_post_data_;
+
+  // For metrics.
+  base::TimeDelta time_spent_reading_compressed_content_states;
 
   DISALLOW_COPY_AND_ASSIGN(BaseSessionService);
 };
