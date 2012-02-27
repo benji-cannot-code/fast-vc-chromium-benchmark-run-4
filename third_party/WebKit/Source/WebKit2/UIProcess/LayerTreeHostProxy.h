bd66c3dfb0c0bea143dc6baa8f6730d66ee46e1d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-27 04:13:32 +0000

    Use Functional instead of a MessageQueue for messages to the LayerTreeHostProxy renderer. https://bugs.webkit.org/show_bug.cgi?id=79478
    This makes a lot of the broilerplate code for message-passing unnecessary, and
    results in a much more succinct implementation.
    
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-02-26
    Reviewed by Noam Rosenthal.
    
    * UIProcess/LayerTreeHostProxy.h:
    (WebKit):
    (LayerTreeHostProxy):
    * UIProcess/qt/LayerTreeHostProxyQt.cpp:
    (WebKit):
    (WebKit::LayerTreeHostProxy::updateTile):
    (WebKit::LayerTreeHostProxy::createImage):
    (WebKit::LayerTreeHostProxy::syncRemoteContent):
    (WebKit::LayerTreeHostProxy::dispatchUpdate):
    (WebKit::LayerTreeHostProxy::createTileForLayer):
    (WebKit::LayerTreeHostProxy::updateTileForLayer):
    (WebKit::LayerTreeHostProxy::removeTileForLayer):
    (WebKit::LayerTreeHostProxy::deleteCompositingLayer):
    (WebKit::LayerTreeHostProxy::setRootCompositingLayer):
    (WebKit::LayerTreeHostProxy::syncCompositingLayerState):
    (WebKit::LayerTreeHostProxy::didRenderFrame):
    (WebKit::LayerTreeHostProxy::createDirectlyCompositedImage):
    (WebKit::LayerTreeHostProxy::destroyDirectlyCompositedImage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@108951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/LayerTreeHostProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/LayerTreeHostProxy.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "BackingStore.h"
 #include "DrawingAreaProxy.h"
-#include "MessageQueue.h"
 #include "Region.h"
 #include "WebLayerTreeInfo.h"
 #include <WebCore/GraphicsContext.h>
@@ -32,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/IntSize.h>
 #include <WebCore/RunLoop.h>
 #include <WebCore/Timer.h>
+#include <wtf/Functional.h>
 #include <wtf/HashSet.h>
 
 #if USE(TEXTURE_MAPPER)
@@ -44,8 +44,6 @@ namespace WebKit {
 class LayerBackingStore;
 class WebLayerInfo;
 class WebLayerUpdateInfo;
-
-class LayerTreeMessageToRenderer;
 
 class LayerTreeHostProxy : public WebCore::GraphicsLayerClient {
 public:
@@ -92,8 +90,8 @@ protected:
     WebCore::IntRect m_visibleContentsRect;
     float m_contentsScale;
 
-    MessageQueue<LayerTreeMessageToRenderer> m_messagesToRenderer;
-    void pushUpdateToQueue(PassOwnPtr<LayerTreeMessageToRenderer>);
+    Vector<Function<void()> > m_renderQueue;
+    void dispatchUpdate(const Function<void()>&);
 
 #if USE(TEXTURE_MAPPER)
     OwnPtr<WebCore::TextureMapper> m_textureMapper;
@@ -107,8 +105,8 @@ protected:
     void syncLayerParameters(const WebLayerInfo&);
     void createTile(WebLayerID, int, float scale);
     void removeTile(WebLayerID, int);
-    void updateTile(WebLayerID, int, const WebCore::IntRect&, const WebCore::IntRect&, ShareableBitmap*);
-    void createImage(int64_t, ShareableBitmap*);
+    void updateTile(WebLayerID, int, const WebCore::IntRect&, const WebCore::IntRect&, PassRefPtr<ShareableBitmap>);
+    void createImage(int64_t, PassRefPtr<ShareableBitmap>);
     void destroyImage(int64_t);
     void assignImageToLayer(WebCore::GraphicsLayer*, int64_t imageID);
     void flushLayerChanges();
