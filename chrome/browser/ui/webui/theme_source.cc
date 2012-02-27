9f2c46bbff3f8ce4f9b1d49fed2401c50a5d8078
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 21:39:03 +0000

    ntp theme fixes
    1. fix where the theme background is displayed for default (0) alignment -- it should be center center. This matches what the bookmark bar does. This may break some themes that rely on the default being top-left alignment. I can't see a way around this.
    2. Fix the bookmark bar for vertical center alignment.
    3. update the ntp background when the bookmark bar is attached/detached.
    
    TODO: transition (3) so that it looks good on windows and mac.
    
    BUG=115594
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/9467027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123804 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/theme_source.cc
+++ chrome/browser/ui/webui/theme_source.cc
@@ -22,8 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 
 // use a resource map rather than hard-coded strings.
-static const char* kNewTabCSSPath = "css/newtab.css";
-static const char* kNewIncognitoTabCSSPath = "css/newincognitotab.css";
+static const char* kNewTabCSSPath = "css/new_tab_theme.css";
+static const char* kNewIncognitoTabCSSPath = "css/incognito_new_tab_theme.css";
 
 static std::string StripQueryParams(const std::string& path) {
   GURL path_url = GURL(std::string(chrome::kChromeUIScheme) + "://" +
