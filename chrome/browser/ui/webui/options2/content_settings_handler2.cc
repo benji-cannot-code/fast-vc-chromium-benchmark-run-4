8f26c5c35b2285588ed89afe3640d49ce34dbb78
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 12:41:44 +0000

    Switch the better session restore on by default.
    This change is ugly, minimal, and easy to revert if needed.
    The actual cleanup will be done later.
    
    BUG=104289
    TEST=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/9424053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123732 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/content_settings_handler2.cc
+++ chrome/browser/ui/webui/options2/content_settings_handler2.cc
@@ -285,8 +285,8 @@ void ContentSettingsHandler::GetLocalizedValues(
                                 web_intents::IsWebIntentsEnabled());
   localized_strings->SetBoolean(
       "enable_restore_session_state",
-      CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableRestoreSessionState));
+      !CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableRestoreSessionState));
 }
 
 void ContentSettingsHandler::Initialize() {
