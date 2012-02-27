d0cf86285bef52163d62bc282ec9ea1d1f131191
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-27 22:53:25 +0000

    Move WebCore-internal DOM notification methods from Node to ContainerNode where appropriate https://bugs.webkit.org/show_bug.cgi?id=79697
    Reviewed by Ryosuke Niwa.
    
    insertedIntoTree/removedFromTree are only used by subclasses of
    ContainerNode. Moreover, attempting to make use of these notifications
    in a non-container Node would currently not work, because
    Node::removedFromDocument/insertedIntoDocument do not dispatch to these methods.
    Rather than adding useless calls to an always-empty virtual method,
    this patch moves these methods to ContainerNode.
    
    Meanwhile, childrenChanged moves to ContainerNode for an obvious reason:
    non-container Nodes have no children to change.
    
    No new tests, refactoring only.
    
    * dom/Attr.cpp:
    (WebCore::Attr::childrenChanged): Remove call to now-nonexistent Node::childrenChanged.
    * dom/ContainerNode.cpp:
    (WebCore::ContainerNode::removeChild): Check that the removed child is a container node before notifying it of removal.
    (WebCore::ContainerNode::parserRemoveChild): ditto.
    (WebCore::ContainerNode::insertedIntoTree): Remove call to now-nonexistent Node::insertedIntoTree.
    (WebCore::ContainerNode::removedFromTree): Remove call to now-nonexistent Node::removedFromTree.
    (WebCore::ContainerNode::childrenChanged): Remove call to now-nonexistent Node::childrenChanged.
    (WebCore::notifyChildInserted): Check that the inserted child is a container node before notifying it of insertion.
    * dom/ContainerNode.h:
    (ContainerNode): Migrate comments from Node.h, point back at it for more notification methods.
    * dom/Node.h:
    (Node): Move methods, update comments to point at ContainerNode.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109026 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -464,8 +464,8 @@ bool ContainerNode::removeChild(Node* oldChild, ExceptionCode& ec)
 
     if (child->inDocument())
         child->removedFromDocument();
-    else
-        child->removedFromTree(true);
+    else if (child->isContainerNode())
+        toContainerNode(child.get())->removedFromTree(true);
 
     dispatchSubtreeModifiedEvent();
 
@@ -514,8 +514,8 @@ void ContainerNode::parserRemoveChild(Node* oldChild)
     childrenChanged(true, prev, next, -1);
     if (oldChild->inDocument())
         oldChild->removedFromDocument();
-    else
-        oldChild->removedFromTree(true);
+    else if (oldChild->isContainerNode())
+        toContainerNode(oldChild)->removedFromTree(true);
 }
 
 // this differs from other remove functions because it forcibly removes all the children,
@@ -813,21 +813,24 @@ void ContainerNode::insertedIntoTree(bool deep)
 {
     if (!deep)
         return;
-    for (Node* child = m_firstChild; child; child = child->nextSibling())
-        child->insertedIntoTree(true);
+    for (Node* child = m_firstChild; child; child = child->nextSibling()) {
+        if (child->isContainerNode())
+            toContainerNode(child)->insertedIntoTree(true);
+    }
 }
 
 void ContainerNode::removedFromTree(bool deep)
 {
     if (!deep)
         return;
-    for (Node* child = m_firstChild; child; child = child->nextSibling())
-        child->removedFromTree(true);
+    for (Node* child = m_firstChild; child; child = child->nextSibling()) {
+        if (child->isContainerNode())
+            toContainerNode(child)->removedFromTree(true);
+    }
 }
 
 void ContainerNode::childrenChanged(bool changedByParser, Node* beforeChange, Node* afterChange, int childCountDelta)
 {
-    Node::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
     if (!changedByParser && childCountDelta)
         document()->updateRangesAfterChildrenChanged(this);
     invalidateNodeListsCacheAfterChildrenChanged();
@@ -1086,8 +1089,8 @@ static void notifyChildInserted(Node* child)
     Node* parentOrHostNode = c->parentOrHostNode();
     if (parentOrHostNode && parentOrHostNode->inDocument())
         c->insertedIntoDocument();
-    else
-        c->insertedIntoTree(true);
+    else if (c->isContainerNode())
+        toContainerNode(c.get())->insertedIntoTree(true);
 
     document->incDOMTreeVersion();
 }
