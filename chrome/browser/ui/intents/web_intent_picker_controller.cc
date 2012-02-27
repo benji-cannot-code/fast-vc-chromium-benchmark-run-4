df796fa298a46c2e9a3921205cea5c2e1ff3212e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-27 23:13:19 +0000

    Remove intent_injector.h include from chrome since it's not used.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9479021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller.cc
+++ chrome/browser/ui/intents/web_intent_picker_controller.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "chrome/browser/webdata/web_data_service.h"
 #include "chrome/common/chrome_notification_types.h"
-#include "content/browser/intents/intent_injector.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_intents_dispatcher.h"
