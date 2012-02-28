89cac7cee78d618b0cd43ef6ce718447472c71db
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 00:03:17 +0000

    Fix ExpiredCertAndGoBackTests.
    According to Darin, it doesn't make sense to call CancelRequest if it's not pending. Checking is_pending before calling CancelRequest fixes the flakiness (passed on 1,000 tries)
    
    BUG=40932
    TEST=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/9453028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123834 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/ssl/ssl_error_handler.cc
+++ content/browser/ssl/ssl_error_handler.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -136,7 +136,7 @@ void SSLErrorHandler::CompleteCancelRequest(int error) {
 
   net::URLRequest* request =
       resource_dispatcher_host_->GetURLRequest(request_id_);
-  if (request) {
+  if (request && request->is_pending()) {
     // The request can be NULL if it was cancelled by the renderer (as the
     // result of the user navigating to a new page from the location bar).
     DVLOG(1) << "CompleteCancelRequest() url: " << request->url().spec();
