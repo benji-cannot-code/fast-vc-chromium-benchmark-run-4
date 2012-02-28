f31c6c4a10910c6c5df3b9561b4acc14421437f7
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 22:12:10 +0000

    Reenable GuestModeOptionsUITest for Aura
    BUG=104393
    TEST=Test does not fail
    
    Review URL: http://codereview.chromium.org/9475011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/guest_mode_options_ui_uitest.cc
+++ chrome/browser/ui/webui/options/chromeos/guest_mode_options_ui_uitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,14 +21,7 @@ class GuestModeOptionsUITest : public OptionsUITest {
   }
 };
 
-// See bug 104393.
-#if defined(USE_AURA)
-#define MAYBE_LoadOptionsByURL FAILS_LoadOptionsByURL
-#else
-#define MAYBE_LoadOptionsByURL LoadOptionsByURL
-#endif
-
-TEST_F(GuestModeOptionsUITest, MAYBE_LoadOptionsByURL) {
+TEST_F(GuestModeOptionsUITest, LoadOptionsByURL) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
 
