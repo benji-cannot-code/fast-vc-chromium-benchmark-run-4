5bd1c9664211f4159906ba9ad89ee0994e1e00ba
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 08:16:10 +0000

    Track browser lifetime properly for standalone platform apps.
    This change ensures the browser process is shutdown properly when
    platform apps run standalone (e.g. from desktop shortcuts) are
    closed.
    
    Tested the following on Windows 7:
    * running and closing a platform app from a desktop shortcut
    * running Chrome, starting a platform app from NTP and shutting
    down Chrome
    * running Chrome, starting a platform app from NTP, closing the
    platform app and then shutting down Chrome
    * the above two tests but starting the platform app from a
    desktop shortcut
    
    BUG=None
    TEST=Details above
    
    
    Review URL: http://codereview.chromium.org/9416050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/extensions/shell_window.cc
+++ chrome/browser/ui/extensions/shell_window.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/extensions/shell_window.h"
 
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/extensions/extension_process_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
@@ -64,7 +65,16 @@ ShellWindow::ShellWindow(ExtensionHost* host)
   // apps are no longer tied to the browser process).
   registrar_.Add(this, content::NOTIFICATION_APP_TERMINATING,
                  content::NotificationService::AllSources());
+
+  // Prevent the browser process from shutting down while this window is open.
+  BrowserList::StartKeepAlive();
 }
 
 ShellWindow::~ShellWindow() {
+  // Unregister now to prevent getting NOTIFICATION_APP_TERMINATING if we're the
+  // last window open.
+  registrar_.RemoveAll();
+
+  // Remove shutdown prevention.
+  BrowserList::EndKeepAlive();
 }
