c0209a3e1635e61537647b730fed1bf914c820ea
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-28 16:11:56 +0000

    [Qt] API changes to QWebNavigationRequest https://bugs.webkit.org/show_bug.cgi?id=78821
    Reviewed by Kenneth Rohde Christiansen.
    
    Changes discussed in API review at Szeged: rename 'button' to 'mouseButton',
    rename 'modifiers' to 'keyboardModifiers', remove 'originatingUrl' and
    use the enum type for 'action'.
    
    * UIProcess/API/qt/qwebnavigationrequest.cpp:
    (QWebNavigationRequestPrivate::QWebNavigationRequestPrivate):
    (QWebNavigationRequestPrivate):
    (QWebNavigationRequest::QWebNavigationRequest):
    (QWebNavigationRequest::setAction):
    (QWebNavigationRequest::mouseButton):
    (QWebNavigationRequest::keyboardModifiers):
    (QWebNavigationRequest::action):
    * UIProcess/API/qt/qwebnavigationrequest_p.h:
    * UIProcess/API/qt/tests/publicapi/tst_publicapi.cpp:
    * UIProcess/API/qt/tests/qmltests/DesktopBehavior/tst_navigationRequested.qml:
    * UIProcess/qt/QtWebPagePolicyClient.cpp:
    (QtWebPagePolicyClient::decidePolicyForNavigationAction):
    * UIProcess/qt/QtWebPagePolicyClient.h:
    (QtWebPagePolicyClient):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwebnavigationrequest.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwebnavigationrequest.cpp
@@ -26,12 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class QWebNavigationRequestPrivate {
 public:
-    QWebNavigationRequestPrivate(const QUrl& url, const QUrl& originatingUrl, Qt::MouseButton button,
-                                 Qt::KeyboardModifiers modifiers, QQuickWebView::NavigationType navigationType)
+    QWebNavigationRequestPrivate(const QUrl& url, Qt::MouseButton mouseButton, Qt::KeyboardModifiers keyboardModifiers, QQuickWebView::NavigationType navigationType)
         : url(url)
-        , originatingUrl(originatingUrl)
-        , button(button)
-        , modifiers(modifiers)
+        , mouseButton(mouseButton)
+        , keyboardModifiers(keyboardModifiers)
         , action(QQuickWebView::AcceptRequest)
         , navigationType(navigationType)
     {
@@ -42,17 +40,15 @@ public:
     }
 
     QUrl url;
-    QUrl originatingUrl;
-    Qt::MouseButton button;
-    Qt::KeyboardModifiers modifiers;
-    int action;
+    Qt::MouseButton mouseButton;
+    Qt::KeyboardModifiers keyboardModifiers;
+    QQuickWebView::NavigationRequestAction action;
     QQuickWebView::NavigationType navigationType;
 };
 
-QWebNavigationRequest::QWebNavigationRequest(const QUrl& url, const QUrl& originatingUrl, Qt::MouseButton button,
-                                             Qt::KeyboardModifiers modifiers, QQuickWebView::NavigationType navigationType, QObject* parent)
+QWebNavigationRequest::QWebNavigationRequest(const QUrl& url, Qt::MouseButton mouseButton, Qt::KeyboardModifiers keyboardModifiers, QQuickWebView::NavigationType navigationType, QObject* parent)
     : QObject(parent)
-    , d(new QWebNavigationRequestPrivate(url, originatingUrl, button, modifiers, navigationType))
+    , d(new QWebNavigationRequestPrivate(url, mouseButton, keyboardModifiers, navigationType))
 {
 }
 
@@ -61,7 +57,7 @@ QWebNavigationRequest::~QWebNavigationRequest()
     delete d;
 }
 
-void QWebNavigationRequest::setAction(int action)
+void QWebNavigationRequest::setAction(QQuickWebView::NavigationRequestAction action)
 {
     if (d->action == action)
         return;
@@ -75,24 +71,19 @@ QUrl QWebNavigationRequest::url() const
     return d->url;
 }
 
-QUrl QWebNavigationRequest::originatingUrl() const
+int QWebNavigationRequest::mouseButton() const
 {
-    return d->originatingUrl;
+    return int(d->mouseButton);
 }
 
-int QWebNavigationRequest::button() const
+int QWebNavigationRequest::keyboardModifiers() const
 {
-    return int(d->button);
+    return int(d->keyboardModifiers);
 }
 
-int QWebNavigationRequest::modifiers() const
+QQuickWebView::NavigationRequestAction QWebNavigationRequest::action() const
 {
-    return int(d->modifiers);
-}
-
-int QWebNavigationRequest::action() const
-{
-    return int(d->action);
+    return d->action;
 }
 
 QQuickWebView::NavigationType QWebNavigationRequest::navigationType() const
