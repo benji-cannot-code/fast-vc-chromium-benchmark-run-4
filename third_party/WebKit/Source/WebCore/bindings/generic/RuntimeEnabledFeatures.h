a3b844b4f5b45e1b2904d18d63b1bd428aad0bf0
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-28 09:23:39 +0000

    RuntimeEnabledFeatures::setMultipleShadowSubtreesEnabled should not be inline https://bugs.webkit.org/show_bug.cgi?id=79753
    Moved the function implementation to the .cpp file.
    
    Reviewed by Hajime Morita.
    
    No new tests. (no functional change)
    
    * bindings/generic/RuntimeEnabledFeatures.cpp:
    (WebCore::RuntimeEnabledFeatures::setMultipleShadowSubtreesEnabled):
    (WebCore):
    * bindings/generic/RuntimeEnabledFeatures.h:
    (RuntimeEnabledFeatures):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109089 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
@@ -197,7 +197,7 @@ public:
     static void setShadowDOMEnabled(bool isEnabled) { isShadowDOMEnabled = isEnabled; }
 
     static bool multipleShadowSubtreesEnabled() { return isMultipleShadowSubtreesEnabled; }
-    static void setMultipleShadowSubtreesEnabled(bool isEnabled) { isMultipleShadowSubtreesEnabled = isEnabled; }
+    static void setMultipleShadowSubtreesEnabled(bool isEnabled);
 #endif
 
 #if ENABLE(STYLE_SCOPED)
