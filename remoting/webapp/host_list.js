93e261bd3521c8f15705a533084b26eb734e26ed
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 22:12:33 +0000

    Replaced IT2Me and host list UI tables with flex-boxes. Added tool-tips to host list. Merged 'off-line' indication with host name.
    BUG=115350
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9518004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124039 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_list.js
+++ remoting/webapp/host_list.js
@@ -167,6 +167,8 @@ remoting.HostList.prototype.display = function() {
    */
   var onDelete = function(hostTableEntry) { that.deleteHost_(hostTableEntry); }
 
+  this.table_.hidden = (this.hosts_.length == 0);
+
   for (var i = 0; i < this.hosts_.length; ++i) {
     /** @type {remoting.Host} */
     var host = this.hosts_[i];
@@ -183,6 +185,7 @@ remoting.HostList.prototype.display = function() {
   if (this.lastError_ != '') {
     l10n.localizeElementFromTag(this.errorDiv_, this.lastError_);
   }
+  this.errorDiv_.hidden = (this.lastError_ == '');
 
   this.showOrHide_(this.hosts_.length != 0 || this.lastError_ != '');
 };
