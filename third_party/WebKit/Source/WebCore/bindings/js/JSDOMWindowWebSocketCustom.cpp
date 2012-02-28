01960b5b14ddca9d3855abacc02204319d885f00
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-28 01:36:25 +0000

    Repair license blocks for files created during modularization https://bugs.webkit.org/show_bug.cgi?id=79721
    Reviewed by Eric Seidel.
    
    We failed to copy the license blocks correctly when moving code into
    these files.  This patch restores the correct license blocks.
    
    * Modules/geolocation/NavigatorGeolocation.cpp:
    * Modules/geolocation/NavigatorGeolocation.h:
    * Modules/mediastream/NavigatorMediaStream.cpp:
    * Modules/mediastream/NavigatorMediaStream.h:
    * bindings/js/JSDOMWindowWebAudioCustom.cpp:
    * bindings/js/JSDOMWindowWebSocketCustom.cpp:
    * storage/DOMWindowSQLDatabase.cpp:
    * storage/DOMWindowSQLDatabase.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMWindowWebSocketCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMWindowWebSocketCustom.cpp
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
+ * Copyright (C) 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
  * Copyright (C) 2011 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
