22da34596ce53f92e59c32b6bd6dd8050b03d666
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-28 00:37:58 +0000

    Old JIT's style of JSVALUE64 strict equality is subtly wrong https://bugs.webkit.org/show_bug.cgi?id=79700
    Reviewed by Oliver Hunt.
    
    * assembler/MacroAssemblerX86_64.h:
    (JSC::MacroAssemblerX86_64::comparePtr):
    (MacroAssemblerX86_64):
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::nonSpeculativeStrictEq):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::nonSpeculativePeepholeStrictEq):
    (JSC::DFG::SpeculativeJIT::nonSpeculativeNonPeepholeStrictEq):
    * jit/JITOpcodes.cpp:
    (JSC::JIT::compileOpStrictEq):
    (JSC::JIT::emitSlow_op_stricteq):
    (JSC::JIT::emitSlow_op_nstricteq):
    * jit/JITStubs.cpp:
    (JSC::DEFINE_STUB_FUNCTION):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109040 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/JITStubs.cpp
+++ third_party/WebKit/Source/JavaScriptCore/jit/JITStubs.cpp
@@ -3295,6 +3295,9 @@ DEFINE_STUB_FUNCTION(EncodedJSValue, op_stricteq)
 
     JSValue src1 = stackFrame.args[0].jsValue();
     JSValue src2 = stackFrame.args[1].jsValue();
+    
+    ASSERT((src1.isCell() && src2.isCell())
+           || src1.isDouble() || src2.isDouble());
 
     bool result = JSValue::strictEqual(stackFrame.callFrame, src1, src2);
     CHECK_FOR_EXCEPTION_AT_END();
@@ -3323,6 +3326,9 @@ DEFINE_STUB_FUNCTION(EncodedJSValue, op_nstricteq)
 
     JSValue src1 = stackFrame.args[0].jsValue();
     JSValue src2 = stackFrame.args[1].jsValue();
+
+    ASSERT((src1.isCell() && src2.isCell())
+           || src1.isDouble() || src2.isDouble());
 
     bool result = !JSValue::strictEqual(stackFrame.callFrame, src1, src2);
     CHECK_FOR_EXCEPTION_AT_END();
