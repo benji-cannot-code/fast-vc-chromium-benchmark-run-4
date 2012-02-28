2aff92c770f11ef9822a69eca902e8e74d783811
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 07:22:51 +0000

    [net] Add |detail| argument to NetworkChangeNotifier::DNSObserver::OnDNSChanged
    BUG=114827
    TEST=./net_unittests
    
    
    Review URL: http://codereview.chromium.org/9474017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123930 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -145,10 +145,10 @@ void NetworkChangeNotifier::NotifyObserversOfIPAddressChange() {
   }
 }
 
-void NetworkChangeNotifier::NotifyObserversOfDNSChange() {
+void NetworkChangeNotifier::NotifyObserversOfDNSChange(unsigned detail) {
   if (g_network_change_notifier) {
     g_network_change_notifier->resolver_state_observer_list_->Notify(
-        &DNSObserver::OnDNSChanged);
+        &DNSObserver::OnDNSChanged, detail);
   }
 }
 
