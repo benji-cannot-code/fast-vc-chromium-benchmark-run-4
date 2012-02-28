fd942fc1dc2bbaf20ca6ae9d32e2359d6f6d8fb7
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 21:50:22 +0000

    Disable subpixel rendering for transparent textfields.
    This updates gfx::RenderText to disable subpixel rendering
    when we're using it to draw into a views::Textfield with a
    non-opaque background.
    
    BUG=115198
    TEST=manual: applied in-progress aura applist search patch and checked that we don't use subpixel rendering for it (but we _do_ still use it for the omnibox)
    
    
    Review URL: http://codereview.chromium.org/9480018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124033 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text.cc
+++ ui/gfx/render_text.cc
@@ -622,6 +622,7 @@ RenderText::RenderText()
       composition_range_(ui::Range::InvalidRange()),
       fade_head_(false),
       fade_tail_(false),
+      background_is_transparent_(false),
       cached_bounds_and_offset_valid_(false) {
 }
 
