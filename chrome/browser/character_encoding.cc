f4fa55a8ddd52ea1366e285d534955851d1552b8
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 03:50:52 +0000

    [Coverity] CID 6203: pass std::string by const-ref
    BUG=none
    TEST=compile
    
    
    Review URL: http://codereview.chromium.org/9474020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123886 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/character_encoding.cc
+++ chrome/browser/character_encoding.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -255,7 +255,7 @@ void ParseEncodingListSeparatedWithComma(
   }
 }
 
-string16 GetEncodingDisplayName(std::string encoding_name,
+string16 GetEncodingDisplayName(const std::string& encoding_name,
                                 int category_string_id) {
   string16 category_name = l10n_util::GetStringUTF16(category_string_id);
   if (category_string_id != IDS_ENCODING_KOREAN &&
