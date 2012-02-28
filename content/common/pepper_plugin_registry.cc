c40ca5b39b876ae1f878a434d8d25d083b115e92
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 23:45:45 +0000

    chromeos: Add logging to pepper plugin loading.
    So that we could see whether those plugins would be related with the
    VMTests timed out.
    
    BUG=chromium-os:26646
    TEST=none.
    
    
    Review URL: http://codereview.chromium.org/9477021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124053 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/pepper_plugin_registry.cc
+++ content/common/pepper_plugin_registry.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -138,6 +138,11 @@ void PepperPluginRegistry::PreloadModules() {
       std::string error;
       base::NativeLibrary library = base::LoadNativeLibrary(plugins[i].path,
                                                             &error);
+#if defined(OS_CHROMEOS)
+      // TODO(xiyuan): Remove this once crosbug.com/26646 is resolved.
+      LOG(ERROR) << "#### PepperPluginRegistry::PreloadModules"
+                 << ", path=" << plugins[i].path.value();
+#endif  // defined (OS_CHROMEOS)
       DLOG_IF(WARNING, !library) << "Unable to load plugin "
                                  << plugins[i].path.value() << " "
                                  << error;
