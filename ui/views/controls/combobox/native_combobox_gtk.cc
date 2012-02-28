0d9d990078e6014ca0e083c279e8fbe8581b74ed
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 05:56:42 +0000

    Fixes combobox regression on windows. The problem with the recent change is that on windows the CBS_DROPDOWNLIST is only created once the combobox is parented. That meant the calls to UpdateFromModel and the like did nothing and we ended up showing the combobox with no content.
    I would like a regression test for this, but I'm going to leave that
    to Mattias.
    
    BUG=115384
    TEST=see bug.
    R=ben@chromium.org,mnissler@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9452001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123914 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/combobox/native_combobox_gtk.cc
+++ ui/views/controls/combobox/native_combobox_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -145,14 +145,6 @@ void NativeComboboxGtk::CreateNativeControl() {
   }
 
   NativeControlCreated(widget);
-}
-
-void NativeComboboxGtk::NativeControlCreated(GtkWidget* native_control) {
-  NativeControlGtk::NativeControlCreated(native_control);
-  // Set the initial state of the combobox.
-  UpdateFromModel();
-  UpdateEnabled();
-  UpdateSelectedItem();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
