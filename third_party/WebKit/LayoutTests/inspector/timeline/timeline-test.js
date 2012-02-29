0f63a1c90a83aa483efed3a4c395564533d67242
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-29 08:29:38 +0000

    2012-02-28  Yury Semikhatsky  <yurys@chromium.org>
            Web Inspector: move DOM counter graphs out of experimental
            https://bugs.webkit.org/show_bug.cgi?id=79802
    
            Enable DOM counter graphs by default.
    
            Reveal nearest record from the left hand side when there is no
            record containing the point where the user clicked.
    
            Reviewed by Pavel Feldman.
    
            * inspector/front-end/Settings.js:
            (WebInspector.ExperimentsSettings):
            * inspector/front-end/TimelinePanel.js:
            (WebInspector.TimelinePanel):
            (WebInspector.TimelinePanel.prototype._endSplitterDragging):
            (WebInspector.TimelinePanel.prototype._onTimelineEventRecorded):
            (WebInspector.TimelinePanel.prototype.sidebarResized):
            (WebInspector.TimelinePanel.prototype._resetPanel):
            (WebInspector.TimelinePanel.prototype._refresh):
            (WebInspector.TimelinePanel.prototype.revealRecordAt):
    
    2012-02-29  Yury Semikhatsky  <yurys@chromium.org>
    
            Web Inspector: move DOM counter graphs out of experimental
            https://bugs.webkit.org/show_bug.cgi?id=79802
    
            Enable DOM counter graphs by default. Rebase timeline tests so that
            the records include counters field.
    
            Reviewed by Pavel Feldman.
    
            * inspector/timeline/timeline-dom-content-loaded-event-expected.txt:
            * inspector/timeline/timeline-event-dispatch-expected.txt:
            * inspector/timeline/timeline-injected-script-eval-expected.txt:
            * inspector/timeline/timeline-layout-expected.txt:
            * inspector/timeline/timeline-load-event-expected.txt:
            * inspector/timeline/timeline-mark-timeline-expected.txt:
            * inspector/timeline/timeline-network-resource-expected.txt:
            * inspector/timeline/timeline-parse-html-expected.txt:
            * inspector/timeline/timeline-recalculate-styles-expected.txt:
            * inspector/timeline/timeline-script-tag-1-expected.txt:
            * inspector/timeline/timeline-script-tag-2-expected.txt:
            * inspector/timeline/timeline-test.js:
            * inspector/timeline/timeline-time-stamp-expected.txt:
            * platform/chromium/inspector/timeline/timeline-animation-frame-expected.txt:
            * platform/chromium/inspector/timeline/timeline-event-dispatch-expected.txt:
            * platform/chromium/inspector/timeline/timeline-layout-expected.txt:
            * platform/chromium/inspector/timeline/timeline-mark-timeline-expected.txt:
            * platform/chromium/inspector/timeline/timeline-network-resource-expected.txt:
            * platform/chromium/inspector/timeline/timeline-paint-expected.txt:
            * platform/chromium/inspector/timeline/timeline-parse-html-expected.txt:
            * platform/chromium/inspector/timeline/timeline-time-stamp-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/timeline/timeline-test.js
+++ third_party/WebKit/LayoutTests/inspector/timeline/timeline-test.js
@@ -14,7 +14,8 @@ InspectorTest.timelinePropertyFormatters = {
     usedHeapSize: "formatAsTypeName",
     totalHeapSize: "formatAsTypeName",
     mimeType: "formatAsTypeName",
-    id: "formatAsTypeName"
+    id: "formatAsTypeName",
+    counters: "formatAsTypeName"
 };
 
 InspectorTest.startTimeline = function(callback)
