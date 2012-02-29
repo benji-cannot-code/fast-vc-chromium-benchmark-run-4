408aa0ed50c8ade11fd4a19cde73412796982adf
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 02:20:26 +0000

    Merge a libxml clang warning fix.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9531004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124094 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libxml/src/xmlschemastypes.c
+++ third_party/libxml/src/xmlschemastypes.c
@@ -5116,9 +5116,9 @@ xmlSchemaValidateListSimpleTypeFacet(xmlSchemaFacetPtr facet,
  */
 static int
 xmlSchemaValidateLengthFacetInternal(xmlSchemaFacetPtr facet,
-				     xmlSchemaTypeType valType,
+				     xmlSchemaValType valType,
 				     const xmlChar *value,
-				     xmlSchemaValPtr val,				     
+				     xmlSchemaValPtr val,
 				     unsigned long *length,
 				     xmlSchemaWhitespaceValueType ws)  
 {
