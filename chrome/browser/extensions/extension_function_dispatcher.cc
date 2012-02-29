429806b5892f56a9fa310eab9b547fe088268dba
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 06:16:17 +0000

    Add setFontName to Font Settings Extension API.
    BUG=114148
    TEST=browser_tests --gtest_filter=ExtensionApiTest.FontSettings
    
    
    Review URL: http://codereview.chromium.org/9481023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -458,6 +458,7 @@ void FactoryRegistry::ResetFunctions() {
 
   // Font settings.
   RegisterFunction<GetFontNameFunction>();
+  RegisterFunction<SetFontNameFunction>();
 
   // ChromeAuth settings.
   RegisterFunction<SetCloudPrintCredentialsFunction>();
--- chrome/test/data/extensions/api_test/font_settings/test.js
+++ chrome/test/data/extensions/api_test/font_settings/test.js
@@ -15,6 +15,14 @@ function expect(expected, message) {
 }
 
 chrome.test.runTests([
+  function setPerScriptFontName() {
+    fs.setFontName({
+      script: 'Hang',
+      genericFamily: 'standard',
+      fontName: 'UnBatang'
+    }, chrome.test.callbackPass());
+  },
+
   function getPerScriptFontName() {
     var expected = 'UnBatang';
     var message = 'Setting for Hangul standard font should be ' + expected;
@@ -22,6 +30,13 @@ chrome.test.runTests([
       script: 'Hang',
       genericFamily: 'standard'
     }, expect({fontName: expected}, message));
+  },
+
+  function setGlobalFontName() {
+    fs.setFontName({
+      genericFamily: 'sansserif',
+      fontName: 'Arial'
+    }, chrome.test.callbackPass());
   },
 
   function getGlobalFontName() {
