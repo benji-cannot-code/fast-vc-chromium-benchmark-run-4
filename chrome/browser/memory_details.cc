0c3c815c5044a01d1039706741804d1f52077fed
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 18:34:20 +0000

    Remove render_sandbox_host_linux.h include from chrome. It was only used to get the pid of that process, so added a method to the ZygoteHost interface to return it instead.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9536019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_details.cc
+++ chrome/browser/memory_details.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
-#include "content/browser/renderer_host/render_sandbox_host_linux.h"
 #include "content/public/browser/zygote_host_linux.h"
 #endif
 
@@ -158,7 +157,8 @@ void MemoryDetails::CollectChildInfoOnUIThread() {
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
   const pid_t zygote_pid = content::ZygoteHost::GetInstance()->GetPid();
-  const pid_t sandbox_helper_pid = RenderSandboxHostLinux::GetInstance()->pid();
+  const pid_t sandbox_helper_pid =
+      content::ZygoteHost::GetInstance()->GetSandboxHelperPid();
 #endif
 
   ProcessData* const chrome_browser = ChromeBrowser();
