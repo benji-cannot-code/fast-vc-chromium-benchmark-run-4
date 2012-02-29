bcc24613738c68b677c044036eca5e245f720f30
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-29 01:31:55 +0000

    Clipboard::getData should return an empty string instead of undefined https://bugs.webkit.org/show_bug.cgi?id=79712
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Per the spec, an empty string should be returned when there is no data for the given typestring.
    
    Test: fast/events/dataTransfer-getData-returns-empty-string.html
    
    * bindings/js/JSClipboardCustom.cpp:
    * bindings/v8/custom/V8ClipboardCustom.cpp:
    * dom/Clipboard.h:
    (Clipboard):
    * dom/Clipboard.idl:
    * platform/blackberry/ClipboardBlackBerry.cpp:
    (WebCore::ClipboardBlackBerry::getData):
    * platform/blackberry/ClipboardBlackBerry.h:
    (ClipboardBlackBerry):
    * platform/chromium/ClipboardChromium.cpp:
    (WebCore::ClipboardChromium::getData):
    * platform/chromium/ClipboardChromium.h:
    (ClipboardChromium):
    * platform/efl/ClipboardEfl.cpp:
    (WebCore::ClipboardEfl::getData):
    * platform/efl/ClipboardEfl.h:
    (ClipboardEfl):
    * platform/gtk/ClipboardGtk.cpp:
    (WebCore::ClipboardGtk::getData):
    * platform/gtk/ClipboardGtk.h:
    (ClipboardGtk):
    * platform/mac/ClipboardMac.h:
    (ClipboardMac):
    * platform/mac/ClipboardMac.mm:
    (WebCore::ClipboardMac::getData):
    * platform/qt/ClipboardQt.cpp:
    (WebCore::ClipboardQt::getData):
    * platform/qt/ClipboardQt.h:
    (ClipboardQt):
    * platform/win/ClipboardUtilitiesWin.cpp:
    (WebCore::getFullCFHTML):
    (WebCore::getURL):
    (WebCore::getPlainText):
    (WebCore::getTextHTML):
    (WebCore::getCFHTML):
    (WebCore::fragmentFromHTML):
    * platform/win/ClipboardUtilitiesWin.h:
    (WebCore):
    * platform/win/ClipboardWin.cpp:
    (WebCore::ClipboardWin::getData):
    * platform/win/ClipboardWin.h:
    (ClipboardWin):
    * platform/wx/ClipboardWx.cpp:
    (WebCore::ClipboardWx::getData):
    * platform/wx/ClipboardWx.h:
    (ClipboardWx):
    
    LayoutTests:
    
    * fast/events/dataTransfer-getData-returns-empty-string-expected.txt: Added.
    * fast/events/dataTransfer-getData-returns-empty-string.html: Added.
    * http/tests/local/drag-over-remote-content-expected.txt:
    * http/tests/security/drag-over-remote-content-iframe-expected.txt:
    * http/tests/security/drag-over-remote-content-iframe.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/ClipboardGtk.h
+++ third_party/WebKit/Source/WebCore/platform/gtk/ClipboardGtk.h
@@ -55,7 +55,7 @@ namespace WebCore {
 
         void clearData(const String&);
         void clearAllData();
-        String getData(const String&, bool&) const;
+        String getData(const String&) const;
         bool setData(const String&, const String&);
 
         virtual PassRefPtr<DOMStringList> types() const;
--- third_party/WebKit/Source/WebCore/platform/wx/ClipboardWx.cpp
+++ third_party/WebKit/Source/WebCore/platform/wx/ClipboardWx.cpp
@@ -59,7 +59,7 @@ void ClipboardWx::clearAllData()
     Pasteboard::generalPasteboard()->clear();
 }
 
-String ClipboardWx::getData(const String& type, bool& success) const 
+String ClipboardWx::getData(const String& type) const 
 {
     notImplemented();
     return ""; 
