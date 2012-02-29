3b7103f68c97f108009fe7415f7df2b1d456f57c
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 18:32:37 +0000

    Hide the mouse cursor when it boots in ChromeOS.
    This is originally implemented in CrOS's platform/window-manager but ported here
    for Aura.
    
    BUG=111385
    TEST=manually
    
    
    Review URL: http://codereview.chromium.org/9416091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124207 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -224,6 +224,8 @@ class DummySystemTrayDelegate : public SystemTrayDelegate {
 Shell* Shell::instance_ = NULL;
 // static
 bool Shell::compact_window_mode_for_test_ = false;
+// static
+bool Shell::initially_hide_cursor_ = false;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Shell::TestApi
@@ -356,6 +358,8 @@ void Shell::Init() {
 
   aura::RootWindow* root_window = GetRootWindow();
   root_window->SetCursor(aura::kCursorPointer);
+  if (initially_hide_cursor_)
+    root_window->ShowCursor(false);
 
   activation_controller_.reset(new internal::ActivationController);
 
--- chrome/browser/chrome_browser_main_extra_parts_aura.cc
+++ chrome/browser/chrome_browser_main_extra_parts_aura.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/aura/ime_controller_chromeos.h"
 #include "chrome/browser/ui/views/aura/volume_controller_chromeos.h"
 #include "chrome/browser/chromeos/input_method/input_method_manager.h"
+#include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/system/runtime_environment.h"
 #endif
 
@@ -39,6 +40,9 @@ void ChromeBrowserMainExtraPartsAura::PreProfileInit() {
           switches::kAuraHostWindowUseFullscreen)) {
     aura::RootWindow::set_use_fullscreen_host_window(true);
     aura::RootWindow::set_hide_host_cursor(true);
+    // Hide the mouse cursor completely at boot.
+    if (!chromeos::UserManager::Get()->user_is_logged_in())
+      ash::Shell::set_initially_hide_cursor(true);
   }
 #endif
 
