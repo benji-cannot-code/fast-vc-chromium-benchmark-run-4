475baa8d18cd86d459a11abea5a4e4a45cbc908f
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 18:37:45 +0000

    Plumb --ui-use-gpu-process to CrOS guest mode.
    BUG=99516
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9538011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -1011,6 +1011,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ash::switches::kAuraPanelManager,
       ash::switches::kAuraWindowAnimationsDisabled,
       switches::kUIEnablePartialSwap,
+      switches::kUIUseGPUProcess,
 #endif
       switches::kUseGL,
       switches::kUserDataDir,
