b87d206ed99b62c0204a179f1cbc8a54cef45056
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 03:56:15 +0000

    Aho-Corasick algorithm for SubstringSetMatcher
    This CL implements the Aho-Corasick algorithm for the SubstringSetMatcher so
    that the runtime of the SubstringSetMatcher is no longer linear in the number
    of registered patterns.
    
    BUG=112155
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9426023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative/substring_set_matcher_unittest.cc
+++ chrome/browser/extensions/api/declarative/substring_set_matcher_unittest.cc
@@ -29,6 +29,9 @@ namespace {
 void TestOnePattern(const std::string& test_string,
                     const std::string& pattern,
                     bool is_match) {
+  std::string test =
+      "TestOnePattern(" + test_string + ", " + pattern + ", " +
+      (is_match ? "1" : "0") + ")";
   std::vector<const SubstringPattern*> patterns;
   SubstringPattern substring_pattern(pattern, 1);
   patterns.push_back(&substring_pattern);
@@ -38,8 +41,8 @@ void TestOnePattern(const std::string& test_string,
   matcher.Match(test_string, &matches);
 
   size_t expected_matches = (is_match ? 1 : 0);
-  EXPECT_EQ(expected_matches, matches.size());
-  EXPECT_EQ(is_match, matches.find(1) != matches.end());
+  EXPECT_EQ(expected_matches, matches.size()) << test;
+  EXPECT_EQ(is_match, matches.find(1) != matches.end()) << test;
 }
 
 void TestTwoPatterns(const std::string& test_string,
@@ -47,6 +50,9 @@ void TestTwoPatterns(const std::string& test_string,
                      const std::string& pattern_2,
                      bool is_match_1,
                      bool is_match_2) {
+  std::string test =
+      "TestTwoPatterns(" + test_string + ", " + pattern_1 + ", " + pattern_2 +
+      ", " + (is_match_1 ? "1" : "0") + ", " + (is_match_2 ? "1" : "0") + ")";
   SubstringPattern substring_pattern_1(pattern_1, 1);
   SubstringPattern substring_pattern_2(pattern_2, 2);
   // In order to make sure that the order in which patterns are registered
@@ -66,9 +72,9 @@ void TestTwoPatterns(const std::string& test_string,
     matcher.Match(test_string, &matches);
 
     size_t expected_matches = (is_match_1 ? 1 : 0) + (is_match_2 ? 1 : 0);
-    EXPECT_EQ(expected_matches, matches.size());
-    EXPECT_EQ(is_match_1, matches.find(1) != matches.end());
-    EXPECT_EQ(is_match_2, matches.find(2) != matches.end());
+    EXPECT_EQ(expected_matches, matches.size()) << test;
+    EXPECT_EQ(is_match_1, matches.find(1) != matches.end()) << test;
+    EXPECT_EQ(is_match_2, matches.find(2) != matches.end()) << test;
   }
 }
 }
@@ -158,4 +164,3 @@ TEST(SubstringSetMatcherTest, RegisterAndRemove) {
   EXPECT_TRUE(matches.end() != matches.find(1));
   EXPECT_TRUE(matches.end() == matches.find(2));
 }
-
