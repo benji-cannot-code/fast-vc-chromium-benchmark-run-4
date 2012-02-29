e26c5ec786e52a7d30ee870eb89d9672981d490d
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 01:29:13 +0000

    UI changes for displaying gdata provider in file manager.
    BUG=chromium-os:26110
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9424011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124084 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_model.js
+++ chrome/browser/resources/file_manager/js/directory_model.js
@@ -62,13 +62,19 @@ DirectoryModel.ARCHIVE_DIRECTORY = 'archive';
 DirectoryModel.RootType = {
   DOWNLOADS: 'downloads',
   ARCHIVE: 'archive',
-  REMOVABLE: 'removable'
+  REMOVABLE: 'removable',
+  GDATA: 'gdata'
 };
 
 /**
 * The name of the downloads directory.
 */
 DirectoryModel.DOWNLOADS_DIRECTORY = 'Downloads';
+
+/**
+* The name of the gdata provider directory.
+*/
+DirectoryModel.GDATA_DIRECTORY = 'gdata';
 
 DirectoryModel.prototype = {
   __proto__: cr.EventTarget.prototype,
@@ -700,7 +706,8 @@ DirectoryModel.prototype = {
     var groups = {
       downloads: null,
       archives: null,
-      removables: null
+      removables: null,
+      gdata: null
     };
 
     metrics.startInterval('Load.Roots');
@@ -711,7 +718,8 @@ DirectoryModel.prototype = {
 
       callback(groups.downloads.
                concat(groups.archives).
-               concat(groups.removables));
+               concat(groups.removables).
+               concat(groups.gdata));
       metrics.recordInterval('Load.Roots');
     }
 
@@ -730,6 +738,16 @@ DirectoryModel.prototype = {
       done();
     }
 
+    function onGData(entry) {
+      groups.gdata = [entry];
+      done();
+    }
+
+    function onGDataError(error) {
+      groups.gdata = [];
+      done();
+    }
+
     var root = this.root_;
     root.getDirectory(DirectoryModel.DOWNLOADS_DIRECTORY, { create: false },
                       onDownloads, onDownloadsError);
@@ -737,6 +755,8 @@ DirectoryModel.prototype = {
                        append.bind(this, 'archives'));
     util.readDirectory(root, DirectoryModel.REMOVABLE_DIRECTORY,
                        append.bind(this, 'removables'));
+    root.getDirectory(DirectoryModel.GDATA_DIRECTORY, { create: false },
+                      onGData, onGDataError);
   },
 
   updateRoots: function(opt_changeDirectoryTo) {
