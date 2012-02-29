1145f01da1f226956daa5ff4db1ea2160479c617
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 18:40:27 +0000

    Upstream test sharder.
    On chromium for android, we shard tests by splitting the set of tests
    and running on multiple connected devices.
    
    BUG=
    TEST=build/android/run_tests.py
    
    
    Review URL: http://codereview.chromium.org/9494007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124214 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/base_test_runner.py
+++ build/android/base_test_runner.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -20,12 +20,17 @@ TEST_SYNC_SERVER_PORT = 8003
 
 
 class BaseTestRunner(object):
-  """Base class for running tests on a single device."""
+  """Base class for running tests on a single device.
 
-  def __init__(self, device):
+  A subclass should implement RunTests() with no parameter, so that calling
+  the Run() method will set up tests, run them and tear them down.
+  """
+
+  def __init__(self, device, shard_index):
     """
       Args:
         device: Tests will run on the device of this ID.
+        shard_index: Index number of the shard on which the test suite will run.
     """
     self.device = device
     self.adb = android_commands.AndroidCommands(device=device)
@@ -42,16 +47,27 @@ class BaseTestRunner(object):
     self.forwarder_base_url = ('http://localhost:%d' %
         self._forwarder_device_port)
     self.flags = FlagChanger(self.adb)
+    self.shard_index = shard_index
 
-  def RunTests(self):
-    # TODO(bulach): this should actually do SetUp / RunTestsInternal / TearDown.
-    # Refactor the various subclasses to expose a RunTestsInternal without
-    # any params.
-    raise NotImplementedError
+  def Run(self):
+    """Calls subclass functions to set up tests, run them and tear them down.
+
+    Returns:
+      Test results returned from RunTests().
+    """
+    self.SetUp()
+    try:
+      return self.RunTests()
+    finally:
+      self.TearDown()
 
   def SetUp(self):
     """Called before tests run."""
     pass
+
+  def RunTests(self):
+    """Runs the tests. Need to be overridden."""
+    raise NotImplementedError
 
   def TearDown(self):
     """Called when tests finish running."""
