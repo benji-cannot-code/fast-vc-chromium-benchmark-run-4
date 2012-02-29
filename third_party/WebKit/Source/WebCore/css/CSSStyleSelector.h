ccd318630fa1642fd98faf1d14512dc409fdc55c
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-29 18:08:27 +0000

    Applying region style should not need to access parent rules https://bugs.webkit.org/show_bug.cgi?id=79910
    Reviewed by Andreas Kling.
    
    Currently CSSStyleSelector::applyProperties looks into parent rules to see if a rule is
    part of region style. The plan is to eliminate the rule parent pointer so this needs to be refactored.
            
    Add a bit to RuleData to indicate if the StyleRule is part of a region style.
    
    * css/CSSStyleSelector.cpp:
    (RuleData):
    (WebCore::RuleData::isInRegionRule):
    (RuleSet):
    (WebCore::CSSStyleSelector::addMatchedProperties):
    (WebCore::CSSStyleSelector::sortAndTransferMatchedRules):
    (WebCore::CSSStyleSelector::collectMatchingRulesForList):
    * css/CSSStyleSelector.h:
    (CSSStyleSelector):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109227 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.h
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.h
@@ -272,7 +272,10 @@ private:
         
         RefPtr<StylePropertySet> properties;
         union {
-            unsigned linkMatchType;
+            struct {
+                unsigned linkMatchType : 2;
+                unsigned isInRegionRule : 1;
+            };
             // Used to make sure all memory is zero-initialized since we compute the hash over the bytes of this object.
             void* possiblyPaddedMember;
         };
@@ -292,7 +295,7 @@ private:
         bool includeEmptyRules;
     };
 
-    static void addMatchedProperties(MatchResult&, StylePropertySet* properties, StyleRule* = 0, unsigned linkMatchType = SelectorChecker::MatchAll);
+    static void addMatchedProperties(MatchResult&, StylePropertySet* properties, StyleRule* = 0, unsigned linkMatchType = SelectorChecker::MatchAll, bool inRegionRule = false);
 
     void matchAllRules(MatchResult&);
     void matchUARules(MatchResult&);
@@ -313,7 +316,7 @@ private:
     template <bool firstPass>
     void applyMatchedProperties(const MatchResult&, bool important, int startIndex, int endIndex, bool inheritedOnly);
     template <bool firstPass>
-    void applyProperties(const StylePropertySet* properties, StyleRule*, bool isImportant, bool inheritedOnly);
+    void applyProperties(const StylePropertySet* properties, StyleRule*, bool isImportant, bool inheritedOnly, bool filterRegionProperties);
 
     static bool isValidRegionStyleProperty(int id);
 
