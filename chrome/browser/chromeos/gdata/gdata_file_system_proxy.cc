949dd8638587d93848785afb0b4e5e180db848fc
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 07:20:05 +0000

    Added entry removal support to remote file system operation.
    BUG=chromium-os:26963
    TEST=GDataFileSystemTest.RemoveFiles, GDataFileSystemTest.FilePathTest
    Review URL: https://chromiumcodereview.appspot.com/9514002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124145 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/file_system_types.h"
 #include "webkit/fileapi/file_system_util.h"
 
+using base::MessageLoopProxy;
 using content::BrowserThread;
 using fileapi::FileSystemOperationInterface;
 
@@ -98,7 +99,7 @@ class FindFileDelegateReplyBase : public FindFileDelegate {
   FilePath search_file_path_;
   // True if the final directory content is required.
   bool require_content_;
-  scoped_refptr<base::MessageLoopProxy> replay_message_proxy_;
+  scoped_refptr<MessageLoopProxy> replay_message_proxy_;
 };
 
 // GetFileInfoDelegate is used to handle results of proxy's content search
@@ -108,8 +109,7 @@ class GetFileInfoDelegate : public FindFileDelegateReplyBase {
   GetFileInfoDelegate(
       scoped_refptr<GDataFileSystem> file_system,
       const FilePath& search_file_path,
-      const FileSystemOperationInterface::GetMetadataCallback&
-         callback)
+      const FileSystemOperationInterface::GetMetadataCallback& callback)
       : FindFileDelegateReplyBase(file_system,
                                   search_file_path,
                                   false  /* require_content */),
@@ -169,8 +169,7 @@ class ReadDirectoryDelegate : public FindFileDelegateReplyBase {
   ReadDirectoryDelegate(
       scoped_refptr<GDataFileSystem> file_system,
       const FilePath& search_file_path,
-      const FileSystemOperationInterface::ReadDirectoryCallback&
-          callback)
+      const FileSystemOperationInterface::ReadDirectoryCallback& callback)
       : FindFileDelegateReplyBase(file_system,
                                   search_file_path,
                                   true  /* require_content */),
@@ -241,6 +240,7 @@ class ReadDirectoryDelegate : public FindFileDelegateReplyBase {
   FileSystemOperationInterface::ReadDirectoryCallback callback_;
 };
 
+// GDataFileSystemProxy class implementation.
 
 GDataFileSystemProxy::GDataFileSystemProxy(Profile* profile)
     : file_system_(GDataFileSystemFactory::GetForProfile(profile)) {
@@ -279,6 +279,31 @@ void GDataFileSystemProxy::ReadDirectory(const GURL& file_url,
       file_path, new ReadDirectoryDelegate(file_system_, file_path, callback));
 }
 
+void GDataFileSystemProxy::Remove(const GURL& file_url, bool recursive,
+    const FileSystemOperationInterface::StatusCallback& callback) {
+  FilePath file_path;
+  if (!ValidateUrl(file_url, &file_path)) {
+    OnFileOperationCompleted(MessageLoopProxy::current(), callback,
+        base::PLATFORM_FILE_ERROR_NOT_FOUND);
+    return;
+  }
+
+  file_system_->Remove(file_path, recursive,
+      base::Bind(&GDataFileSystemProxy::OnFileOperationCompleted,
+                 MessageLoopProxy::current(),
+                 callback));
+}
+
+// static.
+void GDataFileSystemProxy::OnFileOperationCompleted(
+    scoped_refptr<MessageLoopProxy> proxy,
+    const FileSystemOperationInterface::StatusCallback& callback,
+    base::PlatformFileError result) {
+  if (!callback.is_null())
+    proxy->PostTask(FROM_HERE, base::Bind(callback, result));
+}
+
+// static.
 bool GDataFileSystemProxy::ValidateUrl(const GURL& url, FilePath* file_path) {
   // what platform you're on.
   fileapi::FileSystemType type = fileapi::kFileSystemTypeUnknown;
--- webkit/chromeos/fileapi/remote_file_system_operation.cc
+++ webkit/chromeos/fileapi/remote_file_system_operation.cc
@@ -54,6 +54,15 @@ void RemoteFileSystemOperation::ReadDirectory(const GURL& path,
                  base::Owned(this), callback));
 }
 
+void RemoteFileSystemOperation::Remove(const GURL& path, bool recursive,
+                                       const StatusCallback& callback) {
+  DCHECK(SetPendingOperationType(kOperationRemove));
+  remote_proxy_->Remove(path, recursive,
+      base::Bind(&RemoteFileSystemOperation::DidFinishFileOperation,
+                 base::Owned(this), callback));
+}
+
+
 void RemoteFileSystemOperation::CreateFile(const GURL& path,
                                            bool exclusive,
                                            const StatusCallback& callback) {
@@ -75,11 +84,6 @@ void RemoteFileSystemOperation::Copy(const GURL& src_path,
 void RemoteFileSystemOperation::Move(const GURL& src_path,
                                      const GURL& dest_path,
                   const StatusCallback& callback) {
-  NOTIMPLEMENTED();
-}
-
-void RemoteFileSystemOperation::Remove(const GURL& path, bool recursive,
-                                       const StatusCallback& callback) {
   NOTIMPLEMENTED();
 }
 
@@ -169,6 +173,12 @@ void RemoteFileSystemOperation::DidReadDirectory(
     const std::vector<base::FileUtilProxy::Entry>& entries,
     bool has_more) {
   callback.Run(rv, entries, has_more /* has_more */);
+}
+
+void RemoteFileSystemOperation::DidFinishFileOperation(
+    const StatusCallback& callback,
+    base::PlatformFileError rv) {
+  callback.Run(rv);
 }
 
 }  // namespace chromeos
