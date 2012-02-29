5dffca51a8c6bf70119c2f8611d92753f8d4464c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 00:51:33 +0000

    ui/gfx: Simplify RectToSkRect() implementation by using iset() function.
    BUG=100898
    R=pkasting@chromium.org
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9500001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124076 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/skia_util.cc
+++ ui/gfx/skia_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,8 +16,7 @@ namespace gfx {
 
 SkRect RectToSkRect(const gfx::Rect& rect) {
   SkRect r;
-  r.set(SkIntToScalar(rect.x()), SkIntToScalar(rect.y()),
-        SkIntToScalar(rect.right()), SkIntToScalar(rect.bottom()));
+  r.iset(rect.x(), rect.y(), rect.right(), rect.bottom());
   return r;
 }
 
@@ -27,8 +26,8 @@ SkIRect RectToSkIRect(const gfx::Rect& rect) {
 }
 
 gfx::Rect SkRectToRect(const SkRect& rect) {
-  return gfx::Rect(static_cast<int>(rect.fLeft),
-                   static_cast<int>(rect.fTop),
+  return gfx::Rect(static_cast<int>(rect.left()),
+                   static_cast<int>(rect.top()),
                    static_cast<int>(rect.width()),
                    static_cast<int>(rect.height()));
 }
