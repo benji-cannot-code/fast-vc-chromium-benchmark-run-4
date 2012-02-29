1a981b61a1d8616f7e4383ab7a0953b5310095ad
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-02-29 05:16:22 +0000

    Chromium build fix attempt.
    * src/WebDragData.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebDragData.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebDragData.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChromiumDataObject.h"
 #include "ClipboardMimeTypes.h"
+#include "DOMStringList.h"
 #include "platform/WebData.h"
 #include "platform/WebString.h"
 #include "platform/WebURL.h"
