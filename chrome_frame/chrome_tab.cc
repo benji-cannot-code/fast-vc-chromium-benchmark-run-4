783113b317ba589b7f13e6aa082560aa815882bd
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 17:58:04 +0000

    Generate ETW events in Chrome Frame under the control of the same env var as Chrome.
    CHROME_ETW_LOGGING env var now controls whether or not npchrome_frame.dll generates ETW events.  This matches the behavior in chrome.exe.  I also tweaked the log settings so that the same info is included in npchrome_frame.dll log messages as those in chrome.exe.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9487011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124427 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/chrome_tab.cc
+++ chrome_frame/chrome_tab.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,10 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_piece.h"
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
+#include "base/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "base/win/windows_version.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/env_vars.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "chrome_frame/bho.h"
 #include "chrome_frame/chrome_active_document.h"
@@ -834,6 +836,12 @@ extern "C" BOOL WINAPI DllMain(HINSTANCE instance,
         logging::DELETE_OLD_LOG_FILE,
         logging::DISABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS);
 
+    // Log the same items as Chrome.
+    logging::SetLogItems(true,  // enable_process_id
+                         true,  // enable_thread_id
+                         false, // enable_timestamp
+                         true); // enable_tickcount
+
     DllRedirector* dll_redirector = DllRedirector::GetInstance();
     DCHECK(dll_redirector);
 
@@ -846,8 +854,11 @@ extern "C" BOOL WINAPI DllMain(HINSTANCE instance,
           << "Found CF module with no DllGetClassObject export.";
     }
 
-    // Enable ETW logging.
-    logging::LogEventProvider::Initialize(kChromeFrameProvider);
+    // Enable trace control and transport through event tracing for Windows.
+    if (::GetEnvironmentVariable(ASCIIToWide(env_vars::kEtwLogging).c_str(),
+                                 NULL, 0)) {
+      logging::LogEventProvider::Initialize(kChromeFrameProvider);
+    }
   } else if (reason == DLL_PROCESS_DETACH) {
     DllRedirector* dll_redirector = DllRedirector::GetInstance();
     DCHECK(dll_redirector);
