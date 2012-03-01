6a9fda6bb411b292b91d4cf2df96144d87e802fc
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 22:32:14 +0000

    Second attempt to land ffmpeg roll.
    Attempt 2 at landing http://codereview.chromium.org/9317096/
    
    Same as before except for fixes in checkperms/
    
    However, fixes have landed elsewhere for:
    - mp3 decode issue.
    - FrameRateNoVsyncCanvasInternalTest.fishbowl/0
    
    BUG=110776
    TEST=unittests, layouttests, trybots, perf tests...
    
    Review URL: https://chromiumcodereview.appspot.com/9447029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124501 0039d316-1c4b-4281-b951-d872f2087c98

--- media/test/ffmpeg_tests/ffmpeg_tests.cc
+++ media/test/ffmpeg_tests/ffmpeg_tests.cc
@@ -115,12 +115,11 @@ int main(int argc, const char** argv) {
 #endif
 
   // Register FFmpeg and attempt to open file.
-  avcodec_init();
   av_log_set_level(verbose_level);
   av_register_all();
   av_register_protocol2(&kFFmpegFileProtocol, sizeof(kFFmpegFileProtocol));
   AVFormatContext* format_context = NULL;
-  // av_open_input_file wants a char*, which can't work with wide paths.
+  // avformat_open_input() wants a char*, which can't work with wide paths.
   // So we assume ASCII on Windows.  On other platforms we can pass the
   // path bytes through verbatim.
 #if defined(OS_WIN)
@@ -128,8 +127,8 @@ int main(int argc, const char** argv) {
 #else
   const std::string& string_path = in_path.value();
 #endif
-  int result = av_open_input_file(&format_context, string_path.c_str(),
-                                  NULL, 0, NULL);
+  int result = avformat_open_input(&format_context, string_path.c_str(),
+                                   NULL, NULL);
   if (result < 0) {
     switch (result) {
       case AVERROR(EINVAL):
@@ -156,7 +155,7 @@ int main(int argc, const char** argv) {
   }
 
   // Parse a little bit of the stream to fill out the format context.
-  if (av_find_stream_info(format_context) < 0) {
+  if (avformat_find_stream_info(format_context, NULL) < 0) {
     std::cerr << "Error: Could not find stream info for "
               << in_path.value() << std::endl;
     return 1;
@@ -231,7 +230,7 @@ int main(int argc, const char** argv) {
   }
 
   codec_context->error_concealment = FF_EC_GUESS_MVS | FF_EC_DEBLOCK;
-  codec_context->error_recognition = FF_ER_CAREFUL;
+  codec_context->err_recognition = AV_EF_CAREFUL;
 
   // Initialize threaded decode.
   if (target_codec == AVMEDIA_TYPE_VIDEO && video_threads > 0) {
@@ -239,7 +238,7 @@ int main(int argc, const char** argv) {
   }
 
   // Initialize our codec.
-  if (avcodec_open(codec_context, codec) < 0) {
+  if (avcodec_open2(codec_context, codec, NULL) < 0) {
     std::cerr << "Error: Could not open codec "
               << codec_context->codec->name << " for "
               << in_path.value() << std::endl;
@@ -422,7 +421,7 @@ int main(int argc, const char** argv) {
   if (codec_context)
     avcodec_close(codec_context);
   if (format_context)
-    av_close_input_file(format_context);
+    avformat_close_input(&format_context);
 
   // Calculate the sum of times.  Note that some of these may be zero.
   double sum = 0;
