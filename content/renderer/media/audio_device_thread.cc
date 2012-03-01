0705e0d52743dc8ff1c06b8735f53e892cffd223
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 01:57:08 +0000

    Stop the AudioDeviceThread when the IO loop goes away.
    When shutting down, the pipeline code can take a long time to terminate.
    In some rare cases, the IO loop terminates before the pipeline thread and
    the audio device classes would attempt to use the io loop to perform tasks on.
    I checked in a candidate fix for this a couple of weeks ago which monitors the
    lifetime of the IO loop and uses MessageLoopProxy* instead of MessageLoop* to
    avoid issues with using the NULL loop.  That fixes part of the problem.
    What was missing was also stopping the audio thread in this case since the IO
    thread will run teardown before the Pipeline thread calls AudioDevice::Stop().
    In that period, the audio thread will still be running but the callback object
    has been deleted.
    
    Additionally, I'm adding a lock to guard the thread_ variable in AudioDeviceThread.
    Before I was relying on external synchronization in the AudioDevice and
    AudioInputDevice state machine.  However, it doesn't hurt to go belt and
    suspenders although it might not look cool ;)
    
    BUG=115299
    TEST=Follow repro steps in bug report.
    
    
    Review URL: http://codereview.chromium.org/9534002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124330 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/audio_device_thread.cc
+++ content/renderer/media/audio_device_thread.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_restrictions.h"
 #include "media/audio/audio_util.h"
@@ -28,6 +27,11 @@ class AudioDeviceThread::Thread
          const char* thread_name);
 
   void Start();
+
+  // Stops the thread.  If |loop_for_join| is non-NULL, the function posts
+  // a task to join (close) the thread handle later instead of waiting for
+  // the thread.  If loop_for_join is NULL, then the function waits
+  // synchronously for the thread to terminate.
   void Stop(MessageLoop* loop_for_join);
 
  private:
@@ -62,20 +66,23 @@ AudioDeviceThread::~AudioDeviceThread() {
 void AudioDeviceThread::Start(AudioDeviceThread::Callback* callback,
                               base::SyncSocket::Handle socket,
                               const char* thread_name) {
+  base::AutoLock auto_lock(thread_lock_);
   CHECK(thread_ == NULL);
   thread_ = new AudioDeviceThread::Thread(callback, socket, thread_name);
   thread_->Start();
 }
 
 void AudioDeviceThread::Stop(MessageLoop* loop_for_join) {
+  base::AutoLock auto_lock(thread_lock_);
   if (thread_) {
-    if (!loop_for_join) {
-      loop_for_join = MessageLoop::current();
-      CHECK(loop_for_join);
-    }
     thread_->Stop(loop_for_join);
     thread_ = NULL;
   }
+}
+
+bool AudioDeviceThread::IsStopped() {
+  base::AutoLock auto_lock(thread_lock_);
+  return thread_ == NULL;
 }
 
 // AudioDeviceThread::Thread implementation
@@ -114,8 +121,12 @@ void AudioDeviceThread::Thread::Stop(MessageLoop* loop_for_join) {
   }
 
   if (thread != base::kNullThreadHandle) {
-    loop_for_join->PostTask(FROM_HERE,
-        base::Bind(&base::PlatformThread::Join, thread));
+    if (loop_for_join) {
+      loop_for_join->PostTask(FROM_HERE,
+          base::Bind(&base::PlatformThread::Join, thread));
+    } else {
+      base::PlatformThread::Join(thread);
+    }
   }
 }
 
