b77bd9a3314055a7477123d54ec39786ce5ac9a2
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 19:15:52 +0000

    Coverity: No uninitialized fields in constructor. (CID 9446)
    BUG=none
    TEST=compile
    
    Review URL: https://chromiumcodereview.appspot.com/9549014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/external_process_importer_host.cc
+++ chrome/browser/importer/external_process_importer_host.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/in_process_importer_bridge.h"
 
 ExternalProcessImporterHost::ExternalProcessImporterHost()
-    : items_(0),
+    : client_(NULL),
+      source_profile_(NULL),
+      items_(0),
       cancelled_(false),
       import_process_launched_(false) {
 }
