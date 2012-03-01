f3fb2f1a940c66bb577003bfb69a0ea9fda55c97
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-01 21:27:36 +0000

    Prevent layout root to remain set on renderers getting destroyed. https://bugs.webkit.org/show_bug.cgi?id=79953
    Reviewed by Eric Seidel.
    
    Implement Julien Chaffraix's idea.
    
    * page/FrameView.h:
    (WebCore::FrameView::clearLayoutRoot): helper to clear layout root.
    * rendering/RenderObject.cpp:
    (WebCore::clearLayoutRootIfNeeded): if we know we are going
    away and we are the view's layout root, then we need to reset the layout
    root to prevent being used.
    (WebCore):
    (WebCore::RenderObject::willBeDestroyed): call clearLayoutRootIfNeeded at end.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109406 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -2241,6 +2241,21 @@ bool RenderObject::isSelectionBorder() const
     return st == SelectionStart || st == SelectionEnd || st == SelectionBoth;
 }
 
+inline void RenderObject::clearLayoutRootIfNeeded() const
+{
+    if (node() && !documentBeingDestroyed() && frame()) {
+        if (FrameView* view = frame()->view()) {
+            if (view->layoutRoot() == this) {
+                ASSERT_NOT_REACHED();
+                // This indicates a failure to layout the child, which is why
+                // the layout root is still set to |this|. Make sure to clear it
+                // since we are getting destroyed.
+                view->clearLayoutRoot();
+            }
+        }
+    }
+}
+
 void RenderObject::willBeDestroyed()
 {
     // Destroy any leftover anonymous children.
@@ -2291,6 +2306,8 @@ void RenderObject::willBeDestroyed()
         setHasLayer(false);
         toRenderBoxModelObject(this)->destroyLayer();
     }
+
+    clearLayoutRootIfNeeded();
 }
 
 void RenderObject::destroyAndCleanupAnonymousWrappers()
