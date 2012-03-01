b7ff96529babaa6c2a00ed7d887c78e55e213820
yuqiang.xian@intel.com <yuqiang.xian@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-01 07:39:31 +0000

    DFG BasicBlocks should not require that their nodes have continuous indices in the graph https://bugs.webkit.org/show_bug.cgi?id=79899
    Reviewed by Filip Pizlo.
    
    This will make it more convenient to insert nodes into the DFG.
    With this capability we now place the Phi nodes in the corresponding
    blocks.
    Local CSE is modified to not to rely on the assumption of continuous
    node indices in a block.
    This is performance neutral on SunSpider, V8 and Kraken.
    
    * dfg/DFGAbstractState.cpp:
    (JSC::DFG::AbstractState::AbstractState):
    (JSC::DFG::AbstractState::beginBasicBlock):
    (JSC::DFG::AbstractState::execute):
    (JSC::DFG::AbstractState::clobberStructures):
    (JSC::DFG::AbstractState::mergeToSuccessors):
    (JSC::DFG::AbstractState::dump):
    * dfg/DFGAbstractState.h:
    (JSC::DFG::AbstractState::forNode):
    (AbstractState):
    * dfg/DFGArithNodeFlagsInferencePhase.cpp:
    (ArithNodeFlagsInferencePhase):
    * dfg/DFGBasicBlock.h:
    (JSC::DFG::BasicBlock::BasicBlock):
    (BasicBlock):
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::addToGraph):
    (ByteCodeParser):
    (JSC::DFG::ByteCodeParser::insertPhiNode):
    (JSC::DFG::ByteCodeParser::handleInlining):
    (JSC::DFG::ByteCodeParser::parseBlock):
    (JSC::DFG::ByteCodeParser::processPhiStack):
    (JSC::DFG::ByteCodeParser::linkBlock):
    (JSC::DFG::ByteCodeParser::determineReachability):
    (JSC::DFG::ByteCodeParser::parseCodeBlock):
    * dfg/DFGCFAPhase.cpp:
    (JSC::DFG::CFAPhase::performBlockCFA):
    (CFAPhase):
    * dfg/DFGCSEPhase.cpp:
    (JSC::DFG::CSEPhase::CSEPhase):
    (JSC::DFG::CSEPhase::endIndexForPureCSE):
    (JSC::DFG::CSEPhase::pureCSE):
    (JSC::DFG::CSEPhase::impureCSE):
    (JSC::DFG::CSEPhase::globalVarLoadElimination):
    (JSC::DFG::CSEPhase::getByValLoadElimination):
    (JSC::DFG::CSEPhase::checkFunctionElimination):
    (JSC::DFG::CSEPhase::checkStructureLoadElimination):
    (JSC::DFG::CSEPhase::getByOffsetLoadElimination):
    (JSC::DFG::CSEPhase::getPropertyStorageLoadElimination):
    (JSC::DFG::CSEPhase::getIndexedPropertyStorageLoadElimination):
    (JSC::DFG::CSEPhase::getScopeChainLoadElimination):
    (JSC::DFG::CSEPhase::performNodeCSE):
    (JSC::DFG::CSEPhase::performBlockCSE):
    (CSEPhase):
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::dump):
    * dfg/DFGPhase.cpp:
    (JSC::DFG::Phase::beginPhase):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::nonSpeculativeCompare):
    (JSC::DFG::SpeculativeJIT::nonSpeculativeStrictEq):
    (JSC::DFG::SpeculativeJIT::compilePeepHoleBranch):
    (JSC::DFG::SpeculativeJIT::compile):
    (JSC::DFG::SpeculativeJIT::compileStrictEqForConstant):
    (JSC::DFG::SpeculativeJIT::compileStrictEq):
    * dfg/DFGSpeculativeJIT.h:
    (SpeculativeJIT):
    (JSC::DFG::SpeculativeJIT::detectPeepHoleBranch):
    (JSC::DFG::SpeculativeJIT::SpeculativeJIT):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::nonSpeculativeCompareNull):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::nonSpeculativeCompareNull):
    * dfg/DFGVirtualRegisterAllocationPhase.cpp:
    (JSC::DFG::VirtualRegisterAllocationPhase::run):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGBasicBlock.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGBasicBlock.h
@@ -39,11 +39,9 @@ namespace JSC { namespace DFG {
 
 typedef Vector <BlockIndex, 2> PredecessorList;
 
-struct BasicBlock {
-    BasicBlock(unsigned bytecodeBegin, NodeIndex begin, unsigned numArguments, unsigned numLocals)
+struct BasicBlock : Vector<NodeIndex, 8> {
+    BasicBlock(unsigned bytecodeBegin, unsigned numArguments, unsigned numLocals)
         : bytecodeBegin(bytecodeBegin)
-        , begin(begin)
-        , end(NoNode)
         , isOSRTarget(false)
         , cfaHasVisited(false)
         , cfaShouldRevisit(false)
@@ -51,6 +49,7 @@ struct BasicBlock {
         , isLinked(false)
 #endif
         , isReachable(false)
+        , startExcludingPhis(0)
         , variablesAtHead(numArguments, numLocals)
         , variablesAtTail(numArguments, numLocals)
         , valuesAtHead(numArguments, numLocals)
@@ -70,8 +69,6 @@ struct BasicBlock {
     // for other purposes due to inlining.
     unsigned bytecodeBegin;
     
-    NodeIndex begin;
-    NodeIndex end;
     bool isOSRTarget;
     bool cfaHasVisited;
     bool cfaShouldRevisit;
@@ -79,6 +76,7 @@ struct BasicBlock {
     bool isLinked;
 #endif
     bool isReachable;
+    unsigned startExcludingPhis;
     
     PredecessorList m_predecessors;
     
--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGPhase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGPhase.cpp
@@ -36,7 +36,7 @@ void Phase::beginPhase()
 {
     dataLog("Beginning DFG phase %s.\n", m_name);
     dataLog("Graph before %s:\n", m_name);
-    m_graph.dump(m_codeBlock);
+    m_graph.dump();
 }
 
 void Phase::endPhase()
