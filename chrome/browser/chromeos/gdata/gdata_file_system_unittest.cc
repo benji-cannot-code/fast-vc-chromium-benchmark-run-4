0e209925bbe9d78ad0757736004621cb36080c29
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 07:49:22 +0000

    gdata: Fix GDataFileSystemTest with Debug build
    The test failed with a DCHECK() as TokenService checked the current
    thread to be on UI thread, wheare as UI thread was not initialized
    in the test. The fix is as easy as providing a test UI thread.
    
    BUG=chromium-os:27081
    TEST=out/Debug/unit_tests --gtest_filter=GData*
    
    Review URL: https://chromiumcodereview.appspot.com/9569002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/json/json_file_value_serializer.h"
+#include "base/message_loop.h"
 #include "base/path_service.h"
 #include "base/string16.h"
 #include "base/time.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/gdata/gdata_parser.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/testing_profile.h"
+#include "content/test/test_browser_thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -31,6 +33,10 @@ namespace gdata {
 
 class GDataFileSystemTest : public testing::Test {
  protected:
+  GDataFileSystemTest()
+      : ui_thread_(content::BrowserThread::UI, &message_loop_) {
+  }
+
   virtual void SetUp() {
     profile_.reset(new TestingProfile);
     file_system_ = new GDataFileSystem(profile_.get());
@@ -121,6 +127,8 @@ class GDataFileSystemTest : public testing::Test {
     return value;
   }
 
+  MessageLoopForUI message_loop_;
+  content::TestBrowserThread ui_thread_;
   scoped_ptr<TestingProfile> profile_;
   scoped_refptr<GDataFileSystem> file_system_;
 };
