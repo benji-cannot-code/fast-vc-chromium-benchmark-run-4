ed52b8c479594559858ea7f5eb32bbdbb7d902da
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 17:09:03 +0000

    ui/gfx: Include missing skia_util.h to fix Linux ChromiumOS GTK build.
    TBR=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9567009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124421 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/native_theme_chromeos.cc
+++ ui/gfx/native_theme_chromeos.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
 #include "ui/gfx/skbitmap_operations.h"
+#include "ui/gfx/skia_util.h"
 
 namespace {
 
