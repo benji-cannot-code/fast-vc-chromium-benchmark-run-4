3231cb4ab0bda16e3eceaea599e057c4d5a79758
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-01 19:58:33 +0000

    Unreviewed, rolling out r109367. http://trac.webkit.org/changeset/109367 https://bugs.webkit.org/show_bug.cgi?id=75568
    Some tests started to fail in a non obvious way.
    
    Source/WebCore: 
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::updateScrollInfoAfterLayout):
    (WebCore::RenderBlock::layoutBlock):
    (WebCore::RenderBlock::paint):
    (WebCore::RenderBlock::isPointInOverflowControl):
    * rendering/RenderBlock.h:
    (RenderBlock):
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::willBeDestroyed):
    (WebCore::RenderBox::styleDidChange):
    (WebCore::RenderBox::scrollWidth):
    (WebCore::RenderBox::scrollHeight):
    (WebCore::RenderBox::scrollLeft):
    (WebCore::RenderBox::scrollTop):
    (WebCore::RenderBox::setScrollLeft):
    (WebCore::RenderBox::setScrollTop):
    (WebCore::RenderBox::includeVerticalScrollbarSize):
    (WebCore::RenderBox::includeHorizontalScrollbarSize):
    (WebCore::RenderBox::scrolledContentOffset):
    (WebCore::RenderBox::cachedSizeForOverflowClip):
    (WebCore::RenderBox::pushContentsClip):
    (WebCore::RenderBox::popContentsClip):
    (WebCore::RenderBox::addLayoutOverflow):
    * rendering/RenderBox.h:
    (RenderBox):
    (WebCore):
    * rendering/RenderBoxModelObject.cpp:
    (WebCore):
    (WebCore::RenderBoxModelObject::styleDidChange):
    * rendering/RenderBoxModelObject.h:
    (WebCore::RenderBoxModelObject::requiresLayer):
    (RenderBoxModelObject):
    * rendering/RenderDeprecatedFlexibleBox.cpp:
    (WebCore::RenderDeprecatedFlexibleBox::layoutBlock):
    * rendering/RenderTableRow.h:
    (WebCore::RenderTableRow::requiresLayer):
    
    LayoutTests: 
    
    * platform/chromium-win/fast/repaint/iframe-scroll-repaint-expected.png:
    * platform/chromium/test_expectations.txt:
    * platform/efl/test_expectations.txt:
    * platform/gtk/test_expectations.txt:
    * platform/mac/test_expectations.txt:
    * platform/qt/test_expectations.txt:
    * platform/win/Skipped:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -1312,18 +1312,12 @@ void RenderBlock::finishDelayUpdateScrollInfo()
 
 void RenderBlock::updateScrollInfoAfterLayout()
 {
-    if (!hasOverflowClip())
-        return;
-
-    if (!hasLayer()) {
-        updateCachedSizeForOverflowClip();
-        return;
+    if (hasOverflowClip()) {
+        if (gDelayUpdateScrollInfo)
+            gDelayedUpdateScrollInfoSet->add(this);
+        else
+            layer()->updateScrollInfoAfterLayout();
     }
-
-    if (gDelayUpdateScrollInfo)
-        gDelayedUpdateScrollInfoSet->add(this);
-    else
-        layer()->updateScrollInfoAfterLayout();
 }
 
 void RenderBlock::layout()
@@ -1550,7 +1544,7 @@ void RenderBlock::layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeigh
 
         repaintRect.inflate(maximalOutlineSize(PaintPhaseOutline));
         
-        if (hasOverflowClipWithLayer()) {
+        if (hasOverflowClip()) {
             // Adjust repaint rect for scroll offset
             repaintRect.move(-scrolledContentOffset());
 
@@ -2577,7 +2571,7 @@ void RenderBlock::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset)
     // Our scrollbar widgets paint exactly when we tell them to, so that they work properly with
     // z-index.  We paint after we painted the background/border, so that the scrollbars will
     // sit above the background/border.
-    if (hasOverflowClipWithLayer() && style()->visibility() == VISIBLE && (phase == PaintPhaseBlockBackground || phase == PaintPhaseChildBlockBackground) && paintInfo.shouldPaintWithinRoot(this))
+    if (hasOverflowClip() && style()->visibility() == VISIBLE && (phase == PaintPhaseBlockBackground || phase == PaintPhaseChildBlockBackground) && paintInfo.shouldPaintWithinRoot(this))
         layer()->paintOverflowControls(paintInfo.context, roundedIntPoint(adjustedPaintOffset), paintInfo.rect);
 }
 
@@ -4407,7 +4401,7 @@ LayoutUnit RenderBlock::getClearDelta(RenderBox* child, LayoutUnit logicalTop)
 
 bool RenderBlock::isPointInOverflowControl(HitTestResult& result, const LayoutPoint& pointInContainer, const LayoutPoint& accumulatedOffset)
 {
-    if (!scrollsOverflow() || !hasLayer())
+    if (!scrollsOverflow())
         return false;
 
     return layer()->hitTestOverflowControls(result, pointInContainer - toLayoutSize(accumulatedOffset));
