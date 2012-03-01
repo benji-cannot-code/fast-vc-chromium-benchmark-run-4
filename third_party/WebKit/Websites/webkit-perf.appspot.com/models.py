b07e24396260e92ccb3b950291b6d8cee4eff773
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-01 21:55:49 +0000

    Perf-o-matic: generate dashboard images for 30, 90, and 365 days https://bugs.webkit.org/show_bug.cgi?id=80016
    Reviewed by Eric Seidel.
    
    Add the ability to generate graphs for 30, 90, and 365 day graphs but only occasionally
    according to the heuristics in DashboardImage.needs_update.
    
    Also fix a bug in Runs.chart_params that y-axis grid was set to an incorrect value.
     
    * Websites/webkit-perf.appspot.com/controller.py:
    (schedule_runs_update):
    (RunsChartHandler.post):
    * Websites/webkit-perf.appspot.com/json_generators.py:
    (Runs.chart_params):
    * Websites/webkit-perf.appspot.com/json_generators_unittest.py:
    (RunsTest.test_chart_params_with_value):
    * Websites/webkit-perf.appspot.com/models.py:
    (DashboardImage):
    (DashboardImage.needs_update):
    * Websites/webkit-perf.appspot.com/models_unittest.py:
    (HelperTests._assert_there_is_exactly_one_id_holder_and_matches):
    (HelperTests.test_create_in_transaction_with_numeric_id_holder.execute):
    (HelperTests.test_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_failing_in_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_raising_in_create_in_transaction_with_numeric_id_holder):
    (HelperTests.test_delete_model_with_numeric_id_holder.execute):
    (HelperTests.test_delete_model_with_numeric_id_holder):
    (HelperTests.test_model_from_numeric_id.execute):
    (HelperTests):
    (HelperTests.test_model_from_numeric_id):
    (BranchTests.test_create_if_possible):
    (PlatformTests.test_create_if_possible):
    (BuilderTests.test_create):
    (BuilderTests.test_update_password):
    (BuilderTests.test_hashed_password):
    (BuilderTests.test_authenticate):
    (_create_some_builder):
    (_create_build):
    (BuildTests.test_get_or_insert_from_log):
    (TestModelTests.test_update_or_insert):
    (TestModelTests.test_update_or_insert_to_update):
    (TestModelTests.test_merge):
    (TestResultTests.test_get_or_insert_value):
    (TestResultTests.test_get_or_insert_stat_value):
    (TestResultTests.test_replace_to_change_test_name):
    (TestResultTests.test_replace_to_change_test_name_with_stat_value):
    (TestResultTests.test_replace_to_change_test_name_overrides_conflicting_result):
    (ReportLogTests._create_log_with_payload):
    (ReportLogTests.test_builder):
    (ReportLogTests.test_branch):
    (ReportLogTests.test_platform):
    (PersistentCacheTests._assert_persistent_cache):
    (PersistentCacheTests.test_set):
    (PersistentCacheTests.test_get):
    (DashboardImageTests):
    (DashboardImageTests.test_needs_update):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/webkit-perf.appspot.com/models.py
+++ third_party/WebKit/Websites/webkit-perf.appspot.com/models.py
@@ -30,9 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import hashlib
 import json
+import math
 import re
 
 from datetime import datetime
+from datetime import timedelta
 from google.appengine.ext import db
 from google.appengine.api import memcache
 from time import mktime
@@ -327,6 +329,16 @@ class PersistentCache(db.Model):
 
 class DashboardImage(db.Model):
     image = db.BlobProperty(required=True)
+    createdAt = db.DateTimeProperty(required=True, auto_now=True)
+
+    @classmethod
+    def needs_update(cls, branch_id, platform_id, test_id, display_days, now=datetime.now()):
+        if display_days < 10:
+            return True
+        image = DashboardImage.get_by_key_name(cls.key_name(branch_id, platform_id, test_id, display_days))
+        duration = math.sqrt(display_days) / 10
+        # e.g. 13 hours for 30 days, 23 hours for 90 days, and 46 hours for 365 days
+        return not image or image.createdAt < now - timedelta(duration)
 
     @staticmethod
     def key_name(branch_id, platform_id, test_id, display_days):
