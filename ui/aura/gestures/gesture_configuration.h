46ccc10cd2838ec2bc6c3ba38e1139eca69d117c
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 18:23:16 +0000

    Adding persistance for gesture recognition parms. Observer updates GestureRecognition parameters when settings change.
    BUG=114015
    
    
    Review URL: http://codereview.chromium.org/9460001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124433 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_configuration.h
+++ ui/aura/gestures/gesture_configuration.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "base/basictypes.h"
+#include "ui/aura/aura_export.h"
 
 namespace aura {
 
@@ -15,7 +16,7 @@ namespace aura {
 // approaches (windows, chrome, others).  This would turn into an
 // abstract base class.
 
-class GestureConfiguration {
+class AURA_EXPORT GestureConfiguration {
  public:
   static double max_touch_down_duration_in_seconds_for_click() {
     return max_touch_down_duration_in_seconds_for_click_;
