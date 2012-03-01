2812ad9bd0964e7a538764f7bb7a949f51f6ebab
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 21:48:42 +0000

    Aura: PostSubBuffer implementation for --ui-use-gpu-process
    This refactors a bit of code from EGLImageTransportSurface for TextureImageTransportSurface.
    
    BUG=99527
    TEST=run an Aura build of chrome (use_aura=1 chromeos=1) with all the flags! (--force-compositing-mode --ui-enable-partial-swap --enable-partial-swap --ui-use-gpu-process); go to google.com; see cursor blink; resize the window
    
    
    Review URL: http://codereview.chromium.org/9564008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124491 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/image_transport_surface.h
+++ content/common/gpu/image_transport_surface.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(ENABLE_GPU)
 
+#include <vector>
+
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
@@ -17,8 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_message.h"
 #include "ui/gfx/gl/gl_surface.h"
-#include "ui/gfx/size.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/rect.h"
+#include "ui/gfx/size.h"
 #include "ui/gfx/surface/transport_dib.h"
 
 class GpuChannelManager;
@@ -71,6 +74,13 @@ class ImageTransportSurface {
       CreateSurface(GpuChannelManager* manager,
                     GpuCommandBufferStub* stub,
                     const gfx::GLSurfaceHandle& handle);
+ protected:
+  // Used by certain implements of PostSubBuffer to determine
+  // how much needs to be copied between frames.
+  void GetRegionsToCopy(const gfx::Rect& previous_damage_rect,
+                        const gfx::Rect& new_damage_rect,
+                        std::vector<gfx::Rect>* regions);
+
  private:
   DISALLOW_COPY_AND_ASSIGN(ImageTransportSurface);
 };
