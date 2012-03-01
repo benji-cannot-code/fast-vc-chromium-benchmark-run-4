06005258f7e6129661a53c938833a1e9c6f86fe6
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 03:47:11 +0000

    [NTP4] Overlap of apps in NTP when deleting and reinstalling app
    R=estade@chromium.org
    TEST=Install app, move to first position on NTP, uninstall Extensions page, idle
    NTPs should be updated correctly.
    BUG=116284
    
    Review URL: https://chromiumcodereview.appspot.com/9549016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/tile_page.js
+++ chrome/browser/resources/ntp4/tile_page.js
@@ -299,7 +299,7 @@ cr.define('ntp', function() {
       if (opt_animate)
         this.firstChild.classList.add('removing-tile-contents');
       else
-        this.parentNode.removeChild(this);
+        this.tilePage.removeTile(this, false);
     },
 
     /**
