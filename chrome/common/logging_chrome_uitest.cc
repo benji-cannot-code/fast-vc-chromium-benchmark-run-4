0460e64bd72096d391b1f22a164dd8182dfdfeb8
petermayo@chromium.org <petermayo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 23:02:26 +0000

    Change the condition for disabling some tests.
    This more accurately reflects where they fail.  There seem to be very few
    Google branded Chrome builds that aren't official, but this test fails for
    them.
    
    BUG=113399
    TEST=Local passage.
    
    Review URL: http://codereview.chromium.org/9568014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124519 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/logging_chrome_uitest.cc
+++ chrome/common/logging_chrome_uitest.cc
@@ -111,7 +111,7 @@ TEST_F(AssertionTest, Assertion) {
 }
 #endif  // !defined(NDEBUG)
 
-#if !defined(OFFICIAL_BUILD)
+#if !defined(GOOGLE_CHROME_BUILD)
 // Only works on Linux in Release mode with CHROME_HEADLESS=1
 class CheckFalseTest : public UITest {
  protected:
@@ -136,7 +136,7 @@ TEST_F(CheckFalseTest, CheckFails) {
   expected_errors_ = EXPECTED_ASSERT_ERRORS;
   expected_crashes_ = EXPECTED_ASSERT_CRASHES;
 }
-#endif  // !defined(OFFICIAL_BUILD)
+#endif  // !defined(GOOGLE_CHROME_BUILD)
 
 // Tests whether we correctly fail on browser crashes during UI Tests.
 class RendererCrashTest : public UITest {
