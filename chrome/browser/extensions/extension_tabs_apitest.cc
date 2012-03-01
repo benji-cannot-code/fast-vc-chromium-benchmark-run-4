a2e1589573d58db92e7a5878aa333d3a2bbc2e47
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 09:54:24 +0000

    Reenable ExtensionApiTests.Tabs
    BUG=108492
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9537016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -36,14 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAYBE_UpdateWindowShowState UpdateWindowShowState
 #endif
 
-// See crbug.com/108492.
-#if defined(USE_AURA) && defined(OS_CHROMEOS)
-#define MAYBE_Tabs DISABLED_Tabs
-#else
-#define MAYBE_Tabs Tabs
-#endif
-
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Tabs) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
   // The test creates a tab and checks that the URL of the new tab
   // is that of the new tab page.  Make sure the pref that controls
   // this is set.
