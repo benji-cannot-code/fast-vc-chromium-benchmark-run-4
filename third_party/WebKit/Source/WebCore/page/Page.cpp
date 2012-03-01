820a489aa907bf1d747915ef9afc2e5618766e93
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-01 19:36:55 +0000

    Reviewed by Kevin Decker.
    Part of https://bugs.webkit.org/show_bug.cgi?id=79705
    
    Here's some of that tweaking promised in the comment. Adjusting 
    gMaximumUnpaintedAreaRatio down a bit.
    * page/Page.cpp:
    (WebCore):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109379 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Page.cpp
+++ third_party/WebKit/Source/WebCore/page/Page.cpp
@@ -1019,7 +1019,7 @@ static uint64_t gPaintedObjectCounterThreshold = 0;
 
 // These are magical constants that might be tweaked over time.
 static double gMinimumPaintedAreaRatio = 0.1;
-static double gMaximumUnpaintedAreaRatio = 0.1;
+static double gMaximumUnpaintedAreaRatio = 0.04;
 
 void Page::setRelevantRepaintedObjectsCounterThreshold(uint64_t threshold)
 {
