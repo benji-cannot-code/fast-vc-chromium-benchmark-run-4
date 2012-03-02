c1f3f43a5a1c9d3ee160a4482b32421ea6280fe3
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 16:08:31 +0000

    Add a declarative way in c/b/s/sync_prefs.cc to say which data types have prefs whose values are determined by others (e.g. APP_NOTIFICATIONS on APPS). Make the hand-written code use the system, and add {APP,EXTENSION}_SETTINGS.
    R=akalin@chromium.org
    TBR=estade@chromium.org
    BUG=114974
    TEST=unit_tests --gtest_filter=*Sync*
    
    
    Review URL: http://codereview.chromium.org/9500005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_prefs_unittest.cc
+++ chrome/browser/sync/sync_prefs_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -37,9 +37,11 @@ syncable::ModelTypeSet GetNonPassiveTypes() {
 // Returns all types visible from the setup UI.
 syncable::ModelTypeSet GetUserVisibleTypes() {
   syncable::ModelTypeSet user_visible_types(GetNonPassiveTypes());
-  user_visible_types.Remove(syncable::AUTOFILL_PROFILE);
-  user_visible_types.Remove(syncable::SEARCH_ENGINES);
   user_visible_types.Remove(syncable::APP_NOTIFICATIONS);
+  user_visible_types.Remove(syncable::APP_SETTINGS);
+  user_visible_types.Remove(syncable::AUTOFILL_PROFILE);
+  user_visible_types.Remove(syncable::EXTENSION_SETTINGS);
+  user_visible_types.Remove(syncable::SEARCH_ENGINES);
   return user_visible_types;
 }
 
@@ -113,6 +115,10 @@ TEST_F(SyncPrefsTest, PreferredTypesNotKeepEverythingSynced) {
     }
     if (it.Get() == syncable::APPS) {
       expected_preferred_types.Put(syncable::APP_NOTIFICATIONS);
+      expected_preferred_types.Put(syncable::APP_SETTINGS);
+    }
+    if (it.Get() == syncable::EXTENSIONS) {
+      expected_preferred_types.Put(syncable::EXTENSION_SETTINGS);
     }
     sync_prefs.SetPreferredDataTypes(non_passive_types, preferred_types);
     EXPECT_TRUE(expected_preferred_types.Equals(
