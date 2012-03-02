debf4ee5a5bad4c302bb02de62dfd30c2cb19eeb
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 03:10:19 +0000

    Ensure blobs and associated temp/shareable files survive URLRequest uploads.
    BUG=115603
    Review URL: https://chromiumcodereview.appspot.com/9569035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124581 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/upload_data.h
+++ net/base/upload_data.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/supports_user_data.h"
 #include "base/time.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_export.h"
@@ -33,7 +34,16 @@ class NET_EXPORT_PRIVATE ChunkCallback {
   virtual ~ChunkCallback() {}
 };
 
-class NET_EXPORT UploadData : public base::RefCounted<UploadData> {
+//-----------------------------------------------------------------------------
+// A very concrete class representing the data to be uploaded as part of a
+// URLRequest.
+//
+// Until there is a more abstract class for this, this one derives from
+// SupportsUserData to allow users to stash random data by
+// key and ensure it's destruction when UploadData is finally deleted.
+class NET_EXPORT UploadData
+    : public base::RefCounted<UploadData>,
+      public base::SupportsUserData {
  public:
   enum Type {
     TYPE_BYTES,
@@ -232,7 +242,7 @@ class NET_EXPORT UploadData : public base::RefCounted<UploadData> {
 
   friend class base::RefCounted<UploadData>;
 
-  ~UploadData();
+  virtual ~UploadData();
 
   std::vector<Element> elements_;
   int64 identifier_;
