5c2ef8a510c42927f7267d5800e3f8d5983f014a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-02 04:26:40 +0000

    [BlackBerry]Array of Cookies in HTTP request header are not in order. https://bugs.webkit.org/show_bug.cgi?id=79870
    Std::sort and HashMap are not stable. So cookies with the same creating
    time sometimes are sent disorder.
    Change std::sort with std::stable-sort.
    We don't need using HashMap to save so few cookies for one domain.
    It is a wast of time to create HashMap, too.
    So change it with vector.
    
    Patch by Jason Liu <jason.liu@torchmobile.com.cn> on 2012-03-01
    Reviewed by George Staikos.
    
    Test: http/tests/cookies/resources/setArraycookies.php
    
    * platform/blackberry/CookieManager.cpp:
    (WebCore::cookieSorter):
    (WebCore::CookieManager::getRawCookies):
    (WebCore::CookieManager::checkAndTreatCookie):
    (WebCore::CookieManager::addCookieToMap):
    * platform/blackberry/CookieManager.h:
    * platform/blackberry/CookieMap.cpp:
    (WebCore::CookieMap::addOrReplaceCookie):
    (WebCore::CookieMap::removeCookieAtIndex):
    (WebCore::CookieMap::removeCookie):
    (WebCore):
    (WebCore::CookieMap::getAllCookies):
    (WebCore::CookieMap::updateOldestCookie):
    (WebCore::CookieMap::deleteAllCookiesAndDomains):
    * platform/blackberry/CookieMap.h:
    (WebCore::CookieMap::count):
    (CookieMap):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/cookies/resources/setArraycookies.php
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php 
+// Setting Array of cookies
+setcookie("setArraycookie[three]", "cookiethree");
+setcookie("setArraycookie[two]", "cookietwo");
+setcookie("setArraycookie[one]", "cookieone");
+header('Location: setArraycookies-result.php');
+?>
+
