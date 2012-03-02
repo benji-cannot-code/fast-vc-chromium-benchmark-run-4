7dcc1c04840e54849f2e2b6e6ad3fcbc2f1a9ee4
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-02 06:33:00 +0000

    Render overflow controls of an RTL element to its left-side. https://bugs.webkit.org/show_bug.cgi?id=54623
    This change adds a new flag WTF_USE_RTL_SCROLLBAR and render the
    vertical scrollbars and resizers of RTL elements to their left side if
    this new flag is enabled.
    
    Patch by Hironori Bono <hbono@chromium.org> on 2012-03-01
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Test: platform/chromium/fast/events/rtl-scrollbar.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::addOverflowFromPositionedObjects): Move child elements right.
    (WebCore::RenderBlock::determineLogicalLeftPositionForChild): ditto.
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::overflowClipRect): Move the content rectangle right.
    * rendering/RenderLayer.cpp:
    (WebCore::cornerStart): Added a function that calculates the X position of a resizer.
    (WebCore):
    (WebCore::cornerRect): Use cornerStart to move a resizer.
    (WebCore::RenderLayer::verticalScrollbarStart): Added a function that calculates
    the X position of a vertical scrollbar.
    (WebCore::RenderLayer::horizontalScrollbarStart): Added a function that calculates
    the X position of a horizontal scrollbar.
    (WebCore::RenderLayer::scrollbarOffset): Render a vertical scrollbar to the left side
    and move a horizontal scrollbar right by the width of the vertical scrollbar.
    (WebCore::RenderLayer::invalidateScrollbarRect): ditto.
    (WebCore::RenderLayer::positionOverflowControls): ditto.
    (WebCore::RenderLayer::hitTestOverflowControls): ditto.
    * rendering/RenderLayer.h:
    (RenderLayer):
    * rendering/style/RenderStyle.h: Added shouldPlaceBlockDirectionScrollbarOnLogicalLeft,
    which returns if we need to move a left scrollbar to its right side.
    
    Source/WebKit/chromium:
    
    * features.gypi: Set WTF_USE_RTL_SCROLLBAR to 1 on Chromium.
    
    LayoutTests:
    
    * platform/chromium/fast/events/rtl-scrollbar-expected.txt: Added.
    * platform/chromium/fast/events/rtl-scrollbar.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -1653,8 +1653,12 @@ void RenderBlock::addOverflowFromPositionedObjects()
         positionedObject = *it;
         
         // Fixed positioned elements don't contribute to layout overflow, since they don't scroll with the content.
-        if (positionedObject->style()->position() != FixedPosition)
-            addOverflowFromChild(positionedObject);
+        if (positionedObject->style()->position() != FixedPosition) {
+            int x = positionedObject->x();
+            if (style()->shouldPlaceBlockDirectionScrollbarOnLogicalLeft())
+                x -= verticalScrollbarWidth();
+            addOverflowFromChild(positionedObject, IntSize(x, positionedObject->y()));
+        }
     }
 }
 
@@ -2050,6 +2054,8 @@ LayoutUnit RenderBlock::computeStartPositionDeltaForChildAvoidingFloats(const Re
 void RenderBlock::determineLogicalLeftPositionForChild(RenderBox* child)
 {
     LayoutUnit startPosition = borderStart() + paddingStart();
+    if (style()->shouldPlaceBlockDirectionScrollbarOnLogicalLeft())
+        startPosition -= verticalScrollbarWidth();
     LayoutUnit totalAvailableLogicalWidth = borderAndPaddingLogicalWidth() + availableLogicalWidth();
 
     // Add in our start margin.
