70b25b9073f8108bb76c5215cada88ffcb2a4cb5
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 12:45:29 +0000

    Make socket permission also allow Pepper sockets
    BUG=116225
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/9539011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124628 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_content_browser_client.cc
+++ content/shell/shell_content_browser_client.cc
@@ -329,7 +329,8 @@ std::string ShellContentBrowserClient::GetDefaultDownloadName() {
   return "download";
 }
 
-bool ShellContentBrowserClient::AllowSocketAPI(const GURL& url) {
+bool ShellContentBrowserClient::AllowSocketAPI(BrowserContext* browser_context,
+                                               const GURL& url) {
   return false;
 }
 
