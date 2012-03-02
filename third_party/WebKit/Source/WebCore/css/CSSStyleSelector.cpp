bdd54a7e8c0d2764c533609f0b5f850a481ff05d
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-02 08:50:08 +0000

    Handle CSSPropertyWebkitColorCorrection in CSSStyleApplyProperty. https://bugs.webkit.org/show_bug.cgi?id=80056
    Reviewed by Eric Seidel.
    
    No new tests / refactoring only.
    
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::CSSStyleApplyProperty::CSSStyleApplyProperty):
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::collectMatchingRulesForList):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.cpp
@@ -3645,9 +3645,6 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
         return;
     }
 #endif
-    case CSSPropertyWebkitColorCorrection:
-        HANDLE_INHERIT_AND_INITIAL_AND_PRIMITIVE(colorSpace, ColorSpace);
-        return;
     case CSSPropertyInvalid:
         return;
     // Directional properties are resolved by resolveDirectionAwareProperty() before the switch.
@@ -3968,6 +3965,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSSPropertyWebkitBoxOrdinalGroup:
     case CSSPropertyWebkitBoxOrient:
     case CSSPropertyWebkitBoxPack:
+    case CSSPropertyWebkitColorCorrection:
     case CSSPropertyWebkitColumnAxis:
     case CSSPropertyWebkitColumnCount:
     case CSSPropertyWebkitColumnGap:
