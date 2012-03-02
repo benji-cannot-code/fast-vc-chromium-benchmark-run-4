c8dd594c95409ac78524cb66423c2211a48ab948
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 07:42:49 +0000

    IndexedDB: chrome-side changes for permission check from shared worker
    https://bugs.webkit.org/show_bug.cgi?id=79954 is the webkit side.
    
    BUG=116344
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9557009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124618 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/mock_content_browser_client.cc
+++ content/browser/mock_content_browser_client.cc
@@ -155,6 +155,14 @@ bool MockContentBrowserClient::AllowWorkerFileSystem(
   return true;
 }
 
+bool MockContentBrowserClient::AllowWorkerIndexedDB(
+    const GURL& url,
+    const string16& name,
+    content::ResourceContext* context,
+    const std::vector<std::pair<int, int> >& render_views) {
+  return true;
+}
+
 QuotaPermissionContext*
     MockContentBrowserClient::CreateQuotaPermissionContext() {
   return NULL;
--- content/common/worker_messages.h
+++ content/common/worker_messages.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -134,6 +134,13 @@ IPC_SYNC_MESSAGE_CONTROL5_1(WorkerProcessHostMsg_AllowDatabase,
 IPC_SYNC_MESSAGE_CONTROL2_1(WorkerProcessHostMsg_AllowFileSystem,
                             int /* worker_route_id */,
                             GURL /* origin url */,
+                            bool /* result */)
+
+// Sent by the worker process to check whether access to IndexedDB is allowed.
+IPC_SYNC_MESSAGE_CONTROL3_1(WorkerProcessHostMsg_AllowIndexedDB,
+                            int /* worker_route_id */,
+                            GURL /* origin url */,
+                            string16 /* database name */,
                             bool /* result */)
 
 //-----------------------------------------------------------------------------
