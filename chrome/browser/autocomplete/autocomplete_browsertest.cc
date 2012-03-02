75d1bb86cace9348bf573a8bcd86cf10bdb58b18
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 17:57:30 +0000

    Some fixes split off from https://chromiumcodereview.appspot.com/9570064/ to try and make the omnibox_view_browsertest.cc tests less flaky and make it possible to use the HQP in them.
    BUG=none
    TEST=none
    TBR=mrossetti
    Review URL: https://chromiumcodereview.appspot.com/9586012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124681 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_browsertest.cc
+++ chrome/browser/autocomplete/autocomplete_browsertest.cc
@@ -29,15 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_types.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// Autocomplete test is flaky on ChromeOS.
-// http://crbug.com/52928
-#if defined(OS_CHROMEOS)
-#define MAYBE_Autocomplete DISABLED_Autocomplete
-#else
-#define MAYBE_Autocomplete Autocomplete
-#endif
-
-
 namespace {
 
 string16 AutocompleteResultAsString(const AutocompleteResult& result) {
@@ -103,6 +94,14 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, Basic) {
             location_bar->location_entry()->GetText());
   EXPECT_FALSE(location_bar->location_entry()->IsSelectAll());
 }
+
+// Autocomplete test is flaky on ChromeOS.
+// http://crbug.com/52928
+#if defined(OS_CHROMEOS)
+#define MAYBE_Autocomplete DISABLED_Autocomplete
+#else
+#define MAYBE_Autocomplete Autocomplete
+#endif
 
 IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, MAYBE_Autocomplete) {
   // The results depend on the history backend being loaded. Make sure it is
