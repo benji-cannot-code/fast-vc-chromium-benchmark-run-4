73e6b9750ae0d41d12ce48c35bbe0a7fc4fac655
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 18:10:19 +0000

    NaCl: Cleanup: Get some definitions from header file
    Remove the duplicated prototypes and typedefs now that we have a NaCl
    header file to get them from that can be #included from Chromium
    without conflicts.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2628
    TEST=compile
    
    Review URL: https://chromiumcodereview.appspot.com/9565046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124685 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_listener.cc
+++ chrome/nacl/nacl_listener.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_switches.h"
 #include "native_client/src/shared/imc/nacl_imc.h"
+#include "native_client/src/trusted/service_runtime/sel_main_chrome.h"
 
 #if defined(OS_LINUX)
 #include "content/public/common/child_process_sandbox_support_linux.h"
@@ -24,15 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <io.h>
 #endif
-
-// This is ugly.  We need an interface header file for the exported
-// sel_ldr interfaces.
-// TODO(gregoryd,sehr): Add an interface header.
-#if defined(OS_WIN)
-typedef HANDLE NaClHandle;
-#else
-typedef int NaClHandle;
-#endif  // NaClHandle
 
 #if defined(OS_MACOSX)
 namespace {
@@ -69,11 +61,6 @@ int CreateMemoryObject(size_t size, bool executable) {
 
 }  // namespace
 #endif  // defined(OS_MACOSX)
-
-extern "C" void NaClMainForChromium(int handle_count,
-                                    const NaClHandle* handles,
-                                    int debug);
-extern "C" void NaClSetIrtFileDesc(int fd);
 
 NaClListener::NaClListener() : debug_enabled_(false) {}
 
