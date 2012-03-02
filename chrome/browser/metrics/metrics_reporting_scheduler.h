60e7b7ba7ea001696c776362a3c0aab0e91b9da0
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 16:04:46 +0000

    Switch MetricsReportingScheduler to using an actual timer object
    This simplifies the code a bit, and makes it possible to handle Stop() more cleanly.
    
    BUG=None
    TEST=Existing metrics all continue to work as normal
    
    
    Review URL: http://codereview.chromium.org/7001003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124656 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_reporting_scheduler.h
+++ chrome/browser/metrics/metrics_reporting_scheduler.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time.h"
+#include "base/timer.h"
 
 // Scheduler task to drive a MetricsService object's uploading.
 class MetricsReportingScheduler {
@@ -47,7 +48,7 @@ class MetricsReportingScheduler {
   // The MetricsService method to call when uploading should happen.
   base::Closure upload_callback_;
 
-  base::WeakPtrFactory<MetricsReportingScheduler> weak_ptr_factory_;
+  base::OneShotTimer<MetricsReportingScheduler> upload_timer_;
 
   // The interval between being told an upload is done and starting the next
   // upload.
@@ -56,10 +57,6 @@ class MetricsReportingScheduler {
   // Indicates that the scheduler is running (i.e., that Start has been called
   // more recently than Stop).
   bool running_;
-
-  // Indicates that a timer for triggering the next upload has already been
-  // started.
-  bool timer_pending_;
 
   // Indicates that the last triggered upload hasn't resolved yet.
   bool callback_pending_;
