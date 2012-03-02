e50f8d8fefcc2aeba7bdc07a345738b6b1e0feb0
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-02 02:29:59 +0000

    REGRESSION(r106388): Form state is restored to a wrong document. https://bugs.webkit.org/show_bug.cgi?id=79206
    Reviewed by Brady Eidson.
    
    Source/WebCore:
    
    In some cases, the URL of the current HistoryItem and the document
    URL are mismatched.
    A form state should be restored only if the document was loaded
    with a HistoryItem and the document is not loaded as a
    redirection.
    
    Test: fast/loader/form-state-restore-with-locked-back-forward-list.html
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::checkCompleted): Clear m_requestedHistoryItem.
    (WebCore::FrameLoader::loadItem):
    Save the requested HistoryItem for didLoadWithLodItem().
    * loader/FrameLoader.h:
    (WebCore::FrameLoader::requestedHistoryItem):
    Added. Accessor for m_requestedHistoryItem.
    * loader/HistoryController.cpp:
    (WebCore::HistoryController::restoreDocumentState):
    Restore a form state only if the current document was loaded with
    FrameLoader::loadItem() and not redirection.
    
    LayoutTests:
    
    * fast/loader/form-state-restore-with-locked-back-forward-list-expected.txt: Added.
    * fast/loader/form-state-restore-with-locked-back-forward-list.html: Added.
    * fast/loader/resources/form-state-restore-with-locked-back-forward-list-2.html: Added.
    * fast/loader/resources/form-state-restore-with-locked-back-forward-list-3.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.h
@@ -120,6 +120,7 @@ public:
 
     void open(CachedFrameBase&);
     void loadItem(HistoryItem*, FrameLoadType);
+    HistoryItem* requestedHistoryItem() const { return m_requestedHistoryItem.get(); }
 
     static void reportLocalLoadFailed(Frame*, const String& url);
 
@@ -438,6 +439,7 @@ private:
 #endif
 
     KURL m_previousUrl;
+    RefPtr<HistoryItem> m_requestedHistoryItem;
 };
 
 // This function is called by createWindow() in JSDOMWindowBase.cpp, for example, for
