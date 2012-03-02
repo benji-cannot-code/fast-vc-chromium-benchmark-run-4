855386162c5cb43266fcb66e9c89584b2908258f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 01:07:39 +0000

    Set incoming_only flag on the host side when NAT traversal is disabled.
    BUG=115145
    
    Review URL: https://chromiumcodereview.appspot.com/9549029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124555 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/session_manager.h
+++ remoting/protocol/session_manager.h
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/threading/non_thread_safe.h"
 #include "remoting/protocol/session.h"
+#include "remoting/protocol/transport_config.h"
 
 namespace remoting {
 
@@ -73,18 +74,25 @@ class AuthenticatorFactory;
 // TODO(sergeyu): Remove this struct and use TransportConfig instead.
 struct NetworkSettings {
   NetworkSettings()
-      : allow_nat_traversal(false),
+      : nat_traversal_mode(TransportConfig::NAT_TRAVERSAL_DISABLED),
+        max_port(0) {
+  }
+
+  explicit NetworkSettings(bool allow_nat_traversal)
+      : nat_traversal_mode(allow_nat_traversal ?
+                           TransportConfig::NAT_TRAVERSAL_ENABLED :
+                           TransportConfig::NAT_TRAVERSAL_DISABLED),
         min_port(0),
         max_port(0) {
   }
 
-  explicit NetworkSettings(bool allow_nat_traversal_value)
-      : allow_nat_traversal(allow_nat_traversal_value),
+  explicit NetworkSettings(TransportConfig::NatTraversalMode nat_traversal_mode)
+      : nat_traversal_mode(nat_traversal_mode),
         min_port(0),
         max_port(0) {
   }
 
-  bool allow_nat_traversal;
+  TransportConfig::NatTraversalMode nat_traversal_mode;
 
   // |min_port| and |max_port| specify range (inclusive) of ports used by
   // P2P sessions. Any port can be used when both values are set to 0.
