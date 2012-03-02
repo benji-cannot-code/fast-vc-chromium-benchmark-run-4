4b1fc5ab51447546d8b90dda01c11892d60f925c
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 15:00:48 +0000

    Use RelayFileTask instead of CopyOrMoveHelper
    BUG=114732
    TEST='existing tests'
    
    
    Review URL: http://codereview.chromium.org/9570061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124639 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_file_util_proxy.cc
+++ webkit/fileapi/file_system_file_util_proxy.cc
@@ -24,28 +24,6 @@ namespace {
 
 typedef fileapi::FileSystemFileUtilProxy Proxy;
 
-class CopyOrMoveHelper {
- public:
-  CopyOrMoveHelper(CrossFileUtilHelper* helper)
-      : helper_(helper),
-        error_(base::PLATFORM_FILE_OK) {}
-  ~CopyOrMoveHelper() {}
-
-  void RunWork() {
-    error_ = helper_->DoWork();
-  }
-
-  void Reply(const Proxy::StatusCallback& callback) {
-    if (!callback.is_null())
-      callback.Run(error_);
-  }
-
- private:
-  scoped_ptr<CrossFileUtilHelper> helper_;
-  base::PlatformFileError error_;
-  DISALLOW_COPY_AND_ASSIGN(CopyOrMoveHelper);
-};
-
 class EnsureFileExistsHelper {
  public:
   EnsureFileExistsHelper() : error_(base::PLATFORM_FILE_OK), created_(false) {}
@@ -154,14 +132,15 @@ bool FileSystemFileUtilProxy::Copy(
     const FileSystemPath& src_path,
     const FileSystemPath& dest_path,
     const StatusCallback& callback) {
-  CopyOrMoveHelper* helper = new CopyOrMoveHelper(
+  CrossFileUtilHelper* helper =
       new CrossFileUtilHelper(
           context, src_util, dest_util, src_path, dest_path,
-          CrossFileUtilHelper::OPERATION_COPY));
-  return message_loop_proxy->PostTaskAndReply(
-        FROM_HERE,
-        Bind(&CopyOrMoveHelper::RunWork, Unretained(helper)),
-        Bind(&CopyOrMoveHelper::Reply, Owned(helper), callback));
+          CrossFileUtilHelper::OPERATION_COPY);
+
+  return base::FileUtilProxy::RelayFileTask(
+      message_loop_proxy, FROM_HERE,
+      Bind(&CrossFileUtilHelper::DoWork, Owned(helper)),
+      callback);
 }
 
 // static
@@ -173,14 +152,14 @@ bool FileSystemFileUtilProxy::Move(
       const FileSystemPath& src_path,
       const FileSystemPath& dest_path,
     const StatusCallback& callback) {
-  CopyOrMoveHelper* helper = new CopyOrMoveHelper(
+  CrossFileUtilHelper* helper =
       new CrossFileUtilHelper(
           context, src_util, dest_util, src_path, dest_path,
-          CrossFileUtilHelper::OPERATION_MOVE));
-  return message_loop_proxy->PostTaskAndReply(
-        FROM_HERE,
-        Bind(&CopyOrMoveHelper::RunWork, Unretained(helper)),
-        Bind(&CopyOrMoveHelper::Reply, Owned(helper), callback));
+          CrossFileUtilHelper::OPERATION_MOVE);
+  return base::FileUtilProxy::RelayFileTask(
+      message_loop_proxy, FROM_HERE,
+      Bind(&CrossFileUtilHelper::DoWork, Owned(helper)),
+      callback);
 }
 
 // static
