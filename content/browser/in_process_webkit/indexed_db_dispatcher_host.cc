f5f93b383251235f6cb5f90c2f861a9ab533f043
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 23:59:04 +0000

    Fix and re-enable IndexedDBBrowserTestWithLowQuota.QuotaTest
    There were two problems.
    1) We had started to write into profile/IndexedDB/IndexedDB/origin.leveldb/ (note the double IndexedDB).
    2) We were writing into file__0.leveldb but checking for quota in __0.leveldb.  Now we always use __0.leveldb.
    
    BUG=104748
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9567030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124782 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_dispatcher_host.cc
+++ content/browser/in_process_webkit/indexed_db_dispatcher_host.cc
@@ -30,11 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebIDBTransaction.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityOrigin.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebVector.h"
+#include "webkit/database/database_util.h"
 #include "webkit/glue/webkit_glue.h"
 
 using content::BrowserMessageFilter;
 using content::BrowserThread;
 using content::UserMetricsAction;
+using webkit_database::DatabaseUtil;
 using WebKit::WebDOMStringList;
 using WebKit::WebExceptionCode;
 using WebKit::WebIDBCallbacks;
@@ -207,22 +209,10 @@ WebIDBCursor* IndexedDBDispatcherHost::GetCursorFromId(int32 cursor_id) {
 void IndexedDBDispatcherHost::OnIDBFactoryGetDatabaseNames(
     const IndexedDBHostMsg_FactoryGetDatabaseNames_Params& params) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT_DEPRECATED));
-  FilePath base_path = indexed_db_context_->data_path();
-  FilePath indexed_db_path;
-  if (!base_path.empty()) {
-    indexed_db_path = base_path.Append(
-        IndexedDBContextImpl::kIndexedDBDirectory);
-  }
+  FilePath indexed_db_path = indexed_db_context_->data_path();
 
-  // TODO(jorlow): This doesn't support file:/// urls properly. We probably need
-  //               to add some toString method to WebSecurityOrigin that doesn't
-  //               return null for them.  Look at
-  //               DatabaseUtil::GetOriginFromIdentifier.
   WebSecurityOrigin origin(
       WebSecurityOrigin::createFromDatabaseIdentifier(params.origin));
-  GURL origin_url(origin.toString());
-
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT_DEPRECATED));
 
   Context()->GetIDBFactory()->getDatabaseNames(
       new IndexedDBCallbacks<WebDOMStringList>(this, params.thread_id,
@@ -233,20 +223,11 @@ void IndexedDBDispatcherHost::OnIDBFactoryGetDatabaseNames(
 void IndexedDBDispatcherHost::OnIDBFactoryOpen(
     const IndexedDBHostMsg_FactoryOpen_Params& params) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT_DEPRECATED));
-  FilePath base_path = indexed_db_context_->data_path();
-  FilePath indexed_db_path;
-  if (!base_path.empty()) {
-    indexed_db_path = base_path.Append(
-        IndexedDBContextImpl::kIndexedDBDirectory);
-  }
+  FilePath indexed_db_path = indexed_db_context_->data_path();
 
-  // TODO(jorlow): This doesn't support file:/// urls properly. We probably need
-  //               to add some toString method to WebSecurityOrigin that doesn't
-  //               return null for them.  Look at
-  //               DatabaseUtil::GetOriginFromIdentifier.
+  GURL origin_url = DatabaseUtil::GetOriginFromIdentifier(params.origin);
   WebSecurityOrigin origin(
       WebSecurityOrigin::createFromDatabaseIdentifier(params.origin));
-  GURL origin_url(origin.toString());
 
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT_DEPRECATED));
 
@@ -261,12 +242,7 @@ void IndexedDBDispatcherHost::OnIDBFactoryOpen(
 
 void IndexedDBDispatcherHost::OnIDBFactoryDeleteDatabase(
     const IndexedDBHostMsg_FactoryDeleteDatabase_Params& params) {
-  FilePath base_path = indexed_db_context_->data_path();
-  FilePath indexed_db_path;
-  if (!base_path.empty()) {
-    indexed_db_path = base_path.Append(
-        IndexedDBContextImpl::kIndexedDBDirectory);
-  }
+  FilePath indexed_db_path = indexed_db_context_->data_path();
 
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT_DEPRECATED));
   Context()->GetIDBFactory()->deleteDatabase(
@@ -1209,6 +1185,7 @@ void IndexedDBDispatcherHost::
     return;
 
   // TODO(dgrogan): Tell the page the transaction aborted because of quota.
+  // http://crbug.com/113118
   if (parent_->Context()->WouldBeOverQuota(
       transaction_url_map_[transaction_id],
       transaction_size_map_[transaction_id])) {
