ae738e5a94002d3bf63d1b6ea0d26af2964acdfe
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 15:21:24 +0000

    Revert r124635 "Record taskbar launch mode in UMA histogram"
    Causes a debug-only compile failure on Windows.
    
    TBR=ben@chromium.org
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9582008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124644 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_init.cc
+++ chrome/browser/ui/browser_init.cc
@@ -128,7 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
-#include "base/win/windows_version.h"
 #include "chrome/installer/util/auto_launch_util.h"
 #endif
 
@@ -494,7 +493,7 @@ enum LaunchMode {
   LM_SHORTCUT_UNKNOWN,        // Launched from user-defined shortcut.
   LM_SHORTCUT_QUICKLAUNCH,    // Launched from the quick launch bar.
   LM_SHORTCUT_DESKTOP,        // Launched from a desktop shortcut.
-  LM_SHORTCUT_TASKBAR,        // Launched from the taskbar.
+  LM_SHORTCUT_STARTMENU,      // Launched from start menu.
   LM_LINUX_MAC_BEOS           // Other OS buckets start here.
 };
 
@@ -508,14 +507,10 @@ LaunchMode GetLaunchShortcutKind() {
   if (si.dwFlags & 0x800) {
     if (!si.lpTitle)
       return LM_SHORTCUT_NONAME;
-    string16 shortcut(si.lpTitle);
+    std::wstring shortcut(si.lpTitle);
     // The windows quick launch path is not localized.
-    if (shortcut.find(L"\\Quick Launch\\") != string16::npos) {
-      if (base::win::GetVersion() >= base::win::VERSION_WIN7)
-        return LM_SHORTCUT_TASKBAR;
-      else
-        return LM_SHORTCUT_QUICKLAUNCH;
-    }
+    if (shortcut.find(L"\\Quick Launch\\") != std::wstring::npos)
+      return LM_SHORTCUT_QUICKLAUNCH;
     scoped_ptr<base::Environment> env(base::Environment::Create());
     std::string appdata_path;
     env->GetVar("USERPROFILE", &appdata_path);
