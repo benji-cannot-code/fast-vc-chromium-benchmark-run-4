e1705b4a01ba8c08739bceb44b49cae0dfe0cde0
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-02 19:58:36 +0000

    Source/WebCore: REGRESSION(r97353): Crash when accessing location or history properties inside a navigated window https://bugs.webkit.org/show_bug.cgi?id=80133 <rdar://problem/10432233>
    Reviewed by Antti Koivisto.
    
    Test: fast/dom/Window/navigated-window-properties.html
    
    * bindings/js/JSDOMWindowCustom.cpp:
    (WebCore): Remove custom getters for window.location and window.history; they
    were unnecessary and did the wrong thing when DOMWindow returned null values 
    for these.
    * page/DOMWindow.idl: ditto
    * bindings/js/JSDOMBinding.cpp:
    (WebCore::reportException): Remove assert about null values and update comment,
    since this is now an expected state for navigated inner windows.
    
    LayoutTests: REGRESSION(r97353): Crash when accessing location or history properties inside a navigated window
    https://bugs.webkit.org/show_bug.cgi?id=80133
    
    Reviewed by Antti Koivisto.
    
    * fast/dom/Window/navigated-window-properties-expected.txt: Added.
    * fast/dom/Window/navigated-window-properties.html: Added.
    * fast/dom/Window/resources/navigated-window-prop-subframe1.html: Added.
    * fast/dom/Window/resources/navigated-window-prop-subframe2.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/sql/SQLiteStatement.cpp
+++ third_party/WebKit/Source/WebCore/platform/sql/SQLiteStatement.cpp
@@ -98,7 +98,7 @@ int SQLiteStatement::step()
     MutexLocker databaseLock(m_database.databaseMutex());
     if (m_database.isInterrupted())
         return SQLITE_INTERRUPT;
-    ASSERT(m_isPrepared);
+    //ASSERT(m_isPrepared);
 
     if (!m_statement)
         return SQLITE_OK;
