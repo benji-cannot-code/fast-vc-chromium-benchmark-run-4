dcfa1d7077446b8fc83474094d1d9fb1e6626f3d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-03 22:30:23 +0000

    Simplified discreteTimeConstantForSampleRate https://bugs.webkit.org/show_bug.cgi?id=78051
    Remove hardcoded constant and simplify computation of discrete
    time constant.
    
    Patch by Raymond Toy <rtoy@google.com> on 2012-03-03
    Reviewed by Chris Rogers.
    
    No new tests needed.  Covered by existing AudioParam tests.
    
    * platform/audio/AudioUtilities.cpp:
    (WebCore::AudioUtilities::discreteTimeConstantForSampleRate):
    Simplify formula.
    * platform/audio/AudioUtilities.h:
    (AudioUtilities): Update function signature.
    * platform/audio/DynamicsCompressorKernel.cpp:
    (WebCore::DynamicsCompressorKernel::DynamicsCompressorKernel):
    cast to float to avoid warnings.
    * webaudio/AudioParamTimeline.cpp:
    (WebCore::AudioParamTimeline::valuesForTimeRangeImpl): cast to
    float to avoid warnings.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/DynamicsCompressorKernel.cpp
+++ third_party/WebKit/Source/WebCore/platform/audio/DynamicsCompressorKernel.cpp
@@ -69,7 +69,7 @@ DynamicsCompressorKernel::DynamicsCompressorKernel(float sampleRate, unsigned nu
     // Initializes most member variables
     reset();
 
-    m_meteringReleaseK = discreteTimeConstantForSampleRate(meteringReleaseTimeConstant, sampleRate);
+    m_meteringReleaseK = static_cast<float>(discreteTimeConstantForSampleRate(meteringReleaseTimeConstant, sampleRate));
 }
 
 void DynamicsCompressorKernel::setNumberOfChannels(unsigned numberOfChannels)
