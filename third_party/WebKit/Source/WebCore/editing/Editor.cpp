4351b3c22c2d784abfa767b55ca21ed25986df35
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-03 05:48:54 +0000

    Editor's default paragraph separator is not reset when a frame's document is changed https://bugs.webkit.org/show_bug.cgi?id=80065
    Patch by Pablo Flouret <pablof@motorola.com> on 2012-03-02
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Test: editing/execCommand/reset-values-after-navigation.html
    
    * editing/Editor.cpp:
    (WebCore::Editor::clear):
    
    LayoutTests:
    
    * editing/execCommand/default-paragraph-separator.html: Last line of the test did a manual reset of the value to work around this problem. Removed.
    * editing/execCommand/reset-values-after-navigation-expected.txt: Added.
    * editing/execCommand/reset-values-after-navigation.html: Added.
    * editing/execCommand/resources/reset-default-values-helper-1.html: Added.
    * editing/execCommand/resources/reset-default-values-helper-2.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109653 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.cpp
+++ third_party/WebKit/Source/WebCore/editing/Editor.cpp
@@ -860,6 +860,7 @@ void Editor::clear()
     m_compositionNode = 0;
     m_customCompositionUnderlines.clear();
     m_shouldStyleWithCSS = false;
+    m_defaultParagraphSeparator = EditorParagraphSeparatorIsDiv;
 }
 
 bool Editor::insertText(const String& text, Event* triggeringEvent)
