4f464bebca5364dfccbcc973ee540e741b969a58
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-03 00:42:20 +0000

    Fix crash when closing table example.
    BUG=115845
    TEST=none
    R=ben@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9582025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124792 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/table_example.cc
+++ ui/views/examples/table_example.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 namespace examples {
 
-TableExample::TableExample() : ExampleBase("Table") {
+TableExample::TableExample() : ExampleBase("Table") , table_(NULL) {
 }
 
 TableExample::~TableExample() {
