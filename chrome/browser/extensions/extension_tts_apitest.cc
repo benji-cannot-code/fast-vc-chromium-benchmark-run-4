d3906c4585e00ec40184b9301f085f0e2ae83769
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-03 23:22:41 +0000

    Convert uses of int ms to TimeDelta in chrome/browser/extensions.
    BUG=108171
    
    
    Review URL: http://codereview.chromium.org/9581042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124879 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tts_apitest.cc
+++ chrome/browser/extensions/extension_tts_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -64,7 +64,7 @@ class MockExtensionTtsPlatformImpl : public ExtensionTtsPlatformImpl {
             ptr_factory_.GetWeakPtr(),
             false, utterance_id, TTS_EVENT_END, utterance.size(),
             std::string()),
-        0);
+        base::TimeDelta());
   }
 
   void SendEndEventWhenQueueNotEmpty(
@@ -77,7 +77,7 @@ class MockExtensionTtsPlatformImpl : public ExtensionTtsPlatformImpl {
             &MockExtensionTtsPlatformImpl::SendEvent,
             ptr_factory_.GetWeakPtr(),
             true, utterance_id, TTS_EVENT_END, utterance.size(), std::string()),
-        0);
+        base::TimeDelta());
   }
 
   void SendWordEvents(int utterance_id,
@@ -92,7 +92,7 @@ class MockExtensionTtsPlatformImpl : public ExtensionTtsPlatformImpl {
                 ptr_factory_.GetWeakPtr(),
                 false, utterance_id, TTS_EVENT_WORD, i,
                 std::string()),
-            0);
+            base::TimeDelta());
       }
     }
   }
@@ -109,7 +109,7 @@ class MockExtensionTtsPlatformImpl : public ExtensionTtsPlatformImpl {
               &MockExtensionTtsPlatformImpl::SendEvent,
               ptr_factory_.GetWeakPtr(),
               true, utterance_id, event_type, char_index, message),
-          100);
+          base::TimeDelta::FromMilliseconds(100));
       return;
     }
 
