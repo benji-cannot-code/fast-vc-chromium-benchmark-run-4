0b00521d2d6787e766dc2323ae6328431ad89206
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-03 00:57:10 +0000

    Chrome 18 fails html5test.com XHR Blob response test https://bugs.webkit.org/show_bug.cgi?id=76760
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Most of the code was already there, this just fixes the FIXME
    which was causing this feature not to work.  Chromium already
    had this #ifdef enabled, but other ports (at least Mac) do not.
    
    Test: fast/files/xhr-response-blob.html
    
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::didFinishLoading):
    
    LayoutTests:
    
    * fast/files/script-tests/xhr-response-blob.js: Added.
    (xhr.onreadystatechange):
    * fast/files/xhr-response-blob-expected.txt: Added.
    * fast/files/xhr-response-blob.html: Added.
    * platform/chromium/fast/files/xhr-response-blob-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/files/script-tests/xhr-response-blob.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description("Test that XHR.responseType = 'blob' gives you back a Blob.");
+
+if (window.layoutTestController)
+    layoutTestController.waitUntilDone();
+
+function testBlob(blobURL, blobType, doneFunction) {
+    window.xhr = new XMLHttpRequest();
+    xhr.open("GET", blobURL);
+    xhr.responseType = "blob";
+    shouldBeEqualToString("xhr.responseType", "blob");
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) {
+            shouldBeNull("xhr.response");
+            return;
+        }
+        shouldBeTrue("xhr.response instanceof Blob");
+        shouldBeEqualToString("xhr.response.type", blobType);
+        doneFunction();
+    }
+}
+
+testBlob("script-tests/xhr-response-blob.js", "text/javascript", function() {
+    testBlob("resources/does_not_exist.txt", "", function() {
+        testBlob("resources/empty-file", "", function() {
+            if (window.layoutTestController)
+                layoutTestController.notifyDone();
+        })
+    })
+});
