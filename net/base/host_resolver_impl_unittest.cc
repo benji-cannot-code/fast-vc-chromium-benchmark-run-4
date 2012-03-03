37a97605d0494e9532cdf0a24f7a25efaa1d0f3f
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-03 00:41:57 +0000

    [net] Removes leak from HostResolverImplTest.CanceledRequestsReleaseJobSlots.
    TEST=./net_unittests --gtest_filter=HostResolverImplTest.CanceledRequestsReleaseJobSlots
    
    Review URL: https://chromiumcodereview.appspot.com/9584035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124791 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/host_resolver_impl_unittest.cc
+++ net/base/host_resolver_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/message_loop.h"
+#include "base/stl_util.h"
 #include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/synchronization/condition_variable.h"
@@ -921,6 +922,8 @@ TEST_F(HostResolverImplTest, CanceledRequestsReleaseJobSlots) {
     MessageLoop::current()->Run();
 
   MessageLoop::current()->AssertIdle();
+
+  STLDeleteElements(&requests);
 }
 
 // Helper class used by HostResolverImplTest.CancelWithinCallback.
