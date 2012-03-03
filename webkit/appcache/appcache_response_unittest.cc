27038093c93b12aa83f498e18bd99c9c59446e51
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-03 21:02:28 +0000

    Convert uses of int ms to TimeDelta in webkit/appcache and webkit/dom_storage.
    R=michaeln@chromium.org
    BUG=108171
    
    
    Review URL: http://codereview.chromium.org/9572037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124877 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache_response_unittest.cc
+++ webkit/appcache/appcache_response_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -645,7 +645,7 @@ class AppCacheResponseTest : public testing::Test {
     MessageLoop::current()->PostDelayedTask(
         FROM_HERE, base::Bind(&AppCacheResponseTest::VerifyNoCallbacks,
                               base::Unretained(this)),
-        10);
+        base::TimeDelta::FromMilliseconds(10));
   }
 
   void VerifyNoCallbacks() {
