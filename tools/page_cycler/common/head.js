5480b7b3a438f025275d9680d1ba994dc2267d01
jnd@chromium.org <jnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 23:59:23 +0000

    Log pages information to console.
    It allows us to get pages information by monitoring the console outpt instead of printing by UI automation.
    
    BUG=None
    TEST=page cycler should still pass. 
    Review URL: https://chromiumcodereview.appspot.com/9586006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125034 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/page_cycler/common/head.js
+++ tools/page_cycler/common/head.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -65,7 +65,10 @@ function __ontimeout() {
   if (__cycle == (__pages().length * __iterations)) {
     document.cookie = "__pc_done=1; path=/";
     doc = "../../common/report.html";
-    if (window.console) console.log("times: [" + __get_timings() + "]");
+    if (window.console) {
+      console.log("Pages: [" + __get_cookie('__pc_pages') + "]");
+      console.log("times: [" + __get_timings() + "]");
+    }
   } else {
     doc = "../" + __pages()[__page] + "/index.html";
   }
