37a4f84e857ac6514d7f8378fe6454f65f185f4a
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 15:23:59 +0000

    Defer shutting down Chrome after the CF automation connection goes away.
    BUG=98506
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9570017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/chrome_frame_automation_provider.h
+++ chrome/browser/automation/chrome_frame_automation_provider.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,6 +34,10 @@ class ChromeFrameAutomationProvider : public AutomationProvider {
 
   // Returns true if the message received is a valid chrome frame message.
   bool IsValidMessage(uint32 type);
+
+  // Called to release an instance's ref count on the global BrowserProcess
+  // instance.
+  static void ReleaseBrowserProcess();
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeFrameAutomationProvider);
--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -52,6 +52,7 @@ extern const char kAutomationClientChannelID[];
 extern const char kAutomationReinitializeOnChannelError[];
 extern const char kCheckForUpdateIntervalSec[];
 extern const char kCheckCloudPrintConnectorPolicy[];
+extern const char kChromeFrameShutdownDelay[];
 extern const char kChromeVersion[];
 extern const char kCipherSuiteBlacklist[];
 extern const char kClearTokenService[];
