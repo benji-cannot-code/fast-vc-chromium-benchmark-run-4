159856def263193e091ad13044c48bcbff7e634d
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 21:58:27 +0000

    Remove implicit dependency from 'skia' on 'ui'.
    This is needed for the components build. Just adding an explicit dependency from skia to ui doesn't work because ui already depends on skia.
    
    BUG=90078
    TEST=No linker errors when linking libskia.dylib
    
    Review URL: http://codereview.chromium.org/9578010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125009 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/skia_utils_mac.mm
+++ skia/ext/skia_utils_mac.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/bitmap_platform_device_mac.h"
 #include "third_party/skia/include/core/SkRegion.h"
 #include "third_party/skia/include/utils/mac/SkCGUtils.h"
-#include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
 // 10.6 API that we use if available.
 #if !defined(MAC_OS_X_VERSION_10_6) || \
@@ -77,7 +76,8 @@ SkBitmap NSImageOrNSImageRepToSkBitmap(
   DCHECK(context);
 
   // Save the current graphics context so that we can restore it later.
-  gfx::ScopedNSGraphicsContextSaveGState scoped_g_state;
+  NSGraphicsContext* old_context = [NSGraphicsContext currentContext];
+  [NSGraphicsContext saveGraphicsState];
 
   // Dummy context that we will draw into.
   NSGraphicsContext* context_cocoa =
@@ -110,6 +110,10 @@ SkBitmap NSImageOrNSImageRepToSkBitmap(
       [image_rep drawInRect:drawRect];
     }
   }
+
+  [NSGraphicsContext restoreGraphicsState];
+  if (!old_context && base::mac::IsOSLeopardOrEarlier())
+    [NSGraphicsContext setCurrentContext:nil];
 
   return bitmap;
 }
