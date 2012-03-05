45048f95604086acc4ba409645e0d34007c409e6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-05 16:54:58 +0000

    [GTK] plugins/netscape-plugin-page-cache-works.html fails https://bugs.webkit.org/show_bug.cgi?id=74409
    Patch by Zan Dobersek <zandobersek@gmail.com> on 2012-03-05
    Reviewed by Martin Robinson.
    
    Source/WebKit/gtk:
    
    Add a method to override page-cache-for-plugins preference.
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::setPageCacheSupportsPlugins):
    * WebCoreSupport/DumpRenderTreeSupportGtk.h:
    (DumpRenderTreeSupportGtk):
    
    Tools:
    
    Add support for overriding the page-cache-for-plugins preference
    through DumpRenderTreeSupport.
    
    * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    (booleanFromValue): A helper function to convert a string to boolean.
    (LayoutTestController::overridePreference):
    
    LayoutTests:
    
    Unskip one new passing test and reclassify one failure.
    
    * platform/gtk/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.h
@@ -129,6 +129,7 @@ public:
     static int numberOfPendingGeolocationPermissionRequests(WebKitWebView*);
 
     static void setHixie76WebSocketProtocolEnabled(WebKitWebView*, bool enabled);
+    static void setPageCacheSupportsPlugins(WebKitWebView*, bool enabled);
 
     static void deliverAllMutationsIfNecessary();
 
