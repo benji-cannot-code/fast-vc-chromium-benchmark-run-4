7ad669c11dd2686cfc5de03484dc12b375f78827
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-05 08:44:56 +0000

    Unreviewed build fix for !ENABLE(SHADOW_DOM).
    * html/shadow/HTMLContentElement.cpp:
    (WebCore::contentTagName):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/HTMLContentElement.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/HTMLContentElement.cpp
@@ -45,7 +45,7 @@ static const QualifiedName& contentTagName()
 #if ENABLE(SHADOW_DOM)
     return HTMLNames::contentTag;
 #else
-    return HTMLNames::webkitShadowContentTag
+    return HTMLNames::webkitShadowContentTag;
 #endif
 }
 
