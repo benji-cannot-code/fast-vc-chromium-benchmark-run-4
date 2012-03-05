fc795a91c979f5ddc613bb08744b7fe4a955b4f8
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-05 01:34:22 +0000

    2012-03-04 Antonio Gomes <agomes@rim.com>
            Fixed positioned element not correctly positioned after orientation change,
            when viewport metatag is used with "initial-scale=1" MKS_2992596
            PR #138865
    
            Reviewed by George Staikos.
    
            Viewport size changes in two main scenarios:
            - orientation changes;
            - virtual keyboard pop up/hide.
    
            When we are setting a new viewport size, we need to
            update the FixedReportedSize of the mainframe. This size
            is used to return innerWidth|Heigth as well as to calculate
            the fixed position elements height relative to the RenderView [1].
    
            [1] RenderBox::containingBlockLogicalHeightForPositioned.
    
            When we are rotating, we need to delay setting the new FixedReportedSize
            till we layout and are able to figure out the new contents size. At this
            time we call setFixedReportedSize, but we do not reposition the fixed elemts
            afterwards.
    
            Patch adds a call to FrameView::repaintFixedElementsAfterScrolling, after
            setting the new size.
    
            Source/WebKit/blackberry:
            * Api/WebPage.cpp:
            (BlackBerry::WebKit::WebPagePrivate::setViewportSize):
    
            Source/WebCore:
            * manual-tests/blackberry/meta-viewport-fixed-position-preserved-on-rotation.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -3195,7 +3195,7 @@ void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize
     bool newVisibleRectContainsOldVisibleRect = (m_actualVisibleHeight <= transformedActualVisibleSize.height())
                                           && (m_actualVisibleWidth <= transformedActualVisibleSize.width());
 
-    bool atInitialScale = currentScale() == initialScale();
+    bool atInitialScale = m_webPage->isAtInitialZoom();
     bool atTop = !scrollPosition().y();
     bool atLeft = !scrollPosition().x();
 
@@ -3265,8 +3265,10 @@ void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize
     if (setViewMode(viewMode()))
         needsLayout = true;
 
+    bool needsLayoutToFindContentSize = hasPendingOrientation;
+
     // We need to update the viewport size of the WebCore::ScrollView...
-    updateViewportSize(!hasPendingOrientation /* setFixedReportedSize */, false /* sendResizeEvent */);
+    updateViewportSize(!needsLayoutToFindContentSize /* setFixedReportedSize */, false /* sendResizeEvent */);
     notifyTransformedContentsSizeChanged();
 
     // If automatic zooming is disabled, prevent zooming below.
@@ -3303,7 +3305,7 @@ void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize
     // Do our own clamping.
     scale = clampedScale(scale);
 
-    if (hasPendingOrientation) {
+    if (needsLayoutToFindContentSize) {
         // Set the fixed reported size here so that innerWidth|innerHeight works
         // with this new scale.
         TransformationMatrix rotationMatrix;
@@ -3311,6 +3313,7 @@ void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize
         IntRect viewportRect = IntRect(IntPoint::zero(), transformedActualVisibleSize);
         IntRect actualVisibleRect = enclosingIntRect(rotationMatrix.inverse().mapRect(FloatRect(viewportRect)));
         m_mainFrame->view()->setFixedReportedSize(actualVisibleRect.size());
+        m_mainFrame->view()->repaintFixedElementsAfterScrolling();
     }
 
     // We're going to need to send a resize event to JavaScript because
