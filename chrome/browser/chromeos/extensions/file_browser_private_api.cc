7f51bdcf3627e60767575896acb71a046b3cfff4
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 21:45:42 +0000

    Revert 124975 - [filebrowser] Introduce "Open" action for supported file types (e.g. pdf).
    Was breaking ChromeOS, failing tests were:
    - FileSystemExtensionApiTest.FileBrowserTest
    - FileSystemExtensionApiTest.FileBrowserTestWrite
    - FileSystemExtensionApiTest.FileBrowserTestWriteComponent
    
    BUG=chromium-os:26538,chromium-os:26539
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/9395077
    
    TBR=dgozman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9594037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_private_api.cc
+++ chrome/browser/chromeos/extensions/file_browser_private_api.cc
@@ -1782,9 +1782,6 @@ bool FileDialogStringsFunction::RunImpl() {
   SET_STRING(IDS_FILE_BROWSER, MOUNT_ARCHIVE);
   SET_STRING(IDS_FILE_BROWSER, FORMAT_DEVICE);
 
-  SET_STRING(IDS_FILE_BROWSER, OPEN_ACTION);
-  SET_STRING(IDS_FILE_BROWSER, INSTALL_CRX);
-
   SET_STRING(IDS_FILE_BROWSER, GALLERY);
   SET_STRING(IDS_FILE_BROWSER, GALLERY_EDIT);
   SET_STRING(IDS_FILE_BROWSER, GALLERY_SHARE);
@@ -1915,10 +1912,6 @@ bool FileDialogStringsFunction::RunImpl() {
 
   SET_STRING(IDS_FILE_BROWSER, ENQUEUE);
 #undef SET_STRING
-
-  dict->SetString("PDF_VIEW_ENABLED",
-      file_manager_util::ShouldBeOpenedWithPdfPlugin(".pdf") ?
-          "true" : "false");
 
   ChromeURLDataManager::DataSource::SetFontAndTextDirection(dict);
 
