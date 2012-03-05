2c558282c30202643d2967b04e71bf1815c4df10
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 11:00:08 +0000

    [cros] Resolve online login attempt on UI thread.
    BUG=chromium-os:26559
    TEST=No DCHECK during login.
    
    Review URL: http://codereview.chromium.org/9566006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/online_attempt_host.cc
+++ chrome/browser/chromeos/login/online_attempt_host.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/online_attempt_host.h"
 
+#include "base/bind.h"
 #include "base/sha1.h"
 #include "chrome/browser/chromeos/login/auth_attempt_state.h"
 #include "chrome/browser/chromeos/login/authenticator.h"
 #include "chrome/browser/chromeos/login/online_attempt.h"
 #include "chrome/browser/profiles/profile.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace chromeos {
 
@@ -24,6 +26,7 @@ OnlineAttemptHost::~OnlineAttemptHost() {
 void OnlineAttemptHost::Check(Profile* profile,
                               const std::string& username,
                               const std::string& password) {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   std::string attempt_hash = base::SHA1HashString(username + "\n" + password);
   if (attempt_hash != current_attempt_hash_) {
     Reset();
@@ -46,17 +49,27 @@ void OnlineAttemptHost::Check(Profile* profile,
 }
 
 void OnlineAttemptHost::Reset() {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   online_attempt_ = NULL;
   current_attempt_hash_.clear();
   current_username_.clear();
 }
 
 void OnlineAttemptHost::Resolve() {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
   if (state_->online_complete()) {
     bool success = state_->online_outcome().reason() == LoginFailure::NONE;
-    delegate_->OnChecked(current_username_, success);
-    Reset();
+    content::BrowserThread::PostTask(
+        content::BrowserThread::UI, FROM_HERE,
+        base::Bind(&OnlineAttemptHost::ResolveOnUIThread,
+                   base::Unretained(this), success));
   }
+}
+
+void OnlineAttemptHost::ResolveOnUIThread(bool success) {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  delegate_->OnChecked(current_username_, success);
+  Reset();
 }
 
 }  // chromeos
--- chrome/browser/chromeos/login/online_attempt_host.h
+++ chrome/browser/chromeos/login/online_attempt_host.h
@@ -44,7 +44,11 @@ class OnlineAttemptHost : public AuthAttemptStateResolver {
   void Reset();
 
   // AuthAttemptStateResolver overrides.
+  // Executed on IO thread.
   virtual void Resolve() OVERRIDE;
+
+  // Does an actual resolve on UI thread.
+  void ResolveOnUIThread(bool success);
 
  private:
   Delegate* delegate_;
