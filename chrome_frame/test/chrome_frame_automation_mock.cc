debb7dc0bac03252c2aca901050a8d47e26b30c5
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-05 18:05:36 +0000

    Disabling ChromeFrame.Launch
    TBR=grt
    
    BUG=81479
    TEST=chrome_frame_tests.exe
    
    Review URL: https://chromiumcodereview.appspot.com/9599017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/chrome_frame_automation_mock.cc
+++ chrome_frame/test/chrome_frame_automation_mock.cc
@@ -9,7 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const int kLongWaitTimeout = 25 * 1000;
 
-TEST(ChromeFrame, Launch) {
+// Note that this test fails occasionally. Disabling it altogether (rather than
+// marking as flaky to see if the failure cascades down to the next test.
+// http://crbug.com/81479
+TEST(ChromeFrame, DISABLED_Launch) {
   MessageLoopForUI loop;
   AutomationMockLaunch mock_launch(&loop, kLongWaitTimeout);
 
