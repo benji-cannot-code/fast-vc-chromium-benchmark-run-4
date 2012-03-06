93f072b1f1e304cad7bb0c3eb8bbd0cacfacced8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 07:31:45 +0000

    Element not fully repainted after application and removal of transform https://bugs.webkit.org/show_bug.cgi?id=61338
    Source/WebCore:
    
    Patch by Kishore Bolisetty <kbolisetty@innominds.com> on 2012-03-05
    Reviewed by Simon Fraser.
    RenderLayer is created and removed when a transform is applied and removed to element.
    To render the element properly after removal of transform on it, preferred widths and dimensions of
    the element and its containing block needs to be recalculated.
    
    Test: fast/repaint/transform-rotate-and-remove.html
    
    * rendering/RenderBoxModelObject.cpp:
    (WebCore):
    (WebCore::RenderBoxModelObject::styleWillChange):
    (WebCore::RenderBoxModelObject::styleDidChange):
    * rendering/RenderBoxModelObject.h:
    (RenderBoxModelObject):
    
    LayoutTests:
    
    Patch by Kishore Bolisetty <kbolisetty@innominds.com> on 2012-03-05
    Reviewed by Simon Fraser.
    Test to remove the transform property of an element upon onload event.
    
    * fast/repaint/transform-rotate-and-remove.html: Added.
    * platform/chromium-linux/fast/repaint/transform-rotate-and-remove-expected.png: Added.
    * platform/chromium-linux/fast/repaint/transform-rotate-and-remove-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109867 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.h
@@ -241,6 +241,7 @@ private:
     // Used to store state between styleWillChange and styleDidChange
     static bool s_wasFloating;
     static bool s_hadLayer;
+    static bool s_hadTransform;
     static bool s_layerWasSelfPainting;
 };
 
