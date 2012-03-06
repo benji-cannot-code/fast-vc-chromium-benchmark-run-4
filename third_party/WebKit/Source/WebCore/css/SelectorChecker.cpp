918c23609a9b03570b46ebf132f8469aea8bbdb7
rolandsteiner@chromium.org <rolandsteiner@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 09:43:27 +0000

    :scope should behave as :root outside scoped style sheet https://bugs.webkit.org/show_bug.cgi?id=80389
    Source/WebCore:
    
    Fall through to :root implementation if no scope is used.
    
    Reviewed by Antti Koivisto.
    
    Test: fast/css/style-scoped/scope-pseudo.html
    
    * css/SelectorChecker.cpp:
    (WebCore::SelectorChecker::checkOneSelector):
    
    LayoutTests:
    
    Update test with :scope in global style sheet
    
    Reviewed by Antti Koivisto.
    
    * fast/css/style-scoped/scope-pseudo-expected.txt:
    * fast/css/style-scoped/scope-pseudo.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109884 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/SelectorChecker.cpp
+++ third_party/WebKit/Source/WebCore/css/SelectorChecker.cpp
@@ -982,8 +982,6 @@ bool SelectorChecker::checkOneSelector(const SelectorCheckingContext& context, P
                     return true;
             }
             break;
-        case CSSSelector::PseudoScope:
-            return element == context.scope;
         case CSSSelector::PseudoTarget:
             if (element == element->document()->cssTarget())
                 return true;
@@ -1111,6 +1109,10 @@ bool SelectorChecker::checkOneSelector(const SelectorCheckingContext& context, P
                     return true;
                 break;
             }
+        case CSSSelector::PseudoScope:
+            if (context.scope)
+                return element == context.scope;
+            // If there is no scope, :scope should behave as :root -> fall through
         case CSSSelector::PseudoRoot:
             if (element == element->document()->documentElement())
                 return true;
