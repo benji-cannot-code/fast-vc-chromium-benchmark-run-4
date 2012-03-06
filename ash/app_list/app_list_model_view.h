59733463ddcfc53faea4d3b759cbbf044fd7f736
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 06:15:33 +0000

    ash: Cap app list tile width to the max tile width.
    - Add a set_icon_size method to AppListItemView;
    - Fix AppListItemView's GetPreferredSize to return correct size;
    - Update AppListModelView::Layout to cap tile width to max tile width;
    - Also fix a potential divide-by-zero error in CalculateTileSize;
    
    BUG=98308
    TEST=Verify that tile does not occupy the whole screen width when there is only one column.
    
    
    Review URL: http://codereview.chromium.org/9597036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125124 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/app_list/app_list_model_view.h
+++ ash/app_list/app_list_model_view.h
@@ -32,6 +32,8 @@ class AppListModelView : public views::View,
   // Updates from model.
   void Update();
 
+  int SetTileIconSizeAndGetMaxWidth(int icon_dimension);
+
   // Overridden from views::View:
   virtual void Layout() OVERRIDE;
 
