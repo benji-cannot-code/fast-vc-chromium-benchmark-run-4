504326979a2bcb76d5610278e24c24fe5d394361
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 02:17:57 +0000

    Coverity: Fix a pass-by-value.
    CID_COUNT=1
    CID=103428
    BUG=none
    TEST=none
    R=groby
    
    Review URL: https://chromiumcodereview.appspot.com/9599005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125072 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/win/events_win.cc
+++ ui/base/win/events_win.cc
@@ -267,7 +267,7 @@ void UpdateDeviceList() {
   NOTIMPLEMENTED();
 }
 
-bool IsNoopEvent(base::NativeEvent event) {
+bool IsNoopEvent(const base::NativeEvent& event) {
   return event.message == WM_USER + 310;
 }
 
