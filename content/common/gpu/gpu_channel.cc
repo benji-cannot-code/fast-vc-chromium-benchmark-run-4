c129145b7de5980d484b31fccb28cf8808864aa9
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 00:05:38 +0000

    Make unscheduling per-command-buffer
    Unscheduling currently only happens due to swapbuffers, so it makes sense for it to unschedule just the command-buffer, not the channel, letting other command buffers continue.
    
    BUG=112349
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9584019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125039 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.cc
+++ content/common/gpu/gpu_channel.cc
@@ -103,8 +103,7 @@ bool GpuChannel::OnMessageReceived(const IPC::Message& message) {
     deferred_messages_.push_back(new IPC::Message(message));
   }
 
-  if (IsScheduled())
-    OnScheduled();
+  OnScheduled();
 
   return true;
 }
@@ -140,18 +139,6 @@ void GpuChannel::AppendAllCommandBufferStubs(
       !it.IsAtEnd(); it.Advance()) {
     stubs.push_back(it.GetCurrentValue());
   }
-}
-
-bool GpuChannel::IsScheduled() {
-  for (StubMap::Iterator<GpuCommandBufferStub> it(&stubs_);
-       !it.IsAtEnd();
-       it.Advance()) {
-    GpuCommandBufferStub* stub = it.GetCurrentValue();
-    if (!stub->IsScheduled())
-      return false;
-  }
-
-  return true;
 }
 
 void GpuChannel::OnScheduled() {
@@ -239,11 +226,15 @@ bool GpuChannel::OnControlMessageReceived(const IPC::Message& msg) {
 
 void GpuChannel::HandleMessage() {
   handle_messages_scheduled_ = false;
-  if (!IsScheduled())
-      return;
 
   if (!deferred_messages_.empty()) {
-    scoped_ptr<IPC::Message> message(deferred_messages_.front());
+    IPC::Message* m = deferred_messages_.front();
+    GpuCommandBufferStub* stub = stubs_.Lookup(m->routing_id());
+
+    if (stub && !stub->IsScheduled())
+      return;
+
+    scoped_ptr<IPC::Message> message(m);
     deferred_messages_.pop_front();
     processed_get_state_fast_ =
         (message->type() == GpuCommandBufferMsg_GetStateFast::ID);
@@ -259,7 +250,6 @@ void GpuChannel::HandleMessage() {
       // If the channel becomes unscheduled as a result of handling the message
       // or has more work to do, synthesize an IPC message to flush the command
       // buffer that became unscheduled.
-      GpuCommandBufferStub* stub = stubs_.Lookup(message->routing_id());
       if (stub) {
         if (stub->HasUnprocessedCommands() || stub->HasMoreWork()) {
           deferred_messages_.push_front(new GpuCommandBufferMsg_Rescheduled(
@@ -277,7 +267,7 @@ void GpuChannel::HandleMessage() {
     }
   }
 
-  if (IsScheduled() && !deferred_messages_.empty()) {
+  if (!deferred_messages_.empty()) {
     OnScheduled();
   }
 }
