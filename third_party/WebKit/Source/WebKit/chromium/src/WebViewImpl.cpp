1be8ea549c9f9608409ad547575b7b473421b40e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 01:18:12 +0000

    Enable context menu on android
    For android, long press is converted into a right mouse down event
    So we will use that to invoke the context menu
    https://bugs.webkit.org/show_bug.cgi?id=80332
    
    Patch by Min Qin <qinmin@google.com> on 2012-03-05
    Reviewed by Adam Barth.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::mouseDown):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -529,7 +529,7 @@ void WebViewImpl::mouseDown(const WebMouseEvent& event)
         || (event.button == WebMouseEvent::ButtonLeft
             && event.modifiers & WebMouseEvent::ControlKey))
         mouseContextMenu(event);
-#elif OS(UNIX)
+#elif OS(UNIX) || OS(ANDROID)
     if (event.button == WebMouseEvent::ButtonRight)
         mouseContextMenu(event);
 #endif
