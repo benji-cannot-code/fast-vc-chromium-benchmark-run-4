8072e64a49a765bb320c85e78b9f8bf9793057e7
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 07:59:24 +0000

    WebAudio JSC-related fixes https://bugs.webkit.org/show_bug.cgi?id=80299
    Reviewed by Adam Barth.
    
    * bindings/js/JSAudioContextCustom.cpp:
    (WebCore::JSAudioContextConstructor::constructJSAudioContext):
    Wrap the audio context in a DOM wrapper.
    * webaudio/AudioContext.idl: No need to protect EventTarget by V8
    guards, I think.
    * webaudio/JavaScriptAudioNode.idl: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSAudioContextCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSAudioContextCustom.cpp
@@ -102,7 +102,7 @@ EncodedJSValue JSC_HOST_CALL JSAudioContextConstructor::constructJSAudioContext(
     if (!audioContext.get())
         return throwVMError(exec, createReferenceError(exec, "Error creating AudioContext"));
 
-    return JSValue::encode(asObject(toJS(exec, jsConstructor->globalObject(), audioContext.get())));
+    return JSValue::encode(CREATE_DOM_WRAPPER(exec, jsConstructor->globalObject(), AudioContext, audioContext.get()));
 }
 
 } // namespace WebCore
