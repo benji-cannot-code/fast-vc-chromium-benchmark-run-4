24ac7760a82390a1385bd1a916e5047461ce97ef
tpayne@chromium.org <tpayne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 00:02:45 +0000

    Added win media notifier
    BUG=110400
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/9348100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125036 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/system_message_window_win.h
+++ content/browser/system_message_window_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,21 +7,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_SYSTEM_MESSAGE_WINDOW_WIN_H_
 #pragma once
 
-#include "build/build_config.h"
-
 #include <windows.h>
 
 #include "base/basictypes.h"
 #include "content/common/content_export.h"
 
+typedef LRESULT (*VolumeNameFunc)(LPCWSTR drive,
+                                  LPWSTR volume_name,
+                                  unsigned int volume_name_len);
+
 class CONTENT_EXPORT SystemMessageWindowWin {
  public:
   SystemMessageWindowWin();
+  // Only for use in unit tests.
+  explicit SystemMessageWindowWin::SystemMessageWindowWin(
+      VolumeNameFunc volumeNameFunc);
+
   virtual ~SystemMessageWindowWin();
 
   virtual LRESULT OnDeviceChange(UINT event_type, DWORD data);
 
  private:
+  void Init();
+
   LRESULT CALLBACK WndProc(HWND hwnd, UINT message,
                            WPARAM wparam, LPARAM lparam);
 
@@ -37,6 +45,7 @@ class CONTENT_EXPORT SystemMessageWindowWin {
   }
 
   HWND window_;
+  VolumeNameFunc volume_name_func_;
 
   DISALLOW_COPY_AND_ASSIGN(SystemMessageWindowWin);
 };
--- content/content_tests.gypi
+++ content/content_tests.gypi
@@ -247,6 +247,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/speech/speech_recognition_request_unittest.cc',
         'browser/speech/speech_recognizer_impl_unittest.cc',
         'browser/ssl/ssl_host_state_unittest.cc',
+        'browser/system_message_window_win_unittest.cc',
         'browser/tab_contents/navigation_controller_impl_unittest.cc',
         'browser/tab_contents/navigation_entry_impl_unittest.cc',
         'browser/tab_contents/render_view_host_manager_unittest.cc',
