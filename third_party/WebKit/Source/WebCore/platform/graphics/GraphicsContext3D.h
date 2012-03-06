a3862fd71300d4af56e41c13d26a331fe22b3d6b
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 04:24:41 +0000

    GraphicsContext3D.h should include RefCounted.h explicitly https://bugs.webkit.org/show_bug.cgi?id=80251
    Reviewed by Rob Buis.
    
    GraphicsContext3D.h was using RefCounted but including RefCounted.h indirectly
    through GraphicsLayer.h through Animation.h. However Animation.h is included
    by GraphicsLayer.h only when ACCELERATED_COMPOSITING is on. For some configurations
    that don't use ACCELERATED_COMPOSITING but use GraphicsContext3D, like the
    BlackBerry x86 configuration, the indirect inclusion will fail.
    
    This patch is adding explicit inclusion of RefCounted.h to avoid that kind of
    problem.
    
    * platform/graphics/GraphicsContext3D.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109841 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GraphicsContext3D.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GraphicsContext3D.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/HashMap.h>
 #include <wtf/ListHashSet.h>
 #include <wtf/Noncopyable.h>
+#include <wtf/RefCounted.h>
 
 // FIXME: Find a better way to avoid the name confliction for NO_ERROR.
 #if ((PLATFORM(CHROMIUM) && OS(WINDOWS)) || PLATFORM(WIN) || (PLATFORM(QT) && OS(WINDOWS)))
