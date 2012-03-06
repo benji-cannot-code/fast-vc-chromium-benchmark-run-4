683fbf6389719f41ca74320ff233868be67c253c
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 21:01:05 +0000

    Now that uploading to breakpad works on all threads add it to all places that we could return an unrecoverable error. Those places include: 1. Failed model association resulting in unrecoverable error being called on DTC. 2. When disabling a single type due to a perdatatype error. 3. Failing to download requested types resulting in unrecoverable error.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9428054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125220 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/non_frontend_data_type_controller.cc
+++ chrome/browser/sync/glue/non_frontend_data_type_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/api/sync_error.h"
 #include "chrome/browser/sync/glue/change_processor.h"
+#include "chrome/browser/sync/glue/chrome_report_unrecoverable_error.h"
 #include "chrome/browser/sync/glue/model_associator.h"
 #include "chrome/browser/sync/profile_sync_components_factory.h"
 #include "chrome/browser/sync/profile_sync_service.h"
@@ -129,6 +130,9 @@ void NonFrontendDataTypeController::StartAssociation() {
 void NonFrontendDataTypeController::StartFailed(StartResult result,
                                                 const SyncError& error) {
   DCHECK(!BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  if (IsUnrecoverableResult(result))
+    RecordUnrecoverableError(FROM_HERE, "StartFailed");
   StopAssociation();
   StartDone(result,
             result == ASSOCIATION_FAILED ? DISABLED : NOT_RUNNING,
@@ -315,14 +319,6 @@ void NonFrontendDataTypeController::DisableImpl(
     const std::string& message) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   profile_sync_service_->OnDisableDatatype(type(), from_here, message);
-}
-
-void NonFrontendDataTypeController::RecordUnrecoverableError(
-    const tracked_objects::Location& from_here,
-    const std::string& message) {
-  DCHECK(!BrowserThread::CurrentlyOn(BrowserThread::UI));
-  UMA_HISTOGRAM_ENUMERATION("Sync.DataTypeRunFailures", type(),
-                            syncable::MODEL_TYPE_COUNT);
 }
 
 void NonFrontendDataTypeController::RecordAssociationTime(
