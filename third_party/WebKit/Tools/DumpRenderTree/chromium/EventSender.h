ed09a71ba3684fa2a5d75c630ab8634cd1ab45d5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 00:13:51 +0000

    Tools: Correctly compute the positions of GestureScroll* events on Chromium. https://bugs.webkit.org/show_bug.cgi?id=80075
    Patch by Robert Kroeger <rjkroege@chromium.org> on 2012-03-05
    Reviewed by Adam Barth.
    
    * DumpRenderTree/chromium/EventSender.cpp:
    (EventSender::reset):
    (EventSender::gestureEvent):
    * DumpRenderTree/chromium/EventSender.h:
    (EventSender):
    
    LayoutTests: Correct a test to work the same on Gtk and non-Gtk chromium in
    the presence of correctly synthesized GestureScroll* events.
    https://bugs.webkit.org/show_bug.cgi?id=80075
    
    Patch by Robert Kroeger <rjkroege@chromium.org> on 2012-03-05
    Reviewed by Adam Barth.
    
    * fast/events/touch/gesture/gesture-scroll.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/EventSender.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/EventSender.h
@@ -163,7 +163,7 @@ private:
     TestShell* m_shell;
 
     // Location of the touch point that initiated a gesture.
-    WebKit::WebPoint m_gestureStartLocation;
+    WebKit::WebPoint m_currentGestureLocation;
 
     // Location of last mouseMoveTo event.
     static WebKit::WebPoint lastMousePos;
