b4dd66e7668fdce56fb5bba24f1bad30af4f0116
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 02:49:12 +0000

    Refactor dangerous download warning text generation into DownloadItemModel.
    Previously, we had multiple copies of the logic for generating warning text for a dangerous or malicious download. Move all of these into DownloadModel. As an additional benefit, we no longer require UI code to interpret DownloadItem::GetDangerType().
    
    BUG=116551
    TEST=Start a dangerous, malicious, and extension download. Check whether the warning text is correct, and any embedded filename is elided if necessary.
    
    
    Review URL: http://codereview.chromium.org/9569011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/download/download_item_controller.h
+++ chrome/browser/ui/cocoa/download/download_item_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,6 +19,10 @@ class DownloadShelfContextMenuMac;
 
 namespace content {
 class DownloadItem;
+}
+
+namespace gfx {
+class Font;
 }
 
 // A controller class that manages one download item.
@@ -56,6 +60,9 @@ class DownloadItem;
 
   // The time at which this view was created.
   base::Time creationTime_;
+
+  // Default font to use for text metrics.
+  scoped_ptr<gfx::Font> font_;
 
   // The state of this item.
   enum DownoadItemState {
--- chrome/browser/ui/cocoa/download/download_item_mac.mm
+++ chrome/browser/ui/cocoa/download/download_item_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,8 +31,7 @@ DownloadItemMac::~DownloadItemMac() {
 void DownloadItemMac::OnDownloadUpdated(content::DownloadItem* download) {
   DCHECK_EQ(download, download_model_->download());
 
-  if ([item_controller_ isDangerousMode] &&
-      download->GetSafetyState() == DownloadItem::DANGEROUS_BUT_VALIDATED) {
+  if ([item_controller_ isDangerousMode] && !download_model_->IsDangerous()) {
     // We have been approved.
     [item_controller_ clearDangerousMode];
   }
