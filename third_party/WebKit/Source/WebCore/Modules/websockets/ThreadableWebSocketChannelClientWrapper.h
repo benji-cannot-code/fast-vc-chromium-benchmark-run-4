7c78c3c0b955ffa9a8723dffce0eda86fdc03112
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 02:11:17 +0000

    [WebSocket] Introduce ThreadableWebSocketChannel::SendResult https://bugs.webkit.org/show_bug.cgi?id=80356
    Reviewed by Kent Tamura.
    
    Introduced ThreadableWebSocketChannel::SendResult type so that
    WebSocketChannel can pass the validation result.
    
    No new test. No changes in behavior.
    
    * Modules/websockets/ThreadableWebSocketChannel.h: Added SendResult.
    * Modules/websockets/ThreadableWebSocketChannelClientWrapper.cpp:
    (WebCore::ThreadableWebSocketChannelClientWrapper::ThreadableWebSocketChannelClientWrapper):
    (WebCore::ThreadableWebSocketChannelClientWrapper::sendRequestResult): Use ThreadableWebSocketChannel::SendResult instead of bool.
    (WebCore::ThreadableWebSocketChannelClientWrapper::setSendRequestResult): Ditto.
    * Modules/websockets/ThreadableWebSocketChannelClientWrapper.h:
    (ThreadableWebSocketChannelClientWrapper):
    * Modules/websockets/WebSocketChannel.cpp:
    (WebCore::WebSocketChannel::send): Use ThreadableWebSocketChannel::SendResult instead of bool. Pass Cstring to enqueTextFrame instead of String.
    (WebCore::WebSocketChannel::enqueueTextFrame): Ditto.
    (WebCore::WebSocketChannel::processOutgoingFrameQueue): Ditto.
    * Modules/websockets/WebSocketChannel.h:
    (WebSocketChannel):
    (QueuedFrame): Changed the type of stringData from String to CString.
    * Modules/websockets/WorkerThreadableWebSocketChannel.cpp:
    (WebCore::WorkerThreadableWebSocketChannel::send): Use ThreadableWebSocketChannel::SendResult instead of bool.
    (WebCore::workerContextDidSend): Ditto.
    (WebCore::WorkerThreadableWebSocketChannel::Peer::send): Ditto.
    (WebCore::WorkerThreadableWebSocketChannel::Bridge::send): Ditto.
    * Modules/websockets/WorkerThreadableWebSocketChannel.h:
    (WorkerThreadableWebSocketChannel): ditto.
    (Bridge): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/websockets/ThreadableWebSocketChannelClientWrapper.h
+++ third_party/WebKit/Source/WebCore/Modules/websockets/ThreadableWebSocketChannelClientWrapper.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PlatformString.h"
 #include "ScriptExecutionContext.h"
+#include "ThreadableWebSocketChannel.h"
 #include "WebSocketChannelClient.h"
 #include <wtf/Forward.h>
 #include <wtf/OwnPtr.h>
@@ -67,8 +68,8 @@ public:
     String extensions() const;
     void setExtensions(const String&);
 
-    bool sendRequestResult() const;
-    void setSendRequestResult(bool);
+    ThreadableWebSocketChannel::SendResult sendRequestResult() const;
+    void setSendRequestResult(ThreadableWebSocketChannel::SendResult);
 
     unsigned long bufferedAmount() const;
     void setBufferedAmount(unsigned long);
@@ -105,7 +106,7 @@ private:
     // ThreadSafeRefCounted must not have String member variables.
     Vector<UChar> m_subprotocol;
     Vector<UChar> m_extensions;
-    bool m_sendRequestResult;
+    ThreadableWebSocketChannel::SendResult m_sendRequestResult;
     unsigned long m_bufferedAmount;
     bool m_suspended;
     Vector<OwnPtr<ScriptExecutionContext::Task> > m_pendingTasks;
--- third_party/WebKit/Source/WebCore/Modules/websockets/WebSocket.cpp
+++ third_party/WebKit/Source/WebCore/Modules/websockets/WebSocket.cpp
@@ -291,7 +291,7 @@ bool WebSocket::send(const String& message, ExceptionCode& ec)
     }
     // FIXME: check message is valid utf8.
     ASSERT(m_channel);
-    return m_channel->send(message);
+    return m_channel->send(message) == ThreadableWebSocketChannel::SendSuccess;
 }
 
 bool WebSocket::send(ArrayBuffer* binaryData, ExceptionCode& ec)
@@ -311,7 +311,7 @@ bool WebSocket::send(ArrayBuffer* binaryData, ExceptionCode& ec)
         return false;
     }
     ASSERT(m_channel);
-    return m_channel->send(*binaryData);
+    return m_channel->send(*binaryData) == ThreadableWebSocketChannel::SendSuccess;
 }
 
 bool WebSocket::send(Blob* binaryData, ExceptionCode& ec)
@@ -331,7 +331,7 @@ bool WebSocket::send(Blob* binaryData, ExceptionCode& ec)
         return false;
     }
     ASSERT(m_channel);
-    return m_channel->send(*binaryData);
+    return m_channel->send(*binaryData) == ThreadableWebSocketChannel::SendSuccess;
 }
 
 void WebSocket::close(int code, const String& reason, ExceptionCode& ec)
