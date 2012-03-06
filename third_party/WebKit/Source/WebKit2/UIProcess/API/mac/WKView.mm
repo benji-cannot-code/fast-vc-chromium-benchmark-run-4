6e3c23fdc8a0f15f206f89a72774ebce586f2800
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 01:35:47 +0000

    Can't type on some websites (plug-ins steal key events). <rdar://problem/10892291>
    When the plugin is disabled, it is necessary to reset _pluginComplexTextInputIdentifier
    in order to return the correct input context. Failure to do so results in the inputContext
    method to return the plugin input context instead of the context of the browser view.
            
    Reviewed by Sam Weinig.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _setPluginComplexTextInputState:]):
    (-[WKView _handlePluginComplexTextInputKeyDown:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -375,6 +375,7 @@ struct WKViewInterpretKeyEventsParameters {
 
     // Send back an empty string to the plug-in. This will disable text input.
     _data->_page->sendComplexTextInputToPlugin(_data->_pluginComplexTextInputIdentifier, String());
+    _data->_pluginComplexTextInputIdentifier = 0;   // Always reset the identifier when the plugin is disabled.
 }
 
 typedef HashMap<SEL, String> SelectorNameMap;
@@ -1264,8 +1265,10 @@ static const short kIOHIDEventTypeScroll = 6;
     if (string) {
         _data->_page->sendComplexTextInputToPlugin(_data->_pluginComplexTextInputIdentifier, string);
 
-        if (!usingLegacyCocoaTextInput)
+        if (!usingLegacyCocoaTextInput) {
             _data->_pluginComplexTextInputState = PluginComplexTextInputDisabled;
+            _data->_pluginComplexTextInputIdentifier = 0;   // Always reset the identifier when the plugin is disabled.
+        }
     }
 
     return didHandleEvent;
