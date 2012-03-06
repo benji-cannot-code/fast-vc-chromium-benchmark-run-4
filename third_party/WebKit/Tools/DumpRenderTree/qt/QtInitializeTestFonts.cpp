f3159e2f2dee00da2ae3d788fb93e825b37d6ee9
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-06 10:39:40 +0000

    Unreviewed, rolling out r109887. http://trac.webkit.org/changeset/109887 https://bugs.webkit.org/show_bug.cgi?id=80402
    It broke the build (Requested by Ossy on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-03-06
    
    * DumpRenderTree/qt/DumpRenderTree.pro:
    * DumpRenderTree/qt/QtInitializeTestFonts.cpp:
    (WebKit::initializeTestFonts):
    * QtTestBrowser/QtTestBrowser.pro:
    * WebKitTestRunner/InjectedBundle/Target.pri:
    * qmake/mkspecs/features/features.prf:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@109890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/QtInitializeTestFonts.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/qt/QtInitializeTestFonts.cpp
@@ -27,11 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-
 #include "QtInitializeTestFonts.h"
 
-#if HAVE(FONTCONFIG)
+#if defined(HAVE_FONTCONFIG) && HAVE_FONTCONFIG
 #include <QByteArray>
 #include <QDir>
 #include <fontconfig/fontconfig.h>
@@ -41,7 +39,7 @@ namespace WebKit {
 
 void initializeTestFonts()
 {
-#if HAVE(FONTCONFIG)
+#if defined(HAVE_FONTCONFIG) && HAVE_FONTCONFIG
     static int numFonts = -1;
 
     FcInit();
