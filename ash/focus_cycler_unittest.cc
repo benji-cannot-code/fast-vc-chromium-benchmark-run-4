bc3f1ab9fd801ec3f18e5bda4939243ae865f3b9
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 09:13:34 +0000

    Activate the status area only when it's focused using the keyboard.
    ash/status_area/status_area_view.cc:
    chrome/browser/chromeos/status/status_area_view.cc:
    
    Override views::WidgetDelegate::CanActivate. Return false unless the area is focused using the keyboard.
    
    ash/system/tray/system_tray.cc:
    
    Handle a mouse event correctly.
    
    ash/wm/activation_controller.cc:
    
    Change the order of kWindowContainerIds[] so that a browser window gets focused when a window in SettingBubbleContainer is closed.
    
    This fix is similar to http://crrev.com/122459 (by sky).
    
    BUG=115815
    TEST=aura_shell_unittests
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=125323
    
    Review URL: https://chromiumcodereview.appspot.com/9570013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125368 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/focus_cycler_unittest.cc
+++ ash/focus_cycler_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/launcher/launcher.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
+#include "ash/status_area/status_area_view.h"
 #include "ash/wm/window_util.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/shell_factory.h"
@@ -48,6 +49,8 @@ TEST_F(FocusCyclerTest, CycleFocusForward) {
   views::Widget* status_widget = internal::CreateStatusArea(NULL);
   ASSERT_TRUE(status_widget);
   focus_cycler->AddWidget(status_widget);
+  static_cast<internal::StatusAreaView*>(status_widget->GetContentsView())->
+      SetFocusCyclerForTesting(focus_cycler.get());
 
   // Add a mock button to the status area.
   status_widget->GetContentsView()->AddChildView(
@@ -89,6 +92,8 @@ TEST_F(FocusCyclerTest, CycleFocusBackward) {
   views::Widget* status_widget = internal::CreateStatusArea(NULL);
   ASSERT_TRUE(status_widget);
   focus_cycler->AddWidget(status_widget);
+  static_cast<internal::StatusAreaView*>(status_widget->GetContentsView())->
+      SetFocusCyclerForTesting(focus_cycler.get());
 
   // Add a mock button to the status area.
   status_widget->GetContentsView()->AddChildView(
@@ -161,6 +166,8 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusForwardInvisible) {
   views::Widget* status_widget = internal::CreateStatusArea(NULL);
   ASSERT_TRUE(status_widget);
   focus_cycler->AddWidget(status_widget);
+  static_cast<internal::StatusAreaView*>(status_widget->GetContentsView())->
+      SetFocusCyclerForTesting(focus_cycler.get());
 
   // Add a mock button to the status area.
   status_widget->GetContentsView()->AddChildView(
@@ -198,6 +205,8 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusBackwardInvisible) {
   views::Widget* status_widget = internal::CreateStatusArea(NULL);
   ASSERT_TRUE(status_widget);
   focus_cycler->AddWidget(status_widget);
+  static_cast<internal::StatusAreaView*>(status_widget->GetContentsView())->
+      SetFocusCyclerForTesting(focus_cycler.get());
 
   // Add a mock button to the status area.
   status_widget->GetContentsView()->AddChildView(
