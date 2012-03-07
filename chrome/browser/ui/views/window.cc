30534fd5d4513034865fdc0430787096145aa673
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 00:29:10 +0000

    Revert 125251 - Ash: Use translucent frames by default
    Was crashing a test on Linux ChromiumOS Tester (1):
    
    [ RUN      ] PrintPreviewUIUnitTest.PrintPreviewData
      base::debug::StackTrace::StackTrace() [0x27842fe]
      base::(anonymous namespace)::StackDumpSignalHandler() [0x27bcd09]
      0x7f5819766af0
      ConstrainedWindowViews::ConstrainedWindowViews() [0x1c785db]
      ConstrainedHtmlUI::CreateConstrainedHtmlDialog() [0x1c78170]
      printing::PrintPreviewTabController::CreatePrintPreviewTab() [0x1a63a21]
      printing::PrintPreviewTabController::GetOrCreatePreviewTab() [0x1a63dfb]
      PrintPreviewUIUnitTest_PrintPreviewData_Test::TestBody() [0x12200ba]
      testing::Test::Run() [0x2351031]
      testing::TestInfo::Run() [0x23510fa]
      testing::TestCase::Run() [0x2351247]
      testing::internal::UnitTestImpl::RunAllTests() [0x23514dd]
      testing::UnitTest::Run() [0x234fbe3]
      base::TestSuite::Run() [0x3ef7f48]
      main [0x24716e5]
      0x7f5819751c4d
      0x423779
    
    Remove --aura-translucent-frames switch.  Tie old-style frames to the existing --aura-window-mode=compact flag.
    
    Also fix a couple of cases where views::Widget wasn't be flagged as transparent, which would lead to black pixels in the window corners and incorrect headers in the inactive state.
    
    BUG=113075
    TEST=visual, look at active and inactive browser windows, popup windows, cookie management (constrained dialog), task manager (general dialog)
    
    
    Review URL: http://codereview.chromium.org/9618022
    
    TBR=jamescook@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9617029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/window.cc
+++ chrome/browser/ui/views/window.cc
@@ -34,28 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //       The remainder of the code here is dealing with the legacy CrOS WM and
 //       can also be removed.
 
-namespace {
-
-views::Widget* CreateViewsWindowWithParent(gfx::NativeWindow parent,
-                                           views::WidgetDelegate* delegate) {
-  views::Widget* widget = new views::Widget;
-  views::Widget::InitParams params;
-  params.delegate = delegate;
-#if defined(OS_WIN) || defined(USE_AURA)
-  params.parent = parent;
-#endif
-#if defined(USE_AURA)
-  // Outside of compact mode, dialog windows may have translucent frames.
-  // TODO(jamescook): Find a better way to set this.
-  if (!ash::Shell::GetInstance()->IsWindowModeCompact())
-    params.transparent = true;
-#endif
-  widget->Init(params);
-  return widget;
-}
-
-}  // namespace
-
 namespace browser {
 
 views::Widget* CreateViewsWindow(gfx::NativeWindow parent,
@@ -64,7 +42,7 @@ views::Widget* CreateViewsWindow(gfx::NativeWindow parent,
 #if defined(OS_CHROMEOS) && !defined(USE_AURA)
   return chromeos::BubbleWindow::Create(parent, style, delegate);
 #else
-  return CreateViewsWindowWithParent(parent, delegate);
+  return views::Widget::CreateWindowWithParent(delegate, parent);
 #endif
 }
 
