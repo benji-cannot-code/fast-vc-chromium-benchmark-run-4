0ab7426441ff14a93486681256e33ad91dd8d524
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 03:01:35 +0000

    [uber extensions] - make page look better without extensions
    1. when there are no extensions, leave 3em of space above the "Boo... You have no extensions :-(" message
    2. fix said message so it's all on one line (instead of link being on next line)
    3. simplify display/hidden handling for said message
    4. add i18n-html property so that we don't have to do it in javascript
    5. move dev mode checkbox into header (like in the mocks)
    6. simplify DOM
    7. simplify a string in app_resources.grd
    
    BUG=116457,116611
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9586021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -47,12 +47,6 @@ cr.define('extensions', function() {
       $('dev-controls').addEventListener('webkitTransitionEnd',
           this.handleDevControlsTransitionEnd_.bind(this));
 
-      // Setup the gallery related links and text.
-      $('suggest-gallery').innerHTML =
-          localStrings.getString('extensionSettingsSuggestGallery');
-      $('get-more-extensions').innerHTML =
-          localStrings.getString('extensionSettingsGetMoreExtensions');
-
       // Set up the three dev mode buttons (load unpacked, pack and update).
       $('load-unpacked').addEventListener('click',
           this.handleLoadUnpackedExtension_.bind(this));
@@ -181,10 +175,6 @@ cr.define('extensions', function() {
 
     webui_responded_ = true;
 
-    $('no-extensions').hidden = true;
-    $('suggest-gallery').hidden = true;
-    $('get-more-extensions-container').hidden = true;
-
     if (extensionsData.extensions.length > 0) {
       // Enforce order specified in the data or (if equal) then sort by
       // extension name (case-insensitive).
@@ -197,11 +187,6 @@ cr.define('extensions', function() {
           return a.order < b.order ? -1 : 1;
         }
       });
-
-      $('get-more-extensions-container').hidden = false;
-    } else {
-      $('no-extensions').hidden = false;
-      $('suggest-gallery').hidden = false;
     }
 
     if (extensionsData.developerMode) {
