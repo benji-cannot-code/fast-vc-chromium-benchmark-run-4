17a2b43a284264310dec404133edc3decac9d2b4
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 21:31:20 +0000

    Get rid of download_create_info.h entry in DEPS, it's not actually needed.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9619019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125460 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_util.cc
+++ chrome/browser/download/download_util.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/time_format.h"
-#include "content/browser/download/download_create_info.h"
 #include "content/browser/download/download_types.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item.h"
@@ -82,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the same value on all platforms.
 static const double PI = 3.141592653589793;
 
-using content::DownloadFile;
 using content::DownloadItem;
 
 namespace {
