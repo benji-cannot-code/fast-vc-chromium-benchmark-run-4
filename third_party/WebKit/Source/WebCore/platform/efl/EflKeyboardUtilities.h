9509388734dc557c6f5289dec3602618a19d74c4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-07 17:15:07 +0000

    [EFL] Key press event is not processed properly. https://bugs.webkit.org/show_bug.cgi?id=80491
    Patch by ChangSeok Oh <shivamidow@gmail.com> on 2012-03-07
    Reviewed by Gustavo Noronha Silva.
    
    This issue is related with mutation observer feature.
    If enter key is pressed, then a keyboard event should be processed
    and reach to the mutation observer, but it doesn't.
    Some special keys like Enter, Backspace and Tab key should be processed
    and change to a single character code, but EFL port hasn't handled like that.
    
    At least we can verify this with following two tests as I know. but they require another
    functionality for bug79601. I'm going to submit the patch for it after this one.
    
    Test: fast/mutation/end-of-task-delivery.html
          fast/mutation/inline-event-listener.html
    
    * platform/efl/EflKeyboardUtilities.cpp:
    (WebCore::singleCharacterString):
    (WebCore):
    * platform/efl/EflKeyboardUtilities.h:
    (WebCore):
    * platform/efl/PlatformKeyboardEventEfl.cpp:
    (WebCore::PlatformKeyboardEvent::PlatformKeyboardEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/EflKeyboardUtilities.h
+++ third_party/WebKit/Source/WebCore/platform/efl/EflKeyboardUtilities.h
@@ -36,6 +36,7 @@ class String;
 namespace WebCore {
 
 WTF::String keyIdentifierForEvasKeyName(WTF::String&);
+WTF::String singleCharacterString(const WTF::String&);
 int windowsKeyCodeForEvasKeyName(WTF::String&);
 
 } // namespace WebCore
