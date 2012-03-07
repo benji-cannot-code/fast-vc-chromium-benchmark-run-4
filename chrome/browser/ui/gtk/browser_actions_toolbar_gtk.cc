e57b4f1722a545ab140c0d8e89dbad26d299c2ea
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 03:18:45 +0000

    Force browser action icon size to always be 19px
    Fixed the browser action bar so that it will render the icon normal size
    if an icon is not specified. Also re-worked a test case for dynamically changing
    the browser action icon (BrowserActionApiTest.DynamicBrowserAction).
    Made the test more robust by checking the pixels of the icon to make sure it is actually
    changed. Screenshots of before and after are posted on the bug.
    
    BUG=111301
    TEST=Load an extension with a browser action and no icon specified. The icon
    now should be the default extension icon.
    
    Review URL: http://codereview.chromium.org/9424024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125314 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_actions_toolbar_gtk.cc
+++ chrome/browser/ui/gtk/browser_actions_toolbar_gtk.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/browser_actions_toolbar_gtk.h"
 
 #include <algorithm>
+#include <gtk/gtk.h>
 #include <vector>
 
 #include "base/bind.h"
@@ -103,6 +104,7 @@ class BrowserActionButton : public content::NotificationObserver,
         IDR_BROWSER_ACTION_H,
         0,
         NULL));
+    gtk_widget_set_size_request(button(), kButtonWidth, kButtonWidth);
     alignment_.Own(gtk_alignment_new(0, 0, 1, 1));
     gtk_container_add(GTK_CONTAINER(alignment_.get()), button());
     gtk_widget_show(button());
