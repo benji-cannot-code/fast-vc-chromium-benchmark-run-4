50c23c176771fad2d6d0ce4c0be28f9e0718cc70
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 20:51:37 +0000

    Add documentation regarding the nestability of sql::Transaction objects.
    This is because I wondered if they were nestable, looked here for documentation to that affect, didn't see any, and thus assumed they weren't (I also googled sqlite3 nested transactions and saw that they were not natively supported by the DB - I think that the conclusion I drew is probably the one that most readers would have drawn).
    
    BUG=None
    TEST=Ask a random developer if sql::Transaction transactions may be nested.
    
    
    Review URL: http://codereview.chromium.org/9624006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125448 0039d316-1c4b-4281-b951-d872f2087c98

--- sql/transaction.h
+++ sql/transaction.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,6 +20,9 @@ class SQL_EXPORT Transaction {
   // transaction. If you have begun a transaction and not committed it, the
   // constructor will roll back the transaction. If you want to commit, you
   // need to manually call Commit before this goes out of scope.
+  //
+  // Nested transactions are supported. See sql::Connection::BeginTransaction
+  // for details.
   explicit Transaction(Connection* connection);
   ~Transaction();
 
