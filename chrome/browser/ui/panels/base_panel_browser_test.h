0e76ee81d497909b981fdc54318947fc12c50e56
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 21:57:57 +0000

    Fix bug 114813: Resolution/screen configuration changes are missed if no panels exist during the change
    BUG=114813
    TEST=Manual test by closing all panels, changing resolution and opening a new panel
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=125278
    
    Review URL: http://codereview.chromium.org/9395059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/base_panel_browser_test.h
+++ chrome/browser/ui/panels/base_panel_browser_test.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/values.h"
 #include "base/memory/scoped_ptr.h"
-#include "chrome/browser/ui/panels/auto_hiding_desktop_bar.h"
+#include "chrome/browser/ui/panels/display_settings_provider.h"
 #include "chrome/browser/ui/panels/panel.h"
 #include "chrome/browser/ui/panels/panel_strip.h"
 #include "chrome/common/extensions/extension.h"
@@ -18,15 +18,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class BasePanelBrowserTest : public InProcessBrowserTest {
  public:
-  class MockAutoHidingDesktopBar : public AutoHidingDesktopBar {
+  class MockDisplaySettingsProvider : public DisplaySettingsProvider {
    public:
-    virtual ~MockAutoHidingDesktopBar() { }
+    explicit MockDisplaySettingsProvider(Observer* observer)
+        : DisplaySettingsProvider(observer) {
+    }
+    virtual ~MockDisplaySettingsProvider() { }
 
-    virtual void EnableAutoHiding(Alignment alignment,
-                                  bool enabled,
-                                  int thickness) = 0;
-    virtual void SetVisibility(Alignment alignment, Visibility visibility) = 0;
-    virtual void SetThickness(Alignment alignment, int thickness) = 0;
+    virtual void SetWorkArea(const gfx::Rect& work_area) = 0;
+    virtual void EnableAutoHidingDesktopBar(DesktopBarAlignment alignment,
+                                            bool enabled,
+                                            int thickness) = 0;
+    virtual void SetDesktopBarVisibility(DesktopBarAlignment alignment,
+                                         DesktopBarVisibility visibility) = 0;
+    virtual void SetDesktopBarThickness(DesktopBarAlignment alignment,
+                                        int thickness) = 0;
   };
 
   BasePanelBrowserTest();
@@ -94,19 +100,17 @@ class BasePanelBrowserTest : public InProcessBrowserTest {
   static void CloseWindowAndWait(Browser* browser);
   static std::string MakePanelName(int index);
 
-  gfx::Rect testing_work_area() const { return testing_work_area_; }
-  void set_testing_work_area(const gfx::Rect& work_area) {
-    testing_work_area_ = work_area;
-  }
+  gfx::Rect GetTestingWorkArea() const;
+  void SetTestingWorkArea(const gfx::Rect& work_area);
 
-  MockAutoHidingDesktopBar* mock_auto_hiding_desktop_bar() const {
-    return mock_auto_hiding_desktop_bar_.get();
+  MockDisplaySettingsProvider* mock_display_settings_provider() const {
+    return mock_display_settings_provider_;
   }
 
   static const FilePath::CharType* kTestDir;
  private:
-  gfx::Rect testing_work_area_;
-  scoped_refptr<MockAutoHidingDesktopBar> mock_auto_hiding_desktop_bar_;
+  // Passed to and owned by PanelManager.
+  MockDisplaySettingsProvider* mock_display_settings_provider_;
 };
 
 #endif  // CHROME_BROWSER_UI_PANELS_BASE_PANEL_BROWSER_TEST_H_
