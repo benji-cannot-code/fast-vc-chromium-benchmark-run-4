10961e267fb137b23653172a3fa0148714376572
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 20:33:24 +0000

    In CrMallocErrorBreak, do not kill the process if errno is ENOMEM.
    This will allow large JPEG decodes to be handled and optionally killed by the
    OOM killer instead. Based on a sampling of the other malloc_error_break() bugs
    ("unaligned pointer", "freed was not allocated", "double free", and "incorrect
    checksum"), this will only affect the "allocate region" error, as those others
    happen at free(), rather than malloc().
    
    BUG=103980
    TEST=Covered by ProcessUtilTest.MacTerminateOnHeapCorruption
    
    
    Review URL: http://codereview.chromium.org/9597031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125441 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_mac.mm
+++ base/process_util_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -550,6 +550,13 @@ malloc_error_break_t LookUpMallocErrorBreak() {
 
 void CrMallocErrorBreak() {
   g_original_malloc_error_break();
+
+  // Out of memory is certainly not heap corruption, and not necessarily
+  // something for which the process should be terminated. Leave that decision
+  // to the OOM killer.
+  if (errno == ENOMEM)
+    return;
+
   // A unit test checks this error message, so it needs to be in release builds.
   LOG(ERROR) <<
       "Terminating process due to a potential for future heap corruption";
