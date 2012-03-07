88374c3d819d808a372a7979227b59ccdcac41db
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 23:01:20 +0000

    Show confirmation prompt when the user deletes a host.
    BUG=111078
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9623020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125483 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/ui_mode.js
+++ remoting/webapp/ui_mode.js
@@ -38,6 +38,7 @@ remoting.AppMode = {
       CLIENT_SESSION_FINISHED_IT2ME: 'home.client.session-finished.it2me',
       CLIENT_SESSION_FINISHED_ME2ME: 'home.client.session-finished.me2me',
     HISTORY: 'home.history',
+    CONFIRM_HOST_DELETE: 'home.confirm-host-delete',
   IN_SESSION: 'in-session'
 };
 
@@ -50,7 +51,7 @@ remoting.AppMode = {
 remoting.hasModeAttribute = function(element, attr, mode) {
   return element.getAttribute(attr).match(
       new RegExp('(\\s|^)' + mode + '(\\s|$)')) != null;
-}
+};
 
 /**
  * Update the DOM by showing or hiding elements based on whether or not they
