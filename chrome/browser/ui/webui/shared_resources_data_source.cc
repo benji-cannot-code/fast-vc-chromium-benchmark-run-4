7edc3d47648f16f77813da985ed7f1a31110c064
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 18:30:31 +0000

    [linux] Revert use of 'open sans' on Linux.
    BUG=114402
    TEST=Web-UI pages show up in Arial.
    Review URL: https://chromiumcodereview.appspot.com/9609012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125413 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/shared_resources_data_source.cc
+++ chrome/browser/ui/webui/shared_resources_data_source.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,8 +34,6 @@ int PathToIDR(const std::string& path) {
     idr = IDR_FOLDER_OPEN_RTL;
   } else if (path == "ui/resources/throbber.png") {
     idr = IDR_THROBBER;
-  } else if (path == "fonts/open_sans.woff") {
-    idr = IDR_THIRD_PARTY_FONTS_OPEN_SANS;
   } else {
     // The name of the files in the grd list are prefixed with the following
     // directory:
