0b1f35af0ad7bc924176180256ac482a6dc7513d
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-07 20:37:32 +0000

    Implement removeAttributeNode() in terms of ElementAttributeData instead of NamedNodeMap https://bugs.webkit.org/show_bug.cgi?id=80522
    Reviewed by Andreas Kling.
    
    DOM methods exposed in Element and NamedNodeMap are now implemented in terms of
    ElementAttributeData. A helper function takeAttribute() was added to increase
    code sharing. Also removed some unneeded methods.
    
    * dom/Element.cpp:
    (WebCore::Element::removeAttribute): Use ElementAttributeData function directly.
    (WebCore::Element::removeAttributeNode): Get the index manually and use new
    takeAttribute() directly.
    * dom/Element.h:
    (Element): Remove unused removeAttribute(unsigned index).
    * dom/ElementAttributeData.cpp:
    (WebCore::ElementAttributeData::takeAttribute): Like removeAttribute() but
    returns a reference to old Attr.
    (WebCore):
    * dom/ElementAttributeData.h:
    (ElementAttributeData):
    * dom/NamedNodeMap.cpp:
    (WebCore::NamedNodeMap::removeNamedItem): Avoid looking up the property twice by
    getting the index directly from the name, instead of going through the qualified name.
    * dom/NamedNodeMap.h: Remove now unused internal methods for removing attributes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.h
+++ third_party/WebKit/Source/WebCore/dom/Element.h
@@ -161,7 +161,6 @@ public:
     size_t attributeCount() const;
     Attribute* attributeItem(unsigned index) const;
     Attribute* getAttributeItem(const QualifiedName&) const;
-    void removeAttribute(unsigned index);
 
     void scrollIntoView(bool alignToTop = true);
     void scrollIntoViewIfNeeded(bool centerIfNeeded = true);
@@ -672,12 +671,6 @@ inline Attribute* Element::getAttributeItem(const QualifiedName& name) const
 {
     ASSERT(m_attributeMap);
     return m_attributeMap->attributeData()->getAttributeItem(name);
-}
-
-inline void Element::removeAttribute(unsigned index)
-{
-    ASSERT(m_attributeMap);
-    m_attributeMap->removeAttribute(index);
 }
 
 inline void Element::updateInvalidAttributes() const
