9e706164be7c5c5802d1ab1ed5d26631c7c628f9
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-08 23:15:00 +0000

    Full Screen Refactor Part 3: Animate into Full Screen mode using new animation classes. https://bugs.webkit.org/show_bug.cgi?id=78928
    Reviewed by Anders Carlsson.
    
    Source/WebCore:
    
    Move WKFullScreenWindow from WebKit2 into WebCore to be shared by WebKit2 and WebKit.
    * WebCore.xcodeproj/project.pbxproj:
    * platform/mac/WebCoreFullScreenWindow.h:
    * platform/mac/WebCoreFullScreenWindow.mm:
    
    Add symbols for the following classes and functions to the export list:
        WebCoreFullScreenWindow
        WebWindowScaleAnimation
        WebWindowFadeAnimation
        ScrollView::contentsToScreen()
        RenderObject::localToContainerQuad()
        Document::setAnimatingFullScreen()
    * WebCore.exp.in:
    
    Source/WebKit2:
    
    Boilerplate changes to WebKit2 IPC messages and supporting functions.
    * UIProcess/WebFullScreenManagerProxy.cpp:
    (WebKit::WebFullScreenManagerProxy::setAnimatingFullScreen): Added boilerplate.
    * UIProcess/WebFullScreenManagerProxy.h:
    * UIProcess/WebFullScreenManagerProxy.messages.in:
    * UIProcess/mac/WebFullScreenManagerProxyMac.mm:
    (WebKit::WebFullScreenManagerProxy::beganEnterFullScreen): Added boilerplate.
    (WebKit::WebFullScreenManagerProxy::beganExitFullScreen): Added boilerplate.
    * WebProcess/FullScreen/WebFullScreenManager.cpp:
    (WebKit::screenRectOfContents): Added.  Calculates the screen rect of an element's contents.
    (WebKit::WebFullScreenManager::enterFullScreenForElement): Use screenRectOfContents()
    (WebKit::WebFullScreenManager::willEnterFullScreen): Ditto. Do not set the background color. Call new BeganEnterFullScreen
        XPC message.
    (WebKit::WebFullScreenManager::didEnterFullScreen): Do not set the background color.
    (WebKit::WebFullScreenManager::willExitFullScreen): Use screenRectOfContents. Do not set the background color. Call new
        BeganExitFullScreen XPC message.
    (WebKit::WebFullScreenManager::didExitFullScreen): Do not set the background color.
    (WebKit::WebFullScreenManager::setAnimatingFullScreen): Added boilerplate.
    * WebProcess/FullScreen/WebFullScreenManager.h:
    * WebProcess/FullScreen/WebFullScreenManager.messages.in:
    
    * UIProcess/mac/WKFullScreenWindowController.h:
    * UIProcess/mac/WKFullScreenWindowController.mm:
    (-[WKFullScreenWindowController cancelOperation:]): Renamed from _requestExitWithAnimation:.
    (-[WKFullScreenWindowController applicationDidResignActive:]): Call cancelOperation: instead
        of _requestExitWithAnimation.
    (-[WKFullScreenWindowController applicationDidChangeScreenParameters:]): Set the frame of
        both the full screen window and the background window.
    (-[WKFullScreenWindowController enterFullScreen:]): Save a rendered image of the current
        page to use in the placeholder.
    (-[WKFullScreenWindowController beganEnterFullScreenWithInitialFrame:WebCore::finalFrame:WebCore::]): Renamed from
        beganEnterFullScreenAnimation.
    (-[WKFullScreenWindowController finishedEnterFullScreenAnimation:]): No need to swap
        placeholder views here, as they were already swapped in beganEnter...:.
    (-[WKFullScreenWindowController exitFullScreen]):
    (-[WKFullScreenWindowController beganExitFullScreenWithInitialFrame:WebCore::finalFrame:WebCore::]): Renamed from
        beganExitFullScreenAnimation.
    (-[WKFullScreenWindowController finishedExitFullScreenAnimation:]): Swap web view back into place.
    (-[WKFullScreenWindowController close]):
    (-[WKFullScreenWindowController animationDidEnd:]): Added.
    (createBackgroundFullscreenWindow): Added.
    (windowFrameFromApparentFrames): Added.
    (-[WKFullScreenWindowController _startEnterFullScreenAnimationWithDuration:]): Added.
    (-[WKFullScreenWindowController _startExitFullScreenAnimationWithDuration:]): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/WebCoreFullScreenWindow.h
+++ third_party/WebKit/Source/WebCore/platform/mac/WebCoreFullScreenWindow.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,55 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "config.h"
-#import "WebFullScreenManagerProxy.h"
+#ifndef WebCoreFullScreenWindow_h
+#define WebCoreFullScreenWindow_h
 
-#import "LayerTreeContext.h"
-#import "WKFullScreenWindowController.h"
-#import "WKViewInternal.h"
-#import <WebCore/IntRect.h>
+@interface WebCoreFullScreenWindow : NSWindow
+@end
 
-#if ENABLE(FULLSCREEN_API)
-
-using namespace WebCore;
-
-namespace WebKit {
-
-void WebFullScreenManagerProxy::invalidate()
-{
-    if (!m_webView)
-        return;
-    
-    [m_webView closeFullScreenWindowController];
-    m_webView = 0;
-}
-
-void WebFullScreenManagerProxy::enterFullScreen()
-{
-    if (!m_webView)
-        return;
-    [[m_webView fullScreenWindowController] enterFullScreen:nil];
-}
-
-void WebFullScreenManagerProxy::exitFullScreen()
-{
-    if (!m_webView)
-        return;
-    [[m_webView fullScreenWindowController] exitFullScreen];
-}
-    
-void WebFullScreenManagerProxy::beganEnterFullScreen(const IntRect& initialFrame, const IntRect& finalFrame)
-{
-    if (m_webView)
-        [[m_webView fullScreenWindowController] beganEnterFullScreenWithInitialFrame:initialFrame finalFrame:finalFrame];
-}
-
-void WebFullScreenManagerProxy::beganExitFullScreen(const IntRect& initialFrame, const IntRect& finalFrame)
-{
-    if (m_webView)
-        [[m_webView fullScreenWindowController] beganExitFullScreenWithInitialFrame:initialFrame finalFrame:finalFrame];
-}
-
-} // namespace WebKit
-
-#endif
+#endif // WebCoreFullScreenWindow_h
--- third_party/WebKit/Source/WebKit2/UIProcess/WebFullScreenManagerProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebFullScreenManagerProxy.h
@@ -81,9 +81,7 @@ public:
     void didEnterFullScreen();
     void willExitFullScreen();
     void didExitFullScreen();
-    void beginEnterFullScreenAnimation(float duration) { } // FIXME: Remove
-    void beginExitFullScreenAnimation(float duration) { } // FIXME: Remove
-    void disposeOfLayerClient() { } // FIXME: Remove
+    void setAnimatingFullScreen(bool);
 
 private:
     WebFullScreenManagerProxy(WebPageProxy*);
@@ -91,6 +89,8 @@ private:
     void supportsFullScreen(bool withKeyboard, bool&);
     void enterFullScreen();
     void exitFullScreen();
+    void beganEnterFullScreen(const WebCore::IntRect& initialFrame, const WebCore::IntRect& finalFrame);
+    void beganExitFullScreen(const WebCore::IntRect& initialFrame, const WebCore::IntRect& finalFrame);
 
     WebPageProxy* m_page;
     PlatformWebView* m_webView;
