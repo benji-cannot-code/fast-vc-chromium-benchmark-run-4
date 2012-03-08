80de5262790cdf914b5d102cc0b7f1e9c7739141
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 20:57:34 +0000

    Fix 117074: Panels [WIN]: Crash data shows a stack trace consistently involving PanelManager
    The fix is not to involve PanelManager when it is not needed on the balloon notification code.
    
    BUG=117074
    TEST=Manual test to verify PanelManager is not involved on start.
    
    
    Review URL: http://codereview.chromium.org/9621020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125679 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/balloon_collection_impl.h
+++ chrome/browser/notifications/balloon_collection_impl.h
@@ -144,6 +144,10 @@ class BalloonCollectionImpl : public BalloonCollection,
     // to be repositioned.
     bool ComputeOffsetToMoveAbovePanels(const gfx::Rect& panel_bounds);
 
+    void enable_computing_panel_offset() {
+      need_to_compute_panel_offset_ = true;
+    }
+
    private:
     // Layout parameters
     int VerticalEdgeMargin() const;
@@ -161,6 +165,10 @@ class BalloonCollectionImpl : public BalloonCollection,
 
     Placement placement_;
     gfx::Rect work_area_;
+
+    // The flag that indicates that the panel offset computation should be
+    // performed.
+    bool need_to_compute_panel_offset_;
 
     // The offset that guarantees that the notificaitions shown in the
     // lower-right or lower-left corner of the screen will go above currently
