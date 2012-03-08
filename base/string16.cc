004e45c63ea975601f293f54b34c6ccf72c0f47d
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 18:34:50 +0000

    Add PrintTo for string16
    This is required by googletest to print a readable output when failing to compare two string16's (ref: http://goo.gl/jALbO)
    
    Without this the output of a failed comparison could be:
    
    Value of: ElideEmail(UTF8ToUTF16(testcases[i].input), font, font.GetStringWidth(expected_output))
      Actual: { 97, 116, 34, 64, 64, 64, 64, 64, 64, 64, 64, 64, 46, 46, 46, 64, 64, 46, 8230, 64, 109, 97, 100, 110, 101, 115, 115, 46, 99, 111, 109 }
    Expected: expected_output
    Which is: { 97, 116, 34, 64, 64, 64, 64, 64, 64, 64, 64, 64, 46, 46, 46, 64, 64, 46, 64, 46, 8230, 64, 109, 97, 110, 101, 115, 115, 46, 99, 111, 109 }
    
    With this patch the same error is now properly formatted:
    
    Value of: ElideEmail(UTF8ToUTF16(testcases[i].input), font, font.GetStringWidth(expected_output))
      Actual: at"@@@@@@@@@...@@.…@madness.com
    Expected: expected_output
    Which is: at"@@@@@@@@@...@@.@.…@maness.com
    
    BUG=
    TEST=unit_tests
    
    
    Review URL: http://codereview.chromium.org/9641017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125642 0039d316-1c4b-4281-b951-d872f2087c98

--- base/string16.cc
+++ base/string16.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -75,6 +75,10 @@ template class std::basic_string<char16, base::string16_char_traits>;
 namespace base {
 std::ostream& operator<<(std::ostream& out, const string16& str) {
   return out << UTF16ToUTF8(str);
+}
+
+void PrintTo(const string16& str, std::ostream* out) {
+  *out << str;
 }
 }
 
--- base/string16.h
+++ base/string16.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -172,6 +172,9 @@ typedef std::basic_string<char16, base::string16_char_traits> string16;
 namespace base {
 BASE_EXPORT extern std::ostream& operator<<(std::ostream& out,
                                             const string16& str);
+
+// This is required by googletest to print a readable output on test failures.
+BASE_EXPORT extern void PrintTo(const string16& str, std::ostream* out);
 }
 
 #endif  // WCHAR_T_IS_UTF32
