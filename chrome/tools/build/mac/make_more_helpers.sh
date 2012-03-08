fbd63651cd282b650c897179b7deb7b73b95274c
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 12:42:44 +0000

    Make Info.plist in helper apps readable for everyone.
    This avoids a DCHECK when running a Debug build as a different user from the one who built it.
    
    BUG=117148
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9621005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125605 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/build/mac/make_more_helpers.sh
+++ chrome/tools/build/mac/make_more_helpers.sh
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
 
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -72,6 +72,10 @@ $(dirname "${0}")/../../../../build/mac/change_mach_o_flags.py"
   # As usual, defaults might have put the plist into whatever format excites
   # it, but Info.plists get converted back to the expected XML format.
   plutil -convert xml1 "${feature_info_plist}"
+
+  # `defaults` also changes the file permissions, so make the file
+  # world-readable again.
+  chmod a+r "${feature_info_plist}"
 }
 
 if [[ ${#} -ne 2 ]]; then
