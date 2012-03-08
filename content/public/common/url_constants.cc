8784923c39920584d3c79a355fd0e7a690758f1e
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 18:07:54 +0000

    Don't put the error URL in the chrome:// scheme.
    BUG=117230
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9632002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125635 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/url_constants.cc
+++ content/public/common/url_constants.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -54,7 +54,9 @@ const char kChromeUIHangURL[] = "chrome://hang";
 const char kChromeUIKillURL[] = "chrome://kill";
 const char kChromeUIShorthangURL[] = "chrome://shorthang";
 
-const char kUnreachableWebDataURL[] = "chrome://chromewebdata/";
+// This error URL is loaded in normal web renderer processes, so it should not
+// have a chrome:// scheme that might let it be confused with a WebUI page.
+const char kUnreachableWebDataURL[] = "data:text/html,chromewebdata";
 
 const char** GetSavableSchemes() {
   return const_cast<const char**>(g_savable_schemes);
