bfec24dd8c4413dfec11c8d72b1d799810a8c86f
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-08 23:38:11 +0000

    Further unreviewed build fix.
    Add in the WebCore namespace, so that IntRect is pulled in.
    
    * UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp:
    * UIProcess/qt/WebFullScreenManagerProxyQt.cpp:
    * UIProcess/win/WebFullScreenManagerProxyWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/win/WebFullScreenManagerProxyWin.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/win/WebFullScreenManagerProxyWin.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FullScreenController.h>
 #include <WebCore/IntRect.h>
 
+using namespace WebCore;
+
 namespace WebKit {
 
 void WebFullScreenManagerProxy::invalidate()
