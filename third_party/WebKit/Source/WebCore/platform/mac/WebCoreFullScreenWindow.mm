51a618f029338cb7dc4d62fc50e5b6f6de2cc785
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-08 23:46:30 +0000

    Yet another unreviewed build fix.
    Remove an unneeded and Lion-defined window constant from WebCoreFullScreenWindow.
    
    * platform/mac/WebCoreFullScreenWindow.mm:
    (-[WebCoreFullScreenWindow initWithContentRect:styleMask:backing:defer:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110225 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/WebCoreFullScreenWindow.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/WebCoreFullScreenWindow.mm
@@ -45,8 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
     [self setMovableByWindowBackground:NO];
 #endif
-    [self setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
-    
+
     return self;
 }
 
