2c0fb5c19a779331a92d273054838b7cc824d7dc
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 05:01:43 +0000

    [uber extensions] fix overlays for embedding in uber
    also make the alert overlay have a sane appearance
    
    BUG=116459
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9616042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -240,6 +240,8 @@ cr.define('extensions', function() {
     if (node)
       node.classList.add('showing');
     overlay.hidden = !node;
+    uber.invokeMethodOnParent(node ? 'beginInterceptingEvents' :
+                                     'stopInterceptingEvents');
   }
 
   // Export
