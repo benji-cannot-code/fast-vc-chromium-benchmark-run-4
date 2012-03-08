a997cb58961323f860f9fd2564ecf8cad1fd52fc
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 12:38:44 +0000

    Make TabLoader (session_restore.cc) functional again.
    It didn't do anything because Browser::AddRestoredTab was already calling LoadIfNecessary
    for all the restored tabs (since http://codereview.chromium.org/8678002 ).
    
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/9503001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1469,7 +1469,6 @@ WebContents* Browser::AddRestoredTab(
     // contains similar logic.
     new_tab->GetView()->SizeContents(window_->GetRestoredBounds().size());
     new_tab->HideContents();
-    new_tab->GetController().LoadIfNecessary();
   }
   SessionService* session_service =
       SessionServiceFactory::GetForProfileIfExisting(profile_);
