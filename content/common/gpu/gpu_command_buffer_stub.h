ae3053fb60c6745f7fb3710c006a462bce838063
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 03:29:54 +0000

    Drop frontbuffers on chromeos when tab visibility set to hibernated (GLX image transport path)
    BUG=112842
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/9447023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125534 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.h
+++ content/common/gpu/gpu_command_buffer_stub.h
@@ -72,9 +72,9 @@ class CONTENT_EXPORT GpuCommandBufferStubBase {
 };
 
 class GpuCommandBufferStub
-    : public IPC::Channel::Listener,
+    : public GpuCommandBufferStubBase,
+      public IPC::Channel::Listener,
       public IPC::Message::Sender,
-      public GpuCommandBufferStubBase,
       public base::SupportsWeakPtr<GpuCommandBufferStub> {
  public:
   class DestructionObserver {
