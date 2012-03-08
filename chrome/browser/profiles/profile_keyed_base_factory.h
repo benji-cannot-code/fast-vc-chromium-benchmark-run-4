94d1de2a97d364e9ec1d64195fe29e63ec04b890
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-08 03:39:19 +0000

    Profiles: Mark the factories as NonThreadSafe and allow services to be deleted on certain threads.
    This was first committed in r125282 and was reverted in r125282. The speech tests have been fixed so they no longer access a PKSF on the IO thread.
    
    BUG=116892, 116887
    
    First Review URL: http://codereview.chromium.org/9617023
    
    
    Review URL: http://codereview.chromium.org/9608009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_keyed_base_factory.h
+++ chrome/browser/profiles/profile_keyed_base_factory.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 
+#include "base/threading/non_thread_safe.h"
+
 class PrefService;
 class Profile;
 class ProfileKeyedBase;
@@ -21,7 +23,7 @@ class ProfileDependencyManager;
 // RefcountedProfileKeyedServiceFactory. This object describes general profile
 // dependency management; subclasses react to lifecycle events and implement
 // memory management.
-class ProfileKeyedBaseFactory {
+class ProfileKeyedBaseFactory : public base::NonThreadSafe {
  public:
   // A function that replaces the (possibly internal) object used by this
   // factory. For the majority of cases, this is the object returned to users.
