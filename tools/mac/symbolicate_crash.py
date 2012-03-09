edbcdeae0fc1b98bea9031535c98c02a3397f1d0
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-09 20:40:11 +0000

    symbolicate_crash.py: Thread HEX names are sometimes more or less than 4 chars.
    BUG=none
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/9662014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125887 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/mac/symbolicate_crash.py
+++ tools/mac/symbolicate_crash.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -184,7 +184,7 @@ class CrashReport(object):
 
     # The stack trace begins with the thread header, which is identified by a
     # HEX number. The thread names appear to be incorrect in spindumps.
-    user_thread_re = re.compile('^  Thread ([0-9a-fx]{4})')
+    user_thread_re = re.compile('^  Thread ([0-9a-fx]+)')
 
     # When this method is called, the fd has been walked right up to the first
     # line.
