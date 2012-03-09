8f73b3fa1aa9daa112cfa86d73d74952fef4c491
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-09 10:41:10 +0000

    Fix unit_test compile error in chrome/browser/webdata for Android
    BUG=117407
    TEST=
    
    Review URL: http://codereview.chromium.org/9645001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125821 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/autofill_entry_unittest.cc
+++ chrome/browser/webdata/autofill_entry_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,7 +35,7 @@ TEST(AutofillEntryTest, Culling) {
     source.push_back(base::Time::FromInternalValue(
         internal_value + i * offset));
   }
-  sort(source.begin(), source.end());
+  std::sort(source.begin(), source.end());
   EXPECT_TRUE(AutofillEntry::CullTimeStamps(source, &result));
 
   EXPECT_EQ(result.size(), kMaxAutofillTimeStamps);
