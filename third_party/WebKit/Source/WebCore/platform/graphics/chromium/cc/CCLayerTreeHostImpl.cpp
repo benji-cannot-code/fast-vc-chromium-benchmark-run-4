267b33e32e593421eed9dfedf3f5cfdb20528b06
nduca@chromium.org <nduca@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-09 03:52:25 +0000

    [chromium] Pass setVisibility to CCLayerTreeHostImpl regardless of LRC initialization status https://bugs.webkit.org/show_bug.cgi?id=80584
    Reviewed by James Robinson.
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::setVisible):
    (WebCore::CCLayerTreeHost::didBecomeInvisibleOnImplThread):
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::setVisible):
    (WebCore::CCLayerTreeHostImpl::initializeLayerRenderer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp
@@ -434,8 +434,10 @@ void CCLayerTreeHostImpl::setVisible(bool visible)
         return;
     m_visible = visible;
 
-    if (m_layerRenderer)
-        m_layerRenderer->setVisible(visible);
+    if (!m_layerRenderer)
+        return;
+
+    m_layerRenderer->setVisible(visible);
 
     // Reset the damage tracker because the front/back buffers may have been damaged by the GPU
     // process on visibility change.
@@ -457,6 +459,10 @@ bool CCLayerTreeHostImpl::initializeLayerRenderer(PassRefPtr<GraphicsContext3D> 
     }
 
     m_layerRenderer = layerRenderer.release();
+
+    if (!m_visible && m_layerRenderer)
+         m_layerRenderer->setVisible(m_visible);
+
     return m_layerRenderer;
 }
 
