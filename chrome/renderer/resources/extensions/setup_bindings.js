41d54cf48fe9b72058a10d5a76697fded78e7ecf
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-09 06:38:50 +0000

    Convert app_bindings.js to the schema_generated_bindings.js infrastructure. This involves opening up *all* custom bindings to web pages, where access is controlled content-script style using a URL-matches property in the API schema.
    BUG=104100,117282
    TEST=unit_tests,browser_tests
    
    Review URL: http://codereview.chromium.org/9460002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125811 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/setup_bindings.js
+++ chrome/renderer/resources/extensions/setup_bindings.js
@@ -3,20 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var contextInfo = requireNative('context_info');
 var sgb = requireNative('schema_generated_bindings');
 
-require('app');
 require('webstore');
+require('json_schema');
+require('event_bindings');
+require('miscellaneous_bindings');
+require('schema_generated_bindings');
+require('apitest');
 
-if (contextInfo.IsBindingsAllowed()) {
-  require('miscellaneous_bindings');
-  require('schema_generated_bindings');
-  require('apitest');
-
-  // Load the custom bindings for each API.
-  sgb.GetExtensionAPIDefinition().forEach(function(apiDef) {
-    if (contextInfo.IsAPIAllowed(apiDef.namespace))
-      require(apiDef.namespace);
-  });
-}
+// Load the custom bindings for each API.
+sgb.GetExtensionAPIDefinition().forEach(function(apiDef) {
+  require(apiDef.namespace);
+});
