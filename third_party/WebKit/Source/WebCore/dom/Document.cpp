6dd4a2fa604f83f5ed1024516d67f04a580528f2
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-09 01:22:49 +0000

    Mac build fix for micro data API.
    Source/JavaScriptCore: 
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore: 
    
    * Configurations/FeatureDefines.xcconfig:
    * WebCore.xcodeproj/project.pbxproj:
    * dom/Document.cpp:
    (WebCore::Document::removeCachedMicroDataItemList):
    * html/HTMLElement.idl:
    
    Source/WebKit/mac: 
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2: 
    
    * Configurations/FeatureDefines.xcconfig:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -5422,7 +5422,6 @@ void Document::removeCachedMicroDataItemList(MicroDataItemList* list, const Stri
 {
     ASSERT(rareData());
     ASSERT(rareData()->nodeLists());
-    ASSERT_UNUSED(list, list->hasOwnCaches());
 
     NodeListsNodeData* data = rareData()->nodeLists();
 
