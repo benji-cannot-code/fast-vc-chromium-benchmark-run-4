21a0704490409bf519881e0f0887f3b1100eff4d
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-09 19:11:21 +0000

    Help: Implement the sliding 'More info' pane.
    BUG=116091
    TEST=none
    R=csilv
    
    Review URL: https://chromiumcodereview.appspot.com/9657010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125868 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/help/help.js
+++ chrome/browser/resources/help/help.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 <include src="../uber/uber_utils.js">
-<include src="more_info_overlay.js">
 
 cr.define('help', function() {
   var localStrings = new LocalStrings();
@@ -35,23 +34,43 @@ cr.define('help', function() {
       if (productTOS)
         productTOS.innerHTML = localStrings.getString('productTOS');
 
-      var moreInfoOverlay = help.MoreInfoOverlay.getInstance();
-      moreInfoOverlay.initializePage();
-
       $('get-help').onclick = chrome.send.bind(chrome, 'openHelpPage');
       $('report-issue').onclick =
           chrome.send.bind(chrome, 'openFeedbackDialog');
 
-      this.maybeSetOnClick_($('more-info'),
-          this.showOverlay_.bind(this, $('more-info-overlay')));
+      this.maybeSetOnClick_($('more-info-expander'),
+          this.toggleMoreInfo_.bind(this));
 
       this.maybeSetOnClick_($('promote'),
           chrome.send.bind(chrome, 'promoteUpdater'));
       this.maybeSetOnClick_($('relaunch'),
           chrome.send.bind(chrome, 'relaunchNow'));
 
+      var channelChanger = $('channel-changer');
+      if (channelChanger) {
+        var self = this;
+        channelChanger.onchange = function(event) {
+          self.setReleaseChannel_(event.target.value);
+        }
+      }
+
       // Attempt to update.
       chrome.send('onPageLoaded');
+    },
+
+    /**
+     * Toggles the visible state of the 'More Info' section.
+     * @private
+     */
+    toggleMoreInfo_: function() {
+      var moreInfo = $('more-info-container');
+      var visible = moreInfo.className == 'visible';
+      moreInfo.className = visible ? '' : 'visible';
+      moreInfo.addEventListener('webkitTransitionEnd', function(event) {
+        $('more-info-expander').textContent = visible ?
+            localStrings.getString('showMoreInfo') :
+                localStrings.getString('hideMoreInfo');
+      });
     },
 
     /**
@@ -170,7 +189,7 @@ cr.define('help', function() {
      * @private
      */
     updateEnableReleaseChannel_: function(enabled) {
-      $('more-info').hidden = !enabled;
+      $('channel-changer-container').hidden = !enabled;
     },
 
     /**
