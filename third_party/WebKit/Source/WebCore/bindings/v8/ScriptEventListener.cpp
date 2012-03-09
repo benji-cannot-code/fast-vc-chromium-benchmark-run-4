b6bd4828b7c9418a5e2e5725769f32785ed1d3d1
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-09 20:12:35 +0000

    [V8] Fix object scope for inline event attribute handlers https://bugs.webkit.org/show_bug.cgi?id=80329
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    We now create the funciton inside the with-statements with the current scope objects.
    This is important for a few reasons:
    
    - We need to use the real objects and not just lookup the JS properties because these might have been overridden.
    - We need to use the node, form and document at the time of the preparation and not at the time of calling.
    - We need to ensure that event/evt is bound closer than a property with the same name in the object environment
      created by the with-statements.
    
    Tests: fast/dom/inline-event-attributes-lookup-removed-form.html
           fast/dom/inline-event-attributes-lookup-removed.html
           fast/dom/inline-event-attributes-lookup.html
    
    * bindings/v8/ScriptEventListener.cpp:
    (WebCore::eventParameterName):
    (WebCore):
    (WebCore::createAttributeEventListener):
    * bindings/v8/V8LazyEventListener.cpp:
    (WebCore::V8LazyEventListener::V8LazyEventListener):
    (WebCore):
    (WebCore::toObjectWrapper):
    (WebCore::V8LazyEventListener::callListenerFunction):
    (WebCore::V8LazyEventListener::prepareListenerObject):
    * bindings/v8/V8LazyEventListener.h:
    (WebCore):
    (WebCore::V8LazyEventListener::create):
    (V8LazyEventListener):
    
    LayoutTests:
    
    * fast/dom/inline-event-attributes-lookup-expected.txt: Added.
    * fast/dom/inline-event-attributes-lookup-removed-expected.txt: Added.
    * fast/dom/inline-event-attributes-lookup-removed-form-expected.txt: Added.
    * fast/dom/inline-event-attributes-lookup-removed-form.html: Added.
    * fast/dom/inline-event-attributes-lookup-removed.html: Added.
    * fast/dom/inline-event-attributes-lookup.html: Added.
    * fast/forms/lazy-event-listener-scope-chain-expected.txt:
    * fast/forms/lazy-event-listener-scope-chain.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptEventListener.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptEventListener.cpp
@@ -43,6 +43,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+static const String& eventParameterName(bool isSVGEvent)
+{
+    DEFINE_STATIC_LOCAL(const String, eventString, ("event"));
+    DEFINE_STATIC_LOCAL(const String, evtString, ("evt"));
+    return isSVGEvent ? evtString : eventString;
+}
+
 PassRefPtr<V8LazyEventListener> createAttributeEventListener(Node* node, Attribute* attr)
 {
     ASSERT(node);
@@ -58,12 +65,11 @@ PassRefPtr<V8LazyEventListener> createAttributeEventListener(Node* node, Attribu
         ScriptController* scriptController = frame->script();
         if (!scriptController->canExecuteScripts(AboutToExecuteScript))
             return 0;
-
         position = scriptController->eventHandlerPosition();
         sourceURL = node->document()->url().string();
     }
 
-    return V8LazyEventListener::create(attr->localName().string(), node->isSVGElement(), attr->value(), sourceURL, position, WorldContextHandle(UseMainWorld));
+    return V8LazyEventListener::create(attr->localName().string(), eventParameterName(node->isSVGElement()), attr->value(), sourceURL, position, node, WorldContextHandle(UseMainWorld));
 }
 
 PassRefPtr<V8LazyEventListener> createAttributeEventListener(Frame* frame, Attribute* attr)
@@ -81,7 +87,8 @@ PassRefPtr<V8LazyEventListener> createAttributeEventListener(Frame* frame, Attri
 
     TextPosition position = scriptController->eventHandlerPosition();
     String sourceURL = frame->document()->url().string();
-    return V8LazyEventListener::create(attr->localName().string(), frame->document()->isSVGDocument(), attr->value(), sourceURL, position, WorldContextHandle(UseMainWorld));
+
+    return V8LazyEventListener::create(attr->localName().string(), eventParameterName(frame->document()->isSVGDocument()), attr->value(), sourceURL, position, 0, WorldContextHandle(UseMainWorld));
 }
 
 String eventListenerHandlerBody(Document* document, EventListener* listener)
