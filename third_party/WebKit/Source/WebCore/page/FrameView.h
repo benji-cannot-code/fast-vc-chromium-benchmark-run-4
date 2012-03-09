f9dc2e9307093656202d2e98f1fd8a4b0673df51
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-09 22:43:30 +0000

    Unreviewed, rolling out r110309. http://trac.webkit.org/changeset/110309 https://bugs.webkit.org/show_bug.cgi?id=80732
    Seems to have caused a number of SVG crashes (thorton will
    investigate further) (Requested by abarth|gardener on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-03-09
    
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    (WebCore::FrameView::reset):
    (WebCore::FrameView::repaintContentRectangle):
    (WebCore::FrameView::deferredRepaintTimerFired):
    * page/FrameView.h:
    (FrameView):
    * rendering/RenderView.cpp:
    (WebCore::RenderView::shouldRepaint):
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::drawSVGToImageBuffer):
    (WebCore::SVGImage::draw):
    * svg/graphics/SVGImage.h:
    * svg/graphics/SVGImageCache.cpp:
    (WebCore::SVGImageCache::imageContentChanged):
    (WebCore::SVGImageCache::redrawTimerFired):
    * svg/graphics/SVGImageCache.h:
    (SVGImageCache):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110333 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.h
+++ third_party/WebKit/Source/WebCore/page/FrameView.h
@@ -204,10 +204,6 @@ public:
     void checkStopDelayingDeferredRepaints();
     void resetDeferredRepaintDelay();
 
-    void beginDisableRepaints();
-    void endDisableRepaints();
-    bool repaintsDisabled() { return m_disableRepaints > 0; }
-
 #if ENABLE(DASHBOARD_SUPPORT)
     void updateDashboardRegions();
 #endif
@@ -471,9 +467,7 @@ private:
     Timer<FrameView> m_deferredRepaintTimer;
     double m_deferredRepaintDelay;
     double m_lastPaintTime;
-
-    unsigned m_disableRepaints;
-
+    
     bool m_isTrackingRepaints; // Used for testing.
     Vector<IntRect> m_trackedRepaintRects;
 
--- third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
@@ -178,15 +178,10 @@ void SVGImage::drawSVGToImageBuffer(ImageBuffer* buffer, const IntSize& size, fl
     ImageObserver* observer = imageObserver();
     ASSERT(observer);
 
-    // Temporarily reset image observer, we don't want to receive any changeInRect() calls due to this relayout.
+    // Temporarily reset image observer, we don't want to receive any changeInRect() calls due this relayout.
     setImageObserver(0);
-
-    // Disable repainting; we don't want deferred repaints to schedule any timers due to this relayout.
-    frame->view()->beginDisableRepaints();
-
     renderer->setContainerSize(size);
     frame->view()->resize(this->size());
-
     if (zoom != 1)
         frame->setPageZoomFactor(zoom);
 
@@ -208,9 +203,7 @@ void SVGImage::drawSVGToImageBuffer(ImageBuffer* buffer, const IntSize& size, fl
     if (frame->view()->needsLayout())
         frame->view()->layout();
 
-    setImageObserver(observer);
-
-    frame->view()->endDisableRepaints();
+    setImageObserver(observer); 
 }
 
 void SVGImage::draw(GraphicsContext* context, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace, CompositeOperator compositeOp)
@@ -218,7 +211,7 @@ void SVGImage::draw(GraphicsContext* context, const FloatRect& dstRect, const Fl
     if (!m_page)
         return;
 
-    FrameView* view = frameView();
+    FrameView* view = m_page->mainFrame()->view();
 
     GraphicsContextStateSaver stateSaver(*context);
     context->setCompositeOperation(compositeOp);
@@ -261,14 +254,6 @@ RenderBox* SVGImage::embeddedContentBox() const
     if (!rootElement)
         return 0;
     return toRenderBox(rootElement->renderer());
-}
-
-FrameView* SVGImage::frameView() const
-{
-    if (!m_page)
-        return 0;
-
-    return m_page->mainFrame()->view();
 }
 
 bool SVGImage::hasRelativeWidth() const
