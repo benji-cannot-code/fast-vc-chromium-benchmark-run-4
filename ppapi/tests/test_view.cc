fd67a3464efaa75c2886c8d304e2c3a431819807
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-10 04:49:24 +0000

    Revert 125942.
    This is a really cool change, but it appears it wasn't tested
    well enough and the tree wasn't attended after landing it.
    It turns tests on several bots on several platforms (win, mac, chromeos)
    red. Some of the failing tests failed on the tryjobs for this CL, too.
    
    A list of failing tests:
    OutOfProcessPPAPITest.View_PageHideShow: 
    .\test\ui\ppapi_uitest.cc(913): error: Value of: observer.result().c_str()
      Actual: "PageHideShow FAIL: Didn't receive a hide event in timeout. NOTE: This test requires tab visibility to change and won't pass if you just run it in a browser. Normally the UI test should handle this. You can also run manually by waiting 2 secs, creating a new tab, waiting 2 more secs, and closing the new tab."
    Expected: "TestPageHideShow:Created"
    
    PPAPINaClTest.PostMessage_SendingArrayBuffer: 
    [2256:3984:0309/201351:810562:ERROR:external_registry_extension_loader_win.cc(84)] Missing value path for key Software\Google\Chrome\Extensions\jfmjfhklogoienhpfnppmbcbjfjnkonk.
    Killed (timed out).
    
    PPAPITest.NetAddressPrivate_DescribeIPv6: 
    Did not complete.
    
    See also http://crbug.com/117641
    
    - Switch PPAPITests to run in browser_tests instead of ui_tests. The former is sharded and so tests run in parallel. Before this they were taking 17 minutes on the trybot. Now they take about a minute.
    
    Another benefit is we can replace the sleeps with messages, so hopefully this makes the test faster/less flakier.
    
    BUG=115595
    Review URL: https://chromiumcodereview.appspot.com/9646027
    
    TBR=jam@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9664034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126008 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_view.cc
+++ ppapi/tests/test_view.cc
@@ -94,8 +94,9 @@ std::string TestView::TestPageHideShow() {
   ASSERT_FALSE(page_visibility_log_.empty());
   ASSERT_TRUE(page_visibility_log_[0]);
 
-  // Now that we're alive, tell the test knows it can change our visibility.
-  instance_->ReportProgress("TestPageHideShow:Created");
+  // Now that we're alive, set a cookie so the UI test knows it can change our
+  // visibility.
+  instance_->SetCookie("TestPageHideShow:Created", "TRUE");
 
   // Wait until we get a hide event, being careful to handle spurious
   // notifications of ViewChanged.
@@ -116,8 +117,8 @@ std::string TestView::TestPageHideShow() {
         "tab, waiting 2 more secs, and closing the new tab.";
   }
 
-  // Tell the test so it can show us again.
-  instance_->ReportProgress("TestPageHideShow:Hidden");
+  // Set a cookie so the UI test knows it can show us again.
+  instance_->SetCookie("TestPageHideShow:Hidden", "TRUE");
 
   // Wait until we get a show event.
   begin_time = pp::Module::Get()->core()->GetTime();
