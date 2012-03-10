043315c584e781d8527363496ce54c5994ddd05e
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-10 00:18:31 +0000

    Add symbolic link support to FileSystemFileUtil::AbstractFileEnumerator.
    BUG=114732
    TEST="existing tests"
    
    
    Review URL: http://codereview.chromium.org/9638002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125949 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util_posix.cc
+++ base/file_util_posix.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -775,8 +775,14 @@ void FileEnumerator::GetFindInfo(FindInfo* info) {
   info->filename.assign(cur_entry->filename.value());
 }
 
+// static
 bool FileEnumerator::IsDirectory(const FindInfo& info) {
   return S_ISDIR(info.stat.st_mode);
+}
+
+// static
+bool FileEnumerator::IsLink(const FindInfo& info) {
+  return S_ISLNK(info.stat.st_mode);
 }
 
 // static
