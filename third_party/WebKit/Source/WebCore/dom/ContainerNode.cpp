b0c3ddab3ea90e5a45272b3f6e4923c71953252c
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-10 17:08:02 +0000

    Unreviewed, rolling out r110358. http://trac.webkit.org/changeset/110358 https://bugs.webkit.org/show_bug.cgi?id=80706
    No improvement on specified benchmarks.
    
    * dom/ContainerNode.cpp:
    (WebCore::collectNodes):
    (WebCore::collectTargetNodes):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110378 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ChildListMutationScope.h"
 #include "ContainerNodeAlgorithms.h"
 #include "DeleteButtonController.h"
-#include "DocumentFragment.h"
 #include "EventNames.h"
 #include "ExceptionCode.h"
 #include "FloatRect.h"
@@ -63,9 +62,8 @@ static NodeCallbackQueue* s_postAttachCallbackQueue;
 static size_t s_attachDepth;
 static bool s_shouldReEnableMemoryCacheCallsAfterAttach;
 
-static inline void collectNodes(ContainerNode* node, NodeVector& nodes)
+static inline void collectNodes(Node* node, NodeVector& nodes)
 {
-    nodes.reserveCapacity(nodes.size() + node->childNodeCount());
     for (Node* child = node->firstChild(); child; child = child->nextSibling())
         nodes.append(child);
 }
@@ -76,7 +74,7 @@ static void collectTargetNodes(Node* node, NodeVector& nodes)
         nodes.append(node);
         return;
     }
-    collectNodes(static_cast<DocumentFragment*>(node), nodes);
+    collectNodes(node, nodes);
 }
 
 void ContainerNode::removeAllChildren()
