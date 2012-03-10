fb699ee73061f5c5ebd3dec0c89b88cdda3e2baa
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-10 00:36:29 +0000

    Add roundedPoint to HitTestResult and change platform code to use it https://bugs.webkit.org/show_bug.cgi?id=80715
    Reviewed by James Robinson.
    
    Change ports to use roundedPoint to avoid exposing subpixel types to
    platform code.
    
    Source/WebCore:
    
    No new tests.
    
    * rendering/HitTestResult.h:
    (WebCore::HitTestResult::roundedPoint):
    
    Source/WebKit/chromium:
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::mouseDidMoveOverElement):
    * src/ContextMenuClientImpl.cpp:
    (WebKit::ContextMenuClientImpl::getCustomMenuFromDefaultItems):
    
    Source/WebKit/mac:
    
    * WebCoreSupport/WebContextMenuClient.mm:
    (WebContextMenuClient::showContextMenu):
    
    Source/WebKit/qt:
    
    * Api/qwebframe.cpp:
    (QWebHitTestResultPrivate::QWebHitTestResultPrivate):
    
    Source/WebKit/win:
    
    * WebView.cpp:
    (WebView::handleContextMenuEvent):
    
    Source/WebKit2:
    
    * WebProcess/WebPage/WebContextMenu.cpp:
    (WebKit::WebContextMenu::show):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110348 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/ContextMenuClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/ContextMenuClientImpl.cpp
@@ -159,7 +159,7 @@ PlatformMenuDescription ContextMenuClientImpl::getCustomMenuFromDefaultItems(
     Frame* selectedFrame = r.innerNonSharedNode()->document()->frame();
 
     WebContextMenuData data;
-    data.mousePosition = selectedFrame->view()->contentsToWindow(r.point());
+    data.mousePosition = selectedFrame->view()->contentsToWindow(r.roundedPoint());
 
     // Compute edit flags.
     data.editFlags = WebContextMenuData::CanDoNone;
