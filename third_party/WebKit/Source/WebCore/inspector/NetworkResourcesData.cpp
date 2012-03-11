f01bf4762469737d0a8369728f2254d515d13b46
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-11 15:09:17 +0000

    Web Inspector: Network panel does not show responses for application/json data https://bugs.webkit.org/show_bug.cgi?id=80684
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Test: http/tests/inspector/network/async-xhr-json-mime-type.html
    
    * inspector/InspectorPageAgent.cpp:
    (WebCore::createXHRTextDecoder):
    (WebCore::InspectorPageAgent::cachedResourceContent):
    * inspector/InspectorPageAgent.h:
    * inspector/NetworkResourcesData.cpp:
    (WebCore::createOtherResourceTextDecoder):
    (WebCore):
    (WebCore::NetworkResourcesData::responseReceived):
    
    LayoutTests:
    
    * http/tests/inspector/network/async-xhr-json-mime-type-expected.txt: Added.
    * http/tests/inspector/network/async-xhr-json-mime-type.html: Added.
    * http/tests/inspector/network/resources/json.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/NetworkResourcesData.cpp
+++ third_party/WebKit/Source/WebCore/inspector/NetworkResourcesData.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "NetworkResourcesData.h"
 
+#include "DOMImplementation.h"
 #include "SharedBuffer.h"
 #include "TextResourceDecoder.h"
 
@@ -123,6 +124,21 @@ void NetworkResourcesData::resourceCreated(const String& requestId, const String
     m_requestIdToResourceDataMap.set(requestId, new ResourceData(requestId, loaderId));
 }
 
+static PassRefPtr<TextResourceDecoder> createOtherResourceTextDecoder(const String& mimeType, const String& textEncodingName)
+{
+    RefPtr<TextResourceDecoder> decoder;
+    if (!textEncodingName.isEmpty())
+        decoder = TextResourceDecoder::create("text/plain", textEncodingName);
+    else if (DOMImplementation::isXMLMIMEType(mimeType.lower())) {
+        decoder = TextResourceDecoder::create("application/xml");
+        decoder->useLenientXMLDecoding();
+    } else if (equalIgnoringCase(mimeType, "text/html"))
+        decoder = TextResourceDecoder::create("text/html", "UTF-8");
+    else if (mimeType == "text/plain")
+        decoder = TextResourceDecoder::create("text/plain", "ISO-8859-1");
+    return decoder;
+}
+
 void NetworkResourcesData::responseReceived(const String& requestId, const String& frameId, const ResourceResponse& response)
 {
     ResourceData* resourceData = m_requestIdToResourceDataMap.get(requestId);
@@ -130,7 +146,7 @@ void NetworkResourcesData::responseReceived(const String& requestId, const Strin
         return;
     resourceData->setFrameId(frameId);
     resourceData->setUrl(response.url());
-    resourceData->setDecoder(InspectorPageAgent::createDecoder(response.mimeType(), response.textEncodingName()));
+    resourceData->setDecoder(createOtherResourceTextDecoder(response.mimeType(), response.textEncodingName()));
 }
 
 void NetworkResourcesData::setResourceType(const String& requestId, InspectorPageAgent::ResourceType type)
