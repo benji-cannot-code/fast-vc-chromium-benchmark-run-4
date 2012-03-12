90ffc0abe87e6c65f9ebfdc67163f1e863bafe59
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 21:42:51 +0000

    Avoid using Pickle::WriteSize(), which writes an architecture-dependent amount of data, in clipboard pickles. (The goal is to remove that method entirely. Uses that never persist or send pickles over the network are [probably] safe, but having the method around is waiting for accidental misuses.)
    Review URL: http://codereview.chromium.org/9688021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126242 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/custom_data_helper_unittest.cc
+++ ui/base/clipboard/custom_data_helper_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -119,7 +119,7 @@ TEST(CustomDataHelperTest, BadReadTypes) {
   expected.push_back(ASCIIToUTF16("f"));
 
   Pickle malformed;
-  malformed.WriteSize(1000);
+  malformed.WriteUInt64(1000);
   malformed.WriteString16(ASCIIToUTF16("hello"));
   malformed.WriteString16(ASCIIToUTF16("world"));
   std::vector<string16> actual(expected);
@@ -127,7 +127,7 @@ TEST(CustomDataHelperTest, BadReadTypes) {
   EXPECT_EQ(expected, actual);
 
   Pickle malformed2;
-  malformed2.WriteSize(1);
+  malformed2.WriteUInt64(1);
   malformed2.WriteString16(ASCIIToUTF16("hello"));
   std::vector<string16> actual2(expected);
   ReadCustomDataTypes(malformed2.data(), malformed2.size(), &actual2);
@@ -139,7 +139,7 @@ TEST(CustomDataHelperTest, BadPickle) {
   std::map<string16, string16> result_map;
 
   Pickle malformed;
-  malformed.WriteSize(1000);
+  malformed.WriteUInt64(1000);
   malformed.WriteString16(ASCIIToUTF16("hello"));
   malformed.WriteString16(ASCIIToUTF16("world"));
 
@@ -152,7 +152,7 @@ TEST(CustomDataHelperTest, BadPickle) {
   EXPECT_EQ(0u, result_map.size());
 
   Pickle malformed2;
-  malformed2.WriteSize(1);
+  malformed2.WriteUInt64(1);
   malformed2.WriteString16(ASCIIToUTF16("hello"));
 
   ReadCustomDataForType(malformed2.data(),
