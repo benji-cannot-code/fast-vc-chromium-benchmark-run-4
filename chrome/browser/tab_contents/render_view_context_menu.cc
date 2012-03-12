aecc41be908d7b98fcda78002c0c0aa51dce84f5
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 22:51:33 +0000

    Added callback to DownloadUrl() so we can find download failures where the download item isn't created at all.
    Added observer class for browser tests, which uses the callback.
    
    BUG=117033
    TEST=None
    
    Review URL: http://codereview.chromium.org/9570005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126251 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -1526,7 +1526,8 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
                        false,  // Don't prefer_cache
                        -1,  // No POST id
                        save_info,
-                       source_web_contents_);
+                       source_web_contents_,
+                       DownloadManager::OnStartedCallback());
       break;
     }
 
@@ -1554,7 +1555,8 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
                        true,  // prefer_cache
                        post_id,
                        save_info,
-                       source_web_contents_);
+                       source_web_contents_,
+                       DownloadManager::OnStartedCallback());
       break;
     }
 
--- content/browser/tab_contents/tab_contents.cc
+++ content/browser/tab_contents/tab_contents.cc
@@ -2497,7 +2497,8 @@ void TabContents::SaveURL(const GURL& url,
                    true,  // prefer_cache
                    post_id,
                    save_info,
-                   this);
+                   this,
+                   DownloadManager::OnStartedCallback());
 }
 
 void TabContents::CreateViewAndSetSizeForRVH(RenderViewHost* rvh) {
