21cc5c56c8e87cce51edc104756f1f5e817d7be3
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 22:56:38 +0000

    Show a different icon in the launcher for incognito windows
    BUG=116932
    TEST=Open an incognito window and a normal window.  Check that the icons are different
    
    Review URL: https://chromiumcodereview.appspot.com/9649013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126256 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_model.cc
+++ ash/launcher/launcher_model.cc
@@ -11,8 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 LauncherModel::LauncherModel() : next_id_(1) {
-  Add(0, LauncherItem(TYPE_APP_LIST));
-  Add(1, LauncherItem(TYPE_BROWSER_SHORTCUT));
+  LauncherItem app_list;
+  app_list.type = TYPE_APP_LIST;
+  app_list.is_incognito = false;
+
+  LauncherItem browser_shortcut;
+  browser_shortcut.type = TYPE_BROWSER_SHORTCUT;
+  browser_shortcut.is_incognito = false;
+
+  Add(0, app_list);
+  Add(1, browser_shortcut);
 }
 
 LauncherModel::~LauncherModel() {
