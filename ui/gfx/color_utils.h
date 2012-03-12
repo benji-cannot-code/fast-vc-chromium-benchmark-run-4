80c77804351a6a83678f86d7a9032252506bed9a
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 22:57:35 +0000

    [Coverity] Fixed pass-by-value to pass-by-reference
    TBR=asvitkine@chromium.org
    CID=103400
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9662021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126258 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/color_utils.h
+++ ui/gfx/color_utils.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -59,7 +59,7 @@ SkColor GetAverageColorOfFavicon(SkBitmap* bitmap, SkAlpha alpha);
 
 // Builds a histogram based on the Y' of the Y'UV representation of
 // this image.
-UI_EXPORT void BuildLumaHistogram(SkBitmap* bitmap, int histogram[256]);
+UI_EXPORT void BuildLumaHistogram(const SkBitmap& bitmap, int histogram[256]);
 
 // Returns a blend of the supplied colors, ranging from |background| (for
 // |alpha| == 0) to |foreground| (for |alpha| == 255). The alpha channels of
