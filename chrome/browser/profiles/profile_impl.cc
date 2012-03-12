3fd7343a279ff94922c35c09535bbdd42a4b5377
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 21:15:41 +0000

    Remove ExtensionMessageService's legacy support for devtools. - Remove OpenSpecialChannelTo*. - Make EMS non-refcounted now that Profile is the sole owner. - Make MessagePorts hold a RenderProcessHost now that processes are the only types of port. I also changed it to hold a process instead of a RenderViewHost, which doesn't make a difference from an IPC messaging standpoint.
    BUG=104625
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/9667015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -445,7 +445,7 @@ void ProfileImpl::InitExtensions(bool extensions_enabled) {
   extension_info_map_ = new ExtensionInfoMap();
   extension_process_manager_.reset(ExtensionProcessManager::Create(this));
   extension_event_router_.reset(new ExtensionEventRouter(this));
-  extension_message_service_ = new ExtensionMessageService(this);
+  extension_message_service_.reset(new ExtensionMessageService(this));
   extension_navigation_observer_.reset(new ExtensionNavigationObserver(this));
 
   ExtensionErrorReporter::Init(true);  // allow noisy errors.
@@ -626,9 +626,6 @@ ProfileImpl::~ProfileImpl() {
   // FaviconService depends on HistoryServce so make sure we delete
   // HistoryService first.
   favicon_service_.reset();
-
-  if (extension_message_service_)
-    extension_message_service_->DestroyingProfile();
 
   if (pref_proxy_config_tracker_.get())
     pref_proxy_config_tracker_->DetachFromPrefService();
