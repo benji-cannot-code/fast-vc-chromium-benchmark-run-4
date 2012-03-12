13e428c5609d5a17296f7273c0bded73553997fa
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-12 01:29:44 +0000

    Unreviewed, rolling out r110359. http://trac.webkit.org/changeset/110359 https://bugs.webkit.org/show_bug.cgi?id=80799
    SelectPopupMenuTest.ClickItem failure (Requested by ukai on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-03-11
    
    Source/WebCore:
    
    * platform/chromium/PopupListBox.cpp:
    (WebCore::PopupListBox::paint):
    (WebCore::PopupListBox::paintRow):
    (WebCore::PopupListBox::getRowHeight):
    * platform/chromium/PopupListBox.h:
    (PopupContainerSettings):
    * platform/chromium/PopupMenuChromium.cpp:
    (WebCore):
    (WebCore::PopupMenuChromium::show):
    * platform/chromium/PopupMenuChromium.h:
    (PopupMenuChromium):
    * rendering/RenderMenuList.cpp:
    (WebCore::RenderMenuList::showPopup):
    
    Source/WebKit/chromium:
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::gestureEvent):
    (WebKit::WebViewImpl::applyAutofillSuggestions):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
@@ -32,17 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "PopupMenuChromium.h"
-
-#include "Frame.h"
-#include "FrameView.h"
-#include "Page.h"
 #include "PopupContainer.h"
-#include "Settings.h"
 
 namespace WebCore {
 
 int PopupMenuChromium::s_minimumRowHeight = 0;
-int PopupMenuChromium::s_optionPaddingForTouch = 30;
 
 // The settings used for the drop down menu.
 // This is the delegate used if none is provided.
@@ -69,14 +63,8 @@ PopupMenuChromium::~PopupMenuChromium()
 
 void PopupMenuChromium::show(const IntRect& r, FrameView* v, int index)
 {
-    if (!p.popup) {
-        PopupContainerSettings popupSettings = dropDownSettings;
-        popupSettings.defaultDeviceScaleFactor =
-            v->frame()->page()->settings()->defaultDeviceScaleFactor();
-        if (!popupSettings.defaultDeviceScaleFactor)
-            popupSettings.defaultDeviceScaleFactor = 1;
-        p.popup = PopupContainer::create(client(), PopupContainer::Select, popupSettings);
-    }
+    if (!p.popup)
+        p.popup = PopupContainer::create(client(), PopupContainer::Select, dropDownSettings);
     p.popup->showInRect(r, v, index);
 }
 
--- third_party/WebKit/Source/WebCore/rendering/RenderMenuList.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderMenuList.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderBR.h"
 #include "RenderScrollbar.h"
 #include "RenderTheme.h"
-#include "Settings.h"
 #include "TextRun.h"
 #include <math.h>
 
@@ -310,9 +309,6 @@ void RenderMenuList::showPopup()
     // the actual width of the element to size the popup.
     FloatPoint absTopLeft = localToAbsolute(FloatPoint(), false, true);
     LayoutRect absBounds = absoluteBoundingBoxRectIgnoringTransforms();
-    int scale = document()->page()->settings()->defaultDeviceScaleFactor();
-    if (scale && scale != 1)
-        absBounds.scale(scale);
     absBounds.setLocation(roundedLayoutPoint(absTopLeft));
     HTMLSelectElement* select = toHTMLSelectElement(node());
     m_popup->show(absBounds, document()->view(), select->optionToListIndex(select->selectedIndex()));
