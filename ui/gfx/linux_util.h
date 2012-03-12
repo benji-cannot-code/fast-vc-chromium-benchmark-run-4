ad1ba32970202d13e154020909fecbc97f972108
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 17:20:41 +0000

    ui/gfx: Move BGRAToRGBA() function to its unique caller (clipboard_gtk.cc).
    R=tony@chromium.org
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9662013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126161 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/linux_util.h
+++ ui/gfx/linux_util.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GFX_LINUX_UTIL_H_
 #pragma once
 
-#include <stdint.h>
-
 #include <string>
 
 #include "ui/base/ui_export.h"
@@ -27,11 +25,6 @@ UI_EXPORT std::string RemoveWindowsStyleAccelerators(const std::string& label);
 // ConvertAcceleratorsFromWindowsStyle() will return the original string (except
 // with "_" characters doubled, to escape them for GTK).
 UI_EXPORT std::string EscapeWindowsStyleAccelerators(const std::string& label);
-
-// Makes a copy of |pixels| with the ordering changed from BGRA to RGBA.
-// The caller is responsible for free()ing the data. If |stride| is 0, it's
-// assumed to be 4 * |width|.
-uint8_t* BGRAToRGBA(const uint8_t* pixels, int width, int height, int stride);
 
 }  // namespace gfx
 
