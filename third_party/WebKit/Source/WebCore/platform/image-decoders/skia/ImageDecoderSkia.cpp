6f15a0fa4243c7d4c513028f2716ec791fe0dc74
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-12 23:23:56 +0000

    [Skia] Release CGImage used to apply color space conversion to SkBitmap https://bugs.webkit.org/show_bug.cgi?id=80892
    Patch by Brian Salomon <bsalomon@google.com> on 2012-03-12
    Reviewed by Adam Barth.
    
    Regressions should be covered by any test that does image decode on the mac.
    
    * platform/image-decoders/skia/ImageDecoderSkia.cpp:
    (WebCore::resolveColorSpace):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/skia/ImageDecoderSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/skia/ImageDecoderSkia.cpp
@@ -120,7 +120,7 @@ static void resolveColorSpace(const SkBitmap& bitmap, CGColorSpaceRef colorSpace
 {
     int width = bitmap.width();
     int height = bitmap.height();
-    CGImageRef srcImage = SkCreateCGImageRefWithColorspace(bitmap, colorSpace);
+    RetainPtr<CGImageRef> srcImage(AdoptCF, SkCreateCGImageRefWithColorspace(bitmap, colorSpace));
     SkAutoLockPixels lock(bitmap);
     void* pixels = bitmap.getPixels();
     RetainPtr<CGContextRef> cgBitmap(AdoptCF, CGBitmapContextCreate(pixels, width, height, 8, width * 4, deviceRGBColorSpaceRef(), kCGBitmapByteOrder32Host | kCGImageAlphaPremultipliedFirst));
@@ -128,7 +128,7 @@ static void resolveColorSpace(const SkBitmap& bitmap, CGColorSpaceRef colorSpace
         return;
     CGContextSetBlendMode(cgBitmap.get(), kCGBlendModeCopy);
     CGRect bounds = { {0, 0}, {width, height} };
-    CGContextDrawImage(cgBitmap.get(), bounds, srcImage);
+    CGContextDrawImage(cgBitmap.get(), bounds, srcImage.get());
 }
 
 static CGColorSpaceRef createColorSpace(const ColorProfile& colorProfile)
