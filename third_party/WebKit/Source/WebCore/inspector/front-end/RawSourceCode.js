794728a9c4ccb673374afc367a3a0300709ba89e
podivilov@chromium.org <podivilov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-12 15:56:06 +0000

    Web Inspector: DebuggerModel.Location should have scriptId property. https://bugs.webkit.org/show_bug.cgi?id=80830
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    * inspector/front-end/CompilerSourceMapping.js:
    (WebInspector.ClosureCompilerSourceMapping.prototype.sourceLocationToCompiledLocation):
    * inspector/front-end/ConsoleMessage.js:
    (WebInspector.ConsoleMessageImpl.prototype.location):
    * inspector/front-end/DebuggerModel.js:
    (WebInspector.DebuggerModel.Location):
    (WebInspector.DebuggerModel.prototype._failedToParseScriptSource):
    (WebInspector.DebuggerModel.prototype.createRawLocation):
    (WebInspector.DebuggerModel.prototype.createRawLocationByURL):
    * inspector/front-end/DebuggerPresentationModel.js:
    (WebInspector.DebuggerPresentationModel.prototype._consoleMessageAdded):
    (WebInspector.DebuggerPresentationModel.prototype._addConsoleMessageToScript):
    (WebInspector.DebuggerPresentationModel.prototype._addPendingConsoleMessagesToScript):
    (WebInspector.DebuggerPresentationModelResourceBinding.prototype._uiSourceCodeForResource):
    (WebInspector.DebuggerPresentationModel.Linkifier.prototype.linkifyLocation):
    * inspector/front-end/RawSourceCode.js:
    (WebInspector.RawSourceCode.PlainSourceMapping.prototype.uiLocationToRawLocation):
    (WebInspector.RawSourceCode.FormattedSourceMapping.prototype.rawLocationToUILocation):
    (WebInspector.RawSourceCode.FormattedSourceMapping.prototype.uiLocationToRawLocation):
    (WebInspector.RawSourceCode.CompilerSourceMapping.prototype.uiLocationToRawLocation):
    * inspector/front-end/ScriptFormatter.js:
    (WebInspector.ScriptFormatter.locationToPosition):
    (WebInspector.ScriptFormatter.positionToLocation):
    (WebInspector.FormattedSourceMapping.prototype.originalToFormatted):
    (WebInspector.FormattedSourceMapping.prototype.formattedToOriginal):
    
    LayoutTests:
    
    * http/tests/inspector/compiler-source-mapping.html:
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    * http/tests/inspector/search/search-in-concatenated-script.html:
    * http/tests/inspector/search/search-in-script.html:
    * inspector/debugger/debugger-scripts.html:
    * inspector/debugger/linkifier.html:
    * inspector/debugger/raw-source-code.html:
    * inspector/debugger/script-formatter.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110434 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/RawSourceCode.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RawSourceCode.js
@@ -130,25 +130,6 @@ WebInspector.RawSourceCode.prototype = {
     },
 
     /**
-     * @param {number} lineNumber
-     * @param {number=} columnNumber
-     * @return {WebInspector.Script}
-     */
-    _scriptForRawLocation: function(lineNumber, columnNumber)
-    {
-        var closestScript = this._scripts[0];
-        for (var i = 1; i < this._scripts.length; ++i) {
-            var script = this._scripts[i];
-            if (script.lineOffset > lineNumber || (script.lineOffset === lineNumber && script.columnOffset > columnNumber))
-                continue;
-            if (script.lineOffset > closestScript.lineOffset ||
-                (script.lineOffset === closestScript.lineOffset && script.columnOffset > closestScript.columnOffset))
-                closestScript = script;
-        }
-        return closestScript;
-    },
-
-    /**
      * @param {WebInspector.Script} script
      */
     forceUpdateSourceMapping: function(script)
@@ -333,9 +314,7 @@ WebInspector.RawSourceCode.PlainSourceMapping.prototype = {
     uiLocationToRawLocation: function(uiSourceCode, lineNumber, columnNumber)
     {
         console.assert(uiSourceCode === this._uiSourceCodeList[0]);
-        var rawLocation = { lineNumber: lineNumber, columnNumber: columnNumber };
-        rawLocation.scriptId = this._rawSourceCode._scriptForRawLocation(rawLocation.lineNumber, rawLocation.columnNumber).scriptId;
-        return /** @type {DebuggerAgent.Location} */ rawLocation;
+        return WebInspector.debuggerModel.createRawLocation(this._rawSourceCode._scripts[0], lineNumber, columnNumber);
     },
 
     /**
@@ -367,8 +346,8 @@ WebInspector.RawSourceCode.FormattedSourceMapping.prototype = {
      */
     rawLocationToUILocation: function(rawLocation)
     {
-        var location = this._mapping.originalToFormatted(rawLocation);
-        return new WebInspector.UILocation(this._uiSourceCodeList[0], location.lineNumber, location.columnNumber || 0);
+        var location = this._mapping.originalToFormatted(rawLocation.lineNumber, rawLocation.columnNumber);
+        return new WebInspector.UILocation(this._uiSourceCodeList[0], location[0], location[1]);
     },
 
     /**
@@ -380,9 +359,8 @@ WebInspector.RawSourceCode.FormattedSourceMapping.prototype = {
     uiLocationToRawLocation: function(uiSourceCode, lineNumber, columnNumber)
     {
         console.assert(uiSourceCode === this._uiSourceCodeList[0]);
-        var rawLocation = this._mapping.formattedToOriginal(new WebInspector.DebuggerModel.Location(lineNumber, columnNumber));
-        rawLocation.scriptId = this._rawSourceCode._scriptForRawLocation(rawLocation.lineNumber, rawLocation.columnNumber).scriptId;
-        return rawLocation;
+        var location = this._mapping.formattedToOriginal(lineNumber, columnNumber);
+        return WebInspector.debuggerModel.createRawLocation(this._rawSourceCode._scripts[0], location[0], location[1]);
     },
 
     /**
@@ -430,9 +408,8 @@ WebInspector.RawSourceCode.CompilerSourceMapping.prototype = {
      */
     uiLocationToRawLocation: function(uiSourceCode, lineNumber, columnNumber)
     {
-        var rawLocation = this._mapping.sourceLocationToCompiledLocation(uiSourceCode.url, lineNumber);
-        rawLocation.scriptId = this._rawSourceCode._scriptForRawLocation(rawLocation.lineNumber, rawLocation.columnNumber).scriptId;
-        return rawLocation;
+        var location = this._mapping.sourceLocationToCompiledLocation(uiSourceCode.url, lineNumber);
+        return WebInspector.debuggerModel.createRawLocation(this._rawSourceCode._scripts[0], location[0], location[1]);
     },
 
     /**
