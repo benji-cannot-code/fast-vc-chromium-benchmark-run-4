dc05f698337fcc686b3b4ada06dfe53b0d5c76c0
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 23:12:17 +0000

    Revert 126256 - Show a different icon in the launcher for incognito windows
    BUG=116932
    TEST=Open an incognito window and a normal window.  Check that the icons are different
    
    Review URL: https://chromiumcodereview.appspot.com/9649013
    
    TBR=zork@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9692021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126260 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_types.cc
+++ ash/launcher/launcher_types.cc
@@ -10,7 +10,13 @@ namespace ash {
 LauncherItem::LauncherItem()
     : type(TYPE_TABBED),
       num_tabs(1),
-      is_incognito(false),
+      id(0),
+      status(STATUS_CLOSED) {
+}
+
+LauncherItem::LauncherItem(LauncherItemType type)
+    : type(type),
+      num_tabs(0),
       id(0),
       status(STATUS_CLOSED) {
 }
--- ash/launcher/tabbed_launcher_button.cc
+++ ash/launcher/tabbed_launcher_button.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "ash/launcher/launcher_button_host.h"
-#include "ash/launcher/launcher_types.h"
 #include "grit/ui_resources.h"
 #include "ui/base/accessibility/accessible_view_state.h"
 #include "ui/base/animation/multi_animation.h"
@@ -20,9 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace internal {
 
-TabbedLauncherButton::IconView::IconView(
-    TabbedLauncherButton* host,
-    TabbedLauncherButton::IncognitoState is_incognito)
+TabbedLauncherButton::IconView::IconView(TabbedLauncherButton* host)
     : host_(host),
       show_image_(true) {
   if (!browser_image_) {
@@ -30,19 +27,11 @@ TabbedLauncherButton::IconView::IconView(
 
     browser_image_ = new SkBitmap(
         *rb.GetImageNamed(IDR_AURA_LAUNCHER_BROWSER).ToSkBitmap());
-    incognito_browser_image_ = new SkBitmap(
-        *rb.GetImageNamed(IDR_AURA_LAUNCHER_INCOGNITO_BROWSER).ToSkBitmap());
     browser_panel_image_ = new SkBitmap(
         *rb.GetImageNamed(IDR_AURA_LAUNCHER_BROWSER_PANEL).ToSkBitmap());
-    incognito_browser_panel_image_ = new SkBitmap(
-        *rb.GetImageNamed(
-            IDR_AURA_LAUNCHER_INCOGNITO_BROWSER_PANEL).ToSkBitmap());
   }
   set_icon_size(0);
-  if (is_incognito == STATE_NOT_INCOGNITO)
-    LauncherButton::IconView::SetImage(*browser_image_);
-  else
-    LauncherButton::IconView::SetImage(*incognito_browser_image_);
+  LauncherButton::IconView::SetImage(*browser_image_);
 }
 
 TabbedLauncherButton::IconView::~IconView() {
@@ -104,25 +93,19 @@ void TabbedLauncherButton::IconView::OnPaint(gfx::Canvas* canvas) {
 
 // static
 SkBitmap* TabbedLauncherButton::IconView::browser_image_ = NULL;
-SkBitmap* TabbedLauncherButton::IconView::incognito_browser_image_ = NULL;
 SkBitmap* TabbedLauncherButton::IconView::browser_panel_image_ = NULL;
-SkBitmap* TabbedLauncherButton::IconView::incognito_browser_panel_image_ = NULL;
 
 TabbedLauncherButton* TabbedLauncherButton::Create(
     views::ButtonListener* listener,
-    LauncherButtonHost* host,
-    IncognitoState is_incognito) {
-  TabbedLauncherButton* button =
-      new TabbedLauncherButton(listener, host, is_incognito);
+    LauncherButtonHost* host) {
+  TabbedLauncherButton* button = new TabbedLauncherButton(listener, host);
   button->Init();
   return button;
 }
 
 TabbedLauncherButton::TabbedLauncherButton(views::ButtonListener* listener,
-                                           LauncherButtonHost* host,
-                                           IncognitoState is_incognito)
-    : LauncherButton(listener, host),
-      is_incognito_(is_incognito) {
+                                           LauncherButtonHost* host)
+    : LauncherButton(listener, host) {
   set_accessibility_focusable(true);
 }
 
@@ -143,7 +126,7 @@ void TabbedLauncherButton::GetAccessibleState(ui::AccessibleViewState* state) {
 }
 
 LauncherButton::IconView* TabbedLauncherButton::CreateIconView() {
-  return new IconView(this, is_incognito_);
+  return new IconView(this);
 }
 
 }  // namespace internal
