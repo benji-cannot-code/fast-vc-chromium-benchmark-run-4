9605baf460b51d5b6f22bf937fd5be7c6c837234
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-12 14:43:52 +0000

    Unreviewed prospective Qt/Mac build fix
    * runtime/JSGlobalData.cpp: use #USE(CF) instead of PLATFORM(MAC) to determine
    whether to include CoreFoundation headers, used for JIT configuration in JSGlobalData
    constructor.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.cpp
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RegExp.h"
 #endif
 
-#if PLATFORM(MAC)
+#if USE(CF)
 #include <CoreFoundation/CoreFoundation.h>
 #endif
 
