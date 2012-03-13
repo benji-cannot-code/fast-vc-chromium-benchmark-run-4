a2eaed4f3c8cb2fe19aaf325c814940f78bae0ff
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 00:10:41 +0000

    Implement PPB_NetworkMonitor_Private Pepper interface.
    The P2PSocketDispatcher::NetworkListObserver has been moved 
    to webkit_glue:: namespace in webkit/glue/network_list_observer.h . PPB_NetworkMonitor_Private_Impl uses P2PSocketDispatcher to get 
    notifications about network changes.
    
    BUG=114808
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=125761
    
    Review URL: http://codereview.chromium.org/9557006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126274 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/socket_dispatcher.cc
+++ content/renderer/p2p/socket_dispatcher.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/p2p/host_address_request.h"
 #include "content/renderer/p2p/socket_client.h"
 #include "content/renderer/render_view_impl.h"
+#include "webkit/glue/network_list_observer.h"
 
 namespace content {
 
@@ -51,7 +52,7 @@ P2PSocketDispatcher::P2PSocketDispatcher(RenderViewImpl* render_view)
       message_loop_(base::MessageLoopProxy::current()),
       network_notifications_started_(false),
       network_list_observers_(
-          new ObserverListThreadSafe<NetworkListObserver>()),
+          new ObserverListThreadSafe<webkit_glue::NetworkListObserver>()),
       async_message_sender_(new AsyncMessageSender(this)) {
 }
 
@@ -67,7 +68,7 @@ P2PSocketDispatcher::~P2PSocketDispatcher() {
 }
 
 void P2PSocketDispatcher::AddNetworkListObserver(
-    NetworkListObserver* network_list_observer) {
+    webkit_glue::NetworkListObserver* network_list_observer) {
   network_list_observers_->AddObserver(network_list_observer);
   network_notifications_started_ = true;
   async_message_sender_->Send(
@@ -75,7 +76,7 @@ void P2PSocketDispatcher::AddNetworkListObserver(
 }
 
 void P2PSocketDispatcher::RemoveNetworkListObserver(
-    NetworkListObserver* network_list_observer) {
+    webkit_glue::NetworkListObserver* network_list_observer) {
   network_list_observers_->RemoveObserver(network_list_observer);
 }
 
@@ -121,8 +122,8 @@ void P2PSocketDispatcher::UnregisterHostAddressRequest(int id) {
 
 void P2PSocketDispatcher::OnNetworkListChanged(
     const net::NetworkInterfaceList& networks) {
-  network_list_observers_->Notify(&NetworkListObserver::OnNetworkListChanged,
-                                  networks);
+  network_list_observers_->Notify(
+      &webkit_glue::NetworkListObserver::OnNetworkListChanged, networks);
 }
 
 void P2PSocketDispatcher::OnGetHostAddressResult(
---
+++ ppapi/tests/test_network_monitor_private.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PAPPI_TESTS_TEST_NETWORK_MONITOR_PRIVATE_H_
+#define PAPPI_TESTS_TEST_NETWORK_MONITOR_PRIVATE_H_
+
+#include <string>
+
+#include "ppapi/c/pp_stdint.h"
+#include "ppapi/tests/test_case.h"
+
+class TestNetworkMonitorPrivate : public TestCase {
+ public:
+  explicit TestNetworkMonitorPrivate(TestingInstance* instance);
+
+  // TestCase implementation.
+  virtual bool Init();
+  virtual void RunTests(const std::string& filter);
+
+ private:
+  std::string TestBasic();
+  std::string Test2Monitors();
+
+  std::string VerifyNetworkList(PP_Resource network_resource);
+};
+
+#endif  // PAPPI_TESTS_TEST_NETWORK_MONITOR_PRIVATE_H_
