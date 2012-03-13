7cabe8adfd55946f777d8262912f41a41df050b5
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 19:27:51 +0000

    SVG Animations update baseVal instead of animVal https://bugs.webkit.org/show_bug.cgi?id=12437
    Reviewed by Dirk Schulze.
    
    Blind fix for some GC related assertions firing on v8.
    
    Assure that animationStarted/animationEnded calls are happening
    on the same SVGAnimatedProperty. Always call animationEnded(),
    even if we shouldn't do anything for the target element, as its
    destructed, as we still have to reset m_isAnimating.
    
    * svg/SVGAnimateElement.cpp:
    (WebCore::SVGAnimateElement::SVGAnimateElement):
    (WebCore::SVGAnimateElement::resetToBaseValue):
    (WebCore::SVGAnimateElement::targetElementWillChange):
    * svg/SVGAnimateElement.h:
    (SVGAnimateElement):
    * svg/properties/SVGAnimatedPropertyTearOff.h:
    (WebCore::SVGAnimatedPropertyTearOff::animationEnded):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimateElement.h
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimateElement.h
@@ -41,6 +41,8 @@ enum AnimatedPropertyValueType {
     InheritValue
 };
 
+class SVGAnimatedProperty;
+
 class SVGAnimateElement : public SVGAnimationElement {
 public:
     static PassRefPtr<SVGAnimateElement> create(const QualifiedName&, Document*);
@@ -80,7 +82,8 @@ private:
     OwnPtr<SVGAnimatedType> m_fromType;
     OwnPtr<SVGAnimatedType> m_toType;
     OwnPtr<SVGAnimatedType> m_animatedType;
-    
+
+    SVGAnimatedProperty* m_animatedProperty;
     OwnPtr<SVGAnimatedTypeAnimator> m_animator;
 };
 
