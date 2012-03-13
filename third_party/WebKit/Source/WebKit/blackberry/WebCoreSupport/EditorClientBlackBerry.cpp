aed4bcbef270055b3457e24888ac9871fa3c00a7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 22:57:43 +0000

    [BlackBerry] Input focus state should unfocus the frame selection instead of the field. https://bugs.webkit.org/show_bug.cgi?id=81023
    Patch by Mike Fenton <mifenton@rim.com> on 2012-03-13
    Reviewed by Antonio Gomes.
    
    PR 137400.
    
    Prevent cursor drawing by unfocusing the frame selection
    when input mode is not ready.
    
    Greatly simplify the decision to focus input fields by removing
    all ties to the page load state and base it on input.  Input mode
    is disabled when a page load begins and enabled on any user input.
    
    The field is focused and ready for input immediately, only the
    frame selection (or cursor) is unfocused.
    
    Reviewed Internally by Gen Mak, Antonio Gomes and Nima Ghanavatian.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::setLoadState):
    (BlackBerry::WebKit::WebPagePrivate::handleMouseEvent):
    * Api/WebPageClient.h:
    * WebCoreSupport/EditorClientBlackBerry.cpp:
    (WebCore::EditorClientBlackBerry::shouldBeginEditing):
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::InputHandler):
    (BlackBerry::WebKit::InputHandler::setElementUnfocused):
    (BlackBerry::WebKit::InputHandler::enableInputMode):
    (BlackBerry::WebKit::InputHandler::setElementFocused):
    (BlackBerry::WebKit::InputHandler::ensureFocusTextElementVisible):
    (BlackBerry::WebKit::InputHandler::notifyClientOfKeyboardVisibilityChange):
    (BlackBerry::WebKit::InputHandler::handleKeyboardInput):
    (BlackBerry::WebKit::InputHandler::setComposingText):
    * WebKitSupport/InputHandler.h:
    (InputHandler):
    * WebKitSupport/TouchEventHandler.cpp:
    (BlackBerry::WebKit::TouchEventHandler::handleTouchPoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110630 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
@@ -160,7 +160,7 @@ bool EditorClientBlackBerry::shouldBeginEditing(Range* range)
     if (m_webPagePrivate->m_dumpRenderTree)
         return m_webPagePrivate->m_dumpRenderTree->shouldBeginEditingInDOMRange(range);
 
-    return m_webPagePrivate->m_inputHandler->shouldAcceptInputFocus();
+    return true;
 }
 
 bool EditorClientBlackBerry::shouldEndEditing(Range* range)
