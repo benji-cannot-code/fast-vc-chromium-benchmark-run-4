c2ef3b74f678f78bb749fc86f81ee1685c95b527
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 08:33:59 +0000

    Remove a unused parameter in the Linux FilePathWatcher implementation.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9553007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126359 0039d316-1c4b-4281-b951-d872f2087c98

--- base/files/file_path_watcher_linux.cc
+++ base/files/file_path_watcher_linux.cc
@@ -93,12 +93,10 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate,
   // Called for each event coming from the watch. |fired_watch| identifies the
   // watch that fired, |child| indicates what has changed, and is relative to
   // the currently watched path for |fired_watch|. The flag |created| is true if
-  // the object appears, and |is_directory| is set when the event refers to a
-  // directory.
+  // the object appears.
   void OnFilePathChanged(InotifyReader::Watch fired_watch,
                          const FilePath::StringType& child,
-                         bool created,
-                         bool is_directory);
+                         bool created);
 
   // Start watching |path| for changes and notify |delegate| on each change.
   // Returns true if watch for |path| has been added successfully.
@@ -292,8 +290,7 @@ void InotifyReader::OnInotifyEvent(const inotify_event* event) {
        ++watcher) {
     (*watcher)->OnFilePathChanged(event->wd,
                                   child,
-                                  event->mask & (IN_CREATE | IN_MOVED_TO),
-                                  event->mask & IN_ISDIR);
+                                  event->mask & (IN_CREATE | IN_MOVED_TO));
   }
 }
 
@@ -301,12 +298,9 @@ FilePathWatcherImpl::FilePathWatcherImpl()
     : delegate_(NULL) {
 }
 
-void FilePathWatcherImpl::OnFilePathChanged(
-    InotifyReader::Watch fired_watch,
-    const FilePath::StringType& child,
-    bool created,
-    bool is_directory) {
-
+void FilePathWatcherImpl::OnFilePathChanged(InotifyReader::Watch fired_watch,
+                                            const FilePath::StringType& child,
+                                            bool created) {
   if (!message_loop()->BelongsToCurrentThread()) {
     // Switch to message_loop_ to access watches_ safely.
     message_loop()->PostTask(FROM_HERE,
@@ -314,8 +308,7 @@ void FilePathWatcherImpl::OnFilePathChanged(
                    this,
                    fired_watch,
                    child,
-                   created,
-                   is_directory));
+                   created));
     return;
   }
 
@@ -340,9 +333,10 @@ void FilePathWatcherImpl::OnFilePathChanged(
 
       // Update watches if a directory component of the |target_| path
       // (dis)appears. Note that we don't add the additional restriction
-      // of checking is_directory here as changes to symlinks on the
-      // target path will not have is_directory set but as a result we
-      // may sometimes call UpdateWatches unnecessarily.
+      // of checking the event mask to see if it is for a directory here
+      // as changes to symlinks on the target path will not have
+      // IN_ISDIR set in the event masks. As a result we may sometimes
+      // call UpdateWatches() unnecessarily.
       if (change_on_target_path && !UpdateWatches()) {
         delegate_->OnFilePathError(target_);
         return;
@@ -364,7 +358,6 @@ void FilePathWatcherImpl::OnFilePathChanged(
       }
     }
   }
-
 }
 
 bool FilePathWatcherImpl::Watch(const FilePath& path,
