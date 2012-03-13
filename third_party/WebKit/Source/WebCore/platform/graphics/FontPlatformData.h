c7dfd502eb16ea5416a543039391827d99afa342
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 02:17:32 +0000

    [BlackBerry] Upstream the BlackBerry change to platform/graphics/FontPlatformData.h https://bugs.webkit.org/show_bug.cgi?id=80820
    Reviewed by Rob Buis.
    
    The BlackBerry is using the FontPlatformData from Chromium porting.
    
    * platform/graphics/FontPlatformData.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FontPlatformData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/FontPlatformData.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // FIXME: This is temporary until all ports switch to using this file.
-#if PLATFORM(CHROMIUM) && !OS(DARWIN)
+#if (PLATFORM(CHROMIUM) && !OS(DARWIN)) || PLATFORM(BLACKBERRY)
 #include "chromium/FontPlatformData.h"
 #elif PLATFORM(QT)
 #include "qt/FontPlatformData.h"
