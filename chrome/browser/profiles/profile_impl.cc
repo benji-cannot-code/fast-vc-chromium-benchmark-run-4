eac5b6995c503ea1bf34bd5791ecb6a844d8b615
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 23:21:18 +0000

    Revert 126388 - Adding README file to user profile directories.
    BUG=117028
    TEST=Manual: README file should be present under Default/ and every other profile directory.
    
    Review URL: https://chromiumcodereview.appspot.com/9618019
    
    TBR=ivankr@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9699003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/string_tokenizer.h"
 #include "base/string_util.h"
-#include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autocomplete/network_action_predictor.h"
@@ -96,13 +95,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/user_metrics.h"
-#include "grit/chromium_strings.h"
 #include "grit/locale_settings.h"
 #include "net/base/transport_security_state.h"
 #include "net/http/http_server_properties.h"
 #include "webkit/appcache/appcache_service.h"
 #include "webkit/database/database_tracker.h"
-#include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_WIN)
 #include "chrome/browser/instant/promo_counter.h"
@@ -158,13 +155,6 @@ COMPILE_ASSERT(sizeof(ProfileImpl) <= 720u, profile_impl_size_unexpected);
 // Delay, in milliseconds, before we explicitly create the SessionService.
 static const int kCreateSessionServiceDelayMS = 500;
 
-// Text content of README file with a single placeholder for the product name.
-// This is not localizable and hence not in resources.
-static const char kReadmeText[] =
-    "%1$s settings and storage represent user-selected preferences and "
-    "information and MUST not be extracted, overwritten or modified except "
-    "through %1$s defined APIs.";
-
 // Helper method needed because PostTask cannot currently take a Callback
 // function with non-void return type.
 // TODO(jhawkins): Remove once IgnoreResult is fixed.
@@ -178,18 +168,6 @@ FilePath GetCachePath(const FilePath& base) {
 
 FilePath GetMediaCachePath(const FilePath& base) {
   return base.Append(chrome::kMediaCacheDirname);
-}
-
-void EnsureReadmeFile(const FilePath& base) {
-  FilePath readme_path = base.Append(chrome::kReadmeFilename);
-  if (file_util::PathExists(readme_path))
-    return;
-  std::string readme_text = base::StringPrintf(
-      kReadmeText, l10n_util::GetStringUTF8(IDS_PRODUCT_NAME).c_str());
-  if (file_util::WriteFile(
-          readme_path, readme_text.data(), readme_text.size()) == -1) {
-    LOG(ERROR) << "Could not create README file.";
-  }
 }
 
 }  // namespace
@@ -218,9 +196,6 @@ Profile* Profile::CreateProfile(const FilePath& path,
 
   return new ProfileImpl(path, delegate, create_mode);
 }
-
-// static
-int ProfileImpl::create_readme_delay_ms = 60000;
 
 // static
 void ProfileImpl::RegisterUserPrefs(PrefService* prefs) {
@@ -417,12 +392,6 @@ void ProfileImpl::DoFinalInit(bool is_new_profile) {
   ChromePluginServiceFilter::GetInstance()->RegisterResourceContext(
       PluginPrefs::GetForProfile(this),
       io_data_.GetResourceContextNoInit());
-
-  // Delay README creation to not impact startup performance.
-  BrowserThread::PostDelayedTask(
-        BrowserThread::FILE, FROM_HERE,
-        base::Bind(&EnsureReadmeFile, GetPath()),
-        create_readme_delay_ms);
 
   // Creation has been finished.
   if (delegate_)
