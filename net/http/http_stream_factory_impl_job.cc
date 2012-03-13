3a9cd09e7fd847acd6b568f255fd73a10f3380ca
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 21:20:37 +0000

    Make pipelining resilient to proxy changes.
    This is a speculative fix for the crashes described in bug 117649. The symptom
    is a crash when we try to add pending requests to a pipeline. Analyzing the
    crash, it looks like |request_map_| is empty (meaning all pending requests have
    been deleted), but |http_pipelined_request_map_| is non-empty (meaning it has a
    stale pointer).
    
    The most likely cause is the same request is added to the map twice. This would
    happen if SetHttpPipeliningKey was called twice, which could happen if
    DoInitConnection is called twice, which could happen if the connection fails and
    ReconsiderProxyAfterError returns OK, which could happen if the proxy config had
    changed while the first connection was in flight.
    
    If this doesn't fix it, I've also added some extra checking to help nail it
    down.
    
    BUG=117649
    TEST=net_unittests
    
    
    Review URL: http://codereview.chromium.org/9689038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126473 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -1018,8 +1018,10 @@ void HttpStreamFactoryImpl::Job::ReturnToStateInitConnection(
     connection_->socket()->Disconnect();
   connection_->Reset();
 
-  if (request_)
+  if (request_) {
     request_->RemoveRequestFromSpdySessionRequestMap();
+    request_->RemoveRequestFromHttpPipeliningRequestMap();
+  }
 
   next_state_ = STATE_INIT_CONNECTION;
 }
@@ -1131,8 +1133,10 @@ int HttpStreamFactoryImpl::Job::ReconsiderProxyAfterError(int error) {
     if (connection_->socket())
       connection_->socket()->Disconnect();
     connection_->Reset();
-    if (request_)
+    if (request_) {
       request_->RemoveRequestFromSpdySessionRequestMap();
+      request_->RemoveRequestFromHttpPipeliningRequestMap();
+    }
     next_state_ = STATE_RESOLVE_PROXY_COMPLETE;
   } else {
     // If ReconsiderProxyAfterError() failed synchronously, it means
