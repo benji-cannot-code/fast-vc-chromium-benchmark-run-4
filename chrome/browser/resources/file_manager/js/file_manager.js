fd033a675c4b0d5366588a01c3ff0684400bbff2
serya@google.com <serya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 16:23:03 +0000

    Removing leading slash in tab title in File Manager.
    BUG=chromium-os:26885
    TEST=Manual test.
    
    Review URL: https://chromiumcodereview.appspot.com/9677040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126399 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3311,7 +3311,7 @@ FileManager.prototype = {
     this.checkFreeSpace_(this.getCurrentDirectory());
 
     // TODO(dgozman): title may be better than this.
-    this.document_.title = this.getCurrentDirectory();
+    this.document_.title = this.getCurrentDirectory().substr(1);
 
     var self = this;
 
