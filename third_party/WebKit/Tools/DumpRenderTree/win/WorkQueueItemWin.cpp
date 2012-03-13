a03c12ed781e14690a8cc081d11adbca152e61cd
jberlin@webkit.org <jberlin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 01:58:27 +0000

    More Windows build fixing.
    * DumpRenderTree/win/WorkQueueItemWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/win/WorkQueueItemWin.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/win/WorkQueueItemWin.cpp
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebKit/WebKitCOMAPI.h>
 #include <JavaScriptCore/JSStringRef.h>
 #include <JavaScriptCore/JSStringRefCF.h>
-#include <JavaScriptCore/RetainPtr.h>
+#include <wtf/RetainPtr.h>
 #include <wtf/Vector.h>
 #include <string>
 
