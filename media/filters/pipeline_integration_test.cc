58fa41bb8e881b98c378eada30f5fab78043021e
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 01:09:52 +0000

    Disable flaky PipelineIntegrationTests
    TBR=acolwell@chromium.org
    BUG=117921
    
    Review URL: https://chromiumcodereview.appspot.com/9691022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126288 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/pipeline_integration_test.cc
+++ media/filters/pipeline_integration_test.cc
@@ -144,7 +144,8 @@ TEST_F(PipelineIntegrationTest, EncryptedPlayback) {
   Stop();
 }
 
-TEST_F(PipelineIntegrationTest, SeekWhilePaused) {
+// TODO(acolwell): Fix flakiness http://crbug.com/117921
+TEST_F(PipelineIntegrationTest, DISABLED_SeekWhilePaused) {
   ASSERT_TRUE(Start(GetTestDataURL("bear-320x240.webm"), PIPELINE_OK));
 
   base::TimeDelta duration(pipeline_->GetMediaDuration());
@@ -167,7 +168,8 @@ TEST_F(PipelineIntegrationTest, SeekWhilePaused) {
   ASSERT_TRUE(WaitUntilOnEnded());
 }
 
-TEST_F(PipelineIntegrationTest, SeekWhilePlaying) {
+// TODO(acolwell): Fix flakiness http://crbug.com/117921
+TEST_F(PipelineIntegrationTest, DISABLED_SeekWhilePlaying) {
   ASSERT_TRUE(Start(GetTestDataURL("bear-320x240.webm"), PIPELINE_OK));
 
   base::TimeDelta duration(pipeline_->GetMediaDuration());
