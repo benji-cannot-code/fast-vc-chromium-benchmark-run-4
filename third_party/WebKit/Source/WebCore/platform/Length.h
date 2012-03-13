c6bd05ed47df9b8da742c0ac3fd73ebd0853960d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 02:45:38 +0000

    SVG should support transform-origin and relative values https://bugs.webkit.org/show_bug.cgi?id=79068
    Patch by Hans Muller <hmuller@adobe.com> on 2012-03-12
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    Added SVG support for the CSS 'transform-origin' property and for percentage
    values in the transform translate function.  The changes conform to
    http://dev.w3.org/csswg/css3-transforms/.
    
    Tests: svg/transforms/percent-transform-values.xhtml
           svg/transforms/transform-origin-css-property.xhtml
    
    * css/svg.css:
    (*):
    (html|* > svg):
    Default transform-origin for SVG elements is 0 0.
    
    * platform/Length.h:
    (WebCore::Length::calcFloatValue):
    Added a calcFloatValue overload whose max parameter (for percent lengths) is a float.
    The original version will be obsolete when the sub-pixel layout support is completed.
    
    * rendering/style/RenderStyle.cpp:
    * rendering/style/RenderStyle.h:
    (WebCore):
    (WebCore::requireTransformOrigin):
    Transforms that only include translations don't depend on the transform-origin.
    
    (WebCore::RenderStyle::applyTransform):
    SVG elements interpret non-percentage/keyword transform-origin values relative to their viewport,
    unlike HTML which interprets all transform-origin values relative to the element's origin.
    The new FloatRect applyTransform() function handles SVG semantics. Similarly, SVG elements interpret
    percentage/keyword transform-origin values relative to the origin of their objectBoundingBox(), HTML
    uses the borderBox.  All this per http://dev.w3.org/csswg/css3-transforms/.
    
    * svg/SVGStyledTransformableElement.cpp:
    (WebCore::SVGStyledTransformableElement::animatedLocalTransform):
    
    LayoutTests:
    
    Added tests for SVG CSS transform-origin property support and for
    CSS translations specified with percentages and applied to an SVG element.
    
    * svg/transforms/change-transform-origin-css-expected.xhtml: Added.
    * svg/transforms/change-transform-origin-css.xhtml: Added.
    * svg/transforms/change-transform-origin-presentation-attribute-expected.xhtml: Added.
    * svg/transforms/change-transform-origin-presentation-attribute.xhtml: Added.
    * svg/transforms/percent-transform-values-expected.txt: Added.
    * svg/transforms/percent-transform-values.xhtml: Added.
    * svg/transforms/transform-origin-css-property-expected.xhtml: Added.
    * svg/transforms/transform-origin-css-property.xhtml: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110532 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/Length.h
+++ third_party/WebKit/Source/WebCore/platform/Length.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Copyright (C) 1999 Lars Knoll (knoll@kde.org)
     Copyright (C) 2006, 2008 Apple Inc. All rights reserved.
     Copyright (C) 2011 Rik Cabanier (cabanier@adobe.com)
+    Copyright (C) 2011 Adobe Systems Incorporated. All rights reserved.
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -204,6 +205,7 @@ public:
         return 0;
     }
 
+    // FIXME: when subpixel layout is supported this copy of calcFloatValue() can be removed. See bug 71143.
     float calcFloatValue(int maxValue) const
     {
         switch (type()) {
@@ -221,6 +223,28 @@ public:
             case Undefined:
                 ASSERT_NOT_REACHED();
                 return 0;
+        }
+        ASSERT_NOT_REACHED();
+        return 0;
+    }
+
+    float calcFloatValue(float maxValue) const
+    {
+        switch (type()) {
+        case Fixed:
+            return getFloatValue();
+        case Percent:
+            return static_cast<float>(maxValue * percent() / 100.0f);
+        case Auto:
+            return static_cast<float>(maxValue);
+        case Calculated:
+            return nonNanCalculatedValue(maxValue);
+        case Relative:
+        case Intrinsic:
+        case MinIntrinsic:
+        case Undefined:
+            ASSERT_NOT_REACHED();
+            return 0;
         }
         ASSERT_NOT_REACHED();
         return 0;
