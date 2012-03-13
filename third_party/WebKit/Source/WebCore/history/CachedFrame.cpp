b885df829dd6dcaa1c0728bb9676c999216c0d9d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 16:08:50 +0000

    Not reviewed: chromium build fix.
    * history/CachedFrame.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/history/CachedFrame.cpp
+++ third_party/WebKit/Source/WebCore/history/CachedFrame.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CachedPage.h"
 
 #include "CachedFramePlatformData.h"
+#include "DOMWindow.h"
 #include "Document.h"
 #include "DocumentLoader.h"
 #include "ExceptionCode.h"
