00e7c19e2f9a070260f6eef2d1c1f5ff698be3cc
benchan@chromium.org <benchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 10:06:07 +0000

    Implement copy and move operations within the same remote file system.
    This CL makes the following changes:
    1. Implement copy and move operations in RemoteFileSystemOperation,
       DocumentsService, and GDataFileSystem. Copying of regular files and
       directories is yet to be implemented.
    2. Add corresponding unit tests in GDataFileSystemTest.
    
    BUG=chromium-os:26962
    TEST=Tested the following:
    1. Run "unit_tests --gtest_filter='*GData*'"
    2. Manually test copy, move, and rename operations in file manager.
    
    Review URL: http://codereview.chromium.org/9662041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126369 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/gdata/gdata_file_system_proxy.h"
 
+#include <vector>
+
 #include "base/bind.h"
 #include "base/platform_file.h"
 #include "base/string_util.h"
@@ -72,7 +74,6 @@ class GetFileInfoDelegate : public FindFileDelegateReplyBase {
   }
 
  private:
-
   // Relays reply back to the callback on calling thread.
   void Reply(base::PlatformFileError result,
              const base::PlatformFileInfo& file_info,
@@ -152,7 +153,6 @@ class ReadDirectoryDelegate : public FindFileDelegateReplyBase {
   }
 
  private:
-
   // Relays reply back to the callback on calling thread.
   void Reply(base::PlatformFileError result,
              const std::vector<base::FileUtilProxy::Entry>& file_list,
@@ -208,6 +208,33 @@ void GDataFileSystemProxy::GetFileInfo(const GURL& file_url,
       file_path, new GetFileInfoDelegate(file_system_, file_path, callback));
 }
 
+void GDataFileSystemProxy::Copy(const GURL& src_file_url,
+    const GURL& dest_file_url,
+    const FileSystemOperationInterface::StatusCallback& callback) {
+  FilePath src_file_path, dest_file_path;
+  if (!ValidateUrl(src_file_url, &src_file_path) ||
+      !ValidateUrl(dest_file_url, &dest_file_path)) {
+    MessageLoopProxy::current()->PostTask(FROM_HERE,
+         base::Bind(callback, base::PLATFORM_FILE_ERROR_NOT_FOUND));
+    return;
+  }
+
+  file_system_->Copy(src_file_path, dest_file_path, callback);
+}
+
+void GDataFileSystemProxy::Move(const GURL& src_file_url,
+    const GURL& dest_file_url,
+    const FileSystemOperationInterface::StatusCallback& callback) {
+  FilePath src_file_path, dest_file_path;
+  if (!ValidateUrl(src_file_url, &src_file_path) ||
+      !ValidateUrl(dest_file_url, &dest_file_path)) {
+    MessageLoopProxy::current()->PostTask(FROM_HERE,
+         base::Bind(callback, base::PLATFORM_FILE_ERROR_NOT_FOUND));
+    return;
+  }
+
+  file_system_->Move(src_file_path, dest_file_path, callback);
+}
 
 void GDataFileSystemProxy::ReadDirectory(const GURL& file_url,
     const FileSystemOperationInterface::ReadDirectoryCallback& callback) {
