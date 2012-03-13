39fe0bbf733076f3598e91db927f49e2b7c9882c
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-13 21:22:04 +0000

    Another attempt at fixing QtMinimal bot after r110595
    Blind fix, unreviewed.
    
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-03-13
    
    * WebCoreSupport/GeolocationClientQt.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/GeolocationClientQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/GeolocationClientQt.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GeolocationClientQt_h
 #define GeolocationClientQt_h
 
+#if ENABLE(GEOLOCATION)
+
 #include "GeolocationClient.h"
 
 #include <QObject>
@@ -76,5 +78,7 @@ private:
 };
 
 } // namespace WebCore
+
+#endif // ENABLE(GEOLOCATION)
 
 #endif // GeolocationClientQt_h
