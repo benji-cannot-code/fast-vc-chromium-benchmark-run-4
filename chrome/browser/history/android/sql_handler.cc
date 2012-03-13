1ad28af89a3fc4d2aebd5438710c803cfd30684d
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 20:11:15 +0000

    Interface to help AndroidProviderBackend manipulate data in different tables.
    This Cl includes
    a. The definition of interface.
    b. The interface implementation of bookmark model and favicon.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9608001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126450 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/history/android/sql_handler.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/history/android/sql_handler.h"
+
+namespace history {
+
+TableIDRow::TableIDRow()
+    : url_id(0) {
+}
+
+TableIDRow::~TableIDRow() {
+}
+
+SQLHandler::SQLHandler(const BookmarkRow::BookmarkColumnID columns[],
+                       int column_count)
+    : columns_(columns, columns + column_count) {
+}
+
+SQLHandler::~SQLHandler() {
+}
+
+bool SQLHandler::HasColumnIn(const BookmarkRow& row) {
+  for (std::set<BookmarkRow::BookmarkColumnID>::iterator i =
+           columns_.begin(); i != columns_.end(); ++i) {
+    if (row.is_value_set_explicitly(*i))
+      return true;
+  }
+  return false;
+}
+
+bool SQLHandler::HasColumn(BookmarkRow::BookmarkColumnID id) {
+  return columns_.find(id) != columns_.end();
+}
+
+}  // namespace history.
