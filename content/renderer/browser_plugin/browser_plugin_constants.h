6f5859f04d5be0e061e9414b80d21f0eedb5da57
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 01:54:13 +0000

    Implement BrowserPluginPlaceholder.
    A browser plugin is a plugin container that hosts an out-of-process RenderView (guest).
    
    Loading up a new process, creating a new RenderView, navigating to a given URL,
    and establishing a guest-to-host channel can take hundreds of milliseconds.
    
    Furthermore, a RenderView's associated browser-side WebContents, RenderViewHost, and
    SiteInstance must be created and accessed on the UI thread thread on the browser.
    
    Thus, we must avoid blocking the host RenderView as well to avoid introducing the potential
    for a deadlock.
    
    To address the two issues above, we use a BrowserPluginPlaceholder (currently an empty
    WebViewPlugin wrapper) to take place of the guest renderer until the guest renderer is
    ready.
    
    BUG=117894
    TEST=manually
    
    Review URL: http://codereview.chromium.org/9668031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126534 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/renderer/browser_plugin/browser_plugin_constants.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
+#define CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
+
+extern const char kBrowserPluginName[];
+extern const char kBrowserPluginMimeType[];
+extern const char kBrowserPluginPath[];
+extern const char kBrowserPluginDescription[];
+
+#endif  // CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
