534a0b953b3747435473ec3f96254296e2d3a758
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 21:32:09 +0000

    [Coverity] Fix pass-by-val to pass-by-ref
    CID=100255
    
    TBR=rickcam@chromium.org
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9705024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126742 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/gaia/oauth_request_signer.cc
+++ chrome/common/net/gaia/oauth_request_signer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,7 +79,7 @@ const std::string SignatureMethodName(
 
 std::string BuildBaseString(const GURL& request_base_url,
                             OAuthRequestSigner::HttpMethod http_method,
-                            const std::string base_parameters) {
+                            const std::string& base_parameters) {
   return StringPrintf("%s&%s&%s",
                       HttpMethodName(http_method).c_str(),
                       OAuthRequestSigner::Encode(
