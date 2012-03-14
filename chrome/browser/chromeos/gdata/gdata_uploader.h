2e8bf0afc4d458ceaf08917020b21869aff056a5
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 09:22:02 +0000

    Move downloaded files to GCache.
    * Add ResumeUploadResponse in gdata_params.h.
    * ResumeUploadCallback receives params ResumeUploadResponse.
    * util::ParseCreatedResponseContent parses the response xml for resourceId and md5Checksum.
    * Unit test for ParseCreatedResponseContent.
    * Upon HTTP_CREATED, GDataUploader now calls StoreToCache with the resourceId, md5Checksum and temporary file name.
    * Replace InitiateUploadOperationCallback and ResumeUploadOperationCallback with InitiateUploadCallback and ResumeUploadCallback as they are identical.
    
    BUG=chromium-os:23516
    TEST=Files should show up in /user/GCache/v1/blob after download.
    Review URL: https://chromiumcodereview.appspot.com/9692062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_uploader.h
+++ chrome/browser/chromeos/gdata/gdata_uploader.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/gdata/gdata_errorcode.h"
+#include "chrome/browser/chromeos/gdata/gdata_params.h"
 #include "googleurl/src/gurl.h"
 
 namespace gdata {
@@ -61,9 +62,7 @@ class GDataUploader {
 
   // DocumentsService callback for ResumeUpload.
   void OnResumeUploadResponseReceived(const GURL& file_url,
-                                      GDataErrorCode code,
-                                      int64 start_range_received,
-                                      int64 end_range_received);
+                                      const ResumeUploadResponse& response);
 
   // Private data.
 
--- chrome/browser/chromeos/gdata/gdata_util.cc
+++ chrome/browser/chromeos/gdata/gdata_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/file_path.h"
 #include "base/logging.h"
+#include "chrome/common/libxml_utils.h"
 #include "chrome/browser/download/download_util.h"
 #include "content/public/browser/download_item.h"
 #include "content/public/browser/download_manager.h"
@@ -70,6 +71,36 @@ FilePath GetGDataTempDownloadFolderPath() {
   return download_util::GetDefaultDownloadDirectory().Append(
       kGDataDownloadPath);
 }
+void ParseCreatedResponseContent(const std::string& response_content,
+    std::string* resource_id, std::string* md5_checksum) {
+  if (response_content.empty())
+    return;
+
+  XmlReader xml_reader;
+  bool ok = xml_reader.Load(response_content);
+  if (!ok) {
+    NOTREACHED() << "Invalid xml received " << response_content;
+    return;
+  }
+
+  // Read the 'entry' node, and then the first node under entry.
+  for (int i = 0; i < 2; ++i) {
+    ok = xml_reader.Read();
+    if (!ok) {
+      NOTREACHED() << "Read failed";
+      return;
+    }
+  }
+
+  // Look for nodes for resourceId and md5Checksum.
+  while (xml_reader.Next()) {
+    if (xml_reader.NodeName() == "resourceId")
+      xml_reader.ReadElementContent(resource_id);
+    if (xml_reader.NodeName() == "md5Checksum")
+      xml_reader.ReadElementContent(md5_checksum);
+  }
+}
+
 
 }  // namespace util
 }  // namespace gdata
