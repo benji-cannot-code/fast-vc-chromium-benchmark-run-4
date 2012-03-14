d178889d9e219ae9fe07457181712467a7700fa1
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 06:31:34 +0000

    Change AppNotificationManager::ProcessSyncChange method to be defensive in handling changes: - Never return a sync error. - Ignore UPDATE to non-existing items - Ignore UPDATE to existing item (check that new item is same as existing item) - Added unit tests for all code paths.
    Review URL: http://codereview.chromium.org/9666028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_notification.cc
+++ chrome/browser/extensions/app_notification.cc
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/app_notification.h"
 
+#include "base/json/json_writer.h"
 #include "base/string_number_conversions.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/common/guid.h"
@@ -48,7 +49,7 @@ AppNotification* AppNotification::Copy() {
   return copy;
 }
 
-void AppNotification::ToDictionaryValue(DictionaryValue* result) {
+void AppNotification::ToDictionaryValue(DictionaryValue* result) const {
   CHECK(result);
   result->SetBoolean(kIsLocalKey, is_local_);
   if (!creation_time_.is_null())
@@ -131,6 +132,14 @@ bool AppNotification::Equals(const AppNotification& other) const {
           body_ == other.body_ &&
           link_url_ == other.link_url_ &&
           link_text_ == other.link_text_);
+}
+
+std::string AppNotification::ToString() const {
+  DictionaryValue value;
+  ToDictionaryValue(&value);
+  std::string result;
+  base::JSONWriter::Write(&value, true, &result);
+  return result;
 }
 
 AppNotificationList* CopyAppNotificationList(
