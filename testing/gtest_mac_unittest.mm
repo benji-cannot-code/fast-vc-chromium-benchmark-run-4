4eb9f507eac8418401a8a80148173046b47f2b3c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 20:30:07 +0000

    mac: Fix a -Wobjc-redundant-literal-use warning
    BUG=none
    TEST=none
    TBR=rsesek
    
    Review URL: http://codereview.chromium.org/9706027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126722 0039d316-1c4b-4281-b951-d872f2087c98

--- testing/gtest_mac_unittest.mm
+++ testing/gtest_mac_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,7 +21,7 @@ TEST(GTestMac, ExpectNSEQ) {
   EXPECT_NSEQ(@"a", @"a");
 
   NSString* s1 = [NSString stringWithUTF8String:"a"];
-  NSString* s2 = [NSString stringWithString:@"a"];
+  NSString* s2 = @"a";
   EXPECT_NE(s1, s2);
   EXPECT_NSEQ(s1, s2);
 }
