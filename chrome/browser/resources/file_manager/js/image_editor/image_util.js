aca2968c66dc3851d0e96e609506187aeaf079f3
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 15:53:02 +0000

    Fix file name unescaping in ChromeOS Photo Editor
    Replaced unescape call with decodeURIComponent which works better for national alphabets.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9700015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126650 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_util.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_util.js
@@ -463,7 +463,7 @@ ImageUtil.removeChildren = function(element) {
 };
 
 ImageUtil.getFullNameFromUrl = function(url) {
-  url = unescape(url);
+  url = decodeURIComponent(url);
   if (url.indexOf('/') != -1)
     return url.substr(url.lastIndexOf('/') + 1);
   else
