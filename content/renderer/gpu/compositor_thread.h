1dd25640292042a0000ba90e8f7472329d36599a
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 08:32:39 +0000

    Remove dead code left over from old-style WebCompositor initialization path.
    Review URL: http://codereview.chromium.org/9633014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126612 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/compositor_thread.h
+++ content/renderer/gpu/compositor_thread.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,24 +33,24 @@ class CompositorThread {
   IPC::ChannelProxy::MessageFilter* GetMessageFilter() const;
 
   // Callable from the main thread or the compositor's thread.
-  void AddCompositor(int routing_id, int compositor_id);
+  void AddInputHandler(int routing_id, int input_handler_id);
 
   webkit_glue::WebThreadImpl* GetWebThread() { return &thread_; }
 
  private:
   // Callback only from the compositor's thread.
-  void RemoveCompositor(int routing_id);
+  void RemoveInputHandler(int routing_id);
 
   // Called from the compositor's thread.
   void HandleInputEvent(int routing_id,
                         const WebKit::WebInputEvent* input_event);
 
-  class CompositorWrapper;
-  friend class CompositorWrapper;
+  class InputHandlerWrapper;
+  friend class InputHandlerWrapper;
 
   typedef std::map<int,  // routing_id
-                   linked_ptr<CompositorWrapper> > CompositorMap;
-  CompositorMap compositors_;
+                   linked_ptr<InputHandlerWrapper> > InputHandlerMap;
+  InputHandlerMap input_handlers_;
 
   webkit_glue::WebThreadImpl thread_;
   scoped_refptr<InputEventFilter> filter_;
