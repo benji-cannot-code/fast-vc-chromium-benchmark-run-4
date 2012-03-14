cf16fb135a85acc87e011a0b5c447bdb97784c62
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 18:04:30 +0000

    change default capture resolution to 640x480 Review URL: https://chromiumcodereview.appspot.com/9701008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126680 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_impl.cc
+++ content/renderer/media/media_stream_impl.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kVideoCaptureWidth = 352;
-const int kVideoCaptureHeight = 288;
+const int kVideoCaptureWidth = 640;
+const int kVideoCaptureHeight = 480;
 const int kVideoCaptureFramePerSecond = 30;
 
 }  // namespace
