916e0429f97eba0280ede7f81e0fbfa64b7639e7
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 00:56:59 +0000

    Get rid of WebContentsViewCocoa include in chrome so that we can move its creation to content and use DEPS to ensure no more includes.
    We had this same flickering issue in balloon_view_host_mac.mm but we don't need to go to WebContentsViewCocoa. I tested this on 10.7, 10.6, and 10.5 with AdBlock and it doesn't have resizing issues. Matt had seen it resize repeatedly with AdBlock on 10.6. The original bug mentioned RSS but that doesn't use a popup view anymore.
    
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/9696052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126521 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/extension_popup_controller.mm
+++ chrome/browser/ui/cocoa/extensions/extension_popup_controller.mm
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/extensions/extension_view_mac.h"
 #import "chrome/browser/ui/cocoa/info_bubble_window.h"
 #include "chrome/common/chrome_notification_types.h"
-#include "content/browser/tab_contents/web_contents_view_mac.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_source.h"
@@ -348,14 +347,8 @@ class DevtoolsNotificationBridge : public content::NotificationObserver {
   if (NSIsEmptyRect(frame))
     return;
 
-  DCHECK([extensionView_ isKindOfClass:[WebContentsViewCocoa class]]);
-  WebContentsViewCocoa* hostView = (WebContentsViewCocoa*)extensionView_;
-
-  // WebContentsViewCocoa overrides setFrame but not setFrameSize.
-  // We need to defer the update back to the RenderWidgetHost so we don't
-  // get the flickering effect on 10.5 of http://crbug.com/31970
-  [hostView setFrameWithDeferredUpdate:frame];
-  [hostView setNeedsDisplay:YES];
+  [extensionView_ setFrame:frame];
+  [extensionView_ setNeedsDisplay:YES];
 }
 
 - (void)onViewDidShow {
--- content/browser/tab_contents/web_contents_view_mac.mm
+++ content/browser/tab_contents/web_contents_view_mac.mm
@@ -55,9 +55,6 @@ COMPILE_ASSERT_MATCHING_ENUM(DragOperationEvery);
 - (void)clearTabContentsView;
 - (void)closeTabAfterEvent;
 - (void)viewDidBecomeFirstResponder:(NSNotification*)notification;
-// Notify the RenderWidgetHost that the frame was updated so it can resize
-// its contents.
-- (void)renderWidgetHostWasResized;
 @end
 
 namespace web_contents_view_mac {
@@ -492,19 +489,6 @@ void WebContentsViewMac::CloseTab() {
             pasteboard:[NSPasteboard pasteboardWithName:NSDragPboard]
      dragOperationMask:operationMask]);
   [dragSource_ startDrag];
-}
-
-- (void)setFrameWithDeferredUpdate:(NSRect)frameRect {
-  [super setFrame:frameRect];
-  [self performSelector:@selector(renderWidgetHostWasResized)
-             withObject:nil
-             afterDelay:0];
-}
-
-- (void)renderWidgetHostWasResized {
-  TabContents* tabContents = [self tabContents];
-  if (tabContents && tabContents->GetRenderViewHost())
-    tabContents->GetRenderViewHost()->WasResized();
 }
 
 // NSDraggingSource methods
