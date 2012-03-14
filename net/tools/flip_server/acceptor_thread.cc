290812fe45d426ef6da2e69abc304bc23e4b78cf
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 21:31:35 +0000

    Fix a -Wstring-plus-int warning in (linux-only) flip_server.
    BUG=none
    TEST=none
    TBR=eroman
    
    Review URL: http://codereview.chromium.org/9705026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126739 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/acceptor_thread.cc
+++ net/tools/flip_server/acceptor_thread.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -90,7 +90,7 @@ void SMAcceptorThread::HandleConnection(int server_fd,
                     reinterpret_cast<char*>(&on), sizeof(on));
     if (rc < 0) {
       close(server_fd);
-      LOG(ERROR) << "setsockopt() failed fd=" + server_fd;
+      LOG(ERROR) << "setsockopt() failed fd=" << server_fd;
       return;
     }
   }
