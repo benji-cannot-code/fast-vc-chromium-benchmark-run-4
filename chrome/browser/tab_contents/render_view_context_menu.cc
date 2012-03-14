bbe62b9450b5956bc25e78ab022f585d22375f26
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 06:58:23 +0000

    Enable Mac speech support in the context menu to match other Cocoa apps.
    This matches Safari and what Mac users generally expect. I'll need to add the same to the Edit menu in a later CL.
    
    This also cleans up some style issues and moves the Mac dictionary context menu support to only be known to render_view_context_menu_mac.mm and not the cross-platform render_view_context_menu.cc.
    
    BUG=31107, 46153
    TEST=Right click on some selected text in the web content area. The context menu should contain a "Speech" submenu with items "Start Speaking" and "Stop Speaking". Try them out.
    
    Review URL: http://codereview.chromium.org/9702001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126602 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -673,11 +673,6 @@ void RenderViewContextMenu::AppendPopupExtensionItems() {
   AppendDeveloperItems();
 }
 
-void RenderViewContextMenu::LookUpInDictionary() {
-  // Used only in the Mac port.
-  NOTREACHED();
-}
-
 void RenderViewContextMenu::AddMenuItem(int command_id,
                                         const string16& title) {
   menu_model_.AddItem(command_id, title);
@@ -1330,10 +1325,6 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
           WebContextMenuData::CheckableMenuItemEnabled;
     case IDC_WRITING_DIRECTION_MENU:
       return true;
-    case IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY:
-      // This is OK because the menu is not shown when it isn't
-      // appropriate.
-      return true;
 #elif defined(OS_POSIX)
     // TODO(suzhe): this should not be enabled for password fields.
     case IDC_INPUT_METHODS_MENU:
@@ -1424,7 +1415,7 @@ bool RenderViewContextMenu::IsCommandIdChecked(int id) const {
 }
 
 void RenderViewContextMenu::ExecuteCommand(int id) {
-  return ExecuteCommand(id, 0);
+  ExecuteCommand(id, 0);
 }
 
 void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
@@ -1807,9 +1798,6 @@ void RenderViewContextMenu::ExecuteCommand(int id, int event_flags) {
       rvh->NotifyTextDirection();
       break;
     }
-    case IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY:
-      LookUpInDictionary();
-      break;
 #endif  // OS_MACOSX
     case IDC_CONTENT_CONTEXT_PROTOCOL_HANDLER_SETTINGS: {
       content::RecordAction(
--- chrome/browser/tab_contents/render_view_context_menu_mac.mm
+++ chrome/browser/tab_contents/render_view_context_menu_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,8 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/menu_controller.h"
 #include "grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 
 using content::WebContents;
+
+// These are not documented, so use only after checking -respondsToSelector:.
+@interface NSApplication (UndocumentedSpeechMethods)
+- (void)speakString:(NSString*)string;
+- (void)stopSpeaking:(id)sender;
+- (BOOL)isSpeaking;
+@end
 
 namespace {
 
@@ -54,6 +62,7 @@ RenderViewContextMenuMac::RenderViewContextMenuMac(
     const content::ContextMenuParams& params,
     NSView* parent_view)
     : RenderViewContextMenu(web_contents, params),
+      ALLOW_THIS_IN_INITIALIZER_LIST(speech_submenu_model_(this)),
       parent_view_(parent_view) {
 }
 
@@ -62,7 +71,7 @@ RenderViewContextMenuMac::~RenderViewContextMenuMac() {
 
 void RenderViewContextMenuMac::PlatformInit() {
   InitPlatformMenu();
-  menuController_.reset(
+  menu_controller_.reset(
       [[MenuController alloc] initWithModel:&menu_model_
                      useWithPopUpButtonCell:NO]);
 
@@ -94,7 +103,7 @@ void RenderViewContextMenuMac::PlatformInit() {
     base::mac::ScopedSendingEvent sendingEventScoper;
 
     // Show the menu.
-    [NSMenu popUpContextMenu:[menuController_ menu]
+    [NSMenu popUpContextMenu:[menu_controller_ menu]
                    withEvent:clickEvent
                      forView:parent_view_];
   }
@@ -108,7 +117,48 @@ void RenderViewContextMenuMac::ExecuteCommand(int id) {
       [BrowserWindowController browserWindowControllerForWindow:parent_window];
   [controller commitInstant];  // It's ok if controller is nil.
 
-  RenderViewContextMenu::ExecuteCommand(id);
+  ExecuteCommand(id, 0);
+}
+
+void RenderViewContextMenuMac::ExecuteCommand(int command_id, int event_flags) {
+  switch (command_id) {
+    case IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY:
+      LookUpInDictionary();
+      break;
+
+    case IDC_CONTENT_CONTEXT_SPEECH_START_SPEAKING:
+      StartSpeaking();
+      break;
+
+    case IDC_CONTENT_CONTEXT_SPEECH_STOP_SPEAKING:
+      StopSpeaking();
+      break;
+
+    default:
+      RenderViewContextMenu::ExecuteCommand(command_id, event_flags);
+      break;
+  }
+}
+
+bool RenderViewContextMenuMac::IsCommandIdEnabled(int command_id) const {
+  switch (command_id) {
+    case IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY:
+      // This is OK because the menu is not shown when it isn't
+      // appropriate.
+      return true;
+
+    case IDC_CONTENT_CONTEXT_SPEECH_START_SPEAKING:
+      // This is OK because the menu is not shown when it isn't
+      // appropriate.
+      return true;
+
+    case IDC_CONTENT_CONTEXT_SPEECH_STOP_SPEAKING:
+      return [NSApp respondsToSelector:@selector(isSpeaking)] &&
+             [NSApp isSpeaking];
+
+    default:
+      return RenderViewContextMenu::IsCommandIdEnabled(command_id);
+  }
 }
 
 bool RenderViewContextMenuMac::GetAcceleratorForCommandId(
@@ -121,12 +171,26 @@ void RenderViewContextMenuMac::InitPlatformMenu() {
   bool has_selection = !params_.selection_text.empty();
 
   if (has_selection) {
-      menu_model_.AddSeparator();
-      menu_model_.AddItemWithStringId(
-          IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY,
-          IDS_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY);
-  }
+    menu_model_.AddSeparator();
+    menu_model_.AddItemWithStringId(
+        IDC_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY,
+        IDS_CONTENT_CONTEXT_LOOK_UP_IN_DICTIONARY);
 
+    // Add speech items only if NSApp supports the private API we're using.
+    if ([NSApp respondsToSelector:@selector(speakString:)] &&
+        [NSApp respondsToSelector:@selector(stopSpeaking:)]) {
+      speech_submenu_model_.AddItemWithStringId(
+          IDC_CONTENT_CONTEXT_SPEECH_START_SPEAKING,
+          IDS_CONTENT_CONTEXT_SPEECH_START_SPEAKING);
+      speech_submenu_model_.AddItemWithStringId(
+          IDC_CONTENT_CONTEXT_SPEECH_STOP_SPEAKING,
+          IDS_CONTENT_CONTEXT_SPEECH_STOP_SPEAKING);
+      menu_model_.AddSubMenu(
+          IDC_CONTENT_CONTEXT_SPEECH_MENU,
+          l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_SPEECH_MENU),
+          &speech_submenu_model_);
+    }
+  }
 }
 
 void RenderViewContextMenuMac::LookUpInDictionary() {
@@ -144,11 +208,20 @@ void RenderViewContextMenuMac::LookUpInDictionary() {
     NSPerformService(@"Look Up in Dictionary", pboard);
 }
 
+void RenderViewContextMenuMac::StartSpeaking() {
+  NSString* text = base::SysUTF16ToNSString(params_.selection_text);
+  [NSApp speakString:text];
+}
+
+void RenderViewContextMenuMac::StopSpeaking() {
+  [NSApp stopSpeaking:menu_controller_];
+}
+
 void RenderViewContextMenuMac::UpdateMenuItem(int command_id,
                                               bool enabled,
                                               bool hidden,
                                               const string16& title) {
-  NSMenuItem* item = GetMenuItemByID(&menu_model_, [menuController_ menu],
+  NSMenuItem* item = GetMenuItemByID(&menu_model_, [menu_controller_ menu],
                                      command_id);
   if (!item)
     return;
