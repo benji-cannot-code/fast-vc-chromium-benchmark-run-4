14bd1b09f209647caf2abc0ce1c243f71e6834ea
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 19:54:21 +0000

    Revert 126700 because it broke clang on chromeos. ---
    ash uber tray: Add the detailed network popup
    
    BUG=110130, 109480
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9664056
    
    TBR=sadrul@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9702035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126706 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/network/tray_network.h
+++ ash/system/network/tray_network.h
@@ -16,7 +16,6 @@ namespace internal {
 
 namespace tray {
 class NetworkDefaultView;
-class NetworkDetailedView;
 class NetworkTrayView;
 }
 
@@ -40,7 +39,6 @@ class TrayNetwork : public SystemTrayItem,
 
   scoped_ptr<tray::NetworkTrayView> tray_;
   scoped_ptr<tray::NetworkDefaultView> default_;
-  scoped_ptr<tray::NetworkDetailedView> detailed_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayNetwork);
 };
