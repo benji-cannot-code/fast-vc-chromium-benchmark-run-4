69f4cc8d96539bb4c29ac8fc58c3f737050ea618
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-14 21:27:48 +0000

    [chromium] Fix accelerated Canvas2D with threaded compositing. https://bugs.webkit.org/show_bug.cgi?id=80998
    Reviewed by James Robinson.
    
    Source/WebCore: 
    
    Covered by unit tests Canvas2DLayerChromiumTest and
    TreeSynchronizerTest.
    
    * platform/graphics/chromium/Canvas2DLayerChromium.cpp:
    (WebCore::Canvas2DLayerChromium::paintContentsIfDirty):
    (WebCore):
    Delay creation of the front texture for double-buffering until the
    first call to paintContentsIfDirty().
    * platform/graphics/chromium/Canvas2DLayerChromium.h:
    (Canvas2DLayerChromium):
    Remove setTextureManager() and setLayerTreeHost(), since their job
    has been subsumed by paintContentsIfDirty().
    * platform/graphics/chromium/TreeSynchronizer.cpp:
    (WebCore::TreeSynchronizer::updateScrollbarLayerPointersRecursive):
    Perform an early-out if the passed-in layer is NULL.
    
    Source/WebKit/chromium: 
    
    * tests/Canvas2DLayerChromiumTest.cpp:
    Instantiate a CCLayerTreeHost, so that Canvas2DLayerChromium can
    retrieve its texture manager.  Also move the test into the unnamed
    namespace -- since it's already using WebCore, there's little reason for
    it to also be in the namespace.
    * tests/TreeSynchronizerTest.cpp:
    (WebKitTests):
    (WebKitTests::TEST):
    Add a new test for trying to synchronize NULL tree.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/Canvas2DLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/Canvas2DLayerChromium.h
@@ -58,7 +58,6 @@ public:
     virtual bool drawsContent() const;
     virtual void paintContentsIfDirty(const Region& occludedScreenSpace);
 
-    virtual void setLayerTreeHost(CCLayerTreeHost*);
     virtual void updateCompositorResources(GraphicsContext3D*, CCTextureUpdater&);
     virtual void pushPropertiesTo(CCLayerImpl*);
     virtual void unreserveContentsTexture();
@@ -69,7 +68,6 @@ private:
     Canvas2DLayerChromium(PassRefPtr<GraphicsContext3D>, const IntSize&);
 
     friend class Canvas2DLayerChromiumTest;
-    void setTextureManager(TextureManager*);
 
     RefPtr<GraphicsContext3D> m_context;
     bool m_contextLost;
