e24123863693bd1ee6a5d02922320f28b8a89ec6
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 19:58:22 +0000

    [Mac] Add the OS version check functions for 10.8.
    BUG=none
    TEST=Careful review by Mark. And a unittest.
    
    
    Review URL: http://codereview.chromium.org/9706001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126709 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_mac.mm
+++ base/process_util_mac.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -719,6 +719,7 @@ void oom_killer_new() {
 // === Core Foundation CFAllocators ===
 
 bool CanGetContextForCFAllocator() {
+  // TODO(rsesek|avi): See mac_util.h:153 for removing/updating this.
   return !base::mac::IsOSLaterThanLion();
 }
 
