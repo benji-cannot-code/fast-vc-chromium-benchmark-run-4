902ecaff4329d41d465286317cdc3710ec20f988
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 03:19:03 +0000

    Added read-only file error test.
    Depends on CLs 9568003 and 9570005.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/9355050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126541 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_file_util.h
+++ base/test/test_file_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/compiler_specific.h"
+#include "base/file_path.h"
 
 class FilePath;
 
@@ -56,6 +57,20 @@ FilePath WStringAsFilePath(const std::wstring& path);
 // In POSIX, this does not apply to the root user.
 bool MakeFileUnreadable(const FilePath& path) WARN_UNUSED_RESULT;
 bool MakeFileUnwritable(const FilePath& path) WARN_UNUSED_RESULT;
+
+// Saves the current permissions for a path, and restores it on destruction.
+class PermissionRestorer {
+ public:
+  explicit PermissionRestorer(const FilePath& path);
+  ~PermissionRestorer();
+
+ private:
+  const FilePath path_;
+  void* info_;  // The opaque stored permission information.
+  size_t length_;  // The length of the stored permission information.
+
+  DISALLOW_COPY_AND_ASSIGN(PermissionRestorer);
+};
 
 }  // namespace file_util
 
