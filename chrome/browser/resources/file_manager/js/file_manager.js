d7443243d4b6b4183d3eaf5c953b78a043f55882
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 20:05:09 +0000

    Updating pinning/unpinning code to be consistent with current API.
    API still not implemented.
    
    BUG=None
    TEST=Check there is no exception on attempt to pin a file.
    
    
    Review URL: http://codereview.chromium.org/9696029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3095,10 +3095,8 @@ FileManager.prototype = {
       checkbox.checked = entry.gdata_.isPinned = props[0].isPinned;
     }
 
-    if (checkbox.checked) {
-      chrome.fileBrowserPrivate.pinGDataFile([entry.toURL()], callback);
-      console.log('Pinning file ', entry.toURL());
-    }
+    chrome.fileBrowserPrivate.pinGDataFile([entry.toURL()],
+                                           !checkbox.checked, callback);
     event.preventDefault();
   };
 
