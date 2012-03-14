92f43e43934cee5caa6900b63c3aa3ac3fa10d4c
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 03:39:48 +0000

    Revert 126539 - Ash: Allow resize along 1 pixel edge inside window content
    Mocks call for resize handles to function along a single pixel edge inside the window, overlapping the web content.  Use the aura::Window::set_hit_test_bounds_inset() functionality to make hover/click events along that border pass through to the non-client area of the window frames.  This also allows windows to be resized when they are flush against the top/left/right edges of the screen.
    
    BUG=117542
    TEST=aura_shell_unittests ShelfLayoutManager, manually resize window from left/right/bottom/top edges
    
    
    Review URL: http://codereview.chromium.org/9694012
    
    TBR=jamescook@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9705011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/native_tab_contents_view_aura.cc
+++ chrome/browser/ui/views/tab_contents/native_tab_contents_view_aura.cc
@@ -164,7 +164,6 @@ void NativeTabContentsViewAura::InitNativeTabContentsView() {
   params.parent = NULL;
   params.can_activate = true;
   GetWidget()->Init(params);
-  GetNativeWindow()->SetName("NativeTabContentsViewAura");
 #if defined(USE_ASH)
   ash::SetChildWindowVisibilityChangesAnimated(GetWidget()->GetNativeView());
 #else
