b33e0664fb62192ad0a732c917ad4e1556d1a368
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-14 19:50:28 +0000

    background-visibility:hidden should create a RenderLayer https://bugs.webkit.org/show_bug.cgi?id=81134
    Source/WebCore:
    
    Reviewed by Dean Jackson.
    
    Background-visibility:hidden has to create RenderLayers, because it's implemented
    via compositing and/or paint short-circuiting in RenderLayer code.
    
    Test: transforms/3d/general/background-visibility-layers.html
    
    * rendering/RenderBox.h:
    * rendering/RenderBoxModelObject.h:
    (WebCore::RenderBoxModelObject::requiresLayer):
    * rendering/RenderObject.h:
    (WebCore::RenderObject::hasHiddenBackface):
    * rendering/RenderTableRow.h:
    
    LayoutTests:
    
    Reviewed by Dean Jackson.
    
    Test for background-visibility: hidden making a RenderLayer.
    
    * transforms/3d/general/background-visibility-layers-actual.png: Added.
    * transforms/3d/general/background-visibility-layers-actual.txt: Added.
    * transforms/3d/general/background-visibility-layers.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.h
@@ -43,7 +43,7 @@ public:
     RenderBox(Node*);
     virtual ~RenderBox();
 
-    virtual bool requiresLayer() const OVERRIDE { return isRoot() || isPositioned() || isRelPositioned() || isTransparent() || requiresLayerForOverflowClip() || hasTransform() || hasMask() || hasReflection() || hasFilter() || style()->specifiesColumns(); }
+    virtual bool requiresLayer() const OVERRIDE { return isRoot() || isPositioned() || isRelPositioned() || isTransparent() || requiresLayerForOverflowClip() || hasTransform() || hasHiddenBackface() || hasMask() || hasReflection() || hasFilter() || style()->specifiesColumns(); }
     bool requiresLayerForOverflowClip() const;
 
     bool hasOverflowClipWithLayer() const { return hasOverflowClip() && hasLayer(); }
