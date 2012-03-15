b8e75becef7be7aaae1a9b755f4d215603611548
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 21:05:42 +0000

    PrintPreview: Modify PrintMsg_Print_Params data validation check.
    BUG=117323
    TEST=None
    
    Review URL: http://codereview.chromium.org/9677034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126991 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/mock_printer.h
+++ chrome/renderer/mock_printer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -72,6 +72,8 @@ class MockPrinter {
   void ResetPrinter();
   void SetDefaultPrintSettings(const PrintMsg_Print_Params& params);
   void UseInvalidSettings();
+  void UseInvalidPageSize();
+  void UseInvalidContentSize();
 
   // Functions that handles IPC events.
   void GetDefaultPrintSettings(PrintMsg_Print_Params* params);
