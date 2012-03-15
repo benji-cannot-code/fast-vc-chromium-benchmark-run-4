59d43e92f18d04439016f1c491f9eea11b135238
danhn@chromium.org <danhn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 19:24:39 +0000

    Ported NTP4 UI tests to WebUI.
    This CL moves UI tests from the patch off issue 9420040 to WebUI testing framework.
    
    
    TEST=browser_tests --gtest_filter=NTP4*WebUITest*
         ui_tests --gtest_filter=NewTabUI*
    
    
    Review URL: http://codereview.chromium.org/9625020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126965 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/webui/ntp4_browsertest.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/data/webui/ntp4_browsertest.h"
+
+#include "chrome/browser/prefs/pref_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/common/pref_names.h"
+
+NTP4LoggedInWebUITest::NTP4LoggedInWebUITest() {}
+
+NTP4LoggedInWebUITest::~NTP4LoggedInWebUITest() {}
+
+void NTP4LoggedInWebUITest::SetLoginName(const std::string& name) {
+  browser()->profile()->GetPrefs()->SetString(prefs::kGoogleServicesUsername,
+                                              name);
+}
