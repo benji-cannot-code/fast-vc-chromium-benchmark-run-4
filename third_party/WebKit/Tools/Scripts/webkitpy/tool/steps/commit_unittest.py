9c838aa0558a1bab1789097e43b78ac00a12f97b
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-15 22:48:57 +0000

    The commit-queue can hang when the test_expectations.txt style check fails during commit https://bugs.webkit.org/show_bug.cgi?id=81251
    Reviewed by Eric Seidel.
    
    We forgot to check the --non-interactive flag, so we end up prompting
    the "user" who doesn't exist on the bot.  We might consider teaching
    the User class about non-interactive if we have this sort of bug again.
    
    * Scripts/webkitpy/tool/steps/commit.py:
    (Commit.options):
    (Commit._check_test_expectations):
    (Commit.run):
    * Scripts/webkitpy/tool/steps/commit_unittest.py:
    (CommitTest.test_check_test_expectations):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110900 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/commit_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/commit_unittest.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from webkitpy.common.system.outputcapture import OutputCapture
+from webkitpy.common.system.executive import ScriptError
 from webkitpy.common.system.executive_mock import MockExecutive
 from webkitpy.tool.mocktool import MockOptions, MockTool
 from webkitpy.tool.steps.commit import Commit
@@ -40,8 +41,10 @@ class CommitTest(unittest.TestCase):
         capture = OutputCapture()
         options = MockOptions()
         options.git_commit = ""
+        options.non_interactive = True
 
         tool = MockTool()
+        tool.user = None  # Will cause any access of tool.user to raise an exception.
         step = Commit(tool, options)
         state = {
             "changed_files": ["test_expectations.txtXXX"],
@@ -56,4 +59,4 @@ class CommitTest(unittest.TestCase):
         capture.assert_outputs(self, step.run, [state], expected_stderr="MOCK run_and_throw_if_fail: ['mock-check-webkit-style', '--diff-files', 'platform/chromium/test_expectations.txt'], cwd=/mock-checkout\nCommitted r49824: <http://trac.webkit.org/changeset/49824>\n")
 
         tool.executive = MockExecutive(should_log=True, should_throw_when_run=set(["platform/chromium/test_expectations.txt"]))
-        self.assertRaises(SystemExit, capture.assert_outputs, self, step.run, [state])
+        self.assertRaises(ScriptError, capture.assert_outputs, self, step.run, [state])
