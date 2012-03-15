6c56ffbf4d6423bcf27372b95aca6ca2f1f3e7ee
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 07:34:00 +0000

    Printing: Handle more error conditions and remove more cruft.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9703013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -136,8 +136,7 @@ void PrintViewManager::PrintPreviewDone() {
 void PrintViewManager::PreviewPrintingRequestCancelled() {
   if (!web_contents())
     return;
-  RenderViewHost* rvh = web_contents()->GetRenderViewHost();
-  rvh->Send(new PrintMsg_PreviewPrintingRequestCancelled(rvh->GetRoutingID()));
+  Send(new PrintMsg_PreviewPrintingRequestCancelled(routing_id()));
 }
 
 void PrintViewManager::set_observer(PrintViewManagerObserver* observer) {
@@ -487,10 +486,9 @@ void PrintViewManager::DisconnectFromCurrentPrintJob() {
 }
 
 void PrintViewManager::PrintingDone(bool success) {
-  if (!print_job_.get() || !web_contents())
+  if (!print_job_.get())
     return;
-  RenderViewHost* rvh = web_contents()->GetRenderViewHost();
-  rvh->Send(new PrintMsg_PrintingDone(rvh->GetRoutingID(), success));
+  Send(new PrintMsg_PrintingDone(routing_id(), success));
 }
 
 void PrintViewManager::TerminatePrintJob(bool cancel) {
@@ -594,8 +592,10 @@ bool PrintViewManager::OpportunisticallyCreatePrintJob(int cookie) {
 
 bool PrintViewManager::PrintNowInternal(IPC::Message* message) {
   // Don't print / print preview interstitials.
-  if (web_contents()->ShowingInterstitialPage())
+  if (web_contents()->ShowingInterstitialPage()) {
+    delete message;
     return false;
+  }
   return Send(message);
 }
 
