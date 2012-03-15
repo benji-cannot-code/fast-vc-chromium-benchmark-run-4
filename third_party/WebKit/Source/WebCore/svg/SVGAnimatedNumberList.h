1a51c59221f962b577d472720788afd150514f5f
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-15 15:27:21 +0000

    Enable animVal support for SVGNumberList https://bugs.webkit.org/show_bug.cgi?id=81219
    Reviewed by Rob Buis.
    
    Source/WebCore:
    
    Enable animVal support for SVGNumberList. Easy patch, we just need to add the missing
    constructFromCopy() method to SVGAnumatedNumberListAnimator and report AnimatedNumberList
    as supporting animVal.
    
    Covered by existing tests.
    
    * svg/SVGAnimatedNumberList.cpp:
    (WebCore::SVGAnimatedNumberListAnimator::constructFromCopy):
    (WebCore):
    * svg/SVGAnimatedNumberList.h:
    (WebCore):
    (SVGAnimatedNumberListAnimator):
    * svg/SVGAnimatedType.cpp:
    (WebCore::SVGAnimatedType::supportsAnimVal):
    (WebCore::SVGAnimatedType::setVariantValue):
    
    LayoutTests:
    
    Extend SVGNumberList tests to cover animVal/baseVal at the same time.
    
    * platform/chromium/test_expectations.txt:
    * svg/animations/script-tests/svgnumberlist-animation-1.js:
    (sample1):
    (sample2):
    (sample3):
    (sample4):
    (executeTest):
    * svg/animations/script-tests/svgnumberlist-animation-2.js:
    (sample1):
    (sample2):
    (sample3):
    (sample4):
    (executeTest):
    * svg/animations/svgnumberlist-animation-1-expected.txt:
    * svg/animations/svgnumberlist-animation-2-expected.txt:
    * svg/filters/feColorMatrix-invalid-animation-expected.txt: We no longer mutate the DOM, thus the dump is now correct.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimatedNumberList.h
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimatedNumberList.h
@@ -38,6 +38,7 @@ DECLARE_ANIMATED_LIST_PROPERTY(SVGAnimatedNumberList, SVGNumberList, UpperProper
 DEFINE_ANIMATED_PROPERTY(AnimatedNumberList, OwnerType, DOMAttribute, DOMAttribute.localName(), UpperProperty, LowerProperty)
 
 class SVGAnimationElement;
+class SVGGenericAnimatedType;
 
 class SVGAnimatedNumberListAnimator : public SVGAnimatedTypeAnimator {
     
@@ -46,6 +47,7 @@ public:
     virtual ~SVGAnimatedNumberListAnimator() { }
     
     virtual PassOwnPtr<SVGAnimatedType> constructFromString(const String&);
+    virtual PassOwnPtr<SVGAnimatedType> constructFromCopy(SVGGenericAnimatedType*);
     
     virtual void calculateFromAndToValues(OwnPtr<SVGAnimatedType>& fromValue, OwnPtr<SVGAnimatedType>& toValue, const String& fromString, const String& toString);
     virtual void calculateFromAndByValues(OwnPtr<SVGAnimatedType>& fromValue, OwnPtr<SVGAnimatedType>& toValue, const String& fromString, const String& byString);
