e3672098e6e25508b5d3298995770d6c0060212e
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 15:54:35 +0000

    Fix exception on loading File Manager.
    File manager updated commands availablility before roots loaded. Handling code exxpects a nonempty list that cause an exception.
    
    BUG=None.
    TEST=Check DevTools log.
    
    
    Review URL: http://codereview.chromium.org/9705058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1153,8 +1153,8 @@ FileManager.prototype = {
         var entry =
             this.getRootEntry_(this.rootsList_.selectionModel.selectedIndex);
 
-        return DirectoryModel.getRootType(entry.fullPath) ==
-               DirectoryModel.RootType.REMOVABLE;
+        return entry && DirectoryModel.getRootType(entry.fullPath) ==
+                        DirectoryModel.RootType.REMOVABLE;
     }
   };
 
