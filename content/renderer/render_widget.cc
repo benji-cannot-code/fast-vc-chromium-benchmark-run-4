4efaf9a73d19cbe5bd1c5ca48fbf64ef337f8e6e
caseq@google.com <caseq@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 15:57:51 +0000

    Notify DevTools when updating frame in RenderWidget
    Add a call to WebWidget::instrumentBeginFrame() in
    RenderWiddget::doDeferredUpdate() so DevTools can mark
    frames on timeline.
    
    Related issue upstream:
    https://bugs.webkit.org/show_bug.cgi?id=80127
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9592006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126921 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -830,6 +830,7 @@ void RenderWidget::DoDeferredUpdate() {
 
   // Tracking of frame rate jitter
   base::TimeTicks frame_begin_ticks = base::TimeTicks::Now();
+  webwidget_->instrumentBeginFrame();
   AnimateIfNeeded();
 
   // Layout may generate more invalidation.  It may also enable the
@@ -847,6 +848,7 @@ void RenderWidget::DoDeferredUpdate() {
   // animations running layout as these may generate further invalidations.
   if (!paint_aggregator_.HasPendingUpdate()) {
     TRACE_EVENT0("renderer", "EarlyOut_NoPendingUpdate");
+    webwidget_->instrumentCancelFrame();
     return;
   }
 
