b8afdf45cdfc5e03090f353d6fe470dba7fa3b80
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-15 20:06:11 +0000

    ARMv7: prefer vmov(gpr,gpr->double) over vmov(gpr->single) https://bugs.webkit.org/show_bug.cgi?id=81256
    Reviewed by Oliver Hunt.
    
    This is a 0.5% sunspider progression.
    
    * assembler/MacroAssemblerARMv7.h:
    (JSC::MacroAssemblerARMv7::convertInt32ToDouble):
        - switch which form of vmov we use.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h
@@ -908,7 +908,7 @@ public:
 
     void convertInt32ToDouble(RegisterID src, FPRegisterID dest)
     {
-        m_assembler.vmov(fpTempRegisterAsSingle(), src);
+        m_assembler.vmov(fpTempRegister, src, src);
         m_assembler.vcvt_signedToFloatingPoint(dest, fpTempRegisterAsSingle());
     }
 
@@ -916,7 +916,7 @@ public:
     {
         // Fixme: load directly into the fpr!
         load32(address, dataTempRegister);
-        m_assembler.vmov(fpTempRegisterAsSingle(), dataTempRegister);
+        m_assembler.vmov(fpTempRegister, dataTempRegister, dataTempRegister);
         m_assembler.vcvt_signedToFloatingPoint(dest, fpTempRegisterAsSingle());
     }
 
@@ -924,7 +924,7 @@ public:
     {
         // Fixme: load directly into the fpr!
         load32(address.m_ptr, dataTempRegister);
-        m_assembler.vmov(fpTempRegisterAsSingle(), dataTempRegister);
+        m_assembler.vmov(fpTempRegister, dataTempRegister, dataTempRegister);
         m_assembler.vcvt_signedToFloatingPoint(dest, fpTempRegisterAsSingle());
     }
     
