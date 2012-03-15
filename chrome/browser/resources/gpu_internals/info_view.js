96bfe22a085ca64217c0a2d95ed0782c75be45fe
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 16:41:25 +0000

    Display crash report IDs in about:gpu
    BUG=113089
    TEST=about:gpu page
    Review URL: https://chromiumcodereview.appspot.com/9706037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gpu_internals/info_view.js
+++ chrome/browser/resources/gpu_internals/info_view.js
@@ -28,6 +28,8 @@ cr.define('gpu', function() {
                                      this.refresh.bind(this));
       browserBridge.addEventListener('clientInfoChange',
                                      this.refresh.bind(this));
+      browserBridge.addEventListener('crashListChange',
+                                     this.refresh.bind(this));
       this.refresh();
     },
 
@@ -184,6 +186,10 @@ cr.define('gpu', function() {
         featureStatusList.textContent = '';
         problemsDiv.hidden = true;
       }
+
+      // Crash list
+      jstProcess(new JsEvalContext({values: browserBridge.crashList}),
+                 document.getElementById('crash-list'));
 
       // Log messages
       jstProcess(new JsEvalContext({values: browserBridge.logMessages}),
