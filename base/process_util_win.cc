1f5a881f9ae02ebe3d8a8702b2c0c6cedd6c0a37
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 12:40:04 +0000

    Add comments and fix a potential leak in LaunchProcess.
    I found a need to look at the implementation to confirm that process_handle would never be assigned to if the call failed. So I added a comment for future readers.
    
    While doing so, I found an edge case where I think the process handle could be leaked.
    
    BUG=None
    TEST=Ask a random developer whether they must check process_handle if LaunchProcess returns false. If they look at process_util_*.cc, fail.
    
    
    Review URL: http://codereview.chromium.org/9689081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126890 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_win.cc
+++ base/process_util_win.cc
@@ -350,6 +350,8 @@ bool LaunchProcess(const string16& cmdline,
                                       process_info.hProcess)) {
       DLOG(ERROR) << "Could not AssignProcessToObject.";
       KillProcess(process_info.hProcess, kProcessKilledExitCode, true);
+      CloseHandle(process_info.hProcess);
+      CloseHandle(process_info.hThread);
       return false;
     }
 
