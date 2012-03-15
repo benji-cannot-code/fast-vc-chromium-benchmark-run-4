cc4c5eab34524ac0d8737e9d2b496dd405839ebb
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 20:48:31 +0000

    Panel overflow indicator for GTK.
    Enable overflow indicator tests for Linux.
    
    Changed some overflow indicator tests to resize (instead of activate)
    a panel to bump another panel to overflow. This avoids the problem
    of Compiz refusing to activate a window that was created as inactive.
    
    BUG=113537
    TEST=existing panel overflow tests
    
    Review URL: https://chromiumcodereview.appspot.com/9653012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_overflow_indicator_gtk.h
+++ chrome/browser/ui/panels/panel_overflow_indicator_gtk.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/panels/panel_overflow_indicator.h"
 
+#include <gtk/gtk.h>
+
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "ui/base/gtk/gtk_signal.h"
 
 class PanelOverflowIndicatorGtk : public PanelOverflowIndicator {
  public:
@@ -28,6 +31,15 @@ class PanelOverflowIndicatorGtk : public PanelOverflowIndicator {
   virtual bool IsDrawingAttention() const OVERRIDE;
 
  private:
+  CHROMEGTK_CALLBACK_1(PanelOverflowIndicatorGtk, gboolean, OnExpose,
+                       GdkEventExpose*);
+
+  int count_;
+  bool is_drawing_attention_;
+  GtkWidget* window_;
+  GtkWidget* title_;
+  gfx::Rect bounds_;
+
   DISALLOW_COPY_AND_ASSIGN(PanelOverflowIndicatorGtk);
 };
 
