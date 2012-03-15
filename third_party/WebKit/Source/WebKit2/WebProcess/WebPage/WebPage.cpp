8a54a5205e7ff226c33ba02a1c35769e2188333c
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-15 21:33:44 +0000

    REGRESSION(r107168?): Assertion failures under pageContainsAnyHorizontalScrollbars causing multiple "crashes" on the Lion Intel Debug WebKit2 testers https://bugs.webkit.org/show_bug.cgi?id=81162 <rdar://problem/11050423>
    Reviewed by Beth Dakin and Jessie Berlin.
    
    Downgrade the ASSERT to a simple if check since scrollableArea->isOnActivePage() can return false when layout happens during page transitions.
    
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::pageContainsAnyHorizontalScrollbars):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@110888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -3070,7 +3070,8 @@ static bool pageContainsAnyHorizontalScrollbars(Frame* mainFrame)
 
         for (HashSet<ScrollableArea*>::const_iterator it = scrollableAreas->begin(), end = scrollableAreas->end(); it != end; ++it) {
             ScrollableArea* scrollableArea = *it;
-            ASSERT(scrollableArea->isOnActivePage());
+            if (!scrollableArea->isOnActivePage())
+                continue;
 
             if (hasEnabledHorizontalScrollbar(scrollableArea))
                 return true;
