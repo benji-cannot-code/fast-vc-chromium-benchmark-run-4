3fabf29e7fe5376c511c2db32b060903fce65d62
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 10:34:38 +0000

    valgrind.sh vs gdb: keep the registers in gdb in sync with the code.
    This might come at a performance price but I don't think this is important for valgrind.sh
    (please note the tests on the bots are started using a different runner)
    
    BUG=113513
    TEST=http://code.google.com/p/chromium/issues/detail?id=113513#c8
    
    Review URL: http://codereview.chromium.org/9666056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126883 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/bin/bash
 
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -26,6 +26,8 @@ setup_memcheck() {
 
   # Prompt to attach gdb when there was an error detected.
   DEFAULT_TOOL_FLAGS=("--db-command=$GDB -nw %f %p" "--db-attach=yes" \
+                      # Keep the registers in gdb in sync with the code.
+                      "--vex-iropt-precise-memory-exns=yes" \
                       # Overwrite newly allocated or freed objects
                       # with 0x41 to catch inproper use.
                       "--malloc-fill=41" "--free-fill=41" \
