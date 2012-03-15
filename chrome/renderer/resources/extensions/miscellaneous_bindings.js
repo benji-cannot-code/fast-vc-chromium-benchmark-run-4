4e6dd90b2b795f35c889b1094d647a2f304f8f61
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 10:43:22 +0000

    Make sure ports are closed when they're no longer used.
    It appears to be a common pattern that extensions don't invoke the
    responseCallback, and so the port objects leak. Register a callback that
    triggers when the responseCallback is finalized to free up these resources.
    
    BUG=114738, 93566
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9693048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126884 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/miscellaneous_bindings.js
+++ chrome/renderer/resources/extensions/miscellaneous_bindings.js
@@ -16,6 +16,7 @@ var chrome = chrome || {};
   native function PostMessage(portId, msg);
   native function GetChromeHidden();
   native function Print();
+  native function BindToGC();
 
   var chromeHidden = GetChromeHidden();
   var manifestVersion;
@@ -124,7 +125,7 @@ var chrome = chrome || {};
       if (requestEvent.hasListeners()) {
         var port = chromeHidden.Port.createPort(portId, channelName);
         port.onMessage.addListener(function(request) {
-          requestEvent.dispatch(request, sender, function(response) {
+          var responseCallback = function(response) {
             if (port) {
               port.postMessage(response);
               port = null;
@@ -142,7 +143,18 @@ var chrome = chrome || {};
               chrome.extension.lastError = {"message": errorMsg};
               console.error("Could not send response: " + errorMsg);
             }
+          };
+          // In case the extension never invokes the responseCallback, and also
+          // doesn't keep a reference to it, we need to clean up the port. Do
+          // so by attaching to the garbage collection of the responseCallback
+          // using some native hackery.
+          BindToGC(responseCallback, function() {
+            if (port) {
+              port.disconnect();
+              port = null;
+            }
           });
+          requestEvent.dispatch(request, sender, responseCallback);
         });
       }
       return;
