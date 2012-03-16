60349450f34cd45335e39cacdfb30ca042aad95b
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-16 13:42:18 +0000

    Merge setVisibleContentsForScaling with setVisibleContentsRectForPanning https://bugs.webkit.org/show_bug.cgi?id=81346
    Reviewed by Simon Hausmann.
    
    This cleans up the code path and is one step on the way to avoid
    calling both methods in succession, for instance after ending pinch
    zoom.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewLegacyPrivate::updateViewportSize):
    (QQuickWebViewFlickablePrivate::_q_commitScaleChange):
    (QQuickWebViewPrivate::_q_commitPositionChange):
    * UIProcess/DrawingAreaProxy.h:
    (WebKit::DrawingAreaProxy::setVisibleContentsRect):
    * UIProcess/DrawingAreaProxyImpl.cpp:
    (WebKit::DrawingAreaProxyImpl::setVisibleContentsRect):
    * UIProcess/DrawingAreaProxyImpl.h:
    (DrawingAreaProxyImpl):
    * UIProcess/LayerTreeHostProxy.cpp:
    (WebKit::LayerTreeHostProxy::setVisibleContentsRect):
    * UIProcess/LayerTreeHostProxy.h:
    (LayerTreeHostProxy):
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::setVisibleContentsRect):
    * UIProcess/WebLayerTreeRenderer.h:
    (WebLayerTreeRenderer):
    * WebProcess/WebPage/LayerTreeHost.h:
    (WebKit::LayerTreeHost::setVisibleContentsRect):
    * WebProcess/WebPage/LayerTreeHost.messages.in:
    * WebProcess/WebPage/qt/LayerTreeHostQt.cpp:
    (WebKit::LayerTreeHostQt::setVisibleContentsRect):
    * WebProcess/WebPage/qt/LayerTreeHostQt.h:
    (LayerTreeHostQt):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -507,7 +507,7 @@ void QQuickWebViewLegacyPrivate::updateViewportSize()
     // The fixed layout is handled by the FrameView and the drawing area doesn't behave differently
     // whether its fixed or not. We still need to tell the drawing area which part of it
     // has to be rendered on tiles, and in desktop mode it's all of it.
-    webPageProxy->drawingArea()->setVisibleContentsRectForScaling(IntRect(IntPoint(), viewportSize), 1);
+    webPageProxy->drawingArea()->setVisibleContentsRect(IntRect(IntPoint(), viewportSize), 1, FloatPoint());
 }
 
 QQuickWebViewFlickablePrivate::QQuickWebViewFlickablePrivate(QQuickWebView* viewport)
@@ -658,7 +658,7 @@ void QQuickWebViewFlickablePrivate::_q_commitScaleChange()
     const QRect visibleRect(visibleContentsRect());
     float scale = pageView->contentsScale();
 
-    drawingArea->setVisibleContentsRectForScaling(visibleRect, scale);
+    drawingArea->setVisibleContentsRect(visibleRect, scale, FloatPoint());
     webPageProxy->setFixedVisibleContentRect(visibleRect);
 }
 
@@ -669,11 +669,9 @@ void QQuickWebViewPrivate::_q_commitPositionChange(const QPointF& trajectoryVect
         return;
 
     const QRect visibleRect(visibleContentsRect());
-    drawingArea->setVisibleContentsRectForPanning(visibleRect, trajectoryVector);
+    float scale = pageView->contentsScale();
 
-    if (!trajectoryVector.isNull())
-        return;
-
+    drawingArea->setVisibleContentsRect(visibleRect, scale, trajectoryVector);
     webPageProxy->setFixedVisibleContentRect(visibleRect);
 }
 
