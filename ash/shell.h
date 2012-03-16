56848d75ef97d9e136474e3ba74aed9770d8f42f
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 22:43:23 +0000

    Revert 127268 - Ash: Add resize border shadow effect
    * Add ResizeShadow that uses solid-color layers to draw the resize border shadow effect.
    * Add ResizeShadowController to create ResizeShadows and track bounds changes for windows.
    * Wire both into ToplevelWindowEventFilter to track mouse move/enter events near (but not inside) the window frame.
    
    BUG=118325
    TEST=visual, hover mouse near window edges, drag windows
    
    Review URL: https://chromiumcodereview.appspot.com/9677070
    
    TBR=jamescook@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9724005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127277 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -63,7 +63,6 @@ class DragDropController;
 class FocusCycler;
 class InputMethodEventFilter;
 class PartialScreenshotEventFilter;
-class ResizeShadowController;
 class RootWindowEventFilter;
 class RootWindowLayoutManager;
 class ShadowController;
@@ -210,10 +209,6 @@ class ASH_EXPORT Shell : public aura::MonitorObserver {
     initially_hide_cursor_ = hide;
   }
 
-  internal::ResizeShadowController* resize_shadow_controller() {
-    return resize_shadow_controller_.get();
-  }
-
   // Made available for tests.
   internal::ShadowController* shadow_controller() {
     return shadow_controller_.get();
@@ -267,7 +262,6 @@ class ASH_EXPORT Shell : public aura::MonitorObserver {
   scoped_ptr<internal::WindowModalityController> window_modality_controller_;
   scoped_ptr<internal::DragDropController> drag_drop_controller_;
   scoped_ptr<internal::WorkspaceController> workspace_controller_;
-  scoped_ptr<internal::ResizeShadowController> resize_shadow_controller_;
   scoped_ptr<internal::ShadowController> shadow_controller_;
   scoped_ptr<internal::TooltipController> tooltip_controller_;
   scoped_ptr<internal::VisibilityController> visibility_controller_;
--- ash/wm/toplevel_window_event_filter.cc
+++ ash/wm/toplevel_window_event_filter.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/default_window_resizer.h"
 #include "ash/wm/property_util.h"
-#include "ash/wm/resize_shadow_controller.h"
 #include "ash/wm/window_resizer.h"
 #include "ash/wm/window_util.h"
 #include "base/message_loop.h"
@@ -98,10 +97,6 @@ bool ToplevelWindowEventFilter::PreHandleMouseEvent(aura::Window* target,
         return true;
       }
       break;
-    case ui::ET_MOUSE_MOVED:
-      return HandleMouseMoved(target, event);
-    case ui::ET_MOUSE_EXITED:
-      return HandleMouseExited(target, event);
     default:
       break;
   }
@@ -214,33 +209,6 @@ bool ToplevelWindowEventFilter::HandleDrag(aura::Window* target,
     return false;
   window_resizer_->Drag(ConvertPointToParent(target, event->location()));
   return true;
-}
-
-bool ToplevelWindowEventFilter::HandleMouseMoved(aura::Window* target,
-                                                 aura::LocatedEvent* event) {
-  // TODO(jamescook): Move the resize cursor update code into here from
-  // RootWindowEventFilter?
-  internal::ResizeShadowController* controller =
-      Shell::GetInstance()->resize_shadow_controller();
-  if (controller) {
-    if (event->flags() & ui::EF_IS_NON_CLIENT) {
-      int component =
-          target->delegate()->GetNonClientComponent(event->location());
-        controller->ShowShadow(target, component);
-    } else {
-      controller->HideShadow(target);
-    }
-  }
-  return false;
-}
-
-bool ToplevelWindowEventFilter::HandleMouseExited(aura::Window* target,
-                                                  aura::LocatedEvent* event) {
-  internal::ResizeShadowController* controller =
-      Shell::GetInstance()->resize_shadow_controller();
-  if (controller)
-    controller->HideShadow(target);
-  return false;
 }
 
 }  // namespace ash
