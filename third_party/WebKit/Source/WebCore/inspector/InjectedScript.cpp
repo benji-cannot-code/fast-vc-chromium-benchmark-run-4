e64f911e12503405cda2ba04f16097f79837bd14
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-16 14:43:13 +0000

    Web Inspector: TypeBuilder: Introduce OptOutput class for optional output parameters https://bugs.webkit.org/show_bug.cgi?id=80789
    Patch by Peter Rybin <peter.rybin@gmail.com> on 2012-03-16
    Reviewed by Yury Semikhatsky.
    
    OptOutput class is added for optional return parameters.
    
    Strict mode added to Generator that makes all parameter types strict
    and drop pre-set default values for return parameters.
    
    Debugger and Page domain is switched to strict mode.
    
    * inspector/CodeGeneratorInspector.py:
    (CommandReturnPassModel.OptOutput):
    (CommandReturnPassModel.OptOutput.__init__):
    (CommandReturnPassModel.OptOutput.get_return_var_type):
    (CommandReturnPassModel.OptOutput.get_output_argument_prefix):
    (CommandReturnPassModel.OptOutput.get_output_to_raw_expression):
    (CommandReturnPassModel.OptOutput.get_output_parameter_type):
    (CommandReturnPassModel):
    (CommandReturnPassModel.OptOutput.get_set_return_condition):
    (TypeModel.Enum.get_optional.EnumOptional.get_command_return_pass_model):
    (TypeModel.ValueType.ValueOptional.get_command_return_pass_model):
    (OptOutput):
    (Generator.process_event):
    (Generator.process_command):
    * inspector/ContentSearchUtils.cpp:
    (WebCore::ContentSearchUtils::buildObjectForSearchMatch):
    (WebCore::ContentSearchUtils::searchInTextByLines):
    * inspector/ContentSearchUtils.h:
    (ContentSearchUtils):
    * inspector/InjectedScript.cpp:
    (WebCore::InjectedScript::evaluate):
    (WebCore::InjectedScript::callFunctionOn):
    (WebCore::InjectedScript::evaluateOnCallFrame):
    (WebCore::InjectedScript::makeEvalCall):
    * inspector/InjectedScript.h:
    (InjectedScript):
    * inspector/InspectorDOMDebuggerAgent.cpp:
    (WebCore::InspectorDOMDebuggerAgent::didInvalidateStyleAttr):
    (WebCore::InspectorDOMDebuggerAgent::willInsertDOMNode):
    (WebCore::InspectorDOMDebuggerAgent::willRemoveDOMNode):
    (WebCore::InspectorDOMDebuggerAgent::willModifyDOMAttr):
    (WebCore::InspectorDOMDebuggerAgent::pauseOnNativeEventIfNeeded):
    (WebCore::InspectorDOMDebuggerAgent::willSendXMLHttpRequest):
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::setBreakpointByUrl):
    (WebCore::InspectorDebuggerAgent::setBreakpoint):
    (WebCore::InspectorDebuggerAgent::searchInContent):
    (WebCore::InspectorDebuggerAgent::setScriptSource):
    (WebCore::InspectorDebuggerAgent::getFunctionDetails):
    (WebCore::InspectorDebuggerAgent::schedulePauseOnNextStatement):
    (WebCore::InspectorDebuggerAgent::evaluateOnCallFrame):
    (WebCore::InspectorDebuggerAgent::didPause):
    (WebCore::InspectorDebuggerAgent::breakProgram):
    (WebCore::InspectorDebuggerAgent::clearBreakDetails):
    * inspector/InspectorDebuggerAgent.h:
    (InspectorDebuggerAgent):
    * inspector/InspectorPageAgent.cpp:
    (WebCore::buildObjectForCookie):
    (WebCore::buildArrayForCookies):
    (WebCore::InspectorPageAgent::getCookies):
    (WebCore::InspectorPageAgent::getResourceTree):
    (WebCore::InspectorPageAgent::searchInResource):
    (WebCore::InspectorPageAgent::searchInResources):
    (WebCore::InspectorPageAgent::buildObjectForFrame):
    (WebCore::InspectorPageAgent::buildObjectForFrameTree):
    * inspector/InspectorPageAgent.h:
    * inspector/InspectorRuntimeAgent.cpp:
    (WebCore::InspectorRuntimeAgent::evaluate):
    (WebCore::InspectorRuntimeAgent::callFunctionOn):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InjectedScript.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InjectedScript.cpp
@@ -64,7 +64,7 @@ InjectedScript::InjectedScript(ScriptObject injectedScriptObject, InspectedState
 {
 }
 
-void InjectedScript::evaluate(ErrorString* errorString, const String& expression, const String& objectGroup, bool includeCommandLineAPI, bool returnByValue, RefPtr<InspectorObject>* result, bool* wasThrown)
+void InjectedScript::evaluate(ErrorString* errorString, const String& expression, const String& objectGroup, bool includeCommandLineAPI, bool returnByValue, RefPtr<InspectorObject>* result, TypeBuilder::OptOutput<bool>* wasThrown)
 {
     ScriptFunctionCall function(m_injectedScriptObject, "evaluate");
     function.appendArgument(expression);
@@ -74,7 +74,7 @@ void InjectedScript::evaluate(ErrorString* errorString, const String& expression
     makeEvalCall(errorString, function, result, wasThrown);
 }
 
-void InjectedScript::callFunctionOn(ErrorString* errorString, const String& objectId, const String& expression, const String& arguments, bool returnByValue, RefPtr<InspectorObject>* result, bool* wasThrown)
+void InjectedScript::callFunctionOn(ErrorString* errorString, const String& objectId, const String& expression, const String& arguments, bool returnByValue, RefPtr<InspectorObject>* result, TypeBuilder::OptOutput<bool>* wasThrown)
 {
     ScriptFunctionCall function(m_injectedScriptObject, "callFunctionOn");
     function.appendArgument(objectId);
@@ -84,7 +84,7 @@ void InjectedScript::callFunctionOn(ErrorString* errorString, const String& obje
     makeEvalCall(errorString, function, result, wasThrown);
 }
 
-void InjectedScript::evaluateOnCallFrame(ErrorString* errorString, const ScriptValue& callFrames, const String& callFrameId, const String& expression, const String& objectGroup, bool includeCommandLineAPI, bool returnByValue, RefPtr<RemoteObject>* result, bool* wasThrown)
+void InjectedScript::evaluateOnCallFrame(ErrorString* errorString, const ScriptValue& callFrames, const String& callFrameId, const String& expression, const String& objectGroup, bool includeCommandLineAPI, bool returnByValue, RefPtr<RemoteObject>* result, TypeBuilder::OptOutput<bool>* wasThrown)
 {
     ScriptFunctionCall function(m_injectedScriptObject, "evaluateOnCallFrame");
     function.appendArgument(callFrames);
@@ -260,7 +260,7 @@ void InjectedScript::makeCall(ScriptFunctionCall& function, RefPtr<InspectorValu
         *result = InspectorString::create("Exception while making a call.");
 }
 
-void InjectedScript::makeEvalCall(ErrorString* errorString, ScriptFunctionCall& function, RefPtr<InspectorObject>* objectResult, bool* wasThrown)
+void InjectedScript::makeEvalCall(ErrorString* errorString, ScriptFunctionCall& function, RefPtr<InspectorObject>* objectResult, TypeBuilder::OptOutput<bool>* wasThrown)
 {
     RefPtr<InspectorValue> result;
     makeCall(function, &result);
