a9197ea9d4ce4e37a2225969c579fd7a0cd9cc3d
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 15:22:38 +0000

    Update net/base/cookie_*.h includes to net/cookies/cookie_*.h .
    Once complete, forwarding headers will be removed from net/base/ .
    
    TEST=none
    R=akalin
    
    
    Review URL: http://codereview.chromium.org/9703061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127167 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/http_bridge.cc
+++ chrome/browser/sync/glue/http_bridge.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/url_fetcher.h"
-#include "net/base/cookie_monster.h"
 #include "net/base/host_resolver.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
+#include "net/cookies/cookie_monster.h"
 #include "net/http/http_cache.h"
 #include "net/http/http_network_layer.h"
 #include "net/http/http_response_headers.h"
--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
 #include "grit/generated_resources.h"
-#include "net/base/cookie_monster.h"
+#include "net/cookies/cookie_monster.h"
 #include "sync/js/js_arg_list.h"
 #include "sync/js/js_event_details.h"
 #include "sync/util/cryptographer.h"
