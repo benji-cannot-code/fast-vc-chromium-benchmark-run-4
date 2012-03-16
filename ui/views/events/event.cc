4e7052b1a26abe0641ae27d22c1197649339dc0e
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 17:19:15 +0000

    views: Turn an unprocessed ScrollEvent into a MouseWheelEvent.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9704084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127193 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/events/event.cc
+++ ui/views/events/event.cc
@@ -150,6 +150,12 @@ MouseWheelEvent::MouseWheelEvent(const NativeEvent& native_event)
 }
 #endif
 
+MouseWheelEvent::MouseWheelEvent(const ScrollEvent& scroll_event)
+    : MouseEvent(scroll_event.native_event()),
+      offset_(scroll_event.y_offset()) {
+  set_type(ui::ET_MOUSEWHEEL);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // TouchEvent, public:
 
