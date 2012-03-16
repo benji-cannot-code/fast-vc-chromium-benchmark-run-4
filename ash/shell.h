0fb650b13ed5507daffdf987f60b0bc6aa34b5bf
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 22:30:48 +0000

    Ash: Add resize border shadow effect
    * Add ResizeShadow that uses solid-color layers to draw the resize border shadow effect.
    * Add ResizeShadowController to create ResizeShadows and track bounds changes for windows.
    * Wire both into ToplevelWindowEventFilter to track mouse move/enter events near (but not inside) the window frame.
    
    BUG=118325
    TEST=visual, hover mouse near window edges, drag windows
    
    Review URL: https://chromiumcodereview.appspot.com/9677070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127268 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.h
+++ ash/shell.h
@@ -63,6 +63,7 @@ class DragDropController;
 class FocusCycler;
 class InputMethodEventFilter;
 class PartialScreenshotEventFilter;
+class ResizeShadowController;
 class RootWindowEventFilter;
 class RootWindowLayoutManager;
 class ShadowController;
@@ -209,6 +210,10 @@ class ASH_EXPORT Shell : public aura::MonitorObserver {
     initially_hide_cursor_ = hide;
   }
 
+  internal::ResizeShadowController* resize_shadow_controller() {
+    return resize_shadow_controller_.get();
+  }
+
   // Made available for tests.
   internal::ShadowController* shadow_controller() {
     return shadow_controller_.get();
@@ -262,6 +267,7 @@ class ASH_EXPORT Shell : public aura::MonitorObserver {
   scoped_ptr<internal::WindowModalityController> window_modality_controller_;
   scoped_ptr<internal::DragDropController> drag_drop_controller_;
   scoped_ptr<internal::WorkspaceController> workspace_controller_;
+  scoped_ptr<internal::ResizeShadowController> resize_shadow_controller_;
   scoped_ptr<internal::ShadowController> shadow_controller_;
   scoped_ptr<internal::TooltipController> tooltip_controller_;
   scoped_ptr<internal::VisibilityController> visibility_controller_;
