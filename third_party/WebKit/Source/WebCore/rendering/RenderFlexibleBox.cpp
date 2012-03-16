14738129f1f5232d3e8f9a51cd0d19cdcaaa588d
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-16 20:16:17 +0000

    incorrectly removing scrollbar height for auto height flexbox children https://bugs.webkit.org/show_bug.cgi?id=81388
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    In a column flexbox, when computing the preferred height of an auto
    height child, we were subtracting out the horizontal scrollbar height.
    
    New test case in css3/flexbox/columns-auto-size.html.
    
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::preferredMainAxisContentExtentForChild):
    
    LayoutTests:
    
    * css3/flexbox/columns-auto-size-expected.txt:
    * css3/flexbox/columns-auto-size.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -572,7 +572,7 @@ LayoutUnit RenderFlexibleBox::preferredMainAxisContentExtentForChild(RenderBox* 
     Length mainAxisLength = mainAxisLengthForChild(child);
     if (mainAxisLength.isAuto()) {
         LayoutUnit mainAxisExtent = hasOrthogonalFlow(child) ? child->logicalHeight() : child->maxPreferredLogicalWidth();
-        return mainAxisExtent - mainAxisBorderAndPaddingExtentForChild(child) - mainAxisScrollbarExtentForChild(child);
+        return mainAxisExtent - mainAxisBorderAndPaddingExtentForChild(child);
     }
     return mainAxisLength.calcMinValue(mainAxisContentExtent());
 }
