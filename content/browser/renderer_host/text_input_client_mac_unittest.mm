040e9b779591e9b83572793b3e40f8f601b72d2e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 15:32:37 +0000

    content: Fix mac build by adding a missing using declaration.
    BUG=98716
    TBR=jam@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9699121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127170 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/text_input_client_mac_unittest.mm
+++ content/browser/renderer_host/text_input_client_mac_unittest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest_mac.h"
 
 using content::MockRenderProcessHost;
+using content::MockRenderProcessHostFactory;
 using content::RenderWidgetHostImpl;
 
 namespace {
