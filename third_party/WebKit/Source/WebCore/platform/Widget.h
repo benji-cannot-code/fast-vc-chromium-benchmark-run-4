808c3d5cced139722e05f05c31577764b0f69b26
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-16 23:27:03 +0000

    [BlackBerry] Upstream the BlackBerry change to platform/Widget.h https://bugs.webkit.org/show_bug.cgi?id=81304
    Reviewed by Rob Buis.
    
    typedefs of PlatformWidget and PlatformPageClient for the BlackBerry.
    
    * platform/Widget.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111085 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/Widget.h
+++ third_party/WebKit/Source/WebCore/platform/Widget.h
@@ -69,6 +69,10 @@ QT_END_NAMESPACE
 typedef QObject* PlatformWidget;
 #endif
 
+#if PLATFORM(BLACKBERRY)
+typedef void* PlatformWidget;
+#endif
+
 #if PLATFORM(WX)
 class wxWindow;
 typedef wxWindow* PlatformWidget;
@@ -84,6 +88,9 @@ typedef Evas_Object* PlatformWidget;
 #if PLATFORM(QT)
 class QWebPageClient;
 typedef QWebPageClient* PlatformPageClient;
+#elif PLATFORM(BLACKBERRY)
+#include "PageClientBlackBerry.h"
+typedef PageClientBlackBerry* PlatformPageClient;
 #else
 typedef PlatformWidget PlatformPageClient;
 #endif
