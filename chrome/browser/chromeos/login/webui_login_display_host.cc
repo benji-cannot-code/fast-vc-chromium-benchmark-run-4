fd1d7023dadacda4f5929c3a3b585a80f7cdb26b
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 22:35:32 +0000

    Change log level for 26646
    TBR=nkostylev@chromium.org
    BUG=chromium-os:26646
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9722004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_display_host.cc
+++ chrome/browser/chromeos/login/webui_login_display_host.cc
@@ -191,7 +191,7 @@ void WebUILoginDisplayHost::LoadURL(const GURL& url) {
     login_view_->OnWindowCreated();
   }
   // crosbug.com/26646.
-  LOG(WARNING) << "LoadURL:" << url;
+  LOG(ERROR) << "LoadURL:" << url;
   login_view_->LoadURL(url);
 }
 
