b14a91ba6444272de7bf4be169e93058dac9c6a7
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-16 20:25:40 +0000

    Add RenderMultiColumnBlock.cpp to RenderingAllInOne.cpp for Windows build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@111054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderingAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderingAllInOne.cpp
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderMediaControls.cpp"
 #include "RenderMenuList.cpp"
 #include "RenderMeter.cpp"
+#include "RenderMultiColumnBlock.cpp"
 #include "RenderObject.cpp"
 #include "RenderObjectChildList.cpp"
 #include "RenderPart.cpp"
