f102180324f5f889e34c15b95180979e71aae575
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 01:34:00 +0000

    Add a PyAuto API to dump heap profiles.
    BUG=114301
    TEST=none
    
    Review URL: http://codereview.chromium.org/9169024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127070 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -161,6 +161,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mach_ipc_mac.h"
 #endif
 
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#include "third_party/tcmalloc/chromium/src/gperftools/heap-profiler.h"
+#endif
+
 using automation::Error;
 using automation::ErrorCode;
 using automation_util::SendErrorIfModalDialogActive;
@@ -2416,6 +2420,10 @@ void TestingAutomationProvider::SendJSONRequest(int handle,
       &TestingAutomationProvider::TriggerPageActionById;
   handler_map["TriggerBrowserActionById"] =
       &TestingAutomationProvider::TriggerBrowserActionById;
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+  handler_map["HeapProfilerDump"] =
+      &TestingAutomationProvider::HeapProfilerDump;
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 #if defined(OS_CHROMEOS)
   handler_map["GetLoginInfo"] = &TestingAutomationProvider::GetLoginInfo;
   handler_map["ShowCreateAccountUI"] =
@@ -4801,6 +4809,31 @@ void TestingAutomationProvider::TriggerBrowserActionById(
     AutomationJSONReply(this, reply_message).SendSuccess(NULL);
   }
 }
+
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+// Refer to HeapProfilerDump() in chrome/test/pyautolib/pyauto.py for
+// sample json input.
+void TestingAutomationProvider::HeapProfilerDump(
+    DictionaryValue* args,
+    IPC::Message* reply_message) {
+  AutomationJSONReply reply(this, reply_message);
+
+  if (!::IsHeapProfilerRunning()) {
+    reply.SendError("The heap profiler is not running");
+    return;
+  }
+
+  std::string reason_string;
+  if (args->GetString("reason", &reason_string))
+    reason_string += " (via PyAuto testing)";
+  else
+    reason_string = "By PyAuto testing";
+
+  ::HeapProfilerDump(reason_string.c_str());
+
+  reply.SendSuccess(NULL);
+}
+#endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
 // Sample json input:
 //    { "command": "GetAutofillProfile" }
