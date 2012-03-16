3976ace5e733ac0aaae216eee9ce0d4daecfd5d6
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 04:24:18 +0000

    [Sync] Add support for associating a new Synced Bookmarks node.
    Previously we would result in an unrecoverable error when the server sends
    us a newly created Synced Bookmarks node. We now properly detect and associate
    it, allowing us to also associate new bookmarks created under the Synced
    Bookmarks folder.
    
    In addition, we soften our conditions for unrecoverable errors. A bookmark
    that cannot be added is ignored, instead of triggering an error. There are
    very few of these, and are likely triggered by a crash during initial
    association. 
    
    BUG=108978
    TEST=sync_integration_tests
    
    
    Review URL: http://codereview.chromium.org/9570055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127101 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test/integration/bookmarks_helper.cc
+++ chrome/browser/sync/test/integration/bookmarks_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -263,6 +263,10 @@ const BookmarkNode* GetBookmarkBarNode(int index) {
 
 const BookmarkNode* GetOtherNode(int index) {
   return GetBookmarkModel(index)->other_node();
+}
+
+const BookmarkNode* GetSyncedBookmarksNode(int index) {
+  return GetBookmarkModel(index)->mobile_node();
 }
 
 BookmarkModel* GetVerifierBookmarkModel() {
--- chrome/browser/sync/test/integration/bookmarks_helper.h
+++ chrome/browser/sync/test/integration/bookmarks_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,9 @@ const BookmarkNode* GetBookmarkBarNode(int index) WARN_UNUSED_RESULT;
 
 // Used to access the "other bookmarks" node within a particular sync profile.
 const BookmarkNode* GetOtherNode(int index) WARN_UNUSED_RESULT;
+
+// Used to access the "Synced Bookmarks" node within a particular sync profile.
+const BookmarkNode* GetSyncedBookmarksNode(int index) WARN_UNUSED_RESULT;
 
 // Used to access the bookmarks within the verifier sync profile.
 BookmarkModel* GetVerifierBookmarkModel() WARN_UNUSED_RESULT;
