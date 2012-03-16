8934826cbff8360d12babc5f37a8b30ff6728d37
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-16 09:22:36 +0000

    Revert 127107 - Implemented API for tracking ongoing file transfers from file manager.
    new method:
    chrome.fileManagerPrivate.getFileTransfers(function(transfers) {
    });
    
    new event:
    chrome.fileManagerPrivate.onFileTransfersUpdated.addListener(function(transfers) {
    });
    
    where:
    
      transfers = [
        {
          'fileUrl': 'filesystem://.../external/gdata/myfile.txt',
          'transferState': 'started|in_progress|completed|failed',
          'transferType': 'upload|download',
          'processed': 1234,
          'total': 54331
        },
      ...
      ]
    
    
    
    BUG=chromium-os:27819
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9703042
    
    TBR=zelidrag@chromium.org
    Reverted due to
    BUG=118573 - many leaks detected on HeapChecker and Valgrind CrOS bots.
    Review URL: https://chromiumcodereview.appspot.com/9703106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/gaia/gaia_oauth_fetcher.cc
+++ chrome/browser/net/gaia/gaia_oauth_fetcher.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_fetcher.h"
 #include "grit/chromium_strings.h"
 #include "net/base/load_flags.h"
-#include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_status.h"
@@ -689,20 +688,6 @@ void GaiaOAuthFetcher::OnURLFetchComplete(const content::URLFetcher* source) {
     OnOAuthRevokeTokenFetched(data, status, response_code);
   } else {
     NOTREACHED();
-  }
-
-  // Dump full response header for failed attempts:
-  if (response_code >= 400) {
-    if (source->GetResponseCode() ==
-            content::URLFetcher::RESPONSE_CODE_INVALID) {
-      LOG(WARNING) << "Error " << response_code
-                   << ", Response headers are malformed!!";
-    } else {
-      std::string headers;
-      source->GetResponseHeaders()->GetNormalizedHeaders(&headers);
-      LOG(WARNING) << "Error " << response_code
-                   << ", response header:\n" << headers;
-    }
   }
 }
 
