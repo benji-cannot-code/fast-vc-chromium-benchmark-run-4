62ed0652561f7822caa5929b5ca50e54b35985e2
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-17 01:29:39 +0000

    Unreviewed, rolling out r111094. http://trac.webkit.org/changeset/111094 https://bugs.webkit.org/show_bug.cgi?id=80882
    broke test on chromium-win
    
    Source/WebCore:
    
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::alignChildren):
    * rendering/RenderFlexibleBox.h:
    (RenderFlexibleBox):
    
    LayoutTests:
    
    * css3/flexbox/child-overflow-expected.html:
    * css3/flexbox/child-overflow.html:
    * css3/flexbox/flex-align-stretch-expected.txt:
    * css3/flexbox/flex-align-stretch.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -975,7 +975,25 @@ void RenderFlexibleBox::alignChildren(const OrderedFlexItemList& children, Layou
             ASSERT_NOT_REACHED();
             break;
         case AlignStretch: {
-            applyStretchAlignmentToChild(child, lineCrossAxisExtent);
+            if (!isColumnFlow() && child->style()->logicalHeight().isAuto()) {
+                LayoutUnit logicalHeightBefore = child->logicalHeight();
+                LayoutUnit stretchedLogicalHeight = child->logicalHeight() + availableAlignmentSpaceForChild(lineCrossAxisExtent, child);
+                child->setLogicalHeight(stretchedLogicalHeight);
+                child->computeLogicalHeight();
+
+                if (child->logicalHeight() != logicalHeightBefore) {
+                    child->setOverrideHeight(child->logicalHeight());
+                    child->setLogicalHeight(0);
+                    child->setChildNeedsLayout(true);
+                    child->layoutIfNeeded();
+                }
+            } else if (isColumnFlow() && child->style()->logicalWidth().isAuto() && isMultiline()) {
+                // FIXME: Handle min-width and max-width.
+                LayoutUnit childWidth = lineCrossAxisExtent - crossAxisMarginExtentForChild(child);
+                child->setOverrideWidth(std::max(0, childWidth));
+                child->setChildNeedsLayout(true);
+                child->layoutIfNeeded();
+            }
             // Since wrap-reverse flips cross start and cross end, strech children should be aligned with the cross end.
             if (style()->flexWrap() == FlexWrapReverse)
                 adjustAlignmentForChild(child, availableAlignmentSpaceForChild(lineCrossAxisExtent, child));
@@ -1009,32 +1027,6 @@ void RenderFlexibleBox::alignChildren(const OrderedFlexItemList& children, Layou
             if (flexAlignForChild(child) == AlignBaseline)
                 adjustAlignmentForChild(child, minMarginAfterBaseline);
         }
-    }
-}
-
-void RenderFlexibleBox::applyStretchAlignmentToChild(RenderBox* child, LayoutUnit lineCrossAxisExtent)
-{
-    if (!isColumnFlow() && child->style()->logicalHeight().isAuto()) {
-        LayoutUnit logicalHeightBefore = child->logicalHeight();
-        LayoutUnit stretchedLogicalHeight = child->logicalHeight() + availableAlignmentSpaceForChild(lineCrossAxisExtent, child);
-        if (stretchedLogicalHeight < logicalHeightBefore)
-            return;
-
-        child->setLogicalHeight(stretchedLogicalHeight);
-        child->computeLogicalHeight();
-
-        if (child->logicalHeight() != logicalHeightBefore) {
-            child->setOverrideHeight(child->logicalHeight());
-            child->setLogicalHeight(0);
-            child->setChildNeedsLayout(true);
-            child->layoutIfNeeded();
-        }
-    } else if (isColumnFlow() && child->style()->logicalWidth().isAuto() && isMultiline()) {
-        // FIXME: Handle min-width and max-width.
-        LayoutUnit childWidth = lineCrossAxisExtent - crossAxisMarginExtentForChild(child);
-        child->setOverrideWidth(std::max(0, childWidth));
-        child->setChildNeedsLayout(true);
-        child->layoutIfNeeded();
     }
 }
 
