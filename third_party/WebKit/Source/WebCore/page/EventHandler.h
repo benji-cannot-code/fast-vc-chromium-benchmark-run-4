0e71ee07d4086f2646c14d0db370a4363772bc52
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-17 00:52:17 +0000

    GestureTapDown needs to pin in-progress flings https://bugs.webkit.org/show_bug.cgi?id=80875
    Patch by Robert Kroeger <rjkroege@chromium.org> on 2012-03-16
    Reviewed by Adam Barth.
    
    * page/EventHandler.cpp:
    (WebCore):
    (WebCore::EventHandler::handleGestureTapDown):
    * page/EventHandler.h:
    (EventHandler):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111101 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.h
+++ third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -351,6 +351,7 @@ private:
 
 #if ENABLE(GESTURE_EVENTS)
     bool handleGestureScrollCore(const PlatformGestureEvent&, PlatformWheelEventGranularity, bool latchedWheel);
+    bool handleGestureTapDown();
 #endif
 
     Frame* m_frame;
