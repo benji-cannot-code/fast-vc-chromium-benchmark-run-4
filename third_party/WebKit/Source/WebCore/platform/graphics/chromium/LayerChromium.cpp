c3f273890332c19eeaa4c0ee415219dc6c180d0a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-18 21:58:59 +0000

    [chromium] Transform animation state should be inherited from parents https://bugs.webkit.org/show_bug.cgi?id=81363
    Patch by Dana Jansens <danakj@chromium.org> on 2012-03-18
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    Adds a drawTransformIsAnimating() and screenSpaceTransformIsAnimating()
    to the layer classes, and similar to the surface classes. These inherit
    the values from their parents in the same way that the transforms
    themselves do, and reflect if the layer's transform is being affected
    by some active animation.
    
    These can be used in main-thread culling when determining if the layer's
    position is static in the given coordinate space or being animated.
    
    Unit test: CCLayerTreeHostCommonTest.verifyAnimationsForRenderSurfaceHierarchy
    
    * platform/graphics/chromium/LayerChromium.cpp:
    (WebCore::LayerChromium::LayerChromium):
    * platform/graphics/chromium/LayerChromium.h:
    (LayerChromium):
    (WebCore::LayerChromium::drawTransformIsAnimating):
    (WebCore::LayerChromium::setDrawTransformIsAnimating):
    (WebCore::LayerChromium::screenSpaceTransformIsAnimating):
    (WebCore::LayerChromium::setScreenSpaceTransformIsAnimating):
    * platform/graphics/chromium/RenderSurfaceChromium.cpp:
    (WebCore::RenderSurfaceChromium::RenderSurfaceChromium):
    * platform/graphics/chromium/RenderSurfaceChromium.h:
    (WebCore::RenderSurfaceChromium::targetSurfaceTransformsAreAnimating):
    (WebCore::RenderSurfaceChromium::setTargetSurfaceTransformsAreAnimating):
    (WebCore::RenderSurfaceChromium::screenSpaceTransformsAreAnimating):
    (WebCore::RenderSurfaceChromium::setScreenSpaceTransformsAreAnimating):
    (RenderSurfaceChromium):
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
    (WebCore::CCLayerImpl::CCLayerImpl):
    * platform/graphics/chromium/cc/CCLayerImpl.h:
    (CCLayerImpl):
    (WebCore::CCLayerImpl::drawTransformIsAnimating):
    (WebCore::CCLayerImpl::setDrawTransformIsAnimating):
    (WebCore::CCLayerImpl::screenSpaceTransformIsAnimating):
    (WebCore::CCLayerImpl::setScreenSpaceTransformIsAnimating):
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp:
    (WebCore::subtreeShouldBeSkipped):
    (WebCore::calculateDrawTransformsAndVisibilityInternal):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::CCRenderSurface):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    (WebCore::CCRenderSurface::targetSurfaceTransformsAreAnimating):
    (WebCore::CCRenderSurface::setTargetSurfaceTransformsAreAnimating):
    (WebCore::CCRenderSurface::screenSpaceTransformsAreAnimating):
    (WebCore::CCRenderSurface::setScreenSpaceTransformsAreAnimating):
    (CCRenderSurface):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostCommonTest.cpp:
    (WebCore::addOpacityAnimationToLayer):
    (WebCore):
    (WebCore::addTransformAnimationToLayer):
    (WebCore::TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.cpp
@@ -83,6 +83,8 @@ LayerChromium::LayerChromium()
     , m_drawOpacity(0)
     , m_drawOpacityIsAnimating(false)
     , m_targetRenderSurface(0)
+    , m_drawTransformIsAnimating(false)
+    , m_screenSpaceTransformIsAnimating(false)
     , m_contentsScale(1.0)
     , m_layerAnimationDelegate(0)
 {
