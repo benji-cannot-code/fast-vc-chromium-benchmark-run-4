ea90faa0d8b68a2a049f527cb5caca134353624a
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-18 19:28:16 +0000

    [Qt] The background is visible for tiles inside the contents area which are not ready https://bugs.webkit.org/show_bug.cgi?id=81349
    Reviewed by Simon Hausmann.
    
    Split PageProxyNode to BackgroundSGNode and ContentsSGNode.
    BackgroundSGNode paints a solid background, either white or transparent
    (depending on drawsTransparentBackground flag).
    
    * UIProcess/API/qt/qquickwebpage.cpp:
    (ContentsSGNode):
    (ContentsSGNode::ContentsSGNode):
    (ContentsSGNode::changedStates):
    (ContentsSGNode::~ContentsSGNode):
    (BackgroundSGNode):
    (BackgroundSGNode::BackgroundSGNode):
    (BackgroundSGNode::contentsNode):
    (QQuickWebPage::updatePaintNode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111136 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebpage.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebpage.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtQuick/QQuickCanvas>
 #include <QtQuick/QSGGeometryNode>
 #include <QtQuick/QSGMaterial>
+#include <QtQuick/QSGSimpleRectNode>
 #include <private/qsgrendernode_p.h>
 
 QQuickWebPage::QQuickWebPage(QQuickWebView* viewportItem)
@@ -81,8 +82,9 @@ void QQuickWebPagePrivate::paint(QPainter* painter)
         webPageProxy->drawingArea()->paintLayerTree(painter);
 }
 
-struct PageProxyNode : public QSGRenderNode {
-    PageProxyNode(PassRefPtr<WebLayerTreeRenderer> renderer)
+class ContentsSGNode : public QSGRenderNode {
+public:
+    ContentsSGNode(PassRefPtr<WebLayerTreeRenderer> renderer)
         : m_renderer(renderer)
         , m_scale(1)
     {
@@ -105,7 +107,7 @@ struct PageProxyNode : public QSGRenderNode {
         layerTreeRenderer()->paintToCurrentGLContext(renderMatrix, inheritedOpacity(), clipRect());
     }
 
-    ~PageProxyNode()
+    ~ContentsSGNode()
     {
         layerTreeRenderer()->purgeGLResources();
     }
@@ -159,6 +161,29 @@ private:
     float m_scale;
 };
 
+class BackgroundSGNode : public QSGSimpleRectNode {
+public:
+    BackgroundSGNode()
+        : m_contentsNode(0)
+    {
+    }
+
+    ContentsSGNode* contentsNode(PassRefPtr<WebLayerTreeRenderer> renderer)
+    {
+        if (m_contentsNode && m_contentsNode->layerTreeRenderer() == renderer)
+            return m_contentsNode;
+
+        delete m_contentsNode;
+
+        m_contentsNode = new ContentsSGNode(renderer);
+        appendChildNode(m_contentsNode);
+        return m_contentsNode;
+    }
+
+private:
+    ContentsSGNode* m_contentsNode;
+};
+
 QSGNode* QQuickWebPage::updatePaintNode(QSGNode* oldNode, UpdatePaintNodeData*)
 {
     if (!d->webPageProxy->drawingArea())
@@ -167,23 +192,20 @@ QSGNode* QQuickWebPage::updatePaintNode(QSGNode* oldNode, UpdatePaintNodeData*)
     LayerTreeHostProxy* layerTreeHostProxy = d->webPageProxy->drawingArea()->layerTreeHostProxy();
     WebLayerTreeRenderer* renderer = layerTreeHostProxy->layerTreeRenderer();
 
-    PageProxyNode* node = static_cast<PageProxyNode*>(oldNode);
-
-    if (node && node->layerTreeRenderer() != renderer) {
-        // This means that LayerTreeHostProxy was deleted and recreated while old paint node survived.
-        // This could happen if web process have crashed. In this case we have to recreate paint node.
-        delete node;
-        node = 0;
-    }
-
+    BackgroundSGNode* backgroundNode = static_cast<BackgroundSGNode*>(oldNode);
+    if (!backgroundNode)
+        backgroundNode = new BackgroundSGNode();
+    ContentsSGNode* contentsNode = backgroundNode->contentsNode(renderer);
     renderer->syncRemoteContent();
 
-    if (!node)
-        node = new PageProxyNode(renderer);
+    contentsNode->setScale(d->contentsScale);
 
-    node->setScale(d->contentsScale);
+    QColor backgroundColor = d->webPageProxy->drawsTransparentBackground() ? Qt::transparent : Qt::white;
+    QRectF backgroundRect(0, 0, d->contentsSize.width() * d->contentsScale, d->contentsSize.height() * d->contentsScale);
+    backgroundNode->setColor(backgroundColor);
+    backgroundNode->setRect(backgroundRect);
 
-    return node;
+    return backgroundNode;
 }
 
 QtWebPageEventHandler* QQuickWebPage::eventHandler() const
