11caa8bb39fc86c9eb2fb413a43e63f843741a46
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-19 00:39:44 +0000

    WebCore::BitmapImage::getCGImageRef should not be used off the main thread. https://bugs.webkit.org/show_bug.cgi?id=81441
    Reviewed by Anders Carlsson.
    
    Covered by existing tests.
    
    * loader/cache/MemoryCache.cpp:
    (WebCore::memoryCache):
    (WebCore::MemoryCache::add):
    (WebCore::MemoryCache::revalidationFailed):
    (WebCore::MemoryCache::resourceForURL):
    (WebCore::MemoryCache::evict):
    Add back main thread assertions.
    
    * platform/graphics/cg/PatternCG.cpp:
    (PatternCallbackData):
    (WebCore::patternCallback):
    (WebCore::patternReleaseOnMainThreadCallback):
    (WebCore::Pattern::createPlatformPattern):
    Instead of pulling out the CGImageRef in the pattern callback, do it upfront.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
@@ -59,9 +59,8 @@ static const double cDefaultDecodedDataDeletionInterval = 0;
 MemoryCache* memoryCache()
 {
     static MemoryCache* staticCache = new MemoryCache;
-#if PLATFORM(CHROMIUM)
     ASSERT(WTF::isMainThread());
-#endif
+
     return staticCache;
 }
 
@@ -96,10 +95,9 @@ bool MemoryCache::add(CachedResource* resource)
 {
     if (disabled())
         return false;
-#if PLATFORM(CHROMIUM)
+
     ASSERT(WTF::isMainThread());
-#endif
-    
+
     m_resources.set(resource->url(), resource);
     resource->setInCache(true);
     
@@ -143,9 +141,7 @@ void MemoryCache::revalidationSucceeded(CachedResource* revalidatingResource, co
 
 void MemoryCache::revalidationFailed(CachedResource* revalidatingResource)
 {
-#if PLATFORM(CHROMIUM)
     ASSERT(WTF::isMainThread());
-#endif
     LOG(ResourceLoading, "Revalidation failed for %p", revalidatingResource);
     ASSERT(revalidatingResource->resourceToRevalidate());
     revalidatingResource->clearResourceToRevalidate();
@@ -153,9 +149,7 @@ void MemoryCache::revalidationFailed(CachedResource* revalidatingResource)
 
 CachedResource* MemoryCache::resourceForURL(const KURL& resourceURL)
 {
-#if PLATFORM(CHROMIUM)
     ASSERT(WTF::isMainThread());
-#endif
     KURL url = removeFragmentIdentifierIfNeeded(resourceURL);
     CachedResource* resource = m_resources.get(url);
     bool wasPurgeable = MemoryCache::shouldMakeResourcePurgeableOnEviction() && resource && resource->isPurgeable();
@@ -386,9 +380,7 @@ bool MemoryCache::makeResourcePurgeable(CachedResource* resource)
 
 void MemoryCache::evict(CachedResource* resource)
 {
-#if PLATFORM(CHROMIUM)
     ASSERT(WTF::isMainThread());
-#endif
     LOG(ResourceLoading, "Evicting resource %p for '%s' from cache", resource, resource->url().string().latin1().data());
     // The resource may have already been removed by someone other than our caller,
     // who needed a fresh copy for a reload. See <http://bugs.webkit.org/show_bug.cgi?id=12479#c6>.
