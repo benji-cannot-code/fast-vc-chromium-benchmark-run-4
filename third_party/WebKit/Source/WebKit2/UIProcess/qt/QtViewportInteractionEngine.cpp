edf0b6bc1519525813ec76a6a1080aa0d9a078a2
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-19 17:10:17 +0000

    Merge _q_commitScaleChange and _q_commitPositionChange https://bugs.webkit.org/show_bug.cgi?id=81511
    Reviewed by Simon Hausmann.
    
    Both methods were used for notifying WebCore of the new
    viewport and re-tiling. The methods are merge into
    _q_contentViewportChanged.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewFlickablePrivate::onComponentComplete):
    (QQuickWebViewFlickablePrivate::updateViewportSize):
    (QQuickWebViewFlickablePrivate::_q_contentViewportChanged):
    (QQuickWebViewFlickablePrivate::_q_resume):
    * UIProcess/API/qt/qquickwebview_p.h:
    * UIProcess/API/qt/qquickwebview_p_p.h:
    (QQuickWebViewPrivate::_q_contentViewportChanged):
    (QQuickWebViewFlickablePrivate):
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    (WebKit::ViewportUpdateDeferrer::~ViewportUpdateDeferrer):
    (WebKit::QtViewportInteractionEngine::flickableMovingPositionUpdate):
    (WebKit::QtViewportInteractionEngine::wheelEvent):
    (WebKit::QtViewportInteractionEngine::pinchGestureStarted):
    * UIProcess/qt/QtViewportInteractionEngine.h:
    (QtViewportInteractionEngine):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.cpp
@@ -87,7 +87,7 @@ public:
         }
 
         // Make sure that tiles all around the viewport will be requested.
-        emit engine->contentWasMoved(QPointF());
+        emit engine->contentViewportChanged(QPointF());
     }
 
 private:
@@ -223,7 +223,7 @@ void QtViewportInteractionEngine::flickableMovingPositionUpdate()
 {
     QPointF newPosition = m_flickProvider->contentPos();
 
-    emit contentWasMoved(m_lastScrollPosition - newPosition);
+    emit contentViewportChanged(m_lastScrollPosition - newPosition);
 
     m_lastScrollPosition = newPosition;
 }
@@ -280,7 +280,7 @@ void QtViewportInteractionEngine::wheelEvent(QWheelEvent* ev)
     QPointF newPosition = -boundPosition(endPosRange.topLeft(), newPos, endPosRange.bottomRight());
     m_flickProvider->setContentPos(newPosition);
 
-    emit contentWasMoved(currentPosition - newPosition);
+    emit contentViewportChanged(currentPosition - newPosition);
 }
 
 void QtViewportInteractionEngine::pagePositionRequest(const QPoint& pagePosition)
@@ -529,7 +529,7 @@ void QtViewportInteractionEngine::pinchGestureStarted(const QPointF& pinchCenter
     m_pinchStartScale = m_content->contentsScale();
 
     // Reset the tiling look-ahead vector so that tiles all around the viewport will be requested on pinch-end.
-    emit contentWasMoved(QPointF());
+    emit contentViewportChanged(QPointF());
 }
 
 void QtViewportInteractionEngine::pinchGestureRequestUpdate(const QPointF& pinchCenterInViewportCoordinates, qreal totalScaleFactor)
