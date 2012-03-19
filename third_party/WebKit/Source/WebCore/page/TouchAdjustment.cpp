2918a8ecda11492ee2b5f3ebcd31b7ab43e598ca
ahf@0x90.dk <ahf@0x90.dk@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-19 17:09:53 +0000

    Add NodeRenderStyle.h to TouchAdjustment.cpp
    Unreviewed build fix after r111185.
    
    * page/TouchAdjustment.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/TouchAdjustment.cpp
+++ third_party/WebKit/Source/WebCore/page/TouchAdjustment.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntPoint.h"
 #include "IntSize.h"
 #include "Node.h"
+#include "NodeRenderStyle.h"
 #include "RenderBox.h"
 #include "RenderObject.h"
 #include "RenderStyle.h"
