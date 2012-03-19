41037896def17129ebd01a6982cb1a9112bf1fd6
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-19 05:20:09 +0000

    npapi plugins: Make sure WebPlugin has no key functions on mac.
    See http://gcc.gnu.org/onlinedocs/gcc/Vague-Linkage.html
    
    BUG=90078
    TEST=none
    
    Review URL: http://codereview.chromium.org/9720021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127426 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugin.cc
+++ webkit/plugins/npapi/webplugin.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,13 +25,6 @@ bool WebPluginGeometry::Equals(const WebPluginGeometry& rhs) const {
          rects_valid == rhs.rects_valid &&
          visible == rhs.visible;
 }
-
-#if defined(OS_MACOSX)
-WebPluginAcceleratedSurface* WebPlugin::GetAcceleratedSurface(
-    gfx::GpuPreference gpu_preference) {
-  return NULL;
-}
-#endif
 
 }  // namespace npapi
 }  // namespace webkit
