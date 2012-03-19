e7362a7202dc5ddbfee72196768a8a7c207f2349
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-19 05:36:52 +0000

    chromeos: Fix the labels and the descriptions of a flag after change from enable to diasble.
    BUG=110130
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9722034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -588,8 +588,8 @@ const Experiment kExperiments[] = {
 #if defined(USE_ASH)
   {
     "disable-ash-uber-tray",
-    IDS_FLAGS_ENABLE_ASH_UBER_TRAY_NAME,
-    IDS_FLAGS_ENABLE_ASH_UBER_TRAY_DESCRIPTION,
+    IDS_FLAGS_DISABLE_ASH_UBER_TRAY_NAME,
+    IDS_FLAGS_DISABLE_ASH_UBER_TRAY_DESCRIPTION,
     kOsAll,
     SINGLE_VALUE_TYPE(ash::switches::kDisableAshUberTray),
   },
