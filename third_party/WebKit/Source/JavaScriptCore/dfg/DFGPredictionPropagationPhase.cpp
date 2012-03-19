1498a4d6b5ea755ea0b95fce7be4314a154021c2
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-19 23:36:59 +0000

    Prediction propagation for UInt32ToNumber incorrectly assumes that outs outcome does not change throughout the fixpoint https://bugs.webkit.org/show_bug.cgi?id=81583
    Reviewed by Michael Saboff.
    
    * dfg/DFGPredictionPropagationPhase.cpp:
    (JSC::DFG::PredictionPropagationPhase::propagate):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGPredictionPropagationPhase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGPredictionPropagationPhase.cpp
@@ -218,9 +218,9 @@ private:
             
         case UInt32ToNumber: {
             if (nodeCanSpeculateInteger(node.arithNodeFlags()))
-                changed |= setPrediction(PredictInt32);
+                changed |= mergePrediction(PredictInt32);
             else
-                changed |= setPrediction(PredictNumber);
+                changed |= mergePrediction(PredictNumber);
             
             changed |= m_graph[node.child1()].mergeArithNodeFlags(flags);
             break;
