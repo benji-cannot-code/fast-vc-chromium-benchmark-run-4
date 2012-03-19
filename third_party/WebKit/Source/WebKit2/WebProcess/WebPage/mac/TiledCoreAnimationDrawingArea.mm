c44495ec90e211ae778b4ca05aee1466ad517850
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-19 17:37:18 +0000

    Crash when closing an inspected web page with tiled drawing enabled https://bugs.webkit.org/show_bug.cgi?id=81524 <rdar://problem/11062396>
    Reviewed by Sam Weinig.
    
    Guard against the WebPage's underlying WebCore Page being null, which can happen
    when the inspector highlight overlay is uninstalled by the inspector when the inspected
    page goes away.
    
    * WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    (WebKit::TiledCoreAnimationDrawingArea::didUninstallPageOverlay):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
@@ -194,7 +194,8 @@ void TiledCoreAnimationDrawingArea::didInstallPageOverlay()
 void TiledCoreAnimationDrawingArea::didUninstallPageOverlay()
 {
 #if ENABLE(THREADED_SCROLLING)
-    m_webPage->corePage()->scrollingCoordinator()->setForceMainThreadScrollLayerPositionUpdates(false);
+    if (Page* page = m_webPage->corePage())
+        page->scrollingCoordinator()->setForceMainThreadScrollLayerPositionUpdates(false);
 #endif
 
     destroyPageOverlayLayer();
