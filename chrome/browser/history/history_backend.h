c44a8a54db4502fdb2f1fa4ea0ec4e1db1d7dc63
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-19 22:43:11 +0000

    Fixed unit-test to un-revert patch: Issue 9605037: Added Time slicing to History, used it on "Suggested" page BUG=118425 TEST=unit-tests TBR=brettw,csilv
    Review URL: http://codereview.chromium.org/9726006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127556 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_backend.h
+++ chrome/browser/history/history_backend.h
@@ -38,6 +38,7 @@ namespace history {
 
 class CommitLaterTask;
 class HistoryPublisher;
+class VisitFilter;
 
 // *See the .cc file for more information on the design.*
 //
@@ -176,6 +177,13 @@ class HistoryBackend : public base::RefCountedThreadSafe<HistoryBackend>,
       scoped_refptr<QueryMostVisitedURLsRequest> request,
       int result_count,
       int days_back);
+
+  // Request the |result_count| URLs and the chain of redirects
+  // leading to each of these URLs, filterd and sorted based on the |filter|.
+  void QueryFilteredURLs(
+      scoped_refptr<QueryMostVisitedURLsRequest> request,
+      int result_count,
+      const history::VisitFilter& filter);
 
   // QueryMostVisitedURLs without the request.
   void QueryMostVisitedURLsImpl(int result_count,
@@ -384,6 +392,8 @@ class HistoryBackend : public base::RefCountedThreadSafe<HistoryBackend>,
   FRIEND_TEST_ALL_PREFIXES(HistoryBackendTest, GetFaviconForURL);
   FRIEND_TEST_ALL_PREFIXES(HistoryBackendTest,
                            CloneFaviconIsRestrictedToSameDomain);
+  FRIEND_TEST_ALL_PREFIXES(HistoryBackendTest,
+                           QueryFilteredURLs);
 
   friend class ::TestingProfile;
 
