51aeb66ecb0100edbedcd1442666dade6c985716
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-19 13:42:26 +0000

    Make sure the critical notification bubbles dismiss once the user interacts with one of them (in one profile). Also add accessibilty feature to the bubble (to facilitate screen readers).
    BUG=118180
    TEST=See repro steps in bug. Contact dmazzoni for details on testing the accessibility feature.
    Review URL: https://chromiumcodereview.appspot.com/9702121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/critical_notification_bubble_view.h
+++ chrome/browser/ui/views/critical_notification_bubble_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -32,6 +32,11 @@ class CriticalNotificationBubbleView : public views::BubbleDelegateView,
 
   // views::WidgetDelegate overrides:
   virtual void WindowClosing() OVERRIDE;
+
+  // views::View overrides:
+  virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
+  virtual void ViewHierarchyChanged(
+      bool is_add, View* parent, View* child) OVERRIDE;
 
  protected:
   // views::BubbleDelegateView overrides:
