4869a5bf8bf240fd5ca08b063bc032b026b737da
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 20:04:01 +0000

    net: fallback to online revocation checks for EV status when CRLSet has expired.
    After this change our CRLSet logic is:
      * If we have a fresh CRLSet then we don't do online revocation checks unless the
        user has configured them. (It can be configured either via the settings UI,
        or with the EnableOnlineRevocationChecks policy option.)
      * If we don't have a CRLSet, or if it has expired, and we're trying EV verification,
        then we require a positive online revocation check in order to show the EV badge.
        An invalid revocation check reply will prevent the EV badge, but not hard-fail
        the whole verification.
    
    BUG=none
    TEST=net_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/9699043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127757 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate_nss.cc
+++ net/base/x509_certificate_nss.cc
@@ -317,7 +317,6 @@ CRLSetResult CheckRevocationWithCRLSet(CERTCertList* cert_list,
         return kCRLSetRevoked;
       case CRLSet::UNKNOWN:
       case CRLSet::GOOD:
-      case CRLSet::CRL_SET_EXPIRED:
         continue;
       default:
         NOTREACHED();
