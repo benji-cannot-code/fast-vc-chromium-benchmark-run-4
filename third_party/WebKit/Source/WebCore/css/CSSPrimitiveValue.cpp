e1adb640ed248101c57da21a232f63ca686c213e
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 21:54:15 +0000

    Remove unused CSSPrimitiveValue constructors. <http://webkit.org/b/81695>
    Reviewed by Antti Koivisto.
    
    Kill off CSSPrimitiveValue constructors that aren't used anywhere.
    
    * css/CSSPrimitiveValue.cpp:
    (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
    * css/CSSPrimitiveValue.h:
    (CSSPrimitiveValueBase):
    (WebCore::CSSPrimitiveValueBase::CSSPrimitiveValueBase):
    (CSSPrimitiveValue):
    * css/CSSPrimitiveValueMappings.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
@@ -191,20 +191,8 @@ static const AtomicString& valueOrPropertyName(int valueOrPropertyID)
     return nullAtom;
 }
 
-CSSPrimitiveValue::CSSPrimitiveValue()
-    : CSSValue(PrimitiveClass)
-{
-}
-
 CSSPrimitiveValue::CSSPrimitiveValue(int ident)
     : CSSValue(PrimitiveClass)
-{
-    m_primitiveUnitType = CSS_IDENT;
-    m_value.ident = ident;
-}
-
-CSSPrimitiveValue::CSSPrimitiveValue(ClassType classType, int ident)
-    : CSSValue(classType)
 {
     m_primitiveUnitType = CSS_IDENT;
     m_value.ident = ident;
@@ -226,14 +214,6 @@ CSSPrimitiveValue::CSSPrimitiveValue(const String& str, UnitTypes type)
         m_value.string->ref();
 }
 
-
-CSSPrimitiveValue::CSSPrimitiveValue(ClassType classType, const String& str, UnitTypes type)
-    : CSSValue(classType)
-{
-    m_primitiveUnitType = type;
-    if ((m_value.string = str.impl()))
-        m_value.string->ref();
-}
 
 CSSPrimitiveValue::CSSPrimitiveValue(RGBA32 color)
     : CSSValue(PrimitiveClass)
