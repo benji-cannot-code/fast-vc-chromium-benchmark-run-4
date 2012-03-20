45b85a3e9f7800246680fbc9828a4391a8e46915
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 01:16:59 +0000

    Revert 127575 - Revert 127573 - DOMStorageContextImpl that's implemented in terms of the new dom_storage classes. Also compile out existing tests that no longer apply when ENABLE_NEW_DOM_STORAGE_BACKEND is defined.
    And add the missing #include to fix the compile error.
    
    BUG=106763
    Review URL: https://chromiumcodereview.appspot.com/9695013
    
    TBR=michaeln@google.com
    Review URL: https://chromiumcodereview.appspot.com/9726022
    
    TBR=dgrogan@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9726023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127604 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/dom_storage/dom_storage_area.h
+++ webkit/dom_storage/dom_storage_area.h
@@ -28,6 +28,9 @@ class DomStorageArea
     : public base::RefCountedThreadSafe<DomStorageArea> {
 
  public:
+  static const FilePath::CharType kDatabaseFileExtension[];
+  static FilePath DatabaseFileNameFromOrigin(const GURL& origin);
+
   DomStorageArea(int64 namespace_id,
                  const GURL& origin,
                  const FilePath& directory,
@@ -60,8 +63,6 @@ class DomStorageArea
   // Posts a task to write the set of changed values to disk.
   void ScheduleCommitChanges();
   void CommitChanges();
-
-  static FilePath DatabaseFileNameFromOrigin(const GURL& origin);
 
   ~DomStorageArea();
 
