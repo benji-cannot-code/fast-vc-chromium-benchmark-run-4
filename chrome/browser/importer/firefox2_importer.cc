246436ba17db70f352522986b37efa4f913420c5
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 01:44:10 +0000

    Clean up TemplateURLParser:
    * Change signature to return an allocated TemplateURL* instead of requiring callers to create one and then returning a bool.  This will make it easier to modify how TemplateURLs are created later, and also simplifies callers a bit.
    * Remove TemplateURL::add_language() as all callers are already friends and can do this directly.
    * Generate a keyword for the TemplateURL inside the parser itself.  This will become mandatory later when keywords become non-optional.
    * Take a Profile* in the parser constructor.  This is currently unused, I'll use it later and was lazy enough to not want to add the diffs of plumbing it later :)
    * Make Parse() take char* instead of unsigned char* to avoid silly reinterpret_casts on both sides of the call.
    * Now that most of the parsing code is in TemplateURLParsingContext member functions, eliminate a lot of now-unnecessary accessor functions and helpers by inlining them, and take advantage of "friend of TemplateURL" status.
    * Style fixes, e.g. "type *name" -> "type* name".
    * Shorten.
    * Deinline functions in unittest helper class or rename them in unix_hacker() style.
    * Add some ASSERT_NO_FATAL_FAILURE()s.
    * Special-case an edge (that I am not sure can happen) in TemplateURLService::GenerateKeyword() in order to guarantee that callers with a valid URL and |autodetected| set to false will always get a non-empty keyword.  (Later this function will be condensed more so that it guarantees non-empty keywords in all cases, by expecting callers to handle the exceptions before calling it.)
    
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9728010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127613 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/firefox2_importer.cc
+++ chrome/browser/importer/firefox2_importer.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/mork_reader.h"
 #include "chrome/browser/importer/nss_decryptor.h"
 #include "chrome/browser/search_engines/template_url.h"
-#include "chrome/browser/search_engines/template_url_parser.h"
 #include "chrome/common/time_format.h"
 #include "chrome/common/url_constants.h"
 #include "googleurl/src/gurl.h"
