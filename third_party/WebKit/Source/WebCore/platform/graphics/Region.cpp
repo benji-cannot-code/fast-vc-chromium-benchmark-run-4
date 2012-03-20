00fbd9cf62dfcd58944950ee5e68254d6b5ee048
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 05:21:20 +0000

    A quick early-out for Region::contains() test https://bugs.webkit.org/show_bug.cgi?id=81625
    Patch by Dana Jansens <danakj@chromium.org> on 2012-03-19
    Reviewed by Anders Carlsson.
    
    If the bounds of region B include a row/column that is outside the bounds of
    region A, then region B contains a pixel within that row/column that is
    outside of region A.
    
    * platform/graphics/Region.cpp:
    (WebCore::Region::contains):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/Region.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/Region.cpp
@@ -68,6 +68,9 @@ Vector<IntRect> Region::rects() const
 
 bool Region::contains(const Region& region) const
 {
+    if (!m_bounds.contains(region.m_bounds))
+        return false;
+
     return WebCore::intersect(region, *this) == region;
 }
 
