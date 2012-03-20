081473f02b59ba3ff330ceb839980ffeb3659fd3
keishi@chromium.org <keishi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 08:52:20 +0000

    Implement input type=color UI
    BUG=92608
    TEST=
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=126889
    
    Review URL: https://chromiumcodereview.appspot.com/9203001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127656 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents.cc
+++ content/browser/tab_contents/tab_contents.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/view_messages.h"
 #include "content/port/browser/render_widget_host_view_port.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/color_chooser.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/devtools_agent_host_registry.h"
 #include "content/public/browser/download_manager.h"
@@ -273,7 +274,8 @@ TabContents::TabContents(content::BrowserContext* browser_context,
       temporary_zoom_settings_(false),
       content_restrictions_(0),
       view_type_(content::VIEW_TYPE_TAB_CONTENTS),
-      has_opener_(false) {
+      has_opener_(false),
+      color_chooser_(NULL) {
   render_manager_.Init(browser_context, site_instance, routing_id);
 
   view_.reset(content::GetContentClient()->browser()->
@@ -312,6 +314,9 @@ TabContents::~TabContents() {
   // Clear out any JavaScript state.
   if (dialog_creator_)
     dialog_creator_->ResetJavaScriptState(this);
+
+  if (color_chooser_)
+    color_chooser_->End();
 
   NotifyDisconnected();
 
@@ -559,6 +564,10 @@ bool TabContents::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(ViewHostMsg_Find_Reply, OnFindReply)
     IPC_MESSAGE_HANDLER(ViewHostMsg_CrashedPlugin, OnCrashedPlugin)
     IPC_MESSAGE_HANDLER(ViewHostMsg_AppCacheAccessed, OnAppCacheAccessed)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_OpenColorChooser, OnOpenColorChooser)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_EndColorChooser, OnEndColorChooser)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_SetSelectedColorInColorChooser,
+                        OnSetSelectedColorInColorChooser)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP_EX()
 
@@ -1382,6 +1391,20 @@ bool TabContents::HasOpener() const {
   return has_opener_;
 }
 
+void TabContents::DidChooseColorInColorChooser(int color_chooser_id,
+                                               const SkColor& color) {
+  GetRenderViewHost()->Send(new ViewMsg_DidChooseColorResponse(
+      GetRenderViewHost()->GetRoutingID(), color_chooser_id, color));
+}
+
+void TabContents::DidEndColorChooser(int color_chooser_id) {
+  GetRenderViewHost()->Send(new ViewMsg_DidEndColorChooser(
+      GetRenderViewHost()->GetRoutingID(), color_chooser_id));
+  if (delegate_)
+    delegate_->DidEndColorChooser();
+  color_chooser_ = NULL;
+}
+
 bool TabContents::FocusLocationBarByDefault() {
   content::WebUI* web_ui = GetWebUIForCurrentState();
   if (web_ui)
@@ -1689,6 +1712,24 @@ void TabContents::OnAppCacheAccessed(const GURL& manifest_url,
   // Notify observers about navigation.
   FOR_EACH_OBSERVER(WebContentsObserver, observers_,
                     AppCacheAccessed(manifest_url, blocked_by_policy));
+}
+
+void TabContents::OnOpenColorChooser(int color_chooser_id,
+                                     const SkColor& color) {
+  color_chooser_ = delegate_->OpenColorChooser(this, color_chooser_id, color);
+}
+
+void TabContents::OnEndColorChooser(int color_chooser_id) {
+  if (color_chooser_ &&
+      color_chooser_id == color_chooser_->identifier())
+    color_chooser_->End();
+}
+
+void TabContents::OnSetSelectedColorInColorChooser(int color_chooser_id,
+                                                   const SkColor& color) {
+  if (color_chooser_ &&
+      color_chooser_id == color_chooser_->identifier())
+    color_chooser_->SetSelectedColor(color);
 }
 
 // Notifies the RenderWidgetHost instance about the fact that the page is
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'public/browser/cert_store.h',
     'public/browser/child_process_data.h',
     'public/browser/child_process_security_policy.h',
+    'public/browser/color_chooser.h',
     'public/browser/content_browser_client.h',
     'public/browser/content_ipc_logging.h',
     'public/browser/devtools_agent_host_registry.h',
