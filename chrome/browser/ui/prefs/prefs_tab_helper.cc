371c58f0f645ca6fef7b6f40ff87ae9a3c9ad4a3
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 16:55:38 +0000

    Fix prefs migration for font settings.
    BUG=112706
    TEST=Verify font changes work when old preference values exist. See bug for details.
    Review URL: https://chromiumcodereview.appspot.com/9749001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127707 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -324,6 +324,7 @@ static void MigratePreferences(PrefService* prefs) {
     if (!pref->IsDefaultValue()) {
       prefs->Set(kPrefNamesToMigrate[i].to, *pref->GetValue()->DeepCopy());
     }
+    prefs->ClearPref(kPrefNamesToMigrate[i].from);
     prefs->UnregisterPreference(kPrefNamesToMigrate[i].from);
   }
 }
