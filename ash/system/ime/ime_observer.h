a45cd68cda310106cf6c1b3dc09756106af4a441
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 19:30:15 +0000

    ash: Add an entry for IME in the tray.
    BUG=118862
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9751005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127748 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ash/system/ime/ime_observer.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_IME_IME_OBSERVER_H_
+#define ASH_SYSTEM_IME_IME_OBSERVER_H_
+#pragma once
+
+namespace ash {
+
+class IMEObserver {
+ public:
+  virtual ~IMEObserver() {}
+
+  virtual void OnIMERefresh() = 0;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_IME_IME_OBSERVER_H_
