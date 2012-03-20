0875e17db9c9fe411a0299b638ec6ee12510e9cb
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 05:00:28 +0000

    chromeos: Use URLFetcher to download recovery image
    Replace Downloader with URLFetcher.
    
    BUG=113469
    TEST=chromeos://imageburner works
    
    
    Review URL: http://codereview.chromium.org/9731011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127642 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/imageburner/burn_manager_unittest.cc
+++ chrome/browser/chromeos/imageburner/burn_manager_unittest.cc
@@ -157,15 +157,6 @@ TEST(BurnManagerTest, StateMachineNormalWorkflow) {
     .Times(1)
     .RetiresOnSaturation();
 
-  EXPECT_FALSE(state_machine->image_download_requested());
-  EXPECT_FALSE(state_machine->download_started());
-  EXPECT_FALSE(state_machine->download_finished());
-  EXPECT_TRUE(state_machine->new_burn_posible());
-
-  state_machine->OnImageDownloadRequested();
-
-  EXPECT_EQ(StateMachine::INITIAL, state_machine->state());
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_FALSE(state_machine->download_started());
   EXPECT_FALSE(state_machine->download_finished());
   EXPECT_TRUE(state_machine->new_burn_posible());
@@ -173,7 +164,6 @@ TEST(BurnManagerTest, StateMachineNormalWorkflow) {
   state_machine->OnDownloadStarted();
 
   EXPECT_EQ(StateMachine::DOWNLOADING, state_machine->state());
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_TRUE(state_machine->download_started());
   EXPECT_FALSE(state_machine->download_finished());
   EXPECT_FALSE(state_machine->new_burn_posible());
@@ -182,7 +172,6 @@ TEST(BurnManagerTest, StateMachineNormalWorkflow) {
 
   // TODO(tbarzic): make this pass.
   // EXPECT_EQ(StateMachine::INITIAL, state_machine->state());
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_TRUE(state_machine->download_started());
   EXPECT_TRUE(state_machine->download_finished());
   EXPECT_FALSE(state_machine->new_burn_posible());
@@ -190,7 +179,6 @@ TEST(BurnManagerTest, StateMachineNormalWorkflow) {
   state_machine->OnBurnStarted();
 
   EXPECT_EQ(StateMachine::BURNING, state_machine->state());
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_TRUE(state_machine->download_started());
   EXPECT_TRUE(state_machine->download_finished());
   EXPECT_FALSE(state_machine->new_burn_posible());
@@ -198,7 +186,6 @@ TEST(BurnManagerTest, StateMachineNormalWorkflow) {
   state_machine->OnSuccess();
 
   EXPECT_EQ(StateMachine::INITIAL, state_machine->state());
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_TRUE(state_machine->download_started());
   EXPECT_TRUE(state_machine->download_finished());
   EXPECT_TRUE(state_machine->new_burn_posible());
@@ -225,25 +212,21 @@ TEST(BurnManagerTest, StateMachineError) {
   }
   state_machine->AddObserver(&observer);
 
-  state_machine->OnImageDownloadRequested();
   state_machine->OnDownloadStarted();
 
   state_machine->OnError(1234);
 
   // If called before download finished, download flags should be reset.
-  EXPECT_FALSE(state_machine->image_download_requested());
   EXPECT_FALSE(state_machine->download_started());
   EXPECT_EQ(state_machine->state(), StateMachine::INITIAL);
   EXPECT_TRUE(state_machine->new_burn_posible());
 
-  state_machine->OnImageDownloadRequested();
   state_machine->OnDownloadStarted();
   state_machine->OnDownloadFinished();
 
   state_machine->OnError(4321);
 
   // If called after download finished, download flags should not be changed.
-  EXPECT_TRUE(state_machine->image_download_requested());
   EXPECT_TRUE(state_machine->download_started());
   EXPECT_TRUE(state_machine->download_finished());
   EXPECT_EQ(state_machine->state(), StateMachine::INITIAL);
