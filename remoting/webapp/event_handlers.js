0ed5e98bdce1d6f62f84fe3f8fa917c77fc35378
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 16:51:32 +0000

    [Chromoting] Adds a button that cancels PIN entry.
    BUG=116813
    
    Review URL: https://chromiumcodereview.appspot.com/9731014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127705 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/event_handlers.js
+++ remoting/webapp/event_handlers.js
@@ -35,6 +35,9 @@ function onLoad() {
     remoting.connectMe2MeWithPin();
     event.preventDefault();
   };
+  var cancelPinEntry = function() {
+    remoting.initDaemonUi();
+  }
   var doAuthRedirect = function() {
     remoting.oauth2.doAuthRedirect();
   };
@@ -56,6 +59,7 @@ function onLoad() {
       { event: 'click', id: 'client-finished-it2me-button',
         fn: goFinishedIt2Me },
       { event: 'click', id: 'client-finished-me2me-button', fn: goHome },
+      { event: 'click', id: 'cancel-pin-entry-button', fn: cancelPinEntry },
       { event: 'click', id: 'client-reconnect-button', fn: reload },
       { event: 'click', id: 'cancel-access-code-button',
         fn: remoting.cancelConnect },
