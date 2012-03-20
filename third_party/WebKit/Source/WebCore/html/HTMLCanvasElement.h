b26ffed849001b0d1b3c55f1f8b8f51f89b72a60
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 23:03:42 +0000

    Unreviewed, rolling out r111442. http://trac.webkit.org/changeset/111442 https://bugs.webkit.org/show_bug.cgi?id=81711
    Causes crashes and image mismatches in canvas tests (Requested
    by leviw on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-03-20
    
    Source/WebCore: 
    
    * html/HTMLCanvasElement.cpp:
    (WebCore::HTMLCanvasElement::HTMLCanvasElement):
    (WebCore::HTMLCanvasElement::reset):
    (WebCore::HTMLCanvasElement::createImageBuffer):
    (WebCore::HTMLCanvasElement::clearCopiedImage):
    * html/HTMLCanvasElement.h:
    (HTMLCanvasElement):
    * html/canvas/CanvasRenderingContext2D.cpp:
    (WebCore::CanvasRenderingContext2D::CanvasRenderingContext2D):
    (WebCore::CanvasRenderingContext2D::unwindStateStack):
    (WebCore::CanvasRenderingContext2D::reset):
    
    LayoutTests: 
    
    * fast/canvas/canvas-resize-after-paint-without-layout-expected.txt: Removed.
    * fast/canvas/canvas-resize-after-paint-without-layout.html: Removed.
    * platform/chromium-linux/fast/canvas/canvas-resize-after-paint-without-layout-expected.png: Removed.
    * platform/chromium-linux/fast/canvas/setWidthResetAfterForcedRender-expected.png:
    * platform/chromium-linux/platform/chromium/virtual/gpu/fast/canvas/canvas-resize-after-paint-without-layout-expected.png: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111458 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.h
@@ -144,7 +144,6 @@ private:
     void reset();
 
     void createImageBuffer() const;
-    void clearImageBuffer() const;
 
     void setSurfaceSize(const IntSize&);
 
@@ -168,7 +167,6 @@ private:
 
     // m_createdImageBuffer means we tried to malloc the buffer.  We didn't necessarily get it.
     mutable bool m_hasCreatedImageBuffer;
-    mutable bool m_didClearImageBuffer;
     mutable OwnPtr<ImageBuffer> m_imageBuffer;
     
     mutable RefPtr<Image> m_presentedImage;
