86f9fec9010790bca39436a9612a6cbd4aeec821
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 21:02:15 +0000

    Increase reschedule timeout to 1 second.
    Due to the new per-command-buffer descheduling, this was being hit too often due to shader compiles and other expensive operations in still-scheduled command buffers.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9732023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127774 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_scheduler.cc
+++ gpu/command_buffer/service/gpu_scheduler.cc
@@ -20,7 +20,7 @@ using ::base::SharedMemory;
 namespace gpu {
 
 namespace {
-const int64 kRescheduleTimeOutDelay = 100;
+const int64 kRescheduleTimeOutDelay = 1000;
 }
 
 GpuScheduler::GpuScheduler(
