46278e6ced60afd5e50d32eb31e07d2118074a2a
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 01:35:37 +0000

    P2PSocketHostTcp::Init method calls SetSendBufferSize after net::TCPClientSocket object creation. After the object creation TCPClientSocket::socket_ is pointing to invalid address where as bound_socket_ has the right value. The socket_ will have right value of socket fd once Connect method is called for the socket object.
    Review URL: http://codereview.chromium.org/9729011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127612 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_host_tcp.cc
+++ content/browser/renderer_host/p2p/socket_host_tcp.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -60,9 +60,6 @@ bool P2PSocketHostTcp::Init(const net::IPEndPoint& local_address,
   }
   socket_.reset(tcp_socket.release());
 
-  if (socket_->SetSendBufferSize(kMaxSendBufferSize))
-    LOG(WARNING) << "Failed to set send buffer size for TCP socket.";
-
   int result = socket_->Connect(
       base::Bind(&P2PSocketHostTcp::OnConnected, base::Unretained(this)));
   if (result != net::ERR_IO_PENDING) {
@@ -76,8 +73,9 @@ void P2PSocketHostTcp::OnError() {
   socket_.reset();
 
   if (state_ == STATE_UNINITIALIZED || state_ == STATE_CONNECTING ||
-      state_ == STATE_OPEN)
+      state_ == STATE_OPEN) {
     message_sender_->Send(new P2PMsg_OnError(routing_id_, id_));
+  }
 
   state_ = STATE_ERROR;
 }
@@ -89,6 +87,10 @@ void P2PSocketHostTcp::OnConnected(int result) {
   if (result != net::OK) {
     OnError();
     return;
+  }
+
+  if (socket_->SetSendBufferSize(kMaxSendBufferSize)) {
+    LOG(WARNING) << "Failed to set send buffer size for TCP socket.";
   }
 
   net::IPEndPoint address;
