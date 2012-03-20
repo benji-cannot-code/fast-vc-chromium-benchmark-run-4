c69be2dbc705c7e42a85cbdde44573473647acbf
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 16:28:29 +0000

    Build fix.
    * history/PageCache.cpp: Fixed an unused function warning introduced in r111391.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/history/PageCache.cpp
+++ third_party/WebKit/Source/WebCore/history/PageCache.cpp
@@ -84,6 +84,7 @@ enum ReasonFrameCannotBeInPageCache {
 };
 COMPILE_ASSERT(NumberOfReasonsFramesCannotBeInPageCache <= sizeof(unsigned)*8, ReasonFrameCannotBeInPageCacheDoesNotFitInBitmap);
 
+#if PLATFORM(CHROMIUM)
 static int indexOfSingleBit(int32_t v)
 {
     int index = 0;
@@ -99,6 +100,7 @@ static int indexOfSingleBit(int32_t v)
         index += 1;
     return index;
 }
+#endif // PLATFORM(CHROMIUM)
 
 static unsigned logCanCacheFrameDecision(Frame* frame, int indentLevel)
 {
