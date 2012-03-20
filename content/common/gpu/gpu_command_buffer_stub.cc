184c24bbc4bad371734cb900ef2895515df2e3e4
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 17:04:08 +0000

    Fixing issue with off screen contexts DCHECK(surface_) failing during SetMemoryAllocation.
    SetMemoryAllocation already early exits if it does not have a surface, and the DCHECK should not have been here.
    
    TBR=backer
    BUG=119100
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9755009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127708 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -624,8 +624,6 @@ void GpuCommandBufferStub::SetMemoryAllocation(
 
   SendMemoryAllocationToProxy(allocation);
 
-  DCHECK(surface_);
-
   if (!surface_)
     return;
   if (allocation.suggest_have_frontbuffer && allocation.suggest_have_backbuffer)
