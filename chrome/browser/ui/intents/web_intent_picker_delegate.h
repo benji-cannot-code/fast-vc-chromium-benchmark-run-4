e303936cd52d0246843cd68ed290fb5204e48b84
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 18:02:52 +0000

    Revert 127716 - [Web Intents] Inline installation of extensions in web intents picker.
    BUG=113476
    TEST=WebIntentsRegistryTest.GetIntentServicesWithExtensionId, WebIntentPickerControllerBrowserTest.ExtensionInstallSuccess
    
    Review URL: https://chromiumcodereview.appspot.com/9595031
    
    TBR=binji@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9751014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_delegate.h
+++ chrome/browser/ui/intents/web_intent_picker_delegate.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_INTENTS_WEB_INTENT_PICKER_DELEGATE_H_
 #pragma once
 
-#include <string>
 #include "chrome/browser/ui/intents/web_intent_picker_model.h"
 
 namespace content {
@@ -30,9 +29,6 @@ class WebIntentPickerDelegate {
   // disposition.
   virtual void OnInlineDispositionWebContentsCreated(
       content::WebContents* web_contents) = 0;
-
-  // Called when the user has chosen to install a suggested extension.
-  virtual void OnExtensionInstallRequested(const std::string& id) = 0;
 
   // Called when the user cancels out of the dialog, whether by closing it
   // manually or otherwise purposefully.
