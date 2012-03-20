33e8dcba2ff98b8f3621bb0f9a8420730901ff06
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 04:47:25 +0000

    [Chromium] Add registerURLSchemeAsEmptyDocument to WebSecurityPolicy https://bugs.webkit.org/show_bug.cgi?id=81421
    Patch by Charlie Reis <creis@chromium.org> on 2012-03-19
    Reviewed by Adam Barth.
    
    * public/WebSecurityPolicy.h:
    (WebSecurityPolicy):
    * src/WebSecurityPolicy.cpp:
    (WebKit::WebSecurityPolicy::registerURLSchemeAsEmptyDocument):
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSecurityPolicy.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSecurityPolicy.cpp
@@ -69,6 +69,11 @@ void WebSecurityPolicy::registerURLSchemeAsCORSEnabled(const WebString& scheme)
     SchemeRegistry::registerURLSchemeAsCORSEnabled(scheme);
 }
 
+void WebSecurityPolicy::registerURLSchemeAsEmptyDocument(const WebString& scheme)
+{
+    SchemeRegistry::registerURLSchemeAsEmptyDocument(scheme);
+}
+
 void WebSecurityPolicy::addOriginAccessWhitelistEntry(
     const WebURL& sourceOrigin,
     const WebString& destinationProtocol,
