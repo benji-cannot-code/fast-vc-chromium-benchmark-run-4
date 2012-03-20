ada4a9c7f6912cc7f932dbc76a55267faa02a325
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 19:57:48 +0000

    Linux: Only close the wrench menu when the bookmark model changes if the bookmark submenu is actually showing.
    Also, fix a case where we could get into a state where we could not close the menu at all, even if the model changed.
    Review URL: https://chromiumcodereview.appspot.com/9748003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/bookmarks/bookmark_sub_menu_model_gtk.cc
+++ chrome/browser/ui/gtk/bookmarks/bookmark_sub_menu_model_gtk.cc
@@ -122,7 +122,8 @@ BookmarkSubMenuModel::BookmarkSubMenuModel(
       browser_(browser),
       fixed_items_(0),
       bookmark_end_(0),
-      menu_(NULL) {
+      menu_(NULL),
+      menu_showing_(false) {
 }
 
 BookmarkSubMenuModel::~BookmarkSubMenuModel() {
@@ -137,7 +138,7 @@ void BookmarkSubMenuModel::Loaded(BookmarkModel* model, bool ids_reassigned) {
 }
 
 void BookmarkSubMenuModel::BookmarkModelChanged() {
-  if (menu_)
+  if (menu_showing_ && menu_)
     menu_->Cancel();
 }
 
@@ -150,6 +151,7 @@ void BookmarkSubMenuModel::BookmarkModelBeingDeleted(
 }
 
 void BookmarkSubMenuModel::MenuWillShow() {
+  menu_showing_ = true;
   Clear();
   AddCheckItemWithStringId(IDC_SHOW_BOOKMARK_BAR, IDS_SHOW_BOOKMARK_BAR);
   AddItemWithStringId(IDC_SHOW_BOOKMARK_MANAGER, IDS_BOOKMARK_MANAGER);
@@ -187,6 +189,11 @@ void BookmarkSubMenuModel::MenuWillShow() {
       AddSeparator();
     AddSubMenuForNode(model()->mobile_node());
   }
+}
+
+void BookmarkSubMenuModel::MenuClosed() {
+  menu_showing_ = false;
+  BookmarkNodeMenuModel::MenuClosed();
 }
 
 void BookmarkSubMenuModel::ActivatedAt(int index) {
