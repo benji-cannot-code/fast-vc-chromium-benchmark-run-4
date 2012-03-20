eb5acab6663ff7c8c1d162c7850b18f4091e4038
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 14:56:32 +0000

    Speculative fix for Win builder.
    BUG=none
    TEST=none
    TBR=nfullagar@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9749007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127674 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/browser_utils.cc
+++ ppapi/native_client/src/trusted/plugin/browser_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 // Portable interface for browser interaction - API invariant portions.
+#define BASE_IMPLEMENTATION
 
 #include "base/rand_util_c.h"
 
