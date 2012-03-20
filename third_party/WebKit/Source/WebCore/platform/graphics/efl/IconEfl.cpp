fe8c7422bae641d290f3f7ce8fc748219b3032c0
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 05:20:04 +0000

    Unreviewed attempt to fix the EFL build.
    * platform/graphics/efl/IconEfl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/efl/IconEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/efl/IconEfl.cpp
@@ -38,8 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "MIMETypeRegistry.h"
 #include "NotImplemented.h"
-
-#include <text/PlatformString.h>
+#include "PlatformString.h"
 #include <wtf/PassRefPtr.h>
 
 namespace WebCore {
