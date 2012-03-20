e88a06772a7a944bc1771f4b60ddbf848f8a5533
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 05:37:44 +0000

    REGRESSION(r110072): Improper repainting on table with overflow: hidden https://bugs.webkit.org/show_bug.cgi?id=81460
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Test: fast/repaint/table-hover-on-link.html
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::layout): Add a call to RenderBlock::updateCachedSizeForOverflowClip
    so that we properly keep our cached size up to date. This fixes the problem for now but the
    real solution is to make sure it is called on all classes that needs to. This is tracked by
    bug 81461.
    
    LayoutTests:
    
    * fast/repaint/table-hover-on-link-expected.txt: Added.
    * fast/repaint/table-hover-on-link.html: Added.
    
    * platform/chromium-linux/fast/repaint/table-hover-on-link-expected.png: Added.
    * platform/chromium-mac/fast/repaint/table-hover-on-link-expected.png: Added.
    * platform/mac/fast/repaint/table-hover-on-link-expected.png: Added.
    There seem to be some small color difference here (maybe color profile related).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
@@ -448,6 +448,9 @@ void RenderTable::layout()
     // FIXME: Only pass true if width or height changed.
     layoutPositionedObjects(true);
 
+    if (hasOverflowClip() && !hasLayer())
+        updateCachedSizeForOverflowClip();
+
     updateLayerTransform();
 
     // Layout was changed, so probably borders too.
