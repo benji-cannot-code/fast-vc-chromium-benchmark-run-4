e2b084914a538f8b5945513598e25f1865192412
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 05:15:50 +0000

    Division optimizations fail to infer cases of truncated division and mishandle -2147483648/-1 https://bugs.webkit.org/show_bug.cgi?id=81428 <rdar://problem/11067382>
    Reviewed by Oliver Hunt.
    
    If you're a division over integers and you're only used as an integer, then you're
    an integer division and remainder checks become unnecessary. If you're dividing
    -2147483648 by -1, don't crash.
    
    * assembler/MacroAssemblerX86Common.h:
    (MacroAssemblerX86Common):
    (JSC::MacroAssemblerX86Common::add32):
    * dfg/DFGSpeculativeJIT.cpp:
    (DFG):
    (JSC::DFG::SpeculativeJIT::compileIntegerArithDivForX86):
    * dfg/DFGSpeculativeJIT.h:
    (SpeculativeJIT):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * llint/LowLevelInterpreter64.asm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
@@ -1738,6 +1738,9 @@ private:
     void compileArithSub(Node&);
     void compileArithNegate(Node&);
     void compileArithMul(Node&);
+#if CPU(X86) || CPU(X86_64)
+    void compileIntegerArithDivForX86(Node&);
+#endif
     void compileArithMod(Node&);
     void compileSoftModulo(Node&);
     void compileGetTypedArrayLength(const TypedArrayDescriptor&, Node&, bool needsSpeculationCheck);
