8b57a6886125faaec0a8df2ca389081970b82b9f
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 21:39:40 +0000

    Add NetworkListObserver utility class.
    BUG=114808
    
    
    Review URL: http://codereview.chromium.org/9696051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127797 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_network_monitor_private.h
+++ ppapi/tests/test_network_monitor_private.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/tests/test_case.h"
 
+namespace pp {
+class NetworkListPrivate;
+}  // namespace pp
+
 class TestNetworkMonitorPrivate : public TestCase {
  public:
   explicit TestNetworkMonitorPrivate(TestingInstance* instance);
@@ -23,8 +27,10 @@ class TestNetworkMonitorPrivate : public TestCase {
   std::string TestBasic();
   std::string Test2Monitors();
   std::string TestDeleteInCallback();
+  std::string TestListObserver();
 
-  std::string VerifyNetworkList(PP_Resource network_resource);
+  std::string VerifyNetworkListResource(PP_Resource network_resource);
+  std::string VerifyNetworkList(const pp::NetworkListPrivate& network_list);
 };
 
 #endif  // PAPPI_TESTS_TEST_NETWORK_MONITOR_PRIVATE_H_
