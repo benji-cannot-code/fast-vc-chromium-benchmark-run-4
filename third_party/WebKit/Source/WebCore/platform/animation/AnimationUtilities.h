5b6deba1278e99e9e952b0a2394dda3b9ef03106
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-20 06:17:23 +0000

    Add FractionalLayoutUnit version of blend to AnimationUtilities https://bugs.webkit.org/show_bug.cgi?id=81572
    Reviewed by Eric Seidel.
    
    Add version of blend method that operates on FractionalLayoutUnits. This
    will be used in place of the integer version in many places once we
    enable support for subpixel layout.
    
    No new tests.
    
    * platform/FractionalLayoutUnit.h:
    Include math.h to make GTK build happy.
    
    * platform/animation/AnimationUtilities.h:
    (WebCore::blend):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/animation/AnimationUtilities.h
+++ third_party/WebKit/Source/WebCore/platform/animation/AnimationUtilities.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef AnimationUtilities_h
 #define AnimationUtilities_h
 
+#include "FractionalLayoutUnit.h"
 #include <wtf/MathExtras.h>
 
 namespace WebCore {
@@ -49,6 +50,11 @@ inline double blend(double from, double to, double progress)
 inline float blend(float from, float to, double progress)
 {  
     return static_cast<float>(from + (to - from) * progress);
+}
+
+inline FractionalLayoutUnit blend(FractionalLayoutUnit from, FractionalLayoutUnit to, double progress)
+{  
+    return from + (to - from) * progress;
 }
 
 } // namespace WebCore
