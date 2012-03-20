36002a8d4967b44e376c09afb63668b344b8092d
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 17:32:16 +0000

    Load the metro driver dll at startup
    When the metro_driver is present, we need to relinquish the browser main
    thread to it, execution of chrome will continue on another thread, which
    will be used as chrome main thread.
    
    TEST=none, other that chrome should work.
    BUG=118641
    Review URL: https://chromiumcodereview.appspot.com/9718001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/app/breakpad_win.h"
 #include "chrome/app/client_util.h"
+#include "chrome/app/metro_driver_win.h"
 #include "content/public/app/startup_helper_win.h"
 #include "content/public/common/result_codes.h"
 #include "sandbox/src/sandbox_factory.h"
 
-int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, wchar_t*, int) {
+int RunChrome(HINSTANCE instance) {
   bool exit_now = true;
   // We restarted because of a previous crash. Ask user if we should relaunch.
   if (ShowRestartDialogIfCrashed(&exit_now)) {
@@ -37,6 +38,13 @@ int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, wchar_t*, int) {
   int rc = loader->Launch(instance, &sandbox_info);
   loader->RelaunchChromeBrowserWithNewCommandLineIfNeeded();
   delete loader;
-
   return rc;
+}
+
+int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
+  MetroDriver metro_driver;
+  if (metro_driver.in_metro_mode())
+    return metro_driver.RunInMetro(instance, &RunChrome);
+  // Not in metro mode, proceed as normal.
+  return RunChrome(instance);
 }
--- chrome/chrome_exe.gypi
+++ chrome/chrome_exe.gypi
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'app/client_util.h',
         'app/hard_error_handler_win.cc',
         'app/hard_error_handler_win.h',
+        'app/metro_driver_win.cc',
+        'app/metro_driver_win.h',
         '../content/app/startup_helper_win.cc',
       ],
       'mac_bundle_resources': [
