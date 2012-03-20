3495806992172bcc34f6e1ba850b2ce5b24e3134
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 18:40:45 +0000

    update browser tests for options2
    BUG=111579
    TEST=browser_tests:OptionsWebUITest.*
    
    Review URL: https://chromiumcodereview.appspot.com/9750001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/options2_browsertest.js
+++ chrome/browser/ui/webui/options2/options2_browsertest.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,7 @@ OptionsWebUITest.prototype = {
   /**
    * Browse to the options page & call our preLoad().
    */
-  browsePreload: 'chrome://settings',
+  browsePreload: 'chrome://settings-frame',
 
   /**
    * Register a mock handler to ensure expectations are met and options pages
@@ -24,8 +24,7 @@ OptionsWebUITest.prototype = {
    */
   preLoad: function() {
     this.makeAndRegisterMockHandler(
-        ['coreOptionsInitialize',
-         'defaultZoomFactorAction',
+        ['defaultZoomFactorAction',
          'fetchPrefs',
          'observePrefs',
          'setBooleanPref',
@@ -39,11 +38,11 @@ OptionsWebUITest.prototype = {
          // getInstantFieldTrialStatus: function() {},
         ]);
 
-    // Register stubs for methods expected to be called before our tests run.
+    // Register stubs for methods expected to be called before/during tests.
     // Specific expectations can be made in the tests themselves.
     this.mockHandler.stubs().fetchPrefs(ANYTHING);
     this.mockHandler.stubs().observePrefs(ANYTHING);
-    this.mockHandler.stubs().coreOptionsInitialize();
+    this.mockHandler.stubs().coreOptionsUserMetricsAction(ANYTHING);
   },
 };
 
@@ -57,7 +56,7 @@ GEN('#endif  // defined(OS_MACOSX)');
 
 TEST_F('OptionsWebUITest', 'MAYBE_testSetBooleanPrefTriggers', function() {
   // TODO(dtseng): make generic to click all buttons.
-  var showHomeButton = $('toolbar-show-home-button');
+  var showHomeButton = $('show-home-button');
   var trueListValue = [
     'browser.show_home_button',
     true,
@@ -73,19 +72,18 @@ TEST_F('OptionsWebUITest', 'MAYBE_testSetBooleanPrefTriggers', function() {
 
 // Not meant to run on ChromeOS at this time.
 // Not finishing in windows. http://crbug.com/81723
-// TODO(csilv): Redo logic for uber page.
-TEST_F('OptionsWebUITest', 'DISABLED_testRefreshStaysOnCurrentPage',
+TEST_F('OptionsWebUITest', 'testRefreshStaysOnCurrentPage',
     function() {
-  var item = $('advancedPageNav');
-  item.onclick();
+  assertTrue($('search-engine-manager-page').hidden);
+  var item = $('manage-default-search-engines');
+  item.click();
+
+  assertFalse($('search-engine-manager-page').hidden);
+
   window.location.reload();
-  var pageInstance = AdvancedOptions.getInstance();
-  var topPage = OptionsPage.getTopmostVisiblePage();
-  var expectedTitle = pageInstance.title;
-  var actualTitle = document.title;
-  assertEquals("chrome://settings/advanced", document.location.href);
-  assertEquals(expectedTitle, actualTitle);
-  assertEquals(pageInstance, topPage);
+
+  assertEquals('chrome://settings-frame/searchEngines', document.location.href);
+  assertFalse($('search-engine-manager-page').hidden);
 });
 
 /**
