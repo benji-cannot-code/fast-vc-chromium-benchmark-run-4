4734a43d1c1ef55c272f7fec76fe0d108c956e83
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-21 18:28:48 +0000

    Removing HTMLTrackElement does not delete TextTrack https://bugs.webkit.org/show_bug.cgi?id=80873
    Reviewed by Antti Koivisto.
    
    Source/WebCore: 
    
    No new tests, but media/track/track-language-preference.html has been updated
    to test this fix.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::didAddTrack): Renamed from trackWasAdded.
    (WebCore::HTMLMediaElement::willRemoveTrack): Renamed from trackWasRemoved because it is
    called before the track is removed, to allow the TextTrack to be removed.
    * html/HTMLMediaElement.h:
    
    * html/HTMLTrackElement.cpp:
    (WebCore::HTMLTrackElement::insertedIntoTree): Renamed from insertedIntoDocument
    (WebCore::HTMLTrackElement::willRemove): Renamed from removedFromDocument.
    * html/HTMLTrackElement.h:
    
    * html/track/LoadableTextTrack.cpp:
    (WebCore::LoadableTextTrack::trackElementIndex): Don't include tracks that are not in the
    document in the calcualtion.
    
    LayoutTests: 
    
    * media/track/track-language-preference-expected.txt:
    * media/track/track-language-preference.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -2491,7 +2491,7 @@ HTMLTrackElement* HTMLMediaElement::showingTrackWithSameKind(HTMLTrackElement* t
     return 0;
 }
 
-void HTMLMediaElement::trackWasAdded(HTMLTrackElement* trackElement)
+void HTMLMediaElement::didAddTrack(HTMLTrackElement* trackElement)
 {
     ASSERT(trackElement->hasTagName(trackTag));
 
@@ -2514,7 +2514,7 @@ void HTMLMediaElement::trackWasAdded(HTMLTrackElement* trackElement)
         scheduleLoad(TextTrackResource);
 }
 
-void HTMLMediaElement::trackWasRemoved(HTMLTrackElement* trackElement)
+void HTMLMediaElement::willRemoveTrack(HTMLTrackElement* trackElement)
 {
     ASSERT(trackElement->hasTagName(trackTag));
 
@@ -2524,7 +2524,7 @@ void HTMLMediaElement::trackWasRemoved(HTMLTrackElement* trackElement)
 #if !LOG_DISABLED
     if (trackElement->hasTagName(trackTag)) {
         KURL url = trackElement->getNonEmptyURLAttribute(srcAttr);
-        LOG(Media, "HTMLMediaElement::trackWasRemoved - 'src' is %s", urlForLogging(url).utf8().data());
+        LOG(Media, "HTMLMediaElement::willRemoveTrack - 'src' is %s", urlForLogging(url).utf8().data());
     }
 #endif
 
