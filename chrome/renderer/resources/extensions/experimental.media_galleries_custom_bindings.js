776c6da9751abc22d7cefaae4f198a38abe07b1b
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 23:17:34 +0000

    Initial extension bindings for Media Gallery API.
    BUG=NONE
    TEST=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/9358076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128089 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/renderer/resources/extensions/experimental.media_galleries_custom_bindings.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Custom bindings for the Media Gallery API.
+
+var mediaGalleriesNatives = requireNative('experimental_mediaGalleries');
+var CreateMediaGalleryObject = mediaGalleriesNatives.CreateMediaGalleryObject;
+
+var chromeHidden = requireNative('chrome_hidden').GetChromeHidden();
+
+chromeHidden.registerCustomHook('experimental.mediaGalleries',
+                                function(bindingsAPI, extensionId) {
+  var apiFunctions = bindingsAPI.apiFunctions;
+
+  apiFunctions.setCustomCallback('getMediaGalleries',
+                                 function(name, request, response) {
+    var result = [];
+    var resp = response ? chromeHidden.JSON.parse(response) : [];
+    for (var i = 0; i < resp.length; i++) {
+      result.push(CreateMediaGalleryObject(resp[i].id, resp[i].name,
+                                           resp[i].flags));
+
+    }
+    if (request.callback)
+      request.callback(result);
+    request.callback = null;
+  });
+});
