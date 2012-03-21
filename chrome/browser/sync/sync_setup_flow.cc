f9e5f0db3b19cb3234650646ea1afed723bd2355
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 23:16:55 +0000

    [Sync] Remove SET_PASSPHRASE_FAILED reason.
    We only call OnPassphraseRequired if the cryptographer is not ready, and
    we set the reason based on whether there are pending keys.
    
    Also make sure SetDecryptionPassphrase properly handles return value.
    
    BUG=118243
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9796001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/sync_setup_flow.cc
+++ chrome/browser/sync/sync_setup_flow.cc
@@ -355,11 +355,13 @@ void SyncSetupFlow::OnUserConfigured(const SyncConfiguration& configuration) {
     // Caller passed a gaia passphrase. This is illegal if we are currently
     // using a secondary passphrase.
     DCHECK(!service_->IsUsingSecondaryPassphrase());
-    service_->SetDecryptionPassphrase(configuration.gaia_passphrase);
+
     // Since the user entered the passphrase manually, set this flag so we can
     // report an error if the passphrase setting failed.
     user_tried_setting_passphrase_ = true;
-    set_new_decryption_passphrase = true;
+    if (service_->SetDecryptionPassphrase(configuration.gaia_passphrase)) {
+      set_new_decryption_passphrase = true;
+    }
   }
 
   // Set the secondary passphrase, either as a decryption passphrase, or
@@ -373,11 +375,9 @@ void SyncSetupFlow::OnUserConfigured(const SyncConfiguration& configuration) {
     // "enter passphrase" dialog without sending the passphrase to the syncer
     // thread.
     if (service_->IsPassphraseRequiredForDecryption()) {
-      if (!service_->SetDecryptionPassphrase(
-          configuration.secondary_passphrase)) {
-        user_tried_setting_passphrase_ = true;
-        Advance(SyncSetupWizard::ENTER_PASSPHRASE);
-        return;
+      if (service_->SetDecryptionPassphrase(
+              configuration.secondary_passphrase)) {
+        set_new_decryption_passphrase = true;
       }
     } else {
       service_->SetEncryptionPassphrase(configuration.secondary_passphrase,
@@ -385,7 +385,6 @@ void SyncSetupFlow::OnUserConfigured(const SyncConfiguration& configuration) {
     }
     if (service_->IsUsingSecondaryPassphrase()) {
       user_tried_setting_passphrase_ = true;
-      set_new_decryption_passphrase = true;
     } else {
       user_tried_creating_explicit_passphrase_ = true;
     }
@@ -413,8 +412,10 @@ void SyncSetupFlow::OnUserConfigured(const SyncConfiguration& configuration) {
 
 void SyncSetupFlow::OnPassphraseEntry(const std::string& passphrase) {
   Advance(SyncSetupWizard::SETTING_UP);
-  service_->SetDecryptionPassphrase(passphrase);
   user_tried_setting_passphrase_ = true;
+  if (!service_->SetDecryptionPassphrase(passphrase)) {
+    Advance(SyncSetupWizard::ENTER_PASSPHRASE);
+  }
 }
 
 void SyncSetupFlow::OnPassphraseCancel() {
