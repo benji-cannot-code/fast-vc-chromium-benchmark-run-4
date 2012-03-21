41ec7bf1943ac6b7f0adbaac0006bf8d103c68e7
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 23:42:54 +0000

    Update Gdata system notification only when necessary.
    BUG=chromium-os:28162
    TEST=manual.
    
    Review URL: https://chromiumcodereview.appspot.com/9815012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128105 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_event_router.cc
+++ chrome/browser/chromeos/extensions/file_browser_event_router.cc
@@ -84,7 +84,8 @@ FileBrowserEventRouter::FileBrowserEventRouter(
     : delegate_(new FileBrowserEventRouter::FileWatcherDelegate(this)),
       notifications_(new FileBrowserNotifications(profile)),
       profile_(profile),
-      current_gdata_operation_failed_(false) {
+      current_gdata_operation_failed_(false),
+      last_active_gdata_operation_count_(0) {
 }
 
 FileBrowserEventRouter::~FileBrowserEventRouter() {
@@ -274,9 +275,12 @@ void FileBrowserEventRouter::HandleProgressUpdateForSystemNotification(
         !current_gdata_operation_failed_);
     current_gdata_operation_failed_ = false;
   } else {
-    notifications_->ManageNotificationOnGDataSyncProgress(
-        active_operation_count);
+    if (last_active_gdata_operation_count_ != active_operation_count) {
+      notifications_->ManageNotificationOnGDataSyncProgress(
+          active_operation_count);
+    }
   }
+  last_active_gdata_operation_count_ = active_operation_count;
 }
 
 void FileBrowserEventRouter::HandleFileWatchNotification(
