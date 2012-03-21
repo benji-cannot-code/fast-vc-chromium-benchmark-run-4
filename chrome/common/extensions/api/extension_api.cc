963d2cf3252dbb50c77814bbfc1c552c59dec123
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 05:40:14 +0000

    Remove dependence on .json files for IDL-specified extensions/apps APIs
    This is an incremental step towards fully IDL-specified API bindings. This
    patch adds code which generates JSON based on IDL for the purpose of exposing
    bindings to javascript.
    
    BUG=119137
    TEST=Code should compile and existing tests should pass.
    
    
    Review URL: http://codereview.chromium.org/9774001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127908 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/api/extension_api.cc
+++ chrome/common/extensions/api/extension_api.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_split.h"
 #include "base/string_util.h"
 #include "base/values.h"
+#include "chrome/common/extensions/api/generated_schemas.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_permission_set.h"
 #include "googleurl/src/gurl.h"
@@ -67,11 +68,16 @@ static base::ListValue* LoadSchemaList(int resource_id) {
 }
 
 void ExtensionAPI::LoadSchemaFromResource(int resource_id) {
-  scoped_ptr<base::ListValue> loaded(LoadSchemaList(resource_id));
+  RegisterSchema(LoadSchemaList(resource_id));
+}
+
+void ExtensionAPI::RegisterSchema(base::ListValue* loaded_schema) {
+  // We take ownership of loaded_schema, so we need to delete it.
+  scoped_ptr<base::ListValue> scoped_loaded_schema(loaded_schema);
   Value* value = NULL;
   std::string schema_namespace;
-  while (!loaded->empty()) {
-    loaded->Remove(loaded->GetSize() - 1, &value);
+  while (!loaded_schema->empty()) {
+    loaded_schema->Remove(loaded_schema->GetSize() - 1, &value);
     CHECK(value->IsType(Value::TYPE_DICTIONARY));
     const DictionaryValue* schema = static_cast<const DictionaryValue*>(value);
     CHECK(schema->GetString("namespace", &schema_namespace));
@@ -97,7 +103,6 @@ ExtensionAPI::ExtensionAPI() {
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_APP,
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_BOOKMARKMANAGER,
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_DECLARATIVE,
-    IDR_EXTENSION_API_JSON_EXPERIMENTAL_DNS,
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_DOWNLOADS,
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_EXTENSIONS,
     IDR_EXTENSION_API_JSON_EXPERIMENTAL_FONTS,
@@ -149,6 +154,7 @@ ExtensionAPI::ExtensionAPI() {
   for (size_t i = 0; i < arraysize(kJsonApiResourceIds); i++) {
     LoadSchemaFromResource(kJsonApiResourceIds[i]);
   }
+  RegisterSchema(api::GeneratedSchemas::Get());
 
   // Populate {completely,partially}_unprivileged_apis_.
   for (SchemaMap::iterator it = schemas_.begin(); it != schemas_.end(); ++it) {
