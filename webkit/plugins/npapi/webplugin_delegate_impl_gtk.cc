18caa53fc1e7bbdc6e73960ba15dcc370bffc075
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 15:07:12 +0000

    Prevent npapi plugin paint when per-tile painting is used.
    BUG=114410
    TEST=webkit layout test: compositing/plugins/invalidate_rect.html
    
    
    Review URL: http://codereview.chromium.org/9764007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127960 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugin_delegate_impl_gtk.cc
+++ webkit/plugins/npapi/webplugin_delegate_impl_gtk.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -111,7 +111,7 @@ void WebPluginDelegateImpl::PlatformDestroyInstance() {
 
 void WebPluginDelegateImpl::Paint(WebKit::WebCanvas* canvas,
                                   const gfx::Rect& rect) {
-  if (!windowless_)
+  if (!windowless_ || !skia::SupportsPlatformPaint(canvas))
     return;
   skia::ScopedPlatformPaint scoped_platform_paint(canvas);
   cairo_t* context = scoped_platform_paint.GetPlatformSurface();
