a8959add06ec69dc8d33e45ec0f2dde38ac22d29
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 14:22:23 +0000

    Disable background animations on login
    BUG=119113
    TEST=Should see no animations on login
    
    
    Review URL: http://codereview.chromium.org/9761003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/base_login_display_host.cc
+++ chrome/browser/chromeos/login/base_login_display_host.cc
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Whether sign in transitions are enabled.
-const bool kEnableBackgroundAnimation = true;
+const bool kEnableBackgroundAnimation = false;
 const bool kEnableBrowserWindowsOpacityAnimation = true;
 const bool kEnableBrowserWindowsTransformAnimation = true;
 
