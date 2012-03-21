99f93eca85f13daa805b47d99679f28af59f3d1e
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 05:49:58 +0000

    Remove test code that was checked in by accident
    TBR=ben@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9811001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/chrome_browser_main_extra_parts_ash.cc
@@ -36,7 +36,6 @@ ChromeBrowserMainExtraPartsAsh::ChromeBrowserMainExtraPartsAsh()
 
 void ChromeBrowserMainExtraPartsAsh::PreProfileInit() {
 #if defined(OS_CHROMEOS)
-  aura::MonitorManager::set_use_fullscreen_host_window(true);
   if (base::chromeos::IsRunningOnChromeOS() ||
       CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kAuraHostWindowUseFullscreen)) {
