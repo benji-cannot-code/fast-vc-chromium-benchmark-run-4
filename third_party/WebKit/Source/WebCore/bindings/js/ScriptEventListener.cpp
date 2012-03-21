edd916f1b7e0b5cfd1ae1f2efdf2a260d9118268
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-21 17:49:51 +0000

    Web Inspector: only show JS event listeners in Event Listeners tab of Elements panel https://bugs.webkit.org/show_bug.cgi?id=81798
    Reviewed by Pavel Feldman.
    
    * bindings/js/JSInjectedScriptHostCustom.cpp:
    (WebCore::getJSListenerFunctions):
    * bindings/js/ScriptEventListener.cpp:
    (WebCore::eventListenerHandlerBody):
    (WebCore::eventListenerHandlerLocation):
    * bindings/v8/custom/V8InjectedScriptHostCustom.cpp:
    (WebCore::getJSListenerFunctions):
    * inspector/InspectorDOMAgent.cpp:
    (WebCore::InspectorDOMAgent::getEventListeners):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/ScriptEventListener.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptEventListener.cpp
@@ -98,6 +98,7 @@ PassRefPtr<JSLazyEventListener> createAttributeEventListener(Frame* frame, Attri
 String eventListenerHandlerBody(Document* document, EventListener* eventListener)
 {
     const JSEventListener* jsListener = JSEventListener::cast(eventListener);
+    ASSERT(jsListener);
     if (!jsListener)
         return "";
     JSLock lock(SilenceAssertionsOnly);
@@ -111,6 +112,7 @@ String eventListenerHandlerBody(Document* document, EventListener* eventListener
 bool eventListenerHandlerLocation(Document* document, EventListener* eventListener, String& sourceName, int& lineNumber)
 {
     const JSEventListener* jsListener = JSEventListener::cast(eventListener);
+    ASSERT(jsListener);
     if (!jsListener)
         return false;
     JSC::JSObject* jsObject = jsListener->jsFunction(document);
