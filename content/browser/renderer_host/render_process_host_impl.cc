e6533c87771c6b61ec02818b2a3338a6d628e316
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 13:17:48 +0000

    Speech JavaScript API: add --enable-scripted-speech
    This flag doesn't do much at the moment, but will be used to enable
    the new speech API that's being implemented.
    
    BUG=116954
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9748020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127947 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -678,6 +678,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableSessionStorage,
     switches::kDisableSharedWorkers,
     switches::kDisableSpeechInput,
+    switches::kEnableScriptedSpeech,
     switches::kDisableWebAudio,
     switches::kDisableWebSockets,
     switches::kDomAutomationController,
--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -187,6 +187,9 @@ const char kDisableSiteSpecificQuirks[]     = "disable-site-specific-quirks";
 // Disables speech input.
 const char kDisableSpeechInput[]            = "disable-speech-input";
 
+// Enables scripted speech api.
+const char kEnableScriptedSpeech[]          = "enable-scripted-speech";
+
 // Disable web audio API.
 const char kDisableWebAudio[]               = "disable-webaudio";
 
