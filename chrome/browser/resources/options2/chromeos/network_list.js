126b7f958a07ca41923f1c6b5447dc0c796b0c75
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 03:52:49 +0000

    Add policy managed indicator to network controls.
    BUG=chromium:118163
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9749006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/chromeos/network_list.js
+++ chrome/browser/resources/options2/chromeos/network_list.js
@@ -206,6 +206,13 @@ cr.define('options.network', function() {
       this.subtitle_.classList.add('network-selector');
     },
 
+    /**
+     * Adds an indicator to show that the network is policy managed.
+     */
+    showManagedNetworkIndicator: function() {
+      this.appendChild(new ManagedNetworkIndicator());
+    },
+
     /* @inheritDoc */
     decorate: function() {
       ListItem.prototype.decorate.call(this);
@@ -346,6 +353,7 @@ cr.define('options.network', function() {
     /* @inheritDoc */
     decorate: function() {
       // TODO(kevers): Generalize method of setting default label.
+      var policyManaged = false;
       var defaultMessage = this.data_.key == 'wifi' ?
           'networkOffline' : 'networkNotConnected';
       this.subtitle = templateData[defaultMessage];
@@ -355,6 +363,7 @@ cr.define('options.network', function() {
         var networkDetails = list[i];
         if (networkDetails.connecting || networkDetails.connected) {
           this.subtitle = networkDetails.networkName;
+          policyManaged = networkDetails.policyManaged;
           candidateURL = networkDetails.iconURL;
           // Only break when we see a connecting network as it is possible to
           // have a connected network and a connecting network at the same
@@ -372,6 +381,9 @@ cr.define('options.network', function() {
         this.iconType = this.data.key;
 
       this.showSelector();
+
+      if (policyManaged)
+        this.showManagedNetworkIndicator();
 
       // TODO(kevers): Add default icon for VPN when disconnected or in the
       // process of connecting.
