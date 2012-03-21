f8be6e1515fddfe335f5600c469475497bd5cea7
rbyers@chromium.org <rbyers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 01:26:11 +0000

    Add drag and drop support for custom and html data types on Aura
    Also unifies OSExchangeData::CustomFormat and Clipboard::FormatType on Aura so
    we don't need two separate code paths and mime type lists for getting specific
    custom formats.
    
    Note that this means that on some platforms CustomFormat has a dtor (since it
    can hold, eg. a std::string), so we can no longer use static/global
    CustomFormat variables.  In the two places we were doing that I switched to use
    CR_DEFINE_STATIC_LOCAL to allow the static to leak, exactly as we're already
    doing for each implementation of Clipboard::Get*FormatType().
    
    BUG=118416
    TEST=See bug - no automated tests exist for any of this yet :-(
    
    
    Review URL: http://codereview.chromium.org/9770001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127866 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/dragdrop/os_exchange_data.cc
+++ ui/base/dragdrop/os_exchange_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -86,6 +86,8 @@ bool OSExchangeData::HasAllFormats(
 #if defined(OS_WIN)
   if ((formats & FILE_CONTENTS) != 0 && !provider_->HasFileContents())
     return false;
+#endif
+#if defined(OS_WIN) || defined(USE_AURA)
   if ((formats & HTML) != 0 && !provider_->HasHtml())
     return false;
 #endif
@@ -109,6 +111,8 @@ bool OSExchangeData::HasAnyFormat(
 #if defined(OS_WIN)
   if ((formats & FILE_CONTENTS) != 0 && provider_->HasFileContents())
     return true;
+#endif
+#if defined(OS_WIN) || defined(USE_AURA)
   if ((formats & HTML) != 0 && provider_->HasHtml())
     return true;
 #endif
@@ -128,21 +132,23 @@ void OSExchangeData::SetFileContents(const FilePath& filename,
   provider_->SetFileContents(filename, file_contents);
 }
 
-void OSExchangeData::SetHtml(const string16& html, const GURL& base_url) {
-  provider_->SetHtml(html, base_url);
-}
-
 bool OSExchangeData::GetFileContents(FilePath* filename,
                                      std::string* file_contents) const {
   return provider_->GetFileContents(filename, file_contents);
 }
 
-bool OSExchangeData::GetHtml(string16* html, GURL* base_url) const {
-  return provider_->GetHtml(html, base_url);
-}
-
 void OSExchangeData::SetDownloadFileInfo(const DownloadFileInfo& download) {
   return provider_->SetDownloadFileInfo(download);
+}
+#endif
+
+#if defined(OS_WIN) || defined(USE_AURA)
+void OSExchangeData::SetHtml(const string16& html, const GURL& base_url) {
+  provider_->SetHtml(html, base_url);
+}
+
+bool OSExchangeData::GetHtml(string16* html, GURL* base_url) const {
+  return provider_->GetHtml(html, base_url);
 }
 #endif
 
