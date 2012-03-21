0f3ecaf308e16aadf2888feadece9eb495542031
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 07:23:39 +0000

    Add metrics for counting # of origins per filesystem type
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9764015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127921 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/sandbox_mount_point_provider.cc
+++ webkit/fileapi/sandbox_mount_point_provider.cc
@@ -43,7 +43,7 @@ const size_t kOldFileSystemUniqueLength = 16;
 const size_t kOldFileSystemUniqueDirectoryNameLength =
     kOldFileSystemUniqueLength + arraysize(kOldFileSystemUniqueNamePrefix) - 1;
 
-const char kOpenFileSystem[] = "FileSystem.OpenFileSystem";
+const char kOpenFileSystemLabel[] = "FileSystem.OpenFileSystem";
 enum FileSystemError {
   kOK = 0,
   kIncognito,
@@ -51,6 +51,9 @@ enum FileSystemError {
   kCreateDirectoryError,
   kFileSystemErrorMax,
 };
+
+const char kTemporaryOriginsCountLabel[] = "FileSystem.TemporaryOriginsCount";
+const char kPersistentOriginsCountLabel[] = "FileSystem.PersistentOriginsCount";
 
 // Restricted names.
 // http://dev.w3.org/2009/dap/file-system/file-dir-sys.html#naming-restrictions
@@ -276,13 +279,13 @@ void ValidateRootOnFileThread(ObfuscatedFileUtil* file_util,
   FilePath root_path =
       file_util->GetDirectoryForOriginAndType(origin_url, type, create);
   if (root_path.empty()) {
-    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystem,
+    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystemLabel,
                               kCreateDirectoryError,
                               kFileSystemErrorMax);
     // TODO(kinuko): We should return appropriate error code.
     *error_ptr = base::PLATFORM_FILE_ERROR_FAILED;
   } else {
-    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystem, kOK, kFileSystemErrorMax);
+    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystemLabel, kOK, kFileSystemErrorMax);
     *error_ptr = base::PLATFORM_FILE_OK;
   }
   // The reference of file_util will be derefed on the FILE thread
@@ -330,7 +333,7 @@ void SandboxMountPointProvider::ValidateFileSystemRoot(
   if (file_system_options_.is_incognito()) {
     // TODO(kinuko): return an isolated temporary directory.
     callback.Run(base::PLATFORM_FILE_ERROR_SECURITY);
-    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystem,
+    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystemLabel,
                               kIncognito,
                               kFileSystemErrorMax);
     return;
@@ -338,7 +341,7 @@ void SandboxMountPointProvider::ValidateFileSystemRoot(
 
   if (!IsAllowedScheme(origin_url)) {
     callback.Run(base::PLATFORM_FILE_ERROR_SECURITY);
-    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystem,
+    UMA_HISTOGRAM_ENUMERATION(kOpenFileSystemLabel,
                               kInvalidScheme,
                               kFileSystemErrorMax);
     return;
@@ -488,6 +491,10 @@ void SandboxMountPointProvider::GetOriginsForTypeOnFileThread(
     if (enumerator->HasFileSystemType(type))
       origins->insert(origin);
   }
+  UMA_HISTOGRAM_COUNTS((type == kFileSystemTypeTemporary
+                            ? kTemporaryOriginsCountLabel
+                            : kPersistentOriginsCountLabel),
+                       origins->size());
 }
 
 void SandboxMountPointProvider::GetOriginsForHostOnFileThread(
