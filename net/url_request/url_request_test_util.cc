a1cf8713e8dd57be810d19300ee1f74ebbd16e0c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 01:47:12 +0000

    Remove an unnecessary gtest.h include.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9766006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127873 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_test_util.cc
+++ net/url_request/url_request_test_util.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_network_session.h"
 #include "net/http/http_server_properties_impl.h"
 #include "net/url_request/url_request_job_factory.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 
--- net/url_request/url_request_test_util.h
+++ net/url_request/url_request_test_util.h
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_context_storage.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 using base::TimeDelta;
 
