17b35eb87035d52258b2379d76c89a0066528be8
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 17:45:43 +0000

    Touch Lock is based on proximity to other touches.
    The Gesture Recognizer now uses a separate Gesture Sequence for each Window. 
    It also stores a mapping from touch id to Window.
    
    When a new touch comes in:
    
    If there is a capture window, it is associated with the capture window.
    
    Otherwise, if the Gesture Recognizer already has the touch id associated with a target,
    the new touch is associated with the same target.
    
    If not, the GestureRecognizer looks at all other current touches, to see if any is within a 
    certain radius of the new touch. If there is a nearby touch, the new touch is associated 
    with the closest touch.
    
    If a target still can't be found, the touch is associated with the window it occurred above.
    
    BUG=116585
    TEST=GestureRecognizerTest.GestureEventTouchLockSelectsCorrectWindow
    
    
    Review URL: http://codereview.chromium.org/9666004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127997 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_sequence.cc
+++ ui/aura/gestures/gesture_sequence.cc
@@ -172,8 +172,10 @@ GestureSequence::Gestures* GestureSequence::ProcessTouchEventForGesture(
     // this could occur: crbug.com/116537
     // TODO(tdresser): Enable this DCHECK, and remove the following condition
     // DCHECK(!points_[event.touch_id()].in_use());
-    if (!points_[event.touch_id()].in_use())
+    if (!points_[event.touch_id()].in_use()) {
       new_point->set_point_id(point_count_++);
+      new_point->set_touch_id(event.touch_id());
+    }
   }
 
   GestureState last_state = state_;
@@ -256,7 +258,8 @@ GestureSequence::Gestures* GestureSequence::ProcessTouchEventForGesture(
   // When a touch point is released, all points with ids greater than the
   // released point must have their ids decremented, or the set of point_ids
   // could end up with gaps.
-  if (event.type() == ui::ET_TOUCH_RELEASED) {
+  if (event.type() == ui::ET_TOUCH_RELEASED ||
+      event.type() == ui::ET_TOUCH_CANCELLED) {
     GesturePoint& old_point = points_[event.touch_id()];
     for (int i = 0; i < kMaxGesturePoints; ++i) {
       GesturePoint& point = points_[i];
@@ -310,7 +313,7 @@ void GestureSequence::AppendTapDownGestureEvent(const GesturePoint& point,
       point.first_touch_position().y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      0.f, 0.f)));
+      0.f, 0.f, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendClickGestureEvent(const GesturePoint& point,
@@ -321,7 +324,7 @@ void GestureSequence::AppendClickGestureEvent(const GesturePoint& point,
       point.first_touch_position().y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      0.f, 0.f)));
+      0.f, 0.f, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendDoubleClickGestureEvent(const GesturePoint& point,
@@ -332,7 +335,7 @@ void GestureSequence::AppendDoubleClickGestureEvent(const GesturePoint& point,
       point.first_touch_position().y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      0.f, 0.f)));
+      0.f, 0.f, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendScrollGestureBegin(const GesturePoint& point,
@@ -344,7 +347,7 @@ void GestureSequence::AppendScrollGestureBegin(const GesturePoint& point,
       location.y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      0.f, 0.f)));
+      0.f, 0.f, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendScrollGestureEnd(const GesturePoint& point,
@@ -366,7 +369,7 @@ void GestureSequence::AppendScrollGestureEnd(const GesturePoint& point,
       location.y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      railed_x_velocity, railed_y_velocity)));
+      railed_x_velocity, railed_y_velocity, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendScrollGestureUpdate(const GesturePoint& point,
@@ -386,7 +389,7 @@ void GestureSequence::AppendScrollGestureUpdate(const GesturePoint& point,
       location.y(),
       flags_,
       base::Time::FromDoubleT(point.last_touch_time()),
-      dx, dy)));
+      dx, dy, 1 << point.touch_id())));
 }
 
 void GestureSequence::AppendPinchGestureBegin(const GesturePoint& p1,
@@ -399,7 +402,7 @@ void GestureSequence::AppendPinchGestureBegin(const GesturePoint& p1,
       center.y(),
       flags_,
       base::Time::FromDoubleT(p1.last_touch_time()),
-      0.f, 0.f)));
+      0.f, 0.f, 1 << p1.touch_id() | 1 << p2.touch_id())));
 }
 
 void GestureSequence::AppendPinchGestureEnd(const GesturePoint& p1,
@@ -413,7 +416,7 @@ void GestureSequence::AppendPinchGestureEnd(const GesturePoint& p1,
       center.y(),
       flags_,
       base::Time::FromDoubleT(p1.last_touch_time()),
-      scale, 0.f)));
+      scale, 0.f, 1 << p1.touch_id() | 1 << p2.touch_id())));
 }
 
 void GestureSequence::AppendPinchGestureUpdate(const GesturePoint& p1,
@@ -429,7 +432,7 @@ void GestureSequence::AppendPinchGestureUpdate(const GesturePoint& p1,
       center.y(),
       flags_,
       base::Time::FromDoubleT(p1.last_touch_time()),
-      scale, 0.f)));
+      scale, 0.f, 1 << p1.touch_id() | 1 << p2.touch_id())));
 }
 
 bool GestureSequence::Click(const TouchEvent& event,
@@ -507,7 +510,7 @@ void GestureSequence::AppendLongPressGestureEvent() {
       point->first_touch_position().y(),
       flags_,
       base::Time::FromDoubleT(point->last_touch_time()),
-      point->point_id(), 0.f);
+      point->point_id(), 0.f, 1 << point->touch_id());
   root_window_->DispatchGestureEvent(&gesture);
 }
 
