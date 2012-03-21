3d8e7ef0feb048adbfc4a8b57702bcc0e3dab266
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 02:49:55 +0000

    Add Chrome To Mobile switches and about:flags entry. Enable the feature by default & respect the disable switch.
    Stable may disable by default & respect the enable switch.
    Add the needed switches/strings now, before string freeze.
    
    Add flags and strings for [en|dis]abling Chrome To Mobile.
    Add about:flags entry for disabling Chrome To Mobile.
    Add UMA data collection for the new disable switch.
    Add a static Service function to get the feature enabled state.
    Respect the switches in the factory and location bar call sites.
    
    BUG=102709
    TEST=Chrome To Mobile enable/disable flag and switches work.
    
    Review URL: http://codereview.chromium.org/9727008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_to_mobile_service_factory.cc
+++ chrome/browser/chrome_to_mobile_service_factory.cc
@@ -23,6 +23,10 @@ ChromeToMobileService* ChromeToMobileServiceFactory::GetForProfile(
 
 ProfileKeyedService* ChromeToMobileServiceFactory::BuildServiceInstanceFor(
     Profile* profile) const {
+  // Ensure that the service is not instantiated or used if it is disabled.
+  if (!ChromeToMobileService::IsChromeToMobileEnabled())
+    return NULL;
+
   return new ChromeToMobileService(profile);
 }
 
