615248a3707f3966b9789c422f519d24171f9823
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-21 01:19:23 +0000

    Infinite repaint loop with SVGImageCache and deferred repaint timers https://bugs.webkit.org/show_bug.cgi?id=78315 <rdar://problem/10651634>
    Patch by Tim Horton <timothy_horton@apple.com> on 2012-03-18
    Reviewed by Nikolas Zimmermann.
    
    Only defer image redraw on a timer if we're in layout. This breaks
    the repaint loop while still preventing us from drawing inside layout.
    
    Completely disable repaint during relayout inside SVGImage::drawSVGToImageBuffer,
    preventing deferred repaint timers from being started during that process.
    
    No new tests, as the problem only occurs in a nonstandard configuration.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    (WebCore::FrameView::reset):
    (WebCore::FrameView::repaintContentRectangle):
    (WebCore::FrameView::endDeferredRepaints):
    (WebCore::FrameView::startDeferredRepaintTimer):
    (WebCore):
    (WebCore::FrameView::doDeferredRepaints):
    (WebCore::FrameView::deferredRepaintTimerFired):
    (WebCore::FrameView::beginDisableRepaints):
    (WebCore::FrameView::endDisableRepaints):
    * page/FrameView.h:
    (FrameView):
    (WebCore::FrameView::repaintsDisabled):
    * rendering/RenderView.cpp:
    (WebCore::RenderView::shouldRepaint):
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::drawSVGToImageBuffer):
    (WebCore::SVGImage::draw):
    (WebCore::SVGImage::frameView):
    (WebCore):
    * svg/graphics/SVGImage.h:
    (WebCore):
    * svg/graphics/SVGImageCache.cpp:
    (WebCore::SVGImageCache::imageContentChanged):
    (WebCore::SVGImageCache::redraw):
    (WebCore::SVGImageCache::redrawTimerFired):
    (WebCore):
    * svg/graphics/SVGImageCache.h:
    (SVGImageCache):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111480 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/WebCore/svg/graphics/SVGImage.cpp
@@ -178,10 +178,15 @@ void SVGImage::drawSVGToImageBuffer(ImageBuffer* buffer, const IntSize& size, fl
     ImageObserver* observer = imageObserver();
     ASSERT(observer);
 
-    // Temporarily reset image observer, we don't want to receive any changeInRect() calls due this relayout.
+    // Temporarily reset image observer, we don't want to receive any changeInRect() calls due to this relayout.
     setImageObserver(0);
+
+    // Disable repainting; we don't want deferred repaints to schedule any timers due to this relayout.
+    frame->view()->beginDisableRepaints();
+
     renderer->setContainerSize(size);
     frame->view()->resize(this->size());
+
     if (zoom != 1)
         frame->setPageZoomFactor(zoom);
 
@@ -203,7 +208,9 @@ void SVGImage::drawSVGToImageBuffer(ImageBuffer* buffer, const IntSize& size, fl
     if (frame->view()->needsLayout())
         frame->view()->layout();
 
-    setImageObserver(observer); 
+    setImageObserver(observer);
+
+    frame->view()->endDisableRepaints();
 }
 
 void SVGImage::draw(GraphicsContext* context, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace, CompositeOperator compositeOp)
@@ -211,7 +218,7 @@ void SVGImage::draw(GraphicsContext* context, const FloatRect& dstRect, const Fl
     if (!m_page)
         return;
 
-    FrameView* view = m_page->mainFrame()->view();
+    FrameView* view = frameView();
 
     GraphicsContextStateSaver stateSaver(*context);
     context->setCompositeOperation(compositeOp);
@@ -254,6 +261,14 @@ RenderBox* SVGImage::embeddedContentBox() const
     if (!rootElement)
         return 0;
     return toRenderBox(rootElement->renderer());
+}
+
+FrameView* SVGImage::frameView() const
+{
+    if (!m_page)
+        return 0;
+
+    return m_page->mainFrame()->view();
 }
 
 bool SVGImage::hasRelativeWidth() const
