f162504a74a7f120d89f3159d7f763b2dec75046
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-21 07:01:10 +0000

    PPAPI: Remove out-dated or unnecessary TODOs from public files.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9731003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127918 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_graphics_2d.h
+++ ppapi/c/ppb_graphics_2d.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_graphics_2d.idl modified Tue Jan 24 16:03:48 2012. */
+/* From ppb_graphics_2d.idl modified Mon Mar 19 11:35:04 2012. */
 
 #ifndef PPAPI_C_PPB_GRAPHICS_2D_H_
 #define PPAPI_C_PPB_GRAPHICS_2D_H_
@@ -245,12 +245,6 @@ struct PPB_Graphics2D_1_0 {
    * <code>PP_ERROR_INPROGRESS</code> if a flush is already pending that has
    * not issued its callback yet.  In the failure case, nothing will be updated
    * and no callback will be scheduled.
-   */
-  /* TODO(darin): We should ensure that the completion callback always runs, so
-   * that it is easier for consumers to manage memory referenced by a callback.
-   *
-   * TODO(): Add back in the synchronous mode description once we have support
-   * for it.
    */
   int32_t (*Flush)(PP_Resource graphics_2d,
                    struct PP_CompletionCallback callback);
