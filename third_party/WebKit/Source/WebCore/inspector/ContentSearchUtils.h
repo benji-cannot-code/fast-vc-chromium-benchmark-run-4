f16324f7cabd5ae5648c3199f85b04bd8e08ede3
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-21 18:59:55 +0000

    Build fix for !ENABLE(INSPECTOR) after r111005.
    * inspector/ContentSearchUtils.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111587 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.h
+++ third_party/WebKit/Source/WebCore/inspector/ContentSearchUtils.h
@@ -30,9 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ContentSearchUtils_h
 #define ContentSearchUtils_h
 
+#if ENABLE(INSPECTOR)
+
 #include "InspectorTypeBuilder.h"
 #include "PlatformString.h"
-
 #include <wtf/Vector.h>
 
 namespace WebCore {
@@ -51,5 +52,7 @@ String findSourceMapURL(const String& content);
 
 } // namespace ContentSearchUtils
 } // namespace WebCore
+
+#endif // ENABLE(INSPECTOR)
 
 #endif // !defined(ContentSearchUtils_h)
