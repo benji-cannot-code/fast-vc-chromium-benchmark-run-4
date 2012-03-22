72e4a5d96cb96461ca82db27f16319edb17892b0
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 03:25:25 +0000

    Add a helpful console message in case web-inaccessible request.
    BUG=112620
    
    
    Review URL: http://codereview.chromium.org/9805004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/extension_resource_request_policy.cc
+++ chrome/renderer/extensions/extension_resource_request_policy.cc
@@ -7,18 +7,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/stringprintf.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_set.h"
 #include "googleurl/src/gurl.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebConsoleMessage.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
 
 // static
 bool ExtensionResourceRequestPolicy::CanRequestResource(
     const GURL& resource_url,
-    const WebKit::WebFrame* frame,
+    WebKit::WebFrame* frame,
     const ExtensionSet* loaded_extensions) {
   CHECK(resource_url.SchemeIs(chrome::kExtensionScheme));
 
@@ -60,8 +63,14 @@ bool ExtensionResourceRequestPolicy::CanRequestResource(
       !(page_url.SchemeIs(chrome::kChromeDevToolsScheme) &&
           !extension->devtools_url().is_empty()) &&
       !extension->IsResourceWebAccessible(resource_url.path())) {
-    LOG(ERROR) << "Denying load of " << resource_url.spec() << " which "
-               << "is not a web accessible resource.";
+    std::string message = base::StringPrintf(
+        "Denying load of %s. Resources must be listed in the "
+        "web_accessible_resources manifest key in order to be loaded by web "
+        "pages.",
+        resource_url.spec().c_str());
+    frame->addMessageToConsole(
+        WebKit::WebConsoleMessage(WebKit::WebConsoleMessage::LevelError,
+                                  WebKit::WebString::fromUTF8(message)));
     return false;
   }
 
