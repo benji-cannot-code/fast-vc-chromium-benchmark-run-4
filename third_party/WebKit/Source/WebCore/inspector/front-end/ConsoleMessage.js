2a19a0db1bb70d448f7424b9c478fbba2aac2e51
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 14:53:28 +0000

    Web Inspector: Case of the elements of the xml document should be shown as it is in the console https://bugs.webkit.org/show_bug.cgi?id=81902
    Source/WebCore:
    
    When registering a detached root, DOMAgent always presumes it is a node,
    while it can also be a document.
    
    Reviewed by Vsevolod Vlasov.
    
    Test: inspector/console/console-xml-document.html
    
    * inspector/front-end/ConsoleMessage.js:
    (WebInspector.ConsoleMessageImpl.prototype._formatParameterAsNode):
    * inspector/front-end/DOMAgent.js:
    (WebInspector.DOMAgent.prototype._setDetachedRoot):
    
    LayoutTests:
    
    Reviewed by Vsevolod Vlasov.
    
    * inspector/console/console-xml-document-expected.txt: Added.
    * inspector/console/console-xml-document.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleMessage.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleMessage.js
@@ -285,6 +285,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
             if (!treeOutline.children[0].hasChildren)
                 treeOutline.element.addStyleClass("single-node");
             elem.appendChild(treeOutline.element);
+            treeOutline.element.treeElementForTest = treeOutline.children[0];
         }
         object.pushNodeToFrontend(printNode.bind(this));
     },
