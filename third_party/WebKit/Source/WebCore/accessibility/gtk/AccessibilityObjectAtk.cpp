3eff94b2ca89dfa9802aeab3e963027027148028
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 11:17:35 +0000

    [GTK] Fix the only remaining coding style issue in GTK accessibility code https://bugs.webkit.org/show_bug.cgi?id=81885
    Patch by Mario Sanchez Prada <msanchez@igalia.com> on 2012-03-22
    Reviewed by Xan Lopez.
    
    Add needed extra line and remove an unneeded one.
    
    * accessibility/gtk/AccessibilityObjectAtk.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/gtk/AccessibilityObjectAtk.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/gtk/AccessibilityObjectAtk.cpp
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "AccessibilityObject.h"
+
 #include "RenderObject.h"
 #include "RenderText.h"
-
 #include <glib-object.h>
 
 #if HAVE(ACCESSIBILITY)
