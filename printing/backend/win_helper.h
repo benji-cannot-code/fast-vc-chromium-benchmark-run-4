39b3ee27ea4ab186438c535f203617b2ffd9cab6
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 02:21:11 +0000

    Driver related info would be included into PrinterBasicInfo::options so it would be uploaded to cloud print backend.
    BUG=118797
    TEST=none
    
    Review URL: http://codereview.chromium.org/9812002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128141 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/win_helper.h
+++ printing/backend/win_helper.h
@@ -12,12 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <prntvpt.h>
 #include <xpsprint.h>
 
+#include <string>
+
+#include "base/memory/scoped_ptr.h"
 #include "base/string16.h"
 #include "base/win/scoped_handle.h"
 #include "printing/printing_export.h"
 
 // These are helper functions for dealing with Windows Printing.
 namespace printing {
+
+struct PRINTING_EXPORT PrinterBasicInfo;
 
 class PrinterHandleTraits {
  public:
@@ -120,6 +125,11 @@ class PRINTING_EXPORT XPSPrintModule {
   XPSPrintModule() { }
   static bool InitImpl();
 };
+
+PRINTING_EXPORT bool InitBasicPrinterInfo(HANDLE printer,
+                                          PrinterBasicInfo* printer_info);
+
+PRINTING_EXPORT std::string GetDriverInfo(HANDLE printer);
 
 }  // namespace printing
 
