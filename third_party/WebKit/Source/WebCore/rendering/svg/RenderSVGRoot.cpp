4a29d738cfd250423d75cdbf8d13f97d3d85bb69
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 00:31:03 +0000

    Build fix for ENABLE(SVG) && !ENABLE(FILTERS) after r111601.
    * rendering/svg/RenderSVGRoot.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGRoot.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGRoot.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderPart.h"
 #include "RenderSVGContainer.h"
 #include "RenderSVGResource.h"
+#include "RenderSVGResourceContainer.h"
 #include "RenderView.h"
 #include "SVGLength.h"
 #include "SVGRenderingContext.h"
