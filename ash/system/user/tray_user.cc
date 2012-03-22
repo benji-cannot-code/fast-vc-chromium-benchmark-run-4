6503c3a0b81553ff25587485b13c4fcd74a7040b
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 22:40:16 +0000

    ash: User avatar/profile pic in the tray shouldn't look horrendous when resized to a smaller size.
    BUG=110130
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9834026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128318 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/tray_user.cc
+++ ash/system/user/tray_user.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_constants.h"
 #include "base/utf_string_conversions.h"
 #include "grit/ash_strings.h"
+#include "skia/ext/image_operations.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
@@ -280,14 +281,13 @@ class RoundedImageView : public views::View {
 
   // Set the bitmap that should be displayed from a pointer. The pointer
   // contents is copied in the receiver's bitmap.
-  void SetImage(const SkBitmap& bm) {
+  void SetImage(const SkBitmap& bm, const gfx::Size& size) {
     image_ = bm;
-    SetImageSize(gfx::Size(bm.width(), bm.height()));
-  }
+    image_size_ = size;
 
-  // Set the desired image size.
-  void SetImageSize(const gfx::Size& image_size) {
-    image_size_ = image_size;
+    // Try to get the best image quality for the avatar.
+    resized_ = skia::ImageOperations::Resize(image_,
+        skia::ImageOperations::RESIZE_BEST, size.width(), size.height());
     PreferredSizeChanged();
   }
 
@@ -304,16 +304,9 @@ class RoundedImageView : public views::View {
     path.addRoundRect(gfx::RectToSkRect(image_bounds), kRadius, kRadius);
 
     SkPaint paint;
-    SkShader* shader = SkShader::CreateBitmapShader(image_,
+    SkShader* shader = SkShader::CreateBitmapShader(resized_,
                                                     SkShader::kRepeat_TileMode,
                                                     SkShader::kRepeat_TileMode);
-    SkMatrix shader_scale;
-    shader_scale.setScale(SkFloatToScalar(
-        static_cast<float>(image_bounds.width()) / image_.width()),
-        SkFloatToScalar(static_cast<float>(
-            image_bounds.height()) / image_.height()));
-    shader->setLocalMatrix(shader_scale);
-
     paint.setShader(shader);
     paint.setXfermodeMode(SkXfermode::kSrcOver_Mode);
     shader->unref();
@@ -322,6 +315,7 @@ class RoundedImageView : public views::View {
 
  private:
   SkBitmap image_;
+  SkBitmap resized_;
   gfx::Size image_size_;
   int corner_radius_;
 
@@ -340,8 +334,8 @@ views::View* TrayUser::CreateTrayView(user::LoginStatus status) {
   avatar_.reset(new tray::RoundedImageView(kUserIconCornerRadius));
   if (status == user::LOGGED_IN_USER || status == user::LOGGED_IN_OWNER) {
     avatar_->SetImage(
-        ash::Shell::GetInstance()->tray_delegate()->GetUserImage());
-    avatar_->SetImageSize(gfx::Size(kUserIconSize, kUserIconSize));
+        ash::Shell::GetInstance()->tray_delegate()->GetUserImage(),
+        gfx::Size(kUserIconSize, kUserIconSize));
   } else {
     avatar_->SetVisible(false);
   }
@@ -378,8 +372,8 @@ void TrayUser::OnUpdateRecommended() {
 
 void TrayUser::OnUserUpdate() {
   avatar_->SetImage(
-      ash::Shell::GetInstance()->tray_delegate()->GetUserImage());
-  avatar_->SetImageSize(gfx::Size(kUserIconSize, kUserIconSize));
+      ash::Shell::GetInstance()->tray_delegate()->GetUserImage(),
+      gfx::Size(kUserIconSize, kUserIconSize));
 }
 
 }  // namespace internal
