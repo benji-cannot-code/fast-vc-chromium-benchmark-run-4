4a96840140edffb43083b72f685fd8677352659b
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 21:32:15 +0000

    Fix one more Browser dep in AutoFill for Android.
    This came in http://codereview.chromium.org/9704040 after my sync
    so I didn't see it in http://codereview.chromium.org/9814014
    
    BUG=117407
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9808022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128303 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -624,9 +624,13 @@ void AutofillManager::OnHideAutofillPopup() {
 }
 
 void AutofillManager::OnShowPasswordGenerationPopup(const gfx::Rect& bounds) {
+#if defined(OS_ANDROID)
+  NOTIMPLEMENTED();
+#else
   Browser* browser = BrowserList::GetLastActiveWithProfile(
       Profile::FromBrowserContext(web_contents()->GetBrowserContext()));
   browser->window()->ShowPasswordGenerationBubble(bounds);
+#endif  // #if defined(OS_ANDROID)
 }
 
 void AutofillManager::OnLoadedServerPredictions(
