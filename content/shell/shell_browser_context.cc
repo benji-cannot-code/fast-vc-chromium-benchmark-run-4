e463d3638fcd8758a505316d64d7f1c02f8c8154
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 22:45:27 +0000

    Mac content shell: Set up browser context path properly.
    BUG=115784
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9838021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128325 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_browser_context.cc
+++ content/shell/shell_browser_context.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths_win.h"
 #elif defined(OS_LINUX)
 #include "base/nix/xdg_util.h"
+#elif defined(OS_MACOSX)
+#include "base/base_paths_mac.h"
 #endif
 
 using content::BrowserThread;
@@ -108,6 +110,9 @@ FilePath ShellBrowserContext::GetPath() {
                                                  kXdgConfigHomeEnvVar,
                                                  kDotConfigDir));
   path_ = config_dir.Append("content_shell");
+#elif defined(OS_MACOSX)
+  CHECK(PathService::Get(base::DIR_APP_DATA, &path_));
+  path_ = path_.Append("Chromium Content Shell");
 #else
   NOTIMPLEMENTED();
 #endif
