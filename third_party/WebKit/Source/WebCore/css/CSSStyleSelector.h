c15bbf6708a7d56f87941de317662b560620f8dd
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 21:53:05 +0000

    Unreviewed, add missing include statement for CSSValueList.h.
    * css/CSSStyleSelector.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSelector.h
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSelector.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CSSStyleSelector_h
 
 #include "CSSRule.h"
+#include "CSSValueList.h"
 #include "LinkHash.h"
 #include "MediaQueryExp.h"
 #include "RenderStyle.h"
