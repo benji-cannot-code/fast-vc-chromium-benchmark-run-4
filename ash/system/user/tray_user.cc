8624598834d76daffc790b41fe6f6682bfda5228
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 15:03:10 +0000

    ash: Make sure the avatar is always of the fixed size.
    BUG=119450
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9837003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128212 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/tray_user.cc
+++ ash/system/user/tray_user.cc
@@ -379,6 +379,7 @@ void TrayUser::OnUpdateRecommended() {
 void TrayUser::OnUserUpdate() {
   avatar_->SetImage(
       ash::Shell::GetInstance()->tray_delegate()->GetUserImage());
+  avatar_->SetImageSize(gfx::Size(kUserIconSize, kUserIconSize));
 }
 
 }  // namespace internal
