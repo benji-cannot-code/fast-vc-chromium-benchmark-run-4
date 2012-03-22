a8373977667b90246eae4f3db89a88fada362aa9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 12:38:45 +0000

    blur() on shadow host should work when a shadow host contains a focused element in its shadow DOM subtrees https://bugs.webkit.org/show_bug.cgi?id=81102
    Patch by Kaustubh Atrawalkar <kaustubh@motorola.com> on 2012-03-22
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    This implementation will support blur for a focused element when its shadowHost's blur() is called.
    To achieve this Element::blur() function is modified to blur the focused node in it's treeScope.
    
    Test: fast/dom/shadow/shadow-root-blur.html
    
    * dom/Element.cpp:
    (WebCore::Element::blur): Modified to blur current treeScope's focused node.
    * dom/ShadowRoot.h:
    (WebCore::ShadowRoot::activeElement): Rework for code sharing.
    * dom/TreeScope.cpp:
    (WebCore::TreeScope::focusedNode): Added new function to share code for getting focused node.
    * dom/TreeScope.h:
    (TreeScope): New function declartion.
    * html/HTMLDocument.cpp:
    (WebCore::HTMLDocument::activeElement): Rework for code sharing.
    
    LayoutTests:
    
    * fast/dom/shadow/shadow-root-blur-expected.txt: Added.
    * fast/dom/shadow/shadow-root-blur.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Document.h"
 #include "DocumentFragment.h"
+#include "Element.h"
 #include "ExceptionCode.h"
 #include "TreeScope.h"
 #include <wtf/DoublyLinkedList.h>
@@ -123,8 +124,8 @@ inline bool ShadowRoot::isUsedForRendering() const
 
 inline Element* ShadowRoot::activeElement() const
 {
-    if (document()->isHTMLDocument())
-        return treeScope()->activeElement();
+    if (Node* node = treeScope()->focusedNode())
+        return node->isElementNode() ? toElement(node) : 0;
     return 0;
 }
 
