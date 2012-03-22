1ef5f61ecb9159a73aca01968a835421ecbb5e6d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 14:35:51 +0000

    https://bugs.webkit.org/show_bug.cgi?id=81893 Remove custom bindings form ScriptProfileNode.idl of attribute type Array.
    Patch by Vineet Chaudhary <rgf748@motorola.com> on 2012-03-22
    Reviewed by Kentaro Hara.
    
    Replace [CustomGetter] Array with sequence<ScriptProfileNode>.
    To remove the custom bindings code.
    
    Test: No new tests. LayoutTests/fast/profiler/* test are enough for this.
    
    * bindings/js/JSScriptProfileNodeCustom.cpp: Removed custom function.
    (WebCore::JSScriptProfileNode::callUID):
    * bindings/v8/custom/V8ScriptProfileNodeCustom.cpp: Removed custom function.
    (WebCore):
    * inspector/ScriptProfileNode.idl: Replaced Array with sequence<ScriptProfileNode>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSScriptProfileNodeCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSScriptProfileNodeCustom.cpp
@@ -29,12 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSScriptProfileNode.h"
 
-#if ENABLE(JAVASCRIPT_DEBUGGER)
-#include <profiler/ProfileNode.h>
-#endif
-
-#include <runtime/JSArray.h>
-
 using namespace JSC;
 
 namespace WebCore {
@@ -45,20 +39,6 @@ JSValue JSScriptProfileNode::callUID(ExecState*) const
 {
     JSValue result = jsNumber(impl()->callIdentifier().hash());
     return result;
-}
-
-typedef Vector<RefPtr<ProfileNode> > ProfileNodesList;
-
-JSValue JSScriptProfileNode::children(ExecState* exec) const
-{
-    const ProfileNodesList& children = impl()->children();
-    MarkedArgumentBuffer list;
-
-    ProfileNodesList::const_iterator end = children.end();
-    for (ProfileNodesList::const_iterator iter = children.begin(); iter != end; ++iter)
-        list.append(toJS(exec, globalObject(), iter->get()));
-
-    return constructArray(exec, globalObject(), list);
 }
 
 #endif
--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ScriptProfileNodeCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ScriptProfileNodeCustom.cpp
@@ -41,19 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8ScriptProfileNode::childrenAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
-{
-    INC_STATS("DOM.ScriptProfileNode.childrenAccessorGetter");
-    ScriptProfileNode* imp = V8ScriptProfileNode::toNative(info.Holder());
-    const ProfileNodesList& children = imp->children();
-    v8::Handle<v8::Array> result = v8::Array::New(children.size());
-    int index = 0;
-    ProfileNodesList::const_iterator end = children.end();
-    for (ProfileNodesList::const_iterator iter = children.begin(); iter != end; ++iter)
-        result->Set(v8::Integer::New(index++), toV8(iter->get()));
-    return result;
-}
-
 v8::Handle<v8::Value> V8ScriptProfileNode::callUIDAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
 {
     INC_STATS("DOM.ScriptProfileNode.callUIDAccessorGetter");
