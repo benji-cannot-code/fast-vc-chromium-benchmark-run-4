1b42df7d54f73931801be428b4fa4766aa77d8a7
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 13:17:27 +0000

    Update icons for network settings.
    BUG=chromium:113685
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9816018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128202 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/chromeos/network_list.js
+++ chrome/browser/resources/options2/chromeos/network_list.js
@@ -887,7 +887,6 @@ cr.define('options.network', function() {
       loadData_('vpn', data.vpnList, data.rememberedList);
     else
       networkList.deleteItem('vpn');
-
     networkList.updateToggleControl('airplaneMode', data.airplaneMode);
 
     networkList.invalidate();
