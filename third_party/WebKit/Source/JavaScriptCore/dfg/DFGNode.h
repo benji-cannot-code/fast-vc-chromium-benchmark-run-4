9e82361bfcecf10e8478d65b70d2cd8705ee1e00
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 03:47:55 +0000

    DFG speculation on booleans should be rationalized https://bugs.webkit.org/show_bug.cgi?id=81840
    Reviewed by Gavin Barraclough.
            
    This removes isKnownBoolean() and replaces it with AbstractState-based
    optimization, and cleans up the control flow in code gen methods for
    Branch and LogicalNot. Also fixes a goof in Node::shouldSpeculateNumber,
    and removes isKnownNotBoolean() since that method appeared to be a
    helper used solely by 32_64's speculateBooleanOperation().
            
    This is performance-neutral.
    
    * dfg/DFGAbstractState.cpp:
    (JSC::DFG::AbstractState::execute):
    * dfg/DFGNode.h:
    (JSC::DFG::Node::shouldSpeculateNumber):
    * dfg/DFGSpeculativeJIT.cpp:
    (DFG):
    * dfg/DFGSpeculativeJIT.h:
    (SpeculativeJIT):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::fillSpeculateBoolean):
    (JSC::DFG::SpeculativeJIT::compileLogicalNot):
    (JSC::DFG::SpeculativeJIT::emitBranch):
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compileLogicalNot):
    (JSC::DFG::SpeculativeJIT::emitBranch):
    (JSC::DFG::SpeculativeJIT::compile):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGNode.h
@@ -695,7 +695,7 @@ struct Node {
     
     bool shouldSpeculateNumber()
     {
-        return isNumberPrediction(prediction()) || prediction() == PredictNone;
+        return isNumberPrediction(prediction());
     }
     
     bool shouldNotSpeculateInteger()
