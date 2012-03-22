fff98d00d8c08241ef236e1919867e2f03383b09
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 05:52:08 +0000

    Tweak a base::Passed() usage not to also Pass() from the scoped_ptr.
    Review URL: http://codereview.chromium.org/9827005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128175 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/pepper_message_filter.cc
+++ content/browser/renderer_host/pepper_message_filter.cc
@@ -862,7 +862,7 @@ void PepperMessageFilter::DoGetNetworkList() {
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::Bind(&PepperMessageFilter::SendNetworkList,
-                 this, base::Passed(list.Pass())));
+                 this, base::Passed(&list)));
 }
 
 void PepperMessageFilter::SendNetworkList(
