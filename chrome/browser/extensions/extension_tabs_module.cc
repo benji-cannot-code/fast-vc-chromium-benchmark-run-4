279c5552a0b89d555f858b7f5ad80bfb2f4347e3
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 03:21:12 +0000

    Expose the chrome.windows.* API to platform apps.
    BUG=113412
    R=jstritar@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9717011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_module.cc
+++ chrome/browser/extensions/extension_tabs_module.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/extensions/shell_window.h"
 #include "chrome/browser/ui/panels/panel_manager.h"
 #include "chrome/browser/ui/snapshot_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
@@ -561,6 +562,15 @@ bool CreateWindowFunction::RunImpl() {
           window_type = Browser::TYPE_PANEL;
         else
           window_type = Browser::TYPE_POPUP;
+      } else if (type_str == keys::kWindowTypeValueShell &&
+          GetExtension()->is_platform_app()) {
+        GURL window_url =
+            urls.empty() ? GetExtension()->GetFullLaunchURL() : urls[0];
+        ShellWindow* shell_window =
+            ShellWindow::Create(window_profile, GetExtension(), window_url);
+        result_.reset(shell_window->extension_window_controller()->
+            CreateWindowValueWithTabs());
+        return true;
       } else if (type_str != keys::kWindowTypeValueNormal) {
         error_ = keys::kInvalidWindowTypeError;
         return false;
