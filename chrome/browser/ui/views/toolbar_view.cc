f33d06f8a85d1f20fe71610556055169b66d1bd3
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 03:32:09 +0000

    Ash: Update tab strip for new image assets
    * Metrics changes for new art.
    * Allow clicks in the shadow above each tab (4 pixels) to drag the window if the window is restored.  This maintains a large click target for dragging.  When the window is maximized, let that area select the tab since the window cannot be dragged.
    * Remove some unused constants.
    
    BUG=117431
    TEST=Open multiple tabs and check visual appearance for normal window, incognito window, maximized window.  Also apply a theme and check again.
    R=sky@chromium.org
    
    
    Review URL: http://codereview.chromium.org/9814013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128160 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar_view.cc
@@ -84,6 +84,14 @@ static const int kButtonSpacing = 0;
 static const int kButtonSpacing = 1;
 #endif
 
+// The content area line has a shadow that extends a couple of pixels above
+// the toolbar bounds.
+#if defined(USE_ASH)
+const int kContentShadowHeight = 2;
+#else
+const int kContentShadowHeight = 0;
+#endif
+
 // The length of time to run the upgrade notification animation (the time it
 // takes one pulse to run its course and go back to its original brightness).
 static const int kPulseDuration = 2000;
@@ -515,7 +523,8 @@ gfx::Size ToolbarView::GetPreferredSize() {
       normal_background = *rb.GetBitmapNamed(IDR_CONTENT_TOP_CENTER);
     }
 
-    return gfx::Size(min_width, normal_background.height());
+    return gfx::Size(min_width,
+                     normal_background.height() - kContentShadowHeight);
   }
 
   int vertical_spacing = PopupTopSpacing() +
