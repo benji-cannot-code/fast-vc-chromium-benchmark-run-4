6df375b7d9dc0663f424b4a35b9323b223edcd59
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 21:38:28 +0000

    Fix possible issue with multiple clicks on demo login screen. Though this doesn't currently repro, there is a possibility that if the demo login flow takes too long, a user clicking really fast on the login screen may see unexpected behavior due to multiple login attempts being kicked off. This CL will ensure that that doesn't happen.
    R=xiyuan@chromium.org,nkostylev@chromium.org
    BUG=None
    TEST=Tested that login flow works correctly.
    
    
    Review URL: http://codereview.chromium.org/9796003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/demo_user_login.js
+++ chrome/browser/resources/chromeos/login/demo_user_login.js
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Handles a user clicking anywhere on the screen. This will log the demo user
  * in. Yes, this actually _is the intention.
  */
-onClick = function() {
+onClick = function(e) {
+  document.removeEventListener('click', onClick);
+  e.stopPropagation();
   chrome.send('launchDemoUser');
 };
 
