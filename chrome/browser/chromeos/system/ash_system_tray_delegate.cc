5d35ac3ef398552599e6b012ecdd76b9f7d11375
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 09:27:17 +0000

    chromeos: Fix a crash when trying to open settings page when there is no browser window.
    BUG=119400
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9829007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128185 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/ash_system_tray_delegate.cc
+++ chrome/browser/chromeos/system/ash_system_tray_delegate.cc
@@ -419,8 +419,10 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
   // browser window with an empty tab and returns it.
   Browser* GetAppropriateBrowser() {
     Browser* browser = BrowserList::GetLastActive();
-    if (!browser)
-      browser = Browser::NewEmptyWindow(ProfileManager::GetDefaultProfile());
+    if (!browser) {
+      browser = Browser::OpenEmptyWindow(
+          ProfileManager::GetDefaultProfileOrOffTheRecord());
+    }
     return browser;
   }
 
