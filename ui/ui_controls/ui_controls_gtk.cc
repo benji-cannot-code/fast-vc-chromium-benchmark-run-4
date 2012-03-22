7394024827d78f45dec1e21f69697a901d5f0645
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 23:59:13 +0000

    Removed dead code.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9838023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128345 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/ui_controls/ui_controls_gtk.cc
+++ ui/ui_controls/ui_controls_gtk.cc
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/gtk/event_synthesis_gtk.h"
 #include "ui/gfx/rect.h"
 
-#if defined(TOOLKIT_VIEWS)
-#include "ui/views/view.h"
-#include "ui/views/widget/widget.h"
-#endif
-
 namespace {
 
 // static
@@ -251,14 +246,5 @@ bool SendMouseEventsNotifyWhenDone(MouseButton type,
 bool SendMouseClick(MouseButton type) {
   return SendMouseEvents(type, UP | DOWN);
 }
-
-#if defined(TOOLKIT_VIEWS)
-void RunClosureAfterAllPendingUIEvents(const base::Closure& task) {
-  // Send noop event and run task.
-  int x, y;
-  gdk_window_at_pointer(&x, &y);
-  SendMouseMoveNotifyWhenDone(x, y, task);
-}
-#endif
 
 }  // namespace ui_controls
