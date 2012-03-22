1a13cdf38dd167ee9f95c2f83c4dabe95a05dcb9
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 23:36:29 +0000

    Actually move WTF files to their new home https://bugs.webkit.org/show_bug.cgi?id=81844
    [Qt] Unreviewed buildfix after r111778.
    
    * UIProcess/API/qt/qquicknetworkrequest_p.h:
    * WebKit2.pri:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111783 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquicknetworkrequest_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquicknetworkrequest_p.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define qquicknetworkrequest_p_h
 
 #include "QtNetworkRequestData.h"
-#include "RefPtr.h"
+#include "wtf/RefPtr.h"
 #include "qwebkitglobal.h"
 #include <QObject>
 #include <QtDeclarative/qdeclarativelist.h>
