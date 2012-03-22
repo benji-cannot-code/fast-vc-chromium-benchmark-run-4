ab4f18d85989ab015b5b45ee0b4f7d97b4c6785f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 23:08:13 +0000

    run-bindings-tests output a lot of errors in TestSerializedScriptValueInterface.idl https://bugs.webkit.org/show_bug.cgi?id=81852
    Reviewed by Adam Barth.
    
    run-bindings-tests output a lot of errors in TestSerializedScriptValueInterface.idl:
    
    Use of uninitialized value in string eq at WebCore/bindings/scripts/CodeGeneratorJS.pm line 2743.
    Use of uninitialized value $name in substitution (s///) at WebCore/bindings/scripts/CodeGenerator.pm line 119.
    Use of uninitialized value $type in string eq at WebCore/bindings/scripts/CodeGeneratorJS.pm line 2969.
    Use of uninitialized value $type in string eq at WebCore/bindings/scripts/CodeGeneratorJS.pm line 2972.
    Use of uninitialized value $type in hash element at WebCore/bindings/scripts/CodeGenerator.pm line 367.
    Use of uninitialized value $type in hash element at WebCore/bindings/scripts/CodeGenerator.pm line 368.
    ...
    
    This is because [Constructor(...)] does not yet support [Optional] arguments.
    It just supports [Optional=DefaultIsUndefined] and [Optional=DefaultIsNullString] arguments:
    https://trac.webkit.org/wiki/WebKitIDL#Constructor
    
    This patch replaces [Optional] with [Optional=DefaultIsUndefined]
    in TestSerializedScriptValueInterface.idl.
    
    Test: bindings/scripts/test/TestSerializedScriptValueInterface.idl
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateConstructorDefinition):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateConstructorCallback):
    * bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp:
    (WebCore::JSTestSerializedScriptValueInterfaceConstructor::constructJSTestSerializedScriptValueInterface):
    * bindings/scripts/test/TestSerializedScriptValueInterface.idl:
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::V8TestSerializedScriptValueInterface::constructorCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111779 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestSerializedScriptValueInterface.cpp
@@ -33,11 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <runtime/Error.h>
 #include <wtf/GetPtr.h>
 
-#if ENABLE(Condition1) || ENABLE(Condition2)
-#include ".h"
-#include "JS.h"
-#endif
-
 using namespace JSC;
 
 namespace WebCore {
@@ -100,14 +95,6 @@ EncodedJSValue JSC_HOST_CALL JSTestSerializedScriptValueInterfaceConstructor::co
     RefPtr<SerializedScriptValue> data(SerializedScriptValue::create(exec, MAYBE_MISSING_PARAMETER(exec, 1, DefaultIsUndefined)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
-
-    size_t argsCount = exec->argumentCount();
-    if (argsCount <= 2) {
-
-        JSC::JSValue result = toJS(exec, castedThis->globalObject(), WTF::getPtr(impl->constructorCallback(hello, data)));
-        return JSValue::encode(result);
-    }
-
     Array* transferList(toArray(MAYBE_MISSING_PARAMETER(exec, 2, DefaultIsUndefined)));
     if (exec->hadException())
         return JSValue::encode(jsUndefined());
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp
@@ -29,16 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MessagePort.h"
 #include "RuntimeEnabledFeatures.h"
 #include "SerializedScriptValue.h"
-#include "V8.h"
 #include "V8Binding.h"
 #include "V8BindingMacros.h"
 #include "V8BindingState.h"
 #include "V8DOMWrapper.h"
 #include "V8IsolatedContext.h"
 #include "V8Proxy.h"
-#include <wtf/GetPtr.h>
-#include <wtf/RefCounted.h>
-#include <wtf/RefPtr.h>
 #include <wtf/UnusedParam.h>
 
 namespace WebCore {
@@ -218,9 +214,6 @@ v8::Handle<v8::Value> V8TestSerializedScriptValueInterface::constructorCallback(
     RefPtr<SerializedScriptValue> data = SerializedScriptValue::create(args[1], &messagePortArrayTransferList, &arrayBufferArrayTransferList, dataDidThrow);
     if (dataDidThrow)
         return v8::Undefined();
-    if (args.Length() <= 2) {
-        return toV8(imp->Constructor(hello, data));
-    }
 
     RefPtr<TestSerializedScriptValueInterface> impl = TestSerializedScriptValueInterface::create(hello, data, messagePortArrayTransferList);
     v8::Handle<v8::Object> wrapper = args.Holder();
