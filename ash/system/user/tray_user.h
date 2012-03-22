728dc83f221f27d174bf58e12920f7335789ab4d
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 01:50:06 +0000

    Adjust uber tray spacing around avatar and draw avatar using rounded rectangle.
    BUG=117213
    TEST=None.
    
    
    Review URL: http://codereview.chromium.org/9813020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128138 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/tray_user.h
+++ ash/system/user/tray_user.h
@@ -20,6 +20,7 @@ namespace internal {
 
 namespace tray {
 class UserView;
+class RoundedImageView;
 }
 
 class TrayUser : public SystemTrayItem,
@@ -45,7 +46,7 @@ class TrayUser : public SystemTrayItem,
   virtual void OnUserUpdate() OVERRIDE;
 
   scoped_ptr<tray::UserView> user_;
-  scoped_ptr<views::ImageView> avatar_;
+  scoped_ptr<tray::RoundedImageView> avatar_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayUser);
 };
