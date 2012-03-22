b39a118f36712e3657cc7c73e6e27103ca879867
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 18:09:47 +0000

    RenderWidgetHost::WasHidden is called twice on each tab switch on Aura.
    TabContentsContainer::ChangeWebContent was calling native_container_->DetachContents then eventually calls RWH->WasHidden(), so there is no need to call it explicitly a second time.
    
    
    BUG=110645
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/9243013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/tab_contents_container.cc
+++ chrome/browser/ui/views/tab_contents/tab_contents_container.cc
@@ -38,7 +38,6 @@ TabContentsContainer::~TabContentsContainer() {
 void TabContentsContainer::ChangeWebContents(WebContents* contents) {
   if (web_contents_) {
     native_container_->DetachContents(web_contents_);
-    web_contents_->WasHidden();
     RemoveObservers();
   }
   web_contents_ = contents;
