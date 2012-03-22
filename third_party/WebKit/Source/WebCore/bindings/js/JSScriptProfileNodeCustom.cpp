f5b196bcd125290722a9e81c5a689150413fa077
kkristof@inf.u-szeged.hu <kkristof@inf.u-szeged.hu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 16:01:58 +0000

    [Qt] Fix Qt minimal build after r111692
    Reviewed by Csaba Osztrogonác.
    
    * bindings/js/JSScriptProfileNodeCustom.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSScriptProfileNodeCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSScriptProfileNodeCustom.cpp
@@ -27,13 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#if ENABLE(JAVASCRIPT_DEBUGGER)
+
 #include "JSScriptProfileNode.h"
 
 using namespace JSC;
 
 namespace WebCore {
-
-#if ENABLE(JAVASCRIPT_DEBUGGER)
 
 JSValue JSScriptProfileNode::callUID(ExecState*) const
 {
@@ -41,6 +41,6 @@ JSValue JSScriptProfileNode::callUID(ExecState*) const
     return result;
 }
 
-#endif
-
 } // namespace WebCore
+
+#endif
