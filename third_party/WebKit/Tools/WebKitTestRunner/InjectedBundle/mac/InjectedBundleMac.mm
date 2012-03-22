ee32aacec1faa298f105301cc1983f036cdfc318
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-22 22:30:52 +0000

    Font fallback in WebKitTestRunner depends on user settings https://bugs.webkit.org/show_bug.cgi?id=81978
    Reviewed by Jessie Berlin.
    
    * WebKitTestRunner/InjectedBundle/mac/InjectedBundleMac.mm:
    (WTR::InjectedBundle::platformInitialize): Added an AppleLanguages key
    with a value consisting of the array ("en") to the override defaults
    dictionary. This is the same value used in DumpRenderTree, and it
    ensures that font fallback follows a consistent path that is not
    affected by the language preferences of the user running the test.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/mac/InjectedBundleMac.mm
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/mac/InjectedBundleMac.mm
@@ -38,6 +38,7 @@ void InjectedBundle::platformInitialize(WKTypeRef)
         [NSNumber numberWithBool:NO],     @"AppleScrollAnimationEnabled",
         [NSNumber numberWithBool:NO],     @"NSOverlayScrollersEnabled",
         @"Always",                        @"AppleShowScrollBars",
+        [NSArray arrayWithObject:@"en"],  @"AppleLanguages",
         nil];
 
     [[NSUserDefaults standardUserDefaults] setVolatileDomain:dict forName:NSArgumentDomain];    
