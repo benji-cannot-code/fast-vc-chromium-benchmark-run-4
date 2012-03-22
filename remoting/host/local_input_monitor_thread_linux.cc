6793457f080a563037892c0021714335be60a357
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-22 01:38:02 +0000

    Linux: Fix the build for Ubuntu Precise.
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9817020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128133 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/local_input_monitor_thread_linux.cc
+++ remoting/host/local_input_monitor_thread_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // These includes need to be later than dictated by the style guide due to
 // Xlib header pollution, specifically the min, max, and Status macros.
+#include <X11/XKBlib.h>
 #include <X11/Xlibint.h>
 #include <X11/extensions/record.h>
 
@@ -168,7 +169,7 @@ void LocalInputMonitorThread::LocalMouseMoved(const SkIPoint& pos) {
 }
 
 void LocalInputMonitorThread::LocalKeyPressed(int key_code, bool down) {
-  int key_sym = XKeycodeToKeysym(display_, key_code, 0);
+  KeySym key_sym = XkbKeycodeToKeysym(display_, key_code, 0, 0);
   if (key_sym == XK_Control_L || key_sym == XK_Control_R) {
     ctrl_pressed_ = down;
   } else if (key_sym == XK_Alt_L || key_sym == XK_Alt_R) {
