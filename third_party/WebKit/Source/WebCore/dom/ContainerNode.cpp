52b0a9b8e5a197cc2d1696ff3645a221c4d8140d
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 22:51:25 +0000

    Match DOM4 spec with respect to DocumentFragment insertion https://bugs.webkit.org/show_bug.cgi?id=82089
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    DOM4 specifies the behavior of appendChild, insertBefore, and replaceChild
    in terms of "mutation algorithms":
    
    http://dvcs.w3.org/hg/domcore/raw-file/tip/Overview.html#mutation-algorithms
    
    This change updates WebKit to match, in particular with regard to DocumentFragments.
    Previously, ContainerNode would remove nodes one at a time, then add them to the new parent.
    When combined with MutationObservers, this results in overly-verbose mutation records.
    Now we create as few records as possible, matching the spec as well as Gecko's implementation
    of MutationObservers.
    
    Note that we still need to check validity each time through the loop,
    since inserting a node may dispatch events. In a future change, I hope
    to move these events so that they fire only after all nodes are inserted,
    but that's too much to tackle all in one.
    
    Tests: fast/mutation/document-fragment-insertion.html
    
    * dom/ContainerNode.cpp:
    (WebCore::collectChildrenAndRemoveFromOldParent): New helper method
    combining collectTargetNodes() with the removal of the collected nodes from
    their old parent, if any.
    (WebCore::ContainerNode::insertBefore): Use new helper method instead
    of removing nodes one at a time from the fragment.
    (WebCore::ContainerNode::replaceChild): ditto. Also removed some redundant asserts
    and moved the "do nothing" check out of the loop.
    (WebCore::ContainerNode::appendChild): Use new helper method.
    
    LayoutTests:
    
    * fast/dom/Node/fragment-mutation-expected.txt:
    * fast/dom/Node/fragment-mutation.html: Removed tests that no longer make sense
    since the DocumentFragment has no children by the time DOMSubtreeModified is called.
    * fast/mutation/document-fragment-insertion-expected.txt: Added.
    * fast/mutation/document-fragment-insertion.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -70,6 +70,18 @@ static void collectTargetNodes(Node* node, NodeVector& nodes)
     getChildNodes(node, nodes);
 }
 
+static void collectChildrenAndRemoveFromOldParent(Node* node, NodeVector& nodes, ExceptionCode& ec)
+{
+    if (node->nodeType() != Node::DOCUMENT_FRAGMENT_NODE) {
+        nodes.append(node);
+        if (ContainerNode* oldParent = node->parentNode())
+            oldParent->removeChild(node, ec);
+        return;
+    }
+    getChildNodes(node, nodes);
+    toContainerNode(node)->removeChildren();
+}
+
 void ContainerNode::removeAllChildren()
 {
     removeAllChildrenInContainer<Node, ContainerNode>(this);
@@ -128,13 +140,14 @@ bool ContainerNode::insertBefore(PassRefPtr<Node> newChild, Node* refChild, Exce
         return false;
     }
 
-    NodeVector targets;
-    collectTargetNodes(newChild.get(), targets);
-    if (targets.isEmpty())
+    if (refChild->previousSibling() == newChild || refChild == newChild) // nothing to do
         return true;
 
-    // Now actually add the child(ren)
-    if (refChild->previousSibling() == newChild || refChild == newChild) // nothing to do
+    NodeVector targets;
+    collectChildrenAndRemoveFromOldParent(newChild.get(), targets, ec);
+    if (ec)
+        return false;
+    if (targets.isEmpty())
         return true;
 
 #if ENABLE(MUTATION_OBSERVERS)
@@ -145,12 +158,6 @@ bool ContainerNode::insertBefore(PassRefPtr<Node> newChild, Node* refChild, Exce
     RefPtr<Node> refChildPreviousSibling = refChild->previousSibling();
     for (NodeVector::const_iterator it = targets.begin(); it != targets.end(); ++it) {
         Node* child = it->get();
-
-        // If child is already present in the tree, first remove it from the old location.
-        if (ContainerNode* oldParent = child->parentNode())
-            oldParent->removeChild(child, ec);
-        if (ec)
-            return false;
 
         // Due to arbitrary code running in response to a DOM mutation event it's
         // possible that "next" is no longer a child of "this".
@@ -253,7 +260,7 @@ bool ContainerNode::replaceChild(PassRefPtr<Node> newChild, Node* oldChild, Exce
 
     if (oldChild == newChild) // nothing to do
         return true;
-    
+
     // Make sure replacing the old child with the new is ok
     checkReplaceChild(newChild.get(), oldChild, ec);
     if (ec)
@@ -278,22 +285,17 @@ bool ContainerNode::replaceChild(PassRefPtr<Node> newChild, Node* oldChild, Exce
     if (ec)
         return false;
 
+    if (next && (next->previousSibling() == newChild || next == newChild)) // nothing to do
+        return true;
+
     NodeVector targets;
-    collectTargetNodes(newChild.get(), targets);
+    collectChildrenAndRemoveFromOldParent(newChild.get(), targets, ec);
+    if (ec)
+        return false;
 
     // Add the new child(ren)
     for (NodeVector::const_iterator it = targets.begin(); it != targets.end(); ++it) {
         Node* child = it->get();
-
-        // If the new child is already in the right place, we're done.
-        if (next && (next == child || next == child->previousSibling()))
-            break;
-
-        // Remove child from its old position.
-        if (ContainerNode* oldParent = child->parentNode())
-            oldParent->removeChild(child, ec);
-        if (ec)
-            return false;
 
         // Due to arbitrary code running in response to a DOM mutation event it's
         // possible that "next" is no longer a child of "this".
@@ -303,9 +305,6 @@ bool ContainerNode::replaceChild(PassRefPtr<Node> newChild, Node* oldChild, Exce
             break;
         if (child->parentNode())
             break;
-
-        ASSERT(!child->nextSibling());
-        ASSERT(!child->previousSibling());
 
 #if ENABLE(INSPECTOR)
         InspectorInstrumentation::willInsertDOMNode(document(), child, this);
@@ -581,7 +580,10 @@ bool ContainerNode::appendChild(PassRefPtr<Node> newChild, ExceptionCode& ec, bo
         return newChild;
 
     NodeVector targets;
-    collectTargetNodes(newChild.get(), targets);
+    collectChildrenAndRemoveFromOldParent(newChild.get(), targets, ec);
+    if (ec)
+        return false;
+
     if (targets.isEmpty())
         return true;
 
@@ -593,18 +595,12 @@ bool ContainerNode::appendChild(PassRefPtr<Node> newChild, ExceptionCode& ec, bo
     RefPtr<Node> prev = lastChild();
     for (NodeVector::const_iterator it = targets.begin(); it != targets.end(); ++it) {
         Node* child = it->get();
-        // If child is already present in the tree, first remove it
-        if (ContainerNode* oldParent = child->parentNode()) {
-            oldParent->removeChild(child, ec);
-            if (ec)
-                return false;
 
-            // If the child has a parent again, just stop what we're doing, because
-            // that means someone is doing something with DOM mutation -- can't re-parent
-            // a child that already has a parent.
-            if (child->parentNode())
-                break;
-        }
+        // If the child has a parent again, just stop what we're doing, because
+        // that means someone is doing something with DOM mutation -- can't re-parent
+        // a child that already has a parent.
+        if (child->parentNode())
+            break;
 
 #if ENABLE(INSPECTOR)
         InspectorInstrumentation::willInsertDOMNode(document(), child, this);
