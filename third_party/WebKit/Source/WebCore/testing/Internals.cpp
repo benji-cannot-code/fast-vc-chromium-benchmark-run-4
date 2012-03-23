30e723c23c8b73c988e48609cf15bc03ab59f24f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 20:00:10 +0000

    Relanding(r111754): HTMLPluginElement is not destroyed on reload or navigation if getNPObject is called https://bugs.webkit.org/show_bug.cgi?id=80428
    .:
    
    Patch by Dave Michael <dmichael@chromium.org> on 2012-03-23
    
    Source/WebCore:
    
    Patch by Dave Michael <dmichael@chromium.org> on 2012-03-23
    Reviewed by Eric Seidel and Ryosuke Niwa.
    
    Make HTMLPluginElement release its m_NPObject in detach() to break a
    reference-counting cycle that happens on reload or navigation. With this
    change, HTMLPlugInElement::removedFromDocument is unnecessary, so it
    was removed. Note that Releasing m_NPObject does not result in a call to
    the plugin; it simply releases a reference count on the wrapper object
    for this HTMLPlugInElement. (The plugin's NPP_Deallocate is invoked
    when the render tree is destroyed, when PluginView calls
    PluginPackage::unload.) Thus, it is safe to release m_NPObject in
    detach, because it can not result in layout or style changes.
    
    Also added numberOfLiveNodes() and numberOfLiveDocuments() to
    window.internals to enable testing.
    
    Test: plugins/netscape-dom-access-and-reload.html
    
    * WebCore.exp.in:
    * html/HTMLPlugInElement.cpp:
    (WebCore::HTMLPlugInElement::detach):
    * html/HTMLPlugInElement.h:
    (HTMLPlugInElement):
    * testing/Internals.cpp:
    (WebCore):
    (WebCore::Internals::numberOfLiveNodes):
    (WebCore::Internals::numberOfLiveDocuments):
    * testing/Internals.h:
    (Internals):
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    Patch by Dave Michael <dmichael@chromium.org> on 2012-03-23
    Reviewed by Eric Seidel and Ryosuke Niwa.
    
    * win/WebKit2.def: Export a symbol for InspectorCounters::counterValue
    * win/WebKit2CFLite.def: Export a symbol for InspectorCounters::counterValue
    
    LayoutTests:
    
    Patch by Dave Michael <dmichael@chromium.org> on 2012-03-23
    Reviewed by Eric Seidel and Ryosuke Niwa.
    
    Due to unfortunate copy/paste laziness, the new test was using the same
    window.sessionStorage as plugins/reloadplugins-and-pages.html, so that
    if the tests were run in the same session, reloadplugins-and-pages.html
    would *not* reload as it was supposed to, causing a text mismatch. This
    patch uses a more appropriate and unique name so that these two tests
    won't affect each other.
    
    * plugins/netscape-dom-access-and-reload-expected.txt: Added.
    * plugins/netscape-dom-access-and-reload.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.cpp
+++ third_party/WebKit/Source/WebCore/testing/Internals.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "HTMLTextAreaElement.h"
 #include "InspectorController.h"
+#include "InspectorCounters.h"
 #include "InspectorInstrumentation.h"
 #include "InternalSettings.h"
 #include "IntRect.h"
@@ -770,6 +771,18 @@ bool Internals::hasSpellingMarker(Document* document, int from, int length, Exce
 
     return document->frame()->editor()->selectionStartHasMarkerFor(DocumentMarker::Spelling, from, length);
 }
+
+#if ENABLE(INSPECTOR)
+unsigned Internals::numberOfLiveNodes() const
+{
+    return InspectorCounters::counterValue(InspectorCounters::NodeCounter);
+}
+
+unsigned Internals::numberOfLiveDocuments() const
+{
+    return InspectorCounters::counterValue(InspectorCounters::DocumentCounter);
+}
+#endif // ENABLE(INSPECTOR)
 
 bool Internals::hasGrammarMarker(Document* document, int from, int length, ExceptionCode&)
 {
