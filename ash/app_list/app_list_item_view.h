8d4b5d9133f2be6ab05afa7d5dffe693bd73bcc8
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 02:27:16 +0000

    ash: Add drop shadow to app list icons.
    BUG=117238
    TEST=Verify fix for issue 117238
    
    
    Review URL: http://codereview.chromium.org/9816019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128397 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/app_list/app_list_item_view.h
+++ ash/app_list/app_list_item_view.h
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/app_list_item_model_observer.h"
 #include "ash/ash_export.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/custom_button.h"
 
@@ -17,7 +19,6 @@ class SkBitmap;
 
 namespace views {
 class ImageView;
-class Label;
 class MenuRunner;
 }
 
@@ -57,6 +58,15 @@ class ASH_EXPORT AppListItemView : public views::CustomButton,
   static const char kViewClassName[];
 
  private:
+  class IconOperation;
+
+  // Get icon from model and schedule background processing.
+  void UpdateIcon();
+
+  // Reply callback from background shadow generation. |op| is the finished
+  // operation and holds the result image.
+  void ApplyShadow(scoped_refptr<IconOperation> op);
+
   // AppListItemModelObserver overrides:
   virtual void ItemIconChanged() OVERRIDE;
   virtual void ItemTitleChanged() OVERRIDE;
@@ -85,6 +95,9 @@ class ASH_EXPORT AppListItemView : public views::CustomButton,
 
   gfx::Size icon_size_;
   bool selected_;
+
+  scoped_refptr<IconOperation> icon_op_;
+  base::WeakPtrFactory<AppListItemView> apply_shadow_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListItemView);
 };
