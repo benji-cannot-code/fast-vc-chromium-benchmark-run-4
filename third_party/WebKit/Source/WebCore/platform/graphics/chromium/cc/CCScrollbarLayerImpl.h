c4ed914777f4da187fd87d642819bd2acbd679c9
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 17:13:09 +0000

    [chromium] Fix scrollbarLayerLostContext flaky test crash on 10.6 https://bugs.webkit.org/show_bug.cgi?id=82062
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCScrollbarLayerImpl.h:
    (CCScrollbarLayerImpl):
    
    Source/WebKit/chromium:
    
    Override the paint function so that it becomes a no-op. Painting
    scrollbars on the impl thread is not yet supported on platforms other
    than Linux or ChromeOS.
    
    * tests/CCLayerTreeHostImplTest.cpp:
    (ScrollbarLayerFakePaint):
    (WebKitTests::ScrollbarLayerFakePaint::create):
    (WebKitTests::ScrollbarLayerFakePaint::paint):
    (WebKitTests::ScrollbarLayerFakePaint::ScrollbarLayerFakePaint):
    (WebKitTests):
    (WebKitTests::TEST_F):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111875 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarLayerImpl.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarLayerImpl.h
@@ -67,9 +67,9 @@ public:
 protected:
     explicit CCScrollbarLayerImpl(int id);
 
-private:
-    void paint(GraphicsContext*);
+    virtual void paint(GraphicsContext*);
 
+private:
     CCLayerImpl* m_scrollLayer;
     OwnPtr<ManagedTexture> m_texture;
 
