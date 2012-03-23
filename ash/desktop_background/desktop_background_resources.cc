9132d6f10799a4bda48a6929f629db1549c7a988
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 23:18:28 +0000

    A random wallpaper will be picked when building Chrome, but nor Chromium.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9839076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128605 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_resources.cc
+++ ash/desktop_background/desktop_background_resources.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/desktop_background/desktop_background_resources.h"
 
 #include "base/logging.h"
+#include "base/rand_util.h"
 #include "grit/ui_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
@@ -563,14 +564,25 @@ const ash::WallpaperInfo kDefaultWallpapers[] = {
 };
 
 const int kDefaultWallpaperCount = arraysize(kDefaultWallpapers);
+
+#if defined(GOOGLE_CHROME_BUILD)
+const int kLastRandomWallpaperIndex = 12;
+#else
 const int kDefaultWallpaperIndex = 0;
+#endif
 
 }  // namespace
 
 namespace ash {
 
 int GetDefaultWallpaperIndex() {
+#if defined(GOOGLE_CHROME_BUILD)
+  DCHECK(kLastRandomWallpaperIndex < kDefaultWallpaperCount);
+  return base::RandInt(0,
+      std::min(kLastRandomWallpaperIndex, kDefaultWallpaperCount - 1));
+#else
   return kDefaultWallpaperIndex;
+#endif
 }
 
 int GetWallpaperCount() {
