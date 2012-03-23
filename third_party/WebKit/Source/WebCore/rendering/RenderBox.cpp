cf3414afeab80dc874399be3c86bdc97f38d63cc
mikelawther@chromium.org <mikelawther@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 18:30:01 +0000

    CSS3 calc: image sizing works with mixed percentage/absolute https://bugs.webkit.org/show_bug.cgi?id=82017
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Test: css3/calc/img-size-expected.html
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeReplacedLogicalWidthUsing):
    (WebCore::RenderBox::computeReplacedLogicalHeightUsing):
    
    LayoutTests:
    
    Test is now expected to match.
    
    * css3/calc/img-size-expected.html: Renamed from LayoutTests/css3/calc/img-size-expected-mismatch.html.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111883 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -2275,7 +2275,8 @@ LayoutUnit RenderBox::computeReplacedLogicalWidthUsing(Length logicalWidth) cons
     switch (logicalWidth.type()) {
         case Fixed:
             return computeContentBoxLogicalWidth(logicalWidth.value());
-        case Percent: {
+        case Percent: 
+        case Calculated: {
             // FIXME: containingBlockLogicalWidthForContent() is wrong if the replaced element's block-flow is perpendicular to the
             // containing block's block-flow.
             // https://bugs.webkit.org/show_bug.cgi?id=46496
@@ -2307,6 +2308,7 @@ LayoutUnit RenderBox::computeReplacedLogicalHeightUsing(Length logicalHeight) co
         case Fixed:
             return computeContentBoxLogicalHeight(logicalHeight.value());
         case Percent:
+        case Calculated:
         {
             RenderObject* cb = isPositioned() ? container() : containingBlock();
             while (cb->isAnonymous()) {
