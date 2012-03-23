b9ff1f1ff093eefcbc27bf83b848d88bfacce36a
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 01:18:48 +0000

    * We no longer run into the cookie size limit and can "run ALL the tests!" * Browser tests are supposedly faster than ui tests.
    BUG=118499
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9693065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128362 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/dom_automation.js
+++ content/renderer/dom_automation.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -394,6 +394,8 @@ var domAutomation = domAutomation || {};
       return getText(element);
     } else if (type == "innerhtml") {
       return trim(element.innerHTML);
+    } else if (type == "innertext") {
+      return trim(element.innerText);
     } else if (type == "visibility") {
       return isVisible(element);
     } else if (type == "id") {
