790218f30b63433bd9cd22c329208589b48309c6
nduca@google.com <nduca@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 22:40:10 +0000

    Revert 128585 - aura: fast resizes for accelerated pages
    This CL adds a compositor lock that we grab while waiting for the renderer's
    frame. The lock expires with a short delay.
    It makes a lot of pages resize interactively (e.g. NYT), the slow pages being not significantly worse.
    
    BUG=58782
    TEST=load www.nytimes.com, resize, see it's interactive. load gmail, resize, see it's not but it's still working
    
    
    Review URL: http://codereview.chromium.org/9838045
    
    TBR=piman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9808090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128595 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window.cc
+++ ui/aura/root_window.cc
@@ -55,28 +55,7 @@ void GetEventFiltersToNotify(Window* target, EventFilters* filters) {
   }
 }
 
-const int kCompositorLockTimeoutMs = 67;
-
 }  // namespace
-
-CompositorLock::CompositorLock(RootWindow* root_window)
-    : root_window_(root_window) {
-  MessageLoop::current()->PostDelayedTask(
-      FROM_HERE,
-      base::Bind(&CompositorLock::CancelLock, AsWeakPtr()),
-      kCompositorLockTimeoutMs);
-}
-
-CompositorLock::~CompositorLock() {
-  CancelLock();
-}
-
-void CompositorLock::CancelLock() {
-  if (!root_window_)
-    return;
-  root_window_->UnlockCompositor();
-  root_window_ = NULL;
-}
 
 bool RootWindow::hide_host_cursor_ = false;
 
@@ -102,9 +81,7 @@ RootWindow::RootWindow(const gfx::Rect& initial_bounds)
       draw_on_compositing_end_(false),
       defer_draw_scheduling_(false),
       mouse_move_hold_count_(0),
-      should_hold_mouse_moves_(false),
-      compositor_lock_(NULL),
-      draw_on_compositor_unlock_(false) {
+      should_hold_mouse_moves_(false) {
   SetName("RootWindow");
   last_mouse_location_ = host_->QueryMouseLocation();
 
@@ -119,12 +96,6 @@ RootWindow::RootWindow(const gfx::Rect& initial_bounds)
 }
 
 RootWindow::~RootWindow() {
-  if (compositor_lock_) {
-    // No need to schedule a draw, we're going away.
-    draw_on_compositor_unlock_ = false;
-    compositor_lock_->CancelLock();
-    DCHECK(!compositor_lock_);
-  }
   compositor_->RemoveObserver(this);
   // Make sure to destroy the compositor before terminating so that state is
   // cleared and we don't hit asserts.
@@ -190,11 +161,6 @@ bool RootWindow::ConfineCursorToWindow() {
 void RootWindow::Draw() {
   if (waiting_on_compositing_end_) {
     draw_on_compositing_end_ = true;
-    defer_draw_scheduling_ = false;
-    return;
-  }
-  if (compositor_lock_) {
-    draw_on_compositor_unlock_ = true;
     defer_draw_scheduling_ = false;
     return;
   }
@@ -439,12 +405,6 @@ void RootWindow::SetFocusWhenShown(bool focused) {
   host_->SetFocusWhenShown(focused);
 }
 
-scoped_refptr<CompositorLock> RootWindow::GetCompositorLock() {
-  if (!compositor_lock_)
-    compositor_lock_ = new CompositorLock(this);
-  return compositor_lock_;
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // RootWindow, Window overrides:
 
@@ -469,9 +429,7 @@ void RootWindow::SetTransform(const ui::Transform& transform) {
 // RootWindow, ui::CompositorDelegate implementation:
 
 void RootWindow::ScheduleDraw() {
-  if (compositor_lock_) {
-    draw_on_compositor_unlock_ = true;
-  } else if (!defer_draw_scheduling_) {
+  if (!defer_draw_scheduling_) {
     defer_draw_scheduling_ = true;
     MessageLoop::current()->PostTask(
         FROM_HERE,
@@ -866,15 +824,6 @@ void RootWindow::SynthesizeMouseMoveEvent() {
                    ui::EF_IS_SYNTHESIZED);
   DispatchMouseEvent(&event);
 #endif
-}
-
-void RootWindow::UnlockCompositor() {
-  DCHECK(compositor_lock_);
-  compositor_lock_ = NULL;
-  if (draw_on_compositor_unlock_) {
-    draw_on_compositor_unlock_ = false;
-    ScheduleDraw();
-  }
 }
 
 }  // namespace aura
