c0b6fd4a05d9bcfccd19ad11b163d65d847b6e92
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 20:56:10 +0000

    Crash due to accessing removed parent lineboxes when clearing selection. https://bugs.webkit.org/show_bug.cgi?id=81359
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Similar to r110323, adds the canUpdateSelectionOnRootLineBoxes
    check to more places.
    
    Test: editing/selection/clear-selection-crash.html
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::canUpdateSelectionOnRootLineBoxes):
    * rendering/RenderSelectionInfo.h:
    (WebCore::RenderSelectionInfo::RenderSelectionInfo):
    (WebCore::RenderBlockSelectionInfo::RenderBlockSelectionInfo):
    
    LayoutTests:
    
    * editing/selection/clear-selection-crash-expected.txt: Added.
    * editing/selection/clear-selection-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111899 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -2840,6 +2840,9 @@ CursorDirective RenderObject::getCursor(const LayoutPoint&, Cursor&) const
 
 bool RenderObject::canUpdateSelectionOnRootLineBoxes()
 {
+    if (needsLayout())
+        return false;
+
     RenderBlock* containingBlock = this->containingBlock();
     return containingBlock ? !containingBlock->needsLayout() : true;
 }
