378978fd9f9971b98a078f1ae232f291083c6933
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 22:14:00 +0000

    Add an experimental identity API for platform apps.
    This adds a way for platform apps to access the user's signed into chrome account and generate an OAuth2 token:
     - chrome.experimental.identity.getAuthToken
    
    BUG=none
    TEST=ExtensionApiTest.Identity
    
    
    Review URL: http://codereview.chromium.org/9474005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128591 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/declarative/declarative_api.h"
 #include "chrome/browser/extensions/api/extension_action/extension_browser_actions_api.h"
 #include "chrome/browser/extensions/api/extension_action/extension_page_actions_api.h"
+#include "chrome/browser/extensions/api/identity/identity_api.h"
 #include "chrome/browser/extensions/api/offscreen_tabs/offscreen_tabs_api.h"
 #include "chrome/browser/extensions/api/permissions/permissions_api.h"
 #include "chrome/browser/extensions/api/serial/serial_api.h"
@@ -481,6 +482,9 @@ void ExtensionFunctionRegistry::ResetFunctions() {
   RegisterFunction<SendMouseEventOffscreenTabFunction>();
   RegisterFunction<ToDataUrlOffscreenTabFunction>();
   RegisterFunction<UpdateOffscreenTabFunction>();
+
+  // Identity
+  RegisterFunction<extensions::GetAuthTokenFunction>();
 
   // Generated APIs
   extensions::api::GeneratedFunctionRegistry::RegisterAll(this);
--- chrome/chrome_browser_extensions.gypi
+++ chrome/chrome_browser_extensions.gypi
@@ -93,6 +93,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/api/extension_action/extension_page_actions_api.h',
         'browser/extensions/api/extension_action/extension_page_actions_api_constants.cc',
         'browser/extensions/api/extension_action/extension_page_actions_api_constants.h',
+        'browser/extensions/api/identity/identity_api.cc',
+        'browser/extensions/api/identity/identity_api.h',
         'browser/extensions/api/offscreen_tabs/offscreen_tabs_api.cc',
         'browser/extensions/api/offscreen_tabs/offscreen_tabs_api.h',
         'browser/extensions/api/offscreen_tabs/offscreen_tabs_constants.cc',
