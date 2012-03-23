71c14f06360a765775500ed9af950b9cf988ec27
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 19:49:47 +0000

    Fix focus of Bluetooth passkey/pincode fields.
    BUG=chromium-os:27697
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9834060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/chromeos/bluetooth_pair_device_overlay.js
+++ chrome/browser/resources/options2/chromeos/bluetooth_pair_device_overlay.js
@@ -146,6 +146,16 @@ cr.define('options', function() {
     },
 
     /**
+     * Sets input focus on the passkey or pincode field if appropriate.
+     */
+    didShowPage: function() {
+      if (!$('bluetooth-pincode').hidden)
+        $('bluetooth-pincode').focus();
+      else if (!$('bluetooth-passkey').hidden)
+        $('bluetooth-passkey').focus();
+    },
+
+    /**
      * Configures the overlay for pairing a device.
      * @param {Object} device Description of the bluetooth device.
      */
@@ -316,10 +326,7 @@ cr.define('options', function() {
    */
   BluetoothPairing.showDialog = function(device) {
     BluetoothPairing.getInstance().update(device);
-    var overlay = OptionsPage.getTopmostVisiblePage();
-    var dialog = BluetoothPairing.getInstance();
-    if (overlay != dialog)
-      OptionsPage.showPageByName('bluetoothPairing', false);
+    OptionsPage.showPageByName('bluetoothPairing', false);
   };
 
   /**
