b56a9ff841365ab6d898490d0d7be9a4c3a7767e
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 23:53:56 +0000

    Be sure to create the 'Local Storage' directory if needed. Review URL: https://chromiumcodereview.appspot.com/9839078
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128613 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/dom_storage/dom_storage_context.h
+++ webkit/dom_storage/dom_storage_context.h
@@ -159,7 +159,7 @@ class DomStorageContext
   StorageNamespaceMap namespaces_;
 
   // Where localstorage data is stored, maybe empty for the incognito use case.
-  const FilePath directory_;
+  FilePath directory_;
 
   // Used to schedule sequenced background tasks.
   scoped_refptr<DomStorageTaskRunner> task_runner_;
