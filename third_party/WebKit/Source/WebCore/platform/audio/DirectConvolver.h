1bd76ce8abf9d61130d54f1703c23519f615fd93
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 18:14:15 +0000

    Optimize for IPP in DirectConvolver::process() https://bugs.webkit.org/show_bug.cgi?id=80255
    Patch by Xingnan Wang <xingnan.wang@intel.com> on 2012-03-23
    Reviewed by Chris Rogers.
    
    Increase ~2X performance by conv() in IPP.
    
    * platform/audio/DirectConvolver.cpp:
    (WebCore::DirectConvolver::DirectConvolver):
    (WebCore::DirectConvolver::process):
    * platform/audio/DirectConvolver.h:
    (DirectConvolver):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111880 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/DirectConvolver.h
+++ third_party/WebKit/Source/WebCore/platform/audio/DirectConvolver.h
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AudioArray.h"
 
+#if USE(WEBAUDIO_IPP)
+#include <ipps.h>
+#endif // USE(WEBAUDIO_IPP)
+
 namespace WebCore {
 
 class DirectConvolver {
@@ -45,7 +49,10 @@ public:
 private:
     size_t m_inputBlockSize;
 
-    AudioFloatArray m_inputBuffer;
+#if USE(WEBAUDIO_IPP)
+    AudioFloatArray m_overlayBuffer;
+#endif // USE(WEBAUDIO_IPP)
+    AudioFloatArray m_buffer;
 };
 
 } // namespace WebCore
