ba5f83ae775055ad29ff5dff5cef3706b74095d7
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 08:32:42 +0000

    Handle ESC key on Chrome OS view certificate window.
    The window is not a modal dialog and therefore handle ESC key here.
    
    Original CL (reverted) was:
    https://chromiumcodereview.appspot.com/9423053/
    
    BUG=114203
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9810005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/certificate_viewer.js
+++ chrome/browser/resources/certificate_viewer.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,6 +20,12 @@ cr.define('cert_viewer', function() {
     i18nTemplate.process(document, templateData);
     stripGtkAccessorKeys();
     chrome.send('requestCertificateInfo');
+    // TODO(kochi): ESC key should be handled in the views window side.
+    document.addEventListener('keydown', function(e) {
+      if (e.keyCode == 27) {  // ESC
+        chrome.send('DialogClose');
+      }
+    });
   }
 
   /**
