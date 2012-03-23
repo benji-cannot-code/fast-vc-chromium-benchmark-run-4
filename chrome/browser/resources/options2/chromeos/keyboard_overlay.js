56520005b1b37f95856fc47d1c499df3cb1f4050
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 01:40:33 +0000

    Make keyboard settings into a SettingsDialog
    BUG=119352
    TEST=OK and Cancel buttons work as expected
    NOTRY=true
    
    Review URL: http://codereview.chromium.org/9835027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128386 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/chromeos/keyboard_overlay.js
+++ chrome/browser/resources/options2/chromeos/keyboard_overlay.js
@@ -4,30 +4,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('options', function() {
-  /** @const */ var OptionsPage = options.OptionsPage;
 
   /**
    * Encapsulated handling of the keyboard overlay.
    * @constructor
    */
   function KeyboardOverlay() {
-    OptionsPage.call(this, 'keyboard-overlay',
-                     templateData.keyboardOverlayTitle, 'keyboard-overlay');
+    options.SettingsDialog.call(this, 'keyboard-overlay',
+        templateData.keyboardOverlayTitle,
+        'keyboard-overlay',
+        $('keyboard-confirm'), $('keyboard-cancel'));
   }
 
   cr.addSingletonGetter(KeyboardOverlay);
 
   KeyboardOverlay.prototype = {
-    __proto__: OptionsPage.prototype,
-
-    /** @inheritDoc */
-    initializePage: function() {
-      OptionsPage.prototype.initializePage.call(this);
-
-      $('keyboard-overlay-dismiss-button').onclick = function() {
-        OptionsPage.closeOverlay();
-      };
-    }
+    __proto__: options.SettingsDialog.prototype,
   };
 
   // Export
