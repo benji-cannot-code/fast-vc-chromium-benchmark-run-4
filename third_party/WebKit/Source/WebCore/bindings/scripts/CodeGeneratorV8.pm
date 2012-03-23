51453986cbe632f51e297d5aae3c160bf04a9b57
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 02:38:05 +0000

    [v8] wrapSlow methods should ref underlying object before creating wrapper https://bugs.webkit.org/show_bug.cgi?id=81919
    Reviewed by Adam Barth.
    
    Because instatiating the wrapper can trigger GC, it's important that
    wrapSlow() hold a reference to an object when creating a wrapper for
    that object. Once the V8 wrapper exists and is associated with the object,
    the reference can be handed off (via leakRef) to be handled by the normal
    binding code logic (where derefObject is called if the handle is GCed).
    
    Binding tests have been updated to reflect this change.
    
    Testing the change directly is hard. Any test landed today would only
    be a valid test until V8's logic about when to GC changes, at which point
    it would become dead weight. So I don't think it's worth landing a
    layout test along with this.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader): Make wrapSlow take a PassRefPtr for RefCounted objects.
    (GenerateToV8Converters): Get rid of the explicit call to ref() and
    instead call leakRef() when adding a RefCounted object to the DOM map.
    (GetPassRefPtrType): Helper to generate "PassRefPtr<T>", or "PassRefPtr<T<U> >" as appropriate.
    * bindings/scripts/test/V8/V8Float64Array.cpp:
    (WebCore::V8Float64Array::wrapSlow):
    * bindings/scripts/test/V8/V8Float64Array.h:
    (V8Float64Array):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore::V8TestActiveDOMObject::wrapSlow):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.h:
    (V8TestActiveDOMObject):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.cpp:
    (WebCore::V8TestCustomNamedGetter::wrapSlow):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.h:
    (V8TestCustomNamedGetter):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore::V8TestEventConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventConstructor.h:
    (V8TestEventConstructor):
    * bindings/scripts/test/V8/V8TestEventTarget.cpp:
    (WebCore::V8TestEventTarget::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventTarget.h:
    (V8TestEventTarget):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore::V8TestInterface::wrapSlow):
    * bindings/scripts/test/V8/V8TestInterface.h:
    (V8TestInterface):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp:
    (WebCore::V8TestMediaQueryListListener::wrapSlow):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.h:
    (V8TestMediaQueryListListener):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore::V8TestNamedConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestNamedConstructor.h:
    (V8TestNamedConstructor):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::V8TestObj::wrapSlow):
    * bindings/scripts/test/V8/V8TestObj.h:
    (V8TestObj):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::V8TestSerializedScriptValueInterface::wrapSlow):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.h:
    (V8TestSerializedScriptValueInterface):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -423,9 +423,10 @@ END
 END
     }
 
+    my $wrapSlowArgumentType = GetPassRefPtrType($nativeType);
     push(@headerContent, <<END);
 private:
-    static v8::Handle<v8::Object> wrapSlow(${nativeType}*);
+    static v8::Handle<v8::Object> wrapSlow(${wrapSlowArgumentType});
 };
 
 END
@@ -3109,10 +3110,11 @@ sub GenerateToV8Converters
     my $domMapFunction = GetDomMapFunction($dataNode, $interfaceName);
     my $forceNewObjectInput = IsDOMNodeType($interfaceName) ? ", bool forceNewObject" : "";
     my $forceNewObjectCall = IsDOMNodeType($interfaceName) ? ", forceNewObject" : "";
+    my $wrapSlowArgumentType = GetPassRefPtrType($nativeType);
 
     push(@implContent, <<END);
 
-v8::Handle<v8::Object> ${className}::wrapSlow(${nativeType}* impl)
+v8::Handle<v8::Object> ${className}::wrapSlow(${wrapSlowArgumentType} impl)
 {
     v8::Handle<v8::Object> wrapper;
 END
@@ -3135,7 +3137,7 @@ END
         push(@implContent, <<END);
     if (proxy && proxy->windowShell()->context().IsEmpty() && proxy->windowShell()->initContextIfNeeded()) {
         // initContextIfNeeded may have created a wrapper for the object, retry from the start.
-        return ${className}::wrap(impl);
+        return ${className}::wrap(impl.get());
     }
 END
     }
@@ -3167,7 +3169,7 @@ END
     }
 
     push(@implContent, <<END);
-    wrapper = V8DOMWrapper::instantiateV8Object(proxy, &info, impl);
+    wrapper = V8DOMWrapper::instantiateV8Object(proxy, &info, impl.get());
 END
     if (IsNodeSubType($dataNode) || IsVisibleAcrossOrigins($dataNode)) {
         push(@implContent, <<END);
@@ -3180,10 +3182,7 @@ END
     push(@implContent, <<END);
     if (UNLIKELY(wrapper.IsEmpty()))
         return wrapper;
-END
-    push(@implContent, "\n    impl->ref();\n") if IsRefPtrType($interfaceName);
 
-    push(@implContent, <<END);
     v8::Persistent<v8::Object> wrapperHandle = v8::Persistent<v8::Object>::New(wrapper);
 
     if (!hasDependentLifetime)
@@ -3193,12 +3192,9 @@ END
         push(@implContent, <<END);
     wrapperHandle.SetWrapperClassId(v8DOMSubtreeClassId);
 END
-    }    
+    }
     push(@implContent, <<END);
-    ${domMapFunction}.set(impl, wrapperHandle);
-END
-
-    push(@implContent, <<END);
+    ${domMapFunction}.set(impl.leakRef(), wrapperHandle);
     return wrapper;
 }
 END
@@ -3928,6 +3924,14 @@ sub GetRuntimeEnableFunctionName
 
     # Otherwise return a function named RuntimeEnabledFeatures::{methodName}Enabled().
     return "RuntimeEnabledFeatures::" . $codeGenerator->WK_lcfirst($signature->name) . "Enabled";
+}
+
+sub GetPassRefPtrType
+{
+    my $className = shift;
+
+    my $angleBracketSpace = $className =~ />$/ ? " " : "";
+    return "PassRefPtr<${className}${angleBracketSpace}>";
 }
 
 sub DebugPrint
