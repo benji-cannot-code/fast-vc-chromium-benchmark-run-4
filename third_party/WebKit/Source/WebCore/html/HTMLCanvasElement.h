0ae58f3f620cce68bfff5122a2e5fd3ed94ef4d8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 16:58:53 +0000

    Reuse buffer allocation if canvas size does not change https://bugs.webkit.org/show_bug.cgi?id=80871
    Patch by Sami Kyostila <skyostil@chromium.org> on 2012-03-23
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    If the user changes the width or height attributes of a canvas element,
    the contents of the canvas should be cleared and the context state
    should be reset. This has become a common idiom to clear the canvas
    "efficiently" at the start of a frame.
    
    Previously, this code path triggered a full reallocation of the image
    buffer backing the canvas, leading to reduced performance. This patch
    implements an optimization where we reuse the previous image buffer
    allocation if the size of the canvas did not change. Also, instead of
    clearing the canvas every time its dimensions are touched, we only clear
    if anything has been drawn into the canvas since it was previously
    cleared.
    
    Note that for now this optimization only applies for 2D canvases,
    since it is not entirely clear how touching the dimensions of a WebGL
    canvas should work.
    
    Test: fast/canvas/canvas-resize-after-paint-without-layout.html +
          existing layout tests for canvas resetting.
    
    * html/HTMLCanvasElement.cpp:
    (WebCore::HTMLCanvasElement::HTMLCanvasElement):
    (WebCore::HTMLCanvasElement::reset):
    (WebCore::HTMLCanvasElement::createImageBuffer): Save the initial
    graphics context state so we can restore it without creating a new
    context.
    (WebCore::HTMLCanvasElement::clearImageBuffer):
    (WebCore):
    (WebCore::HTMLCanvasElement::clearCopiedImage):
    * html/HTMLCanvasElement.h:
    (HTMLCanvasElement):
    * html/canvas/CanvasRenderingContext2D.cpp:
    (WebCore::CanvasRenderingContext2D::reset): No need to notify the
    compositor when the context is reset, because clearing the image buffer
    does the same thing. We can also skip the notification if we did not
    have an image buffer at the time of the reset, because the reset will
    not have any visual impact in this case. Finally, if the canvas size
    did change, the notification is also unnecessary because of the call
    to RenderObject::repaint() from HTMLCanvasElement::reset().
    
    LayoutTests:
    
    Add layout test to check canvas resizing without changing its layout size.
    
    We also update the expected image one canvas clearing test. The test
    is setting the size of a canvas and expecting it to be cleared in the process.
    With the optimization to retain the underlying ImageBuffer, we no longer call
    RenderReplaced::repaint() as a part of this process. This function used to
    repaint both the canvas itself (100x50) as well as its local selection
    rectangle (100x54).
    
    In this case the local selection rectangle is larger than the canvas because
    the canvas is contained within an anonymous RenderBlock that also has two empty
    text nodes. The extra 4 pixels are needed for drawing the selection rectangle
    around any descenders in the the text of those nodes.
    
    Since clearing the canvas has no effect on the selection rectangle, we only
    need to repaint the area of the canvas itself.
    
    * fast/canvas/canvas-resize-after-paint-without-layout.html: Added.
    * fast/canvas/canvas-resize-after-paint-without-layout-expected.txt: Added.
    * platform/chromium-linux/fast/canvas/canvas-resize-after-paint-without-layout-expected.png: Added.
    * platform/chromium-linux/fast/canvas/setWidthResetAfterForcedRender-expected.png: Updated.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111872 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.h
@@ -46,6 +46,7 @@ namespace WebCore {
 class CanvasContextAttributes;
 class CanvasRenderingContext;
 class GraphicsContext;
+class GraphicsContextStateSaver;
 class HTMLCanvasElement;
 class Image;
 class ImageData;
@@ -144,6 +145,7 @@ private:
     void reset();
 
     void createImageBuffer() const;
+    void clearImageBuffer() const;
 
     void setSurfaceSize(const IntSize&);
 
@@ -167,7 +169,9 @@ private:
 
     // m_createdImageBuffer means we tried to malloc the buffer.  We didn't necessarily get it.
     mutable bool m_hasCreatedImageBuffer;
+    mutable bool m_didClearImageBuffer;
     mutable OwnPtr<ImageBuffer> m_imageBuffer;
+    mutable OwnPtr<GraphicsContextStateSaver> m_contextStateSaver;
     
     mutable RefPtr<Image> m_presentedImage;
     mutable RefPtr<Image> m_copiedImage; // FIXME: This is temporary for platforms that have to copy the image buffer to render (and for CSSCanvasValue).
