c33790245fa5dbd70751a89b5565583f2a454468
rnk@chromium.org <rnk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 16:25:43 +0000

    Revert "Refactor BaseTimer to avoid spamming the MessageLoop with orphaned tasks."
    This reverts commit r128412, which is causing DCHECKs in thread
    destruction in media unittests.
    
    BUG=119714,119750
    TBR=sky@chromium.org,petermayo@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9839059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128506 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/webkit_thread.cc
+++ content/browser/in_process_webkit/webkit_thread.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -67,10 +67,6 @@ void WebKitThread::InternalWebKitThread::Init() {
 void WebKitThread::InternalWebKitThread::CleanUp() {
   DCHECK(webkit_platform_support_.get());
   WebKit::shutdown();
-  // Delete BrowserWebKitPlatformSupportImpl now while on the same thread that
-  // constructed it. (This prevents the WebKit shared timer from being destroyed
-  // on a different thread than the one using it.)
-  webkit_platform_support_.reset();
 }
 
 }  // namespace content
