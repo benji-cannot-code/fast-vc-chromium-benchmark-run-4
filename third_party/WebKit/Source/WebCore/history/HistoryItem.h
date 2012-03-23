753c35f5a80b4e90ab4cb4c1039055617cf521cf
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-23 03:20:39 +0000

    [BlackBerry] Add HistoryItemViewState for BlackBerry port https://bugs.webkit.org/show_bug.cgi?id=81867
    Reviewed by Rob Buis.
    
    HistoryItemViewState is the blackberry specific field of HistoryItem.
    It's used to maintain the porting specific view state.
    
    No new test because the port can't be built yet.
    
    * history/HistoryItem.h:
    (HistoryItem):
    (WebCore::HistoryItem::viewState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/history/HistoryItem.h
+++ third_party/WebKit/Source/WebCore/history/HistoryItem.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2006, 2008, 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2012 Research In Motion Limited. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -43,6 +44,10 @@ typedef struct objc_object* id;
 #include <QVariant>
 #include <QByteArray>
 #include <QDataStream>
+#endif
+
+#if PLATFORM(BLACKBERRY)
+#include "HistoryItemViewState.h"
 #endif
 
 namespace WebCore {
@@ -200,6 +205,10 @@ public:
     QDataStream& saveState(QDataStream& out, int version) const;
 #endif
 
+#if PLATFORM(BLACKBERRY)
+    HistoryItemViewState& viewState() { return m_viewState; }
+#endif
+
 #ifndef NDEBUG
     int showTree() const;
     int showTreeWithIndent(unsigned indentLevel) const;
@@ -290,6 +299,9 @@ private:
     QVariant m_userData;
 #endif
 
+#if PLATFORM(BLACKBERRY)
+    HistoryItemViewState m_viewState;
+#endif
 }; //class HistoryItem
 
 } //namespace WebCore
