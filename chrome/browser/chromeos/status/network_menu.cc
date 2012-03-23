7f36bd56005f664b64ca49b3de974e7f8d43b8c9
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 17:44:14 +0000

    Update network icon resources.
    BUG=chromium:113685
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9837013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128519 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/network_menu.cc
+++ chrome/browser/chromeos/status/network_menu.cc
@@ -551,7 +551,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
       flag |= FLAG_ASSOCIATED;
     SkBitmap icon;
     icon = NetworkMenuIcon::GetBitmap(ethernet_network,
-                                      NetworkMenuIcon::SIZE_SMALL);
+                                      NetworkMenuIcon::COLOR_DARK);
     menu_items_.push_back(MenuItem(ui::MenuModel::TYPE_COMMAND,
                                    label, icon, std::string(), flag));
   }
@@ -593,7 +593,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
       if (ShouldHighlightNetwork(wifi_networks[i]))
         flag |= FLAG_ASSOCIATED;
       const SkBitmap icon = NetworkMenuIcon::GetBitmap(wifi_networks[i],
-          NetworkMenuIcon::SIZE_SMALL);
+          NetworkMenuIcon::COLOR_DARK);
       menu_items_.push_back(
           MenuItem(ui::MenuModel::TYPE_COMMAND,
                    label, icon, wifi_networks[i]->service_path(), flag));
@@ -604,7 +604,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
         ui::MenuModel::TYPE_COMMAND,
         l10n_util::GetStringUTF16(IDS_OPTIONS_SETTINGS_OTHER_WIFI_NETWORKS),
         NetworkMenuIcon::GetConnectedBitmap(NetworkMenuIcon::ARCS,
-                                            NetworkMenuIcon::SIZE_SMALL),
+                                            NetworkMenuIcon::COLOR_DARK),
         std::string(), FLAG_ADD_WIFI));
   }
 
@@ -664,7 +664,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
       if (isActive)
         flag |= FLAG_ASSOCIATED;
       const SkBitmap icon = NetworkMenuIcon::GetBitmap(cell_networks[i],
-          NetworkMenuIcon::SIZE_SMALL);
+          NetworkMenuIcon::COLOR_DARK);
       menu_items_.push_back(
           MenuItem(ui::MenuModel::TYPE_COMMAND,
                    label, icon, cell_networks[i]->service_path(), flag));
@@ -720,7 +720,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
             l10n_util::GetStringUTF16(
                 IDS_OPTIONS_SETTINGS_OTHER_CELLULAR_NETWORKS),
             NetworkMenuIcon::GetDisconnectedBitmap(NetworkMenuIcon::BARS,
-                                                   NetworkMenuIcon::SIZE_SMALL),
+                                                   NetworkMenuIcon::COLOR_DARK),
             std::string(), FLAG_ADD_CELLULAR));
       }
     }
@@ -739,8 +739,7 @@ void MainMenuModel::InitMenuItems(bool should_open_button_options) {
   if (StatusAreaViewChromeos::IsBrowserMode()) {
     if (cros->connected_network() || cros->virtual_network_connected()) {
       menu_items_.push_back(MenuItem());  // Separator
-      const SkBitmap icon = NetworkMenuIcon::GetVpnBitmap(
-          NetworkMenuIcon::SIZE_SMALL);
+      const SkBitmap icon = NetworkMenuIcon::GetVpnBitmap();
       menu_items_.push_back(MenuItem(
           ui::MenuModel::TYPE_SUBMENU,
           l10n_util::GetStringUTF16(IDS_STATUSBAR_NETWORK_PRIVATE_NETWORKS),
@@ -874,7 +873,7 @@ void VPNMenuModel::InitMenuItems(bool should_open_button_options) {
     if (ShouldHighlightNetwork(vpn))
       flag |= FLAG_ASSOCIATED;
     const SkBitmap icon = NetworkMenuIcon::GetBitmap(vpn,
-        NetworkMenuIcon::SIZE_SMALL);
+        NetworkMenuIcon::COLOR_DARK);
     menu_items_.push_back(
         MenuItem(ui::MenuModel::TYPE_COMMAND,
                  label, icon, vpn->service_path(), flag));
