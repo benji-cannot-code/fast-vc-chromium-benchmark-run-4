89d8b3e36170fc694472cde92076f331b55b8e7a
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 09:24:45 +0000

    Enabled resize by mouse in detached panels on Mac.
    This adds an overlay view on top of the panel to intercept mouse messages and redirect them into Panel resizing API.
    
    It also adds a NativePanel::EnableResizeByMouse(bool) method which is piped through in this patch but will be used in the next patch, to enable/disable cursor rects.
    
    Review URL: http://codereview.chromium.org/9834023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_view.cc
+++ chrome/browser/ui/panels/panel_browser_view.cc
@@ -595,6 +595,9 @@ bool PanelBrowserView::IsAnimatingBounds() const {
   return bounds_animator_.get() && bounds_animator_->is_animating();
 }
 
+void PanelBrowserView::EnableResizeByMouse(bool enable) {
+}
+
 // NativePanelTesting implementation.
 class NativePanelTestingWin : public NativePanelTesting {
  public:
