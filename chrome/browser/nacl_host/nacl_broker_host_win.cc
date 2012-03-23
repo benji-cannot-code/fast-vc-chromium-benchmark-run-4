216e23ee72945693c83feaae12faf4a118661f13
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 22:58:33 +0000

    NaCl: Remove unused "stopping_" field
    This field is set but never read.
    
    BUG=none
    TEST=compile
    
    
    Review URL: http://codereview.chromium.org/9834057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_broker_host_win.cc
+++ chrome/browser/nacl_host/nacl_broker_host_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_child_process_host.h"
 #include "content/public/common/child_process_host.h"
 
-NaClBrokerHost::NaClBrokerHost()
-    : stopping_(false) {
+NaClBrokerHost::NaClBrokerHost() {
   process_.reset(content::BrowserChildProcessHost::Create(
       content::PROCESS_TYPE_NACL_BROKER, this));
 }
@@ -73,6 +72,5 @@ void NaClBrokerHost::OnLoaderLaunched(const std::wstring& loader_channel_id,
 }
 
 void NaClBrokerHost::StopBroker() {
-  stopping_ = true;
   process_->Send(new NaClProcessMsg_StopBroker());
 }
