64fbab8a3a7b1dadb997f0e11cd0cf226d583dcf
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-24 01:13:46 +0000

    Fix Chromium 10.6/10.5 tests that broke with r111917. There's a bug with garden-o-matic when rebaselining all the Chromium Mac ports. Other ports seem fine.
    git-svn-id: svn://svn.chromium.org/blink/trunk@111961 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -616,7 +616,7 @@ void RenderFlexibleBox::layoutFlexItems(bool relayoutChildren)
 
         layoutAndPlaceChildren(crossAxisOffset, orderedChildren, childSizes, availableFreeSpace, lineContexts);
     }
-
+//
     alignChildren(flexIterator, lineContexts);
 
     if (style()->flexWrap() == FlexWrapReverse)
