4808cb9bbc3413fbbc500cea656084dd85e03fe3
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 00:11:59 +0000

    Minor fixes to BackoffEntry.
    1. maximum_backoff_ms and entry_lifetime_ms now are int64.
    2. Protection from integer overflow when calculating delay.
    
    Review URL: https://chromiumcodereview.appspot.com/9838069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128617 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/backoff_entry.cc
+++ net/base/backoff_entry.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <limits>
 
 #include "base/logging.h"
 #include "base/rand_util.h"
@@ -122,10 +123,13 @@ base::TimeTicks BackoffEntry::CalculateReleaseTime() const {
   delay *= pow(policy_->multiply_factor, effective_failure_count - 1);
   delay -= base::RandDouble() * policy_->jitter_factor * delay;
 
+  const int64 kMaxInt64 = std::numeric_limits<int64>::max();
+  int64 delay_int = (delay > kMaxInt64) ?
+      kMaxInt64 : static_cast<int64>(delay + 0.5);
+
   // Ensure that we do not exceed maximum delay.
-  int64 delay_int = static_cast<int64>(delay + 0.5);
-  delay_int = std::min(delay_int,
-                       static_cast<int64>(policy_->maximum_backoff_ms));
+  if (policy_->maximum_backoff_ms >= 0)
+    delay_int = std::min(delay_int, policy_->maximum_backoff_ms);
 
   // Never reduce previously set release horizon, e.g. due to Retry-After
   // header.
