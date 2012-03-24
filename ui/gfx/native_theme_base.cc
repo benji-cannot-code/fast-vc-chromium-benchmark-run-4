350a4a1529a9a6e49de50d71ce38f5bf4a8c01d3
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 01:41:09 +0000

    aura: Update scrollbar theme.
    BUG=105894
    TEST=manual
    TBR=sky
    
    Review URL: https://chromiumcodereview.appspot.com/9837072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128647 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/native_theme_base.cc
+++ ui/gfx/native_theme_base.cc
@@ -34,6 +34,9 @@ const SkColor kSliderThumbBorderDarkGrey =
 
 const SkColor kMenuPopupBackgroundColor = SkColorSetRGB(210, 225, 246);
 
+const unsigned int kDefaultScrollbarWidth = 15;
+const unsigned int kDefaultScrollbarButtonLength = 14;
+
 // Get lightness adjusted color.
 SkColor BrightenColor(const color_utils::HSL& hsl, SkAlpha alpha,
     double lightness_amount) {
@@ -50,9 +53,6 @@ SkColor BrightenColor(const color_utils::HSL& hsl, SkAlpha alpha,
 }  // namespace
 
 namespace gfx {
-
-unsigned int NativeThemeBase::button_length_ = 14;
-unsigned int NativeThemeBase::scrollbar_width_ = 15;
 
 gfx::Size NativeThemeBase::GetPartSize(Part part,
                                        State state,
@@ -84,10 +84,10 @@ gfx::Size NativeThemeBase::GetPartSize(Part part,
       return gfx::Size(kCheckboxAndRadioWidth, kCheckboxAndRadioHeight);
     case kScrollbarDownArrow:
     case kScrollbarUpArrow:
-      return gfx::Size(scrollbar_width_, button_length_);
+      return gfx::Size(scrollbar_width_, scrollbar_button_length_);
     case kScrollbarLeftArrow:
     case kScrollbarRightArrow:
-      return gfx::Size(button_length_, scrollbar_width_);
+      return gfx::Size(scrollbar_button_length_, scrollbar_width_);
     case kScrollbarHorizontalThumb:
       // This matches Firefox on Linux.
       return gfx::Size(2 * scrollbar_width_, scrollbar_width_);
@@ -205,7 +205,9 @@ void NativeThemeBase::Paint(SkCanvas* canvas,
   }
 }
 
-NativeThemeBase::NativeThemeBase() {
+NativeThemeBase::NativeThemeBase()
+    : scrollbar_width_(kDefaultScrollbarWidth),
+      scrollbar_button_length_(kDefaultScrollbarButtonLength) {
 }
 
 NativeThemeBase::~NativeThemeBase() {
