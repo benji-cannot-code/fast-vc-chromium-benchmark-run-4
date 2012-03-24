8a1a3614c0edfa20ea193d170a6472cdaf8594ac
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 00:03:56 +0000

    Hook up new DomStorage backing for for single-process chromium too.
    TBR=jam
    BUG=106763
    Review URL: https://chromiumcodereview.appspot.com/9826040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128615 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webstoragenamespace_impl.h
+++ content/renderer/renderer_webstoragenamespace_impl.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "content/common/dom_storage_common.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageNamespace.h"
+#include "webkit/dom_storage/dom_storage_types.h"
+// The above is to include the ENABLE_NEW_DOM_STORAGE_BACKEND flag
+// in all consumers.
 
 class RendererWebStorageNamespaceImpl : public WebKit::WebStorageNamespace {
  public:
@@ -25,10 +28,6 @@ class RendererWebStorageNamespaceImpl : public WebKit::WebStorageNamespace {
   virtual void close();
 
  private:
-  // Used during lazy initialization of namespace_id_.
-  const DOMStorageType storage_type_;
-
-  // Our namespace ID.
   int64 namespace_id_;
 };
 
