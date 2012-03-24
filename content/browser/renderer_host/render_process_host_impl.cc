bc59955d3a5697640f558d66da8194147573bf81
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 21:54:50 +0000

    Remove old unused disable-glsl-translator flag
    TEST=none
    BUG=none
    
    
    Review URL: http://codereview.chromium.org/9811020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128779 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -668,7 +668,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableFileSystem,
     switches::kDisableGeolocation,
     switches::kDisableGLMultisampling,
-    switches::kDisableGLSLTranslator,
     switches::kDisableGpuDriverBugWorkarounds,
     switches::kDisableGpuVsync,
     switches::kDisableJavaScriptI18NAPI,
