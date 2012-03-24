922f74aac74f65625c4b180bee08500e6659d35b
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 17:44:01 +0000

    Add app shortcut context menu
    BUG=119518
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9836081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128762 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/launcher/chrome_launcher_delegate.cc
+++ chrome/browser/ui/views/ash/launcher/chrome_launcher_delegate.cc
@@ -313,6 +313,13 @@ void ChromeLauncherDelegate::PinAppWithID(const std::string& app_id,
   PersistPinnedState();
 }
 
+void ChromeLauncherDelegate::SetAppType(ash::LauncherID id, AppType app_type) {
+  if (id_to_item_map_.find(id) == id_to_item_map_.end())
+    return;
+
+  id_to_item_map_[id].app_type = app_type;
+}
+
 void ChromeLauncherDelegate::UnpinAppsWithID(const std::string& app_id) {
   for (IDToItemMap::iterator i = id_to_item_map_.begin();
        i != id_to_item_map_.end(); ) {
@@ -364,7 +371,7 @@ string16 ChromeLauncherDelegate::GetTitle(const ash::LauncherItem& item) {
 
 ui::MenuModel* ChromeLauncherDelegate::CreateContextMenu(
     const ash::LauncherItem& item) {
-  return new LauncherContextMenu(this, item.id);
+  return new LauncherContextMenu(this, item);
 }
 
 ash::LauncherID ChromeLauncherDelegate::GetIDByWindow(
