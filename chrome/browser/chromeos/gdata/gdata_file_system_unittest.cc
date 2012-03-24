337886f37c945b3d7891835caef671b5089ceb5a
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 17:39:42 +0000

    gdata: Fix use-after-free in GDataFileSystem shutdown
    There were races in the shutdown logic.
    
    BUG=119712
    TEST=git try --bot=linux_chromeos_valgrind --testfilter=unit_tests:GDataFileSystem.*
    
    Review URL: https://chromiumcodereview.appspot.com/9834071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
@@ -1313,8 +1313,7 @@ TEST_F(GDataFileSystemTest, MoveFileToInvalidPath) {
   EXPECT_TRUE(FindFile(dest_file_path) == NULL);
 }
 
-// Race in shutdown. http://crbug.com/119712
-TEST_F(GDataFileSystemTest, DISABLED_RemoveFiles) {
+TEST_F(GDataFileSystemTest, RemoveFiles) {
   EXPECT_CALL(*mock_sync_client_, OnCacheInitialized()).Times(1);
 
   LoadRootFeedDocument("root_feed.json");
@@ -1367,7 +1366,8 @@ TEST_F(GDataFileSystemTest, DISABLED_RemoveFiles) {
   // Try removing root file element.
   EXPECT_FALSE(RemoveFile(FilePath(FILE_PATH_LITERAL("gdata"))));
 
-  message_loop_.RunAllPending();  // Wait to get our result.
+  // Need this to ensure OnDirectoryChanged() is run.
+  RunAllPendingForCache();
 }
 
 TEST_F(GDataFileSystemTest, CreateDirectory) {
