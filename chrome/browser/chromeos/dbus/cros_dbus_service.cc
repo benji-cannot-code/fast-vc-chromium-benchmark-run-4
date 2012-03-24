bbb71eaee1db33ca08dc3c140e62a75e31d0b977
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 01:54:49 +0000

    chromeos: Remove browser_thread.h includes in chromeos/dbus
    BUG=119583
    TEST=build success
    
    
    Review URL: http://codereview.chromium.org/9808076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/cros_dbus_service.cc
+++ chrome/browser/chromeos/dbus/cros_dbus_service.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/threading/platform_thread.h"
 #include "chrome/browser/chromeos/dbus/proxy_resolution_service_provider.h"
-#include "content/public/browser/browser_thread.h"
 #include "dbus/bus.h"
 #include "dbus/exported_object.h"
 #include "dbus/object_path.h"
