ce63819327a324962e8f6a2285d1bb07ae3c8324
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-24 05:54:38 +0000

    [chromium] When prepainting fails, tiles dirty rects may be cleared https://bugs.webkit.org/show_bug.cgi?id=82107
    Patch by Dana Jansens <danakj@chromium.org> on 2012-03-23
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    When prepainting, if a tile is unable to be reserved due to memory
    limits, we bail out of prepareToUpdateTiles. But we would have
    cleared the dirty rect of any previous tiles. This leaves them
    in a bad state where their textures are reserved, but their textureIds
    are set to 0, and they are not marked dirty. This means that they will
    not be updated and displayed if they become visible, since it is
    assumed that valid textures with zero textureId must have a dirty
    region.
    
    We fix this by not clearing the dirty rects until we know we are
    going to update the layer.
    
    Unit test: TiledLayerChromiumTest.pushTilesAfterIdlePaintFailed
    
    * platform/graphics/chromium/TiledLayerChromium.cpp:
    (WebCore::TiledLayerChromium::prepareToUpdateTiles):
    * platform/graphics/chromium/cc/CCTiledLayerImpl.cpp:
    (WebCore::CCTiledLayerImpl::hasTextureIdForTileAt):
    (WebCore):
    * platform/graphics/chromium/cc/CCTiledLayerImpl.h:
    (CCTiledLayerImpl):
    
    Source/WebKit/chromium:
    
    * tests/TiledLayerChromiumTest.cpp:
    (WTF::FakeTextureAllocator::createTexture):
    (WTF::FakeLayerTextureUpdater::Texture::updateRect):
    (FakeCCTiledLayerImpl):
    (WTF::FakeCCTiledLayerImpl::hasTextureIdForTileAt):
    (WTF::TEST):
    (WTF):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@111978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCTiledLayerImpl.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCTiledLayerImpl.h
@@ -76,6 +76,7 @@ protected:
     explicit CCTiledLayerImpl(int id);
     // Exposed for testing.
     bool hasTileAt(int, int) const;
+    bool hasTextureIdForTileAt(int, int) const;
 
     virtual TransformationMatrix quadTransform() const;
 
