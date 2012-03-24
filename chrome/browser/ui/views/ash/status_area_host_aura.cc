a8f39d546a23a5d6e311ba91352362b4821f2f03
mbolohan@chromium.org <mbolohan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 06:02:28 +0000

    Fix stale titles from appearing in proxy settings dialog by creating a new dialog for each show.
    BUG=chromium-os:25941
    TEST=see bug
    
    
    Review URL: http://codereview.chromium.org/9835026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128699 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/status_area_host_aura.cc
+++ chrome/browser/ui/views/ash/status_area_host_aura.cc
@@ -154,9 +154,9 @@ void StatusAreaHostAura::ExecuteStatusAreaCommand(
         chromeos::BaseLoginDisplayHost::default_host()) {
       gfx::NativeWindow native_window =
           chromeos::BaseLoginDisplayHost::default_host()->GetNativeWindow();
-      proxy_settings_dialog_.reset(new chromeos::ProxySettingsDialog(
-          NULL, native_window));
-      proxy_settings_dialog_->Show();
+      chromeos::ProxySettingsDialog* dialog =
+          new chromeos::ProxySettingsDialog(NULL, native_window);
+      dialog->Show();
     } else {
       NOTREACHED();
     }
@@ -166,10 +166,9 @@ void StatusAreaHostAura::ExecuteStatusAreaCommand(
       gfx::NativeWindow native_window =
           chromeos::ScreenLocker::default_screen_locker()->delegate()->
               GetNativeWindow();
-      proxy_settings_dialog_.reset(new chromeos::ProxySettingsDialog(
-                                   NULL,
-                                   native_window));
-      proxy_settings_dialog_->Show();
+      chromeos::ProxySettingsDialog* dialog =
+          new chromeos::ProxySettingsDialog(NULL, native_window);
+      dialog->Show();
     } else {
       NOTREACHED();
     }
