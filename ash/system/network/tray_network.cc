17e85edff9bdd5fc1f56c286010fe3515b398825
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 06:07:08 +0000

    When screen locked (LOGGED_IN_LOCKED): (1) the shutdown etc. buttons should not show up (2) the date item should not be 'activatable' (i.e no hover underline, no click) (3) the settings entry should not show up (4) the IME 'customize' option should not show up (still possible to change IME) {5} Cannot add bluethooth devices or enable/disable bluetooth (still possible to toggle bluetooth connection for individual devices) (5) clicking the network entries should not do anything (can't change network being used) (6) Cannot enable/disable wifi/cellular network
    When user is signed out (LOGGED_IN_NONE):
    (1) Hide IME settings
    (2) Hide Bluetooth settings (can still enable and disable bluetooth)
    
    BUG=119503
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/9839074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128703 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/network/tray_network.cc
+++ ash/system/network/tray_network.cc
@@ -210,9 +210,12 @@ class NetworkDetailedView : public views::View,
 
     AppendHeaderEntry();
     AppendNetworkEntries();
-    AppendNetworkExtra();
-    AppendNetworkToggles();
-    AppendSettingsEntry();
+
+    if (login_ != user::LOGGED_IN_LOCKED) {
+      AppendNetworkExtra();
+      AppendNetworkToggles();
+      AppendSettingsEntry();
+   }
 
     Layout();
   }
@@ -418,9 +421,14 @@ class NetworkDetailedView : public views::View,
       info_bubble_->GetWidget()->Close();
       info_bubble_ = NULL;
     }
-    if (sender == header_) {
+
+    if (sender == header_)
       Shell::GetInstance()->tray()->ShowDefaultView();
-    } else if (sender == settings_) {
+
+    if (login_ == user::LOGGED_IN_LOCKED)
+      return;
+
+    if (sender == settings_) {
       delegate->ShowNetworkSettings();
     } else if (sender == proxy_settings_) {
       delegate->ChangeProxySettings();
--- chrome/browser/chromeos/system/ash_system_tray_delegate.cc
+++ chrome/browser/chromeos/system/ash_system_tray_delegate.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/base_login_display_host.h"
 #include "chrome/browser/chromeos/login/login_display_host.h"
 #include "chrome/browser/chromeos/login/message_bubble.h"
+#include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/mobile_config.h"
@@ -206,8 +207,12 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
 
   virtual ash::user::LoginStatus GetUserLoginStatus() const OVERRIDE {
     UserManager* manager = UserManager::Get();
+    const chromeos::ScreenLocker* locker =
+        chromeos::ScreenLocker::default_screen_locker();
     if (!manager->IsUserLoggedIn())
       return ash::user::LOGGED_IN_NONE;
+    if (locker && locker->locked())
+      return ash::user::LOGGED_IN_LOCKED;
     if (manager->IsCurrentUserOwner())
       return ash::user::LOGGED_IN_OWNER;
     if (manager->IsLoggedInAsGuest())
@@ -674,9 +679,11 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
   }
 
   virtual void LockScreen() OVERRIDE {
+    tray_->UpdateAfterLoginStatusChange(GetUserLoginStatus());
   }
 
   virtual void UnlockScreen() OVERRIDE {
+    tray_->UpdateAfterLoginStatusChange(GetUserLoginStatus());
   }
 
   virtual void UnlockScreenFailed() OVERRIDE {
