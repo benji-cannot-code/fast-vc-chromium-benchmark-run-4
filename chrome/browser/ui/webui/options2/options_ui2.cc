72e5ba7eda225a2aec1d1a4f4fbc088f2935e4ea
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-25 01:49:01 +0000

    Extensions: Don't pass the selected unpacked extension path from JS to C++ handler.
    This is an unecessary hoop to jump through since the C++ is the original
    callback of the file dialog.
    
    BUG=117715
    TEST=none
    TBR=sky
    R=csilv,aa
    
    
    Review URL: http://codereview.chromium.org/9703039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128801 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/options_ui2.cc
+++ chrome/browser/ui/webui/options2/options_ui2.cc
@@ -87,13 +87,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using content::RenderViewHost;
-using content::WebContents;
-using content::WebUIMessageHandler;
+
+namespace {
+
+const char kLocalizedStringsFile[] = "strings.js";
+const char kOptionsBundleJsFile[]  = "options_bundle.js";
+
+}  // namespace
 
 namespace options2 {
-
-static const char kLocalizedStringsFile[] = "strings.js";
-static const char kOptionsBundleJsFile[]  = "options_bundle.js";
 
 ////////////////////////////////////////////////////////////////////////////////
 //
