9501ec2173d5ceb2b714ac333a4e6536a09382ec
kevino@webkit.org <kevino@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-25 19:47:29 +0000

    [wx] Unreviewed build fix. Add header needed for Unix builds.
    git-svn-id: svn://svn.chromium.org/blink/trunk@112034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/wx/WebKitSupport/FrameLoaderClientWx.cpp
+++ third_party/WebKit/Source/WebKit/wx/WebKitSupport/FrameLoaderClientWx.cpp
@@ -59,6 +59,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/RefPtr.h>
 
 #include <stdio.h>
+#if OS(UNIX)
+#include <sys/utsname.h>
+#endif
 
 #include "FrameNetworkingContextWx.h"
 #include "WebFrame.h"
