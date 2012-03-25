68c43acec0ddcbffb4e020fb449004f19e4665af
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-25 21:21:29 +0000

    Remove the Throttling.HttpResponseCode histogram.
    The experiment is over, let's get rid of the (big) histogram.
    
    BUG=None
    
    
    Review URL: http://codereview.chromium.org/9826043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128831 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_throttler_unittest.cc
+++ net/url_request/url_request_throttler_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -183,7 +183,6 @@ class URLRequestThrottlerEntryTest : public testing::Test {
 const char* kHistogramNames[] = {
   "Throttling.CustomRetryAfterMs",
   "Throttling.FailureCountAtSuccess",
-  "Throttling.HttpResponseCode",
   "Throttling.PerceivedDowntime",
   "Throttling.RequestThrottled",
   "Throttling.SiteOptedOut",
@@ -301,8 +300,6 @@ TEST_F(URLRequestThrottlerEntryTest, InterfaceUpdateSuccessThenFailure) {
   entry_->UpdateWithResponse("", &success_response);
 
   CalculateHistogramDeltas();
-  ASSERT_EQ(1, samples_["Throttling.HttpResponseCode"].counts(503));
-  ASSERT_EQ(2, samples_["Throttling.HttpResponseCode"].counts(200));
   ASSERT_EQ(1, samples_["Throttling.FailureCountAtSuccess"].counts(1));
   ASSERT_EQ(1, samples_["Throttling.PerceivedDowntime"].TotalCount());
 }
