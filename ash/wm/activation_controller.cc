1dfcbc792695db4d5c6bd2aa140205ce858ee260
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-25 00:33:46 +0000

    Keep hiding windows' layers stacked above the new active window's layer.
    This prevents the new active window from drawing atop the disappearing window as it animates away.
    
    http://crbug.com/119235
    TEST=none
    
    Review URL: http://codereview.chromium.org/9839072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128796 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/activation_controller.cc
+++ ash/wm/activation_controller.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/base/ui_base_types.h"
+#include "ui/gfx/compositor/layer.h"
 
 namespace ash {
 namespace internal {
@@ -142,8 +143,15 @@ bool ActivationController::OnWillFocusWindow(aura::Window* window,
 
 void ActivationController::OnWindowVisibilityChanged(aura::Window* window,
                                                      bool visible) {
-  if (!visible)
-    ActivateNextWindow(window);
+  if (!visible) {
+    aura::Window* next_window = ActivateNextWindow(window);
+    if (next_window && next_window->parent() == window->parent()) {
+      // Despite the activation change, we need to keep the window being hidden
+      // stacked above the new window so it stays on top as it animates away.
+      window->layer()->parent()->StackAbove(window->layer(),
+                                            next_window->layer());
+    }
+  }
 }
 
 void ActivationController::OnWindowDestroying(aura::Window* window) {
@@ -223,9 +231,13 @@ void ActivationController::ActivateWindowWithEvent(aura::Window* window,
   }
 }
 
-void ActivationController::ActivateNextWindow(aura::Window* window) {
-  if (wm::IsActiveWindow(window))
-    ActivateWindow(GetTopmostWindowToActivate(window));
+aura::Window* ActivationController::ActivateNextWindow(aura::Window* window) {
+  aura::Window* next_window = NULL;
+  if (wm::IsActiveWindow(window)) {
+    next_window = GetTopmostWindowToActivate(window);
+    ActivateWindow(next_window);
+  }
+  return next_window;
 }
 
 aura::Window* ActivationController::GetTopmostWindowToActivate(
--- ash/wm/activation_controller_unittest.cc
+++ ash/wm/activation_controller_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/event_generator.h"
 #include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/test/test_windows.h"
+#include "ui/gfx/compositor/layer.h"
 
 #if defined(OS_WIN)
 // Windows headers define macros for these function names which screw with us.
@@ -348,6 +349,27 @@ TEST_F(ActivationControllerTest, PreventFocusToNonActivatableWindow) {
   EXPECT_FALSE(w21->HasFocus());
   EXPECT_TRUE(wm::IsActiveWindow(w1.get()));
   EXPECT_TRUE(w1->HasFocus());
+}
+
+// Verifies code in ActivationController::OnWindowVisibilityChanged() that keeps
+// hiding windows layers stacked above the newly active window while they
+// animate away.
+TEST_F(ActivationControllerTest, AnimateHideMaintainsStacking) {
+  aura::test::TestWindowDelegate wd;
+  scoped_ptr<aura::Window> w1(aura::test::CreateTestWindowWithDelegate(
+    &wd, -1, gfx::Rect(50, 50, 50, 50), NULL));
+  scoped_ptr<aura::Window> w2(aura::test::CreateTestWindowWithDelegate(
+      &wd, -2, gfx::Rect(75, 75, 50, 50), NULL));
+  wm::ActivateWindow(w2.get());
+  EXPECT_TRUE(wm::IsActiveWindow(w2.get()));
+  w2->Hide();
+  typedef std::vector<ui::Layer*> Layers;
+  const Layers& children = w1->parent()->layer()->children();
+  Layers::const_iterator w1_iter =
+      std::find(children.begin(), children.end(), w1->layer());
+  Layers::const_iterator w2_iter =
+      std::find(children.begin(), children.end(), w2->layer());
+  EXPECT_TRUE(w2_iter > w1_iter);
 }
 
 }  // namespace test
