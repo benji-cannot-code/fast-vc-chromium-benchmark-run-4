32de402c45ed03ca3c7f6a6ebdc9722bb3fababa
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 22:39:18 +0000

    Tried to fix an unused parameter warning after r112154.
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::layoutBlock): Changed to not pass relayoutChildren to
    layoutFlexItems.
    (WebCore::RenderFlexibleBox::layoutFlexItems): Removed the unused relayoutChildren parameter.
    * rendering/RenderFlexibleBox.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -267,7 +267,7 @@ void RenderFlexibleBox::layoutBlock(bool relayoutChildren, int, BlockLayoutPass)
     FlexOrderHashSet flexOrderValues;
     computeMainAxisPreferredSizes(relayoutChildren, flexOrderValues);
     FlexOrderIterator flexIterator(this, flexOrderValues);
-    layoutFlexItems(relayoutChildren, flexIterator, lineContexts);
+    layoutFlexItems(flexIterator, lineContexts);
 
     LayoutUnit oldClientAfterEdge = clientLogicalBottom();
     computeLogicalHeight();
@@ -611,7 +611,7 @@ LayoutUnit RenderFlexibleBox::computeAvailableFreeSpace(LayoutUnit preferredMain
     return heightResult - preferredMainAxisExtent;
 }
 
-void RenderFlexibleBox::layoutFlexItems(bool relayoutChildren, FlexOrderIterator& iterator, WTF::Vector<LineContext>& lineContexts)
+void RenderFlexibleBox::layoutFlexItems(FlexOrderIterator& iterator, WTF::Vector<LineContext>& lineContexts)
 {
     OrderedFlexItemList orderedChildren;
     LayoutUnit preferredMainAxisExtent;
