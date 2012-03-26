3cd92beb2b8da5cababab76be7b3777d0ceb627f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 10:51:41 +0000

    Unreviewed, rolling out r111993. http://trac.webkit.org/changeset/111993 https://bugs.webkit.org/show_bug.cgi?id=82184
    It broke layout and API tests and made WTR crash (Requested by
    Ossy on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-03-26
    
    Source/WebKit2:
    
    * UIProcess/qt/QtPageClient.cpp:
    (QtPageClient::isViewWindowActive):
    
    Tools:
    
    * WebKitTestRunner/qt/PlatformWebViewQt.cpp:
    (WTR::PlatformWebView::PlatformWebView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtPageClient.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtPageClient.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qquickwebview_p.h"
 #include "qquickwebview_p_p.h"
 #include <QGuiApplication>
-#include <QQuickCanvas>
 #include <WebCore/Cursor.h>
 #include <WebCore/DragData.h>
 #include <WebCore/FloatRect.h>
@@ -257,9 +256,8 @@ WebCore::IntSize QtPageClient::viewSize()
 
 bool QtPageClient::isViewWindowActive()
 {
-    if (!m_webView || !m_webView->canvas())
-        return false;
-    return m_webView->canvas()->isActive();
+    // FIXME: The scene graph does not have the concept of being active or not when this was written.
+    return true;
 }
 
 bool QtPageClient::isViewFocused()
