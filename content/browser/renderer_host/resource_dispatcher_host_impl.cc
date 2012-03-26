d294b9e5886ee44fef6cc698f51dfcd52c7161d8
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 22:05:31 +0000

    Revert 128993 - Refactor BaseTimer to avoid spamming the MessageLoop with orphaned tasks.
    This change maintains the same API promises*, but instead of orphaning tasks when they are stopped, the BaseTimer_Helper class holds on to the task until either (1) it expires or (2) the user requests a delay that would arrive earlier than the pending task. If the user requests a longer delay than the pending task, a followup task will be posted when the pending task fires to span the remaining time.
    
    * The one change of usage is related to threading. The threading requirements are now more strict. It is not allowed to destruct a timer on a different thread than the one used to post tasks. A thread ID DCHECK is now in place that will help catch misuse. Some existing instances are changed as part of this CL.
    
    A side effect of this change is that the BaseTimer and DelayTimer are simplified to use features of BaseTimer_Helper (which is now called Timer).
    
    As suggested in timer.h, I ran the disabled TimerTest tests from linux, and they pass consistently. I also added some new tests to verify correct run states.
    
    BUG=117451,103667,119714,119750
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=128412
    
    Reverted: http://src.chromium.org/viewvc/chrome?view=rev&revision=128506
    
    Review URL: https://chromiumcodereview.appspot.com/9655006
    
    TBR=jbates@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9791009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129018 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host_impl.cc
+++ content/browser/renderer_host/resource_dispatcher_host_impl.cc
@@ -339,9 +339,6 @@ ResourceDispatcherHostImpl::ResourceDispatcherHostImpl()
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::Bind(&appcache::AppCacheInterceptor::EnsureRegistered));
-
-  update_load_states_timer_.reset(
-      new base::RepeatingTimer<ResourceDispatcherHostImpl>());
 }
 
 ResourceDispatcherHostImpl::~ResourceDispatcherHostImpl() {
@@ -609,7 +606,7 @@ void ResourceDispatcherHostImpl::OnShutdown() {
   // Make sure we shutdown the timer now, otherwise by the time our destructor
   // runs if the timer is still running the Task is deleted twice (once by
   // the MessageLoop and the second time by RepeatingTimer).
-  update_load_states_timer_.reset();
+  update_load_states_timer_.Stop();
 
   // Clear blocked requests if any left.
   // Note that we have to do this in 2 passes as we cannot call
@@ -1372,8 +1369,8 @@ void ResourceDispatcherHostImpl::RemovePendingRequest(
   pending_requests_.erase(iter);
 
   // If we have no more pending requests, then stop the load state monitor
-  if (pending_requests_.empty() && update_load_states_timer_.get())
-    update_load_states_timer_->Stop();
+  if (pending_requests_.empty())
+    update_load_states_timer_.Stop();
 }
 
 // net::URLRequest::Delegate ---------------------------------------------------
@@ -1735,8 +1732,8 @@ void ResourceDispatcherHostImpl::StartRequest(net::URLRequest* request) {
   request->Start();
 
   // Make sure we have the load state monitor running
-  if (!update_load_states_timer_->IsRunning()) {
-    update_load_states_timer_->Start(FROM_HERE,
+  if (!update_load_states_timer_.IsRunning()) {
+    update_load_states_timer_.Start(FROM_HERE,
         TimeDelta::FromMilliseconds(kUpdateLoadStatesIntervalMsec),
         this, &ResourceDispatcherHostImpl::UpdateLoadStates);
   }
--- media/audio/audio_input_controller.h
+++ media/audio/audio_input_controller.h
@@ -145,7 +145,7 @@ class MEDIA_EXPORT AudioInputController
       SyncWriter* sync_writer);
 
   // Starts recording using the created audio input stream.
-  // This method is called on the creator thread.
+  // This method is called on the audio thread.
   virtual void Record();
 
   // Closes the audio input stream. The state is changed and the resources
@@ -216,7 +216,7 @@ class MEDIA_EXPORT AudioInputController
   // when an audio input device is unplugged whilst recording on Windows.
   // See http://crbug.com/79936 for details.
   // This member is only touched by the creating thread.
-  scoped_ptr<base::DelayTimer<AudioInputController> > no_data_timer_;
+  base::DelayTimer<AudioInputController> no_data_timer_;
 
   // |state_| is written on the audio thread and is read on the hardware audio
   // thread. These operations need to be locked. But lock is not required for
