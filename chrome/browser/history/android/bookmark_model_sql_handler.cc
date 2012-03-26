db6e438244421266ba0def7958ef3ce3f00d0cc2
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 23:14:14 +0000

    The AndroidProviderBackend Part 1.
    The main class to implement android content provider.
    
    This CL is the implementation of Bookmark part.
    
    It doesn't include Search related API.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9704077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129036 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/android/bookmark_model_sql_handler.cc
+++ chrome/browser/history/android/bookmark_model_sql_handler.cc
@@ -22,12 +22,11 @@ namespace {
 
 // The interesting columns of this handler.
 const BookmarkRow::BookmarkColumnID kInterestingColumns[] = {
-  BookmarkRow::BOOKMARK, BookmarkRow::TITLE };
+    BookmarkRow::BOOKMARK, BookmarkRow::TITLE };
 
 } // namespace
 
-BookmarkModelSQLHandler::Task::Task(Profile* profile)
-    : profile_(profile) {
+BookmarkModelSQLHandler::Task::Task() {
 }
 
 void BookmarkModelSQLHandler::Task::AddBookmarkToMobileFolder(
@@ -83,20 +82,16 @@ BookmarkModelSQLHandler::Task::~Task() {
 }
 
 BookmarkModel* BookmarkModelSQLHandler::Task::GetBookmarkModel() {
-  ProfileManager* manager = g_browser_process->profile_manager();
-  if (!manager)
+  Profile* profile = ProfileManager::GetLastUsedProfile();
+  if (!profile)
     return NULL;
-  if (!manager->IsValidProfile(profile_))
-    return NULL;
-  return profile_->GetBookmarkModel();
+  return profile->GetBookmarkModel();
 }
 
 BookmarkModelSQLHandler::BookmarkModelSQLHandler(
-    URLDatabase* url_database,
-    Profile* profile)
+    URLDatabase* url_database)
     : SQLHandler(kInterestingColumns, arraysize(kInterestingColumns)),
-      url_database_(url_database),
-      profile_(profile) {
+      url_database_(url_database) {
 }
 
 BookmarkModelSQLHandler::~BookmarkModelSQLHandler() {
@@ -114,20 +109,20 @@ bool BookmarkModelSQLHandler::Update(const BookmarkRow& row,
         BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
             &BookmarkModelSQLHandler::Task::AddBookmark,
             scoped_refptr<BookmarkModelSQLHandler::Task>(
-                new BookmarkModelSQLHandler::Task(profile_)),
+                new BookmarkModelSQLHandler::Task()),
             i->url, url_row.title(), row.parent_id()));
       } else {
         BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
             &BookmarkModelSQLHandler::Task::RemoveBookmark,
             scoped_refptr<BookmarkModelSQLHandler::Task>(
-                new BookmarkModelSQLHandler::Task(profile_)),
+                new BookmarkModelSQLHandler::Task()),
             i->url));
       }
     } else if (row.is_value_set_explicitly(BookmarkRow::TITLE)) {
       BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
           &BookmarkModelSQLHandler::Task::UpdateBookmarkTitle,
           scoped_refptr<BookmarkModelSQLHandler::Task>(
-              new BookmarkModelSQLHandler::Task(profile_)),
+              new BookmarkModelSQLHandler::Task()),
           i->url, row.title()));
     }
   }
@@ -140,7 +135,7 @@ bool BookmarkModelSQLHandler::Delete(const TableIDRows& ids_set) {
     BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
         &BookmarkModelSQLHandler::Task::RemoveBookmark,
         scoped_refptr<BookmarkModelSQLHandler::Task>(
-            new BookmarkModelSQLHandler::Task(profile_)),
+            new BookmarkModelSQLHandler::Task()),
         i->url));
   }
   return true;
@@ -152,13 +147,13 @@ bool BookmarkModelSQLHandler::Insert(BookmarkRow* row) {
     BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
         &BookmarkModelSQLHandler::Task::AddBookmark,
         scoped_refptr<BookmarkModelSQLHandler::Task>(
-            new BookmarkModelSQLHandler::Task(profile_)),
+            new BookmarkModelSQLHandler::Task()),
         row->url(), row->title(), row->parent_id()));
   } else {
     BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, base::Bind(
         &BookmarkModelSQLHandler::Task::AddBookmarkToMobileFolder,
         scoped_refptr<BookmarkModelSQLHandler::Task>(
-            new BookmarkModelSQLHandler::Task(profile_)),
+            new BookmarkModelSQLHandler::Task()),
         row->url(), row->title()));
   }
   return true;
