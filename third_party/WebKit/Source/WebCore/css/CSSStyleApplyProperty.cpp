3bc1f815672bed37a1fd09bee49afb1969cad5b6
mikelawther@chromium.org <mikelawther@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 17:45:20 +0000

    CSS3 calc: mixed percent/absolute support for vertical-align https://bugs.webkit.org/show_bug.cgi?id=82152
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Tests: css3/calc/vertical-align-expected.html
           css3/calc/vertical-align.html
    
    * css/CSSStyleApplyProperty.cpp:
    (WebCore::ApplyPropertyVerticalAlign::applyValue):
    
    LayoutTests:
    
    * css3/calc/vertical-align-expected.html: Added.
    * css3/calc/vertical-align.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleApplyProperty.cpp
@@ -1568,7 +1568,7 @@ public:
         if (primitiveValue->getIdent())
             return selector->style()->setVerticalAlign(*primitiveValue);
 
-        selector->style()->setVerticalAlignLength(primitiveValue->convertToLength<FixedIntegerConversion | PercentConversion>(selector->style(), selector->rootElementStyle(), selector->style()->effectiveZoom()));
+        selector->style()->setVerticalAlignLength(primitiveValue->convertToLength<FixedIntegerConversion | PercentConversion | CalculatedConversion>(selector->style(), selector->rootElementStyle(), selector->style()->effectiveZoom()));
     }
 
     static PropertyHandler createHandler()
