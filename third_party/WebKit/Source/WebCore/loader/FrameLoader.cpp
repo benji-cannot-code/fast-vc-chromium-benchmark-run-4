55ba3131efed1e7cbebee036cec5695eaf772698
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 20:34:00 +0000

    Simplify setting loading state in DocumentLoader https://bugs.webkit.org/show_bug.cgi?id=82099
    Reviewed by Adam Barth.
    
    The logic for deciding what to return for DocumentLoader::isLoading()
    is crazy. It's indirectly based on the ResourceLoaders that have
    registered themselves with the DocumentLoader, but we can make that
    relationship more direct.
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::checkLoadComplete): Renamed from updateLoading, since it's not actually
        updating anything anymore. It now calls DOMWindow::finishedLoading() if loading is in fact done.
    (WebCore::DocumentLoader::startLoadingMainResource): The only reason this had a return value was to call
        updateLoading() if loading failed. Just call checkLoadComplete() directly (this allows it to
        be private, whereas updateLoading() was public).
    (WebCore::DocumentLoader::subresourceLoaderFinishedLoadingOnePart):
    * loader/DocumentLoader.h:
    (WebCore::DocumentLoader::isLoading): Rather than holding a separate bool, return based on the presence
        of non-multipart ResourceLoaders directly.
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::isLoading): Depend on DocumentLoader::isLoading() for the activeDocumentLoader(),
        rather than indirectly the other way around.
    (WebCore::FrameLoader::checkLoadCompleteForThisFrame): Remove several assertions that should now be
        absolutely identical to the remaining !pdl->isLoading().
    (WebCore::FrameLoader::continueLoadAfterWillSubmitForm):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -1639,7 +1639,7 @@ bool FrameLoader::isLoading() const
     DocumentLoader* docLoader = activeDocumentLoader();
     if (!docLoader)
         return false;
-    return docLoader->isLoadingMainResource() || docLoader->isLoadingSubresources() || docLoader->isLoadingPlugIns();
+    return docLoader->isLoading();
 }
 
 bool FrameLoader::frameHasLoaded() const
@@ -2213,9 +2213,6 @@ void FrameLoader::checkLoadCompleteForThisFrame()
                 m_delegateIsHandlingProvisionalLoadError = false;
 
                 ASSERT(!pdl->isLoading());
-                ASSERT(!pdl->isLoadingMainResource());
-                ASSERT(!pdl->isLoadingSubresources());
-                ASSERT(!pdl->isLoadingPlugIns());
 
                 // If we're in the middle of loading multipart data, we need to restore the document loader.
                 if (isReplacing() && !m_documentLoader.get())
@@ -2317,9 +2314,7 @@ void FrameLoader::continueLoadAfterWillSubmitForm()
     }
 
     m_provisionalDocumentLoader->timing()->markNavigationStart(frame());
-
-    if (!m_provisionalDocumentLoader->startLoadingMainResource(identifier))
-        m_provisionalDocumentLoader->updateLoading();
+    m_provisionalDocumentLoader->startLoadingMainResource(identifier);
 }
 
 static KURL originatingURLFromBackForwardList(Page* page)
