fc8cdf4f0cec82525fdc50f31c098e5a64d9036f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 23:36:06 +0000

    test-webkitpy: split test-finding code into a different module https://bugs.webkit.org/show_bug.cgi?id=82253
    Reviewed by Adam Barth.
    
    Per suggestion from abarth, this change splits all the
    filesystem-crawling, test-finding code in test-webkitpy out into
    a separate module and switches to using a FileSystem object.
    This makes things much more testable, so we also add tests :).
    
    We also add a realpath() method to the Filesystem object, since
    test-webkitpy needs that in order to be able to resolve symlinks
    properly to determine whether a file is under a particular tree
    or not.
    
    * Scripts/webkitpy/common/system/filesystem.py:
    (FileSystem.realpath):
    * Scripts/webkitpy/common/system/filesystem_mock.py:
    (MockFileSystem.realpath):
    * Scripts/webkitpy/test/main.py:
    (Tester.__init__):
    (Tester.add_tree):
    (Tester.run):
    (Tester._run_tests):
    (Tester._log_exception):
    * Scripts/webkitpy/test/test_finder.py: Added.
    (TestDirectoryTree):
    (TestDirectoryTree.__init__):
    (TestDirectoryTree.find_modules):
    (TestDirectoryTree.find_modules.file_filter):
    (TestDirectoryTree.to_module):
    (TestDirectoryTree.clean):
    (TestFinder):
    (TestFinder.__init__):
    (TestFinder.add_tree):
    (TestFinder.additional_paths):
    (TestFinder.clean_trees):
    (TestFinder.is_module):
    (TestFinder.to_module):
    (TestFinder.find_names):
    (TestFinder._exclude):
    * Scripts/webkitpy/test/test_finder_unittest.py: Added.
    (TestFinderTest):
    (TestFinderTest.setUp):
    (TestFinderTest.tearDown):
    (TestFinderTest.test_additional_system_paths):
    (TestFinderTest.test_is_module):
    (TestFinderTest.test_to_module):
    (TestFinderTest.test_clean):
    (TestFinderTest.test_find_names):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker_unittest.py
@@ -47,12 +47,13 @@ stopping_queue = None
 
 WORKER_NAME = 'TestWorker'
 
-def make_broker(manager, worker_model, start_queue=None, stop_queue=None):
+
+def make_broker(manager, max_workers, start_queue=None, stop_queue=None):
     global starting_queue
     global stopping_queue
     starting_queue = start_queue
     stopping_queue = stop_queue
-    return manager_worker_broker.get(worker_model, manager, _TestWorker)
+    return manager_worker_broker.get(max_workers, manager, _TestWorker)
 
 
 class _TestWorker(manager_worker_broker.AbstractWorker):
@@ -88,16 +89,13 @@ class _TestWorker(manager_worker_broker.AbstractWorker):
 
 class FunctionTests(unittest.TestCase):
     def test_get__inline(self):
-        self.assertTrue(make_broker(self, 'inline') is not None)
+        self.assertTrue(make_broker(self, 1) is not None)
 
     def test_get__processes(self):
         # This test sometimes fails on Windows. See <http://webkit.org/b/55087>.
         if sys.platform in ('cygwin', 'win32'):
             return
-        self.assertTrue(make_broker(self, 'processes') is not None)
-
-    def test_get__unknown(self):
-        self.assertRaises(ValueError, make_broker, self, 'unknown')
+        self.assertTrue(make_broker(self, 2) is not None)
 
 
 class _TestsMixin(object):
@@ -126,10 +124,10 @@ class _TestsMixin(object):
         self._broker = None
         self._done = False
         self._exception = None
-        self._worker_model = None
+        self._max_workers = None
 
     def make_broker(self, starting_queue=None, stopping_queue=None):
-        self._broker = make_broker(self, self._worker_model, starting_queue,
+        self._broker = make_broker(self, self._max_workers, starting_queue,
                                    stopping_queue)
 
     def test_name(self):
@@ -178,7 +176,7 @@ class _TestsMixin(object):
 class InlineBrokerTests(_TestsMixin, unittest.TestCase):
     def setUp(self):
         _TestsMixin.setUp(self)
-        self._worker_model = 'inline'
+        self._max_workers = 1
 
     def test_inline_arguments(self):
         self.make_broker()
@@ -196,7 +194,7 @@ if sys.platform not in ('cygwin', 'win32'):
     class MultiProcessBrokerTests(_TestsMixin, unittest.TestCase):
         def setUp(self):
             _TestsMixin.setUp(self)
-            self._worker_model = 'processes'
+            self._max_workers = 2
 
 
 class InterfaceTest(unittest.TestCase):
