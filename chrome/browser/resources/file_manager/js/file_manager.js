fc7ad18b11e217f76873c5b679a7c6f94c8df49f
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 18:59:40 +0000

    [filebrowser] Perform default handlers for cut/copy/paste events in renaming input.
    BUG=chromium-os:27218
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/9856017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128959 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3144,7 +3144,8 @@ FileManager.prototype = {
   }
 
   FileManager.prototype.onCopy_ = function(event) {
-    if (!this.selection || this.selection.totalCount == 0)
+    if (!this.selection || this.selection.totalCount == 0 ||
+        this.document_.activeElement.tagName == 'INPUT')
       return;
 
     event.preventDefault();
@@ -3155,7 +3156,8 @@ FileManager.prototype = {
 
   FileManager.prototype.onCut_ = function(event) {
     if (!this.selection || this.selection.totalCount == 0 ||
-        this.commands_['cut'].disabled)
+        this.commands_['cut'].disabled ||
+        this.document_.activeElement.tagName == 'INPUT')
       return;
 
     event.preventDefault();
@@ -3166,7 +3168,8 @@ FileManager.prototype = {
 
   FileManager.prototype.onPaste_ = function(event) {
     // Check that the data copied by ourselves.
-    if (!event.clipboardData.getData('fs/isCut'))
+    if (!event.clipboardData.getData('fs/isCut') ||
+        this.document_.activeElement.tagName == 'INPUT')
       return;
     event.preventDefault();
     this.pasteFromClipboard_(event.clipboardData);
