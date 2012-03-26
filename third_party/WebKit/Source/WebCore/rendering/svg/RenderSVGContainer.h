1a1eeb0260db7090421148180a9f33ba5e7abbcf
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 14:36:59 +0000

    Add invalid bounding box concept to SVG containers https://bugs.webkit.org/show_bug.cgi?id=81104
    Patch by Philip Rogers <pdr@google.com> on 2012-03-26
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore:
    
    An empty <g> element needs to use an invalid bounding box because
    an empty bounding box isn't the default state. This change
    introduces the concept of an invalid object bounding box for
    both RenderSVGContainer and RenderSVGRoot. Code that
    does not explicitly check that the bounding box is valid
    should be unaffected by this change. We use this new invalid
    flag in computeContainerBoundingBoxes so that we do not
    include invalid bounding boxes.
    
    This change also contains a small fix in
    RenderSVGContainer::toRenderSVGContainer which depended on
    RenderSVGViewportContainer not inheriting from RenderSVGContainer,
    which it now does.
    
    Test: svg/custom/getBBox-empty-container.html
    
    * rendering/svg/RenderSVGContainer.cpp:
    (WebCore::RenderSVGContainer::RenderSVGContainer):
    (WebCore::RenderSVGContainer::updateCachedBoundaries):
    * rendering/svg/RenderSVGContainer.h:
    (WebCore::RenderSVGContainer::isObjectBoundingBoxValid):
    (RenderSVGContainer):
    (WebCore::toRenderSVGContainer):
    * rendering/svg/RenderSVGRoot.cpp:
    (WebCore::RenderSVGRoot::RenderSVGRoot):
    (WebCore::RenderSVGRoot::updateCachedBoundaries):
    * rendering/svg/RenderSVGRoot.h:
    (RenderSVGRoot):
    * rendering/svg/SVGRenderSupport.cpp:
    (WebCore):
    (WebCore::updateObjectBoundingBox):
    (WebCore::SVGRenderSupport::computeContainerBoundingBoxes):
    * rendering/svg/SVGRenderSupport.h:
    (SVGRenderSupport):
    
    LayoutTests:
    
    * svg/custom/getBBox-empty-container-expected.txt: Added.
    * svg/custom/getBBox-empty-container.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGContainer.h
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGContainer.h
@@ -43,6 +43,7 @@ public:
     virtual void paint(PaintInfo&, const LayoutPoint&);
     virtual void setNeedsBoundariesUpdate() { m_needsBoundariesUpdate = true; }
     virtual bool didTransformToRootUpdate() { return false; }
+    bool isObjectBoundingBoxValid() const { return m_objectBoundingBoxValid; }
 
 protected:
     virtual RenderObjectChildList* virtualChildren() { return children(); }
@@ -77,6 +78,7 @@ protected:
 private:
     RenderObjectChildList m_children;
     FloatRect m_objectBoundingBox;
+    bool m_objectBoundingBoxValid;
     FloatRect m_strokeBoundingBox;
     FloatRect m_repaintBoundingBox;
     bool m_needsBoundariesUpdate : 1;
@@ -84,15 +86,13 @@ private:
   
 inline RenderSVGContainer* toRenderSVGContainer(RenderObject* object)
 {
-    // Note: isSVGContainer is also true for RenderSVGViewportContainer, which is not derived from this.
-    ASSERT(!object || (object->isSVGContainer() && strcmp(object->renderName(), "RenderSVGViewportContainer")));
+    ASSERT(!object || object->isSVGContainer());
     return static_cast<RenderSVGContainer*>(object);
 }
 
 inline const RenderSVGContainer* toRenderSVGContainer(const RenderObject* object)
 {
-    // Note: isSVGContainer is also true for RenderSVGViewportContainer, which is not derived from this.
-    ASSERT(!object || (object->isSVGContainer() && strcmp(object->renderName(), "RenderSVGViewportContainer")));
+    ASSERT(!object || object->isSVGContainer());
     return static_cast<const RenderSVGContainer*>(object);
 }
 
