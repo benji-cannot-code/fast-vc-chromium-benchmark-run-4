9148ad5e375ee30514e58dff63c4348adb3d1ad7
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-26 12:11:47 +0000

    Increase code sharing between StylePropertySet and CSSPropertyLonghand. https://bugs.webkit.org/show_bug.cgi?id=81960
    Reviewed by Antti Koivisto.
    
    Use longhands declaration from CSSPropertyLonghand in StylePropertySet in order
    to avoid code duplication.
    
    No new tests : refactoring only, we shouldn't have any behavior difference.
    
    * css/CSSPropertyLonghand.cpp:
    (WebCore::backgroundLonghand):
    Re-order the array to match the spec default order.
    (WebCore::webkitMaskLonghand):
    Re-order to match the original order.
    (WebCore::webkitTransformOriginLonghand):
    A LayoutTest shows that a property was missing.
    * css/StylePropertySet.cpp:
    (WebCore::StylePropertySet::getPropertyValue):
    (WebCore::StylePropertySet::borderSpacingValue):
    (WebCore::StylePropertySet::get4Values):
    (WebCore::StylePropertySet::getLayeredShorthandValue):
    (WebCore::StylePropertySet::getShorthandValue):
    (WebCore::StylePropertySet::getCommonValue):
    (WebCore::StylePropertySet::asText):
    * css/StylePropertySet.h:
    (WebCore):
    (StylePropertySet):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112078 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertySet.h
+++ third_party/WebKit/Source/WebCore/css/StylePropertySet.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class CSSRule;
+class CSSPropertyLonghand;
 class CSSStyleDeclaration;
 class KURL;
 class PropertySetCSSStyleDeclaration;
@@ -118,17 +119,13 @@ private:
 
     void setNeedsStyleRecalc();
 
-    String getShorthandValue(const int* properties, size_t) const;
-    String getCommonValue(const int* properties, size_t) const;
-    String getLayeredShorthandValue(const int* properties, size_t) const;
-    String get4Values(const int* properties) const;
-    String borderSpacingValue(const int properties[2]) const;
+    String getShorthandValue(const CSSPropertyLonghand&) const;
+    String getCommonValue(const CSSPropertyLonghand&) const;
+    String getLayeredShorthandValue(const CSSPropertyLonghand&) const;
+    String get4Values(const CSSPropertyLonghand&) const;
+    String borderSpacingValue(const CSSPropertyLonghand&) const;
     String fontValue() const;
     bool appendFontLonghandValueIfExplicit(int propertyID, StringBuilder& result) const;
-
-    template<size_t size> String getShorthandValue(const int (&properties)[size]) const { return getShorthandValue(properties, size); }
-    template<size_t size> String getCommonValue(const int (&properties)[size]) const { return getCommonValue(properties, size); }
-    template<size_t size> String getLayeredShorthandValue(const int (&properties)[size]) const { return getLayeredShorthandValue(properties, size); }
 
     bool removeShorthandProperty(int propertyID);
     bool propertyMatches(const CSSProperty*) const;
