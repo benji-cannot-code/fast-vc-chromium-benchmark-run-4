269d786daa94ed64aa3ebddbbb1db1f862ae61b1
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 04:53:27 +0000

    ash: Using ScreenLocker from PowerManagerClient::Observer::LockScreen is not safe.
    Instead of using ScreenLocker, keep a boolean flag to decide whether or not the
    screen is locked.
    
    BUG=120087
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9773013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/ash_system_tray_delegate.cc
+++ chrome/browser/chromeos/system/ash_system_tray_delegate.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/base_login_display_host.h"
 #include "chrome/browser/chromeos/login/login_display_host.h"
 #include "chrome/browser/chromeos/login/message_bubble.h"
-#include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/mobile_config.h"
@@ -131,6 +130,7 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
         network_menu_(ALLOW_THIS_IN_INITIALIZER_LIST(new NetworkMenu(this))),
         clock_type_(base::k24HourClock),
         search_key_mapped_to_(input_method::kSearchKey),
+        screen_locked_(false),
         data_promo_notification_(new DataPromoNotification()) {
     AudioHandler::GetInstance()->AddVolumeObserver(this);
     DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(this);
@@ -208,11 +208,9 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
 
   virtual ash::user::LoginStatus GetUserLoginStatus() const OVERRIDE {
     UserManager* manager = UserManager::Get();
-    const chromeos::ScreenLocker* locker =
-        chromeos::ScreenLocker::default_screen_locker();
     if (!manager->IsUserLoggedIn())
       return ash::user::LOGGED_IN_NONE;
-    if (locker && locker->locked())
+    if (screen_locked_)
       return ash::user::LOGGED_IN_LOCKED;
     if (manager->IsCurrentUserOwner())
       return ash::user::LOGGED_IN_OWNER;
@@ -678,10 +676,12 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
   }
 
   virtual void LockScreen() OVERRIDE {
+    screen_locked_ = true;
     tray_->UpdateAfterLoginStatusChange(GetUserLoginStatus());
   }
 
   virtual void UnlockScreen() OVERRIDE {
+    screen_locked_ = false;
     tray_->UpdateAfterLoginStatusChange(GetUserLoginStatus());
   }
 
@@ -917,6 +917,7 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
   PowerSupplyStatus power_supply_status_;
   base::HourClockType clock_type_;
   int search_key_mapped_to_;
+  bool screen_locked_;
 
   scoped_ptr<BluetoothAdapter> bluetooth_adapter_;
 
