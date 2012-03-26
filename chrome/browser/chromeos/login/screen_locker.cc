ea4c64134620b31699ac00c10680f8bbfd52f3c6
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 14:11:10 +0000

    Disable extension IMEs when the screen is locked
    R=yusukes@chromium.org
    BUG=chromium-os:27049
    TEST=See bug
    
    
    Review URL: http://codereview.chromium.org/9809016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -162,6 +162,7 @@ class ScreenLockObserver : public chromeos::PowerManagerClient::Observer,
       // We don't want to shut down the IME, even if the hardware layout is the
       // only IME left.
       manager->SetEnableAutoImeShutdown(false);
+      manager->SetEnableExtensionIMEs(false);
       manager->SetInputMethodConfig(
           chromeos::language_prefs::kGeneralSectionName,
           chromeos::language_prefs::kPreloadEnginesConfigName,
@@ -179,6 +180,7 @@ class ScreenLockObserver : public chromeos::PowerManagerClient::Observer,
           chromeos::input_method::InputMethodConfigValue::kValueTypeStringList;
       value.string_list_value = saved_active_input_method_list_;
       manager->SetEnableAutoImeShutdown(true);
+      manager->SetEnableExtensionIMEs(true);
       manager->SetInputMethodConfig(
           chromeos::language_prefs::kGeneralSectionName,
           chromeos::language_prefs::kPreloadEnginesConfigName,
