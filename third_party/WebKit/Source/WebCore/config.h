f4df2103c6fe41cf9c05e91f31bcd06a1b8cd50d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 15:44:16 +0000

    [BlackBerry] Disable DisallowCType.h usage https://bugs.webkit.org/show_bug.cgi?id=82211
    Patch by Ming Xie <mxie@rim.com> on 2012-03-27
    Reviewed by Rob Buis.
    
    Build fix: QNX port does use ctype.h, so we should not
    include <wtf/DisallowCType.h> in WebCore/config.h
    
    No new tests - Build Fix
    
    * config.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112275 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/config.h
+++ third_party/WebKit/Source/WebCore/config.h
@@ -93,7 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // are used from wx headers. On GTK+ for Mac many GTK+ files include <libintl.h>
 // or <glib/gi18n-lib.h>, which in turn include <xlocale/_ctype.h> which uses
 // isacii(). 
-#if !PLATFORM(QT) && !PLATFORM(WX) && !PLATFORM(CHROMIUM) && !(OS(DARWIN) && PLATFORM(GTK)) && !defined(_LIBCPP_VERSION)
+#if !PLATFORM(QT) && !PLATFORM(WX) && !PLATFORM(CHROMIUM) && !(OS(DARWIN) && PLATFORM(GTK)) && !OS(QNX) && !defined(_LIBCPP_VERSION)
 #include <wtf/DisallowCType.h>
 #endif
 
