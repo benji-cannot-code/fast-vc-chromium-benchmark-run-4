b56ff71c206e233ab85cc248c1d86dd986f8b5b1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 20:01:38 +0000

    views: Fix memory leak in ComboboxExample.
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9834076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129249 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/combobox_example.h
+++ ui/views/examples/combobox_example.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,11 +9,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "ui/base/models/combobox_model.h"
 #include "ui/views/controls/combobox/combobox_listener.h"
 #include "ui/views/examples/example_base.h"
 
 namespace views {
 namespace examples {
+
+// A combobox model implementation that generates a list of "Item <index>".
+class ComboboxModelExample : public ui::ComboboxModel {
+ public:
+  ComboboxModelExample();
+  virtual ~ComboboxModelExample();
+
+  // Overridden from ui::ComboboxModel:
+  virtual int GetItemCount() const OVERRIDE;
+  virtual string16 GetItemAt(int index) OVERRIDE;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ComboboxModelExample);
+};
 
 class ComboboxExample : public ExampleBase, public ComboboxListener {
  public:
@@ -29,6 +44,7 @@ class ComboboxExample : public ExampleBase, public ComboboxListener {
                            int prev_index,
                            int new_index) OVERRIDE;
 
+  ComboboxModelExample combobox_model_;
   Combobox* combobox_;
 
   DISALLOW_COPY_AND_ASSIGN(ComboboxExample);
