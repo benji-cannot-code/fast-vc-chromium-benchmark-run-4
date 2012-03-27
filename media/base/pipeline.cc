f919dc1d0ed51979cb875fa50f62540f1dbde78e
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 20:13:14 +0000

    Make AddFilter void and remove bullshit fallback code.
    Review URL: http://codereview.chromium.org/9863025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129259 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline.cc
+++ media/base/pipeline.cc
@@ -1110,13 +1110,6 @@ void Pipeline::FinishDestroyingFiltersTask() {
   error_caused_teardown_ = false;
 }
 
-bool Pipeline::PrepareFilter(scoped_refptr<Filter> filter) {
-  bool ret = pipeline_init_state_->composite->AddFilter(filter.get());
-  if (!ret)
-    SetError(PIPELINE_ERROR_INITIALIZATION_FAILED);
-  return ret;
-}
-
 void Pipeline::InitializeDemuxer() {
   DCHECK_EQ(MessageLoop::current(), message_loop_);
   DCHECK(IsPipelineOk());
@@ -1219,8 +1212,7 @@ bool Pipeline::InitializeAudioRenderer(
     return false;
   }
 
-  if (!PrepareFilter(audio_renderer_))
-    return false;
+  pipeline_init_state_->composite->AddFilter(audio_renderer_);
 
   audio_renderer_->Initialize(
       decoder,
@@ -1245,8 +1237,7 @@ bool Pipeline::InitializeVideoRenderer(
     return false;
   }
 
-  if (!PrepareFilter(video_renderer_))
-    return false;
+  pipeline_init_state_->composite->AddFilter(video_renderer_);
 
   video_renderer_->Initialize(
       decoder,
