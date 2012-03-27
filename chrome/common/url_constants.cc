f1ecae66863fd36bf47151c0940b3dced4069bba
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 20:58:50 +0000

    Revert 129266 - Ash: Special header transparency for single open windows
    Added a special transparency setting for when a single large window is open.
    Made window transparency values based on constants, rather than baked into assets.
    Added chrome://transparency so designers can fiddle with the values without waiting for an engineer to make a change and the builds to cycle.  We'll take this out when we have final values.
    
    BUG=120275,120454
    TEST=manual
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9864010
    
    TBR=jamescook@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9861040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129272 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -98,10 +98,6 @@ const char kChromeUITermsOemURL[] = "chrome://terms/oem";
 const char kChromeUIUserImageURL[] = "chrome://userimage/";
 #endif
 
-#if defined(USE_ASH)
-const char kChromeUITransparencyURL[] = "chrome://transparency/";
-#endif
-
 #if defined(FILE_MANAGER_EXTENSION)
 const char kChromeUIFileManagerURL[] = "chrome://files/";
 #endif
@@ -231,10 +227,6 @@ const char kChromeUIWrenchMenu[] = "wrench-menu";
 
 const char kEULAPathFormat[] = "/usr/share/chromeos-assets/eula/%s/eula.html";
 const char kOemEulaURLPath[] = "oem";
-#endif
-
-#if defined(USE_ASH)
-const char kChromeUITransparencyHost[] = "transparency";
 #endif
 
 #if defined(FILE_MANAGER_EXTENSION)
--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -91,10 +91,6 @@ extern const char kChromeUITermsOemURL[];
 extern const char kChromeUIUserImageURL[];
 #endif
 
-#if defined(USE_ASH)
-extern const char kChromeUITransparencyURL[];
-#endif
-
 #if defined(FILE_MANAGER_EXTENSION)
 extern const char kChromeUIFileManagerURL[];
 #endif
@@ -222,10 +218,6 @@ extern const char kChromeUIWrenchMenu[];
 
 extern const char kEULAPathFormat[];
 extern const char kOemEulaURLPath[];
-#endif
-
-#if defined(USE_ASH)
-extern const char kChromeUITransparencyHost[];
 #endif
 
 #if defined(FILE_MANAGER_EXTENSION)
