badc3e00e80197e4c52d46da2c9cfb1ea8edea60
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 22:13:06 +0000

    Hold a reference to refChild in insertBefore before calling collectChildrenAndRemoveFromOldParent https://bugs.webkit.org/show_bug.cgi?id=82377
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    This fixes a regression from r111925.
    
    Test: fast/dom/insertBefore-refChild-crash.html
    
    * dom/ContainerNode.cpp:
    (WebCore::ContainerNode::insertBefore): Move the 'next' RefPtr above the call to
    collectChildrenAndRemoveFromOldParent and rename refChildPreviousSibling
    to 'prev' (matching appendChild and replaceChild).
    
    LayoutTests:
    
    * fast/dom/insertBefore-refChild-crash-expected.txt: Added.
    * fast/dom/insertBefore-refChild-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNode.cpp
@@ -143,6 +143,8 @@ bool ContainerNode::insertBefore(PassRefPtr<Node> newChild, Node* refChild, Exce
     if (refChild->previousSibling() == newChild || refChild == newChild) // nothing to do
         return true;
 
+    RefPtr<Node> next = refChild;
+
     NodeVector targets;
     collectChildrenAndRemoveFromOldParent(newChild.get(), targets, ec);
     if (ec)
@@ -154,8 +156,7 @@ bool ContainerNode::insertBefore(PassRefPtr<Node> newChild, Node* refChild, Exce
     ChildListMutationScope mutation(this);
 #endif
 
-    RefPtr<Node> next = refChild;
-    RefPtr<Node> refChildPreviousSibling = refChild->previousSibling();
+    RefPtr<Node> prev = next->previousSibling();
     for (NodeVector::const_iterator it = targets.begin(); it != targets.end(); ++it) {
         Node* child = it->get();
 
@@ -177,7 +178,7 @@ bool ContainerNode::insertBefore(PassRefPtr<Node> newChild, Node* refChild, Exce
         insertBeforeCommon(next.get(), child);
 
         // Send notification about the children change.
-        childrenChanged(false, refChildPreviousSibling.get(), next.get(), 1);
+        childrenChanged(false, prev.get(), next.get(), 1);
         notifyChildInserted(child);
 
         // Add child to the rendering tree.
