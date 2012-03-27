f4bd471676f68ca96e07f0855a359d8a3fe38ea0
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 00:11:27 +0000

    Makes the shelf remain visible while a context menu is showing in the launcher.
    BUG=120301
    TEST=see bug
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9860011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129059 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_runner.cc
+++ ui/views/controls/menu/menu_runner.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,8 @@ class MenuRunnerImpl : public internal::MenuControllerDelegate {
   explicit MenuRunnerImpl(MenuItemView* menu);
 
   MenuItemView* menu() { return menu_; }
+
+  bool running() const { return running_; }
 
   // See description above class for details.
   void Release();
@@ -267,6 +269,10 @@ MenuRunner::RunResult MenuRunner::RunMenuAt(Widget* parent,
                                             MenuItemView::AnchorPosition anchor,
                                             int32 types) {
   return holder_->RunMenuAt(parent, button, bounds, anchor, types);
+}
+
+bool MenuRunner::IsRunning() const {
+  return holder_->running();
 }
 
 void MenuRunner::Cancel() {
--- ui/views/controls/menu/menu_runner.h
+++ ui/views/controls/menu/menu_runner.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -85,6 +85,9 @@ class VIEWS_EXPORT MenuRunner {
                       const gfx::Rect& bounds,
                       MenuItemView::AnchorPosition anchor,
                       int32 types) WARN_UNUSED_RESULT;
+
+  // Returns true if we're in a nested message loop running the menu.
+  bool IsRunning() const;
 
   // Hides and cancels the menu. This does nothing if the menu is not open.
   void Cancel();
