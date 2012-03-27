9ffab817d23d8e364d1d2aa74d4cf53a6566edd6
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 14:51:03 +0000

    Web Inspector: Tabbed pane should set focus on its contents on tab click. https://bugs.webkit.org/show_bug.cgi?id=82323
    Reviewed by Pavel Feldman.
    
    Otherwise selected tab does not have focus.
    Also added tabIndex on tabElements to prevent pasting on closing middle click.
    
    * inspector/front-end/TabbedPane.js:
    (WebInspector.TabbedPane):
    (WebInspector.TabbedPane.prototype.focus):
    (WebInspector.TabbedPane.prototype.selectTab):
    (WebInspector.TabbedPaneTab.prototype._createTabElement):
    (WebInspector.TabbedPaneTab.prototype._tabClicked):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TabbedPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TabbedPane.js
@@ -87,6 +87,16 @@ WebInspector.TabbedPane.prototype = {
         this._closeableTabs = closeableTabs;
     },
 
+    focus: function()
+    {
+        if (!this.visibleView)
+            return;
+        if (typeof(this.visibleView.focus) === "function")
+            this.visibleView.focus();
+        else
+            this.visibleView.element.focus();
+    },
+
     /**
      * @param {string} id
      * @param {string} tabTitle
@@ -173,7 +183,7 @@ WebInspector.TabbedPane.prototype = {
         this._tabsHistory.splice(0, 0, tab);
         
         this._updateTabElements();
-        
+
         var eventData = { tabId: id, view: tab.view, isUserGesture: userGesture };
         this.dispatchEventToListeners(WebInspector.TabbedPane.EventTypes.TabSelected, eventData);
         return true;
@@ -589,6 +599,7 @@ WebInspector.TabbedPaneTab.prototype = {
     {
         var tabElement = document.createElement("div");
         tabElement.addStyleClass("tabbed-pane-header-tab");
+        tabElement.tabIndex = -1;
         
         var titleElement = tabElement.createChild("span", "tabbed-pane-header-tab-title");
         titleElement.textContent = this.title;
@@ -628,5 +639,6 @@ WebInspector.TabbedPaneTab.prototype = {
             this._tabbedPane.closeTab(this.id, true);
         else
             this._tabbedPane.selectTab(this.id, true);
+        this._tabbedPane.focus();
     }
 }
