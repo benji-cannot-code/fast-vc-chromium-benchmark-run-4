f415585923be7f00bdefb486749bd8a8724dd176
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 02:41:20 +0000

    Enable layout testing of the scroll animator. https://bugs.webkit.org/show_bug.cgi?id=81858 Add a call to the InternalSettings that layout tests can use to turn on scroll animation. Enable animation updates for the Chromium platform DRT when scroll animation has been turned on in a test. This should be a no-op for all current layout tests.
    Patch by Scott Byer <scottbyer@chromium.org> on 2012-03-26
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    No new tests. Layout test results should be unchanged.
    
    * testing/InternalSettings.cpp:
    (WebCore::InternalSettings::setEnableScrollAnimator):
    (WebCore):
    (WebCore::InternalSettings::scrollAnimatorEnabled):
    * testing/InternalSettings.h:
    (InternalSettings):
    * testing/InternalSettings.idl:
    
    Source/WebKit/chromium:
    
    * public/WebSettings.h:
    * src/WebSettingsImpl.cpp:
    (WebKit::WebSettingsImpl::scrollAnimatorEnabled):
    (WebKit):
    * src/WebSettingsImpl.h:
    (WebSettingsImpl):
    
    Tools:
    
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::serviceAnimation):
    (WebViewHost::scheduleAnimation):
    * DumpRenderTree/chromium/WebViewHost.h:
    (WebViewHost):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
@@ -132,6 +132,7 @@ public:
     virtual void setPasswordEchoDurationInSeconds(double);
     virtual void setShouldPrintBackgrounds(bool);
     virtual void setEnableScrollAnimator(bool);
+    virtual bool scrollAnimatorEnabled() const;
     virtual void setHixie76WebSocketProtocolEnabled(bool);
     virtual void setVisualWordMovementEnabled(bool);
     virtual void setShouldDisplaySubtitles(bool);
