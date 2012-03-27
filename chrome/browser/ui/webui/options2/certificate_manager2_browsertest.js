53802655c22ef5ee5a04c0de61ada7366faf0dbe
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 20:44:57 +0000

    enable options2 browsertests
    BUG=none
    TEST=the tests themselves
    
    
    Review URL: http://codereview.chromium.org/9864013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/certificate_manager2_browsertest.js
+++ chrome/browser/ui/webui/options2/certificate_manager2_browsertest.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,7 @@ CertificateManagerWebUITest.prototype = {
   /**
    * Browse to the certificate manager.
    **/
-  browsePreload: 'chrome://settings/certificates',
+  browsePreload: 'chrome://settings-frame/certificates',
 };
 
 // Mac and Windows go to native certificate manager.
