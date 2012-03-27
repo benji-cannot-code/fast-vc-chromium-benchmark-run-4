1b9493ce94cd50a856982d3efdbeb7536d06a18c
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 00:07:16 +0000

    aura: Enable drag/drop from the downloads tray.
    BUG=120057
    TEST=dragging files from downloads tray works.
    
    
    Review URL: http://codereview.chromium.org/9856024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129057 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_util.cc
+++ chrome/browser/download/download_util.cc
@@ -56,6 +56,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(TOOLKIT_VIEWS)
 #include "ui/base/dragdrop/drag_utils.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
+#if !defined(TOOLKIT_USES_GTK)
+#include "ui/base/dragdrop/drag_drop_types.h"
+#include "ui/gfx/screen.h"
+#include "ui/views/widget/widget.h"
+#endif
 #endif
 
 #if defined(TOOLKIT_USES_GTK)
@@ -392,17 +397,15 @@ void DragDownload(const DownloadItem* download,
                 download->GetFileNameToReportUser().LossyDisplayName());
   }
 
-#if defined(USE_AURA)
-  // TODO(beng):
-  NOTIMPLEMENTED();
-#elif defined(OS_WIN)
-  scoped_refptr<ui::DragSource> drag_source(new ui::DragSource);
+#if !defined(TOOLKIT_USES_GTK)
+  views::Widget* widget = views::Widget::GetWidgetForNativeView(view);
+  gfx::Point location = gfx::Screen::GetCursorScreenPoint();
+  // We do not care about notifying the DragItemView on completion of drag. So
+  // we pass NULL to RunShellDrag for the source view.
+  widget->RunShellDrag(NULL, data, location,
+      ui::DragDropTypes::DRAG_COPY | ui::DragDropTypes::DRAG_LINK);
 
-  // Run the drag and drop loop
-  DWORD effects;
-  DoDragDrop(ui::OSExchangeDataProviderWin::GetIDataObject(data),
-             drag_source.get(), DROPEFFECT_COPY | DROPEFFECT_LINK, &effects);
-#elif defined(TOOLKIT_USES_GTK)
+#else
   GtkWidget* root = gtk_widget_get_toplevel(view);
   if (!root)
     return;
@@ -414,7 +417,7 @@ void DragDownload(const DownloadItem* download,
 
   widget->DoDrag(data,
                  ui::DragDropTypes::DRAG_COPY | ui::DragDropTypes::DRAG_LINK);
-#endif  // OS_WIN
+#endif  // TOOLKIT_USES_GTK
 }
 #elif defined(USE_X11)
 void DragDownload(const DownloadItem* download,
