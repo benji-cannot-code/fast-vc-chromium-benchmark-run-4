1d4671b55b9ba1a49b03a370869138dc7497184b
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 14:12:32 +0000

    Mac content shell: Fix menu keys.
    BUG=120156
    TEST=as in bug
    
    Review URL: https://chromiumcodereview.appspot.com/9773042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129184 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_mac.mm
+++ content/shell/shell_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/scoped_nsobject.h"
 #include "base/string_piece.h"
 #include "base/sys_string_conversions.h"
+#include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
 #include "content/shell/resource.h"
@@ -239,6 +240,16 @@ void Shell::URLEntered(std::string url_string) {
       url = GURL("http://" + url_string);
     LoadURL(url);
   }
+}
+
+void Shell::HandleKeyboardEvent(const NativeWebKeyboardEvent& event) {
+  if (event.skip_in_browser)
+    return;
+
+  // The event handling to get this strictly right is a tangle; cheat here a bit
+  // by just letting the menus have a chance at it.
+  if ([event.os_event type] == NSKeyDown)
+    [[NSApp mainMenu] performKeyEquivalent:event.os_event];
 }
 
 }  // namespace content
