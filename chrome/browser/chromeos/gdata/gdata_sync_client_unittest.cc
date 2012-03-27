8e9858ec645ce283988ad1f1a852ca7ec4538d0d
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 06:03:51 +0000

    gdata: Serialize fetching of pinned files
    From GDataSyncClient::OnFilePinned(), we were unconditionally
    calling DoFetchLoop(), which resulted in calling GetFileForResourceId()
    nearly in parallel. We should serialize fetching of pinned files.
    
    BUG=chromium-os:28495
    TEST=run unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/9866026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129152 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_sync_client_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_sync_client_unittest.cc
@@ -118,7 +118,7 @@ TEST_F(GDataSyncClientTest, StartInitialScan) {
   // resource_id_fetched is not collected in the queue.
 }
 
-TEST_F(GDataSyncClientTest, DoFetchLoop) {
+TEST_F(GDataSyncClientTest, StartFetchLoop) {
   SetUpTestFiles();
 
   sync_client_->AddResourceIdForTesting("resource_id_not_fetched_foo");
@@ -126,7 +126,7 @@ TEST_F(GDataSyncClientTest, DoFetchLoop) {
   sync_client_->AddResourceIdForTesting("resource_id_not_fetched_baz");
 
   // The three files will be fetched by GetFileForResourceId(), once
-  // DoFetchLoop() starts.
+  // StartFetchLoop() starts.
   EXPECT_CALL(*mock_file_system_,
               GetFileForResourceId("resource_id_not_fetched_foo", _))
       .WillOnce(MockGetFileForResourceId(
@@ -149,7 +149,7 @@ TEST_F(GDataSyncClientTest, DoFetchLoop) {
           std::string("mime_type_does_not_matter"),
           REGULAR_FILE));
 
-  sync_client_->DoFetchLoop();
+  sync_client_->StartFetchLoop();
 }
 
 TEST_F(GDataSyncClientTest, OnFilePinned) {
