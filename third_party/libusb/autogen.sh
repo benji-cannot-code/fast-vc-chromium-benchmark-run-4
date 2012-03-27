e0c9b88e10309ad89ca12dc44bacf0d4b137bb54
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 00:36:20 +0000

    Import libusb 1.0.9-rc3 into third_party.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9826025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129069 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/libusb/autogen.sh
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# use glibtoolize if it is available (darwin)
+(glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
+
+$LIBTOOLIZE --copy --force || exit 1
+aclocal || exit 1
+autoheader || exit 1
+autoconf || exit 1
+automake -a -c || exit 1
+./configure --enable-maintainer-mode --enable-debug-log \
+	--enable-examples-build $*
