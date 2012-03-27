500add1480280bb4790f292179a36ce948f42972
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 15:01:00 +0000

    Assertion failure in acid2.
    Rubber-stamped by Andreas Kling.
    
    Remove assert added in http://trac.webkit.org/changeset/112258. It seems insertedInto/removedFromDocument
    don't always pair.
    
    * svg/SVGDocumentExtensions.cpp:
    (WebCore::SVGDocumentExtensions::~SVGDocumentExtensions):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGDocumentExtensions.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGDocumentExtensions.cpp
@@ -51,8 +51,6 @@ SVGDocumentExtensions::SVGDocumentExtensions(Document* document)
 
 SVGDocumentExtensions::~SVGDocumentExtensions()
 {
-    ASSERT(m_svgFontFaceElements.isEmpty());
-
     deleteAllValues(m_animatedElements);
     deleteAllValues(m_pendingResources);
     deleteAllValues(m_pendingResourcesForRemoval);
