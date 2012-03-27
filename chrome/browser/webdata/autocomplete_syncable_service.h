084bb0b33174f8431bb0633b2254d006decc5879
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 01:58:38 +0000

    Cull autofill entries older than 60 days. Tested with 20K Autocomplete entries selected 100000 times. 500 of them are in range. ---Start MergeDataAndStartSyncing for autofill Read from db - 7s Sift through them selecting only those needed for update - ~0.5s Process 476 changes - <1s ---End MergeDataAndStartSyncing for autofill - total ~8.5 s Callback back after deleting extra entries - 17s
    BUG=28990
    TEST=unit-test
    
    
    Review URL: http://codereview.chromium.org/9585020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/autocomplete_syncable_service.h
+++ chrome/browser/webdata/autocomplete_syncable_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <map>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -94,9 +95,15 @@ class AutocompleteSyncableService
       AutocompleteEntryMap;
 
   // Creates or updates an autocomplete entry based on |data|.
+  // |data| -  an entry for sync.
+  // |loaded_data| - entries that were loaded from local storage.
+  // |new_entries| - entries that came from the sync.
+  // |ignored_entries| - entries that came from the sync, but too old to be
+  // stored and immediately discarded.
   void CreateOrUpdateEntry(const SyncData& data,
                            AutocompleteEntryMap* loaded_data,
-                           std::vector<AutofillEntry>* bundle);
+                           std::vector<AutofillEntry>* new_entries,
+                           std::vector<AutofillEntry>* ignored_entries);
 
   // Writes |entry| data into supplied |autofill_specifics|.
   static void WriteAutofillEntry(const AutofillEntry& entry,
@@ -118,6 +125,9 @@ class AutocompleteSyncableService
   void set_sync_processor(SyncChangeProcessor* sync_processor) {
     sync_processor_.reset(sync_processor);
   }
+
+  // Ignore deletions of the following keys as they were never synced.
+  std::set<AutofillKey> keys_to_ignore_;
 
   // Lifetime of AutocompleteSyncableService object is shorter than
   // |web_data_service_| passed to it.
