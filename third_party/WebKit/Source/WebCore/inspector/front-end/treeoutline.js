4034bc1194c0ebb07bf55c48e58ab2ef3fc6161b
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 12:22:42 +0000

    Web Inspector: Tree outline should not start search on key press if it is being edited. https://bugs.webkit.org/show_bug.cgi?id=82327
    Reviewed by Pavel Feldman.
    
    This is needed for snippet renaming support.
    
    * inspector/front-end/treeoutline.js:
    (TreeOutline):
    (TreeOutline.prototype._treeKeyPress):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/treeoutline.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/treeoutline.js
@@ -39,6 +39,7 @@ function TreeOutline(listNode, nonFocusable)
     this.children = [];
     this.selectedTreeElement = null;
     this._childrenListNode = listNode;
+    this.childrenListElement = this._childrenListNode;
     this._childrenListNode.removeChildren();
     this.expandTreeElementsWhenArrowing = false;
     this.root = true;
@@ -336,7 +337,7 @@ TreeOutline.prototype.treeElementFromPoint = function(x, y)
 
 TreeOutline.prototype._treeKeyPress = function(event)
 {
-    if (!this.searchable)
+    if (!this.searchable || WebInspector.isBeingEdited(this._childrenListNode))
         return;
     
     var searchText = String.fromCharCode(event.charCode);
