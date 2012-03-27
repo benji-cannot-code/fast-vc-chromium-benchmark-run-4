a6736a06c51493ca3b1d549a5627d3a185684fdf
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 09:29:18 +0000

    Enable animVal support for SVGAnimatedBoolean https://bugs.webkit.org/show_bug.cgi?id=82311
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Enable animVal support for SVGAnimatedBoolean. Very simple now that everything is prepared.
    All we have to do is add startAnimValAnimation/etc. methods to SVGAnimatedBooleanAnimator.
    
    Extended existing tests to cover this.
    
    * svg/SVGAnimatedBoolean.cpp:
    (WebCore::SVGAnimatedBooleanAnimator::startAnimValAnimation):
    (WebCore::SVGAnimatedBooleanAnimator::stopAnimValAnimation):
    (WebCore::SVGAnimatedBooleanAnimator::resetAnimValToBaseVal):
    (WebCore::SVGAnimatedBooleanAnimator::animValWillChange):
    (WebCore::SVGAnimatedBooleanAnimator::animValDidChange):
    * svg/SVGAnimatedBoolean.h:
    (SVGAnimatedBooleanAnimator):
    * svg/SVGAnimatedType.cpp:
    (WebCore::SVGAnimatedType::setValueAsString):
    (WebCore::SVGAnimatedType::supportsAnimVal):
    
    LayoutTests:
    
    Extend SVGAnimatedBoolean tests to cover animVal support.
    
    * svg/animations/script-tests/svgboolean-animation-1.js:
    (sample1):
    (sample2):
    (sample3):
    * svg/animations/svgboolean-animation-1-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimatedType.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimatedType.cpp
@@ -399,14 +399,6 @@ bool SVGAnimatedType::setValueAsString(const QualifiedName& attrName, const Stri
 {
     ExceptionCode ec = 0;
     switch (m_type) {
-    case AnimatedAngle:
-        ASSERT(m_data.angle);
-        m_data.angle->setValueAsString(value, ec);
-        break;
-    case AnimatedBoolean:
-        ASSERT(m_data.boolean);
-        *m_data.boolean = value == "true" ? true : false;
-        break;
     case AnimatedColor:
         ASSERT(m_data.color);
         *m_data.color = value.isEmpty() ? Color() : SVGColor::colorFromRGBColorString(value);
@@ -454,6 +446,8 @@ bool SVGAnimatedType::setValueAsString(const QualifiedName& attrName, const Stri
         break;
 
     // These types don't appear in the table in SVGStyledElement::cssPropertyToTypeMap() and thus don't need setValueAsString() support. 
+    case AnimatedAngle:
+    case AnimatedBoolean:
     case AnimatedNumberList:
     case AnimatedNumberOptionalNumber:
     case AnimatedInteger:
@@ -478,6 +472,7 @@ bool SVGAnimatedType::supportsAnimVal(AnimatedPropertyType type)
     // FIXME: This lists the current state of our animVal support.
     switch (type) {
     case AnimatedAngle:
+    case AnimatedBoolean:
     case AnimatedInteger:
     case AnimatedIntegerOptionalInteger:
     case AnimatedLength:
@@ -487,7 +482,6 @@ bool SVGAnimatedType::supportsAnimVal(AnimatedPropertyType type)
     case AnimatedNumberOptionalNumber:
     case AnimatedTransformList:
         return true;
-    case AnimatedBoolean:
     case AnimatedColor:
     case AnimatedEnumeration:
     case AnimatedPath:
