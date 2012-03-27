fde3f601798407b65ef47bbda68c4f580bc4c56e
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-27 21:29:41 +0000

    Fix a missing block call that was found post review.
    git-svn-id: svn://svn.chromium.org/blink/trunk@112315 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/SoftLinking.h
+++ third_party/WebKit/Source/WebCore/platform/mac/SoftLinking.h
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             if (!result) \
                 result = dlopen("/System/Library/StagedFrameworks/Safari/" #framework ".framework/" #framework, RTLD_LAZY); \
             return result; \
-        }; \
+        }(); \
         return frameworkLibrary; \
     }
 
