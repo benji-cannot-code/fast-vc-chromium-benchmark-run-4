e18c353ab9d888dcadf7070a50cff96de559e1ee
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-28 12:26:03 +0000

    [BlackBerry] Remove PlatformMouseEvent::setClickCount for BlackBerry https://bugs.webkit.org/show_bug.cgi?id=82422
    Reviewed by Rob Buis.
    
    PlatformMouseEvent::setClickCount() is never used by BlackBerry.
    The declaration was added in PlatformMouseEvent.h internally for
    BlackBerry porting, but it has not been upstreamed yet.
    
    * platform/blackberry/PlatformMouseEventBlackBerry.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/blackberry/PlatformMouseEventBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/blackberry/PlatformMouseEventBlackBerry.cpp
@@ -40,9 +40,4 @@ PlatformMouseEvent::PlatformMouseEvent(const IntPoint& eventPos, const IntPoint&
     ASSERT(type != MouseEventScroll);
 }
 
-void PlatformMouseEvent::setClickCount(int count)
-{
-    m_clickCount = count;
-}
-
 } // namespace WebCore
