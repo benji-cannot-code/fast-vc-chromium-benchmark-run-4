ec66b008b57e526fa988f48cbee041ab457a6b86
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 00:41:28 +0000

    Fix test crashes caused by toxic use of ListenSocket.
    ListenSocket doesn't like it if delegates delete instances from within DidFoo() callbacks.  Instead of releasing references to sockets from within ConfigurableConnection::SendWithOptions (which is invoked indirectly by way of HTTPTestServer::DidRead), post a task to do so.  Handle the fact that there may be connections in an HTTPTestServer instance for which the underlying socket_ is NULL by harvesting them in HTTPTestServer::FindConnection().  As a result of all of this, it's possible that DidRead or DidClose may be called with a ListenSocket* that is no longer present in the server's connection list.  This isn't an error condition, so simply handle it gracefully in Did{Read,Close}.
    
    BUG=120458
    TEST=nothing special.  chrome_frame_tests.exe should stop crashing so much.
    
    
    Review URL: http://codereview.chromium.org/9863048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129319 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome_frame/test/test_server.h
+++ chrome_frame/test/test_server.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -360,6 +360,9 @@ class ConfigurableConnection : public base::RefCounted<ConfigurableConnection> {
   // Sends a chunk of the response and queues itself as a task for sending
   // next chunk of |data_|.
   void SendChunk();
+
+  // Closes the connection by releasing this instance's reference on its socket.
+  void Close();
 
   scoped_refptr<net::ListenSocket> socket_;
   Request r_;
