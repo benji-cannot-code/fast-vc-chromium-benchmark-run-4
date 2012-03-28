e5a0a4c36a56e88972cb703eedf87114de36a112
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-28 07:05:07 +0000

    Make Chromium port for Android use hardware gpu path default. https://bugs.webkit.org/show_bug.cgi?id=82033
    Patch by Hao Zheng <zhenghao@chromium.org> on 2012-03-28
    Reviewed by Dirk Pranke.
    
    Skip canvas tests, which are run in virtual gpu test suites.
    
    * Scripts/webkitpy/layout_tests/port/chromium_android.py:
    (ChromiumAndroidPort.__init__):
    (ChromiumAndroidPort.skipped_tests):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112368 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_android.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_android.py
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
 # Copyright (C) 2012 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,6 +33,7 @@ import re
 import signal
 import time
 
+from webkitpy.layout_tests.port import base
 from webkitpy.layout_tests.port import chromium
 from webkitpy.layout_tests.port import factory
 
@@ -145,6 +147,9 @@ class ChromiumAndroidPort(chromium.ChromiumPort):
     def __init__(self, host, port_name, **kwargs):
         chromium.ChromiumPort.__init__(self, host, port_name, **kwargs)
 
+        # The Chromium port for Android always uses the hardware GPU path.
+        self._options.enable_hardware_gpu = True
+
         self._operating_system = 'android'
         self._version = 'icecreamsandwich'
         self._original_governor = None
@@ -224,6 +229,13 @@ class ChromiumAndroidPort(chromium.ChromiumPort):
         # Leave the forwarder and tests httpd server there because they are
         # useful for debugging and do no harm to subsequent tests.
         self._teardown_performance()
+
+    def skipped_tests(self, test_list):
+        return base.Port._real_tests(self, [
+            # Canvas tests are run as virtual gpu tests.
+            'fast/canvas',
+            'canvas/philip',
+        ])
 
     def _build_path(self, *comps):
         return self._host_port._build_path(*comps)
