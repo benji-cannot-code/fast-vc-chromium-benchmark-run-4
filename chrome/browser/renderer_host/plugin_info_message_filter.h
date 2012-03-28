eee42bf1ec3fc404edc5a784779b6396f5376c9e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 00:46:09 +0000

    revert 128949 (and dependent 129252)
    Speculative, it might have caused a Morejs regression.
    
    BUG=120273
    TEST=none
    TBR=bauerb
    
    Review URL: http://codereview.chromium.org/9874001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/plugin_info_message_filter.h
+++ chrome/browser/renderer_host/plugin_info_message_filter.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct ChromeViewHostMsg_GetPluginInfo_Status;
 class GURL;
 class HostContentSettingsMap;
-class PluginFinder;
 class Profile;
 
 namespace content {
@@ -28,9 +27,6 @@ class ResourceContext;
 
 namespace webkit {
 struct WebPluginInfo;
-namespace npapi {
-class PluginGroup;
-}
 }
 
 // This class filters out incoming IPC messages requesting plug-in information.
@@ -46,11 +42,10 @@ class PluginInfoMessageFilter : public content::BrowserMessageFilter {
     Context();
     ~Context();
 
-    void DecidePluginStatus(
-        const GetPluginInfo_Params& params,
-        const webkit::WebPluginInfo& plugin,
-        PluginFinder* plugin_finder,
-        ChromeViewHostMsg_GetPluginInfo_Status* status) const;
+    void DecidePluginStatus(const GetPluginInfo_Params& params,
+                            ChromeViewHostMsg_GetPluginInfo_Status* status,
+                            webkit::WebPluginInfo* plugin,
+                            std::string* actual_mime_type) const;
     bool FindEnabledPlugin(int render_view_id,
                            const GURL& url,
                            const GURL& top_origin_url,
@@ -58,7 +53,7 @@ class PluginInfoMessageFilter : public content::BrowserMessageFilter {
                            ChromeViewHostMsg_GetPluginInfo_Status* status,
                            webkit::WebPluginInfo* plugin,
                            std::string* actual_mime_type) const;
-    void GetPluginContentSetting(const webkit::WebPluginInfo& plugin,
+    void GetPluginContentSetting(const webkit::WebPluginInfo* plugin,
                                  const GURL& policy_url,
                                  const GURL& plugin_url,
                                  const std::string& resource,
@@ -94,12 +89,6 @@ class PluginInfoMessageFilter : public content::BrowserMessageFilter {
   void PluginsLoaded(const GetPluginInfo_Params& params,
                      IPC::Message* reply_msg,
                      const std::vector<webkit::WebPluginInfo>& plugins);
-
-  void GotPluginFinder(const GetPluginInfo_Params& params,
-                       IPC::Message* reply_msg,
-                       const webkit::WebPluginInfo& plugin,
-                       const std::string& actual_mime_type,
-                       PluginFinder* plugin_finder);
 
   Context context_;
 
