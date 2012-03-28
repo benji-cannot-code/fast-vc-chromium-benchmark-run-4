7583e14347cf6034ec1349e00a9d62e353a8149d
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 13:44:41 +0000

    Using a random wallpaper each time user login. If user specifically select a wallpaper, then use the selected wallpaper.
    It also avoid to save wallpaper index to local state when an ephemeral user logged in.
    
    BUG=120215
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9856016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/user_images_grid.js
+++ chrome/browser/resources/chromeos/user_images_grid.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -128,13 +128,19 @@ cr.define('options', function() {
     },
     set selectedItemUrl(url) {
       for (var i = 0, el; el = this.dataModel.item(i); i++) {
-        if (el.url === url) {
-          this.inProgramSelection_ = true;
-          this.selectionModel.selectedIndex = i;
-          this.selectionModel.leadIndex = i;
-          this.inProgramSelection_ = false;
-        }
+        if (el.url === url)
+          this.selectedItemIndex = i;
       }
+    },
+
+    /**
+     * Set index to the image selected.
+     * @type {number} index The index of selected image.
+     */
+    set selectedItemIndex(index) {
+      this.inProgramSelection_ = true;
+      this.selectionModel.selectedIndex = index;
+      this.inProgramSelection_ = false;
     },
 
     /** @inheritDoc */
