f653ba9bd8edf996175a247f37c08ecf4cf38960
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 19:37:01 +0000

    Sync promo: Only show skip button on startup
    BUG=117456
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9836093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129462 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_promo/sync_promo.js
+++ chrome/browser/resources/sync_promo/sync_promo.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(sail): Refactor options_page and remove this include.
 <include src="../options/options_page.js"/>
+<include src="../shared/js/util.js"/>
 <include src="../sync_setup_overlay.js"/>
 
 cr.define('sync_promo', function() {
@@ -133,6 +134,12 @@ cr.define('sync_promo', function() {
       };
       $('confirm-everything-cancel').addEventListener('click', cancelFunc);
       $('choose-datatypes-cancel').addEventListener('click', cancelFunc);
+
+      // Add the source parameter to the document so that it can be used to
+      // selectively show and hide elements using CSS.
+      var params = parseQueryParams(document.location);
+      if (params.source == '0')
+        document.documentElement.setAttribute('isstartup', '');
     },
 
     /**
