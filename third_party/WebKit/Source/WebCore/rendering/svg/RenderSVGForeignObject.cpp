793f7196ee4c2a1ecc20b317fe632e16e9cf25e7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-28 21:00:37 +0000

    Incorrect foreignObject hit test results when overlapping other SVG elements https://bugs.webkit.org/show_bug.cgi?id=82059
    Patch by Florin Malita <fmalita@google.com> on 2012-03-28
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore:
    
    Test: svg/hittest/foreign-object-background.svg
    
    Foreign content needs to be hit-tested atomically due to the (pseudo)
    stacking context established by FOs.
    
    * rendering/svg/RenderSVGForeignObject.cpp:
    (WebCore::RenderSVGForeignObject::nodeAtFloatPoint):
    Hit test all phases on FO HitTestForeground.
    
    LayoutTests:
    
    * svg/hittest/foreign-object-background-expected.txt: Added.
    * svg/hittest/foreign-object-background.svg: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGForeignObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGForeignObject.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "GraphicsContext.h"
 #include "LayoutRepainter.h"
+#include "RenderObject.h"
 #include "RenderSVGResource.h"
 #include "RenderView.h"
 #include "SVGForeignObjectElement.h"
@@ -166,13 +167,20 @@ void RenderSVGForeignObject::layout()
 
 bool RenderSVGForeignObject::nodeAtFloatPoint(const HitTestRequest& request, HitTestResult& result, const FloatPoint& pointInParent, HitTestAction hitTestAction)
 {
+    // Embedded content is drawn in the foreground phase.
+    if (hitTestAction != HitTestForeground)
+        return false;
+
     FloatPoint localPoint = localTransform().inverse().mapPoint(pointInParent);
 
     // Early exit if local point is not contained in clipped viewport area
     if (SVGRenderSupport::isOverflowHidden(this) && !m_viewport.contains(localPoint))
         return false;
 
-    return RenderBlock::nodeAtPoint(request, result, roundedLayoutPoint(localPoint), LayoutPoint(), hitTestAction);
+    // FOs establish a stacking context, so we need to hit-test all layers.
+    return RenderBlock::nodeAtPoint(request, result, roundedLayoutPoint(localPoint), LayoutPoint(), HitTestForeground)
+        || RenderBlock::nodeAtPoint(request, result, roundedLayoutPoint(localPoint), LayoutPoint(), HitTestFloat)
+        || RenderBlock::nodeAtPoint(request, result, roundedLayoutPoint(localPoint), LayoutPoint(), HitTestChildBlockBackgrounds);
 }
 
 bool RenderSVGForeignObject::nodeAtPoint(const HitTestRequest&, HitTestResult&, const LayoutPoint&, const LayoutPoint&, HitTestAction)
