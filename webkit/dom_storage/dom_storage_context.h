b402362d849de55e56c32bde8606a09e5235049d
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 00:32:27 +0000

    DomStorageContext unittests.
    TBR=tony
    BUG=106763
    Review URL: https://chromiumcodereview.appspot.com/9860018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129317 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/dom_storage/dom_storage_context.h
+++ webkit/dom_storage/dom_storage_context.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomic_sequence_num.h"
 #include "base/basictypes.h"
 #include "base/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
 #include "base/time.h"
@@ -147,6 +148,8 @@ class DomStorageContext
   void CloneSessionNamespace(int64 existing_id, int64 new_id);
 
  private:
+  friend class DomStorageContextTest;
+  FRIEND_TEST_ALL_PREFIXES(DomStorageContextTest, Basics);
   friend class base::RefCountedThreadSafe<DomStorageContext>;
   typedef std::map<int64, scoped_refptr<DomStorageNamespace> >
       StorageNamespaceMap;
