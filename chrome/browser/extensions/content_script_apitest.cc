743c16e62aa42e264658d76c44ff7c0f4e8fd32d
abodenha@google.com <abodenha@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 21:58:05 +0000

    Mark flaky test as flaky.
    BUG=120762
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9885001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/content_script_apitest.cc
+++ chrome/browser/extensions/content_script_apitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -81,8 +81,14 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, ContentScriptViewSource) {
   ASSERT_TRUE(RunExtensionTest("content_scripts/view_source")) << message_;
 }
 
+#if defined (OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(
+    ExtensionApiTest,
+    FLAKY_ContentScriptStylesInjectedIntoExistingRenderers) {
+#else
 IN_PROC_BROWSER_TEST_F(
     ExtensionApiTest, ContentScriptStylesInjectedIntoExistingRenderers) {
+#endif
   ASSERT_TRUE(StartTestServer());
 
   ui_test_utils::WindowedNotificationObserver signal(
