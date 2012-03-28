5de402fc27f7a99b9449bb7c880f790523f8869b
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 01:15:49 +0000

    We disable it on beta until we fix the existing issues. The reason being clients are throttled at 5 uploads per day. So other crashes might be lost. We would ideally want to treat unrecoverable errors as crashes as well and fix them. But since we have so many now we should fix all these before we enable this on beta.
    This will be enabled on trunk in another check in after branch point.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9837092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129333 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/chrome_report_unrecoverable_error.cc
+++ chrome/browser/sync/glue/chrome_report_unrecoverable_error.cc
@@ -16,17 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_sync {
 
 void ChromeReportUnrecoverableError() {
+  // This currently works on windows platform but is disabled for now.
   // TODO(lipalani): Add this for other platforms as well.
-#if defined(OS_WIN)
-  // Get the breakpad pointer from chrome.exe
-  typedef void (__cdecl *DumpProcessFunction)();
-  DumpProcessFunction DumpProcess = reinterpret_cast<DumpProcessFunction>(
-      ::GetProcAddress(::GetModuleHandle(
-                       chrome::kBrowserProcessExecutableName),
-                       "DumpProcessWithoutCrash"));
-  if (DumpProcess)
-    DumpProcess();
-#endif  // OS_WIN
+  // TODO(lipalani) : Re-enable error reporting on trunk.
 }
 
 }  // namespace browser_sync
