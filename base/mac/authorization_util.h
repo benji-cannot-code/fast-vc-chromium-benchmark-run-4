7ba1b8a8fc9730c7ce397fa9334156847f3c87bd
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 04:43:47 +0000

    New BASE_EXPORT macros.
    BUG=90078
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9791049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129372 0039d316-1c4b-4281-b951-d872f2087c98

--- base/mac/authorization_util.h
+++ base/mac/authorization_util.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <sys/types.h>
 
+#include "base/base_export.h"
+
 namespace base {
 namespace mac {
 
@@ -38,6 +40,7 @@ namespace mac {
 // |prompt| will be used as the prompt string and an icon appropriate for the
 // application will be displayed in a prompt dialog.  Note that the system
 // appends its own text to the prompt string.  Returns NULL on failure.
+BASE_EXPORT
 AuthorizationRef AuthorizationCreateToRunAsRoot(CFStringRef prompt);
 
 // Calls straight through to AuthorizationExecuteWithPrivileges.  If that
@@ -57,6 +60,7 @@ OSStatus ExecuteWithPrivilegesAndGetPID(AuthorizationRef authorization,
 // exit status is placed in |exit_status|, otherwise, -1 is stored.
 // |exit_status| may be NULL and this function will still wait for the process
 // to exit.
+BASE_EXPORT
 OSStatus ExecuteWithPrivilegesAndWait(AuthorizationRef authorization,
                                       const char* tool_path,
                                       AuthorizationFlags options,
