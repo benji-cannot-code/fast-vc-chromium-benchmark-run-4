3ba93a86a48b6237741ffd91c510279270f9a5ec
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-28 00:12:30 +0000

    Add assertions to valueForLength() and RenderBox::computeLogicalWidthInRegionUsing() to help with debugging. https://bugs.webkit.org/show_bug.cgi?id=82393
    Reviewed by Eric Seidel.
    
    No new tests / adding assetions only.
    
    * css/LengthFunctions.cpp:
    (WebCore::valueForLength):
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeLogicalWidthInRegionUsing):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -1846,6 +1846,8 @@ LayoutUnit RenderBox::computeLogicalWidthInRegionUsing(LogicalWidthType widthTyp
     else
         logicalWidth = styleToUse->logicalMaxWidth();
 
+    ASSERT(!logicalWidth.isUndefined());
+
     if (logicalWidth.isIntrinsicOrAuto()) {
         RenderView* renderView = view();
         LayoutUnit marginStart = minimumValueForLength(styleToUse->marginStart(), availableLogicalWidth, renderView);
