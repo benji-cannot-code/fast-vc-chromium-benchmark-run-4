94fc2e4e500a793791c728ed0dba2d00ba404b39
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 03:57:59 +0000

    chromeos: Show an appropriate description of the networks in the tray list.
    If a wireless network is connecting, or a cellular network needs activation
    etc., then show the appropriate description in the list.
    
    BUG=120342
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9836112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129356 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/status/network_menu.cc
+++ chrome/browser/chromeos/status/network_menu.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/status/network_menu_icon.h"
 #include "chrome/browser/chromeos/status/status_area_view_chromeos.h"
 #include "chrome/browser/defaults.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/dialog_style.h"
 #include "chrome/browser/ui/views/window.h"
 #include "chrome/common/chrome_switches.h"
@@ -91,9 +91,8 @@ void SetMenuMargins(views::MenuItemView* menu_item_view, int top, int bottom) {
 // Activate a cellular network.
 void ActivateCellular(const chromeos::CellularNetwork* cellular) {
   DCHECK(cellular);
-  Browser* browser = BrowserList::GetLastActive();
-  if (!browser)
-    return;
+  Browser* browser = Browser::GetOrCreateTabbedBrowser(
+      ProfileManager::GetDefaultProfileOrOffTheRecord());
   browser->OpenMobilePlanTabAndActivate();
 }
 
@@ -490,9 +489,9 @@ void NetworkMenuModel::ActivatedAt(int index) {
     if (active_vpn)
       cros->DisconnectFromNetwork(active_vpn);
   } else if (flags & FLAG_VIEW_ACCOUNT) {
-    Browser* browser = BrowserList::GetLastActive();
-    if (browser)
-      browser->ShowSingletonTab(GURL(top_up_url_));
+    Browser* browser = Browser::GetOrCreateTabbedBrowser(
+        ProfileManager::GetDefaultProfileOrOffTheRecord());
+    browser->ShowSingletonTab(GURL(top_up_url_));
   }
 }
 
@@ -1037,9 +1036,8 @@ void NetworkMenu::RunMenu(views::View* source) {
 
 void NetworkMenu::ShowTabbedNetworkSettings(const Network* network) const {
   DCHECK(network);
-  Browser* browser = BrowserList::GetLastActive();
-  if (!browser)
-    return;
+  Browser* browser = Browser::GetOrCreateTabbedBrowser(
+      ProfileManager::GetDefaultProfileOrOffTheRecord());
 
   // In case of a VPN, show the config settings for the connected network.
   if (network->type() == chromeos::TYPE_VPN) {
