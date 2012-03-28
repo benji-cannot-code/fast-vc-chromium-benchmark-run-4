5092844c49e43b46ddf495de2563de3e7ab1fec9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-28 14:37:56 +0000

    window.scrollBy() scrolls incorrectly when zoomed in/out https://bugs.webkit.org/show_bug.cgi?id=45228
    Patch by Eugene Girard <girard@chromium.org> on 2012-03-28
    Reviewed by Simon Fraser.
    
    scrollBy now uses layout (not CSS) units. (As scrollTo does.)
    
    Source/WebCore:
    
    Test: fast/dom/zoom-scroll-page-test.html
    
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::scrollBy):
    
    LayoutTests:
    
    * fast/dom/zoom-scroll-page-test-expected.txt: Added.
    * fast/dom/zoom-scroll-page-test.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
@@ -1350,7 +1350,8 @@ void DOMWindow::scrollBy(int x, int y) const
     if (!view)
         return;
 
-    view->scrollBy(IntSize(x, y));
+    IntSize scaledOffset(view->mapFromCSSToLayoutUnits(x), view->mapFromCSSToLayoutUnits(y));
+    view->scrollBy(scaledOffset);
 }
 
 void DOMWindow::scrollTo(int x, int y) const
