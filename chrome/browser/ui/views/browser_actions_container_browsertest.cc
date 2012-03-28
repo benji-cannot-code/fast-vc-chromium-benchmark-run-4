4968baf5bc43717dde0be8156aa244c6754db8dc
georgey@chromium.org <georgey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 22:56:24 +0000

    Disable on windows BrowserActionsContainerTest.Basic BUG=120770 TEST=disabled TBR=sky@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9886002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129508 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/browser_actions_container_browsertest.cc
+++ chrome/browser/ui/views/browser_actions_container_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -47,7 +47,12 @@ class BrowserActionsContainerTest : public ExtensionBrowserTest {
 };
 
 // Test the basic functionality.
+// http://crbug.com/120770
+#if defined(OS_WIN)
+IN_PROC_BROWSER_TEST_F(BrowserActionsContainerTest, DISABLED_Basic) {
+#else
 IN_PROC_BROWSER_TEST_F(BrowserActionsContainerTest, Basic) {
+#endif
   BrowserActionsContainer::disable_animations_during_testing_ = true;
 
   // Load an extension with no browser action.
