f13664033cbbf2f45652f730bb4d0d1126e8bafc
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-28 00:49:13 +0000

    sync: fix delayed invocation of OnURLFetchComplete landing on garbage HttpBridge
    BUG=111511
    TEST=HttpBridgeTest
    
    
    Review URL: http://codereview.chromium.org/9732018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129323 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/http_bridge.h
+++ chrome/browser/sync/glue/http_bridge.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -146,6 +146,12 @@ class HttpBridge : public base::RefCountedThreadSafe<HttpBridge>,
   // of indirection is so that the unit test can override this behavior but we
   // still have a function to statically pass to PostTask.
   void CallMakeAsynchronousPost() { MakeAsynchronousPost(); }
+
+  // Used to destroy a fetcher when the bridge is Abort()ed, to ensure that
+  // a reference to |this| is held while flushing any pending fetch completion
+  // callbacks coming from the IO thread en route to finally destroying the
+  // fetcher.
+  void DestroyURLFetcherOnIOThread(content::URLFetcher* fetcher);
 
   // Gets a customized net::URLRequestContext for bridged requests. See
   // RequestContext definition for details.
