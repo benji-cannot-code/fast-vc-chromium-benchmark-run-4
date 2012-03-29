7f771035b059e2ce511d2791a9d317868ef7249a
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 09:47:19 +0000

    Fix illegal include of tools/json_schema_compiler/any.h
    BUG=112155
    TEST=no
    TBR=mpcomplete@chromium.org
    TBR=kalman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9903012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative_webrequest/webrequest_action.h
+++ chrome/browser/extensions/api/declarative_webrequest/webrequest_action.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/linked_ptr.h"
-#include "tools/json_schema_compiler/any.h"
+#include "chrome/common/extensions/api/experimental.declarative.h"
 
 namespace base {
 class Value;
