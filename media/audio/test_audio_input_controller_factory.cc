f5654eb7126a43a99677585baaf05c7c09987b29
rnk@chromium.org <rnk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 20:14:16 +0000

    Revert 129623, this was creating ~92 (test-only) heapcheck leak reports.
    - Fixed Close() method of TestAudioInputController (callback on caller thread).
    
    It has been changed to make this test class work coherently with the mocked
    AudioInputController, according to the modifications made in CL 9858007 (which
    issue the callback from the caller thread, not from the audio thread anymore).
    
    BUG=120110, 120111
    TEST=Run content_unittests.
    
    Review URL: https://chromiumcodereview.appspot.com/9864031
    
    TBR=primiano@chromium.org,jhawkins@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9924025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129674 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/test_audio_input_controller_factory.cc
+++ media/audio/test_audio_input_controller_factory.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/bind.h"
-#include "base/bind_helpers.h"
 #include "media/audio/test_audio_input_controller_factory.h"
 #include "media/audio/audio_io.h"
 
@@ -27,9 +25,7 @@ TestAudioInputController::~TestAudioInputController() {
 }
 
 void TestAudioInputController::Close(const base::Closure& closed_task) {
-  message_loop_->PostTaskAndReply(FROM_HERE,
-                                  base::Bind(&base::DoNothing),
-                                  closed_task);
+  message_loop_->PostTask(FROM_HERE, closed_task);
 }
 
 TestAudioInputControllerFactory::TestAudioInputControllerFactory()
