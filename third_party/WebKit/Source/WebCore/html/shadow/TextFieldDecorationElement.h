4e9eaa88d0a02e9d16f4d426b6f43f0d925b78af
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-29 06:00:23 +0000

    Add TextFieldDecorationElement::decorate() https://bugs.webkit.org/show_bug.cgi?id=82572
    Reviewed by Hajime Morita.
    
    This change will be needed for Bug 82143.
    No new tests because of no behavior changes yet. This code change
    doesn't affect non-Chromium ports for now.
    
    * dom/ShadowRoot.cpp:
    (WebCore::ShadowRoot::create):
    Remove unnecessary assertion. We should allow multiple UA shadow roots.
    * html/shadow/TextFieldDecorationElement.cpp:
    (WebCore::TextFieldDecorationElement::decorate):
    Added.
    This function adds another ShadowRoot, and it contains one flexible box container.
    The container contains existin ShadowRoot content and the decoration element.
    * html/shadow/TextFieldDecorationElement.h:
    (TextFieldDecorationElement): Add the declaration of decorate().
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/TextFieldDecorationElement.h
+++ third_party/WebKit/Source/WebCore/html/shadow/TextFieldDecorationElement.h
@@ -64,6 +64,7 @@ class TextFieldDecorationElement : public HTMLDivElement {
 public:
     static PassRefPtr<TextFieldDecorationElement> create(Document*, TextFieldDecorator*);
     TextFieldDecorator* textFieldDecorator() { return m_textFieldDecorator; }
+    void decorate(HTMLInputElement*);
 
 private:
     TextFieldDecorationElement(Document*, TextFieldDecorator*);
