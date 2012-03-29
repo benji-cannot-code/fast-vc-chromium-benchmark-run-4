63c44b7e7dde48dc4caf010ea01df4dc4ea60d38
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 23:11:46 +0000

    Optimized fetching of the very first feed
    Changed the very first feed from either disk cache or the server to be 200 items only. The remaining chunk of the content will be pulled separately and UI will be receive event notification when it completes. This should significantly speed up startup time for users with 10,000+ items.
    
    BUG=chromium-os:28532
    TEST=Unit:BrowserUrlUtilTest.AppendOrReplaceQueryParameter , Manual: make sure first chunk of root directory content shows quickly for any size of docs corpus.
    
    Review URL: https://chromiumcodereview.appspot.com/9918024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/browser_url_util.h
+++ chrome/browser/net/browser_url_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,6 +35,23 @@ void WriteURLToClipboard(const GURL& url,
 GURL AppendQueryParameter(const GURL& url,
                           const std::string& name,
                           const std::string& value);
+
+// Returns a new GURL by appending or replacing the given query parameter name
+// and the value. If |name| appears more than once, only the first name-value
+// pair is replaced. Unsafe characters in the name and the value are escaped
+// like %XX%XX. The original query component is preserved if it's present.
+//
+// Examples:
+//
+// AppendOrReplaceQueryParameter(
+//     GURL("http://example.com"), "name", "new").spec()
+// => "http://example.com?name=value"
+// AppendOrReplaceQueryParameter(
+//     GURL("http://example.com?x=y&name=old"), "name", "new").spec()
+// => "http://example.com?x=y&name=new"
+GURL AppendOrReplaceQueryParameter(const GURL& url,
+                                   const std::string& name,
+                                   const std::string& value);
 
 }  // namespace chrome_browser_net
 
--- chrome/browser/net/browser_url_util_unittest.cc
+++ chrome/browser/net/browser_url_util_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,6 +28,57 @@ TEST(BrowserUrlUtilTest, AppendQueryParameter) {
   EXPECT_EQ("http://example.com/path?existing=one&na+me=v.alue%3D",
             AppendQueryParameter(GURL("http://example.com/path?existing=one"),
                                  "na me", "v.alue=").spec());
+
+}
+
+TEST(BrowserUrlUtilTest, AppendOrReplaceQueryParameter) {
+  // Appending a name-value pair to a URL without a query component.
+  EXPECT_EQ("http://example.com/path?name=value",
+            AppendOrReplaceQueryParameter(GURL("http://example.com/path"),
+                                 "name", "value").spec());
+
+  // Appending a name-value pair to a URL with a query component.
+  // The original component should be preserved, and the new pair should be
+  // appended with '&'.
+  EXPECT_EQ("http://example.com/path?existing=one&name=value",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?existing=one"),
+          "name", "value").spec());
+
+  // Appending a name-value pair with unsafe characters included. The
+  // unsafe characters should be escaped.
+  EXPECT_EQ("http://example.com/path?existing=one&na+me=v.alue%3D",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?existing=one"),
+          "na me", "v.alue=").spec());
+
+  // Replace value of an existing paramater.
+  EXPECT_EQ("http://example.com/path?existing=one&name=new",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?existing=one&name=old"),
+          "name", "new").spec());
+
+  // Replace a name-value pair with unsafe characters included. The
+  // unsafe characters should be escaped.
+  EXPECT_EQ("http://example.com/path?na+me=n.ew%3D&existing=one",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?na+me=old&existing=one"),
+          "na me", "n.ew=").spec());
+
+  // Replace the value of first parameter with this name only.
+  EXPECT_EQ("http://example.com/path?name=new&existing=one&name=old",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?name=old&existing=one&name=old"),
+          "name", "new").spec());
+
+  // Preserve the content of the original params regarless of our failure to
+  // interpret them correctly.
+  EXPECT_EQ("http://example.com/path?bar&name=new&left=&"
+            "=right&=&&name=again",
+      AppendOrReplaceQueryParameter(
+          GURL("http://example.com/path?bar&name=old&left=&"
+                "=right&=&&name=again"),
+          "name", "new").spec());
 }
 
 }  // namespace chrome_browser_net.
