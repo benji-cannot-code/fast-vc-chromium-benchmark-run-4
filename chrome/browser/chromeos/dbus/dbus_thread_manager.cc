d8164e9fa3355133307a29c034ab371ccd6f2bb4
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 03:44:39 +0000

    Add FlimflamNetworkClient
    BUG=chromium-os:16557
    TEST=build success
    
    
    Review URL: http://codereview.chromium.org/9791045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129572 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/dbus_thread_manager.cc
+++ chrome/browser/chromeos/dbus/dbus_thread_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dbus/cashew_client.h"
 #include "chrome/browser/chromeos/dbus/cros_disks_client.h"
 #include "chrome/browser/chromeos/dbus/cryptohome_client.h"
+#include "chrome/browser/chromeos/dbus/flimflam_network_client.h"
 #include "chrome/browser/chromeos/dbus/image_burner_client.h"
 #include "chrome/browser/chromeos/dbus/introspectable_client.h"
 #include "chrome/browser/chromeos/dbus/power_manager_client.h"
@@ -63,6 +64,9 @@ class DBusThreadManagerImpl : public DBusThreadManager {
     // Create the Cryptohome client.
     cryptohome_client_.reset(
         CryptohomeClient::Create(system_bus_.get()));
+    // Create the Flimflam Network client.
+    flimflam_network_client_.reset(
+        FlimflamNetworkClient::Create(system_bus_.get()));
     // Create the image burner client.
     image_burner_client_.reset(ImageBurnerClient::Create(system_bus_.get()));
     // Create the introspectable object client.
@@ -136,6 +140,11 @@ class DBusThreadManagerImpl : public DBusThreadManager {
   }
 
   // DBusThreadManager override.
+  virtual FlimflamNetworkClient* GetFlimflamNetworkClient() OVERRIDE {
+    return flimflam_network_client_.get();
+  }
+
+  // DBusThreadManager override.
   virtual ImageBurnerClient* GetImageBurnerClient() OVERRIDE {
     return image_burner_client_.get();
   }
@@ -175,6 +184,7 @@ class DBusThreadManagerImpl : public DBusThreadManager {
   scoped_ptr<CashewClient> cashew_client_;
   scoped_ptr<CrosDisksClient> cros_disks_client_;
   scoped_ptr<CryptohomeClient> cryptohome_client_;
+  scoped_ptr<FlimflamNetworkClient> flimflam_network_client_;
   scoped_ptr<ImageBurnerClient> image_burner_client_;
   scoped_ptr<IntrospectableClient> introspectable_client_;
   scoped_ptr<PowerManagerClient> power_manager_client_;
