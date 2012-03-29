3fbbe71be6dfb93e11e9673a7dc4c506ed593400
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 21:30:50 +0000

    Move ui/view/touchui/touch_mode_support.* to ui/base and make it platform independent so it can be used to implement other touch features in aura.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9910019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129694 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/touch/touch_mode_support.h
+++ ui/base/touch/touch_mode_support.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_VIEWS_TOUCHUI_TOUCH_MODE_SUPPORT_H_
-#define UI_VIEWS_TOUCHUI_TOUCH_MODE_SUPPORT_H_
+#ifndef UI_BASE_TOUCH_TOUCH_MODE_SUPPORT_H_
+#define UI_BASE_TOUCH_TOUCH_MODE_SUPPORT_H_
 #pragma once
 
 #include "base/basictypes.h"
-#include "ui/views/views_export.h"
+#include "ui/base/ui_export.h"
 
-class VIEWS_EXPORT TouchModeSupport {
+class UI_EXPORT TouchModeSupport {
  public:
   // Returns whether we should be operating in touch-friendly mode.
   static bool IsTouchOptimized();
@@ -19,4 +19,4 @@ class VIEWS_EXPORT TouchModeSupport {
   DISALLOW_IMPLICIT_CONSTRUCTORS(TouchModeSupport);
 };
 
-#endif  // UI_VIEWS_TOUCHUI_TOUCH_MODE_SUPPORT_H_
+#endif  // UI_BASE_TOUCH_TOUCH_MODE_SUPPORT_H_
