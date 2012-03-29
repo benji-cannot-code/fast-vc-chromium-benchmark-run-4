665ce488ea92005af25230c946e48a525eb7ae16
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 18:22:47 +0000

    [Coverity] fixed uninit ctor
    CID=103634
    
    R=sergeyu@chromium.org
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9865028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129650 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/transport.cc
+++ remoting/protocol/transport.cc
@@ -24,7 +24,7 @@ std::string TransportRoute::GetTypeString(RouteType type) {
   return "";
 }
 
-TransportRoute::TransportRoute() {
+TransportRoute::TransportRoute() : type(DIRECT) {
 }
 
 TransportRoute::~TransportRoute() {
