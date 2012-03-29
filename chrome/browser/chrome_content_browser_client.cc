0b00aebbdb6b0d6c778d7c7e7296d2724b14a31f
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 01:59:37 +0000

    Eliminate VerifyPermissions. Propagate --enable-platform-apps to utility process.
    VerifyPermissions was replaced with something simpler in http://codereview.chromium.org/9867003/, and another simple browser test in chrome/browser/extensions/crx_installer_browsertest.cc.
    
    We weren't copying --enable-platform-apps to the utility process that unzipped CRX files, so it wasn't able to load platform app CRXes.
    
    BUG=120075, also in-person reported during hackathon
    TEST=added (and refactored others)
    
    Review URL: https://chromiumcodereview.appspot.com/9839106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129556 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -794,6 +794,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
   } else if (process_type == switches::kUtilityProcess) {
     static const char* const kSwitchNames[] = {
       switches::kEnableExperimentalExtensionApis,
+      switches::kEnablePlatformApps,
       switches::kWhitelistedExtensionID,
     };
 
