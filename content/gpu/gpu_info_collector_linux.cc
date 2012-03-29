47aedd2fc901f20d8ba185549382108a896f63b3
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 03:28:30 +0000

    Move ATI driver version collection back to UI thread.
    BUG=115212
    TEST=
    
    Review URL: http://codereview.chromium.org/9895002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129570 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_info_collector_linux.cc
+++ content/gpu/gpu_info_collector_linux.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -185,21 +185,23 @@ bool CollectGraphicsInfo(content::GPUInfo* gpu_info) {
   gpu_info->finalized = true;
   bool rt = CollectGraphicsInfoGL(gpu_info);
 
-  if (gpu_info->vendor_id == 0x1002) {  // ATI
-    std::string ati_driver_version = CollectDriverVersionATI();
-    if (ati_driver_version != "") {
-      gpu_info->driver_vendor = "ATI / AMD";
-      gpu_info->driver_version = ati_driver_version;
-    }
-  }
-
   return rt;
 }
 
 bool CollectPreliminaryGraphicsInfo(content::GPUInfo* gpu_info) {
   DCHECK(gpu_info);
 
-  return CollectVideoCardInfo(gpu_info);
+  bool rt = CollectVideoCardInfo(gpu_info);
+
+  if (gpu_info->vendor_id == 0x1002) {  // ATI
+    std::string ati_driver_version = CollectDriverVersionATI();
+    if (!ati_driver_version.empty()) {
+      gpu_info->driver_vendor = "ATI / AMD";
+      gpu_info->driver_version = ati_driver_version;
+    }
+  }
+
+  return rt;
 }
 
 bool CollectVideoCardInfo(content::GPUInfo* gpu_info) {
