a558bcc8bdef92b5ef40db865c44d01a70c14119
primiano@chromium.org <primiano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 10:54:31 +0000

    Fixed a regression on speech arisen after r129173 .
    Made OnAudioClosed a member of SpeechRecognizerImpl. In this way, the closure used in the call to audio_controller_->Close() will hold a reference on SpeechRecognizerImpl until the Close has finished. Without this, the OnData method might be called from the audio thread after the object has been destructed.
    
    BUG=120173
    TEST=none.
    
    Review URL: https://chromiumcodereview.appspot.com/9791070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129606 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/speech/speech_recognizer_impl.cc
+++ content/browser/speech/speech_recognizer_impl.cc
@@ -58,7 +58,6 @@ bool DetectClipping(const speech::AudioChunk& chunk) {
   return false;
 }
 
-void OnAudioClosed(AudioInputController*) {}
 }  // namespace
 
 SpeechRecognizer* SpeechRecognizer::Create(
@@ -303,6 +302,8 @@ void SpeechRecognizerImpl::HandleOnData(scoped_refptr<AudioChunk> raw_audio) {
     StopAudioCapture();
 }
 
+void SpeechRecognizerImpl::OnAudioClosed(AudioInputController*) {}
+
 void SpeechRecognizerImpl::OnSpeechRecognitionEngineResult(
     const content::SpeechRecognitionResult& result) {
   // Guard against the listener freeing us until we finish our job.
@@ -332,7 +333,8 @@ void SpeechRecognizerImpl::CloseAudioControllerAsynchronously() {
   // purpose of such callback is to keep the audio controller refcounted until
   // Close has completed (in the audio thread) and automatically destroy it
   // afterwards (upon return from OnAudioClosed).
-  audio_controller_->Close(base::Bind(&OnAudioClosed, audio_controller_));
+  audio_controller_->Close(base::Bind(&SpeechRecognizerImpl::OnAudioClosed,
+                                      this, audio_controller_));
   audio_controller_ = NULL;  // The controller is still refcounted by Bind.
 }
 
