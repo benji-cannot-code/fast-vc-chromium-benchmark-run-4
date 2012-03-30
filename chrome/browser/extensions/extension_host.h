b84fcdfc000c12f0f56162cca6f4e3cea3e28b81
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 20:50:43 +0000

    Reset |associated_web_contents_| in extension host when the web contents goes away
    BUG=chromium-os:27892
    TEST=manually checked that crash described in bug does not repro (comment #6 in the bug)
    
    Review URL: https://chromiumcodereview.appspot.com/9912002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_host.h
+++ chrome/browser/extensions/extension_host.h
@@ -127,9 +127,7 @@ class ExtensionHost : public content::WebContentsDelegate,
 
   // ExtensionFunctionDispatcher::Delegate
   virtual content::WebContents* GetAssociatedWebContents() const OVERRIDE;
-  void set_associated_web_contents(content::WebContents* web_contents) {
-    associated_web_contents_ = web_contents;
-  }
+  void SetAssociatedWebContents(content::WebContents* web_contents);
 
   // Returns true if the render view is initialized and didn't crash.
   bool IsRenderViewLive() const;
