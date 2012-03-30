a991c30b2dd99b441455310f238679e2860d621c
reed@google.com <reed@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 17:08:51 +0000

    pass alpha directly to player, rather than creating a layer (for performance) https://bugs.webkit.org/show_bug.cgi?id=82360
    Reviewed by Stephen White.
    
    Performance change, existing webkit tests apply.
    
    * public/WebMediaPlayer.h:
    (WebMediaPlayer):
    * src/WebMediaPlayerClientImpl.cpp:
    (WebKit::WebMediaPlayerClientImpl::paintCurrentFrameInContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112682 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.cpp
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "MediaPlayer.h"
 #include "NotImplemented.h"
+#include "PlatformContextSkia.h"
 #include "RenderView.h"
 #include "TimeRanges.h"
 #include "VideoFrameChromium.h"
@@ -40,11 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(ACCELERATED_COMPOSITING)
 #include "RenderLayerCompositor.h"
-#endif
-
-// WebCommon.h defines WEBKIT_USING_SKIA so this has to be included last.
-#if WEBKIT_USING_SKIA
-#include "PlatformContextSkia.h"
 #endif
 
 #include <wtf/Assertions.h>
@@ -496,20 +492,9 @@ void WebMediaPlayerClientImpl::paintCurrentFrameInContext(GraphicsContext* conte
     // Since we're accessing platformContext() directly we have to manually
     // check.
     if (m_webMediaPlayer && !context->paintingDisabled()) {
-#if WEBKIT_USING_SKIA
         PlatformGraphicsContext* platformContext = context->platformContext();
         WebCanvas* canvas = platformContext->canvas();
-
-        canvas->saveLayerAlpha(0, platformContext->getNormalizedAlpha());
-
-        m_webMediaPlayer->paint(canvas, rect);
-
-        canvas->restore();
-#elif WEBKIT_USING_CG
-        m_webMediaPlayer->paint(context->platformContext(), rect);
-#else
-        notImplemented();
-#endif
+        m_webMediaPlayer->paint(canvas, rect, platformContext->getNormalizedAlpha());
     }
 }
 
