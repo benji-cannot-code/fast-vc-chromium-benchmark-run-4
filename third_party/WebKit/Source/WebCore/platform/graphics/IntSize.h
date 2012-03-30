02da97917df0c5212d90d5d544354fd15f9fb629
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 15:47:04 +0000

    [Qt] Find zoomable area using area-based hit-testing https://bugs.webkit.org/show_bug.cgi?id=82609
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-03-30
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Implement API for calculating the best zoomable area for a
    tap-to-zoom gesture.
    It picks the area with the largest intersection with the touch. In most
    cases this will be all areas fully containing the area, and returns the
    smallest inner-most of these.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::bestZoomableAreaForTouchPoint):
    * page/EventHandler.h:
    * page/TouchAdjustment.cpp:
    (WebCore::TouchAdjustment::nodeIsZoomTarget):
    (WebCore::TouchAdjustment::appendZoomableSubtargets):
    (WebCore::TouchAdjustment::compileZoomableSubtargets):
    (WebCore::TouchAdjustment::areaOfIntersection):
    (WebCore::TouchAdjustment::findAreaWithLargestIntersection):
    (WebCore::findBestZoomableArea):
    * page/TouchAdjustment.h:
    * platform/graphics/IntSize.h:
    (WebCore::IntSize::area):
    
    Source/WebKit2:
    
    Add area to findZoomableAreaForPoint and use new TOUCH_ADJUSTMENT
    code-path to find the best zoomable area.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::findZoomableAreaForPoint):
    * UIProcess/WebPageProxy.h:
    (WebPageProxy):
    * UIProcess/qt/QtWebPageEventHandler.cpp:
    (QtWebPageEventHandler::handleDoubleTapEvent):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit):
    (WebKit::WebPage::findZoomableAreaForPoint):
    * WebProcess/WebPage/WebPage.h:
    (WebPage):
    * WebProcess/WebPage/WebPage.messages.in:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/IntSize.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/IntSize.h
@@ -105,6 +105,11 @@ public:
         *this = expandedTo(IntSize());
     }
 
+    int area() const
+    {
+        return m_width * m_height;
+    }
+
     int diagonalLengthSquared() const
     {
         return m_width * m_width + m_height * m_height;
