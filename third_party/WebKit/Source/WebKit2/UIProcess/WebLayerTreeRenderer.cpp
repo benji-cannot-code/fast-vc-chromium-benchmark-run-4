944ed0f4d801c57f4c8aec513726f2f2dca561c9
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 00:21:00 +0000

    [Qt][WK2] Direct composited image assignment doesn't work https://bugs.webkit.org/show_bug.cgi?id=82525
    Reviewed by Kenneth Rohde Christiansen.
    
    We don't need to check whether the image or contentsRect are updated,
    since assignImageToLayer is a cheap operation after the LayerBackingStore
    refactor.
    
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::setLayerChildren):
    (WebKit::WebLayerTreeRenderer::setLayerState):
    (WebKit::WebLayerTreeRenderer::renderNextFrame):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112617 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
@@ -174,7 +174,6 @@ void WebLayerTreeRenderer::syncLayerParameters(const WebLayerInfo& layerInfo)
     ensureLayer(id);
     LayerMap::iterator it = m_layers.find(id);
     GraphicsLayer* layer = it->second;
-    bool needsToUpdateImageTiles = layerInfo.imageIsUpdated || (layerInfo.contentsRect != layer->contentsRect() && layerInfo.imageBackingStoreID);
 
     layer->setName(layerInfo.name);
 
@@ -191,8 +190,7 @@ void WebLayerTreeRenderer::syncLayerParameters(const WebLayerInfo& layerInfo)
     layer->setContentsRect(layerInfo.contentsRect);
     layer->setDrawsContent(layerInfo.drawsContent);
 
-    if (needsToUpdateImageTiles)
-        assignImageToLayer(layer, layerInfo.imageBackingStoreID);
+    assignImageToLayer(layer, layerInfo.imageBackingStoreID);
 
     // Never make the root layer clip.
     layer->setMasksToBounds(layerInfo.isRootLayer ? false : layerInfo.masksToBounds);
@@ -319,6 +317,11 @@ void WebLayerTreeRenderer::destroyImage(int64_t imageID)
 
 void WebLayerTreeRenderer::assignImageToLayer(GraphicsLayer* layer, int64_t imageID)
 {
+    if (!imageID) {
+        layer->setContentsToMedia(0);
+        return;
+    }
+
     HashMap<int64_t, RefPtr<TextureMapperBackingStore> >::iterator it = m_directlyCompositedImages.find(imageID);
     ASSERT(it != m_directlyCompositedImages.end());
     layer->setContentsToMedia(it->second.get());
--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
@@ -358,7 +358,6 @@ void WebGraphicsLayer::setContentsToImage(Image* image)
 
     m_layerInfo.imageBackingStoreID = newID;
     m_image = image;
-    m_layerInfo.imageIsUpdated = true;
     GraphicsLayer::setContentsToImage(image);
 }
 
@@ -432,12 +431,8 @@ WebGraphicsLayer* toWebGraphicsLayer(GraphicsLayer* layer)
 void WebGraphicsLayer::syncCompositingStateForThisLayerOnly()
 {
     // The remote image might have been released by purgeBackingStores.
-    if (m_image) {
-        if (!m_layerInfo.imageBackingStoreID) {
-            m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
-            m_layerInfo.imageIsUpdated = true;
-        }
-    }
+    if (m_image && !m_layerInfo.imageBackingStoreID)
+        m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
 
     if (m_modified) {
         computeTransformedVisibleRect();
@@ -470,7 +465,6 @@ void WebGraphicsLayer::syncCompositingStateForThisLayerOnly()
     updateContentBuffers();
 
     m_modified = false;
-    m_layerInfo.imageIsUpdated = false;
     if (m_hasPendingAnimations)
         notifyAnimationStarted(WTF::currentTime());
     m_layerInfo.animations.clear();
