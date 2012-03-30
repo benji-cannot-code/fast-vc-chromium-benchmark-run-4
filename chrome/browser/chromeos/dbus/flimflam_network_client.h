10bc2fa7d7b0d12b4e1a9793e152ea31a2ae1ea9
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 06:13:35 +0000

    Add chromeos::FlimflamClientHelper
    BUG=chromium-os:16557
    TEST=build success
    
    
    Review URL: http://codereview.chromium.org/9930001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/flimflam_network_client.h
+++ chrome/browser/chromeos/dbus/flimflam_network_client.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/callback.h"
+#include "chrome/browser/chromeos/dbus/flimflam_client_helper.h"
 
 namespace base {
 
@@ -31,20 +32,11 @@ namespace chromeos {
 // initializes the DBusThreadManager instance.
 class FlimflamNetworkClient {
  public:
-  // An enum to describe whether or not a DBus method call succeeded.
-  enum CallStatus {
-    FAILURE,
-    SUCCESS,
-  };
-
-  // A callback to handle PropertyChanged signals.
-  typedef base::Callback<void(const std::string& name,
-                              const base::Value& value)> PropertyChangedHandler;
-
-  // A callback to handle responses of methods with DictionaryValue results.
-  typedef base::Callback<void(
-      CallStatus call_status,
-      const base::DictionaryValue& result)> DictionaryValueCallback;
+  typedef FlimflamClientHelper::PropertyChangedHandler PropertyChangedHandler;
+  typedef FlimflamClientHelper::DictionaryValueCallback DictionaryValueCallback;
+  typedef FlimflamClientHelper::CallStatus CallStatus;
+  static const CallStatus FAILURE = FlimflamClientHelper::FAILURE;
+  static const CallStatus SUCCESS = FlimflamClientHelper::SUCCESS;
 
   virtual ~FlimflamNetworkClient();
 
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -493,6 +493,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/dbus/dbus_thread_manager.h',
         'browser/chromeos/dbus/flimflam_ipconfig_client.cc',
         'browser/chromeos/dbus/flimflam_ipconfig_client.h',
+        'browser/chromeos/dbus/flimflam_client_helper.cc',
+        'browser/chromeos/dbus/flimflam_client_helper.h',
         'browser/chromeos/dbus/flimflam_network_client.cc',
         'browser/chromeos/dbus/flimflam_network_client.h',
         'browser/chromeos/dbus/flimflam_profile_client.cc',
