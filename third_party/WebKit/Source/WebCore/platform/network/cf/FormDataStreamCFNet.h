9d225d8ec4e4ab02458a04ad6c62d5cb35dbdcb9
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 10:03:00 +0000

    Build fix after r112482.
    * platform/network/cf/FormDataStreamCFNet.h: Added missing forward decleration.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/FormDataStreamCFNet.h
+++ third_party/WebKit/Source/WebCore/platform/network/cf/FormDataStreamCFNet.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 
 typedef struct _CFURLRequest* CFMutableURLRequestRef;
+typedef struct __CFReadStream* CFReadStreamRef;
 typedef const struct _CFURLRequest* CFURLRequestRef;
 
 namespace WebCore {
