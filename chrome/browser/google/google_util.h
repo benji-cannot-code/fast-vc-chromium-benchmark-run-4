506ba72f54a74e78a550eff8c55fd5f105757a9b
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 19:57:43 +0000

    Added a comment on authenticity guarantees in google_util.
    BUG=120657
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9950016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_util.h
+++ chrome/browser/google/google_util.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
+// This namespace provides various helpers around handling Google-related URLs
+// and state relating to Google Chrome distributions (such as RLZ).
 namespace google_util {
 
 extern const char kLinkDoctorBaseURL[];
@@ -39,12 +41,16 @@ bool GetBrand(std::string* brand);
 // install. Returns false if the information is not available.
 bool GetReactivationBrand(std::string* brand);
 
+// WARNING: The following IsGoogleXXX() functions use heuristics to rule out
+// "obviously false" answers.  They do NOT guarantee that the string in question
+// is actually on a Google-owned domain, just that it looks plausible.  If you
+// need to restrict some behavior to only happen on Google's officially-owned
+// domains, use TransportSecurityState::IsGooglePinnedProperty() instead.
+
 // True if |host| is "[www.]google.<TLD>" with a valid TLD.
 bool IsGoogleHostname(const std::string& host);
-
 // True if |url| represents a valid Google home page URL.
 bool IsGoogleHomePageUrl(const std::string& url);
-
 // True if |url| represents a valid Google search URL.
 bool IsGoogleSearchUrl(const std::string& url);
 
