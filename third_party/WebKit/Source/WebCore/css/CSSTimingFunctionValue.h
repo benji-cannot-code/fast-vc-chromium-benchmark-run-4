a0646f4c66b3916ff3b3a48b901a667b5a47b885
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 22:21:43 +0000

    Kill CSSTimingFunctionValue. <http://webkit.org/b/82787>
    Reviewed by Antti Koivisto.
    
    Remove CSSTimingFunctionValue and let the 3 subclasses inherit directly from CSSValue.
    CSSTimingFunctionValue is a pointless middle-man class that adds nothing.
    
    * css/CSSStyleSelector.cpp:
    (WebCore::CSSStyleSelector::collectMatchingRulesForList):
    * css/CSSTimingFunctionValue.h:
    (WebCore::CSSLinearTimingFunctionValue::CSSLinearTimingFunctionValue):
    (WebCore::CSSCubicBezierTimingFunctionValue::CSSCubicBezierTimingFunctionValue):
    (WebCore::CSSStepsTimingFunctionValue::CSSStepsTimingFunctionValue):
    * css/CSSValue.h:
    (WebCore::CSSValue::isCubicBezierTimingFunctionValue):
    (WebCore::CSSValue::isLinearTimingFunctionValue):
    (WebCore::CSSValue::isStepsTimingFunctionValue):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSTimingFunctionValue.h
+++ third_party/WebKit/Source/WebCore/css/CSSTimingFunctionValue.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,20 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSSTimingFunctionValue : public CSSValue {
-public:
-    bool isLinearTimingFunctionValue() const { return classType() == LinearTimingFunctionClass; }
-    bool isCubicBezierTimingFunctionValue() const { return classType() == CubicBezierTimingFunctionClass; }
-    bool isStepsTimingFunctionValue() const { return classType() == StepsTimingFunctionClass; }
-
-protected:
-    CSSTimingFunctionValue(ClassType classType)
-        : CSSValue(classType)
-    {
-    }
-};
-
-class CSSLinearTimingFunctionValue : public CSSTimingFunctionValue {
+class CSSLinearTimingFunctionValue : public CSSValue {
 public:
     static PassRefPtr<CSSLinearTimingFunctionValue> create()
     {
@@ -56,12 +43,12 @@ public:
 
 private:
     CSSLinearTimingFunctionValue()
-        : CSSTimingFunctionValue(LinearTimingFunctionClass)
+        : CSSValue(LinearTimingFunctionClass)
     {
     }
 };
 
-class CSSCubicBezierTimingFunctionValue : public CSSTimingFunctionValue {
+class CSSCubicBezierTimingFunctionValue : public CSSValue {
 public:
     static PassRefPtr<CSSCubicBezierTimingFunctionValue> create(double x1, double y1, double x2, double y2)
     {
@@ -77,7 +64,7 @@ public:
 
 private:
     CSSCubicBezierTimingFunctionValue(double x1, double y1, double x2, double y2)
-        : CSSTimingFunctionValue(CubicBezierTimingFunctionClass)
+        : CSSValue(CubicBezierTimingFunctionClass)
         , m_x1(x1)
         , m_y1(y1)
         , m_x2(x2)
@@ -91,7 +78,7 @@ private:
     double m_y2;
 };
 
-class CSSStepsTimingFunctionValue : public CSSTimingFunctionValue {
+class CSSStepsTimingFunctionValue : public CSSValue {
 public:
     static PassRefPtr<CSSStepsTimingFunctionValue> create(int steps, bool stepAtStart)
     {
@@ -105,7 +92,7 @@ public:
 
 private:
     CSSStepsTimingFunctionValue(int steps, bool stepAtStart)
-        : CSSTimingFunctionValue(StepsTimingFunctionClass)
+        : CSSValue(StepsTimingFunctionClass)
         , m_steps(steps)
         , m_stepAtStart(stepAtStart)
     {
