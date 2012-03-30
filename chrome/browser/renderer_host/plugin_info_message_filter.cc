81ab35d6ad0aa096d6d75055a8f0b58345eb4269
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 03:30:19 +0000

    Revert 129775 - Remove plug-in infobar experiment, basically a revert of the revisions noted in the bug. Revert was done manually due to refactoring, etc.
    BUG=85988
    Review URL: https://chromiumcodereview.appspot.com/9802030
    
    TBR=cevans@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9963003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/plugin_info_message_filter.cc
+++ chrome/browser/renderer_host/plugin_info_message_filter.cc
@@ -27,7 +27,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugin_installer.h"
 #endif
 
+#if defined(OS_WIN)
+// These includes are only necessary for the PluginInfobarExperiment.
+#include "chrome/common/attrition_experiments.h"
+#include "chrome/installer/util/google_update_settings.h"
+#endif
+
 using content::PluginService;
+
+namespace {
+
+// Override the behavior of the security infobars for plugins. Only
+// operational on windows and only for a small slice of the of the
+// UMA opted-in population.
+void PluginInfobarExperiment(bool* allow_outdated,
+                             bool* always_authorize) {
+#if !defined(OS_WIN)
+  return;
+#else
+ std::wstring client_value;
+ if (!GoogleUpdateSettings::GetClient(&client_value))
+   return;
+ if (client_value == attrition_experiments::kPluginNoBlockNoOOD) {
+   *always_authorize = true;
+   *allow_outdated = true;
+ } else if (client_value == attrition_experiments::kPluginNoBlockDoOOD) {
+   *always_authorize = true;
+   *allow_outdated = false;
+ } else if (client_value == attrition_experiments::kPluginDoBlockNoOOD) {
+   *always_authorize = false;
+   *allow_outdated = true;
+ } else if (client_value == attrition_experiments::kPluginDoBlockDoOOD) {
+   *always_authorize = false;
+   *allow_outdated = false;
+ }
+#endif
+}
+
+}  // namespace
 
 PluginInfoMessageFilter::Context::Context(int render_process_id,
                                           Profile* profile)
@@ -161,6 +198,8 @@ void PluginInfoMessageFilter::Context::DecidePluginStatus(
 
   bool allow_outdated = allow_outdated_plugins_.GetValue();
   bool always_authorize = always_authorize_plugins_.GetValue();
+
+  PluginInfobarExperiment(&allow_outdated, &always_authorize);
 
   // Check if the plug-in is outdated.
   if (group->IsVulnerable(plugin) && !allow_outdated) {
