7ca22f99be1562d6bfb4ec572811e2fd5a01da44
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 21:30:03 +0000

    views/omnibox: Forward declare Menu2.
    BUG=119612
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9950026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129945 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_view_win.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_win.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/mouse_wheel_util.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/button_drag_utils.h"
+#include "ui/views/controls/menu/menu_2.h"
 #include "ui/views/controls/textfield/native_textfield_win.h"
 #include "ui/views/widget/widget.h"
 
--- chrome/browser/ui/views/omnibox/omnibox_view_win.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_win.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autocomplete/autocomplete_popup_contents_view.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/gfx/font.h"
-#include "ui/views/controls/menu/menu_2.h"
 #include "webkit/glue/window_open_disposition.h"
 
 class AutocompleteEditController;
@@ -32,6 +31,7 @@ class AutocompletePopupView;
 class LocationBarView;
 
 namespace views {
+class Menu2;
 class NativeViewHost;
 class View;
 }
