dad0d83818031d69696ee993ac535bd9e2605be2
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-30 01:36:05 +0000

    Unreviewed, rolling out r112579. http://trac.webkit.org/changeset/112579 https://bugs.webkit.org/show_bug.cgi?id=82657
    Does not compile in Windows component build
    
    Source/WebCore:
    
    * WebCore.gypi:
    
    Source/WebKit/chromium:
    
    * WebKit.gyp:
    * src/ResourceHandle.cpp: Renamed from Source/WebCore/platform/network/chromium/ResourceHandle.cpp.
    (WebCore):
    (WebCore::ResourceHandleInternal::ResourceHandleInternal):
    (WebCore::ResourceHandleInternal::start):
    (WebCore::ResourceHandleInternal::cancel):
    (WebCore::ResourceHandleInternal::setDefersLoading):
    (WebCore::ResourceHandleInternal::allowStoredCredentials):
    (WebCore::ResourceHandleInternal::willSendRequest):
    (WebCore::ResourceHandleInternal::didSendData):
    (WebCore::ResourceHandleInternal::didReceiveResponse):
    (WebCore::ResourceHandleInternal::didDownloadData):
    (WebCore::ResourceHandleInternal::didReceiveData):
    (WebCore::ResourceHandleInternal::didReceiveCachedMetadata):
    (WebCore::ResourceHandleInternal::didFinishLoading):
    (WebCore::ResourceHandleInternal::didFail):
    (WebCore::ResourceHandleInternal::FromResourceHandle):
    (WebCore::ResourceHandle::ResourceHandle):
    (WebCore::ResourceHandle::create):
    (WebCore::ResourceHandle::firstRequest):
    (WebCore::ResourceHandle::client):
    (WebCore::ResourceHandle::setClient):
    (WebCore::ResourceHandle::setDefersLoading):
    (WebCore::ResourceHandle::start):
    (WebCore::ResourceHandle::hasAuthenticationChallenge):
    (WebCore::ResourceHandle::clearAuthentication):
    (WebCore::ResourceHandle::cancel):
    (WebCore::ResourceHandle::~ResourceHandle):
    (WebCore::ResourceHandle::loadsBlocked):
    (WebCore::ResourceHandle::loadResourceSynchronously):
    (WebCore::ResourceHandle::willLoadFromCache):
    (WebCore::ResourceHandle::cacheMetadata):
    * src/ResourceHandleInternal.h: Renamed from Source/WebCore/platform/network/chromium/ResourceHandleInternal.h.
    (WebCore):
    (ResourceHandleInternal):
    (WebCore::ResourceHandleInternal::~ResourceHandleInternal):
    (WebCore::ResourceHandleInternal::setOwner):
    (WebCore::ResourceHandleInternal::request):
    (WebCore::ResourceHandleInternal::client):
    (WebCore::ResourceHandleInternal::setClient):
    (WebCore::ResourceHandleInternal::loader):
    * src/WrappedResourceRequest.h: Renamed from Source/WebCore/platform/chromium/support/WrappedResourceRequest.h.
    (WebKit):
    (WrappedResourceRequest):
    (WebKit::WrappedResourceRequest::~WrappedResourceRequest):
    (WebKit::WrappedResourceRequest::WrappedResourceRequest):
    (WebKit::WrappedResourceRequest::bind):
    (Handle):
    (WebKit::WrappedResourceRequest::Handle::dispose):
    * src/WrappedResourceResponse.h: Renamed from Source/WebCore/platform/chromium/support/WrappedResourceResponse.h.
    (WebKit):
    (WrappedResourceResponse):
    (WebKit::WrappedResourceResponse::~WrappedResourceResponse):
    (WebKit::WrappedResourceResponse::WrappedResourceResponse):
    (WebKit::WrappedResourceResponse::bind):
    (Handle):
    (WebKit::WrappedResourceResponse::Handle::dispose):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WrappedResourceRequest.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WrappedResourceRequest.h
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WrappedResourceRequest_h
 #define WrappedResourceRequest_h
 
+#include "platform/WebURLRequest.h"
 #include "WebURLRequestPrivate.h"
-#include <public/WebURLRequest.h>
 
 namespace WebKit {
 
