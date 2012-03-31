fb2fa7ee24afff52103f5bd9c27ccf38275608c8
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-31 11:00:06 +0000

    Unreviewed, avoid the need for pixel results (reported for Chromium) for tests added in r112769.
    * svg/css/script-tests/svg-attribute-length-parsing.js:
    * svg/css/script-tests/svg-attribute-parser-mode.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/svg/css/script-tests/svg-attribute-length-parsing.js
+++ third_party/WebKit/LayoutTests/svg/css/script-tests/svg-attribute-length-parsing.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
 description("Test CSS <length> parsing on SVG presentation attributes.")
 createSVGTestCase();
 
