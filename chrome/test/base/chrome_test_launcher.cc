c6709143ef35708da6488f9b930da89c0843f63a
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-31 00:48:21 +0000

    Fix handling of failure to delete the temp dir in r129992
    BUG=120965
    TEST=browser_tests still works
    
    Review URL: https://chromiumcodereview.appspot.com/9959045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_test_launcher.cc
+++ chrome/test/base/chrome_test_launcher.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/test/test_launcher.h"
 
 #include "base/command_line.h"
+#include "base/file_util.h"
 #include "base/logging.h"
 #include "base/scoped_temp_dir.h"
 #include "chrome/app/chrome_main_delegate.h"
@@ -81,8 +82,12 @@ class ChromeTestLauncherDelegate : public test_launcher::TestLauncherDelegate {
     }
 
     // Clean up previous temp dir.
-    if (!temp_dir_.path().empty() && !temp_dir_.Delete())
+    // We Take() the directory and delete it ourselves so that the next
+    // CreateUniqueTempDir will succeed even if deleting the directory fails.
+    if (!temp_dir_.path().empty() &&
+        !file_util::Delete(temp_dir_.Take(), true)) {
       LOG(ERROR) << "Error deleting previous temp profile directory";
+    }
 
     // Create a new user data dir and pass it to the child.
     if (!temp_dir_.CreateUniqueTempDir() || !temp_dir_.IsValid()) {
