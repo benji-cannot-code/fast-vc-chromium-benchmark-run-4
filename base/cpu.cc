b2aa4123e15c028b4b621d44cce48e7a854bdde4
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-31 07:08:53 +0000

    Fix base::CPU detection.
    ARCH_CPU_X86_FAMILY was never being defined, causing base::CPU::Initialize to be a no-op.
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9836125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130046 0039d316-1c4b-4281-b951-d872f2087c98

--- base/cpu.cc
+++ base/cpu.cc
@@ -1,17 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/cpu.h"
+
+#include <string.h>
+
+#include "build/build_config.h"
 
 #if defined(ARCH_CPU_X86_FAMILY)
 #if defined(_MSC_VER)
 #include <intrin.h>
 #endif
 #endif
-
-#include <string.h>
 
 namespace base {
 
--- base/time_win_unittest.cc
+++ base/time_win_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -209,6 +209,10 @@ TEST(TimeTicks, TimerPerformance) {
 }
 
 TEST(TimeTicks, Drift) {
+  // If QPC is disabled, this isn't measuring anything.
+  if (!TimeTicks::IsHighResClockWorking())
+    return;
+
   const int kIterations = 100;
   int64 total_drift = 0;
 
@@ -228,8 +232,7 @@ TEST(TimeTicks, Drift) {
   }
 
   // Sanity check. We expect some time drift to occur, especially across
-  // the number of iterations we do. However, if the QPC is disabled, this
-  // is not measuring anything (drift is zero in that case).
+  // the number of iterations we do.
   EXPECT_LT(0, total_drift);
 
   printf("average time drift in microseconds: %lld\n",
