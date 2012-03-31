c34b5dc43de6c753ac053ddfc26eaefa82bb0977
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-31 03:19:02 +0000

    Make ShareGroup thread safe
    In another CL I'll refactor the id handlers AND
    make the places sharing resources correctly set things
    up.
    
    TEST=none
    BUG=120297
    
    
    Review URL: http://codereview.chromium.org/9918027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130034 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/program_info_manager.cc
+++ gpu/command_buffer/client/program_info_manager.cc
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "../client/program_info_manager.h"
+#include "../client/atomicops.h"
 #include "../client/gles2_implementation.h"
 
 #include <map>
@@ -194,6 +195,8 @@ class CachedProgramInfoManager : public ProgramInfoManager {
   typedef std::map<GLuint, ProgramInfo> ProgramInfoMap;
 
   ProgramInfoMap program_infos_;
+
+  mutable Lock lock_;
 };
 
 CachedProgramInfoManager::ProgramInfo::UniformInfo::UniformInfo(
@@ -373,6 +376,7 @@ CachedProgramInfoManager::ProgramInfo*
 }
 
 void CachedProgramInfoManager::CreateInfo(GLuint program) {
+  AutoLock auto_lock(lock_);
   DeleteInfo(program);
   std::pair<ProgramInfoMap::iterator, bool> result =
       program_infos_.insert(std::make_pair(program, ProgramInfo()));
@@ -386,6 +390,7 @@ void CachedProgramInfoManager::DeleteInfo(GLuint program) {
 
 bool CachedProgramInfoManager::GetProgramiv(
     GLES2Implementation* gl, GLuint program, GLenum pname, GLint* params) {
+  AutoLock auto_lock(lock_);
   ProgramInfo* info = GetProgramInfo(gl, program);
   if (!info) {
     return false;
@@ -395,6 +400,7 @@ bool CachedProgramInfoManager::GetProgramiv(
 
 GLint CachedProgramInfoManager::GetAttribLocation(
     GLES2Implementation* gl, GLuint program, const char* name) {
+  AutoLock auto_lock(lock_);
   ProgramInfo* info = GetProgramInfo(gl, program);
   if (info) {
     return info->GetAttribLocation(name);
@@ -404,6 +410,7 @@ GLint CachedProgramInfoManager::GetAttribLocation(
 
 GLint CachedProgramInfoManager::GetUniformLocation(
     GLES2Implementation* gl, GLuint program, const char* name) {
+  AutoLock auto_lock(lock_);
   ProgramInfo* info = GetProgramInfo(gl, program);
   if (info) {
     return info->GetUniformLocation(name);
@@ -415,6 +422,7 @@ bool CachedProgramInfoManager::GetActiveAttrib(
     GLES2Implementation* gl,
     GLuint program, GLuint index, GLsizei bufsize, GLsizei* length,
     GLint* size, GLenum* type, char* name) {
+  AutoLock auto_lock(lock_);
   ProgramInfo* info = GetProgramInfo(gl, program);
   if (info) {
     const ProgramInfo::VertexAttribInfo* attrib_info =
@@ -449,6 +457,7 @@ bool CachedProgramInfoManager::GetActiveUniform(
     GLES2Implementation* gl,
     GLuint program, GLuint index, GLsizei bufsize, GLsizei* length,
     GLint* size, GLenum* type, char* name) {
+  AutoLock auto_lock(lock_);
   ProgramInfo* info = GetProgramInfo(gl, program);
   if (info) {
     const ProgramInfo::UniformInfo* uniform_info = info->GetUniformInfo(index);
