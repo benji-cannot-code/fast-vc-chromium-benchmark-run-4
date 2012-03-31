72d7719aba286e9a6a82c61d70eabcf100dd828a
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-31 00:52:52 +0000

    Fix return type for virtual borderBoundingBox method https://bugs.webkit.org/show_bug.cgi?id=82561
    Reviewed by Eric Seidel.
    
    No new tests, no change in functionality.
    
    * editing/DeleteButtonController.cpp:
    (WebCore::isDeletableElement):
    * rendering/RenderBox.h:
    (WebCore::RenderBox::borderBoxRect):
    Rename pixelSnappedBorderBoxRect to borderBoxRect and remove LayoutRect
    version of same as we always want to use the pixel snapped version to
    ensure proper rounding and alignment to device pixels.
    (The way this rect is pixel snapped, using the m_frameRect location,
    makes it hard for calling code to take the subpixel rect and correctly
    snap it).
    
    (WebCore::RenderBox::borderBoundingBox):
    * rendering/RenderBoxModelObject.h:
    Change pure virtual definition of borderBoundingBox to return an IntRect
    to match implementation in RenderBox.
    
    (RenderBoxModelObject):
    * rendering/RenderInline.h:
    (WebCore::RenderInline::borderBoundingBox):
    Change overloaded method to IntRect to match RenderBox implementation.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/DeleteButtonController.cpp
+++ third_party/WebKit/Source/WebCore/editing/DeleteButtonController.cpp
@@ -93,7 +93,7 @@ static bool isDeletableElement(const Node* node)
         return false;
 
     RenderBox* box = toRenderBox(renderer);
-    LayoutRect borderBoundingBox = box->borderBoundingBox();
+    IntRect borderBoundingBox = box->borderBoundingBox();
     if (borderBoundingBox.width() < minimumWidth || borderBoundingBox.height() < minimumHeight)
         return false;
 
