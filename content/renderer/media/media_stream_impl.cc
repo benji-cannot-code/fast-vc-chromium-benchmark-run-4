168f4f3f3b70c76448c10a3486b40e5b13522c8e
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-31 09:37:00 +0000

    Fix Win/Linux builds after WebKit r112784.
    TBR=abarth@webkit.org
    
    Review URL: https://chromiumcodereview.appspot.com/9969041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130051 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_impl.cc
+++ content/renderer/media/media_stream_impl.cc
@@ -23,8 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/p2p/socket_dispatcher.h"
 #include "jingle/glue/thread_wrapper.h"
 #include "media/base/message_loop_factory.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamDescriptor.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaStreamRegistry.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityOrigin.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamDescriptor.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamSource.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebVector.h"
 
