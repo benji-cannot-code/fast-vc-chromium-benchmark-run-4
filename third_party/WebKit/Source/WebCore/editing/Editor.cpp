c34b38a9cd1f060bf567374b5b474f1c9ca74d46
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-03-31 00:54:50 +0000

    https://bugs.webkit.org/show_bug.cgi?id=82761
    Try to narrow down the cause of this assertion by adding
    an assertion about m_frame.
    
    * editing/Editor.cpp:
    (WebCore::Editor::markAllMisspellingsAndBadGrammarInRanges):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.cpp
+++ third_party/WebKit/Source/WebCore/editing/Editor.cpp
@@ -1909,6 +1909,7 @@ void Editor::markBadGrammar(const VisibleSelection& selection)
 
 void Editor::markAllMisspellingsAndBadGrammarInRanges(TextCheckingTypeMask textCheckingOptions, Range* spellingRange, Range* grammarRange)
 {
+    ASSERT(m_frame);
     ASSERT(unifiedTextCheckerEnabled());
 
     // There shouldn't be pending autocorrection at this moment.
