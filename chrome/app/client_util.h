14e0551871d6db5658f74e6a299719bcce353664
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 20:24:48 +0000

    Two small changes so I can reuse the MainDllLoader
    For loading the metro_driver, now that is going to be in the
    version directory.
    
    BUG=118641
    TEST=chrome starts.
    Review URL: https://chromiumcodereview.appspot.com/9956031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130196 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/client_util.h
+++ chrome/app/client_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,6 +17,9 @@ namespace sandbox {
   struct SandboxInterfaceInfo;
 }
 
+// Gets the path of the current exe with a trailing backslash.
+std::wstring GetExecutablePath();
+
 // Implements the common aspects of loading chrome.dll for both chrome and
 // chromium scenarios, which are in charge of implementing two abstract
 // methods: GetRegistryPath() and OnBeforeLaunch().
@@ -32,13 +35,12 @@ class MainDllLoader {
   // upon termination.
   int Launch(HINSTANCE instance, sandbox::SandboxInterfaceInfo* sbox_info);
 
+  // Look into the registry to find the latest version.
+  std::wstring GetVersion();
+
   // Launches a new instance of the browser if the current instance in
   // persistent mode an upgrade is detected.
   void RelaunchChromeBrowserWithNewCommandLineIfNeeded();
-
-  // Derived classes must return the relative registry path that holds the
-  // most current version of chrome.dll.
-  virtual std::wstring GetRegistryPath() = 0;
 
   // Called after chrome.dll has been loaded but before the entry point
   // is invoked. Derived classes can implement custom actions here.
@@ -53,6 +55,10 @@ class MainDllLoader {
   }
 
  protected:
+  // Derived classes must return the relative registry path that holds the
+  // most current version of chrome.dll.
+  virtual std::wstring GetRegistryPath() = 0;
+
   HMODULE Load(std::wstring* out_version, std::wstring* out_file);
 
  private:
