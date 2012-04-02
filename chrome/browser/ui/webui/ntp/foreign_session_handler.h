4cb693040a7b3fb15efd8b86959813cdeb719b98
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 14:59:39 +0000

    Allow hiding of sessions from Other Devices via context menu.
    Also add modification times to session headers, and a separator between
    multiple windows in the same session.
    
    BUG=120200
    TEST=Manual.
    
    Review URL: https://chromiumcodereview.appspot.com/9959030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130125 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/foreign_session_handler.h
+++ chrome/browser/ui/webui/ntp/foreign_session_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/time.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sync/glue/session_model_associator.h"
 #include "content/public/browser/notification_observer.h"
@@ -41,6 +42,9 @@ class ForeignSessionHandler : public content::WebUIMessageHandler,
   // Returns true if tab sync is enabled for this profile, otherwise false.
   bool IsTabSyncEnabled();
 
+  // Returns a string used to show the user when a session was last modified.
+  string16 FormatSessionTime(const base::Time& time);
+
   // Determines which session is to be opened, and then calls
   // OpenForeignSession, to begin the process of opening a new browser window.
   // This is a javascript callback handler.
@@ -50,6 +54,12 @@ class ForeignSessionHandler : public content::WebUIMessageHandler,
   // This is a javascript callback handler, and it is also called when the sync
   // model has changed and the new tab page needs to reflect the changes.
   void HandleGetForeignSessions(const ListValue* args);
+
+  // Delete a foreign session. This will remove it from the list of foreign
+  // sessions on all devices. It will reappear if the session is re-activated
+  // on the original device.
+  // This is a javascript callback handler.
+  void HandleDeleteForeignSession(const ListValue* args);
 
   // Helper methods to create JSON compatible objects from Session objects.
   bool SessionTabToValue(const SessionTab& tab, DictionaryValue* dictionary);
