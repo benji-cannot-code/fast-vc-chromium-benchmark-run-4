5910b76ca3b3d7777edc298eef40dff18ee38a9f
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 22:36:02 +0000

    Fix typo in policy_service_stub.cc
    BUG=117407
    
    Review URL: http://codereview.chromium.org/9963067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130234 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_service_stub.cc
+++ chrome/browser/policy/policy_service_stub.cc
@@ -25,7 +25,7 @@ const PolicyMap* PolicyServiceStub::GetPolicies(
   return NULL;
 };
 
-bool PolicyServiceStub:IsInitializationComplete() const {
+bool PolicyServiceStub::IsInitializationComplete() const {
   return true;
 }
 
