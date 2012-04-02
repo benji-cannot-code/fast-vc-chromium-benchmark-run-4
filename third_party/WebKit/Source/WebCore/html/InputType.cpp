1e9427d5ffbeeb469498104c8631646ba2fcac24
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-02 05:07:53 +0000

    Fix some problems of text field decoration https://bugs.webkit.org/show_bug.cgi?id=82693
    Reviewed by Dimitri Glazkov.
    
    - Fix a problem that decorations are not removed when the input type is
    changed.
    - Add a comment to the 'willDetach' callback.
    
    No new tests because the behavior changes are not visible yet.
    
    * GNUMakefile.list.am: Add HTMLShadowElement.*.
    * html/InputType.cpp:
    (WebCore::InputType::destroyShadowSubtree):
    Remove all of ShadowRoot contents, and add a <shadow> element to each of
    them. We don't remove ShadowRoots from the tree because it's not
    supported well.
    * html/shadow/TextFieldDecorationElement.cpp:
    (getDecorationRootAndDecoratedRoot): A helper function for decorate().
    If the input element has a ShadowRoot with single <shadow> child, we
    don't create new ShadowRoot and reuse it.
    (WebCore::TextFieldDecorationElement::decorate):
    Use getDecorationRootAndDecoratedRoot().
    * html/shadow/TextFieldDecorationElement.h:
    (TextFieldDecorator): Add a comment to willDetach().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/InputType.cpp
+++ third_party/WebKit/Source/WebCore/html/InputType.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FormDataList.h"
 #include "HTMLFormElement.h"
 #include "HTMLInputElement.h"
+#include "HTMLShadowElement.h"
 #include "HiddenInputType.h"
 #include "ImageInputType.h"
 #include "KeyboardEvent.h"
@@ -71,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+using namespace HTMLNames;
 using namespace std;
 
 typedef PassOwnPtr<InputType> (*InputTypeFactoryFunction)(HTMLInputElement*);
@@ -383,8 +385,17 @@ void InputType::destroyShadowSubtree()
     if (!element()->hasShadowRoot())
         return;
 
-    if (ShadowRoot* root = element()->shadowTree()->oldestShadowRoot())
+    ShadowRoot* root = element()->shadowTree()->oldestShadowRoot();
+    ASSERT(root);
+    root->removeAllChildren();
+
+    // It's ok to clear contents of all other ShadowRoots because they must have
+    // been created by TextFieldDecorationElement, and we don't allow adding
+    // AuthorShadowRoot to HTMLInputElement.
+    while ((root = root->youngerShadowRoot())) {
         root->removeAllChildren();
+        root->appendChild(HTMLShadowElement::create(shadowTag, element()->document()));
+    }
 }
 
 double InputType::parseToDouble(const String&, double defaultValue) const
