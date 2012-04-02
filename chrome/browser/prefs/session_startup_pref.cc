709f0150ccc23383bf1371becad1c3856be953f2
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 20:23:23 +0000

    [protector] Move check whether pref is managed to ProtectedPrefsWatcher.
    BUG=None
    TEST=None (no observable changes)
    
    
    Review URL: http://codereview.chromium.org/9963052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/session_startup_pref.cc
+++ chrome/browser/prefs/session_startup_pref.cc
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/window_restore_utils.h"
 #endif
 
+using protector::ProtectedPrefsWatcher;
+using protector::ProtectorServiceFactory;
+
 namespace {
 
 // For historical reasons the enum and value registered in the prefs don't line
@@ -178,24 +181,18 @@ SessionStartupPref::Type SessionStartupPref::PrefValueToType(int pref_value) {
 
 // static
 bool SessionStartupPref::DidStartupPrefChange(Profile* profile) {
-  PrefService* prefs = profile->GetPrefs();
-  protector::ProtectedPrefsWatcher* prefs_watcher =
-      protector::ProtectorServiceFactory::GetForProfile(profile)->
-          GetPrefsWatcher();
-  if (!TypeIsManaged(prefs) &&
+  ProtectedPrefsWatcher* prefs_watcher =
+      ProtectorServiceFactory::GetForProfile(profile)->GetPrefsWatcher();
+  if (prefs_watcher->DidPrefChange(prefs::kRestoreOnStartup))
+    return true;
 #if defined(OS_MACOSX)
-      // On Mac OS, default value for |kRestoreOnStartup| depends on system
-      // settings and may be different from one run to another.
-      !prefs->FindPreference(prefs::kRestoreOnStartup)->IsDefaultValue() &&
+  // On Mac OS, default value for |kRestoreOnStartup| depends on system
+  // settings and may be different from one run to another.
+  PrefService* prefs = profile->GetPrefs();
+  if (prefs->FindPreference(prefs::kRestoreOnStartup)->IsDefaultValue())
+    return false;
 #endif
-      prefs_watcher->DidPrefChange(prefs::kRestoreOnStartup)) {
-    return true;
-  }
-  if (!URLsAreManaged(prefs) &&
-      prefs_watcher->DidPrefChange(prefs::kURLsToRestoreOnStartup)) {
-    return true;
-  }
-  return false;
+  return prefs_watcher->DidPrefChange(prefs::kRestoreOnStartup);
 }
 
 // static
