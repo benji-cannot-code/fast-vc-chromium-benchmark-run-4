d8a8f5bab963f13d8160839ae1f2b7743d236221
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 23:36:27 +0000

    Using FromDatabaseTime/ToDatabaseTime.
    Using FromDatabase/ToDatabaseTime instead of MillisecondsToTime/ToMillseconds to
    make code more clear.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9969060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/android/android_time.h
+++ chrome/browser/history/android/android_time.h
@@ -10,14 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace history {
 
-// Android use the millisecond as time unit, the below 2 methods are used
-// convert between millisecond and base::Time.
-inline base::Time MillisecondsToTime(int64 milliseconds) {
+// Android's system time is the milliseconds since January 1, 1970 00:00:00 UTC,
+// the below 2 methods are used convert between base::Time and the milliseconds
+// stored in database.
+inline base::Time FromDatabaseTime(int64 milliseconds) {
   return base::TimeDelta::FromMilliseconds(milliseconds) +
       base::Time::UnixEpoch();
 }
 
-inline int64 ToMilliseconds(const base::Time& time) {
+inline int64 ToDatabaseTime(const base::Time& time) {
   return (time - base::Time::UnixEpoch()).InMilliseconds();
 }
 
