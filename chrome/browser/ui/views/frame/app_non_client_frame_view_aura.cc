fce125251a387de6e48b6fe845a5238536ef477e
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 17:29:14 +0000

    Register as observer to catch widget closing On occasion we were crashing in shutdown because a widget was being freed before the frame.
    BUG=120771
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9968043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/app_non_client_frame_view_aura.cc
+++ chrome/browser/ui/views/frame/app_non_client_frame_view_aura.cc
@@ -182,7 +182,6 @@ AppNonClientFrameViewAura::AppNonClientFrameViewAura(
 AppNonClientFrameViewAura::~AppNonClientFrameViewAura() {
   if (control_widget_)
     control_widget_->Close();
-  control_widget_ = NULL;
   mouse_watcher_.Stop();
 }
 
@@ -232,6 +231,7 @@ void AppNonClientFrameViewAura::OnMouseEntered(
   if (!control_widget_) {
     control_widget_ = new views::Widget;
     views::Widget::InitParams params(views::Widget::InitParams::TYPE_CONTROL);
+    control_widget_->AddObserver(this);
     params.parent = browser_view()->GetNativeHandle();
     params.transparent = true;
     control_widget_->Init(params);
@@ -265,6 +265,11 @@ void AppNonClientFrameViewAura::MouseMovedOutOfHost() {
   layer->SetOpacity(0);
 }
 
+void AppNonClientFrameViewAura::OnWidgetClosing(views::Widget* widget) {
+  DCHECK_EQ(control_widget_, widget);
+  control_widget_ = NULL;
+}
+
 gfx::Rect AppNonClientFrameViewAura::GetControlBounds() const {
   gfx::Size preferred = control_view_->GetPreferredSize();
   gfx::Point location(width() - preferred.width(), 0);
@@ -277,7 +282,6 @@ gfx::Rect AppNonClientFrameViewAura::GetControlBounds() const {
 void AppNonClientFrameViewAura::Close() {
   if (control_widget_)
     control_widget_->Close();
-  control_widget_ = NULL;
   mouse_watcher_.Stop();
   frame()->Close();
 }
@@ -285,7 +289,6 @@ void AppNonClientFrameViewAura::Close() {
 void AppNonClientFrameViewAura::Restore() {
   if (control_widget_)
     control_widget_->Close();
-  control_widget_ = NULL;
   mouse_watcher_.Stop();
   frame()->Restore();
 }
--- chrome/browser/ui/views/frame/app_non_client_frame_view_aura.h
+++ chrome/browser/ui/views/frame/app_non_client_frame_view_aura.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/animation/animation_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/mouse_watcher.h"
+#include "ui/views/widget/widget.h"
 
 namespace aura {
 class Window;
@@ -22,7 +23,8 @@ class SlideAnimation;
 
 // NonClientFrameViewAura implementation for apps.
 class AppNonClientFrameViewAura : public BrowserNonClientFrameView,
-                                  public views::MouseWatcherListener {
+                                  public views::MouseWatcherListener,
+                                  public views::Widget::Observer {
  public:
   AppNonClientFrameViewAura(
       BrowserFrame* frame, BrowserView* browser_view);
@@ -51,6 +53,9 @@ class AppNonClientFrameViewAura : public BrowserNonClientFrameView,
 
   // views::MouseWatcherListener.
   virtual void MouseMovedOutOfHost() OVERRIDE;
+
+  // views::Widget::Observer.
+  virtual void OnWidgetClosing(views::Widget* widget) OVERRIDE;
 
   // Close the app window.
   void Close();
