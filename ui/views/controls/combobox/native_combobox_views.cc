c0b4d5a22e34fc11de8c44469beb60ef7c3f02ca
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 18:30:53 +0000

    views: Add coverage of other keys on the keyboard to the combobox unittest.
    TEST=views_unittests --gtest_filter=NativeComboboxViewsTest.KeyTest
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9968027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130168 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/combobox/native_combobox_views.cc
+++ ui/views/controls/combobox/native_combobox_views.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/command_line.h"
-#include "base/utf_string_conversions.h"
 #include "grit/ui_resources.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/base/models/combobox_model.h"
@@ -90,33 +88,33 @@ bool NativeComboboxViews::OnMouseDragged(const views::MouseEvent& mouse_event) {
 
 bool NativeComboboxViews::OnKeyPressed(const views::KeyEvent& key_event) {
   // TODO(oshima): handle IME.
-  DCHECK(key_event.type() == ui::ET_KEY_PRESSED);
+  DCHECK_EQ(key_event.type(), ui::ET_KEY_PRESSED);
 
   // Check if we are in the default state (-1) and set to first item.
-  if(selected_index_ == -1)
+  if (selected_index_ == -1)
     selected_index_ = 0;
 
   int new_index = selected_index_;
   switch (key_event.key_code()) {
-    // Move to the next element if any.
+    // Move to the next item if any.
     case ui::VKEY_DOWN:
       if (new_index < (combobox_->model()->GetItemCount() - 1))
         new_index++;
       break;
 
-    // Move to the end of the list,
+    // Move to the end of the list.
     case ui::VKEY_END:
     case ui::VKEY_NEXT:
       new_index = combobox_->model()->GetItemCount() - 1;
       break;
 
-    // Move to the top of the list.
+    // Move to the beginning of the list.
    case ui::VKEY_HOME:
    case ui::VKEY_PRIOR:
       new_index = 0;
       break;
 
-    // move to the previous element if any
+    // Move to the previous item if any.
     case ui::VKEY_UP:
       if (new_index > 0)
         new_index--;
