0126a1f848371a8ea2818768e298adb9dd601cd3
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-02 20:34:59 +0000

    WebKit should throttle memory pressure notifications in proportion to handler time https://bugs.webkit.org/show_bug.cgi?id=82674
    Rubber-stamped by Darin Adler.
    
    Updated r112910: <http://trac.webkit.org/changeset/112910> to address
    post checkin concerns raised in original bug.
    
    No additional tests. This passes existing test and was verified using
    manual tests on a small memory system with many websites open.
    
    * platform/mac/MemoryPressureHandlerMac.mm:
    (WebCore):
    (WebCore::MemoryPressureHandler::respondToMemoryPressure):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112930 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/MemoryPressureHandlerMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/MemoryPressureHandlerMac.mm
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <notify.h>
 #endif
 
+using std::max;
+
 namespace WebCore {
 
 #if !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
@@ -54,8 +56,8 @@ static int _notifyToken;
 // this is 1 / s_holdOffMultiplier percent of the time.
 // These value seems reasonable and testing verifies that it throttles frequent
 // low memory events, greatly reducing CPU usage.
-static const time_t s_minimumHoldOffTime = 5;
-static const time_t s_holdOffMultiplier = 20;
+static const unsigned s_minimumHoldOffTime = 5;
+static const unsigned s_holdOffMultiplier = 20;
 
 void MemoryPressureHandler::install()
 {
@@ -117,23 +119,15 @@ void MemoryPressureHandler::holdOff(unsigned seconds)
 
 void MemoryPressureHandler::respondToMemoryPressure()
 {
-    double startTime, endTime;
-    unsigned holdOffTime;
-
     uninstall();
 
-    startTime = monotonicallyIncreasingTime();
+    double startTime = monotonicallyIncreasingTime();
 
     releaseMemory(false);
 
-    endTime = monotonicallyIncreasingTime();
+    unsigned holdOffTime = (monotonicallyIncreasingTime() - startTime) * s_holdOffMultiplier;
 
-    holdOffTime = (unsigned)((endTime - startTime) * (double)s_holdOffMultiplier);
-
-    if (holdOffTime < s_minimumHoldOffTime)
-        holdOffTime = s_minimumHoldOffTime;
-
-    holdOff(holdOffTime);
+    holdOff(max(holdOffTime, s_minimumHoldOffTime));
 }
 #endif // !PLATFORM(IOS)
 
