409317e5b9023b096120a1d7e1891684d7eaa9a3
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-02 18:23:19 +0000

    <rdar://problem/11020155> and https://bugs.webkit.org/show_bug.cgi?id=82910 REGRESSION (98963 and 109091):  Crash when creating a WebArchive of a page with @page style rules
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Test: webarchive/css-page-rule-crash.html
    
    * css/CSSStyleSheet.cpp:
    (WebCore::CSSStyleSheet::addSubresourceStyleURLs): Page rules do not (currently) have subresource urls to get,
      and are not CSSStyleRules.
    
    LayoutTests:
    
    * webarchive/css-page-rule-crash-expected.webarchive: Added.
    * webarchive/css-page-rule-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSStyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSStyleSheet.cpp
@@ -462,7 +462,7 @@ void CSSStyleSheet::addSubresourceStyleURLs(ListHashSet<KURL>& urls)
             CSSRule* rule = styleSheet->m_childRules[i].get();
             if (rule->isFontFaceRule())
                 static_cast<CSSFontFaceRule*>(rule)->addSubresourceStyleURLs(urls);
-            else if (rule->isStyleRule() || rule->isPageRule())
+            else if (rule->isStyleRule())
                 static_cast<CSSStyleRule*>(rule)->styleRule()->addSubresourceStyleURLs(urls, this);
         }
     }
