05cd4181d1be10560f3b36152bf337f75b5c4177
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-02 23:54:58 +0000

    check-webkit-style errors when removing .png files https://bugs.webkit.org/show_bug.cgi?id=82933
    Reviewed by David Levin.
    
    * Scripts/webkitpy/style/patchreader.py:
    (PatchReader.check): Make sure the file exists and pass in a FileSystem() object (for mocking).
    * Scripts/webkitpy/style/patchreader_unittest.py:
    (test_check_patch_with_png_deletion):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/style/patchreader_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/patchreader_unittest.py
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+from webkitpy.common.system.filesystem_mock import MockFileSystem
 from webkitpy.style.patchreader import PatchReader
 
 
@@ -90,4 +91,14 @@ index ef65bee..e3db70e 100644
 -foobar
 """)
         # _mock_check_file should not be called for the deletion patch.
+        self._assert_checked([], 1)
+
+    def test_check_patch_with_png_deletion(self):
+        fs = MockFileSystem()
+        diff_text = """Index: LayoutTests/platform/mac/foo-expected.png
+===================================================================
+Cannot display: file marked as a binary type.
+svn:mime-type = image/png
+"""
+        self._patch_checker.check(diff_text, fs)
         self._assert_checked([], 1)
