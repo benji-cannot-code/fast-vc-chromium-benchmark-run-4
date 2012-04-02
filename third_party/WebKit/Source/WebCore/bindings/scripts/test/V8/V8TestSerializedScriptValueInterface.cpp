ce9fe99041fa4298cfcc6c051c3767a0c36db6e1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-02 17:56:58 +0000

    Unreviewed, rolling out r112318. http://trac.webkit.org/changeset/112318 https://bugs.webkit.org/show_bug.cgi?id=82912
    Possible OOM issues (Requested by aklein on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-04-02
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateConstructorCallback):
    (GenerateNamedConstructorCallback):
    (GenerateToV8Converters):
    (GetDomMapFunction):
    * bindings/scripts/test/V8/V8Float64Array.cpp:
    (WebCore::V8Float64Array::wrapSlow):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore::V8TestActiveDOMObject::wrapSlow):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.cpp:
    (WebCore::V8TestCustomNamedGetter::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore::V8TestEventConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventTarget.cpp:
    (WebCore::V8TestEventTarget::wrapSlow):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore::V8TestInterface::wrapSlow):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp:
    (WebCore::V8TestMediaQueryListListener::wrapSlow):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore::V8TestNamedConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::V8TestObj::wrapSlow):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::V8TestSerializedScriptValueInterface::wrapSlow):
    * bindings/v8/V8DOMWrapper.cpp:
    (WebCore::V8DOMWrapper::setJSWrapperForDOMNode):
    * bindings/v8/V8DOMWrapper.h:
    (V8DOMWrapper):
    (WebCore::V8DOMWrapper::setJSWrapperForDOMObject):
    (WebCore::V8DOMWrapper::setJSWrapperForActiveDOMObject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@112906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp
@@ -289,7 +289,7 @@ v8::Handle<v8::Object> V8TestSerializedScriptValueInterface::wrapSlow(PassRefPtr
 
     if (!hasDependentLifetime)
         wrapperHandle.MarkIndependent();
-    V8DOMWrapper::setJSWrapperForDOMObject(impl, wrapperHandle);
+    getDOMObjectMap().set(impl.leakRef(), wrapperHandle);
     return wrapper;
 }
 
--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventTargetHeaders.h"
 #include "EventTargetInterfaces.h"
 #include "FrameLoaderClient.h"
-#include "SVGElementInstance.h"
 #include "StylePropertySet.h"
 #include "V8AbstractEventListener.h"
 #include "V8Binding.h"
@@ -68,13 +67,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if ENABLE(SVG)
-void V8DOMWrapper::setJSWrapperForDOMSVGElementInstance(PassRefPtr<SVGElementInstance> element, v8::Persistent<v8::Object> wrapper)
+void V8DOMWrapper::setJSWrapperForDOMNode(PassRefPtr<Node> node, v8::Persistent<v8::Object> wrapper)
 {
     ASSERT(maybeDOMWrapper(wrapper));
-    getDOMSVGElementInstanceMap().set(element.leakRef(), wrapper);
+    if (node->isActiveNode())
+        getActiveDOMNodeMap().set(node.leakRef(), wrapper);
+    else
+        getDOMNodeMap().set(node.leakRef(), wrapper);
 }
-#endif
 
 v8::Local<v8::Function> V8DOMWrapper::getConstructor(WrapperTypeInfo* type, v8::Handle<v8::Value> objectPrototype)
 {
