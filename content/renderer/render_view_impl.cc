61b4a65dd5dcaed0eff2a430ea1f3c10defcabda
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 17:00:50 +0000

    Clear the selection text when performing empty selections.
    There is a variable in RenderViewImpl named selection_text_ that according to the description holds the text selection corresponding to the last time that didChangeSelection was called. However, it is not cleaned up when this method reports an empty selection. This can prevent detecting some selection changes and sending their update messages to the browser side.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9963105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130394 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1709,6 +1709,9 @@ void RenderViewImpl::didChangeSelection(bool is_empty_selection) {
       return;
   handling_select_range_ = false;
 
+  if (is_empty_selection)
+    selection_text_.clear();
+
   SyncSelectionIfRequired();
 }
 
