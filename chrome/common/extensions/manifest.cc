779fc8866fb4704bf109a88b7d9347cf207e6125
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 21:33:08 +0000

    Revert 130462 - Implement FeatureProvider for ExtensionAPI.
    BUG=120069
    
    Review URL: https://chromiumcodereview.appspot.com/9950046
    
    TBR=aa@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9950119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/manifest.cc
+++ chrome/common/extensions/manifest.cc
@@ -39,7 +39,7 @@ bool Manifest::ValidateManifest(string16* error) const {
       continue;
     }
 
-    Feature::Availability result = feature->IsAvailableToManifest(
+    Feature::Availability result = feature->IsAvailable(
         extension_id_, GetType(), Feature::ConvertLocation(location_),
         GetManifestVersion());
     if (result != Feature::IS_AVAILABLE) {
@@ -156,7 +156,7 @@ bool Manifest::CanAccessKey(const std::string& key) const {
   if (!feature.get())
     return false;
 
-  return Feature::IS_AVAILABLE == feature->IsAvailableToManifest(
+  return Feature::IS_AVAILABLE == feature->IsAvailable(
       extension_id_, GetType(), Feature::ConvertLocation(location_),
       GetManifestVersion());
 }
