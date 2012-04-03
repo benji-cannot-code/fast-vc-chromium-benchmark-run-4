d189d23fb4d424cb6cbe349e540fa2c27ed8e122
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-03 09:24:39 +0000

    Unreviewed, rolling out r112987 and r112993. http://trac.webkit.org/changeset/112987 http://trac.webkit.org/changeset/112993 https://bugs.webkit.org/show_bug.cgi?id=83010
    hits ASSERTs in Debug builds (Requested by philn-tp on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-04-03
    
    Source/WebCore:
    
    * dom/Document.cpp:
    (WebCore::Document::Document):
    (WebCore::Document::initSecurityContext):
    * dom/Document.h:
    (Document):
    * html/HTMLAttributeNames.in:
    * html/HTMLFrameElementBase.cpp:
    (WebCore::HTMLFrameElementBase::parseAttribute):
    (WebCore::HTMLFrameElementBase::location):
    * html/HTMLIFrameElement.idl:
    * html/parser/HTMLTreeBuilder.cpp:
    (WebCore::HTMLTreeBuilder::defaultForInitial):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::urlSelected):
    (WebCore::FrameLoader::submitForm):
    (WebCore::FrameLoader::outgoingReferrer):
    (WebCore::FrameLoader::loadFrameRequest):
    (WebCore::FrameLoader::load):
    (WebCore::FrameLoader::loadWithNavigationAction):
    (WebCore::FrameLoader::reloadWithOverrideEncoding):
    (WebCore::FrameLoader::reload):
    (WebCore::FrameLoader::loadResourceSynchronously):
    * loader/FrameLoader.h:
    (FrameLoader):
    
    LayoutTests:
    
    * fast/frames/srcdoc/reloading-a-srcdoc-document-loads-it-again-expected.txt: Removed.
    * fast/frames/srcdoc/reloading-a-srcdoc-document-loads-it-again.html: Removed.
    * fast/frames/srcdoc/setting-src-does-nothing-expected.txt: Removed.
    * fast/frames/srcdoc/setting-src-does-nothing.html: Removed.
    * fast/frames/srcdoc/setting-srcdoc-reloads-document-expected.txt: Removed.
    * fast/frames/srcdoc/setting-srcdoc-reloads-document.html: Removed.
    * fast/frames/srcdoc/srcdoc-beats-src-dom-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-beats-src-dom.html: Removed.
    * fast/frames/srcdoc/srcdoc-beats-src-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-beats-src.html: Removed.
    * fast/frames/srcdoc/srcdoc-can-be-in-qurks-mode-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-can-be-in-qurks-mode.html: Removed.
    * fast/frames/srcdoc/srcdoc-can-navigate-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-can-navigate.html: Removed.
    * fast/frames/srcdoc/srcdoc-defaults-to-standards-mode-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-defaults-to-standards-mode.html: Removed.
    * fast/frames/srcdoc/srcdoc-loads-content-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-loads-content.html: Removed.
    * fast/frames/srcdoc/srcdoc-urls-expected.txt: Removed.
    * fast/frames/srcdoc/srcdoc-urls.html: Removed.
    * http/tests/security/srcdoc-can-access-parent-expected.txt: Removed.
    * http/tests/security/srcdoc-can-access-parent.html: Removed.
    * http/tests/security/srcdoc-in-sandbox-cannot-access-parent-expected.txt: Removed.
    * http/tests/security/srcdoc-in-sandbox-cannot-access-parent.html: Removed.
    * http/tests/security/srcdoc-inherits-referrer-expected.txt: Removed.
    * http/tests/security/srcdoc-inherits-referrer-for-forms-expected.txt: Removed.
    * http/tests/security/srcdoc-inherits-referrer-for-forms.html: Removed.
    * http/tests/security/srcdoc-inherits-referrer.html: Removed.
    * platform/chromium/http/tests/security/srcdoc-in-sandbox-cannot-access-parent-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.h
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.h
@@ -169,8 +169,6 @@ public:
     void didChangeTitle(DocumentLoader*);
     void didChangeIcons(IconType);
 
-    bool shouldTreatURLAsSrcdocDocument(const KURL&) const;
-
     FrameLoadType loadType() const;
 
     CachePolicy subresourceCachePolicy() const;
@@ -309,8 +307,6 @@ private:
     void clearProvisionalLoad();
     void transitionToCommitted(PassRefPtr<CachedPage>);
     void frameLoadCompleted();
-
-    SubstituteData defaultSubstituteDataForURL(const KURL&);
 
     static void callContinueLoadAfterNavigationPolicy(void*, const ResourceRequest&, PassRefPtr<FormState>, bool shouldContinue);
     static void callContinueLoadAfterNewWindowPolicy(void*, const ResourceRequest&, PassRefPtr<FormState>, const String& frameName, const NavigationAction&, bool shouldContinue);
