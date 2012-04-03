c3e876f37660bcb05464342ce390ecb8e8b5b968
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-03 20:02:29 +0000

    [BlackBerry] Expose CaseSensitive, Wrap, and HighlightAllMatches in WebPage::findNextString() https://bugs.webkit.org/show_bug.cgi?id=82643
    Source/WebKit/blackberry:
    
    Enhance BlackBerry::WebKit::WebPage::findNextString()
    
    This patch adds support for toggling case sensitivity,
    search wrapping, and whether or not to highlight all matches
    in addition to the next found match.
    
    I refactored and renamed the new setActiveMatchAndMarker() method
    to move the active match from one range to another. This was
    required because in the case of a non wrapped search we do not
    want to adjust the m_activeMatch if another match is not found.
    
    Internal Review by Andy Chen.
    
    Patch by Mike Lattanzio <mlattanzio@rim.com> on 2012-04-03
    Reviewed by Rob Buis.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::findNextString):
    * Api/WebPage.h:
    * WebKitSupport/InPageSearchManager.cpp:
    (BlackBerry::WebKit::InPageSearchManager::InPageSearchManager):
    (BlackBerry::WebKit::InPageSearchManager::findNextString):
    (BlackBerry::WebKit::InPageSearchManager::findAndMarkText):
    (BlackBerry::WebKit::InPageSearchManager::setActiveMatchAndMarker):
    (BlackBerry::WebKit::InPageSearchManager::scopeStringMatches):
    * WebKitSupport/InPageSearchManager.h:
    (InPageSearchManager):
    
    Tools:
    
    Update LayoutTestController to accomodate the new find API.
    It now provides caseSensitive functionality to DRT.
    
    Internal Review by Andy Chen.
    
    Patch by Mike Lattanzio <mlattanzio@rim.com> on 2012-04-03
    Reviewed by Rob Buis.
    
    * DumpRenderTree/blackberry/LayoutTestControllerBlackBerry.cpp:
    (LayoutTestController::findString):
    
    LayoutTests:
    
    Update findString-markers to also test caseSensitive.
    All findString tests used to be case insensitive before.
    
    Internal Review by Andy Chen.
    
    Patch by Mike Lattanzio <mlattanzio@rim.com> on 2012-04-03
    Reviewed by Rob Buis.
    
    * platform/blackberry/editing/text-iterator/findString-markers-expected.txt:
    * platform/blackberry/editing/text-iterator/findString-markers.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InPageSearchManager.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InPageSearchManager.h
@@ -40,7 +40,7 @@ public:
     InPageSearchManager(WebPagePrivate*);
     ~InPageSearchManager();
 
-    bool findNextString(const String& text, bool forward);
+    bool findNextString(const String&, WebCore::FindOptions, bool wrap, bool highlightAllMatches);
     void frameUnloaded(const WebCore::Frame*);
 
 private:
@@ -48,7 +48,7 @@ private:
     friend class DeferredScopeStringMatches;
 
     void clearTextMatches();
-    void setMarkerActive(WebCore::Range*, bool);
+    void setActiveMatchAndMarker(PassRefPtr<WebCore::Range>);
     bool findAndMarkText(const String&, WebCore::Range*, WebCore::Frame*, const WebCore::FindOptions&, bool);
     bool shouldSearchForText(const String&);
     void scopeStringMatches(const String& text, bool reset, WebCore::Frame* scopingFrame = 0);
@@ -63,7 +63,9 @@ private:
     String m_activeSearchString;
     int m_activeMatchCount;
     bool m_scopingComplete;
+    bool m_scopingCaseInsensitive;
     bool m_locatingActiveMatch;
+    bool m_highlightAllMatches;
     int m_activeMatchIndex;
 };
 
