f9a4c392e7c5f2cb575d6a7933253ffde18b79f5
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-03 21:21:46 +0000

    Having a drop handler prevents navigation on drop even if event is not cancelled https://bugs.webkit.org/show_bug.cgi?id=79172
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Only early return if the drop handler prevents the default action.
    http://trac.webkit.org/changeset/105396 introduced this issue when fixing some other aspects
    of DnD handling.
    
    Test: fast/events/drop-handler-should-not-stop-navigate.html
    
    * page/DragController.cpp:
    (WebCore::DragController::performDrag):
    (WebCore::DragController::concludeEditDrag): Remove the assert. By definition, we want to
        allow default actions to run now if they weren't explicitly canceled.
    
    LayoutTests:
    
    * fast/events/drag-dataTransferItemList.html: Fix drop handler to prevent default.
    * fast/events/drop-handler-should-not-stop-navigate-expected.txt: Added.
    * fast/events/drop-handler-should-not-stop-navigate.html: Added.
    * http/tests/security/clipboard/clipboard-file-access.html: Change dragover to drop handler
        to prevent bubbled events from causing navigation.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DragController.cpp
+++ third_party/WebKit/Source/WebCore/page/DragController.cpp
@@ -213,7 +213,7 @@ bool DragController::performDrag(DragData* dragData)
             preventedDefault = mainFrame->eventHandler()->performDragAndDrop(createMouseEvent(dragData), clipboard.get());
             clipboard->setAccessPolicy(ClipboardNumb); // Invalidate clipboard here for security
         }
-        if (m_isHandlingDrag || preventedDefault) {
+        if (preventedDefault) {
             m_documentUnderMouse = 0;
             return true;
         }
@@ -423,7 +423,6 @@ bool DragController::dispatchTextInputEventFor(Frame* innerFrame, DragData* drag
 bool DragController::concludeEditDrag(DragData* dragData)
 {
     ASSERT(dragData);
-    ASSERT(!m_isHandlingDrag);
 
     RefPtr<HTMLInputElement> fileInput = m_fileInputElementUnderMouse;
     if (m_fileInputElementUnderMouse) {
