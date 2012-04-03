086bcdb471e4535bb4601616084d705d32a99dde
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 02:45:22 +0000

    [uber page] Fix sync setup dialog focus.
    R=estade@chromium.org
    TEST=Focus works when showing "Sign in to Chrome" dialog from settings page.
    BUG=115864
    
    Review URL: https://chromiumcodereview.appspot.com/9903011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -87,9 +87,8 @@ cr.define('options', function() {
     getEncryptionRadioCheckedValue_: function() {
       var f = $('choose-data-types-form');
       for (var i = 0; i < f.encrypt.length; ++i) {
-        if (f.encrypt[i].checked) {
+        if (f.encrypt[i].checked)
           return f.encrypt[i].value;
-        }
       }
 
       return undefined;
@@ -155,8 +154,9 @@ cr.define('options', function() {
 
       var f = $('choose-data-types-form');
       if (this.getPassphraseRadioCheckedValue_() != 'explicit' ||
-          $('google-option').disabled)
+          $('google-option').disabled) {
         return true;
+      }
 
       var customPassphrase = $('custom-passphrase');
       if (customPassphrase.value.length == 0) {
@@ -492,6 +492,12 @@ cr.define('options', function() {
       chrome.send('SyncSetupAttachHandler');
     },
 
+    /**
+     * Shows the appropriate sync setup page.
+     * @param {string} page A page of the sync setup to show.
+     * @param {object} args Data from the C++ to forward on to the right
+     *     section.
+     */
     showSyncSetupPage_: function(page, args) {
       this.setThrobbersVisible_(false);
 
@@ -502,15 +508,19 @@ cr.define('options', function() {
 
       this.setInputElementsDisabledState_(false);
 
-      if (page == 'login')
-        this.showGaiaLogin_(args);
-      else if (page == 'configure' || page == 'passphrase')
-        this.showConfigure_(args);
-
+      // NOTE: Because both showGaiaLogin_() and showConfigure_() change the
+      // focus, we need to ensure that the overlay container and dialog aren't
+      // [hidden] (as trying to focus() nodes inside of a [hidden] DOM section
+      // doesn't work).
       if (page == 'done')
         this.closeOverlay_();
       else
         this.showOverlay_();
+
+      if (page == 'login')
+        this.showGaiaLogin_(args);
+      else if (page == 'configure' || page == 'passphrase')
+        this.showConfigure_(args);
     },
 
     /**
@@ -524,14 +534,20 @@ cr.define('options', function() {
         throbbers[i].style.visibility = visible ? 'visible' : 'hidden';
     },
 
+    /**
+     * Set the appropriate focus on the GAIA login section of the overlay.
+     * @private
+     */
     loginSetFocus_: function() {
-      var email = $('gaia-email');
-      var passwd = $('gaia-passwd');
+      var email = this.getLoginEmail_();
       if (email && !email.value) {
         email.focus();
-      } else if (passwd) {
-        passwd.focus();
+        return;
       }
+
+      var passwd = this.getLoginPasswd_();
+      if (passwd)
+        passwd.focus();
     },
 
     /**
