6f26bd51eb85bcf04539a6f6daab7457911cf073
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-03 09:57:19 +0000

    Unreviewed, rolling out r112954 and r112969. http://trac.webkit.org/changeset/112954 http://trac.webkit.org/changeset/112969 https://bugs.webkit.org/show_bug.cgi?id=79172
    Hits "ASSERTION FAILED: !m_isHandlingDrag" in DnD-related
    tests on Debug bots
    
    Source/WebCore:
    
    * page/DragController.cpp:
    (WebCore::DragController::performDrag):
    
    LayoutTests:
    
    * fast/events/drag-dataTransferItemList.html:
    * fast/events/drop-handler-should-not-stop-navigate-expected.txt: Removed.
    * fast/events/drop-handler-should-not-stop-navigate.html: Removed.
    * http/tests/security/clipboard/clipboard-file-access.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DragController.cpp
+++ third_party/WebKit/Source/WebCore/page/DragController.cpp
@@ -213,7 +213,7 @@ bool DragController::performDrag(DragData* dragData)
             preventedDefault = mainFrame->eventHandler()->performDragAndDrop(createMouseEvent(dragData), clipboard.get());
             clipboard->setAccessPolicy(ClipboardNumb); // Invalidate clipboard here for security
         }
-        if (preventedDefault) {
+        if (m_isHandlingDrag || preventedDefault) {
             m_documentUnderMouse = 0;
             return true;
         }
