25a575fb964f2a3580ef935866f3c15c2a89ff80
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 13:51:58 +0000

    Reapply 130248 - Add full support for filesystem URLs. Trying to get the build into the right state.
    TBR=pfeldman
    
    Review URL: https://chromiumcodereview.appspot.com/9956101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130363 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_sniffer.cc
+++ net/base/mime_sniffer.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -568,12 +568,12 @@ bool ShouldSniffMimeType(const GURL& url, const std::string& mime_type) {
   if (!should_sniff_counter)
     should_sniff_counter =
         UMASnifferHistogramGet("mime_sniffer.ShouldSniffMimeType2", 3);
-  // We are willing to sniff the mime type for HTTP, HTTPS, and FTP
   bool sniffable_scheme = url.is_empty() ||
                           url.SchemeIs("http") ||
                           url.SchemeIs("https") ||
                           url.SchemeIs("ftp") ||
-                          url.SchemeIsFile();
+                          url.SchemeIsFile() ||
+                          url.SchemeIsFileSystem();
   if (!sniffable_scheme) {
     should_sniff_counter->Add(1);
     return false;
