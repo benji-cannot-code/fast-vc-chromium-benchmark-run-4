7f712f5bfb9551ade32d7f50c8f98285b503f496
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-04 18:18:44 +0000

    Call suddenTerminationChanged directly https://bugs.webkit.org/show_bug.cgi?id=83114
    Patch by Mark Pilgrim <pilgrim@chromium.org> on 2012-04-04
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    * platform/chromium/PlatformSupport.h:
    (PlatformSupport):
    * platform/chromium/SuddenTerminationChromium.cpp:
    (WebCore::disableSuddenTermination):
    (WebCore::enableSuddenTermination):
    
    Source/WebKit/chromium:
    
    * src/PlatformSupport.cpp:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/SuddenTerminationChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/SuddenTerminationChromium.cpp
@@ -32,18 +32,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "SuddenTermination.h"
 
-#include "PlatformSupport.h"
+#include <public/Platform.h>
 
 namespace WebCore {
 
 void disableSuddenTermination()
 {
-    PlatformSupport::suddenTerminationChanged(false);
+    WebKit::Platform::current()->suddenTerminationChanged(false);
 }
 
 void enableSuddenTermination()
 {
-    PlatformSupport::suddenTerminationChanged(true);
+    WebKit::Platform::current()->suddenTerminationChanged(true);
 }
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebKit/chromium/src/PlatformSupport.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/PlatformSupport.cpp
@@ -717,13 +717,6 @@ void PlatformSupport::histogramEnumeration(const char* name, int sample, int bou
     webKitPlatformSupport()->histogramEnumeration(name, sample, boundaryValue);
 }
 
-// Sudden Termination ---------------------------------------------------------
-
-void PlatformSupport::suddenTerminationChanged(bool enabled)
-{
-    webKitPlatformSupport()->suddenTerminationChanged(enabled);
-}
-
 // Theming --------------------------------------------------------------------
 
 #if OS(WINDOWS)
