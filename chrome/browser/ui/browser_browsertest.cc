29fee9bee362072d46231e02a95823311764e3e2
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 18:54:35 +0000

    Honor window size for chrome.windows.create when parent window is maximized.
    BUG=111618
    
    Review URL: https://chromiumcodereview.appspot.com/9968078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130696 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_browsertest.cc
+++ chrome/browser/ui/browser_browsertest.cc
@@ -1511,8 +1511,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest,
 
   // Create a popup (non-main-UI-type) browser. Settings command as well
   // as Extensions should be disabled.
-  Browser* popup_browser = browser()->CreateForType(Browser::TYPE_POPUP,
-                                                    browser()->profile());
+  Browser* popup_browser = browser()->CreateWithParams(
+      Browser::CreateParams(Browser::TYPE_POPUP, browser()->profile()));
   CommandUpdater* popup_command_updater = popup_browser->command_updater();
   EXPECT_FALSE(popup_command_updater->IsCommandEnabled(IDC_MANAGE_EXTENSIONS));
   EXPECT_FALSE(popup_command_updater->IsCommandEnabled(IDC_OPTIONS));
@@ -1526,8 +1526,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest,
 IN_PROC_BROWSER_TEST_F(BrowserTest,
                        DisableOptionsAndImportMenuItemsConsistently) {
   // Create a popup browser.
-  Browser* popup_browser = browser()->CreateForType(Browser::TYPE_POPUP,
-                                                    browser()->profile());
+  Browser* popup_browser = browser()->CreateWithParams(
+      Browser::CreateParams(Browser::TYPE_POPUP, browser()->profile()));
   CommandUpdater* command_updater = popup_browser->command_updater();
   // OPTIONS and IMPORT_SETTINGS are disabled for a non-normal UI.
   EXPECT_FALSE(command_updater->IsCommandEnabled(IDC_OPTIONS));
@@ -1691,8 +1691,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest2, NoTabsInPopups) {
   EXPECT_EQ(1, browser()->tab_count());
 
   // Open a popup browser with a single blank foreground tab.
-  Browser* popup_browser = browser()->CreateForType(Browser::TYPE_POPUP,
-                                                    browser()->profile());
+  Browser* popup_browser = browser()->CreateWithParams(
+      Browser::CreateParams(Browser::TYPE_POPUP, browser()->profile()));
   popup_browser->AddBlankTab(true);
   EXPECT_EQ(1, popup_browser->tab_count());
 
@@ -1708,8 +1708,9 @@ IN_PROC_BROWSER_TEST_F(BrowserTest2, NoTabsInPopups) {
   EXPECT_EQ(2, browser()->tab_count());
 
   // Open an app frame browser with a single blank foreground tab.
-  Browser* app_browser =
-      browser()->CreateForApp(L"Test", browser()->profile(), false);
+  Browser* app_browser = browser()->CreateWithParams(
+      Browser::CreateParams::CreateForApp(
+          L"Test", browser()->profile(), false));
   app_browser->AddBlankTab(true);
   EXPECT_EQ(1, app_browser->tab_count());
 
@@ -1726,8 +1727,9 @@ IN_PROC_BROWSER_TEST_F(BrowserTest2, NoTabsInPopups) {
   EXPECT_EQ(3, browser()->tab_count());
 
   // Open an app frame popup browser with a single blank foreground tab.
-  Browser* app_popup_browser =
-      browser()->CreateForApp(L"Test", browser()->profile(), false);
+  Browser* app_popup_browser = browser()->CreateWithParams(
+      Browser::CreateParams::CreateForApp(
+          L"Test", browser()->profile(), false));
   app_popup_browser->AddBlankTab(true);
   EXPECT_EQ(1, app_popup_browser->tab_count());
 
