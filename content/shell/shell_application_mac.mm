3c8c9537372689cdfd88f294654b6a6577b7c77e
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 20:47:32 +0000

    Mac content shell: Fix new browser window.
    This is a revert of most of r130140, and a better re-implementation thereof.
    
    BUG=120153
    TEST=as described
    
    Review URL: https://chromiumcodereview.appspot.com/9982011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130723 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_application_mac.mm
+++ content/shell/shell_application_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "content/shell/shell.h"
 #include "content/shell/shell_browser_context.h"
+#include "content/shell/shell_content_browser_client.h"
 #include "googleurl/src/gurl.h"
 
 @implementation ShellCrApplication
@@ -26,7 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (IBAction)newDocument:(id)sender {
-  content::Shell::CreateNewWindow(content::ShellBrowserContext::GetInstance(),
+  content::ShellBrowserContext* browserContext =
+      static_cast<content::ShellContentBrowserClient*>(
+        content::GetContentClient()->browser())->browser_context();
+  content::Shell::CreateNewWindow(browserContext,
                                   GURL("about:blank"),
                                   NULL,
                                   MSG_ROUTING_NONE,
--- content/shell/shell_browser_context.h
+++ content/shell/shell_browser_context.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/singleton.h"
 #include "content/public/browser/browser_context.h"
 
 class DownloadManager;
@@ -20,11 +19,13 @@ namespace content {
 
 class DownloadManagerDelegate;
 class ResourceContext;
+class ShellBrowserMainParts;
 class ShellDownloadManagerDelegate;
 
 class ShellBrowserContext : public BrowserContext {
  public:
-  static ShellBrowserContext* GetInstance();
+  explicit ShellBrowserContext(ShellBrowserMainParts* shell_main_parts);
+  virtual ~ShellBrowserContext();
 
   // BrowserContext implementation.
   virtual FilePath GetPath() OVERRIDE;
@@ -43,10 +44,6 @@ class ShellBrowserContext : public BrowserContext {
   virtual quota::SpecialStoragePolicy* GetSpecialStoragePolicy() OVERRIDE;
 
  private:
-  ShellBrowserContext();
-  virtual ~ShellBrowserContext();
-  friend struct DefaultSingletonTraits<ShellBrowserContext>;
-
   // Performs initialization of the ShellBrowserContext while IO is still
   // allowed on the current thread.
   void InitWhileIOAllowed();
@@ -58,6 +55,8 @@ class ShellBrowserContext : public BrowserContext {
   scoped_refptr<net::URLRequestContextGetter> url_request_getter_;
   scoped_refptr<GeolocationPermissionContext> geolocation_permission_context_;
   scoped_refptr<SpeechRecognitionPreferences> speech_recognition_preferences_;
+
+  ShellBrowserMainParts* shell_main_parts_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellBrowserContext);
 };
