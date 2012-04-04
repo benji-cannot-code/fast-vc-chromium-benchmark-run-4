fc71e8ee4499c5a68f270ec078aa2007c504a4bd
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 19:26:13 +0000

    Adds a TabContentsViewAura.
    It is derived from TCVV/NTCVA. Right now, it's behind the flag --enable-tcva.
    
    http://crbug.com/118410
    TEST=none
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=130442
    Review URL: https://chromiumcodereview.appspot.com/9963079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130704 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/tab_contents/tab_contents.cc
+++ content/browser/tab_contents/tab_contents.cc
@@ -65,7 +65,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/web_intent_data.h"
 #include "webkit/glue/webpreferences.h"
 
-#if defined(OS_WIN) && !defined(USE_AURA)
+#if defined(USE_AURA)
+#include "content/browser/tab_contents/tab_contents_view_aura.h"
+#elif defined(OS_WIN)
 #include "content/browser/tab_contents/tab_contents_view_win.h"
 #elif defined(TOOLKIT_GTK)
 #include "content/browser/tab_contents/tab_contents_view_gtk.h"
@@ -282,7 +284,9 @@ TabContents::TabContents(content::BrowserContext* browser_context,
     content::WebContentsViewDelegate* delegate =
         content::GetContentClient()->browser()->GetWebContentsViewDelegate(
             this);
-#if defined(OS_WIN) && !defined(USE_AURA)
+#if defined(USE_AURA)
+    view_.reset(new TabContentsViewAura(this, delegate));
+#elif defined(OS_WIN)
     view_.reset(new TabContentsViewWin(this, delegate));
 #elif defined(TOOLKIT_GTK)
     view_.reset(new content::TabContentsViewGtk(this, delegate));
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -665,6 +665,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'browser/tab_contents/tab_contents.cc',
     'browser/tab_contents/tab_contents.h',
     'browser/tab_contents/tab_contents_view_aura.cc',
+    'browser/tab_contents/tab_contents_view_aura.h',
     'browser/tab_contents/tab_contents_view_gtk.cc',
     'browser/tab_contents/tab_contents_view_gtk.h',
     'browser/tab_contents/tab_contents_view_helper.cc',
