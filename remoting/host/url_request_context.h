086da1b53a8ee14f488e51b08b8825f57a3f93a9
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 04:02:02 +0000

    Mark ~URLRequestContext() and ~URLRequestContextGetter() as private.
    Destructors for ref-counted object should be private. This enusres that
    they are deleted by scoped_refptr<> only.
    
    Review URL: https://chromiumcodereview.appspot.com/9958132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130555 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/url_request_context.h
+++ remoting/host/url_request_context.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "net/proxy/proxy_config_service.h"
@@ -29,14 +30,17 @@ class URLRequestContext : public net::URLRequestContext {
   explicit URLRequestContext(net::ProxyConfigService* net_proxy_config_service);
 
  private:
+  virtual ~URLRequestContext();
+
   net::URLRequestContextStorage storage_;
+
+  DISALLOW_COPY_AND_ASSIGN(URLRequestContext);
 };
 
 class URLRequestContextGetter : public net::URLRequestContextGetter {
  public:
   URLRequestContextGetter(MessageLoop* io_message_loop,
                           MessageLoop* file_message_loop);
-  virtual ~URLRequestContextGetter();
 
   // Overridden from net::URLRequestContextGetter:
   virtual net::URLRequestContext* GetURLRequestContext() OVERRIDE;
@@ -44,9 +48,13 @@ class URLRequestContextGetter : public net::URLRequestContextGetter {
       GetIOMessageLoopProxy() const OVERRIDE;
 
  private:
+  virtual ~URLRequestContextGetter();
+
   scoped_refptr<net::URLRequestContext> url_request_context_;
   scoped_refptr<base::MessageLoopProxy> io_message_loop_proxy_;
   scoped_ptr<net::ProxyConfigService> proxy_config_service_;
+
+  DISALLOW_COPY_AND_ASSIGN(URLRequestContextGetter);
 };
 
 }  // namespace remoting
