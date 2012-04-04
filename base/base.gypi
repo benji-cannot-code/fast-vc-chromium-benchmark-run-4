fbb826ac5be9a2cca59a8c836603fe4ff74fe3a2
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 21:55:53 +0000

    Add support for UIA accessibility interfaces like IAccessibleEx and IRawElementProviderSimple. This is required for the on screen keyboard to show up in Windows 8 metro. At this point we only implement the GetPatternProvider/GetPropertyValue methods in the IRawElementProviderSimple. interface and the rest of the methods in the interfaces are left unimplemented.
    The GetPatternProvider/GetPropertyValue only handle the case where the user touches an editable
    portion on the window/page. This ensures that the OSK shows up on the omnibox and
    the web page. The ITextProvider and IValueProvider implementation is provided by
    the UIATextProvider class which lives in base/win/accessibility_misc_utils.cc/.h files.
    
    BUG=118641
    Review URL: https://chromiumcodereview.appspot.com/9958139
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130744 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base.gypi
+++ base/base.gypi
@@ -402,6 +402,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'nix/xdg_util.cc',
           'nix/xdg_util.h',
           'wayland/wayland_event.h',
+          'win/accessibility_misc_utils.h',
+          'win/accessibility_misc_utils.cc',
           'win/enum_variant.h',
           'win/enum_variant.cc',
           'win/event_trace_consumer.h',
--- ui/views/accessibility/native_view_accessibility_win.h
+++ ui/views/accessibility/native_view_accessibility_win.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atlcom.h>
 
 #include <oleacc.h>
+#include <UIAutomationCore.h>
 
 #include "base/memory/scoped_ptr.h"
 #include "third_party/iaccessible2/ia2_api_all.h"
@@ -41,7 +42,9 @@ NativeViewAccessibilityWin
     public IDispatchImpl<IAccessible2, &IID_IAccessible2,
                            &LIBID_IAccessible2Lib>,
     public IAccessibleText,
-    public IServiceProvider {
+    public IServiceProvider,
+    public IAccessibleEx,
+    public IRawElementProviderSimple {
  public:
   BEGIN_COM_MAP(NativeViewAccessibilityWin)
     COM_INTERFACE_ENTRY2(IDispatch, IAccessible2)
@@ -49,6 +52,8 @@ NativeViewAccessibilityWin
     COM_INTERFACE_ENTRY(IAccessible2)
     COM_INTERFACE_ENTRY(IAccessibleText)
     COM_INTERFACE_ENTRY(IServiceProvider)
+    COM_INTERFACE_ENTRY(IAccessibleEx)
+    COM_INTERFACE_ENTRY(IRawElementProviderSimple)
   END_COM_MAP()
 
   // Create method for view accessibility.
@@ -286,6 +291,46 @@ NativeViewAccessibilityWin
   //
 
   STDMETHODIMP QueryService(REFGUID guidService, REFIID riid, void** object);
+
+  //
+  // IAccessibleEx methods not implemented.
+  //
+  STDMETHODIMP GetObjectForChild(long child_id, IAccessibleEx** ret) {
+    return E_NOTIMPL;
+  }
+
+  STDMETHODIMP GetIAccessiblePair(IAccessible** acc, long* child_id) {
+    return E_NOTIMPL;
+  }
+
+  STDMETHODIMP GetRuntimeId(SAFEARRAY** runtime_id) {
+    return E_NOTIMPL;
+  }
+
+  STDMETHODIMP ConvertReturnedElement(IRawElementProviderSimple* element,
+                                      IAccessibleEx** acc) {
+    return E_NOTIMPL;
+  }
+
+  //
+  // IRawElementProviderSimple methods.
+  //
+  // The GetPatternProvider/GetPropertyValue methods need to be implemented for
+  // the on-screen keyboard to show up in Windows 8 metro.
+  STDMETHODIMP GetPatternProvider(PATTERNID id, IUnknown** provider);
+  STDMETHODIMP GetPropertyValue(PROPERTYID id, VARIANT* ret);
+
+  //
+  // IRawElementProviderSimple methods not implemented.
+  //
+  STDMETHODIMP get_ProviderOptions(enum ProviderOptions* ret) {
+    return E_NOTIMPL;
+  }
+
+  STDMETHODIMP get_HostRawElementProvider(
+      IRawElementProviderSimple** provider) {
+    return E_NOTIMPL;
+  }
 
   // Returns a conversion from the event (as defined in accessibility_types.h)
   // to an MSAA event.
