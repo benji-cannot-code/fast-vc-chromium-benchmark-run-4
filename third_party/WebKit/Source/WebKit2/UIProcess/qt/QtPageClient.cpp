7fae94a610e8adff7561550334ad2604bfbef77a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-04 13:35:33 +0000

    [Qt][WK2] QtWebKit2 should support Page Visibility API https://bugs.webkit.org/show_bug.cgi?id=81164
    Patch by Jesus Sanchez-Palencia <jesus.palencia@openbossa.org> on 2012-04-04
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebKit2:
    
    Implementing the last bits needed for getting the expected
    behavior for Page Visibility API, according to its spec.
    The only missing part in Qt was a way to check whether
    the view was exposed or not.
    
    * UIProcess/qt/QtPageClient.cpp:
    (QtPageClient::isViewVisible):
    
    Tools:
    
    Enabling Page Visiblity API for Qt WebKit2.
    
    * qmake/mkspecs/features/features.prf:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtPageClient.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtPageClient.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "qquickwebview_p.h"
 #include "qquickwebview_p_p.h"
 #include <QGuiApplication>
+#include <QQuickCanvas>
 #include <WebCore/Cursor.h>
 #include <WebCore/DragData.h>
 #include <WebCore/FloatRect.h>
@@ -271,6 +272,11 @@ bool QtPageClient::isViewVisible()
 {
     if (!m_webView)
         return false;
+
+    // FIXME: this is a workaround while QWindow::isExposed() is not ready.
+    if (m_webView->canvas() && m_webView->canvas()->windowState() == Qt::WindowMinimized)
+        return false;
+
     return m_webView->isVisible() && m_webView->page()->isVisible();
 }
 
