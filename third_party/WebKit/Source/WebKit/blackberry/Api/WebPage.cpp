2e39e74b121e4bad29e04016de5f1f0fd0c661e8
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-04 16:59:17 +0000

    [BlackBerry] Make the switch statement in WebPage::notifyAppActivationStateChange() stronger https://bugs.webkit.org/show_bug.cgi?id=83085
    Reviewed by Antonio Gomes.
    
    PR 121109
    
    This way when we add something to the enum and forget to add to the switch we'll get a compile error.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::notifyAppActivationStateChange):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -5084,9 +5084,6 @@ void WebPage::notifyAppActivationStateChange(ActivationStateType activationState
         case ActivationStandby:
             (*it)->handleAppStandbyEvent();
             break;
-        default: // FIXME: Get rid of the default to force a compiler error instead of using a runtime error. See PR #121109.
-            ASSERT_NOT_REACHED();
-            break;
         }
     }
 
