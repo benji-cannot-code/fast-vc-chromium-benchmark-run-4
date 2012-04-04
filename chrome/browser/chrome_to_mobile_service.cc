0e1887b6547d05fb3d99b24ce9e878dafd1cc157
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 00:44:07 +0000

    Skip Chrome To Mobile's service check if cookies are blocked.
    CTM typically checks a cookie before accessing cloud print.
    This is done to reduce cloud print QPS: http://crrev.com/130220
    Skip this check *if* the profile blocks reading this cookie.
    
    This will (hopefully negligibly) increase cloud print QPS.
    About ~0.01% of users have CookieBlockingEnabledPerDefault.
    See: https://uma.googleplex.com/counts/
    
    BUG=102709,118691
    TEST=Chrome To Mobile works, even with cookies disabled.
    
    Review URL: https://chromiumcodereview.appspot.com/9956094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_to_mobile_service.cc
+++ chrome/browser/chrome_to_mobile_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/browser/content_settings/cookie_settings.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_url.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/token_service.h"
@@ -357,6 +358,17 @@ void ChromeToMobileService::RequestAccountInfo() {
   std::string url_string = StringPrintf(kAccountInfoURL,
       guid::GenerateGUID().c_str(), kChromeToMobileRequestor);
   GURL url(url_string);
+
+  // Account information is read from the profile's cookie. If cookies are
+  // blocked, access cloud print directly to list any potential devices.
+  scoped_refptr<CookieSettings> cookie_settings =
+      CookieSettings::Factory::GetForProfile(profile_);
+  if (cookie_settings && !cookie_settings->IsReadingCookieAllowed(url, url)) {
+    cloud_print_accessible_ = true;
+    RequestMobileListUpdate();
+    return;
+  }
+
   account_info_request_.reset(
       content::URLFetcher::Create(url, content::URLFetcher::GET, this));
   account_info_request_->SetRequestContext(profile_->GetRequestContext());
