7a19b6bd1d1c7fb278604feb991fb328cf62b159
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 10:00:33 +0000

    Allow users to change site permissions via the WebsiteSettingsUI (GTK only)
    BUG=113688
    TEST=WebsiteSettingsModel*
    
    Review URL: https://chromiumcodereview.appspot.com/9909017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130586 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/website_settings_ui.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/website_settings_ui.h"
+
+WebsiteSettingsUI::CookieInfo::CookieInfo()
+    : allowed(-1), blocked(-1) {
+}
+
+WebsiteSettingsUI::PermissionInfo::PermissionInfo()
+    : type(CONTENT_SETTINGS_TYPE_DEFAULT),
+      setting(CONTENT_SETTING_DEFAULT),
+      default_setting(CONTENT_SETTING_DEFAULT) {
+}
+
+WebsiteSettingsUI::~WebsiteSettingsUI() {
+}
