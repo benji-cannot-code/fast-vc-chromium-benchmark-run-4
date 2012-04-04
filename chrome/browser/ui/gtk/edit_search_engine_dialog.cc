151864bfc986a666e72c0f00ac5440e46e1fa534
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 06:49:58 +0000

    Revert 130431 - Move the URL string from TemplateURLRef onto the owning TemplateURL.  This will make it easier to move the data members of TemplateURL into a new class later.
    This changes the accessors for TemplateURL's TemplateURLRefs.  There are now separate accessors for the URLs as strings and as TemplateURLRefs, and the latter have changed to returning a const ref, meaning they no longer return NULL when the corresponding URL string is empty.  This makes a number of callers clearer.
    
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9968016
    
    TBR=pkasting@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9965143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/edit_search_engine_dialog.cc
+++ chrome/browser/ui/gtk/edit_search_engine_dialog.cc
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+std::string GetDisplayURL(const TemplateURL& turl) {
+  return turl.url() ? UTF16ToUTF8(turl.url()->DisplayURL()) : std::string();
+}
+
 // Forces text to lowercase when connected to an editable's "insert-text"
 // signal.  (Like views Textfield::STYLE_LOWERCASE.)
 void LowercaseInsertTextHandler(GtkEditable *editable, const gchar *text,
@@ -150,8 +154,7 @@ void EditSearchEngineDialog::Init(GtkWindow* parent_window, Profile* profile) {
         UTF16ToUTF8(controller_->template_url()->keyword()).c_str());
     gtk_entry_set_text(
         GTK_ENTRY(url_entry_),
-        UTF16ToUTF8(controller_->template_url()->url_ref().DisplayURL()).
-            c_str());
+        GetDisplayURL(*controller_->template_url()).c_str());
     // We don't allow users to edit prepopulated URLs.
     gtk_editable_set_editable(
         GTK_EDITABLE(url_entry_),
