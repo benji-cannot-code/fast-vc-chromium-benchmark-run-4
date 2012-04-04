607e234829659bafa212b634ebebf12bbf5ed5a4
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 21:16:28 +0000

    Moved special-case handling of 'home' mode.
    This was causing the host list to be reloaded when the confirmation dialog was
    dismissed, which was often too fast for the delete to have been committed in
    the directory.
    
    BUG=121013
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9947004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130734 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/event_handlers.js
+++ remoting/webapp/event_handlers.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var remoting = remoting || {};
 
 function onLoad() {
-  var goHome = function() {
+  var restartWebapp = function() {
     window.location.replace(chrome.extension.getURL('main.html'));
   };
   var goEnterAccessCode = function() {
@@ -19,7 +19,7 @@ function onLoad() {
     if (remoting.currentMode == remoting.AppMode.CLIENT_CONNECT_FAILED_IT2ME) {
       remoting.setMode(remoting.AppMode.CLIENT_UNCONNECTED);
     } else {
-      remoting.setMode(remoting.AppMode.HOME);
+      restartWebapp();
     }
   };
   var reload = function() {
@@ -49,11 +49,11 @@ function onLoad() {
       { event: 'click', id: 'access-mode-button', fn: goEnterAccessCode },
       { event: 'click', id: 'cancel-share-button', fn: remoting.cancelShare },
       { event: 'click', id: 'stop-sharing-button', fn: remoting.cancelShare },
-      { event: 'click', id: 'host-finished-button', fn: goHome },
+      { event: 'click', id: 'host-finished-button', fn: restartWebapp },
       { event: 'click', id: 'client-finished-it2me-button',
         fn: goFinishedIt2Me },
-      { event: 'click', id: 'client-finished-me2me-button', fn: goHome },
-      { event: 'click', id: 'cancel-pin-entry-button', fn: goHome },
+      { event: 'click', id: 'client-finished-me2me-button', fn: restartWebapp },
+      { event: 'click', id: 'cancel-pin-entry-button', fn: restartWebapp },
       { event: 'click', id: 'client-reconnect-button', fn: reload },
       { event: 'click', id: 'cancel-access-code-button',
         fn: remoting.cancelConnect },
