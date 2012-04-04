8b22c7057e2d3139fa025f664e97708a6377390e
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 06:32:05 +0000

    Revert 130447 - Use a common font baseline in RenderText classes.
    This ensures the text in different runs gets aligned
    along a common bottom baseline and that the correct
    font's baseline gets used in case of font fallback.
    
    Also, uses the correct height when centering text.
    
    BUG=105550
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9968040
    
    TBR=asvitkine@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9965140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130562 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text_win.cc
+++ ui/gfx/render_text_win.cc
@@ -180,7 +180,6 @@ std::map<std::string, std::vector<Font> > RenderTextWin::cached_linked_fonts_;
 
 RenderTextWin::RenderTextWin()
     : RenderText(),
-      common_baseline_(0),
       needs_layout_(false) {
   memset(&script_control_, 0, sizeof(script_control_));
   memset(&script_state_, 0, sizeof(script_state_));
@@ -466,10 +465,7 @@ void RenderTextWin::EnsureLayout() {
 void RenderTextWin::DrawVisualText(Canvas* canvas) {
   DCHECK(!needs_layout_);
 
-  Point offset(GetOriginForDrawing());
-  // Skia will draw glyphs with respect to the baseline.
-  offset.Offset(0, common_baseline_);
-
+  Point offset(GetOriginForSkiaDrawing());
   SkScalar x = SkIntToScalar(offset.x());
   SkScalar y = SkIntToScalar(offset.y());
 
@@ -523,7 +519,6 @@ void RenderTextWin::ItemizeLogicalText() {
   STLDeleteContainerPointers(runs_.begin(), runs_.end());
   runs_.clear();
   string_size_ = Size(0, GetFont().GetHeight());
-  common_baseline_ = 0;
   if (text().empty())
     return;
 
@@ -690,7 +685,6 @@ void RenderTextWin::LayoutVisualText() {
     DCHECK(SUCCEEDED(hr));
     string_size_.set_height(std::max(string_size_.height(),
                                      run->font.GetHeight()));
-    common_baseline_ = std::max(common_baseline_, run->font.GetBaseline());
 
     if (run->glyph_count > 0) {
       run->advance_widths.reset(new int[run->glyph_count]);
