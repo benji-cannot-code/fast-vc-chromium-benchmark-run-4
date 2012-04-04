b723a68cc65a07aa090b380612029ebfa3718b79
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 19:57:50 +0000

    ScopedProcessInformation protects against process/thread handle leaks from CreateProcess calls.
    This CL includes the definition of the class along with changes to clients of CreateProcess.
    
    Please see http://codereview.chromium.org/9959018/ for the most substantial usages I propose introducing.
    
    This was motivated by realizing that my previous fix to LaunchProcess actually still had a leak of the thread handle.
    
    BUG=None
    TEST=tests added in base_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/9700038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130710 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/wts_session_process_launcher_win.cc
+++ remoting/host/wts_session_process_launcher_win.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/scoped_handle.h"
+#include "base/win/scoped_process_information.h"
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_macros.h"
@@ -213,7 +214,7 @@ bool LaunchProcessAsUser(const FilePath& binary,
   string16 application_name = binary.value();
   string16 desktop = ASCIIToUTF16(kDefaultDesktopName);
 
-  PROCESS_INFORMATION process_info;
+  base::win::ScopedProcessInformation process_info;
   STARTUPINFOW startup_info;
 
   memset(&startup_info, 0, sizeof(startup_info));
@@ -230,14 +231,13 @@ bool LaunchProcessAsUser(const FilePath& binary,
                             NULL,
                             NULL,
                             &startup_info,
-                            &process_info)) {
+                            process_info.Receive())) {
     LOG_GETLASTERROR(ERROR) <<
         "Failed to launch a process with a user token";
     return false;
   }
 
-  CloseHandle(process_info.hThread);
-  process_out->set_handle(process_info.hProcess);
+  process_out->set_handle(process_info.TakeProcessHandle());
   return true;
 }
 
