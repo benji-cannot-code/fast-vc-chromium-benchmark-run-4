7040cfeafaa108a0c825285b69ffb14b47b069d9
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-04 08:47:00 +0000

    Web Inspector: event marks are missing in the timeline overview https://bugs.webkit.org/show_bug.cgi?id=83043
    Reviewed by Pavel Feldman.
    
    - invoke overview event dividers update as part of overview update, not lower pane update;
    - store raw records for event dividers within overview;
    - moved createEventDividers to a class method of TimelinePresentationModel for reuse.
    - ditto for forAllRecords
    
    * inspector/front-end/TimelineOverviewPane.js:
    (WebInspector.TimelineOverviewPane):
    (WebInspector.TimelineOverviewPane.prototype._update):
    (WebInspector.TimelineOverviewPane.prototype._updateCategoryStrips):
    (WebInspector.TimelineOverviewPane.prototype._updateEventDividers):
    (WebInspector.TimelineOverviewPane.prototype._onRecordAdded):
    (WebInspector.TimelineOverviewPane.prototype._reset):
    (WebInspector.HeapGraph.prototype.update):
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._updateEventDividers):
    (WebInspector.TimelinePanel.prototype._innerAddRecordToTimeline.addTimestampRecords):
    (WebInspector.TimelinePanel.prototype._innerAddRecordToTimeline):
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.isEventDivider):
    (WebInspector.TimelinePresentationModel.forAllRecords):
    (WebInspector.TimelinePresentationModel.createEventDivider):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113151 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelineOverviewPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelineOverviewPane.js
@@ -42,6 +42,7 @@ WebInspector.TimelineOverviewPane = function(model)
 
     this._windowStartTime = 0;
     this._windowEndTime = Infinity;
+    this._eventDividers = [];
 
     this._model = model;
 
@@ -111,7 +112,7 @@ WebInspector.TimelineOverviewPane = function(model)
     this._overviewGrid.setScrollAndDividerTop(0, 0);
     this._overviewCalculator = new WebInspector.TimelineOverviewCalculator();
 
-    model.addEventListener(WebInspector.TimelineModel.Events.RecordAdded, this._scheduleRefresh, this);
+    model.addEventListener(WebInspector.TimelineModel.Events.RecordAdded, this._onRecordAdded, this);
     model.addEventListener(WebInspector.TimelineModel.Events.RecordsCleared, this._reset, this);
 }
 
@@ -205,6 +206,7 @@ WebInspector.TimelineOverviewPane.prototype = {
 
         this._overviewCalculator.setWindow(this._model.minimumRecordTime(), this._model.maximumRecordTime());
         this._overviewGrid.updateDividers(true, this._overviewCalculator);
+        this._updateEventDividers();
     },
 
     _updateCategoryStrips: function()
@@ -232,7 +234,7 @@ WebInspector.TimelineOverviewPane.prototype = {
                 timelines[categoryName][j] = true;
         }
         var records = this._model.records;
-        WebInspector.TimelinePanel.forAllRecords(records, markPercentagesForRecord.bind(this));
+        WebInspector.TimelinePresentationModel.forAllRecords(records, markPercentagesForRecord.bind(this));
 
         // Convert sparse arrays to continuous segments, render graphs for each.
         for (var category in categories) {
@@ -257,19 +259,18 @@ WebInspector.TimelineOverviewPane.prototype = {
         }
     },
 
-    updateEventDividers: function(records, dividerConstructor)
+    _updateEventDividers: function()
     {
+        var records = this._eventDividers;
         this._overviewGrid.removeEventDividers();
         var dividers = [];
         for (var i = 0; i < records.length; ++i) {
             var record = records[i];
-            if (record.type === WebInspector.TimelineModel.RecordType.BeginFrame)
-                continue;
             var positions = this._overviewCalculator.computeBarGraphPercentages(record);
             var dividerPosition = Math.round(positions.start * 10);
             if (dividers[dividerPosition])
                 continue;
-            var divider = dividerConstructor(record);
+            var divider = WebInspector.TimelinePresentationModel.createEventDivider(record);
             divider.style.left = positions.start + "%";
             dividers[dividerPosition] = divider;
         }
@@ -291,6 +292,19 @@ WebInspector.TimelineOverviewPane.prototype = {
         this._scheduleRefresh();
     },
 
+    _onRecordAdded: function(event)
+    {
+        var record = event.data;
+        var eventDividers = this._eventDividers;
+        function addEventDividers(record)
+        {
+            if (WebInspector.TimelinePresentationModel.isEventDivider(record))
+                eventDividers.push(record);
+        }
+        WebInspector.TimelinePresentationModel.forAllRecords([record], addEventDividers);
+        this._scheduleRefresh();
+    },
+
     _reset: function()
     {
         this._windowStartTime = 0;
@@ -298,6 +312,7 @@ WebInspector.TimelineOverviewPane.prototype = {
         this._overviewWindow.reset();
         this._overviewCalculator.reset();
         this._overviewGrid.updateDividers(true, this._overviewCalculator);
+        this._eventDividers = [];
         if (this._verticalOverview)
             this._verticalOverview.reset();
         this._update();
@@ -746,7 +761,7 @@ WebInspector.HeapGraph.prototype = {
         var minUsedHeapSize = 100000000000;
         var minTime = this._model.minimumRecordTime();
         var maxTime = this._model.maximumRecordTime();;
-        WebInspector.TimelinePanel.forAllRecords(records, function(r) {
+        WebInspector.TimelinePresentationModel.forAllRecords(records, function(r) {
             maxUsedHeapSize = Math.max(maxUsedHeapSize, r.usedHeapSize || maxUsedHeapSize);
             minUsedHeapSize = Math.min(minUsedHeapSize, r.usedHeapSize || minUsedHeapSize);
         });
@@ -758,7 +773,7 @@ WebInspector.HeapGraph.prototype = {
         var yFactor = height / (maxUsedHeapSize - minUsedHeapSize);
 
         var histogram = new Array(width);
-        WebInspector.TimelinePanel.forAllRecords(records, function(r) {
+        WebInspector.TimelinePresentationModel.forAllRecords(records, function(r) {
             if (!r.usedHeapSize)
                 return;
             var x = Math.round((WebInspector.TimelineModel.endTimeInSeconds(r) - minTime) * xFactor);
