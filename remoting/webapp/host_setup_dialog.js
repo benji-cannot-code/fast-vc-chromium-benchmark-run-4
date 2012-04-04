241ca8849cd13598f8b32ae7898b520752f8be33
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 23:49:13 +0000

    Unregister host on stop.
    BUG=121146
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9950138
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130775 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_setup_dialog.js
+++ remoting/webapp/host_setup_dialog.js
@@ -246,6 +246,8 @@ remoting.HostSetupDialog.prototype.updateState_ = function() {
 
 /**
  * Registers new host.
+ * TODO(jamiewalch): This doesn't feel like it belongs here. I think it would be
+ * better as a member of daemon_plugin, alongside unregister.
  */
 remoting.HostSetupDialog.prototype.registerHost_ = function() {
   /** @type {remoting.HostSetupDialog} */
@@ -410,6 +412,7 @@ remoting.HostSetupDialog.prototype.stopHost_ = function() {
       return;
     }
 
+    that.daemon_.unregister();
     flow.switchToNextStep(result);
     that.updateState_();
   }
