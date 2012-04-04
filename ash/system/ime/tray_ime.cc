6ba03e79bd1b5e9a8eee6f781074d53c14111df4
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 01:55:24 +0000

    Close uber tray when IME property is selected.
    BUG=119679
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/9959142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130538 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/ime/tray_ime.cc
+++ ash/system/ime/tray_ime.cc
@@ -157,6 +157,7 @@ class IMEDetailedView : public views::View,
         if (prop_find != property_map_.end()) {
           std::string key = prop_find->second;
           delegate->ActivateIMEProperty(key);
+          GetWidget()->Close();
         }
       }
     }
