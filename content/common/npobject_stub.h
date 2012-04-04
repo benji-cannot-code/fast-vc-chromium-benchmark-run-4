30d0a64286315452f68d433fd49cc85158ca7317
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 18:58:03 +0000

    Revert 130199 - Revert 128179 - Make sure the plugin scriptable object is released before NPP_Destroy.
    We're reinstating this patch based on its impact on plugin crash rates between 20.0.1089.0 (with patch) and 20.0.1090.0 (without) builds.
    
    When the we tear down a plugin instance the plugin process first invokes NPP_Destroy, and then tears down the IPC channel to the renderer, to give NPP_Destroy a chance to do last-minute scripting.  When the IPC channel for the last instance is torn down we also clean up the IPC channels and stubs for any plugin-side NPObjects that remain.
    
    We suspect that some plugins implement the scriptable object as part of the plugin instance, rather than independently ref-counted, so that our releasing the object after NPP_Destroy actually triggers the plugin process to crash.
    
    This CL tears down the stub for the plugin's scriptable object before we call NPP_Destroy.
    
    BUG=101968,119414
    
    Original Review URL: http://codereview.chromium.org/9817023
    Revert Review URL: https://chromiumcodereview.appspot.com/9959078
    
    TBR=cpu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9979022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130698 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/npobject_stub.h
+++ content/common/npobject_stub.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -42,7 +42,8 @@ class NPObjectStub : public IPC::Channel::Listener,
   // Schedules tear-down of this stub.  The underlying NPObject reference is
   // released, and further invokations form the IPC channel will fail once this
   // call has returned.  Deletion of the stub is deferred to the main loop, in
-  // case it is touched as the stack unwinds.
+  // case it is touched as the stack unwinds.  DeleteSoon() is safe to call
+  // more than once, until control returns to the main loop.
   void DeleteSoon();
 
   // IPC::Message::Sender implementation:
