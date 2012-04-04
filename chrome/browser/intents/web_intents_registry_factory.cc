ab6c39e1650bd7afa69a764495e45b7a9dc9e1dc
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 00:46:50 +0000

    IWYU fix
    R=jhawkins@chromium.org
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9969085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130529 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/web_intents_registry_factory.cc
+++ chrome/browser/intents/web_intents_registry_factory.cc
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/intents/web_intents_registry_factory.h"
+
+#include "base/memory/singleton.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/intents/web_intents_registry.h"
-#include "chrome/browser/intents/web_intents_registry_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 
