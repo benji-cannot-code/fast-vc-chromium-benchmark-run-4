860e3c63e76a5440098aef79336aa511db6c2a11
hbono@chromium.org <hbono@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-04 02:18:49 +0000

    Prevent spellchecking text pasted to an element having spellchecking disabled https://bugs.webkit.org/show_bug.cgi?id=81323
    Reviewed by Hajime Morita.
    
    This change prevent calling SpellChecker::requestCheckingFor when pasting text
    to an element whose spellcheck attribute is false or a password input.
    
    Source/WebCore:
    
    Test: editing/spelling/spellcheck-paste-disabled.html
    
    * editing/Editor.cpp:
    (WebCore::Editor::replaceSelectionWithFragment): Disabled spellchecking on password inputs.
    * editing/SpellChecker.cpp:
    (WebCore::SpellChecker::isCheckable): Return false when spellchecking is disabled.
    
    LayoutTests:
    
    * editing/spelling/spellcheck-paste-disabled-expected.txt: Added.
    * editing/spelling/spellcheck-paste-disabled.html: Added.
    * platform/efl/Skipped: Skipped due to the lack of requestCheckingOfString().
    * platform/gtk/Skipped: ditto.
    * platform/mac-leopard/Skipped: ditto.
    * platform/mac-lion/Skipped: ditto.
    * platform/mac-wk2/Skipped: ditto.
    * platform/qt/Skipped: ditto.
    * platform/win-wk2/Skipped: ditto.
    * platform/win/Skipped: ditto.
    * platform/wincairo/Skipped: ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.cpp
+++ third_party/WebKit/Source/WebCore/editing/Editor.cpp
@@ -414,6 +414,8 @@ void Editor::replaceSelectionWithFragment(PassRefPtr<DocumentFragment> fragment,
     applyCommand(ReplaceSelectionCommand::create(m_frame->document(), fragment, options, EditActionPaste));
     revealSelectionAfterEditingOperation();
 
+    if (m_frame->selection()->isInPasswordField())
+        return;
     Node* nodeToCheck = m_frame->selection()->rootEditableElement();
     if (!nodeToCheck)
         return;
