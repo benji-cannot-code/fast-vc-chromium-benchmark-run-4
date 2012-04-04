eb598040e6e21f932c51279345b3cb4a90b11ac2
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 09:37:20 +0000

    Revert 130578 - Enable cookies per default in net. Add an API to disable them by default, and do that in Chrome
    BUG=none
    TEST=existing tests shouldn't break
    
    Review URL: https://chromiumcodereview.appspot.com/9865018
    
    TBR=jochen@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9956142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130581 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -65,12 +65,6 @@ uint64 GenerateURLRequestIdentifier() {
   return g_next_url_request_identifier++;
 }
 
-// True once the first URLRequest was started.
-bool g_url_requests_started = false;
-
-// True if cookies are accepted by default.
-bool g_default_can_use_cookies = true;
-
 }  // namespace
 
 URLRequest::ProtocolFactory*
@@ -328,12 +322,6 @@ int URLRequest::GetResponseCode() {
 }
 
 // static
-void URLRequest::SetDefaultCookiePolicyToBlock() {
-  CHECK(!g_url_requests_started);
-  g_default_can_use_cookies = false;
-}
-
-// static
 bool URLRequest::IsHandledProtocol(const std::string& scheme) {
   return URLRequestJobManager::GetInstance()->SupportsScheme(scheme);
 }
@@ -392,7 +380,6 @@ void URLRequest::set_delegate(Delegate* delegate) {
 }
 
 void URLRequest::Start() {
-  g_url_requests_started = true;
   response_info_.request_time = Time::Now();
 
   // Only notify the delegate for the initial request.
@@ -833,7 +820,7 @@ bool URLRequest::CanGetCookies(const CookieList& cookie_list) const {
     return context_->network_delegate()->NotifyReadingCookies(this,
                                                               cookie_list);
   }
-  return g_default_can_use_cookies;
+  return false;
 }
 
 bool URLRequest::CanSetCookie(const std::string& cookie_line,
@@ -844,7 +831,7 @@ bool URLRequest::CanSetCookie(const std::string& cookie_line,
                                                              cookie_line,
                                                              options);
   }
-  return g_default_can_use_cookies;
+  return false;
 }
 
 
