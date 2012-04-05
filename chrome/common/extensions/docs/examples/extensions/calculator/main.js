da77e4ebc622a5f56594955a68f503466208f0b2
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 21:28:32 +0000

    Update the calculator to use the launch event and transient background pages
    The launch event is added by http://codereview.chromium.org/9969087
    
    R=miket@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9949031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131014 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/extensions/calculator/main.js
+++ chrome/common/extensions/docs/examples/extensions/calculator/main.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.experimental.runtime.onLaunched.addListener(function() {
+chrome.experimental.app.onLaunched.addListener(function() {
   chrome.windows.create({
       url: 'calculator.html',
       type: 'shell',
