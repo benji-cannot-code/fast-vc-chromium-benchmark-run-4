ca881bbc33c2a07649522afa6d284834ffb7aacd
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 23:01:45 +0000

    Reverted logic that was fetching the the root feed in two phases. Now we will agin wait for the entire feed before we report anythi
    BUG=chromium-os:28381
    TEST=Manual: make sure that we show either entire content of /gdata source or wait until we completely read it.
    
    Review URL: https://chromiumcodereview.appspot.com/10010013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131038 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -882,7 +882,7 @@ FileManager.prototype = {
           this.gdataLoadingTimer_ = null;
           this.onGDataUnreachable_('GData load timeout');
         }.bind(this),
-        10 * 1000) ;
+        15 * 60 * 1000) ;
   };
 
   FileManager.prototype.clearGDataLoadingTimer_ = function(message) {
