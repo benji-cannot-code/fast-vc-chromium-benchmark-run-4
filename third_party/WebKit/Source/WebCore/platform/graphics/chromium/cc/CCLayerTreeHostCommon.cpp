ae749022e378be3b650907b88e8081f0e753c69a
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 20:14:18 +0000

    [chromium] Need to clip to homogeneous w=0 plane when applying transforms. https://bugs.webkit.org/show_bug.cgi?id=80806
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    Unit tests added to CCLayerTreeHostCommon. This change is also
    covered by other existing unit tests and layout tests.
    
    WebCore TransformationMatrix mapRect / mapQuad / projectQuad do
    not properly handle the case where a surface is oriented partially
    behind the camera, with a perspective projection. In this case,
    projected points may appear to be valid in cartesian coordinates,
    but they are indeed not valid, and this problem can only be
    detected in homogeneous coordinates after applying the transform,
    before the divide-by-w step.
    
    The correct solution is to clip geometry where w < 0. This patch
    makes this change local to chromium only, to fix rendering bugs
    that arise from this problem. The primary fix is to correct
    calculateVisibleLayerRect(), but other ancillary locations are
    also fixed, in particular, the antialiasing code path is simply
    skipped when this case arises.
    
    Eventually this math needs to be merged into TransformationMatrix,
    to fix hit-testing bugs that occur in both Chromium and Safari.
    
    * WebCore.gypi:
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::findTileProgramUniforms):
    (WebCore::LayerRendererChromium::drawTileQuad):
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp:
    (WebCore::CCLayerTreeHostCommon::calculateVisibleRect):
    (WebCore::isScaleOrTranslation):
    (WebCore::calculateDrawTransformsAndVisibilityInternal):
    * platform/graphics/chromium/cc/CCMathUtil.cpp: Added.
    (WebCore):
    (WebCore::HomogeneousCoordinate::HomogeneousCoordinate):
    (HomogeneousCoordinate):
    (WebCore::HomogeneousCoordinate::shouldBeClipped):
    (WebCore::HomogeneousCoordinate::cartesianPoint2d):
    (WebCore::projectPoint):
    (WebCore::mapPoint):
    (WebCore::computeClippedPointForEdge):
    (WebCore::expandBoundsToIncludePoint):
    (WebCore::computeEnclosingRectOfClippedQuad):
    (WebCore::computeEnclosingRect):
    (WebCore::CCMathUtil::mapClippedRect):
    (WebCore::CCMathUtil::projectClippedRect):
    (WebCore::CCMathUtil::mapQuad):
    (WebCore::CCMathUtil::projectQuad):
    * platform/graphics/chromium/cc/CCMathUtil.h: Added.
    (WebCore):
    (CCMathUtil):
    * platform/graphics/chromium/cc/CCOcclusionTracker.cpp:
    (WebCore::computeUnoccludedContentRect):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostCommonTest.cpp:
    (WebKitTests::TEST):
    (WebKitTests):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113364 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/CCLayerImpl.h"
 #include "cc/CCLayerIterator.h"
 #include "cc/CCLayerSorter.h"
+#include "cc/CCMathUtil.h"
 #include "cc/CCRenderSurface.h"
 
 namespace WebCore {
@@ -45,7 +46,7 @@ namespace WebCore {
 IntRect CCLayerTreeHostCommon::calculateVisibleRect(const IntRect& targetSurfaceRect, const IntRect& layerBoundRect, const TransformationMatrix& transform)
 {
     // Is this layer fully contained within the target surface?
-    IntRect layerInSurfaceSpace = transform.mapRect(layerBoundRect);
+    IntRect layerInSurfaceSpace = CCMathUtil::mapClippedRect(transform, layerBoundRect);
     if (targetSurfaceRect.contains(layerInSurfaceSpace))
         return layerBoundRect;
 
@@ -60,7 +61,7 @@ IntRect CCLayerTreeHostCommon::calculateVisibleRect(const IntRect& targetSurface
     // axis-aligned), but is a reasonable filter on the space to consider.
     // Non-invertible transforms will create an empty rect here.
     const TransformationMatrix surfaceToLayer = transform.inverse();
-    IntRect layerRect = surfaceToLayer.projectQuad(FloatQuad(FloatRect(minimalSurfaceRect))).enclosingBoundingBox();
+    IntRect layerRect = enclosingIntRect(CCMathUtil::projectClippedRect(surfaceToLayer, FloatRect(minimalSurfaceRect)));
     layerRect.intersect(layerBoundRect);
     return layerRect;
 }
@@ -71,7 +72,6 @@ static bool isScaleOrTranslation(const TransformationMatrix& m)
            && !m.m21() && !m.m23() && !m.m24()
            && !m.m31() && !m.m32() && !m.m43()
            && m.m44();
-
 }
 
 template<typename LayerType>
@@ -410,7 +410,7 @@ static bool calculateDrawTransformsAndVisibilityInternal(LayerType* layer, Layer
         layer->setDrawTransform(combinedTransform);
         layer->setDrawTransformIsAnimating(animatingTransformToTarget);
         layer->setScreenSpaceTransformIsAnimating(animatingTransformToScreen);
-        transformedLayerRect = enclosingIntRect(layer->drawTransform().mapRect(layerRect));
+        transformedLayerRect = enclosingIntRect(CCMathUtil::mapClippedRect(layer->drawTransform(), layerRect));
 
         layer->setDrawOpacity(drawOpacity);
         layer->setDrawOpacityIsAnimating(drawOpacityIsAnimating);
