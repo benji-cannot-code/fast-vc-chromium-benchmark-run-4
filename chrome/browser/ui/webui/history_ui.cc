69b20061c201797605949be72ea10ac7bf54f237
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 14:11:14 +0000

    History Tab: Un-bookmarking the page on clicking star icon.
    BUG=115383
    TEST=As described in the bug.
    
    Review URL: https://chromiumcodereview.appspot.com/9965057
    Patch from Sanjoy Pal <ncj674@motorola.com>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130914 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/history_ui.cc
+++ chrome/browser/ui/webui/history_ui.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
+#include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/history/history_notifications.h"
 #include "chrome/browser/history/history_types.h"
 #include "chrome/browser/profiles/profile.h"
@@ -175,6 +176,9 @@ void BrowsingHistoryHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback("clearBrowsingData",
       base::Bind(&BrowsingHistoryHandler::HandleClearBrowsingData,
                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback("removeBookmark",
+      base::Bind(&BrowsingHistoryHandler::HandleRemoveBookmark,
+                 base::Unretained(this)));
 }
 
 void BrowsingHistoryHandler::HandleGetHistory(const ListValue* args) {
@@ -284,6 +288,13 @@ void BrowsingHistoryHandler::HandleClearBrowsingData(const ListValue* args) {
   if (browser)
     browser->OpenClearBrowsingDataDialog();
 #endif
+}
+
+void BrowsingHistoryHandler::HandleRemoveBookmark(const ListValue* args) {
+  string16 url = ExtractStringValue(args);
+  Profile* profile = Profile::FromWebUI(web_ui());
+  BookmarkModel* model = profile->GetBookmarkModel();
+  bookmark_utils::RemoveAllBookmarks(model, GURL(url));
 }
 
 void BrowsingHistoryHandler::QueryComplete(
