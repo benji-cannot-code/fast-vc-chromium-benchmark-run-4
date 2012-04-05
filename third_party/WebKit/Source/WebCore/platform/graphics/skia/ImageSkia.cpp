44da089b1020361c8368e3c4554733966147c5cb
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 20:44:11 +0000

    [chromium] Drawing an accelerated canvas onto itself is slow. https://bugs.webkit.org/show_bug.cgi?id=83295
    Fixed by using a GPU-side deepCopy() in skia, so we don't trigger a
    readback and re-upload.
    
    Reviewed by James Robinson.
    
    Correctness is covered by existing tests. Performance is covered
    by the test case attached to the bug.
    
    * platform/graphics/skia/ImageSkia.cpp:
    (WebCore::BitmapImageSingleFrameSkia::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -480,7 +480,8 @@ PassRefPtr<BitmapImageSingleFrameSkia> BitmapImageSingleFrameSkia::create(const 
 {
     if (copyPixels) {
         SkBitmap temp;
-        bitmap.copyTo(&temp, bitmap.config());
+        if (!bitmap.deepCopyTo(&temp, bitmap.config()))
+            bitmap.copyTo(&temp, bitmap.config());
         return adoptRef(new BitmapImageSingleFrameSkia(temp));
     }
     return adoptRef(new BitmapImageSingleFrameSkia(bitmap));
