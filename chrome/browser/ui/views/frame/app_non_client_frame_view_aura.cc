a9fd76212c258ab61ebd0922f252c2d85dc376eb
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 16:18:01 +0000

    Makes AppNonClientFrameViewAura remove the observer from the destructor if the control_widget_ is non-NULL. Close is async, so without this the control_widget_ still has the AppNonClientFrameViewAura as an observer and could later call back to AppNonClientFrameViewAura.
    BUG=121666
    TEST=see bug
    R=davemoore@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9995001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130936 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/app_non_client_frame_view_aura.cc
+++ chrome/browser/ui/views/frame/app_non_client_frame_view_aura.cc
@@ -182,8 +182,10 @@ AppNonClientFrameViewAura::AppNonClientFrameViewAura(
 }
 
 AppNonClientFrameViewAura::~AppNonClientFrameViewAura() {
-  if (control_widget_)
+  if (control_widget_) {
+    control_widget_->RemoveObserver(this);
     control_widget_->Close();
+  }
   mouse_watcher_.Stop();
 }
 
