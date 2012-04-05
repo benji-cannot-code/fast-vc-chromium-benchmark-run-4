643f619d697a549c7e218a4eca14491af8b15004
dgreid@chromium.org <dgreid@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 18:58:50 +0000

    Add ability to create a platform thread with a priority.
    Add a function to create a thread with a given priority.  This allows
    for a thread on Linux to set its priority without the need for passing
    around its tid.  SetThreadPriority remains unimplemented on Linux for
    now.  Use the new create function from AudioDeviceThread::Create().
    
    BUG=116172
    TEST=Manual, build and run chrome, check thread priorities with ps
    like this:
    ps -C chrome -Lo pid,tid,pri_api,rtprio,cmd
    
    
    Review URL: http://codereview.chromium.org/9950127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130982 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_posix.cc
+++ base/threading/platform_thread_posix.cc
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_LINUX)
 #include <sys/prctl.h>
+#include <sys/resource.h>
 #include <sys/syscall.h>
+#include <sys/time.h>
 #include <unistd.h>
 #endif
 
@@ -70,7 +72,8 @@ void* ThreadFunc(void* params) {
 
 bool CreateThread(size_t stack_size, bool joinable,
                   PlatformThread::Delegate* delegate,
-                  PlatformThreadHandle* thread_handle) {
+                  PlatformThreadHandle* thread_handle,
+                  ThreadPriority priority) {
 #if defined(OS_MACOSX)
   base::InitThreading();
 #endif  // OS_MACOSX
@@ -123,6 +126,24 @@ bool CreateThread(size_t stack_size, bool joinable,
   params->delegate = delegate;
   params->joinable = joinable;
   success = !pthread_create(thread_handle, &attributes, ThreadFunc, params);
+
+  if (priority != kThreadPriority_Normal) {
+#if defined(OS_LINUX)
+    if (priority == kThreadPriority_RealtimeAudio) {
+      // Linux isn't posix compliant with setpriority(2), it will set a thread
+      // priority if it is passed a tid, not affecting the rest of the threads
+      // in the process.  Setting this priority will only succeed if the user
+      // has been granted permission to adjust nice values on the system.
+      const int kNiceSetting = -10;
+      if (setpriority(PRIO_PROCESS, PlatformThread::CurrentId(), kNiceSetting))
+        DVLOG(1) << "Failed to set nice value of thread to " << kNiceSetting;
+    } else {
+      NOTREACHED() << "Unknown thread priority.";
+    }
+#else
+    PlatformThread::SetThreadPriority(*thread_handle, priority);
+#endif
+  }
 
   pthread_attr_destroy(&attributes);
   if (!success)
@@ -227,7 +248,15 @@ const char* PlatformThread::GetName() {
 bool PlatformThread::Create(size_t stack_size, Delegate* delegate,
                             PlatformThreadHandle* thread_handle) {
   return CreateThread(stack_size, true /* joinable thread */,
-                      delegate, thread_handle);
+                      delegate, thread_handle, kThreadPriority_Normal);
+}
+
+// static
+bool PlatformThread::CreateWithPriority(size_t stack_size, Delegate* delegate,
+                                        PlatformThreadHandle* thread_handle,
+                                        ThreadPriority priority) {
+  return CreateThread(stack_size, true,  // joinable thread
+                      delegate, thread_handle, priority);
 }
 
 // static
@@ -235,7 +264,7 @@ bool PlatformThread::CreateNonJoinable(size_t stack_size, Delegate* delegate) {
   PlatformThreadHandle unused;
 
   bool result = CreateThread(stack_size, false /* non-joinable thread */,
-                             delegate, &unused);
+                             delegate, &unused, kThreadPriority_Normal);
   return result;
 }
 
