fb3210c663d55bdbebaa14470f9554d22c4e1887
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 07:10:58 +0000

    Change the cpp wrappers of audio input/video capture to use CompletionCallbackWithOutput.
    And also fix a few bugs in the PP_ArrayOutput and CallbackWithOutput code.
    - Make sure POD members of Dispatcher\.* are properly initialized.
    - Avoid leaking var/resource references in DispatcherWithOutput\.*::operator().
    
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9965080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130849 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/array_output.h
+++ ppapi/cpp/array_output.h
@@ -202,6 +202,7 @@ class ArrayOutputAdapterWithStorage : public ArrayOutputAdapter<T> {
 class VarArrayOutputAdapterWithStorage : public ArrayOutputAdapter<PP_Var> {
  public:
   VarArrayOutputAdapterWithStorage();
+  virtual ~VarArrayOutputAdapterWithStorage();
 
   // Returns the final array of resource objects, converting the PP_Vars
   // written by the browser to pp::Var objects.
@@ -236,6 +237,13 @@ class ResourceArrayOutputAdapterWithStorage
  public:
   ResourceArrayOutputAdapterWithStorage() {
     set_output(&temp_storage_);
+  }
+
+  virtual ~ResourceArrayOutputAdapterWithStorage() {
+    if (!temp_storage_.empty()) {
+      // An easy way to release the resource references held by this object.
+      output();
+    }
   }
 
   // Returns the final array of resource objects, converting the PP_Resources
