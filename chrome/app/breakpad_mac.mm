56cda243f8cd98df8128f53e92e8594571d53a7a
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 07:27:56 +0000

    Migrate LOG_IF to VLOG_IF in Breakpad code.
    BUG=none
    TEST=Less chatter on the console
    
    
    Review URL: http://codereview.chromium.org/9959143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_mac.mm
+++ chrome/app/breakpad_mac.mm
@@ -175,7 +175,7 @@ void InitCrashReporter() {
   }
 
   if (!enable_breakpad) {
-    LOG_IF(INFO, is_browser) << "Breakpad disabled";
+    VLOG_IF(1, is_browser) << "Breakpad disabled";
     return;
   }
 
