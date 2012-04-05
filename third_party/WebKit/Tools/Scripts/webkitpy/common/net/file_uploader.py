benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 14a4c8b027db5d9e63a0d2c44e95050f0c755447

nrwt is failing to upload test results on the chromium-mac-leopard bots https://bugs.webkit.org/show_bug.cgi?id=83230
Reviewed by Ojan Vafai.

More debugging info and a possible fix - stop messing with the
default network timeout, and don't swallow URLErrors.

* Scripts/webkitpy/common/net/file_uploader.py:
(FileUploader._upload_data):
* Scripts/webkitpy/common/net/networktransaction.py:
(NetworkTransaction.run):
* Scripts/webkitpy/layout_tests/layout_package/json_results_generator.py:
(JSONResultsGeneratorBase.upload_json_files):

git-svn-id: svn://svn.chromium.org/blink/trunk@113271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Tools/Scripts/webkitpy/common/net/file_uploader.py
@@ -29,11 +29,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import codecs
+import logging
 import mimetypes
 import socket
 import urllib2
 
 from webkitpy.common.net.networktransaction import NetworkTransaction
+
+
+_log = logging.getLogger(__name__)
 
 
 def get_mime_type(filename):
@@ -113,8 +117,10 @@ class FileUploader(object):
         response = None
         try:
             # FIXME: We shouldn't mutate global static state.
-            socket.setdefaulttimeout(self._timeout_seconds)
-            response = NetworkTransaction(timeout_seconds=self._timeout_seconds).run(callback)
+            # FIXME: clean this up once we understand what's going on on chromium leopard bots.
+            if not self._debug:
+                socket.setdefaulttimeout(self._timeout_seconds)
+            return NetworkTransaction(timeout_seconds=self._timeout_seconds).run(callback)
         finally:
-            socket.setdefaulttimeout(orig_timeout)
-            return response
+            if not self._debug:
+                socket.setdefaulttimeout(orig_timeout)

third_party/WebKit/Tools/Scripts/webkitpy/common/net/networktransaction.py
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (C) 2010 Google Inc. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -60,6 +60,12 @@ class NetworkTransaction(object):
                 self._check_for_timeout()
                 _log.warn("Received HTTP status %s loading \"%s\".  Retrying in %s seconds..." % (e.code, e.filename, self._backoff_seconds))
                 self._sleep()
+            except urllib2.URLError, e:
+                # FIXME: urllib2 seems to convert socket errors to this.
+                self._check_for_timeout()
+                _log.warn("Received URLError %s.  Retrying in %s seconds..." % (str(s), self._backoff_seconds))
+                self._sleep()
+
 
     def _check_for_timeout(self):
         if self._total_sleep + self._backoff_seconds > self._timeout_seconds:

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/json_results_generator.py
@@ -321,7 +321,10 @@ class JSONResultsGeneratorBase(object):
         uploader = FileUploader(url, 120, debug=True)
         try:
             response = uploader.upload_as_multipart_form_data(self._filesystem, files, attrs)
-            _log.debug("Upload returned %d: '%s'" % (response.code, response.read()))
+            if response:
+                _log.debug("Upload returned %d: '%s'" % (response.code, response.read()))
+            else:
+                _log.debug("Upload returned None")
         except Exception, err:
             _log.error("Upload failed: %s" % err)
             return

commit 05f514c039dd8d32e4d2021077226f7dd4f9eb64

Fix typo introduced in r113271.
Unreviewed, build fix.

* Scripts/webkitpy/common/net/file_uploader.py:
(FileUploader._upload_data.callback):
(FileUploader):

git-svn-id: svn://svn.chromium.org/blink/trunk@113277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Tools/Scripts/webkitpy/common/net/file_uploader.py
@@ -109,7 +109,7 @@ class FileUploader(object):
     def _upload_data(self, content_type, data):
         def callback():
             if self._debug:
-                _log.debug("uploading %d bytes to '%s', content-type '%s'" % len(data), self._url, content_type)
+                _log.debug("uploading %d bytes to '%s', content-type '%s'" % (len(data), self._url, content_type))
             request = urllib2.Request(self._url, data, {"Content-Type": content_type})
             return urllib2.urlopen(request)
 