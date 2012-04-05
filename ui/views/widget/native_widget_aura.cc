5b6034938153b544ca70158c65dc2ca7734866f8
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 18:11:21 +0000

    Aura/ash split: Desktop linux clients should respsect window manager close window events.
    BUG=121725
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9969126
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130971 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -180,6 +180,7 @@ void NativeWidgetAura::InitNativeWidget(const Widget::InitParams& params) {
     root_window_.reset(new aura::RootWindow(bounds));
     root_window_->SetEventFilter(
         new aura::DesktopRootWindowEventFilter(root_window_.get()));
+    root_window_->AddRootWindowObserver(this);
 
     aura::client::SetActivationClient(
         root_window_.get(),
@@ -812,6 +813,13 @@ void NativeWidgetAura::OnWindowDestroyed() {
 
 void NativeWidgetAura::OnWindowVisibilityChanged(bool visible) {
   delegate_->OnNativeWidgetVisibilityChanged(visible);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// NativeWidgetAura, aura::RootWindowObserver implementation:
+
+void NativeWidgetAura::OnRootWindowHostClosed(const aura::RootWindow* root) {
+  GetWidget()->Close();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- ui/views/widget/native_widget_aura.h
+++ ui/views/widget/native_widget_aura.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "ui/aura/client/activation_delegate.h"
 #include "ui/aura/client/drag_drop_delegate.h"
+#include "ui/aura/root_window_observer.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/base/events.h"
 #include "ui/views/views_export.h"
@@ -31,6 +32,7 @@ class TooltipManagerAura;
 
 class VIEWS_EXPORT NativeWidgetAura : public internal::NativeWidgetPrivate,
                                       public aura::WindowDelegate,
+                                      public aura::RootWindowObserver,
                                       public aura::client::ActivationDelegate,
                                       public aura::client::DragDropDelegate {
  public:
@@ -149,6 +151,9 @@ class VIEWS_EXPORT NativeWidgetAura : public internal::NativeWidgetPrivate,
   virtual void OnWindowDestroying() OVERRIDE;
   virtual void OnWindowDestroyed() OVERRIDE;
   virtual void OnWindowVisibilityChanged(bool visible) OVERRIDE;
+
+  // Overridden from aura::RootWindowObserver:
+  virtual void OnRootWindowHostClosed(const aura::RootWindow* root) OVERRIDE;
 
   // Overridden from aura::client::ActivationDelegate:
   virtual bool ShouldActivate(const aura::Event* event) OVERRIDE;
