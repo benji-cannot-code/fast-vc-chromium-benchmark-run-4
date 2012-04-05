719c226186ab1811663296a1f423b6477aa4b43b
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 11:50:35 +0000

    IsCommentNode flags is a waste. https://bugs.webkit.org/show_bug.cgi?id=83251
    Reviewed by Ryosuke Niwa.
    
    Effectively nobody uses it. To save a precious NodeFlags bit, this
    change removes NodeFlags::IsCommentFlag and its companion mehtod
    Node::isCommentNode().
    
    No new tests. No behaviour change.
    
    * dom/CharacterData.h:
    (WebCore::CharacterData::CharacterData):
    * dom/Comment.cpp:
    (WebCore::Comment::Comment):
    * dom/Node.h:
    * rendering/RenderTreeAsText.cpp:
    (WebCore::getTagName):
    This was the only client of Node::isCommentNode(). Replaced it with nodeType() call.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Comment.cpp
+++ third_party/WebKit/Source/WebCore/dom/Comment.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 inline Comment::Comment(Document* document, const String& text)
-    : CharacterData(document, text, CreateComment)
+    : CharacterData(document, text, CreateOther)
 {
 }
 
