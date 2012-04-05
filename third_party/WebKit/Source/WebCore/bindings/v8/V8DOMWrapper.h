7b511c8f43d50c4c3c861d94b284bde7e061ec04
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 01:38:28 +0000

    Use PassRefPtr in V8DOMWrapper interface to avoid explicit ref() calls https://bugs.webkit.org/show_bug.cgi?id=82238
    Reviewed by Adam Barth.
    
    Relanding r112163 without modification, as it still seems valid.
    Will watch Chrome Canaries closely for any memory issues.
    
    The setJSWrapper* methods previously featured a comment that asked
    callers to ref the objects before passing them in. This change makes
    that contract explicit (and allows the removal of the comment).
    
    In addition, for ConstructorCallbacks, this change slightly reduces
    refcount churn by passing on the initial ref via RefPtr::release().
    
    No new tests, no change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateConstructorCallback): Use RefPtr::release() to avoid refcount churn and remove explicit ref() call.
    (GenerateNamedConstructorCallback): ditto.
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::V8DOMWindowShell::installDOMWindow): Cast to a PassRefPtr and remove explicit ref call.
    * bindings/v8/V8DOMWrapper.cpp:
    (WebCore::V8DOMWrapper::setJSWrapperForDOMNode): Pass leaked refs into the DOMNodeMaps.
    * bindings/v8/V8DOMWrapper.h:
    (V8DOMWrapper): Make the setJSWrapperFor* methods take PassRefPtr<T>.
    (WebCore::V8DOMWrapper::setJSWrapperForDOMObject): Pass leaked ref into the DOMObjectMap.
    (WebCore::V8DOMWrapper::setJSWrapperForActiveDOMObject): Pass leaked ref into the ActiveDOMObjectMap.
    * bindings/v8/V8Proxy.h:
    (WebCore::toV8): Remove explicit ref.
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::initContextIfNeeded): Cast to a PassRefPTr and remove explicit ref call.
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore::v8HTMLImageElementConstructorCallback): Use RefPtr::release() to avoid refcount churn and remove explicit ref.
    * bindings/v8/custom/V8WebKitMutationObserverCustom.cpp:
    (WebCore::V8WebKitMutationObserver::constructorCallback): ditto.
    * bindings/v8/custom/V8WebSocketCustom.cpp:
    (WebCore::V8WebSocket::constructorCallback): ditto.
    * bindings/v8/custom/V8XMLHttpRequestConstructor.cpp:
    (WebCore::V8XMLHttpRequest::constructorCallback): ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113272 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NodeFilter.h"
 #include "PlatformString.h"
 #include "V8CustomXPathNSResolver.h"
+#include "V8DOMMap.h"
 #include "V8Event.h"
 #include "V8IsolatedContext.h"
 #include "V8Utilities.h"
@@ -47,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XPathNSResolver.h"
 #include <v8.h>
 #include <wtf/MainThread.h>
+#include <wtf/PassRefPtr.h>
 
 namespace WebCore {
 
@@ -105,10 +107,9 @@ namespace WebCore {
         static v8::Local<v8::Function> constructorForType(WrapperTypeInfo*, WorkerContext*);
 #endif
 
-        // Set JS wrapper of a DOM object, the caller in charge of increase ref.
-        static void setJSWrapperForDOMObject(void*, v8::Persistent<v8::Object>);
-        static void setJSWrapperForActiveDOMObject(void*, v8::Persistent<v8::Object>);
-        static void setJSWrapperForDOMNode(Node*, v8::Persistent<v8::Object>);
+        template<typename T> static void setJSWrapperForDOMObject(PassRefPtr<T>, v8::Persistent<v8::Object>);
+        template<typename T> static void setJSWrapperForActiveDOMObject(PassRefPtr<T>, v8::Persistent<v8::Object>);
+        static void setJSWrapperForDOMNode(PassRefPtr<Node>, v8::Persistent<v8::Object>);
 
         static bool isValidDOMObject(v8::Handle<v8::Value>);
 
@@ -152,6 +153,22 @@ namespace WebCore {
         static V8BindingPerContextData* perContextData(WorkerContext*);
 #endif
     };
+
+    template<typename T>
+    void V8DOMWrapper::setJSWrapperForDOMObject(PassRefPtr<T> object, v8::Persistent<v8::Object> wrapper)
+    {
+        ASSERT(maybeDOMWrapper(wrapper));
+        ASSERT(!domWrapperType(wrapper)->toActiveDOMObjectFunction);
+        getDOMObjectMap().set(object.leakRef(), wrapper);
+    }
+
+    template<typename T>
+    void V8DOMWrapper::setJSWrapperForActiveDOMObject(PassRefPtr<T> object, v8::Persistent<v8::Object> wrapper)
+    {
+        ASSERT(maybeDOMWrapper(wrapper));
+        ASSERT(domWrapperType(wrapper)->toActiveDOMObjectFunction);
+        getActiveDOMObjectMap().set(object.leakRef(), wrapper);
+    }
 }
 
 #endif // V8DOMWrapper_h
