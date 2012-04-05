614b319b0a1fa2abdd7c19de9807d305566c683e
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 22:34:15 +0000

    Ash: Update window transparency values
    Sets the transparency to 10% for active windows, 20% for inactive windows, and 80% for solo (single) windows including maximized windows.
    
    BUG=122176
    TEST=visual
    R=sky@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10008020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131030 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/frame_painter.cc
+++ ash/wm/frame_painter.cc
@@ -126,9 +126,9 @@ bool IsVisibleNormalWindow(aura::Window* window) {
 namespace ash {
 
 // static
-int FramePainter::kActiveWindowOpacity = 255;
-int FramePainter::kInactiveWindowOpacity = 166;
-int FramePainter::kSoloWindowOpacity = 230;
+int FramePainter::kActiveWindowOpacity = 230;  // "Linus-approved" values
+int FramePainter::kInactiveWindowOpacity = 204;
+int FramePainter::kSoloWindowOpacity = 51;
 std::set<FramePainter*>* FramePainter::instances_ = NULL;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -569,9 +569,6 @@ int FramePainter::GetTitleOffsetX() const {
 }
 
 bool FramePainter::UseSoloWindowHeader(aura::Window* ignore) const {
-  // Maximized windows don't use solo window transparency.
-  if (frame_ && frame_->IsMaximized())
-    return false;
   // In unit tests this can be called after the shell is destroyed.
   if (!Shell::HasInstance())
     return false;
