4a0a35476e46b1afa0635d867581db8331e3e312
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 10:38:06 +0000

    Make URLMatcherConditionSet refcounted
    This CL makes the URLMatcherConditionSet refcounted so that we don't need to copy-by-value it anymore in stl containers. This is a preparation for extending it by additional criteria for filtering schemas and ports.
    
    BUG=112155
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/9986001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130887 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative_webrequest/webrequest_condition.h
+++ chrome/browser/extensions/api/declarative_webrequest/webrequest_condition.h
@@ -37,7 +37,7 @@ namespace extensions {
 class WebRequestCondition {
  public:
   WebRequestCondition(
-      const URLMatcherConditionSet& url_matcher_conditions,
+      scoped_refptr<URLMatcherConditionSet> url_matcher_conditions,
       const WebRequestConditionAttributes& condition_attributes);
   ~WebRequestCondition();
 
@@ -57,14 +57,14 @@ class WebRequestCondition {
   // This ID is registered in a URLMatcher that can inform us in case of a
   // match.
   URLMatcherConditionSet::ID url_matcher_condition_set_id() const {
-    return url_matcher_conditions_.id();
+    return url_matcher_conditions_->id();
   }
 
   // Returns the set of conditions that are checked on the URL. This is the
   // primary trigger for WebRequestCondition and therefore never empty.
   // (If it was empty, the URLMatcher would never notify us about network
   // requests which might fulfill the entire WebRequestCondition).
-  const URLMatcherConditionSet& url_matcher_condition_set() const {
+  scoped_refptr<URLMatcherConditionSet> url_matcher_condition_set() const {
     return url_matcher_conditions_;
   }
 
@@ -81,7 +81,7 @@ class WebRequestCondition {
       const base::Value* value,
       std::string* error);
 
-  URLMatcherConditionSet url_matcher_conditions_;
+  scoped_refptr<URLMatcherConditionSet> url_matcher_conditions_;
   WebRequestConditionAttributes condition_attributes_;
 
   // Bit vector indicating all RequestStages during which all
@@ -125,7 +125,7 @@ class WebRequestConditionSet {
 
   // Appends the URLMatcherConditionSet from all conditions to |condition_sets|.
   void GetURLMatcherConditionSets(
-      std::vector<URLMatcherConditionSet>* condition_sets) const;
+      URLMatcherConditionSet::Vector* condition_sets) const;
 
  private:
   typedef std::vector<linked_ptr<WebRequestCondition> > Conditions;
