c42b24fe2baba198b38d1c7cb106b83997bf538a
charles.wei@torchmobile.com.cn <charles.wei@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 03:43:21 +0000

    Need using namespace std for system calls in MIMESniffing.cpp https://bugs.webkit.org/show_bug.cgi?id=82706
    Reviewed by Antonio Gomes.
    
    No new tests, just fix the compile error for BlackBerry porting.
    
    * platform/network/MIMESniffing.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/MIMESniffing.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/MIMESniffing.cpp
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 #include <stdint.h>
 
+using namespace std;
+
 // MIME type sniffing implementation based on http://tools.ietf.org/html/draft-abarth-mime-sniff-06
 
 namespace {
