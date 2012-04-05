478a7c10ab7d5c32c0fedb39a76c3e003370fc57
craig.schlenter@chromium.org <craig.schlenter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 20:33:22 +0000

    Fix blob storage internals CSP typo.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10009017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131004 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/blob/view_blob_internals_job.cc
+++ webkit/blob/view_blob_internals_job.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,7 +40,7 @@ void StartHTML(std::string* out) {
       "<!DOCTYPE HTML>"
       "<html><title>Blob Storage Internals</title>"
       "<meta http-equiv=\"X-WebKit-CSP\""
-      "  content=\"obejct-src 'none'; script-src 'none'\">\n"
+      "  content=\"object-src 'none'; script-src 'none'\">\n"
       "<style>\n"
       "body { font-family: sans-serif; font-size: 0.8em; }\n"
       "tt, code, pre { font-family: WebKitHack, monospace; }\n"
