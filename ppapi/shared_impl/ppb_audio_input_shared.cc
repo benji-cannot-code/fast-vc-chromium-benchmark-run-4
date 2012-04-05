8142c028eddb67ebe5a8eff6f2caccf849e2011e
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 17:56:56 +0000

    Update PPB_AudioInput_Shared to work with the new format of the shared memory buffer.
    BUG=None
    TEST=ppapi/examples/audio_input doesn't crash in debug build.
    
    
    Review URL: http://codereview.chromium.org/9963128
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130965 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/ppb_audio_input_shared.cc
+++ ppapi/shared_impl/ppb_audio_input_shared.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/shared_impl/ppb_audio_input_shared.h"
 
 #include "base/logging.h"
+#include "media/audio/audio_parameters.h"
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/shared_impl/ppapi_globals.h"
 #include "ppapi/shared_impl/ppb_device_ref_shared.h"
@@ -226,13 +227,22 @@ void PPB_AudioInput_Shared::StartThread() {
 }
 
 void PPB_AudioInput_Shared::Run() {
+  // The shared memory represents AudioInputBufferParameters and the actual data
+  // buffer.
+  media::AudioInputBuffer* buffer =
+      static_cast<media::AudioInputBuffer*>(shared_memory_->memory());
+  uint32_t data_buffer_size =
+      shared_memory_size_ - sizeof(media::AudioInputBufferParameters);
   int pending_data;
-  void* buffer = shared_memory_->memory();
 
   while (sizeof(pending_data) == socket_->Receive(&pending_data,
                                                   sizeof(pending_data)) &&
          pending_data >= 0) {
-    audio_input_callback_(buffer, shared_memory_size_, user_data_);
+    // While closing the stream, we may receive buffers whose size is different
+    // from |data_buffer_size|.
+    CHECK_LE(buffer->params.size, data_buffer_size);
+    if (buffer->params.size > 0)
+      audio_input_callback_(&buffer->audio[0], buffer->params.size, user_data_);
   }
 }
 
