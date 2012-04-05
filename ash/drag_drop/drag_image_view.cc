f9fc302d25772624a7554914cab713c9535d313b
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 22:14:10 +0000

    ash: Use TYPE_WINDOW_FRAMELESS for drag widget.
    Removes the shadow artifact from drag widgets and makes them
    match the apperance on MacOSX and Linux.
    
    BUG=118022
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9963137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131022 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/drag_drop/drag_image_view.cc
+++ ash/drag_drop/drag_image_view.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,7 @@ using views::Widget;
 Widget* CreateDragWidget() {
   Widget* drag_widget = new Widget;
   Widget::InitParams params;
-  params.type = Widget::InitParams::TYPE_TOOLTIP;
+  params.type = Widget::InitParams::TYPE_WINDOW_FRAMELESS;
   params.keep_on_top = true;
   params.accept_events = false;
   params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
