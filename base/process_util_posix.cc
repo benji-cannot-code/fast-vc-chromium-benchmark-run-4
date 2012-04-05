deff4af260b531dc390b32f359a2cdfbd92d9dc9
halyavin@google.com <halyavin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 11:38:36 +0000

    Add #include <iterator> to files that use std::back_inserter. See http://code.google.com/p/chromium/source/search?q=back_inserter+-%23include%5C+%5C%3Citerator%5C%3E&origq=back_inserter+-%23include%5C+%5C%3Citerator%5C%3E&btnG=Search+Trunk
    BUG= none
    TEST= none
    
    Review URL: http://codereview.chromium.org/9958156
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130893 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_posix.cc
+++ base/process_util_posix.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include <iterator>
 #include <limits>
 #include <set>
 
--- chrome/browser/autocomplete/autocomplete.cc
+++ chrome/browser/autocomplete/autocomplete.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete.h"
 
 #include <algorithm>
+#include <iterator>
 #include <set>
 
 #include "base/basictypes.h"
