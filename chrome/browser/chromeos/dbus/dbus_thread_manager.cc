d1a0f9f57c05724a539c969c85012069c4192da1
sleffler@chromium.org <sleffler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 07:27:34 +0000

    chromeos: add client support for debugd systrace support
    Add support for communicating with the debugd service and enable access
    to use systrace methods.
    
    BUG=chromium-os:27809
    TEST=use new about:tracing support to collect system event data
    
    Change-Id: I1b6ed75f3ece960c77527e7b9d3a34e5cb76401e
    
    
    Review URL: http://codereview.chromium.org/9700073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/dbus_thread_manager.cc
+++ chrome/browser/chromeos/dbus/dbus_thread_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dbus/dbus_client_implementation_type.h"
 #include "chrome/browser/chromeos/dbus/cros_disks_client.h"
 #include "chrome/browser/chromeos/dbus/cryptohome_client.h"
+#include "chrome/browser/chromeos/dbus/debug_daemon_client.h"
 #include "chrome/browser/chromeos/dbus/flimflam_ipconfig_client.h"
 #include "chrome/browser/chromeos/dbus/flimflam_network_client.h"
 #include "chrome/browser/chromeos/dbus/flimflam_profile_client.h"
@@ -73,6 +74,9 @@ class DBusThreadManagerImpl : public DBusThreadManager {
     // Create the Cryptohome client.
     cryptohome_client_.reset(
         CryptohomeClient::Create(client_type, system_bus_.get()));
+    // Create the debugdaemon client.
+    debugdaemon_client_.reset(
+        DebugDaemonClient::Create(client_type, system_bus_.get()));
     // Create the Flimflam IPConfig client.
     flimflam_ipconfig_client_.reset(
         FlimflamIPConfigClient::Create(client_type, system_bus_.get()));
@@ -157,6 +161,11 @@ class DBusThreadManagerImpl : public DBusThreadManager {
   }
 
   // DBusThreadManager override.
+  virtual DebugDaemonClient* GetDebugDaemonClient() OVERRIDE {
+    return debugdaemon_client_.get();
+  }
+
+  // DBusThreadManager override.
   virtual FlimflamIPConfigClient* GetFlimflamIPConfigClient() OVERRIDE {
     return flimflam_ipconfig_client_.get();
   }
@@ -210,6 +219,7 @@ class DBusThreadManagerImpl : public DBusThreadManager {
   scoped_ptr<CashewClient> cashew_client_;
   scoped_ptr<CrosDisksClient> cros_disks_client_;
   scoped_ptr<CryptohomeClient> cryptohome_client_;
+  scoped_ptr<DebugDaemonClient> debugdaemon_client_;
   scoped_ptr<FlimflamIPConfigClient> flimflam_ipconfig_client_;
   scoped_ptr<FlimflamNetworkClient> flimflam_network_client_;
   scoped_ptr<FlimflamProfileClient> flimflam_profile_client_;
