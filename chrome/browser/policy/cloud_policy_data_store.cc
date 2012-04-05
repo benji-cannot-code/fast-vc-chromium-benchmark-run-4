366d91a45d8f2309debc405d83aba496d78c021a
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 12:19:05 +0000

    Fix race on enterprise enrollment.
    The policy fetch was racing against the installation-time attributes
    write. Enrollment would fail if the policy fetch won the race. This
    change explicitly prevents policy fetches until the device policy
    machinery is ready to accept them.
    
    BUG=chromium:28248
    TEST=Hard. Enroll devices a lot, preferably with a slow TPM and against a fast server. Enrollment shouldn't fail.
    
    Review URL: https://chromiumcodereview.appspot.com/9982005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130897 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud_policy_data_store.cc
+++ chrome/browser/policy/cloud_policy_data_store.cc
@@ -35,13 +35,14 @@ CloudPolicyDataStore::CloudPolicyDataStore(
       policy_type_(policy_type),
       known_machine_id_(false),
       token_cache_loaded_(false),
+      policy_fetching_enabled_(true),
       device_mode_(DEVICE_MODE_PENDING) {}
 
 void CloudPolicyDataStore::SetDeviceToken(const std::string& device_token,
                                           bool from_cache) {
   DCHECK(token_cache_loaded_ != from_cache);
   if (!token_cache_loaded_) {
-    // The cache should be the first to set the token. (It may be "")
+    // The cache should be the first to set the token (it may be empty).
     DCHECK(from_cache);
     token_cache_loaded_ = true;
   } else {
@@ -113,6 +114,11 @@ void CloudPolicyDataStore::set_known_machine_id(bool known_machine_id) {
   known_machine_id_ = known_machine_id;
 }
 
+void CloudPolicyDataStore::set_policy_fetching_enabled(
+    bool policy_fetching_enabled) {
+  policy_fetching_enabled_ = policy_fetching_enabled;
+}
+
 void CloudPolicyDataStore::set_device_mode(DeviceMode device_mode) {
   device_mode_ = device_mode;
 }
@@ -152,6 +158,10 @@ const std::string& CloudPolicyDataStore::policy_type() const {
 
 bool CloudPolicyDataStore::token_cache_loaded() const {
   return token_cache_loaded_;
+}
+
+bool CloudPolicyDataStore::policy_fetching_enabled() const {
+  return policy_fetching_enabled_;
 }
 
 const std::string& CloudPolicyDataStore::user_name() const {
