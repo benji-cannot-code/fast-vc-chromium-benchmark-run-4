6d5942e0e2da16298f04d3ba6fef67445206d065
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-05 03:36:12 +0000

    Switch web intents API to be vendor-prefixed https://bugs.webkit.org/show_bug.cgi?id=83172
    Patch by Greg Billock <gbillock@google.com> on 2012-04-04
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    * Modules/intents/DOMWindowIntents.idl:
    * Modules/intents/NavigatorIntents.cpp:
    (WebCore::NavigatorIntents::webkitStartActivity):
    * Modules/intents/NavigatorIntents.h:
    (NavigatorIntents):
    * Modules/intents/NavigatorIntents.idl:
    
    LayoutTests:
    
    * webintents/resources/web-intents-reload-orig.html:
    * webintents/resources/web-intents-testing.js:
    (startIntentWithCallbacks):
    * webintents/web-intents-api-expected.txt:
    * webintents/web-intents-api.html:
    * webintents/web-intents-invoke.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/intents/NavigatorIntents.h
+++ third_party/WebKit/Source/WebCore/Modules/intents/NavigatorIntents.h
@@ -41,7 +41,7 @@ typedef int ExceptionCode;
 
 class NavigatorIntents {
 public:
-    static void startActivity(Navigator*, PassRefPtr<Intent>, PassRefPtr<IntentResultCallback> successCallback, PassRefPtr<IntentResultCallback> errorCallback, ExceptionCode&);
+    static void webkitStartActivity(Navigator*, PassRefPtr<Intent>, PassRefPtr<IntentResultCallback> successCallback, PassRefPtr<IntentResultCallback> errorCallback, ExceptionCode&);
 
 private:
     NavigatorIntents();
