a292006f2c3452042b859010fe9913940dfdeba4
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 03:51:10 +0000

    Make sure that scoped_ptr<> cannot be used with ref-counted objects.
    It's easy to type scoped_ptr<> instead of scoped_refptr<>. Such bugs are
    hard to debug sometimes. This change adds compile-time check to make sure 
    that scoped_ptr<> is not used for ref-counted objects.
    
    Also fixed one (benign) instance of scoped_ptr<> being used for a 
    ref-counted object.
    
    Review URL: http://codereview.chromium.org/9958150
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130835 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/scoped_ptr.h
+++ base/memory/scoped_ptr.h
@@ -97,8 +97,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdlib.h>
 
+#include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/move.h"
+#include "base/template_util.h"
+
+namespace base {
+
+namespace subtle {
+class RefCountedBase;
+class RefCountedThreadSafeBase;
+}  // namespace subtle
+
+namespace internal {
+
+template <typename T> struct IsNotRefCounted {
+  enum {
+    value = !base::is_convertible<T*, base::subtle::RefCountedBase*>::value &&
+        !base::is_convertible<T*, base::subtle::RefCountedThreadSafeBase*>::
+            value
+  };
+};
+
+}  // namespace internal
+}  // namespace base
 
 // A scoped_ptr<T> is like a T*, except that the destructor of scoped_ptr<T>
 // automatically deletes the pointer it holds (if any).
@@ -112,6 +134,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 template <class C>
 class scoped_ptr {
   MOVE_ONLY_TYPE_FOR_CPP_03(scoped_ptr, RValue)
+
+  COMPILE_ASSERT(base::internal::IsNotRefCounted<C>::value,
+                 C_is_refcounted_type_and_needs_scoped_refptr);
 
  public:
 
--- base/memory/scoped_ptr_unittest.nc
+++ base/memory/scoped_ptr_unittest.nc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/ref_counted.h"
 
 namespace {
 
@@ -14,12 +15,22 @@ class Parent {
 class Child : public Parent {
 };
 
+class RefCountedClass : public base::RefCountedThreadSafe<RefCountedClass> {
+};
+
 }  // namespace
 
 #if defined(NCTEST_NO_PASSAS_DOWNCAST)  // [r"invalid conversion from"]
 
 scoped_ptr<Child> DowncastUsingPassAs(scoped_ptr<Parent> object) {
   return object.PassAs<Child>();
+}
+
+#elif defined(NCTEST_NO_REF_COUNTED_SCOPED_PTR)  // [r"creating array with negative size"]
+
+// scoped_ptr<> should not work for ref-counted objects.
+void WontCompile() {
+  scoped_ptr<RefCountedClass> x;
 }
 
 #endif
