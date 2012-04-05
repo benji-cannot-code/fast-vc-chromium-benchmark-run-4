5950be28a0eb392d81a3c5d81d7cc2f0a5d2a1d2
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 15:17:35 +0000

    Move code from internet options to internet details.
    BUG=120211
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9864039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/options.js
+++ chrome/browser/resources/options2/options.js
@@ -127,8 +127,6 @@ function load() {
                                 [$('account-picture')]);
     OptionsPage.registerOverlay(DetailsInternetPage.getInstance(),
                                 BrowserOptions.getInstance());
-    OptionsPage.registerOverlay(InternetOptions.getInstance(),
-                                BrowserOptions.getInstance());
     OptionsPage.registerOverlay(KeyboardOverlay.getInstance(),
                                 BrowserOptions.getInstance(),
                                 [$('keyboard-settings-button')]);
@@ -196,13 +194,6 @@ function load() {
   if (path.length > 1) {
     // Skip starting slash and remove trailing slash (if any).
     var pageName = path.slice(1).replace(/\/$/, '');
-
-    if (pageName == 'proxy') {
-      // The following page doesn't have a unique URL at the moment, so do
-      // something sensible if a user pastes this link or refreshes on this URL.
-      pageName = ProxyOptions ? ProxyOptions.getInstance().parentPage.name :
-                                AdvancedOptions.getInstance().name;
-    }
     OptionsPage.showPageByName(pageName, true, {replaceState: true});
   } else {
     OptionsPage.showDefaultPage();
