d028eb9fc4c23194455bcc9f4d8890a4b0174e40
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 04:43:05 +0000

    Do not start 'dragging' of partial screenshot when mouse is pressed.
    In case that mouse is just pressed and released immediately, it should
    not take screenshot.
    
    BUG=120475
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10006001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131100 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/partial_screenshot_view.cc
+++ ash/wm/partial_screenshot_view.cc
@@ -88,13 +88,13 @@ void PartialScreenshotView::OnPaint(gfx::Canvas* canvas) {
 
 bool PartialScreenshotView::OnMousePressed(const views::MouseEvent& event) {
   start_position_ = event.location();
-  is_dragging_ = true;
   return true;
 }
 
 bool PartialScreenshotView::OnMouseDragged(const views::MouseEvent& event) {
   current_position_ = event.location();
   SchedulePaint();
+  is_dragging_ = true;
   return true;
 }
 
@@ -104,8 +104,11 @@ bool PartialScreenshotView::OnMouseWheel(const views::MouseWheelEvent& event) {
 }
 
 void PartialScreenshotView::OnMouseReleased(const views::MouseEvent& event) {
-  is_dragging_ = false;
   Cancel();
+  if (!is_dragging_)
+    return;
+
+  is_dragging_ = false;
   if (screenshot_delegate_) {
     aura::RootWindow *root_window = Shell::GetRootWindow();
     screenshot_delegate_->HandleTakePartialScreenshot(
