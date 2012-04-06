c7f983ae04bd2e241849d9db8a3d24e938ce916a
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 22:08:45 +0000

    Delete renderbuffers when no longer referenced
    TEST=unit tests
    BUG=122007
    
    R=apatrick@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10000001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131197 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2388,8 +2388,6 @@ void GLES2DecoderImpl::DeleteRenderbuffersHelper(
         }
       }
       state_dirty_ = true;
-      GLuint service_id = renderbuffer->service_id();
-      glDeleteRenderbuffersEXT(1, &service_id);
       RemoveRenderbufferInfo(client_ids[ii]);
     }
   }
