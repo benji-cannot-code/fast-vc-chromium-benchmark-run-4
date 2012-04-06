640b26159199e53c66996923417645f58ff77560
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 19:35:32 +0000

    Adding extraction of is_calculating_battery_time field from protobuf
    This CL adds in extracting the value from the incomming protobuf and storing it
    in an instance of PowerSupplyStatus.
    
    BUG=chromium-os:28881
    TEST=Confirmed that build still worked.
    
    Review URL: https://chromiumcodereview.appspot.com/9977005
    Patch from Ryan Harrison <rharrison@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131166 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/power/power_supply_status.h
+++ ash/system/power/power_supply_status.h
@@ -33,6 +33,8 @@ struct ASH_EXPORT PowerSupplyStatus {
 
   double battery_percentage;
 
+  bool is_calculating_battery_time;
+
   PowerSupplyStatus();
   std::string ToString() const;
 };
--- chromeos/dbus/power_supply_status.h
+++ chromeos/dbus/power_supply_status.h
@@ -25,6 +25,8 @@ struct CHROMEOS_EXPORT PowerSupplyStatus {
 
   double battery_percentage;
 
+  bool is_calculating_battery_time;
+
   PowerSupplyStatus();
   std::string ToString() const;
 };
