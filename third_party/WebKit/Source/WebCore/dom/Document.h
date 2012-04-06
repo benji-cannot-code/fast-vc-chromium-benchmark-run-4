f49249a7d8b3adf99c3b3b71c9c8781d081831e4
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-06 22:15:02 +0000

    Block cross-origin iframe scroll to fragment. https://bugs.webkit.org/show_bug.cgi?id=73083
    Reviewed by Adam Barth.
    
    Add a restriction similar to what FF has done for all iframes for over a
    year now. Our change is less disruptive in that it only does this in the
    cross-orgin case, which is where the fragment scrolling is problematic.
    
    Source/WebCore:
    
    Test: http/tests/navigation/anchor-frames-cross-origin.html
    
    * dom/Document.cpp:
    (WebCore::Document::canBeAccessedByEveryAncestorFrame):
    (WebCore):
    * dom/Document.h:
    (Document):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::finishedParsing):
    (WebCore::FrameLoader::loadInSameDocument):
    (WebCore::FrameLoader::loadURL):
    (WebCore::FrameLoader::loadWithDocumentLoader):
    (WebCore::FrameLoader::shouldPerformFragmentNavigation):
    (WebCore::FrameLoader::scrollToFragmentIfAllowed):
    (WebCore):
    * loader/FrameLoader.h:
    (FrameLoader):
    
    LayoutTests:
    
    * http/tests/inspector/resource-parameters-expected.txt:
    * http/tests/navigation/anchor-frames-cross-origin-expected.txt: Added.
    * http/tests/navigation/anchor-frames-cross-origin.html: Added.
    * http/tests/navigation/resources/frame-with-anchor-cross-origin.html: Added.
    * http/tests/security/xssAuditor/anchor-url-dom-write-location-expected.txt:
    * http/tests/security/xssAuditor/anchor-url-dom-write-location-inline-event-expected.txt:
    * http/tests/security/xssAuditor/anchor-url-dom-write-location-inline-event-null-char-expected.txt:
    * http/tests/security/xssAuditor/anchor-url-dom-write-location-javascript-URL-expected.txt:
    * http/tests/security/xssAuditor/anchor-url-dom-write-location2-expected.txt:
    * http/tests/security/xssAuditor/dom-write-location-inline-event-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -625,6 +625,7 @@ public:
     virtual void disableEval();
 
     bool canNavigate(Frame* targetFrame);
+    bool canBeAccessedByEveryAncestorFrame();
 
     CSSStyleSheet* pageUserSheet();
     void clearPageUserSheet();
