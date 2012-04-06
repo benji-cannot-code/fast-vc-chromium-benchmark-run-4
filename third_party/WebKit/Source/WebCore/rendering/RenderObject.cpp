7a7bf2bac1fe7ebd5c70db1b45bc48cdf41b0192
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-06 20:41:20 +0000

    Unreviewed build fix after r113486 and r113487.
    * rendering/RenderObject.cpp:
    Added missing #include.
    
    * rendering/RenderView.h:
    Removed bad OVERRIDE.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderTableRow.h"
 #include "RenderTheme.h"
 #include "RenderView.h"
+#include "Settings.h"
 #include "TransformState.h"
 #include "htmlediting.h"
 #include <algorithm>
