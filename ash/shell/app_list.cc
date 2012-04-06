32e8be9a4812f5165ceb9fbb0f68d740aa6287db
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 21:50:16 +0000

    chromeos: No icon shadow for slow boards.
    Shadow generate seems causing app list animation janky on slow boards.
    Thus, disable it on those boards.
    
    BUG=122371
    TEST=Verify fix for issue 122371.
    
    
    Review URL: http://codereview.chromium.org/10008057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131193 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/app_list.cc
+++ ash/shell/app_list.cc
@@ -113,6 +113,10 @@ class ExampleAppListViewDelegate : public ash::AppListViewDelegate {
 
  private:
   // Overridden from ash::AppListViewDelegate:
+  virtual bool ShouldGenerateIconShadow() OVERRIDE {
+    return true;
+  }
+
   virtual void SetModel(AppListModel* model) OVERRIDE {
     model_ = model;
   }
