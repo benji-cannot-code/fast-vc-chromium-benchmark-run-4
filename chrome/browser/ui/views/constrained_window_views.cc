671b927562d66e88dccedce301393f212d503dce
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 02:59:09 +0000

    Support non-hierarchy ownership for aura::Windows.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/10007015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131084 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.cc
+++ chrome/browser/ui/views/constrained_window_views.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_switches.h"
 #include "ash/shell.h"
 #include "ash/wm/custom_frame_view_ash.h"
+#include "ash/wm/visibility_controller.h"
 #include "base/command_line.h"
 #include "ui/aura/window.h"
 #endif
@@ -584,6 +585,7 @@ ConstrainedWindowViews::ConstrainedWindowViews(
 #if defined(USE_ASH)
   // Ash window headers can be transparent.
   params.transparent = true;
+  ash::SetChildWindowVisibilityChangesAnimated(params.parent);
 #endif
   Init(params);
 
