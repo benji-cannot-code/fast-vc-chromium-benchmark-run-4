32725ab54270a898e0d90ade60154a63cb4a842f
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 18:42:46 +0000

    Aura/ash split: Hopefully get linux_aura green trybot.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10008023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/select_file_dialog_unittest.cc
+++ chrome/browser/ui/select_file_dialog_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/test/test_browser_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if defined(USE_AURA) && defined(OS_WIN)
+#if defined(USE_AURA)
 // http://crbug.com/105200
 #define MAYBE_ExpectAsynchronousListenerCall DISABLED_ExpectAsynchronousListenerCall
 #else
--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "unicode/rbbi.h"
 #include "unicode/uloc.h"
 
-#if defined(TOOLKIT_GTK)
+#if defined(OS_LINUX)
 #include <glib.h>
 #endif
 
@@ -400,7 +400,7 @@ std::string GetApplicationLocale(const std::string& pref_locale) {
     candidates.push_back(base::i18n::GetConfiguredLocale());
   }
 
-#elif defined(OS_CHROMEOS) || defined(USE_AURA)
+#elif defined(OS_CHROMEOS) || (defined(USE_AURA) && !defined(OS_LINUX))
 
   // On ChromeOS, use the application locale preference.
   if (!pref_locale.empty())
@@ -412,7 +412,8 @@ std::string GetApplicationLocale(const std::string& pref_locale) {
   if (!pref_locale.empty())
     candidates.push_back(pref_locale);
 
-#elif defined(TOOLKIT_GTK)
+#elif defined(OS_LINUX)
+  // If we're on a different Linux system, we have glib.
 
   // GLib implements correct environment variable parsing with
   // the precedence order: LANGUAGE, LC_ALL, LC_MESSAGES and LANG.
