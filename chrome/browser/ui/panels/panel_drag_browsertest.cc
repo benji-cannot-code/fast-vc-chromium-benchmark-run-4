6a2e6bdabf8f0c7d3f3a3807fcb9752435c02999
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 03:18:08 +0000

    Prevent Panels to be draggable under the system menu on Mac.
    BUG=120309
    
    
    Review URL: http://codereview.chromium.org/9950134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131090 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_drag_browsertest.cc
+++ chrome/browser/ui/panels/panel_drag_browsertest.cc
@@ -689,6 +689,7 @@ IN_PROC_BROWSER_TEST_F(PanelDragBrowserTest, CloseDetachedPanelOnDrag) {
 
     // Cancel the drag.
     panel1_testing->CancelDragTitlebar();
+    WaitForBoundsAnimationFinished(panel1);
     EXPECT_FALSE(drag_controller->IsDragging());
 
     ASSERT_EQ(3, detached_strip->num_panels());
