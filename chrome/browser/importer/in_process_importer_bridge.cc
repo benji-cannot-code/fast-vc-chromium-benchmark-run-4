d48f2ae6c8e509895aa5cc2f2234851c5f14694f
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-07 01:39:13 +0000

    include <iterator>, fixes build for non-pch on vs2010
    in_process_importer_bridge uses std::back_inserter, but doesn't #include
    <iterator>. This happens to work on VS 2008 because other standard headers
    include <iterator>, and it works on 2010 with PCH because the PCH includes the
    std headers. However, on 2010 and without PCH it's a compile error.
    
    R=mirandac@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10005053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/in_process_importer_bridge.cc
+++ chrome/browser/importer/in_process_importer_bridge.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/ie7_password.h"
 #endif
 
+#include <iterator>
+
 using content::BrowserThread;
 
 InProcessImporterBridge::InProcessImporterBridge(ProfileWriter* writer,
