26e912a6ad23030b50b80976f1afeb1dafa5c024
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-07 15:30:43 +0000

    [BlackBerry] Add Battery Status API support https://bugs.webkit.org/show_bug.cgi?id=82615
    Reviewed by George Staikos.
    
    .:
    
    Enable BATTERY_STATUS.
    
    * Source/cmake/OptionsBlackBerry.cmake:
    
    Source/WebKit:
    
    Add BatteryClientBlackBerry.cpp to the build.
    
    * PlatformBlackBerry.cmake:
    
    Source/WebKit/blackberry:
    
    Add client implementation for Battery API.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::init):  initialize BatteryClient.
    * WebCoreSupport/BatteryClientBlackBerry.cpp: Added.
    (WebCore):
    (WebCore::BatteryClientBlackBerry::BatteryClientBlackBerry):
    (WebCore::BatteryClientBlackBerry::setController):
    (WebCore::BatteryClientBlackBerry::startUpdating):
    (WebCore::BatteryClientBlackBerry::stopUpdating):
    (WebCore::BatteryClientBlackBerry::batteryControllerDestroyed):
    (WebCore::BatteryClientBlackBerry::onLevelChange):
    (WebCore::BatteryClientBlackBerry::onChargingChange):
    (WebCore::BatteryClientBlackBerry::onChargingTimeChange):
    (WebCore::BatteryClientBlackBerry::onDischargingTimeChange):
    * WebCoreSupport/BatteryClientBlackBerry.h: Added.
    (WebCore):
    (BatteryClientBlackBerry):
    (WebCore::BatteryClientBlackBerry::~BatteryClientBlackBerry):
    
    Tools:
    
    Enable BATTERY_STATUS.
    
    * Scripts/build-webkit:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -26,6 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BackingStoreClient.h"
 #include "BackingStoreCompositingSurface.h"
 #include "BackingStore_p.h"
+#if ENABLE(BATTERY_STATUS)
+#include "BatteryClientBlackBerry.h"
+#endif
 #include "CString.h"
 #include "CachedImage.h"
 #include "Chrome.h"
@@ -449,6 +452,10 @@ void WebPagePrivate::init(const WebString& pageGroupName)
     WebCore::provideDeviceMotionTo(m_page, new DeviceMotionClientBlackBerry(this));
 #if ENABLE(VIBRATION)
     WebCore::provideVibrationTo(m_page, new VibrationClientBlackBerry());
+#endif
+
+#if ENABLE(BATTERY_STATUS)
+    WebCore::provideBatteryTo(m_page, new WebCore::BatteryClientBlackBerry);
 #endif
 
 #if ENABLE(NOTIFICATIONS) || ENABLE(LEGACY_NOTIFICATIONS)
