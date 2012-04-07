89863afdd3bebffbdccb59477459993b6edf3e2b
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-07 10:09:54 +0000

    [net/dns] Handle synchronous completions of DnsUDPAttempt.
    BUG=120976
    TEST=./net_unittests --gtest_filter=DnsTransactionTest.Sync*
    
    Review URL: https://chromiumcodereview.appspot.com/9958082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131271 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/dns_test_util.h
+++ net/dns/dns_test_util.h
@@ -52,6 +52,8 @@ static const char* const kT0IpAddresses[] = {
 };
 static const char kT0CanonName[] = "www.l.google.com";
 static const int kT0TTL = 0x000000e4;
+// +1 for the CNAME record.
+static const unsigned kT0RecordCount = arraysize(kT0IpAddresses) + 1;
 
 //-----------------------------------------------------------------------------
 // Query/response set for codereview.chromium.org, ID is fixed to 1.
@@ -84,6 +86,8 @@ static const char* const kT1IpAddresses[] = {
 };
 static const char kT1CanonName[] = "ghs.l.google.com";
 static const int kT1TTL = 0x0000010b;
+// +1 for the CNAME record.
+static const unsigned kT1RecordCount = arraysize(kT1IpAddresses) + 1;
 
 //-----------------------------------------------------------------------------
 // Query/response set for www.ccs.neu.edu, ID is fixed to 2.
@@ -115,6 +119,8 @@ static const char* const kT2IpAddresses[] = {
 };
 static const char kT2CanonName[] = "vulcan.ccs.neu.edu";
 static const int kT2TTL = 0x0000012c;
+// +1 for the CNAME record.
+static const unsigned kT2RecordCount = arraysize(kT2IpAddresses) + 1;
 
 //-----------------------------------------------------------------------------
 // Query/response set for www.google.az, ID is fixed to 3.
@@ -158,6 +164,8 @@ static const char* const kT3IpAddresses[] = {
 };
 static const char kT3CanonName[] = "www.l.google.com";
 static const int kT3TTL = 0x00000015;
+// +2 for the CNAME records.
+static const unsigned kT3RecordCount = arraysize(kT3IpAddresses) + 2;
 
 class DnsClient;
 // Creates mock DnsClient for testing HostResolverImpl.
