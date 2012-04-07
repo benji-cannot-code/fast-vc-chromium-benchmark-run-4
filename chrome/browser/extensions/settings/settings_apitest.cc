2aede4e0eb3a619a20193e631ab8565938e55375
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-07 06:40:11 +0000

    Disable moar tests whose code is missing on beta.
    TBR=yoz@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10022012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/settings/settings_apitest.cc
+++ chrome/browser/extensions/settings/settings_apitest.cc
@@ -173,7 +173,7 @@ class ExtensionSettingsApiTest : public ExtensionApiTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, SimpleTest) {
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, DISABLED_SimpleTest) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
   ASSERT_TRUE(RunExtensionTest("settings/simple_test")) << message_;
@@ -183,7 +183,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, SimpleTest) {
 // Note that only split-mode incognito is tested, because spanning mode
 // incognito looks the same as normal mode when the only API activity comes
 // from background pages.
-IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, SplitModeIncognito) {
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, DISABLED_SplitModeIncognito) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
 
@@ -210,7 +210,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, SplitModeIncognito) {
 }
 
 IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest,
-    OnChangedNotificationsBetweenBackgroundPages) {
+    DISABLED_OnChangedNotificationsBetweenBackgroundPages) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
 
@@ -236,7 +236,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest,
-    SyncAndLocalAreasAreSeparate) {
+    DISABLED_SyncAndLocalAreasAreSeparate) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
 
