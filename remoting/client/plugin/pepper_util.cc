afd7d993efb8cffe2ea0d865142672c2b6cd948e
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-08 02:58:45 +0000

    Switch PepperView to use asynchronous completion for Flush().
    PepperView was requesting "optional" synchronous/asynchronous completion from Flush(), so as to be able to handle errors, which was not possible with the CompletionCallbackClosureAdapter, since the closure would not receive the result.
    
    This CL replaces CompletionCallbackClosureAdapter with a PpCompletionCallback() function that accepts a base::Callback<void(int)> and returns a pp::CompletionCallback suitable for passing directly to Flush().  PepperView is also updated to use the new helper, and to expect Flush() to always complete asynchronously.
    
    TEST=Chromoting client renders correctly.
    
    
    Review URL: http://codereview.chromium.org/10010033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131304 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_util.cc
+++ remoting/client/plugin/pepper_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,10 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-void CompletionCallbackClosureAdapter(void* user_data, int32_t not_used) {
-  base::Closure* closure = reinterpret_cast<base::Closure*>(user_data);
-  closure->Run();
-  delete closure;
+static void CallbackAdapter(void* user_data, int32_t result) {
+  scoped_ptr<base::Callback<void(int)> > callback(
+      reinterpret_cast<base::Callback<void(int)>*>(user_data));
+  callback->Run(result);
+}
+
+pp::CompletionCallback PpCompletionCallback(
+    base::Callback<void(int)> callback) {
+  return pp::CompletionCallback(&CallbackAdapter,
+                                new base::Callback<void(int)>(callback));
 }
 
 }  // namespace remoting
