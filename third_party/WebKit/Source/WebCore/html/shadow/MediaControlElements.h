18c24146ad0de980aa0b824572d53ad0647186e7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 20:10:42 +0000

    Source/WebCore: Simplified volume slider rendering. https://bugs.webkit.org/show_bug.cgi?id=82150
    Patch by Victor Carbune <vcarbune@adobe.com> on 2012-04-09
    Reviewed by Eric Carlson.
    
    Test: media/video-controls-rendering-toggle-display-none.html
    
    * css/mediaControlsChromium.css: Update controls css for Chromium.
    (audio::-webkit-media-controls-panel, video::-webkit-media-controls-panel):
    (audio::-webkit-media-controls-mute-button, video::-webkit-media-controls-mute-button):
    (audio::-webkit-media-controls-volume-slider-container, video::-webkit-media-controls-volume-slider-container):
    * css/mediaControlsQuickTime.css: Update controls css for Safari.
    (audio::-webkit-media-controls-volume-slider-container, video::-webkit-media-controls-volume-slider-container):
    * html/shadow/MediaControlElements.cpp: Removed particular renderer.
    (WebCore):
    * html/shadow/MediaControlElements.h:
    (MediaControlVolumeSliderContainerElement): Removed particular renderer.
    * html/shadow/MediaControlRootElement.cpp:
    (WebCore::MediaControlRootElement::create): Added a div element as a container. Removed extra
    unused mute button.
    * html/shadow/MediaControlRootElementChromium.cpp: Added an extra div element as a container for
    the mute button and the volume slider to easily position them relative to each other.
    (WebCore::MediaControlRootElementChromium::create):
    
    LayoutTests: Simplified volume rendering.
    https://bugs.webkit.org/show_bug.cgi?id=82150
    
    Patch by Victor Carbune <vcarbune@adobe.com> on 2012-04-09
    Reviewed by Eric Carlson.
    
    * media/video-controls-rendering-toggle-display-none-expected.txt: Added.
    * media/video-controls-rendering-toggle-display-none.html: Added.
    * platform/chromium/test_expectations.txt: Marked tests for rebaseline.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlElements.h
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlElements.h
@@ -160,7 +160,6 @@ public:
 
 private:
     MediaControlVolumeSliderContainerElement(Document*);
-    virtual RenderObject* createRenderer(RenderArena*, RenderStyle*);
     virtual void defaultEventHandler(Event*);
     virtual MediaControlElementType displayType() const;
     virtual const AtomicString& shadowPseudoId() const;
