243b6ef23c958178912761e0f6c4bce91ad94334
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 22:40:03 +0000

    [BlackBerry] Allow simulator to use GLES2 acceleration https://bugs.webkit.org/show_bug.cgi?id=81672
    Patch by Ming Xie <mxie@rim.com> on 2012-04-09
    Reviewed by Rob Buis.
    
    .:
    
    * Source/cmake/OptionsBlackBerry.cmake:
    
    Tools:
    
    * Scripts/webkitdirs.pm:
    (blackberryCMakeArguments):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -933,6 +933,7 @@ sub blackberryCMakeArguments()
     if ($cpu eq "a9") {
         $cpu = $arch . "v7le";
         push @cmakeExtraOptions, '-DTARGETING_PLAYBOOK=1';
+        push @cmakeExtraOptions, '-DENABLE_GLES2=1';
     }
 
     my $stageDir = $ENV{"STAGE_DIR"};
@@ -959,6 +960,7 @@ sub blackberryCMakeArguments()
 
     push @cmakeExtraOptions, "-DCMAKE_SKIP_RPATH='ON'" if isDarwin();
     push @cmakeExtraOptions, "-DENABLE_DRT=1" if $ENV{"ENABLE_DRT"};
+    push @cmakeExtraOptions, "-DENABLE_GLES2=1" if $ENV{"ENABLE_GLES2"};
 
     my @includeSystemDirectories;
     push @includeSystemDirectories, File::Spec->catdir($stageInc, "grskia", "skia");
