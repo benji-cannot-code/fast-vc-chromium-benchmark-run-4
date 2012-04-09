3d1f72a2b1687909fd27b2b304e9d725813d4666
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 21:59:05 +0000

    [Qt][WK2] Accelerated and non-accelerated animations need to be synchronized https://bugs.webkit.org/show_bug.cgi?id=75780
    Reviewed by Kenneth Rohde Christiansen.
    
    Remove the ui-side animation code, and return false from addAnimation so that web-side
    animations kick in.
    
    * Shared/WebLayerTreeInfo.cpp:
    (WebKit):
    * Shared/WebLayerTreeInfo.h:
    (WebLayerInfo):
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::paintToCurrentGLContext):
    (WebKit::WebLayerTreeRenderer::setLayerState):
    * WebProcess/WebCoreSupport/WebGraphicsLayer.cpp:
    (WebCore::WebGraphicsLayer::didChangeGeometry):
    (WebCore::WebGraphicsLayer::syncLayerState):
    (WebCore::WebGraphicsLayer::computeTransformedVisibleRect):
    * WebProcess/WebCoreSupport/WebGraphicsLayer.h:
    (WebGraphicsLayer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebLayerTreeInfo.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebLayerTreeInfo.h
@@ -49,32 +49,6 @@ struct WebLayerUpdateInfo {
     static bool decode(CoreIPC::ArgumentDecoder*, WebLayerUpdateInfo&);
 };
 
-struct WebLayerAnimation {
-    WebLayerAnimation()
-        : operation(InvalidAnimation)
-        , keyframeList(WebCore::AnimatedPropertyInvalid)
-        , startTime(0) { }
-    WebLayerAnimation(const WebCore::KeyframeValueList& valueList)
-        : operation(InvalidAnimation)
-        , keyframeList(valueList)
-        , startTime(0) { }
-
-    String name;
-    enum Operation {
-        AddAnimation,
-        RemoveAnimation,
-        PauseAnimation,
-        InvalidAnimation
-    } operation;
-    WebCore::IntSize boxSize;
-    RefPtr<WebCore::Animation> animation;
-    WebCore::KeyframeValueList keyframeList;
-    double startTime;
-
-    void encode(CoreIPC::ArgumentEncoder*) const;
-    static bool decode(CoreIPC::ArgumentDecoder*, WebLayerAnimation&);
-};
-
 struct WebLayerInfo {
     WebLayerInfo()
         : id(InvalidWebLayerID)
@@ -112,7 +86,6 @@ struct WebLayerInfo {
         unsigned int flags;
     };
     Vector<WebLayerID> children;
-    Vector<WebLayerAnimation> animations;
     RefPtr<ShareableBitmap> imageBackingStore;
 
     void encode(CoreIPC::ArgumentEncoder*) const;
