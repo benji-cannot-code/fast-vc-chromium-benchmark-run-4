6c063deb673bf5275ecb99bdd4269358f97f3471
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 16:27:37 +0000

    Move --user-agent overriding logic from chrome into content.
    BUG=117259
    TEST=start chrome and content_shell with and without --user-agent and visit a site like www.whatsmyuseragent.com to verify the expected user agent
    
    Review URL: https://chromiumcodereview.appspot.com/9623027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131365 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/content_client.cc
+++ content/public/common/content_client.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,17 +17,11 @@ static ContentClient* g_client;
 void SetContentClient(ContentClient* client) {
   g_client = client;
 
-  // TODO(dpranke): Doing real work (calling webkit_glue::SetUserAgent)
-  // inside what looks like a function that initializes a global is a
-  // bit odd, but we need to make sure this is done before
-  // webkit_glue::GetUserAgent() is called (so that the UA doesn't change).
-  //
-  // It would be cleaner if we could rename this to something like a
-  // content::Initialize(). Maybe we can merge this into ContentMain() somehow?
+  // Set the default user agent as provided by the client. We need to make
+  // sure this is done before webkit_glue::GetUserAgent() is called (so that
+  // the UA doesn't change).
   if (client) {
-    bool custom = false;
-    std::string ua = client->GetUserAgent(&custom);
-    webkit_glue::SetUserAgent(ua, custom);
+    webkit_glue::SetUserAgent(client->GetUserAgent(), false);
   }
 }
 
