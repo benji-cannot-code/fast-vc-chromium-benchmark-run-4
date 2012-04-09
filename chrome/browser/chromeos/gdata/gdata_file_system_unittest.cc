faa05edbce0cb4e37378b08ca16d5f6059ed35ad
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 20:03:50 +0000

    Block gdata file system destruction until io_weak_ptr_factory is reset.
    There may be tasks on IO thread that access gdata_file_system members,
    so we should make sure they are all done before we nuke them (file system data members, not tasks).
    All tasks on IO thread after io_weak_ptr_factory reset task will be ignored,
    so file system destruction may proceed after the weak ptr factory is nuked.
    
    BUG=chromium-os:28888
    TEST=gdata unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10008033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131418 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
@@ -120,7 +120,7 @@ class GDataFileSystemTest : public testing::Test {
   GDataFileSystemTest()
       : ui_thread_(content::BrowserThread::UI, &message_loop_),
         file_thread_(content::BrowserThread::FILE),
-        io_thread_(content::BrowserThread::IO, &message_loop_),
+        io_thread_(content::BrowserThread::IO),
         file_system_(NULL),
         mock_doc_service_(NULL),
         mock_sync_client_(NULL),
@@ -133,6 +133,7 @@ class GDataFileSystemTest : public testing::Test {
 
   virtual void SetUp() OVERRIDE {
     file_thread_.StartIOThread();
+    io_thread_.StartIOThread();
 
     profile_.reset(new TestingProfile);
 
@@ -156,7 +157,6 @@ class GDataFileSystemTest : public testing::Test {
   virtual void TearDown() OVERRIDE {
     ASSERT_TRUE(file_system_);
     EXPECT_CALL(*mock_doc_service_, CancelAll()).Times(1);
-    file_system_->ShutdownOnUIThread();
     delete file_system_;
     file_system_ = NULL;
 
