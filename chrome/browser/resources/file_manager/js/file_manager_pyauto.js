de84cd8caa1d6fe87779f31c9020d5ef1ec1a165
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 12:31:34 +0000

    Fix file manager pyauto test.
    Internal cut/copy/paste API changed to document commands.
    Copying directory to itself is prohibited now.
    
    BUG=chromium-os:26669
    TEST=desktopui_PyAutoFunctionalTests.chromeos_file_browser
    Review URL: https://chromiumcodereview.appspot.com/10010036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131350 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager_pyauto.js
+++ chrome/browser/resources/file_manager/js/file_manager_pyauto.js
@@ -39,7 +39,7 @@ var pyautoAPI = {
    * @return {object} A a list of item names.
    */
   listDirectory: function() {
-    var list = []
+    var list = [];
     var dm = fileManager.directoryModel_.fileList;
     for (var i = 0; i < dm.length; i++) {
       list.push(dm.item(i).name);
@@ -78,11 +78,18 @@ var pyautoAPI = {
     this.sendDone_();
   },
 
+  executeClipboardCommand_: function(command) {
+    // Input should not be focused, or the cut/cop/paste command
+    // will be treated as textual editing.
+    fileManager.filenameInput_.blur();
+    fileManager.document_.execCommand(command);
+  },
+
   /**
    * Copy selected items to clipboard.
    */
   copyItems: function() {
-    fileManager.copySelectionToClipboard();
+    this.executeClipboardCommand_('copy');
     this.sendDone_();
   },
 
@@ -90,7 +97,7 @@ var pyautoAPI = {
    * Cut selected items to clipboard.
    */
   cutItems: function() {
-    fileManager.cutSelectionToClipboard();
+    this.executeClipboardCommand_('cut');
     this.sendDone_();
   },
 
@@ -105,7 +112,7 @@ var pyautoAPI = {
     }.bind(this);
 
     dm.addEventListener('rescan-completed', onRescan);
-    fileManager.pasteFromClipboard();
+    this.executeClipboardCommand_('paste');
   },
 
   /**
@@ -133,7 +140,14 @@ var pyautoAPI = {
    * @param {string} name Name of the directory.
    */
   createDirectory: function(name) {
-    fileManager.createNewFolder(name, this.sendDone_);
+    var dm = fileManager.directoryModel_;
+    var onRescan = function() {
+      dm.removeEventListener('rescan-completed', onRescan);
+      this.sendDone_();
+    }.bind(this);
+
+    dm.addEventListener('rescan-completed', onRescan);
+    fileManager.directoryModel_.createDirectory(name, function(){});
   },
 
   /**
