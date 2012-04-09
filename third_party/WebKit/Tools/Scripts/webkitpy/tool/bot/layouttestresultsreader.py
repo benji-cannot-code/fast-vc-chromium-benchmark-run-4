6526f43f116d5bf4cbb3d1ed43537cb54807ca86
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 07:55:12 +0000

    Enable webkit_unit_tests for commit queue and EWS while tracking failures https://bugs.webkit.org/show_bug.cgi?id=83329
    Unreviewed.  Add a missing import to unbreak all the Chromium EWS/CQ bots. :)
    
    line 62, in _create_unit_test_results
     return UnitTestResults.results_from_string(results_xml)
     NameError: global name 'UnitTestResults' is not defined
    
    * Scripts/webkitpy/tool/bot/layouttestresultsreader.py:
    * Scripts/webkitpy/tool/bot/layouttestresultsreader_unittest.py:
    (LayoutTestResultsReaderTest.test_create_unit_test_results):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/layouttestresultsreader.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/layouttestresultsreader.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from webkitpy.common.net.layouttestresults import LayoutTestResults
+from webkitpy.common.net.unittestresults import UnitTestResults
 from webkitpy.common.system.deprecated_logging import error, log
 from webkitpy.tool.steps.runtests import RunTests
 
