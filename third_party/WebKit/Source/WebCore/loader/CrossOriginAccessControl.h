3826a17921bff06695df58ca37ea51a2a4092bcf
bbudge@chromium.org <bbudge@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 17:17:25 +0000

    Cross-origin preflight request should not include credentials. https://bugs.webkit.org/show_bug.cgi?id=37676
    Source/WebCore:
    
    Modifies createAccessControlPreflightRequest so it never allows credentials.
    
    Reviewed by Adam Barth.
    
    http/tests/xmlhttprequest/access-control-preflight-credential-sync.html
    http/tests/xmlhttprequest/access-control-preflight-credential-async.html
    
    * loader/CrossOriginAccessControl.cpp:
    (WebCore::createAccessControlPreflightRequest):
    * loader/CrossOriginAccessControl.h:
    (WebCore):
    * loader/DocumentThreadableLoader.cpp:
    (WebCore::DocumentThreadableLoader::makeCrossOriginAccessRequestWithPreflight):
    
    LayoutTests:
    
    Modifies preflight credential tests to make sure that cookies aren't sent along with the
    CORS prefetch request.
    
    Reviewed by Adam Barth.
    
    * http/tests/xmlhttprequest/access-control-preflight-credential-async-expected.txt:
    * http/tests/xmlhttprequest/access-control-preflight-credential-async.html:
    * http/tests/xmlhttprequest/access-control-preflight-credential-sync-expected.txt:
    * http/tests/xmlhttprequest/access-control-preflight-credential-sync.html:
    * http/tests/xmlhttprequest/resources/basic-auth/access-control-auth-basic.php:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/CrossOriginAccessControl.h
+++ third_party/WebKit/Source/WebCore/loader/CrossOriginAccessControl.h
@@ -47,7 +47,7 @@ bool isOnAccessControlSimpleRequestHeaderWhitelist(const String& name, const Str
 bool isOnAccessControlResponseHeaderWhitelist(const String&);
 
 void updateRequestForAccessControl(ResourceRequest&, SecurityOrigin*, StoredCredentials);
-ResourceRequest createAccessControlPreflightRequest(const ResourceRequest&, SecurityOrigin*, StoredCredentials);
+ResourceRequest createAccessControlPreflightRequest(const ResourceRequest&, SecurityOrigin*);
 
 bool passesAccessControlCheck(const ResourceResponse&, StoredCredentials, SecurityOrigin*, String& errorDescription);
 void parseAccessControlExposeHeadersAllowList(const String& headerValue, HTTPHeaderSet&);
