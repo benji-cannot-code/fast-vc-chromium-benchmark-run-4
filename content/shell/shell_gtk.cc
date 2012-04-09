fbce7033745623c34d7501b89003197313569922
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 20:50:02 +0000

    Remove duplicated gtk_init() in the Shell::PlatformInitialize(). gtk_init() is called in content layer by calling gfx::GtkInitFromCommandLine().
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/10025013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131428 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_gtk.cc
+++ content/shell/shell_gtk.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 void Shell::PlatformInitialize() {
-  gtk_init(NULL, NULL);
 }
 
 base::StringPiece Shell::PlatformResourceProvider(int key) {
