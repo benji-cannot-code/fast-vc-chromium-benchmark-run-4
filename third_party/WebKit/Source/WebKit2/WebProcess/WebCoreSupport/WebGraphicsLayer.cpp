9aceb6895022077b94110554f90e9beacd03c084
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-09 23:45:43 +0000

    [Qt][WK2] Sync the layer's state and the layer's children separately https://bugs.webkit.org/show_bug.cgi?id=82534
    Reviewed by Kenneth Rohde Christiansen.
    
    Switched the WebLayerInfo struct, which includes a single layer's state,
    to use only POD types that can be serialized via SimpleArgumentCoder.
    To allow POD-serialization, we serialize the children IDs in a seperate
    message (SetLayerChildren), and only when the tree layout has actually
    been modified.
    
    * Shared/WebLayerTreeInfo.cpp:
    (WebKit::WebLayerInfo::encode):
    (WebKit::WebLayerInfo::decode):
    * Shared/WebLayerTreeInfo.h:
    (WebKit::WebLayerInfo::WebLayerInfo):
    * UIProcess/LayerTreeHostProxy.cpp:
    (WebKit::LayerTreeHostProxy::setCompositingLayerState):
    (WebKit):
    (WebKit::LayerTreeHostProxy::setCompositingLayerChildren):
    * UIProcess/LayerTreeHostProxy.h:
    (LayerTreeHostProxy):
    * UIProcess/LayerTreeHostProxy.messages.in:
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::setLayerChildren):
    (WebKit):
    (WebKit::WebLayerTreeRenderer::setLayerState):
    * UIProcess/WebLayerTreeRenderer.h:
    (WebLayerTreeRenderer):
    * WebProcess/WebCoreSupport/WebGraphicsLayer.cpp:
    (WebCore::WebGraphicsLayer::didChangeLayerState):
    (WebCore):
    (WebCore::WebGraphicsLayer::didChangeChildren):
    (WebCore::WebGraphicsLayer::didChangeGeometry):
    (WebCore::WebGraphicsLayer::WebGraphicsLayer):
    (WebCore::WebGraphicsLayer::setChildren):
    (WebCore::WebGraphicsLayer::addChild):
    (WebCore::WebGraphicsLayer::addChildAtIndex):
    (WebCore::WebGraphicsLayer::addChildAbove):
    (WebCore::WebGraphicsLayer::addChildBelow):
    (WebCore::WebGraphicsLayer::replaceChild):
    (WebCore::WebGraphicsLayer::removeFromParent):
    (WebCore::WebGraphicsLayer::setDrawsContent):
    (WebCore::WebGraphicsLayer::setContentsOpaque):
    (WebCore::WebGraphicsLayer::setBackfaceVisibility):
    (WebCore::WebGraphicsLayer::setOpacity):
    (WebCore::WebGraphicsLayer::setContentsRect):
    (WebCore::WebGraphicsLayer::setContentsToImage):
    (WebCore::WebGraphicsLayer::setMaskLayer):
    (WebCore::WebGraphicsLayer::setReplicatedByLayer):
    (WebCore::WebGraphicsLayer::setNeedsDisplayInRect):
    (WebCore::WebGraphicsLayer::id):
    (WebCore::WebGraphicsLayer::syncChildren):
    (WebCore::WebGraphicsLayer::syncLayerState):
    (WebCore::WebGraphicsLayer::syncImageBackingStore):
    (WebCore::WebGraphicsLayer::syncCompositingStateForThisLayerOnly):
    (WebCore::WebGraphicsLayer::setRootLayer):
    * WebProcess/WebCoreSupport/WebGraphicsLayer.h:
    (WebGraphicsLayerClient):
    (WebGraphicsLayer):
    * WebProcess/WebPage/qt/LayerTreeHostQt.cpp:
    (WebKit::LayerTreeHostQt::syncLayerState):
    (WebKit):
    (WebKit::LayerTreeHostQt::syncLayerChildren):
    * WebProcess/WebPage/qt/LayerTreeHostQt.h:
    (LayerTreeHostQt):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.cpp
@@ -61,9 +61,16 @@ static WebLayerID toWebLayerID(GraphicsLayer* layer)
     return layer ? toWebGraphicsLayer(layer)->id() : 0;
 }
 
-void WebGraphicsLayer::notifyChange()
+void WebGraphicsLayer::didChangeLayerState()
 {
-    m_modified = true;
+    m_shouldSyncLayerState = true;
+    if (client())
+        client()->notifySyncRequired(this);
+}
+
+void WebGraphicsLayer::didChangeChildren()
+{
+    m_shouldSyncChildren = true;
     if (client())
         client()->notifySyncRequired(this);
 }
@@ -82,21 +89,22 @@ void WebGraphicsLayer::setShouldUpdateVisibleRect()
 
 void WebGraphicsLayer::didChangeGeometry()
 {
-    notifyChange();
+    didChangeLayerState();
     setShouldUpdateVisibleRect();
 }
 
 WebGraphicsLayer::WebGraphicsLayer(GraphicsLayerClient* client)
     : GraphicsLayer(client)
     , m_maskTarget(0)
-    , m_modified(true)
     , m_inUpdateMode(false)
     , m_shouldUpdateVisibleRect(true)
+    , m_shouldSyncLayerState(true)
+    , m_shouldSyncChildren(true)
     , m_webGraphicsLayerClient(0)
-    , m_contentsScale(1.f)
+    , m_contentsScale(1)
 {
     static WebLayerID nextLayerID = 1;
-    m_layerInfo.id = nextLayerID++;
+    m_id = nextLayerID++;
     layerByIDMap().add(id(), this);
 }
 
@@ -124,9 +132,9 @@ bool WebGraphicsLayer::setChildren(const Vector<GraphicsLayer*>& children)
     for (int i = 0; i < children.size(); ++i) {
         WebGraphicsLayer* child = toWebGraphicsLayer(children[i]);
         child->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-        child->notifyChange();
+        child->didChangeLayerState();
     }
-    notifyChange();
+    didChangeChildren();
     return true;
 }
 
@@ -134,32 +142,32 @@ void WebGraphicsLayer::addChild(GraphicsLayer* layer)
 {
     GraphicsLayer::addChild(layer);
     toWebGraphicsLayer(layer)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-    toWebGraphicsLayer(layer)->notifyChange();
-    notifyChange();
+    toWebGraphicsLayer(layer)->didChangeLayerState();
+    didChangeChildren();
 }
 
 void WebGraphicsLayer::addChildAtIndex(GraphicsLayer* layer, int index)
 {
     GraphicsLayer::addChildAtIndex(layer, index);
     toWebGraphicsLayer(layer)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-    toWebGraphicsLayer(layer)->notifyChange();
-    notifyChange();
+    toWebGraphicsLayer(layer)->didChangeLayerState();
+    didChangeChildren();
 }
 
 void WebGraphicsLayer::addChildAbove(GraphicsLayer* layer, GraphicsLayer* sibling)
 {
     GraphicsLayer::addChildAbove(layer, sibling);
     toWebGraphicsLayer(layer)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-    toWebGraphicsLayer(layer)->notifyChange();
-    notifyChange();
+    toWebGraphicsLayer(layer)->didChangeLayerState();
+    didChangeChildren();
 }
 
 void WebGraphicsLayer::addChildBelow(GraphicsLayer* layer, GraphicsLayer* sibling)
 {
     GraphicsLayer::addChildBelow(layer, sibling);
     toWebGraphicsLayer(layer)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-    toWebGraphicsLayer(layer)->notifyChange();
-    notifyChange();
+    toWebGraphicsLayer(layer)->didChangeLayerState();
+    didChangeChildren();
 }
 
 bool WebGraphicsLayer::replaceChild(GraphicsLayer* oldChild, GraphicsLayer* newChild)
@@ -167,20 +175,20 @@ bool WebGraphicsLayer::replaceChild(GraphicsLayer* oldChild, GraphicsLayer* newC
     bool ok = GraphicsLayer::replaceChild(oldChild, newChild);
     if (!ok)
         return false;
-    notifyChange();
-    toWebGraphicsLayer(oldChild)->notifyChange();
+    didChangeChildren();
+    toWebGraphicsLayer(oldChild)->didChangeLayerState();
     toWebGraphicsLayer(newChild)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
-    toWebGraphicsLayer(newChild)->notifyChange();
+    toWebGraphicsLayer(newChild)->didChangeLayerState();
     return true;
 }
 
 void WebGraphicsLayer::removeFromParent()
 {
     if (WebGraphicsLayer* parentLayer = toWebGraphicsLayer(parent()))
-        parentLayer->notifyChange();
+        parentLayer->didChangeChildren();
     GraphicsLayer::removeFromParent();
 
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setPosition(const FloatPoint& p)
@@ -254,7 +262,7 @@ void WebGraphicsLayer::setDrawsContent(bool b)
         return;
     GraphicsLayer::setDrawsContent(b);
 
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setContentsOpaque(bool b)
@@ -264,7 +272,7 @@ void WebGraphicsLayer::setContentsOpaque(bool b)
     if (m_mainBackingStore)
         m_mainBackingStore->setSupportsAlpha(!b);
     GraphicsLayer::setContentsOpaque(b);
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setBackfaceVisibility(bool b)
@@ -273,7 +281,7 @@ void WebGraphicsLayer::setBackfaceVisibility(bool b)
         return;
 
     GraphicsLayer::setBackfaceVisibility(b);
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setOpacity(float opacity)
@@ -282,7 +290,7 @@ void WebGraphicsLayer::setOpacity(float opacity)
         return;
 
     GraphicsLayer::setOpacity(opacity);
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setContentsRect(const IntRect& r)
@@ -291,7 +299,7 @@ void WebGraphicsLayer::setContentsRect(const IntRect& r)
         return;
 
     GraphicsLayer::setContentsRect(r);
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setContentsNeedsDisplay()
@@ -310,13 +318,13 @@ void WebGraphicsLayer::setContentsToImage(Image* image)
         // We adopt first, in case this is the same frame - that way we avoid destroying and recreating the image.
         newID = m_webGraphicsLayerClient->adoptImageBackingStore(image);
         m_webGraphicsLayerClient->releaseImageBackingStore(m_layerInfo.imageBackingStoreID);
-        notifyChange();
+        didChangeLayerState();
         if (m_layerInfo.imageBackingStoreID && newID == m_layerInfo.imageBackingStoreID)
             return;
     } else {
         // If m_webGraphicsLayerClient is not set yet there should be no backing store ID.
         ASSERT(!m_layerInfo.imageBackingStoreID);
-        notifyChange();
+        didChangeLayerState();
     }
 
     m_layerInfo.imageBackingStoreID = newID;
@@ -338,8 +346,8 @@ void WebGraphicsLayer::setMaskLayer(GraphicsLayer* layer)
     WebGraphicsLayer* webGraphicsLayer = toWebGraphicsLayer(layer);
     webGraphicsLayer->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
     webGraphicsLayer->setMaskTarget(this);
-    webGraphicsLayer->notifyChange();
-    notifyChange();
+    webGraphicsLayer->didChangeLayerState();
+    didChangeLayerState();
 
 }
 
@@ -352,7 +360,7 @@ void WebGraphicsLayer::setReplicatedByLayer(GraphicsLayer* layer)
         toWebGraphicsLayer(layer)->setWebGraphicsLayerClient(m_webGraphicsLayerClient);
 
     GraphicsLayer::setReplicatedByLayer(layer);
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setNeedsDisplay()
@@ -364,12 +372,12 @@ void WebGraphicsLayer::setNeedsDisplayInRect(const FloatRect& rect)
 {
     if (m_mainBackingStore)
         m_mainBackingStore->invalidate(IntRect(rect));
-    notifyChange();
+    didChangeLayerState();
 }
 
 WebLayerID WebGraphicsLayer::id() const
 {
-    return m_layerInfo.id;
+    return m_id;
 }
 
 void WebGraphicsLayer::syncCompositingState(const FloatRect& rect)
@@ -391,13 +399,24 @@ WebGraphicsLayer* toWebGraphicsLayer(GraphicsLayer* layer)
     return static_cast<WebGraphicsLayer*>(layer);
 }
 
-void WebGraphicsLayer::syncLayerParameters()
- {
-    if (!m_modified)
+void WebGraphicsLayer::syncChildren()
+{
+    if (!m_shouldSyncChildren)
         return;
+    m_shouldSyncChildren = false;
+    Vector<WebLayerID> childIDs;
+    for (size_t i = 0; i < children().size(); ++i)
+        childIDs.append(toWebLayerID(children()[i]));
 
-    m_modified = false;
-    m_layerInfo.name = name();
+    m_webGraphicsLayerClient->syncLayerChildren(m_id, childIDs);
+}
+
+void WebGraphicsLayer::syncLayerState()
+ {
+    if (!m_shouldSyncLayerState)
+        return;
+    m_shouldSyncLayerState = false;
+
     m_layerInfo.anchorPoint = anchorPoint();
     m_layerInfo.backfaceVisible = backfaceVisibility();
     m_layerInfo.childrenTransform = childrenTransform();
@@ -413,24 +432,24 @@ void WebGraphicsLayer::syncLayerParameters()
     m_layerInfo.replica = toWebLayerID(replicaLayer());
     m_layerInfo.size = size();
     m_layerInfo.transform = transform();
-    m_layerInfo.children.clear();
+    m_webGraphicsLayerClient->syncLayerState(m_id, m_layerInfo);
+}
 
-    for (size_t i = 0; i < children().size(); ++i)
-        m_layerInfo.children.append(toWebLayerID(children()[i]));
-
-    m_webGraphicsLayerClient->didSyncCompositingStateForLayer(m_layerInfo);
+void WebGraphicsLayer::ensureImageBackingStore()
+{
+    if (!m_image)
+        return;
+    if (!m_layerInfo.imageBackingStoreID)
+        m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
 }
 void WebGraphicsLayer::syncCompositingStateForThisLayerOnly()
 {
     // The remote image might have been released by purgeBackingStores.
-    if (m_image && !m_layerInfo.imageBackingStoreID)
-        m_layerInfo.imageBackingStoreID = m_webGraphicsLayerClient->adoptImageBackingStore(m_image.get());
-
+    ensureImageBackingStore();
     computeTransformedVisibleRect();
-    syncLayerParameters();
+    syncChildren();
+    syncLayerState();
     updateContentBuffers();
-
-    m_modified = false;
 }
 
 void WebGraphicsLayer::tiledBackingStorePaintBegin()
@@ -440,7 +459,7 @@ void WebGraphicsLayer::tiledBackingStorePaintBegin()
 void WebGraphicsLayer::setRootLayer(bool isRoot)
 {
     m_layerInfo.isRootLayer = isRoot;
-    notifyChange();
+    didChangeLayerState();
 }
 
 void WebGraphicsLayer::setVisibleContentRectTrajectoryVector(const FloatPoint& trajectoryVector)
