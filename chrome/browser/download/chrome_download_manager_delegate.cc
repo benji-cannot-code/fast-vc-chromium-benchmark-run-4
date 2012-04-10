5318b96a57f5bbd83bf79282fe283cda9a47e5a6
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 17:01:15 +0000

    Fulfill IWYU and add missing include for Android build.
    http://codereview.chromium.org/9651020/ added a dep on WebContentsDelegate
    so it should technically be required. This broke on Android because
    browser.h isn't included which is causing other builds to include the
    header.
    
    BUG=117407
    
    
    Review URL: http://codereview.chromium.org/10033024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_intents_dispatcher.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
