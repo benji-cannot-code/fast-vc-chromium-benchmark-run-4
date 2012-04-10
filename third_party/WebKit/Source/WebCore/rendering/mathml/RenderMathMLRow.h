758d84cb6b350f6da8e5fd0a3478d33c185caa33
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-10 19:15:36 +0000

    msqrt's implied mrow should do operator stretching https://bugs.webkit.org/show_bug.cgi?id=82353
    Patch by David Barton <dbarton@mathscribe.com> on 2012-04-10
    Reviewed by Julien Chaffraix.
    
    Source/WebCore:
    
    An <msqrt> with more than one child is supposed to surround them with an implied <mrow>.
    This patch does this, and thus includes vertical operator stretching. We also fix some
    problems with the old RenderMathMLSquareRoot.cpp: possibly shared RenderStyles were
    being modified, and layout() was using heights of children without checking whether they
    needed layout themselves first.
    
    To leave room for the radical sign, we use intrinsic padding. This is simpler and more
    efficient than using anonymous RenderStyles.
    
    Added tests to mo-stretch.html and style.xhtml. Regression tests are in roots.xhtml.
    
    * css/mathml.css:
    (msqrt):
    * rendering/RenderBoxModelObject.h:
    (RenderBoxModelObject):
    * rendering/mathml/RenderMathMLBlock.cpp:
    (WebCore::RenderMathMLBlock::RenderMathMLBlock):
    (WebCore::RenderMathMLBlock::paddingTop):
    (WebCore::RenderMathMLBlock::paddingBottom):
    (WebCore::RenderMathMLBlock::paddingLeft):
    (WebCore::RenderMathMLBlock::paddingRight):
    (WebCore::RenderMathMLBlock::paddingBefore):
    (WebCore::RenderMathMLBlock::paddingAfter):
    (WebCore::RenderMathMLBlock::paddingStart):
    (WebCore::RenderMathMLBlock::paddingEnd):
    * rendering/mathml/RenderMathMLBlock.h:
    (RenderMathMLBlock):
    * rendering/mathml/RenderMathMLRow.cpp:
    (WebCore::RenderMathMLRow::RenderMathMLRow):
    (WebCore::RenderMathMLRow::createAnonymousWithParentRenderer):
    * rendering/mathml/RenderMathMLRow.h:
    (RenderMathMLRow):
    * rendering/mathml/RenderMathMLSquareRoot.cpp:
    (WebCore):
    (WebCore::RenderMathMLSquareRoot::addChild):
    (WebCore::RenderMathMLSquareRoot::computePreferredLogicalWidths):
    (WebCore::RenderMathMLSquareRoot::computeLogicalHeight):
    (WebCore::RenderMathMLSquareRoot::layout):
    (WebCore::RenderMathMLSquareRoot::paint):
    * rendering/mathml/RenderMathMLSquareRoot.h:
    (RenderMathMLSquareRoot):
    
    LayoutTests:
    
    * mathml/presentation/mo-stretch.html:
    * mathml/presentation/style.xhtml:
    * platform/mac/mathml/presentation/attributes-expected.txt:
    * platform/mac/mathml/presentation/mo-stretch-expected.png:
    * platform/mac/mathml/presentation/mo-stretch-expected.txt:
    * platform/mac/mathml/presentation/roots-expected.txt:
    * platform/mac/mathml/presentation/style-expected.png:
    * platform/mac/mathml/presentation/style-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/mathml/RenderMathMLRow.h
+++ third_party/WebKit/Source/WebCore/rendering/mathml/RenderMathMLRow.h
@@ -35,9 +35,13 @@ namespace WebCore {
     
 class RenderMathMLRow : public RenderMathMLBlock {
 public:
-    RenderMathMLRow(Element*);
+    RenderMathMLRow(Node*);
+    static RenderMathMLRow* createAnonymousWithParentRenderer(const RenderObject*);
+    
     virtual bool isRenderMathMLRow() const { return true; }
+    
     virtual void stretchToHeight(int) {}
+    
 protected:
     virtual void layout();
 
