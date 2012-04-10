a1a296b2c645cd823a1895843d6abf150a341ce5
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 22:39:12 +0000

    Implement a DomMutationObserver hook to allow PyAuto to watch for changes to the DOM.
    BUG=122655
    TEST=functional/apptest.py
    
    Review URL: http://codereview.chromium.org/9950002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/apptest.py
+++ chrome/test/functional/apptest.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import pyauto_functional  # must be imported before pyauto
 import pyauto
-
+import domselector
 
 class PyAutoEventsTest(pyauto.PyUITest):
   """Tests using the event queue."""
@@ -14,9 +14,8 @@ class PyAutoEventsTest(pyauto.PyUITest):
     """Basic test for the event queue."""
     url = self.GetHttpURLForDataPath('apptest', 'basic.html')
     driver = self.NewWebDriver()
-    event_id = self.AddDomRaisedEventObserver(automation_id=4444);
-    success_id = self.AddDomRaisedEventObserver('test success',
-                                                automation_id=4444);
+    event_id = self.AddDomEventObserver(automation_id=4444, recurring=True)
+    success_id = self.AddDomEventObserver('test success', automation_id=4444)
     self.NavigateToURL(url)
     self._ExpectEvent(event_id, 'init')
     self._ExpectEvent(event_id, 'login ready')
@@ -25,10 +24,26 @@ class PyAutoEventsTest(pyauto.PyUITest):
     self._ExpectEvent(event_id, 'login done')
     self.GetNextEvent(success_id)
 
-  def _ExpectEvent(self, event_id, event_name):
+  def testDomMutationEvents(self):
+    """Basic tests for Dom Mutation observers."""
+    url = self.GetHttpURLForDataPath('apptest', 'dom_mutations.html')
+    self.NavigateToURL(url)
+    self.GetNextEvent(self.AddDomMutationObserver(
+        'add', domselector.XPath('/html/body')))
+    self.GetNextEvent(self.AddDomMutationObserver(
+        'add', domselector.CSSSelector('#login'), expected_value='Log In'))
+    success_id = self.AddDomMutationObserver(
+        'change', domselector.XPath('id(\'console\')'),
+        expected_value='.*succeeded.*')
+    self.NewWebDriver().find_element_by_id('login').click()
+    self.GetNextEvent(self.AddDomMutationObserver(
+        'remove', domselector.XPath('id(\'fail\')/a')))
+    self.GetNextEvent(success_id)
+
+  def _ExpectEvent(self, event_id, expected_event_name):
     """Checks that the next event is expected."""
     e = self.GetNextEvent(event_id)
-    self.assertEqual(e.get('name'), event_name,
+    self.assertEqual(e.get('name'), expected_event_name,
                      msg="unexpected event: %s" % e)
 
 
---
+++ chrome/test/pyautolib/domselector.py
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Defines a common interface for using DOM selectors."""
+
+
+class DOMSelector(object):
+  """Generic class implementing a DOM selector."""
+  type_string = None
+
+  def __init__(self, pattern):
+    """Initialize selector.
+
+    Args:
+     pattern: A string pattern formatted as a selector.
+    """
+    self.pattern = pattern
+
+
+class XPath(DOMSelector):
+  """An XPath type selector."""
+  type_string = 'xpath'
+
+
+class CSSSelector(DOMSelector):
+  """A CSS Selector type selector."""
+  type_string = 'css'
