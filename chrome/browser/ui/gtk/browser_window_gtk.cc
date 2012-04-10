16fd60e4a72a307d0e78fe31be0e0e73efb4705a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 17:04:20 +0000

    Simplify IsFullscreenBubbleVisible().
    The pattern get() != NULL is widely used while get() ? true : false has just
    more three occurrences.
    
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10027004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_window_gtk.cc
+++ chrome/browser/ui/gtk/browser_window_gtk.cc
@@ -951,7 +951,7 @@ bool BrowserWindowGtk::IsFullscreen() const {
 }
 
 bool BrowserWindowGtk::IsFullscreenBubbleVisible() const {
-  return fullscreen_exit_bubble_.get() ? true : false;
+  return fullscreen_exit_bubble_ != NULL;
 }
 
 LocationBar* BrowserWindowGtk::GetLocationBar() const {
