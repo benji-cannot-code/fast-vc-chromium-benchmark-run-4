30401761e651472eec09711623ef5855c3aea577
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 00:42:04 +0000

    Add support in Chrome to navigate to the corresponding url in metro mode, if we were launched via the search charm or via a metro url navigation.
    BUG=118641
    Review URL: https://chromiumcodereview.appspot.com/10032014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131505 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_init.cc
+++ chrome/browser/ui/browser_init.cc
@@ -128,6 +128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
+#include "base/win/metro.h"
 #include "base/win/windows_version.h"
 #include "chrome/installer/util/auto_launch_util.h"
 #endif
@@ -263,6 +264,13 @@ bool AutolaunchInfoBarDelegate::Cancel() {
                  profile_->GetPath().BaseName().value()));
   return true;
 }
+
+// Metro driver export, this is how metro passes the initial navigated url
+// back to us.
+extern "C" {
+typedef const wchar_t* (*GetInitialUrl)();
+}
+
 
 #endif  // OS_WIN
 
@@ -1667,6 +1675,22 @@ std::vector<GURL> BrowserInit::GetURLsFromCommandLine(
       }
     }
   }
+#if defined(OS_WIN)
+  // If we are in Windows 8 metro mode and were launched as a result of the
+  // search charm or via a url navigation in metro, then fetch the url being
+  // navigated to from the metro handler and initiate the navigation
+  // accordingly.
+  if (urls.empty()) {
+    HMODULE metro = base::win::GetMetroModule();
+    if (metro) {
+      GetInitialUrl initial_metro_url = reinterpret_cast<GetInitialUrl>(
+          ::GetProcAddress(metro, "GetInitialUrl"));
+      string16 url = initial_metro_url();
+      if (!url.empty())
+        urls.push_back(GURL(url));
+    }
+  }
+#endif  // OS_WIN
   return urls;
 }
 
