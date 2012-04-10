ed7ab49396000a195b34b0de9c310e816061ff19
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-10 06:24:57 +0000

    combine two arrays in XPathException into one https://bugs.webkit.org/show_bug.cgi?id=83442
    Patch by Lu Guanqun <guanqun.lu@intel.com> on 2012-04-09
    Reviewed by Kentaro Hara.
    
    No new tests required.
    
    * xml/XPathException.cpp:
    (XPathExceptionNameDescription):
    (WebCore):
    (WebCore::XPathException::initializeDescription):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/XPathException.cpp
+++ third_party/WebKit/Source/WebCore/xml/XPathException.cpp
@@ -32,18 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-// FIXME: This should be an array of structs to pair the names and descriptions.
-static const char* const exceptionNames[] = {
-    "INVALID_EXPRESSION_ERR",
-    "TYPE_ERR"
+static struct XPathExceptionNameDescription {
+    const char* const name;
+    const char* const description;
+} exceptions[] = {
+    { "INVALID_EXPRESSION_ERR", "The expression had a syntax error or otherwise is not a legal expression according to the rules of the specific XPathEvaluator." },
+    { "TYPE_ERR", "The expression could not be converted to return the specified type." }
 };
-
-static const char* const exceptionDescriptions[] = {
-    "The expression had a syntax error or otherwise is not a legal expression according to the rules of the specific XPathEvaluator.",
-    "The expression could not be converted to return the specified type."
-};
-
-COMPILE_ASSERT(WTF_ARRAY_LENGTH(exceptionNames) == WTF_ARRAY_LENGTH(exceptionDescriptions), XPathExceptionTablesMustMatch);
 
 bool XPathException::initializeDescription(ExceptionCode ec, ExceptionCodeDescription* description)
 {
@@ -54,11 +49,11 @@ bool XPathException::initializeDescription(ExceptionCode ec, ExceptionCodeDescri
     description->code = ec - XPathExceptionOffset;
     description->type = XPathExceptionType;
 
-    size_t tableSize = WTF_ARRAY_LENGTH(exceptionNames);
+    size_t tableSize = WTF_ARRAY_LENGTH(exceptions);
     size_t tableIndex = ec - INVALID_EXPRESSION_ERR;
 
-    description->name = tableIndex < tableSize ? exceptionNames[tableIndex] : 0;
-    description->description = tableIndex < tableSize ? exceptionDescriptions[tableIndex] : 0;
+    description->name = tableIndex < tableSize ? exceptions[tableIndex].name : 0;
+    description->description = tableIndex < tableSize ? exceptions[tableIndex].description : 0;
 
     return true;
 }
