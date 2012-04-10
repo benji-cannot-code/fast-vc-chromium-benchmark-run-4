ca05833f416921091da2051b8b068f0318c2de6e
asharif@chromium.org <asharif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 20:37:33 +0000

    Added code so renderer would cleanly exit.
    This code is only invoked when --renderer-clean-exit is passed to Chrome.
    
    It does the following: 
    1. Makes the browser process not send a SIGTERM to its children.
    2. Makes the renderer process not call _exit() in OnChannelError().
    
    Why is this needed? The renderer process in Chrome does not exit cleanly
    currently so when Chrome is profiled for optimization we do not get
    representative data and miss out on optimization opportunities.
    This CL addresses that problem by ensuring that exit handlers including
    profile dumpers get run before the renderer exits.
    
    BUG=107584
    TEST=Rebuilt Chrome with -fprofile-generate. Verified that the renderer
    process' profile is included when Chrome is closed.
    
    
    Review URL: http://codereview.chromium.org/9936002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131625 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -734,6 +734,9 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #if !defined(OFFICIAL_BUILD)
     switches::kRendererCheckFalseTest,
 #endif  // !defined(OFFICIAL_BUILD)
+#if defined(OS_POSIX)
+    switches::kRendererCleanExit,
+#endif
     switches::kRendererCrashTest,
     switches::kRendererStartupDialog,
     switches::kShowPaintRects,
