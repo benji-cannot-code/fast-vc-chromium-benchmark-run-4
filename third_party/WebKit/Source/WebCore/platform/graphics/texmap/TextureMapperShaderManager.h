8f71f516f40bc723b03504d790412aa2a2534983
alex@webkit.org <alex@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-10 18:56:57 +0000

    2012-04-10  Alejandro G. Castro  <alex@igalia.com>
            [GTK] Build fix TextureMapper compilation and solve warning.
    
            Reviewed by Martin Robinson.
    
            * platform/graphics/texmap/TextureMapperGL.cpp:
            (WebCore::BitmapTextureGL::updateContents):
            * platform/graphics/texmap/TextureMapperShaderManager.h:
            (WebCore::TextureMapperShaderManager::getShaderProgram):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperShaderManager.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperShaderManager.h
@@ -119,6 +119,8 @@ public:
         case OpacityAndMask:
             program = TextureMapperShaderProgramOpacityAndMask::create();
             break;
+        case Invalid:
+            ASSERT_NOT_REACHED();
         }
         m_textureMapperShaderProgramMap.add(shaderType, program);
         return program;
