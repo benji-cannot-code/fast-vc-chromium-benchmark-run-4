85b967a97c633fa18c5ba3b589ecbe53d74f76b5
carlosvaldivia@google.com <carlosvaldivia@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 22:52:44 +0000

    Upstream crash changes for android.
    Third time is a charm.
    
    Original Reviews:
    
    https://chromiumcodereview.appspot.com/9838033/
    http://codereview.chromium.org/9967017
    
    Revert "Revert 131593"
    
    This reverts commit e306ea7f630d4264075913ea3a1a728d98ca9605.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9960072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/crash_handler_host_linuxish_stub.cc
+++ chrome/browser/crash_handler_host_linuxish_stub.cc
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // This is a stub file which is compiled in when we are building without
 // breakpad support.
 
-#include "chrome/browser/crash_handler_host_linux.h"
+#include "chrome/browser/crash_handler_host_linuxish.h"
 
 #include "base/memory/singleton.h"
 
