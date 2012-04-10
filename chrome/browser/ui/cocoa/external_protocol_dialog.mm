3602c5ef975dcecfb42a401b64fa1480be7ff2ce
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-10 20:21:42 +0000

    Add a few missing "self = [super init..." notices by the clang static analyzer.
    BUG=none
    TEST=none
    TBR=dimich
    
    Review URL: http://codereview.chromium.org/10031018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131621 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/external_protocol_dialog.mm
+++ chrome/browser/ui/cocoa/external_protocol_dialog.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,6 +38,9 @@ void ExternalProtocolHandler::RunExternalProtocolDialog(
 @implementation ExternalProtocolDialogController
 - (id)initWithGURL:(const GURL*)url {
   DCHECK_EQ(MessageLoop::TYPE_UI, MessageLoop::current()->type());
+
+  if (!(self = [super init]))
+    return nil;
 
   url_ = *url;
   creation_time_ = base::Time::Now();
