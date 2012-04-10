9153c9fb032a18287b092e87fa96f84bd21ee15d
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-10 01:19:12 +0000

    Update perf-o-matic config.js per upstream change.
    * Websites/webkit-perf.appspot.com/js/config.js:
    (var):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/LayoutTestController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/LayoutTestController.cpp
@@ -2553,6 +2553,7 @@ void LayoutTestController::addURLToRedirect(std::string origin, std::string dest
 
 const std::string& LayoutTestController::redirectionDestinationForURL(std::string origin)
 {
+    fprintf(stderr, "[%s]\n", origin.c_str());
     return m_URLsToRedirect[origin];
 }
 
