7742625195207430ac41bb2e71cacb99fc744d22
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 14:49:21 +0000

    [Qt] Remove unnecessary rendering code https://bugs.webkit.org/show_bug.cgi?id=83591
    Reviewed by Noam Rosenthal.
    
    - Remove code that isn't needed since the introduction of WebLayerTreeRenderer
    - Remove TextureMapper::bindSurface(0) calls following beginPainting, which calls it itself.
    
    * UIProcess/API/qt/qquickwebpage.cpp:
    (QQuickWebPagePrivate::paint):
    * UIProcess/API/qt/qquickwebpage_p_p.h:
    (QQuickWebPagePrivate):
    * UIProcess/DrawingAreaProxy.h:
    * UIProcess/DrawingAreaProxyImpl.cpp:
    * UIProcess/DrawingAreaProxyImpl.h:
    (DrawingAreaProxyImpl):
    * UIProcess/LayerTreeHostProxy.cpp:
    * UIProcess/LayerTreeHostProxy.h:
    (LayerTreeHostProxy):
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::paintToCurrentGLContext):
    (WebKit::WebLayerTreeRenderer::paintToGraphicsContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebpage_p_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebpage_p_p.h
@@ -41,7 +41,6 @@ public:
 
     void updateSize();
 
-    void paintToCurrentGLContext(const QTransform&, float opacity, const QRectF& clipRect);
     void paint(QPainter*);
     void resetPaintNode();
 
--- third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
@@ -122,7 +122,6 @@ void WebLayerTreeRenderer::paintToCurrentGLContext(const TransformationMatrix& m
 
     layer->setTextureMapper(m_textureMapper.get());
     m_textureMapper->beginPainting();
-    m_textureMapper->bindSurface(0);
     m_textureMapper->beginClip(TransformationMatrix(), clipRect);
 
     if (currentRootLayer->opacity() != opacity || currentRootLayer->transform() != matrix) {
@@ -150,7 +149,6 @@ void WebLayerTreeRenderer::paintToGraphicsContext(QPainter* painter)
     GraphicsContext graphicsContext(painter);
     m_textureMapper->setGraphicsContext(&graphicsContext);
     m_textureMapper->beginPainting();
-    m_textureMapper->bindSurface(0);
     layer->paint();
     m_textureMapper->endPainting();
     m_textureMapper->setGraphicsContext(0);
