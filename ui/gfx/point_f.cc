2b5d1179577df142e18d002642b70316d31d2b93
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 17:20:57 +0000

    Fix typo
    TBR=sky@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10054014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131786 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/point_f.cc
+++ ui/gfx/point_f.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-template class PointBase<Point, float>;
+template class PointBase<PointF, float>;
 
 PointF::PointF() : PointBase<PointF, float>(0, 0) {
 }
