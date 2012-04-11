05950cf185d5c240986983c6eb907b313b782fa8
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 17:23:53 +0000

    Comments for RenderWidgetHostView Class Hierarchy
    Review URL: https://chromiumcodereview.appspot.com/9960078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131788 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -37,6 +37,8 @@ class RenderWidgetHostImpl;
 //
 // To enable embedders that add ports, everything else in content/
 // should use the RenderWidgetHostViewPort interface.
+//
+// RenderWidgetHostView class hierarchy described in render_widget_host_view.h.
 class CONTENT_EXPORT RenderWidgetHostViewBase
     : public RenderWidgetHostViewPort {
  public:
