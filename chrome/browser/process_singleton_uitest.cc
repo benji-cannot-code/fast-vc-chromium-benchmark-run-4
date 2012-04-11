f0e15b7371246eac1c4c18eea8100908685a532b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 16:27:01 +0000

    vectors are cleared with "clear()", not with "empty()".
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10034006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131780 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton_uitest.cc
+++ chrome/browser/process_singleton_uitest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -316,7 +316,7 @@ TEST_F(ProcessSingletonTest, MAYBE_StartupRaceCondition) {
     // "There can be only one!" :-)
     ASSERT_EQ(static_cast<size_t>(1), pending_starters.size());
     size_t last_index = pending_starters.front();
-    pending_starters.empty();
+    pending_starters.clear();
     if (chrome_starters_[last_index]->process_handle_ !=
         base::kNullProcessHandle) {
       KillProcessTree(chrome_starters_[last_index]->process_handle_);
