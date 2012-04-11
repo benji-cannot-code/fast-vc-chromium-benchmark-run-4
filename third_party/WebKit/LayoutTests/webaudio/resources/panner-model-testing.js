b39ddca41b2b2256c77cda05dd4e5418f5a28c39
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 19:26:28 +0000

    add stereo source support in EqualPowerPanner https://bugs.webkit.org/show_bug.cgi?id=81092
    Patch by Wei James <james.wei@intel.com> on 2012-04-11
    Reviewed by Chris Rogers.
    
    Source/WebCore:
    
    Test: webaudio/panner-equalpower-stereo.html
    
    * platform/audio/EqualPowerPanner.cpp:
    (WebCore::EqualPowerPanner::pan):
    
    LayoutTests:
    
    * webaudio/panner-equalpower-stereo-expected.txt: Added.
    * webaudio/panner-equalpower-stereo.html: Copied from LayoutTests/webaudio/panner-equalpower.html.
    * webaudio/panner-equalpower.html:
    * webaudio/resources/audio-testing.js:
    (createStereoImpulseBuffer):
    * webaudio/resources/panner-model-testing.js:
    (createGraph):
    (createTestAndRun):
    (equalPowerGain):
    (checkResult):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/webaudio/resources/panner-model-testing.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/panner-model-testing.js
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var sampleRate = 44100.0;
 
+var numberOfChannels = 1;
+
 // Time step when each panner node starts.
 var timeStep = 0.001;
 
@@ -34,7 +36,11 @@ function createGraph(context, nodeCount) {
     // to include both 0 and 180 deg.
     var angleStep = Math.PI / (nodeCount - 1);
 
-    impulse = createImpulseBuffer(context, pulseLengthFrames);
+    if (numberOfChannels == 2) {
+        impulse = createStereoImpulseBuffer(context, pulseLengthFrames);
+    }
+    else
+        impulse = createImpulseBuffer(context, pulseLengthFrames);
 
     for (var k = 0; k < nodeCount; ++k) {
         bufferSource[k] = context.createBufferSource();
@@ -57,7 +63,9 @@ function createGraph(context, nodeCount) {
     }
 }
 
-function createTestAndRun(context, nodeCount) {
+function createTestAndRun(context, nodeCount, numberOfSourceChannels) {
+    numberOfChannels = numberOfSourceChannels;
+
     createGraph(context, nodeCount);
 
     context.oncomplete = checkResult;
@@ -75,12 +83,30 @@ function angleToAzimuth(angle) {
 function equalPowerGain(angle) {
     var azimuth = angleToAzimuth(angle);
 
-    var panPosition = (azimuth + 90) / 180;
+    if (numberOfChannels == 1) {
+        var panPosition = (azimuth + 90) / 180;
 
-    var gainL = Math.cos(0.5 * Math.PI * panPosition);
-    var gainR = Math.sin(0.5 * Math.PI * panPosition);
+        var gainL = Math.cos(0.5 * Math.PI * panPosition);
+        var gainR = Math.sin(0.5 * Math.PI * panPosition);
 
-    return { left : gainL, right : gainR };
+        return { left : gainL, right : gainR };
+    } else {
+        if (azimuth <= 0) {
+            var panPosition = (azimuth + 90) / 90;
+    
+            var gainL = 1 + Math.cos(0.5 * Math.PI * panPosition);
+            var gainR = Math.sin(0.5 * Math.PI * panPosition);
+    
+            return { left : gainL, right : gainR };
+        } else {
+            var panPosition = azimuth / 90;
+    
+            var gainL = Math.cos(0.5 * Math.PI * panPosition);
+            var gainR = 1 + Math.sin(0.5 * Math.PI * panPosition);
+    
+            return { left : gainL, right : gainR };
+        }
+    }
 }
 
 function checkResult(event) {
@@ -91,7 +117,7 @@ function checkResult(event) {
     // The max error we allow between the rendered impulse and the
     // expected value.  This value is experimentally determined.  Set
     // to 0 to make the test fail to see what the actual error is.
-    var maxAllowedError = 6.4e-7;
+    var maxAllowedError = 1.3e-6;
   
     var success = true;
 
