df15b696cb8e7e938a722cbbcc3b2644ca8ff79d
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 13:41:51 +0000

    Shadow tree TreeScope data is not removed by ContainerNode::removeAllChildren https://bugs.webkit.org/show_bug.cgi?id=83484
    Unreviewed attempted build fix.
    
    * dom/ContainerNodeAlgorithms.h: Adding Document.h include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNodeAlgorithms.h
+++ third_party/WebKit/Source/WebCore/dom/ContainerNodeAlgorithms.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ContainerNodeAlgorithms_h
 #define ContainerNodeAlgorithms_h
 
+#include <Document.h>
 #include <wtf/Assertions.h>
 
 namespace WebCore {
