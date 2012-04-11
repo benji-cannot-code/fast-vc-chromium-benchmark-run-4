aad4f1c799e1ace5d471c8a1100ddfb9efaf7297
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 01:37:47 +0000

    webkit-patch: add a print-expectations command https://bugs.webkit.org/show_bug.cgi?id=83347
    Reviewed by Adam Barth.
    
    This command will print the expected results for a given set of
    tests on a given set of ports matching a given set of keywords.
    
    Example output (printing all the tests not expected to pass, and their
    expected results, on a Mac):
    
    $ webkit-patch print-expectations -x pass fast/html
    // For mac-snowleopard
    fast/html/details-open4.html = TEXT
    fast/html/details-open2.html = TEXT
    fast/html/details-no-summary4.html = TEXT
    fast/html/details-open-javascript.html = TEXT
    $
    
    You can also print full test-expectatons.txt lines using --full and a
    CSV-style report (which can be useful for post-processing) using --csv.
    
    It will replace the 'skipped-files' command (which will be
    removed in a subsequent patch) and is a more general (and
    cleaner and properly layered) solution.
    
    Also add an update() command to MockOptions() to make overriding keywords easier.
    
    * Scripts/webkitpy/layout_tests/models/test_expectations.py:
    (TestExpectationSerializer.to_string):
    (TestExpectationSerializer):
    (TestExpectationSerializer.to_csv):
    (TestExpectationSerializer._format_result):
    (TestExpectationLine.create_passing_expectation):
    (TestExpectationsModel.get_test_set_for_keyword):
    (TestExpectationsModel.has_keyword):
    (TestExpectations.model):
    * Scripts/webkitpy/tool/commands/queries.py:
    (execute):
    (PrintExpectations):
    (PrintExpectations.__init__):
    (PrintExpectations.execute):
    (PrintExpectations._filter_tests):
    (PrintExpectations._format_lines):
    (PrintExpectations._model):
    * Scripts/webkitpy/tool/commands/queries_unittest.py:
    (TestPrintExpectations): Added.
    * Scripts/webkitpy/tool/mocktool.py:
    (MockOptions):
    (MockOptions.__init__):
    (MockOptions.update): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/queries_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/queries_unittest.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.common.net.bugzilla import Bugzilla
 from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.thirdparty.mock import Mock
@@ -117,6 +118,54 @@ class FailureReasonTest(unittest.TestCase):
             raise Exception("MESSAGE")
         tool.checkout().commit_info_for_revision = raising_mock
         self.assertEquals(command._blame_line_for_revision(None), "FAILED to fetch CommitInfo for rNone, exception: MESSAGE")
+
+
+class PrintExpectationsTest(unittest.TestCase):
+    def run_test(self, tests, expected_stdout, **args):
+        options = MockOptions(all=False, csv=False, full=False, platform='test-win-xp',
+                              include_keyword=[], exclude_keyword=[]).update(**args)
+        tool = MockTool()
+        command = PrintExpectations()
+        command.bind_to_tool(tool)
+
+        oc = OutputCapture()
+        try:
+            oc.capture_output()
+            command.execute(options, tests, tool)
+        finally:
+            stdout, _, _ = oc.restore_output()
+        self.assertEquals(stdout, expected_stdout)
+
+    def test_basic(self):
+        self.run_test(['failures/expected/text.html', 'failures/expected/image.html'],
+                      ('// For test-win-xp\n'
+                       'failures/expected/image.html = IMAGE\n'
+                       'failures/expected/text.html = TEXT\n'))
+
+    def test_full(self):
+        self.run_test(['failures/expected/text.html', 'failures/expected/image.html'],
+                      ('// For test-win-xp\n'
+                       'WONTFIX : failures/expected/image.html = IMAGE\n'
+                       'WONTFIX : failures/expected/text.html = TEXT\n'),
+                      full=True)
+
+    def test_exclude(self):
+        self.run_test(['failures/expected/text.html', 'failures/expected/image.html'],
+                      ('// For test-win-xp\n'
+                       'failures/expected/text.html = TEXT\n'),
+                      exclude_keyword=['image'])
+
+    def test_include(self):
+        self.run_test(['failures/expected/text.html', 'failures/expected/image.html'],
+                      ('// For test-win-xp\n'
+                       'failures/expected/image.html\n'),
+                      include_keyword=['image'])
+
+    def test_csv(self):
+        self.run_test(['failures/expected/text.html', 'failures/expected/image.html'],
+                      ('test-win-xp,failures/expected/image.html,wontfix,image\n'
+                       'test-win-xp,failures/expected/text.html,wontfix,text\n'),
+                      csv=True)
 
 
 class PrintBaselinesTest(unittest.TestCase):
