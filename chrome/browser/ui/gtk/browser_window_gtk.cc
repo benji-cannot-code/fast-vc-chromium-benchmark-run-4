85bf4b8fb487cc1d090f3bb9dc64108004db9710
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 13:45:29 +0000

    gtk: Refactor download-in-progress class to match with the views side.
    R=erg@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10024048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_window_gtk.cc
+++ chrome/browser/ui/gtk/browser_window_gtk.cc
@@ -1286,7 +1286,7 @@ void BrowserWindowGtk::ShowPasswordGenerationBubble(const gfx::Rect& rect) {
 }
 
 void BrowserWindowGtk::ConfirmBrowserCloseWithPendingDownloads() {
-  new DownloadInProgressDialogGtk(browser());
+  DownloadInProgressDialogGtk::Show(browser(), GetNativeHandle());
 }
 
 void BrowserWindowGtk::Observe(int type,
