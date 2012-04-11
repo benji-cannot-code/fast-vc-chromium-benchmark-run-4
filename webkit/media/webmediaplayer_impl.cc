c5fc69b5b0c71c217b1606ecd6d9eae00522bb29
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 02:34:48 +0000

    Add "filesystem" to the list of media-serving schemes logged to UMA.
    Review URL: http://codereview.chromium.org/10038006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131700 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_impl.cc
+++ webkit/media/webmediaplayer_impl.cc
@@ -188,7 +188,8 @@ enum URLSchemeForHistogram {
   kFileURLScheme,
   kBlobURLScheme,
   kDataURLScheme,
-  kMaxURLScheme = kDataURLScheme  // Must be equal to highest enum value.
+  kFileSystemScheme,
+  kMaxURLScheme = kFileSystemScheme  // Must be equal to highest enum value.
 };
 
 URLSchemeForHistogram URLScheme(const GURL& url) {
@@ -201,6 +202,7 @@ URLSchemeForHistogram URLScheme(const GURL& url) {
   if (url.SchemeIs("file")) return kFileURLScheme;
   if (url.SchemeIs("blob")) return kBlobURLScheme;
   if (url.SchemeIs("data")) return kDataURLScheme;
+  if (url.SchemeIs("filesystem")) return kFileSystemScheme;
   return kUnknownURLScheme;
 }
 
