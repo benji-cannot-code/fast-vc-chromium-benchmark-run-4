bba7c454b0a5ec57ed8d0c79fe59f5a9c591295b
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 02:58:36 +0000

    Make idle logout window not cause the app list to disappear. When the app list is up, create the idle logout window as a child of the app list. This way the idle logout window will still always be on top but will not cause the app list to disappear.
    R=xiyuan@chromium.org
    BUG=chromium-os:28683
    TEST=Tested that the window comes up on top of the app list and goes away; both without causing the app list window to be disturbed.
    
    Review URL: https://chromiumcodereview.appspot.com/10031050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131706 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/ui/idle_logout_dialog_view.cc
+++ chrome/browser/chromeos/ui/idle_logout_dialog_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/ui/idle_logout_dialog_view.h"
 
+#include "ash/shell.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/time.h"
@@ -171,8 +172,15 @@ void IdleLogoutDialogView::Show() {
 
   UpdateCountdown();
 
-  views::Widget::CreateWindow(this);
-  GetWidget()->SetAlwaysOnTop(true);
+  // If the apps list is displayed, we should show as it's child. Not doing
+  // so will cause the apps list to disappear.
+  gfx::NativeWindow app_list = ash::Shell::GetInstance()->GetAppListWindow();
+  if (app_list) {
+    views::Widget::CreateWindowWithParent(this, app_list);
+  } else {
+    views::Widget::CreateWindow(this);
+    GetWidget()->SetAlwaysOnTop(true);
+  }
   GetWidget()->Show();
 
   // Update countdown every 1 second.
