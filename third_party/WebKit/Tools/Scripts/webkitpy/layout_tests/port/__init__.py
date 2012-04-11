a41863212098b862817dff323ecb5d2853345d79
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 01:30:24 +0000

    webkitpy: refactor handling of --platform and related options https://bugs.webkit.org/show_bug.cgi?id=83525
    Reviewed by Adam Barth.
    
    This change moves to centralize handling of --platform, --debug,
    --gtk, and other similar flags into a central place next to the
    code that actually uses those flags in PortFactory to get the
    right Port object.
    
    * Scripts/webkitpy/layout_tests/port/__init__.py:
    * Scripts/webkitpy/layout_tests/port/factory.py:
    (port_options):
    * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    (parse_args):
    * Scripts/webkitpy/tool/commands/queries.py:
    (PrintBaselines.__init__):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/__init__.py
@@ -34,3 +34,4 @@ import builders  # Why is this in port?
 
 from base import Port  # It's possible we don't need to export this virtual baseclass outside the module.
 from driver import Driver, DriverInput, DriverOutput
+from factory import port_options
