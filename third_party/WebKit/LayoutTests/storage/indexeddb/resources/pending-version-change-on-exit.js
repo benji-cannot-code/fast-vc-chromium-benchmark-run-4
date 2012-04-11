fb1e8f7a6eecae56e8a6c61dea010e241db4dd57
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-11 02:59:54 +0000

    Notify observers of WorkerRunLoop stopping before the V8 isolate dies. https://bugs.webkit.org/show_bug.cgi?id=83104
    Source/WebCore:
    
    PlatformSupport::didStopWorkerRunLoop ultimately causes
    ~V8AbstractEventListener to call
    v8::Local<v8::Object>::New(m_listener) after the V8 isolate has been
    disposed, which manifests as a crash in V8.
    
    The current code in trunk runs this at shutdown:
    1) removeAllDOMObjects()
    2) dispose of V8
    3) didStopWorkerRunLoop()  <-- problem
    
    This patch changes the order to be:
    1) removeAllDOMObjects()
    2) didStopWorkerRunLoop()
    3) dispose of V8
    
    We put didStopWorkerRunLoop after removeAllDOMObjects because we don't
    want chromium code that runs on a webcore worker to run after it
    receives the didStopWorkerRunLoop signal. The destructors of some IDB
    objects are run by removeAllDOMObjects, so putting
    didStopWorkerRunLoop before removeAllDOMObjects would violate that
    constraint.
    
    It's possible that there's a lower layer fix available in V8 or the
    bindings.
    
    Reviewed by David Levin.
    
    Test: storage/indexeddb/pending-version-change-on-exit.html
    
    * bindings/v8/WorkerScriptController.cpp:
    (WebCore::WorkerScriptController::~WorkerScriptController):
    New location of didStopWorkerRunLoop. removeAllDOMObjects and V8
    disposal are called here, to run something between them it also has
    to go here.
    
    * workers/WorkerThread.cpp:
    (WebCore::WorkerThread::workerThread): Old location of
    didStopWorkerRunLoop.
    
    LayoutTests:
    
    Reviewed by David Levin.
    
    * storage/indexeddb/pending-version-change-on-exit-expected.txt: Added.
    * storage/indexeddb/pending-version-change-on-exit.html: Added.
    * storage/indexeddb/resources/pending-version-change-on-exit.js: Added.
    (test.request.onsuccess.request.onblocked):
    (test.request.onsuccess):
    (test):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113818 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/storage/indexeddb/resources/pending-version-change-on-exit.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (this.importScripts) {
+    importScripts('../../../fast/js/resources/js-test-pre.js');
+    importScripts('shared.js');
+}
+
+function test() {
+  removeVendorPrefixes();
+  dbname = "pending-version-change-on-exit";
+  evalAndLog("request = indexedDB.open(\"" + dbname + "\")");
+  request.onsuccess = function(e) {
+    db = request.result;
+    evalAndLog("request = db.setVersion(1)");
+    request.onsuccess = unexpectedSuccessCallback;
+    request.onblocked = function() {
+      testPassed("worker received blocked event.");
+      finishJSTest();
+    };
+  };
+}
+
+test();
--- third_party/WebKit/Source/WebCore/workers/WorkerThread.cpp
+++ third_party/WebKit/Source/WebCore/workers/WorkerThread.cpp
@@ -143,6 +143,8 @@ void WorkerThread::workerThread()
         }
     }
 #if PLATFORM(CHROMIUM)
+    // The corresponding call to didStopWorkerRunLoop is in
+    // ~WorkerScriptController.
     PlatformSupport::didStartWorkerRunLoop(&m_runLoop);
 #endif
 
@@ -157,10 +159,6 @@ void WorkerThread::workerThread()
     m_startupData.clear();
 
     runEventLoop();
-
-#if PLATFORM(CHROMIUM)
-    PlatformSupport::didStopWorkerRunLoop(&m_runLoop);
-#endif
 
     ThreadIdentifier threadID = m_threadID;
 
