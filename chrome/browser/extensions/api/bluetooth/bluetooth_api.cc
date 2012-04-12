5f089cd12760817544e0bffadaa737bdf5c596ff
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 04:21:49 +0000

    Fix getDevicesWithService.
    The Value passing was wrong.  This fixes it.
    
    TEST=my demo app outputs devices now
    BUG=none
    
    
    Review URL: http://codereview.chromium.org/10020056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
@@ -51,7 +51,7 @@ bool BluetoothGetDevicesWithServiceFunction::RunImpl() {
 
   BluetoothAdapter::ConstDeviceList devices = adapter()->GetDevices();
 
-  ListValue* matches = new ListValue();
+  ListValue* matches = new ListValue;
   for (BluetoothAdapter::ConstDeviceList::const_iterator i =
       devices.begin(); i != devices.end(); ++i) {
     const BluetoothDevice::ServiceList& services = (*i)->GetServices();
@@ -61,7 +61,7 @@ bool BluetoothGetDevicesWithServiceFunction::RunImpl() {
         experimental_bluetooth::Device device;
         device.name = UTF16ToUTF8((*i)->GetName());
         device.address = (*i)->address();
-        matches->Append(device.ToValue().get());
+        matches->Append(device.ToValue().release());
       }
     }
   }
