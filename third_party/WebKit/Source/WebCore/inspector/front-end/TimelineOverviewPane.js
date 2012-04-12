6fa1ef9ce9eb0753a6a7a0912db4dabb0b69d4a7
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-12 16:40:23 +0000

    Web Inspector: zoom timeline frame on dblclick on the frame bar https://bugs.webkit.org/show_bug.cgi?id=83741
    Reviewed by Pavel Feldman.
    
           - put timeline frame bars into a container, so we can have one listener;
           - upon dblclick, find the frame, get its window coordinates and set overview selection window accordingly.
    
    * inspector/front-end/TimelineOverviewPane.js:
    (WebInspector.TimelineOverviewPane.prototype.zoomToFrame): Get frame coordinates, set overview window.
    (WebInspector.TimelineVerticalOverview.prototype.update): Maintain framesPerBar as a member for reuse in getFramePosition()
    (WebInspector.TimelineVerticalOverview.prototype.framePosition): Map frame to screen positions.
    (WebInspector.TimelineVerticalOverview.prototype._renderBars): Factored out barNumberToScreenPosition()
    (WebInspector.TimelineVerticalOverview.prototype._barNumberToScreenPosition):
    (WebInspector.TimelineVerticalOverview.prototype.getWindowTimes): Drive-by style fix.
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._updateFrames): Put frames into a containing div.
    (WebInspector.TimelinePanel.prototype._onFrameDoubleClicked): Zoom to frame upon double click.
    * inspector/front-end/timelinePanel.css:
    (.timeline-frame-container):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelineOverviewPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelineOverviewPane.js
@@ -293,6 +293,18 @@ WebInspector.TimelineOverviewPane.prototype = {
         this._scheduleRefresh();
     },
 
+    /**
+     * @param {WebInspector.TimelineFrame} frame
+     */
+    zoomToFrame: function(frame)
+    {
+        var window = this._verticalOverview.framePosition(frame);
+        if (!window)
+            return;
+
+        this._overviewWindow._setWindowPosition(window.start, window.end);
+    },
+
     _onRecordAdded: function(event)
     {
         var record = event.data;
@@ -924,9 +936,9 @@ WebInspector.TimelineVerticalOverview.prototype = {
     update: function()
     {
         const minBarWidth = 4;
-        var framesPerBar = Math.max(1, this._frames.length * minBarWidth / this.element.clientWidth);
+        this._framesPerBar = Math.max(1, this._frames.length * minBarWidth / this.element.clientWidth);
         this._barTimes = [];
-        var visibleFrames = this._aggregateFrames(framesPerBar);
+        var visibleFrames = this._aggregateFrames(this._framesPerBar);
 
         const paddingTop = 4;
         var scale = (this.element.clientHeight - paddingTop) / this._normalBarLength;
@@ -940,6 +952,20 @@ WebInspector.TimelineVerticalOverview.prototype = {
     addFrame: function(frame)
     {
         this._frames.push(frame);
+    },
+
+    framePosition: function(frame)
+    {
+        var frameNumber = this._frames.indexOf(frame);
+        if (frameNumber < 0)
+            return;
+        var barNumber = Math.floor(frameNumber / this._framesPerBar);
+        var firstBar = this._framesPerBar > 1 ? barNumber : Math.max(barNumber - 1, 0);
+        var lastBar = this._framesPerBar > 1 ? barNumber : Math.min(barNumber + 1, this._barTimes.length - 1);
+        return {
+            start: Math.ceil(this._barNumberToScreenPosition(firstBar) - this._actualPadding / 2),
+            end: Math.floor(this._barNumberToScreenPosition(lastBar + 1) - this._actualPadding / 2)
+        }
     },
 
     /**
@@ -984,10 +1010,14 @@ WebInspector.TimelineVerticalOverview.prototype = {
         this._actualPadding = Math.min(Math.floor(this._actualOuterBarWidth / 3), maxPadding);
 
         for (var i = 0; i < frames.length; ++i) {
-            var x = this._outerPadding + this._actualOuterBarWidth * i;
             var width = this._actualOuterBarWidth - this._actualPadding;
-            this._renderBar(x, width, frames[i], scale);
+            this._renderBar(this._barNumberToScreenPosition(i), width, frames[i], scale);
         }
+    },
+
+    _barNumberToScreenPosition: function(n)
+    {
+        return this._outerPadding + this._actualOuterBarWidth * n;
     },
 
     _renderBar: function(left, width, frame, scale)
@@ -1034,7 +1064,7 @@ WebInspector.TimelineVerticalOverview.prototype = {
 
     getWindowTimes: function(windowLeft, windowRight)
     {
-        var windowSpan = this.element.clientWidth
+        var windowSpan = this.element.clientWidth;
         var leftOffset = windowLeft * windowSpan - this._outerPadding + this._actualPadding;
         var rightOffset = windowRight * windowSpan - this._outerPadding;
         var bars = this.element.children;
