d4612df18a0643c48c60680c7db79f0c27dbcec4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-12 03:48:45 +0000

    AudioContext createChannelSplitter() method should have optional argument for number of outputs https://bugs.webkit.org/show_bug.cgi?id=83648
    Patch by Raymond Liu <raymond.liu@intel.com> on 2012-04-11
    Reviewed by Chris Rogers.
    
    Check https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html for the optional argument.
    
    Source/WebCore:
    
    Test: webaudio/audiochannelsplitter.html
    
    * Modules/webaudio/AudioChannelSplitter.cpp:
    (WebCore::AudioChannelSplitter::create):
    (WebCore::AudioChannelSplitter::AudioChannelSplitter):
    (WebCore::AudioChannelSplitter::process):
    * Modules/webaudio/AudioChannelSplitter.h:
    (AudioChannelSplitter):
    * Modules/webaudio/AudioContext.cpp:
    (WebCore::AudioContext::createChannelSplitter):
    (WebCore):
    * Modules/webaudio/AudioContext.h:
    (AudioContext):
    * Modules/webaudio/AudioContext.idl:
    
    LayoutTests:
    
    * webaudio/audiochannelsplitter-expected.txt: Added.
    * webaudio/audiochannelsplitter.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelSplitter.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelSplitter.cpp
@@ -29,22 +29,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AudioChannelSplitter.h"
 
+#include "AudioContext.h"
 #include "AudioNodeInput.h"
 #include "AudioNodeOutput.h"
 
 namespace WebCore {
     
-// This is considering that 5.1 (6 channels) is the largest we'll ever deal with.
-// It can easily be increased to support more if the web audio specification is updated.
-const unsigned NumberOfOutputs = 6;
+PassRefPtr<AudioChannelSplitter> AudioChannelSplitter::create(AudioContext* context, float sampleRate, unsigned numberOfOutputs)
+{
+    if (!numberOfOutputs || numberOfOutputs > AudioContext::maxNumberOfChannels())
+        return 0;
 
-AudioChannelSplitter::AudioChannelSplitter(AudioContext* context, float sampleRate)
+    return adoptRef(new AudioChannelSplitter(context, sampleRate, numberOfOutputs));      
+}
+
+AudioChannelSplitter::AudioChannelSplitter(AudioContext* context, float sampleRate, unsigned numberOfOutputs)
     : AudioNode(context, sampleRate)
 {
     addInput(adoptPtr(new AudioNodeInput(this)));
 
     // Create a fixed number of outputs (able to handle the maximum number of channels fed to an input).
-    for (unsigned i = 0; i < NumberOfOutputs; ++i)
+    for (unsigned i = 0; i < numberOfOutputs; ++i)
         addOutput(adoptPtr(new AudioNodeOutput(this, 1)));
     
     setNodeType(NodeTypeChannelSplitter);
@@ -60,8 +65,7 @@ void AudioChannelSplitter::process(size_t framesToProcess)
     
     unsigned numberOfSourceChannels = source->numberOfChannels();
     
-    ASSERT(numberOfOutputs() == NumberOfOutputs);
-    for (unsigned i = 0; i < NumberOfOutputs; ++i) {
+    for (unsigned i = 0; i < numberOfOutputs(); ++i) {
         AudioBus* destination = output(i)->bus();
         ASSERT(destination);
         
--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelSplitter.h
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelSplitter.h
@@ -35,10 +35,7 @@ class AudioContext;
     
 class AudioChannelSplitter : public AudioNode {
 public:
-    static PassRefPtr<AudioChannelSplitter> create(AudioContext* context, float sampleRate)
-    {
-        return adoptRef(new AudioChannelSplitter(context, sampleRate));      
-    }
+    static PassRefPtr<AudioChannelSplitter> create(AudioContext*, float sampleRate, unsigned numberOfOutputs);
 
     // AudioNode
     virtual void process(size_t framesToProcess);
@@ -48,7 +45,7 @@ private:
     virtual double tailTime() const OVERRIDE { return 0; }
     virtual double latencyTime() const OVERRIDE { return 0; }
 
-    AudioChannelSplitter(AudioContext*, float sampleRate);
+    AudioChannelSplitter(AudioContext*, float sampleRate, unsigned numberOfOutputs);
 };
 
 } // namespace WebCore
