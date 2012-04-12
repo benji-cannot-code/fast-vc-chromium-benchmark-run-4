3163e960814d7c99345be940e22764541144f37b
serya@google.com <serya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 18:45:56 +0000

    Fix BookmarksArrayDataModel.
    BUG=122816
    TEST=Manual test.
    
    Review URL: https://chromiumcodereview.appspot.com/10067005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/bmm/bookmark_list.js
+++ chrome/browser/resources/bookmark_manager/js/bmm/bookmark_list.js
@@ -21,7 +21,6 @@ cr.define('bmm', function() {
    * @extends {ArrayDataModel}
    */
   function BookmarksArrayDataModel(items) {
-    this.bookmarksArrayDataModelArray_ = items;
     ArrayDataModel.call(this, items);
   }
 
@@ -34,10 +33,8 @@ cr.define('bmm', function() {
      * @return {number} The index of the found node or -1 if not found.
      */
     findIndexById: function(id) {
-      var arr = this.bookmarksArrayDataModelArray_;
-      var length = arr.length
-      for (var i = 0; i < length; i++) {
-        if (arr[i].id == id)
+      for (var i = 0; i < this.length; i++) {
+        if (this.item(i).id == id)
           return i;
       }
       return -1;
@@ -248,7 +245,7 @@ cr.define('bmm', function() {
         // We create a new data model with updated items in the right order.
         var dataModel = this.dataModel;
         var items = {};
-        for (var i = this.dataModel.length -1 ; i >= 0; i--) {
+        for (var i = this.dataModel.length - 1; i >= 0; i--) {
           var bookmarkNode = dataModel.item(i);
           items[bookmarkNode.id] = bookmarkNode;
         }
