ddde9525989f442d25174275971ef1914195b0ee
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-12 01:13:07 +0000

    Unreviewed attempting to make Qt's eccentric hardware work.
    * assembler/MacroAssemblerARM.h:
    (JSC::MacroAssemblerARM::compare8):
    (MacroAssemblerARM):
    * assembler/MacroAssemblerMIPS.h:
    (JSC::MacroAssemblerMIPS::compare8):
    (MacroAssemblerMIPS):
    * assembler/MacroAssemblerSH4.h:
    (JSC::MacroAssemblerSH4::compare8):
    (MacroAssemblerSH4):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@113934 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
@@ -688,6 +688,12 @@ public:
         m_assembler.mov_r(dest, ARMAssembler::getOp2(1), ARMCondition(cond));
     }
 
+    void compare8(RelationalCondition cond, AbsoluteAddress left, TrustedImm32 right, RegisterID dest)
+    {
+        load8(left.m_ptr, ARMRegisters::S1);
+        compare32(cond, ARMRegisters::S1, right, dest);
+    }
+
     void test32(ResultCondition cond, RegisterID reg, TrustedImm32 mask, RegisterID dest)
     {
         if (mask.m_value == -1)
@@ -744,7 +750,7 @@ public:
         load32(left.m_ptr, ARMRegisters::S1);
         return branch32(cond, ARMRegisters::S1, right);
     }
-
+    
     Jump branch32(RelationalCondition cond, AbsoluteAddress left, TrustedImm32 right)
     {
         load32(left.m_ptr, ARMRegisters::S1);
