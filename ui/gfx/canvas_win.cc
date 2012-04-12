dc4b2e180dcf97d62817bdb8d1217b38a99d78fe
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 23:05:18 +0000

    Fix Windows use_canvas_skia=0 build.
    This got broken by http://crrev.com/131696 because use_canvas_skia=1 is
    currently the default for Windows.
    
    BUG=105550
    TEST=Compiles on Windows with use_canva_skia=0.
    
    Review URL: http://codereview.chromium.org/10038024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132079 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas_win.cc
+++ ui/gfx/canvas_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/shadow_value.h"
 
 namespace {
 
