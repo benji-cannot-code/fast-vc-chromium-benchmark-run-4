37270c3b7901707c1ebcb1f3a8dea106440fa6af
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 11:12:13 +0000

    Revert 131963 - Added "Store Debug Logs" functionality. Logs from /var/log are gzipped and stored on the fileshelf.
    BUG=chromium-os:25700
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9965072
    
    TBR=ygorshenin@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10069009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/chromeos_view.js
+++ chrome/browser/resources/net_internals/chromeos_view.js
@@ -126,17 +126,7 @@ var CrosView = (function() {
   }
 
   /**
-   *  Set storing debug logs status.
-   *
-   *  @private
-   */
-  function setStoreDebugLogsStatus_(status) {
-    $(CrosView.STORE_DEBUG_LOGS_STATUS_ID).innerText = status;
-  }
-
-  /**
-   *  Add event listeners for the file selection, passcode input
-   *  fields and for the button for debug logs storing.
+   *  Add event listeners for the file selection and passcode input fields.
    *
    *  @private
    */
@@ -147,11 +137,6 @@ var CrosView = (function() {
 
     $(CrosView.PASSCODE_INPUT_ID).addEventListener('change', function(event) {
       setPasscode_(this.value);
-    }, false);
-
-    $(CrosView.STORE_DEBUG_LOGS_ID).addEventListener('click', function(event) {
-      $(CrosView.STORE_DEBUG_LOGS_STATUS_ID).innerText = '';
-      g_browser.storeDebugLogs();
     }, false);
   }
 
@@ -177,7 +162,6 @@ var CrosView = (function() {
     DivView.call(this, CrosView.MAIN_BOX_ID);
 
     g_browser.addCrosONCFileParseObserver(this);
-    g_browser.addStoreDebugLogsObserver(this);
     addEventListeners_();
   }
 
@@ -190,8 +174,6 @@ var CrosView = (function() {
   CrosView.PASSCODE_ID = 'chromeos-view-password-div';
   CrosView.PASSCODE_INPUT_ID = 'chromeos-view-onc-password';
   CrosView.PARSE_STATUS_ID = 'chromeos-view-parse-status';
-  CrosView.STORE_DEBUG_LOGS_ID = 'chromeos-view-store-debug-logs';
-  CrosView.STORE_DEBUG_LOGS_STATUS_ID = 'chromeos-view-store-debug-logs-status';
 
   cr.addSingletonGetter(CrosView);
 
@@ -200,7 +182,6 @@ var CrosView = (function() {
     __proto__: DivView.prototype,
 
     onONCFileParse: setParseStatus_,
-    onStoreDebugLogs: setStoreDebugLogsStatus_,
   };
 
   return CrosView;
