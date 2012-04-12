93cdcfecc13bfa79dc0c227ab6e91eb12113017c
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 21:10:14 +0000

    Restrict the storage API to chrome dev channel.
    I checked that this actually works by hacking chrome_version_info_posix.cc to
    make it return beta (it turns out that setting CHROME_VERSION_EXTRA isn't
    enough by itself)
    
    
    BUG=121826
    TEST=still tested by storage browser/unittests and feature unittests
    
    
    Review URL: http://codereview.chromium.org/10069001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132059 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/feature.cc
+++ chrome/common/extensions/feature.cc
@@ -51,8 +51,19 @@ struct Mappings {
 
 static base::LazyInstance<Mappings> g_mappings = LAZY_INSTANCE_INITIALIZER;
 
+std::string GetChannelName(VersionInfo::Channel channel) {
+  typedef std::map<std::string, VersionInfo::Channel> ChannelsMap;
+  ChannelsMap channels = g_mappings.Get().channels;
+  for (ChannelsMap::iterator i = channels.begin(); i != channels.end(); ++i) {
+    if (i->second == channel)
+      return i->first;
+  }
+  NOTREACHED();
+  return "unknown";
+}
+
 struct Channel {
-  Channel() : channel(chrome::VersionInfo::GetChannel()) {}
+  Channel() : channel(VersionInfo::GetChannel()) {}
 
   chrome::VersionInfo::Channel channel;
 };
@@ -138,7 +149,7 @@ Feature::Feature()
     platform_(UNSPECIFIED_PLATFORM),
     min_manifest_version_(0),
     max_manifest_version_(0),
-    supported_channel_(VersionInfo::CHANNEL_UNKNOWN) {
+    channel_(VersionInfo::CHANNEL_UNKNOWN) {
 }
 
 Feature::Feature(const Feature& other)
@@ -149,7 +160,7 @@ Feature::Feature(const Feature& other)
       platform_(other.platform_),
       min_manifest_version_(other.min_manifest_version_),
       max_manifest_version_(other.max_manifest_version_),
-      supported_channel_(other.supported_channel_) {
+      channel_(other.channel_) {
 }
 
 Feature::~Feature() {
@@ -163,7 +174,7 @@ bool Feature::Equals(const Feature& other) const {
       platform_ == other.platform_ &&
       min_manifest_version_ == other.min_manifest_version_ &&
       max_manifest_version_ == other.max_manifest_version_ &&
-      supported_channel_ == other.supported_channel_;
+      channel_ == other.channel_;
 }
 
 // static
@@ -196,7 +207,7 @@ void Feature::Parse(const DictionaryValue* value) {
   value->GetInteger("min_manifest_version", &min_manifest_version_);
   value->GetInteger("max_manifest_version", &max_manifest_version_);
   ParseEnum<VersionInfo::Channel>(
-      value, "supported_channel", &supported_channel_,
+      value, "channel", &channel_,
       g_mappings.Get().channels);
 }
 
@@ -224,8 +235,10 @@ std::string Feature::GetErrorMessage(Feature::Availability result) {
     case NOT_PRESENT:
       return "Requires a different Feature that is not present.";
     case UNSUPPORTED_CHANNEL:
-      return base::StringPrintf("Channel %d is unsupported.",
-                                supported_channel_);
+      return base::StringPrintf(
+          "Google Chrome %s channel or newer is required, but current is %s.",
+          GetChannelName(channel_).c_str(),
+          GetChannelName(g_channel.Get().channel).c_str());
   }
 
   return "";
@@ -274,7 +287,7 @@ Feature::Availability Feature::IsAvailableToManifest(
   if (max_manifest_version_ != 0 && manifest_version > max_manifest_version_)
     return INVALID_MAX_MANIFEST_VERSION;
 
-  if (supported_channel_ < g_channel.Get().channel)
+  if (channel_ < g_channel.Get().channel)
     return UNSUPPORTED_CHANNEL;
 
   return IS_AVAILABLE;
