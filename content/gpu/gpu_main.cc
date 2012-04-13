c5d062fcc18ee4482217fe80e72e1ea87894ca9c
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 21:08:51 +0000

    Add an initial Linux GPU sandbox using the seccomp filter framework. Review URL: https://chromiumcodereview.appspot.com/10051022
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132266 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/environment.h"
 #include "base/message_loop.h"
+#include "base/rand_util.h"
 #include "base/stringprintf.h"
 #include "base/threading/platform_thread.h"
 #include "base/win/scoped_com_initializer.h"
@@ -36,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(TOOLKIT_GTK)
 #include "ui/gfx/gtk_util.h"
+#endif
+
+#if defined(OS_LINUX)
+#include "content/public/common/sandbox_init.h"
 #endif
 
 // Main function for starting the Gpu process.
@@ -101,15 +106,19 @@ int GpuMain(const content::MainFunctionParams& parameters) {
     dead_on_arrival = true;
   }
 
+  // Warm up the random subsystem, which needs to done pre-sandbox on all
+  // platforms.
+  (void) base::RandUint64();
+
+#if defined(OS_LINUX)
+  content::InitializeSandbox();
+#endif
+
   base::win::ScopedCOMInitializer com_initializer;
 
 #if defined(OS_WIN)
   // Preload this DLL because the sandbox prevents it from loading.
   LoadLibrary(L"setupapi.dll");
-
-  // Cause advapi32 to load before the sandbox is turned on.
-  unsigned int dummy_rand;
-  rand_s(&dummy_rand);
 
   sandbox::TargetServices* target_services =
       parameters.sandbox_info->target_services;
