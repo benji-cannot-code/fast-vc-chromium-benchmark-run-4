a03aad0414f02d63ff62972fac39a82b68c75c68
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 18:09:47 +0000

    In Windows8 metro mode prevent moving/sizing/maximizing of the chrome browser frame.
    BUG=118641
    Review URL: https://chromiumcodereview.appspot.com/10038040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/ash/launcher/launcher_updater.h"
 #include "chrome/browser/ui/views/ash/window_positioner.h"
 #elif defined(OS_WIN)
+#include "base/win/metro.h"
 #include "chrome/browser/aeropeek_manager.h"
 #include "chrome/browser/jumplist_win.h"
 #include "ui/views/widget/native_widget_win.h"
@@ -1523,6 +1524,17 @@ bool BrowserView::ExecuteWindowsCommand(int command_id) {
 #if defined(OS_WIN) && !defined(USE_AURA)
   if (command_id == IDC_DEBUG_FRAME_TOGGLE)
     GetWidget()->DebugToggleFrameType();
+
+  // In Windows 8 metro mode prevent sizing and moving.
+  if (base::win::GetMetroModule()) {
+    // Windows uses the 4 lower order bits of |notification_code| for type-
+    // specific information so we must exclude this when comparing.
+    static const int sc_mask = 0xFFF0;
+    if (((command_id & sc_mask) == SC_MOVE) ||
+        ((command_id & sc_mask) == SC_SIZE) ||
+        ((command_id & sc_mask) == SC_MAXIMIZE))
+      return true;
+  }
 #endif
   // Translate WM_APPCOMMAND command ids into a command id that the browser
   // knows how to handle.
