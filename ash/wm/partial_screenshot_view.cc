7ee1c41c51c6d1c66fcfdd405c4746a35a119805
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 07:38:35 +0000

    SetCapture of mouse events in partial screenshot. Mouse events are routed to the capture window.  If the user presses Ctrl-Shift-F5 when a context menu appears, all of the mouse events will go to the menu instead of partial screenshot view, which causes unexpected behaviors.
    BUG=120653
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/9969149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132155 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/partial_screenshot_view.cc
+++ ash/wm/partial_screenshot_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/partial_screenshot_event_filter.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
+#include "ui/base/events.h"
 #include "ui/base/cursor/cursor.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/rect.h"
@@ -57,6 +58,9 @@ void PartialScreenshotView::StartPartialScreenshot(
   widget->GetNativeView()->SetName("PartialScreenshotView");
   widget->StackAtTop();
   widget->Show();
+  // Captures mouse events in case that context menu already captures the
+  // events.  This will close the context menu.
+  widget->GetNativeView()->SetCapture(ui::CW_LOCK_MOUSE | ui::CW_LOCK_TOUCH);
 
   view->set_window(widget->GetNativeWindow());
   Shell::GetInstance()->partial_screenshot_filter()->Activate(view);
@@ -84,6 +88,10 @@ void PartialScreenshotView::OnPaint(gfx::Canvas* canvas) {
     screenshot_rect.Inset(1, 1, 1, 1);
     canvas->DrawRect(screenshot_rect, SK_ColorWHITE);
   }
+}
+
+void PartialScreenshotView::OnMouseCaptureLost() {
+  Cancel();
 }
 
 bool PartialScreenshotView::OnMousePressed(const views::MouseEvent& event) {
