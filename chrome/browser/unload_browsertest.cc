f0c0a9d6bad5d87612b920781751a6860e319951
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 00:42:45 +0000

    Fix flakiness in unload browser test. The problem is that the sudden termination IPC comes after loading stop, so it's a race condition whether it's handled before we close the window and wait for the prompt. Expliclity set it if it's not set by the time the tests run.
    BUG=123110
    Review URL: https://chromiumcodereview.appspot.com/10071029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/unload_browsertest.cc
+++ chrome/browser/unload_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/process_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/net/url_request_mock_util.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/test/net/url_request_mock_http_job.h"
 #include "net/url_request/url_request_test_util.h"
@@ -133,6 +135,14 @@ class UnloadTest : public InProcessBrowserTest {
                          const char* expected_title) {
     ui_test_utils::NavigateToURL(browser(),
                                  GURL("data:text/html," + html_content));
+    content::RenderProcessHost* process =
+        browser()->GetSelectedWebContents()->GetRenderProcessHost();
+    if (html_content.find("unload=") != std::string::npos &&
+        process->SuddenTerminationAllowed()) {
+      LOG(INFO) << "Explicitly unsetting sudden termination because IPC didn't "
+                   "arrive yet";
+      process->SetSuddenTerminationAllowed(false);
+    }
     CheckTitle(expected_title);
   }
 
