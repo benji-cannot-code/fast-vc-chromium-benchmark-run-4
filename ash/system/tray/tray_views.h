352545aed65ee8c7a56607e499aba41bf3d891e4
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 20:43:48 +0000

    ash: Disable some entries in the network tray-popup instead of removing them.
    BUG=122187
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10079006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132262 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_views.h
+++ ash/system/tray/tray_views.h
@@ -99,6 +99,7 @@ class HoverHighlightView : public ActionableView {
   virtual void OnMouseEntered(const views::MouseEvent& event) OVERRIDE;
   virtual void OnMouseExited(const views::MouseEvent& event) OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
+  virtual void OnEnabledChanged() OVERRIDE;
   virtual void OnPaintBackground(gfx::Canvas* canvas) OVERRIDE;
 
   ViewClickListener* listener_;
