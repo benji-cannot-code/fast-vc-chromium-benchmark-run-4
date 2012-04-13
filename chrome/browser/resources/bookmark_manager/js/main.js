230cbe942be8dbb6b721fa28965096c0b0d2259a
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 17:14:45 +0000

    fix dnd in bookmark manager
    preventDefault in handleDrop, as suggested by arv
    
    BUG=122850
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10065034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132203 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/main.js
+++ chrome/browser/resources/bookmark_manager/js/main.js
@@ -767,6 +767,8 @@ var dnd = {
       else
         chrome.experimental.bookmarkManager.drop(parentId);
 
+      e.preventDefault();
+
       // TODO(arv): Select the newly dropped items.
     }
     this.dropDestination = null;
