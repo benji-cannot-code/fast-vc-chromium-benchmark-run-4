b7c45c85d70edcd9652a1580275957581ea197bc
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 00:47:49 +0000

    Add sandbox support for associating peer processes
    TEST=HandlePolicyTest.DuplicatePeerHandle
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=131940
    Review URL: https://chromiumcodereview.appspot.com/9960045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132112 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/tests/common/controller.cc
+++ sandbox/tests/common/controller.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/process.h"
+#include "base/process_util.h"
 #include "base/sys_string_conversions.h"
 #include "base/win/windows_version.h"
 #include "sandbox/src/sandbox_factory.h"
@@ -90,12 +92,14 @@ BrokerServices* GetBroker() {
 
 TestRunner::TestRunner(JobLevel job_level, TokenLevel startup_token,
                        TokenLevel main_token)
-    : is_init_(false), is_async_(false), target_process_id_(0) {
+    : is_init_(false), is_async_(false), no_sandbox_(false),
+      target_process_id_(0) {
   Init(job_level, startup_token, main_token);
 }
 
 TestRunner::TestRunner()
-    : is_init_(false), is_async_(false), target_process_id_(0) {
+    : is_init_(false), is_async_(false), no_sandbox_(false),
+      target_process_id_(0) {
   Init(JOB_LOCKDOWN, USER_RESTRICTED_SAME_ACCESS, USER_LOCKDOWN);
 }
 
@@ -210,11 +214,21 @@ int TestRunner::InternalRunTest(const wchar_t* command) {
 
   std::wstring arguments(L"\"");
   arguments += prog_name;
-  arguments += L"\" -child ";
+  arguments += L"\" -child";
+  arguments += no_sandbox_ ? L"-no-sandbox " : L" ";
   arguments += command;
 
-  result = broker_->SpawnTarget(prog_name, arguments.c_str(), policy_,
-                                &target);
+  if (no_sandbox_) {
+    STARTUPINFO startup_info = {sizeof(STARTUPINFO)};
+    if (!::CreateProcessW(prog_name, &arguments[0], NULL, NULL, FALSE, 0,
+                          NULL, NULL, &startup_info, &target)) {
+      return SBOX_ERROR_GENERIC;
+    }
+    broker_->AddTargetPeer(target.hProcess);
+  } else {
+    result = broker_->SpawnTarget(prog_name, arguments.c_str(), policy_,
+                                  &target);
+  }
 
   if (SBOX_ALL_OK != result)
     return SBOX_TEST_FAILED_TO_RUN_TEST;
@@ -305,18 +319,20 @@ int DispatchCall(int argc, wchar_t **argv) {
     command(argc - 4, argv + 4);
 
   TargetServices* target = SandboxFactory::GetTargetServices();
-  if (!target)
+  if (target) {
+    if (SBOX_ALL_OK != target->Init())
+      return SBOX_TEST_FAILED_TO_EXECUTE_COMMAND;
+
+    if (BEFORE_REVERT == state)
+      return command(argc - 4, argv + 4);
+    else if (EVERY_STATE == state)
+      command(argc - 4, argv + 4);
+
+    target->LowerToken();
+  } else if (0 != _wcsicmp(argv[1], L"-child-no-sandbox")) {
     return SBOX_TEST_FAILED_TO_EXECUTE_COMMAND;
+  }
 
-  if (SBOX_ALL_OK != target->Init())
-    return SBOX_TEST_FAILED_TO_EXECUTE_COMMAND;
-
-  if (BEFORE_REVERT == state)
-    return command(argc - 4, argv + 4);
-  else if (EVERY_STATE == state)
-    command(argc - 4, argv + 4);
-
-  target->LowerToken();
   return command(argc - 4, argv + 4);
 }
 
