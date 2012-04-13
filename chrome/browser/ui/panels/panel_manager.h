8d07dd4b47a573977c09e026bff3c60abe3edb9d
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-13 07:35:27 +0000

    Revert 132144 - Broke ASAN
    Move full-screen detection logic from PanelManager to DisplaySettingsProvider.
    
    Also fixed a bug that pnels remain visible with full screen apps on Snow Leopard.
    The fix is to also call orderBack/orderFront when full-screen is detected. This is because the full-sreen window is still in normal level and our panel windows stay in the top of z-order when its level is changed to normal.
    
    BUG=123058
    TEST=Manual test by launching panels and make an app full-screen
    
    
    Review URL: http://codereview.chromium.org/10051020
    
    TBR=jianli@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10066041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_manager.h
+++ chrome/browser/ui/panels/panel_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/lazy_instance.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/timer.h"
 #include "chrome/browser/ui/panels/display_settings_provider.h"
 #include "chrome/browser/ui/panels/panel.h"
 #include "chrome/browser/ui/panels/panel_constants.h"
@@ -26,8 +27,7 @@ class PanelResizeController;
 class PanelMouseWatcher;
 
 // This class manages a set of panels.
-class PanelManager : public DisplaySettingsProvider::DisplayAreaObserver,
-                     public DisplaySettingsProvider::FullScreenObserver {
+class PanelManager : public DisplaySettingsProvider::DisplayAreaObserver {
  public:
   // Returns a single instance.
   static PanelManager* GetInstance();
@@ -125,6 +125,7 @@ class PanelManager : public DisplaySettingsProvider::DisplayAreaObserver,
     return docked_strip_.get();
   }
 
+  bool is_full_screen() const { return is_full_screen_; }
   OverflowPanelStrip* overflow_strip() const {
     return overflow_strip_.get();
   }
@@ -179,8 +180,8 @@ class PanelManager : public DisplaySettingsProvider::DisplayAreaObserver,
   // Overridden from DisplaySettingsProvider::DisplayAreaObserver:
   virtual void OnDisplayAreaChanged(const gfx::Rect& display_area) OVERRIDE;
 
-  // Overridden from DisplaySettingsProvider::FullScreenObserver:
-  virtual void OnFullScreenModeChanged(bool is_full_screen) OVERRIDE;
+  // Tests if the current active app is in full screen mode.
+  void CheckFullScreenMode();
 
   // Tests may want to use a mock panel mouse watcher.
   void SetMouseWatcher(PanelMouseWatcher* watcher);
@@ -206,6 +207,12 @@ class PanelManager : public DisplaySettingsProvider::DisplayAreaObserver,
   // changed. The testing code could set this flag to false so that other tests
   // will not be affected.
   bool auto_sizing_enabled_;
+
+  // Timer used to track if the current active app is in full screen mode.
+  base::RepeatingTimer<PanelManager> full_screen_mode_timer_;
+
+  // True if current active app is in full screen mode.
+  bool is_full_screen_;
 
   // True only while moving panels to overflow. Used to prevent moving panels
   // out of overflow while in the process of moving panels to overflow.
--- chrome/browser/ui/panels/panel_window_controller_cocoa.mm
+++ chrome/browser/ui/panels/panel_window_controller_cocoa.mm
@@ -1021,14 +1021,6 @@ enum {
 
 - (void)fullScreenModeChanged:(bool)isFullScreen {
   [self updateWindowLevel];
-
-  // The full-screen window is in normal level and changing the panel window to
-  // same normal level will not move it below the full-screen window. Thus we
-  // need to reorder the panel window.
-  if (isFullScreen)
-    [[self window] orderBack:nil];
-  else
-    [[self window] orderFrontRegardless];
 }
 
 - (BOOL)canBecomeKeyWindow {
@@ -1052,9 +1044,8 @@ enum {
   if (![self isWindowLoaded])
     return;
   // Make sure we don't draw on top of a window in full screen mode.
-  Panel* panel = windowShim_->panel();
-  if (panel->manager()->display_settings_provider()->is_full_screen() ||
-      !panel->always_on_top()) {
+  if (windowShim_->panel()->manager()->is_full_screen() ||
+      !windowShim_->panel()->always_on_top()) {
     [[self window] setLevel:NSNormalWindowLevel];
     return;
   }
@@ -1073,7 +1064,7 @@ enum {
   // While this is OK for expanded panels, it makes minimized panels impossible
   // to activate. As a result, we still use NSStatusWindowLevel for minimized
   // panels, since it's impossible to compose IME text in them anyway.
-  if (panel->IsMinimized()) {
+  if (windowShim_->panel()->IsMinimized()) {
     [[self window] setLevel:NSStatusWindowLevel];
     return;
   }
