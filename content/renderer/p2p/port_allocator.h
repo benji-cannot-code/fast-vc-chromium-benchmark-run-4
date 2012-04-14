617434163af41df83b5276c9ef8d87d580e618cf
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-14 01:38:16 +0000

    Libjingle roll 129:132.
    Also update P2PPortAllocator for compatibility with the latest libjingle
    changes.
    
    BUG=121613
    
    Review URL: https://chromiumcodereview.appspot.com/10069046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132307 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/port_allocator.h
+++ content/renderer/p2p/port_allocator.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -88,8 +88,6 @@ class P2PPortAllocatorSession : public cricket::BasicPortAllocatorSession,
   scoped_ptr<WebKit::WebURLLoader> relay_session_request_;
   int relay_session_attempts_;
   std::string relay_session_response_;
-  std::string relay_username_;
-  std::string relay_password_;
   talk_base::SocketAddress relay_ip_;
   int relay_udp_port_;
   int relay_tcp_port_;
