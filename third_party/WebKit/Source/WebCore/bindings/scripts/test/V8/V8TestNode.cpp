ee0075dd1d01885f60fea8253c1188cfde847779
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-14 03:06:50 +0000

    Unreviewed, rolling out r114036. http://trac.webkit.org/changeset/114036 https://bugs.webkit.org/show_bug.cgi?id=83969
    Breaks gmail.com causing it to never finish loading (Requested
    by danakj on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-04-13
    
    Source/WebCore:
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateHeader):
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateNamedConstructorCallback):
    (GenerateImplementation):
    * bindings/scripts/IDLAttributes.txt:
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore::JSTestObj::createPrototype):
    * bindings/scripts/test/JS/JSTestObj.h:
    * bindings/scripts/test/TestObj.idl:
    * bindings/scripts/test/V8/V8Float64Array.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestEventTarget.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestNode.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore):
    * bindings/v8/NPV8Object.cpp:
    (WebCore::npObjectTypeInfo):
    * bindings/v8/V8BindingPerContextData.cpp:
    (WebCore::V8BindingPerContextData::init):
    (WebCore::V8BindingPerContextData::createWrapperFromCacheSlowCase):
    (WebCore::V8BindingPerContextData::constructorForTypeSlowCase):
    * bindings/v8/V8BindingPerContextData.h:
    (V8BindingPerContextData):
    * bindings/v8/V8HiddenPropertyName.h:
    (WebCore):
    * bindings/v8/WrapperTypeInfo.h:
    (WrapperTypeInfo):
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore):
    * dom/NodeList.idl:
    
    LayoutTests:
    
    * fast/dom/HTMLSelectElement/named-options-expected.txt:
    * fast/dom/HTMLSelectElement/script-tests/named-options.js:
    * fast/dom/NodeList/node-list-array-class-expected.txt: Removed.
    * fast/dom/NodeList/node-list-array-class.html: Removed.
    * fast/dom/NodeList/nodelist-item-call-as-function-expected.txt:
    * fast/dom/everything-to-string-expected.txt:
    * fast/dom/prototype-inheritance-2-expected.txt:
    * fast/js/recursion-limit-equal-expected.txt:
    * inspector/console/command-line-api-expected.txt:
    * inspector/console/console-dir-expected.txt:
    * inspector/console/console-format-collections-expected.txt:
    * jquery/traversing-expected.txt:
    * platform/chromium/fast/dom/HTMLSelectElement/named-options-expected.txt: Removed.
    * platform/chromium/fast/dom/everything-to-string-expected.txt: Removed.
    * platform/chromium/inspector/console/command-line-api-expected.txt: Removed.
    * platform/chromium/inspector/console/console-dir-expected.txt:
    * platform/chromium/inspector/console/console-format-collections-expected.txt:
    * platform/chromium/jquery/traversing-expected.txt: Removed.
    * platform/mac/fast/dom/prototype-inheritance-2-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114200 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestNode.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestNode.cpp
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-WrapperTypeInfo V8TestNode::info = { V8TestNode::GetTemplate, V8TestNode::derefObject, 0, &V8Node::info, 0 };
+WrapperTypeInfo V8TestNode::info = { V8TestNode::GetTemplate, V8TestNode::derefObject, 0, &V8Node::info };
 
 namespace TestNodeV8Internal {
 
