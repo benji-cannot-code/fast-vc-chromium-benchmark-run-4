0dfbf1446091aa4914fc492a744280cd14401110
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-14 23:54:33 +0000

    views: Remove unused const from crypto module password dialog.
    TBR=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10092001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132349 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/crypto_module_password_dialog_view.cc
+++ chrome/browser/ui/views/crypto_module_password_dialog_view.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
 
-int kInputPasswordMinWidth = 8;
-
 namespace browser {
 
 // CryptoModulePasswordDialogView
@@ -111,9 +109,11 @@ void CryptoModulePasswordDialogView::Init(
 views::View* CryptoModulePasswordDialogView::GetInitiallyFocusedView() {
   return password_entry_;
 }
+
 ui::ModalType CryptoModulePasswordDialogView::GetModalType() const {
   return ui::MODAL_TYPE_WINDOW;
 }
+
 views::View* CryptoModulePasswordDialogView::GetContentsView() {
   return this;
 }
