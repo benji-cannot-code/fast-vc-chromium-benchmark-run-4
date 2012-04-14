9f8223e4263f5ed907841019ea9a72a12388c008
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-14 12:19:08 +0000

    FTP: make the code handling server messages in directory listings more general.
    BUG=122953
    
    Review URL: http://codereview.chromium.org/10068016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132334 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_unittest.cc
+++ net/ftp/ftp_directory_listing_parser_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -137,6 +137,7 @@ const char* kTestFiles[] = {
   "dir-listing-ls-27",  // windows-1251
 
   "dir-listing-ls-28",  // Hylafax FTP server
+  "dir-listing-ls-29",
 
   "dir-listing-netware-1",
   "dir-listing-netware-2",
