080277d5ff2b68d09c2a98b5335c99516c7a468e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-14 01:25:30 +0000

    AudioContext createChannelMerger() method should have optional argument for number of inputs. https://bugs.webkit.org/show_bug.cgi?id=83759
    Patch by Raymond Liu <raymond.liu@intel.com> on 2012-04-13
    Reviewed by Chris Rogers.
    
    Check https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html for the optional argument.
    
    Source/WebCore:
    
    Test: webaudio/audiochannelmerger-basic.html
    
    * Modules/webaudio/AudioChannelMerger.cpp:
    (WebCore):
    (WebCore::AudioChannelMerger::create):
    (WebCore::AudioChannelMerger::AudioChannelMerger):
    * Modules/webaudio/AudioChannelMerger.h:
    (AudioChannelMerger):
    * Modules/webaudio/AudioContext.cpp:
    (WebCore::AudioContext::createChannelMerger):
    (WebCore):
    * Modules/webaudio/AudioContext.h:
    (AudioContext):
    * Modules/webaudio/AudioContext.idl:
    
    LayoutTests:
    
    * webaudio/audiochannelmerger-basic-expected.txt: Added.
    * webaudio/audiochannelmerger-basic.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelMerger.h
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioChannelMerger.h
@@ -39,10 +39,7 @@ class AudioContext;
     
 class AudioChannelMerger : public AudioNode {
 public:
-    static PassRefPtr<AudioChannelMerger> create(AudioContext* context, float sampleRate)
-    {
-        return adoptRef(new AudioChannelMerger(context, sampleRate));      
-    }
+    static PassRefPtr<AudioChannelMerger> create(AudioContext*, float sampleRate, unsigned numberOfInputs);
 
     // AudioNode
     virtual void process(size_t framesToProcess);
@@ -55,7 +52,7 @@ private:
     virtual double tailTime() const OVERRIDE { return 0; }
     virtual double latencyTime() const OVERRIDE { return 0; }
 
-    AudioChannelMerger(AudioContext*, float sampleRate);
+    AudioChannelMerger(AudioContext*, float sampleRate, unsigned numberOfInputs);
 };
 
 } // namespace WebCore
