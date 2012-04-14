e626caae2ba0d6d899bb4360f4b1472393408898
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-14 03:24:53 +0000

    Close notification windows while message loop is still active Reset tooltip_controller explicitly so that timer stops at shutdown. Use tray_ reference in SystemTrayDelegate instead of going through ash instance. Make sure delegates doesn't access tray after the tray has been deleted.
    BUG=104998
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=131902
    
    Review URL: http://codereview.chromium.org/9968101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132320 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -567,6 +567,7 @@ Shell::~Shell() {
 
   // The system tray needs to be reset before all the windows are destroyed.
   tray_.reset();
+  tray_delegate_.reset();
 
   // Desroy secondary monitor's widgets before all the windows are destroyed.
   monitor_controller_.reset();
@@ -589,6 +590,7 @@ Shell::~Shell() {
   window_cycle_controller_.reset();
   event_client_.reset();
   monitor_controller_.reset();
+  tooltip_controller_.reset();
 
   // Launcher widget has a InputMethodBridge that references to
   // input_method_filter_'s input_method_. So explicitly release launcher_
--- chrome/browser/ui/browser_list_aura.cc
+++ chrome/browser/ui/browser_list_aura.cc
@@ -7,9 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/notifications/notification_ui_manager.h"
 
 // static
 void BrowserList::HandleAppExitingForPlatform() {
+  // Close All non browser windows now. Those includes notifications
+  // and windows created by Ash (launcher, background, etc).
+  g_browser_process->notification_ui_manager()->CancelAll();
+  // TODO(oshima): Close all non browser windows here while
+  // the message loop is still alive.
 #if defined(OS_CHROMEOS)
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableZeroBrowsersOpenForTests)) {
