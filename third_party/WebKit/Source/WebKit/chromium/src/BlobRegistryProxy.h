5a4db2c5c03fe70c24b04e484627a0f3a796a910
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-16 20:14:39 +0000

    [chromium] Remove BlobRegistryProxy::createResourceHandle https://bugs.webkit.org/show_bug.cgi?id=84047
    Reviewed by Nate Chapin.
    
    * src/BlobRegistryProxy.h: (BlobRegistryProxy::createResourceHandle): This function
    is no longer used.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/BlobRegistryProxy.h
+++ third_party/WebKit/Source/WebKit/chromium/src/BlobRegistryProxy.h
@@ -48,7 +48,6 @@ public:
     virtual void registerBlobURL(const KURL&, const KURL& srcURL);
     virtual void unregisterBlobURL(const KURL&);
 
-    virtual PassRefPtr<ResourceHandle> createResourceHandle(const ResourceRequest&, ResourceHandleClient*) { return 0; }
     virtual bool loadResourceSynchronously(const ResourceRequest&, ResourceError&, ResourceResponse&, Vector<char>& data) { return false; }
 
 private:
