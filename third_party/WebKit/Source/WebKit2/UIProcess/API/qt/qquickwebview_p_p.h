2a0f9f2a82f246f6e9f8c2464913fab8360bf65a
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-16 13:38:27 +0000

    [Qt] Clean up how the interaction engine is making use of ViewportAttributes https://bugs.webkit.org/show_bug.cgi?id=83895
    Reviewed by Simon Hausmann.
    
    Refactor how the interaction engine is using the ViewportAttributes
    and get rid of the Contraints subclass.
    
    Push the ViewportAttributes through to the QQuickWebViewPrivate class
    and add some default values in the case the Legacy view is used.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewLegacyPrivate::QQuickWebViewLegacyPrivate):
    (QQuickWebViewFlickablePrivate::didChangeViewportProperties):
    * UIProcess/API/qt/qquickwebview_p_p.h:
    (QQuickWebViewPrivate):
    * UIProcess/API/qt/qwebviewportinfo.cpp:
    (QWebViewportInfo::QWebViewportInfo):
    (QWebViewportInfo::~QWebViewportInfo):
    (QWebViewportInfo::currentScale):
    (QWebViewportInfo::devicePixelRatio):
    (QWebViewportInfo::initialScale):
    (QWebViewportInfo::minimumScale):
    (QWebViewportInfo::maximumScale):
    (QWebViewportInfo::isScalable):
    (QWebViewportInfo::layoutSize):
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    (WebKit::QtViewportInteractionEngine::cssScaleFromItem):
    (WebKit::QtViewportInteractionEngine::itemScaleFromCSS):
    (WebKit::QtViewportInteractionEngine::itemCoordFromCSS):
    (WebKit::QtViewportInteractionEngine::innerBoundedCSSScale):
    (WebKit::QtViewportInteractionEngine::outerBoundedCSSScale):
    (WebKit::QtViewportInteractionEngine::zoomToAreaGestureEnded):
    (WebKit::QtViewportInteractionEngine::setCSSScaleBounds):
    (WebKit::QtViewportInteractionEngine::setCSSScale):
    (WebKit::QtViewportInteractionEngine::pinchGestureStarted):
    (WebKit::QtViewportInteractionEngine::pinchGestureRequestUpdate):
    (WebKit::QtViewportInteractionEngine::pinchGestureEnded):
    * UIProcess/qt/QtViewportInteractionEngine.h:
    (QtViewportInteractionEngine):
    (WebKit::QtViewportInteractionEngine::hadUserInteraction):
    (WebKit::QtViewportInteractionEngine::setAllowsUserScaling):
    (WebKit::QtViewportInteractionEngine::setContentToDevicePixelRatio):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QtCore/QObject>
 #include <QtCore/QScopedPointer>
+#include <WebCore/ViewportArguments.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/RefPtr.h>
 
@@ -160,6 +161,8 @@ protected:
     QDeclarativeComponent* proxyAuthenticationDialog;
     QDeclarativeComponent* filePicker;
     QDeclarativeComponent* databaseQuotaDialog;
+
+    WebCore::ViewportAttributes attributes;
 
     bool m_useDefaultContentItemSize;
     bool m_navigatorQtObjectEnabled;
