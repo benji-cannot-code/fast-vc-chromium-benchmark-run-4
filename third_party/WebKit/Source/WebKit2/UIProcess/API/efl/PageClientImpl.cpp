a6186709b2eb4a778cd990b4bda708633a00e72c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-16 14:43:17 +0000

    [EFL][WK2] Fix build break in PageClientImpl.cpp.  https://bugs.webkit.org/show_bug.cgi?=id=84034
     Reviewed by Kenneth Rohde Christiansen.
    
     Fixes typo in didChangeViewportProperites definition.
    
     * UIProcess/API/efl/PageClientImpl.cpp:
     (WebKit::PageClientImpl::didChangeViewportProperties):
    
    Patch by Michał Pakuła vel Rutka <m.pakula@samsung.com> on 2012-04-16
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114253 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/PageClientImpl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/PageClientImpl.cpp
@@ -132,7 +132,7 @@ void PageClientImpl::setCursorHiddenUntilMouseMoves(bool)
     notImplemented();
 }
 
-void PageClientImpl::didChangeViewportProperties(const WebCore::ViewportAttribues&)
+void PageClientImpl::didChangeViewportProperties(const WebCore::ViewportAttributes&)
 {
     notImplemented();
 }
