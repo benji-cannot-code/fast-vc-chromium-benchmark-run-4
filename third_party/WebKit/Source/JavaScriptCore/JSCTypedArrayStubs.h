91b7f36a5a8f599b9efa90aa4a33b14ef5ffc74f
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-16 17:41:52 +0000

    Fix COMMANDLINE_TYPEDARRAYS build https://bugs.webkit.org/show_bug.cgi?id=84051
    Reviewed by Gavin Barraclough.
    
    Update for new putByIndex API and wtf changes.
    
    * JSCTypedArrayStubs.h:
    (JSC):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/JSCTypedArrayStubs.h
+++ third_party/WebKit/Source/JavaScriptCore/JSCTypedArrayStubs.h
@@ -29,10 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "JSObject.h"
 #include "ObjectPrototype.h"
+#include <wtf/Float32Array.h>
+#include <wtf/Float64Array.h>
 #include <wtf/Forward.h>
+#include <wtf/Int16Array.h>
+#include <wtf/Int32Array.h>
+#include <wtf/Int8Array.h>
+#include <wtf/Uint16Array.h>
+#include <wtf/Uint32Array.h>
+#include <wtf/Uint8Array.h>
 
 namespace JSC {
-
+    
 #define TYPED_ARRAY(name, type) \
 class JS##name##Array : public JSNonFinalObject { \
 public: \
@@ -48,7 +56,7 @@ public: \
     static bool getOwnPropertyDescriptor(JSC::JSObject*, JSC::ExecState*, const JSC::Identifier& propertyName, JSC::PropertyDescriptor&);\
     static bool getOwnPropertySlotByIndex(JSC::JSCell*, JSC::ExecState*, unsigned propertyName, JSC::PropertySlot&);\
     static void put(JSC::JSCell*, JSC::ExecState*, const JSC::Identifier& propertyName, JSC::JSValue, JSC::PutPropertySlot&);\
-    static void putByIndex(JSC::JSCell*, JSC::ExecState*, unsigned propertyName, JSC::JSValue);\
+    static void putByIndex(JSC::JSCell*, JSC::ExecState*, unsigned propertyName, JSC::JSValue, bool);\
     static const JSC::ClassInfo s_info;\
 \
     static JSC::Structure* createStructure(JSC::JSGlobalData& globalData, JSC::JSGlobalObject* globalObject, JSC::JSValue prototype)\
