6592aa6070ae4a3afbdc272c6193be04aa5c4ea9
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-16 16:50:27 +0000

    Send login notification after initializing default user avatar.
    BUG=122532
    TEST=Log in with file /root/.forget_usernames, avatar is correctly set and does not change when locking.
    
    
    Review URL: http://codereview.chromium.org/9972004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132414 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -381,8 +381,6 @@ void UserManagerImpl::UserLoggedIn(const std::string& email) {
   // This user must be in the front of the user list.
   users_.insert(users_.begin(), logged_in_user_);
 
-  NotifyOnLogin();
-
   if (is_current_user_new_) {
     SetInitialUserImage(email);
   } else {
@@ -414,6 +412,8 @@ void UserManagerImpl::UserLoggedIn(const std::string& email) {
                               histogram_index,
                               kHistogramImagesCount);
   }
+
+  NotifyOnLogin();
 }
 
 void UserManagerImpl::DemoUserLoggedIn() {
