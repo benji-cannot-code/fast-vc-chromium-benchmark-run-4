25f10668d3dddd5bbf3c3360f65bb83be84fb5fa
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-16 06:11:28 +0000

    [WebUI TaskManager] Fix JS check nits in chrome/browser/resources/task_manager.
    TBR=mazda@chromium.org, tbreisacher@chromium.org
    BUG=122009
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10095003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132374 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/task_manager/commands.js
+++ chrome/browser/resources/task_manager/commands.js
@@ -48,7 +48,6 @@ TaskManagerCommands = {
 
   /**
    * Sends command to enable or disable the given columns to update the data.
-   * @public
    */
   setUpdateColumn: function(columnId, isEnabled) {
     chrome.send('setUpdateColumn', [columnId, isEnabled]);
--- chrome/browser/resources/task_manager/measure_time.js
+++ chrome/browser/resources/task_manager/measure_time.js
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * To be included as a first script in main.html
  */
 
+/**
+ * measureTime class
+ * @constructor
+ */
 var measureTime = {
   isEnabled: localStorage.measureTimeEnabled,
 
