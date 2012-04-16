5eeab57dc4e96e990da88bd6bbf1c256aed4280e
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-16 16:02:22 +0000

    TabContents -> WebContentsImpl, part 13.
    Remove the TabContents typedef.
    
    BUG=105875
    TEST=no change
    
    
    Review URL: http://codereview.chromium.org/10082021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132410 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/ssl/ssl_policy.cc
+++ content/browser/ssl/ssl_policy.cc
@@ -112,7 +112,7 @@ void SSLPolicy::OnRequestStarted(SSLRequestInfo* info) {
 }
 
 void SSLPolicy::UpdateEntry(NavigationEntryImpl* entry,
-                            TabContents* tab_contents) {
+                            WebContentsImpl* web_contents) {
   DCHECK(entry);
 
   InitializeEntryIfNeeded(entry);
@@ -159,7 +159,7 @@ void SSLPolicy::UpdateEntry(NavigationEntryImpl* entry,
     return;
   }
 
-  if (tab_contents->DisplayedInsecureContent())
+  if (web_contents->DisplayedInsecureContent())
     entry->GetSSL().content_status |= SSLStatus::DISPLAYED_INSECURE_CONTENT;
 }
 
