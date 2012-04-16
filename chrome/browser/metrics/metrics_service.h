d516a095d4f2f06fbe484a6c3afd0f68d29e1c52
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-16 22:38:32 +0000

    Transmit a X-Chrome-UMA-Enabled bit to Google domains from clients that have UMA enabled.
    BUG=123609
    TEST=With UMA enabled (not Chromium), visit www.google.com and ensure that the request header includes X-Chrome-UMA-Enabled with value "1". Also, ensure that unit_tests GoogleUtilTests all pass.
    
    Review URL: http://codereview.chromium.org/10054029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.h
+++ chrome/browser/metrics/metrics_service.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class BookmarkModel;
 class BookmarkNode;
+class ChromeNetworkDelegate;
 class MetricsReportingScheduler;
 class PrefService;
 class Profile;
@@ -438,6 +439,7 @@ class MetricsServiceHelper {
   friend class InstantFieldTrial;
   friend bool prerender::IsOmniboxEnabled(Profile* profile);
   friend class extensions::ExtensionDownloader;
+  friend class ChromeNetworkDelegate;
 
   // Returns true if prefs::kMetricsReportingEnabled is set.
   static bool IsMetricsReportingEnabled();
