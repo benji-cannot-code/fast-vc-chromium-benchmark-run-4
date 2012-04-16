460226b6f60cf002a82864fac8d65d0a1dea02cd
serya@google.com <serya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-16 09:17:28 +0000

    Not blinking action tasks.
    BUG=chromium-os:29248
    TEST=Manual test.
    
    Review URL: https://chromiumcodereview.appspot.com/10001004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132386 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2243,11 +2243,6 @@ FileManager.prototype = {
     }
 
     this.previewSummary_.textContent = str('COMPUTING_SELECTION');
-    // Removing childrens of task buttons and preview thumbnails after simple
-    // event dispatched (see above). This can ensure a smooth disappearing
-    // animation when nothing is selected.
-    this.taskItems_.visible = false;
-    this.taskItems_.clear();
     removeChildren(this.previewThumbnails_);
 
     var fileCount = 0;
@@ -2328,7 +2323,6 @@ FileManager.prototype = {
     }
 
     if (this.dialogType_ == FileManager.DialogType.FULL_PAGE) {
-      this.taskItems_.clear();
       // Some internal tasks cannot be defined in terms of file patterns,
       // so we pass selection to check for them manually.
       if (selection.directoryCount == 0 && selection.fileCount > 0) {
@@ -2544,7 +2538,7 @@ FileManager.prototype = {
       if (defaultTask == null) defaultTask = task;
     }
 
-    this.taskItems_.visible = tasksCount > 0;
+    this.taskItems_.hidden = tasksCount == 0;
     if (tasksCount > 1) {
       // Duplicate default task in drop-down list.
       this.renderTaskItem_(defaultTask);
