62fe5090f88756f132f13d5c6935ebc7410049d0
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-16 18:45:29 +0000

    matchMedia() MediaQueryList not updating https://bugs.webkit.org/show_bug.cgi?id=75903
    Patch by Luiz Agostini <luiz.agostini@palm.com> on 2012-04-16
    Reviewed by Antti Koivisto.
    
    Source/WebCore: 
    
    Test: fast/media/media-query-list-08.html
    
    Viewport related MediaQueryList listeners were not triggered and the value
    of matches were not updated if the document's style selector were not
    affected by viewport changes.
    
    The new method evaluateMediaQueries() is now called by FrameView instead of
    styleSelectorChanged() if the style selector is not affected by viewport changes.
    
    * dom/Document.cpp:
    (WebCore::Document::evaluateMediaQueries):
    (WebCore):
    (WebCore::Document::styleSelectorChanged):
    * dom/Document.h:
    (Document):
    * page/FrameView.cpp:
    (WebCore::FrameView::layout):
    
    LayoutTests: 
    
    Testing MediaQueryLists listeners related to viewport changes.
    
    * fast/media/media-query-list-08-expected.txt: Added.
    * fast/media/media-query-list-08.html: Added.
    
    Skipping the new test in Qt due to an issue in its DumpRenderTree.
    
    * platform/qt/Skipped:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -506,6 +506,8 @@ public:
      */
     void styleSelectorChanged(StyleSelectorUpdateFlag);
 
+    void evaluateMediaQueryList();
+
     bool usesSiblingRules() const { return m_usesSiblingRules || m_usesSiblingRulesOverride; }
     void setUsesSiblingRules(bool b) { m_usesSiblingRulesOverride = b; }
     bool usesFirstLineRules() const { return m_usesFirstLineRules; }
