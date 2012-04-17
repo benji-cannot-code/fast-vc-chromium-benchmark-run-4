1857f1b1e491f70d4da0f2ed3539368a2a2af935
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 19:59:15 +0000

    Fix typo.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10105036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132624 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/ppp_entrypoints.cc
+++ ppapi/cpp/ppp_entrypoints.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,7 +24,7 @@ pp::Module* Module::Get() {
   return g_module_singleton;
 }
 
-void SetBrokerGetIntefaceFunc(PP_GetInterface_Func broker_get_interface) {
+void SetBrokerGetInterfaceFunc(PP_GetInterface_Func broker_get_interface) {
   g_broker_get_interface = broker_get_interface;
 }
 
