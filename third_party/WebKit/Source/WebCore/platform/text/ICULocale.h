4e4b981aacf9799ac3f9c56e52d8b92390873283
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 05:57:20 +0000

    Rename LocalizedNumberICU.h to ICULocale.h https://bugs.webkit.org/show_bug.cgi?id=84119
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    LocalizedNumberICU.h contains only ICULocale class. It should be
    named as ICULocale.h. We're going to move some functions in
    LocalizedDateICU.cpp and LocalizedCalendarICU.cpp to ICULocale,
    and add unit tests for them.
    
    * WebCore.gypi: Rename LocalizedNumberICU.h to ICULocale.h
    * platform/text/ICULocale.h:
    Renamed from Source/WebCore/platform/text/LocalizedNumberICU.h.
    * platform/text/LocalizedNumberICU.cpp:
    Rename LocalizedNumberICU.h to ICULocale.h
    
    Source/WebKit/chromium:
    
    * tests/LocalizedNumberICUTest.cpp: Rename LocalizedNumberICU.h to ICULocale.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/ICULocale.h
+++ third_party/WebKit/Source/WebCore/platform/text/ICULocale.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef LocalizedNumberICU_h
-#define LocalizedNumberICU_h
+#ifndef ICULocale_h
+#define ICULocale_h
 
 #include <unicode/unum.h>
 #include <wtf/Forward.h>
