3d4d1be424786cec8078ff5cc2b3568481834a6a
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 00:57:30 +0000

    GTK: Stop listening to gtk signals in the omnibox before destroying the model.
    BUG=123530
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10103012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132498 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
+++ chrome/browser/ui/gtk/omnibox/omnibox_popup_view_gtk.h
@@ -33,6 +33,10 @@ namespace gfx {
 class Image;
 }
 
+namespace ui {
+class GtkSignalRegistrar;
+}
+
 class OmniboxPopupViewGtk : public AutocompletePopupView,
                             public content::NotificationObserver {
  public:
@@ -102,6 +106,7 @@ class OmniboxPopupViewGtk : public AutocompletePopupView,
   CHROMEGTK_CALLBACK_1(OmniboxPopupViewGtk, gboolean, HandleExpose,
                        GdkEventExpose*);
 
+  scoped_ptr<ui::GtkSignalRegistrar> signal_registrar_;
   scoped_ptr<AutocompletePopupModel> model_;
   OmniboxView* omnibox_view_;
   GtkWidget* location_bar_;
