912a002aea7c71fdecc67df0c70635b52a0af03b
tdresser@chromium.org <tdresser@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 19:25:25 +0000

    On touch capture, cancel non-captured touches.
    When a touch capture occurs, any touches locked to other windows are cancelled.
    
    These touches are then locked to a GestureConsumer which ignores all
    further events, until the touch is released.
    
    BUG=123211
    TEST=WindowTest.TouchCaptureCancelsOtherTouches, TouchCaptureDoesntCancelCapturedTouches
    
    
    Review URL: http://codereview.chromium.org/10038030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132619 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/gestures/gesture_sequence.cc
+++ ui/base/gestures/gesture_sequence.cc
@@ -315,6 +315,7 @@ void GestureSequence::Reset() {
   set_state(GS_NO_GESTURE);
   for (int i = 0; i < kMaxGesturePoints; ++i)
     points_[i].Reset();
+  point_count_ = 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
