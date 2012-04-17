06aa4d637ed561ed928eebdf6ca9078db97b50a8
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 19:00:15 +0000

    gesture recognizer: Send correct events to do gesture-flings.
    - When a scroll-end gesture has non-zero velocity, generate a fling-start event.
    - For each tap-down gesture, include a fling-cancel in case a fling is in progress.
    
    BUG=120154
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9956147
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132610 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_recognizer_unittest.cc
+++ ui/aura/gestures/gesture_recognizer_unittest.cc
@@ -38,6 +38,7 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
         pinch_update_(false),
         pinch_end_(false),
         long_press_(false),
+        fling_(false),
         three_finger_swipe_(false),
         scroll_x_(0),
         scroll_y_(0),
@@ -58,6 +59,7 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
     pinch_update_ = false;
     pinch_end_ = false;
     long_press_ = false;
+    fling_ = false;
     three_finger_swipe_ = false;
 
     scroll_begin_position_.SetPoint(0, 0);
@@ -78,6 +80,7 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
   bool pinch_update() const { return pinch_update_; }
   bool pinch_end() const { return pinch_end_; }
   bool long_press() const { return long_press_; }
+  bool fling() const { return fling_; }
   bool three_finger_swipe() const { return three_finger_swipe_; }
 
   const gfx::Point scroll_begin_position() const {
@@ -128,6 +131,11 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
         long_press_ = true;
         touch_id_ = gesture->delta_x();
         break;
+      case ui::ET_SCROLL_FLING_START:
+        EXPECT_TRUE(scroll_end_);
+        EXPECT_TRUE(velocity_x_ != 0 || velocity_y_ != 0);
+        fling_ = true;
+        break;
       case ui::ET_GESTURE_THREE_FINGER_SWIPE:
         three_finger_swipe_ = true;
         velocity_x_ = gesture->delta_x();
@@ -150,6 +158,7 @@ class GestureEventConsumeDelegate : public TestWindowDelegate {
   bool pinch_update_;
   bool pinch_end_;
   bool long_press_;
+  bool fling_;
   bool three_finger_swipe_;
 
   gfx::Point scroll_begin_position_;
