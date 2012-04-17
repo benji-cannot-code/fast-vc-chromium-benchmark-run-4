5ff37b6898f3d2d618377ab56de48cf07216077c
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 18:35:48 +0000

    Exiting full screen video brings the wrong Safari window to the foreground https://bugs.webkit.org/show_bug.cgi?id=83936
    Reviewed by Adele Peterson.
    
    Tell the original webView's window to makeKeyAndOrderFront once the exit
    animation completes.
    
    Source/WebKit/mac:
    
    * WebView/WebFullScreenController.mm:
    (-[WebFullScreenController finishedExitFullScreenAnimation:]):
    
    Source/WebKit2:
    
    * UIProcess/mac/WKFullScreenWindowController.mm:
    (-[WKFullScreenWindowController finishedExitFullScreenAnimation:]):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebFullScreenController.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebFullScreenController.mm
@@ -378,7 +378,9 @@ static IntRect screenRectOfContents(Element* element)
     
     [_backgroundWindow.get() orderOut:self];
     [_backgroundWindow.get() setFrame:NSZeroRect display:YES];
-    
+
+    [[_webView window] makeKeyAndOrderFront:self];
+
     NSEnableScreenUpdates();
 }
 
