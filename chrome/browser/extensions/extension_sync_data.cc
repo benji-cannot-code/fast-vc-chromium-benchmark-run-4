cf569f81d82342e6c70d3a1422ff95489afb1082
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 05:13:45 +0000

    Use the non-localized "canonical name" of an extension to write to its sync data, rather than the localized name.
    BUG=123214
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10086019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_sync_data.cc
+++ chrome/browser/extensions/extension_sync_data.cc
@@ -44,7 +44,7 @@ ExtensionSyncData::ExtensionSyncData(const Extension& extension,
       incognito_enabled_(incognito_enabled),
       version_(*extension.version()),
       update_url_(extension.update_url()),
-      name_(extension.name()) {
+      name_(extension.non_localized_name()) {
 }
 
 ExtensionSyncData::~ExtensionSyncData() {}
