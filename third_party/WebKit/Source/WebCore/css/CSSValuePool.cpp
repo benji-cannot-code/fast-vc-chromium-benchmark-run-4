b3e5cd3611ebc81f985a3edf5f951536f6d1415c
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 00:28:11 +0000

    Remove contextStyleSheet argument from CSSValuePool::createFontFaceValue(). <http://webkit.org/b/83988>
    Reviewed by Antti Koivisto.
    
    Remove the 'context style sheet' argument to <font face> value parsing.
    It was only ever used to grab at the CSSValuePool back when they were per-Document.
    
    * css/CSSParser.h:
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseFontFaceValue):
    * css/CSSValuePool.h:
    * css/CSSValuePool.cpp:
    (WebCore::CSSValuePool::createFontFaceValue):
    * html/HTMLFontElement.cpp:
    (WebCore::HTMLFontElement::collectStyleForAttribute):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSValuePool.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSValuePool.cpp
@@ -135,7 +135,7 @@ PassRefPtr<CSSPrimitiveValue> CSSValuePool::createFontFamilyValue(const String& 
     return value;
 }
 
-PassRefPtr<CSSValueList> CSSValuePool::createFontFaceValue(const AtomicString& string, StyleSheetInternal* contextStyleSheet)
+PassRefPtr<CSSValueList> CSSValuePool::createFontFaceValue(const AtomicString& string)
 {
     // Just wipe out the cache and start rebuilding if it gets too big.
     const int maximumFontFaceCacheSize = 128;
@@ -144,7 +144,7 @@ PassRefPtr<CSSValueList> CSSValuePool::createFontFaceValue(const AtomicString& s
 
     RefPtr<CSSValueList>& value = m_fontFaceValueCache.add(string, 0).iterator->second;
     if (!value)
-        value = CSSParser::parseFontFaceValue(string, contextStyleSheet);
+        value = CSSParser::parseFontFaceValue(string);
     return value;
 }
 
