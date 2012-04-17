21790a3aadcb68baf061dc89abea0e91ab475697
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 13:45:54 +0000

    Rewrite isolate_smoke_test.py to increase the coverage.
    Also fix discrepancy in the way timestamp was calculated. It was causing issues
    on OSX and Windows.
    
    R=nsylvain@chromium.org
    BUG=98834
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10068032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132569 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/data/isolate/fail.py
+++ tools/isolate/data/isolate/fail.py
@@ -1,15 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-{
-  'variables': {
-    'command': [
-      'python',
-      'fail.py',
-    ],
-    'isolate_dependency_tracked': [
-      'fail.py',
-    ],
-  },
-}
+
+import sys
+
+
+def main():
+  print 'Failing'
+  return 1
+
+
+if __name__ == '__main__':
+  sys.exit(main())
