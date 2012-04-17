49e8df28fcddddf2fea0ec1c6778346b2d537470
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 11:04:12 +0000

    Made File Manager respect the user-selected launch type (tab/pinned tab/window/fullscreen)"
    BUG=chromium-os:27884
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10094012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132564 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_handler_util.h
+++ chrome/browser/chromeos/extensions/file_handler_util.h
@@ -83,6 +83,8 @@ class FileTaskExecutor : public base::RefCountedThreadSafe<FileTaskExecutor> {
    virtual Browser* browser() = 0;
    virtual void Done(bool success) = 0;
 
+   Profile* profile() { return profile_; }
+
  private:
   struct FileDefinition {
     FileDefinition();
