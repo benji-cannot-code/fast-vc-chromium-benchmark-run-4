5beb3a046eee765f2426e43ecfc13c7a78d07078
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 04:36:00 +0000

    Move PepperNetworkManager to client/plugin
    Review URL: http://codereview.chromium.org/10103016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132533 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_network_manager.cc
+++ remoting/client/plugin/pepper_network_manager.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "remoting/protocol/pepper_network_manager.h"
+#include "remoting/client/plugin/pepper_network_manager.h"
 
 #include "base/bind.h"
 #include "base/message_loop.h"
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/private/net_address_private.h"
 
 namespace remoting {
-namespace protocol {
 
 PepperNetworkManager::PepperNetworkManager(const pp::InstanceHandle& instance)
     : monitor_(instance, &PepperNetworkManager::OnNetworkListCallbackHandler,
@@ -111,5 +110,4 @@ void PepperNetworkManager::SendNetworksChangedSignal() {
   SignalNetworksChanged();
 }
 
-}  // namespace protocol
 }  // namespace remoting
--- remoting/client/plugin/pepper_network_manager.h
+++ remoting/client/plugin/pepper_network_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_PROTOCOL_PEPPER_NETWORK_MANAGER_H_
-#define REMOTING_PROTOCOL_PEPPER_NETWORK_MANAGER_H_
+#ifndef REMOTING_CLIENT_PLUGIN_PEPPER_NETWORK_MANAGER_H_
+#define REMOTING_CLIENT_PLUGIN_PEPPER_NETWORK_MANAGER_H_
 
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
@@ -17,7 +17,6 @@ class NetworkListPrivate;
 }  // namespace pp
 
 namespace remoting {
-namespace protocol {
 
 // PepperNetworkManager uses the PPB_NetworkMonitor_Private API to
 // implement the NetworkManager interface that libjingle uses to
@@ -46,7 +45,6 @@ class PepperNetworkManager : public talk_base::NetworkManagerBase {
   base::WeakPtrFactory<PepperNetworkManager> weak_factory_;
 };
 
-}  // namespace protocol
 }  // namespace remoting
 
-#endif  // REMOTING_PROTOCOL_PEPPER_NETWORK_MANAGER_H_
+#endif  // REMOTING_CLIENT_PLUGIN_PEPPER_NETWORK_MANAGER_H_
