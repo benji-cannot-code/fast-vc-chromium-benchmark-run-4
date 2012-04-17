28ece2ab903dce7763fbcb4193b6376de3ae8789
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 03:42:46 +0000

    [Chromium] Move popup location detection code from WebViewImpl to WebPagePopupImpl https://bugs.webkit.org/show_bug.cgi?id=84116
    Reviewed by Kentaro Hara.
    
    This makes no behavior change. Just move some code.
    To fix Bug 84007, WebPagePopupImpl needs to know if the popup is
    above the target element or below the target element.
    
    * src/WebPagePopupImpl.cpp:
    (WebKit::WebPagePopupImpl::init): Move some code from WebViewImpl::openPagePopup().
    * src/WebPagePopupImpl.h:
    (WebPagePopupImpl): Rename an argument name.
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::openPagePopup): Move some code to WebPagePopupImpl::init().
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114336 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPagePopupImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPagePopupImpl.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebPagePopupImpl.h"
 
+#include "Chrome.h"
 #include "EmptyClients.h"
 #include "FileChooser.h"
 #include "FocusController.h"
@@ -45,9 +46,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebInputEvent.h"
 #include "WebInputEventConversion.h"
 #include "WebPagePopup.h"
+#include "WebViewImpl.h"
 #include "WebWidgetClient.h"
 
 using namespace WebCore;
+using namespace std;
 
 namespace WebKit {
 
@@ -134,12 +137,21 @@ WebPagePopupImpl::~WebPagePopupImpl()
     ASSERT(!m_page);
 }
 
-bool WebPagePopupImpl::init(WebViewImpl* webView, PagePopupClient* popupClient, const IntRect& boundsInScreen)
+bool WebPagePopupImpl::init(WebViewImpl* webView, PagePopupClient* popupClient, const IntRect& originBoundsInRootView)
 {
     ASSERT(webView);
     ASSERT(popupClient);
     m_webView = webView;
     m_popupClient = popupClient;
+
+    WebSize rootViewSize = webView->size();
+    IntSize popupSize = popupClient->contentSize();
+    IntRect popupBoundsInRootView(IntPoint(max(0, originBoundsInRootView.x()), max(0, originBoundsInRootView.maxY())), popupSize);
+    if (popupBoundsInRootView.maxY() > rootViewSize.height)
+        popupBoundsInRootView.setY(max(0, originBoundsInRootView.y() - popupSize.height()));
+    if (popupBoundsInRootView.maxX() > rootViewSize.width)
+        popupBoundsInRootView.setX(max(0, rootViewSize.width - popupSize.width()));
+    IntRect boundsInScreen = webView->page()->chrome()->rootViewToScreen(popupBoundsInRootView);
 
     m_widgetClient->setWindowRect(boundsInScreen);
     m_windowRectInScreen = boundsInScreen;
