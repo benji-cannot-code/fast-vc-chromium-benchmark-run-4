76c4fc8131b7813b240195072d6116117e333b59
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 02:11:09 +0000

    Make json_schema_compiler remove 'nocompile' nodes from JSON at the JSON level rather than at the model level. This gives us automatic nocompile of all properties; previously not all were supported.
    TEST=tools/json_schema_compiler/json_schema_test.py, unit_tests --gtest_filter=JsonSchemaCompiler*
    
    Review URL: http://codereview.chromium.org/10108005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132514 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/json_schema_compiler/json_schema_test.py
@@ -0,0 +1,79 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from json_schema import CachedLoad
+import json_schema_test
+import unittest
+
+class JsonSchemaUnittest(unittest.TestCase):
+  def testNocompile(self):
+    compiled = [
+      {
+        "namespace": "compile",
+        "functions": [],
+        "types":     {}
+      },
+
+      {
+        "namespace": "functions",
+        "functions": [
+          {
+            "id": "two"
+          },
+          {
+            "id": "four"
+          }
+        ],
+
+        "types": {
+          "one": { "key": "value" }
+        }
+      },
+
+      {
+        "namespace": "types",
+        "functions": [
+          { "id": "one" }
+        ],
+        "types": {
+          "two": {
+            "key": "value"
+          },
+          "four": {
+            "key": "value"
+          }
+        }
+      },
+
+      {
+        "namespace": "nested",
+        "properties": {
+          "sync": {
+            "functions": [
+              {
+                "id": "two"
+              },
+              {
+                "id": "four"
+              }
+            ],
+            "types": {
+              "two": {
+                "key": "value"
+              },
+              "four": {
+                "key": "value"
+              }
+            }
+          }
+        }
+      }
+    ]
+
+    self.assertEquals(compiled, CachedLoad('test/json_schema_test.json'))
+
+if __name__ == '__main__':
+  unittest.main()
