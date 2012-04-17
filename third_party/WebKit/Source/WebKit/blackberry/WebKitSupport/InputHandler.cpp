7be37d85ece94710b34c1ac39afae8dc18012348
mifenton@rim.com <mifenton@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 15:20:29 +0000

    [BlackBerry] Pattern matching should be applied to numbers https://bugs.webkit.org/show_bug.cgi?id=84152
    Reviewed by Antonio Gomes.
    
    PR 148906.
    
    Add pattern matching for number based on [0-9] to trigger
    input help matching number fields.
    
    * WebKitSupport/DOMSupport.cpp:
    (BlackBerry::WebKit::DOMSupport::elementPatternIndicatesNumber):
    (DOMSupport):
    (BlackBerry::WebKit::DOMSupport::elementPatternIndicatesHexadecimal):
    (BlackBerry::WebKit::DOMSupport::elementPatternMatches):
    * WebKitSupport/DOMSupport.h:
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::convertInputType):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114380 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -159,6 +159,8 @@ static BlackBerryInputType convertInputType(const HTMLInputElement* inputElement
         return InputTypeEmail;
     if (DOMSupport::elementIdOrNameIndicatesUrl(inputElement))
         return InputTypeURL;
+    if (DOMSupport::elementPatternIndicatesNumber(inputElement))
+        return InputTypeNumber;
     if (DOMSupport::elementPatternIndicatesHexadecimal(inputElement))
         return InputTypeHexadecimal;
 
