937107f763d941a325f432dcf50e5d3e300367f6
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 02:46:51 +0000

    Implement STYLE_LOWERCASE style for Aura NativeTextfield.
    BUG=109308
    TEST=views_unittest --gtest_filter="NativeTextfieldViews*"
    
    
    Review URL: http://codereview.chromium.org/9358049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132525 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/textfield_views_model.h
+++ ui/views/controls/textfield/textfield_views_model.h
@@ -186,7 +186,7 @@ class VIEWS_EXPORT TextfieldViewsModel {
   bool Copy();
 
   // Pastes text from the clipboard at current cursor position. Returns true
-  // if text has changed after pasting.
+  // if any text is pasted.
   bool Paste();
 
   // Tells if any text is selected, even if the selection is in composition
@@ -209,7 +209,7 @@ class VIEWS_EXPORT TextfieldViewsModel {
   void GetTextRange(ui::Range* range) const;
 
   // Sets composition text and attributes. If there is composition text already,
-  // it’ll be replaced by the new one. Otherwise, current selection will be
+  // it'll be replaced by the new one. Otherwise, current selection will be
   // replaced. If there is no selection, the composition text will be inserted
   // at the insertion point.
   // Any changes to the model except text insertion will confirm the current
