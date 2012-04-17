ffc9dac4f8769195a2d231fd497ed8a6a6d127d1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-17 13:14:50 +0000

    [Qt] Zoom out on second double-tap. https://bugs.webkit.org/show_bug.cgi?id=84145
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-04-17
    Reviewed by Simon Hausmann.
    
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    (WebKit::QtViewportInteractionEngine::QtViewportInteractionEngine):
    (WebKit::QtViewportInteractionEngine::zoomToAreaGestureEnded):
    (WebKit::QtViewportInteractionEngine::pinchGestureStarted):
    * UIProcess/qt/QtViewportInteractionEngine.h:
    (QtViewportInteractionEngine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
@@ -146,6 +146,7 @@ private:
     OwnPtr<ViewportUpdateDeferrer> m_touchUpdateDeferrer;
 
     bool m_hadUserInteraction;
+    bool m_zoomedToArea;
 
     class ScaleAnimation : public QVariantAnimation {
     public:
