f5136a83735d523316368ae6997f2ff37eaff130
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 22:35:31 +0000

    Sandbox: Add support for interceptions on Windows 8.
    BUG=123068
    TEST=sbox_unittests, sbox_integration_tests
    Review URL: https://chromiumcodereview.appspot.com/10021033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132680 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/src/service_resolver.h
+++ sandbox/src/service_resolver.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -97,6 +97,21 @@ class Wow64ResolverThunk : public ServiceResolverThunk {
 };
 
 // This is the concrete resolver used to perform service-call type functions
+// inside ntdll.dll on WOW64 for Windows 8.
+class Wow64W8ResolverThunk : public ServiceResolverThunk {
+ public:
+  // The service resolver needs a child process to write to.
+  Wow64W8ResolverThunk(HANDLE process, bool relaxed)
+      : ServiceResolverThunk(process, relaxed) {}
+  virtual ~Wow64W8ResolverThunk() {}
+
+ private:
+  virtual bool IsFunctionAService(void* local_thunk) const;
+
+  DISALLOW_COPY_AND_ASSIGN(Wow64W8ResolverThunk);
+};
+
+// This is the concrete resolver used to perform service-call type functions
 // inside ntdll.dll on Windows 2000 and XP pre SP2.
 class Win2kResolverThunk : public ServiceResolverThunk {
  public:
@@ -111,6 +126,21 @@ class Win2kResolverThunk : public ServiceResolverThunk {
   virtual bool IsFunctionAService(void* local_thunk) const;
 
   DISALLOW_COPY_AND_ASSIGN(Win2kResolverThunk);
+};
+
+// This is the concrete resolver used to perform service-call type functions
+// inside ntdll.dll on Windows 8.
+class Win8ResolverThunk : public ServiceResolverThunk {
+ public:
+  // The service resolver needs a child process to write to.
+  Win8ResolverThunk(HANDLE process, bool relaxed)
+      : ServiceResolverThunk(process, relaxed) {}
+  virtual ~Win8ResolverThunk() {}
+
+ private:
+  virtual bool IsFunctionAService(void* local_thunk) const;
+
+  DISALLOW_COPY_AND_ASSIGN(Win8ResolverThunk);
 };
 
 }  // namespace sandbox
