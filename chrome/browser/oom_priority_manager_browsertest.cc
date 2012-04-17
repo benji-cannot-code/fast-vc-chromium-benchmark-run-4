3f7a8cdb18d0cce95673d7c820443606286e7497
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 02:15:20 +0000

    Fixes FindBar crash. The crash appears to be because if you select a discarded tab we don't tell the FindBarController of the new TabContentsWrapper and the FindBar is left referencing the wrong and possibly deleted TabContentsWrapper.
    BUG=123671
    TEST=none
    R=jamescook@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10106011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132518 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/oom_priority_manager_browsertest.cc
+++ chrome/browser/oom_priority_manager_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/find_bar/find_bar_controller.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -99,12 +100,18 @@ IN_PROC_BROWSER_TEST_F(OomPriorityManagerTest, OomPriorityManagerBasics) {
   // Running when all tabs are discarded should do nothing.
   EXPECT_FALSE(g_browser_process->oom_priority_manager()->DiscardTab());
 
+  // Force creation of the FindBarController.
+  browser()->GetFindBarController();
+
   // Select the first tab.  It should reload.
   WindowedNotificationObserver reload1(
       content::NOTIFICATION_NAV_ENTRY_COMMITTED,
       content::NotificationService::AllSources());
   browser()->SelectNumberedTab(0);
   reload1.Wait();
+  // Make sure the FindBarController gets the right TabContentsWrapper.
+  EXPECT_EQ(browser()->GetFindBarController()->tab_contents(),
+            browser()->GetSelectedTabContentsWrapper());
   EXPECT_EQ(0, browser()->active_index());
   EXPECT_FALSE(browser()->IsTabDiscarded(0));
   EXPECT_TRUE(browser()->IsTabDiscarded(1));
