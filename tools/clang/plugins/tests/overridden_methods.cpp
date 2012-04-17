849f203c08769ab88834b513f7821262a94eb4dc
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-17 01:31:24 +0000

    Check implementation files for virtual and OVERRIDE.
    Currently behind a flag "-Xclang -plugin-arg-find-bad-constructs -Xclang skip-virtuals-in-implementations"
    
    BUG=115047
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10076002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132507 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/plugins/tests/overridden_methods.cpp
+++ tools/clang/plugins/tests/overridden_methods.cpp
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,30 @@ void DerivedClass::SomeMethod() {}
 void DerivedClass::SomeOtherMethod() {}
 void DerivedClass::WebKitModifiedSomething() {}
 
+class ImplementationInterimClass : public BaseClass {
+ public:
+  // Should not warn about pure virtual methods.
+  virtual void SomeMethod() = 0;
+};
+
+class ImplementationDerivedClass : public ImplementationInterimClass,
+                                   public webkit_glue::WebKitObserverImpl {
+ public:
+  // Should not warn about destructors.
+  virtual ~ImplementationDerivedClass() {}
+  // Should warn.
+  virtual void SomeMethod();
+  // Should not warn if marked as override.
+  virtual void SomeOtherMethod() override;
+  // Should not warn for inline implementations in implementation files.
+  virtual void SomeInlineMethod() {}
+  // Should not warn if overriding a method whose origin is WebKit.
+  virtual void WebKitModifiedSomething();
+  // Should warn if overridden method isn't pure.
+  virtual void SomeNonPureBaseMethod() {}
+};
+
 int main() {
   DerivedClass something;
+  ImplementationDerivedClass something_else;
 }
--- tools/clang/plugins/tests/virtual_methods.cpp
+++ tools/clang/plugins/tests/virtual_methods.cpp
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,13 +17,13 @@ class VirtualMethodsInImplementation {
 // Stubs to fill in the abstract method
 class ConcreteVirtualMethodsInHeaders : public VirtualMethodsInHeaders {
  public:
-  virtual void MethodIsAbstract() {}
+  virtual void MethodIsAbstract() override {}
 };
 
 class ConcreteVirtualMethodsInImplementation
     : public VirtualMethodsInImplementation {
  public:
-  virtual void MethodIsAbstract() {}
+  virtual void MethodIsAbstract() override {}
 };
 
 // Fill in the implementations
