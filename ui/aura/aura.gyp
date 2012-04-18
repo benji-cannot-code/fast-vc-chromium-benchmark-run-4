bb1c473df8cf65265d5158cbd716978e73cc779b
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 20:32:14 +0000

    Aura/ash split: Remove hacks and get chrome linking without ash.
    For the first time now, you can do:
    
        > build/gyp_chromium -Duse_aura=1 -Duse_ash=0
    
    And get a running chrome. It has lots of issues, especially related to
    window placement, tab handling, etc, but it pops up and renders web content.
    
    Also consolidates most of the desktop behavior into its own class.
    
    Also makes views_examples_exe work again. Several of the hacks are no longer
    needed after Ben's refactoring to support WebView.
    
    BUG=116458,119759
    TEST=none
    
    Review URL: http://codereview.chromium.org/10081022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132856 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/aura.gyp
+++ ui/aura/aura.gyp
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'desktop/desktop_dispatcher_client.h',
         'desktop/desktop_root_window_event_filter.cc',
         'desktop/desktop_root_window_event_filter.h',
+        'desktop/desktop_stacking_client.cc',
+        'desktop/desktop_stacking_client.h',
         'dispatcher_linux.cc',
         'dispatcher_linux.h',
         'dispatcher_win.cc',
--- ui/views/test/test_views_delegate.cc
+++ ui/views/test/test_views_delegate.cc
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "ui/base/clipboard/clipboard.h"
 
+#if defined(USE_AURA)
+#include "ui/views/widget/desktop_native_widget_helper_aura.h"
+#endif
+
 namespace views {
 
 TestViewsDelegate::TestViewsDelegate()
@@ -57,5 +61,12 @@ bool TestViewsDelegate::UseTransparentWindows() const {
 int TestViewsDelegate::GetDispositionForEvent(int event_flags) {
   return 0;
 }
+
+#if defined(USE_AURA)
+views::NativeWidgetHelperAura* TestViewsDelegate::CreateNativeWidgetHelper(
+    views::NativeWidgetAura* native_widget) {
+  return new views::DesktopNativeWidgetHelperAura(native_widget);
+}
+#endif
 
 }  // namespace views
