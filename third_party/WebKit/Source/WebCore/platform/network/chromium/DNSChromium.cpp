d315df144cbdeb0fcd3afabf912887f85f6df8fd
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 23:10:18 +0000

    [Chromium] Call prefetchDNS directly https://bugs.webkit.org/show_bug.cgi?id=84262
    Reviewed by Kentaro Hara.
    
    Part of a series, see tracking bug 82948.
    
    Source/WebCore:
    
    * platform/chromium/PlatformSupport.h:
    (PlatformSupport):
    * platform/network/chromium/DNSChromium.cpp:
    (WebCore::prefetchDNS):
    
    Source/WebKit/chromium:
    
    * src/PlatformSupport.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/DNSChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/DNSChromium.cpp
@@ -27,13 +27,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DNS.h"
 
-#include "PlatformSupport.h"
+#include <public/Platform.h>
 
 namespace WebCore {
 
 void prefetchDNS(const String& hostname)
 {
-    PlatformSupport::prefetchDNS(hostname);
+    WebKit::Platform::current()->prefetchHostName(hostname);
 }
 
 } // namespace WebCore
