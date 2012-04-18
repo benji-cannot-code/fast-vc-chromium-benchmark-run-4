7c2c4086505cf75c02d26d599e4ce630f096993e
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 20:02:30 +0000

    Do call updateControls()
    BUG=119594
    TEST=Tested manually
    TBR=jhawkins@chromium.org
    
    Review URL: http://codereview.chromium.org/10115021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132848 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/chromeos/internet_detail.js
+++ chrome/browser/resources/options2/chromeos/internet_detail.js
@@ -32,7 +32,7 @@ cr.define('options.internet', function() {
    * @param {Event} e The update event.
    */
   function handlePrefUpdate(e) {
-    DetailsInternetPage.prototype.updateControls;
+    DetailsInternetPage.prototype.updateControls();
   }
 
   /////////////////////////////////////////////////////////////////////////////
