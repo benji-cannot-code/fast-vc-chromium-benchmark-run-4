ec6541f15dc0075fce562d5bc7e9457288c9bca7
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 07:30:33 +0000

    Wrap Libcros's Monitor/Disconnect functions with NetworkWatcher
    This change is a preparation for switching from Libcros to the new implementation.
    It also isolates GValue related stuff into cros_network_functions.cc.
    
    BUG=chromium-os:16557
    TEST=unit_tests --gtest_filter="CrosNetworkFunctionsLibcrosTest.*"
    
    
    Review URL: http://codereview.chromium.org/10086001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/sms_observer.h
+++ chrome/browser/chromeos/sms_observer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,9 +18,6 @@ class Profile;
 
 namespace chromeos {
 
-struct SMS;
-class SMSHandler;
-
 // Performs monitoring of incoming SMS and shows system notifications.
 class SmsObserver : public NetworkLibrary::NetworkManagerObserver {
  public:
@@ -28,7 +25,7 @@ class SmsObserver : public NetworkLibrary::NetworkManagerObserver {
   virtual ~SmsObserver();
 
  private:
-  typedef std::map<std::string, SMSHandler*> ObserversMap;
+  typedef std::map<std::string, CrosNetworkWatcher*> ObserversMap;
 
   // NetworkLibrary:NetworkManagerObserver implementation:
   virtual void OnNetworkManagerChanged(NetworkLibrary* obj) OVERRIDE;
