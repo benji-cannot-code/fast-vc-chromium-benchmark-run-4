ef92b381795da252bc055ab83f54ca8a56ff8322
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 17:54:49 +0000

    Add #ifdefs to separate NaCl and non-NaCl PPAPI Proxy code. This CL moves non-NaCl interface proxies like Flash, and also temporarily moves unported interfaces such as Graphics3D. BUG=116317 TEST=builds without PPAPI link errors Review URL: https://chromiumcodereview.appspot.com/10116003
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132817 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/ppb_instance_shared.cc
+++ ppapi/shared_impl/ppb_instance_shared.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -59,6 +59,7 @@ int32_t PPB_Instance_Shared::ValidateRequestInputEvents(
   return PP_OK;
 }
 
+#if !defined(OS_NACL)
 bool PPB_Instance_Shared::ValidateSetCursorParams(PP_MouseCursor_Type type,
                                                   PP_Resource image,
                                                   const PP_Point* hot_spot) {
@@ -98,5 +99,6 @@ bool PPB_Instance_Shared::ValidateSetCursorParams(PP_MouseCursor_Type type,
     return false;
   return true;
 }
+#endif  // !defined(OS_NACL)
 
 }  // namespace ppapi
