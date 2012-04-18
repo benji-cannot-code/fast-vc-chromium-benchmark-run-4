41cc136647b30f2a508cd13015e37e04a3165885
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 18:01:32 +0000

    TabContents -> WebContentsImpl, part 23.
    TabContents -> WebContents in chrome/, part 6.
    
    BUG=105875
    TEST=no change
    
    Review URL: https://chromiumcodereview.appspot.com/10116002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132819 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/prefs/prefs_tab_helper.h
+++ chrome/browser/ui/prefs/prefs_tab_helper.h
@@ -40,7 +40,7 @@ class PrefsTabHelper : public content::NotificationObserver {
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  // Update the TabContents's RendererPreferences.
+  // Update the WebContents's RendererPreferences.
   void UpdateRendererPreferences();
 
   Profile* GetProfile();
