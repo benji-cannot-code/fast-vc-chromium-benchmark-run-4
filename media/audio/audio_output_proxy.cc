ea5067157fac17a0962a526ac033151a34afaea1
enal@chromium.org <enal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 23:14:13 +0000

    Software audio mixer. Code goes through old or new paths depending on the AudioParameters or command-line flag. Added unit tests.
    Review URL: http://codereview.chromium.org/9691001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132891 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_proxy.cc
+++ media/audio/audio_output_proxy.cc
@@ -15,21 +15,19 @@ namespace media {
 AudioOutputProxy::AudioOutputProxy(AudioOutputDispatcher* dispatcher)
     : dispatcher_(dispatcher),
       state_(kCreated),
-      physical_stream_(NULL),
       volume_(1.0) {
 }
 
 AudioOutputProxy::~AudioOutputProxy() {
   DCHECK(CalledOnValidThread());
   DCHECK(state_ == kCreated || state_ == kClosed);
-  DCHECK(!physical_stream_);
 }
 
 bool AudioOutputProxy::Open() {
   DCHECK(CalledOnValidThread());
   DCHECK_EQ(state_, kCreated);
 
-  if (!dispatcher_->StreamOpened()) {
+  if (!dispatcher_->OpenStream()) {
     state_ = kError;
     return false;
   }
@@ -40,18 +38,13 @@ bool AudioOutputProxy::Open() {
 
 void AudioOutputProxy::Start(AudioSourceCallback* callback) {
   DCHECK(CalledOnValidThread());
-  DCHECK(physical_stream_ == NULL);
   DCHECK_EQ(state_, kOpened);
 
-  physical_stream_= dispatcher_->StreamStarted();
-  if (!physical_stream_) {
+  if (!dispatcher_->StartStream(callback, this)) {
     state_ = kError;
     callback->OnError(this, 0);
     return;
   }
-
-  physical_stream_->SetVolume(volume_);
-  physical_stream_->Start(callback);
   state_ = kPlaying;
 }
 
@@ -60,19 +53,14 @@ void AudioOutputProxy::Stop() {
   if (state_ != kPlaying)
     return;
 
-  DCHECK(physical_stream_);
-  physical_stream_->Stop();
-  dispatcher_->StreamStopped(physical_stream_);
-  physical_stream_ = NULL;
+  dispatcher_->StopStream(this);
   state_ = kOpened;
 }
 
 void AudioOutputProxy::SetVolume(double volume) {
   DCHECK(CalledOnValidThread());
   volume_ = volume;
-  if (physical_stream_) {
-    physical_stream_->SetVolume(volume);
-  }
+  dispatcher_->StreamVolumeSet(this, volume);
 }
 
 void AudioOutputProxy::GetVolume(double* volume) {
@@ -83,10 +71,9 @@ void AudioOutputProxy::GetVolume(double* volume) {
 void AudioOutputProxy::Close() {
   DCHECK(CalledOnValidThread());
   DCHECK(state_ == kCreated || state_ == kError || state_ == kOpened);
-  DCHECK(!physical_stream_);
 
   if (state_ != kCreated)
-    dispatcher_->StreamClosed();
+    dispatcher_->CloseStream(this);
 
   state_ = kClosed;
 
--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -28,6 +28,8 @@ MEDIA_EXPORT extern const char kUsePulseAudio[];
 
 MEDIA_EXPORT extern const char kVideoThreads[];
 
+MEDIA_EXPORT extern const char kEnableAudioMixer[];
+
 }  // namespace switches
 
 #endif  // MEDIA_BASE_MEDIA_SWITCHES_H_
