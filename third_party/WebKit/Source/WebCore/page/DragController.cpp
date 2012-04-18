2ecc4d4dd8edb63585fcbcbcf4b5e27820d6cb3f
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 20:23:10 +0000

    [WK2] Sandbox violations prevent attaching files to gmail messages         https://bugs.webkit.org/show_bug.cgi?id=84263         <rdar://problem/11248260>
            Reviewed by Oliver Hunt.
    
            * page/DragActions.h: Removed the newly added upload action. We cannot know if or when an
            upload is going to happen - a drop even listener can always get a reference to a File object,
            and upload it with XMLHttpRequest.
    
            * page/DragController.cpp: (WebCore::DragController::concludeEditDrag): Removed a separate
            willPerformDragDestinationAction call for upload action - ther is nothing we'd want to only
            do when dropping on a file upload button.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114551 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DragController.cpp
+++ third_party/WebKit/Source/WebCore/page/DragController.cpp
@@ -470,7 +470,6 @@ bool DragController::concludeEditDrag(DragData* dragData)
             return false;
 
         fileInput->receiveDroppedFiles(filenames);
-        m_client->willPerformDragDestinationAction(DragDestinationActionUpload, dragData);
         return true;
     }
 
