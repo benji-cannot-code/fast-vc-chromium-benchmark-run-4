dd17c3953e480c1e29228119563bb8dc6521d0c7
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 02:44:48 +0000

    Change extension permissions upgrade text to "Review permissions..."
    Undo 132662: "Hack: add an ellipsis to Re-enable button for extension permissions increase." which was for M19.
    
    BUG=122389
    TEST=Install an extension which is disabled on update for permissions increase. Look at the button text.
    
    
    Review URL: http://codereview.chromium.org/10116008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132733 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_disabled_ui.cc
+++ chrome/browser/extensions/extension_disabled_ui.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
-#include "base/i18n/rtl.h"
 #include "base/lazy_instance.h"
 #include "base/message_loop.h"
 #include "base/memory/ref_counted.h"
@@ -232,13 +231,8 @@ string16 ExtensionDisabledGlobalError::GetBubbleViewMessage() {
 }
 
 string16 ExtensionDisabledGlobalError::GetBubbleViewAcceptButtonLabel() {
-  if (!base::i18n::IsRTL()) {
-    return l10n_util::GetStringUTF16(
-        IDS_EXTENSION_DISABLED_ERROR_ENABLE_BUTTON) + UTF8ToUTF16("...");
-  } else {
-    return UTF8ToUTF16("...") + l10n_util::GetStringUTF16(
-        IDS_EXTENSION_DISABLED_ERROR_ENABLE_BUTTON);
-  }
+  return l10n_util::GetStringUTF16(
+      IDS_EXTENSION_DISABLED_ERROR_ENABLE_BUTTON);
 }
 
 string16 ExtensionDisabledGlobalError::GetBubbleViewCancelButtonLabel() {
