e24b79a9c9817f56d573f471e3f45135fa6a31f9
jschuh@chromium.org <jschuh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 22:29:02 +0000

    Win8 builds usually fail due to cygwin rebasing https://bugs.webkit.org/show_bug.cgi?id=84274
    Reviewed by Dirk Pranke.
    
    Cygwin can fail to spawn children if the DLL is rebased. This happens
    frequently enough on Windows 8 that we should retry on failure.
    
    No new tests. No behavior changed.
    
    * bindings/scripts/preprocessor.pm:
    (applyPreprocessor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/preprocessor.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/preprocessor.pm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 use strict;
 use warnings;
 
+use Config;
 use IPC::Open2;
 
 BEGIN {
@@ -61,7 +62,20 @@ sub applyPreprocessor
     my @macros = grep { $_ } split(/\s+/, $defines); # grep skips empty macros.
     @macros = map { "-D$_" } @macros;
 
-    my $pid = open2(\*PP_OUT, \*PP_IN, split(' ', $preprocessor), @args, @macros, $fileName);
+    my $pid = 0;
+    if ($Config{osname} eq "cygwin") {
+        # This call can fail if Windows rebases cygwin, so retry a few times until it succeeds.
+        for (my $tries = 0; !$pid && ($tries < 20); $tries++) {
+            eval {
+                $pid = open2(\*PP_OUT, \*PP_IN, split(' ', $preprocessor), @args, @macros, $fileName);
+                1;
+            } or do {
+                sleep 1;
+            }
+        };
+    } else {
+        $pid = open2(\*PP_OUT, \*PP_IN, split(' ', $preprocessor), @args, @macros, $fileName);
+    }
     close PP_IN;
     my @documentContent = <PP_OUT>;
     close PP_OUT;
