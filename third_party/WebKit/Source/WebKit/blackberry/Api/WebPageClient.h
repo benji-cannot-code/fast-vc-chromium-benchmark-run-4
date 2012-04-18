2155deb3786a93c2ac5361f9923284f5afc41bb5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 08:40:40 +0000

    Source/WebCore: [BlackBerry] Tab awareness for HTML5 concurrent audio https://bugs.webkit.org/show_bug.cgi?id=82930 Support for concurrent HTML5 audio improvements being made in the platform library, which need to be aware of tabs and tab visibility. PR96004.
    Patch by Max Feil <mfeil@rim.com> on 2012-04-18
    Reviewed by George Staikos.
    
    Test: media/audio-concurrent-supported.html
    
    * platform/blackberry/PageClientBlackBerry.h:
    * platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp:
    (WebCore::MediaPlayerPrivate::MediaPlayerPrivate):
    (WebCore::MediaPlayerPrivate::showErrorDialog):
    (WebCore::MediaPlayerPrivate::isTabVisible):
    (WebCore):
    * platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.h:
    (MediaPlayerPrivate):
    
    Source/WebKit/blackberry: [BlackBerry] Tab awareness for HTML5 concurrent audio
    https://bugs.webkit.org/show_bug.cgi?id=82930
    Support for concurrent HTML5 audio improvements being made in
    the platform library, which need to be aware of tabs and tab
    visibility. PR96004.
    
    Patch by Max Feil <mfeil@rim.com> on 2012-04-18
    Reviewed by George Staikos.
    
    * Api/WebPageClient.h:
    * Api/WebPage_p.h:
    (BlackBerry::WebKit::WebPagePrivate::isVisible):
    
    LayoutTests: [BlackBerry] Tab awareness for HTML5 concurrent audio
    https://bugs.webkit.org/show_bug.cgi?id=82930
    Layout test for concurrent HTML5 audio. This is a simple test
    to confirm that multiple audio elements can play at the same time.
    
    Patch by Max Feil <mfeil@rim.com> on 2012-04-18
    Reviewed by George Staikos.
    
    * media/audio-concurrent-supported-expected.txt: Added.
    * media/audio-concurrent-supported.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114493 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPageClient.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPageClient.h
@@ -223,6 +223,7 @@ public:
     virtual bool lockOrientation(bool landscape) = 0;
     virtual void unlockOrientation() = 0;
     virtual bool isActive() const = 0;
+    virtual bool isVisible() const = 0;
     virtual void requestWebGLPermission(const WebString&) = 0;
 
     virtual void setToolTip(WebString) = 0;
