a06be9a941c6b81eccab65382d27979c9b847c76
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 16:25:40 +0000

    [Chromium] REGRESSION: Popup shrinks because of autocomplete https://bugs.webkit.org/show_bug.cgi?id=84139 http://code.google.com/p/chromium/issues/detail?id=123432
    Source/WebCore:
    
    Do not update the window rect on the wrong client but instead return the popup widget's
    new screen coordinates from the refresh() method.
    
    Reviewed by Kent Tamura.
    
    * platform/chromium/PopupContainer.cpp:
    (WebCore::PopupContainer::refresh):
    * platform/chromium/PopupContainer.h:
    (PopupContainer):
    
    Source/WebKit/chromium:
    
    Do not use the PopupContainer's frameRect() to update the popup client's window rect,
    since PopupContainer is always positioned at (0, 0) inside the client's window rect,
    and the latter one is positioned relative to the screen origin.
    
    Reviewed by Kent Tamura.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::refreshAutofillPopup):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/PopupContainer.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/PopupContainer.cpp
@@ -408,7 +408,7 @@ void PopupContainer::showInRect(const IntRect& r, FrameView* v, int index)
     showPopup(v);
 }
 
-void PopupContainer::refresh(const IntRect& targetControlRect)
+IntRect PopupContainer::refresh(const IntRect& targetControlRect)
 {
     listBox()->setBaseWidth(max(m_originalFrameRect.width() - kBorderSize * 2, 0));
     listBox()->updateFromElement();
@@ -424,14 +424,9 @@ void PopupContainer::refresh(const IntRect& targetControlRect)
     if (size() != widgetRectInScreen.size())
         resize(widgetRectInScreen.size());
 
-    ChromeClientChromium* chromeClient = chromeClientChromium();
-    if (chromeClient) {
-        // Update the WebWidget location (which is relative to the screen origin).
-        if (widgetRectInScreen != chromeClient->windowRect())
-            chromeClient->setWindowRect(widgetRectInScreen);
-    }
-
     invalidate();
+
+    return widgetRectInScreen;
 }
 
 inline bool PopupContainer::isRTL() const
