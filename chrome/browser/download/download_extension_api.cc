af28b92166d8bcb1aacbcf3e2cbe5996a87d231f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 15:43:20 +0000

    Remove download_state_info.h from chrome/browser/DEPS now that it's not used anymore.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/10041016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132794 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extension_api.cc
+++ chrome/browser/download/download_extension_api.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/renderer_host/chrome_render_message_filter.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/webui/web_ui_util.h"
-#include "content/browser/download/download_state_info.h"
 #include "content/public/browser/download_interrupt_reasons.h"
 #include "content/public/browser/download_item.h"
 #include "content/public/browser/download_save_info.h"
