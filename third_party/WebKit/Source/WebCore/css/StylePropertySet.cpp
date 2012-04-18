fedefc94ef14dc8b4c3dde1ad54b0972ac0ba0fa
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 21:21:35 +0000

    Prevent switch case fallthrough in StylePropertySet::getPropertyValue(). https://bugs.webkit.org/show_bug.cgi?id=84266
    Reviewed by Kentaro Hara.
    
    No new tests / code cleanup found by static analysis.
    
    * css/StylePropertySet.cpp:
    (WebCore::StylePropertySet::getPropertyValue):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114560 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
@@ -170,6 +170,7 @@ String StylePropertySet::getPropertyValue(CSSPropertyID propertyID) const
         RefPtr<CSSValue> value = getPropertyCSSValue(CSSPropertyMarkerStart);
         if (value)
             return value->cssText();
+        return String();
     }
 #endif
     case CSSPropertyBorderRadius:
