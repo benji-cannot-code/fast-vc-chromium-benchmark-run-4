1c9a110fc4b377fffd234339342c8667eef7b83f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 00:29:24 +0000

    build-webkit: make ninja work with chromium linux https://bugs.webkit.org/show_bug.cgi?id=84213
    Reviewed by Eric Seidel.
    
    Fix typos so that build-webkit will use ninja to
    build chromium if a build.ninja file exists and is up-to-date.
    
    * Scripts/webkitdirs.pm:
    (determineIsChromiumNinja):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1157,7 +1157,7 @@ sub determineIsChromiumNinja()
 
     my $hasUpToDateNinjabuild = 0;
     if (-e "out/$config/build.ninja") {
-        my $statNinja = stat("out/$config/build.ninja");
+        my $statNinja = stat("out/$config/build.ninja")->mtime;
 
         my $statXcode = 0;
         if (-e 'Source/WebKit/chromium/WebKit.xcodeproj') {
@@ -1166,7 +1166,7 @@ sub determineIsChromiumNinja()
 
         my $statMake = 0;
         if (-e 'Makefile.chromium') {
-          $statXcode = stat('Makefile.chromium')->mtime;
+          $statMake = stat('Makefile.chromium')->mtime;
         }
 
         $hasUpToDateNinjabuild = $statNinja > $statXcode && $statNinja > $statMake;
