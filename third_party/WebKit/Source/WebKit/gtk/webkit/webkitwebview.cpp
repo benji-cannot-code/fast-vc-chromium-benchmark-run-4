a0a2481e52cb2cfcc0ee48340e4462b4e6c63675
tomz@codeaurora.org <tomz@codeaurora.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-18 18:01:17 +0000

    Unreviewed, rolling out r114506. http://trac.webkit.org/changeset/114506 https://bugs.webkit.org/show_bug.cgi?id=84254
    Seems to be making gtk's DumpRenderTree crash occasionally
    (Requested by tomz on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-04-18
    
    Source/WebKit/gtk:
    
    * webkit/webkitwebsettings.cpp:
    (webkit_web_settings_class_init):
    (webkit_web_settings_set_property):
    (webkit_web_settings_get_property):
    * webkit/webkitwebsettingsprivate.h:
    * webkit/webkitwebview.cpp:
    (webkit_web_view_update_settings):
    (webkit_web_view_settings_notify):
    
    Tools:
    
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (resetDefaultsToConsistentValues):
    
    LayoutTests:
    
    * platform/gtk/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebview.cpp
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebview.cpp
@@ -3385,7 +3385,6 @@ static void webkit_web_view_update_settings(WebKitWebView* webView)
     coreSettings->setJavaEnabled(settingsPrivate->enableJavaApplet);
     coreSettings->setHyperlinkAuditingEnabled(settingsPrivate->enableHyperlinkAuditing);
     coreSettings->setDNSPrefetchingEnabled(settingsPrivate->enableDNSPrefetching);
-    coreSettings->setMediaPlaybackRequiresUserGesture(settingsPrivate->mediaPlaybackRequiresUserGesture);
 
 #if ENABLE(SQL_DATABASE)
     AbstractDatabase::setIsAvailable(settingsPrivate->enableHTML5Database);
@@ -3524,8 +3523,6 @@ static void webkit_web_view_settings_notify(WebKitWebSettings* webSettings, GPar
         settings->setJavaEnabled(g_value_get_boolean(&value));
     else if (name == g_intern_string("enable-hyperlink-auditing"))
         settings->setHyperlinkAuditingEnabled(g_value_get_boolean(&value));
-    else if (name == g_intern_string("media-playback-requires-user-gesture"))
-        settings->setMediaPlaybackRequiresUserGesture(g_value_get_boolean(&value));
 
 #if ENABLE(SPELLCHECK)
     else if (name == g_intern_string("spell-checking-languages")) {
