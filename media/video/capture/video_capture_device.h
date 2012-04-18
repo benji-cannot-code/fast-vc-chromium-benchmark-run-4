42c79ba7d8d91cf8c29e55239767dc397a1417b4
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 17:29:19 +0000

    add more color formats NV21 and YV12 in video capture (mainly from Android). Review URL: https://chromiumcodereview.appspot.com/10075001
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132808 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture_device.h
+++ media/video/capture/video_capture_device.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -43,6 +43,8 @@ class MEDIA_EXPORT VideoCaptureDevice {
     kRGB24,
     kARGB,
     kMJPEG,  // Currently only supported on Windows.
+    kNV21,
+    kYV12,
   };
 
   // Describes the format a camera capture video in.
