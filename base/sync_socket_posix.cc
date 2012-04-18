65a64f4d6d4cca12247f9ab3dbd43566a8aa9e2d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 23:03:46 +0000

    Revert 132842 - If we are using blocking write, when the renderer stop getting the data without notifying the browser, it will hang the browser. This happens with some plugins which use the sync sockets provided by the Pepper. This patch change CancellableSyncSocket to be non-blocking on sending, so that we don't need to worry the whole browser hangs by one plugin application.
    Also, we remove the lock in audio_sync_reader.cc since it is not really needed if we don't set the socket_ to NULL when calling Close(). By doing this we allow the user to close the socket while another thread is writing to the socket.
    
    
    BUG=121152
    TEST=ipc_tests
    
    Review URL: http://codereview.chromium.org/10000004
    
    TBR=xians@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10083064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132888 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sync_socket_posix.cc
+++ base/sync_socket_posix.cc
@@ -7,11 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <limits.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 
 #if defined(OS_SOLARIS)
 #include <sys/filio.h>
@@ -97,8 +96,7 @@ size_t SyncSocket::Send(const void* buffer, size_t length) {
   DCHECK_LE(length, kMaxMessageLength);
   const char* charbuffer = static_cast<const char*>(buffer);
   int len = file_util::WriteFileDescriptor(handle_, charbuffer, length);
-
-  return (len == -1) ? 0 : static_cast<size_t>(len);
+  return static_cast<size_t>(len);
 }
 
 size_t SyncSocket::Receive(void* buffer, size_t length) {
@@ -125,25 +123,6 @@ CancelableSyncSocket::CancelableSyncSocket(Handle handle)
 
 bool CancelableSyncSocket::Shutdown() {
   return HANDLE_EINTR(shutdown(handle(), SHUT_RDWR)) >= 0;
-}
-
-size_t CancelableSyncSocket::Send(const void* buffer, size_t length) {
-  long flags = 0;
-  flags = fcntl(handle_, F_GETFL, NULL);
-  if (flags != -1 && (flags & O_NONBLOCK) == 0) {
-    // Set the socket to non-blocking mode for sending if its original mode
-    // is blocking.
-    fcntl(handle_, F_SETFL, flags | O_NONBLOCK);
-  }
-
-  size_t len = SyncSocket::Send(buffer, length);
-
-  if (flags != -1 && (flags & O_NONBLOCK) == 0) {
-    // Restore the original flags.
-    fcntl(handle_, F_SETFL, flags);
-  }
-
-  return len;
 }
 
 // static
