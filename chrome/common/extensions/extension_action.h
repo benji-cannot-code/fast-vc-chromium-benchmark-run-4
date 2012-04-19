44e39439f18d7093dbfb056639c0eabf8c66e6c4
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 17:57:47 +0000

    extensions: Do not pass SkColor by const-reference as it's defined as POD(integer).
    This is a follow up to r128782.
    
    R=aa@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10117021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_action.h
+++ chrome/common/extensions/extension_action.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -108,7 +108,7 @@ class ExtensionAction {
   }
 
   // Set this action's badge text color on a specific tab.
-  void SetBadgeTextColor(int tab_id, const SkColor& text_color) {
+  void SetBadgeTextColor(int tab_id, SkColor text_color) {
     SetValue(&badge_text_color_, tab_id, text_color);
   }
   // Get the text color for a tab, or the default color if no text color
@@ -118,7 +118,7 @@ class ExtensionAction {
   }
 
   // Set this action's badge background color on a specific tab.
-  void SetBadgeBackgroundColor(int tab_id, const SkColor& color) {
+  void SetBadgeBackgroundColor(int tab_id, SkColor color) {
     SetValue(&badge_background_color_, tab_id, color);
   }
   // Get the badge background color for a tab, or the default if no color
