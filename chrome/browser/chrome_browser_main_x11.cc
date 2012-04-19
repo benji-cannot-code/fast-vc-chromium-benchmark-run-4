266a12c5bfc00b22ea08ba0663c04ad52677132d
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 02:36:06 +0000

    Don't cache the IO thread in the MetricsService
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10117034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_x11.cc
+++ chrome/browser/chrome_browser_main_x11.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/debug/debugger.h"
+#include "base/message_loop.h"
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/ui/browser_list.h"
