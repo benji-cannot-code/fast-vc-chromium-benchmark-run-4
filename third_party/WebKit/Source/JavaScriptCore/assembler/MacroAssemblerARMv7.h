21052947314732eb478f1137dbe55b031994b42c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-19 18:44:04 +0000

    It should be possible to load from Float64 arrays on ARMv7 without crashing https://bugs.webkit.org/show_bug.cgi?id=84361
    Reviewed by Oliver Hunt.
    
    * assembler/MacroAssemblerARMv7.h:
    (JSC::MacroAssemblerARMv7::loadDouble):
    (JSC::MacroAssemblerARMv7::storeDouble):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114660 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARMv7.h
@@ -766,9 +766,10 @@ public:
 
     void loadDouble(BaseIndex address, FPRegisterID dest)
     {
-        UNUSED_PARAM(address);
-        UNUSED_PARAM(dest);
-        unreachableForPlatform();
+        move(address.index, addressTempRegister);
+        lshift32(TrustedImm32(address.scale), addressTempRegister);
+        add32(address.base, addressTempRegister);
+        loadDouble(Address(addressTempRegister, address.offset), dest);
     }
     
     void loadFloat(BaseIndex address, FPRegisterID dest)
@@ -814,7 +815,7 @@ public:
     void storeDouble(FPRegisterID src, BaseIndex address)
     {
         move(address.index, addressTempRegister);
-        mul32(TrustedImm32(1 << address.scale), addressTempRegister, addressTempRegister);
+        lshift32(TrustedImm32(address.scale), addressTempRegister);
         add32(address.base, addressTempRegister);
         storeDouble(src, Address(addressTempRegister, address.offset));
     }
