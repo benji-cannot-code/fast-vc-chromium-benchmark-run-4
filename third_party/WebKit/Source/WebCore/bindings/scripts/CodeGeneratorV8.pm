90d59c9a78334c0b80ba7f047cbbb1bd3dd5dd79
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-19 17:00:21 +0000

    Unreviewed, rolling out r114421. http://trac.webkit.org/changeset/114421 https://bugs.webkit.org/show_bug.cgi?id=84103
    Chromium crash
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateNormalAttrGetter):
    (GenerateFunctionCallString):
    (NativeToJSValue):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore::TestEventConstructorV8Internal::attr1AttrGetter):
    (WebCore::TestEventConstructorV8Internal::attr2AttrGetter):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore::TestInterfaceV8Internal::supplementalStr1AttrGetter):
    (WebCore::TestInterfaceV8Internal::supplementalStr2AttrGetter):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::readOnlyStringAttrAttrGetter):
    (WebCore::TestObjV8Internal::stringAttrAttrGetter):
    (WebCore::TestObjV8Internal::reflectedStringAttrAttrGetter):
    (WebCore::TestObjV8Internal::reflectedURLAttrAttrGetter):
    (WebCore::TestObjV8Internal::reflectedCustomURLAttrAttrGetter):
    (WebCore::TestObjV8Internal::stringAttrWithGetterExceptionAttrGetter):
    (WebCore::TestObjV8Internal::stringAttrWithSetterExceptionAttrGetter):
    (WebCore::TestObjV8Internal::hashAttrGetter):
    (WebCore::TestObjV8Internal::conditionalMethod1Callback):
    * bindings/v8/V8Binding.cpp:
    (WebCore::getElementStringAttr):
    * bindings/v8/V8Binding.h:
    (WebCore::v8ExternalString):
    (WebCore::v8String):
    (WebCore::v8StringOrNull):
    (WebCore::v8StringOrUndefined):
    (WebCore::v8StringOrFalse):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1010,7 +1010,7 @@ END
     return value;
 END
         } else {
-            push(@implContentDecls, "    " . ReturnNativeToJSValue($attribute->signature, $result, "info.GetIsolate()").";\n");
+            push(@implContentDecls, "    " . ReturnNativeToJSValue($attribute->signature, $result).";\n");
         }
     }
 
@@ -3341,7 +3341,7 @@ sub GenerateFunctionCallString()
     }
 
     $return .= ".release()" if ($returnIsRef);
-    $result .= $indent . ReturnNativeToJSValue($function->signature, $return, "args.GetIsolate()") . ";\n";
+    $result .= $indent . ReturnNativeToJSValue($function->signature, $return) . ";\n";
 
     return $result;
 }
@@ -3758,7 +3758,6 @@ sub NativeToJSValue
 {
     my $signature = shift;
     my $value = shift;
-    my $getIsolate = shift;
     my $type = GetTypeFromSignature($signature);
 
     return "v8Boolean($value)" if $type eq "boolean";
@@ -3789,15 +3788,14 @@ sub NativeToJSValue
 
     if ($codeGenerator->IsStringType($type)) {
         my $conv = $signature->extendedAttributes->{"TreatReturnedNullStringAs"};
-        my $getIsolateArgument = $getIsolate ? ", $getIsolate" : "";
         if (defined $conv) {
-            return "v8StringOrNull($value$getIsolateArgument)" if $conv eq "Null";
-            return "v8StringOrUndefined($value$getIsolateArgument)" if $conv eq "Undefined";
-            return "v8StringOrFalse($value$getIsolateArgument)" if $conv eq "False";
+            return "v8StringOrNull($value)" if $conv eq "Null";
+            return "v8StringOrUndefined($value)" if $conv eq "Undefined";
+            return "v8StringOrFalse($value)" if $conv eq "False";
 
             die "Unknown value for TreatReturnedNullStringAs extended attribute";
         }
-        return "v8String($value$getIsolateArgument)";
+        return "v8String($value)";
     }
 
     my $arrayType = $codeGenerator->GetArrayType($type);
