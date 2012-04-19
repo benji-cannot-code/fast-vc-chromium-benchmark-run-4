78ff45c2843232c26fa48ff0bdd6b8e4c4811187
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-19 16:59:34 +0000

    Unreviewed, rolling out r114626. http://trac.webkit.org/changeset/114626 https://bugs.webkit.org/show_bug.cgi?id=84349
    caused new assertions in debug builds (Requested by smfr_ on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-04-19
    
    Source/WebCore:
    
    * inspector/InspectorResourceAgent.cpp:
    (WebCore::InspectorResourceAgent::didReceiveData):
    * inspector/NetworkResourcesData.cpp:
    (WebCore::NetworkResourcesData::responseReceived):
    * inspector/NetworkResourcesData.h:
    (ResourceData):
    (NetworkResourcesData):
    * inspector/front-end/RequestView.js:
    (WebInspector.RequestView.hasTextContent):
    
    LayoutTests:
    
    * http/tests/inspector/network/network-image-404-expected.txt: Removed.
    * http/tests/inspector/network/network-image-404.html: Removed.
    * http/tests/inspector/network/resources/404.php: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/RequestView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RequestView.js
@@ -59,7 +59,7 @@ WebInspector.RequestView.hasTextContent = function(request)
 {
     if (request.type.isTextType())
         return true; 
-    if (request.type === WebInspector.resourceTypes.Other || request.hasErrorStatusCode())
+    if (request.type === WebInspector.resourceTypes.Other)
         return request.content && !request.contentEncoded;
     return false;
 }
