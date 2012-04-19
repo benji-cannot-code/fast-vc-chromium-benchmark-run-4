736ae5ade036ce6283fac12eaca219a7acbe7217
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-19 15:37:16 +0000

    [EFL] dispatchDidCancelClientRedirect needs implementation https://bugs.webkit.org/show_bug.cgi?id=83669
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-04-19
    Reviewed by Antonio Gomes.
    
    Source/WebKit/efl:
    
    Emit a new "redirect,cancelled" signal on the ewk_frame when a
    redirect is cancelled. This is needed by EFL's DumpRenderTree to dump
    frame load callbacks information.
    
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    (WebCore::FrameLoaderClientEfl::dispatchDidCancelClientRedirect):
    * ewk/ewk_frame.cpp:
    (ewk_frame_redirect_cancelled):
    * ewk/ewk_frame.h:
    * ewk/ewk_private.h:
    
    Tools:
    
    Catch the new ewk_frame "redirect,cancelled" signal in EFL's DRT and
    and print out information that is expected by several test cases if
    LayoutTestController's dumpFrameLoadCallback() returns true.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::createView):
    (DumpRenderTreeChrome::onFrameCreated):
    (DumpRenderTreeChrome::onFrameRedirectCancelled):
    * DumpRenderTree/efl/DumpRenderTreeChrome.h:
    (DumpRenderTreeChrome):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.h
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.h
@@ -93,6 +93,8 @@ private:
 
     static void onFrameLoadFinished(void*, Evas_Object*, void*);
 
+    static void onFrameRedirectCancelled(void*, Evas_Object*, void*);
+
     static void onFrameLoadError(void*, Evas_Object*, void*);
     static void onDidDetectXSS(void*, Evas_Object*, void*);
 };
