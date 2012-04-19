014768e49b4b5cfd624515be072f4b76a4ea8c2a
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 04:02:39 +0000

    Run pending-version-change-stuck-works-with-terminate.html as browser test.
    BUG=123418
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10116014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132940 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -40,6 +40,7 @@ static const char* kBasicTests[] = {
 
 static const char* kComplexTests[] = {
   "prefetch-bugfix-108071.html",
+  "pending-version-change-stuck-works-with-terminate.html",
   NULL
 };
 
