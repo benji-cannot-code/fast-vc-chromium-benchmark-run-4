23f5c1235c6c3f15b54dd0cf3664021ed2cd3bf5
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 00:29:33 +0000

    Fix a bug in EXT_texture_filter_anisotropic related queries.
    At the moment these queries always return 0.  It turns out that in command buffer we fail to specify the returned data size, so it's by default to size 0, which in turn ends up returns value 0.
    
    BUG=124108
    TEST=ext-texture-filter-anisotropic.html
    R=apatrick
    Review URL: https://chromiumcodereview.appspot.com/10083061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132904 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/common/gles2_cmd_utils.cc
+++ gpu/command_buffer/common/gles2_cmd_utils.cc
@@ -208,6 +208,8 @@ int GLES2Util::GLGetNumValuesReturned(int id) const {
     //    GL_CHROMIUM_framebuffer_multisample
     case GL_MAX_SAMPLES_EXT:
       return 1;
+    case GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT:
+      return 1;
 
     // -- glGetBufferParameteriv
     case GL_BUFFER_SIZE:
@@ -290,6 +292,8 @@ int GLES2Util::GLGetNumValuesReturned(int id) const {
     case GL_TEXTURE_WRAP_S:
       return 1;
     case GL_TEXTURE_WRAP_T:
+      return 1;
+    case GL_TEXTURE_MAX_ANISOTROPY_EXT:
       return 1;
 
     // -- glGetVertexAttribfv, glGetVertexAttribiv
