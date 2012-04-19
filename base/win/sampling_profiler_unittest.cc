301c70e655360706a42d33e36cf5be87254045f3
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 19:55:37 +0000

    Disabling the SamplingProfilerTest.Sample test on Windows 8 as the ZwSetIntervalProfile API fails with a privilege not held by the client error.
    BUG=124077
    R=cpu
    Review URL: https://chromiumcodereview.appspot.com/10021069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133044 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/sampling_profiler_unittest.cc
+++ base/win/sampling_profiler_unittest.cc
@@ -1,13 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/win/sampling_profiler.h"
-
+#include "base/logging.h"
 #include "base/test/test_timeouts.h"
+#include "base/win/sampling_profiler.h"
 #include "base/win/pe_image.h"
 #include "base/win/scoped_handle.h"
+#include "base/win/windows_version.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 // The address of our image base.
@@ -60,6 +61,10 @@ TEST_F(SamplingProfilerTest, Initialize) {
 }
 
 TEST_F(SamplingProfilerTest, Sample) {
+  if (base::win::GetVersion() == base::win::VERSION_WIN8) {
+    LOG(INFO) << "Not running test on Windows 8";
+    return;
+  }
   SamplingProfiler profiler;
 
   // Initialize with a huge bucket size, aiming for a single bucket.
