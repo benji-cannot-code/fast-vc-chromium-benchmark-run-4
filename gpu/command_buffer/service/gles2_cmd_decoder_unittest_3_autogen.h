efd119108edff2adad42a03f7a66bf70d154511c
twiz@chromium.org <twiz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 03:20:27 +0000

    Introduction of CHROMIUM_copy_texture extension that respects pixel-store semantics, and allows copying of BGRA textures.
    OpenGL ES does not natively allow for copying textures with a BGRA format. The EXT_texture_format_BGRA8888 extension does not specify support for glCopyTexImage calls on these textures.
    
    This extension provides a routine to perform texture copies to/from BGRA-backed textures that also respects the following CHROMIUM pixel storage modifiers:
    UNPACK_FLIP_Y_CHROMIUM
    UNPACK_PREMULTIPLY_ALPHA_CHROMIUM
    
    This extension will be useful for the following purposes:
    - Copying accelerated Canvas2D contents to WebGL textures without a software readback. (And potentially the same for video-webgl texture copies.)
    - Copying Canvas2D contents to the compositor backing store.
    
    BUG=101051
    TEST=none
    
    Review URL: http://codereview.chromium.org/9968113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132934 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_3_autogen.h
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_3_autogen.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(gman): GetTranslatedShaderSourceANGLE
 // TODO(gman): PostSubBufferCHROMIUM
 // TODO(gman): TexImageIOSurface2DCHROMIUM
+// TODO(gman): CopyTextureCHROMIUM
 // TODO(gman): DrawArraysInstancedANGLE
 // TODO(gman): DrawElementsInstancedANGLE
 // TODO(gman): VertexAttribDivisorANGLE
