fff9ec37fc3acbb4b105f5cbb0106e519c7cb588
mitchellwrosen@chromium.org <mitchellwrosen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 04:00:42 +0000

    BUG=110233
    Refactored "chrome/common/extensions/extension_manifests_unittest.cc" into
    several different files, now located in
    "chrome/common/extensions/manifest_tests/". I did my best to organize the files
    based on related functionality, although there is some overlap, and perhaps some
    poor choices of organization. Please review my refactoring job and recommend me
    a more appropriate organization of unit tests.
    
    Additionally, I made the RunTestcases() function slightly more robust.
    Previously, it was designed to facilitate running a batch of
    LoadAndExpectError() tests. Now, it can also handle LoadAndExpectSuccess() tests
    in the case that the return value (an exception) may be ignored. This may or may
    not be a useful addition; currently, only a couple unit tests feature such code.
    
    
    Review URL: http://codereview.chromium.org/9431018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132938 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/manifest_tests/extension_manifests_old_unittest.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/extensions/manifest_tests/extension_manifest_test.h"
+
+#include "chrome/common/extensions/extension.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+// Tests that the old permission name "unlimited_storage" still works for
+// backwards compatibility (we renamed it to "unlimitedStorage").
+TEST_F(ExtensionManifestTest, OldUnlimitedStoragePermission) {
+  scoped_refptr<Extension> extension = LoadAndExpectSuccess(
+      "old_unlimited_storage.json",
+      Extension::INTERNAL,
+      Extension::STRICT_ERROR_CHECKS);
+  EXPECT_TRUE(extension->HasAPIPermission(
+      ExtensionAPIPermission::kUnlimitedStorage));
+}
---
+++ chrome/common/extensions/manifest_tests/extension_manifests_ui_unittest.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/extensions/manifest_tests/extension_manifest_test.h"
+
+#include "chrome/common/extensions/extension_manifest_constants.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace errors = extension_manifest_errors;
+
+TEST_F(ExtensionManifestTest, DisallowMultipleUISurfaces) {
+  LoadAndExpectError("multiple_ui_surfaces.json", errors::kOneUISurfaceOnly);
+}
