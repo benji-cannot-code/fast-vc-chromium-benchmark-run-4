391f2937e06fc88871118d6e291b2940a5c3a042
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 07:19:51 +0000

    Remove the tooltip from the guest login pod
    R=hashimoto@chromium.org
    BUG=123360
    TEST=On the chrome OS login screen, hover the mouse over the guest pod and ensure there is no tooltip.
    
    Review URL: https://chromiumcodereview.appspot.com/10124003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -238,7 +238,7 @@ cr.define('login', function() {
       this.signedInIndicatorElement.hidden = !this.user_.signedIn;
 
       if (this.isGuest) {
-        this.imageElement.title = this.user_.displayName;
+        this.imageElement.title = '';
         this.enterButtonElement.hidden = false;
         this.passwordElement.hidden = true;
         this.signinButtonElement.hidden = true;
