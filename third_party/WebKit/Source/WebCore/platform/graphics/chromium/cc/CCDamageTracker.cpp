a079c5107d90c240d86e9bead38ef798ec749179
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-20 17:57:18 +0000

    [chromium] Some filters require inflating damage rect in CCDamageTracker https://bugs.webkit.org/show_bug.cgi?id=84373
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    When a layer is blurred, damaged pixels are blurred out into a radius
    and their damage should be expanded to include total blurred region.
    
    Unit test: CCDamageTrackerTest.verifyDamageForBlurredSurface
    
    * platform/graphics/chromium/cc/CCDamageTracker.cpp:
    (WebCore::CCDamageTracker::updateDamageTrackingState):
    (WebCore::CCDamageTracker::expandDamageRectWithForegroundFilters):
    (WebCore):
    * platform/graphics/chromium/cc/CCDamageTracker.h:
    (WebCore):
    (CCDamageTracker):
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::trackDamageForAllSurfaces):
    
    Source/WebKit/chromium:
    
    * tests/CCDamageTrackerTest.cpp:
    (WebKitTests::emulateDrawingOneFrame):
    (WebKitTests::TEST_F):
    (WebKitTests):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCDamageTracker.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCDamageTracker.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/CCDamageTracker.h"
 
+#include "FilterOperations.h"
 #include "cc/CCLayerImpl.h"
 #include "cc/CCLayerTreeHostCommon.h"
 #include "cc/CCRenderSurface.h"
@@ -57,7 +58,7 @@ CCDamageTracker::~CCDamageTracker()
 {
 }
 
-void CCDamageTracker::updateDamageTrackingState(const Vector<CCLayerImpl*>& layerList, int targetSurfaceLayerID, bool targetSurfacePropertyChangedOnlyFromDescendant, const IntRect& targetSurfaceContentRect, CCLayerImpl* targetSurfaceMaskLayer)
+void CCDamageTracker::updateDamageTrackingState(const Vector<CCLayerImpl*>& layerList, int targetSurfaceLayerID, bool targetSurfacePropertyChangedOnlyFromDescendant, const IntRect& targetSurfaceContentRect, CCLayerImpl* targetSurfaceMaskLayer, const FilterOperations& filters)
 {
     //
     // This function computes the "damage rect" of a target surface, and updates the state
@@ -137,6 +138,8 @@ void CCDamageTracker::updateDamageTrackingState(const Vector<CCLayerImpl*>& laye
         m_currentDamageRect = damageFromActiveLayers;
         m_currentDamageRect.uniteIfNonZero(damageFromSurfaceMask);
         m_currentDamageRect.uniteIfNonZero(damageFromLeftoverRects);
+
+        expandDamageRectWithForegroundFilters(filters);
     }
 
     // The next history map becomes the current map for the next frame.
@@ -305,6 +308,17 @@ void CCDamageTracker::extendDamageForRenderSurface(CCLayerImpl* layer, FloatRect
         // In the current implementation, a change in the replica mask damages the entire replica region.
         if (replicaIsNew || replicaMaskLayer->layerPropertyChanged() || !replicaMaskLayer->updateRect().isEmpty())
             targetDamageRect.uniteIfNonZero(replicaMaskLayerRect);
+    }
+}
+
+void CCDamageTracker::expandDamageRectWithForegroundFilters(const FilterOperations& filters)
+{
+    // Filters can spread damage around in the surface.
+    if (filters.hasFilterThatMovesPixels()) {
+        int top, right, bottom, left;
+        filters.getOutsets(top, right, bottom, left);
+        m_currentDamageRect.move(-left, -top);
+        m_currentDamageRect.expand(left + right, top + bottom);
     }
 }
 
