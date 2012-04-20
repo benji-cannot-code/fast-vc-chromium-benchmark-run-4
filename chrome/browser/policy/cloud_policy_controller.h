243006972d6647d508b770e7690b44ad94845fa3
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 10:30:07 +0000

    Don't trigger a state change on policy refresh rate changes.
    Otherwise, we might trigger additional notifications inadvertently.
    
    BUG=chromium-os:29245
    TEST=See bug.
    
    
    Review URL: http://codereview.chromium.org/10137001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133173 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud_policy_controller.h
+++ chrome/browser/policy/cloud_policy_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/time.h"
 #include "chrome/browser/policy/cloud_policy_constants.h"
 #include "chrome/browser/policy/cloud_policy_data_store.h"
 
@@ -121,7 +122,13 @@ class CloudPolicyController : public CloudPolicyDataStore::Observer {
   void SetState(ControllerState new_state);
 
   // Computes the policy refresh delay to use.
-  int64 GetRefreshDelay();
+  base::TimeDelta GetRefreshDelay();
+
+  // Schedules a DoWork() invocation |delay| time units later.
+  void ScheduleDelayedWorkTask(const base::TimeDelta& delay);
+
+  // Gets the last refresh time, defaulting to |now| if there is none.
+  base::Time GetLastRefreshTime(const base::Time& now);
 
   DeviceManagementService* service_;
   CloudPolicyCacheBase* cache_;
