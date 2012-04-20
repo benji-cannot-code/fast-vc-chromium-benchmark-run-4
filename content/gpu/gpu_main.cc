7df52f628fcb6445a166dd5b034c2e2547c6d93a
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 22:29:42 +0000

    Final(?) eradications of GDK / GTK from the GPU process.
    TBR=darin
    Review URL: https://chromiumcodereview.appspot.com/10078018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133285 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -35,10 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #endif
 
-#if defined(TOOLKIT_GTK)
-#include "ui/gfx/gtk_util.h"
-#endif
-
 #if defined(OS_LINUX)
 #include "content/public/common/sandbox_init.h"
 #endif
@@ -62,9 +58,6 @@ int GpuMain(const content::MainFunctionParams& parameters) {
         SEM_NOOPENFILEERRORBOX);
 #elif defined(USE_X11)
     ui::SetDefaultX11ErrorHandlers();
-#endif
-#if defined(TOOLKIT_GTK)
-    gfx::GdkInitFromCommandLine(*CommandLine::ForCurrentProcess());
 #endif
   }
 
