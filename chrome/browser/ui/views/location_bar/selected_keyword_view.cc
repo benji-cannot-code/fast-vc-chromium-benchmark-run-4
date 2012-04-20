ae2e2257b6b39d3df131a828d241f161edc035cc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 23:51:45 +0000

    views: Do not pass SkColor by const-reference as it's defined as POD(integer). Final patch.
    This is a follow up to r128782.
    
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10169016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133306 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/selected_keyword_view.cc
+++ chrome/browser/ui/views/location_bar/selected_keyword_view.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 SelectedKeywordView::SelectedKeywordView(const int background_images[],
                                          int contained_image,
-                                         const SkColor& color,
+                                         SkColor color,
                                          Profile* profile)
     : IconLabelBubbleView(background_images, contained_image, color),
       profile_(profile) {
--- chrome/browser/ui/webui/ntp/favicon_webui_handler.cc
+++ chrome/browser/ui/webui/ntp/favicon_webui_handler.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-StringValue* SkColorToCss(const SkColor& color) {
+StringValue* SkColorToCss(SkColor color) {
   return new StringValue(base::StringPrintf("rgb(%d, %d, %d)",
                                             SkColorGetR(color),
                                             SkColorGetG(color),
