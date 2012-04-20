06eaba31be5851e9368e15fd7f44ab3e21b18d83
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 18:31:45 +0000

    Changed URLRequestContextGetter used for gdata calls to system.
    BUG=chromium-os:29043
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10157003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_operations.cc
+++ chrome/browser/chromeos/gdata/gdata_operations.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/string_number_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/gdata/gdata_util.h"
 #include "chrome/browser/net/browser_url_util.h"
 #include "chrome/common/libxml_utils.h"
@@ -141,7 +142,7 @@ void AuthOperation::Start() {
   scopes.push_back(kSpreadsheetsScope);
   scopes.push_back(kUserContentScope);
   oauth2_access_token_fetcher_.reset(new OAuth2AccessTokenFetcher(
-      this, profile_->GetRequestContext()));
+      this, g_browser_process->system_request_context()));
   NotifyStart();
   oauth2_access_token_fetcher_->Start(
       GaiaUrls::GetInstance()->oauth2_chrome_client_id(),
@@ -228,7 +229,7 @@ void UrlFetchOperationBase::Start(const std::string& auth_token) {
   DVLOG(1) << "URL: " << url.spec();
 
   url_fetcher_.reset(URLFetcher::Create(url, GetRequestType(), this));
-  url_fetcher_->SetRequestContext(profile_->GetRequestContext());
+  url_fetcher_->SetRequestContext(g_browser_process->system_request_context());
   // Always set flags to neither send nor save cookies.
   url_fetcher_->SetLoadFlags(
       net::LOAD_DO_NOT_SEND_COOKIES | net::LOAD_DO_NOT_SAVE_COOKIES |
