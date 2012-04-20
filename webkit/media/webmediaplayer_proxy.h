040350df89acd32d2dea638df2765a65a5eae79c
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 14:06:48 +0000

    Revert 133191 - Update MediaSource API implementation to match v0.4 spec. - Adding support for sourceAddId() & sourceRemoveId(). - Adding id parameter to sourceAppend() signature.
    BUG=122897
    TEST=Covered by LayoutTests
    TBR=scherkus@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10153003
    
    TBR=acolwell@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133195 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_proxy.h
+++ webkit/media/webmediaplayer_proxy.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
 #include "media/base/pipeline.h"
-#include "media/filters/chunk_demuxer.h"
 #include "media/filters/chunk_demuxer_client.h"
 #include "media/filters/ffmpeg_video_decoder.h"
 #include "webkit/media/buffered_data_source.h"
@@ -90,10 +89,7 @@ class WebMediaPlayerProxy
 
   // Methods for Demuxer communication.
   void DemuxerFlush();
-  media::ChunkDemuxer::Status DemuxerAddId(const std::string& id,
-                                           const std::string& type);
-  bool DemuxerRemoveId(const std::string& id);
-  bool DemuxerAppend(const std::string& id, const uint8* data, size_t length);
+  bool DemuxerAppend(const uint8* data, size_t length);
   void DemuxerEndOfStream(media::PipelineStatus status);
   void DemuxerShutdown();
 
