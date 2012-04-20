ce65eaae868ccf12d38a89a8abd5a3b8609c044e
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-20 00:58:09 +0000

    [Chromium] IndexedDB: Prep for changing keyPath return type https://bugs.webkit.org/show_bug.cgi?id=84208
    To introduce Array-type key paths (see http://webkit.org/b/84207) the keyPath() accessors
    on WebIDBObjectStore and WebIDBIndex need to change return type. To do so requires temporarily
    changing the method names to keyPathString with compatibility shims (on the WebKit side)
    then switching the implementation in the Chromium port in a follow-up patch.
    
    Reviewed by Kent Tamura.
    
    * public/WebIDBIndex.h:
    (WebKit::WebIDBIndex::keyPath):
    (WebKit::WebIDBIndex::keyPathString):
    * public/WebIDBObjectStore.h:
    (WebKit::WebIDBObjectStore::keyPath):
    (WebKit::WebIDBObjectStore::keyPathString):
    * src/WebIDBIndexImpl.cpp:
    (WebKit::WebIDBIndexImpl::keyPathString):
    * src/WebIDBIndexImpl.h:
    (WebIDBIndexImpl):
    * src/WebIDBObjectStoreImpl.cpp:
    (WebKit::WebIDBObjectStoreImpl::keyPathString):
    * src/WebIDBObjectStoreImpl.h:
    (WebIDBObjectStoreImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebIDBObjectStoreImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebIDBObjectStoreImpl.cpp
@@ -58,7 +58,7 @@ WebString WebIDBObjectStoreImpl::name() const
     return m_objectStore->name();
 }
 
-WebString WebIDBObjectStoreImpl::keyPath() const
+WebString WebIDBObjectStoreImpl::keyPathString() const
 {
     return m_objectStore->keyPath();
 }
