5b8f643dc7f4e8c578f9dd1a1020919206e4c452
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 02:46:37 +0000

    Breaking KioskModeTest.EnableKioskModeTest on Linux ChromiumOS Tests (dbg)(3) reliably and reverting locally fixed.
    Revert 133057 - Reland Propagate OnNativeWidgetMove to delegate/observers, etc.
    Continuation of reverted CL http://codereview.chromium.org/10021029
    The original CL was reverted due to a naming conflict between:
    WidgetDelegate::OnWidgetMove and Widget::Observer::OnWidgetMove.
    I renamed the new Observer method to OnWidgetMoved.
    
    Original description:
    Propagate OnNativeWidgetMove to delegate/observers, etc.
    NativeWidget[Aura|Win] calls new Widget::OnNativeWidgetMove.
    Widgets alert delegates and new Widget::Observer::OnWidgetMove.
    
    TabDragController2: consume new observer method, simplify.
    Nix BrowserWindowMoveObserver; use Widget::Observer instead.
    Replace move_loop_browser_view_ with move_loop_widget.
    Replace GetAttachedBrowserView with GetAttachedBrowserWidget.
    
    BubbleDelegateView: consume new observer method.
    Make bubbles move with their anchor_view's Widgets.
    SchedulePaint on BubbleFrameView when mirroring its arrow.
    
    BUG=105151, 116336
    TEST=Bubbles can move with the browser window (inspecting extension popups, etc.); no Ash / --enable-tab-browser-dragging regressions.
    
    Review URL: http://codereview.chromium.org/10025039
    
    TBR=msw@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10142008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BookmarkBarView;
 class Browser;
 class BrowserViewLayout;
+class BrowserWindowMoveObserver;
 class ContentsContainer;
 class DownloadShelfView;
 class Extension;
@@ -209,6 +210,10 @@ class BrowserView : public BrowserWindow,
   // Restores the focused view. This is also used to set the initial focus
   // when a new browser window is created.
   void RestoreFocus();
+
+  void set_move_observer(BrowserWindowMoveObserver* observer) {
+    move_observer_ = observer;
+  }
 
 #if defined(USE_ASH)
   LauncherUpdater* icon_updater() const { return icon_updater_.get(); }
@@ -680,6 +685,8 @@ class BrowserView : public BrowserWindow,
   bool force_location_bar_focus_;
 
   PendingFullscreenRequest fullscreen_request_;
+
+  BrowserWindowMoveObserver* move_observer_;
 
   gfx::ScopedSysColorChangeListener color_change_listener_;
 
