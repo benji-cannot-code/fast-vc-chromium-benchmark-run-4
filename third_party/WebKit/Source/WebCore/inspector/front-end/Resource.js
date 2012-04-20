aafb85116a9c887e8dbec6c8a4ae84cdabebbd50
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-20 16:28:07 +0000

    Web Inspector: follow up to r114729: more @return annotations added. https://bugs.webkit.org/show_bug.cgi?id=84447
    Reviewed by Yury Semikhatsky.
    
    * inspector/front-end/Resource.js:
    (WebInspector.Resource):
    (WebInspector.ResourceRevision.prototype.get content):
    * inspector/front-end/ResourceTreeModel.js:
    (WebInspector.ResourceTreeFrame.prototype._addRequest):
    * inspector/front-end/ResourceUtils.js:
    (String.prototype.asParsedURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Resource.js
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @param {string} documentURL
  * @param {NetworkAgent.FrameId} frameId
  * @param {NetworkAgent.LoaderId} loaderId
- * @param {WebInspector.ResourceType=} type
- * @param {string=} mimeType
+ * @param {WebInspector.ResourceType} type
+ * @param {string} mimeType
  */
 WebInspector.Resource = function(request, url, documentURL, frameId, loaderId, type, mimeType)
 {
@@ -52,8 +52,8 @@ WebInspector.Resource = function(request, url, documentURL, frameId, loaderId, t
     this._mimeType = mimeType;
     this.history = [];
 
-    this._content = undefined;
-    this._contentEncoded = undefined;
+    /** @type {?string} */ this._content;
+    /** @type {boolean} */ this._contentEncoded;
     this._pendingContentCallbacks = [];
 }
 
@@ -155,7 +155,7 @@ WebInspector.Resource.Events = {
 
 WebInspector.Resource.prototype = {
     /**
-     * @type {?WebInspector.NetworkRequest}
+     * @return {?WebInspector.NetworkRequest}
      */
     get request()
     {
@@ -163,7 +163,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {string}
      */
     get url()
     {
@@ -182,7 +182,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {string}
      */
     get documentURL()
     {
@@ -190,7 +190,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {NetworkAgent.FrameId}
+     * @return {NetworkAgent.FrameId}
      */
     get frameId()
     {
@@ -198,7 +198,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {NetworkAgent.LoaderId}
+     * @return {NetworkAgent.LoaderId}
      */
     get loaderId()
     {
@@ -206,7 +206,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {string}
      */
     get displayName()
     {
@@ -214,7 +214,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {WebInspector.ResourceType}
+     * @return {WebInspector.ResourceType}
      */
     get type()
     {
@@ -222,7 +222,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {string}
      */
     get mimeType()
     {
@@ -230,7 +230,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {Array.<WebInspector.ConsoleMessage>}
+     * @return {Array.<WebInspector.ConsoleMessage>}
      */
     get messages()
     {
@@ -252,7 +252,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {number}
+     * @return {number}
      */
     get errors()
     {
@@ -265,7 +265,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {number}
+     * @return {number}
      */
     get warnings()
     {
@@ -286,7 +286,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {?string}
      */
     get content()
     {
@@ -294,7 +294,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {boolean}
      */
     get contentEncoded()
     {
@@ -302,7 +302,7 @@ WebInspector.Resource.prototype = {
     },
 
     /**
-     * @type {number}
+     * @return {number}
      */
     get contentTimestamp()
     {
@@ -463,7 +463,7 @@ WebInspector.Resource.prototype.__proto__ = WebInspector.Object.prototype;
  * @constructor
  * @implements {WebInspector.ContentProvider}
  * @param {WebInspector.Resource} resource
- * @param {string|undefined} content
+ * @param {?string|undefined} content
  * @param {number} timestamp
  */
 WebInspector.ResourceRevision = function(resource, content, timestamp)
@@ -475,7 +475,7 @@ WebInspector.ResourceRevision = function(resource, content, timestamp)
 
 WebInspector.ResourceRevision.prototype = {
     /**
-     * @type {WebInspector.Resource}
+     * @return {WebInspector.Resource}
      */
     get resource()
     {
@@ -483,7 +483,7 @@ WebInspector.ResourceRevision.prototype = {
     },
 
     /**
-     * @type {number}
+     * @return {number}
      */
     get timestamp()
     {
@@ -491,11 +491,11 @@ WebInspector.ResourceRevision.prototype = {
     },
 
     /**
-     * @type {string}
+     * @return {?string}
      */
     get content()
     {
-        return this._content;
+        return this._content || null;
     },
 
     revertToThis: function()
--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourceUtils.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourceUtils.js
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * @constructor
- * @param {String|string} url
+ * @param {string} url
  */
 WebInspector.ParsedURL = function(url)
 {
@@ -111,7 +111,7 @@ WebInspector.ParsedURL.prototype = {
  */
 String.prototype.asParsedURL = function()
 {
-    var parsedURL = new WebInspector.ParsedURL(this);
+    var parsedURL = new WebInspector.ParsedURL(this.toString());
     if (parsedURL.isValid)
         return parsedURL;
     return null;
