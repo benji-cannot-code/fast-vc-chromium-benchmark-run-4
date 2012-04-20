6ff54245414f5e9b68b0ed1673ecbadb9a10a53c
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 23:03:04 +0000

    Fixed the DomMutationEvent PyAuto methods so that MutationObservers are only created after the DOM has loaded.
    This allows the hooks to be called any time after the page has begun loading.
    
    BUG=None
    TEST=functional/apptest.py
    
    
    Review URL: http://codereview.chromium.org/10155003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pyautolib/dom_mutation_observer.js
+++ chrome/test/pyautolib/dom_mutation_observer.js
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *  expected_value: If not null, regular expression to match with the value of
  *      |attribute| after the mutation.
  */
-
 function(automation_id, observer_id, observer_type, xpath, attribute,
          expected_value) {
 
@@ -233,22 +232,28 @@ function(automation_id, observer_id, observer_type, xpath, attribute,
   }
 
   /* Interpret arguments and launch the requested observer function. */
-  var observer;
-  switch (observer_type) {
-    case "add":
-      observeAdd(xpath);
-      break;
-    case "remove":
-      observeRemove(xpath);
-      break;
-    case "change":
-      observeChange(xpath);
-      break;
-    case "exists":
-      observeExists(xpath);
-      break;
+  function installMutationObserver() {
+    switch (observer_type) {
+      case "add":
+        observeAdd(xpath);
+        break;
+      case "remove":
+        observeRemove(xpath);
+        break;
+      case "change":
+        observeChange(xpath);
+        break;
+      case "exists":
+        observeExists(xpath);
+        break;
+    }
+    console.log("MutationObserver javscript injection completed.");
   }
 
-  console.log("MutationObserver javscript injection completed.");
-
+  /* Ensure the DOM is loaded before attempting to create MutationObservers. */
+  if (document.body) {
+    installMutationObserver();
+  } else {
+    window.addEventListener("DOMContentLoaded", installMutationObserver, true);
+  }
 }
