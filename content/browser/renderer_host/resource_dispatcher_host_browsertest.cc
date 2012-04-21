052886538eae2c9700b82fdce72313c46ac44307
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-21 02:15:28 +0000

    Fix compile error for GCC 4.6 following r133307
    ASSERT_EQ(false, ...) -> ASSERT_FALSE(...)
    
    BUG=none
    TEST=it compiles
    TBR=jam@chromium.org
    R=thestig@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10144014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133324 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host_browsertest.cc
+++ content/browser/renderer_host/resource_dispatcher_host_browsertest.cc
@@ -195,7 +195,7 @@ IN_PROC_BROWSER_TEST_F(ResourceDispatcherHostBrowserTest,
   CheckTitleTest(GetMockURL("content-sniffer-test3.html"),
                  "Content Sniffer Test 3", 1);
   EXPECT_EQ(1, browser()->tab_count());
-  ASSERT_EQ(false, got_downloads());
+  ASSERT_FALSE(got_downloads());
 }
 
 IN_PROC_BROWSER_TEST_F(ResourceDispatcherHostBrowserTest,
@@ -261,7 +261,7 @@ IN_PROC_BROWSER_TEST_F(ResourceDispatcherHostBrowserTest,
       test_server()->GetURL("files/title2.html"),
       "Title Of Awesomeness", 1);
 
-  ASSERT_EQ(false, got_downloads());
+  ASSERT_FALSE(got_downloads());
 }
 
 // Tests that onunload is run for cross-site requests.  (Bug 1114994)
