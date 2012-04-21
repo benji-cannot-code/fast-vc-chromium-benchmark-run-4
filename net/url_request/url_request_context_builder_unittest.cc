3bfd5e436132a00f362bd522c1f2e7a2a141e322
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-21 01:21:06 +0000

    Support simple proxy configuration for all platforms other than Linux
    For Linux, a ProxyConfigService must be provided.
    
    BUG=122779
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10155004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133318 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_context_builder_unittest.cc
+++ net/url_request/url_request_context_builder_unittest.cc
@@ -5,11 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/url_request/url_request_context_builder.h"
 
+#include "build/build_config.h"
 #include "net/test/test_server.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+
+#if defined(OS_LINUX)
+#include "net/proxy/proxy_config.h"
+#include "net/proxy/proxy_config_service_fixed.h"
+#endif  // defined(OS_LINUX)
 
 namespace net {
 
@@ -34,7 +40,12 @@ class URLRequestContextBuilderTest : public PlatformTest {
  protected:
   URLRequestContextBuilderTest()
       : test_server_(
-          FilePath(FILE_PATH_LITERAL("net/data/url_request_unittest"))) {}
+          FilePath(FILE_PATH_LITERAL("net/data/url_request_unittest"))) {
+#if defined(OS_LINUX)
+    builder_.set_proxy_config_service(
+        new ProxyConfigServiceFixed(ProxyConfig::CreateDirect()));
+#endif  // defined(OS_LINUX)
+  }
 
   LocalHttpTestServer test_server_;
   URLRequestContextBuilder builder_;
