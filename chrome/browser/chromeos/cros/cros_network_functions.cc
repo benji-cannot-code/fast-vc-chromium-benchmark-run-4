b4cc0aae1444a43a1ae86d7fe3131be8a02a7350
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-21 07:30:50 +0000

    Reimplement SetNetworkIPConfigProperty and RemoveIPconfig without Libcros
    BUG=chromium-os:16557
    TEST=unit_tests --gtest_filter="CrosNetworkFunctions*"
    
    
    Review URL: http://codereview.chromium.org/10143007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cros_network_functions.cc
+++ chrome/browser/chromeos/cros/cros_network_functions.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/cashew_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/flimflam_device_client.h"
+#include "chromeos/dbus/flimflam_ipconfig_client.h"
 #include "chromeos/dbus/flimflam_manager_client.h"
 #include "chromeos/dbus/flimflam_profile_client.h"
 #include "chromeos/dbus/flimflam_service_client.h"
@@ -264,9 +265,15 @@ void CrosSetNetworkDeviceProperty(const char* device_path,
 void CrosSetNetworkIPConfigProperty(const char* ipconfig_path,
                                     const char* property,
                                     const base::Value& value) {
-  ScopedGValue gvalue(ConvertValueToGValue(value));
-  chromeos::SetNetworkIPConfigPropertyGValue(ipconfig_path, property,
-                                             gvalue.get());
+  if (g_libcros_network_functions_enabled) {
+    ScopedGValue gvalue(ConvertValueToGValue(value));
+    chromeos::SetNetworkIPConfigPropertyGValue(ipconfig_path, property,
+                                               gvalue.get());
+  } else {
+    DBusThreadManager::Get()->GetFlimflamIPConfigClient()->SetProperty(
+        dbus::ObjectPath(ipconfig_path), property, value,
+        base::Bind(&DoNothing));
+  }
 }
 
 void CrosSetNetworkManagerProperty(const char* property,
@@ -553,7 +560,12 @@ bool CrosAddIPConfig(const char* device_path, IPConfigType type) {
 }
 
 bool CrosRemoveIPConfig(IPConfig* config) {
-  return chromeos::RemoveIPConfig(config);
+  if (g_libcros_network_functions_enabled) {
+    return chromeos::RemoveIPConfig(config);
+  } else {
+    return DBusThreadManager::Get()->GetFlimflamIPConfigClient()->
+        CallRemoveAndBlock(dbus::ObjectPath(config->path));
+  }
 }
 
 void CrosFreeIPConfigStatus(IPConfigStatus* status) {
