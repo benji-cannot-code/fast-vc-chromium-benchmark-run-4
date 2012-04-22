bf7ea05b0a4ae650766f8074c00be6142921adbd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-22 16:43:27 +0000

    [Qt] Multi-level tap-to-zoom. https://bugs.webkit.org/show_bug.cgi?id=84456
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-04-22
    Reviewed by Kenneth Rohde Christiansen.
    
    Replace tap-to-zoomed flag with a stack of progressively higher zoom levels,
    and zoom out to last zoom-level when attempting to zoom to current level.
    
    Additionally detect a series of tap-to-zoom gestures on the same level and
    continue to zoom out.
    
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    (WebKit::QtViewportInteractionEngine::QtViewportInteractionEngine):
    (WebKit::QtViewportInteractionEngine::zoomToAreaGestureEnded):
    (WebKit::QtViewportInteractionEngine::pinchGestureStarted):
    * UIProcess/qt/QtViewportInteractionEngine.h:
    (QtViewportInteractionEngine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
@@ -149,7 +149,6 @@ private:
     OwnPtr<ViewportUpdateDeferrer> m_touchUpdateDeferrer;
 
     bool m_hadUserInteraction;
-    bool m_zoomedToArea;
 
     class ScaleAnimation : public QVariantAnimation {
     public:
@@ -164,6 +163,8 @@ private:
     QPointF m_lastPinchCenterInViewportCoordinates;
     QPointF m_lastScrollPosition;
     qreal m_pinchStartScale;
+    qreal m_zoomOutScale;
+    QList<qreal> m_scaleStack;
 };
 
 }
