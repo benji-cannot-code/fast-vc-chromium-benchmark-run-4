ced09c26a7718378c397c03592091d85761461f9
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-23 16:20:06 +0000

    [V8] Add an optional Isolate argument to toV8(). https://bugs.webkit.org/show_bug.cgi?id=84161
    Reviewed by Nate Chapin.
    
    The final objective is to pass Isolate around in V8 bindings.
    This patch adds an optional Isolate argument to toV8().
    After rewriting all toV8() callers so that they pass Isolate,
    I will make the Isolate argument non-optional.
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    Modified as described above.
    (GenerateHeader):
    (NativeToJSValue):
    * bindings/v8/custom/V8BlobCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8CSSRuleCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8CSSStyleSheetCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8CSSValueCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8CanvasPixelArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8DOMTokenListCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8DataViewCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8DocumentCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8EntryCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8EntrySyncCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8EventCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Float32ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Float64ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8HTMLCollectionCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8HTMLElementCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8IDBAnyCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8IDBKeyCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8ImageDataCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Int16ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Int32ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Int8ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8LocationCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8NamedNodeMapCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8NodeCustom.cpp:
    (WebCore::toV8Slow):
    * bindings/v8/custom/V8SVGDocumentCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8SVGElementCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8SVGPathSegCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8ScriptProfileCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8ScriptProfileNodeCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8StyleSheetCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Uint16ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Uint32ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Uint8ArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8Uint8ClampedArrayCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8WorkerContextCustom.cpp:
    (WebCore::toV8):
    
    * bindings/scripts/test/V8/V8Float64Array.h:
    Updated run-bindings-tests results.
    (WebCore):
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestEventConstructor.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestEventTarget.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestInterface.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestNamedConstructor.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestNode.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestObj.h:
    (WebCore::toV8):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.h:
    (WebCore::toV8):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8SVGDocumentCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8SVGDocumentCustom.cpp
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> toV8(SVGDocument* impl, bool forceNewObject)
+v8::Handle<v8::Value> toV8(SVGDocument* impl, v8::Isolate* isolate, bool forceNewObject)
 {
     if (!impl)
         return v8::Null();
