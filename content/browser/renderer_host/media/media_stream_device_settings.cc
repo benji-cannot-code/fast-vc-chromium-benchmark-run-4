6dc445c8868924fd2889ed7e3a764358e8f2558a
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 17:05:25 +0000

    Duplicated typedef declaration of DeviceMap
    BUG=NONE
    TEST=NONE
    Review URL: https://chromiumcodereview.appspot.com/10166001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133465 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/media_stream_device_settings.cc
+++ content/browser/renderer_host/media/media_stream_device_settings.cc
@@ -77,7 +77,7 @@ class DeviceIdEquals {
 
 namespace media_stream {
 
-typedef std::map< MediaStreamType, StreamDeviceInfoArray > DeviceMap;
+typedef std::map<MediaStreamType, StreamDeviceInfoArray> DeviceMap;
 
 // Device request contains all data needed to keep track of requests between the
 // different calls.
@@ -102,8 +102,6 @@ class MediaStreamDeviceSettingsRequest : public MediaStreamRequest {
   // RequestMediaAccessPermission, to make sure that we never post twice to it.
   bool posted_task;
 };
-
-typedef std::map<MediaStreamType, StreamDeviceInfoArray> DeviceMap;
 
 MediaStreamDeviceSettings::MediaStreamDeviceSettings(
     SettingsRequester* requester)
