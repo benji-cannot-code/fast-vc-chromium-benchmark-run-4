a0b1a790d6bca29e090b8139b47a23be02aa4408
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-23 19:24:44 +0000

    Split up flakiness_dashboard.html into multiple files. This is a step in the direction of making the code more maintainable (e.g. being able to use qunit for testing).
    * TestResultServer/static-dashboards/dashboard_base.js:
    * TestResultServer/static-dashboards/flakiness_dashboard.css: Added.
    * TestResultServer/static-dashboards/flakiness_dashboard.html:
    * TestResultServer/static-dashboards/flakiness_dashboard.js: Copied from Tools/TestResultServer/static-dashboards/flakiness_dashboard.html.
    * TestResultServer/static-dashboards/flakiness_dashboard_tests.css: Added.
    * TestResultServer/static-dashboards/flakiness_dashboard_unittests.js: Renamed from Tools/TestResultServer/static-dashboards/flakiness_dashboard_tests.js.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/dashboard_base.js
@@ -587,7 +587,7 @@ function appendJSONScriptElements()
     parseParameters();
 
     if (g_crossDashboardState.useTestData) {
-        appendScript('flakiness_dashboard_tests.js');
+        appendScript('flakiness_dashboard_unittests.js');
         return;
     }
 
