409bf9fc01c25fbe3ad21a3d053ee2f099d9e1e9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-23 15:05:37 +0000

    [EFL][WK2] Fix build break when non-cross platform CONTEXT_MENUS are enabled. https://bugs.webkit.org/show_bug.cgi?id=84136
    Patch by Michał Pakuła vel Rutka <m.pakula@samsung.com> on 2012-04-23
    Reviewed by Andreas Kling.
    
    There was a mismatch between contextMenuItemVector declaration and definition.
    Fixed by changing PlatformMenuDescription for EFL port by adding a const modifier.
    
    No new tests required.
    
    * platform/PlatformMenuDescription.h:
    (WebCore): Added const modifier to PlatformMenuDescription definition.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PlatformMenuDescription.h
+++ third_party/WebKit/Source/WebCore/platform/PlatformMenuDescription.h
@@ -53,7 +53,7 @@ namespace WebCore {
     typedef wxMenu* PlatformMenuDescription;
 #elif PLATFORM(EFL)
     class ContextMenuItem;
-    typedef Vector<ContextMenuItem>* PlatformMenuDescription;
+    typedef const Vector<ContextMenuItem>* PlatformMenuDescription;
 #else
     typedef void* PlatformMenuDescription;
 #endif
