d360b030687a0b36f42d8b7c07dfe477f5e22a96
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 04:01:35 +0000

    Update uses of TimeDelta in mac bookmark test code.
    R=jrg@chromium.org
    BUG=108171
    
    
    Review URL: http://codereview.chromium.org/10026015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_hover_state_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_hover_state_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,7 +49,8 @@ TEST_F(BookmarkBarFolderHoverStateTest, HoverState) {
   message_loop.PostDelayedTask(
       FROM_HERE,
       MessageLoop::QuitClosure(),
-      bookmarks::kDragHoverOpenDelay * 1000.0 * 1.5);
+      base::TimeDelta::FromMilliseconds(
+          bookmarks::kDragHoverOpenDelay * 1000.0 * 1.5));
   message_loop.Run();
   ASSERT_EQ(kHoverStateOpen, [bbfhs hoverState]);
   ASSERT_EQ(button, [bbfhs hoverButton]);
@@ -69,7 +70,8 @@ TEST_F(BookmarkBarFolderHoverStateTest, HoverState) {
   message_loop.PostDelayedTask(
       FROM_HERE,
       MessageLoop::QuitClosure(),
-      bookmarks::kDragHoverCloseDelay * 1000.0 * 1.5);
+      base::TimeDelta::FromMilliseconds(
+          bookmarks::kDragHoverCloseDelay * 1000.0 * 1.5));
   message_loop.Run();
   ASSERT_EQ(kHoverStateClosed, [bbfhs hoverState]);
   ASSERT_EQ(nil, [bbfhs hoverButton]);
