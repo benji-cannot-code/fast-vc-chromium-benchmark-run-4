303ad6e00beaa19809ff158d6deb5299c7c0e011
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-23 11:34:27 +0000

    JS binding code generator doesn't handle "attribute unsigned long[]" well https://bugs.webkit.org/show_bug.cgi?id=84540
    Patch by Vineet Chaudhary <rgf748@motorola.com> on 2012-04-23
    Reviewed by Kentaro Hara.
    
    Codegenerator should handle spaces with the sequence<> to support
    numeric types like "unsigned long", "int" .. etc. and primitive types
    like "boolean", "Date" etc.
    
    Tests: bindings/scripts/test/TestObj.idl
    
    * bindings/scripts/CodeGenerator.pm:
    (SkipIncludeHeader): Rename AvoidInclusionOfType to SkipIncludeHeader.
    (GetArrayType):
    * bindings/scripts/CodeGeneratorJS.pm:
    (AddIncludesForType):
    (NativeToJSValue):
    * bindings/scripts/CodeGeneratorObjC.pm:
    (AddIncludesForType):
    (GenerateImplementation):
    * bindings/scripts/CodeGeneratorV8.pm:
    (AddIncludesForType):
    (GetHeaderClassInclude):
    (GenerateNormalAttrGetter):
    (NativeToJSValue):
    * bindings/scripts/IDLStructure.pm:
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore):
    (WebCore::jsTestObjIntSequenceAttr):
    (WebCore::jsTestObjShortSequenceAttr):
    (WebCore::jsTestObjLongSequenceAttr):
    (WebCore::jsTestObjLongLongSequenceAttr):
    (WebCore::jsTestObjUnsignedIntSequenceAttr):
    (WebCore::jsTestObjUnsignedShortSequenceAttr):
    (WebCore::jsTestObjUnsignedLongSequenceAttr):
    (WebCore::jsTestObjUnsignedLongLongSequenceAttr):
    (WebCore::jsTestObjFloatSequenceAttr):
    (WebCore::jsTestObjDoubleSequenceAttr):
    (WebCore::jsTestObjBooleanSequenceAttr):
    (WebCore::jsTestObjVoidSequenceAttr):
    (WebCore::jsTestObjDateSequenceAttr):
    (WebCore::setJSTestObjSequenceAttr):
    (WebCore::setJSTestObjIntSequenceAttr):
    (WebCore::setJSTestObjShortSequenceAttr):
    (WebCore::setJSTestObjLongSequenceAttr):
    (WebCore::setJSTestObjLongLongSequenceAttr):
    (WebCore::setJSTestObjUnsignedIntSequenceAttr):
    (WebCore::setJSTestObjUnsignedShortSequenceAttr):
    (WebCore::setJSTestObjUnsignedLongSequenceAttr):
    (WebCore::setJSTestObjUnsignedLongLongSequenceAttr):
    (WebCore::setJSTestObjFloatSequenceAttr):
    (WebCore::setJSTestObjDoubleSequenceAttr):
    (WebCore::setJSTestObjBooleanSequenceAttr):
    (WebCore::setJSTestObjVoidSequenceAttr):
    (WebCore::setJSTestObjDateSequenceAttr):
    * bindings/scripts/test/JS/JSTestObj.h:
    (WebCore):
    * bindings/scripts/test/ObjC/DOMTestObj.h:
    * bindings/scripts/test/TestObj.idl:
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::intSequenceAttrAttrGetter):
    (TestObjV8Internal):
    (WebCore::TestObjV8Internal::intSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::shortSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::shortSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::longSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::longSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::longLongSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::longLongSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::unsignedIntSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::unsignedIntSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::unsignedShortSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::unsignedShortSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::unsignedLongSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::unsignedLongSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::unsignedLongLongSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::unsignedLongLongSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::floatSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::floatSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::doubleSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::doubleSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::booleanSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::booleanSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::voidSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::voidSequenceAttrAttrSetter):
    (WebCore::TestObjV8Internal::dateSequenceAttrAttrGetter):
    (WebCore::TestObjV8Internal::dateSequenceAttrAttrSetter):
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGenerator.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGenerator.pm
@@ -341,10 +341,14 @@ sub ParseInterface
 
 # Helpers for all CodeGenerator***.pm modules
 
-sub AvoidInclusionOfType
+sub SkipIncludeHeader
 {
     my $object = shift;
     my $type = shift;
+
+    return 1 if $primitiveTypeHash{$type};
+    return 1 if $numericTypeHash{$type};
+    return 1 if $type eq "String";
 
     # Special case: SVGPoint.h / SVGNumber.h do not exist.
     return 1 if $type eq "SVGPoint" or $type eq "SVGNumber";
@@ -451,7 +455,7 @@ sub GetArrayType
     my $object = shift;
     my $type = shift;
 
-    return $1 if $type =~ /^sequence<([\w\d_]+)>.*/;
+    return $1 if $type =~ /^sequence<([\w\d_\s]+)>.*/;
     return "";
 }
 
