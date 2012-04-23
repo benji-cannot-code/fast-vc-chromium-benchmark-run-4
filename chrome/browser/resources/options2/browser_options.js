d0e8f1fae28979ac65dee5759a1e539c514c4103
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 23:20:06 +0000

    [webui settings] send user profiles info earlier during load
    this prevents jiggle during load because the profile info is available from when the page first loads, instead of a split second later.
    
    BUG=none
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/10139019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/browser_options.js
+++ chrome/browser/resources/options2/browser_options.js
@@ -209,25 +209,31 @@ cr.define('options', function() {
           });
 
       // Users section.
-      var profilesList = $('profiles-list');
-      options.browser_options.ProfileList.decorate(profilesList);
-      profilesList.autoExpands = true;
+      if (typeof templateData.profilesInfo != 'undefined') {
+        $('profiles-section').hidden = false;
 
-      profilesList.addEventListener('change',
-          this.setProfileViewButtonsStatus_);
-      $('profiles-create').onclick = function(event) {
-        chrome.send('createProfile');
-      };
-      $('profiles-manage').onclick = function(event) {
-        var selectedProfile = self.getSelectedProfileItem_();
-        if (selectedProfile)
-          ManageProfileOverlay.showManageDialog(selectedProfile);
-      };
-      $('profiles-delete').onclick = function(event) {
-        var selectedProfile = self.getSelectedProfileItem_();
-        if (selectedProfile)
-          ManageProfileOverlay.showDeleteDialog(selectedProfile);
-      };
+        var profilesList = $('profiles-list');
+        options.browser_options.ProfileList.decorate(profilesList);
+        profilesList.autoExpands = true;
+
+        this.setProfilesInfo_(templateData.profilesInfo);
+
+        profilesList.addEventListener('change',
+            this.setProfileViewButtonsStatus_);
+        $('profiles-create').onclick = function(event) {
+          chrome.send('createProfile');
+        };
+        $('profiles-manage').onclick = function(event) {
+          var selectedProfile = self.getSelectedProfileItem_();
+          if (selectedProfile)
+            ManageProfileOverlay.showManageDialog(selectedProfile);
+        };
+        $('profiles-delete').onclick = function(event) {
+          var selectedProfile = self.getSelectedProfileItem_();
+          if (selectedProfile)
+            ManageProfileOverlay.showDeleteDialog(selectedProfile);
+        };
+      }
 
       if (cr.isChromeOS) {
         if (!UIAccountTweaks.loggedInAsGuest()) {
@@ -662,16 +668,6 @@ cr.define('options', function() {
                                        $('enable-auto-login-checkbox'));
       }
       $('enable-auto-login-checkbox').hidden = !syncData.autoLoginVisible;
-    },
-
-    /**
-     * Display or hide the profiles section of the page. This is used for
-     * multi-profile settings.
-     * @param {boolean} visible True to show the section.
-     * @private
-     */
-    setProfilesSectionVisible_: function(visible) {
-      $('profiles-section').hidden = !visible;
     },
 
     /**
@@ -1376,7 +1372,6 @@ cr.define('options', function() {
     'setMetricsReportingCheckboxState',
     'setMetricsReportingSettingVisibility',
     'setProfilesInfo',
-    'setProfilesSectionVisible',
     'setScreenMagnifierCheckboxState',
     'setSpokenFeedbackCheckboxState',
     'setThemesResetButtonEnabled',
--- chrome/browser/ui/webui/options2/browser_options_handler2.cc
+++ chrome/browser/ui/webui/options2/browser_options_handler2.cc
@@ -365,7 +365,7 @@ void BrowserOptionsHandler::GetLocalizedValues(DictionaryValue* values) {
 #endif
 
   // Pass along sync status early so it will be available during page init.
-  values->Set("syncData", GetSyncStateDictionary());
+  values->Set("syncData", GetSyncStateDictionary().release());
 
   values->SetString("privacyLearnMoreURL", chrome::kPrivacyLearnMoreURL);
 
@@ -391,6 +391,9 @@ void BrowserOptionsHandler::GetLocalizedValues(DictionaryValue* values) {
   values->SetBoolean("multiple_profiles",
       g_browser_process->profile_manager()->GetNumberOfProfiles() > 1);
 #endif
+
+  if (multiprofile_)
+    values->Set("profilesInfo", GetProfilesInfoList().release());
 }
 
 void BrowserOptionsHandler::RegisterCloudPrintValues(DictionaryValue* values) {
@@ -531,8 +534,8 @@ void BrowserOptionsHandler::RegisterMessages() {
 }
 
 void BrowserOptionsHandler::OnStateChanged() {
-  scoped_ptr<DictionaryValue> value(GetSyncStateDictionary());
-  web_ui()->CallJavascriptFunction("BrowserOptions.updateSyncState", *value);
+  web_ui()->CallJavascriptFunction("BrowserOptions.updateSyncState",
+                                   *GetSyncStateDictionary());
 
   SendProfilesInfo();
 }
@@ -609,9 +612,6 @@ void BrowserOptionsHandler::InitializeHandler() {
 void BrowserOptionsHandler::InitializePage() {
   OnTemplateURLServiceChanged();
   ObserveThemeChanged();
-
-  if (multiprofile_)
-    SendProfilesInfo();
 
   SetupMetricsReportingCheckbox();
   SetupMetricsReportingSettingVisibility();
@@ -904,20 +904,13 @@ void BrowserOptionsHandler::GetInstantFieldTrialStatus(const ListValue* args) {
                                    enabled);
 }
 
-void BrowserOptionsHandler::SendProfilesInfo() {
-  // Set profile creation text and button if multi-profiles switch is on.
-  scoped_ptr<Value> visible(Value::CreateBooleanValue(multiprofile_));
-  web_ui()->CallJavascriptFunction("BrowserOptions.setProfilesSectionVisible",
-                                   *visible);
-
-  if (!multiprofile_)
-    return;
-
+scoped_ptr<ListValue> BrowserOptionsHandler::GetProfilesInfoList() {
   ProfileInfoCache& cache =
       g_browser_process->profile_manager()->GetProfileInfoCache();
-  ListValue profile_info_list;
+  scoped_ptr<ListValue> profile_info_list(new ListValue);
   FilePath current_profile_path =
       web_ui()->GetWebContents()->GetBrowserContext()->GetPath();
+
   for (size_t i = 0, e = cache.GetNumberOfProfiles(); i < e; ++i) {
     DictionaryValue* profile_value = new DictionaryValue();
     FilePath profile_path = cache.GetPathOfProfileAtIndex(i);
@@ -940,11 +933,15 @@ void BrowserOptionsHandler::SendProfilesInfo() {
                                cache.GetDefaultAvatarIconUrl(icon_index));
     }
 
-    profile_info_list.Append(profile_value);
+    profile_info_list->Append(profile_value);
   }
 
+  return profile_info_list.Pass();
+}
+
+void BrowserOptionsHandler::SendProfilesInfo() {
   web_ui()->CallJavascriptFunction("BrowserOptions.setProfilesInfo",
-                                   profile_info_list);
+                                   *GetProfilesInfoList());
 }
 
 void BrowserOptionsHandler::CreateProfile(const ListValue* args) {
@@ -1000,13 +997,13 @@ void BrowserOptionsHandler::UpdateAccountPicture() {
 }
 #endif
 
-DictionaryValue* BrowserOptionsHandler::GetSyncStateDictionary() {
-  DictionaryValue* sync_status = new DictionaryValue;
+scoped_ptr<DictionaryValue> BrowserOptionsHandler::GetSyncStateDictionary() {
+  scoped_ptr<DictionaryValue> sync_status(new DictionaryValue);
   ProfileSyncService* service(ProfileSyncServiceFactory::
       GetInstance()->GetForProfile(Profile::FromWebUI(web_ui())));
   sync_status->SetBoolean("syncSystemEnabled", !!service);
   if (!service)
-    return sync_status;
+    return sync_status.Pass();
 
   sync_status->SetBoolean("setupCompleted",
                           service->HasSyncSetupCompleted());
@@ -1031,7 +1028,7 @@ DictionaryValue* BrowserOptionsHandler::GetSyncStateDictionary() {
       CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableAutologin) &&
       service->AreCredentialsAvailable());
 
-  return sync_status;
+  return sync_status.Pass();
 }
 
 void BrowserOptionsHandler::HandleSelectDownloadLocation(
