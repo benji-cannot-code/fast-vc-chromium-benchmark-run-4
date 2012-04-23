41e4a972d011de5b5a410b75aa8e02b5a430dc4c
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-23 19:07:14 +0000

    [V8] Pass Isolate to toV8() (Part4) https://bugs.webkit.org/show_bug.cgi?id=84269
    Reviewed by Nate Chapin.
    
    The objective is to pass Isolate to all toV8()s.
    Since there are a lot of toV8()s, I'll make the change
    step by step. This patch passes Isolate to toV8() in
    several custom bindings.
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8HTMLCanvasElementCustom.cpp:
    (WebCore::V8HTMLCanvasElement::getContextCallback):
    * bindings/v8/custom/V8HTMLCollectionCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
    (WebCore::V8HTMLDocument::allAccessorGetter):
    * bindings/v8/custom/V8HTMLElementCustom.cpp:
    (WebCore::toV8Object):
    (WebCore::V8HTMLElement::itemValueAccessorGetter):
    * bindings/v8/custom/V8HTMLFormElementCustom.cpp:
    (WebCore::V8HTMLFormElement::indexedPropertyGetter):
    (WebCore::V8HTMLFormElement::namedPropertyGetter):
    * bindings/v8/custom/V8HTMLFrameSetElementCustom.cpp:
    (WebCore::V8HTMLFrameSetElement::namedPropertyGetter):
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore::v8HTMLImageElementConstructorCallback):
    * bindings/v8/custom/V8HTMLLinkElementCustom.cpp:
    (WebCore::V8HTMLLinkElement::sizesAccessorGetter):
    * bindings/v8/custom/V8HTMLOptionsCollectionCustom.cpp:
    (WebCore::V8HTMLOptionsCollection::indexedPropertyGetter):
    * bindings/v8/custom/V8HTMLOutputElementCustom.cpp:
    (WebCore::V8HTMLOutputElement::htmlForAccessorGetter):
    * bindings/v8/custom/V8HTMLSelectElementCustom.cpp:
    (WebCore::V8HTMLSelectElement::indexedPropertyGetter):
    * bindings/v8/custom/V8ImageDataCustom.cpp:
    (WebCore::toV8):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLElementCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLElementCustom.cpp
@@ -42,13 +42,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 #if ENABLE(MICRODATA)
-static v8::Handle<v8::Value> toV8Object(MicroDataItemValue* itemValue)
+static v8::Handle<v8::Value> toV8Object(MicroDataItemValue* itemValue, v8::Isolate* isolate)
 {
     if (!itemValue)
         return v8::Null();
 
     if (itemValue->isNode())
-        return toV8(itemValue->getNode());
+        return toV8(itemValue->getNode(), isolate);
 
     return v8String(itemValue->getString());
 }
@@ -65,7 +65,7 @@ v8::Handle<v8::Value> toV8(HTMLElement* impl, v8::Isolate* isolate, bool forceNe
 v8::Handle<v8::Value> V8HTMLElement::itemValueAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
 {
     HTMLElement* impl = V8HTMLElement::toNative(info.Holder());
-    return toV8Object(impl->itemValue().get());
+    return toV8Object(impl->itemValue().get(), info.GetIsolate());
 }
 
 void V8HTMLElement::itemValueAccessorSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLFormElementCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLFormElementCustom.cpp
@@ -50,7 +50,7 @@ v8::Handle<v8::Value> V8HTMLFormElement::indexedPropertyGetter(uint32_t index, c
     RefPtr<Node> formElement = form->elements()->item(index);
     if (!formElement)
         return notHandledByInterceptor();
-    return toV8(formElement.release());
+    return toV8(formElement.release(), info.GetIsolate());
 }
 
 v8::Handle<v8::Value> V8HTMLFormElement::namedPropertyGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
@@ -76,9 +76,9 @@ v8::Handle<v8::Value> V8HTMLFormElement::namedPropertyGetter(v8::Local<v8::Strin
     ASSERT(!elements.isEmpty());
 
     if (elements.size() == 1)
-        return toV8(elements.at(0).release());
+        return toV8(elements.at(0).release(), info.GetIsolate());
 
-    return toV8(V8NamedNodesCollection::create(elements));
+    return toV8(V8NamedNodesCollection::create(elements), info.GetIsolate());
 }
 
 } // namespace WebCore
