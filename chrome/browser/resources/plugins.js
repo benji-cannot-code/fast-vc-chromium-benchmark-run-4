1141ea4eef6649bac4ba6f506ac796b32819aaa4
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 11:52:17 +0000

    Disable "always allowed" checkbox in chrome://plugins for disabled plug-ins.
    BUG=124220
    TEST=see bug for manual test
    
    Review URL: http://codereview.chromium.org/10095029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/plugins.js
+++ chrome/browser/resources/plugins.js
@@ -52,6 +52,7 @@ function loadShowDetailsFromPrefs(show_details) {
  *         update_url: 'http://update/',
  *         critical: true,
  *         enabled: true,
+ *         identifier: 'plugin-name',
  *         plugin_files: [
  *           {
  *             path: '/blahblah/blahblah/MyCrappyPlugin.plugin',
