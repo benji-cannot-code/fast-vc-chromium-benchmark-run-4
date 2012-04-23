232769da85cf71fba86906c78c71a492b27df836
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 22:01:25 +0000

    Revert 133469 - Method should not be OVERRIDE. Fix Clang.
    BUG=none
    TEST=none
    TBR=sky
    Review URL: https://chromiumcodereview.appspot.com/10183007
    
    TBR=ben@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10199003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/keyboard_overlay_dialog_view.cc
+++ chrome/browser/ui/views/keyboard_overlay_dialog_view.cc
@@ -40,7 +40,6 @@ KeyboardOverlayDialogView::KeyboardOverlayDialogView(
     BrowserView* parent_view)
     : HtmlDialogView(profile, parent_view->browser(), delegate),
       parent_view_(parent_view) {
-  RegisterDialogAccelerators();
 }
 
 KeyboardOverlayDialogView::~KeyboardOverlayDialogView() {
