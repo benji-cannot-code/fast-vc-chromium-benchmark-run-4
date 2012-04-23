8442b84284f298162d66a3394184147ef13fdc44
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 16:52:40 +0000

    explicit ContextMenuParams::ContextMenuParams
    I came across this style violation while hacking at http://codereview.chromium.org/9875026/ , this CL fixes it.
    
    BUG=None
    
    
    Review URL: http://codereview.chromium.org/10155010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133463 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/context_menu_params.h
+++ content/public/common/context_menu_params.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,7 +40,7 @@ struct CONTENT_EXPORT CustomContextMenuContext {
 //              could be used for more contextual actions.
 struct CONTENT_EXPORT ContextMenuParams {
   ContextMenuParams();
-  ContextMenuParams(const WebKit::WebContextMenuData& data);
+  explicit ContextMenuParams(const WebKit::WebContextMenuData& data);
   ~ContextMenuParams();
 
   // This is the type of Context Node that the context menu was invoked on.
