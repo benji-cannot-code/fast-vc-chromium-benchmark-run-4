371f1c44ac98a7f0fbfaa1082216a08bb08b0f79
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 22:56:07 +0000

    Noone should use base class, so it's safe to remove virtual destructors for point/size/rect
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10203004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133795 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/point.h
+++ ui/gfx/point.h
@@ -35,7 +35,7 @@ class UI_EXPORT Point : public PointBase<Point, int> {
   explicit Point(const CGPoint& point);
 #endif
 
-  virtual ~Point() {}
+  ~Point() {}
 
 #if defined(OS_WIN)
   POINT ToPOINT() const;
--- ui/gfx/size.h
+++ ui/gfx/size.h
@@ -30,7 +30,7 @@ class UI_EXPORT Size : public SizeBase<Size, int> {
   explicit Size(const CGSize& s);
 #endif
 
-  virtual ~Size();
+  ~Size();
 
 #if defined(OS_MACOSX)
   Size& operator=(const CGSize& s);
