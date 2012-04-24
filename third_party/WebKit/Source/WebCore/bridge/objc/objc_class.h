3e3e2baed8c1b34932f6b8602fb423d1668d1a99
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 07:48:20 +0000

    Implement the field cache of ObjcClass in WebKit types https://bugs.webkit.org/show_bug.cgi?id=84667
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-04-24
    Reviewed by Geoffrey Garen.
    
    Every time we get a field through the Objective-C bridge, we waste some time converting
    the incoming string to an CFString.
    
    This patch implement the field cache based on WTF types so that we can return directly
    in case of a positive match.
    
    When we do not have a match on the Identifier, we allocate the memory and find the valid field
    as before.
    
    * bridge/objc/objc_class.h:
    (ObjcClass):
    * bridge/objc/objc_class.mm:
    (JSC::Bindings::deleteMethod):
    (Bindings):
    (JSC::Bindings::ObjcClass::ObjcClass):
    (JSC::Bindings::ObjcClass::fieldNamed):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
+++ third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2003 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2003, 2012 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -51,7 +51,7 @@ public:
 private:
     ClassStructPtr _isa;
     RetainPtr<CFMutableDictionaryRef> _methods;
-    RetainPtr<CFMutableDictionaryRef> _fields;
+    mutable HashMap<RefPtr<StringImpl>, OwnPtr<Field> > m_fieldCache;
 };
 
 } // namespace Bindings
