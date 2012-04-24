d7e85c8d74fadf8b0b2b79046e7c3d135531aa4b
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 17:17:45 +0000

    [CSS Filters] Move m_filter and related fields from RenderLayer to a different structure and only allocate it when needed https://bugs.webkit.org/show_bug.cgi?id=84460
    Reviewed by Dean Jackson.
    
    The new RenderLayerFilterInfo is meant to be allocated only when needed by filters. The link to the RenderLayerFilterInfo
    is maintained using a HashMap and a single boolean in the RenderLayer class.
    
    No new tests, just extracted a couple of fields inside the RenderLayer to a separate class.
    
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * rendering/FilterEffectRenderer.cpp:
    (WebCore::FilterEffectRendererHelper::prepareFilterEffect):
    (WebCore::FilterEffectRendererHelper::beginFilterEffect):
    (WebCore::FilterEffectRendererHelper::applyFilterEffect):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::RenderLayer):
    (WebCore::RenderLayer::~RenderLayer):
    (WebCore::RenderLayer::paintsWithFilters):
    (WebCore::RenderLayer::requiresFullLayerImageForFilters):
    (WebCore::RenderLayer::setFilterBackendNeedsRepaintingInRect):
    (WebCore::RenderLayer::paintLayerContents):
    (WebCore::RenderLayer::updateOrRemoveFilterEffect):
    * rendering/RenderLayer.h:
    (WebCore::RenderLayer::filterRenderer):
    (RenderLayer):
    (WebCore::RenderLayer::filterInfo):
    (WebCore::RenderLayer::ensureFilterInfo):
    (WebCore::RenderLayer::removeFilterInfoIfNeeded):
    (WebCore::RenderLayer::hasFilterInfo):
    (WebCore::RenderLayer::setHasFilterInfo):
    * rendering/RenderLayerFilterInfo.cpp: Added.
    (WebCore):
    (WebCore::RenderLayerFilterInfo::filterInfoForRenderLayer):
    (WebCore::RenderLayerFilterInfo::createFilterInfoForRenderLayerIfNeeded):
    (WebCore::RenderLayerFilterInfo::removeFilterInfoForRenderLayer):
    (WebCore::RenderLayerFilterInfo::RenderLayerFilterInfo):
    (WebCore::RenderLayerFilterInfo::~RenderLayerFilterInfo):
    (WebCore::RenderLayerFilterInfo::setRenderer):
    * rendering/RenderLayerFilterInfo.h: Added.
    (WebCore):
    (RenderLayerFilterInfo):
    (WebCore::RenderLayerFilterInfo::dirtySourceRect):
    (WebCore::RenderLayerFilterInfo::addDirtySourceRect):
    (WebCore::RenderLayerFilterInfo::resetDirtySourceRect):
    (WebCore::RenderLayerFilterInfo::renderer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Target.pri
+++ third_party/WebKit/Source/WebCore/Target.pri
@@ -1260,6 +1260,7 @@ SOURCES += \
     rendering/RenderLayer.cpp \
     rendering/RenderLayerBacking.cpp \
     rendering/RenderLayerCompositor.cpp \
+    rendering/RenderLayerFilterInfo.cpp \
     rendering/RenderLineBoxList.cpp \
     rendering/RenderListBox.cpp \
     rendering/RenderListItem.cpp \
--- third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
@@ -379,11 +379,11 @@ LayoutRect FilterEffectRenderer::computeSourceImageRectForDirtyRect(const Layout
 
 bool FilterEffectRendererHelper::prepareFilterEffect(RenderLayer* renderLayer, const LayoutRect& filterBoxRect, const LayoutRect& dirtyRect, const LayoutRect& layerRepaintRect)
 {
-    ASSERT(m_haveFilterEffect && renderLayer->filter());
+    ASSERT(m_haveFilterEffect && renderLayer->filterRenderer());
     m_renderLayer = renderLayer;
     m_repaintRect = dirtyRect;
 
-    FilterEffectRenderer* filter = renderLayer->filter();
+    FilterEffectRenderer* filter = renderLayer->filterRenderer();
     LayoutRect filterSourceRect = filter->computeSourceImageRectForDirtyRect(filterBoxRect, dirtyRect);
     m_paintOffset = filterSourceRect.location();
 
@@ -409,7 +409,7 @@ GraphicsContext* FilterEffectRendererHelper::beginFilterEffect(GraphicsContext* 
 {
     ASSERT(m_renderLayer);
     
-    FilterEffectRenderer* filter = m_renderLayer->filter();
+    FilterEffectRenderer* filter = m_renderLayer->filterRenderer();
     filter->allocateBackingStoreIfNeeded();
     // Paint into the context that represents the SourceGraphic of the filter.
     GraphicsContext* sourceGraphicsContext = filter->inputContext();
@@ -432,8 +432,8 @@ GraphicsContext* FilterEffectRendererHelper::beginFilterEffect(GraphicsContext* 
 
 GraphicsContext* FilterEffectRendererHelper::applyFilterEffect()
 {
-    ASSERT(m_haveFilterEffect && m_renderLayer->filter());
-    FilterEffectRenderer* filter = m_renderLayer->filter();
+    ASSERT(m_haveFilterEffect && m_renderLayer->filterRenderer());
+    FilterEffectRenderer* filter = m_renderLayer->filterRenderer();
     filter->inputContext()->restore();
 
     filter->apply();
