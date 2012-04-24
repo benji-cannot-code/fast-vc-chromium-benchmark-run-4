2e58fabc7e111b373f824bc5cdd50ca4b07f7978
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 15:29:22 +0000

    Fix includes in StrongInlines.h and ScriptValue.h https://bugs.webkit.org/show_bug.cgi?id=84659
    Reviewed by Geoffrey Garen.
    
    Source/JavaScriptCore:
    
    * heap/StrongInlines.h: Include JSGlobalData.h, since JSGlobalData's
    definiition is required here.
    
    Source/WebCore:
    
    This change was prompted by an attempt to use ScriptValue.h from a
    WebCore header file and running into trouble with the (as it turns out
    unnecessary) include of JSDOMBinding.h.
    
    * bindings/js/ScriptValue.cpp: Add include of JSDOMBinding.h, now that
    it's not included by the header.
    * bindings/js/ScriptValue.h: Remove unnecessary include of JSDOMBinding.h.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/StrongInlines.h
+++ third_party/WebKit/Source/JavaScriptCore/heap/StrongInlines.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef StrongInlines_h
 #define StrongInlines_h
 
+#include "JSGlobalData.h"
+
 namespace JSC {
 
 template <typename T>
