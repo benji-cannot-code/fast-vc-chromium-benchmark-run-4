3f98b8e2a280e9370c066dd1dc4a147a58d612ca
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 23:28:42 +0000

    ash: Clean up extension icon loading special cases.
    Those special cases are handled in ImageLoadingTracker after r133657.
    
    BUG=none.
    TEST=No change.
    
    
    Review URL: http://codereview.chromium.org/10212004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/app_list/extension_app_item.h
+++ chrome/browser/ui/views/ash/app_list/extension_app_item.h
@@ -37,9 +37,6 @@ class ExtensionAppItem : public ChromeAppListItem,
   // Loads extension icon.
   void LoadImage(const Extension* extension);
 
-  // Loads default extension icon.
-  void LoadDefaultImage();
-
   void ShowExtensionOptions();
   void StartExtensionUninstall();
 
