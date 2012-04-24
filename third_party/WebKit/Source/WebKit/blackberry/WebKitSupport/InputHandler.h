557ef0a2c3e59e8c2de3ea6038870e8e083858e3
mifenton@rim.com <mifenton@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 16:05:28 +0000

    [BlackBerry] Add additional details including the bounds of the box for caretPositionChanged. https://bugs.webkit.org/show_bug.cgi?id=84728
    Reviewed by Antonio Gomes.
    
    PR 138988.
    
    Update caretPositionChanged to include the limit of the input field
    for single line inputs.
    
    Reviewed Internally by Gen Mak.
    
    * Api/WebPageClient.h:
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::boundingBoxForInputField):
    (WebKit):
    * WebKitSupport/InputHandler.h:
    (InputHandler):
    * WebKitSupport/SelectionHandler.cpp:
    (BlackBerry::WebKit::SelectionHandler::clippingRectForVisibleContent):
    (BlackBerry::WebKit::SelectionHandler::caretPositionChanged):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115066 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.h
@@ -103,6 +103,8 @@ public:
 
     WTF::String elementText();
 
+    WebCore::IntRect boundingBoxForInputField();
+
     // IMF driven calls.
     bool setBatchEditingActive(bool);
     bool setSelection(int start, int end, bool changeIsPartOfComposition = false);
