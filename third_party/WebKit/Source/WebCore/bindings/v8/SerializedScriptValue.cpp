61bdd71ce2924834389e426d14a788813ab0181c
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 17:01:26 +0000

    [V8] Pass Isolate around in SerializedScriptValue.cpp (Part1) https://bugs.webkit.org/show_bug.cgi?id=84663
    Reviewed by Nate Chapin.
    
    The objective is to pass Isolate to toV8() in SerializedScriptValue.cpp.
    
    Part1: Add an Isolate argument to SerializedScriptValue::create(),
    SerializedScriptValue::deserialize(), etc.
    Part2: Add an m_isolate member to SerializedScriptValue::Writer()
    and SerializedScriptValue::Reader().
    Part3: Pass Isolate to toV8().
    
    This patch fixes Part1.
    
    No tests. No change in behavior.
    
    * bindings/v8/SerializedScriptValue.cpp:
    (WebCore::SerializedScriptValue::create):
    (WebCore::SerializedScriptValue::undefinedValue):
    (WebCore::SerializedScriptValue::booleanValue):
    (WebCore::SerializedScriptValue::numberValue):
    (WebCore::SerializedScriptValue::SerializedScriptValue):
    (WebCore::SerializedScriptValue::deserialize):
    (WebCore::SerializedScriptValue::deserializeForInspector):
    * bindings/v8/SerializedScriptValue.h:
    (SerializedScriptValue):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115077 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/SerializedScriptValue.cpp
@@ -2063,15 +2063,16 @@ private:
 
 PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(v8::Handle<v8::Value> value,
                                                                 MessagePortArray* messagePorts, ArrayBufferArray* arrayBuffers,
-                                                                bool& didThrow)
+                                                                bool& didThrow,
+                                                                v8::Isolate* isolate)
 {
-    return adoptRef(new SerializedScriptValue(value, messagePorts, arrayBuffers, didThrow));
+    return adoptRef(new SerializedScriptValue(value, messagePorts, arrayBuffers, didThrow, isolate));
 }
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(v8::Handle<v8::Value> value)
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(v8::Handle<v8::Value> value, v8::Isolate* isolate)
 {
     bool didThrow;
-    return adoptRef(new SerializedScriptValue(value, 0, 0, didThrow));
+    return adoptRef(new SerializedScriptValue(value, 0, 0, didThrow, isolate));
 }
 
 PassRefPtr<SerializedScriptValue> SerializedScriptValue::createFromWire(const String& data)
@@ -2079,7 +2080,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::createFromWire(const St
     return adoptRef(new SerializedScriptValue(data));
 }
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(const String& data)
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::create(const String& data, v8::Isolate* isolate)
 {
     Writer writer;
     writer.writeWebCoreString(data);
@@ -2106,7 +2107,7 @@ SerializedScriptValue* SerializedScriptValue::nullValue()
     return nullValue.get();
 }
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValue::undefinedValue()
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::undefinedValue(v8::Isolate* isolate)
 {
     Writer writer;
     writer.writeUndefined();
@@ -2114,7 +2115,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::undefinedValue()
     return adoptRef(new SerializedScriptValue(wireData));
 }
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValue::booleanValue(bool value)
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::booleanValue(bool value, v8::Isolate* isolate)
 {
     Writer writer;
     if (value)
@@ -2125,7 +2126,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::booleanValue(bool value
     return adoptRef(new SerializedScriptValue(wireData));
 }
 
-PassRefPtr<SerializedScriptValue> SerializedScriptValue::numberValue(double value)
+PassRefPtr<SerializedScriptValue> SerializedScriptValue::numberValue(double value, v8::Isolate* isolate)
 {
     Writer writer;
     writer.writeNumber(value);
@@ -2190,7 +2191,8 @@ PassOwnPtr<SerializedScriptValue::ArrayBufferContentsArray> SerializedScriptValu
 
 SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value, 
                                              MessagePortArray* messagePorts, ArrayBufferArray* arrayBuffers,
-                                             bool& didThrow)
+                                             bool& didThrow,
+                                             v8::Isolate* isolate)
 {
     didThrow = false;
     Writer writer;
@@ -2241,7 +2243,7 @@ SerializedScriptValue::SerializedScriptValue(const String& wireData)
     m_data = wireData.isolatedCopy();
 }
 
-v8::Handle<v8::Value> SerializedScriptValue::deserialize(MessagePortArray* messagePorts)
+v8::Handle<v8::Value> SerializedScriptValue::deserialize(MessagePortArray* messagePorts, v8::Isolate* isolate)
 {
     if (!m_data.impl())
         return v8::Null();
@@ -2252,12 +2254,12 @@ v8::Handle<v8::Value> SerializedScriptValue::deserialize(MessagePortArray* messa
 }
 
 #if ENABLE(INSPECTOR)
-ScriptValue SerializedScriptValue::deserializeForInspector(ScriptState* scriptState)
+ScriptValue SerializedScriptValue::deserializeForInspector(ScriptState* scriptState, v8::Isolate* isolate)
 {
     v8::HandleScope handleScope;
     v8::Context::Scope contextScope(scriptState->context());
 
-    return ScriptValue(deserialize());
+    return ScriptValue(deserialize(0, isolate));
 }
 #endif
 
