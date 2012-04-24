7dc23efdf74437f67084777f8f21761016bafbc5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 22:10:29 +0000

    ash: Make the buttons in the tray-popup headers accessible from keyboard.
    BUG=124864
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10218002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133780 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_views.cc
+++ ash/system/tray/tray_views.cc
@@ -293,6 +293,7 @@ TrayPopupHeaderButton::TrayPopupHeaderButton(views::ButtonListener* listener,
                     views::ImageButton::ALIGN_MIDDLE);
   set_background(views::Background::CreateSolidBackground(
       ash::kHeaderBackgroundColor));
+  set_focusable(true);
 }
 
 TrayPopupHeaderButton::~TrayPopupHeaderButton() {}
@@ -304,6 +305,13 @@ gfx::Size TrayPopupHeaderButton::GetPreferredSize() {
 void TrayPopupHeaderButton::OnPaintBorder(gfx::Canvas* canvas) {
   // Left border.
   canvas->FillRect(gfx::Rect(0, 0, 1, height()), ash::kBorderDarkColor);
+}
+
+void TrayPopupHeaderButton::OnPaintFocusBorder(gfx::Canvas* canvas) {
+  if (HasFocus() && (focusable() || IsAccessibilityFocusable())) {
+    canvas->DrawRect(gfx::Rect(2, 1, width() - 4, height() - 3),
+                     kFocusBorderColor);
+  }
 }
 
 void TrayPopupHeaderButton::StateChanged() {
