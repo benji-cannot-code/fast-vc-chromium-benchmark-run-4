ef774f65cc6dbf571abdf058cc8277c9d6dc96a6
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-24 00:52:26 +0000

    [V8] Pass Isolate to toV8() (Part6) https://bugs.webkit.org/show_bug.cgi?id=84273
    Reviewed by Nate Chapin.
    
    The objective is to pass Isolate to all toV8()s.
    Since there are a lot of toV8()s, I'll make the change
    step by step. This patch passes Isolate to toV8() in
    several custom bindings.
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8HTMLAllCollectionCustom.cpp:
    (WebCore::getNamedItems):
    (WebCore::getItem):
    (WebCore::V8HTMLAllCollection::namedPropertyGetter):
    (WebCore::V8HTMLAllCollection::itemCallback):
    (WebCore::V8HTMLAllCollection::namedItemCallback):
    (WebCore::V8HTMLAllCollection::callAsFunctionCallback):
    * bindings/v8/custom/V8HTMLCollectionCustom.cpp:
    (WebCore::getNamedItems):
    (WebCore::V8HTMLCollection::namedPropertyGetter):
    (WebCore::V8HTMLCollection::namedItemCallback):
    (WebCore::toV8):
    * bindings/v8/custom/V8SVGPathSegCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8StyleSheetCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8StyleSheetListCustom.cpp:
    (WebCore::V8StyleSheetList::namedPropertyGetter):
    * bindings/v8/custom/V8TrackEventCustom.cpp:
    (WebCore::V8TrackEvent::trackAccessorGetter):
    * bindings/v8/custom/V8WebGLRenderingContextCustom.cpp:
    (WebCore::toV8Object):
    (WebCore::getObjectParameter):
    (WebCore::V8WebGLRenderingContext::getAttachedShadersCallback):
    (WebCore::V8WebGLRenderingContext::getExtensionCallback):
    (WebCore::V8WebGLRenderingContext::getFramebufferAttachmentParameterCallback):
    (WebCore::V8WebGLRenderingContext::getParameterCallback):
    (WebCore::V8WebGLRenderingContext::getProgramParameterCallback):
    (WebCore::V8WebGLRenderingContext::getShaderParameterCallback):
    (WebCore::V8WebGLRenderingContext::getUniformCallback):
    * bindings/v8/custom/V8XMLHttpRequestCustom.cpp:
    (WebCore::V8XMLHttpRequest::responseAccessorGetter):
    * bindings/v8/custom/V8XSLTProcessorCustom.cpp:
    (WebCore::V8XSLTProcessor::transformToFragmentCallback):
    (WebCore::V8XSLTProcessor::transformToDocumentCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@114972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLCollectionCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLCollectionCustom.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static v8::Handle<v8::Value> getNamedItems(HTMLCollection* collection, AtomicString name)
+static v8::Handle<v8::Value> getNamedItems(HTMLCollection* collection, AtomicString name, v8::Isolate* isolate)
 {
     Vector<RefPtr<Node> > namedItems;
     collection->namedItems(name, namedItems);
@@ -51,9 +51,9 @@ static v8::Handle<v8::Value> getNamedItems(HTMLCollection* collection, AtomicStr
         return v8::Handle<v8::Value>();
 
     if (namedItems.size() == 1)
-        return toV8(namedItems.at(0).release());
+        return toV8(namedItems.at(0).release(), isolate);
 
-    return toV8(V8NamedNodesCollection::create(namedItems));
+    return toV8(V8NamedNodesCollection::create(namedItems), isolate);
 }
 
 v8::Handle<v8::Value> V8HTMLCollection::namedPropertyGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
@@ -66,14 +66,14 @@ v8::Handle<v8::Value> V8HTMLCollection::namedPropertyGetter(v8::Local<v8::String
         return notHandledByInterceptor();
 
     HTMLCollection* imp = V8HTMLCollection::toNative(info.Holder());
-    return getNamedItems(imp, v8StringToAtomicWebCoreString(name));
+    return getNamedItems(imp, v8StringToAtomicWebCoreString(name), info.GetIsolate());
 }
 
 v8::Handle<v8::Value> V8HTMLCollection::namedItemCallback(const v8::Arguments& args)
 {
     INC_STATS("DOM.HTMLCollection.namedItem()");
     HTMLCollection* imp = V8HTMLCollection::toNative(args.Holder());
-    v8::Handle<v8::Value> result = getNamedItems(imp, toWebCoreString(args[0]));
+    v8::Handle<v8::Value> result = getNamedItems(imp, toWebCoreString(args[0]), args.GetIsolate());
 
     if (result.IsEmpty())
         return v8::Undefined();
