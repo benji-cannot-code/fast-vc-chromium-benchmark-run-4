0fd12861d49694ae0acdc6c1ebeb448dffb6cbd6
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 15:47:15 +0000

    ash: Some code-cleanup in the system-tray code.
    Move the remaining status-area code into system-tray.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10140017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133691 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/focus_cycler_unittest.cc
+++ ash/focus_cycler_unittest.cc
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/launcher/launcher.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
-#include "ash/system/tray/system_tray.h"
-#include "ash/system/tray/system_tray_widget_delegate.h"
+#include "ash/status_area/status_area_view.h"
 #include "ash/wm/window_util.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/shell_factory.h"
@@ -30,12 +29,6 @@ namespace {
 internal::StatusAreaView* GetStatusAreaView(views::Widget* widget) {
   return static_cast<internal::StatusAreaView*>(
       widget->GetContentsView());
-}
-
-SystemTray* CreateSystemTray() {
-  SystemTray* tray = new SystemTray;
-  tray->CreateWidget();
-  return tray;
 }
 
 }  // namespace
@@ -62,10 +55,10 @@ TEST_F(FocusCyclerTest, CycleFocusForward) {
   scoped_ptr<FocusCycler> focus_cycler(new FocusCycler());
 
   // Add the Status area
-  scoped_ptr<SystemTray> tray(CreateSystemTray());
-  ASSERT_TRUE(tray->widget());
-  focus_cycler->AddWidget(tray->widget());
-  GetStatusAreaView(tray->widget())->SetFocusCyclerForTesting(
+  views::Widget* status_widget = internal::CreateStatusArea(NULL);
+  ASSERT_TRUE(status_widget);
+  focus_cycler->AddWidget(status_widget);
+  GetStatusAreaView(status_widget)->SetFocusCyclerForTesting(
       focus_cycler.get());
 
   // Add the launcher
@@ -85,7 +78,7 @@ TEST_F(FocusCyclerTest, CycleFocusForward) {
 
   // Cycle focus to the status area
   focus_cycler->RotateFocus(FocusCycler::FORWARD);
-  EXPECT_TRUE(tray->widget()->IsActive());
+  EXPECT_TRUE(status_widget->IsActive());
 
   // Cycle focus to the launcher
   focus_cycler->RotateFocus(FocusCycler::FORWARD);
@@ -100,10 +93,10 @@ TEST_F(FocusCyclerTest, CycleFocusBackward) {
   scoped_ptr<FocusCycler> focus_cycler(new FocusCycler());
 
   // Add the Status area
-  scoped_ptr<SystemTray> tray(CreateSystemTray());
-  ASSERT_TRUE(tray->widget());
-  focus_cycler->AddWidget(tray->widget());
-  GetStatusAreaView(tray->widget())->SetFocusCyclerForTesting(
+  views::Widget* status_widget = internal::CreateStatusArea(NULL);
+  ASSERT_TRUE(status_widget);
+  focus_cycler->AddWidget(status_widget);
+  GetStatusAreaView(status_widget)->SetFocusCyclerForTesting(
       focus_cycler.get());
 
   // Add the launcher
@@ -127,7 +120,7 @@ TEST_F(FocusCyclerTest, CycleFocusBackward) {
 
   // Cycle focus to the status area
   focus_cycler->RotateFocus(FocusCycler::BACKWARD);
-  EXPECT_TRUE(tray->widget()->IsActive());
+  EXPECT_TRUE(status_widget->IsActive());
 
   // Cycle focus to the browser
   focus_cycler->RotateFocus(FocusCycler::BACKWARD);
@@ -169,10 +162,10 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusForwardInvisible) {
   scoped_ptr<FocusCycler> focus_cycler(new FocusCycler());
 
   // Add the Status area
-  scoped_ptr<SystemTray> tray(CreateSystemTray());
-  ASSERT_TRUE(tray->widget());
-  focus_cycler->AddWidget(tray->widget());
-  GetStatusAreaView(tray->widget())->SetFocusCyclerForTesting(
+  views::Widget* status_widget = internal::CreateStatusArea(NULL);
+  ASSERT_TRUE(status_widget);
+  focus_cycler->AddWidget(status_widget);
+  GetStatusAreaView(status_widget)->SetFocusCyclerForTesting(
       focus_cycler.get());
 
   // Add the launcher
@@ -192,7 +185,7 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusForwardInvisible) {
 
   // Cycle focus to the status area
   focus_cycler->RotateFocus(FocusCycler::FORWARD);
-  EXPECT_TRUE(tray->widget()->IsActive());
+  EXPECT_TRUE(status_widget->IsActive());
 
   // Cycle focus to the browser
   focus_cycler->RotateFocus(FocusCycler::FORWARD);
@@ -203,10 +196,10 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusBackwardInvisible) {
   scoped_ptr<FocusCycler> focus_cycler(new FocusCycler());
 
   // Add the Status area
-  scoped_ptr<SystemTray> tray(CreateSystemTray());
-  ASSERT_TRUE(tray->widget());
-  focus_cycler->AddWidget(tray->widget());
-  GetStatusAreaView(tray->widget())->SetFocusCyclerForTesting(
+  views::Widget* status_widget = internal::CreateStatusArea(NULL);
+  ASSERT_TRUE(status_widget);
+  focus_cycler->AddWidget(status_widget);
+  GetStatusAreaView(status_widget)->SetFocusCyclerForTesting(
       focus_cycler.get());
 
   // Add the launcher
@@ -226,7 +219,7 @@ TEST_F(FocusCyclerLauncherTest, CycleFocusBackwardInvisible) {
 
   // Cycle focus to the status area
   focus_cycler->RotateFocus(FocusCycler::BACKWARD);
-  EXPECT_TRUE(tray->widget()->IsActive());
+  EXPECT_TRUE(status_widget->IsActive());
 
   // Cycle focus to the browser
   focus_cycler->RotateFocus(FocusCycler::BACKWARD);
--- chrome/browser/resources/file_manager/js/file_manager_pyauto.js
+++ chrome/browser/resources/file_manager/js/file_manager_pyauto.js
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * Refer to chrome/test/functional/chromeos_file_browser.py for examples
  * of how this API is used.
- *
- * TODO(olege): Fix style warnings.
  */
 var pyautoAPI = {
   /**
@@ -21,7 +19,7 @@ var pyautoAPI = {
    */
   addItemToSelection: function(name) {
     var entryExists = false;
-    var dm = fileManager.directoryModel_.getFileList();
+    var dm = fileManager.directoryModel_.fileList;
     for (var i = 0; i < dm.length; i++) {
       if (dm.item(i).name == name) {
         fileManager.currentList_.selectionModel.setIndexSelected(i, true);
@@ -42,7 +40,7 @@ var pyautoAPI = {
    */
   listDirectory: function() {
     var list = [];
-    var dm = fileManager.directoryModel_.getFileList();
+    var dm = fileManager.directoryModel_.fileList;
     for (var i = 0; i < dm.length; i++) {
       list.push(dm.item(i).name);
     }
