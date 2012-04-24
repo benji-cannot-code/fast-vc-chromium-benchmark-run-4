da7941b23149546d3e828a70b91b1d1fc097392a
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 03:34:15 +0000

    Rework chromeos::FlimflamDevice/ServiceClient with CallMethodWithErrorCallback
    Modify FlimflamDeviceClient's PIN operation methods and Register to use CallMethodWithErrorCallback
    Modify FlimflamServiceClient::Connect to use CallMethodWithErrorCallback
    
    BUG=chromium-os:16557
    TEST=chromeos_tests
    
    Review URL: https://chromiumcodereview.appspot.com/10170020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133624 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/flimflam_service_client.h
+++ chromeos/dbus/flimflam_service_client.h
@@ -39,6 +39,7 @@ class CHROMEOS_EXPORT FlimflamServiceClient {
   typedef FlimflamClientHelper::PropertyChangedHandler PropertyChangedHandler;
   typedef FlimflamClientHelper::VoidCallback VoidCallback;
   typedef FlimflamClientHelper::DictionaryValueCallback DictionaryValueCallback;
+  typedef FlimflamClientHelper::ErrorCallback ErrorCallback;
 
   virtual ~FlimflamServiceClient();
 
@@ -77,7 +78,8 @@ class CHROMEOS_EXPORT FlimflamServiceClient {
   // Calls Connect method.
   // |callback| is called after the method call succeeds.
   virtual void Connect(const dbus::ObjectPath& service_path,
-                       const VoidCallback& callback) = 0;
+                       const base::Closure& callback,
+                       const ErrorCallback& error_callback) = 0;
 
   // Calls Disconnect method.
   // |callback| is called after the method call succeeds.
