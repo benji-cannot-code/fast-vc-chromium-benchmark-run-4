75eab4b17e3edefab5e552701090fa518e665629
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 02:43:24 +0000

    Metro/HiDPI: Move 1x icons into separate pak file
    Currently all 1x art files are repacked into chrome.pak files.
    
    This is a problem on Windows where we want to choose which pak file to load based on metro and DPI scale.
    
    As a first step this CL does the following:
     - add a new enable_hidpi build flag. This allows us to test HiDPI mode on Windows Chrome.
     - stop packing theme_resources_standard.pak and ui_resources_standard.pak into chrome.pak
     - update the Mac and Windows installer code to package the extra pak files.
    
    Note, I'll be updating the Linux installer script in a separate CL. I'm still looking into the ChromeOS situation.
    
    BUG=114311
    TEST=Ran on Windows, and Mac and Linux.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=132517
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=132760
    
    Review URL: https://chromiumcodereview.appspot.com/10024050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133613 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/build/win/create_installer_archive.py
+++ chrome/tools/build/win/create_installer_archive.py
@@ -90,7 +90,8 @@ def CompressUsingLZMA(build_dir, compressed_file, input_file):
   RunSystemCommand(cmd)
 
 
-def CopyAllFilesToStagingDir(config, distribution, staging_dir, build_dir):
+def CopyAllFilesToStagingDir(config, distribution, staging_dir, build_dir,
+                             enable_hidpi):
   """Copies the files required for installer archive.
   Copies all common files required for various distributions of Chromium and
   also files for the specific Chromium build specified by distribution.
@@ -101,6 +102,8 @@ def CopyAllFilesToStagingDir(config, distribution, staging_dir, build_dir):
       distribution = distribution[1:]
     CopySectionFilesToStagingDir(config, distribution.upper(),
                                  staging_dir, build_dir)
+  if enable_hidpi == '1':
+    CopySectionFilesToStagingDir(config, 'HIDPI', staging_dir, build_dir)
 
 
 def CopySectionFilesToStagingDir(config, section, staging_dir, build_dir):
@@ -325,11 +328,13 @@ def main(options):
   # building the optimized mini_installer.
   if options.build_dir != options.output_dir:
     CopyAllFilesToStagingDir(config, options.distribution,
-                             staging_dir, options.output_dir)
+                             staging_dir, options.output_dir,
+                             options.enable_hidpi)
 
   # Now copy the remainder of the files from the build dir.
   CopyAllFilesToStagingDir(config, options.distribution,
-                           staging_dir, options.build_dir)
+                           staging_dir, options.build_dir,
+                           options.enable_hidpi)
 
   version_numbers = current_version.split('.')
   current_build_number = version_numbers[2] + '.' + version_numbers[3]
@@ -381,6 +386,8 @@ def _ParseOptions():
            '{BSDIFF|COURGETTE}.')
   parser.add_option('-n', '--output_name', default='chrome',
       help='Name used to prefix names of generated archives.')
+  parser.add_option('--enable_hidpi', default='0',
+      help='Whether to include HiDPI resource files.')
 
   options, args = parser.parse_args()
   if not options.build_dir:
