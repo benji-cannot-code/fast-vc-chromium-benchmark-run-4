02e9d741056a6750461c02e366a6fcde16a9f13e
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 11:33:43 +0000

    [cros] Add support for locale specific configuration in mobile config.
    BUG=chrome-os-partner:8092
    TEST=MobileConfigTest
    
    
    Review URL: http://codereview.chromium.org/10141006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133667 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/mobile_config_unittest.cc
+++ chrome/browser/chromeos/mobile_config_unittest.cc
@@ -47,6 +47,11 @@ const char kGoodMobileConfig[] =
     "      ],\n"
     "    },"
     "  },"
+    " \"initial_locales\" : {\n"
+    "  \"en-US\" : {\n"
+    "    \"setup_url\" : \"accounts.carrier.com\",\n"
+    "  },"
+    " },"
     "}";
 
 const char kOldDealMobileConfig[] =
@@ -107,7 +112,12 @@ const char kLocalMobileConfig[] =
     "        },\n"
     "      ],\n"
     "    },"
-     "  },"
+    "  },"
+    " \"initial_locales\" : {\n"
+    "  \"en-US\" : {\n"
+    "    \"setup_url\" : \"accounts.carrier.com/localized/\",\n"
+    "  },"
+    " },"
     "}";
 
 }  // anonymous namespace
@@ -140,6 +150,17 @@ TEST(MobileConfigTest, Basic) {
   base::Time reference_time;
   base::Time::FromString("31/12/12 0:00", &reference_time);
   EXPECT_EQ(reference_time, deal->expire_date());
+
+  const MobileConfig::LocaleConfig* locale_config;
+  locale_config = config.GetLocaleConfig();
+  EXPECT_TRUE(locale_config != NULL);
+  EXPECT_EQ("accounts.carrier.com", locale_config->setup_url());
+
+  // Check same manifest but with another initial locale.
+  MobileConfig config_uk(kGoodMobileConfig, "en-GB");
+  EXPECT_TRUE(config_uk.IsReady());
+  locale_config = config_uk.GetLocaleConfig();
+  EXPECT_TRUE(locale_config == NULL);
 }
 
 TEST(MobileConfigTest, OldDeal) {
@@ -214,6 +235,16 @@ TEST(MobileConfigTest, LocalConfig) {
   base::Time reference_time;
   base::Time::FromString("31/12/13 0:00", &reference_time);
   EXPECT_EQ(reference_time, deal->expire_date());
+
+  // Now reload same global/local config files but with proper initial locale.
+  MobileConfig config_us(kGoodMobileConfig, "en-US");
+  EXPECT_TRUE(config_us.IsReady());
+  config_us.LoadManifestFromString(kLocalMobileConfig);
+  EXPECT_TRUE(config_us.IsReady());
+  const MobileConfig::LocaleConfig* locale_config;
+  locale_config = config_us.GetLocaleConfig();
+  EXPECT_TRUE(locale_config != NULL);
+  EXPECT_EQ("accounts.carrier.com/localized/", locale_config->setup_url());
 }
 
 }  // namespace chromeos
