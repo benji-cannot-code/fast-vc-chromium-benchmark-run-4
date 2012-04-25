f5e607b8b58f6208273a910d05c4ed4227bdc025
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 00:12:23 +0000

    Patch by Thouraya ANDOLSI <thouraya.andolsi@st.com> on 2012-04-24 Reviewed by Oliver Hunt.
    https://bugs.webkit.org/show_bug.cgi?id=84727.
    Fix build when ENABLE_JIT_CONSTANT_BLINDING enabled.
    
    * assembler/MacroAssemblerSH4.h:
    (JSC::MacroAssemblerSH4::or32):
    (JSC::MacroAssemblerSH4::and32):
    (JSC::MacroAssemblerSH4::lshift32):
    (JSC::MacroAssemblerSH4::xor32):
    (JSC::MacroAssemblerSH4::branchSub32):
    (JSC::MacroAssemblerSH4::urshift32):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115144 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerSH4.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerSH4.h
@@ -157,6 +157,17 @@ public:
         releaseScratch(scr);
     }
 
+    void and32(TrustedImm32 imm, RegisterID src, RegisterID dest)
+    {
+        if (src != dest) {
+            move(imm, dest);
+            and32(src, dest);
+            return;
+        }
+
+        and32(imm, dest);
+    }
+
     void lshift32(RegisterID shiftamount, RegisterID dest)
     {
         if (shiftamount == SH4Registers::r0)
@@ -192,6 +203,14 @@ public:
         m_assembler.loadConstant((imm.m_value & 0x1f) , scr);
         m_assembler.shllRegReg(dest, scr);
         releaseScratch(scr);
+    }
+
+    void lshift32(RegisterID src, TrustedImm32 shiftamount, RegisterID dest)
+    {
+        if (src != dest)
+            move(src, dest);
+
+        lshift32(shiftamount, dest);
     }
 
     void mul32(RegisterID src, RegisterID dest)
@@ -238,6 +257,29 @@ public:
             move(op2, dest);
             or32(op1, dest);
         }
+    }
+
+    
+void or32(TrustedImm32 imm, RegisterID src, RegisterID dest)
+    {
+        if  (src != dest) {
+            move(imm, dest);
+            or32(src, dest);
+            return;
+        }
+
+        or32(imm, dest);
+    }
+
+    void xor32(TrustedImm32 imm, RegisterID src, RegisterID dest)
+    {
+        if  (src != dest) {
+            move(imm, dest);
+            xor32(src, dest);
+            return;
+        }
+
+        xor32(imm, dest);
     }
 
     void rshift32(RegisterID shiftamount, RegisterID dest)
@@ -1745,6 +1787,13 @@ public:
         return branchSub32(cond, scratchReg3, dest);
     }
 
+    Jump branchSub32(ResultCondition cond, RegisterID src1, RegisterID src2, RegisterID dest)
+    {
+        if (src1 != dest)
+            move(src1, dest);
+        return branchSub32(cond, src2, dest);
+    }
+
     Jump branchOr32(ResultCondition cond, RegisterID src, RegisterID dest)
     {
         ASSERT((cond == Signed) || (cond == Zero) || (cond == NonZero));
@@ -1804,6 +1853,14 @@ public:
         m_assembler.loadConstant(-(imm.m_value & 0x1f), scr);
         m_assembler.shaRegReg(dest, scr);
         releaseScratch(scr);
+    }
+
+    void urshift32(RegisterID src, TrustedImm32 shiftamount, RegisterID dest)
+    {
+        if (src != dest)
+           move(src, dest);
+
+        urshift32(shiftamount, dest);
     }
 
     Call call()
