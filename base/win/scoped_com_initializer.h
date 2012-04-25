2d2ccd3b5dd27d5a6702cb2ba6c7651f4aaae295
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 05:56:42 +0000

    win: Fix another small language violation found by clang.
    No functionality change.
    
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10151010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133866 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/scoped_com_initializer.h
+++ base/win/scoped_com_initializer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,7 +35,7 @@ class ScopedCOMInitializer {
     Initialize(COINIT_MULTITHREADED);
   }
 
-  ScopedCOMInitializer::~ScopedCOMInitializer() {
+  ~ScopedCOMInitializer() {
 #ifndef NDEBUG
     // Using the windows API directly to avoid dependency on platform_thread.
     DCHECK_EQ(GetCurrentThreadId(), thread_id_);
