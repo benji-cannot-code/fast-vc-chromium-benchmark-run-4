fbd30470c60e86bcb7c7f359df2333700b6f551b
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 17:12:20 +0000

    Source/WebCore: REGRESSION (r100311): YummySoup app crashes when trying to print https://bugs.webkit.org/show_bug.cgi?id=83918
    Reviewed by Alexey Proskuryakov.
    
    Test: http/tests/xmlhttprequest/cancel-during-failure-crash.html
    
    * loader/ResourceLoader.cpp:
    (WebCore::ResourceLoader::didFail): Set m_calledDidFinishLoad when calling
       didFailToLoad() to prevent it from getting called twice if we cancel
       re-entrantly.
    
    LayoutTests: Test for https://bugs.webkit.org/show_bug.cgi?id=83918.
    
    Reviewed by Alexey Proskuryakov.
    
    * http/tests/cache/cancel-during-failure-crash-expected.txt: Added.
    * http/tests/cache/cancel-during-failure-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/ResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/ResourceLoader.cpp
@@ -61,7 +61,7 @@ ResourceLoader::ResourceLoader(Frame* frame, ResourceLoaderOptions options)
     , m_reachedTerminalState(false)
     , m_calledWillCancel(false)
     , m_cancelled(false)
-    , m_calledDidFinishLoad(false)
+    , m_notifiedLoadComplete(false)
     , m_defersLoading(frame->page()->defersLoading())
     , m_options(options)
 {
@@ -305,9 +305,9 @@ void ResourceLoader::didFinishLoadingOnePart(double finishTime)
         return;
     ASSERT(!m_reachedTerminalState);
 
-    if (m_calledDidFinishLoad)
+    if (m_notifiedLoadComplete)
         return;
-    m_calledDidFinishLoad = true;
+    m_notifiedLoadComplete = true;
     if (m_options.sendLoadCallbacks == SendCallbacks)
         frameLoader()->notifier()->didFinishLoad(this, finishTime);
 }
@@ -325,8 +325,11 @@ void ResourceLoader::didFail(const ResourceError& error)
     if (FormData* data = m_request.httpBody())
         data->removeGeneratedFilesIfNeeded();
 
-    if (m_options.sendLoadCallbacks == SendCallbacks && !m_calledDidFinishLoad)
-        frameLoader()->notifier()->didFailToLoad(this, error);
+    if (!m_notifiedLoadComplete) {
+        m_notifiedLoadComplete = true;
+        if (m_options.sendLoadCallbacks == SendCallbacks)
+            frameLoader()->notifier()->didFailToLoad(this, error);
+    }
 
     releaseResources();
 }
@@ -373,7 +376,7 @@ void ResourceLoader::cancel(const ResourceError& error)
             m_handle = 0;
         }
 
-        if (m_options.sendLoadCallbacks == SendCallbacks && m_identifier && !m_calledDidFinishLoad)
+        if (m_options.sendLoadCallbacks == SendCallbacks && m_identifier && !m_notifiedLoadComplete)
             frameLoader()->notifier()->didFailToLoad(this, nonNullError);
     }
 
