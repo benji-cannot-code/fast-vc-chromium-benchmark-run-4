09b9367bed8bf90179a38b0bf0dd54971412aa3e
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 21:59:20 +0000

    IndexedDB pyauto tests rely on restarting the browser and navigating to a new tab. On CrOS the restart was causing the previous session's tabs to be restored, which confused the test.
    Set a preference to start sessions on the new tab page, , and refactor to avoid polling for results so that failures are detected immediately and with useful error messages.
    
    BUG=123045
    TEST=python chrome/test/functional/indexeddb.py
    R=nirnimesh@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10216002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/indexeddb/bug_90635.js
+++ chrome/test/data/indexeddb/bug_90635.js
@@ -1,19 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-window.indexedDB = window.indexedDB || window.webkitIndexedDB;
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
-
-function unexpectedErrorCallback()
-{
-  document.title = 'fail - unexpected error callback';
-}
-function unexpectedAbortCallback()
-{
-  document.title = 'fail - unexpected abort callback';
-}
 
 function test()
 {
@@ -22,7 +10,7 @@ function test()
   } else if (document.location.hash === '#part2') {
     testPart2();
   } else {
-    document.title = 'fail - no hash';
+    result('fail - unexpected hash');
   }
 }
 
@@ -73,8 +61,8 @@ function test_store(db, msg) {
   if (store1.keyPath !== null ||
       store2.keyPath !== '' ||
       store3.keyPath !== 'some_path') {
-    document.title = 'fail - ' + msg;
+    result('fail - ' + msg);
   } else {
-    document.title = 'pass - ' + msg;
+    result('pass - ' + msg);
   }
 }
---
+++ chrome/test/data/indexeddb/shared.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.indexedDB = window.indexedDB || window.webkitIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+
+function result(message) {
+  var div = document.createElement('div');
+  div.id = "result";
+  div.innerText = message;
+  document.body.appendChild(div);
+}
+
+function unexpectedErrorCallback()
+{
+  result('fail - unexpected error callback');
+}
+
+function unexpectedAbortCallback()
+{
+  result('fail - unexpected abort callback');
+}
+
+function unexpectedCompleteCallback()
+{
+  result('fail - unexpected complete callback');
+}
