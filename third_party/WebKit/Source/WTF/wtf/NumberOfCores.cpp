c5f04baba8e1ab522532e17a4434d45d1017e260
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 23:02:18 +0000

    Include <sys/param.h>, needed for sysctl() on OpenBSD/NetBSD https://bugs.webkit.org/show_bug.cgi?id=82585
    Patch by Landry Breuil <landry@openbsd.org> on 2012-04-25
    Reviewed by Zoltan Herczeg.
    
    * wtf/NumberOfCores.cpp: include <sys/param.h> on the BSDs
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/NumberOfCores.cpp
+++ third_party/WebKit/Source/WTF/wtf/NumberOfCores.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NumberOfCores.h"
 
 #if OS(DARWIN) || OS(OPENBSD) || OS(NETBSD) || OS(FREEBSD)
+#include <sys/param.h>
 // sys/types.h must come before sys/sysctl.h because the latter uses
 // data types defined in the former. See sysctl(3) and style(9).
 #include <sys/types.h>
