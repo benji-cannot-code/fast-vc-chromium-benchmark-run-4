7351916ff37e60b9d12064a609f926f6ac194d6c
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 13:29:00 +0000

    [Qt] Make the web view's url property follow the active url
    https://bugs.webkit.org/show_bug.cgi?id=77554
    
    The url property of the webview now reflects the 'active' url of the
    page, which maps to either the currently loading url, in the case of
    an ongoing load, or the result of a load, even when the load failed.
    
    In practice this means that setting the url though QML, or navigating
    to a new url in the page by e.g clicking, will both instantly change
    the url-property of the webview to the target url. This differs from
    earlier behavior, where we would update the url when the load
    committed.
    
    An optional argument is added to loadHtml(), to allow setting
    the unreachable url when providing replacement content for failed
    loads.
    
    A slight change in the activeUrl() implementation is also done,
    where we now favour the url of an pending API request, even when
    we don't have a mainframe yet.
    
    Finally, the location bar in the minibrowser is updated to behave
    a bit more like normal browsers in terms of when the url will change
    and how active focus is handled.
    
    Reviewed by Simon Hausmann.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageLoadClient.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtWebPageLoadClient.h
@@ -41,6 +41,7 @@ public:
 
 private:
     void didStartProvisionalLoadForFrame(const QUrl&);
+    void didReceiveServerRedirectForProvisionalLoadForFrame(const QUrl&);
     void didCommitLoadForFrame();
     void didSameDocumentNavigationForFrame();
     void didReceiveTitleForFrame();
@@ -48,11 +49,12 @@ private:
     void didChangeBackForwardList();
 
     void dispatchLoadSucceeded();
-    void dispatchLoadFailed(WKErrorRef);
+    void dispatchLoadFailed(WKFrameRef, WKErrorRef);
     void setLoadProgress(int);
 
     // WKPageLoadClient callbacks.
     static void didStartProvisionalLoadForFrame(WKPageRef, WKFrameRef, WKTypeRef userData, const void* clientInfo);
+    static void didReceiveServerRedirectForProvisionalLoadForFrame(WKPageRef, WKFrameRef, WKTypeRef userData, const void* clientInfo);
     static void didFailProvisionalLoadWithErrorForFrame(WKPageRef, WKFrameRef, WKErrorRef, WKTypeRef userData, const void* clientInfo);
     static void didCommitLoadForFrame(WKPageRef, WKFrameRef, WKTypeRef userData, const void* clientInfo);
     static void didFinishLoadForFrame(WKPageRef, WKFrameRef, WKTypeRef userData, const void* clientInfo);
