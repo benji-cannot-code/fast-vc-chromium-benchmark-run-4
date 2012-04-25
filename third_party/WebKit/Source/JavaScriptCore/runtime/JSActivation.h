2bd870524a0b474c40fa66cfdbeffb7271ecb1a9
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 16:11:29 +0000

    Closure in try {} with catch captures all locals from the enclosing function https://bugs.webkit.org/show_bug.cgi?id=84804
    Reviewed by Oliver Hunt.
    
    Changed the capturing of local variables from capturing when eval is used,
    within a "with" or within a "catch" to be just when an eval is used.
    Renamed the function returning that we should capture from
    getCapturedVariables() to usesEval(), since that what it noew returns.
    Needed to fix the "with" code to only range check when the activation
    has actually been torn off.  Added m_isTornOff to JSActivation to
    track this.
    
    * parser/Parser.h:
    (JSC::Scope::usesEval):
    (JSC::Scope::getCapturedVariables):
    * runtime/JSActivation.cpp:
    (JSC::JSActivation::JSActivation):
    (JSC::JSActivation::symbolTableGet):
    (JSC::JSActivation::symbolTablePut):
    * runtime/JSActivation.h:
    (JSActivation):
    (JSC::JSActivation::tearOff):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSActivation.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSActivation.h
@@ -93,7 +93,8 @@ namespace JSC {
         NEVER_INLINE PropertySlot::GetValueFunc getArgumentsGetter();
 
         int m_numCapturedArgs;
-        int m_numCapturedVars : 31;
+        int m_numCapturedVars : 30;
+        bool m_isTornOff : 1;
         bool m_requiresDynamicChecks : 1;
         int m_argumentsRegister;
     };
@@ -134,6 +135,7 @@ namespace JSC {
             registers[i].set(globalData, this, m_registers[i].get());
 
         setRegisters(registers, registerArray.release());
+        m_isTornOff = true;
     }
 
 } // namespace JSC
