83f0ef93d9bc98e7eb4f54e65d210eb91802bdb7
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 01:25:40 +0000

    Set svn:executable bit on scripts that are executable.
    Add a PRESUBMIT.py to run checkperms.py when it is modified and to run pylint on it.
    
    R=thestig@chromium.org
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10134019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133835 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/memory_watcher/scripts/memtrace.pl
+++ tools/memory_watcher/scripts/memtrace.pl
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/perl
-
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 #
 # Blame callstacks for each memory allocation.
 # Similar to memprof.pl, will also try to filter out unuseful stacks.
