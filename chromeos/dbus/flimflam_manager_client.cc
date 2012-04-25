159973682dedf5bc0bad56aab8fbc5733361b3fa
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 13:57:53 +0000

    Reimplement CrosGetWifiAccessPoints without Libcros
    BUG=chromium-os:16557
    TEST=unit_tests --gtest_filter="CrosNetworkFunctionsTest.CrosGetWifiAccessPoints"
    
    
    Review URL: http://codereview.chromium.org/10202009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133902 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/flimflam_manager_client.cc
+++ chromeos/dbus/flimflam_manager_client.cc
@@ -74,6 +74,12 @@ class FlimflamManagerClientImpl : public FlimflamManagerClient {
     helper_.CallDictionaryValueMethod(&method_call, callback);
   }
 
+  virtual base::DictionaryValue* CallGetPropertiesAndBlock() OVERRIDE {
+    dbus::MethodCall method_call(flimflam::kFlimflamManagerInterface,
+                                 flimflam::kGetPropertiesFunction);
+    return helper_.CallDictionaryValueMethodAndBlock(&method_call);
+  }
+
   virtual void SetProperty(const std::string& name,
                            const base::Value& value,
                            const VoidCallback& callback) OVERRIDE {
@@ -160,6 +166,11 @@ class FlimflamManagerClientStubImpl : public FlimflamManagerClient {
             &FlimflamManagerClientStubImpl::PassEmptyDictionaryValue,
             weak_ptr_factory_.GetWeakPtr(),
             callback));
+  }
+
+  // FlimflamManagerClient override.
+  virtual base::DictionaryValue* CallGetPropertiesAndBlock() OVERRIDE {
+    return new base::DictionaryValue;
   }
 
   // FlimflamManagerClient override.
