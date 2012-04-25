a9741c925a25892c3d45094d155d37282eb4992b
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-25 10:31:03 +0000

    Fix leak in ScopedClipboardWriterGlue.
    BUG=123117
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10078001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133890 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/scoped_clipboard_writer_glue.h
+++ webkit/glue/scoped_clipboard_writer_glue.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBKIT_GLUE_SCOPED_CLIPBOARD_WRITER_GLUE_H_
 
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
+#include "base/memory/scoped_ptr.h"
 #include "webkit/glue/clipboard_client.h"
 
 class ScopedClipboardWriterGlue : public ui::ScopedClipboardWriter {
@@ -18,7 +19,7 @@ class ScopedClipboardWriterGlue : public ui::ScopedClipboardWriter {
   void WriteBitmapFromPixels(const void* pixels, const gfx::Size& size);
 
  private:
-  webkit_glue::ClipboardClient::WriteContext* context_;
+  scoped_ptr<webkit_glue::ClipboardClient::WriteContext> context_;
   DISALLOW_COPY_AND_ASSIGN(ScopedClipboardWriterGlue);
 };
 
