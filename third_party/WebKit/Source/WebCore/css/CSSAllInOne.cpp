a9df1df5ad7f182db87e31741a35bdd4acf19e93
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-25 16:28:02 +0000

    Not reviewed, attempt to fix Windows build after r115215.
    * css/CSSAllInOne.cpp: Don't try to include CSSStyleSelector.cpp.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSAllInOne.cpp
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSSelectorList.cpp"
 #include "CSSStyleDeclaration.cpp"
 #include "CSSStyleRule.cpp"
-#include "CSSStyleSelector.cpp"
 #include "CSSStyleSheet.cpp"
 #include "CSSTimingFunctionValue.cpp"
 #include "CSSUnicodeRangeValue.cpp"
