a13de8c5a03ae0b7210ef86ef21a42d8fc71ed80
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-26 02:17:10 +0000

    Limiting the maximum time the Chromoting Host Controller UAC promt can be suspended to 15 minutes.
    Review URL: http://codereview.chromium.org/10221011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134046 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/daemon_controller_win.cc
+++ remoting/host/plugin/daemon_controller_win.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string16.h"
 #include "base/stringize_macros.h"
 #include "base/threading/thread.h"
+#include "base/time.h"
+#include "base/timer.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "base/win/scoped_bstr.h"
@@ -46,6 +48,9 @@ const char16 kDaemonControllerElevationMoniker[] =
 // Name of the Daemon Controller's worker thread.
 const char kDaemonControllerThreadName[] = "Daemon Controller thread";
 
+// The maximum interval between showing UAC prompts.
+const int kUacTimeoutSec = 15 * 60;
+
 // A base::Thread implementation that initializes COM on the new thread.
 class ComThread : public base::Thread {
  public:
@@ -63,7 +68,12 @@ class ComThread : public base::Thread {
   virtual void Init() OVERRIDE;
   virtual void CleanUp() OVERRIDE;
 
+  void ReleaseElevatedController();
+
   ScopedComPtr<IDaemonControl> control_;
+
+  // This timer is used to release |control_| after a timeout.
+  base::OneShotTimer<ComThread> release_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(ComThread);
 };
@@ -145,6 +155,11 @@ HRESULT ComThread::ActivateElevatedController(
     if (FAILED(hr)) {
       return hr;
     }
+
+    // Release |control_| upon expiration of the timeout.
+    release_timer_.Start(FROM_HERE,
+                         base::TimeDelta::FromSeconds(kUacTimeoutSec),
+                         this, &ComThread::ReleaseElevatedController);
   }
 
   *control_out = control_.get();
@@ -162,8 +177,12 @@ void ComThread::Init() {
 }
 
 void ComThread::CleanUp() {
-  control_.Release();
+  ReleaseElevatedController();
   CoUninitialize();
+}
+
+void ComThread::ReleaseElevatedController() {
+  control_.Release();
 }
 
 DaemonControllerWin::DaemonControllerWin()
