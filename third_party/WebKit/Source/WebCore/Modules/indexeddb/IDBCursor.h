22d36ca490dab463c7fb70ff9bb60668ed88f690
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 02:23:34 +0000

    IndexedDB: implement cursor.advance() https://bugs.webkit.org/show_bug.cgi?id=84174
    Patch by Alec Flett <alecflett@chromium.org> on 2012-04-25
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Implement IDBCursor.advance() to spec.
    
    Test: storage/indexeddb/cursor-advance.html
    
    * Modules/indexeddb/IDBBackingStore.h:
    * Modules/indexeddb/IDBCursor.cpp:
    (WebCore::IDBCursor::advance):
    (WebCore):
    * Modules/indexeddb/IDBCursor.h:
    (IDBCursor):
    * Modules/indexeddb/IDBCursor.idl:
    * Modules/indexeddb/IDBCursorBackendImpl.cpp:
    (WebCore::IDBCursorBackendImpl::advance):
    (WebCore):
    (WebCore::IDBCursorBackendImpl::advanceInternal):
    * Modules/indexeddb/IDBCursorBackendImpl.h:
    (IDBCursorBackendImpl):
    * Modules/indexeddb/IDBCursorBackendInterface.h:
    * Modules/indexeddb/IDBLevelDBBackingStore.cpp:
    (WebCore):
    
    Source/WebKit/chromium:
    
    Hook up chromium IPC to IDBCursor.advance().
    
    * src/IDBCursorBackendProxy.cpp:
    (WebKit::IDBCursorBackendProxy::advance):
    (WebKit):
    * src/IDBCursorBackendProxy.h:
    (IDBCursorBackendProxy):
    * src/WebIDBCursorImpl.cpp:
    (WebKit::WebIDBCursorImpl::advance):
    
    LayoutTests:
    
    Tests for cursor.advance().
    
    * storage/indexeddb/cursor-advance-expected.txt: Added.
    * storage/indexeddb/cursor-advance.html: Added.
    * storage/indexeddb/resources/cursor-advance.js: Added.
    (test):
    (openSuccess):
    (createAndPopulateObjectStore):
    (makeAdvanceTest.continueToTest):
    (makeAdvanceTest.request.onerror):
    (makeAdvanceTest):
    (mkCursor):
    (runTest):
    (testAll):
    (testSimple):
    (testContinueThenAdvance):
    (testAdvanceMultiple):
    (testAdvanceToEnd):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBCursor.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBCursor.h
@@ -68,6 +68,7 @@ public:
     IDBAny* source() const;
 
     PassRefPtr<IDBRequest> update(ScriptExecutionContext*, PassRefPtr<SerializedScriptValue>, ExceptionCode&);
+    void advance(unsigned long, ExceptionCode&);
     void continueFunction(PassRefPtr<IDBKey>, ExceptionCode&);
     PassRefPtr<IDBRequest> deleteFunction(ScriptExecutionContext*, ExceptionCode&);
 
