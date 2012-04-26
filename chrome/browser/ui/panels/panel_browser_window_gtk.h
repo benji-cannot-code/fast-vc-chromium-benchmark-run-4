71b4af277625030a9faaa9ec86246e3ec4d61968
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-26 08:17:11 +0000

    GTK Panels - cleanup. Remove logic for hiding overflow panels.
    BUG=125040
    TEST=Existing.
    
    
    Review URL: http://codereview.chromium.org/10154015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_window_gtk.h
+++ chrome/browser/ui/panels/panel_browser_window_gtk.h
@@ -118,7 +118,6 @@ class PanelBrowserWindowGtk : public BrowserWindowGtk,
   void EnsureDragHelperCreated();
 
   void SetBoundsInternal(const gfx::Rect& bounds, bool animate);
-  void ResizeWindow(int width, int height);
 
   void DrawAttentionFrame(cairo_t* cr, GtkWidget* widget,
                           GdkEventExpose* event);
