196a270e46f9c1729ecf134daa814b5e251b411e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 04:50:28 +0000

    [chromium] REGRESSION(112286) Compositor initialization blocks for program compilation / linking https://bugs.webkit.org/show_bug.cgi?id=84822
    Patch by James Robinson <jamesr@chromium.org> on 2012-04-25
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    r112286 introduced a subtle regression in the chromium compositor startup sequence - by querying the texture
    copy program's uniform location at the end of LayerRendererChromium::initialize(), the compositor's thread was
    blocked until the service side compiled _all_ eagerly initialized shaders. The intent of the way the compositor
    programs are created is that a set of commonly-used programs are sent to the service side, but no blocking calls
    are made until after we go through the first paint (with the hope that the service side will complete the
    compilation by then).
    
    Fixed by moving program initialization (which also grabs uniform locations) until the first actual use of the
    copier. It may be worth deferring the program initialization completely if it's not used very often.
    
    Added unit test in LayerRendererChromiumTests to make sure LRC initialization does not make any
    synchronous calls (like getUniformLocation()).
    
    * platform/graphics/chromium/TextureCopier.cpp:
    (WebCore::AcceleratedTextureCopier::AcceleratedTextureCopier):
    (WebCore::AcceleratedTextureCopier::copyTexture):
    
    Source/WebKit/chromium:
    
    Add a test that makes sure we don't make blocking calls during LayerRendererChromium initialization.
    
    * tests/LayerRendererChromiumTest.cpp:
    (ForbidSynchronousCallContext):
    (ForbidSynchronousCallContext::ForbidSynchronousCallContext):
    (ForbidSynchronousCallContext::finish):
    (ForbidSynchronousCallContext::getActiveAttrib):
    (ForbidSynchronousCallContext::getActiveUniform):
    (ForbidSynchronousCallContext::getAttachedShaders):
    (ForbidSynchronousCallContext::getAttribLocation):
    (ForbidSynchronousCallContext::getBooleanv):
    (ForbidSynchronousCallContext::getBufferParameteriv):
    (ForbidSynchronousCallContext::getContextAttributes):
    (ForbidSynchronousCallContext::getError):
    (ForbidSynchronousCallContext::getFloatv):
    (ForbidSynchronousCallContext::getFramebufferAttachmentParameteriv):
    (TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ProgramBinding.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ProgramBinding.h
@@ -44,7 +44,7 @@ public:
     void init(GraphicsContext3D*, const String& vertexShader, const String& fragmentShader);
     void cleanup(GraphicsContext3D*);
 
-    unsigned program() const { return m_program; }
+    unsigned program() const { ASSERT(m_initialized); return m_program; }
     bool initialized() const { return m_initialized; }
 
 protected:
@@ -68,6 +68,7 @@ public:
     {
         ASSERT(context);
         ASSERT(m_program);
+        ASSERT(!m_initialized);
 
         m_vertexShader.init(context, m_program);
         m_fragmentShader.init(context, m_program);
