f18e9ae557d5299072778eba195cc3c6a8525da1
mjs@apple.com <mjs@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 06:27:07 +0000

    Teach LayoutTestHelper to correctly restore the color profile when it's a factory profile https://bugs.webkit.org/show_bug.cgi?id=84931
    Reviewed by Filip Pizlo.
    
    * DumpRenderTree/mac/LayoutTestHelper.m:
    (installLayoutTestColorProfile): Account for the possibility of the default
    profile being a factory profile.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/LayoutTestHelper.m
+++ third_party/WebKit/Tools/DumpRenderTree/mac/LayoutTestHelper.m
@@ -66,6 +66,11 @@ static void installLayoutTestColorProfile()
         if (profileInfo) {
             sUserColorProfileURL = (CFURLRef)CFDictionaryGetValue(profileInfo, CFSTR("1"));
             CFRetain(sUserColorProfileURL);
+        } else {
+            profileInfo = (CFDictionaryRef)CFDictionaryGetValue(deviceInfo, kColorSyncFactoryProfiles);
+            CFDictionaryRef factoryProfile = (CFDictionaryRef)CFDictionaryGetValue(profileInfo, CFSTR("1"));
+            sUserColorProfileURL = (CFURLRef)CFDictionaryGetValue(factoryProfile, kColorSyncDeviceProfileURL);
+            CFRetain(sUserColorProfileURL);
         }
         
         CFRelease(deviceInfo);
