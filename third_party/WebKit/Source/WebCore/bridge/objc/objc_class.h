218c5a32ad9fed58ee785b466edf131163d7555a
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 21:56:23 +0000

    Use String instead of RefPtr<StringImpl> for the cache of ObjcClass https://bugs.webkit.org/show_bug.cgi?id=84932
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-04-26
    Reviewed by Andreas Kling.
    
    The cache with RefPtr<StringImpl*> was added with r115007.
    
    This patch aims at making the code a little easier to read. By using String,
    one would not need to know the Traits for StringImpl.
    
    * bridge/objc/objc_class.h:
    (ObjcClass):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
+++ third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define KJS_BINDINGS_OBJC_CLASS_H
 
 #include "objc_runtime.h"
+#include <wtf/text/WTFString.h>
 
 namespace JSC {
 namespace Bindings {
@@ -51,7 +52,7 @@ public:
 private:
     ClassStructPtr _isa;
     RetainPtr<CFMutableDictionaryRef> _methods;
-    mutable HashMap<RefPtr<StringImpl>, OwnPtr<Field> > m_fieldCache;
+    mutable HashMap<String, OwnPtr<Field> > m_fieldCache;
 };
 
 } // namespace Bindings
