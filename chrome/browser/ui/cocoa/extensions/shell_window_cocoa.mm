cc126a7cd274b72a2f81e7d8de818d314d5521f5
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-26 05:52:17 +0000

    Mac Platform Apps: Fix showing video in shell window
    Playing videos in a shell window wasn't working.
    
    The problem was that we were using a plain NSWindow so the underlay OpenGL surface wasn't visible. Fix was to use a UnderlayOpenGLHostingWindow.
    
    BUG=124236
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10217013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/browser_window_utils.h"
 #include "chrome/browser/ui/cocoa/extensions/extension_view_mac.h"
 #include "chrome/common/extensions/extension.h"
+#import "ui/base/cocoa/underlay_opengl_hosting_window.h"
 
 @implementation ShellWindowController
 
@@ -28,11 +29,11 @@ ShellWindowCocoa::ShellWindowCocoa(ExtensionHost* host)
   NSRect rect = NSMakeRect(0, 0, kDefaultWidth, kDefaultHeight);
   NSUInteger styleMask = NSTitledWindowMask | NSClosableWindowMask |
                          NSMiniaturizableWindowMask | NSResizableWindowMask;
-  scoped_nsobject<NSWindow> window(
-      [[NSWindow alloc] initWithContentRect:rect
-                                  styleMask:styleMask
-                                    backing:NSBackingStoreBuffered
-                                      defer:NO]);
+  scoped_nsobject<NSWindow> window([[UnderlayOpenGLHostingWindow alloc]
+      initWithContentRect:rect
+                styleMask:styleMask
+                  backing:NSBackingStoreBuffered
+                    defer:NO]);
   [window setTitle:base::SysUTF8ToNSString(host->extension()->name())];
 
   NSView* view = host->view()->native_view();
