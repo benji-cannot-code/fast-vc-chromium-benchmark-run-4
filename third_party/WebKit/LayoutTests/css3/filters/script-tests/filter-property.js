512fdec7f08a6ac8ec6dd6879b0655621e155b48
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 00:29:41 +0000

    [Chromium] Layout Test css3/filters/custom/custom-filter-property-computed-style.html is failing https://bugs.webkit.org/show_bug.cgi?id=84067
    Reviewed by Ryosuke Niwa.
    
    All the CSS filters/shaders script tests expected to have a document.styleSheets.item(0) that they could change.
    The script-tests add a <link> that references a CSS file from a different parent folder, so the script
    has no access to the actual rules anymore. Updated all the tests to use createElement("style")
    and get the styleSheet by using el.sheet instead of assuming that there's one style already created.
    
    * css3/filters/script-tests/custom-filter-property-computed-style.js:
    * css3/filters/script-tests/custom-filter-property-parsing-invalid.js:
    (testInvalidFilterRule):
    * css3/filters/script-tests/effect-custom-disabled.js:
    (testInvalidFilterRule):
    * css3/filters/script-tests/filter-property-computed-style.js:
    (testComputedFilterRule):
    * css3/filters/script-tests/filter-property-parsing-invalid.js:
    (testInvalidFilterRule):
    * css3/filters/script-tests/filter-property-parsing.js:
    (testFilterRule):
    * css3/filters/script-tests/filter-property.js:
    * platform/chromium/test_expectations.txt: Removed reference to this bug.
    * platform/mac/css3/filters/custom/custom-filter-property-computed-style-expected.txt: Removed. This is not necessary anymore.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/css3/filters/script-tests/filter-property.js
+++ third_party/WebKit/LayoutTests/css3/filters/script-tests/filter-property.js
@@ -1,10 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test the parsing of the -webkit-filter property.");
 
-// add a -webkit-filter property to the start of the stylesheet
-document.styleSheets.item(0).addRule("body", "-webkit-filter: url(#a) url(#b)", 0);
+var styleElement = document.createElement("style");
+document.head.appendChild(styleElement);
+var stylesheet = styleElement.sheet;
 
-var cssRule = document.styleSheets.item(0).cssRules.item(0);
+// add a -webkit-filter property to the start of the stylesheet
+stylesheet.addRule("body", "-webkit-filter: url(#a) url(#b)", 0);
+
+var cssRule = stylesheet.cssRules.item(0);
 
 shouldBe("cssRule.type", "1");
 
