3f0b0d850fd944223007b66a85762ba66c969292
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-26 00:26:03 +0000

    CSS Shaders: Use u_texture instead of s_texture. It was updated in the spec https://bugs.webkit.org/show_bug.cgi?id=82618
    Source/WebCore:
    
    Reviewed by Dean Jackson.
    
    Changed the uniform name passed to the CSS Shaders from s_texture to u_texture.
    https://dvcs.w3.org/hg/FXTF/raw-file/tip/filters/index.html
    
    No new tests, just updating existing ones.
    
    * platform/graphics/filters/CustomFilterShader.cpp:
    (WebCore::CustomFilterShader::defaultFragmentShaderString):
    (WebCore::CustomFilterShader::initializeParameterLocations):
    
    LayoutTests:
    
    Reviewed by Dean Jackson.
    
    Changed the uniform name passed to the CSS Shaders from s_texture to u_texture.
    https://dvcs.w3.org/hg/FXTF/raw-file/tip/filters/index.html
    
    * css3/filters/resources/color-add.fs:
    * css3/filters/resources/color-offset-parameters.fs:
    * css3/filters/resources/color-offset.fs:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/css3/filters/resources/color-offset.fs
+++ third_party/WebKit/LayoutTests/css3/filters/resources/color-offset.fs
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 precision mediump float;
 varying vec2 v_texCoord;
-uniform sampler2D s_texture;
+uniform sampler2D u_texture;
 void main()
 {
     // Offset the color value with 0.3 on each color channel.
-    gl_FragColor = texture2D(s_texture, v_texCoord) + 0.3;
+    gl_FragColor = texture2D(u_texture, v_texCoord) + 0.3;
 }
