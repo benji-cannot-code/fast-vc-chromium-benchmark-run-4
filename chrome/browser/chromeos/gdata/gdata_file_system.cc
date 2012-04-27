3c7ac02b85f09df4c8fe38483ae9bfa1343823b2
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 21:20:18 +0000

    gdata: Remove GDataFileSystem::FindEntryByPathSync()
    BUG=chromium-os:30066
    TEST=run unit tests
    
    Review URL: https://chromiumcodereview.appspot.com/10255016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system.cc
@@ -989,13 +989,6 @@ void GDataFileSystem::Authenticate(const AuthStatusCallback& callback) {
   documents_service_->Authenticate(callback);
 }
 
-void GDataFileSystem::FindEntryByPathSync(
-    const FilePath& search_file_path,
-    FindEntryDelegate* delegate) {
-  base::AutoLock lock(lock_);
-  root_->FindEntryByPath(search_file_path, delegate);
-}
-
 void GDataFileSystem::FindEntryByResourceIdSync(
     const std::string& resource_id,
     FindEntryDelegate* delegate) {
@@ -1086,7 +1079,7 @@ void GDataFileSystem::FindEntryByPathOnCallingThread(
     const FilePath& search_file_path,
     const FindEntryCallback& callback) {
   FindEntryCallbackRelayDelegate delegate(callback);
-  FindEntryByPathSync(search_file_path, &delegate);
+  root_->FindEntryByPath(search_file_path, &delegate);
 }
 
 void GDataFileSystem::ReloadFeedFromServerIfNeeded(
