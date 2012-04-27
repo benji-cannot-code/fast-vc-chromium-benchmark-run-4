3c7feff1c872f7938a0aeaca36744431fefd1ff4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 09:47:05 +0000

    [EFL] DRT should support LayoutTestController's dumpResourceLoadCallbacks() https://bugs.webkit.org/show_bug.cgi?id=81891
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-04-27
    Reviewed by Philippe Normand.
    
    Source/WebKit/efl:
    
    New "load,resource,finished", "load,resource,failed", and
    "resource,response,received" signals were added to notify about different
    resource load events. These additional signals are needed for proper
    support of LayoutTestController's dumpResourceLoadCallbacks() in EFL's
    DumpRenderTree.
    
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    (WebCore::FrameLoaderClientEfl::assignIdentifierToInitialRequest):
    (WebCore::FrameLoaderClientEfl::dispatchDidReceiveResponse):
    (WebCore::FrameLoaderClientEfl::dispatchDidFinishLoading):
    (WebCore::FrameLoaderClientEfl::dispatchDidFailLoading):
    * ewk/ewk_frame.cpp:
    (ewk_frame_response_received):
    (ewk_frame_load_finished):
    (ewk_frame_load_resource_finished):
    (ewk_frame_load_resource_failed):
    (ewk_frame_load_error):
    * ewk/ewk_frame.h:
    * ewk/ewk_private.h:
    * ewk/ewk_view.h:
    
    Tools:
    
    Implement support for LayoutTestController's
    dumpResourceLoadCallbacks() in EFL's DumpRenderTree by catching new
    ewk_view signals about resource loading and print out the expected
    information. This allows for several test cases to be removed from the
    skip list.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::createView):
    (descriptionSuitableForTestResult):
    (DumpRenderTreeChrome::onResponseReceived):
    (DumpRenderTreeChrome::onResourceLoadFinished):
    (DumpRenderTreeChrome::onResourceLoadFailed):
    (DumpRenderTreeChrome::onNewResourceRequest):
    * DumpRenderTree/efl/DumpRenderTreeChrome.h:
    (DumpRenderTreeChrome):
    
    LayoutTests:
    
    Unskip tests which depend on EFL's DumpRenderTree support for
    LayoutTestController's dumpResourceLoadCallbacks(), now that it is
    implemented.
    
    * platform/efl/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.h
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.h
@@ -32,9 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Eina.h>
 #include <Evas.h>
+#include <wtf/HashMap.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 #include <wtf/Vector.h>
+#include <wtf/text/CString.h>
 
 class DumpRenderTreeChrome {
 public:
@@ -62,6 +64,7 @@ private:
     Evas* m_evas;
     OwnPtr<GCController> m_gcController;
     Vector<Evas_Object*> m_extraViews;
+    static HashMap<unsigned long, CString> m_dumpAssignedUrls;
 
     // Smart callbacks
     static void onWindowObjectCleared(void*, Evas_Object*, void*);
@@ -97,6 +100,14 @@ private:
 
     static void onFrameLoadError(void*, Evas_Object*, void*);
     static void onDidDetectXSS(void*, Evas_Object*, void*);
+
+    static void onResponseReceived(void*, Evas_Object*, void*);
+
+    static void onResourceLoadFinished(void*, Evas_Object*, void*);
+
+    static void onResourceLoadFailed(void*, Evas_Object*, void*);
+
+    static void onNewResourceRequest(void*, Evas_Object*, void*);
 };
 
 #endif // DumpRenderTreeChrome_h
