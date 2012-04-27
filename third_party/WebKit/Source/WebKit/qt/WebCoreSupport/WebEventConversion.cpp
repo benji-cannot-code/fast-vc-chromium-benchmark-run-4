cda3956a4080efff503225c87f3b40b655e6bda1
kkristof@inf.u-szeged.hu <kkristof@inf.u-szeged.hu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 12:02:12 +0000

    Move WebKit1 specific conversion of touch-events to WebKit1 https://bugs.webkit.org/show_bug.cgi?id=84951
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-04-27
    Reviewed by Kenneth Rohde Christiansen.
    
    Fix compile when touch-events are disabled.
    
    * WebCoreSupport/WebEventConversion.cpp:
    (WebCore):
    * WebCoreSupport/WebEventConversion.h:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/WebEventConversion.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/WebEventConversion.cpp
@@ -221,7 +221,7 @@ WebKitPlatformWheelEvent::WebKitPlatformWheelEvent(QWheelEvent* e)
     applyDelta(e->delta(), e->orientation());
 }
 
-
+#if ENABLE(TOUCH_EVENTS)
 class WebKitPlatformTouchEvent : public PlatformTouchEvent {
 public:
     WebKitPlatformTouchEvent(QTouchEvent*);
@@ -303,6 +303,7 @@ WebKitPlatformTouchPoint::WebKitPlatformTouchPoint(const QTouchEvent::TouchPoint
     m_force = point.pressure();
     // FIXME: Support m_rotationAngle if QTouchEvent at some point supports it.
 }
+#endif
 
 PlatformWheelEvent convertWheelEvent(QWheelEvent* event)
 {
@@ -314,9 +315,11 @@ PlatformWheelEvent convertWheelEvent(QGraphicsSceneWheelEvent* event)
     return WebKitPlatformWheelEvent(event);
 }
 
+#if ENABLE(TOUCH_EVENTS)
 PlatformTouchEvent convertTouchEvent(QTouchEvent* event)
 {
     return WebKitPlatformTouchEvent(event);
 }
+#endif
 
 }
