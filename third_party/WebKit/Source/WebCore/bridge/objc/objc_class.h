40e6b72de4b7514468982a6f0c1ef385b70d69a4
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 01:03:26 +0000

    Use WebKit types for the cache of ObjcClass::methodsNamed() https://bugs.webkit.org/show_bug.cgi?id=85012
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-04-26
    Reviewed by Geoffrey Garen.
    
    This patch redefines the method cache ObjcClass to avoid memory allocations in the case of positive match.
    
    Instead of using the converted name as the key, the original identifier string is used. This shortcuts
    all the other operations when there is a match.
    
    A side effect is a method can appear multiple times in the cache if it is invoked with different names using
    the escape character "$". An attaquer could bloat the cache with a few hundreds strings.
    In the common case, having each name mapped is an improvment.
    
    * bridge/objc/objc_class.h:
    (ObjcClass):
    * bridge/objc/objc_class.mm:
    (JSC::Bindings::ObjcClass::ObjcClass):
    (JSC::Bindings::ObjcClass::methodsNamed):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
+++ third_party/WebKit/Source/WebCore/bridge/objc/objc_class.h
@@ -51,7 +51,7 @@ public:
     
 private:
     ClassStructPtr _isa;
-    RetainPtr<CFMutableDictionaryRef> _methods;
+    mutable HashMap<String, OwnPtr<Method> > m_methodCache;
     mutable HashMap<String, OwnPtr<Field> > m_fieldCache;
 };
 
