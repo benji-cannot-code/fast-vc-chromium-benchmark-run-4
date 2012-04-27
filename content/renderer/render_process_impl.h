17811e78667a8480d6c1abcbc637d4a59749f443
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 00:13:04 +0000

    Remove an unused #include of a native_client header
    BUG=none
    TEST=build
    
    
    Review URL: http://codereview.chromium.org/10207027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134179 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_process_impl.h
+++ content/renderer/render_process_impl.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/timer.h"
 #include "content/renderer/render_process.h"
-#include "native_client/src/shared/imc/nacl_imc.h"
 
 namespace skia {
 class PlatformCanvas;
