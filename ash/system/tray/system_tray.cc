b0f399a2bc4231757fac36b1b4712f8c8a65a9a6
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 00:16:16 +0000

    Fix the bluetooth status notification issue on systemtray.
    BUG=132208
    TEST=System tray panel should refresh to show bluetooth adapter status when it is plugged in for the case described in the bug.
    
    
    Review URL: http://codereview.chromium.org/10214005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134181 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -49,7 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/screen.h"
 #include "ui/gfx/skia_util.h"
 #include "ui/views/border.h"
+#include "ui/views/bubble/bubble_border.h"
 #include "ui/views/bubble/bubble_delegate.h"
+#include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/box_layout.h"
@@ -90,12 +92,20 @@ class TrayPopupItemContainer : public views::View {
                                 NULL);
     SetLayoutManager(new views::FillLayout);
     AddChildView(view);
+    SetVisible(view->visible());
   }
 
   virtual ~TrayPopupItemContainer() {}
 
  private:
   // Overridden from views::View.
+  virtual void ChildVisibilityChanged(View* child) OVERRIDE {
+    if (visible() == child->visible())
+      return;
+    SetVisible(child->visible());
+    PreferredSizeChanged();
+  }
+
   virtual void OnMouseEntered(const views::MouseEvent& event) OVERRIDE {
     hover_ = true;
     SchedulePaint();
@@ -167,10 +177,13 @@ class SystemTrayBubbleBackground : public views::Background {
   DISALLOW_COPY_AND_ASSIGN(SystemTrayBubbleBackground);
 };
 
-class SystemTrayBubbleBorder : public views::Border {
+class SystemTrayBubbleBorder : public views::BubbleBorder {
  public:
   explicit SystemTrayBubbleBorder(views::View* owner)
-      : owner_(owner) {
+      : views::BubbleBorder(views::BubbleBorder::BOTTOM_RIGHT,
+                            views::BubbleBorder::NO_SHADOW),
+        owner_(owner) {
+    set_alignment(views::BubbleBorder::ALIGN_EDGE_TO_ANCHOR_EDGE);
   }
 
   virtual ~SystemTrayBubbleBorder() {}
@@ -214,11 +227,6 @@ class SystemTrayBubbleBorder : public views::Border {
       canvas->DrawPath(path, paint);
     }
   }
-
-  virtual void GetInsets(gfx::Insets* insets) const OVERRIDE {
-    insets->Set(0, 0, kArrowHeight, 0);
-  }
-
   views::View* owner_;
 
   DISALLOW_COPY_AND_ASSIGN(SystemTrayBubbleBorder);
@@ -258,7 +266,7 @@ class SystemTrayBubble : public views::BubbleDelegateView {
  public:
   SystemTrayBubble(ash::SystemTray* tray,
                    views::View* anchor,
-                   std::vector<ash::SystemTrayItem*>& items,
+                   const std::vector<ash::SystemTrayItem*>& items,
                    bool detailed)
       : views::BubbleDelegateView(anchor, views::BubbleBorder::BOTTOM_RIGHT),
         tray_(tray),
@@ -285,6 +293,10 @@ class SystemTrayBubble : public views::BubbleDelegateView {
 
   bool detailed() const { return detailed_; }
   void set_can_activate(bool activate) { can_activate_ = activate; }
+
+  void SetBubbleBorder(views::BubbleBorder* border) {
+    GetBubbleFrameView()->SetBubbleBorder(border);
+  }
 
   void StartAutoCloseTimer(int seconds) {
     autoclose_.Stop();
@@ -338,6 +350,10 @@ class SystemTrayBubble : public views::BubbleDelegateView {
   }
 
   // Overridden from views::View.
+  virtual void ChildPreferredSizeChanged(View* child) OVERRIDE {
+    SizeToContents();
+  }
+
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE {
     if (can_activate_) {
       state->role = ui::AccessibilityTypes::ROLE_WINDOW;
@@ -593,7 +609,7 @@ void SystemTray::SetPaintsBackground(
   hide_background_animator_.SetPaintsBackground(value, change_type);
 }
 
-void SystemTray::ShowItems(std::vector<SystemTrayItem*>& items,
+void SystemTray::ShowItems(const std::vector<SystemTrayItem*>& items,
                            bool detailed,
                            bool activate) {
   CHECK(!popup_);
@@ -607,8 +623,7 @@ void SystemTray::ShowItems(std::vector<SystemTrayItem*>& items,
     should_show_launcher_ = true;
   bubble_->SetAlignment(views::BubbleBorder::ALIGN_EDGE_TO_ANCHOR_EDGE);
   popup_->non_client_view()->frame_view()->set_background(NULL);
-  popup_->non_client_view()->frame_view()->set_border(
-      new SystemTrayBubbleBorder(bubble_));
+  bubble_->SetBubbleBorder(new SystemTrayBubbleBorder(bubble_));
   MessageLoopForUI::current()->AddObserver(this);
   popup_->AddObserver(this);
 
--- ui/views/bubble/bubble_frame_view.h
+++ ui/views/bubble/bubble_frame_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,6 +49,8 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView {
   gfx::Rect GetUpdatedWindowBounds(const gfx::Rect& anchor_rect,
                                    gfx::Size client_size,
                                    bool try_mirroring_arrow);
+
+  void SetBubbleBorder(BubbleBorder* border);
 
  protected:
   // Returns the bounds for the monitor showing the specified |rect|.
