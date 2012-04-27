dafd0635125a899a3eacdadaeda9c3b3f4e0f99f
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 16:35:38 +0000

    [Gtk][DOM Bindings] Feature-protected properties are put under condition guards https://bugs.webkit.org/show_bug.cgi?id=85068
    Reviewed by Martin Robinson.
    
    Generated feature-dependent properties are now present regardless of that
    feature being enabled. On getting or setting that property's value a warning
    is thrown if the feature is not enabled. Additionally, if the generated
    interface is feature-dependant, when getting or setting any property's value
    a warning is thrown if the feature is not enabled.
    
    No new tests - covered by existing bindings tests.
    
    * bindings/scripts/CodeGeneratorGObject.pm:
    (GenerateProperty):
    (GenerateProperties):
    * bindings/scripts/test/GObject/WebKitDOMTestInterface.cpp: Rebaseline.
    (webkit_dom_test_interface_set_property):
    (webkit_dom_test_interface_get_property):
    (webkit_dom_test_interface_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp: Ditto.
    (webkit_dom_test_obj_set_property):
    (webkit_dom_test_obj_get_property):
    (webkit_dom_test_obj_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestSerializedScriptValueInterface.cpp: Ditto.
    (webkit_dom_test_serialized_script_value_interface_get_property):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
@@ -347,6 +347,11 @@ sub GetWriteableProperties {
 sub GenerateConditionalWarning
 {
     my $node = shift;
+    my $indentSize = shift;
+    if (!$indentSize) {
+        $indentSize = 4;
+    }
+
     my $conditional = $node->extendedAttributes->{"Conditional"};
     my @warn;
 
@@ -355,15 +360,15 @@ sub GenerateConditionalWarning
             my @splitConditionals = split(/&/, $conditional);
             foreach $condition (@splitConditionals) {
                 push(@warn, "#if !ENABLE($condition)\n");
-                push(@warn, "    WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($condition) . "\")\n");
+                push(@warn, ' ' x $indentSize . "WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($condition) . "\")\n");
                 push(@warn, "#endif\n");
             }
         } elsif ($conditional =~ /\|/) {
             foreach $condition (split(/\|/, $conditional)) {
-                push(@warn, "    WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($condition) . "\")\n");
+                push(@warn, ' ' x $indentSize . "WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($condition) . "\")\n");
             }
         } else {
-            push(@warn, "    WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($conditional) . "\")\n");
+            push(@warn, ' ' x $indentSize . "WEBKIT_WARN_FEATURE_NOT_PRESENT(\"" . HumanReadableConditional($conditional) . "\")\n");
         }
     }
 
@@ -374,8 +379,12 @@ sub GenerateProperty {
     my $attribute = shift;
     my $interfaceName = shift;
     my @writeableProperties = @{shift @_};
+    my $parentNode = shift;
 
     my $conditionalString = $codeGenerator->GenerateConditionalString($attribute->signature);
+    my @conditionalWarn = GenerateConditionalWarning($attribute->signature, 8);
+    my $parentConditionalString = $codeGenerator->GenerateConditionalString($parentNode);
+    my @parentConditionalWarn = GenerateConditionalWarning($parentNode, 8);
     my $camelPropName = $attribute->signature->name;
     my $setPropNameFunction = $codeGenerator->WK_ucfirst($camelPropName);
     my $getPropNameFunction = $codeGenerator->WK_lcfirst($camelPropName);
@@ -384,9 +393,7 @@ sub GenerateProperty {
     my $propNameCaps = uc($propName);
     $propName =~ s/_/-/g;
     my ${propEnum} = "PROP_${propNameCaps}";
-    push(@cBodyProperties, "#if ${conditionalString}\n") if $conditionalString;
     push(@cBodyProperties, "    ${propEnum},\n");
-    push(@cBodyProperties, "#endif /* ${conditionalString} */\n") if $conditionalString;
 
     my $propType = $attribute->signature->type;
     my ${propGType} = decamelize($propType);
@@ -435,16 +442,23 @@ sub GenerateProperty {
     push(@setterArguments, "ec") if @{$attribute->setterExceptions};
 
     if (grep {$_ eq $attribute} @writeableProperties) {
-        push(@txtSetProps, "#if ${conditionalString}\n") if $conditionalString;
         push(@txtSetProps, "    case ${propEnum}: {\n");
+        push(@txtSetProps, "#if ${parentConditionalString}\n") if $parentConditionalString;
+        push(@txtSetProps, "#if ${conditionalString}\n") if $conditionalString;
         push(@txtSetProps, "        WebCore::ExceptionCode ec = 0;\n") if @{$attribute->setterExceptions};
         push(@txtSetProps, "        ${setterFunctionName}(" . join(", ", @setterArguments) . ");\n");
-        push(@txtSetProps, "        break;\n    }\n");
+        push(@txtSetProps, "#else\n") if $conditionalString;
+        push(@txtSetProps, @conditionalWarn) if scalar(@conditionalWarn);
         push(@txtSetProps, "#endif /* ${conditionalString} */\n") if $conditionalString;
+        push(@txtSetProps, "#else\n") if $parentConditionalString;
+        push(@txtSetProps, @parentConditionalWarn) if scalar(@parentConditionalWarn);
+        push(@txtSetProps, "#endif /* ${parentConditionalString} */\n") if $parentConditionalString;
+        push(@txtSetProps, "        break;\n    }\n");
     }
 
-    push(@txtGetProps, "#if ${conditionalString}\n") if $conditionalString;
     push(@txtGetProps, "    case ${propEnum}: {\n");
+    push(@txtGetProps, "#if ${parentConditionalString}\n") if $parentConditionalString;
+    push(@txtGetProps, "#if ${conditionalString}\n") if $conditionalString;
     push(@txtGetProps, "        WebCore::ExceptionCode ec = 0;\n") if @{$attribute->getterExceptions};
 
     my $postConvertFunction = "";
@@ -474,8 +488,13 @@ sub GenerateProperty {
         }
     }
 
-    push(@txtGetProps, "        break;\n    }\n");
+    push(@txtGetProps, "#else\n") if $conditionalString;
+    push(@txtGetProps, @conditionalWarn) if scalar(@conditionalWarn);
     push(@txtGetProps, "#endif /* ${conditionalString} */\n") if $conditionalString;
+    push(@txtGetProps, "#else\n") if $parentConditionalString;
+    push(@txtGetProps, @parentConditionalWarn) if scalar(@parentConditionalWarn);
+    push(@txtGetProps, "#endif /* ${parentConditionalString} */\n") if $parentConditionalString;
+    push(@txtGetProps, "        break;\n    }\n");
 
     my %param_spec_options = ("int", "G_MININT, /* min */\nG_MAXINT, /* max */\n0, /* default */",
                               "boolean", "FALSE, /* default */",
@@ -501,9 +520,7 @@ sub GenerateProperty {
                                                            $param_spec_options{$gtype}
                                                            ${gparamflag}));
 EOF
-    push(@txtInstallProps, "#if ${conditionalString}\n") if $conditionalString;
     push(@txtInstallProps, $txtInstallProp);
-    push(@txtInstallProps, "#endif /* ${conditionalString} */\n") if $conditionalString;
 }
 
 sub GenerateProperties {
@@ -578,7 +595,7 @@ EOF
     foreach my $attribute (@readableProperties) {
         if ($attribute->signature->type ne "EventListener" &&
             $attribute->signature->type ne "MediaQueryListListener") {
-            GenerateProperty($attribute, $interfaceName, \@writeableProperties);
+            GenerateProperty($attribute, $interfaceName, \@writeableProperties, $dataNode);
         }
     }
 
