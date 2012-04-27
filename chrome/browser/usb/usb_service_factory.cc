8b9f69ec51698d2352b3563676c03ebc46775d59
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 16:09:57 +0000

    Adding UsbService and UsbDevice constructs
    UsbService and UsbDevice allow for platform-independent access to the system's
    USB subsystem.
    
    BUG=none
    TEST=none
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=134216
    
    Review URL: http://codereview.chromium.org/10161035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134278 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/usb/usb_service_factory.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/usb/usb_service_factory.h"
+
+#include "base/memory/singleton.h"
+#include "chrome/browser/profiles/profile_dependency_manager.h"
+#include "chrome/browser/usb/usb_service.h"
+
+UsbServiceFactory* UsbServiceFactory::GetInstance() {
+  return Singleton<UsbServiceFactory>::get();
+}
+
+UsbService* UsbServiceFactory::GetForProfile(Profile* profile) {
+  return static_cast<UsbService*>(
+      GetInstance()->GetServiceForProfile(profile, true));
+}
+
+UsbServiceFactory::UsbServiceFactory() : ProfileKeyedServiceFactory(
+    "UsbService", ProfileDependencyManager::GetInstance()) {}
+
+UsbServiceFactory::~UsbServiceFactory() {}
+
+ProfileKeyedService* UsbServiceFactory::BuildServiceInstanceFor(
+    Profile* profile) const {
+  return new UsbService();
+}
