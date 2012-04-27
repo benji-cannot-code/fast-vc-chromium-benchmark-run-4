ad9254dd408c3c3c54bdc8b550050712a8bc1d59
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 00:49:42 +0000

    Fix presubmit style issues for JS and add network_action_predictor to JS checker.
    BUG=122002,122011
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10236014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/network_action_predictor/network_action_predictor.js
+++ chrome/browser/resources/network_action_predictor/network_action_predictor.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 function requestNetworkActionPredictorDb() {
   console.debug('Requesting NAP DB');
-  chrome.send('requestNetworkActionPredictorDb', [])
+  chrome.send('requestNetworkActionPredictorDb');
 }
 
 
@@ -79,3 +79,5 @@ function updateDatabaseView(database) {
 }
 
 document.addEventListener('DOMContentLoaded', requestNetworkActionPredictorDb);
+
+
