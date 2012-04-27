114a28a953dadcf45ac359f03d04cd5385b6fbfa
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 17:18:00 +0000

    Add renderer process support in PyAuto HeapProfilerDump API.
    BUG=123752
    TEST=none
    
    Review URL: http://codereview.chromium.org/10035055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_tab_helper.cc
+++ chrome/browser/automation/automation_tab_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -55,6 +55,12 @@ void AutomationTabHelper::RemoveObserver(TabEventObserver* observer) {
 void AutomationTabHelper::SnapshotEntirePage() {
   Send(new AutomationMsg_SnapshotEntirePage(routing_id()));
 }
+
+#if !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
+void AutomationTabHelper::HeapProfilerDump(const std::string& reason) {
+  Send(new AutomationMsg_HeapProfilerDump(routing_id(), reason));
+}
+#endif  // !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
 
 bool AutomationTabHelper::has_pending_loads() const {
   return is_loading_ || !pending_client_redirects_.empty();
--- chrome/browser/automation/automation_tab_helper.h
+++ chrome/browser/automation/automation_tab_helper.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -88,6 +88,11 @@ class AutomationTabHelper
 
   // Snapshots the entire page without resizing.
   void SnapshotEntirePage();
+
+#if !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
+  // Dumps a heap profile.
+  void HeapProfilerDump(const std::string& reason);
+#endif  // !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
 
   // Returns true if the tab is loading or the tab is scheduled to load
   // immediately. Note that scheduled loads may be canceled.
