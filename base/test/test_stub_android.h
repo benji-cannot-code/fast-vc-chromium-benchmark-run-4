234ec10ef2cb3f9ff5602dfdec0f1d137caa8cfb
peter@chromium.org <peter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 14:05:07 +0000

    Initialize Android OS path overrides for testing
    WebKit's TestWebKitAPI and webkit_unit_tests rely on paths on the phone to
    be overridden in order to run correctly.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10203014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134265 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_stub_android.h
+++ base/test/test_stub_android.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,5 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Initialize the Android test environment.
 void InitAndroidTestStub();
+
+// Register path providers for tests.
+void InitAndroidOSPathStub();
 
 #endif  // BASE_TEST_TEST_STUB_ANDROID_H_
--- webkit/support/platform_support_android.cc
+++ webkit/support/platform_support_android.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/string16.h"
 #include "base/string_piece.h"
+#include "base/test/test_stub_android.h"
 #include "googleurl/src/gurl.h"
 #include "grit/webkit_resources.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -20,7 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webkit_support {
 
 void BeforeInitialize(bool unit_test_mode) {
-  NOTIMPLEMENTED(); // TODO(zhenghao): Implement this function.
+  InitAndroidOSPathStub();
+  // Set XML_CATALOG_FILES environment variable to blank to prevent libxml from
+  // loading and complaining the non-exsistent /etc/xml/catalog file.
+  setenv("XML_CATALOG_FILES", "", 0);
 }
 
 void AfterInitialize(bool unit_test_mode) {
@@ -50,7 +54,6 @@ void BeforeShutdown() {
 }
 
 void AfterShutdown() {
-  NOTIMPLEMENTED(); // TODO(zhenghao): Implement this function.
 }
 
 }  // namespace webkit_support
