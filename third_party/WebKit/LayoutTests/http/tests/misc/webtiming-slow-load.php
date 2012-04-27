033b711bf0929459a72386bfe120157f48927918
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 19:31:24 +0000

    Clean up tests in preparation for ES5.2 global scope fix https://bugs.webkit.org/show_bug.cgi?id=85001
    Reviewed by Gavin Barraclough.
    
    * fast/css/resize-value-compared.html:
    * fast/dom/resources/navigation-type-matches-querystring.html:
    * fast/dom/script-tests/webtiming-document-open.js:
    * fast/dom/script-tests/webtiming-navigate-within-document.js:
    * fast/dom/script-tests/webtiming.js:
    * http/tests/inspector/indexeddb/indexeddb-test.js:
    * http/tests/misc/resources/webtiming-cross-origin-and-back2.html:
    * http/tests/misc/resources/webtiming-cross-origin-redirect.html:
    * http/tests/misc/resources/webtiming-no-origin.html:
    * http/tests/misc/resources/webtiming-one-redirect.html:
    * http/tests/misc/resources/webtiming-ssl.html:
    * http/tests/misc/resources/webtiming-two-redirects.html:
    * http/tests/misc/webtiming-slow-load.php:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/misc/webtiming-slow-load.php
+++ third_party/WebKit/LayoutTests/http/tests/misc/webtiming-slow-load.php
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <script>
 description("Verifies that requestStart and responseStart are available before the main document has finished loading.");
 
-var performance = window.performance || {};
+window.performance = window.performance || {};
 var navigation = performance.navigation || {};
 var timing = performance.timing || {};
 
