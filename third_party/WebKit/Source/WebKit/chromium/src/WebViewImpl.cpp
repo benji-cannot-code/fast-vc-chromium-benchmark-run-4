c9b4211554f54fc014d849c47d316265101dc021
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 16:29:13 +0000

    Unreviewed, rolling out r115400. http://trac.webkit.org/changeset/115400 https://bugs.webkit.org/show_bug.cgi?id=84420
    Broke unit tests.
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::popupOpened):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::applyAutofillSuggestions):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -2784,8 +2784,8 @@ void WebViewImpl::applyAutofillSuggestions(
     if (m_autofillPopupShowing) {
         refreshAutofillPopup();
     } else {
-        m_autofillPopupShowing = true;
         m_autofillPopup->showInRect(focusedNode->getPixelSnappedRect(), focusedNode->ownerDocument()->view(), 0);
+        m_autofillPopupShowing = true;
     }
 }
 
