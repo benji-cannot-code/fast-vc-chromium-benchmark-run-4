9f1370bdcc97eb2328ef38edcf97a5c04d0c6af0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-27 15:29:08 +0000

    [Blackberry] remove redundant returns https://bugs.webkit.org/show_bug.cgi?id=85033
    Patch by Chris Guan <chris.guan@torchmobile.com.cn> on 2012-04-27
    Reviewed by Antonio Gomes.
    
    Remove two redundant returns which were added by accident.
    
    No new test cases, because no behaviour has changed.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::setSelection):
    (BlackBerry::WebKit::WebPage::setCaretPosition):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -4450,7 +4450,7 @@ void WebPage::setSelection(const Platform::IntPoint& startPoint, const Platform:
     invalidPoint = IntPoint(endPoint) == DOMSupport::InvalidPoint;
     IntPoint end = invalidPoint ? DOMSupport::InvalidPoint : d->mapFromTransformed(endPoint);
 
-    return d->m_selectionHandler->setSelection(start, end);
+    d->m_selectionHandler->setSelection(start, end);
 }
 
 void WebPage::setCaretPosition(const Platform::IntPoint& position)
@@ -4459,7 +4459,7 @@ void WebPage::setCaretPosition(const Platform::IntPoint& position)
         return;
     // Handled by selection handler as it's point based.
     // Transform this events coordinates to webkit content coordinates.
-    return d->m_selectionHandler->setCaretPosition(d->mapFromTransformed(position));
+    d->m_selectionHandler->setCaretPosition(d->mapFromTransformed(position));
 }
 
 void WebPage::selectAtPoint(const Platform::IntPoint& location)
