06c50d5cc86d10b741f8ac8ce357adfd4fff00c8
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-28 04:26:01 +0000

    Stop using aligned allocation for WeakBlock https://bugs.webkit.org/show_bug.cgi?id=85124
    Reviewed by Anders Carlsson.
    
    We don't actually use the alignment for anything.
    
    * heap/WeakBlock.cpp:
    (JSC::WeakBlock::create):
    (JSC::WeakBlock::WeakBlock): Switched from aligned allocation to regular
    allocation.
    
    * heap/WeakBlock.h:
    (WeakBlock): Don't use HeapBlock because HeapBlock requires aligned
    allocation. This change required me to add some declarations that we used
    to inherit from HeapBlock.
    
    (WeakBlock::blockFor): Removed. This function relied on aligned allocation
    but didn't do anything for us.
    
    (WeakBlock::deallocate): Removed. WeakBlock doesn't own any of the deallocation
    logic, so it shouldn't own the function.
    
    * heap/WeakSet.cpp:
    (JSC::WeakSet::~WeakSet):
    (JSC::WeakSet::finalizeAll):
    (JSC::WeakSet::visitLiveWeakImpls):
    (JSC::WeakSet::visitDeadWeakImpls):
    (JSC::WeakSet::sweep):
    (JSC::WeakSet::shrink):
    (JSC::WeakSet::resetAllocator):
    (JSC::WeakSet::tryFindAllocator):
    * heap/WeakSet.h:
    (WeakSet): Updated declarations to reflect WeakBlock not inheriting from
    HeapBlock. This allowed me to remove some casts, which was nice.
    
    (JSC::WeakSet::deallocate): Directly set the deallocated flag instead of
    asking WeakBlock to do it for us.  We don't need to have a WeakBlock
    pointer to set the flag, so stop asking for one.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/WeakBlock.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/WeakBlock.cpp
@@ -37,7 +37,7 @@ namespace JSC {
 
 WeakBlock* WeakBlock::create()
 {
-    PageAllocationAligned allocation = PageAllocationAligned::allocate(blockSize, blockSize, OSAllocator::JSGCHeapPages);
+    PageAllocation allocation = PageAllocation::allocate(blockSize, OSAllocator::JSGCHeapPages);
     if (!static_cast<bool>(allocation))
         CRASH();
     return new (NotNull, allocation.base()) WeakBlock(allocation);
@@ -48,8 +48,8 @@ void WeakBlock::destroy(WeakBlock* block)
     block->m_allocation.deallocate();
 }
 
-WeakBlock::WeakBlock(PageAllocationAligned& allocation)
-    : HeapBlock(allocation)
+WeakBlock::WeakBlock(PageAllocation& allocation)
+    : m_allocation(allocation)
 {
     for (size_t i = 0; i < weakImplCount(); ++i) {
         WeakImpl* weakImpl = &weakImpls()[i];
