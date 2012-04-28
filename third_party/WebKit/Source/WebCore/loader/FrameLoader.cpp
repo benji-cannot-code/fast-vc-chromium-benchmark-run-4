b89d824396b66c68c5b01a67e5a3729a22dae12d
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-28 06:28:37 +0000

    Ensure that there's always a provisional document loader if the frame loader is in provisional state https://bugs.webkit.org/show_bug.cgi?id=83894
    Reviewed by Nate Chapin.
    
    We're still seeing crashes in the FrameLoader where the FrameLoader's
    state is "provisional" but there is no provisional document loader. I
    added code to update the FrameLoader's state everytime the provisional
    document loader is cleared, and added checks that the FrameLoader's
    state can't be set to provisional without a provisional loader.
    
    If the crashes go away, or the newly added checks reveal the culprit,
    we should relex the checks to use ASSERT() instead of CRASH().
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::init):
    (WebCore::FrameLoader::setupForReplace):
    (WebCore::FrameLoader::stopAllLoaders):
    (WebCore::FrameLoader::clearProvisionalLoad):
    (WebCore::FrameLoader::continueFragmentScrollAfterNavigationPolicy):
    (WebCore::FrameLoader::continueLoadAfterNavigationPolicy):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -218,6 +218,9 @@ void FrameLoader::init()
     // It would be better if this could be done with even fewer steps.
     m_stateMachine.advanceTo(FrameLoaderStateMachine::CreatingInitialEmptyDocument);
     setPolicyDocumentLoader(m_client->createDocumentLoader(ResourceRequest(KURL(ParsedURLString, emptyString())), SubstituteData()).get());
+    // FIXME: Make this an ASSERT() instead once we figured out what's going wrong.
+    if (!m_policyDocumentLoader.get())
+        CRASH();
     setProvisionalDocumentLoader(m_policyDocumentLoader.get());
     setState(FrameStateProvisional);
     m_provisionalDocumentLoader->setResponse(ResourceResponse(KURL(), "text/html", 0, String(), String()));
@@ -1116,6 +1119,9 @@ void FrameLoader::prepareForLoadStart()
 
 void FrameLoader::setupForReplace()
 {
+    // FIXME: Make this an ASSERT() instead once we figured out what's going wrong.
+    if (!m_documentLoader.get())
+        CRASH();
     setState(FrameStateProvisional);
     m_provisionalDocumentLoader = m_documentLoader;
     m_documentLoader = 0;
@@ -1524,6 +1530,8 @@ void FrameLoader::stopAllLoaders(ClearProvisionalItemPolicy clearProvisionalItem
         m_documentLoader->stopLoading();
 
     setProvisionalDocumentLoader(0);
+    if (m_state == FrameStateProvisional)
+        setState(FrameStateComplete);
 
     m_checkTimer.stop();
 
@@ -1638,9 +1646,9 @@ void FrameLoader::setState(FrameState newState)
 
 void FrameLoader::clearProvisionalLoad()
 {
-    setProvisionalDocumentLoader(0);
     if (Page* page = m_frame->page())
         page->progress()->progressCompleted(m_frame);
+    setProvisionalDocumentLoader(0);
     setState(FrameStateComplete);
 }
 
@@ -2596,6 +2604,9 @@ void FrameLoader::continueFragmentScrollAfterNavigationPolicy(const ResourceRequ
     if (m_provisionalDocumentLoader && !equalIgnoringFragmentIdentifier(m_provisionalDocumentLoader->request().url(), request.url())) {
         m_provisionalDocumentLoader->stopLoading();
         setProvisionalDocumentLoader(0);
+        // FIXME: Make this an ASSERT() instead once we figured out what's going wrong.
+        if (m_state == FrameStateProvisional)
+            CRASH();
     }
 
     bool isRedirect = m_quickRedirectComing || policyChecker()->loadType() == FrameLoadTypeRedirectWithLockedBackForwardList;    
@@ -2758,6 +2769,9 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
     }
 #endif
 
+    // FIXME: Make this an ASSERT() instead once we figured out what's going wrong.
+    if (!m_policyDocumentLoader.get())
+        CRASH();
     setProvisionalDocumentLoader(m_policyDocumentLoader.get());
     m_loadType = type;
     setState(FrameStateProvisional);
