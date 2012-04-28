b2a57ba7381a4bdd48dc715fdd1cf2ae121b6c83
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-28 03:12:25 +0000

    [BlackBerry] Fixed background is scrolling in http://www.nieuwecode.nl https://bugs.webkit.org/show_bug.cgi?id=85109
    Patch by Arvid Nilsson <anilsson@rim.com> on 2012-04-27
    Reviewed by Antonio Gomes.
    
    Since the BlackBerry port uses very similar fixed position acceleration
    as the Qt WebKit2 port, the same fix that worked for them in bug 83980
    works for us.
    
    Fixed by opting in to the FIXED_POSITION_CREATES_STACKING_CONTEXT
    mechanism.
    
    Covered by existing manual test fixed-position-no-z-index.html.
    
    * css/StyleResolver.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115530 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
@@ -149,7 +149,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StyleCachedImageSet.h"
 #endif
 
-#if PLATFORM(QT)
+#if PLATFORM(QT) || PLATFORM(BLACKBERRY)
 #define FIXED_POSITION_CREATES_STACKING_CONTEXT 1
 #endif
 
