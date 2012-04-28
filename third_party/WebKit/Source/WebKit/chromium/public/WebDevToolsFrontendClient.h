8ceebbac8ad90a02e1653972050582b835ba8593
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-28 19:11:45 +0000

    Web Inspector: InspectorFrontendHost.append has to be implemented for saving heap snapshots. https://bugs.webkit.org/show_bug.cgi?id=85137
    We can save a file with help of InspectorFrontendHost.save method,
    but it is suitable only for relatively small portions of data and
    can't process the 6Gb heap snapshot.
    These methods just pass the url and content into embedder.
    
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * inspector/InspectorFrontendClient.h:
    (InspectorFrontendClient):
    * inspector/InspectorFrontendClientLocal.h:
    (WebCore::InspectorFrontendClientLocal::append):
    * inspector/InspectorFrontendHost.cpp:
    (WebCore::InspectorFrontendHost::append):
    (WebCore):
    * inspector/InspectorFrontendHost.h:
    (InspectorFrontendHost):
    * inspector/InspectorFrontendHost.idl:
    
    Source/WebKit/chromium:
    
    * public/WebDevToolsFrontendClient.h:
    (WebKit::WebDevToolsFrontendClient::append):
    * src/InspectorFrontendClientImpl.cpp:
    (WebKit::InspectorFrontendClientImpl::append):
    (WebKit):
    * src/InspectorFrontendClientImpl.h:
    (InspectorFrontendClientImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115577 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebDevToolsFrontendClient.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebDevToolsFrontendClient.h
@@ -52,6 +52,7 @@ public:
     virtual void moveWindowBy(const WebFloatPoint&) { }
     virtual void openInNewTab(const WebString& side) { }
     virtual void save(const WebString& url, const WebString& content, bool saveAs) { }
+    virtual void append(const WebString& url, const WebString& content) { }
 
 protected:
     virtual ~WebDevToolsFrontendClient() {}
