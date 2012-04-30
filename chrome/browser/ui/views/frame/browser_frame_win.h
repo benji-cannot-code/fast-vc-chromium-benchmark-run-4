5b58fba246f08748ce5115d036d8e5f2f3cd7dfe
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 21:23:40 +0000

    Add support initiating navigations/search requests in Chrome metro mode via a registered window message. This is done by overriding the WindowImpl::OnWndProc virtual function in the BrowserFrameWin class which subclasses the NativeWidgetWin instance for the browser frame.
    BUG=118641
    R=sky,cpu
    Review URL: https://chromiumcodereview.appspot.com/10275002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134602 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame_win.h
+++ chrome/browser/ui/views/frame/browser_frame_win.h
@@ -62,6 +62,11 @@ class BrowserFrameWin : public views::NativeWidgetWin,
   virtual int GetMinimizeButtonOffset() const OVERRIDE;
   virtual void TabStripDisplayModeChanged() OVERRIDE;
 
+  // Overridden from WindowImpl:
+  virtual LRESULT OnWndProc(UINT message,
+                            WPARAM w_param,
+                            LPARAM l_param) OVERRIDE;
+
  private:
   // Updates the DWM with the frame bounds.
   void UpdateDWMFrame();
@@ -72,6 +77,9 @@ class BrowserFrameWin : public views::NativeWidgetWin,
 
   // Adds optional debug items for frame type toggling.
   void AddFrameToggleItems();
+
+  // Handles metro navigation search requests.
+  void HandleMetroRequest(WPARAM w_param, LPARAM l_param);
 
   // The BrowserView is our ClientView. This is a pointer to it.
   BrowserView* browser_view_;
