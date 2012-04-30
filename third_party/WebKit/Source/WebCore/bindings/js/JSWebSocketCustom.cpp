63a728ac63c08b70c10e78352e2584145e32c3e0
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-30 21:37:14 +0000

    [V8][JSC] Remove hard-coded "Not enough arguments" errors https://bugs.webkit.org/show_bug.cgi?id=85207
    Reviewed by Sam Weinig.
    
    In bug 85022 and bug 85097, we implemented
    createNotEnoughArgumentsError() in JSC and
    V8Proxy::throwNotEnoughArgumentsError() in V8 and partially
    removed hard-coded "Not enough arguments" errors.
    This patch removes hard-coded "Not enough arguments"
    errors by using the helper methods.
    
    No tests. No change in behavior.
    
    * bindings/js/JSAudioContextCustom.cpp:
    (WebCore::JSAudioContextConstructor::constructJSAudioContext):
    * bindings/js/JSSVGLengthCustom.cpp:
    (WebCore::JSSVGLength::convertToSpecifiedUnits):
    * bindings/js/JSWebSocketCustom.cpp:
    (WebCore::JSWebSocketConstructor::constructJSWebSocket):
    (WebCore::JSWebSocket::send):
    * bindings/js/JSXMLHttpRequestCustom.cpp:
    (WebCore::JSXMLHttpRequest::open):
    * bindings/v8/ScriptController.cpp:
    (WebCore::setValueAndClosePopupCallback):
    * bindings/v8/custom/V8AudioContextCustom.cpp:
    (WebCore::V8AudioContext::constructorCallback):
    * bindings/v8/custom/V8SVGLengthCustom.cpp:
    (WebCore::V8SVGLength::convertToSpecifiedUnitsCallback):
    * bindings/v8/custom/V8WebSocketCustom.cpp:
    (WebCore::V8WebSocket::constructorCallback):
    (WebCore::V8WebSocket::sendCallback):
    * bindings/v8/custom/V8XMLHttpRequestCustom.cpp:
    (WebCore::V8XMLHttpRequest::openCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSWebSocketCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSWebSocketCustom.cpp
@@ -59,7 +59,7 @@ EncodedJSValue JSC_HOST_CALL JSWebSocketConstructor::constructJSWebSocket(ExecSt
         return throwVMError(exec, createReferenceError(exec, "WebSocket constructor associated document is unavailable"));
 
     if (!exec->argumentCount())
-        return throwVMError(exec, createTypeError(exec, "Not enough arguments"));
+        return throwVMError(exec, createNotEnoughArgumentsError(exec));
 
     String urlString = ustringToString(exec->argument(0).toString(exec)->value(exec));
     if (exec->hadException())
@@ -94,7 +94,7 @@ EncodedJSValue JSC_HOST_CALL JSWebSocketConstructor::constructJSWebSocket(ExecSt
 JSValue JSWebSocket::send(ExecState* exec)
 {
     if (!exec->argumentCount())
-        return throwError(exec, createTypeError(exec, "Not enough arguments"));
+        return throwError(exec, createNotEnoughArgumentsError(exec));
 
     JSValue message = exec->argument(0);
     ExceptionCode ec = 0;
