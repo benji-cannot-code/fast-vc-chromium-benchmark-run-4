bb6280bf4093f53f8411621a8e68dea98a3c3998
yi.4.shen@nokia.com <yi.4.shen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-30 13:51:18 +0000

    Inserting a paragraph between quoted lines in editing/deleting/delete-4038408-fix.html doesn't work https://bugs.webkit.org/show_bug.cgi?id=78193
    Reviewed by Ryosuke Niwa.
    
    When pasting a copied portion of a blockquote with a newline at the end into an unquoted area,
    the newline is inserted after the blockquote since we don't want it also to be quoted. However,
    this behavior has also applied when we insert a paragraph between quoted lines, which is incorrect.
    To figure out the right place to insert a paragraph, we need providing more information to the
    InsertParagraphSeparatorCommand by introducing a boolean parameter "pasteBlockqutoeIntoUnquotedArea".
    
    Source/WebCore:
    
    Tests: editing/inserting/insert-paragraph-separator-in-blockquote.html
           editing/pasteboard/paste-wrapped-blockquote-into-nonblockquote.html
    
    * editing/CompositeEditCommand.cpp:
    (WebCore::CompositeEditCommand::insertParagraphSeparator):
    * editing/CompositeEditCommand.h:
    (CompositeEditCommand):
    * editing/InsertParagraphSeparatorCommand.cpp:
    (WebCore::InsertParagraphSeparatorCommand::InsertParagraphSeparatorCommand):
    (WebCore::InsertParagraphSeparatorCommand::doApply):
    * editing/InsertParagraphSeparatorCommand.h:
    (WebCore::InsertParagraphSeparatorCommand::create):
    (InsertParagraphSeparatorCommand):
    * editing/ReplaceSelectionCommand.cpp:
    (WebCore::ReplaceSelectionCommand::doApply):
    
    LayoutTests:
    
    * editing/inserting/insert-paragraph-separator-in-blockquote-expected.txt: Added.
    * editing/inserting/insert-paragraph-separator-in-blockquote.html: Added.
    * editing/pasteboard/paste-wrapped-blockquote-into-nonblockquote-expected.txt: Added.
    * editing/pasteboard/paste-wrapped-blockquote-into-nonblockquote.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/InsertParagraphSeparatorCommand.h
+++ third_party/WebKit/Source/WebCore/editing/InsertParagraphSeparatorCommand.h
@@ -35,13 +35,13 @@ class EditingStyle;
 
 class InsertParagraphSeparatorCommand : public CompositeEditCommand {
 public:
-    static PassRefPtr<InsertParagraphSeparatorCommand> create(Document* document, bool useDefaultParagraphElement = false)
+    static PassRefPtr<InsertParagraphSeparatorCommand> create(Document* document, bool useDefaultParagraphElement = false, bool pasteBlockqutoeIntoUnquotedArea = false)
     {
-        return adoptRef(new InsertParagraphSeparatorCommand(document, useDefaultParagraphElement));
+        return adoptRef(new InsertParagraphSeparatorCommand(document, useDefaultParagraphElement, pasteBlockqutoeIntoUnquotedArea));
     }
 
 private:
-    InsertParagraphSeparatorCommand(Document*, bool useDefaultParagraphElement);
+    InsertParagraphSeparatorCommand(Document*, bool useDefaultParagraphElement, bool pasteBlockqutoeIntoUnquotedArea);
 
     virtual void doApply();
 
@@ -57,6 +57,7 @@ private:
     RefPtr<EditingStyle> m_style;
 
     bool m_mustUseDefaultParagraphElement;
+    bool m_pasteBlockqutoeIntoUnquotedArea;
 };
 
 }
