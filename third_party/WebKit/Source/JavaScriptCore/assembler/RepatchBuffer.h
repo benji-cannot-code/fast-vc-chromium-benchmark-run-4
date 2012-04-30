b8db51eabe646f0223f2fe6f2be1e1b62c3ece24
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-04-30 23:34:20 +0000

    Should be able to build YARR JIT without the JS language JIT https://bugs.webkit.org/show_bug.cgi?id=85242
    Reviewed by Michael Saboff.
    
    Some build macros are wrong.
    
    * assembler/RepatchBuffer.h:
    * jit/ExecutableAllocator.h:
    (JSC):
    * jit/JITExceptions.cpp:
    * runtime/InitializeThreading.cpp:
    (JSC::initializeThreadingOnce):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/RepatchBuffer.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/RepatchBuffer.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef RepatchBuffer_h
 #define RepatchBuffer_h
 
-#if ENABLE(ASSEMBLER)
+#if ENABLE(JIT)
 
 #include "CodeBlock.h"
 #include <MacroAssembler.h>
