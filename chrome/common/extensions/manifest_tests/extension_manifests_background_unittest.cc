859659e04acef9f5437617891f11695480e4eb83
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 21:04:51 +0000

    Allow features to refer to subkeys in _manifest_features.json.
    Manifest restriction violations are now warnings instead of fatal errors.
    
    Add background.persistent subkey and restrict it to dev mode.
    
    BUG=122459
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/10217017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134596 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/manifest_tests/extension_manifests_background_unittest.cc
+++ chrome/common/extensions/manifest_tests/extension_manifests_background_unittest.cc
@@ -10,9 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_error_utils.h"
 #include "chrome/common/extensions/extension_manifest_constants.h"
+#include "chrome/common/extensions/simple_feature_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace errors = extension_manifest_errors;
+
+namespace extensions {
 
 TEST_F(ExtensionManifestTest, BackgroundPermission) {
   LoadAndExpectError("background_permission.json",
@@ -62,11 +65,13 @@ TEST_F(ExtensionManifestTest, BackgroundPage) {
   EXPECT_EQ("/foo.html", extension->GetBackgroundURL().path());
 
   manifest->SetInteger(keys::kManifestVersion, 2);
-  LoadAndExpectError(
-      Manifest(manifest.get(), ""),
-      ExtensionErrorUtils::FormatErrorMessage(
-          errors::kFeatureNotAllowed, "background_page"),
-      Extension::INTERNAL, Extension::STRICT_ERROR_CHECKS);
+  Feature* feature = SimpleFeatureProvider::GetManifestFeatures()->
+      GetFeature("background_page");
+  extension = LoadAndExpectSuccess(Manifest(manifest.get(), ""));
+  ASSERT_TRUE(extension);
+  ASSERT_EQ(1u, extension->install_warnings().size());
+  EXPECT_EQ(feature->GetErrorMessage(Feature::INVALID_MAX_MANIFEST_VERSION),
+            extension->install_warnings()[0]);
 }
 
 TEST_F(ExtensionManifestTest, BackgroundAllowNoJsAccess) {
@@ -79,3 +84,5 @@ TEST_F(ExtensionManifestTest, BackgroundAllowNoJsAccess) {
   ASSERT_TRUE(extension);
   EXPECT_FALSE(extension->allow_background_js_access());
 }
+
+}  // namespace extensions
