cc934eccdf0bc944a0f54191c38d54c4a69a5c92
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 23:51:04 +0000

    Revert 134625 - Transmit a X-Chrome-UMA-Enabled bit to Google domains from clients that have UMA enabled. Causing startup crash on Linux chromeos bot.
    This requires a change to the ChromeNetworkDelegate where we feed the incognito state (a bool) into the object at creation time, so we can check that field when doing our header setting.
    
    BUG=123609
    TEST=With UMA enabled (not Chromium), visit www.google.com and ensure that the request header includes X-Chrome-UMA-Enabled with value "1". Ensure that disabling UMA also disables the transmission of this header entirely.  Also, ensure that unit_tests GoogleUtilTests all pass.
    TBR=ivankr@chromium.org
    
    Review URL: http://codereview.chromium.org/10108026
    
    TBR=stevet@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10264023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134641 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -161,6 +161,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
 
   scoped_ptr<ProfileParams> params(new ProfileParams);
   params->path = profile->GetPath();
+  params->is_incognito = profile->IsOffTheRecord();
   params->clear_local_state_on_exit =
       pref_service->GetBoolean(prefs::kClearSiteDataOnExit);
 
@@ -247,7 +248,8 @@ void ProfileIOData::AppRequestContext::SetHttpTransactionFactory(
 ProfileIOData::AppRequestContext::~AppRequestContext() {}
 
 ProfileIOData::ProfileParams::ProfileParams()
-    : clear_local_state_on_exit(false),
+    : is_incognito(false),
+      clear_local_state_on_exit(false),
       io_thread(NULL),
 #if defined(ENABLE_NOTIFICATIONS)
       notification_service(NULL),
@@ -261,8 +263,7 @@ ProfileIOData::ProfileIOData(bool is_incognito)
     : initialized_(false),
       ALLOW_THIS_IN_INITIALIZER_LIST(
           resource_context_(new ResourceContext(this))),
-      initialized_on_UI_thread_(false),
-      is_incognito_(is_incognito) {
+      initialized_on_UI_thread_(false) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 }
 
@@ -465,7 +466,7 @@ void ProfileIOData::LazyInitialize() const {
   transport_security_persister_.reset(
       new TransportSecurityPersister(transport_security_state_.get(),
                                      profile_params_->path,
-                                     is_incognito()));
+                                     profile_params_->is_incognito));
 
   // NOTE(willchan): Keep these protocol handlers in sync with
   // ProfileIOData::IsHandledProtocol().
@@ -477,7 +478,7 @@ void ProfileIOData::LazyInitialize() const {
   }
   bool set_protocol = job_factory_->SetProtocolHandler(
       chrome::kExtensionScheme,
-      CreateExtensionProtocolHandler(is_incognito(),
+      CreateExtensionProtocolHandler(profile_params_->is_incognito,
                                      profile_params_->extension_info_map));
   DCHECK(set_protocol);
   set_protocol = job_factory_->SetProtocolHandler(
@@ -515,7 +516,7 @@ void ProfileIOData::LazyInitialize() const {
 
 void ProfileIOData::ApplyProfileParamsToContext(
     ChromeURLRequestContext* context) const {
-  context->set_is_incognito(is_incognito());
+  context->set_is_incognito(profile_params_->is_incognito);
   context->set_accept_language(profile_params_->accept_language);
   context->set_accept_charset(profile_params_->accept_charset);
   context->set_referrer_charset(profile_params_->referrer_charset);
@@ -525,7 +526,6 @@ void ProfileIOData::ApplyProfileParamsToContext(
 void ProfileIOData::ShutdownOnUIThread() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   enable_referrers_.Destroy();
-  enable_metrics_.Destroy();
   clear_local_state_on_exit_.Destroy();
   safe_browsing_enabled_.Destroy();
   session_startup_pref_.Destroy();
