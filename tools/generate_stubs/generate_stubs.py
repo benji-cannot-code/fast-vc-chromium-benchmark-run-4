5f3b4fdc2a150c5b7cd56986416048cb910794ce
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 19:51:27 +0000

    avoid race between dir existence check and dir creation
    On clean builds, if there's two instances running at the same time makedirs occasionally raises and kills the build.
    
    R=ajwong@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10051026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134571 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/generate_stubs/generate_stubs.py
+++ tools/generate_stubs/generate_stubs.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -925,6 +925,18 @@ def ParseOptions():
   return options, args
 
 
+def EnsureDirExists(dir):
+  """Creates a directory. Does not use the more obvious 'if not exists: create'
+  to avoid race with other invocations of the same code, which will error out
+  on makedirs if another invocation has succeeded in creating the directory
+  since the existence check."""
+  try:
+    os.makedirs(dir)
+  except:
+    if not os.path.isdir(dir):
+      raise
+
+
 def CreateOutputDirectories(options):
   """Creates the intermediate and final output directories.
 
@@ -943,10 +955,8 @@ def CreateOutputDirectories(options):
   if intermediate_dir is None:
     intermediate_dir = out_dir
 
-  if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
-  if not os.path.exists(intermediate_dir):
-    os.makedirs(intermediate_dir)
+  EnsureDirExists(out_dir)
+  EnsureDirExists(intermediate_dir)
 
   return out_dir, intermediate_dir
 
