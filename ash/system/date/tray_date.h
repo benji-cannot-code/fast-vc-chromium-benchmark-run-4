50af04bbd24fb79ce5b4676287df65441fad279b
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 20:25:44 +0000

    ash: Add vertical spacing to multi-line text in tray.
    This breaks the multi-line date/day-of-week and battery text
    in the uber system tray across multiple labels so we can add
    a bit more spacing between the lines.
    
    A side effect (maybe a slight improvement?) is that some of
    the longer strings (e.g.  "Calculating remaining time") now
    get elided instead of getting wrapped and clipped.
    
    Since I needed to change the battery-related strings anyway,
    I'm also splitting the "2h 23m"-style string into
    "2h 23m remaining" and "2h 23m until full".
    
    BUG=124664,124074
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10209038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134585 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/date/tray_date.h
+++ ash/system/date/tray_date.h
@@ -14,7 +14,7 @@ namespace ash {
 namespace internal {
 
 namespace tray {
-class DateView;
+class TimeView;
 }
 
 class TrayDate : public SystemTrayItem,
@@ -37,7 +37,7 @@ class TrayDate : public SystemTrayItem,
   virtual void OnDateFormatChanged() OVERRIDE;
   virtual void Refresh() OVERRIDE;
 
-  scoped_ptr<tray::DateView> date_tray_;
+  scoped_ptr<tray::TimeView> time_tray_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayDate);
 };
