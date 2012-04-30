0b88743e4b284db17dd88c007b40aba9219b4857
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 18:42:18 +0000

    Remove panel size limit when user resizes it.
    BUG=123669
    TEST=existing panel resize tests, modified
    
    Review URL: https://chromiumcodereview.appspot.com/10260001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134559 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/detached_panel_strip.cc
+++ chrome/browser/ui/panels/detached_panel_strip.cc
@@ -78,7 +78,7 @@ void DetachedPanelStrip::ResizePanelWindow(
   // Make sure the new size does not violate panel's size restrictions.
   gfx::Size new_size(preferred_window_size.width(),
                      preferred_window_size.height());
-  panel->ClampSize(&new_size);
+  new_size = panel->ClampSize(new_size);
 
   // Update restored size.
   if (new_size != panel->full_size())
