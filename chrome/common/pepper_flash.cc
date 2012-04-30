e12aac358dbbfae0de2fb49584ba6ef4636347ae
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-30 16:57:19 +0000

    Change the name of the Pepper Flash field trial. This will change its position in the --force-fieldtrials value, moving it to the front.
    Note:
    - For channels that are not running the field trial (e.g., Canary or Beta), --force-fieldtrials won't show ApplyPepperFlash at all.
    - The value of ApplyPepperFlash (Enable/DisableByDefault) doesn't necessarily reflect whether Pepper Flash is in use. The user may mannually change the settings.
    
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/10254022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pepper_flash.cc
+++ chrome/common/pepper_flash.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char* const kFieldTrialName = "PepperFlash";
+const char* const kFieldTrialName = "ApplyPepperFlash";
 const char* const kDisableGroupName = "DisableByDefault";
 const char* const kEnableGroupName = "EnableByDefault";
 int g_disabled_group_number = -1;
