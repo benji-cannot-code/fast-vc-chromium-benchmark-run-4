0c267c9af8e9f87562d47ed6f5f84e9f3926580e
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 03:56:29 +0000

    Add notifyClientDimensions API to the Chromoting Client plugin.
    BUG=110212
    TEST=Will be testable via subsequent API-consumer CL.
    
    
    Review URL: http://codereview.chromium.org/10262019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134671 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/connection_to_host.h
+++ remoting/protocol/connection_to_host.h
@@ -84,6 +84,7 @@ class ConnectionToHost : public SignalStrategy::Listener,
   virtual const SessionConfig& config();
 
   virtual ClipboardStub* clipboard_stub();
+  virtual HostStub* host_stub();
   virtual InputStub* input_stub();
 
   // SignalStrategy::StatusObserver interface.
