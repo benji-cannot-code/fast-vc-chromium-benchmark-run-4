485f2a3213052e9911fd0ad91c95e2b5090a8597
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 20:59:18 +0000

    Created uber tray UI for monitoring ongoing WebDrive sync operations
    BUG=chromium-os:30075
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10214013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134774 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/bluetooth/tray_bluetooth.h"
 #include "ash/system/brightness/tray_brightness.h"
 #include "ash/system/date/tray_date.h"
+#include "ash/system/drive/tray_drive.h"
 #include "ash/system/ime/tray_ime.h"
 #include "ash/system/network/tray_network.h"
 #include "ash/system/power/power_status_observer.h"
@@ -134,6 +135,10 @@ class TrayPopupItemContainer : public views::View {
     if (visible() == child->visible())
       return;
     SetVisible(child->visible());
+    PreferredSizeChanged();
+  }
+
+  virtual void ChildPreferredSizeChanged(View* child) OVERRIDE {
     PreferredSizeChanged();
   }
 
@@ -606,37 +611,6 @@ void SystemTrayBubble::OnWidgetVisibilityChanged(views::Widget* widget,
 
 }  // namespace internal
 
-// From system_tray_delegate.h
-
-NetworkIconInfo::NetworkIconInfo()
-    : highlight(false),
-      tray_icon_visible(true) {
-}
-
-NetworkIconInfo::~NetworkIconInfo() {
-}
-
-BluetoothDeviceInfo::BluetoothDeviceInfo()
-    : connected(false) {
-}
-
-BluetoothDeviceInfo::~BluetoothDeviceInfo() {
-}
-
-IMEInfo::IMEInfo()
-    : selected(false) {
-}
-
-IMEInfo::~IMEInfo() {
-}
-
-IMEPropertyInfo::IMEPropertyInfo()
-    : selected(false) {
-}
-
-IMEPropertyInfo::~IMEPropertyInfo() {
-}
-
 // SystemTray
 
 SystemTray::SystemTray()
@@ -647,6 +621,7 @@ SystemTray::SystemTray()
       brightness_observer_(NULL),
       caps_lock_observer_(NULL),
       clock_observer_(NULL),
+      drive_observer_(NULL),
       ime_observer_(NULL),
       network_observer_(NULL),
       power_status_observer_(NULL),
@@ -697,6 +672,7 @@ void SystemTray::CreateItems() {
   internal::TrayAccessibility* tray_accessibility =
       new internal::TrayAccessibility;
   internal::TrayCapsLock* tray_caps_lock = new internal::TrayCapsLock;
+  internal::TrayDrive* tray_drive = new internal::TrayDrive;
   internal::TrayIME* tray_ime = new internal::TrayIME;
   internal::TrayUpdate* tray_update = new internal::TrayUpdate;
 
@@ -706,6 +682,7 @@ void SystemTray::CreateItems() {
   brightness_observer_ = tray_brightness;
   caps_lock_observer_ = tray_caps_lock;
   clock_observer_ = tray_date;
+  drive_observer_ = tray_drive;
   ime_observer_ = tray_ime;
   network_observer_ = tray_network;
   power_status_observer_ = tray_power;
@@ -717,6 +694,7 @@ void SystemTray::CreateItems() {
   AddTrayItem(tray_power);
   AddTrayItem(tray_network);
   AddTrayItem(tray_bluetooth);
+  AddTrayItem(tray_drive);
   AddTrayItem(tray_ime);
   AddTrayItem(tray_volume);
   AddTrayItem(tray_brightness);
