6f6308d7bcaa3cebc2a2992c2f26398ee08aef33
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 13:00:45 +0000

    Revert r134673 ""View website" instead of "View in Web Store""
    It broke multiple browser_tests like
    ExtensionWebstorePrivateApiTest.InstallAccepted and
    ExtensionNoConfirmWebstorePrivateApiTest.InstallTheme on multiple OSes.
    
    TBR=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10274026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -66,7 +66,6 @@ namespace extension_filenames {
 namespace extension_info_keys {
   extern const char kDescriptionKey[];
   extern const char kEnabledKey[];
-  extern const char kHomepageProvidedKey[];
   extern const char kHomepageUrlKey[];
   extern const char kIdKey[];
   extern const char kMayDisableKey[];
