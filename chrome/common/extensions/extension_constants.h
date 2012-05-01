891f9df8cb052c6b81516bd364dba4963e0f8ea0
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 03:57:13 +0000

    "View website" instead of "View in Web Store" for extensions that have a homepage url
    BUG=105858
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10103015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_constants.h
+++ chrome/common/extensions/extension_constants.h
@@ -66,6 +66,7 @@ namespace extension_filenames {
 namespace extension_info_keys {
   extern const char kDescriptionKey[];
   extern const char kEnabledKey[];
+  extern const char kHomepageProvidedKey[];
   extern const char kHomepageUrlKey[];
   extern const char kIdKey[];
   extern const char kMayDisableKey[];
