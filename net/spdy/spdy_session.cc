c8f29d5cb85438c42bcdf817bb6f8e68f09b12f3
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 19:08:54 +0000

    Change a DCHECK into a CHECK in SpdySession::WriteStreamData to ensure that we're not attempting to write data on a stream that is not active. Attempting to track down a crash willchan noticed.
    Review URL: http://codereview.chromium.org/10267021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134744 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -753,10 +753,8 @@ int SpdySession::WriteStreamData(SpdyStreamId stream_id,
                                  net::IOBuffer* data, int len,
                                  SpdyDataFlags flags) {
   // Find our stream
-  DCHECK(IsStreamActive(stream_id));
+  CHECK(IsStreamActive(stream_id));
   scoped_refptr<SpdyStream> stream = active_streams_[stream_id];
-  if (!stream)
-    return ERR_INVALID_SPDY_STREAM;
   CHECK_EQ(stream->stream_id(), stream_id);
 
   if (len > kMaxSpdyFrameChunkSize) {
@@ -1509,8 +1507,7 @@ void SpdySession::OnSynReply(const SpdySynReplyControlFrame& frame,
             stream_id, 0)));
   }
 
-  bool valid_stream = IsStreamActive(stream_id);
-  if (!valid_stream) {
+  if (!IsStreamActive(stream_id)) {
     // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received SYN_REPLY for invalid stream " << stream_id;
     return;
@@ -1544,8 +1541,7 @@ void SpdySession::OnHeaders(const SpdyHeadersControlFrame& frame,
             stream_id, 0)));
   }
 
-  bool valid_stream = IsStreamActive(stream_id);
-  if (!valid_stream) {
+  if (!IsStreamActive(stream_id)) {
     // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received HEADERS for invalid stream " << stream_id;
     return;
@@ -1571,8 +1567,7 @@ void SpdySession::OnRstStream(const SpdyRstStreamControlFrame& frame) {
       make_scoped_refptr(
           new NetLogSpdyRstParameter(stream_id, frame.status(), "")));
 
-  bool valid_stream = IsStreamActive(stream_id);
-  if (!valid_stream) {
+  if (!IsStreamActive(stream_id)) {
     // NOTE:  it may just be that the stream was cancelled.
     LOG(WARNING) << "Received RST for invalid stream" << stream_id;
     return;
@@ -1680,7 +1675,7 @@ void SpdySession::OnWindowUpdate(
 
 void SpdySession::SendWindowUpdate(SpdyStreamId stream_id,
                                    int32 delta_window_size) {
-  DCHECK(IsStreamActive(stream_id));
+  CHECK(IsStreamActive(stream_id));
   scoped_refptr<SpdyStream> stream = active_streams_[stream_id];
   CHECK_EQ(stream->stream_id(), stream_id);
 
