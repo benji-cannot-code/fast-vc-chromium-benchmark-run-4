514a688d7a9e040ebe9ac232eb9e3f62704b833b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 17:25:00 +0000

    browser: Remove window.{cc,h} module completely.
    R=ben@chromium.org
    TBR=stevenjb@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10271002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/ui/setting_level_bubble.cc
+++ chrome/browser/chromeos/ui/setting_level_bubble.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/views/window.h"
 #include "ui/gfx/screen.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/layout/fill_layout.h"
