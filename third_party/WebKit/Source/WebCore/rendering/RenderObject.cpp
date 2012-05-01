d65605ada2acccb2b497cf7c8807a12d99fe0611
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-01 02:15:51 +0000

    RenderObject incorrectly lists maximalOutlineSize as a LayoutUnit https://bugs.webkit.org/show_bug.cgi?id=85248
    Reviewed by Eric Seidel.
    
    Reverting RenderObject::maximalOutlineSize to int. This is a slop value for repaint
    rects that doesn't affect layout. It also derives its value from RenderView's function
    of the same name, which is already an integer.
    
    No new tests. No change in behavior.
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::maximalOutlineSize):
    * rendering/RenderObject.h:
    (RenderObject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -2692,7 +2692,7 @@ bool RenderObject::willRenderImage(CachedImage*)
     return !document()->inPageCache() && !document()->view()->isOffscreen();
 }
 
-LayoutUnit RenderObject::maximalOutlineSize(PaintPhase p) const
+int RenderObject::maximalOutlineSize(PaintPhase p) const
 {
     if (p != PaintPhaseOutline && p != PaintPhaseSelfOutline && p != PaintPhaseChildOutlines)
         return 0;
