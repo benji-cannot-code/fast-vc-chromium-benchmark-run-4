a12ab411142660c3c2e6ca86b6bb58862590672e
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-01 00:06:37 +0000

    Add absoluteValue method for LayoutUnits to allow overloading abs() https://bugs.webkit.org/show_bug.cgi?id=85214
    Reviewed by Eric Seidel.
    
    Adding an absoluteValue free inline function that operates on LayoutUnits, which
    allows us to have one function signature for ints or FractionalLayoutUnits. We
    can't simply add a FractionalLayoutUnit flavor of abs because it confuses
    some compilers due to the implicit FractionalLayoutUnit constructors that take
    ints and floats.
    
    No new tests. No change in behavior.
    
    * page/SpatialNavigation.cpp:
    (WebCore::distanceDataForNode):
    * rendering/LayoutTypes.h:
    (WebCore::absoluteValue):
    (WebCore):
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::checkPaginationAndFloatsAtEndLine):
    * rendering/RenderLineBoxList.cpp:
    (WebCore::RenderLineBoxList::rangeIntersectsRect):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::repaintAfterLayoutIfNeeded):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115691 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -1430,7 +1430,7 @@ bool RenderObject::repaintAfterLayoutIfNeeded(RenderBoxModelObject* repaintConta
     // two rectangles (but typically only one).
     RenderStyle* outlineStyle = outlineStyleForRepaint();
     LayoutUnit ow = outlineStyle->outlineSize();
-    LayoutUnit width = abs(newOutlineBox.width() - oldOutlineBox.width());
+    LayoutUnit width = absoluteValue(newOutlineBox.width() - oldOutlineBox.width());
     if (width) {
         LayoutUnit shadowLeft;
         LayoutUnit shadowRight;
@@ -1449,7 +1449,7 @@ bool RenderObject::repaintAfterLayoutIfNeeded(RenderBoxModelObject* repaintConta
             repaintUsingContainer(repaintContainer, rightRect);
         }
     }
-    LayoutUnit height = abs(newOutlineBox.height() - oldOutlineBox.height());
+    LayoutUnit height = absoluteValue(newOutlineBox.height() - oldOutlineBox.height());
     if (height) {
         LayoutUnit shadowTop;
         LayoutUnit shadowBottom;
