e2020e1618e9ee0f7b6a29676d7896ddaaaeef30
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 22:21:06 +0000

    GpuMemoryManager suggests values for renderer Contents Texture Managers' preferred memory limit.
    BUG=123382
    TEST=Manual
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=134428
    
    Review URL: http://codereview.chromium.org/10083056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134801 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.h
+++ content/common/gpu/gpu_command_buffer_stub.h
@@ -64,6 +64,7 @@ class CONTENT_EXPORT GpuCommandBufferStubBase {
   virtual ~GpuCommandBufferStubBase() {}
 
   // Will not have surface state if this is an offscreen commandbuffer.
+  virtual bool client_has_memory_allocation_changed_callback() const = 0;
   virtual bool has_surface_state() const = 0;
   virtual const SurfaceState& surface_state() const = 0;
 
@@ -115,6 +116,7 @@ class GpuCommandBufferStub
   virtual bool Send(IPC::Message* msg) OVERRIDE;
 
   // GpuCommandBufferStubBase implementation:
+  virtual bool client_has_memory_allocation_changed_callback() const OVERRIDE;
   virtual bool has_surface_state() const OVERRIDE;
   virtual const GpuCommandBufferStubBase::SurfaceState& surface_state() const
       OVERRIDE;
@@ -206,6 +208,8 @@ class GpuCommandBufferStub
   void OnDiscardBackbuffer();
   void OnEnsureBackbuffer();
 
+  void OnSetClientHasMemoryAllocationChangedCallback(bool);
+
   void OnReschedule();
 
   void OnCommandProcessed();
@@ -229,6 +233,7 @@ class GpuCommandBufferStub
   gfx::GpuPreference gpu_preference_;
   int32 route_id_;
   bool software_;
+  bool client_has_memory_allocation_changed_callback_;
   uint32 last_flush_count_;
   scoped_ptr<GpuCommandBufferStubBase::SurfaceState> surface_state_;
   GpuMemoryAllocation allocation_;
