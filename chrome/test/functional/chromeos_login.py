d161fabd1fcc2e28a0ce0eb46dd725d257d19813
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 02:07:54 +0000

    Added a new automation hook ExecuteJavascriptInOOBEWebUI() to permit executing abitrary javascript prior to login on chromeos.
    Change-Id: If96d94bb22ac5bd25fa4ab1e3a4c3ba52ea47370
    
    BUG=chromium-os:29466,chromium-os:20709
    TEST=functional/chromeos_login.py (testExecuteJavascriptInOOBEWebUI)
    
    Review URL: http://codereview.chromium.org/10257015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/chromeos_login.py
+++ chrome/test/functional/chromeos_login.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -31,6 +31,13 @@ class ChromeosLogin(pyauto.PyUITest):
       A dictionary with the keys 'username' and 'password'
     """
     return self.GetPrivateInfo()[account_type]
+
+  def testExecuteJavascriptInOOBEWebUI(self):
+    """Test that javascript can be executed at the login page."""
+    msg = 'test success'
+    ret = self.ExecuteJavascriptInOOBEWebUI(
+              'window.domAutomationController.send("%s");' % msg)
+    self.assertEqual(ret, msg)
 
   def testGoodLogin(self):
     """Test that login is successful with valid credentials."""
