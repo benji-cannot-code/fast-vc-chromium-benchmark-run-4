edd223f4ecb95f66f072229ded4b5acea7d35c37
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-01 22:04:44 +0000

    [Mac] Give info bubble windows a gaussian blur and a smaller border radius.
    BUG=none
    TEST=Visual. Bubbles look more up-to-date on later OSes.
    
    
    Review URL: http://codereview.chromium.org/10272035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/info_bubble_view.h
+++ chrome/browser/ui/cocoa/info_bubble_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -13,7 +13,7 @@ namespace info_bubble {
 
 const CGFloat kBubbleArrowHeight = 8.0;
 const CGFloat kBubbleArrowWidth = 15.0;
-const CGFloat kBubbleCornerRadius = 8.0;
+const CGFloat kBubbleCornerRadius = 2.0;
 const CGFloat kBubbleArrowXOffset = kBubbleArrowWidth + kBubbleCornerRadius;
 
 enum BubbleArrowLocation {
