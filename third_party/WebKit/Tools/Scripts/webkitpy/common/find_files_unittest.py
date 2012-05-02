0d2e486ad182889335d30398dba5d95b2bdea7f0
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-02 15:13:35 +0000

    webkitpy: Fix sys.platform check in find_files_unittest. https://bugs.webkit.org/show_bug.cgi?id=85339
    Reviewed by Ryosuke Niwa.
    
    Follow-up to r101618 -- 'win' is not a valid sys.platform value,
    'win32' should be used instead. The check always returned false
    due to this, so assert_normalize() was never called.
    
    * Scripts/webkitpy/common/find_files_unittest.py:
    (TestWinNormalize.test_win): Add the missing import for the
    FileSystem class and fix the sys.platform check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/find_files_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/find_files_unittest.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import sys
 import unittest
 
+from webkitpy.common.system.filesystem import FileSystem
 import find_files
 
 
@@ -56,7 +57,7 @@ class TestWinNormalize(unittest.TestCase):
     def test_win(self):
         # This tests the actual windows platform, to ensure we get the same
         # results that we get in test_mocked_win().
-        if sys.platform != 'win':
+        if sys.platform != 'win32':
             return
         self.assert_filesystem_normalizes(FileSystem())
 
