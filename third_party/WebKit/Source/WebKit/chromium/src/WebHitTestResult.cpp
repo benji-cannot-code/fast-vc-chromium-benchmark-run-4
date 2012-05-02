1c483ee903776af1cad9fc4e5059aa958d08efc4
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-02 20:29:39 +0000

    Fix usage of layout types in platform code https://bugs.webkit.org/show_bug.cgi?id=85392
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    No new tests, no change in functionality.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::handleGestureTap):
    Use rounded point for gestures as event handling is still mostly int based.
    
    Source/WebKit/chromium:
    
    * src/WebHitTestResult.cpp:
    (WebKit::WebHitTestResult::localPoint):
    * src/WebSurroundingText.cpp:
    (WebKit::WebSurroundingText::initialize):
    Explicitly cast WebPoint to IntPoint to avoid ambiguous conversion error.
    
    Source/WebKit/efl:
    
    * ewk/ewk_frame.cpp:
    (ewk_frame_focused_element_geometry_get):
    * ewk/ewk_view.cpp:
    (_Ewk_View_Private_Data):
    (ewk_view_scale_set):
    
    Source/WebKit2:
    
    * WebProcess/WebCoreSupport/WebGraphicsLayer.cpp:
    (WebCore::WebGraphicsLayer::tiledBackingStoreVisibleRect):
    Use enclosingIntRect to convert visible rect for transform results to
    line up with device pixels.
    
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::editorState):
    Use pixelSnapped rect for editor rect calculation as it represents a
    device coordinate.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebHitTestResult.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebHitTestResult.cpp
@@ -48,7 +48,7 @@ WebNode WebHitTestResult::node() const
 
 WebPoint WebHitTestResult::localPoint() const
 {
-    return WebPoint(m_private->localPoint());
+    return roundedIntPoint(m_private->localPoint());
 }
 
 WebHitTestResult::WebHitTestResult(const HitTestResult& result)
