fa1c86915870f7fbcd90dab9543e727b1508cc3d
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 21:46:01 +0000

    Make the PIN confirmation dialog focused properly.
    BUG=125242
    
    
    Review URL: http://codereview.chromium.org/10269030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134997 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/daemon_controller_win.cc
+++ remoting/host/plugin/daemon_controller_win.cc
@@ -119,6 +119,7 @@ class DaemonControllerWin : public remoting::DaemonController {
   void DoSetWindow(void* window_handle);
 
   ScopedComPtr<IDaemonControl> control_;
+  ScopedComPtr<IDaemonControlUi> control_ui_;
 
   // This timer is used to release |control_| after a timeout.
   scoped_ptr<base::OneShotTimer<DaemonControllerWin> > release_timer_;
@@ -260,6 +261,9 @@ HRESULT DaemonControllerWin::ActivateElevatedController() {
                           base::TimeDelta::FromSeconds(kUacTimeoutSec),
                           this,
                           &DaemonControllerWin::ReleaseElevatedController);
+
+    // Ignore the error. IID_IDaemonControlUi is optional.
+    control_.QueryInterface(IID_IDaemonControlUi, control_ui_.ReceiveVoid());
   }
 
   return S_OK;
@@ -269,6 +273,7 @@ void DaemonControllerWin::ReleaseElevatedController() {
   DCHECK(worker_thread_.message_loop_proxy()->BelongsToCurrentThread());
 
   control_.Release();
+  control_ui_.Release();
   release_timer_.reset();
 }
 
@@ -430,7 +435,7 @@ void DaemonControllerWin::DoInstallAsNeededAndStart(
   // Otherwise, install it if its COM registration entry is missing.
   if (hr == CO_E_CLASSSTRING) {
     scoped_ptr<DaemonInstallerWin> installer = DaemonInstallerWin::Create(
-        window_handle_,
+        GetTopLevelWindow(window_handle_),
         base::Bind(&DaemonControllerWin::OnInstallationComplete,
                    base::Unretained(this),
                    base::Passed(&config),
@@ -466,6 +471,16 @@ void DaemonControllerWin::DoSetConfigAndStart(
     return;
   }
 
+  // Make sure that the PIN confirmation dialog is focused properly.
+  if (control_ui_.get() != NULL) {
+    hr = control_ui_->SetOwnerWindow(
+      reinterpret_cast<LONG_PTR>(GetTopLevelWindow(window_handle_)));
+    if (FAILED(hr)) {
+      done_callback.Run(HResultToAsyncResult(hr));
+      return;
+    }
+  }
+
   hr = control_->SetConfig(config_str);
   if (FAILED(hr)) {
     done_callback.Run(HResultToAsyncResult(hr));
@@ -494,6 +509,16 @@ void DaemonControllerWin::DoUpdateConfig(
   if (config_str == NULL) {
     done_callback.Run(HResultToAsyncResult(E_OUTOFMEMORY));
     return;
+  }
+
+  // Make sure that the PIN confirmation dialog is focused properly.
+  if (control_ui_.get() != NULL) {
+    hr = control_ui_->SetOwnerWindow(
+      reinterpret_cast<LONG_PTR>(GetTopLevelWindow(window_handle_)));
+    if (FAILED(hr)) {
+      done_callback.Run(HResultToAsyncResult(hr));
+      return;
+    }
   }
 
   hr = control_->UpdateConfig(config_str);
