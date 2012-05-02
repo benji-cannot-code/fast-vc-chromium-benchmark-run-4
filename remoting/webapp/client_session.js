e5e3098d235e1844c94bb08d1e310534576a006a
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 01:23:31 +0000

    Send new client dimensions to host whenever they change.
    This will be manually testable by checking for NotifyClientDimensions messages logged by hosts, once the host end logging is enabled.
    
    DO NOT COMMIT THIS UNTIL CL 10262035 HAS LANDED.
    
    BUG=110212
    
    Review URL: http://codereview.chromium.org/10265033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134846 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/client_session.js
+++ remoting/webapp/client_session.js
@@ -59,6 +59,10 @@ remoting.ClientSession = function(hostJid, hostPublicKey, sharedSecret,
   this.scaleToFit = false;
   this.logToServer = new remoting.LogToServer();
   this.onStateChange = onStateChange;
+
+  /** @type {number?} */
+  this.notifyClientDimensionsTimer_ = null;
+
   /** @type {remoting.ClientSession} */
   var that = this;
   /** @type {function():void} @private */
@@ -72,20 +76,22 @@ remoting.ClientSession = function(hostJid, hostPublicKey, sharedSecret,
   /** @type {function():void} @private */
   this.callToggleFullScreen_ = function() { that.toggleFullScreen_(); };
   /** @type {remoting.MenuButton} @private */
-  this.sendKeysMenu_ = new remoting.MenuButton(
-      document.getElementById('send-keys-menu')
-  );
-  /** @type {remoting.MenuButton} @private */
   this.screenOptionsMenu_ = new remoting.MenuButton(
       document.getElementById('screen-options-menu'),
       function() { that.onShowOptionsMenu_(); }
   );
+  /** @type {remoting.MenuButton} @private */
+  this.sendKeysMenu_ = new remoting.MenuButton(
+      document.getElementById('send-keys-menu')
+  );
+
   /** @type {HTMLElement} @private */
   this.shrinkToFit_ = document.getElementById('enable-shrink-to-fit');
   /** @type {HTMLElement} @private */
   this.originalSize_ = document.getElementById('disable-shrink-to-fit');
   /** @type {HTMLElement} @private */
   this.fullScreen_ = document.getElementById('toggle-full-screen');
+
   this.shrinkToFit_.addEventListener('click', this.callEnableShrink_, false);
   this.originalSize_.addEventListener('click', this.callDisableShrink_, false);
   this.fullScreen_.addEventListener('click', this.callToggleFullScreen_, false);
@@ -505,6 +511,21 @@ remoting.ClientSession.prototype.setState_ = function(newState) {
  */
 remoting.ClientSession.prototype.onResize = function() {
   this.updateDimensions();
+
+  if (this.notifyClientDimensionsTimer_) {
+    window.clearTimeout(this.notifyClientDimensionsTimer_);
+    this.notifyClientDimensionsTimer_ = null;
+  }
+
+  // Defer notifying the host of the change until the window stops resizing, to
+  // avoid overloading the control channel with notifications.
+  /** @type {remoting.ClientSession} */
+  var that = this;
+  var notifyClientDimensions = function() {
+    that.plugin.notifyClientDimensions(window.innerWidth, window.innerHeight);
+  }
+  this.notifyClientDimensionsTimer_ =
+      window.setTimeout(notifyClientDimensions, 1000);
 };
 
 /**
