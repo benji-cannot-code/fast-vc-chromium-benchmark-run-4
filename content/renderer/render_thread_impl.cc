83ff1289d2a7d1629affd8bdf87673a01505a420
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 09:17:47 +0000

    I am adding a new flag, --enable-peer-connection, that solely controls the availability of the WebKit PeerConnection classes. This flag is in addition to --enable-media-stream.
    We are doing this change to be able to have webkitGetUserMedia  available by default but not any PeerConnection related functionality.
    
    Bug=124993
    TEST=Without --enable-peer-connection (and --enable-media-stream) no PeerConnection class can be created in JS.
    
    Review URL: https://chromiumcodereview.appspot.com/10169045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134883 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -531,6 +531,9 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
   WebKit::WebRuntimeFeatures::enableMediaStream(
       command_line.HasSwitch(switches::kEnableMediaStream));
 
+  WebKit::WebRuntimeFeatures::enablePeerConnection(
+      command_line.HasSwitch(switches::kEnablePeerConnection));
+
   WebKit::WebRuntimeFeatures::enableFullScreenAPI(
       !command_line.HasSwitch(switches::kDisableFullScreen));
 
