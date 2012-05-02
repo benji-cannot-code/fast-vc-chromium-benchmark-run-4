88d491c6623f94d4a6942b78cd5dc4948bb25b25
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 19:45:24 +0000

    Launch platform apps from shortcuts in the normal user_data_dir on Windows and Linux.
    This change reverts earlier changes to use a separate user_data_dir for
    platform apps.
    
    BUG=None
    TEST=Examined shortcut properties.
    
    
    Review URL: http://codereview.chromium.org/10296002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134969 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_linux.cc
+++ chrome/browser/web_applications/web_app_linux.cc
@@ -26,8 +26,8 @@ bool CreatePlatformShortcut(
                                                          &shortcut_template)) {
     return false;
   }
-  return ShellIntegrationLinux::CreateDesktopShortcutForChromeApp(
-      shortcut_info, web_app_path, shortcut_template);
+  return ShellIntegrationLinux::CreateDesktopShortcut(
+      shortcut_info, shortcut_template);
 }
 
 }  // namespace internals
