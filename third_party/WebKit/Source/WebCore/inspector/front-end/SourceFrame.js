96fbc2170cc90ca6ade53947308df2d1b0403d2c
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-02 11:56:34 +0000

    Web Inspector: live edit fails to report error https://bugs.webkit.org/show_bug.cgi?id=85357
    Reviewed by Yury Semikhatsky.
    
    ProtocolError is now a string, not an error object.
    
    * inspector/front-end/DatabaseQueryView.js:
    (WebInspector.DatabaseQueryView.prototype._queryError):
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame.prototype.didEditContent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -507,8 +507,7 @@ WebInspector.SourceFrame.prototype = {
     didEditContent: function(error, content)
     {
         if (error) {
-            if (error.message)
-                WebInspector.log(error.message, WebInspector.ConsoleMessage.MessageLevel.Error, true);
+            WebInspector.log(error, WebInspector.ConsoleMessage.MessageLevel.Error, true);
             return;
         }
     },
