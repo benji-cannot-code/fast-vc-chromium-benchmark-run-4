5372bb8b03579277f5775c33eae5e6a91b4595b1
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-02 23:26:50 +0000

    Remove unused adjustForAbsoluteZoom method in RenderObject.h https://bugs.webkit.org/show_bug.cgi?id=85396
    Reviewed by Eric Seidel.
    
    We only want to use the integer adjustForAbsoluteZoom method, so this remnant is both unused
    and potentially confusing.
    
    No new tests. Removing unused code.
    
    * rendering/RenderObject.h:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115908 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.h
@@ -1178,11 +1178,6 @@ inline int adjustForAbsoluteZoom(int value, RenderObject* renderer)
     return adjustForAbsoluteZoom(value, renderer->style());
 }
 
-inline int adjustForAbsoluteZoom(FractionalLayoutUnit value, RenderObject* renderer)
-{
-    return adjustForAbsoluteZoom(value.floor(), renderer->style());
-}
-
 inline void adjustFloatQuadForAbsoluteZoom(FloatQuad& quad, RenderObject* renderer)
 {
     float zoom = renderer->style()->effectiveZoom();
