67bf838d039cbc359aa2a53e4846e58b74598149
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-02 08:51:50 +0000

    REGRESSION: Web Inspector doesn't show cookies anymore https://bugs.webkit.org/show_bug.cgi?id=85349
    Pass root node instead of DataGrid object to the "populateNode" method.
    Added compiler annotations so that closure compiler catches such errors.
    
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/CookiesTable.js:
    (WebInspector.CookiesTable.prototype._rebuildTable):
    * inspector/front-end/NetworkRequest.js:
    (WebInspector.NetworkRequest.prototype.addFrameError):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkRequest.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkRequest.js
@@ -831,7 +831,7 @@ WebInspector.NetworkRequest.prototype = {
      */
     addFrameError: function(errorMessage, time)
     {
-        errorObject = {};
+        var errorObject = {};
         errorObject.errorMessage = errorMessage;
         errorObject.time = time;
         this._pushFrame(errorObject);
