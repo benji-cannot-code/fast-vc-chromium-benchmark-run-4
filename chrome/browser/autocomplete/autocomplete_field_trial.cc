19d370f2289c94deeb77a971235085aec61e1eda
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 00:58:14 +0000

    Make Omnibox HistoryURL Provider always aggressive.
    Remove the code for the aggressive HistoryURL provider field trial and make the HistoryURL provider aggressive for everyone.  Also remove the single about flag that allowed users to force a particular behavior.  (Pretty much no one did.)
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10260020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134839 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_field_trial.cc
+++ chrome/browser/autocomplete/autocomplete_field_trial.cc
@@ -14,19 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Field trial names.
-static const char kAggressiveHUPFieldTrialName[] =
-    "OmniboxAggressiveHistoryURLProvider";
 static const char kDisallowInlineHQPFieldTrialName[] =
     "OmniboxDisallowInlineHQP";
 static const char kSuggestFieldTrialName[] = "OmniboxSearchSuggest";
 
 // Field trial experiment probabilities.
-
-// For aggressive History URL Provider field trial, put 50% ( = 50/100 )
-// of the users in the aggressive experiment group.
-const base::FieldTrial::Probability kAggressiveHUPFieldTrialDivisor = 100;
-const base::FieldTrial::Probability
-    kAggressiveHUPFieldTrialExperimentFraction = 50;
 
 // For inline History Quick Provider field trial, put 10% ( = 10/100 )
 // of the users in the disallow-inline experiment group.
@@ -42,9 +34,6 @@ const int kSuggestFieldTrialNumberOfGroups = 20;
 // Field trial IDs.
 // Though they are not literally "const", they are set only once, in
 // Activate() below.
-
-// Field trial ID for the aggressive History URL Provider experiment group.
-int aggressive_hup_experiment_group = 0;
 
 // Field trial ID for the disallow-inline History Quick Provider
 // experiment group.
@@ -63,21 +52,12 @@ void AutocompleteFieldTrial::Activate() {
   // trust the omnibox.  Hence, to create the field trials we require
   // that field trials can be made sticky.
   if (base::FieldTrialList::IsOneTimeRandomizationEnabled()) {  // sticky trials
-    // Create aggressive History URL Provider field trial.
-    // Make it expire on August 1, 2012.
-    scoped_refptr<base::FieldTrial> trial(
-        base::FieldTrialList::FactoryGetFieldTrial(
-            kAggressiveHUPFieldTrialName, kAggressiveHUPFieldTrialDivisor,
-            "Standard", 2012, 8, 1, NULL));
-    trial->UseOneTimeRandomization();
-    aggressive_hup_experiment_group = trial->AppendGroup("Aggressive",
-        kAggressiveHUPFieldTrialExperimentFraction);
-
     // Create inline History Quick Provider field trial.
     // Make it expire on November 8, 2012.
-    trial = base::FieldTrialList::FactoryGetFieldTrial(
+    scoped_refptr<base::FieldTrial> trial(
+        base::FieldTrialList::FactoryGetFieldTrial(
         kDisallowInlineHQPFieldTrialName, kDisallowInlineHQPFieldTrialDivisor,
-        "Standard", 2012, 11, 8, NULL);
+        "Standard", 2012, 11, 8, NULL));
     trial->UseOneTimeRandomization();
     disallow_inline_hqp_experiment_group = trial->AppendGroup("DisallowInline",
         kDisallowInlineHQPFieldTrialExperimentFraction);
@@ -96,20 +76,6 @@ void AutocompleteFieldTrial::Activate() {
   // kSuggestFieldTrialNumGroups - 1 more.
   for (int i = 1; i < kSuggestFieldTrialNumberOfGroups; i++)
     trial->AppendGroup(base::StringPrintf("%d", i), 1);
-}
-
-bool AutocompleteFieldTrial::InAggressiveHUPFieldTrial() {
-  return base::FieldTrialList::TrialExists(kAggressiveHUPFieldTrialName);
-}
-
-bool AutocompleteFieldTrial::InAggressiveHUPFieldTrialExperimentGroup() {
-  if (!base::FieldTrialList::TrialExists(kAggressiveHUPFieldTrialName))
-    return false;
-
-  // Return true if we're in the aggressive experiment group.
-  const int group = base::FieldTrialList::FindValue(
-      kAggressiveHUPFieldTrialName);
-  return group == aggressive_hup_experiment_group;
 }
 
 bool AutocompleteFieldTrial::InDisallowInlineHQPFieldTrial() {
