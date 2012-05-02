8bddc7891a6871e2e4cb6fa41b98c3bcc630aac8
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 22:39:40 +0000

    Update Panel mininimize/restore button on strip and expansion state change.
    Previously code for each platform was doing it at draw/bounds change/layout.
    
    Also removed nearby code that was obsoleted by overflow strip removal.
    
    BUG=125039,125054
    TEST=minimize/restore panels and drag them to/from dock. verify buttons update.
    
    
    Review URL: http://codereview.chromium.org/10260028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/detached_panel_strip.cc
+++ chrome/browser/ui/panels/detached_panel_strip.cc
@@ -39,13 +39,13 @@ void DetachedPanelStrip::AddPanel(Panel* panel,
                                   PositioningMask positioning_mask) {
   // positioning_mask is ignored since the detached panel is free-floating.
   DCHECK_NE(this, panel->panel_strip());
-  panel->SetPanelStrip(this);
+  panel->set_panel_strip(this);
   panels_.insert(panel);
 }
 
 void DetachedPanelStrip::RemovePanel(Panel* panel) {
   DCHECK_EQ(this, panel->panel_strip());
-  panel->SetPanelStrip(NULL);
+  panel->set_panel_strip(NULL);
   panels_.erase(panel);
 }
 
@@ -197,6 +197,7 @@ void DetachedPanelStrip::UpdatePanelOnStripChange(Panel* panel) {
                                         Panel::USE_SYSTEM_ATTENTION));
   panel->SetAlwaysOnTop(false);
   panel->EnableResizeByMouse(true);
+  panel->UpdateMinimizeRestoreButtonVisibility();
 }
 
 void DetachedPanelStrip::OnPanelActiveStateChanged(Panel* panel) {
