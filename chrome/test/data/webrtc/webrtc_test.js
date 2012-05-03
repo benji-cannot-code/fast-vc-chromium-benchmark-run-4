2b9668c07d99641e7ee95decaa2398a4e63a5583
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 14:09:21 +0000

    Adapted test to new WebRTC API.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10342012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webrtc/webrtc_test.js
+++ chrome/test/data/webrtc/webrtc_test.js
@@ -67,8 +67,9 @@ function requestWebcamAndMicrophone() {
   }
 
   debug("Requesting webcam and microphone.");
-  navigator.webkitGetUserMedia(
-    "video,audio", getUserMediaOkCallback, getUserMediaFailedCallback);
+  navigator.webkitGetUserMedia({video:true, audio:true},
+                               getUserMediaOkCallback,
+                               getUserMediaFailedCallback);
   returnToPyAuto('ok-requested');
 }
 
