90aecacc7e2882342c393d0cf66d98f524c05881
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 03:13:51 +0000

    This reduces so the enrollment dialog so it just spawns a tab.
    Since neither WebView or ExtensionDialog support setting window.location from javascript, we have to load the enrollment URI in a full tab, so this changes the enrollment dialog so that it will show a message informing the user what to do, and then launches the URI in a tab.
    
    It means that we're no longer able to continue connecting in the case where the certificate has been added, but at least it's functional.
    
    BUG=chromium-os:29200
    TEST=Ran on device, imported ONC, and tested with http and extension enrollment URIs.
    
    
    Review URL: http://codereview.chromium.org/10230012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135076 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/vpn_config_view.cc
+++ chrome/browser/chromeos/options/vpn_config_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/cros/onc_constants.h"
 #include "chrome/browser/chromeos/enrollment_dialog_view.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/net/x509_certificate_model.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -351,8 +352,8 @@ bool VPNConfigView::Login() {
         break;
     }
     vpn->SetEnrollmentDelegate(
-        EnrollmentDialogView::CreateEnrollmentDelegate(
-            GetWidget()->GetNativeWindow()));
+        CreateEnrollmentDelegate(GetWidget()->GetNativeWindow(),
+                                 ProfileManager::GetLastUsedProfile()));
     cros->ConnectToVirtualNetwork(vpn);
   }
   // Connection failures are responsible for updating the UI, including
--- chrome/browser/chromeos/options/wifi_config_view.cc
+++ chrome/browser/chromeos/options/wifi_config_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/onc_constants.h"
 #include "chrome/browser/chromeos/enrollment_dialog_view.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
@@ -729,8 +730,8 @@ bool WifiConfigView::Login() {
     }
     bool share_default = (wifi->profile_type() != PROFILE_USER);
     wifi->SetEnrollmentDelegate(
-        EnrollmentDialogView::CreateEnrollmentDelegate(
-            GetWidget()->GetNativeWindow()));
+        CreateEnrollmentDelegate(GetWidget()->GetNativeWindow(),
+                                 ProfileManager::GetLastUsedProfile()));
     cros->ConnectToWifiNetwork(wifi, GetShareNetwork(share_default));
     // Connection failures are responsible for updating the UI, including
     // reopening dialogs.
