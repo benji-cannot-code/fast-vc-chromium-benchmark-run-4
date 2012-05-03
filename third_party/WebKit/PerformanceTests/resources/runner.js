a3b57ae994b83818de077eb74b65f5af0f991120
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 00:29:26 +0000

    Unreviewed, rolling out r115905. http://trac.webkit.org/changeset/115905 https://bugs.webkit.org/show_bug.cgi?id=85435
    New computeStatistics test is failing on chromium ports
    (Requested by tomz on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-05-02
    
    PerformanceTests:
    
    * resources/runner.js:
    
    LayoutTests:
    
    * fast/harness/perftests/perf-runner-compute-statistics-expected.txt: Removed.
    * fast/harness/perftests/perf-runner-compute-statistics.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PerformanceTests/resources/runner.js
+++ third_party/WebKit/PerformanceTests/resources/runner.js
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// There are tests for computeStatistics() located in LayoutTests/fast/harness/perftests
 
 var PerfTestRunner = {};
 
