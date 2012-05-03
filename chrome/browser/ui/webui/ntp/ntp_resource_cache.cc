8bcfee8cc402f7e518d17b3add2b8fcb734f2bb4
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 00:10:37 +0000

    Remove bookmark_bar_view include from ntp_resource_cache.cc
    The include of bookmark_bar_view.h is not more necessary as the constant the code
    was using now lives in chrome/browser/defaults.h
    
    BUG=125846
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10261037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -51,9 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/sys_color_change_listener.h"
 
-#if defined(OS_WIN) || defined(TOOLKIT_VIEWS)
-#include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
-#elif defined(OS_MACOSX)
+#if defined(OS_MACOSX)
 #include "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_constants.h"
 #elif defined(TOOLKIT_GTK)
 #include "chrome/browser/ui/gtk/bookmarks/bookmark_bar_gtk.h"
