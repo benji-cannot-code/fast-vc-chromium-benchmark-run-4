57ecb4b666659ff20b8bb4b56c8b481846789edc
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 15:11:27 +0000

    Remove extra checks for empty string when parsing CSS value https://bugs.webkit.org/show_bug.cgi?id=85480
    Reviewed by Alexis Menard.
    
    Source/WebCore:
    
    Each parse value helper function was checking whether the value string was empty. For the
    common case this check is already done by StylePropertySet::setProperty(). So this patch
    make CSSParser::parseValue() assume the value string is not empty, and fix the other two
    clients.
    
    Test: fast/html/font-face-empty-should-not-crash.html
    
    * css/CSSParser.cpp:
    (WebCore::parseColorValue): Replace the string empty check by an ASSERT() to document
    function's expectations.
    (WebCore::parseSimpleLengthValue): Ditto.
    (WebCore::parseKeywordValue): Ditto.
    (WebCore::CSSParser::parseFontFaceValue): This will be covered by the added test.
    (WebCore::CSSParser::parseValue):
    * css/WebKitCSSMatrix.cpp:
    (WebCore::WebKitCSSMatrix::setMatrixValue): This is already covered by
    transforms/cssmatrix-2d-interface.xhtml.
    
    LayoutTests:
    
    * fast/html/font-face-empty-should-not-crash-expected.txt: Added.
    * fast/html/font-face-empty-should-not-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -363,9 +363,8 @@ static inline bool isColorPropertyID(CSSPropertyID propertyId)
 
 static bool parseColorValue(StylePropertySet* declaration, CSSPropertyID propertyId, const String& string, bool important, CSSParserMode cssParserMode)
 {
+    ASSERT(!string.isEmpty());
     bool strict = isStrictParserMode(cssParserMode);
-    if (!string.length())
-        return false;
     if (!isColorPropertyID(propertyId))
         return false;
     CSSParserString cssString;
@@ -443,12 +442,10 @@ static inline bool isSimpleLengthPropertyID(CSSPropertyID propertyId, bool& acce
 
 static bool parseSimpleLengthValue(StylePropertySet* declaration, CSSPropertyID propertyId, const String& string, bool important, CSSParserMode cssParserMode)
 {
+    ASSERT(!string.isEmpty());
     bool acceptsNegativeNumbers;
     bool strict = isStrictParserMode(cssParserMode);
     unsigned length = string.length();
-
-    if (!length)
-        return false;
 
     double number;
     bool ok;
@@ -934,8 +931,7 @@ static inline bool isKeywordPropertyID(CSSPropertyID propertyId)
 
 static bool parseKeywordValue(StylePropertySet* declaration, CSSPropertyID propertyId, const String& string, bool important)
 {
-    if (string.isEmpty())
-        return false;
+    ASSERT(!string.isEmpty());
 
     if (!isKeywordPropertyID(propertyId))
         return false;
@@ -964,6 +960,8 @@ static bool parseKeywordValue(StylePropertySet* declaration, CSSPropertyID prope
 
 PassRefPtr<CSSValueList> CSSParser::parseFontFaceValue(const AtomicString& string)
 {
+    if (string.isEmpty())
+        return 0;
     RefPtr<StylePropertySet> dummyStyle = StylePropertySet::create();
     if (!parseValue(dummyStyle.get(), CSSPropertyFontFamily, string, false, CSSQuirksMode, 0))
         return 0;
@@ -972,6 +970,7 @@ PassRefPtr<CSSValueList> CSSParser::parseFontFaceValue(const AtomicString& strin
 
 bool CSSParser::parseValue(StylePropertySet* declaration, CSSPropertyID propertyID, const String& string, bool important, CSSParserMode cssParserMode, StyleSheetInternal* contextStyleSheet)
 {
+    ASSERT(!string.isEmpty());
     if (parseSimpleLengthValue(declaration, propertyID, string, important, cssParserMode))
         return true;
     if (parseColorValue(declaration, propertyID, string, important, cssParserMode))
