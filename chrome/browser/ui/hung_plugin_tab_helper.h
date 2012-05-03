9b3b69033a9ed86390132a57e721a412af7c1a78
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 04:01:54 +0000

    Make the hung plugin infobar more defensive.
    The ownership system of the infobars appears to be insane. In various places
    it will just delete all of your infobars. Since I need to maintain a mapping of
    all the infobars we have open, I added some extra (speculative since I couldn't
    repro the crash) tracking code here to make sure everything is up-to-date.
    
    BUG=123663
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10247002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/hung_plugin_tab_helper.h
+++ chrome/browser/ui/hung_plugin_tab_helper.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string16.h"
 #include "base/time.h"
 #include "base/timer.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
 
 class FilePath;
@@ -30,7 +32,8 @@ class InfoBarTabHelper;
 //   terminating the plugin.
 // - Hide the infobar if the plugin starts responding again.
 // - Keep track of all of this for any number of plugins.
-class HungPluginTabHelper : public content::WebContentsObserver {
+class HungPluginTabHelper : public content::WebContentsObserver,
+                            public content::NotificationObserver {
  public:
   explicit HungPluginTabHelper(content::WebContents* contents);
   virtual ~HungPluginTabHelper();
@@ -40,6 +43,11 @@ class HungPluginTabHelper : public content::WebContentsObserver {
   virtual void PluginHungStatusChanged(int plugin_child_id,
                                        const FilePath& plugin_path,
                                        bool is_hung) OVERRIDE;
+
+  // NotificationObserver overrides.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
  private:
   class InfoBarDelegate;
@@ -77,10 +85,6 @@ class HungPluginTabHelper : public content::WebContentsObserver {
   // Called by an infobar when the user selects to kill the plugin.
   void KillPlugin(int child_id);
 
-  // Called by an infobar when the user presses the close button to dismiss the
-  // bar without doing anything.
-  void BarClosed(int child_id);
-
   // Called on a timer for a hung plugin to re-show the bar.
   void OnReshowTimer(int child_id);
 
@@ -94,6 +98,8 @@ class HungPluginTabHelper : public content::WebContentsObserver {
 
   // Possibly returns null.
   InfoBarTabHelper* GetInfoBarHelper();
+
+  content::NotificationRegistrar registrar_;
 
   // All currently hung plugins.
   PluginStateMap hung_plugins_;
