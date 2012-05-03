aade878e176bc5d60b95f0ea4c4a56af05cd3443
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 20:32:29 +0000

    Compositing 'requiresOwnBackingStore' logic caused new clip rect assertions https://bugs.webkit.org/show_bug.cgi?id=85455
    Reviewed by Dean Jackson.
    
    r114283 added logic that allows compositing layers to avoid allocating their own
    backing store and to paint into an ancestor instead. However, that caused
    assertions in RenderLayer::updateClipRects() about m_clipRectsRoot being
    incorrect, because clip rect code assumed that compositing layers
    always painted themselves.
    
    Fixed by calling paintsIntoCompositedAncestor() in RenderLayer::clippingRoot(),
    so that clip rect computation matches painting.
    
    I wasn't able to easily make a test that reproduces the assertion in DRT.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::clippingRoot):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -1078,7 +1078,7 @@ RenderLayer* RenderLayer::clippingRoot() const
         ASSERT(current);
         if (current->transform()
 #if USE(ACCELERATED_COMPOSITING)
-            || current->isComposited()
+            || (current->isComposited() && !current->backing()->paintsIntoCompositedAncestor())
 #endif
         )
             return const_cast<RenderLayer*>(current);
