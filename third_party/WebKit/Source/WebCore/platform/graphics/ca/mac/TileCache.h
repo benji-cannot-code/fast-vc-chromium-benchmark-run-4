38cb32921bfc6d93e1815c9f84b4f636bf813353
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 20:43:21 +0000

    Move repaint counter drawing code out into a separate function https://bugs.webkit.org/show_bug.cgi?id=85539
    Reviewed by Simon Fraser.
    
    The majority of code in TileCache::drawLayer deals with drawing the repaint counter. Move this code out
    into a separate function to make it more clear what drawLayer does.
    
    * platform/graphics/ca/mac/TileCache.h:
    (TileCache):
    * platform/graphics/ca/mac/TileCache.mm:
    (WebCore::TileCache::drawLayer):
    (WebCore::TileCache::drawRepaintCounter):
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.h
@@ -57,7 +57,7 @@ public:
 
     void setNeedsDisplay();
     void setNeedsDisplayInRect(const IntRect&);
-    void drawLayer(WebTileLayer*, CGContextRef);
+    void drawLayer(WebTileLayer *, CGContextRef);
 
     void setScale(CGFloat);
 
@@ -93,6 +93,7 @@ private:
     RetainPtr<WebTileLayer> createTileLayer(const IntRect&);
 
     bool shouldShowRepaintCounters() const;
+    void drawRepaintCounter(WebTileLayer *, CGContextRef);
 
     WebTileCacheLayer* m_tileCacheLayer;
     RetainPtr<CALayer> m_tileContainerLayer;
