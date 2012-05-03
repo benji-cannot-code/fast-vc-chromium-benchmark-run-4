496fef91f2d8676599322ca91e3e5758169bd353
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 20:28:47 +0000

    Fix use of ScopedComPtr that caused crash in destructor
    BUG=122026
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10267024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135203 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/extension_api/tts_extension_api_win.cc
+++ chrome/browser/speech/extension_api/tts_extension_api_win.cc
@@ -70,7 +70,7 @@ bool ExtensionTtsPlatformImplWin::Speak(
   std::wstring prefix;
   std::wstring suffix;
 
-  if (!speech_synthesizer_)
+  if (!speech_synthesizer_.get())
     return false;
 
   // TODO(dmazzoni): support languages other than the default: crbug.com/88059
@@ -115,7 +115,7 @@ bool ExtensionTtsPlatformImplWin::Speak(
 }
 
 bool ExtensionTtsPlatformImplWin::StopSpeaking() {
-  if (speech_synthesizer_) {
+  if (speech_synthesizer_.get()) {
     // Clear the stream number so that any further events relating to this
     // utterance are ignored.
     stream_number_ = 0;
@@ -129,7 +129,7 @@ bool ExtensionTtsPlatformImplWin::StopSpeaking() {
 }
 
 bool ExtensionTtsPlatformImplWin::IsSpeaking() {
-  if (speech_synthesizer_) {
+  if (speech_synthesizer_.get()) {
     SPVOICESTATUS status;
     HRESULT result = speech_synthesizer_->GetStatus(&status, NULL);
     if (result == S_OK) {
@@ -188,18 +188,12 @@ void ExtensionTtsPlatformImplWin::OnSpeechEvent() {
 }
 
 ExtensionTtsPlatformImplWin::ExtensionTtsPlatformImplWin()
-  : speech_synthesizer_(NULL),
-    utterance_id_(0),
+  : utterance_id_(0),
     prefix_len_(0),
     stream_number_(0),
     char_position_(0) {
-  CoCreateInstance(
-      CLSID_SpVoice,
-      NULL,
-      CLSCTX_SERVER,
-      IID_ISpVoice,
-      reinterpret_cast<void**>(&speech_synthesizer_));
-  if (speech_synthesizer_) {
+  speech_synthesizer_.CreateInstance(CLSID_SpVoice);
+  if (speech_synthesizer_.get()) {
     ULONGLONG event_mask =
         SPFEI(SPEI_START_INPUT_STREAM) |
         SPFEI(SPEI_TTS_BOOKMARK) |
