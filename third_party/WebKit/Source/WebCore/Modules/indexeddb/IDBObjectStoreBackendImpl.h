53858f569d702fc0f78c18d7f78d01b68022edb2
zmo@google.com <zmo@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 01:12:03 +0000

    Unreviewed, rolling out r115902. http://trac.webkit.org/changeset/115902 https://bugs.webkit.org/show_bug.cgi?id=85441
    Compile failure on linux 32 (Requested by zhenyao on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-05-02
    
    Source/WebCore: 
    
    * Modules/indexeddb/IDBBackingStore.h:
    (IDBBackingStore):
    * Modules/indexeddb/IDBLevelDBBackingStore.cpp:
    (WebCore::IDBLevelDBBackingStore::nextAutoIncrementNumber):
    * Modules/indexeddb/IDBLevelDBBackingStore.h:
    (IDBLevelDBBackingStore):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp:
    (WebCore::IDBObjectStoreBackendImpl::putInternal):
    (WebCore::IDBObjectStoreBackendImpl::genAutoIncrementKey):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.h:
    (IDBObjectStoreBackendImpl):
    
    LayoutTests: 
    
    * storage/indexeddb/key-generator-expected.txt:
    * storage/indexeddb/resources/key-generator.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBObjectStoreBackendImpl.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBObjectStoreBackendImpl.h
@@ -116,7 +116,7 @@ private:
 
     typedef HashMap<String, RefPtr<IDBIndexBackendImpl> > IndexMap;
     IndexMap m_indexes;
-    int64_t m_autoIncrementNumber;
+    int m_autoIncrementNumber;
 };
 
 } // namespace WebCore
