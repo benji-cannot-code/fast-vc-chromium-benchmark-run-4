3a39c64f7f84926adae69c2893ced1236129b569
xji@chromium.org <xji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 19:42:32 +0000

    Enable ctrl-arrow moves caret by word in visual order in Linux.
    Currently ctrl-arrow key moves caret by word in logical order. Our RTL native speakers prefer it to be in visual order. 
    
    The WebKit side change is in r115788.
    
    This change should not affect left-to-right context.
    But in right-to-left context or bi-directional context, when pressing ctrl-arrow, the caret will move by word in visual order.
    
    Given the following logical text "abc def HIJ NML OPQ rst uvw" (assume capital letter represents Hebrew), it's display as: "abc def QPO LMN JIH rst uvw".
    Press ctrl-right-arrow from leftmost position, the caret positions will be 
    "|abc def QPO LMN JIH rst uvw"
    "abc| def QPO LMN JIH rst uvw"
    "abc def| QPO LMN JIH rst uvw"
    "abc def QPO| LMN JIH rst uvw"
    "abc def QPO LMN| JIH rst uvw"
    "abc def QPO LMN JIH| rst uvw"
    "abc def QPO LMN JIH rst| uvw"
    "abc def QPO LMN JIH rst uvw|"
    
    Press ctrl-left-arrow from rightmost position, the caret positions will be:
    "abc def QPO LMN JIH rst uvw|"
    "abc def QPO LMN JIH rst |uvw"
    "abc def QPO LMN JIH |rst uvw"
    "abc def QPO LMN |JIH rst uvw"
    "abc def QPO |LMN JIH rst uvw"
    "abc def |QPO LMN JIH rst uvw"
    "abc |def QPO LMN JIH rst uvw"
    "|abc def QPO LMN JIH rst uvw"
    
    Currently, when pressing ctrl-right-arrow from leftmost position, the caret positions are:
    "|abc def QPO LMN JIH rst uvw"
    "abc| def QPO LMN JIH rst uvw"
    "abc def| QPO LMN JIH rst uvw"
    "abc def QPO LMN |JIH rst uvw"
    "abc def QPO |LMN JIH rst uvw"
    "abc def QPO LMN JIH| rst uvw"
    "abc def QPO LMN JIH rst| uvw"
    "abc def QPO LMN JIH rst uvw|"
    
    As a side effect, it also fixes the shift-ctrl-arrow behavior in Linux (crbug.com/8141).
    After which, Linux's shift-ctrl-arrow behaves the same as Firefox in Linux and the same as Chromium Windows in the sense that:
    In LTR text box, text selection followings logical order, same as before.
    But in RTL text box, text selection follows anti-logical order.
    
    Given an example, <div dir=rtl>abc def</div>, display as "abc def", press shift-ctrl-left-arrow when cursor at "abc |def", currently "abc " is selected. After the fix, "def" is selected.
    
    
    BUG=10741, 8141
    TEST=manual. press ctrl-arrow and shift-ctrl-arrow in bi-directional text in chromium linux.
    
    Review URL: http://codereview.chromium.org/10319003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135192 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/gtk_key_bindings_handler.cc
+++ content/browser/renderer_host/gtk_key_bindings_handler.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -215,7 +215,7 @@ void GtkKeyBindingsHandler::MoveCursor(
       command = (count > 0 ? "MoveRight" : "MoveLeft");
       break;
     case GTK_MOVEMENT_WORDS:
-      command = (count > 0 ? "MoveWordForward" : "MoveWordBackward");
+      command = (count > 0 ? "MoveWordRight" : "MoveWordLeft");
       break;
     case GTK_MOVEMENT_DISPLAY_LINES:
       command = (count > 0 ? "MoveDown" : "MoveUp");
