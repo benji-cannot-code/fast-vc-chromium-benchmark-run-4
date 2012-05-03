0942e2c1713dbbc77d07948ac1a8a49253b5ce0e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-03 18:57:52 +0000

    Add methods to create WebArrayBuffer/WebArrayBufferView from v8 objects https://bugs.webkit.org/show_bug.cgi?id=84899
    Patch by Antony Sargent <asargent@chromium.org> on 2012-05-03
    Reviewed by Adam Barth.
    
    * public/WebArrayBuffer.h:
    (WebArrayBuffer):
    * public/platform/WebArrayBufferView.h:
    (v8):
    (WebArrayBufferView):
    * src/WebArrayBuffer.cpp:
    (WebKit):
    (WebKit::WebArrayBuffer::createFromV8Value):
    * src/WebArrayBufferView.cpp:
    (WebKit):
    (WebKit::WebArrayBufferView::createFromV8Value):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@115995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/platform/WebArrayBufferView.h
+++ third_party/WebKit/Source/WebKit/chromium/public/platform/WebArrayBufferView.h
@@ -33,6 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCommon.h"
 #include "WebPrivatePtr.h"
 
+#if WEBKIT_USING_V8
+namespace v8 {
+class Value;
+template <class T> class Handle;
+}
+#endif
+
 namespace WTF { class ArrayBufferView; }
 
 namespace WebKit {
@@ -50,6 +57,10 @@ public:
 
     WEBKIT_EXPORT void assign(const WebArrayBufferView&);
     WEBKIT_EXPORT void reset();
+
+#if WEBKIT_USING_V8
+    WEBKIT_EXPORT static WebArrayBufferView* createFromV8Value(v8::Handle<v8::Value>);
+#endif
 
 #if WEBKIT_IMPLEMENTATION
     WebArrayBufferView(const WTF::PassRefPtr<WTF::ArrayBufferView>&);
--- third_party/WebKit/Source/WebKit/chromium/src/WebArrayBufferView.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebArrayBufferView.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "platform/WebArrayBufferView.h"
 
+#include "V8ArrayBufferView.h"
 #include <wtf/ArrayBufferView.h>
 
 using namespace WTF;
@@ -60,6 +61,16 @@ unsigned WebArrayBufferView::byteLength() const
 {
     return m_private->byteLength();
 }
+
+#if WEBKIT_USING_V8
+WebArrayBufferView* WebArrayBufferView::createFromV8Value(v8::Handle<v8::Value> value)
+{
+    if (!WebCore::V8ArrayBufferView::HasInstance(value))
+        return 0;
+    ArrayBufferView* view = WebCore::V8ArrayBufferView::toNative(value->ToObject());
+    return new WebArrayBufferView(view);
+}
+#endif
 
 WebArrayBufferView::WebArrayBufferView(const PassRefPtr<ArrayBufferView>& value)
     : m_private(value)
