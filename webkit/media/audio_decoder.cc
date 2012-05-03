6a75fbc229404d85ee9f1b5e0cb02b2c36a947ba
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-03 05:37:35 +0000

    Reduce log level to DVLOG(1) in webkit_media::AudioDecoder.
    TBR=crogers
    
    
    Review URL: http://codereview.chromium.org/10348004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135103 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/audio_decoder.cc
+++ webkit/media/audio_decoder.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -57,13 +57,13 @@ bool DecodeAudioFileData(
   //   (sample_rate != 0.0) ? sample_rate : file_sample_rate;
   double destination_sample_rate = file_sample_rate;
 
-  DLOG(INFO) << "Decoding file data -"
-      << " data: " << data
-      << " data size: " << data_size
-      << " duration: " << duration
-      << " number of frames: " << number_of_frames
-      << " sample rate: " << file_sample_rate
-      << " number of channels: " << number_of_channels;
+  DVLOG(1) << "Decoding file data -"
+           << " data: " << data
+           << " data size: " << data_size
+           << " duration: " << duration
+           << " number of frames: " << number_of_frames
+           << " sample rate: " << file_sample_rate
+           << " number of channels: " << number_of_channels;
 
   // Change to destination sample-rate.
   number_of_frames = static_cast<size_t>(number_of_frames *
