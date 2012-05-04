db7684ffb4b0fc78e1d06eee685b7fe4802d862e
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-04 15:24:38 +0000

    Web Inspector: eliminate temporaryProfile property from ProfilesPanel. https://bugs.webkit.org/show_bug.cgi?id=85623
    We can run different profilers at the same time therefore we have to keep temorary profile per profiler type.
    
    Reviewed by Yury Semikhatsky.
    
    * inspector/front-end/CSSSelectorProfileView.js:
    (WebInspector.CSSSelectorProfileType.prototype.createView):
    (WebInspector.CSSSelectorProfileType.prototype.createTemporaryProfile):
    (WebInspector.CSSSelectorProfileType.prototype.createProfile):
    * inspector/front-end/HeapSnapshotView.js:
    (WebInspector.HeapSnapshotProfileType.prototype.createView):
    (WebInspector.HeapSnapshotProfileType.prototype.createTemporaryProfile):
    (WebInspector.HeapSnapshotProfileType.prototype.createProfile):
    * inspector/front-end/ProfileView.js:
    (WebInspector.CPUProfileType.prototype.startRecordingProfile):
    (WebInspector.CPUProfileType.prototype.createView):
    (WebInspector.CPUProfileType.prototype.createTemporaryProfile):
    (WebInspector.CPUProfileType.prototype.createProfile):
    * inspector/front-end/ProfilesPanel.js:
    (WebInspector.ProfileType.prototype.createSidebarTreeElementForProfile):
    (WebInspector.ProfileType.prototype.createTemporaryProfile):
    (WebInspector.ProfileType.prototype.createProfile):
    (WebInspector.ProfileHeader):
    (WebInspector.HeapProfileHeader):
    (WebInspector.ProfilesPanel.prototype.addProfileHeader):
    (WebInspector.ProfilesPanel.prototype.findTemporaryProfile):
    (WebInspector.ProfilesPanel.prototype._removeTemporaryProfile):
    (WebInspector.ProfilesPanel.prototype._populateProfiles.populateCallback.var):
    (WebInspector.ProfilesPanel.prototype._populateProfiles.populateCallback):
    (WebInspector.ProfilesPanel.prototype._populateProfiles):
    (WebInspector.ProfilesPanel.prototype.setRecordingProfile):
    (WebInspector.ProfilesPanel.prototype.takeHeapSnapshot):
    (WebInspector.ProfilesPanel.prototype._reportHeapSnapshotProgress):
    (WebInspector.ProfilerDispatcher.prototype.addProfileHeader):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116104 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/CSSSelectorProfileView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/CSSSelectorProfileView.js
@@ -350,6 +350,16 @@ WebInspector.CSSSelectorProfileType.prototype = {
     createView: function(profile)
     {
         return new WebInspector.CSSSelectorProfileView(profile);
+    },
+
+    createTemporaryProfile: function()
+    {
+        return new WebInspector.ProfileHeader(WebInspector.CSSSelectorProfileType.TypeId, WebInspector.UIString("Recording\u2026"));
+    },
+
+    createProfile: function(profile)
+    {
+        return new WebInspector.ProfileHeader(profile.typeId, profile.title, profile.uid);
     }
 }
 
