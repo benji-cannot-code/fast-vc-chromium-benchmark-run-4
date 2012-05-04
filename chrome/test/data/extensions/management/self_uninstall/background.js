03d21c26966492edd165c67a4defcd5ab3908476
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 22:17:56 +0000

    Segfault for self uninstall extension
    extension_function_dispatcher.cc was trying to access the profile after the
    extension was uninstalled. I just added a check to make sure the extension
    was still installed before the profile was accessed.
    
    BUG=121727
    TEST=Load the extensions attached to the bug. They will not segfault.
    
    
    Review URL: http://codereview.chromium.org/10260025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135454 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/management/self_uninstall/background.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The name of the extension to uninstall, from manifest.json.
+var EXPECTED_NAME = "Self Uninstall Test";
+
+chrome.management.getAll(function(items) {
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    if (item.name != EXPECTED_NAME) continue;
+    chrome.management.uninstall(item.id);
+  }
+});
