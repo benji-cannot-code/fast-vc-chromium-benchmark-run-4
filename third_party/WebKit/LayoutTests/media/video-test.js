e0656d6d0aa483af6ce734f935cca1f2dfdcec3a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-04 19:53:55 +0000

    Layout Test media/track/track-cues-pause-on-exit.html is flaky on chromium debug builds https://bugs.webkit.org/show_bug.cgi?id=80067
    Patch by Victor Carbune <vcarbune@adobe.com> on 2012-05-04
    Reviewed by Eric Carlson.
    
    Trying a different approach, blocking on waitForEvent for the video element.
    
    * media/track/track-cues-pause-on-exit-expected.txt: Updated.
    * media/track/track-cues-pause-on-exit.html: Changed to use
    waitForEvent and listen to exit events right after.
    * media/video-test.js: Updated waitForEvent to enable listening for
    events on other elements as well.
    (waitForEvent._eventCallback):
    (waitForEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116148 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/media/video-test.js
+++ third_party/WebKit/LayoutTests/media/video-test.js
@@ -156,12 +156,15 @@ function waitForEventAndEnd(eventName, funcString)
     waitForEvent(eventName, funcString, true)
 }
 
-function waitForEvent(eventName, func, endit, oneTimeOnly)
+function waitForEvent(eventName, func, endit, oneTimeOnly, element)
 {
+    if (!element)
+        element = mediaElement;
+
     function _eventCallback(event)
     {
         if (oneTimeOnly)
-            mediaElement.removeEventListener(eventName, _eventCallback, true);
+            element.removeEventListener(eventName, _eventCallback, true);
 
         consoleWrite("EVENT(" + eventName + ")");
 
@@ -172,7 +175,7 @@ function waitForEvent(eventName, func, endit, oneTimeOnly)
             endTest();
     }
 
-    mediaElement.addEventListener(eventName, _eventCallback, true);
+    element.addEventListener(eventName, _eventCallback, true);
 }
 
 function waitForEventTestAndEnd(eventName, testFuncString)
