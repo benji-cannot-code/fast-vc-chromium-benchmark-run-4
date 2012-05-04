7939600e8c03d7367f4f2caab763460befc5c2b1
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 01:36:55 +0000

    Make platform apps get isolated storage by default.
    Also centralizes the checks for when isolated storage is applicable to happen
    in Extension::LoadAppIsolation, so that Extension::is_storage_isolated()
    don't have to check for the experimental permission anymore.
    
    R=creis@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10349015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135269 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/isolation/test.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  chrome.test.runTests([
+    function testCookieNotSet() {
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+          'GET',
+          'http://localhost:' + config.testServer.port + '/echoheader?Cookie',
+          true);
+      xhr.onload = function() {
+        // Cookies should not have been passed in the request, so the echo of
+        // their header should be the Pythonic "None".
+        chrome.test.assertEq('None', xhr.responseText);
+        chrome.test.succeed();
+      };
+      xhr.onerror = function() {
+        chrome.test.fail('Unexpected HTTP status ' + xhr.status);
+      }
+      xhr.send();
+    }
+  ]);
+});
