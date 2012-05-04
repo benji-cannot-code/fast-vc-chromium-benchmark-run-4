18718ce7f6c98b777417dd3ae85a9c30f3a8d946
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 16:14:46 +0000

    Add some aura::Window debug methods
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/10355002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135354 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/string_util.h"
+#include "base/stringprintf.h"
 #include "ui/aura/client/event_client.h"
 #include "ui/aura/client/stacking_client.h"
 #include "ui/aura/client/visibility_client.h"
@@ -837,5 +838,25 @@ void Window::UpdateLayerName(const std::string& name) {
   layer()->set_name(layer_name);
 #endif
 }
+
+#ifndef NDEBUG
+std::string Window::GetDebugInfo() const {
+  return StringPrintf(
+      "%s<%d> bounds(%d, %d, %d, %d) %s",
+      name().empty() ? "Unknown" : name().c_str(), id(),
+      bounds().x(), bounds().y(), bounds().width(), bounds().height(),
+      IsVisible() ? "Visible" : "Hidden");
+}
+
+void Window::PrintWindowHierarchy(int depth) const {
+  printf("%*s%s\n", depth * 2, "", GetDebugInfo().c_str());
+  for (Windows::const_reverse_iterator it = children_.rbegin(),
+           rend = children_.rend();
+       it != rend; ++it) {
+    Window* child = *it;
+    child->PrintWindowHierarchy(depth + 1);
+  }
+}
+#endif
 
 }  // namespace aura
