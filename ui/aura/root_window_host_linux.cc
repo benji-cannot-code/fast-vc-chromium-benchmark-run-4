e8455536c1957c3c16b48ab413874c1c63791c90
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 19:55:30 +0000

    aura: Re-enable CheckXEventForConsistency checks for XI2.2
    BUG=109884
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10257005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135398 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_linux.cc
+++ ui/aura/root_window_host_linux.cc
@@ -43,6 +43,7 @@ const char kRootWindowHostLinuxKey[] = "__AURA_ROOT_WINDOW_HOST_LINUX__";
 // The events reported for slave devices can have incorrect information for some
 // fields. This utility function is used to check for such inconsistencies.
 void CheckXEventForConsistency(XEvent* xevent) {
+#if defined(USE_XI2_MT)
   static bool expect_master_event = false;
   static XIDeviceEvent slave_event;
   static gfx::Point slave_location;
@@ -87,6 +88,7 @@ void CheckXEventForConsistency(XEvent* xevent) {
     CHECK_EQ(slave_event.mods.locked, xievent->mods.locked);
     CHECK_EQ(slave_event.mods.effective, xievent->mods.effective);
   }
+#endif  // defined(USE_XI2_MT)
 }
 
 // Returns X font cursor shape from an Aura cursor.
@@ -197,8 +199,7 @@ int CoalescePendingXIMotionEvents(const XEvent* xev, XEvent* last_event) {
     // with one from the master and one from the slave so there will
     // always be at least one pending.
     if (!ui::TouchFactory::GetInstance()->ShouldProcessXI2Event(&next_event)) {
-      // See crbug.com/109884.
-      // CheckXEventForConsistency(&next_event);
+      CheckXEventForConsistency(&next_event);
       XFreeEventData(display, &next_event.xcookie);
       XNextEvent(display, &next_event);
       continue;
@@ -228,8 +229,7 @@ int CoalescePendingXIMotionEvents(const XEvent* xev, XEvent* last_event) {
         // Get the event and its cookie data.
         XNextEvent(display, last_event);
         XGetEventData(display, &last_event->xcookie);
-        // See crbug.com/109884.
-        // CheckXEventForConsistency(last_event);
+        CheckXEventForConsistency(last_event);
         ++num_coalesed;
         continue;
       } else {
@@ -397,8 +397,7 @@ RootWindowHostLinux::~RootWindowHostLinux() {
 bool RootWindowHostLinux::Dispatch(const base::NativeEvent& event) {
   XEvent* xev = event;
 
-  // See crbug.com/109884.
-  // CheckXEventForConsistency(xev);
+  CheckXEventForConsistency(xev);
 
   switch (xev->type) {
     case Expose:
