d98d8c1c3493e9652e6b8c54833bf0ff379eacd0
lipalani@chromium.org <lipalani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 21:05:36 +0000

    Add more debug statements to debug 120709
    BUG=120709
    TEST=
    
    Review URL: http://codereview.chromium.org/10384010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/non_frontend_data_type_controller.h
+++ chrome/browser/sync/glue/non_frontend_data_type_controller.h
@@ -185,6 +185,10 @@ class NonFrontendDataTypeController : public DataTypeController {
   // TODO(lipalani): Remove this after debugging.
   base::WaitableEvent start_association_called_;
 
+  // This is added for debugging purpose.
+  // TODO(lipalani): Remove after debugging.
+  bool start_models_failed_;
+
   DISALLOW_COPY_AND_ASSIGN(NonFrontendDataTypeController);
 };
 
