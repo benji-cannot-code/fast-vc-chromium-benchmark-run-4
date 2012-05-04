cc4ed4f5e3973ec98fcb8966c3107a088d586d99
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 21:57:41 +0000

    ash: Avoid recreating all the views in the network tray popup all the time.
    Instead of recreating all the items in the popup everytime network state
    changes, create all the items beforehand, and update the visibility/state as
    necessary. This should fix some of the visual jarring when connecting to a
    different network.
    
    BUG=125755, 120515
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10332017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135450 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_views.h
+++ ash/system/tray/tray_views.h
@@ -135,6 +135,7 @@ class FixedSizedScrollView : public views::ScrollView {
  private:
   // Overridden from views::View.
   virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual void Layout() OVERRIDE;
   virtual void OnBoundsChanged(const gfx::Rect& previous_bounds) OVERRIDE;
   virtual void OnMouseEntered(const views::MouseEvent& event) OVERRIDE;
   virtual void OnPaintFocusBorder(gfx::Canvas* canvas) OVERRIDE;
