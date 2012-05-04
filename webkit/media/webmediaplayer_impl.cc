e1cd5915da7e45057a005b3f39f1a800928e9167
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 05:27:36 +0000

    Report MEDIA_ERR_NETWORK instead of MEDIA_ERR_SRC_NOT_SUPPORTED when a network error occurs.
    BUG=125955
    TEST=load up a URL in http://ascherkus.github.com/iheartvideo and while it's playing kill the webserver -- MEDIA_ERR_NETWORK should be reported
    
    Review URL: http://codereview.chromium.org/10352006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135299 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_impl.cc
+++ webkit/media/webmediaplayer_impl.cc
@@ -902,6 +902,7 @@ void WebMediaPlayerImpl::OnPipelineError(PipelineStatus error) {
       break;
 
     case media::PIPELINE_ERROR_NETWORK:
+    case media::PIPELINE_ERROR_READ:
       SetNetworkState(WebMediaPlayer::NetworkStateNetworkError);
       break;
 
@@ -909,12 +910,10 @@ void WebMediaPlayerImpl::OnPipelineError(PipelineStatus error) {
     case media::PIPELINE_ERROR_REQUIRED_FILTER_MISSING:
     case media::PIPELINE_ERROR_COULD_NOT_RENDER:
     case media::PIPELINE_ERROR_URL_NOT_FOUND:
-    case media::PIPELINE_ERROR_READ:
     case media::DEMUXER_ERROR_COULD_NOT_OPEN:
     case media::DEMUXER_ERROR_COULD_NOT_PARSE:
     case media::DEMUXER_ERROR_NO_SUPPORTED_STREAMS:
     case media::DECODER_ERROR_NOT_SUPPORTED:
-      // Format error.
       SetNetworkState(WebMediaPlayer::NetworkStateFormatError);
       break;
 
@@ -922,7 +921,6 @@ void WebMediaPlayerImpl::OnPipelineError(PipelineStatus error) {
     case media::PIPELINE_ERROR_ABORT:
     case media::PIPELINE_ERROR_OPERATION_PENDING:
     case media::PIPELINE_ERROR_INVALID_STATE:
-      // Decode error.
       SetNetworkState(WebMediaPlayer::NetworkStateDecodeError);
       break;
 
