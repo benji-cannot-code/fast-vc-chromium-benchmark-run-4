b147580260609197b2ea2532b46f92a337dc77b4
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 22:24:16 +0000

    Add a couple more syscalls for the Nvidia binary driver, based on a real-world strace from a failing machine.
    Also one more Flash policy tweak.
    
    BUG=125712
    Review URL: https://chromiumcodereview.appspot.com/10383010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135461 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_init_linux.cc
+++ content/common/sandbox_init_linux.cc
@@ -188,6 +188,11 @@ static void EmitAllowKillSelf(int signal,
   EmitAllowSyscallArgN(__NR_kill, 2, signal, program);
 }
 
+static void EmitAllowGettime(std::vector<struct sock_filter>* program) {
+  EmitAllowSyscall(__NR_clock_gettime, program);
+  EmitAllowSyscall(__NR_gettimeofday, program);
+}
+
 static void ApplyGPUPolicy(std::vector<struct sock_filter>* program) {
   // "Hot" syscalls go first.
   EmitAllowSyscall(__NR_read, program);
@@ -198,9 +203,10 @@ static void ApplyGPUPolicy(std::vector<struct sock_filter>* program) {
   EmitAllowSyscall(__NR_write, program);
   EmitAllowSyscall(__NR_writev, program);
   EmitAllowSyscall(__NR_gettid, program);
+  EmitAllowSyscall(__NR_sched_yield, program);  // Nvidia binary driver.
+  EmitAllowGettime(program);
 
   // Less hot syscalls.
-  EmitAllowSyscall(__NR_clock_gettime, program);
   EmitAllowSyscall(__NR_futex, program);
   EmitAllowSyscall(__NR_madvise, program);
   EmitAllowSyscall(__NR_sendmsg, program);
@@ -232,8 +238,9 @@ static void ApplyGPUPolicy(std::vector<struct sock_filter>* program) {
   EmitAllowSyscall(__NR_munlock, program);
   EmitAllowSyscall(__NR_exit, program);
   EmitAllowSyscall(__NR_exit_group, program);
-  EmitAllowSyscall(__NR_getpid, program);  // Seen in Nvidia binary driver.
-  EmitAllowSyscall(__NR_getppid, program);  // Seen in ATI binary driver.
+  EmitAllowSyscall(__NR_getpid, program);  // Nvidia binary driver.
+  EmitAllowSyscall(__NR_getppid, program);  // ATI binary driver.
+  EmitAllowSyscall(__NR_lseek, program);  // Nvidia binary driver.
   EmitAllowKillSelf(SIGTERM, program);  // GPU watchdog.
 
   // Generally, filename-based syscalls will fail with ENOENT to behave
@@ -253,7 +260,7 @@ static void ApplyFlashPolicy(std::vector<struct sock_filter>* program) {
   EmitAllowSyscall(__NR_times, program);
 
   // Less hot syscalls.
-  EmitAllowSyscall(__NR_gettimeofday, program);
+  EmitAllowGettime(program);
   EmitAllowSyscall(__NR_clone, program);
   EmitAllowSyscall(__NR_set_robust_list, program);
   EmitAllowSyscall(__NR_getuid, program);
