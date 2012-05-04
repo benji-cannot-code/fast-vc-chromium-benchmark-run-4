b35bd4766fb305879e711796e17ebc31533b73ac
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 19:05:42 +0000

    Fix unsafe infobar usage pattern in ExtensionDevToolsClientHost.  Listening for infobar deletion to determine when it's appropriate to remove an infobar from the InfoBarTabHelper doesn't work correctly in two cases: (1) when the infobar is in the midst of closing (when it's already unowned but not yet deleted) and (2) if it's leaked due to being closed while in a background tab (will be fixed someday by refactoring changes I'm still waiting to land).
    Also safely handle the case of AddInfoBar() failing, by plumbing a return code back.
    
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10264025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/infobars/infobar_tab_helper.cc
+++ chrome/browser/infobars/infobar_tab_helper.cc
@@ -33,10 +33,10 @@ InfoBarTabHelper::~InfoBarTabHelper() {
   RemoveAllInfoBars(false);
 }
 
-void InfoBarTabHelper::AddInfoBar(InfoBarDelegate* delegate) {
+bool InfoBarTabHelper::AddInfoBar(InfoBarDelegate* delegate) {
   if (!infobars_enabled_) {
     delegate->InfoBarClosed();
-    return;
+    return false;
   }
 
   for (InfoBars::const_iterator i(infobars_.begin()); i != infobars_.end();
@@ -44,7 +44,7 @@ void InfoBarTabHelper::AddInfoBar(InfoBarDelegate* delegate) {
     if ((*i)->EqualsDelegate(delegate)) {
       DCHECK_NE(*i, delegate);
       delegate->InfoBarClosed();
-      return;
+      return false;
     }
   }
 
@@ -65,18 +65,17 @@ void InfoBarTabHelper::AddInfoBar(InfoBarDelegate* delegate) {
       chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_ADDED,
       content::Source<InfoBarTabHelper>(this),
       content::Details<InfoBarAddedDetails>(delegate));
+  return true;
 }
 
 void InfoBarTabHelper::RemoveInfoBar(InfoBarDelegate* delegate) {
   RemoveInfoBarInternal(delegate, true);
 }
 
-void InfoBarTabHelper::ReplaceInfoBar(InfoBarDelegate* old_delegate,
+bool InfoBarTabHelper::ReplaceInfoBar(InfoBarDelegate* old_delegate,
                                       InfoBarDelegate* new_delegate) {
-  if (!infobars_enabled_) {
-    AddInfoBar(new_delegate);  // Deletes the delegate.
-    return;
-  }
+  if (!infobars_enabled_)
+    return AddInfoBar(new_delegate);  // Deletes the delegate.
 
   InfoBars::iterator i(std::find(infobars_.begin(), infobars_.end(),
                                  old_delegate));
@@ -93,6 +92,7 @@ void InfoBarTabHelper::ReplaceInfoBar(InfoBarDelegate* old_delegate,
       chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REPLACED,
       content::Source<InfoBarTabHelper>(this),
       content::Details<InfoBarReplacedDetails>(&replaced_details));
+  return true;
 }
 
 InfoBarDelegate* InfoBarTabHelper::GetInfoBarDelegateAt(size_t index) {
