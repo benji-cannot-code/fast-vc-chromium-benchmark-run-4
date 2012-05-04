f0ed25118459bde97cf651db5caa23a17f5479ef
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-04 21:33:51 +0000

    ASSERT(beforeChildAnonymousContainer->isTable()); fails in RenderBlock::addChildIgnoringAnonymousColumnBlocks. https://bugs.webkit.org/show_bug.cgi?id=84606
    Reviewed by Julien Chaffraix.
    
    Source/WebCore:
    
    RenderBlock::removeChild forgot to set display on the anonymous block, causing it
    to display as INLINE. To prevent this kind of failure in future, we replace
    createAnonymousStyle with createAnonymousStyleWithDisplay to make everyone explictly
    pass display as the argument.
    
    Test: fast/block/block-add-child-crash.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::removeChild):
    (WebCore::RenderBlock::createAnonymousWithParentRendererAndDisplay):
    (WebCore::RenderBlock::createAnonymousColumnsWithParentRenderer):
    (WebCore::RenderBlock::createAnonymousColumnSpanWithParentRenderer):
    * rendering/RenderInline.cpp:
    (WebCore::updateStyleOfAnonymousBlockContinuations):
    (WebCore::RenderInline::addChildIgnoringContinuation):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::propagateStyleToAnonymousChildren):
    * rendering/RenderRuby.cpp:
    (WebCore::createAnonymousRubyInlineBlock):
    * rendering/RenderRubyRun.cpp:
    (WebCore::RenderRubyRun::createRubyBase):
    (WebCore::RenderRubyRun::staticCreateRubyRun):
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::createAnonymousWithParentRenderer):
    * rendering/RenderTableCell.cpp:
    (WebCore::RenderTableCell::createAnonymousWithParentRenderer):
    * rendering/RenderTableRow.cpp:
    (WebCore::RenderTableRow::createAnonymousWithParentRenderer):
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::createAnonymousWithParentRenderer):
    * rendering/mathml/RenderMathMLBlock.cpp:
    (WebCore::RenderMathMLBlock::createAlmostAnonymousBlock):
    * rendering/mathml/RenderMathMLRow.cpp:
    (WebCore::RenderMathMLRow::createAnonymousWithParentRenderer):
    * rendering/mathml/RenderMathMLSubSup.cpp:
    (WebCore::RenderMathMLSubSup::addChild):
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::createAnonymousStyleWithDisplay):
    * rendering/style/RenderStyle.h:
    
    LayoutTests:
    
    * fast/block/block-add-child-crash-expected.txt: Added.
    * fast/block/block-add-child-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -1956,7 +1956,7 @@ void RenderObject::propagateStyleToAnonymousChildren(bool blockChildrenOnly)
             continue;
 #endif
 
-        RefPtr<RenderStyle> newStyle = RenderStyle::createAnonymousStyle(style());
+        RefPtr<RenderStyle> newStyle = RenderStyle::createAnonymousStyleWithDisplay(style(), child->style()->display());
         if (style()->specifiesColumns()) {
             if (child->style()->specifiesColumns())
                 newStyle->inheritColumnPropertiesFrom(style());
@@ -1969,7 +1969,6 @@ void RenderObject::propagateStyleToAnonymousChildren(bool blockChildrenOnly)
         if (child->isRelPositioned() && toRenderBlock(child)->isAnonymousBlockContinuation())
             newStyle->setPosition(child->style()->position());
 
-        newStyle->setDisplay(child->style()->display());
         child->setStyle(newStyle.release());
     }
 }
