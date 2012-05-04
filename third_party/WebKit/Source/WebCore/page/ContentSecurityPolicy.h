0bc7c7935cc280747ce3373a91cccaa70f19c9ef
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-04 21:40:48 +0000

    Refactor CSP state to prepare for having both a ReportOnly and an Enforced policy https://bugs.webkit.org/show_bug.cgi?id=85662
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    This patch refactors the ContentSecurityPolicy state into a separate
    DirectiveList class to prepare for
    https://bugs.webkit.org/show_bug.cgi?id=85561, which will cause us to
    need two directive lists: one for enforcement and one for monitoring.
    
    This patch shouldn't cause any change in behavior.
    
    * page/ContentSecurityPolicy.cpp:
    (CSPDirectiveList):
    (WebCore::CSPDirectiveList::header):
    (WebCore::CSPDirectiveList::headerType):
    (WebCore::CSPDirectiveList::denyIfEnforcingPolicy):
    (WebCore):
    (WebCore::CSPDirectiveList::CSPDirectiveList):
    (WebCore::CSPDirectiveList::create):
    (WebCore::CSPDirectiveList::reportViolation):
    (WebCore::CSPDirectiveList::logUnrecognizedDirective):
    (WebCore::CSPDirectiveList::checkEval):
    (WebCore::CSPDirectiveList::operativeDirective):
    (WebCore::CSPDirectiveList::checkInlineAndReportViolation):
    (WebCore::CSPDirectiveList::checkEvalAndReportViolation):
    (WebCore::CSPDirectiveList::checkSourceAndReportViolation):
    (WebCore::CSPDirectiveList::allowJavaScriptURLs):
    (WebCore::CSPDirectiveList::allowInlineEventHandlers):
    (WebCore::CSPDirectiveList::allowInlineScript):
    (WebCore::CSPDirectiveList::allowInlineStyle):
    (WebCore::CSPDirectiveList::allowEval):
    (WebCore::CSPDirectiveList::allowScriptFromSource):
    (WebCore::CSPDirectiveList::allowObjectFromSource):
    (WebCore::CSPDirectiveList::allowChildFrameFromSource):
    (WebCore::CSPDirectiveList::allowImageFromSource):
    (WebCore::CSPDirectiveList::allowStyleFromSource):
    (WebCore::CSPDirectiveList::allowFontFromSource):
    (WebCore::CSPDirectiveList::allowMediaFromSource):
    (WebCore::CSPDirectiveList::allowConnectFromSource):
    (WebCore::CSPDirectiveList::parse):
    (WebCore::CSPDirectiveList::parseDirective):
    (WebCore::CSPDirectiveList::parseReportURI):
    (WebCore::CSPDirectiveList::createCSPDirective):
    (WebCore::CSPDirectiveList::applySandboxPolicy):
    (WebCore::CSPDirectiveList::addDirective):
    (WebCore::ContentSecurityPolicy::ContentSecurityPolicy):
    (WebCore::ContentSecurityPolicy::~ContentSecurityPolicy):
    (WebCore::ContentSecurityPolicy::copyStateFrom):
    (WebCore::ContentSecurityPolicy::didReceiveHeader):
    (WebCore::ContentSecurityPolicy::setOverrideAllowInlineStyle):
    (WebCore::ContentSecurityPolicy::header):
    (WebCore::ContentSecurityPolicy::headerType):
    (WebCore::ContentSecurityPolicy::allowJavaScriptURLs):
    (WebCore::ContentSecurityPolicy::allowInlineEventHandlers):
    (WebCore::ContentSecurityPolicy::allowInlineScript):
    (WebCore::ContentSecurityPolicy::allowInlineStyle):
    (WebCore::ContentSecurityPolicy::allowEval):
    (WebCore::ContentSecurityPolicy::allowScriptFromSource):
    (WebCore::ContentSecurityPolicy::allowObjectFromSource):
    (WebCore::ContentSecurityPolicy::allowChildFrameFromSource):
    (WebCore::ContentSecurityPolicy::allowImageFromSource):
    (WebCore::ContentSecurityPolicy::allowStyleFromSource):
    (WebCore::ContentSecurityPolicy::allowFontFromSource):
    (WebCore::ContentSecurityPolicy::allowMediaFromSource):
    (WebCore::ContentSecurityPolicy::allowConnectFromSource):
    * page/ContentSecurityPolicy.h:
    (WebCore):
    * workers/WorkerMessagingProxy.cpp:
    (WebCore::WorkerMessagingProxy::startWorkerContext):
    
    Source/WebKit/chromium:
    
    Update callers to the new function name.
    
    * src/SharedWorkerRepository.cpp:
    (WebCore::SharedWorkerScriptLoader::notifyFinished):
    * src/WebWorkerClientImpl.cpp:
    (WebKit::WebWorkerClientImpl::startWorkerContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class CSPDirective;
+class CSPDirectiveList;
 class ScriptExecutionContext;
 class KURL;
 
@@ -54,8 +54,9 @@ public:
     };
 
     void didReceiveHeader(const String&, HeaderType);
-    String policy() { return m_header; }
-    HeaderType headerType() { return m_reportOnly ? ReportOnly : EnforcePolicy; }
+
+    const String& header() const;
+    HeaderType headerType() const;
 
     bool allowJavaScriptURLs() const;
     bool allowInlineEventHandlers() const;
@@ -77,42 +78,9 @@ public:
 private:
     explicit ContentSecurityPolicy(ScriptExecutionContext*);
 
-    void parse(const String&);
-    bool parseDirective(const UChar* begin, const UChar* end, String& name, String& value);
-    void parseReportURI(const String&);
-    void addDirective(const String& name, const String& value);
-    void applySandboxPolicy(const String& sandboxPolicy);
-
-    PassOwnPtr<CSPDirective> createCSPDirective(const String& name, const String& value);
-
-    CSPDirective* operativeDirective(CSPDirective*) const;
-    void reportViolation(const String& directiveText, const String& consoleMessage) const;
-    void logUnrecognizedDirective(const String& name) const;
-    bool checkEval(CSPDirective*) const;
-
-    bool checkInlineAndReportViolation(CSPDirective*, const String& consoleMessage) const;
-    bool checkEvalAndReportViolation(CSPDirective*, const String& consoleMessage) const;
-    bool checkSourceAndReportViolation(CSPDirective*, const KURL&, const String& type) const;
-
-    bool denyIfEnforcingPolicy() const { return m_reportOnly; }
-
-    bool m_havePolicy;
     ScriptExecutionContext* m_scriptExecutionContext;
-
-    bool m_reportOnly;
-    String m_header;
-    OwnPtr<CSPDirective> m_defaultSrc;
-    OwnPtr<CSPDirective> m_scriptSrc;
-    OwnPtr<CSPDirective> m_objectSrc;
-    OwnPtr<CSPDirective> m_frameSrc;
-    OwnPtr<CSPDirective> m_imgSrc;
-    OwnPtr<CSPDirective> m_styleSrc;
-    OwnPtr<CSPDirective> m_fontSrc;
-    OwnPtr<CSPDirective> m_mediaSrc;
-    OwnPtr<CSPDirective> m_connectSrc;
-    bool m_haveSandboxPolicy;
     bool m_overrideInlineStyleAllowed;
-    Vector<KURL> m_reportURLs;
+    OwnPtr<CSPDirectiveList> m_policy;
 };
 
 }
