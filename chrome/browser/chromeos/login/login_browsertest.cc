4cb7bf6e141cfd41314a48a36d7608e995242524
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 04:17:37 +0000

    chromeos/drm_settings.cc: fix browser_tests!
    - Be more careful about checking the user logged in state.
    
    Avoid cases where the user is stubbed or is non-existent during testing.
    
    - Add GetSystemSalt to the mocking.
    
    Fixing UserPassed tests.
    
    TEST=builds; browser_tests look like they did before my change landed.
    BUG=none
    Change-Id: I5d6b2df8d0085ea60fa9dccf64c026f9b2ab7831
    
    R=brettw,jschuh,vtl
    
    Review URL: https://chromiumcodereview.appspot.com/10376007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_browsertest.cc
+++ chrome/browser/chromeos/login/login_browsertest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,6 +39,8 @@ class LoginTestBase : public CrosInProcessBrowserTest {
     mock_network_library_ = cros_mock_->mock_network_library();
     EXPECT_CALL(*mock_cryptohome_library_, IsMounted())
         .WillRepeatedly(Return(true));
+    EXPECT_CALL(*mock_cryptohome_library_, GetSystemSalt())
+        .WillRepeatedly(Return(std::string("stub_system_salt")));
     EXPECT_CALL(*mock_network_library_, AddUserActionObserver(_))
         .Times(AnyNumber());
   }
