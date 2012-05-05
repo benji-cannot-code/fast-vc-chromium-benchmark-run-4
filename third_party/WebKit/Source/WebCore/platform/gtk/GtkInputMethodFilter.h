98085598ed76178abc501a30553b5861eb2fec4f
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-05 21:41:00 +0000

    [GTK] WebKit should properly set the input method context window location https://bugs.webkit.org/show_bug.cgi?id=84981
    Patch by Martin Robinson <mrobinson@igalia.com> on 2012-05-05
    Reviewed by Gustavo Noronha Silva.
    
    Source/WebCore:
    
    * platform/gtk/GtkInputMethodFilter.cpp:
    (WebCore::GtkInputMethodFilter::setCursorRect): Add this method to plumb the
    cursor rect through to the input method.
    * platform/gtk/GtkInputMethodFilter.h: Add the new method declaration.
    
    Source/WebKit/gtk:
    
    * WebCoreSupport/ChromeClientGtk.cpp:
    (WebKit::ChromeClient::paint): During painting, update the cursor rectangle
    if possible.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/GtkInputMethodFilter.h
+++ third_party/WebKit/Source/WebCore/platform/gtk/GtkInputMethodFilter.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "GRefPtrGtk.h"
+#include "IntRect.h"
 #include <gdk/gdk.h>
 #include <wtf/text/WTFString.h>
 
@@ -47,6 +48,7 @@ public:
     void confirmComposition();
     void cancelContextComposition();
     void updatePreedit();
+    void setCursorRect(const IntRect& location);
 
     GtkIMContext* context() { return m_context.get(); }
 
@@ -82,6 +84,7 @@ private:
     bool m_preventNextCommit;
     bool m_justSentFakeKeyUp;
     unsigned int m_lastFilteredKeyPressCodeWithNoResults;
+    IntPoint m_lastCareLocation;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FileIconLoader.h"
 #include "FileSystem.h"
 #include "FloatRect.h"
+#include "FocusController.h"
 #include "FrameLoadRequest.h"
 #include "FrameView.h"
 #include "GtkUtilities.h"
@@ -599,6 +600,13 @@ void ChromeClient::paint(WebCore::Timer<ChromeClient>*)
     m_dirtyRegion = Region();
     m_lastDisplayTime = currentTime();
     m_repaintSoonSourceId = 0;
+
+    // We update the IM context window location here, because we want it to be
+    // synced with cursor movement. For instance, a text field can move without
+    // the selection changing.
+    Frame* focusedFrame = core(m_webView)->focusController()->focusedOrMainFrame();
+    if (focusedFrame && focusedFrame->editor()->canEdit())
+        m_webView->priv->imFilter.setCursorRect(frame->selection()->absoluteCaretBounds());
 }
 
 void ChromeClient::invalidateRootView(const IntRect&, bool immediate)
