a6be49e51c3af09f4e6034690b135e76fb35fecc
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 23:20:39 +0000

    Add proper prefix to the dom checker tests.
    TBR=cmp
    Review URL: https://chromiumcodereview.appspot.com/10356038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/perf/dom_checker_uitest.cc
+++ chrome/test/perf/dom_checker_uitest.cc
@@ -231,7 +231,7 @@ class DomCheckerTest : public UITest {
 };
 
 // Always fails, see but http://crbug.com/21321
-TEST_F(DomCheckerTest, FAIL_File) {
+TEST_F(DomCheckerTest, FAILS_File) {
   if (!CommandLine::ForCurrentProcess()->HasSwitch(kRunDomCheckerTest))
     return;
 
@@ -244,7 +244,7 @@ TEST_F(DomCheckerTest, FAIL_File) {
 // expected results file that didn't exist.  Fixing that bug revealed
 // that the expected results weren't correct anyway.
 // http://crbug.com/21321
-TEST_F(DomCheckerTest, FAIL_Http) {
+TEST_F(DomCheckerTest, FAILS_Http) {
   if (!CommandLine::ForCurrentProcess()->HasSwitch(kRunDomCheckerTest))
     return;
 
