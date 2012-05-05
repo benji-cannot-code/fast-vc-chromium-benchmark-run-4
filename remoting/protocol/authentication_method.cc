02a0be79729a8ca3b2e7dd27c6de7b6dd3cf9c28
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 01:30:37 +0000

    Remove some unused includes.
    Review URL: http://codereview.chromium.org/10310024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135513 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/authentication_method.cc
+++ remoting/protocol/authentication_method.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "crypto/hmac.h"
 #include "remoting/protocol/auth_util.h"
-#include "remoting/protocol/v1_authenticator.h"
-#include "remoting/protocol/v2_authenticator.h"
 
 namespace remoting {
 namespace protocol {
