a03b8780f6586c05e05b65b6216e27062d968306
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-05 09:46:20 +0000

    Web Inspector: simplify the JavaScriptOutline dialog interaction. https://bugs.webkit.org/show_bug.cgi?id=85701
    Reviewed by Yury Semikhatsky.
    
    Removed bidirectional dependency between scripts panel and outline dialog.
    
    * inspector/front-end/FilteredItemSelectionDialog.js:
    (WebInspector.JavaScriptOutlineDialog):
    (WebInspector.JavaScriptOutlineDialog.show):
    (WebInspector.JavaScriptOutlineDialog.prototype.requestItems):
    (WebInspector.JavaScriptOutlineDialog.prototype._didBuildOutlineChunk):
    (WebInspector.JavaScriptOutlineDialog.prototype.selectItem):
    * inspector/front-end/ScriptFormatterWorker.js:
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype.addToWatch):
    (WebInspector.ScriptsPanel.prototype._showJavaScriptOutlineDialog):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ScriptsPanel.js
@@ -45,7 +45,6 @@ WebInspector.ScriptsPanel = function(presentationModel)
         return this.visibleView;
     }
     WebInspector.GoToLineDialog.install(this, viewGetter.bind(this));
-    WebInspector.JavaScriptOutlineDialog.install(this, viewGetter.bind(this));
 
     this.debugToolbar = this._createDebugToolbar();
 
@@ -127,6 +126,7 @@ WebInspector.ScriptsPanel = function(presentationModel)
 
     var scriptOutlineShortcut = WebInspector.JavaScriptOutlineDialog.createShortcut();
     helpSection.addKey(scriptOutlineShortcut.name, WebInspector.UIString("Go to function"));
+    this.registerShortcut(scriptOutlineShortcut.key, this._showJavaScriptOutlineDialog.bind(this));
 
     var panelEnablerHeading = WebInspector.UIString("You need to enable debugging before you can use the Scripts panel.");
     var panelEnablerDisclaimer = WebInspector.UIString("Enabling debugging will make scripts run slower.");
@@ -212,17 +212,6 @@ WebInspector.ScriptsPanel.prototype = {
         this.sidebarPanes.watchExpressions.show();
 
         this._navigatorController.wasShown();
-    },
-
-    _didBuildOutlineChunk: function(event)
-    {
-        WebInspector.JavaScriptOutlineDialog.didAddChunk(event.data);
-        if (event.data.total === event.data.index) {
-            if (this._outlineWorker) {
-                this._outlineWorker.terminate();
-                delete this._outlineWorker;
-            }
-        }
     },
 
     /**
@@ -448,28 +437,6 @@ WebInspector.ScriptsPanel.prototype = {
         this._updateScriptViewStatusBarItems();
 
         return sourceFrame;
-    },
-
-    requestVisibleScriptOutline: function()
-    {
-        
-        /**
-         * @param {?string} content
-         * @param {boolean} contentEncoded
-         * @param {string} mimeType
-         */
-        function contentCallback(content, contentEncoded, mimeType)
-        {
-            if (this._outlineWorker)
-                this._outlineWorker.terminate();
-            this._outlineWorker = new Worker("ScriptFormatterWorker.js");
-            this._outlineWorker.onmessage = this._didBuildOutlineChunk.bind(this);
-            const method = "outline";
-            this._outlineWorker.postMessage({ method: method, params: { content: content, id: this.visibleView.uiSourceCode.id } });
-        }
-
-        if (this.visibleView.uiSourceCode)
-            this.visibleView.uiSourceCode.requestContent(contentCallback.bind(this));
     },
 
     /**
@@ -953,6 +920,11 @@ WebInspector.ScriptsPanel.prototype = {
     addToWatch: function(expression)
     {
         this.sidebarPanes.watchExpressions.addExpression(expression);
+    },
+
+    _showJavaScriptOutlineDialog: function()
+    {
+         WebInspector.JavaScriptOutlineDialog.show(this.visibleView, this.visibleView);
     }
 }
 
