918845a3a4fc7c57273fdde75fafa5d0a81e634b
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 03:11:41 +0000

    Add a first-class off-store install UI to chrome://extensions/.
    This patch implements GTK only. Other ports will be separate CLs.
    
    BUG=55584
    
    Review URL: https://chromiumcodereview.appspot.com/10270031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135525 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_view_gtk.cc
+++ content/browser/web_contents/web_contents_view_gtk.cc
@@ -240,6 +240,10 @@ bool WebContentsViewGtk::IsDoingDrag() const {
 void WebContentsViewGtk::CancelDragAndCloseTab() {
 }
 
+WebDropData* WebContentsViewGtk::GetDropData() const {
+  return drag_dest_->current_drop_data();
+}
+
 bool WebContentsViewGtk::IsEventTracking() const {
   return false;
 }
