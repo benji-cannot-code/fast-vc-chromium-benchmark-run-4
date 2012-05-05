43baa5cac8da20738f1912497cc57021f9991ce9
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 00:45:38 +0000

    Return a list of plugins that support clearing site data instead of the first one.
    BUG=126089
    TEST=see bug for manual test
    
    
    Review URL: http://codereview.chromium.org/10351013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135497 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/plugin_data_remover.h
+++ content/public/browser/plugin_data_remover.h
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CONTENT_PUBLIC_BROWSER_PLUGIN_DATA_REMOVER_H_
 #define CONTENT_PUBLIC_BROWSER_PLUGIN_DATA_REMOVER_H_
 #pragma once
+
+#include <vector>
 
 #include "base/time.h"
 #include "content/common/content_export.h"
@@ -30,11 +32,10 @@ class CONTENT_EXPORT PluginDataRemover {
   // Starts removing plug-in data stored since |begin_time|.
   virtual base::WaitableEvent* StartRemoving(base::Time begin_time) = 0;
 
-  // Returns whether there is a plug-in installed that supports removing LSO
-  // data. If it returns true |plugin| is also set to that plugin. This method
+  // Returns a list of all plug-ins that support removing LSO data. This method
   // will use cached plugin data. Call PluginService::GetPlugins() if the latest
   // data is needed.
-  static bool IsSupported(webkit::WebPluginInfo* plugin);
+  static void GetSupportedPlugins(std::vector<webkit::WebPluginInfo>* plugins);
 };
 
 }  // namespace content
