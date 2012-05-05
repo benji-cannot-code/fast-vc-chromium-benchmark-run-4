15ae2a7416ddebc08ef3379c18f33f5a7f860a6f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-05 21:52:34 +0000

    ui/aura: Include x11_util.h in root_window_host.cc to pick missing functions.
    BUG=10404
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10310032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135561 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window_host_linux.cc
+++ ui/aura/root_window_host_linux.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/touch/touch_factory.h"
 #include "ui/base/view_prop.h"
+#include "ui/base/x/x11_util.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/image/image.h"
 
