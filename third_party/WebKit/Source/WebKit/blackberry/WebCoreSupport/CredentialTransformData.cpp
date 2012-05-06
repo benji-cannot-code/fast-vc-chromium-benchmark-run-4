18ba55b62300b3612ba9615271dfe66a5a2779d6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-06 22:05:38 +0000

    [BlackBerry] Enable credential persistance and auto fill https://bugs.webkit.org/show_bug.cgi?id=85572
    Patch by Jonathan Dong <jonathan.dong@torchmobile.com.cn> on 2012-05-06
    Reviewed by Rob Buis.
    
    .:
    
    Enable credential persistance and auto fill feature by adding
    ENABLE_BLACKBERRY_CREDENTIAL_PERSIST=1.
    
    * Source/cmake/OptionsBlackBerry.cmake:
    
    Source/WebKit/blackberry:
    
    Set autofilled status of credential input fields when credential
    information is auto filled by CredentialManager.
    By doing this the input element will get painted with yellow background
    which can notify user this input field is auto filled by the browser.
    
    No behavior changes, so no new test.
    
    * WebCoreSupport/CredentialTransformData.cpp:
    (WebCore::CredentialTransformData::setCredential):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/CredentialTransformData.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/CredentialTransformData.cpp
@@ -171,7 +171,9 @@ void CredentialTransformData::setCredential(const Credential& credential)
 
     m_credential = credential;
     m_userNameElement->setValue(credential.user());
+    m_userNameElement->setAutofilled();
     m_passwordElement->setValue(credential.password());
+    m_passwordElement->setAutofilled();
 }
 
 bool CredentialTransformData::findPasswordFormFields(HTMLFormElement* form)
