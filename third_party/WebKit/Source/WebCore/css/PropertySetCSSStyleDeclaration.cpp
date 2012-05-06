9f9c150513ef3d219f3280a8784eb09b659f65a0
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-06 10:51:00 +0000

    Build fix.
    Not reviewed.
    
    * css/PropertySetCSSStyleDeclaration.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/PropertySetCSSStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/PropertySetCSSStyleDeclaration.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PropertySetCSSStyleDeclaration.h"
 
 #include "CSSParser.h"
+#include "CSSStyleSheet.h"
 #include "HTMLNames.h"
 #include "InspectorInstrumentation.h"
 #include "MutationObserverInterestGroup.h"
