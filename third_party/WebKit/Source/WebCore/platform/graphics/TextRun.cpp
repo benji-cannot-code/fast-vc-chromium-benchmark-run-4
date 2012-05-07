bd952e32ea8f4dc77c8965620e2bbded7ef3670f
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 02:19:15 +0000

    Shrink TextRun object size https://bugs.webkit.org/show_bug.cgi?id=85751
    Reviewed by Darin Adler.
    
    Reorder the member variables in TextRun so it shrinks from 56 to 40 bytes on my 64-bit build. This is important
    for SVG, since RenderSVGText shrinks because of this.
    
    Also add a compile assert for the expected object size.
    
    * platform/graphics/TextRun.cpp:
    (ExpectedTextRunSize):
    (WebCore):
    * platform/graphics/TextRun.h:
    (WebCore::TextRun::TextRun):
    (WebCore::TextRun::direction):
    (TextRun):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116260 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/TextRun.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/TextRun.cpp
@@ -29,6 +29,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class ExpectedTextRunSize {
+    const void* pointer;;
+    int integers[2];
+    float float1;
+#if ENABLE(SVG)
+    float float2;
+#endif
+    float float3;
+    uint32_t bitfields : 9;
+    RefPtr<TextRun::RenderingContext> renderingContext;
+};
+
+COMPILE_ASSERT(sizeof(TextRun) == sizeof(ExpectedTextRunSize), "TextRun is not of expected size");
+
 bool TextRun::s_allowsRoundingHacks = false;
 
 void TextRun::setAllowsRoundingHacks(bool allowsRoundingHacks)
