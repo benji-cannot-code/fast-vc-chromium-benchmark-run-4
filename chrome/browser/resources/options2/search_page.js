92b4e86cfd645ecfa7d0897d333a334d9543e91c
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-07 21:31:44 +0000

    uber-page: Fix a bug where sections can show up unexpectedly when a search-result is active.
    When a section is hidden by default (e.g. bluetooth), it is not marked
    'search-hidden' after a search. So when the system marks it as visible (e.g.
    BluetoothOptionsHandler detects bluetooth is available), it is made visible even
    if it doesn't match the search criteria. So make sure 'search-hidden' is set
    even for the sections that are hidden by default.
    
    BUG=126201
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10365008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135725 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/search_page.js
+++ chrome/browser/resources/options2/search_page.js
@@ -298,8 +298,7 @@ cr.define('options', function() {
         page = pagesToSearch[key];
         var elements = page.pageDiv.querySelectorAll('section');
         for (var i = 0, node; node = elements[i]; i++) {
-          if (!node.hidden)
-            node.classList.add('search-hidden');
+          node.classList.add('search-hidden');
         }
       }
 
@@ -312,11 +311,10 @@ cr.define('options', function() {
           var elements =
               page.pageDiv.querySelectorAll('section');
           for (var i = 0, node; node = elements[i]; i++) {
-            if (!node.hidden) {
-              if (this.performReplace_(regEx, replaceString, node)) {
-                node.classList.remove('search-hidden');
+            if (this.performReplace_(regEx, replaceString, node)) {
+              node.classList.remove('search-hidden');
+              if (!node.hidden)
                 foundMatches = true;
-              }
             }
           }
         }
