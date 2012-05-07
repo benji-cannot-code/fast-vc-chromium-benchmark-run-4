cb7140cfa001ed8fb6e8c48b75e3494d55a9e475
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-07 20:58:49 +0000

    gdata: Fix a bug where "Don't use mobile data for sync" was not honored
    The original code added in crrev.com/130815 did not work at all, as
    it didn't observe network change events...
    
    BUG=chromium-os:28494
    TEST=added some tests to ensure that the observer was registered; not yet tested on 3G, as I don't have a device with 3G.
    
    Review URL: https://chromiumcodereview.appspot.com/10378037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_sync_client_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_sync_client_unittest.cc
@@ -47,6 +47,10 @@ class GDataSyncClientTest : public testing::Test {
     mock_network_library_ = new chromeos::MockNetworkLibrary;
     chromeos::CrosLibrary::Get()->GetTestApi()->SetNetworkLibrary(
         mock_network_library_, true);
+    EXPECT_CALL(*mock_network_library_, AddNetworkManagerObserver(
+        sync_client_.get())).Times(1);
+    EXPECT_CALL(*mock_network_library_, RemoveNetworkManagerObserver(
+        sync_client_.get())).Times(1);
 
     // Create a temporary directory.
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
@@ -60,6 +64,9 @@ class GDataSyncClientTest : public testing::Test {
   }
 
   virtual void TearDown() OVERRIDE {
+    // The sync client should be deleted before NetworkLibrary, as the sync
+    // client registers itself as observer of NetworkLibrary.
+    sync_client_.reset();
     chromeos::CrosLibrary::Shutdown();
   }
 
@@ -71,6 +78,10 @@ class GDataSyncClientTest : public testing::Test {
         .Times(AnyNumber())
         .WillRepeatedly((Return(active_network_.get())));
     chromeos::Network::TestApi(active_network_.get()).SetConnected(true);
+    // Notify the sync client that the network is changed. This is done via
+    // NetworkLibrary in production, but here, we simulate the behavior by
+    // directly calling OnNetworkManagerChanged().
+    sync_client_->OnNetworkManagerChanged(mock_network_library_);
   }
 
   // Sets up MockNetworkLibrary as if it's connected to cellular network.
@@ -81,6 +92,7 @@ class GDataSyncClientTest : public testing::Test {
         .Times(AnyNumber())
         .WillRepeatedly((Return(active_network_.get())));
     chromeos::Network::TestApi(active_network_.get()).SetConnected(true);
+    sync_client_->OnNetworkManagerChanged(mock_network_library_);
   }
 
   // Sets up MockNetworkLibrary as if it's disconnected.
@@ -92,6 +104,7 @@ class GDataSyncClientTest : public testing::Test {
         .WillRepeatedly((Return(active_network_.get())));
     // Here false is passed to make it disconnected.
     chromeos::Network::TestApi(active_network_.get()).SetConnected(false);
+    sync_client_->OnNetworkManagerChanged(mock_network_library_);
   }
 
   // Sets up test files in the temporary directory.
@@ -225,7 +238,7 @@ TEST_F(GDataSyncClientTest, StartFetchLoop_Offline) {
 
 TEST_F(GDataSyncClientTest, StartFetchLoop_CelluarDisabled) {
   SetUpTestFiles();
-  ConnectToCellular();
+  ConnectToWifi();  // First connect to Wifi.
 
   sync_client_->AddResourceIdForTesting("resource_id_not_fetched_foo");
   sync_client_->AddResourceIdForTesting("resource_id_not_fetched_bar");
@@ -235,12 +248,14 @@ TEST_F(GDataSyncClientTest, StartFetchLoop_CelluarDisabled) {
   // fetching over cellular network is disabled by default.
   EXPECT_CALL(*mock_file_system_, GetFileByResourceId(_, _, _)).Times(0);
 
-  sync_client_->StartFetchLoop();
+  // Then connect to cellular. This will kick off StartFetchLoop().
+  ConnectToCellular();
 }
 
 TEST_F(GDataSyncClientTest, StartFetchLoop_CelluarEnabled) {
   SetUpTestFiles();
-  ConnectToCellular();
+  ConnectToWifi();  // First connect to Wifi.
+
   // Enable fetching over cellular network.
   profile_->GetPrefs()->SetBoolean(prefs::kDisableGDataOverCellular, false);
 
@@ -272,7 +287,8 @@ TEST_F(GDataSyncClientTest, StartFetchLoop_CelluarEnabled) {
           std::string("mime_type_does_not_matter"),
           REGULAR_FILE));
 
-  sync_client_->StartFetchLoop();
+  // Then connect to cellular. This will kick off StartFetchLoop().
+  ConnectToCellular();
 }
 
 TEST_F(GDataSyncClientTest, StartFetchLoop_GDataDisabled) {
