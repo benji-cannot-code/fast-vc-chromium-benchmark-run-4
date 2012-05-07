6387cf22541033ca7cf04c04337ef34e2169dad6
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 02:10:27 +0000

    [chromium] Add WebKit API to access multiple attribute of input element https://bugs.webkit.org/show_bug.cgi?id=85355
    Reviewed by Kent Tamura.
    
    We need this to implement the datalist UI for <input type=email multiple>.
    
    * public/WebInputElement.h:
    (WebInputElement):
    * src/WebInputElement.cpp:
    (WebKit::WebInputElement::isMultiple):
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebInputElement.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebInputElement.h
@@ -86,6 +86,7 @@ namespace WebKit {
         WEBKIT_EXPORT int selectionEnd() const;
         WEBKIT_EXPORT bool isValidValue(const WebString&) const;
         WEBKIT_EXPORT bool isChecked() const;
+        WEBKIT_EXPORT bool isMultiple() const;
 
         WEBKIT_EXPORT WebNodeCollection dataListOptions() const;
 
