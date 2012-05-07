a60b96a55584943a2568eefe575ef0edda376640
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 23:15:00 +0000

    webkitdirs.pm: Make determineNumberOfCPUs work on FreeBSD. https://bugs.webkit.org/show_bug.cgi?id=81481
    Patch by Raphael Kubo da Costa <rakuco@FreeBSD.org> on 2012-05-07
    Reviewed by Dirk Pranke.
    
    FreeBSD has the same sysctl interface present in Darwin to query
    the number of CPUs available, so use that too.
    
    * Scripts/webkitdirs.pm:
    (determineNumberOfCPUs): Share the sysctl call with Darwin.
    (isFreeBSD): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -337,7 +337,7 @@ sub determineNumberOfCPUs
     } elsif (isWindows() || isCygwin()) {
         # Assumes cygwin
         $numberOfCPUs = `ls /proc/registry/HKEY_LOCAL_MACHINE/HARDWARE/DESCRIPTION/System/CentralProcessor | wc -w`;
-    } elsif (isDarwin()) {
+    } elsif (isDarwin() || isFreeBSD()) {
         chomp($numberOfCPUs = `sysctl -n hw.ncpu`);
     }
 }
@@ -1256,6 +1256,11 @@ sub isMsys()
 sub isLinux()
 {
     return ($^O eq "linux") || 0;
+}
+
+sub isFreeBSD()
+{
+    return ($^O eq "freebsd") || 0;
 }
 
 sub isARM()
