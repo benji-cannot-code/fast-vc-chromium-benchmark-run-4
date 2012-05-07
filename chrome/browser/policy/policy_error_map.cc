982a43e49d3bcb2715cb6cb4543d0a150940c774
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-07 18:36:53 +0000

    Wire up the policy for controlling pinned apps in the ash launcher.
    This declares the policy so the policy subsystem can enforce it and
    shows it in about:policy.
    
    BUG=chromium-os:29917
    TEST=Configure pinned apps, log in as a managed user, observe that the launcher shows the pinned apps as configured.
    
    Review URL: https://chromiumcodereview.appspot.com/10316022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_error_map.cc
+++ chrome/browser/policy/policy_error_map.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/string_number_conversions.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "grit/generated_resources.h"
@@ -18,24 +19,29 @@ namespace policy {
 struct PolicyErrorMap::PendingError {
   PendingError(const std::string& policy,
                const std::string& subkey,
+               int index,
                int message_id,
                const std::string& replacement)
       : policy(policy),
         subkey(subkey),
+        index(index),
         message_id(message_id),
         has_replacement(true),
         replacement(replacement) {}
 
   PendingError(const std::string& policy,
                const std::string& subkey,
+               int index,
                int message_id)
       : policy(policy),
         subkey(subkey),
+        index(index),
         message_id(message_id),
         has_replacement(false) {}
 
   std::string policy;
   std::string subkey;
+  int index;
   int message_id;
   bool has_replacement;
   std::string replacement;
@@ -52,26 +58,39 @@ bool PolicyErrorMap::IsReady() const {
 }
 
 void PolicyErrorMap::AddError(const std::string& policy, int message_id) {
-  AddError(PendingError(policy, std::string(), message_id));
+  AddError(PendingError(policy, std::string(), -1, message_id));
 }
 
 void PolicyErrorMap::AddError(const std::string& policy,
                               const std::string& subkey,
                               int message_id) {
-  AddError(PendingError(policy, subkey, message_id));
+  AddError(PendingError(policy, subkey, -1, message_id));
+}
+
+void PolicyErrorMap::AddError(const std::string& policy,
+                              int index,
+                              int message_id) {
+  AddError(PendingError(policy, std::string(), index, message_id));
 }
 
 void PolicyErrorMap::AddError(const std::string& policy,
                               int message_id,
                               const std::string& replacement) {
-  AddError(PendingError(policy, std::string(), message_id, replacement));
+  AddError(PendingError(policy, std::string(), -1, message_id, replacement));
 }
 
 void PolicyErrorMap::AddError(const std::string& policy,
                               const std::string& subkey,
                               int message_id,
                               const std::string& replacement) {
-  AddError(PendingError(policy, subkey, message_id, replacement));
+  AddError(PendingError(policy, subkey, -1, message_id, replacement));
+}
+
+void PolicyErrorMap::AddError(const std::string& policy,
+                              int index,
+                              int message_id,
+                              const std::string& replacement) {
+  AddError(PendingError(policy, std::string(), index, message_id, replacement));
 }
 
 string16 PolicyErrorMap::GetErrors(const std::string& policy) {
@@ -128,6 +147,10 @@ void PolicyErrorMap::Convert(const PendingError& error) {
   if (!error.subkey.empty()) {
     message = l10n_util::GetStringFUTF16(IDS_POLICY_SUBKEY_ERROR,
                                          ASCIIToUTF16(error.subkey),
+                                         submessage);
+  } else if (error.index >= 0) {
+    message = l10n_util::GetStringFUTF16(IDS_POLICY_LIST_ENTRY_ERROR,
+                                         base::IntToString16(error.index),
                                          submessage);
   } else {
     message = submessage;
--- chrome/test/functional/policy_test_cases.py
+++ chrome/test/functional/policy_test_cases.py
@@ -248,6 +248,7 @@ class PolicyPrefsTestCases(object):
     'GDataDisabled': (None, True, [], ['chromeos']),
     'GDataDisabledOverCellular':
         (None, True, [], ['chromeos']),
+    'PinnedLauncherApps': (None, [], [], ['chromeos']),
 
     # ChromeOS Device policies:
     'DevicePolicyRefreshRate': (None, 300000, [], ['chromeos']),
