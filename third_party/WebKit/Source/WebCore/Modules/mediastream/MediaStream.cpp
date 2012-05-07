66f1cf1df69d802b5852aa35888316fb5e943583
adam.bergkvist@ericsson.com <adam.bergkvist@ericsson.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 15:02:35 +0000

    MediaStream should not be an ActiveDOMObject https://bugs.webkit.org/show_bug.cgi?id=85191
    Reviewed by Adam Barth.
    
    The model with MediaStreamDescriptor and MediaStream (and LocalMediaStream)
    allows the JavaScript objects (MediaStream and LocalMediaStream) to be
    cleaned up while the MediaStreamDescriptor lives on to manage the stream in
    the platform. This happens for example when a URL is created to represent
    a MediaStream (using createObjectURL()). In that case, the MediaStreamDescriptor
    is put into the MediaStreamRegistry and even though the MediaStream object is
    lost, the URL still works since the descriptor is kept in the registry.
    
    The changes introduced in r113460 (http://webkit.org/b/83143) turned
    MediaStream and LocalMediaStream into ActiveDOMObjects. For example on page
    reload, LocalMediaStream calls MediaStreamCenter::didStopLocalMediaStream()
    via its ActiveDOMObject::stop() method. However, when a page reload occurs,
    the LocalMediaStream object may have been cleaned up already and
    MediaStreamCenter::didStopLocalMediaStream() will not be called.
    
    One way to make the behavior consistent would be to call
    MediaStreamCenter::didStopLocalMediaStream() when the descriptor is cleaned up,
    cause then we wouldn't be dependent on the LocalMediaStream object being alive.
    However, calling MediaStreamCenter::didStopLocalMediaStream() might not be the
    correct thing to do when all references to the descriptor are lost since there
    can be MediaStream objects constructed from the tracks of the LocalMediaStream
    that should continue to work. MediaStreamCenter::didStopLocalMediaStream() was
    intended for LocalMediaStream.stop() which is used to revoke access to devices;
    that should not necessarily happen when the descriptor of a LocalMediaStream is
    cleaned up. If it's necessary for some ports to signal to the platform that a
    MediaStreamDescriptor is cleaned up, then I would suggest adding a new function,
    willDestroyMediaStreamDescriptor(), to the MediaStreamCenter interface.
    
    The current resolution is to make MediaStream a ContextDestructionObserver
    instead of an ActiveDOMObject.
    
    Currently not testable.
    
    * Modules/mediastream/LocalMediaStream.cpp:
    (WebCore::LocalMediaStream::create):
    * Modules/mediastream/LocalMediaStream.h:
    (LocalMediaStream):
    * Modules/mediastream/LocalMediaStream.idl:
    * Modules/mediastream/MediaStream.cpp:
    (WebCore::MediaStream::create):
    (WebCore::MediaStream::MediaStream):
    (WebCore::MediaStream::scriptExecutionContext):
    * Modules/mediastream/MediaStream.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.cpp
+++ third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExceptionCode.h"
 #include "MediaStreamCenter.h"
 #include "MediaStreamSource.h"
-#include "ScriptExecutionContext.h"
 #include "UUID.h"
 
 namespace WebCore {
@@ -78,20 +77,16 @@ PassRefPtr<MediaStream> MediaStream::create(ScriptExecutionContext* context, Pas
     RefPtr<MediaStreamDescriptor> descriptor = MediaStreamDescriptor::create(createCanonicalUUIDString(), audioSources, videoSources);
     MediaStreamCenter::instance().didConstructMediaStream(descriptor.get());
 
-    RefPtr<MediaStream> stream = adoptRef(new MediaStream(context, descriptor.release()));
-    stream->suspendIfNeeded();
-    return stream.release();
+    return adoptRef(new MediaStream(context, descriptor.release()));
 }
 
 PassRefPtr<MediaStream> MediaStream::create(ScriptExecutionContext* context, PassRefPtr<MediaStreamDescriptor> streamDescriptor)
 {
-    RefPtr<MediaStream> stream = adoptRef(new MediaStream(context, streamDescriptor));
-    stream->suspendIfNeeded();
-    return stream.release();
+    return adoptRef(new MediaStream(context, streamDescriptor));
 }
 
 MediaStream::MediaStream(ScriptExecutionContext* context, PassRefPtr<MediaStreamDescriptor> streamDescriptor)
-    : ActiveDOMObject(context, this)
+    : ContextDestructionObserver(context)
     , m_descriptor(streamDescriptor)
 {
     m_descriptor->setOwner(this);
@@ -138,7 +133,7 @@ const AtomicString& MediaStream::interfaceName() const
 
 ScriptExecutionContext* MediaStream::scriptExecutionContext() const
 {
-    return ActiveDOMObject::scriptExecutionContext();
+    return ContextDestructionObserver::scriptExecutionContext();
 }
 
 EventTargetData* MediaStream::eventTargetData()
--- third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.h
+++ third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(MEDIA_STREAM)
 
-#include "ActiveDOMObject.h"
+#include "ContextDestructionObserver.h"
 #include "EventTarget.h"
 #include "MediaStreamDescriptor.h"
 #include "MediaStreamTrackList.h"
@@ -38,9 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class ScriptExecutionContext;
-
-class MediaStream : public RefCounted<MediaStream>, public MediaStreamDescriptorOwner, public EventTarget, public ActiveDOMObject {
+class MediaStream : public RefCounted<MediaStream>, public MediaStreamDescriptorOwner, public EventTarget, public ContextDestructionObserver {
 public:
     // Must match the constants in the .idl file.
     enum ReadyState {
