ffa494269756d60de36b3eed034b1bc4084f7d11
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 05:38:38 +0000

    CSP violation reports should have all the fields required by the spec https://bugs.webkit.org/show_bug.cgi?id=85682
    Reviewed by Darin Adler.
    
    Source/WebCore: 
    
    This patch updates our CSP violation reports to include all the fields
    in the spec.  We've discussed all these fields in the working group,
    and we have fairly high confidence that these are safe to expose.
    
    Tests: http/tests/security/contentSecurityPolicy/report-and-enforce.html
           http/tests/security/contentSecurityPolicy/report-blocked-uri-cross-origin.html
           http/tests/security/contentSecurityPolicy/report-blocked-uri.html
           http/tests/security/contentSecurityPolicy/report-only-from-header.html
           http/tests/security/contentSecurityPolicy/report-only.html
           http/tests/security/contentSecurityPolicy/report-uri-from-child-frame.html
           http/tests/security/contentSecurityPolicy/report-uri.html
    
    * page/ContentSecurityPolicy.cpp:
    (CSPDirectiveList):
    (WebCore::CSPDirectiveList::reportViolation):
    (WebCore::CSPDirectiveList::checkSourceAndReportViolation):
    
    LayoutTests: 
    
    Update test results to show the new fields in the violation reports.
    This patch also adds tests that exercise both the same-origin and the
    cross-origin case for blocked-uri.
    
    * http/tests/security/contentSecurityPolicy/report-and-enforce-expected.txt:
    * http/tests/security/contentSecurityPolicy/report-blocked-uri-cross-origin-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/report-blocked-uri-cross-origin.html: Added.
    * http/tests/security/contentSecurityPolicy/report-blocked-uri-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/report-blocked-uri.html: Added.
    * http/tests/security/contentSecurityPolicy/report-only-expected.txt:
    * http/tests/security/contentSecurityPolicy/report-only-from-header-expected.txt:
    * http/tests/security/contentSecurityPolicy/report-uri-expected.txt:
    * http/tests/security/contentSecurityPolicy/report-uri-from-child-frame-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
@@ -519,7 +519,7 @@ private:
     PassOwnPtr<CSPDirective> createCSPDirective(const String& name, const String& value);
 
     CSPDirective* operativeDirective(CSPDirective*) const;
-    void reportViolation(const String& directiveText, const String& consoleMessage) const;
+    void reportViolation(const String& directiveText, const String& consoleMessage, const KURL& blockedURL = KURL()) const;
     void logUnrecognizedDirective(const String& name) const;
     bool checkEval(CSPDirective*) const;
 
@@ -576,7 +576,7 @@ PassOwnPtr<CSPDirectiveList> CSPDirectiveList::create(ScriptExecutionContext* sc
     return policy.release();
 }
 
-void CSPDirectiveList::reportViolation(const String& directiveText, const String& consoleMessage) const
+void CSPDirectiveList::reportViolation(const String& directiveText, const String& consoleMessage, const KURL& blockedURL) const
 {
     String message = m_reportOnly ? "[Report Only] " + consoleMessage : consoleMessage;
     m_scriptExecutionContext->addConsoleMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, message);
@@ -604,9 +604,15 @@ void CSPDirectiveList::reportViolation(const String& directiveText, const String
     // harmless information.
 
     RefPtr<InspectorObject> cspReport = InspectorObject::create();
-    cspReport->setString("document-uri", document->url());
+    cspReport->setString("document-uri", document->url().strippedForUseAsReferrer());
+    String referrer = document->referrer();
+    if (!referrer.isEmpty())
+        cspReport->setString("referrer", referrer);
     if (!directiveText.isEmpty())
         cspReport->setString("violated-directive", directiveText);
+    cspReport->setString("original-policy", m_header);
+    if (blockedURL.isValid())
+        cspReport->setString("blocked-uri", document->securityOrigin()->canRequest(blockedURL) ? blockedURL.strippedForUseAsReferrer() : SecurityOrigin::create(blockedURL)->toString());
 
     RefPtr<InspectorObject> reportObject = InspectorObject::create();
     reportObject->setObject("csp-report", cspReport.release());
@@ -653,7 +659,7 @@ bool CSPDirectiveList::checkSourceAndReportViolation(CSPDirective* directive, co
 {
     if (!directive || directive->allows(url))
         return true;
-    reportViolation(directive->text(), "Refused to load " + type + " from '" + url.string() + "' because of Content-Security-Policy.\n");
+    reportViolation(directive->text(), "Refused to load " + type + " from '" + url.string() + "' because of Content-Security-Policy.\n", url);
     return denyIfEnforcingPolicy();
 }
 
