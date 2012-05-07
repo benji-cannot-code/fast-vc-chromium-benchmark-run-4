c7ad913fcb44d8320456623808fd31aaab7c43a9
mary.wu@torchmobile.com.cn <mary.wu@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-07 04:09:39 +0000

    [BlackBerry] Support html5 download attribute. https://bugs.webkit.org/show_bug.cgi?id=85044
    Reviewed by Antonio Gomes.
    
    Source/WebCore:
    
    Pass download attribute filename to networkJob and it could be
    overridden by the Content-Disposition HTTP header's filename parameter.
    
    * platform/network/blackberry/NetworkJob.cpp:
    (WebCore::NetworkJob::initialize):
    * platform/network/blackberry/ResourceRequest.h:
    (WebCore::ResourceRequest::setSuggestSaveName):
    (WebCore::ResourceRequest::suggestSaveName):
    (ResourceRequest):
    * platform/network/blackberry/ResourceRequestBlackBerry.cpp:
    (WebCore::ResourceRequest::initializePlatformRequest):
    
    Source/WebKit/blackberry:
    
    If a link has download attribute, we should pass that value and be able to
    save file with the name according to HTML5:
    http://updates.html5rocks.com/2011/08/Downloading-resources-in-HTML5-a-download
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::load):
    (BlackBerry::WebKit::WebPage::download):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::startDownload):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequest.h
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequest.h
@@ -118,6 +118,8 @@ public:
     void initializePlatformRequest(BlackBerry::Platform::NetworkRequest&, bool cookiesEnabled, bool isInitial = false, bool isRedirect = false) const;
     void setForceDownload(bool forceDownload) { m_forceDownload = true; }
     bool forceDownload() const { return m_forceDownload; }
+    void setSuggestedSaveName(const String& name) { m_suggestedSaveName = name; }
+    String suggestedSaveName() const { return m_suggestedSaveName; }
 
     // What this request is for.
     TargetType targetType() const { return m_targetType; }
@@ -134,6 +136,7 @@ private:
     String m_token;
     String m_anchorText;
     String m_overrideContentType;
+    String m_suggestedSaveName;
     bool m_isXMLHTTPRequest;
     bool m_mustHandleInternally;
     bool m_isRequestedByPlugin;
@@ -151,6 +154,7 @@ struct CrossThreadResourceRequestData : public CrossThreadResourceRequestDataBas
     String m_token;
     String m_anchorText;
     String m_overrideContentType;
+    String m_suggestedSaveName;
     bool m_isXMLHTTPRequest;
     bool m_mustHandleInternally;
     bool m_isRequestedByPlugin;
