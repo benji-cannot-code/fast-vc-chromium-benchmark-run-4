05ce443b51418cf08968ed810a161b2fe4f65082
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-07 18:48:55 +0000

    Do not remove automation when all browser windows close on ChromeOS and Mac
    On ChromeOS and Mac, all browser windows closing does not signify
    app termination.  Do not delete AutomationProvider on these platforms.
    
    BUG=chromium-os:30536
    TEST=power_LoadTest on chromeos
    
    R=kkania@chromium.org
    
    Review URL: http://codereview.chromium.org/10379023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135679 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -279,6 +279,7 @@ void TestingAutomationProvider::OnBrowserAdded(const Browser* browser) {
 }
 
 void TestingAutomationProvider::OnBrowserRemoved(const Browser* browser) {
+#if !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
   // For backwards compatibility with the testing automation interface, we
   // want the automation provider (and hence the process) to go away when the
   // last browser goes away.
@@ -290,6 +291,7 @@ void TestingAutomationProvider::OnBrowserRemoved(const Browser* browser) {
         FROM_HERE,
         base::Bind(&TestingAutomationProvider::OnRemoveProvider, this));
   }
+#endif  // !defined(OS_CHROMEOS) && !defined(OS_MACOSX)
 }
 
 void TestingAutomationProvider::OnSourceProfilesLoaded() {
