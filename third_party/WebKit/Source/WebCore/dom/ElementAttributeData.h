64e534aba0a6dce0d6737c91a948ba266e7733a4
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 14:09:37 +0000

    Shrink ElementAttributeData by factoring out Attr object count. <http://webkit.org/b/85825>
    Reviewed by Antti Koivisto.
    
    Stop tracking the number of Attr objects that point to a given Element on the
    Element itself and manage this by having a global hashmap of Element => AttrList,
    where AttrList is a vector of (pointers to) the associated Attr objects.
    
    This shrinks ElementAttributeData by one integer, effectively reducing memory
    consumption by ~530kB when viewing the full HTML5 spec at <http://whatwg.org/c>.
    
    * dom/ElementAttributeData.h:
    (ElementAttributeData):
    
        Remove m_attrCount...
    
    * dom/Node.h:
    (WebCore::Node::hasAttrList):
    (WebCore::Node::setHasAttrList):
    (WebCore::Node::clearHasAttrList):
    
        ...replacing it with a Node flag that tells us whether there's an Attr
        object map for this Node (only applies to Elements.)
    
    * dom/ElementAttributeData.cpp:
    (WebCore::attrListMap):
    (WebCore::attrListForElement):
    (WebCore::ensureAttrListForElement):
    (WebCore::removeAttrListForElement):
    (WebCore::ElementAttributeData::attrIfExists):
    (WebCore::ElementAttributeData::ensureAttr):
    (WebCore::ElementAttributeData::setAttr):
    (WebCore::ElementAttributeData::removeAttr):
    (WebCore::ElementAttributeData::detachAttributesFromElement):
    
        Map Element => per-Element AttrList in a global hash.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ElementAttributeData.h
+++ third_party/WebKit/Source/WebCore/dom/ElementAttributeData.h
@@ -107,7 +107,6 @@ private:
     friend class HTMLConstructionSite;
 
     ElementAttributeData()
-        : m_attrCount(0)
     {
     }
 
@@ -126,8 +125,6 @@ private:
     SpaceSplitString m_classNames;
     AtomicString m_idForStyleResolution;
     Vector<Attribute> m_attributes;
-
-    unsigned m_attrCount;
 };
 
 inline void ElementAttributeData::removeAttribute(const QualifiedName& name, Element* element)
