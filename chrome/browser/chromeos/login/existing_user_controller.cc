4669123495ae5bd6b7a2967f447d4c06a27925a5
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 18:34:09 +0000

    [cros] Show release notes in app window + restore sesssion.
    This covers special case of "Release notes" in ChromeOS.
    All existing users would be shown new Getting Started Guide in app window in addition to their restored session when they would sign in after next update.
    Just passing --app=[GSG URL] is not enough as session would not be restored.
    
    Also using --app-restore-session + chrome://newtab for new user case so that user won't end up with empty desktop after closing GSG window.
    
    Not adding --app-restore-session switch to ChromeOS only switches so that it won't complicate BrowserInit code with ifdefs.
    
    BUG=126098, 1260025
    TEST=Manual. Sign in with existing user after update and see GSG in app window + restored session.
    
    Review URL: https://chromiumcodereview.appspot.com/10332024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135882 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/net/gaia/google_service_auth_error.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/url_constants.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "content/public/browser/browser_thread.h"
@@ -741,6 +742,9 @@ void ExistingUserController::InitializeStartUrls() const {
   if (!guide_url.empty()) {
     CommandLine::ForCurrentProcess()->AppendSwitchASCII(switches::kApp,
                                                         guide_url);
+    // NTP would open in the background, app window with GSG would be focused
+    // so that user won't have an empty desktop after GSG is closed.
+    CommandLine::ForCurrentProcess()->AppendArg(chrome::kChromeUINewTabURL);
   } else {
     // We should not be adding any start URLs if guide
     // is defined as it launches as a standalone app window.
@@ -820,9 +824,8 @@ void ExistingUserController::OptionallyShowReleaseNotes(
   if (current_version.components()[0] > prev_version.components()[0]) {
     std::string release_notes_url = GetGettingStartedGuideURL();
     if (!release_notes_url.empty()) {
-      // TODO(nkostylev): Ideally we'd want to show this in app window
-      // but passing --app=<url> would ignore session restore.
-      CommandLine::ForCurrentProcess()->AppendArg(release_notes_url);
+      CommandLine::ForCurrentProcess()->AppendSwitchASCII(switches::kApp,
+                                                          release_notes_url);
       prefs->SetString(prefs::kChromeOSReleaseNotesVersion,
                        current_version.GetString());
     }
