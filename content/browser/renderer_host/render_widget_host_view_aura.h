bf9985bbecbe8b32f3208aa1a609b0d89521ed87
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 00:13:13 +0000

    Move GLHelper ownership to ImageTransportFactory, and make it thread safe
    There were several problems with previous implementation:
    - there was a use-after-free in CopyTextureToImpl. ReadBackFramebufferComplete was responsible for deleting the texture, but in some cases it could be called after the GLHelper was destroyed, accessing a deleted field, which could have been overwritten (i.e. we'll delete the wrong texture).
    - When creating thumbnails we would create a GLHelper per page, which creates a context (and some resources). That context doesn't go away until we close the page, it's wasteful.
    - If we create the thumbnail on closing the page, we would block
    
    Instead, this patch changes it to create a single GLHelper and fixes the cancelling semantics so that we don't have use-after-frees
    
    BUG=123933
    TEST=check for no leak
    
    Review URL: https://chromiumcodereview.appspot.com/10290007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135764 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.h
+++ content/browser/renderer_host/render_widget_host_view_aura.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/webcursor.h"
 
 namespace content {
-class GLHelper;
 class RenderWidgetHostImpl;
 class RenderWidgetHostView;
 }
@@ -225,8 +224,6 @@ class RenderWidgetHostViewAura
 
   // The model object.
   content::RenderWidgetHostImpl* host_;
-
-  scoped_ptr<content::GLHelper> gl_helper_;
 
   aura::Window* window_;
 
