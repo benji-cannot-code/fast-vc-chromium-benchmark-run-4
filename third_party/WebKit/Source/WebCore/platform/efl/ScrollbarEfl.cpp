2a60462ddde6c8a73e7bcd4c091c522b22c44045
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 19:36:34 +0000

    [EFL][DRT]Do not create ScrollbarEfl when mockScrollbar is enabled. https://bugs.webkit.org/show_bug.cgi?id=81315
    Reviewed by Chang Shu.
    
    ScrollbarEfl creates custom scrollbars as separated layer and they are
    not related to ScrollbarTheme.
    So, DRT/Efl creates custom scrollbars on webview which paints mockScrollbar.
    
    This patch prevents creating custom scrollbars when mockScrollbar is enabled.
    
    Because DRT/Efl only renders the webview now, expected results are same
    until Bug 79853 is landed.
    
    * platform/efl/ScrollbarEfl.cpp:
    (Scrollbar::createNativeScrollbar):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/ScrollbarEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/ScrollbarEfl.cpp
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HostWindow.h"
 #include "IntRect.h"
-#include "NotImplemented.h"
 #include "Page.h"
 #include "ScrollbarTheme.h"
+#include "Settings.h"
 
 #include <Ecore.h>
 #include <Edje.h>
@@ -45,6 +45,9 @@ using namespace WebCore;
 
 PassRefPtr<Scrollbar> Scrollbar::createNativeScrollbar(ScrollableArea* scrollableArea, ScrollbarOrientation orientation, ScrollbarControlSize size)
 {
+    if (Settings::mockScrollbarsEnabled())
+        return adoptRef(new Scrollbar(scrollableArea, orientation, size));
+
     return adoptRef(new ScrollbarEfl(scrollableArea, orientation, size));
 }
 
