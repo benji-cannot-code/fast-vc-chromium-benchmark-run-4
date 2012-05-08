e47b1d5c803f3c1d5af4a23b24c41ff65544e25d
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 04:11:08 +0000

    Unreviewed build fix: adding missing include.
    * src/AsyncFileSystemChromium.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/AsyncFileSystemChromium.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/AsyncFileSystemChromium.cpp
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFileSystemCallbacksImpl.h"
 #include "WebFileWriter.h"
 #include "WebKit.h"
+#include "platform/WebFileSystem.h"
+#include "platform/WebKitPlatformSupport.h"
 #include <public/Platform.h>
 #include <public/WebFileSystem.h>
 #include <wtf/text/CString.h>
