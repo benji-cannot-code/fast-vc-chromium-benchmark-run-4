2b1f5e0b48e6c4f749354f3e35ef743c484c0567
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 01:22:12 +0000

    Cleanup: IWYU for BrowserProcess.
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10258009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process.h
+++ chrome/browser/browser_process.h
@@ -13,11 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <string>
-#include <vector>
 
 #include "base/basictypes.h"
-#include "base/memory/ref_counted.h"
-#include "ipc/ipc_message.h"
 
 class AutomationProviderList;
 class BackgroundModeManager;
@@ -52,6 +49,11 @@ namespace net {
 class URLRequestContextGetter;
 }
 
+namespace policy {
+class BrowserPolicyConnector;
+class PolicyService;
+}
+
 namespace prerender {
 class PrerenderTracker;
 }
@@ -60,11 +62,6 @@ namespace printing {
 class BackgroundPrintingManager;
 class PrintJobManager;
 class PrintPreviewTabController;
-}
-
-namespace policy {
-class BrowserPolicyConnector;
-class PolicyService;
 }
 
 namespace safe_browsing {
--- chrome/test/base/testing_browser_process.h
+++ chrome/test/base/testing_browser_process.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
