782ce15d4f3719f793affcc6b43f91530a55e579
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 00:12:05 +0000

    NTP: Allow collapsing of tab lists in Other Devices menu.
    Adds a new 'Collapse' menu item to the session context menu.
    When a session is collapsed, its name remains visible, but the list
    of tabs underneath it is not shown. The list can be reshown using
    the 'Expand' menu item.
    
    This CL also contains some visual tweaks: the drop-down menu should
    come immediately after the device name, and device sections should have
    more space between them.
    
    BUG=124828
    TEST=Manual.
    
    Review URL: https://chromiumcodereview.appspot.com/10388007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_ui.cc
+++ chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -283,6 +283,7 @@ void NewTabUI::RegisterUserPrefs(PrefService* prefs) {
   MostVisitedHandler::RegisterUserPrefs(prefs);
   if (NewTabUI::IsSuggestionsPageEnabled())
     SuggestionsHandler::RegisterUserPrefs(prefs);
+  browser_sync::ForeignSessionHandler::RegisterUserPrefs(prefs);
 }
 
 // static
