9042eed0bc8c5aad4c6e3f370404057b116fddf2
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 22:20:40 +0000

    Add "kernel" source nacl_mount
    kernel_intercept grabs calls to open, close, getcwd, read, write, etc and routes
    them to the kernel_proxy.  kernel_proxy implements manages mounts, and implements
    thread safe system calls on top of kernel_object primatives.  kernel_object
    primatives track file descriptors and file handles.
    
    
    BUG=122229
    R=binji@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10315015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135927 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/libraries/nacl_mounts/kernel_handle.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <fcntl.h>
+#include <pthread.h>
+
+#include "nacl_mounts/kernel_handle.h"
+#include "nacl_mounts/mount.h"
+#include "nacl_mounts/mount_node.h"
+
+
+// It is only legal to construct a handle while the kernel lock is held.
+KernelHandle::KernelHandle(Mount* mnt, MountNode* node, int mode)
+    : mount_(mnt),
+      node_(node),
+      mode_(mode),
+      offs_(0) {
+  if (mode & O_APPEND) offs_ = node->GetSize();
+}
+
