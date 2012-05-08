4a77971142cf58fb4a15b7a8edef0c83cc58fe6a
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 09:44:29 +0000

    Make sure the Find box stays open on navigations to fragments within the same page.
    BUG=72504
    TEST=Added automated test (FindInPageControllerTest.NavigateClearsOrdinal)
    Review URL: https://chromiumcodereview.appspot.com/10384004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135815 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
+++ chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::NavigationController;
 using content::WebContents;
 
+const std::string kAnchorPage = "anchor.html";
+const std::string kAnchor = "#chapter2";
 const std::string kFramePage = "frames.html";
 const std::string kFrameData = "framedata_general.html";
 const std::string kUserSelectPage = "user-select.html";
@@ -608,6 +610,30 @@ IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, FindDisappearOnNavigate) {
 
   EXPECT_TRUE(GetFindBarWindowInfo(&position, &fully_visible));
   EXPECT_FALSE(fully_visible);
+}
+
+IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, FindStayVisibleOnAnchorLoad) {
+  // First we navigate to our special focus tracking page.
+  GURL url = GetURL(kAnchorPage);
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  browser()->ShowFindBar();
+
+  gfx::Point position;
+  bool fully_visible = false;
+
+  // Make sure it is open.
+  EXPECT_TRUE(GetFindBarWindowInfo(&position, &fully_visible));
+  EXPECT_TRUE(fully_visible);
+
+  // Navigate to the same page (but add an anchor/ref/fragment/whatever the kids
+  // are calling it these days).
+  GURL url_with_anchor = url.Resolve(kAnchor);
+  ui_test_utils::NavigateToURL(browser(), url_with_anchor);
+
+  // Make sure it is still open.
+  EXPECT_TRUE(GetFindBarWindowInfo(&position, &fully_visible));
+  EXPECT_TRUE(fully_visible);
 }
 
 #if defined(OS_MACOSX)
