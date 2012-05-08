46c3db28d38933786b0ba8268d8f75cf35817a91
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 18:37:15 +0000

    Windows: Get AcceleratedPresenter by window handle rather than surface ID.
    This is now consistent for
    GpuProcessHost::OnAcceleratedSurfaceBuffersSwapped
    GpuProcessHost::OnAcceleratedSurfaceSuspend
    GpuProcessHost::OnAcceleratedSurfaceRelease
    
    The issue with getting it via surface ID is GpuSurfaceTracker::GetSurfaceHandle asserts if the surface ID is not in its map.
    
    BUG=125002
    Review URL: https://chromiumcodereview.appspot.com/10383054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135883 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -631,13 +631,13 @@ void GpuProcessHost::OnAcceleratedSurfaceSuspend(int32 surface_id) {
   TRACE_EVENT0("renderer",
       "GpuProcessHost::OnAcceleratedSurfaceSuspend");
 
-  gfx::GLSurfaceHandle handle = GpuSurfaceTracker::Get()->GetSurfaceHandle(
-      surface_id);
-  if (!handle.handle)
+  gfx::PluginWindowHandle handle =
+      GpuSurfaceTracker::Get()->GetSurfaceWindowHandle(surface_id);
+  if (!handle)
     return;
 
   scoped_refptr<AcceleratedPresenter> presenter(
-      AcceleratedPresenter::GetForWindow(handle.handle));
+      AcceleratedPresenter::GetForWindow(handle));
   if (!presenter)
     return;
 
@@ -649,13 +649,13 @@ void GpuProcessHost::OnAcceleratedSurfaceRelease(
   TRACE_EVENT0("renderer",
       "GpuProcessHost::OnAcceleratedSurfaceRelease");
 
-  gfx::GLSurfaceHandle handle = GpuSurfaceTracker::Get()->GetSurfaceHandle(
-      params.surface_id);
-  if (!handle.handle)
+  gfx::PluginWindowHandle handle =
+      GpuSurfaceTracker::Get()->GetSurfaceWindowHandle(params.surface_id);
+  if (!handle)
     return;
 
   scoped_refptr<AcceleratedPresenter> presenter(
-      AcceleratedPresenter::GetForWindow(handle.handle));
+      AcceleratedPresenter::GetForWindow(handle));
   if (!presenter)
     return;
 
