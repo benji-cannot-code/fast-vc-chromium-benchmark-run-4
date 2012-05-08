49c698f00681fba6e763c023273c376030dab27a
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 18:06:19 +0000

    Roll Skia to 3866. Review URL: https://chromiumcodereview.appspot.com/10383063
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135872 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/SkFontHost_fontconfig.cpp
+++ skia/ext/SkFontHost_fontconfig.cpp
@@ -128,7 +128,6 @@ public:
 // static
 SkTypeface* SkFontHost::CreateTypeface(const SkTypeface* familyFace,
                                        const char familyName[],
-                                       const void* data, size_t bytelength,
                                        SkTypeface::Style style)
 {
     std::string resolved_family_name;
@@ -151,7 +150,7 @@ SkTypeface* SkFontHost::CreateTypeface(const SkTypeface* familyFace,
     unsigned filefaceid;
     if (!GetFcImpl()->Match(NULL, &filefaceid,
                             false, -1, /* no filefaceid */
-                            resolved_family_name, data, bytelength,
+                            resolved_family_name, NULL, 0,
                             &bold, &italic)) {
         return NULL;
     }
