108d8a6a32fc9216c7e7c4637de5cab430ac49e5
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 13:37:21 +0000

    Only read VersionInfo::Channel on the browser process.
    BUG=126535
    TEST=unit_tests --gtest_filter='*Feature*', unit_tests --gtest_filter='*Manifest*'
    
    Review URL: https://chromiumcodereview.appspot.com/10386022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_system.cc
+++ chrome/browser/extensions/extension_system.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/extensions/extension_icon_source.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/feature.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -46,6 +47,10 @@ using content::BrowserThread;
 //
 
 ExtensionSystem::ExtensionSystem() {
+  // In lieu of a way for Feature to check whether it's running on the browser
+  // process, tell it.
+  // See http://crbug.com/126535.
+  extensions::Feature::SetChannelCheckingEnabled(true);
 }
 
 ExtensionSystem::~ExtensionSystem() {
--- chrome/common/extensions/feature_unittest.cc
+++ chrome/common/extensions/feature_unittest.cc
@@ -454,6 +454,7 @@ TEST(ExtensionFeatureTest, Equals) {
 
 Feature::Availability IsAvailableInChannel(
     const std::string& channel, VersionInfo::Channel channel_for_testing) {
+  Feature::SetChannelCheckingEnabled(true);
   Feature::SetChannelForTesting(channel_for_testing);
 
   Feature feature;
@@ -463,11 +464,15 @@ Feature::Availability IsAvailableInChannel(
     feature.Parse(&feature_value);
   }
 
-  return feature.IsAvailableToManifest(
+  Feature::Availability availability = feature.IsAvailableToManifest(
       "random-extension",
       Extension::TYPE_UNKNOWN,
       Feature::UNSPECIFIED_LOCATION,
       -1);
+
+  Feature::ResetChannelForTesting();
+  Feature::ResetChannelCheckingEnabled();
+  return availability;
 }
 
 TEST(ExtensionFeatureTest, SupportedChannel) {
@@ -542,8 +547,6 @@ TEST(ExtensionFeatureTest, SupportedChannel) {
       IsAvailableInChannel("", VersionInfo::CHANNEL_BETA));
   EXPECT_EQ(Feature::UNSUPPORTED_CHANNEL,
       IsAvailableInChannel("", VersionInfo::CHANNEL_STABLE));
-
-  Feature::SetChannelForTesting(VersionInfo::CHANNEL_UNKNOWN);
 }
 
 }  // namespace
