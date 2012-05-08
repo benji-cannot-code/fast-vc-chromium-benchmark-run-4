d519548dbd51c3917739c455d96a805112243695
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 04:12:07 +0000

    Fix performance regression for floats caused by LayoutUnit change https://bugs.webkit.org/show_bug.cgi?id=85834
    Reviewed by Eric Seidel.
    
    Fix performance regression caused by r116009 by disabling the use of
    64bit math in FractionalLayoutUnit, simplifying the pixelSnappedMaxX/Y
    math, inlining a couple of methods and replacing the literal 0 (zero)
    with ZERO_LAYOUT_UNIT.
    
    No new tests, no change in functionality.
    
    * platform/FractionalLayoutUnit.h:
    (WebCore::boundedMultiply):
    (WebCore::operator*):
    (WebCore::operator/):
    Disable the use of 64bit (long long) math in the case where the fraction
    is set to 1.
    
    * platform/graphics/FractionalLayoutRect.h:
    (WebCore::FractionalLayoutRect::pixelSnappedMaxX):
    (WebCore::FractionalLayoutRect::pixelSnappedMaxY):
    Simplify the pixel snapping logic for maxX/maxY.
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::isSelfCollapsingBlock):
    (WebCore::RenderBlock::layoutBlock):
    (WebCore::RenderBlock::computeOverflow):
    (WebCore::RenderBlock::clearFloatsIfNeeded):
    (WebCore::RenderBlock::paintChildren):
    (WebCore::RenderBlock::blockSelectionGap):
    (WebCore::RenderBlock::logicalLeftSelectionGap):
    (WebCore::RenderBlock::logicalRightSelectionGap):
    (WebCore::RenderBlock::computeLogicalLocationForFloat):
    (WebCore::RenderBlock::lowestFloatLogicalBottom):
    (WebCore::RenderBlock::getClearDelta):
    (WebCore::RenderBlock::computeBlockPreferredLogicalWidths):
    (WebCore::RenderBlock::addFocusRingRects):
    (WebCore::RenderBlock::adjustLinePositionForPagination):
    * rendering/RenderBlock.h:
    (WebCore::RenderBlock::availableLogicalWidthForLine):
    (WebCore::RenderBlock::availableLogicalWidthForContent):
    (WebCore::RenderBlock::FloatingObject::x):
    (WebCore::RenderBlock::FloatingObject::maxX):
    (WebCore::RenderBlock::FloatingObject::y):
    (WebCore::RenderBlock::FloatingObject::maxY):
    (WebCore::RenderBlock::FloatingObject::width):
    (WebCore::RenderBlock::FloatingObject::height):
    (FloatingObject):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedX):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedMaxX):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedY):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedMaxY):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedWidth):
    (WebCore::RenderBlock::FloatingObject::pixelSnappedHeight):
    (WebCore::RenderBlock::RenderBlockRareData::positiveMarginBeforeDefault):
    (WebCore::RenderBlock::RenderBlockRareData::negativeMarginBeforeDefault):
    (WebCore::RenderBlock::RenderBlockRareData::positiveMarginAfterDefault):
    (WebCore::RenderBlock::RenderBlockRareData::negativeMarginAfterDefault):
    * rendering/RenderBoxModelObject.cpp:
    (WebCore::accumulateRelativePositionOffsets):
    (WebCore::RenderBoxModelObject::offsetLeft):
    (WebCore::RenderBoxModelObject::offsetTop):
    (WebCore::RenderBoxModelObject::computedCSSPaddingTop):
    (WebCore::RenderBoxModelObject::computedCSSPaddingBottom):
    (WebCore::RenderBoxModelObject::computedCSSPaddingLeft):
    (WebCore::RenderBoxModelObject::computedCSSPaddingRight):
    (WebCore::RenderBoxModelObject::computedCSSPaddingBefore):
    (WebCore::RenderBoxModelObject::computedCSSPaddingAfter):
    (WebCore::RenderBoxModelObject::computedCSSPaddingStart):
    (WebCore::RenderBoxModelObject::computedCSSPaddingEnd):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBoxModelObject.cpp
@@ -479,7 +479,7 @@ static LayoutUnit accumulateRelativePositionOffsets(const RenderObject* child, R
 {
     if (!child->isAnonymousBlock() || !child->isRelPositioned())
         return 0;
-    LayoutUnit offset = 0;
+    LayoutUnit offset = ZERO_LAYOUT_UNIT;
     RenderObject* p = toRenderBlock(child)->inlineElementContinuation();
     while (p && p->isRenderInline()) {
         if (p->isRelPositioned())
@@ -541,7 +541,7 @@ LayoutUnit RenderBoxModelObject::offsetLeft() const
     // If the element is the HTML body element or does not have an associated box
     // return 0 and stop this algorithm.
     if (isBody())
-        return 0;
+        return ZERO_LAYOUT_UNIT;
     
     RenderBoxModelObject* offsetPar = offsetParent();
     LayoutUnit xPos = (isBox() ? toRenderBox(this)->left() : ZERO_LAYOUT_UNIT);
@@ -575,7 +575,7 @@ LayoutUnit RenderBoxModelObject::offsetTop() const
     // If the element is the HTML body element or does not have an associated box
     // return 0 and stop this algorithm.
     if (isBody())
-        return 0;
+        return ZERO_LAYOUT_UNIT;
     
     RenderBoxModelObject* offsetPar = offsetParent();
     LayoutUnit yPos = (isBox() ? toRenderBox(this)->top() : ZERO_LAYOUT_UNIT);
@@ -615,7 +615,7 @@ int RenderBoxModelObject::pixelSnappedOffsetHeight() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingTop() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingTop();
     if (padding.isPercent())
@@ -627,7 +627,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingTop() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingBottom() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingBottom();
     if (padding.isPercent())
@@ -639,7 +639,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingBottom() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingLeft() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingLeft();
     if (padding.isPercent())
@@ -651,7 +651,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingLeft() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingRight() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingRight();
     if (padding.isPercent())
@@ -663,7 +663,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingRight() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingBefore() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingBefore();
     if (padding.isPercent())
@@ -675,7 +675,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingBefore() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingAfter() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingAfter();
     if (padding.isPercent())
@@ -687,7 +687,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingAfter() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingStart() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingStart();
     if (padding.isPercent())
@@ -699,7 +699,7 @@ LayoutUnit RenderBoxModelObject::computedCSSPaddingStart() const
 
 LayoutUnit RenderBoxModelObject::computedCSSPaddingEnd() const
 {
-    LayoutUnit w = 0;
+    LayoutUnit w = ZERO_LAYOUT_UNIT;
     RenderView* renderView = 0;
     Length padding = style()->paddingEnd();
     if (padding.isPercent())
