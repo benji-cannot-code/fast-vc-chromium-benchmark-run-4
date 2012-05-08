bbe44c747ad35b14a735777d960156af975e2e16
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 03:00:43 +0000

    If generating a domain bound cert fails, continue the connection without it.
    BUG=125768
    TEST=hack ServerBoundCertService::GenerateCert to always fail
    
    Review URL: https://chromiumcodereview.appspot.com/10315008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135783 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/ssl/ssl3con.c
+++ net/third_party/nss/ssl/ssl3con.c
@@ -6088,6 +6088,7 @@ ssl3_RestartHandshakeAfterCertReq(sslSocket *         ss,
 	if (certChain) {
 	    CERT_DestroyCertificateList(certChain);
 	}
+	PORT_SetError(SEC_ERROR_LIBRARY_FAILURE);
 	rv = SECFailure;
     }
     return rv;
