8fff5167aec1376a5a2b80f5f9f43276f2593bc0
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 22:45:30 +0000

    Fix TTS code to be compatible with new extension API parameter serialization.
    Some TTS tests broke when I landed codereview.chromium.org/10161038 because it
    turned out the tts.speak method takes the incoming options dictionary argument
    and re-purposes it to send an event it dispatches back to extension code. With
    the above reverenced CL changes to avoid JSON.stringify, instead using
    V8ValueConverter, it turns out one subtle difference between the two is that
    JSON.stringify will ignore key/value pairs where the value is not a
    stringifiable type (in this case, a function), whereas V8ValueConverter leaves
    the key in and inserts a null value. This was causing the schema parameter
    validation to fail for the event dispatch.
    
    BUG=122675
    TEST=Existing tests should pass, and re-landing the CL referenced above should
    work with the TTS API browser tests.
    
    Review URL: https://chromiumcodereview.appspot.com/10382065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135933 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/extension_api/tts_extension_api_constants.cc
+++ chrome/browser/speech/extension_api/tts_extension_api_constants.cc
@@ -23,6 +23,7 @@ const char kDesiredEventTypesKey[] = "desiredEventTypes";
 const char kExtensionIdKey[] = "extensionId";
 const char kSrcIdKey[] = "srcId";
 const char kIsFinalEventKey[] = "isFinalEvent";
+const char kOnEventKey[] = "onEvent";
 
 const char kGenderFemale[] = "female";
 const char kGenderMale[] = "male";
