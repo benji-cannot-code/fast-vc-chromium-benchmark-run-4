3a03df794efca42ca69b59325c8c588c84d9bbea
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 15:08:51 +0000

    gtk: Remove tab_strip_model_delegate.h include from tab_strip_gtk.cc
    TabStripGtk is not using this include so remove it now.
    
    R=erg@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10356039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/tabs/tab_strip_gtk.cc
+++ chrome/browser/ui/gtk/tabs/tab_strip_gtk.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/tabs/tab_strip_model_delegate.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_navigator.h"
