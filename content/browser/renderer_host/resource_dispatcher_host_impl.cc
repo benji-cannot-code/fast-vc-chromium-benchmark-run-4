f96bdf5e8ffde671f65a55ad7558dd4f4e612c58
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-08 23:25:22 +0000

    Allow for overriding the handling of the referrer header in the network stack
    When a non-default referrer policy is effective, the network stack should not
    mess with the header
    
    BUG=124750
    TEST=the test for that code path is disabled :-/
    
    Review URL: https://chromiumcodereview.appspot.com/10309005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135943 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/resource_dispatcher_host_impl.cc
+++ content/browser/renderer_host/resource_dispatcher_host_impl.cc
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/appcache/appcache_interfaces.h"
 #include "webkit/blob/blob_storage_controller.h"
 #include "webkit/blob/shareable_file_reference.h"
+#include "webkit/glue/webkit_glue.h"
 
 using base::Time;
 using base::TimeDelta;
@@ -814,6 +815,7 @@ void ResourceDispatcherHostImpl::BeginRequest(
     request->set_method(request_data.method);
     request->set_first_party_for_cookies(request_data.first_party_for_cookies);
     request->set_referrer(referrer.url.spec());
+    webkit_glue::ConfigureURLRequestForReferrerPolicy(request, referrer.policy);
     net::HttpRequestHeaders headers;
     headers.AddHeadersFromString(request_data.headers);
     request->SetExtraRequestHeaders(headers);
--- webkit/glue/webkit_glue.h
+++ webkit/glue/webkit_glue.h
@@ -19,11 +19,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/platform_file.h"
 #include "base/string16.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebCanvas.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebReferrerPolicy.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFileError.h"
 #include "webkit/glue/webkit_glue_export.h"
 
 class GURL;
 class SkBitmap;
+
+namespace net {
+class URLRequest;
+}
 
 namespace skia {
 class PlatformCanvas;
@@ -154,6 +159,10 @@ std::string GetInspectorProtocolVersion();
 // Tells caller whether the given protocol version is supported by the.
 WEBKIT_GLUE_EXPORT bool IsInspectorProtocolVersionSupported(
     const std::string& version);
+
+// Configures the URLRequest according to the referrer policy.
+WEBKIT_GLUE_EXPORT void ConfigureURLRequestForReferrerPolicy(
+    net::URLRequest* request, WebKit::WebReferrerPolicy referrer_policy);
 
 }  // namespace webkit_glue
 
