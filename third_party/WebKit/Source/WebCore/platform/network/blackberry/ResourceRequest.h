6f058497322c5fc4197fbb1c0bcd0570b25ee0cc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 14:02:35 +0000

    [Blackberry] remove m_isRequestedByPlugin in ResourceRequest https://bugs.webkit.org/show_bug.cgi?id=84559
    Patch by Chris Guan <chris.guan@torchmobile.com.cn> on 2012-05-08
    Reviewed by Antonio Gomes.
    
    Source/WebCore:
    
    ResourceRequest is a network level abstraction, and it should
    not know anything about web concepts such as plug-ins, and Blackberry
    does not need m_isRequestedByPlugin any longer, So clean up all related
    code.
    
    Tested on a website of http://edition.cnn.com, a new window should be
    opened only when user has a singe tap gesture on a plugin element.
    
    * platform/network/blackberry/ResourceRequest.h:
    (WebCore::ResourceRequest::ResourceRequest):
    (ResourceRequest):
    (CrossThreadResourceRequestData):
    * platform/network/blackberry/ResourceRequestBlackBerry.cpp:
    (WebCore::ResourceRequest::doPlatformCopyData):
    (WebCore::ResourceRequest::doPlatformAdopt):
    
    Source/WebKit/blackberry:
    
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::dispatchDecidePolicyForNewWindowAction):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequest.h
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequest.h
@@ -56,7 +56,6 @@ public:
         : ResourceRequestBase(KURL(ParsedURLString, url), UseProtocolCachePolicy)
         , m_isXMLHTTPRequest(false)
         , m_mustHandleInternally(false)
-        , m_isRequestedByPlugin(false)
         , m_forceDownload(false)
         , m_targetType(TargetIsUnspecified)
     {
@@ -66,7 +65,6 @@ public:
         : ResourceRequestBase(url, UseProtocolCachePolicy)
         , m_isXMLHTTPRequest(false)
         , m_mustHandleInternally(false)
-        , m_isRequestedByPlugin(false)
         , m_forceDownload(false)
         , m_targetType(TargetIsUnspecified)
     {
@@ -76,7 +74,6 @@ public:
         : ResourceRequestBase(url, policy)
         , m_isXMLHTTPRequest(false)
         , m_mustHandleInternally(false)
-        , m_isRequestedByPlugin(false)
         , m_forceDownload(false)
         , m_targetType(TargetIsUnspecified)
     {
@@ -87,7 +84,6 @@ public:
         : ResourceRequestBase(KURL(), UseProtocolCachePolicy)
         , m_isXMLHTTPRequest(false)
         , m_mustHandleInternally(false)
-        , m_isRequestedByPlugin(false)
         , m_forceDownload(false)
         , m_targetType(TargetIsUnspecified)
     {
@@ -107,9 +103,6 @@ public:
 
     void setIsXMLHTTPRequest(bool isXMLHTTPRequest) { m_isXMLHTTPRequest = isXMLHTTPRequest; }
     bool isXMLHTTPRequest() const { return m_isXMLHTTPRequest; }
-
-    void setIsRequestedByPlugin(bool isRequestedByPlugin) { m_isRequestedByPlugin = isRequestedByPlugin; }
-    bool isRequestedByPlugin() const { return m_isRequestedByPlugin; }
 
     // Marks requests which must be handled by webkit even if LinksHandledExternally is set.
     void setMustHandleInternally(bool mustHandleInternally) { m_mustHandleInternally = mustHandleInternally; }
@@ -139,7 +132,6 @@ private:
     String m_suggestedSaveName;
     bool m_isXMLHTTPRequest;
     bool m_mustHandleInternally;
-    bool m_isRequestedByPlugin;
     bool m_forceDownload;
     TargetType m_targetType;
 
@@ -157,7 +149,6 @@ struct CrossThreadResourceRequestData : public CrossThreadResourceRequestDataBas
     String m_suggestedSaveName;
     bool m_isXMLHTTPRequest;
     bool m_mustHandleInternally;
-    bool m_isRequestedByPlugin;
     bool m_forceDownload;
     ResourceRequest::TargetType m_targetType;
 };
