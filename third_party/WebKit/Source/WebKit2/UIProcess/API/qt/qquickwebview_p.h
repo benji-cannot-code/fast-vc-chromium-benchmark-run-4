666f78a7167a72f29fa7ef57f3161ac94ba9aa74
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 14:12:55 +0000

    [Qt] Add QML/WK2 evaluateJavaScript experimental API https://bugs.webkit.org/show_bug.cgi?id=85496
    Reviewed by Simon Hausmann.
    
    This is an initial patch adding support for strings,
    numbers and bools as return values.
    
    Test: qmltests/WebView/tst_evaluateJavaScript.qml
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (JSCallbackClosure):
    (toQString):
    (toQJSValue):
    (buildQJSValue):
    (javaScriptCallback):
    (QQuickWebViewExperimental::evaluateJavaScript):
    (QQuickWebView::runJavaScriptInMainFrame):
    * UIProcess/API/qt/qquickwebview_p.h:
    * UIProcess/API/qt/tests/qmltests/WebView/tst_evaluateJavaScript.qml: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116420 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p.h
@@ -66,6 +66,7 @@ QT_BEGIN_NAMESPACE
 class QPainter;
 class QUrl;
 class QQuickFlickable;
+class QJSValue;
 QT_END_NAMESPACE
 
 
@@ -333,6 +334,7 @@ public Q_SLOTS:
     void goBackTo(int index);
     void goForwardTo(int index);
     void postMessage(const QString&);
+    void evaluateJavaScript(const QString& script, const QJSValue& value = QJSValue());
 
 Q_SIGNALS:
     void alertDialogChanged();
