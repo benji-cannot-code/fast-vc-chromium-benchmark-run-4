e420e55d157409e7c7947ab60ab5182d99a9e736
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-08 23:50:52 +0000

    [WK2] Push wheel events if there are too many in queue https://bugs.webkit.org/show_bug.cgi?id=85747 <rdar://problem/11390790>
    Reviewed by Anders Carlsson.
    
    It is possible that a whole bunch of messages added to the message queue, or a series
    of long-running messages, cause unresponsiveness. The reason for this is that we have
    a scroll event waiting for acknowledgment from the web process before it sends the next
    event. And in the time between the user has scrolled, causing a large backlog of scroll
    events to be held in the UI process.
    
    We should push new scroll events if the queue accumulates too many of them.
    
    * UIProcess/WebPageProxy.h: The vector m_currentlyProcessedWheelEvents used to hold the
    series of wheel events that were coalesced and sent as a single wheel event to the web
    process. When the web process acknowledges this with didReceiveEvent, the UI process
    cleared that vector, then tried to coalesce the next wheel event to send. Now we might have
    multiple sets of coalesced wheel events that we are sending to the web process. To keep
    track of these sets, m_currentlyProcessedWheelEvents now is a queue of Vectors.
    (WebPageProxy):
    * UIProcess/WebPageProxy.cpp: Add new constant wheelEventQueueSizeThreshold representing
    the threshold of scroll events to look for before we start pushing events.
    (WebKit::canCoalesce): Move static function so that handleWheelEvent() has access. No changes.
    (WebKit::coalesce): Move static function so that handleWheelEvent() has access. No changes.
    (WebKit::coalescedWheelEvent): Move static function so that handleWheelEvent() has access. No changes.
    (WebKit::WebPageProxy::handleWheelEvent): If we are currently waiting for acknowledgment
    from the web process that a wheel event has been handled, we add it to the queue. We
    check to see that the queue size is within our threshold before we return early. Otherwise
    we will start pushing events in the queue. Refactor the rest of the function into
    processNextQueuedWheelEvent() and sendWheelEvent(). If we are not currently waiting for
    acknowledgment, nor have events in the queue, then we send the current wheel event.
    (WebKit::WebPageProxy::processNextQueuedWheelEvent): Try to coalesce events based on the
    wheel event at the head of the queue, and send that event to the web process.
    (WebKit::WebPageProxy::sendWheelEvent): Refactored from handleWheelEvent().
    (WebKit::WebPageProxy::didReceiveEvent): Instead of clearing m_currentlyProcessedWheelEvents,
    which contained the set of one coalesced wheel event, we pull the head Vector, which
    contains the same set of events. Refactor to use processNextQueuedWheelEvent().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
@@ -904,6 +904,9 @@ private:
     void windowedPluginGeometryDidChange(const WebCore::IntRect& frameRect, const WebCore::IntRect& clipRect, uint64_t windowID);
 #endif
 
+    void processNextQueuedWheelEvent();
+    void sendWheelEvent(const WebWheelEvent&);
+
     PageClient* m_pageClient;
     WebLoaderClient m_loaderClient;
     WebPolicyClient m_policyClient;
@@ -1025,7 +1028,7 @@ private:
 #endif
     Deque<NativeWebKeyboardEvent> m_keyEventQueue;
     Deque<NativeWebWheelEvent> m_wheelEventQueue;
-    Vector<NativeWebWheelEvent> m_currentlyProcessedWheelEvents;
+    Deque<OwnPtr<Vector<NativeWebWheelEvent> > > m_currentlyProcessedWheelEvents;
 
     bool m_processingMouseMoveEvent;
     OwnPtr<NativeWebMouseEvent> m_nextMouseMoveEvent;
