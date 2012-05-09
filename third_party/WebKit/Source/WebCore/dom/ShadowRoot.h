15a38744c102e90947179e51983672dc75a7c6b5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-09 12:53:42 +0000

    ShadowRoot needs applyAuthorStyles https://bugs.webkit.org/show_bug.cgi?id=78472
    Patch by Takashi Sakamoto <tasak@google.com> on 2012-05-09
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    Implemented applyAuthorStyles attribute defined in the following spec:
    http://dvcs.w3.org/hg/webcomponents/raw-file/tip/spec/shadow/index.html#shadow-root-attributes
    Since applyAuthorSheets attribute has been already implemented,
    renamed all applyAuthorSheets to applyAuthorStyles and
    added applyAuthorStyles to ShadowRoot.idl.
    Currently, changing dynamically applyAuthorStyles doesn't work. I will fix this isse in bugs:84215: https://bugs.webkit.org/show_bug.cgi?id=84251
    
    Test: fast/dom/shadow/shadow-root-applyAuthorStyles.html
          fast/dom/shadow/shadow-root-applyAuthorStyles-expected.html
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::collectMatchingRulesForList):
    * dom/ShadowRoot.cpp:
    (WebCore::ShadowRoot::ShadowRoot):
    (WebCore::ShadowRoot::applyAuthorStyles):
    (WebCore::ShadowRoot::setApplyAuthorStyles):
    * dom/ShadowRoot.h:
    * dom/TreeScope.cpp:
    (WebCore::TreeScope::applyAuthorStyles):
    * dom/TreeScope.h:
    (TreeScope):
    Changed all applyAuthorSheets to applyAuthorSytles.
    (ShadowRoot):
    * dom/ShadowRoot.idl:
    Added a new attribute, boolean applyAuthorStyles.
    
    LayoutTests:
    
    * fast/dom/shadow/shadow-root-applyAuthorStyles.html:
    * fast/dom/shadow/shadow-root-applyAuthorStyles-expected.html:
    Tested four cases:
    case 1: applyAuthorStyles is false,
    case 2: applyAuthorStyles is true with <script>..</script>,
    case 3: applyAuthorStyles is true with inline script declaration, and
    case 4: applyAuthorStyles is true with important UA property.
    The case 1, 2 and 3 specify "span { ... }" with author styles and
    verify rendering results.
    The case 4 specifies '<input type="file" />' with an author style,
    text-align: end, and verify rendering result.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116521 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
@@ -68,8 +68,8 @@ public:
     InsertionPoint* insertionPointFor(Node*) const;
     void hostChildrenChanged();
 
-    virtual bool applyAuthorSheets() const;
-    void setApplyAuthorSheets(bool);
+    virtual bool applyAuthorStyles() const OVERRIDE;
+    void setApplyAuthorStyles(bool);
 
     Element* host() const { return shadowHost(); }
     ElementShadow* owner() const;
@@ -105,7 +105,7 @@ private:
 
     ShadowRoot* m_prev;
     ShadowRoot* m_next;
-    bool m_applyAuthorSheets : 1;
+    bool m_applyAuthorStyles : 1;
     InsertionPoint* m_insertionPointAssignedTo;
 };
 
--- third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
+++ third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
@@ -140,7 +140,7 @@ Element* TreeScope::findAnchor(const String& name)
     return 0;
 }
 
-bool TreeScope::applyAuthorSheets() const
+bool TreeScope::applyAuthorStyles() const
 {
     return true;
 }
