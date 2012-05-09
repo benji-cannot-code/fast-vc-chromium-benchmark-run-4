6bd0df396dced27469144a407d675e2d2b0b9696
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 23:45:26 +0000

    update the default browser state on every refresh
    BUG=125610
    TEST=go to settings, refresh page. Check that the default browser state is still correct.
    
    Review URL: https://chromiumcodereview.appspot.com/10310052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/browser_options_handler2.cc
+++ chrome/browser/ui/webui/options2/browser_options_handler2.cc
@@ -554,7 +554,6 @@ void BrowserOptionsHandler::InitializeHandler() {
   default_browser_policy_.Init(prefs::kDefaultBrowserSettingEnabled,
                                g_browser_process->local_state(),
                                this);
-  UpdateDefaultBrowserState();
 
   registrar_.Add(this, chrome::NOTIFICATION_PROFILE_CACHED_INFO_CHANGED,
                  content::NotificationService::AllSources());
@@ -610,6 +609,7 @@ void BrowserOptionsHandler::InitializePage() {
   OnTemplateURLServiceChanged();
   ObserveThemeChanged();
   OnStateChanged();
+  UpdateDefaultBrowserState();
 
   SetupMetricsReportingCheckbox();
   SetupMetricsReportingSettingVisibility();
