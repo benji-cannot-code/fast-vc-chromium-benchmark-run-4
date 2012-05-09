f63d4971a4eec861247c483fc32c1dfd5f1b5065
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-09 09:04:02 +0000

    [EFL][DRT] LayoutTestController addUserScript implementation https://bugs.webkit.org/show_bug.cgi?id=84718
    Patch by Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com> on 2012-05-09
    Reviewed by Antonio Gomes.
    
    Source/WebKit/efl:
    
    Added helper functions for adding/clearing of user scripts.
    
    * WebCoreSupport/DumpRenderTreeSupportEfl.cpp:
    (DumpRenderTreeSupportEfl::addUserScript):
    (DumpRenderTreeSupportEfl::clearUserScripts):
    * WebCoreSupport/DumpRenderTreeSupportEfl.h:
    
    Tools:
    
    Added implementation of addUserScript() to LayoutTestController.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::resetDefaultsToConsistentValues): clear added user scripts
    * DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
    (LayoutTestController::addUserScript): added implementation
    
    LayoutTests:
    
    Unskipped corresponding testcases.
    
    * platform/efl/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
@@ -254,6 +254,7 @@ void DumpRenderTreeChrome::resetDefaultsToConsistentValues()
 
     DumpRenderTreeSupportEfl::clearFrameName(mainFrame());
     DumpRenderTreeSupportEfl::clearOpener(mainFrame());
+    DumpRenderTreeSupportEfl::clearUserScripts(mainView());
     DumpRenderTreeSupportEfl::setInteractiveFormValidationEnabled(mainView(), true);
     DumpRenderTreeSupportEfl::setAuthorAndUserStylesEnabled(mainView(), true);
     DumpRenderTreeSupportEfl::setSmartInsertDeleteEnabled(mainView(), false);
