b3996fb09a6c54255201c9804f4d30c6c25f0ee2
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 21:54:27 +0000

    Coverity: Fix a leak.
    Also:
    * Clarify the interface for DictionaryValue::MergeDictionary.
    * Add tests that MergeDictionary performs a deep copy.
    
    FYI:
    * ReadExternalExtensionPrefFile() needs unit testing.
    
    CID=103941
    BUG=none
    TEST=ValuesTest.MergeDictionaryDeepCopy
    R=finnur
    TBR=brettw
    
    Review URL: https://chromiumcodereview.appspot.com/10358020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136137 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.h
+++ base/values.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -314,10 +314,11 @@ class BASE_EXPORT DictionaryValue : public Value {
   // the copy.  This never returns NULL, even if |this| itself is empty.
   DictionaryValue* DeepCopyWithoutEmptyChildren();
 
-  // Merge a given dictionary into this dictionary. This is done recursively,
-  // i.e. any subdictionaries will be merged as well. In case of key collisions,
-  // the passed in dictionary takes precedence and data already present will be
-  // replaced.
+  // Merge |dictionary| into this dictionary. This is done recursively, i.e. any
+  // sub-dictionaries will be merged as well. In case of key collisions, the
+  // passed in dictionary takes precedence and data already present will be
+  // replaced. Values within |dictionary| are deep-copied, so |dictionary| may
+  // be freed any time after this call.
   void MergeDictionary(const DictionaryValue* dictionary);
 
   // Swaps contents with the |other| dictionary.
