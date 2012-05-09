34c565ab465d6c3957e11751541eee3b3add8627
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 21:03:19 +0000

    Allow the RWHVW to be focused when a window is restored from minimized.
    This restores a hack that was in place in the old NativeTabContentsContainer code that seemed to clear focus during restoration (though the hack was for another purpose, the hack seemed to fix this bug too). I've isolated the hack to the re-activation code.
    
    http://crbug.com/125976
    TEST=see bug
    Review URL: https://chromiumcodereview.appspot.com/10378043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136124 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/accessible_pane_view_unittest.cc
+++ ui/views/accessible_pane_view_unittest.cc
@@ -85,6 +85,7 @@ TEST_F(AccessiblePaneViewTest, SimpleSetPaneFocus) {
   View* root = widget->GetRootView();
   root->AddChildView(test_view);
   widget->Show();
+  widget->Activate();
 
   // Set pane focus succeeds, focus on child.
   EXPECT_TRUE(test_view->SetPaneFocusAndFocusDefault());
@@ -114,6 +115,7 @@ TEST_F(AccessiblePaneViewTest, TwoSetPaneFocus) {
   root->AddChildView(test_view);
   root->AddChildView(test_view_2);
   widget->Show();
+  widget->Activate();
 
   // Set pane focus succeeds, focus on child.
   EXPECT_TRUE(test_view->SetPaneFocusAndFocusDefault());
@@ -143,6 +145,7 @@ TEST_F(AccessiblePaneViewTest, PaneFocusTraversal) {
   root->AddChildView(original_test_view);
   root->AddChildView(test_view);
   widget->Show();
+  widget->Activate();
 
   // Set pane focus on first view.
   EXPECT_TRUE(original_test_view->SetPaneFocus(
