95ddc536d26edd4af71a76e7cfa834f8479bee66
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 17:36:25 +0000

    Coverity: Fix pass by value error.
    CID=103860
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10379032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_ui.h
+++ chrome/browser/extensions/extension_install_ui.h
@@ -176,7 +176,7 @@ class ExtensionInstallUI : public ImageLoadingTracker::Observer {
   virtual void ConfirmInlineInstall(Delegate* delegate,
                                     const Extension* extension,
                                     SkBitmap* icon,
-                                    Prompt prompt);
+                                    const Prompt& prompt);
 
   // This is called by the installer to verify whether the installation from
   // the webstore should proceed.
