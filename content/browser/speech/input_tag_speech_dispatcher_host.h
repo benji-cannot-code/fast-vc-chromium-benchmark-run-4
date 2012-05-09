f48ae6b91a83c57c3c4af222612fb8a2f55d3396
primiano@chromium.org <primiano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 08:07:07 +0000

    Adding missing #pragma once in speech recognition headers.
    BUG=116954
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10270009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136002 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/speech/input_tag_speech_dispatcher_host.h
+++ content/browser/speech/input_tag_speech_dispatcher_host.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef CONTENT_BROWSER_SPEECH_INPUT_TAG_SPEECH_DISPATCHER_HOST_H_
 #define CONTENT_BROWSER_SPEECH_INPUT_TAG_SPEECH_DISPATCHER_HOST_H_
+#pragma once
 
 #include "base/memory/scoped_ptr.h"
 #include "content/common/content_export.h"
