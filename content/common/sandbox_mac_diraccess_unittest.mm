356143ed8008c0274ced19885803c2135ca0d60a
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 21:57:38 +0000

    Mac: Re-enable MacDirAccessSandboxTest.SandboxAccess
    The test failure was likely caused by an infrastructure issue.
    
    BUG=89170
    TEST=All unit tests should pass.
    TBR=thakis@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10389038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136140 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_mac_diraccess_unittest.mm
+++ content/common/sandbox_mac_diraccess_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -143,8 +143,7 @@ class ScopedDirectoryDelete {
 
 typedef scoped_ptr_malloc<FilePath, ScopedDirectoryDelete> ScopedDirectory;
 
-// Disabled as times out, taking out remaining tests: 89170.
-TEST_F(MacDirAccessSandboxTest, DISABLED_SandboxAccess) {
+TEST_F(MacDirAccessSandboxTest, SandboxAccess) {
   using file_util::CreateDirectory;
 
   FilePath tmp_dir;
