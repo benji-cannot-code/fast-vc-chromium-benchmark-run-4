8ea4d73e5897c4b03c3d09f36c6af58a300866fb
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 22:00:44 +0000

    Revert 136127 - Adding use_system_libusb option for third_party/libusb
    There are build and distribution configurations under which packaged libraries
    cannot be included. The use_system_libusb option allows these platforms (Linux,
    currently) to use the pre-installed libusb.
    
    BUG=124803
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10332075
    
    TBR=gdk@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10387048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/usb/usb_service.cc
+++ chrome/browser/usb/usb_service.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/stl_util.h"
 #include "chrome/browser/usb/usb_device.h"
-#include "third_party/libusb/libusb.h"
+#include "third_party/libusb/libusb/libusb.h"
 
 UsbService::UsbService() : running_(true), thread_("UsbThread") {
   libusb_init(&context_);
@@ -76,10 +76,7 @@ void UsbService::PostHandleEventTask() {
 }
 
 void UsbService::HandleEvent() {
-  // TODO(gdk): Once there is a reasonable expectation that platforms will use
-  // libusb >= 1.0.9 this should be changed to handle_events_completed, as the
-  // use of handle_events is deprecated.
-  libusb_handle_events(context_);
+  libusb_handle_events_completed(context_, NULL);
   if (running_) {
     PostHandleEventTask();
   }
