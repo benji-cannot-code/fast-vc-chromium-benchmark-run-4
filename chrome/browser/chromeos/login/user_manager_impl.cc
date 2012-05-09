b22962e0866d9d581c1f804310cd55e3923db9d8
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 04:10:24 +0000

    Set initial wallpaper for new/demo/ephmeral users.
    This tiny CL splitted from CL 10302016 (lgtmed). It sets initial wallpaper correctly for new/demo/ephmeral users and fixes the browser_tests crashes. CL 10302016 is punted to M21 but this piece is necessary for m20 So I split it for easier merge.
    
    TBR=nikita
    BUG=127305
    TEST=Log in a new/demo/ephmeral user, check if the wallpaper is the default
    wallpaper.
    
    Review URL: https://chromiumcodereview.appspot.com/10382072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -424,6 +424,7 @@ void UserManagerImpl::DemoUserLoggedIn() {
   is_current_user_ephemeral_ = true;
   logged_in_user_ = new User(kDemoUser, false);
   SetInitialUserImage(kDemoUser);
+  SetInitialUserWallpaper(kDemoUser);
   NotifyOnLogin();
 }
 
@@ -440,6 +441,7 @@ void UserManagerImpl::EphemeralUserLoggedIn(const std::string& email) {
   is_current_user_ephemeral_ = true;
   logged_in_user_ = CreateUser(email);
   SetInitialUserImage(email);
+  SetInitialUserWallpaper(email);
   NotifyOnLogin();
 }
 
@@ -974,6 +976,13 @@ void UserManagerImpl::SetInitialUserWallpaper(const std::string& username) {
   else
     current_user_wallpaper_index_ = ash::GetDefaultWallpaperIndex();
   SaveUserWallpaperIndex(current_user_wallpaper_index_);
+  // Some browser tests do not have shell instance. And it is not necessary to
+  // create a wallpaper for these tests. Add HasInstance check to prevent tests
+  // crash and speed up the tests by avoid loading wallpaper.
+  if (ash::Shell::HasInstance()) {
+    ash::Shell::GetInstance()->desktop_background_controller()->
+        SetDefaultWallpaper(current_user_wallpaper_index_);
+  }
 }
 
 int UserManagerImpl::GetUserWallpaperIndex() {
