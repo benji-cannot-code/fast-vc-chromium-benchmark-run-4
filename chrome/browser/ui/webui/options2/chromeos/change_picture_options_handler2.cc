63bd19c92ab8eaf0a4473816c7080d1c7a53990b
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 19:49:25 +0000

    Disable custom pictures when ephemeral users enabled
    Setting a custom picture creates a file outside the user's cryptohome.
    This should be disallowed for ephemeral users to prevent information
    leaking.
    
    BUG=chromium-os:30014
    TEST=Manual check that custom pictures are no longer offered
    TEST=unit_tests, browser_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10389018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136098 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/chromeos/change_picture_options_handler2.cc
+++ chrome/browser/ui/webui/options2/chromeos/change_picture_options_handler2.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/path_service.h"
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/login/camera_detector.h"
 #include "chrome/browser/chromeos/login/default_user_images.h"
@@ -93,6 +94,10 @@ void ChangePictureOptionsHandler::GetLocalizedValues(
   localized_strings->SetString("profilePhotoLoading",
       l10n_util::GetStringUTF16(
           IDS_OPTIONS_CHANGE_PICTURE_PROFILE_LOADING_PHOTO));
+
+  localized_strings->SetString("userIsEphemeral",
+      UserManager::Get()->IsCurrentUserEphemeral() ?
+          ASCIIToUTF16("true") : ASCIIToUTF16("false"));
 }
 
 void ChangePictureOptionsHandler::RegisterMessages() {
