acd1fcefaf81094864bedefe476e633009daf383
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 17:59:38 +0000

    Reverting to the deprecated version of GetDefaultRequestContext which maintians a pointer that can return null and won't try to initialize the request context on shutdown. Will fix properly in a follow up CL to maintain this getter per profile.
    BUG=125292
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10381047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136075 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -1201,7 +1201,7 @@ class WarmingObserver : public NetworkLibrary::NetworkManagerObserver {
           chrome_browser_net::UrlInfo::EARLY_LOAD_MOTIVATED,
           kConnectionsNeeded,
           make_scoped_refptr(
-              ProfileManager::GetDefaultProfile()->GetRequestContext()));
+              Profile::Deprecated::GetDefaultRequestContextTemporary()));
       netlib->RemoveNetworkManagerObserver(this);
       delete this;
     }
@@ -1217,7 +1217,7 @@ void LoginUtilsImpl::PrewarmAuthentication() {
         chrome_browser_net::UrlInfo::EARLY_LOAD_MOTIVATED,
         kConnectionsNeeded,
         make_scoped_refptr(
-            ProfileManager::GetDefaultProfile()->GetRequestContext()));
+            Profile::Deprecated::GetDefaultRequestContextTemporary()));
   } else {
     new WarmingObserver();
   }
