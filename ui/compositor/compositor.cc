473b46b6c8ffd98e3122aa5966765d4f47b3c22a
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 20:32:27 +0000

    Don't call ScheduleDraw from destructor
    BUG=127614
    TEST=asan bot passes
    
    Review URL: https://chromiumcodereview.appspot.com/10387062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136377 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -146,6 +146,8 @@ Compositor::Compositor(CompositorDelegate* delegate,
 }
 
 Compositor::~Compositor() {
+  // Don't call |CompositorDelegate::ScheduleDraw| from this point.
+  delegate_ = NULL;
   // There's a cycle between |root_web_layer_| and |host_|, which results in
   // leaking and/or crashing. Explicitly set the root layer to NULL so the cycle
   // is broken.
@@ -176,7 +178,7 @@ void Compositor::ScheduleDraw() {
     // compositeImmediately() directly.
     layout();
     host_.composite();
-  } else {
+  } else if (delegate_) {
     delegate_->ScheduleDraw();
   }
 }
