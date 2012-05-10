6479285c8c849e59087dc0cf7cb34037e3562c2a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 16:01:33 +0000

    ash: Remove the row for power, and add a row for date + system buttons.
    BUG=127430
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10381081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136318 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/power/tray_power.h
+++ ash/system/power/tray_power.h
@@ -14,8 +14,6 @@ namespace ash {
 namespace internal {
 
 namespace tray {
-class DateView;
-class PowerPopupView;
 class PowerTrayView;
 }
 
@@ -38,8 +36,6 @@ class TrayPower : public SystemTrayItem,
   // Overridden from PowerStatusObserver.
   virtual void OnPowerStatusChanged(const PowerSupplyStatus& status) OVERRIDE;
 
-  tray::DateView* date_;
-  tray::PowerPopupView* power_;
   tray::PowerTrayView* power_tray_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayPower);
